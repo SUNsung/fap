b69ad391699b27c0b31489a3cc9337529f7b7fab
SUNsung <weatherius@gmail.com>
2019-05-14 15:07:09 +0300

    Rewrite [545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py] File in: 2019-05-14 15:07:08

--- pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
+++ pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
@@ -1,285 +1,84 @@
 
         
-            infile, outfile = args
+            lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+                 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
+                 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
+                 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
+                 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331,
+                 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
+                 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467,
+                 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563,
+                 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631,
+                 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709,
+                 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+                 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877,
+                 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967,
+                 971, 977, 983, 991, 997]
     
-        def test_secondary_proxy_https(self):
-        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL()
-        req = compat_urllib_request.Request('https://yt-dl.org/ip')
-        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
-        self.assertEqual(
-            ydl.urlopen(req).read().decode('utf-8'),
-            params['secondary_server_ip'])
-    
-        def test_allsubtitles(self):
-        self.DL.params['writesubtitles'] = True
-        self.DL.params['allsubtitles'] = True
-        subtitles = self.getSubtitles()
-        self.assertEqual(set(subtitles.keys()), set(['en']))
-        self.assertEqual(md5(subtitles['en']), 'acaca989e24a9e45a6719c9b3d60815c')
-    
-        def test___qualname__(self):
-        # PEP 3155
-        self.assertEqual(self.b.__qualname__, 'FuncAttrsTest.setUp.<locals>.b')
-        self.assertEqual(FuncAttrsTest.setUp.__qualname__, 'FuncAttrsTest.setUp')
-        self.assertEqual(global_function.__qualname__, 'global_function')
-        self.assertEqual(global_function().__qualname__,
-                         'global_function.<locals>.<lambda>')
-        self.assertEqual(global_function()().__qualname__,
-                         'global_function.<locals>.inner_function')
-        self.assertEqual(global_function()()().__qualname__,
-                         'global_function.<locals>.inner_function.<locals>.LocalClass')
-        self.assertEqual(inner_global_function.__qualname__, 'inner_global_function')
-        self.assertEqual(inner_global_function().__qualname__, 'inner_global_function.<locals>.inner_function2')
-        self.b.__qualname__ = 'c'
-        self.assertEqual(self.b.__qualname__, 'c')
-        self.b.__qualname__ = 'd'
-        self.assertEqual(self.b.__qualname__, 'd')
-        # __qualname__ must be a string
-        self.cannot_set_attr(self.b, '__qualname__', 7, TypeError)
-    
-            The method optionally resolves the names relative to a given module.
-        '''
-        parts = name.split('.')
-        error_case, error_message = None, None
-        if module is None:
-            parts_copy = parts[:]
-            while parts_copy:
-                try:
-                    module_name = '.'.join(parts_copy)
-                    module = __import__(module_name)
-                    break
-                except ImportError:
-                    next_attribute = parts_copy.pop()
-                    # Last error so we can give it to the user if needed.
-                    error_case, error_message = _make_failed_import_test(
-                        next_attribute, self.suiteClass)
-                    if not parts_copy:
-                        # Even the top level import failed: report that error.
-                        self.errors.append(error_message)
-                        return error_case
-            parts = parts[1:]
-        obj = module
-        for part in parts:
-            try:
-                parent, obj = obj, getattr(obj, part)
-            except AttributeError as e:
-                # We can't traverse some part of the name.
-                if (getattr(obj, '__path__', None) is not None
-                    and error_case is not None):
-                    # This is a package (no __path__ per importlib docs), and we
-                    # encountered an error importing something. We cannot tell
-                    # the difference between package.WrongNameTestClass and
-                    # package.wrong_module_name so we just report the
-                    # ImportError - it is more informative.
-                    self.errors.append(error_message)
-                    return error_case
-                else:
-                    # Otherwise, we signal that an AttributeError has occurred.
-                    error_case, error_message = _make_failed_test(
-                        part, e, self.suiteClass,
-                        'Failed to access attribute:\n%s' % (
-                            traceback.format_exc(),))
-                    self.errors.append(error_message)
-                    return error_case
-    
-            class MyTest(unittest.TestCase):
-            def test_1(self): pass
-            foobar = Trap()
-    
-    PYTHON2_EXCEPTIONS = (
-    'ArithmeticError',
-    'AssertionError',
-    'AttributeError',
-    'BaseException',
-    'BufferError',
-    'BytesWarning',
-    'DeprecationWarning',
-    'EOFError',
-    'EnvironmentError',
-    'Exception',
-    'FloatingPointError',
-    'FutureWarning',
-    'GeneratorExit',
-    'IOError',
-    'ImportError',
-    'ImportWarning',
-    'IndentationError',
-    'IndexError',
-    'KeyError',
-    'KeyboardInterrupt',
-    'LookupError',
-    'MemoryError',
-    'NameError',
-    'NotImplementedError',
-    'OSError',
-    'OverflowError',
-    'PendingDeprecationWarning',
-    'ReferenceError',
-    'RuntimeError',
-    'RuntimeWarning',
-    # StandardError is gone in Python 3, so we map it to Exception
-    'StopIteration',
-    'SyntaxError',
-    'SyntaxWarning',
-    'SystemError',
-    'SystemExit',
-    'TabError',
-    'TargetScopeError',
-    'TypeError',
-    'UnboundLocalError',
-    'UnicodeDecodeError',
-    'UnicodeEncodeError',
-    'UnicodeError',
-    'UnicodeTranslateError',
-    'UnicodeWarning',
-    'UserWarning',
-    'ValueError',
-    'Warning',
-    'ZeroDivisionError',
-)
-    
-            self.check_tokenize('''\
-async def foo():
-  async for i in 1: pass''', '''\
-    NAME       'async'       (1, 0) (1, 5)
-    NAME       'def'         (1, 6) (1, 9)
-    NAME       'foo'         (1, 10) (1, 13)
-    OP         '('           (1, 13) (1, 14)
-    OP         ')'           (1, 14) (1, 15)
-    OP         ':'           (1, 15) (1, 16)
-    NEWLINE    '\\n'          (1, 16) (1, 17)
-    INDENT     '  '          (2, 0) (2, 2)
-    NAME       'async'       (2, 2) (2, 7)
-    NAME       'for'         (2, 8) (2, 11)
-    NAME       'i'           (2, 12) (2, 13)
-    NAME       'in'          (2, 14) (2, 16)
-    NUMBER     '1'           (2, 17) (2, 18)
-    OP         ':'           (2, 18) (2, 19)
-    NAME       'pass'        (2, 20) (2, 24)
-    DEDENT     ''            (3, 0) (3, 0)
-    ''')
-    
-        def persistent_id(self, obj):
-        # Instead of pickling MemoRecord as a regular class instance, we emit a
-        # persistent ID.
-        if isinstance(obj, MemoRecord):
-            # Here, our persistent ID is simply a tuple, containing a tag and a
-            # key, which refers to a specific record in the database.
-            return ('MemoRecord', obj.key)
-        else:
-            # If obj does not have a persistent ID, return None. This means obj
-            # needs to be pickled as usual.
-            return None
-    
-    # Proxy type for generator objects
-class GeneratorProxy(BaseProxy):
-    _exposed_ = ['__next__']
-    def __iter__(self):
-        return self
-    def __next__(self):
-        return self._callmethod('__next__')
-    
-    #
-#
-#
-    
-    
-def html_page_context(app, pagename, templatename, context, doctree):
-    if templatename != 'page.html':
-        return
-    
-            #Required string to create directory title pages
-        dir_title = '<h2 class=titledir>' + operating_sys.capitalize() + '</h2></body></html>'
-    
-        # When mapping from image ROIs to feature map ROIs, there's some aliasing
-    # (some distinct image ROIs get mapped to the same feature ROI).
-    # Here, we identify duplicate feature ROIs, so we only compute features
-    # on the unique subset.
-    if cfg.DEDUP_BOXES > 0 and not cfg.MODEL.FASTER_RCNN:
-        v = np.array([1, 1e3, 1e6, 1e9, 1e12])
-        hashes = np.round(inputs['rois'] * cfg.DEDUP_BOXES).dot(v)
-        _, index, inv_index = np.unique(
-            hashes, return_index=True, return_inverse=True
-        )
-        inputs['rois'] = inputs['rois'][index, :]
-        boxes = boxes[index, :]
-    
-    
-def add_bbox_regression_targets(roidb):
-    '''Add information needed to train bounding-box regressors.'''
-    for entry in roidb:
-        entry['bbox_targets'] = compute_bbox_regression_targets(entry)
-    
-        fc_dim = cfg.FAST_RCNN.MLP_HEAD_DIM
-    model.FC(current, 'fc6', dim_in * roi_size * roi_size, fc_dim)
-    model.Relu('fc6', 'fc6')
-    return 'fc6', fc_dim
+            return new_key
 
     
+            if len(X) < 2 * self.min_leaf_size:
+            self.prediction = np.mean(y)
+            return
     
-def main(opts):
-    logger = logging.getLogger(__name__)
-    roidb = combined_roidb_for_training(
-        cfg.TRAIN.DATASETS, cfg.TRAIN.PROPOSAL_FILES)
-    logger.info('{:d} roidb entries'.format(len(roidb)))
-    roi_data_loader = RoIDataLoader(
-        roidb,
-        num_loaders=cfg.DATA_LOADER.NUM_THREADS,
-        minibatch_queue_size=cfg.DATA_LOADER.MINIBATCH_QUEUE_SIZE,
-        blobs_queue_capacity=cfg.DATA_LOADER.BLOBS_QUEUE_CAPACITY
-    )
-    blob_names = roi_data_loader.get_output_names()
+        difference = log_predict - log_actual
+    square_diff = np.square(difference)
+    mean_square_diff = square_diff.mean()
     
+        return res
     
-def testSignedInt():
-    check(b'\x99\xd0\x00\xd0\x80\xd0\xff\xd1\x00\x00\xd1\x80\x00'
-          b'\xd1\xff\xff\xd2\x00\x00\x00\x00\xd2\x80\x00\x00\x00'
-          b'\xd2\xff\xff\xff\xff', (0,
-                                    -128,
-                                    -1,
-                                    0,
-                                    -32768,
-                                    -1,
-                                    0,
-                                    -2147483648,
-                                    -1, ))
-    
-    import platform
-import os
-import subprocess
-from .clipboards import (init_osx_clipboard,
-                         init_gtk_clipboard, init_qt_clipboard,
-                         init_xclip_clipboard, init_xsel_clipboard,
-                         init_klipper_clipboard, init_no_clipboard)
-from .windows import init_windows_clipboard
+        changed = True  # 标记样本类别是否改变
+    n_iter = 0  # 记录迭代次数
+    while changed and n_iter < max_iter:
+        changed = False
+        n_iter += 1
     
     
-def test_getitem_callable():
-    # GH 12533
-    s = pd.Series(4, index=list('ABCD'))
-    result = s[lambda x: 'A']
-    assert result == s.loc['A']
-    
-        dct = dict(__instancecheck__=_check, __subclasscheck__=_check)
-    meta = type('ABCBase', (type, ), dct)
-    return meta(name, tuple(), dct)
-    
-    
-@pytest.fixture
-def df_mixed_floats():
-    return DataFrame({'A': ['foo', 'bar', 'foo', 'bar',
-                            'foo', 'bar', 'foo', 'foo'],
-                      'B': ['one', 'one', 'two', 'three',
-                            'two', 'two', 'one', 'three'],
-                      'C': np.random.randn(8),
-                      'D': np.array(
-                          np.random.randn(8), dtype='float32')})
+def search_tokens(tokens, inverse_index, word_freq=None):
+    ''''''
+    ret = dict()
+    for term in tokens:
+        if term in inverse_index:
+            ret[frozenset([term])] = inverse_index[term]
+        else:
+            ret[frozenset([term])] = set()
+    return ret
     
     
-def assert_resolves(css, props, inherited=None):
-    resolve = CSSResolver()
-    actual = resolve(css, inherited=inherited)
-    assert props == actual
+def clip_relu(x, max_value):
+    '''截断 ReLU
+    `o = min(max(0., x), max_value)`
+    '''
+    o = tf.nn.relu(x)
+    o = tf.minimum(o, max_value)
+    return o
+    
+    
+# TODO(huay): char_cnn_embedding
+def char_cnn_embedding(x, c_embed_size=8, share_cnn_weights=True, name='char_cnn_embedding', reuse=None):
+    '''
+    In:  [N, max_n_word, max_n_char]
+    Out: [N, max_n_word, c_embed_size]
+    
+        Args:
+        h: context encoding     shape: [N, T, d]
+        u: question encoding    shape: [N, J, d]
+        T(int): context length
+        J(int): question length
+        d(int): features size
+        name(str):
+        reuse(bool):
+    '''
+    T = T or int(h.get_shape()[-2])
+    
+    
+def get_params_dict():
+    '''以字典形式获取所有 trainable 参数'''
+    param_dict = dict()
+    for var in tf.trainable_variables():
+        param_dict[var.name] = {'shape': list(map(int, var.shape)),
+                                'number': int(reduce(mul, var.shape, 1))}
+    return param_dict
+    
+        Each code snipped should go into a separate file in codedir.
