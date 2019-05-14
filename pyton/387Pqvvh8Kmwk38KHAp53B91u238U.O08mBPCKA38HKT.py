4c753fb11c19a4dd7cd5fd5a5541a3613938e22b
SUNsung <weatherius@gmail.com>
2019-05-14 06:19:49 +0300

    Rewrite [387Pqvvh8Kmwk38KHAp53B91u238U.O08mBPCKA38HKT.py] File in: 2019-05-14 06:19:48

--- pyton/387Pqvvh8Kmwk38KHAp53B91u238U.O08mBPCKA38HKT.py
+++ pyton/387Pqvvh8Kmwk38KHAp53B91u238U.O08mBPCKA38HKT.py
@@ -1,167 +1,195 @@
 
         
         
-def how_to_configure(proc, TIMEOUT):
-    proc.sendline(u'fuck')
-    assert proc.expect([TIMEOUT, u'alias isn't configured'])
-
-    
-    
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
-    
-    
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'echo test')
-    
-    You can download from:
-  https://osxfuse.github.io/
-Error: An unsatisfied requirement failed this build.'''
-    
-    
-class RangeMinValueValidator(MinValueValidator):
-    def compare(self, a, b):
-        return a.lower is None or a.lower < b
-    message = _('Ensure that this range is completely greater than or equal to %(limit_value)s.')
-
-    
-    
-def test_sparse_categorical_crossentropy_4d():
-    y_pred = K.variable(np.array([[[[0.7, 0.1, 0.2],
-                                    [0.0, 0.3, 0.7],
-                                    [0.1, 0.1, 0.8]],
-                                   [[0.3, 0.7, 0.0],
-                                    [0.3, 0.4, 0.3],
-                                    [0.2, 0.5, 0.3]],
-                                   [[0.8, 0.1, 0.1],
-                                    [1.0, 0.0, 0.0],
-                                    [0.4, 0.3, 0.3]]]]))
-    y_true = K.variable(np.array([[[0, 1, 0],
-                                   [2, 1, 0],
-                                   [2, 2, 1]]]))
-    expected_loss = - (np.log(0.7) + np.log(0.3) + np.log(0.1) +
-                       np.log(K.epsilon()) + np.log(0.4) + np.log(0.2) +
-                       np.log(0.1) + np.log(K.epsilon()) + np.log(0.3)) / 9
-    loss = K.eval(losses.sparse_categorical_crossentropy(y_true, y_pred))
-    assert np.isclose(expected_loss, np.mean(loss))
-    
-    # Model creation using tensors from the get_next() graph node.
-inputs, targets = iterator.get_next()
-model_input = layers.Input(tensor=inputs)
-model_output = cnn_layers(model_input)
-train_model = keras.models.Model(inputs=model_input, outputs=model_output)
-    
-        # Returns
-        A Keras model instance (compiled).
+def check_format(filename):
     '''
-    model = Sequential()
-    model.add(Dense(dense_units, input_shape=(max_words,),
-                    kernel_initializer=kernel_initializer))
-    model.add(Activation(activation))
-    model.add(dropout(dropout_rate))
-    
-        @staticmethod
-    def import_windows_ca(certfile):
-        xlog.debug('Begin to import Windows CA')
-        with open(certfile, 'rb') as fp:
-            certdata = fp.read()
-            if certdata.startswith(b'-----'):
-                begin = b'-----BEGIN CERTIFICATE-----'
-                end = b'-----END CERTIFICATE-----'
-                certdata = base64.b64decode(b''.join(certdata[certdata.find(begin)+len(begin):certdata.find(end)].strip().splitlines()))
-        try:
-            common_name = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, certdata).get_subject().CN
-        except Exception as e:
-            logging.error('load_certificate(certfile=%r) 失败：%s', certfile, e)
-            return -1
-    
-        if __password__ and __password__ != kwargs.get('password', ''):
-        start_response('403 Forbidden', [('Content-Type', 'text/html')])
-        yield message_html('403 Wrong password', 'Wrong password(%r)' % kwargs.get('password', ''), 'GoAgent proxy.ini password is wrong!')
-        raise StopIteration
-    
-                            #print 'no viable alt'
-                        self.noViableAlt(s, input)
-                        return 0
-    
-        Validation of predicates
-    occurs when normally parsing the alternative just like matching a token.
-    Disambiguating predicate evaluation occurs when we hoist a predicate into
-    a prediction decision.
+    validates that each line is formatted correctly,
+    appending to error list as needed
     '''
+    with open(filename) as fp:
+        lines = list(line.rstrip() for line in fp)
+    check_alphabetical(lines)
+    # START Check Entries
+    num_in_category = min_entries_per_section + 1
+    category = ''
+    category_line = 0
+    for line_num, line in enumerate(lines):
+        if section_title_re.match(line):
+            title_links.append(section_title_re.match(line).group(1))
+        # check each section for the minimum number of entries
+        if line.startswith(anchor):
+            match = anchor_re.match(line)
+            if match:
+                if match.group(1) not in title_links:
+                    add_error(line_num, 'section header ({}) not added as a title link'.format(match.group(1)))
+            else:
+                add_error(line_num, 'section header is not formatted correctly')
+            if num_in_category < min_entries_per_section:
+                add_error(category_line, '{} section does not have the minimum {} entries (only has {})'.format(
+                    category, min_entries_per_section, num_in_category))
+            category = line.split(' ')[1]
+            category_line = line_num
+            num_in_category = 0
+            continue
+        # skips lines that we do not care about
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        num_in_category += 1
+        segments = line.split('|')[1:-1]
+        if len(segments) < num_segments:
+            add_error(line_num, 'entry does not have all the required sections (have {}, need {})'.format(
+                len(segments), num_segments))
+            continue
+        # START Global
+        for segment in segments:
+            # every line segment should start and end with exactly 1 space
+            if len(segment) - len(segment.lstrip()) != 1 or len(segment) - len(segment.rstrip()) != 1:
+                add_error(line_num, 'each segment must start and end with exactly 1 space')
+        # END Global
+        segments = [seg.strip() for seg in segments]
+        check_entry(line_num, segments)
+    # END Check Entries
     
-            raise NotImplementedError
+        # Define the MLP
+    if self.hparams.hidden_layers == 0:
+      self.weights1 = tf.get_variable(
+          'W1',
+          shape=[network_input, self.hparams.num_classes],
+          dtype=tf.float32)
+      self.bias1 = tf.get_variable(
+          'b1',
+          shape=[self.hparams.num_classes],
+          dtype=tf.float32)
     
-    def handleSlides(slides):
-    for slide in slides:
-        handleSlide(slide)
+        Args:
+      data_name: The name of the data dict, to select which in/out matrices
+        to use.
+      data_bxtxd:  Numpy array training data with shape:
+        batch_size x # time steps x # dimensions
+      ext_input_bxtxi: Numpy array training external input with shape:
+        batch_size x # time steps x # external input dims
+      do_eval_cost (optional): If true, the IWAE (Importance Weighted
+         Autoencoder) log likeihood bound, instead of the VAE version.
+      do_average_batch (optional): average over the batch, useful for getting
+      good IWAE costs, and model outputs for a single data point.
     
-    # A simple generator function
-def baz():
-    for i in range(10):
-        yield i*i
+    from utils import write_datasets
+from synthetic_data_utils import add_alignment_projections, generate_data
+from synthetic_data_utils import generate_rnn, get_train_n_valid_inds
+from synthetic_data_utils import nparray_and_transpose
+from synthetic_data_utils import spikify_data, gaussify_data, split_list_by_inds
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.signal
     
-    *TL;DR80
-Decouples an abstraction from its implementation.
-'''
+      if prefix_words.find('<S>') != 0:
+    prefix_words = '<S> ' + prefix_words
     
-        def test_c_observers_shall_be_detachable(cls):
-        cls.s.detach(cls.dec_obs)
-        # hex viewer shall be remaining if dec viewer is detached first
-        cls.assertEqual(isinstance(cls.s._observers[0], HexViewer), True)
-        cls.assertEqual(len(cls.s._observers), 1)
-        cls.s.detach(cls.hex_obs)
-        cls.assertEqual(len(cls.s._observers), 0)
+        self._id_to_word = []
+    self._word_to_id = {}
+    self._unk = -1
+    self._bos = -1
+    self._eos = -1
     
-        def test_cloning_propperty_assigned_values(self):
-        sample_object_1 = self.prototype.clone()
-        sample_object_2 = self.prototype.clone(value='re-assigned')
-        self.assertNotEqual(sample_object_1.value, sample_object_2.value)
+      # Averages for real and fake token values.
+  real = tf.mul(values, present)
+  fake = tf.mul(values, missing)
+  real_avg = tf.reduce_sum(real) / real_count
+  fake_avg = tf.reduce_sum(fake) / fake_count
     
-        def test_car_adapter_shall_make_very_loud_noise(self):
-        car = Car()
-        car_adapter = Adapter(car, make_noise=car.make_noise)
-        noise = car_adapter.make_noise(10)
-        expected_noise = 'vroom!!!!!!!!!!'
+    # Dependency imports
     
-        # Select the heuristic function for combining the heatmaps
-    if cfg.TEST.KPS_AUG.HEUR == 'HM_AVG':
-        np_f = np.mean
-    elif cfg.TEST.KPS_AUG.HEUR == 'HM_MAX':
-        np_f = np.amax
-    else:
-        raise NotImplementedError(
-            'Heuristic {} not supported'.format(cfg.TEST.KPS_AUG.HEUR)
+          for s in xrange(t, FLAGS.sequence_length):
+        cum_advantage += reward_missing_list[s] * np.power(gamma, (s - t)) * (
+            rewards_list[s] - baselines[s])
+      # Clip advantages.
+      cum_advantage = tf.clip_by_value(cum_advantage, -FLAGS.advantage_clipping,
+                                       FLAGS.advantage_clipping)
+      advantages.append(reward_missing_list[t] * cum_advantage)
+      final_gen_objective += tf.multiply(
+          log_probability,
+          reward_missing_list[t] * tf.stop_gradient(cum_advantage))
+    
+    
+def find_all_ngrams(dataset, n):
+  '''Generate a list of all ngrams.'''
+  return zip(*[dataset[i:] for i in xrange(n)])
+    
+    import optparse
+import os
+from os.path import dirname as dirn
+import sys
+    
+    from youtube_dl.utils import intlist_to_bytes
+from youtube_dl.aes import aes_encrypt, key_expansion
+    
+    filenames = {
+    'bin': 'youtube-dl',
+    'exe': 'youtube-dl.exe',
+    'tar': 'youtube-dl-%s.tar.gz' % version}
+build_dir = os.path.join('..', '..', 'build', version)
+for key, filename in filenames.items():
+    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
+    fn = os.path.join(build_dir, filename)
+    with open(fn, 'rb') as f:
+        data = f.read()
+    if not data:
+        raise ValueError('File %s is empty!' % fn)
+    sha256sum = hashlib.sha256(data).hexdigest()
+    new_version[key] = (url, sha256sum)
+    
+    
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
+    
+    README_FILE = 'README.md'
+helptext = sys.stdin.read()
+    
+        template = template.replace('{{fileopts}}', '|'.join(fileopts))
+    template = template.replace('{{diropts}}', '|'.join(diropts))
+    template = template.replace('{{flags}}', ' '.join(flags))
+    
+    # The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+    
+    
+def _mkdir(d):
+    if not os.path.exists(d):
+        os.mkdir(d)
+    
+        return test_template
+    
+    
+class CaseInsensitiveDict(MutableMapping):
+    '''A case-insensitive ``dict``-like object.
+    
+        get_item_parameters = pytest.mark.parametrize(
+        'key, value', (
+            ('bad_gateway', 502),
+            ('not_a_key', None)
         )
-    
-    
-def basic_bn_shortcut(model, prefix, blob_in, dim_in, dim_out, stride):
-    ''' For a pre-trained network that used BN. An AffineChannel op replaces BN
-    during fine-tuning.
-    '''
-    
-        if upsample_heatmap:
-        # Increase heatmap output size via bilinear upsampling
-        blob_out = model.BilinearInterpolation(
-            blob_out, 'kps_score', cfg.KRCNN.NUM_KEYPOINTS,
-            cfg.KRCNN.NUM_KEYPOINTS, cfg.KRCNN.UP_SCALE
-        )
-    
-    import numpy as np
-    
-        # rois are in [[batch_idx, x0, y0, x1, y2], ...] format
-    # Combine predictions across all levels and retain the top scoring
-    rois = np.concatenate([blob.data for blob in roi_inputs])
-    scores = np.concatenate([blob.data for blob in score_inputs]).squeeze()
-    inds = np.argsort(-scores)[:post_nms_topN]
-    rois = rois[inds, :]
-    return rois
-    
-        # Anchors at a single feature cell
-    cell_anchors = generate_anchors(
-        stride=stride, sizes=anchor_sizes, aspect_ratios=anchor_aspect_ratios
     )
-    num_cell_anchors = cell_anchors.shape[0]
+    
+    
+def test_idna_with_version_attribute(mocker):
+    '''Verify we're actually setting idna version when it should be available.'''
+    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
+    assert info()['idna'] == {'version': '2.6'}
+
+    
+    
+class TestExtractZippedPaths:
+    
+            for field, val in fields:
+            if isinstance(val, basestring) or not hasattr(val, '__iter__'):
+                val = [val]
+            for v in val:
+                if v is not None:
+                    # Don't call str() on bytestrings: in Py3 it all goes wrong.
+                    if not isinstance(v, bytes):
+                        v = str(v)
+    
+        return merge_setting(request_hooks, session_hooks, dict_class)
