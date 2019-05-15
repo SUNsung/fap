43fad8ffdbe0c5180f1ea35b87c7ed02b6798ad8
SUNsung <weatherius@gmail.com>
2019-05-15 08:14:04 +0300

    Rewrite [30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb] File in: 2019-05-15 08:14:03

--- ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
+++ ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
@@ -1,79 +1,55 @@
 
         
-          gem.licenses      = ['MIT']
+            # The path used after confirmation.
+    def after_confirmation_path_for(resource_name, resource)
+      if signed_in?(resource_name)
+        signed_in_root_path(resource)
+      else
+        new_session_path(resource_name)
+      end
+    end
     
-    Given(/^an invalid release named '(.+)'$/) do |filename|
-  run_vagrant_command('mkdir -p #{TestApp.release_path(filename)}')
+          def remember_cookie_values(resource)
+        options = { httponly: true }
+        options.merge!(forget_cookie_values(resource))
+        options.merge!(
+          value: resource.class.serialize_into_cookie(resource),
+          expires: resource.remember_expires_at
+        )
+      end
+    
+        unless env['devise.skip_trackable']
+      warden.session(scope)['last_request_at'] = Time.now.utc.to_i
+    end
+  end
 end
 
     
-        def any?(key)
-      value = fetch(key)
-      if value && value.respond_to?(:any?)
-        begin
-          return value.any?
-        rescue ArgumentError # rubocop:disable Lint/HandleExceptions
-          # Gracefully ignore values whose `any?` method doesn't accept 0 args
-        end
-      end
-    
-          def response
-        return @response if defined? @response
-    
-          ServerKey = Struct.new(:hostname, :port)
-    
           private
     
-            def current
-          @order = find_current_order
-          if @order
-            respond_with(@order, default_template: :show, locals: { root_object: @order })
-          else
-            head :no_content
-          end
+            # This is the method called to provision the system. This method
+        # is expected to do whatever necessary to provision the system (create files,
+        # SSH, etc.)
+        def provision!
         end
     
-      # Used on the blog index to split posts on the <!--more--> marker
-  def excerpt(input)
-    if input.index(/<!--\s*more\s*-->/i)
-      input.split(/<!--\s*more\s*-->/i)[0]
-    else
-      input
+            # An internal finalize call that no subclass should override.
+        def _finalize!
+          @__finalized = true
+        end
+      end
     end
   end
-    
-    # enable logging
-FPM::Util.send :module_function, :logger
-FPM::Util.logger.level = :info
-FPM::Util.logger.subscribe STDERR
-    
-      def to_s(format=nil)
-    return super('NAME_FULLVERSION_ARCH.TYPE') if format.nil?
-    return super(format)
-  end
-    
-      end # def output
-end # class FPM::Package::IPS
+end
 
     
-        if script?(:before_install) or script?(:after_install) or \
-        script?(:before_upgrade) or script?(:after_upgrade) or \
-        script?(:before_remove) or script?(:after_remove)
-      install_script = template('pacman/INSTALL.erb').result(binding)
-      install_script_file = build_path('.INSTALL')
-      File.write(install_script_file, install_script)
+          spec['main'] =
+          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
+          find_files.(Bootstrap.fonts_path) +
+          %w(assets/javascripts/bootstrap.js)
+    
+        def str_to_byte_pos(pos)
+      @s.string.slice(0, pos).bytesize
     end
-    
-        # Generate the package 'Prototype' file
-    File.open('#{build_path}/Prototype', 'w') do |prototype|
-      prototype.puts('i pkginfo')
-      prototype.puts('i preinstall') if self.scripts['pre-install']
-      prototype.puts('i postinstall') if self.scripts['post-install']
-    
-        cleanup_staging
-    # Tell 'dir' to input '.' and chdir/prefix will help it figure out the
-    # rest.
-    dir.input('.')
-    @staging_path = dir.staging_path
-    dir.cleanup_build
-  end # def input
+  end
+end
