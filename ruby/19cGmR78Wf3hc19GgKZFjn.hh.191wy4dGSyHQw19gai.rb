b1b0197457e61ed5a2130505566f97b767de7dc4
SUNsung <weatherius@gmail.com>
2019-05-15 07:16:08 +0300

    Rewrite [19cGmR78Wf3hc19GgKZFjn.hh.191wy4dGSyHQw19gai.rb] File in: 2019-05-15 07:16:07

--- ruby/19cGmR78Wf3hc19GgKZFjn.hh.191wy4dGSyHQw19gai.rb
+++ ruby/19cGmR78Wf3hc19GgKZFjn.hh.191wy4dGSyHQw19gai.rb
@@ -1,41 +1,48 @@
 
         
-              spec['version'] = Bootstrap::VERSION
-    
-      # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found).
-  config.i18n.fallbacks = true
-    
-      def test_font_helper_without_suffix
-    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
+          def report
+    @report ||= JSON.parse(request.body.read)['csp-report'].slice(
+      'blocked-uri',
+      'disposition',
+      'document-uri',
+      'effective-directive',
+      'original-policy',
+      'referrer',
+      'script-sample',
+      'status-code',
+      'violated-directive',
+      'line-number',
+      'source-file'
+    )
   end
     
-          def order_time(time)
-        [I18n.l(time.to_date), time.strftime('%l:%M %p').strip].join(' ')
-      end
+    end
+    
+          @directives[directive] ||= []
+    
+        THEME_SETTING = 'extend_content_security_policy'
+    
+    require 'builder'
+require 'feedbag'
+require 'json'
+require 'nokogiri'
+    
+        w.uid = 'git'
+    w.gid = 'git'
+    
+      def failed_order
+    params[:order] || 'desc'
+  end
+    
+      it 'exits with Resque::TermException when using TERM_CHILD and not forking' do
+    old_job_per_fork = ENV['FORK_PER_JOB']
+    begin
+      ENV['FORK_PER_JOB'] = 'false'
+      worker_pid, child_pid, _result = start_worker(0, true)
+      assert_equal worker_pid, child_pid, 'child_pid should equal worker_pid, since we are not forking'
+      assert Resque.redis.lpop( 'sigterm-test:ensure_block_executed' ), 'post cleanup did not occur. SIGKILL sent too early?'
+    ensure
+      ENV['FORK_PER_JOB'] = old_job_per_fork
     end
   end
 end
-
-    
-          # Order Form
-      expect(page).not_to have_css('.edit-item')
-      # Order Tabs
-      expect(page).not_to have_link('Details')
-      expect(page).not_to have_link('Customer')
-      expect(page).not_to have_link('Adjustments')
-      expect(page).not_to have_link('Payments')
-      expect(page).not_to have_link('Returns')
-    end
-    
-            def product_params
-          params.require(:product).permit(permitted_product_attributes)
-        end
-    
-          @@user_attributes = [:id, :email, :created_at, :updated_at]
-    
-        it 'accepts jsfiddle link with a custom-tab parameter' do
-      expect do
-        generate_new_liquid(jsfiddle_link_with_custom_tabs)
-      end.not_to raise_error
-    end
