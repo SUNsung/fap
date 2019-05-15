d391662088475e5399285da537edbafa7d0f1802
SUNsung <weatherius@gmail.com>
2019-05-15 03:42:35 +0300

    Create [35O3iebbD.zQM35e20C.6N9Cy635295xr2b9A7w35eOf.rb] File in: 2019-05-15 03:42:35

---
+++ ruby/35O3iebbD.zQM35e20C.6N9Cy635295xr2b9A7w35eOf.rb
@@ -0,0 +1,83 @@
+      when IAX_CTRL_PROGRESS
+        dprint('PROGRESS')
+    
+    IAX_IE_CALLED_NUMBER  = 1
+IAX_IE_CALLING_NUMBER = 2
+IAX_IE_AUTH_METHODS   = 3
+IAX_IE_CALLING_NAME   = 4
+IAX_IE_USERNAME       = 6
+IAX_IE_DESIRED_CODEC  = 9
+IAX_IE_ORIGINAL_DID   = 10
+IAX_IE_ACTUAL_CODECS  = 8
+IAX_IE_PROTO_VERSION  = 11
+IAX_IE_REG_REFRESH    = 19
+IAX_IE_CHALLENGE_DATA = 15
+IAX_IE_CHALLENGE_RESP = 16
+IAX_IE_APPARENT_ADDR  = 18
+IAX_IE_REGREJ_CAUSE   = 22
+IAX_IE_HANGUP_CAUSE   = 42
+    
+    
+  #
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
+  }
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a KRB_AP_REQ definition, containing the Kerberos protocol version number,
+        # the message type KRB_AP_REQ, an options field to indicate any options in use, and the ticket and authenticator
+        # themselves
+        class ApReq < Element
+          # @!attribute pvno
+          #   @return [Integer] The protocol version number
+          attr_accessor :pvno
+          # @!attribute msg_type
+          #   @return [Integer] The type of the protocol message
+          attr_accessor :msg_type
+          # @!attribute options
+          #   @return [Integer] request options, affects processing
+          attr_accessor :options
+          # @!attribute ticket
+          #   @return [Rex::Proto::Kerberos::Model::Ticket] The ticket authenticating the client to the server
+          attr_accessor :ticket
+          # @!attribute authenticator
+          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] This contains the authenticator, which includes the
+          #   client's choice of a subkey
+          attr_accessor :authenticator
+    
+              # @!attribute type
+          #   @return [Integer] The algorithm used to generate the checksum
+          attr_accessor :type
+          # @!attribute checksum
+          #   @return [String] The checksum itself
+          attr_accessor :checksum
+    
+              # Decodes the stime field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Time]
+          def decode_stime(input)
+            input.value[0].value
+          end
