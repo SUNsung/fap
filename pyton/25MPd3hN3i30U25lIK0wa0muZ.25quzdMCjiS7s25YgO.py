12f3a073c57cf33dcae25be238d17660187d22a4
SUNsung <weatherius@gmail.com>
2019-05-15 09:29:07 +0300

    Rewrite [25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py] File in: 2019-05-15 09:29:06

--- pyton/25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py
+++ pyton/25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py
@@ -1,233 +1,146 @@
 
         
-        password = key
-new_key = aes_encrypt(password, key_expansion(password))
-r = openssl_encode('aes-128-ctr', new_key, iv)
-print('aes_decrypt_text 16')
-print(repr(r))
+        import sys
+import os
+import textwrap
     
-        with io.open(infile, encoding='utf-8') as inf:
-        readme = inf.read()
-    
-    
-def main():
-    parser = optparse.OptionParser(usage='%prog OUTFILE.md')
-    options, args = parser.parse_args()
-    if len(args) != 1:
-        parser.error('Expected an output filename')
-    
-        with open(ZSH_COMPLETION_TEMPLATE) as f:
-        template = f.read()
-    
-        def test_cbc_decrypt(self):
-        data = bytes_to_intlist(
-            b'\x97\x92+\xe5\x0b\xc3\x18\x91ky9m&\xb3\xb5@\xe6'\xc2\x96.\xc8u\x88\xab9-[\x9e|\xf1\xcd'
-        )
-        decrypted = intlist_to_bytes(aes_cbc_decrypt(data, self.key, self.iv))
-        self.assertEqual(decrypted.rstrip(b'\x08'), self.secret_msg)
-    
-    tensorboard = TensorBoard(batch_size=batch_size,
-                          embeddings_freq=1,
-                          embeddings_layer_names=['features'],
-                          embeddings_metadata='metadata.tsv',
-                          embeddings_data=x_test)
-    
-        if K.image_data_format() == 'channels_last':
-        x_train = x_train.transpose(0, 2, 3, 1)
-        x_test = x_test.transpose(0, 2, 3, 1)
+        def test_youtube_subtitles_ttml_format(self):
+        self.DL.params['writesubtitles'] = True
+        self.DL.params['subtitlesformat'] = 'ttml'
+        subtitles = self.getSubtitles()
+        self.assertEqual(md5(subtitles['en']), 'e306f8c42842f723447d9f63ad65df54')
     
     
-def l1_l2(l1=0.01, l2=0.01):
-    return L1L2(l1=l1, l2=l2)
+@pytest.mark.parametrize(
+    ('username', 'password', 'message'),
+    (('a', 'test', b'Incorrect username.'), ('test', 'a', b'Incorrect password.')),
+)
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
     
-        # Test that writing over the input data works predictably
-    for mode in ['torch', 'tf']:
-        x = np.random.uniform(0, 255, (2, 10, 10, 3))
-        xint = x.astype('int')
-        x2 = utils.preprocess_input(x, mode=mode)
-        xint2 = utils.preprocess_input(xint)
-        assert_allclose(x, x2)
-        assert xint.astype('float').max() != xint2.max()
-    # Caffe mode works differently from the others
-    x = np.random.uniform(0, 255, (2, 10, 10, 3))
-    xint = x.astype('int')
-    x2 = utils.preprocess_input(x, data_format='channels_last', mode='caffe')
-    xint2 = utils.preprocess_input(xint)
-    assert_allclose(x, x2[..., ::-1])
-    assert xint.astype('float').max() != xint2.max()
-    
-        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
-        deserialized = losses.deserialize(serialized)
-    assert isinstance(deserialized, MSE_MAE_loss)
-    assert deserialized.mse_fraction == 0.3
+        def main(self, *args, **kwargs):
+        # Set a global flag that indicates that we were invoked from the
+        # command line interface. This is detected by Flask.run to make the
+        # call into a no-op. This is necessary to avoid ugly errors when the
+        # script that is loaded here also attempts to start a server.
+        os.environ['FLASK_RUN_FROM_CLI'] = 'true'
     
     
-def test_regularization_shared_layer():
-    dense_layer = Dense(num_classes,
-                        kernel_regularizer=regularizers.l1(),
-                        activity_regularizer=regularizers.l1())
-    
-    model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
-    
-    # Build the Decoder Model
-latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
-x = Dense(shape[1] * shape[2] * shape[3])(latent_inputs)
-x = Reshape((shape[1], shape[2], shape[3]))(x)
-    
-    
-def _mkanchors(ws, hs, x_ctr, y_ctr):
-    '''Given a vector of widths (ws) and heights (hs) around a center
-    (x_ctr, y_ctr), output a set of anchors (windows).
+def attach_enctype_error_multidict(request):
+    '''Since Flask 0.8 we're monkeypatching the files object in case a
+    request is detected that does not use multipart form data but the files
+    object is accessed.
     '''
-    ws = ws[:, np.newaxis]
-    hs = hs[:, np.newaxis]
-    anchors = np.hstack(
-        (
-            x_ctr - 0.5 * (ws - 1),
-            y_ctr - 0.5 * (hs - 1),
-            x_ctr + 0.5 * (ws - 1),
-            y_ctr + 0.5 * (hs - 1)
-        )
-    )
-    return anchors
-    
-        return blob_out
-    
-    def add_generic_rpn_outputs(model, blob_in, dim_in, spatial_scale_in):
-    '''Add RPN outputs (objectness classification and bounding box regression)
-    to an RPN model. Abstracts away the use of FPN.
-    '''
-    loss_gradients = None
-    if cfg.FPN.FPN_ON:
-        # Delegate to the FPN module
-        FPN.add_fpn_rpn_outputs(model, blob_in, dim_in, spatial_scale_in)
-        if cfg.MODEL.FASTER_RCNN:
-            # CollectAndDistributeFpnRpnProposals also labels proposals when in
-            # training mode
-            model.CollectAndDistributeFpnRpnProposals()
-        if model.train:
-            loss_gradients = FPN.add_fpn_rpn_losses(model)
-    else:
-        # Not using FPN, add RPN to a single scale
-        add_single_scale_rpn_outputs(model, blob_in, dim_in, spatial_scale_in)
-        if model.train:
-            loss_gradients = add_single_scale_rpn_losses(model)
-    return loss_gradients
-    
-    logger = logging.getLogger(__name__)
-    
-        kp_fg_rois_per_this_image = np.minimum(fg_rois_per_image, kp_fg_inds.size)
-    if kp_fg_inds.size > kp_fg_rois_per_this_image:
-        kp_fg_inds = np.random.choice(
-            kp_fg_inds, size=kp_fg_rois_per_this_image, replace=False
-        )
-    
-            print('Test: Pop on no elements')
-        assert_equal(stacks.pop(), None)
-    
-            print('Success: test_sort_stack')
-    
-    		print('Test: min node')
-		assert_equal(myTree.minNode(), 1)
+    oldcls = request.files.__class__
     
     
-def get_service_dependents(service_dict, services):
-    name = service_dict['name']
-    return [
-        service for service in services
-        if (name in get_service_names(service.get('links', [])) or
-            name in get_service_names_from_volumes_from(service.get('volumes_from', [])) or
-            name == get_service_name_from_network_mode(service.get('network_mode')) or
-            name == get_service_name_from_network_mode(service.get('pid')) or
-            name in service.get('depends_on', []))
-    ]
+init_bashrc = u'''echo '
+export SHELL=/bin/bash
+export PS1='$ '
+echo > $HISTFILE
+eval $(thefuck --alias {})
+echo 'instant mode ready: $THEFUCK_INSTANT_MODE'
+' > ~/.bashrc'''
     
     
-def warn_for_external_links(name, service):
-    external_links = service.get('external_links')
-    if external_links:
-        log.warn(
-            'Service {name} has external_links: {ext}, which now work '
-            'slightly differently. In particular, two containers must be '
-            'connected to at least one network in common in order to '
-            'communicate, even if explicitly linked together.\n\n'
-            'Either connect the external container to your app's default '
-            'network, or connect both the external container and your '
-            'service's containers to a pre-existing network. See '
-            'https://docs.docker.com/compose/networking/ '
-            'for more on how to do this.'
-            .format(name=name, ext=external_links))
+@pytest.mark.parametrize('command, packages, which', [
+    (Command('a_bad_cmd', 'a_bad_cmd: command not found'),
+     [], None),
+    (Command('vim', ''), [], None),
+    (Command('', ''), [], None),
+    (Command('vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim'),
+    (Command('sudo vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim')])
+def test_not_match(mocker, command, packages, which):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=which)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
     
-        if answer == 'y' or answer == 'yes':
-        return True
-    elif answer == 'n' or answer == 'no':
-        return False
-    elif answer == '':
-        return default
-    else:
-        return None
+    
+@pytest.mark.parametrize('script, output', [
+    ('apt', invalid_operation('saerch')),
+    ('apt-get', invalid_operation('isntall')),
+    ('apt-cache', invalid_operation('rumove'))])
+def test_match(script, output):
+    assert match(Command(script, output))
+    
+        new_command = get_new_command(Command('sudo apt list --upgradable', match_output))
+    assert new_command == 'sudo apt upgrade'
 
     
-        return create_custom_host_file(client, filename, content)
+    
+Invalid choice: 'dynamdb', maybe you meant:
+    
+    
+@pytest.mark.skipif(_is_not_okay_to_test(),
+                    reason='No need to run if there\'s no formula')
+def test_get_new_command(brew_no_available_formula):
+    assert get_new_command(Command('brew install elsticsearch',
+                                   brew_no_available_formula))\
+        == 'brew install elasticsearch'
+    
+        fpath = os.path.join(path, 'train')
+    x_train, y_train = load_batch(fpath, label_key=label_mode + '_labels')
+    
+        out1 = utils.preprocess_input(x, 'channels_last')
+    out1int = utils.preprocess_input(xint, 'channels_last')
+    out2 = utils.preprocess_input(np.transpose(x, (0, 3, 1, 2)),
+                                  'channels_first')
+    out2int = utils.preprocess_input(np.transpose(xint, (0, 3, 1, 2)),
+                                     'channels_first')
+    assert_allclose(out1, out2.transpose(0, 2, 3, 1))
+    assert_allclose(out1int, out2int.transpose(0, 2, 3, 1))
+    
+        y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+    
+        # Input shape
+        5D tensor with shape:
+        `(samples, timesteps, channels, rows, cols)` if data_format='channels_first'
+        or 5D tensor with shape:
+        `(samples, timesteps, rows, cols, channels)` if data_format='channels_last'.
+    
+    # Generate corrupted MNIST images by adding noise with normal dist
+# centered at 0.5 and std=0.5
+noise = np.random.normal(loc=0.5, scale=0.5, size=x_train.shape)
+x_train_noisy = x_train + noise
+noise = np.random.normal(loc=0.5, scale=0.5, size=x_test.shape)
+x_test_noisy = x_test + noise
+    
+    history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
 
     
-        def test_docker_client_no_home(self):
-        with mock.patch.dict(os.environ):
-            try:
-                del os.environ['HOME']
-            except KeyError:
-                pass
-            docker_client(os.environ)
-    
-        def test_format_unicode_info(self):
-        message = b'\xec\xa0\x95\xec\x88\x98\xec\xa0\x95'
-        output = self.formatter.format(make_log_record(logging.INFO, message))
-        assert output == message.decode('utf-8')
-    
-    from compose.cli import verbose_proxy
-from tests import unittest
+    if K.image_data_format() == 'channels_first':
+    input_shape = (1, img_rows, img_cols)
+else:
+    input_shape = (img_rows, img_cols, 1)
     
     
-class ServerPlayback:
-    def __init__(self):
-        self.flowmap = {}
-        self.configured = False
-    
-            self.id = str(uuid.uuid4())
-        self.mitmcert = None
-        self.timestamp_start = time.time()
-        self.timestamp_end = None
-        self.timestamp_tls_setup = None
-        self.sni = None
-        self.cipher_name = None
-        self.alpn_proto_negotiated = None
-        self.tls_version = None
-        self.tls_extensions = None
-    
-    
-class Reply:
-    '''
-    Messages sent through a channel are decorated with a 'reply' attribute. This
-    object is used to respond to the message through the return channel.
-    '''
-    def __init__(self, obj):
-        self.obj = obj
-        # Spawn an event loop in the current thread
-        self.q = queue.Queue()
-    
-        def __init__(self, **kwargs):
-        self.names = kwargs
-        self.values = {}
-        for k, v in kwargs.items():
-            self.values[v] = k
-        if len(self.names) != len(self.values):
-            raise ValueError('Duplicate values not allowed.')
-    
-    from mitmproxy import contentviews
-from mitmproxy import ctx
-from mitmproxy import flowfilter
-from mitmproxy import io, flow
-from mitmproxy import version
-from mitmproxy.tools.web.app import flow_to_json
+def test_conv_input_length():
+    assert conv_utils.conv_input_length(None, 7, 'same', 1) is None
+    assert conv_utils.conv_input_length(112, 7, 'same', 1) == 112
+    assert conv_utils.conv_input_length(112, 7, 'same', 2) == 223
+    assert conv_utils.conv_input_length(28, 5, 'valid', 1) == 32
+    assert conv_utils.conv_input_length(14, 5, 'valid', 2) == 31
+    assert conv_utils.conv_input_length(36, 5, 'full', 1) == 32
+    assert conv_utils.conv_input_length(18, 5, 'full', 2) == 31
