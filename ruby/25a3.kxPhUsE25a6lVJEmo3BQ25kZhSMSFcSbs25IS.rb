4caac509a7f860d4baad787308e3da7fa873c9be
SUNsung <weatherius@gmail.com>
2019-05-15 16:01:32 +0300

    Rewrite [25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb] File in: 2019-05-15 16:01:32

--- ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
+++ ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
@@ -1,70 +1,11 @@
 
         
-            # Delimit on each variable
-    str.split(/[;&]/).each { |vv|
-      var = vv
-      val = ''
-    }
+                attr_reader :attributes
     
-                if OpenSSL::HMAC.digest('MD5', k1, decrypted) != checksum
-              raise ::RuntimeError, 'RC4-HMAC decryption failed, incorrect checksum verification'
-            end
-    
-              # Decodes the flags field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_flags(input)
-            input.value[0].value.to_i
-          end
-    
-                res
-          end
-    
-      if config.log_to.include? 'file'
-    # Configure an appender that will write log events to a file.
-    if AppConfig.environment.logging.logrotate.enable?
-      # The file will be rolled on a daily basis, and the rolled files will be kept
-      # the configured number of days. Older files will be deleted. The default pattern
-      # layout is used when formatting log events into strings.
-      Logging.appenders.rolling_file('file',
-                                     filename:      config.paths['log'].first,
-                                     keep:          AppConfig.environment.logging.logrotate.days.to_i,
-                                     age:           'daily',
-                                     truncate:      false,
-                                     auto_flushing: true,
-                                     layout:        layout
-                                    )
-    else
-      # No file rolling, use logrotate to roll the logfile.
-      Logging.appenders.file('file',
-                             filename:      config.paths['log'].first,
-                             truncate:      false,
-                             auto_flushing: true,
-                             layout:        layout
-                            )
-    end
-  end
-    
-        remove_duplicates
-    remove_index :share_visibilities, name: :shareable_and_user_id
-    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
-    
-    Then /^I should have (\d+) nsfw posts$/ do |num_posts|
-  page.should have_css('.nsfw-shield', count: num_posts.to_i)
-end
-    
-        it 'only lets you read your own notifications' do
-      user2 = bob
-    
-        it 'does redirect if there is an invalid invite token' do
-      get :new, params: {invite: {token: 'fssdfsd'}}
-      expect(response).to redirect_to new_user_session_path
-    end
-    
-        def initialize(processed_source)
-      @processed_source = processed_source
-    end
-    
-        def for(file_or_dir)
-      return @options_config if @options_config
+            if obj && obj.errors[method].present?
+          errors = safe_join(obj.errors[method], '<br />'.html_safe)
+          content_tag(:span, errors, class: 'formError')
+        else
+          ''
+        end
+      end
