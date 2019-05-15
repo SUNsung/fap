6a35b14d93e9f94ee45fe2b8c00e90ec00d3bdc5
SUNsung <weatherius@gmail.com>
2019-05-15 13:00:27 +0300

    Rewrite [11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb] File in: 2019-05-15 13:00:26

--- ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
+++ ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
@@ -1,181 +1,169 @@
 
         
-        module Docs
-  class Filter < ::HTML::Pipeline::Filter
-    def css(*args)
-      doc.css(*args)
+          it 'creates an alert if a new agent with invalid json is submitted' do
+    visit '/'
+    page.find('a', text: 'Agents').trigger(:mouseover)
+    click_on('New Agent')
+    
+        it 'defauls foreground and background colors' do
+      scenario.tag_fg_color = nil
+      scenario.tag_bg_color = nil
+      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
     end
-    
-        def request_one(url)
-      Request.run url, request_options
-    end
-    
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
-    
-      # Returns real navigational formats which are supported by Rails
-  def navigational_formats
-    @navigational_formats ||= Devise.navigational_formats.select { |format| Mime::EXTENSION_LOOKUP[format.to_s] }
   end
     
-        def password_change(record, opts={})
-      devise_mail(record, :password_change, opts)
+        after(:each) do
+      @agent_runner.stop
+      AgentRunner.class_variable_set(:@@agents, [])
+    end
+    
+        it 'outputs a structure containing name, description, the date, all agents & their links' do
+      data = exporter.as_json
+      expect(data[:name]).to eq(name)
+      expect(data[:description]).to eq(description)
+      expect(data[:source_url]).to eq(source_url)
+      expect(data[:guid]).to eq(guid)
+      expect(data[:schema_version]).to eq(1)
+      expect(data[:tag_fg_color]).to eq(tag_fg_color)
+      expect(data[:tag_bg_color]).to eq(tag_bg_color)
+      expect(data[:icon]).to eq(icon)
+      expect(Time.parse(data[:exported_at])).to be_within(2).of(Time.now.utc)
+      expect(data[:links]).to eq([{ :source => guid_order(agent_list, :jane_weather_agent), :receiver => guid_order(agent_list, :jane_rain_notifier_agent)}])
+      expect(data[:control_links]).to eq([])
+      expect(data[:agents]).to eq(agent_list.sort_by{|a| a.guid}.map { |agent| exporter.agent_as_json(agent) })
+      expect(data[:agents].all? { |agent_json| agent_json[:guid].present? && agent_json[:type].present? && agent_json[:name].present? }).to be_truthy
+    
+        stub.any_instance_of(Agents::SchedulerAgent).second_precision_enabled { true }
+    
+        it 'requires an agent' do
+      @log.agent = nil
+      expect(@log).not_to be_valid
+      expect(@log).to have(1).error_on(:agent)
+    end
+    
+          max_length = if tag = str.slice!(/ \[.+\]\z/)
+        terminal_width - tag.length
+      else
+        terminal_width
+      end
+    
+            css('pre[name]').each do |node|
+          node.before(%(<div class='pre-title'>#{node['name']}</div>))
+        end
+    
+      # Check if there is no signed in user before doing the sign out.
+  #
+  # If there is no signed in user, it will set the flash message and redirect
+  # to the after_sign_out path.
+  def verify_signed_out_user
+    if all_signed_out?
+      set_flash_message! :notice, :already_signed_out
+    
+      # Attempt to find the mapped route for devise based on request path
+  def devise_mapping
+    @devise_mapping ||= request.env['devise.mapping']
+  end
+    
+      def app
+    Rails.application
+  end
+end
+
+    
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
+        end
+    
+    module Devise
+  module Models
+    # Rememberable manages generating and clearing token for remembering the user
+    # from a saved cookie. Rememberable also has utility methods for dealing
+    # with serializing the user into the cookie and back from the cookie, trying
+    # to lookup the record based on the saved information.
+    # You probably wouldn't use rememberable methods directly, they are used
+    # mostly internally for handling the remember token.
+    #
+    # == Options
+    #
+    # Rememberable adds the following options in devise_for:
+    #
+    #   * +remember_for+: the time you want the user will be remembered without
+    #     asking for credentials. After this time the user will be blocked and
+    #     will have to enter their credentials again. This configuration is also
+    #     used to calculate the expires time for the cookie created to remember
+    #     the user. By default remember_for is 2.weeks.
+    #
+    #   * +extend_remember_period+: if true, extends the user's remember period
+    #     when remembered via cookie. False by default.
+    #
+    #   * +rememberable_options+: configuration options passed to the created cookie.
+    #
+    # == Examples
+    #
+    #   User.find(1).remember_me!  # regenerating the token
+    #   User.find(1).forget_me!    # clearing the token
+    #
+    #   # generating info to put into cookies
+    #   User.serialize_into_cookie(user)
+    #
+    #   # lookup the user based on the incoming cookie information
+    #   User.serialize_from_cookie(cookie_string)
+    module Rememberable
+      extend ActiveSupport::Concern
+    
+        def resource_params
+      params.require(:user).permit(
+        :unconfirmed_email
+      )
     end
   end
 end
 
     
-    module Devise
-  module Controllers
-    # A module that may be optionally included in a controller in order
-    # to provide remember me behavior. Useful when signing in is done
-    # through a callback, like in OmniAuth.
-    module Rememberable
-      # Return default cookie values retrieved from session options.
-      def self.cookie_values
-        Rails.configuration.session_options.slice(:path, :domain, :secure)
+        private
+    
+      def maxwidth_or_default
+    (params[:maxwidth].presence || 400).to_i
+  end
+    
+        head 200
+  end
+    
+        render json: @web_subscription, serializer: REST::WebPushSubscriptionSerializer
+  end
+    
+      caveats <<~EOS
+    Installation or Uninstallation may fail with Exit Code 19 (Conflicting Processes running) if Browsers, Safari Notification Service or SIMBL Services (e.g. Flashlight) are running or Adobe Creative Cloud or any other Adobe Products are already installed. See Logs in /Library/Logs/Adobe/Installers if Installation or Uninstallation fails, to identifify the conflicting processes.
+  EOS
+end
+
+    
+        # regression tests for https://github.com/spree/spree/issues/6888
+    context 'per page dropdown', js: true do
+      before do
+        select '45', from: 'per_page'
+        wait_for_ajax
+        expect(page).to have_select('per_page', selected: '45')
+        expect(page).to have_selector(:css, 'select.per-page-selected-45')
       end
     
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
-    
-    module Devise
-  module Mailers
-    module Helpers
-      extend ActiveSupport::Concern
-    
-          # Update password saving the record and clearing token. Returns true if
-      # the passwords are valid and the record was saved, false otherwise.
-      def reset_password(new_password, new_password_confirmation)
-        if new_password.present?
-          self.password = new_password
-          self.password_confirmation = new_password_confirmation
-          save
-        else
-          errors.add(:password, :blank)
-          false
-        end
-      end
-    
-          # Checks whether the user session has expired based on configured time.
-      def timedout?(last_access)
-        !timeout_in.nil? && last_access && last_access <= timeout_in.ago
-      end
-    
-            # This is the method called to provision the system. This method
-        # is expected to do whatever necessary to provision the system (create files,
-        # SSH, etc.)
-        def provision!
+            def advance
+          authorize! :update, @order, order_token
+          while @order.next; end
+          respond_with(@order, default_template: 'spree/api/v1/orders/show', status: 200)
         end
     
-              # Setup the options hash
-          options ||= {}
-    
-            # Returns the {Components} for this plugin.
-        #
-        # @return [Components]
-        def self.components
-          @components ||= Components.new
+            def line_items_attributes
+          { line_items_attributes: {
+            id: params[:id],
+            quantity: params[:line_item][:quantity],
+            options: line_item_params[:options] || {}
+          } }
         end
     
-            # This is called early, before a machine is instantiated, to check
-        # if this provider is installed. This should return true or false.
-        #
-        # If the provider is not installed and Vagrant determines it is
-        # able to install this provider, then it will do so. Installation
-        # is done by calling Environment.install_provider.
-        #
-        # If Environment.can_install_provider? returns false, then an error
-        # will be shown to the user.
-        def self.installed?
-          # By default return true for backwards compat so all providers
-          # continue to work.
-          true
-        end
+              def add_store_credit
+            spree_authorize! :update, spree_current_order, order_token
     
-        # Get a value by the given key.
-    #
-    # This will evaluate the block given to `register` and return the
-    # resulting value.
-    def get(key)
-      return nil if !@items.key?(key)
-      return @results_cache[key] if @results_cache.key?(key)
-      @results_cache[key] = @items[key].call
-    end
-    alias :[] :get
-    
-        pod 'ObjCPod', path: 'ObjCPod'
-    pod 'SwiftPod', path: 'SwiftPod'
-    pod 'MixedPod', path: 'MixedPod'
-    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
-    
-          def executable_path
-        <<-EOS
-### Installation Source
-    
-          # renders hidden field and link to remove record using nested_attributes
-      def link_to_icon_remove_fields(form)
-        url = form.object.persisted? ? [:admin, form.object] : '#'
-        link_to_with_icon('delete', '', url,
-                          class: 'spree_remove_fields btn btn-sm btn-danger',
-                          data: {
-                            action: 'remove'
-                          },
-                          title: Spree.t(:remove),
-                          no_text: true
-                         ) + form.hidden_field(:_destroy)
-      end
-    
-          # Insure checkbox still checked
-      expect(find('#q_considered_risky_eq')).to be_checked
-      # Insure we have the risky order, R100
-      within_row(1) do
-        expect(page).to have_content('R100')
-      end
-      # Insure the non risky order is not present
-      expect(page).not_to have_content('R200')
-    end
-    
-                expect(order.shipments.count).to eq(1)
-            expect(order.shipments.first.inventory_units_for(product.master).sum(&:quantity)).to eq(2)
-            expect(order.shipments.first.stock_location.id).to eq(stock_location.id)
-          end
-    
-              def render_order(result)
-            if result.success?
-              render_serialized_payload { serialized_current_order }
-            else
-              render_error_payload(result.error)
-            end
-          end
-    
-                respond_with(@order, default_template: :show, status: 201)
-          else
-            @order = Spree::Order.create!(user: current_api_user, store: current_store)
-            if Cart::Update.call(order: @order, params: order_params).success?
-              respond_with(@order, default_template: :show, status: 201)
-            else
-              invalid_resource!(@order)
-            end
-          end
-        end
-    
-              @stock_item = scope.new(stock_item_params)
-          if @stock_item.save
-            @stock_item.adjust_count_on_hand(count_on_hand)
-            respond_with(@stock_item, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_item)
-          end
-        end
+          mattr_reader *ATTRIBUTES
