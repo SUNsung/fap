aa6df7335d1996595e701003536baa4f1b05fc6a
SUNsung <weatherius@gmail.com>
2019-05-15 17:06:50 +0300

    Rewrite [12zY4vaf7VeGs12WFUH.tFk8ts12Sst0hENYpoY124Pb.cpp] File in: 2019-05-15 17:06:49

--- cccccc/12zY4vaf7VeGs12WFUH.tFk8ts12Sst0hENYpoY124Pb.cpp
+++ cccccc/12zY4vaf7VeGs12WFUH.tFk8ts12Sst0hENYpoY124Pb.cpp
@@ -1,331 +1,173 @@
 
         
-          for (const auto& node : item_.MainOpsFanin()) {
-    PrintNodeInfo(node, properties, debug, os);
-  }
-  for (const auto& node : item_.EnqueueOpsFanin()) {
-    PrintNodeInfo(node, properties, debug, os);
-  }
-    
-    
-    {}  // namespace tensorflow
-
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #endif  // TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
-
-    
-      // Creates a 3d FFT plan.
-  virtual std::unique_ptr<Plan> Create3dPlan(Stream *stream, uint64 num_x,
-                                             uint64 num_y, uint64 num_z,
-                                             Type type, bool in_place_fft) = 0;
-    
-    void CensusClientCallData::OnDoneRecvTrailingMetadataCb(void* user_data,
-                                                        grpc_error* error) {
-  grpc_call_element* elem = reinterpret_cast<grpc_call_element*>(user_data);
-  CensusClientCallData* calld =
-      reinterpret_cast<CensusClientCallData*>(elem->call_data);
-  GPR_ASSERT(calld != nullptr);
-  if (error == GRPC_ERROR_NONE) {
-    GPR_ASSERT(calld->recv_trailing_metadata_ != nullptr);
-    FilterTrailingMetadata(calld->recv_trailing_metadata_,
-                           &calld->elapsed_time_);
-  }
-  GRPC_CLOSURE_RUN(calld->initial_on_done_recv_trailing_metadata_,
-                   GRPC_ERROR_REF(error));
-}
-    
-    
-    {}  // namespace grpc
-    
-    MeasureDouble RpcServerServerLatency() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcServerServerLatencyMeasureName,
-      'Time between first byte of request received to last byte of response '
-      'sent, or terminal error',
-      kUnitMilliseconds);
-  return measure;
-}
-    
-    #include 'src/cpp/ext/filters/census/rpc_encoding.h'
-    
-    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
-    const grpc::string& name, const grpc::string& value) {
-  class StringOption final : public ServerBuilderOption {
-   public:
-    StringOption(const grpc::string& name, const grpc::string& value)
-        : name_(name), value_(value) {}
-    }
+        namespace {
     }
     
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  host_cpu_load_info_data_t cpuinfo;
-  mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
-  if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO,
-                      (host_info_t)&cpuinfo, &count) == KERN_SUCCESS) {
-    for (int i = 0; i < CPU_STATE_MAX; i++) total += cpuinfo.cpu_ticks[i];
-    busy = total - cpuinfo.cpu_ticks[CPU_STATE_IDLE];
-  }
-  return std::make_pair(busy, total);
-}
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
     
-    #endif  // GPR_WINDOWS
+    AtomQuotaPermissionContext::~AtomQuotaPermissionContext() {}
+    
+    
+    {}  // namespace auto_updater
 
     
-    // Given a map from type K to a set of value type V, removes the given key and
-// the associated set, and returns the set. Returns an empty set if the key is
-// not found.
-template <typename K, typename V>
-std::set<V> UnorderedMapOfSetExtract(std::unordered_map<K, std::set<V>>& map,
-                                     const K& key) {
-  auto it = map.find(key);
-  if (it != map.end()) {
-    auto set = std::move(it->second);
-    map.erase(it);
-    return set;
-  }
-  return {};
-};
-    
-    ThreadPoolInterface* CreateDefaultThreadPool();
-    
-    #include <grpc/support/log.h>
-    
-    #include <dmlc/registry.h>
-#include <xgboost/base.h>
-#include <xgboost/data.h>
-#include <functional>
-#include <string>
-#include <utility>
-#include <vector>
-#include '../../src/gbm/gblinear_model.h'
-#include '../../src/common/host_device_vector.h'
-    
-    
-    {
-    {
-    {  inline void PutChar(char ch) {
-    out_buf += ch;
-    if (out_buf.length() >= kBufferSize) Flush();
-  }
-  inline void Flush(void) {
-    if (out_buf.length() != 0) {
-      fp->Write(&out_buf[0], out_buf.length());
-      out_buf.clear();
+    namespace {
     }
+    
+    workspace.RunOperatorOnce(op)
+print('indices: \n', workspace.FetchBlob('indices'))
+    
+    op = core.CreateOperator(
+    'Floor',
+    ['X'],
+    ['X'],
+)
+    
+    REGISTER_CPU_OPERATOR(GatherRangesToDense, GatherRangesToDenseOp<CPUContext>);
+NO_GRADIENT(GatherRangesToDense);
+    
+    OPERATOR_SCHEMA(GivenTensorBoolFill)
+    .NumInputs(0, 1)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}})
+    .Arg(
+        'values',
+        'The value for the elements of the output tensor.',
+        true /* required */)
+    .Arg(
+        'shape',
+        'The shape of the output tensor.'
+        'Cannot set the shape argument and pass in an input at the same time.')
+    .Arg(
+        'extra_shape',
+        'The additional dimensions appended at the end of the shape indicated'
+        'by the input blob.'
+        'Cannot set the extra_shape argument when there is no input blob.')
+    .Arg(
+        'input_as_shape',
+        '1D tensor containing the desired output shape. First input must be in CPU context.')
+    .TensorInferenceFunction(FillerTensorInference<TensorProto_DataType_BOOL>);
+    
+    class GetCol2ImGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'Im2Col', '', std::vector<string>{GO(0)}, std::vector<string>{GI(0)});
   }
 };
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_BASE64_H_
-
+REGISTER_GRADIENT(Col2Im, GetCol2ImGradient);
     
-    // logistic loss, but predict un-transformed margin
-struct LogisticRaw : public LogisticRegression {
-  // duplication is necessary, as __device__ specifier
-  // cannot be made conditional on template parameter
-  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return x; }
-  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
-    predt = common::Sigmoid(predt);
-    return predt - label;
-  }
-  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
-    const float eps = 1e-16f;
-    predt = common::Sigmoid(predt);
-    return fmaxf(predt * (1.0f - predt), eps);
-  }
-  template <typename T>
-    static T PredTransform(T x) { return x; }
-  template <typename T>
-    static T FirstOrderGradient(T predt, T label) {
-    predt = common::Sigmoid(predt);
-    return predt - label;
-  }
-  template <typename T>
-    static T SecondOrderGradient(T predt, T label) {
-    const T eps = T(1e-16f);
-    predt = common::Sigmoid(predt);
-    return std::max(predt * (T(1.0f) - predt), eps);
-  }
-  static const char* DefaultEvalMetric() { return 'auc'; }
+    
+    {  // Set the maximum number of read-only files that will be mapped via mmap.
+  // Must be called before creating an Env.
+  static void SetReadOnlyMMapLimit(int limit);
 };
     
-      for (auto alphabet_size : test_cases) {
-    for (int i = 0; i < repetitions; i++) {
-      std::vector<int> input(num_elements);
-      std::generate(input.begin(), input.end(),
-        [=]() { return rand() % alphabet_size; });
-      CompressedBufferWriter cbw(alphabet_size);
-    }
-    }
+      uint32_t target_size = 10000;
+  uint32_t num_items = 0;
+  uint32_t count = 0;
+  std::string key;
+  std::string value, old_value;
     
-    /*! \brief interface of objective function */
-class ObjFunction {
+    // Snapshots are kept in a doubly-linked list in the DB.
+// Each SnapshotImpl corresponds to a particular sequence number.
+class SnapshotImpl : public Snapshot {
  public:
-  /*! \brief virtual destructor */
-  virtual ~ObjFunction() = default;
-  /*!
-   * \brief set configuration from pair iterators.
-   * \param begin The beginning iterator.
-   * \param end The end iterator.
-   * \tparam PairIter iterator<std::pair<std::string, std::string> >
-   */
-  template<typename PairIter>
-  inline void Configure(PairIter begin, PairIter end);
-  /*!
-   * \brief Configure the objective with the specified parameters.
-   * \param args arguments to the objective function.
-   */
-  virtual void Configure(const std::vector<std::pair<std::string, std::string> >& args) = 0;
-  /*!
-   * \brief Get gradient over each of predictions, given existing information.
-   * \param preds prediction of current round
-   * \param info information about labels, weights, groups in rank
-   * \param iteration current iteration number.
-   * \param out_gpair output of get gradient, saves gradient and second order gradient in
-   */
-  virtual void GetGradient(const HostDeviceVector<bst_float>& preds,
-                           const MetaInfo& info,
-                           int iteration,
-                           HostDeviceVector<GradientPair>* out_gpair) = 0;
+  SnapshotImpl(SequenceNumber sequence_number)
+      : sequence_number_(sequence_number) {}
     }
     
-      void PredictContribution(DMatrix* p_fmat, std::vector<bst_float>* out_contribs,
-                           const gbm::GBTreeModel& model, unsigned ntree_limit,
-                           bool approximate,
-                           int condition,
-                           unsigned condition_feature) override {
-    const int nthread = omp_get_max_threads();
-    InitThreadTemp(nthread,  model.param.num_feature);
-    const MetaInfo& info = p_fmat->Info();
-    // number of valid trees
-    ntree_limit *= model.param.num_output_group;
-    if (ntree_limit == 0 || ntree_limit > model.trees.size()) {
-      ntree_limit = static_cast<unsigned>(model.trees.size());
+    struct TableAndFile {
+  RandomAccessFile* file;
+  Table* table;
+};
+    
+    TEST(VersionEditTest, EncodeDecode) {
+  static const uint64_t kBig = 1ull << 50;
     }
-    const int ngroup = model.param.num_output_group;
-    size_t ncolumns = model.param.num_feature + 1;
-    // allocate space for (number of features + bias) times the number of rows
-    std::vector<bst_float>& contribs = *out_contribs;
-    contribs.resize(info.num_row_ * ncolumns * model.param.num_output_group);
-    // make sure contributions is zeroed, we could be reusing a previously
-    // allocated one
-    std::fill(contribs.begin(), contribs.end(), 0);
-    // initialize tree node mean values
-    #pragma omp parallel for schedule(static)
-    for (bst_omp_uint i = 0; i < ntree_limit; ++i) {
-      model.trees[i]->FillNodeMeanValues();
+    
+    // A file abstraction for sequential writing.  The implementation
+// must provide buffering since callers may append small fragments
+// at a time to the file.
+class LEVELDB_EXPORT WritableFile {
+ public:
+  WritableFile() = default;
     }
-    const std::vector<bst_float>& base_margin = info.base_margin_.HostVector();
-    // start collecting the contributions
-    for (const auto &batch : p_fmat->GetRowBatches()) {
-      // parallel over local batch
-      const auto nsize = static_cast<bst_omp_uint>(batch.Size());
-#pragma omp parallel for schedule(static)
-      for (bst_omp_uint i = 0; i < nsize; ++i) {
-        auto row_idx = static_cast<size_t>(batch.base_rowid + i);
-        unsigned root_id = info.GetRoot(row_idx);
-        RegTree::FVec& feats = thread_temp[omp_get_thread_num()];
-        // loop over all classes
-        for (int gid = 0; gid < ngroup; ++gid) {
-          bst_float* p_contribs =
-              &contribs[(row_idx * ngroup + gid) * ncolumns];
-          feats.Fill(batch[i]);
-          // calculate contributions
-          for (unsigned j = 0; j < ntree_limit; ++j) {
-            if (model.tree_info[j] != gid) {
-              continue;
+    
+            int32_t const& Sign() const;
+        int32_t const& Exp() const;
+        std::vector<uint32_t> const& Mantissa() const;
+    
+    int CCalcEngine::IsNumberInvalid(const wstring& numberString, int iMaxExp, int iMaxMantissa, uint32_t radix) const
+{
+    int iError = 0;
+    }
+    
+            case IDC_PERCENT:
+        {
+            // If the operator is multiply/divide, we evaluate this as 'X [op] (Y%)'
+            // Otherwise, we evaluate it as 'X [op] (X * Y%)'
+            if (m_nOpCode == IDC_MUL || m_nOpCode == IDC_DIV)
+            {
+                result = rat / 100;
             }
-            if (!approximate) {
-              model.trees[j]->CalculateContributions(feats, root_id, p_contribs,
-                                                     condition, condition_feature);
-            } else {
-              model.trees[j]->CalculateContributionsApprox(feats, root_id, p_contribs);
+            else
+            {
+                result = rat * (m_lastVal / 100);
             }
-          }
-          feats.Drop(batch[i]);
-          // add base margin to BIAS
-          if (base_margin.size() != 0) {
-            p_contribs[ncolumns - 1] += base_margin[row_idx * ngroup + gid];
-          } else {
-            p_contribs[ncolumns - 1] += model.base_margin;
-          }
+            break;
         }
-      }
-    }
-  }
     
-      for (const auto &batch : (*dmat)->GetRowBatches()) {
-    ASSERT_EQ(batch[0][0].fvalue, 0.0f);
-    ASSERT_EQ(batch[0][1].fvalue, -4.0f);
-    ASSERT_EQ(batch[2][0].fvalue, 3.0f);
-    ASSERT_EQ(batch[2][1].fvalue, 0.0f);
-  }
+    inline constexpr auto IDS_DIVBYZERO = IDS_ERRORS_FIRST;
+inline constexpr auto IDS_DOMAIN = IDS_ERRORS_FIRST + 1;
+inline constexpr auto IDS_UNDEFINED = IDS_ERRORS_FIRST + 2;
+inline constexpr auto IDS_POS_INFINITY = IDS_ERRORS_FIRST + 3;
+inline constexpr auto IDS_NEG_INFINITY = IDS_ERRORS_FIRST + 4;
+inline constexpr auto IDS_NOMEM = IDS_ERRORS_FIRST + 6;
+inline constexpr auto IDS_TOOMANY = IDS_ERRORS_FIRST + 7;
+inline constexpr auto IDS_OVERFLOW = IDS_ERRORS_FIRST + 8;
+inline constexpr auto IDS_NORESULT = IDS_ERRORS_FIRST + 9;
+inline constexpr auto IDS_INSUFFICIENT_DATA = IDS_ERRORS_FIRST + 10;
     
-    void DHTReplaceNodeTask::sendMessage()
-{
-  std::shared_ptr<DHTNode> questionableNode = bucket_->getLRUQuestionableNode();
-  if (!questionableNode) {
-    setFinished(true);
-  }
-  else {
-    getMessageDispatcher()->addMessageToQueue(
-        getMessageFactory()->createPingMessage(questionableNode), timeout_,
-        make_unique<DHTPingReplyMessageCallback<DHTReplaceNodeTask>>(this));
-  }
-}
-    
-      std::shared_ptr<DHTBucket>
-  getBucketFor(const std::shared_ptr<DHTNode>& node) const;
-    
-      // localnode
-  // 8bytes reserved
-  readBytes(fp, buf, buf.size(), 8);
-  // localnode ID
-  readBytes(fp, buf, buf.size(), DHT_ID_LENGTH);
-  auto localNode = std::make_shared<DHTNode>(buf);
-  // 4bytes reserved
-  readBytes(fp, buf, buf.size(), 4);
-    
-      WRITE_CHECK(fp, header, 8);
-  // write save date
-  uint64_t ntime = hton64(Time().getTimeFromEpoch());
-  WRITE_CHECK(fp, &ntime, sizeof(ntime));
-    
-    #include 'common.h'
-    
-    
-    {} // namespace aria2
-
-    
-    class DHTTaskFactoryImpl : public DHTTaskFactory {
-private:
-  std::shared_ptr<DHTNode> localNode_;
+        if (install_callbacks)
+    {
+        s3ePointerRegister(S3E_POINTER_BUTTON_EVENT, ImGui_Marmalade_PointerButtonEventCallback, 0);
+        s3eKeyboardRegister(S3E_KEYBOARD_KEY_EVENT, ImGui_Marmalade_KeyCallback, 0);
+        s3eKeyboardRegister(S3E_KEYBOARD_CHAR_EVENT, ImGui_Marmalade_CharCallback, 0);
     }
     
-    void DHTTaskQueueImpl::executeTask()
-{
-  A2_LOG_DEBUG('Updating periodicTaskQueue1');
-  periodicTaskQueue1_.update();
-  A2_LOG_DEBUG('Updating periodicTaskQueue2');
-  periodicTaskQueue2_.update();
-  A2_LOG_DEBUG('Updating immediateTaskQueue');
-  immediateTaskQueue_.update();
-}
+        IMGUI_API bool BuildFontAtlas(ImFontAtlas* atlas, unsigned int extra_flags = 0);
     
-    void DNSCache::markBad(const std::string& hostname, const std::string& ipaddr,
-                       uint16_t port)
-{
-  auto target = std::make_shared<CacheEntry>(hostname, port);
-  auto i = entries_.find(target);
-  if (i != entries_.end()) {
-    (*i)->markBad(ipaddr);
-  }
-}
+    static ImGui_ImplVulkanH_Window g_MainWindowData;
+static int                      g_MinImageCount = 2;
+static bool                     g_SwapChainRebuild = false;
+static int                      g_SwapChainResizeWidth = 0;
+static int                      g_SwapChainResizeHeight = 0;
+    
+    // Implemented features:
+//  [X] Renderer: User texture binding. Use 'LPDIRECT3DTEXTURE9' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+    
+    // Implemented features:
+//  [X] Renderer: User texture binding. Use 'CIwTexture*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+    
+            // Start the Dear ImGui frame
+        ImGui_ImplAllegro5_NewFrame();
+        ImGui::NewFrame();
+    
+        // Setup Platform/Renderer bindings
+    ImGui_Marmalade_Init(true);
+    
+    // note that you can play with the hashing functions all you
+// want without needing to change the decompressor
+#define stb__hc(q,h,c)      (((h) << 7) + ((h) >> 25) + q[c])
+#define stb__hc2(q,h,c,d)   (((h) << 14) + ((h) >> 18) + (q[c] << 7) + q[d])
+#define stb__hc3(q,c,d,e)   ((q[c] << 14) + (q[d] << 7) + q[e])
+    
+            // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!), 
+        // you may need to backup/reset/restore current shader using the commented lines below.
+        //GLint last_program; 
+        //glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
+        //glUseProgram(0);
+        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+        //glUseProgram(last_program);
