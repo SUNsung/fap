8a5a9466a33b14302519a4e7a0103576f4136f60
SUNsung <weatherius@gmail.com>
2019-05-17 00:42:44 +0300

    Rewrite [57T9773pt8kA657On0oT6tzC.Y57Cx0kbCIqeJ.57Koc.cpp] File in: 2019-05-17 00:42:43

--- cccccc/57T9773pt8kA657On0oT6tzC.Y57Cx0kbCIqeJ.57Koc.cpp
+++ cccccc/57T9773pt8kA657On0oT6tzC.Y57Cx0kbCIqeJ.57Koc.cpp
@@ -1,224 +1,240 @@
 
         
-        #include 'swift/Basic/Cache.h'
-#include 'llvm/ADT/DenseMap.h'
-#include 'llvm/Support/Mutex.h'
-    
-    DIRECTIONAL_PREPOSITION(above)
-DIRECTIONAL_PREPOSITION(after)
-DIRECTIONAL_PREPOSITION(along)
-DIRECTIONAL_PREPOSITION(alongside)
-DIRECTIONAL_PREPOSITION(as)
-DIRECTIONAL_PREPOSITION(at)
-DIRECTIONAL_PREPOSITION(before)
-DIRECTIONAL_PREPOSITION(below)
-DIRECTIONAL_PREPOSITION(by)
-DIRECTIONAL_PREPOSITION(following)
-DIRECTIONAL_PREPOSITION(for)
-DIRECTIONAL_PREPOSITION(from)
-DIRECTIONAL_PREPOSITION(given)
-DIRECTIONAL_PREPOSITION(in)
-DIRECTIONAL_PREPOSITION(including)
-DIRECTIONAL_PREPOSITION(inside)
-DIRECTIONAL_PREPOSITION(into)
-DIRECTIONAL_PREPOSITION(matching)
-DIRECTIONAL_PREPOSITION(of)
-DIRECTIONAL_PREPOSITION(on)
-DIRECTIONAL_PREPOSITION(passing)
-DIRECTIONAL_PREPOSITION(preceding)
-DIRECTIONAL_PREPOSITION(since)
-DIRECTIONAL_PREPOSITION(to)
-DIRECTIONAL_PREPOSITION(until)
-DIRECTIONAL_PREPOSITION(using)
-DIRECTIONAL_PREPOSITION(via)
-DIRECTIONAL_PREPOSITION(when)
-PREPOSITION(with)
-DIRECTIONAL_PREPOSITION(within)
-    
-      struct IndentScope {
-    TreePrinter *Printer;
-    size_t OldLength;
-    IndentScope(TreePrinter *printer, StringRef indent)
-        : Printer(printer), OldLength(printer->Indent.size()) {
-      Printer->Indent += indent;
+        class GetMergeMultiMapFeatureTensorsGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    vector<string> input_blob_names{};
+    vector<string> output_blob_names{};
     }
-    ~IndentScope() { Printer->Indent.resize(OldLength); }
-  };
+    }
     
-    bool importer::isCFTypeDecl(
-       const clang::TypedefNameDecl *Decl) {
-  if (CFPointeeInfo::classifyTypedef(Decl))
-    return true;
-  return false;
+    const AuthProperty AuthPropertyIterator::operator*() {
+  return std::pair<grpc::string_ref, grpc::string_ref>(
+      property_->name,
+      grpc::string_ref(property_->value, property_->value_length));
 }
     
-    #endif // SWIFT_IMPORTER_CFTYPEINFO_H
+    
+    { private:
+  CensusContext context_;
+  // Metadata elements for tracing and census stats data.
+  grpc_linked_mdelem stats_bin_;
+  grpc_linked_mdelem tracing_bin_;
+  // Client method.
+  absl::string_view method_;
+  std::string qualified_method_;
+  grpc_slice path_;
+  // The recv trailing metadata callbacks.
+  grpc_metadata_batch* recv_trailing_metadata_;
+  grpc_closure* initial_on_done_recv_trailing_metadata_;
+  grpc_closure on_done_recv_trailing_metadata_;
+  // recv message
+  grpc_closure* initial_on_done_recv_message_;
+  grpc_closure on_done_recv_message_;
+  // Start time (for measuring latency).
+  absl::Time start_time_;
+  // Server elapsed time in nanoseconds.
+  uint64_t elapsed_time_;
+  // The received message--may be null.
+  grpc_core::OrphanablePtr<grpc_core::ByteStream>* recv_message_;
+  // Number of messages in this RPC.
+  uint64_t recv_message_count_;
+  uint64_t sent_message_count_;
+  // Buffer needed for grpc_slice to reference when adding trace context
+  // metatdata to outgoing message.
+  char tracing_buf_[kMaxTraceContextLen];
+};
+    
+    // Serializes the outgoing stats context.  Field IDs are 1 byte followed by
+// field data. A 1 byte version ID is always encoded first. Tags are directly
+// serialized into the given grpc_slice.
+size_t StatsContextSerialize(size_t max_tags_len, grpc_slice* tags);
+    
+    // Client
+MeasureDouble RpcClientSentBytesPerRpc() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcClientSentBytesPerRpcMeasureName,
+      'Total bytes sent across all request messages per RPC', kUnitBytes);
+  return measure;
+}
+    
+    
+    {  // TODO: For performance:
+  // uint8_t version;
+  // uint8_t trace_id_field_id;
+  uint8_t trace_id[::opencensus::trace::TraceId::kSize];
+  // uint8_t span_id_field_id;
+  uint8_t span_id[::opencensus::trace::SpanId::kSize];
+  // uint8_t trace_options_field_id;
+  uint8_t trace_options[::opencensus::trace::TraceOptions::kSize];
+};
+    
+    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_SERVER_FILTER_H
+#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_SERVER_FILTER_H
+    
+    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
+    const grpc::string& name, const grpc::string& value) {
+  class StringOption final : public ServerBuilderOption {
+   public:
+    StringOption(const grpc::string& name, const grpc::string& value)
+        : name_(name), value_(value) {}
+    }
+    }
+    
+            bool IsInfinite() override;
+    
+        template <>
+    /*static*/ NDArrayViewPtr NDArrayView::RandomUniform<int8_t>(const NDShape& shape, double rangeBegin, double rangeEnd, unsigned long seed, const DeviceDescriptor& device)
+    {
+        return NDArrayView::_RandomUniform<int8_t, char>(shape, rangeBegin, rangeEnd, seed, device);
+    }
+    
+    // base class that we can catch, independent of the type parameter
+struct /*interface*/ IExceptionWithCallStackBase
+{
+    virtual const char * CallStack() const = 0;
+    virtual ~IExceptionWithCallStackBase() noexcept = default;
+};
+    
+    /*!
+ * \brief Registry entry for tree updater.
+ */
+struct TreeUpdaterReg
+    : public dmlc::FunctionRegEntryBase<TreeUpdaterReg,
+                                        std::function<TreeUpdater* ()> > {
+};
+    
+    
+    {
+    {      if (size_to_read != 0) {
+        CHECK_EQ(fi->Read(dmlc::BeginPtr(data_vec) + offset_vec[i],
+                          size_to_read * sizeof(Entry)),
+                 size_to_read * sizeof(Entry))
+            << 'Invalid SparsePage file';
+        curr_offset += size_to_read;
+      }
+      i = j;
+    }
+    // seek to end of record
+    if (curr_offset != disk_offset_.back()) {
+      fi->Seek(begin + disk_offset_.back() * sizeof(Entry));
+    }
+    return true;
+  }
+    
+      // localnode
+  // 8bytes reserved
+  readBytes(fp, buf, buf.size(), 8);
+  // localnode ID
+  readBytes(fp, buf, buf.size(), DHT_ID_LENGTH);
+  auto localNode = std::make_shared<DHTNode>(buf);
+  // 4bytes reserved
+  readBytes(fp, buf, buf.size(), 4);
+    
+    
+    {} // namespace aria2
 
     
-    /// Translate the given operator character into its mangled form.
-///
-/// Current operator characters:   @/=-+*%<>!&|^~ and the special operator '..'
-char Mangle::translateOperatorChar(char op) {
-  switch (op) {
-    case '&': return 'a'; // 'and'
-    case '@': return 'c'; // 'commercial at sign'
-    case '/': return 'd'; // 'divide'
-    case '=': return 'e'; // 'equal'
-    case '>': return 'g'; // 'greater'
-    case '<': return 'l'; // 'less'
-    case '*': return 'm'; // 'multiply'
-    case '!': return 'n'; // 'negate'
-    case '|': return 'o'; // 'or'
-    case '+': return 'p'; // 'plus'
-    case '?': return 'q'; // 'question'
-    case '%': return 'r'; // 'remainder'
-    case '-': return 's'; // 'subtract'
-    case '~': return 't'; // 'tilde'
-    case '^': return 'x'; // 'xor'
-    case '.': return 'z'; // 'zperiod' (the z is silent)
-    default:
-      return op;
+    DHTTaskFactoryImpl::DHTTaskFactoryImpl()
+    : routingTable_(nullptr),
+      dispatcher_(nullptr),
+      factory_(nullptr),
+      taskQueue_(nullptr),
+      timeout_(DHT_MESSAGE_TIMEOUT)
+{
+}
+    
+      // returns 'unknown'
+  virtual const std::string& getMessageType() const CXX11_OVERRIDE;
+    
+    std::vector<DNSCache::AddrEntry>::const_iterator
+DNSCache::CacheEntry::find(const std::string& addr) const
+{
+  for (auto i = addrEntries_.begin(), eoi = addrEntries_.end(); i != eoi; ++i) {
+    if ((*i).addr_ == addr) {
+      return i;
+    }
   }
+  return addrEntries_.end();
 }
     
-        struct Size2D {
-        Size2D() : width(0), height(0) {}
-        Size2D(size_t width_, size_t height_) : width(width_), height(height_) {}
+    namespace aria2 {
     }
     
-    void absDiff(const Size2D &size,
-             const s16 *src0Base, ptrdiff_t src0Stride,
-             const s16 *src1Base, ptrdiff_t src1Stride,
-             s16 *dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, AbsDiffSigned<s16>());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
+    class ExtensionManagerProcessorFactory : public ::apache::thrift::TProcessorFactory {
+ public:
+  ExtensionManagerProcessorFactory(const ::apache::thrift::stdcxx::shared_ptr< ExtensionManagerIfFactory >& handlerFactory) :
+      handlerFactory_(handlerFactory) {}
+    }
+    
+    #include 'ExtensionManager.h'
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TSimpleServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+    
+    
+    {};
+    
+          resp = smartctl.getDevInfo(devname, full_type);
+      if (resp.err != NOERR) {
+        LOG(WARNING) << 'There was an error retrieving drive information with '
+                        'hardware driver: '
+                     << libsmartctl::errStr(resp.err);
+        return;
+      }
+      // Only consider found if no error was returned.
+      found = true;
+    
+    TEST_F(kernelExtensions, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from kernel_extensions');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'idx', IntType}
+  //      {'refs', IntType}
+  //      {'size', IntType}
+  //      {'name', NormalType}
+  //      {'version', NormalType}
+  //      {'linked_against', NormalType}
+  //      {'path', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
 }
     
-        f32 alpha, beta, gamma;
-    float32x4_t valpha, vbeta, vgamma;
-    wAdd(f32 _alpha, f32 _beta, f32 _gamma):
-        alpha(_alpha), beta(_beta), gamma(_gamma)
+    #include <osquery/tests/integration/tables/helper.h>
+    
+    using ::apollo::canbus::ChassisDetail;
+    
+    #include 'glog/logging.h'
+    
+    unsigned int BaseMapMatrix::CreateBinary(unsigned char* buf,
+                                         unsigned int buf_size) const {
+  return 0;
+}
+    
+    
     {
-        valpha = vdupq_n_f32(_alpha);
-        vbeta = vdupq_n_f32(_beta);
-        vgamma = vdupq_n_f32(_gamma + 0.5);
-    }
-    
-        ptrdiff_t idx_l = internal::borderInterpolate(-1, size.width, borderType, borderMargin.left, borderMargin.right) * cn;
-    ptrdiff_t idx_r = internal::borderInterpolate(size.width, size.width, borderType, borderMargin.left, borderMargin.right) * cn;
-    
-    template <typename T>
-inline T *getRowPtr(T *base, ptrdiff_t stride, size_t row)
-{
-    char *baseRaw = const_cast<char *>(reinterpret_cast<const char *>(base));
-    return reinterpret_cast<T *>(baseRaw + ptrdiff_t(row) * stride);
-}
-    
-    typedef void (* rshiftConstFunc)(const Size2D &size,
-                                const s16 * srcBase, ptrdiff_t srcStride,
-                                u8 * dstBase, ptrdiff_t dstStride,
-                                CONVERT_POLICY cpolicy);
-    
-                    v_dst0 = vmlal_n_s16(v_dst0, vget_low_s16(t0_16s), kernelBase[5]);
-                v_dst0 = vmlal_n_s16(v_dst0, vget_low_s16(t1_16s), kernelBase[4]);
-                v_dst0 = vmlal_n_s16(v_dst0, vget_low_s16(t2_16s), kernelBase[3]);
-    
-            int s[2];
-        vst1_s32(s, vs2);
-    
-        // the others
-    for (size_t i = 1; i < size.height ; ++i)
     {
-        src = internal::getRowPtr(srcBase, srcStride, i);
-        u32 * prevSum = internal::getRowPtr(sumBase, sumStride, i - 1);
-        sum = internal::getRowPtr(sumBase, sumStride, i);
-    }
+    {}  // namespace gem
+}  // namespace canbus
+}  // namespace apollo
+
     
-    
-    {                // make border
-                    if (border == BORDER_MODE_REPLICATE || border == BORDER_MODE_REFLECT)
-                    {
-                        tcurr = vsetq_lane_s16(vgetq_lane_s16(tcurr, 0),tcurr, 7);
-                    }
-                    else if (border == BORDER_MODE_CONSTANT)
-                    {
-                        tcurr = vsetq_lane_s16(borderValue, tcurr, 7);
-                    }
-                    else if (border == BORDER_MODE_REFLECT101)
-                    {
-                        tcurr = vsetq_lane_s16(vgetq_lane_s16(tcurr, 1),tcurr, 7);
-                    }
-                continue;
-            }
-    
-    using json = nlohmann::json;
-    
-        // create an array from an array_t value
-    json::array_t array_value = {'one', 'two', 3, 4.5, false};
-    json j_array_t(array_value);
-    
-    void ActionCamera::setEye(float x, float y, float z)
-{
-    _eye.set(x, y, z);
-    updateTransform();
-}
-    
-    /** @class OrbitCamera
- *
- * @brief OrbitCamera action.
- * Orbits the camera around the center of the screen using spherical coordinates.
- * @ingroup Actions
- */
-class CC_DLL OrbitCamera : public ActionCamera
-{
-public:
-    /** Creates a OrbitCamera action with radius, delta-radius,  z, deltaZ, x, deltaX. 
-     *
-     * @param t Duration in seconds.
-     * @param radius The start radius.
-     * @param deltaRadius The delta radius.
-     * @param angleZ The start angle in Z.
-     * @param deltaAngleZ The delta angle in Z.
-     * @param angleX The start angle in X.
-     * @param deltaAngleX The delta angle in X.
-     * @return An OrbitCamera.
-     */
-    static OrbitCamera* create(float t, float radius, float deltaRadius, float angleZ, float deltaAngleZ, float angleX, float deltaAngleX);
-    
-    /** Positions the camera according to spherical coordinates. 
-     *
-     * @param r The spherical radius.
-     * @param zenith The spherical zenith.
-     * @param azimuth The spherical azimuth.
-     */
-    void sphericalRadius(float *r, float *zenith, float *azimuth);
-    }
-    
-        /**
-    @brief Set whether lens is concave.
-    @param concave Whether lens is concave.
-    */
-    void setConcave(bool concave) { _concave = concave; }
-    
-    // AtlasNode - draw
-void AtlasNode::draw(Renderer *renderer, const Mat4 &transform, uint32_t flags)
-{
-    // ETC1 ALPHA supports.
-    _quadCommand.init(_globalZOrder, _textureAtlas->getTexture(), getGLProgramState(), _blendFunc, _textureAtlas->getQuads(), _quadsToDraw, transform, flags);
-    
-    renderer->addCommand(&_quadCommand);
+    // config detail: {'name': 'commanded_value', 'enum': {0:
+// 'COMMANDED_VALUE_HEADLIGHTS_OFF', 1: 'COMMANDED_VALUE_LOW_BEAMS', 2:
+// 'COMMANDED_VALUE_HIGH_BEAMS'}, 'precision': 1.0, 'len': 8, 'is_signed_var':
+// False, 'offset': 0.0, 'physical_range': '[0|2]', 'bit': 15, 'type': 'enum',
+// 'order': 'motorola', 'physical_unit': ''}
+Headlight_rpt_77::Commanded_valueType Headlightrpt77::commanded_value(
+    const std::uint8_t* bytes, int32_t length) const {
+  Byte t0(bytes + 1);
+  int32_t x = t0.get_byte(0, 8);
     }
