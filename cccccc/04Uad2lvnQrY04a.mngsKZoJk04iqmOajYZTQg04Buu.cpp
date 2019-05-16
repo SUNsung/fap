5f1355dcdb03b35da0985825a6de8af4b0a0c445
SUNsung <weatherius@gmail.com>
2019-05-16 16:37:57 +0300

    Rewrite [04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp] File in: 2019-05-16 16:37:56

--- cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
+++ cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
@@ -1,342 +1,124 @@
 
         
-        namespace atom {
+        StringRef importer::getCFTypeName(
+            const clang::TypedefNameDecl *decl) {
+  if (auto pointee = CFPointeeInfo::classifyTypedef(decl)) {
+    auto name = decl->getName();
+    if (pointee.isRecord() || pointee.isTypedef())
+      if (name.endswith(SWIFT_CFTYPE_SUFFIX))
+        return name.drop_back(strlen(SWIFT_CFTYPE_SUFFIX));
+    }
     }
     
-    #endif  // ATOM_BROWSER_API_ATOM_API_AUTO_UPDATER_H_
-
-    
-    #include 'atom/browser/render_process_preferences.h'
-#include 'native_mate/handle.h'
-#include 'native_mate/wrappable.h'
-    
-     private:
-  static int32_t next_id_;
-  static atom::KeyWeakMap<int32_t>* weak_map_;  // leaked on purpose
-    
-      // net::URLRequestJobFactory::ProtocolHandler:
-  net::URLRequestJob* MaybeCreateJob(
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const override;
-  bool IsSafeRedirectTarget(const GURL& location) const override;
-    
-    #endif  // ATOM_BROWSER_NET_ASAR_ASAR_PROTOCOL_HANDLER_H_
-
-    
-    void URLRequestAboutJob::Start() {
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(&URLRequestAboutJob::StartAsync,
-                                weak_ptr_factory_.GetWeakPtr()));
-}
-    
-      // URLRequestJob:
-  void Start() override;
-  void Kill() override;
-  bool GetMimeType(std::string* mime_type) const override;
-    
-    namespace atom {
+    class CFPointeeInfo {
+  bool IsValid;
+  bool IsConst;
+  llvm::PointerUnion<const clang::RecordDecl *, const clang::TypedefNameDecl *>
+      Decl;
+  CFPointeeInfo() = default;
     }
     
-    template <>
-template <typename T>
-bool EnforceFiniteOp<CPUContext>::DoRunWithType() {
-  EnforceOnCPU<T>(Input(0));
-  return true;
-}
+    #include 'llvm/ADT/Optional.h'
+#include 'llvm/ADT/StringRef.h'
     
-    <details>
+      if (!convertUTF8toUTF32(InputUTF8, InputCodePoints, mapNonSymbolChars))
+    return false;
     
-    RANGES dimensions description:
-1: represents list of examples within a batch
-2: represents list features
-3: two values which are start and length or a range (to be applied on DATA)
-    
-    op = core.CreateOperator(
-    'GivenTensorFill',
-    [],
-    ['out'],
-    values=[1., 2., 3.],
-    shape=[3],
-)
-    
-              vector<TensorShape> out(1);
-          switch (order) {
-            case StorageOrder::NCHW:
-              out[0] = CreateTensorShape(
-                  vector<int>{N, C * kernel_h * kernel_w, out_h, out_w},
-                  TensorProto::FLOAT);
-              break;
-            case StorageOrder::NHWC:
-              out[0] = CreateTensorShape(
-                  vector<int>{N, out_h, out_w, kernel_h * kernel_w * C},
-                  TensorProto::FLOAT);
-              break;
-            default:
-              CAFFE_THROW('Unknown storage order: ', order);
-          }
-    
-    REGISTER_CUDA_OPERATOR(LC1D, LocallyConnectedOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(
-    LC1DGradient,
-    LocallyConnectedGradientOp<float, CUDAContext>);
-    
-    int16_t word_blob_quality(WERD_RES *word, ROW *row);
-void reject_whole_page(PAGE_RES_IT &page_res_it);
-    
-    class TBOX;
-    
-    
-    {  int delta = this - prev;
-  int32_t n = prev->n_ + 1;
-  int32_t sig_x = prev->sig_x_ + delta;
-  int64_t sig_xsq = prev->sig_xsq_ + delta * delta;
-  int64_t cost = (sig_xsq - sig_x * sig_x / n) / n;
-  cost += prev->total_cost_;
-  UpdateIfBetter(cost, prev->total_steps_ + 1, prev, n, sig_x, sig_xsq);
-  return cost;
-}
-    
-    template <typename T> class GenericVector;
-    
-    struct PARA : public ELIST_LINK {
- public:
-  PARA() : model(nullptr), is_list_item(false),
-           is_very_first_or_continuation(false), has_drop_cap(false) {}
-    }
-    
-        bool flag(REJ_FLAGS rej_flag) {
-      if (rej_flag < 16)
-        return flags1.bit (rej_flag);
-      else
-        return flags2.bit (rej_flag - 16);
-    }
-    
-    // A useful base class to facilitate the common operation of sorting a vector
-// of owned pointer data using a separate key. This class owns its data pointer,
-// deleting it when it has finished with it, and providing copy constructor and
-// operator= that have move semantics so that the data does not get copied and
-// only a single instance of KDPtrPair holds a specific data pointer.
-template <typename Key, typename Data>
-class KDPtrPair {
- public:
-  KDPtrPair() : data_(nullptr) {}
-  KDPtrPair(Key k, Data* d) : data_(d), key_(k) {}
-  // Copy constructor steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  KDPtrPair(KDPtrPair& src) : data_(src.data_), key_(src.key_) {
-    src.data_ = nullptr;
+    bool ArgsToFrontendInputsConverter::enforceFilelistExclusion() {
+  if (Args.hasArg(options::OPT_INPUT) && FilelistPathArg) {
+    Diags.diagnose(SourceLoc(),
+                   diag::error_cannot_have_input_files_with_file_list);
+    return true;
   }
-  // Destructor deletes data, assuming it is the sole owner.
-  ~KDPtrPair() {
-    delete this->data_;
-    this->data_ = nullptr;
+  // The following is not strictly necessary, but the restriction makes
+  // it easier to understand a given command line:
+  if (Args.hasArg(options::OPT_primary_file) && PrimaryFilelistPathArg) {
+    Diags.diagnose(
+        SourceLoc(),
+        diag::error_cannot_have_primary_files_with_primary_file_list);
+    return true;
   }
-  // Operator= steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  void operator=(KDPtrPair& src) {
-    delete this->data_;
-    this->data_ = src.data_;
-    src.data_ = nullptr;
-    this->key_ = src.key_;
-  }
-    }
-    
-      // Return a pointer to the object identified by id.
-  // If we haven't yet loaded the object, use loader to load it.
-  // If loader fails to load it, record a nullptr entry in the cache
-  // and return nullptr -- further attempts to load will fail (even
-  // with a different loader) until DeleteUnusedObjects() is called.
-  // We delete the given loader.
-  T *Get(STRING id,
-         TessResultCallback<T *> *loader) {
-    T *retval = nullptr;
-    mu_.Lock();
-    for (int i = 0; i < cache_.size(); i++) {
-      if (id == cache_[i].id) {
-        retval = cache_[i].object;
-        if (cache_[i].object != nullptr) {
-          cache_[i].count++;
-        }
-        mu_.Unlock();
-        delete loader;
-        return retval;
-      }
-    }
-    cache_.push_back(ReferenceCount());
-    ReferenceCount &rc = cache_.back();
-    rc.id = id;
-    retval = rc.object = loader->Run();
-    rc.count = (retval != nullptr) ? 1 : 0;
-    mu_.Unlock();
-    return retval;
-  }
-    
-    // Client
-MeasureDouble RpcClientSentBytesPerRpc() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcClientSentBytesPerRpcMeasureName,
-      'Total bytes sent across all request messages per RPC', kUnitBytes);
-  return measure;
+  return false;
 }
-    
-      // Serializes rpc server stats into the provided buffer.  It returns the
-  // number of bytes written to the buffer. If the buffer is smaller than
-  // kRpcServerStatsSize bytes it will return kEncodeDecodeFailure. Inlined for
-  // performance reasons.
-  static size_t Encode(uint64_t time, char* buf, size_t buf_size) {
-    if (buf_size < kRpcServerStatsSize) {
-      return kEncodeDecodeFailure;
-    }
-    }
-    
-    #include 'absl/strings/string_view.h'
-#include 'absl/time/clock.h'
-#include 'absl/time/time.h'
-#include 'include/grpc/grpc_security.h'
-#include 'src/cpp/ext/filters/census/channel_filter.h'
-#include 'src/cpp/ext/filters/census/context.h'
-    
-    #endif  // GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
-
-    
-    #endif  // GRPC_INTERNAL_CPP_THREAD_POOL_INTERFACE_H
-
-    
-    
-    {}  // namespace grpc
-
     
     
     {
-    {
-    {}  // namespace conti_radar
-}  // namespace drivers
-}  // namespace apollo
-
+    {}}
+    
+    using LineNumber = uint32_t;
+using ColNumber  = uint32_t;
+using LineRange  = std::tuple<LineNumber,LineNumber>;
     
     
-    {  switch (x) {
-    case 0x0:
-      return RCS_THRESHOLD_STANDARD;
-    case 0x1:
-      return RCS_THRESHOLD_HIGH_SENSITIVITY;
-    default:
-      return RCS_THRESHOLD_ERROR;
-  }
-}
+    {      dword(sc_formater.instruction);
+   }
     
-    void Spline1dSeg::SetSplineFunc(const PolynomialXd& spline_func) {
-  spline_func_ = spline_func;
-  derivative_ = PolynomialXd::DerivedFrom(spline_func_);
-  second_order_derivative_ = PolynomialXd::DerivedFrom(derivative_);
-  third_order_derivative_ = PolynomialXd::DerivedFrom(second_order_derivative_);
-}
+    /**
+ * Implement this interface to report information to debugger or execute
+ * debugger commands.
+ */
+struct IDebuggable {
+  enum Support {
+    SupportInfo = 1,
+    SupportDump = 2,
+    SupportVerb = 4,
+  };
+    }
     
-    // config detail: {'name': 'manual_input', 'enum': {0:
-// 'MANUAL_INPUT_HEADLIGHTS_OFF', 1: 'MANUAL_INPUT_LOW_BEAMS', 2:
-// 'MANUAL_INPUT_HIGH_BEAMS'}, 'precision': 1.0, 'len': 8, 'is_signed_var':
-// False, 'offset': 0.0, 'physical_range': '[0|2]', 'bit': 7, 'type': 'enum',
-// 'order': 'motorola', 'physical_unit': ''}
-Headlight_rpt_77::Manual_inputType Headlightrpt77::manual_input(
-    const std::uint8_t* bytes, int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 8);
+    #include 'hphp/runtime/ext/std/ext_std_file.h'
+#include 'hphp/runtime/base/file.h'
+#include 'hphp/runtime/server/static-content-cache.h'
+    
+    /**
+ * For php://output, a simple wrapper of g_context->out().
+ */
+struct OutputFile : File {
+  DECLARE_RESOURCE_ALLOCATION(OutputFile);
+    }
+    
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
     }
     
     
-    {    _radZ = (float)CC_DEGREES_TO_RADIANS(_angleZ);
-    _radX = (float)CC_DEGREES_TO_RADIANS(_angleX);
+    {};
+    
+    void ExtensionException::__set_uuid(const ExtensionRouteUUID val) {
+  this->uuid = val;
 }
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-    AccelDeccelAmplitude::~AccelDeccelAmplitude()
+std::ostream& operator<<(std::ostream& out, const ExtensionException& obj)
 {
-    CC_SAFE_RELEASE(_other);
+  obj.printTo(out);
+  return out;
 }
     
+    #include <osquery/utils/expected/expected.h>
+#include <osquery/utils/system/posix/errno.h>
     
-    {        return true;
-    }
+    #pragma once
     
-        /**
-    @brief Get the amplitude rate of the effect.
-    @return Return the amplitude rate of the effect.
-    */
-    float getAmplitudeRate() const { return _amplitudeRate; }
-    /**
-    @brief Set the amplitude rate of the effect.
-    @param amplitudeRate The value of amplitude rate will be set.
-    */
-    void setAmplitudeRate(float amplitudeRate) { _amplitudeRate = amplitudeRate; }
+    // Sanity check integration test for kernel_panics
+// Spec file: specs/darwin/kernel_panics.table
     
-    Vector<Node*> ActionManager::pauseAllRunningActions()
-{
-    Vector<Node*> idsWithActions;
-    
-    for (tHashElement *element=_targets; element != nullptr; element = (tHashElement *)element->hh.next) 
-    {
-        if (! element->paused) 
-        {
-            element->paused = true;
-            idsWithActions.pushBack(element->target);
-        }
-    }    
-    
-    return idsWithActions;
+    TEST_F(keychainItems, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from keychain_items');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'label', NormalType}
+  //      {'description', NormalType}
+  //      {'comment', NormalType}
+  //      {'created', NormalType}
+  //      {'modified', NormalType}
+  //      {'type', NormalType}
+  //      {'path', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
 }
-    
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-    
-    ActionTween* ActionTween::reverse() const
-{
-    return ActionTween::create(_duration, _key, _to, _from);
-}
-    
-        /** Adds an animation from an NSDictionary.
-     * Make sure that the frames were previously loaded in the SpriteFrameCache.
-     * @param dictionary An NSDictionary.
-     * @param plist The path of the relative file,it use to find the plist path for load SpriteFrames.
-     * @since v1.1
-	 @js NA
-     */
-    void addAnimationsWithDictionary(const ValueMap& dictionary,const std::string& plist);
-    
-    #ifndef COCOS_2D_CCAUTOPOLYGON_H__
-#define COCOS_2D_CCAUTOPOLYGON_H__
-    
-        // Overrides
-    /**
-     * @lua NA
-     */
-    virtual void onEnter() override;
-    /**
-     * @lua NA
-     */
-    virtual void onEnterTransitionDidFinish() override;
-    /**
-     * @lua NA
-     */
-    virtual void onExitTransitionDidStart() override;
-    /**
-     * @lua NA
-     */
-    virtual void onExit() override;
-    virtual void visit(Renderer *renderer, const Mat4 &parentTransform, uint32_t parentFlags) override;
-    
-    virtual void setCameraMask(unsigned short mask, bool applyChildren = true) override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    ClippingNode();
-    
-    /**
-     * @js NA
-     * @lua NA
-     */
-    virtual ~ClippingNode();
