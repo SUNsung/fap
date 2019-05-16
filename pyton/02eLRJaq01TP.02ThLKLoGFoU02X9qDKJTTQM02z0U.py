7338d534eb59afb4ac589ebc5d8b5d79b769b2ef
SUNsung <weatherius@gmail.com>
2019-05-16 14:05:33 +0300

    Rewrite [02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py] File in: 2019-05-16 14:05:32

--- pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
+++ pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
@@ -1,75 +1,177 @@
 
         
-        
-def test_get_new_command():
-    new_command = get_new_command(Command('apt list --upgradable', match_output))
-    assert new_command == 'apt upgrade'
+            if len(clean) & 1:
+        clean += 'X'
     
-      aws help
-  aws <command> help
-  aws <command> <subcommand> help
-aws: error: argument operation: Invalid choice, valid choices are:
+    def makeKeyFiles(name, keySize):
+    if os.path.exists('%s_pubkey.txt' % (name)) or os.path.exists('%s_privkey.txt' % (name)):
+        print('\nWARNING:')
+        print(''%s_pubkey.txt' or '%s_privkey.txt' already exists. \nUse a different name or delete these files and re-run this program.' % (name, name))
+        sys.exit()
     
+    This is a pure Python implementation of Dynamic Programming solution to the longest increasing subsequence of a given sequence.
     
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_get_new_command(brew_no_available_formula):
-    assert get_new_command(Command('brew install elsticsearch',
-                                   brew_no_available_formula))\
-        == 'brew install elasticsearch'
+        def solve_sub_array(self):
+        rear = [int(self.array[0])]*len(self.array)
+        sum_value = [int(self.array[0])]*len(self.array)
+        for i in range(1, len(self.array)):
+            sum_value[i] = max(int(self.array[i]) + sum_value[i-1], int(self.array[i]))
+            rear[i] = max(sum_value[i], rear[i-1])
+        return rear[len(self.array)-1]
     
-    from scrapy.commands import ScrapyCommand
-from scrapy.exceptions import UsageError
+        difference = predict - actual
+    numerator = np.sum(difference) / len(predict) 
+    denumerator =  np.sum(actual) / len(predict)
+    print(numerator)
+    print(denumerator)
     
-                # determine real callback
-            cb = response.meta['_callback']
-            if not cb:
-                if opts.callback:
-                    cb = opts.callback
-                elif opts.rules and self.first_response == response:
-                    cb = self.get_callback_from_rules(spider, response)
+            a += a
+        b >>= 1
     
-        def process_options(self, args, opts):
-        ScrapyCommand.process_options(self, args, opts)
-        try:
-            opts.spargs = arglist_to_dict(opts.spargs)
-        except ValueError:
-            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
-        if opts.output:
-            if opts.output == '-':
-                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
-            else:
-                self.settings.set('FEED_URI', opts.output, priority='cmdline')
-            feed_exporters = without_none_values(self.settings.getwithbase('FEED_EXPORTERS'))
-            valid_output_formats = feed_exporters.keys()
-            if not opts.output_format:
-                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
-            if opts.output_format not in valid_output_formats:
-                raise UsageError('Unrecognized output format '%s', set one'
-                                 ' using the '-t' switch or as a file extension'
-                                 ' from the supported list %s' % (opts.output_format,
-                                                                  tuple(valid_output_formats)))
-            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
+    '''
     
         def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('--verbose', '-v', dest='verbose', action='store_true',
-            help='also display twisted/python/platform info (useful for bug reports)')
+        '''
+        Populate option parse with options available for this command
+        '''
+        group = OptionGroup(parser, 'Global Options')
+        group.add_option('--logfile', metavar='FILE',
+            help='log file. if omitted stderr will be used')
+        group.add_option('-L', '--loglevel', metavar='LEVEL', default=None,
+            help='log level (default: %s)' % self.settings['LOG_LEVEL'])
+        group.add_option('--nolog', action='store_true',
+            help='disable logging completely')
+        group.add_option('--profile', metavar='FILE', default=None,
+            help='write python cProfile stats to FILE')
+        group.add_option('--pidfile', metavar='FILE',
+            help='write process ID to FILE')
+        group.add_option('-s', '--set', action='append', default=[], metavar='NAME=VALUE',
+            help='set/override setting (may be repeated)')
+        group.add_option('--pdb', action='store_true', help='enable pdb on failure')
     
-            # extract cookies from Set-Cookie and drop invalid/expired cookies
-        cookiejarkey = request.meta.get('cookiejar')
-        jar = self.jars[cookiejarkey]
-        jar.extract_cookies(response, request)
-        self._debug_set_cookie(response, spider)
+            infos = []
+        if not self.wasSuccessful():
+            write('FAILED')
+            failed, errored = map(len, (self.failures, self.errors))
+            if failed:
+                infos.append('failures=%d' % failed)
+            if errored:
+                infos.append('errors=%d' % errored)
+        else:
+            write('OK')
     
-    # How to display URL addresses: 'footnote', 'no', or 'inline'.
-#
-# texinfo_show_urls = 'footnote'
+    logger = logging.getLogger(__name__)
+    
+    class Command(ScrapyCommand):
+    
+            self._signer = None
+        if is_botocore():
+            import botocore.auth
+            import botocore.credentials
+            kw.pop('anon', None)
+            if kw:
+                raise TypeError('Unexpected keyword arguments: %s' % kw)
+            if not self.anon:
+                SignerCls = botocore.auth.AUTH_TYPE_MAPS['s3']
+                self._signer = SignerCls(botocore.credentials.Credentials(
+                    aws_access_key_id, aws_secret_access_key))
+        else:
+            _S3Connection = _get_boto_connection()
+            try:
+                self.conn = _S3Connection(
+                    aws_access_key_id, aws_secret_access_key, **kw)
+            except Exception as ex:
+                raise NotConfigured(str(ex))
+    
+            def _identityVerifyingInfoCallback(self, connection, where, ret):
+            if where & SSL_CB_HANDSHAKE_START:
+                set_tlsext_host_name(connection, self._hostnameBytes)
+            elif where & SSL_CB_HANDSHAKE_DONE:
+                try:
+                    verifyHostname(connection, self._hostnameASCII)
+                except verification_errors as e:
+                    logger.warning(
+                        'Remote certificate is not valid for hostname '{}'; {}'.format(
+                            self._hostnameASCII, e))
+    
+            # Fixes Twisted 11.1.0+ support as HTTPClientFactory is expected
+        # to have _disconnectedDeferred. See Twisted r32329.
+        # As Scrapy implements it's own logic to handle redirects is not
+        # needed to add the callback _waitForDisconnect.
+        # Specifically this avoids the AttributeError exception when
+        # clientConnectionFailed method is called.
+        self._disconnectedDeferred = defer.Deferred()
+    
+            If it has already been started, stop it. In all cases, close all spiders
+        and the downloader.
+        '''
+        if self.running:
+            # Will also close spiders and downloader
+            return self.stop()
+        elif self.open_spiders:
+            # Will also close downloader
+            return self._close_all_spiders()
+        else:
+            return defer.succeed(self.downloader.close())
+    
+        def _has_ajax_crawlable_variant(self, response):
+        '''
+        Return True if a page without hash fragment could be 'AJAX crawlable'
+        according to https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
+        '''
+        body = response.text[:self.lookup_bytes]
+        return _has_ajaxcrawlable_meta(body)
     
     
-def __meters_to_kilometers(meters: float) -> float:
-    '''Convert meters to kilometers.'''
-    return meters * 0.001
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
 
     
+        # TODO: decoder/encoder should accept cls? Otherwise, subclassing
+    # JSONObjectWithFields is tricky...
+    header_cls = Header
+    header = jose.Field(
+        'header', omitempty=True, default=header_cls(),
+        decoder=header_cls.from_json)
     
+        def test_b_observers_shall_be_attachable(cls):
+        cls.s.attach(cls.dec_obs)
+        cls.assertEqual(isinstance(cls.s._observers[0], DecimalViewer), True)
+        cls.assertEqual(len(cls.s._observers), 1)
+        cls.s.attach(cls.hex_obs)
+        cls.assertEqual(isinstance(cls.s._observers[1], HexViewer), True)
+        cls.assertEqual(len(cls.s._observers), 2)
+    
+        def test_subscriber_shall_be_detachable_from_subscriptions(cls):
+        subscription = 'sub msg'
+        pro = Provider()
+        sub = Subscriber('sub name', pro)
+        sub.subscribe(subscription)
+        cls.assertEqual(len(pro.subscribers[subscription]), 1)
+        sub.unsubscribe(subscription)
+        cls.assertEqual(len(pro.subscribers[subscription]), 0)
+    
+        def test_initial_state(self):
+        state = self.radio.state.name
+        expected_state_name = 'AM'
+        self.assertEqual(state, expected_state_name)
+    
+        def test_relatives_after_access(self):
+        print(u'John's relatives: {0}'.format(self.John.relatives))
+        self.assertIn('relatives', self.John.__dict__)
+    
+        def test_sales_manager_shall_not_respond_through_proxy_with_delay(cls):
+        cls.ntp.busy = 'Yes'
+        start_time = time()
+        cls.ntp.talk()
+        end_time = time()
+        execution_time = end_time - start_time
+        print_output = cls.output.getvalue()
+        expected_print_output = 'Proxy checking for Sales Manager availability\n\
+This Sales Manager will not talk to you whether he/she is busy or not\n'
+        cls.assertEqual(print_output, expected_print_output)
+        expected_execution_time = 1
+        cls.assertEqual(int(execution_time * 10), expected_execution_time)
+
+    
+        REGISTRY = {}
