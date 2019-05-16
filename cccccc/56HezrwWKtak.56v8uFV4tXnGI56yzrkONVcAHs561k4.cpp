978cead5d66e9ef0fdb1ca280aee8cdab39ac6df
SUNsung <weatherius@gmail.com>
2019-05-16 17:51:31 +0300

    Rewrite [56HezrwWKtak.56v8uFV4tXnGI56yzrkONVcAHs561k4.cpp] File in: 2019-05-16 17:51:30

--- cccccc/56HezrwWKtak.56v8uFV4tXnGI56yzrkONVcAHs561k4.cpp
+++ cccccc/56HezrwWKtak.56v8uFV4tXnGI56yzrkONVcAHs561k4.cpp
@@ -1,152 +1,220 @@
 
         
-        OPERATOR_SCHEMA(EnforceFinite)
-    .NumInputs(1)
-    .NumOutputs(0)
-    .SetDoc(R'DOC(
-Raise if there is NaN or Inf values in the input tensor.
-)DOC')
-    .Input(0, 'input', 'Input tensor');
-    
-    workspace.RunOperatorOnce(op)
-print('indices: \n', workspace.FetchBlob('indices'))
-    
-    Example 1:
-  DATA  = [1, 2, 3, 4, 5, 6, 7, 8]
-  RANGES = [
-    [
-      [2, 4],
-      [0, 2],
-    ],
-    [
-      [0, 0],
-      [6, 2],
-    ]
-  ]
-  lengths = [4, 2]
-  OUTPUT[0] = [[3, 4, 5, 6], [0, 0, 0, 0]]
-  OUTPUT[1] = [[1, 2], [7, 8]]
-    
-    OPERATOR_SCHEMA(Glu)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-Applies gated linear unit to the input Tensor X. The output Y is half the size
-of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
-[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
-X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
-)DOC')
-    .Input(0, 'X', '1D input tensor')
-    .Output(0, 'Y', '1D output tensor');
-    
-    namespace grpc {
-    }
-    
-    #include 'absl/strings/string_view.h'
-#include 'absl/time/time.h'
-#include 'src/cpp/ext/filters/census/channel_filter.h'
-#include 'src/cpp/ext/filters/census/context.h'
-    
-    // Thread compatible.
-class CensusContext {
- public:
-  CensusContext() : span_(::opencensus::trace::Span::BlankSpan()) {}
-    }
-    
-    #include 'opencensus/stats/stats.h'
-#include 'src/cpp/ext/filters/census/grpc_plugin.h'
-    
-    
-    {  const protobuf::FileDescriptor* file_desc =
-      descriptor_pool_->FindFileContainingSymbol(symbol);
-  if (file_desc == nullptr) {
-    return Status(StatusCode::NOT_FOUND, 'Symbol not found.');
+        
+    {
+    {    output() = 'A(m, 0) == A(m-1, 1)';
   }
-  std::unordered_set<grpc::string> seen_files;
-  FillFileDescriptorResponse(file_desc, response, &seen_files);
-  return Status::OK;
+};
+    
+    #endif  // TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
+
+    
+    // Destructor passed to TF_NewTensor when it reuses a numpy buffer. Stores a
+// pointer to the pyobj in a buffer to be dereferenced later when we're actually
+// holding the GIL. Data and len are ignored.
+void DelayedNumpyDecref(void* data, size_t len, void* obj);
+    
+    // We define the PY_ARRAY_UNIQUE_SYMBOL in this .cc file and provide an
+// ImportNumpy function to populate it.
+#define TF_IMPORT_NUMPY
+    
+    class RandomAccessFile;
+    
+    
+    {  tensorflow::DeviceNameUtils::ParsedName parsed_name;
+  if (!tensorflow::DeviceNameUtils::ParseFullName(node_def.device(),
+                                                  &parsed_name)) {
+    LOG(WARNING) << 'Failed to parse device from node_def: '
+                 << node_def.ShortDebugString();
+    return '';
+  }
+  string class_name = '';
+  tensorflow::FindKernelDef(tensorflow::DeviceType(parsed_name.type.c_str()),
+                            node_def, nullptr /* kernel_def */, &class_name)
+      .IgnoreError();
+  return class_name;
+}
+    
+    enum RecordType {
+  // Zero is reserved for preallocated files
+  kZeroType = 0,
+    }
+    
+    #include <stddef.h>
+#include <stdint.h>
+    
+    Writer::Writer(WritableFile* dest, uint64_t dest_length)
+    : dest_(dest), block_offset_(dest_length % kBlockSize) {
+  InitTypeCrc(type_crc_);
 }
     
     
-    {  const protobuf::DescriptorPool* descriptor_pool_;
-  const std::vector<string>* services_;
+    {  // Verify that the size of the key space not touched by the reads
+  // is pretty much unchanged.
+  const int64_t final_other_size = Size(Key(n), Key(kCount));
+  ASSERT_LE(final_other_size, initial_other_size + 1048576);
+  ASSERT_GE(final_other_size, initial_other_size / 5 - 1048576);
+}
+    
+    
+    {  // crc32c values for all supported record types.  These are
+  // pre-computed to reduce the overhead of computing the crc of the
+  // record type stored in the header.
+  uint32_t type_crc_[kMaxRecordType + 1];
 };
     
     
-    {   private:
-    DynamicThreadPool* pool_;
-    grpc_core::Thread thd_;
-    void ThreadFunc();
-  };
-  std::mutex mu_;
-  std::condition_variable cv_;
-  std::condition_variable shutdown_cv_;
-  bool shutdown_;
-  std::queue<std::function<void()>> callbacks_;
-  int reserve_threads_;
-  int nthreads_;
-  int threads_waiting_;
-  std::list<DynamicThread*> dead_threads_;
+    {  Env* const env_;
+  const std::string dbname_;
+  const Options& options_;
+  Cache* cache_;
+};
     
-    #endif  // GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
+      // Given a key, return an approximate byte offset in the file where
+  // the data for that key begins (or would begin if the key were
+  // present in the file).  The returned value is in terms of file
+  // bytes, and so includes effects like compression of the underlying data.
+  // E.g., the approximate offset of the last key in the table will
+  // be close to the file length.
+  uint64_t ApproximateOffsetOf(const Slice& key) const;
+    
+      // Add key,value to the table being constructed.
+  // REQUIRES: key is after any previously added key according to comparator.
+  // REQUIRES: Finish(), Abandon() have not been called
+  void Add(const Slice& key, const Slice& value);
+    
+    /*!
++ * \brief The result holder of dispatch mode of each Node in the graph.
++ * \note Stored under graph.attrs['dispatch_mode'], provided by Pass 'InferStorageType'
++ *
++ * \code
++ *  Graph g = ApplyPass(src_graph, 'InferStorageType');
++ *  const DispatchModeVector& dispatch_modes = g.GetAttr<DispatchModeVector>('dispatch_mode');
++ *  // get dispatch mode by entry node id
++ *  int node_type = dispatch_modes[nid];
++ * \endcode
++ *
++ * \sa FInferStorageType
++ */
+using DispatchModeVector = std::vector<DispatchMode>;
+    
+      for (auto& e : idx.outputs()) {
+    ++ref_count[idx.entry_id(e)];
+  }
+  for (uint32_t nid = 0; nid < idx.num_nodes(); ++nid) {
+    for (auto &e : idx[nid].inputs) {
+      ++ref_count[idx.entry_id(e)];
+    }
+  }
+    
+    namespace mxnet {
+namespace io {
+/*! \return the parameter of default augmenter */
+std::vector<dmlc::ParamFieldInfo> ListDefaultAugParams();
+std::vector<dmlc::ParamFieldInfo> ListDefaultDetAugParams();
+}  // namespace io
+}  // namespace mxnet
+#endif  // MXNET_IO_IMAGE_AUGMENTER_H_
 
     
     
-/*
- * Given sines and cosines, tells if A's angle is less than B's on -Pi, Pi
- * (in other words, is A 'righter' than B)
- */
-bool IsRighter(float32 sinA, float32 cosA, float32 sinB, float32 cosB){
-	if (sinA < 0){
-		if (sinB > 0 || cosA <= cosB) return true;
-		else return false;
-	} else {
-		if (sinB < 0 || cosA <= cosB) return false;
-		else return true;
-	}
+    {  // creat mean image.
+  inline void CreateMeanImg(void) {
+    if (param_.verbose) {
+      LOG(INFO) << 'Cannot find ' << param_.mean_img
+                << ': create mean image, this will take some time...';
+    }
+    double start = dmlc::GetTime();
+    size_t imcnt = 1;  // NOLINT(*)
+    CHECK(this->Next_()) << 'input iterator failed.';
+    meanimg_.Resize(outimg_.shape_);
+    mshadow::Copy(meanimg_, outimg_);
+    while (this->Next_()) {
+      meanimg_ += outimg_;
+      imcnt += 1;
+      double elapsed = dmlc::GetTime() - start;
+      if (imcnt % 10000L == 0 && param_.verbose) {
+        LOG(INFO) << imcnt << ' images processed, ' << elapsed << ' sec elapsed';
+      }
+    }
+    meanimg_ *= (1.0f / imcnt);
+    // save as mxnet python compatible format.
+    TBlob tmp = meanimg_;
+    {
+      std::unique_ptr<dmlc::Stream> fo(dmlc::Stream::Create(param_.mean_img.c_str(), 'w'));
+      NDArray::Save(fo.get(),
+                    {NDArray(tmp, 0)},
+                    {'mean_img'});
+    }
+    if (param_.verbose) {
+      LOG(INFO) << 'Save mean image to ' << param_.mean_img << '..';
+    }
+    meanfile_ready_ = true;
+    this->BeforeFirst();
+  }
+};
+    
+    #ifndef MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
+#define MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
+#include <dmlc/parameter.h>
+#include <string>
+#include <utility>
+#include <vector>
+#include 'mxnet/ndarray.h'
+    
+    // C callback that can be used by TVM to extract
+// the WrapAsyncCall function.
+extern 'C' MXNET_DLL int MXTVMBridge(TVMFunctionHandle pregister) {
+  using tvm::runtime::PackedFunc;
+  const PackedFunc& fregister =
+      *static_cast<PackedFunc*>(pregister);
+  fregister('WrapAsyncCall', PackedFunc(mxnet::WrapAsyncCall));
+  return 0;
 }
+
     
-    void b2Triangle::Set(const b2Triangle& toMe) {
-	for (int32 i=0; i<3; ++i) {
-		x[i] = toMe.x[i];
-		y[i] = toMe.y[i];
-	}
-}
+      ~DHTRoutingTable();
     
-    		inline bool GetFlip(void)
-		{
-			return m_pencoding->GetFlip();
-		}
+        taskFactory->setLocalNode(localNode);
+    taskFactory->setRoutingTable(routingTable.get());
+    taskFactory->setMessageDispatcher(dispatcher.get());
+    taskFactory->setMessageFactory(factory.get());
+    taskFactory->setTaskQueue(taskQueue.get());
+    taskFactory->setTimeout(std::chrono::seconds(messageTimeout));
     
-    		virtual bool IsDifferential(void) = 0;
-    
-    	// ################################################################################
-	// Block4x4EncodingBits_A8
-	// Encoding bits for the A portion of RGBA8
-	// ################################################################################
-    
-    
-  typedef struct  AF_Blue_StringRec_
-  {
-    AF_Blue_String  string;
-    FT_UShort       properties;
+    class DHTTaskExecutor {
+private:
+  int numConcurrent_;
+  std::vector<std::shared_ptr<DHTTask>> execTasks_;
+  std::deque<std::shared_ptr<DHTTask>> queue_;
     }
     
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
+    #include 'common.h'
     
-    /** 16x16 multiplication where the result fits in 32 bits */
-#undef MULT16_16
-static OPUS_INLINE opus_val32 MULT16_16_armv5e(opus_val16 a, opus_val16 b)
-{
-  int res;
-  __asm__(
-      '#MULT16_16\n\t'
-      'smulbb %0, %1, %2;\n'
-      : '=r'(res)
-      : 'r'(a), 'r'(b)
-  );
-  return res;
+      _InternalExtensionInfo__isset __isset;
+    
+      std::string content;
+  auto status = readFile('/etc/exports', content);
+  if (!status.ok()) {
+    VLOG(1) << 'Error reading /etc/exports: ' << status.toString();
+    return {};
+  }
+    
+    // A global char* and an SQL function to access its current value
+// from within an SQL statement. This program used to use the
+// sqlite_exec_printf() API to substitute a string into an SQL statement.
+// The correct way to do this with sqlite3 is to use the bind API, but
+// since the shell is built around the callback paradigm it would be a lot
+// of work. Instead just use this hack, which is quite harmless.
+static const char* zShellStatic = nullptr;
+void shellstaticFunc(sqlite3_context* context,
+                     int argc,
+                     sqlite3_value** /* argv */) {
+  (void)argc;
+  assert(0 == argc);
+  assert(zShellStatic);
+  sqlite3_result_text(context, zShellStatic, -1, SQLITE_STATIC);
 }
-#define MULT16_16(a, b) (MULT16_16_armv5e(a, b))
+    
+    #include <osquery/utils/expected/expected.h>
+#include <osquery/utils/system/posix/errno.h>
