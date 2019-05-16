378d365c04fa9634582772b491b06c2478f3ccc1
SUNsung <weatherius@gmail.com>
2019-05-16 04:36:16 +0300

    Rewrite [33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb] File in: 2019-05-16 04:36:16

--- ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
+++ ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
@@ -1,133 +1,146 @@
 
         
-                css('.openParens').each do |node|
-          node.parent.name = 'pre'
-          node.parent.content = node.parent.css('code, pre').map(&:content).join('\n')
-        end
-    
-            css('.nav-index-section').each do |node|
-          node.content = node.content
-        end
-    
-    class Api::OEmbedController < Api::BaseController
-  respond_to :json
-    
-      private
-    
-      def update
-    if subscription.verify(body, request.headers['HTTP_X_HUB_SIGNATURE'])
-      ProcessingWorker.perform_async(@account.id, body.force_encoding('UTF-8'))
-    end
-    
-        @web_subscription.update!(data: data_params)
-    
-      Devise.omniauth_configs.each_key do |provider|
-    provides_callback_for provider
+          it 'requires a URL or file uplaod' do
+    visit new_scenario_imports_path
+    click_on 'Start Import'
+    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
   end
     
-      # body
-  xml.tag!('body') do
-    xml.tag!('outline', text: TITLE, title: TITLE) do
-      blogs.each do |blog|
-        xml.tag!('outline', type: 'rss', text: blog.name, title: blog.name,
-          xmlUrl: blog.rss_url, htmlUrl: blog.web_url)
-      end
+        it 'can not be turned off' do
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'true' }
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
     end
   end
 end
-    
-    World(RemoteCommandHelpers)
 
     
-            # Note that `scm_plugin_installed?` comes from Capistrano::DSL
-        if scm_plugin_installed?
-          delete(:scm)
-          return
-        end
+          expect(Utils.unindent(<<-MD)).to eq('Hello World\n  This is\n  indented\nthough')
+        Hello World
+          This is
+          indented
+        though
+      MD
     
-            if callable.is_a?(Question)
-          ValidatedQuestion.new(validation_callback)
-        else
-          validation_callback
-        end
+        it 'should provide the since attribute after the first run' do
+      time = (Time.now-1.minute).iso8601
+      @checker.memory[:last_event] = time
+      @checker.save
+      expect(@checker.reload.send(:query_parameters)).to eq({:query => {:since => time}})
+    end
+  end
+    
+        def add_entry(entry)
+      if @index.add?(entry.as_json.to_s)
+        @entries << entry.dup
+        @types[entry.type].count += 1 if entry.type
       end
+    end
     
-    require 'bundler/cli'
-require 'bundler/friendly_errors'
+        def replace(index, name)
+      @filters[assert_index(index)] = filter_const(name)
+    end
     
-    class LogStash::PluginManager::Update < LogStash::PluginManager::Command
-  REJECTED_OPTIONS = [:path, :git, :github]
-  # These are local gems used by LS and needs to be filtered out of other plugin gems
-  NON_PLUGIN_LOCAL_GEMS = ['logstash-core', 'logstash-core-plugin-api']
+    module Docs
+  class Entry
+    class Invalid < StandardError; end
     
-    def attach_attachment(name, definition = nil)
-  snippet = 'has_attached_file :#{name}'
-  if definition
-    snippet += ', \n'
-    snippet += definition
-  end
-  snippet += '\ndo_not_validate_attachment_file_type :#{name}\n'
-  cd('.') do
-    transform_file('app/models/user.rb') do |content|
-      content.sub(/end\Z/, '#{snippet}\nend')
+        private
+    
+        def mime_type
+      headers['Content-Type'] || 'text/plain'
+    end
+    
+    module Docs
+  class Subscriber < ActiveSupport::Subscriber
+    cattr_accessor :namespace
+    
+            if mod
+          if name == 'Index'
+            return slug.split('/')[1..-2].join('/')
+          elsif name == 'Angular'
+            return slug.split('/').last.split('-').first
+          end
+        end
+    
+        def resource_params
+      params.require(:email_domain_block).permit(:domain)
     end
   end
 end
-    
-    When /^(?:|I )attach the file '([^']*)' to '([^']*)'$/ do |path, field|
-  attach_file(field, File.expand_path(path))
-end
-    
-    class PaperclipGenerator < ActiveRecord::Generators::Base
-  desc 'Create a migration to add paperclip-specific fields to your model. ' +
-       'The NAME argument is the name of your model, and the following ' +
-       'arguments are the name of the attachments'
-    
-        # True if the dimensions represent a vertical rectangle
-    def vertical?
-      height > width
-    end
-    
-      included do
-    helper_method :organization
-    before_action :add_organization_to_page_title
-  end
-    
-      def create
-    scope = @server ? @server.domains : organization.domains
-    @domain = scope.build(params.require(:domain).permit(:name, :verification_method))
-    
-      def new
-    @http_endpoint = @server.http_endpoints.build
-  end
-    
-      def create
-    @ip_pool = IPPool.new(safe_params)
-    if @ip_pool.save
-      redirect_to_with_json [:edit, @ip_pool], :notice => 'IP Pool has been added successfully. You can now add IP addresses to it.'
-    else
-      render_form_errors 'new', @ip_pool
-    end
-  end
-    
-      def remove_from_queue
-    if @message.queued_message && !@message.queued_message.locked?
-      @message.queued_message.destroy
-    end
-    redirect_to_with_json organization_server_message_path(organization, @server, @message.id)
-  end
+
     
       private
     
-      def create
-    @user_invite = UserInvite.active.find_by!(:uuid => params[:invite_token])
-    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email_address, :password, :password_confirmation))
-    @user.email_verified_at = Time.now
-    if @user.save
-      @user_invite.accept(@user)
-      self.current_user = @user
-      redirect_to root_path
-    else
-      render 'new', :layout => 'sub'
+        unless active_session.web_push_subscription.nil?
+      active_session.web_push_subscription.destroy!
+      active_session.update!(web_push_subscription: nil)
     end
+    
+    module ExportControllerConcern
+  extend ActiveSupport::Concern
+    
+      def set_session_activity
+    return unless session_needs_update?
+    current_session.touch
   end
+    
+            # Receives a kerberos response through the connection
+        #
+        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] the kerberos
+        #   response message
+        # @raise [RuntimeError] if the connection isn't established, the transport protocol is unknown, not supported
+        #   or the response can't be parsed
+        # @raise [NotImplementedError] if the transport protocol isn't supported
+        def recv_response
+          if connection.nil?
+            raise ::RuntimeError, 'Kerberos Client: connection not established'
+          end
+    
+              # Rex::Proto::Kerberos::Model::ApReq decoding isn't supported
+          #
+          # @raise [NotImplementedError]
+          def decode(input)
+            raise ::NotImplementedError, 'AP-REQ decoding not supported'
+          end
+    
+              # Encodes the type
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_type(type)
+            bn = OpenSSL::BN.new(type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+              # Decrypts the cipher with etype encryption schema
+          #
+          # @param key [String] the key to decrypt
+          # @param msg_type [Integer] the message type
+          # @return [String] the decrypted `cipher`
+          # @raise [RuntimeError] if decryption doesn't succeed
+          # @raise [NotImplementedError] if encryption isn't supported
+          def decrypt(key, msg_type)
+            if cipher.nil? || cipher.empty?
+              return ''
+            end
+    
+              # Decodes the cname field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Rex::Proto::Kerberos::Model::PrincipalName]
+          def decode_cname(input)
+            Rex::Proto::Kerberos::Model::PrincipalName.decode(input.value[0])
+          end
+    
+        system_command '#{staged_path}/AdobePatchInstaller.app/Contents/MacOS/AdobePatchInstaller',
+                   args: [
+                           '--mode=silent',
+                         ],
+                   sudo: true
+  end
+    
+      puts '== Installing dependencies =='
+  system! 'gem install bundler --conservative'
+  system('bundle check') || system!('bundle install')
+    
+      # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = false
