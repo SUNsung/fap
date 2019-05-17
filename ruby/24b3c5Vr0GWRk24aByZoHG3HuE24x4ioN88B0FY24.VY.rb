58c79ba71bad4808f1456dcf268498b5cfc7c847
SUNsung <weatherius@gmail.com>
2019-05-17 16:49:11 +0300

    Rewrite [24b3c5Vr0GWRk24aByZoHG3HuE24x4ioN88B0FY24.VY.rb] File in: 2019-05-17 16:49:11

--- ruby/24b3c5Vr0GWRk24aByZoHG3HuE24x4ioN88B0FY24.VY.rb
+++ ruby/24b3c5Vr0GWRk24aByZoHG3HuE24x4ioN88B0FY24.VY.rb
@@ -1,105 +1,55 @@
 
         
-        DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
+            head + [data.length].pack('v') + data
+  end
     
-    Jekyll::PluginManager.require_from_bundler
     
-            def handle_websockets_event(websocket)
-          websocket.onopen { |handshake| connect(websocket, handshake) }
-          websocket.onclose { disconnect(websocket) }
-          websocket.onmessage { |msg| print_message(msg) }
-          websocket.onerror { |error| log_error(error) }
-        end
+          private
     
-        def process(args)
-      arg_is_present? args, '--server', 'The --server command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
-                          use the 'build' subcommand.'
-      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
-                          '--watch'.'
-      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
-                          the '--watch' switch.'
-      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
-                          favour of 'highlighter'.'
-      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
-                          your config files.'
-      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
-                          config files.'
-      no_subcommand(args)
-    end
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of an encrypted message.
+        class EncryptedData < Element
+          # @!attribute name_type
+          #   @return [Integer] The encryption algorithm
+          attr_accessor :etype
+          # @!attribute kvno
+          #   @return [Integer] The version number of the key
+          attr_accessor :kvno
+          # @!attribute cipher
+          #   @return [String] The enciphered text
+          attr_accessor :cipher
     
-          def fallback_data
-        @fallback_data ||= {}
-      end
-    end
+              # Decodes the value from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [String]
+          def decode_value(input)
+            input.value[0].value
+          end
+    
+              # Decodes the ticket field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Rex::Proto::Kerberos::Type::Ticket]
+          def decode_ticket(input)
+            Rex::Proto::Kerberos::Model::Ticket.decode(input.value[0])
+          end
+    
+      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
+  gem.executables   = %w(cap capify)
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ['lib']
+    
+      def test_symlink_exists(path)
+    exists?('L', path)
+  end
+    
+      def run_vagrant_command(command)
+    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
+    return [stdout, stderr] if status.success?
+    raise VagrantSSHCommandError, status
   end
 end
-
-    
-    (allow file-read-metadata)
-(allow file-read*
-  ; This is currenly only added because using `xcodebuild` to build a resource
-  ; bundle target starts a FSEvents stream on `/`. No idea why that would be
-  ; needed, but for now it doesn’t seem like a real problem.
-  (literal '/')
-  (regex
-    ; TODO see if we can restrict this more, but it's going to be hard
-    #'^/Users/[^.]+/*'
-    ;#'^/Users/[^.]+/.netrc'
-    ;#'^/Users/[^.]+/.gemrc'
-    ;#'^/Users/[^.]+/.gem/*'
-    ;#'^/Users/[^.]+/Library/.*'
-    #'^/Library/*'
-    #'^/System/Library/*'
-    #'^/usr/lib/*'
-    #'^/usr/share/*'
-    #'^/private/*'
-    #'^/dev/*'
-    #'^<%= ruby_prefix %>'
-    #'^<%= pod_prefix %>'
-    #'^<%= xcode_app_path %>'
-    #'^<%= Pod::Config.instance.repos_dir %>'
-<% prefixes.each do |prefix| %>
-    #'^<%= prefix %>/*'
-<% end %>
-  )
-)
-    
-          def main_sidebar_classes
-        if cookies['sidebar-minimized'] == 'true'
-          'col-3 col-md-2 sidebar'
-        else
-          'p-0 col-3 col-md-2 sidebar'
-        end
-      end
-    
-        it 'does not display forbidden links' do
-      visit spree.edit_admin_order_path(order)
-    
-          within_row(2) { click_icon :split }
-      targetted_select2 'LA(#{order.reload.shipments.last.number})', from: '#s2id_item_stock_location'
-      click_icon :save
-      wait_for_ajax
-      expect(page.find('#shipment_#{order.reload.shipments.last.id}')).to be_present
-    end
-  end
-end
-
-    
-              if Spree::Dependencies.cart_update_service.constantize.call(order: @order, params: line_items_attributes).success?
-            @line_item.reload
-            respond_with(@line_item, default_template: :show)
-          else
-            invalid_resource!(@line_item)
-          end
-        end
-    
-            def option_types_params
-          params[:product].fetch(:option_types, [])
-        end
-    
-                render_order(result)
-          end
