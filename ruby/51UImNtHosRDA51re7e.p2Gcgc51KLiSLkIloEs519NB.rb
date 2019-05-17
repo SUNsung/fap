8655b0c1a4a87c896bc3925f40f7f8a9e4efc9b1
SUNsung <weatherius@gmail.com>
2019-05-17 17:31:47 +0300

    Rewrite [51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb] File in: 2019-05-17 17:31:46

--- ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
+++ ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
@@ -1,96 +1,85 @@
 
         
-                # Executes a command on the remote machine with administrative
-        # privileges. See {#execute} for documentation, as the API is the
-        # same.
-        #
-        # @see #execute
-        def sudo(command, opts=nil)
-        end
+            it 'allows to delete a user' do
+      visit admin_users_path
+      find(:css, 'a[href='/admin/users/#{users(:bob).id}']').click
+      expect(page).to have_text('User 'bob' was deleted.')
+      expect(page).to have_no_text('bob@example.com')
+    end
     
-            # This is called to upgrade this V1 config to V2. The parameter given
-        # is the full V2 configuration object, so you can do anything to it
-        # that you want.
-        #
-        # No return value is expected, modifications should be made directly
-        # to the new V2 object.
-        #
-        # @param [V2::Root] new
-        def upgrade(new)
-        end
+        it 'shows all options for agents that can be scheduled, create and receive events' do
+      select_agent_type('Website Agent scrapes')
+      expect(page).not_to have_content('This type of Agent cannot create events.')
+    end
     
-              # Return the registry
-          data[:providers]
-        end
-    
-      private
-    
-      def show
-    if subscription.valid?(params['hub.topic'])
-      @account.update(subscription_expires_at: future_expires)
-      render plain: encoded_challenge, status: 200
-    else
-      head 404
+        it 'returns a correct icon tag for other services' do
+      icon = omniauth_provider_icon(:'37signals')
+      expect(icon).to be_html_safe
+      elem = Nokogiri(icon).at('i.fa.fa-lock')
+      expect(elem).to be_a Nokogiri::XML::Element
     end
   end
     
-      def subscription_params
-    params.require(:subscription).permit(:endpoint, keys: [:auth, :p256dh])
-  end
-    
-      private
-    
-      # Setup the root logger with the Rails log level and the desired set of
-  # appenders. The list of appenders to use should be set in the environment
-  # specific configuration file.
-  #
-  # For example, in a production application you would not want to log to
-  # STDOUT, but you would want to send an email for 'error' and 'fatal'
-  # messages:
-  #
-  # => config/environments/production.rb
-  #
-  #     config.log_to = %w[file email]
-  #
-  # In development you would want to log to STDOUT and possibly to a file:
-  #
-  # => config/environments/development.rb
-  #
-  #     config.log_to = %w[stdout file]
-  #
-  Logging.logger.root.appenders = config.log_to unless config.log_to.empty?
-    
-        it 'generates the contacts_json fixture', :fixture => true do
-      json = bob.contacts.map { |c|
-               ContactPresenter.new(c, bob).full_hash_with_person
-             }.to_json
-      save_fixture(json, 'contacts_json')
-    end
-  end
-end
-
-    
-    describe StatusMessagesController, :type => :controller do
-  describe '#bookmarklet' do
-    before do
-      sign_in bob, scope: :user
+      describe '#relative_distance_of_time_in_words' do
+    it 'in the past' do
+      expect(relative_distance_of_time_in_words(Time.now-5.minutes)).to eq('5m ago')
     end
     
-    SPREE_GEMS = %w(core api cmd backend frontend sample).freeze
+      describe 'down' do
+    let :valid_options do
+      super().merge('extract' => new_extract,
+                    'template' => new_template)
+    end
     
-              def serialize_order(order)
-            resource_serializer.new(order.reload, include: resource_includes, fields: sparse_fields).serializable_hash
-          end
+      it 'replaces invalid byte sequences in a message' do
+    log = AgentLog.new(:agent => agents(:jane_website_agent), level: 3)
+    log.message = '\u{3042}\xffA\x95'
+    expect { log.save! }.not_to raise_error
+    expect(log.message).to eq('\u{3042}<ff>A\<95>')
+  end
     
-            def new; end
+      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
+  gem.executables   = %w(cap capify)
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ['lib']
     
-          def variant_attributes
-        if @current_user_roles&.include?('admin')
-          @@variant_attributes + [:cost_price]
+      def test_file_exists(path)
+    exists?('f', path)
+  end
+    
+        [stdout, stderr, status]
+  end
+    
+            @response = (gets || '').chomp
+      end
+    
+    http://capistranorb.com/documentation/advanced-features/custom-scm
+    
+          def each
+        servers_by_key.values.each { |server| yield server }
+      end
+    
+          # Returns an array of source file location(s) where the given key was
+      # assigned (i.e. where `set` was called). If the key was never assigned,
+      # returns `nil`.
+      def source_locations(key)
+        locations[key]
+      end
+    
+        def pane_index
+      index + tab.project.pane_base_index
+    end
+    
+    # Custom Matchers
+require_relative 'matchers/pane_matcher'
+    
+          def find_project_file(name, local = false)
+        path = config_path(name, local)
+        if File.exist?(path)
+          path
         else
-          @@variant_attributes
+          generate_project_file(name, path)
         end
       end
-    end
-  end
-end
+    
+        Make sure that you've set these variables in your ENV:
