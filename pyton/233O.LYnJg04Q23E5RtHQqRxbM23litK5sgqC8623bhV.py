f7de3a8ee2767975e612e0c386a62b6f0a4f8967
SUNsung <weatherius@gmail.com>
2019-05-17 07:09:00 +0300

    Rewrite [233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py] File in: 2019-05-17 07:08:59

--- pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
+++ pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
@@ -1,57 +1,157 @@
 
         
-            def test_error_content_length(self):
-        # Issue #16088: standard error responses should have a content-length
-        self.con.request('NOTFOUND', '/')
-        res = self.con.getresponse()
-        self.assertEqual(res.status, HTTPStatus.NOT_FOUND)
+        
+@deconstructible
+class KeysValidator:
+    '''A validator designed for HStore to require/restrict keys.'''
     
-            expected = [loader.suiteClass([MyTestCase('test')])]
-        self.assertEqual(list(suite), expected)
+        def create(self):
+        # Because a cache can fail silently (e.g. memcache), we don't know if
+        # we are failing to create a new session because of a key collision or
+        # because the cache is missing. So we try for a (large) number of times
+        # and then raise an exception. That's the risk you shoulder if using
+        # cache backing.
+        for i in range(10000):
+            self._session_key = self._get_new_session_key()
+            try:
+                self.save(must_create=True)
+            except CreateError:
+                continue
+            self.modified = True
+            return
+        raise RuntimeError(
+            'Unable to create a new session key. '
+            'It is likely that the cache is unavailable.')
     
-    class BaseRotatingHandler(logging.FileHandler):
-    '''
-    Base class for handlers that rotate log files at a certain point.
-    Not meant to be instantiated directly.  Instead, use RotatingFileHandler
-    or TimedRotatingFileHandler.
-    '''
-    def __init__(self, filename, mode, encoding=None, delay=False):
+        def delete(self, session_key=None):
         '''
-        Use the specified filename for streamed logging
+        To delete, clear the session key and the underlying data structure
+        and set the modified flag so that the cookie is set on the client for
+        the current request.
         '''
-        logging.FileHandler.__init__(self, filename, mode, encoding, delay)
-        self.mode = mode
-        self.encoding = encoding
-        self.namer = None
-        self.rotator = None
+        self._session_key = ''
+        self._session_cache = {}
+        self.modified = True
+    
+        def build_widget_attrs(self, attrs, widget=None):
+        widget = widget or self.field.widget
+        attrs = dict(attrs)  # Copy attrs to avoid modifying the argument.
+        if widget.use_required_attribute(self.initial) and self.field.required and self.form.use_required_attribute:
+            attrs['required'] = True
+        if self.field.disabled:
+            attrs['disabled'] = True
+        return attrs
+    
+            request_line = '{method} {path}{query} HTTP/1.1'.format(
+            method=self._orig.method,
+            path=url.path or '/',
+            query='?' + url.query if url.query else ''
+        )
+    
+        def __init__(self, **kwargs):
+        '''
+        :param env: an class:`Environment` instance
+        :param kwargs: additional keyword argument that some
+                       processor might require.
+    
+        config['implicit_content_type'] = 'json'
+    config.save()
+    config.load()
+    assert 'implicit_content_type' not in config
+    assert not config['default_options']
+    
+        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
+                                   'blob/{revision}/{package}/'
+                                   '{path}#L{lineno}')
+    '''
+    revision = _get_git_revision()
+    return partial(_linkcode_resolve, revision=revision, package=package,
+                   url_fmt=url_fmt)
+
+    
+        output_file.close()
+
+    
+    These images how similar features are merged together using
+feature agglomeration.
+'''
+print(__doc__)
+    
+    An illustration of various linkage option for agglomerative clustering on
+a 2D embedding of the digits dataset.
+    
+    import numpy as np
+import matplotlib.pyplot as plt
+    
+                    # Read the new file to memory
+                with open(_filename) as _file:
+                    _contents = _file.read()
+    
+            for spidername in args or spider_loader.list():
+            spidercls = spider_loader.load(spidername)
+            spidercls.start_requests = lambda s: conman.from_spider(s, result)
+    
+        def run(self, args, opts):
+        settings = self.crawler_process.settings
+        if opts.get:
+            s = settings.get(opts.get)
+            if isinstance(s, BaseSettings):
+                print(json.dumps(s.copy_to_dict()))
+            else:
+                print(s)
+        elif opts.getbool:
+            print(settings.getbool(opts.getbool))
+        elif opts.getint:
+            print(settings.getint(opts.getint))
+        elif opts.getfloat:
+            print(settings.getfloat(opts.getfloat))
+        elif opts.getlist:
+            print(settings.getlist(opts.getlist))
+
+    
+            ajax_crawl_request.meta['ajax_crawlable'] = True
+        return ajax_crawl_request
+    
+        def _debug_set_cookie(self, response, spider):
+        if self.debug:
+            cl = [to_native_str(c, errors='replace')
+                  for c in response.headers.getlist('Set-Cookie')]
+            if cl:
+                cookies = '\n'.join('Set-Cookie: {}\n'.format(c) for c in cl)
+                msg = 'Received cookies from: {}\n{}'.format(response, cookies)
+                logger.debug(msg, extra={'spider': spider})
+    
+    import urllib
+    
+    	xml = get_html('http://www.ehow.com/services/video/series.xml?demand_ehow_videoid=%s' % vid)
+    
+	from xml.dom.minidom import parseString
+	doc = parseString(xml)
+	tab = doc.getElementsByTagName('related')[0].firstChild
+    
+    from ..common import *
+from ..extractor import VideoExtractor
+from .universal import *
+    
+        # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
     
     
-# Does a path exist?
-# This is false for dangling symbolic links on systems that support them.
-def exists(path):
-    '''Test whether a path exists.  Returns False for broken symbolic links'''
-    try:
-        os.stat(path)
-    except (OSError, ValueError):
-        return False
-    return True
+def test_image(image_to_check, model):
+    unknown_image = face_recognition.load_image_file(image_to_check)
+    face_locations = face_recognition.face_locations(unknown_image, number_of_times_to_upsample=0, model=model)
     
-    --Pepé
-''')
+        :param img: An image (as a numpy array)
+    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
+    :param model: Which face detection model to use. 'hog' is less accurate but faster on CPUs. 'cnn' is a more accurate
+                  deep-learning model which is GPU/CUDA accelerated (if available). The default is 'hog'.
+    :return: A list of dlib 'rect' objects of found face locations
+    '''
+    if model == 'cnn':
+        return cnn_face_detector(img, number_of_times_to_upsample)
+    else:
+        return face_detector(img, number_of_times_to_upsample)
     
-            print('Empty input')
-        assert_equal(func([]), [])
-    
-        # TODO: It would be better if we had unit tests for each
-    # method in addition to the following end-to-end test
-    def test_end_to_end(self):
-        print('Test: Empty stack')
-        stack = Stack()
-        assert_equal(stack.peek(), None)
-        assert_equal(stack.pop(), None)
-    
-        def __repr__(self):
-        return str(self.obj) + ': ' + str(self.key)
-    
-    for i in range(0, 21050, 1000):
-    open('ci.song.%s.json' % i, 'w').write(json.dumps(cis[i:i+1000], indent=2, ensure_ascii=False))
+        # Quit
+    cv2.destroyAllWindows()
