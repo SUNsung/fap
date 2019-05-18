cfa79b5129fbba129399d07b207774ab7b27786b
SUNsung <weatherius@gmail.com>
2019-05-18 08:44:43 +0300

    Rewrite [41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py] File in: 2019-05-18 08:44:42

--- pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
+++ pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
@@ -1,132 +1,136 @@
 
         
-          # Note the use of get_variable vs. tf.Variable.  this is because get_variable
-  # does not allow the initialization of the variable with a value.
-  if normalized:
-    w_collections = [tf.GraphKeys.GLOBAL_VARIABLES, 'norm-variables']
-    if collections:
-      w_collections += collections
-    if mat_init_value is not None:
-      w = tf.Variable(mat_init_value, name=wname, collections=w_collections,
-                      trainable=trainable)
-    else:
-      w = tf.get_variable(wname, [in_size, out_size], initializer=mat_init,
-                          collections=w_collections, trainable=trainable)
-    w = tf.nn.l2_normalize(w, dim=0) # x W, so xW_j = \sum_i x_bi W_ij
-  else:
-    w_collections = [tf.GraphKeys.GLOBAL_VARIABLES]
-    if collections:
-      w_collections += collections
-    if mat_init_value is not None:
-      w = tf.Variable(mat_init_value, name=wname, collections=w_collections,
-                      trainable=trainable)
-    else:
-      w = tf.get_variable(wname, [in_size, out_size], initializer=mat_init,
-                          collections=w_collections, trainable=trainable)
-  b = None
-  if do_bias:
-    b_collections = [tf.GraphKeys.GLOBAL_VARIABLES]
-    if collections:
-      b_collections += collections
-    bname = (name + '/b') if name else '/b'
-    if bias_init_value is None:
-      b = tf.get_variable(bname, [1, out_size],
-                          initializer=tf.zeros_initializer(),
-                          collections=b_collections,
-                          trainable=trainable)
-    else:
-      b = tf.Variable(bias_init_value, name=bname,
-                      collections=b_collections,
-                      trainable=trainable)
+        def main():
+    # First, we load the current README into memory as an array of lines
+    with open('README.md', 'r') as read_me_file:
+        read_me = read_me_file.readlines()
     
-        return out
-    
-                self._thread_local.num_401_calls += 1
-            pat = re.compile(r'digest ', flags=re.IGNORECASE)
-            self._thread_local.chal = parse_dict_header(pat.sub('', s_auth, count=1))
-    
-        def __getitem__(self, name):
-        '''Dict-like __getitem__() for compatibility with client code. Throws
-        exception if there are more than one cookie with name. In that case,
-        use the more explicit get() method instead.
-    
-        return {'name': implementation, 'version': implementation_version}
-    
-        # Redirection.
-    300: ('multiple_choices',),
-    301: ('moved_permanently', 'moved', '\\o-'),
-    302: ('found',),
-    303: ('see_other', 'other'),
-    304: ('not_modified',),
-    305: ('use_proxy',),
-    306: ('switch_proxy',),
-    307: ('temporary_redirect', 'temporary_moved', 'temporary'),
-    308: ('permanent_redirect',
-          'resume_incomplete', 'resume',),  # These 2 to be removed in 3.0
+        def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
     
     
-def merge(to_release, from_releases, sentry_models):
-    # The following models reference release:
-    # ReleaseCommit.release
-    # ReleaseEnvironment.release_id
-    # ReleaseProject.release
-    # GroupRelease.release_id
-    # GroupResolution.release
-    # Group.first_release
-    # ReleaseFile.release
+def create_app(test_config=None):
+    '''Create and configure an instance of the Flask application.'''
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY='dev',
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    
+    from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+    
+            return cls(self, **kwargs)
     
     
-class Migration(DataMigration):
-    def forwards(self, orm):
-        'Write your forwards methods here.'
-        db.commit_transaction()
+def explain_template_loading_attempts(app, template, attempts):
+    '''This should help developers understand what failed'''
+    info = ['Locating template '%s':' % template]
+    total_found = 0
+    blueprint = None
+    reqctx = _request_ctx_stack.top
+    if reqctx is not None and reqctx.request.blueprint is not None:
+        blueprint = reqctx.request.blueprint
     
-            # Adding model 'RawEvent'
-        db.create_table(
-            'sentry_rawevent', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'project', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.Project']
-                    )
-                ), (
-                    'event_id',
-                    self.gf('django.db.models.fields.CharField')(max_length=32, null=True)
-                ), (
-                    'datetime',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-                ('data', self.gf('sentry.db.models.fields.node.NodeField')(null=True, blank=True)),
-            )
-        )
-        db.send_create_signal('sentry', ['RawEvent'])
+    This typically means that you attempted to use functionality that needed
+to interface with the current application object in some way. To solve
+this, set up an application context with app.app_context().  See the
+documentation for more information.\
+'''
+    
+            return list(result)
+    
+        # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    
+        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
+        loaded_model = keras.models.load_model(model_filename)
+        loaded_model.predict(np.random.rand(128, 2))
     
     
-class Migration(SchemaMigration):
-    def forwards(self, orm):
-        # Adding model 'VersionDSymFile'
-        db.create_table(
-            'sentry_versiondsymfile', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'dsym_file', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.ProjectDSymFile'], null=True
-                    )
-                ), (
-                    'dsym_app', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.DSymApp']
-                    )
-                ), ('version', self.gf('django.db.models.fields.CharField')(max_length=32)),
-                ('build', self.gf('django.db.models.fields.CharField')(max_length=32, null=True)), (
-                    'date_added',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['VersionDSymFile'])
+def create_multi_input_model_from(layer1, layer2):
+    input_1 = Input(shape=(data_dim,))
+    input_2 = Input(shape=(data_dim,))
+    out1 = layer1(input_1)
+    out2 = layer2(input_2)
+    out = Average()([out1, out2])
+    model = Model([input_1, input_2], out)
+    model.add_loss(K.mean(out2))
+    model.add_loss(1)
+    model.add_loss(1)
+    return model
+    
+        ```python
+    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
+    > labels
+    array([0, 2, 1, 2, 0])
+    # `to_categorical` converts this into a matrix with as many
+    # columns as there are classes. The number of rows
+    # stays the same.
+    > to_categorical(labels)
+    array([[ 1.,  0.,  0.],
+           [ 0.,  0.,  1.],
+           [ 0.,  1.,  0.],
+           [ 0.,  0.,  1.],
+           [ 1.,  0.,  0.]], dtype=float32)
+    ```
+    '''
+    
+    print('Building model...')
+model = Sequential()
+model.add(Dense(512, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+    
+    
+def test_conv_output_length():
+    assert conv_utils.conv_output_length(None, 7, 'same', 1) is None
+    assert conv_utils.conv_output_length(224, 7, 'same', 1) == 224
+    assert conv_utils.conv_output_length(224, 7, 'same', 2) == 112
+    assert conv_utils.conv_output_length(32, 5, 'valid', 1) == 28
+    assert conv_utils.conv_output_length(32, 5, 'valid', 2) == 14
+    assert conv_utils.conv_output_length(32, 5, 'causal', 1) == 32
+    assert conv_utils.conv_output_length(32, 5, 'causal', 2) == 16
+    assert conv_utils.conv_output_length(32, 5, 'full', 1) == 36
+    assert conv_utils.conv_output_length(32, 5, 'full', 2) == 18
+    
+    PROJ_METADATA = '%s.json' % PROJ_NAME
+    
+    from ..common import *
+import json
+    
+        res = yield from async_setup_component(hass, automation.DOMAIN, {
+        automation.DOMAIN: {
+            'alias': 'hello',
+            'trigger': {
+                'platform': 'homeassistant',
+                'event': 'shutdown',
+            },
+            'action': {
+                'service': 'test.automation',
+            }
+        }
+    })
+    assert res
+    assert not automation.is_on(hass, 'automation.hello')
+    assert len(calls) == 0
+    
+        with tf.variable_scope(name, reuse=reuse):
+        alpha = get_w(alpha_shape, w_initializer=alpha_init, name='alpha')
+        # o = relu(x) + 0.5 * tf.multiply(alpha, x - tf.abs(x))  # TFLearn
+        o = leaky_relu(x, alpha)  # TensorLayer / <Deep Learning>
+    
+        这里实际上没有用到 J 和 d 这个参数，保留是为了与 `attention_flow()` 的参数兼容
