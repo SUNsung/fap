202908fed5adbf28a9c0b74c18c7799ed6ac349d
SUNsung <weatherius@gmail.com>
2019-05-18 23:32:05 +0300

    Rewrite [50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py] File in: 2019-05-18 23:32:04

--- pyton/50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py
+++ pyton/50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py
@@ -1,84 +1,147 @@
 
         
-        # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+        import re
+import sys
     
-        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
-        loaded_model = keras.models.load_model(model_filename)
-        loaded_model.predict(np.random.rand(128, 2))
-    
-            # helper function
-        def get_tuple_shape(nb_channels):
-            result = list(state_shape)
-            if self.cell.data_format == 'channels_first':
-                result[1] = nb_channels
-            elif self.cell.data_format == 'channels_last':
-                result[3] = nb_channels
-            else:
-                raise KeyError
-            return tuple(result)
-    
-        # Max concurrency is limited by global CONCURRENT_REQUESTS setting
-    max_concurrent_requests = 8
-    # Requests per second goal
-    qps = None # same as: 1 / download_delay
-    download_delay = None
-    # time in seconds to delay server responses
-    latency = None
-    # number of slots to create
-    slots = 1
+        def logout(self):
+        return self._client.get('/auth/logout')
     
     
-class Command(ScrapyCommand):
+def test_author_required(app, client, auth):
+    # change the post author to another user
+    with app.app_context():
+        db = get_db()
+        db.execute('UPDATE post SET author_id = 2 WHERE id = 1')
+        db.commit()
     
+        def fake_init_db():
+        Recorder.called = True
     
-def _import_file(filepath):
-    abspath = os.path.abspath(filepath)
-    dirname, file = os.path.split(abspath)
-    fname, fext = os.path.splitext(file)
-    if fext != '.py':
-        raise ValueError('Not a Python source file: %s' % abspath)
-    if dirname:
-        sys.path = [dirname] + sys.path
-    try:
-        module = import_module(fname)
-    finally:
-        if dirname:
-            sys.path.pop(0)
-    return module
-    
-    
-class ScrapesContract(Contract):
-    ''' Contract to check presence of fields in scraped items
-        @scrapes page_name page_body
-    '''
-    
-    
-class S3DownloadHandler(object):
-    
-        @nonce.decoder
-    def nonce(value):  # pylint: disable=missing-docstring,no-self-argument
-        try:
-            return jose.decode_b64jose(value)
-        except jose.DeserializationError as error:
-            # TODO: custom error
-            raise jose.DeserializationError('Invalid nonce: {0}'.format(error))
-    
-    # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
-    
-    '''
+    # This was the only thing that Flask used to export at one point and it had
+# a more generic name.
+jsonify = json.jsonify
 
     
-    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url)
-    title = r1(r'<title>(.*)</title>', html)
-    assert title
-    id = r1(r'flvid\s*=\s*(\d+)', html)
-    assert id
-    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
+            :param filename: the filename of the config.  This can either be an
+                         absolute filename or a filename relative to the
+                         root path.
+        :param silent: set to ``True`` if you want silent failure for missing
+                       files.
     
-    site_info = 'CBS.com'
-download = cbs_download
-download_playlist = playlist_not_supported('cbs')
+        class _FakeSignal(object):
+        '''If blinker is unavailable, create a fake class with the same
+        interface that allows sending of signals but will fail with an
+        error on anything else.  Instead of doing anything on send, it
+        will just ignore the arguments and do nothing instead.
+        '''
+    
+            for blueprint in self.app.iter_blueprints():
+            loader = blueprint.jinja_loader
+            if loader is not None:
+                for template in loader.list_templates():
+                    result.add(template)
+    
+        output = []
+    Stream = get_stream_type(env, args)
+    
+    USERNAME = 'user'
+PASSWORD = 'password'
+# Basic auth encoded `USERNAME` and `PASSWORD`
+# noinspection SpellCheckingInspection
+BASIC_AUTH_HEADER_VALUE = 'Basic dXNlcjpwYXNzd29yZA=='
+BASIC_AUTH_URL = '/basic-auth/{0}/{1}'.format(USERNAME, PASSWORD)
+AUTH_OK = {'authenticated': True, 'user': USERNAME}
+    
+        config['implicit_content_type'] = 'form'
+    config.save()
+    config.load()
+    assert 'implicit_content_type' not in config
+    assert config['default_options'] == ['--form']
+    
+    error_msg = None
+    
+    
+def test_unicode_url_query_arg_item(httpbin):
+    r = http(httpbin.url + '/get', u'test==%s' % UNICODE)
+    assert HTTP_OK in r
+    assert r.json['args'] == {'test': UNICODE}, r
+    
+    # Note: This isn't exactly the same as a 'percent match'. The scale isn't linear. But you can assume that images with a
+# smaller distance are more similar to each other than ones with a larger distance.
+    
+        # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
+    
+        # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
+
+    
+    
+if __name__ == '__main__':
+    main()
+
+    
+        # 将每一个人脸与已知样本图片比对
+    for face_encoding in face_encodings:
+        # 看是否属于奥巴马或者拜登
+        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        name = '<Unknown Person>'
+    
+        # Create workers
+    for worker_id in range(1, worker_num + 1):
+        p.append(Process(target=process, args=(worker_id, read_frame_list, write_frame_list)))
+        p[worker_id].start()
+    
+    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+    
+        # Draw a label with a name below the face
+    text_width, text_height = draw.textsize(name)
+    draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+    draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+    
+        def find_all_path(self, start, end, path=None):
+        path = path or []
+        path.append(start)
+        if start == end:
+            return [path]
+        paths = []
+        for node in self.graph.get(start, []):
+            if node not in path:
+                newpaths = self.find_all_path(node, end, path[:])
+                paths.extend(newpaths)
+        return paths
+    
+    try:
+    from unittest.mock import patch, call
+except ImportError:
+    from mock import patch, call
+    
+        def test_parents(self):
+        for _ in range(2):
+            self.assertEqual(self.John.parents, 'Father and mother')
+        self.assertEqual(self.John.call_count2, 1)
+
+    
+        fftv.publish('cartoon')
+    fftv.publish('music')
+    fftv.publish('ads')
+    fftv.publish('movie')
+    fftv.publish('cartoon')
+    fftv.publish('cartoon')
+    fftv.publish('movie')
+    fftv.publish('blank')
+    
+        def contribute(self):
+        self.blackboard.common_state['problems'] += random.randint(10, 20)
+        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
+        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
+        self.blackboard.common_state['progress'] += random.randint(10, 30)
+    
+        assert (cm1 == cm2) and (cm1 != cm3)
+    assert (cm1 is cm2) and (cm1 is not cm3)
+    assert len(instances_pool) == 2
