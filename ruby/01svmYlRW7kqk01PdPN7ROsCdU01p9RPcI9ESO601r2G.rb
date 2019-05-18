b58d5e61a562b199696d6b8ed969bb3f73bc052f
SUNsung <weatherius@gmail.com>
2019-05-18 05:07:39 +0300

    Rewrite [01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb] File in: 2019-05-18 05:07:39

--- ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
+++ ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
@@ -1,226 +1,161 @@
 
         
-        def gemspec_file
-  '#{name}.gemspec'
-end
+        protected
     
-    require 'benchmark/ips'
-require 'jekyll'
-require 'json'
     
-    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
-COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
-    
-      </body>
-</html>
-HTML
-CONTENT_NOT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-          it 'shellescapes the exclude_dirs correctly' do
-        directory = 'My Dir'
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: ['#{directory}'])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --exclude-dir #{directory.shellescape}')
-      end
-    
-        it 'shelljoins multiple args' do
-      message = 'A message'
-      command = command_from_args('git', 'commit', '-m', message)
-      expect(command).to eq('git commit -m #{message.shellescape}')
-    end
-    
-    class String
-  # CrossplatformShellwords
-  def shellescape
-    CrossplatformShellwords.shellescape(self)
-  end
-end
-    
-    # The * turns the array into a parameter list
-# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
-# We need to use that, because otherwise invocations like
-# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
-exec(*exec_arr)
-
-    
-            # attributes - A Hash containing the user details. The keys of this
-        #              Hash (and any nested hashes) must be symbols.
-        def initialize(attributes)
-          @attributes = attributes
+    {      # Add the item to the hash with logic to convert values to an array
+      # if so desired.
+      if (qstring.include?(var))
+        if (qstring[var].kind_of?(Array))
+          qstring[var] << val
+        else
+          curr = self.qstring[var]
+          qstring[var] = [ curr, val ]
         end
+      else
+        qstring[var] = val
       end
-    end
-  end
-end
-
+    }
     
-          def call(env)
-        transaction, retval = Transaction.run do
-          @app.call(env)
-        end
     
-      #
-  # Parses a header from a string.
   #
-  # XXX - Putting : in a header value breaks this badly
-  def from_s(header)
-    reset
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
+  }
     
-        check_100()
-  end
     
-        # If a procedure was passed, mount the resource with it.
-    if (opts['Proc'])
-      mount(name, Handler::Proc, false, opts['Proc'], opts['VirtualDirectory'])
-    else
-      raise ArgumentError, 'You must specify a procedure.'
-    end
-  end
-    
-        # Linear little-endian signed PCM is our native format
-    when IAX_CODEC_LINEAR_PCM
-      buff
-    
-    IAX_CTRL_HANGUP   = 1
-IAX_CTRL_RINGING  = 3
-IAX_CTRL_ANSWER   = 4
-IAX_CTRL_BUSY     = 5
-IAX_CTRL_PROGRESS = 14
-IAX_CTRL_PROCEED  = 15
-    
-      def self.create_ipmi_getchannel_probe
-    [   # Get Channel Authentication Capabilities
-      0x06, 0x00, 0xff, 0x07, # RMCP Header
-      0x00, 0x00, 0x00, 0x00,
-      0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x20, 0x18,
-      0xc8, 0x81, 0x00, 0x38, 0x8e, 0x04, 0xb5
+  # open rmcpplus_request with cipherzero
+  def self.create_ipmi_session_open_cipher_zero_request(console_session_id)
+    head = [
+      0x06, 0x00, 0xff, 0x07,   # RMCP Header
+      0x06,                     # RMCP+ Authentication Type
+      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
+      0x00, 0x00, 0x00, 0x00,   # Session ID
+      0x00, 0x00, 0x00, 0x00    # Sequence Number
     ].pack('C*')
-  end
     
-            # Decodes a Kerberos response
-        #
-        # @param data [String] the raw response message
-        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] the kerberos message response
-        # @raise [RuntimeError] if the response can't be processed
-        def decode_kerb_response(data)
-          asn1 = OpenSSL::ASN1.decode(data)
-          msg_type = asn1.value[0].value[1].value[0].value
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Crypto
-        module Rc4Hmac
-          # Decrypts the cipher using RC4-HMAC schema
-          #
-          # @param cipher [String] the data to decrypt
-          # @param key [String] the key to decrypt
-          # @param msg_type [Integer] the message type
-          # @return [String] the decrypted cipher
-          # @raise [RuntimeError] if decryption doesn't succeed
-          def decrypt_rc4_hmac(cipher, key, msg_type)
-            unless cipher && cipher.length > 16
-              raise ::RuntimeError, 'RC4-HMAC decryption failed'
-            end
-    
-                int
+                seq.to_der
           end
     
-              # Rex::Proto::Kerberos::Model::AuthorizationData decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'Authorization Data decoding not supported'
-          end
-    
-                res
-          end
-    
-              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
+              # Decodes the crealm field
           #
           # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_pvno(input)
-            input.value[0].value.to_i
+          # @return [String]
+          def decode_crealm(input)
+            input.value[0].value
           end
     
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
+      GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
+    file package(gem, '.gem') => ['pkg/', '#{directory + '/' + gem}.gemspec'] do |f|
+      sh 'cd #{directory} && gem build #{gem}.gemspec'
+      mv directory + '/' + File.basename(f.name), f.name
+    end
+    
+    
+    {    if (pre.style.display == 'block') {
+      pre.style.display = 'none';
+      post.style.display = 'none';
+      context.style.background = 'none';
+    } else {
+      pre.style.display = 'block';
+      post.style.display = 'block';
+      context.style.background = '#fffed9';
+    }
+  }
+    
+      # set version
+  content.sub! /(s\.version.*=\s+).*/, '\\1\'#{Rack::Protection::VERSION}\''
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   XSS and others
+    # Supported browsers:: Firefox 23+, Safari 7+, Chrome 25+, Opera 15+
+    #
+    # Description:: Content Security Policy, a mechanism web applications
+    #               can use to mitigate a broad class of content injection
+    #               vulnerabilities, such as cross-site scripting (XSS).
+    #               Content Security Policy is a declarative policy that lets
+    #               the authors (or server administrators) of a web application
+    #               inform the client about the sources from which the
+    #               application expects to load resources.
+    #
+    # More info::   W3C CSP Level 1 : https://www.w3.org/TR/CSP1/ (deprecated)
+    #               W3C CSP Level 2 : https://www.w3.org/TR/CSP2/ (current)
+    #               W3C CSP Level 3 : https://www.w3.org/TR/CSP3/ (draft)
+    #               https://developer.mozilla.org/en-US/docs/Web/Security/CSP
+    #               http://caniuse.com/#search=ContentSecurityPolicy
+    #               http://content-security-policy.com/
+    #               https://securityheaders.io
+    #               https://scotthelme.co.uk/csp-cheat-sheet/
+    #               http://www.html5rocks.com/en/tutorials/security/content-security-policy/
+    #
+    # Sets the 'Content-Security-Policy[-Report-Only]' header.
+    #
+    # Options: ContentSecurityPolicy configuration is a complex topic with
+    #          several levels of support that has evolved over time.
+    #          See the W3C documentation and the links in the more info
+    #          section for CSP usage examples and best practices. The
+    #          CSP3 directives in the 'NO_ARG_DIRECTIVES' constant need to be
+    #          presented in the options hash with a boolean 'true' in order
+    #          to be used in a policy.
+    #
+    class ContentSecurityPolicy < Base
+      default_options default_src: :none, script_src: ''self'',
+                      img_src: ''self'', style_src: ''self'',
+                      connect_src: ''self'', report_only: false
+    
+          def remove_bad_cookies(request, response)
+        return if bad_cookies.empty?
+        paths = cookie_paths(request.path)
+        bad_cookies.each do |name|
+          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
+        end
+      end
+    
+          def escape_string(str)
+        str = @escaper.escape_url(str)        if @url
+        str = @escaper.escape_html(str)       if @html
+        str = @escaper.escape_javascript(str) if @javascript
+        str
+      end
     end
   end
 end
 
     
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
+      it 'prevents ajax requests without a valid token' do
+    expect(post('/', {}, 'HTTP_X_REQUESTED_WITH' => 'XMLHttpRequest')).not_to be_ok
   end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
-  end
-end
     
-      describe '#new' do
-    before do
-      sign_in alice, scope: :user
+        it 'denies requests with sneaky encoded session cookies' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.%73ession=SESSION_TOKEN'
+      expect(last_response).not_to be_ok
     end
     
-      describe '#update' do
-    it 'marks a notification as read if it gets no other information' do
-      note = FactoryGirl.create(:notification)
-      expect(Notification).to receive(:where).and_return([note])
-      expect(note).to receive(:set_read_state).with(true)
-      get :update, params: {id: note.id}, format: :json
-    end
-    
-    describe ShareVisibilitiesController, :type => :controller do
-  before do
-    @status = alice.post(:status_message, :text => 'hello', :to => alice.aspects.first)
-  end
-    
-    
-    control_lines = control_contents.split('\n')
-    control_lines.each do |line|
-      key, val = line.split(/ += +/, 2)
-      if control.has_key? key
-        control[key].push(val)
-      else
-        control[key] = [val]
+          def upload_dest
+        @upload_dest
       end
-    end
     
-      # Input a PEAR package.
-  #
-  # The parameter is a PHP PEAR package name.
-  #
-  # Attributes that affect behavior here:
-  # * :prefix - changes the install root, default is /usr/share
-  # * :pear_package_name_prefix - changes the
-  def input(input_package)
-    if !program_in_path?('pear')
-      raise ExecutableNotFound.new('pear')
-    end
-    
-        begin
-      safesystem('#{attributes[:python_bin]} -c 'import pkg_resources'')
-    rescue FPM::Util::ProcessFailed => e
-      logger.error('Your python environment is missing a working setuptools module. I tried to find the 'pkg_resources' module but failed.', :python => attributes[:python_bin], :error => e)
-      raise FPM::Util::ProcessFailed, 'Python (#{attributes[:python_bin]}) is missing pkg_resources module.'
-    end
-    
-        abort 'FPM failed!' unless FPM::Command.new('fpm').run(args) == 0
+        page = @wiki.page('D')
+    assert page.nil?
   end
-end
