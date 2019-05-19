295f555fd4671168a1ad8b99d0fbc863d05240e8
SUNsung <weatherius@gmail.com>
2019-05-19 03:28:04 +0300

    Rewrite [08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb] File in: 2019-05-19 03:28:03

--- ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
+++ ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
@@ -1,74 +1,26 @@
 
         
-              test 'when specify to inherit and overwrite as an :only option which does not match current action name and is opposite from parent controller' do
-        controller = WithConditionalOverrideFlippedAndInheriting.new
-        controller.process(:non_overwritten)
-        assert_equal 'Overwrite Hello non overwritten!', controller.response_body
-      end
-    
-    module Fastlane
-  # Handles receiving commands from the socket server, finding the Action to be invoked,
-  # invoking it, and returning any return values
-  class SocketServerActionCommandExecutor < CommandExecutor
-    attr_accessor :runner
-    attr_accessor :actions_requiring_special_handling
-    
-          it 'works with both select and exclude regex' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-              select_regex: /\.*m/,
-              exclude_regex: /Test/
-            )
-          end').runner.execute(:test)
-    
-        it 'adds an environment Hash at the beginning' do
-      message = 'A message'
-      command = command_from_args({ 'PATH' => '/usr/local/bin' }, 'git', 'commit', '-m', message)
-      expect(command).to eq('PATH=/usr/local/bin git commit -m #{message.shellescape}')
-    end
-    
-            it 'sets up screenshots folder in current folder' do
-          expect(options[:screenshots_path]).to eq('./screenshots')
+                def declaration_with_comment(node)
+          buffer = processed_source.buffer
+          begin_pos = get_source_range(node, comments_as_separators).begin_pos
+          end_line = buffer.line_for_position(node.loc.expression.end_pos)
+          end_pos = buffer.line_range(end_line).end_pos
+          Parser::Source::Range.new(buffer, begin_pos, end_pos)
         end
     
-          it 'requires the passwords to match when changing them' do
-        visit edit_admin_user_path(users(:bob))
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: 'no_match'
-        click_on 'Update User'
-        expect(page).to have_text('Password confirmation doesn't match')
-      end
-    end
+            def on_send(node)
+          erb_new_with_non_keyword_arguments(node) do |arguments|
+            return if correct_arguments?(arguments)
     
-        it 'can be turned off' do
-      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'false' }
-      DefaultScenarioImporter.import(user)
-    end
-    
-      describe 'down' do
-    let :valid_options do
-      super().merge('extract' => new_extract,
-                    'template' => new_template)
-    end
-    
-        pod 'ObjCPod', path: 'ObjCPod'
-    pod 'SwiftPod', path: 'SwiftPod'
-    pod 'MixedPod', path: 'MixedPod'
-    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
-    
-        def logstash_gem_home
-      ::File.join(BUNDLE_DIR, ruby_engine, gem_ruby_version)
-    end
-    
-          packet_gem.pack
-    
-        platforms.types.each do |type|
-      desc 'Run acceptance test in #{type} machines'
-      task type do
-        ENV['LS_TEST_PLATFORM']=type
-        exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/*_spec.rb']]))
-      end
-    end
+            def message_for_normal_block(variable, all_arguments)
+          if all_arguments.none?(&:referenced?) &&
+             !define_method_call?(variable)
+            if all_arguments.count > 1
+              'You can omit all the arguments if you don't care about them.'
+            else
+              'You can omit the argument if you don't care about it.'
+            end
+          else
+            message_for_underscore_prefix(variable)
+          end
+        end
