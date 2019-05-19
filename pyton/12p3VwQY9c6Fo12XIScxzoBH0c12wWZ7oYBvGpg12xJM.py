a26b260beb819a3d1d7ccea0750071b6db0aa6d3
SUNsung <weatherius@gmail.com>
2019-05-20 01:37:10 +0300

    Rewrite [12p3VwQY9c6Fo12XIScxzoBH0c12wWZ7oYBvGpg12xJM.py] File in: 2019-05-20 01:37:10

--- pyton/12p3VwQY9c6Fo12XIScxzoBH0c12wWZ7oYBvGpg12xJM.py
+++ pyton/12p3VwQY9c6Fo12XIScxzoBH0c12wWZ7oYBvGpg12xJM.py
@@ -1,154 +1,68 @@
 
         
-            @value.setter
-    def value(self, new_value):
-        if 1 <= new_value <= 13:
-            self._value = new_value
-        else:
-            raise ValueError('Invalid card value: {}'.format(new_value))
+            # Then we cluster the lines together as blocks
+    # Each block represents a collection of lines that should be sorted
+    # This was done by assuming only links ([...](...)) are meant to be sorted
+    # Clustering is done by indentation
+    blocks = []
+    last_indent = None
+    for line in read_me:
+        s_line = line.lstrip()
+        indent = len(line) - len(s_line)
     
-        def extract_year_month(self, timestamp):
-        '''Return the year and month portions of the timestamp.'''
-        ...
+    from twisted import version as _txv
+twisted_version = (_txv.major, _txv.minor, _txv.micro)
     
+            if opts.nolog:
+            self.settings.set('LOG_ENABLED', False, priority='cmdline')
     
-@pytest.mark.functional
-def test_select_command_with_arrows(proc, TIMEOUT):
-    select_command_with_arrows(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'git help')
+        def syntax(self):
+        return '[-v]'
     
-    match_output = '''
-Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
-Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
-Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
-Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
-Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
-Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
-Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
-Fetched 89.2 kB in 0s (122 kB/s)
-Reading package lists... Done
-Building dependency tree
-Reading state information... Done
-8 packages can be upgraded. Run 'apt list --upgradable' to see them.
-'''
+        try:
+        # XXX: this try-except is not needed in Twisted 17.0.0+ because
+        # it requires pyOpenSSL 0.16+.
+        from OpenSSL.SSL import SSL_CB_HANDSHAKE_DONE, SSL_CB_HANDSHAKE_START
+    except ImportError:
+        SSL_CB_HANDSHAKE_START = 0x10
+        SSL_CB_HANDSHAKE_DONE = 0x20
     
+        def __call__(self, text, **kargs):
+        words = jieba.tokenize(text, mode='search')
+        token = Token()
+        for (w, start_pos, stop_pos) in words:
+            if not accepted_chars.match(w) and len(w) <= 1:
+                continue
+            token.original = token.text = w
+            token.pos = start_pos
+            token.startchar = start_pos
+            token.endchar = stop_pos
+            yield token
     
-@pytest.mark.parametrize('command', [
-    Command('cargo buid', no_such_subcommand_old),
-    Command('cargo buils', no_such_subcommand)])
-def test_match(command):
-    assert match(command)
+    Force_Split_Words = set([])
+def load_model():
+    start_p = pickle.load(get_module_res('finalseg', PROB_START_P))
+    trans_p = pickle.load(get_module_res('finalseg', PROB_TRANS_P))
+    emit_p = pickle.load(get_module_res('finalseg', PROB_EMIT_P))
+    return start_p, trans_p, emit_p
     
-        parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
-    parser.add_argument('-d', action='store', dest='directory')
-    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
-    parser.add_argument('--overwrite', action='store_true', default = False)    
-    results = parser.parse_args()
+    tags = jieba.analyse.extract_tags(content, topK=topK)
     
-            Use attention **before** lstm:
-            ```
-            tf.reset_default_graph()
+    parser = OptionParser(USAGE)
+parser.add_option('-k', dest='topK')
+parser.add_option('-w', dest='withWeight')
+opt, args = parser.parse_args()
     
-    from ...utils import get_shape, get_w
+    for f_name in glob.glob(pattern):
+    with open(f_name) as f:
+        print('read file:', f_name)
+        for line in f: #one line as a document
+            words = ' '.join(jieba.cut(line))
+            docs.append(words)
     
-        return x
-
+    USAGE ='usage:    python extract_tags.py [file name] -k [top k]'
     
-        def __init__(self, l1=0., l2=0.):
-        '''
-        Args:
-            l1(float): L1 正则化的系数
-            l2(float): L2 正则化的系数
-        '''
-        self.l1 = np.asarray(l1, dtype=np.float32)
-        self.l2 = np.asarray(l2, dtype=np.float32)
-    
-    word_unk = 'aam'
-ngrams = compute_ngrams(word_unk, min_ngrams, max_ngrams)  # min_ngrams, max_ngrams = 2, 4
-word_vec = np.zeros(model.vector_size, dtype=np.float32)
-ngrams_found = 0
-for ngram in ngrams:
-    ngram_hash = ft_hash(ngram) % model.bucket
-    if ngram_hash in model.wv.hash2index:
-        word_vec += model.wv.vectors_ngrams[model.wv.hash2index[ngram_hash]]
-        ngrams_found += 1
-    
-    
-def evaluate_masks(
-    json_dataset,
-    all_boxes,
-    all_segms,
-    output_dir,
-    use_salt=True,
-    cleanup=False
-):
-    if cfg.CLUSTER.ON_CLUSTER:
-        # On the cluster avoid saving these files in the job directory
-        output_dir = '/tmp'
-    res_file = os.path.join(
-        output_dir, 'segmentations_' + json_dataset.name + '_results')
-    if use_salt:
-        res_file += '_{}'.format(str(uuid.uuid4()))
-    res_file += '.json'
-    
-    from detectron.core.config import cfg
-from detectron.datasets.json_dataset import JsonDataset
-import detectron.utils.boxes as box_utils
-import detectron.utils.keypoints as keypoint_utils
-import detectron.utils.segms as segm_utils
-    
-        optim.build_data_parallel_model(model, _single_gpu_build_func)
-    return model
-    
-    
-_RENAME = {
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up4convs':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up4convs',
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up',
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0upshare':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v0upshare',
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0up':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v0up',
-    # Removed head_builder module in favor of the more specific fast_rcnn name
-    'head_builder.add_roi_2mlp_head':
-        'fast_rcnn_heads.add_roi_2mlp_head',
-}
-    
-    
-class CollectAndDistributeFpnRpnProposalsOp(object):
-    def __init__(self, train):
-        self._train = train
-    
-    
-def get_field_of_anchors(
-    stride, anchor_sizes, anchor_aspect_ratios, octave=None, aspect=None
-):
-    global _threadlocal_foa
-    if not hasattr(_threadlocal_foa, 'cache'):
-        _threadlocal_foa.cache = {}
-    
-        blobs['retnet_fg_num'], blobs['retnet_bg_num'] = 0.0, 0.0
-    for im_i, entry in enumerate(roidb):
-        scale = im_scales[im_i]
-        im_height = np.round(entry['height'] * scale)
-        im_width = np.round(entry['width'] * scale)
-        gt_inds = np.where(
-            (entry['gt_classes'] > 0) & (entry['is_crowd'] == 0))[0]
-        assert len(gt_inds) > 0, \
-            'Empty ground truth empty for image is not allowed. Please check.'
-    
-        episode_count = 100
-    reward = 0
-    done = False
-    
-    # Top-down car dynamics simulation.
-#
-# Some ideas are taken from this great tutorial http://www.iforce2d.net/b2dtut/top-down-car by Chris Campbell.
-# This simulation is a bit more detailed, with wheels rotation.
-#
-# Created by Oleg Klimov. Licensed on the same terms as the rest of OpenAI Gym.
+    url = sys.argv[1]
+content = open(url,'rb').read()
+t1 = time.time()
+words = list(pseg.cut(content))
