2564496386abc74e7ea92842510d1e0314fe714e
SUNsung <weatherius@gmail.com>
2019-05-19 14:31:08 +0300

    Rewrite [58t8Qst.rivv258zpISd8bJ7jg58.gYtqC9SAP258fea.rb] File in: 2019-05-19 14:31:07

--- ruby/58t8Qst.rivv258zpISd8bJ7jg58.gYtqC9SAP258fea.rb
+++ ruby/58t8Qst.rivv258zpISd8bJ7jg58.gYtqC9SAP258fea.rb
@@ -1,101 +1,39 @@
 
         
-                # Download a file from the remote machine to the local machine.
-        #
-        # @param [String] from Path of the file on the remote machine.
-        # @param [String] to Path of where to save the file locally.
-        def download(from, to)
-        end
+              # private
     
-            # Mounts a shared folder via NFS. This assumes that the exports
-        # via the host are already done.
-        def mount_nfs(ip, folders)
-          raise BaseError, _key: :unsupported_nfs
-        end
-    
-              hosts
-        end
-    
-            # This is the method called to when the system is being destroyed
-        # and allows the provisioners to engage in any cleanup tasks necessary.
-        def cleanup
-        end
+          def has_sidebar
+        @sidebar = (@page.sidebar || false) if @sidebar.nil? && @page
+        !!@sidebar
       end
-    end
+    
+    # Make sure we're in the test dir, the tests expect that to be the current
+# directory.
+TEST_DIR  = File.join(File.dirname(__FILE__), *%w[.])
+    
+        # TODO: Remove to_url once write_page changes are merged.
+    @wiki.write_page('ééééé'.to_url, :markdown, '한글 text', commit_details)
+    page = @wiki.page('eeeee')
+    assert_equal '한글 text', utf8(page.raw_data)
   end
-end
-
     
-            # This returns any automatically detected errors.
-        #
-        # @return [Array<String>]
-        def _detected_errors
-          return [] if !@__invalid_methods || @__invalid_methods.empty?
-          return [I18n.t('vagrant.config.common.bad_field',
-                         fields: @__invalid_methods.to_a.sort.join(', '))]
-        end
-    
-            # This should return a hash of information that explains how to
-        # SSH into the machine. If the machine is not at a point where
-        # SSH is even possible, then `nil` should be returned.
-        #
-        # The general structure of this returned hash should be the
-        # following:
-        #
-        #     {
-        #       host: '1.2.3.4',
-        #       port: '22',
-        #       username: 'mitchellh',
-        #       private_key_path: '/path/to/my/key'
-        #     }
-        #
-        # **Note:** Vagrant only supports private key based authentication,
-        # mainly for the reason that there is no easy way to exec into an
-        # `ssh` prompt with a password, whereas we can pass a private key
-        # via commandline.
-        #
-        # @return [Hash] SSH information. For the structure of this hash
-        #   read the accompanying documentation for this method.
-        def ssh_info
-          nil
-        end
-    
-    # This is the version that ships with OS X 10.10, so be sure we test against it.
-# At the same time, the 1.7.7 version won't install cleanly on Ruby > 2.2,
-# so we use a fork that makes a trivial change to a macro invocation.
-gem 'json', :git => 'https://github.com/segiddins/json.git', :branch => 'seg-1.7.7-ruby-2.2'
-    
-          # Checks whether this case statement has an `else` branch.
-      #
-      # @return [Boolean] whether the `case` statement has an `else` branch
-      def else?
-        loc.else
-      end
-    end
+    desc 'Create a release build and push to rubygems'
+task :release => :build do
+  unless `git branch` =~ /^\* master$/
+    puts 'You must be on the master branch to release!'
+    exit!
   end
+  sh 'git commit --allow-empty -a -m 'Release #{version}''
+  sh 'git pull --rebase origin master'
+  sh 'git tag v#{version}'
+  sh 'git push origin master'
+  sh 'git push origin v#{version}'
+  sh 'gem push pkg/#{name}-#{version}.gem'
 end
-
     
-          # Checks whether this node body is a void context.
-      #
-      # @return [Boolean] whether the `def` node body is a void context
-      def void_context?
-        method?(:initialize) || assignment_method?
-      end
-    
-    module RuboCop
-  module AST
-    # A node extension for `hash` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `hash` nodes within RuboCop.
-    class HashNode < Node
-      # Returns an array of all the key value pairs in the `hash` literal.
-      #
-      # @return [Array<PairNode>] an array of `pair` nodes
-      def pairs
-        each_pair.to_a
-      end
-    
-        def possible_types
-      MIME::Types.type_for(@filepath).collect(&:content_type)
-    end
+      s.add_dependency 'gollum-lib', '~> 4.2', '>= 4.2.10'
+  s.add_dependency 'kramdown', '~> 1.9.0'
+  s.add_dependency 'sinatra', '~> 1.4', '>= 1.4.4'
+  s.add_dependency 'mustache', ['>= 0.99.5', '< 1.0.0']
+  s.add_dependency 'useragent', '~> 0.16.2'
+  s.add_dependency 'gemojione', '~> 3.2'
