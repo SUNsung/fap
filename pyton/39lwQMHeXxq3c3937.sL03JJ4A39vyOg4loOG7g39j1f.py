3726b078e901a5a146eca8d055224039a4476f55
SUNsung <weatherius@gmail.com>
2019-05-19 07:38:08 +0300

    Rewrite [39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py] File in: 2019-05-19 07:38:07

--- pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
+++ pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
@@ -1,80 +1,131 @@
 
         
-            def test_b_observers_shall_be_attachable(cls):
-        cls.s.attach(cls.dec_obs)
-        cls.assertEqual(isinstance(cls.s._observers[0], DecimalViewer), True)
-        cls.assertEqual(len(cls.s._observers), 1)
-        cls.s.attach(cls.hex_obs)
-        cls.assertEqual(isinstance(cls.s._observers[1], HexViewer), True)
-        cls.assertEqual(len(cls.s._observers), 2)
+            with gzip.open(paths[2], 'rb') as lbpath:
+        y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
     
-        def test_extended_property_values_cloning(self):
-        sample_object_1 = self.prototype.clone()
-        sample_object_1.some_value = 'test string'
-        sample_object_2 = self.prototype.clone()
-        self.assertRaises(AttributeError, lambda: sample_object_2.some_value)
+        # Test single image
+    x = np.random.uniform(0, 255, (10, 10, 3))
+    inputs = Input(shape=x.shape)
+    outputs = Lambda(utils.preprocess_input, output_shape=x.shape)(inputs)
+    model = Model(inputs, outputs)
+    assert model.predict(x[np.newaxis])[0].shape == x.shape
     
     
-class OutOfData(UnpackException):
-    pass
+def create_multi_input_model_from(layer1, layer2):
+    input_1 = Input(shape=(data_dim,))
+    input_2 = Input(shape=(data_dim,))
+    out1 = layer1(input_1)
+    out2 = layer2(input_2)
+    out = Average()([out1, out2])
+    model = Model([input_1, input_2], out)
+    model.add_loss(K.mean(out2))
+    model.add_loss(1)
+    model.add_loss(1)
+    return model
+    
+    model1 = create_network(num_classes=num_classes, **network1)
+history_model1 = model1.fit(x_train,
+                            y_train,
+                            batch_size=batch_size,
+                            epochs=epochs,
+                            verbose=1,
+                            validation_split=0.1)
+    
+        def triger_check_network(self, fail=False, force=False):
+        time_now = time.time()
+        if not force:
+            if self._checking_num > 0:
+                return
     
     
-# List of frequently used SAS date and datetime formats
-# http://support.sas.com/documentation/cdl/en/etsug/60372/HTML/default/viewer.htm#etsug_intervals_sect009.htm
-# https://github.com/epam/parso/blob/master/src/main/java/com/epam/parso/impl/SasFileConstants.java
-sas_date_formats = ('DATE', 'DAY', 'DDMMYY', 'DOWNAME', 'JULDAY', 'JULIAN',
-                    'MMDDYY', 'MMYY', 'MMYYC', 'MMYYD', 'MMYYP', 'MMYYS',
-                    'MMYYN', 'MONNAME', 'MONTH', 'MONYY', 'QTR', 'QTRR',
-                    'NENGO', 'WEEKDATE', 'WEEKDATX', 'WEEKDAY', 'WEEKV',
-                    'WORDDATE', 'WORDDATX', 'YEAR', 'YYMM', 'YYMMC', 'YYMMD',
-                    'YYMMP', 'YYMMS', 'YYMMN', 'YYMON', 'YYMMDD', 'YYQ',
-                    'YYQC', 'YYQD', 'YYQP', 'YYQS', 'YYQN', 'YYQR', 'YYQRC',
-                    'YYQRD', 'YYQRP', 'YYQRS', 'YYQRN',
-                    'YYMMDDP', 'YYMMDDC', 'E8601DA', 'YYMMDDN', 'MMDDYYC',
-                    'MMDDYYS', 'MMDDYYD', 'YYMMDDS', 'B8601DA', 'DDMMYYN',
-                    'YYMMDDD', 'DDMMYYB', 'DDMMYYP', 'MMDDYYP', 'YYMMDDB',
-                    'MMDDYYN', 'DDMMYYC', 'DDMMYYD', 'DDMMYYS',
-                    'MINGUO')
+def gae_application(environ, start_response):
+    cookie = environ.get('HTTP_COOKIE', '')
+    options = environ.get('HTTP_X_GOA_OPTIONS', '')
+    if environ['REQUEST_METHOD'] == 'GET' and not cookie:
+        if '204' in environ['QUERY_STRING']:
+            start_response('204 No Content', [])
+            yield ''
+        else:
+            timestamp = long(os.environ['CURRENT_VERSION_ID'].split('.')[1])/2**28
+            ctime = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(timestamp+8*3600))
+            html = u'GoAgent Python Server %s \u5df2\u7ecf\u5728\u5de5\u4f5c\u4e86\uff0c\u90e8\u7f72\u65f6\u95f4 %s\n' % (__version__, ctime)
+            start_response('200 OK', [('Content-Type', 'text/plain; charset=utf-8')])
+            yield html.encode('utf8')
+        raise StopIteration
+    
+    ## All tokens go to the parser (unless skip() is called in that rule)
+# on a particular 'channel'.  The parser tunes to a particular channel
+# so that whitespace etc... can go to the parser on a 'hidden' channel.
+DEFAULT_CHANNEL = 0
+    
+            else:
+            return self.c
+    
+            self._state.errorRecovery = False
+        self.input.consume()
     
     
-def testSignedInt():
-    check(b'\x99\xd0\x00\xd0\x80\xd0\xff\xd1\x00\x00\xd1\x80\x00'
-          b'\xd1\xff\xff\xd2\x00\x00\x00\x00\xd2\x80\x00\x00\x00'
-          b'\xd2\xff\xff\xff\xff', (0,
-                                    -128,
-                                    -1,
-                                    0,
-                                    -32768,
-                                    -1,
-                                    0,
-                                    -2147483648,
-                                    -1, ))
+class HsmMethodTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.hsm = HierachicalStateMachine()
     
-    print('\n=== Pull Request #%s ===' % pr_num)
     
-            # 3. TODO: resolve other font-relative units
-        for side in self.SIDES:
-            prop = 'border-{side}-width'.format(side=side)
-            if prop in props:
-                props[prop] = self.size_to_pt(
-                    props[prop], em_pt=font_size,
-                    conversions=self.BORDER_WIDTH_RATIOS)
-            for prop in ['margin-{side}'.format(side=side),
-                         'padding-{side}'.format(side=side)]:
-                if prop in props:
-                    # TODO: support %
-                    props[prop] = self.size_to_pt(
-                        props[prop], em_pt=font_size,
-                        conversions=self.MARGIN_RATIOS)
+class Professor(AbstractExpert):
+    @property
+    def is_eager_to_contribute(self):
+        return True if self.blackboard.common_state['problems'] > 100 else False
     
-            exp = pd.Index([pd.Timestamp('2011-01-01 09:00', tz=tz),
-                        pd.Timestamp('2011-01-01 10:00'),
-                        pd.Timestamp('2011-01-01 11:00', tz=tz)],
-                       dtype=object)
-        tm.assert_index_equal(
-            idx.fillna(pd.Timestamp('2011-01-01 10:00')), exp)
+        def __init__(self, successor=None):
+        self.successor = successor
     
-    seg_list = jieba.cut('我来到北京清华大学', cut_all=True)
-print('Full Mode: ' + '/ '.join(seg_list))  # 全模式
+    parser = ArgumentParser(usage='%s -m jieba [options] filename' % sys.executable, description='Jieba command line interface.', epilog='If no filename specified, use STDIN instead.')
+parser.add_argument('-d', '--delimiter', metavar='DELIM', default=' / ',
+                    nargs='?', const=' ',
+                    help='use DELIM instead of ' / ' for word delimiter; or a space if it is used without DELIM')
+parser.add_argument('-p', '--pos', metavar='DELIM', nargs='?', const='_',
+                    help='enable POS tagging; if DELIM is specified, use DELIM instead of '_' for POS delimiter')
+parser.add_argument('-D', '--dict', help='use DICT as dictionary')
+parser.add_argument('-u', '--user-dict',
+                    help='use USER_DICT together with the default dictionary or DICT (if specified)')
+parser.add_argument('-a', '--cut-all',
+                    action='store_true', dest='cutall', default=False,
+                    help='full pattern cutting (ignored with POS tagging)')
+parser.add_argument('-n', '--no-hmm', dest='hmm', action='store_false',
+                    default=True, help='don't use the Hidden Markov Model')
+parser.add_argument('-q', '--quiet', action='store_true', default=False,
+                    help='don't print loading messages to stderr')
+parser.add_argument('-V', '--version', action='version',
+                    version='Jieba ' + jieba.__version__)
+parser.add_argument('filename', nargs='?', help='input file')
     
-    print('speed' , len(content)/tm_cost, ' bytes/second')
+        iterkeys = lambda d: iter(d.keys())
+    itervalues = lambda d: iter(d.values())
+    iteritems = lambda d: iter(d.items())
+    
+    
+def viterbi(obs, states, start_p, trans_p, emit_p):
+    V = [{}]  # tabular
+    path = {}
+    for y in states:  # init
+        V[0][y] = start_p[y] + emit_p[y].get(obs[0], MIN_FLOAT)
+        path[y] = [y]
+    for t in xrange(1, len(obs)):
+        V.append({})
+        newpath = {}
+        for y in states:
+            em_p = emit_p[y].get(obs[t], MIN_FLOAT)
+            (prob, state) = max(
+                [(V[t - 1][y0] + trans_p[y0].get(y, MIN_FLOAT) + em_p, y0) for y0 in PrevStatus[y]])
+            V[t][y] = prob
+            newpath[y] = path[state] + [y]
+        path = newpath
+    
+            for y in obs_states:
+            prob, state = max((V[t - 1][y0] + trans_p[y0].get(y, MIN_INF) +
+                               emit_p[y].get(obs[t], MIN_FLOAT), y0) for y0 in prev_states)
+            V[t][y] = prob
+            mem_path[t][y] = state
+    
+    log_f = open('1.log','w')
+log_f.write(' / '.join(map(str, words)))
