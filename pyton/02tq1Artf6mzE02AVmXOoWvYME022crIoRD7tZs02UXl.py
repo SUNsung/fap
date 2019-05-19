49eb24559837c8e96ae7f55a3dcc755be28cfc38
SUNsung <weatherius@gmail.com>
2019-05-19 03:49:07 +0300

    Rewrite [02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py] File in: 2019-05-19 03:49:07

--- pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
+++ pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
@@ -1,149 +1,184 @@
 
         
-            def escalate_call(self):
-        self.call.level = Rank.SUPERVISOR
-        self._escalate_call()
+        anchor = '###'
+min_entries_per_section = 3
+auth_keys = ['apiKey', 'OAuth', 'X-Mashape-Key', 'No']
+punctuation = ['.', '?', '!']
+https_keys = ['Yes', 'No']
+cors_keys = ['Yes', 'No', 'Unknown']
     
-    
-class Categorizer(object):
-    
-        def extract_max_priority_page(self):
-        '''Return the highest priority link in `links_to_crawl`.'''
-        pass
-    
-    
-def test_create(client, auth, app):
-    auth.login()
-    assert client.get('/create').status_code == 200
-    client.post('/create', data={'title': 'created', 'body': ''})
-    
-        def context_processor(self, f):
-        '''Like :meth:`Flask.context_processor` but for a blueprint.  This
-        function is only executed for requests handled by a blueprint.
-        '''
-        self.record_once(lambda s: s.app.template_context_processors
-            .setdefault(self.name, []).append(f))
-        return f
-    
-                resp = app.response_class()
-            if not session_interface.is_null_session(sess):
-                session_interface.save_session(app, sess, resp)
-            headers = resp.get_wsgi_headers(c.request.environ)
-            self.cookie_jar.extract_wsgi(c.request.environ, headers)
-    
-            app.add_url_rule('/counter', view_func=CounterAPI.as_view('counter'))
+        'Flipping' an entry means that that image and associated metadata (e.g.,
+    ground truth boxes and object proposals) are horizontally flipped.
     '''
+    flipped_roidb = []
+    for entry in roidb:
+        width = entry['width']
+        boxes = entry['boxes'].copy()
+        oldx1 = boxes[:, 0].copy()
+        oldx2 = boxes[:, 2].copy()
+        boxes[:, 0] = width - oldx2 - 1
+        boxes[:, 2] = width - oldx1 - 1
+        assert (boxes[:, 2] >= boxes[:, 0]).all()
+        flipped_entry = {}
+        dont_copy = ('boxes', 'segms', 'gt_keypoints', 'flipped')
+        for k, v in entry.items():
+            if k not in dont_copy:
+                flipped_entry[k] = v
+        flipped_entry['boxes'] = boxes
+        flipped_entry['segms'] = segm_utils.flip_segms(
+            entry['segms'], entry['height'], entry['width']
+        )
+        if dataset.keypoints is not None:
+            flipped_entry['gt_keypoints'] = keypoint_utils.flip_keypoints(
+                dataset.keypoints, dataset.keypoint_flip_map,
+                entry['gt_keypoints'], entry['width']
+            )
+        flipped_entry['flipped'] = True
+        flipped_roidb.append(flipped_entry)
+    roidb.extend(flipped_roidb)
     
     
-class RangeMinValueValidator(MinValueValidator):
-    def compare(self, a, b):
-        return a.lower is None or a.lower < b
-    message = _('Ensure that this range is completely greater than or equal to %(limit_value)s.')
-
-    
-        @classmethod
-    def get_session_store_class(cls):
-        raise NotImplementedError
-    
-        module = AnsibleModule(
-        argument_spec=dict(
-            component=dict(required=True, aliases=['name']),
-            version=dict(required=True),
-            token=dict(required=True, no_log=True),
-            state=dict(required=True, choices=['started', 'finished', 'failed']),
-            hosts=dict(required=False, default=[socket.gethostname()], aliases=['host']),
-            env=dict(required=False),
-            owner=dict(required=False),
-            description=dict(required=False),
-            message=dict(required=False),
-            source_system=dict(required=False, default='ansible'),
-            validate_certs=dict(default='yes', type='bool'),
-            url=dict(required=False, default='https://api.bigpanda.io'),
-        ),
-        supports_check_mode=True,
+def VGG16_rpn_frozen_features(model):
+    return build_generic_detection_model(
+        model, VGG16.add_VGG16_conv5_body, freeze_conv_body=True
     )
     
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.six.moves.urllib.parse import urlencode
-from ansible.module_utils._text import to_native
-from ansible.module_utils.urls import fetch_url
+    Flexible network configuration is achieved by specifying the function name that
+builds a network module (e.g., the name of the conv backbone or the mask roi
+head). However we may wish to change names over time without breaking previous
+config files. This module provides backwards naming compatibility by providing
+a mapping from the old name to the new name.
     
-    - name: Disable api feature
-  icinga2_feature:
-    name: api
-    state: absent
-'''
     
-    CLI_DEFAULTS = dict(
-    server_root=server_root_tmp,
-    ctl='nginx',
-)
-'''CLI defaults.'''
+def _add_allreduce_graph(model):
+    '''Construct the graph that performs Allreduce on the gradients.'''
+    # Need to all-reduce the per-GPU gradients if training with more than 1 GPU
+    all_params = model.TrainableParams()
+    assert len(all_params) % cfg.NUM_GPUS == 0
+    # The model parameters are replicated on each GPU, get the number
+    # distinct parameter blobs (i.e., the number of parameter blobs on
+    # each GPU)
+    params_per_gpu = int(len(all_params) / cfg.NUM_GPUS)
+    with c2_utils.CudaScope(0):
+        # Iterate over distinct parameter blobs
+        for i in range(params_per_gpu):
+            # Gradients from all GPUs for this parameter blob
+            gradients = [
+                model.param_to_grad[p] for p in all_params[i::params_per_gpu]
+            ]
+            if len(gradients) > 0:
+                if cfg.USE_NCCL:
+                    model.net.NCCLAllreduce(gradients, gradients)
+                else:
+                    muji.Allreduce(model.net, gradients, reduced_affix='')
     
-            :returns: `set` of unsaved files
-        '''
-        save_state = self.aug.get('/augeas/save')
-        self.aug.set('/augeas/save', 'noop')
-        # Existing Errors
-        ex_errs = self.aug.match('/augeas//error')
-        try:
-            # This is a noop save
-            self.aug.save()
-        except (RuntimeError, IOError):
-            self._log_save_errors(ex_errs)
-            # Erase Save Notes
-            self.save_notes = ''
-            raise errors.PluginError(
-                'Error saving files, check logs for more info.')
     
-    ALL_SSL_OPTIONS_HASHES = [
-    '2086bca02db48daf93468332543c60ac6acdb6f0b58c7bfdf578a5d47092f82a',
-    '4844d36c9a0f587172d9fa10f4f1c9518e3bcfa1947379f155e16a70a728c21a',
-    '5a922826719981c0a234b1fbcd495f3213e49d2519e845ea0748ba513044b65b',
-    '4066b90268c03c9ba0201068eaa39abbc02acf9558bb45a788b630eb85dadf27',
-    'f175e2e7c673bd88d0aff8220735f385f916142c44aa83b09f1df88dd4767a88',
-    'cfdd7c18d2025836ea3307399f509cfb1ebf2612c87dd600a65da2a8e2f2797b',
-    '80720bd171ccdc2e6b917ded340defae66919e4624962396b992b7218a561791',
-    'c0c022ea6b8a51ecc8f1003d0a04af6c3f2bc1c3ce506b3c2dfc1f11ef931082',
-]
-'''SHA256 hashes of the contents of previous versions of all versions of MOD_SSL_CONF_SRC'''
+def collect(inputs, is_training):
+    cfg_key = 'TRAIN' if is_training else 'TEST'
+    post_nms_topN = cfg[cfg_key].RPN_POST_NMS_TOP_N
+    k_max = cfg.FPN.RPN_MAX_LEVEL
+    k_min = cfg.FPN.RPN_MIN_LEVEL
+    num_lvls = k_max - k_min + 1
+    roi_inputs = inputs[:num_lvls]
+    score_inputs = inputs[num_lvls:]
+    if is_training:
+        score_inputs = score_inputs[:-2]
     
-        :ivar bool ssl: SSLEngine on in vhost
-    :ivar bool enabled: Virtual host is enabled
-    :ivar bool modmacro: VirtualHost is using mod_macro
-    :ivar VirtualHost ancestor: A non-SSL VirtualHost this is based on
     
-        @certbot_util.patch_get_utility()
-    def test_select_correct(self, mock_util):
-        mock_util().checklist.return_value = (
-            display_util.OK, [self.vhosts[3].display_repr(),
-                              self.vhosts[2].display_repr()])
-        vhs = select_vhost_multiple([self.vhosts[3],
-                                     self.vhosts[2],
-                                     self.vhosts[1]])
-        self.assertTrue(self.vhosts[2] in vhs)
-        self.assertTrue(self.vhosts[3] in vhs)
-        self.assertFalse(self.vhosts[1] in vhs)
+def loader_loop(roi_data_loader):
+    load_timer = Timer()
+    iters = 100
+    for i in range(iters):
+        load_timer.tic()
+        roi_data_loader.get_next_minibatch()
+        load_timer.toc()
+        print('{:d}/{:d}: Average get_next_minibatch time: {:.3f}s'.format(
+              i + 1, iters, load_timer.average_time))
     
-        def test_nonexistent_generic(self):
-        with mock.patch('certbot.util.get_os_info') as mock_info:
-            mock_info.return_value = ('nonexistent', 'irrelevant')
-            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
-                mock_like.return_value = ['unknonwn']
-                self.assertEqual(entrypoint.get_configurator(),
-                                 configurator.ApacheConfigurator)
+    EMACS_FT_NAME = {
+    'asm-mode'             : 'asm',
+    'awk-mode'             : 'awk',
+    'sh-mode'              : 'bash',
+    # basic
+    'brainfuck-mode'       : 'bf',
+    # chapel
+    'clojure-mode'         : 'clojure',
+    'coffee-mode'          : 'coffee',
+    'c++-mode'             : 'cpp',
+    'c-mode'               : 'c',
+    'csharp-mode'          : 'csharp',
+    'd-mode'               : 'd',
+    'dart-mode'            : 'dart',
+    'dylan-mode'           : 'dylan',
+    'delphi-mode'          : 'delphi',
+    'emacs-lisp-mode'      : 'elisp',
+    # elixir
+    'elm-mode'             : 'elm',
+    'erlang-mode'          : 'erlang',
+    # factor
+    'forth-mode'           : 'forth',
+    'fortran-mode'         : 'fortran',
+    'fsharp-mode'          : 'fsharp',
+    'go-mode'              : 'go',
+    'groovy-mode'          : 'groovy',
+    'haskell-mode'         : 'haskell',
+    # 'hy-mode'
+    'java-mode'            : 'java',
+    'js-jsx-mode'          : 'js',
+    'js-mode'              : 'js',
+    'js2-jsx-mode'         : 'js',
+    'js2-mode'             : 'js',
+    'julia-mode'           : 'julia',
+    'kotlin-mode'          : 'kotlin',
+    'lisp-interaction-mode': 'lisp',
+    'lisp-mode'            : 'lisp',
+    'lua-mode'             : 'lua',
+    # mathematica
+    'matlab-mode'          : 'matlab',
+    # mongo
+    'objc-mode'            : 'objective-c',
+    # ocaml
+    'perl-mode'            : 'perl',
+    'perl6-mode'           : 'perl6',
+    'php-mode'             : 'php',
+    # psql
+    'python-mode'          : 'python',
+    # python3
+    # r -- ess looks it, but I don't know the mode name off hand
+    'racket-mode'          : 'racket',
+    'ruby-mode'            : 'ruby',
+    'rust-mode'            : 'rust',
+    'solidity-mode'        : 'solidity',
+    'scala-mode'           : 'scala',
+    'scheme-mode'          : 'scheme',
+    'sql-mode'             : 'sql',
+    'swift-mode'           : 'swift',
+    'tcl-mode'             : 'tcl',
+    # tcsh
+    'visual-basic-mode'    : 'vb',
+    # vbnet
+    # vim
+}
     
-        def test_repr(self):
-        self.assertEqual(repr(self.addr2), 'certbot_apache.obj.Addr(('127.0.0.1', '443'))')
+            self._list[prefix] = set(self._get_list(prefix=prefix))
+        return self._list[prefix]
     
-        auth_key = common_test.AUTH_KEY
-    achalls = common_test.ACHALLS
+            # if there is a language name in the section name,
+        # cut it off (de:python => python)
+        if '/' in topic:
+            section_name, topic = topic.split('/', 1)
+            if ':' in section_name:
+                _, section_name = section_name.split(':', 1)
+            section_name = SO_NAME.get(section_name, section_name)
+            topic = '%s/%s' % (section_name, topic)
     
-    # Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'certbot-apache', u'certbot-apache Documentation',
-     author, 'certbot-apache', 'One line description of project.',
-     'Miscellaneous'),
-]
+    import re
+import json
+    
+    MYDIR = os.path.abspath(os.path.join(__file__, '..', '..'))
+sys.path.append('%s/lib/' % MYDIR)
+    
+        answers_found = []
+    for topic in get_topics_list(skip_internal=True, skip_dirs=True):
+    
+    def _visualize(answers, request_options, search_mode=False):
