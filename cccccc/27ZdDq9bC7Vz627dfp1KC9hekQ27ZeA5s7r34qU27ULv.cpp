1fd920f9cca42a30a07c9d276ba66c03cc40f286
SUNsung <weatherius@gmail.com>
2019-05-19 11:26:35 +0300

    Rewrite [27ZdDq9bC7Vz627dfp1KC9hekQ27ZeA5s7r34qU27ULv.cpp] File in: 2019-05-19 11:26:34

--- cccccc/27ZdDq9bC7Vz627dfp1KC9hekQ27ZeA5s7r34qU27ULv.cpp
+++ cccccc/27ZdDq9bC7Vz627dfp1KC9hekQ27ZeA5s7r34qU27ULv.cpp
@@ -1,160 +1,261 @@
 
         
-        DIRECTIONAL_PREPOSITION(above)
-DIRECTIONAL_PREPOSITION(after)
-DIRECTIONAL_PREPOSITION(along)
-DIRECTIONAL_PREPOSITION(alongside)
-DIRECTIONAL_PREPOSITION(as)
-DIRECTIONAL_PREPOSITION(at)
-DIRECTIONAL_PREPOSITION(before)
-DIRECTIONAL_PREPOSITION(below)
-DIRECTIONAL_PREPOSITION(by)
-DIRECTIONAL_PREPOSITION(following)
-DIRECTIONAL_PREPOSITION(for)
-DIRECTIONAL_PREPOSITION(from)
-DIRECTIONAL_PREPOSITION(given)
-DIRECTIONAL_PREPOSITION(in)
-DIRECTIONAL_PREPOSITION(including)
-DIRECTIONAL_PREPOSITION(inside)
-DIRECTIONAL_PREPOSITION(into)
-DIRECTIONAL_PREPOSITION(matching)
-DIRECTIONAL_PREPOSITION(of)
-DIRECTIONAL_PREPOSITION(on)
-DIRECTIONAL_PREPOSITION(passing)
-DIRECTIONAL_PREPOSITION(preceding)
-DIRECTIONAL_PREPOSITION(since)
-DIRECTIONAL_PREPOSITION(to)
-DIRECTIONAL_PREPOSITION(until)
-DIRECTIONAL_PREPOSITION(using)
-DIRECTIONAL_PREPOSITION(via)
-DIRECTIONAL_PREPOSITION(when)
-PREPOSITION(with)
-DIRECTIONAL_PREPOSITION(within)
-    
-    
-    {
-    {
-    {      if (node->Right) {
-        print(node->Right, ChildKind::Right);
-      }
-    }
-  }
-};
-    
-      IAMResult(DeclName declName, IAMAccessorKind kind, EffectiveClangContext dc)
-      : name(declName), accessorKind(kind), effectiveDC(dc) {}
-    
-    
-    {public:
-  Cygwin(const Driver &D, const llvm::Triple &Triple)
-      : GenericUnix(D, Triple) {}
-  ~Cygwin() = default;
-};
-    
-        const QString &getAppName() const { return appName; }
-    const QIcon &getAppIcon() const { return appIcon; }
-    const QIcon &getTrayAndWindowIcon() const { return trayAndWindowIcon; }
-    const QString &getTitleAddText() const { return titleAddText; }
-    
-        void showTab_SM(bool fShow);
-    void showTab_VM(bool fShow);
-    
-    #ifndef SECP256K1_HASH_IMPL_H
-#define SECP256K1_HASH_IMPL_H
-    
-        s_one[31] = 1;
-    /* Check against pubkey creation when the basepoint is the generator */
-    for (i = 0; i < 100; ++i) {
-        secp256k1_sha256_t sha;
-        unsigned char s_b32[32];
-        unsigned char output_ecdh[32];
-        unsigned char output_ser[32];
-        unsigned char point_ser[33];
-        size_t point_ser_len = sizeof(point_ser);
-        secp256k1_scalar s;
-    }
-    
-    // Bech32 is a string encoding format used in newer address types.
-// The output consists of a human-readable part (alphanumeric), a
-// separator character (1), and a base32 data section, the last
-// 6 characters of which are a checksum.
-//
-// For more information, see BIP 173.
-    
-    
-    {    input[4] = ReadLE32(k + 0);
-    input[5] = ReadLE32(k + 4);
-    input[6] = ReadLE32(k + 8);
-    input[7] = ReadLE32(k + 12);
-    if (keylen == 32) { /* recommended */
-        k += 16;
-        constants = sigma;
-    } else { /* keylen == 16 */
-        constants = tau;
-    }
-    input[8] = ReadLE32(k + 0);
-    input[9] = ReadLE32(k + 4);
-    input[10] = ReadLE32(k + 8);
-    input[11] = ReadLE32(k + 12);
-    input[0] = ReadLE32(constants + 0);
-    input[1] = ReadLE32(constants + 4);
-    input[2] = ReadLE32(constants + 8);
-    input[3] = ReadLE32(constants + 12);
-    input[12] = 0;
-    input[13] = 0;
-    input[14] = 0;
-    input[15] = 0;
-}
-    
-      TemporaryFile() {
-    path = tmppath();
-  }
-    
-    workspace.RunOperatorOnce(op)
-print('indices: \n', workspace.FetchBlob('indices'))
-    
-    [[noreturn]] void exception_wrapper::onNoExceptionError(
-    char const* const name) {
-  std::ios_base::Init ioinit_; // ensure std::cerr is alive
-  std::cerr << 'Cannot use `' << name
-            << '` with an empty folly::exception_wrapper' << std::endl;
-  std::terminate();
-}
-    
-    #include <folly/Executor.h>
-    
-    
-    {  /**
-   * Returns a double in [min, max), if min == max, returns 0.
-   */
-  template <class RNG = ThreadLocalPRNG, class /* EnableIf */ = ValidRNG<RNG>>
-  static double randDouble(double min, double max, RNG&& rng) {
-    if (std::fabs(max - min) < std::numeric_limits<double>::epsilon()) {
-      return 0;
-    }
-    return std::uniform_real_distribution<double>(min, max)(rng);
-  }
-};
-    
-      explicit VirtualExecutor(Executor* executor)
-      : VirtualExecutor(getKeepAliveToken(executor)) {}
-    
-    #pragma once
-    
-    /**
- * This class creates core-local caches for a given shared_ptr, to
- * mitigate contention when acquiring/releasing it.
- *
- * It has the same thread-safety guarantees as shared_ptr: it is safe
- * to concurrently call get(), but reset()s must be synchronized with
- * reads and other resets().
- *
- * @author Giuseppe Ottaviano <ott@fb.com>
- */
-template <class T, size_t kNumSlots = 64>
-class CoreCachedSharedPtr {
+        class AtomQuotaPermissionContext : public content::QuotaPermissionContext {
  public:
-  explicit CoreCachedSharedPtr(const std::shared_ptr<T>& p = nullptr) {
-    reset(p);
+  typedef content::QuotaPermissionContext::QuotaPermissionResponse response;
+    }
+    
+    namespace auto_updater {
+    }
+    
+    #include 'base/macros.h'
+#include 'build/build_config.h'
+#include 'native_mate/arguments.h'
+    
+    namespace atom {
+    }
+    
+    #include 'atom/browser/net/url_request_about_job.h'
+    
+    
+    {}  // namespace atom
+
+    
+      // ViewsDelegate:
+  void OnBeforeWidgetInit(
+      views::Widget::InitParams* params,
+      views::internal::NativeWidgetDelegate* delegate) override;
+  ui::ContextFactory* GetContextFactory() override;
+  ui::ContextFactoryPrivate* GetContextFactoryPrivate() override;
+    
+    
+    {  EXPECT_EQ(expected, decode_data.Data());
+}
+    
+    TEST(StatusOr, TestPointerAssignmentStatusOKConverting) {
+  Derived derived;
+  StatusOr<Derived*> source(&derived);
+  StatusOr<Base2*>   target;
+  target = source;
+  EXPECT_EQ(source.status(), target.status());
+  EXPECT_EQ(static_cast<const Base2*>(source.ValueOrDie()),
+            target.ValueOrDie());
+}
+    
+    
+    {
+    {    reflection->MutableUnknownFields(message)->Clear();
+  }
+ private:
+  virtual bool ShouldBeClear(const FieldDescriptor *field) = 0;
+};
+    
+    using google::protobuf::util::GogoDataStripper;
+    
+        std::cerr << 'Generating ' << input_file
+        << ' to ' << output_file << std::endl;
+    benchmarks::BenchmarkDataset dataset;
+    Message* message;
+    std::string dataset_payload = ReadFile(input_file);
+    GOOGLE_CHECK(dataset.ParseFromString(dataset_payload))
+      << 'Can' t parse data file ' << input_file;
+    
+      void ScrubMessage(DescriptorProto *message_type) {
+    for (int i = 0; i < message_type->mutable_enum_type()->size(); i++) {
+      ScrubEnum(message_type->mutable_enum_type(i));
+    }
+    for (int i = 0; i < message_type->mutable_nested_type()->size(); i++) {
+      ScrubMessage(message_type->mutable_nested_type(i));
+    }
+  }
+    
+    AuthPropertyIterator& AuthPropertyIterator::operator++() {
+  grpc_auth_property_iterator iter = {ctx_, index_, name_};
+  property_ = grpc_auth_property_iterator_next(&iter);
+  ctx_ = iter.ctx;
+  index_ = iter.index;
+  name_ = iter.name;
+  return *this;
+}
+    
+    #include <grpc/support/port_platform.h>
+    
+    void CensusClientCallData::StartTransportStreamOpBatch(
+    grpc_call_element* elem, TransportStreamOpBatch* op) {
+  if (op->send_initial_metadata() != nullptr) {
+    census_context* ctxt = op->get_census_context();
+    GenerateClientContext(
+        qualified_method_, &context_,
+        (ctxt == nullptr) ? nullptr : reinterpret_cast<CensusContext*>(ctxt));
+    size_t tracing_len = TraceContextSerialize(context_.Context(), tracing_buf_,
+                                               kMaxTraceContextLen);
+    if (tracing_len > 0) {
+      GRPC_LOG_IF_ERROR(
+          'census grpc_filter',
+          grpc_metadata_batch_add_tail(
+              op->send_initial_metadata()->batch(), &tracing_bin_,
+              grpc_mdelem_from_slices(
+                  GRPC_MDSTR_GRPC_TRACE_BIN,
+                  grpc_slice_from_copied_buffer(tracing_buf_, tracing_len))));
+    }
+    grpc_slice tags = grpc_empty_slice();
+    // TODO: Add in tagging serialization.
+    size_t encoded_tags_len = StatsContextSerialize(kMaxTagsLen, &tags);
+    if (encoded_tags_len > 0) {
+      GRPC_LOG_IF_ERROR(
+          'census grpc_filter',
+          grpc_metadata_batch_add_tail(
+              op->send_initial_metadata()->batch(), &stats_bin_,
+              grpc_mdelem_from_slices(GRPC_MDSTR_GRPC_TAGS_BIN, tags)));
+    }
   }
     }
+    
+    
+    { private:
+  CensusContext context_;
+  // Metadata elements for tracing and census stats data.
+  grpc_linked_mdelem stats_bin_;
+  grpc_linked_mdelem tracing_bin_;
+  // Client method.
+  absl::string_view method_;
+  std::string qualified_method_;
+  grpc_slice path_;
+  // The recv trailing metadata callbacks.
+  grpc_metadata_batch* recv_trailing_metadata_;
+  grpc_closure* initial_on_done_recv_trailing_metadata_;
+  grpc_closure on_done_recv_trailing_metadata_;
+  // recv message
+  grpc_closure* initial_on_done_recv_message_;
+  grpc_closure on_done_recv_message_;
+  // Start time (for measuring latency).
+  absl::Time start_time_;
+  // Server elapsed time in nanoseconds.
+  uint64_t elapsed_time_;
+  // The received message--may be null.
+  grpc_core::OrphanablePtr<grpc_core::ByteStream>* recv_message_;
+  // Number of messages in this RPC.
+  uint64_t recv_message_count_;
+  uint64_t sent_message_count_;
+  // Buffer needed for grpc_slice to reference when adding trace context
+  // metatdata to outgoing message.
+  char tracing_buf_[kMaxTraceContextLen];
+};
+    
+    
+    {    uint8_t version = buf[kVersionIdOffset];
+    uint32_t fieldID = buf[kServerElapsedTimeOffset];
+    if (version != kVersionId || fieldID != kServerElapsedTimeField) {
+      *time = 0;
+      return kEncodeDecodeFailure;
+    }
+    *time = absl::little_endian::Load64(
+        &buf[kServerElapsedTimeOffset + kFieldIdSize]);
+    return kRpcServerStatsSize;
+  }
+    
+    Status ProtoServerReflection::GetAllExtensionNumbers(
+    ServerContext* context, const grpc::string& type,
+    ExtensionNumberResponse* response) {
+  if (descriptor_pool_ == nullptr) {
+    return Status::CANCELLED;
+  }
+    }
+    
+        virtual void UpdateArguments(ChannelArguments* args) override {
+      args->SetInt(name_, value_);
+    }
+    virtual void UpdatePlugins(
+        std::vector<std::unique_ptr<ServerBuilderPlugin>>* plugins) override {}
+    
+            if (sectionShape.Rank() > m_maskShape.Rank())
+            LogicError('NDMask::MaskSection: The section shape '%S' rank (%d) must be <= rank (%d) of 'this' mask.', sectionShape.AsString().c_str(), (int)sectionShape.Rank(), (int)m_maskShape.Rank());
+    
+        template <typename T>
+    inline void ValidateType(const Dictionary& dict, const std::wstring& typeValue, size_t currentVersion)
+    {
+        if (!dict.Contains(typeKey))
+        {
+            const auto& version = GetVersion(dict);
+            LogicError('Required key '%ls' is not found in the dictionary (%s).',
+                       typeKey.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
+        } 
+    }
+    
+        void Trainer::SummarizeTrainingProgress()
+    {
+        // Aggregate across workers training loss and eval criteria. Needed for BMUF like learner which don't aggregate after every minibatch.
+        if (m_parameterLearners->DoAggregateMetricsIfNeededLambda)
+        {
+            NDArrayViewPtr localLossValue = nullptr;
+            if (m_aggregatedTrainingLossValue && m_aggregatedTrainingLossValue->IsInitialized())
+            {
+                localLossValue = m_aggregatedTrainingLossValue->Data();
+            }
+    }
+    }
+    
+        // this returns the map directly (read-only) and will lazily initialize it for a given seed
+    const std::vector<INDEXTYPE>& operator()(size_t seed) // throw()
+    {
+        // if wrong seed then lazily recache the sequence
+        if (seed != currentseed && randomizationrange != randomizeDisable)
+        {
+            // test for numeric overflow
+            if (map.size() - 1 != (INDEXTYPE)(map.size() - 1))
+                RuntimeError('RandomOrdering: INDEXTYPE has too few bits for this corpus');
+            // 0, 1, 2...
+            foreach_index (t, map)
+                map[t] = (INDEXTYPE) t;
+    }
+    }
+    
+    
+    {private:
+    long long m_start;
+    long long m_end;
+};
+    
+    #if 0
+        for (let& node : dependentSet)
+            fprintf(stderr, 'CloneFunction: cloning %ls\n', node->NodeDescription().c_str());
+#endif
+    
+            // if dimension not specified we assume two operands' dimensions should match
+        Input(0)->ValidateInferInputDimsFrom(TensorShape(rows1));
+    
+        // called from ComputationNode::ValidateInferInputDimsFrom()
+    // In case of an error, this function just backs out without updating.
+    // The caller must verify the dimensions.
+    // This is a bit weird since it is called after this node has been Validated once.
+    // BUGBUG: This will clear out any random initialization to 0. So currently this is not usable for most cases.
+    void InferInputDimsFrom(const TensorShape& otherShape);
+    
+    int ObjectQualityInfo60C::longitude_accel_rms(const std::uint8_t* bytes,
+                                              int32_t length) const {
+  Byte t0(bytes + 3);
+  int32_t x = t0.get_byte(0, 4);
+    }
+    
+    RadarState201::RadarState201() {}
+const uint32_t RadarState201::ID = 0x201;
+    
+      Byte t2(bytes + 6);
+  t = t2.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
+    
+      Brakerpt6c brake;
+  brake.Parse(bytes, length, &chassis_detail);
+    
+    #include 'glog/logging.h'
+    
+    void Hornrpt79::Parse(const std::uint8_t* bytes, int32_t length,
+                      ChassisDetail* chassis) const {
+  chassis->mutable_gem()->mutable_horn_rpt_79()->set_output_value(
+      output_value(bytes, length));
+  chassis->mutable_gem()->mutable_horn_rpt_79()->set_commanded_value(
+      commanded_value(bytes, length));
+  chassis->mutable_gem()->mutable_horn_rpt_79()->set_manual_input(
+      manual_input(bytes, length));
+}
