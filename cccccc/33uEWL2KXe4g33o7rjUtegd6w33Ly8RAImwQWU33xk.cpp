158c248baec8ae6d992cdc38065f52b7ead8c1b1
SUNsung <weatherius@gmail.com>
2019-05-19 08:11:08 +0300

    Rewrite [33uEWL2KXe4g33o7rjUtegd6w33Ly8RAImwQWU33xk.cpp] File in: 2019-05-19 08:11:08

--- cccccc/33uEWL2KXe4g33o7rjUtegd6w33Ly8RAImwQWU33xk.cpp
+++ cccccc/33uEWL2KXe4g33o7rjUtegd6w33Ly8RAImwQWU33xk.cpp
@@ -1,249 +1,197 @@
 
         
-          GtkRequisition menu_req;
-  gtk_widget_size_request(GTK_WIDGET(menu), &menu_req);
-  GdkScreen* screen;
-  gdk_display_get_pointer(gdk_display_get_default(), &screen, NULL, NULL, NULL);
-  gint monitor = gdk_screen_get_monitor_at_point(screen, *x, *y);
-    
-      for(auto* item: menu_items_) {
-    item->RemoveKeys();
-  }
-    
-    void MenuItem::SetSubmenu(Menu* sub_menu) {
-  submenu_ = sub_menu;
-  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
-    sub_menu->UpdateKeys(gtk_accel_group);
-  }
-  if (sub_menu == NULL)
-    gtk_menu_item_remove_submenu(GTK_MENU_ITEM(menu_item_));
-  else
-    gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item_), sub_menu->menu_);
+        void MapLiteTestUtil::ExpectClear(const unittest::TestMapLite& message) {
+  MapTestUtilImpl::ExpectClear(message);
 }
     
-    
-    {} // namespace extensions
-#endif
-
-    
-          gfx::Rect rect = gfx_display.bounds();
-      DisplayGeometry& bounds = displayResult->bounds;
-      bounds.x = rect.x();
-      bounds.y = rect.y();
-      bounds.width = rect.width();
-      bounds.height = rect.height();
-    
-    template <typename T>
-class UniversalTersePrinter {
+    // We use this class instead of ArrayByteSource to simulate a ByteSource that
+// contains multiple fragments.  ArrayByteSource returns the entire array in
+// one fragment.
+class MockByteSource : public ByteSource {
  public:
-  static void Print(const T& value, ::std::ostream* os) {
-    UniversalPrint(value, os);
-  }
-};
-template <typename T>
-class UniversalTersePrinter<T&> {
- public:
-  static void Print(const T& value, ::std::ostream* os) {
-    UniversalPrint(value, os);
-  }
-};
-template <typename T, size_t N>
-class UniversalTersePrinter<T[N]> {
- public:
-  static void Print(const T (&value)[N], ::std::ostream* os) {
-    UniversalPrinter<T[N]>::Print(value, os);
-  }
-};
-template <>
-class UniversalTersePrinter<const char*> {
- public:
-  static void Print(const char* str, ::std::ostream* os) {
-    if (str == NULL) {
-      *os << 'NULL';
-    } else {
-      UniversalPrint(string(str), os);
+  MockByteSource(StringPiece data, int block_size)
+    : data_(data), block_size_(block_size) {}
     }
-  }
-};
-template <>
-class UniversalTersePrinter<char*> {
- public:
-  static void Print(char* str, ::std::ostream* os) {
-    UniversalTersePrinter<const char*>::Print(str, os);
-  }
-};
     
-    #define EXPECT_FATAL_FAILURE_ON_ALL_THREADS(statement, substr) \
-  do { \
-    class GTestExpectFatalFailureHelper {\
-     public:\
-      static void Execute() { statement; }\
-    };\
-    ::testing::TestPartResultArray gtest_failures;\
-    ::testing::internal::SingleFailureChecker gtest_checker(\
-        &gtest_failures, ::testing::TestPartResult::kFatalFailure, (substr));\
-    {\
-      ::testing::ScopedFakeTestPartResultReporter gtest_reporter(\
-          ::testing::ScopedFakeTestPartResultReporter:: \
-          INTERCEPT_ALL_THREADS, &gtest_failures);\
-      GTestExpectFatalFailureHelper::Execute();\
-    }\
-  } while (::testing::internal::AlwaysFalse())
+    // integral_constant, defined in tr1, is a wrapper for an integer
+// value. We don't really need this generality; we could get away
+// with hardcoding the integer type to bool. We use the fully
+// general integer_constant for compatibility with tr1.
     
+    // Author: brianolson@google.com (Brian Olson)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+//
+// Test program to verify that GzipInputStream is compatible with command line
+// gunzip or java.util.zip.GzipInputStream
+//
+// Reads gzip stream on standard input and writes decompressed data to standard
+// output.
     
-    {  // To refer to typedefs in the fixture, add the 'typename
-  // TestFixture::' prefix.
-  typename TestFixture::List values;
-  values.push_back(n);
-  ...
+    int main(int argc, char* argv[]) {
+  google::protobuf::compiler::GoGoProtoGenerator generator;
+  return google::protobuf::compiler::PluginMain(argc, argv, &generator);
 }
-    
-    #endif  // GTEST_INCLUDE_GTEST_INTERNAL_GTEST_LINKED_PTR_H_
 
     
-      template <GTEST_10_TYPENAMES_(U)>
-  tuple& CopyFrom(const GTEST_10_TUPLE_(U)& t) {
-    f0_ = t.f0_;
-    f1_ = t.f1_;
-    f2_ = t.f2_;
-    f3_ = t.f3_;
-    f4_ = t.f4_;
-    f5_ = t.f5_;
-    f6_ = t.f6_;
-    f7_ = t.f7_;
-    f8_ = t.f8_;
-    f9_ = t.f9_;
-    return *this;
+    // This function fills in a Person message based on user input.
+void PromptForAddress(tutorial::Person* person) {
+  cout << 'Enter person ID number: ';
+  int id;
+  cin >> id;
+  person->set_id(id);
+  cin.ignore(256, '\n');
+    }
+    
+    #include 'src/cpp/ext/filters/census/rpc_encoding.h'
+    
+    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
+  uint64_t busy = 0, total = 0;
+  FILETIME idle, kernel, user;
+  if (GetSystemTimes(&idle, &kernel, &user) != 0) {
+    total = FiletimeToInt(kernel) + FiletimeToInt(user);
+    busy = total - FiletimeToInt(idle);
   }
-    
-    #ifndef GTEST_INCLUDE_GTEST_INTERNAL_GTEST_TYPE_UTIL_H_
-#define GTEST_INCLUDE_GTEST_INTERNAL_GTEST_TYPE_UTIL_H_
-    
-      // Now, we have that n is odd and n >= 3.
-    
-      // Asserts that s.c_string() returns NULL.
-  //
-  // <TechnicalDetails>
-  //
-  // If we write NULL instead of
-  //
-  //   static_cast<const char *>(NULL)
-  //
-  // in this assertion, it will generate a warning on gcc 3.4.  The
-  // reason is that EXPECT_EQ needs to know the types of its
-  // arguments in order to print them when it fails.  Since NULL is
-  // #defined as 0, the compiler will use the formatter function for
-  // int to print it.  However, gcc thinks that NULL should be used as
-  // a pointer, not an int, and therefore complains.
-  //
-  // The root of the problem is C++'s lack of distinction between the
-  // integer number 0 and the null pointer constant.  Unfortunately,
-  // we have to live with this fact.
-  //
-  // </TechnicalDetails>
-  EXPECT_STREQ(NULL, s.c_string());
-    
-    void ReversePolygon(b2Polygon& p){
-	ReversePolygon(p.x,p.y,p.nVertices);
+  return std::make_pair(busy, total);
 }
-	
-void ReversePolygon(float* x, float* y, int n) {
-        if (n == 1)
-            return;
-        int32 low = 0;
-        int32 high = n - 1;
-        while (low < high) {
-            float32 buffer = x[low];
-            x[low] = x[high];
-            x[high] = buffer;
-            buffer = y[low];
-            y[low] = y[high];
-            y[high] = buffer;
-            ++low;
-            --high;
+    
+    void LoadDataStore::ReportStreamCreated(const grpc::string& hostname,
+                                        const grpc::string& lb_id,
+                                        const grpc::string& load_key) {
+  per_host_stores_[hostname].ReportStreamCreated(lb_id, load_key);
+}
+    
+        void ProgressWriter::UpdateDistributedSync(size_t samples, const ValuePtr& accumulatedMetric)
+    {
+        m_distributedSync->Update(samples, nullptr, accumulatedMetric,
+            [this](const std::pair<size_t, size_t> samples, std::pair<size_t, size_t> updates,
+                   const std::pair<double, double> /*aggregateLoss*/, std::pair<double, double> aggregateMetric)
+        {
+            OnWriteDistributedSyncUpdate(samples, updates, aggregateMetric);
+        });
+    }
+    
+    void ProgressWriter::WriteTrainingSummary(const ValuePtr& accumulatedLoss, const ValuePtr& accumulatedMetric)
+    {
+        m_training->WriteSummary(
+            accumulatedLoss, accumulatedMetric,
+            [this](size_t samples, size_t updates, size_t summaries, double aggregateLoss, double aggregateMetric,
+                   uint64_t elapsedMs)
+            {
+                OnWriteTrainingSummary(samples, updates, summaries, aggregateLoss, aggregateMetric, elapsedMs);
+            });
+    }
+    
+        template <typename T> 
+    inline std::string GetVersionsString(size_t currentVersion, size_t dictVersion)
+    {
+        std::stringstream info;
+        info << 'Current ' << Typename<T>() << ' version = ' << currentVersion 
+             << ', Dictionary version = ' << dictVersion;
+        return info.str();
+    }
+    
+    #pragma once
+    
+    
+    {            return { m_packedData->GetMatrix<ElementType>(), m_packedDataLayout };
         }
-}
     
-    namespace b2ConvexDecomp {
+        void Start();
+    void Stop();
+    void Restart();
+    
+                // reshape Input(1)
+            Input(1)->SetDims(TensorShape(dimsA), false);
+            fprintf(stderr, '\n%ls %ls operation: For legacy compatibility, the sample layout of second input (%ls %ls operation) was patched to [%s] (from [%s])\n',
+                NodeName().c_str(), OperationName().c_str(), Input(1)->NodeName().c_str(), Input(1)->OperationName().c_str(), string(Input(1)->GetSampleLayout()).c_str(), dimsBstring.c_str());
+    
+        // compute backward algorithm
+    static void BackwardCompute(
+        Matrix<ElemType>& decodedpath,
+        const Matrix<ElemType>& backtrace, const size_t stp)
+    {
+        int iNumPos = backtrace.GetNumCols();
+        int iNumLab = backtrace.GetNumRows();
     }
     
-    			if (!pImageData)
-			{
-				break;
-			}
     
-    	struct RawImage
-	{
-		int uiExtendedWidth;
-		int uiExtendedHeight;
-		unsigned int uiEncodingBitsBytes;
-		std::shared_ptr<unsigned char> paucEncodingBits;
-	};
-    
-    #define MULT16_16SU(a,b) ((opus_val32)(opus_val16)(a)*(opus_val32)(opus_uint16)(b))
-#define MULT32_32_Q31(a,b) ADD32(ADD32(SHL32(MULT16_16(SHR32((a),16),SHR((b),16)),1), SHR32(MULT16_16SU(SHR32((a),16),((b)&0x0000ffff)),15)), SHR32(MULT16_16SU(SHR32((b),16),((a)&0x0000ffff)),15))
-    
-    class DHTMessageCallback;
-    
-      bool addNode(const std::shared_ptr<DHTNode>& node);
-    
-      Time serializedTime_;
-    
-    
-    {} // namespace aria2
-
-    
-        DHTRoutingTableDeserializer deserializer(family);
-    const std::string& dhtFile = e->getOption()->get(
-        family == AF_INET ? PREF_DHT_FILE_PATH : PREF_DHT_FILE_PATH6);
-    try {
-      deserializer.deserialize(dhtFile);
-      localNode = deserializer.getLocalNode();
+    {
+    {
+    {
+    {                // now assign the actual pointers 
+                for (int i = 0; i < memoryCounter; i++)
+                {
+                    auto matrixPtr = make_shared<Matrix<ElemType>>(devId);
+                    if (!matrixPtr) // this can't really happen, because we haven't started allocating memory yet
+                        LogicError('MatrixPool: failed to get a valid matrix.');
+                    for (auto& memInfo : memInfoVec)
+                    {
+                        if (memInfo.deviceId == devId && memInfo.isWorkSpace == wsFlag && memInfo.memoryId == i)
+                        {
+                            for (auto pOutMatrixPtr : memInfo.pMatrixPtrs)
+                            {
+                                *pOutMatrixPtr = matrixPtr;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-    catch (RecoverableException& e) {
-      A2_LOG_ERROR_EX(
-          fmt('Exception caught while loading DHT routing table from %s',
-              dhtFile.c_str()),
-          e);
-    }
-    if (!localNode) {
-      localNode = std::make_shared<DHTNode>();
-    }
+};
     
-    #include 'DHTTask.h'
-#include 'Logger.h'
-#include 'LogFactory.h'
-#include 'a2functional.h'
-#include 'fmt.h'
+        // a count of how many sequnces are packed for a particular frame.
+    // reset to zero, and compute from current layout information
+    // this information is useful when creating the tensor descriptors for CuDNN.
+    numSequencesForFrame2.resize(maxSeqLength);
+    fill(numSequencesForFrame2.begin(), numSequencesForFrame2.end(), 0L);
     
-    class DownloadContext;
-class PeerStorage;
-class DHTTask;
-class DHTNode;
-class DHTBucket;
     
-    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
-                                           const std::string& ipaddr,
-                                           uint16_t port,
-                                           const unsigned char* secret) const
-{
-  unsigned char src[DHT_ID_LENGTH + COMPACT_LEN_IPV6 + SECRET_SIZE];
-  memset(src, 0, sizeof(src));
-  int compactlen = bittorrent::packcompact(src + DHT_ID_LENGTH, ipaddr, port);
-  if (compactlen == 0) {
-    throw DL_ABORT_EX(fmt('Token generation failed: ipaddr=%s, port=%u',
-                          ipaddr.c_str(), port));
+    {  LineNumber line;
+  ColNumber col;
+};
+    
+    Object APCCollection::createObject() const {
+  if (m_arrayHandle->isTypedValue()) {
+    Variant local(m_arrayHandle->toLocal());
+    assertx(local.isArray());
+    return Object::attach(
+      collections::alloc(m_colType, local.getArrayData())
+    );
   }
-  memcpy(src, infoHash, DHT_ID_LENGTH);
-  memcpy(src + DHT_ID_LENGTH + COMPACT_LEN_IPV6, secret, SECRET_SIZE);
-  unsigned char md[20];
-  message_digest::digest(md, sizeof(md), MessageDigest::sha1().get(), src,
-                         sizeof(src));
-  return std::string(&md[0], &md[sizeof(md)]);
+    }
+    
+    // No `ini` binding yet. Hdf still takes precedence but will be removed
+// once we have made all options ini-aware. All new settings should
+// use the ini path of this method (i.e., pass a bogus Hdf or keep it null)
+void Config::Iterate(std::function<void (const IniSettingMap&,
+                                         const Hdf&,
+                                         const std::string&)> cb,
+                     const IniSettingMap &ini, const Hdf& config,
+                     const std::string &name,
+                     const bool prepend_hhvm /* = true */) {
+  Hdf hdf = name.empty() ? config : config[name];
+  if (hdf.exists() && !hdf.isEmpty()) {
+    for (Hdf c = hdf.firstChild(); c.exists(); c = c.next()) {
+      cb(IniSetting::Map::object, c, '');
+    }
+  } else {
+    Hdf empty;
+    auto ini_value = name.empty() ? ini :
+      ini_iterate(ini, IniName(name, prepend_hhvm));
+    if (ini_value.isArray()) {
+      for (ArrayIter iter(ini_value.toArray()); iter; ++iter) {
+        cb(iter.second(), empty, iter.first().toString().toCppString());
+      }
+    }
+  }
 }
     
     
-    {} // namespace aria2
-
+    {///////////////////////////////////////////////////////////////////////////////
+}
     
-        void markBad(const std::string& addr);
+      // implementing File
+  bool open(const String& filename, const String& mode) override;
+  bool close() override;
