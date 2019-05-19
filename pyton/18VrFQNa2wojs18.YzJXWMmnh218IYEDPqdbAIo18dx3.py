d0fc0bd526cbf0ef3245c461eee3f24edb5008a6
SUNsung <weatherius@gmail.com>
2019-05-19 06:30:37 +0300

    Rewrite [18VrFQNa2wojs18.YzJXWMmnh218IYEDPqdbAIo18dx3.py] File in: 2019-05-19 06:30:36

--- pyton/18VrFQNa2wojs18.YzJXWMmnh218IYEDPqdbAIo18dx3.py
+++ pyton/18VrFQNa2wojs18.YzJXWMmnh218IYEDPqdbAIo18dx3.py
@@ -1,82 +1,37 @@
 
         
-                When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.lookup.get(query)
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
+            def short_desc(self):
+        return 'Check spider contracts'
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
+    
+        def syntax(self):
+        return '<spider>'
+    
+        def _print_response(self, response, opts):
+        if opts.headers:
+            self._print_headers(response.request.headers, b'>')
+            print('>')
+            self._print_headers(response.headers, b'<')
         else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
+            self._print_bytes(response.body)
+    
+        def print_requests(self, lvl=None, colour=True):
+        if lvl is None:
+            levels = list(self.requests.keys())
+            if levels:
+                requests = self.requests[max(levels)]
             else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
-
+                requests = []
+        else:
+            requests = self.requests.get(lvl, [])
     
-        def add_friend_request(self, from_user_id, to_user_id):
-        pass
-    
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
-    
-    
-if __name__ == '__main__':
-    RemoveDuplicateUrls.run()
-
-    
-    
-class State(Enum):
-    unvisited = 0
-    visited = 1
-    
-        def __init__(self, key, value):
-        self.key = key
-        self.value = value
-    
-    
-class QueryApi(object):
-    
-        def __init__(self, url, contents, child_urls):
-        self.url = url
-        self.contents = contents
-        self.child_urls = child_urls
-        self.signature = self.create_signature()
-    
-        :param known_face_encodings: A list of known face encodings
-    :param face_encoding_to_check: A single face encoding to compare against the list
-    :param tolerance: How much distance between faces to consider it a match. Lower is more strict. 0.6 is typical best performance.
-    :return: A list of True/False values indicating which known_face_encodings match the face encoding to check
-    '''
-    return list(face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
-
-    
-    # This is a demo of running face recognition on a video file and saving the results to a new video file.
-#
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-        # Loop over each face found in the frame to see if it's someone we know.
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
-        name = '<Unknown Person>'
-    
-        # 载入用户上传的图片
-    img = face_recognition.load_image_file(file_stream)
-    # 为用户上传的图片中的人脸编码
-    unknown_face_encodings = face_recognition.face_encodings(img)
+        def long_desc(self):
+        return 'Run the spider defined in the given file'
