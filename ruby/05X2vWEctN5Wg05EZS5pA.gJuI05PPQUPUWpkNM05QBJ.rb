b2546ff5c9ff9d60f685c7cd1c868f27d5b44429
SUNsung <weatherius@gmail.com>
2019-05-19 09:42:33 +0300

    Rewrite [05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb] File in: 2019-05-19 09:42:33

--- ruby/05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb
+++ ruby/05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb
@@ -1,59 +1,12 @@
 
         
-                  if options['multiple']
-            add_default_name_and_id_for_value(@checked_value, options)
-            options.delete('multiple')
-          else
-            add_default_name_and_id(options)
-          end
-    
-              def hidden_field
-            hidden_name = @html_options[:name] || hidden_field_name
-            @template_object.hidden_field_tag(hidden_name, '', id: nil)
-          end
-    
-        def set_email_domain_block
-      @email_domain_block = EmailDomainBlock.find(params[:id])
+            def to_a
+      @filters.dup
     end
     
-            render template: 'admin/reports/show'
-      end
+          str.truncate(max_length).ljust(max_length) << tag.to_s
     end
     
-      def process_push_request
-    case hub_mode
-    when 'subscribe'
-      Pubsubhubbub::SubscribeService.new.call(account_from_topic, hub_callback, hub_secret, hub_lease_seconds, verified_domain)
-    when 'unsubscribe'
-      Pubsubhubbub::UnsubscribeService.new.call(account_from_topic, hub_callback)
-    else
-      ['Unknown mode: #{hub_mode}', 422]
-    end
-  end
-    
-      def create
-    active_session = current_session
-    
-      def update
-    setting.data = params[:data]
-    setting.save!
-    
-      private
-    
-      private
-    
-    ENV['COCOAPODS_DISABLE_STATS'] = 'true'
-
-    
-    class FPM::Package::NPM < FPM::Package
-  class << self
-    include FPM::Util
-  end
-  # Flags '--foo' will be accessable  as attributes[:npm_foo]
-  option '--bin', 'NPM_EXECUTABLE',
-    'The path to the npm executable you wish to run.', :default => 'npm'
-    
-      def initialize(package_name, opts = {}, &block)
-    @options = OpenStruct.new(:name => package_name.to_s)
-    @source, @target = opts.values_at(:source, :target).map(&:to_s)
-    @directory = File.expand_path(opts[:directory].to_s)
+            css('code').each do |node|
+          node.inner_html = node.inner_html.squish
+        end
