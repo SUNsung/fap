3c712fdb5a4c919a504ab63f42dd41e597efa3e7
SUNsung <weatherius@gmail.com>
2019-05-20 17:40:35 +0300

    Rewrite [18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py] File in: 2019-05-20 17:40:35

--- pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
+++ pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
@@ -1,19 +1,91 @@
 
         
-            def move_to_front(self, node):
-        pass
+            with open('README.md', 'w+') as sorted_file:
+        # Then all of the blocks are sorted individually
+        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
+        # And the result is written back to README.md
+        sorted_file.write(''.join(blocks))
     
-        def reducer(self, key, values):
-        '''Sum values for each key.
+    filenames = {
+    'bin': 'youtube-dl',
+    'exe': 'youtube-dl.exe',
+    'tar': 'youtube-dl-%s.tar.gz' % version}
+build_dir = os.path.join('..', '..', 'build', version)
+for key, filename in filenames.items():
+    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
+    fn = os.path.join(build_dir, filename)
+    with open(fn, 'rb') as f:
+        data = f.read()
+    if not data:
+        raise ValueError('File %s is empty!' % fn)
+    sha256sum = hashlib.sha256(data).hexdigest()
+    new_version[key] = (url, sha256sum)
+    
+    try:
+    input = raw_input
+except NameError:
+    pass
+    
+    sys.path.insert(0, dirn(dirn((os.path.abspath(__file__)))))
+import youtube_dl
+    
+            jsi = JSInterpreter('function f(){return (1 + 2) * 3;}')
+        self.assertEqual(jsi.call_function('f'), 9)
     
     
-class State(Enum):
-    unvisited = 0
-    visited = 1
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding model 'GroupCommitResolution'
+        db.create_table(
+            'sentry_groupcommitresolution', (
+                (
+                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
+                        primary_key=True
+                    )
+                ), (
+                    'group_id',
+                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
+                ), (
+                    'commit_id',
+                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
+                ), (
+                    'datetime', self.gf('django.db.models.fields.DateTimeField')(
+                        db_index=True
+                    )
+                ),
+            )
+        )
+        db.send_create_signal('sentry', ['GroupCommitResolution'])
     
     
-def setup(app):
-    app.add_config_value('edit_on_github_project', '', True)
-    app.add_config_value('edit_on_github_branch', 'master', True)
-    app.add_config_value('edit_on_github_src_path', '', True)  # 'eg' 'docs/'
-    app.connect('html-page-context', html_page_context)
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+    
+            # Changing field 'Environment.project_id'
+        if is_postgres():
+            db.execute('ALTER TABLE sentry_environment ALTER COLUMN project_id DROP NOT NULL')
+        else:
+            db.alter_column(
+                'sentry_environment',
+                'project_id',
+                self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)
+            )
+    
+            for release_project in RangeQuerySetWrapperWithProgressBar(
+            orm.ReleaseProject.objects.all()
+        ):
+            orm.ReleaseProject.objects.filter(id=release_project.id).update(
+                new_groups=orm.Group.objects.filter(
+                    project_id=release_project.project_id,
+                    first_release_id=release_project.release_id,
+                ).count()
+            )
+    
+    
+class Migration(DataMigration):
+    def forwards(self, orm):
+        from sentry.utils.query import RangeQuerySetWrapperWithProgressBar
+    
+        def backwards(self, orm):
+        # Removing unique constraint on 'UserOption', fields ['user', 'organization', 'key']
+        db.delete_unique('sentry_useroption', ['user_id', 'organization_id', 'key'])
