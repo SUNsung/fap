21be0c960c1b85efbcbd126e200fcfb9f2da9a9d
SUNsung <weatherius@gmail.com>
2019-05-21 02:09:23 +0300

    Rewrite [336TSE5Oqfd1I33QuR3XMqCOGg331gc.GoRCME33VsM.py] File in: 2019-05-21 02:09:22

--- pyton/336TSE5Oqfd1I33QuR3XMqCOGg331gc.GoRCME33VsM.py
+++ pyton/336TSE5Oqfd1I33QuR3XMqCOGg331gc.GoRCME33VsM.py
@@ -1,103 +1,202 @@
 
         
-        # write out the dataset
-write_datasets(FLAGS.save_dir, FLAGS.datafile_name, datasets)
-print ('Saved to ', os.path.join(FLAGS.save_dir,
-                                 FLAGS.datafile_name + '_' + dataset_name))
-
+        from __future__ import print_function
     
-    rates_b, x0s_b, _ = generate_data(rnn_b, T=T, E=E, x0s=x0s, P_sxn=P_nxn,
-                                  input_magnitude=0.0, input_times=None)
-spikes_b = spikify_data(rates_b, rng, rnn_b['dt'], rnn_b['max_firing_rate'])
-    
-      def _score_patches(self, word_patches):
-    '''Score a 2D matrix of word_patches and stitch results together.'''
-    batch_size, num_timesteps = self.shape
-    nrow, ncol = len(word_patches), len(word_patches[0])
-    max_len = num_timesteps * ncol
-    probs = np.zeros([0, max_len])  # accumulate results into this.
-    
-        with tf.gfile.Open(filename) as f:
-      idx = 0
-      for line in f:
-        word_name = line.strip()
-        if word_name == '<S>':
-          self._bos = idx
-        elif word_name == '</S>':
-          self._eos = idx
-        elif word_name == '<UNK>':
-          self._unk = idx
-        if word_name == '!!!MAXTERMID':
-          continue
-    
-      Yields:
-    Pairs of the batched data, each a matrix of shape [batch_size, num_steps].
-    The second element of the tuple is the same data time-shifted to the
-    right by one. The third is a set of weights with 1 indicating a word was
-    present and 0 not.
-    
-      tf.gfile.MakeDirs(FLAGS.output_path)
-  output_file = tf.gfile.GFile(
-      os.path.join(FLAGS.output_path, 'reviews.txt'), mode='w')
-    
-    # Dependency imports
+    from .cifar import load_batch
+from ..utils.data_utils import get_file
+from .. import backend as K
 import numpy as np
-    
-      if FLAGS.dis_share_embedding:
-    assert hparams.dis_rnn_size == hparams.gen_rnn_size, (
-        'If you wish to share Discriminator/Generator embeddings, they must be'
-        ' same dimension.')
-    with tf.variable_scope('gen/rnn', reuse=True):
-      embedding = tf.get_variable('embedding',
-                                  [FLAGS.vocab_size, hparams.gen_rnn_size])
-    
-        n_features = 10
-    list_n_samples = np.linspace(100, 1000000, 5).astype(np.int)
-    lasso_results, lars_lasso_results = compute_bench(alpha, list_n_samples,
-                                            [n_features], precompute=True)
-    
-    We selected two sets of two variables from the Boston housing data set
-as an illustration of what kind of analysis can be done with several
-outlier detection tools. For the purpose of visualization, we are working
-with two-dimensional examples, but one should be aware that things are
-not so trivial in high-dimension, as it will be pointed out.
-    
-    from sklearn import manifold, datasets
-    
-    # histogram
-plt.figure(4, figsize=(3, 2.2))
-plt.clf()
-plt.axes([.01, .01, .98, .98])
-plt.hist(X, bins=256, color='.5', edgecolor='.5')
-plt.yticks(())
-plt.xticks(regular_values)
-values = np.sort(values)
-for center_1, center_2 in zip(values[:-1], values[1:]):
-    plt.axvline(.5 * (center_1 + center_2), color='b')
-    
-    # Different variance
-X_varied, y_varied = make_blobs(n_samples=n_samples,
-                                cluster_std=[1.0, 2.5, 0.5],
-                                random_state=random_state)
-y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_varied)
-    
-        file_path = './data.txt'
-    data = load_data(file_path)
+import os
     
     
-def sigmoid(x):
-    ''''''
-    return tf.nn.sigmoid(x)
+def test_preprocess_input_symbolic():
+    # Test image batch
+    x = np.random.uniform(0, 255, (2, 10, 10, 3))
+    inputs = Input(shape=x.shape[1:])
+    outputs = Lambda(utils.preprocess_input, output_shape=x.shape[1:])(inputs)
+    model = Model(inputs, outputs)
+    assert model.predict(x).shape == x.shape
     
     
-def get_shape(x):
-    '''
-    References:
-        tflearn.utils.get_incoming_shape
-    '''
-    if isinstance(x, (tf.Tensor, tf.SparseTensor)):
-        return x.get_shape().as_list()
-    elif type(x) in [np.array, np.ndarray, list, tuple]:
-        return list(np.shape(x))
-    else:
-        raise Exception('Invalid `x`.')
+def test_cifar():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data('fine')
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data('coarse')
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
+    
+    # the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    
+                if new_key is None:
+                break
+    
+        def solve_sub_array(self):
+        rear = [int(self.array[0])]*len(self.array)
+        sum_value = [int(self.array[0])]*len(self.array)
+        for i in range(1, len(self.array)):
+            sum_value[i] = max(int(self.array[i]) + sum_value[i-1], int(self.array[i]))
+            rear[i] = max(sum_value[i], rear[i-1])
+        return rear[len(self.array)-1]
+    
+        difference = predict - actual
+    numerator = np.sum(difference) / len(predict) 
+    denumerator =  np.sum(actual) / len(predict)
+    print(numerator)
+    print(denumerator)
+    
+            sha1 = get_linux_ca_sha1(nss_path)
+        ca_hash = CertUtil.ca_thumbprint.replace(':', '')
+        if sha1 == ca_hash:
+            xlog.info('Database $HOME/.pki/nssdb cert exist')
+            return
+    
+    @section tokenstrees Tokens and Trees
+    
+    
+    def getCharPositionInLine(self):
+        return self.input.charPositionInLine
+    
+    
+class HeaderTest(unittest.TestCase):
+    '''Tests for acme.jws.Header.'''
+    
+    HEADER_ARGS = {'Strict-Transport-Security': HSTS_ARGS,
+               'Upgrade-Insecure-Requests': UIR_ARGS}
+    
+    # Additional templates that should be rendered to pages, maps page names to
+# template names.
+#html_additional_pages = {}
+    
+    Use of this plugin requires a configuration file containing Cloudflare API
+credentials, obtained from your Cloudflare
+`account page <https://www.cloudflare.com/a/account/my-account>`_.
+    
+       certbot certonly \\
+     --dns-cloudxns \\
+     --dns-cloudxns-credentials ~/.secrets/certbot/cloudxns.ini \\
+     --dns-cloudxns-propagation-seconds 60 \\
+     -d example.com
+    
+    # Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (master_doc, 'certbot-dns-cloudxns', u'certbot-dns-cloudxns Documentation',
+     author, 'certbot-dns-cloudxns', 'One line description of project.',
+     'Miscellaneous'),
+]
+    
+            '''
+        self.command = None  # set in case of error on the first line
+        self.request_version = version = self.default_request_version
+        self.close_connection = True
+        requestline = str(self.raw_requestline, 'iso-8859-1')
+        requestline = requestline.rstrip('\r\n')
+        self.requestline = requestline
+        words = requestline.split()
+        if len(words) == 0:
+            return False
+    
+            The record is formatted, and then sent to the syslog server. If
+        exception information is present, it is NOT sent to the server.
+        '''
+        try:
+            msg = self.format(record)
+            if self.ident:
+                msg = self.ident + msg
+            if self.append_nul:
+                msg += '\000'
+    
+    # Add the html version.  This converts the message into a multipart/alternative
+# container, with the original text message as the first part and the new html
+# message as the second part.
+asparagus_cid = make_msgid()
+msg.add_alternative('''\
+<html>
+  <head></head>
+  <body>
+    <p>Salut!</p>
+    <p>Cela ressemble à un excellent
+        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
+            recipie
+        </a> déjeuner.
+    </p>
+    <img src='cid:{asparagus_cid}' />
+  </body>
+</html>
+'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
+# note that we needed to peel the <> off the msgid for use in the html.
+    
+    
+def main():
+    parser = ArgumentParser(description='''\
+Send the contents of a directory as a MIME message.
+Unless the -o option is given, the email is sent by forwarding to your local
+SMTP server, which then does the normal delivery process.  Your local machine
+must be running an SMTP server.
+''')
+    parser.add_argument('-d', '--directory',
+                        help='''Mail the contents of the specified directory,
+                        otherwise use the current directory.  Only the regular
+                        files in the directory are sent, and we don't recurse to
+                        subdirectories.''')
+    parser.add_argument('-o', '--output',
+                        metavar='FILE',
+                        help='''Print the composed message to FILE instead of
+                        sending the message to the SMTP server.''')
+    parser.add_argument('-s', '--sender', required=True,
+                        help='The value of the From: header (required)')
+    parser.add_argument('-r', '--recipient', required=True,
+                        action='append', metavar='RECIPIENT',
+                        default=[], dest='recipients',
+                        help='A To: header value (at least one required)')
+    args = parser.parse_args()
+    directory = args.directory
+    if not directory:
+        directory = '.'
+    # Create the message
+    msg = EmailMessage()
+    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
+    msg['To'] = ', '.join(args.recipients)
+    msg['From'] = args.sender
+    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
+    
+        counter = 1
+    for part in msg.walk():
+        # multipart/* are just containers
+        if part.get_content_maintype() == 'multipart':
+            continue
+        # Applications should really sanitize the given filename so that an
+        # email message can't be used to overwrite important files
+        filename = part.get_filename()
+        if not filename:
+            ext = mimetypes.guess_extension(part.get_content_type())
+            if not ext:
+                # Use a generic bag-of-bits extension
+                ext = '.bin'
+            filename = 'part-%03d%s' % (counter, ext)
+        counter += 1
+        with open(os.path.join(args.directory, filename), 'wb') as fp:
+            fp.write(part.get_payload(decode=True))
+    
+    print('Enter your SQL commands to execute in sqlite3.')
+print('Enter a blank line to exit.')
+    
+        assert len(kernel_size) == n_layer, 'len(kernel_size) == n_layer'
+    
+      label_list = processor.get_labels()
+    
+      def tokenize(self, text):
+    split_tokens = []
+    for token in self.basic_tokenizer.tokenize(text):
+      for sub_token in self.wordpiece_tokenizer.tokenize(token):
+        split_tokens.append(sub_token)
