6d2f492f0e99711812763036bfcaf7bb60de0b53
SUNsung <weatherius@gmail.com>
2019-05-21 00:31:43 +0300

    Rewrite [486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp] File in: 2019-05-21 00:31:42

--- cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
+++ cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
@@ -1,164 +1,77 @@
 
         
-        SILLayout::SILLayout(CanGenericSignature Sig,
-                     ArrayRef<SILField> Fields)
-  : GenericSigAndFlags(Sig, getFlagsValue(anyMutable(Fields))),
-    NumFields(Fields.size())
+        #ifndef ATOM_APP_COMMAND_LINE_ARGS_H_
+#define ATOM_APP_COMMAND_LINE_ARGS_H_
+    
+    #include 'atom/browser/api/event_emitter.h'
+    
+    
+    {}  // namespace api
+    
+    class AutoUpdater {
+ public:
+  typedef std::map<std::string, std::string> HeaderMap;
+    }
+    
+    namespace atom {
+    }
+    
+    #endif  // ATOM_BROWSER_NET_URL_REQUEST_ABOUT_JOB_H_
+
+    
+    
+    {}  // namespace atom
+    
+        // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    
+            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+    }
+    
+    bool ImGui::InputTextMultiline(const char* label, std::string* str, const ImVec2& size, ImGuiInputTextFlags flags, ImGuiInputTextCallback callback, void* user_data)
 {
-#ifndef NDEBUG
-  verifyFields(Sig, Fields);
-#endif
-  auto FieldsMem = getTrailingObjects<SILField>();
-  for (unsigned i : indices(Fields)) {
-    new (FieldsMem + i) SILField(Fields[i]);
-  }
+    IM_ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
+    flags |= ImGuiInputTextFlags_CallbackResize;
+    }
+    
+            static float f = 0.0f;
+        ImGui::Text('Hello, world!');
+        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);
+        ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / io.Framerate, io.Framerate);
+        ImGui::ShowDemoWindow(NULL);
+    
+        // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window* window = SDL_CreateWindow('Dear ImGui SDL2+OpenGL3 example', SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+    
+        io.SetClipboardTextFn = ImGui_ImplSDL2_SetClipboardText;
+    io.GetClipboardTextFn = ImGui_ImplSDL2_GetClipboardText;
+    io.ClipboardUserData = NULL;
+    
+        // Backup GL state
+    GLint last_texture; glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
+    GLint last_polygon_mode[2]; glGetIntegerv(GL_POLYGON_MODE, last_polygon_mode);
+    GLint last_viewport[4]; glGetIntegerv(GL_VIEWPORT, last_viewport);
+    GLint last_scissor_box[4]; glGetIntegerv(GL_SCISSOR_BOX, last_scissor_box);
+    glPushAttrib(GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT | GL_TRANSFORM_BIT);
+    
+    #include 'gtest/gtest.h'
+    
+    using ::apollo::drivers::canbus::Byte;
+    
+    
+    {  Global_rpt_6a::Override_statusType ret =
+      static_cast<Global_rpt_6a::Override_statusType>(x);
+  return ret;
 }
-    
-      auto buffer = clangSrcMgr.getBuffer(decomposedLoc.first);
-  unsigned mirrorID;
-    
-      bool isGetter() const {
-    return accessorKind == IAMAccessorKind::Getter;
-  }
-    
-    void VerifyDebugInfoJobAction::anchor() {}
-    
-    #endif
-
-    
-    #endif
-
-    
-    #define FILL_LINES2(macro,type) \
-            macro##_LINE(type,0) \
-            macro##_LINE(type,1)
-#define FILL_LINES3(macro,type) \
-            FILL_LINES2(macro,type) \
-            macro##_LINE(type,2)
-#define FILL_LINES4(macro,type) \
-            FILL_LINES3(macro,type) \
-            macro##_LINE(type,3)
-    
-    
-    {            v_y = vld2q_u8(srcy + syj + 32);
-            v_dst.val[0] = vld1q_u8(srcu + sj + 16);
-            v_dst.val[1] = v_y.val[0];
-            v_dst.val[2] = vld1q_u8(srcv + sj + 16);
-            v_dst.val[3] = v_y.val[1];
-            vst4q_u8(dst + dj + 64, v_dst);
-        }
-#endif
-    
-    namespace CAROTENE_NS {
-    }
-    
-             vline_f32 = vaddq_f32(vline_f32, vhalf);
-         vline_s32 = vcvtq_s32_f32(vline_f32);
-    
-                    vst1_u8(dst + x, ls);
-            }
-            break;
-        case 2:
-            for (; x <= colsn - 8*2; x += 8*2)
-            {
-                internal::prefetch(lane + x);
-    
-            uint32x4_t el4h = vaddq_u32(el8shr01l, el8shr01h);
-    
-    ```
-    
-              const TensorShape& X = in[0];
-          int N = 0, C = 0, H = 0, W = 0;
-          switch (order) {
-            case StorageOrder::NCHW:
-              N = X.dims(0);
-              C = X.dims(1);
-              H = X.dims(2);
-              W = X.dims(3);
-              break;
-            case StorageOrder::NHWC:
-              N = X.dims(0);
-              H = X.dims(1);
-              W = X.dims(2);
-              C = X.dims(3);
-              break;
-            default:
-              CAFFE_THROW('Unknown storage order: ', order);
-          }
-    
-    #endif /* #if !UCONFIG_NO_TRANSLITERATION */
-
-    
-    U_CAPI void U_EXPORT2
-uhash_deleteScriptSet(void *obj) {
-    icu::ScriptSet *s = static_cast<icu::ScriptSet *>(obj);
-    delete s;
-}
-
-    
-    void
-SelectFormat::applyPattern(const UnicodeString& newPattern, UErrorCode& status) {
-    if (U_FAILURE(status)) {
-      return;
-    }
-    }
-    
-    #if !UCONFIG_NO_BREAK_ITERATION
-    
-    #include 'unicode/utypes.h'
-#include 'sharedobject.h'
-    
-    UnicodeString &
-SmallIntFormatter::format(
-        int32_t smallPositiveValue,
-        const IntDigitCountRange &range,
-        UnicodeString &appendTo) {
-    int32_t digits = range.pin(gDigitCount[smallPositiveValue]);
-    }
-    
-    #include 'src/heap/heap-inl.h'
-    
-    
-// Returns number of bytes used, including *data.
-int DisassemblerIA32::JumpShort(byte* data) {
-  DCHECK_EQ(0xEB, *data);
-  byte b = *(data+1);
-  byte* dest = data + static_cast<int8_t>(b) + 2;
-  AppendToBuffer('jmp %s', NameOfAddress(dest));
-  return 2;
-}
-    
-    
-    {
-    {}  // namespace internal
-}  // namespace v8
-    
-    Operand TurboAssembler::ExternalReferenceAsOperand(ExternalReference reference,
-                                                   Register scratch) {
-  // TODO(jgruber): Add support for enable_root_array_delta_access.
-  if (root_array_available() && options().isolate_independent_code) {
-    if (IsAddressableThroughRootRegister(isolate(), reference)) {
-      // Some external references can be efficiently loaded as an offset from
-      // kRootRegister.
-      intptr_t offset =
-          RootRegisterOffsetForExternalReference(isolate(), reference);
-      return Operand(kRootRegister, offset);
-    } else {
-      // Otherwise, do a memory load from the external reference table.
-      mov(scratch, Operand(kRootRegister,
-                           RootRegisterOffsetForExternalReferenceTableEntry(
-                               isolate(), reference)));
-      return Operand(scratch, 0);
-    }
-  }
-  Move(scratch, Immediate(reference));
-  return Operand(scratch, 0);
-}
-    
-    #define SSSE3_INSTRUCTION_LIST(V) \
-  V(phaddd, 66, 0F, 38, 02)       \
-  V(phaddw, 66, 0F, 38, 01)       \
-  V(pshufb, 66, 0F, 38, 00)       \
-  V(psignb, 66, 0F, 38, 08)       \
-  V(psignw, 66, 0F, 38, 09)       \
-  V(psignd, 66, 0F, 38, 0A)
