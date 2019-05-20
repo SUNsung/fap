ff0ba10cb60a815a097edc6bd05203cf4f068558
SUNsung <weatherius@gmail.com>
2019-05-20 10:50:46 +0300

    Rewrite [262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py] File in: 2019-05-20 10:50:45

--- pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
+++ pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
@@ -1,62 +1,153 @@
 
         
-            def setUp(self):
-        BaseTestCase.setUp(self)
-        self.cwd = os.getcwd()
-        basetempdir = tempfile.gettempdir()
-        os.chdir(basetempdir)
-        self.data = b'We are the knights who say Ni!'
-        self.tempdir = tempfile.mkdtemp(dir=basetempdir)
-        self.tempdir_name = os.path.basename(self.tempdir)
-        self.base_url = '/' + self.tempdir_name
-        tempname = os.path.join(self.tempdir, 'test')
-        with open(tempname, 'wb') as temp:
-            temp.write(self.data)
-            temp.flush()
-        mtime = os.stat(tempname).st_mtime
-        # compute last modification datetime for browser cache tests
-        last_modif = datetime.datetime.fromtimestamp(mtime,
-            datetime.timezone.utc)
-        self.last_modif_datetime = last_modif.replace(microsecond=0)
-        self.last_modif_header = email.utils.formatdate(
-            last_modif.timestamp(), usegmt=True)
+            def load(self):
+        try:
+            session_data = self._cache.get(self.cache_key)
+        except Exception:
+            # Some backends (e.g. memcache) raise an exception on invalid
+            # cache keys. If this happens, reset the session. See #17810.
+            session_data = None
+        if session_data is not None:
+            return session_data
+        self._session_key = None
+        return {}
     
-        def discover(self, start_dir, pattern='test*.py', top_level_dir=None):
-        '''Find and return all test modules from the specified start
-        directory, recursing into subdirectories to find them and return all
-        tests found within them. Only test files that match the pattern will
-        be loaded. (Using shell style pattern matching.)
     
-        def test_additive(self):
-        # Additive
-        self.check_tokenize('x = 1 - y + 15 - 1 + 0x124 + z + a[5]', '''\
-    NAME       'x'           (1, 0) (1, 1)
-    OP         '='           (1, 2) (1, 3)
-    NUMBER     '1'           (1, 4) (1, 5)
-    OP         '-'           (1, 6) (1, 7)
-    NAME       'y'           (1, 8) (1, 9)
-    OP         '+'           (1, 10) (1, 11)
-    NUMBER     '15'          (1, 12) (1, 14)
-    OP         '-'           (1, 15) (1, 16)
-    NUMBER     '1'           (1, 17) (1, 18)
-    OP         '+'           (1, 19) (1, 20)
-    NUMBER     '0x124'       (1, 21) (1, 26)
-    OP         '+'           (1, 27) (1, 28)
-    NAME       'z'           (1, 29) (1, 30)
-    OP         '+'           (1, 31) (1, 32)
-    NAME       'a'           (1, 33) (1, 34)
-    OP         '['           (1, 34) (1, 35)
-    NUMBER     '5'           (1, 35) (1, 36)
-    OP         ']'           (1, 36) (1, 37)
-    ''')
+def x_robots_tag(func):
+    @wraps(func)
+    def inner(request, *args, **kwargs):
+        response = func(request, *args, **kwargs)
+        response['X-Robots-Tag'] = 'noindex, noodp, noarchive'
+        return response
+    return inner
     
-        # Fetch the records to be pickled.
-    cursor.execute('SELECT * FROM memos')
-    memos = [MemoRecord(key, task) for key, task in cursor]
-    # Save the records using our custom DBPickler.
-    file = io.BytesIO()
-    DBPickler(file).dump(memos)
+        for node in doctree.traverse(settingslist_node):
+        settings_list = nodes.bullet_list()
+        settings_list.extend([make_setting_element(d, app, fromdocname)
+                              for d in sorted(env.scrapy_all_settings,
+                                              key=itemgetter('setting_name'))
+                              if fromdocname != d['docname']])
+        node.replace_self(settings_list)
     
+    '''
+    
+            self.crawler_process.crawl(spidercls, **opts.spargs)
+        self.crawler_process.start()
+    
+        text = html.remove_tags_with_content(text, ('script', 'noscript'))
+    text = html.replace_entities(text)
+    text = html.remove_comments(text)
+    return _ajax_crawlable_re.search(text) is not None
+
+    
+        if '_text' in dictified['video'][0]['file'][0]:  #link exist
+        video_dict['links'] = [i['file'][0]['_text'].strip() for i in dictified['video']]
+    
+        ep = 'http://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={}'
+    
+    	xml = get_html('http://www.ehow.com/services/video/series.xml?demand_ehow_videoid=%s' % vid)
+    
+	from xml.dom.minidom import parseString
+	doc = parseString(xml)
+	tab = doc.getElementsByTagName('related')[0].firstChild
+    
+    
+class Funshion(VideoExtractor):
+    name = 'funshion'
+    stream_types = [
+        {'id': 'sdvd'},
+        {'id': 'sdvd_h265'},
+        {'id': 'hd'},
+        {'id': 'hd_h265'},
+        {'id': 'dvd'},
+        {'id': 'dvd_h265'},
+        {'id': 'tv'},
+        {'id': 'tv_h265'}
+    ]
+    a_mobile_url = 'http://m.fun.tv/implay/?mid=302555'
+    video_ep = 'http://pv.funshion.com/v7/video/play/?id={}&cl=mweb&uc=111'
+    media_ep = 'http://pm.funshion.com/v7/media/play/?id={}&cl=mweb&uc=111'
+    coeff = None
+    
+        # 'The specifier name is a ``dotted name'' that may resolve either to
+    # a module, a test case class, a TestSuite instance, a test method
+    # within a test case class, or a callable object which returns a
+    # TestCase or TestSuite instance.'
+    # ...
+    # 'The method optionally resolves name relative to the given module'
     #
-# Function used to calculate result
-#
+    # What happens when presented with an impossible attribute name?
+    def test_loadTestsFromNames__relative_malformed_name(self):
+        loader = unittest.TestLoader()
+    
+    About = help_about.AboutDialog
+    
+    
+SYMBOL_FILE              = support.findfile('symbol.py')
+GEN_SYMBOL_FILE          = os.path.join(os.path.dirname(__file__),
+                                        '..', '..', 'Tools', 'scripts',
+                                        'generate_symbol_py.py')
+GRAMMAR_FILE             = os.path.join(os.path.dirname(__file__),
+                                        '..', '..', 'Include', 'graminit.h')
+TEST_PY_FILE             = 'symbol_test.py'
+    
+    # We can extract the richest alternative in order to display it:
+richest = msg.get_body()
+partfiles = {}
+if richest['content-type'].maintype == 'text':
+    if richest['content-type'].subtype == 'plain':
+        for line in richest.get_content().splitlines():
+            print(line)
+        sys.exit()
+    elif richest['content-type'].subtype == 'html':
+        body = richest
+    else:
+        print('Don't know how to display {}'.format(richest.get_content_type()))
+        sys.exit()
+elif richest['content-type'].content_type == 'multipart/related':
+    body = richest.get_body(preferencelist=('html'))
+    for part in richest.iter_attachments():
+        fn = part.get_filename()
+        if fn:
+            extension = os.path.splitext(part.get_filename())[1]
+        else:
+            extension = mimetypes.guess_extension(part.get_content_type())
+        with tempfile.NamedTemporaryFile(suffix=extension, delete=False) as f:
+            f.write(part.get_content())
+            # again strip the <> to go from email form of cid to html form.
+            partfiles[part['content-id'][1:-1]] = f.name
+else:
+    print('Don't know how to display {}'.format(richest.get_content_type()))
+    sys.exit()
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+    # The magic_html_parser has to rewrite the href='cid:....' attributes to
+    # point to the filenames in partfiles.  It also has to do a safety-sanitize
+    # of the html.  It could be written using html.parser.
+    f.write(magic_html_parser(body.get_content(), partfiles))
+webbrowser.open(f.name)
+os.remove(f.name)
+for fn in partfiles.values():
+    os.remove(fn)
+    
+    def handleSlideTitle(title):
+    print('<h2>%s</h2>' % getText(title.childNodes))
+    
+                if buffer.lstrip().upper().startswith('SELECT'):
+                print(cur.fetchall())
+        except sqlite3.Error as e:
+            print('An error occurred:', e.args[0])
+        buffer = ''
+    
+    
+class RetriesExceededError(Error):
+    pass
+
+    
+        Args:
+        seed (Optional[int]): None seeds from an operating system specific randomness source.
+        max_bytes: Maximum number of bytes to use in the hashed seed.
+    '''
+    if seed is None:
+        seed = create_seed(max_bytes=max_bytes)
+    hash = hashlib.sha512(str(seed).encode('utf8')).digest()
+    return _bigint_from_bytes(hash[:max_bytes])
