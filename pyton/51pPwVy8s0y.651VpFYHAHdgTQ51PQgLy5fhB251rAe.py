9fb28f61e7b0978b017d858ab1fa85d392d676c7
SUNsung <weatherius@gmail.com>
2019-05-20 10:28:09 +0300

    Rewrite [51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py] File in: 2019-05-20 10:28:08

--- pyton/51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py
+++ pyton/51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py
@@ -1,134 +1,40 @@
 
         
-          wname = (name + '/W') if name else '/W'
-    
-        fname = os.path.join(FLAGS.save_dir, 'lstm_emb_step_%d.npy' % i)
-    with tf.gfile.Open(fname, mode='w') as f:
-      np.save(f, lstm_emb)
-    sys.stderr.write('LSTM embedding step %d file saved\n' % i)
-    
-    
-def parse_commonsense_reasoning_test(test_data_name):
-  '''Read JSON test data.'''
-  with tf.gfile.Open(os.path.join(
-      FLAGS.data_dir, 'commonsense_test',
-      '{}.json'.format(test_data_name)), 'r') as f:
-    data = json.load(f)
-    
-      for n in xrange(num_batches):
-    indices_batch = indices[n]
-    new_arr_batch = new_arr[n]
-    
-            # init attention
-        attention = _init_attention(encoder_state)
-      else:
-        # construct attention
-        attention = attention_construct_fn(cell_output, attention_keys,
-                                           attention_values)
-        cell_output = attention
-    
-    with open('update/LATEST_VERSION', 'w') as f:
-    f.write(version)
-    
-        def gen_ies_md(ies):
-        for ie in ies:
-            ie_md = '**{0}**'.format(ie.IE_NAME)
-            ie_desc = getattr(ie, 'IE_DESC', None)
-            if ie_desc is False:
-                continue
-            if ie_desc is not None:
-                ie_md += ': {0}'.format(ie.IE_DESC)
-            if not ie.working():
-                ie_md += ' (Currently broken)'
-            yield ie_md
-    
-    ZSH_COMPLETION_FILE = 'youtube-dl.zsh'
-ZSH_COMPLETION_TEMPLATE = 'devscripts/zsh-completion.in'
-    
-        def test_encrypt(self):
-        msg = b'message'
-        key = list(range(16))
-        encrypted = aes_encrypt(bytes_to_intlist(msg), key)
-        decrypted = intlist_to_bytes(aes_decrypt(encrypted, key))
-        self.assertEqual(decrypted, msg)
-    
-    
-if __name__ == '__main__':
-    unittest.main()
+            def __init__(self):
+        self.name = 'unaccent'
 
     
-        def test_proxy_http(self):
-        params = self._check_params(['primary_proxy', 'primary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL({
-            'proxy': params['primary_proxy']
-        })
-        self.assertEqual(
-            ydl.urlopen('http://yt-dl.org/ip').read().decode('utf-8'),
-            params['primary_server_ip'])
+        def save(self, must_create=False):
+        '''
+        Save the current session data to the database. If 'must_create' is
+        True, raise a database error if the saving operation doesn't create a
+        new entry (as opposed to possibly updating an existing entry).
+        '''
+        if self.session_key is None:
+            return self.create()
+        data = self._get_session(no_load=must_create)
+        obj = self.create_model_instance(data)
+        using = router.db_for_write(self.model, instance=obj)
+        try:
+            with transaction.atomic(using=using):
+                obj.save(force_insert=must_create, force_update=not must_create, using=using)
+        except IntegrityError:
+            if must_create:
+                raise CreateError
+            raise
+        except DatabaseError:
+            if not must_create:
+                raise UpdateError
+            raise
     
-            for deferred in self.deferred_functions:
-            deferred(state)
+        # TODO: decoder should check that nonce is in the protected header
     
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
+    import josepy as jose
     
-        package is the name of the root module of the package
+            # Retrieve list of modified files
+        # Note: Noop saves can cause the file to be listed twice, I used a
+        # set to remove this possibility. This is a known augeas 0.10 error.
+        save_paths = self.aug.match('/augeas/events/saved')
     
-    import sys
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import load_files
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-    
-        # TASK: Build a grid search to find out whether unigrams or bigrams are
-    # more useful.
-    # Fit the pipeline on the training set using grid search for the parameters
-    parameters = {
-        'vect__ngram_range': [(1, 1), (1, 2)],
-    }
-    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
-    grid_search.fit(docs_train, y_train)
-    
-    The outer product of the row and column label vectors shows a
-representation of the checkerboard structure.
-    
-    n_clusters = 3
-    
-    for center_1, center_2 in zip(regular_values[:-1], regular_values[1:]):
-    plt.axvline(.5 * (center_1 + center_2), color='b', linestyle='--')
-    
-        changed = True  # 标记样本类别是否改变
-    n_iter = 0  # 记录迭代次数
-    while changed and n_iter < max_iter:
-        changed = False
-        n_iter += 1
-    
-    
-def hard_sigmoid(x):
-    '''
-    x = 0.                  x < -2.5
-      = 1.                  x > 2.5
-      = 0.2 * x + 0.5       otherwise
-    '''
-    x = (0.2 * x) + 0.5
-    x = tf.clip_by_value(x, 0., 1.)
-    return x
-    
-        return x
-    
-        #Required strings to create intermediate HTML files
-    header = '<html><head><link rel=stylesheet type=text/css href=' + colorscheme + '.css></head><body>\n'
-    footer = '</body></html>'
-    title_content = '<h1 class=titlemain>tldr pages</h1><h4 class=titlesub>Simplified and community driven man pages</h4></body></html>'
+    # Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
