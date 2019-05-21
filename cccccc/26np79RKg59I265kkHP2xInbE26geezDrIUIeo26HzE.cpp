371db55a5ec63934f4049d66a2024928ff456502
SUNsung <weatherius@gmail.com>
2019-05-21 04:29:14 +0300

    Rewrite [26np79RKg59I265kkHP2xInbE26geezDrIUIeo26HzE.cpp] File in: 2019-05-21 04:29:14

--- cccccc/26np79RKg59I265kkHP2xInbE26geezDrIUIeo26HzE.cpp
+++ cccccc/26np79RKg59I265kkHP2xInbE26geezDrIUIeo26HzE.cpp
@@ -1,415 +1,186 @@
 
         
-        REGISTER_CPU_OPERATOR(
-    MergeSingleMapFeatureTensors,
-    MergeSingleMapFeatureTensorsOp<CPUContext>);
-OPERATOR_SCHEMA(MergeSingleMapFeatureTensors)
-    .SetDoc(
-        'Merge given single-feature tensors with map features into one '
-        'multi-feature tensor.' +
-        doc)
-    .NumInputs([](int n) { return n >= 4 && n % 4 == 0; })
-    .NumOutputs(5)
-    .Input(0, 'in1_lengths', '.lengths')
-    .Input(1, 'in1_keys', '.keys')
-    .Input(2, 'in1_values', '.values')
-    .Input(3, 'in1_presence', '.presence')
-    .Output(0, 'out_lengths', '.lengths')
-    .Output(1, 'out_keys', '.keys')
-    .Output(2, 'out_values_lengths', '.values.lengths')
-    .Output(3, 'out_values_keys', '.values.keys')
-    .Output(4, 'out_values_values', '.values.values')
-    .Arg('feature_ids', 'feature ids');
-    
-    Contrast Example 2 with Example 1. For each data point per feature, the values
-are sorted by the corresponding KEY.
-)DOC')
-    .Input(0, 'DATA', 'Tensor of rank 1.')
-    .Input(
-        1,
-        'RANGES',
-        'Tensor of int32/int64 ranges, of dims (N, M, 2). '
-        'Where N is number of examples and M is a size of each example. '
-        'Last dimention represents a range in the format (start, lengths)')
-    .Input(2, 'KEY', 'Tensor of rank 1 and type int64.')
-    .Output(0, 'OUTPUT', '1-D tensor of size sum of range lengths')
-    .Arg('lengths', 'Expected lengths for ranges')
-    .TensorInferenceFunction([](const OperatorDef& def,
-                                const vector<TensorShape>& in) {
-      ArgumentHelper helper(def);
-      auto lengths = helper.GetRepeatedArgument<int>('lengths');
-      CAFFE_ENFORCE_EQ(in[0].dims_size(), 1, 'DATA should be 1-D tensor.');
-      CAFFE_ENFORCE_EQ(in[1].dims_size(), 3, 'RANGES should be 3-D tensor.');
-      if (in.size() > 2) {
-        CAFFE_ENFORCE_EQ(in[2].dims_size(), 1, 'KEY should be 1-D tensor.');
-      }
-      CAFFE_ENFORCE_GT(lengths.size(), 0, 'lengths should be non-empty.');
-      std::vector<TensorShape> out(lengths.size());
-      for (int i = 0; i < lengths.size(); ++i) {
-        out[i].set_data_type(in[0].data_type());
-        out[i].add_dims(in[1].dims(0));
-        out[i].add_dims(lengths[i]);
-      }
-      return out;
-    });
-    
-    
-    {  TBLOB* blob;
-  Tesseract* tesseract;
-  BLOB_CHOICE_LIST** choices;
-};
-    
-    // Computes the histogram for the given image rectangle, and the given
-// single channel. Each channel is always one byte per pixel.
-// Histogram is always a kHistogramSize(256) element array to count
-// occurrences of each pixel value.
-void HistogramRect(Pix* src_pix, int channel,
-                   int left, int top, int width, int height,
-                   int* histogram);
-    
-    // AMBIG_TABLE[i] stores a set of ambiguities whose
-// wrong ngram starts with unichar id i.
-using UnicharAmbigsVector = GenericVector<AmbigSpec_LIST *>;
-    
-    U_NAMESPACE_BEGIN
-    
-    
-//--------------------------------------------------------------------------
-//
-//   Clone
-//
-//--------------------------------------------------------------------------
-RegexPattern  *RegexPattern::clone() const {
-    RegexPattern  *copy = new RegexPattern(*this);
-    return copy;
-}
-    
-    ScriptSet &ScriptSet::parseScripts(const UnicodeString &scriptString, UErrorCode &status) {
-    resetAll();
-    if (U_FAILURE(status)) {
-        return *this;
-    }
-    UnicodeString oneScriptName;
-    for (int32_t i=0; i<scriptString.length();) {
-        UChar32 c = scriptString.char32At(i);
-        i = scriptString.moveIndex32(i, 1);
-        if (!u_isUWhiteSpace(c)) {
-            oneScriptName.append(c);
-            if (i < scriptString.length()) {
-                continue;
-            }
-        }
-        if (oneScriptName.length() > 0) {
-            char buf[40];
-            oneScriptName.extract(0, oneScriptName.length(), buf, sizeof(buf)-1, US_INV);
-            buf[sizeof(buf)-1] = 0;
-            int32_t sc = u_getPropertyValueEnum(UCHAR_SCRIPT, buf);
-            if (sc == UCHAR_INVALID_CODE) {
-                status = U_ILLEGAL_ARGUMENT_ERROR;
-            } else {
-                this->set((UScriptCode)sc, status);
-            }
-            if (U_FAILURE(status)) {
-                return *this;
-            }
-            oneScriptName.remove();
-        }
-    }
-    return *this;
-}
-    
-    SharedBreakIterator::~SharedBreakIterator() {
-  delete ptr;
-}
-    
-    class U_I18N_API SharedCalendar : public SharedObject {
-public:
-    SharedCalendar(Calendar *calToAdopt) : ptr(calToAdopt) { }
-    virtual ~SharedCalendar();
-    const Calendar *get() const { return ptr; }
-    const Calendar *operator->() const { return ptr; }
-    const Calendar &operator*() const { return *ptr; }
-private:
-    Calendar *ptr;
-    SharedCalendar(const SharedCalendar &);
-    SharedCalendar &operator=(const SharedCalendar &);
-};
-    
-    //----------------------------------------------------------------------
-    
-    #ifndef SMPDTFST_H
-#define SMPDTFST_H
-    
-    void CollationKey::setLength(int32_t newLength) {
-    // U_ASSERT(newLength >= 0 && newLength <= getCapacity());
-    fFlagAndLength = (fFlagAndLength & 0x80000000) | newLength;
-    fHashCode = kInvalidHashCode;
-}
-    
-    
-    { private:
-  /*! \brief the underlying stream */
-  dmlc::Stream *stream_;
-  /*! \brief buffer to hold data */
-  std::string buffer_;
-  /*! \brief length of valid data in buffer */
-  size_t read_len_;
-  /*! \brief pointer in the buffer */
-  size_t read_ptr_;
-};
-    
-    SparsePageWriter::~SparsePageWriter() {
-  for (auto& queue : qworkers_) {
-    // use nullptr to signal termination.
-    std::shared_ptr<SparsePage> sig(nullptr);
-    queue.Push(std::move(sig));
-  }
-  for (auto& thread : workers_) {
-    thread->join();
-  }
-}
-    
-    
-    {
-    {/*!
- * \brief Quantile sketch use WXQSummary
- * \tparam DType type of data content
- * \tparam RType type of rank
- */
-template<typename DType, typename RType = unsigned>
-class WXQuantileSketch :
-      public QuantileSketchTemplate<DType, RType, WXQSummary<DType, RType> > {
-};
-/*!
- * \brief Quantile sketch use WQSummary
- * \tparam DType type of data content
- * \tparam RType type of rank
- */
-template<typename DType, typename RType = unsigned>
-class GKQuantileSketch :
-      public QuantileSketchTemplate<DType, RType, GKSummary<DType, RType> > {
-};
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_QUANTILE_H_
+        #endif // BITCOIN_QT_OPENURIDIALOG_H
 
     
-    namespace xgboost {
-/*!
- * \brief interface of gradient boosting model.
- */
-class GradientBooster {
- public:
-  /*! \brief virtual destructor */
-  virtual ~GradientBooster() = default;
-  /*!
-   * \brief set configuration from pair iterators.
-   * \param begin The beginning iterator.
-   * \param end The end iterator.
-   * \tparam PairIter iterator<std::pair<std::string, std::string> >
-   */
-  template<typename PairIter>
-  inline void Configure(PairIter begin, PairIter end);
-  /*!
-   * \brief Set the configuration of gradient boosting.
-   *  User must call configure once before InitModel and Training.
-   *
-   * \param cfg configurations on both training and model parameters.
-   */
-  virtual void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) = 0;
-  /*!
-   * \brief load model from stream
-   * \param fi input stream.
-   */
-  virtual void Load(dmlc::Stream* fi) = 0;
-  /*!
-   * \brief save model to stream.
-   * \param fo output stream
-   */
-  virtual void Save(dmlc::Stream* fo) const = 0;
-  /*!
-   * \brief whether the model allow lazy checkpoint
-   * return true if model is only updated in DoBoost
-   * after all Allreduce calls
-   */
-  virtual bool AllowLazyCheckPoint() const {
-    return false;
-  }
-  /*!
-   * \brief perform update to the model(boosting)
-   * \param p_fmat feature matrix that provide access to features
-   * \param in_gpair address of the gradient pair statistics of the data
-   * \param obj The objective function, optional, can be nullptr when use customized version
-   * the booster may change content of gpair
-   */
-  virtual void DoBoost(DMatrix* p_fmat,
-                       HostDeviceVector<GradientPair>* in_gpair,
-                       ObjFunction* obj = nullptr) = 0;
-    }
-    }
+    private:
+    reverse_lock(reverse_lock const&);
+    reverse_lock& operator=(reverse_lock const&);
     
-    class SimpleBatchIteratorImpl : public BatchIteratorImpl {
- public:
-  explicit SimpleBatchIteratorImpl(SparsePage* page) : page_(page) {}
-  const SparsePage& operator*() const override {
-    CHECK(page_ != nullptr);
-    return *page_;
-  }
-  void operator++() override { page_ = nullptr; }
-  bool AtEnd() const override { return page_ == nullptr; }
-  SimpleBatchIteratorImpl* Clone() override {
-    return new SimpleBatchIteratorImpl(*this);
-  }
-    }
+        azi = (secp256k1_fe *)checked_malloc(cb, sizeof(secp256k1_fe) * count);
+    secp256k1_fe_inv_all_var(azi, az, count);
+    free(az);
     
     
-  while (true)
-  {
-    xfer += iprot->readFieldBegin(fname, ftype, fid);
-    if (ftype == ::apache::thrift::protocol::T_STOP) {
-      break;
-    }
-    switch (fid)
-    {
-      case 1:
-        if (ftype == ::apache::thrift::protocol::T_STRING) {
-          xfer += iprot->readString(this->sql);
-          this->__isset.sql = true;
-        } else {
-          xfer += iprot->skip(ftype);
-        }
-        break;
-      default:
-        xfer += iprot->skip(ftype);
-        break;
-    }
-    xfer += iprot->readFieldEnd();
-  }
-    
-    int main(int argc, char **argv) {
-  int port = 9090;
-  ::apache::thrift::stdcxx::shared_ptr<ExtensionManagerHandler> handler(new ExtensionManagerHandler());
-  ::apache::thrift::stdcxx::shared_ptr<TProcessor> processor(new ExtensionManagerProcessor(handler));
-  ::apache::thrift::stdcxx::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  ::apache::thrift::stdcxx::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  ::apache::thrift::stdcxx::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
-    }
-    
-        errorMsg << 'The instruction at ' << exAddrStr.str()
-             << ' referenced memory at ' << memAddrStr.str() << '.';
-    switch (record.ExceptionInformation[0]) {
-    case 0:
-      errorMsg << ' The memory could not be read.';
-      break;
-    case 1:
-      errorMsg << ' The memory could not be written.';
-      break;
-    case 8:
-      errorMsg << ' DEP access violation.';
-      break;
-    }
-    r['exception_message'] = errorMsg.str();
-  } else if ((record.ExceptionCode == EXCEPTION_IN_PAGE_ERROR) &&
-             (record.NumberParameters == 3)) {
-    std::ostringstream memAddrStr;
-    memAddrStr << '0x' << std::hex << record.ExceptionInformation[1];
-    
-    
-    {
-    {struct mei_version {
-  uint32_t important_details[7];
-  uint16_t major;
-  uint16_t minor;
-  uint16_t hotfix;
-  uint16_t build;
-  uint16_t r_major;
-  uint16_t r_minor;
-  uint16_t r_hotfix;
-  uint16_t r_build;
-  uint16_t codes[6];
-};
+    {    /* cleanup */
+    secp256k1_context_destroy(none);
+    secp256k1_context_destroy(sign);
+    secp256k1_context_destroy(vrfy);
+    secp256k1_context_destroy(both);
 }
+    
+    #include <stdint.h>
+#include <stdlib.h>
+    
+    
+    {  int Compare(const InternalKey& a, const InternalKey& b) const;
+};
+    
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
 }
 
     
-    #include <osquery/events/linux/probes/syscall_event.h>
+            Dictionary distributedState = checkpoint[distributedStatePropertyName].Value<Dictionary>();
     
-    #include <osquery/config/config.h>
-#include <osquery/filesystem/filesystem.h>
-#include <osquery/flags.h>
-#include <osquery/logger.h>
-#include <osquery/registry_factory.h>
-#include <osquery/utils/config/default_paths.h>
+    #pragma once
     
-    #include 'gtest/gtest.h'
-    
-    bool RadarState201::send_quality(const std::uint8_t* bytes,
-                                 int32_t length) const {
-  Byte t0(bytes + 5);
-  uint32_t x = t0.get_byte(4, 1);
+    namespace CNTK
+{
+    struct VariableFields final : public std::enable_shared_from_this<VariableFields>
+    {
+        friend class CompositeFunction;
+    }
     }
     
-    using Eigen::MatrixXd;
+            if (::WaitForSingleObject(m_handle, wait ? INFINITE : 0) != WAIT_OBJECT_0)
+        {
+            // failed to acquire
+            int rc = ::CloseHandle(m_handle);
+            if ((rc == CLOSEHANDLE_ERROR) && !std::uncaught_exception())
+            {
+                RuntimeError('Acquire: Handler close failure with error code %d', ::GetLastError());
+            }
+            m_handle = NULL;
+            return false;
+        }
     
     
-    { protected:
-  GemVehicleFactory gem_factory_;
+    {    ~ScopeTimer()
+    {
+        if (m_verbosity > 2)
+        {
+            m_aggregateTimer.Stop();
+            double time = m_aggregateTimer.ElapsedSeconds();
+            fprintf(stderr, m_message.c_str(), time);
+        }
+    }
 };
     
-    TEST_F(Accelrpt68Test, reset) {
-  Accelrpt68 acc;
-  int32_t length = 8;
-  ChassisDetail chassis_detail;
-  uint8_t bytes[8] = {0x01, 0x02, 0x03, 0x04, 0x11, 0x12, 0x13, 0x14};
+    
+    {        for (const auto& pair : rootLookupMap)
+        {
+            if (groupMap.find(pair.second) == groupMap.end())
+                InvalidArgument('lookup root should be group key');
+        }
+        m_aliasLookup = rootLookupMap;
     }
     
-    using namespace std;
+            // create the necessary shape.
+        shapeYT = TensorShape(this->GetTensorSliceFor(SIZE_MAX, fr));
+        // this swap results in a shape with swapped dimensions, but also swapped strides
+        shapeYT.SwapDimsInPlace(1, 2);
+        // this copy is necessary so that the strides are dense.
+        shapeYT = TensorShape(shapeYT.GetDims());
     
-    #include <stdexcept>
+      // Doesn't include records skipped because of
+  // CompactionFilter::Decision::kRemoveAndSkipUntil.
+  int64_t num_record_drop_user = 0;
     
-    namespace folly {
+    Status WriteBatchBase::DeleteRange(ColumnFamilyHandle* column_family,
+                                   const SliceParts& begin_key,
+                                   const SliceParts& end_key) {
+  std::string begin_key_buf, end_key_buf;
+  Slice begin_key_slice(begin_key, &begin_key_buf);
+  Slice end_key_slice(end_key, &end_key_buf);
+  return DeleteRange(column_family, begin_key_slice, end_key_slice);
+}
+    
+      // When an actor (column family) requests a stop token, all writes will be
+  // stopped until the stop token is released (deleted)
+  std::unique_ptr<WriteControllerToken> GetStopToken();
+  // When an actor (column family) requests a delay token, total delay for all
+  // writes to the DB will be controlled under the delayed write rate. Every
+  // write needs to call GetDelay() with number of bytes writing to the DB,
+  // which returns number of microseconds to sleep.
+  std::unique_ptr<WriteControllerToken> GetDelayToken(
+      uint64_t delayed_write_rate);
+  // When an actor (column family) requests a moderate token, compaction
+  // threads will be increased
+  std::unique_ptr<WriteControllerToken> GetCompactionPressureToken();
+    
+      bool FilterMergeOperand(int level, const rocksdb::Slice& key,
+                          const rocksdb::Slice& existing_value) const override {
+    fprintf(stderr, 'FilterMerge(%s)\n', key.ToString().c_str());
+    ++merge_count_;
+    return existing_value == 'bad';
+  }
+    
+      // Read a key using the snapshot
+  read_options.snapshot = snapshot;
+  s = txn->GetForUpdate(read_options, 'abc', &value);
+  assert(value == 'def');
+    
+      PinnableSlice pinnable_val;
+  db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key1', &pinnable_val);
+  assert(s.IsNotFound());
+  // Reset PinnableSlice after each use and before each reuse
+  pinnable_val.Reset();
+  db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key2', &pinnable_val);
+  assert(pinnable_val == 'value');
+  pinnable_val.Reset();
+  // The Slice pointed by pinnable_val is not valid after this point
+    
+    #include 'rocksdb/db.h'
+#include 'rocksdb/status.h'
+    
+      // Compress blocks using the specified compression algorithm.  This
+  // parameter can be changed dynamically.
+  //
+  // Default: kSnappyCompression, which gives lightweight but fast
+  // compression.
+  //
+  // Typical speeds of kSnappyCompression on an Intel(R) Core(TM)2 2.4GHz:
+  //    ~200-500MB/s compression
+  //    ~400-800MB/s decompression
+  // Note that these speeds are significantly faster than most
+  // persistent storage speeds, and therefore it is typically never
+  // worth switching to kNoCompression.  Even if the input data is
+  // incompressible, the kSnappyCompression implementation will
+  // efficiently detect that and will switch to uncompressed mode.
+  CompressionType compression;
+    
+    TEST(ProtocolDataTest, CheckSum) {
+  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
+  const uint8_t result =
+      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
+  EXPECT_EQ(0xE7, result);
+}
+    
+    TEST_F(SpeedLimitTest, GetSpeedLimitByS) {
+  EXPECT_EQ(speed_limit_.speed_limit_points().size(), 100);
+  double s = 0.0;
+  const double ds = 0.01;
+  while (s < 99.0) {
+    double v_limit = speed_limit_.GetSpeedLimitByS(s);
+    }
     }
     
-    // Some utility routines relating to unicode.
+    #include 'glog/logging.h'
     
-    inline UriTuple as_tuple(const folly::Uri& k) {
-  return UriTuple(
-      k.scheme(),
-      k.username(),
-      k.password(),
-      k.host(),
-      k.port(),
-      k.path(),
-      k.query(),
-      k.fragment());
-}
+    Brakemotorrpt170::Brakemotorrpt170() {}
+const int32_t Brakemotorrpt170::ID = 0x70;
     
-      template <typename T>
-  static T* get_shared_ptr(counted_base* base) {
-    return (T*)base;
-  }
+    namespace apollo {
+namespace canbus {
+namespace gem {
+    }
+    }
+    }
     
-    #include <folly/detail/AtFork.h>
-    
-    inline std::string familyNameStr(sa_family_t family) {
-  switch (family) {
-    case AF_INET:
-      return 'AF_INET';
-    case AF_INET6:
-      return 'AF_INET6';
-    case AF_UNSPEC:
-      return 'AF_UNSPEC';
-    case AF_UNIX:
-      return 'AF_UNIX';
-    default:
-      return familyNameStrDefault(family);
-  }
-}
-    
-      // create an in6_addr from an uint8_t*
-  static inline in6_addr mkAddress6(const uint8_t* src) {
-    in6_addr addr;
-    std::memset(&addr, 0, 16);
-    std::memcpy(addr.s6_addr, src, 16);
-    return addr;
-  }
+    namespace apollo {
+namespace canbus {
+namespace gem {
+    }
+    }
+    }
