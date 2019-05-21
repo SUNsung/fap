1a36066bf7beed10183960f34ccbd918ba7e18ab
SUNsung <weatherius@gmail.com>
2019-05-21 08:45:42 +0300

    Rewrite [20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp] File in: 2019-05-21 08:45:42

--- cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
+++ cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
@@ -1,166 +1,253 @@
 
         
-        #ifndef BITCOIN_QT_NETWORKSTYLE_H
-#define BITCOIN_QT_NETWORKSTYLE_H
+        #include 'swift/AST/RequirementEnvironment.h'
+#include 'swift/AST/ASTContext.h'
+#include 'swift/AST/Decl.h'
+#include 'swift/AST/DeclContext.h'
+#include 'swift/AST/GenericSignature.h'
+#include 'swift/AST/GenericSignatureBuilder.h'
+#include 'swift/AST/ProtocolConformance.h'
+#include 'swift/AST/Types.h'
+#include 'llvm/ADT/Statistic.h'
     
-    static SECP256K1_INLINE void secp256k1_gej_double_nonzero(secp256k1_gej *r, const secp256k1_gej *a, secp256k1_fe *rzr) {
-    VERIFY_CHECK(!secp256k1_gej_is_infinity(a));
-    secp256k1_gej_double_var(r, a, rzr);
-}
-    
-    /// Format list of arguments to the stream according to given format string.
-template<typename... Args>
-void format(std::ostream& out, const char* fmt, const Args&... args)
-{
-    vformat(out, fmt, makeFormatList(args...));
-}
-    
-    /** A hasher class for RIPEMD-160. */
-class CRIPEMD160
-{
-private:
-    uint32_t s[5];
-    unsigned char buf[64];
-    uint64_t bytes;
+    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
+  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
+  llvm::sys::ScopedLock L(DCache.Mux);
     }
     
-        movdqa	XFER, [TBL + 1*16]
-    paddd	XFER, X0
-    movdqa	[rsp + _XFER], XFER
-    FOUR_ROUNDS_AND_SCHED
-    
-    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
-  result->append(key.user_key.data(), key.user_key.size());
-  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
+    void CacheImpl::removeAll() {
+  cache_remove_all(static_cast<cache_t*>(Impl));
 }
     
-            friend Rational operator&(Rational lhs, Rational const& rhs);
-        friend Rational operator|(Rational lhs, Rational const& rhs);
-        friend Rational operator^(Rational lhs, Rational const& rhs);
     
-            virtual void Announce(NarratorAnnouncement^ announcement);
+    {  OutLines.append(Lines.begin(), Lines.end());
+}
+
     
-    namespace CalculatorApp
-{
-    EngineResourceProvider::EngineResourceProvider()
-    {
-        m_resLoader = ResourceLoader::GetForViewIndependentUse('CEngineStrings');
-    }
-    }
+      static CFPointeeInfo forTypedef(const clang::TypedefNameDecl *decl) {
+    assert(decl);
+    CFPointeeInfo info;
+    info.IsValid = true;
+    info.IsConst = false;
+    info.Decl = decl;
+    return info;
+  }
     
-    #include 'pch.h'
-#include 'ExpressionCommandDeserializer.h'
-    
-      static void clear(Data& data);
-    
-      virtual const std::string& getType() const CXX11_OVERRIDE;
-    
-    #include 'DHTNode.h'
-#include 'DHTBucket.h'
-#include 'DHTBucketTree.h'
-#include 'DHTTaskQueue.h'
-#include 'DHTTaskFactory.h'
-#include 'DHTTask.h'
-#include 'util.h'
-#include 'LogFactory.h'
-#include 'Logger.h'
-#include 'fmt.h'
-    
-    #include 'TimeA2.h'
-    
-    #define WRITE_CHECK(fp, ptr, count)                                            \
-  if (fp.write((ptr), (count)) != (count)) {                                   \
-    throw DL_ABORT_EX(                                                         \
-        fmt('Failed to save DHT routing table to %s.', filename.c_str()));     \
+      TemporaryFile() {
+    path = tmppath();
   }
     
     
-    {} // namespace aria2
-    
-      ~DHTTaskExecutor();
-    
-      virtual std::shared_ptr<DHTTask>
-  createReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
-                        const std::shared_ptr<DHTNode>& newNode) CXX11_OVERRIDE;
-    
-    #include <memory>
-    
-    
-    {  virtual void
-  addImmediateTask(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+    {  std::vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'SubGradient',
+        '',
+        std::vector<std::string>{GO(0), I(0), I(1)},
+        std::vector<std::string>{GI(0), GI(1)});
+  }
 };
     
-    bool DNSCache::CacheEntry::add(const std::string& addr)
-{
-  for (std::vector<AddrEntry>::const_iterator i = addrEntries_.begin(),
-                                              eoi = addrEntries_.end();
-       i != eoi; ++i) {
-    if ((*i).addr_ == addr) {
-      return false;
-    }
-  }
-  addrEntries_.push_back(AddrEntry(addr));
-  return true;
+    workspace.ResetWorkspace()
+    
+      // This and other putatively are the same, so call the (permanent) callback
+  // for each blob index where the bounding boxes match.
+  // The callback is deleted on completion.
+  void ProcessMatchedBlobs(const TWERD& other, TessCallback1<int>* cb) const;
+    
+    bool ParagraphModel::Comparable(const ParagraphModel &other) const {
+  if (justification_ != other.justification_)
+    return false;
+  if (justification_ == JUSTIFICATION_CENTER ||
+      justification_ == JUSTIFICATION_UNKNOWN)
+    return true;
+  int tolerance = (tolerance_ + other.tolerance_) / 4;
+  return NearlyEqual(margin_ + first_indent_,
+                     other.margin_ + other.first_indent_, tolerance) &&
+         NearlyEqual(margin_ + body_indent_,
+                     other.margin_ + other.body_indent_, tolerance);
 }
     
-    #include <string>
-#include <set>
-#include <algorithm>
-#include <vector>
+    //////////////////////////////////////////////////////////////////////
     
-            void addWrite() {
-            if (m_write) return;
-            m_write = new QSocketNotifier(m_ctx->c.fd, QSocketNotifier::Write, 0);
-            connect(m_write, SIGNAL(activated(int)), this, SLOT(write()));
-        }
-    
-        cache.set('test1', val1);
-    ASSERT_EQ(cache.get('test1').get(), val1.get());
-    val1.reset();
-    ASSERT_EQ(dtor_num, 0);
-    
-            for (int i = 0; i < 100; ++i)
-        {
-            std::string addr3 = Coroutine::gethostbyname('ipv6.sjtu.edu.cn', AF_INET);
-            std::string addr4 = Coroutine::gethostbyname('ipv6.sjtu.edu.cn', AF_INET6);
+      if (comma != data) {
+    // we have meta
+    ssize_t meta_len = comma - data;
+    data_len -= meta_len;
+    char* semi = (char*)memchr(data, ';', meta_len);
+    char* slash = (char*)memchr(data, '/', meta_len);
     }
     
-    TEST(ringbuffer, thread)
-{
-    int i;
-    pool = swRingBuffer_new(1024 * 1024 * 4, 1);
-    ASSERT_NE(pool, nullptr);
+    #include 'hphp/util/stack-trace.h'
+    
+    template<typename F>
+void logLowPriPerfWarning(folly::StringPiece event, F fillCols);
+template<typename F>
+void logLowPriPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
+    
+      explicit PlainFile(FILE *stream = nullptr,
+                     bool nonblocking = false,
+                     const String& wrapper_type = null_string,
+                     const String& stream_type = null_string);
+  explicit PlainFile(int fd,
+                     bool nonblocking = false,
+                     const String& wrapper = null_string,
+                     const String& stream_type = null_string);
+  ~PlainFile() override;
+    
+    
+    {};
+    
+    ExtensionResponse::ExtensionResponse(const ExtensionResponse& other20) {
+  status = other20.status;
+  response = other20.response;
+  __isset = other20.__isset;
+}
+ExtensionResponse& ExtensionResponse::operator=(const ExtensionResponse& other21) {
+  status = other21.status;
+  response = other21.response;
+  __isset = other21.__isset;
+  return *this;
+}
+void ExtensionResponse::printTo(std::ostream& out) const {
+  using ::apache::thrift::to_string;
+  out << 'ExtensionResponse(';
+  out << 'status=' << to_string(status);
+  out << ', ' << 'response=' << to_string(response);
+  out << ')';
+}
+    
+      void __set_uuid(const ExtensionRouteUUID val);
+    
+    namespace osquery {
+namespace tables {
+    }
     }
     
-    TEST(coroutine_base, yield_resume)
-{
-    long _cid;
-    long cid = Coroutine::create([](void *arg)
+    
     {
-        long cid = Coroutine::get_current_cid();
-        Coroutine *co = Coroutine::get_by_cid(cid);
-        co->yield();
-        *(long *) arg = Coroutine::get_current_cid();
-    }, &_cid);
+    {
+    {  for (const auto& share_line : osquery::split(content, '\n')) {
+    genNFSShare(share_line, results);
+  }
+  return results;
+}
+}
+}
+
+    
+        if (type == nullptr) {
+      found = true;
+      return;
     }
     
+    Expected<int, PosixError> syscall(struct perf_event_attr* attr,
+                                  pid_t pid,
+                                  int cpu,
+                                  int group_fd,
+                                  unsigned long const flags);
     
-    {            ret = chan->push(&i);
-            ASSERT_TRUE(ret);
-            ret = chan->push(&i);
-            ASSERT_TRUE(ret);
-        }, &chan),
     
-    static swThreadPool pool;
-static int _pipe;
-const static int N = 10000;
+    {
+    {} // namespace table_tests
+} // namespace osquery
+
     
-        int length;
-    char address[256];
-    int port = 0;
-    int ret;
-    
-    void MyServer::onTask(int task_id, int src_worker_id, const DataBuffer &data)
-{
-    printf('PID=%d\tTaskID=%d\n', getpid(), task_id);
+    TEST_F(KernelInfo, test_sanity) {
+  QueryData data = execute_query('select * from kernel_info');
+  ValidatatioMap row_map = {{'version', NonEmptyString},
+                            {'arguments', NormalType},
+                            {'path', NormalType},
+                            {'device', NonEmptyString}};
+  validate_rows(data, row_map);
 }
+    
+    #include <osquery/tests/integration/tables/helper.h>
+    
+    TEST_F(KernelModules, test_sanity) {
+  QueryData data = execute_query('select * from kernel_modules');
+  ASSERT_GT(data.size(), 0ul);
+  ValidatatioMap row_map = {
+      {'name', NonEmptyString},
+      {'size', NonNegativeInt},
+      {'used_by', NonEmptyString},
+      {'status', NonEmptyString},
+      {'address', NonNegativeInt},
+  };
+  validate_rows(data, row_map);
+}
+    
+    TEST_F(keychainAcls, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from keychain_acls');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'keychain_path', NormalType}
+  //      {'authorizations', NormalType}
+  //      {'path', NormalType}
+  //      {'description', NormalType}
+  //      {'label', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
+}
+    
+    TEST_F(keychainItems, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from keychain_items');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'label', NormalType}
+  //      {'description', NormalType}
+  //      {'comment', NormalType}
+  //      {'created', NormalType}
+  //      {'modified', NormalType}
+  //      {'type', NormalType}
+  //      {'path', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
+}
+    
+        // create string from std::string
+    std::string s_stdstring = 'The quick brown fox jumps over the lazy dog.';
+    json j_stdstring(s_stdstring);
+    
+    #include 'rocksdb/cache.h'
+#include 'rocksdb/compaction_filter.h'
+#include 'rocksdb/db.h'
+#include 'rocksdb/options.h'
+#include 'rocksdb/slice.h'
+#include 'rocksdb/table.h'
+#include 'rocksdb/utilities/options_util.h'
+    
+    std::string kDBPath = '/tmp/rocksdb_simple_example';
+    
+      // Lookup page cache by page identifier
+  //
+  // page_key   Page identifier
+  // buf        Buffer where the data should be copied
+  // size       Size of the page
+  virtual Status Lookup(const Slice& key, std::unique_ptr<char[]>* data,
+                        size_t* size) = 0;
+    
+      // Instead of creating a snapshot, take ownership of the input snapshot.
+  ManagedSnapshot(DB* db, const Snapshot* _snapshot);
+    
+    class OptimisticTransactionDB : public StackableDB {
+ public:
+  // Open an OptimisticTransactionDB similar to DB::Open().
+  static Status Open(const Options& options, const std::string& dbname,
+                     OptimisticTransactionDB** dbptr);
+    }
