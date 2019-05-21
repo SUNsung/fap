ccb03419df5ad671e3b4f5969fcadad69df4d0a8
SUNsung <weatherius@gmail.com>
2019-05-21 22:48:06 +0300

    Rewrite [21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py] File in: 2019-05-21 22:48:05

--- pyton/21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py
+++ pyton/21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py
@@ -1,37 +1,155 @@
 
         
-            def load(self):
+        # the signals
+from .signals import (
+    signals_available,
+    template_rendered,
+    request_started,
+    request_finished,
+    got_request_exception,
+    request_tearing_down,
+    appcontext_tearing_down,
+    appcontext_pushed,
+    appcontext_popped,
+    message_flashed,
+    before_render_template,
+)
+    
         try:
-            session_data = self._cache.get(self.cache_key)
-        except Exception:
-            # Some backends (e.g. memcache) raise an exception on invalid
-            # cache keys. If this happens, reset the session. See #17810.
-            session_data = None
-        if session_data is not None:
-            return session_data
-        self._session_key = None
-        return {}
+        try:
+            with _Mgr():
+                raise AssertionError()
+        except:
+            raise
+    except TypeError:
+        BROKEN_PYPY_CTXMGR_EXIT = True
+    except AssertionError:
+        pass
+    
+        Defines all the global objects that are proxies to the current
+    active context.
+    
+        If you configure your own :class:`logging.StreamHandler`, you may want to
+    use this for the stream. If you are using file or dict configuration and
+    can't import this directly, you can refer to it as
+    ``ext://flask.logging.wsgi_errors_stream``.
+    '''
+    return request.environ['wsgi.errors'] if request else sys.stderr
     
     
-class BaseSessionManager(models.Manager):
-    def encode(self, session_dict):
+def test_on_run_after_other_commands(usage_tracker_io, shell_pid, shell, logs):
+    shell_pid.return_value = 12
+    shell.get_history.return_value = ['fuck', 'ls']
+    _change_tracker(usage_tracker_io, 12)
+    main()
+    logs.how_to_configure_alias.assert_called_once()
+    
+    
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
+    history_changed(proc, TIMEOUT, u'echo test')
+    
+    
+@pytest.mark.parametrize('command, new_command, packages', [
+    (Command('vim', ''), 'sudo apt-get install vim && vim',
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('convert', ''), 'sudo apt-get install imagemagick && convert',
+     [('imagemagick', 'main'),
+      ('graphicsmagick-imagemagick-compat', 'universe')]),
+    (Command('sudo vim', ''), 'sudo apt-get install vim && sudo vim',
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo convert', ''), 'sudo apt-get install imagemagick && sudo convert',
+     [('imagemagick', 'main'),
+      ('graphicsmagick-imagemagick-compat', 'universe')])])
+def test_get_new_command(mocker, command, new_command, packages):
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
+    
+    
+@pytest.mark.parametrize('script, output', [
+    ('brew link sshfs', output),
+    ('cat output', output),
+    ('brew install sshfs', '')])
+def test_not_match(script, output):
+    command = Command(script, output)
+    assert not match(command)
+    
+        The Django sessions framework is entirely cookie-based. It does
+    not fall back to putting session IDs in URLs. This is an intentional
+    design decision. Not only does that behavior make URLs ugly, it makes
+    your site vulnerable to session-ID theft via the 'Referer' header.
+    
+        def __init__(self, msg, with_headers=True, with_body=True,
+                 on_body_chunk_downloaded=None):
         '''
-        Return the given session dictionary serialized and encoded as a string.
-        '''
-        session_store_class = self.model.get_session_store_class()
-        return session_store_class().encode(session_dict)
+        :param msg: a :class:`models.HTTPMessage` subclass
+        :param with_headers: if `True`, headers will be included
+        :param with_body: if `True`, body will be included
     
-        if section is not None:
-        if section not in sitemaps:
-            raise Http404('No sitemap available for section: %r' % section)
-        maps = [sitemaps[section]]
-    else:
-        maps = sitemaps.values()
-    page = request.GET.get('p', 1)
+            '''
+        self.enabled = True
+        self.kwargs = kwargs
     
     
-def display_temp(hass: HomeAssistant, temperature: float, unit: str,
-                 precision: float) -> float:
-    '''Convert temperature into preferred units/precision for display.'''
-    temperature_unit = unit
-    ha_unit = hass.config.units.temperature_unit
+def test_migrate_implicit_content_type():
+    config = MockEnvironment().config
+    
+            # host:port => host_port
+        hostname = hostname.replace(':', '_')
+        path = os.path.join(config_dir,
+                            SESSIONS_DIR_NAME,
+                            hostname,
+                            session_name + '.json')
+    
+        def reset_appid(self):
+        # called by web_control
+        with self.lock:
+            self.working_appid_list = list()
+            for appid in self.config.GAE_APPIDS:
+                if not appid:
+                    self.config.GAE_APPIDS.remove(appid)
+                    continue
+                self.working_appid_list.append(appid)
+            self.not_exist_appids = []
+            self.out_of_quota_appids = []
+        self.last_reset_time = time.time()
+    
+            self.last_check_time = time_now
+        threading.Thread(target=self._simple_check_worker).start()
+    
+    try:
+    from io import BytesIO
+except ImportError:
+    from cStringIO import StringIO as BytesIO
+try:
+    from google.appengine.api import urlfetch
+    from google.appengine.runtime import apiproxy_errors
+except ImportError:
+    urlfetch = None
+try:
+    import sae
+except ImportError:
+    sae = None
+try:
+    import bae.core.wsgi
+except ImportError:
+    bae = None
+try:
+    import socket
+    import select
+except ImportError:
+    socket = None
+try:
+    import OpenSSL
+except ImportError:
+    OpenSSL = None
+    
+    '''
+    
+        pass
+    
+            An index from 0..n-1 of the token object in the input stream.
+        This must be valid in order to use the ANTLRWorks debugger.
+        
+        Using setter/getter methods is deprecated. Use o.index instead.'''
