5d95871492f79d84ad2a58affd7fe7ebe8652fad
SUNsung <weatherius@gmail.com>
2019-05-21 13:08:24 +0300

    Rewrite [33uEWL2KXe4g33o7rjUtegd6w33Ly8RAImwQWU33xk.cpp] File in: 2019-05-21 13:08:23

--- cccccc/33uEWL2KXe4g33o7rjUtegd6w33Ly8RAImwQWU33xk.cpp
+++ cccccc/33uEWL2KXe4g33o7rjUtegd6w33Ly8RAImwQWU33xk.cpp
@@ -1,423 +1,122 @@
 
         
-        UvTaskRunner::~UvTaskRunner() {
-  for (auto& iter : tasks_) {
-    uv_unref(reinterpret_cast<uv_handle_t*>(iter.first));
-    delete iter.first;
-  }
-}
+        	// C-style inteface to the encoder
+	void Encode(float *a_pafSourceRGBA,
+				unsigned int a_uiSourceWidth,
+				unsigned int a_uiSourceHeight,
+				Image::Format a_format,
+				ErrorMetric a_eErrMetric,
+				float a_fEffort,
+				unsigned int a_uiJobs,
+				unsigned int a_uimaxJobs,
+				unsigned char **a_ppaucEncodingBits,
+				unsigned int *a_puiEncodingBitsBytes,
+				unsigned int *a_puiExtendedWidth,
+				unsigned int *a_puiExtendedHeight,
+				int *a_piEncodingTime_ms, bool a_bVerboseOutput = false);
     
-    #include 'atom/browser/api/event_emitter.h'
-    
-    namespace auto_updater {
+    			for (unsigned int uiPixelOrder = 0; uiPixelOrder < PIXELS / 2; uiPixelOrder++)
+			{
+				unsigned int uiPixel1 = pauiPixelMapping1[uiPixelOrder];
+				unsigned int uiPixel2 = pauiPixelMapping2[uiPixelOrder];
     }
     
-    AboutProtocolHandler::~AboutProtocolHandler() {}
+    #endif  // VPX_DSP_TXFM_COMMON_H_
+
     
-    void OffscreenViewProxy::SetBounds(const gfx::Rect& bounds) {
-  view_bounds_ = bounds;
-}
+       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
     
-      std::wstring out;
-  out.push_back(L''');
-  for (size_t i = 0; i < arg.size(); ++i) {
-    if (arg[i] == '\\') {
-      // Find the extent of this run of backslashes.
-      size_t start = i, end = start + 1;
-      for (; end < arg.size() && arg[end] == '\\'; ++end) {
-      }
-      size_t backslash_count = end - start;
-    }
-    }
-    
-    
-    {}  // namespace atom
-    
-    
-    {}  // namespace atom
-    
-    /**
- * Factory method
- */
-static Transliterator* RemoveTransliterator_create(const UnicodeString& /*ID*/,
-                                                   Transliterator::Token /*context*/) {
-    /* We don't need the ID or context. We just remove data */
-    return new RemoveTransliterator();
-}
-    
-    
-    {    // Compare identical level.
-    left.move(&left, equalPrefixLength, UITER_ZERO);
-    right.move(&right, equalPrefixLength, UITER_ZERO);
-    const Normalizer2Impl &nfcImpl = data->nfcImpl;
-    if(settings->dontCheckFCD()) {
-        UIterNFDIterator leftIter(left);
-        UIterNFDIterator rightIter(right);
-        return compareNFDIter(nfcImpl, leftIter, rightIter);
-    } else {
-        FCDUIterNFDIterator leftIter(data, left, equalPrefixLength);
-        FCDUIterNFDIterator rightIter(data, right, equalPrefixLength);
-        return compareNFDIter(nfcImpl, leftIter, rightIter);
-    }
-}
-    
-    ScriptSet & ScriptSet::operator =(const ScriptSet &other) {
-    for (uint32_t i=0; i<UPRV_LENGTHOF(bits); i++) {
-        bits[i] = other.bits[i];
-    }
-    return *this;
-}
-    
-    class UnicodeSet;
-    
-    #if !UCONFIG_NO_BREAK_ITERATION
-    
-    class NumberFormat;
-    
-    U_NAMESPACE_BEGIN
-    
-    
-    {        int32_t length = other.getLength();
-        if (length > getCapacity() && reallocate(length, 0) == NULL) {
-            return setToBogus();
-        }
-        if (length > 0) {
-            uprv_memcpy(getBytes(), other.getBytes(), length);
-        }
-        fFlagAndLength = (fFlagAndLength & 0x80000000) | length;
-        fHashCode = other.fHashCode;
-    }
-    
-        // Complex (nested replacers) Processing Code :
-    else {
-        /* When there are segments to be copied, use the Replaceable.copy()
-         * API in order to retain out-of-band data.  Copy everything to the
-         * end of the string, then copy them back over the key.  This preserves
-         * the integrity of indices into the key and surrounding context while
-         * generating the output text.
-         */
-        UnicodeString buf;
-        int32_t oOutput; // offset into 'output'
-        isComplex = FALSE;
-    }
-    
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-namespace can {
-    }
-    }
-    }
-    }
-    
-    using ::apollo::canbus::ChassisDetail;
-    
-      x <<= 4;
-  x |= t;
-    
-    double ObjectExtendedInfo60D::oritation_angle(const std::uint8_t* bytes,
-                                              int32_t length) const {
-  Byte t0(bytes + 4);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-    
-    {  switch (x) {
-    case 0x0:
-      return RCS_THRESHOLD_STANDARD;
-    case 0x1:
-      return RCS_THRESHOLD_HIGH_SENSITIVITY;
-    default:
-      return RCS_THRESHOLD_ERROR;
-  }
-}
-    
-    unsigned int BaseMapMatrix::CreateBinary(unsigned char* buf,
-                                         unsigned int buf_size) const {
-  return 0;
-}
-    
-    const PolynomialXd& Spline1dSeg::spline_func() const { return spline_func_; }
-    
-    #include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_3_72.h'
-    
-    // config detail: {'name': 'brk_can_timeout', 'enum': {0:
-// 'BRK_CAN_TIMEOUT_NO_ACTIVE_CAN_TIMEOUT', 1:
-// 'BRK_CAN_TIMEOUT_ACTIVE_CAN_TIMEOUT'}, 'precision': 1.0, 'len': 1,
-// 'is_signed_var': False, 'offset': 0.0, 'physical_range': '[0|1]', 'bit': 4,
-// 'type': 'enum', 'order': 'motorola', 'physical_unit': ''}
-Global_rpt_6a::Brk_can_timeoutType Globalrpt6a::brk_can_timeout(
-    const std::uint8_t* bytes, int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(4, 1);
-    }
-    
-    class RenderActionUpdateAttr : public RenderAction {
- public:
-  explicit RenderActionUpdateAttr(
-      const std::string &page_id, const std::string &ref,
-      std::vector<std::pair<std::string, std::string>> *attrs);
-    }
-    
-    namespace WeexCore {
-class RenderAppBar : public RenderObject {
- private:
-  std::map<std::string, std::string> *GetDefaultStyle();
-    }
-    }
-    
-        /**
-     *  unicode to utf8 convertor with zero dependency inspired by java sdk character source
-     * */
-    void utf16_convert_to_utf8_string(uint16_t *utf16, int length, std::string& utf8);
-    void utf16_convert_to_utf8_quote_string(uint16_t *utf16, int length, std::string& utf8);
-    /**
-     * return byte count in utf8, buffer size should can contains convert values
-     * */
-    int utf16_convert_to_utf8_cstr(uint16_t *utf16, int length, char* buffer);
-    int utf16_convert_to_utf8_quote_cstr(uint16_t *utf16, int length, char* buffer);
-    
-    
-    {
-    { private:
-  static RenderCreator *g_pInstance;
-  
-  std::map<std::string, std::string> affineTypes_; // affineTypes_[A] = B means A is rendered like B
-};
-}  // namespace WeexCore
-    
-    #include <string>
-#include 'core/render/action/render_action_interface.h'
-    
-      grpc::ServerBuilder builder;
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-  std::cerr << 'Server listening on ' << server_address << std::endl;
-    
-    // A common interface for objects having comments in the source.
-// Return formatted comments to be inserted in generated code.
-struct CommentHolder {
-  virtual ~CommentHolder() {}
-  virtual grpc::string GetLeadingComments(const grpc::string prefix) const = 0;
-  virtual grpc::string GetTrailingComments(const grpc::string prefix) const = 0;
-  virtual std::vector<grpc::string> GetAllComments() const = 0;
-};
-    
-      assert(animal->name()->str() == 'Dog');
-  assert(animal->sound()->str() == 'Bark');
-  (void)animal; // To silence 'Unused Variable' warnings.
-    
-      std::string Value(float v, const std::string &src) const FLATBUFFERS_OVERRIDE;
-  std::string Inf(float v) const FLATBUFFERS_OVERRIDE;
-  std::string NaN(float v) const FLATBUFFERS_OVERRIDE;
-    
-    // independent from idl_parser, since this code is not needed for most clients
-    
-    
-    {
-    {
-    {
-    {
-    {            code +=
-                field.value.type.element == BASE_TYPE_BOOL
-                    ? 'false'
-                    : (IsScalar(field.value.type.element) ? default_cast + '0'
-                                                          : 'null');
-            break;
-          }
-          case BASE_TYPE_UNION:
-            if (lang_.language == IDLOptions::kCSharp) {
-              code += '() where TTable : struct, IFlatbufferObject';
-              code += offset_prefix + '(TTable?)' + getter;
-              code += '<TTable>(o) : null';
-            } else {
-              code += '(' + type_name + ' obj)' + offset_prefix + getter;
-              code += '(obj, o) : null';
-            }
-            break;
-          default: FLATBUFFERS_ASSERT(0);
-        }
-      }
-      code += member_suffix;
-      code += '}\n';
-      if (field.value.type.base_type == BASE_TYPE_VECTOR) {
-        code +=
-            '  public int ' + MakeCamel(field.name, lang_.first_camel_upper);
-        code += 'Length';
-        code += lang_.getter_prefix;
-        code += offset_prefix;
-        code += lang_.accessor_prefix + '__vector_len(o) : 0; ';
-        code += lang_.getter_suffix;
-        code += '}\n';
-        // See if we should generate a by-key accessor.
-        if (field.value.type.element == BASE_TYPE_STRUCT &&
-            !field.value.type.struct_def->fixed) {
-          auto &sd = *field.value.type.struct_def;
-          auto &fields = sd.fields.vec;
-          for (auto kit = fields.begin(); kit != fields.end(); ++kit) {
-            auto &key_field = **kit;
-            if (key_field.key) {
-              auto qualified_name = WrapInNameSpace(sd);
-              code += '  public ' + qualified_name + lang_.optional_suffix + ' ';
-              code += MakeCamel(field.name, lang_.first_camel_upper) + 'ByKey(';
-              code += GenTypeNameDest(key_field.value.type) + ' key)';
-              code += offset_prefix;
-              code += qualified_name + '.__lookup_by_key(';
-              if (lang_.language == IDLOptions::kJava)
-                code += 'null, ';
-              code += lang_.accessor_prefix + '__vector(o), key, ';
-              code += lang_.accessor_prefix + 'bb) : null; ';
-              code += '}\n';
-              if (lang_.language == IDLOptions::kJava) {
-                code += '  public ' + qualified_name + lang_.optional_suffix + ' ';
-                code += MakeCamel(field.name, lang_.first_camel_upper) + 'ByKey(';
-                code += qualified_name + lang_.optional_suffix + ' obj, ';
-                code += GenTypeNameDest(key_field.value.type) + ' key)';
-                code += offset_prefix;
-                code += qualified_name + '.__lookup_by_key(obj, ';
-                code += lang_.accessor_prefix + '__vector(o), key, ';
-                code += lang_.accessor_prefix + 'bb) : null; ';
-                code += '}\n';
-              }
-              break;
-            }
-          }
-        }
-      }
-      // Generate a ByteBuffer accessor for strings & vectors of scalars.
-      if ((field.value.type.base_type == BASE_TYPE_VECTOR &&
-           IsScalar(field.value.type.VectorType().base_type)) ||
-          field.value.type.base_type == BASE_TYPE_STRING) {
-        switch (lang_.language) {
-          case IDLOptions::kJava:
-            code += '  public ByteBuffer ';
-            code += MakeCamel(field.name, lang_.first_camel_upper);
-            code += 'AsByteBuffer() { return ';
-            code += lang_.accessor_prefix + '__vector_as_bytebuffer(';
-            code += NumToString(field.value.offset) + ', ';
-            code +=
-                NumToString(field.value.type.base_type == BASE_TYPE_STRING
-                                ? 1
-                                : InlineSize(field.value.type.VectorType()));
-            code += '); }\n';
-            code += '  public ByteBuffer ';
-            code += MakeCamel(field.name, lang_.first_camel_upper);
-            code += 'InByteBuffer(ByteBuffer _bb) { return ';
-            code += lang_.accessor_prefix + '__vector_in_bytebuffer(_bb, ';
-            code += NumToString(field.value.offset) + ', ';
-            code +=
-                NumToString(field.value.type.base_type == BASE_TYPE_STRING
-                                ? 1
-                                : InlineSize(field.value.type.VectorType()));
-            code += '); }\n';
-            break;
-          case IDLOptions::kCSharp:
-            code += '#if ENABLE_SPAN_T\n';
-            code += '  public Span<byte> Get';
-            code += MakeCamel(field.name, lang_.first_camel_upper);
-            code += 'Bytes() { return ';
-            code += lang_.accessor_prefix + '__vector_as_span(';
-            code += NumToString(field.value.offset);
-            code += '); }\n';
-            code += '#else\n';
-            code += '  public ArraySegment<byte>? Get';
-            code += MakeCamel(field.name, lang_.first_camel_upper);
-            code += 'Bytes() { return ';
-            code += lang_.accessor_prefix + '__vector_as_arraysegment(';
-            code += NumToString(field.value.offset);
-            code += '); }\n';
-            code += '#endif\n';
-    
-    
-    {    code += Indent + '/**\n';
-    code += Indent + ' * @return ' + GenTypeGet(field.value.type) + '\n';
-    code += Indent + ' */\n';
-    code += Indent + 'public function get';
-    code += MakeCamel(field.name);
-    code += '()\n';
-    code += Indent + '{\n';
-    code += Indent + Indent + '$o = $this->__offset(' +
-            NumToString(field.value.offset) + ');\n' + Indent + Indent +
-            'return $o != 0 ? ';
-    code += '$this->bb->get';
-    code += MakeCamel(GenTypeGet(field.value.type)) + '($o + $this->bb_pos)';
-    code += ' : ' + GenDefaultValue(field.value) + ';\n';
-    code += Indent + '}\n\n';
-  }
-    
-    class PythonGenerator : public BaseGenerator {
- public:
-  PythonGenerator(const Parser &parser, const std::string &path,
-                  const std::string &file_name)
-      : BaseGenerator(parser, path, file_name, '' /* not used */,
-                      '' /* not used */),
-        float_const_gen_('float('nan')', 'float('inf')', 'float('-inf')') {
-    static const char * const keywords[] = {
-      'False',
-      'None',
-      'True',
-      'and',
-      'as',
-      'assert',
-      'break',
-      'class',
-      'continue',
-      'def',
-      'del',
-      'elif',
-      'else',
-      'except',
-      'finally',
-      'for',
-      'from',
-      'global',
-      'if',
-      'import',
-      'in',
-      'is',
-      'lambda',
-      'nonlocal',
-      'not',
-      'or',
-      'pass',
-      'raise',
-      'return',
-      'try',
-      'while',
-      'with',
-      'yield'
-    };
-    keywords_.insert(std::begin(keywords), std::end(keywords));
-  }
-    }
-    
-    std::string Parser::UnqualifiedName(std::string full_qualified_name) {
-  Namespace *ns = new Namespace();
-    }
-    
-    template<typename T, typename U>
-void TestEq(T expval, U val, const char *exp, const char *file, int line,
-            const char *func = 0) {
-  if (U(expval) != val) {
-    TestFail(flatbuffers::NumToString(expval).c_str(),
-             flatbuffers::NumToString(val).c_str(), exp, file, line, func);
-  }
-}
-    
-    
-#if !FLATBUFFERS_GRPC_DISABLE_AUTO_VERIFICATION
-  {
-    // Test that an invalid request errors out correctly
-    grpc::ClientContext context;
-    flatbuffers::grpc::Message<Monster> request;  // simulate invalid message
-    flatbuffers::grpc::Message<Stat> response;
-    auto status = stub->Store(&context, request, &response);
-    // The rpc status should be INTERNAL to indicate a verification error. This
-    // matches the protobuf gRPC status code for an unparseable message.
-    assert(!status.ok());
-    assert(status.error_code() == ::grpc::StatusCode::INTERNAL);
-    assert(strcmp(status.error_message().c_str(),
-                  'Message verification failed') == 0);
-  }
+    #define DIV32(a, b) DIV32_(a, b, __FILE__, __LINE__)
+static OPUS_INLINE int DIV32_(opus_int64 a, opus_int64 b, char *file, int line)
+{
+   opus_int64 res;
+   if (b==0)
+   {
+      fprintf(stderr, 'DIV32: divide by zero: %d/%d in %s: line %d\n', (int)a, (int)b, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
 #endif
+      return 0;
+   }
+    }
+    
+        // output element at index 2 (third element)
+    std::cout << array.at(2) << '\n';
+    
+        // out_of_range.402
+    try
+    {
+        // try to use the array index '-'
+        json::const_reference ref = j.at('/array/-'_json_pointer);
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+        // create values of different integer types
+    short n_short = 42;
+    int n_int = -23;
+    long n_long = 1024;
+    int_least32_t n_int_least32_t = -17;
+    uint8_t n_uint8_t = 8;
+    
+    int main()
+{
+    // create JSON values
+    json object = {{'one', 1}, {'two', 2}};
+    json null;
+    }
+    
+      // drop column family
+  s = db->DropColumnFamily(handles[1]);
+  assert(s.ok());
+    
+      // Load the options file.
+  DBOptions loaded_db_opt;
+  std::vector<ColumnFamilyDescriptor> loaded_cf_descs;
+  s = LoadLatestOptions(kDBPath, Env::Default(), &loaded_db_opt,
+                        &loaded_cf_descs);
+  assert(s.ok());
+  assert(loaded_db_opt.create_if_missing == db_opt.create_if_missing);
+    
+    // Supported only for Leveled compaction
+Status SuggestCompactRange(DB* db, ColumnFamilyHandle* column_family,
+                           const Slice* begin, const Slice* end);
+Status SuggestCompactRange(DB* db, const Slice* begin, const Slice* end);
+    
+    // Abstract handle to particular state of a DB.
+// A Snapshot is an immutable object and can therefore be safely
+// accessed from multiple threads without any external synchronization.
+//
+// To Create a Snapshot, call DB::GetSnapshot().
+// To Destroy a Snapshot, call DB::ReleaseSnapshot(snapshot).
+class Snapshot {
+ public:
+  // returns Snapshot's sequence number
+  virtual SequenceNumber GetSequenceNumber() const = 0;
+    }
+    
+        QCoreApplication app(argc, argv);
+    
+        inline void consumer_remove(Coroutine *co)
+    {
+        consumer_queue.remove(co);
+    }
+    
+        SwooleG.use_signalfd = 1;
+    
+        swServer_add_port(&serv, SW_SOCK_UDP, '0.0.0.0', 9502);
+    swServer_add_port(&serv, SW_SOCK_TCP6, '::', 9503);
+    swServer_add_port(&serv, SW_SOCK_UDP6, '::', 9504);
