d4cab44a7efaa741c49a934c9f7e06658aa11c03
SUNsung <weatherius@gmail.com>
2019-05-21 16:01:41 +0300

    Rewrite [06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py] File in: 2019-05-21 16:01:41

--- pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
+++ pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
@@ -1,106 +1,77 @@
 
         
-            self.instances_to_load = tf.placeholder(dtype=tf.string, shape=[None])
-    self.labels_to_load = lexnet_common.load_all_labels(self.instances_to_load)
-    self.pairs_to_load = lexnet_common.load_all_pairs(self.instances_to_load)
-    
-      plt.subplot(nrows,2,11+subplot_cidx)
-  plt.imshow(data_nxt, aspect='auto', interpolation='nearest')
-  plt.title(col_title + ' Data')
-    
-      # Split the data, inputs, labels and times into train vs. validation.
-  rates_train, rates_valid = \
-      split_list_by_inds(rates, train_inds, valid_inds)
-  noisy_data_train, noisy_data_valid = \
-      split_list_by_inds(noisy_data, train_inds, valid_inds)
-  input_train, inputs_valid = \
-      split_list_by_inds(inputs, train_inds, valid_inds)
-  condition_labels_train, condition_labels_valid = \
-      split_list_by_inds(condition_labels, train_inds, valid_inds)
-  input_times_train, input_times_valid = \
-      split_list_by_inds(input_times, train_inds, valid_inds)
-    
-    # unroll RNN for T timesteps
-outputs_t = []
-states_t = []
-    
-        global_word_ids = []
-    current_idx = 0
-    for word_ids in ids:
-      current_size = len(word_ids) - 1  # without <BOS> symbol
-      cur_ids = np.arange(current_idx, current_idx + current_size)
-      global_word_ids.append(cur_ids)
-      current_idx += current_size
-    
-          x[i] = final_x
-      y[i] = final_y
-    
-      Returns:
-    gen_train_op: Generator training op.
-  '''
-  del hparams
-  with tf.name_scope('train_generator'):
-    if FLAGS.generator_optimizer == 'sgd':
-      gen_optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    elif FLAGS.generator_optimizer == 'adam':
-      gen_optimizer = tf.train.AdamOptimizer(learning_rate)
-    else:
-      raise NotImplementedError
-    gen_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
-    ]
-    print('\nOptimizing Generator vars:')
-    for v in gen_vars:
-      print(v)
-    
-      Args:
-    hparams:  MaskGAN hyperparameters.
-  '''
-  ## Dictionary of init savers.
-  init_savers = {}
-    
-      if model == 'dis':
-    assert FLAGS.discriminator_model == 'rnn_nas'
-    assert hparams.dis_num_layers == 2
-    
-    # ZoneoutWrapper.
-from regularization import zoneout
-    
-            params = get_params(test_case.get('params', {}))
-        params['outtmpl'] = tname + '_' + params['outtmpl']
-        if is_playlist and 'playlist' not in test_case:
-            params.setdefault('extract_flat', 'in_playlist')
-            params.setdefault('skip_download', True)
-    
-    rootDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    
-    # Allow direct execution
-import os
-import sys
-import unittest
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    
-    
-def process_code(read_filehandle, text_filehandle, line, linenum, sourcefile, codedir, name, index, indent_depth):
-    fenced = (line.strip() == '```')
-    if fenced:
-        try:
-            line = read_filehandle.next()
-            linenum += 1
-            text_filehandle.write('\n')
-        except StopIteration:
-            return ('', linenum)
-    start_linenum = linenum
-    has_actual_code = False
-    has_question_marks = False
-    linebuffer = []
-    while ((fenced and line.strip() != '```') or (not fenced and is_inside_code(line, indent_depth))):
-        # copy comments to plain text for spell check
-        comment_idx = line.find('//')
-        no_comment_line = line
-        if comment_idx >= 0:
-            no_comment_line = line[:comment_idx].strip()
-            text_filehandle.write(line[comment_idx + 2:])
+            # Separating the 'table of contents' from the contents (blocks)
+    table_of_contents = ''.join(read_me.split('- - -')[0])
+    blocks = ''.join(read_me.split('- - -')[1]).split('\n# ')
+    for i in range(len(blocks)):
+        if i == 0:
+            blocks[i] = blocks[i] + '\n'
         else:
-            # write empty line so line numbers stay stable
-            text_filehandle.write('\n')
+            blocks[i] = '# ' + blocks[i] + '\n'
+    
+    
+@pytest.fixture(autouse=True)
+def shell(mocker):
+    shell = mocker.patch('thefuck.entrypoints.not_configured.shell',
+                         new_callable=MagicMock)
+    shell.get_history.return_value = []
+    shell.how_to_configure.return_value = ShellConfiguration(
+        content='eval $(thefuck --alias)',
+        path='/tmp/.bashrc',
+        reload='bash',
+        can_configure_automatically=True)
+    return shell
+    
+    
+@pytest.mark.functional
+def test_how_to_configure_alias(proc, TIMEOUT):
+    proc.sendline('unset -f fuck')
+    how_to_configure(proc, TIMEOUT)
+
+    
+     install - install packages
+ remove  - remove packages
+    
+    no_such_subcommand = '''error: no such subcommand
+    
+        def request(self, flow):
+        if self.stream:
+            self.active_flows.add(flow)
+    
+        def done(self):
+        if self.reloadtask:
+            self.reloadtask.cancel()
+    
+            - Upstream proxy CONNECT requests should have authentication added, and
+          subsequent already connected requests should not.
+        - Upstream proxy regular requests
+        - Reverse proxy regular requests (CONNECT is invalid in this mode)
+    '''
+    def __init__(self):
+        self.auth = None
+    
+    
+    class ChrmChunk(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self.white_point = self._root.Point(self._io, self, self._root)
+            self.red = self._root.Point(self._io, self, self._root)
+            self.green = self._root.Point(self._io, self, self._root)
+            self.blue = self._root.Point(self._io, self, self._root)
+    
+            else:
+            currentByte = 0x00
+            strReturn = ''
+            while True:
+                currentByte = self.dequeueAndLog()
+                if (currentByte != 0x00):
+                    strReturn += chr(currentByte)
+                else:
+                    break
+    
+        def __getattr__(self, k):
+        if k in self.names:
+            return self.names[k]
+        raise AttributeError('No such attribute: %s', k)
