3b978e35d2a55f08406b962aa68ca764382002f6
SUNsung <weatherius@gmail.com>
2019-05-21 21:02:46 +0300

    Rewrite [27sDgllMSn1g27YNtO8gjV8HM27ymjFyuPlGJU27u.e.cpp] File in: 2019-05-21 21:02:45

--- cccccc/27sDgllMSn1g27YNtO8gjV8HM27ymjFyuPlGJU27u.e.cpp
+++ cccccc/27sDgllMSn1g27YNtO8gjV8HM27ymjFyuPlGJU27u.e.cpp
@@ -1,190 +1,196 @@
 
         
-            http://www.apache.org/licenses/LICENSE-2.0
-    
-    #include 'tensorflow/core/framework/op.h'
-#include 'tensorflow/core/framework/op_kernel.h'
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
-    
-    #endif  // TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
+        
+    {  return Begin + oldSize;
+}  
 
     
-    #include 'tensorflow/stream_executor/cuda/cuda_platform_id.h'
-    
-      Brew mode_;
-    
-     protected:
-  /// @copydoc AbsValLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-      virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
-    
-    #endif  // CAFFE_CONCAT_LAYER_HPP_
+    void PrefixMapKeyPrinter<unsigned char>::print(raw_ostream &out,
+                                               ArrayRef<unsigned char> key) {
+  out << '\'';
+  for (auto byte : key) {
+    if (byte < 16) out << '0';
+    out.write_hex(byte);
+  }
+  out << '\'';
+}
 
     
-    #endif  // CAFFE_CONTRASTIVE_LOSS_LAYER_HPP_
-
+    % for start_code_point, end_code_point, value in break_table.property_value_ranges:
+%   if start_code_point == 0:
+  if (C <= ${end_code_point})
+%   else:
+  if (C >= ${start_code_point} && C <= ${end_code_point})
+%   end
+    return GraphemeClusterBreakProperty::${value};
+% end
+    
+    #include <caffe/proto/caffe.pb.h>
+#include <dmlc/parameter.h>
+#include <dmlc/base.h>
+#include <dmlc/json.h>
+#include <dmlc/logging.h>
+#include <dmlc/type_traits.h>
+#include <google/protobuf/message.h>
+#include <google/protobuf/text_format.h>
+    
+      /*!
+   * \brief Constructor takes function to run.
+   * \param size size of the thread pool.
+   * \param func the function to run on the thread pool.
+   */
+  explicit ThreadPool(size_t size, std::function<void()> func)
+      : worker_threads_(size) {
+    CHECK_GT(size, 0);
+    for (auto& i : worker_threads_) {
+      i = std::thread(func);
+    }
+  }
+  explicit ThreadPool(size_t size,
+                      std::function<void(std::shared_ptr<dmlc::ManualEvent> ready)> func,
+                      const bool wait)
+      : worker_threads_(size) {
+    CHECK_GT(size, 0);
+    for (auto& i : worker_threads_) {
+      std::shared_ptr<dmlc::ManualEvent> ptr = std::make_shared<dmlc::ManualEvent>();
+      ready_events_.emplace_back(ptr);
+      i = std::thread(func, ptr);
+    }
+    if (wait) {
+      WaitForReady();
+    }
+  }
+  ~ThreadPool() noexcept(false) {
+    for (auto&& i : worker_threads_) {
+      i.join();
+    }
+  }
     
     
     {
-    {  // Recursive copy function: this is similar to crop_copy() but loops over all
-  // but the last two dimensions to allow for ND cropping while still relying on
-  // a CUDA kernel for the innermost two dimensions for performance reasons.  An
-  // alterantive implementation could rely on the kernel more by passing
-  // offsets, but this is problematic because of its variable length.
-  // Since in the standard (N,C,W,H) case N,C are usually not cropped a speedup
-  // could be achieved by not looping the application of the copy_kernel around
-  // these dimensions.
-  void crop_copy_gpu(const vector<Blob<Dtype>*>& bottom,
-                const vector<Blob<Dtype>*>& top,
-                const vector<int>& offsets,
-                vector<int> indices,
-                int cur_dim,
-                const Dtype* src_data,
-                Dtype* dest_data,
-                bool is_forward);
-};
-}  // namespace caffe
+    {inline void Dequantize2BitImpl(mshadow::Stream<mshadow::cpu> *s,
+                               const std::vector<mxnet::TBlob> &inputs,
+                               const float threshold) {
+  Dequantize2BitKernelLaunch(s, inputs, threshold);
+}
+}  // namespace kvstore
+}  // namespace mxnet
     
-    #ifdef USE_CUDNN
-/*
- * @brief cuDNN implementation of ConvolutionLayer.
- *        Fallback to ConvolutionLayer for CPU mode.
- *
- * cuDNN accelerates convolution through forward kernels for filtering and bias
- * plus backward kernels for the gradient w.r.t. the filters, biases, and
- * inputs. Caffe + cuDNN further speeds up the computation through forward
- * parallelism across groups and backward parallelism across gradients.
- *
- * The CUDNN engine does not have memory overhead for matrix buffers. For many
- * input and filter regimes the CUDNN engine is faster than the CAFFE engine,
- * but for fully-convolutional models and large inputs the CAFFE engine can be
- * faster as long as it fits in memory.
-*/
-template <typename Dtype>
-class CuDNNConvolutionLayer : public ConvolutionLayer<Dtype> {
- public:
-  explicit CuDNNConvolutionLayer(const LayerParameter& param)
-      : ConvolutionLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNConvolutionLayer();
-    }
+      /*!
+   * \brief returns compression factor, which is the factor by which size of gradient
+   * reduces when using a particular type of compression
+   */
+  int GetCompressionFactor();
     
-    #ifdef USE_CUDNN
-template <typename Dtype>
-class CuDNNLRNLayer : public LRNLayer<Dtype> {
- public:
-  explicit CuDNNLRNLayer(const LayerParameter& param)
-      : LRNLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNLRNLayer();
-    }
-    
-    // A CallData class will be created for every grpc call within a channel. It is
-// used to store data and methods specific to that call. CensusClientCallData is
-// thread-compatible, however typically only 1 thread should be interacting with
-// a call at a time.
-class CensusClientCallData : public CallData {
- public:
-  // Maximum size of trace context is sent on the wire.
-  static constexpr uint32_t kMaxTraceContextLen = 64;
-  // Maximum size of tags that are sent on the wire.
-  static constexpr uint32_t kMaxTagsLen = 2048;
-    }
-    
-    namespace grpc {
-    }
-    
-    #endif  // GRPC_INTERNAL_CPP_EXT_PROTO_SERVER_REFLECTION_H
-
-    
-    
-    {}  // namespace grpc
-
-    
-    
-    {
-    {}  // namespace load_reporter
-}  // namespace grpc
-    
-    				if (szArgList[i + 1] != NULL && szArgList[i + 1][0] != '/')
-				{
-					cmderOptions.cmderCfgRoot = szArgList[i + 1];
-					i++;
-				}
-			}
-			else if (_wcsicmp(L'/start', szArgList[i]) == 0)
-			{
-				int len = wcslen(szArgList[i + 1]);
-				if (wcscmp(&szArgList[i + 1][len - 1], L'\'') == 0)
-				{
-					szArgList[i + 1][len - 1] = '\0';
-				}
-    
-            bool IsZero() const;
-    
-                static void OnVirtualKeyControlChordPropertyChanged(
-                Windows::UI::Xaml::DependencyObject^ target,
-                MyVirtualKey oldValue,
-                MyVirtualKey newValue);
-    
-    
-    {  EsdCanClient esd_can_client;
-  EXPECT_TRUE(esd_can_client.Init(param));
-  EXPECT_EQ(esd_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
-  std::vector<CanFrame> frames;
-  int32_t num = 0;
-  EXPECT_EQ(esd_can_client.Send(frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  EXPECT_EQ(esd_can_client.Receive(&frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
-  CanFrame can_frame;
-  frames.push_back(can_frame);
-  EXPECT_EQ(esd_can_client.SendSingleFrame(frames),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  esd_can_client.Stop();
+    template<typename xpu, typename OP>
+void EvalOneHot_(const TBlob &index, const TBlob &rhs,
+                 TBlob *ret, RunContext ctx) {
+  LOG(INFO) << 'The operator onehot_encode is deprecated; use one_hot instead.';
+  using namespace mshadow::expr;
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  // TODO(eric): support mixed type encoding, i.e. int index and float rhs.
+  CHECK_EQ(ret->type_flag_, mshadow::default_type_flag)
+    << 'one_hot_encode only support float32 as input/output';
+  CHECK_EQ(rhs.type_flag_, mshadow::default_type_flag)
+    << 'one_hot_encode only support float32 as input/output';
+  CHECK_EQ(index.type_flag_, mshadow::default_type_flag)
+    << 'one_hot_encode only support float32 as input/output';
+  ret->get<xpu, 2, real_t>(s) =
+    one_hot_encode(index.get<xpu, 1, real_t>(s),
+                   rhs.shape_[1]);
 }
     
-    #include <memory>
-#include <set>
+      // Pointers to the given variable and result variable
+  // We exchange what given and result point to at every step
+  Tensor<cpu, 2, DType> *given = &storage,
+    *result = &out, *tmp;
     
-    ObjectExtendedInfo60D::ObjectExtendedInfo60D() {}
-const uint32_t ObjectExtendedInfo60D::ID = 0x60D;
+    /*!
+ * Copyright (c) 2015 by Contributors
+ * \file crop.cc
+ * \brief
+ * \author Wei Wu
+*/
+    
+      virtual void Forward(const OpContext &ctx,
+                       const std::vector<TBlob> &in_data,
+                       const std::vector<OpReqType> &req,
+                       const std::vector<TBlob> &out_data,
+                       const std::vector<TBlob> &aux_args) {
+    using namespace mshadow;
+    CHECK_EQ(in_data.size(), 2U);
+    CHECK_EQ(out_data.size(), 3U);
+    Stream<gpu> *s = ctx.get_stream<gpu>();
+    Tensor<gpu, 4, DType> data = in_data[st::kData].get<gpu, 4, DType>(s);
+    Tensor<gpu, 4, DType> out = out_data[st::kOut].get<gpu, 4, DType>(s);
+    Shape<3> loc_shape = Shape3(data.size(0), 2, 3);
+    Shape<4> grid_shape = Shape4(out.size(0), out.size(2), out.size(3), 2);
+    Tensor<gpu, 3, DType> loc = in_data[st::kLoc].get_with_shape<gpu, 3, DType>(loc_shape, s);
+    Tensor<gpu, 4, DType> grid = out_data[st::kGridSrc]
+                                .get_with_shape<gpu, 4, DType>(grid_shape, s);
+    if (!init_cudnn_) {
+     Init(s, in_data, out_data);
+    }
+    CHECK_EQ(data.CheckContiguous(), true);
+    CHECK_EQ(out.CheckContiguous(), true);
+    typename DataType<DType>::ScaleType alpha = 1.0f;
+    typename DataType<DType>::ScaleType beta = 0.0f;
+    if (param_.transform_type == st::kAffine) {
+      CUDNN_CALL(cudnnSpatialTfGridGeneratorForward(s->dnn_handle_,
+                                                    st_desc_,
+                                                    loc.dptr_,
+                                                    grid.dptr_));
+    }
+    CUDNN_CALL(cudnnSpatialTfSamplerForward(s->dnn_handle_,
+                                            st_desc_,
+                                            &alpha,
+                                            in_desc_,
+                                            data.dptr_,
+                                            grid.dptr_,
+                                            &beta,
+                                            out_desc_,
+                                            out.dptr_));
+  }
+    
+    Operator* NativeOpProp::CreateOperator(Context ctx) const {
+  DO_BIND_DISPATCH(CreateOp, param_);
+}
+    
+    /*!
+ * Copyright (c) 2015 by Contributors
+ * \file identity_attach_KL_sparse_reg.cc
+ * \brief\
+*/
+#include './identity_attach_KL_sparse_reg-inl.h'
+#include <nnvm/op_attr_types.h>
+    
+    TEST(ByteTest, SetGetHighLowBit) {
+  unsigned char byte_value = 0x37;
+  Byte value(&byte_value);
+  value.set_value_high_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
+  value.set_value_low_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
+}
     
     
-    {  int ret = x;
+    { protected:
+  SpeedLimit speed_limit_;
+};
+    
+    
+    {
+    {
+    {
+    {  double ret = x * 0.001000;
   return ret;
 }
-    
-    RcsThreshold RadarState201::rcs_threshold(const std::uint8_t* bytes,
-                                          int32_t length) const {
-  Byte t0(bytes + 7);
-  uint32_t x = t0.get_byte(2, 3);
-    }
-    
-    const PolynomialXd& Spline1dSeg::ThirdOrderDerivative() const {
-  return third_order_derivative_;
-}
+}  // namespace gem
+}  // namespace canbus
+}  // namespace apollo
+
     
     namespace apollo {
 namespace canbus {
@@ -193,84 +199,85 @@ namespace gem {
     }
     }
     
-    // config detail: {'name': 'commanded_value', 'enum': {0: 'COMMANDED_VALUE_OFF',
-// 1: 'COMMANDED_VALUE_ON'}, 'precision': 1.0, 'len': 8, 'is_signed_var': False,
-// 'offset': 0.0, 'physical_range': '[0|1]', 'bit': 15, 'type': 'enum', 'order':
-// 'motorola', 'physical_unit': ''}
-Horn_rpt_79::Commanded_valueType Hornrpt79::commanded_value(
-    const std::uint8_t* bytes, int32_t length) const {
-  Byte t0(bytes + 1);
-  int32_t x = t0.get_byte(0, 8);
+    void Brakerpt6c::Parse(const std::uint8_t* bytes, int32_t length,
+                       ChassisDetail* chassis) const {
+  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_manual_input(
+      manual_input(bytes, length));
+  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_commanded_value(
+      commanded_value(bytes, length));
+  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_output_value(
+      output_value(bytes, length));
+  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_brake_on_off(
+      brake_on_off(bytes, length));
+}
+    
+    // config detail: {'name': 'veh_can_timeout', 'offset': 0.0, 'precision': 1.0,
+// 'len': 1, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 2,
+// 'type': 'bool', 'order': 'motorola', 'physical_unit': ''}
+bool Globalrpt6a::veh_can_timeout(const std::uint8_t* bytes,
+                                  int32_t length) const {
+  Byte t0(bytes + 0);
+  int32_t x = t0.get_byte(2, 1);
     }
     
-    Quad3 TiledGrid3DAction::getOriginalTile(const Vec2& pos) const
-{
-    TiledGrid3D *g = (TiledGrid3D*)_gridNodeTarget->getGrid();
-    return g->getOriginalTile(pos);
-}
-    
-    
-    {    return action;
-}
-    
-     THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-    
-    protected:
-    bool _isNeedCleanUp;
-    
-    GridBase* PageTurn3D::getGrid()
-{
-    auto result = Grid3D::create(_gridSize, _gridNodeTarget->getGridRect());
-    if (result)
+        for (i = 1; i < 20000; i++)
     {
-        result->setNeedDepthTestForBlit(true);
+        uint32_t key = i * 37;
+        swRbtree_insert(tree, key, (void *) (long) (i * 8));
     }
     
-    return result;
+            int setContext(redisAsyncContext * ac) {
+            if (ac->ev.data != NULL) {
+                return REDIS_ERR;
+            }
+            m_ctx = ac;
+            m_ctx->ev.data = this;
+            m_ctx->ev.addRead = RedisQtAddRead;
+            m_ctx->ev.delRead = RedisQtDelRead;
+            m_ctx->ev.addWrite = RedisQtAddWrite;
+            m_ctx->ev.delWrite = RedisQtDelWrite;
+            m_ctx->ev.cleanup = RedisQtCleanup;
+            return REDIS_OK;
+        }
+    
+    
+    {    friend
+    void getCallback(redisAsyncContext *, void *, void *);
+};
+    
+        cache.set('test2', val2);
+    ASSERT_EQ(cache.get('test2').get(), val2.get());
+    val2.reset();
+    ASSERT_EQ(dtor_num, 1);
+    ASSERT_EQ(cache.get('test'), nullptr);
+    
+        std::unordered_map<std::string, cache_list_t::iterator> cache_map;
+    cache_list_t cache_list;
+    size_t cache_capacity;
+    
+    TEST(coroutine_base, yield_resume)
+{
+    long _cid;
+    long cid = Coroutine::create([](void *arg)
+    {
+        long cid = Coroutine::get_current_cid();
+        Coroutine *co = Coroutine::get_by_cid(cid);
+        co->yield();
+        *(long *) arg = Coroutine::get_current_cid();
+    }, &_cid);
+    }
+    
+    private:
+    int check_task_param(int dst_worker_id);
+    
+    void my_onClose(swServer *serv, swDataHead *info)
+{
+    printf('PID=%d\tClose fd=%d|from_id=%d\n', getpid(), info->fd, info->from_id);
 }
     
-    /**
-@brief Progress from a percentage to another percentage.
-@since v0.99.1
-*/
-class CC_DLL ProgressFromTo : public ActionInterval
-{
-public:
-    /** 
-     * @brief Create and initializes the action with a duration, a 'from' percentage and a 'to' percentage.
-     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
-     * @param fromPercentage Specify the source percentage.
-     * @param toPercentage Specify the destination percentage.
-     * @return If the creation success, return a pointer of ProgressFromTo action; otherwise, return nil.
-     */
-    static ProgressFromTo* create(float duration, float fromPercentage, float toPercentage);
-    }
     
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+    {    virtual void onTask(int task_id, int src_worker_id, const DataBuffer &data);
+    virtual void onFinish(int task_id, const DataBuffer &data);
+};
     
-                if( ! spriteFrame ) {
-                CCLOG('cocos2d: AnimationCache: Animation '%s' refers to frame '%s' which is not currently in the SpriteFrameCache. This frame will not be added to the animation.', name.c_str(), spriteFrameName.c_str());
-    }
-    
-        // shader stuff
-    setGLProgramState(GLProgramState::getOrCreateWithGLProgramName(GLProgram::SHADER_NAME_POSITION_TEXTURE_COLOR_NO_MVP, texture));
-    
-    
-    {};
-    
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+    #include <iostream>
