8c25883002e71854ba713d7acf53f1fa839adb6c
SUNsung <weatherius@gmail.com>
2019-05-21 07:45:10 +0300

    Rewrite [05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb] File in: 2019-05-21 07:45:09

--- ruby/05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb
+++ ruby/05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb
@@ -1,53 +1,186 @@
 
         
-            def root_url
-      context[:root_url]
-    end
-    
-        def parse(response)
-      parser = Parser.new(response.body)
-      [parser.html, parser.title]
-    end
-    
-          str.truncate(max_length).ljust(max_length) << tag.to_s
-    end
-    
-            css('.l-sub-section', '.alert', '.banner').each do |node|
-          node.name = 'blockquote'
-        end
-    
-            # Remove examples
-        css('.runnable-example').each do |node|
-          node.parent.remove
-        end
-    
-        streams = {
-      liked:         Stream::Likes,
-      mentioned:     Stream::Mention,
-      followed_tags: Stream::FollowedTag,
-      activity:      Stream::Activity
-    }
-    
-    class LogStash::PluginManager::Unpack < LogStash::PluginManager::PackCommand
-  option '--tgz', :flag, 'unpack a packaged tar.gz file', :default => !LogStash::Environment.windows?
-  option '--zip', :flag, 'unpack a packaged  zip file', :default => LogStash::Environment.windows?
-    
-        context 'without a specific plugin' do
-      it 'display a list of plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list')
-        expect(result.stdout.split('\n').size).to be > 1
+            def process_response?(response)
+      if response.error?
+        raise <<~ERROR
+          Error status code (#{response.code}): #{response.return_message}
+          #{response.url}
+          #{JSON.pretty_generate(response.headers).slice(2..-3)}
+        ERROR
+      elsif response.blank?
+        raise 'Empty response body: #{response.url}'
       end
     
-        context 'update all the plugins' do
-      it 'has executed successfully' do
-        logstash.run_command_in_path('bin/logstash-plugin update --no-verify')
-        expect(logstash).to have_installed?(plugin_name, '0.1.1')
+            # Remove ng-* attributes
+        css('*').each do |node|
+          node.attributes.each_key do |attribute|
+            node.remove_attribute(attribute) if attribute.start_with? 'ng-'
+          end
+        end
+    
+        # This returns whether the guest is ready to work. If this returns
+    # `false`, then {#detect!} should be called in order to detect the
+    # guest OS.
+    #
+    # @return [Boolean]
+    def ready?
+      !!capability_host_chain
+    end
+  end
+end
+
+    
+              # Handle the case that argv was empty or didn't contain any subcommand
+          main_args = argv.dup if main_args.nil?
+    
+            # Returns the instance variables as a hash of key-value pairs.
+        def instance_variables_hash
+          instance_variables.inject({}) do |acc, iv|
+            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
+            acc
+          end
+        end
+    
+            # Defines additional configuration keys to be available in the
+        # Vagrantfile. The configuration class should be returned by a
+        # block passed to this method. This is done to ensure that the class
+        # is lazy loaded, so if your class inherits from any classes that
+        # are specific to Vagrant 1.0, then the plugin can still be defined
+        # without breaking anything in future versions of Vagrant.
+        #
+        # @param [String] name Configuration key.
+        # @param [Boolean] upgrade_safe If this is true, then this configuration
+        #   key is safe to load during an upgrade, meaning that it depends
+        #   on NO Vagrant internal classes. Do _not_ set this to true unless
+        #   you really know what you're doing, since you can cause Vagrant
+        #   to crash (although Vagrant will output a user-friendly error
+        #   message if this were to happen).
+        def self.config(name=UNSET_VALUE, upgrade_safe=false, &block)
+          data[:config] ||= Registry.new
+    
+            def initialize
+          # The action hooks hash defaults to []
+          @action_hooks = Hash.new { |h, k| h[k] = [] }
+    
+            occisect = Isect.new
+        @spheres[0].intersect(ray, occisect)
+        @spheres[1].intersect(ray, occisect)
+        @spheres[2].intersect(ray, occisect)
+        @plane.intersect(ray, occisect)
+        if occisect.hit then
+          occlusion = occlusion + 1.0
+        else
+          0.0
+        end
+      end
+    end
+    
+    FIRST     = -> l { LEFT[RIGHT[l]] }
+IF        = -> b { b }
+LEFT      = -> p { p[-> x { -> y { x } } ] }
+RIGHT     = -> p { p[-> x { -> y { y } } ] }
+IS_EMPTY  = LEFT
+REST      = -> l { RIGHT[RIGHT[l]] }
+    
+    def mkboard
+  (0...ROW*COL).each{|i|
+    if i % ROW >= ROW-NP
+      $b[i] = -2
+    else
+      $b[i] = -1
+    end
+    $b[3*ROW+3]=$b[3*ROW+4]=$b[4*ROW+3]=$b[4*ROW+4]=-2
+  }
+end
+    
+          spec['version'] = Bootstrap::VERSION
+    
+      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
+    @logger     = Logger.new
+    @repo       = repo
+    @branch     = branch || 'master'
+    @branch_sha = get_branch_sha
+    @cache_path = cache_path
+    @repo_url   = 'https://github.com/#@repo'
+    @save_to    = {
+        js:    'assets/javascripts/bootstrap',
+        scss:  'assets/stylesheets/bootstrap',
+        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
+  end
+    
+        def str_to_byte_pos(pos)
+      @s.string.slice(0, pos).bytesize
+    end
+  end
+end
+    
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
+      end
+    end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
+  end
+end
+
+    
+          def actual_path
+        $PROGRAM_NAME
       end
     end
   end
 end
 
     
-      desc 'Release all gems to rubygems'
-  task release: :build do
-    sh 'git tag -a -m \'Version #{version}\' v#{version}'
+            self.summary = 'The repl listens to commands on standard input'
+        self.description = <<-DESC
+          The repl listens to commands on standard input and prints their
+          result to standard output.
+          It accepts all the other ipc subcommands. The repl will signal the
+          end of output with the the ASCII CR+LF `\\n\\r`.
+        DESC
+    
+    When /^(?:|I )press '([^']*)'$/ do |button|
+  click_button(button)
+end
+    
+      # Provides configurability to Paperclip. The options available are:
+  # * whiny: Will raise an error if Paperclip cannot process thumbnails of
+  #   an uploaded image. Defaults to true.
+  # * log: Logs progress to the Rails log. Uses ActiveRecord's logger, so honors
+  #   log levels, etc. Defaults to true.
+  # * command_path: Defines the path at which to find the command line
+  #   programs if they are not visible to Rails the system's search path. Defaults to
+  #   nil, which uses the first executable found in the user's search path.
+  # * use_exif_orientation: Whether to inspect EXIF data to determine an
+  #   image's orientation. Defaults to true.
+  def self.options
+    @options ||= {
+      command_path: nil,
+      content_type_mappings: {},
+      log: true,
+      log_command: true,
+      read_timeout: nil,
+      swallow_stderr: true,
+      use_exif_orientation: true,
+      whiny: true,
+      is_windows: Gem.win_platform?
+    }
+  end
+    
+        def type_from_file_contents
+      type_from_mime_magic || type_from_file_command
+    rescue Errno::ENOENT => e
+      Paperclip.log('Error while determining content type: #{e}')
+      SENSIBLE_DEFAULT
+    end
+    
+        # Returns the basename of the file. e.g. 'file' for 'file.jpg'
+    def basename attachment, style_name
+      File.basename(attachment.original_filename, '.*'.freeze)
+    end
