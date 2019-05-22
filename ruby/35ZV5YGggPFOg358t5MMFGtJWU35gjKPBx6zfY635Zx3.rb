62f221dab15a9834fcf59d378cd6cf1bcdb295a9
SUNsung <weatherius@gmail.com>
2019-05-22 14:37:45 +0300

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2019-05-22 14:37:44

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,154 +1,33 @@
 
         
-        def global_require
-  JSON.pretty_generate(DATA)
-end
-    
-        def no_subcommand(args)
-      unless args.empty? ||
-          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
-        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
-                          Run `jekyll help` to find out more.'
-        abort
-      end
-    end
-    
-    module Fastlane
-  # Handles receiving commands from the socket server, finding the Action to be invoked,
-  # invoking it, and returning any return values
-  class SocketServerActionCommandExecutor < CommandExecutor
-    attr_accessor :runner
-    attr_accessor :actions_requiring_special_handling
-    
-        def handle_ssl_error!(e)
-      # SSL errors are very common when the Ruby or OpenSSL installation is somehow broken
-      # We want to show a nice error message to the user here
-      # We have over 20 GitHub issues just for this one error:
-      #   https://github.com/fastlane/fastlane/search?q=errno%3D0+state%3DSSLv3+read+server&type=Issues
-      suggest_ruby_reinstall(e)
-      display_user_error!(e, e.to_s)
-    end
-    
-            cmd << ['-am #{message.shellescape}']
-        cmd << '--force' if options[:force]
-        cmd << '-s' if options[:sign]
-        cmd << tag.shellescape
-        cmd << options[:commit].to_s if options[:commit]
-    
-          if @verify_block
-        begin
-          @verify_block.call(value)
-        rescue => ex
-          UI.error('Error setting value '#{value}' for option '#{@key}'')
-          raise Interface::FastlaneError.new, ex.to_s
+                def collection_method
+          :pull_requests_comments
         end
-      end
     
-    if git.modified_files.include?('snapshot/lib/assets/SnapshotHelperXcode8.swift')
-  warn('You modified `SnapshotHelperXcode8.swift`, make sure to update the version number at the bottom of the file to notify users about the new helper file.')
-end
-    
-    # The * turns the array into a parameter list
-# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
-# We need to use that, because otherwise invocations like
-# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
-exec(*exec_arr)
-
-    
-    require_relative 'options'
-require_relative 'runner'
-    
-    # Include LoggerSilence from ActiveSupport. This is needed to silent assets
-# requests with `config.assets.quiet`, because the default silence method of
-# the logging gem is no-op. See: https://github.com/TwP/logging/issues/11
-Logging::Logger.send :alias_method, :local_level, :level
-Logging::Logger.send :alias_method, :local_level=, :level=
-Logging::Logger.send :include, LoggerSilence
-
-    
-    When /^I (?:log|sign) out$/ do
-  logout
-  step 'I go to the root page'
-end
-    
-        it 'generates the aspects_manage fixture', :fixture => true do
-      get :index, params: {a_id: @aspect.id}
-      save_fixture(html_for('body'), 'aspects_manage')
-    end
-    
-        it 'generates a jasmine fixture', :fixture => true do
-      get :bookmarklet
-      save_fixture(html_for('body'), 'bookmarklet')
-    end
-    
-        sign_in(alice, scope: :user)
-  end
-    
-        it 'should redirect back in the html version if it has > 0 notifications' do
-      FactoryGirl.create(:notification, recipient: alice, type: 'Notifications::StartedSharing')
-      get :read_all, params: {type: 'liked'}, format: :html
-      expect(response).to redirect_to(notifications_path)
-    end
-    
-          it 'should remove participation' do
-        delete :destroy, params: {post_id: post.id}
-        expect(alice.participations.where(:target_id => post.id)).not_to exist
-        expect(response.code).to eq('200')
-      end
-    end
-    
-            it 'reshares the absolute root' do
-          post_request!
-          expect(@post.reshares.count).to eq(0)
-          expect(@root.reshares.count).to eq(2)
+            # Builds a user using a Hash that was built from a JSON payload.
+        def self.from_json_hash(raw_hash)
+          new(Representation.symbolize_hash(raw_hash))
         end
+    
+        def display_error_message(ex)
+      unless options.backtrace
+        Rake.application.options.suppress_backtrace_pattern = backtrace_pattern if backtrace_pattern
+        trace '(Backtrace restricted to imported tasks)'
       end
+    
+        def initialize(app)
+      @app = app
+    end
+    
+            close_body(body) if reaction
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
+    
+          get '/', {}, 'HTTP_COOKIE' => '_session=EVIL_SESSION_TOKEN; _session=SESSION_TOKEN'
+      expect(last_response).not_to be_ok
     end
   end
-    
-                  expect(cop.highlights).to eq([close])
-              expect(cop.messages)
-                .to eq([described_class::ALWAYS_SAME_LINE_MESSAGE])
-            end
-    
-          # Checks whether this node body is a void context.
-      # Always `true` for `for`.
-      #
-      # @return [true] whether the `for` node body is a void context
-      def void_context?
-        true
-      end
-    
-          # A shorthand for getting the last argument of the node.
-      # Equivalent to `arguments.last`.
-      #
-      # @return [Node, nil] the last argument of the node,
-      #                     or `nil` if there are no arguments
-      def last_argument
-        arguments[-1]
-      end
-    
-        # Outputs a list of categories as comma-separated <a> links. This is used
-    # to output the category list for each post on a category page.
-    #
-    #  +categories+ is the list of categories to format.
-    #
-    # Returns string
-    #
-    def category_links(categories)
-      categories.sort.map { |c| category_link c }.join(', ')
-    end
-    
-        def handle_gist_redirecting(data)
-      redirected_url = data.header['Location']
-      if redirected_url.nil? || redirected_url.empty?
-        raise ArgumentError, 'GitHub replied with a 302 but didn't provide a location in the response headers.'
-      end
-    
-          unless file.file?
-        return 'File #{file} could not be found'
-      end
-    
-        def poster
-      'poster='#{@poster}'' if @poster
-    end
+end
