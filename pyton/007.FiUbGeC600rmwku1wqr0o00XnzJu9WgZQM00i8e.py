99dd55c9370eaf87e78dd6274433f44a3f456cc5
SUNsung <weatherius@gmail.com>
2019-05-22 09:01:19 +0300

    Rewrite [007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py] File in: 2019-05-22 09:01:18

--- pyton/007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py
+++ pyton/007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py
@@ -1,120 +1,106 @@
 
         
-        # Pull out some commonly used parameters.
-# These are user parameters (configuration)
-rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
-T = FLAGS.T
-C = FLAGS.C
-N = FLAGS.N
-S = FLAGS.S
-input_magnitude = FLAGS.input_magnitude
-nreplications = FLAGS.nreplications
-E = nreplications * C         # total number of trials
-# S is the number of measurements in each datasets, w/ each
-# dataset having a different set of observations.
-ndatasets = N/S                 # ok if rounded down
-train_percentage = FLAGS.train_percentage
-ntime_steps = int(T / FLAGS.dt)
-# End of user parameters
+            for i in range(1, n + 1):
+        print('==================')
+        print('Iteration %02d of %02d' % (i, n))
+        print('==================')
+        n_features = i * step
+        n_informative = n_features / 10
+    
+    ward = AgglomerativeClustering(n_clusters=3, linkage='ward')
+    
+    solutions = os.listdir(exercise_dir)
+    
+    This example shows the effect of imposing a connectivity graph to capture
+local structure in the data. The graph is simply the graph of 20 nearest
+neighbors.
+    
+    labels = ('Waveform 1', 'Waveform 2', 'Waveform 3')
+    
+    X_restored = agglo.inverse_transform(X_reduced)
+images_restored = np.reshape(X_restored, images.shape)
+plt.figure(1, figsize=(4, 3.5))
+plt.clf()
+plt.subplots_adjust(left=.01, right=.99, bottom=.01, top=.91)
+for i in range(4):
+    plt.subplot(3, 4, i + 1)
+    plt.imshow(images[i], cmap=plt.cm.gray, vmax=16, interpolation='nearest')
+    plt.xticks(())
+    plt.yticks(())
+    if i == 1:
+        plt.title('Original data')
+    plt.subplot(3, 4, 4 + i + 1)
+    plt.imshow(images_restored[i], cmap=plt.cm.gray, vmax=16,
+               interpolation='nearest')
+    if i == 1:
+        plt.title('Agglomerated data')
+    plt.xticks(())
+    plt.yticks(())
     
     
-def write_data(data_fname, data_dict, use_json=False, compression=None):
-  '''Write data in HD5F format.
+#----------------------------------------------------------------------
+# Visualize the clustering
+def plot_clustering(X_red, labels, title=None):
+    x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
+    X_red = (X_red - x_min) / (x_max - x_min)
     
     
-def _SampleModel(prefix_words, vocab):
-  '''Predict next words using the given prefix words.
+try:  # SciPy >= 0.16 have face in misc
+    from scipy.misc import face
+    face = face(gray=True)
+except ImportError:
+    face = sp.face(gray=True)
     
-        self.bos_chars = self._convert_word_to_char_ids(self.bos_char)
-    self.eos_chars = self._convert_word_to_char_ids(self.eos_char)
+    plt.subplot(222)
+plt.scatter(X_aniso[:, 0], X_aniso[:, 1], c=y_pred)
+plt.title('Anisotropicly Distributed Blobs')
     
-      Args:
-    hparams:  Hyperparameters for the MaskGAN.
-    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
-    is_training:  Whether the model is training.
-    reuse (Optional):  Whether to reuse the model.
+    The second plot demonstrate one single run of the ``MiniBatchKMeans``
+estimator using a ``init='random'`` and ``n_init=1``. This run leads to
+a bad convergence (local optimum) with estimated centers stuck
+between ground truth clusters.
     
-    from __future__ import print_function
+    # #############################################################################
+# Plot result
     
-        fpath = os.path.join(path, 'train')
-    x_train, y_train = load_batch(fpath, label_key=label_mode + '_labels')
+        '''
+    prob = np.array(prob)
+    outcome = np.array(outcome)
     
-        outputs1 = Lambda(lambda x: utils.preprocess_input(x, 'channels_last'),
-                      output_shape=x.shape[1:])(inputs)
-    model1 = Model(inputs, outputs1)
-    out1 = model1.predict(x)
-    x2 = np.transpose(x, (0, 3, 1, 2))
-    inputs2 = Input(shape=x2.shape[1:])
-    outputs2 = Lambda(lambda x: utils.preprocess_input(x, 'channels_first'),
-                      output_shape=x2.shape[1:])(inputs2)
-    model2 = Model(inputs2, outputs2)
-    out2 = model2.predict(x2)
-    assert_allclose(out1, out2.transpose(0, 2, 3, 1))
+    # Now I have an array of 12 columns and 891 rows
+# I can access any element I want, so the entire first column would
+# be data[0::,0].astype(np.float) -- This means all of the rows (from start to end), in column 0
+# I have to add the .astype() command, because
+# when appending the rows, python thought it was a string - so needed to convert
     
-    import datetime
-import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+    with tf.device('/cpu:0'):
+  sum = tf.add_n(c2) #Addition of all elements in c2, i.e. A^n + B^n
     
-    import numpy as np
-import keras
-from keras.datasets import reuters
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.preprocessing.text import Tokenizer
+        This function is useful for visualizing datasets whose rows are images,
+    and also columns of matrices for transforming those rows
+    (such as the first layer of a neural net).
     
-    FREQ_CAT_NUM = 4
     
-        def feed(self, byte_str):
-        for i in range(len(byte_str)):
-            coding_state = self.coding_sm.next_state(byte_str[i])
-            if coding_state == MachineState.ERROR:
-                self.logger.debug('%s %s prober hit error at byte %s',
-                                  self.charset_name, self.language, i)
-                self._state = ProbingState.NOT_ME
-                break
-            elif coding_state == MachineState.ITS_ME:
-                self._state = ProbingState.FOUND_IT
-                break
-            elif coding_state == MachineState.START:
-                char_len = self.coding_sm.get_current_charlen()
-                if i == 0:
-                    self._last_char[1] = byte_str[0]
-                    self.distribution_analyzer.feed(self._last_char, char_len)
-                else:
-                    self.distribution_analyzer.feed(byte_str[i - 1:i + 1],
-                                                    char_len)
+def gauss_weight(ndim_in, ndim_out=None, sd=.005):
+    if ndim_out is None:
+        ndim_out = ndim_in
+    W = numpy.random.randn(ndim_in, ndim_out) * sd
+    return numpy.asarray(W, dtype=config.floatX)
     
-    # EUC-JP
     
-            # Log all prober confidences if none met MINIMUM_THRESHOLD
-        if self.logger.getEffectiveLevel() == logging.DEBUG:
-            if self.result['encoding'] is None:
-                self.logger.debug('no probers hit minimum threshold')
-                for group_prober in self._charset_probers:
-                    if not group_prober:
-                        continue
-                    if isinstance(group_prober, CharSetGroupProber):
-                        for prober in group_prober.probers:
-                            self.logger.debug('%s %s confidence = %s',
-                                              prober.charset_name,
-                                              prober.language,
-                                              prober.get_confidence())
-                    else:
-                        self.logger.debug('%s %s confidence = %s',
-                                          prober.charset_name,
-                                          prober.language,
-                                          prober.get_confidence())
-        return self.result
-
+def set_p_to_zero(pvect, i):
+    ''' Provided utility function: given a symbolic vector of
+    probabilities and an index 'i', set the probability of the
+    i-th element to 0 and renormalize the probabilities so they
+    sum to 1.
+    '''
+    new_pvect = T.set_subtensor(pvect[i], 0.)
+    new_pvect = new_pvect / new_pvect.sum()
+    return new_pvect
     
-        def get_confidence(self):
-        unlike = 0.99
-        if self._num_mb_chars < 6:
-            unlike *= self.ONE_CHAR_PROB ** self._num_mb_chars
-            return 1.0 - unlike
-        else:
-            return unlike
+        def test_clean_date_time_zone(self):
+        date, date_time, time_zone_parsed = \
+            self.mr_job.clean_date_time_zone(self.DATE_TIME_ZONE_VALID)
+        self.assertEqual(date, self.DATE_VALID)
+        self.assertEqual(date_time, self.DATE_TIME_VALID)
+        self.assertEqual(time_zone_parsed, self.TIME_ZONE_VALID)
