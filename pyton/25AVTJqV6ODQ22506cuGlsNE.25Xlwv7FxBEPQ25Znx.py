b1dff33a4c1e2c31df9bdb70faa70b41b4d4c969
SUNsung <weatherius@gmail.com>
2019-05-22 07:56:17 +0300

    Rewrite [25AVTJqV6ODQ22506cuGlsNE.25Xlwv7FxBEPQ25Znx.py] File in: 2019-05-22 07:56:16

--- pyton/25AVTJqV6ODQ22506cuGlsNE.25Xlwv7FxBEPQ25Znx.py
+++ pyton/25AVTJqV6ODQ22506cuGlsNE.25Xlwv7FxBEPQ25Znx.py
@@ -1,271 +1,131 @@
 
         
-        model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu', name='features'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+            styles = {
+        # No corresponding class for the following:
+        #Text:                     '', # class:  ''
+        Whitespace:                'underline #f8f8f8',      # class: 'w'
+        Error:                     '#a40000 border:#ef2929', # class: 'err'
+        Other:                     '#000000',                # class 'x'
+    }
     
-        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
-        loaded_model = keras.models.load_model(model_filename)
-        loaded_model.predict(np.random.rand(128, 2))
-    
-        input_tensor = Input(shape=(data_dim,))
-    dummy_model = Model(input_tensor, dense_layer(input_tensor))
-    
-        Example 2 - Training models with weights merge on CPU using cpu_relocation
-    
-    x_train = x_train.reshape(x_train.shape[0], -1, 1)
-x_test = x_test.reshape(x_test.shape[0], -1, 1)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+    # Syntax sugar.
+_ver = sys.version_info
     
     
-def categorical_crossentropy(y_true, y_pred):
-    return K.categorical_crossentropy(y_true, y_pred)
+class MockResponse(object):
+    '''Wraps a `httplib.HTTPMessage` to mimic a `urllib.addinfourl`.
     
-        def _remote_state(self):
-        '''Returns the remote state from AWS'''
-        description = self.rule.describe()
-        if not description:
+            return request_content
+    
+        if state == 'present':
+        if not all((replication_id, cluster_id)):
+            module.fail_json(msg='The state 'present' requires options: 'replication_id' and 'cluster_id'')
+        response, changed = create(module, connection, replication_id, cluster_id, name)
+    elif state == 'absent':
+        response, changed = delete(module, connection, name)
+    elif state == 'copy':
+        if not all((target, bucket)):
+            module.fail_json(msg='The state 'copy' requires options: 'target' and 'bucket'.')
+        response, changed = copy(module, connection, name, target, bucket)
+    
+    - oneandone_moitoring_policy:
+    auth_token: oneandone_private_api_key
+    state: absent
+    name: ansible monitoring policy
+    
+    from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.six.moves.urllib.parse import urlencode
+from ansible.module_utils._text import to_native
+from ansible.module_utils.urls import fetch_url
+    
+    
+class QPSSpider(Spider):
+    
+        def __enter__(self):
+        from scrapy.utils.test import get_testenv
+        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
+        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
+                                     env=get_testenv())
+        self.proc.stdout.readline()
+    
+    from scrapy.commands import ScrapyCommand
+from scrapy.exceptions import UsageError
+    
+    from w3lib.url import is_url
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('--get', dest='get', metavar='SETTING',
+            help='print raw setting value')
+        parser.add_option('--getbool', dest='getbool', metavar='SETTING',
+            help='print setting value, interpreted as a boolean')
+        parser.add_option('--getint', dest='getint', metavar='SETTING',
+            help='print setting value, interpreted as an integer')
+        parser.add_option('--getfloat', dest='getfloat', metavar='SETTING',
+            help='print setting value, interpreted as a float')
+        parser.add_option('--getlist', dest='getlist', metavar='SETTING',
+            help='print setting value, interpreted as a list')
+    
+            Same as Twisted's private _sslverify.ClientTLSOptions,
+        except that VerificationError, CertificateError and ValueError
+        exceptions are caught, so that the connection is not closed, only
+        logging warnings.
+        '''
+    
+        def process_request(self, request, spider):
+        if request.meta.get('dont_merge_cookies', False):
             return
-        return description['state']
     
-            for server_cert in server_certs:
-            if not name:
-                server_cert = iam.get_server_certificate(ServerCertificateName=server_cert['ServerCertificateName'])['ServerCertificate']
-            cert_md = server_cert['ServerCertificateMetadata']
-            results[cert_md['ServerCertificateName']] = {
-                'certificate_body': server_cert['CertificateBody'],
-                'server_certificate_id': cert_md['ServerCertificateId'],
-                'server_certificate_name': cert_md['ServerCertificateName'],
-                'arn': cert_md['Arn'],
-                'path': cert_md['Path'],
-                'expiration': cert_md['Expiration'].isoformat(),
-                'upload_date': cert_md['UploadDate'].isoformat(),
-            }
     
-        json_output = {}
-    if mod_params['view'] == 'topics':
-        json_output['topics'] = list_func(pubsub_client.list_topics())
-    elif mod_params['view'] == 'subscriptions':
-        if mod_params['topic']:
-            t = pubsub_client.topic(mod_params['topic'])
-            json_output['subscriptions'] = list_func(t.list_subscriptions())
+def test_match():
+    cases = [
+        (None, b'\xc0'),
+        (False, b'\xc2'),
+        (True, b'\xc3'),
+        (0, b'\x00'),
+        (127, b'\x7f'),
+        (128, b'\xcc\x80'),
+        (256, b'\xcd\x01\x00'),
+        (-1, b'\xff'),
+        (-33, b'\xd0\xdf'),
+        (-129, b'\xd1\xff\x7f'),
+        ({1: 1}, b'\x81\x01\x01'),
+        (1.0, b'\xcb\x3f\xf0\x00\x00\x00\x00\x00\x00'),
+        ((), b'\x90'),
+        (tuple(range(15)), (b'\x9f\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09'
+                            b'\x0a\x0b\x0c\x0d\x0e')),
+        (tuple(range(16)), (b'\xdc\x00\x10\x00\x01\x02\x03\x04\x05\x06\x07'
+                            b'\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f')),
+        ({}, b'\x80'),
+        ({x: x for x in range(15)},
+         (b'\x8f\x00\x00\x01\x01\x02\x02\x03\x03\x04\x04\x05\x05\x06\x06\x07'
+          b'\x07\x08\x08\t\t\n\n\x0b\x0b\x0c\x0c\r\r\x0e\x0e')),
+        ({x: x for x in range(16)},
+         (b'\xde\x00\x10\x00\x00\x01\x01\x02\x02\x03\x03\x04\x04\x05\x05\x06'
+          b'\x06\x07\x07\x08\x08\t\t\n\n\x0b\x0b\x0c\x0c\r\r\x0e\x0e'
+          b'\x0f\x0f')),
+    ]
+    
+    
+def test_getitem_callable():
+    # GH 12533
+    s = pd.Series(4, index=list('ABCD'))
+    result = s[lambda x: 'A']
+    assert result == s.loc['A']
+    
+    
+ABCGeneric = _ABCGeneric('ABCGeneric', tuple(), {})
+
+    
+    
+Your class ``TestDtype`` will inherit all the tests defined on
+``BaseDtypeTests``. pytest's fixture discover will supply your ``dtype``
+wherever the test requires it. You're free to implement additional tests.
+    
+        @classmethod
+    def construct_from_string(cls, string):
+        if string == cls.name:
+            return cls()
         else:
-            json_output['subscriptions'] = list_func(pubsub_client.list_subscriptions())
-    
-    - heroku_collaborator:
-    api_key: YOUR_API_KEY
-    user: '{{ item.user }}'
-    apps: '{{ item.apps | default(apps) }}'
-    suppress_invitation: '{{ item.suppress_invitation | default(suppress_invitation) }}'
-    state: '{{ item.state | default('present') }}'
-  with_items:
-    - { user: 'a.b@example.com' }
-    - { state: 'absent', user: 'b.c@example.com', suppress_invitation: false }
-    - { user: 'x.y@example.com', apps: ['heroku-example-app'] }
-'''
-    
-    HAS_ONEANDONE_SDK = True
-    
-        # Send the data to bigpanda
-    data = json.dumps(body)
-    headers = {'Authorization': 'Bearer %s' % token, 'Content-Type': 'application/json'}
-    try:
-        response, info = fetch_url(module, request_url, data=data, headers=headers)
-        if info['status'] == 200:
-            module.exit_json(changed=True, **deployment)
-        else:
-            module.fail_json(msg=json.dumps(info))
-    except Exception as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-    
-    
-class Icinga2FeatureHelper:
-    def __init__(self, module):
-        self.module = module
-        self._icinga2 = module.get_bin_path('icinga2', True)
-        self.feature_name = self.module.params['name']
-        self.state = self.module.params['state']
-    
-    
-class ResourceTest(unittest.TestCase):
-    '''Tests for acme.fields.Resource.'''
-    
-    AUGEAS_LENS_DIR = pkg_resources.resource_filename(
-    'certbot_apache', 'augeas_lens')
-'''Path to the Augeas lens directory'''
-    
-    ========================================  =====================================
-``--dns-cloudxns-credentials``            CloudXNS credentials_ INI file.
-                                          (Required)
-``--dns-cloudxns-propagation-seconds``    The number of seconds to wait for DNS
-                                          to propagate before asking the ACME
-                                          server to verify the DNS record.
-                                          (Default: 30)
-========================================  =====================================
-    
-    # http://docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs
-# on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
-    
-    
-def FormatDebugInfoResponse_Completer_ServerNotRunningWithNoLogfiles_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'completer' ][ 'servers' ][ 0 ].update( {
-    'is_running': False,
-    'logfiles': []
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Completer name completer debug information:\n'
-      '  Server name not running\n'
-      '  Server name executable: /path/to/executable\n'
-      '  No logfiles available\n'
-      '  Server name key: value\n'
-      '  Key: value\n'
-    )
-  )
-
-    
-    from hamcrest import assert_that, equal_to
-from mock import patch, call
-    
-    
-def KeywordsFromSyntaxListOutput_PhpSyntax_ContainsPreProc_test():
-  assert_that( syntax_parse._KeywordsFromSyntaxListOutput(
-                   ContentsOfTestFile( 'php_syntax' ) ),
-               has_items( 'skip', 'function' ) )
-    
-    
-# This file provides an UnsafeThreadPoolExecutor, which operates exactly like
-# the upstream Python version of ThreadPoolExecutor with one exception: it
-# doesn't wait for worker threads to finish before shutting down the Python
-# interpreter.
-#
-# This is dangerous for many workloads, but fine for some (like when threads
-# only send network requests). The YCM workload is one of those workloads where
-# it's safe (the aforementioned network requests case).
-    
-        def done(self):
-        '''Return True of the future was cancelled or finished executing.'''
-        with self._condition:
-            return self._state in [CANCELLED, CANCELLED_AND_NOTIFIED, FINISHED]
-    
-    class ProcessPoolExecutor(_base.Executor):
-    def __init__(self, max_workers=None):
-        '''Initializes a new ProcessPoolExecutor instance.
-    
-    Win1250HungarianModel = {
-  'char_to_order_map': win1250HungarianCharToOrderMap,
-  'precedence_matrix': HungarianLangModel,
-  'typical_positive_ratio': 0.947368,
-  'keep_english_letter': True,
-  'charset_name': 'windows-1250',
-  'language': 'Hungarian',
-}
-
-    
-        def feed(self, byte_str):
-        for i in range(len(byte_str)):
-            coding_state = self.coding_sm.next_state(byte_str[i])
-            if coding_state == MachineState.ERROR:
-                self.logger.debug('%s %s prober hit error at byte %s',
-                                  self.charset_name, self.language, i)
-                self._state = ProbingState.NOT_ME
-                break
-            elif coding_state == MachineState.ITS_ME:
-                self._state = ProbingState.FOUND_IT
-                break
-            elif coding_state == MachineState.START:
-                char_len = self.coding_sm.get_current_charlen()
-                if i == 0:
-                    self._last_char[1] = byte_str[0]
-                    self.distribution_analyzer.feed(self._last_char, char_len)
-                else:
-                    self.distribution_analyzer.feed(byte_str[i - 1:i + 1],
-                                                    char_len)
-    
-            charset_name = self._model['charset_name']
-        if self.state == ProbingState.DETECTING:
-            if self._total_seqs > self.SB_ENOUGH_REL_THRESHOLD:
-                confidence = self.get_confidence()
-                if confidence > self.POSITIVE_SHORTCUT_THRESHOLD:
-                    self.logger.debug('%s confidence = %s, we have a winner',
-                                      charset_name, confidence)
-                    self._state = ProbingState.FOUND_IT
-                elif confidence < self.NEGATIVE_SHORTCUT_THRESHOLD:
-                    self.logger.debug('%s confidence = %s, below negative '
-                                      'shortcut threshhold %s', charset_name,
-                                      confidence,
-                                      self.NEGATIVE_SHORTCUT_THRESHOLD)
-                    self._state = ProbingState.NOT_ME
-    
-        def reset(self):
-        return 0
-
-    
-    def test_text_envs():
-    env = gym.make('FrozenLake-v0')
-    video = VideoRecorder(env)
-    try:
-        env.reset()
-        video.capture_frame()
-        video.close()
-    finally:
-        os.remove(video.path)
-
-    
-    SIZE = 0.02
-ENGINE_POWER            = 100000000*SIZE*SIZE
-WHEEL_MOMENT_OF_INERTIA = 4000*SIZE*SIZE
-FRICTION_LIMIT          = 1000000*SIZE*SIZE     # friction ~= mass ~= size^2 (calculated implicitly using density)
-WHEEL_R  = 27
-WHEEL_W  = 14
-WHEELPOS = [
-    (-55,+80), (+55,+80),
-    (-55,-82), (+55,-82)
-    ]
-HULL_POLY1 =[
-    (-60,+130), (+60,+130),
-    (+60,+110), (-60,+110)
-    ]
-HULL_POLY2 =[
-    (-15,+120), (+15,+120),
-    (+20, +20), (-20,  20)
-    ]
-HULL_POLY3 =[
-    (+25, +20),
-    (+50, -10),
-    (+50, -40),
-    (+20, -90),
-    (-20, -90),
-    (-50, -40),
-    (-50, -10),
-    (-25, +20)
-    ]
-HULL_POLY4 =[
-    (-50,-120), (+50,-120),
-    (+50,-90),  (-50,-90)
-    ]
-WHEEL_COLOR = (0.0,0.0,0.0)
-WHEEL_WHITE = (0.3,0.3,0.3)
-MUD_COLOR   = (0.4,0.4,0.0)
+            raise TypeError('Cannot construct a '{}' from '
+                            ''{}''.format(cls, string))
