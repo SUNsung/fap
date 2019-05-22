19f15ebf15c87dc45fb3c607da965b2b41ba69ae
SUNsung <weatherius@gmail.com>
2019-05-22 22:41:01 +0300

    Rewrite [04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py] File in: 2019-05-22 22:41:00

--- pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
+++ pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
@@ -1,148 +1,113 @@
 
         
         
-def replace_settingslist_nodes(app, doctree, fromdocname):
-    env = app.builder.env
+def is_setting_index(node):
+    if node.tagname == 'index':
+        # index entries for setting directives look like:
+        # [(u'pair', u'SETTING_NAME; setting', u'std:setting-SETTING_NAME', '')]
+        entry_type, info, refid = node['entries'][0][:3]
+        return entry_type == 'pair' and info.endswith('; setting')
+    return False
     
-                _contents = _contents.replace(match.group(3), match.group(4))
-    else:
-        # We don't understand what the current line means!
-        print('Not Understood: ' + line)
-
+        # Max concurrency is limited by global CONCURRENT_REQUESTS setting
+    max_concurrent_requests = 8
+    # Requests per second goal
+    qps = None # same as: 1 / download_delay
+    download_delay = None
+    # time in seconds to delay server responses
+    latency = None
+    # number of slots to create
+    slots = 1
     
-            if 'latency' in request.args:
-            latency = float(request.args['latency'][0])
-            reactor.callLater(latency, self._finish, request)
-            return NOT_DONE_YET
+        def short_desc(self):
+        return 'Edit spider'
     
+        def short_desc(self):
+        return 'Run a self-contained spider (without creating a project)'
     
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
-    
-        requires_project = True
-    
-            Quoting https://twistedmatrix.com/documents/current/api/twisted.web.client.Agent.html:
-        'The default is to use a BrowserLikePolicyForHTTPS,
-        so unless you have special requirements you can leave this as-is.'
-    
-        class _v19_S3Connection(S3Connection):
-        '''A dummy S3Connection wrapper that doesn't do any synchronous download'''
-        def _mexe(self, method, bucket, key, headers, *args, **kwargs):
-            return headers
+    class Command(ScrapyCommand):
     
     
-def _parsed_url_args(parsed):
-    # Assume parsed is urlparse-d from Request.url,
-    # which was passed via safe_url_string and is ascii-only.
-    b = lambda s: to_bytes(s, encoding='ascii')
-    path = urlunparse(('', '', parsed.path or '/', parsed.params, parsed.query, ''))
-    path = b(path)
-    host = b(parsed.hostname)
-    port = parsed.port
-    scheme = b(parsed.scheme)
-    netloc = b(parsed.netloc)
-    if port is None:
-        port = 443 if scheme == b'https' else 80
-    return scheme, netloc, host, port, path
+class Command(ScrapyCommand):
     
-            if cookie.get('path', None):
-            cookie_str += '; Path=%s' % cookie['path']
-        if cookie.get('domain', None):
-            cookie_str += '; Domain=%s' % cookie['domain']
     
-                if len(face_bounding_boxes) != 1:
-                # If there are no people (or too many people) in a training image, skip the image.
-                if verbose:
-                    print('Image {} not suitable for training: {}'.format(img_path, 'Didn't find a face' if len(face_bounding_boxes) < 1 else 'Found more than one face'))
+def _parse(url):
+    ''' Return tuple of (scheme, netloc, host, port, path),
+    all in bytes except for port which is int.
+    Assume url is from Request.url, which was passed via safe_url_string
+    and is ascii-only.
+    '''
+    url = url.strip()
+    parsed = urlparse(url)
+    return _parsed_url_args(parsed)
+    
+            print('Two or more elements')
+        data = [5, 1, 7, 2, 6, -3, 5, 7, -10]
+        assert_equal(func(data), sorted(data))
+    
+            print('Test: Enqueue on empty stack')
+        print('Test: Enqueue on non-empty stack')
+        print('Test: Multiple enqueue in a row')
+        num_items = 3
+        for i in range(0, num_items):
+            queue.enqueue(i)
+    
+            print('Test: Two or more element stack (general case)')
+        num_items = 10
+        numbers = [randint(0, 10) for x in range(num_items)]
+        sorted_stack = self.get_sorted_stack(stack, numbers)
+        sorted_numbers = []
+        for _ in range(num_items):
+            sorted_numbers.append(sorted_stack.pop())
+        assert_equal(sorted_numbers, sorted(numbers, reverse=True))
+    
+    		print('Test: min node')
+		assert_equal(myTree.minNode(), 1)
+    
+    
+class PriorityQueue(object):
+    
+        def load(self, loader):
+        loader.add_option(
+            'termlog_verbosity', str, 'info',
+            'Log verbosity.',
+            choices=log.LogTierOrder
+        )
+    
+        def configure(self, updated):
+        # FIXME: We're doing this because our proxy core is terminally confused
+        # at the moment. Ideally, we should be able to check if we're in
+        # reverse proxy mode at the HTTP layer, so that scripts can put the
+        # proxy in reverse proxy mode for specific requests.
+        if 'upstream_auth' in updated:
+            if ctx.options.upstream_auth is None:
+                self.auth = None
             else:
-                # Add face encoding for current image to the training set
-                X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
-                y.append(class_dir)
-    
-    # Find all the faces in the image using the default HOG-based model.
-# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-# See also: find_faces_in_picture_cnn.py
-face_locations = face_recognition.face_locations(image)
-    
-    for face_location in face_locations:
+                self.auth = parse_upstream_auth(ctx.options.upstream_auth)
     
     
-def face_locations(img, number_of_times_to_upsample=1, model='hog'):
-    '''
-    Returns an array of bounding boxes of human faces in a image
+    class IhdrChunk(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self.width = self._io.read_u4be()
+            self.height = self._io.read_u4be()
+            self.bit_depth = self._io.read_u1()
+            self.color_type = self._root.ColorType(self._io.read_u1())
+            self.compression_method = self._io.read_u1()
+            self.filter_method = self._io.read_u1()
+            self.interlace_method = self._io.read_u1()
     
-        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
-    context = multiprocessing
-    if 'forkserver' in multiprocessing.get_all_start_methods():
-        context = multiprocessing.get_context('forkserver')
+        def take(self):
+        '''
+        Scripts or other parties make 'take' a reply out of a normal flow.
+        For example, intercepted flows are taken out so that the connection thread does not proceed.
+        '''
+        if self.state != 'start':
+            raise exceptions.ControlException(
+                'Reply is {}, but expected it to be start.'.format(self.state)
+            )
+        self._state = 'taken'
     
-    # Load a sample picture and learn how to recognize it.
-print('Loading known face image(s)')
-obama_image = face_recognition.load_image_file('obama_small.jpg')
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-    
-                frame_number = frame_count - 128 + frame_number_in_batch
-            print('I found {} face(s) in frame #{}.'.format(number_of_faces_in_frame, frame_number))
-    
-    # Find all the faces and face encodings in the unknown image
-face_locations = face_recognition.face_locations(unknown_image)
-face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
-    
-        if not unknown_encodings:
-        # print out fact that no faces were found in image
-        print_result(image_to_check, 'no_persons_found', None, show_distance)
-    
-            print('I see someone named {}!'.format(name))
-
-    
-    
-def detect_faces_in_image(file_stream):
-    # 用face_recognition.face_encodings(img)接口提前把奥巴马人脸的编码录入
-    known_face_encoding = [-0.09634063,  0.12095481, -0.00436332, -0.07643753,  0.0080383,
-                            0.01902981, -0.07184699, -0.09383309,  0.18518871, -0.09588896,
-                            0.23951106,  0.0986533 , -0.22114635, -0.1363683 ,  0.04405268,
-                            0.11574756, -0.19899382, -0.09597053, -0.11969153, -0.12277931,
-                            0.03416885, -0.00267565,  0.09203379,  0.04713435, -0.12731361,
-                           -0.35371891, -0.0503444 , -0.17841317, -0.00310897, -0.09844551,
-                           -0.06910533, -0.00503746, -0.18466514, -0.09851682,  0.02903969,
-                           -0.02174894,  0.02261871,  0.0032102 ,  0.20312519,  0.02999607,
-                           -0.11646006,  0.09432904,  0.02774341,  0.22102901,  0.26725179,
-                            0.06896867, -0.00490024, -0.09441824,  0.11115381, -0.22592428,
-                            0.06230862,  0.16559327,  0.06232892,  0.03458837,  0.09459756,
-                           -0.18777156,  0.00654241,  0.08582542, -0.13578284,  0.0150229 ,
-                            0.00670836, -0.08195844, -0.04346499,  0.03347827,  0.20310158,
-                            0.09987706, -0.12370517, -0.06683611,  0.12704916, -0.02160804,
-                            0.00984683,  0.00766284, -0.18980607, -0.19641446, -0.22800779,
-                            0.09010898,  0.39178532,  0.18818057, -0.20875394,  0.03097027,
-                           -0.21300618,  0.02532415,  0.07938635,  0.01000703, -0.07719778,
-                           -0.12651891, -0.04318593,  0.06219772,  0.09163868,  0.05039065,
-                           -0.04922386,  0.21839413, -0.02394437,  0.06173781,  0.0292527 ,
-                            0.06160797, -0.15553983, -0.02440624, -0.17509389, -0.0630486 ,
-                            0.01428208, -0.03637431,  0.03971229,  0.13983178, -0.23006812,
-                            0.04999552,  0.0108454 , -0.03970895,  0.02501768,  0.08157793,
-                           -0.03224047, -0.04502571,  0.0556995 , -0.24374914,  0.25514284,
-                            0.24795187,  0.04060191,  0.17597422,  0.07966681,  0.01920104,
-                           -0.01194376, -0.02300822, -0.17204897, -0.0596558 ,  0.05307484,
-                            0.07417042,  0.07126575,  0.00209804]
-    
-    import tensorflow as tf
-    
-    import tensorflow as tf
-    
-    
-def permute(x, perm):
-    '''
-    Examples:
-        x.shape == [128, 32, 1]
-        x = permute(x, [0, 2, 1])
-        x.shape == [128, 1, 32]
-    
-    if word_vec.any():  #
-    word_vec = word_vec / max(1, ngrams_found)
-else:  # 如果一个 ngram 都没找到，gensim 会报错；个人认为把 0 向量传出来也可以
-    raise KeyError('all ngrams for word %s absent from model' % word_unk)
+    class BiDi:
