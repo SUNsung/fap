db5f7f49c7c9ea12331f8fd297cd4a2355ea9266
SUNsung <weatherius@gmail.com>
2019-05-22 03:41:19 +0300

    Rewrite [59jukGhgYYxQM59qYy8Fehu14o59DT41hPFZn7Y59cG.cpp] File in: 2019-05-22 03:41:19

--- cccccc/59jukGhgYYxQM59qYy8Fehu14o59DT41hPFZn7Y59cG.cpp
+++ cccccc/59jukGhgYYxQM59qYy8Fehu14o59DT41hPFZn7Y59cG.cpp
@@ -1,316 +1,168 @@
 
         
-        namespace tesseract {
-    }
-    
-    #ifndef TESSERACT_CSTRUCT_BOXWORD_H_
-#define TESSERACT_CSTRUCT_BOXWORD_H_
-    
-      // Backwards compatible constrained fit with a supplied gradient.
-  // Deprecated. Use ConstrainedFit(const FCOORD& direction) where possible
-  // to avoid potential difficulties with infinite gradients.
-  double ConstrainedFit(double m, float* c);
-    
-    
-// Returns the median value of the vector, given that the values are
-// circular, with the given modulus. Values may be signed or unsigned,
-// eg range from -pi to pi (modulus 2pi) or from 0 to 2pi (modulus 2pi).
-// NOTE that the array is shuffled, but the time taken is linear.
-// An assumption is made that most of the values are spread over no more than
-// half the range, but wrap-around is accounted for if the median is near
-// the wrap-around point.
-// Cannot be a member of GenericVector, as it makes heavy used of LLSQ.
-// T must be an integer or float/double type.
-template<typename T> T MedianOfCircularValues(T modulus, GenericVector<T>* v) {
-  LLSQ stats;
-  T halfrange = static_cast<T>(modulus / 2);
-  int num_elements = v->size();
-  for (int i = 0; i < num_elements; ++i) {
-    stats.add((*v)[i], (*v)[i] + halfrange);
-  }
-  bool offset_needed = stats.y_variance() < stats.x_variance();
-  if (offset_needed) {
-    for (int i = 0; i < num_elements; ++i) {
-      (*v)[i] += halfrange;
-    }
-  }
-  int median_index = v->choose_nth_item(num_elements / 2);
-  if (offset_needed) {
-    for (int i = 0; i < num_elements; ++i) {
-      (*v)[i] -= halfrange;
-    }
-  }
-  return (*v)[median_index];
-}
-    
-    class BLOCK;
-class FCOORD;
-class TBOX;
-class UNICHARSET;
-    
-      bool is_list_item;
-    
-    // Computes the Otsu threshold(s) for the given histogram.
-// Also returns H = total count in histogram, and
-// omega0 = count of histogram below threshold.
-int OtsuStats(const int* histogram, int* H_out, int* omega0_out);
-    
-      // Sets up the ptr array to length, whatever it was before.
-  void initialise(int16_t length);
-    
-    #include 'errcode.h'
-    
-    // Vector of IntKDPair.
-class KDVector : public GenericVector<IntKDPair> {
-  // TODO(rays) Add some code to manipulate a KDVector. For now there
-  // is nothing and this class is effectively a specialization typedef.
-};
-    
-    // A simple object cache which maps a string to an object of type T.
-// Usually, these are expensive objects that are loaded from disk.
-// Reference counting is performed, so every Get() needs to be followed later
-// by a Free().  Actual deletion is accomplished by DeleteUnusedObjects().
-template<typename T>
-class ObjectCache {
- public:
-  ObjectCache() = default;
-  ~ObjectCache() {
-    mu_.Lock();
-    for (int i = 0; i < cache_.size(); i++) {
-      if (cache_[i].count > 0) {
-        tprintf('ObjectCache(%p)::~ObjectCache(): WARNING! LEAK! object %p '
-                'still has count %d (id %s)\n',
-                this, cache_[i].object, cache_[i].count,
-                cache_[i].id.string());
-      } else {
-        delete cache_[i].object;
-        cache_[i].object = nullptr;
-      }
-    }
-    mu_.Unlock();
-  }
-    }
-    
-    
-    {}  // namespace mxnet
-    
-     public:
-  /*! \brief cuda kernel argument descriptor */
-  struct ArgType {
-    /*! \brief whether argument is NDArray */
-    bool is_ndarray;
-    /*! \brief whether argument is constant (input) */
-    bool is_const;
-    /*! \brief data type of argument */
-    mshadow::TypeFlag dtype;
-  };
-  /*! \brief Cuda kernel */
-  class Kernel {
-   public:
-    /*! \brief Launch the kernel */
-    void Launch(const Context& ctx, const std::vector<dmlc::any>& args,
-                uint32_t grid_dim_x, uint32_t grid_dim_y, uint32_t grid_dim_z,
-                uint32_t block_dim_x, uint32_t block_dim_y, uint32_t block_dim_z,
-                uint32_t shared_mem);
-    /*! \brief kernel interface signature */
-    const std::vector<ArgType>& signature() { return signature_; }
-    }
-    
-    /*!
- * \brief Thread pool.
- */
-class ThreadPool {
- public:
-  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
-  struct SetReadyOnDestroy {
-    explicit inline SetReadyOnDestroy(const std::shared_ptr<dmlc::ManualEvent>& event)
-      : event_(event) {
-    }
-    inline ~SetReadyOnDestroy() {
-      if (event_) {
-        event_->signal();
-      }
-    }
-    std::shared_ptr<dmlc::ManualEvent>  event_;
-  };
-    }
-    
-    
-    {
-    {/*! \brief typedef the factory function of data iterator */
-typedef std::function<ImageAugmenter *()> ImageAugmenterFactory;
-/*!
- * \brief Registry entry for DataIterator factory functions.
- */
-struct ImageAugmenterReg
-    : public dmlc::FunctionRegEntryBase<ImageAugmenterReg,
-                                        ImageAugmenterFactory> {
-};
-//--------------------------------------------------------------
-// The following part are API Registration of Iterators
-//--------------------------------------------------------------
-/*!
- * \brief Macro to register image augmenter
- *
- * \code
- * // example of registering a mnist iterator
- * REGISTER_IMAGE_AUGMENTER(aug_default)
- * .describe('default augmenter')
- * .set_body([]() {
- *     return new DefaultAugmenter();
- *   });
- * \endcode
- */
-#define MXNET_REGISTER_IMAGE_AUGMENTER(name)                            \
-  DMLC_REGISTRY_REGISTER(::mxnet::io::ImageAugmenterReg, ImageAugmenterReg, name)
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_USE_OPENCV
-    
-    /*!
- *  Copyright (c) 2015 by Contributors
- * \file iter_prefetcher.h
- * \brief define a prefetcher using threaditer to keep k batch fetched
- */
-#ifndef MXNET_IO_ITER_PREFETCHER_H_
-#define MXNET_IO_ITER_PREFETCHER_H_
-    
-    #ifndef MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
-#define MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
-#include <dmlc/parameter.h>
-#include <string>
-#include <utility>
-#include <vector>
-#include 'mxnet/ndarray.h'
-    
-    
-    {  if (has('dist')) {
-#if MXNET_USE_DIST_KVSTORE
-    kv = new kvstore::KVStoreDist(use_device_comm);
-    if (!has('_async') && kv->IsWorkerNode() && kv->get_rank() == 0) {
-      // configure the server to be the sync mode
-      kv->SendCommandToServers(static_cast<int>(kvstore::CommandType::kSyncMode), '');
-    }
-#else
-    LOG(FATAL) << 'compile with USE_DIST_KVSTORE=1 to use ' << tname;
-    return nullptr;
-#endif  // MXNET_USE_DIST_KVSTORE
-  } else {
-    if (has('nccl')) {
-#if MXNET_USE_NCCL
-      kv = new kvstore::KVStoreNCCL();
-#else
-      LOG(FATAL) << 'compile with USE_NCCL=1 to use ' << tname;
-      return nullptr;
-#endif
-    } else {
-      kv =  new kvstore::KVStoreLocal(use_device_comm);
-    }
-  }
-  kv->type_ = tname;
-  return kv;
-}
-    
-    namespace mxnet {
-namespace op {
-    }
-    }
-    
-    
-    {
-    {.add_argument('data', 'Symbol or Symbol[]', 'Tensor or List of Tensors, the second input '
-'will be used as crop_like shape reference')
-.add_arguments(CropParam::__FIELDS__())
-.set_key_var_num_args('num_args');
-}  // namespace op
-}  // namespace mxnet
+        #undef VERB
+#undef DIRECTIONAL_PREPOSITION
+#undef PREPOSITION
 
     
-      virtual void Backward(const OpContext &ctx,
-                        const std::vector<TBlob> &out_grad,
-                        const std::vector<TBlob> &in_data,
-                        const std::vector<TBlob> &out_data,
-                        const std::vector<OpReqType> &req,
-                        const std::vector<TBlob> &in_grad,
-                        const std::vector<TBlob> &aux_args) {
-    using namespace mshadow;
-    using namespace mshadow::expr;
-    CHECK_EQ(out_grad.size(), 1U);
-    CHECK_EQ(in_data.size(), 1U);
-    CHECK_EQ(out_data.size(), 2U);
-    CHECK_EQ(req.size(), 1U);
-    CHECK_EQ(in_grad.size(), 1U);
-    typename DataType<DType>::ScaleType alpha = 1.0f;
-    typename DataType<DType>::ScaleType beta = 0.0f;
-    Stream<gpu> *s = ctx.get_stream<gpu>();
-    Tensor<gpu, 4, DType> grad = out_grad[lrn_enum::kOut].get<gpu, 4, DType>(s);
-    Tensor<gpu, 4, DType> data = in_data[lrn_enum::kData].get<gpu, 4, DType>(s);
-    Tensor<gpu, 4, DType> output_data = out_data[lrn_enum::kOut].get<gpu, 4, DType>(s);
-    Tensor<gpu, 4, DType> input_grad = in_grad[lrn_enum::kData].get<gpu, 4, DType>(s);
-    CHECK_EQ(s->dnn_handle_ownership_, mshadow::Stream<gpu>::OwnHandle);
-    CUDNN_CALL(cudnnLRNCrossChannelBackward(s->dnn_handle_,
-                                            lrn_desc_,
-                                            CUDNN_LRN_CROSS_CHANNEL_DIM1,
-                                            &alpha,
-                                            shape_desc_,
-                                            output_data.dptr_,
-                                            shape_desc_,
-                                            grad.dptr_,
-                                            shape_desc_,
-                                            data.dptr_,
-                                            &beta,
-                                            shape_desc_,
-                                            input_grad.dptr_));
+          if (node->Further) {
+        // Further indent, and include the line to the right child if
+        // there is one.
+        IndentScope is(this, node->Right ? '|   ' : '    ');
+        print(node->Further, ChildKind::Further);
+      }
+    
+      static CFPointeeInfo forConstVoid() {
+    CFPointeeInfo info;
+    info.IsValid = true;
+    info.IsConst = true;
+    info.Decl = nullptr;
+    return info;
   }
     
-      for (auto& blob : in_data) {
-    ptrs.push_back(reinterpret_cast<void*>(new NDArray(blob, ndctx.dev_id)));
-    tags.push_back(0);
-  }
-  for (auto& blob : out_data) {
-    NDArray* nd = new NDArray(blob, ndctx.dev_id);
-    ptrs.push_back(reinterpret_cast<void*>(nd));
-    ndvar.push_back(nd->var());
-    tags.push_back(1);
-  }
-  std::sort(ndvar.begin(), ndvar.end());
-  ndvar.resize(std::unique(ndvar.begin(), ndvar.end()) - ndvar.begin());
+     public:
+  // Constructs an empty Message.
+  Message();
     
-    // This is an example interface of external-compaction algorithm.
-// Compaction algorithm can be implemented outside the core-RocksDB
-// code by using the pluggable compaction APIs that RocksDb provides.
-class Compactor : public EventListener {
- public:
-  // Picks and returns a compaction task given the specified DB
-  // and column family.  It is the caller's responsibility to
-  // destroy the returned CompactionTask.  Returns 'nullptr'
-  // if it cannot find a proper compaction task.
-  virtual CompactionTask* PickCompaction(
-      DB* db, const std::string& cf_name) = 0;
+    #if GTEST_HAS_PARAM_TEST
+// The pure interface class that all value-parameterized tests inherit from.
+// A value-parameterized class must inherit from both ::testing::Test and
+// ::testing::WithParamInterface. In most cases that just means inheriting
+// from ::testing::TestWithParam, but more complicated test hierarchies
+// may need to inherit from Test and WithParamInterface at different levels.
+//
+// This interface has support for accessing the test parameter value via
+// the GetParam() method.
+//
+// Use it with one of the parameter generator defining functions, like Range(),
+// Values(), ValuesIn(), Bool(), and Combine().
+//
+// class FooTest : public ::testing::TestWithParam<int> {
+//  protected:
+//   FooTest() {
+//     // Can use GetParam() here.
+//   }
+//   virtual ~FooTest() {
+//     // Can use GetParam() here.
+//   }
+//   virtual void SetUp() {
+//     // Can use GetParam() here.
+//   }
+//   virtual void TearDown {
+//     // Can use GetParam() here.
+//   }
+// };
+// TEST_P(FooTest, DoesBar) {
+//   // Can use GetParam() method here.
+//   Foo foo;
+//   ASSERT_TRUE(foo.DoesBar(GetParam()));
+// }
+// INSTANTIATE_TEST_CASE_P(OneToTenRange, FooTest, ::testing::Range(1, 10));
+    
+    // FilePath - a class for file and directory pathname manipulation which
+// handles platform-specific conventions (like the pathname separator).
+// Used for helper functions for naming files in a directory for xml output.
+// Except for Set methods, all methods are const or static, which provides an
+// 'immutable value object' -- useful for peace of mind.
+// A FilePath with a value ending in a path separator ('like/this/') represents
+// a directory, otherwise it is assumed to represent a file. In either case,
+// it may or may not represent an actual file or directory in the file system.
+// Names are NOT checked for syntax correctness -- no checking for illegal
+// characters, malformed paths, etc.
+    
+        // First, registers the first type-parameterized test in the type
+    // list.
+    MakeAndRegisterTestInfo(
+        (std::string(prefix) + (prefix[0] == '\0' ? '' : '/') + case_name + '/'
+         + StreamableToString(index)).c_str(),
+        GetPrefixUntilComma(test_names).c_str(),
+        GetTypeName<Type>().c_str(),
+        NULL,  // No value parameter.
+        GetTypeId<FixtureClass>(),
+        TestClass::SetUpTestCase,
+        TestClass::TearDownTestCase,
+        new TestFactoryImpl<TestClass>);
+    
+    // Client
+MeasureDouble RpcClientSentBytesPerRpc() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcClientSentBytesPerRpcMeasureName,
+      'Total bytes sent across all request messages per RPC', kUnitBytes);
+  return measure;
+}
+    
+    ::opencensus::stats::MeasureInt64 RpcServerSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcServerSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcServerReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcServerReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcServerServerLatency();
+::opencensus::stats::MeasureInt64 RpcServerCompletedRpcs();
+    
+    constexpr size_t RpcServerStatsEncoding::kRpcServerStatsSize;
+constexpr size_t RpcServerStatsEncoding::kEncodeDecodeFailure;
+constexpr size_t RpcServerStatsEncoding::kVersionIdSize;
+constexpr size_t RpcServerStatsEncoding::kFieldIdSize;
+constexpr size_t RpcServerStatsEncoding::kVersionIdOffset;
+constexpr size_t RpcServerStatsEncoding::kVersionId;
+    
+      protobuf::FileDescriptorProto file_desc_proto;
+  grpc::string data;
+  file_desc->CopyTo(&file_desc_proto);
+  file_desc_proto.SerializeToString(&data);
+  response->mutable_file_descriptor_response()->add_file_descriptor_proto(data);
+    
+      Status GetFileContainingExtension(
+      ServerContext* context,
+      const reflection::v1alpha::ExtensionRequest* request,
+      reflection::v1alpha::ServerReflectionResponse* response);
+    
+    
+    {}  // namespace grpc
+    
+        pq = swHeap_new(SIZE, SW_MAX_HEAP);
+    ASSERT_NE(pq, nullptr);
+    
+    
+    {    friend
+    void getCallback(redisAsyncContext *, void *, void *);
+};
+    
+    TEST(lru_cache, basic)
+{
+    shared_ptr<string> val = make_shared<string>('hello');
+    shared_ptr<string> val1 = make_shared<string>('hello1');
     }
     
-      // Start a transaction
-  Transaction* txn = txn_db->BeginTransaction(write_options);
-  assert(txn);
+        SwooleG.main_reactor = (swReactor *) sw_malloc(sizeof(swReactor));
+    swReactor_create(SwooleG.main_reactor, SW_REACTOR_MAXEVENTS);
     
-      delete db;
+    static void thread_read(int i)
+{
+    pkg recv_pkg;
+    uint32_t tmp;
+    int ret;
+    uint32_t recv_count = 0;
+    int j = 0;
+    swPipe *sock = &threads[i].pipe;
+    int task_n = WRITE_N / READ_THREAD_N;
+    }
     
-    #include <stdint.h>
-#include <memory>
-#include <string>
+    TEST(os_wait, waitpid_after_child_exit)
+{
+    coro_test([](void *arg)
+    {
+        swoole_coroutine_signal_init();
+    }
+    }
     
-      // Builds an openable snapshot of RocksDB on the same disk, which
-  // accepts an output directory on the same disk, and under the directory
-  // (1) hard-linked SST files pointing to existing live SST files
-  // SST files will be copied if output directory is on a different filesystem
-  // (2) a copied manifest files and other files
-  // The directory should not already exist and will be created by this API.
-  // The directory will be an absolute path
-  // log_size_for_flush: if the total log file size is equal or larger than
-  // this value, then a flush is triggered for all the column families. The
-  // default value is 0, which means flush is always triggered. If you move
-  // away from the default, the checkpoint may not contain up-to-date data
-  // if WAL writing is not always enabled.
-  // Flush will always trigger if it is 2PC.
-  virtual Status CreateCheckpoint(const std::string& checkpoint_dir,
-                                  uint64_t log_size_for_flush = 0);
+        sigemptyset(&curset);
+    sigprocmask(SIG_BLOCK, NULL, &curset);
+    ret = sigismember(&curset,SIGUSR1);
+    ASSERT_EQ(ret, 1);
+    
+        g_receive_count++;
+    
+    void swoole_throw_error(int code)
+{
+    throw swoole::Exception(code);
+}
