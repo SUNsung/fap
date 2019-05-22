e00c3be2b29b68c82d4b253dbb895d5ae880db41
SUNsung <weatherius@gmail.com>
2019-05-22 03:09:21 +0300

    Rewrite [21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb] File in: 2019-05-22 03:09:21

--- ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
+++ ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
@@ -1,92 +1,137 @@
 
         
-              return class_ref
+        # == Schema Information
+#
+# Table name: directory_items
+#
+#  id             :integer          not null, primary key
+#  period_type    :integer          not null
+#  user_id        :integer          not null
+#  likes_received :integer          not null
+#  likes_given    :integer          not null
+#  topics_entered :integer          not null
+#  topic_count    :integer          not null
+#  post_count     :integer          not null
+#  created_at     :datetime
+#  updated_at     :datetime
+#  days_visited   :integer          default(0), not null
+#  posts_read     :integer          default(0), not null
+#
+# Indexes
+#
+#  index_directory_items_on_days_visited             (days_visited)
+#  index_directory_items_on_likes_given              (likes_given)
+#  index_directory_items_on_likes_received           (likes_received)
+#  index_directory_items_on_period_type_and_user_id  (period_type,user_id) UNIQUE
+#  index_directory_items_on_post_count               (post_count)
+#  index_directory_items_on_posts_read               (posts_read)
+#  index_directory_items_on_topic_count              (topic_count)
+#  index_directory_items_on_topics_entered           (topics_entered)
+#
+
+    
+      def self.version(lang_string)
+    (@lang_string_cache ||= {})[lang_string] ||=
+      Digest::SHA1.hexdigest(bundle lang_string.split('|'))
+  end
+    
+        private
+    
+        # doing this as early as possible so we have an
+    # accurate counter
+    if queue_start = env['HTTP_X_REQUEST_START']
+      queue_start = queue_start.split('t=')[1].to_f
+      queue_time = (Time.now.to_f - queue_start)
+      env['REQUEST_QUEUE_SECONDS'] = queue_time
     end
     
-          launch_event = builder.new_event(:launch)
-      post_thread = client.post_event(launch_event)
-      unless post_thread.nil?
-        @threads << post_thread
-      end
+          # Yields the supplied block, responding to any rate limit errors.
+      #
+      # The exact strategy used for handling rate limiting errors depends on
+      # whether we are running in parallel mode or not. For more information see
+      # `#rate_or_wait_for_rate_limit`.
+      def with_rate_limit
+        return yield unless rate_limiting_enabled?
+    
+            def sidekiq_worker_class
+          ImportIssueWorker
+        end
+    
+                rows << {
+              label_id: label_id,
+              target_id: target_id,
+              target_type: issue.issuable_type,
+              created_at: time,
+              updated_at: time
+            }
+          end
+    
+            def collection_method
+          :lfs_objects
+        end
+    
+          private
+    
+          # Returns true if the given object has already been imported, false
+      # otherwise.
+      #
+      # object - The object to check.
+      def already_imported?(object)
+        id = id_for_already_imported_cache(object)
+    
+            expose_attribute :id, :login
+    
+        def initialize
+      @pages = {}
     end
     
-          def self.authors
-        ['lmirosevic', 'maschall']
+    require 'active_support/subscriber'
+    
+            css('h1 + code').each do |node|
+          node.before('<p></p>')
+          while node.next_element.name == 'code'
+            node.previous_element << ' '
+            node.previous_element << node.next_element
+          end
+          node.previous_element.prepend_child(node)
+        end
+    
+      # staged_path not available in Installer/Uninstall Stanza, workaround by nesting with preflight/postflight
+  # see https://github.com/Homebrew/homebrew-cask/pull/8887
+  # and https://github.com/Homebrew/homebrew-cask-versions/pull/296
+    
+          def page_dir
+        @page_dir
       end
     
-            message = '#{grouping}/test/#{specified_build_number} (fastlane)'
-        tag = '#{grouping}/test/#{specified_build_number}'
-        expect(result).to eq('git tag -am #{message.shellescape} #{tag.shellescape}')
-      end
+      test 'extract destination file name in case of path renaming' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
+    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
+  end
     
-            expect(result).to eq('appledoc --project-name \'Project Name\' --project-company \'Company\' --docset-install-path \'docs/install/path\' --exit-threshold \'2\' input/dir')
-      end
-    
-        # [String] A description shown to the user
-    attr_accessor :description
-    
-            FastlaneCore::CertChecker.wwdr_certificate_installed?
-      end
-    
-        def resource_params
-      params.require(:user).permit(
-        :unconfirmed_email
-      )
-    end
+        # Title is based on file name when h1_title is false.
+    actual = @view.title
+    assert_equal 'H1', title
   end
 end
 
     
-        self.state = :registered
-    
-              # Encodes the options field
-          #
-          # @return [OpenSSL::ASN1::BitString]
-          def encode_options
-            OpenSSL::ASN1::BitString.new([options].pack('N'))
-          end
-    
-              # Decodes the cname field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::PrincipalName]
-          def decode_cname(input)
-            Rex::Proto::Kerberos::Model::PrincipalName.decode(input.value[0])
-          end
-    
-    Gem::Specification.new do |gem|
-  gem.name          = 'capistrano'
-  gem.version       = Capistrano::VERSION
-  gem.authors       = ['Tom Clements', 'Lee Hambley']
-  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
-  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
-  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
-  gem.homepage      = 'http://capistranorb.com/'
-    
-    Then(/^the release is created$/) do
-  run_vagrant_command('ls -g #{TestApp.releases_path}')
+    Given /^I update my user view to include the attachment$/ do
+  steps %{
+    Given I overwrite 'app/views/users/show.html.erb' with:
+      '''
+      <p>Name: <%= @user.name %></p>
+      <p>Attachment: <%= image_tag @user.attachment.url %></p>
+      '''
+  }
 end
     
-          def print_deprecation_warnings_if_applicable
-        if using_default_scm?
-          warn_add_git_to_capfile unless scm_plugin_installed?
-        elsif built_in_scm_name?
-          warn_set_scm_is_deprecated
-        elsif third_party_scm_name?
-          warn_third_party_scm_must_be_upgraded
-        end
-      end
+    Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
     
-    module Capistrano
-  class Configuration
-    class Servers
-      include Enumerable
-    
-          # Internal use only.
-      def peek(key, default=nil, &block)
-        value = fetch_for(key, default, &block)
-        while callable_without_parameters?(value)
-          value = (values[key] = value.call)
-        end
-        value
-      end
+        # The aspect ratio of the dimensions.
+    def aspect
+      width / height
+    end
