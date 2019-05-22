d8127b86e3209af781c7f8a18be7428f8059c5e7
SUNsung <weatherius@gmail.com>
2019-05-22 14:01:54 +0300

    Rewrite [46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb] File in: 2019-05-22 14:01:53

--- ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
+++ ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
@@ -1,180 +1,76 @@
 
         
-        def local_require
-  require 'json'
-  JSON.pretty_generate(DATA)
+        def remove_head_from_history(markdown)
+  index = markdown =~ %r!^##\s+\d+\.\d+\.\d+!
+  markdown[index..-1]
 end
     
-    # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
+    require 'benchmark/ips'
+require 'jekyll'
+require 'json'
     
-    Jekyll::PluginManager.require_from_bundler
-    
-        def deprecation_message(message)
-      Jekyll.logger.warn 'Deprecation:', message
-    end
-    
-          def parse_expression(str)
-        Liquid::Variable.new(str, Liquid::ParseContext.new)
+      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
+    cmd = g.split('-').last
+    p.command(cmd.to_sym) do |c|
+      c.syntax cmd
+      c.action do
+        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
+          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
       end
+    end
+  end
     
-            def importer_class
-          IssueAndLabelLinksImporter
-        end
-    
-                rows << {
-              label_id: label_id,
-              target_id: target_id,
-              target_type: issue.issuable_type,
-              created_at: time,
-              updated_at: time
-            }
+            # rubocop:disable Metrics/AbcSize
+        def process(args, opts)
+          if !args || args.empty?
+            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
           end
     
-          # Associates the given database ID with the current object.
-      #
-      # database_id - The ID of the corresponding database row.
-      def cache_database_id(database_id)
-        Caching.write(cache_key, database_id)
+          it 'should be invalid when the referenced url doesn't contain a scenario' do
+        stub_request(:get, 'http://example.com/scenarios/1/export.json').to_return(:status => 200, :body => invalid_data)
+        subject.url = 'http://example.com/scenarios/1/export.json'
+        expect(subject).not_to be_valid
+        expect(subject.errors[:base]).to include('The provided data does not appear to be a valid Scenario.')
       end
     
-          def initialize(app)
-        @app = app
-      end
+      describe '#working?' do
+    it 'it is working when at least one event was emitted' do
+      expect(@checker).not_to be_working
+      @checker.memory[:last_event] = '2014-04-17T10:25:31.000+02:00'
+      @checker.check
+      expect(@checker.reload).to be_working
+    end
+  end
+end
+
     
-        self.code    = code.to_i
-    self.message = message
-    self.proto   = proto
+    Given /^I add this snippet to config\/application.rb:$/ do |snippet|
+  file_name = 'config/application.rb'
+  cd('.') do
+    content = File.read(file_name)
+    File.open(file_name, 'w') {|f| f << content.sub(/class Application < Rails::Application.*$/, 'class Application < Rails::Application\n#{snippet}\n')}
+  end
+end
     
-        # Convert u-law into signed PCM
-    when IAX_CODEC_G711_MULAW
-      Rex::Proto::IAX2::Codecs::MuLaw.decode(buff)
+      def self.source_root
+    @source_root ||= File.expand_path('../templates', __FILE__)
+  end
     
-              # Encodes the realm field
-          #
-          # @return [String]
-          def encode_realm
-            encoded = ''
-            encoded << [realm.length].pack('N')
-            encoded << realm
-    
-              # Decodes the auth_time field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_auth_time(input)
-            input.value[0].value
-          end
-    
-              # Encodes the etype
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_etype
-            bn = OpenSSL::BN.new(etype.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-              # Decodes the Rex::Proto::Kerberos::Model::KdcResponse from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode KdcResponse, invalid input'
-            end
-    
-              # Decodes the Rex::Proto::Kerberos::Model::KrbError from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode KrbError, invalid input'
-            end
-    
-          # Returns an array of all the keys in the `hash` literal.
-      #
-      # @return [Array<Node>] an array of keys in the `hash` literal
-      def keys
-        each_key.to_a
-      end
-    
-    module RuboCop
-  module AST
-    # A node extension for `kwsplat` nodes. This will be used in place of a
-    # plain  node when the builder constructs the AST, making its methods
-    # available to all `kwsplat` nodes within RuboCop.
-    class KeywordSplatNode < Node
-      include HashElementNode
-    
-            cop_names.each do |cop_name|
-          yield qualified_cop_name(cop_name), disabled, comment_line_number,
-                single_line
-        end
-      end
+        def initialize
+      clear
     end
     
-      option ['-t', '--output-type'], 'OUTPUT_TYPE',
-    'the type of package you want to create (deb, rpm, solaris, etc)',
-    :attribute_name => :output_type
-  option ['-s', '--input-type'], 'INPUT_TYPE',
-    'the package type to use as input (gem, rpm, python, etc)',
-    :attribute_name => :input_type
-  option ['-C', '--chdir'], 'CHDIR',
-    'Change directory to here before searching for files',
-    :attribute_name => :chdir
-  option '--prefix', 'PREFIX',
-    'A path to prefix files with when building the target package. This may ' \
-    'not be necessary for all input packages. For example, the 'gem' type ' \
-    'will prefix with your gem directory automatically.'
-  option ['-p', '--package'], 'OUTPUT', 'The package file path to output.'
-  option ['-f', '--force'], :flag, 'Force output even if it will overwrite an ' \
-    'existing file', :default => false
-  option ['-n', '--name'], 'NAME', 'The name to give to the package'
-    
-        # Query details about our now-installed package.
-    # We do this by using 'npm ls' with json + long enabled to query details
-    # about the installed package.
-    npm_ls_out = safesystemout(attributes[:npm_bin], 'ls', '--json', '--long', *npm_flags)
-    npm_ls = JSON.parse(npm_ls_out)
-    name, info = npm_ls['dependencies'].first
-    
-      # Take a flat package as input
-  def input(input_path)
-    # TODO: Fail if it's a Distribution pkg or old-fashioned
-    expand_dir = File.join(build_path, 'expand')
-    # expand_dir must not already exist for pkgutil --expand
-    safesystem('pkgutil --expand #{input_path} #{expand_dir}')
-    
-        # do channel-discover if required
-    if !attributes[:pear_channel].nil?
-      logger.info('Custom channel specified', :channel => attributes[:pear_channel])
-      channel_list = safesystemout('pear', '-c', config, 'list-channels')
-      if channel_list !~ /#{Regexp.quote(attributes[:pear_channel])}/
-        logger.info('Discovering new channel', :channel => attributes[:pear_channel])
-        safesystem('pear', '-c', config, 'channel-discover', attributes[:pear_channel])
-      end
-      input_package = '#{attributes[:pear_channel]}/#{input_package}'
-      logger.info('Prefixing package name with channel', :package => input_package)
+        # Returns a String describing the file's content type
+    def detect
+      if blank_name?
+        SENSIBLE_DEFAULT
+      elsif empty_file?
+        EMPTY_TYPE
+      elsif calculated_type_matches.any?
+        calculated_type_matches.first
+      else
+        type_from_file_contents || SENSIBLE_DEFAULT
+      end.to_s
     end
     
-          base = staging_path(File.join(attributes[:prefix], '#{platform.platform}/#{platform.target_version || 'default'}'))
-      target = File.join(base, 'files')
-      actions_script = File.join(base, 'install_actions.sh')
-      ::PleaseRun::Installer.install_files(platform, target, false)
-      ::PleaseRun::Installer.write_actions(platform, actions_script)
-    end
-    
-        args.flatten!.compact!
+        private
