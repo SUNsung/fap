76ff30d7e2c8bccfe3900dab1430f9b2d52036c4
SUNsung <weatherius@gmail.com>
2019-05-24 01:33:45 +0300

    Rewrite [08TqtTl.4I0WI08WatVnzbKKFg08.peDLVj438Q08ECz.py] File in: 2019-05-24 01:33:44

--- pyton/08TqtTl.4I0WI08WatVnzbKKFg08.peDLVj438Q08ECz.py
+++ pyton/08TqtTl.4I0WI08WatVnzbKKFg08.peDLVj438Q08ECz.py
@@ -1,128 +1,211 @@
 
         
-        
-class RFC3339FieldTest(unittest.TestCase):
-    '''Tests for acme.fields.RFC3339Field.'''
+            def pop(self, exc=_sentinel):
+        '''Pops the app context.'''
+        try:
+            self._refcnt -= 1
+            if self._refcnt <= 0:
+                if exc is _sentinel:
+                    exc = sys.exc_info()[1]
+                self.app.do_teardown_appcontext(exc)
+        finally:
+            rv = _app_ctx_stack.pop()
+        assert rv is self, 'Popped wrong app context.  (%r instead of %r)' % (rv, self)
+        appcontext_popped.send(self.app)
     
-        @classmethod
-    # pylint: disable=arguments-differ,too-many-arguments
-    def sign(cls, payload, key, alg, nonce, url=None, kid=None):
-        # Per ACME spec, jwk and kid are mutually exclusive, so only include a
-        # jwk field if kid is not provided.
-        include_jwk = kid is None
-        return super(JWS, cls).sign(payload, key=key, alg=alg,
-                                    protect=frozenset(['nonce', 'url', 'kid', 'jwk', 'alg']),
-                                    nonce=nonce, url=url, kid=kid,
-                                    include_jwk=include_jwk)
+        with pytest.raises(NoAppException):
+        locate_app(info, iname, aname)
+    
+        #: The canonical way to decorate class-based views is to decorate the
+    #: return value of as_view().  However since this moves parts of the
+    #: logic from the class declaration to the place where it's hooked
+    #: into the routing system.
+    #:
+    #: You can place one or more decorators in this list and whenever the
+    #: view function is created the result is automatically decorated.
+    #:
+    #: .. versionadded:: 0.8
+    decorators = ()
+    
+    # utilities we import from Werkzeug and Jinja2 that are unused
+# in the module but are exported as public interface.
+from werkzeug.exceptions import abort
+from werkzeug.utils import redirect
+from jinja2 import Markup, escape
+    
+    
+@functools.lru_cache()
+def get_citext_oids(connection_alias):
+    '''Return citext array OIDs.'''
+    with connections[connection_alias].cursor() as cursor:
+        cursor.execute('SELECT typarray FROM pg_type WHERE typname = 'citext'')
+        return tuple(row[0] for row in cursor)
+    
+        @property
+    def cache_key(self):
+        return self.cache_key_prefix + self._get_or_create_session_key()
+    
+    from django.contrib.sites.shortcuts import get_current_site
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from django.http import Http404
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.utils.http import http_date
+    
+        @cached_property
+    def initial(self):
+        data = self.form.get_initial_for_field(self.field, self.name)
+        # If this is an auto-generated default date, nix the microseconds for
+        # standardized handling. See #22502.
+        if (isinstance(data, (datetime.datetime, datetime.time)) and
+                not self.field.widget.supports_microseconds):
+            data = data.replace(microsecond=0)
+        return data
+    
+        If the constructor, ``.update``, or equality comparison
+    operations are given keys that have equal ``.lower()``s, the
+    behavior is undefined.
+    '''
+    
+    
+@pytest.fixture
+def httpbin(httpbin):
+    return prepare_url(httpbin)
+    
+    #: Python 2.x?
+is_py2 = (_ver[0] == 2)
+    
+    
+def dispatch_hook(key, hooks, hook_data, **kwargs):
+    '''Dispatches a hook dictionary on a given piece of data.'''
+    hooks = hooks or {}
+    hooks = hooks.get(key)
+    if hooks:
+        if hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        for hook in hooks:
+            _hook_data = hook(hook_data, **kwargs)
+            if _hook_data is not None:
+                hook_data = _hook_data
+    return hook_data
 
     
-    # The master toctree document.
-master_doc = 'index'
     
-    Loosely based on https://github.com/astropy/astropy/pull/347
-'''
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
+    '''
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
     
-            if self.flash_briefings.get(briefing_id) is None:
-            err = 'No configured Alexa flash briefing was found for: %s'
-            _LOGGER.error(err, briefing_id)
-            return b'', 404
+            # Test for whitespace
+        with pytest.raises(InvalidHeader):
+            r = requests.get(httpbin('get'), headers=headers_space)
+        # Test for tab
+        with pytest.raises(InvalidHeader):
+            r = requests.get(httpbin('get'), headers=headers_tab)
     
-    from homeassistant.components.device_tracker import (
-    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
-from homeassistant.const import CONF_HOST
-import homeassistant.helpers.config_validation as cv
+                tested_methods = conman.tested_methods_from_spidercls(spidercls)
+            if opts.list:
+                for method in tested_methods:
+                    contract_reqs[spidercls.name].append(method)
+            elif tested_methods:
+                self.crawler_process.crawl(spidercls)
     
-        def _update_info(self):
-        '''Ensure the information from the router are up to date.
+            self.crawler_process.crawl(spname, **opts.spargs)
+        self.crawler_process.start()
     
-            send_data(name, json_body)
+        def long_desc(self):
+        return 'Fetch a URL using the Scrapy downloader and print its content ' \
+            'to stdout. You may want to use --nolog to disable logging'
     
-            if payload_dict:
-            payload = '{%s}' % ','.join('{}:{}'.format(key, val)
-                                        for key, val in
-                                        payload_dict.items())
+            # If no credentials could be found anywhere,
+        # consider this an anonymous connection request by default;
+        # unless 'anon' was set explicitly (True/False).
+        anon = kw.get('anon')
+        if anon is None and not aws_access_key_id and not aws_secret_access_key:
+            kw['anon'] = True
+        self.anon = kw.get('anon')
     
-            def process(self, event):
-            '''On Watcher event, fire HA event.'''
-            _LOGGER.debug('process(%s)', event)
-            if not event.is_directory:
-                folder, file_name = os.path.split(event.src_path)
-                self.hass.bus.fire(
-                    DOMAIN, {
-                        'event_type': event.event_type,
-                        'path': event.src_path,
-                        'file': file_name,
-                        'folder': folder,
-                        })
     
-        def __call__(self, method):
-        '''Decorate a function.'''
-        @wraps(method)
-        async def wrapper(view, request, *args, **kwargs):
-            '''Wrap a request handler with data validation.'''
-            data = None
+class AjaxCrawlMiddleware(object):
+    '''
+    Handle 'AJAX crawlable' pages marked as crawlable via meta tag.
+    For more info see https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
+    '''
+    
+    if args.quiet:
+    jieba.setLogLevel(60)
+if args.pos:
+    import jieba.posseg
+    posdelim = args.pos
+    def cutfunc(sentence, _, HMM=True):
+        for w, f in jieba.posseg.cut(sentence, HMM):
+            yield w + posdelim + f
+else:
+    cutfunc = jieba.cut
+    
+    
+def __cut(sentence):
+    global emit_P
+    prob, pos_list = viterbi(sentence, 'BMES', start_P, trans_P, emit_P)
+    begin, nexti = 0, 0
+    # print pos_list, sentence
+    for i, char in enumerate(sentence):
+        pos = pos_list[i]
+        if pos == 'B':
+            begin = i
+        elif pos == 'E':
+            yield sentence[begin:i + 1]
+            nexti = i + 1
+        elif pos == 'S':
+            yield char
+            nexti = i + 1
+    if nexti < len(sentence):
+        yield sentence[nexti:]
+    
+        def load_word_tag(self, f):
+        self.word_tag_tab = {}
+        f_name = resolve_filename(f)
+        for lineno, line in enumerate(f, 1):
             try:
-                data = await request.json()
-            except ValueError:
-                if not self._allow_empty or \
-                   (await request.content.read()) != b'':
-                    _LOGGER.error('Invalid JSON received.')
-                    return view.json_message('Invalid JSON.', 400)
-                data = {}
+                line = line.strip().decode('utf-8')
+                if not line:
+                    continue
+                word, _, tag = line.split(' ')
+                self.word_tag_tab[word] = tag
+            except Exception:
+                raise ValueError(
+                    'invalid POS dictionary entry in %s at Line %s: %s' % (f_name, lineno, line))
+        f.close()
     
-        for i in range(1, n+1):
-        dp[i][0] = True
-    
-    def revise_centroids(data, k, cluster_assignment):
-    new_centroids = []
-    for i in range(k):
-        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
-        member_data_points = data[cluster_assignment==i]
-        # Compute the mean of the data points. Fill in the blank (RHS only)
-        centroid = member_data_points.mean(axis=0)
-        new_centroids.append(centroid)
-    new_centroids = np.array(new_centroids)
-    
-    return new_centroids
-    
-    word_bysig = collections.defaultdict(list)
-for word in word_list:
-    word_bysig[signature(word)].append(word)
+    parser = OptionParser(USAGE)
+parser.add_option('-k', dest='topK')
+parser.add_option('-w', dest='withWeight')
+opt, args = parser.parse_args()
     
     
-def b_expo(a, b):
-    res = 0
-    while b > 0:
-        if b&1:
-            res += a
+
     
-        def test_am_station_overflow_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1250'
-        self.assertEqual(station, expected_station)
+        mat = np.empty(quat.shape[:-1] + (3, 3), dtype=np.float64)
+    mat[..., 0, 0] = 1.0 - (yY + zZ)
+    mat[..., 0, 1] = xY - wZ
+    mat[..., 0, 2] = xZ + wY
+    mat[..., 1, 0] = xY + wZ
+    mat[..., 1, 1] = 1.0 - (xX + zZ)
+    mat[..., 1, 2] = yZ - wX
+    mat[..., 2, 0] = xZ - wY
+    mat[..., 2, 1] = yZ + wX
+    mat[..., 2, 2] = 1.0 - (xX + yY)
+    return np.where((Nq > _FLOAT_EPS)[..., np.newaxis, np.newaxis], mat, np.eye(3))
     
-    *TL;DR80
-Implements state as a derived class of the state pattern interface.
-Implements state transitions by invoking methods from the pattern's superclass.
-'''
+        def __init__(self, atexit_register=True):
+        self.lock = threading.Lock()
+        self.next_id = -1
+        self.closeables = weakref.WeakValueDictionary()
     
-        def build_floor(self):
-        raise NotImplementedError
+            filename = '{}.stats.json'.format(self.file_prefix)
+        self.path = os.path.join(self.directory, filename)
     
-        def register_object(self, name, obj):
-        '''Register an object'''
-        self._objects[name] = obj
-    
-    
-class MidnightTimeProvider(object):
-    '''
-    Class implemented as hard-coded stub (in contrast to configurable stub).
-    '''
-    
-    class TimeDisplay(object):
-    
-        def __init__(self, obj, **adapted_methods):
-        '''We set the adapted methods in the object's dict'''
-        self.obj = obj
-        self.__dict__.update(adapted_methods)
+    class UnrecordableEnv(object):
+    metadata = {'render.modes': [None]}
