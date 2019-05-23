66d502dc668d7e7c1bdbbaa4e6eb13df77ec45d2
SUNsung <weatherius@gmail.com>
2019-05-23 13:02:45 +0300

    Rewrite [37yn634lSJQo37y3yz4zVxROk37IVGs1l7cbGg37kGp.rb] File in: 2019-05-23 13:02:44

--- ruby/37yn634lSJQo37y3yz4zVxROk37IVGs1l7cbGg37kGp.rb
+++ ruby/37yn634lSJQo37y3yz4zVxROk37IVGs1l7cbGg37kGp.rb
@@ -1,150 +1,58 @@
 
         
-        module ActionView
-  module Helpers
-    module Tags # :nodoc:
-      class CollectionRadioButtons < Base # :nodoc:
-        include CollectionHelpers
-    
-                options = options.dup
-            options[:field_name]           = @method_name
-            options[:include_position]     = true
-            options[:prefix]             ||= @object_name
-            options[:index]                = @auto_index if @auto_index && !options.has_key?(:index)
-    
-              builder = LabelBuilder.new(@template_object, @object_name, @method_name, @object, tag_value)
-    
-          # Reads an integer from the cache, or returns nil if no value was found.
-      #
-      # See Caching.read for more information.
-      def self.read_integer(raw_key, timeout: TIMEOUT)
-        value = read(raw_key, timeout: timeout)
-    
-            def collection_method
-          :pull_requests_comments
+                def render(&block)
+          render_collection_for(RadioButtonBuilder, &block)
         end
     
-          # Any options to be passed to the method used for retrieving the data to
-      # import.
-      def collection_options
-        {}
+      def build(theme_ids)
+    builder = Builder.new
+    
+        # Update denormalzied posts_read_count
+    DB.exec(<<~SQL, seen_at: last_seen)
+      UPDATE user_stats SET posts_read_count = X.c
+      FROM
+      (SELECT pt.user_id,
+              COUNT(*) AS c
+       FROM users AS u
+       JOIN post_timings AS pt ON pt.user_id = u.id
+       JOIN topics t ON t.id = pt.topic_id
+       WHERE u.last_seen_at > :seen_at AND
+             t.archetype = 'regular' AND
+             t.deleted_at IS NULL
+       GROUP BY pt.user_id) AS X
+       WHERE X.user_id = user_stats.user_id AND
+             X.c <> posts_read_count
+    SQL
+  end
+    
+      def cookie_hash(unhashed_auth_token)
+    hash = {
+      value: unhashed_auth_token,
+      httponly: true,
+      expires: SiteSetting.maximum_session_age.hours.from_now,
+      secure: SiteSetting.force_https
+    }
+    
+    class SinatraStaticServer < Sinatra::Base
+    
+    require './plugins/pygments_code'
+require './plugins/raw'
+require 'pathname'
+    
+        def render(context)
+      output = super
+      types = {
+        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
+        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
+        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
+      }
+      if @videos.size > 0
+        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
+        @videos.each do |v|
+          video << '<source src='#{v}' #{types[File.extname(v)]}>'
+        end
+        video += '</video>'
+      else
+        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
       end
     end
-  end
-end
-
-    
-        # This will detect the proper guest OS for the machine and set up
-    # the class to actually execute capabilities.
-    def detect!
-      guest_name = @machine.config.vm.guest
-      initialize_capabilities!(guest_name, @guests, @capabilities, @machine)
-    rescue Errors::CapabilityHostExplicitNotDetected => e
-      raise Errors::GuestExplicitNotDetected, value: e.extra_data[:value]
-    rescue Errors::CapabilityHostNotDetected
-      raise Errors::GuestNotDetected
-    end
-    
-            # Merge another configuration object into this one. This assumes that
-        # the other object is the same class as this one. This should not
-        # mutate this object, but instead should return a new, merged object.
-        #
-        # The default implementation will simply iterate over the instance
-        # variables and merge them together, with this object overriding
-        # any conflicting instance variables of the older object. Instance
-        # variables starting with '__' (double underscores) will be ignored.
-        # This lets you set some sort of instance-specific state on your
-        # configuration keys without them being merged together later.
-        #
-        # @param [Object] other The other configuration object to merge from,
-        #   this must be the same type of object as this one.
-        # @return [Object] The merged object.
-        def merge(other)
-          result = self.class.new
-    
-              result
-        end
-    
-            # This should return a brief (60 characters or less) synopsis of what
-        # this command does. It will be used in the output of the help.
-        #
-        # @return [String]
-        def self.synopsis
-          ''
-        end
-    
-            # This unregisters a plugin so that its components will no longer
-        # be used. Note that this should only be used for testing purposes.
-        def unregister(plugin)
-          if @registered.include?(plugin)
-            @logger.info('Unregistered: #{plugin.name}')
-            @registered.delete(plugin)
-          end
-        end
-      end
-    end
-  end
-end
-
-    
-            # This is called early, before a machine is instantiated, to check
-        # if this provider is usable. This should return true or false.
-        #
-        # If raise_error is true, then instead of returning false, this
-        # should raise an error with a helpful message about why this
-        # provider cannot be used.
-        #
-        # @param [Boolean] raise_error If true, raise exception if not usable.
-        # @return [Boolean]
-        def self.usable?(raise_error=false)
-          # Return true by default for backwards compat since this was
-          # introduced long after providers were being written.
-          true
-        end
-    
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
-    end
-  end
-end
-
-    
-          @conv2 = Conversation.create(hash)
-      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
-             .increase_unread(alice)
-    
-        it 'redirects to /stream for a mobile user' do
-      request.headers['X_MOBILE_DEVICE'] = true
-      post :create, params: {user: {remember_me: '0', username: @user.username, password: 'evankorth'}}
-      expect(response).to be_redirect
-      expect(response.location).to match /^#{stream_url}\??$/
-    end
-  end
-    
-    # This is basically a copy of the original bundler 'bundle' shim
-# with the addition of the loading of our Bundler patches that
-# modify Bundler's caching behaviour.
-    
-    require 'clamp'
-require 'pluginmanager/util'
-require 'pluginmanager/gemfile'
-require 'pluginmanager/install'
-require 'pluginmanager/remove'
-require 'pluginmanager/list'
-require 'pluginmanager/update'
-require 'pluginmanager/pack'
-require 'pluginmanager/unpack'
-require 'pluginmanager/generate'
-require 'pluginmanager/prepare_offline_pack'
-require 'pluginmanager/proxy_support'
-configure_proxy
-    
-      def execute
-    signal_deprecation_warning_for_pack
-    
-    class LogStash::PluginManager::Update < LogStash::PluginManager::Command
-  REJECTED_OPTIONS = [:path, :git, :github]
-  # These are local gems used by LS and needs to be filtered out of other plugin gems
-  NON_PLUGIN_LOCAL_GEMS = ['logstash-core', 'logstash-core-plugin-api']
