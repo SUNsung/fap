d06abc043d29e99901fc0812712e6b9e81a1cca9
SUNsung <weatherius@gmail.com>
2019-05-23 11:44:56 +0300

    Rewrite [23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp] File in: 2019-05-23 11:44:56

--- cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
+++ cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
@@ -1,82 +1,500 @@
 
         
-        /*!
- * \brief Macro to register linear updater.
- */
-#define XGBOOST_REGISTER_LINEAR_UPDATER(UniqueId, Name)                        \
-  static DMLC_ATTRIBUTE_UNUSED ::xgboost::LinearUpdaterReg&                    \
-      __make_##LinearUpdaterReg##_##UniqueId##__ =                             \
-          ::dmlc::Registry< ::xgboost::LinearUpdaterReg>::Get()->__REGISTER__( \
-              Name)
+        protected:
+    bool eventFilter(QObject *object, QEvent *event);
     
     
-    {
-    {void SparsePageWriter::Alloc(std::shared_ptr<SparsePage>* out_page) {
-  CHECK(*out_page == nullptr);
-  if (num_free_buffer_ != 0) {
-    out_page->reset(new SparsePage());
-    --num_free_buffer_;
-  } else {
-    CHECK(qrecycle_.Pop(out_page));
-  }
+    {    /* d = (a0*2) * a3 */
+    'leaq (%%r10,%%r10,1),%%rax\n'
+    'mulq %%r13\n'
+    'movq %%rax,%%rbx\n'
+    'movq %%rdx,%%rcx\n'
+    /* d += (a1*2) * a2 */
+    'leaq (%%r11,%%r11,1),%%rax\n'
+    'mulq %%r12\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* c = a4 * a4 */
+    'movq %%r14,%%rax\n'
+    'mulq %%r14\n'
+    'movq %%rax,%%r8\n'
+    'movq %%rdx,%%r9\n'
+    /* d += (c & M) * R */
+    'andq %%r15,%%rax\n'
+    'movq $0x1000003d10,%%rdx\n'
+    'mulq %%rdx\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* c >>= 52 (%%r8 only) */
+    'shrdq $52,%%r9,%%r8\n'
+    /* t3 (tmp1) = d & M */
+    'movq %%rbx,%%rsi\n'
+    'andq %%r15,%%rsi\n'
+    'movq %%rsi,%q1\n'
+    /* d >>= 52 */
+    'shrdq $52,%%rcx,%%rbx\n'
+    'xorq %%rcx,%%rcx\n'
+    /* a4 *= 2 */
+    'addq %%r14,%%r14\n'
+    /* d += a0 * a4 */
+    'movq %%r10,%%rax\n'
+    'mulq %%r14\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* d+= (a1*2) * a3 */
+    'leaq (%%r11,%%r11,1),%%rax\n'
+    'mulq %%r13\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* d += a2 * a2 */
+    'movq %%r12,%%rax\n'
+    'mulq %%r12\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* d += c * R */
+    'movq %%r8,%%rax\n'
+    'movq $0x1000003d10,%%rdx\n'
+    'mulq %%rdx\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* t4 = d & M (%%rsi) */
+    'movq %%rbx,%%rsi\n'
+    'andq %%r15,%%rsi\n'
+    /* d >>= 52 */
+    'shrdq $52,%%rcx,%%rbx\n'
+    'xorq %%rcx,%%rcx\n'
+    /* tx = t4 >> 48 (tmp3) */
+    'movq %%rsi,%%rax\n'
+    'shrq $48,%%rax\n'
+    'movq %%rax,%q3\n'
+    /* t4 &= (M >> 4) (tmp2) */
+    'movq $0xffffffffffff,%%rax\n'
+    'andq %%rax,%%rsi\n'
+    'movq %%rsi,%q2\n'
+    /* c = a0 * a0 */
+    'movq %%r10,%%rax\n'
+    'mulq %%r10\n'
+    'movq %%rax,%%r8\n'
+    'movq %%rdx,%%r9\n'
+    /* d += a1 * a4 */
+    'movq %%r11,%%rax\n'
+    'mulq %%r14\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* d += (a2*2) * a3 */
+    'leaq (%%r12,%%r12,1),%%rax\n'
+    'mulq %%r13\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* u0 = d & M (%%rsi) */
+    'movq %%rbx,%%rsi\n'
+    'andq %%r15,%%rsi\n'
+    /* d >>= 52 */
+    'shrdq $52,%%rcx,%%rbx\n'
+    'xorq %%rcx,%%rcx\n'
+    /* u0 = (u0 << 4) | tx (%%rsi) */
+    'shlq $4,%%rsi\n'
+    'movq %q3,%%rax\n'
+    'orq %%rax,%%rsi\n'
+    /* c += u0 * (R >> 4) */
+    'movq $0x1000003d1,%%rax\n'
+    'mulq %%rsi\n'
+    'addq %%rax,%%r8\n'
+    'adcq %%rdx,%%r9\n'
+    /* r[0] = c & M */
+    'movq %%r8,%%rax\n'
+    'andq %%r15,%%rax\n'
+    'movq %%rax,0(%%rdi)\n'
+    /* c >>= 52 */
+    'shrdq $52,%%r9,%%r8\n'
+    'xorq %%r9,%%r9\n'
+    /* a0 *= 2 */
+    'addq %%r10,%%r10\n'
+    /* c += a0 * a1 */
+    'movq %%r10,%%rax\n'
+    'mulq %%r11\n'
+    'addq %%rax,%%r8\n'
+    'adcq %%rdx,%%r9\n'
+    /* d += a2 * a4 */
+    'movq %%r12,%%rax\n'
+    'mulq %%r14\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* d += a3 * a3 */
+    'movq %%r13,%%rax\n'
+    'mulq %%r13\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* c += (d & M) * R */
+    'movq %%rbx,%%rax\n'
+    'andq %%r15,%%rax\n'
+    'movq $0x1000003d10,%%rdx\n'
+    'mulq %%rdx\n'
+    'addq %%rax,%%r8\n'
+    'adcq %%rdx,%%r9\n'
+    /* d >>= 52 */
+    'shrdq $52,%%rcx,%%rbx\n'
+    'xorq %%rcx,%%rcx\n'
+    /* r[1] = c & M */
+    'movq %%r8,%%rax\n'
+    'andq %%r15,%%rax\n'
+    'movq %%rax,8(%%rdi)\n'
+    /* c >>= 52 */
+    'shrdq $52,%%r9,%%r8\n'
+    'xorq %%r9,%%r9\n'
+    /* c += a0 * a2 (last use of %%r10) */
+    'movq %%r10,%%rax\n'
+    'mulq %%r12\n'
+    'addq %%rax,%%r8\n'
+    'adcq %%rdx,%%r9\n'
+    /* fetch t3 (%%r10, overwrites a0),t4 (%%rsi) */
+    'movq %q2,%%rsi\n'
+    'movq %q1,%%r10\n'
+    /* c += a1 * a1 */
+    'movq %%r11,%%rax\n'
+    'mulq %%r11\n'
+    'addq %%rax,%%r8\n'
+    'adcq %%rdx,%%r9\n'
+    /* d += a3 * a4 */
+    'movq %%r13,%%rax\n'
+    'mulq %%r14\n'
+    'addq %%rax,%%rbx\n'
+    'adcq %%rdx,%%rcx\n'
+    /* c += (d & M) * R */
+    'movq %%rbx,%%rax\n'
+    'andq %%r15,%%rax\n'
+    'movq $0x1000003d10,%%rdx\n'
+    'mulq %%rdx\n'
+    'addq %%rax,%%r8\n'
+    'adcq %%rdx,%%r9\n'
+    /* d >>= 52 (%%rbx only) */
+    'shrdq $52,%%rcx,%%rbx\n'
+    /* r[2] = c & M */
+    'movq %%r8,%%rax\n'
+    'andq %%r15,%%rax\n'
+    'movq %%rax,16(%%rdi)\n'
+    /* c >>= 52 */
+    'shrdq $52,%%r9,%%r8\n'
+    'xorq %%r9,%%r9\n'
+    /* c += t3 */
+    'addq %%r10,%%r8\n'
+    /* c += d * R */
+    'movq %%rbx,%%rax\n'
+    'movq $0x1000003d10,%%rdx\n'
+    'mulq %%rdx\n'
+    'addq %%rax,%%r8\n'
+    'adcq %%rdx,%%r9\n'
+    /* r[3] = c & M */
+    'movq %%r8,%%rax\n'
+    'andq %%r15,%%rax\n'
+    'movq %%rax,24(%%rdi)\n'
+    /* c >>= 52 (%%r8 only) */
+    'shrdq $52,%%r9,%%r8\n'
+    /* c += t4 (%%r8 only) */
+    'addq %%rsi,%%r8\n'
+    /* r[4] = c */
+    'movq %%r8,32(%%rdi)\n'
+: '+S'(a), '=m'(tmp1), '=m'(tmp2), '=m'(tmp3)
+: 'D'(r)
+: '%rax', '%rbx', '%rcx', '%rdx', '%r8', '%r9', '%r10', '%r11', '%r12', '%r13', '%r14', '%r15', 'cc', 'memory'
+);
 }
-}  // namespace data
-}  // namespace xgboost
     
-        uint16_t port;
-    auto connection = make_unique<DHTConnectionImpl>(family);
-    {
-      port = e->getBtRegistry()->getUdpPort();
-      const std::string& addr = e->getOption()->get(
-          family == AF_INET ? PREF_DHT_LISTEN_ADDR : PREF_DHT_LISTEN_ADDR6);
-      // If UDP port is already used, use the same port
-      // number. Normally IPv4 port is available, then IPv6 port is
-      // (especially for port >= 1024). We don't loose much by doing
-      // this. We did the same thing in TCP socket. See BtSetup.cc.
-      bool rv;
-      if (port == 0) {
-        auto sgl =
-            util::parseIntSegments(e->getOption()->get(PREF_DHT_LISTEN_PORT));
-        sgl.normalize();
-        rv = connection->bind(port, addr, sgl);
-      }
-      else {
-        rv = connection->bind(port, addr);
-      }
-      if (!rv) {
-        throw DL_ABORT_EX('Error occurred while binding UDP port for DHT');
-      }
-      localNode->setPort(port);
+    
+    {    /* Try to multiply it by bad values */
+    CHECK(secp256k1_ecdh(ctx, output, &point, s_zero) == 0);
+    CHECK(secp256k1_ecdh(ctx, output, &point, s_overflow) == 0);
+    /* ...and a good one */
+    s_overflow[31] -= 1;
+    CHECK(secp256k1_ecdh(ctx, output, &point, s_overflow) == 1);
+}
+    
+        ret	
+    
+    
+    inline bool ParseInternalKey(const Slice& internal_key,
+                             ParsedInternalKey* result) {
+  const size_t n = internal_key.size();
+  if (n < 8) return false;
+  uint64_t num = DecodeFixed64(internal_key.data() + n - 8);
+  unsigned char c = num & 0xff;
+  result->sequence = num >> 8;
+  result->type = static_cast<ValueType>(c);
+  result->user_key = Slice(internal_key.data(), n - 8);
+  return (c <= static_cast<unsigned char>(kTypeValue));
+}
+    
+    // Generate param traits read methods.
+#include 'ipc/param_traits_read_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+    
+    namespace nw {
     }
-    A2_LOG_DEBUG(fmt('Initialized local node ID=%s',
-                     util::toHex(localNode->getID(), DHT_ID_LENGTH).c_str()));
-    auto tracker = std::make_shared<DHTMessageTracker>();
-    auto routingTable = make_unique<DHTRoutingTable>(localNode);
-    auto factory = make_unique<DHTMessageFactoryImpl>(family);
-    auto dispatcher = make_unique<DHTMessageDispatcherImpl>(tracker);
-    auto receiver = make_unique<DHTMessageReceiver>(tracker);
-    auto taskQueue = make_unique<DHTTaskQueueImpl>();
-    auto taskFactory = make_unique<DHTTaskFactoryImpl>();
-    auto peerAnnounceStorage = make_unique<DHTPeerAnnounceStorage>();
-    auto tokenTracker = make_unique<DHTTokenTracker>();
-    // For now, UDPTrackerClient was enabled along with DHT
-    auto udpTrackerClient = std::make_shared<UDPTrackerClient>();
-    const auto messageTimeout =
-        e->getOption()->getAsInt(PREF_DHT_MESSAGE_TIMEOUT);
-    // wiring up
-    tracker->setRoutingTable(routingTable.get());
-    tracker->setMessageFactory(factory.get());
     
     
-    {} // namespace aria2
+    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
+};
     
-    public:
-  DHTTaskQueueImpl();
     
-    DNSCache& DNSCache::operator=(const DNSCache& c)
-{
-  if (this != &c) {
-    entries_ = c.entries_;
-  }
-  return *this;
+    {  DISALLOW_COPY_AND_ASSIGN(MenuDelegate);
+};
+    
+    void Menu::UpdateStates() {
 }
+    
+        bool Read(ClipboardData& data) {
+      switch(data.type) {
+        case TYPE_TEXT:
+        return ReadText(data);
+        break;
+        case TYPE_HTML:
+        return ReadHTML(data);
+        break;
+        case TYPE_RTF:
+        return ReadRTF(data);
+        break;
+        case TYPE_PNG:
+        case TYPE_JPEG:
+        return ReadImage(data);
+        break;
+        case TYPE_NONE:
+        NOTREACHED();
+        return false;
+      }
+      NOTREACHED();
+      return false;      
+    }
+    
+      private:
+    ~NwScreenDisplayObserver() override;
+    // gfx::DisplayObserver implementation.
+    void OnDisplayMetricsChanged(const display::Display& display, uint32_t changed_metrics) override;
+    void OnDisplayAdded(const display::Display& new_display) override;
+    void OnDisplayRemoved(const display::Display& old_display) override;
+    
+    TegraBinaryOp_Invoker(max, max)
+    
+    #include <carotene/definitions.hpp>
+#include <stdint.h>
+#include <cstddef>
+    
+        //2-line buffer
+    std::vector<f32> _buf(4*(cn * (size.width + 2) + 32 / sizeof(f32)));
+    f32* lanea = internal::alignPtr(&_buf[cn], 32);
+    f32* laneA = internal::alignPtr(lanea + cn * (size.width + 2), 32);
+    
+    template<typename T>
+struct OpCmpEQ
+{
+    typedef T type;
+    }
+    
+      kFullType = 1,
+    
+      ~AutoCompactTest() {
+    delete db_;
+    DestroyDB(dbname_, Options());
+    delete tiny_cache_;
+  }
+    
+     private:
+  Status EmitPhysicalRecord(RecordType type, const char* ptr, size_t length);
+    
+    
+    {  Env* const env_;
+  const std::string dbname_;
+  const Options& options_;
+  Cache* cache_;
+};
+    
+      // Return the approximate offset in the database of the data for
+  // 'key' as of version 'v'.
+  uint64_t ApproximateOffsetOf(Version* v, const InternalKey& key);
+    
+      // Returns true iff the status indicates an InvalidArgument.
+  bool IsInvalidArgument() const { return code() == kInvalidArgument; }
+    
+     private:
+  friend class TableCache;
+  struct Rep;
+    
+      WriteBatch();
+    
+    class FilterBlockReader {
+ public:
+  // REQUIRES: 'contents' and *policy must stay live while *this is live.
+  FilterBlockReader(const FilterPolicy* policy, const Slice& contents);
+  bool KeyMayMatch(uint64_t block_offset, const Slice& key);
+    }
+    
+      double FalsePositiveRate() {
+    char buffer[sizeof(int)];
+    int result = 0;
+    for (int i = 0; i < 10000; i++) {
+      if (Matches(Key(i + 1000000000, buffer))) {
+        result++;
+      }
+    }
+    return result / 10000.0;
+  }
+    
+    #include 'port/port.h'
+#include 'port/thread_annotations.h'
+#include 'util/mutexlock.h'
+#include 'util/testharness.h'
+#include 'util/testutil.h'
+    
+    
+    {  // Add to priority queue
+  queue_.push_back(BGItem());
+  queue_.back().function = function;
+  queue_.back().arg = arg;
+}
+    
+    class ExtensionResponse : public virtual ::apache::thrift::TBase {
+ public:
+    }
+    
+    /// Gets supported hardwareDriver from system storage controllers.
+static inline void getSmartCtlDeviceType(
+    const std::vector<std::string>& storage_drivers,
+    std::vector<hardwareDriver>& types) {
+  for (const auto& driver : storage_drivers) {
+    auto hw_driver = kSmartExplicitDriverToDevice.find(driver);
+    if (hw_driver != kSmartExplicitDriverToDevice.end()) {
+      types.push_back(hw_driver->second);
+    }
+  }
+}
+    
+    
+    {
+    {} // namespace perf_event_open
+} // namespace osquery
+
+    
+      // Author: @guliashvili
+  // Creation Time: 4/09/2018
+  bool isTotalQueryCounterMonitorEnabled();
+    
+    
+    {
+    {struct mei_version {
+  uint32_t important_details[7];
+  uint16_t major;
+  uint16_t minor;
+  uint16_t hotfix;
+  uint16_t build;
+  uint16_t r_major;
+  uint16_t r_minor;
+  uint16_t r_hotfix;
+  uint16_t r_build;
+  uint16_t codes[6];
+};
+}
+}
+
+    
+    TEST_F(PerfOutputTests,
+       impl_consumeWrappedMessagesFromCircularBuffer_without_wrapping) {
+  using WrappedMessage = ebpf::PerfOutput<TestMessage>::WrappedMessage;
+  auto const test_size = std::size_t{9};
+  auto buf = std::vector<ebpf::impl::ByteType>(
+      sizeof(WrappedMessage) * test_size + 128, 0);
+  auto buf_ptr = &buf[0];
+  for (std::size_t i = 0; i < test_size; ++i) {
+    auto wrapped = WrappedMessage{};
+    wrapped.msg.a_ = i + 1;
+    wrapped.msg.b_ = i * 2 + 2;
+    wrapped.msg.c_ = 'j';
+    wrapped.msg.d_ = 'k';
+    wrapped.size = sizeof(TestMessage);
+    wrapped.header.type = PERF_RECORD_SAMPLE;
+    wrapped.header.size = sizeof(WrappedMessage);
+    auto const wrapped_ptr =
+        reinterpret_cast<ebpf::impl::ByteType const*>(&wrapped);
+    std::copy(wrapped_ptr, wrapped_ptr + sizeof(WrappedMessage), buf_ptr);
+    buf_ptr += sizeof(WrappedMessage);
+  }
+  auto dst = std::vector<TestMessage>{};
+  auto status =
+      ebpf::impl::consumeWrappedMessagesFromCircularBuffer<WrappedMessage>(
+          &buf[0],
+          0,
+          static_cast<std::size_t>(buf_ptr - &buf[0]),
+          buf.size(),
+          dst);
+  ASSERT_FALSE(status.isError());
+  ASSERT_EQ(dst.size(), test_size);
+  for (std::size_t i = 0; i < test_size; ++i) {
+    EXPECT_EQ(dst[i].a_, static_cast<int>(i + 1));
+    EXPECT_EQ(dst[i].b_, static_cast<int>(i * 2 + 2));
+    EXPECT_EQ(dst[i].c_, 'j');
+    EXPECT_EQ(dst[i].d_, 'k');
+  }
+}
+    
+    TEST_F(iokitDevicetree, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from iokit_devicetree');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'name', NormalType}
+  //      {'class', NormalType}
+  //      {'id', IntType}
+  //      {'parent', IntType}
+  //      {'device_path', NormalType}
+  //      {'service', IntType}
+  //      {'busy_state', IntType}
+  //      {'retain_count', IntType}
+  //      {'depth', IntType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
+}
+    
+    class iptables : public testing::Test {
+  protected:
+    void SetUp() override {
+      setUpEnvironment();
+    }
+};
+    
+    TEST_F(kernelExtensions, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from kernel_extensions');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'idx', IntType}
+  //      {'refs', IntType}
+  //      {'size', IntType}
+  //      {'name', NormalType}
+  //      {'version', NormalType}
+  //      {'linked_against', NormalType}
+  //      {'path', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
+}
+    
+    namespace osquery {
+namespace table_tests {
+    }
+    }
+    
+    // Sanity check integration test for keychain_acls
+// Spec file: specs/darwin/keychain_acls.table
