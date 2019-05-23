df9a9b93f24ff8adb4aa4e57c6bd718c6898d618
SUNsung <weatherius@gmail.com>
2019-05-23 05:33:26 +0300

    Rewrite [41qO2ov7DaQCA41wanSTNImOYA41jsnIROMyxs41P4n.cpp] File in: 2019-05-23 05:33:25

--- cccccc/41qO2ov7DaQCA41wanSTNImOYA41jsnIROMyxs41P4n.cpp
+++ cccccc/41qO2ov7DaQCA41wanSTNImOYA41jsnIROMyxs41P4n.cpp
@@ -1,259 +1,273 @@
 
         
-        #include <string>
-#include <vector>
-#include 'tensorflow/core/framework/op_def.pb.h'
-#include 'tensorflow/core/framework/op_gen_lib.h'
-#include 'tensorflow/core/platform/types.h'
+          // Delegate implementations.
+  void OnError(const std::string& error) override;
+  void OnError(const std::string& message,
+               const int code,
+               const std::string& domain) override;
+  void OnCheckingForUpdate() override;
+  void OnUpdateAvailable() override;
+  void OnUpdateNotAvailable() override;
+  void OnUpdateDownloaded(const std::string& release_notes,
+                          const std::string& release_name,
+                          const base::Time& release_date,
+                          const std::string& update_url) override;
     
-      void AddDocStringArgs();
-  void AddDocStringInputs();
-  void AddDocStringAttrs();
-  void AddDocStringNameArg();
-  void AddOutputGlobals();
-  void AddDocStringOutputs();
-  void AddBody(const string& prefix);
-  void AddBodyNoReturn(const string& apply_prefix);
-  void AddExport();
+    void BrowserWindow::UpdateDraggableRegions(
+    content::RenderFrameHost* rfh,
+    const std::vector<DraggableRegion>& regions) {
+  if (window_->has_frame())
+    return;
+  static_cast<NativeWindowViews*>(window_.get())
+      ->UpdateDraggableRegions(DraggableRegionsToSkRegion(regions));
+}
     
-    #include 'tensorflow/c/c_api.h'
-#include 'tensorflow/core/framework/types.pb.h'
-#include 'tensorflow/core/lib/core/status.h'
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    #include <vector>
     
-    // Global registry mapping C API error codes to the corresponding custom Python
-// exception type. This is used to expose the exception types to C extension
-// code (i.e. so we can raise custom exceptions via SWIG).
-//
-// Init() must be called exactly once at the beginning of the process before
-// Lookup() can be used.
-//
-// Example usage:
-//   TF_Status* status = TF_NewStatus();
-//   TF_Foo(..., status);
-//
-//   if (TF_GetCode(status) != TF_OK) {
-//     PyObject* exc_type = PyExceptionRegistry::Lookup(TF_GetCode(status));
-//     // Arguments to OpError base class. Set `node_def` and `op` to None.
-//     PyObject* args =
-//       Py_BuildValue('sss', nullptr, nullptr, TF_Message(status));
-//     PyErr_SetObject(exc_type, args);
-//     Py_DECREF(args);
-//     TF_DeleteStatus(status);
-//     return NULL;
-//   }
-class PyExceptionRegistry {
- public:
-  // Initializes the process-wide registry. Should be called exactly once near
-  // the beginning of the process. The arguments are the various Python
-  // exception types (e.g. `cancelled_exc` corresponds to
-  // errors.CancelledError).
-  static void Init(PyObject* code_to_exc_type_map);
-    }
-    
-    namespace tensorflow {
-namespace swig {
-    }
-    }
+      AtomQuotaPermissionContext();
     
     
-    {
-    {}  // namespace cuda
-}  // namespace stream_executor
+    {}  // namespace auto_updater
 
     
-      // Creates a batched FFT plan.
-  //
-  // stream:          The GPU stream in which the FFT runs.
-  // rank:            Dimensionality of the transform (1, 2, or 3).
-  // elem_count:      Array of size rank, describing the size of each dimension.
-  // input_embed, output_embed:
-  //                  Pointer of size rank that indicates the storage dimensions
-  //                  of the input/output data in memory. If set to null_ptr all
-  //                  other advanced data layout parameters are ignored.
-  // input_stride:    Indicates the distance (number of elements; same below)
-  //                  between two successive input elements.
-  // input_distance:  Indicates the distance between the first element of two
-  //                  consecutive signals in a batch of the input data.
-  // output_stride:   Indicates the distance between two successive output
-  //                  elements.
-  // output_distance: Indicates the distance between the first element of two
-  //                  consecutive signals in a batch of the output data.
-  virtual std::unique_ptr<Plan> CreateBatchedPlan(
-      Stream *stream, int rank, uint64 *elem_count, uint64 *input_embed,
-      uint64 input_stride, uint64 input_distance, uint64 *output_embed,
-      uint64 output_stride, uint64 output_distance, Type type,
-      bool in_place_fft, int batch_count) = 0;
+    namespace atom {
+    }
     
-    bool HostTimer::Stop(Stream* stream) {
-  return stream->ThenDoHostCallback([this]() { this->StopNow(); }).ok();
+    bool AboutProtocolHandler::IsSafeRedirectTarget(const GURL& location) const {
+  return false;
 }
     
+    #include 'atom/browser/net/url_request_about_job.h'
     
-    {  // Set the maximum number of read-only files that will be mapped via mmap.
-  // Must be called before creating an Env.
-  static void SetReadOnlyMMapLimit(int limit);
-};
+      base::WeakPtrFactory<URLRequestAboutJob> weak_ptr_factory_;
     
-    namespace leveldb {
-    }
-    
-      void Build(int n) {
-    std::string key_space, value_space;
-    WriteBatch batch;
-    for (int i = 0; i < n; i++) {
-      // if ((i % 100) == 0) fprintf(stderr, '@ %d of %d\n', i, n);
-      Slice key = Key(i, &key_space);
-      batch.Clear();
-      batch.Put(key, Value(i, &value_space));
-      WriteOptions options;
-      // Corrupt() doesn't work without this sync on windows; stat reports 0 for
-      // the file size.
-      if (i == n - 1) {
-        options.sync = true;
-      }
-      ASSERT_OK(db_->Write(options, &batch));
-    }
+      std::vector<ui::FileInfo> file_infos;
+  for (const base::FilePath& file : files) {
+    file_infos.push_back(ui::FileInfo(file, base::FilePath()));
   }
+  data.SetFilenames(file_infos);
     
-      // If true, an error is raised if the database already exists.
-  bool error_if_exists = false;
-    
-      // Given a key, return an approximate byte offset in the file where
-  // the data for that key begins (or would begin if the key were
-  // present in the file).  The returned value is in terms of file
-  // bytes, and so includes effects like compression of the underlying data.
-  // E.g., the approximate offset of the last key in the table will
-  // be close to the file length.
-  uint64_t ApproximateOffsetOf(const Slice& key) const;
-    
-    class Slice;
-    
-    #include 'leveldb/slice.h'
-    
-    
-    {  // Total memory usage of the arena.
-  //
-  // TODO(costan): This member is accessed via atomics, but the others are
-  //               accessed without any locking. Is this OK?
-  std::atomic<size_t> memory_usage_;
-};
-    
-    std::string GetWindowsErrorMessage(DWORD error_code) {
-  std::string message;
-  char* error_text = nullptr;
-  // Use MBCS version of FormatMessage to match return value.
-  size_t error_text_size = ::FormatMessageA(
-      FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER |
-          FORMAT_MESSAGE_IGNORE_INSERTS,
-      nullptr, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-      reinterpret_cast<char*>(&error_text), 0, nullptr);
-  if (!error_text) {
-    return message;
-  }
-  message.assign(error_text, error_text_size);
-  ::LocalFree(error_text);
-  return message;
-}
-    
-      while(true) {
-    if(!this->sync_.getPending(fname, mtype, rseqid)) {
-      iprot_->readMessageBegin(fname, mtype, rseqid);
-    }
-    if(seqid == rseqid) {
-      if (mtype == ::apache::thrift::protocol::T_EXCEPTION) {
-        ::apache::thrift::TApplicationException x;
-        x.read(iprot_);
-        iprot_->readMessageEnd();
-        iprot_->getTransport()->readEnd();
-        sentry.commit();
-        throw x;
-      }
-      if (mtype != ::apache::thrift::protocol::T_REPLY) {
-        iprot_->skip(::apache::thrift::protocol::T_STRUCT);
-        iprot_->readMessageEnd();
-        iprot_->getTransport()->readEnd();
-      }
-      if (fname.compare('registerExtension') != 0) {
-        iprot_->skip(::apache::thrift::protocol::T_STRUCT);
-        iprot_->readMessageEnd();
-        iprot_->getTransport()->readEnd();
-    }
-    }
+        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0,
+                     const typename internal::VecTraits<T>::vec64 & v_src1,
+                     typename internal::VecTraits<T>::vec64 & v_dst) const
+    {
+        v_dst = internal::vqadd(v_src0, v_src1);
     }
     
-    uint32_t InternalOptionInfo::write(::apache::thrift::protocol::TProtocol* oprot) const {
-  uint32_t xfer = 0;
-  ::apache::thrift::protocol::TOutputRecursionTracker tracker(*oprot);
-  xfer += oprot->writeStructBegin('InternalOptionInfo');
+                vst1q_f32(dsta + x, wa);
+            vst1q_f32(dstb + x, wb);
+            vst1q_f32(dstc + x, wc);
+        }
+        if(x < colsn)
+        {
+            x = colsn-4;
+            goto box3x3f32_horiz_ll;
+        }
+    }
+    else
+    {
+        //horizontal convolution (last 2 lines)
+        f32* dstb = internal::getRowPtr(dstBase, dstStride, i-1);
+        size_t x = 0;
+        for (; x <= colsn - 4; x += 4)
+        {
+            internal::prefetch(laneA + x + cn);
+            internal::prefetch(laneB + x + cn);
+box3x3f32_horiz_last2:
+            float32x4_t lane0a = vld1q_f32(laneA + x - cn);
+            float32x4_t lane2a = vld1q_f32(laneA + x + cn);
+            float32x4_t lane1a = vld1q_f32(laneA + x);
+    
+    
+    {        for (; i < size.width; i++)
+            result += (src[i] != 0)?1:0;
+        if (result < 0)//saturate in case of overflow ~ 8GB of non-zeros...
+        {
+            return 0x7fFFffFF;
+        }
+    }
+    return result;
+#else
+    (void)_size;
+    (void)srcBase;
+    (void)srcStride;
+    
+    #include <carotene/definitions.hpp>
+    
+    void convert_dataset(const char* image_filename, const char* label_filename,
+        const char* db_filename) {
+  // Open files
+  std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
+  std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
+  CHECK(image_file) << 'Unable to open file ' << image_filename;
+  CHECK(label_file) << 'Unable to open file ' << label_filename;
+  // Read the magic and the meta data
+  uint32_t magic;
+  uint32_t num_items;
+  uint32_t num_labels;
+  uint32_t rows;
+  uint32_t cols;
     }
     
-    /**
- * @brief Represents a hardware driver type that SMART api can you use to query
- * device information.
- *
- * @param driver name of SMART controller driver
- * @param maxID max ID number of which disks on the controller is monitored
+    
+    {}  // namespace caffe
+    
+      /**
+   * @brief Applies the transformation defined in the data layer's
+   * transform_param block to the data.
+   *
+   * @param datum
+   *    Datum containing the data to be transformed.
+   * @param transformed_blob
+   *    This is destination blob. It can be part of top blob's data if
+   *    set_cpu_data() is used. See data_layer.cpp for an example.
+   */
+  void Transform(const Datum& datum, Blob<Dtype>* transformed_blob);
+    
+    #endif  // CAFFE_ABSVAL_LAYER_HPP_
+
+    
+    #endif  // CAFFE_ARGMAX_LAYER_HPP_
+
+    
+      virtual inline const char* type() const { return 'BNLL'; }
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #ifdef USE_CUDNN
+/**
+ * @brief CuDNN acceleration of SigmoidLayer.
  */
-struct hardwareDriver {
-  std::string driver;
-  size_t maxID;
-};
+template <typename Dtype>
+class CuDNNSigmoidLayer : public SigmoidLayer<Dtype> {
+ public:
+  explicit CuDNNSigmoidLayer(const LayerParameter& param)
+      : SigmoidLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNSigmoidLayer();
+    }
     
-    #include <osquery/core.h>
-#include <osquery/utils/expected/expected.h>
-#include <osquery/utils/status/status.h>
+    #endif
+
     
-    struct mei_response {
-  uint32_t maxlen;
-  uint8_t version;
-};
+    // Given a MutableIterator to the start of a block, run DetectParagraphs on
+// that block and commit the results to the underlying ROW and BLOCK structs,
+// saving the ParagraphModels in models.  Caller owns the models.
+// We use unicharset during the function to answer questions such as 'is the
+// first letter of this word upper case?'
+void DetectParagraphs(int debug_level,
+                      bool after_text_recognition,
+                      const MutableIterator *block_start,
+                      GenericVector<ParagraphModel *> *models);
     
-    
-    {  ValidatatioMap row_map = {{'filter_name', NonEmptyString},
-                            {'chain', NormalType},
-                            {'policy', NormalType},
-                            {'target', NormalType},
-                            {'protocol', IntType},
-                            {'src_port', IntMinMaxCheck(0, 65535)},
-                            {'dst_port', IntMinMaxCheck(0, 65535)},
-                            {'src_ip', verifyEmptyStringOrIpAddress},
-                            {'src_mask', verifyEmptyStringOrIpAddress},
-                            {'iniface', NormalType},
-                            {'iniface_mask', verifyEmptyStringOrIpAddress},
-                            {'dst_ip', verifyEmptyStringOrIpAddress},
-                            {'dst_mask', verifyEmptyStringOrIpAddress},
-                            {'outiface', NormalType},
-                            {'outiface_mask', verifyEmptyStringOrIpAddress},
-                            {'match', SpecificValuesCheck{'yes', 'no'}},
-                            {'packets', NonNegativeInt},
-                            {'bytes', NonNegativeInt}};
-  validate_rows(data, row_map);
+    bool ParagraphModel::ValidBodyLine(int lmargin, int lindent,
+                                   int rindent, int rmargin) const {
+  switch (justification_) {
+    case JUSTIFICATION_LEFT:
+      return NearlyEqual(lmargin + lindent, margin_ + body_indent_,
+                         tolerance_);
+    case JUSTIFICATION_RIGHT:
+      return NearlyEqual(rmargin + rindent, margin_ + body_indent_,
+                         tolerance_);
+    case JUSTIFICATION_CENTER:
+      return NearlyEqual(lindent, rindent, tolerance_ * 2);
+    default:
+      // shouldn't happen
+      return false;
+  }
 }
     
-    TEST_F(keychainAcls, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from keychain_acls');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'keychain_path', NormalType}
-  //      {'authorizations', NormalType}
-  //      {'path', NormalType}
-  //      {'description', NormalType}
-  //      {'label', NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
+    // Computes the Otsu threshold(s) for the given image rectangle, making one
+// for each channel. Each channel is always one byte per pixel.
+// Returns an array of threshold values and an array of hi_values, such
+// that a pixel value >threshold[channel] is considered foreground if
+// hi_values[channel] is 0 or background if 1. A hi_value of -1 indicates
+// that there is no apparent foreground. At least one hi_value will not be -1.
+// Delete thresholds and hi_values with delete [] after use.
+// The return value is the number of channels in the input image, being
+// the size of the output thresholds and hi_values arrays.
+int OtsuThreshold(Pix* src_pix, int left, int top, int width, int height,
+                  int** thresholds, int** hi_values);
+    
+    #include 'ccutil.h'
+#include 'errcode.h'
+#include 'genericvector.h'
+#include 'tesscallback.h'
+    
+      // Main worker method that retrieves the next number in the sequence.
+  // Returns kInvalidVal if called more than N times after object initialization
+  int GetVal() {
+    const int kInvalidVal = -1;
+    const int kMaxNaturalNumberValue = 1 << num_bits_;
+    if (next_num_ >= kMaxNaturalNumberValue)
+      return kInvalidVal;
+    int n = next_num_;
+    }
+    
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_IMPL_API void     ImGui_Marmalade_InvalidateDeviceObjects();
+IMGUI_IMPL_API bool     ImGui_Marmalade_CreateDeviceObjects();
+    
+    // Called by Init/NewFrame/Shutdown
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateFontsTexture();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyFontsTexture();
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyDeviceObjects();
+
+    
+    
+    {    return 0;
 }
+    
+    
+    {    // This function call won't return, and will engage in an infinite loop, processing events from the browser, and dispatching them.
+    emscripten_set_main_loop_arg(main_loop, NULL, 0, true);
+}
+    
+    static void check_vk_result(VkResult err)
+{
+    if (err == 0) return;
+    printf('VkResult %d\n', err);
+    if (err < 0)
+        abort();
+}
+    
+    void    ImGui_ImplVulkan_DestroyDeviceObjects()
+{
+    ImGui_ImplVulkan_InitInfo* v = &g_VulkanInitInfo;
+    ImGui_ImplVulkanH_DestroyWindowRenderBuffers(v->Device, &g_MainWindowRenderBuffers, v->Allocator);
+    ImGui_ImplVulkan_DestroyFontUploadObjects();
+    }
+    
+    namespace rocksdb {
+    }
+    
+    
+    {}  //  namespace rocksdb
+
+    
+    StopWriteToken::~StopWriteToken() {
+  assert(controller_->total_stopped_ >= 1);
+  --controller_->total_stopped_;
+}
+    
+    
+    {}  // namespace rocksdb
+    
+      DB* db = nullptr;
+  DestroyDB(kDBPath, options);
+  Status s = DB::Open(options, kDBPath, &db);
+  assert(s.ok());
+  assert(db);
+    
+      const char* Name() const override { return 'MyFilter'; }
