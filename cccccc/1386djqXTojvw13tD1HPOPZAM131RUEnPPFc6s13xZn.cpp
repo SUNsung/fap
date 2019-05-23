0c9fa7e809017eb697ff1d9d0b5a7adb4aad7d3f
SUNsung <weatherius@gmail.com>
2019-05-24 02:31:23 +0300

    Rewrite [1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp] File in: 2019-05-24 02:31:22

--- cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
+++ cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
@@ -1,112 +1,45 @@
 
         
-        
-// Called on every log record (each one of which is a WriteBatch)
-// found in a kLogFile.
-static void WriteBatchPrinter(uint64_t pos, Slice record, WritableFile* dst) {
-  std::string r = '--- offset ';
-  AppendNumberTo(&r, pos);
-  r += '; ';
-  if (record.size() < 12) {
-    r += 'log record length ';
-    AppendNumberTo(&r, record.size());
-    r += ' is too small\n';
-    dst->Append(r);
-    return;
-  }
-  WriteBatch batch;
-  WriteBatchInternal::SetContents(&batch, record);
-  r += 'sequence ';
-  AppendNumberTo(&r, WriteBatchInternal::Sequence(&batch));
-  r.push_back('\n');
-  dst->Append(r);
-  WriteBatchItemPrinter batch_item_printer;
-  batch_item_printer.dst_ = dst;
-  Status s = batch.Iterate(&batch_item_printer);
-  if (!s.ok()) {
-    dst->Append('  error: ' + s.ToString() + '\n');
-  }
+            http://www.apache.org/licenses/LICENSE-2.0
+    
+    
+    {}  // namespace tensorflow
+    
+    void ImportNumpy() {
+  import_array1();
 }
     
-    namespace leveldb {
+    namespace tensorflow {
     }
     
-    bool HandleDumpCommand(Env* env, char** files, int num) {
-  StdoutPrinter printer;
-  bool ok = true;
-  for (int i = 0; i < num; i++) {
-    Status s = DumpFile(env, files[i], &printer);
-    if (!s.ok()) {
-      fprintf(stderr, '%s\n', s.ToString().c_str());
-      ok = false;
-    }
-  }
-  return ok;
+    #ifndef TENSORFLOW_STREAM_EXECUTOR_HOST_OR_DEVICE_SCALAR_H_
+#define TENSORFLOW_STREAM_EXECUTOR_HOST_OR_DEVICE_SCALAR_H_
+    
+    bool UvTaskRunner::PostDelayedTask(const base::Location& from_here,
+                                   base::OnceClosure task,
+                                   base::TimeDelta delay) {
+  auto* timer = new uv_timer_t;
+  timer->data = this;
+  uv_timer_init(loop_, timer);
+  uv_timer_start(timer, UvTaskRunner::OnTimeout, delay.InMilliseconds(), 0);
+  tasks_[timer] = std::move(task);
+  return true;
 }
     
-      // crc32c values for all supported record types.  These are
-  // pre-computed to reduce the overhead of computing the crc of the
-  // record type stored in the header.
-  uint32_t type_crc_[kMaxRecordType + 1];
-    
-    
-    {
-    {  void ArchiveFile(const std::string& fname) {
-    // Move into another directory.  E.g., for
-    //    dir/foo
-    // rename to
-    //    dir/lost/foo
-    const char* slash = strrchr(fname.c_str(), '/');
-    std::string new_dir;
-    if (slash != nullptr) {
-      new_dir.assign(fname.data(), slash - fname.data());
-    }
-    new_dir.append('/lost');
-    env_->CreateDir(new_dir);  // Ignore error
-    std::string new_file = new_dir;
-    new_file.append('/');
-    new_file.append((slash == nullptr) ? fname.c_str() : slash + 1);
-    Status s = env_->RenameFile(fname, new_file);
-    Log(options_.info_log, 'Archiving %s: %s\n',
-        fname.c_str(), s.ToString().c_str());
-  }
-};
-}  // namespace
-    
-      int level_;
-  uint64_t max_output_file_size_;
-  Version* input_version_;
-  VersionEdit edit_;
-    
-    namespace leveldb {
+    namespace atom {
     }
     
+    #endif  // ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
+
     
-    {
-    {}}
+    #if defined(OS_POSIX)
+// The 'magic' file descriptor that the relauncher process' write side of the
+// pipe shows up on. Chosen to avoid conflicting with stdin, stdout, and
+// stderr.
+extern const int kRelauncherSyncFD;
+#endif
     
-    void Assembler::bl(int32_t offset) {
-  EmitIForm(18, uint32_t(offset), 0, 1);
-}
-    
-    void fillMap(BaseMap* map, const APCArray* ar) {
-  for (auto i = uint32_t{0}; i < ar->size(); ++i) {
-    map->set(*ar->getKey(i).asTypedValue(),
-             *ar->getValue(i)->toLocal().asTypedValue());
-  }
-}
-    
-    #include <folly/ScopeGuard.h>
-    
-    #include 'hphp/runtime/base/file.h'
-#include 'hphp/runtime/base/mem-file.h'
-#include 'hphp/runtime/base/stream-wrapper.h'
-#include <folly/String.h>
-#include <folly/portability/SysStat.h>
-#include <folly/portability/Unistd.h>
-    
-    struct GlobStreamWrapper final : Stream::Wrapper {
-  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-  req::ptr<Directory> opendir(const String& path) override;
-};
+      base::LaunchOptions options;
+  options.new_process_group = true;  // detach
+  options.fds_to_remap.push_back(std::make_pair(devnull.get(), STDERR_FILENO));
+  options.fds_to_remap.push_back(std::make_pair(devnull.get(), STDOUT_FILENO));
