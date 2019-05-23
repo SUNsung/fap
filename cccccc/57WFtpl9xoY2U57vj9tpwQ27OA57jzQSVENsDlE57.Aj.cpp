65567feffc1f4c1b5bf75520cd7cd225bdd85e26
SUNsung <weatherius@gmail.com>
2019-05-23 14:38:26 +0300

    Rewrite [57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp] File in: 2019-05-23 14:38:25

--- cccccc/57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp
+++ cccccc/57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp
@@ -1,315 +1,139 @@
 
         
-        
-    {}  // namespace
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    namespace atom {
-    }
     
-    #include 'atom/browser/api/event_emitter.h'
+    {}  // namespace tensorflow
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
      private:
-  void Destroy();
+  PyRecordReader();
     
-    namespace atom {
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+            // Get the size of the thread attribute list.
+        InitializeProcThreadAttributeList(NULL, 1, 0, &attrListSize);
+    
+    
+    {DMLC_REGISTER_DATA_PARSER(uint32_t, real_t, dense_libsvm,
+  data::CreateDenseLibSVMParser<uint32_t __DMLC_COMMA real_t>);
+}  // namespace dmlc
+
+    
+    void SimpleCSRSource::LoadBinary(dmlc::Stream* fi) {
+  int tmagic;
+  CHECK(fi->Read(&tmagic, sizeof(tmagic)) == sizeof(tmagic)) << 'invalid input file format';
+  CHECK_EQ(tmagic, kMagic) << 'invalid format, magic number mismatch';
+  info.LoadBinary(fi);
+  fi->Read(&page_.offset.HostVector());
+  fi->Read(&page_.data.HostVector());
+}
+    
+      void PredictInstance(const SparsePage::Inst& inst,
+                       std::vector<bst_float>* out_preds,
+                       const gbm::GBTreeModel& model, unsigned ntree_limit,
+                       unsigned root_index) override {
+    if (thread_temp.size() == 0) {
+      thread_temp.resize(1, RegTree::FVec());
+      thread_temp[0].Init(model.param.num_feature);
+    }
+    ntree_limit *= model.param.num_output_group;
+    if (ntree_limit == 0 || ntree_limit > model.trees.size()) {
+      ntree_limit = static_cast<unsigned>(model.trees.size());
+    }
+    out_preds->resize(model.param.num_output_group *
+                      (model.param.size_leaf_vector + 1));
+    // loop over output groups
+    for (int gid = 0; gid < model.param.num_output_group; ++gid) {
+      (*out_preds)[gid] =
+          PredValue(inst, model.trees, model.tree_info, gid, root_index,
+                    &thread_temp[0], 0, ntree_limit) +
+          model.base_margin;
+    }
+  }
+  void PredictLeaf(DMatrix* p_fmat, std::vector<bst_float>* out_preds,
+                   const gbm::GBTreeModel& model, unsigned ntree_limit) override {
+    const int nthread = omp_get_max_threads();
+    InitThreadTemp(nthread, model.param.num_feature);
+    const MetaInfo& info = p_fmat->Info();
+    // number of valid trees
+    ntree_limit *= model.param.num_output_group;
+    if (ntree_limit == 0 || ntree_limit > model.trees.size()) {
+      ntree_limit = static_cast<unsigned>(model.trees.size());
+    }
+    std::vector<bst_float>& preds = *out_preds;
+    preds.resize(info.num_row_ * ntree_limit);
+    // start collecting the prediction
+    for (const auto &batch : p_fmat->GetRowBatches()) {
+      // parallel over local batch
+      const auto nsize = static_cast<bst_omp_uint>(batch.Size());
+#pragma omp parallel for schedule(static)
+      for (bst_omp_uint i = 0; i < nsize; ++i) {
+        const int tid = omp_get_thread_num();
+        auto ridx = static_cast<size_t>(batch.base_rowid + i);
+        RegTree::FVec& feats = thread_temp[tid];
+        feats.Fill(batch[i]);
+        for (unsigned j = 0; j < ntree_limit; ++j) {
+          int tid = model.trees[j]->GetLeafIndex(feats, info.GetRoot(ridx));
+          preds[ridx * ntree_limit + j] = static_cast<bst_float>(tid);
+        }
+        feats.Drop(batch[i]);
+      }
+    }
+  }
+    
+    TEST(c_api, XGDMatrixCreateFromMat_omp) {
+  std::vector<int> num_rows = {100, 11374, 15000};
+  for (auto row : num_rows) {
+    int num_cols = 50;
+    int num_missing = 5;
+    DMatrixHandle handle;
+    std::vector<float> data(num_cols * row, 1.5);
+    for (int i = 0; i < num_missing; i++) {
+      data[i] = std::numeric_limits<float>::quiet_NaN();
+    }
+    }
     }
     
-        CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 0));
-    CHECK(!secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
-    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 1));
-    CHECK(secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
-    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 2));
-    CHECK(!secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
-    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 3));
-    CHECK(!secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
+        Span<const float> s2 = arr;
+    SPAN_ASSERT_TRUE(s2.size() == 3, status_);
+    SPAN_ASSERT_TRUE(s2.data() == &arr[0], status_);
     
-    /// Format list of arguments to the stream according to the given format string.
-///
-/// The name vformat() is chosen for the semantic similarity to vprintf(): the
-/// list of format arguments is held in a single function argument.
-inline void vformat(std::ostream& out, const char* fmt, FormatListRef list)
-{
-    detail::formatImpl(out, fmt, list.m_formatters, list.m_N);
-}
-    
-    void InternalKeyComparator::FindShortestSeparator(
-      std::string* start,
-      const Slice& limit) const {
-  // Attempt to shorten the user portion of the key
-  Slice user_start = ExtractUserKey(*start);
-  Slice user_limit = ExtractUserKey(limit);
-  std::string tmp(user_start.data(), user_start.size());
-  user_comparator_->FindShortestSeparator(&tmp, user_limit);
-  if (tmp.size() < user_start.size() &&
-      user_comparator_->Compare(user_start, tmp) < 0) {
-    // User key has become shorter physically, but larger logically.
-    // Tack on the earliest possible number to the shortened user key.
-    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
-    assert(this->Compare(*start, tmp) < 0);
-    assert(this->Compare(tmp, limit) < 0);
-    start->swap(tmp);
-  }
-}
-    
-    namespace grpc {
+    namespace rocksdb {
     }
     
-    MeasureInt64 RpcServerReceivedMessagesPerRpc() {
-  static const auto measure =
-      MeasureInt64::Register(kRpcServerReceivedMessagesPerRpcMeasureName,
-                             'Number of messages received per RPC', kCount);
-  return measure;
-}
-    
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  host_cpu_load_info_data_t cpuinfo;
-  mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
-  if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO,
-                      (host_info_t)&cpuinfo, &count) == KERN_SUCCESS) {
-    for (int i = 0; i < CPU_STATE_MAX; i++) total += cpuinfo.cpu_ticks[i];
-    busy = total - cpuinfo.cpu_ticks[CPU_STATE_IDLE];
+      bool FilterMergeOperand(int level, const rocksdb::Slice& key,
+                          const rocksdb::Slice& existing_value) const override {
+    fprintf(stderr, 'FilterMerge(%s)\n', key.ToString().c_str());
+    ++merge_count_;
+    return existing_value == 'bad';
   }
-  return std::make_pair(busy, total);
-}
     
-    ///Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    
+    {  virtual ~Checkpoint() {}
 };
     
+    #include 'rocksdb/utilities/lua/rocks_lua_custom_library.h'
     
-int main() {
-    }
-    
-    
-    {    return 0;
+    /*
+ * Class:     org_rocksdb_BackupableDBOptions
+ * Method:    setShareTableFiles
+ * Signature: (JZ)V
+ */
+void Java_org_rocksdb_BackupableDBOptions_setShareTableFiles(JNIEnv* /*env*/,
+                                                             jobject /*jobj*/,
+                                                             jlong jhandle,
+                                                             jboolean flag) {
+  auto* bopt = reinterpret_cast<rocksdb::BackupableDBOptions*>(jhandle);
+  bopt->share_table_files = flag;
 }
-
-    
-    
-void printArr(const vector<int>& vec){
-    for(int e: vec)
-        cout << e << ' ';
-    cout << endl;
-}
-    
-    #include <iostream>
-    
-    
-// InOrder Morris Traversal
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(1)
-class Solution {
-    }
-    
-    /// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-    
-    int main() {
-    }
-    
-    #include <iostream>
-#include <vector>
-#include <stack>
-    
-    
-    
-      Object createObject() const;
-    
-    struct FileStreamWrapper final : Stream::Wrapper {
-  static req::ptr<MemFile> openFromCache(const String& filename,
-                                         const String& mode);
-  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-  int access(const String& path, int mode) override {
-    return ::access(File::TranslatePath(path).data(), mode);
-  }
-  int stat(const String& path, struct stat* buf) override {
-    return ::stat(File::TranslatePath(path).data(), buf);
-  }
-  int lstat(const String& path, struct stat* buf) override {
-    return ::lstat(File::TranslatePath(path).data(), buf);
-  }
-  int unlink(const String& path) override;
-  int rename(const String& oldname, const String& newname) override;
-  int mkdir(const String& path, int mode, int options) override;
-  int rmdir(const String& path, int /*options*/) override {
-    ERROR_RAISE_WARNING(::rmdir(File::TranslatePath(path).data()));
-    return ret;
-  }
-  bool isNormalFileStream() const override { return true; }
-    }
-    
-    template<typename AHM>
-void checkAHMSubMaps(const AHM& map, folly::StringPiece mapName,
-                     std::atomic<bool>& done) {
-  if (LIKELY(map.numSubMaps() == 1) ||
-      done.load(std::memory_order_relaxed) ||
-      done.exchange(true, std::memory_order_relaxed)) {
-    return;
-  }
-    }
-    
-    #ifndef incl_HPHP_PIPE_H_
-#define incl_HPHP_PIPE_H_
-    
-      // overriding ResourceData
-  const String& o_getClassNameHook() const override { return classnameof(); }
-    
-    class ExtensionMultiface : virtual public ExtensionIf {
- public:
-  ExtensionMultiface(std::vector<apache::thrift::stdcxx::shared_ptr<ExtensionIf> >& ifaces) : ifaces_(ifaces) {
-  }
-  virtual ~ExtensionMultiface() {}
- protected:
-  std::vector<apache::thrift::stdcxx::shared_ptr<ExtensionIf> > ifaces_;
-  ExtensionMultiface() {}
-  void add(::apache::thrift::stdcxx::shared_ptr<ExtensionIf> iface) {
-    ifaces_.push_back(iface);
-  }
- public:
-  void ping(ExtensionStatus& _return) {
-    size_t sz = ifaces_.size();
-    size_t i = 0;
-    for (; i < (sz - 1); ++i) {
-      ifaces_[i]->ping(_return);
-    }
-    ifaces_[i]->ping(_return);
-    return;
-  }
-    }
-    
-    int main(int argc, char **argv) {
-  int port = 9090;
-  ::apache::thrift::stdcxx::shared_ptr<ExtensionHandler> handler(new ExtensionHandler());
-  ::apache::thrift::stdcxx::shared_ptr<TProcessor> processor(new ExtensionProcessor(handler));
-  ::apache::thrift::stdcxx::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  ::apache::thrift::stdcxx::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  ::apache::thrift::stdcxx::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
-    }
-    
-    
-    {};
-    
-      uint32_t read(::apache::thrift::protocol::TProtocol* iprot);
-  uint32_t write(::apache::thrift::protocol::TProtocol* oprot) const;
-    
-      // Start looping through starting at the first options
-  // (so skip the exports)
-  for (auto iter = line.begin() + options_index; iter != line.end(); ++iter) {
-    if (iter->compare('-ro') == 0 || iter->compare('-o') == 0) {
-      readonly = 1;
-    }
-  }
-    
-    
-    {
-    {
-    {  return results;
-}
-}
-}
-
-    
-    namespace osquery {
-namespace table_tests {
-    }
-    }
-    
-      {
-    PinnableSlice pinnable_val;
-    db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key2', &pinnable_val);
-    assert(pinnable_val == 'value');
-  }
-    
-    // Supported only for Leveled compaction
-Status SuggestCompactRange(DB* db, ColumnFamilyHandle* column_family,
-                           const Slice* begin, const Slice* end);
-Status SuggestCompactRange(DB* db, const Slice* begin, const Slice* end);
-    
-      // Is cache storing uncompressed data ?
-  //
-  // True if the cache is configured to store uncompressed data else false
-  virtual bool IsCompressed() = 0;
-    
-        while ((n = (node_t*) swHeap_pop(pq)))
-    {
-        ASSERT_EQ(_map[n->val], n->pri);
-        free(n);
-    }
-    
-        inline void consumer_remove(Coroutine *co)
-    {
-        consumer_queue.remove(co);
-    }
-    
-    using namespace std;
-    
-    static void do_defer_tasks(swReactor *reactor)
-{
-    list<defer_task *> *tasks = (list<defer_task *> *) reactor->defer_tasks;
-    if (!tasks)
-    {
-        return;
-    }
-    reactor->defer_tasks = nullptr;
-    while (!tasks->empty())
-    {
-        defer_task *task = tasks->front();
-        tasks->pop_front();
-        task->callback(task->data);
-        delete task;
-    }
-    delete tasks;
-}
-    
-        unlink(sock1_path);
-    unlink(sock2_path);
-    
-        swSignalfd_init();
-    swSignal_add(SIGUSR1,sig_usr1);
-    swSignalfd_setup(SwooleG.main_reactor);
-    
-    [[noreturn]] void exception_wrapper::onNoExceptionError(
-    char const* const name) {
-  std::ios_base::Init ioinit_; // ensure std::cerr is alive
-  std::cerr << 'Cannot use `' << name
-            << '` with an empty folly::exception_wrapper' << std::endl;
-  std::terminate();
-}
-    
-    
-    {} // namespace folly
-
-    
-      /**
-   * Returns true 1/n of the time. If n == 0, always returns false
-   */
-  template <class RNG = ThreadLocalPRNG, class /* EnableIf */ = ValidRNG<RNG>>
-  static bool oneIn(uint32_t n, RNG&& rng) {
-    if (n == 0) {
-      return false;
-    }
-    return rand32(0, n, rng) == 0;
-  }
-    
-     public:
-  explicit VirtualExecutor(KeepAlive<> executor)
-      : executor_(std::move(executor)) {
-    assert(!isKeepAliveDummy(executor_));
-  }
-    
-      template <typename T>
-  static T* release_ptr(counted_ptr<T, Atom>& p) {
-    auto res = p.p_;
-    p.p_ = nullptr;
-    return res;
-  }
