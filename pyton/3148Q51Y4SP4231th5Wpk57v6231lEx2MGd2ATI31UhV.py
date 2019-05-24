98811b103358581ed3d018fcdac7b37f4fcaac77
SUNsung <weatherius@gmail.com>
2019-05-24 13:54:42 +0300

    Rewrite [3148Q51Y4SP4231th5Wpk57v6231lEx2MGd2ATI31UhV.py] File in: 2019-05-24 13:54:41

--- pyton/3148Q51Y4SP4231th5Wpk57v6231lEx2MGd2ATI31UhV.py
+++ pyton/3148Q51Y4SP4231th5Wpk57v6231lEx2MGd2ATI31UhV.py
@@ -1,230 +1,30 @@
 
         
-        
-@pytest.fixture(params=[(python_3, False),
-                        (python_3, True),
-                        (python_2, False)])
-def proc(request, spawnu, TIMEOUT):
-    container, instant_mode = request.param
-    proc = spawnu(*container)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(init_bashrc.format(
-        u'--enable-experimental-instant-mode' if instant_mode else ''))
-    proc.sendline(u'bash')
-    if instant_mode:
-        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
-    return proc
-    
-    
-init_zshrc = u'''echo '
-export SHELL=/usr/bin/zsh
-export HISTFILE=~/.zsh_history
-echo > $HISTFILE
-export SAVEHIST=100
-export HISTSIZE=100
-eval $(thefuck --alias {})
-setopt INC_APPEND_HISTORY
-echo 'instant mode ready: $THEFUCK_INSTANT_MODE'
-' > ~/.zshrc'''
-    
-    
-@pytest.mark.parametrize('command, new_command, packages', [
-    (Command('vim', ''), 'sudo apt-get install vim && vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('convert', ''), 'sudo apt-get install imagemagick && convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')]),
-    (Command('sudo vim', ''), 'sudo apt-get install vim && sudo vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('sudo convert', ''), 'sudo apt-get install imagemagick && sudo convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')])])
-def test_get_new_command(mocker, command, new_command, packages):
-    mocker.patch('thefuck.rules.apt_get._get_packages',
-                 create=True, return_value=packages)
-    
-    apt-get is a simple command line interface for downloading and
-installing packages. The most frequently used commands are update
-and install.
-    
-    
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_match(brew_no_available_formula, brew_already_installed,
-               brew_install_no_argument):
-    assert match(Command('brew install elsticsearch',
-                         brew_no_available_formula))
-    assert not match(Command('brew install git',
-                             brew_already_installed))
-    assert not match(Command('brew install', brew_install_no_argument))
-    
-    
-def main():
-    argument_spec = ec2_argument_spec()
-    argument_spec.update(
-        dict(
-            name=dict(required=True, type='str'),
-            state=dict(required=True, type='str', choices=['present', 'absent', 'copy']),
-            replication_id=dict(type='str'),
-            cluster_id=dict(type='str'),
-            target=dict(type='str'),
-            bucket=dict(type='str'),
-        )
-    )
-    
-        try:
-        region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
-        iam = boto3_conn(module, conn_type='client', resource='iam', region=region, endpoint=ec2_url, **aws_connect_kwargs)
-    except botocore.exceptions.ClientError as e:
-        module.fail_json(msg='Boto3 Client Error - ' + str(e.msg))
-    
-    
-def all_details(client, module):
-    '''
-    Returns all lambda related facts.
-    
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-    
-    RETURN = '''
-zone:
-  description: DNS zone as returned by IPA API.
-  returned: always
-  type: dict
-'''
-    
-        try:
-        client.login(username=module.params['ipa_user'],
-                     password=module.params['ipa_pass'])
-        changed, hbacrule = ensure(module, client)
-        module.exit_json(changed=changed, hbacrule=hbacrule)
-    except Exception as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-    
-        # If we're in check mode, just exit pretending like we succeeded
-    if module.check_mode:
-        module.exit_json(changed=True)
-    
-    
-def main():
-    global module
-    module = AnsibleModule(
-        argument_spec=dict(
-            device=dict(required=True, aliases=['host', 'name']),
-            landscape=dict(required=True),
-            state=dict(choices=['present', 'absent'], default='present'),
-            community=dict(required=True, no_log=True),
-            agentport=dict(type='int', default=161),
-            url=dict(required=True, aliases=['oneclick_url']),
-            url_username=dict(required=True, aliases=['oneclick_user']),
-            url_password=dict(required=True, no_log=True, aliases=['oneclick_password']),
-            use_proxy=dict(type='bool', default='yes'),
-            validate_certs=dict(type='bool', default='yes'),
-        ),
-        required_if=[('state', 'present', ['community'])],
-        supports_check_mode=True
-    )
-    
-    The JWS implementation in josepy only implements the base JOSE standard. In
-order to support the new header fields defined in ACME, this module defines some
-ACME-specific classes that layer on top of josepy.
-'''
-import josepy as jose
-    
-    # There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-#today = ''
-# Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
-    
-        '''
-    def __init__(self, *args, **kwargs):
-        super(AugeasConfigurator, self).__init__(*args, **kwargs)
-    
-    ALL_SSL_OPTIONS_HASHES = [
-    '2086bca02db48daf93468332543c60ac6acdb6f0b58c7bfdf578a5d47092f82a',
-    '4844d36c9a0f587172d9fa10f4f1c9518e3bcfa1947379f155e16a70a728c21a',
-    '5a922826719981c0a234b1fbcd495f3213e49d2519e845ea0748ba513044b65b',
-    '4066b90268c03c9ba0201068eaa39abbc02acf9558bb45a788b630eb85dadf27',
-    'f175e2e7c673bd88d0aff8220735f385f916142c44aa83b09f1df88dd4767a88',
-    'cfdd7c18d2025836ea3307399f509cfb1ebf2612c87dd600a65da2a8e2f2797b',
-    '80720bd171ccdc2e6b917ded340defae66919e4624962396b992b7218a561791',
-    'c0c022ea6b8a51ecc8f1003d0a04af6c3f2bc1c3ce506b3c2dfc1f11ef931082',
-]
-'''SHA256 hashes of the contents of previous versions of all versions of MOD_SSL_CONF_SRC'''
-    
-        @certbot_util.patch_get_utility()
-    def test_noninteractive(self, mock_util):
-        mock_util().menu.side_effect = errors.MissingCommandlineFlag('no vhost default')
-        try:
-            self._call(self.vhosts)
-        except errors.MissingCommandlineFlag as e:
-            self.assertTrue('vhost ambiguity' in str(e))
-    
-        def test_nonexistent_generic(self):
-        with mock.patch('certbot.util.get_os_info') as mock_info:
-            mock_info.return_value = ('nonexistent', 'irrelevant')
-            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
-                mock_like.return_value = ['unknonwn']
-                self.assertEqual(entrypoint.get_configurator(),
-                                 configurator.ApacheConfigurator)
-    
-    '''
-Dependency Injection (DI) is a technique whereby one object supplies the dependencies (services)
-to another object (client).
-It allows to decouple objects: no need to change client code simply because an object it depends on
-needs to be changed to a different one. (Open/Closed principle)
-    
-        def __repr__(self):
-        fmt = '<Price: {}, price after discount: {}>'
-        return fmt.format(self.price, self.price_after_discount())
-    
-    
-class BoldWrapper(TextTag):
-    '''Wraps a tag in <b>'''
-    
-    '''
-*TL;DR80
-Provides an interface to resource that is expensive to duplicate.
-'''
-    
-        def test_shall_toggle_from_fm_to_am(self):
-        self.radio.toggle_amfm()
-        state = self.radio.state.name
-        expected_state_name = 'AM'
-        self.assertEqual(state, expected_state_name)
+            try:
+        citext_oids = get_citext_oids(connection.alias)
+        array_type = psycopg2.extensions.new_array_type(citext_oids, 'citext[]', psycopg2.STRING)
+        psycopg2.extensions.register_type(array_type, None)
+    except ProgrammingError:
+        # citext is not available on the database.
+        #
+        # The same comments in the except block of the above call to
+        # register_hstore() also apply here.
+        pass
 
     
-            # Scenario: call is successful, validationMode is passed
-        create_payment_profile(self.customer_id, 'address', 'credit_card',
-                               'liveMode')
-        CreateRequest.assert_called_with(customerProfileId=self.customer_id,
-                                         paymentProfile=payment_profile,
-                                         validationMode='liveMode')
     
-            # Scenario: transaction_id < 0
-        return_value = refund_transaction(self.user, -transaction_id,
-                                          campaign_id, amount)
-        bid.refund.assert_called_once_with(amount)
-        self.assertEqual(return_value, (True, None))
+class Session(AbstractBaseSession):
+    '''
+    Django provides full support for anonymous sessions. The session
+    framework lets you store and retrieve arbitrary data on a
+    per-site-visitor basis. It stores data on the server side and
+    abstracts the sending and receiving of cookies. Cookies contain a
+    session ID -- not the data itself.
     
     
-class ModeratorPermissionSetTest(unittest.TestCase):
-    def test_loads(self):
-        self.assertTrue(ModeratorPermissionSet.loads(None).is_superuser())
-        self.assertFalse(ModeratorPermissionSet.loads('').is_superuser())
-    
-    
-class TestImgixResizer(RedditTestCase):
-    def setUp(self):
-        self.provider = ImgixImageResizingProvider()
-        self.patch_g(
-            imgix_domain='example.com',
-            imgix_signing=False,
-        )
-    
-            for width in (108, 216, 320, 640, 960, 1080):
-            url = self.provider.resize_image(image, width)
-            self.assertEqual(url, 'https://unsplash.it/%d/%d' % (width,
-                width*2))
+@html_safe
+class BoundWidget:
+    '''
+    A container class used for iterating over widgets. This is useful for
+    widgets that have choices. For example, the following can be used in a
+    template:
