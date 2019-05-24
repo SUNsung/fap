c8b9f041d17fe9b305368ae1fb462625eb3d41ee
SUNsung <weatherius@gmail.com>
2019-05-24 19:19:48 +0300

    Rewrite [37eEHXK1fE6VI37wWGC4gYiOs37i0eryjIsQAU37CJe.cpp] File in: 2019-05-24 19:19:48

--- cccccc/37eEHXK1fE6VI37wWGC4gYiOs37i0eryjIsQAU37CJe.cpp
+++ cccccc/37eEHXK1fE6VI37wWGC4gYiOs37i0eryjIsQAU37CJe.cpp
@@ -1,79 +1,109 @@
 
         
-        /*
- * True if line from a0->a1 intersects b0->b1
- */
-bool intersect(const b2Vec2& a0, const b2Vec2& a1,
-			   const b2Vec2& b0, const b2Vec2& b1) {
-	b2Vec2 myVec(0.0f,0.0f);
-	return intersect(a0, a1, b0, b1, myVec);
-}
+        #include 'include/secp256k1_ecdh.h'
+#include 'ecmult_const_impl.h'
     
-    	void EncodeMipmaps(float *a_pafSourceRGBA,
-		unsigned int a_uiSourceWidth,
-		unsigned int a_uiSourceHeight,
-		Image::Format a_format,
-		ErrorMetric a_eErrMetric,
-		float a_fEffort,
-		unsigned int a_uiJobs,
-		unsigned int a_uiMaxJobs,
-		unsigned int a_uiMaxMipmaps,
-		unsigned int a_uiMipFilterFlags,
-		RawImage* a_pMipmaps,
-		int *a_piEncodingTime_ms, bool a_bVerboseOutput = false);
+    int secp256k1_ecdsa_recover(const secp256k1_context* ctx, secp256k1_pubkey *pubkey, const secp256k1_ecdsa_recoverable_signature *signature, const unsigned char *msg32) {
+    secp256k1_ge q;
+    secp256k1_scalar r, s;
+    secp256k1_scalar m;
+    int recid;
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(secp256k1_ecmult_context_is_built(&ctx->ecmult_ctx));
+    ARG_CHECK(msg32 != NULL);
+    ARG_CHECK(signature != NULL);
+    ARG_CHECK(pubkey != NULL);
+    }
     
-      FT_LOCAL( void )
-  af_latin_metrics_scale( AF_LatinMetrics  metrics,
-                          AF_Scaler        scaler );
+    static int recovery_test_nonce_function(unsigned char *nonce32, const unsigned char *msg32, const unsigned char *key32, const unsigned char *algo16, void *data, unsigned int counter) {
+    (void) msg32;
+    (void) key32;
+    (void) algo16;
+    (void) data;
+    }
     
-    /** Change a 32-bit value into a 16-bit value. The value is assumed to fit in 16-bit, otherwise the result is undefined */
-#define EXTRACT16(x) ((opus_val16)(x))
-/** Change a 16-bit value into a 32-bit value */
-#define EXTEND32(x) ((opus_val32)(x))
+    // Error handling; calls assert() by default.
+#define TINYFORMAT_ERROR(reasonString) throw tinyformat::format_error(reasonString)
     
+      ASSERT_TRUE(ParseInternalKey(in, &decoded));
+  ASSERT_EQ(key, decoded.user_key.ToString());
+  ASSERT_EQ(seq, decoded.sequence);
+  ASSERT_EQ(vt, decoded.type);
     
-Extension_shutdown_result::~Extension_shutdown_result() throw() {
-}
+    #include <condition_variable>
+#include <mutex>
+#include <string>
+#include <system_error>
+#include <vector>
     
+    #include 'caffe2/operators/glu_op.h'
     
-  virtual ~Extension_shutdown_pargs() throw();
-    
-      virtual ~ExtensionManager_deregisterExtension_args() throw();
-  ExtensionRouteUUID uuid;
-    
-    using namespace  ::osquery::extensions;
-    
-    /**
- * @brief Represents a hardware driver type that SMART api can you use to query
- * device information.
- *
- * @param driver name of SMART controller driver
- * @param maxID max ID number of which disks on the controller is monitored
- */
-struct hardwareDriver {
-  std::string driver;
-  size_t maxID;
+    template <typename T, class Context>
+class BernoulliJSDOp final : public Operator<Context> {
+ public:
+  USE_SIMPLE_CTOR_DTOR(BernoulliJSDOp);
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  bool RunOnDevice() override;
 };
     
-      ASSERT_GE(data.size(), 0ul);
+    #include 'caffe/util/device_alternate.hpp'
     
-    TEST_F(keychainAcls, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from keychain_acls');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'keychain_path', NormalType}
-  //      {'authorizations', NormalType}
-  //      {'path', NormalType}
-  //      {'description', NormalType}
-  //      {'label', NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
-}
+    /**
+ * @brief Fills a Blob with values @f$ x \sim U(-a, +a) @f$ where @f$ a @f$ is
+ *        set inversely proportional to number of incoming nodes, outgoing
+ *        nodes, or their average.
+ *
+ * A Filler based on the paper [Bengio and Glorot 2010]: Understanding
+ * the difficulty of training deep feedforward neuralnetworks.
+ *
+ * It fills the incoming matrix by randomly sampling uniform data from [-scale,
+ * scale] where scale = sqrt(3 / n) where n is the fan_in, fan_out, or their
+ * average, depending on the variance_norm option. You should make sure the
+ * input blob has shape (num, a, b, c) where a * b * c = fan_in and num * b * c
+ * = fan_out. Note that this is currently not the case for inner product layers.
+ *
+ * TODO(dox): make notation in above comment consistent with rest & use LaTeX.
+ */
+template <typename Dtype>
+class XavierFiller : public Filler<Dtype> {
+ public:
+  explicit XavierFiller(const FillerParameter& param)
+      : Filler<Dtype>(param) {}
+  virtual void Fill(Blob<Dtype>* blob) {
+    CHECK(blob->count());
+    int fan_in = blob->count() / blob->shape(0);
+    // Compatibility with ND blobs
+    int fan_out = blob->num_axes() > 1 ?
+                  blob->count() / blob->shape(1) :
+                  blob->count();
+    Dtype n = fan_in;  // default to fan_in
+    if (this->filler_param_.variance_norm() ==
+        FillerParameter_VarianceNorm_AVERAGE) {
+      n = (fan_in + fan_out) / Dtype(2);
+    } else if (this->filler_param_.variance_norm() ==
+        FillerParameter_VarianceNorm_FAN_OUT) {
+      n = fan_out;
+    }
+    Dtype scale = sqrt(Dtype(3) / n);
+    caffe_rng_uniform<Dtype>(blob->count(), -scale, scale,
+        blob->mutable_cpu_data());
+    CHECK_EQ(this->filler_param_.sparse(), -1)
+         << 'Sparsity not supported by this Filler.';
+  }
+};
+    
+      virtual inline const char* type() const { return 'AbsVal'; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+    
+    #include <vector>
+    
+    #endif  // CAFFE_ARGMAX_LAYER_HPP_
+
+    
+    
+    {  Blob<Dtype> col_buffer_;
+  Blob<Dtype> bias_multiplier_;
+};
+    
+    #endif  // CAFFE_CUDNN_SIGMOID_LAYER_HPP_
