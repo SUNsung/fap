0d8c3fb8fb31afe25b6e32511732559f41415c5d
SUNsung <weatherius@gmail.com>
2019-05-24 15:50:22 +0300

    Rewrite [30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py] File in: 2019-05-24 15:50:21

--- pyton/30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py
+++ pyton/30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py
@@ -1,110 +1,128 @@
 
         
-        
-if __name__ == '__main__':
-    main()
-
+        import io
+import optparse
+import os
+import sys
     
-    match_output = '''
-Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
-Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
-Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
-Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
-Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
-Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
-Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
-Fetched 89.2 kB in 0s (122 kB/s)
-Reading package lists... Done
-Building dependency tree
-Reading state information... Done
-8 packages can be upgraded. Run 'apt list --upgradable' to see them.
+        with open(ZSH_COMPLETION_FILE, 'w') as f:
+        f.write(template)
+    
+        def debug(self, msg):
+        pass
+    
+    
+def unicode_is_ascii(u_string):
+    '''Determine if unicode string only contains ASCII characters.
+    
+    
+class CaseInsensitiveDict(MutableMapping):
+    '''A case-insensitive ``dict``-like object.
+    
+        def inner(*suffix):
+        return urljoin(httpbin_url, '/'.join(suffix))
+    
+        :param pool_connections: The number of urllib3 connection pools to cache.
+    :param pool_maxsize: The maximum number of connections to save in the pool.
+    :param max_retries: The maximum number of retries each connection
+        should attempt. Note, this applies only to failed DNS lookups, socket
+        connections and connection timeouts, never to requests where data has
+        made it to the server. By default, Requests does not retry failed
+        connections. If you need granular control over the conditions under
+        which we retry a request, import urllib3's ``Retry`` class and pass
+        that instead.
+    :param pool_block: Whether the connection pool should block for connections.
+    
+    This module handles import compatibility issues between Python 2 and
+Python 3.
 '''
     
+                # the (n+1)th request fails
+            with pytest.raises(requests.exceptions.ConnectionError):
+                r = requests.get(server_url)
     
-Invalid choice: 'scn', maybe you meant:
+    Some codes have multiple names, and both upper- and lower-case versions of
+the names are allowed. For example, ``codes.ok``, ``codes.OK``, and
+``codes.okay`` all correspond to the HTTP status code 200.
+'''
+    
+        def test_cookie_removed_on_expire(self, httpbin):
+        s = requests.session()
+        s.get(httpbin('cookies/set?foo=bar'))
+        assert s.cookies['foo'] == 'bar'
+        s.get(
+            httpbin('response-headers'),
+            params={
+                'Set-Cookie':
+                    'foo=deleted; expires=Thu, 01-Jan-1970 00:00:01 GMT'
+            }
+        )
+        assert 'foo' not in s.cookies
+    
+    from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import boto3_conn, camel_dict_to_snake_dict
+from ansible.module_utils.ec2 import ec2_argument_spec, get_aws_connection_info
+    
+            if state == 'absent' and user in heroku_collaborator_list:
+            if not module.check_mode:
+                heroku_app.remove_collaborator(user)
+            affected_apps += [app]
+            result_state = True
+        elif state == 'present' and user not in heroku_collaborator_list:
+            if not module.check_mode:
+                heroku_app.add_collaborator(user_id_or_email=user, silent=module.params['suppress_invitation'])
+            affected_apps += [app]
+            result_state = True
+    
+        vca = vca_login(module)
+    
+    EXAMPLES = '''
+# Ensure role is present
+- ipa_role:
+    name: dba
+    description: Database Administrators
+    state: present
+    user:
+    - pinky
+    - brain
+    ipa_host: ipa.example.com
+    ipa_user: admin
+    ipa_pass: topsecret
+    
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+    
+    # Copyright 2014 Benjamin Curtis <benjamin.curtis@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+    
+    RETURN = '''
+#
+'''
+    
+        def load_word_tag(self, f):
+        self.word_tag_tab = {}
+        f_name = resolve_filename(f)
+        for lineno, line in enumerate(f, 1):
+            try:
+                line = line.strip().decode('utf-8')
+                if not line:
+                    continue
+                word, _, tag = line.split(' ')
+                self.word_tag_tab[word] = tag
+            except Exception:
+                raise ValueError(
+                    'invalid POS dictionary entry in %s at Line %s: %s' % (f_name, lineno, line))
+        f.close()
     
     
-@pytest.mark.parametrize('script, output', [
-    ('brew link sshfs', output),
-    ('cat output', output),
-    ('brew install sshfs', '')])
-def test_not_match(script, output):
-    command = Command(script, output)
-    assert not match(command)
+def get_top_states(t_state_v, K=4):
+    return sorted(t_state_v, key=t_state_v.__getitem__, reverse=True)[:K]
     
+    content = open(file_name, 'rb').read()
     
-@pytest.mark.parametrize('command', [
-    Command('cargo buid', no_such_subcommand_old),
-    Command('cargo buils', no_such_subcommand)])
-def test_match(command):
-    assert match(command)
-    
-    remove_dups(a1)
-print_linked_list(a1)
-remove_dups_wothout_set(a1)
-print_linked_list(a1)
+    print(','.join(tags))
 
     
-            assert self.CONTROLLER is not None
     
-        def test_no_body_signing_register(self):
-        res = self.do_login(
-            headers={
-                signing.SIGNATURE_BODY_HEADER: None,
-            },
-            expect_errors=True,
-        )
-        self.assert_403_response(res, 'signing.body.invalid.invalid_format')
-    
-            # Give ourselves enough users that we can get some reasonable amount of
-        # precision when checking amounts per bucket.
-        NUM_USERS = FeatureState.NUM_BUCKETS * 2000
-        fullnames = []
-        for i in xrange(NUM_USERS):
-            fullnames.append('t2_%s' % str(i))
-    
-        def test_float(self):
-        self.assertEquals(3.0, ConfigValue.float('3'))
-        self.assertEquals(-3.0, ConfigValue.float('-3'))
-        with self.assertRaises(ValueError):
-            ConfigValue.float('asdf')
-    
-    
-class TestModuleGetFlattenedSources(unittest.TestCase):
-    def test_flat_modules_include_all_sources(self):
-        test_files = ['foo.js', 'bar.js', 'baz.js', 'qux.js']
-        test_module = TestModule('test_module', *test_files)
-        self.assertEqual(test_module.build(), concat_sources(test_files))
-    
-        def test_censor(self):
-        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
-                      height=800)
-        url = self.provider.resize_image(image, censor_nsfw=True)
-        self.assertEqual(url, 'https://example.com/a.jpg?blur=600&px=32')
-
-    
-    
-class TestLocalResizer(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.provider = NoOpImageResizingProvider()
-    
-        def test_invalid_header(self):
-        body = '{'user': 'reddit', 'password': 'hunter2'}'
-        platform = 'test'
-        version = 1
-        header = '1:%s:%s:deadbeef' % (platform, version)
-        self.assert_invalid(
-            body,
-            header=header,
-            error=signing.ERRORS.UNPARSEABLE,
-            global_version=1,
-        )
-    
-    
-MESSAGE = 'the quick brown fox jumped over...'
-BLOCK_O_PADDING = ('\x10\x10\x10\x10\x10\x10\x10\x10'
-                   '\x10\x10\x10\x10\x10\x10\x10\x10')
-SECRET = 'abcdefghijklmnopqrstuvwxyz'
-ENCRYPTED = ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaIbzth1QTzJxzHbHGnJywG5V1uR3tWtSB'
-             '8hTyIcfg6rUZC4Wo0pT8jkEt9o1c%2FkTn')
+t0 = time.time()
+print('training...')
