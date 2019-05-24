fb7630b67b86325b5be96b9f131d984555e537a3
SUNsung <weatherius@gmail.com>
2019-05-24 11:29:55 +0300

    Rewrite [29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp] File in: 2019-05-24 11:29:54

--- cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
+++ cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
@@ -1,338 +1,235 @@
 
         
-          image_file.read(reinterpret_cast<char*>(&magic), 4);
-  magic = swap_endian(magic);
-  CHECK_EQ(magic, 2051) << 'Incorrect image file magic.';
-  label_file.read(reinterpret_cast<char*>(&magic), 4);
-  magic = swap_endian(magic);
-  CHECK_EQ(magic, 2049) << 'Incorrect label file magic.';
-  image_file.read(reinterpret_cast<char*>(&num_items), 4);
-  num_items = swap_endian(num_items);
-  label_file.read(reinterpret_cast<char*>(&num_labels), 4);
-  num_labels = swap_endian(num_labels);
-  CHECK_EQ(num_items, num_labels);
-  image_file.read(reinterpret_cast<char*>(&rows), 4);
-  rows = swap_endian(rows);
-  image_file.read(reinterpret_cast<char*>(&cols), 4);
-  cols = swap_endian(cols);
+        
+    {private:
+    Ui::OpenURIDialog *ui;
+};
     
-     private:
-  // The private constructor to avoid duplicate instantiation.
-  Caffe();
     
-    #endif  // CAFFE_DATA_LAYERS_HPP_
+    {}
+    
+    void test_bad_scalar(void) {
+    unsigned char s_zero[32] = { 0 };
+    unsigned char s_overflow[32] = {
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe,
+        0xba, 0xae, 0xdc, 0xe6, 0xaf, 0x48, 0xa0, 0x3b,
+        0xbf, 0xd2, 0x5e, 0x8c, 0xd0, 0x36, 0x41, 0x41
+    };
+    unsigned char s_rand[32] = { 0 };
+    unsigned char output[32];
+    secp256k1_scalar rand;
+    secp256k1_pubkey point;
+    }
+    
+        /* Check bad contexts and NULLs for recovery */
+    ecount = 0;
+    CHECK(secp256k1_ecdsa_recover(none, &recpubkey, &recsig, message) == 0);
+    CHECK(ecount == 1);
+    CHECK(secp256k1_ecdsa_recover(sign, &recpubkey, &recsig, message) == 0);
+    CHECK(ecount == 2);
+    CHECK(secp256k1_ecdsa_recover(vrfy, &recpubkey, &recsig, message) == 1);
+    CHECK(ecount == 2);
+    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, message) == 1);
+    CHECK(ecount == 2);
+    CHECK(secp256k1_ecdsa_recover(both, NULL, &recsig, message) == 0);
+    CHECK(ecount == 3);
+    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, NULL, message) == 0);
+    CHECK(ecount == 4);
+    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, NULL) == 0);
+    CHECK(ecount == 5);
+    
+    
+// Detect when a type is not a wchar_t string
+template<typename T> struct is_wchar { typedef int tinyformat_wchar_is_not_supported; };
+template<> struct is_wchar<wchar_t*> {};
+template<> struct is_wchar<const wchar_t*> {};
+template<int n> struct is_wchar<const wchar_t[n]> {};
+template<int n> struct is_wchar<wchar_t[n]> {};
+    
+        UniValue v3;
+    BOOST_CHECK(v3.setNumStr('32482348723847471234'));
+    BOOST_CHECK_THROW(v3.get_int64(), std::runtime_error);
+    BOOST_CHECK(v3.setNumStr('1000'));
+    BOOST_CHECK_EQUAL(v3.get_int64(), 1000);
+    
+    // Bech32 is a string encoding format used in newer address types.
+// The output consists of a human-readable part (alphanumeric), a
+// separator character (1), and a base32 data section, the last
+// 6 characters of which are a checksum.
+//
+// For more information, see BIP 173.
+    
+    /** A hasher class for RIPEMD-160. */
+class CRIPEMD160
+{
+private:
+    uint32_t s[5];
+    unsigned char buf[64];
+    uint64_t bytes;
+    }
+    
+    void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
+                                        std::string* dst) const {
+  // We rely on the fact that the code in table.cc does not mind us
+  // adjusting keys[].
+  Slice* mkey = const_cast<Slice*>(keys);
+  for (int i = 0; i < n; i++) {
+    mkey[i] = ExtractUserKey(keys[i]);
+    // TODO(sanjay): Suppress dups?
+  }
+  user_policy_->CreateFilter(keys, n, dst);
+}
+    
+      // Return an internal key (suitable for passing to an internal iterator)
+  Slice internal_key() const { return Slice(kstart_, end_ - kstart_); }
+    
+      dict->lookup('Size', &obj1);
+  if (obj1.isArray() && obj1.arrayGetLength() == 2) {
+    obj1.arrayGet(0, &obj2);
+    width = obj2.getNum();
+    obj2.free();
+    obj1.arrayGet(1, &obj2);
+    height = obj2.getNum();
+    obj2.free();
+    writePSFmt('%%ImageDimensions: {0:.6g} {1:.6g}\n', width, height);
+  }
+  obj1.free();
+    
+    
+    {  PDFRectangle mediaBox;
+  PDFRectangle cropBox;
+  GBool haveCropBox;
+  PDFRectangle bleedBox;
+  PDFRectangle trimBox;
+  PDFRectangle artBox;
+  int rotate;
+  Object lastModified;
+  Object boxColorInfo;
+  Object group;
+  Object metadata;
+  Object pieceInfo;
+  Object separationInfo;
+  Object resources;
+};
+    
+    
+    {  GooList intervals;
+};
 
     
-    /**
- * @brief Takes at least two Blob%s and concatenates them along either the num
- *        or channel dimension, outputting the result.
- */
-template <typename Dtype>
-class ConcatLayer : public Layer<Dtype> {
- public:
-  explicit ConcatLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    #include 'caffe/layers/loss_layer.hpp'
-    
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #ifdef USE_CUDNN
-/*
- * @brief cuDNN implementation of ConvolutionLayer.
- *        Fallback to ConvolutionLayer for CPU mode.
- *
- * cuDNN accelerates convolution through forward kernels for filtering and bias
- * plus backward kernels for the gradient w.r.t. the filters, biases, and
- * inputs. Caffe + cuDNN further speeds up the computation through forward
- * parallelism across groups and backward parallelism across gradients.
- *
- * The CUDNN engine does not have memory overhead for matrix buffers. For many
- * input and filter regimes the CUDNN engine is faster than the CAFFE engine,
- * but for fully-convolutional models and large inputs the CAFFE engine can be
- * faster as long as it fits in memory.
-*/
-template <typename Dtype>
-class CuDNNConvolutionLayer : public ConvolutionLayer<Dtype> {
- public:
-  explicit CuDNNConvolutionLayer(const LayerParameter& param)
-      : ConvolutionLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNConvolutionLayer();
-    }
-    
-      std::string Key(int i) {
-    char buf[100];
-    snprintf(buf, sizeof(buf), 'key%06d', i);
-    return std::string(buf);
+      // skip over stream data
+  if (Lexer::LOOK_VALUE_NOT_CACHED != lexer->lookCharLastValueCached) {
+      // take into account the fact that we've cached one value
+      pos = pos - 1;
+      lexer->lookCharLastValueCached = Lexer::LOOK_VALUE_NOT_CACHED;
   }
+  lexer->setPos(pos + length);
     
-    char* leveldb_env_get_test_directory(leveldb_env_t* env) {
-  std::string result;
-  if (!env->rep->GetTestDirectory(&result).ok()) {
-    return nullptr;
-  }
-    }
     
-    #include 'db/log_format.h'
-#include 'leveldb/slice.h'
-#include 'leveldb/status.h'
-    
-        if (!status.ok()) {
-      env_->DeleteFile(tmp);
-    } else {
-      // Discard older manifests
-      for (size_t i = 0; i < manifests_.size(); i++) {
-        ArchiveFile(dbname_ + '/' + manifests_[i]);
+    {
+    {
+    {      for (int j = i; j > 0; j--) {
+        keys[j] = keys[j - 1];
+        items[j] = items[j - 1];
       }
+      
+      keys[0] = keyHit;
+      items[0] = itemHit;
+      return itemHit;
+    }
+  }
+  return 0;
+}
+    
+    class PopplerObjectCache
+{
+  public:
+    PopplerObjectCache (int cacheSizeA, XRef *xrefA);
+    ~PopplerObjectCache();
     }
     
-      static Iterator* BlockReader(void*, const ReadOptions&, const Slice&);
     
-      // Finish building the block and return a slice that refers to the
-  // block contents.  The returned slice will remain valid for the
-  // lifetime of this builder or until Reset() is called.
-  Slice Finish();
-    
-            static const size_t s_serializationVersion = 0;
-    
-                if ((numSequences > 1) && (currentSequenceData->Device() != DeviceDescriptor::CPUDevice()))
-                InvalidArgument('Value::Create: All NDArrayView objects must be located on the CPU');
-    
-            // Note: We do not clone m_blockFunctionVariableMapping
-        auto clone = MakeSharedObject<VariableFields>(m_shape,
-            m_varKind,
-            m_dataType,
-            m_ownerFunction,
-            (m_value) ? m_value->DeepClone() : nullptr,
-            m_needsGradient,
-            m_dynamicAxes,
-            m_isSparse,
-            m_name,
-            Internal::GenerateUid(m_varKind));
-    
-        // this returns the map directly (read-only) and will lazily initialize it for a given seed
-    const std::vector<INDEXTYPE>& operator()(size_t seed) // throw()
-    {
-        // if wrong seed then lazily recache the sequence
-        if (seed != currentseed && randomizationrange != randomizeDisable)
-        {
-            // test for numeric overflow
-            if (map.size() - 1 != (INDEXTYPE)(map.size() - 1))
-                RuntimeError('RandomOrdering: INDEXTYPE has too few bits for this corpus');
-            // 0, 1, 2...
-            foreach_index (t, map)
-                map[t] = (INDEXTYPE) t;
-    }
-    }
-    
-    public:
-    ScopeTimer(size_t verbosity, const std::string& message)
-        : m_verbosity(verbosity), m_message(message)
-    {
-        if (m_verbosity > 2)
-        {
-            m_aggregateTimer.Start();
-        }
-    }
-    
-    #include <unordered_set>
-#include <map>
-#include <string>
-#include <vector>
-#include <stdexcept>
-#include <list>
-#include <memory>
-#include <algorithm>
-#include <utility>
-#include <assert.h>
-    
-     private:
-  StreamBufferReader reader_;
-  int tmp_ch;
-  int num_prev;
-  unsigned char buf_prev[2];
-  // whether we need to do strict check
-  static const bool kStrictCheck = false;
+    {  void addElement (double elapsed);
+  int getCount () { return count; }
+  double getTotal () { return total; }
+  double getMin () { return max; }
+  double getMax () { return max; }
+private:
+  int count;			// size of <elems> array
+  double total;			// number of elements in array
+  double min;			// reference count
+  double max;			// reference count
 };
-/*! \brief the stream that write to base64, note we take from file pointers */
-class Base64OutStream: public dmlc::Stream {
- public:
-  explicit Base64OutStream(dmlc::Stream *fp) : fp(fp) {
-    buf_top = 0;
-  }
-  virtual void Write(const void *ptr, size_t size) {
-    using base64::EncodeTable;
-    size_t tlen = size;
-    const unsigned char *cptr = static_cast<const unsigned char*>(ptr);
-    while (tlen) {
-      while (buf_top < 3  && tlen != 0) {
-        buf[++buf_top] = *cptr++; --tlen;
-      }
-      if (buf_top == 3) {
-        // flush 4 bytes out
-        PutChar(EncodeTable[buf[1] >> 2]);
-        PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
-        PutChar(EncodeTable[((buf[2] << 2) | (buf[3] >> 6)) & 0x3F]);
-        PutChar(EncodeTable[buf[3] & 0x3F]);
-        buf_top = 0;
-      }
-    }
-  }
-  virtual size_t Read(void *ptr, size_t size) {
-    LOG(FATAL) << 'Base64OutStream do not support read';
-    return 0;
-  }
-  /*!
-   * \brief finish writing of all current base64 stream, do some post processing
-   * \param endch character to put to end of stream, if it is EOF, then nothing will be done
-   */
-  inline void Finish(char endch = EOF) {
-    using base64::EncodeTable;
-    if (buf_top == 1) {
-      PutChar(EncodeTable[buf[1] >> 2]);
-      PutChar(EncodeTable[(buf[1] << 4) & 0x3F]);
-      PutChar('=');
-      PutChar('=');
-    }
-    if (buf_top == 2) {
-      PutChar(EncodeTable[buf[1] >> 2]);
-      PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
-      PutChar(EncodeTable[(buf[2] << 2) & 0x3F]);
-      PutChar('=');
-    }
-    buf_top = 0;
-    if (endch != EOF) PutChar(endch);
-    this->Flush();
-  }
     
-    /*! \brief interface of objective function */
-class ObjFunction {
- public:
-  /*! \brief virtual destructor */
-  virtual ~ObjFunction() = default;
-  /*!
-   * \brief set configuration from pair iterators.
-   * \param begin The beginning iterator.
-   * \param end The end iterator.
-   * \tparam PairIter iterator<std::pair<std::string, std::string> >
-   */
-  template<typename PairIter>
-  inline void Configure(PairIter begin, PairIter end);
-  /*!
-   * \brief Configure the objective with the specified parameters.
-   * \param args arguments to the objective function.
-   */
-  virtual void Configure(const std::vector<std::pair<std::string, std::string> >& args) = 0;
-  /*!
-   * \brief Get gradient over each of predictions, given existing information.
-   * \param preds prediction of current round
-   * \param info information about labels, weights, groups in rank
-   * \param iteration current iteration number.
-   * \param out_gpair output of get gradient, saves gradient and second order gradient in
-   */
-  virtual void GetGradient(const HostDeviceVector<bst_float>& preds,
-                           const MetaInfo& info,
-                           int iteration,
-                           HostDeviceVector<GradientPair>* out_gpair) = 0;
+      GooString* getContentType() { return contentType; }
+  GooString* getFileName() { return fileName; }
+    
+    /**
+ * @brief Intel HECI GUID for the update application.
+ *
+ * This GUID can be used on every OS over the HECI protocol to inspect the
+ * FW version.
+ */
+const std::vector<uint8_t> kMEIUpdateGUID{
+    232, 205, 157, 48, 177, 204, 98, 64, 143, 120, 96, 1, 21, 163, 67, 39,
+};
+    
+    namespace osquery {
+namespace table_tests {
+    }
     }
     
-      void Write(const SparsePage& page, dmlc::Stream* fo) override {
-    const auto& offset_vec = page.offset.HostVector();
-    const auto& data_vec = page.data.HostVector();
-    CHECK(offset_vec.size() != 0 && offset_vec[0] == 0);
-    CHECK_EQ(offset_vec.back(), data_vec.size());
-    fo->Write(offset_vec);
-    min_index_ = page.base_rowid;
-    fo->Write(&min_index_, sizeof(min_index_));
-    index_.data.resize(data_vec.size());
-    value_.data.resize(data_vec.size());
-    }
     
-    bool DHTRoutingTable::addNode(const std::shared_ptr<DHTNode>& node, bool good)
-{
-  A2_LOG_DEBUG(fmt('Trying to add node:%s', node->toString().c_str()));
-  if (*localNode_ == *node) {
-    A2_LOG_DEBUG('Adding node with the same ID with localnode is not allowed.');
-    return false;
-  }
-  auto treeNode = dht::findTreeNodeFor(root_.get(), node->getID());
-  while (1) {
-    const std::shared_ptr<DHTBucket>& bucket = treeNode->getBucket();
-    if (bucket->addNode(node)) {
-      A2_LOG_DEBUG('Added DHTNode.');
-      return true;
-    }
-    else if (bucket->splitAllowed()) {
-      A2_LOG_DEBUG(fmt('Splitting bucket. Range:%s-%s',
-                       util::toHex(bucket->getMinID(), DHT_ID_LENGTH).c_str(),
-                       util::toHex(bucket->getMaxID(), DHT_ID_LENGTH).c_str()));
-      treeNode->split();
-      ++numBucket_;
-      if (treeNode->getLeft()->isInRange(node->getID())) {
-        treeNode = treeNode->getLeft();
-      }
-      else {
-        treeNode = treeNode->getRight();
-      }
-    }
-    else {
-      if (good) {
-        bucket->cacheNode(node);
-        A2_LOG_DEBUG(fmt('Cached node=%s', node->toString().c_str()));
-      }
-      return false;
-    }
-  }
-  return false;
+    {
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    TEST(ByteTest, SetGetHighLowBit) {
+  unsigned char byte_value = 0x37;
+  Byte value(&byte_value);
+  value.set_value_high_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
+  value.set_value_low_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
 }
     
-      int numBucket_;
+    namespace apollo {
+namespace drivers {
+namespace conti_radar {
+    }
+    }
+    }
     
-    #include <memory>
+    void ObjectQualityInfo60C::Parse(const std::uint8_t* bytes, int32_t length,
+                                 ContiRadar* conti_radar) const {
+  int obj_id = object_id(bytes, length);
+    }
     
-    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
-                                           const std::string& ipaddr,
-                                           uint16_t port) const
-{
-  return generateToken(infoHash, ipaddr, port, secret_[0]);
+      MatrixXd mat_golden(10, 10);
+  // clang-format off
+  mat_golden <<
+     20, -15,   6,  -1,   0,   0,   0,   0,   0,   0,
+    -15,  20, -15,   6,  -1,   0,   0,   0,   0,   0,
+      6, -15,  20, -15,   6,  -1,   0,   0,   0,   0,
+     -1,   6, -15,  20, -15,   6,  -1,   0,   0,   0,
+      0,  -1,   6, -15,  20, -15,   6,  -1,   0,   0,
+      0,   0,  -1,   6, -15,  20, -15,   6,  -1,   0,
+      0,   0,   0,  -1,   6, -15,  20, -15,   6,  -1,
+      0,   0,   0,   0,  -1,   6, -15,  19, -12,   3,
+      0,   0,   0,   0,   0,  -1,   6, -12,  10,  -3,
+      0,   0,   0,   0,   0,   0,  -1,   3,  -3,   1;
+  // clang-format on
+  EXPECT_EQ(mat, mat_golden);
+    
+    #include 'modules/canbus/vehicle/gem/gem_message_manager.h'
+    
+    
+    {  double ret = x * 0.001000;
+  return ret;
 }
-    
-      DHTTokenTracker(const unsigned char* initialSecret);
-    
-    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
-                                             std::chrono::seconds interval)
-    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
-{
-}
-    
-      virtual ~DHTTokenUpdateCommand();
-    
-    std::string DHTUnknownMessage::toString() const
-{
-  size_t sampleLength = 8;
-  if (length_ < sampleLength) {
-    sampleLength = length_;
-  }
-  return fmt('dht unknown Remote:%s(%u) length=%lu, first 8 bytes(hex)=%s',
-             ipaddr_.c_str(), port_, static_cast<unsigned long>(length_),
-             util::toHex(data_, sampleLength).c_str());
-}
-    
-    DNSCache::AddrEntry::AddrEntry(const AddrEntry& c) = default;
