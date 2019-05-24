d90b3dc5aa3e4824a9814fe80f0a8e6eb9c4c7ac
SUNsung <weatherius@gmail.com>
2019-05-24 08:00:39 +0300

    Rewrite [53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py] File in: 2019-05-24 08:00:38

--- pyton/53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py
+++ pyton/53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py
@@ -1,129 +1,35 @@
 
         
-        
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
-def delete(id):
-    '''Delete a post.
-    
-        # close and remove the temporary database
-    os.close(db_fd)
-    os.unlink(db_path)
-    
-    
-def test_logout(client, auth):
-    auth.login()
-    
-    
-# context locals
-_request_ctx_stack = LocalStack()
-_app_ctx_stack = LocalStack()
-current_app = LocalProxy(_find_app)
-request = LocalProxy(partial(_lookup_req_object, 'request'))
-session = LocalProxy(partial(_lookup_req_object, 'session'))
-g = LocalProxy(partial(_lookup_app_object, 'g'))
-
-    
-        #: If matching the URL failed, this is the exception that will be
-    #: raised / was raised as part of the request handling.  This is
-    #: usually a :exc:`~werkzeug.exceptions.NotFound` exception or
-    #: something similar.
-    routing_exception = None
-    
-        set_init_version(version)
-    make_git_commit('Bump version number to %s', version)
-    make_git_tag(version)
-    build()
-    set_init_version(dev_version)
-    
-    
-@pytest.fixture(autouse=True)
-def leak_detector():
-    yield
-    
-    if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-    
-        # Arguments
-        l1: Float; L1 regularization factor.
-        l2: Float; L2 regularization factor.
+                if n_dim == 2:
+            return tf.nn.softmax(x)
+        else:
+            e = tf.exp(x - tf.reduce_max(x, axis=axis, keepdims=True))
+            s = tf.reduce_sum(e, axis=axis, keepdims=True)
+            return e / s
     '''
+    return tf.nn.softmax(x, axis=axis)
+    
+        return o
     
     
-def test_fashion_mnist():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-        assert len(x_train) == len(y_train) == 60000
-        assert len(x_test) == len(y_test) == 10000
     
-    if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-    
-    import numpy as np
-import keras
-from keras.datasets import reuters
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.preprocessing.text import Tokenizer
-    
-    - Klambauer, G., Unterthiner, T., Mayr, A., & Hochreiter, S. (2017).
-  Self-Normalizing Neural Networks. arXiv preprint arXiv:1706.02515.
-  https://arxiv.org/abs/1706.02515
+    sum_ngrams = 0
+for s in sentences:
+    for w in s:
+        w = w.lower()
+        # from gensim.models._utils_any2vec import compute_ngrams
+        ret = compute_ngrams(w, min_ngrams, max_ngrams)
+        print(ret)
+        sum_ngrams += len(ret)
 '''
-from __future__ import print_function
-    
-    
-def mean_squared_error(y_true, y_pred):
-    return K.mean(K.square(y_pred - y_true), axis=-1)
-    
-        for n_components in [i.astype(int) for i in
-                         np.linspace(data.shape[1] // 10,
-                                     data.shape[1], num=4)]:
-        all_times = defaultdict(list)
-        all_errors = defaultdict(list)
-        pca = PCA(n_components=n_components)
-        rpca = PCA(n_components=n_components, svd_solver='randomized',
-                   random_state=1999)
-        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
-                                                               ('rpca', rpca)]}
-    
-        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
-    plt.plot(sample_sizes, one_core, label='one core')
-    plt.plot(sample_sizes, multi_core, label='multi core')
-    plt.xlabel('n_samples')
-    plt.ylabel('Time (s)')
-    plt.title('Parallel %s' % func.__name__)
-    plt.legend()
-    
-                # Shuffle data
-            idx = np.arange(n_train)
-            np.random.seed(13)
-            np.random.shuffle(idx)
-            X_train = X_train[idx]
-            y_train = y_train[idx]
-    
-    plt.matshow(fit_data, cmap=plt.cm.Blues)
-plt.title('After biclustering; rearranged to show biclusters')
-    
-    np.random.seed(5)
-    
-    plt.show()
-
-    
-    import numpy as np
-import matplotlib.pyplot as plt
+['<h', 'he', 'el', 'll', 'lo', 'o>', '<he', 'hel', 'ell', 'llo', 'lo>', '<hel', 'hell', 'ello', 'llo>']
+['<w', 'wo', 'or', 'rl', 'ld', 'd>', '<wo', 'wor', 'orl', 'rld', 'ld>', '<wor', 'worl', 'orld', 'rld>']
+['<!', '!>', '<!>']
+['<i', 'i>', '<i>']
+['<a', 'am', 'm>', '<am', 'am>', '<am>']
+['<h', 'hu', 'ua', 'ay', 'y>', '<hu', 'hua', 'uay', 'ay>', '<hua', 'huay', 'uay>']
+['<.', '.>', '<.>']
+'''
+assert sum_ngrams == len(model.wv.vectors_ngrams)
+print(sum_ngrams)  # 57
+print()
