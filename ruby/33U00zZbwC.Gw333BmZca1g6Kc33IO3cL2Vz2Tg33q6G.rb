ebea97a7b50edd3d12e76e7835c3b737318d29c8
SUNsung <weatherius@gmail.com>
2019-05-25 16:16:10 +0300

    Rewrite [33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb] File in: 2019-05-25 16:16:09

--- ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
+++ ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
@@ -1,56 +1,19 @@
 
         
-                  Gitlab::Git
-            .diff_line_code(file_path, diff_line.new_pos, diff_line.old_pos)
-        end
-    
-            css('br', 'hr', '.material-icons', '.header-link', '.breadcrumb').remove
-    
-            if mod
-          if name == 'Index'
-            return slug.split('/')[1..-2].join('/')
-          elsif name == 'Angular'
-            return slug.split('/').last.split('-').first
-          end
-        end
-    
-      def show
-    if subscription.valid?(params['hub.topic'])
-      @account.update(subscription_expires_at: future_expires)
-      render plain: encoded_challenge, status: 200
-    else
-      head 404
+            it 'accepts jsfiddle link with a custom-tab parameter' do
+      expect do
+        generate_new_liquid(jsfiddle_link_with_custom_tabs)
+      end.not_to raise_error
     end
+    
+      def permitted_attributes_for_update
+    %i[body_markdown receive_notifications]
   end
     
-          weeks << {
-        week: week.to_time.to_i.to_s,
-        statuses: Redis.current.get('activity:statuses:local:#{week_id}') || '0',
-        logins: Redis.current.pfcount('activity:logins:#{week_id}').to_s,
-        registrations: Redis.current.get('activity:accounts:local:#{week_id}') || '0',
-      }
+          it 'does not mute the other commenter's comments in the same thread' do
+        results = parent_comment_by_og.subtree.where(user: other_commenter).pluck(:receive_notifications)
+        expect(results.uniq).to eq [true]
+      end
     end
-    
-      #
-  # More advanced []= that does downcase storage.
-  #
-  def []=(key, value)
-    stored = false
-    
-              sent = 0
-          case protocol
-          when 'tcp'
-            sent = send_request_tcp(req)
-          when 'udp'
-            sent = send_request_udp(req)
-          else
-            raise ::RuntimeError, 'Kerberos Client: unknown transport protocol'
-          end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a principal, an asset (e.g., a
-        # workstation user or a network server) on a network.
-        class Element
+  end
+end
