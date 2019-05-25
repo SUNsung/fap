fcdab55488baf1cf77234992a67d77744edf5533
SUNsung <weatherius@gmail.com>
2019-05-25 20:12:18 +0300

    Rewrite [46xn6wnJeL4e646VZFh6QdOPA.46DV3.VzTVc5w46QAn.cpp] File in: 2019-05-25 20:12:17

--- cccccc/46xn6wnJeL4e646VZFh6QdOPA.46DV3.VzTVc5w46QAn.cpp
+++ cccccc/46xn6wnJeL4e646VZFh6QdOPA.46DV3.VzTVc5w46QAn.cpp
@@ -1,310 +1,170 @@
 
         
-        void Menu::UpdateKeys(views::FocusManager *focus_manager){
-  if (focus_manager == NULL){
-    return ;
-  } else {
-    focus_manager_ = focus_manager;
-    for(auto* item : menu_items_) {
-      item->UpdateKeys(focus_manager);
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    namespace stream_executor {
+namespace cuda {
     }
-  }
+    }
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    #include <cstdint>  // for int16_t
+    
+    namespace tesseract {
+class Tesseract;
 }
     
-    bool NwAppSetProxyConfigFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  net::ProxyConfigWithAnnotation config;
-  std::unique_ptr<nwapi::nw__app::SetProxyConfig::Params> params(
-      nwapi::nw__app::SetProxyConfig::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
-    }
-    
-          std::unique_ptr<SkBitmap> bitmap(new SkBitmap());
-      if (data.type == TYPE_PNG &&
-        !gfx::PNGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size(), bitmap.get())) {
-        error_ = 'Failed to decode as PNG';
-        return false;
-      } else if (data.type == TYPE_JPEG) {
-        std::unique_ptr<SkBitmap> tmp_bitmap = gfx::JPEGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size());
-        if (tmp_bitmap == NULL) {
-          error_ = 'Failed to decode as JPEG';
-          return false;
-        }
-        bitmap = std::move(tmp_bitmap);
-      }
-    
-    class NwClipboardClearSyncFunction : public NWSyncExtensionFunction {
+    // This structure captures all information needed about a text line for the
+// purposes of paragraph detection.  It is meant to be exceedingly light-weight
+// so that we can easily test paragraph detection independent of the rest of
+// Tesseract.
+class RowInfo {
  public:
-  NwClipboardClearSyncFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
+  // Constant data derived from Tesseract output.
+  STRING text;        // the full UTF-8 text of the line.
+  bool ltr;           // whether the majority of the text is left-to-right
+                      // TODO(eger) make this more fine-grained.
     }
     
+      // Sets the destination filename and enables images to be written to a PDF
+  // on destruction.
+  void WritePDF(const char* filename) {
+    if (pixaGetCount(pixa_) > 0) {
+      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, 'AllDebugImages', filename);
+      pixaClear(pixa_);
+    }
+  }
     
+      double m() const;  // get gradient
+  double c(double m) const;            // get constant
+  double rms(double m, double c) const;            // get error
+  double pearson() const;  // get correlation coefficient.
+    
+        void NDMask::Clear()
     {
-    {    return SingleGradientDef(
-        'MergeSingleListFeatureTensorsGradient',
-        '',
-        input_blob_names,
-        output_blob_names);
-  }
-};
-    
-    op = core.CreateOperator(
-    'FindDuplicateElements',
-    ['data'],
-    ['indices'],
-)
-    
-    #include 'genericvector.h'  // for GenericVector
-#include 'rect.h'           // for TBOX
-    
-    bool ParagraphModel::ValidFirstLine(int lmargin, int lindent,
-                                    int rindent, int rmargin) const {
-  switch (justification_) {
-    case JUSTIFICATION_LEFT:
-      return NearlyEqual(lmargin + lindent, margin_ + first_indent_,
-                         tolerance_);
-    case JUSTIFICATION_RIGHT:
-      return NearlyEqual(rmargin + rindent, margin_ + first_indent_,
-                         tolerance_);
-    case JUSTIFICATION_CENTER:
-      return NearlyEqual(lindent, rindent, tolerance_ * 2);
-    default:
-      // shouldn't happen
-      return false;
-  }
-}
-    
-    // Computes the Otsu threshold(s) for the given image rectangle, making one
-// for each channel. Each channel is always one byte per pixel.
-// Returns an array of threshold values and an array of hi_values, such
-// that a pixel value >threshold[channel] is considered foreground if
-// hi_values[channel] is 0 or background if 1. A hi_value of -1 indicates
-// that there is no apparent foreground. At least one hi_value will not be -1.
-// Delete thresholds and hi_values with delete [] after use.
-// The return value is the number of channels in the input image, being
-// the size of the output thresholds and hi_values arrays.
-int OtsuThreshold(Pix* src_pix, int left, int top, int width, int height,
-                  int** thresholds, int** hi_values);
-    
-      // Similar to the above, but return the vector of unichar ids for which
-  // the given unichar_id is an ambiguity (appears in the 'wrong' part of
-  // some ambiguity pair).
-  inline const UnicharIdVector *ReverseAmbigsForAdaption(
-      UNICHAR_ID unichar_id) const {
-    if (reverse_ambigs_for_adaption_.empty()) return nullptr;
-    return reverse_ambigs_for_adaption_[unichar_id];
-  }
-    
-    #endif  // THIRD_PARTY_TESSERACT_CCUTIL_DOUBLEPTR_H_
-
-    
-    // GenericHeap requires 1 template argument:
-// Pair will normally be either KDPairInc<Key, Data> or KDPairDec<Key, Data>
-// for some arbitrary Key and scalar, smart pointer, or non-ownership pointer
-// Data type, according to whether a MIN heap or a MAX heap is desired,
-// respectively. Using KDPtrPairInc<Key, Data> or KDPtrPairDec<Key, Data>,
-// GenericHeap can also handle simple Data pointers and own them.
-// If no additional data is required, Pair can also be a scalar, since
-// GenericHeap doesn't look inside it except for operator<.
-//
-// The heap is stored as a packed binary tree in an array hosted by a
-// GenericVector<Pair>, with the invariant that the children of each node are
-// both NOT Pair::operator< the parent node. KDPairInc defines Pair::operator<
-// to use Key::operator< to generate a MIN heap and KDPairDec defines
-// Pair::operator< to use Key::operator> to generate a MAX heap by reversing
-// all the comparisons.
-// See http://en.wikipedia.org/wiki/Heap_(data_structure) for more detail on
-// the basic heap implementation.
-//
-// Insertion and removal are both O(log n) and, unlike the STL heap, an
-// explicit Reshuffle function allows a node to be repositioned in time O(log n)
-// after changing its value.
-//
-// Accessing the element for revaluation is a more complex matter, since the
-// index and pointer can be changed arbitrarily by heap operations.
-// Revaluation can be done by making the Data type in the Pair derived from or
-// contain a DoublePtr as its first data element, making it possible to convert
-// the pointer to a Pair using KDPairInc::RecastDataPointer.
-template <typename Pair>
-class GenericHeap {
- public:
-  GenericHeap() = default;
-  // The initial size is only a GenericVector::reserve. It is not enforced as
-  // the size limit of the heap. Caller must implement their own enforcement.
-  explicit GenericHeap(int initial_size) {
-    heap_.reserve(initial_size);
-  }
+        // Clear the mask by marking all samples as Valid
+        GetMatrix()->SetValue((char)MaskKind::Valid);
     }
     
-    
-    {        return m_prevMinibatchAggregateTrainingLossValue->AsScalar<double>() / m_prevMinibatchNumSamples;
-    }
-    
-        Value::Value(const NDArrayViewPtr& data, const NDMaskPtr& mask)
-        : m_data(data), m_mask(mask)
-    {
-        if (mask != nullptr)
-        {
-            auto dataShape = data->Shape();
-            auto maskShape = mask->Shape();
-    }
-    }
-    
-        // Releases the mutex
-    void Release()
-    {
-        assert(m_fd != -1);
-        // removing file
-        unlink(m_fileName.c_str());
-        // Note: file is intentionally removed *before* releasing the lock
-        // to ensure that locked file isn't deleted by the non-owner of the lock
-        m_lock.l_type = F_UNLCK;
-        // Now removing the lock and closing the file descriptor
-        // waiting processes will be notified
-        int rc = fcntl(m_fd, F_SETLKW, &m_lock);
-        if (rc == FCNTL_ERROR)
-        {
-            RuntimeError('Mutex Release: Failed to release mutex %s', m_fileName.c_str());
-        }
-        close(m_fd);
-        m_fd = -1;
-    }
-    
-    static inline size_t rand(const size_t begin, const size_t end)
+    bool DataReader::SetNetOutput(
+    const std::vector<std::vector<std::pair<wstring, size_t>>>& uttInfo,
+    const MatrixBase& outputs,
+    const MBLayoutPtr pMBLayout)
 {
-    const size_t randno = ::rand() * RAND_MAX + ::rand(); // BUGBUG: still only covers 32-bit range
-    return begin + randno % (end - begin);
+    bool ans = false;
+    for (size_t i = 0; i < m_ioNames.size(); i++)
+        ans = (m_dataReaders[m_ioNames[i]]->SetNetOutput(uttInfo, outputs, pMBLayout) || ans);
+    return ans;
 }
     
     
-    {private:
-    long long m_start;
-    long long m_end;
-};
+    {        // change to other values so can support multiple sentences in each minibatch
+        ForwardCompute(alpha, backtrace, pos_scores, pair_scores, stt);
+        BackwardCompute(functionValues, backtrace, stp);
+    };
     
-    namespace xgboost {
-/*!
- * \brief interface of linear updater
+    // Copies internal accumulator to the output.
+template <class ElemType>
+void EpochAccumulatorNode<ElemType>::CopyAccumulatorToValue()
+{
+    // Value gets resized in UpdateFunctionValuesSize that is called in BeforeForwardProp. Resize fills matrix with NaN
+    // values, so m_value matrix cannot be used as persistent storage between ForwardProp calls.
+    Value().SetValue(*m_accumulator);
+}
+    
+    /*
+ * If Trace::hhbbc_time >= 1, print some stats about the program to a
+ * temporary file.  If it's greater than or equal to 2, also dump it
+ * to stdout.
  */
-class LinearUpdater {
- public:
-  /*! \brief virtual destructor */
-  virtual ~LinearUpdater() = default;
-  /*!
-   * \brief Initialize the updater with given arguments.
-   * \param args arguments to the objective function.
-   */
-  virtual void Init(
-      const std::vector<std::pair<std::string, std::string> >& args) = 0;
-    }
+void print_stats(const Index&, const php::Program&);
+    
+    int64_t VMTOC::pushElem(int64_t elem, bool elemMayChange) {
+  int64_t offset;
+  if (elemMayChange) {
+    offset = allocTOC(elem);
+  }
+  else {
+    auto& map_elem = m_map[elem];
+    if (map_elem) return map_elem;
+    offset = allocTOC(elem);
+    map_elem = offset;
+  }
     }
     
     
-    {template<typename IndexType, typename DType = real_t>
-Parser<IndexType> *
-CreateDenseLibSVMParser(const std::string& path,
-                        const std::map<std::string, std::string>& args,
-                        unsigned part_index,
-                        unsigned num_parts) {
-  CHECK_NE(args.count('num_col'), 0) << 'expect num_col in dense_libsvm';
-  return new DensifyParser<IndexType>(
-            Parser<IndexType>::Create(path.c_str(), part_index, num_parts, 'libsvm'),
-           uint32_t(atoi(args.at('num_col').c_str())));
+    {///////////////////////////////////////////////////////////////////////////////
 }
-}  // namespace data
     
+    bool ArrayDirectory::isEof() const {
+  return m_it.end();
+}
     
-    {
-    {
-    {  inline void PutChar(char ch) {
-    out_buf += ch;
-    if (out_buf.length() >= kBufferSize) Flush();
-  }
-  inline void Flush(void) {
-    if (out_buf.length() != 0) {
-      fp->Write(&out_buf[0], out_buf.length());
-      out_buf.clear();
-    }
-  }
-};
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_BASE64_H_
+    #endif // incl_HPHP_OUTPUT_FILE_H_
 
     
+    #ifndef incl_HPHP_PERF_EVENT_H_
+#define incl_HPHP_PERF_EVENT_H_
     
-    {
-    {void SparsePageWriter::Alloc(std::shared_ptr<SparsePage>* out_page) {
-  CHECK(*out_page == nullptr);
-  if (num_free_buffer_ != 0) {
-    out_page->reset(new SparsePage());
-    --num_free_buffer_;
-  } else {
-    CHECK(qrecycle_.Pop(out_page));
-  }
+    
+    {  logAHMSubMapWarning(mapName);
 }
-}  // namespace data
-}  // namespace xgboost
     
-    // Finally register the objective function.
-// After it succeeds you can try use xgboost with objective=mylogistic
-XGBOOST_REGISTER_OBJECTIVE(MyLogistic, 'mylogistic')
-.describe('User defined logistic regression plugin')
-.set_body([]() { return new MyLogistic(); });
     
-        // Remove the assertion on batch.index, which can be null in the case that the data in this
-    // batch is entirely sparse. Although it's true that this indicates a likely issue with the
-    // user's data workflows, passing XGBoost entirely sparse data should not cause it to fail.
-    // See https://github.com/dmlc/xgboost/issues/1827 for complete detail.
-    // CHECK(batch.index != nullptr);
+    {///////////////////////////////////////////////////////////////////////////////
+}
     
-      xfer += iprot->readStructBegin(fname);
+    class Env;
+class WriteControllerToken;
     
-      void ping(ExtensionStatus& _return) {
-    // Your implementation goes here
-    printf('ping\n');
-  }
+      // initialize BlockBasedTableOptions
+  auto cache = NewLRUCache(1 * 1024 * 1024 * 1024);
+  BlockBasedTableOptions bbt_opts;
+  bbt_opts.block_size = 32 * 1024;
+  bbt_opts.block_cache = cache;
     
-    void genNFSShare(const std::string& share_line, QueryData& results) {
-  auto line = osquery::split(share_line);
-  if (line.size() == 0 || boost::starts_with(line[0], '#')) {
-    return;
-  }
-    }
+      // open DB
+  Status s = DB::Open(options, kDBPath, &db);
+  assert(s.ok());
     
-    QueryData genKernelIntegrity(QueryContext &context) {
-  QueryData results;
-  Row r;
-  std::string content;
-  std::string text_segment_hash;
-  std::string syscall_addr_modified;
-    }
+    // Supported only for Leveled compaction
+Status SuggestCompactRange(DB* db, ColumnFamilyHandle* column_family,
+                           const Slice* begin, const Slice* end);
+Status SuggestCompactRange(DB* db, const Slice* begin, const Slice* end);
     
-    void querySmartDevices(
-    libsmartctl::ClientInterface& smartctl,
-    std::function<void(
-        std::function<void(const std::string&, hardwareDriver*)>)> walk_func,
-    QueryData& results) {
-  // hw_info is for tracking info retrieve with an explicit HW controller.  It
-  // is indexed by serial_number, since that's how you correlate the data with
-  // auto-detect retrieved SMART info.
-  std::map<std::string, Row> hw_info;
-    }
-    
-    #include <linux/perf_event.h>
-    
-      enum class IsEnabledError {
-    CallFailed = 1,
-    IncorrectResponseFormat = 2,
-    IncorrectValue = 3
-  };
-  Expected<bool, Killswitch::IsEnabledError> isEnabled(const std::string& key);
-    
-      wrapped.msg.a_ = 3;
-  wrapped.msg.b_ = 4;
-  std::copy(wrapped_ptr,
-            wrapped_ptr + first_part_size,
-            &buf[0] + tail + sizeof(WrappedMessage));
-  std::copy(wrapped_ptr + first_part_size,
-            wrapped_ptr + sizeof(WrappedMessage),
-            &buf[0]);
+      static Status Open(const DBOptions& db_options, const std::string& dbname,
+                     const std::vector<ColumnFamilyDescriptor>& column_families,
+                     std::vector<ColumnFamilyHandle*>* handles,
+                     OptimisticTransactionDB** dbptr);
