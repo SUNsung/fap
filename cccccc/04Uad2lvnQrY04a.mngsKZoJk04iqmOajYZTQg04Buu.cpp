61af63533cd27e567e5e6b874febb1872c7fdbc9
SUNsung <weatherius@gmail.com>
2019-05-25 11:02:27 +0300

    Rewrite [04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp] File in: 2019-05-25 11:02:26

--- cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
+++ cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
@@ -1,124 +1,179 @@
 
         
-        StringRef importer::getCFTypeName(
-            const clang::TypedefNameDecl *decl) {
-  if (auto pointee = CFPointeeInfo::classifyTypedef(decl)) {
-    auto name = decl->getName();
-    if (pointee.isRecord() || pointee.isTypedef())
-      if (name.endswith(SWIFT_CFTYPE_SUFFIX))
-        return name.drop_back(strlen(SWIFT_CFTYPE_SUFFIX));
+        PyArray_Descr NPyBfloat16_Descr = {
+    PyObject_HEAD_INIT(nullptr) & PyBfloat16_Type,  // typeobj
+    // We must register bfloat16 with a kind other than 'f', because numpy
+    // considers two types with the same kind and size to be equal, but
+    // float16 != bfloat16.
+    'V',  // kind
+    // TODO(phawkins): there doesn't seem to be a way of guaranteeing a type
+    // character is unique.
+    'E',                                                  // type
+    '=',                                                  // byteorder
+    NPY_NEEDS_PYAPI | NPY_USE_GETITEM | NPY_USE_SETITEM,  // hasobject
+    0,                                                    // type_num
+    sizeof(bfloat16),                                     // elsize
+    alignof(bfloat16),                                    // alignment
+    nullptr,                                              // subarray
+    nullptr,                                              // fields
+    nullptr,                                              // names
+    &NPyBfloat16_ArrFuncs,                                // f
+};
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    namespace tensorflow {
     }
-    }
     
-    class CFPointeeInfo {
-  bool IsValid;
-  bool IsConst;
-  llvm::PointerUnion<const clang::RecordDecl *, const clang::TypedefNameDecl *>
-      Decl;
-  CFPointeeInfo() = default;
-    }
+    // Safe containers for an owned TF_Status. On destruction, the handle
+// will be deleted by TF_DeleteStatus.
+using Safe_TF_StatusPtr = std::unique_ptr<TF_Status, detail::TFStatusDeleter>;
+Safe_TF_StatusPtr make_safe(TF_Status* status);
     
-    #include 'llvm/ADT/Optional.h'
-#include 'llvm/ADT/StringRef.h'
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-      if (!convertUTF8toUTF32(InputUTF8, InputCodePoints, mapNonSymbolChars))
-    return false;
-    
-    bool ArgsToFrontendInputsConverter::enforceFilelistExclusion() {
-  if (Args.hasArg(options::OPT_INPUT) && FilelistPathArg) {
-    Diags.diagnose(SourceLoc(),
-                   diag::error_cannot_have_input_files_with_file_list);
-    return true;
+      const tensorflow::OpRegistrationData* op_reg_data;
+  auto status =
+      tensorflow::OpRegistry::Global()->LookUp(node_def.op(), &op_reg_data);
+  if (!status.ok()) {
+    LOG(WARNING) << 'Op ' << node_def.op() << ' not found: ' << status;
+    return '';
   }
-  // The following is not strictly necessary, but the restriction makes
-  // it easier to understand a given command line:
-  if (Args.hasArg(options::OPT_primary_file) && PrimaryFilelistPathArg) {
-    Diags.diagnose(
-        SourceLoc(),
-        diag::error_cannot_have_primary_files_with_primary_file_list);
-    return true;
-  }
-  return false;
+  AddDefaultsToNodeDef(op_reg_data->op_def, &node_def);
+    
+    // Functions for getting information about kernels registered in the binary.
+#ifndef TENSORFLOW_PYTHON_UTIL_KERNEL_REGISTRY_H_
+#define TENSORFLOW_PYTHON_UTIL_KERNEL_REGISTRY_H_
+    
+    #ifndef TENSORFLOW_STREAM_EXECUTOR_DEVICE_OPTIONS_H_
+#define TENSORFLOW_STREAM_EXECUTOR_DEVICE_OPTIONS_H_
+    
+    
+    { private:
+  SE_DISALLOW_COPY_AND_ASSIGN(FftSupport);
+};
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    void HostTimer::StartNow() { start_time_ = clock::now(); }
+    
+    #include 'atom/browser/native_window_views.h'
+    
+    #include 'atom/browser/api/event_emitter.h'
+    
+    
+    {}  // namespace api
+    
+      // content::QuotaPermissionContext:
+  void RequestQuotaPermission(const content::StorageQuotaParams& params,
+                              int render_process_id,
+                              const PermissionCallback& callback) override;
+    
+    
+    {}  // namespace auto_updater
+
+    
+      // net::URLRequestJobFactory::ProtocolHandler:
+  net::URLRequestJob* MaybeCreateJob(
+      net::URLRequest* request,
+      net::NetworkDelegate* network_delegate) const override;
+  bool IsSafeRedirectTarget(const GURL& location) const override;
+    
+    void URLRequestAboutJob::Start() {
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(&URLRequestAboutJob::StartAsync,
+                                weak_ptr_factory_.GetWeakPtr()));
 }
     
-    
-    {
-    {}}
-    
-    using LineNumber = uint32_t;
-using ColNumber  = uint32_t;
-using LineRange  = std::tuple<LineNumber,LineNumber>;
-    
-    
-    {      dword(sc_formater.instruction);
-   }
-    
-    /**
- * Implement this interface to report information to debugger or execute
- * debugger commands.
- */
-struct IDebuggable {
-  enum Support {
-    SupportInfo = 1,
-    SupportDump = 2,
-    SupportVerb = 4,
-  };
-    }
-    
-    #include 'hphp/runtime/ext/std/ext_std_file.h'
-#include 'hphp/runtime/base/file.h'
-#include 'hphp/runtime/server/static-content-cache.h'
-    
-    /**
- * For php://output, a simple wrapper of g_context->out().
- */
-struct OutputFile : File {
-  DECLARE_RESOURCE_ALLOCATION(OutputFile);
-    }
-    
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////
-    }
-    
-    
-    {};
-    
-    void ExtensionException::__set_uuid(const ExtensionRouteUUID val) {
-  this->uuid = val;
+    void OffscreenViewProxy::SetBitmap(const SkBitmap& bitmap) {
+  if (view_bounds_.width() == bitmap.width() &&
+      view_bounds_.height() == bitmap.height() && observer_) {
+    view_bitmap_.reset(new SkBitmap(bitmap));
+    observer_->OnProxyViewPaint(view_bounds_);
+  }
 }
-std::ostream& operator<<(std::ostream& out, const ExtensionException& obj)
+    
+    #include 'ui/aura/client/drag_drop_client.h'
+#include 'ui/aura/window.h'
+#include 'ui/base/dragdrop/drag_drop_types.h'
+#include 'ui/base/dragdrop/file_info.h'
+#include 'ui/base/dragdrop/os_exchange_data.h'
+#include 'ui/display/screen.h'
+#include 'ui/gfx/geometry/point.h'
+#include 'ui/views/button_drag_utils.h'
+#include 'ui/views/widget/widget.h'
+#include 'url/gurl.h'
+    
+    // A '1' in this array means the character is white space.  A '1' or
+// '2' means the character ends a name or command.
+static const char specialChars[256] = {
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0,   // 0x
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1x
+  1, 0, 0, 0, 0, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 2,   // 2x
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0,   // 3x
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 4x
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0,   // 5x
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 6x
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0,   // 7x
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 8x
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 9x
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // ax
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // bx
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // cx
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // dx
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // ex
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0    // fx
+};
+    
+    void PreScanOutputDev::drawImageMask(GfxState *state, Object * /*ref*/, Stream *str,
+				     int width, int height, GBool /*invert*/,
+				     GBool /*interpolate*/, GBool inlineImg) {
+  int i, j;
+    }
+    
+    Sound::~Sound()
 {
-  obj.printTo(out);
-  return out;
+  delete fileName;
+  streamObj->free();
+  delete streamObj;
 }
     
-    #include <osquery/utils/expected/expected.h>
-#include <osquery/utils/system/posix/errno.h>
+    #include 'StdinCachedFile.h'
     
-    #pragma once
+    class StdinCacheLoader : public CachedFileLoader {
+    }
     
-    // Sanity check integration test for kernel_panics
-// Spec file: specs/darwin/kernel_panics.table
+    PDFDoc *
+StdinPDFDocBuilder::buildPDFDoc(const GooString &uri, GooString *ownerPassword,
+                                    GooString *userPassword, void *guiDataA)
+{
+  Object obj;
+    }
     
-    TEST_F(keychainItems, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from keychain_items');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'label', NormalType}
-  //      {'description', NormalType}
-  //      {'comment', NormalType}
-  //      {'created', NormalType}
-  //      {'modified', NormalType}
-  //      {'type', NormalType}
-  //      {'path', NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
+    // Implemented features:
+//  [X] Renderer: User texture binding. Use 'ID3D10ShaderResourceView*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+    
+        bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    
+    
+    {    for (UINT i = 0; i < NUM_BACK_BUFFERS; i++)
+        if (g_mainRenderTargetResource[i]) { g_mainRenderTargetResource[i]->Release(); g_mainRenderTargetResource[i] = NULL; }
 }
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
