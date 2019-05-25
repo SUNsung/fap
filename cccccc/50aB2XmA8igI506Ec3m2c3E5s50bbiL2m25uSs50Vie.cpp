ab76a19d71a50d563f3b8ea91c148ba1b371feca
SUNsung <weatherius@gmail.com>
2019-05-26 02:10:06 +0300

    Rewrite [50aB2XmA8igI506Ec3m2c3E5s50bbiL2m25uSs50Vie.cpp] File in: 2019-05-26 02:10:06

--- cccccc/50aB2XmA8igI506Ec3m2c3E5s50bbiL2m25uSs50Vie.cpp
+++ cccccc/50aB2XmA8igI506Ec3m2c3E5s50bbiL2m25uSs50Vie.cpp
@@ -1,228 +1,113 @@
 
         
-          // Writing direction
-  bool is_vertical_text = (block->classify_rotation().x() == 0.0);
-  bool right_to_left = block->right_to_left();
-  *writing_direction =
-      is_vertical_text
-          ? WRITING_DIRECTION_TOP_TO_BOTTOM
-          : (right_to_left
-                ? WRITING_DIRECTION_RIGHT_TO_LEFT
-                : WRITING_DIRECTION_LEFT_TO_RIGHT);
-    
-    
-    {  TBLOB* blob;
-  Tesseract* tesseract;
-  BLOB_CHOICE_LIST** choices;
-};
-    
-    
-    {  tesseract::StringParam* sIt;
-  tesseract::IntParam* iIt;
-  tesseract::BoolParam* bIt;
-  tesseract::DoubleParam* dIt;
-};
-    
-    
-    {  // Do sanity checks and minor fixes on best_choice.
-  if (word->best_choice->length() > word_length) {
-    word->best_choice->make_bad();  // should never happen
-    tprintf('recog_word: Discarded long string \'%s\''
-            ' (%d characters vs %d blobs)\n',
-            word->best_choice->unichar_string().string(),
-            word->best_choice->length(), word_length);
-    tprintf('Word is at:');
-    word->word->bounding_box().print();
-  }
-  if (word->best_choice->length() < word_length) {
-    UNICHAR_ID space_id = unicharset.unichar_to_id(' ');
-    while (word->best_choice->length() < word_length) {
-      word->best_choice->append_unichar_id(space_id, 1, 0.0,
-                                           word->best_choice->certainty());
+        
+    {    // exception out_of_range.401
+    try
+    {
+        // try to write at a nonexisting key
+        object.at('the fast') = 'il rapido';
     }
-  }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
 }
-    
-      /// Get a clone/copy of the source image rectangle.
-  /// The returned Pix must be pixDestroyed.
-  /// This function will be used in the future by the page layout analysis, and
-  /// the layout analysis that uses it will only be available with Leptonica,
-  /// so there is no raw equivalent.
-  Pix* GetPixRect();
-    
-    #endif  // TESSERACT_CCUTIL_BOXREAD_H_
 
     
-      // Accessors.
-  int total_cost() const {
-    return total_cost_;
-  }
-  int Pathlength() const {
-    return total_steps_;
-  }
-  const DPPoint* best_prev() const {
-    return best_prev_;
-  }
-  void AddLocalCost(int new_cost) {
-    local_cost_ += new_cost;
-  }
+    using json = nlohmann::json;
     
-    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
-  for (unsigned int i = 0; i < args_.size(); i++) {
-    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
-      return args_[i].value.string;
-    }
-  }
-  return '';
-}
-    
-    void CensusClientCallData::OnDoneRecvTrailingMetadataCb(void* user_data,
-                                                        grpc_error* error) {
-  grpc_call_element* elem = reinterpret_cast<grpc_call_element*>(user_data);
-  CensusClientCallData* calld =
-      reinterpret_cast<CensusClientCallData*>(elem->call_data);
-  GPR_ASSERT(calld != nullptr);
-  if (error == GRPC_ERROR_NONE) {
-    GPR_ASSERT(calld->recv_trailing_metadata_ != nullptr);
-    FilterTrailingMetadata(calld->recv_trailing_metadata_,
-                           &calld->elapsed_time_);
-  }
-  GRPC_CLOSURE_RUN(calld->initial_on_done_recv_trailing_metadata_,
-                   GRPC_ERROR_REF(error));
-}
-    
-    MeasureDouble RpcServerReceivedBytesPerRpc() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcServerReceivedBytesPerRpcMeasureName,
-      'Total bytes received across all messages per RPC', kUnitBytes);
-  return measure;
-}
-    
-    
-    {}  // namespace grpc
-    
-    
-    {  const protobuf::FieldDescriptor* field_desc =
-      descriptor_pool_->FindExtensionByNumber(desc,
-                                              request->extension_number());
-  if (field_desc == nullptr) {
-    return Status(StatusCode::NOT_FOUND, 'Extension not found.');
-  }
-  std::unordered_set<grpc::string> seen_files;
-  FillFileDescriptorResponse(field_desc->file(), response, &seen_files);
-  return Status::OK;
-}
-    
-    void InitProtoReflectionServerBuilderPlugin() {
-  static bool already_here = false;
-  if (already_here) return;
-  already_here = true;
-  ::grpc::ServerBuilder::InternalAddPluginFactory(&CreateProtoReflection);
-}
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    
-    {    // Create Descriptor Pool
-    {
-        VkDescriptorPoolSize pool_sizes[] =
-        {
-            { VK_DESCRIPTOR_TYPE_SAMPLER, 1000 },
-            { VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1000 },
-            { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 1000 },
-            { VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1000 },
-            { VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 1000 },
-            { VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 1000 },
-            { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1000 },
-            { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1000 },
-            { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1000 },
-            { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 1000 },
-            { VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 1000 }
-        };
-        VkDescriptorPoolCreateInfo pool_info = {};
-        pool_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-        pool_info.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
-        pool_info.maxSets = 1000 * IM_ARRAYSIZE(pool_sizes);
-        pool_info.poolSizeCount = (uint32_t)IM_ARRAYSIZE(pool_sizes);
-        pool_info.pPoolSizes = pool_sizes;
-        err = vkCreateDescriptorPool(g_Device, &pool_info, g_Allocator, &g_DescriptorPool);
-        check_vk_result(err);
-    }
-}
-    
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
-    
-        // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
-    
-    extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+    int main()
 {
-    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
-        return true;
+    // create JSON values
+    json object = {{'one', 1}, {'two', 2}};
+    json null;
     }
     
-        void restore();
-    void updateTransform();
     
-    FlipX * FlipX::clone() const
-{
-    // no copy constructor
-    return FlipX::create(_flipX);
+    {  return 0;
 }
-//
-// FlipY
-//
-    
-    void ActionManager::removeAllActionsByTag(int tag, Node *target)
-{
-    CCASSERT(tag != Action::INVALID_TAG, 'Invalid tag value!');
-    CCASSERT(target != nullptr, 'target can't be nullptr!');
-    if (target == nullptr)
-    {
-        return;
-    }
-    
-    tHashElement *element = nullptr;
-    HASH_FIND_PTR(_targets, &target, element);
-    
-    if (element)
-    {
-        auto limit = element->actions->num;
-        for (int i = 0; i < limit;)
-        {
-            Action *action = static_cast<Action*>(element->actions->arr[i]);
-    }
-    }
-    }
-    
-    #endif // __ACTION_CCTILEDGRID_ACTION_H__
 
     
-    Animation::Animation()
-: _totalDelayUnits(0.0f)
-, _delayPerUnit(0.0f)
-, _duration(0.0f)
-, _restoreOriginalFrame(false)
-, _loops(0)
-{
+      // Load the options file.
+  DBOptions loaded_db_opt;
+  std::vector<ColumnFamilyDescriptor> loaded_cf_descs;
+  s = LoadLatestOptions(kDBPath, Env::Default(), &loaded_db_opt,
+                        &loaded_cf_descs);
+  assert(s.ok());
+  assert(loaded_db_opt.create_if_missing == db_opt.create_if_missing);
+    
+      {
+    std::string string_val;
+    // If it cannot pin the value, it copies the value to its internal buffer.
+    // The intenral buffer could be set during construction.
+    PinnableSlice pinnable_val(&string_val);
+    db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key2', &pinnable_val);
+    assert(pinnable_val == 'value');
+    // If the value is not pinned, the internal buffer must have the value.
+    assert(pinnable_val.IsPinned() || string_val == 'value');
+  }
+    
+    #ifndef ROCKSDB_LITE
+    
+    // Move all L0 files to target_level skipping compaction.
+// This operation succeeds only if the files in L0 have disjoint ranges; this
+// is guaranteed to happen, for instance, if keys are inserted in sorted
+// order. Furthermore, all levels between 1 and target_level must be empty.
+// If any of the above condition is violated, InvalidArgument will be
+// returned.
+Status PromoteL0(DB* db, ColumnFamilyHandle* column_family,
+                 int target_level = 1);
+    
+      // If non-NULL, use the specified filter policy to reduce disk reads.
+  // Many applications will benefit from passing the result of
+  // NewBloomFilterPolicy() here.
+  //
+  // Default: NULL
+  const FilterPolicy* filter_policy;
+    
+     private:
+  void Init(
+      const std::string& lua_script,
+      const std::vector<std::shared_ptr<RocksLuaCustomLibrary>>& libraries) {
+    if (lua_state_) {
+      luaL_openlibs(lua_state_);
+      for (const auto& library : libraries) {
+        luaL_openlib(lua_state_, library->Name(), library->Lib(), 0);
+        library->CustomSetup(lua_state_);
+      }
+      luaL_dostring(lua_state_, lua_script.c_str());
+    }
+  }
+    
+    // Returns the current memory usage of the specified DB instances.
+class MemoryUtil {
+ public:
+  enum UsageType : int {
+    // Memory usage of all the mem-tables.
+    kMemTableTotal = 0,
+    // Memory usage of those un-flushed mem-tables.
+    kMemTableUnFlushed = 1,
+    // Memory usage of all the table readers.
+    kTableReadersTotal = 2,
+    // Memory usage by Cache.
+    kCacheTotal = 3,
+    kNumUsageTypes = 4
+  };
     }
     
-    void AtlasNode::calculateMaxItems()
-{
-    Size s = _textureAtlas->getTexture()->getContentSize();
+      // Attempt to acquire lock.  Return OK on success, or other Status on failure.
+  // If returned status is OK, TransactionDB will eventually call UnLock().
+  virtual Status Lock() = 0;
     
-    if (_ignoreContentScaleFactor)
-    {
-        s = _textureAtlas->getTexture()->getContentSizeInPixels();
-    }
-    
-    _itemsPerColumn = (int)(s.height / _itemHeight);
-    _itemsPerRow = (int)(s.width / _itemWidth);
+    /*
+ * Class:     org_rocksdb_BackupableDBOptions
+ * Method:    setCallbackTriggerIntervalSize
+ * Signature: (JJ)V
+ */
+void Java_org_rocksdb_BackupableDBOptions_setCallbackTriggerIntervalSize(
+    JNIEnv* /*env*/, jobject /*jobj*/, jlong jhandle,
+    jlong jcallback_trigger_interval_size) {
+  auto* bopt = reinterpret_cast<rocksdb::BackupableDBOptions*>(jhandle);
+  bopt->callback_trigger_interval_size =
+      static_cast<uint64_t>(jcallback_trigger_interval_size);
 }
