42c4ede64f62bda55a6d55d3063788834c82b895
SUNsung <weatherius@gmail.com>
2019-05-25 03:40:43 +0300

    Rewrite [39OGY9ehipqhg39hiazVMr3UU39RaBMcKMi44Y39Uip.cpp] File in: 2019-05-25 03:40:42

--- cccccc/39OGY9ehipqhg39hiazVMr3UU39RaBMcKMi44Y39Uip.cpp
+++ cccccc/39OGY9ehipqhg39hiazVMr3UU39RaBMcKMi44Y39Uip.cpp
@@ -1,160 +1,140 @@
 
         
-        template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35,
-    typename T36, typename T37, typename T38, typename T39, typename T40,
-    typename T41>
-internal::ValueArray41<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-    T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40,
-    T41> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
-    T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
-    T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
-    T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31, T32 v32, T33 v33,
-    T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39, T40 v40, T41 v41) {
-  return internal::ValueArray41<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39,
-      T40, T41>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14,
-      v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28,
-      v29, v30, v31, v32, v33, v34, v35, v36, v37, v38, v39, v40, v41);
+        void CacheImpl::releaseValue(void *Value) {
+  cache_release_value(static_cast<cache_t*>(Impl), Value);
 }
     
-    // First, define a fixture class template.  It should be parameterized
-// by a type.  Remember to derive it from testing::Test.
-template <typename T>
-class FooTest : public testing::Test {
- public:
-  ...
-  typedef std::list<T> List;
-  static T shared_;
-  T value_;
-};
+    bool importer::isCFTypeDecl(
+       const clang::TypedefNameDecl *Decl) {
+  if (CFPointeeInfo::classifyTypedef(Decl))
+    return true;
+  return false;
+}
     
-      // Gets the i-th test case among all the test cases. i can range from 0 to
-  // total_test_case_count() - 1. If i is not in that range, returns NULL.
-  TestCase* GetMutableTestCase(int i);
-    
-      // Returns a pathname for a file that does not currently exist. The pathname
-  // will be directory/base_name.extension or
-  // directory/base_name_<number>.extension if directory/base_name.extension
-  // already exists. The number will be incremented until a pathname is found
-  // that does not already exist.
-  // Examples: 'dir/foo_test.xml' or 'dir/foo_test_1.xml'.
-  // There could be a race condition if two or more processes are calling this
-  // function at the same time -- they could both pick the same filename.
-  static FilePath GenerateUniqueFileName(const FilePath& directory,
-                                         const FilePath& base_name,
-                                         const char* extension);
-    
-    #endif  // GTEST_OS_WINDOWS
-    
-      bool operator==(T* p) const { return value_ == p; }
-  bool operator!=(T* p) const { return value_ != p; }
-  template <typename U>
-  bool operator==(linked_ptr<U> const& ptr) const {
-    return value_ == ptr.get();
+    std::string Mangle::translateOperator(StringRef Op) {
+  std::string Encoded;
+  for (char ch : Op) {
+    Encoded.push_back(translateOperatorChar(ch));
   }
-  template <typename U>
-  bool operator!=(linked_ptr<U> const& ptr) const {
-    return value_ != ptr.get();
+  return Encoded;
+}
+    
+    static void printNode(DemanglerPrinter &Out, const Node *node, unsigned depth) {
+  // Indent two spaces per depth.
+  for (unsigned i = 0; i < depth * 2; ++i) {
+    Out << ' ';
+  }
+  if (!node) {
+    Out << '<<NULL>>';
+    return;
+  }
+  Out << 'kind=' << getNodeKindString(node->getKind());
+  if (node->hasText()) {
+    Out << ', text=\'' << node->getText() << '\'';
+  }
+  if (node->hasIndex()) {
+    Out << ', index=' << node->getIndex();
+  }
+  Out << '\n';
+  for (auto &child : *node) {
+    printNode(Out, child, depth + 1);
+  }
+}
+    
+      /// The target to be passed to the compiler invocation. By default, this
+  /// is the target triple, but this may be overridden to accommodate some
+  /// platforms.
+  virtual std::string getTargetForLinker() const;
+    
+    Status Writer::EmitPhysicalRecord(RecordType t, const char* ptr,
+                                  size_t length) {
+  assert(length <= 0xffff);  // Must fit in two bytes
+  assert(block_offset_ + kHeaderSize + length <= kBlockSize);
+    }
+    
+      void SetComparatorName(const Slice& name) {
+    has_comparator_ = true;
+    comparator_ = name.ToString();
+  }
+  void SetLogNumber(uint64_t num) {
+    has_log_number_ = true;
+    log_number_ = num;
+  }
+  void SetPrevLogNumber(uint64_t num) {
+    has_prev_log_number_ = true;
+    prev_log_number_ = num;
+  }
+  void SetNextFile(uint64_t num) {
+    has_next_file_number_ = true;
+    next_file_number_ = num;
+  }
+  void SetLastSequence(SequenceNumber seq) {
+    has_last_sequence_ = true;
+    last_sequence_ = seq;
+  }
+  void SetCompactPointer(int level, const InternalKey& key) {
+    compact_pointers_.push_back(std::make_pair(level, key));
   }
     
-      // Returns true iff the given string ends with the given suffix, ignoring
-  // case. Any string is considered to end with an empty suffix.
-  static bool EndsWithCaseInsensitive(
-      const std::string& str, const std::string& suffix);
+      // Return a handle to the current DB state.  Iterators created with
+  // this handle will all observe a stable snapshot of the current DB
+  // state.  The caller must call ReleaseSnapshot(result) when the
+  // snapshot is no longer needed.
+  virtual const Snapshot* GetSnapshot() = 0;
     
-      tuple& operator=(const tuple& t) { return CopyFrom(t); }
     
-    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
-int Factorial(int n) {
-  int result = 1;
-  for (int i = 1; i <= n; i++) {
-    result *= i;
+    {    fprintf(stderr,
+            'expected=%d..%d; got=%d; bad_keys=%d; bad_values=%d; missed=%d\n',
+            min_expected, max_expected, correct, bad_keys, bad_values, missed);
+    ASSERT_LE(min_expected, correct);
+    ASSERT_GE(max_expected, correct);
   }
+    
+      WritableFile* dest_;
+  int block_offset_;  // Current offset in block
+    
+    namespace leveldb {
     }
     
-    // In this example, we test the MyString class (a simple string).
+      // Return an iterator for the specified file number (the corresponding
+  // file length must be exactly 'file_size' bytes).  If 'tableptr' is
+  // non-null, also sets '*tableptr' to point to the Table object
+  // underlying the returned iterator, or to nullptr if no Table object
+  // underlies the returned iterator.  The returned '*tableptr' object is owned
+  // by the cache and should not be deleted, and is valid for as long as the
+  // returned iterator is live.
+  Iterator* NewIterator(const ReadOptions& options, uint64_t file_number,
+                        uint64_t file_size, Table** tableptr = nullptr);
     
-    namespace HPHP { namespace HHBBC {
+      // Number of open files that can be used by the DB.  You may need to
+  // increase this if your database has a large working set (budget
+  // one open file per 2MB of working set).
+  int max_open_files = 1000;
+    
+    #endif  // STORAGE_LEVELDB_INCLUDE_WRITE_BATCH_H_
+
+    
+    void
+FixedSortKeyByteSink::AppendBeyondCapacity(const char *bytes, int32_t /*n*/, int32_t length) {
+    // buffer_ != NULL && bytes != NULL && n > 0 && appended_ > capacity_
+    // Fill the buffer completely.
+    int32_t available = capacity_ - length;
+    if (available > 0) {
+        uprv_memcpy(buffer_ + length, bytes, available);
     }
-    }
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    
-    { private:
-  int mkdir_recursive(const String& path, int mode);
-};
-    
-    struct GlobStreamWrapper final : Stream::Wrapper {
-  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-  req::ptr<Directory> opendir(const String& path) override;
-};
-    
-    #include 'hphp/runtime/base/perf-warning.h'
-    
-    /**
- * Factory method
- */
-static Transliterator* RemoveTransliterator_create(const UnicodeString& /*ID*/,
-                                                   Transliterator::Token /*context*/) {
-    /* We don't need the ID or context. We just remove data */
-    return new RemoveTransliterator();
 }
     
-    ScientificNumberFormatter *ScientificNumberFormatter::createInstance(
-            DecimalFormat *fmtToAdopt,
-            Style *styleToAdopt,
-            UErrorCode &status) {
-    LocalPointer<DecimalFormat> fmt(fmtToAdopt);
-    LocalPointer<Style> style(styleToAdopt);
-    if (U_FAILURE(status)) {
-        return NULL;
-    }
-    ScientificNumberFormatter *result =
-            new ScientificNumberFormatter(
-                    fmt.getAlias(),
-                    style.getAlias(),
-                    status);
-    if (result == NULL) {
-        status = U_MEMORY_ALLOCATION_ERROR;
-        return NULL;
-    }
-    fmt.orphan();
-    style.orphan();
-    if (U_FAILURE(status)) {
-        delete result;
-        return NULL;
-    }
-    return result;
+    U_NAMESPACE_BEGIN
+    
+    U_CAPI UBool U_EXPORT2
+uhash_equalsScriptSet(const UElement key1, const UElement key2) {
+    icu::ScriptSet *s1 = static_cast<icu::ScriptSet *>(key1.pointer);
+    icu::ScriptSet *s2 = static_cast<icu::ScriptSet *>(key2.pointer);
+    return (*s1 == *s2);
 }
     
-    SearchIterator & SearchIterator::operator=(const SearchIterator &that)
-{
-    if (this != &that) {
-        m_breakiterator_            = that.m_breakiterator_;
-        m_text_                     = that.m_text_;
-        m_search_->breakIter        = that.m_search_->breakIter;
-        m_search_->isCanonicalMatch = that.m_search_->isCanonicalMatch;
-        m_search_->isOverlap        = that.m_search_->isOverlap;
-        m_search_->elementComparisonType = that.m_search_->elementComparisonType;
-        m_search_->matchedIndex     = that.m_search_->matchedIndex;
-        m_search_->matchedLength    = that.m_search_->matchedLength;
-        m_search_->text             = that.m_search_->text;
-        m_search_->textLength       = that.m_search_->textLength;
-    }
-    return *this;
-}
-    
-    class UnicodeSet;
+    U_CAPI void U_EXPORT2
+uhash_deleteScriptSet(void *obj);
     
     class U_I18N_API SharedNumberFormat : public SharedObject {
 public:
@@ -167,123 +147,4 @@ private:
     NumberFormat *ptr;
     SharedNumberFormat(const SharedNumberFormat &);
     SharedNumberFormat &operator=(const SharedNumberFormat &);
-};
-    
-        /**
-     * Make this instance have no limit on significant digits.
-     */
-    void clear() {
-        fMin = 0;
-        fMax = INT32_MAX;
-    }
-    
-    UnicodeString &
-SmallIntFormatter::format(
-        int32_t smallPositiveValue,
-        const IntDigitCountRange &range,
-        UnicodeString &appendTo) {
-    int32_t digits = range.pin(gDigitCount[smallPositiveValue]);
-    }
-    
-        /**
-     * Start offset, in the match text, of the <em>rightmost</em>
-     * match.
-     */
-    int32_t matchStart;
-    
-    
-    {    return;
-}
-    
-        if(n == 0)
-        return NULL;
-    
-    
-    {        return res;
-    }
-    
-            int count[3] = {0};
-        for(int i = 0 ; i < nums.size() ; i ++){
-            assert(nums[i] >= 0 && nums[i] <= 2);
-            count[nums[i]] ++;
-        }
-    
-        vector<int> vec2 = {2};
-    Solution().sortColors(vec2);
-    printArr(vec2);
-    
-            ListNode* pre = dummyHead;
-        for(int i = 0; i < m - 1; i ++){
-            pre = pre->next
-        }
-    
-            vector<int> res;
-        if( root == NULL )
-            return res;
-    
-    
-    {
-    {                if(prev->right == NULL){
-                    prev->right = cur;
-                    cur = cur->left;
-                }
-                else{
-                    prev->right = NULL;
-                    res.push_back(cur->val);
-                    cur = cur->right;
-                }
-            }
-        }
-    
-    
-    {
-    {        if(node){
-            res.push_back(node->val);
-            preorderTraversal(node->left, res);
-            preorderTraversal(node->right, res);
-        }
-    }
-};
-    
-    // Recursive
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
-public:
-    vector<int> postorderTraversal(TreeNode* root) {
-    }
-    }
-    
-            vector<int> res;
-        if(root == NULL)
-            return res;
-    
-    DEFINE_string(adapter_config_filename, 'modules/canbus/conf/adapter.conf',
-              'The adapter config file');
-    
-    #include 'modules/common/time/time.h'
-#include 'modules/drivers/canbus/common/byte.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
-#include 'modules/drivers/radar/conti_radar/protocol/const_vars.h'
-    
-    int ObjectGeneralInfo60B::object_id(const std::uint8_t* bytes,
-                                    int32_t length) const {
-  Byte t0(bytes);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-    unsigned int BaseMapMatrix::CreateBinary(unsigned char* buf,
-                                         unsigned int buf_size) const {
-  return 0;
-}
-    
-    
-    {  MatrixXd bd_golden(10, 1);
-  bd_golden << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
-  EXPECT_EQ(bd, bd_golden);
-}
-    
-    class Accelrpt68Test : public ::testing::Test {
- public:
-  virtual void SetUp() {}
 };
