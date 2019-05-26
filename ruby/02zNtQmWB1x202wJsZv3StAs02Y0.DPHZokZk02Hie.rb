6d33883f15faeec551b5a7dfd16d1b2df3275bab
SUNsung <weatherius@gmail.com>
2019-05-26 16:16:59 +0300

    Rewrite [02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb] File in: 2019-05-26 16:16:58

--- ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
+++ ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
@@ -1,173 +1,95 @@
 
         
-        require 'benchmark/ips'
-require 'jekyll'
-require 'json'
-    
-    require 'benchmark/ips'
-require 'pathutil'
-    
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-          # rubocop:disable Metrics/ParameterLists
-      def before_step_result(_keyword, _step_match, _multiline_arg, status, exception, \
-              _source_indent, background, _file_colon_line)
-    
-            parsed_expr = parse_expression(expression)
-        @context.stack do
-          groups = input.group_by do |item|
-            @context[variable] = item
-            parsed_expr.render(@context)
-          end
-          grouped_array(groups)
-        end
-      end
-    
-          File.write(new_path, '1')
-      false
-    end
-    
-        def run!
-      require_program(:version, :description)
-      trap('INT') { abort(program(:int_message)) } if program(:int_message)
-      trap('INT') { program(:int_block).call } if program(:int_block)
-      global_option('-h', '--help', 'Display help documentation') do
-        args = @args - %w(-h --help)
-        command(:help).run(*args)
-        return
-      end
-      global_option('-v', '--version', 'Display version information') do
-        say(version)
-        return
-      end
-      parse_global_options
-      remove_global_options(options, @args)
-    
-          it 'adds options param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            options: '--use-single-star --keep-intermediate-files --search-undocumented-doc'
-          )
-        end').runner.execute(:test)
-    
-          it 'sets the platform to watchOS' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              platform: 'watchOS'
-            )
-          end').runner.execute(:test)
-    
-    module CrossplatformShellwords
-  # handle switching between implementations of shellescape
-  def shellescape(str)
-    if FastlaneCore::Helper.windows?
-      WindowsShellwords.shellescape(str)
-    else
-      # using `escape` instead of expected `shellescape` here
-      # which corresponds to Shellword's `String.shellescape` implementation
-      # https://github.com/ruby/ruby/blob/1cf2bb4b2085758112503e7da7414d1ef52d4f48/lib/shellwords.rb#L216
-      Shellwords.escape(str)
-    end
-  end
-  module_function :shellescape
-    
-            c.action do |args, options|
-          Cert.config = FastlaneCore::Configuration.create(Cert::Options.available_options, options.__hash__)
-          Cert::Runner.new.launch
-        end
-      end
-    
-    require 'English'
-    
+        module Devise
+  module Controllers
+    # Provide the ability to store a location.
+    # Used to redirect back to a desired path after sign in.
+    # Included by default in all controllers.
+    module StoreLocation
+      # Returns and delete (if it's navigational format) the url stored in the session for
+      # the given scope. Useful for giving redirect backs after sign up:
       #
-  # Dispatches the supplied request for a given connection.
-  #
-  def dispatch_request(cli, request)
-    # Is the client requesting keep-alive?
-    if ((request['Connection']) and
-       (request['Connection'].downcase == 'Keep-Alive'.downcase))
-      cli.keepalive = true
+      # Example:
+      #
+      #   redirect_to stored_location_for(:user) || root_path
+      #
+      def stored_location_for(resource_or_scope)
+        session_key = stored_location_key_for(resource_or_scope)
+    
+        def default_constraints(options)
+      @constraints = Hash.new
+      @constraints.merge!(options[:constraints]) if options[:constraints]
     end
     
-                k1 = OpenSSL::HMAC.digest('MD5', key, [msg_type].pack('V'))
-            k3 = OpenSSL::HMAC.digest('MD5', k1, checksum)
-    
-                seq_asn1 = OpenSSL::ASN1::ASN1Data.new([seq], AP_REQ, :APPLICATION)
-    
-                seq_values.each do |val|
-              case val.tag
-              when 0
-                self.etype = decode_etype(val)
-              when 1
-                self.kvno = decode_kvno(val)
-              when 2
-                self.cipher = decode_cipher(val)
-              else
-                raise ::RuntimeError, 'Failed to decode EncryptedData SEQUENCE'
-              end
-            end
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an
-          # OpenSSL::ASN1::Sequence
-          #
-          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
-          def decode_asn1(input)
-            seq_values = input.value
-            self.type = decode_type(seq_values[0])
-            self.value = decode_value(seq_values[1])
-          end
-    
-      namespace :install do
-    GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
-      desc 'Build and install #{gem} as local gem'
-      task gem => package(gem, '.gem') do
-        sh 'gem install #{package(gem, '.gem')}'
+          def extend_remember_period
+        self.class.extend_remember_period
       end
+    
+        def resource_params
+      params.require(:email_domain_block).permit(:domain)
     end
+  end
+end
+
     
-        # we assume that the first file that requires 'sinatra' is the
-    # app_file. all other path related options are calculated based
-    # on this path by default.
-    set :app_file, caller_files.first || $0
-    
-      task :index do
-    doc = File.read('README.md')
-    file = 'doc/rack-protection-readme.md'
-    Dir.mkdir 'doc' unless File.directory? 'doc'
-    puts 'writing #{file}'
-    File.open(file, 'w') { |f| f << doc }
+      def hub_callback
+    params['hub.callback']
   end
     
-            safe?(env) ||
-          valid_token?(session, env['HTTP_X_CSRF_TOKEN']) ||
-          valid_token?(session, Request.new(env).params[options[:authenticity_param]]) ||
-          ( options[:allow_if] && options[:allow_if].call(env) )
-      end
+    class Api::Web::SettingsController < Api::Web::BaseController
+  respond_to :json
     
-          def encrypt(value)
-        options[:encryptor].hexdigest value.to_s
-      end
+      def session_needs_update?
+    !current_session.nil? && current_session.updated_at < UPDATE_SIGN_IN_HOURS.hours.ago
+  end
+end
+
     
-          def csp_policy
-        directives = []
+                  message = format(MESSAGES[i - 1], arg_value: arguments[i].source)
     
-          def redirect(env)
-        request = Request.new(env)
-        warn env, 'attack prevented by #{self.class}'
-        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
+            def autocorrect(node)
+          UnusedArgCorrector.correct(processed_source, node)
+        end
+    
+        # Outputs a list of categories as comma-separated <a> links. This is used
+    # to output the category list for each post on a category page.
+    #
+    #  +categories+ is the list of categories to format.
+    #
+    # Returns string
+    #
+    def category_links(categories)
+      categories.sort.map { |c| category_link c }.join(', ')
+    end
+    
+            Dir.chdir(includes_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(file)
+            source = File.read(file)
+            partial = Liquid::Template.parse(source)
+            context.stack do
+              rtn = rtn + partial.render(context)
+            end
+          else
+            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
+          end
+        end
       end
+      rtn
+    end
+  end
+    
+    module OctopressFilters
+  def self.pre_filter(page)
+    if page.ext.match('html|textile|markdown|md|haml|slim|xml')
+      input = BacktickCodeBlock::render_code_block(page.content)
+      page.content = input.gsub /(<figure.+?>.+?<\/figure>)/m do
+        TemplateWrapper::safe_wrap($1)
+      end
+    end
+  end
+  def self.post_filter(page)
+    if page.ext.match('html|textile|markdown|md|haml|slim|xml')
+      page.output = TemplateWrapper::unwrap(page.output)
+    end
+  end
