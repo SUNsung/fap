739feeafad592ac73ef3d134bebe96a6bca23ea9
SUNsung <weatherius@gmail.com>
2019-05-26 18:46:44 +0300

    Rewrite [13EpPWLQX1oo13gy.xBrSUR1o13ZPmlBO.6pQ13nk.py] File in: 2019-05-26 18:46:44

--- pyton/13EpPWLQX1oo13gy.xBrSUR1o13ZPmlBO.6pQ13nk.py
+++ pyton/13EpPWLQX1oo13gy.xBrSUR1o13ZPmlBO.6pQ13nk.py
@@ -1,57 +1,95 @@
 
         
-        
-class ArrayMaxLengthValidator(MaxLengthValidator):
-    message = ngettext_lazy(
-        'List contains %(show_value)d item, it should contain no more than %(limit_value)d.',
-        'List contains %(show_value)d items, it should contain no more than %(limit_value)d.',
-        'limit_value')
+            # Define the loss function and the optimization algorithm
+    self.cross_entropies = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits=self.scores, labels=tf.reduce_mean(self.batch_labels))
+    self.cost = tf.reduce_sum(self.cross_entropies, name='cost')
+    self.global_step = tf.Variable(0, name='global_step', trainable=False)
+    self.optimizer = tf.train.AdamOptimizer()
+    self.train_op = self.optimizer.minimize(self.cost,
+                                            global_step=self.global_step)
     
-        def __init__(self, get_response=None):
-        if not apps.is_installed('django.contrib.sites'):
-            raise ImproperlyConfigured(
-                'You cannot use RedirectFallbackMiddleware when '
-                'django.contrib.sites is not installed.'
-            )
-        super().__init__(get_response)
+        Args:
+      data_dict: The dictionary of data (training and validation) used for
+        training and evaluation of the model, respectively.
     
-        def flush(self):
-        '''
-        Remove the current session data from the database and regenerate the
-        key.
-        '''
-        self.clear()
-        self.delete(self.session_key)
-        self._session_key = None
+      keys = ['train_truth', 'train_ext_input', 'valid_data',
+          'valid_truth', 'valid_ext_input', 'valid_train']
+  for k in keys:
+    if k not in data_dict:
+      data_dict[k] = None
+    
+    
+def _LoadModel(gd_file, ckpt_file):
+  '''Load the model from GraphDef and Checkpoint.
+    
+      for batch in range(num_batches):
+    x = np.zeros([batch_size, num_steps], dtype=np.int32)
+    y = np.zeros([batch_size, num_steps], dtype=np.int32)
+    w = np.zeros([batch_size, num_steps], dtype=np.float)
+    
+      if epoch_size == 0:
+    raise ValueError('epoch_size == 0, decrease batch_size or num_steps')
+    
+      if FLAGS.data_set == 'ptb':
+    model_str = 'Model'
+  else:
+    model_str = 'model'
+    
+    
+class CompleterAvailableRequest( BaseRequest ):
+  def __init__( self, filetypes ):
+    super( CompleterAvailableRequest, self ).__init__()
+    self.filetypes = filetypes
+    self._response = None
+    
+    
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+@YouCompleteMeInstance()
+@patch( 'ycm.client.base_request._logger', autospec = True )
+@patch( 'ycm.vimsupport.PostVimMessage', new_callable = ExtendedMock )
+def SendCompletionRequest_ErrorFromServer_test( ycm,
+                                                post_vim_message,
+                                                logger ):
+  current_buffer = VimBuffer( 'buffer' )
+  with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
+    with MockCompletionRequest( ServerError( 'Server error' ) ):
+      ycm.SendCompletionRequest()
+      ok_( ycm.CompletionRequestReady() )
+      response = ycm.GetCompletionResponse()
+      logger.exception.assert_called_with( 'Error while handling server '
+                                           'response' )
+      post_vim_message.assert_has_exact_calls( [
+        call( 'Server error', truncate = True )
+      ] )
+      assert_that(
+        response,
+        has_entries( {
+          'completions': empty(),
+          'completion_start_column': -1
+        } )
+      )
 
     
     
-class AbstractBaseSession(models.Model):
-    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
-    session_data = models.TextField(_('session data'))
-    expire_date = models.DateTimeField(_('expire date'), db_index=True)
+def MockAsyncServerResponseException( exception ):
+  '''Return a fake future object that is complete, but raises an exception.
+  Suitable for mocking a response future within a client request. For example:
     
-            if row1 == row2:
-            ciphertext += table[row1*5+(col1+1)%5]
-            ciphertext += table[row2*5+(col2+1)%5]
-        elif col1 == col2:
-            ciphertext += table[((row1+1)%5)*5+col1]
-            ciphertext += table[((row2+1)%5)*5+col2]
-        else: # rectangle
-            ciphertext += table[row1*5+col2]
-            ciphertext += table[row2*5+col1]
+    # If false, no module index is generated.
+#html_use_modindex = True
     
-                return self.dp[x][y]
     
-        def __init__(self, arr):
-        # we need a list not a string, so do something to change the type
-        self.array = arr.split(',')
-        print(('the input array is:', self.array))
+if __name__ == '__main__':
+    main()
     
-    #Mean Bias Deviation
-def mbd(predict, actual):
-    predict = np.array(predict)
-    actual = np.array(actual)
-    
-        freqPairs = list(freqToLetter.items())
-    freqPairs.sort(key = getItemAtIndexZero, reverse = True)
+        def insert(self, node):
+        self.array.append(node)
+        return self.array[-1]
