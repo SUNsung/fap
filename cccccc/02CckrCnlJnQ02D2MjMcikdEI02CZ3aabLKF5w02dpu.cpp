ef4efa3f4639d982f48528fe176118fc8df8f1dd
SUNsung <weatherius@gmail.com>
2019-05-26 14:47:24 +0300

    Rewrite [02CckrCnlJnQ02D2MjMcikdEI02CZ3aabLKF5w02dpu.cpp] File in: 2019-05-26 14:47:24

--- cccccc/02CckrCnlJnQ02D2MjMcikdEI02CZ3aabLKF5w02dpu.cpp
+++ cccccc/02CckrCnlJnQ02D2MjMcikdEI02CZ3aabLKF5w02dpu.cpp
@@ -1,256 +1,243 @@
 
         
-        bool ParagraphModel::ValidBodyLine(int lmargin, int lindent,
-                                   int rindent, int rmargin) const {
-  switch (justification_) {
-    case JUSTIFICATION_LEFT:
-      return NearlyEqual(lmargin + lindent, margin_ + body_indent_,
-                         tolerance_);
-    case JUSTIFICATION_RIGHT:
-      return NearlyEqual(rmargin + rindent, margin_ + body_indent_,
-                         tolerance_);
-    case JUSTIFICATION_CENTER:
-      return NearlyEqual(lindent, rindent, tolerance_ * 2);
-    default:
-      // shouldn't happen
-      return false;
-  }
-}
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+#define TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
     
-    #include 'errcode.h'
-#include 'genericvector.h'
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    // Called by python code on initialization.
+//
+// 'trampoline' must represent a python function which has the
+// following signature:
+//   (string, list(ndarray)) | (string, list(EagerTensor)) ->
+//     ndarray | list(ndarray) | python scalar |
+//     EagerTensor | list(EagerTensor) | None
+//
+// The trampoline takes two arguments, the first is a string token
+// used by the python frontend's dispatching logic; the second is a
+// list of numpy ndarrays or EagerTensor objects. It can return a
+// single numpy ndarray, a list of numpy ndarrays, a python scalar, an
+// EagerTensor, a list of EagerTensors, or None.
+//
+// PyFunc requires inputs and outputs to be ndarrays. EagerPyFunc requires
+// inputs to be a list of EagerTensors and outputs to be an EagerTensor, a list
+// of EagerTensors, or None.
+//
+// The C++ runtime converts outputs back to Tensor objects.
+//
+// This function is called by script_ops.py during its module initialization.
+//
+// TODO(zhifengc): Support distributed runtime.
+void InitializePyTrampoline(PyObject* trampoline);
     
     
-    {}  // namespace grpc
-    
-    Status ProtoServerReflection::ServerReflectionInfo(
-    ServerContext* context,
-    ServerReaderWriter<ServerReflectionResponse, ServerReflectionRequest>*
-        stream) {
-  ServerReflectionRequest request;
-  ServerReflectionResponse response;
-  Status status;
-  while (stream->Read(&request)) {
-    switch (request.message_request_case()) {
-      case ServerReflectionRequest::MessageRequestCase::kFileByFilename:
-        status = GetFileByName(context, request.file_by_filename(), &response);
-        break;
-      case ServerReflectionRequest::MessageRequestCase::kFileContainingSymbol:
-        status = GetFileContainingSymbol(
-            context, request.file_containing_symbol(), &response);
-        break;
-      case ServerReflectionRequest::MessageRequestCase::
-          kFileContainingExtension:
-        status = GetFileContainingExtension(
-            context, &request.file_containing_extension(), &response);
-        break;
-      case ServerReflectionRequest::MessageRequestCase::
-          kAllExtensionNumbersOfType:
-        status = GetAllExtensionNumbers(
-            context, request.all_extension_numbers_of_type(),
-            response.mutable_all_extension_numbers_response());
-        break;
-      case ServerReflectionRequest::MessageRequestCase::kListServices:
-        status =
-            ListService(context, response.mutable_list_services_response());
-        break;
-      default:
-        status = Status(StatusCode::UNIMPLEMENTED, '');
-    }
-    }
-    }
-    
-    CreateThreadPoolFunc g_ctp_impl = CreateDefaultThreadPoolImpl;
-    
-    // Allows different codebases to use their own thread pool impls
-typedef ThreadPoolInterface* (*CreateThreadPoolFunc)(void);
-void SetCreateThreadPool(CreateThreadPoolFunc func);
-    
-    void CoreStatsToProto(const grpc_stats_data& core, Stats* proto) {
-  for (int i = 0; i < GRPC_STATS_COUNTER_COUNT; i++) {
-    Metric* m = proto->add_metrics();
-    m->set_name(grpc_stats_counter_name[i]);
-    m->set_count(core.counters[i]);
-  }
-  for (int i = 0; i < GRPC_STATS_HISTOGRAM_COUNT; i++) {
-    Metric* m = proto->add_metrics();
-    m->set_name(grpc_stats_histogram_name[i]);
-    Histogram* h = m->mutable_histogram();
-    for (int j = 0; j < grpc_stats_histo_buckets[i]; j++) {
-      Bucket* b = h->add_buckets();
-      b->set_start(grpc_stats_histo_bucket_boundaries[i][j]);
-      b->set_count(core.histograms[grpc_stats_histo_start[i] + j]);
-    }
-  }
-}
-    
-    namespace mxnet {
-namespace io {
-/*! \return the parameter of default augmenter */
-std::vector<dmlc::ParamFieldInfo> ListDefaultAugParams();
-std::vector<dmlc::ParamFieldInfo> ListDefaultDetAugParams();
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_IO_IMAGE_AUGMENTER_H_
+    {}  // namespace tensorflow
 
     
-      ~PrefetcherIter() {
-    while (recycle_queue_.size() != 0) {
-      DataBatch *batch = recycle_queue_.front();
-      recycle_queue_.pop();
-      delete batch;
-    }
-    delete out_;
-    iter.Destroy();
-  }
+    #include 'tensorflow/core/framework/node_def.pb.h'
+#include 'tensorflow/core/framework/node_def_util.h'
+#include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
+#include 'tensorflow/core/framework/types.h'
+#include 'tensorflow/core/lib/core/status.h'
+#include 'tensorflow/core/util/device_name_utils.h'
     
-    /*!
- * \file gradient_compression-inl.h
- * \author Rahul Huilgol
- * \brief Declares and defines functions used to quantize and dequantize data
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    bool HostTimer::Stop(Stream* stream) {
+  return stream->ThenDoHostCallback([this]() { this->StopNow(); }).ok();
+}
+    
+      // Stops the timer at the present point in the stream.
+  bool Stop(Stream *stream);
+    
+    UnicodeString &ScientificNumberFormatter::MarkupStyle::format(
+        const UnicodeString &original,
+        FieldPositionIterator &fpi,
+        const UnicodeString &preExponent,
+        const DecimalFormatStaticSets & /*unusedDecimalFormatSets*/,
+        UnicodeString &appendTo,
+        UErrorCode &status) const {
+    if (U_FAILURE(status)) {
+        return appendTo;
+    }
+    FieldPosition fp;
+    int32_t copyFromOffset = 0;
+    while (fpi.next(fp)) {
+        switch (fp.getField()) {
+        case UNUM_EXPONENT_SYMBOL_FIELD:
+            appendTo.append(
+                    original,
+                    copyFromOffset,
+                    fp.getBeginIndex() - copyFromOffset);
+            copyFromOffset = fp.getEndIndex();
+            appendTo.append(preExponent);
+            appendTo.append(fBeginMarkup);
+            break;
+        case UNUM_EXPONENT_FIELD:
+            appendTo.append(
+                    original,
+                    copyFromOffset,
+                    fp.getEndIndex() - copyFromOffset);
+            copyFromOffset = fp.getEndIndex();
+            appendTo.append(fEndMarkup);
+            break;
+        default:
+            break;
+        }
+    }
+    appendTo.append(
+            original, copyFromOffset, original.length() - copyFromOffset);
+    return appendTo;
+}
+    
+    ScriptSet & ScriptSet::operator =(const ScriptSet &other) {
+    for (uint32_t i=0; i<UPRV_LENGTHOF(bits); i++) {
+        bits[i] = other.bits[i];
+    }
+    return *this;
+}
+    
+    #define LOW_A             ((UChar)0x0061)
+#define LOW_B             ((UChar)0x0062)
+#define LOW_C             ((UChar)0x0063)
+#define LOW_D             ((UChar)0x0064)
+#define LOW_E             ((UChar)0x0065)
+#define LOW_F             ((UChar)0x0066)
+#define LOW_G             ((UChar)0x0067)
+#define LOW_H             ((UChar)0x0068)
+#define LOW_I             ((UChar)0x0069)
+#define LOW_J             ((UChar)0x006a)
+#define LOW_K             ((UChar)0x006B)
+#define LOW_L             ((UChar)0x006C)
+#define LOW_M             ((UChar)0x006D)
+#define LOW_N             ((UChar)0x006E)
+#define LOW_O             ((UChar)0x006F)
+#define LOW_P             ((UChar)0x0070)
+#define LOW_Q             ((UChar)0x0071)
+#define LOW_R             ((UChar)0x0072)
+#define LOW_S             ((UChar)0x0073)
+#define LOW_T             ((UChar)0x0074)
+#define LOW_U             ((UChar)0x0075)
+#define LOW_V             ((UChar)0x0076)
+#define LOW_W             ((UChar)0x0077)
+#define LOW_X             ((UChar)0x0078)
+#define LOW_Y             ((UChar)0x0079)
+#define LOW_Z             ((UChar)0x007A)
+    
+    #endif /* #if !UCONFIG_NO_BREAK_ITERATION */
+
+    
+    void
+SimpleTimeZone::initTransitionRules(UErrorCode& status) {
+    if (U_FAILURE(status)) {
+        return;
+    }
+    if (transitionRulesInitialized) {
+        return;
+    }
+    deleteTransitionRules();
+    UnicodeString tzid;
+    getID(tzid);
+    }
+    
+    #include 'unicode/uobject.h'
+#include 'unicode/utypes.h'
+    
+    #include 'unicode/utypes.h'
+    
+        /**
+     * Remove any match data.  This must be called before performing a
+     * set of matches with this segment.
+     */
+    void resetMatch();
+    
+            for (oOutput=0; oOutput<output.length(); ) {
+            if (oOutput == cursorPos) {
+                // Record the position of the cursor
+                newStart = destLimit - destStart; // relative to start
+            }
+            UChar32 c = output.char32At(oOutput);
+            UnicodeReplacer* r = data->lookupReplacer(c);
+            if (r == NULL) {
+                // Accumulate straight (non-segment) text.
+                buf.append(c);
+            } else {
+                isComplex = TRUE;
+    }
+    }
+    
+    #endif
+
+    
+    namespace HPHP { namespace HHBBC {
+    }
+    }
+    
+    
+    {  // We had a counted inner array---we need to do an O(N) copy to get the
+  // collection into the request local heap.
+  auto const apcArr = APCArray::fromHandle(m_arrayHandle);
+  auto const col = Object::attach(collections::alloc(m_colType));
+  switch (m_colType) {
+  case CollectionType::ImmVector:
+  case CollectionType::Vector:
+    fillCollection(static_cast<BaseVector*>(col.get()), apcArr);
+    break;
+  case CollectionType::ImmSet:
+  case CollectionType::Set:
+    fillCollection(static_cast<BaseSet*>(col.get()), apcArr);
+    break;
+  case CollectionType::ImmMap:
+  case CollectionType::Map:
+    fillMap(static_cast<BaseMap*>(col.get()), apcArr);
+    break;
+  case CollectionType::Pair:
+    always_assert(0);
+    break;
+  }
+  return col;
+}
+    
+    void Config::ParseIniFile(const std::string &filename, IniSettingMap &ini,
+                          const bool constants_only /* = false */,
+                          const bool is_system /* = true */ ) {
+    std::ifstream ifs(filename);
+    std::string str((std::istreambuf_iterator<char>(ifs)),
+                    std::istreambuf_iterator<char>());
+    std::string with_includes;
+    Config::ReplaceIncludesWithIni(filename, str, with_includes);
+    Config::SetParsedIni(ini, with_includes, filename, constants_only,
+                         is_system);
+}
+    
+    /*
+ * Attempt to log an entry to the perf warning service.
+ *
+ * If StructuredLog::enabled() returns false or this event is discarded by the
+ * effective sample rate, nothing will be logged. If both of those checks pass,
+ * fillCols will be passed a StructuredLogEntry& to populate, which will then
+ * be logged. The column names 'event_name' and 'priority' are reserved and
+ * will be overwritten is fillCols() sets them.
+ *
+ * The effective sample rate is determined by Eval.PerfWarningSampleRate * rate
+ * (or kDefaultPerfWarningRate for the overloads that don't take a rate). If
+ * the effective sample rate is 0, all events will be discarded.
  */
-#ifndef MXNET_KVSTORE_GRADIENT_COMPRESSION_INL_H_
-#define MXNET_KVSTORE_GRADIENT_COMPRESSION_INL_H_
+template<typename F>
+void logPerfWarning(folly::StringPiece event, F fillCols);
+template<typename F>
+void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
     
-    #if MXNET_USE_DIST_KVSTORE
-#include './kvstore_dist.h'
-std::atomic<int> mxnet::kvstore::KVStoreDist::customer_id_{0};
-#endif  // MXNET_USE_DIST_KVSTORE
-#if MXNET_USE_NCCL
-#include './kvstore_nccl.h'
-#endif  // MXNET_USE_NCCL
-    
-      std::vector<Tensor<cpu, 2, DType> > ts_t_arr;
-  for (int i = 0; i < static_cast<int>(ts_arr.size()); ++i) {
-    ts_t_arr.emplace_back(Shape2(ts_arr[i].size(1), ts_arr[i].size(0)));
-    AllocSpace(&ts_t_arr[i]);
-    flip<cpu, DType>(ts_arr[i].size(0), ts_arr[i].size(1), ts_t_arr[i].dptr_,
-      ts_t_arr[i].stride_, ts_arr[i].dptr_, ts_arr[i].stride_);
-  }
-    
-    /*!
- *  Copyright (c) 2018 by Contributors
- * \file transformer.cc
- * \brief CPU implementation of the operators used in Transformer
- */
-#include <mxnet/base.h>
-#include './transformer-inl.h'
-#include '../tensor/elemwise_unary_op.h'
-    
-      ~CuDNNBilinearSamplerOp() {
-    if (init_cudnn_) {
-      CUDNN_CALL(cudnnDestroySpatialTransformerDescriptor(st_desc_));
-      CUDNN_CALL(cudnnDestroyTensorDescriptor(in_desc_));
-      CUDNN_CALL(cudnnDestroyTensorDescriptor(out_desc_));
-    }
-  }
-    
-    MXNET_REGISTER_OP_PROPERTY(IdentityAttachKLSparseReg, IdentityAttachKLSparseRegProp)
-.describe('Apply a sparse regularization to the output a sigmoid activation function.')
-.add_argument('data', 'NDArray-or-Symbol', 'Input data.')
-.add_arguments(IdentityAttachKLSparseRegParam::__FIELDS__());
-    
-    void ImGui_ImplGLUT_KeyboardUpFunc(unsigned char c, int x, int y)
-{
-    //printf('char_up_func %d '%c'\n', c, c);
-    ImGuiIO& io = ImGui::GetIO();
-    if (c >= 1 && c <= 26)
-        io.KeysDown[c] = io.KeysDown[c - 1 + 'a'] = io.KeysDown[c - 1 + 'A'] = false;
-    else if (c >= 'a' && c <= 'z')
-        io.KeysDown[c] = io.KeysDown[c - 'a' + 'A'] = false;
-    else if (c >= 'A' && c <= 'Z')
-        io.KeysDown[c] = io.KeysDown[c - 'A' + 'a'] = false;
-    else
-        io.KeysDown[c] = false;
-    ImGui_ImplGLUT_UpdateKeyboardMods();
-    (void)x; (void)y; // Unused
-}
-    
-    IMGUI_IMPL_API bool     ImGui_ImplDX10_Init(ID3D10Device* device);
-IMGUI_IMPL_API void     ImGui_ImplDX10_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplDX10_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplDX10_RenderDrawData(ImDrawData* draw_data);
-    
-    #include <stdint.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-#include 'imgui.h'
-#include 'imgui_impl_allegro5.h'
-    
-            // Rendering
-        ImGui::Render();
-        IwGxSetColClear(clear_color.x * 255, clear_color.y * 255, clear_color.z * 255, clear_color.w * 255);
-        IwGxClear();
-        ImGui_Marmalade_RenderDrawData(ImGui::GetDrawData());
-        IwGxSwapBuffers();
-    
-    int main(int, char**)
-{
-    // Setup window
-    glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit())
-        return 1;
-    GLFWwindow* window = glfwCreateWindow(1280, 720, 'Dear ImGui GLFW+OpenGL2 example', NULL, NULL);
-    if (window == NULL)
-        return 1;
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
-    }
-    
-    IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_Init(const char* glsl_version = NULL);
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
-    
-    
-    {    InputTextCallback_UserData cb_user_data;
-    cb_user_data.Str = str;
-    cb_user_data.ChainCallback = callback;
-    cb_user_data.ChainCallbackUserData = user_data;
-    return InputText(label, (char*)str->c_str(), str->capacity() + 1, flags, InputTextCallback, &cb_user_data);
-}
-    
-    DHTReplaceNodeTask::~DHTReplaceNodeTask() = default;
-    
-    void DHTRoutingTable::dropNode(const std::shared_ptr<DHTNode>& node)
-{
-  getBucketFor(node)->dropNode(node);
-}
-/*
-  void DHTRoutingTable::moveBucketHead(const std::shared_ptr<DHTNode>& node)
-  {
-  getBucketFor(node)->moveToHead(node);
-  }
-*/
-void DHTRoutingTable::moveBucketTail(const std::shared_ptr<DHTNode>& node)
-{
-  getBucketFor(node)->moveToTail(node);
-}
-    
-        routingTable->setTaskQueue(taskQueue.get());
-    routingTable->setTaskFactory(taskFactory.get());
-    
-    #endif // D_DHT_SETUP_H
-
-    
-    #endif // D_DHT_TASK_FACTORY_H
-
-    
-    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
-                                             std::chrono::seconds interval)
-    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
-{
-}
-    
-    #endif // D_DHT_TOKEN_UPDATE_COMMAND_H
-
-    
-    DNSCache::CacheEntry& DNSCache::CacheEntry::operator=(const CacheEntry& c)
-{
-  if (this != &c) {
-    hostname_ = c.hostname_;
-    port_ = c.port_;
-    addrEntries_ = c.addrEntries_;
-  }
-  return *this;
-}
+      FILE *getStream() { return m_stream;}
