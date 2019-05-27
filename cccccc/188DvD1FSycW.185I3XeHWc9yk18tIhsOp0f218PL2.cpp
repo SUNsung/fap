fa7d9c6f5b4ac26e10fb9d15149c8b400d651238
SUNsung <weatherius@gmail.com>
2019-05-27 11:46:00 +0300

    Rewrite [188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp] File in: 2019-05-27 11:45:59

--- cccccc/188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp
+++ cccccc/188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp
@@ -1,40 +1,167 @@
 
         
-        // Author: qrczak@google.com (Marcin Kowalczyk)
+        
+    {}  // namespace atom
+
     
-    
-    {  string comments;
-  SourceLocation location;
-  if (descriptor_->GetSourceLocation(&location)) {
-    comments = BuildCommentsString(location, true);
-  } else {
-    comments = '';
-  }
-  variables_['comments'] = comments;
-}
-    
-    static inline int internalInflateInit2(
-    z_stream* zcontext, GzipInputStream::Format format) {
-  int windowBitsFormat = 0;
-  switch (format) {
-    case GzipInputStream::GZIP: windowBitsFormat = 16; break;
-    case GzipInputStream::AUTO: windowBitsFormat = 32; break;
-    case GzipInputStream::ZLIB: windowBitsFormat = 0; break;
-  }
-  return inflateInit2(zcontext, /* windowBits */15 | windowBitsFormat);
-}
-    
-    void MapLiteTestUtil::ExpectMapFieldsSetInitialized(
-    const unittest::TestMapLite& message) {
-  MapTestUtilImpl::ExpectMapFieldsSetInitialized<unittest::MapEnumLite,
-                                                 unittest::MAP_ENUM_FOO_LITE>(
-      message);
-}
-    
-    Status::Status() : error_code_(error::OK) {
-}
-    
-    TEST(TemplateUtilTest, TestTemplateTypeEquals) {
-  // Check that the TemplateTypeEquals works correctly.
-  bool value = false;
+    namespace atom {
     }
+    
+      // content::JavaScriptDialogManager implementations.
+  void RunJavaScriptDialog(content::WebContents* web_contents,
+                           content::RenderFrameHost* rfh,
+                           content::JavaScriptDialogType dialog_type,
+                           const base::string16& message_text,
+                           const base::string16& default_prompt_text,
+                           DialogClosedCallback callback,
+                           bool* did_suppress_message) override;
+  void RunBeforeUnloadDialog(content::WebContents* web_contents,
+                             content::RenderFrameHost* rfh,
+                             bool is_reload,
+                             DialogClosedCallback callback) override;
+  void CancelDialogs(content::WebContents* web_contents,
+                     bool reset_state) override;
+    
+    
+    {}  // namespace atom
+
+    
+    namespace c10d {
+namespace test {
+    }
+    }
+    
+    REGISTER_CPU_OPERATOR(
+    MergeSingleMapFeatureTensors,
+    MergeSingleMapFeatureTensorsOp<CPUContext>);
+OPERATOR_SCHEMA(MergeSingleMapFeatureTensors)
+    .SetDoc(
+        'Merge given single-feature tensors with map features into one '
+        'multi-feature tensor.' +
+        doc)
+    .NumInputs([](int n) { return n >= 4 && n % 4 == 0; })
+    .NumOutputs(5)
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_keys', '.keys')
+    .Input(2, 'in1_values', '.values')
+    .Input(3, 'in1_presence', '.presence')
+    .Output(0, 'out_lengths', '.lengths')
+    .Output(1, 'out_keys', '.keys')
+    .Output(2, 'out_values_lengths', '.values.lengths')
+    .Output(3, 'out_values_keys', '.values.keys')
+    .Output(4, 'out_values_values', '.values.values')
+    .Arg('feature_ids', 'feature ids');
+    
+    X before running op:
+[[ 3.813361   -1.319647    5.2089314  -4.931328    0.6218652 ]
+ [ 7.2757645   5.5552588   5.785643   -2.4790506  -0.41400087]
+ [ 1.1541046  -6.933266    3.3754056   1.6569928  -1.7670316 ]
+ [-3.4932013   4.891472    1.5530115  -3.2443287  -4.605099  ]
+ [-4.574543   -7.360948    5.91305    -8.196495   -5.357458  ]]
+X after running op:
+[[ 3. -2.  5. -5.  0.]
+ [ 7.  5.  5. -3. -1.]
+ [ 1. -7.  3.  1. -2.]
+ [-4.  4.  1. -4. -5.]
+ [-5. -8.  5. -9. -6.]]
+    
+    template <typename T, class Context>
+class BernoulliJSDGradientOp final : public Operator<Context> {
+ public:
+  USE_SIMPLE_CTOR_DTOR(BernoulliJSDGradientOp);
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  bool RunOnDevice() override;
+};
+    
+    #endif  // STORAGE_LEVELDB_UTIL_ENV_POSIX_TEST_HELPER_H_
+
+    
+    
+    {
+    {}  // namespace log
+}  // namespace leveldb
+    
+      // Return the current version.
+  Version* current() const { return current_; }
+    
+      // Returns a new iterator over the table contents.
+  // The result of NewIterator() is initially invalid (caller must
+  // call one of the Seek methods on the iterator before using it).
+  Iterator* NewIterator(const ReadOptions&) const;
+    
+    char* Arena::AllocateNewBlock(size_t block_bytes) {
+  char* result = new char[block_bytes];
+  blocks_.push_back(result);
+  memory_usage_.fetch_add(block_bytes + sizeof(char*),
+                          std::memory_order_relaxed);
+  return result;
+}
+    
+    int main()
+{
+    // create JSON object
+    json object =
+    {
+        {'the good', 'il buono'},
+        {'the bad', 'il cattivo'},
+        {'the ugly', 'il brutto'}
+    };
+    }
+    
+        // read-only access
+    
+        // print values
+    std::cout << object << '\n';
+    std::cout << null << '\n';
+    
+            static void renderNamespaceItems(QSharedPointer<Operations> operations,
+                                         RedisClient::Connection::NamespaceItems items,
+                                         QSharedPointer<AbstractNamespaceItem> parent,
+                                         const QSet<QByteArray> &expandedNamespaces);
+    
+    
+QString binaryStringToEscapedString(const QByteArray &value)
+{
+    QString val = QString::fromStdString(value.toStdString());
+    }
+    
+    #include <QtNetwork>
+    
+    
+    {};
+    
+    
+    {  virtual grpc::string get_input_type_name() const = 0;
+  virtual grpc::string get_output_type_name() const = 0;
+  virtual bool NoStreaming() const = 0;
+  virtual bool ClientStreaming() const = 0;
+  virtual bool ServerStreaming() const = 0;
+  virtual bool BidiStreaming() const = 0;
+};
+    
+    #ifdef _WIN32
+  #define TEST_ASSERT_FUNC(exp) TestEq(exp, true, #exp, __FILE__, __LINE__, __FUNCTION__)
+  #define TEST_EQ_FUNC(exp, val) TestEq(exp, val, #exp, __FILE__, __LINE__, __FUNCTION__)
+#else
+  #define TEST_ASSERT_FUNC(exp) TestEq(exp, true, #exp, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+  #define TEST_EQ_FUNC(exp, val) TestEq(exp, val, #exp, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+#endif
+    
+      const uint8_t *data() const { return GRPC_SLICE_START_PTR(slice_); }
+    
+    struct GrpcLikeMessageBuilder : private AllocatorMember,
+                                public flatbuffers::FlatBufferBuilder {
+private:
+  GrpcLikeMessageBuilder(const GrpcLikeMessageBuilder &);
+  GrpcLikeMessageBuilder &operator=(const GrpcLikeMessageBuilder &);
+    }
+    
+    #include <grpc++/impl/codegen/async_stream.h>
+#include <grpc++/impl/codegen/async_unary_call.h>
+#include <grpc++/impl/codegen/method_handler_impl.h>
+#include <grpc++/impl/codegen/proto_utils.h>
+#include <grpc++/impl/codegen/rpc_method.h>
+#include <grpc++/impl/codegen/service_type.h>
+#include <grpc++/impl/codegen/status.h>
+#include <grpc++/impl/codegen/stub_options.h>
+#include <grpc++/impl/codegen/sync_stream.h>
