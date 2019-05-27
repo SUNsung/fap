c0011d831bb92675ae9ff2460bdd71684e193f1d
SUNsung <weatherius@gmail.com>
2019-05-27 19:39:28 +0300

    Rewrite [141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp] File in: 2019-05-27 19:39:27

--- cccccc/141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp
+++ cccccc/141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp
@@ -1,239 +1,334 @@
 
         
-            accumulateSquareConstFunc funcs[16] =
+        
     {
-        accumulateSquareConst<0>,
-        accumulateSquareConst<1>,
-        accumulateSquareConst<2>,
-        accumulateSquareConst<3>,
-        accumulateSquareConst<4>,
-        accumulateSquareConst<5>,
-        accumulateSquareConst<6>,
-        accumulateSquareConst<7>,
-        accumulateSquareConst<8>,
-        accumulateSquareConst<9>,
-        accumulateSquareConst<10>,
-        accumulateSquareConst<11>,
-        accumulateSquareConst<12>,
-        accumulateSquareConst<13>,
-        accumulateSquareConst<14>,
-        accumulateSquareConst<15>
-    }, func = funcs[shift];
-    
-    
-    {            v_y = vld2q_u8(srcy + syj + 32);
-            v_dst.val[0] = v_y.val[0];
-            v_dst.val[1] = vld1q_u8(srcu + sj + 16);
-            v_dst.val[2] = v_y.val[1];
-            v_dst.val[3] = vld1q_u8(srcv + sj + 16);
-            vst4q_u8(dst + dj + 64, v_dst);
-        }
-#endif
-    
-    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
-inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
-inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(s32, u16, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src1]]                              \n\t'
-             'vld1.32 {d2-d3}, [%[src2]]                              \n\t'
-             'vqmovun.s32 d4, q0                                      \n\t'
-             'vqmovun.s32 d5, q1                                      \n\t'
-             'vst1.16 {d4-d5}, [%[dst]]                               \n\t'
-             : /*no output*/
-             : [src1] 'r' (_src + i + 0),
-               [src2] 'r' (_src + i + 4),
-               [dst] 'r' (_dst + i)
-             : 'd0','d1','d2','d3','d4','d5'
-         );
-     }
-})
-#else
-CVT_FUNC(s32, u16, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         int32x4_t vline1_s32 = vld1q_s32(_src + i);
-         int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
-    }
-    }
-    
-    
-    {    return (u8)(vget_lane_s32(q8, 0) - 1);
-}
-    
-        uint16x8_t v_maxval8 = vdupq_n_u16(maxVal),
-               v_minval8 = vdupq_n_u16(minVal);
-    u64 mask[2] = { 0ul };
-    
-    
-    {}  // namespace xgboost
+    {}  // end namespace grappler
+}  // end namespace tensorflow
 
     
-    
-    {  /*! \brief push up temp */
-  inline void PushTemp() {
-    temp.Reserve(limit_size * 2);
-    for (size_t l = 1; true; ++l) {
-      this->InitLevel(l + 1);
-      // check if level l is empty
-      if (level[l].size == 0) {
-        level[l].SetPrune(temp, limit_size);
-        break;
-      } else {
-        // level 0 is actually temp space
-        level[0].SetPrune(temp, limit_size);
-        temp.SetCombine(level[0], level[l]);
-        if (temp.size > limit_size) {
-          // try next level
-          level[l].size = 0;
-        } else {
-          // if merged record is still smaller, no need to send to next level
-          level[l].CopyFrom(temp); break;
-        }
-      }
+      void Compute(OpKernelContext* context) override {
+    // Output a scalar string.
+    Tensor* output_tensor = nullptr;
+    OP_REQUIRES_OK(context,
+                   context->allocate_output(0, TensorShape(), &output_tensor));
+    auto output = output_tensor->scalar<string>();
     }
+    
+    #ifndef TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
+    
+    Status TF_TensorToPyArray(Safe_TF_TensorPtr tensor, PyObject** out_ndarray);
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+    
+    
+    {  DCHECK(PyDict_Check(code_to_exc_type_map));
+  PyObject* key;
+  PyObject* value;
+  Py_ssize_t pos = 0;
+  while (PyDict_Next(code_to_exc_type_map, &pos, &key, &value)) {
+    TF_Code code = static_cast<TF_Code>(PyLong_AsLong(key));
+    singleton_->exc_types_[code] = value;
+    // The exception classes should also have the lifetime of the process, but
+    // incref just in case.
+    Py_INCREF(value);
   }
-  /*! \brief get the summary after finalize */
-  inline void GetSummary(SummaryContainer *out) {
-    if (level.size() != 0) {
-      out->Reserve(limit_size * 2);
-    } else {
-      out->Reserve(inqueue.queue.size());
+}
+    
+    // A wrapper around io::RecordReader that is more easily SWIG wrapped for
+// Python.  An instance of this class is not safe for concurrent access
+// by multiple threads.
+class PyRecordReader {
+ public:
+  // TODO(vrv): make this take a shared proto to configure
+  // the compression options.
+  static PyRecordReader* New(const string& filename, uint64 start_offset,
+                             const string& compression_type_string,
+                             TF_Status* out_status);
     }
-    inqueue.MakeSummary(out);
-    if (level.size() != 0) {
-      level[0].SetPrune(*out, limit_size);
-      for (size_t l = 1; l < level.size(); ++l) {
-        if (level[l].size == 0) continue;
-        if (level[0].size == 0) {
-          level[0].CopyFrom(level[l]);
-        } else {
-          out->SetCombine(level[0], level[l]);
-          level[0].SetPrune(*out, limit_size);
-        }
-      }
-      out->CopyFrom(level[0]);
-    } else {
-      if (out->size > limit_size) {
-        temp.Reserve(limit_size);
-        temp.SetPrune(*out, limit_size);
-        out->CopyFrom(temp);
-      }
-    }
+    
+      const tensorflow::OpRegistrationData* op_reg_data;
+  auto status =
+      tensorflow::OpRegistry::Global()->LookUp(node_def.op(), &op_reg_data);
+  if (!status.ok()) {
+    LOG(WARNING) << 'Op ' << node_def.op() << ' not found: ' << status;
+    return '';
   }
-  // used for debug, check if the sketch is valid
-  inline void CheckValid(RType eps) const {
-    for (size_t l = 1; l < level.size(); ++l) {
-      level[l].CheckValid(eps);
+  AddDefaultsToNodeDef(op_reg_data->op_def, &node_def);
+    
+    using std::chrono::duration_cast;
+    
+      // Begins the timer at the present point in the stream.
+  bool Start(Stream *stream);
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+        void showTab_SM(bool fShow);
+    void showTab_VM(bool fShow);
+    
+    static void secp256k1_sha256_write(secp256k1_sha256_t *hash, const unsigned char *data, size_t len) {
+    size_t bufsize = hash->bytes & 0x3F;
+    hash->bytes += len;
+    while (bufsize + len >= 64) {
+        /* Fill the buffer, and process it. */
+        memcpy(((unsigned char*)hash->buf) + bufsize, data, 64 - bufsize);
+        data += 64 - bufsize;
+        len -= 64 - bufsize;
+        secp256k1_sha256_transform(hash->s, hash->buf);
+        bufsize = 0;
     }
-  }
-  // initialize level space to at least nlevel
-  inline void InitLevel(size_t nlevel) {
-    if (level.size() >= nlevel) return;
-    data.resize(limit_size * nlevel);
-    level.resize(nlevel, Summary(nullptr, 0));
-    for (size_t l = 0; l < level.size(); ++l) {
-      level[l].data = dmlc::BeginPtr(data) + l * limit_size;
-    }
-  }
-  // input data queue
-  typename Summary::Queue inqueue;
-  // number of levels
-  size_t nlevel;
-  // size of summary in each level
-  size_t limit_size;
-  // the level of each summaries
-  std::vector<Summary> level;
-  // content of the summary
-  std::vector<Entry> data;
-  // temporal summary, used for temp-merge
-  SummaryContainer temp;
-};
-    
-        // Remove the assertion on batch.index, which can be null in the case that the data in this
-    // batch is entirely sparse. Although it's true that this indicates a likely issue with the
-    // user's data workflows, passing XGBoost entirely sparse data should not cause it to fail.
-    // See https://github.com/dmlc/xgboost/issues/1827 for complete detail.
-    // CHECK(batch.index != nullptr);
-    
-     public:
-  void PredictBatch(DMatrix* dmat, HostDeviceVector<bst_float>* out_preds,
-                    const gbm::GBTreeModel& model, int tree_begin,
-                    unsigned ntree_limit = 0) override {
-    if (this->PredictFromCache(dmat, out_preds, model, ntree_limit)) {
-      return;
-    }
-    }
-    
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'ID3D10ShaderResourceView*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-    
-    // Build with, e.g:
-//   # cl.exe binary_to_compressed_c.cpp
-//   # gcc binary_to_compressed_c.cpp
-// You can also find a precompiled Windows binary in the binary/demo package available from https://github.com/ocornut/imgui
-    
-    
-    {    ImGuiIO& io = ImGui::GetIO();
-    if (g_pVertexShaderBlob) { g_pVertexShaderBlob->Release(); g_pVertexShaderBlob = NULL; }
-    if (g_pPixelShaderBlob) { g_pPixelShaderBlob->Release(); g_pPixelShaderBlob = NULL; }
-    if (g_pRootSignature) { g_pRootSignature->Release(); g_pRootSignature = NULL; }
-    if (g_pPipelineState) { g_pPipelineState->Release(); g_pPipelineState = NULL; }
-    if (g_pFontTextureResource) { g_pFontTextureResource->Release(); g_pFontTextureResource = NULL; io.Fonts->TexID = NULL; } // We copied g_pFontTextureView to io.Fonts->TexID so let's clear that as well.
-    for (UINT i = 0; i < g_numFramesInFlight; i++)
-    {
-        FrameResources* fr = &g_pFrameResources[i];
-        if (fr->IndexBuffer)  { fr->IndexBuffer->Release();  fr->IndexBuffer = NULL; }
-        if (fr->VertexBuffer) { fr->VertexBuffer->Release(); fr->VertexBuffer = NULL; }
+    if (len) {
+        /* Fill the buffer with what remains. */
+        memcpy(((unsigned char*)hash->buf) + bufsize, data, len);
     }
 }
     
-    // **DO NOT USE THIS CODE IF YOUR CODE/ENGINE IS USING MODERN OPENGL (SHADERS, VBO, VAO, etc.)**
-// **Prefer using the code in imgui_impl_opengl3.cpp**
-// This code is mostly provided as a reference to learn how ImGui integration works, because it is shorter to read.
-// If your code is using GL3+ context or any semi modern OpenGL calls, using this is likely to make everything more
-// complicated, will require your code to reset every single OpenGL attributes to their initial state, and might
-// confuse your GPU driver.
-// The GL2 code is unable to reset attributes or even call e.g. 'glUseProgram(0)' because they don't exist in that API.
+    // Bech32 is a string encoding format used in newer address types.
+// The output consists of a human-readable part (alphanumeric), a
+// separator character (1), and a base32 data section, the last
+// 6 characters of which are a checksum.
+//
+// For more information, see BIP 173.
     
-    #define WRITE_CHECK(fp, ptr, count)                                            \
-  if (fp.write((ptr), (count)) != (count)) {                                   \
-    throw DL_ABORT_EX(                                                         \
-        fmt('Failed to save DHT routing table to %s.', filename.c_str()));     \
-  }
+    ;; input is [rsp + _XFER + %1 * 4]
+%macro DO_ROUND 1
+    mov	y0, e		; y0 = e
+    ror	y0, (25-11)	; y0 = e >> (25-11)
+    mov	y1, a		; y1 = a
+    xor	y0, e		; y0 = e ^ (e >> (25-11))
+    ror	y1, (22-13)	; y1 = a >> (22-13)
+    mov	y2, f		; y2 = f
+    xor	y1, a		; y1 = a ^ (a >> (22-13)
+    ror	y0, (11-6)	; y0 = (e >> (11-6)) ^ (e >> (25-6))
+    xor	y2, g		; y2 = f^g
+    xor	y0, e		; y0 = e ^ (e >> (11-6)) ^ (e >> (25-6))
+    ror	y1, (13-2)	; y1 = (a >> (13-2)) ^ (a >> (22-2))
+    and	y2, e		; y2 = (f^g)&e
+    xor	y1, a		; y1 = a ^ (a >> (13-2)) ^ (a >> (22-2))
+    ror	y0, 6		; y0 = S1 = (e>>6) & (e>>11) ^ (e>>25)
+    xor	y2, g		; y2 = CH = ((f^g)&e)^g
+    add	y2, y0		; y2 = S1 + CH
+    ror	y1, 2		; y1 = S0 = (a>>2) ^ (a>>13) ^ (a>>22)
+    add	y2, [rsp + _XFER + %1 * 4]	; y2 = k + w + S1 + CH
+    mov	y0, a		; y0 = a
+    add	h, y2		; h = h + S1 + CH + k + w
+    mov	y2, a		; y2 = a
+    or	y0, c		; y0 = a|c
+    add	d, h		; d = d + h + S1 + CH + k + w
+    and	y2, c		; y2 = a&c
+    and	y0, b		; y0 = (a|c)&b
+    add	h, y1		; h = h + S1 + CH + k + w + S0
+    or	y0, y2		; y0 = MAJ = (a|c)&b)|(a&c)
+    add	h, y0		; h = h + S1 + CH + k + w + S0 + MAJ
+    ROTATE_ARGS
+%endm
+    
+      // We iterate twice.  In the second iteration, everything is the
+  // same except the log record never makes it to the MANIFEST file.
+  for (int iter = 0; iter < 2; iter++) {
+    port::AtomicPointer* error_type = (iter == 0)
+        ? &env_->manifest_sync_error_
+        : &env_->manifest_write_error_;
+    }
+    
+    const char* InternalFilterPolicy::Name() const {
+  return user_policy_->Name();
+}
+    
+    /// @brief Fills a Blob with constant or randomly-generated data.
+template <typename Dtype>
+class Filler {
+ public:
+  explicit Filler(const FillerParameter& param) : filler_param_(param) {}
+  virtual ~Filler() {}
+  virtual void Fill(Blob<Dtype>* blob) = 0;
+ protected:
+  FillerParameter filler_param_;
+};  // class Filler
     
     
-    {  virtual void
-  addImmediateTask(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+    {  shared_ptr<boost::thread> thread_;
 };
     
-    void DHTTokenUpdateCommand::process()
+    
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #ifdef USE_CUDNN
+/**
+ * @brief CuDNN acceleration of SigmoidLayer.
+ */
+template <typename Dtype>
+class CuDNNSigmoidLayer : public SigmoidLayer<Dtype> {
+ public:
+  explicit CuDNNSigmoidLayer(const LayerParameter& param)
+      : SigmoidLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNSigmoidLayer();
+    }
+    
+    
+    {}  // namespace caffe
+    
+    				if (szArgList[i + 1] != NULL && szArgList[i + 1][0] != '/')
+				{
+					cmderOptions.cmderCfgRoot = szArgList[i + 1];
+					i++;
+				}
+			}
+			else if (_wcsicmp(L'/start', szArgList[i]) == 0)
+			{
+				int len = wcslen(szArgList[i + 1]);
+				if (wcscmp(&szArgList[i + 1][len - 1], L'\'') == 0)
+				{
+					szArgList[i + 1][len - 1] = '\0';
+				}
+    
+    namespace leveldb {
+    }
+    
+    Comparator::~Comparator() = default;
+    
+      // Reports dropped bytes to the reporter.
+  // buffer_ must be updated to remove the dropped bytes prior to invocation.
+  void ReportCorruption(uint64_t bytes, const char* reason);
+  void ReportDrop(uint64_t bytes, const Status& reason);
+    
+      // State used to check for number of overlapping grandparent files
+  // (parent == level_ + 1, grandparent == level_ + 2)
+  std::vector<FileMetaData*> grandparents_;
+  size_t grandparent_index_;  // Index in grandparent_starts_
+  bool seen_key_;             // Some output key has been seen
+  int64_t overlapped_bytes_;  // Bytes of overlap between current output
+                              // and grandparent files
+    
+    #ifndef STORAGE_LEVELDB_INCLUDE_OPTIONS_H_
+#define STORAGE_LEVELDB_INCLUDE_OPTIONS_H_
+    
+    #ifndef STORAGE_LEVELDB_INCLUDE_WRITE_BATCH_H_
+#define STORAGE_LEVELDB_INCLUDE_WRITE_BATCH_H_
+    
+    typedef void (*PSOutputFunc)(void *stream, char *data, int len);
+    
+      // Get duration
+  int getDuration() { return duration;}
+    
+    #include <stdlib.h>
+#include <stddef.h>
+#include 'ProfileData.h'
+    
+      if (obj->dictLookup('D', &tmp)->isArray()) {
+    Array * dim = tmp.getArray();
+    
+    if (dim->getLength() >= 2) {
+      Object dd;
+      if (dim->get(0, &dd)->isInt()) {
+	width = dd.getInt();
+      }
+      dd.free();
+      if (dim->get(1, &dd)->isInt()) {
+	height = dd.getInt();
+      }
+      dd.free();
+    }
+  }
+  tmp.free();
+    
+    #ifdef ENABLE_PLUGINS
+    
+    SplashGouraudPattern::~SplashGouraudPattern() {
+}
+    
+    int StdinCacheLoader::load(const std::vector<ByteRange> &ranges, CachedFileWriter *writer)
 {
-  try {
-    tokenTracker_->updateTokenSecret();
+  return 0;
+}
+    
+    //------------------------------------------------------------------------
+// StdinPDFDocBuilder
+//------------------------------------------------------------------------
+    
+    void DHTReplaceNodeTask::sendMessage()
+{
+  std::shared_ptr<DHTNode> questionableNode = bucket_->getLRUQuestionableNode();
+  if (!questionableNode) {
+    setFinished(true);
   }
-  catch (RecoverableException& e) {
-    A2_LOG_ERROR_EX(EX_EXCEPTION_CAUGHT, e);
+  else {
+    getMessageDispatcher()->addMessageToQueue(
+        getMessageFactory()->createPingMessage(questionableNode), timeout_,
+        make_unique<DHTPingReplyMessageCallback<DHTReplaceNodeTask>>(this));
   }
 }
     
-      static const std::string E;
+    public:
+  DHTRoutingTableDeserializer(int family);
     
-      void put(const std::string& hostname, const std::string& ipaddr,
-           uint16_t port);
+      // localnode
+  // 8bytes reserved
+  WRITE_CHECK(fp, zero, 8);
+  // 20bytes localnode ID
+  WRITE_CHECK(fp, localNode_->getID(), DHT_ID_LENGTH);
+  // 4bytes reserved
+  WRITE_CHECK(fp, zero, 4);
     
-    using json = nlohmann::json;
     
-        // print values
-    std::cout << object << '\n';
-    std::cout << null << '\n';
+    {} // namespace aria2
+    
+    namespace aria2 {
+    }
+    
+    
+    {  size_t getQueueSize() const { return queue_.size(); }
+};
+    
+    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createReplaceNodeTask(
+    const std::shared_ptr<DHTBucket>& bucket,
+    const std::shared_ptr<DHTNode>& newNode)
+{
+  auto task = std::make_shared<DHTReplaceNodeTask>(bucket, newNode);
+  task->setTimeout(timeout_);
+  setCommonProperty(task);
+  return task;
+}
+    
+    DHTTokenTracker::DHTTokenTracker()
+{
+  util::generateRandomData(secret_[0], SECRET_SIZE);
+  memcpy(secret_[1], secret_[0], SECRET_SIZE);
+}
+    
+    
+    {  void updateTokenSecret();
+};
+    
+    #include <memory>
+    
+    public:
+  // _remoteNode is always null
+  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                    const unsigned char* data, size_t length,
+                    const std::string& ipaddr, uint16_t port);
