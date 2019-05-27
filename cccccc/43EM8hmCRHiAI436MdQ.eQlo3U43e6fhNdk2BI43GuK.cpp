f45a7275e65152434f7ffe99a2206a20d6e4e240
SUNsung <weatherius@gmail.com>
2019-05-27 06:01:17 +0300

    Rewrite [43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp] File in: 2019-05-27 06:01:16

--- cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
+++ cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
@@ -1,38 +1,53 @@
 
         
-        OPERATOR_SCHEMA(Glu)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-Applies gated linear unit to the input Tensor X. The output Y is half the size
-of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
-[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
-X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
-)DOC')
-    .Input(0, 'X', '1D input tensor')
-    .Output(0, 'Y', '1D output tensor');
+        #ifndef STORAGE_LEVELDB_DB_LOG_FORMAT_H_
+#define STORAGE_LEVELDB_DB_LOG_FORMAT_H_
     
-    #if DMLC_ENABLE_STD_THREAD
-namespace xgboost {
-namespace data {
-    }
-    }
-    
-    namespace xgboost {
-namespace obj {
-    }
-    }
-    
-    
-    {
-    {}  // namespace obj
-}  // namespace xgboost
+    #endif  // STORAGE_LEVELDB_DB_LOG_READER_H_
 
     
-    void SimpleCSRSource::SaveBinary(dmlc::Stream* fo) const {
-  int tmagic = kMagic;
-  fo->Write(&tmagic, sizeof(tmagic));
-  info.SaveBinary(fo);
-  fo->Write(page_.offset.HostVector());
-  fo->Write(page_.data.HostVector());
+      WritableFile* dest_;
+  int block_offset_;  // Current offset in block
+    
+    template <typename Key, class Comparator>
+void SkipList<Key, Comparator>::Insert(const Key& key) {
+  // TODO(opt): We can use a barrier-free variant of FindGreaterOrEqual()
+  // here since Insert() is externally synchronized.
+  Node* prev[kMaxHeight];
+  Node* x = FindGreaterOrEqual(key, prev);
+    }
+    
+      // Check that opening non-existent file fails.
+  SequentialFile* seq_file;
+  RandomAccessFile* rand_file;
+  ASSERT_TRUE(!env_->NewSequentialFile('/dir/non_existent', &seq_file).ok());
+  ASSERT_TRUE(!seq_file);
+  ASSERT_TRUE(!env_->NewRandomAccessFile('/dir/non_existent', &rand_file).ok());
+  ASSERT_TRUE(!rand_file);
+    
+    
+    {}  // namespace leveldb
+    
+        overlapped.OffsetHigh = static_cast<DWORD>(offset >> 32);
+    overlapped.Offset = static_cast<DWORD>(offset);
+    if (!::ReadFile(handle_.get(), scratch, static_cast<DWORD>(n), &bytes_read,
+                    &overlapped)) {
+      DWORD error_code = ::GetLastError();
+      if (error_code != ERROR_HANDLE_EOF) {
+        *result = Slice(scratch, 0);
+        return Status::IOError(filename_, GetWindowsErrorMessage(error_code));
+      }
+    }
+    
+            if (!m_HistoryCollector.FOpndAddedToHistory())
+        {
+            m_HistoryCollector.AddOpndToHistory(m_numberString, m_currentVal);
+        }
+    
+    #include 'Rational.h'
+    
+    NarratorAnnouncement ^ CalculatorAnnouncement::GetMemoryItemChangedAnnouncement(String ^ announcement)
+{
+    return ref new NarratorAnnouncement(
+        announcement, CalculatorActivityIds::MemoryItemChanged, AutomationNotificationKind::ActionCompleted, AutomationNotificationProcessing::MostRecent);
 }
