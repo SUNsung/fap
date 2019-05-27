e20c57e61f102b545d598f3dcfc154e41b2f5f2f
SUNsung <weatherius@gmail.com>
2019-05-27 05:43:14 +0300

    Rewrite [386VqW3J45jbQ38NKlHIv6Q38638SYVGpi0mZ2k38bFe.rb] File in: 2019-05-27 05:43:13

--- ruby/386VqW3J45jbQ38NKlHIv6Q38638SYVGpi0mZ2k38bFe.rb
+++ ruby/386VqW3J45jbQ38NKlHIv6Q38638SYVGpi0mZ2k38bFe.rb
@@ -1,126 +1,146 @@
 
         
-              def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :tag,
-                                       env_name: 'FL_GIT_TAG_TAG',
-                                       description: 'Define your own tag text. This will replace all other parameters',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :grouping,
-                                       env_name: 'FL_GIT_TAG_GROUPING',
-                                       description: 'Is used to keep your tags organised under one 'folder'',
-                                       default_value: 'builds'),
-          FastlaneCore::ConfigItem.new(key: :prefix,
-                                       env_name: 'FL_GIT_TAG_PREFIX',
-                                       description: 'Anything you want to put in front of the version number (e.g. 'v')',
-                                       default_value: ''),
-          FastlaneCore::ConfigItem.new(key: :postfix,
-                                       env_name: 'FL_GIT_TAG_POSTFIX',
-                                       description: 'Anything you want to put at the end of the version number (e.g. '-RC1')',
-                                       default_value: ''),
-          FastlaneCore::ConfigItem.new(key: :build_number,
-                                       env_name: 'FL_GIT_TAG_BUILD_NUMBER',
-                                       description: 'The build number. Defaults to the result of increment_build_number if you\'re using it',
-                                       default_value: Actions.lane_context[Actions::SharedValues::BUILD_NUMBER],
-                                       default_value_dynamic: true,
-                                       is_string: false),
-          FastlaneCore::ConfigItem.new(key: :message,
-                                       env_name: 'FL_GIT_TAG_MESSAGE',
-                                       description: 'The tag message. Defaults to the tag's name',
-                                       default_value_dynamic: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :commit,
-                                       env_name: 'FL_GIT_TAG_COMMIT',
-                                       description: 'The commit or object where the tag will be set. Defaults to the current HEAD',
-                                       default_value_dynamic: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :force,
-                                       env_name: 'FL_GIT_TAG_FORCE',
-                                       description: 'Force adding the tag',
-                                       optional: true,
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :sign,
-                                       env_name: 'FL_GIT_TAG_SIGN',
-                                       description: 'Make a GPG-signed tag, using the default e-mail address's key',
-                                       optional: true,
-                                       is_string: false,
-                                       default_value: false)
-        ]
+        require 'action_view/helpers/tags/collection_helpers'
+    
+    require 'active_support/core_ext/time/calculations'
+    
+          test 'when layout is specified as a proc without parameters it works just the same' do
+        controller = WithZeroArityProc.new
+        controller.process(:index)
+        assert_equal 'Overwrite Hello zero arity proc!', controller.response_body
       end
     
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            tag: '#{tag}',
-            message: '#{message}',
-            commit: '#{commit}'
-          })
-        end').runner.execute(:test)
+    Benchmark.ips do |x|
+  x.report('local-require') { local_require }
+  x.report('global-require') { global_require }
+  x.report('graceful-require') { graceful_require }
+  x.compare!
+end
+
     
-          context 'with valid path to compile_commands.json' do
-        context 'with no path to oclint' do
-          let(:result) do
-            Fastlane::FastFile.new.parse('lane :test do
-              oclint( compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json' )
-            end').runner.execute(:test)
-          end
-          let(:command) { 'cd #{File.expand_path('.').shellescape} && oclint -report-type=html -o=oclint_report.html' }
+        private
     
-    puts('[WARNING] You are calling #{tool_name} directly. Usage of the tool name without the `fastlane` prefix is deprecated in fastlane 2.0'.yellow)
-puts('Please update your scripts to use `fastlane #{tool_name} #{full_params}` instead.'.yellow)
+    class Discourse::Cors
+  ORIGINS_ENV = 'Discourse_Cors_Origins'
     
-    desc 'generates TOC'
-task :toc, [:readme] do |t, a|
-  a.with_defaults :readme => 'README.md'
-    
-                  <ol start='<%= frame.lineno %>' class='context' id='<%= id %>'
-                  onclick='toggle(<%= id %>);'>
-                <li class='context-line' id='context-<%= id %>'><code><%=
-                  h frame.context_line %></code></li>
-              </ol>
-    
-          DIRECTIVES = %i(base_uri child_src connect_src default_src
-                      font_src form_action frame_ancestors frame_src
-                      img_src manifest_src media_src object_src
-                      plugin_types referrer reflected_xss report_to
-                      report_uri require_sri_for sandbox script_src
-                      style_src worker_src).freeze
-    
-          def handle(hash)
-        was = hash.dup
-        hash.replace escape(hash)
-        was
+        since =
+      case period_type
+      when :daily then 1.day.ago
+      when :weekly then 1.week.ago
+      when :monthly then 1.month.ago
+      when :quarterly then 3.months.ago
+      when :yearly then 1.year.ago
+      else 1000.years.ago
       end
     
-        it 'Returns nil when Referer header is invalid' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
-      expect(subject.referrer(env)).to be_nil
+        it 'no-ops on invalid values' do
+      previous = builder.build
+    
+        def site_setting_extension
+      { script_src: SiteSetting.content_security_policy_script_src.split('|') }
+    end
+    
+      private
+    
+        {
+      status: status,
+      is_crawler: helper.is_crawler?,
+      has_auth_cookie: helper.has_auth_cookie?,
+      is_background: !!(request.path =~ /^\/message-bus\// || request.path =~ /\/topics\/timings/),
+      is_mobile: helper.is_mobile?,
+      track_view: track_view,
+      timing: timing,
+      queue_seconds: env['REQUEST_QUEUE_SECONDS']
+    }.tap do |h|
+      h[:user_agent] = env['HTTP_USER_AGENT'] if h[:is_crawler]
+    end
+  end
+    
+        svg_subset = '''<!--
+Discourse SVG subset of Font Awesome Free by @fontawesome - https://fontawesome.com
+License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+-->
+<svg xmlns='http://www.w3.org/2000/svg' style='display: none;'>
+'''.dup
+    
+    describe Admin::UsersController do
+  it 'requires to be signed in as an admin' do
+    login_as(users(:bob))
+    visit admin_users_path
+    expect(page).to have_text('Admin access required to view that page.')
+  end
+    
+        it 'should ignore strings which just contain a JSONPath' do
+      expect(LiquidMigrator.convert_string('$.data')).to eq('$.data')
+      expect(LiquidMigrator.convert_string('$first_title')).to eq('$first_title')
+      expect(LiquidMigrator.convert_string(' $.data', true)).to eq(' $.data')
+      expect(LiquidMigrator.convert_string('lorem $.data', true)).to eq('lorem $.data')
+    end
+    it 'should raise an exception when encountering complex JSONPaths' do
+      expect { LiquidMigrator.convert_string('$.data.test.*', true) }.
+        to raise_error('JSONPath '$.data.test.*' is too complex, please check your migration.')
+    end
+  end
+    
+      describe 'up' do
+    it 'should update extract and template options for an existing WebsiteAgent' do
+      expect(agent.options).to include('extract' => old_extract,
+                                       'template' => old_template)
+      ConvertWebsiteAgentTemplateForMerge.new.up
+      agent.reload
+      expect(agent.options).to include('extract' => new_extract,
+                                       'template' => new_template)
+    end
+  end
+    
+        it 'updates Agents' last_error_log_at when an error is logged' do
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 3, :outbound_event => events(:jane_website_agent_event))
+      expect(agents(:jane_website_agent).reload.last_error_log_at).to be_nil
+    
+    # This is basically a copy of the original bundler 'bundle' shim
+# with the addition of the loading of our Bundler patches that
+# modify Bundler's caching behaviour.
+    
+    class LogStash::PluginManager::Unpack < LogStash::PluginManager::PackCommand
+  option '--tgz', :flag, 'unpack a packaged tar.gz file', :default => !LogStash::Environment.windows?
+  option '--zip', :flag, 'unpack a packaged  zip file', :default => LogStash::Environment.windows?
+    
+          def page_name
+        @name.gsub('-', ' ')
+      end
+    
+      test 'creating page is blocked' do
+    Precious::App.set(:wiki_options, { allow_editing: false})
+    post '/create', :content => 'abc', :page => 'D',
+         :format             => 'markdown', :message => 'def'
+    assert !last_response.ok?
+    
+      s.add_dependency 'gollum-lib', '~> 4.2', '>= 4.2.10'
+  s.add_dependency 'kramdown', '~> 1.9.0'
+  s.add_dependency 'sinatra', '~> 1.4', '>= 1.4.4'
+  s.add_dependency 'mustache', ['>= 0.99.5', '< 1.0.0']
+  s.add_dependency 'useragent', '~> 0.16.2'
+  s.add_dependency 'gemojione', '~> 3.2'
+    
+            if obj && obj.errors[method].present?
+          errors = safe_join(obj.errors[method], '<br />'.html_safe)
+          content_tag(:span, errors, class: 'formError')
+        else
+          ''
+        end
+      end
+    
+            def line_item_params
+          params.require(:line_item).permit(:quantity, :variant_id, options: line_item_options)
+        end
+      end
     end
   end
 end
 
     
-          expected_header = <<-END.chomp
-rack.%2573ession=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-END
-      expect(last_response.headers['Set-Cookie']).to eq(expected_header)
-    end
-  end
+                render_order(result)
+          end
     
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
+      # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = false
