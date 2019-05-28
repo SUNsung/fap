cd6260472274c51c2f9de241f14a9ae784e918a8
SUNsung <weatherius@gmail.com>
2019-05-28 09:41:23 +0300

    Rewrite [55UzJ1aT38DnE55D7XQghKQtmk55LBB4RDiqBKw55vuL.cpp] File in: 2019-05-28 09:41:22

--- cccccc/55UzJ1aT38DnE55D7XQghKQtmk55LBB4RDiqBKw55vuL.cpp
+++ cccccc/55UzJ1aT38DnE55D7XQghKQtmk55LBB4RDiqBKw55vuL.cpp
@@ -1,165 +1,222 @@
 
         
-        class GetSubGradient final : public GradientMakerBase {
-  using GradientMakerBase::GradientMakerBase;
-    }
+            f32 alpha, beta, gamma;
+    typedef typename TypeTraits<T>::wide wtype;
+    wAdd<wtype> wideAdd;
+    wAdd(f32 _alpha, f32 _beta, f32 _gamma):
+        alpha(_alpha), beta(_beta), gamma(_gamma),
+        wideAdd(_alpha, _beta, _gamma) {}
     
-    REGISTER_CPU_OPERATOR(EnforceFinite, EnforceFiniteOp<CPUContext>);
-    
-    OPERATOR_SCHEMA(Im2Col)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc('The Im2Col operator from Matlab.')
-    .TensorInferenceFunction(
-        [](const OperatorDef& def, const vector<TensorShape>& in) {
-          ArgumentHelper helper(def);
-          auto pad = helper.GetSingleArgument<int>('pad', 0);
-          auto kernel_h = helper.GetSingleArgument<int>(
-              'kernel_h', helper.GetSingleArgument<int>('kernel', 0));
-          auto kernel_w = helper.GetSingleArgument<int>(
-              'kernel_w', helper.GetSingleArgument<int>('kernel', 0));
-          auto dilation_h = helper.GetSingleArgument<int>(
-              'dilation_h', helper.GetSingleArgument<int>('dilation', 1));
-          auto dilation_w = helper.GetSingleArgument<int>(
-              'dilation_w', helper.GetSingleArgument<int>('dilation', 1));
-          auto stride_h = helper.GetSingleArgument<int>(
-              'stride_h', helper.GetSingleArgument<int>('stride', 1));
-          auto stride_w = helper.GetSingleArgument<int>(
-              'stride_w', helper.GetSingleArgument<int>('stride', 1));
-          auto order = StringToStorageOrder(
-              helper.GetSingleArgument<string>('order', 'NCHW'));
-    }
-    
-    namespace leveldb {
-    }
-    
-      // Is this a trivial compaction that can be implemented by just
-  // moving a single input file to the next level (no merging or splitting)
-  bool IsTrivialMove() const;
-    
-      // Random reads.
-  ASSERT_OK(env_->NewRandomAccessFile('/dir/f', &rand_file));
-  ASSERT_OK(rand_file->Read(6, 5, &result, scratch));  // Read 'world'.
-  ASSERT_EQ(0, result.compare('world'));
-  ASSERT_OK(rand_file->Read(0, 5, &result, scratch));  // Read 'hello'.
-  ASSERT_EQ(0, result.compare('hello'));
-  ASSERT_OK(rand_file->Read(10, 100, &result, scratch));  // Read 'd'.
-  ASSERT_EQ(0, result.compare('d'));
-    
-    namespace leveldb {
-    }
-    
-      // Given a key, return an approximate byte offset in the file where
-  // the data for that key begins (or would begin if the key were
-  // present in the file).  The returned value is in terms of file
-  // bytes, and so includes effects like compression of the underlying data.
-  // E.g., the approximate offset of the last key in the table will
-  // be close to the file length.
-  uint64_t ApproximateOffsetOf(const Slice& key) const;
-    
-      if (r->pending_index_entry) {
-    assert(r->data_block.empty());
-    r->options.comparator->FindShortestSeparator(&r->last_key, key);
-    std::string handle_encoding;
-    r->pending_handle.EncodeTo(&handle_encoding);
-    r->index_block.Add(r->last_key, Slice(handle_encoding));
-    r->pending_index_entry = false;
-  }
-    
-    
-    {}  // namespace leveldb
-
-    
-      // Return a pointer to a newly allocated memory block of 'bytes' bytes.
-  char* Allocate(size_t bytes);
-    
-    
-    {  int cached_weight = 0;
-  for (int i = 0; i < index; i++) {
-    const int weight = (i & 1 ? kLight : kHeavy);
-    int r = Lookup(i);
-    if (r >= 0) {
-      cached_weight += weight;
-      ASSERT_EQ(1000 + i, r);
-    }
-  }
-  ASSERT_LE(cached_weight, kCacheSize + kCacheSize / 10);
-}
-    
-      HANDLE get() const { return handle_; }
-    
-            // Inner state of the underlying reader.
-        // Is set in the RestoreFromCheckpoint call and used in the next GetNextMinibatch
-        // when the reader state is restored after the first StartEpoch call.
-        Internal::Optional<Dictionary> m_state;
-    
-    
-    {protected:
-    std::string m_callStack;
-};
-    
-    #include <string>
-    
-    
+        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0, const typename internal::VecTraits<T>::vec64 & v_src1,
+              typename internal::VecTraits<T>::unsign::vec64 & v_dst) const
     {
-    {        // Remaining elements
-        if (squashInputs)
-        {
-            //squash sequences of identical samples
-            for (size_t i = 1; i < columnIndices.size(); i++)
-            {
-                size_t refId = (int)firstSeq(0, columnIndices[i]);
-                if (lastId != refId)
+        v_dst = internal::vmvn(internal::vceq(v_src0, v_src1));
+    }
+    
+             int8x8_t vline_s8 = vqmovn_s16(vcombine_s16(vline1_s16, vline2_s16));
+    
+                    if( mask[0] == 0 )
                 {
-                    lastId = refId;
-                    if (std::find(tokensToIgnore.begin(), tokensToIgnore.end(), refId) == tokensToIgnore.end())
-                        out_SampleSeqVec.push_back(refId);
+                    if (mask[1] != 0)
+                    {
+                        j -= 8;
+                        ptr -= 8;
+                    }
+                    continue;
                 }
-            }
-        }
-        else
-        {
-            for (size_t i = 1; i < columnIndices.size(); i++)
+    
+    inline float32x2_t vrecp_f32(float32x2_t val)
+{
+    float32x2_t reciprocal = vrecpe_f32(val);
+    reciprocal = vmul_f32(vrecps_f32(val, reciprocal), reciprocal);
+    reciprocal = vmul_f32(vrecps_f32(val, reciprocal), reciprocal);
+    return reciprocal;
+}
+    
+            if (IsReadOnly())
+            RuntimeError('NDArrayView::CopyFrom: Cannot modify contents of a readonly NDArrayView.');
+    
+                m_samples.second += samples;
+            m_updates.second++;
+            m_totalUpdates++;
+            
+            if (ShouldWriteUpdate(m_updates.second))
             {
-                auto refId = (int)firstSeq(0, columnIndices[i]);
-                if (std::find(tokensToIgnore.begin(), tokensToIgnore.end(), refId) == tokensToIgnore.end())
-                    out_SampleSeqVec.push_back(refId);
+                // Time to output the accumulated updates.
+                // Note that we take snapshot of the accumulated loss/metric only when we want to write.
+                // We do it this way on purpose, since accumulated loss/metric may be stored on a GPU
+                // and we want to minimize the number of GPU->CPU data transfers.
+                if (accumulatedLoss)
+                {
+                    m_loss.second = accumulatedLoss->AsScalar<double>();
+                }
+    }
+    
+            static FunctionPtr Deserialize(const Dictionary& dictionary,
+            const std::unordered_map<std::wstring, Variable>& uidToVariableMap,
+            const CNTK::DeviceDescriptor& device);
+    
+    
+template <class ConfigRecordType>
+void DataReader::InitFromConfig(const ConfigRecordType& /*config*/)
+{
+    RuntimeError('Init shouldn't be called, use constructor');
+    // not implemented, calls the underlying class instead
+}
+    
+    
+    {private:
+    long long m_start;
+    long long m_end;
+};
+    
+            int m_id;         // sample id
+        int m_rank0;        // original rank based on label
+        int m_rank;         // rank based on s in the associated query
+        ElemType m_score;    // score
+        ElemType m_gain;    // gain
+        bool operator < (const Url &url) const{
+            // tie breaking
+            if (m_score == url.m_score || std::isnan(m_score) || std::isnan(url.m_score))
+            {
+                return m_gain < url.m_gain;
+            }
+    }
+    
+    private: 
+    bool CheckOverlap(pair<int, int>occ, vector<pair<int, int>>&occVec)
+    {
+        bool bRet = false;
+        for (auto& o : occVec)
+        {
+            if (occ.first <= o.second && occ.second >= o.first)
+            {
+                bRet = true;
+                break;
             }
         }
+//#define SUPRESS_MEMSHARING // #define this to disable memory sharing by always return true 
+// TODO: Make this a runtime option.
+#ifdef SUPRESS_MEMSHARING
+        bRet = true; 
+#endif
+        return bRet;
+    }
+    
+    class DHTNode;
+class DHTRoutingTable;
+class DHTTaskQueue;
+class DHTTaskFactory;
+class DHTPeerAnnounceStorage;
+class DHTTokenTracker;
+class DHTMessageDispatcher;
+class DHTMessageReceiver;
+class DHTMessageFactory;
+    
+      bool addGoodNode(const std::shared_ptr<DHTNode>& node);
+    
+    DHTTaskExecutor::DHTTaskExecutor(int numConcurrent)
+    : numConcurrent_(numConcurrent)
+{
+}
+    
+    
+    {} // namespace aria2
+    
+      virtual std::shared_ptr<DHTTask>
+  createReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
+                        const std::shared_ptr<DHTNode>& newNode) CXX11_OVERRIDE;
+    
+    #include 'DHTTaskQueue.h'
+#include 'DHTTaskExecutor.h'
+    
+        std::vector<AddrEntry>::const_iterator find(const std::string& addr) const;
+    
+    
+    {  oprot_->writeMessageEnd();
+  oprot_->getTransport()->writeEnd();
+  oprot_->getTransport()->flush();
+}
+    
+      virtual ~ExtensionManagerIfFactory() {}
+    
+    #include 'ExtensionManager.h'
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TSimpleServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+    
+    class KernelInfo : public testing::Test {
+  protected:
+    void SetUp() override {
+      setUpEnvironment();
     }
 };
     
-    SEXP XGDMatrixSaveBinary_R(SEXP handle, SEXP fname, SEXP silent) {
-  R_API_BEGIN();
-  CHECK_CALL(XGDMatrixSaveBinary(R_ExternalPtrAddr(handle),
-                                 CHAR(asChar(fname)),
-                                 asInteger(silent)));
-  R_API_END();
-  return R_NilValue;
+    TEST_F(kernelIntegrity, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from kernel_integrity');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'sycall_addr_modified', IntType}
+  //      {'text_segment_hash', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
 }
     
-      /*!
-   * \brief feature contributions to individual predictions; the output will be a vector
-   *         of length (nfeats + 1) * num_output_group * nsample, arranged in that order
-   * \param dmat feature matrix
-   * \param out_contribs output vector to hold the contributions
-   * \param ntree_limit limit the number of trees used in prediction, when it equals 0, this means
-   *    we do not limit number of trees
-   * \param approximate use a faster (inconsistent) approximation of SHAP values
-   * \param condition condition on the condition_feature (0=no, -1=cond off, 1=cond on).
-   * \param condition_feature feature to condition on (i.e. fix) during calculations
-   */
-  virtual void PredictContribution(DMatrix* dmat,
-                           std::vector<bst_float>* out_contribs,
-                           unsigned ntree_limit = 0, bool approximate = false,
-                           int condition = 0, unsigned condition_feature = 0) = 0;
+    TEST_F(kernelPanics, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from kernel_panics');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'path', NormalType}
+  //      {'time', NormalType}
+  //      {'registers', NormalType}
+  //      {'frame_backtrace', NormalType}
+  //      {'module_backtrace', NormalType}
+  //      {'dependencies', NormalType}
+  //      {'name', NormalType}
+  //      {'os_version', NormalType}
+  //      {'kernel_version', NormalType}
+  //      {'system_model', NormalType}
+  //      {'uptime', IntType}
+  //      {'last_loaded', NormalType}
+  //      {'last_unloaded', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
+}
     
-      bool Read(SparsePage* page, dmlc::SeekStream* fi) override {
-    auto& offset_vec = page->offset.HostVector();
-    auto& data_vec = page->data.HostVector();
-    if (!fi->Read(&(offset_vec))) return false;
-    CHECK_NE(offset_vec.size(), 0) << 'Invalid SparsePage file';
-    this->LoadIndexValue(fi);
-    }
-    
-      TestIterRef(int * _status): status_(_status) {}
+    TEST_F(keychainAcls, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from keychain_acls');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'keychain_path', NormalType}
+  //      {'authorizations', NormalType}
+  //      {'path', NormalType}
+  //      {'description', NormalType}
+  //      {'label', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
+}
