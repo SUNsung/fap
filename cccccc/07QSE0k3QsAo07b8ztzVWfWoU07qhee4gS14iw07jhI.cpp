13e6b44c7de1f8a05edbb724a08c35002bd81155
SUNsung <weatherius@gmail.com>
2019-05-28 16:51:00 +0300

    Rewrite [07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp] File in: 2019-05-28 16:50:59

--- cccccc/07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp
+++ cccccc/07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp
@@ -1,127 +1,271 @@
 
         
-                int k = length - n;
-        assert(k >= 0);
-        ListNode* cur = dummyHead;
-        for(int i = 0 ; i < k ; i ++)
-            cur = cur->next;
+        #define MERGE_QUAD(sgn, bits, n) { \
+                                     FILL_LINES##n(PREF, sgn##bits) \
+                                     MERGE_ASM##n(sgn, bits) \
+                                 }
     
-                    char c = stack.top();
-                stack.pop();
-    
-    
-    {private:
-    int nextDifferentCharacterIndex(const vector<int> &nums, int p){
-        for( ; p < nums.size() ; p ++ )
-            if( nums[p] != nums[p - 1] )
-                break;
-        return p;
-    }
-};
-    
-    // Counting
-// Time Complexity: O(n)
-// Space Complexity: O(3)
-class Solution {
-public:
-    void sortColors(vector<int> &nums) {
-    }
+    namespace internal {
     }
     
-    public:
-    vector<int> inorderTraversal(TreeNode* root) {
+             int32x4_t vline1_s32 = vmovl_s16(vget_low_s16(vline1_s16));
+         int32x4_t vline2_s32 = vmovl_s16(vget_high_s16(vline1_s16));
+         int32x4_t vline3_s32 = vmovl_s16(vget_low_s16(vline2_s16));
+         int32x4_t vline4_s32 = vmovl_s16(vget_high_s16(vline2_s16));
+    
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
+CVTS_FUNC(u8, s32, 16,
+    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
+    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
+{
+    for (size_t i = 0; i < w; i += 16)
+    {
+        internal::prefetch(_src + i);
+        __asm__ (
+            'vld1.8 {d4-d5}, [%[src]]                              \n\t'
+            'vmovl.u8 q3, d4                                       \n\t'
+            'vmovl.u8 q4, d5                                       \n\t'
+            'vmovl.u16 q5, d6                                      \n\t'
+            'vmovl.u16 q6, d7                                      \n\t'
+            'vmovl.u16 q7, d8                                      \n\t'
+            'vmovl.u16 q8, d9                                      \n\t'
+            'vcvt.f32.u32 q9, q5                                   \n\t'
+            'vcvt.f32.u32 q10, q6                                  \n\t'
+            'vcvt.f32.u32 q11, q7                                  \n\t'
+            'vcvt.f32.u32 q12, q8                                  \n\t'
+            'vmul.f32 q13, q9, q0                                  \n\t'
+            'vmul.f32 q14, q10, q0                                 \n\t'
+            'vmul.f32 q15, q11, q0                                 \n\t'
+            'vmul.f32 q2, q12, q0                                  \n\t'
+            'vadd.f32 q3, q13, q1                                  \n\t'
+            'vadd.f32 q4, q14, q1                                  \n\t'
+            'vadd.f32 q5, q15, q1                                  \n\t'
+            'vadd.f32 q6, q2, q1                                   \n\t'
+            'vcvt.s32.f32 q7, q3                                   \n\t'
+            'vcvt.s32.f32 q8, q4                                   \n\t'
+            'vcvt.s32.f32 q9, q5                                   \n\t'
+            'vcvt.s32.f32 q10, q6                                  \n\t'
+            'vst1.32 {d14-d15}, [%[dst1]]                          \n\t'
+            'vst1.32 {d16-d17}, [%[dst2]]                          \n\t'
+            'vst1.32 {d18-d19}, [%[dst3]]                          \n\t'
+            'vst1.32 {d20-d21}, [%[dst4]]                          \n\t'
+            : /*no output*/
+            : [src] 'r' (_src + i),
+              [dst1] 'r' (_dst + i + 0),
+              [dst2] 'r' (_dst + i + 4),
+              [dst3] 'r' (_dst + i + 8),
+              [dst4] 'r' (_dst + i + 12),
+              'w'  (vscale), 'w' (vshift)
+            : 'd4','d5','d6','d7','d8','d9','d10',
+            'd11','d12','d13','d14','d15','d16','d17',
+            'd18','d19','d20','d21','d22','d23','d24',
+            'd25','d26','d27','d28','d29','d30','d31'
+        );
+    }
+})
+#else
+CVTS_FUNC(u8, s32, 16,
+    float32x4_t vscale = vdupq_n_f32((f32)alpha);
+    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
+{
+    for (size_t i = 0; i < w; i += 16)
+    {
+        internal::prefetch(_src + i);
+        uint8x16_t vline = vld1q_u8(_src + i);
+        uint16x8_t vline1_u16 = vmovl_u8(vget_low_u8 (vline));
+        uint16x8_t vline2_u16 = vmovl_u8(vget_high_u8(vline));
+        uint32x4_t vline1_u32 = vmovl_u16(vget_low_u16 (vline1_u16));
+        uint32x4_t vline2_u32 = vmovl_u16(vget_high_u16(vline1_u16));
+        uint32x4_t vline3_u32 = vmovl_u16(vget_low_u16 (vline2_u16));
+        uint32x4_t vline4_u32 = vmovl_u16(vget_high_u16(vline2_u16));
+        float32x4_t vline1_f32 = vcvtq_f32_u32(vline1_u32);
+        float32x4_t vline2_f32 = vcvtq_f32_u32(vline2_u32);
+        float32x4_t vline3_f32 = vcvtq_f32_u32(vline3_u32);
+        float32x4_t vline4_f32 = vcvtq_f32_u32(vline4_u32);
+        vline1_f32 = vmulq_f32(vline1_f32, vscale);
+        vline2_f32 = vmulq_f32(vline2_f32, vscale);
+        vline3_f32 = vmulq_f32(vline3_f32, vscale);
+        vline4_f32 = vmulq_f32(vline4_f32, vscale);
+        vline1_f32 = vaddq_f32(vline1_f32, vshift);
+        vline2_f32 = vaddq_f32(vline2_f32, vshift);
+        vline3_f32 = vaddq_f32(vline3_f32, vshift);
+        vline4_f32 = vaddq_f32(vline4_f32, vshift);
+        int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
+        int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
+        int32x4_t vline3_s32 = vcvtq_s32_f32(vline3_f32);
+        int32x4_t vline4_s32 = vcvtq_s32_f32(vline4_f32);
+        vst1q_s32(_dst + i + 0,  vline1_s32);
+        vst1q_s32(_dst + i + 4,  vline2_s32);
+        vst1q_s32(_dst + i + 8,  vline3_s32);
+        vst1q_s32(_dst + i + 12, vline4_s32);
+    }
+})
+#endif
+    
+    #include 'common.hpp'
+    
+                for(; j < (ptrdiff_t)size.width - 16 - 3; j += 16, ptr += 16)
+            {
+                internal::prefetch(ptr);
+                internal::prefetch(ptr + pixel[0]);
+                internal::prefetch(ptr + pixel[2]);
     }
     
-            stack<TreeNode*> stack;
-        TreeNode* cur = root;
-        while(cur != NULL || !stack.empty()){
+    void gaussianBlur5x5(const Size2D &size, s32 cn,
+                     const u16 * srcBase, ptrdiff_t srcStride,
+                     u16 * dstBase, ptrdiff_t dstStride,
+                     BORDER_MODE borderType, u16 borderValue, Margin borderMargin)
+{
+    internal::assertSupportedConfiguration(isGaussianBlur5x5Supported(size, cn, borderType));
+#ifdef CAROTENE_NEON
+    size_t colsn = size.width * cn;
     }
     
-                int new_level_num = 0;
-            vector<int> level;
-            for(int i = 0; i < level_num; i ++){
-                TreeNode* node = q.front();
-                q.pop();
-                level.push_back(node->val);
-    }
-    
-    #include <iostream>
-#include <vector>
+      {
+    string str;
+    StringByteSink sink(&str);
+    limit_source.CopyTo(&sink, limit_source.Available());
+    EXPECT_EQ('ello ', str);
+    EXPECT_EQ(0, limit_source.Available());
+    EXPECT_EQ(6, source.Available());
+  }
     
     
     {
     {
-    {            if(command.s == 'print')
-                res.push_back(command.node->val);
-            else{
-                assert(command.s == 'go');
-                stack.push(Command('print', command.node));
-                if(command.node->right)
-                    stack.push(Command('go',command.node->right));
-                if(command.node->left)
-                    stack.push(Command('go',command.node->left));
-            }
-        }
-        return res;
-    }
-};
-    
-            stack.push(root);
-        while(!stack.empty()){
-    }
-    
-    #endif
+    {}  // namespace util
+}  // namespace protobuf
+}  // namespace google
 
     
-    //////////////////////////////////////////////////////////////////////
+    TEST(StatusOr, TestStatusCtor) {
+  StatusOr<int> thing(Status::CANCELLED);
+  EXPECT_FALSE(thing.ok());
+  EXPECT_EQ(Status::CANCELLED, thing.status());
+}
     
-    
-    {  if (TOCoffset > INT16_MAX) {
-    int16_t complement = 0;
-    // If last four bytes is still bigger than a signed 16bits, uses as two
-    // complement.
-    if ((TOCoffset & UINT16_MAX) > INT16_MAX) complement = 1;
-    addis(rt, reg::r2, static_cast<int16_t>((TOCoffset >> 16) + complement));
-    ld (rt, rt[TOCoffset & UINT16_MAX]);
-  } else {
-    ld (rt, reg::r2[TOCoffset]);
-    emitNop(instr_size_in_bytes);
+    TEST(StructurallyValidTest, ValidUTF8String) {
+  // On GCC, this string can be written as:
+  //   'abcd 1234 - \u2014\u2013\u2212'
+  // MSVC seems to interpret \u differently.
+  string valid_str('abcd 1234 - \342\200\224\342\200\223\342\210\222 - xyz789');
+  EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data(),
+                                      valid_str.size()));
+  // Additional check for pointer alignment
+  for (int i = 1; i < 8; ++i) {
+    EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data() + i,
+                                        valid_str.size() - i));
   }
 }
     
-          X_form_t x_formater {{
-                            rc,
-                            xop,
-                            static_cast<uint32_t>(rb),
-                            static_cast<uint32_t>(ra),
-                            static_cast<uint32_t>(rt),
-                            op
-                          }};
-    
-    namespace HPHP {
-    }
-    
-    #include 'hphp/runtime/base/file.h'
-#include 'hphp/runtime/base/execution-context.h'
-#include 'hphp/runtime/base/request-event-handler.h'
-    
-    U_NAMESPACE_BEGIN
-    
-    void SearchIterator::setText(const UnicodeString &text, UErrorCode &status)
-{
-    if (U_SUCCESS(status)) {
-        if (text.length() == 0) {
-            status = U_ILLEGAL_ARGUMENT_ERROR;
-        }
-        else {
-            m_text_        = text;
-            m_search_->text = m_text_.getBuffer();
-            m_search_->textLength = m_text_.length();
-        }
-    }
+    std::string ReadFile(const std::string& name) {
+  std::ifstream file(name.c_str());
+  GOOGLE_CHECK(file.is_open()) << 'Couldn't find file ''
+      << name
+      << '', please make sure you are running this command from the benchmarks'
+      << ' directory.\n';
+  return std::string((std::istreambuf_iterator<char>(file)),
+                     std::istreambuf_iterator<char>());
 }
     
-    #if !UCONFIG_NO_BREAK_ITERATION
+    using google::protobuf::util::Proto3DataStripper;
+    
+      virtual bool Generate(const FileDescriptor* file,
+                        const string& parameter,
+                        GeneratorContext* context,
+                        string* error) const {
+    FileDescriptorProto new_file;
+    file->CopyTo(&new_file);
+    SchemaGroupStripper::StripFile(file, &new_file);
+    }
+    
+    // Main function:  Reads the entire address book from a file and prints all
+//   the information inside.
+int main(int argc, char* argv[]) {
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+    }
+    
+        class Rational
+    {
+    public:
+        Rational() noexcept;
+        Rational(Number const& n) noexcept;
+        Rational(Number const& p, Number const& q) noexcept;
+        Rational(int32_t i);
+        Rational(uint32_t ui);
+        Rational(uint64_t ui);
+    }
+    
+    void CCalcEngine::InitChopNumbers()
+{
+    // these rat numbers are set only once and then never change regardless of
+    // base or precision changes
+    assert(m_chopNumbers.size() >= 4);
+    m_chopNumbers[0] = Rational{ rat_qword };
+    m_chopNumbers[1] = Rational{ rat_dword };
+    m_chopNumbers[2] = Rational{ rat_word };
+    m_chopNumbers[3] = Rational{ rat_byte };
+    }
+    
+    CalculatorHistory::CalculatorHistory(size_t maxSize)
+    : m_maxHistorySize(maxSize)
+{
+}
+    
+        static std::vector<uint32_t> DigitGroupingStringToGroupingVector(std::wstring_view groupingString);
+    std::wstring GroupDigits(std::wstring_view delimiter, std::vector<uint32_t> const& grouping, std::wstring_view displayString, bool isNumNegative = false);
+    
+    //-----------------------------------------------------------------------------
+//
+//   External functions defined in the math package.
+//
+//-----------------------------------------------------------------------------
+    
+    
+    {
+    {            if (U_SUCCESS(status)) {
+                destLen += appendGroup(groupNum, dest, status);
+            }
+        }  // End of $ capture group handling
+    }  // End of per-character loop through the replacement string.
+    
+    
+//---------------------------------------------------------------------
+//
+//   matcher(UnicodeString, err)
+//
+//---------------------------------------------------------------------
+RegexMatcher *RegexPattern::matcher(const UnicodeString &input,
+                                    UErrorCode          &status)  const {
+    RegexMatcher    *retMatcher = matcher(status);
+    if (retMatcher != NULL) {
+        retMatcher->fDeferredStatus = status;
+        retMatcher->reset(input);
+    }
+    return retMatcher;
+}
+    
+    //-------------------------------------------------------------------------------
+//
+//  ScriptSet - A bit set representing a set of scripts.
+//
+//              This class was originally used exclusively with script sets appearing
+//              as part of the spoof check whole script confusable binary data. Its
+//              use has since become more general, but the continued use to wrap
+//              prebuilt binary data does constrain the design.
+//
+//-------------------------------------------------------------------------------
+class U_I18N_API ScriptSet: public UMemory {
+  public:
+    ScriptSet();
+    ScriptSet(const ScriptSet &other);
+    ~ScriptSet();
+    }
+    
+    SharedBreakIterator::SharedBreakIterator(
+        BreakIterator *biToAdopt) : ptr(biToAdopt) { }
     
     #include 'unicode/utypes.h'
 #include 'sharedobject.h'
@@ -138,48 +282,12 @@ private:
     SharedPluralRules &operator=(const SharedPluralRules &);
 };
     
-    // Called by TimeZone::createDefault(), then clone() inside a Mutex - be careful.
-SimpleTimeZone &
-SimpleTimeZone::operator=(const SimpleTimeZone &right)
-{
-    if (this != &right)
-    {
-        TimeZone::operator=(right);
-        rawOffset      = right.rawOffset;
-        startMonth     = right.startMonth;
-        startDay       = right.startDay;
-        startDayOfWeek = right.startDayOfWeek;
-        startTime      = right.startTime;
-        startTimeMode  = right.startTimeMode;
-        startMode      = right.startMode;
-        endMonth       = right.endMonth;
-        endDay         = right.endDay;
-        endDayOfWeek   = right.endDayOfWeek;
-        endTime        = right.endTime;
-        endTimeMode    = right.endTimeMode;
-        endMode        = right.endMode;
-        startYear      = right.startYear;
-        dstSavings     = right.dstSavings;
-        useDaylight    = right.useDaylight;
-        clearTransitionRules();
-    }
-    return *this;
-}
+    U_NAMESPACE_BEGIN
     
     // Bitwise comparison for the collation keys.
-UCollationResult
-CollationKey::compareTo(const CollationKey& target, UErrorCode &status) const
+Collator::EComparisonResult
+CollationKey::compareTo(const CollationKey& target) const
 {
-  if(U_SUCCESS(status)) {
-    const uint8_t *src = getBytes();
-    const uint8_t *tgt = target.getBytes();
-    }
-    }
-    
-        /**
-     * @param keyword for example 'few' or 'other'
-     * @return the plural form corresponding to the keyword, or OTHER
-     */
-    static Form orOtherFromString(const char *keyword) {
-        return static_cast<Form>(indexOrOtherIndexFromString(keyword));
-    }
+    UErrorCode errorCode = U_ZERO_ERROR;
+    return static_cast<Collator::EComparisonResult>(compareTo(target, errorCode));
+}
