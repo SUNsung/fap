d5eaa0799fbef422d231d0a9fa8b4c11a4793909
SUNsung <weatherius@gmail.com>
2019-05-28 23:51:42 +0300

    Rewrite [47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp] File in: 2019-05-28 23:51:41

--- cccccc/47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp
+++ cccccc/47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp
@@ -1,26 +1,46 @@
 
         
-        
-    {    /* On the first run, return 0 to force a second run */
-    if (counter == 0) {
-        memset(nonce32, 0, 32);
-        return 1;
+        // Type-opaque holder for an argument to format(), with associated actions on
+// the type held as explicit function pointers.  This allows FormatArg's for
+// each argument to be allocated as a homogenous array inside FormatList
+// whereas a naive implementation based on inheritance does not.
+class FormatArg
+{
+    public:
+        FormatArg()
+             : m_value(nullptr),
+             m_formatImpl(nullptr),
+             m_toIntImpl(nullptr)
+         { }
     }
-    /* On the second run, return an overflow to force a third run */
-    if (counter == 1) {
-        memset(nonce32, 0xff, 32);
-        return 1;
-    }
-    /* On the next run, return a valid nonce, but flip a coin as to whether or not to fail signing. */
-    memset(nonce32, 1, 32);
-    return secp256k1_rand_bits(1);
-}
     
-    // Generate param traits read methods.
-#include 'ipc/param_traits_read_macros.h'
-namespace IPC {
+    
+    
+    #ifndef BITCOIN_BECH32_H
+#define BITCOIN_BECH32_H
+    
+    // Generate constructors.
+#include 'ipc/struct_constructor_macros.h'
 #include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
+    
+    class App {
+ public:
+  static void Call(const std::string& method,
+                   const base::ListValue& arguments);
+    }
+    
+      virtual void Call(const std::string& method,
+                    const base::ListValue& arguments,
+                    content::RenderFrameHost* rvh = nullptr);
+  virtual void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result);
+    
+    Clipboard::Clipboard(int id,
+           const base::WeakPtr<DispatcherHost>& dispatcher_host,
+           const base::DictionaryValue& option)
+    : Base(id, dispatcher_host, option) {
+}
     
     class Clipboard : public Base {
  public:
@@ -31,105 +51,258 @@ namespace IPC {
     }
     
     
-#include 'base/basictypes.h'
+    {}
     
-    bool MenuDelegate::IsItemForCommandIdDynamic(int command_id) const {
-  if (command_id < 0)
-    return false;
-    }
     
-       bool IsItemForCommandIdDynamic(int command_id) const override;
-   base::string16 GetLabelForCommandId(int command_id) const override;
-   bool GetIconForCommandId(int command_id,
-                                   gfx::Image* icon) const override;
+    {}  // namespace nwapi
+
     
-    void Menu::UpdateStates() {
+    
+    {}
+    
+    #include 'base/values.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'content/nw/src/api/menu/menu.h'
+#include 'gdk/gdkkeysyms.h'//to get keyval from name
+    
+    void MenuItem::SetEnabled(bool enabled) {
+  is_enabled_ = enabled;
+  if (menu_)
+    menu_->UpdateStates();
 }
     
-    bool NwClipboardGetListSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  std::unique_ptr<GetListSync::Params> params(GetListSync::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
-  std::unique_ptr<ClipboardReader> reader(new ClipboardReader());
+          std::unique_ptr<SkBitmap> bitmap(new SkBitmap());
+      if (data.type == TYPE_PNG &&
+        !gfx::PNGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size(), bitmap.get())) {
+        error_ = 'Failed to decode as PNG';
+        return false;
+      } else if (data.type == TYPE_JPEG) {
+        std::unique_ptr<SkBitmap> tmp_bitmap = gfx::JPEGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size());
+        if (tmp_bitmap == NULL) {
+          error_ = 'Failed to decode as JPEG';
+          return false;
+        }
+        bitmap = std::move(tmp_bitmap);
+      }
+    
+    #include 'extensions/browser/extension_function.h'
+    
+    		Image image(a_pafSourceRGBA, a_uiSourceWidth,
+					a_uiSourceHeight,
+					a_eErrMetric);
+		image.m_bVerboseOutput = a_bVerboseOutput;
+		image.Encode(a_format, a_eErrMetric, a_fEffort, a_uiJobs, a_uiMaxJobs);
+    
+    
+    {			return fDX*fDX + fDY*fDY + fDZ*fDZ + fDW*fDW;
+		}
+    
+    	class Block4x4EncodingBits_RG11
+	{
+	public:
     }
     
+      The output buffer must be at least 5% larger than the input buffer
+  and can not be smaller than 66 bytes.
     
-    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.clearSync', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwClipboardClearSyncFunction);
+       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+    
+    /** 16x32 multiply, followed by a 16-bit shift right and 32-bit add.
+    Result fits in 32 bits. */
+#undef MAC16_32_Q16
+static OPUS_INLINE opus_val32 MAC16_32_Q16_armv5e(opus_val32 c, opus_val16 a,
+ opus_val32 b)
+{
+  int res;
+  __asm__(
+      '#MAC16_32_Q16\n\t'
+      'smlawb %0, %1, %2, %3;\n'
+      : '=r'(res)
+      : 'r'(b), 'r'(a), 'r'(c)
+  );
+  return res;
+}
+#define MAC16_32_Q16(c, a, b) (MAC16_32_Q16_armv5e(c, a, b))
+    
+       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+    
+      // check if font is already embedded
+  fileName = font->getExtFontFile();
+  for (i = 0; i < fontFileNameLen; ++i) {
+    if (!fontFileNames[i]->cmp(fileName)) {
+      return psFileNames[i]->copy();
+    }
+  }
+    
+      // Get the next PostScript token.  Returns false at end-of-stream.
+  GBool getToken(char *buf, int size, int *length);
+    
+    class PopplerCache
+{
+  public:
+    PopplerCache(int cacheSizeA);
+    ~PopplerCache();
+    
+    /* The item returned is owned by the cache */
+    PopplerCacheItem *lookup(const PopplerCacheKey &key);
+    
+    /* The key and item pointers ownership is taken by the cache */
+    void put(PopplerCacheKey *key, PopplerCacheItem *item);
+    
+    /* The max size of the cache */
+    int size();
+    
+    /* The number of items in the cache */
+    int numberOfItems();
+    
+    /* The n-th item in the cache */
+    PopplerCacheItem *item(int index);
+    
+    /* The n-th key in the cache */
+    PopplerCacheKey *key(int index);
+  
+  private:
+    PopplerCache(const PopplerCache &cache); // not allowed
+  
+    PopplerCacheKey **keys;
+    PopplerCacheItem **items;
+    int lastValidCacheIndex;
+    int cacheSize;
 };
     
-      ::opencensus::trace::SpanContext Context() const { return span_.context(); }
-  ::opencensus::trace::Span Span() const { return span_; }
-  void EndSpan() { span_.End(); }
-    
-    
-    {  const protobuf::FieldDescriptor* field_desc =
-      descriptor_pool_->FindExtensionByNumber(desc,
-                                              request->extension_number());
-  if (field_desc == nullptr) {
-    return Status(StatusCode::NOT_FOUND, 'Extension not found.');
-  }
-  std::unordered_set<grpc::string> seen_files;
-  FillFileDescriptorResponse(field_desc->file(), response, &seen_files);
-  return Status::OK;
+    void PreScanOutputDev::fill(GfxState *state) {
+  check(state->getFillColorSpace(), state->getFillColor(),
+	state->getFillOpacity(), state->getBlendMode());
 }
     
-    #include <grpcpp/grpcpp.h>
-#include 'src/proto/grpc/reflection/v1alpha/reflection.grpc.pb.h'
-    
-                    auto tempRat = m_chopNumbers[m_numwidth] >> holdVal;
-                tempRat = Integer(tempRat);
-    
-        Rational Sin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Cos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Tan(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ASin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ACos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ATan(Rational const& rat, ANGLE_TYPE angletype);
-    
-    INarratorAnnouncementHost^ LiveRegionHost::MakeHost()
-{
-    return ref new LiveRegionHost();
+    ProfileData::ProfileData() {
+	count = 0;
+	total = 0.0;
+	min = 0.0;
+	max = 0.0;
 }
     
-        wstring EngineResourceProvider::GetCEngineString(const wstring& id)
-    {
-        const auto& localizationSettings = LocalizationSettings::GetInstance();
-    }
+      int width;                             // -1
+  int height;                            // -1
+  
+  // floating window position
+  MediaWindowRelativeTo relativeTo;      // windowRelativeToDocument (or to desktop)
+  double XPosition;                      // 0.5
+  double YPosition;                      // 0.5
     
-    CBinaryCommand CommandDeserializer::DeserializeBinary()
-{
-    int cmd = m_dataReader->ReadInt32();
-    return CBinaryCommand(cmd);
+    
+    {  return newsound;
 }
 
     
-    int main()
+      virtual ~SplashGouraudPattern();
+    
+    PDFDoc *
+StdinPDFDocBuilder::buildPDFDoc(const GooString &uri, GooString *ownerPassword,
+                                    GooString *userPassword, void *guiDataA)
 {
-    // create JSON array
-    json array = {'first', '2nd', 'third', 'fourth'};
+  Object obj;
     }
     
-        // out_of_range.401
-    try
-    {
-        // try to use a an invalid array index
-        json::reference ref = j.at('/array/4'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+    void RemoveTransliterator::handleTransliterate(Replaceable& text, UTransPosition& index,
+                                               UBool /*isIncremental*/) const {
+    // Our caller (filteredTransliterate) has already narrowed us
+    // to an unfiltered run.  Delete it.
+    UnicodeString empty;
+    text.handleReplaceBetween(index.start, index.limit, empty);
+    int32_t len = index.limit - index.start;
+    index.contextLimit -= len;
+    index.limit -= len;
+}
+U_NAMESPACE_END
     
+        case URX_ONECHAR:
+    case URX_ONECHAR_I:
+        if (val < 0x20) {
+            printf('%#x', val);
+        } else {
+            printf(''%s'', CStr(UnicodeString(val))());
+        }
+        break;
     
-    {    // out_of_range.404
-    try
-    {
-        // try to use a JSON pointer that cannot be resolved
-        json::const_reference ref = j.at('/number/foo'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
+    ScientificNumberFormatter::ScientificNumberFormatter(
+        const ScientificNumberFormatter &other)
+        : UObject(other),
+          fPreExponent(other.fPreExponent),
+          fDecimalFormat(NULL),
+          fStyle(NULL),
+          fStaticSets(other.fStaticSets) {
+    fDecimalFormat = static_cast<DecimalFormat *>(
+            other.fDecimalFormat->clone());
+    fStyle = other.fStyle->clone();
+}
+    
+    #if !UCONFIG_NO_FORMATTING
+    
+    SharedBreakIterator::SharedBreakIterator(
+        BreakIterator *biToAdopt) : ptr(biToAdopt) { }
+    
+    U_NAMESPACE_END
+    
+    int32_t StandardPlural::indexFromString(const UnicodeString &keyword, UErrorCode &errorCode) {
+    if (U_FAILURE(errorCode)) { return OTHER; }
+    int32_t i = indexOrNegativeFromString(keyword);
+    if (i >= 0) {
+        return i;
+    } else {
+        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
+        return OTHER;
     }
 }
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    /*
+ * If Trace::hhbbc_time >= 1, print some stats about the program to a
+ * temporary file.  If it's greater than or equal to 2, also dump it
+ * to stdout.
+ */
+void print_stats(const Index&, const php::Program&);
+    
+    void VMTOC::setTOCDataBlock(HPHP::DataBlock *db) {
+  if(m_tocvector == nullptr) {
+    m_tocvector = db;
+    HPHP::Address addr = m_tocvector->frontier();
+    forceAlignment(addr);
+  }
+  return;
+}
+    
+       void EmitSCForm(const uint8_t op,
+                   const uint16_t lev) {
+      SC_form_t sc_formater {{
+                              1,
+                              lev,
+                              op
+                            }};
+    }
+    
+    #ifndef HPHP_DATA_STREAM_WRAPPER_H
+#define HPHP_DATA_STREAM_WRAPPER_H
