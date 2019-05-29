5329aee3a9cd704551390dbfb9163ac05797d1cc
SUNsung <weatherius@gmail.com>
2019-05-29 06:02:52 +0300

    Rewrite [32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py] File in: 2019-05-29 06:02:52

--- pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
+++ pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
@@ -1,192 +1,282 @@
 
         
-            assert proc.expect([TIMEOUT, u'usage'])
+            Args:
+      inputs: A 2D batch x input_dim tensor of inputs.
+      state: The previous state from the last time step.
+      scope (optional): TF variable scope for defined GRU variables.
+    
+    flags = tf.app.flags
+flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
+                    'Directory for saving data.')
+flags.DEFINE_string('datafile_name', 'thits_data',
+                    'Name of data file for input case.')
+flags.DEFINE_string('noise_type', 'poisson', 'Noise type for data.')
+flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
+flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
+flags.DEFINE_integer('C', 100, 'Number of conditions')
+flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
+flags.DEFINE_integer('S', 50, 'Number of sampled units from RNN')
+flags.DEFINE_integer('npcs', 10, 'Number of PCS for multi-session case.')
+flags.DEFINE_float('train_percentage', 4.0/5.0,
+                   'Percentage of train vs validation trials')
+flags.DEFINE_integer('nreplications', 40,
+                     'Number of noise replications of the same underlying rates.')
+flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
+flags.DEFINE_float('x0_std', 1.0,
+                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
+flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
+flags.DEFINE_float('dt', 0.010, 'Time bin')
+flags.DEFINE_float('input_magnitude', 20.0,
+                   'For the input case, what is the value of the input?')
+flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
+FLAGS = flags.FLAGS
+    
+    flags = tf.app.flags
+flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
+                    'Directory for saving data.')
+flags.DEFINE_string('datafile_name', 'itb_rnn',
+                    'Name of data file for input case.')
+flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
+flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
+flags.DEFINE_integer('C', 800, 'Number of conditions')
+flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
+flags.DEFINE_float('train_percentage', 4.0/5.0,
+                   'Percentage of train vs validation trials')
+flags.DEFINE_integer('nreplications', 5,
+                     'Number of spikifications of the same underlying rates.')
+flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
+flags.DEFINE_float('dt', 0.010, 'Time bin')
+flags.DEFINE_float('max_firing_rate', 30.0,
+                   'Map 1.0 of RNN to a spikes per second')
+flags.DEFINE_float('u_std', 0.25,
+                   'Std dev of input to integration to bound model')
+flags.DEFINE_string('checkpoint_path', 'SAMPLE_CHECKPOINT',
+                    '''Path to directory with checkpoints of model
+                    trained on integration to bound task. Currently this
+                    is a placeholder which tells the code to grab the
+                    checkpoint that is provided with the code
+                    (in /trained_itb/..). If you have your own checkpoint
+                    you would like to restore, you would point it to
+                    that path.''')
+FLAGS = flags.FLAGS
+    
+    rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
+rnn_rngs = [np.random.RandomState(seed=FLAGS.synth_data_seed+1),
+            np.random.RandomState(seed=FLAGS.synth_data_seed+2)]
+T = FLAGS.T
+C = FLAGS.C
+N = FLAGS.N
+nreplications = FLAGS.nreplications
+E = nreplications * C
+train_percentage = FLAGS.train_percentage
+ntimesteps = int(T / FLAGS.dt)
+    
+    FLAGS = tf.flags.FLAGS
+# General flags.
+tf.flags.DEFINE_string('mode', 'eval',
+                       'One of [sample, eval, dump_emb, dump_lstm_emb]. '
+                       ''sample' mode samples future word predictions, using '
+                       'FLAGS.prefix as prefix (prefix could be left empty). '
+                       ''eval' mode calculates perplexity of the '
+                       'FLAGS.input_data. '
+                       ''dump_emb' mode dumps word and softmax embeddings to '
+                       'FLAGS.save_dir. embeddings are dumped in the same '
+                       'order as words in vocabulary. All words in vocabulary '
+                       'are dumped.'
+                       'dump_lstm_emb dumps lstm embeddings of FLAGS.sentence '
+                       'to FLAGS.save_dir.')
+tf.flags.DEFINE_string('pbtxt', '',
+                       'GraphDef proto text file used to construct model '
+                       'structure.')
+tf.flags.DEFINE_string('ckpt', '',
+                       'Checkpoint directory used to fill model values.')
+tf.flags.DEFINE_string('vocab_file', '', 'Vocabulary file.')
+tf.flags.DEFINE_string('save_dir', '',
+                       'Used for 'dump_emb' mode to save word embeddings.')
+# sample mode flags.
+tf.flags.DEFINE_string('prefix', '',
+                       'Used for 'sample' mode to predict next words.')
+tf.flags.DEFINE_integer('max_sample_words', 100,
+                        'Sampling stops either when </S> is met or this number '
+                        'of steps has passed.')
+tf.flags.DEFINE_integer('num_samples', 3,
+                        'Number of samples to generate for the prefix.')
+# dump_lstm_emb mode flags.
+tf.flags.DEFINE_string('sentence', '',
+                       'Used as input for 'dump_lstm_emb' mode.')
+# eval mode flags.
+tf.flags.DEFINE_string('input_data', '',
+                       'Input data files for eval model.')
+tf.flags.DEFINE_integer('max_eval_steps', 1000000,
+                        'Maximum mumber of steps to run 'eval' mode.')
     
     
-@pytest.mark.functional
-def test_how_to_configure_alias(proc, TIMEOUT):
-    proc.sendline('unset -f fuck')
-    how_to_configure(proc, TIMEOUT)
-
+def evaluate_ensemble(test_data_name, number_of_lms):
+  ensemble = EnsembleLM(test_data_name)
+  model_list = ['lm{:02d}'.format(i+1) for i in range(number_of_lms)]
+  for model_name in model_list:
+    ensemble.add_single_model(model_name)
+  accuracy = ensemble.evaluate()
+  print('Accuracy of {} LM(s) on {} = {}'.format(
+      number_of_lms, test_data_name, accuracy))
+    
+    import tensorflow as tf
+    
+      Returns:
+    variable_mapping:  Dictionary with Key: ckpt_name, Value: model_varself.
+  '''
+  assert FLAGS.discriminator_model == 'seq2seq_vd'
+  assert hparams.dis_num_layers == 2
+    
+          done: `None`, which is used by the `dynamic_rnn_decoder` to indicate
+      that `sequence_lengths` in `dynamic_rnn_decoder` should be used.
+    
+    with open('update/LATEST_VERSION', 'w') as f:
+    f.write(version)
     
     
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'tcsh')
-    proc.sendline(u'setenv PYTHONIOENCODING utf8')
-    proc.sendline(u'eval `thefuck --alias`')
-    return proc
+# find the correct sorting and add the required base classes so that sublcasses
+# can be correctly created
+classes = _ALL_CLASSES[:-1]
+ordered_cls = []
+while classes:
+    for c in classes[:]:
+        bases = set(c.__bases__) - set((object, InfoExtractor, SearchInfoExtractor))
+        stop = False
+        for b in bases:
+            if b not in classes and b not in ordered_cls:
+                if b.__name__ == 'GenericIE':
+                    exit()
+                classes.insert(0, b)
+                stop = True
+        if stop:
+            break
+        if all(b in ordered_cls for b in bases):
+            ordered_cls.append(c)
+            classes.remove(c)
+            break
+ordered_cls.append(_ALL_CLASSES[-1])
     
-    match_output = '''
-Listing... Done
-heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
-resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
-squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
-unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
-'''
+        def gen_ies_md(ies):
+        for ie in ies:
+            ie_md = '**{0}**'.format(ie.IE_NAME)
+            ie_desc = getattr(ie, 'IE_DESC', None)
+            if ie_desc is False:
+                continue
+            if ie_desc is not None:
+                ie_md += ': {0}'.format(ie.IE_DESC)
+            if not ie.working():
+                ie_md += ' (Currently broken)'
+            yield ie_md
     
+    # The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+from youtube_dl.version import __version__
+version = __version__
+# The full version, including alpha/beta/rc tags.
+release = version
     
-@pytest.mark.parametrize('script, output', [
-    ('brew link sshfs', output),
-    ('cat output', output),
-    ('brew install sshfs', '')])
-def test_not_match(script, output):
-    command = Command(script, output)
-    assert not match(command)
-    
-    
-@parametrize_extensions
-@parametrize_filename
-@parametrize_script
-def test_side_effect(ext, tar_error, filename, unquoted, quoted, script, fixed):
-    tar_error(unquoted.format(ext))
-    side_effect(Command(script.format(filename.format(ext)), ''), None)
-    assert set(os.listdir('.')) == {unquoted.format(ext), 'd'}
-    
-    # Remove a subnet group
-- elasticache_subnet_group:
-    state: absent
-    name: norwegian-blue
-'''
-    
-    - oneandone_moitoring_policy:
-    auth_token: oneandone_private_api_key
-    monitoring_policy: ansible monitoring policy updated
-    remove_servers:
-     - server01
-    wait: true
-    state: update
-'''
-    
-    
-def protocol_to_string(protocol):
-    protocol = protocol_to_tuple(protocol)
-    if protocol[0] is True:
-        return 'Tcp'
-    elif protocol[1] is True:
-        return 'Udp'
-    elif protocol[2] is True:
-        return 'Icmp'
-    elif protocol[3] is True:
-        return 'Other'
-    elif protocol[4] is True:
-        return 'Any'
-    
-        if not hasattr(env, 'scrapy_all_settings'):
-        env.scrapy_all_settings = []
-    
-    # Check minimum required Python version
-import sys
-if sys.version_info < (2, 7):
-    print('Scrapy %s requires Python 2.7' % __version__)
-    sys.exit(1)
-    
-    
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
-    
-            if self.crawler_process.bootstrap_failed:
-            self.exitcode = 1
-
-    
-    from scrapy.commands import ScrapyCommand
-from scrapy.http import Request
-from scrapy.exceptions import UsageError
-from scrapy.utils.datatypes import SequenceExclude
-from scrapy.utils.spider import spidercls_for_request, DefaultSpider
-    
-        def syntax(self):
-        return '[options] <spider_file>'
-    
+            def try_rm_tcs_files(tcs=None):
+            if tcs is None:
+                tcs = test_cases
+            for tc in tcs:
+                tc_filename = get_tc_filename(tc)
+                try_rm(tc_filename)
+                try_rm(tc_filename + '.part')
+                try_rm(os.path.splitext(tc_filename)[0] + '.info.json')
+        try_rm_tcs_files()
         try:
-        import boto.auth
-    except ImportError:
-        _S3Connection = _v19_S3Connection
-    else:
-        _S3Connection = _v20_S3Connection
+            try_num = 1
+            while True:
+                try:
+                    # We're not using .download here since that is just a shim
+                    # for outside error handling, and returns the exit code
+                    # instead of the result dict.
+                    res_dict = ydl.extract_info(
+                        test_case['url'],
+                        force_generic_extractor=params.get('force_generic_extractor', False))
+                except (DownloadError, ExtractorError) as err:
+                    # Check if the exception is not a network related one
+                    if not err.exc_info[0] in (compat_urllib_error.URLError, socket.timeout, UnavailableVideoError, compat_http_client.BadStatusLine) or (err.exc_info[0] == compat_HTTPError and err.exc_info[1].code == 503):
+                        raise
     
-        def backwards(self, orm):
-        'Write your backwards methods here.'
+    
+@pytest.mark.parametrize(
+    ('username', 'password', 'message'),
+    (
+        ('', '', b'Username is required.'),
+        ('a', '', b'Password is required.'),
+        ('test', 'test', b'already registered'),
+    ),
+)
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register', data={'username': username, 'password': password}
+    )
+    assert message in response.data
     
     
-class Migration(SchemaMigration):
-    def forwards(self, orm):
-        # Adding model 'EnvironmentProject'
-        db.create_table(
-            'sentry_environmentproject', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'project', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.Project']
-                    )
-                ), (
-                    'environment', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.Environment']
-                    )
-                ),
-            )
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+    
+            buf.append(
+            '  Make sure to directly send your %s-request to this URL '
+            'since we can't make browsers or HTTP clients redirect '
+            'with form data reliably or without user interaction.' % request.method
         )
-        db.send_create_signal('sentry', ['EnvironmentProject'])
+        buf.append('\n\nNote: this exception is only raised in debug mode')
+        AssertionError.__init__(self, ''.join(buf).encode('utf-8'))
     
-        def backwards(self, orm):
+    import random
     
-            for name, organization_id in dupe_envs:
-            envs = list(
-                orm.Environment.objects.filter(
-                    name=name,
-                    organization_id=organization_id,
-                ).order_by('date_added')
-            )
-            to_env = envs[0]
-            from_envs = envs[1:]
+    from .hash_table import HashTable
     
-            # Adding field 'ApiAuthorization.scope_list'
-        db.add_column(
-            'sentry_apiauthorization',
-            'scope_list',
-            self.gf('sentry.db.models.fields.array.ArrayField')(
-                of=('django.db.models.fields.TextField', [], {})
-            ),
-            keep_default=False
+    This is a pure Python implementation of Dynamic Programming solution to the edit distance problem.
+    
+        def solve_sub_array(self):
+        rear = [int(self.array[0])]*len(self.array)
+        sum_value = [int(self.array[0])]*len(self.array)
+        for i in range(1, len(self.array)):
+            sum_value[i] = max(int(self.array[i]) + sum_value[i-1], int(self.array[i]))
+            rear[i] = max(sum_value[i], rear[i-1])
+        return rear[len(self.array)-1]
+    
+                if (arr[i-1] <= j):
+                dp[i][j] = dp[i][j] or dp[i-1][j-arr[i-1]]
+    
+    import numpy as np
+    
+    IMPORT_MAPPING.update({
+    'cPickle': 'pickle',
+    '_elementtree': 'xml.etree.ElementTree',
+    'FileDialog': 'tkinter.filedialog',
+    'SimpleDialog': 'tkinter.simpledialog',
+    'DocXMLRPCServer': 'xmlrpc.server',
+    'SimpleHTTPServer': 'http.server',
+    'CGIHTTPServer': 'http.server',
+    # For compatibility with broken pickles saved in old Python 3 versions
+    'UserDict': 'collections',
+    'UserList': 'collections',
+    'UserString': 'collections',
+    'whichdb': 'dbm',
+    'StringIO':  'io',
+    'cStringIO': 'io',
+})
+    
+    '''Send the contents of a directory as a MIME message.'''
+    
+        print('-' * 20)
+    
+    def calculate(func, args):
+    result = func(*args)
+    return '%s says that %s%s = %s' % (
+        multiprocessing.current_process().name,
+        func.__name__, args, result
         )
     
-    
-class Migration(SchemaMigration):
-    def forwards(self, orm):
-        # Adding model 'VersionDSymFile'
-        db.create_table(
-            'sentry_versiondsymfile', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'dsym_file', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.ProjectDSymFile'], null=True
-                    )
-                ), (
-                    'dsym_app', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.DSymApp']
-                    )
-                ), ('version', self.gf('django.db.models.fields.CharField')(max_length=32)),
-                ('build', self.gf('django.db.models.fields.CharField')(max_length=32, null=True)), (
-                    'date_added',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['VersionDSymFile'])
-    
-            # Deleting field 'ReleaseFile.dist'
-        db.delete_column('sentry_releasefile', 'dist_id')
+    cur.close()
+con.close()
