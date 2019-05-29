ec458acd378389f4a54a1a8c680a2ff4b22a440e
SUNsung <weatherius@gmail.com>
2019-05-29 06:51:38 +0300

    Rewrite [58o7Yz.9DS0U58rstzGQ1.PDQ58HlkaOFyGnY587Gj.py] File in: 2019-05-29 06:51:37

--- pyton/58o7Yz.9DS0U58rstzGQ1.PDQ58HlkaOFyGnY587Gj.py
+++ pyton/58o7Yz.9DS0U58rstzGQ1.PDQ58HlkaOFyGnY587Gj.py
@@ -1,42 +1,32 @@
 
         
-                # Adding unique constraint on 'EnvironmentProject', fields ['project', 'environment']
-        db.create_unique('sentry_environmentproject', ['project_id', 'environment_id'])
+            def test_duplicate_function_equality(self):
+        # Body of `duplicate' is the exact same as self.b
+        def duplicate():
+            'my docstring'
+            return 3
+        self.assertNotEqual(self.b, duplicate)
     
-            # Changing field 'ReleaseEnvironment.project_id'
-        if is_postgres():
-            db.execute(
-                'ALTER TABLE sentry_environmentrelease ALTER COLUMN project_id DROP NOT NULL'
-            )
-        else:
-            db.alter_column(
-                'sentry_environmentrelease',
-                'project_id',
-                self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)
-            )
+        def testFileno(self):
+        self.createTempFile()
+        with open(self.filename, 'rb') as rawf:
+            bz2f = BZ2File(rawf)
+            try:
+                self.assertEqual(bz2f.fileno(), rawf.fileno())
+            finally:
+                bz2f.close()
+        self.assertRaises(ValueError, bz2f.fileno)
     
-            # Adding model 'ApiAuthorization'
-        db.create_table(
-            'sentry_apiauthorization', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'application', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.ApiApplication'], null=True
-                    )
-                ), (
-                    'user', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.User']
-                    )
-                ), ('scopes', self.gf('django.db.models.fields.BigIntegerField')(default=None)), (
-                    'date_added',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['ApiAuthorization'])
+    from email.policy import default
     
-            # Removing unique constraint on 'VersionDSymFile', fields ['dsym_file', 'version', 'build']
-        db.delete_unique('sentry_versiondsymfile', ['dsym_file_id', 'version', 'build'])
+    def handlePoints(points):
+    print('<ul>')
+    for point in points:
+        handlePoint(point)
+    print('</ul>')
+    
+    # Function to return the operator module
+def get_operator_module():
+    return operator
+    
+    DB_FILE = 'mydb'
