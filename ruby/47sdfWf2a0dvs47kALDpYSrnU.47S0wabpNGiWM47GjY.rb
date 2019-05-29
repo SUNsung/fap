81eb983f88c3a01d169f72d45e5c398cb920b02f
SUNsung <weatherius@gmail.com>
2019-05-29 17:48:45 +0300

    Rewrite [47sdfWf2a0dvs47kALDpYSrnU.47S0wabpNGiWM47GjY.rb] File in: 2019-05-29 17:48:45

--- ruby/47sdfWf2a0dvs47kALDpYSrnU.47S0wabpNGiWM47GjY.rb
+++ ruby/47sdfWf2a0dvs47kALDpYSrnU.47S0wabpNGiWM47GjY.rb
@@ -1,320 +1,40 @@
 
         
-                begin
-          yield
-        rescue Octokit::TooManyRequests
-          raise_or_wait_for_rate_limit
+              def index
+        render template: ActionView::Template::Text.new('Hello nil!')
+      end
+    end
     
-            def importer_class
-          IssueAndLabelLinksImporter
-        end
+      def status_finder
+    StatusFinder.new(params[:url])
+  end
     
-            def sidekiq_worker_class
-          ImportLfsObjectWorker
-        end
+        render_empty
+  end
     
-            def sidekiq_worker_class
-          ImportNoteWorker
-        end
+      def export_data
+    raise 'Override in controller'
+  end
     
+      def preferred_locale
+    http_accept_language.preferred_language_from(available_locales)
+  end
     
-    {            # These fields are not displayed for LegacyDiffNote notes, so it
-            # doesn't really matter what we set them to.
-            a_mode: '100644',
-            b_mode: '100644',
-            new_file: false
-          }
+        %w[iOS macOS].each do |platform|
+        abstract_target '#{platform} Pods' do
+            project '#{platform} Modules.xcodeproj'
+    
+            def execute_repl_command(repl_command)
+          unless repl_command == '\n'
+            repl_commands = repl_command.split
+            subcommand = repl_commands.shift.capitalize
+            arguments = repl_commands
+            subcommand_class = Pod::Command::IPC.const_get(subcommand)
+            subcommand_class.new(CLAide::ARGV.new(arguments)).run
+            signal_end_of_output
+          end
         end
       end
     end
   end
 end
-
-    
-            def issuable_type
-          pull_request? ? 'MergeRequest' : 'Issue'
-        end
-      end
-    end
-  end
-end
-
-    
-              if !matches || !matches[:type]
-            raise(
-              ArgumentError,
-              'The note URL #{note.html_url.inspect} is not supported'
-            )
-          end
-    
-            # Builds a user from a GitHub API response.
-        #
-        # user - An instance of `Sawyer::Resource` containing the user details.
-        def self.from_api_response(user)
-          new(id: user.id, login: user.login)
-        end
-    
-            if controller.content_type == 'text/html'
-          action
-        else
-          '#{action} (#{controller.content_type})'
-        end
-      end
-    
-        def as_json
-      { entries: entries_as_json, types: types_as_json }
-    end
-    
-        def at_xpath(*args)
-      doc.at_xpath(*args)
-    end
-    
-            css('.filetree').each do |node|
-          node.content = node.css('.file').map(&:inner_html).join('\n')
-          node.name = 'pre'
-          node.remove_attribute('class')
-        end
-    
-          def get_type
-        if slug.start_with?('guide/')
-          'Guide'
-        elsif slug.start_with?('cookbook/')
-          'Cookbook'
-        elsif slug == 'glossary'
-          'Guide'
-        else
-          type = at_css('.nav-title.is-selected').content.strip
-          type.remove! ' Reference'
-          type << ': #{mod}' if mod
-          type
-        end
-      end
-    
-          respond_to_on_destroy
-    end
-  end
-    
-        def email_changed(record, opts={})
-      devise_mail(record, :email_changed, opts)
-    end
-    
-          def devise_mapping
-        @devise_mapping ||= Devise.mappings[scope_name]
-      end
-    
-        def default_used_helpers(options)
-      singularizer = lambda { |s| s.to_s.singularize.to_sym }
-    
-            if Devise.activerecord51?
-          def clear_reset_password_token?
-            encrypted_password_changed = respond_to?(:will_save_change_to_encrypted_password?) && will_save_change_to_encrypted_password?
-            authentication_keys_changed = self.class.authentication_keys.any? do |attribute|
-              respond_to?('will_save_change_to_#{attribute}?') && send('will_save_change_to_#{attribute}?')
-            end
-    
-      #
-  # Gets cookies from the Set-Cookie header in a format to be used
-  # in the 'cookie' send_request field
-  #
-  def get_cookies
-    cookies = ''
-    if (self.headers.include?('Set-Cookie'))
-      set_cookies = self.headers['Set-Cookie']
-      key_vals = set_cookies.scan(/\s?([^, ;]+?)=([^, ;]*?)[;,]/)
-      key_vals.each do |k, v|
-        # Dont downcase actual cookie name as may be case sensitive
-        name = k.downcase
-        next if name == 'path'
-        next if name == 'expires'
-        next if name == 'domain'
-        next if name == 'max-age'
-        cookies << '#{k}=#{v}; '
-      end
-    end
-    
-      #
-  # Dispatches the supplied request for a given connection.
-  #
-  def dispatch_request(cli, request)
-    # Is the client requesting keep-alive?
-    if ((request['Connection']) and
-       (request['Connection'].downcase == 'Keep-Alive'.downcase))
-      cli.keepalive = true
-    end
-    
-      def wait_for(*stypes)
-    begin
-      ::Timeout.timeout( IAX_DEFAULT_TIMEOUT ) do
-        while (res = self.queue.pop )
-          if stypes.include?(res[1])
-            return res
-          end
-        end
-      end
-    rescue ::Timeout::Error
-      return nil
-    end
-  end
-    
-    =begin
-      +------+-----------+-----------------------------------------+
-      | Hex  | Name      | Description                             |
-      +------+-----------+-----------------------------------------+
-      | 0x01 | NEW       | Initiate a new call                     |
-      |      |           |                                         |
-      | 0x02 | PING      | Ping request                            |
-      |      |           |                                         |
-      | 0x03 | PONG      | Ping or poke reply                      |
-      |      |           |                                         |
-      | 0x04 | ACK       | Explicit acknowledgment                 |
-      |      |           |                                         |
-      | 0x05 | HANGUP    | Initiate call tear-down                 |
-      |      |           |                                         |
-      | 0x06 | REJECT    | Reject a call                           |
-      |      |           |                                         |
-      | 0x07 | ACCEPT    | Accept a call                           |
-      |      |           |                                         |
-      | 0x08 | AUTHREQ   | Authentication request                  |
-      |      |           |                                         |
-      | 0x09 | AUTHREP   | Authentication reply                    |
-      |      |           |                                         |
-      | 0x0a | INVAL     | Invalid message                         |
-      |      |           |                                         |
-      | 0x0b | LAGRQ     | Lag request                             |
-      |      |           |                                         |
-      | 0x0c | LAGRP     | Lag reply                               |
-      |      |           |                                         |
-      | 0x0d | REGREQ    | Registration request                    |
-      |      |           |                                         |
-      | 0x0e | REGAUTH   | Registration authentication             |
-      |      |           |                                         |
-      | 0x0f | REGACK    | Registration acknowledgement            |
-      |      |           |                                         |
-      | 0x10 | REGREJ    | Registration reject                     |
-      |      |           |                                         |
-      | 0x11 | REGREL    | Registration release                    |
-      |      |           |                                         |
-      | 0x12 | VNAK      | Video/Voice retransmit request          |
-      |      |           |                                         |
-      | 0x13 | DPREQ     | Dialplan request                        |
-      |      |           |                                         |
-      | 0x14 | DPREP     | Dialplan reply                          |
-      |      |           |                                         |
-      | 0x15 | DIAL      | Dial                                    |
-      |      |           |                                         |
-      | 0x16 | TXREQ     | Transfer request                        |
-      |      |           |                                         |
-      | 0x17 | TXCNT     | Transfer connect                        |
-      |      |           |                                         |
-      | 0x18 | TXACC     | Transfer accept                         |
-      |      |           |                                         |
-      | 0x19 | TXREADY   | Transfer ready                          |
-      |      |           |                                         |
-      | 0x1a | TXREL     | Transfer release                        |
-      |      |           |                                         |
-      | 0x1b | TXREJ     | Transfer reject                         |
-      |      |           |                                         |
-      | 0x1c | QUELCH    | Halt audio/video [media] transmission   |
-      |      |           |                                         |
-      | 0x1d | UNQUELCH  | Resume audio/video [media] transmission |
-      |      |           |                                         |
-      | 0x1e | POKE      | Poke request                            |
-      |      |           |                                         |
-      | 0x1f | Reserved  | Reserved for future use                 |
-      |      |           |                                         |
-      | 0x20 | MWI       | Message waiting indication              |
-      |      |           |                                         |
-      | 0x21 | UNSUPPORT | Unsupported message                     |
-      |      |           |                                         |
-      | 0x22 | TRANSFER  | Remote transfer request                 |
-      |      |           |                                         |
-      | 0x23 | Reserved  | Reserved for future use                 |
-      |      |           |                                         |
-      | 0x24 | Reserved  | Reserved for future use                 |
-      |      |           |                                         |
-      | 0x25 | Reserved  | Reserved for future use                 |
-      +------+-----------+-----------------------------------------+
-=end
-    
-      #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
-  #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
-    
-    
-  # open rmcpplus_request with cipherzero
-  def self.create_ipmi_session_open_cipher_zero_request(console_session_id)
-    head = [
-      0x06, 0x00, 0xff, 0x07,   # RMCP Header
-      0x06,                     # RMCP+ Authentication Type
-      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
-      0x00, 0x00, 0x00, 0x00,   # Session ID
-      0x00, 0x00, 0x00, 0x00    # Sequence Number
-    ].pack('C*')
-    
-              # Encodes the end_time field
-          #
-          # @return [String]
-          def encode_end_time
-            [end_time].pack('N')
-          end
-    
-              # Rex::Proto::Kerberos::Model::LastRequest encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'LastRequest encoding not supported'
-          end
-    
-          File.open('bower.json', 'w') do |f|
-        f.puts JSON.pretty_generate(spec)
-      end
-    end
-  end
-end
-
-    
-        def byte_to_str_pos(pos)
-      @s.string.byteslice(0, pos).length
-    end
-    
-      def test_image_helper
-    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
-  end
-    
-          # This double assignment is to prevent an 'unused variable' warning on
-      # Ruby 1.9.3.  Yes, it is dumb, but I don't like Ruby yelling at me.
-      path = path = (req.script_name + req.path_info).squeeze('/')
-    
-      it 'accepts post requests with masked X-CSRF-Token header' do
-    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => masked_token)
-    expect(last_response).to be_ok
-  end
-    
-    
-    {      # Checks whether the `hash` literal is delimited by curly braces.
-      #
-      # @return [Boolean] whether the `hash` literal is enclosed in braces
-      def braces?
-        loc.end && loc.end.is?('}')
-      end
-    end
-  end
-end
-
-    
-          def self.parse(identifier)
-        parts = identifier.split('/', 2)
-    
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
-  end
