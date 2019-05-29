4835f5dc47622552ed0104524f9d3adb7d0cca70
SUNsung <weatherius@gmail.com>
2019-05-29 05:42:55 +0300

    Rewrite [25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py] File in: 2019-05-29 05:42:54

--- pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
+++ pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
@@ -1,163 +1,28 @@
 
         
-        
-def load_all_labels(records):
-  '''Reads TensorFlow examples from a RecordReader and returns only the labels.
+                pos_delta = action[:, :3]
+        quat_delta = action[:, 3:]
     
-    CHECKPOINT_PB_LOAD_NAME = 'checkpoint'
-CHECKPOINT_NAME = 'lfads_vae'
-CSV_LOG = 'fitlog'
-OUTPUT_FILENAME_STEM = ''
-DEVICE = 'gpu:0' # 'cpu:0', or other gpus, e.g. 'gpu:1'
-MAX_CKPT_TO_KEEP = 5
-MAX_CKPT_TO_KEEP_LVE = 5
-PS_NEXAMPLES_TO_PROCESS = MAX_INT # if larger than number of examples, process all
-EXT_INPUT_DIM = 0
-IC_DIM = 64
-FACTORS_DIM = 50
-IC_ENC_DIM = 128
-GEN_DIM = 200
-GEN_CELL_INPUT_WEIGHT_SCALE = 1.0
-GEN_CELL_REC_WEIGHT_SCALE = 1.0
-CELL_WEIGHT_SCALE = 1.0
-BATCH_SIZE = 128
-LEARNING_RATE_INIT = 0.01
-LEARNING_RATE_DECAY_FACTOR = 0.95
-LEARNING_RATE_STOP = 0.00001
-LEARNING_RATE_N_TO_COMPARE = 6
-INJECT_EXT_INPUT_TO_GEN = False
-DO_TRAIN_IO_ONLY = False
-DO_TRAIN_ENCODER_ONLY = False
-DO_RESET_LEARNING_RATE = False
-FEEDBACK_FACTORS_OR_RATES = 'factors'
-DO_TRAIN_READIN = True
-    
-    
-def nparray_and_transpose(data_a_b_c):
-  '''Convert the list of items in data to a numpy array, and transpose it
-  Args:
-    data: data_asbsc: a nested, nested list of length a, with sublist length
-      b, with sublist length c.
-  Returns:
-    a numpy 3-tensor with dimensions a x c x b
-'''
-  data_axbxc = np.array([datum_b_c for datum_b_c in data_a_b_c])
-  data_axcxb = np.transpose(data_axbxc, axes=[0,2,1])
-  return data_axcxb
-    
-    import os
-import pickle as pkl
-import numpy as np
-import tensorflow as tf
-import utils
-    
-      def _convert_word_to_char_ids(self, word):
-    code = np.zeros([self.max_word_length], dtype=np.int32)
-    code[:] = ord(self.pad_char)
-    
-    
-def rnn_zaremba(hparams, model):
-  '''Returns the PTB Variable name to MaskGAN Variable dictionary mapping.  This
-  is a highly restrictive function just for testing.  This will need to be
-  generalized.
-    
-          done: A boolean vector to indicate which sentences has reached a
-      `end_of_sequence_id`. This is used for early stopping by the
-      `dynamic_rnn_decoder`. When `time>=maximum_length` a boolean vector with
-      all elements as `true` is returned.
-    
-          # Prediction is linear output for Discriminator.
-      predictions = tf.contrib.layers.linear(outputs, 1, scope=vs)
-    
-    from time import time
-    
-            if not in_exercise_region or linestrip.startswith('#'):
-            output_file.write(line)
-    
-    plt.show()
+    def test_text_envs():
+    env = gym.make('FrozenLake-v0')
+    video = VideoRecorder(env)
+    try:
+        env.reset()
+        video.capture_frame()
+        video.close()
+    finally:
+        os.remove(video.path)
 
     
-                plt.subplots_adjust(bottom=0, top=.89, wspace=0,
-                                left=0, right=1)
-            plt.suptitle('n_cluster=%i, connectivity=%r' %
-                         (n_clusters, connectivity is not None), size=17)
-    
-    plt.subplot(3, 4, 10)
-plt.imshow(np.reshape(agglo.labels_, images[0].shape),
-           interpolation='nearest', cmap=plt.cm.nipy_spectral)
-plt.xticks(())
-plt.yticks(())
-plt.title('Labels')
-plt.show()
-
-    
-    for linkage in ('ward', 'average', 'complete', 'single'):
-    clustering = AgglomerativeClustering(linkage=linkage, n_clusters=10)
-    t0 = time()
-    clustering.fit(X_red)
-    print('%s :\t%.2fs' % (linkage, time() - t0))
-    
-    # A regex that matches standard linkcheck output lines
-line_re = re.compile(u'(.*)\:\d+\:\s\[(.*)\]\s(?:(.*)\sto\s(.*)|(.*))')
-    
-    
-class QPSSpider(Spider):
-    
-        def adjust_request_args(self, args):
-        args['url'] = self.args[0]
-        return args
-    
-            self._signer = None
-        if is_botocore():
-            import botocore.auth
-            import botocore.credentials
-            kw.pop('anon', None)
-            if kw:
-                raise TypeError('Unexpected keyword arguments: %s' % kw)
-            if not self.anon:
-                SignerCls = botocore.auth.AUTH_TYPE_MAPS['s3']
-                self._signer = SignerCls(botocore.credentials.Credentials(
-                    aws_access_key_id, aws_secret_access_key))
-        else:
-            _S3Connection = _get_boto_connection()
-            try:
-                self.conn = _S3Connection(
-                    aws_access_key_id, aws_secret_access_key, **kw)
-            except Exception as ex:
-                raise NotConfigured(str(ex))
-    
-        def handleEndHeaders(self):
-        self.factory.gotHeaders(self.headers)
-    
-        def encodePriority(self, facility, priority):
-        '''
-        Encode the facility and priority. You can pass in strings or
-        integers - if strings are passed, the facility_names and
-        priority_names mapping dictionaries are used to convert them to
-        integers.
-        '''
-        if isinstance(facility, str):
-            facility = self.facility_names[facility]
-        if isinstance(priority, str):
-            priority = self.priority_names[priority]
-        return (facility << 3) | priority
-    
-        def compare_files(self, file1, file2):
-        with open(file1) as fp:
-            lines1 = fp.readlines()
-        with open(file2) as fp:
-            lines2 = fp.readlines()
-        self.assertEqual(lines1, lines2)
-    
-    
-# Are two filenames really pointing to the same file?
-def samefile(f1, f2):
-    '''Test whether two pathnames reference the same actual file'''
-    s1 = os.stat(f1)
-    s2 = os.stat(f2)
-    return samestat(s1, s2)
-    
-            # Add even more data to it, requiring resize
-        out.append(bzd.decompress(self.DATA[200:300], 5))
-    
-    ##
+    # All concrete subclasses of AlgorithmicEnv
+ALL_ENVS = [
+    alg.copy_.CopyEnv, 
+    alg.duplicated_input.DuplicatedInputEnv,
+    alg.repeat_copy.RepeatCopyEnv,
+    alg.reverse.ReverseEnv,
+    alg.reversed_addition.ReversedAdditionEnv,
+]
+ALL_TAPE_ENVS = [env for env in ALL_ENVS 
+    if issubclass(env, alg.algorithmic_env.TapeAlgorithmicEnv)]
+ALL_GRID_ENVS = [env for env in ALL_ENVS 
+    if issubclass(env, alg.algorithmic_env.GridAlgorithmicEnv)]
