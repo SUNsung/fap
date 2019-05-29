254c97b1c21a21798ebc877e5f1f3c0fd4e040c8
SUNsung <weatherius@gmail.com>
2019-05-29 03:40:47 +0300

    Rewrite [56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb] File in: 2019-05-29 03:40:46

--- ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
+++ ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
@@ -1,62 +1,52 @@
 
         
-            def str_to_byte_pos(pos)
-      @s.string.slice(0, pos).bytesize
+            context 'without name' do
+      it 'displays error message' do
+        expect { noname_project.name }.to_not raise_error
+      end
+    end
+    
+          it 'returns false' do
+        expect(described_class.installed?).to be_falsey
+      end
     end
   end
-end
     
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
-    
-    desc 'Default deploy task'
-task :deploy do
-  # Check if preview posts exist, which should not be published
-  if File.exists?('.preview-mode')
-    puts '## Found posts in preview mode, regenerating files ...'
-    File.delete('.preview-mode')
-    Rake::Task[:generate].execute
-  end
-    
-    Liquid::Template.register_tag('img', Jekyll::ImageTag)
-
-    
-        def render(context)
-      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
-      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
-      file = code_path + @file
-    
-          unless file.file?
-        return 'File #{file} could not be found'
-      end
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
+        def implode
+      if yes?('Are you sure you want to delete all tmuxinator configs?', :red)
+        Tmuxinator::Config.directories.each do |directory|
+          FileUtils.remove_dir(directory)
         end
-        video += '</video>'
-      else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+        say 'Deleted all tmuxinator projects.'
       end
     end
     
-        def pre
-      _pre = yaml['pre']
-    
-            Tmuxinator::Project.load(project_file, options).validate!
+        context 'deprecations' do
+      before do
+        allow($stdin).to receive_messages(getc: 'y')
       end
     
-          it 'returns true' do
-        expect(described_class.installed?).to be_truthy
-      end
+        def send_pane_command(cmd, window_index, _pane_index)
+      send_keys(cmd, window_index)
     end
+    
+      s.files         = Dir['lib/**/*', 'spec/**/*', 'bin/*', 'completion/*']
+  s.executables   = s.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  s.test_files    = s.files.grep(%r{^(test|spec|features)/})
+  s.require_paths = ['lib']
+    
+              it 'doesn't raise an integrity error' do
+            is_expected.to_not raise_error
+          end
+        end
+    
+          def check_content_type_blacklist!(new_file)
+        content_type = new_file.content_type
+        if content_type_blacklist && blacklisted_content_type?(content_type)
+          raise CarrierWave::IntegrityError, I18n.translate(:'errors.messages.content_type_blacklist_error', content_type: content_type)
+        end
+      end
+    
+          included do
+        before :cache, :check_extension_whitelist!
+      end
