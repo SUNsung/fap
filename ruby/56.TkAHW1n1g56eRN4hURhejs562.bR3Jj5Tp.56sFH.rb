afd0fca02da71c98e4bbd9843d58568897e0668c
SUNsung <weatherius@gmail.com>
2019-05-31 01:43:06 +0300

    Rewrite [56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb] File in: 2019-05-31 01:43:06

--- ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
+++ ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
@@ -1,115 +1,36 @@
 
         
-              class MergeRequest < ActiveRecord::Base
-        self.table_name = 'merge_requests'
+          describe '.seed' do
+    it 'imports a set of agents to get the user going when they are first created' do
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+    end
     
-            def collection_options
-          { state: 'all', sort: 'created', direction: 'asc' }
-        end
-      end
+      after :each do
+    @scheduler.shutdown(:wait)
+    
+      describe 'path request must exist' do
+    it 'should check that validation added if path does not exist' do
+      opts = @opts.tap { |o| o.delete('path') }
+      @checker = Agents::AftershipAgent.new(:name => 'tectonic', :options => opts)
+      @checker.user = users(:bob)
+      expect(@checker.save).to eq false
+      expect(@checker.errors.full_messages.first).to eq('You need to specify a path request')
     end
   end
-end
-
     
-            def sidekiq_worker_class
-          ImportNoteWorker
-        end
+        it 'accepts jsfiddle link' do
+      liquid = generate_new_liquid(jsfiddle_link)
+      rendered_jsfiddle_iframe = liquid.render
+      Approvals.verify(rendered_jsfiddle_iframe, name: 'jsfiddle_liquid_tag', format: :html)
+    end
     
-    lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'capistrano/version'
-    
-    Then(/^the failure task will not run$/) do
-  failed = TestApp.shared_path.join('failed')
-  expect { run_vagrant_command(test_file_exists(failed)) }
-    .to raise_error(VagrantHelpers::VagrantSSHCommandError)
-end
-    
-      def symlinked?(symlink_path, target_path)
-    '[ #{symlink_path} -ef #{target_path} ]'
-  end
-    
-      def run_vagrant_command(command)
-    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
-    return [stdout, stderr] if status.success?
-    raise VagrantSSHCommandError, status
-  end
-end
-    
-        def any?(key)
-      value = fetch(key)
-      if value && value.respond_to?(:any?)
-        begin
-          return value.any?
-        rescue ArgumentError # rubocop:disable Lint/HandleExceptions
-          # Gracefully ignore values whose `any?` method doesn't accept 0 args
-        end
+          it 'mutes all of the original commenter's comments in a single thread' do
+        user_ids_of_muted_comments = Comment.where(receive_notifications: false).pluck(:user_id)
+        expect(user_ids_of_muted_comments.uniq).to eq [original_commenter.id]
       end
     
-          def extract_options(array)
-        array.last.is_a?(::Hash) ? array.pop : {}
-      end
-    end
+      def update
+    @broadcast = Broadcast.find(params[:id])
+    @broadcast.update(broadcast_params)
+    redirect_to '/internal/broadcasts'
   end
-end
-
-    
-    module Capistrano
-  class Configuration
-    # Holds the variables assigned at Capistrano runtime via `set` and retrieved
-    # with `fetch`. Does internal bookkeeping to help identify user mistakes
-    # like spelling errors or unused variables that may lead to unexpected
-    # behavior.
-    class Variables
-      CAPISTRANO_LOCATION = File.expand_path('../..', __FILE__).freeze
-      IGNORED_LOCATIONS = [
-        '#{CAPISTRANO_LOCATION}/configuration/variables.rb:',
-        '#{CAPISTRANO_LOCATION}/configuration.rb:',
-        '#{CAPISTRANO_LOCATION}/dsl/env.rb:',
-        '/dsl.rb:',
-        '/forwardable.rb:'
-      ].freeze
-      private_constant :CAPISTRANO_LOCATION, :IGNORED_LOCATIONS
-    
-      def safe_params
-    params.require(:address_endpoint).permit(:address)
-  end
-    
-      included do
-    helper_method :organization
-    before_action :add_organization_to_page_title
-  end
-    
-    end
-
-    
-      def check
-    if @domain.check_dns(:manual)
-      redirect_to_with_json [organization, @server, :domains], :notice => 'Your DNS records for #{@domain.name} look good!'
-    else
-      redirect_to_with_json [:setup, organization, @server, @domain], :alert => 'There seems to be something wrong with your DNS records. Check below for information.'
-    end
-  end
-    
-      def get_messages(scope)
-    if scope == 'held'
-      options = {:where => {:held => 1}}
-    else
-      options = {:where => {:scope => scope, :spam => false}, :order => :timestamp, :direction => 'desc'}
-    
-      def destroy
-    if @user == current_user
-      redirect_to_with_json [organization, :users], :alert => 'You cannot revoke your own access.'
-      return
-    end
-    
-          organization_domains = server.organization.domains.verified.order(:name)
-      unless organization_domains.empty?
-        s << '<optgroup label='Organization Domains'>'
-        for domain in organization_domains
-          selected = domain == selected_domain ? 'selected='selected'' : ''
-          s << '<option value='#{domain.id}' #{selected}>#{domain.name}</option>'
-        end
-        s << '</optgroup>'
-      end
