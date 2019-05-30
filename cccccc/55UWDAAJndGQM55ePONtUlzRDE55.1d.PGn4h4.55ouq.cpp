3c57a4495a3cb61df3cd8828f668c8e9cef8122a
SUNsung <weatherius@gmail.com>
2019-05-30 05:10:40 +0300

    Rewrite [55UWDAAJndGQM55ePONtUlzRDE55.1d.PGn4h4.55ouq.cpp] File in: 2019-05-30 05:10:39

--- cccccc/55UWDAAJndGQM55ePONtUlzRDE55.1d.PGn4h4.55ouq.cpp
+++ cccccc/55UWDAAJndGQM55ePONtUlzRDE55.1d.PGn4h4.55ouq.cpp
@@ -1,153 +1,96 @@
 
         
-          scoped_ptr<base::Value> value_option(
-      converter->FromV8Value(options, isolate->GetCurrentContext()));
-  if (!value_option.get() ||
-      !value_option->IsType(base::Value::TYPE_DICTIONARY))
-    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-        'Unable to convert 'option' passed to AllocateObject')));
+        **Code**
     
-       void Call(const std::string& method,
-                    const base::ListValue& arguments) override;
-   void CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result) override;
-    
-    bool MenuDelegate::IsCommandIdChecked(int command_id) const {
-  if (command_id < 0)
-    return false;
+    template <>
+void GluOp<float, CPUContext>::ComputeGlu(
+    const int M,
+    const int split_dim,
+    const int N,
+    const float* Xdata,
+    float* Ydata) {
+  const int xStride = 2 * split_dim * N;
+  const int yStride = split_dim * N;
+  for (int i = 0; i < M; ++i) {
+    const int idx = i * xStride;
+    const int idy = i * yStride;
+    for (int j = 0; j < split_dim; ++j) {
+      const int jN = j * N;
+      const int jdx1 = idx + jN;
+      const int jdx2 = idx + (j + split_dim) * N;
+      const int jdy = idy + jN;
+      for (int k = 0; k < N; ++k) {
+        const float x1 = Xdata[jdx1 + k];
+        const float x2 = Xdata[jdx2 + k];
+        Ydata[jdy + k] = x1 * sigmoid(x2);
+      }
     }
-    
-    MenuItem::MenuItem(int id,
-                   const base::WeakPtr<ObjectManager>& object_manager,
-                   const base::DictionaryValue& option,
-                   const std::string& extension_id)
-  : Base(id, object_manager, option, extension_id) {
-  Create(option);
+  }
 }
     
-    void MenuItem::Create(const base::DictionaryValue& option) {
-  std::string type;
-  option.GetString('type', &type);
-  submenu_ = NULL;
-  gtk_accel_group = NULL;
-    }
-    
-    
-    {} // namespace extensions
-#endif
+    #endif
 
     
+    // Main entry point for Paragraph Detection Algorithm.
+//
+// Given a set of equally spaced textlines (described by row_infos),
+// Split them into paragraphs.  See http://goto/paragraphstalk
+//
+// Output:
+//   row_owners - one pointer for each row, to the paragraph it belongs to.
+//   paragraphs - this is the actual list of PARA objects.
+//   models - the list of paragraph models referenced by the PARA objects.
+//            caller is responsible for deleting the models.
+void DetectParagraphs(int debug_level,
+                      GenericVector<RowInfo> *row_infos,
+                      GenericVector<PARA *> *row_owners,
+                      PARA_LIST *paragraphs,
+                      GenericVector<ParagraphModel *> *models);
     
-       case URX_CARET:                    //  ^, test for start of line
-            if (fp->fInputIdx != fAnchorStart) {
-                fp = (REStackFrame *)fStack->popFrame(fFrameSize);
-            }
-            break;
+      // Backwards compatible constrained fit with a supplied gradient.
+  // Deprecated. Use ConstrainedFit(const FCOORD& direction) where possible
+  // to avoid potential difficulties with infinite gradients.
+  double ConstrainedFit(double m, float* c);
     
+      // Sets up the DENORM to execute a non-linear transformation based on
+  // preserving an even distribution of stroke edges. The transformation
+  // operates only within the given box, scaling input coords within the box
+  // non-linearly to a box of target_width by target_height, with all other
+  // coords being clipped to the box edge. As with SetupNormalization above,
+  // final_xshift and final_yshift are applied after scaling, and the bottom-
+  // left of box is used as a pre-scaling origin.
+  // x_coords is a collection of the x-coords of vertical edges for each
+  // y-coord starting at box.bottom().
+  // y_coords is a collection of the y-coords of horizontal edges for each
+  // x-coord starting at box.left().
+  // Eg x_coords[0] is a collection of the x-coords of edges at y=bottom.
+  // Eg x_coords[1] is a collection of the x-coords of edges at y=bottom + 1.
+  // The second-level vectors must all be sorted in ascending order.
+  void SetupNonLinear(const DENORM* predecessor, const TBOX& box,
+                      float target_width, float target_height,
+                      float final_xshift, float final_yshift,
+                      const GenericVector<GenericVector<int> >& x_coords,
+                      const GenericVector<GenericVector<int> >& y_coords);
     
-ScriptSet &ScriptSet::set(UScriptCode script, UErrorCode &status) {
-    if (U_FAILURE(status)) {
-        return *this;
-    }
-    if (script < 0 || script >= (int32_t)sizeof(bits) * 8) {
-        status = U_ILLEGAL_ARGUMENT_ERROR;
-        return *this;
-    }
-    uint32_t index = script / 32;
-    uint32_t bit   = 1 << (script & 31);
-    bits[index] |= bit;
-    return *this;
-}
-    
-    
-    {
-    {        if (matchlength > 0) {
-            // if matchlength is 0 we are at the start of the iteration
-            if (m_search_->isOverlap) {
-                offset ++;
-            }
-            else {
-                offset += matchlength;
-            }
-        }
-        return handleNext(offset, status);
-    }
-    return USEARCH_DONE;
-}
-    
-    #include 'unicode/format.h'
-#include 'unicode/locid.h'
-#include 'unicode/parseerr.h'
-#include 'unicode/utypes.h'
-#include 'uvector.h'
-#include 'hash.h'
-    
-    #include 'unicode/utypes.h'
-#include 'sharedobject.h'
-    
-    void StoreBuffer::MoveEntriesToRememberedSet(int index) {
-  if (!lazy_top_[index]) return;
-  DCHECK_GE(index, 0);
-  DCHECK_LT(index, kStoreBuffers);
-  Address last_inserted_addr = kNullAddress;
-  MemoryChunk* chunk = nullptr;
-    }
+    /*
+Method:
+Start at vertex with minimum y (pick maximum x one if there are two).  
+We aim our 'lastDir' vector at (1.0, 0)
+We look at the two rays going off from our start vertex, and follow whichever
+has the smallest angle (in -Pi -> Pi) wrt lastDir ('rightest' turn)
     
     
-    {
-    {}  // namespace internal
-}  // namespace v8
-
+    {	};
     
-      void FlushToGlobal(int task_id) {
-    PublishPushSegmentToGlobal(task_id);
-    PublishPopSegmentToGlobal(task_id);
-  }
+    typedef opus_val32 celt_sig;
+typedef opus_val16 celt_norm;
+typedef opus_val32 celt_ener;
     
-     private:
-  // Set the ModRM byte without an encoded 'reg' register. The
-  // register is encoded later as part of the emit_operand operation.
-  inline void set_modrm(int mod, Register rm) {
-    DCHECK_EQ(mod & -4, 0);
-    buf_[0] = mod << 6 | rm.code();
-    len_ = 1;
-  }
+       - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
     
-      // Replace the current (input) frame with the output frames.
-  Label outer_push_loop, inner_push_loop,
-      outer_loop_header, inner_loop_header;
-  // Outer loop state: eax = current FrameDescription**, edx = one
-  // past the last FrameDescription**.
-  __ mov(edx, Operand(eax, Deoptimizer::output_count_offset()));
-  __ mov(eax, Operand(eax, Deoptimizer::output_offset()));
-  __ lea(edx, Operand(eax, edx, times_system_pointer_size, 0));
-  __ jmp(&outer_loop_header);
-  __ bind(&outer_push_loop);
-  // Inner loop state: esi = current FrameDescription*, ecx = loop
-  // index.
-  __ mov(esi, Operand(eax, 0));
-  __ mov(ecx, Operand(esi, FrameDescription::frame_size_offset()));
-  __ jmp(&inner_loop_header);
-  __ bind(&inner_push_loop);
-  __ sub(ecx, Immediate(sizeof(uint32_t)));
-  __ push(Operand(esi, ecx, times_1, FrameDescription::frame_content_offset()));
-  __ bind(&inner_loop_header);
-  __ test(ecx, ecx);
-  __ j(not_zero, &inner_push_loop);
-  __ add(eax, Immediate(kSystemPointerSize));
-  __ bind(&outer_loop_header);
-  __ cmp(eax, edx);
-  __ j(below, &outer_push_loop);
+    #ifdef OPUS_ARM_INLINE_MEDIA
     
-      // Rest of parameters passed to JSEntry and JSConstructEntry.
-  static constexpr int kNewTargetArgOffset = +3 * kSystemPointerSize;
-  static constexpr int kFunctionArgOffset = +4 * kSystemPointerSize;
-  static constexpr int kReceiverArgOffset = +5 * kSystemPointerSize;
-  static constexpr int kArgcOffset = +6 * kSystemPointerSize;
-  static constexpr int kArgvOffset = +7 * kSystemPointerSize;
-    
-      // ---------------------------------------------------------------------------
-  // Runtime calls
-    
-      // StoreIC implementation.
+       - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
