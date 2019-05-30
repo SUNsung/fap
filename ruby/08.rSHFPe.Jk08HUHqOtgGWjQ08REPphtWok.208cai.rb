dcb3271019b148da23d6c479462a867842d9695e
SUNsung <weatherius@gmail.com>
2019-05-30 03:53:07 +0300

    Rewrite [08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb] File in: 2019-05-30 03:53:06

--- ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
+++ ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
@@ -1,26 +1,40 @@
 
         
-                def declaration_with_comment(node)
-          buffer = processed_source.buffer
-          begin_pos = get_source_range(node, comments_as_separators).begin_pos
-          end_line = buffer.line_for_position(node.loc.expression.end_pos)
-          end_pos = buffer.line_range(end_line).end_pos
-          Parser::Source::Range.new(buffer, begin_pos, end_pos)
-        end
+        DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
     
-            def on_send(node)
-          erb_new_with_non_keyword_arguments(node) do |arguments|
-            return if correct_arguments?(arguments)
     
-            def message_for_normal_block(variable, all_arguments)
-          if all_arguments.none?(&:referenced?) &&
-             !define_method_call?(variable)
-            if all_arguments.count > 1
-              'You can omit all the arguments if you don't care about them.'
-            else
-              'You can omit the argument if you don't care about it.'
-            end
-          else
-            message_for_underscore_prefix(variable)
-          end
-        end
+def pathutil_relative
+  Pathutil.new(DOC_PATH).relative_path_from(COL_PATH).to_s
+end
+    
+            # For a description of the protocol see
+        # http://feedback.livereload.com/knowledgebase/articles/86174-livereload-protocol
+        def reload(pages)
+          pages.each do |p|
+            json_message = JSON.dump(
+              :command => 'reload',
+              :path    => p.url,
+              :liveCSS => true
+            )
+    
+                    @env.machine_names.each do |machine_name|
+                  if machine_name =~ regex
+                    machines << get_machine.call(machine_name)
+                  end
+                end
+    
+    When /^(?:|I )fill in '([^']*)' with '([^']*)'$/ do |field, value|
+  fill_in(field, :with => value)
+end
+    
+      def migration_version
+    if Rails.version.start_with? '5'
+      '[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]'
+    end
+  end
+end
+
+    
+        def initialize(filepath)
+      @filepath = filepath
+    end
