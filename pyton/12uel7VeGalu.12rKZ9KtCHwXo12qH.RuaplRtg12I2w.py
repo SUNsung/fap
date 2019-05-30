472db61b2c2792337c173c132838773bbd837929
SUNsung <weatherius@gmail.com>
2019-05-30 19:32:29 +0300

    Rewrite [12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py] File in: 2019-05-30 19:32:28

--- pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
+++ pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
@@ -1,79 +1,151 @@
 
         
-            @property
-    def body(self):
-        body = self._orig.body
-        if isinstance(body, str):
-            # Happens with JSON/form request data parsed from the command line.
-            body = body.encode('utf8')
-        return body or b''
-
+                for project in RangeQuerySetWrapperWithProgressBar(orm.Project.objects.all()):
+            orm.Environment.objects.filter(
+                project_id=project.id, organization_id__isnull=True
+            ).update(organization_id=project.organization_id)
     
-    
-def test_max_redirects(httpbin):
-    r = http('--max-redirects=1', '--follow', httpbin.url + '/redirect/3',
-             error_exit_ok=True)
-    assert r.exit_status == ExitStatus.ERROR_TOO_MANY_REDIRECTS
-
-    
-        def _get_path(self):
-        return os.path.join(self.directory, self.name + '.json')
-    
-            if ext:
-            fn += ext
-    
-    '''
-requests._internal_utils
-~~~~~~~~~~~~~~
-    
-            Punctuation:               'bold #000000',   # class: 'p'
-    
-        # Check chardet for compatibility.
-    major, minor, patch = chardet_version.split('.')[:3]
-    major, minor, patch = int(major), int(minor), int(patch)
-    # chardet >= 3.0.2, < 3.1.0
-    assert major == 3
-    assert minor < 1
-    assert patch >= 2
-    
-        expires = None
-    if morsel['max-age']:
-        try:
-            expires = int(time.time() + int(morsel['max-age']))
-        except ValueError:
-            raise TypeError('max-age: %s must be integer' % morsel['max-age'])
-    elif morsel['expires']:
-        time_template = '%a, %d-%b-%Y %H:%M:%S GMT'
-        expires = calendar.timegm(
-            time.strptime(morsel['expires'], time_template)
+            # Changing field 'Environment.organization_id'
+        db.alter_column(
+            'sentry_environment',
+            'organization_id',
+            self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)
         )
-    return create_cookie(
-        comment=morsel['comment'],
-        comment_url=bool(morsel['comment']),
-        discard=False,
-        domain=morsel['domain'],
-        expires=expires,
-        name=morsel.key,
-        path=morsel['path'],
-        port=None,
-        rest={'HttpOnly': morsel['httponly']},
-        rfc2109=False,
-        secure=bool(morsel['secure']),
-        value=morsel.value,
-        version=morsel['version'] or 0,
-    )
     
-            with server as (host, port):
-            server_url = 'http://{}:{}'.format(host, port)
-            for _ in range(requests_to_handle):
-                r = requests.get(server_url)
-                assert r.status_code == 200
+            # Adding field 'ApiToken.scope_list'
+        db.add_column(
+            'sentry_apitoken',
+            'scope_list',
+            self.gf('sentry.db.models.fields.array.ArrayField')(
+                of=('django.db.models.fields.TextField', [], {})
+            ),
+            keep_default=False
+        )
     
-            for field, val in fields:
-            if isinstance(val, basestring) or not hasattr(val, '__iter__'):
-                val = [val]
-            for v in val:
-                if v is not None:
-                    # Don't call str() on bytestrings: in Py3 it all goes wrong.
-                    if not isinstance(v, bytes):
-                        v = str(v)
+            # Removing unique constraint on 'VersionDSymFile', fields ['dsym_file', 'version', 'build']
+        db.delete_unique('sentry_versiondsymfile', ['dsym_file_id', 'version', 'build'])
+    
+            for user in RangeQuerySetWrapperWithProgressBar(users):
+            orm.UserEmail.objects.get_or_create(user=user, email=user.email)
+    
+        def find_path(self, start, end, path=None):
+        path = path or []
+    
+        def test_data_change_shall_notify_all_observers_once(cls):
+        with patch.object(cls.dec_obs, 'update') as mock_dec_obs_update, patch.object(
+            cls.hex_obs, 'update'
+        ) as mock_hex_obs_update:
+            cls.sub.data = 10
+            cls.assertEqual(mock_dec_obs_update.call_count, 1)
+            cls.assertEqual(mock_hex_obs_update.call_count, 1)
+    
+        def test_provider_shall_update_affected_subscribers_with_published_subscription(cls):
+        pro = Provider()
+        pub = Publisher(pro)
+        sub1 = Subscriber('sub 1 name', pro)
+        sub1.subscribe('sub 1 msg 1')
+        sub1.subscribe('sub 1 msg 2')
+        sub2 = Subscriber('sub 2 name', pro)
+        sub2.subscribe('sub 2 msg 1')
+        sub2.subscribe('sub 2 msg 2')
+        with patch.object(sub1, 'run') as mock_subscriber1_run, patch.object(sub2, 'run') as mock_subscriber2_run:
+            pro.update()
+            cls.assertEqual(mock_subscriber1_run.call_count, 0)
+            cls.assertEqual(mock_subscriber2_run.call_count, 0)
+        pub.publish('sub 1 msg 1')
+        pub.publish('sub 1 msg 2')
+        pub.publish('sub 2 msg 1')
+        pub.publish('sub 2 msg 2')
+        with patch.object(sub1, 'run') as mock_subscriber1_run, patch.object(sub2, 'run') as mock_subscriber2_run:
+            pro.update()
+            expected_sub1_calls = [call('sub 1 msg 1'), call('sub 1 msg 2')]
+            mock_subscriber1_run.assert_has_calls(expected_sub1_calls)
+            expected_sub2_calls = [call('sub 2 msg 1'), call('sub 2 msg 2')]
+            mock_subscriber2_run.assert_has_calls(expected_sub2_calls)
+
+    
+    
+class TestDynamicExpanding(unittest.TestCase):
+    def setUp(self):
+        self.John = Person('John', 'Coder')
+    
+        def test_cat_adapter_shall_make_noise(self):
+        cat = Cat()
+        cat_adapter = Adapter(cat, make_noise=cat.meow)
+        noise = cat_adapter.make_noise()
+        expected_noise = 'meow!'
+        self.assertEqual(noise, expected_noise)
+    
+        def tearDown(cls):
+        ''' Function/test case scope teardown. '''
+        cls.output.close()
+        sys.stdout = cls.saved_stdout
+    
+    
+class StandbyStateTest(unittest.TestCase):
+    ''' Exemplary 2nd level state test class (here: Standby state). Add missing
+    state test classes... '''
+    
+        def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+        '''
+            Here the name of the class is used as key but it could be any class
+            parameter.
+        '''
+        cls.REGISTRY[new_cls.__name__] = new_cls
+        return new_cls
+    
+        def add_expert(self, expert):
+        self.experts.append(expert)
+    
+    
+def main():
+    '''
+    >>> move = Action('move')
+    >>> person = Person('Jack', move)
+    >>> person.do_action().amount('5m').stop()
+    Jack move 5m then stop
+    '''
+    
+            Content-Disposition is used as a header for a multipart body:
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition
+    '''
+    try:
+        parse_data = email.parser.BytesHeaderParser().parsebytes
+    except AttributeError:
+        # Fall back in case of Python 2.x
+        parse_data = email.parser.HeaderParser().parsestr
+    
+    
+def replay(api):
+    file_path = get_file_path(api)
+    if not file_path:
+        return
+    CURRENTLY_REPLAYING.append(True)
+    count = 0
+    try:
+        with open(file_path, 'r') as reader:
+            for line in reader:
+                if line.strip():
+                    count += 1
+                    command = json.loads(line)
+                    replay_command(command)
+    finally:
+        CURRENTLY_REPLAYING.pop(0)
+    if count:
+        LOGGER.info('Restored %s API calls from persistent file: %s' % (count, file_path))
+    
+        def test_api_gateway_lambda_proxy_integration_any_method(self):
+        self._test_api_gateway_lambda_proxy_integration_any_method(
+            self.TEST_LAMBDA_PROXY_BACKEND_ANY_METHOD,
+            self.API_PATH_LAMBDA_PROXY_BACKEND_ANY_METHOD)
+    
+        def test_is_query_allowable(self):
+        self.assertTrue(s3_listener.ProxyListenerS3.is_query_allowable('POST', 'uploadId'))
+        self.assertTrue(s3_listener.ProxyListenerS3.is_query_allowable('POST', ''))
+        self.assertTrue(s3_listener.ProxyListenerS3.is_query_allowable('PUT', ''))
+        self.assertFalse(s3_listener.ProxyListenerS3.is_query_allowable('POST', 'differentQueryString'))
+        # abort multipart upload is a delete with the same query string as a complete multipart upload
+        self.assertFalse(s3_listener.ProxyListenerS3.is_query_allowable('DELETE', 'uploadId'))
+        self.assertFalse(s3_listener.ProxyListenerS3.is_query_allowable('DELETE', 'differentQueryString'))
+        self.assertFalse(s3_listener.ProxyListenerS3.is_query_allowable('PUT', 'uploadId'))
