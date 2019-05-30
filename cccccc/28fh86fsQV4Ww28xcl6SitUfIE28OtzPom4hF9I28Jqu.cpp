6b06396cbc25f7afa922098251a1e993595efab1
SUNsung <weatherius@gmail.com>
2019-05-30 17:46:09 +0300

    Rewrite [28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp] File in: 2019-05-30 17:46:09

--- cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
+++ cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
@@ -1,351 +1,170 @@
 
         
-        #ifndef ATOM_APP_COMMAND_LINE_ARGS_H_
-#define ATOM_APP_COMMAND_LINE_ARGS_H_
+          static void ClearCache(content::RenderProcessHost* render_view_host);
+  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
+                             const std::string& proxy_config);
     
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(RenderProcessPreferences);
-};
-    
-    namespace auto_updater {
-    }
-    
-    const SkBitmap* OffscreenViewProxy::GetBitmap() const {
-  return view_bitmap_.get();
-}
-    
-    int LaunchProgram(const StringVector& relauncher_args,
-                  const StringVector& argv) {
-  // Redirect the stdout of child process to /dev/null, otherwise after
-  // relaunch the child process will raise exception when writing to stdout.
-  base::ScopedFD devnull(HANDLE_EINTR(open('/dev/null', O_WRONLY)));
-    }
-    
-    // Generate param traits log methods.
-#include 'ipc/param_traits_log_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
-
-    
-    
-    {}  // namespace nwapi
-    
-    v8::Handle<v8::Value> DeallocateObject(int routing_id,
-                                       int object_id) {
-  RenderThread::Get()->Send(new ShellViewHostMsg_Deallocate_Object(
-      routing_id, object_id));
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  return v8::Undefined(isolate);
-}
-    
-    // Tell browser to allocate a new object.
-// function AllocateObject(id, name, options);
-v8::Handle<v8::Value> AllocateObject(int routing_id,
-                                     int object_id,
-                                     const std::string& type,
-                                     v8::Handle<v8::Value> options);
-    
-    class Clipboard : public Base {
- public:
-  Clipboard(int id,
-            const base::WeakPtr<DispatcherHost>& dispatcher_host,
-            const base::DictionaryValue& option);
-  ~Clipboard() override;
+    class EventListener : public Base {
+  std::map<int, BaseEvent*> listerners_;
     }
     
     
-    {
-    {    if (zoom_controller) {
-      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
-      if (zoom_factor > content::kMaximumZoomFactor) {
-        zoom_factor = content::kMaximumZoomFactor;
-      }
-      if (zoom_factor < content::kMinimumZoomFactor) {
-        zoom_factor = content::kMinimumZoomFactor;
-      }
-      x *= zoom_factor;
-      y *= zoom_factor;
-    }
-    
-    Popup(x, y, rvh);
-  } else if (method == 'EnableShowEvent') {
-    arguments.GetBoolean(0, &enable_show_event_);
-  } else {
-    NOTREACHED() << 'Invalid call to Menu method:' << method
-                 << ' arguments:' << arguments;
+    {  // Convert from content coordinates to window coordinates.
+  // This code copied from chrome_web_contents_view_delegate_views.cc
+  aura::Window* target_window = GetActiveNativeView(rfh);
+  aura::Window* root_window = target_window->GetRootWindow();
+  views::Widget* top_level_widget =
+    views::Widget::GetTopLevelWidgetForNativeView(target_window);
+  aura::client::ScreenPositionClient* screen_position_client =
+        aura::client::GetScreenPositionClient(root_window);
+  if (screen_position_client) {
+    screen_position_client->ConvertPointToScreen(target_window,
+             &screen_point);
   }
+  set_delay_destruction(true);
+  menu_runner_.reset(new views::MenuRunner(menu_model_.get(), views::MenuRunner::CONTEXT_MENU,
+                                           base::Bind(&Menu::OnMenuClosed, base::Unretained(this))));
+  menu_runner_->RunMenuAt(top_level_widget,
+                       nullptr,
+                       gfx::Rect(screen_point, gfx::Size()),
+                       views::MENU_ANCHOR_TOPRIGHT,
+                       ui::MENU_SOURCE_NONE);
+  // It is possible for the same MenuMessageLoopAura to start a nested
+  // message-loop while it is already running a nested loop. So make
+  // sure the quit-closure gets reset to the outer loop's quit-closure
+  // once the innermost loop terminates.
+  {
+    base::AutoReset<base::Closure> reset_quit_closure(&message_loop_quit_,
+                                                      base::Closure());
+  
+    //base::MessageLoop* loop = base::MessageLoop::current();
+    base::MessageLoopCurrent::ScopedNestableTaskAllower allow;
+    base::RunLoop run_loop;
+    message_loop_quit_ = run_loop.QuitClosure();
+  
+    run_loop.Run();
+  }
+  set_delay_destruction(false);
+  if (pending_destruction())
+    object_manager_->OnDeallocateObject(id_);
 }
     
-    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
- public:
-  NwMenuGetNSStringFWithFixupFunction() {}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    namespace extensions {
+    }
     
- protected:
-  ~NwMenuGetNSStringFWithFixupFunction() override {}
     
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.allocateId', UNKNOWN)
  private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
+  DISALLOW_COPY_AND_ASSIGN(NwObjAllocateIdFunction);
 };
     
-      // Computes all the cross product distances of the points from the line,
-  // storing the actual (signed) cross products in distances_.
-  // Ignores distances of points that are further away than the previous point,
-  // and overlaps the previous point by at least half.
-  void ComputeDistances(const ICOORD& start, const ICOORD& end);
-    
-      // Copies UNICHAR_IDs from dst to src. Returns the number of ids copied.
-  // The function assumes that the arrays are terminated by INVALID_UNICHAR_ID
-  // and that dst has enough space for all the elements from src.
-  static inline int copy(const UNICHAR_ID src[], UNICHAR_ID dst[]) {
-    int i = 0;
-    do {
-      dst[i] = src[i];
-    } while (dst[i++] != INVALID_UNICHAR_ID);
-    return i - 1;
-  }
-    
-      // The pointed-to Pair has changed its key value, so the location of pair
-  // is reshuffled to maintain the heap invariant.
-  // Must be a valid pointer to an element of the heap_!
-  // Caution! Since GenericHeap is based on GenericVector, reallocs may occur
-  // whenever the vector is extended and elements may get shuffled by any
-  // Push or Pop operation. Therefore use this function only if Data in Pair is
-  // of type DoublePtr, derived (first) from DoublePtr, or has a DoublePtr as
-  // its first element. Reshuffles the heap to maintain the invariant.
-  // Time = O(log n).
-  void Reshuffle(Pair* pair) {
-    int index = pair - &heap_[0];
-    Pair hole_pair = heap_[index];
-    index = SiftDown(index, hole_pair);
-    index = SiftUp(index, hole_pair);
-    heap_[index] = hole_pair;
-  }
-    
-    			if (!pImageData)
-			{
-				break;
-			}
-    
-    		// the alpha mix for a 4x4 block of pixels
-		enum class SourceAlphaMix
-		{
-			UNKNOWN,
-			//
-			OPAQUE,			// all 1.0
-			TRANSPARENT,	// all 0.0 or NAN
-			TRANSLUCENT		// not all opaque or transparent
-		};
-    
-    
-    {	}
-    
-    
-    {	}
-    
-    enum InitOptions {ioReverseSolution = 1, ioStrictlySimple = 2, ioPreserveCollinear = 4};
-enum JoinType {jtSquare, jtRound, jtMiter};
-enum EndType {etClosedPolygon, etClosedLine, etOpenButt, etOpenSquare, etOpenRound};
-    
-    #if defined (__cplusplus)
-extern 'C' {
-#endif
+    // Render function.
+// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
+void ImGui_ImplAllegro5_RenderDrawData(ImDrawData* draw_data)
+{
+    // Avoid rendering when minimized
+    if (draw_data->DisplaySize.x <= 0.0f || draw_data->DisplaySize.y <= 0.0f)
+        return;
     }
     
-    #ifndef KISS_FFT_GUTS_H
-#define KISS_FFT_GUTS_H
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
     
-    /* a32 + (b32 * (opus_int32)((opus_int16)(c32))) >> 16 output have to be 32bit int */
-#undef silk_SMLAWB
-static OPUS_INLINE opus_int32 silk_SMLAWB_armv5e(opus_int32 a, opus_int32 b,
- opus_int16 c)
-{
-  int res;
-  __asm__(
-      '#silk_SMLAWB\n\t'
-      'smlawb %0, %1, %2, %3\n\t'
-      : '=r'(res)
-      : 'r'(b), 'r'(c), 'r'(a)
-  );
-  return res;
+    #include 'imgui.h'
+#include 'imgui_impl_marmalade.h'
+    
+        XINPUT_STATE xinput_state;
+    io.BackendFlags &= ~ImGuiBackendFlags_HasGamepad;
+    if (g_HasGamepad && XInputGetState(0, &xinput_state) == ERROR_SUCCESS)
+    {
+        const XINPUT_GAMEPAD& gamepad = xinput_state.Gamepad;
+        io.BackendFlags |= ImGuiBackendFlags_HasGamepad;
+    }
+    
+    
+    {        // Rendering
+        ImGui::Render();
+        al_clear_to_color(al_map_rgba_f(clear_color.x, clear_color.y, clear_color.z, clear_color.w));
+        ImGui_ImplAllegro5_RenderDrawData(ImGui::GetDrawData());
+        al_flip_display();
+    }
+    
+        CleanupDeviceD3D();
+    ::DestroyWindow(hwnd);
+    ::UnregisterClass(wc.lpszClassName, wc.hInstance);
+    
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+    
+        // serialize the JSON numbers
+    std::cout << j_integer_t << '\n';
+    std::cout << j_unsigned_t << '\n';
+    std::cout << j_enum << '\n';
+    std::cout << j_short << '\n';
+    std::cout << j_int << '\n';
+    std::cout << j_long << '\n';
+    std::cout << j_int_least32_t << '\n';
+    std::cout << j_uint8_t << '\n';
+    std::cout << j_ok << '\n';
+    std::cout << j_nan << '\n';
+    std::cout << j_infinity << '\n';
+    std::cout << j_float << '\n';
+    std::cout << j_float_nan << '\n';
+    std::cout << j_double << '\n\n';
+    
+     public:
+  Err(Err const&) = default;
+  Err(Err&&) = default;
+  Err& operator=(Err const&) = default;
+  Err& operator=(Err&&) = default;
+    
+      for (unsigned i = 0; i < iter; ++i) {
+    FB_LOG_EVERY_MS(INFO, -1) << 'every -1ms';
+  }
+    
+    void compareBenchmarkResults(const std::string& base, const std::string& test) {
+  printResultComparison(resultsFromFile(base), resultsFromFile(test));
 }
-#define silk_SMLAWB(a, b, c) (silk_SMLAWB_armv5e(a, b, c))
-    
-    #include 'src/cpp/ext/filters/census/context.h'
     
     
-    {}  // namespace grpc
+    {} // namespace folly
 
     
-    #include <grpc/support/port_platform.h>
     
-    
-    {
-    {   private:
-    const grpc::string name_;
-    const grpc::string value_;
-  };
-  return std::unique_ptr<ServerBuilderOption>(new StringOption(name, value));
-}
-    
-    namespace grpc {
-namespace load_reporter {
-    }
-    }
-    
-    // A thread pool interface for running callbacks.
-class ThreadPoolInterface {
- public:
-  virtual ~ThreadPoolInterface() {}
-    }
-    
-        /**
-     * System registration hook.
-     */
-    static void registerIDs();
-    
-        UBool operator == (const ScriptSet &other) const;
-    ScriptSet & operator = (const ScriptSet &other);
-    
-    U_NAMESPACE_BEGIN
-    
-        /**
-     * Returns TRUE if this instance represents no constraints on significant
-     * digits.
-     */
-    UBool isNoConstraints() const {
-        return fMin == 0 && fMax == INT32_MAX;
-    }
-    
-    
-    {    // Always emit at least '0'
-    if (digits == 0) {
-        return appendTo.append((UChar) 0x30);
-    }
-    return appendTo.append(gDigits, ((smallPositiveValue  + 1) << 2) - digits, digits);
-}
-    
-    U_NAMESPACE_BEGIN
-    
-    
-    {    /**
-     * Sets U_ILLEGAL_ARGUMENT_ERROR if the keyword is not a plural form.
-     *
-     * @param keyword for example 'few' or 'other'
-     * @return the index of the plural form corresponding to the keyword
-     */
-    static int32_t indexFromString(const UnicodeString &keyword, UErrorCode &errorCode);
+    {  /**
+   * The strategy parameter is used to tune the compression algorithm.
+   * Supported values:
+   * - Z_DEFAULT_STRATEGY: normal data
+   * - Z_FILTERED: data produced by a filter (or predictor)
+   * - Z_HUFFMAN_ONLY: force Huffman encoding only (no string match)
+   * - Z_RLE: limit match distances to one
+   * - Z_FIXED: prevents the use of dynamic Huffman codes
+   *
+   * The strategy parameter only affects the compression ratio but not the
+   * correctness of the compressed output.
+   */
+  int strategy;
 };
     
-    #include 'unicode/utypes.h'
-    
-      void InitOutPredictions(const MetaInfo& info,
-                          HostDeviceVector<bst_float>* out_preds,
-                          const gbm::GBTreeModel& model) const {
-    size_t n = model.param.num_output_group * info.num_row_;
-    const auto& base_margin = info.base_margin_.HostVector();
-    out_preds->Resize(n);
-    std::vector<bst_float>& out_preds_h = out_preds->HostVector();
-    if (base_margin.size() == n) {
-      CHECK_EQ(out_preds->Size(), n);
-      std::copy(base_margin.begin(), base_margin.end(), out_preds_h.begin());
-    } else {
-      if (!base_margin.empty()) {
-        std::ostringstream oss;
-        oss << 'Warning: Ignoring the base margin, since it has incorrect length. '
-            << 'The base margin must be an array of length ';
-        if (model.param.num_output_group > 1) {
-          oss << '[num_class] * [number of data points], i.e. '
-              << model.param.num_output_group << ' * ' << info.num_row_
-              << ' = ' << n << '. ';
-        } else {
-          oss << '[number of data points], i.e. ' << info.num_row_ << '. ';
-        }
-        oss << 'Instead, all data points will use '
-            << 'base_score = ' << model.base_margin;
-        LOG(INFO) << oss.str();
-      }
-      std::fill(out_preds_h.begin(), out_preds_h.end(), model.base_margin);
-    }
+      bool compare_exchange_weak(
+      SharedPtr& expected,
+      const SharedPtr& n,
+      std::memory_order mo = std::memory_order_seq_cst) noexcept {
+    return compare_exchange_weak(
+        expected, n, mo, detail::default_failure_memory_order(mo));
   }
-    
-    using namespace  ::osquery::extensions;
-    
-    void swap(InternalOptionInfo &a, InternalOptionInfo &b) {
-  using ::std::swap;
-  swap(a.value, b.value);
-  swap(a.default_value, b.default_value);
-  swap(a.type, b.type);
-  swap(a.__isset, b.__isset);
-}
-    
-    
-    {
-    {    std::ostringstream oss;
-    std::copy(line.begin() + options_index,
-              line.end(),
-              std::ostream_iterator<std::string>(oss, ' '));
-    r['options'] = oss.str();
-    results.push_back(r);
-  }
-}
-    
-      // Get the hash value for the kernel's .text memory segment
-  auto f2 = osquery::readFile(kKernelTextHashPath, content);
-  if (f2.ok()) {
-    boost::trim(content);
-    text_segment_hash = content;
-  } else {
-    VLOG(1) << 'Cannot read file: ' << kKernelTextHashPath;
-    return results;
-  }
-    
-    
-    {
-    {    if (boost::algorithm::starts_with(table, prefix)) {
-      linenoiseAddCompletion(lc, table.c_str());
-    }
-  }
-}
-    
-      hardwareDriver* type = nullptr;
-  if (types.size() == 1) {
-    type = &(types[0]);
-  }
-    
-    TEST_F(iokitDevicetree, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from iokit_devicetree');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'name', NormalType}
-  //      {'class', NormalType}
-  //      {'id', IntType}
-  //      {'parent', IntType}
-  //      {'device_path', NormalType}
-  //      {'service', IntType}
-  //      {'busy_state', IntType}
-  //      {'retain_count', IntType}
-  //      {'depth', IntType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
-}
-    
-    TEST_F(iptables, test_sanity) {
-  auto const data = execute_query('select * from iptables');
+  bool compare_exchange_weak(
+      SharedPtr& expected,
+      const SharedPtr& n,
+      std::memory_order success,
+      std::memory_order failure) /* noexcept */ {
+    auto newptr = get_newptr(n);
+    PackedPtr oldptr, expectedptr;
     }
     
-    // Sanity check integration test for kernel_panics
-// Spec file: specs/darwin/kernel_panics.table
+    #include <cstdint>
+#include <limits>
+#include <random>
+#include <set>
