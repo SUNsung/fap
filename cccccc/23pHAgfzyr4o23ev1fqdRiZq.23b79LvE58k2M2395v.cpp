baf6cb057d4d61bc8e62f1934df9dc8d7ef43a97
SUNsung <weatherius@gmail.com>
2019-05-30 20:13:31 +0300

    Rewrite [23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp] File in: 2019-05-30 20:13:30

--- cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
+++ cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
@@ -1,144 +1,247 @@
 
         
-        
-    {  DISALLOW_COPY_AND_ASSIGN(AutoUpdater);
-};
+        public:
+    explicit SignVerifyMessageDialog(const PlatformStyle *platformStyle, QWidget *parent);
+    ~SignVerifyMessageDialog();
     
-    void Event::SetSenderAndMessage(content::RenderFrameHost* sender,
-                                IPC::Message* message) {
-  DCHECK(!sender_);
-  DCHECK(!message_);
-  sender_ = sender;
-  message_ = message;
+    private:
+    reverse_lock(reverse_lock const&);
+    reverse_lock& operator=(reverse_lock const&);
+    
+    int secp256k1_ecdsa_recoverable_signature_convert(const secp256k1_context* ctx, secp256k1_ecdsa_signature* sig, const secp256k1_ecdsa_recoverable_signature* sigin) {
+    secp256k1_scalar r, s;
+    int recid;
     }
     
-    Delegate* AutoUpdater::GetDelegate() {
-  return delegate_;
+    static int recovery_test_nonce_function(unsigned char *nonce32, const unsigned char *msg32, const unsigned char *key32, const unsigned char *algo16, void *data, unsigned int counter) {
+    (void) msg32;
+    (void) key32;
+    (void) algo16;
+    (void) data;
+    }
+    
+    std::string ParsedInternalKey::DebugString() const {
+  char buf[50];
+  snprintf(buf, sizeof(buf), '' @ %llu : %d',
+           (unsigned long long) sequence,
+           int(type));
+  std::string result = ''';
+  result += EscapeString(user_key.ToString());
+  result += buf;
+  return result;
 }
     
-    bool AboutProtocolHandler::IsSafeRedirectTarget(const GURL& location) const {
-  return false;
+    
+// Owned filenames have the form:
+//    dbname/CURRENT
+//    dbname/LOCK
+//    dbname/LOG
+//    dbname/LOG.old
+//    dbname/MANIFEST-[0-9]+
+//    dbname/[0-9]+.(log|sst|ldb)
+bool ParseFileName(const std::string& fname,
+                   uint64_t* number,
+                   FileType* type) {
+  Slice rest(fname);
+  if (rest == 'CURRENT') {
+    *number = 0;
+    *type = kCurrentFile;
+  } else if (rest == 'LOCK') {
+    *number = 0;
+    *type = kDBLockFile;
+  } else if (rest == 'LOG' || rest == 'LOG.old') {
+    *number = 0;
+    *type = kInfoLogFile;
+  } else if (rest.starts_with('MANIFEST-')) {
+    rest.remove_prefix(strlen('MANIFEST-'));
+    uint64_t num;
+    if (!ConsumeDecimalNumber(&rest, &num)) {
+      return false;
+    }
+    if (!rest.empty()) {
+      return false;
+    }
+    *type = kDescriptorFile;
+    *number = num;
+  } else {
+    // Avoid strtoull() to keep filename format independent of the
+    // current locale
+    uint64_t num;
+    if (!ConsumeDecimalNumber(&rest, &num)) {
+      return false;
+    }
+    Slice suffix = rest;
+    if (suffix == Slice('.log')) {
+      *type = kLogFile;
+    } else if (suffix == Slice('.sst') || suffix == Slice('.ldb')) {
+      *type = kTableFile;
+    } else if (suffix == Slice('.dbtmp')) {
+      *type = kTempFile;
+    } else {
+      return false;
+    }
+    *number = num;
+  }
+  return true;
 }
     
-    void URLRequestAboutJob::Kill() {
-  weak_ptr_factory_.InvalidateWeakPtrs();
-  URLRequestJob::Kill();
-}
+    #include <caffe/layer.hpp>
+#include <caffe/blob.hpp>
+#include <caffe/layer_factory.hpp>
     
-    #ifndef ATOM_BROWSER_NET_URL_REQUEST_ABOUT_JOB_H_
-#define ATOM_BROWSER_NET_URL_REQUEST_ABOUT_JOB_H_
+    // these gpu functions are defined in gradient_compression.cu
+void Quantize2BitImpl(mshadow::Stream<mshadow::gpu> *s, const std::vector<mxnet::TBlob> &inputs,
+                      const float threshold);
+void Dequantize2BitImpl(mshadow::Stream<mshadow::gpu> *s, const std::vector<mxnet::TBlob> &inputs,
+                        const float threshold);
     
-    void OffscreenViewProxy::OnEvent(ui::Event* event) {
-  if (view_) {
-    view_->OnEvent(event);
+      /*!
+  * \brief Issues quantize operation to be scheduled by the engine
+  * Compresses `from` into `to` and accumulates the quantization error
+  * into 'residual', using the quantization of type `type_`
+  * \param from the ndarray containing original data to be quantized
+  * \param to the target ndarray which contains quantized data
+  * \param residual the ndarray which accumulates quantization error
+  * \param priority Priority of the action.
+  */
+  void Quantize(const mxnet::NDArray &from, mxnet::NDArray *to,
+                mxnet::NDArray *residual, const int priority);
+    
+    
+template<>
+void EvalRandom<DEVICE, ExponentialDistribution>(
+    const real_t &lambda,
+    const real_t &dummy,  // this is to satisfy the SampleOp lambda signature
+    const Resource &resource,
+    TBlob *ret,
+    RunContext ctx) {
+  typedef cpu xpu;  // No support for gpu for this distribution.
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  switch (ret->type_flag_) {
+  case mshadow::kFloat32:
+    {
+      mshadow::Random<xpu, float> *prnd = resource.get_random<xpu, float>(s);
+      mshadow::Tensor<xpu, 2, float> tmp = ret->FlatTo2D<xpu, float>(s);
+      prnd->SampleExponential(&tmp, float(lambda));  // NOLINT(*)
+      break;
+    }
+  case mshadow::kFloat64:
+    {
+      mshadow::Random<xpu, double> *prnd = resource.get_random<xpu, double>(s);
+      mshadow::Tensor<xpu, 2, double> tmp = ret->FlatTo2D<xpu, double>(s);
+      prnd->SampleExponential(&tmp, double(lambda));  // NOLINT(*)
+      break;
+    }
+  default:
+    LOG(FATAL) << 'Random only support float32 and float64';
   }
 }
     
-    namespace internal {
+    
+    {    size_t const_loc_ptr = 0;
+    for (int i = 0; i < args.size(); ++i) {
+      if (args.type_codes[i] == kTVMNDArrayTypeCode) {
+        const NDArray& nd =
+            static_cast<NDArray*>(args.values[i].v_handle)[0];
+        // We cannot set the value until
+        type_codes_[i] = kArrayHandle;
+        array_data_.push_back(nd);
+        array_loc_.push_back(i);
+        // check if there is read or mutate
+        // by default assume we mutate the array.
+        if (const_loc_ptr < const_loc.size() &&
+            i == const_loc[const_loc_ptr]) {
+          const_vars->push_back(nd.var());
+          ++const_loc_ptr;
+        } else {
+          mutate_vars->push_back(nd.var());
+        }
+      } else {
+        CHECK_LT(args.type_codes[i], kTVMType)
+            << 'Only allow POD type in mxnet async call';
+      }
     }
+  }
     
-    DelayedNativeViewHost::~DelayedNativeViewHost() {}
     
-    #include 'unicode/utypes.h'
-    
-    U_NAMESPACE_BEGIN
-    
-    SharedBreakIterator::~SharedBreakIterator() {
-  delete ptr;
+    {  Stream<xpu> *stream = ctx.get_stream<xpu>();
+  Tensor<xpu, 2, DType> out = out_data[0].get<xpu, 2, DType>(stream);
+  std::vector<Tensor<xpu, 2, DType> > ts_arr(in_data.size());
+  std::transform(in_data.begin(), in_data.end(), ts_arr.begin(),
+                 [&stream](TBlob blob) -> Tensor<xpu, 2, DType> {
+                   return blob.get<xpu, 2, DType>(stream);
+                 });
+  khatri_rao(out, ts_arr);
 }
     
-    U_NAMESPACE_END
+      virtual void Backward(const OpContext &ctx,
+                        const std::vector<TBlob> &out_grad,
+                        const std::vector<TBlob> &in_data,
+                        const std::vector<TBlob> &out_data,
+                        const std::vector<OpReqType> &req,
+                        const std::vector<TBlob> &in_grad,
+                        const std::vector<TBlob> &aux_args) {
+    using namespace mshadow;
+    using namespace mshadow::expr;
+    CHECK_EQ(out_grad.size(), 1U);
+    CHECK_EQ(in_data.size(), 1U);
+    CHECK_EQ(out_data.size(), 2U);
+    CHECK_EQ(req.size(), 1U);
+    CHECK_EQ(in_grad.size(), 1U);
+    typename DataType<DType>::ScaleType alpha = 1.0f;
+    typename DataType<DType>::ScaleType beta = 0.0f;
+    Stream<gpu> *s = ctx.get_stream<gpu>();
+    Tensor<gpu, 4, DType> grad = out_grad[lrn_enum::kOut].get<gpu, 4, DType>(s);
+    Tensor<gpu, 4, DType> data = in_data[lrn_enum::kData].get<gpu, 4, DType>(s);
+    Tensor<gpu, 4, DType> output_data = out_data[lrn_enum::kOut].get<gpu, 4, DType>(s);
+    Tensor<gpu, 4, DType> input_grad = in_grad[lrn_enum::kData].get<gpu, 4, DType>(s);
+    CHECK_EQ(s->dnn_handle_ownership_, mshadow::Stream<gpu>::OwnHandle);
+    CUDNN_CALL(cudnnLRNCrossChannelBackward(s->dnn_handle_,
+                                            lrn_desc_,
+                                            CUDNN_LRN_CROSS_CHANNEL_DIM1,
+                                            &alpha,
+                                            shape_desc_,
+                                            output_data.dptr_,
+                                            shape_desc_,
+                                            grad.dptr_,
+                                            shape_desc_,
+                                            data.dptr_,
+                                            &beta,
+                                            shape_desc_,
+                                            input_grad.dptr_));
+  }
     
-    class U_I18N_API SharedCalendar : public SharedObject {
-public:
-    SharedCalendar(Calendar *calToAdopt) : ptr(calToAdopt) { }
-    virtual ~SharedCalendar();
-    const Calendar *get() const { return ptr; }
-    const Calendar *operator->() const { return ptr; }
-    const Calendar &operator*() const { return *ptr; }
-private:
-    Calendar *ptr;
-    SharedCalendar(const SharedCalendar &);
-    SharedCalendar &operator=(const SharedCalendar &);
-};
-    
-    U_NAMESPACE_END
-    
-                OrderedUnit(int id, std::wstring name, std::wstring abbreviation, int order, bool isConversionSource = false, bool isConversionTarget = false, bool isWhimsical = false)
-                : UnitConversionManager::Unit(id, name, abbreviation, isConversionSource, isConversionTarget, isWhimsical), order(order)
-            {
-            }
-    
-    void TitleBarHelper::RegisterForLayoutChanged()
-{
-    m_layoutChangedToken =
-        m_coreTitleBar->LayoutMetricsChanged += ref new TypedEventHandler<CoreApplicationViewTitleBar^, Object^>(
-        [this](CoreApplicationViewTitleBar^ cTitleBar, Object^)
-    {
-        // Update title bar control size as needed to account for system size changes
-        SetTitleBarHeight(cTitleBar->Height);
-    });
-}
-    
-        private:
-        Windows::UI::Xaml::Controls::ListView^ m_tokenList;
-        Windows::UI::Xaml::Controls::MenuFlyout^ m_displayFlyout;
-        bool m_doAnimate;
-        bool m_resultAnimate;
-        bool m_isLastAnimatedInScientific;
-        bool m_isLastAnimatedInProgrammer;
-        bool m_IsLastFlyoutMemory = false;
-        bool m_IsLastFlyoutHistory = false;
-    
-    #include 'Views/Memory.g.h'
-#include 'Common/TitleBarHelper.h'
-#include 'Converters/BooleanNegationConverter.h'
-#include 'Converters/VisibilityNegationConverter.h'
-#include 'CalcViewModel/StandardCalculatorViewModel.h'
-    
-        // out_of_range.109
-    try
-    {
-        // try to use an array index that is not a number
-        json::reference ref = j.at('/array/one'_json_pointer);
-    }
-    catch (json::parse_error& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-    
-        // read-only access
-    
-    
-    {    std::cout << null << '\n';
-    std::cout << *res2.first << ' ' << std::boolalpha << res2.second << '\n';
-}
-
-    
-    class WriteCallback {
- public:
-  virtual ~WriteCallback() {}
-    }
-    
-      // Attempt to read a key using the snapshot.  This will fail since
-  // the previous write outside this txn conflicts with this read.
-  read_options.snapshot = snapshot;
-  s = txn->GetForUpdate(read_options, 'abc', &value);
-  assert(s.IsBusy());
-    
-    // Supported only for Leveled compaction
-Status SuggestCompactRange(DB* db, ColumnFamilyHandle* column_family,
-                           const Slice* begin, const Slice* end);
-Status SuggestCompactRange(DB* db, const Slice* begin, const Slice* end);
-    
-      // Number of keys between restart points for delta encoding of keys.
-  // This parameter can be changed dynamically.  Most clients should
-  // leave this parameter alone.
-  //
-  // Default: 16
-  int block_restart_interval;
-    
-    #include <map>
-#include <string>
-#include <unordered_set>
+    #include <algorithm>
 #include <vector>
+#include './spatial_transformer-inl.h'
+namespace mxnet {
+namespace op {
+#if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 5
+template<typename DType>
+class CuDNNSpatialTransformerOp : public Operator {
+ public:
+  explicit CuDNNSpatialTransformerOp(SpatialTransformerParam param) {
+    this->param_ = param;
+    init_cudnn_ = false;
+    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
+    if (param_.sampler_type == st::kBilinear) {
+      sampler_ = CUDNN_SAMPLER_BILINEAR;
+    }
+  }
+    }
+    }
+    }
+    
+    template<typename xpu>
+void NDArrayOp<xpu>::Forward(const OpContext &ctx,
+                   const std::vector<TBlob> &in_data,
+                   const std::vector<OpReqType> &req,
+                   const std::vector<TBlob> &out_data,
+                   const std::vector<TBlob> &aux_args) {
+  using namespace mshadow;
+  Context ndctx = get_ctx();
+  std::vector<void*> ptrs;
+  std::vector<Engine::VarHandle> ndvar;
+  std::vector<int> tags;
+  for (auto& i : req) CHECK_NE(i, kAddTo);
+    }
