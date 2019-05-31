976292a6cad996f281648815552f5a31da9e85f4
SUNsung <weatherius@gmail.com>
2019-05-31 05:19:45 +0300

    Rewrite [56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb] File in: 2019-05-31 05:19:45

--- ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
+++ ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
@@ -1,123 +1,148 @@
 
         
-            def require_local_account!
-      redirect_to admin_account_path(@account.id) unless @account.local? && @account.user.present?
+                def render
+          options = @options.stringify_keys
+          options['type']     = 'checkbox'
+          options['value']    = @checked_value
+          options['checked'] = 'checked' if input_checked?(options)
+    
+    if pathutil_relative == native_relative
+  Benchmark.ips do |x|
+    x.report('pathutil') { pathutil_relative }
+    x.report('native')   { native_relative }
+    x.compare!
+  end
+else
+  print 'PATHUTIL: '
+  puts pathutil_relative
+  print 'NATIVE:   '
+  puts native_relative
+end
+
+    
+      </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+          def exception(exception, status)
+        return if @hide_this_step
+    
+          attr_accessor :page, :layout, :content, :paginator
+      attr_accessor :highlighter_prefix, :highlighter_suffix
+    
+        def insert_after(index, *names)
+      insert assert_index(index) + 1, *names
     end
     
-      def set_web_push_subscription
-    @web_subscription = ::Web::PushSubscription.find_by(access_token_id: doorkeeper_token.id)
+        def add(path, content)
+      @pages[path] = content
+    end
+    
+        def process_response?(response)
+      if response.error?
+        raise <<~ERROR
+          Error status code (#{response.code}): #{response.return_message}
+          #{response.url}
+          #{JSON.pretty_generate(response.headers).slice(2..-3)}
+        ERROR
+      elsif response.blank?
+        raise 'Empty response body: #{response.url}'
+      end
+    
+            css('h1[class]').remove_attr('class')
+        css('table[class]').remove_attr('class')
+        css('table[width]').remove_attr('width')
+        css('tr[style]').remove_attr('style')
+    
+            name.prepend '#{breadcrumbs.join('.')}#' if breadcrumbs.present? && breadcrumbs[0] != name
+        name << '()' if %w(Function Method Constructor).include?(subtitle)
+        name
+      end
+    
+            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
+          node.before(node.children).remove
+        end
+    
+        def test_code_around_binding_on_irb
+      workspace = eval('IRB::WorkSpace.new(binding)', binding, '(irb)')
+      assert_equal(nil, workspace.code_around_binding)
+    end
+    
+      # This method is called when the parser found syntax error.
+  def compile_error(msg)
   end
     
-          explicit_plugins_specs.each do |spec|
-        packet_gem.add(spec.name)
-      end
+      # staged_path not available in Installer/Uninstall Stanza, workaround by nesting with preflight/postflight
+  # see https://github.com/Homebrew/homebrew-cask/pull/8887
+  # and https://github.com/Homebrew/homebrew-cask-versions/pull/296
     
-        platforms.types.each do |type|
-      desc 'Run acceptance test in #{type} machines'
-      task type do
-        ENV['LS_TEST_PLATFORM']=type
-        exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/*_spec.rb']]))
-      end
-    end
-    
-          it 'list the plugin with his version' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose #{plugin_name}')
-        expect(result).to run_successfully_and_output(/^#{plugin_name} \(\d+\.\d+.\d+\)/)
-      end
+      def remove_duplicates
+    where = 'WHERE s1.user_id = s2.user_id AND s1.shareable_id = s2.shareable_id AND '\
+      's1.shareable_type = s2.shareable_type AND s1.id > s2.id'
+    if AppConfig.postgres?
+      execute('DELETE FROM share_visibilities AS s1 USING share_visibilities AS s2 #{where}')
+    else
+      execute('DELETE s1 FROM share_visibilities s1, share_visibilities s2 #{where}')
     end
   end
 end
 
     
-          # A helper class for comparing the positions of different parts of a
-      # `pair` node.
-      class HashElementDelta
-        def initialize(first, second)
-          @first = first
-          @second = second
+    describe ContactsController, :type => :controller do
+  describe '#index' do
+    before do
+      AppConfig.chat.enabled = true
+      @aspect = bob.aspects.create(:name => 'another aspect')
+      bob.share_with alice.person, @aspect
+      bob.share_with eve.person, @aspect
+      sign_in bob, scope: :user
+    end
     
-          dir = if File.directory?(file_or_dir)
-              file_or_dir
-            else
-              File.dirname(file_or_dir)
-            end
-      @path_cache[dir] ||= ConfigLoader.configuration_file_for(dir)
-      path = @path_cache[dir]
-      @object_cache[path] ||= begin
-                                print 'For #{dir}: ' if ConfigLoader.debug?
-                                ConfigLoader.configuration_from_file(path)
-                              end
+        it 'generates a jasmine fixture', :fixture => true do
+      contact = alice.contact_for(bob.person)
+      aspect = alice.aspects.create(:name => 'people')
+      contact.aspects << aspect
+      contact.save
+      get :new, params: {person_id: bob.person.id}
+      save_fixture(html_for('body'), 'status_message_new')
     end
   end
 end
 
     
-          def part_of_ignored_node?(node)
-        ignored_nodes.map(&:loc).any? do |ignored_loc|
-          if ignored_loc.expression.begin_pos > node.source_range.begin_pos
-            next false
+        context 'on my own post' do
+      before do
+        aspect_to_post = alice.aspects.where(:name => 'generic').first
+        @post = alice.post :status_message, :text => 'something', :to => aspect_to_post
+      end
+    
+      task :all => [:readmes, :index]
+end
+    
+          def encrypt(value)
+        options[:encryptor].hexdigest value.to_s
+      end
+    
+            # Set these key values to boolean 'true' to include in policy
+        NO_ARG_DIRECTIVES.each do |d|
+          if options.key?(d) && options[d].is_a?(TrueClass)
+            directives << d.to_s.sub(/_/, '-')
           end
-    
-          def preference_field_for(form, field, options)
-        case options[:type]
-        when :integer
-          form.text_field(field, preference_field_options(options))
-        when :boolean
-          form.check_box(field, preference_field_options(options))
-        when :string
-          form.text_field(field, preference_field_options(options))
-        when :password
-          form.password_field(field, preference_field_options(options))
-        when :text
-          form.text_area(field, preference_field_options(options))
-        else
-          form.text_field(field, preference_field_options(options))
-        end
-      end
-    
-          # finds class for a given symbol / string
-      #
-      # Example :
-      # :products returns Spree::Product
-      # :my_products returns MyProduct if MyProduct is defined
-      # :my_products returns My::Product if My::Product is defined
-      # if cannot constantize it returns nil
-      # This will allow us to use cancan abilities on tab
-      def klass_for(name)
-        model_name = name.to_s
-    
-          it 'can be used with search filtering' do
-        click_on 'Filter'
-        fill_in 'q_number_cont', with: 'R200'
-        click_on 'Filter Results'
-        expect(page).not_to have_content('R100')
-        within_row(1) { expect(page).to have_content('R200') }
-        expect(current_url).to match(/per_page\=45/)
-        expect(page).to have_select('per_page', selected: '45')
-        select '60', from: 'per_page'
-        wait_for_ajax
-        expect(page).to have_select('per_page', selected: '60')
-        expect(page).to have_selector(:css, 'select.per-page-selected-60')
-        expect(page).not_to have_content('R100')
-        within_row(1) { expect(page).to have_content('R200') }
-        expect(current_url).to match(/per_page\=60/)
-      end
-    end
-    
-              @products = @products.distinct.page(params[:page]).per(params[:per_page])
-          expires_in 15.minutes, public: true
-          headers['Surrogate-Control'] = 'max-age=#{15.minutes}'
-          respond_with(@products)
         end
     
-        def generate_new_liquid(link)
-      Liquid::Template.register_tag('jsfiddle', JSFiddleTag)
-      Liquid::Template.parse('{% jsfiddle #{link} %}')
-    end
-    
-      def broadcast_params
-    params.permit(:title, :processed_html, :type_of, :sent)
-    # left out body_markdown and processed_html attributes
-    #   until we decide we're using them
+      it 'sets a new csrf token for the session in env, even after a 'safe' request' do
+    get('/', {}, {})
+    expect(env['rack.session'][:csrf]).not_to be_nil
   end
-end
