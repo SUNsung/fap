f33d148c0d04da68f5a60a0dad24f25e06ea72dd
SUNsung <weatherius@gmail.com>
2019-06-01 01:24:37 +0300

    Rewrite [11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb] File in: 2019-06-01 01:24:37

--- ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
+++ ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
@@ -1,95 +1,12 @@
 
         
-                def importer_class
-          IssueAndLabelLinksImporter
-        end
-    
-    module Gitlab
-  module GithubImport
-    module Importer
-      class LfsObjectsImporter
-        include ParallelScheduling
-    
-            def importer_class
-          NoteImporter
-        end
-    
-            # Builds a diff note from a GitHub API response.
-        #
-        # note - An instance of `Sawyer::Resource` containing the note details.
-        def self.from_api_response(note)
-          matches = note.html_url.match(NOTEABLE_ID_REGEX)
-    
-            # Builds an issue from a GitHub API response.
-        #
-        # issue - An instance of `Sawyer::Resource` containing the issue
-        #         details.
-        def self.from_api_response(issue)
-          user =
-            if issue.user
-              Representation::User.from_api_response(issue.user)
-            end
-    
-            alias_method :issuable_type, :noteable_type
-      end
+            it 'returns a label 'No' if a given agent is not working' do
+      stub(@agent).working? { false }
+      label = working(@agent)
+      expect(label).to be_html_safe
+      expect(Nokogiri(label).text).to eq 'No'
     end
   end
-end
-
     
-        puts('Unpacking #{package_file}')
-    
-      # We compare the before the update and after the update
-  def display_updated_plugins(previous_gem_specs_map)
-    update_count = 0
-    find_latest_gem_specs.values.each do |spec|
-      name = spec.name.downcase
-      if previous_gem_specs_map.has_key?(name)
-        if spec.version != previous_gem_specs_map[name].version
-          puts('Updated #{spec.name} #{previous_gem_specs_map[name].version.to_s} to #{spec.version.to_s}')
-          update_count += 1
-        end
-      else
-        puts('Installed #{spec.name} #{spec.version.to_s}')
-        update_count += 1
-      end
-    end
-    
-      describe '#system?' do
-    context 'when the pipeline is a system pipeline' do
-      let(:settings) { mock_settings({ 'pipeline.system' => true })}
-    
-        platforms.types.each do |type|
-      desc 'Run acceptance test in #{type} machines'
-      task type do
-        ENV['LS_TEST_PLATFORM']=type
-        exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/*_spec.rb']]))
-      end
-    end
-    
-        after(:all) do
-      logstash.uninstall
-    end
-    
-          within_row(2) { click_icon :split }
-      targetted_select2 'LA(#{order.reload.shipments.last.number})', from: '#s2id_item_stock_location'
-      click_icon :save
-      wait_for_ajax
-      expect(page.find('#shipment_#{order.reload.shipments.last.id}')).to be_present
-    end
-  end
-end
-
-    
-        for_each_gem do |gem_path|
-      sh 'gem push '#{gem_path}''
-    end
-  end
-end
-    
-              if @product.errors.empty?
-            respond_with(@product.reload, status: 200, default_template: :show)
-          else
-            invalid_resource!(@product)
-          end
-        end
+    describe JobsHelper do
+  let(:job) { Delayed::Job.new }
