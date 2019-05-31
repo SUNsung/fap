1fcbe53abd58f3b5d31294b4d592554849e3c987
SUNsung <weatherius@gmail.com>
2019-05-31 18:23:08 +0300

    Rewrite [03tsrILecRwRM03wrPfIT3GhIQ03JiSIW2.BhI03cfk.py] File in: 2019-05-31 18:23:07

--- pyton/03tsrILecRwRM03wrPfIT3GhIQ03JiSIW2.BhI03cfk.py
+++ pyton/03tsrILecRwRM03wrPfIT3GhIQ03JiSIW2.BhI03cfk.py
@@ -1,5 +1,67 @@
 
         
-            for category, entries in sections.items():
-        if sorted(entries) != entries:
-            add_error(section_line_num[category], '{} section is not in alphabetical order'.format(category))
+        
+def hex_str(int_list):
+    return codecs.encode(intlist_to_bytes(int_list), 'hex')
+    
+    new_version = {}
+    
+        template = template.replace('@SITES@', textwrap.indent('\n'.join(ie_htmls), '\t'))
+    
+    if __name__ == '__main__':
+    main()
+
+    
+    with io.open(README_FILE, 'w', encoding='utf-8') as f:
+    f.write(header)
+    f.write(options)
+    f.write(footer)
+
+    
+    # Allow direct execution
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    
+            final_headers[name] = value
+    return final_headers
+    
+        @property
+    def body(self):
+        body = self._orig.body
+        if isinstance(body, str):
+            # Happens with JSON/form request data parsed from the command line.
+            body = body.encode('utf8')
+        return body or b''
+
+    
+    from httpie.plugins.base import AuthPlugin
+    
+    
+def test_migrate_implicit_content_type():
+    config = MockEnvironment().config
+    
+    
+def rst_filenames():
+    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
+        if '.tox' not in root:
+            for filename in fnmatch.filter(filenames, '*.rst'):
+                yield os.path.join(root, filename)
+    
+    
+def test_unicode_json_item_verbose(httpbin):
+    r = http('--verbose', '--json',
+             'POST', httpbin.url + '/post', u'test=%s' % UNICODE)
+    assert HTTP_OK in r
+    assert UNICODE in r
+    
+        def test_request_body_from_file_by_path_no_field_name_allowed(
+            self, httpbin):
+        env = MockEnvironment(stdin_isatty=True)
+        r = http('POST', httpbin.url + '/post', 'field-name@' + FILE_PATH_ARG,
+                 env=env, error_exit_ok=True)
+        assert 'perhaps you meant --form?' in r.stderr
+    
+        def failed(self):
+        self._progress_reporter.stop()
