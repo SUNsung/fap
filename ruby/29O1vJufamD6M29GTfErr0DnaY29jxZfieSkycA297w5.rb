fc9ea29c42e570da0c5ca40fed67b36f9128e7fd
SUNsung <weatherius@gmail.com>
2019-05-31 06:28:43 +0300

    Rewrite [29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb] File in: 2019-05-31 06:28:42

--- ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
+++ ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
@@ -1,67 +1,39 @@
 
         
-                      accept = if current_value.respond_to?(:call)
-                current_value.call(item)
-              else
-                Array(current_value).map(&:to_s).include?(value.to_s)
-              end
+              # Iterates over all of the objects for the given method (e.g. `:labels`).
+      #
+      # method - The method to send to Octokit for querying data.
+      # args - Any arguments to pass to the Octokit method.
+      def each_object(method, *args, &block)
+        return to_enum(__method__, method, *args) unless block_given?
     
-            def render(&block)
-          render_collection_for(RadioButtonBuilder, &block)
+    module Gitlab
+  module GithubImport
+    module Importer
+      class DiffNotesImporter
+        include ParallelScheduling
+    
+    module Gitlab
+  module GithubImport
+    module Importer
+      class IssuesImporter
+        include ParallelScheduling
+    
+            # issue - An instance of `Gitlab::GithubImport::Representation::Issue`
+        # project - An instance of `Project`
+        # client - An instance of `Gitlab::GithubImport::Client`
+        def initialize(issue, project, client)
+          @issue = issue
+          @project = project
+          @client = client
+          @label_finder = LabelFinder.new(project)
         end
     
-              if name_and_id['for']
-            name_and_id['id'] = name_and_id['for']
-          else
-            name_and_id.delete('id')
+              lfs_objects.each do |object|
+            yield object
           end
-    
-          test 'when layout is overwritten by false in render, render no layout' do
-        controller = WithString.new
-        controller.process(:overwrite_false)
-        assert_equal 'Hello string!', controller.response_body
-      end
-    
-      protected
-    
-          respond_to_on_destroy
-    end
-  end
-    
-        def email_changed(record, opts={})
-      devise_mail(record, :email_changed, opts)
-    end
-    
-            if options[:bypass]
-          ActiveSupport::Deprecation.warn(<<-DEPRECATION.strip_heredoc, caller)
-          [Devise] bypass option is deprecated and it will be removed in future version of Devise.
-          Please use bypass_sign_in method instead.
-          Example:
-    
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-              # Make sure we're only working with one VM if single target
-          if options[:single_target] && vms.length != 1
-            vm = @env.primary_vm
-            raise Errors::MultiVMTargetRequired if !vm
-            vms = [vm]
-          end
-    
-            # Sets a human-friendly description of the plugin.
-        #
-        # @param [String] value Description of the plugin.
-        # @return [String] Description of the plugin.
-        def self.description(value=UNSET_VALUE)
-          get_or_set(:description, value)
-        end
-    
-            # This is the method called to when the system is being destroyed
-        # and allows the provisioners to engage in any cleanup tasks necessary.
-        def cleanup
+        rescue StandardError => e
+          Rails.logger.error('The Lfs import process failed. #{e.message}')
         end
       end
     end
@@ -69,95 +41,45 @@
 end
 
     
-            # Defines a capability for the given host. The block should return
-        # a class/module that has a method with the capability name, ready
-        # to be executed. This means that if it is an instance method,
-        # the block should return an instance of the class.
-        #
-        # @param [String] host The name of the host
-        # @param [String] cap The name of the capability
-        def self.host_capability(host, cap, &block)
-          components.host_capabilities[host.to_sym].register(cap.to_sym, &block)
-          nil
+            def importer_class
+          NoteImporter
         end
     
-        def self.extensions
-      ['.air']
+          # The base cache key to use for tracking already imported objects.
+      ALREADY_IMPORTED_CACHE_KEY =
+        'github-importer/already-imported/%{project}/%{collection}'.freeze
+    
+            # Builds a user using a Hash that was built from a JSON payload.
+        def self.from_json_hash(raw_hash)
+          new(Representation.symbolize_hash(raw_hash))
+        end
+    
+        def create
+      authorize ReportNote, :create?
+    
+      def process_push_request
+    case hub_mode
+    when 'subscribe'
+      Pubsubhubbub::SubscribeService.new.call(account_from_topic, hub_callback, hub_secret, hub_lease_seconds, verified_domain)
+    when 'unsubscribe'
+      Pubsubhubbub::UnsubscribeService.new.call(account_from_topic, hub_callback)
+    else
+      ['Unknown mode: #{hub_mode}', 422]
     end
-    
-        def URIAddEncodedOctetToBuffer(octet, result, index)
-      result[index] = 37; # Char code of '%'.
-      index         += 1
-      result[index] = @@hexCharCodeArray[octet >> 4];
-      index         += 1
-      result[index] = @@hexCharCodeArray[octet & 0x0F];
-      index += 1
-      return index;
-    end
-    
-          def line_class(line)
-        if line =~ /^@@/
-          'gc'
-        elsif line =~ /^\+/
-          'gi'
-        elsif line =~ /^\-/
-          'gd'
-        else
-          ''
-        end
-      end
-    
-          def header
-        if @header.nil?
-          if page = @page.header
-            @header = page.text_data
-          else
-            @header = false
-          end
-        end
-        @header
-      end
-    
-          def string_to_code(string)
-        # sha bytes
-        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
-        # Thanks donpark's IdenticonUtil.java for this.
-        # Match the following Java code
-        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
-        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
-    
-          def page_dir
-        @page_dir
-      end
-    
-          def date
-        first = page.last_version
-        return Time.now.strftime(DATE_FORMAT) unless first
-        first.authored_date.strftime(DATE_FORMAT)
-      end
-    
-      test 'extract destination file name in case of path renaming' do
-    view = Precious::Views::LatestChanges.new
-    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
-    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
   end
     
-        # Test page_header_from_content(@content)
-    actual = @view.title
-    assert_equal '1 & 2', actual
+      def request_locale
+    preferred_locale || compatible_locale
   end
     
-      base_path = wiki_options[:base_path]
+      def user_needs_sign_in_update?
+    user_signed_in? && (current_user.current_sign_in_at.nil? || current_user.current_sign_in_at < UPDATE_SIGN_IN_HOURS.hours.ago)
+  end
+end
+
     
-        # Extract the path string that Gollum::Wiki expects
-    def extract_path(file_path)
-      return nil if file_path.nil?
-      last_slash = file_path.rindex('/')
-      if last_slash
-        file_path[0, last_slash]
+          it 'returns an array of reshares for a post' do
+        bob.reshare!(@post)
+        get :index, params: {post_id: @post.id}, format: :json
+        expect(JSON.parse(response.body).map {|h| h['id'] }).to eq(@post.reshares.map(&:id))
       end
-    end
-    
-    # Set ruby to UTF-8 mode
-# This is required for Ruby 1.8.7 which gollum still supports.
-$KCODE = 'U' if RUBY_VERSION[0, 3] == '1.8'
