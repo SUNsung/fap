6739c1707f86f405bbf8adfb73868b793de07ec8
SUNsung <weatherius@gmail.com>
2019-05-31 15:03:03 +0300

    Rewrite [58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb] File in: 2019-05-31 15:03:02

--- ruby/58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb
+++ ruby/58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb
@@ -1,73 +1,106 @@
 
         
-        module LogStash
-  module PluginManager
+            # This is the object that used to be in new_object's position
+    loc = memory_location(new_tenant)
+    assert loc, 'should have a memory location'
+    
+        t = Time.at(10000000000000000000000)
+    assert_equal('<<10000000000000000000000>>', t.strftime('<<%s>>'))
+    assert_equal('<<010000000000000000000000>>', t.strftime('<<%24s>>'))
+    assert_equal('<<010000000000000000000000>>', t.strftime('<<%024s>>'))
+    assert_equal('<< 10000000000000000000000>>', t.strftime('<<%_24s>>'))
+  end
+    
+      find_files = ->(path) {
+    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
+      path if File.file?(path)
+    end.compact
+  }
+    
+        def log_http_get_files(files, from, cached = false)
+      return if files.empty?
+      s = '  #{'CACHED ' if cached}GET #{files.length} files from #{from} #{files * ' '}...'
+      if cached
+        puts dark green s
+      else
+        puts dark cyan s
+      end
+    end
+    
+          # private
+    
+          def mathjax
+        @mathjax
+      end
+    
+          locations = Array.new
+      while (data.code.to_i == 301 || data.code.to_i == 302)
+        data = handle_gist_redirecting(data)
+        break if locations.include? data.header['Location']
+        locations << data.header['Location']
+      end
+    
+          if File.symlink?(includes_dir)
+        return 'Includes directory '#{includes_dir}' cannot be a symlink'
+      end
+    
+          Dir.chdir(file_path) do
+        contents = file.read
+        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
+          contents = $1.lstrip
+        end
+        contents = pre_filter(contents)
+        if @raw
+          contents
+        else
+          partial = Liquid::Template.parse(contents)
+          context.stack do
+            partial.render(context)
+          end
+        end
+      end
+    end
   end
 end
     
-      private
+    Given /^I update my user view to include the attachment$/ do
+  steps %{
+    Given I overwrite 'app/views/users/show.html.erb' with:
+      '''
+      <p>Name: <%= @user.name %></p>
+      <p>Attachment: <%= image_tag @user.attachment.url %></p>
+      '''
+  }
+end
     
-      def validate_cache_location
-    cache_location = LogStash::Environment::CACHE_PATH
-    if File.exist?(cache_location)
-      puts('Directory #{cache_location} is going to be overwritten, do you want to continue? (Y/N)')
-      override = ( 'y' == STDIN.gets.strip.downcase ? true : false)
-      if override
-        FileUtils.rm_rf(cache_location)
-      else
-        puts('Unpack cancelled: file #{cache_location} already exists, please delete or move it')
-        exit
+    module RailsCommandHelpers
+  def framework_version?(version_string)
+    framework_version =~ /^#{version_string}/
+  end
+    
+      def migration_version
+    if Rails.version.start_with? '5'
+      '[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]'
+    end
+  end
+end
+
+    
+        def definitions_for(klass)
+      parent_classes = klass.ancestors.reverse
+      parent_classes.each_with_object({}) do |ancestor, inherited_definitions|
+        inherited_definitions.deep_merge! @attachments[ancestor]
       end
     end
   end
 end
 
     
-        # Attributes are special! We do not want to remove the default values of
-    # the destination package type unless their value is specified on the
-    # source package object.
-    pkg.attributes.merge!(self.attributes)
-    
-        for i in 0..(TAR_CHUNK_SIZE-1)
-      checksum += header.getbyte(i)
+        def possible_types
+      MIME::Types.type_for(@filepath).collect(&:content_type)
     end
     
-        settings['registry'] = attributes[:npm_registry] if attributes[:npm_registry_given?]
-    set_default_prefix unless attributes[:prefix_given?]
-    
-      def generate_mtree
-    ::Dir.chdir(build_path) do
-      cmd = 'LANG=C bsdtar '
-      cmd += '-czf .MTREE '
-      cmd += '--format=mtree '
-      cmd += '--options='!all,use-set,type,uid,gid,mode,time,size,md5,sha256,link' '
-      cmd += ::Dir.entries('.').reject{|entry| entry =~ /^\.{1,2}$/ }.join(' ')
-      safesystem(cmd)
-    end
-  end # def generate_mtree
-    
-      private
-  def input(command)
-    platforms = [
-      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
-      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
-      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
-      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
-      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
-    ]
-    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
-                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
-                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
-                             'log_directory', 'log_file_stderr', 'log_file_stdout']
-    
-        ::Dir.mkdir(params[:output])
-    builddir = ::Dir.pwd
-    
-        # use dir to set stuff up properly, mainly so I don't have to reimplement
-    # the chdir/prefix stuff special for zip.
-    dir = convert(FPM::Package::Dir)
-    if attributes[:chdir]
-      dir.attributes[:chdir] = File.join(build_path, attributes[:chdir])
-    else
-      dir.attributes[:chdir] = build_path
+        # Returns the filename, the same way as ':basename.:extension' would.
+    def filename attachment, style_name
+      [ basename(attachment, style_name), extension(attachment, style_name) ].delete_if(&:empty?).join('.'.freeze)
     end
