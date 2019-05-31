bb25811b68042a20ab97f062f5e2592340b6817b
SUNsung <weatherius@gmail.com>
2019-05-31 08:59:31 +0300

    Rewrite [57u16gUxq1y657BGrPzNaat0Q57IR0IBKAq29Q57yJ.py] File in: 2019-05-31 08:59:31

--- pyton/57u16gUxq1y657BGrPzNaat0Q57IR0IBKAq29Q57yJ.py
+++ pyton/57u16gUxq1y657BGrPzNaat0Q57IR0IBKAq29Q57yJ.py
@@ -1,165 +1,165 @@
 
         
-            # Create the LSTM
-    self.__lstm__()
+            def set(self, results, query):
+        '''Set the result for the given query key in the cache.
     
-      # Turn rates, noisy_data, and input into numpy arrays.
-  rates_train = nparray_and_transpose(rates_train)
-  rates_valid = nparray_and_transpose(rates_valid)
-  noisy_data_train = nparray_and_transpose(noisy_data_train)
-  noisy_data_valid = nparray_and_transpose(noisy_data_valid)
-  input_train = nparray_and_transpose(input_train)
-  inputs_valid = nparray_and_transpose(inputs_valid)
+        def __init__(self, vehicle_size, license_plate, spot_size):
+        self.vehicle_size = vehicle_size
+        self.license_plate = license_plate
+        self.spot_size
+        self.spots_taken = []
     
-      if mat_init_value is not None and mat_init_value.shape != (in_size, out_size):
-    raise ValueError(
-        'Provided mat_init_value must have shape [%d, %d].'%(in_size, out_size))
-  if bias_init_value is not None and bias_init_value.shape != (1,out_size):
-    raise ValueError(
-        'Provided bias_init_value must have shape [1,%d].'%(out_size,))
+        def current_year_month(self):
+        '''Return the current year and month.'''
+        ...
     
-          scorings.append(dict(
-          correctness=correctness,
-          sentence=sentence,
-          joint_prob=joint_prob,
-          word_probs=word_probs))
-    scoring_mode = 'full' if self.test_data_name == 'pdp60' else 'partial'
-    return utils.compare_substitutions(
-        self.question_ids, scorings, scoring_mode)
+        def _hash_function(self, key):
+        return key % self.size
     
-      # Generating more data on train set.
-  if FLAGS.sample_mode == SAMPLE_TRAIN:
-    data_set = train_data
-  elif FLAGS.sample_mode == SAMPLE_VALIDATION:
-    data_set = valid_data
-  else:
-    raise NotImplementedError
+        def get(self, query):
+        '''Get the stored query result from the cache.
     
+        publicKey, privateKey = generateKey(keySize)
+    print('\nWriting public key to file %s_pubkey.txt...' % name)
+    with open('%s_pubkey.txt' % name, 'w') as fo:
+        fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
     
-def wasserstein_discriminator_loss_intrabatch(values, is_real_input):
-  '''Wasserstein discriminator loss.  This is an odd variant where the value
-  difference is between the real tokens and the fake tokens within a single
-  batch.
+        def __prepare__(self, N = 0, M = 0):
+        self.dp = [[-1 for y in range(0,M)] for x in range(0,N)]
     
-      Args:
-    hparams:  MaskGAN hyperparameters.
-    learning_rate:  tf.Variable scalar learning rate.
-    final_gen_objective:  Scalar final REINFORCE objective for the sequence.
-    averages_op:  ExponentialMovingAverage apply average op to
-      maintain the baseline.
-    global_step:  global_step tf.Variable.
+    def reformatHex(i):
+	'''[summary]
+	Converts the given integer into 8-digit hex number.
     
-        # Create contiguous masked section to be False.
-    for i, index in enumerate(start_mask):
-      p[i, index:index + masked_length] = False
+    min_length = 8
+max_length = 16
+password = ''.join(random.choice(chars) for x in range(random.randint(min_length, max_length)))
+print('Password: ' + password)
+print('[ If you are thinking of using this passsword, You better save it. ]')
     
-          # Now calculate the attention-weighted vector.
-      alignments = tf.expand_dims(alignments, 2)
-      context_vector = tf.reduce_sum(alignments * values, [1])
-      context_vector.set_shape([None, num_units])
+        def __str__(self):
+        defaults = dict(type(self).__dict__)
+        actual = dict(defaults)
+        actual.update(self.__dict__)
+        actual['config'] = self.config
+        return repr_dict_nice(dict(
+            (key, value)
+            for key, value in actual.items()
+            if not key.startswith('_'))
+        )
     
-        def test_timeout(self):
-        self.assertTrue(self.timeout.timeout)
-        self.assertFalse(self.invalid.timeout)
-    
-        def setUp(self):
-        self.decoded = datetime.datetime(2015, 3, 27, tzinfo=pytz.utc)
-        self.encoded = '2015-03-27T00:00:00Z'
-    
-    AUTOHSTS_FREQ = 172800
-'''Minimum time since last increase to perform a new one: 48h'''
-    
-        # Round in the units appropriate
-    if precision == PRECISION_HALVES:
-        temperature = round(temperature * 2) / 2.0
-    elif precision == PRECISION_TENTHS:
-        temperature = round(temperature, 1)
-    # Integer as a fall back (PRECISION_WHOLE)
-    else:
-        temperature = round(temperature)
-    
-        return cls_boxes, cls_segms, cls_keyps
+            headers = [
+            '%s: %s' % (
+                name,
+                value if isinstance(value, str) else value.decode('utf8')
+            )
+            for name, value in headers.items()
+        ]
     
     
-def cityscapes_to_coco(cityscapes_id):
-    lookup = {
-        0: 0,  # ... background
-        1: 2,  # bicycle
-        2: 3,  # car
-        3: 1,  # person
-        4: 7,  # train
-        5: 8,  # truck
-        6: 4,  # motorcycle
-        7: 6,  # bus
-        8: -1,  # rider (-1 means rand init)
-    }
-    return lookup[cityscapes_id]
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+FILE_PATH_ARG = patharg(FILE_PATH)
+BIN_FILE_PATH_ARG = patharg(BIN_FILE_PATH)
+JSON_FILE_PATH_ARG = patharg(JSON_FILE_PATH)
     
     
-def evaluate_boxes(
-    json_dataset,
-    all_boxes,
-    output_dir,
-    use_salt=True,
-    cleanup=True,
-    use_matlab=False
-):
-    salt = '_{}'.format(str(uuid.uuid4())) if use_salt else ''
-    filenames = _write_voc_results_files(json_dataset, all_boxes, salt)
-    _do_python_eval(json_dataset, salt, output_dir)
-    if use_matlab:
-        _do_matlab_eval(json_dataset, salt, output_dir)
-    if cleanup:
-        for filename in filenames:
-            shutil.copy(filename, output_dir)
-            os.remove(filename)
-    return None
+def test_default_options(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http(httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['form'] == {'foo': 'bar'}
     
-    from detectron.core.config import cfg
-from detectron.utils.net import get_group_gn
+        def test_POST_explicit_JSON_auto_JSON_accept(self, httpbin):
+        r = http('--json', 'POST', httpbin.url + '/post')
+        assert HTTP_OK in r
+        assert r.json['headers']['Accept'] == JSON_ACCEPT
+        # Make sure Content-Type gets set even with no data.
+        # https://github.com/jakubroztocil/httpie/issues/137
+        assert 'application/json' in r.json['headers']['Content-Type']
     
+            plugin = plugin_manager.get_auth_plugin(auth['type'])()
     
-def get_new_name(func_name):
-    if func_name in _RENAME:
-        func_name = _RENAME[func_name]
-    return func_name
-
+        def report_speed(self):
     
-    logger = logging.getLogger(__name__)
+    import mock
     
-        shape = (sampled_fg_rois.shape[0] * cfg.KRCNN.NUM_KEYPOINTS, 1)
-    heats = heats.reshape(shape)
-    weights = weights.reshape(shape)
+    The JWS implementation in josepy only implements the base JOSE standard. In
+order to support the new header fields defined in ACME, this module defines some
+ACME-specific classes that layer on top of josepy.
+'''
+import josepy as jose
     
     
-def main(opts):
-    logger = logging.getLogger(__name__)
-    roidb = combined_roidb_for_training(
-        cfg.TRAIN.DATASETS, cfg.TRAIN.PROPOSAL_FILES)
-    logger.info('{:d} roidb entries'.format(len(roidb)))
-    roi_data_loader = RoIDataLoader(
-        roidb,
-        num_loaders=cfg.DATA_LOADER.NUM_THREADS,
-        minibatch_queue_size=cfg.DATA_LOADER.MINIBATCH_QUEUE_SIZE,
-        blobs_queue_capacity=cfg.DATA_LOADER.BLOBS_QUEUE_CAPACITY
-    )
-    blob_names = roi_data_loader.get_output_names()
+    def init_augeas(self):
+        ''' Initialize the actual Augeas instance '''
+        import augeas
+        self.aug = augeas.Augeas(
+            # specify a directory to load our preferred lens from
+            loadpath=constants.AUGEAS_LENS_DIR,
+            # Do not save backup (we do it ourselves), do not load
+            # anything by default
+            flags=(augeas.Augeas.NONE |
+                   augeas.Augeas.NO_MODL_AUTOLOAD |
+                   augeas.Augeas.ENABLE_SPAN))
+        # See if any temporary changes need to be recovered
+        # This needs to occur before VirtualHost objects are setup...
+        # because this will change the underlying configuration and potential
+        # vhosts
+        self.recovery_routine()
     
-    import os
-import sys
-import codecs
-import re
+    AUGEAS_LENS_DIR = pkg_resources.resource_filename(
+    'certbot_apache', 'augeas_lens')
+'''Path to the Augeas lens directory'''
     
-    define('num', default=10000, help='number of iterations')
+    .. code-block:: bash
+   :caption: To acquire a certificate for ``example.com``, waiting 60 seconds
+             for DNS propagation
     
-        All imported modules are watched by default.
+        s = score_euclidean(data[0], data[1])
+    print(s)
+    
+            if n_dim == 2:
+            return tf.nn.softmax(x)
+        else:
+            e = tf.exp(x - tf.reduce_max(x, axis=axis, keepdims=True))
+            s = tf.reduce_sum(e, axis=axis, keepdims=True)
+            return e / s
     '''
-    _watched_files.add(filename)
+    return tf.nn.softmax(x, axis=axis)
     
-    if typing.TYPE_CHECKING:
-    from typing import Set, Dict, Tuple  # noqa: F401
+    
+def relu6(x):
+    '''
+    `o = min(max(x, 0), 6)`
+    '''
+    return tf.nn.relu6(x)
+    
+            if use_mean_attention:
+            a = tf.reduce_mean(a, axis=1)  # [batch_size, n_step]
+            a = tf.expand_dims(a, axis=1)  # [batch_size, 1, n_step]
+            a = tf.tile(a, [1, n_input, 1])  # [batch_size, n_input, n_step]
+    
+    References:
+    tensorlayer.layers.Conv2dLayer
+'''
+import tensorflow as tf
+    
+        如果需要 reuse 推荐使用类实现的 `Dense`
+    
+    
+def get_shape(x):
+    '''
+    References:
+        tflearn.utils.get_incoming_shape
+    '''
+    if isinstance(x, (tf.Tensor, tf.SparseTensor)):
+        return x.get_shape().as_list()
+    elif type(x) in [np.array, np.ndarray, list, tuple]:
+        return list(np.shape(x))
+    else:
+        raise Exception('Invalid `x`.')
+    
+    References:
+    https://www.tensorflow.org/api_guides/python/array_ops
+    keras.backend
+'''
