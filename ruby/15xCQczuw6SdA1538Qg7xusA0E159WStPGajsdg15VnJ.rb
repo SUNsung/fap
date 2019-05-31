ce03e35c1312c7e1b52a3a616118892306c1946c
SUNsung <weatherius@gmail.com>
2019-05-31 04:14:12 +0300

    Rewrite [15xCQczuw6SdA1538Qg7xusA0E159WStPGajsdg15VnJ.rb] File in: 2019-05-31 04:14:11

--- ruby/15xCQczuw6SdA1538Qg7xusA0E159WStPGajsdg15VnJ.rb
+++ ruby/15xCQczuw6SdA1538Qg7xusA0E159WStPGajsdg15VnJ.rb
@@ -26,70 +26,102 @@
 end
 
     
-          def self.options
-        options = []
-        options.concat(super.reject { |option, _| option == '--silent' })
+        # we assume that the first file that requires 'sinatra' is the
+    # app_file. all other path related options are calculated based
+    # on this path by default.
+    set :app_file, caller_files.first || $0
+    
+            # Set these key values to boolean 'true' to include in policy
+        NO_ARG_DIRECTIVES.each do |d|
+          if options.key?(d) && options[d].is_a?(TrueClass)
+            directives << d.to_s.sub(/_/, '-')
+          end
+        end
+    
+          def escape_string(str)
+        str = @escaper.escape_url(str)        if @url
+        str = @escaper.escape_html(str)       if @html
+        str = @escaper.escape_javascript(str) if @javascript
+        str
       end
-    
-    def bump_version
-  old_file = File.read('lib/#{name}.rb')
-  old_version_line = old_file[/^\s*VERSION\s*=\s*.*/]
-  new_version = next_version
-  # replace first match of old vesion with new version
-  old_file.sub!(old_version_line, '  VERSION = '#{new_version}'')
-    
-        EMOJI_PATHNAME = Pathname.new(Gemojione.images_path).freeze
-    
-        def initialize(dir, existing, attempted, message = nil)
-      @dir            = dir
-      @existing_path  = existing
-      @attempted_path = attempted
-      super(message || 'Cannot write #{@dir}/#{@attempted_path}, found #{@dir}/#{@existing_path}.')
     end
   end
 end
+
     
-            link = if options[:icon]
-                 link_to_with_icon(
-                   options[:icon],
-                   titleized_label,
-                   destination_url,
-                   class: 'w-100 p-3 d-flex align-items-center'
-                 )
-               else
-                 link_to(
-                   titleized_label,
-                   destination_url,
-                   class: 'sidebar-submenu-item w-100 py-1 px-3 d-block #{'text-success' if selected}'
-                 )
-               end
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   CSRF
+    # Supported browsers:: all
+    # More infos::         http://flask.pocoo.org/docs/0.10/security/#json-security
+    #                      http://haacked.com/archive/2008/11/20/anatomy-of-a-subtle-json-vulnerability.aspx
+    #
+    # JSON GET APIs are vulnerable to being embedded as JavaScript when the
+    # Array prototype has been patched to track data. Checks the referrer
+    # even on GET requests if the content type is JSON.
+    #
+    # If request includes Origin HTTP header, defers to HttpOrigin to determine
+    # if the request is safe. Please refer to the documentation for more info.
+    #
+    # The `:allow_if` option can be set to a proc to use custom allow/deny logic.
+    class JsonCsrf < Base
+      default_options :allow_if => nil
     
-                  expect(order.shipments.count).to eq(1)
-              expect(order.shipments.first.inventory_units_for(product.master).sum(&:quantity)).to eq(2)
-              expect(order.shipments.first.stock_location.id).to eq(stock_location.id)
-            end
-          end
+          def has_sidebar
+        @sidebar = (@page.sidebar || false) if @sidebar.nil? && @page
+        !!@sidebar
+      end
     
-            def index
-          authorize! :index, Order
-          @orders = Order.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@orders)
+          def title
+        '#{@page.title}'
+      end
+    
+          def next_link
+        label = 'Next &raquo;'
+        if @versions.size == Gollum::Page.per_page
+          link = '/history/#{@page.name}?page=#{@page_num+1}'
+          %(<a href='#{link}' hotkey='l'>#{label}</a>)
+        else
+          %(<span class='disabled'>#{label}</span>)
         end
+      end
+    end
+  end
+end
+
     
-          @@option_value_attributes = [
-        :id, :name, :presentation, :option_type_name, :option_type_id,
-        :option_type_presentation
-      ]
+    def rubyforge_project
+  name
+end
     
-    chdir APP_ROOT do
-  # This script is a starting point to setup your application.
-  # Add necessary setup steps to this file.
+    exec         = {}
+options      = {
+  :port => 4567,
+  :bind => '0.0.0.0',
+}
+wiki_options = {
+  :live_preview  => false,
+  :allow_uploads => false,
+  :allow_editing => true,
+}
     
-      # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
+      s.add_development_dependency 'rack-test', '~> 0.6.2'
+  s.add_development_dependency 'shoulda', '~> 3.5.0'
+  s.add_development_dependency 'minitest-reporters', '~> 0.14.16'
+  s.add_development_dependency 'twitter_cldr', '~> 3.2.0'
+  s.add_development_dependency 'mocha', '~> 1.1.0'
+  s.add_development_dependency 'test-unit', '~> 3.1.0'
+  s.add_development_dependency 'webrick', '~> 1.3.1'
     
-      # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+      <form method='post' action='/msg'>
+    <input type='text' name='msg'>
+    <input type='submit' value='Add Message'>
+  </form>
+    
+        config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=#{2.days.to_i}'
+    }
+  else
+    config.action_controller.perform_caching = false
