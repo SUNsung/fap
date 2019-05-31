2ede16935ddf7330504e035f2aa9c3b85ad08286
SUNsung <weatherius@gmail.com>
2019-06-01 02:08:10 +0300

    Rewrite [06qnuhYGXwmQI06ymEE6u5RDJo0660m1X524ac06aG.cpp] File in: 2019-06-01 02:08:09

--- cccccc/06qnuhYGXwmQI06ymEE6u5RDJo0660m1X524ac06aG.cpp
+++ cccccc/06qnuhYGXwmQI06ymEE6u5RDJo0660m1X524ac06aG.cpp
@@ -1,44 +1,83 @@
 
         
-        #define INSTANTIATE_LAYER_GPU_FUNCS(classname) \
-  INSTANTIATE_LAYER_GPU_FORWARD(classname); \
-  INSTANTIATE_LAYER_GPU_BACKWARD(classname)
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/common.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    #endif  // CAFFE_LAYER_FACTORY_H_
-
-    
-    #endif  // CAFFE_ARGMAX_LAYER_HPP_
-
-    
-      int channel_axis_;
-  int num_;
-  int channels_;
-  int group_;
-  int out_spatial_dim_;
-  int weight_offset_;
-  int num_output_;
-  bool bias_term_;
-  bool is_1x1_;
-  bool force_nd_im2col_;
-    
-    
-    {  // extra temporarary variables is used to carry out sums/broadcasting
-  // using BLAS
-  Blob<Dtype> batch_sum_multiplier_;
-  Blob<Dtype> num_by_chans_;
-  Blob<Dtype> spatial_sum_multiplier_;
-};
+          // This random number generator facade hides boost and CUDA rng
+  // implementation from one another (for cross-platform compatibility).
+  class RNG {
+   public:
+    RNG();
+    explicit RNG(unsigned int seed);
+    explicit RNG(const RNG&);
+    RNG& operator=(const RNG&);
+    void* generator();
+   private:
+    class Generator;
+    shared_ptr<Generator> generator_;
+  };
     
      protected:
-  /// @copydoc BNLLLayer
+  /**
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the predictions @f$ x @f$, a Blob with values in
+   *      @f$ [-\infty, +\infty] @f$ indicating the predicted score for each of
+   *      the @f$ K = CHW @f$ classes. Each @f$ x_n @f$ is mapped to a predicted
+   *      label @f$ \hat{l}_n @f$ given by its maximal index:
+   *      @f$ \hat{l}_n = \arg\max\limits_k x_{nk} @f$
+   *   -# @f$ (N \times 1 \times 1 \times 1) @f$
+   *      the labels @f$ l @f$, an integer-valued Blob with values
+   *      @f$ l_n \in [0, 1, 2, ..., K - 1] @f$
+   *      indicating the correct class label among the @f$ K @f$ classes
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
+   *      the computed accuracy: @f$
+   *        \frac{1}{N} \sum\limits_{n=1}^N \delta\{ \hat{l}_n = l_n \}
+   *      @f$, where @f$
+   *      \delta\{\mathrm{condition}\} = \left\{
+   *         \begin{array}{lr}
+   *            1 & \mbox{if condition} \\
+   *            0 & \mbox{otherwise}
+   *         \end{array} \right.
+   *      @f$
+   */
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
+    }
+    
+    
+    { private:
+  struct pair_sort_first {
+    bool operator()(const std::pair<int, int> &left,
+                    const std::pair<int, int> &right) {
+      return left.first < right.first;
+    }
+  };
+  void check_batch_reindex(int initial_num, int final_num,
+                           const Dtype* ridx_data);
+};
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    #include <vector>
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+                           const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+                            const vector<bool>& propagate_down,
+                            const vector<Blob<Dtype>*>& bottom);
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #endif  // CAFFE_CUDNN_LRN_LAYER_HPP_
+
     
     #ifdef USE_CUDNN
 /*
@@ -60,116 +99,138 @@ class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
   virtual inline int ExactNumTopBlobs() const { return 1; }
     }
     
-    #include <vector>
+    #include 'rect.h'    // for TBOX
+#include 'strngs.h'  // for STRING
     
+    // Reads all boxes from the string. Otherwise, as ReadAllBoxes.
+// continue_on_failure allows reading to continue even if an invalid box is
+// encountered and will return true if it succeeds in reading some boxes.
+// It otherwise gives up and returns false on encountering an invalid box.
+bool ReadMemBoxes(int target_page, bool skip_blanks, const char* box_data,
+                  bool continue_on_failure,
+                  GenericVector<TBOX>* boxes,
+                  GenericVector<STRING>* texts,
+                  GenericVector<STRING>* box_texts,
+                  GenericVector<int>* pages);
     
-    {
-    {}  // namespace log
-}  // namespace leveldb
+      // Deletes the box with the given index, and shuffles up the rest.
+  // Recomputes the bounding box.
+  void DeleteBox(int index);
     
-    // Returns a new environment that stores its data in memory and delegates
-// all non-file-storage tasks to base_env. The caller must delete the result
-// when it is no longer needed.
-// *base_env must remain live while the result is in use.
-LEVELDB_EXPORT Env* NewMemEnv(Env* base_env);
+    #endif  // TESSERACT_CCMAIN_OTSUTHR_H_
+
     
-      // Return the ith key
-  Slice Key(int i, std::string* storage) {
-    char buf[100];
-    snprintf(buf, sizeof(buf), '%016d', i);
-    storage->assign(buf, strlen(buf));
-    return Slice(*storage);
+    // A useful base class to facilitate the common operation of sorting a vector
+// of owned pointer data using a separate key. This class owns its data pointer,
+// deleting it when it has finished with it, and providing copy constructor and
+// operator= that have move semantics so that the data does not get copied and
+// only a single instance of KDPtrPair holds a specific data pointer.
+template <typename Key, typename Data>
+class KDPtrPair {
+ public:
+  KDPtrPair() : data_(nullptr) {}
+  KDPtrPair(Key k, Data* d) : data_(d), key_(k) {}
+  // Copy constructor steals the pointer from src and nulls it in src, thereby
+  // moving the (single) ownership of the data.
+  KDPtrPair(KDPtrPair& src) : data_(src.data_), key_(src.key_) {
+    src.data_ = nullptr;
   }
-    
-      // Returns an estimate of the number of bytes of data in use by this
-  // data structure. It is safe to call when MemTable is being modified.
-  size_t ApproximateMemoryUsage();
-    
-      // Check that append works.
-  ASSERT_OK(env_->NewAppendableFile('/dir/f', &writable_file));
-  ASSERT_OK(env_->GetFileSize('/dir/f', &file_size));
-  ASSERT_EQ(3, file_size);
-  ASSERT_OK(writable_file->Append('hello'));
-  delete writable_file;
-    
-    namespace leveldb {
+  // Destructor deletes data, assuming it is the sole owner.
+  ~KDPtrPair() {
+    delete this->data_;
+    this->data_ = nullptr;
+  }
+  // Operator= steals the pointer from src and nulls it in src, thereby
+  // moving the (single) ownership of the data.
+  void operator=(KDPtrPair& src) {
+    delete this->data_;
+    this->data_ = src.data_;
+    src.data_ = nullptr;
+    this->key_ = src.key_;
+  }
     }
     
-     private:
-  char* AllocateFallback(size_t bytes);
-  char* AllocateNewBlock(size_t block_bytes);
-    
-      virtual void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) {
-    param_.InitAllowUnknown(kwargs);
-    base_->Init(kwargs);
-    rnd_.seed(kRandMagic + param_.seed);
-    outimg_.set_pad(false);
-    meanimg_.set_pad(false);
-    if (param_.mean_img.length() != 0) {
-      std::unique_ptr<dmlc::Stream> fi(
-          dmlc::Stream::Create(param_.mean_img.c_str(), 'r', true));
-      if (fi.get() == nullptr) {
-        this->CreateMeanImg();
-      } else {
-        fi.reset(nullptr);
-        if (param_.verbose) {
-          LOG(INFO) << 'Load mean image from ' << param_.mean_img;
+      // Return a pointer to the object identified by id.
+  // If we haven't yet loaded the object, use loader to load it.
+  // If loader fails to load it, record a nullptr entry in the cache
+  // and return nullptr -- further attempts to load will fail (even
+  // with a different loader) until DeleteUnusedObjects() is called.
+  // We delete the given loader.
+  T *Get(STRING id,
+         TessResultCallback<T *> *loader) {
+    T *retval = nullptr;
+    mu_.Lock();
+    for (int i = 0; i < cache_.size(); i++) {
+      if (id == cache_[i].id) {
+        retval = cache_[i].object;
+        if (cache_[i].object != nullptr) {
+          cache_[i].count++;
         }
-        // use python compatible ndarray store format
-        std::vector<NDArray> data;
-        std::vector<std::string> keys;
-        {
-          std::unique_ptr<dmlc::Stream> fi(dmlc::Stream::Create(param_.mean_img.c_str(), 'r'));
-          NDArray::Load(fi.get(), &data, &keys);
-        }
-        CHECK_EQ(data.size(), 1U)
-            << 'Invalid mean image file format';
-        data[0].WaitToRead();
-        mshadow::Tensor<cpu, 3> src = data[0].data().get<cpu, 3, real_t>();
-        meanimg_.Resize(src.shape_);
-        mshadow::Copy(meanimg_, src);
-        meanfile_ready_ = true;
+        mu_.Unlock();
+        delete loader;
+        return retval;
       }
     }
+    cache_.push_back(ReferenceCount());
+    ReferenceCount &rc = cache_.back();
+    rc.id = id;
+    retval = rc.object = loader->Run();
+    rc.count = (retval != nullptr) ? 1 : 0;
+    mu_.Unlock();
+    return retval;
   }
     
-      ~PrefetcherIter() {
-    while (recycle_queue_.size() != 0) {
-      DataBatch *batch = recycle_queue_.front();
-      recycle_queue_.pop();
-      delete batch;
-    }
-    delete out_;
-    iter.Destroy();
+    #ifndef PLUGIN_CAFFE_CAFFE_COMMON_H_
+#define PLUGIN_CAFFE_CAFFE_COMMON_H_
+    
+    KVStore* KVStore::Create(const char *type_name) {
+  std::string tname = type_name;
+  std::transform(tname.begin(), tname.end(), tname.begin(), ::tolower);
+  KVStore* kv = nullptr;
+  bool use_device_comm = false;
+  auto has = [tname](const std::string& pattern) {
+    return tname.find(pattern) != std::string::npos;
+  };
+  if (has('device')) {
+    use_device_comm = true;
   }
-    
-    namespace mxnet {
     }
-    
-    Crop the 2nd and 3rd dim of input data, with the corresponding size of h_w or
-with width and height of the second input symbol, i.e., with one input, we need h_w to
-specify the crop height and width, otherwise the second input symbol's size will be used
-)code' ADD_FILELINE)
     
     
     {
-    {
-    {  bool init_cudnn_;
-  cudnnDataType_t dtype_;
-  cudnnSpatialTransformerDescriptor_t st_desc_;
-  cudnnTensorDescriptor_t in_desc_;
-  cudnnTensorDescriptor_t out_desc_;
-  cudnnSamplerType_t sampler_;
-  #if CUDNN_MAJOR >= 5
-  cudnnTensorFormat_t format_;
-  #endif
-  SpatialTransformerParam param_;
-};
-#endif  // __CUDACC__ && CUDNN
+    {.add_argument('data', 'Symbol or Symbol[]', 'Tensor or List of Tensors, the second input '
+'will be used as crop_like shape reference')
+.add_arguments(CropParam::__FIELDS__())
+.set_key_var_num_args('num_args');
 }  // namespace op
 }  // namespace mxnet
+
+    
+    namespace mxnet {
+namespace op {
+template<>
+Operator *CreateOp<cpu>(NativeOpParam param) {
+  return new NativeOp<cpu>(param);
+}
+    }
+    }
+    
+    public:
+  DHTResponseMessage(const std::shared_ptr<DHTNode>& localNode,
+                     const std::shared_ptr<DHTNode>& remoteNode,
+                     const std::string& transactionID);
     
     
-    {
-    {}  // namespace op
-}  // namespace mxnet
+    {} // namespace aria2
+
+    
+      Time getSerializedTime() const { return serializedTime_; }
+    
+    void DHTTaskQueueImpl::addPeriodicTask2(const std::shared_ptr<DHTTask>& task)
+{
+  periodicTaskQueue2_.addTask(task);
+}
+    
+      virtual void process() CXX11_OVERRIDE;
+    
+    bool DHTUnknownMessage::send() { return true; }
