4948bab68b87347665e71fa38f84e32d06f3d380
SUNsung <weatherius@gmail.com>
2019-06-01 17:43:12 +0300

    Rewrite [12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp] File in: 2019-06-01 17:43:11

--- cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
+++ cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
@@ -1,279 +1,238 @@
 
         
-                //  Create the Pseudo Console and pipes to it
-        HANDLE hPipeIn{ INVALID_HANDLE_VALUE };
-        HANDLE hPipeOut{ INVALID_HANDLE_VALUE };
-        hr = CreatePseudoConsoleAndPipes(&hPC, &hPipeIn, &hPipeOut);
-        if (S_OK == hr)
-        {
-            // Create & start thread to listen to the incoming pipe
-            // Note: Using CRT-safe _beginthread() rather than CreateThread()
-            HANDLE hPipeListenerThread{ reinterpret_cast<HANDLE>(_beginthread(PipeListener, 0, hPipeIn)) };
+        namespace {
     }
     
-    // Internal macro for implementing {EXPECT|ASSERT}_PRED_FORMAT5.
-// Don't use this in your code.
-#define GTEST_PRED_FORMAT5_(pred_format, v1, v2, v3, v4, v5, on_failure)\
-  GTEST_ASSERT_(pred_format(#v1, #v2, #v3, #v4, #v5, v1, v2, v3, v4, v5), \
-                on_failure)
+    namespace atom {
+    }
     
-    // A concrete DeathTestFactory implementation for normal use.
-class DefaultDeathTestFactory : public DeathTestFactory {
+      v8::Local<v8::Value> URLRequest(v8::Isolate* isolate);
+    
+    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
  public:
-  virtual bool Create(const char* statement, const RE* regex,
-                      const char* file, int line, DeathTest** test);
+  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
+  ~AtomJavaScriptDialogManager() override;
+    }
+    
+    
+    {  DISALLOW_IMPLICIT_CONSTRUCTORS(AutoUpdater);
 };
     
-      // Formats an int value as '%X'.
-  static std::string FormatHexInt(int value);
+    #if defined(OS_LINUX)
+typedef PowerObserverLinux PowerObserver;
+#else
+typedef base::PowerObserver PowerObserver;
+#endif  // defined(OS_LINUX)
     
-      // Gets the first element of the queue, or NULL if the queue is empty.
-  QueueNode<E>* Head() { return head_; }
-  const QueueNode<E>* Head() const { return head_; }
-    
-        static std::wstring GetTrainerStateCheckpointFilePath(const std::wstring& modelFilePath)
-    {
-        const wchar_t* checkpointExt = L'.ckp';
-        return modelFilePath + checkpointExt;
-    }
-    
-    #include 'stdafx.h'
-#include 'CNTKLibrary.h'
-    
-    
-    {        // Creating Composite Data Reader that allow to combine deserializers.
-        // This should be changed to link statically when SGD uses the new interfaces.
-        wstring ioName = L'ioName';
-        GetReaderProc getReaderProc = (GetReaderProc)Plugin::Load(readerType, GetReaderName(precision));
-        m_ioNames.push_back(ioName);
-        assert(getReaderProc != nullptr);
-        getReaderProc(&m_dataReaders[ioName]);
-    }
-    else
-    {
-        wstring readerType = config(L'readerType', L'Cntk.Deserializers.TextFormat');
-        wstring ioName = L'ioName';
-        // backward support to use only one type of data reader
-        // get the name for the reader we want to use, default to CNTKTextFormatReader
-        GetReaderProc getReaderProc = (GetReaderProc)Plugin::Load(readerType, GetReaderName(precision));
-        m_ioNames.push_back(ioName);
-        assert(getReaderProc != nullptr);
-        getReaderProc(&m_dataReaders[ioName]);
-    }
-    
-    // -----------------------------------------------------------------------
-// (deprecated) PerDimMeanVarNormalizationNode (feature, mean, invStdDev)
-// Computes
-//   output = (feature - mean) .* invStdDev
-// where mean and invStdDev are meant to be single elements while features
-// is minibatch data.
-// Deprecated since it can be trivially expressed in BrainScript.
-// -----------------------------------------------------------------------
-    
-            // Edit distance between subsequences
-        Matrix<float> grid(CPUDEVICE);
-        
-        // Number of insertions between subsequences
-        Matrix<float> insMatrix(CPUDEVICE);
-        
-        //Number of deletions between subsequences
-        Matrix<float> delMatrix(CPUDEVICE);
-    
-        virtual void Save(File& fstream) const override
-    {
-        Base::Save(fstream);
-        fstream << m_isOutputSparse;
-    }
-    
-        template <class ElemType>
-    void OptimizedMemoryAllocationFunc()
-    {
-        vector<MemRequestInfo<ElemType>>& memInfoVec = GetMemRequestInfoVec<ElemType>();
-        if (memInfoVec.empty())
-            return; 
-    }
-    
-    
-    {
-    {    inline Coroutine* pop_coroutine(enum opcode type)
-    {
-        Coroutine* co;
-        if (type == PRODUCER)
-        {
-            co = producer_queue.front();
-            producer_queue.pop_front();
-            swTraceLog(SW_TRACE_CHANNEL, 'resume producer cid=%ld', co->get_cid());
-        }
-        else // if (type == CONSUMER)
-        {
-            co = consumer_queue.front();
-            consumer_queue.pop_front();
-            swTraceLog(SW_TRACE_CHANNEL, 'resume consumer cid=%ld', co->get_cid());
-        }
-        return co;
-    }
-};
-};
-
-    
-        for (i = 0; i < MAP_SIZE; i++)
-    {
-        pkt = (swFdInfo *) malloc(sizeof(swFdInfo));
-        pkt->key = i;
-        pkt->fd = i * 37;
-        swHashMap_add_int(ht, pkt->fd, pkt);
-        lists[i] = pkt;
-    }
-    
-        SwooleG.main_reactor = (swReactor *) sw_malloc(sizeof(swReactor));
-    swReactor_create(SwooleG.main_reactor, SW_REACTOR_MAXEVENTS);
-    
-            if (pid == 0)
-        {
-            usleep(100000);
-            exit(0);
-        }
-    
-    
-    {    unlink(sock1_path);
-    unlink(sock2_path);
-}
-#endif
-
-    
-    
-    {    ASSERT_EQ(swThreadPool_free(&pool), SW_OK);
-    ASSERT_EQ(result, N);
-}
-#endif
-    
-    void swoole_throw_error(int code)
-{
-    throw swoole::Exception(code);
-}
-
-    
-     THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-    
-    
-    {
-    {
-    {            p.z = (r * ( 1 - cosBeta ) * cosTheta);// '100' didn't work for
-            p.x = p.z * sinf(rotateByYAxis) + p.x * cosf(rotateByYAxis);
-            p.z = p.z * cosf(rotateByYAxis) - p.x * sinf(rotateByYAxis);
-            p.z/=7;
-            //    Stop z coord from dropping beneath underlying page in a transition
-            // issue #751
-            if( p.z < 0.5f )
-            {
-                p.z = 0.5f;
-            }
-            
-            // Set new coords
-            p.x += getGridRect().origin.x;
-            setVertex(Vec2(i, j), p);
-            
-        }
-    }
-}
-    
-    /**
- @brief This action simulates a page turn from the bottom right hand corner of the screen.
- 
- @details It's not much use by itself but is used by the PageTurnTransition.
-         Based on an original paper by L Hong et al.
-         http://www.parc.com/publication/1638/turning-pages-of-3d-electronic-books.html
-  
- @since v0.8.2
- */
-class CC_DLL PageTurn3D : public Grid3DAction
-{
-public:
-    /**
-     * @js NA 
-     */
-    virtual GridBase* getGrid() override;
-    }
-    
-    bool Animation::initWithSpriteFrames(const Vector<SpriteFrame*>& frames, float delay/* = 0.0f*/, unsigned int loops/* = 1*/)
-{
-    _delayPerUnit = delay;
-    _loops = loops;
-    }
-    
-        /** Gets the units of time the frame takes.
-     *
-     * @return The units of time the frame takes.
-     */
-    float getDelayUnits() const { return _delayUnits; };
-    
-    /** Sets the units of time the frame takes.
-     *
-     * @param delayUnits The units of time the frame takes.
-     */
-    void setDelayUnits(float delayUnits) { _delayUnits = delayUnits; };
-    
-    /** @brief Gets user information
-     * A AnimationFrameDisplayedNotification notification will be broadcast when the frame is displayed with this dictionary as UserInfo. 
-     * If UserInfo is nil, then no notification will be broadcast.
-     *
-     * @return A dictionary as UserInfo
-     */
-    const ValueMap& getUserInfo() const { return _userInfo; };
-    ValueMap& getUserInfo() { return _userInfo; };
-    
-    /** Sets user information.
-     * @param userInfo A dictionary as UserInfo.
-     */
-    void setUserInfo(const ValueMap& userInfo)
-    {
-        _userInfo = userInfo;
-    }
-    
-    // Overrides
-    virtual AnimationFrame *clone() const override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    /**
-     * @js ctor
-     */
-    AnimationFrame();
-    /**
-     * @js NA
-     * @lua NA
-     */
-    virtual ~AnimationFrame();
-    
-    /** initializes the animation frame with a spriteframe, number of delay units and a notification user info */
-    bool initWithSpriteFrame(SpriteFrame* spriteFrame, float delayUnits, const ValueMap& userInfo);
-    
-    ClippingRectangleNode* ClippingRectangleNode::create(const Rect& clippingRegion)
-{
-    ClippingRectangleNode* node = new (std::nothrow) ClippingRectangleNode();
-    if (node && node->init()) {
-        node->setClippingRegion(clippingRegion);
-        node->autorelease();
+      // Up until now, the parent process was blocked in a read waiting for the
+  // write above to complete. The parent process is now free to exit. Wait for
+  // that to happen.
+  struct kevent event;
+  int events = kevent(kq.get(), nullptr, 0, &event, 1, nullptr);
+  if (events != 1) {
+    if (events < 0) {
+      PLOG(ERROR) << 'kevent (monitor)';
     } else {
-        CC_SAFE_DELETE(node);
+      LOG(ERROR) << 'kevent (monitor): unexpected result ' << events;
+    }
+    return;
+  }
+    
+      // views::View:
+  void Layout() override;
+  gfx::Size GetMinimumSize() const override;
+  gfx::Size GetMaximumSize() const override;
+    
+    namespace atom {
     }
     
-    return node;
+        if (node->Left) {
+      IndentScope ms(this, (childKind == ChildKind::Left ||
+                            childKind == ChildKind::Root) ? '  ' : '| ');
+      print(node->Left, ChildKind::Left);
+    }
+    
+    swift::unicode::GraphemeClusterBreakProperty
+swift::unicode::getGraphemeClusterBreakProperty(uint32_t C) {
+  // FIXME: replace linear search with a trie lookup.
+    }
+    
+    
+    {public:
+  Windows(const Driver &D, const llvm::Triple &Triple) : ToolChain(D, Triple) {}
+  ~Windows() = default;
+  std::string sanitizerRuntimeLibName(StringRef Sanitizer,
+                                      bool shared = true) const override;
+};
+    
+    Optional<FrontendInputsAndOutputs> ArgsToFrontendInputsConverter::convert(
+    SmallVectorImpl<std::unique_ptr<llvm::MemoryBuffer>> *buffers) {
+  SWIFT_DEFER {
+    if (buffers) {
+      std::move(ConfigurationFileBuffers.begin(),
+                ConfigurationFileBuffers.end(),
+                std::back_inserter(*buffers));
+      // Clearing the original list of buffers isn't strictly necessary, but
+      // makes the behavior more sensible if we were to call convert() again.
+      ConfigurationFileBuffers.clear();
+    }
+  };
+    }
+    
+        typedef ptrdiff_t  stride_t;
+    
+    #include 'common.hpp'
+#include 'vtransform.hpp'
+    
+    
+    {    void operator() (const u32 * src0, const u32 * src1, u32 * dst) const
+    {
+        dst[0] = saturate_cast<u32>(alpha*src0[0] + beta*src1[0] + gamma);
+    }
+};
+    
+    void blur3x3(const Size2D &size,
+             const u8 * srcBase, ptrdiff_t srcStride,
+             u8 * dstBase, ptrdiff_t dstStride,
+             BORDER_MODE border, u8 borderValue)
+{
+    internal::assertSupportedConfiguration(isBlur3x3Supported(size, border));
+#ifdef CAROTENE_NEON
+    const int16x8_t v_scale = vmovq_n_s16(3640);
+    const uint16x8_t v_border_x3 = vdupq_n_u16(borderValue * 3);
+    const uint16x8_t v_zero = vdupq_n_u16(0);
+    const uint8x8_t v_border = vdup_n_u8(borderValue);
+    }
+    
+    #endif //CAROTENE_NEON
+    
+                uint32x4_t vlx1 = vmvnq_u32(vequ1);
+            uint32x4_t vlx2 = vmvnq_u32(vequ2);
+    
+            const s16* ln0 = idx_rm2 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rm2) : tmp;
+        const s16* ln1 = idx_rm1 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rm1) : tmp;
+        const s16* ln2 = internal::getRowPtr(srcBase, srcStride, i);
+        const s16* ln3 = idx_rp1 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rp1) : tmp;
+        const s16* ln4 = idx_rp2 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rp2) : tmp;
+    
+    
+    {} // namespace CAROTENE_NS
+
+    
+    
+    {            u32 buf[8];
+            vst1_u32(buf, vget_low_u32(el8shr01l));
+            vst1_u32(buf+2, el2l);
+            vst1_u32(buf+4, el2hl);
+            vst1_u32(buf+6, el2hh);
+            for(u32 k=0; k < 8; k++)
+                sqsum[j+k] = prev + prevSqSum[j+k] + buf[k];
+            prev += buf[7];
+        }
+    
+    inline uint8x8_t vqtbl1_u8 (uint8x16_t a, uint8x8_t b)
+{
+#ifdef __aarch64__
+    // AArch64 supports this natively
+    return ::vqtbl1_u8(a, b);
+#else
+    union { uint8x16_t v; uint8x8x2_t w; } u = { a };
+    return vtbl2_u8(u.w, b);
+#endif
 }
     
+    struct TemporaryFile {
+  std::string path;
+    }
     
+    namespace {
+    }
     
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+              vector<TensorShape> out(1);
+          switch (order) {
+            case StorageOrder::NCHW:
+              out[0] = CreateTensorShape(
+                  vector<int>{N, C * kernel_h * kernel_w, out_h, out_w},
+                  TensorProto::FLOAT);
+              break;
+            case StorageOrder::NHWC:
+              out[0] = CreateTensorShape(
+                  vector<int>{N, out_h, out_w, kernel_h * kernel_w * C},
+                  TensorProto::FLOAT);
+              break;
+            default:
+              CAFFE_THROW('Unknown storage order: ', order);
+          }
+    
+    namespace caffe2 {
+    }
+    
+    #ifndef B2_TRIANGLE_H
+#define B2_TRIANGLE_H
+    
+    namespace Etc
+{
+	// ----------------------------------------------------------------------------------------------------
+	// C-style inteface to the encoder
+	//
+	void Encode(float *a_pafSourceRGBA,
+				unsigned int a_uiSourceWidth, 
+				unsigned int a_uiSourceHeight,
+				Image::Format a_format,
+				ErrorMetric a_eErrMetric,
+				float a_fEffort,
+				unsigned int a_uiJobs,
+				unsigned int a_uiMaxJobs,
+				unsigned char **a_ppaucEncodingBits,
+				unsigned int *a_puiEncodingBitsBytes,
+				unsigned int *a_puiExtendedWidth,
+				unsigned int *a_puiExtendedHeight, 
+				int *a_piEncodingTime_ms, bool a_bVerboseOutput)
+	{
+    }
+    }
+    
+    	// ----------------------------------------------------------------------------------------------------
+	// calculate the error for the block by summing the pixel errors
+	//
+	void Block4x4Encoding::CalcBlockError(void)
+	{
+		m_fError = 0.0f;
+    }
+    
+    		TryDifferential(m_boolMostLikelyFlip, 1, -2, 0);
+		TryDifferential(m_boolMostLikelyFlip, 1, 2, 0);
+		TryDifferential(m_boolMostLikelyFlip, 1, 0, 2);
+		TryDifferential(m_boolMostLikelyFlip, 1, 0, -2);
+    
+    /* a32 + (b32 * (opus_int32)((opus_int16)(c32))) >> 16 output have to be 32bit int */
+#undef silk_SMLAWB
+#define silk_SMLAWB(a, b, c) ((a) + silk_SMULWB(b, c))
+    
+    AuthPropertyIterator::AuthPropertyIterator()
+    : property_(nullptr), ctx_(nullptr), index_(0), name_(nullptr) {}
+    
+    Status ProtoServerReflection::ListService(ServerContext* context,
+                                          ListServiceResponse* response) {
+  if (services_ == nullptr) {
+    return Status(StatusCode::NOT_FOUND, 'Services not found.');
+  }
+  for (auto it = services_->begin(); it != services_->end(); ++it) {
+    ServiceResponse* service_response = response->add_service();
+    service_response->set_name(*it);
+  }
+  return Status::OK;
+}
+    
+    namespace grpc {
+namespace {
+    }
+    }
+    
+    #include <utility>
+    
+    using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::nanoseconds;
+using std::chrono::seconds;
+using std::chrono::system_clock;
