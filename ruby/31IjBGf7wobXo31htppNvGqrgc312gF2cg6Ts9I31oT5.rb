103a44769553147a768fa6fef2891b9dfb28c744
SUNsung <weatherius@gmail.com>
2019-06-01 07:41:16 +0300

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2019-06-01 07:41:15

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,34 +1,71 @@
 
         
-                # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
-            end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
-          end
-        end
+              it 'adds docset_feed_url param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            docset_feed_url: 'http://docset-feed-url.com'
+          )
+        end').runner.execute(:test)
+    
+        os = 'windows'
+    shelljoin_testcases.each do |testcase|
+      it testcase['it'] + ': ' + testcase['it_result'][os] do
+        array = testcase['input']
+        expect_correct_implementation_to_be_called(array, :shelljoin, os)
+        joined = array.shelljoin
+        expect(joined).to eq(testcase['expect'][os])
       end
     end
   end
-end
-
     
-        it 'accepts jsfiddle link with a / at the end' do
-      jsfiddle_link = 'http://jsfiddle.net/link2twenty/v2kx9jcd/'
-      expect do
-        generate_new_liquid(jsfiddle_link)
-      end.not_to raise_error
+      puts '== Installing dependencies =='
+  system! 'gem install bundler --conservative'
+  system('bundle check') || system!('bundle install')
+    
+      # Do not eager load code on boot.
+  config.eager_load = false
+    
+        context 'when user does not have access to report' do
+      let(:report_id) { 3 }
+      
+      it { should be false }
     end
+  end
+end
+    
+        it { should eq 'foo' }
+  end
+    
+      sub_test_case 'max_per_page' do
+    test 'by default' do
+      assert_nil Kaminari.config.max_per_page
+    end
+    test 'configure via config block' do
+      begin
+        Kaminari.configure {|c| c.max_per_page = 100}
+        assert_equal 100, Kaminari.config.max_per_page
+      ensure
+        Kaminari.configure {|c| c.max_per_page = nil}
+      end
+    end
+  end
+    
+        # Public: Filename
+    #
+    # Examples
+    #
+    #   Blob.new('/path/to/linguist/lib/linguist.rb', '').path
+    #   # =>  '/path/to/linguist/lib/linguist.rb'
+    #
+    # Returns a String
+    attr_reader :path
+    
+            # If the option-list begins with `set ` or `se `, it indicates an alternative
+        # modeline syntax partly-compatible with older versions of Vi. Here, the colon
+        # serves as a terminator for an option sequence, delimited by whitespace.
+        (?=
+          # So we have to ensure the modeline ends with a colon
+          : (?=[ \t]* set? [ \t] [^\n:]+ :) |
