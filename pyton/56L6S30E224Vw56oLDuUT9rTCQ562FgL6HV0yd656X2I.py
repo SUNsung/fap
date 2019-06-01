1de214d28da45a2f3844a86cd60e0888afbd1722
SUNsung <weatherius@gmail.com>
2019-06-01 06:52:11 +0300

    Rewrite [56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py] File in: 2019-06-01 06:52:11

--- pyton/56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py
+++ pyton/56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py
@@ -1,12 +1,70 @@
 
         
-                jsi = JSInterpreter('function f(){var x = 20; x += 30 + 1; return x;}')
-        self.assertEqual(jsi.call_function('f'), 51)
+        
+class Formatting(object):
+    '''A delegate class that invokes the actual processors.'''
+    
+            super(EncodedStream, self).__init__(**kwargs)
     
     
-    class BkgdGreyscale(KaitaiStruct):
-        def __init__(self, _io, _parent=None, _root=None):
-            self._io = _io
-            self._parent = _parent
-            self._root = _root if _root else self
-            self.value = self._io.read_u2be()
+def test_default_options_overwrite(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['json'] == {'foo': 'bar'}
+    
+        def test_GET_explicit_JSON_explicit_headers(self, httpbin):
+        r = http('--json', 'GET', httpbin.url + '/headers',
+                 'Accept:application/xml',
+                 'Content-Type:application/xml')
+        assert HTTP_OK in r
+        assert ''Accept': 'application/xml'' in r
+        assert ''Content-Type': 'application/xml'' in r
+    
+    from utils import TESTS_ROOT
+    
+    
+@mock.patch('httpie.core.get_response')
+def test_error_traceback(get_response):
+    exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    with raises(ConnectionError):
+        main(['--ignore-stdin', '--traceback', 'www.google.com'])
+    
+        return last_byte_pos + 1
+    
+        if not HAS_BOTO:
+        module.fail_json(msg='boto required for this module')
+    
+        changed = False
+    if state == 'present':
+        if not ipa_dnszone:
+            changed = True
+            if not module.check_mode:
+                client.dnszone_add(zone_name=zone_name)
+        else:
+            changed = False
+    else:
+        if ipa_dnszone:
+            changed = True
+            if not module.check_mode:
+                client.dnszone_del(zone_name=zone_name)
+    
+    ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+    
+    
+- name: Remove device from CA Spectrum
+  local_action:
+    module: spectrum_device
+    device: '{{ ansible_host }}'
+    landscape: '{{ landscape_handle }}'
+    oneclick_url: http://oneclick.example.com:8080
+    oneclick_user: username
+    oneclick_password: password
+    use_proxy: no
+    state: absent
+'''
