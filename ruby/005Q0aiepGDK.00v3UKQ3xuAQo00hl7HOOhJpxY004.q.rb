4678f4b1d026abe5915d80f70e3e6f7431bc44aa
SUNsung <weatherius@gmail.com>
2019-06-01 03:20:35 +0300

    Rewrite [005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb] File in: 2019-06-01 03:20:34

--- ruby/005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb
+++ ruby/005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb
@@ -1,74 +1,100 @@
 
         
-        module ActionView
-  module Helpers
-    module Tags # :nodoc:
-      class CollectionRadioButtons < Base # :nodoc:
-        include CollectionHelpers
+        DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
     
-          test 'when layout is overwritten by false in render, render no layout' do
-        controller = WithString.new
-        controller.process(:overwrite_false)
-        assert_equal 'Hello string!', controller.response_body
+    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
+COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
+    
+        SiteSetting.traditional_markdown_linebreaks = true
+    SiteSetting.enable_markdown_typographer = false
+    
+        delegate :base_url, to: :ContentSecurityPolicy
+    
+        Extension.theme_extensions(theme_ids).each { |extension| builder << extension }
+    Extension.plugin_extensions.each { |extension| builder << extension }
+    builder << Extension.site_setting_extension
+    
+    class ContentSecurityPolicy
+  class Middleware
+    def initialize(app)
+      @app = app
+    end
+    
+      describe 'report-uri' do
+    it 'is enabled by SiteSetting' do
+      SiteSetting.content_security_policy_collect_reports = true
+      report_uri = parse(policy)['report-uri'].first
+      expect(report_uri).to eq('http://test.localhost/csp_reports')
+    
+          log(action: changed_rows == 0 ? 'seen wrong token' : 'seen token',
+          user_auth_token_id: user_token.id,
+          user_id: user_token.user_id,
+          auth_token: user_token.auth_token,
+          user_agent: opts && opts[:user_agent],
+          path: opts && opts[:path],
+          client_ip: opts && opts[:client_ip])
+    end
+    
+      end
+end
+    
+    module Devise
+  module Controllers
+    # A module that may be optionally included in a controller in order
+    # to provide remember me behavior. Useful when signing in is done
+    # through a callback, like in OmniAuth.
+    module Rememberable
+      # Return default cookie values retrieved from session options.
+      def self.cookie_values
+        Rails.configuration.session_options.slice(:path, :domain, :secure)
       end
     
-          def link_to_edit(resource, options = {})
-        url = options[:url] || edit_object_url(resource)
-        options[:data] = { action: 'edit' }
-        options[:class] = 'btn btn-primary btn-sm'
-        link_to_with_icon('edit', Spree.t(:edit), url, options)
-      end
+        alias :name :singular
     
-          before do
-        order1.promotions << promotion
-        order1.save
-        visit spree.admin_orders_path
-      end
+    # Exit cleanly from an early interrupt
+Signal.trap('INT') { exit 1 }
     
-    desc 'Generates a dummy app for testing for every Spree engine'
-task :test_app do
-  SPREE_GEMS.each do |gem_name|
-    Dir.chdir('#{File.dirname(__FILE__)}/#{gem_name}') do
-      sh 'rake test_app'
+        puts('Packaging plugins for offline usage')
+    
+            PluginManager.ui.debug('Looking if package named: #{plugin_name} exists at #{uri}')
+    
+          PluginManager.ui.info('Installing file: #{local_file}')
+      uncompressed_path = uncompress(local_file)
+      PluginManager.ui.debug('Pack uncompressed to #{uncompressed_path}')
+      pack = LogStash::PluginManager::PackInstaller::Pack.new(uncompressed_path)
+      raise PluginManager::InvalidPackError, 'The pack must contains at least one plugin' unless pack.valid?
+    
+          puts user_feedback_string_for('halting', args[:platform], machines, {'experimental' => experimental})
+      options = {:debug => ENV['LS_QA_DEBUG']}
+    
+              def serialized_current_order
+            serialize_order(spree_current_order)
+          end
+        end
+      end
     end
   end
 end
+
     
-                if @order.completed? || @order.next
-              state_callback(:after)
-              respond_with(@order, default_template: 'spree/api/v1/orders/show')
-            else
-              respond_with(@order, default_template: 'spree/api/v1/orders/could_not_transition', status: 422)
-            end
-          else
-            invalid_resource!(@order)
+            def line_items_attributes
+          { line_items_attributes: {
+            id: params[:id],
+            quantity: params[:line_item][:quantity],
+            options: line_item_params[:options] || {}
+          } }
+        end
+    
+            def find_order(lock = false)
+          @order = Spree::Order.lock(lock).find_by!(number: params[:id])
+        end
+    
+              def serialize_payment_methods(payment_methods)
+            payment_methods_serializer.new(payment_methods).serializable_hash
           end
-        end
     
-              if @product.errors.empty?
-            respond_with(@product.reload, status: 200, default_template: :show)
-          else
-            invalid_resource!(@product)
-          end
-        end
-    
-          rtn = ''
-      (context.environments.first['site'][@array_name] || []).each do |file|
-        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
-          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
-        end
-    
-    module OctopressFilters
-  def self.pre_filter(page)
-    if page.ext.match('html|textile|markdown|md|haml|slim|xml')
-      input = BacktickCodeBlock::render_code_block(page.content)
-      page.content = input.gsub /(<figure.+?>.+?<\/figure>)/m do
-        TemplateWrapper::safe_wrap($1)
-      end
-    end
-  end
-  def self.post_filter(page)
-    if page.ext.match('html|textile|markdown|md|haml|slim|xml')
-      page.output = TemplateWrapper::unwrap(page.output)
-    end
-  end
+          @@address_attributes = [
+        :id, :firstname, :lastname, :full_name, :address1, :address2, :city,
+        :zipcode, :phone, :company, :alternative_phone, :country_id, :state_id,
+        :state_name, :state_text
+      ]
