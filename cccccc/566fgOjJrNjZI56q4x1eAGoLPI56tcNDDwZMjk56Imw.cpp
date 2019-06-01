a2240326d152de99cf9a81c8297718d658524232
SUNsung <weatherius@gmail.com>
2019-06-01 20:10:09 +0300

    Rewrite [566fgOjJrNjZI56q4x1eAGoLPI56tcNDDwZMjk56Imw.cpp] File in: 2019-06-01 20:10:08

--- cccccc/566fgOjJrNjZI56q4x1eAGoLPI56tcNDDwZMjk56Imw.cpp
+++ cccccc/566fgOjJrNjZI56q4x1eAGoLPI56tcNDDwZMjk56Imw.cpp
@@ -1,252 +1,87 @@
 
         
-        class PAGE_RES_IT;
-class ROW;
-class WERD_RES;
-    
-    
-    {  bool rword_indicates_list_item;
-  bool rword_likely_starts_idea;
-  bool rword_likely_ends_idea;
-};
-    
-    #include 'genericvector.h'
-#include 'kdpair.h'
-#include 'points.h'
-    
-    class LLSQ {
- public:
-  LLSQ() {  // constructor
-    clear();  // set to zeros
-  }
-  void clear();  // initialize
+          virtual bool Next(void) {
+    if (out_ != nullptr) {
+      recycle_queue_.push(out_); out_ = nullptr;
     }
-    
-    // Computes the histogram for the given image rectangle, and the given
-// single channel. Each channel is always one byte per pixel.
-// Histogram is always a kHistogramSize(256) element array to count
-// occurrences of each pixel value.
-void HistogramRect(Pix* src_pix, int channel,
-                   int left, int top, int width, int height,
-                   int* histogram);
-    
-    This module may look unnecessarily verbose, but here's the philosophy...
-    
-      // Fills in two ambiguity tables (replaceable and dangerous) with information
-  // read from the ambigs file. An ambiguity table is an array of lists.
-  // The array is indexed by a class id. Each entry in the table provides
-  // a list of potential ambiguities which can start with the corresponding
-  // character. For example the ambiguity 'rn -> m', would be located in the
-  // table at index of unicharset.unichar_to_id('r').
-  // In 1-1 ambiguities (e.g. s -> S, 1 -> I) are recorded in
-  // one_to_one_definite_ambigs_. This vector is also indexed by the class id
-  // of the wrong part of the ambiguity and each entry contains a vector of
-  // unichar ids that are ambiguous to it.
-  // encoder_set is used to encode the ambiguity strings, undisturbed by new
-  // unichar_ids that may be created by adding the ambigs.
-  void LoadUnicharAmbigs(const UNICHARSET& encoder_set,
-                         TFile *ambigs_file, int debug_level,
-                         bool use_ambigs_for_adaption, UNICHARSET *unicharset);
-    
-      // The pointed-to Pair has changed its key value, so the location of pair
-  // is reshuffled to maintain the heap invariant.
-  // Must be a valid pointer to an element of the heap_!
-  // Caution! Since GenericHeap is based on GenericVector, reallocs may occur
-  // whenever the vector is extended and elements may get shuffled by any
-  // Push or Pop operation. Therefore use this function only if Data in Pair is
-  // of type DoublePtr, derived (first) from DoublePtr, or has a DoublePtr as
-  // its first element. Reshuffles the heap to maintain the invariant.
-  // Time = O(log n).
-  void Reshuffle(Pair* pair) {
-    int index = pair - &heap_[0];
-    Pair hole_pair = heap_[index];
-    index = SiftDown(index, hole_pair);
-    index = SiftUp(index, hole_pair);
-    heap_[index] = hole_pair;
-  }
-    
-    #include <cmath>
-    
-    # endif  // NDEBUG for EXPECT_DEBUG_DEATH
-#endif  // GTEST_HAS_DEATH_TEST
-    
-     private:
-  std::vector<TestPartResult> array_;
-    
-    // Helper function for implementing {EXPECT|ASSERT}_PRED5.  Don't use
-// this in your code.
-template <typename Pred,
-          typename T1,
-          typename T2,
-          typename T3,
-          typename T4,
-          typename T5>
-AssertionResult AssertPred5Helper(const char* pred_text,
-                                  const char* e1,
-                                  const char* e2,
-                                  const char* e3,
-                                  const char* e4,
-                                  const char* e5,
-                                  Pred pred,
-                                  const T1& v1,
-                                  const T2& v2,
-                                  const T3& v3,
-                                  const T4& v4,
-                                  const T5& v5) {
-  if (pred(v1, v2, v3, v4, v5)) return AssertionSuccess();
+    // do recycle
+    if (recycle_queue_.size() == param_.prefetch_buffer) {
+      DataBatch *old_batch =  recycle_queue_.front();
+      // can be more efficient on engine
+      for (NDArray& arr : old_batch->data) {
+        arr.WaitToWrite();
+      }
+      recycle_queue_.pop();
+      iter.Recycle(&old_batch);
     }
-    
-      void Reseed(UInt32 seed) { state_ = seed; }
-    
-    // A unique type used as the default value for the arguments of class
-// template Types.  This allows us to simulate variadic templates
-// (e.g. Types<int>, Type<int, double>, and etc), which C++ doesn't
-// support directly.
-struct None {};
-    
-    
-    {    // Now, we have i <= n/i < n.
-    // If n is divisible by i, n is not prime.
-    if (n % i == 0) return false;
+    return iter.Next(&out_);
+  }
+  virtual const DataBatch &Value(void) const {
+    return *out_;
   }
     
     
-    {  // Can we set the MyString to NULL?
-  s.Set(NULL);
-  EXPECT_STREQ(NULL, s.c_string());
-}
+    {
+    {.add_argument('data', 'Symbol or Symbol[]', 'Tensor or List of Tensors, the second input '
+'will be used as crop_like shape reference')
+.add_arguments(CropParam::__FIELDS__())
+.set_key_var_num_args('num_args');
+}  // namespace op
+}  // namespace mxnet
 
     
-    // TODO(costan): Remove port::kLittleEndian and the fast paths based on
-//               std::memcpy when clang learns to optimize the generic code, as
-//               described in https://bugs.llvm.org/show_bug.cgi?id=41761
-//
-// The platform-independent code in DecodeFixed{32,64}() gets optimized to mov
-// on x86 and ldr on ARM64, by both clang and gcc. However, only gcc optimizes
-// the platform-independent code in EncodeFixed{32,64}() to mov / str.
-    
-    
-    {      // End write transaction
-      if (FLAGS_transaction && transaction) {
-        status = sqlite3_step(end_trans_stmt);
-        StepErrorCheck(status);
-        status = sqlite3_reset(end_trans_stmt);
-        ErrorCheck(status);
-      }
+    template<typename xpu>
+void NDArrayOp<xpu>::Forward(const OpContext &ctx,
+                   const std::vector<TBlob> &in_data,
+                   const std::vector<OpReqType> &req,
+                   const std::vector<TBlob> &out_data,
+                   const std::vector<TBlob> &aux_args) {
+  using namespace mshadow;
+  Context ndctx = get_ctx();
+  std::vector<void*> ptrs;
+  std::vector<Engine::VarHandle> ndvar;
+  std::vector<int> tags;
+  for (auto& i : req) CHECK_NE(i, kAddTo);
     }
     
-      Benchmark()
-      : db_(nullptr),
-        num_(FLAGS_num),
-        reads_(FLAGS_reads < 0 ? FLAGS_num : FLAGS_reads),
-        bytes_(0),
-        rand_(301) {
-    std::vector<std::string> files;
-    std::string test_dir;
-    Env::Default()->GetTestDirectory(&test_dir);
-    Env::Default()->GetChildren(test_dir.c_str(), &files);
-    if (!FLAGS_use_existing_db) {
-      for (int i = 0; i < files.size(); i++) {
-        if (Slice(files[i]).starts_with('dbbench_polyDB')) {
-          std::string file_name(test_dir);
-          file_name += '/';
-          file_name += files[i];
-          Env::Default()->DeleteFile(file_name.c_str());
-        }
-      }
+    int64_t VMTOC::getValue(int64_t index, bool qword) {
+  HPHP::Address addr = reinterpret_cast<HPHP::Address>(
+      static_cast<intptr_t>(index) + getPtrVector());
+  int64_t ret_val = 0;
+  int max_elem = qword ? 8 : 4;
+  for (int i = max_elem-1; i >= 0; i--) {
+    ret_val = addr[i] + (ret_val << 8);
+  }
+  return ret_val;
+}
+    
+      void EmitXFXForm(const uint8_t op,
+                   const RegNumber rs,
+                   const SpecialReg spr,
+                   const uint16_t xo,
+                   const uint8_t rsv = 0) {
     }
-  }
     
-      // Atomically release *mu and block on this condition variable until
-  // either a call to SignalAll(), or a call to Signal() that picks
-  // this thread to wakeup.
-  // REQUIRES: this thread holds *mu
-  void Wait();
+      // The HHIRLICM runtime option is all capitals, so separation
+  // cannot be determined. Special case it.
+  boost::replace_first(out, 'hhirlicm', 'hhir_licm');
+  // The HHVM ini option becomes the standard PHP option.
+  boost::replace_first(out,
+                       'hhvm.server.upload.max_file_uploads',
+                       'max_file_uploads');
+  // Make sure IPv6 or IPv4 are handled correctly
+  boost::replace_first(out, '_i_pv', '_ipv');
+  boost::replace_first(out, '.i_pv', '.ipv');
+  // urls are special too. Let's not have 'ur_ls'
+  boost::replace_first(out, '_ur_ls', '_urls');
+  boost::replace_first(out, '.ur_ls', '.urls');
+  // No use of Eval in our ini strings
+  boost::replace_first(out, '.eval.', '.');
+  boost::replace_first(out, '.my_sql.', '.mysql.');
+  boost::replace_first(out, '.enable_hip_hop_syntax', '.force_hh');
     
-    static std::string MakeFileName(const std::string& dbname, uint64_t number,
-                                const char* suffix) {
-  char buf[100];
-  snprintf(buf, sizeof(buf), '/%06llu.%s',
-           static_cast<unsigned long long>(number), suffix);
-  return dbname + buf;
-}
+    #include 'hphp/runtime/base/file.h'
+#include 'hphp/runtime/base/stream-wrapper.h'
+#include 'hphp/runtime/base/stream-wrapper-registry.h'
     
-      fname = TempFileName('tmp', 999);
-  ASSERT_EQ('tmp/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(999, number);
-  ASSERT_EQ(kTempFile, type);
-    
-      Cache::Handle* handle = nullptr;
-  Status s = FindTable(file_number, file_size, &handle);
-  if (!s.ok()) {
-    return NewErrorIterator(s);
-  }
-    
-    static void TestEncodeDecode(const VersionEdit& edit) {
-  std::string encoded, encoded2;
-  edit.EncodeTo(&encoded);
-  VersionEdit parsed;
-  Status s = parsed.DecodeFrom(encoded);
-  ASSERT_TRUE(s.ok()) << s.ToString();
-  parsed.EncodeTo(&encoded2);
-  ASSERT_EQ(encoded, encoded2);
-}
-    
-      // Return the sequence number for the start of this batch.
-  static SequenceNumber Sequence(const WriteBatch* batch);
-    
-    #if defined(_WIN32)
-// The leveldb::Env class below contains a DeleteFile method.
-// At the same time, <windows.h>, a fairly popular header
-// file for Windows applications, defines a DeleteFile macro.
-//
-// Without any intervention on our part, the result of this
-// unfortunate coincidence is that the name of the
-// leveldb::Env::DeleteFile method seen by the compiler depends on
-// whether <windows.h> was included before or after the LevelDB
-// headers.
-//
-// To avoid headaches, we undefined DeleteFile (if defined) and
-// redefine it at the bottom of this file. This way <windows.h>
-// can be included before this file (or not at all) and the
-// exported method will always be leveldb::Env::DeleteFile.
-#if defined(DeleteFile)
-#undef DeleteFile
-#define LEVELDB_DELETEFILE_UNDEFINED
-#endif  // defined(DeleteFile)
-#endif  // defined(_WIN32)
-    
-    #include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
-    
-    
-    {  int ret = x;
-  return ret;
-}
-    
-    #include 'modules/canbus/vehicle/gem/protocol/accel_cmd_67.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_cmd_6b.h'
-#include 'modules/canbus/vehicle/gem/protocol/global_cmd_69.h'
-#include 'modules/canbus/vehicle/gem/protocol/headlight_cmd_76.h'
-#include 'modules/canbus/vehicle/gem/protocol/horn_cmd_78.h'
-#include 'modules/canbus/vehicle/gem/protocol/shift_cmd_65.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_cmd_6d.h'
-#include 'modules/canbus/vehicle/gem/protocol/turn_cmd_63.h'
-#include 'modules/canbus/vehicle/gem/protocol/wiper_cmd_90.h'
-    
-      Byte t1(bytes + 5);
-  int32_t t = t1.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-    using ::apollo::drivers::canbus::Byte;
-    
-    void Hornrpt79::Parse(const std::uint8_t* bytes, int32_t length,
-                      ChassisDetail* chassis) const {
-  chassis->mutable_gem()->mutable_horn_rpt_79()->set_output_value(
-      output_value(bytes, length));
-  chassis->mutable_gem()->mutable_horn_rpt_79()->set_commanded_value(
-      commanded_value(bytes, length));
-  chassis->mutable_gem()->mutable_horn_rpt_79()->set_manual_input(
-      manual_input(bytes, length));
-}
+    ///////////////////////////////////////////////////////////////////////////////
