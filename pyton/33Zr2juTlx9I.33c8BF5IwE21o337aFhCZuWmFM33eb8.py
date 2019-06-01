63eba7c49a0898fb1e4137cc70721f095f2f3782
SUNsung <weatherius@gmail.com>
2019-06-01 17:07:13 +0300

    Rewrite [33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py] File in: 2019-06-01 17:07:13

--- pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
+++ pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
@@ -1,135 +1,122 @@
 
         
-            # #14971: Make sure the dotted name resolution works even if the actual
-    # function doesn't have the same name as is used to find it.
-    def test_loadTestsFromName__function_with_different_name_than_method(self):
-        # lambdas have the name '<lambda>'.
-        m = types.ModuleType('m')
-        class MyTestCase(unittest.TestCase):
-            test = lambda: 1
-        m.testcase_1 = MyTestCase
-    
-    
-def open_binary(package: Package, resource: Resource) -> BinaryIO:
-    '''Return a file-like object opened for binary reading of the resource.'''
-    resource = _normalize_path(resource)
-    package = _get_package(package)
-    reader = _get_resource_reader(package)
-    if reader is not None:
-        return reader.open_resource(resource)
-    _check_location(package)
-    absolute_package_path = os.path.abspath(package.__spec__.origin)
-    package_path = os.path.dirname(absolute_package_path)
-    full_path = os.path.join(package_path, resource)
+        print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
     try:
-        return open(full_path, mode='rb')
-    except OSError:
-        # Just assume the loader is a resource loader; all the relevant
-        # importlib.machinery loaders are and an AttributeError for
-        # get_data() will make it clear what is needed from the loader.
-        loader = cast(ResourceLoader, package.__spec__.loader)
-        data = None
-        if hasattr(package.__spec__.loader, 'get_data'):
-            with suppress(OSError):
-                data = loader.get_data(full_path)
-        if data is None:
-            package_name = package.__spec__.name
-            message = '{!r} resource not found in {!r}'.format(
-                resource, package_name)
-            raise FileNotFoundError(message)
-        else:
-            return BytesIO(data)
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
     
-        def testRead(self):
-        self.createTempFile()
-        with BZ2File(self.filename) as bz2f:
-            self.assertRaises(TypeError, bz2f.read, float())
-            self.assertEqual(bz2f.read(), self.TEXT)
+    # -- General configuration ------------------------------------------------
     
-    def f(x):
-    return 1.0 / (x - 5.0)
-    
-    def test():
-    NUMBER_OF_PROCESSES = 4
-    TASKS1 = [(mul, (i, 7)) for i in range(20)]
-    TASKS2 = [(plus, (i, 8)) for i in range(10)]
-    
-    cur.execute('insert into people (name_last, age) values ('Yeltsin',   72)')
-cur.execute('insert into people (name_last, age) values ('Putin',     51)')
+        def test_youtube(self):
+        self._assert_restricted('07FYdnEawAQ', '07FYdnEawAQ.mp4', 10)
     
     
-class EventNotification( BaseRequest ):
-  def __init__( self, event_name, buffer_number = None, extra_data = None ):
-    super( EventNotification, self ).__init__()
-    self._event_name = event_name
-    self._buffer_number = buffer_number
-    self._extra_data = extra_data
-    self._response_future = None
-    self._cached_response = None
+if __name__ == '__main__':
+    unittest.main()
+
     
     
-  def _ThreadMain( self ):
-    while True:
-      time.sleep( self._ping_interval_seconds )
+try:
+    _DEV_NULL = subprocess.DEVNULL
+except AttributeError:
+    _DEV_NULL = open(os.devnull, 'wb')
     
-        # Execute the template string in a temporary namespace and
-    # support tracing utilities by setting a value for frame.f_globals['__name__']
-    namespace = dict(_itemgetter=_itemgetter, __name__='namedtuple_%s' % typename,
-                     _property=property, _tuple=tuple)
-    try:
-        exec(template, namespace)
-    except SyntaxError:
-        e = _sys.exc_info()[1]
-        raise SyntaxError(e.message + ':\n' + template)
-    result = namespace[typename]
+            jsi = JSInterpreter('function f(){return 11 >> 2;}')
+        self.assertEqual(jsi.call_function('f'), 2)
     
-    delim = text_type(args.delimiter)
-cutall = args.cutall
-hmm = args.hmm
-fp = open(args.filename, 'r') if args.filename else sys.stdin
+                    if ''' not in code and ''' not in code:
+                    continue
+                assertRegexpMatches(
+                    self,
+                    code,
+                    r'(?:(?:#.*?|\s*)\n)*from __future__ import (?:[a-z_]+,\s*)*unicode_literals',
+                    'unicode_literals import  missing in %s' % fn)
     
-            return ws
+        if not module.check_mode and (additions > 0 or deletions > 0):
+        task = gateway.save_services_configuration()
+        vca.block_until_completed(task)
     
-    seg_list = jieba.cut_for_search('小明硕士毕业于中国科学院计算所，后在日本京都大学深造')  # 搜索引擎模式
-print(', '.join(seg_list))
+        def role_remove_service(self, name, item):
+        return self.role_remove_member(name=name, item={'service': item})
     
-    USAGE = 'usage:    python extract_tags.py [file name] -k [top k]'
+        # Build the common request body
+    body = dict()
+    for k in ('component', 'version', 'hosts'):
+        v = module.params[k]
+        if v is not None:
+            body[k] = v
     
-    content = open(file_name, 'rb').read()
+    short_description: Manage Icinga2 feature
+description:
+    - This module can be used to enable or disable an Icinga2 feature.
+version_added: '2.3'
+author: 'Loic Blot (@nerzhul)'
+options:
+    name:
+      description:
+      - This is the feature name to enable or disable.
+      required: True
+    state:
+      description:
+      - If set to C(present) and feature is disabled, then feature is enabled.
+      - If set to C(present) and feature is already enabled, then nothing is changed.
+      - If set to C(absent) and feature is enabled, then feature is disabled.
+      - If set to C(absent) and feature is already disabled, then nothing is changed.
+      choices: [ 'present', 'absent' ]
+      default: present
+'''
     
-        def testDefaultCut(self):
-        for content in test_contents:
-            result = jieba.cut(content)
-            assert isinstance(result, types.GeneratorType), 'Test DefaultCut Generator error'
-            result = list(result)
-            assert isinstance(result, list), 'Test DefaultCut error on content: %s' % content
-            print(' , '.join(result), file=sys.stderr)
-        print('testDefaultCut', file=sys.stderr)
-    
-    import jieba
-    
-        output_predict_file = os.path.join(FLAGS.output_dir, 'test_results.tsv')
-    with tf.gfile.GFile(output_predict_file, 'w') as writer:
-      tf.logging.info('***** Predict results *****')
-      for prediction in result:
-        probabilities = prediction['probabilities']
-        output_line = '\t'.join(
-            str(class_probability)
-            for class_probability in probabilities) + '\n'
-        writer.write(output_line)
-    
-        self.assertAllEqual(
-        tokenizer.tokenize('unwanted running'),
-        ['un', '##want', '##ed', 'runn', '##ing'])
-    
-            assert filter_storage.isMuted('1C5sgvWaSgfaTpV5kjBCnCiKtENNMYo69q')
-        assert site.storage.query(query_num_json).fetchone()['num'] == 0
-    
-        def _decode_array(self, size, offset):
-        array = []
-        for _ in range(size):
-            (value, offset) = self.decode(offset)
-            array.append(value)
-        return array, offset
-    
-            server_change_attributes(compute_api=compute_api, target_server=target_server, wished_server=wished_server)
+    module: stackdriver
+short_description: Send code deploy and annotation events to stackdriver
+description:
+  - Send code deploy and annotation events to Stackdriver
+version_added: '1.6'
+author: 'Ben Whaley (@bwhaley)'
+options:
+  key:
+    description:
+      - API key.
+    required: true
+  event:
+    description:
+      - The type of event to send, either annotation or deploy
+    choices: ['annotation', 'deploy']
+  revision_id:
+    description:
+      - The revision of the code that was deployed. Required for deploy events
+  deployed_by:
+    description:
+      - The person or robot responsible for deploying the code
+    default: 'Ansible'
+  deployed_to:
+    description:
+      - 'The environment code was deployed to. (ie: development, staging, production)'
+  repository:
+    description:
+      - The repository (or project) deployed
+  msg:
+    description:
+      - The contents of the annotation message, in plain text.  Limited to 256 characters. Required for annotation.
+  annotated_by:
+    description:
+      - The person or robot who the annotation should be attributed to.
+    default: 'Ansible'
+  level:
+    description:
+      - one of INFO/WARN/ERROR, defaults to INFO if not supplied.  May affect display.
+    choices: ['INFO', 'WARN', 'ERROR']
+    default: 'INFO'
+  instance_id:
+    description:
+      - id of an EC2 instance that this event should be attached to, which will limit the contexts where this event is shown
+  event_epoch:
+    description:
+      - 'Unix timestamp of where the event should appear in the timeline, defaults to now. Be careful with this.'
+'''
