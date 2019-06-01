1fb33de95d0c463259686f8c2208b9bd76404a8e
SUNsung <weatherius@gmail.com>
2019-06-01 05:28:41 +0300

    Rewrite [40gmHGKyKNt0Q406fma5grX4uE40nCz1SWSuwY40NtJ.cpp] File in: 2019-06-01 05:28:41

--- cccccc/40gmHGKyKNt0Q406fma5grX4uE40nCz1SWSuwY40NtJ.cpp
+++ cccccc/40gmHGKyKNt0Q406fma5grX4uE40nCz1SWSuwY40NtJ.cpp
@@ -1,399 +1,154 @@
 
         
-        #include <utility>
+            /** Colorize an icon (given object) with the text color */
+    QIcon TextColorIcon(const QIcon& icon) const;
     
-    namespace atom {
+        void showTab_SM(bool fShow);
+    void showTab_VM(bool fShow);
+    
+    ; shuffle xBxA -> 00BA
+_SHUF_00BA:              ddq 0xFFFFFFFFFFFFFFFF0b0a090803020100
+    
+    TEST(DBTest, ManifestWriteError) {
+  // Test for the following problem:
+  // (a) Compaction produces file F
+  // (b) Log record containing F is written to MANIFEST file, but Sync() fails
+  // (c) GC deletes F
+  // (d) After reopening DB, reads fail since deleted F is named in log record
     }
     
-    namespace asar {
+    #endif  // STORAGE_LEVELDB_DB_FILENAME_H_
+
+    
+    static const int kBlockSize = 32768;
+    
+    namespace leveldb {
     }
     
-    class RootViewMac : public views::View {
- public:
-  explicit RootViewMac(NativeWindow* window);
-  ~RootViewMac() override;
+    namespace log {
     }
     
+    #ifndef STORAGE_LEVELDB_DB_SKIPLIST_H_
+#define STORAGE_LEVELDB_DB_SKIPLIST_H_
     
-    {private:
-    Ui::OpenURIDialog *ui;
+     private:
+  Status FindTable(uint64_t file_number, uint64_t file_size, Cache::Handle**);
+    
+      // Random reads.
+  ASSERT_OK(env_->NewRandomAccessFile('/dir/f', &rand_file));
+  ASSERT_OK(rand_file->Read(6, 5, &result, scratch));  // Read 'world'.
+  ASSERT_EQ(0, result.compare('world'));
+  ASSERT_OK(rand_file->Read(0, 5, &result, scratch));  // Read 'hello'.
+  ASSERT_EQ(0, result.compare('hello'));
+  ASSERT_OK(rand_file->Read(10, 100, &result, scratch));  // Read 'd'.
+  ASSERT_EQ(0, result.compare('d'));
+    
+      // An iterator is either positioned at a key/value pair, or
+  // not valid.  This method returns true iff the iterator is valid.
+  virtual bool Valid() const = 0;
+    
+      // Indicate that the contents of this builder should be abandoned.  Stops
+  // using the file passed to the constructor after this function returns.
+  // If the caller is not going to call Finish(), it must call Abandon()
+  // before destroying this builder.
+  // REQUIRES: Finish(), Abandon() have not been called
+  void Abandon();
+    
+    
+    {  std::string rep_;  // See comment in write_batch.cc for the format of rep_
 };
     
-    void test_ecdh_api(void) {
-    /* Setup context that just counts errors */
-    secp256k1_context *tctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
-    secp256k1_pubkey point;
-    unsigned char res[32];
-    unsigned char s_one[32] = { 0 };
-    int32_t ecount = 0;
-    s_one[31] = 1;
+      FilterBlockBuilder(const FilterBlockBuilder&) = delete;
+  FilterBlockBuilder& operator=(const FilterBlockBuilder&) = delete;
+    
+      Arena(const Arena&) = delete;
+  Arena& operator=(const Arena&) = delete;
+    
+    TEST(CacheTest, Prune) {
+  Insert(1, 100);
+  Insert(2, 200);
     }
     
-    int secp256k1_ecdsa_recover(const secp256k1_context* ctx, secp256k1_pubkey *pubkey, const secp256k1_ecdsa_recoverable_signature *signature, const unsigned char *msg32) {
-    secp256k1_ge q;
-    secp256k1_scalar r, s;
-    secp256k1_scalar m;
-    int recid;
-    VERIFY_CHECK(ctx != NULL);
-    ARG_CHECK(secp256k1_ecmult_context_is_built(&ctx->ecmult_ctx));
-    ARG_CHECK(msg32 != NULL);
-    ARG_CHECK(signature != NULL);
-    ARG_CHECK(pubkey != NULL);
-    }
-    
-    TEST(FormatTest, InternalKeyShortSeparator) {
-  // When user keys are same
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foo', 99, kTypeValue)));
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foo', 101, kTypeValue)));
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foo', 100, kTypeValue)));
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foo', 100, kTypeDeletion)));
-    }
-    
-        void operator() (const typename internal::VecTraits<T>::vec128 & v_src0,
-                     const typename internal::VecTraits<T>::vec128 & v_src1,
-                     typename internal::VecTraits<T>::vec128 & v_dst) const
-    {
-        typename internal::VecTraits<T>::vec128 v_min = internal::vminq(v_src0, v_src1);
-        typename internal::VecTraits<T>::vec128 v_max = internal::vmaxq(v_src0, v_src1);
-        v_dst = internal::vqsubq(v_max, v_min);
-    }
+    // Modified by EnvWindowsTestHelper::SetReadOnlyMMapLimit().
+int g_mmap_limit = kDefaultMmapLimit;
     
     
-    {        v_dst = vcombine_u8(vmovn_u16(v_dst0), vmovn_u16(v_dst1));
-    }
-    
-                uint8x8_t l2 = l18u;
-            uint8x8_t l0 = vext_u8(l, l18u, 6);
-            int16x8_t l1x2 = vreinterpretq_s16_u16(vshll_n_u8(vext_u8(l, l18u, 7), 1));
-    
-    IMPL_CMPOP(EQ, u8)
-IMPL_CMPOP(EQ, s8)
-IMPL_CMPOP(EQ, u16)
-IMPL_CMPOP(EQ, s16)
-IMPL_CMPOP(EQ, u32)
-IMPL_CMPOP(EQ, s32)
-IMPL_CMPOP(EQ, f32)
-    
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const u8 * src = internal::getRowPtr(srcBase, srcStride, i);
-        s16 * dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t j = 0;
-    }
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(u8, s32, 16,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 16)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.8 {d4-d5}, [%[src]]                              \n\t'
-            'vmovl.u8 q3, d4                                       \n\t'
-            'vmovl.u8 q4, d5                                       \n\t'
-            'vmovl.u16 q5, d6                                      \n\t'
-            'vmovl.u16 q6, d7                                      \n\t'
-            'vmovl.u16 q7, d8                                      \n\t'
-            'vmovl.u16 q8, d9                                      \n\t'
-            'vcvt.f32.u32 q9, q5                                   \n\t'
-            'vcvt.f32.u32 q10, q6                                  \n\t'
-            'vcvt.f32.u32 q11, q7                                  \n\t'
-            'vcvt.f32.u32 q12, q8                                  \n\t'
-            'vmul.f32 q13, q9, q0                                  \n\t'
-            'vmul.f32 q14, q10, q0                                 \n\t'
-            'vmul.f32 q15, q11, q0                                 \n\t'
-            'vmul.f32 q2, q12, q0                                  \n\t'
-            'vadd.f32 q3, q13, q1                                  \n\t'
-            'vadd.f32 q4, q14, q1                                  \n\t'
-            'vadd.f32 q5, q15, q1                                  \n\t'
-            'vadd.f32 q6, q2, q1                                   \n\t'
-            'vcvt.s32.f32 q7, q3                                   \n\t'
-            'vcvt.s32.f32 q8, q4                                   \n\t'
-            'vcvt.s32.f32 q9, q5                                   \n\t'
-            'vcvt.s32.f32 q10, q6                                  \n\t'
-            'vst1.32 {d14-d15}, [%[dst1]]                          \n\t'
-            'vst1.32 {d16-d17}, [%[dst2]]                          \n\t'
-            'vst1.32 {d18-d19}, [%[dst3]]                          \n\t'
-            'vst1.32 {d20-d21}, [%[dst4]]                          \n\t'
-            : /*no output*/
-            : [src] 'r' (_src + i),
-              [dst1] 'r' (_dst + i + 0),
-              [dst2] 'r' (_dst + i + 4),
-              [dst3] 'r' (_dst + i + 8),
-              [dst4] 'r' (_dst + i + 12),
-              'w'  (vscale), 'w' (vshift)
-            : 'd4','d5','d6','d7','d8','d9','d10',
-            'd11','d12','d13','d14','d15','d16','d17',
-            'd18','d19','d20','d21','d22','d23','d24',
-            'd25','d26','d27','d28','d29','d30','d31'
-        );
-    }
-})
-#else
-CVTS_FUNC(u8, s32, 16,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 16)
-    {
-        internal::prefetch(_src + i);
-        uint8x16_t vline = vld1q_u8(_src + i);
-        uint16x8_t vline1_u16 = vmovl_u8(vget_low_u8 (vline));
-        uint16x8_t vline2_u16 = vmovl_u8(vget_high_u8(vline));
-        uint32x4_t vline1_u32 = vmovl_u16(vget_low_u16 (vline1_u16));
-        uint32x4_t vline2_u32 = vmovl_u16(vget_high_u16(vline1_u16));
-        uint32x4_t vline3_u32 = vmovl_u16(vget_low_u16 (vline2_u16));
-        uint32x4_t vline4_u32 = vmovl_u16(vget_high_u16(vline2_u16));
-        float32x4_t vline1_f32 = vcvtq_f32_u32(vline1_u32);
-        float32x4_t vline2_f32 = vcvtq_f32_u32(vline2_u32);
-        float32x4_t vline3_f32 = vcvtq_f32_u32(vline3_u32);
-        float32x4_t vline4_f32 = vcvtq_f32_u32(vline4_u32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline3_f32 = vmulq_f32(vline3_f32, vscale);
-        vline4_f32 = vmulq_f32(vline4_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        vline3_f32 = vaddq_f32(vline3_f32, vshift);
-        vline4_f32 = vaddq_f32(vline4_f32, vshift);
-        int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        int32x4_t vline3_s32 = vcvtq_s32_f32(vline3_f32);
-        int32x4_t vline4_s32 = vcvtq_s32_f32(vline4_f32);
-        vst1q_s32(_dst + i + 0,  vline1_s32);
-        vst1q_s32(_dst + i + 4,  vline2_s32);
-        vst1q_s32(_dst + i + 8,  vline3_s32);
-        vst1q_s32(_dst + i + 12, vline4_s32);
-    }
-})
-#endif
-    
-        ptrdiff_t width = (ptrdiff_t)size.width, height = (ptrdiff_t)size.height;
-    static const vshrq_s32_func vshrq_s32_a[33] =
-    {
-        vshrq_s32<0>,
-        vshrq_s32<1>,
-        vshrq_s32<2>,
-        vshrq_s32<3>,
-        vshrq_s32<4>,
-        vshrq_s32<5>,
-        vshrq_s32<6>,
-        vshrq_s32<7>,
-        vshrq_s32<8>,
-        vshrq_s32<9>,
-        vshrq_s32<10>,
-        vshrq_s32<11>,
-        vshrq_s32<12>,
-        vshrq_s32<13>,
-        vshrq_s32<14>,
-        vshrq_s32<15>,
-        vshrq_s32<16>,
-        vshrq_s32<17>,
-        vshrq_s32<18>,
-        vshrq_s32<19>,
-        vshrq_s32<20>,
-        vshrq_s32<21>,
-        vshrq_s32<22>,
-        vshrq_s32<23>,
-        vshrq_s32<24>,
-        vshrq_s32<25>,
-        vshrq_s32<26>,
-        vshrq_s32<27>,
-        vshrq_s32<28>,
-        vshrq_s32<29>,
-        vshrq_s32<30>,
-        vshrq_s32<31>,
-        vshrq_s32<32>
-    };
-    vshrq_s32_func vshrq_s32_p = vshrq_s32_a[scale];
-    
-                int32x2_t vnz1 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx1), vget_high_u32(vlx1)));
-            int32x2_t vnz2 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx2), vget_high_u32(vlx2)));
-    
-                    int16x8_t vdot1 = vmull_s8(vget_low_s8(vs1), vget_low_s8(vs2));
-                int16x8_t vdot2 = vmull_s8(vget_high_s8(vs1), vget_high_s8(vs2));
-    
-    template <typename T>
-void flip(const Size2D & size,
-          const void * srcBase, ptrdiff_t srcStride,
-          void * dstBase, ptrdiff_t dstStride,
-          FLIP_MODE flipMode)
-{
-    using namespace internal;
-    }
-    
-            //left&right borders
-        if (borderType != BORDER_MODE_CONSTANT)
-            for (s32 k = 0; k < cn; ++k)
-            {
-                lane[-cn+k] = lane[idx_l1 + k];
-                lane[-cn-cn+k] = lane[idx_l2 + k];
-    }
-    
-    void Laplacian3x3(const Size2D &size,
-                  const u8 * srcBase, ptrdiff_t srcStride,
-                  u8 * dstBase, ptrdiff_t dstStride,
-                  BORDER_MODE border, u8 borderValue)
-{
-    internal::assertSupportedConfiguration(isLaplacian3x3Supported(size, border));
-#ifdef CAROTENE_NEON
-    const uint16x8_t v_border_x3 = vdupq_n_u16(borderValue * 3);
-    const uint16x8_t v_zero = vdupq_n_u16(0);
-    const uint8x8_t v_border = vdup_n_u8(borderValue);
-    }
-    
-        /**
-     * Transliterator API.
-     * @return A copy of the object.
-     */
-    virtual Transliterator* clone(void) const;
-    
-    
-    {        if(equalPrefixLength > 0) {
-            if((leftUnit >= 0 && data->isUnsafeBackward(leftUnit, numeric)) ||
-                    (rightUnit >= 0 && data->isUnsafeBackward(rightUnit, numeric))) {
-                // Identical prefix: Back up to the start of a contraction or reordering sequence.
-                do {
-                    --equalPrefixLength;
-                    leftUnit = left.previous(&left);
-                    right.previous(&right);
-                } while(equalPrefixLength > 0 && data->isUnsafeBackward(leftUnit, numeric));
-            }
-            // See the notes in the UTF-16 version.
-        }
-    }
-    
-    #define DOT               ((UChar)0x002E)
-#define SINGLE_QUOTE      ((UChar)0x0027)
-#define SLASH             ((UChar)0x002F)
-#define BACKSLASH         ((UChar)0x005C)
-#define SPACE             ((UChar)0x0020)
-#define TAB               ((UChar)0x0009)
-#define QUOTATION_MARK    ((UChar)0x0022)
-#define ASTERISK          ((UChar)0x002A)
-#define COMMA             ((UChar)0x002C)
-#define HYPHEN            ((UChar)0x002D)
-#define U_ZERO            ((UChar)0x0030)
-#define U_ONE             ((UChar)0x0031)
-#define U_TWO             ((UChar)0x0032)
-#define U_THREE           ((UChar)0x0033)
-#define U_FOUR            ((UChar)0x0034)
-#define U_FIVE            ((UChar)0x0035)
-#define U_SIX             ((UChar)0x0036)
-#define U_SEVEN           ((UChar)0x0037)
-#define U_EIGHT           ((UChar)0x0038)
-#define U_NINE            ((UChar)0x0039)
-#define COLON             ((UChar)0x003A)
-#define SEMI_COLON        ((UChar)0x003B)
-#define CAP_A             ((UChar)0x0041)
-#define CAP_B             ((UChar)0x0042)
-#define CAP_R             ((UChar)0x0052)
-#define CAP_Z             ((UChar)0x005A)
-#define LOWLINE           ((UChar)0x005F)
-#define LEFTBRACE         ((UChar)0x007B)
-#define RIGHTBRACE        ((UChar)0x007D)
-    
-    #if !UCONFIG_NO_BREAK_ITERATION
-    
-    #include 'unicode/utypes.h'
-#include 'sharedobject.h'
-    
-    U_NAMESPACE_END
-    
-    SimpleDateFormatStaticSets::SimpleDateFormatStaticSets(UErrorCode &status)
-: fDateIgnorables(NULL),
-  fTimeIgnorables(NULL),
-  fOtherIgnorables(NULL)
-{
-    fDateIgnorables  = new UnicodeSet(UNICODE_STRING('[-,./[:whitespace:]]', 20), status);
-    fTimeIgnorables  = new UnicodeSet(UNICODE_STRING('[-.:[:whitespace:]]', 19),  status);
-    fOtherIgnorables = new UnicodeSet(UNICODE_STRING('[:whitespace:]', 14),       status);
-    }
-    
-        // Note: This method is semantically const, but physically non-const.
-    
-    /**
- * Standard CLDR plural form/category constants.
- * See http://www.unicode.org/reports/tr35/tr35-numbers.html#Language_Plural_Rules
- */
-class U_I18N_API StandardPlural {
-public:
-    enum Form {
-        ZERO,
-        ONE,
-        TWO,
-        FEW,
-        MANY,
-        OTHER,
-        COUNT
-    };
-    }
-    
-    #endif /* #if !UCONFIG_NO_TRANSLITERATION */
-    
-    /**
- * UnicodeFunctor API
- */
-void StringReplacer::setData(const TransliterationRuleData* d) {
-    data = d;
-    int32_t i = 0;
-    while (i<output.length()) {
-        UChar32 c = output.char32At(i);
-        UnicodeFunctor* f = data->lookup(c);
-        if (f != NULL) {
-            f->setData(data);
-        }
-        i += U16_LENGTH(c);
-    }
+    {  return gTrue;
 }
     
-    const std::string& DHTResponseMessage::getType() const { return R; }
     
-    DHTTaskFactoryImpl::DHTTaskFactoryImpl()
-    : routingTable_(nullptr),
-      dispatcher_(nullptr),
-      factory_(nullptr),
-      taskQueue_(nullptr),
-      timeout_(DHT_MESSAGE_TIMEOUT)
-{
-}
+    {  PDFRectangle mediaBox;
+  PDFRectangle cropBox;
+  GBool haveCropBox;
+  PDFRectangle bleedBox;
+  PDFRectangle trimBox;
+  PDFRectangle artBox;
+  int rotate;
+  Object lastModified;
+  Object boxColorInfo;
+  Object group;
+  Object metadata;
+  Object pieceInfo;
+  Object separationInfo;
+  Object resources;
+};
     
-    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
-                                           const std::string& ipaddr,
-                                           uint16_t port) const
-{
-  return generateToken(infoHash, ipaddr, port, secret_[0]);
-}
-    
-    #include 'common.h'
-#include <string>
-    
-    DHTUnknownMessage::DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
-                                     const unsigned char* data, size_t length,
-                                     const std::string& ipaddr, uint16_t port)
-    : DHTMessage(localNode, std::shared_ptr<DHTNode>()),
-      length_(length),
-      ipaddr_(ipaddr),
-      port_(port)
-{
-  if (length_ == 0) {
-    data_ = nullptr;
+      number = index - base + interval->first;
+  switch (interval->style) {
+  case Interval::Arabic:
+    snprintf (buffer, sizeof(buffer), '%d', number);
+    number_string.append(buffer);
+    break;
+  case Interval::LowercaseRoman:
+    toRoman(number, &number_string, gFalse);
+    break;
+  case Interval::UppercaseRoman:
+    toRoman(number, &number_string, gTrue);
+    break;
+  case Interval::UppercaseLatin:
+  case Interval::LowercaseLatin:
+    number = 0;
+    break;
+  case Interval::None:
+    break;
   }
-  else {
-    data_ = new unsigned char[length];
-    memcpy(data_, data, length);
-  }
+    
+    ProfileData::ProfileData() {
+	count = 0;
+	total = 0.0;
+	min = 0.0;
+	max = 0.0;
 }
     
-    namespace aria2 {
+      // Destructor.
+  ~ProfileData() {}
+    
+    
+    {  //
+  // parse Media Screen Parameters
+  if (obj->dictLookup('SP', &tmp2)->isDict()) { // media screen parameters
+    Object params;
+    if (tmp2.dictLookup('MH', &params)->isDict()) {
+      MH.parseMediaScreenParameters(&params);
     }
+    params.free();
+    if (tmp2.dictLookup('BE', &params)->isDict()) {
+      BE.parseMediaScreenParameters(&params);
+    }
+    params.free();
+  }
+  tmp2.free();
+}
+    
+    
+    {  Object encryptDict;
+  XpdfSecurityHandler *xsh;
+  void *docData;
+  int permFlags;
+  Guchar fileKey[16];
+  int fileKeyLength;
+  int encVersion;
+  CryptAlgorithm encAlgorithm;
+  GBool ok;
+};
+#endif // ENABLE_PLUGINS
