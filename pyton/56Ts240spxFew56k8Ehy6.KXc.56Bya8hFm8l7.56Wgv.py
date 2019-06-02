19dcf9c8ae9ae29ce00b87f5d7094982a5d18d00
SUNsung <weatherius@gmail.com>
2019-06-02 08:39:11 +0300

    Rewrite [56Ts240spxFew56k8Ehy6.KXc.56Bya8hFm8l7.56Wgv.py] File in: 2019-06-02 08:39:10

--- pyton/56Ts240spxFew56k8Ehy6.KXc.56Bya8hFm8l7.56Wgv.py
+++ pyton/56Ts240spxFew56k8Ehy6.KXc.56Bya8hFm8l7.56Wgv.py
@@ -1,3 +1,237 @@
 
         
         
+def serialize(regularizer):
+    return serialize_keras_object(regularizer)
+    
+        out1 = utils.preprocess_input(x, 'channels_last')
+    out1int = utils.preprocess_input(xint, 'channels_last')
+    out2 = utils.preprocess_input(np.transpose(x, (2, 0, 1)),
+                                  'channels_first')
+    out2int = utils.preprocess_input(np.transpose(xint, (2, 0, 1)),
+                                     'channels_first')
+    assert_allclose(out1, out2.transpose(1, 2, 0))
+    assert_allclose(out1int, out2int.transpose(1, 2, 0))
+    
+        input_tensor = Input(shape=(data_dim,))
+    dummy_model = Model(input_tensor, dense_layer(input_tensor))
+    
+    This example has modular design. The encoder, decoder and autoencoder
+are 3 models that share weights. For example, after training the
+autoencoder, the encoder can be used to  generate latent vectors
+of input data for low-dim visualization like PCA or TSNE.
+'''
+    
+    # convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+    
+    
+def categorical_crossentropy(y_true, y_pred):
+    return K.categorical_crossentropy(y_true, y_pred)
+    
+    import scrapy
+from scrapy.commands import ScrapyCommand
+from scrapy.utils.template import render_templatefile, string_camelcase
+from scrapy.exceptions import UsageError
+    
+    from antlr3.constants import EOF
+from antlr3.exceptions import NoViableAltException, BacktrackingFailed
+    
+        def __init__(self, state=None):
+        # Input stream of the recognizer. Must be initialized by a subclass.
+        self.input = None
+    
+    	for video in tab.childNodes:
+		if re.search(contentid, video.attributes['link'].value):
+			url = video.attributes['flv'].value
+			break
+    
+        type, ext, size = url_info(video_url, headers=fake_headers)
+    
+            else:
+            # gallery image
+            content = get_content(self.url)
+            image = json.loads(match1(content, r'image\s*:\s*({.*}),'))
+            ext = image['ext']
+            self.streams = {
+                'original': {
+                    'src': ['http://i.imgur.com/%s%s' % (image['hash'], ext)],
+                    'size': image['size'],
+                    'container': ext[1:]
+                },
+                'thumbnail': {
+                    'src': ['http://i.imgur.com/%ss%s' % (image['hash'], '.jpg')],
+                    'container': 'jpg'
+                }
+            }
+            self.title = image['title'] or image['hash']
+    
+        # Post-hoc scale-specific 3x3 convs
+    blobs_fpn = []
+    spatial_scales = []
+    for i in range(num_backbone_stages):
+        if cfg.FPN.USE_GN:
+            # use GroupNorm
+            fpn_blob = model.ConvGN(
+                output_blobs[i],
+                'fpn_{}'.format(fpn_level_info.blobs[i]),
+                dim_in=fpn_dim,
+                dim_out=fpn_dim,
+                group_gn=get_group_gn(fpn_dim),
+                kernel=3,
+                pad=1,
+                stride=1,
+                weight_init=xavier_fill,
+                bias_init=const_fill(0.0)
+            )
+        else:
+            fpn_blob = model.Conv(
+                output_blobs[i],
+                'fpn_{}'.format(fpn_level_info.blobs[i]),
+                dim_in=fpn_dim,
+                dim_out=fpn_dim,
+                kernel=3,
+                pad=1,
+                stride=1,
+                weight_init=xavier_fill,
+                bias_init=const_fill(0.0)
+            )
+        blobs_fpn += [fpn_blob]
+        spatial_scales += [fpn_level_info.spatial_scales[i]]
+    
+    
+def add_fast_rcnn_losses(model):
+    '''Add losses for RoI classification and bounding box regression.'''
+    cls_prob, loss_cls = model.net.SoftmaxWithLoss(
+        ['cls_score', 'labels_int32'], ['cls_prob', 'loss_cls'],
+        scale=model.GetLossScale()
+    )
+    loss_bbox = model.net.SmoothL1Loss(
+        [
+            'bbox_pred', 'bbox_targets', 'bbox_inside_weights',
+            'bbox_outside_weights'
+        ],
+        'loss_bbox',
+        scale=model.GetLossScale()
+    )
+    loss_gradients = blob_utils.get_loss_gradients(model, [loss_cls, loss_bbox])
+    model.Accuracy(['cls_prob', 'labels_int32'], 'accuracy_cls')
+    model.AddLosses(['loss_cls', 'loss_bbox'])
+    model.AddMetrics('accuracy_cls')
+    return loss_gradients
+    
+    
+def _build_forward_graph(model, single_gpu_build_func):
+    '''Construct the forward graph on each GPU.'''
+    all_loss_gradients = {}  # Will include loss gradients from all GPUs
+    # Build the model on each GPU with correct name and device scoping
+    for gpu_id in range(cfg.NUM_GPUS):
+        with c2_utils.NamedCudaScope(gpu_id):
+            all_loss_gradients.update(single_gpu_build_func(model))
+    return all_loss_gradients
+    
+    
+def add_single_scale_rpn_losses(model):
+    '''Add losses for a single scale RPN model (i.e., no FPN).'''
+    # Spatially narrow the full-sized RPN label arrays to match the feature map
+    # shape
+    model.net.SpatialNarrowAs(
+        ['rpn_labels_int32_wide', 'rpn_cls_logits'], 'rpn_labels_int32'
+    )
+    for key in ('targets', 'inside_weights', 'outside_weights'):
+        model.net.SpatialNarrowAs(
+            ['rpn_bbox_' + key + '_wide', 'rpn_bbox_pred'], 'rpn_bbox_' + key
+        )
+    loss_rpn_cls = model.net.SigmoidCrossEntropyLoss(
+        ['rpn_cls_logits', 'rpn_labels_int32'],
+        'loss_rpn_cls',
+        scale=model.GetLossScale()
+    )
+    loss_rpn_bbox = model.net.SmoothL1Loss(
+        [
+            'rpn_bbox_pred', 'rpn_bbox_targets', 'rpn_bbox_inside_weights',
+            'rpn_bbox_outside_weights'
+        ],
+        'loss_rpn_bbox',
+        beta=1. / 9.,
+        scale=model.GetLossScale()
+    )
+    loss_gradients = blob_utils.get_loss_gradients(
+        model, [loss_rpn_cls, loss_rpn_bbox]
+    )
+    model.AddLosses(['loss_rpn_cls', 'loss_rpn_bbox'])
+    return loss_gradients
+
+    
+        # Select foreground RoIs as those with >= FG_THRESH overlap
+    fg_inds = np.where(max_overlaps >= cfg.TRAIN.FG_THRESH)[0]
+    # Guard against the case when an image has fewer than fg_rois_per_image
+    # foreground RoIs
+    fg_rois_per_this_image = np.minimum(fg_rois_per_image, fg_inds.size)
+    # Sample foreground regions without replacement
+    if fg_inds.size > 0:
+        fg_inds = npr.choice(
+            fg_inds, size=fg_rois_per_this_image, replace=False
+        )
+    
+                if k.find('retnet_cls_labels') >= 0:
+                tmp = []
+                # concat anchors within an image
+                for i in range(0, len(v), A):
+                    tmp.append(np.concatenate(v[i: i + A], axis=1))
+                # concat images
+                blobs[k] = np.concatenate(tmp, axis=0)
+            else:
+                # for the bbox branch elements [per FPN level],
+                #  we have the targets and the fg boxes locations
+                # in the shape: M x 4 where M is the number of fg locations in a
+                # given image at the current FPN level. For the given level,
+                # the bbox predictions will be. The elements in the list are in
+                # order [[a0, ..., a9], [a0, ..., a9]]
+                # Concatenate them to form M x 4
+                blobs[k] = np.concatenate(v, axis=0)
+    return True
+    
+        roi_data_loader.register_sigint_handler()
+    roi_data_loader.start(prefill=True)
+    total_time = 0
+    for i in range(opts.num_batches):
+        start_t = time.time()
+        for _ in range(opts.x_factor):
+            workspace.RunNetOnce(net)
+        total_time += (time.time() - start_t) / opts.x_factor
+        logger.info(
+            '{:d}/{:d}: Averge dequeue time: {:.3f}s  [{:d}/{:d}]'.format(
+                i + 1, opts.num_batches, total_time / (i + 1),
+                roi_data_loader._minibatch_queue.qsize(),
+                cfg.DATA_LOADER.MINIBATCH_QUEUE_SIZE
+            )
+        )
+        # Sleep to simulate the time taken by running a little network
+        time.sleep(opts.sleep_time)
+        # To inspect:
+        # blobs = workspace.FetchBlobs(all_blobs)
+        # from IPython import embed; embed()
+    logger.info('Shutting down data loader...')
+    roi_data_loader.shutdown()
+    
+            mocap_id = sim.model.body_mocapid[obj1_id]
+        if mocap_id != -1:
+            # obj1 is the mocap, obj2 is the welded body
+            body_idx = obj2_id
+        else:
+            # obj2 is the mocap, obj1 is the welded body
+            mocap_id = sim.model.body_mocapid[obj2_id]
+            body_idx = obj1_id
+    
+        def register(self, closeable):
+        '''Registers an object with a 'close' method.
+    
+        def gas(self, gas):
+        'control: rear wheel drive'
+        gas = np.clip(gas, 0, 1)
+        for w in self.wheels[2:4]:
+            diff = gas - w.gas
+            if diff > 0.1: diff = 0.1  # gradually increase, but stop immediately
+            w.gas += diff
