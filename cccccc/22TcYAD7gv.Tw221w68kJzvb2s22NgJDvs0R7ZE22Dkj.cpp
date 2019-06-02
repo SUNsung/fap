86dc7518f5723ad5528ffa506e0c15a0e4be5f06
SUNsung <weatherius@gmail.com>
2019-06-02 10:03:38 +0300

    Rewrite [22TcYAD7gv.Tw221w68kJzvb2s22NgJDvs0R7ZE22Dkj.cpp] File in: 2019-06-02 10:03:36

--- cccccc/22TcYAD7gv.Tw221w68kJzvb2s22NgJDvs0R7ZE22Dkj.cpp
+++ cccccc/22TcYAD7gv.Tw221w68kJzvb2s22NgJDvs0R7ZE22Dkj.cpp
@@ -1,123 +1,315 @@
 
         
-        //---- Avoid multiple STB libraries implementations, or redefine path/filenames to prioritize another version
-// By default the embedded implementations are declared static and not available outside of imgui cpp files.
-//#define IMGUI_STB_TRUETYPE_FILENAME   'my_folder/stb_truetype.h'
-//#define IMGUI_STB_RECT_PACK_FILENAME  'my_folder/stb_rect_pack.h'
-//#define IMGUI_DISABLE_STB_TRUETYPE_IMPLEMENTATION
-//#define IMGUI_DISABLE_STB_RECT_PACK_IMPLEMENTATION
+        using namespace swift;
     
-    // InitXXX function with 'install_callbacks=true': install GLFW callbacks. They will call user's previously installed callbacks, if any.
-// InitXXX function with 'install_callbacks=false': do not install GLFW callbacks. You will need to call them yourself from your own GLFW callbacks.
-IMGUI_IMPL_API void     ImGui_ImplGlfw_MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_CharCallback(GLFWwindow* window, unsigned int c);
-
-    
-        stb_out4(0);       // 64-bit length requires 32-bit leading 0
-    stb_out4(length);
-    stb_out4(stb__window);
-    
-        // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-    
-    
-    {        UINT64 fenceValue = g_fenceLastSignaledValue + 1;
-        g_pd3dCommandQueue->Signal(g_fence, fenceValue);
-        g_fenceLastSignaledValue = fenceValue;
-        frameCtxt->FenceValue = fenceValue;
+    unsigned swift::measureNewline(const char *BufferPtr, const char *BufferEnd) {
+  if (BufferPtr == BufferEnd)
+    return 0;
     }
     
-            static float f = 0.0f;
-        ImGui::Text('Hello, world!');
-        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);
-        ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / io.Framerate, io.Framerate);
-        ImGui::ShowDemoWindow(NULL);
+    struct IAMOptions {
+  // TODO: fine-grained control over how we infer
+  static IAMOptions getDefault();
+};
     
     
-    {     // Show/hide OSD keyboard
-    if (io.WantTextInput)
-    {
-        // Some text input widget is active?
-        if (!g_osdKeyboardEnabled)
-        {
-            g_osdKeyboardEnabled = true;
-            s3eKeyboardSetInt(S3E_KEYBOARD_GET_CHAR, 1);    // show OSD keyboard
-        }
-    }
-    else
-    {
-        // No text input widget is active
-        if (g_osdKeyboardEnabled)
-        {
-            g_osdKeyboardEnabled = false;
-            s3eKeyboardSetInt(S3E_KEYBOARD_GET_CHAR, 0);    // hide OSD keyboard
-        }
-    }
-}
-
-    
-        // create values of different floating-point types
-    json::number_float_t v_ok = 3.141592653589793;
-    json::number_float_t v_nan = NAN;
-    json::number_float_t v_infinity = INFINITY;
-    
-    TEST(ByteTest, CopyConstructor) {
-  unsigned char byte_value = 0xFF;
-  Byte value(&byte_value);
-  Byte another_value(value);
-  EXPECT_EQ(another_value.to_hex_string(), value.to_hex_string());
-  EXPECT_EQ(another_value.to_binary_string(), value.to_binary_string());
+    {  return 0;
 }
     
-    
-    {  Byte t1(bytes + 2);
-  uint32_t t = t1.get_byte(3, 5);
-  x <<= 5;
-  x |= t;
-  double ret = x * CLUSTER_DIST_RES + CLUSTER_DIST_LONG_MIN;
-  return ret;
-}
-    
-    
-    {  int ret = x;
-  return ret;
-}
-    
-    
-    {  int ret = x;
-  return ret;
-}
-    
-        auto it_lower = std::lower_bound(
-        speed_limit_.speed_limit_points().begin(),
-        speed_limit_.speed_limit_points().end(), s,
-        [](const std::pair<double, double>& point, const double curr_s) {
-          return point.first < curr_s;
-        });
-    
-    Eigen::MatrixXd SplineSegKernel::SecondOrderDerivativeKernel(
-    const uint32_t num_params, const double accumulated_x) {
-  if (num_params > reserved_order_ + 1) {
-    CalculateSecondOrderDerivative(num_params);
+    namespace google {
+namespace protobuf {
+namespace util {
+namespace error {
+inline string CodeEnumToString(error::Code code) {
+  switch (code) {
+    case OK:
+      return 'OK';
+    case CANCELLED:
+      return 'CANCELLED';
+    case UNKNOWN:
+      return 'UNKNOWN';
+    case INVALID_ARGUMENT:
+      return 'INVALID_ARGUMENT';
+    case DEADLINE_EXCEEDED:
+      return 'DEADLINE_EXCEEDED';
+    case NOT_FOUND:
+      return 'NOT_FOUND';
+    case ALREADY_EXISTS:
+      return 'ALREADY_EXISTS';
+    case PERMISSION_DENIED:
+      return 'PERMISSION_DENIED';
+    case UNAUTHENTICATED:
+      return 'UNAUTHENTICATED';
+    case RESOURCE_EXHAUSTED:
+      return 'RESOURCE_EXHAUSTED';
+    case FAILED_PRECONDITION:
+      return 'FAILED_PRECONDITION';
+    case ABORTED:
+      return 'ABORTED';
+    case OUT_OF_RANGE:
+      return 'OUT_OF_RANGE';
+    case UNIMPLEMENTED:
+      return 'UNIMPLEMENTED';
+    case INTERNAL:
+      return 'INTERNAL';
+    case UNAVAILABLE:
+      return 'UNAVAILABLE';
+    case DATA_LOSS:
+      return 'DATA_LOSS';
   }
-  Eigen::MatrixXd term_matrix;
-  IntegratedTermMatrix(num_params, accumulated_x, 'second_order', &term_matrix);
-  return kernel_second_order_derivative_.block(0, 0, num_params, num_params)
-      .cwiseProduct(term_matrix);
+    }
+    }
+    }
+    }
+    }
+    
+    TEST(StructurallyValidTest, InvalidUTF8String) {
+  const string invalid_str('abcd\xA0\xB0\xA0\xB0\xA0\xB0 - xyz789');
+  EXPECT_FALSE(IsStructurallyValidUTF8(invalid_str.data(),
+                                       invalid_str.size()));
+  // Additional check for pointer alignment
+  for (int i = 1; i < 8; ++i) {
+    EXPECT_FALSE(IsStructurallyValidUTF8(invalid_str.data() + i,
+                                         invalid_str.size() - i));
+  }
 }
     
-    #include 'modules/canbus/vehicle/gem/protocol/brake_rpt_6c.h'
+    // ----
+// Author: lar@google.com (Laramie Leavitt)
+//
+// These tests are really compile time tests.
+// If you try to step through this in a debugger
+// you will not see any evaluations, merely that
+// value is assigned true or false sequentially.
+    
+    #include <google/protobuf/io/gzip_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+    
+      while (true) {
+    void* outptr;
+    int outlen;
+    bool ok;
+    do {
+      ok = out.Next(&outptr, &outlen);
+      if (!ok) {
+        break;
+      }
+    } while (outlen <= 0);
+    readlen = read(STDIN_FILENO, outptr, outlen);
+    if (readlen <= 0) {
+      out.BackUp(outlen);
+      break;
+    }
+    if (readlen < outlen) {
+      out.BackUp(outlen - readlen);
+    }
+  }
+    
+        for (int i = 0; i < dataset.payload_size(); i++) {
+      message->ParseFromString(dataset.payload(i));
+      Proto3DataStripper stripper;
+      stripper.StripMessage(message);
+      dataset.set_payload(i, message->SerializeAsString());
+    }
+    
+    using google::protobuf::Descriptor;
+using google::protobuf::DescriptorProto;
+using google::protobuf::FileDescriptorProto;
+using google::protobuf::FieldDescriptorProto;
+using google::protobuf::Message;
+using google::protobuf::EnumValueDescriptorProto;
+    
+    using google::protobuf::util::TimeUtil;
+    
+        for (int j = 0; j < person.phones_size(); j++) {
+      const tutorial::Person::PhoneNumber& phone_number = person.phones(j);
+    }
+    
+     private:
+  StreamBufferReader reader_;
+  int tmp_ch;
+  int num_prev;
+  unsigned char buf_prev[2];
+  // whether we need to do strict check
+  static const bool kStrictCheck = false;
+};
+/*! \brief the stream that write to base64, note we take from file pointers */
+class Base64OutStream: public dmlc::Stream {
+ public:
+  explicit Base64OutStream(dmlc::Stream *fp) : fp(fp) {
+    buf_top = 0;
+  }
+  virtual void Write(const void *ptr, size_t size) {
+    using base64::EncodeTable;
+    size_t tlen = size;
+    const unsigned char *cptr = static_cast<const unsigned char*>(ptr);
+    while (tlen) {
+      while (buf_top < 3  && tlen != 0) {
+        buf[++buf_top] = *cptr++; --tlen;
+      }
+      if (buf_top == 3) {
+        // flush 4 bytes out
+        PutChar(EncodeTable[buf[1] >> 2]);
+        PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
+        PutChar(EncodeTable[((buf[2] << 2) | (buf[3] >> 6)) & 0x3F]);
+        PutChar(EncodeTable[buf[3] & 0x3F]);
+        buf_top = 0;
+      }
+    }
+  }
+  virtual size_t Read(void *ptr, size_t size) {
+    LOG(FATAL) << 'Base64OutStream do not support read';
+    return 0;
+  }
+  /*!
+   * \brief finish writing of all current base64 stream, do some post processing
+   * \param endch character to put to end of stream, if it is EOF, then nothing will be done
+   */
+  inline void Finish(char endch = EOF) {
+    using base64::EncodeTable;
+    if (buf_top == 1) {
+      PutChar(EncodeTable[buf[1] >> 2]);
+      PutChar(EncodeTable[(buf[1] << 4) & 0x3F]);
+      PutChar('=');
+      PutChar('=');
+    }
+    if (buf_top == 2) {
+      PutChar(EncodeTable[buf[1] >> 2]);
+      PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
+      PutChar(EncodeTable[(buf[2] << 2) & 0x3F]);
+      PutChar('=');
+    }
+    buf_top = 0;
+    if (endch != EOF) PutChar(endch);
+    this->Flush();
+  }
+    
+    
+    { protected:
+  /*! \brief internal base score of the model */
+  bst_float base_score_;
+  /*! \brief objective function */
+  std::unique_ptr<ObjFunction> obj_;
+  /*! \brief The gradient booster used by the model*/
+  std::unique_ptr<GradientBooster> gbm_;
+  /*! \brief The evaluation metrics used to evaluate the model. */
+  std::vector<std::unique_ptr<Metric> > metrics_;
+  /*! \brief Training parameter. */
+  LearnerTrainParam tparam_;
+};
+    
+    namespace xgboost {
+struct LearnerTrainParam : public dmlc::Parameter<LearnerTrainParam> {
+  // stored random seed
+  int seed;
+  // whether seed the PRNG each iteration
+  bool seed_per_iteration;
+  // data split mode, can be row, col, or none.
+  DataSplitMode dsplit;
+  // tree construction method
+  TreeMethod tree_method;
+  // number of threads to use if OpenMP is enabled
+  // if equals 0, use system default
+  int nthread;
+  // flag to disable default metric
+  int disable_default_eval_metric;
+  // primary device.
+  int gpu_id;
+  // number of devices to use, -1 implies using all available devices.
+  int n_gpus;
+    }
+    }
+    
+    namespace xgboost {
+/*!
+ * \brief interface of evaluation metric used to evaluate model performance.
+ *  This has nothing to do with training, but merely act as evaluation purpose.
+ */
+class Metric {
+ protected:
+  LearnerTrainParam const* tparam_;
+    }
+    }
+    
+    namespace xgboost {
+/*!
+ * \brief interface of tree update module, that performs update of a tree.
+ */
+class TreeUpdater {
+ protected:
+  LearnerTrainParam const* tparam_;
+    }
+    }
+    
+      static GPUSet Empty() { return GPUSet(); }
+    
+      void moveBucketTail(const std::shared_ptr<DHTNode>& node);
+    
+    #include <vector>
+#include <memory>
+    
+      virtual std::shared_ptr<DHTTask>
+  createPingTask(const std::shared_ptr<DHTNode>& remoteNode,
+                 int numRetry = 0) = 0;
+    
+    namespace aria2 {
+    }
+    
+      using ::apache::thrift::protocol::TProtocolException;
+    
+      _ExtensionManager_options_result__isset __isset;
+    
+      void shutdown() {
+    // Your implementation goes here
+    printf('shutdown\n');
+  }
+    
+      virtual ~ExtensionResponse() throw();
+  ExtensionStatus status;
+  ExtensionPluginResponse response;
+    
+    #include <osquery/tables.h>
+    
+    /*
+** Read input from *in and process it.  If *in==0 then input
+** is interactive - the user is typing it it.  Otherwise, input
+** is coming from a file or device.  A prompt is issued and history
+** is saved only if input is interactive.  An interrupt signal will
+** cause this routine to exit immediately, unless input is interactive.
+**
+** Return the number of errors.
+*/
+static int process_input(struct callback_data* p, FILE* in) {
+  /* A single input line */
+  char* zLine = nullptr;
+    }
+    
+          resp.content['disk_id'] = std::to_string(i);
+      // Change device type to driver_typerrr
+      resp.content['driver_type'] =
+          type->driver.substr(0, type->driver.length() - 1);
+    
+    namespace osquery {
+namespace table_tests {
+    }
+    }
+    
+    
+    {
+    {} // namespace table_tests
+} // namespace osquery
+
+    
+    namespace osquery {
+namespace table_tests {
+    }
+    }
+    
+    int main()
+{
+    // create a JSON value
+    json j =
+    {
+        {'number', 1}, {'string', 'foo'}, {'array', {1, 2}}
+    };
+    }
