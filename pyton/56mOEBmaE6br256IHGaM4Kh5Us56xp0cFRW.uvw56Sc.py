6b6e48d536efcc9eb364e357a47c5fa50aec1ffb
SUNsung <weatherius@gmail.com>
2019-06-02 17:52:13 +0300

    Rewrite [56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py] File in: 2019-06-02 17:52:13

--- pyton/56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py
+++ pyton/56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py
@@ -1,86 +1,134 @@
 
         
-        def _make_failed_load_tests(name, exception, suiteClass):
-    message = 'Failed to call load_tests:\n%s' % (traceback.format_exc(),)
-    return _make_failed_test(
-        name, exception, suiteClass, message)
-    
-    # Same, but for 3.x to 2.x
-REVERSE_IMPORT_MAPPING = dict((v, k) for (k, v) in IMPORT_MAPPING.items())
-assert len(REVERSE_IMPORT_MAPPING) == len(IMPORT_MAPPING)
-REVERSE_NAME_MAPPING = dict((v, k) for (k, v) in NAME_MAPPING.items())
-assert len(REVERSE_NAME_MAPPING) == len(NAME_MAPPING)
-    
-    
-if __name__ == '__main__':
-    main()
+          Args:
+    pairs: the word pairs (list of tuple of two strings).
+    labels: the gold-standard labels for these pairs (array of rel ID).
+    predictions: the predicted labels for these pairs (array of rel ID).
+    classes: a list of relation names.
+    predictions_file: where to save the predictions.
+  '''
+  with open(predictions_file, 'w') as f_out:
+    for pair, label, pred in zip(pairs, labels, predictions):
+      w1, w2 = pair
+      f_out.write('\t'.join([w1, w2, classes[label], classes[pred]]) + '\n')
 
     
+    flags = tf.app.flags
+flags.DEFINE_string('kind', 'train',
+                    'Type of model to build {train, \
+                    posterior_sample_and_average, \
+                    posterior_push_mean, \
+                    prior_sample, write_model_params')
+flags.DEFINE_string('output_dist', OUTPUT_DISTRIBUTION,
+                    'Type of output distribution, 'poisson' or 'gaussian'')
+flags.DEFINE_boolean('allow_gpu_growth', False,
+                     'If true, only allocate amount of memory needed for \
+                     Session. Otherwise, use full GPU memory.')
+    }
     
-def main():
-    parser = ArgumentParser(description='''\
-Unpack a MIME message into a directory of files.
-''')
-    parser.add_argument('-d', '--directory', required=True,
-                        help='''Unpack the MIME message into the named
-                        directory, which will be created if it doesn't already
-                        exist.''')
-    parser.add_argument('msgfile')
-    args = parser.parse_args()
-    
-        print('-' * 20)
-    
-    References:
-    tensorlayer.layers.Conv2dLayer
-'''
-import tensorflow as tf
-    
-        def _build(self, n_input):
-        ''''''
-        with tf.variable_scope(self.name or 'dense'):
-            self.W, self.b = get_wb([n_input, self.n_unit])
-    
-      {} requires <code>A File Icon</code> package for enhanced<br>support of
-  the file-specific icons.
-  <br><br>Would you like to install it?<br>
-  <br><a href='install'>Install</a> <a href='cancel'>Cancel</a>
-</div>
-'''.format(THEME_NAME)
-    
-    
-def revert_ui_theme(path):
-  set_ui_theme(path)
-    
-        :param repo_dir: Local directory of newly cloned repo.
-    :returns project_template: Relative path to project template.
+      Args:
+    data_e: nexamples length list of NxT trials
+    dt: how often the data are sampled
+    max_firing_rate: the firing rate that is associated with a value of 1.0
+  Returns:
+    gauss_e: a list of length b of the data with noise.
     '''
-    logger.debug('Searching {} for the project template.'.format(repo_dir))
     
-    # If false, no index is generated.
-#epub_use_index = True
+      def reset(self):
+    self.sess.run(self.tensors['states_init'])
     
-            # Carefully delete the created ~/.cookiecutter_replay dir only in
-        # certain conditions.
-        restore_backup_dir(
-            cookiecutter_replay_dir,
-            cookiecutter_replay_dir_backup,
-            cookiecutter_replay_dir_found
-        )
+      # Add the 0th time-step for coherence.
+  first_token = np.expand_dims(inputs_eval[:, 0], axis=1)
+  sequence_eval = np.concatenate((first_token, sequence_eval), axis=1)
     
     
-@pytest.fixture
-def replay_file(replay_test_dir, template_name):
-    '''Fixture to return a actual file name of the dump.'''
-    file_name = '{}.json'.format(template_name)
-    return os.path.join(replay_test_dir, file_name)
+def create_dis_pretrain_op(hparams, dis_loss, global_step):
+  '''Create a train op for pretraining.'''
+  with tf.name_scope('pretrain_generator'):
+    optimizer = tf.train.AdamOptimizer(hparams.dis_pretrain_learning_rate)
+    dis_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('dis')
+    ]
+    if FLAGS.dis_update_share_embedding and FLAGS.dis_share_embedding:
+      shared_embedding = [
+          v for v in tf.trainable_variables()
+          if v.op.name == 'gen/decoder/rnn/embedding'
+      ][0]
+      dis_vars.append(shared_embedding)
+    dis_grads = tf.gradients(dis_loss, dis_vars)
+    dis_grads_clipped, _ = tf.clip_by_global_norm(dis_grads,
+                                                  FLAGS.grad_clipping)
+    dis_pretrain_op = optimizer.apply_gradients(
+        zip(dis_grads_clipped, dis_vars), global_step=global_step)
+    return dis_pretrain_op
     
-        assert str(err.value) == (
-        'A valid repository for '{}' could not be found in the following '
-        'locations:\n{}'.format(
-            template_path,
-            '\n'.join([
-                template_path,
-                str(tmpdir / 'tests/unknown-repo')
-            ]),
-        )
-    )
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+      Args:
+    hparams:  Hyperparameters for the MaskGAN.
+    model:  Model type, one of ['gen', 'dis'].
+    
+        def reset_appid(self):
+        # called by web_control
+        with self.lock:
+            self.working_appid_list = list()
+            for appid in self.config.GAE_APPIDS:
+                if not appid:
+                    self.config.GAE_APPIDS.remove(appid)
+                    continue
+                self.working_appid_list.append(appid)
+            self.not_exist_appids = []
+            self.out_of_quota_appids = []
+        self.last_reset_time = time.time()
+    
+                return CertUtil._get_cert(commonname, isip, sans)
+    
+    from front_base.openssl_wrap import SSLContext
+from front_base.connect_creator import ConnectCreator
+from front_base.host_manager import HostManagerBase
+import front_base.check_ip
+from config import config
+    
+                if fail or self.network_stat != 'OK':
+                # Fail or unknown
+                if time_now - self.last_check_time < 3:
+                    return
+            else:
+                if time_now - self.last_check_time < 10:
+                    return
+    
+    \note Please be warned that the line numbers in the API documentation do not
+match the real locations in the source code of the package. This is an
+unintended artifact of doxygen, which I could only convince to use the
+correct module names by concatenating all files from the package into a single
+module file...
+    
+                if isinstance(self.input, TokenStream):
+                self.token = self.input.LT(1)
+                self.line = self.token.line
+                self.charPositionInLine = self.token.charPositionInLine
+    
+    
+    def rewind(self, marker=None):
+        '''
+        Reset the stream so that next call to index would return marker.
+        The marker will usually be index() but it doesn't have to be.  It's
+        just a marker to indicate what state the stream was in.  This is
+        essentially calling release() and seek().  If there are markers
+        created after this marker argument, this routine must unroll them
+        like a stack.  Assume the state the stream was in when this marker
+        was created.
+    
+        # Replace quoted strings and digit separators.  Both single quotes
+    # and double quotes are processed in the same loop, otherwise
+    # nested quotes wouldn't work.
+    collapsed = ''
+    while True:
+      # Find the first quote character
+      match = Match(r'^([^\'']*)([\''])(.*)$', elided)
+      if not match:
+        collapsed += elided
+        break
+      head, quote, tail = match.groups()
