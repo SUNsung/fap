8f8df1d306bd0272e70da27e229995d892f69e0a
SUNsung <weatherius@gmail.com>
2019-06-03 01:06:52 +0300

    Rewrite [331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py] File in: 2019-06-03 01:06:51

--- pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
+++ pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
@@ -1,113 +1,71 @@
 
         
-          labels = tf.squeeze(curr_features['rel_id'], [-1])
-  return labels
+            def bfs(self, source, dest):
+        if source is None:
+            return False
+        queue = deque()
+        queue.append(source)
+        source.visit_state = State.visited
+        while queue:
+            node = queue.popleft()
+            print(node)
+            if dest is node:
+                return True
+            for adjacent_node in node.adj_nodes.values():
+                if adjacent_node.visit_state == State.unvisited:
+                    queue.append(adjacent_node)
+                    adjacent_node.visit_state = State.visited
+        return False
     
-        for index, instance in enumerate(inputs):
+    import pylons
+from pylons.i18n.translation import _get_translator
+from routes.util import URLGenerator
+from pylons import url
+import baseplate.events
+import pkg_resources
+import paste.fixture
+import paste.script.appinstall
+from paste.deploy import loadapp
     
-    # Sometimes the task can be sufficiently hard to learn that the
-# optimizer takes the 'easy route', and simply minimizes the KL
-# divergence, setting it to near zero, and the optimization gets
-# stuck.  These two parameters will help avoid that by by getting the
-# optimization to 'latch' on to the main optimization, and only
-# turning in the regularizers later.
-flags.DEFINE_integer('kl_start_step', KL_START_STEP,
-                     'Start increasing weight after this many steps.')
-# training passes, not epochs, increase by 0.5 every kl_increase_steps
-flags.DEFINE_integer('kl_increase_steps', KL_INCREASE_STEPS,
-                     'Increase weight of kl cost to avoid local minimum.')
-# Same story for l2 regularizer.  One wants a simple generator, for scientific
-# reasons, but not at the expense of hosing the optimization.
-flags.DEFINE_integer('l2_start_step', L2_START_STEP,
-                     'Start increasing l2 weight after this many steps.')
-flags.DEFINE_integer('l2_increase_steps', L2_INCREASE_STEPS,
-                     'Increase weight of l2 cost to avoid local minimum.')
+            # Make get_ids return a valid payment_method_id
+        get_ids.return_value.append(payment_method_id)
+        # Assign arbitrary CustomerID, which comes from Authorize
+        get_id.return_value = 1000
+        create_authorization_hold.return_value = transaction_id
     
-    # Getting hard vs. easy data can be a little stochastic, so we set the seed.
+        def test_dict(self):
+        self.assertEquals({}, ConfigValue.dict(str, str)(''))
+        self.assertEquals({'a': ''}, ConfigValue.dict(str, str)('a'))
+        self.assertEquals({'a': 3}, ConfigValue.dict(str, int)('a: 3'))
+        self.assertEquals({'a': 3, 'b': 4},
+                          ConfigValue.dict(str, int)('a: 3, b: 4'))
+        self.assertEquals({'a': (3, 5), 'b': (4, 6)},
+                          ConfigValue.dict(
+                              str, ConfigValue.tuple_of(int), delim=';')
+                          ('a: 3, 5;  b: 4, 6'))
     
-    x0s = []
-condition_labels = []
-condition_number = 0
-for c in range(C):
-  x0 = FLAGS.x0_std * rng.randn(N, 1)
-  x0s.append(np.tile(x0, nreplications))
-  for ns in range(nreplications):
-    condition_labels.append(condition_number)
-  condition_number += 1
-x0s = np.concatenate(x0s, axis=1)
+        def test_is_superuser(self):
+        perm_set = PermissionSet()
+        self.assertFalse(perm_set.is_superuser())
+        perm_set[perm_set.ALL] = True
+        self.assertTrue(perm_set.is_superuser())
+        perm_set[perm_set.ALL] = False
+        self.assertFalse(perm_set.is_superuser())
     
-      Raises:
-    ValueError: if batch_size or num_steps are too high.
-  '''
-  del epoch_size_override
-  data_len = len(raw_data)
-  num_batches = data_len // batch_size - 1
+        def test_signing(self):
+        epoch_time = 1234567890
+        header = self.make_sig_header(
+            '{'user': 'reddit', 'password': 'hunter2'}',
+            epoch=epoch_time,
+        )
+        self.assertEqual(
+            header,
+            '1:test:1:1234567890:'
+            '0fc3d90d83ac7433a5376c17f2aea9b470c368740c91c513e819e3a4980349de'
+        )
     
-      elif FLAGS.baseline_method is None:
-    num_missing = tf.reduce_sum(missing)
-    final_gen_objective += tf.reduce_sum(rewards) / (num_missing + eps)
-    baselines = tf.zeros_like(rewards)
-    critic_loss = None
-    maintain_averages_op = None
-    advantages = cumulative_rewards
-    
-      Args:
-    name: to label variables.
-    num_units: hidden state dimension.
-    attention_option: how to compute attention, either 'luong' or 'bahdanau'.
-      'bahdanau': additive (Bahdanau et al., ICLR'2015)
-      'luong': multiplicative (Luong et al., EMNLP'2015)
-    reuse: whether to reuse variable scope.
-    dtype: (default: `tf.float32`) data type to use.
-    
-        if not FLAGS.dis_share_embedding:
-      embedding = tf.get_variable('embedding',
-                                  [FLAGS.vocab_size, hparams.dis_rnn_size])
-    
-            :param mime: E.g., 'application/atom+xml'.
-        :param content: The body content as text
-    
-        # noinspection PyMethodOverriding
-    def get_auth(self, username, password):
-        return HTTPBasicAuth(username, password)
-    
-    
-def repr_dict_nice(d):
-    def prepare_dict(d):
-        for k, v in d.items():
-            if isinstance(v, dict):
-                v = dict(prepare_dict(v))
-            elif isinstance(v, bytes):
-                v = v.decode('utf8')
-            elif not isinstance(v, (int, str)):
-                v = repr(v)
-            yield k, v
-    return json.dumps(
-        dict(prepare_dict(d)),
-        indent=4, sort_keys=True,
-    )
-    
-    
-FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
-FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
-JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
-BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
-    
-    
-def test_unicode_raw_json_item(httpbin):
-    r = http('--json', 'POST', httpbin.url + '/post',
-             u'test:={ '%s' : [ '%s' ] }' % (UNICODE, UNICODE))
-    assert HTTP_OK in r
-    assert r.json['json'] == {'test': {UNICODE: [UNICODE]}}
-    
-        '''
-    
-    
-def __kilometers_to_meters(kilometers: float) -> float:
-    '''Convert kilometers to meters.'''
-    return kilometers * 1000
-    
-        # with patch('homeassistant.config.async_hass_config_yaml',
-    #            Mock(return_value=mock_coro(config))):
-    #     yield from hass.services.async_call(
-    #         automation.DOMAIN, automation.SERVICE_RELOAD, blocking=True)
+        def test_unpad_evil_message(self):
+        from r2.lib.tracking import _unpad_message
+        evil = ('a' * 88) + chr(57)
+        result = _unpad_message(evil)
+        self.assertEquals(result, '')
