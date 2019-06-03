21880c3d44bffef72c46af6ab0b10cb5cffa4e9f
SUNsung <weatherius@gmail.com>
2019-06-03 15:10:45 +0300

    Rewrite [30M9.sNps8kE307ZESvm7plc630B5fAg4eaYaw30Je4.rb] File in: 2019-06-03 15:10:44

--- ruby/30M9.sNps8kE307ZESvm7plc630B5fAg4eaYaw30Je4.rb
+++ ruby/30M9.sNps8kE307ZESvm7plc630B5fAg4eaYaw30Je4.rb
@@ -1,121 +1,87 @@
 
         
-                  add_default_name_and_id_for_value(tag_value, name_and_id)
-          options.delete('index')
-          options.delete('namespace')
-          options['for'] = name_and_id['id'] unless options.key?('for')
+                def preload_pipeline_warnings
+          # This preloads the number of warnings for every pipeline, ensuring
+          # that Ci::Pipeline#has_warnings? doesn't execute any additional
+          # queries.
+          @pipeline.number_of_warnings
+        end
     
-              def field_type
-            self.class.field_type
+    module Gitlab
+  module GithubImport
+    module Importer
+      class LfsObjectsImporter
+        include ParallelScheduling
+    
+            # We inject the page number here to make sure that all importers always
+        # start where they left off. Simply starting over wouldn't work for
+        # repositories with a lot of data (e.g. tens of thousands of comments).
+        options = collection_options.merge(page: page_counter.current)
+    
+            # Initializes the system. Any subclasses MUST make sure this
+        # method is called on the parent. Therefore, if a subclass overrides
+        # `initialize`, then you must call `super`.
+        def initialize(vm)
+          @vm = vm
+        end
+    
+              @registered.each do |plugin|
+            hosts.merge!(plugin.host.to_hash)
           end
-      end
-    end
-  end
+    
+            # This is the method called to provision the system. This method
+        # is expected to do whatever necessary to provision the system (create files,
+        # SSH, etc.)
+        def provision!
+        end
+    
+    Then(/^the failure task will run$/) do
+  failed = TestApp.shared_path.join('failed')
+  run_vagrant_command(test_file_exists(failed))
 end
-
     
-          # Returns true if the given value is present in the set.
+          # Returns the else branch of the `case` statement, if any.
       #
-      # raw_key - The key of the set to check.
-      # value - The value to check for.
-      def self.set_includes?(raw_key, value)
-        key = cache_key_for(raw_key)
-    
-          def custom_api_endpoint
-        github_omniauth_provider.dig('args', 'client_options', 'site')
+      # @return [Node] the else branch node of the `case` statement
+      # @return [nil] if the case statement does not have an else branch.
+      def else_branch
+        node_parts[-1]
       end
     
-            def sidekiq_worker_class
-          ImportIssueWorker
-        end
+          # The body of the method definition.
+      #
+      # @note this can be either a `begin` node, if the method body contains
+      #       multiple expressions, or any other node, if it contains a single
+      #       expression.
+      #
+      # @return [Node] the body of the method definition
+      def body
+        node_parts[0]
+      end
     
-            def collection_method
-          :issues_comments
-        end
+    module RuboCop
+  module Cop
+    # This auto-corrects gem dependency order
+    class OrderedGemCorrector
+      extend OrderedGemNode
     
-          private
+                1.upto(3) do |i|
+              next if !arguments[i] || arguments[i].hash_type?
     
-        def root?
-      path == 'index'
-    end
-    
-        def url
-      @url ||= URL.parse request.base_url
-    end
-    
-            css('.l-sub-section', '.alert', '.banner').each do |node|
-          node.name = 'blockquote'
-        end
-    
-            css('*[layout]').remove_attr('layout')
-        css('*[layout-xs]').remove_attr('layout-xs')
-        css('*[flex]').remove_attr('flex')
-        css('*[flex-xs]').remove_attr('flex-xs')
-        css('*[ng-class]').remove_attr('ng-class')
-        css('*[align]').remove_attr('align')
-        css('h1, h2, h3').remove_attr('class')
-    
-            # Merge another configuration object into this one. This assumes that
-        # the other object is the same class as this one. This should not
-        # mutate this object, but instead should return a new, merged object.
-        #
-        # The default implementation will simply iterate over the instance
-        # variables and merge them together, with this object overriding
-        # any conflicting instance variables of the older object. Instance
-        # variables starting with '__' (double underscores) will be ignored.
-        # This lets you set some sort of instance-specific state on your
-        # configuration keys without them being merged together later.
-        #
-        # @param [Object] other The other configuration object to merge from,
-        #   this must be the same type of object as this one.
-        # @return [Object] The merged object.
-        def merge(other)
-          result = self.class.new
-    
-            include Vagrant::Util
-    
-            # Sets a human-friendly description of the plugin.
-        #
-        # @param [String] value Description of the plugin.
-        # @return [String] Description of the plugin.
-        def self.description(value=UNSET_VALUE)
-          get_or_set(:description, value)
-        end
-    
-              # Make sure we're only working with one VM if single target
-          if options[:single_target] && machines.length != 1
-            @logger.debug('Using primary machine since single target')
-            primary_name = @env.primary_machine_name
-            raise Errors::MultiVMTargetRequired if !primary_name
-            machines = [get_machine.call(primary_name)]
+              if variable.explicit_block_local_variable?
+            message
+          else
+            augment_message(message, variable)
           end
-    
-            # This is called early, before a machine is instantiated, to check
-        # if this provider is installed. This should return true or false.
-        #
-        # If the provider is not installed and Vagrant determines it is
-        # able to install this provider, then it will do so. Installation
-        # is done by calling Environment.install_provider.
-        #
-        # If Environment.can_install_provider? returns false, then an error
-        # will be shown to the user.
-        def self.installed?
-          # By default return true for backwards compat so all providers
-          # continue to work.
-          true
         end
     
-    # This is basically a copy of the original bundler 'bundle' shim
-# with the addition of the loading of our Bundler patches that
-# modify Bundler's caching behaviour.
-    
-          it 'returns true if the pipeline is a system pipeline' do
-        expect(subject.system?).to be_truthy
-      end
+        def script_url_for(gist_id, filename)
+      url = 'https://gist.github.com/#{gist_id}.js'
+      url = '#{url}?file=#{filename}' unless filename.nil? or filename.empty?
+      url
     end
     
-      # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-    
-      # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+        def render(context)
+      file_dir = (context.registers[:site].source || 'source')
+      file_path = Pathname.new(file_dir).expand_path
+      file = file_path + @file
