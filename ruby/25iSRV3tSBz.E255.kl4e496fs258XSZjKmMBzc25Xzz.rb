814dd821835fa9fdae6a074e1fd43a30877abfe3
SUNsung <weatherius@gmail.com>
2019-06-03 18:19:16 +0300

    Rewrite [25iSRV3tSBz.E255.kl4e496fs258XSZjKmMBzc25Xzz.rb] File in: 2019-06-03 18:19:13

--- ruby/25iSRV3tSBz.E255.kl4e496fs258XSZjKmMBzc25Xzz.rb
+++ ruby/25iSRV3tSBz.E255.kl4e496fs258XSZjKmMBzc25Xzz.rb
@@ -1,4 +1,42 @@
 
         
-          def execute
-    signal_deprecation_warning_for_pack
+          private
+    
+      def export_filename
+    '#{controller_name}.csv'
+  end
+end
+
+    
+            def run
+          print_version
+          signal_end_of_output
+          listen
+        end
+    
+          def role_properties_for(rolenames)
+        roles = rolenames.to_set
+        rps = Set.new unless block_given?
+        roles_for(rolenames).each do |host|
+          host.roles.intersection(roles).each do |role|
+            [host.properties.fetch(role)].flatten(1).each do |props|
+              if block_given?
+                yield host, role, props
+              else
+                rps << (props || {}).merge(role: role, hostname: host.hostname)
+              end
+            end
+          end
+        end
+        block_given? ? nil : rps
+      end
+    
+    
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'paths'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'selectors'))
+    
+        def self.names_for(klass)
+      instance.names_for(klass)
+    end
