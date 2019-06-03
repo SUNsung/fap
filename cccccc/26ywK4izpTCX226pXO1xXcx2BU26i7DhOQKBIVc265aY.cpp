93a25bf002623c07290189df76560faf75acc520
SUNsung <weatherius@gmail.com>
2019-06-03 20:25:12 +0300

    Rewrite [26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp] File in: 2019-06-03 20:25:12

--- cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
+++ cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
@@ -1,124 +1,104 @@
 
         
-        // Hash function for PyBfloat16. We use the identity function, which is a weak
-// hash function.
-HashType PyBfloat16_Hash(PyObject* self) {
-  bfloat16 x = reinterpret_cast<PyBfloat16*>(self)->value;
-  return x.value;
-}
+        #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
     
-    #include <Python.h>
+    #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
     
-    Status TF_TensorToPyArray(Safe_TF_TensorPtr tensor, PyObject** out_ndarray);
+        std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> output(
+        context->Open(basename + '.proto'));
+    string content = new_pool_.BuildFile(new_file)->DebugString();
+    Printer printer(output.get(), '$');
+    printer.WriteRaw(content.c_str(), content.size());
     
-    // Destructor passed to TF_NewTensor when it reuses a numpy buffer. Stores a
-// pointer to the pyobj in a buffer to be dereferenced later when we're actually
-// holding the GIL. Data and len are ignored.
-void DelayedNumpyDecref(void* data, size_t len, void* obj);
-    
-    #include <Python.h>
-    
-    string TryFindKernelClass(const string& serialized_node_def) {
-  tensorflow::NodeDef node_def;
-  if (!node_def.ParseFromString(serialized_node_def)) {
-    LOG(WARNING) << 'Error parsing node_def';
-    return '';
-  }
+    class EnumGenerator {
+ public:
+  explicit EnumGenerator(const EnumDescriptor* descriptor);
+  ~EnumGenerator();
     }
     
-    // This enum represents potential configurations of L1/shared memory when
-// running a particular kernel. These values represent user preference, and
-// the runtime is not required to respect these choices.
-enum class KernelCacheConfig {
-  // Indicates no preference for device L1/shared memory configuration.
-  kNoPreference,
+    namespace HPHP { namespace HHBBC {
+    }
     }
     
-    void SILLayout::Profile(llvm::FoldingSetNodeID &id,
-                        CanGenericSignature Generics,
-                        ArrayRef<SILField> Fields) {
-  id.AddPointer(Generics.getPointer());
-  for (auto &field : Fields) {
-    id.AddPointer(field.getLoweredType().getPointer());
-    id.AddBoolean(field.isMutable());
+    
+    {  // We had a counted inner array---we need to do an O(N) copy to get the
+  // collection into the request local heap.
+  auto const apcArr = APCArray::fromHandle(m_arrayHandle);
+  auto const col = Object::attach(collections::alloc(m_colType));
+  switch (m_colType) {
+  case CollectionType::ImmVector:
+  case CollectionType::Vector:
+    fillCollection(static_cast<BaseVector*>(col.get()), apcArr);
+    break;
+  case CollectionType::ImmSet:
+  case CollectionType::Set:
+    fillCollection(static_cast<BaseSet*>(col.get()), apcArr);
+    break;
+  case CollectionType::ImmMap:
+  case CollectionType::Map:
+    fillMap(static_cast<BaseMap*>(col.get()), apcArr);
+    break;
+  case CollectionType::Pair:
+    always_assert(0);
+    break;
   }
+  return col;
 }
+    
+      static const APCCollection* fromHandle(const APCHandle* handle) {
+    assertx(handle->checkInvariants());
+    assertx(handle->kind() == APCKind::SharedCollection);
+    static_assert(offsetof(APCCollection, m_handle) == 0, '');
+    return reinterpret_cast<const APCCollection*>(handle);
+  }
+    
+    // This method must return a char* which is owned by the IniSettingMap
+// to avoid issues with the lifetime of the char*
+const char* Config::Get(const IniSettingMap &ini, const Hdf& config,
+                        const std::string& name /* = '' */,
+                        const char *defValue /* = nullptr */,
+                        const bool prepend_hhvm /* = true */) {
+  auto ini_name = IniName(name, prepend_hhvm);
+  Hdf hdf = name != '' ? config[name] : config;
+  auto value = ini_iterate(ini, ini_name);
+  if (value.isString()) {
+    // See generic Get##METHOD below for why we are doing this
+    // Note that value is a string, so value.toString() is not
+    // a temporary.
+    const char* ini_ret = value.toString().data();
+    const char* hdf_ret = hdf.configGet(ini_ret);
+    if (hdf_ret != ini_ret) {
+      ini_ret = hdf_ret;
+      IniSetting::SetSystem(ini_name, ini_ret);
+    }
+    return ini_ret;
+  }
+  return hdf.configGet(defValue);
+}
+    
+    #endif
 
     
-      char *oldBegin = Begin;
-  char *oldEnd = End;
-  std::size_t oldSize = (std::size_t) (oldEnd - oldBegin);
+    #include 'hphp/util/stack-trace.h'
     
-      // If this is a getter/setter, the other accessor
-  const clang::FunctionDecl *pairedAccessor = nullptr;
+    #include <atomic>
     
+      explicit PlainFile(FILE *stream = nullptr,
+                     bool nonblocking = false,
+                     const String& wrapper_type = null_string,
+                     const String& stream_type = null_string);
+  explicit PlainFile(int fd,
+                     bool nonblocking = false,
+                     const String& wrapper = null_string,
+                     const String& stream_type = null_string);
+  ~PlainFile() override;
     
-    {  return 0;
-}
-    
-      FrontendInputsAndOutputs result;
-  for (auto &file : Files) {
-    bool isPrimary = primaryFiles.count(file) > 0;
-    result.addInput(InputFile(file, isPrimary));
-    if (isPrimary)
-      primaryFiles.erase(file);
-  }
-    
-            const std::unordered_map<StreamInformation, MinibatchData>& GetNextMinibatch(
-            size_t minibatchSizeInSamples,
-            size_t minibatchSizeInSequences,
-            size_t numberOfWorkers,
-            size_t workerRank,
-            const DeviceDescriptor& device = DeviceDescriptor::UseDefaultDevice()) override;
-    
-            bool result = (!m_distributed) ?
-            TrainLocalMinibatch(GetInputs(arguments), outputsToFetch, IsAtSweepEnd(arguments), computeDevice) :
-            TrainDistributedMinibatch(GetInputs(arguments), outputsToFetch, IsAtSweepEnd(arguments), computeDevice);
-    
-            if (::WaitForSingleObject(m_handle, wait ? INFINITE : 0) != WAIT_OBJECT_0)
-        {
-            // failed to acquire
-            int rc = ::CloseHandle(m_handle);
-            if ((rc == CLOSEHANDLE_ERROR) && !std::uncaught_exception())
-            {
-                RuntimeError('Acquire: Handler close failure with error code %d', ::GetLastError());
-            }
-            m_handle = NULL;
-            return false;
-        }
-    
-    public:
-    ScopeTimer(size_t verbosity, const std::string& message)
-        : m_verbosity(verbosity), m_message(message)
-    {
-        if (m_verbosity > 2)
-        {
-            m_aggregateTimer.Start();
-        }
-    }
-    
-                Input(0)->SetDims1(nInput, nHidden);
-            Input(0)->UpdateFunctionValuesSize();
-            Input(0)->Value().SetValue(1.0);
-            Input(1)->Value().TransferFromDeviceToDevice(m_deviceId, CPUDEVICE, true);
-            Input(1)->Value().SwitchToMatrixType(DENSE, matrixFormatDense, false);
-            Input(1)->SetDims1(nHidden, nOutput);
-            Input(1)->UpdateFunctionValuesSize();
-            Input(1)->Value().SetValue(0.0);
-            Input(1)->Value().SetValue(0, 0, 1.0);
-            Input(1)->Value().SetValue(1, 1, 2.0);
-            Input(1)->Value().TransferFromDeviceToDevice(CPUDEVICE, m_deviceId, true);
-            Input(1)->Value().SwitchToMatrixType(SPARSE, matrixFormatSparseCSC, true);
-            SetDims1(nInput, nOutput);
-            UpdateFunctionValuesSize();
-    
-        template <class ElemType>
-    void RequestRelease(shared_ptr<Matrix<ElemType>> *pMatrixPtr)
-    {
-        auto memInfo = GetMemInfo(pMatrixPtr);
-        if (memInfo != nullptr)
-        {
-            memInfo->SetReleaseStep(m_stepCounter);
-        }
-        m_stepCounter++; 
-    }
+      explicit UrlFile(const char *method = 'GET', const Array& headers = null_array,
+                   const String& postData = null_string,
+                   int maxRedirect = HttpClient::defaultMaxRedirect,
+                   int timeout = -1, bool ignoreErrors = false);
