3d0f04b73085b42001f66a630b8ebe5df717a060
SUNsung <weatherius@gmail.com>
2019-06-03 16:24:19 +0300

    Rewrite [49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb] File in: 2019-06-03 16:24:18

--- ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
+++ ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
@@ -1,84 +1,98 @@
 
         
-          #
-  # The raw command string associated with the header which will vary between
-  # requests and responses.
-  #
-  attr_accessor :junk_headers
-  attr_accessor :cmd_string
-  attr_accessor :fold
+                def initialize(object_name, method_name, template_object, checked_value, unchecked_value, options)
+          @checked_value   = checked_value
+          @unchecked_value = unchecked_value
+          super(object_name, method_name, template_object, options)
+        end
     
-        json
-  end
+                case options[:default]
+            when nil
+              Time.current
+            when Date, Time
+              options[:default]
+            else
+              default = options[:default].dup
     
-      def decode_audio_frame(buff)
-    case self.codec
+            redirect_to admin_report_path(@report), notice: I18n.t('admin.report_notes.created_msg')
+      else
+        @report_notes = @report.notes.latest
+        @report_history = @report.history
+        @form = Form::StatusBatch.new
     
+    module UserTrackingConcern
+  extend ActiveSupport::Concern
     
-  # open rmcpplus_request with cipherzero
-  def self.create_ipmi_session_open_cipher_zero_request(console_session_id)
-    head = [
-      0x06, 0x00, 0xff, 0x07,   # RMCP Header
-      0x06,                     # RMCP+ Authentication Type
-      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
-      0x00, 0x00, 0x00, 0x00,   # Session ID
-      0x00, 0x00, 0x00, 0x00    # Sequence Number
-    ].pack('C*')
-    
-              private
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Crypto
-        module Rc4Hmac
-          # Decrypts the cipher using RC4-HMAC schema
-          #
-          # @param cipher [String] the data to decrypt
-          # @param key [String] the key to decrypt
-          # @param msg_type [Integer] the message type
-          # @return [String] the decrypted cipher
-          # @raise [RuntimeError] if decryption doesn't succeed
-          def decrypt_rc4_hmac(cipher, key, msg_type)
-            unless cipher && cipher.length > 16
-              raise ::RuntimeError, 'RC4-HMAC decryption failed'
-            end
-    
-                seq_values.each do |val|
-              case val.tag
-              when 0
-                self.etype = decode_etype(val)
-              when 1
-                self.kvno = decode_kvno(val)
-              when 2
-                self.cipher = decode_cipher(val)
-              else
-                raise ::RuntimeError, 'Failed to decode EncryptedData SEQUENCE'
-              end
-            end
-          end
-    
-                self
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::LastReque from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
-    
-    When /^(?:|I )press '([^']*)'$/ do |button|
-  click_button(button)
-end
-    
-      def self.source_root
-    @source_root ||= File.expand_path('../templates', __FILE__)
-  end
-    
-        module ClassMethods
-      def attachment_definitions
-        Paperclip::AttachmentRegistry.definitions_for(self)
-      end
+      # if rss_url already in existing opml file, use that; otherwise, do a lookup
+  rss_url = nil
+  if File.exist?(OUTPUT_FILENAME)
+    xml = Nokogiri::XML(File.open(OUTPUT_FILENAME))
+    existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first
+    if existing_blog
+      rss_url = existing_blog.attr('xmlUrl')
+      puts '#{name}: ALREADY HAVE'
     end
   end
+    
+      def down
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id
+  end
+    
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
+  end
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
+  end
 end
+    
+        sign_in(alice, scope: :user)
+  end
+    
+      describe '#index' do
+    context 'with a private post' do
+      before do
+        @alices_aspect = alice.aspects.where(name: 'generic').first
+        @post = alice.post(:status_message, text: 'hey', to: @alices_aspect.id)
+      end
+    
+        h = {}
+    attrs.each do | e |
+    
+      private
+    
+    require 'pleaserun/cli'
+    
+      # Generate the proper tar flags based on the path name.
+  def tar_compression_flag(path)
+    case path
+      when /\.tar\.bz2$/
+        return '-j'
+      when /\.tar\.gz$|\.tgz$/
+        return '-z'
+      when /\.tar\.xz$/
+        return '-J'
+      else
+        return nil
+    end
+  end # def tar_compression_flag
+end # class FPM::Package::Tar
+
+    
+        # use dir to set stuff up properly, mainly so I don't have to reimplement
+    # the chdir/prefix stuff special for zip.
+    dir = convert(FPM::Package::Dir)
+    if attributes[:chdir]
+      dir.attributes[:chdir] = File.join(build_path, attributes[:chdir])
+    else
+      dir.attributes[:chdir] = build_path
+    end
+    
+    # Like the ::Gem::Package::TarWriter but contains some backports and bug fixes
+class FPM::Util::TarWriter < ::Gem::Package::TarWriter
+  if FPM::Issues::TarWriter.has_issues_with_split_name?
+    def split_name(name)
+      if name.bytesize > 256 then
+        raise ::Gem::Package::TooLongFileName.new('File \'#{name}\' has a too long path (should be 256 or less)')
+      end
