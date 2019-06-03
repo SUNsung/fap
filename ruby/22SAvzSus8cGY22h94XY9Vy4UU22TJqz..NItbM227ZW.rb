c8d8337ebb5ae3bdc24ee9a52e2df4f04a5f4fc0
SUNsung <weatherius@gmail.com>
2019-06-03 06:24:39 +0300

    Rewrite [22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb] File in: 2019-06-03 06:24:38

--- ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
+++ ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
@@ -1,116 +1,13 @@
 
         
-            private_constant :GA_TRACKING
-    attr_accessor :session_id
-    attr_accessor :client
+        class Discourse::Cors
+  ORIGINS_ENV = 'Discourse_Cors_Origins'
     
-            context 'when command is update' do
-          let(:command) { 'update' }
-    
-        # Creates a new option
-    # @param key (Symbol) the key which is used as command parameters or key in the fastlane tools
-    # @param env_name (String) the name of the environment variable, which is only used if no other values were found
-    # @param description (String) A description shown to the user
-    # @param short_option (String) A string of length 1 which is used for the command parameters (e.g. -f)
-    # @param default_value the value which is used if there was no given values and no environment values
-    # @param default_value_dynamic (Boolean) Set if the default value is generated dynamically
-    # @param verify_block an optional block which is called when a new value is set.
-    #   Check value is valid. This could be type checks or if a folder/file exists
-    #   You have to raise a specific exception if something goes wrong. Append .red after the string
-    # @param is_string *DEPRECATED: Use `type` instead* (Boolean) is that parameter a string? Defaults to true. If it's true, the type string will be verified.
-    # @param type (Class) the data type of this config item. Takes precedence over `is_string`. Use `:shell_string` to allow types `String`, `Hash` and `Array` that will be converted to shell-escaped strings
-    # @param skip_type_validation (Boolean) is false by default. If set to true, type of the parameter will not be validated.
-    # @param optional (Boolean) is false by default. If set to true, also string values will not be asked to the user
-    # @param conflicting_options ([]) array of conflicting option keys(@param key). This allows to resolve conflicts intelligently
-    # @param conflict_block an optional block which is called when options conflict happens
-    # @param deprecated (Boolean|String) Set if the option is deprecated. A deprecated option should be optional and is made optional if the parameter isn't set, and fails otherwise
-    # @param sensitive (Boolean) Set if the variable is sensitive, such as a password or API token, to prevent echoing when prompted for the parameter
-    # @param display_in_shell (Boolean) Set if the variable can be used from shell
-    # rubocop:disable Metrics/ParameterLists
-    def initialize(key: nil,
-                   env_name: nil,
-                   description: nil,
-                   short_option: nil,
-                   default_value: nil,
-                   default_value_dynamic: false,
-                   verify_block: nil,
-                   is_string: true,
-                   type: nil,
-                   skip_type_validation: false,
-                   optional: nil,
-                   conflicting_options: nil,
-                   conflict_block: nil,
-                   deprecated: nil,
-                   sensitive: nil,
-                   code_gen_sensitive: false,
-                   code_gen_default_value: nil,
-                   display_in_shell: true)
-      UI.user_error!('key must be a symbol') unless key.kind_of?(Symbol)
-      UI.user_error!('env_name must be a String') unless (env_name || '').kind_of?(String)
-    
-          it 'should shell escape keychain names when checking for installation' do
-        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
-        expect(FastlaneCore::Helper).to receive(:backticks).with(name_regex, anything).and_return('')
-    
-    tool_name = File.basename($0)
-    
-    require_relative 'options'
-require_relative 'runner'
-    
-          def perform(start_id, stop_id)
-        update = '
-          latest_merge_request_diff_id = (
-            SELECT MAX(id)
-            FROM merge_request_diffs
-            WHERE merge_requests.id = merge_request_diffs.merge_request_id
-          )'.squish
-    
-              issue.label_names.each do |label_name|
-            # Although unlikely it's technically possible for an issue to be
-            # given a label that was created and assigned after we imported all
-            # the project's labels.
-            next unless (label_id = label_finder.id_for(label_name))
-    
-    describe 'BasicObject instance metaclass' do
-  before :each do
-    @object = BasicObject.new
-    @meta = class << @object; self; end
-  end
-    
-      it 'preserves __LINE__ across multiple calls to eval' do
-    obj = BindingSpecs::Demo.new(1)
-    bind = obj.get_binding
-    bind.eval('__LINE__').should == obj.get_line_of_binding
-    bind.eval('__LINE__').should == obj.get_line_of_binding
-  end
-    
-        def get_binding_with_send_and_line
-      [send(:binding), __LINE__]
+          expect(builder.build).to_not include('invalid')
     end
     
-      url 'http://swupdl.adobe.com/updates/oobe/aam20/mac/AdobeLightroom-#{version.major}.0/#{version}/setup.dmg'
-  name 'Adobe Photoshop Lightroom'
-  homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
-    
-          sh <<-SH
-        git commit --allow-empty -a -m '#{source_version} release'  &&
-        git tag -s v#{source_version} -m '#{source_version} release'  &&
-        git push && (git push origin || true) &&
-        git push --tags && (git push origin --tags || true)
-      SH
-    end
-    
-      task :index do
-    doc = File.read('README.md')
-    file = 'doc/rack-protection-readme.md'
-    Dir.mkdir 'doc' unless File.directory? 'doc'
-    puts 'writing #{file}'
-    File.open(file, 'w') { |f| f << doc }
+      if fast_forwards.include?(name)
+    puts '#{name}: TEMP IGNORE'
+    unavailable.push(Struct::Blog.new(name, web_url, nil))
+    next
   end
-    
-          def session_key
-        @session_key ||= options[:session_key]
-      end
-    end
-  end
-end
