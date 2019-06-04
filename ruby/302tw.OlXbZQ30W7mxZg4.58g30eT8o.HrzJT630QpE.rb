16bc99a20fc57dd2e8742f5c337a71cf0f3487df
SUNsung <weatherius@gmail.com>
2019-06-04 03:47:19 +0300

    Rewrite [302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb] File in: 2019-06-04 03:47:19

--- ruby/302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb
+++ ruby/302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb
@@ -1,34 +1,142 @@
 
         
-            # wrap in double quotes if contains space
-    if str =~ /\s/
-      # double quotes have to be doubled if will be quoted
-      str.gsub!(''', '''')
-      return ''' + str + '''
-    else
-      return str
+            def translation_scope
+      'devise.confirmations'
     end
-  end
-  module_function :shellescape
 end
 
     
-      describe 'on other OSs (macOS, Linux)' do
-    before(:each) do
-      allow(FastlaneCore::Helper).to receive(:windows?).and_return(false)
-    end
+      # DELETE /resource/sign_out
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
+  end
     
-    # The * turns the array into a parameter list
-# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
-# We need to use that, because otherwise invocations like
-# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
-exec(*exec_arr)
+      # Get message for given
+  def find_message(kind, options = {})
+    options[:scope] ||= translation_scope
+    options[:default] = Array(options[:default]).unshift(kind.to_sym)
+    options[:resource_name] = resource_name
+    options = devise_i18n_options(options)
+    I18n.t('#{options[:resource_name]}.#{kind}', options)
+  end
+    
+          def remember_me_is_active?(resource)
+        return false unless resource.respond_to?(:remember_me)
+        scope = Devise::Mapping.find_scope!(resource)
+        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
+        resource.remember_me?(token, generated_at)
+      end
+    
+                authentication_keys_changed || encrypted_password_changed
+          end
+        else
+          def clear_reset_password_token?
+            encrypted_password_changed = respond_to?(:encrypted_password_changed?) && encrypted_password_changed?
+            authentication_keys_changed = self.class.authentication_keys.any? do |attribute|
+              respond_to?('#{attribute}_changed?') && send('#{attribute}_changed?')
+            end
+    
+    end
+end
+end
 
     
-          describe 'running without fastlane' do
-        before do
-          allow(FastlaneCore::Helper).to receive(:fastlane_enabled?).and_return(false)
-    
-      def passthru
-    render status: 404, plain: 'Not found. Authentication passthru.'
+      #
+  # Returns the hardcore alias for the HTTP service
+  #
+  def self.hardcore_alias(*args)
+    '#{(args[0] || '')}#{(args[1] || '')}'
   end
+    
+    
+  #
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
+  }
+    
+    
+  # open rmcpplus_request with cipherzero
+  def self.create_ipmi_session_open_cipher_zero_request(console_session_id)
+    head = [
+      0x06, 0x00, 0xff, 0x07,   # RMCP Header
+      0x06,                     # RMCP+ Authentication Type
+      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
+      0x00, 0x00, 0x00, 0x00,   # Session ID
+      0x00, 0x00, 0x00, 0x00    # Sequence Number
+    ].pack('C*')
+    
+              # Encodes the type field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_type
+            bn = OpenSSL::BN.new(type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+                int
+          end
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a Kerberos KDC-REQ (response) data
+        # definition
+        class KdcResponse < Element
+          # @!attribute pvno
+          #   @return [Integer] The protocol version number
+          attr_accessor :pvno
+          # @!attribute msg_type
+          #   @return [Integer] The type of a protocol message
+          attr_accessor :msg_type
+          # @!attribute crealm
+          #   @return [String] The realm part of the client's principal identifier
+          attr_accessor :crealm
+          # @!attribute cname
+          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the client's principal identifier
+          attr_accessor :cname
+          # @!attribute ticket
+          #   @return [Rex::Proto::Kerberos::Model::Ticket] The issued ticket
+          attr_accessor :ticket
+          # @!attribute enc_part
+          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] The encrypted part of the response
+          attr_accessor :enc_part
+    
+      url 'http://swupdl.adobe.com/updates/oobe/aam20/mac/AdobeLightroom-#{version.major}.0/#{version}/setup.dmg'
+  name 'Adobe Photoshop Lightroom'
+  homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
+    
+    # Add our files (should be in the current directory):
+package.input('my-executable=/usr/bin/')
+package.input('my-library.so=/usr/lib/')
+    
+              if(file.eof?())
+            raise StandardError.new('Invalid tar stream, eof before end-of-tar record')
+          end
+    
+      public(:input, :output, :identifier, :to_s)
+    
+        # Add necessary metadata to the generated manifest.
+    metadata_template = template('p5p_metadata.erb').result(binding)
+    File.write(build_path('#{name}.mog'), metadata_template)
