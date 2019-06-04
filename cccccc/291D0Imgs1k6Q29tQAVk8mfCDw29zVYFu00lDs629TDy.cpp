e87221a6b0e6ad480434f2d4a50d84242b027c33
SUNsung <weatherius@gmail.com>
2019-06-04 17:19:53 +0300

    Rewrite [291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp] File in: 2019-06-04 17:19:53

--- cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
+++ cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
@@ -1,232 +1,147 @@
 
         
-                //  Create the Pseudo Console and pipes to it
-        HANDLE hPipeIn{ INVALID_HANDLE_VALUE };
-        HANDLE hPipeOut{ INVALID_HANDLE_VALUE };
-        hr = CreatePseudoConsoleAndPipes(&hPC, &hPipeIn, &hPipeOut);
-        if (S_OK == hr)
-        {
-            // Create & start thread to listen to the incoming pipe
-            // Note: Using CRT-safe _beginthread() rather than CreateThread()
-            HANDLE hPipeListenerThread{ reinterpret_cast<HANDLE>(_beginthread(PipeListener, 0, hPipeIn)) };
-    }
-    
-      /**
-   * Caffe's thread local state will be initialized using the current
-   * thread values, e.g. device id, solver index etc. The random seed
-   * is initialized using caffe_rng_rand.
-   */
-  void StartInternalThread();
-    
-    /**
- * @brief Computes the classification accuracy for a one-of-many
- *        classification task.
- */
-template <typename Dtype>
-class AccuracyLayer : public Layer<Dtype> {
- public:
-  /**
-   * @param param provides AccuracyParameter accuracy_param,
-   *     with AccuracyLayer options:
-   *   - top_k (\b optional, default 1).
-   *     Sets the maximum rank @f$ k @f$ at which a prediction is considered
-   *     correct.  For example, if @f$ k = 5 @f$, a prediction is counted
-   *     correct if the correct label is among the top 5 predicted labels.
-   */
-  explicit AccuracyLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    
-    {  // Best available image.
-  Pix* pix_;
-  // True if the source image is white-on-black.
-  bool inverse_;
-  // Block the word came from. If not null, block->re_rotation() takes the
-  // 'untransformed' coordinates even further back to the original image.
-  // Used only on the first DENORM in a chain.
-  const BLOCK* block_;
-  // Rotation to apply between translation to the origin and scaling.
-  const FCOORD* rotation_;
-  // Previous transformation in a chain.
-  const DENORM* predecessor_;
-  // Non-linear transformation maps directly from each integer offset from the
-  // origin to the corresponding x-coord. Owned by the DENORM.
-  GenericVector<float>* x_map_;
-  // Non-linear transformation maps directly from each integer offset from the
-  // origin to the corresponding y-coord. Owned by the DENORM.
-  GenericVector<float>* y_map_;
-  // x-coordinate to be mapped to final_xshift_ in the result.
-  float x_origin_;
-  // y-coordinate to be mapped to final_yshift_ in the result.
-  float y_origin_;
-  // Scale factors for x and y coords. Applied to pre-rotation system.
-  float x_scale_;
-  float y_scale_;
-  // Destination coords of the x_origin_ and y_origin_.
-  float final_xshift_;
-  float final_yshift_;
-};
-    
-     private:
-  // A hole in the heap exists at hole_index, and we want to fill it with the
-  // given pair. SiftUp sifts the hole upward to the correct position and
-  // returns the destination index without actually putting pair there.
-  int SiftUp(int hole_index, const Pair& pair) {
-    int parent;
-    while (hole_index > 0 && pair < heap_[parent = ParentNode(hole_index)]) {
-      heap_[hole_index] = heap_[parent];
-      hole_index = parent;
-    }
-    return hole_index;
-  }
-    
-    // A useful base class to facilitate the common operation of sorting a vector
-// of owned pointer data using a separate key. This class owns its data pointer,
-// deleting it when it has finished with it, and providing copy constructor and
-// operator= that have move semantics so that the data does not get copied and
-// only a single instance of KDPtrPair holds a specific data pointer.
-template <typename Key, typename Data>
-class KDPtrPair {
- public:
-  KDPtrPair() : data_(nullptr) {}
-  KDPtrPair(Key k, Data* d) : data_(d), key_(k) {}
-  // Copy constructor steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  KDPtrPair(KDPtrPair& src) : data_(src.data_), key_(src.key_) {
-    src.data_ = nullptr;
-  }
-  // Destructor deletes data, assuming it is the sole owner.
-  ~KDPtrPair() {
-    delete this->data_;
-    this->data_ = nullptr;
-  }
-  // Operator= steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  void operator=(KDPtrPair& src) {
-    delete this->data_;
-    this->data_ = src.data_;
-    src.data_ = nullptr;
-    this->key_ = src.key_;
-  }
-    }
-    
-    
-    {  CCUtilMutex mu_;
-  GenericVector<ReferenceCount> cache_;
-};
-    
-    
-    {        auto sequenceLength = sequenceData.size() / shapeSize;
-        std::vector<NDArrayViewPtr> sequencesView(1);
-        auto sequenceDataShape = sampleShape.AppendShape({ sequenceLength });
-        sequencesView[0] = MakeSharedObject<NDArrayView>(sequenceDataShape, sequenceData);
-        return Create(sampleShape, sequencesView, { sequenceStartFlag }, device, readOnly, /*createNewCopy =*/ true);
-    }
-    
-        Variable Variable::CompositePreservingCopy(const std::shared_ptr<const Function>& composite) const
-    {
-        // We have to preserve the whole subgraph.
-        Variable result;
-        result.m_outputComposite = composite;
-        result.m_dataFields = m_dataFields;
-        return result;
-    }
-    
-        static void DecideStartEndingOutputLab(const Matrix<ElemType>& lbls, int& stt, int& stp)
-    {
-        if (stt != -1 && stp != -1)
-            return; // have computed before
-    }
-    
-    
-    {                    if (!memAllocInfoVec.empty())
-                    {
-                        // since we assign from highest memory to lowest, every memory that has been allocated can accommodate the 
-                        // current memory request, unless there is a conflict (overlap) 
-                        auto iter = memAllocInfoVec.begin();
-                        while (iter != memAllocInfoVec.end() && CheckOverlap(make_pair(memInfo.allocStep, memInfo.releaseStep), iter->occupancy))
-                            iter++;
-                        if (iter == memAllocInfoVec.end())
-                        {
-                            // no current memory can be assigned, need to create a new one 
-                            vector<pair<int, int>> occ;
-                            occ.push_back(make_pair(memInfo.allocStep, memInfo.releaseStep));
-                            MemAllocInfo ma(memoryCounter, memInfo.matrixSize, occ);
-                            // insert in the front of the vector to maintain sorted order 
-                            memAllocInfoVec.insert(memAllocInfoVec.begin(), ma);
-                            memInfo.SetMemoryId(memoryCounter);
-                            memoryCounter++;
-                        }
-                        else
-                        {
-                            iter->occupancy.push_back(make_pair(memInfo.allocStep, memInfo.releaseStep));
-                            memInfo.SetMemoryId(iter->memoryId);
-                        }
-                    }
-                    else
-                    {
-                        vector<pair<int, int>> occ;
-                        occ.push_back(make_pair(memInfo.allocStep, memInfo.releaseStep));
-                        MemAllocInfo ma(memoryCounter, memInfo.matrixSize, occ);
-                        memAllocInfoVec.push_back(ma);
-                        memInfo.SetMemoryId(memoryCounter);
-                        memoryCounter++;
-                    }
-                }
-    
-    template<class ElemType>
-/*virtual*/ void OptimizedRNNStackNode<ElemType>::CopyTo(ComputationNodeBasePtr nodeP, const std::wstring& newName, const CopyNodeFlags flags) const /*override*/
+        class OpenURIDialog : public QDialog
 {
-    Base::CopyTo(nodeP, newName, flags);
-    if (flags & CopyNodeFlags::copyNodeValue)
-    {
-        auto node = dynamic_pointer_cast<OptimizedRNNStackNode<ElemType>>(nodeP);
-        node->m_rnnAttributes = m_rnnAttributes;
+    Q_OBJECT
     }
+    
+    double UniValue::get_real() const
+{
+    if (typ != VNUM)
+        throw std::runtime_error('JSON value is not a number as expected');
+    double retval;
+    if (!ParseDouble(getValStr(), &retval))
+        throw std::runtime_error('JSON double out of range');
+    return retval;
 }
     
-    #include 'modules/drivers/canbus/can_client/esd/esd_can_client.h'
+        UniValue v2(UniValue::VSTR);
+    BOOST_CHECK(v2.isStr());
     
-    using apollo::drivers::canbus::Byte;
+    #include <stdint.h>
+#include <stdlib.h>
     
-    bool RadarState201::send_quality(const std::uint8_t* bytes,
-                                 int32_t length) const {
-  Byte t0(bytes + 5);
-  uint32_t x = t0.get_byte(4, 1);
+    
+    {  // (e) Do a non-sync write; should fail
+  w.sync = false;
+  ASSERT_TRUE(!db_->Put(w, 'k3', 'v3').ok());
+  ASSERT_EQ('v1', Get('k1'));
+  ASSERT_EQ('NOT_FOUND', Get('k2'));
+  ASSERT_EQ('NOT_FOUND', Get('k3'));
+}
+    
+    // A comparator for internal keys that uses a specified comparator for
+// the user key portion and breaks ties by decreasing sequence number.
+class InternalKeyComparator : public Comparator {
+ private:
+  const Comparator* user_comparator_;
+ public:
+  explicit InternalKeyComparator(const Comparator* c) : user_comparator_(c) { }
+  virtual const char* Name() const;
+  virtual int Compare(const Slice& a, const Slice& b) const;
+  virtual void FindShortestSeparator(
+      std::string* start,
+      const Slice& limit) const;
+  virtual void FindShortSuccessor(std::string* key) const;
     }
     
-    // config detail: {'name': 'manual_input', 'offset': 0.0, 'precision': 0.001,
-// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 7,
-// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
-double Accelrpt68::manual_input(const std::uint8_t* bytes,
-                                int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 8);
+      Status Verify(int start_idx, int num_vals,
+                ExpectedVerifResult expected) const {
+    std::string val;
+    std::string value_space;
+    Status s;
+    for (int i = start_idx; i < start_idx + num_vals && s.ok(); i++) {
+      Value(i, &value_space);
+      s = ReadValue(i, &val);
+      if (expected == VAL_EXPECT_NO_ERROR) {
+        if (s.ok()) {
+          ASSERT_EQ(value_space, val);
+        }
+      } else if (s.ok()) {
+        fprintf(stderr, 'Expected an error at %d, but was OK\n', i);
+        s = Status::IOError(dbname_, 'Expected value error:');
+      } else {
+        s = Status::OK();  // An expected error
+      }
+    }
+    return s;
+  }
+    
+    // Return the name of a temporary file owned by the db named 'dbname'.
+// The result will be prefixed with 'dbname'.
+extern std::string TempFileName(const std::string& dbname, uint64_t number);
+    
+    void SimpleCSRSource::LoadBinary(dmlc::Stream* fi) {
+  int tmagic;
+  CHECK(fi->Read(&tmagic, sizeof(tmagic)) == sizeof(tmagic)) << 'invalid input file format';
+  CHECK_EQ(tmagic, kMagic) << 'invalid format, magic number mismatch';
+  info.LoadBinary(fi);
+  fi->Read(&page_.offset.HostVector());
+  fi->Read(&page_.data.HostVector());
+}
+    
+    #endif
+    
+        // see if we have already split on this feature,
+    // if so we undo that split so we can redo it for this node
+    unsigned path_index = 0;
+    for (; path_index <= unique_depth; ++path_index) {
+      if (static_cast<unsigned>(unique_path[path_index].feature_index) == split_index) break;
+    }
+    if (path_index != unique_depth + 1) {
+      incoming_zero_fraction = unique_path[path_index].zero_fraction;
+      incoming_one_fraction = unique_path[path_index].one_fraction;
+      UnwindPath(unique_path, unique_depth, path_index);
+      unique_depth -= 1;
     }
     
-    // config detail: {'name': 'commanded_value', 'offset': 0.0, 'precision': 0.001,
-// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 23,
-// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
-double Brakerpt6c::commanded_value(const std::uint8_t* bytes,
-                                   int32_t length) const {
-  Byte t0(bytes + 2);
-  int32_t x = t0.get_byte(0, 8);
+    #include <folly/GLog.h>
+    
+    namespace folly {
     }
+    
+    using UriTuple = std::tuple<
+    const std::string&,
+    const std::string&,
+    const std::string&,
+    const std::string&,
+    uint16_t,
+    const std::string&,
+    const std::string&,
+    const std::string&>;
+    
+      VirtualExecutor(const VirtualExecutor&) = delete;
+  VirtualExecutor& operator=(const VirtualExecutor&) = delete;
+    
+        // Check if we need to push a batch from local -> global
+    auto batchcount = EXTERNAL_OFFSET / 2;
+    if (get_local_count(newlocal) > batchcount) {
+      CountedDetail::inc_shared_count(newlocal.get(), batchcount);
+      putOwnedBase(newlocal.get(), batchcount, order);
+    }
+    
+      void reset(const std::shared_ptr<T>& p = nullptr) {
+    // Allocate each Holder in a different CoreRawAllocator stripe to
+    // prevent false sharing. Their control blocks will be adjacent
+    // thanks to allocate_shared().
+    for (auto slot : folly::enumerate(slots_)) {
+      auto alloc = getCoreAllocator<Holder, kNumSlots>(slot.index);
+      auto holder = std::allocate_shared<Holder>(alloc, p);
+      *slot = std::shared_ptr<T>(holder, p.get());
+    }
+  }
+    
+    size_t qfind_first_byte_of_bitset(
+    const StringPieceLite haystack,
+    const StringPieceLite needles) {
+  std::bitset<256> s;
+  for (auto needle : needles) {
+    s[(uint8_t)needle] = true;
+  }
+  for (size_t index = 0; index < haystack.size(); ++index) {
+    if (s[(uint8_t)haystack[index]]) {
+      return index;
+    }
+  }
+  return std::string::npos;
+}
