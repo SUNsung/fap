4f0fa1a05412777e1235633eb9f2afe9a115a154
SUNsung <weatherius@gmail.com>
2019-06-04 23:32:14 +0300

    Rewrite [07i882tM3xmtc07IpzCtyZTIV607.i0O03UvUjU07zCu.rb] File in: 2019-06-04 23:32:14

--- ruby/07i882tM3xmtc07IpzCtyZTIV607.i0O03UvUjU07zCu.rb
+++ ruby/07i882tM3xmtc07IpzCtyZTIV607.i0O03UvUjU07zCu.rb
@@ -1,109 +1,84 @@
 
         
-            it 'does not send previously configured sources when the current agent does not support them' do
-      select_agent_type('Website Agent scrapes')
-      select2('SF Weather', from: 'Sources')
-      select_agent_type('Webhook Agent')
-      fill_in(:agent_name, with: 'No sources')
-      click_on 'Save'
-      expect(page).to have_content('No sources')
-      agent = Agent.find_by(name: 'No sources')
-      expect(agent.sources).to eq([])
+          # Under Phusion Passenger smart spawning, we need to reopen all IO streams
+  # after workers have forked.
+  #
+  # The rolling file appender uses shared file locks to ensure that only one
+  # process will roll the log file. Each process writing to the file must have
+  # its own open file descriptor for `flock` to function properly. Reopening
+  # the file descriptors after forking ensures that each worker has a unique
+  # file descriptor.
+  if defined? PhusionPassenger
+    PhusionPassenger.on_event(:starting_worker_process) do |forked|
+      Logging.reopen if forked
+    end
+  end
+end
+    
+        it 'generates a jasmine fixture', :fixture => true do
+      get :index, params: {conversation_id: @conv1.id}
+      save_fixture(html_for('body'), 'conversations_unread')
+    
+        it 'generates a jasmine fixture', :fixture => true do
+      contact = alice.contact_for(bob.person)
+      aspect = alice.aspects.create(:name => 'people')
+      contact.aspects << aspect
+      contact.save
+      get :new, params: {person_id: bob.person.id}
+      save_fixture(html_for('body'), 'status_message_new')
+    end
+  end
+end
+
+    
+          it_behaves_like 'on a visible post'
     end
     
-              valid_parsed_trigger_agent_data.each do |key, value|
-            if key == :type
-              value = value.split('::').last
+    describe ResharesController, :type => :controller do
+  describe '#create' do
+    let(:post_request!) {
+      post :create, params: {root_guid: @post_guid}, format: :json
+    }
+    
+          dir = if File.directory?(file_or_dir)
+              file_or_dir
+            else
+              File.dirname(file_or_dir)
             end
-            expect(trigger_agent_diff).to respond_to(key)
-            field = trigger_agent_diff.send(key)
-            expect(field).to be_a(ScenarioImport::AgentDiff::FieldDiff)
-            expect(field.incoming).to eq(value)
-            expect(field.updated).to eq(value)
-            expect(field.current).to be_nil
-          end
-          expect(trigger_agent_diff).not_to respond_to(:schedule)
+      @path_cache[dir] ||= ConfigLoader.configuration_file_for(dir)
+      path = @path_cache[dir]
+      @object_cache[path] ||= begin
+                                print 'For #{dir}: ' if ConfigLoader.debug?
+                                ConfigLoader.configuration_from_file(path)
+                              end
+    end
+  end
+end
+
+    
+        # Iterate over all the options and set defaults
+    if self.class.respond_to?(:declared_options)
+      self.class.declared_options.each do |option|
+        option.attribute_name.tap do |attr|
+          # clamp makes option attributes available as accessor methods
+          # do --foo-bar is available as 'foo_bar'
+          # make these available as package attributes.
+          attr = '#{attr}?' if !respond_to?(attr)
+          input.attributes[attr.to_sym] = send(attr) if respond_to?(attr)
         end
       end
     end
     
-      describe '#log_for_agent' do
-    it 'creates AgentLogs' do
-      log = AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 4, :outbound_event => events(:jane_website_agent_event))
-      expect(log).not_to be_new_record
-      expect(log.agent).to eq(agents(:jane_website_agent))
-      expect(log.outbound_event).to eq(events(:jane_website_agent_event))
-      expect(log.message).to eq('some message')
-      expect(log.level).to eq(4)
-    end
+        scripts[:after_install] = template(File.join('pleaserun', 'scripts', 'after-install.sh')).result(binding)
+    scripts[:before_remove] = template(File.join('pleaserun', 'scripts', 'before-remove.sh')).result(binding)
+  end # def input
     
-      describe '#complete_project_id' do
-    it 'should return a array of hashes' do
-      expect(@checker.complete_project_id).to eq [{text: 'test (1234)', id: 1234}, {text: 'test1 (1235)', id: 1235}]
-    end
-  end
-    
-      describe '#receive' do
-    it 'sends a message' do
-      stub(HTTParty).post { {'id' => 1, 'message' => 'blah', 'title' => 'blah','source_name' => 'Custom Notification'} }
-      @checker.receive([@event])
-    end
-    
-        def empty?
-      @entries.empty?
-    end
-    
-        attr_reader :filters
-    
-      # staged_path not available in Installer/Uninstall Stanza, workaround by nesting with preflight/postflight
-  # see https://github.com/Homebrew/homebrew-cask/pull/8887
-  # and https://github.com/Homebrew/homebrew-cask-versions/pull/296
-    
-        assert body.include?('<span class='username'>Charles Pence</span>'), '/latest_changes should include the Author Charles Pence'
-    assert body.include?('a8ad3c0'), '/latest_changes should include the :latest_changes_count commit'
-    assert !body.include?('60f12f4'), '/latest_changes should not include more than latest_changes_count commits'
-    assert body.include?('<a href='Data-Two.csv/874f597a5659b4c3b153674ea04e406ff393975e'>Data-Two.csv</a>'), '/latest_changes include links to modified files in #{body}'
-    assert body.include?('<a href='Hobbit/874f597a5659b4c3b153674ea04e406ff393975e'>Hobbit.md</a>'), '/latest_changes should include links to modified pages in #{body}'
-  end
-    
-    __END__
-    
-      # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
-    
-      config.vm.define 'freebsd10' do |freebsd10|
-    freebsd10.vm.box = 'tjay/freebsd-10.1'
-  end
-    
-        # Write the output somewhere, package can be nil if no --package is specified,
-    # and that's OK.
-    
-        self.description = info['description']
-    # Supposedly you can upload a package for npm with no author/author email
-    # so I'm being safer with this. Author can also be a hash or a string
-    self.vendor = 'Unknown <unknown@unknown.unknown>'
-    if info.include?('author')
-      author_info = info['author']
-      # If a hash, assemble into a string
-      if author_info.respond_to? :fetch
-        self.vendor = sprintf('%s <%s>', author_info.fetch('name', 'unknown'),
-                              author_info.fetch('email', 'unknown@unknown.unknown'))
-      else
-        # Probably will need a better check for validity here
-        self.vendor = author_info unless author_info == ''
-      end
-    end
-    
-      private
-  def input(command)
-    platforms = [
-      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
-      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
-      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
-      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
-      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
-    ]
-    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
-                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
-                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
-                             'log_directory', 'log_file_stderr', 'log_file_stdout']
+        args = [ tar_cmd,
+             '-C',
+             staging_path,
+             '-cf',
+             payload_tar,
+             '--owner=0',
+             '--group=0',
+             '--numeric-owner',
+             '.' ]
