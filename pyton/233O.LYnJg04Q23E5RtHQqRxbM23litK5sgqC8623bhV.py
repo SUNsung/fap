f6309a0e38f135fb8271f698bbe47f943fd3938e
SUNsung <weatherius@gmail.com>
2019-06-05 10:32:37 +0300

    Rewrite [233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py] File in: 2019-06-05 10:32:37

--- pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
+++ pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
@@ -1,188 +1,256 @@
 
         
-            proc.sendline(u'ehco test')
-    
-    
-@pytest.mark.functional
-def test_select_command_with_arrows(proc, TIMEOUT):
-    select_command_with_arrows(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'git help')
-    
-    
-@pytest.mark.parametrize('command', [
-    Command('apt-cache search foo', ''),
-    Command('aptitude search foo', ''),
-    Command('apt search foo', ''),
-    Command('apt-get install foo', ''),
-    Command('apt-get source foo', ''),
-    Command('apt-get clean', ''),
-    Command('apt-get remove', ''),
-    Command('apt-get update', ''),
-    Command('sudo apt update', no_match_output)
-])
-def test_not_match(command):
-    assert not match(command)
-    
-    You can download from:
-  https://osxfuse.github.io/
-Error: An unsatisfied requirement failed this build.'''
-    
-    
-@pytest.mark.parametrize('command, new_command', [
-    (Command('cargo buid', no_such_subcommand_old), 'cargo build'),
-    (Command('cargo buils', no_such_subcommand), 'cargo build')])
-def test_get_new_command(command, new_command):
-    assert get_new_command(command) == new_command
+            def override_category_budget(self, category, amount):
+        self.categories_to_budget_map[category] = amount
 
     
+        def reducer(self, key, values):
+        '''Sum values for each key.
     
-@parametrize_extensions
-@parametrize_filename
-@parametrize_script
-def test_match(ext, tar_error, filename, unquoted, quoted, script, fixed):
-    tar_error(unquoted.format(ext))
-    assert match(Command(script.format(filename.format(ext)), ''))
-    
-            if opts.meta:
-            try:
-                opts.meta = json.loads(opts.meta)
-            except ValueError:
-                raise UsageError('Invalid -m/--meta value, pass a valid json string to -m or --meta. ' \
-                                'Example: --meta='{\'foo\' : \'bar\'}'', print_help=False)
-    
-        def run(self, args, opts):
-        settings = self.crawler_process.settings
-        if opts.get:
-            s = settings.get(opts.get)
-            if isinstance(s, BaseSettings):
-                print(json.dumps(s.copy_to_dict()))
-            else:
-                print(s)
-        elif opts.getbool:
-            print(settings.getbool(opts.getbool))
-        elif opts.getint:
-            print(settings.getint(opts.getint))
-        elif opts.getfloat:
-            print(settings.getfloat(opts.getfloat))
-        elif opts.getlist:
-            print(settings.getlist(opts.getlist))
-
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
     
     
-logger = logging.getLogger(__name__)
+@pytest.mark.parametrize('script, output', [
+    ('apt', invalid_operation('saerch')),
+    ('apt-get', invalid_operation('isntall')),
+    ('apt-cache', invalid_operation('rumove'))])
+def test_match(script, output):
+    assert match(Command(script, output))
     
-            # XXX: Google parses at least first 100k bytes; scrapy's redirect
-        # middleware parses first 4k. 4k turns out to be insufficient
-        # for this middleware, and parsing 100k could be slow.
-        # We use something in between (32K) by default.
-        self.lookup_bytes = settings.getint('AJAXCRAWL_MAXSIZE', 32768)
+                std = y_train.std(axis=0)
+            mean = y_train.mean(axis=0)
+            y_train = (y_train - mean) / std
+            y_test = (y_test - mean) / std
+    
+    $ kernprof.py -l sparsity_benchmark.py
+$ python -m line_profiler sparsity_benchmark.py.lprof
+    
+        n = 10
+    step = 10000
+    n_samples = 10000
+    dim = 10
+    n_classes = 10
+    for i in range(n):
+        print('============================================')
+        print('Entering iteration %s of %s' % (i, n))
+        print('============================================')
+        n_samples += step
+        X = np.random.randn(n_samples, dim)
+        Y = np.random.randint(0, n_classes, (n_samples,))
+        bench_scikit_tree_classifier(X, Y)
+        Y = np.random.randn(n_samples)
+        bench_scikit_tree_regressor(X, Y)
+    
+        # TASK: Build a vectorizer / classifier pipeline that filters out tokens
+    # that are too rare or too frequent
+    pipeline = Pipeline([
+        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
+        ('clf', LinearSVC(C=1000)),
+    ])
+    
+    import time
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.parasite_axes import host_subplot
+from mpl_toolkits.axisartist.axislines import Axes
+from scipy.sparse.csr import csr_matrix
+    
+    for connectivity in (None, knn_graph):
+    for n_clusters in (30, 3):
+        plt.figure(figsize=(10, 4))
+        for index, linkage in enumerate(('average',
+                                         'complete',
+                                         'ward',
+                                         'single')):
+            plt.subplot(1, 4, index + 1)
+            model = AgglomerativeClustering(linkage=linkage,
+                                            connectivity=connectivity,
+                                            n_clusters=n_clusters)
+            t0 = time.time()
+            model.fit(X)
+            elapsed_time = time.time() - t0
+            plt.scatter(X[:, 0], X[:, 1], c=model.labels_,
+                        cmap=plt.cm.nipy_spectral)
+            plt.title('linkage=%s\n(time %.2fs)' % (linkage, elapsed_time),
+                      fontdict=dict(verticalalignment='top'))
+            plt.axis('equal')
+            plt.axis('off')
+    
+    The plots display firstly what a K-means algorithm would yield
+using three clusters. It is then shown what the effect of a bad
+initialization is on the classification process:
+By setting n_init to only 1 (default is 10), the amount of
+times that the algorithm will be run with different centroid
+seeds is reduced.
+The next plot displays what using eight clusters would deliver
+and finally the ground truth.
     
     
-def fetch(version):
-    base = 'http://wheels.scipy.org'
-    tree = html.parse(base)
-    root = tree.getroot()
+X, y = nudge_images(X, y)
     
-            df = seed_df(seed_nans, n, m)
-        bins = None, np.arange(0, max(5, df['3rd'].max()) + 1, 2)
-        keys = '1st', '2nd', ['1st', '2nd']
-        for k, b in product(keys, bins):
-            binned.append((df, k, b, n, m))
-            ids.append('{}-{}-{}'.format(k, n, m))
+    import josepy as jose
+import pyrfc3339
     
-    '''
-from .casting import BaseCastingTests  # noqa
-from .constructors import BaseConstructorsTests  # noqa
-from .dtype import BaseDtypeTests  # noqa
-from .getitem import BaseGetitemTests  # noqa
-from .groupby import BaseGroupbyTests  # noqa
-from .interface import BaseInterfaceTests  # noqa
-from .methods import BaseMethodsTests  # noqa
-from .ops import BaseArithmeticOpsTests, BaseComparisonOpsTests, BaseOpsUtil  # noqa
-from .printing import BasePrintingTests  # noqa
-from .reduce import BaseNoReduceTests, BaseNumericReduceTests, BaseBooleanReduceTests  # noqa
-from .missing import BaseMissingTests  # noqa
-from .reshaping import BaseReshapingTests  # noqa
-from .setitem import BaseSetitemTests  # noqa
-from .io import BaseParsingTests  # noqa
-
+    # List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build']
+    
+            :param str lens: lens to check for errors
+    
+        @certbot_util.patch_get_utility()
+    def test_multiple_names(self, mock_util):
+        mock_util().menu.return_value = (display_util.OK, 5)
+    
+            self.assertTrue(self.vhost1.same_server(self.vhost2))
+        self.assertTrue(no_name1.same_server(no_name2))
+    
+    # For 'manual' documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
+    
+    .. code-block:: bash
+   :caption: To acquire a certificate for ``example.com``, waiting 60 seconds
+             for DNS propagation
+    
+    def get_extension(link):
+    extension = os.path.splitext(link)[1][1:]
+    if extension in ['pdf', 'html']:
+        return extension
+    if 'pdf' in extension:
+        return 'pdf'    
+    return 'pdf'    
     
     
-@pytest.mark.parametrize('style,equiv', [
-    ('margin: 1px; margin-top: inherit',
-     'margin-bottom: 1px; margin-right: 1px; margin-left: 1px'),
-    ('margin-top: inherit', ''),
-    ('margin-top: initial', ''),
-])
-def test_css_none_absent(style, equiv):
-    assert_same_resolution(style, equiv)
+# This class can be used to keep the ycmd server alive for the duration of the
+# life of the client. By default, ycmd shuts down if it doesn't see a request in
+# a while.
+class YcmdKeepalive( object ):
+  def __init__( self, ping_interval_seconds = 60 * 10 ):
+    self._keepalive_thread = Thread( target = self._ThreadMain )
+    self._keepalive_thread.daemon = True
+    self._ping_interval_seconds = ping_interval_seconds
     
-    from __future__ import unicode_literals
+    SYNTAX_REGION_ARGUMENT_REGEX = re.compile(
+  r'^(?:matchgroup|start)=.*$' )
+    
+    
+from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 # Not installing aliases from python-future; it's unreliable and slow.
 from builtins import *  # noqa
     
-      return group_name_to_group
+      _assert_rejects( f, { 'text' : 'This is an unimportant taco',
+                        'kind' : 'WARNING' } )
+  _assert_accepts( f, { 'text' : 'This taco will be shown',
+                        'kind' : 'ERROR' } )
     
+        Args:
+        executor_reference: A weakref.ref to the ProcessPoolExecutor that owns
+            this thread. Used to determine if the ProcessPoolExecutor has been
+            garbage collected and that this function can exit.
+        process: A list of the multiprocessing.Process instances used as
+            workers.
+        pending_work_items: A dict mapping work ids to _WorkItems e.g.
+            {5: <_WorkItem...>, 6: <_WorkItem...>, ...}
+        work_ids_queue: A queue.Queue of work ids e.g. Queue([5, 6, ...]).
+        call_queue: A multiprocessing.Queue that will be filled with _CallItems
+            derived from _WorkItems for processing by the process workers.
+        result_queue: A multiprocessing.Queue of _ResultItems generated by the
+            process workers.
+    '''
+    nb_shutdown_processes = [0]
+    def shutdown_one_process():
+        '''Tell a worker to terminate, which will in turn wake us again'''
+        call_queue.put(None)
+        nb_shutdown_processes[0] += 1
+    while True:
+        _add_call_item_to_queue(pending_work_items,
+                                work_ids_queue,
+                                call_queue)
     
-def RegexSingleList_test():
-  opts = _JavaFilter( { 'regex' : [ 'taco' ] } )
-  f = _CreateFilterForTypes( opts, [ 'java' ] )
+    from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
     
+            self.assertEqual(set([CANCELLED_AND_NOTIFIED_FUTURE,
+                              EXCEPTION_FUTURE,
+                              SUCCESSFUL_FUTURE]),
+                         completed_futures)
     
-class FakeFuture( object ):
-  '''A fake version of a future response object, just about suitable for
-  mocking a server response as generated by PostDataToHandlerAsync.
-  Not usually used directly. See MockAsyncServerResponse* methods'''
-  def __init__( self, done, response = None, exception = None ):
-    self._done = done
+        THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE. '''
     
-    def _add_call_item_to_queue(pending_work_items,
-                            work_ids,
-                            call_queue):
-    '''Fills call_queue with _WorkItems from pending_work_items.
+        def process(self, new_face):
+        ''' Sharpen using the requested technique '''
+        amount = self.config['amount'] / 100.0
+        kernel_center = self.get_kernel_size(new_face, self.config['radius'])
+        new_face = getattr(self, self.config['method'])(new_face, kernel_center, amount)
+        return new_face
     
-    # If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-#show_authors = False
+    import logging
+import os
+import re
     
-        author='LittleCoder',
-    author_email='i7meavnktqegm1b@qq.com',
+    from ._base import Output, logger
     
-    def produce_group_chat(core, msg):
-    r = re.match('(@[0-9a-z]*?):<br/>(.*)$', msg['Content'])
-    if r:
-        actualUserName, content = r.groups()
-        chatroomUserName = msg['FromUserName']
-    elif msg['FromUserName'] == core.storageClass.userName:
-        actualUserName = core.storageClass.userName
-        content = msg['Content']
-        chatroomUserName = msg['ToUserName']
-    else:
-        msg['ActualUserName'] = core.storageClass.userName
-        msg['ActualNickName'] = core.storageClass.nickName
-        msg['IsAt'] = False
-        utils.msg_formatter(msg, 'Content')
-        return
-    chatroom = core.storageClass.search_chatrooms(userName=chatroomUserName)
-    member = utils.search_dict_list((chatroom or {}).get(
-        'MemberList') or [], 'UserName', actualUserName)
-    if member is None:
-        chatroom = core.update_chatroom(chatroomUserName)
-        member = utils.search_dict_list((chatroom or {}).get(
-            'MemberList') or [], 'UserName', actualUserName)
-    if member is None:
-        logger.debug('chatroom member fetch failed with %s' % actualUserName)
-        msg['ActualNickName'] = ''
-        msg['IsAt'] = False
-    else:
-        msg['ActualNickName'] = member.get('DisplayName', '') or member['NickName']
-        atFlag = '@' + (chatroom['Self'].get('DisplayName', '') or core.storageClass.nickName)
-        msg['IsAt'] = (
-            (atFlag + (u'\u2005' if u'\u2005' in msg['Content'] else ' '))
-            in msg['Content'] or msg['Content'].endswith(atFlag))
-    msg['ActualUserName'] = actualUserName
-    msg['Content']        = content
-    utils.msg_formatter(msg, 'Content')
+        def set_type_ext(self, path=None):
+        ''' Set the extension '''
+        if path is not None:
+            self.path = path
+        if self.path is not None:
+            item_ext = os.path.splitext(self.path)[1].lower()
+            if item_ext in DataItem.vid_ext:
+                item_type = 'vid'
+            elif item_ext in DataItem.audio_ext:
+                item_type = 'audio'
+            else:
+                item_type = 'dir'
+            self.type = item_type
+            self.ext = item_ext
+            logger.debug('path: '%s', type: '%s', ext: '%s'', self.path, self.type, self.ext)
+        else:
+            return
+    
+        @property
+    def frames_count(self):
+        ''' Return current frames count '''
+        retval = len(self.data)
+        logger.trace(retval)
+        return retval
+    
+                    var_x = K.reshape(inputs, (batch_size,
+                                           self.group,
+                                           channels // self.group,
+                                           height,
+                                           width))
+                mean = K.mean(var_x, axis=[2, 3, 4], keepdims=True)
+                std = K.sqrt(K.var(var_x, axis=[2, 3, 4], keepdims=True) + self.epsilon)
+                var_x = (var_x - mean) / std
+    
+    ln = fp.readline()
+while ln:
+    l = ln.rstrip('\r\n')
+    result = delim.join(cutfunc(ln.rstrip('\r\n'), cutall, hmm))
+    if PY2:
+        result = result.encode(default_encoding)
+    print(result)
+    ln = fp.readline()
+    
+    import jieba
+import jieba.analyse
+from optparse import OptionParser
+    
+    parser = OptionParser(USAGE)
+parser.add_option('-k', dest='topK')
+parser.add_option('-w', dest='withWeight')
+opt, args = parser.parse_args()
