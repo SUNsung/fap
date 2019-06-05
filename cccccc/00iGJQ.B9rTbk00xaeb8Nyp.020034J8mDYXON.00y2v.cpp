d1a05182a00daab449fedabd9bdc42ca602318fe
SUNsung <weatherius@gmail.com>
2019-06-05 22:22:42 +0300

    Rewrite [00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp] File in: 2019-06-05 22:22:41

--- cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
+++ cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
@@ -1,185 +1,238 @@
 
         
-            enum COLOR_SPACE
-    {
-        COLOR_SPACE_BT601,
-        COLOR_SPACE_BT709
-    };
+            int m_nTempCom;                          /* Holding place for the last command.          */
+    int m_openParenCount;                    // Number of open parentheses.
+    std::array<int, MAXPRECDEPTH> m_nOp;     /* Holding array for parenthesis operations.    */
+    std::array<int, MAXPRECDEPTH> m_nPrecOp; /* Holding array for precedence  operations.    */
+    size_t m_precedenceOpCount;              /* Current number of precedence ops in holding. */
+    int m_nLastCom;                          // Last command entered.
+    ANGLE_TYPE m_angletype;                  // Current Angle type when in dec mode. one of deg, rad or grad
+    NUM_WIDTH m_numwidth;                    // one of qword, dword, word or byte mode.
+    int32_t m_dwWordBitWidth;                // # of bits in currently selected word size
     
-    void bitwiseAnd(const Size2D &size,
-                const u8 *src0Base, ptrdiff_t src0Stride,
-                const u8 *src1Base, ptrdiff_t src1Stride,
-                u8 *dstBase, ptrdiff_t dstStride)
+    namespace CalculatorApp
 {
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, BitwiseAnd());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
+    namespace Common
+    {
+        ref class AlwaysSelectedCollectionView sealed : public Windows::UI::Xaml::DependencyObject, public Windows::UI::Xaml::Data::ICollectionView
+        {
+            internal : AlwaysSelectedCollectionView(Windows::UI::Xaml::Interop::IBindableVector ^ source)
+                : m_currentPosition(-1)
+            {
+                m_source = source;
+    }
+    }
+    }
+    }
+    
+    NarratorAnnouncement ^ CalculatorAnnouncement::GetMemoryItemAddedAnnouncement(String ^ announcement)
+{
+    return ref new NarratorAnnouncement(
+        announcement, CalculatorActivityIds::MemoryItemAdded, AutomationNotificationKind::ItemAdded, AutomationNotificationProcessing::MostRecent);
 }
     
-            size_t x = 0;
-        for (; x <= colsn - 4; x += 4)
+    #include <mxnet/op_attr_types.h>
+#include <vector>
+    
+    /*!
+ * Copyright (c) 2016 by Contributors
+ * \file caffe_fieldentry.h
+ * \brief Implement FieldEntry<caffe::LayerParameter>
+ * \author Haoran Wang
+ */
+#ifndef PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
+#define PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
+    
+      virtual void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) {
+    param_.InitAllowUnknown(kwargs);
+    base_->Init(kwargs);
+    rnd_.seed(kRandMagic + param_.seed);
+    outimg_.set_pad(false);
+    meanimg_.set_pad(false);
+    if (param_.mean_img.length() != 0) {
+      std::unique_ptr<dmlc::Stream> fi(
+          dmlc::Stream::Create(param_.mean_img.c_str(), 'r', true));
+      if (fi.get() == nullptr) {
+        this->CreateMeanImg();
+      } else {
+        fi.reset(nullptr);
+        if (param_.verbose) {
+          LOG(INFO) << 'Load mean image from ' << param_.mean_img;
+        }
+        // use python compatible ndarray store format
+        std::vector<NDArray> data;
+        std::vector<std::string> keys;
         {
-            internal::prefetch(ln0 + x);
-            internal::prefetch(ln1 + x);
-            internal::prefetch(ln2 + x);
-box3x3f32_vert_ll:
-            float32x4_t v0 = vld1q_f32(ln0+x);
-            float32x4_t v1 = vld1q_f32(ln1+x);
-            float32x4_t v2 = vld1q_f32(ln2+x);
+          std::unique_ptr<dmlc::Stream> fi(dmlc::Stream::Create(param_.mean_img.c_str(), 'r'));
+          NDArray::Load(fi.get(), &data, &keys);
+        }
+        CHECK_EQ(data.size(), 1U)
+            << 'Invalid mean image file format';
+        data[0].WaitToRead();
+        mshadow::Tensor<cpu, 3> src = data[0].data().get<cpu, 3, real_t>();
+        meanimg_.Resize(src.shape_);
+        mshadow::Copy(meanimg_, src);
+        meanfile_ready_ = true;
+      }
     }
+  }
     
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const u8 * src = internal::getRowPtr(srcBase, srcStride, i);
-        s16 * dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t j = 0;
+    struct dequantize_2bit {
+  MSHADOW_XINLINE static void Map(int i,
+                                  float *out,
+                                  float *in,
+                                  const float neg_threshold,
+                                  const float pos_threshold) {
+    // get position of dequantized value to fill
+    float *outval = out + i;
+    // gets byte which holds quantized value for this position
+    char *ch_ptr = reinterpret_cast<char *>(in + (i >> 4));
+    ch_ptr += ((i & 15) >> 2);
+    // masks used to quantize data
+    const uint8_t posbits[] = {0xc0, 0x30, 0x0c, 0x03};
+    const uint8_t negbits[] = {0x80, 0x20, 0x08, 0x02};
+    // col denotes which two bits of a byte are set for this value
+    // col=0 implies first two bits, col=3 implies last two bits,...
+    const int col = i & 3;
+    const uint8_t mask = posbits[col];
+    const uint8_t negmask = negbits[col];
+    const uint8_t masked = *ch_ptr & mask;
+    if (masked == mask) {
+      *outval = pos_threshold;
+    } else if (masked == negmask) {
+      // use posbits for mask as posbits are both 1s
+      // then compare masked with negbits to see if only negbits were set
+      *outval = neg_threshold;
+    } else {
+      *outval = 0;
     }
-    
-    #ifdef CAROTENE_NEON
-    
-                uint32x4_t lane0 = vld1q_u32(lane + x - 2);
-            uint32x4_t lane4 = vld1q_u32(lane + x + 2);
-            uint32x4_t lane1 = vld1q_u32(lane + x - 1);
-            uint32x4_t lane3 = vld1q_u32(lane + x + 1);
-            uint32x4_t lane2 = vld1q_u32(lane + x + 0);
-    
-            int16x8_t tnext, tc, t0;
-        int16x8_t tnext2, tnext3;
-        int16x8_t tnext1Old, tnext2Old, tnext3Old;
-        int16x8_t tnext4OldOldOld, tnext5OldOldOld;
-    
-    REGISTER_CPU_OPERATOR(EnforceFinite, EnforceFiniteOp<CPUContext>);
-    
-    REGISTER_CUDA_OPERATOR(LC2D, LocallyConnectedOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(
-    LC2DGradient,
-    LocallyConnectedGradientOp<float, CUDAContext>);
-    
-    enum GARBAGE_LEVEL
-{
-  G_NEVER_CRUNCH,
-  G_OK,
-  G_DODGY,
-  G_TERRIBLE
+  }
 };
     
-      // Sets the destination filename and enables images to be written to a PDF
-  // on destruction.
-  void WritePDF(const char* filename) {
-    if (pixaGetCount(pixa_) > 0) {
-      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, 'AllDebugImages', filename);
-      pixaClear(pixa_);
+      /*!
+   * \brief returns type of compression if any
+   */
+  CompressionType get_type();
+    
+      void Init(const TVMArgs& args,
+            const std::vector<int>& const_loc,
+            std::vector<Engine::VarHandle>* const_vars,
+            std::vector<Engine::VarHandle>* mutate_vars) {
+    values_.clear();
+    type_codes_.clear();
+    values_.insert(values_.end(), args.values, args.values + args.size());
+    type_codes_.insert(
+        type_codes_.end(), args.type_codes, args.type_codes + args.size());
+    }
+    
+    namespace mxnet {
+namespace op {
+template<>
+Operator* CreateOp<cpu>(CropParam param) {
+  return new CropOp<cpu>(param);
+}
+    }
+    }
+    
+      virtual void Backward(const OpContext &ctx,
+                        const std::vector<TBlob> &out_grad,
+                        const std::vector<TBlob> &in_data,
+                        const std::vector<TBlob> &out_data,
+                        const std::vector<OpReqType> &req,
+                        const std::vector<TBlob> &in_grad,
+                        const std::vector<TBlob> &aux_args) {
+    using namespace mshadow;
+    CHECK_EQ(in_data.size(), 2U);
+    CHECK_EQ(out_data.size(), 3U);
+    CHECK_EQ(out_grad.size(), 1U);
+    Stream<gpu> *s = ctx.get_stream<gpu>();
+    Tensor<gpu, 4, DType> data = in_data[st::kData].get<gpu, 4, DType>(s);
+    Tensor<gpu, 4, DType> grad = out_grad[st::kOut].get<gpu, 4, DType>(s);
+    Tensor<gpu, 4, DType> ddata = in_grad[st::kData].get<gpu, 4, DType>(s);
+    Shape<3> loc_shape = Shape3(data.size(0), 2, 3);
+    Shape<4> grid_shape = Shape4(grad.size(0), grad.size(2), grad.size(3), 2);
+    Tensor<gpu, 3, DType> dloc = in_grad[st::kLoc].get_with_shape<gpu, 3, DType>(loc_shape, s);
+    Tensor<gpu, 4, DType> grid = out_data[st::kGridSrc]
+                    .get_with_shape<gpu, 4, DType>(grid_shape, s);
+    // do not use out_grad[st::kGridSrc], because dgrid is a intermediate tensor, and not include in
+    // DeclareBackwardDependency, another, we can we reuse grid for inplace operator
+    typename DataType<DType>::ScaleType alpha = 1.0f;
+    typename DataType<DType>::ScaleType beta = 0.0f;
+    typename DataType<DType>::ScaleType alpha_dgrid = 1.0f;
+    typename DataType<DType>::ScaleType beta_dgrid = 0.0f;
+    CUDNN_CALL(cudnnSpatialTfSamplerBackward(s->dnn_handle_,
+                                             st_desc_,
+                                             &alpha,
+                                             in_desc_,
+                                             data.dptr_,
+                                             &beta,
+                                             in_desc_/*reuse in_desc_*/,
+                                             ddata.dptr_/*output*/,
+                                             &alpha_dgrid,
+                                             out_desc_/*reuse out_desc_*/,
+                                             grad.dptr_,
+                                             grid.dptr_,
+                                             &beta_dgrid,
+                                             grid.dptr_));
+    if (param_.transform_type == st::kAffine) {
+      CUDNN_CALL(cudnnSpatialTfGridGeneratorBackward(s->dnn_handle_,
+                                                     st_desc_,
+                                                     grid.dptr_,
+                                                     dloc.dptr_/*out*/));
     }
   }
     
-      // Adds a new point. Takes a copy - the pt doesn't need to stay in scope.
-  // Add must be called on points in sequence along the line.
-  void Add(const ICOORD& pt);
-  // Associates a half-width with the given point if a point overlaps the
-  // previous point by more than half the width, and its distance is further
-  // than the previous point, then the more distant point is ignored in the
-  // distance calculation. Useful for ignoring i dots and other diacritics.
-  void Add(const ICOORD& pt, int halfwidth);
     
-      // Returns the x,y means as an FCOORD.
-  FCOORD mean_point() const;
-    
-      // Return whether a given text line could be a first paragraph line according
-  // to this paragraph model.
-  bool ValidFirstLine(int lmargin, int lindent, int rindent, int rmargin) const;
-    
-    // A useful base class to facilitate the common operation of sorting a vector
-// of owned pointer data using a separate key. This class owns its data pointer,
-// deleting it when it has finished with it, and providing copy constructor and
-// operator= that have move semantics so that the data does not get copied and
-// only a single instance of KDPtrPair holds a specific data pointer.
-template <typename Key, typename Data>
-class KDPtrPair {
- public:
-  KDPtrPair() : data_(nullptr) {}
-  KDPtrPair(Key k, Data* d) : data_(d), key_(k) {}
-  // Copy constructor steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  KDPtrPair(KDPtrPair& src) : data_(src.data_), key_(src.key_) {
-    src.data_ = nullptr;
-  }
-  // Destructor deletes data, assuming it is the sole owner.
-  ~KDPtrPair() {
-    delete this->data_;
-    this->data_ = nullptr;
-  }
-  // Operator= steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  void operator=(KDPtrPair& src) {
-    delete this->data_;
-    this->data_ = src.data_;
-    src.data_ = nullptr;
-    this->key_ = src.key_;
-  }
-    }
-    
-        ProgressWriter::~ProgressWriter()
     {
-    }
+    {}  // namespace op
+}  // namespace mxnet
+
     
     
-    {        // Creating Composite Data Reader that allow to combine deserializers.
-        // This should be changed to link statically when SGD uses the new interfaces.
-        wstring ioName = L'ioName';
-        GetReaderProc getReaderProc = (GetReaderProc)Plugin::Load(readerType, GetReaderName(precision));
-        m_ioNames.push_back(ioName);
-        assert(getReaderProc != nullptr);
-        getReaderProc(&m_dataReaders[ioName]);
-    }
-    else
+    // ============
+    // number types
+    // ============
+    
+    
     {
-        wstring readerType = config(L'readerType', L'Cntk.Deserializers.TextFormat');
-        wstring ioName = L'ioName';
-        // backward support to use only one type of data reader
-        // get the name for the reader we want to use, default to CNTKTextFormatReader
-        GetReaderProc getReaderProc = (GetReaderProc)Plugin::Load(readerType, GetReaderName(precision));
-        m_ioNames.push_back(ioName);
-        assert(getReaderProc != nullptr);
-        getReaderProc(&m_dataReaders[ioName]);
-    }
-    
-    // Exception wrapper to include native call stack string
-template <class E>
-class ExceptionWithCallStack : public E, public IExceptionWithCallStackBase
-{
-public:
-    ExceptionWithCallStack(const std::string& msg, const std::string& callstack) :
-        E(msg), m_callStack(callstack)
-    { }
-    }
-    
-    // Rand based on Mersenne Twister.
-// We use our own distribution in order to match baselines between different operating systems,
-// instead of using std::shuffle which uses unitform_distribution internally.
-// TODO: Switching to Boost would eliminate this problem.
-template <typename TVector>
-inline void RandomShuffleMT(TVector& v, size_t begin, size_t end, std::mt19937_64& rng)
-{
-    for(size_t i = begin; i < end; ++i)
     {
-        const size_t randomLocation = RandMT(i, end, rng);
-        std::swap(v[i], v[randomLocation]);
-    }
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+    
+    TEST(ByteTest, SetGetHighLowBit) {
+  unsigned char byte_value = 0x37;
+  Byte value(&byte_value);
+  value.set_value_high_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
+  value.set_value_low_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
 }
     
-        virtual bool InputUsedInComputingInputNodesGradients(size_t /*childIndex*/) const override
-    {
-        return false;
+    #include 'glog/logging.h'
+    
+      x <<= 2;
+  x |= t;
+    
+    // config detail: {'name': 'commanded_value', 'offset': 0.0, 'precision': 0.001,
+// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 23,
+// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
+double Accelrpt68::commanded_value(const std::uint8_t* bytes,
+                                   int32_t length) const {
+  Byte t0(bytes + 2);
+  int32_t x = t0.get_byte(0, 8);
     }
+    
+    namespace apollo {
+namespace canbus {
+namespace gem {
+    }
+    }
+    }
+    
+    #include 'glog/logging.h'
