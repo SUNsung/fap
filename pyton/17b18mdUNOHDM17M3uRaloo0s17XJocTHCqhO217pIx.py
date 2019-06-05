eba826f15ccad7369da6e64efb0204d93e5d02f7
SUNsung <weatherius@gmail.com>
2019-06-05 10:52:40 +0300

    Rewrite [17b18mdUNOHDM17M3uRaloo0s17XJocTHCqhO217pIx.py] File in: 2019-06-05 10:52:39

--- pyton/17b18mdUNOHDM17M3uRaloo0s17XJocTHCqhO217pIx.py
+++ pyton/17b18mdUNOHDM17M3uRaloo0s17XJocTHCqhO217pIx.py
@@ -1,207 +1,143 @@
 
         
-            def __call__(self, value):
-        keys = set(value)
-        missing_keys = self.keys - keys
-        if missing_keys:
-            raise ValidationError(
-                self.messages['missing_keys'],
-                code='missing_keys',
-                params={'keys': ', '.join(missing_keys)},
-            )
-        if self.strict:
-            extra_keys = keys - self.keys
-            if extra_keys:
-                raise ValidationError(
-                    self.messages['extra_keys'],
-                    code='extra_keys',
-                    params={'keys': ', '.join(extra_keys)},
-                )
-    
-            r = None
         try:
-            r = Redirect.objects.get(site=current_site, old_path=full_path)
-        except Redirect.DoesNotExist:
-            pass
-        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
-            try:
-                r = Redirect.objects.get(
-                    site=current_site,
-                    old_path=request.get_full_path(force_append_slash=True),
-                )
-            except Redirect.DoesNotExist:
-                pass
-        if r is not None:
-            if r.new_path == '':
-                return self.response_gone_class()
-            return self.response_redirect_class(r.new_path)
+	xrange		#Python 2
+except NameError:
+	xrange = range	#Python 3
+    
+    def longestSub(ARRAY): 			#This function is recursive
+	
+	ARRAY_LENGTH = len(ARRAY)
+	if(ARRAY_LENGTH <= 1):  	#If the array contains only one element, we return it (it's the stop condition of recursion)
+		return ARRAY
+								#Else
+	PIVOT=ARRAY[0]
+	isFound=False
+	i=1
+	LONGEST_SUB=[]
+	while(not isFound and i<ARRAY_LENGTH):
+		if (ARRAY[i] < PIVOT):
+			isFound=True
+			TEMPORARY_ARRAY = [ element for element in ARRAY[i:] if element >= ARRAY[i] ]
+			TEMPORARY_ARRAY = longestSub(TEMPORARY_ARRAY)
+			if ( len(TEMPORARY_ARRAY) > len(LONGEST_SUB) ):
+				LONGEST_SUB = TEMPORARY_ARRAY
+		else:
+			i+=1
+    
+    from matplotlib import pyplot as plt
+def plot_heterogeneity(heterogeneity, k):
+    plt.figure(figsize=(7,4))
+    plt.plot(heterogeneity, linewidth=4)
+    plt.xlabel('# Iterations')
+    plt.ylabel('Heterogeneity')
+    plt.title('Heterogeneity of clustering over time, K={0:d}'.format(k))
+    plt.rcParams.update({'font.size': 16})
+    plt.show()
     
     
-@functools.lru_cache()
-def get_default_renderer():
-    renderer_class = import_string(settings.FORM_RENDERER)
-    return renderer_class()
-    
-        def __eq__(self, other):
-        if isinstance(other, Mapping):
-            other = CaseInsensitiveDict(other)
-        else:
-            return NotImplemented
-        # Compare insensitively
-        return dict(self.lower_items()) == dict(other.lower_items())
-    
-        def proxy_headers(self, proxy):
-        '''Returns a dictionary of the headers to add to any request sent
-        through a proxy. This works with urllib3 magic to ensure that they are
-        correctly sent to the proxy, rather than in a tunnelled request if
-        CONNECT is being used.
-    
-    if is_py2:
-    from urllib import (
-        quote, unquote, quote_plus, unquote_plus, urlencode, getproxies,
-        proxy_bypass, proxy_bypass_environment, getproxies_environment)
-    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urldefrag
-    from urllib2 import parse_http_list
-    import cookielib
-    from Cookie import Morsel
-    from StringIO import StringIO
-    from collections import Callable, Mapping, MutableMapping, OrderedDict
+def test_cifar():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data('fine')
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data('coarse')
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
     
     
-def morsel_to_cookie(morsel):
-    '''Convert a Morsel object into a Cookie containing the one k/v pair.'''
+def test_regularization_shared_layer():
+    dense_layer = Dense(num_classes,
+                        kernel_regularizer=regularizers.l1(),
+                        activity_regularizer=regularizers.l1())
+    
+    batch_size = 128
+num_classes = 10
+epochs = 20
+    
+        `log(cosh(x))` is approximately equal to `(x ** 2) / 2` for small `x` and
+    to `abs(x) - log(2)` for large `x`. This means that 'logcosh' works mostly
+    like the mean squared error, but will not be so strongly affected by the
+    occasional wildly incorrect prediction.
+    
+    # If true, '()' will be appended to :func: etc. cross-reference text.
+#add_function_parentheses = True
+    
+            file = request.files['file']
+    
+    # Load a second sample picture and learn how to recognize it.
+biden_image = face_recognition.load_image_file('biden.jpg')
+biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+    
+        # Load a sample picture and learn how to recognize it.
+    obama_image = face_recognition.load_image_file('obama.jpg')
+    obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    
+    Equations take from Yaniv, Yates, & Smith (1991):
+  'Measures of Descrimination Skill in Probabilistic Judgement'
+    
+    # Now I have my proportion of survivors, simply round them such that if <0.5
+# I predict they dont surivive, and if >= 0.5 they do
+survival_table[ survival_table < 0.5 ] = 0
+survival_table[ survival_table >= 0.5 ] = 1
+    
+    # female = 0, Male = 1
+train_df['Gender'] = train_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
     
     
-def info():
-    '''Generate information for a bug report.'''
-    try:
-        platform_info = {
-            'system': platform.system(),
-            'release': platform.release(),
-        }
-    except IOError:
-        platform_info = {
-            'system': 'Unknown',
-            'release': 'Unknown',
-        }
-    
-                sock.close()
-    
-        if no_proxy:
-        # We need to check whether we match here. We need to see if we match
-        # the end of the hostname, both with and without the port.
-        no_proxy = (
-            host for host in no_proxy.replace(' ', '').split(',') if host
-        )
-    
-        :param url: URL for the new :class:`Request` object.
-    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-        object to send in the body of the :class:`Request`.
-    :param json: (optional) json data to send in the body of the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
+def load_data_and_labels():
     '''
-    
-    # Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'Home-Assistant', 'Home Assistant Documentation',
-     author, 'Home Assistant', 'Open-source home automation platform.',
-     'Miscellaneous'),
-]
-    
-    
-def display_temp(hass: HomeAssistant, temperature: float, unit: str,
-                 precision: float) -> float:
-    '''Convert temperature into preferred units/precision for display.'''
-    temperature_unit = unit
-    ha_unit = hass.config.units.temperature_unit
-    
-    
-@asyncio.coroutine
-def test_if_fires_on_hass_start(hass):
-    '''Test the firing when HASS starts.'''
-    calls = async_mock_service(hass, 'test', 'automation')
-    hass.state = CoreState.not_running
-    config = {
-        automation.DOMAIN: {
-            'alias': 'hello',
-            'trigger': {
-                'platform': 'homeassistant',
-                'event': 'start',
-            },
-            'action': {
-                'service': 'test.automation',
-            }
-        }
-    }
-    
-    
-def _expand_to_class_specific_mask_targets(masks, mask_class_labels):
-    '''Expand masks from shape (#masks, M ** 2) to (#masks, #classes * M ** 2)
-    to encode class specific mask targets.
+    Loads MR polarity data from files, splits the data into words and generates labels.
+    Returns split sentences and labels.
     '''
-    assert masks.shape[0] == mask_class_labels.shape[0]
-    M = cfg.MRCNN.RESOLUTION
+    # Load data from files
+    positive_examples = list(open('./data/rt-polarity.pos', encoding='ISO-8859-1').readlines())
+    positive_examples = [s.strip() for s in positive_examples]
+    negative_examples = list(open('./data/rt-polarity.neg', encoding='ISO-8859-1').readlines())
+    negative_examples = [s.strip() for s in negative_examples]
+    # Split by words
+    x_text = positive_examples + negative_examples
+    x_text = [clean_str(sent) for sent in x_text]
+    x_text = [s.split(' ') for s in x_text]
+    # Generate labels
+    positive_labels = [[0, 1] for _ in positive_examples]
+    negative_labels = [[1, 0] for _ in negative_examples]
+    y = np.concatenate([positive_labels, negative_labels], 0)
+    return [x_text, y]
     
-        net = core.Net('dequeue_net')
-    net.type = 'dag'
-    all_blobs = []
-    for gpu_id in range(cfg.NUM_GPUS):
-        with core.NameScope('gpu_{}'.format(gpu_id)):
-            with core.DeviceScope(muji.OnGPU(gpu_id)):
-                for blob_name in blob_names:
-                    blob = core.ScopedName(blob_name)
-                    all_blobs.append(blob)
-                    workspace.CreateBlob(blob)
-                    logger.info('Creating blob: {}'.format(blob))
-                net.DequeueBlobs(
-                    roi_data_loader._blobs_queue_name, blob_names)
-    logger.info('Protobuf:\n' + str(net.Proto()))
+            # generate a matrix to store the output
+        dt = X.dtype
+        if output_pixel_vals:
+            dt = 'uint8'
+        out_array = numpy.zeros(out_shape, dtype=dt)
     
+        for _ in range(n_samples):
+        history = collections.deque(1.2 * np.ones(history_len) + 0.2 * \
+                                    (np.random.rand(history_len) - 0.5))
+        # Preallocate the array for the time-series
+        inp = np.zeros((sample_len,1))
+        
+        for timestep in range(sample_len):
+            for _ in range(delta_t):
+                xtau = history.popleft()
+                history.append(timeseries)
+                timeseries = history[-1] + (0.2 * xtau / (1.0 + xtau ** 10) - \
+                             0.1 * history[-1]) / delta_t
+            inp[timestep] = timeseries
+        
+        # Squash timeseries through tanh
+        inp = np.tanh(inp - 1)
+        samples.append(inp)
+    return samples
     
-if __name__ == '__main__':
-    sys.exit(main())
-
+        def show_decomp(i=0):
+        plot_image_components(data[i], Xproj[i],
+                              pca.mean_, pca.components_)
     
-    
-@pytest.mark.parametrize(
-    'invalid_css,remainder', [
-        # No colon
-        ('hello-world', ''),
-        ('border-style: solid; hello-world', 'border-style: solid'),
-        ('border-style: solid; hello-world; font-weight: bold',
-         'border-style: solid; font-weight: bold'),
-        # Unclosed string fail
-        # Invalid size
-        ('font-size: blah', 'font-size: 1em'),
-        ('font-size: 1a2b', 'font-size: 1em'),
-        ('font-size: 1e5pt', 'font-size: 1em'),
-        ('font-size: 1+6pt', 'font-size: 1em'),
-        ('font-size: 1unknownunit', 'font-size: 1em'),
-        ('font-size: 10', 'font-size: 1em'),
-        ('font-size: 10 pt', 'font-size: 1em'),
-    ])
-def test_css_parse_invalid(invalid_css, remainder):
-    with tm.assert_produces_warning(CSSWarning):
-        assert_same_resolution(invalid_css, remainder)
-    
-        logging.warning('Starting fetch with curl client')
-    curl_client = CurlAsyncHTTPClient()
-    curl_client.fetch('http://localhost:%d/' % options.port,
-                      callback=callback)
-    IOLoop.current().start()
-    
-       Tornado requires the `~asyncio.AbstractEventLoop.add_reader` family of
-   methods, so it is not compatible with the `~asyncio.ProactorEventLoop` on
-   Windows. Use the `~asyncio.SelectorEventLoop` instead.
-'''
-    
-        def get_target_delegate(
-        self, target: Any, request: httputil.HTTPServerRequest, **target_params: Any
-    ) -> Optional[httputil.HTTPMessageDelegate]:
-        '''Returns an instance of `~.httputil.HTTPMessageDelegate` for a
-        Rule's target. This method is called by `~.find_handler` and can be
-        extended to provide additional target types.
+    interact(show_decomp, i=(0, data.shape[0] - 1));
