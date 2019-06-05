06bcd594fb67ef5ae12c13bebe5c263b06e65095
SUNsung <weatherius@gmail.com>
2019-06-06 02:16:59 +0300

    Rewrite [29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb] File in: 2019-06-06 02:16:59

--- ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
+++ ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
@@ -1,66 +1,137 @@
 
         
-        describe JobsHelper do
-  let(:job) { Delayed::Job.new }
+        require 'action_view/helpers/tags/collection_helpers'
     
-    describe DefaultScenarioImporter do
-  let(:user) { users(:bob) }
-  describe '.import' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      mock(DefaultScenarioImporter).seed(is_a(User))
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'true' }
-      DefaultScenarioImporter.import(user)
+                options = options.dup
+            options[:field_name]           = @method_name
+            options[:include_position]     = true
+            options[:prefix]             ||= @object_name
+            options[:index]                = @auto_index if @auto_index && !options.has_key?(:index)
+    
+          def self.symbols
+        type_klass::SET.symbols
+      end
     end
-    
-          @scheduler.schedule_scheduler_agents
-    
-        it 'should generate the correct specific tracking url' do
-      @checker.options['path'] = 'trackings/usps/9361289878905919630610'
-      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/trackings/usps/9361289878905919630610')
-    end
-    
-        def show
-      authorize @user, :change_email?
-    end
-    
-      def self.provides_callback_for(provider)
-    provider_id = provider.to_s.chomp '_oauth2'
-    
-    module ExportControllerConcern
-  extend ActiveSupport::Concern
-    
-      def preferred_locale
-    http_accept_language.preferred_language_from(available_locales)
   end
-    
-    class MediaProxyController < ApplicationController
-  include RoutingHelper
-    
-      # Wraps a string in escaped quotes if it contains whitespace.
-  def quote
-    /\s/ =~ self ? '\'#{self}\'' : '#{self}'
-  end
-    
-    class Scene
-  def initialize
-    @spheres = Array.new
-    @spheres[0] = Sphere.new(Vec.new(-2.0, 0.0, -3.5), 0.5)
-    @spheres[1] = Sphere.new(Vec.new(-0.5, 0.0, -3.0), 0.5)
-    @spheres[2] = Sphere.new(Vec.new(1.0, 0.0, -2.2), 0.5)
-    @plane = Plane.new(Vec.new(0.0, -0.5, 0.0), Vec.new(0.0, 1.0, 0.0))
-  end
-    
-    def to_array(proc)
-  array = []
-    
-    def mkboard
-  (0...ROW*COL).each{|i|
-    if i % ROW >= ROW-NP
-      $b[i] = -2
-    else
-      $b[i] = -1
-    end
-    $b[3*ROW+3]=$b[3*ROW+4]=$b[4*ROW+3]=$b[4*ROW+4]=-2
-  }
 end
+
+    
+          def index
+        render template: 'some/template', locals: { foo: 'less than 3' }
+      end
+    end
+    
+    class Api::Web::SettingsController < Api::Web::BaseController
+  respond_to :json
+    
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
+  end
+    
+    require 'bundler/cli'
+require 'bundler/friendly_errors'
+    
+    if $0 == __FILE__
+  begin
+    LogStash::PluginManager::Main.run('bin/logstash-plugin', ARGV)
+  rescue LogStash::PluginManager::Error => e
+    $stderr.puts(e.message)
+    exit(1)
+  end
+end
+
+    
+        def prepare_package(explicit_plugins, temp_path)
+      FileUtils.mkdir_p(::File.join(temp_path, LOGSTASH_DIR))
+      FileUtils.mkdir_p(::File.join(temp_path, DEPENDENCIES_DIR))
+    
+            if Utils::HttpClient.remote_file_exist?(uri)
+          PluginManager.ui.debug('Found package at: #{uri}')
+          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
+        else
+          PluginManager.ui.debug('Package not found at: #{uri}')
+          return nil
+        end
+      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
+        # This probably means there is a firewall in place of the proxy is not correctly configured.
+        # So lets skip this strategy but log a meaningful errors.
+        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
+    
+        def user_feedback_string_for(action, platform, machines, options={})
+      experimental_string = options['experimental'] ? 'experimental' : 'non experimental'
+      message  = '#{action} all #{experimental_string} VM's defined in acceptance/Vagrantfile'
+      '#{message} for #{platform}: #{machines}' if !platform.nil?
+    end
+    
+        after(:all) do
+      logstash.uninstall
+    end
+    
+        before do
+      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
+      # Logstash won't update when we have a pinned version in the gemfile so we remove them
+      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
+      expect(logstash).to have_installed?(plugin_name, previous_version)
+    end
+    
+    # Set some attributes
+package.name = 'my-service'
+package.version = '1.0'
+    
+          if @command.inputs
+        mandatory(@command.input_type == 'dir', '--inputs is only valid with -s dir')
+      end
+    
+        # npm installs dependencies in the module itself, so if you do
+    # 'npm install express' it installs dependencies (like 'connect')
+    # to: node_modules/express/node_modules/connect/...
+    #
+    # To that end, I don't think we necessarily need to include
+    # any automatic dependency information since every 'npm install'
+    # is fully self-contained. That's why you don't see any bother, yet,
+    # to include the package's dependencies in here.
+    #
+    # It's possible someone will want to decouple that in the future,
+    # but I will wait for that feature request.
+  end
+    
+      option '--postinstall-action', 'POSTINSTALL_ACTION',
+    'Post-install action provided in package metadata. ' \
+    'Optionally one of '#{POSTINSTALL_ACTIONS.join('', '')}'.' do |value|
+    if !POSTINSTALL_ACTIONS.include?(value)
+      raise ArgumentError, 'osxpkg-postinstall-action value of '#{value}' is invalid. ' \
+        'Must be one of #{POSTINSTALL_ACTIONS.join(', ')}'
+    end
+    value
+  end
+    
+        # easy_install will put stuff in @tmpdir/packagename/, so find that:
+    #  @tmpdir/somepackage/setup.py
+    dirs = ::Dir.glob(File.join(target, '*'))
+    if dirs.length != 1
+      raise 'Unexpected directory layout after easy_install. Maybe file a bug? The directory is #{build_path}'
+    end
+    return dirs.first
+  end # def download
+    
+      # Returns the path to the tar file containing the packed up staging directory
+  def payload
+    payload_tar = build_path('payload.tar')
+    logger.info('Creating payload tar ', :path => payload_tar)
+    
+      def default_output
+    v = version
+    v = '#{epoch}:#{v}' if epoch
+    if iteration
+      '#{name}_#{v}-#{iteration}_#{architecture}.#{type}'
+    else
+      '#{name}_#{v}_#{architecture}.#{type}'
+    end
+  end # def default_output
+end # class FPM::Deb
+    
+        stdout_r_str = nil
+    exit_code = execmd(args, :stdin=>false, :stderr=>false) do |stdout|
+      stdout_r_str = stdout.read
+    end
+    success = (exit_code == 0)
