64c035cb851ca52ff71dfb358d1a7478a1bc935b
SUNsung <weatherius@gmail.com>
2019-06-05 07:46:14 +0300

    Rewrite [11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py] File in: 2019-06-05 07:46:13

--- pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
+++ pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
@@ -1,91 +1,148 @@
 
         
-            with client:
-        auth.logout()
-        assert 'user_id' not in session
+        def generateKey(keySize):
+    print('Generating prime p...')
+    p = rabinMiller.generateLargePrime(keySize)
+    print('Generating prime q...')
+    q = rabinMiller.generateLargePrime(keySize)
+    n = p * q
+    
+                if new_key is None:
+                break
+    
+                return self.dp[x][y]
+    
+    def not32(i):
+	i_str = format(i,'032b')
+	new_str = ''
+	for c in i_str:
+		new_str += '1' if c=='0' else '0'
+	return int(new_str,2)
+    
+        return score
 
     
-        auth.login()
-    response = client.get('/')
-    assert b'test title' in response.data
-    assert b'by test on 2018-01-01' in response.data
-    assert b'test\nbody' in response.data
-    assert b'href='/1/update'' in response.data
-    
-    
-def _dump_loader_info(loader):
-    yield 'class: %s.%s' % (type(loader).__module__, type(loader).__name__)
-    for key, value in sorted(loader.__dict__.items()):
-        if key.startswith('_'):
-            continue
-        if isinstance(value, (tuple, list)):
-            if not all(isinstance(x, (str, text_type)) for x in value):
-                continue
-            yield '%s:' % key
-            for item in value:
-                yield '  - %s' % item
-            continue
-        elif not isinstance(value, (str, text_type, int, float, bool)):
-            continue
-        yield '%s: %r' % (key, value)
-    
-    This typically means that you attempted to use functionality that needed
-an active HTTP request.  Consult the documentation on testing for
-information about how to avoid this problem.\
+    * @author chinmoy159
+* @version 1.0 dated 10/08/2017
 '''
-_app_ctx_err_msg = '''\
-Working outside of application context.
     
-        app.session_interface.serializer.register(TagOrderedDict, index=0)
     
-        Implements signals based on blinker if available, otherwise
-    falls silently back to a noop.
+'''
+* Wondering how this method works !
+* It's pretty simple.
+* Let's say you need to calculate a ^ b
+* RULE 1 : a * b = (a+a) * (b/2) ---- example : 4 * 4 = (4+4) * (4/2) = 8 * 2
+* RULE 2 : IF b is ODD, then ---- a * b = a + (a * (b - 1)) :: where (b - 1) is even.
+* Once b is even, repeat the process to get a * b
+* Repeat the process till b = 1 OR b = 0, because a*1 = a AND a*0 = 0
+*
+* As far as the modulo is concerned,
+* the fact : (a+b) % c = ((a%c) + (b%c)) % c
+* Now apply RULE 1 OR 2, whichever is required.
+'''
+
     
-            if trv is not None:
-            return trv
-        raise TemplateNotFound(template)
     
-            url = url_parse(path)
-        base_url = '{scheme}://{netloc}/{path}'.format(
-            scheme=url.scheme or url_scheme,
-            netloc=url.netloc or http_host,
-            path=app_root.lstrip('/')
+# ALTERNATIVE METHODS  
+# ctbi= characters that must be in password
+# i= how many letters or characters the password length will be 
+def password_generator(ctbi, i):
+  # Password generator = full boot with random_number, random_letters, and random_character FUNCTIONS
+  pass  # Put your code here...
+    
+            status_line = 'HTTP/{version} {status} {reason}'.format(
+            version=version,
+            status=original.status,
+            reason=original.reason
         )
-        path = url.path
+        headers = [status_line]
+        try:
+            # `original.msg` is a `http.client.HTTPMessage` on Python 3
+            # `_headers` is a 2-tuple
+            headers.extend(
+                '%s: %s' % header for header in original.msg._headers)
+        except AttributeError:
+            # and a `httplib.HTTPMessage` on Python 2.x
+            # `headers` is a list of `name: val<CRLF>`.
+            headers.extend(h.strip() for h in original.msg.headers)
+    
+        def __init__(self, groups, env=Environment(), **kwargs):
+        '''
+        :param groups: names of processor groups to be applied
+        :param env: Environment
+        :param kwargs: additional keyword arguments for processors
+    
+        package_name = '(builtin)'
+    
+        # Auth
+    def get_auth_plugins(self):
+        return [plugin for plugin in self if issubclass(plugin, AuthPlugin)]
     
     
-def get_git_tags():
-    return set(
-        Popen(['git', 'tag'], stdout=PIPE).communicate()[0].splitlines()
+def test_current_version():
+    version = Environment().config['__meta__']['httpie']
+    assert version == __version__
+
+    
+    
+def rst_filenames():
+    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
+        if '.tox' not in root:
+            for filename in fnmatch.filter(filenames, '*.rst'):
+                yield os.path.join(root, filename)
+    
+    from httpie import __version__
+from httpie.compat import is_windows
+    
+    The Fast R-CNN head produces a feature representation of the RoI for the purpose
+of bounding box classification and regression. The box output module converts
+the feature representation into classification and regression predictions.
+'''
+    
+    
+def _generate_anchors(base_size, scales, aspect_ratios):
+    '''Generate anchor (reference) windows by enumerating aspect ratios X
+    scales wrt a reference (0, 0, base_size - 1, base_size - 1) window.
+    '''
+    anchor = np.array([1, 1, base_size, base_size], dtype=np.float) - 1
+    anchors = _ratio_enum(anchor, aspect_ratios)
+    anchors = np.vstack(
+        [_scale_enum(anchors[i, :], scales) for i in range(anchors.shape[0])]
     )
-    
-        @app.teardown_appcontext
-    def teardown_app(error=None):
-        called.append('app')
+    return anchors
     
     
-class AbstractBaseSession(models.Model):
-    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
-    session_data = models.TextField(_('session data'))
-    expire_date = models.DateTimeField(_('expire date'), db_index=True)
+def rpn(model):
+    logger.warn(
+        'Deprecated: use `MODEL.TYPE: generalized_rcnn` with '
+        '`MODEL.RPN_ONLY: True`'
+    )
+    return generalized_rcnn(model)
+    
+        model.AddMetrics(['retnet_fg_num', 'retnet_bg_num'])
+    # ==========================================================================
+    # bbox regression loss - SelectSmoothL1Loss for multiple anchors at a location
+    # ==========================================================================
+    for lvl in range(k_min, k_max + 1):
+        suffix = 'fpn{}'.format(lvl)
+        bbox_loss = model.net.SelectSmoothL1Loss(
+            [
+                'retnet_bbox_pred_' + suffix,
+                'retnet_roi_bbox_targets_' + suffix,
+                'retnet_roi_fg_bbox_locs_' + suffix, 'retnet_fg_num'
+            ],
+            'retnet_loss_bbox_' + suffix,
+            beta=cfg.RETINANET.BBOX_REG_BETA,
+            scale=model.GetLossScale() * cfg.RETINANET.BBOX_REG_WEIGHT
+        )
+        gradients.append(bbox_loss)
+        losses.append('retnet_loss_bbox_' + suffix)
     
     
-@x_robots_tag
-def index(request, sitemaps,
-          template_name='sitemap_index.xml', content_type='application/xml',
-          sitemap_url_name='django.contrib.sitemaps.views.sitemap'):
+class GenerateProposalLabelsOp(object):
     
-            env.scrapy_all_settings.append({
-            'docname': env.docname,
-            'setting_name': setting_name,
-            'refid': refid,
-        })
-    
-    
-class Command(ScrapyCommand):
-    
-    
-    
-        args = parser.parse_args()
-    
-        url='https://github.com/littlecodersh/ItChat',
+    from detectron.core.config import cfg
+import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
+import detectron.roi_data.retinanet as retinanet_roi_data
+import detectron.roi_data.rpn as rpn_roi_data
+import detectron.utils.blob as blob_utils
