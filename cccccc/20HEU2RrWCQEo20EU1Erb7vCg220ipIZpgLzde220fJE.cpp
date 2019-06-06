d95e03397490edfc3c90190476e9b2e02210a255
SUNsung <weatherius@gmail.com>
2019-06-06 22:48:34 +0300

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2019-06-06 22:48:33

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,163 +1,39 @@
 
         
-            http://www.apache.org/licenses/LICENSE-2.0
-    
-    REGISTER_OP('ShapelessOp');
-    
-      // Register ufuncs
-  auto register_ufunc = [&](const char* name, PyUFuncGenericFunction fn,
-                            const std::array<int, 3>& types) {
-    Safe_PyObjectPtr ufunc_obj =
-        make_safe(PyObject_GetAttrString(numpy.get(), name));
-    if (!ufunc_obj) {
-      return false;
-    }
-    PyUFuncObject* ufunc = reinterpret_cast<PyUFuncObject*>(ufunc_obj.get());
-    if (types.size() != ufunc->nargs) {
-      PyErr_Format(PyExc_AssertionError,
-                   'ufunc %s takes %d arguments, loop takes %lu', name,
-                   ufunc->nargs, types.size());
-      return false;
-    }
-    if (PyUFunc_RegisterLoopForType(ufunc, npy_bfloat16_, fn,
-                                    const_cast<int*>(types.data()),
-                                    nullptr) < 0) {
-      return false;
-    }
-    return true;
-  };
-    
-    namespace tensorflow {
-    }
-    
-    #endif  // TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_H_
-
-    
-    namespace tensorflow {
-    }
-    
-    #include <Python.h>
-    
-    #ifndef TENSORFLOW_PYTHON_LIB_IO_PY_RECORD_READER_H_
-#define TENSORFLOW_PYTHON_LIB_IO_PY_RECORD_READER_H_
-    
-    port::StatusOr<StreamExecutor*> ExecutorCache::GetOrCreate(
-    const StreamExecutorConfig& config,
-    const std::function<ExecutorFactory>& factory) {
-  // In the fast path case, the cache already has an entry and we can just
-  // return after Get() which only takes a shared lock and not a unique lock.
-  // If we need to create, we take a unique lock on cache_.
-  auto fast_result = Get(config);
-  if (fast_result.ok()) {
-    return fast_result;
-  }
-    }
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    static internal::GrpcLibraryInitializer g_gli_initializer;
-ChannelCredentials::ChannelCredentials() { g_gli_initializer.summon(); }
-    
-    bool AuthPropertyIterator::operator==(const AuthPropertyIterator& rhs) const {
-  if (property_ == nullptr || rhs.property_ == nullptr) {
-    return property_ == rhs.property_;
-  } else {
-    return index_ == rhs.index_;
-  }
-}
-    
-    
-    {}  // namespace grpc
-    
-      CensusServerCallData()
-      : gc_(nullptr),
-        auth_context_(nullptr),
-        recv_initial_metadata_(nullptr),
-        initial_on_done_recv_initial_metadata_(nullptr),
-        initial_on_done_recv_message_(nullptr),
-        recv_message_(nullptr),
-        recv_message_count_(0),
-        sent_message_count_(0) {
-    memset(&census_bin_, 0, sizeof(grpc_linked_mdelem));
-    memset(&path_, 0, sizeof(grpc_slice));
-    memset(&on_done_recv_initial_metadata_, 0, sizeof(grpc_closure));
-    memset(&on_done_recv_message_, 0, sizeof(grpc_closure));
-  }
-    
-    #include <unordered_set>
-#include <vector>
-    
-    #ifndef GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
-#define GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
-    
-    
-    {
-    {}  // namespace load_reporter
-}  // namespace grpc
-    
-    #if !defined(GPR_LINUX) && !defined(GPR_WINDOWS) && !defined(GPR_APPLE)
-    
-            for (;;) {
-          auto start = index.fetch_add(work_chunk);
-          auto const stop = std::min(start + work_chunk, inputs.size());
-          if (start >= stop) break;
-    }
-    
-    #endif
-
-    
-      const char* data = filename.data();
-  int data_len = filename.length();
-  bool base64 = false;
-  if (strncmp(data, 'data:', sizeof('data:') - 1)) {
-    return nullptr;
-  }
-  data += sizeof('data:') - 1;
-  data_len -= sizeof('data:') - 1;
-    
-    #ifndef HPHP_GLOB_STREAM_WRAPPER_H
-#define HPHP_GLOB_STREAM_WRAPPER_H
-    
-    #pragma once
-    
-    
-    {    Rational Sinh(Rational const& rat);
-    Rational Cosh(Rational const& rat);
-    Rational Tanh(Rational const& rat);
-    Rational ASinh(Rational const& rat);
-    Rational ACosh(Rational const& rat);
-    Rational ATanh(Rational const& rat);
-}
-
-    
-    INarratorAnnouncementHost^ NarratorAnnouncementHostFactory::GetHostProducer()
-{
-    for (INarratorAnnouncementHost^ host : NarratorAnnouncementHostFactory::s_hosts)
-    {
-        if (host->IsHostAvailable())
-        {
-            return host;
-        }
+        	void EncodeMipmaps(float *a_pafSourceRGBA,
+		unsigned int a_uiSourceWidth,
+		unsigned int a_uiSourceHeight,
+		Image::Format a_format,
+		ErrorMetric a_eErrMetric,
+		float a_fEffort,
+		unsigned int a_uiJobs,
+		unsigned int a_uiMaxJobs,
+		unsigned int a_uiMaxMipmaps,
+		unsigned int a_uiMipFilterFlags,
+		RawImage* a_pMipmapImages,
+		int *a_piEncodingTime_ms, 
+		bool a_bVerboseOutput)
+	{
+		auto mipWidth = a_uiSourceWidth;
+		auto mipHeight = a_uiSourceHeight;
+		int totalEncodingTime = 0;
+		for(unsigned int mip = 0; mip < a_uiMaxMipmaps && mipWidth >= 1 && mipHeight >= 1; mip++)
+		{
+			float* pImageData = nullptr;
+			float* pMipImage = nullptr;
     }
     }
     
-    namespace CalculatorApp::Common::Automation
-{
-    public ref class NarratorNotifier sealed : public Windows::UI::Xaml::DependencyObject
-    {
-    public:
-        NarratorNotifier();
-    }
-    }
+    	protected:
     
-        case CustomPeers::AutomationNotificationKind::ActionAborted:
-        return StandardPeers::AutomationNotificationKind::ActionAborted;
+    	// pixel indices for different block halves
+	const unsigned int Block4x4Encoding_ETC1::s_auiLeftPixelMapping[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
+	const unsigned int Block4x4Encoding_ETC1::s_auiRightPixelMapping[8] = { 8, 9, 10, 11, 12, 13, 14, 15 };
+	const unsigned int Block4x4Encoding_ETC1::s_auiTopPixelMapping[8] = { 0, 1, 4, 5, 8, 9, 12, 13 };
+	const unsigned int Block4x4Encoding_ETC1::s_auiBottomPixelMapping[8] = { 2, 3, 6, 7, 10, 11, 14, 15 };
     
-    const int c_unitsOfDate = 4;                 // Units Year,Month,Week,Day
-const int c_unitsGreaterThanDays = 3;        // Units Greater than Days (Year/Month/Week) 3
-const int c_daysInWeek = 7;
+    std::ostream& operator <<(std::ostream &s, const IntPoint &p);
+std::ostream& operator <<(std::ostream &s, const Path &p);
+std::ostream& operator <<(std::ostream &s, const Paths &p);
+    
+    #ifdef OPUS_ARM_INLINE_MEDIA
