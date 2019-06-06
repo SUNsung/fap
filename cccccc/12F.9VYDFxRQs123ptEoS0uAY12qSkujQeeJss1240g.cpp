d6d474b1a1ceb003f397092a889af96f8d846c34
SUNsung <weatherius@gmail.com>
2019-06-06 07:20:28 +0300

    Rewrite [12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp] File in: 2019-06-06 07:20:28

--- cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
+++ cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
@@ -1,238 +1,240 @@
 
         
-        namespace {
-    }
+        #include 'base/basictypes.h'
+#include '../dispatcher_host.h'
     
-    namespace atom {
-    }
-    
-      v8::Local<v8::Value> URLRequest(v8::Isolate* isolate);
-    
-    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
- public:
-  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
-  ~AtomJavaScriptDialogManager() override;
-    }
-    
-    
-    {  DISALLOW_IMPLICIT_CONSTRUCTORS(AutoUpdater);
-};
-    
-    #if defined(OS_LINUX)
-typedef PowerObserverLinux PowerObserver;
-#else
-typedef base::PowerObserver PowerObserver;
-#endif  // defined(OS_LINUX)
-    
-      // Up until now, the parent process was blocked in a read waiting for the
-  // write above to complete. The parent process is now free to exit. Wait for
-  // that to happen.
-  struct kevent event;
-  int events = kevent(kq.get(), nullptr, 0, &event, 1, nullptr);
-  if (events != 1) {
-    if (events < 0) {
-      PLOG(ERROR) << 'kevent (monitor)';
-    } else {
-      LOG(ERROR) << 'kevent (monitor): unexpected result ' << events;
-    }
-    return;
-  }
-    
-      // views::View:
-  void Layout() override;
-  gfx::Size GetMinimumSize() const override;
-  gfx::Size GetMaximumSize() const override;
-    
-    namespace atom {
-    }
-    
-        if (node->Left) {
-      IndentScope ms(this, (childKind == ChildKind::Left ||
-                            childKind == ChildKind::Root) ? '  ' : '| ');
-      print(node->Left, ChildKind::Left);
-    }
-    
-    swift::unicode::GraphemeClusterBreakProperty
-swift::unicode::getGraphemeClusterBreakProperty(uint32_t C) {
-  // FIXME: replace linear search with a trie lookup.
-    }
-    
-    
-    {public:
-  Windows(const Driver &D, const llvm::Triple &Triple) : ToolChain(D, Triple) {}
-  ~Windows() = default;
-  std::string sanitizerRuntimeLibName(StringRef Sanitizer,
-                                      bool shared = true) const override;
-};
-    
-    Optional<FrontendInputsAndOutputs> ArgsToFrontendInputsConverter::convert(
-    SmallVectorImpl<std::unique_ptr<llvm::MemoryBuffer>> *buffers) {
-  SWIFT_DEFER {
-    if (buffers) {
-      std::move(ConfigurationFileBuffers.begin(),
-                ConfigurationFileBuffers.end(),
-                std::back_inserter(*buffers));
-      // Clearing the original list of buffers isn't strictly necessary, but
-      // makes the behavior more sensible if we were to call convert() again.
-      ConfigurationFileBuffers.clear();
-    }
-  };
-    }
-    
-        typedef ptrdiff_t  stride_t;
-    
-    #include 'common.hpp'
-#include 'vtransform.hpp'
-    
-    
-    {    void operator() (const u32 * src0, const u32 * src1, u32 * dst) const
-    {
-        dst[0] = saturate_cast<u32>(alpha*src0[0] + beta*src1[0] + gamma);
-    }
-};
-    
-    void blur3x3(const Size2D &size,
-             const u8 * srcBase, ptrdiff_t srcStride,
-             u8 * dstBase, ptrdiff_t dstStride,
-             BORDER_MODE border, u8 borderValue)
-{
-    internal::assertSupportedConfiguration(isBlur3x3Supported(size, border));
-#ifdef CAROTENE_NEON
-    const int16x8_t v_scale = vmovq_n_s16(3640);
-    const uint16x8_t v_border_x3 = vdupq_n_u16(borderValue * 3);
-    const uint16x8_t v_zero = vdupq_n_u16(0);
-    const uint8x8_t v_border = vdup_n_u8(borderValue);
-    }
-    
-    #endif //CAROTENE_NEON
-    
-                uint32x4_t vlx1 = vmvnq_u32(vequ1);
-            uint32x4_t vlx2 = vmvnq_u32(vequ2);
-    
-            const s16* ln0 = idx_rm2 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rm2) : tmp;
-        const s16* ln1 = idx_rm1 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rm1) : tmp;
-        const s16* ln2 = internal::getRowPtr(srcBase, srcStride, i);
-        const s16* ln3 = idx_rp1 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rp1) : tmp;
-        const s16* ln4 = idx_rp2 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rp2) : tmp;
-    
-    
-    {} // namespace CAROTENE_NS
-
-    
-    
-    {            u32 buf[8];
-            vst1_u32(buf, vget_low_u32(el8shr01l));
-            vst1_u32(buf+2, el2l);
-            vst1_u32(buf+4, el2hl);
-            vst1_u32(buf+6, el2hh);
-            for(u32 k=0; k < 8; k++)
-                sqsum[j+k] = prev + prevSqSum[j+k] + buf[k];
-            prev += buf[7];
-        }
-    
-    inline uint8x8_t vqtbl1_u8 (uint8x16_t a, uint8x8_t b)
-{
-#ifdef __aarch64__
-    // AArch64 supports this natively
-    return ::vqtbl1_u8(a, b);
-#else
-    union { uint8x16_t v; uint8x8x2_t w; } u = { a };
-    return vtbl2_u8(u.w, b);
-#endif
+    Menu::Menu(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+           const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
+  Create(option);
 }
     
-    struct TemporaryFile {
-  std::string path;
+    bool MenuDelegate::GetIconForCommandId(int command_id,
+                                       gfx::Image* icon) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  if (item->icon_.IsEmpty())
+    return false;
     }
     
-    namespace {
-    }
+      for(auto* item: menu_items_) {
+    item->RemoveKeys();
+  }
     
-              vector<TensorShape> out(1);
-          switch (order) {
-            case StorageOrder::NCHW:
-              out[0] = CreateTensorShape(
-                  vector<int>{N, C * kernel_h * kernel_w, out_h, out_w},
-                  TensorProto::FLOAT);
-              break;
-            case StorageOrder::NHWC:
-              out[0] = CreateTensorShape(
-                  vector<int>{N, out_h, out_w, kernel_h * kernel_w * C},
-                  TensorProto::FLOAT);
-              break;
-            default:
-              CAFFE_THROW('Unknown storage order: ', order);
-          }
+    MenuItem::MenuItem(int id,
+                   const base::WeakPtr<ObjectManager>& object_manager,
+                   const base::DictionaryValue& option,
+                   const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id) {
+  Create(option);
+}
     
-    namespace caffe2 {
-    }
+     protected:
+  ~NwClipboardSetListSyncFunction() override;
     
-    #ifndef B2_TRIANGLE_H
-#define B2_TRIANGLE_H
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.callObjectMethodSync', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwObjCallObjectMethodSyncFunction);
+};
+    
+    
+    {    DISALLOW_COPY_AND_ASSIGN(NwScreenDisplayObserver);
+  };
+    
+    #endif //NW_SRC_API_NW_SCREEN_API_H_
+
+    
+    			image.m_bVerboseOutput = a_bVerboseOutput;
+			image.Encode(a_format, a_eErrMetric, a_fEffort, a_uiJobs, a_uiMaxJobs);
     
     namespace Etc
 {
-	// ----------------------------------------------------------------------------------------------------
-	// C-style inteface to the encoder
-	//
-	void Encode(float *a_pafSourceRGBA,
-				unsigned int a_uiSourceWidth, 
-				unsigned int a_uiSourceHeight,
-				Image::Format a_format,
-				ErrorMetric a_eErrMetric,
-				float a_fEffort,
-				unsigned int a_uiJobs,
-				unsigned int a_uiMaxJobs,
-				unsigned char **a_ppaucEncodingBits,
-				unsigned int *a_puiEncodingBitsBytes,
-				unsigned int *a_puiExtendedWidth,
-				unsigned int *a_puiExtendedHeight, 
-				int *a_piEncodingTime_ms, bool a_bVerboseOutput)
-	{
-    }
+	class Block4x4EncodingBits;
     }
     
-    	// ----------------------------------------------------------------------------------------------------
-	// calculate the error for the block by summing the pixel errors
-	//
-	void Block4x4Encoding::CalcBlockError(void)
-	{
-		m_fError = 0.0f;
+    		case Image::Format::RGB8A1:
+		case Image::Format::SRGB8A1:
+			switch (m_sourcealphamix)
+			{
+			case SourceAlphaMix::OPAQUE:
+				m_pencoding = new Block4x4Encoding_RGB8A1_Opaque;
+				break;
     }
     
-    		TryDifferential(m_boolMostLikelyFlip, 1, -2, 0);
-		TryDifferential(m_boolMostLikelyFlip, 1, 2, 0);
-		TryDifferential(m_boolMostLikelyFlip, 1, 0, 2);
-		TryDifferential(m_boolMostLikelyFlip, 1, 0, -2);
+    #define C_SUBFROM( res , a)\
+    do {(res).r = ADD32((res).r,(a).r);  (res).i = SUB32((res).i,(a).i); \
+    }while(0)
     
-    /* a32 + (b32 * (opus_int32)((opus_int16)(c32))) >> 16 output have to be 32bit int */
-#undef silk_SMLAWB
-#define silk_SMLAWB(a, b, c) ((a) + silk_SMULWB(b, c))
+    #define MULT16_16_P13(a,b) (SHR(ADD32(4096,MULT16_16((a),(b))),13))
+#define MULT16_16_P14(a,b) (SHR(ADD32(8192,MULT16_16((a),(b))),14))
+#define MULT16_16_P15(a,b) (SHR(ADD32(16384,MULT16_16((a),(b))),15))
     
-    AuthPropertyIterator::AuthPropertyIterator()
-    : property_(nullptr), ctx_(nullptr), index_(0), name_(nullptr) {}
+    /* a32 + (opus_int32)((opus_int16)(b32)) * (c32 >> 16) */
+#undef silk_SMLABT
+static OPUS_INLINE opus_int32 silk_SMLABT_armv5e(opus_int32 a, opus_int32 b,
+ opus_int32 c)
+{
+  int res;
+  __asm__(
+      '#silk_SMLABT\n\t'
+      'smlabt %0, %1, %2, %3\n\t'
+      : '=r'(res)
+      : 'r'(b), 'r'(c), 'r'(a)
+  );
+  return res;
+}
+#define silk_SMLABT(a, b, c) (silk_SMLABT_armv5e(a, b, c))
     
-    Status ProtoServerReflection::ListService(ServerContext* context,
-                                          ListServiceResponse* response) {
-  if (services_ == nullptr) {
-    return Status(StatusCode::NOT_FOUND, 'Services not found.');
+    void RemoveTransliterator::handleTransliterate(Replaceable& text, UTransPosition& index,
+                                               UBool /*isIncremental*/) const {
+    // Our caller (filteredTransliterate) has already narrowed us
+    // to an unfiltered run.  Delete it.
+    UnicodeString empty;
+    text.handleReplaceBetween(index.start, index.limit, empty);
+    int32_t len = index.limit - index.start;
+    index.contextLimit -= len;
+    index.limit -= len;
+}
+U_NAMESPACE_END
+    
+    
+    {    BreakIterator *get() const { return ptr; }
+    BreakIterator *operator->() const { return ptr; }
+    BreakIterator &operator*() const { return *ptr; }
+private:
+    BreakIterator *ptr;
+    SharedBreakIterator(const SharedBreakIterator &);
+    SharedBreakIterator &operator=(const SharedBreakIterator &);
+};
+    
+    
+    {    /**
+     * Formats positiveValue using the given range of digit counts.
+     * Always uses standard digits '0' through '9'. Formatted value is
+     * left padded with '0' as necessary to achieve minimum digit count.
+     * Does not produce any grouping separators or trailing decimal point.
+     * Calling format to format a value with a particular digit count range
+     * when canFormat indicates that the same value and digit count range
+     * cannot be formatted results in undefined behavior.
+     *
+     * @param positiveValue the value to format
+     * @param range the acceptable range of digit counts.
+     */
+    static UnicodeString &format(
+            int32_t positiveValue,
+            const IntDigitCountRange &range,
+            UnicodeString &appendTo);
+    
+};
+    
+        /**
+     * @param keyword for example 'few' or 'other'
+     * @return the index of the plural form corresponding to the keyword, or a negative value
+     */
+    static int32_t indexOrNegativeFromString(const UnicodeString &keyword);
+    
+    
+void FreeListCategory::RepairFreeList(Heap* heap) {
+  FreeSpace n = top();
+  while (!n.is_null()) {
+    MapWordSlot map_location = n.map_slot();
+    // We can't use .is_null() here because *map_location returns an
+    // Object (for which 'is null' is not defined, as it would be
+    // indistinguishable from 'is Smi(0)'). Only HeapObject has 'is_null()'.
+    if (map_location.contains_value(kNullAddress)) {
+      map_location.store(ReadOnlyRoots(heap).free_space_map());
+    } else {
+      DCHECK(map_location.contains_value(
+          ReadOnlyRoots(heap).free_space_map().ptr()));
+    }
+    n = n->next();
   }
-  for (auto it = services_->begin(); it != services_->end(); ++it) {
-    ServiceResponse* service_response = response->add_service();
-    service_response->set_name(*it);
-  }
-  return Status::OK;
 }
     
-    namespace grpc {
-namespace {
-    }
+      inline Address UnmarkDeletionAddress(Address address) {
+    return address & ~kDeletionTag;
+  }
+    
+    bool Sweeper::SweepSpaceIncrementallyFromTask(AllocationSpace identity) {
+  if (Page* page = GetSweepingPageSafe(identity)) {
+    ParallelSweepPage(page, identity);
+  }
+  return sweeping_list_[GetSweepSpaceIndex(identity)].empty();
+}
+    
+    void Assembler::fild_d(Operand adr) {
+  EnsureSpace ensure_space(this);
+  EMIT(0xDF);
+  emit_operand(ebp, adr);
+}
+    
+    static const ByteMnemonic zero_operands_instr[] = {
+  {0xC3, 'ret', UNSET_OP_ORDER},
+  {0xC9, 'leave', UNSET_OP_ORDER},
+  {0x90, 'nop', UNSET_OP_ORDER},
+  {0xF4, 'hlt', UNSET_OP_ORDER},
+  {0xCC, 'int3', UNSET_OP_ORDER},
+  {0x60, 'pushad', UNSET_OP_ORDER},
+  {0x61, 'popad', UNSET_OP_ORDER},
+  {0x9C, 'pushfd', UNSET_OP_ORDER},
+  {0x9D, 'popfd', UNSET_OP_ORDER},
+  {0x9E, 'sahf', UNSET_OP_ORDER},
+  {0x99, 'cdq', UNSET_OP_ORDER},
+  {0x9B, 'fwait', UNSET_OP_ORDER},
+  {0xFC, 'cld', UNSET_OP_ORDER},
+  {0xAB, 'stos', UNSET_OP_ORDER},
+  {-1, '', UNSET_OP_ORDER}
+};
+    
+    // The ExtraArg1Register not part of the real JS calling convention and is
+// mostly there to simplify consistent interface descriptor definitions across
+// platforms. Note that on ia32 it aliases kJavaScriptCallCodeStartRegister.
+constexpr Register kJavaScriptCallExtraArg1Register = ecx;
+    
+    #define SSSE3_INSTRUCTION_LIST(V) \
+  V(phaddd, 66, 0F, 38, 02)       \
+  V(phaddw, 66, 0F, 38, 01)       \
+  V(pshufb, 66, 0F, 38, 00)       \
+  V(psignb, 66, 0F, 38, 08)       \
+  V(psignw, 66, 0F, 38, 09)       \
+  V(psignd, 66, 0F, 38, 0A)
+    
+    
+    {
+    {    BIND(&end);
+    return var_result.value();
+  };
+  auto floatFunction = [=](Node* lhs, Node* rhs) {
+    return Float64Sub(lhs, rhs);
+  };
+  return Generate_BinaryOperationWithFeedback(
+      context, lhs, rhs, slot_id, feedback_vector, smiFunction, floatFunction,
+      Operation::kSubtract, rhs_is_smi);
+}
+    
+    class RenderActionAddEvent : public RenderAction {
+ public:
+  explicit RenderActionAddEvent(const std::string &page_id,
+                                const std::string &ref,
+                                const std::string &event);
     }
     
-    #include <utility>
+    class RenderActionCreateFinish : public RenderAction {
+ public:
+  explicit RenderActionCreateFinish(const std::string &page_id);
+    }
     
-    using std::chrono::duration_cast;
-using std::chrono::high_resolution_clock;
-using std::chrono::nanoseconds;
-using std::chrono::seconds;
-using std::chrono::system_clock;
+        /**
+     *  unicode to utf8 convertor with zero dependency inspired by java sdk character source
+     * */
+    void utf16_convert_to_utf8_string(uint16_t *utf16, int length, std::string& utf8);
+    void utf16_convert_to_utf8_quote_string(uint16_t *utf16, int length, std::string& utf8);
+    /**
+     * return byte count in utf8, buffer size should can contains convert values
+     * */
+    int utf16_convert_to_utf8_cstr(uint16_t *utf16, int length, char* buffer);
+    int utf16_convert_to_utf8_quote_cstr(uint16_t *utf16, int length, char* buffer);
