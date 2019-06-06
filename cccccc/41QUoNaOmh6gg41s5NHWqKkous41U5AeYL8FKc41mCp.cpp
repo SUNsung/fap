304ad5eff73a3d901f39375953de4ac8e9445897
SUNsung <weatherius@gmail.com>
2019-06-06 19:46:04 +0300

    Rewrite [41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp] File in: 2019-06-06 19:46:03

--- cccccc/41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp
+++ cccccc/41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp
@@ -1,384 +1,91 @@
 
         
-        
-    {  return false;
-}
+        #include <google/protobuf/compiler/command_line_interface.h>
+#include <google/protobuf/compiler/csharp/csharp_helpers.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/io/printer.h>
     
-     private:
-  ~UvTaskRunner() override;
-  static void OnTimeout(uv_timer_t* timer);
-  static void OnClose(uv_handle_t* handle);
+    #include <ostream>
+#include <stdio.h>
+#include <string>
+#include <utility>
     
-     protected:
-  Screen(v8::Isolate* isolate, display::Screen* screen);
-  ~Screen() override;
-    
-    class AtomQuotaPermissionContext : public content::QuotaPermissionContext {
- public:
-  typedef content::QuotaPermissionContext::QuotaPermissionResponse response;
+      virtual bool Generate(const FileDescriptor* file,
+                        const string& parameter,
+                        GeneratorContext* context,
+                        string* error) const {
+    FileDescriptorProto new_file;
+    file->CopyTo(&new_file);
+    SchemaGroupStripper::StripFile(file, &new_file);
     }
     
-    void AutoUpdater::SetDelegate(Delegate* delegate) {
-  delegate_ = delegate;
-}
-    
-      // There is a new update which has been downloaded.
-  virtual void OnUpdateDownloaded(const std::string& release_notes,
-                                  const std::string& release_name,
-                                  const base::Time& release_date,
-                                  const std::string& update_url) {}
-    
-    #ifndef ATOM_BROWSER_RELAUNCHER_H_
-#define ATOM_BROWSER_RELAUNCHER_H_
-    
-      // PID 1 identifies init. launchd, that is. launchd never starts the
-  // relauncher process directly, having this parent_pid means that the parent
-  // already exited and launchd 'inherited' the relauncher as its child.
-  // There's no reason to synchronize with launchd.
-  if (parent_pid == 1) {
-    LOG(ERROR) << 'unexpected parent_pid';
-    return;
-  }
-    
-    
-    {}  // namespace atom
-    
-    #ifndef ATOM_BROWSER_UI_COCOA_VIEWS_DELEGATE_MAC_H_
-#define ATOM_BROWSER_UI_COCOA_VIEWS_DELEGATE_MAC_H_
-    
-    #ifndef DIRECTIONAL_PREPOSITION
-#  define DIRECTIONAL_PREPOSITION(Word) PREPOSITION(Word)
-#endif
-    
-    from GYBUnicodeDataUtils import GraphemeClusterBreakPropertyTable, get_extended_grapheme_cluster_rules_matrix
-    
-    /// The list of known CF types.  We use 'constexpr' to verify that this is
-/// emitted as a constant.  Note that this is expected to be sorted in
-/// quasi-lexicographic order.
-static constexpr const llvm::StringLiteral KnownCFTypes[] = {
-#define CF_TYPE(NAME) #NAME,
-#define NON_CF_TYPE(NAME)
-#include 'SortedCFDatabase.def'
-};
-const size_t NumKnownCFTypes = sizeof(KnownCFTypes) / sizeof(*KnownCFTypes);
-    
-    static void printNode(DemanglerPrinter &Out, const Node *node, unsigned depth) {
-  // Indent two spaces per depth.
-  for (unsigned i = 0; i < depth * 2; ++i) {
-    Out << ' ';
-  }
-  if (!node) {
-    Out << '<<NULL>>';
-    return;
-  }
-  Out << 'kind=' << getNodeKindString(node->getKind());
-  if (node->hasText()) {
-    Out << ', text=\'' << node->getText() << '\'';
-  }
-  if (node->hasIndex()) {
-    Out << ', index=' << node->getIndex();
-  }
-  Out << '\n';
-  for (auto &child : *node) {
-    printNode(Out, child, depth + 1);
-  }
-}
-    
-    class LLVM_LIBRARY_VISIBILITY GenericUnix : public ToolChain {
-protected:
-  InvocationInfo constructInvocation(const InterpretJobAction &job,
-                                     const JobContext &context) const override;
-  InvocationInfo constructInvocation(const AutolinkExtractJobAction &job,
-                                     const JobContext &context) const override;
-    }
-    
-    struct OSResults {
-  OSResults() : unicharset(nullptr) {
-    for (int i = 0; i < 4; ++i) {
-      for (int j = 0; j < kMaxNumberOfScripts; ++j)
-        scripts_na[i][j] = 0;
-      orientations[i] = 0;
+      {
+    // Write the new address book back to disk.
+    fstream output(argv[1], ios::out | ios::trunc | ios::binary);
+    if (!address_book.SerializeToOstream(&output)) {
+      cerr << 'Failed to write address book.' << endl;
+      return -1;
     }
   }
-  void update_best_orientation();
-  // Set the estimate of the orientation to the given id.
-  void set_best_orientation(int orientation_id);
-  // Update/Compute the best estimate of the script assuming the given
-  // orientation id.
-  void update_best_script(int orientation_id);
-  // Return the index of the script with the highest score for this orientation.
-  TESS_API int get_best_script(int orientation_id) const;
-  // Accumulate scores with given OSResults instance and update the best script.
-  void accumulate(const OSResults& osr);
-    }
     
-    #include 'tesseractclass.h'
-#ifdef _OPENMP
-#include <omp.h>
-#endif  // _OPENMP
-    
-    
-    { private:
-  // The collection of images to put in the PDF.
-  Pixa* pixa_;
-  // The fonts used to draw text captions.
-  L_Bmf* fonts_;
-};
-    
-      /* Reject modes generated after QUALITY_ACCEPT but before MINIMAL_REJ accep*/
-  R_DOC_REJ,    // TEMP Document rejection
-  R_BLOCK_REJ,  // TEMP Block rejection
-  R_ROW_REJ,    // TEMP Row rejection
-  R_UNLV_REJ,   // TEMP ~ turned to - or ^ turned to space
-    
-    #endif  // THIRD_PARTY_TESSERACT_CCUTIL_DOUBLEPTR_H_
+    #endif  // GOOGLE_PROTOBUF_COMPILER_OBJECTIVEC_ENUM_H__
 
     
-      // Main worker method that retrieves the next number in the sequence.
-  // Returns kInvalidVal if called more than N times after object initialization
-  int GetVal() {
-    const int kInvalidVal = -1;
-    const int kMaxNaturalNumberValue = 1 << num_bits_;
-    if (next_num_ >= kMaxNaturalNumberValue)
-      return kInvalidVal;
-    int n = next_num_;
-    }
-    
-    // EXPECT_DEATH_IF_SUPPORTED(statement, regex) and
-// ASSERT_DEATH_IF_SUPPORTED(statement, regex) expand to real death tests if
-// death tests are supported; otherwise they just issue a warning.  This is
-// useful when you are combining death test assertions with normal test
-// assertions in one test.
-#if GTEST_HAS_DEATH_TEST
-# define EXPECT_DEATH_IF_SUPPORTED(statement, regex) \
-    EXPECT_DEATH(statement, regex)
-# define ASSERT_DEATH_IF_SUPPORTED(statement, regex) \
-    ASSERT_DEATH(statement, regex)
-#else
-# define EXPECT_DEATH_IF_SUPPORTED(statement, regex) \
-    GTEST_UNSUPPORTED_DEATH_TEST_(statement, regex, )
-# define ASSERT_DEATH_IF_SUPPORTED(statement, regex) \
-    GTEST_UNSUPPORTED_DEATH_TEST_(statement, regex, return)
-#endif
-    
-    template <typename T>
-class TypeWithoutFormatter<T, kProtobuf> {
- public:
-  static void PrintValue(const T& value, ::std::ostream* os) {
-    const ::testing::internal::string short_str = value.ShortDebugString();
-    const ::testing::internal::string pretty_str =
-        short_str.length() <= kProtobufOneLinerMaxLength ?
-        short_str : ('\n' + value.DebugString());
-    *os << ('<' + pretty_str + '>');
-  }
-};
-    
-      // The c'tor sets this object as the test part result reporter used
-  // by Google Test.  The 'result' parameter specifies where to report the
-  // results. This reporter will only catch failures generated in the current
-  // thread. DEPRECATED
-  explicit ScopedFakeTestPartResultReporter(TestPartResultArray* result);
-    
-    // First, define a fixture class template.  It should be parameterized
-// by a type.  Remember to derive it from testing::Test.
-template <typename T>
-class FooTest : public testing::Test {
- public:
-  ...
-  typedef std::list<T> List;
-  static T shared_;
-  T value_;
-};
-    
-      tuple& operator=(const tuple& t) { return CopyFrom(t); }
-    
-    #endif  // GTEST_SAMPLES_PRIME_TABLES_H_
-
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
     
     
-    {  // <TechnicalDetails>
-  //
-  // EXPECT_EQ(expected, actual) is the same as
-  //
-  //   EXPECT_TRUE((expected) == (actual))
-  //
-  // except that it will print both the expected value and the actual
-  // value when the assertion fails.  This is very helpful for
-  // debugging.  Therefore in this case EXPECT_EQ is preferred.
-  //
-  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
-  // and is thus more general.
-  //
-  // </TechnicalDetails>
-}
-    
-    // Clones a 0-terminated C string, allocating memory using new.
-const char* MyString::CloneCString(const char* a_c_string) {
-  if (a_c_string == NULL) return NULL;
-    }
-    
-      // Asserts that s.c_string() returns NULL.
-  //
-  // <TechnicalDetails>
-  //
-  // If we write NULL instead of
-  //
-  //   static_cast<const char *>(NULL)
-  //
-  // in this assertion, it will generate a warning on gcc 3.4.  The
-  // reason is that EXPECT_EQ needs to know the types of its
-  // arguments in order to print them when it fails.  Since NULL is
-  // #defined as 0, the compiler will use the formatter function for
-  // int to print it.  However, gcc thinks that NULL should be used as
-  // a pointer, not an int, and therefore complains.
-  //
-  // The root of the problem is C++'s lack of distinction between the
-  // integer number 0 and the null pointer constant.  Unfortunately,
-  // we have to live with this fact.
-  //
-  // </TechnicalDetails>
-  EXPECT_STREQ(NULL, s.c_string());
-    
-    class CensusChannelData : public ChannelData {
- public:
-  grpc_error* Init(grpc_channel_element* elem,
-                   grpc_channel_element_args* args) override;
-};
-    
-    void FilterTrailingMetadata(grpc_metadata_batch* b, uint64_t* elapsed_time) {
-  if (b->idx.named.grpc_server_stats_bin != nullptr) {
-    ServerStatsDeserialize(
-        reinterpret_cast<const char*>(GRPC_SLICE_START_PTR(
-            GRPC_MDVALUE(b->idx.named.grpc_server_stats_bin->md))),
-        GRPC_SLICE_LENGTH(GRPC_MDVALUE(b->idx.named.grpc_server_stats_bin->md)),
-        elapsed_time);
-    grpc_metadata_batch_remove(b, b->idx.named.grpc_server_stats_bin);
-  }
-}
-    
-      static void OnDoneSendInitialMetadataCb(void* user_data, grpc_error* error);
-    
-    namespace grpc {
-namespace load_reporter {
-    }
-    }
-    
-    using grpc::core::Bucket;
-using grpc::core::Histogram;
-using grpc::core::Metric;
-using grpc::core::Stats;
-    
-    void CoreStatsToProto(const grpc_stats_data& core, grpc::core::Stats* proto);
-void ProtoToCoreStats(const grpc::core::Stats& proto, grpc_stats_data* core);
-    
-    
-    {}  // namespace leveldb
-    
-    #include 'leveldb/export.h'
-#include 'leveldb/iterator.h'
-    
-    #ifndef STORAGE_LEVELDB_INCLUDE_WRITE_BATCH_H_
-#define STORAGE_LEVELDB_INCLUDE_WRITE_BATCH_H_
-    
-    
-    {}  // namespace leveldb
-    
-     private:
-  void GenerateFilter();
-    
-    char* Arena::AllocateAligned(size_t bytes) {
-  const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
-  static_assert((align & (align - 1)) == 0,
-                'Pointer size should be a power of 2');
-  size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1);
-  size_t slop = (current_mod == 0 ? 0 : align - current_mod);
-  size_t needed = bytes + slop;
-  char* result;
-  if (needed <= alloc_bytes_remaining_) {
-    result = alloc_ptr_ + slop;
-    alloc_ptr_ += needed;
-    alloc_bytes_remaining_ -= needed;
-  } else {
-    // AllocateFallback always returned aligned memory
-    result = AllocateFallback(bytes);
-  }
-  assert((reinterpret_cast<uintptr_t>(result) & (align - 1)) == 0);
-  return result;
-}
-    
-    #endif  // STORAGE_LEVELDB_UTIL_ARENA_H_
-
-    
-    TEST(CacheTest, Prune) {
-  Insert(1, 100);
-  Insert(2, 200);
-    }
-    
-      // Get file to use for testing.
-  std::string test_dir;
-  ASSERT_OK(env_->GetTestDirectory(&test_dir));
-  std::string test_file_name = test_dir + '/open_on_read.txt';
-  WritableFile* writable_file;
-  ASSERT_OK(env_->NewWritableFile(test_file_name, &writable_file));
-    
-    // Helper class to limit resource usage to avoid exhaustion.
-// Currently used to limit read-only file descriptors and mmap file usage
-// so that we do not run out of file descriptors or virtual memory, or run into
-// kernel performance problems for very large databases.
-class Limiter {
- public:
-  // Limit maximum number of resources to |max_acquires|.
-  Limiter(int max_acquires) : acquires_allowed_(max_acquires) {}
-    }
-    
-    #include <vector>
-    
-    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
-    
-    void ClusterGeneralInfo701::Parse(const std::uint8_t* bytes, int32_t length,
-                                  ContiRadar* conti_radar) const {
-  auto obs = conti_radar->add_contiobs();
-  obs->set_clusterortrack(true);
-  obs->set_obstacle_id(obstacle_id(bytes, length));
-  obs->set_longitude_dist(longitude_dist(bytes, length));
-  obs->set_lateral_dist(lateral_dist(bytes, length));
-  obs->set_longitude_vel(longitude_vel(bytes, length));
-  obs->set_lateral_vel(lateral_vel(bytes, length));
-  obs->set_rcs(rcs(bytes, length));
-  obs->set_dynprop(dynprop(bytes, length));
-  double timestamp = apollo::common::time::Clock::NowInSeconds();
-  auto header = obs->mutable_header();
-  header->CopyFrom(conti_radar->header());
-  header->set_timestamp_sec(timestamp);
-}
-    
-    
-    {  switch (x) {
-    case 0x0:
-      return OUTPUT_TYPE_NONE;
-    case 0x1:
-      return OUTPUT_TYPE_OBJECTS;
-    case 0x2:
-      return OUTPUT_TYPE_CLUSTERS;
-    default:
-      return OUTPUT_TYPE_ERROR;
-  }
-}
-    
-    namespace apollo {
-namespace localization {
-namespace msf {
-    }
-    }
-    }
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    
-    {  double ret = x * 0.001000;
+    {  Byte t1(bytes + 5);
+  uint32_t t = t1.get_byte(6, 2);
+  x <<= 2;
+  x |= t;
+  double ret = x * CLUSTER_VREL_RES + CLUSTER_VREL_LONG_MIN;
   return ret;
+}
+    
+    TEST_F(SpeedLimitTest, GetSpeedLimitByS) {
+  EXPECT_EQ(speed_limit_.speed_limit_points().size(), 100);
+  double s = 0.0;
+  const double ds = 0.01;
+  while (s < 99.0) {
+    double v_limit = speed_limit_.GetSpeedLimitByS(s);
+    }
+    }
+    
+      MatrixXd mat_golden(20, 10);
+  // clang-format off
+  mat_golden <<
+   -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0, -1,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  1,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0, -1,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  1,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0, -1,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  1,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0, -1,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  1,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0, -1,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  1,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0, -1,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  1,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0, -1,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  1,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0, -1,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  1,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0, -1,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  1;
+  // clang-format on
+  EXPECT_EQ(mat, mat_golden);
+    
+      kernel.AddSecondOrderDerivativeMatrix(init_derivative, 1.0);
+    
+    void SplineSegKernel::CalculateDerivative(const uint32_t num_params) {
+  kernel_derivative_ = Eigen::MatrixXd::Zero(num_params, num_params);
+  for (int r = 1; r < kernel_derivative_.rows(); ++r) {
+    for (int c = 1; c < kernel_derivative_.cols(); ++c) {
+      kernel_derivative_(r, c) = r * c / (r + c - 1.0);
+    }
+  }
 }
     
     namespace apollo {
@@ -388,72 +95,24 @@ namespace gem {
     }
     }
     
-    // config detail: {'name': 'longitude_seconds', 'offset': 0.0, 'precision': 1.0,
-// 'len': 8, 'is_signed_var': True, 'physical_range': '[-128|127]', 'bit': 47,
-// 'type': 'int', 'order': 'motorola', 'physical_unit': 'sec'}
-int Latlonheadingrpt82::longitude_seconds(const std::uint8_t* bytes,
-                                          int32_t length) const {
-  Byte t0(bytes + 5);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-    #include '2d/CCGrid.h'
-#include '2d/CCNodeGrid.h'
-#include 'base/CCDirector.h'
-    
-    
-    {private:
-    CC_DISALLOW_COPY_AND_ASSIGN(FlipX);
-};
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __CCACTIONTWEEN_H__
-#define __CCACTIONTWEEN_H__
-    
-    
-    {    /**  A AnimationFrameDisplayedNotification notification will be broadcast when the frame is displayed with this dictionary as UserInfo. If UserInfo is nil, then no notification will be broadcast. */
-    ValueMap _userInfo;
-    
-private:
-    CC_DISALLOW_COPY_AND_ASSIGN(AnimationFrame);
-};
+      Brakerpt6c brake;
+  brake.Parse(bytes, length, &chassis_detail);
     
     
     {
-    {        AnimationCache::getInstance()->addAnimation(animation, name);
-    }
+    {
+    {
+    {  Headlight_rpt_77::Commanded_valueType ret =
+      static_cast<Headlight_rpt_77::Commanded_valueType>(x);
+  return ret;
 }
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+}  // namespace gem
+}  // namespace canbus
+}  // namespace apollo
+
     
     
-#include '2d/CCClippingRectangleNode.h'
-#include 'base/CCDirector.h'
-#include 'renderer/CCRenderer.h'
-#include 'math/Vec2.h'
-#include 'platform/CCGLView.h'
-    
-    #include '2d/CCNode.h'
-#include 'renderer/CCCustomCommand.h'
-#include 'platform/CCGL.h'
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+    {  Horn_rpt_79::Output_valueType ret =
+      static_cast<Horn_rpt_79::Output_valueType>(x);
+  return ret;
+}
