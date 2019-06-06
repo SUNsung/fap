7a20fc0f7de45b764e7c8a183d610a48d1db6f25
SUNsung <weatherius@gmail.com>
2019-06-06 16:34:17 +0300

    Rewrite [26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py] File in: 2019-06-06 16:34:17

--- pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
+++ pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
@@ -1,182 +1,97 @@
 
         
-          def predict_with_score(self, session, inputs):
-    '''Predict the classification of the test set.
+            def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
     
-              eval_feed = {model.inputs: x, model.targets: y, model.present: p}
+        youngest_snapshot = max(snapshots, key=_get_snapshot_starttime)
     
-    import tensorflow as tf
+        results = dict(ansible_facts={'lambda_facts': {'function': all_facts}}, changed=False)
     
-        rnn_inputs = tf.nn.embedding_lookup(embedding, sequence)
-    rnn_inputs = tf.unstack(rnn_inputs, axis=1)
+        gateway = vca.get_gateway(vdc_name, gateway_name)
+    if not gateway:
+        module.fail_json(msg='Not able to find the gateway %s, please check '
+                             'the gateway_name param' % gateway_name)
     
+            if privilege is not None:
+            changed = client.modify_if_diff(name, ipa_role.get('memberof_privilege', []), privilege,
+                                            client.role_add_privilege,
+                                            client.role_remove_privilege) or changed
+        if service is not None:
+            changed = client.modify_if_diff(name, ipa_role.get('member_service', []), service,
+                                            client.role_add_service,
+                                            client.role_remove_service) or changed
+        if user is not None:
+            changed = client.modify_if_diff(name, ipa_role.get('member_user', []), user,
+                                            client.role_add_user,
+                                            client.role_remove_user) or changed
     
-class BtreeGistExtension(CreateExtension):
-    
-        def flush(self):
-        '''
-        Remove the current session data from the database and regenerate the
-        key.
-        '''
-        self.clear()
-        self.delete(self.session_key)
-        self._session_key = None
-
-    
-        def css_classes(self, extra_classes=None):
-        '''
-        Return a string of space-separated CSS classes for this field.
-        '''
-        if hasattr(extra_classes, 'split'):
-            extra_classes = extra_classes.split()
-        extra_classes = set(extra_classes or [])
-        if self.errors and hasattr(self.form, 'error_css_class'):
-            extra_classes.add(self.form.error_css_class)
-        if self.field.required and hasattr(self.form, 'required_css_class'):
-            extra_classes.add(self.form.required_css_class)
-        return ' '.join(extra_classes)
-    
-        return (x_train, y_train), (x_test, y_test)
-
-    
-        # Returns
-        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+        if module.params['repo']:
+        params['deploy[scm_repository]'] = module.params['repo']
     
     
-allobj = [losses.mean_squared_error,
-          losses.mean_absolute_error,
-          losses.mean_absolute_percentage_error,
-          losses.mean_squared_logarithmic_error,
-          losses.squared_hinge,
-          losses.hinge,
-          losses.categorical_crossentropy,
-          losses.binary_crossentropy,
-          losses.kullback_leibler_divergence,
-          losses.poisson,
-          losses.cosine_proximity,
-          losses.logcosh,
-          losses.categorical_hinge]
+def do_send_request(module, url, params, key):
+    data = json.dumps(params)
+    headers = {
+        'Content-Type': 'application/json',
+        'x-stackdriver-apikey': key
+    }
+    response, info = fetch_url(module, url, headers=headers, data=data, method='POST')
+    if info['status'] != 200:
+        module.fail_json(msg='Unable to send msg: %s' % info['msg'])
     
-        To save the multi-gpu model, use `.save(fname)` or `.save_weights(fname)`
-    with the template model (the argument you passed to `multi_gpu_model`),
-    rather than the model returned by `multi_gpu_model`.
-    '''
-    if K.backend() != 'tensorflow':
-        raise ValueError('`multi_gpu_model` is only available '
-                         'with the TensorFlow backend.')
-    
-    learning_rate = 1e-6
-clip_norm = 1.0
-    
-    # input image dimensions
-img_rows, img_cols = 28, 28
-# number of convolutional filters to use
-filters = 32
-# size of pooling area for max pooling
-pool_size = 2
-# convolution kernel size
-kernel_size = 3
+            if not is_namespace:
+            tests = list(self._find_tests(start_dir, pattern))
+        return self.suiteClass(tests)
     
     
-def create_network(n_dense=6,
-                   dense_units=16,
-                   activation='selu',
-                   dropout=AlphaDropout,
-                   dropout_rate=0.1,
-                   kernel_initializer='lecun_normal',
-                   optimizer='adam',
-                   num_classes=1,
-                   max_words=max_words):
-    '''Generic function to create a fully-connected neural network.
+# Are two open files really referencing the same file?
+# (Not necessarily the same file descriptor!)
+def sameopenfile(fp1, fp2):
+    '''Test whether two open file objects reference the same file'''
+    s1 = os.fstat(fp1)
+    s2 = os.fstat(fp2)
+    return samestat(s1, s2)
     
     
-def deserialize(name, custom_objects=None):
-    return deserialize_keras_object(name,
-                                    module_objects=globals(),
-                                    custom_objects=custom_objects,
-                                    printable_module_name='loss function')
+def open_text(package: Package,
+              resource: Resource,
+              encoding: str = 'utf-8',
+              errors: str = 'strict') -> TextIO:
+    '''Return a file-like object opened for text reading of the resource.'''
+    resource = _normalize_path(resource)
+    package = _get_package(package)
+    reader = _get_resource_reader(package)
+    if reader is not None:
+        return TextIOWrapper(reader.open_resource(resource), encoding, errors)
+    _check_location(package)
+    absolute_package_path = os.path.abspath(package.__spec__.origin)
+    package_path = os.path.dirname(absolute_package_path)
+    full_path = os.path.join(package_path, resource)
+    try:
+        return open(full_path, mode='r', encoding=encoding, errors=errors)
+    except OSError:
+        # Just assume the loader is a resource loader; all the relevant
+        # importlib.machinery loaders are and an AttributeError for
+        # get_data() will make it clear what is needed from the loader.
+        loader = cast(ResourceLoader, package.__spec__.loader)
+        data = None
+        if hasattr(package.__spec__.loader, 'get_data'):
+            with suppress(OSError):
+                data = loader.get_data(full_path)
+        if data is None:
+            package_name = package.__spec__.name
+            message = '{!r} resource not found in {!r}'.format(
+                resource, package_name)
+            raise FileNotFoundError(message)
+        else:
+            return TextIOWrapper(BytesIO(data), encoding, errors)
     
-            String:                    '#4e9a06',        # class: 's'
-        String.Backtick:           '#4e9a06',        # class: 'sb'
-        String.Char:               '#4e9a06',        # class: 'sc'
-        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
-        String.Double:             '#4e9a06',        # class: 's2'
-        String.Escape:             '#4e9a06',        # class: 'se'
-        String.Heredoc:            '#4e9a06',        # class: 'sh'
-        String.Interpol:           '#4e9a06',        # class: 'si'
-        String.Other:              '#4e9a06',        # class: 'sx'
-        String.Regex:              '#4e9a06',        # class: 'sr'
-        String.Single:             '#4e9a06',        # class: 's1'
-        String.Symbol:             '#4e9a06',        # class: 'ss'
-    
-    This module handles import compatibility issues between Python 2 and
-Python 3.
-'''
-    
-            .. seealso:: itervalues() and iteritems().
-        '''
-        for cookie in iter(self):
-            yield cookie.name
-    
-        with server as (host, port):
-        url = 'http://{}:{}/'.format(host, port)
-        r = requests.get(url, auth=auth)
-        # Verify server succeeded in authenticating.
-        assert r.status_code == 200
-        # Verify Authorization was sent in final request.
-        assert 'Authorization' in r.request.headers
-        assert r.request.headers['Authorization'].startswith('Digest ')
-        # Verify redirect happened as we expected.
-        assert r.history[0].status_code == 302
-        close_server.set()
-    
-        if not content_type:
-        return None
-    
-    
-class BadNonceTest(unittest.TestCase):
-    '''Tests for acme.errors.BadNonce.'''
-    
-    
-class Header(jose.Header):
-    '''ACME-specific JOSE Header. Implements nonce, kid, and url.
-    '''
-    nonce = jose.Field('nonce', omitempty=True, encoder=jose.encode_b64jose)
-    kid = jose.Field('kid', omitempty=True)
-    url = jose.Field('url', omitempty=True)
-    
-        def setUp(self):
-        self.privkey = KEY
-        self.pubkey = self.privkey.public_key()
-        self.nonce = jose.b64encode(b'Nonce')
-        self.url = 'hi'
-        self.kid = 'baaaaa'
-    
-    MANAGED_COMMENT = 'DO NOT REMOVE - Managed by Certbot'
-MANAGED_COMMENT_ID = MANAGED_COMMENT+', VirtualHost id: {0}'
-'''Managed by Certbot comments and the VirtualHost identification template'''
-
-    
-    # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'repoze.sphinx.autointerface',
-]
-    
-    # The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
+    #
+# Function used to calculate result
 #
-# The short X.Y version.
-version = u'0'
-# The full version, including alpha/beta/rc tags.
-release = u'0'
     
-    # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+    con.commit()
