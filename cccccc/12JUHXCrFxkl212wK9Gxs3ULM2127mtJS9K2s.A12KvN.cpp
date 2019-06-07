6ec986c2b9f85beefb603f31dee5bc0cbaff41cd
SUNsung <weatherius@gmail.com>
2019-06-07 10:08:02 +0300

    Rewrite [12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp] File in: 2019-06-07 10:08:02

--- cccccc/12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp
+++ cccccc/12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp
@@ -1,350 +1,217 @@
 
         
-        class Base {
- public:
-  Base(int id,
-       const base::WeakPtr<ObjectManager>& manager,
-       const base::DictionaryValue& option,
-       const std::string& extension_id);
-  virtual ~Base();
+        // Generate param traits log methods.
+#include 'ipc/param_traits_log_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+
+    
+    #include <string>
+    
+        bool Read(ClipboardData& data) {
+      switch(data.type) {
+        case TYPE_TEXT:
+        return ReadText(data);
+        break;
+        case TYPE_HTML:
+        return ReadHTML(data);
+        break;
+        case TYPE_RTF:
+        return ReadRTF(data);
+        break;
+        case TYPE_PNG:
+        case TYPE_JPEG:
+        return ReadImage(data);
+        break;
+        case TYPE_NONE:
+        NOTREACHED();
+        return false;
+      }
+      NOTREACHED();
+      return false;      
     }
     
-    
-    {  int result = 0;
-  RenderThread::Get()->Send(new ShellViewHostMsg_AllocateId(
-      routing_id,
-      &result));
-  return scope.Escape(v8::Integer::New(isolate, result));
-}
-    
-    // Call method of an object in browser.
-// function CallObjectMethod(id, type, method, args);
-v8::Handle<v8::Value> CallObjectMethod(int routing_id,
-                                       int object_id,
-                                       const std::string& type,
-                                       const std::string& method,
-                                       v8::Handle<v8::Value> args);
-    
-    #include 'content/nw/src/api/menu/menu.h'
-    
-    bool NwAppClearCacheFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  content::BrowsingDataRemover* remover = content::BrowserContext::GetBrowsingDataRemover(
-      Profile::FromBrowserContext(context_));
-    }
-    
-    class NwAppClearCacheFunction : public NWSyncExtensionFunction, public content::BrowsingDataRemover::Observer {
+    class NwClipboardClearSyncFunction : public NWSyncExtensionFunction {
  public:
-  NwAppClearCacheFunction();
+  NwClipboardClearSyncFunction();
   bool RunNWSync(base::ListValue* response, std::string* error) override;
-  void OnBrowsingDataRemoverDone() override;
+    }
+    
+    bool NwObjCallObjectMethodSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
     }
     
      protected:
-  ~NwClipboardClearSyncFunction() override;
+  ResponseAction Run() override;
+  ~NwObjCallObjectMethodAsyncFunction() override;
     
-    namespace extensions {
+      // implement nw.Screen.stopMonitor()
+  class NwScreenStopMonitorFunction : public NWSyncExtensionFunction {
+  public:
+    NwScreenStopMonitorFunction();
+    bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-          displayResult->id = gfx_display.id();
-      displayResult->scale_factor = gfx_display.device_scale_factor();
-      displayResult->is_built_in = gfx_display.IsInternal();
-      displayResult->rotation = gfx_display.RotationAsDegree();
-      displayResult->touch_support = (int)gfx_display.touch_support();
     
-    void absDiff(const Size2D &size,
-             const u16 *src0Base, ptrdiff_t src0Stride,
-             const u16 *src1Base, ptrdiff_t src1Stride,
-             u16 *dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, AbsDiff<u16>());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
+    {    // Compare identical level.
+    const Normalizer2Impl &nfcImpl = data->nfcImpl;
+    left += equalPrefixLength;
+    right += equalPrefixLength;
+    if(leftLength > 0) {
+        leftLength -= equalPrefixLength;
+        rightLength -= equalPrefixLength;
+    }
+    if(settings->dontCheckFCD()) {
+        UTF8NFDIterator leftIter(left, leftLength);
+        UTF8NFDIterator rightIter(right, rightLength);
+        return compareNFDIter(nfcImpl, leftIter, rightIter);
+    } else {
+        FCDUTF8NFDIterator leftIter(data, left, leftLength);
+        FCDUTF8NFDIterator rightIter(data, right, rightLength);
+        return compareNFDIter(nfcImpl, leftIter, rightIter);
+    }
 }
     
-            for (; sj < roiw8; sj += 8, syj += 16, dj += 32)
-        {
-            uint8x8x2_t v_y = vld2_u8(srcy + syj);
-            uint8x8x4_t v_dst;
-            v_dst.val[0] = vld1_u8(srcu + sj);
-            v_dst.val[1] = v_y.val[0];
-            v_dst.val[2] = vld1_u8(srcv + sj);
-            v_dst.val[3] = v_y.val[1];
-            vst4_u8(dst + dj, v_dst);
-        }
+    void ScientificNumberFormatter::getPreExponent(
+        const DecimalFormatSymbols &dfs, UnicodeString &preExponent) {
+    preExponent.append(dfs.getConstSymbol(
+            DecimalFormatSymbols::kExponentMultiplicationSymbol));
+    preExponent.append(dfs.getConstSymbol(DecimalFormatSymbols::kOneDigitSymbol));
+    preExponent.append(dfs.getConstSymbol(DecimalFormatSymbols::kZeroDigitSymbol));
+}
     
-        // the others
-    for (size_t i = 1; i < size.height ; ++i)
-    {
-        src = internal::getRowPtr(srcBase, srcStride, i);
-        u32 * prevSum = internal::getRowPtr(sumBase, sumStride, i - 1);
-        sum = internal::getRowPtr(sumBase, sumStride, i);
-    }
+    #if !UCONFIG_NO_BREAK_ITERATION
     
-    
-    {
-    {} }
-    
-    #include 'stdafx.h'
-#include 'CNTKLibrary.h'
-#include 'Utils.h'
-    
-        void Trainer::PrintNodeTiming()
-    {
-        if (m_combinedTrainingFunction)
-        {
-            m_combinedTrainingFunction->PrintNodeTiming();
-        }
-    }
-    
-    #pragma once
-    
-    
-    {        assert(m_dataFields->m_value != nullptr);
-        return m_dataFields->m_value;
-    }
-    
-    // ===================================================================
-// ComputationNetworkFromFile
-// scripting wrapper to construct ComputationNetwork from file (aka 'Load')
-// ===================================================================
-    
-        virtual void /*IComputationNode::*/ BeginForwardProp() override
-    {
-        // We are a leaf, so UpdateFunctionValuesSize() won't be called for us.
-        UpdateFunctionValuesSize();
-        Base::BeginForwardProp();
-    }
-    
-        // get tensor shapes
-    let& shapeA = Input(0)->GetSampleLayout(); // parameters
-    let& shapeB = Input(1)->GetSampleLayout(); // data
-    auto dimsA = shapeA.GetDims();
-    auto dimsB = shapeB.GetDims();
-    
-        if (U_SUCCESS(status)) {
-        reset();
-        while (find()) {
-            appendReplacement(dest, replacement, status);
-            if (U_FAILURE(status)) {
-                break;
-            }
-        }
-        appendTail(dest, status);
-    }
-    
-    static const UChar SELECT_KEYWORD_OTHER[] = {LOW_O, LOW_T, LOW_H, LOW_E, LOW_R, 0};
-    
-    #if !UCONFIG_NO_FORMATTING
-    
-    // SharedBreakIterator encapsulates a shared BreakIterator. Because
-// BreakIterator has mutable semantics, clients must ensure that all uses
-// of a particular shared BreakIterator is protected by the same mutex
-// ensuring that only one thread at a time gets access to that shared
-// BreakIterator. Clients can accomplish this by creating a mutex for all
-// uses of break iterator within a particular class. Then objects of that
-// class may then freely share break iterators among themselves. However,
-// these shared break iterators must never be exposed outside of that class.
-class U_I18N_API SharedBreakIterator : public SharedObject {
-public:
-    SharedBreakIterator(BreakIterator *biToAdopt);
-    virtual ~SharedBreakIterator();
-    }
-    
-    U_NAMESPACE_END
-    
-    class PluralRules;
+    #ifndef __SHARED_NUMBERFORMAT_H__
+#define __SHARED_NUMBERFORMAT_H__
     
     U_NAMESPACE_BEGIN
     
-    uint8_t *CollationKey::reallocate(int32_t newCapacity, int32_t length) {
-    uint8_t *newBytes = static_cast<uint8_t *>(uprv_malloc(newCapacity));
-    if(newBytes == NULL) { return NULL; }
-    if(length > 0) {
-        uprv_memcpy(newBytes, getBytes(), length);
-    }
-    if(fFlagAndLength < 0) { uprv_free(fUnion.fFields.fBytes); }
-    fUnion.fFields.fBytes = newBytes;
-    fUnion.fFields.fCapacity = newCapacity;
-    fFlagAndLength |= 0x80000000;
-    return newBytes;
-}
     
-    #endif  // !UCONFIG_NO_FORMATTING
-
+class SimpleDateFormatStaticSets : public UMemory
+{
+public:
+    SimpleDateFormatStaticSets(UErrorCode &status);
+    ~SimpleDateFormatStaticSets();
+    
+    static void    initSets(UErrorCode *status);
+    static UBool   cleanup();
+    
+    static UnicodeSet *getIgnorables(UDateFormatField fieldIndex);
+    
+private:
+    UnicodeSet *fDateIgnorables;
+    UnicodeSet *fTimeIgnorables;
+    UnicodeSet *fOtherIgnorables;
+};
+    
+    #ifndef __STANDARDPLURAL_H__
+#define __STANDARDPLURAL_H__
     
         /**
-     * @param keyword for example 'few' or 'other'
-     * @return the index of the plural form corresponding to the keyword, or a negative value
+     * Returns a string representation of this replacer.  If the
+     * result of calling this function is passed to the appropriate
+     * parser, typically TransliteratorParser, it will produce another
+     * replacer that is equal to this one.
+     * @param result the string to receive the pattern.  Previous
+     * contents will be deleted.
+     * @param escapeUnprintable if TRUE then convert unprintable
+     * character to their hex escape representations, \\uxxxx or
+     * \\Uxxxxxxxx.  Unprintable characters are defined by
+     * Utility.isUnprintable().
+     * @return a reference to 'result'.
      */
-    static int32_t indexOrNegativeFromString(const UnicodeString &keyword);
+    virtual UnicodeString& toReplacerPattern(UnicodeString& result,
+                                             UBool escapeUnprintable) const;
     
-    void DHTReplaceNodeTask::sendMessage()
-{
-  std::shared_ptr<DHTNode> questionableNode = bucket_->getLRUQuestionableNode();
-  if (!questionableNode) {
-    setFinished(true);
-  }
-  else {
-    getMessageDispatcher()->addMessageToQueue(
-        getMessageFactory()->createPingMessage(questionableNode), timeout_,
-        make_unique<DHTPingReplyMessageCallback<DHTReplaceNodeTask>>(this));
-  }
-}
-    
-      int numRetry_;
-    
-    void DHTRoutingTableSerializer::serialize(const std::string& filename)
-{
-  A2_LOG_INFO(fmt('Saving DHT routing table to %s.', filename.c_str()));
-  std::string filenameTemp = filename;
-  filenameTemp += '__temp';
-  BufferedFile fp(filenameTemp.c_str(), BufferedFile::WRITE);
-  if (!fp) {
-    throw DL_ABORT_EX(
-        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
-  }
-  char header[8];
-  memset(header, 0, sizeof(header));
-  // magic
-  header[0] = 0xa1u;
-  header[1] = 0xa2u;
-  // format ID
-  header[2] = 0x02u;
-  // version
-  header[6] = 0;
-  header[7] = 0x03u;
-    }
-    
-        uint16_t port;
-    auto connection = make_unique<DHTConnectionImpl>(family);
-    {
-      port = e->getBtRegistry()->getUdpPort();
-      const std::string& addr = e->getOption()->get(
-          family == AF_INET ? PREF_DHT_LISTEN_ADDR : PREF_DHT_LISTEN_ADDR6);
-      // If UDP port is already used, use the same port
-      // number. Normally IPv4 port is available, then IPv6 port is
-      // (especially for port >= 1024). We don't loose much by doing
-      // this. We did the same thing in TCP socket. See BtSetup.cc.
-      bool rv;
-      if (port == 0) {
-        auto sgl =
-            util::parseIntSegments(e->getOption()->get(PREF_DHT_LISTEN_PORT));
-        sgl.normalize();
-        rv = connection->bind(port, addr, sgl);
-      }
-      else {
-        rv = connection->bind(port, addr);
-      }
-      if (!rv) {
-        throw DL_ABORT_EX('Error occurred while binding UDP port for DHT');
-      }
-      localNode->setPort(port);
-    }
-    A2_LOG_DEBUG(fmt('Initialized local node ID=%s',
-                     util::toHex(localNode->getID(), DHT_ID_LENGTH).c_str()));
-    auto tracker = std::make_shared<DHTMessageTracker>();
-    auto routingTable = make_unique<DHTRoutingTable>(localNode);
-    auto factory = make_unique<DHTMessageFactoryImpl>(family);
-    auto dispatcher = make_unique<DHTMessageDispatcherImpl>(tracker);
-    auto receiver = make_unique<DHTMessageReceiver>(tracker);
-    auto taskQueue = make_unique<DHTTaskQueueImpl>();
-    auto taskFactory = make_unique<DHTTaskFactoryImpl>();
-    auto peerAnnounceStorage = make_unique<DHTPeerAnnounceStorage>();
-    auto tokenTracker = make_unique<DHTTokenTracker>();
-    // For now, UDPTrackerClient was enabled along with DHT
-    auto udpTrackerClient = std::make_shared<UDPTrackerClient>();
-    const auto messageTimeout =
-        e->getOption()->getAsInt(PREF_DHT_MESSAGE_TIMEOUT);
-    // wiring up
-    tracker->setRoutingTable(routingTable.get());
-    tracker->setMessageFactory(factory.get());
-    
-    #include 'DHTTask.h'
-#include 'Logger.h'
-#include 'LogFactory.h'
-#include 'a2functional.h'
-#include 'fmt.h'
-    
-    public:
-  DHTTaskQueueImpl();
-    
-    DHTUnknownMessage::DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
-                                     const unsigned char* data, size_t length,
-                                     const std::string& ipaddr, uint16_t port)
-    : DHTMessage(localNode, std::shared_ptr<DHTNode>()),
-      length_(length),
-      ipaddr_(ipaddr),
-      port_(port)
-{
-  if (length_ == 0) {
-    data_ = nullptr;
-  }
-  else {
-    data_ = new unsigned char[length];
-    memcpy(data_, data, length);
-  }
-}
-    
-    #include 'DHTMessage.h'
-    
-    
-    {  sender.Update();
-  sender.Stop();
-  EXPECT_FALSE(sender.IsRunning());
-}
-    
-    #include 'gtest/gtest.h'
-    
-    
-    {  int ret = x;
+    vector<detail::BenchmarkResult> resultsFromFile(const std::string& filename) {
+  string content;
+  readFile(filename.c_str(), content);
+  vector<detail::BenchmarkResult> ret;
+  benchmarkResultsFromDynamic(parseJson(content), ret);
   return ret;
 }
     
     
-    {
-    {}  // namespace planning
-}  // namespace apollo
-
+    {  std::string arguments;
+  ASSERT_TRUE(getTracepointArguments(
+      'folly', 'test_static_tracepoint_many_arg_types', 0, arguments));
+  std::array<int, 8> expected{{
+      sizeof(uint32_t),
+      sizeof(uint32_t),
+      sizeof(bool),
+      sizeof(char),
+      sizeof(short),
+      sizeof(long),
+      sizeof(float),
+      sizeof(double),
+  }};
+  checkTracepointArguments(arguments, expected);
+}
     
-    #include 'modules/drivers/canbus/common/byte.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-    @since v0.99.5
-@js cc.animationCache
-*/
-class CC_DLL AnimationCache : public Ref
-{
-public:
-    /**
-     * @js ctor
-     */
-    AnimationCache();
-    /**
-     * @js NA
-     * @lua NA
-     */
-    ~AnimationCache();
-    /** Returns the shared instance of the Animation cache 
-	 @js NA
-	*/
-    static AnimationCache* getInstance();
+    namespace uri_detail {
     }
+    
+      std::shared_ptr<T> get() const {
+    folly::hazptr_local<1> hazptr;
+    auto slots = hazptr[0].get_protected(slots_);
+    if (!slots) {
+      return nullptr;
+    }
+    return (slots->slots_)[AccessSpreader<>::current(kNumSlots)];
+  }
+    
+    
+    {} // namespace
+    
+    void AtFork::unregisterHandler(void* object) {
+  auto& list = AtForkList::instance();
+  std::lock_guard<std::mutex> lg(list.tasksLock);
+  for (auto it = list.tasks.begin(); it != list.tasks.end(); ++it) {
+    if (it->object == object) {
+      list.tasks.erase(it);
+      return;
+    }
+  }
+}
+    
+            /**
+         * Copy constructor.
+         * It performs `fast copy`: For performance purpose, copying a Point<T> or Point<T> or cv::Mat just copies the
+         * reference, it still shares the same internal data.
+         * Modifying the copied element will modify the original one.
+         * Use clone() for a slower but real copy, similarly to cv::Mat and Point<T>.
+         * @param point Point to be copied.
+         */
+        Point<T>(const Point<T>& point);
+    
+    
+    {    return grpc::Status::OK;
+  }
+    
+      // Use the `CreateWeapon` shortcut to create Weapons with all fields set.
+  auto sword = CreateWeapon(builder, weapon_one_name, weapon_one_damage);
+  auto axe = CreateWeapon(builder, weapon_two_name, weapon_two_damage);
+    
+    #ifdef __ANDROID__
+  #include <android/log.h>
+  #define TEST_OUTPUT_LINE(...) \
+      __android_log_print(ANDROID_LOG_INFO, 'FlatBuffers', __VA_ARGS__)
+  #define FLATBUFFERS_NO_FILE_TESTS
+#else
+  #define TEST_OUTPUT_LINE(...) \
+      { printf(__VA_ARGS__); printf('\n'); }
+#endif
+    
+    std::string GenType(const std::string &name) {
+  return '\'type\' : \'' + name + '\'';
+}
+    
+    #include 'namespace_test1_generated.h'
