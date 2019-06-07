fa557016ecf5af35b34a116e69f7658221829b66
SUNsung <weatherius@gmail.com>
2019-06-07 12:16:23 +0300

    Rewrite [10nUcfrV7Rxw10mLMP4kUqmUU10.HyAqkhSSfg10dIX.py] File in: 2019-06-07 12:16:23

--- pyton/10nUcfrV7Rxw10mLMP4kUqmUU10.HyAqkhSSfg10dIX.py
+++ pyton/10nUcfrV7Rxw10mLMP4kUqmUU10.HyAqkhSSfg10dIX.py
@@ -1,150 +1,218 @@
-from inspect import getsource
-import io
-import os
-from os.path import dirname as dirn
-import sys
-    
-        with open(ZSH_COMPLETION_FILE, 'w') as f:
-        f.write(template)
-    
-    # Output file base name for HTML help builder.
-htmlhelp_basename = 'youtube-dldoc'
 
-    
-        def test_cache(self):
-        ydl = FakeYDL({
-            'cachedir': self.test_dir,
-        })
-        c = Cache(ydl)
-        obj = {'x': 1, 'y': ['ä', '\\a', True]}
-        self.assertEqual(c.load('test_cache', 'k.'), None)
-        c.store('test_cache', 'k.', obj)
-        self.assertEqual(c.load('test_cache', 'k2'), None)
-        self.assertFalse(_is_empty(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), obj)
-        self.assertEqual(c.load('test_cache', 'y'), None)
-        self.assertEqual(c.load('test_cache2', 'k.'), None)
-        c.remove()
-        self.assertFalse(os.path.exists(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), None)
-    
-            self.server_process.terminate()
-        self.server_process.communicate()
+        
+            proc.sendline(u'ehco test')
     
     
-def test_reuters():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = reuters.load_data()
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        assert len(x_train) + len(x_test) == 11228
-        (x_train, y_train), (x_test, y_test) = reuters.load_data(maxlen=10)
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        word_index = reuters.get_word_index()
-        assert isinstance(word_index, dict)
+def plot_feature_times(all_times, batch_size, all_components, data):
+    plt.figure()
+    plot_results(all_components, all_times['pca'], label='PCA')
+    plot_results(all_components, all_times['ipca'],
+                 label='IncrementalPCA, bsize=%i' % batch_size)
+    plt.legend(loc='upper left')
+    plt.suptitle('Algorithm runtime vs. n_components\n \
+                 LFW, size %i x %i' % data.shape)
+    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
+    plt.ylabel('Time (seconds)')
     
-    # Display the 1st 8 corrupted and denoised images
-rows, cols = 10, 30
-num = rows * cols
-imgs = np.concatenate([x_test[:num], x_test_noisy[:num], x_decoded[:num]])
-imgs = imgs.reshape((rows * 3, cols, image_size, image_size))
-imgs = np.vstack(np.split(imgs, rows, axis=1))
-imgs = imgs.reshape((rows * 3, -1, image_size, image_size))
-imgs = np.vstack([np.hstack(i) for i in imgs])
-imgs = (imgs * 255).astype(np.uint8)
-plt.figure()
-plt.axis('off')
-plt.title('Original images: top rows, '
-          'Corrupted Input: middle rows, '
-          'Denoised Input:  third rows')
-plt.imshow(imgs, interpolation='none', cmap='gray')
-Image.fromarray(imgs).save('corrupted_and_denoised.png')
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
 plt.show()
 
     
-    import urllib
+        revision is a git commit reference (hash or name)
     
-        if re.match(r'https?://movie', url):
-        title = match1(html, 'name='description' content='([^']+)')
-        tid = match1(url, 'trailer/(\d+)')
-        real_url = 'https://movie.douban.com/trailer/video_url?tid=%s' % tid
-        type, ext, size = url_info(real_url)
+    For each class of models we make the model complexity vary through the choice
+of relevant model parameters and measure the influence on both computational
+performance (latency) and predictive power (MSE or Hamming Loss).
+'''
     
-    	html = get_html(url)
-	contentid = r1(r'<meta name='contentid' scheme='DMINSTR2' content='([^']+)' />', html)
-	vid = r1(r''demand_ehow_videoid':'([^']+)'', html)
-	assert vid
+    In both examples below, the main result is that the empirical covariance
+estimate, as a non-robust one, is highly influenced by the heterogeneous
+structure of the observations. Although the robust covariance estimate is
+able to focus on the main mode of the data distribution, it sticks to the
+assumption that the data should be Gaussian distributed, yielding some biased
+estimation of the data structure, but yet accurate to some extent.
+The One-Class SVM does not assume any parametric form of the data distribution
+and can therefore model the complex shape of the data much better.
     
-    # [
-# (
-#   regex_match_url,
-#   remote_api_method,
-#   additional_query_parameter_for_method,
-#   parser_for_additional_parameter,
-#   field_where_photourls_are_saved
-# )
-# ]
-url_patterns = [
-    # www.flickr.com/photos/{username|NSID}/sets|albums/{album-id}
-    (
-        pattern_url_photoset,
-        'photosets.getPhotos',
-        'photoset_id',
-        get_photoset_id,
-        'photoset'
-    ),
-    # www.flickr.com/photos/{username|NSID}/{pageN}?
-    (
-        pattern_url_photostream,
-        # according to flickr api documentation, this method needs to be
-        # authenticated in order to filter photo visible to the calling user
-        # but it seems works fine anonymously as well
-        'people.getPhotos',
-        'user_id',
-        get_NSID,
-        'photos'
-    ),
-    # www.flickr.com/photos/{username|NSID}/galleries/{gallery-id}
-    (
-        pattern_url_gallery,
-        'galleries.getPhotos',
-        'gallery_id',
-        get_gallery_id,
-        'photos'
-    ),
-    # www.flickr.com/groups/{groupname|groupNSID}/
-    (
-        pattern_url_group,
-        'groups.pools.getPhotos',
-        'group_id',
-        get_NSID,
-        'photos'
-    ),
-    # www.flickr.com/photos/{username|NSID}/favorites/*
-    (
-        pattern_url_favorite,
-        'favorites.getList',
-        'user_id',
-        get_NSID,
-        'photos'
+    iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+    
+    import numpy as np
+import matplotlib.pyplot as plt
+    
+    import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+    
+            colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a',
+                                             '#f781bf', '#a65628', '#984ea3',
+                                             '#999999', '#e41a1c', '#dede00']),
+                                      int(max(y_pred) + 1))))
+        plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[y_pred])
+    
+    # KMeans
+ax = fig.add_subplot(1, 3, 1)
+for k, col in zip(range(n_clusters), colors):
+    my_members = k_means_labels == k
+    cluster_center = k_means_cluster_centers[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], 'w',
+            markerfacecolor=col, marker='.')
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+            markeredgecolor='k', markersize=6)
+ax.set_title('KMeans')
+ax.set_xticks(())
+ax.set_yticks(())
+plt.text(-3.5, 1.8,  'train time: %.2fs\ninertia: %f' % (
+    t_batch, k_means.inertia_))
+    
+    
+def setup(app):
+    app.add_crossref_type(
+        directivename = 'setting',
+        rolename      = 'setting',
+        indextemplate = 'pair: %s; setting',
     )
-]
+    app.add_crossref_type(
+        directivename = 'signal',
+        rolename      = 'signal',
+        indextemplate = 'pair: %s; signal',
+    )
+    app.add_crossref_type(
+        directivename = 'command',
+        rolename      = 'command',
+        indextemplate = 'pair: %s; command',
+    )
+    app.add_crossref_type(
+        directivename = 'reqmeta',
+        rolename      = 'reqmeta',
+        indextemplate = 'pair: %s; reqmeta',
+    )
+    app.add_role('source', source_role)
+    app.add_role('commit', commit_role)
+    app.add_role('issue', issue_role)
+    app.add_role('rev', rev_role)
     
-        @classmethod
-    def funshion_decrypt_str(cls, a_str, coeff):
-        # r'.{27}0' pattern, untested
-        if len(a_str) == 28 and a_str[-1] == '0':
-            data_bytes = base64.b64decode(a_str[:27] + '=')
-            clear = cls.funshion_decrypt(data_bytes, coeff)
-            return binascii.hexlify(clear.encode('utf8')).upper()
+            if now - self.lastmark >= 3:
+            self.lastmark = now
+            qps = len(self.tail) / sum(self.tail)
+            print('samplesize={0} concurrent={1} qps={2:0.2f}'.format(len(self.tail), self.concurrent, qps))
     
-        def api_req(self, url):
-        xml_str = get_content(url)
-        dom = parseString(xml_str)
-        status = dom.getElementsByTagName('result')[0].getAttribute('status')
-        if status != 'success':
-            raise Exception('API returned fail')
+        def __init__(self, *a, **kw):
+        super(QPSSpider, self).__init__(*a, **kw)
+        if self.qps is not None:
+            self.qps = float(self.qps)
+            self.download_delay = 1 / self.qps
+        elif self.download_delay is not None:
+            self.download_delay = float(self.download_delay)
+    
+    # Ignore noisy twisted deprecation warnings
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='twisted')
+del warnings
+    
+    from scrapy.commands import ScrapyCommand
+from scrapy.contracts import ContractsManager
+from scrapy.utils.misc import load_object
+from scrapy.utils.conf import build_component_list
+    
+        def process_options(self, args, opts):
+        ScrapyCommand.process_options(self, args, opts)
+        try:
+            opts.spargs = arglist_to_dict(opts.spargs)
+        except ValueError:
+            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
+        if opts.output:
+            if opts.output == '-':
+                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
+            else:
+                self.settings.set('FEED_URI', opts.output, priority='cmdline')
+            feed_exporters = without_none_values(
+                self.settings.getwithbase('FEED_EXPORTERS'))
+            valid_output_formats = feed_exporters.keys()
+            if not opts.output_format:
+                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
+            if opts.output_format not in valid_output_formats:
+                raise UsageError('Unrecognized output format '%s', set one'
+                                 ' using the '-t' switch or as a file extension'
+                                 ' from the supported list %s' % (opts.output_format,
+                                                                  tuple(valid_output_formats)))
+            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
+    
+            editor = self.settings['EDITOR']
+        try:
+            spidercls = self.crawler_process.spider_loader.load(args[0])
+        except KeyError:
+            return self._err('Spider not found: %s' % args[0])
+    
+        def _print_bytes(self, bytes_):
+        bytes_writer = sys.stdout if six.PY2 else sys.stdout.buffer
+        bytes_writer.write(bytes_ + b'\n')
+    
+            if opts.meta:
+            try:
+                opts.meta = json.loads(opts.meta)
+            except ValueError:
+                raise UsageError('Invalid -m/--meta value, pass a valid json string to -m or --meta. ' \
+                                'Example: --meta='{\'foo\' : \'bar\'}'', print_help=False)
+    
+        def process_options(self, args, opts):
+        ScrapyCommand.process_options(self, args, opts)
+        try:
+            opts.spargs = arglist_to_dict(opts.spargs)
+        except ValueError:
+            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
+        if opts.output:
+            if opts.output == '-':
+                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
+            else:
+                self.settings.set('FEED_URI', opts.output, priority='cmdline')
+            feed_exporters = without_none_values(self.settings.getwithbase('FEED_EXPORTERS'))
+            valid_output_formats = feed_exporters.keys()
+            if not opts.output_format:
+                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
+            if opts.output_format not in valid_output_formats:
+                raise UsageError('Unrecognized output format '%s', set one'
+                                 ' using the '-t' switch or as a file extension'
+                                 ' from the supported list %s' % (opts.output_format,
+                                                                  tuple(valid_output_formats)))
+            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
+    
+    if twisted_version >= (14, 0, 0):
+    
+            self._signer = None
+        if is_botocore():
+            import botocore.auth
+            import botocore.credentials
+            kw.pop('anon', None)
+            if kw:
+                raise TypeError('Unexpected keyword arguments: %s' % kw)
+            if not self.anon:
+                SignerCls = botocore.auth.AUTH_TYPE_MAPS['s3']
+                self._signer = SignerCls(botocore.credentials.Credentials(
+                    aws_access_key_id, aws_secret_access_key))
+        else:
+            _S3Connection = _get_boto_connection()
+            try:
+                self.conn = _S3Connection(
+                    aws_access_key_id, aws_secret_access_key, **kw)
+            except Exception as ex:
+                raise NotConfigured(str(ex))
+    
+            Same as Twisted's private _sslverify.ClientTLSOptions,
+        except that VerificationError, CertificateError and ValueError
+        exceptions are caught, so that the connection is not closed, only
+        logging warnings.
+        '''
+    
+            if self.slot.start_requests is not None:
+            # not all start requests are handled
+            return False
