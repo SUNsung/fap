8fe8e8d5233925233b21f0320024c1bb4df82ad2
SUNsung <weatherius@gmail.com>
2019-06-07 11:40:58 +0300

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2019-06-07 11:40:58

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,33 +1,88 @@
 
         
-                def collection_method
-          :pull_requests_comments
+                  def hidden_field
+            hidden_name = @html_options[:name] || hidden_field_name
+            @template_object.hidden_field_tag(hidden_name, '', id: nil)
+          end
+    
+            class << self
+          def field_type
+            @field_type ||= name.split('::').last.sub('Field', '').downcase
+          end
         end
     
-            # Builds a user using a Hash that was built from a JSON payload.
-        def self.from_json_hash(raw_hash)
-          new(Representation.symbolize_hash(raw_hash))
-        end
+      # Add groups and the proper project name to the output.
+  project_name 'Homebrew'
+  add_group 'Cask', %r{^/cask/}
+  add_group 'Commands', [%r{/cmd/}, %r{^/dev-cmd/}]
+  add_group 'Extensions', %r{^/extend/}
+  add_group 'OS', [%r{^/extend/os/}, %r{^/os/}]
+  add_group 'Requirements', %r{^/requirements/}
+  add_group 'Scripts', [
+    %r{^/brew.rb$},
+    %r{^/build.rb$},
+    %r{^/postinstall.rb$},
+    %r{^/test.rb$},
+  ]
+end
+
     
-        def display_error_message(ex)
-      unless options.backtrace
-        Rake.application.options.suppress_backtrace_pattern = backtrace_pattern if backtrace_pattern
-        trace '(Backtrace restricted to imported tasks)'
+      #
+  # Parser Events
+  #
+    
+    tmp = open(while_tmp, 'r')
+test_ok(tmp.kind_of?(File))
+    
+        execute 'INSERT INTO share_visibilities (user_id, shareable_id, shareable_type) ' \
+            'SELECT post_visibility.user_id, photos.id, 'Photo' FROM photos ' \
+            'INNER JOIN posts ON posts.guid = photos.status_message_guid AND posts.type = 'StatusMessage' ' \
+            'LEFT OUTER JOIN share_visibilities ON share_visibilities.shareable_id = photos.id ' \
+            'INNER JOIN share_visibilities AS post_visibility ON post_visibility.shareable_id = posts.id ' \
+            'WHERE photos.public = false AND share_visibilities.shareable_id IS NULL ' \
+            'AND post_visibility.shareable_type = 'Post''
+  end
+    
+      describe '#destroy' do
+    let(:post) { FactoryGirl.create(:status_message) }
+    
+          it 'succeeds' do
+        put :update, params: {id: 42, post_id: @status.id}, format: :js
+        expect(response).to be_success
       end
     
-        def initialize(app)
-      @app = app
-    end
     
-            close_body(body) if reaction
     
-        it 'Reads referrer from Host header when Referer header is relative' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
-      expect(subject.referrer(env)).to eq('foo.com')
-    end
+    module Rack
+  module Protection
+    class Base
+      DEFAULT_OPTIONS = {
+        :reaction    => :default_reaction, :logging   => true,
+        :message     => 'Forbidden',       :encryptor => Digest::SHA1,
+        :session_key => 'rack.session',    :status    => 403,
+        :allow_empty_referrer => true,
+        :report_key           => 'protection.failed',
+        :html_types           => %w[text/html application/xhtml text/xml application/xml]
+      }
     
-          get '/', {}, 'HTTP_COOKIE' => '_session=EVIL_SESSION_TOKEN; _session=SESSION_TOKEN'
-      expect(last_response).not_to be_ok
+          DIRECTIVES = %i(base_uri child_src connect_src default_src
+                      font_src form_action frame_ancestors frame_src
+                      img_src manifest_src media_src object_src
+                      plugin_types referrer reflected_xss report_to
+                      report_uri require_sri_for sandbox script_src
+                      style_src worker_src).freeze
+    
+          def initialize(*)
+        super
+    
+      it 'accepts post requests with correct X-CSRF-Token header' do
+    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => token)
+    expect(last_response).to be_ok
+  end
+    
+        it 'Returns nil when Referer header is invalid' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
+      expect(subject.referrer(env)).to be_nil
     end
   end
 end
