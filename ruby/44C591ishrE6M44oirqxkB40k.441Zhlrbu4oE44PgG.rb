1b6f786834701aa7d2cee23d244d5107b906cceb
SUNsung <weatherius@gmail.com>
2019-06-08 01:55:11 +0300

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2019-06-08 01:55:10

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,120 +1,68 @@
 
         
-              if @actions_requiring_special_handling.include?(action_name)
-        command_return = run_action_requiring_special_handling(
-          command: command,
-          parameter_map: parameter_map,
-          action_return_type: action_class_ref.return_type
-        )
-        return command_return
-      end
-    
-          it 'sets the command to bootstrap' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(command: 'bootstrap')
-          end').runner.execute(:test)
-    
-          it 'works with select regex when regex is string' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-              select_regex: \'\/AppDelegate\'
-            )
-          end').runner.execute(:test)
-    
-          it 'should not be fooled by 10 local code signing identities available' do
-        allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
-        allow(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return('     10 valid identities found\n')
-        expect(FastlaneCore::UI).not_to(receive(:error))
-    
-        context '(de)activating users' do
-      it 'does not show deactivation buttons for the current user' do
-        visit admin_users_path
-        expect(page).to have_no_css('a[href='/admin/users/#{users(:jane).id}/deactivate']')
-      end
-    
-        fill_in(:agent_options, with: '{
-      'expected_receive_period_in_days': '2'
-      'keep_event': 'false'
-    }')
-    expect(get_alert_text_from { click_on 'Save' }).to have_text('Sorry, there appears to be an error in your JSON input. Please fix it before continuing.')
-  end
-    
-    end
+        # == Schema Information
+#
+# Table name: directory_items
+#
+#  id             :integer          not null, primary key
+#  period_type    :integer          not null
+#  user_id        :integer          not null
+#  likes_received :integer          not null
+#  likes_given    :integer          not null
+#  topics_entered :integer          not null
+#  topic_count    :integer          not null
+#  post_count     :integer          not null
+#  created_at     :datetime
+#  updated_at     :datetime
+#  days_visited   :integer          default(0), not null
+#  posts_read     :integer          default(0), not null
+#
+# Indexes
+#
+#  index_directory_items_on_days_visited             (days_visited)
+#  index_directory_items_on_likes_given              (likes_given)
+#  index_directory_items_on_likes_received           (likes_received)
+#  index_directory_items_on_period_type_and_user_id  (period_type,user_id) UNIQUE
+#  index_directory_items_on_post_count               (post_count)
+#  index_directory_items_on_posts_read               (posts_read)
+#  index_directory_items_on_topic_count              (topic_count)
+#  index_directory_items_on_topics_entered           (topics_entered)
+#
 
     
-    describe ConvertWebsiteAgentTemplateForMerge do
-  let :old_extract do
-    {
-      'url' => { 'css' => '#comic img', 'value' => '@src' },
-      'title' => { 'css' => '#comic img', 'value' => '@alt' },
-      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
-    }
-  end
-    
-      describe '#log_for_agent' do
-    it 'creates AgentLogs' do
-      log = AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 4, :outbound_event => events(:jane_website_agent_event))
-      expect(log).not_to be_new_record
-      expect(log.agent).to eq(agents(:jane_website_agent))
-      expect(log.outbound_event).to eq(events(:jane_website_agent_event))
-      expect(log.message).to eq('some message')
-      expect(log.level).to eq(4)
-    end
+        def build
+      policy = ActionDispatch::ContentSecurityPolicy.new
     
         private
     
-        render json: @web_subscription, serializer: REST::WebPushSubscriptionSerializer
+          keyword = params.require(:keyword)
+      data = SvgSprite.search(keyword)
+    
+      # topic_reply_count is a count of posts in other users' topics
+  def update_topic_reply_count
+    self.topic_reply_count = Topic
+      .joins('INNER JOIN posts ON topics.id = posts.topic_id AND topics.user_id <> posts.user_id')
+      .where('posts.deleted_at IS NULL AND posts.user_id = ?', self.user_id)
+      .distinct
+      .count
   end
     
-      def update
-    setting.data = params[:data]
-    setting.save!
-    
-          if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: provider_id.capitalize) if is_navigational_format?
-      else
-        session['devise.#{provider}_data'] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
-      end
-    end
-  end
-    
-      included do
-    before_action :authenticate_user!
-    before_action :load_export
-  end
-    
-      def compatible_locale
-    http_accept_language.compatible_language_from(available_locales)
-  end
-    
-      # Update version.rb file with BOOTSTRAP_SHA
-  def store_version
-    path    = 'lib/bootstrap-sass/version.rb'
-    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
-    File.open(path, 'w') { |f| f.write(content) }
-  end
+      gem.add_development_dependency 'danger'
+  gem.add_development_dependency 'mocha'
+  gem.add_development_dependency 'rspec'
+  gem.add_development_dependency 'rubocop', '0.48.1'
 end
 
     
-            fields = object.preferences.keys.map do |key|
-          if object.has_preference?(key)
-            form.label('preferred_#{key}', Spree.t(key) + ': ') +
-              preference_field_for(form, 'preferred_#{key}', type: object.preference_type(key))
-          end
-        end
-        safe_join(fields, '<br />'.html_safe)
-      end
+        def version
+      ['--version', '-V',
+       'Display the program version.',
+       lambda do |_value|
+         puts 'Capistrano Version: #{Capistrano::VERSION} (Rake Version: #{Rake::VERSION})'
+         exit
+       end]
+    end
     
-            def load_order(lock = false)
-          @order = Spree::Order.lock(lock).find_by!(number: params[:id])
-          raise_insufficient_quantity and return if @order.insufficient_stock_lines.present?
-          @order.state = params[:state] if params[:state]
-          state_callback(:before)
-        end
+          private
     
-              def update_service
-            Spree::Api::Dependencies.storefront_checkout_update_service.constantize
-          end
+          attr_reader :locations, :values, :fetched_keys
