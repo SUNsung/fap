3faca342a0655ff5012e497bc6c9417f0580289f
SUNsung <weatherius@gmail.com>
2019-06-07 04:55:25 +0300

    Rewrite [29dyXK0K7wdUU29l1lxRjIE8gE29IyZNGAS3SnY29V1s.py] File in: 2019-06-07 04:55:24

--- pyton/29dyXK0K7wdUU29l1lxRjIE8gE29IyZNGAS3SnY29V1s.py
+++ pyton/29dyXK0K7wdUU29l1lxRjIE8gE29IyZNGAS3SnY29V1s.py
@@ -1,114 +1,82 @@
 
         
-        
-if __name__ == '__main__':
-    main()
+                When updating an entry, updates its position to the front of the LRU list.
+        If the entry is new and the cache is at capacity, removes the oldest entry
+        before the new entry is added.
+        '''
+        node = self.map[query]
+        if node is not None:
+            # Key exists in cache, update the value
+            node.results = results
+            self.linked_list.move_to_front(node)
+        else:
+            # Key does not exist in cache
+            if self.size == self.MAX_SIZE:
+                # Remove the oldest entry from the linked list and lookup
+                self.lookup.pop(self.linked_list.tail.query, None)
+                self.linked_list.remove_from_tail()
+            else:
+                self.size += 1
+            # Add the new key and value
+            new_node = Node(query, results)
+            self.linked_list.append_to_front(new_node)
+            self.lookup[query] = new_node
 
     
-        ipa_dnszone = client.dnszone_find(zone_name)
+        def _colision_resolution(self, key, data=None):
+        new_key = self.hash_function(key + 1)
     
-        def role_remove_member(self, name, item):
-        return self._post_json(method='role_remove_member', name=name, item=item)
+    def getBlock(bitString):
+	'''[summary]
+	Iterator:
+		Returns by each call a list of length 16 with the 32 bit
+		integer blocks. 
     
-    ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-    
-    
-def _makeLoader(prefix, sortUsing, suiteClass=None, testNamePatterns=None):
-    loader = TestLoader()
-    loader.sortTestMethodsUsing = sortUsing
-    loader.testMethodPrefix = prefix
-    loader.testNamePatterns = testNamePatterns
-    if suiteClass:
-        loader.suiteClass = suiteClass
-    return loader
-    
-            self.check_tokenize('''\
-async def foo():
-  def foo(await):
-    await = 1
-  if 1:
-    await
-async += 1
-''', '''\
-    NAME       'async'       (1, 0) (1, 5)
-    NAME       'def'         (1, 6) (1, 9)
-    NAME       'foo'         (1, 10) (1, 13)
-    OP         '('           (1, 13) (1, 14)
-    OP         ')'           (1, 14) (1, 15)
-    OP         ':'           (1, 15) (1, 16)
-    NEWLINE    '\\n'          (1, 16) (1, 17)
-    INDENT     '  '          (2, 0) (2, 2)
-    NAME       'def'         (2, 2) (2, 5)
-    NAME       'foo'         (2, 6) (2, 9)
-    OP         '('           (2, 9) (2, 10)
-    NAME       'await'       (2, 10) (2, 15)
-    OP         ')'           (2, 15) (2, 16)
-    OP         ':'           (2, 16) (2, 17)
-    NEWLINE    '\\n'          (2, 17) (2, 18)
-    INDENT     '    '        (3, 0) (3, 4)
-    NAME       'await'       (3, 4) (3, 9)
-    OP         '='           (3, 10) (3, 11)
-    NUMBER     '1'           (3, 12) (3, 13)
-    NEWLINE    '\\n'          (3, 13) (3, 14)
-    DEDENT     ''            (4, 2) (4, 2)
-    NAME       'if'          (4, 2) (4, 4)
-    NUMBER     '1'           (4, 5) (4, 6)
-    OP         ':'           (4, 6) (4, 7)
-    NEWLINE    '\\n'          (4, 7) (4, 8)
-    INDENT     '    '        (5, 0) (5, 4)
-    NAME       'await'       (5, 4) (5, 9)
-    NEWLINE    '\\n'          (5, 9) (5, 10)
-    DEDENT     ''            (6, 0) (6, 0)
-    DEDENT     ''            (6, 0) (6, 0)
-    NAME       'async'       (6, 0) (6, 5)
-    OP         '+='          (6, 6) (6, 8)
-    NUMBER     '1'           (6, 9) (6, 10)
-    NEWLINE    '\\n'          (6, 10) (6, 11)
-    ''')
+        # with a probability of 1, always throw errors
+    config.DYNAMODB_ERROR_PROBABILITY = 1.0
+    assert_raises(ClientError, table.put_item, Item={PARTITION_KEY: short_uid(), 'data': 'foobar123'})
     
     
-class Dummy_about_dialog():
-    # Dummy class for testing file display functions.
-    idle_credits = About.show_idle_credits
-    idle_readme = About.show_readme
-    idle_news = About.show_idle_news
-    # Called by the above
-    display_file_text = About.display_file_text
-    _utest = True
+def get_or_create_file(config_file):
+    if os.path.exists(config_file):
+        return config_file
+    try:
+        save_file(config_file, '{}')
+        return config_file
+    except Exception:
+        pass
     
-    import os
-import smtplib
-# For guessing MIME type based on file name extension
-import mimetypes
+            params = apigateway_listener.extract_path_params('/foo/bar/baz', '/foo/{proxy+}')
+        self.assertEqual(params, {'proxy+': 'bar/baz'})
     
-    def plus(a, b):
-    time.sleep(0.5 * random.random())
-    return a + b
-    
-    # To run this, you need a Raspberry Pi 2 (or greater) with face_recognition and
-# the picamera[array] module installed.
-# You can follow this installation instructions to get your RPi set up:
-# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
-    
-            face_encoding_a1 = api.face_encodings(img_a1)[0]
-        face_encoding_a2 = api.face_encodings(img_a2)[0]
-        face_encoding_a3 = api.face_encodings(img_a3)[0]
-        face_encoding_b1 = api.face_encodings(img_b1)[0]
-    
-    # Loop through each face found in the unknown image
-for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-    # See if the face is a match for the known face(s)
-    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-    
-        # Sparkle the eyes
-    d.polygon(face_landmarks['left_eye'], fill=(255, 255, 255, 30))
-    d.polygon(face_landmarks['right_eye'], fill=(255, 255, 255, 30))
+        return getattr(command_class, command)
     
     
-def batch_face_locations(images, number_of_times_to_upsample=1, batch_size=128):
-    '''
-    Returns an 2d array of bounding boxes of human faces in a image using the cnn face detector
-    If you are using a GPU, this can give you much faster results since the GPU
-    can process batches of images at once. If you aren't using a GPU, you don't need this function.
+def is_ubuntu():
+    return platform.system() == 'Linux' and platform.linux_distribution()[0] == 'Ubuntu'
+    
+    
+def get_source_name_from_network_mode(network_mode, source_type):
+    if not network_mode:
+        return
+    
+    
+def update_init_py_version(version):
+    path = os.path.join(REPO_ROOT, 'compose', '__init__.py')
+    with open(path, 'r') as f:
+        contents = f.read()
+    contents = re.sub(r'__version__ = '[0-9a-z.-]+'', '__version__ = '{}''.format(version), contents)
+    with open(path, 'w') as f:
+        f.write(contents)
+    
+    
+class ResilienceTest(DockerClientTestCase):
+    def setUp(self):
+        self.db = self.create_service(
+            'db',
+            volumes=[VolumeSpec.parse('/var/db')],
+            command='top')
+        self.project = Project('composetest', [self.db], self.client)
+    
+        def assert_produces(self, reader, expectations):
+        split = split_buffer(reader())
