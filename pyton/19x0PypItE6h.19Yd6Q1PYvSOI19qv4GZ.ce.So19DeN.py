b4e0af77843c43cb35002f1f93a569f81318bb6a
SUNsung <weatherius@gmail.com>
2019-06-07 10:34:18 +0300

    Rewrite [19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py] File in: 2019-06-07 10:34:18

--- pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
+++ pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
@@ -1,177 +1,75 @@
 
         
-        
-def test_when_cant_configure_automatically(shell_pid, shell, logs):
-    shell_pid.return_value = 12
-    shell.how_to_configure.return_value = ShellConfiguration(
-        content='eval $(thefuck --alias)',
-        path='/tmp/.bashrc',
-        reload='bash',
-        can_configure_automatically=False)
-    main()
-    logs.how_to_configure_alias.assert_called_once()
-    
-    
-@pytest.mark.functional
-def test_select_command_with_arrows(proc, TIMEOUT):
-    select_command_with_arrows(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'git help')
-    
-    
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'echo test')
-    
-    
-misspelled_command = '''\
-usage: aws [options] <command> <subcommand> [<subcommand> ...] [parameters]
-To see help text, you can run:
-    
-    
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_match(brew_no_available_formula, brew_already_installed,
-               brew_install_no_argument):
-    assert match(Command('brew install elsticsearch',
-                         brew_no_available_formula))
-    assert not match(Command('brew install git',
-                             brew_already_installed))
-    assert not match(Command('brew install', brew_install_no_argument))
-    
-    \tDid you mean `build`?
-'''
-    
-        def __init__(self):
-        self.name = 'hstore'
-    
-    
-@deconstructible
-class KeysValidator:
-    '''A validator designed for HStore to require/restrict keys.'''
-    
-        def load(self):
-        try:
-            data = self._cache.get(self.cache_key)
-        except Exception:
-            # Some backends (e.g. memcache) raise an exception on invalid
-            # cache keys. If this happens, reset the session. See #17810.
-            data = None
-    
-        def create_model_instance(self, data):
-        '''
-        Return a new instance of the session model object, which represents the
-        current session state. Intended to be used for saving the session data
-        to the database.
-        '''
-        return self.model(
-            session_key=self._get_or_create_session_key(),
-            session_data=self.encode(data),
-            expire_date=self.get_expiry_date(),
-        )
-    
-        def __enter__(self):
-        from scrapy.utils.test import get_testenv
-        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
-        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                     env=get_testenv())
-        self.proc.stdout.readline()
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
-                          help='set spider argument (may be repeated)')
-        parser.add_option('-o', '--output', metavar='FILE',
-                          help='dump scraped items into FILE (use - for stdout)')
-        parser.add_option('-t', '--output-format', metavar='FORMAT',
-                          help='format to use for dumping items with -o')
-    
-        if twisted_version < (17, 0, 0):
-        from twisted.internet._sslverify import _maybeSetHostNameIndication
-        set_tlsext_host_name = _maybeSetHostNameIndication
-    else:
-        def set_tlsext_host_name(connection, hostNameBytes):
-            connection.set_tlsext_host_name(hostNameBytes)
-    
-    
-def get_line_value(r, n):
-    rls = r.split('\r\n')
-    if len(rls) < n + 1:
-        return None
-    
-                msg = 'missing %s at %s' % (
-                tokenName, self.getTokenErrorDisplay(e.token)
-                )
-    
-        # Used for issues that may render the system inoperable or have effects on
-    # data integrity (e.g. issues in the processing pipeline.)
-    SEVERITY_CRITICAL = 'critical'
-    
-    
-class CeleryAliveCheck(StatusCheck):
-    def check(self):
-        # There is no queue, and celery is not running, so never show error
-        if settings.CELERY_ALWAYS_EAGER:
-            return []
-        last_ping = options.get('sentry:last_worker_ping') or 0
-        if last_ping >= time() - 300:
-            return []
-    
-        This is currenlty only used for ``in_next_release`` resolutions.
-    '''
+            '''Get an elasticache connection'''
     try:
-        release = Release.objects.get_from_cache(
-            id=release_id,
-        )
-    except Release.DoesNotExist:
-        return
+        conn = connect_to_region(region_name=region, **aws_connect_kwargs)
+    except boto.exception.NoAuthHandlerFound as e:
+        module.fail_json(msg=e.message)
     
-            for platform, project_id in queryset:
-            platform = platform.lower()
-            if platform not in VALID_PLATFORMS:
-                continue
-            ProjectPlatform.objects.create_or_update(
-                project_id=project_id,
-                platform=platform,
-                values={
-                    'last_seen': now,
-                },
-            )
-        min_project_id += step
-    
-        STOP_WORDS = set((
-        'the', 'of', 'is', 'and', 'to', 'in', 'that', 'we', 'for', 'an', 'are',
-        'by', 'be', 'as', 'on', 'with', 'can', 'if', 'from', 'which', 'you', 'it',
-        'this', 'then', 'at', 'have', 'all', 'not', 'one', 'has', 'or', 'that'
-    ))
+    EXAMPLES = '''
+# Retrieve server certificate
+- iam_server_certificate_facts:
+    name: production-cert
+  register: server_cert
     
     
-def add_force_split(word):
-    global Force_Split_Words
-    Force_Split_Words.add(word)
+def protocol_to_string(protocol):
+    protocol = protocol_to_tuple(protocol)
+    if protocol[0] is True:
+        return 'Tcp'
+    elif protocol[1] is True:
+        return 'Udp'
+    elif protocol[2] is True:
+        return 'Icmp'
+    elif protocol[3] is True:
+        return 'Other'
+    elif protocol[4] is True:
+        return 'Any'
+    
+                    # Only add to current user
+                if store == CERT_SYSTEM_STORE_CURRENT_USER and not ca_exists:
+                    ret = crypt32.CertAddEncodedCertificateToStore(store_handle, X509_ASN_ENCODING, certdata, len(certdata), CERT_STORE_ADD_ALWAYS, None)
+            except Exception as e:
+                xlog.warning('CertUtil.import_windows_ca failed: %r', e)
+                if isinstance(e, OSError):
+                    store_handle = None
+                    continue
+                return False
+            finally:
+                if pCertCtx:
+                    crypt32.CertFreeCertificateContext(pCertCtx)
+                    pCertCtx = None
+                if store_handle:
+                    crypt32.CertCloseStore(store_handle, 0)
+                    store_handle = None
+    
+    # tree.RewriteRuleElementStream
+# tree.RewriteRuleSubtreeStream
+# tree.RewriteRuleTokenStream
+# CharStream
+# DFA
+# TokenSource
     
     
-def get_top_states(t_state_v, K=4):
-    return sorted(t_state_v, key=t_state_v.__getitem__, reverse=True)[:K]
+def get_github_url(app, view, path):
+    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
+    return (
+        github_fmt.format(app.config.edit_on_github_project, view,
+                          app.config.edit_on_github_branch,
+                          app.config.edit_on_github_src_path, path))
     
-    USAGE = 'usage:    python extract_tags.py [file name] -k [top k]'
+    # If true, an OpenSearch description file will be output, and all pages will
+# contain a <link> tag referring to it.  The value of this option must be the
+# base URL from which the finished HTML is served.
+#
+# html_use_opensearch = ''
     
-    print('transform')
-counts = count_vect.fit_transform(docs)
-tfidf = TfidfTransformer().fit_transform(counts)
-print(tfidf.shape)
     
-    import jieba
-jieba.enable_parallel(4)
-import jieba.analyse
-from optparse import OptionParser
-    
-            print('Success: test_selection_sort\n')
-    
-    		elif current.rightChild is None:
-			if current is self.root:
-				self.root = current.rightChild
-			elif isLeft:
-				parent.lChild = current.rightChild
-			else:
-				parent.rightChild = current.rightChild
+def GetProgram(plist):
+  '''Plists have either a Program or ProgramArguments key,
+     if the executable requires command line options.
+  '''
+  try:
+    return '['%s']' % plist['Program'], HashFile(plist['Program'])
+  except KeyError:
+    return plist['ProgramArguments'], HashFile(plist['ProgramArguments'])
