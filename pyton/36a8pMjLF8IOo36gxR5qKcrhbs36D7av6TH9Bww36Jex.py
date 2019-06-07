856565c1644acd0394c72cd9a78b6318404a92b8
SUNsung <weatherius@gmail.com>
2019-06-07 06:30:31 +0300

    Rewrite [36a8pMjLF8IOo36gxR5qKcrhbs36D7av6TH9Bww36Jex.py] File in: 2019-06-07 06:30:31

--- pyton/36a8pMjLF8IOo36gxR5qKcrhbs36D7av6TH9Bww36Jex.py
+++ pyton/36a8pMjLF8IOo36gxR5qKcrhbs36D7av6TH9Bww36Jex.py
@@ -1,105 +1,59 @@
 
         
-            def __init__(self, rank):
-        self.state = CallState.READY
-        self.rank = rank
-        self.employee = None
+        site_info = 'CBS.com'
+download = cbs_download
+download_playlist = playlist_not_supported('cbs')
+
     
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
+    #----------------------------------------------------------------------
+#helper
+#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python
+def dictify(r,root=True):
+    if root:
+        return {r.tag : dictify(r, False)}
+    d=copy(r.attrib)
+    if r.text:
+        d['_text']=r.text
+    for x in r.findall('./*'):
+        if x.tag not in d:
+            d[x.tag]=[]
+        d[x.tag].append(dictify(x,False))
+    return d
     
-    # Read lines from the linkcheck output file
-try:
-    with open('build/linkcheck/output.txt') as out:
-        output_lines = out.readlines()
-except IOError:
-    print('linkcheck output not found; please run linkcheck first.')
-    exit(1)
+    # image size suffixes used in inline json 'key' field
+# listed in descending order
+size_suffixes = ['o', 'k', 'h', 'l', 'c', 'z', 'm', 'n', 's', 't', 'q', 'sq']
     
-            if 'latency' in request.args:
-            latency = float(request.args['latency'][0])
-            reactor.callLater(latency, self._finish, request)
-            return NOT_DONE_YET
+        @classmethod
+    def checksum(cls, sha1_str):
+        if len(sha1_str) != 41:
+            return False
+        if not re.match(r'[0-9A-Za-z]{41}', sha1_str):
+            return False
+        sha1 = sha1_str[:-1]
+        if (15 & sum([int(char, 16) for char in sha1])) == int(sha1_str[-1], 16):
+            return True
+        return False
     
-            self.crawler_process.crawl(spname, **opts.spargs)
-        self.crawler_process.start()
+        url = list(set([
+        unicodize(str.replace(i, '\\/', '/'))
+        for i in re.findall(r'<meta property='og:video:secure_url' content='(.*?)'>', html)
+    ]))
     
-        def long_desc(self):
-        return ('Edit a spider using the editor defined in the EDITOR environment'
-                ' variable or else the EDITOR setting')
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('--spider', dest='spider',
-            help='use this spider')
-        parser.add_option('--headers', dest='headers', action='store_true', \
-            help='print response HTTP headers instead of body')
-        parser.add_option('--no-redirect', dest='no_redirect', action='store_true', \
-            default=False, help='do not handle HTTP 3xx status codes and print response as-is')
-    
-            if opts.meta:
-            try:
-                opts.meta = json.loads(opts.meta)
-            except ValueError:
-                raise UsageError('Invalid -m/--meta value, pass a valid json string to -m or --meta. ' \
-                                'Example: --meta='{\'foo\' : \'bar\'}'', print_help=False)
-    
-            # If no credentials could be found anywhere,
-        # consider this an anonymous connection request by default;
-        # unless 'anon' was set explicitly (True/False).
-        anon = kw.get('anon')
-        if anon is None and not aws_access_key_id and not aws_secret_access_key:
-            kw['anon'] = True
-        self.anon = kw.get('anon')
-    
-    current_path = os.path.dirname(os.path.abspath(__file__))
-root_path = os.path.abspath(os.path.join(current_path, os.pardir, os.pardir, os.pardir))
-data_path = os.path.abspath(os.path.join(root_path, os.pardir, os.pardir, 'data', 'gae_proxy'))
-if not os.path.isdir(data_path):
-    data_path = current_path
-    
-        if __password__ and __password__ != kwargs.get('password', ''):
-        start_response('403 Forbidden', [('Content-Type', 'text/html')])
-        yield message_html('403 Wrong password', 'Wrong password(%r)' % kwargs.get('password', ''), 'GoAgent proxy.ini password is wrong!')
-        raise StopIteration
-    
-        Because operations never actually alter the buffer, you may always get
-    the original token stream back without undoing anything.  Since
-    the instructions are queued up, you can easily simulate transactions and
-    roll back any changes if there is an error just by removing instructions.
-    For example,
-    
-    
-    def setText(self, text):
-        '''
-        Override the text for this token.  getText() will return this text
-        rather than pulling from the buffer.  Note that this does not mean
-        that start/stop indexes are not valid.  It means that that input
-        was converted to a new string in the token object.
-	'''
-        self._text = text
-    
-    setup_face_landmarks = '''
-import face_recognition
-    
-    # Load a test image and get encondings for it
-image_to_test = face_recognition.load_image_file('obama2.jpg')
-image_to_test_encoding = face_recognition.face_encodings(image_to_test)[0]
-    
-    # Find all the faces in the image using the default HOG-based model.
-# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-# See also: find_faces_in_picture_cnn.py
-face_locations = face_recognition.face_locations(image)
-    
-        return known_names, known_face_encodings
-    
-    # 提示：运行本案例需要安装Flask，你可以用下面的代码安装Flask
-# $ pip3 install flask
-    
-    # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file('obama.jpg')
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+            else:
+            # gallery image
+            content = get_content(self.url)
+            image = json.loads(match1(content, r'image\s*:\s*({.*}),'))
+            ext = image['ext']
+            self.streams = {
+                'original': {
+                    'src': ['http://i.imgur.com/%s%s' % (image['hash'], ext)],
+                    'size': image['size'],
+                    'container': ext[1:]
+                },
+                'thumbnail': {
+                    'src': ['http://i.imgur.com/%ss%s' % (image['hash'], '.jpg')],
+                    'container': 'jpg'
+                }
+            }
+            self.title = image['title'] or image['hash']
