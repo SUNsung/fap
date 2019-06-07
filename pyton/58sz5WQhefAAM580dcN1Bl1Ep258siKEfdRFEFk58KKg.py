9afd2deb8c0bbed91cbbb08b26bf3de860f1bdd1
SUNsung <weatherius@gmail.com>
2019-06-07 17:57:48 +0300

    Rewrite [58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py] File in: 2019-06-07 17:57:47

--- pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
+++ pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
@@ -1,125 +1,37 @@
 
         
-            # Set the random seed
-    if hparams.random_seed > 0:
-      tf.set_random_seed(hparams.random_seed)
+            if last_snapshot_min_age > 0:
+        try:
+            current_snapshots = ec2.get_all_snapshots(filters={'volume_id': volume_id})
+        except boto.exception.BotoServerError as e:
+            module.fail_json(msg='%s: %s' % (e.error_code, e.error_message))
     
-      # Note that we put these 'truth' rates and input into this
-  # structure, the only data that is used in LFADS are the noisy
-  # data e.g. spike trains.  The rest is either for printing or posterity.
-  data = {'train_truth': rates_train,
-          'valid_truth': rates_valid,
-          'input_train_truth' : input_train,
-          'input_valid_truth' : inputs_valid,
-          'train_data' : noisy_data_train,
-          'valid_data' : noisy_data_valid,
-          'train_percentage' : train_percentage,
-          'nreplications' : nreplications,
-          'dt' : rnn['dt'],
-          'input_magnitude' : input_magnitude,
-          'input_times_train' : input_times_train,
-          'input_times_valid' : input_times_valid,
-          'P_sxn' : P_sxn,
-          'condition_labels_train' : condition_labels_train,
-          'condition_labels_valid' : condition_labels_valid,
-          'conversion_factor': 1.0 / rnn['conversion_factor']}
-  datasets[dataset_name] = data
+    EXAMPLES = '''
+# Retrieve server certificate
+- iam_server_certificate_facts:
+    name: production-cert
+  register: server_cert
     
-    DATA_DIR = 'rnn_synth_data_v1.0'
+            if module.params.get('next_marker'):
+            params['Marker'] = module.params.get('next_marker')
     
-        # Split special chars at the start of word
-    will_split = True
-    while will_split:
-      will_split = False
-      for char in _START_SPECIAL_CHARS:
-        if word.startswith(char):
-          tokenized.append(char)
-          word = word[len(char):]
-          will_split = True
-    
-      wasserstein_loss = real_avg - fake_avg
-  return wasserstein_loss
-    
-          for s in xrange(t, FLAGS.sequence_length):
-        cum_advantage += reward_missing_list[s] * np.power(gamma, (s - t)) * (
-            rewards_list[s] - baselines[s])
-      # Clip advantages.
-      cum_advantage = tf.clip_by_value(cum_advantage, -FLAGS.advantage_clipping,
-                                       FLAGS.advantage_clipping)
-      advantages.append(reward_missing_list[t] * cum_advantage)
-      final_gen_objective += tf.multiply(
-          log_probability,
-          reward_missing_list[t] * tf.stop_gradient(cum_advantage))
-    
-      for var in variables:
-    clipped_var = tf.clip_by_value(var, c_lower, c_upper)
+    EXAMPLES = '''
+- heroku_collaborator:
+    api_key: YOUR_API_KEY
+    user: max.mustermann@example.com
+    apps: heroku-example-app
+    state: present
     
     
-def construct_ngrams_dict(ngrams_list):
-  '''Construct a ngram dictionary which maps an ngram tuple to the number
-  of times it appears in the text.'''
-  counts = {}
-    
-        ie_htmls = []
-    for ie in youtube_dl.list_extractors(age_limit=None):
-        ie_html = '<b>{}</b>'.format(ie.IE_NAME)
-        ie_desc = getattr(ie, 'IE_DESC', None)
-        if ie_desc is False:
-            continue
-        elif ie_desc is not None:
-            ie_html += ': {}'.format(ie.IE_DESC)
-        if not ie.working():
-            ie_html += ' (Currently broken)'
-        ie_htmls.append('<li>{}</li>'.format(ie_html))
-    
-    options = helptext[helptext.index('  General Options:') + 19:]
-options = re.sub(r'(?m)^  (\w.+)$', r'## \1', options)
-options = '# OPTIONS\n' + options + '\n'
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-                        # Pandoc's definition_lists. See http://pandoc.org/README.html
-                    # for more information.
-                    ret += '\n%s\n:   %s\n' % (option, description)
-                    continue
-            ret += line.lstrip() + '\n'
-        else:
-            ret += line + '\n'
-    
-    import io
-import re
-    
-            Keyword:                   'bold #004461',   # class: 'k'
-        Keyword.Constant:          'bold #004461',   # class: 'kc'
-        Keyword.Declaration:       'bold #004461',   # class: 'kd'
-        Keyword.Namespace:         'bold #004461',   # class: 'kn'
-        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
-        Keyword.Reserved:          'bold #004461',   # class: 'kr'
-        Keyword.Type:              'bold #004461',   # class: 'kt'
-    
-        @pytest.mark.parametrize('error', [IOError, OSError])
-    def test_super_len_handles_files_raising_weird_errors_in_tell(self, error):
-        '''If tell() raises errors, assume the cursor is at position zero.'''
-        class BoomFile(object):
-            def __len__(self):
-                return 5
-    
-            This function eventually generates a ``Cookie`` header from the
-        given cookies using cookielib. Due to cookielib's design, the header
-        will not be regenerated if it already exists, meaning this function
-        can only be called once for the life of the
-        :class:`PreparedRequest <PreparedRequest>` object. Any subsequent calls
-        to ``prepare_cookies`` will have no actual effect, unless the 'Cookie'
-        header is removed beforehand.
-        '''
-        if isinstance(cookies, cookielib.CookieJar):
-            self._cookies = cookies
-        else:
-            self._cookies = cookiejar_from_dict(cookies)
-    
-        :rtype: str
-    '''
-    scheme, netloc, path, params, query, fragment = urlparse(url, new_scheme)
+def protocol_to_string(protocol):
+    protocol = protocol_to_tuple(protocol)
+    if protocol[0] is True:
+        return 'Tcp'
+    elif protocol[1] is True:
+        return 'Udp'
+    elif protocol[2] is True:
+        return 'Icmp'
+    elif protocol[3] is True:
+        return 'Other'
+    elif protocol[4] is True:
+        return 'Any'
