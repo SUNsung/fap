98217478dfd15ea80ebf99f98af280ea40a8fa46
SUNsung <weatherius@gmail.com>
2019-06-07 22:29:13 +0300

    Rewrite [57u16gUxq1y657BGrPzNaat0Q57IR0IBKAq29Q57yJ.py] File in: 2019-06-07 22:29:12

--- pyton/57u16gUxq1y657BGrPzNaat0Q57IR0IBKAq29Q57yJ.py
+++ pyton/57u16gUxq1y657BGrPzNaat0Q57IR0IBKAq29Q57yJ.py
@@ -1,65 +1,60 @@
 
         
-            def set(self, results, query):
-        '''Set the result for the given query key in the cache.
+        
+class Message(object):
     
-        def __init__(self, vehicle_size, license_plate, spot_size):
-        self.vehicle_size = vehicle_size
-        self.license_plate = license_plate
-        self.spot_size
-        self.spots_taken = []
-    
-        def current_year_month(self):
-        '''Return the current year and month.'''
-        ...
-    
-        def _hash_function(self, key):
-        return key % self.size
-    
-        def get(self, query):
-        '''Get the stored query result from the cache.
-    
-        publicKey, privateKey = generateKey(keySize)
-    print('\nWriting public key to file %s_pubkey.txt...' % name)
-    with open('%s_pubkey.txt' % name, 'w') as fo:
-        fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
-    
-        def __prepare__(self, N = 0, M = 0):
-        self.dp = [[-1 for y in range(0,M)] for x in range(0,N)]
-    
-    def reformatHex(i):
-	'''[summary]
-	Converts the given integer into 8-digit hex number.
-    
-    min_length = 8
-max_length = 16
-password = ''.join(random.choice(chars) for x in range(random.randint(min_length, max_length)))
-print('Password: ' + password)
-print('[ If you are thinking of using this passsword, You better save it. ]')
-    
-        def __str__(self):
-        defaults = dict(type(self).__dict__)
-        actual = dict(defaults)
-        actual.update(self.__dict__)
-        actual['config'] = self.config
-        return repr_dict_nice(dict(
-            (key, value)
-            for key, value in actual.items()
-            if not key.startswith('_'))
-        )
-    
-            headers = [
-            '%s: %s' % (
-                name,
-                value if isinstance(value, str) else value.decode('utf8')
-            )
-            for name, value in headers.items()
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer),
+            self.mr(mapper=self.mapper_sort,
+                    reducer=self.reducer_identity),
         ]
     
+        def get(self, key):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                return item.value
+        raise KeyError('Key not found')
     
-FILE_PATH_ARG = patharg(FILE_PATH)
-BIN_FILE_PATH_ARG = patharg(BIN_FILE_PATH)
-JSON_FILE_PATH_ARG = patharg(JSON_FILE_PATH)
+            When updating an entry, updates its position to the front of the LRU list.
+        If the entry is new and the cache is at capacity, removes the oldest entry
+        before the new entry is added.
+        '''
+        node = self.map[query]
+        if node is not None:
+            # Key exists in cache, update the value
+            node.results = results
+            self.linked_list.move_to_front(node)
+        else:
+            # Key does not exist in cache
+            if self.size == self.MAX_SIZE:
+                # Remove the oldest entry from the linked list and lookup
+                self.lookup.pop(self.linked_list.tail.query, None)
+                self.linked_list.remove_from_tail()
+            else:
+                self.size += 1
+            # Add the new key and value
+            new_node = Node(query, results)
+            self.linked_list.append_to_front(new_node)
+            self.lookup[query] = new_node
+
+    
+            version = {
+            9: '0.9',
+            10: '1.0',
+            11: '1.1',
+            20: '2',
+        }[original.version]
+    
+    
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
     
     
 def test_default_options(httpbin):
@@ -69,97 +64,232 @@ def test_default_options(httpbin):
     r = http(httpbin.url + '/post', 'foo=bar', env=env)
     assert r.json['form'] == {'foo': 'bar'}
     
-        def test_POST_explicit_JSON_auto_JSON_accept(self, httpbin):
-        r = http('--json', 'POST', httpbin.url + '/post')
-        assert HTTP_OK in r
-        assert r.json['headers']['Accept'] == JSON_ACCEPT
-        # Make sure Content-Type gets set even with no data.
-        # https://github.com/jakubroztocil/httpie/issues/137
-        assert 'application/json' in r.json['headers']['Content-Type']
     
-            plugin = plugin_manager.get_auth_plugin(auth['type'])()
-    
-        def report_speed(self):
-    
-    import mock
-    
-    The JWS implementation in josepy only implements the base JOSE standard. In
-order to support the new header fields defined in ACME, this module defines some
-ACME-specific classes that layer on top of josepy.
-'''
-import josepy as jose
+def has_docutils():
+    try:
+        # noinspection PyUnresolvedReferences
+        import docutils
+        return True
+    except ImportError:
+        return False
     
     
-    def init_augeas(self):
-        ''' Initialize the actual Augeas instance '''
-        import augeas
-        self.aug = augeas.Augeas(
-            # specify a directory to load our preferred lens from
-            loadpath=constants.AUGEAS_LENS_DIR,
-            # Do not save backup (we do it ourselves), do not load
-            # anything by default
-            flags=(augeas.Augeas.NONE |
-                   augeas.Augeas.NO_MODL_AUTOLOAD |
-                   augeas.Augeas.ENABLE_SPAN))
-        # See if any temporary changes need to be recovered
-        # This needs to occur before VirtualHost objects are setup...
-        # because this will change the underlying configuration and potential
-        # vhosts
-        self.recovery_routine()
+def trim_filename_if_needed(filename, directory='.', extra=0):
+    max_len = get_filename_max_length(directory) - extra
+    if len(filename) > max_len:
+        filename = trim_filename(filename, max_len)
+    return filename
     
-    AUGEAS_LENS_DIR = pkg_resources.resource_filename(
-    'certbot_apache', 'augeas_lens')
-'''Path to the Augeas lens directory'''
+        centers, ret = k_means(data, 3)
+    # print(ret)
+
     
-    .. code-block:: bash
-   :caption: To acquire a certificate for ``example.com``, waiting 60 seconds
-             for DNS propagation
+        # 读取文件
+    for f in files_list:
+        txt = open(f).read()
+        # words = word_tokenize(txt)
+        words = tokenizer.tokenize(txt)
+        # creating inverted index data structure
+        for word in words:
+            word = word_clean(word)  # 单词清洗
+            if word not in index:
+                index[word] = {f}
+            else:
+                index[word].add(f)
     
-        s = score_euclidean(data[0], data[1])
-    print(s)
     
-            if n_dim == 2:
-            return tf.nn.softmax(x)
-        else:
-            e = tf.exp(x - tf.reduce_max(x, axis=axis, keepdims=True))
-            s = tf.reduce_sum(e, axis=axis, keepdims=True)
-            return e / s
+def clip_relu(x, max_value):
+    '''截断 ReLU
+    `o = min(max(0., x), max_value)`
     '''
-    return tf.nn.softmax(x, axis=axis)
+    o = tf.nn.relu(x)
+    o = tf.minimum(o, max_value)
+    return o
     
     
-def relu6(x):
-    '''
-    `o = min(max(x, 0), 6)`
-    '''
-    return tf.nn.relu6(x)
-    
-            if use_mean_attention:
-            a = tf.reduce_mean(a, axis=1)  # [batch_size, n_step]
-            a = tf.expand_dims(a, axis=1)  # [batch_size, 1, n_step]
-            a = tf.tile(a, [1, n_input, 1])  # [batch_size, n_input, n_step]
-    
-    References:
-    tensorlayer.layers.Conv2dLayer
-'''
-import tensorflow as tf
-    
-        如果需要 reuse 推荐使用类实现的 `Dense`
     
     
-def get_shape(x):
-    '''
-    References:
-        tflearn.utils.get_incoming_shape
-    '''
-    if isinstance(x, (tf.Tensor, tf.SparseTensor)):
-        return x.get_shape().as_list()
-    elif type(x) in [np.array, np.ndarray, list, tuple]:
-        return list(np.shape(x))
+def multi_highway_conv2d(x, kernel_size, n_layer,
+                         act_fn=relu,
+                         strides=1,
+                         padding='SAME',
+                         carry_bias=-1.0,
+                         name=None):
+    '''多层 highway_conv2d'''
+    if isinstance(kernel_size, int):
+        kernel_size = [kernel_size] * n_layer
+    
+        with tf.variable_scope(name or 'attention_flow', reuse=reuse):
+        h_expand = tf.tile(tf.expand_dims(h, axis=2), [1, 1, J, 1])  # [N, T, J, d]
+        u_expand = tf.tile(tf.expand_dims(u, axis=1), [1, T, 1, 1])  # [N, T, J, d]
+        hu = tf.multiply(h_expand, u_expand)  # [N, T, J, d]
+        h_u_hu = tf.concat([h_expand, u_expand, hu], axis=-1)  # [N, T, J, 3d]
+        W_s = get_w([3 * d, 1])  # [3d, 1]
+    
+    
+def foo():
+    print('foo')
+    
+    #A Python script to generate a single PDF document with all the tldr pages. It works by generating 
+#intermediate HTML files from existing md files using Python-markdown, applying desired formatting 
+#through CSS, and finally rendering them as PDF. There is no LaTeX dependency for generating the PDF.
+    
+    import six
+    
+    for i, backend in enumerate(backends):
+    for prefix, path in prefix_map.items():
+        if backend.startswith(prefix):
+            models = __import__(path, globals(), locals(), ['models'], level=0).models
+            if i == 0:
+                # If this is the first iteration of the loop, we need to
+                # emulate ``from x import *`` by copying the module contents
+                # into the local (module) scope. This follows the same rules as
+                # the import statement itself, as defined in the refrence docs:
+                # https://docs.python.org/2.7/reference/simple_stmts.html#import
+                if getattr(models, '__all__', None) is not None:
+                    predicate = lambda name: name in models.__all__
+                else:
+                    predicate = lambda name: not name.startswith('_')
+                locals().update({k: v for k, v in vars(models).items() if predicate(k)})
+            break
     else:
-        raise Exception('Invalid `x`.')
+        raise ImproperlyConfigured('Found unknown tagstore backend '%s'' % backend)
+
     
-    References:
-    https://www.tensorflow.org/api_guides/python/array_ops
-    keras.backend
-'''
+            # Changing field 'TagKey.environment_id'
+        db.alter_column(u'tagstore_tagkey', 'environment_id', self.gf(
+            'sentry.db.models.fields.bounded.BoundedBigIntegerField')(null=True))
+    
+    
+@instrumented_task(
+    name='sentry.tagstore.tasks.delete_tag_key',
+    queue='cleanup',
+    default_retry_delay=60 * 5,
+    max_retries=MAX_RETRIES
+)
+@retry(exclude=(DeleteAborted, ))
+def delete_tag_key(object_id, model, transaction_id=None, **kwargs):
+    from sentry import deletions
+    
+        def delete(self):
+        using = router.db_for_read(EventTag)
+        cursor = connections[using].cursor()
+        cursor.execute(
+            '''
+            DELETE FROM tagstore_eventtag
+            WHERE project_id = %s
+              AND id = %s
+        ''', [self.project_id, self.id]
+        )
+
+    
+        GroupResolution.objects.filter(id__in=[r.id for r in resolution_list]).update(
+        release=release,
+        type=GroupResolution.Type.in_release,
+        status=GroupResolution.Status.resolved,
+    )
+    
+    from datetime import timedelta
+from django.utils import timezone
+    
+    from time import time
+    
+        # Combine the heatmaps
+    if cfg.TEST.KPS_AUG.SCALE_SIZE_DEP:
+        heatmaps_c = combine_heatmaps_size_dep(
+            heatmaps_ts, ds_ts, us_ts, boxes, heur_f
+        )
+    else:
+        heatmaps_c = heur_f(heatmaps_ts)
+    
+                    for k in range(boxes.shape[0]):
+                    score = boxes[k, -1]
+                    mask = masks[:, :, k]
+                    pngname = os.path.join(
+                        'results',
+                        basename + '_' + clss + '_{}.png'.format(k))
+                    # write txt
+                    fid_txt.write('{} {} {}\n'.format(pngname, clss_id, score))
+                    # save mask
+                    cv2.imwrite(os.path.join(output_dir, pngname), mask * 255)
+    logger.info('Evaluating...')
+    cityscapes_eval.main([])
+    return None
+
+    
+    
+def _get_voc_results_file_template(json_dataset, salt):
+    info = voc_info(json_dataset)
+    year = info['year']
+    image_set = info['image_set']
+    devkit_path = info['devkit_path']
+    # VOCdevkit/results/VOC2007/Main/<comp_id>_det_test_aeroplane.txt
+    filename = 'comp4' + salt + '_det_' + image_set + '_{:s}.txt'
+    return os.path.join(devkit_path, 'results', 'VOC' + year, 'Main', filename)
+    
+    
+def _generate_anchors(base_size, scales, aspect_ratios):
+    '''Generate anchor (reference) windows by enumerating aspect ratios X
+    scales wrt a reference (0, 0, base_size - 1, base_size - 1) window.
+    '''
+    anchor = np.array([1, 1, base_size, base_size], dtype=np.float) - 1
+    anchors = _ratio_enum(anchor, aspect_ratios)
+    anchors = np.vstack(
+        [_scale_enum(anchors[i, :], scales) for i in range(anchors.shape[0])]
+    )
+    return anchors
+    
+        if not model.train or cfg.MODEL.FASTER_RCNN:
+        # Proposals are needed during:
+        #  1) inference (== not model.train) for RPN only and Faster R-CNN
+        #  OR
+        #  2) training for Faster R-CNN
+        # Otherwise (== training for RPN only), proposals are not needed
+        model.net.Sigmoid('rpn_cls_logits', 'rpn_cls_probs')
+        model.GenerateProposals(
+            ['rpn_cls_probs', 'rpn_bbox_pred', 'im_info'],
+            ['rpn_rois', 'rpn_roi_probs'],
+            anchors=anchors,
+            spatial_scale=spatial_scale
+        )
+    
+    logger = logging.getLogger(__name__)
+    
+    '''Construct minibatches for Detectron networks.'''
+    
+        @property
+    def output_params(self):
+        ''' FFMPEG Output parameters '''
+        codec = self.config['codec']
+        tune = self.config['tune']
+        # Force all frames to the same size
+        output_args = ['-vf', 'scale={}'.format(self.output_dimensions)]
+    
+        def check_transparency_format(self):
+        ''' Make sure that the output format is correct if draw_transparent is selected '''
+        transparent = self.config['draw_transparent']
+        if not transparent or (transparent and self.config['format'] == 'png'):
+            return
+        logger.warning('Draw Transparent selected, but the requested format does not support '
+                       'transparency. Changing output format to 'png'')
+        self.config['format'] = 'png'
+    
+        @staticmethod
+    def get_original_rotation_matrix(dimensions, angle):
+        ''' Calculate original rotation matrix and invert '''
+        logger.trace('Getting original rotation matrix: (dimensions: %s, angle: %s)',
+                     dimensions, angle)
+        height, width = dimensions
+        center = (width/2, height/2)
+        r_mat = cv2.getRotationMatrix2D(  # pylint: disable=no-member
+            center, -1.0 * angle, 1.)
+    
+            self.topwidget.wm_geometry('+%d+%d' % (xpos, ypos))
+    
+        @staticmethod
+    def getch():
+        ''' Returns a keyboard character after kbhit() has been called.
+            Should not be called in the same program as getarrow(). '''
+        if os.name == 'nt':
+            return msvcrt.getch().decode('utf-8')
+        return sys.stdin.read(1)
