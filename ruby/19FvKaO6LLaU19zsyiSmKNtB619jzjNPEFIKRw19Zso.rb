889d04544df760b614e8bac8ee77cd2037bf3e83
SUNsung <weatherius@gmail.com>
2019-06-08 17:04:49 +0300

    Rewrite [19FvKaO6LLaU19zsyiSmKNtB619jzjNPEFIKRw19Zso.rb] File in: 2019-06-08 17:04:49

--- ruby/19FvKaO6LLaU19zsyiSmKNtB619jzjNPEFIKRw19Zso.rb
+++ ruby/19FvKaO6LLaU19zsyiSmKNtB619jzjNPEFIKRw19Zso.rb
@@ -1,98 +1,116 @@
-    it 'no-ops on invalid values' do
-      previous = builder.build
-    
-        @omniauth.before_request_phase do |env|
-      # If the user is trying to reconnect to an existing account, store in session
-      request = ActionDispatch::Request.new(env)
-      request.session[:auth_reconnect] = !!request.params['reconnect']
+
+        
+            def release
+      context[:release]
     end
+    
+        def initialize
+      @pages = {}
+    end
+    
+        return str
   end
     
-      def self.cleanup!
-    DB.exec(<<~SQL)
-      DELETE FROM drafts
-       WHERE sequence < (
-        SELECT MAX(s.sequence)
-          FROM draft_sequences s
-         WHERE s.draft_key = drafts.draft_key
-           AND s.user_id = drafts.user_id
-      )
-    SQL
+                decrypted
+          end
     
-        def report_uri
-      '#{base_url}/csp_reports'
+              private
+    
+                res
+          end
+    
+    class FixPhotosShareVisibilities < ActiveRecord::Migration[4.2]
+  class Photo < ApplicationRecord
+  end
+    
+          it 'it doesn't call toggle_hidden_shareable' do
+        expect(@controller.current_user).not_to receive(:toggle_hidden_shareable).with(an_instance_of(StatusMessage))
+        begin
+          put :update, params: {id: 42, post_id: @status.id}, format: :js
+        rescue ActiveRecord::RecordNotFound
+        end
+      end
     end
   end
 end
 
     
-      private
+        streams.each do |stream_path, stream_class|
+      describe 'a GET to #{stream_path}' do
+        it 'assigns a stream of the proper class' do
+          get stream_path
+          expect(response).to be_success
+          expect(assigns[:stream]).to be_a stream_class
+        end
+      end
+    end
+  end
     
-          if data.blank?
-        render body: nil, status: 404
+    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
+  file_shared_path = TestApp.shared_path.join(file)
+  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
+  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
+end
+    
+    http://capistranorb.com/documentation/advanced-features/custom-scm
+    
+          def add_host(host, properties={})
+        new_host = Server[host]
+        new_host.port = properties[:port] if properties.key?(:port)
+        # This matching logic must stay in sync with `Server#matches?`.
+        key = ServerKey.new(new_host.hostname, new_host.port)
+        existing = servers_by_key[key]
+        if existing
+          existing.user = new_host.user if new_host.user
+          existing.with(properties)
+        else
+          servers_by_key[key] = new_host.with(properties)
+        end
+      end
+    
+          def assert_value_or_block_not_both(value, block)
+        return if value.nil? || block.nil?
+        raise Capistrano::ValidationError,
+              'Value and block both passed to Configuration#set'
+      end
+    
+    %i(git_strategy hg_strategy svn_strategy).each do |strategy|
+  validate(strategy) do |key, _value|
+    warn(
+      '[Deprecation Warning] #{key} is deprecated and will be removed in '\
+      'Capistrano 3.7.0.\n'\
+      'https://github.com/capistrano/capistrano/blob/master/UPGRADING-3.7.md'
+    )
+  end
+end
+    
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
+      end
+    end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
+    
+        def handle_gist_redirecting(data)
+      redirected_url = data.header['Location']
+      if redirected_url.nil? || redirected_url.empty?
+        raise ArgumentError, 'GitHub replied with a 302 but didn't provide a location in the response headers.'
+      end
+    
+      class IncludeArrayTag < Liquid::Tag
+    Syntax = /(#{Liquid::QuotedFragment}+)/
+    def initialize(tag_name, markup, tokens)
+      if markup =~ Syntax
+        @array_name = $1
       else
-        render plain: data.inspect, disposition: nil, content_type: 'text/plain'
+        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
       end
-    end
-  end
-end
-
-    
-      def log_off_user(session, cookies)
-    user = current_user
-    
-              lfs_objects.each do |object|
-            yield object
-          end
-        rescue StandardError => e
-          Rails.logger.error('The Lfs import process failed. #{e.message}')
-        end
-      end
-    end
-  end
-end
-
-    
-          # The name of the method to call to retrieve the data to import.
-      def collection_method
-        raise NotImplementedError
-      end
-    
-            # attributes - A hash containing the raw issue details. The keys of this
-        #              Hash (and any nested hashes) must be symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
-    
-      # We manage the result cache ourselves and the default of 10 minutes can be
-  # too low (particularly on Travis CI), causing results from some integration
-  # tests to be dropped. This causes random fluctuations in test coverage.
-  merge_timeout 86400
-    
-        def self.can_extract?(path)
-      mime_type = 'application/vnd.adobe.air-application-installer-package+zip'
-      path.magic_number.match?(/.{59}#{Regexp.escape(mime_type)}/)
-    end
-    
-    module RuboCop
-  module AST
-    # A node extension for `def` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `def` nodes within RuboCop.
-    class DefNode < Node
-      include ParameterizedNode
-      include MethodIdentifierPredicates
-    
-          def part_of_ignored_node?(node)
-        ignored_nodes.map(&:loc).any? do |ignored_loc|
-          if ignored_loc.expression.begin_pos > node.source_range.begin_pos
-            next false
-          end
-    
-            minimum_target_ruby_version 2.6
-    
-              if all_arguments.none?(&:referenced?)
-            proc_message = 'Also consider using a proc without arguments ' \
-                           'instead of a lambda if you want it ' \
-                           'to accept any arguments but don't care about them.'
-          end
