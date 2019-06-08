8e42c1be9244eb572fae04a45a9ecafeba5a4e2f
SUNsung <weatherius@gmail.com>
2019-06-08 19:03:28 +0300

    Rewrite [36a8pMjLF8IOo36gxR5qKcrhbs36D7av6TH9Bww36Jex.py] File in: 2019-06-08 19:03:27

--- pyton/36a8pMjLF8IOo36gxR5qKcrhbs36D7av6TH9Bww36Jex.py
+++ pyton/36a8pMjLF8IOo36gxR5qKcrhbs36D7av6TH9Bww36Jex.py
@@ -1,59 +1,79 @@
 
         
-        site_info = 'CBS.com'
-download = cbs_download
-download_playlist = playlist_not_supported('cbs')
-
+        from setuptools import setup, find_packages
+setup(
+    name = proj_info['name'],
+    version = VERSION,
     
-    #----------------------------------------------------------------------
-#helper
-#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python
-def dictify(r,root=True):
-    if root:
-        return {r.tag : dictify(r, False)}
-    d=copy(r.attrib)
-    if r.text:
-        d['_text']=r.text
-    for x in r.findall('./*'):
-        if x.tag not in d:
-            d[x.tag]=[]
-        d[x.tag].append(dictify(x,False))
-    return d
+    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
+    title = r1(r'<title>(.*)</title>', html)
+    assert title
+    id = r1(r'flvid\s*=\s*(\d+)', html)
+    assert id
+    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
     
-    # image size suffixes used in inline json 'key' field
-# listed in descending order
-size_suffixes = ['o', 'k', 'h', 'l', 'c', 'z', 'm', 'n', 's', 't', 'q', 'sq']
+        def prepare(self, **kwargs):
     
-        @classmethod
-    def checksum(cls, sha1_str):
-        if len(sha1_str) != 41:
-            return False
-        if not re.match(r'[0-9A-Za-z]{41}', sha1_str):
-            return False
-        sha1 = sha1_str[:-1]
-        if (15 & sum([int(char, 16) for char in sha1])) == int(sha1_str[-1], 16):
-            return True
-        return False
     
-        url = list(set([
-        unicodize(str.replace(i, '\\/', '/'))
-        for i in re.findall(r'<meta property='og:video:secure_url' content='(.*?)'>', html)
-    ]))
+class CNTV(VideoExtractor):
+    name = 'CNTV.com'
+    stream_types = [
+        {'id': '1', 'video_profile': '1280x720_2000kb/s', 'map_to': 'chapters4'},
+        {'id': '2', 'video_profile': '1280x720_1200kb/s', 'map_to': 'chapters3'},
+        {'id': '3', 'video_profile': '640x360_850kb/s', 'map_to': 'chapters2'},
+        {'id': '4', 'video_profile': '480x270_450kb/s', 'map_to': 'chapters'},
+        {'id': '5', 'video_profile': '320x180_200kb/s', 'map_to': 'lowChapters'},
+    ]
     
-            else:
-            # gallery image
-            content = get_content(self.url)
-            image = json.loads(match1(content, r'image\s*:\s*({.*}),'))
-            ext = image['ext']
+        for quality in ['1080','720','480','380','240','144','auto']:
+        try:
+            real_url = info[quality][1]['url']
+            if real_url:
+                break
+        except KeyError:
+            pass
+    
+    	type, ext, size = url_info(url)
+	print_info(site_info, title, type, size)
+	
+	if not info_only:
+		download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+    
+def get_m3u8_url(stream_id):
+    return 'http://live-ws.huomaotv.cn/live/%s/playlist.m3u8' % stream_id
+    
+        filename = '%s.%s' % (title, ext)
+    filepath = os.path.join(output_dir, filename)
+    if not force and os.path.exists(filepath):
+        print('Skipping {}: file already exists\n'.format(filepath))
+        return
+    bar = SimpleProgressBar(total_size, 1)
+    print('Downloading %s ...' % tr(filename))
+    url_save_icourses(url, filepath, bar, total_size, headers=headers, **kwargs)
+    bar.done()
+    
+    def ifeng_download_by_id(id, title = None, output_dir = '.', merge = True, info_only = False):
+    assert r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', id), id
+    url = 'http://vxml.ifengimg.com/video_info_new/%s/%s/%s.xml' % (id[-2], id[-2:], id)
+    xml = get_html(url, 'utf-8')
+    title = r1(r'Name='([^']+)'', xml)
+    title = unescape_html(title)
+    url = r1(r'VideoPlayUrl='([^']+)'', xml)
+    from random import randint
+    r = randint(10, 19)
+    url = url.replace('http://wideo.ifeng.com/', 'http://ips.ifeng.com/wideo.ifeng.com/')
+    type, ext, size = url_info(url)
+    
+            elif re.search(r'i\.imgur\.com/', self.url):
+            # direct image
+            _, container, size = url_info(self.url)
             self.streams = {
                 'original': {
-                    'src': ['http://i.imgur.com/%s%s' % (image['hash'], ext)],
-                    'size': image['size'],
-                    'container': ext[1:]
-                },
-                'thumbnail': {
-                    'src': ['http://i.imgur.com/%ss%s' % (image['hash'], '.jpg')],
-                    'container': 'jpg'
+                    'src': [self.url],
+                    'size': size,
+                    'container': container
                 }
             }
-            self.title = image['title'] or image['hash']
+            self.title = r1(r'i\.imgur\.com/([^./]*)', self.url)
