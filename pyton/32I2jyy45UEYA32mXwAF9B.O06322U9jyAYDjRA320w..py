7ec220c81cc6a54ea4c25cbf93189f49e65cc36d
SUNsung <weatherius@gmail.com>
2019-06-08 16:03:28 +0300

    Rewrite [32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py] File in: 2019-06-08 16:03:28

--- pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
+++ pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
@@ -1,282 +1,59 @@
 
         
-            Args:
-      inputs: A 2D batch x input_dim tensor of inputs.
-      state: The previous state from the last time step.
-      scope (optional): TF variable scope for defined GRU variables.
+        
+def to_native_string(string, encoding='ascii'):
+    '''Given a string object, regardless of type, returns a representation of
+    that string in the native string type, encoding and decoding where
+    necessary. This assumes ASCII unless told otherwise.
+    '''
+    if isinstance(string, builtin_str):
+        out = string
+    else:
+        if is_py2:
+            out = string.encode(encoding)
+        else:
+            out = string.decode(encoding)
     
-    flags = tf.app.flags
-flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
-                    'Directory for saving data.')
-flags.DEFINE_string('datafile_name', 'thits_data',
-                    'Name of data file for input case.')
-flags.DEFINE_string('noise_type', 'poisson', 'Noise type for data.')
-flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
-flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
-flags.DEFINE_integer('C', 100, 'Number of conditions')
-flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
-flags.DEFINE_integer('S', 50, 'Number of sampled units from RNN')
-flags.DEFINE_integer('npcs', 10, 'Number of PCS for multi-session case.')
-flags.DEFINE_float('train_percentage', 4.0/5.0,
-                   'Percentage of train vs validation trials')
-flags.DEFINE_integer('nreplications', 40,
-                     'Number of noise replications of the same underlying rates.')
-flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
-flags.DEFINE_float('x0_std', 1.0,
-                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
-flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
-flags.DEFINE_float('dt', 0.010, 'Time bin')
-flags.DEFINE_float('input_magnitude', 20.0,
-                   'For the input case, what is the value of the input?')
-flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
-FLAGS = flags.FLAGS
+        def __init__(self, *args, **kwargs):
+        '''Initialize RequestException with `request` and `response` objects.'''
+        response = kwargs.pop('response', None)
+        self.response = response
+        self.request = kwargs.pop('request', None)
+        if (response is not None and not self.request and
+                hasattr(response, 'request')):
+            self.request = self.response.request
+        super(RequestException, self).__init__(*args, **kwargs)
     
-    flags = tf.app.flags
-flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
-                    'Directory for saving data.')
-flags.DEFINE_string('datafile_name', 'itb_rnn',
-                    'Name of data file for input case.')
-flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
-flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
-flags.DEFINE_integer('C', 800, 'Number of conditions')
-flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
-flags.DEFINE_float('train_percentage', 4.0/5.0,
-                   'Percentage of train vs validation trials')
-flags.DEFINE_integer('nreplications', 5,
-                     'Number of spikifications of the same underlying rates.')
-flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
-flags.DEFINE_float('dt', 0.010, 'Time bin')
-flags.DEFINE_float('max_firing_rate', 30.0,
-                   'Map 1.0 of RNN to a spikes per second')
-flags.DEFINE_float('u_std', 0.25,
-                   'Std dev of input to integration to bound model')
-flags.DEFINE_string('checkpoint_path', 'SAMPLE_CHECKPOINT',
-                    '''Path to directory with checkpoints of model
-                    trained on integration to bound task. Currently this
-                    is a placeholder which tells the code to grab the
-                    checkpoint that is provided with the code
-                    (in /trained_itb/..). If you have your own checkpoint
-                    you would like to restore, you would point it to
-                    that path.''')
-FLAGS = flags.FLAGS
-    
-    rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
-rnn_rngs = [np.random.RandomState(seed=FLAGS.synth_data_seed+1),
-            np.random.RandomState(seed=FLAGS.synth_data_seed+2)]
-T = FLAGS.T
-C = FLAGS.C
-N = FLAGS.N
-nreplications = FLAGS.nreplications
-E = nreplications * C
-train_percentage = FLAGS.train_percentage
-ntimesteps = int(T / FLAGS.dt)
-    
-    FLAGS = tf.flags.FLAGS
-# General flags.
-tf.flags.DEFINE_string('mode', 'eval',
-                       'One of [sample, eval, dump_emb, dump_lstm_emb]. '
-                       ''sample' mode samples future word predictions, using '
-                       'FLAGS.prefix as prefix (prefix could be left empty). '
-                       ''eval' mode calculates perplexity of the '
-                       'FLAGS.input_data. '
-                       ''dump_emb' mode dumps word and softmax embeddings to '
-                       'FLAGS.save_dir. embeddings are dumped in the same '
-                       'order as words in vocabulary. All words in vocabulary '
-                       'are dumped.'
-                       'dump_lstm_emb dumps lstm embeddings of FLAGS.sentence '
-                       'to FLAGS.save_dir.')
-tf.flags.DEFINE_string('pbtxt', '',
-                       'GraphDef proto text file used to construct model '
-                       'structure.')
-tf.flags.DEFINE_string('ckpt', '',
-                       'Checkpoint directory used to fill model values.')
-tf.flags.DEFINE_string('vocab_file', '', 'Vocabulary file.')
-tf.flags.DEFINE_string('save_dir', '',
-                       'Used for 'dump_emb' mode to save word embeddings.')
-# sample mode flags.
-tf.flags.DEFINE_string('prefix', '',
-                       'Used for 'sample' mode to predict next words.')
-tf.flags.DEFINE_integer('max_sample_words', 100,
-                        'Sampling stops either when </S> is met or this number '
-                        'of steps has passed.')
-tf.flags.DEFINE_integer('num_samples', 3,
-                        'Number of samples to generate for the prefix.')
-# dump_lstm_emb mode flags.
-tf.flags.DEFINE_string('sentence', '',
-                       'Used as input for 'dump_lstm_emb' mode.')
-# eval mode flags.
-tf.flags.DEFINE_string('input_data', '',
-                       'Input data files for eval model.')
-tf.flags.DEFINE_integer('max_eval_steps', 1000000,
-                        'Maximum mumber of steps to run 'eval' mode.')
+        :param cookie_dict: Dict of key/values to insert into CookieJar.
+    :param cookiejar: (optional) A cookiejar to add the cookies to.
+    :param overwrite: (optional) If False, will not replace cookies
+        already in the jar with new ones.
+    :rtype: CookieJar
+    '''
+    if cookiejar is None:
+        cookiejar = RequestsCookieJar()
     
     
-def evaluate_ensemble(test_data_name, number_of_lms):
-  ensemble = EnsembleLM(test_data_name)
-  model_list = ['lm{:02d}'.format(i+1) for i in range(number_of_lms)]
-  for model_name in model_list:
-    ensemble.add_single_model(model_name)
-  accuracy = ensemble.evaluate()
-  print('Accuracy of {} LM(s) on {} = {}'.format(
-      number_of_lms, test_data_name, accuracy))
+def test_chunked_upload():
+    '''can safely send generators'''
+    close_server = threading.Event()
+    server = Server.basic_response_server(wait_to_close_event=close_server)
+    data = iter([b'a', b'b', b'c'])
     
-    import tensorflow as tf
-    
-      Returns:
-    variable_mapping:  Dictionary with Key: ckpt_name, Value: model_varself.
-  '''
-  assert FLAGS.discriminator_model == 'seq2seq_vd'
-  assert hparams.dis_num_layers == 2
-    
-          done: `None`, which is used by the `dynamic_rnn_decoder` to indicate
-      that `sequence_lengths` in `dynamic_rnn_decoder` should be used.
-    
-    with open('update/LATEST_VERSION', 'w') as f:
-    f.write(version)
-    
-    
-# find the correct sorting and add the required base classes so that sublcasses
-# can be correctly created
-classes = _ALL_CLASSES[:-1]
-ordered_cls = []
-while classes:
-    for c in classes[:]:
-        bases = set(c.__bases__) - set((object, InfoExtractor, SearchInfoExtractor))
-        stop = False
-        for b in bases:
-            if b not in classes and b not in ordered_cls:
-                if b.__name__ == 'GenericIE':
-                    exit()
-                classes.insert(0, b)
-                stop = True
-        if stop:
-            break
-        if all(b in ordered_cls for b in bases):
-            ordered_cls.append(c)
-            classes.remove(c)
-            break
-ordered_cls.append(_ALL_CLASSES[-1])
-    
-        def gen_ies_md(ies):
-        for ie in ies:
-            ie_md = '**{0}**'.format(ie.IE_NAME)
-            ie_desc = getattr(ie, 'IE_DESC', None)
-            if ie_desc is False:
-                continue
-            if ie_desc is not None:
-                ie_md += ': {0}'.format(ie.IE_DESC)
-            if not ie.working():
-                ie_md += ' (Currently broken)'
-            yield ie_md
-    
-    # The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-from youtube_dl.version import __version__
-version = __version__
-# The full version, including alpha/beta/rc tags.
-release = version
-    
-            def try_rm_tcs_files(tcs=None):
-            if tcs is None:
-                tcs = test_cases
-            for tc in tcs:
-                tc_filename = get_tc_filename(tc)
-                try_rm(tc_filename)
-                try_rm(tc_filename + '.part')
-                try_rm(os.path.splitext(tc_filename)[0] + '.info.json')
-        try_rm_tcs_files()
-        try:
-            try_num = 1
-            while True:
-                try:
-                    # We're not using .download here since that is just a shim
-                    # for outside error handling, and returns the exit code
-                    # instead of the result dict.
-                    res_dict = ydl.extract_info(
-                        test_case['url'],
-                        force_generic_extractor=params.get('force_generic_extractor', False))
-                except (DownloadError, ExtractorError) as err:
-                    # Check if the exception is not a network related one
-                    if not err.exc_info[0] in (compat_urllib_error.URLError, socket.timeout, UnavailableVideoError, compat_http_client.BadStatusLine) or (err.exc_info[0] == compat_HTTPError and err.exc_info[1].code == 503):
-                        raise
-    
-    
-@pytest.mark.parametrize(
-    ('username', 'password', 'message'),
-    (
-        ('', '', b'Username is required.'),
-        ('a', '', b'Password is required.'),
-        ('test', 'test', b'already registered'),
-    ),
-)
-def test_register_validate_input(client, username, password, message):
-    response = client.post(
-        '/auth/register', data={'username': username, 'password': password}
-    )
-    assert message in response.data
-    
-    
-def test_init_db_command(runner, monkeypatch):
-    class Recorder(object):
-        called = False
-    
-            buf.append(
-            '  Make sure to directly send your %s-request to this URL '
-            'since we can't make browsers or HTTP clients redirect '
-            'with form data reliably or without user interaction.' % request.method
-        )
-        buf.append('\n\nNote: this exception is only raised in debug mode')
-        AssertionError.__init__(self, ''.join(buf).encode('utf-8'))
-    
-    import random
-    
-    from .hash_table import HashTable
-    
-    This is a pure Python implementation of Dynamic Programming solution to the edit distance problem.
-    
-        def solve_sub_array(self):
-        rear = [int(self.array[0])]*len(self.array)
-        sum_value = [int(self.array[0])]*len(self.array)
-        for i in range(1, len(self.array)):
-            sum_value[i] = max(int(self.array[i]) + sum_value[i-1], int(self.array[i]))
-            rear[i] = max(sum_value[i], rear[i-1])
-        return rear[len(self.array)-1]
-    
-                if (arr[i-1] <= j):
-                dp[i][j] = dp[i][j] or dp[i-1][j-arr[i-1]]
-    
-    import numpy as np
-    
-    IMPORT_MAPPING.update({
-    'cPickle': 'pickle',
-    '_elementtree': 'xml.etree.ElementTree',
-    'FileDialog': 'tkinter.filedialog',
-    'SimpleDialog': 'tkinter.simpledialog',
-    'DocXMLRPCServer': 'xmlrpc.server',
-    'SimpleHTTPServer': 'http.server',
-    'CGIHTTPServer': 'http.server',
-    # For compatibility with broken pickles saved in old Python 3 versions
-    'UserDict': 'collections',
-    'UserList': 'collections',
-    'UserString': 'collections',
-    'whichdb': 'dbm',
-    'StringIO':  'io',
-    'cStringIO': 'io',
-})
-    
-    '''Send the contents of a directory as a MIME message.'''
-    
-        print('-' * 20)
-    
-    def calculate(func, args):
-    result = func(*args)
-    return '%s says that %s%s = %s' % (
-        multiprocessing.current_process().name,
-        func.__name__, args, result
+        def test_text_response(self):
+        '''the text_response_server sends the given text'''
+        server = Server.text_response_server(
+            'HTTP/1.1 200 OK\r\n' +
+            'Content-Length: 6\r\n' +
+            '\r\nroflol'
         )
     
-    cur.close()
-con.close()
+    
+def iter_slices(string, slice_length):
+    '''Iterate over slices of a string.'''
+    pos = 0
+    if slice_length is None or slice_length <= 0:
+        slice_length = len(string)
+    while pos < len(string):
+        yield string[pos:pos + slice_length]
+        pos += slice_length
