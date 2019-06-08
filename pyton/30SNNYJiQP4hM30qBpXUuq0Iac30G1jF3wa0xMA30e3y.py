16c4296fbd20fff49aeb885b21941b7bb68639a1
SUNsung <weatherius@gmail.com>
2019-06-08 14:03:48 +0300

    Rewrite [30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py] File in: 2019-06-08 14:03:48

--- pyton/30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py
+++ pyton/30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py
@@ -1,78 +1,56 @@
 
         
-            def handle_budget_notifications(self, key, total):
-        '''Call notification API if nearing or exceeded budget.'''
-        ...
+        GRADE_COLOR_1 = COLOR_HEADS = RED_E
+GRADE_COLOR_2 = COLOR_TAILS = BLUE_C
     
-        def set(self, key, value):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                item.value = value
-                return
-        self.table[hash_index].append(Item(key, value))
+    class Dodecahedron(Mobject1D):
+    def generate_points(self):
+        phi = (1 + np.sqrt(5)) / 2
+        x = np.array([1, 0, 0])
+        y = np.array([0, 1, 0])
+        z = np.array([0, 0, 1])
+        v1, v2 = (phi, 1/phi, 0), (phi, -1/phi, 0)
+        vertex_pairs = [
+            (v1, v2),
+            (x+y+z, v1),
+            (x+y-z, v1),
+            (x-y+z, v2),
+            (x-y-z, v2),
+        ]
+        five_lines_points = Mobject(*[
+            Line(pair[0], pair[1], density = 1.0/self.epsilon)
+            for pair in vertex_pairs
+        ]).points
+        #Rotate those 5 edges into all 30.
+        for i in range(3):
+            perm = [j%3 for j in range(i, i+3)]
+            for b in [-1, 1]:
+                matrix = b*np.array([x[perm], y[perm], z[perm]])
+                self.add_points(np.dot(five_lines_points, matrix))
+        self.pose_at_angle()
+        self.set_color(GREEN)
     
-        def __init__(self, url, contents, child_urls):
-        self.url = url
-        self.contents = contents
-        self.child_urls = child_urls
-        self.signature = self.create_signature()
+        def coords_to_center(self, n, k):
+        x_offset = self.cell_width * (k + self.nrows / 2.0 - n / 2.0)
+        y_offset = self.cell_height * (self.nrows - n)
+        return self.bottom_left + x_offset * RIGHT + y_offset * UP
     
-    ward = AgglomerativeClustering(n_clusters=3, linkage='ward')
-    
-        # TASK: Predict the outcome on the testing set and store it in a variable
-    # named y_predicted
-    
-    # Anisotropicly distributed data
-transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
-X_aniso = np.dot(X, transformation)
-y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_aniso)
-    
-    from sklearn.cluster import MiniBatchKMeans, KMeans
-from sklearn.metrics.pairwise import pairwise_distances_argmin
-from sklearn.datasets.samples_generator import make_blobs
-    
-    
-class Resource(jose.Field):
-    '''Resource MITM field.'''
-    
-    # -- Options for LaTeX output ---------------------------------------------
-    
-    latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-    }
-    
-    '''
-
-    
-    for i, face_distance in enumerate(face_distances):
-    print('The test image has a distance of {:.2} from known image #{}'.format(face_distance, i))
-    print('- With a normal cutoff of 0.6, would the test image match the known image? {}'.format(face_distance < 0.6))
-    print('- With a very strict cutoff of 0.5, would the test image match the known image? {}'.format(face_distance < 0.5))
-    print()
-
-    
-            if file and allowed_file(file.filename):
-            # The image file seems valid! Detect faces and return the result.
-            return detect_faces_in_image(file)
+        def add_braces_and_labels(self):
+        for attr in 'horizontal_parts', 'vertical_parts':
+            if not hasattr(self, attr):
+                continue
+            parts = getattr(self, attr)
+            for subattr in 'braces', 'labels':
+                if hasattr(parts, subattr):
+                    self.add(getattr(parts, subattr))
     
     
-def print_result(filename, name, distance, show_distance=False):
-    if show_distance:
-        print('{},{},{}'.format(filename, name, distance))
-    else:
-        print('{},{}'.format(filename, name))
+def random_bright_color():
+    color = random_color()
+    curr_rgb = color_to_rgb(color)
+    new_rgb = interpolate(
+        curr_rgb, np.ones(len(curr_rgb)), 0.5
+    )
+    return Color(rgb=new_rgb)
     
-        # Print the location of each facial feature in this image
-    for facial_feature in face_landmarks.keys():
-        print('The {} in this face has the following points: {}'.format(facial_feature, face_landmarks[facial_feature]))
-    
-    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-    # Find all the faces and face encodings in the unknown image
-face_locations = face_recognition.face_locations(unknown_image)
-face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+            self.assertEqual(response.status_code, 405)
