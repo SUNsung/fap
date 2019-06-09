9617da0d9fd333726c78272456b0f3a2171e5820
SUNsung <weatherius@gmail.com>
2019-06-09 23:37:33 +0300

    Rewrite [29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb] File in: 2019-06-09 23:37:33

--- ruby/29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb
+++ ruby/29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb
@@ -1,100 +1,138 @@
 
         
-                  current_range = declaration_with_comment(node)
-          previous_range = declaration_with_comment(previous_declaration)
-    
-              ignored_end_pos = if ignored_loc.respond_to?(:heredoc_body)
-                              ignored_loc.heredoc_end.end_pos
-                            else
-                              ignored_loc.expression.end_pos
-                            end
-          ignored_end_pos >= node.source_range.end_pos
+                def initialize(object_name, method_name, template_object, checked_value, unchecked_value, options)
+          @checked_value   = checked_value
+          @unchecked_value = unchecked_value
+          super(object_name, method_name, template_object, options)
         end
+    
+              def render_component(builder)
+            builder.radio_button + builder.label
+          end
       end
-    
-            def_node_matcher :erb_new_with_non_keyword_arguments, <<-PATTERN
-          (send
-            (const {nil? cbase} :ERB) :new $...)
-        PATTERN
-    
-    # test/spec/mini 3
-# http://gist.github.com/25455
-# chris@ozmm.org
-# file:lib/test/spec/mini.rb
-def context(*args, &block)
-  return super unless (name = args.first) && block
-  require 'test/unit'
-  klass = Class.new(defined?(ActiveSupport::TestCase) ? ActiveSupport::TestCase : Test::Unit::TestCase) do
-    def self.test(name, &block)
-      define_method('test_#{name.gsub(/\W/, '_')}', &block) if block
     end
-    
-    context 'Precious::Views::Editing' do
-  include Rack::Test::Methods
-  setup do
-    examples = testpath 'examples'
-    @path    = File.join(examples, 'test.git')
-    Precious::App.set(:gollum_path, @path)
-    FileUtils.cp_r File.join(examples, 'revert.git'), @path, :remove_destination => true
-    @wiki = Gollum::Wiki.new(@path)
   end
-    
-      setup do
-    @path = cloned_testpath('examples/revert.git')
-    @wiki = Gollum::Wiki.new(@path)
-    Precious::App.set(:gollum_path, @path)
-    Precious::App.set(:wiki_options, {allow_editing: true})
-  end
-    
-      test 'extracting paths from URLs' do
-    assert_nil extract_path('Eye-Of-Sauron')
-    assert_equal 'Mordor', extract_path('Mordor/Sauron')
-    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
-  end
-    
-    context 'Frontend Unicode support' do
-  include Rack::Test::Methods
-    
-    $redis = Redis.new
-    
-      puts '\n== Removing old logs and tempfiles =='
-  system! 'bin/rails log:clear tmp:clear'
-    
-      # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
 
     
-      # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+        str = str.dup
     
-        # Initializes a new CategoryFeed.
-    #
-    #  +base+         is the String path to the <source>.
-    #  +category_dir+ is the String path between <source> and the category folder.
-    #  +category+     is the category currently being processed.
-    def initialize(site, base, category_dir, category)
-      @site = site
-      @base = base
-      @dir  = category_dir
-      @name = 'atom.xml'
-      self.process(@name)
-      # Read the YAML data from the layout page.
-      self.read_yaml(File.join(base, '_includes/custom'), 'category_feed.xml')
-      self.data['category']    = category
-      # Set the title for this page.
-      title_prefix             = site.config['category_title_prefix'] || 'Category: '
-      self.data['title']       = '#{title_prefix}#{category}'
-      # Set the meta-description for this page.
-      meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
-      self.data['description'] = '#{meta_description_prefix}#{category}'
+          it 'should not be fooled by 10 local code signing identities available' do
+        allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
+        allow(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return('     10 valid identities found\n')
+        expect(FastlaneCore::UI).not_to(receive(:error))
     
-      class IncludeArrayTag < Liquid::Tag
-    Syntax = /(#{Liquid::QuotedFragment}+)/
-    def initialize(tag_name, markup, tokens)
-      if markup =~ Syntax
-        @array_name = $1
-      else
-        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
+    # The * turns the array into a parameter list
+# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
+# We need to use that, because otherwise invocations like
+# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
+exec(*exec_arr)
+
+    
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
       end
+    end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
+  end
+end
+
+    
+    group :development do
+  cp_gem 'claide',                'CLAide'
+  cp_gem 'cocoapods-core',        'Core'
+  cp_gem 'cocoapods-deintegrate', 'cocoapods-deintegrate'
+  cp_gem 'cocoapods-downloader',  'cocoapods-downloader'
+  cp_gem 'cocoapods-plugins',     'cocoapods-plugins'
+  cp_gem 'cocoapods-search',      'cocoapods-search'
+  cp_gem 'cocoapods-stats',       'cocoapods-stats'
+  cp_gem 'cocoapods-trunk',       'cocoapods-trunk'
+  cp_gem 'cocoapods-try',         'cocoapods-try'
+  cp_gem 'molinillo',             'Molinillo'
+  cp_gem 'nanaimo',               'Nanaimo'
+  cp_gem 'xcodeproj',             'Xcodeproj'
+    
+    Gem::Specification.new do |gem|
+  gem.name          = 'capistrano'
+  gem.version       = Capistrano::VERSION
+  gem.authors       = ['Tom Clements', 'Lee Hambley']
+  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
+  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
+  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
+  gem.homepage      = 'http://capistranorb.com/'
+    
+        def exit_because_of_exception(ex)
+      if respond_to?(:deploying?) && deploying?
+        exit_deploy_because_of_exception(ex)
+      else
+        super
+      end
+    end
+    
+            if Rake::Task.task_defined?('deploy:check')
+          before 'deploy:check', '#{scm_name}:check'
+        end
+    
+          def ignored_node?(node)
+        # Same object found in array?
+        ignored_nodes.any? { |n| n.equal?(node) }
+      end
+    
+    module RuboCop
+  module Cop
+    module Lint
+      # This cop checks for unused block arguments.
+      #
+      # @example
+      #
+      #   # bad
+      #
+      #   do_something do |used, unused|
+      #     puts used
+      #   end
+      #
+      #   do_something do |bar|
+      #     puts :foo
+      #   end
+      #
+      #   define_method(:foo) do |bar|
+      #     puts :baz
+      #   end
+      #
+      # @example
+      #
+      #   #good
+      #
+      #   do_something do |used, _unused|
+      #     puts used
+      #   end
+      #
+      #   do_something do
+      #     puts :foo
+      #   end
+      #
+      #   define_method(:foo) do |_bar|
+      #     puts :baz
+      #   end
+      class UnusedBlockArgument < Cop
+        include UnusedArgument
+    
+      context 'hook value is Array' do
+    before do
+      project.yaml[hook_name] = [
+        'echo 'on hook'',
+        'echo 'another command here''
+      ]
+    end
+    
+      return captured_stdout.string, captured_stderr.string
+ensure
+  $stdout = orig_stdout
+  $stderr = orig_stderr
+end
