7fda75f5d4c67cd65afa8a8c9e77270e8afdba4b
SUNsung <weatherius@gmail.com>
2019-06-09 11:36:49 +0300

    Rewrite [12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb] File in: 2019-06-09 11:36:49

--- ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
+++ ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
@@ -1,18 +1,44 @@
 
         
-                def initialize(object_name, method_name, template_object, checked_value, unchecked_value, options)
-          @checked_value   = checked_value
-          @unchecked_value = unchecked_value
-          super(object_name, method_name, template_object, options)
-        end
+        #############################################################################
+#
+# Standard tasks
+#
+#############################################################################
     
-    module Gitlab
-  module QueryLimiting
-    # Middleware for reporting (or raising) when a request performs more than a
-    # certain amount of database queries.
-    class Middleware
-      CONTROLLER_KEY = 'action_controller.instance'.freeze
-      ENDPOINT_KEY = 'api.endpoint'.freeze
+    # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+    
+        def process(args)
+      arg_is_present? args, '--server', 'The --server command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
+                          use the 'build' subcommand.'
+      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
+                          '--watch'.'
+      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
+                          the '--watch' switch.'
+      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
+                          favour of 'highlighter'.'
+      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
+                          your config files.'
+      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
+                          config files.'
+      no_subcommand(args)
+    end
+    
+    module Jekyll
+  module Drops
+    class UnifiedPayloadDrop < Drop
+      mutable true
     
           it 'requires the passwords to match when changing them' do
         visit edit_admin_user_path(users(:bob))
@@ -23,124 +49,25 @@
       end
     end
     
-        let!(:bob_weather_agent) {
-      agents(:bob_weather_agent)
-    }
-    
-      it 'imports a scenario which requires a service' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'spec/data_fixtures/twitter_scenario.json'))
-    click_on 'Start Import'
-    check('I confirm that I want to import these Agents.')
-    expect { click_on 'Finish Import' }.to change(Scenario, :count).by(1)
-    expect(page).to have_text('Import successful!')
-  end
-end
-
-    
-        it 'defauls foreground and background colors' do
-      scenario.tag_fg_color = nil
-      scenario.tag_bg_color = nil
-      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
-    end
-  end
-    
-        it 'respects an environment variable that specifies a path or URL to a different scenario' do
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
+        it 'returns a label 'Missing Gems' if a given agent has dependencies missing' do
+      stub(@agent).dependencies_missing? { true }
+      label = working(@agent)
+      expect(label).to be_html_safe
+      expect(Nokogiri(label).text).to eq 'Missing Gems'
     end
     
-          expect(Utils.unindent('Hello\n  I am indented')).to eq('Hello\n  I am indented')
-    
-      describe '#log_for_agent' do
-    it 'creates AgentLogs' do
-      log = AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 4, :outbound_event => events(:jane_website_agent_event))
-      expect(log).not_to be_new_record
-      expect(log.agent).to eq(agents(:jane_website_agent))
-      expect(log.outbound_event).to eq(events(:jane_website_agent_event))
-      expect(log.message).to eq('some message')
-      expect(log.level).to eq(4)
+        it 'works for running jobs' do
+      job.locked_at = Time.now
+      job.locked_by = 'test'
+      expect(status(job)).to eq('<span class='label label-info'>running</span>')
     end
     
-      before(:each) do
-    stub_request(:get, /events.json$/).to_return(
-      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    stub_request(:get, /projects.json$/).to_return(
-      :body => JSON.dump([{name: 'test', id: 1234},{name: 'test1', id: 1235}]),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    stub_request(:get, /02:00$/).to_return(
-      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    @valid_params = { :project_id => 6789 }
-    
-    alias printf_orig printf
-def printf *args
-  # $fp.printf(*args)
-end
-    
-          Dir.chdir(file_path) do
-        contents = file.read
-        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
-          contents = $1.lstrip
-        end
-        contents = pre_filter(contents)
-        if @raw
-          contents
-        else
-          partial = Liquid::Template.parse(contents)
-          context.stack do
-            partial.render(context)
-          end
-        end
+          it 'handles signals' do
+        @agent_runner.instance_variable_set(:@signal_queue, ['TERM'])
+        @agent_runner.run
       end
     end
-  end
-end
     
-      public(:input)
-end # class FPM::Package::NPM
-
-    
-        pear_cmd = 'pear -c #{config} remote-info #{input_package}'
-    logger.info('Fetching package information', :package => input_package, :command => pear_cmd)
-    name = %x{#{pear_cmd} | sed -ne '/^Package\s*/s/^Package\s*//p'}.chomp
-    self.name = '#{attributes[:pear_package_name_prefix]}-#{name}'
-    self.version = %x{#{pear_cmd} | sed -ne '/^Installed\s*/s/^Installed\s*//p'}.chomp
-    self.description  = %x{#{pear_cmd} | sed -ne '/^Summary\s*/s/^Summary\s*//p'}.chomp
-    logger.debug('Package info', :name => self.name, :version => self.version,
-                  :description => self.description)
-    
-        cwd = ::Dir.pwd
-    ::Dir.chdir(staging_path)
-    
-      public(:input)
-end # class FPM::Package::PleaseRun
-
-    
-      # Helper for user lookup
-  def uid2user(uid)
-    begin
-      pwent = Etc.getpwuid(uid)
-      return pwent.name
-    rescue ArgumentError => e
-      # Invalid user id? No user? Return the uid.
-      logger.warn('Failed to find username for uid #{uid}')
-      return uid.to_s
-    end
-  end # def uid2user
-    
-        realpath = Pathname.new(input_path).realpath.to_s
-    ::Dir.chdir(build_path) do
-      safesystem('unzip', realpath)
-    end
-    
-        (@source.empty? || @target.empty? || options.name.empty?) &&
-      abort('Must specify package name, source and output')
+    describe Rufus::Scheduler do
+  before :each do
+    Agent.delete_all
