bb76e647a6643d151c5f7b431119c55b58fc8244
SUNsung <weatherius@gmail.com>
2019-06-09 23:59:56 +0300

    Rewrite [14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb] File in: 2019-06-09 23:59:56

--- ruby/14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb
+++ ruby/14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb
@@ -1,215 +1,153 @@
 
         
-        # Mimic how the proposed change would first execute a couple of checks and
-# proceed to process with Liquid if necessary
-def conditional_liquid(content)
-  return content if content.nil? || content.empty?
-  return content unless content.include?('{%') || content.include?('{{')
-  always_liquid(content)
-end
-    }
-    }
-    }
-    
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-            # rubocop:disable Metrics/AbcSize
-        def process(args, opts)
-          if !args || args.empty?
-            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
-          end
-    
-        def document?
-      @content =~ DOCUMENT_RGX
-    end
-    
-        class RateLimiter
-      attr_accessor :limit
-    
-          str.truncate(max_length).ljust(max_length) << tag.to_s
-    end
-    
-            if at_css('.api-type-label.module')
-          at_css('h1').content = subpath.remove('api/')
+                def collection_method
+          :pull_requests_comments
         end
     
-            name = name.split(':').first
+            waiter
+      end
     
-      #
-  # Parses a header from a string.
-  #
-  # XXX - Putting : in a header value breaks this badly
-  def from_s(header)
-    reset
+        # The path used after unlocking the resource
+    def after_unlock_path_for(resource)
+      new_session_path(resource)  if is_navigational_format?
+    end
     
-      # open rmcpplus_request
-  def self.create_ipmi_session_open_request(console_session_id)
-    head = [
-      0x06, 0x00, 0xff, 0x07,   # RMCP Header
-      0x06,                     # RMCP+ Authentication Type
-      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
-      0x00, 0x00, 0x00, 0x00,   # Session ID
-      0x00, 0x00, 0x00, 0x00    # Sequence Number
-    ].pack('C*')
-    
-            # Receives a Kerberos Response over a tcp connection
-        #
-        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] the kerberos message response
-        # @raise [RuntimeError] if the response can't be processed
-        # @raise [EOFError] if expected data can't be read
-        def recv_response_tcp
-          length_raw = connection.get_once(4, timeout)
-          unless length_raw && length_raw.length == 4
-            raise ::RuntimeError, 'Kerberos Client: failed to read response'
-          end
-          length = length_raw.unpack('N')[0]
-    
-                data_encrypt = Rex::Text::rand_text(8) + data
-    
-              def initialize(options = {})
-            self.class.attributes.each do |attr|
-              if options.has_key?(attr)
-                m = (attr.to_s + '=').to_sym
-                self.send(m, options[attr])
-              end
-            end
-          end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        class EncKdcResponse < Element
-          # @!attribute key
-          #   @return [Rex::Proto::Kerberos::Model::EncryptionKey] The session key
-          attr_accessor :key
-          # @!attribute last_req
-          #   @return [Array<Rex::Proto::Kerberos::Model::LastRequest>] This field is returned by the KDC and specifies the time(s)
-          #   of the last request by a principal
-          attr_accessor :last_req
-          # @!attribute nonce
-          #   @return [Integer] random number
-          attr_accessor :nonce
-          # @!attribute key_expiration
-          #   @return [Time] The key-expiration field is part of the response from the
-          #   KDC and specifies the time that the client's secret key is due to expire
-          attr_accessor :key_expiration
-          # @!attribute flags
-          #   @return [Integer] This field indicates which of various options were used or
-          #   requested when the ticket was issued
-          attr_accessor :flags
-          # @!attribute auth_time
-          #   @return [Time] the time of initial authentication for the named principal
-          attr_accessor :auth_time
-          # @!attribute start_time
-          #   @return [Time] Specifies the time after which the ticket is valid
-          attr_accessor :start_time
-          # @!attribute end_time
-          #   @return [Time] This field contains the time after which the ticket will
-          #   not be honored (its expiration time)
-          attr_accessor :end_time
-          # @!attribute renew_till
-          #   @return [Time] This field is only present in tickets that have the
-          #   RENEWABLE flag set in the flags field.  It indicates the maximum
-          #   endtime that may be included in a renewal
-          attr_accessor :renew_till
-          # @!attribute srealm
-          #   @return [String] The realm part of the server's principal identifier
-          attr_accessor :srealm
-          # @!attribute sname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
-          attr_accessor :sname
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a Kerberos KRB-ERROR (response error)
-        # message definition.
-        class KrbError < Element
-          # @!attribute pvno
-          #   @return [Integer] The protocol version number
-          attr_accessor :pvno
-          # @!attribute msg_type
-          #   @return [Integer] The type of a protocol message
-          attr_accessor :msg_type
-          # @!attribute ctime
-          #   @return [Time] The current time of the client's host
-          attr_accessor :ctime
-          # @!attribute cusec
-          #   @return [Integer] The microseconds part of the client timestamp
-          attr_accessor :cusec
-          # @!attribute stime
-          #   @return [Time] The current time of the server
-          attr_accessor :stime
-          # @!attribute susec
-          #   @return [Integer] The microseconds part of the server timestamp
-          attr_accessor :susec
-          # @!attribute error_code
-          #   @return [Integer] The error request returned by kerberos or the server when a request fails
-          attr_accessor :error_code
-          # @!attribute crealm
-          #   @return [String] The realm part of the client's principal identifier
-          attr_accessor :crealm
-          # @!attribute cname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the client's principal identifier
-          attr_accessor :cname
-          # @!attribute realm
-          #   @return [String] The realm part of the server's principal identifier
-          attr_accessor :realm
-          # @!attribute sname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
-          attr_accessor :sname
-          # @!attribute e_data
-          #   @return [String] additional data about the error (ASN.1 encoded data)
-          attr_accessor :e_data
-    
-              workspace = eval(code, binding, f.path)
-          assert_equal(<<~EOS, without_term { workspace.code_around_binding })
-    
-            # Set these key values to boolean 'true' to include in policy
-        NO_ARG_DIRECTIVES.each do |d|
-          if options.key?(d) && options[d].is_a?(TrueClass)
-            directives << d.to_s.sub(/_/, '-')
-          end
-        end
-    
-          def call(env)
-        request               = Request.new(env)
-        status, headers, body = app.call(env)
-    
-      subject { described_class.new(lambda {}) }
-    
-          def field_container(model, method, options = {}, &block)
-        css_classes = options[:class].to_a
-        css_classes << 'field'
-        css_classes << 'withError' if error_message_on(model, method).present?
-        content_tag(
-          :div, capture(&block),
-          options.merge(class: css_classes.join(' '), id: '#{model}_#{method}_field')
+          def remember_cookie_values(resource)
+        options = { httponly: true }
+        options.merge!(forget_cookie_values(resource))
+        options.merge!(
+          value: resource.class.serialize_into_cookie(resource),
+          expires: resource.remember_expires_at
         )
       end
     
-        it 'is able to apply a ransack filter by clicking a quickfilter icon', js: true do
-      label_pending = page.find '.badge-pending'
-      parent_td = label_pending.find(:xpath, '..')
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
     
-              def find_spree_current_order
-            Spree::Api::Dependencies.storefront_current_order_finder.constantize.new.execute(
-              store: spree_current_store,
-              user: spree_current_user,
-              token: order_token,
-              currency: current_currency
-            )
-          end
+          def mailer_reply_to(mapping)
+        mailer_sender(mapping, :reply_to)
+      end
+    
+              include mod
+        end
+    
+        def pos
+      byte_to_str_pos @s.pos
+    end
+    
+        def silence_log
+      @silence = true
+      yield
+    ensure
+      @silence = false
+    end
+  end
+end
+
+    
+      # Precompile additional assets.
+  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  # config.assets.precompile += %w( search.js )
+    
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+    
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
+      end
+    end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
+  end
+end
+
+    
+      # Setup a color scheme called 'bright' than can be used to add color codes
+  # to the pattern layout. Color schemes should only be used with appenders
+  # that write to STDOUT or STDERR; inserting terminal color codes into a file
+  # is generally considered bad form.
+  Logging.color_scheme('bright',
+                       levels:  {
+                         info:  :green,
+                         warn:  :yellow,
+                         error: :red,
+                         fatal: %i(white on_red)
+                       },
+                       date:    :blue,
+                       logger:  :cyan,
+                       message: :magenta
+                      )
+    
+      class SendPublic < Base
+    def perform(*_args)
+      # don't federate in cucumber
+    end
+  end
+    
+      def safely_remove_file(_path)
+    run_vagrant_command('rm #{test_file}')
+  rescue
+    VagrantHelpers::VagrantSSHCommandError
+  end
+end
+    
+      class VagrantSSHCommandError < RuntimeError; end
+    
+    MESSAGE
+      end
+    end
+  end
+end
+
+    
+          def add_host(host, properties={})
+        new_host = Server[host]
+        new_host.port = properties[:port] if properties.key?(:port)
+        # This matching logic must stay in sync with `Server#matches?`.
+        key = ServerKey.new(new_host.hostname, new_host.port)
+        existing = servers_by_key[key]
+        if existing
+          existing.user = new_host.user if new_host.user
+          existing.with(properties)
+        else
+          servers_by_key[key] = new_host.with(properties)
+        end
+      end
+    
+      def validate_target_file
+    if File.exist?(target_file)
+      if  delete_target_file?
+        File.delete(target_file)
+      else
+        signal_error('Package creation cancelled, a previously generated package exist at location: #{target_file}, move this file to safe place and run the command again')
+      end
+    end
+  end
+    
+            PluginManager.ui.debug('Looking if package named: #{plugin_name} exists at #{uri}')
+    
+        def user_feedback_string_for(action, platform, machines, options={})
+      experimental_string = options['experimental'] ? 'experimental' : 'non experimental'
+      message  = '#{action} all #{experimental_string} VM's defined in acceptance/Vagrantfile'
+      '#{message} for #{platform}: #{machines}' if !platform.nil?
+    end
+    
+        before do
+      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
+      # Logstash won't update when we have a pinned version in the gemfile so we remove them
+      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
+      expect(logstash).to have_installed?(plugin_name, previous_version)
+    end
