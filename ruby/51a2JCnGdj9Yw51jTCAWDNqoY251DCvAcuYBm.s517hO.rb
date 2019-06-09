e20f47275a846d7b66517ae08b7fa3e4f120f7c5
SUNsung <weatherius@gmail.com>
2019-06-09 15:37:12 +0300

    Rewrite [51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb] File in: 2019-06-09 15:37:12

--- ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
+++ ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
@@ -1,3 +1,87 @@
 
         
-        
+          def report
+    @report ||= JSON.parse(request.body.read)['csp-report'].slice(
+      'blocked-uri',
+      'disposition',
+      'document-uri',
+      'effective-directive',
+      'original-policy',
+      'referrer',
+      'script-sample',
+      'status-code',
+      'violated-directive',
+      'line-number',
+      'source-file'
+    )
+  end
+    
+    end
+    
+        def extendable?(directive)
+      EXTENDABLE_DIRECTIVES.include?(directive)
+    end
+    
+        langs.sort
+  end
+    
+          response.headers['Last-Modified'] = 10.years.ago.httpdate
+      response.headers['Content-Length'] = svg_sprite.bytesize.to_s
+      immutable_for 1.year
+    
+    # == Schema Information
+#
+# Table name: user_stats
+#
+#  user_id                  :integer          not null, primary key
+#  topics_entered           :integer          default(0), not null
+#  time_read                :integer          default(0), not null
+#  days_visited             :integer          default(0), not null
+#  posts_read_count         :integer          default(0), not null
+#  likes_given              :integer          default(0), not null
+#  likes_received           :integer          default(0), not null
+#  topic_reply_count        :integer          default(0), not null
+#  new_since                :datetime         not null
+#  read_faq                 :datetime
+#  first_post_created_at    :datetime
+#  post_count               :integer          default(0), not null
+#  topic_count              :integer          default(0), not null
+#  bounce_score             :float            default(0.0), not null
+#  reset_bounce_score_after :datetime
+#  flags_agreed             :integer          default(0), not null
+#  flags_disagreed          :integer          default(0), not null
+#  flags_ignored            :integer          default(0), not null
+#  first_unread_at          :datetime         not null
+#
+
+    
+        context '(de)activating users' do
+      it 'does not show deactivation buttons for the current user' do
+        visit admin_users_path
+        expect(page).to have_no_css('a[href='/admin/users/#{users(:jane).id}/deactivate']')
+      end
+    
+        it 'sends escape characters correctly to the backend' do
+      emitter.events << Event.new(payload: {data: 'Line 1\nLine 2\nLine 3'})
+      formatting_agent.sources << emitter
+      formatting_agent.options.merge!('instructions' => {'data' => '{{data | newline_to_br | strip_newlines | split: '<br />' | join: ','}}'})
+      formatting_agent.save!
+    
+      it 'requires a URL or file uplaod' do
+    visit new_scenario_imports_path
+    click_on 'Start Import'
+    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
+  end
+    
+      it 'replaces invalid byte sequences in a message' do
+    log = AgentLog.new(:agent => agents(:jane_website_agent), level: 3)
+    log.message = '\u{3042}\xffA\x95'
+    expect { log.save! }.not_to raise_error
+    expect(log.message).to eq('\u{3042}<ff>A\<95>')
+  end
+    
+        it 'is able to apply a ransack filter by clicking a quickfilter icon', js: true do
+      label_pending = page.find '.badge-pending'
+      parent_td = label_pending.find(:xpath, '..')
+    
+          @@shipment_attributes = [:id, :tracking, :number, :cost, :shipped_at, :state]
