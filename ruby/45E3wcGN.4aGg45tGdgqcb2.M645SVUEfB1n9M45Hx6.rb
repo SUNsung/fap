7a5f6248bcc0665b3cc21a9e645b33308351a81c
SUNsung <weatherius@gmail.com>
2019-06-10 02:48:50 +0300

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2019-06-10 02:48:49

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,17 +1,64 @@
 
         
-            # Extract the command string
-    self.cmd_string = header.slice!(/.+\r\n/)
-    
-        # /%3faaa=bbbbb
-    # which could possibly decode to '/?aaa=bbbbb', which if the IDS normalizes first, then splits the URI on ?, then it can be bypassed
-    if self.junk_param_start
-      str.sub!(/\//, '/%3f' + Rex::Text.rand_text_alpha(rand(5) + 1) + '=' + Rex::Text.rand_text_alpha(rand(10) + 1) + '/../')
+        class ContentSecurityPolicy
+  class Middleware
+    def initialize(app)
+      @app = app
     end
     
-              # Encodes the end_time field
-          #
-          # @return [String]
-          def encode_end_time
-            [end_time].pack('N')
-          end
+          keyword = params.require(:keyword)
+      data = SvgSprite.search(keyword)
+    
+        # Update denormalzied posts_read_count
+    DB.exec(<<~SQL, seen_at: last_seen)
+      UPDATE user_stats SET posts_read_count = X.c
+      FROM
+      (SELECT pt.user_id,
+              COUNT(*) AS c
+       FROM users AS u
+       JOIN post_timings AS pt ON pt.user_id = u.id
+       JOIN topics t ON t.id = pt.topic_id
+       WHERE u.last_seen_at > :seen_at AND
+             t.archetype = 'regular' AND
+             t.deleted_at IS NULL
+       GROUP BY pt.user_id) AS X
+       WHERE X.user_id = user_stats.user_id AND
+             X.c <> posts_read_count
+    SQL
+  end
+    
+          if user.admin && defined?(Rack::MiniProfiler)
+        # clear the profiling cookie to keep stuff tidy
+        cookies.delete('__profilin')
+      end
+    
+        if hijack = request.env['rack.hijack']
+    
+            val ? true : false
+      end
+    
+            def importer_class
+          NoteImporter
+        end
+    
+          # Associates the given database ID with the current object.
+      #
+      # database_id - The ID of the corresponding database row.
+      def cache_database_id(database_id)
+        Caching.write(cache_key, database_id)
+      end
+    
+          # Any options to be passed to the method used for retrieving the data to
+      # import.
+      def collection_options
+        {}
+      end
+    end
+  end
+end
+
+    
+    require 'builder'
+require 'feedbag'
+require 'json'
+require 'nokogiri'
