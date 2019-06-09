55e1261ae295b8e22627d9f7b0936c3e5fde68dd
SUNsung <weatherius@gmail.com>
2019-06-09 06:03:55 +0300

    Rewrite [42qUlG.4MUW1Y42ciJgNlfX4QM42B5YfiDS6QPw42BkW.py] File in: 2019-06-09 06:03:54

--- pyton/42qUlG.4MUW1Y42ciJgNlfX4QM42B5YfiDS6QPw42BkW.py
+++ pyton/42qUlG.4MUW1Y42ciJgNlfX4QM42B5YfiDS6QPw42BkW.py
@@ -1,127 +1,68 @@
 
         
-                (foo, 2), p1
-        (bar, 3), p1
-        (foo, 3), p2
-        (bar, 10), p3
-        (foo, 1), p4
-    
-            When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.map[query]
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(query, results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
+        
+if __name__ == '__main__':
+    main()
 
     
-        def crawl_page(self, page):
-        for url in page.child_urls:
-            self.data_store.add_link_to_crawl(url)
-        self.reverse_index_queue.generate(page)
-        self.doc_index_queue.generate(page)
-        self.data_store.remove_link_to_crawl(page.url)
-        self.data_store.insert_crawled_link(page.url, page.signature)
-    
-    class Command(ScrapyCommand):
-    
-            try:
-            self.max_bound = int(self.args[2])
-        except IndexError:
-            self.max_bound = float('inf')
-    
-        from twisted.internet.ssl import (optionsForClientTLS,
-                                      CertificateOptions,
-                                      platformTrust)
-    from twisted.web.client import BrowserLikePolicyForHTTPS
-    from twisted.web.iweb import IPolicyForHTTPS
+            monitoring_policy = oneandone_conn.delete_monitoring_policy_process(
+            monitoring_policy_id=monitoring_policy_id,
+            process_id=process_id)
+        return monitoring_policy
+    except Exception as ex:
+        module.fail_json(msg=str(ex))
     
     
-def make_color_fn(code):
-    return lambda s: ansi_color(code, s)
+if __name__ == '__main__':
+    main()
+
     
-        if not hasattr(command_class, command):
-        raise NoSuchCommand(command, command_class)
+            # check if scaling needs to be done to be in new_range
+        if arr_min < new_range[0] or arr_max > new_range[1]:
+            # perform min-max scaling
+            scaled = (new_range[1] - new_range[0]) * (arr - arr_min) / (arr_max -
+                                                                        arr_min) + new_range[0]
+        else:
+            # return array if already in range
+            scaled = arr
     
-    # WindowsError is not defined on non-win32 platforms. Avoid runtime errors by
-# defining it as OSError (its parent class) if missing.
-try:
-    WindowsError
-except NameError:
-    WindowsError = OSError
+        def unsharp_mask(self, new_face, kernel_center, amount):
+        ''' Sharpen using unsharp mask '''
+        kernel_size = kernel_center[0]
+        threshold = self.config['threshold'] / 255.0
+        blur = cv2.GaussianBlur(new_face, kernel_size, 0)  # pylint: disable=no-member
+        low_contrast_mask = (abs(new_face - blur) < threshold).astype('float32')
+        sharpened = (new_face * (1.0 + amount)) + (blur * -amount)
+        new_face = (new_face * (1.0 - low_contrast_mask)) + (sharpened * low_contrast_mask)
+        return new_face
+
     
-    
-def get_named_volumes(services):
-    volume_specs = [
-        VolumeSpec.parse(volume)
-        for service in services.values()
-        for volume in service.get('volumes', [])
-    ]
-    names = {
-        spec.external
-        for spec in volume_specs
-        if spec.is_named_volume
-    }
-    return {name: {'external': True} for name in names}
-    
-    from compose.config.config import ConfigDetails
-from compose.config.config import ConfigFile
-from compose.config.config import load
-    
-        def test_format_error(self):
-        output = self.formatter.format(make_log_record(logging.ERROR))
-        expected = colors.red('ERROR') + ': '
-        assert output == expected + MESSAGE
-    
-      Args:
-    from_tensor: float Tensor of shape [batch_size, from_seq_length,
-      from_width].
-    to_tensor: float Tensor of shape [batch_size, to_seq_length, to_width].
-    attention_mask: (optional) int32 Tensor of shape [batch_size,
-      from_seq_length, to_seq_length]. The values should be 1 or 0. The
-      attention scores will effectively be set to -infinity for any positions in
-      the mask that are 0, and will be unchanged for positions that are 1.
-    num_attention_heads: int. Number of attention heads.
-    size_per_head: int. Size of each attention head.
-    query_act: (optional) Activation function for the query transform.
-    key_act: (optional) Activation function for the key transform.
-    value_act: (optional) Activation function for the value transform.
-    attention_probs_dropout_prob: (optional) float. Dropout probability of the
-      attention probabilities.
-    initializer_range: float. Range of the weight initializer.
-    do_return_2d_tensor: bool. If True, the output will be of shape [batch_size
-      * from_seq_length, num_attention_heads * size_per_head]. If False, the
-      output will be of shape [batch_size, from_seq_length, num_attention_heads
-      * size_per_head].
-    batch_size: (Optional) int. If the input is 2D, this might be the batch size
-      of the 3D version of the `from_tensor` and `to_tensor`.
-    from_seq_length: (Optional) If the input is 2D, this might be the seq length
-      of the 3D version of the `from_tensor`.
-    to_seq_length: (Optional) If the input is 2D, this might be the seq length
-      of the 3D version of the `to_tensor`.
+        def pre_encode(self, image):
+        logger.trace('Pre-encoding image')
+        image = cv2.imencode(self.extension, image, self.args)[1]  # pylint: disable=no-member
+        return image
     
     
-class InputFeatures(object):
-  '''A single set of features of data.'''
+class YAMLSerializer(Serializer):
+    ''' YAML Serializer '''
+    ext = 'yml'
+    woptions = 'w'
+    roptions = 'r'
     
-      if use_tpu:
-    optimizer = tf.contrib.tpu.CrossShardOptimizer(optimizer)
+        def draw_landmarks(self, color_id=3, radius=1):
+        ''' Draw the facial landmarks '''
+        color = self.colors[color_id]
+        for alignment in self.alignments:
+            landmarks = alignment['landmarksXY']
+            logger.trace('Drawing Landmarks: (landmarks: %s, color: %s, radius: %s)',
+                         landmarks, color, radius)
+            for (pos_x, pos_y) in landmarks:
+                cv2.circle(self.image,  # pylint: disable=no-member
+                           (pos_x, pos_y),
+                           radius,
+                           color,
+                           -1)
     
-      When running eval/predict on the TPU, we need to pad the number of examples
-  to be a multiple of the batch size, because the TPU requires a fixed batch
-  size. The alternative is to drop the last batch, which is bad because it means
-  the entire output data won't be generated.
+                # New terminal setting unbuffered
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.file_desc, termios.TCSAFLUSH, self.new_term)
