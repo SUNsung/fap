0472214a18feb3a3505a0e10d7ecec2fc00d1083
SUNsung <weatherius@gmail.com>
2019-06-09 19:07:20 +0300

    Rewrite [57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py] File in: 2019-06-09 19:07:19

--- pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
+++ pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
@@ -1,132 +1,195 @@
 
         
-        match_output = '''
-Listing... Done
-heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
-resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
-squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
-unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
-'''
+          word_pairs = curr_features['pair']
+  return word_pairs
     
-    dynamodb                                 | dynamodbstreams
-ec2                                      | ecr
+    DATASET: The top level dictionary is simply name (string -> dictionary).
+The nested dictionary is the DATA DICTIONARY, which has the following keys:
+  'train_data' and 'valid_data', whose values are the corresponding training
+    and validation data with shape
+    ExTxD, E - # examples, T - # time steps, D - # dimensions in data.
+  The data dictionary also has a few more keys:
+    'train_ext_input' and 'valid_ext_input', if there are know external inputs
+      to the system being modeled, these take on dimensions:
+      ExTxI, E - # examples, T - # time steps, I = # dimensions in input.
+   'alignment_matrix_cxf' - If you are using multiple days data, it's possible
+     that one can align the channels (see manuscript).  If so each dataset will
+     contain this matrix, which will be used for both the input adapter and the
+     output adapter for each dataset. These matrices, if provided, must be of
+     size [data_dim x factors] where data_dim is the number of neurons recorded
+     on that day, and factors is chosen and set through the '--factors' flag.
+   'alignment_bias_c' - See alignment_matrix_cxf.  This bias will used to
+     the offset for the alignment transformation.  It will *subtract* off the
+     bias from the data, so pca style inits can align factors across sessions.
+    
+      input_title = ''
+  if 'controller_outputs' in model_vals.keys():
+    input_title += ' Controller Output'
+    plt.subplot(nrows,2,3+subplot_cidx)
+    u_t = model_vals['controller_outputs'][0:-1]
+    plot_time_series(u_t, bidx, n_to_plot=n_to_plot, color='c', scale=1.0,
+                     title=col_title + input_title)
+    
+      # Build and run the model, for varying purposes.
+  config = tf.ConfigProto(allow_soft_placement=True,
+                          log_device_placement=False)
+  if FLAGS.allow_gpu_growth:
+    config.gpu_options.allow_growth = True
+  sess = tf.Session(config=config)
+  with sess.as_default():
+    with tf.device(hps.device):
+      if kind == 'train':
+        train(hps, datasets)
+      elif kind == 'posterior_sample_and_average':
+        write_model_runs(hps, datasets, hps.output_filename_stem,
+                         push_mean=False)
+      elif kind == 'posterior_push_mean':
+        write_model_runs(hps, datasets, hps.output_filename_stem,
+                         push_mean=True)
+      elif kind == 'prior_sample':
+        write_model_samples(hps, datasets, hps.output_filename_stem)
+      elif kind == 'write_model_params':
+        write_model_parameters(hps, hps.output_filename_stem, datasets)
+      else:
+        assert False, ('Kind %s is not implemented. ' % kind)
+    
+      # First generate all firing rates. in the next loop, generate all
+  # replications this allows the random state for rate generation to be
+  # independent of n_replications.
+  dataset_name = 'dataset_N' + str(N) + '_S' + str(S)
+  if S < N:
+    dataset_name += '_n' + str(n+1)
     
     
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_get_new_command(brew_no_available_formula):
-    assert get_new_command(Command('brew install elsticsearch',
-                                   brew_no_available_formula))\
-        == 'brew install elasticsearch'
+class IntegrationToBoundModel:
+  def __init__(self, N):
+    scale = 0.8 / float(N**0.5)
+    self.N = N
+    self.Wh_nxn = tf.Variable(tf.random_normal([N, N], stddev=scale))
+    self.b_1xn = tf.Variable(tf.zeros([1, N]))
+    self.Bu_1xn = tf.Variable(tf.zeros([1, N]))
+    self.Wro_nxo = tf.Variable(tf.random_normal([N, 1], stddev=scale))
+    self.bro_o = tf.Variable(tf.zeros([1]))
+    
+      for batch in range(num_batches):
+    x = np.zeros([batch_size, num_steps], dtype=np.int32)
+    y = np.zeros([batch_size, num_steps], dtype=np.int32)
+    w = np.zeros([batch_size, num_steps], dtype=np.float)
+    
+      Args:
+    gen_logits: Generator logits.
+    gen_labels:  Labels for the correct token.
+    dis_predictions:  Discriminator predictions.
+    is_real_input:  Tensor indicating whether the label is present.
+    
+      Args;
+    hparams:  Hyperparameters for the MaskGAN.
+    inputs:  tf.int32 Tensor of the sequence input of shape [batch_size,
+      sequence_length].
+    present:  tf.bool Tensor indicating the presence or absence of the token
+      of shape [batch_size, sequence_length].
+    is_training:  Whether the model is training.
+    is_validating:  Whether the model is being run in validation mode for
+      calculating the perplexity.
+    reuse (Optional):  Whether to reuse the model.
     
     
-@pytest.fixture
-def httpbin_secure(httpbin_secure):
-    return prepare_url(httpbin_secure)
+def create_dis_train_op(hparams, dis_loss, global_step):
+  '''Create Discriminator train op.'''
+  with tf.name_scope('train_discriminator'):
+    dis_optimizer = tf.train.AdamOptimizer(hparams.dis_learning_rate)
+    dis_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('dis')
+    ]
+    if FLAGS.dis_update_share_embedding and FLAGS.dis_share_embedding:
+      shared_embedding = [
+          v for v in tf.trainable_variables()
+          if v.op.name == 'gen/decoder/rnn/embedding'
+      ][0]
+      dis_vars.append(shared_embedding)
+    print('\nOptimizing Discriminator vars:')
+    for v in dis_vars:
+      print(v)
+    dis_grads = tf.gradients(dis_loss, dis_vars)
+    dis_grads_clipped, _ = tf.clip_by_global_norm(dis_grads,
+                                                  FLAGS.grad_clipping)
+    dis_train_op = dis_optimizer.apply_gradients(
+        zip(dis_grads_clipped, dis_vars), global_step=global_step)
+    return dis_train_op, dis_grads_clipped, dis_vars
+    
+        ## Discriminator Models.
+    if (FLAGS.discriminator_model == 'rnn_nas' or
+        FLAGS.discriminator_model == 'rnn_zaremba' or
+        FLAGS.discriminator_model == 'rnn_vd' or
+        FLAGS.discriminator_model == 'cnn'):
+      load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
+      print('Restoring Discriminator from %s.' % load_ckpt)
+      tf.logging.info('Restoring Discriminator from %s.' % load_ckpt)
+      dis_init_saver = init_savers['dis_init_saver']
+      dis_init_saver.restore(sess, load_ckpt)
+    
+        lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+                 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
+                 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
+                 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
+                 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331,
+                 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
+                 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467,
+                 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563,
+                 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631,
+                 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709,
+                 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+                 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877,
+                 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967,
+                 971, 977, 983, 991, 997]
+    
+    	for n in xrange(m+1):
+		for k in xrange(1, m):
+			memo[n][k] += memo[n][k-1]
+			if n-k > 0:
+				memo[n][k] += memo[n-k-1][k]
+    
+    def centroid_pairwise_dist(X,centroids):
+    return pairwise_distances(X,centroids,metric='euclidean')
+    
+        difference = predict - actual
+    square_diff = np.square(difference)
+    
+    min_length = 8
+max_length = 16
+password = ''.join(random.choice(chars) for x in range(random.randint(min_length, max_length)))
+print('Password: ' + password)
+print('[ If you are thinking of using this passsword, You better save it. ]')
+    
+        # Test single image
+    x = np.random.uniform(0, 255, (10, 10, 3))
+    inputs = Input(shape=x.shape)
+    outputs = Lambda(utils.preprocess_input, output_shape=x.shape)(inputs)
+    model = Model(inputs, outputs)
+    assert model.predict(x[np.newaxis])[0].shape == x.shape
+    
+    # the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    
+    scores = model.evaluate(x_test, y_test, verbose=0)
+print('IRNN test score:', scores[0])
+print('IRNN test accuracy:', scores[1])
 
     
-            self.cert_verify(conn, request.url, verify, cert)
-        url = self.request_url(request, proxies)
-        self.add_headers(request, stream=stream, timeout=timeout, verify=verify, cert=cert, proxies=proxies)
+    model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
     
-        This function works best on CPython and PyPy: in particular, it probably
-    doesn't work for Jython or IronPython. Future investigation should be done
-    to work out the correct shape of the code for those platforms.
-    '''
-    implementation = platform.python_implementation()
+    print('Loading data...')
+(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
+                                                         test_split=0.2)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
     
-    
-def dispatch_hook(key, hooks, hook_data, **kwargs):
-    '''Dispatches a hook dictionary on a given piece of data.'''
-    hooks = hooks or {}
-    hooks = hooks.get(key)
-    if hooks:
-        if hasattr(hooks, '__call__'):
-            hooks = [hooks]
-        for hook in hooks:
-            _hook_data = hook(hook_data, **kwargs)
-            if _hook_data is not None:
-                hook_data = _hook_data
-    return hook_data
-
-    
-    # mapping coco categories to cityscapes (our converted json) id
-# cityscapes
-# INFO roidb.py: 220: 1       bicycle: 7286
-# INFO roidb.py: 220: 2           car: 53684
-# INFO roidb.py: 220: 3        person: 35704
-# INFO roidb.py: 220: 4         train: 336
-# INFO roidb.py: 220: 5         truck: 964
-# INFO roidb.py: 220: 6    motorcycle: 1468
-# INFO roidb.py: 220: 7           bus: 758
-# INFO roidb.py: 220: 8         rider: 3504
-    
-    $ find . -name '*.yaml' -exec sed -i -e \
-   's/head_builder\.add_roi_2mlp_head/fast_rcnn_heads.add_roi_2mlp_head/g' {} \;
-    
-        # rois are in [[batch_idx, x0, y0, x1, y2], ...] format
-    # Combine predictions across all levels and retain the top scoring
-    rois = np.concatenate([blob.data for blob in roi_inputs])
-    scores = np.concatenate([blob.data for blob in score_inputs]).squeeze()
-    inds = np.argsort(-scores)[:post_nms_topN]
-    rois = rois[inds, :]
-    return rois
-    
-        # Generate canonical proposals from shifted anchors
-    # Enumerate all shifted positions on the (H, W) grid
-    fpn_max_size = cfg.FPN.COARSEST_STRIDE * np.ceil(
-        cfg.TRAIN.MAX_SIZE / float(cfg.FPN.COARSEST_STRIDE)
-    )
-    field_size = int(np.ceil(fpn_max_size / float(stride)))
-    shifts = np.arange(0, field_size) * stride
-    shift_x, shift_y = np.meshgrid(shifts, shifts)
-    shift_x = shift_x.ravel()
-    shift_y = shift_y.ravel()
-    shifts = np.vstack((shift_x, shift_y, shift_x, shift_y)).transpose()
-    
-    import numpy as np
-import unittest
-    
-            with self.mock_del_msg(message, False):
-            res = self.do_del_msg(message.name)
-    
-        def assert_failure(self, res, code=None):
-        self.assertEqual(res.status, 200)
-        body = res.body
-        body = json.loads(body)
-        self.assertTrue('json' in body)
-        errors = body['json'].get('errors')
-        self.assertTrue(code in [x[0] for x in errors])
-        data = body['json'].get('data')
-        self.assertFalse(bool(data))
-    
-        def get_loggedin_users(self, num_users):
-        users = []
-        for i in xrange(num_users):
-            users.append(MockAccount(name=str(i), _fullname='t2_%s' % str(i)))
-        return users
-    
-        # Control chars break out of quotes in multiple browsers
-    def test_control_chars(self):
-        testcase = u'*{font-family:'foobar\x03;color:red;';}'
-        self.assertInvalid(testcase)
-    
-    from r2.lib import js
-    
-        def test_simple_self_post(self):
-        post = Link(is_self=True, selftext='''
-Some text here.
-https://example.com
-https://reddit.com''')
-        url = _get_scrape_url(post)
-        self.assertEqual(url, 'https://example.com')
-    
-        def test_censor(self):
-        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
-                      height=800)
-        url = self.provider.resize_image(image, censor_nsfw=True)
-        self.assertEqual(url, 'https://example.com/a.jpg?blur=600&px=32')
-
-    
-        def test_protocol_relative(self):
-        self.assertIsNotSafeRedditUrl('//foobaz.example.com/aa/baz#quux')
+        `log(cosh(x))` is approximately equal to `(x ** 2) / 2` for small `x` and
+    to `abs(x) - log(2)` for large `x`. This means that 'logcosh' works mostly
+    like the mean squared error, but will not be so strongly affected by the
+    occasional wildly incorrect prediction.
