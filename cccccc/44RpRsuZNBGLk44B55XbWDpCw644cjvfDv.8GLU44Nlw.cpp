5e38337df0d0cc890464a82946305ea79e1a07e3
SUNsung <weatherius@gmail.com>
2019-06-09 07:14:46 +0300

    Rewrite [44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp] File in: 2019-06-09 07:14:45

--- cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
+++ cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
@@ -1,131 +1,264 @@
 
         
-        #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
-    
-      static void OnDoneRecvTrailingMetadataCb(void* user_data, grpc_error* error);
-    
-    #include <grpc/support/port_platform.h>
-    
-        virtual void UpdateArguments(ChannelArguments* args) override {
-      args->SetString(name_, value_);
+          if (conformanceSig) {
+    for (auto &rawReq : conformanceSig->getRequirements()) {
+      if (auto req = rawReq.subst(conformanceToSyntheticTypeFn,
+                                  conformanceToSyntheticConformanceFn))
+        builder.addRequirement(*req, source, nullptr);
     }
-    virtual void UpdatePlugins(
-        std::vector<std::unique_ptr<ServerBuilderPlugin>>* plugins) override {}
+  }
     
-    #ifndef GRPC_INTERNAL_CPP_UTIL_CORE_STATS_H
-#define GRPC_INTERNAL_CPP_UTIL_CORE_STATS_H
+      size_t numTrailingObjects(OverloadToken<ProtocolConformanceRef>) const {
+    return numConformanceRequirements;
+  }
     
-    void Assembler::li64TOC(const Reg64& rt, int64_t imm64, ImmType /*immt*/,
-                        bool immMayChange) {
-  int64_t TOCoffset;
-  TOCoffset = VMTOC::getInstance().pushElem(imm64, immMayChange);
-    }
+    void CacheImpl::destroy() {
+  cache_destroy(static_cast<cache_t*>(Impl));
+}
+
     
-    APCHandle::Pair APCCollection::WrapArray(APCHandle::Pair inner,
-                                         CollectionType colType) {
-  auto const col = new APCCollection;
-  col->m_arrayHandle = inner.handle;
-  col->m_colType = colType;
-  return { &col->m_handle, inner.size + sizeof(APCCollection) };
+        void emitCodeContext(clang::FullSourceLoc Loc,
+                         clang::DiagnosticsEngine::Level Level,
+                         SmallVectorImpl<clang::CharSourceRange>& Ranges,
+                         ArrayRef<clang::FixItHint> Hints) override {}
+    
+    void Demangler::dump() {
+  for (unsigned Idx = 0; Idx < NodeStack.size(); ++Idx) {
+    fprintf(stderr, 'NodeStack[%u]:\n', Idx);
+    NodeStack[Idx]->dump();
+    fprintf(stderr, '\n');
+  }
+  fprintf(stderr, 'Position = %zd:\n%.*s\n%*s\n', Pos,
+          (int)Text.size(), Text.data(), (int)Pos + 1, '^');
 }
     
-      Object createObject() const;
+    static bool isValidUnicodeScalar(uint32_t S) {
+  // Also accept the range of 0xD800 - 0xD880, which is used for non-symbol
+  // ASCII characters.
+  return (S < 0xD880) || (S >= 0xE000 && S <= 0x1FFFFF);
+}
     
     
-    {}
-    
-    
-    { private:
-  int mkdir_recursive(const String& path, int mode);
+    {public:
+  Darwin(const Driver &D, const llvm::Triple &Triple) : ToolChain(D, Triple) {}
+  ~Darwin() = default;
+  std::string sanitizerRuntimeLibName(StringRef Sanitizer,
+                                      bool shared = true) const override;
 };
     
-    
-    {///////////////////////////////////////////////////////////////////////////////
+    bool ArgsToFrontendInputsConverter::readInputFilesFromFilelist() {
+  bool hadDuplicates = false;
+  bool hadError =
+      forAllFilesInFilelist(FilelistPathArg, [&](StringRef file) -> void {
+        hadDuplicates = addFile(file) || hadDuplicates;
+      });
+  if (hadError)
+    return true;
+  return false; // FIXME: Don't bail out for duplicates, too many tests depend
+                // on it.
 }
     
     
-    {}
-    
-    
-    {        // Apply multiply operator
-        if (cfg.RasterizerMultiply != 1.0f)
-        {
-            unsigned char multiply_table[256];
-            ImFontAtlasBuildMultiplyCalcLookupTable(multiply_table, cfg.RasterizerMultiply);
-            stbrp_rect* r = &src_tmp.Rects[0];
-            for (int glyph_i = 0; glyph_i < src_tmp.GlyphsCount; glyph_i++, r++)
-                if (r->was_packed)
-                    ImFontAtlasBuildMultiplyRectAlpha8(multiply_table, atlas->TexPixelsAlpha8, r->x, r->y, r->w, r->h, atlas->TexWidth * 1);
-        }
-        src_tmp.Rects = NULL;
-    }
-    
-    IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForOpenGL(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForVulkan(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplGlfw_NewFrame();
-    
-        // Cleanup
-    ImGui_ImplOpenGL2_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-    
-    // About GLSL version:
-//  The 'glsl_version' initialization parameter should be NULL (default) or a '#version XXX' string.
-//  On computer platform the GLSL version default to '#version 130'. On OpenGL ES 3 platform it defaults to '#version 300 es'
-//  Only override if your GL version doesn't handle this GLSL version. See GLSL version table at the top of imgui_impl_opengl3.cpp.
-    
-    Action::Action()
-:_originalTarget(nullptr)
-,_target(nullptr)
-,_tag(Action::INVALID_TAG)
-,_flags(0)
-{
-#if CC_ENABLE_SCRIPT_BINDING
-    ScriptEngineProtocol* engine = ScriptEngineManager::getInstance()->getScriptEngine();
-    _scriptType = engine != nullptr ? engine->getScriptType() : kScriptTypeNone;
-#endif
-}
-    
-        /** 
-     * Creates the action with X Y Z factor.
-     * @param duration Duration time, in seconds.
-     * @param sx Scale factor of x.
-     * @param sy Scale factor of y.
-     * @param sz Scale factor of z.
-     * @return An autoreleased ScaleBy object.
-     */
-    static ScaleBy* create(float duration, float sx, float sy, float sz);
-    
-    
-    {    setTile(pos, coords);
-}
-    
-    
-    {    //turn the result into simply polygon (AKA, fix overlap)
-    
-    //clamp into the specified rect
-    ClipperLib::Clipper cl;
-    cl.StrictlySimple(true);
-    cl.AddPath(p->Contour, ClipperLib::ptSubject, true);
-    //create the clipping rect
-    ClipperLib::Path clamp;
-    clamp.push_back(ClipperLib::IntPoint(0, 0));
-    clamp.push_back(ClipperLib::IntPoint(rect.size.width/_scaleFactor * PRECISION, 0));
-    clamp.push_back(ClipperLib::IntPoint(rect.size.width/_scaleFactor * PRECISION, rect.size.height/_scaleFactor * PRECISION));
-    clamp.push_back(ClipperLib::IntPoint(0, rect.size.height/_scaleFactor * PRECISION));
-    cl.AddPath(clamp, ClipperLib::ptClip, true);
-    cl.Execute(ClipperLib::ctIntersection, out);
-    
-    std::vector<Vec2> outPoints;
-    ClipperLib::PolyNode* p2 = out.GetFirst();
-    while(p2->IsHole()){
-        p2 = p2->GetNext();
-    }
-    for(const auto& pt : p2->Contour)
+    {    void operator() (const u8 * src0, const u8 * src1, u8 * dst) const
     {
-        outPoints.push_back(Vec2(pt.X/PRECISION, pt.Y/PRECISION));
+        dst[0] = src0[0] & src1[0];
     }
-    return outPoints;
+};
+    
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
+CVT_FUNC(f32, s32, 8,
+     register float32x4_t vhalf asm ('q0') = vdupq_n_f32(0.5f);,
+{
+     for (size_t i = 0; i < w; i += 8)
+     {
+         internal::prefetch(_src + i);
+         __asm__ (
+             'vld1.32 {d2-d3}, [%[src1]]                              \n\t'
+             'vld1.32 {d4-d5}, [%[src2]]                              \n\t'
+             'vadd.f32 q3, q1, q0                                     \n\t'
+             'vadd.f32 q4, q2, q0                                     \n\t'
+             'vcvt.s32.f32 q5, q3                                     \n\t'
+             'vcvt.s32.f32 q6, q4                                     \n\t'
+             'vst1.32 {q5}, [%[dst1]]                                 \n\t'
+             'vst1.32 {q6}, [%[dst2]]                                 \n\t'
+             : /*no output*/
+             : [src1] 'r' (_src + i),
+               [src2] 'r' (_src + i + 4),
+               [dst1] 'r' (_dst + i),
+               [dst2] 'r' (_dst + i + 4),
+               'w' (vhalf)
+             : 'd2','d3','d4','d5','d6','d7','d8','d9','d10','d11','d12','d13'
+         );
+     }
+})
+#else
+CVT_FUNC(f32, s32, 8,
+     float32x4_t vhalf = vdupq_n_f32(0.5f);,
+{
+     for (size_t i = 0; i < w; i += 8)
+     {
+         internal::prefetch(_src + i);
+         float32x4_t vline_f32 = vld1q_f32(_src + i);
+    }
+    }
+    
+        // this ugly contruction is needed to avoid:
+    // /usr/lib/gcc/arm-linux-gnueabihf/4.8/include/arm_neon.h:3581:59: error: argument must be a constant
+    // return (int16x8_t)__builtin_neon_vshl_nv8hi (__a, __b, 1);
+    
+    #ifdef CAROTENE_NEON
+    
+            if(i + 2 <= size.width)
+        {
+            float32x2_t vres = vmul_f32(vld1_f32(src0 + i), vld1_f32(src1 + i));
+            result += vget_lane_f32(vres, 0) + vget_lane_f32(vres, 1);
+            i += 2;
+        }
+    
+                uint32x4_t v_mask0 = vorrq_u32(vceqq_u32(v_src0, v_maxval4), vceqq_u32(v_src0, v_minval4));
+            uint32x4_t v_mask1 = vorrq_u32(vceqq_u32(v_src1, v_maxval4), vceqq_u32(v_src1, v_minval4));
+    
+    using google::protobuf::FileDescriptorProto;
+using google::protobuf::FileDescriptor;
+using google::protobuf::DescriptorPool;
+using google::protobuf::io::Printer;
+using google::protobuf::util::SchemaGroupStripper;
+using google::protobuf::util::EnumScrubber;
+    
+    #include <string>
+#include <set>
+#include <vector>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/io/printer.h>
+    
+    template <>
+template <typename T>
+bool EnforceFiniteOp<CPUContext>::DoRunWithType() {
+  EnforceOnCPU<T>(Input(0));
+  return true;
+}
+    
+    namespace caffe2 {
+    }
+    
+    
+    {} // namespace caffe2
+    
+    	void EncodeMipmaps(float *a_pafSourceRGBA,
+		unsigned int a_uiSourceWidth,
+		unsigned int a_uiSourceHeight,
+		Image::Format a_format,
+		ErrorMetric a_eErrMetric,
+		float a_fEffort,
+		unsigned int a_uiJobs,
+		unsigned int a_uiMaxJobs,
+		unsigned int a_uiMaxMipmaps,
+		unsigned int a_uiMipFilterFlags,
+		RawImage* a_pMipmapImages,
+		int *a_piEncodingTime_ms, 
+		bool a_bVerboseOutput)
+	{
+		auto mipWidth = a_uiSourceWidth;
+		auto mipHeight = a_uiSourceHeight;
+		int totalEncodingTime = 0;
+		for(unsigned int mip = 0; mip < a_uiMaxMipmaps && mipWidth >= 1 && mipHeight >= 1; mip++)
+		{
+			float* pImageData = nullptr;
+			float* pMipImage = nullptr;
+    }
+    }
+    
+    		inline bool HasPunchThroughPixels(void)
+		{
+			return m_boolPunchThroughPixels;
+		}
+    
+    	// ----------------------------------------------------------------------------------------------------
+	//
+    
+    #endif  // VPX_DSP_TXFM_COMMON_H_
+
+    
+    #define NEG16(x) (-(x))
+#define NEG32(x) (-(x))
+#define EXTRACT16(x) (x)
+#define EXTEND32(x) (x)
+#define SHR16(a,shift) (a)
+#define SHL16(a,shift) (a)
+#define SHR32(a,shift) (a)
+#define SHL32(a,shift) (a)
+#define PSHR32(a,shift) (a)
+#define VSHR32(a,shift) (a)
+    
+       - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+    
+    
+/** 16x32 multiplication, followed by a 15-bit shift right. Results fits in 32 bits */
+#undef MULT16_32_Q15
+static OPUS_INLINE opus_val32 MULT16_32_Q15_armv4(opus_val16 a, opus_val32 b)
+{
+  unsigned rd_lo;
+  int rd_hi;
+  __asm__(
+      '#MULT16_32_Q15\n\t'
+      'smull %0, %1, %2, %3\n\t'
+      : '=&r'(rd_lo), '=&r'(rd_hi)
+      : '%r'(b), 'r'(a<<16)
+  );
+  /*We intentionally don't OR in the high bit of rd_lo for speed.*/
+  return rd_hi<<1;
+}
+#define MULT16_32_Q15(a, b) (MULT16_32_Q15_armv4(a, b))
+    
+    /*The number of bits to output at a time.*/
+# define EC_SYM_BITS   (8)
+/*The total number of bits in each of the state registers.*/
+# define EC_CODE_BITS  (32)
+/*The maximum symbol value.*/
+# define EC_SYM_MAX    ((1U<<EC_SYM_BITS)-1)
+/*Bits to shift by to move a symbol into the high-order position.*/
+# define EC_CODE_SHIFT (EC_CODE_BITS-EC_SYM_BITS-1)
+/*Carry bit of the high-order range symbol.*/
+# define EC_CODE_TOP   (((opus_uint32)1U)<<(EC_CODE_BITS-1))
+/*Low-order bit of the high-order range symbol.*/
+# define EC_CODE_BOT   (EC_CODE_TOP>>EC_SYM_BITS)
+/*The number of bits available for the last, partial symbol in the code field.*/
+# define EC_CODE_EXTRA ((EC_CODE_BITS-2)%EC_SYM_BITS+1)
+#endif
+
+    
+    MeasureDouble RpcClientServerLatency() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcClientServerLatencyMeasureName,
+      'Time between first byte of request received to last byte of response '
+      'sent, or terminal error (propagated from the server)',
+      kUnitMilliseconds);
+  return measure;
+}
+    
+    void PerHostStore::ReportStreamCreated(const grpc::string& lb_id,
+                                       const grpc::string& load_key) {
+  GPR_ASSERT(lb_id != kInvalidLbId);
+  SetUpForNewLbId(lb_id, load_key);
+  // Prior to this one, there was no load balancer receiving report, so we may
+  // have unassigned orphaned stores to assign to this new balancer.
+  // TODO(juanlishen): If the load key of this new stream is the same with
+  // some previously adopted orphan store, we may want to take the orphan to
+  // this stream. Need to discuss with LB team.
+  if (assigned_stores_.size() == 1) {
+    for (const auto& p : per_balancer_stores_) {
+      const grpc::string& other_lb_id = p.first;
+      const std::unique_ptr<PerBalancerStore>& orphaned_store = p.second;
+      if (other_lb_id != lb_id) {
+        orphaned_store->Resume();
+        AssignOrphanedStore(orphaned_store.get(), lb_id);
+      }
+    }
+  }
+  // The first connected balancer will adopt the kInvalidLbId.
+  if (per_balancer_stores_.size() == 1) {
+    SetUpForNewLbId(kInvalidLbId, '');
+    ReportStreamClosed(kInvalidLbId);
+  }
 }
