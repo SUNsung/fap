874285d84d7d6f6cc9c33eb45db80b4d7a92c8bc
SUNsung <weatherius@gmail.com>
2019-06-09 06:37:14 +0300

    Rewrite [22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb] File in: 2019-06-09 06:37:13

--- ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
+++ ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
@@ -1,25 +1,44 @@
 
         
-        describe GivenDailyLike do
+            %w[iOS macOS].each do |platform|
+        abstract_target '#{platform} Pods' do
+            project '#{platform} Modules.xcodeproj'
     
-        def run(action_named: nil, action_class_ref: nil, parameter_map: nil)
-      action_return = runner.execute_action(action_named, action_class_ref, [parameter_map], custom_dir: '.')
-      return action_return
+          private
+    
+    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
+Gem.use_paths(LogStash::Environment.logstash_gem_home)
+    
+      parameter 'file', 'the package file name', :attribute_name => :package_file, :required => true
+    
+        desc 'Run one single machine acceptance test'
+    task :single, :machine do |t, args|
+      ENV['LS_VAGRANT_HOST']  = args[:machine]
+      exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/**/**/*_spec.rb']]))
     end
+  end
+end
+
     
-        def root_url
-      context[:root_url]
-    end
+              it 'successfully install the plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest')
+          end
     
-        delegate :empty?, :blank?, to: :pages
+          # Returns the delta between this element's value and the argument's.
+      #
+      # @note Keyword splats always return a delta of 0
+      #
+      # @return [Integer] the delta between the two values
+      def value_delta(other)
+        HashElementDelta.new(self, other).value_delta
+      end
     
-              node.before(%(<div class='pre-title'>#{node['title']}</div>)) if node['title']
-    
-            css('.c10', '.showcase', '.showcase-content', '.l-main-section', 'div.div', 'div[flex]', 'code-tabs', 'md-card', 'md-card-content', 'div:not([class])', 'footer', '.card-row', '.card-row-container', 'figure', 'blockquote', 'exported', 'defined', 'div.ng-scope', '.code-example header', 'section.desc', '.row', '.dart-api-entry-main', '.main-content', 'section.summary', 'span.signature').each do |node|
-          node.before(node.children).remove
-        end
-    
-            # Remove examples
-        css('.runnable-example').each do |node|
-          node.parent.remove
+            def declaration_with_comment(node)
+          buffer = processed_source.buffer
+          begin_pos = get_source_range(node, comments_as_separators).begin_pos
+          end_line = buffer.line_for_position(node.loc.expression.end_pos)
+          end_pos = buffer.line_range(end_line).end_pos
+          Parser::Source::Range.new(buffer, begin_pos, end_pos)
         end
