4b125106d204ca1652a04e0cce8a0060a666e43b
SUNsung <weatherius@gmail.com>
2019-06-10 13:59:02 +0300

    Rewrite [29q3Id5YRF2k29rYwjIAeqvbk29uQXNMruuUc295GL.rb] File in: 2019-06-10 13:59:02

--- ruby/29q3Id5YRF2k29rYwjIAeqvbk29uQXNMruuUc295GL.rb
+++ ruby/29q3Id5YRF2k29rYwjIAeqvbk29uQXNMruuUc295GL.rb
@@ -1,99 +1,23 @@
 
         
-            if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
+                  issue.label_names.each do |label_name|
+            # Although unlikely it's technically possible for an issue to be
+            # given a label that was created and assigned after we imported all
+            # the project's labels.
+            next unless (label_id = label_finder.id_for(label_name))
     
-          # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
+          def initialize(app)
+        @app = app
       end
     
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-          def mailer_sender(mapping, sender = :from)
-        default_sender = default_params[sender]
-        if default_sender.present?
-          default_sender.respond_to?(:to_proc) ? instance_eval(&default_sender) : default_sender
-        elsif Devise.mailer_sender.is_a?(Proc)
-          Devise.mailer_sender.call(mapping.name)
-        else
-          Devise.mailer_sender
+            def keyword_splat?
+          [first, second].any?(&:kwsplat_type?)
         end
       end
     
-        # Create magic predicates for verifying what module is activated by this map.
-    # Example:
-    #
-    #   def confirmable?
-    #     self.modules.include?(:confirmable)
-    #   end
-    #
-    def self.add_module(m)
-      class_eval <<-METHOD, __FILE__, __LINE__ + 1
-        def #{m}?
-          self.modules.include?(:#{m})
-        end
-      METHOD
-    end
+            def on_send(node)
+          erb_new_with_non_keyword_arguments(node) do |arguments|
+            return if correct_arguments?(arguments)
     
-        # put the non-standard line terminations back to normal
-    # gah.  not having look behinds suck,
-    header.gsub!(/([^\r])\n/n,'\1' + '\r\n')
-    
-            # Creates a connection through a Rex socket
-        #
-        # @return [Rex::Socket::Tcp]
-        # @raise [RuntimeError] if the connection can not be created
-        def connect
-          return connection if connection
-    
-                seq_asn1.to_der
-          end
-    
-              # Encodes the checksum field
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_checksum
-            OpenSSL::ASN1::OctetString.new(checksum)
-          end
-        end
-      end
-    end
-  end
-end
-    
-              # Decodes the Rex::Proto::Kerberos::Model::Element from the input. This
-          # method has been designed to be overridden by subclasses.
-          #
-          # @raise [NoMethodError]
-          def decode(input)
-            raise ::NoMethodError, 'Method designed to be overridden'
-          end
-    
-              # Decodes the type from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_type(input)
-            input.value[0].value.to_i
-          end
-    
-      def vagrant_cli_command(command)
-    puts '[vagrant] #{command}'
-    stdout, stderr, status = Dir.chdir(VAGRANT_ROOT) do
-      Open3.capture3('#{VAGRANT_BIN} #{command}')
-    end
-    
-    set_if_empty :pty, false
+        # data tar.
+    tar_path(staging_path(''), datatar_path)
