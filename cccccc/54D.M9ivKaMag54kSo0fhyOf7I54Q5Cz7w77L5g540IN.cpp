7b0c991986483058019657254d9ac17ca6e0d6ec
SUNsung <weatherius@gmail.com>
2019-06-10 09:34:59 +0300

    Rewrite [54D.M9ivKaMag54kSo0fhyOf7I54Q5Cz7w77L5g540IN.cpp] File in: 2019-06-10 09:34:58

--- cccccc/54D.M9ivKaMag54kSo0fhyOf7I54Q5Cz7w77L5g540IN.cpp
+++ cccccc/54D.M9ivKaMag54kSo0fhyOf7I54Q5Cz7w77L5g540IN.cpp
@@ -1,247 +1,205 @@
 
         
-            /** Colorize an icon (given filename) with the icon color */
-    QIcon SingleColorIcon(const QString& filename) const;
+          // Next, add each of the requirements (mapped from the requirement's
+  // interface types into the abstract type parameters).
+  for (auto &rawReq : reqSig->getRequirements()) {
+    if (auto req = rawReq.subst(reqToSyntheticEnvMap))
+      builder.addRequirement(*req, source, conformanceDC->getParentModule());
+  }
     
-    
-    {    Lock& lock;
-    Lock templock;
-};
-    
-    
-    {    secp256k1_fe_sqr(&z12, &a->z);
-    u1 = a->x; secp256k1_fe_normalize_weak(&u1);
-    secp256k1_fe_mul(&u2, &b->x, &z12);
-    s1 = a->y; secp256k1_fe_normalize_weak(&s1);
-    secp256k1_fe_mul(&s2, &b->y, &z12); secp256k1_fe_mul(&s2, &s2, &a->z);
-    secp256k1_fe_negate(&h, &u1, 1); secp256k1_fe_add(&h, &u2);
-    secp256k1_fe_negate(&i, &s1, 1); secp256k1_fe_add(&i, &s2);
-    if (secp256k1_fe_normalizes_to_zero_var(&h)) {
-        if (secp256k1_fe_normalizes_to_zero_var(&i)) {
-            secp256k1_gej_double_var(r, a, rzr);
-        } else {
-            if (rzr != NULL) {
-                secp256k1_fe_set_int(rzr, 0);
-            }
-            r->infinity = 1;
-        }
-        return;
-    }
-    secp256k1_fe_sqr(&i2, &i);
-    secp256k1_fe_sqr(&h2, &h);
-    secp256k1_fe_mul(&h3, &h, &h2);
-    if (rzr != NULL) {
-        *rzr = h;
-    }
-    secp256k1_fe_mul(&r->z, &a->z, &h);
-    secp256k1_fe_mul(&t, &u1, &h2);
-    r->x = t; secp256k1_fe_mul_int(&r->x, 2); secp256k1_fe_add(&r->x, &h3); secp256k1_fe_negate(&r->x, &r->x, 3); secp256k1_fe_add(&r->x, &i2);
-    secp256k1_fe_negate(&r->y, &r->x, 5); secp256k1_fe_add(&r->y, &t); secp256k1_fe_mul(&r->y, &r->y, &i);
-    secp256k1_fe_mul(&h3, &h3, &s1); secp256k1_fe_negate(&h3, &h3, 1);
-    secp256k1_fe_add(&r->y, &h3);
+    void CacheImpl::destroy() {
+  removeAll();
+  delete static_cast<DefaultCache*>(Impl);
 }
     
-    void test_ecdh_generator_basepoint(void) {
-    unsigned char s_one[32] = { 0 };
-    secp256k1_pubkey point[2];
-    int i;
+    % for start_code_point, end_code_point, value in break_table.property_value_ranges:
+%   if start_code_point == 0:
+  if (C <= ${end_code_point})
+%   else:
+  if (C >= ${start_code_point} && C <= ${end_code_point})
+%   end
+    return GraphemeClusterBreakProperty::${value};
+% end
+    
+      if (auto ptr = type->getAs<clang::PointerType>()) {
+    auto pointee = ptr->getPointeeType();
     }
     
-        if (!bytes) return;
+      Optional<unsigned> selfIndex = None;
     
-    // Deserialize incoming server stats. Returns the number of bytes deserialized.
-size_t ServerStatsDeserialize(const char* buf, size_t buf_size,
-                              uint64_t* server_elapsed_time);
+    void ModuleWrapJobAction::anchor() {}
     
-      void StartTransportStreamOpBatch(grpc_call_element* elem,
-                                   TransportStreamOpBatch* op) override;
-    
-      void Add(const std::function<void()>& callback) override;
-    
-    #include <grpc/support/port_platform.h>
-    
-    #include <dmlc/registry.h>
-#include <xgboost/base.h>
-#include <xgboost/data.h>
-#include <functional>
-#include <string>
-#include <utility>
-#include <vector>
-#include '../../src/gbm/gblinear_model.h'
-#include '../../src/common/host_device_vector.h'
-    
-    
-    {    for (size_t i = 0; i < batch.size; ++i) {
-      offset_[i + 1] = (i + 1) * num_col_;
-      Row<IndexType> row = batch[i];
-      for (uint32_t j = 0; j < num_col_; ++j) {
-        dense_index_[i * num_col_ + j] = j;
-      }
-      for (unsigned k = 0; k < row.length; ++k) {
-        uint32_t index = row.get_index(k);
-        CHECK_LT(index, num_col_)
-            << 'Featuere index larger than num_col';
-        dense_value_[i * num_col_ + index]  = row.get_value(k);
-      }
-    }
-    out_ = batch;
-    out_.index = dmlc::BeginPtr(dense_index_);
-    out_.value = dmlc::BeginPtr(dense_value_);
-    out_.offset = dmlc::BeginPtr(offset_);
-    return true;
-  }
-    
-    namespace xgboost {
-namespace common {
-/*! \brief buffer reader of the stream that allows you to get */
-class StreamBufferReader {
- public:
-  explicit StreamBufferReader(size_t buffer_size)
-      :stream_(NULL),
-       read_len_(1), read_ptr_(1) {
-    buffer_.resize(buffer_size);
-  }
-  /*!
-   * \brief set input stream
-   */
-  inline void set_stream(dmlc::Stream *stream) {
-    stream_ = stream;
-    read_len_ = read_ptr_ = 1;
-  }
-  /*!
-   * \brief allows quick read using get char
-   */
-  inline char GetChar(void) {
-    while (true) {
-      if (read_ptr_ < read_len_) {
-        return buffer_[read_ptr_++];
-      } else {
-        read_len_ = stream_->Read(&buffer_[0], buffer_.length());
-        if (read_len_ == 0) return EOF;
-        read_ptr_ = 0;
-      }
-    }
-  }
-  /*! \brief whether we are reaching the end of file */
-  inline bool AtEnd(void) const {
-    return read_len_ == 0;
-  }
-    }
-    }
+    class LLVM_LIBRARY_VISIBILITY GenericUnix : public ToolChain {
+protected:
+  InvocationInfo constructInvocation(const InterpretJobAction &job,
+                                     const JobContext &context) const override;
+  InvocationInfo constructInvocation(const AutolinkExtractJobAction &job,
+                                     const JobContext &context) const override;
     }
     
-    /*! \brief collection of rowset */
-class RowSetCollection {
- public:
-  /*! \brief data structure to store an instance set, a subset of
-   *  rows (instances) associated with a particular node in a decision
-   *  tree. */
-  struct Elem {
-    const size_t* begin{nullptr};
-    const size_t* end{nullptr};
-    int node_id{-1};
-      // id of node associated with this instance set; -1 means uninitialized
-    Elem()
-         = default;
-    Elem(const size_t* begin,
-         const size_t* end,
-         int node_id)
-        : begin(begin), end(end), node_id(node_id) {}
-    }
-    }
+      if (FilelistPathArg ? readInputFilesFromFilelist()
+                      : readInputFilesFromCommandLine())
+    return None;
+  Optional<std::set<StringRef>> primaryFiles = readPrimaryFiles();
+  if (!primaryFiles)
+    return None;
     
-    BatchSet SimpleDMatrix::GetColumnBatches() {
-  // column page doesn't exist, generate it
-  if (!column_page_) {
-    auto page = dynamic_cast<SimpleCSRSource*>(source_.get())->page_;
-    column_page_.reset(
-        new SparsePage(page.GetTranspose(source_->info.num_col_)));
-  }
-  auto begin_iter =
-      BatchIterator(new SimpleBatchIteratorImpl(column_page_.get()));
-  return BatchSet(begin_iter);
-}
+    #define TEGRA_MUL(src1, sz1, src2, sz2, dst, sz, w, h, scale) \
+( \
+    CAROTENE_NS::isSupportedConfiguration() ? \
+    CAROTENE_NS::mul(CAROTENE_NS::Size2D(w, h), \
+                     src1, sz1, \
+                     src2, sz2, \
+                     dst, sz, \
+                     scale, \
+                     CAROTENE_NS::CONVERT_POLICY_SATURATE), \
+    CV_HAL_ERROR_OK \
+    : CV_HAL_ERROR_NOT_IMPLEMENTED \
+)
     
-      bool operator < (const Extension_call_args & ) const;
+        bool isGaussianPyramidDownU8Supported(const Size2D &srcSize, const Size2D &dstSize, u8 cn);
+    void gaussianPyramidDown(const Size2D &srcSize,
+                             const u8 *srcBase, ptrdiff_t srcStride,
+                             const Size2D &dstSize,
+                             u8 *dstBase, ptrdiff_t dstStride, u8 cn);
     
-    
-  virtual ~ExtensionManager_getQueryColumns_pargs() throw();
-  const std::string* sql;
-    
-    #include <osquery/tables.h>
-    
-    /// Define short-hand shell switches.
-SHELL_FLAG(bool, L, false, 'List all table names');
-SHELL_FLAG(string, A, '', 'Select all from a table');
-    
-    #include <osquery/logger.h>
-#include <osquery/sql.h>
-#include <osquery/tables.h>
-#include <osquery/tables/smart/smart_drives.h>
-    
-    #pragma once
-    
-    EnterExitJoiner::KeyType createKey(EventType const type,
-                                   __s32 const pid,
-                                   __s32 const tgid) {
-  auto key = EnterExitJoiner::KeyType(static_cast<std::uint32_t>(pid));
-  key <<= 32;
-  key |= static_cast<std::uint32_t>(tgid);
-  key <<= 32;
-  key |= static_cast<std::uint32_t>(type);
-  return key;
-}
-    
-      // header
-  readBytes(fp, buf, buf.size(), 8);
-  if (memcmp(header, buf, 8) == 0) {
-    version = 3;
-  }
-  else if (memcmp(headerCompat, buf, 8) == 0) {
-    version = 2;
-  }
-  else {
-    throw DL_ABORT_EX(fmt('Failed to load DHT routing table from %s. cause:%s',
-                          filename.c_str(), 'bad header'));
-  }
-    
-    class DHTTaskExecutor {
-private:
-  int numConcurrent_;
-  std::vector<std::shared_ptr<DHTTask>> execTasks_;
-  std::deque<std::shared_ptr<DHTTask>> queue_;
-    }
-    
-    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createPeerLookupTask(
-    const std::shared_ptr<DownloadContext>& ctx, uint16_t tcpPort,
-    const std::shared_ptr<PeerStorage>& peerStorage)
+    void bitwiseNot(const Size2D &size,
+                const u8 *srcBase, ptrdiff_t srcStride,
+                u8 *dstBase, ptrdiff_t dstStride)
 {
-  auto task = std::make_shared<DHTPeerLookupTask>(ctx, tcpPort);
-  // TODO this may be not freed by RequestGroup::releaseRuntimeResource()
-  task->setPeerStorage(peerStorage);
-  setCommonProperty(task);
-  return task;
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
+    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+    }
+    
+        size_t i = 0;
+    s32* dsta = internal::getRowPtr(dstBase, dstStride, 0);
+    for (; i < size.height-1; i+=2)
+    {
+        //vertical convolution
+        ptrdiff_t idx_rm1 = internal::borderInterpolate(i - 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
+        ptrdiff_t idx_rp2 = internal::borderInterpolate(i + 2, size.height, borderType, borderMargin.top, borderMargin.bottom);
+    }
+    
+    
+    {
+    {        for (; dj < size.width; sj += 4, ++dj)
+        {
+            dst[dj] = src[sj + coi];
+        }
+    }
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)coi;
+#endif
 }
     
-      virtual void
-  addPeriodicTask2(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
     
-    const std::string DHTUnknownMessage::E('e');
+    {    func(size, srcBase, srcStride, dstBase, dstStride, cpolicy);
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)shift;
+    (void)cpolicy;
+#endif
+}
     
-        // out_of_range.109
-    try
-    {
-        // try to use an array index that is not a number
-        json::const_reference ref = j.at('/array/one'_json_pointer);
+    
+    {        for (; i < size.width; i++)
+            result += (src[i] != 0)?1:0;
+        if (result < 0)//saturate in case of overflow ~ 8GB of non-zeros...
+        {
+            return 0x7fFFffFF;
+        }
     }
-    catch (json::parse_error& e)
+    return result;
+#else
+    (void)_size;
+    (void)srcBase;
+    (void)srcStride;
+    
+    f64 dotProduct(const Size2D &_size,
+               const s8 * src0Base, ptrdiff_t src0Stride,
+               const s8 * src1Base, ptrdiff_t src1Stride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    Size2D size(_size);
+    if (src0Stride == src1Stride &&
+        src0Stride == (ptrdiff_t)(size.width))
     {
-        std::cout << e.what() << '\n';
+        size.width *= size.height;
+        size.height = 1;
+    }
     }
     
-        // create an object from std::multimap
-    std::multimap<std::string, bool> c_mmap
+                // combine 3 'shifted' vectors
+            t0 = vextq_u16(tprev, tcurr, 7);
+            t1 = tcurr;
+            t2 = vextq_u16(tcurr, tnext, 1);
+    
+    template <typename T, int elsize> struct vtail
+{
+    static inline void inRange(const T *, const T *, const T *,
+                               u8 *, size_t &, size_t)
     {
-        {'one', true}, {'two', true}, {'three', false}, {'three', true}
-    };
-    json j_mmap(c_mmap); // only one entry for key 'three' is used
+        //do nothing since there couldn't be enough data
+    }
+};
+template <typename T> struct vtail<T, 2>
+{
+    static inline void inRange(const T * src, const T * rng1, const T * rng2,
+                               u8 * dst, size_t &x, size_t width)
+    {
+        typedef typename internal::VecTraits<T>::vec128 vec128;
+        typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
+        //There no more than 15 elements in the tail, so we could handle 8 element vector only once
+        if( x + 8 < width)
+        {
+             vec128  vs = internal::vld1q( src + x);
+             vec128 vr1 = internal::vld1q(rng1 + x);
+             vec128 vr2 = internal::vld1q(rng2 + x);
+            uvec128  vd = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
+            internal::vst1(dst + x, internal::vmovn(vd));
+            x+=8;
+        }
+    }
+};
+template <typename T> struct vtail<T, 1>
+{
+    static inline void inRange(const T * src, const T * rng1, const T * rng2,
+                               u8 * dst, size_t &x, size_t width)
+    {
+        typedef typename internal::VecTraits<T>::vec128 vec128;
+        typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
+        typedef typename internal::VecTraits<T>::vec64 vec64;
+        typedef typename internal::VecTraits<T>::unsign::vec64 uvec64;
+        //There no more than 31 elements in the tail, so we could handle once 16+8 or 16 or 8 elements
+        if( x + 16 < width)
+        {
+             vec128  vs = internal::vld1q( src + x);
+             vec128 vr1 = internal::vld1q(rng1 + x);
+             vec128 vr2 = internal::vld1q(rng2 + x);
+            uvec128  vd = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
+            internal::vst1q(dst + x, vd);
+            x+=16;
+        }
+        if( x + 8 < width)
+        {
+             vec64  vs = internal::vld1( src + x);
+             vec64 vr1 = internal::vld1(rng1 + x);
+             vec64 vr2 = internal::vld1(rng2 + x);
+            uvec64  vd = internal::vand(internal::vcge(vs, vr1), internal::vcge(vr2, vs));
+            internal::vst1(dst + x, vd);
+            x+=8;
+        }
+    }
+};
