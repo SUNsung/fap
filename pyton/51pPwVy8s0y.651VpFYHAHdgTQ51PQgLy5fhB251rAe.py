96fb496ecbb20e7a482a669ed3a8846066a9c162
SUNsung <weatherius@gmail.com>
2019-06-10 16:29:06 +0300

    Rewrite [51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py] File in: 2019-06-10 16:29:05

--- pyton/51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py
+++ pyton/51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py
@@ -1,173 +1,169 @@
 
         
-        
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
+        lazy_extractors_filename = sys.argv[1]
+if os.path.exists(lazy_extractors_filename):
+    os.remove(lazy_extractors_filename)
     
     
-# find the correct sorting and add the required base classes so that sublcasses
-# can be correctly created
-classes = _ALL_CLASSES[:-1]
-ordered_cls = []
-while classes:
-    for c in classes[:]:
-        bases = set(c.__bases__) - set((object, InfoExtractor, SearchInfoExtractor))
-        stop = False
-        for b in bases:
-            if b not in classes and b not in ordered_cls:
-                if b.__name__ == 'GenericIE':
-                    exit()
-                classes.insert(0, b)
-                stop = True
-        if stop:
-            break
-        if all(b in ordered_cls for b in bases):
-            ordered_cls.append(c)
-            classes.remove(c)
-            break
-ordered_cls.append(_ALL_CLASSES[-1])
+def _is_empty(d):
+    return not bool(os.listdir(d))
     
-        outfile, = args
+    import unittest
     
-    # Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named 'default.css' will overwrite the builtin 'default.css'.
-html_static_path = ['_static']
-    
-        def pop(self, exc=_sentinel):
-        '''Pops the app context.'''
-        try:
-            self._refcnt -= 1
-            if self._refcnt <= 0:
-                if exc is _sentinel:
-                    exc = sys.exc_info()[1]
-                self.app.do_teardown_appcontext(exc)
-        finally:
-            rv = _app_ctx_stack.pop()
-        assert rv is self, 'Popped wrong app context.  (%r instead of %r)' % (rv, self)
-        appcontext_popped.send(self.app)
-    
-            # If the request method is HEAD and we don't have a handler for it
-        # retry with GET.
-        if meth is None and request.method == 'HEAD':
-            meth = getattr(self, 'get', None)
-    
-        assert c.get('/').data == b'GET'
-    assert c.post('/').data == b'POST'
-    assert c.put('/').status_code == 405
-    meths = parse_set_header(c.open('/', method='OPTIONS').headers['Allow'])
-    assert sorted(meths) == ['GET', 'HEAD', 'OPTIONS', 'POST']
-    
-            if error is None:
-            # the name is available, store it in the database and go to
-            # the login page
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password)),
-            )
-            db.commit()
-            return redirect(url_for('auth.login'))
-    
-        with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+        def test_call(self):
+        jsi = JSInterpreter('''
+        function x() { return 2; }
+        function y(a) { return x() + a; }
+        function z() { return y(3); }
+        ''')
+        self.assertEqual(jsi.call_function('z'), 5)
     
     
-def x_robots_tag(func):
-    @wraps(func)
-    def inner(request, *args, **kwargs):
-        response = func(request, *args, **kwargs)
-        response['X-Robots-Tag'] = 'noindex, noodp, noarchive'
-        return response
-    return inner
-    
-    '''
-requests._internal_utils
-~~~~~~~~~~~~~~
+class TestThePlatformSubtitles(BaseTestSubtitles):
+    # from http://www.3playmedia.com/services-features/tools/integrations/theplatform/
+    # (see http://theplatform.com/about/partners/type/subtitles-closed-captioning/)
+    url = 'theplatform:JFUjUE1_ehvq'
+    IE = ThePlatformIE
     
     
-@pytest.fixture
-def httpbin(httpbin):
-    return prepare_url(httpbin)
+class TestUnicodeLiterals(unittest.TestCase):
+    def test_all_files(self):
+        for dirpath, dirnames, filenames in os.walk(rootDir):
+            for ignore_dir in IGNORED_DIRS:
+                if ignore_dir in dirnames:
+                    # If we remove the directory from dirnames os.walk won't
+                    # recurse into it
+                    dirnames.remove(ignore_dir)
+            for basename in filenames:
+                if not basename.endswith('.py'):
+                    continue
+                if basename in IGNORED_FILES:
+                    continue
     
-            new_content = sock.recv(chunks)
-        if not new_content:
-            break
-    
-    # TODO: response is the only one
-    
-        text_302 = (b'HTTP/1.1 302 FOUND\r\n'
-                b'Content-Length: 0\r\n'
-                b'Location: /\r\n\r\n')
-    
-        :param method: method for the new :class:`Request` object.
-    :param url: URL for the new :class:`Request` object.
-    :param params: (optional) Dictionary, list of tuples or bytes to send
-        in the query string for the :class:`Request`.
-    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-        object to send in the body of the :class:`Request`.
-    :param json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
-    :param headers: (optional) Dictionary of HTTP Headers to send with the :class:`Request`.
-    :param cookies: (optional) Dict or CookieJar object to send with the :class:`Request`.
-    :param files: (optional) Dictionary of ``'name': file-like-objects`` (or ``{'name': file-tuple}``) for multipart encoding upload.
-        ``file-tuple`` can be a 2-tuple ``('filename', fileobj)``, 3-tuple ``('filename', fileobj, 'content_type')``
-        or a 4-tuple ``('filename', fileobj, 'content_type', custom_headers)``, where ``'content-type'`` is a string
-        defining the content type of the given file and ``custom_headers`` a dict-like object containing additional headers
-        to add for the file.
-    :param auth: (optional) Auth tuple to enable Basic/Digest/Custom HTTP Auth.
-    :param timeout: (optional) How many seconds to wait for the server to send data
-        before giving up, as a float, or a :ref:`(connect timeout, read
-        timeout) <timeouts>` tuple.
-    :type timeout: float or tuple
-    :param allow_redirects: (optional) Boolean. Enable/disable GET/OPTIONS/POST/PUT/PATCH/DELETE/HEAD redirection. Defaults to ``True``.
-    :type allow_redirects: bool
-    :param proxies: (optional) Dictionary mapping protocol to the URL of the proxy.
-    :param verify: (optional) Either a boolean, in which case it controls whether we verify
-            the server's TLS certificate, or a string, in which case it must be a path
-            to a CA bundle to use. Defaults to ``True``.
-    :param stream: (optional) if ``False``, the response content will be immediately downloaded.
-    :param cert: (optional) if String, path to ssl client cert file (.pem). If Tuple, ('cert', 'key') pair.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
+    logger = logging.getLogger(__name__)
     
     # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
+extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-]
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode']
+    
+      # Ignore 'nextgroup=' argument in first position and the arguments
+  # 'skipwhite', 'skipnl', and 'skipempty' that immediately come after.
+  nextgroup_at_start = False
+  if words[ 0 ].startswith( 'nextgroup=' ):
+    nextgroup_at_start = True
+    words = words[ 1: ]
+    
+          # We set up a fake response.
+      with patch( 'ycm.client.base_request._JsonFromFuture',
+                  side_effect = response_method ):
+        yield
+    
+        exception = RuntimeError( 'Check client handles exception' )
+    with patch.object( ycm._message_poll_request,
+                       '_response_future',
+                       new = MockAsyncServerResponseException( exception ) ):
+      ycm.OnPeriodicTick() # Uses ycm._message_poll_request ...
+  '''
+  return mock.MagicMock( wraps = FakeFuture( True, None, exception ) )
     
     
-def get_service_name_from_network_mode(network_mode):
-    return get_source_name_from_network_mode(network_mode, 'service')
+def KeywordsFromSyntaxListOutput_JavaSyntax_test():
+  expected_keywords = (
+    'code', 'text', 'cols', 'datetime', 'disabled', 'shape', 'codetype', 'alt',
+    'compact', 'style', 'valuetype', 'short', 'finally', 'continue', 'extends',
+    'valign', 'bordercolor', 'do', 'return', 'rel', 'rules', 'void',
+    'nohref', 'abbr', 'background', 'scrolling', 'instanceof', 'name',
+    'summary', 'try', 'default', 'noshade', 'coords', 'dir', 'frame', 'usemap',
+    'ismap', 'static', 'hspace', 'vlink', 'for', 'selected', 'rev', 'vspace',
+    'content', 'method', 'version', 'volatile', 'above', 'new', 'charoff',
+    'public', 'alink', 'enum', 'codebase', 'if', 'noresize', 'interface',
+    'checked', 'byte', 'super', 'throw', 'src', 'language', 'package',
+    'standby', 'script', 'longdesc', 'maxlength', 'cellpadding', 'throws',
+    'tabindex', 'color', 'colspan', 'accesskey', 'float', 'while', 'private',
+    'height', 'boolean', 'wrap', 'prompt', 'nowrap', 'size', 'rows', 'span',
+    'clip', 'bgcolor', 'top', 'long', 'start', 'scope', 'scheme', 'type',
+    'final', 'lang', 'visibility', 'else', 'assert', 'transient', 'link',
+    'catch', 'true', 'serializable', 'target', 'lowsrc', 'this', 'double',
+    'align', 'value', 'cite', 'headers', 'below', 'protected', 'declare',
+    'classid', 'defer', 'false', 'synchronized', 'int', 'abstract', 'accept',
+    'hreflang', 'char', 'border', 'id', 'native', 'rowspan', 'charset',
+    'archive', 'strictfp', 'readonly', 'axis', 'cellspacing', 'profile',
+    'multiple', 'object', 'action', 'pagex', 'pagey', 'marginheight', 'data',
+    'class', 'frameborder', 'enctype', 'implements', 'break', 'gutter', 'url',
+    'clear', 'face', 'switch', 'marginwidth', 'width', 'left' )
     
-    import os
     
-        def test_parse_v1_from_container(self):
-        volume_from = VolumeFromSpec.parse('foo:ro', self.services, V1)
-        assert volume_from == VolumeFromSpec('foo', 'ro', 'container')
+# -- Options for HTML output ---------------------------------------------------
     
-            parallel_execute(
-            objects=objects,
-            func=process,
-            get_name=lambda obj: obj,
-            msg='Processing',
-            get_deps=get_deps,
+    
+def main():
+    parse_command_line()
+    
+            return remaining
+    
+        def add_handler(
+        self, fd: Union[int, _Selectable], handler: Callable[..., None], events: int
+    ) -> None:
+        fd, fileobj = self.split_fd(fd)
+        if fd in self.handlers:
+            raise ValueError('fd %s added twice' % fd)
+        self.handlers[fd] = (fileobj, handler)
+        if events & IOLoop.READ:
+            self.asyncio_loop.add_reader(fd, self._handle_events, fd, IOLoop.READ)
+            self.readers.add(fd)
+        if events & IOLoop.WRITE:
+            self.asyncio_loop.add_writer(fd, self._handle_events, fd, IOLoop.WRITE)
+            self.writers.add(fd)
+    
+        def generate(self, writer: '_CodeWriter') -> None:
+        assert writer.loader is not None
+        included = writer.loader.load(self.name, self.template_name)
+        with writer.include(included, self.line):
+            included.file.body.generate(writer)
+    
+        def load(self, loader):
+        loader.add_option(
+            'stickycookie', Optional[str], None,
+            'Set sticky cookie filter. Matched against requests.'
         )
     
-        def test_preserves_unicode_sequences_within_lines(self):
-        string = u'a\u2022c\n'
+        def websocket_start(self, f):
+        if ctx.options.stream_websockets:
+            f.stream = True
+            ctx.log.info('Streaming WebSocket messages between {client} and {server}'.format(
+                client=human.format_address(f.client_conn.address),
+                server=human.format_address(f.server_conn.address))
+            )
+
     
-          output_spec = tf.contrib.tpu.TPUEstimatorSpec(
-          mode=mode,
-          loss=total_loss,
-          train_op=train_op,
-          scaffold_fn=scaffold_fn)
-    elif mode == tf.estimator.ModeKeys.EVAL:
+        def call(self, path: str, *args: typing.Sequence[typing.Any]) -> typing.Any:
+        '''
+            Call a command with native arguments. May raise CommandError.
+        '''
+        if path not in self.commands:
+            raise exceptions.CommandError('Unknown command: %s' % path)
+        return self.commands[path].func(*args)
     
-        all_nbest_json[example.qas_id] = nbest_json
+    		page.addToken(0x05, 'DisplayName')
+		page.addToken(0x06, 'Phone')
+		page.addToken(0x07, 'Office')
+		page.addToken(0x08, 'Title')
+		page.addToken(0x09, 'Company')
+		page.addToken(0x0A, 'Alias')
+		page.addToken(0x0B, 'FirstName')
+		page.addToken(0x0C, 'LastName')
+		page.addToken(0x0D, 'HomePhone')
+		page.addToken(0x0E, 'MobilePhone')
+		page.addToken(0x0F, 'EmailAddress')
+		page.addToken(0x10, 'Picture')
+		page.addToken(0x11, 'Status')
+		page.addToken(0x12, 'Data')
+		self.codePages.append(page)
+		# endregion
+    
+    class BiDi:
