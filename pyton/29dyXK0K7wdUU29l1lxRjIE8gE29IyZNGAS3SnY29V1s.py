9e30b81e1729d805f4449fe77922c270d7b269d1
SUNsung <weatherius@gmail.com>
2019-06-11 02:05:50 +0300

    Rewrite [29dyXK0K7wdUU29l1lxRjIE8gE29IyZNGAS3SnY29V1s.py] File in: 2019-06-11 02:05:49

--- pyton/29dyXK0K7wdUU29l1lxRjIE8gE29IyZNGAS3SnY29V1s.py
+++ pyton/29dyXK0K7wdUU29l1lxRjIE8gE29IyZNGAS3SnY29V1s.py
@@ -1,82 +1,166 @@
 
         
-                When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.map[query]
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(query, results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
+          Args:
+    cm: The confusion matrix.
+    labels: The class names.
+  '''
+  columnwidth = 10
+  empty_cell = ' ' * columnwidth
+  short_labels = [label[:12].rjust(10, ' ') for label in labels]
+    
+          with tf.variable_scope('Candidate'):
+        c_x = 0.0
+        if x is not None:
+          c_x = linear(x, self._num_units, name='x_2_c', do_bias=False,
+                       alpha=self._input_weight_scale,
+                       normalized=False,
+                       collections=self._input_collections)
+        c_rh = linear(r*h, self._num_units, name='rh_2_c', do_bias=True,
+                     alpha=self._rec_weight_scale,
+                     collections=self._rec_collections)
+        c = tf.tanh(c_x + c_rh)
+    
+      Args:
+    data_fname: The filename of teh file in which to write the data.
+    data_dict:  The dictionary of data to write. The keys are strings
+      and the values are numpy arrays.
+    use_json (optional): human readable format for simple items
+    compression (optional): The compression to use for h5py (disabled by
+      default because the library borks on scalars, otherwise try 'gzip').
+  '''
+    
+      softmax_weights = sess.run(t['softmax_weights'])
+  fname = FLAGS.save_dir + '/embeddings_softmax.npy'
+  with tf.gfile.Open(fname, mode='w') as f:
+    np.save(f, softmax_weights)
+  sys.stderr.write('Finished softmax weights\n')
+    
+      def reset(self):
+    self.sess.run(self.tensors['states_init'])
+    
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+        lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+                 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
+                 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
+                 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
+                 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331,
+                 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
+                 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467,
+                 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563,
+                 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631,
+                 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709,
+                 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+                 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877,
+                 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967,
+                 971, 977, 983, 991, 997]
+    
+    This is a pure Python implementation of Dynamic Programming solution to the longest_sub_array problem.
+    
+    
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+    
+    
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+    
+    
+def test_fashion_mnist():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+        assert len(x_train) == len(y_train) == 60000
+        assert len(x_test) == len(y_test) == 10000
+    
+        ```python
+    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
+    > labels
+    array([0, 2, 1, 2, 0])
+    # `to_categorical` converts this into a matrix with as many
+    # columns as there are classes. The number of rows
+    # stays the same.
+    > to_categorical(labels)
+    array([[ 1.,  0.,  0.],
+           [ 0.,  0.,  1.],
+           [ 0.,  1.,  0.],
+           [ 0.,  0.,  1.],
+           [ 1.,  0.,  0.]], dtype=float32)
+    ```
+    '''
+    
+    # Model creation using tensors from the get_next() graph node.
+inputs, targets = iterator.get_next()
+model_input = layers.Input(tensor=inputs)
+model_output = cnn_layers(model_input)
+train_model = keras.models.Model(inputs=model_input, outputs=model_output)
+    
+    x_train = x_train.reshape(x_train.shape[0], -1, 1)
+x_test = x_test.reshape(x_test.shape[0], -1, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+    
+    import numpy as np
+import keras
+from keras.datasets import reuters
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.preprocessing.text import Tokenizer
+    
+            self.title = match1(html, r'<meta property='og:title' content='([^']*)'')
+    
+    site_info = 'CBS.com'
+download = cbs_download
+download_playlist = playlist_not_supported('cbs')
 
     
-        def _colision_resolution(self, key, data=None):
-        new_key = self.hash_function(key + 1)
+    def giphy_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
     
-    def getBlock(bitString):
-	'''[summary]
-	Iterator:
-		Returns by each call a list of length 16 with the 32 bit
-		integer blocks. 
+            else:
+            # gallery image
+            content = get_content(self.url)
+            image = json.loads(match1(content, r'image\s*:\s*({.*}),'))
+            ext = image['ext']
+            self.streams = {
+                'original': {
+                    'src': ['http://i.imgur.com/%s%s' % (image['hash'], ext)],
+                    'size': image['size'],
+                    'container': ext[1:]
+                },
+                'thumbnail': {
+                    'src': ['http://i.imgur.com/%ss%s' % (image['hash'], '.jpg')],
+                    'container': 'jpg'
+                }
+            }
+            self.title = image['title'] or image['hash']
     
-        # with a probability of 1, always throw errors
-    config.DYNAMODB_ERROR_PROBABILITY = 1.0
-    assert_raises(ClientError, table.put_item, Item={PARTITION_KEY: short_uid(), 'data': 'foobar123'})
+            setattr(Foo, 'test_partial', functools.partial(noop, None))
     
     
-def get_or_create_file(config_file):
-    if os.path.exists(config_file):
-        return config_file
+# This follows symbolic links, so both islink() and isdir() can be true
+# for the same path on systems that support symlinks
+def isfile(path):
+    '''Test whether a path is a regular file'''
     try:
-        save_file(config_file, '{}')
-        return config_file
-    except Exception:
-        pass
+        st = os.stat(path)
+    except (OSError, ValueError):
+        return False
+    return stat.S_ISREG(st.st_mode)
     
-            params = apigateway_listener.extract_path_params('/foo/bar/baz', '/foo/{proxy+}')
-        self.assertEqual(params, {'proxy+': 'bar/baz'})
+    con.commit()
     
-        return getattr(command_class, command)
-    
-    
-def is_ubuntu():
-    return platform.system() == 'Linux' and platform.linux_distribution()[0] == 'Ubuntu'
+            self._built = False
     
     
-def get_source_name_from_network_mode(network_mode, source_type):
-    if not network_mode:
-        return
-    
-    
-def update_init_py_version(version):
-    path = os.path.join(REPO_ROOT, 'compose', '__init__.py')
-    with open(path, 'r') as f:
-        contents = f.read()
-    contents = re.sub(r'__version__ = '[0-9a-z.-]+'', '__version__ = '{}''.format(version), contents)
-    with open(path, 'w') as f:
-        f.write(contents)
-    
-    
-class ResilienceTest(DockerClientTestCase):
-    def setUp(self):
-        self.db = self.create_service(
-            'db',
-            volumes=[VolumeSpec.parse('/var/db')],
-            command='top')
-        self.project = Project('composetest', [self.db], self.client)
-    
-        def assert_produces(self, reader, expectations):
-        split = split_buffer(reader())
