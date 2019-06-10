c850c7380207401e3bd748ac9c9fab27d7c1762d
SUNsung <weatherius@gmail.com>
2019-06-10 11:47:57 +0300

    Rewrite [50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp] File in: 2019-06-10 11:47:57

--- cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
+++ cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
@@ -1,212 +1,319 @@
 
         
-        TEST(MovableMessageTest, MoveToArena) {
-  Arena arena;
+        void CacheImpl::setAndRetain(void *Key, void *Value, size_t Cost) {
+  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
+  llvm::sys::ScopedLock L(DCache.Mux);
     }
     
-    #include <google/protobuf/testing/googletest.h>
-#include <gtest/gtest.h>
-#include <google/protobuf/testing/file.h>
-    
-    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
-    
-      desired_output_for_decode = 'abcdefghIJ';
-  expected = string('\x0A\x0', 2);
-  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
-                                                     desired_output_for_decode);
-  EXPECT_EQ(expected, result);
+    CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
+  llvm::SmallString<32> NameBuf(Name);
+  cache_attributes_t Attrs = {
+    CACHE_ATTRIBUTES_VERSION_2,
+    CBs.keyHashCB,
+    CBs.keyIsEqualCB,
+    nullptr,
+    CBs.keyDestroyCB,
+    CBs.valueReleaseCB,
+    nullptr,
+    nullptr,
+    CBs.UserData,
+    CBs.valueRetainCB,
+  };
+    }
     
     
-    {
-    {
-    {}  // namespace util
-}  // namespace protobuf
-}  // namespace google
+    {}%
+    
+    public:
+  static CFPointeeInfo classifyTypedef(const clang::TypedefNameDecl *decl);
+    
+    static int digit_index(char value) {
+  if (value >= 'a' && value <= 'z')
+    return value - 'a';
+  if (value >= 'A' && value <= 'J')
+    return value - 'A' + 26;
+  return -1;
+}
+    
+      if (!Files.empty() && !hasAnyPrimaryFiles) {
+    Optional<std::vector<std::string>> userSuppliedNamesOrErr =
+        OutputFilesComputer::getOutputFilenamesFromCommandLineOrFilelist(Args,
+                                                                         Diags);
+    if (userSuppliedNamesOrErr && userSuppliedNamesOrErr->size() == 1)
+      result.setIsSingleThreadedWMO(true);
+  }
+    
+    int main(int argc, char** argv) { return leveldb::test::RunAllTests(); }
 
     
-    // Author: brianolson@google.com (Brian Olson)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
-//
-// Test program to verify that GzipInputStream is compatible with command line
-// gunzip or java.util.zip.GzipInputStream
-//
-// Reads gzip stream on standard input and writes decompressed data to standard
-// output.
     
-        std::cerr << 'Generating ' << input_file
-        << ' to ' << output_file << std::endl;
-    benchmarks::BenchmarkDataset dataset;
-    Message* message;
-    std::string dataset_payload = ReadFile(input_file);
-    GOOGLE_CHECK(dataset.ParseFromString(dataset_payload))
-      << 'Can' t parse data file ' << input_file;
+    {    fprintf(stderr,
+            'expected=%d..%d; got=%d; bad_keys=%d; bad_values=%d; missed=%d\n',
+            min_expected, max_expected, correct, bad_keys, bad_values, missed);
+    ASSERT_LE(min_expected, correct);
+    ASSERT_GE(max_expected, correct);
+  }
     
-     private:
-  static bool IsMessageSet(const Descriptor *descriptor) {
-    if (descriptor != nullptr
-        && descriptor->options().message_set_wire_format()) {
+      // Offset at which to start looking for the first record to return
+  uint64_t const initial_offset_;
+    
+      // Given a key, return an approximate byte offset in the file where
+  // the data for that key begins (or would begin if the key were
+  // present in the file).  The returned value is in terms of file
+  // bytes, and so includes effects like compression of the underlying data.
+  // E.g., the approximate offset of the last key in the table will
+  // be close to the file length.
+  uint64_t ApproximateOffsetOf(const Slice& key) const;
+    
+      double FalsePositiveRate() {
+    char buffer[sizeof(int)];
+    int result = 0;
+    for (int i = 0; i < 10000; i++) {
+      if (Matches(Key(i + 1000000000, buffer))) {
+        result++;
+      }
+    }
+    return result / 10000.0;
+  }
+    
+    TEST(CacheTest, EntriesArePinned) {
+  Insert(100, 101);
+  Cache::Handle* h1 = cache_->Lookup(EncodeKey(100));
+  ASSERT_EQ(101, DecodeValue(cache_->Value(h1)));
+    }
+    
+    SEXP XGBoosterBoostOneIter_R(SEXP handle, SEXP dtrain, SEXP grad, SEXP hess) {
+  R_API_BEGIN();
+  CHECK_EQ(length(grad), length(hess))
+      << 'gradient and hess must have same length';
+  int len = length(grad);
+  std::vector<float> tgrad(len), thess(len);
+  #pragma omp parallel for schedule(static)
+  for (int j = 0; j < len; ++j) {
+    tgrad[j] = REAL(grad)[j];
+    thess[j] = REAL(hess)[j];
+  }
+  CHECK_CALL(XGBoosterBoostOneIter(R_ExternalPtrAddr(handle),
+                                 R_ExternalPtrAddr(dtrain),
+                                 BeginPtr(tgrad), BeginPtr(thess),
+                                 len));
+  R_API_END();
+  return R_NilValue;
+}
+    
+    template<typename DType>
+inline void CompressArray<DType>::Write(dmlc::Stream* fo) {
+  encoded_chunks_.clear();
+  encoded_chunks_.push_back(0);
+  for (size_t i = 0; i < out_buffer_.size(); ++i) {
+    encoded_chunks_.push_back(encoded_chunks_.back() + out_buffer_[i].length());
+  }
+  fo->Write(raw_chunks_);
+  fo->Write(encoded_chunks_);
+  for (const std::string& buf : out_buffer_) {
+    fo->Write(dmlc::BeginPtr(buf), buf.length());
+  }
+}
+    
+        // Remove the assertion on batch.index, which can be null in the case that the data in this
+    // batch is entirely sparse. Although it's true that this indicates a likely issue with the
+    // user's data workflows, passing XGBoost entirely sparse data should not cause it to fail.
+    // See https://github.com/dmlc/xgboost/issues/1827 for complete detail.
+    // CHECK(batch.index != nullptr);
+    
+      double double2[2] = {1.0, 2.0};
+  EXPECT_EQ(info.GetRoot(1), 0)
+    << 'When no root_index is given, was expecting default value 0';
+  info.SetInfo('root_index', double2, xgboost::kDouble, 2);
+  EXPECT_EQ(info.GetRoot(1), 2.0f);
+    
+    #include '../helpers.h'
+    
+      common::ConfigParser cp(argv[1]);
+  auto cfg = cp.Parse();
+  cfg.emplace_back('seed', '0');
+    
+    
+    {  // 7. Ill-formed data should throw exception
+  for (const char* str : {'data = C:\\My Documents\\cat.csv', 'cow=',
+                          'C# = 100%', '= woof ',
+                          'interaction_constraints = [[0, 2], [1]]',
+                          'data = \'train.txt#cache',
+                          'data = \'train.txt#cache', 'foo = \'bar\''}) {
+    ASSERT_THROW(parser.ParseKeyValuePair(str, &key, &value), dmlc::Error);
+  }
+}
+    
+    #include <dmlc/io.h>
+#include <rabit/rabit.h>
+#include <string>
+#include <cstring>
+    
+    #endif
+    
+    U_NAMESPACE_END
+    
+    int32_t 
+SmallIntFormatter::estimateDigitCount(
+        int32_t positiveValue, const IntDigitCountRange &range) {
+    if (positiveValue >= gMaxFastInt) {
+        return range.getMax();
+    }
+    return range.pin(gDigitCount[positiveValue]);
+}
+    
+    #endif   // #if !UCONFIG_NO_FORMATTING
+#endif   // SMPDTFST_H
+
+    
+    const char *StandardPlural::getKeyword(Form p) {
+    U_ASSERT(ZERO <= p && p < COUNT);
+    return gKeywords[p];
+}
+    
+        /**
+     * Union the set of all characters that may output by this object
+     * into the given set.
+     * @param toUnionTo the set into which to union the output characters
+     */
+    virtual void addReplacementSetTo(UnicodeSet& toUnionTo) const;
+    
+    namespace {
+const int MAX_RETRY = 2;
+} // namespace
+    
+      virtual ~DHTResponseMessage();
+    
+      DHTTaskQueue* taskQueue_;
+    
+    
+    {  const int clen = bittorrent::getCompactLength(family_);
+  // nodes
+  for (std::vector<std::shared_ptr<DHTNode>>::const_iterator i = nodes_.begin(),
+                                                             eoi = nodes_.end();
+       i != eoi; ++i) {
+    const std::shared_ptr<DHTNode>& node = *i;
+    // Write IP address + port in Compact IP-address/port info form.
+    unsigned char compactPeer[COMPACT_LEN_IPV6];
+    int compactlen = bittorrent::packcompact(compactPeer, node->getIPAddress(),
+                                             node->getPort());
+    if (compactlen != clen) {
+      memset(compactPeer, 0, clen);
+    }
+    uint8_t clen1 = clen;
+    // 1byte compact peer format length
+    WRITE_CHECK(fp, &clen1, sizeof(clen1));
+    // 7bytes reserved
+    WRITE_CHECK(fp, zero, 7);
+    // clen bytes compact peer
+    WRITE_CHECK(fp, compactPeer, static_cast<size_t>(clen));
+    // 24-clen bytes reserved
+    WRITE_CHECK(fp, zero, static_cast<size_t>(24 - clen));
+    // 20bytes: node ID
+    WRITE_CHECK(fp, node->getID(), DHT_ID_LENGTH);
+    // 4bytes reserved
+    WRITE_CHECK(fp, zero, 4);
+  }
+  if (fp.close() == EOF) {
+    throw DL_ABORT_EX(
+        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
+  }
+  if (!File(filenameTemp).renameTo(filename)) {
+    throw DL_ABORT_EX(
+        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
+  }
+  A2_LOG_INFO('DHT routing table was saved successfully');
+}
+    
+    
+    {  // Returns two vector of Commands.  First one contains regular
+  // commands.  Secod one contains so called routine commands, which
+  // executed once per event poll returns.
+  std::pair<std::vector<std::unique_ptr<Command>>,
+            std::vector<std::unique_ptr<Command>>>
+  setup(DownloadEngine* e, int family);
+};
+    
+    class DHTTask {
+public:
+  virtual ~DHTTask() = default;
+    }
+    
+    public:
+  DHTTaskExecutor(int numConcurrent);
+    
+    class DHTTask;
+    
+    bool DHTTokenTracker::validateToken(const std::string& token,
+                                    const unsigned char* infoHash,
+                                    const std::string& ipaddr,
+                                    uint16_t port) const
+{
+  for (auto& elem : secret_) {
+    if (generateToken(infoHash, ipaddr, port, elem) == token) {
       return true;
     }
-    return false;
   }
+  return false;
+}
     
-    
-  if (mode == psModePS || mode == psModePSOrigPageSizes) {
-    GooString pageLabel;
-    const GBool gotLabel = m_catalog->indexToLabel(pageNum -1, &pageLabel);
-    if (gotLabel) {
-      // See bug13338 for why we try to avoid parentheses...
-      GBool needParens;
-      GooString *filteredString = filterPSLabel(&pageLabel, &needParens);
-      if (needParens) {
-	writePSFmt('%%Page: ({0:t}) {1:d}\n', filteredString, seqPage);
-      } else {
-	writePSFmt('%%Page: {0:t} {1:d}\n', filteredString, seqPage);
-      }
-      delete filteredString;
-    } else {
-      writePSFmt('%%Page: {0:d} {1:d}\n', pageNum, seqPage);
+    class DHTTokenUpdateCommand : public TimeBasedCommand {
+private:
+  DHTTokenTracker* tokenTracker_;
     }
-    if (mode != psModePSOrigPageSizes)
-      writePS('%%BeginPageSetup\n');
-  }
     
-    #include <config.h>
-    
-    void Page::display(OutputDev *out, double hDPI, double vDPI,
-		   int rotate, GBool useMediaBox, GBool crop,
-		   GBool printing, Catalog *catalog,
-		   GBool (*abortCheckCbk)(void *data),
-		   void *abortCheckCbkData,
-                   GBool (*annotDisplayDecideCbk)(Annot *annot, void *user_data),
-                   void *annotDisplayDecideCbkData) {
-  displaySlice(out, hDPI, vDPI, rotate, useMediaBox, crop, -1, -1, -1, -1, printing, catalog,
-	       abortCheckCbk, abortCheckCbkData,
-               annotDisplayDecideCbk, annotDisplayDecideCbkData);
-}
-    
-      // Get duration, the maximum length of time, in seconds,
-  // that the page is displayed before the presentation automatically
-  // advances to the next page
-  double getDuration() { return duration; }
-    
-      MediaWindowParameters();
-  ~MediaWindowParameters();
-    
-    
-    {  if (!ok) {
-    return gFalse;
-  }
-  if (authData) {
-    ownerPassword = ((StandardAuthData *)authData)->ownerPassword;
-    userPassword = ((StandardAuthData *)authData)->userPassword;
-  } else {
-    ownerPassword = NULL;
-    userPassword = NULL;
-  }
-  if (!Decrypt::makeFileKey(encVersion, encRevision, fileKeyLength,
-			    ownerKey, userKey, permFlags, fileID,
-			    ownerPassword, userPassword, fileKey,
-			    encryptMetadata, &ownerPasswordOk)) {
-    return gFalse;
-  }
-  return gTrue;
-}
-    
-            Rational& operator<<=(Rational const& rhs);
-        Rational& operator>>=(Rational const& rhs);
-    
-        auto lastDecGrouping = m_decGrouping;
-    wstring grpStr = m_resourceProvider->GetCEngineString(L'sGrouping');
-    m_decGrouping = DigitGroupingStringToGroupingVector(grpStr.empty() ? DEFAULT_GRP_STR : grpStr);
-    
-    shared_ptr<HISTORYITEM> const& CalculatorHistory::GetHistoryItem(_In_ unsigned int uIdx)
+    std::string DHTUnknownMessage::toString() const
 {
-    assert(uIdx >= 0 && uIdx < m_historyItems.size());
-    return m_historyItems.at(uIdx);
+  size_t sampleLength = 8;
+  if (length_ < sampleLength) {
+    sampleLength = length_;
+  }
+  return fmt('dht unknown Remote:%s(%u) length=%lu, first 8 bytes(hex)=%s',
+             ipaddr_.c_str(), port_, static_cast<unsigned long>(length_),
+             util::toHex(data_, sampleLength).c_str());
 }
     
-        CalcEngine::Rational m_currentVal;                               // Currently displayed number used everywhere.
-    CalcEngine::Rational m_lastVal;                                  // Number before operation (left operand).
-    std::array<CalcEngine::Rational, MAXPRECDEPTH> m_parenVals;      // Holding array for parenthesis values.
-    std::array<CalcEngine::Rational, MAXPRECDEPTH> m_precedenceVals; // Holding array for precedence values.
-    bool m_bError;                                                   // Error flag.
-    bool m_bInv;                                                     // Inverse on/off flag.
-    bool m_bNoPrevEqu;                                               /* Flag for previous equals.          */
+    #include <vector>
     
-                virtual bool MoveCurrentToPosition(int index) = Windows::UI::Xaml::Data::ICollectionView::MoveCurrentToPosition
-            {
-                if (index < 0 || index >= static_cast<int>(m_source->Size))
-                {
-                    return false;
-                }
-    }
+      Byte t1(bytes + 3);
+  int32_t t = t1.get_byte(4, 4);
     
-    #pragma once
-#include 'INarratorAnnouncementHost.h'
-    
-      for (auto alphabet_size : test_cases) {
-    for (int i = 0; i < repetitions; i++) {
-      std::vector<int> input(num_elements);
-      std::generate(input.begin(), input.end(),
-        [=]() { return rand() % alphabet_size; });
-      CompressedBufferWriter cbw(alphabet_size);
+    TEST_F(SpeedLimitTest, GetSpeedLimitByS) {
+  EXPECT_EQ(speed_limit_.speed_limit_points().size(), 100);
+  double s = 0.0;
+  const double ds = 0.01;
+  while (s < 99.0) {
+    double v_limit = speed_limit_.GetSpeedLimitByS(s);
     }
     }
     
-    void SparsePage::Push(const SparsePage &batch) {
-  auto& data_vec = data.HostVector();
-  auto& offset_vec = offset.HostVector();
-  const auto& batch_offset_vec = batch.offset.HostVector();
-  const auto& batch_data_vec = batch.data.HostVector();
-  size_t top = offset_vec.back();
-  data_vec.resize(top + batch.data.Size());
-  std::memcpy(dmlc::BeginPtr(data_vec) + top,
-              dmlc::BeginPtr(batch_data_vec),
-              sizeof(Entry) * batch.data.Size());
-  size_t begin = offset.Size();
-  offset_vec.resize(begin + batch.Size());
-  for (size_t i = 0; i < batch.Size(); ++i) {
-    offset_vec[i + begin] = top + batch_offset_vec[i + 1];
-  }
+    // config detail: {'name': 'manual_input', 'offset': 0.0, 'precision': 0.001,
+// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 7,
+// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
+double Brakerpt6c::manual_input(const std::uint8_t* bytes,
+                                int32_t length) const {
+  Byte t0(bytes + 0);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    
+    {
+    {
+    {
+    {  Headlight_rpt_77::Commanded_valueType ret =
+      static_cast<Headlight_rpt_77::Commanded_valueType>(x);
+  return ret;
 }
+}  // namespace gem
+}  // namespace canbus
+}  // namespace apollo
+
     
-    #include <utility>
-#include <map>
-#include <memory>
-#include <string>
-#include <vector>
-    
-      std::vector<unsigned> feature_list(nfeature);
-  std::iota(feature_list.begin(), feature_list.end(), 0);
-    
-        std::unique_ptr<DHTTaskFactory> taskFactory;
-    
-    namespace aria2 {
+    // config detail: {'name': 'latitude_seconds', 'offset': 0.0, 'precision': 1.0,
+// 'len': 8, 'is_signed_var': True, 'physical_range': '[-128|127]', 'bit': 23,
+// 'type': 'int', 'order': 'motorola', 'physical_unit': 'sec'}
+int Latlonheadingrpt82::latitude_seconds(const std::uint8_t* bytes,
+                                         int32_t length) const {
+  Byte t0(bytes + 2);
+  int32_t x = t0.get_byte(0, 8);
     }
-    
-    #include 'common.h'
-    
-    namespace aria2 {
-    }
-    
-      template <typename OutputIterator>
-  void findAll(OutputIterator out, const std::string& hostname,
-               uint16_t port) const
-  {
-    auto target = std::make_shared<CacheEntry>(hostname, port);
-    auto i = entries_.find(target);
-    if (i != entries_.end()) {
-      (*i)->getAllGoodAddrs(out);
-    }
-  }
