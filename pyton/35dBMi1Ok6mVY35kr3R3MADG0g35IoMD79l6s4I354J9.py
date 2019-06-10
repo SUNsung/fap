44f0dfc2542327aee982d7a763be77c60098a8b3
SUNsung <weatherius@gmail.com>
2019-06-10 14:22:42 +0300

    Rewrite [35dBMi1Ok6mVY35kr3R3MADG0g35IoMD79l6s4I354J9.py] File in: 2019-06-10 14:22:42

--- pyton/35dBMi1Ok6mVY35kr3R3MADG0g35IoMD79l6s4I354J9.py
+++ pyton/35dBMi1Ok6mVY35kr3R3MADG0g35IoMD79l6s4I354J9.py
@@ -1,89 +1,152 @@
 
         
         
-DOCUMENTATION = '''
----
-module: iam_server_certificate_facts
-short_description: Retrieve the facts of a server certificate
-description:
-  - Retrieve the attributes of a server certificate
-version_added: '2.2'
-author: 'Allen Sanabria (@linuxdynasty)'
-requirements: [boto3, botocore]
-options:
-  name:
-    description:
-      - The name of the server certificate you are retrieving attributes for.
-    required: true
-extends_documentation_fragment:
-    - aws
-    - ec2
-'''
-    
-            for rule in rules:
-            firewall_rule = oneandone.client.FirewallPolicyRule(
-                protocol=rule['protocol'],
-                port_from=rule['port_from'],
-                port_to=rule['port_to'],
-                source=rule['source'])
-            firewall_rules.append(firewall_rule)
-    
-    
-DOCUMENTATION = '''
----
-module: honeybadger_deployment
-author: 'Benjamin Curtis (@stympy)'
-version_added: '2.2'
-short_description: Notify Honeybadger.io about app deployments
-description:
-   - Notify Honeybadger.io about app deployments (see http://docs.honeybadger.io/article/188-deployment-tracking)
-options:
-  token:
-    description:
-      - API token.
-    required: true
-  environment:
-    description:
-      - The environment name, typically 'production', 'staging', etc.
-    required: true
-  user:
-    description:
-      - The username of the person doing the deployment
-  repo:
-    description:
-      - URL of the project repository
-  revision:
-    description:
-      - A hash, number, tag, or other identifier showing what revision was deployed
-  url:
-    description:
-      - Optional URL to submit the notification to.
-    default: 'https://api.honeybadger.io/v1/deploys'
-  validate_certs:
-    description:
-      - If C(no), SSL certificates for the target url will not be validated. This should only be used
-        on personally controlled sites using self-signed certificates.
-    type: bool
-    default: 'yes'
-    
-        return device_ip
-    
-            o = tf.nn.conv2d(x, W, strides=strides, padding=padding) + b
-        o = act_fn(o)
-    
-    
-    
-        return o
-    
-    
-def get_shape(x):
+class HashTable:
     '''
-    References:
-        tflearn.utils.get_incoming_shape
+        Basic Hash Table example with open addressing and linear probing
     '''
-    if isinstance(x, (tf.Tensor, tf.SparseTensor)):
-        return x.get_shape().as_list()
-    elif type(x) in [np.array, np.ndarray, list, tuple]:
-        return list(np.shape(x))
-    else:
-        raise Exception('Invalid `x`.')
+    
+        def _colision_resolution(self, key, data=None):
+        i = 1
+        new_key = self.hash_function(key + i*i)
+    
+        for i in range(1, n+1):
+        for j in range(1, s+1):
+            dp[i][j]= dp[i][j-1]
+    
+    def revise_centroids(data, k, cluster_assignment):
+    new_centroids = []
+    for i in range(k):
+        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
+        member_data_points = data[cluster_assignment==i]
+        # Compute the mean of the data points. Fill in the blank (RHS only)
+        centroid = member_data_points.mean(axis=0)
+        new_centroids.append(centroid)
+    new_centroids = np.array(new_centroids)
+    
+    return new_centroids
+    
+        Those are used for calculating differences between
+    predicted values and actual values.
+    
+    
+def b_expo_mod(a, b, c):
+    res = 1
+    while b > 0:
+        if b&1:
+            res = ((res%c) * (a%c)) % c
+    
+        return res
+    
+    from httpie.compat import is_windows
+from httpie.config import DEFAULT_CONFIG_DIR, Config
+    
+            '''
+        r.headers['Authorization'] = type(self).make_header(
+            self.username, self.password).encode('latin1')
+        return r
+    
+            def get_auth(self, username=None, password=None):
+            assert self.raw_auth is None
+            assert username is None
+            assert password is None
+            return basic_auth()
+    
+    
+def test_default_options_overwrite(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['json'] == {'foo': 'bar'}
+    
+    
+@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
+@pytest.mark.parametrize('filename', filenames)
+def test_rst_file_syntax(filename):
+    p = subprocess.Popen(
+        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    err = p.communicate()[1]
+    assert p.returncode == 0, err.decode('utf8')
+
+    
+    
+@mock.patch('httpie.core.get_response')
+def test_error_traceback(get_response):
+    exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    with raises(ConnectionError):
+        main(['--ignore-stdin', '--traceback', 'www.google.com'])
+    
+    
+@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
+def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
+    r = http(follow_flag, httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 1
+    assert HTTP_OK in r
+    
+        def test_upload_multiple_fields_with_the_same_name(self, httpbin):
+        r = http('--form', '--verbose', 'POST', httpbin.url + '/post',
+                 'test-file@%s' % FILE_PATH_ARG,
+                 'test-file@%s' % FILE_PATH_ARG)
+        assert HTTP_OK in r
+        assert r.count('Content-Disposition: form-data; name='test-file';'
+                       ' filename='%s'' % os.path.basename(FILE_PATH)) == 2
+        # Should be 4, but is 3 because httpbin
+        # doesn't seem to support filed field lists
+        assert r.count(FILE_CONTENT) in [3, 4]
+        assert r.count('Content-Type: text/plain') == 2
+    
+            with open(self.path, 'w') as f:
+            json.dump(self, f, indent=4, sort_keys=True, ensure_ascii=True)
+            f.write('\n')
+    
+            start = time.time()
+        func(X, n_jobs=-1)
+        multi_core.append(time.time() - start)
+    
+                std = y_train.std(axis=0)
+            mean = y_train.mean(axis=0)
+            y_train = (y_train - mean) / std
+            y_test = (y_test - mean) / std
+    
+    from sklearn.cluster import AgglomerativeClustering
+from sklearn.neighbors import kneighbors_graph
+    
+    for center_1, center_2 in zip(regular_values[:-1], regular_values[1:]):
+    plt.axvline(.5 * (center_1 + center_2), color='b', linestyle='--')
+    
+    The first plot shows the best inertia reached for each combination
+of the model (``KMeans`` or ``MiniBatchKMeans``) and the init method
+(``init='random'`` or ``init='kmeans++'``) for increasing values of the
+``n_init`` parameter that controls the number of initializations.
+    
+    import time as time
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.datasets.samples_generator import make_swiss_roll
+    
+                if not callable(cb):
+                cb_method = getattr(spider, cb, None)
+                if callable(cb_method):
+                    cb = cb_method
+                else:
+                    logger.error('Cannot find callback %(callback)r in spider: %(spider)s',
+                                 {'callback': cb, 'spider': spider.name})
+                    return
+    
+    from twisted.web.client import HTTPClientFactory
+from twisted.web.http import HTTPClient
+from twisted.internet import defer
+    
+        parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
+    parser.add_argument('-d', action='store', dest='directory')
+    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
+    parser.add_argument('--overwrite', action='store_true', default = False)    
+    results = parser.parse_args()
