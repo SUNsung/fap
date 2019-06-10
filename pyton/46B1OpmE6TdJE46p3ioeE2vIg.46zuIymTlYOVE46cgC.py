67d745f9b80f354b1b69e1304c6af391ab519f70
SUNsung <weatherius@gmail.com>
2019-06-10 12:49:37 +0300

    Rewrite [46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py] File in: 2019-06-10 12:49:37

--- pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
+++ pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
@@ -1,250 +1,106 @@
 
         
-        
-def get_db():
-    '''Connect to the application's configured database. The connection
-    is unique for each request and will be reused if this is called
-    again.
+            def _print_response(self, response, opts):
+        if opts.headers:
+            self._print_headers(response.request.headers, b'>')
+            print('>')
+            self._print_headers(response.headers, b'<')
+        else:
+            self._print_bytes(response.body)
+    
+        def process_options(self, args, opts):
+        ScrapyCommand.process_options(self, args, opts)
+        try:
+            opts.spargs = arglist_to_dict(opts.spargs)
+        except ValueError:
+            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
+        if opts.output:
+            if opts.output == '-':
+                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
+            else:
+                self.settings.set('FEED_URI', opts.output, priority='cmdline')
+            feed_exporters = without_none_values(self.settings.getwithbase('FEED_EXPORTERS'))
+            valid_output_formats = feed_exporters.keys()
+            if not opts.output_format:
+                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
+            if opts.output_format not in valid_output_formats:
+                raise UsageError('Unrecognized output format '%s', set one'
+                                 ' using the '-t' switch or as a file extension'
+                                 ' from the supported list %s' % (opts.output_format,
+                                                                  tuple(valid_output_formats)))
+            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
+    
+    
+# XXX: move it to w3lib?
+_ajax_crawlable_re = re.compile(six.u(r'<meta\s+name=['\']fragment['\']\s+content=['\']!['\']/?>'))
+def _has_ajaxcrawlable_meta(text):
     '''
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-    
-        def add_app_template_test(self, f, name=None):
-        '''Register a custom template test, available application wide.  Like
-        :meth:`Flask.add_template_test` but for a blueprint.  Works exactly
-        like the :meth:`app_template_test` decorator.
-    
-        Implements signals based on blinker if available, otherwise
-    falls silently back to a noop.
-    
-        def list_templates(self):
-        result = set()
-        loader = self.app.jinja_loader
-        if loader is not None:
-            result.update(loader.list_templates())
-    
-            url = url_parse(path)
-        base_url = '{scheme}://{netloc}/{path}'.format(
-            scheme=url.scheme or url_scheme,
-            netloc=url.netloc or http_host,
-            path=app_root.lstrip('/')
-        )
-        path = url.path
-    
-                # If we have no method at all in there we don't want to add a
-            # method list. This is for instance the case for the base class
-            # or another subclass of a base method view that does not introduce
-            # new methods.
-            if methods:
-                cls.methods = methods
-    
-        if K.image_data_format() == 'channels_last':
-        x_train = x_train.transpose(0, 2, 3, 1)
-        x_test = x_test.transpose(0, 2, 3, 1)
-    
-        fpath = os.path.join(path, 'train')
-    x_train, y_train = load_batch(fpath, label_key=label_mode + '_labels')
-    
-        # Test single image
-    x = np.random.uniform(0, 255, (10, 10, 3))
-    xint = x.astype('int32')
-    assert utils.preprocess_input(x).shape == x.shape
-    assert utils.preprocess_input(xint).shape == xint.shape
-    
-    
-def test_boston_housing():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-    
-        To save the multi-gpu model, use `.save(fname)` or `.save_weights(fname)`
-    with the template model (the argument you passed to `multi_gpu_model`),
-    rather than the model returned by `multi_gpu_model`.
+    >>> _has_ajaxcrawlable_meta('<html><head><meta name='fragment'  content='!'/></head><body></body></html>')
+    True
+    >>> _has_ajaxcrawlable_meta('<html><head><meta name='fragment' content='!'></head></html>')
+    True
+    >>> _has_ajaxcrawlable_meta('<html><head><!--<meta name='fragment'  content='!'/>--></head><body></body></html>')
+    False
+    >>> _has_ajaxcrawlable_meta('<html></html>')
+    False
     '''
-    if K.backend() != 'tensorflow':
-        raise ValueError('`multi_gpu_model` is only available '
-                         'with the TensorFlow backend.')
     
+    # The name for this set of Sphinx documents.  If None, it defaults to
+# '<project> v<release> documentation'.
+#html_title = None
     
-def train_model(model, train, test, num_classes):
-    x_train = train[0].reshape((train[0].shape[0],) + input_shape)
-    x_test = test[0].reshape((test[0].shape[0],) + input_shape)
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
-    
-    import numpy as np
-import keras
-from keras.datasets import reuters
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.preprocessing.text import Tokenizer
-    
-        # Raises
-        ValueError if unknown identifier.
-    '''
-    if identifier is None:
-        return None
-    if isinstance(identifier, six.string_types):
-        identifier = str(identifier)
-        return deserialize(identifier)
-    if isinstance(identifier, dict):
-        return deserialize(identifier)
-    elif callable(identifier):
-        return identifier
-    else:
-        raise ValueError('Could not interpret '
-                         'loss function identifier:', identifier)
+            # Display results overlaid on an image
+        show_prediction_labels_on_image(os.path.join('knn_examples/test', image_file), predictions)
 
     
-        def _remote_state(self):
-        '''Returns the remote state from AWS'''
-        description = self.rule.describe()
-        if not description:
-            return
-        return description['state']
-    
-        except botocore.exceptions.ClientError:
-        pass
-    
-        creds, params = get_google_cloud_credentials(module)
-    spanner_client = spanner.Client(project=params['project_id'],
-                                    credentials=creds,
-                                    user_agent=CLOUD_CLIENT_USER_AGENT)
-    changed = False
-    json_output = {}
-    
-    
-DOCUMENTATION = '''
----
-module: ipa_dnszone
-author: Fran Fitzpatrick (@fxfitz)
-short_description: Manage FreeIPA DNS Zones
-description:
-- Add and delete an IPA DNS Zones using IPA API
-options:
-  zone_name:
-    description:
-    - The DNS zone name to which needs to be managed.
-    required: true
-  state:
-    description: State to ensure
-    required: false
-    default: present
-    choices: ['present', 'absent']
-extends_documentation_fragment: ipa.documentation
-version_added: '2.5'
-'''
-    
-    ALL_SSL_OPTIONS_HASHES = [
-    '2086bca02db48daf93468332543c60ac6acdb6f0b58c7bfdf578a5d47092f82a',
-    '4844d36c9a0f587172d9fa10f4f1c9518e3bcfa1947379f155e16a70a728c21a',
-    '5a922826719981c0a234b1fbcd495f3213e49d2519e845ea0748ba513044b65b',
-    '4066b90268c03c9ba0201068eaa39abbc02acf9558bb45a788b630eb85dadf27',
-    'f175e2e7c673bd88d0aff8220735f385f916142c44aa83b09f1df88dd4767a88',
-    'cfdd7c18d2025836ea3307399f509cfb1ebf2612c87dd600a65da2a8e2f2797b',
-    '80720bd171ccdc2e6b917ded340defae66919e4624962396b992b7218a561791',
-    'c0c022ea6b8a51ecc8f1003d0a04af6c3f2bc1c3ce506b3c2dfc1f11ef931082',
-]
-'''SHA256 hashes of the contents of previous versions of all versions of MOD_SSL_CONF_SRC'''
-    
-        def test_rollback_error(self):
-        self.config.reverter.rollback_checkpoints = mock.Mock(
-            side_effect=errors.ReverterError)
-        self.assertRaises(errors.PluginError, self.config.rollback_checkpoints)
-    
-    # If true, show URL addresses after external links.
-#latex_show_urls = False
-    
-    # If true, 'Created using Sphinx' is shown in the HTML footer.
-# Default is True.
-#html_show_sphinx = True
-    
-    image = face_recognition.load_image_file('{}')
-face_locations = face_recognition.face_locations(image)
-'''
-    
-            if file and allowed_file(file.filename):
-            # The image file seems valid! Detect faces and return the result.
-            return detect_faces_in_image(file)
-    
-        if os.path.isdir(image_to_check):
-        if cpus == 1:
-            [test_image(image_file, model) for image_file in image_files_in_folder(image_to_check)]
-        else:
-            process_images_in_process_pool(image_files_in_folder(image_to_check), cpus, model)
-    else:
-        test_image(image_to_check, model)
+    print('I found {} face(s) in this photograph.'.format(len(face_locations)))
     
     # To run this, you need a Raspberry Pi 2 (or greater) with face_recognition and
 # the picamera[array] module installed.
 # You can follow this installation instructions to get your RPi set up:
 # https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
     
-    # This code finds all faces in a list of images using the CNN model.
-#
-# This demo is for the _special case_ when you need to find faces in LOTS of images very quickly and all the images
-# are the exact same size. This is common in video processing applications where you have lots of video frames
-# to process.
-#
-# If you are processing a lot of images and using a GPU with CUDA, batch processing can be ~3x faster then processing
-# single images at a time. But if you aren't using a GPU, then batch processing isn't going to be very helpful.
-#
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read the video file.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+        face_found = False
+    is_obama = False
     
-            if True in result:
-            [print_result(image_to_check, name, distance, show_distance) for is_match, name, distance in zip(result, known_names, distances) if is_match]
-        else:
-            print_result(image_to_check, 'unknown_person', None, show_distance)
+        # Load a second sample picture and learn how to recognize it.
+    biden_image = face_recognition.load_image_file('biden.jpg')
+    biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
     
-        # 图片上传失败，输出以下html代码
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
-    '''
+    # Display the resulting image
+pil_image.show()
     
+        def _call(self, x):
+        ''''''
+        o = self.act_fn(tf.matmul(x, self.W) + self.b)
+        return o
     
-class Migration(DataMigration):
-    def forwards(self, orm):
-        db.commit_transaction()
-        try:
-            self._forwards(orm)
-        except Exception:
-            # Explicitly resume the transaction because
-            # South is going to try and roll it back, but when
-            # it can't find one, it'll error itself, masking
-            # the actual exception being raised
-            #
-            # See https://github.com/getsentry/sentry/issues/5035
-            db.start_transaction()
-            raise
-        db.start_transaction()
+        def __call__(self, x):
+        '''
+        Args:
+            x: 注意 x.dtype == float32
+        '''
+        # x = tf.cast(x, dtype=tf.float32)  # 交给外部处理
+        loss_regularization = 0.
+        if self.l1:
+            loss_regularization += tf.reduce_sum(self.l1 * tf.abs(x))
+        if self.l2:
+            loss_regularization += tf.reduce_sum(self.l2 * tf.square(x))
+        return loss_regularization
     
-            # Adding field 'ReleaseFile.dist'
-        db.add_column(
-            'sentry_releasefile',
-            'dist',
-            self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                to=orm['sentry.Distribution'], null=True
-            ),
-            keep_default=False
-        )
+    References:
+    https://www.tensorflow.org/api_guides/python/array_ops
+    keras.backend
+'''
+    
+    :copyright: (c) 2010-2017 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+'''
+    
+    from datetime import timedelta
+from django.db.models import Max
+from django.utils import timezone
+    
+    import logging
+    
+    import logging
