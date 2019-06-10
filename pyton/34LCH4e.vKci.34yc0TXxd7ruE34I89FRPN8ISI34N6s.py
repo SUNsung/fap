1d8572b93f2d296c539e7141c66ebdd0dd5c12e7
SUNsung <weatherius@gmail.com>
2019-06-10 15:23:28 +0300

    Rewrite [34LCH4e.vKci.34yc0TXxd7ruE34I89FRPN8ISI34N6s.py] File in: 2019-06-10 15:23:28

--- pyton/34LCH4e.vKci.34yc0TXxd7ruE34I89FRPN8ISI34N6s.py
+++ pyton/34LCH4e.vKci.34yc0TXxd7ruE34I89FRPN8ISI34N6s.py
@@ -1,129 +1,213 @@
 
         
         
-class Hand(object):
+# OPTIMIZATION
+flags.DEFINE_integer('batch_size', BATCH_SIZE,
+                     'Batch size to use during training.')
+flags.DEFINE_float('learning_rate_init', LEARNING_RATE_INIT,
+                   'Learning rate initial value')
+flags.DEFINE_float('learning_rate_decay_factor', LEARNING_RATE_DECAY_FACTOR,
+                   'Learning rate decay, decay by this fraction every so \
+                   often.')
+flags.DEFINE_float('learning_rate_stop', LEARNING_RATE_STOP,
+                   'The lr is adaptively reduced, stop training at this value.')
+# Rather put the learning rate on an exponentially decreasiong schedule,
+# the current algorithm pays attention to the learning rate, and if it
+# isn't regularly decreasing, it will decrease the learning rate.  So far,
+# it works fine, though it is not perfect.
+flags.DEFINE_integer('learning_rate_n_to_compare', LEARNING_RATE_N_TO_COMPARE,
+                     'Number of previous costs current cost has to be worse \
+                     than, to lower learning rate.')
     
-            When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.lookup.get(query)
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
+    
+# Initial condition generation, and condition label generation.  This
+# happens outside of the dataset loop, so that all datasets have the
+# same conditions, which is similar to a neurophys setup.
+condition_number = 0
+x0s = []
+condition_labels = []
+for c in range(C):
+  x0 = FLAGS.x0_std * rng.randn(N, 1)
+  x0s.append(np.tile(x0, nreplications)) # replicate x0 nreplications times
+  # replicate the condition label nreplications times
+  for ns in range(nreplications):
+    condition_labels.append(condition_number)
+  condition_number += 1
+x0s = np.concatenate(x0s, axis=1)
+    
+    
+rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
+u_rng = np.random.RandomState(seed=FLAGS.synth_data_seed+1)
+T = FLAGS.T
+C = FLAGS.C
+N = FLAGS.N  # must be same N as in trained model (provided example is N = 50)
+nreplications = FLAGS.nreplications
+E = nreplications * C  # total number of trials
+train_percentage = FLAGS.train_percentage
+ntimesteps = int(T / FLAGS.dt)
+batch_size = 1  # gives one example per ntrial
+    
+        self.bos_char = free_ids[0]  # <begin sentence>
+    self.eos_char = free_ids[1]  # <end sentence>
+    self.bow_char = free_ids[2]  # <begin word>
+    self.eow_char = free_ids[3]  # <end word>
+    self.pad_char = free_ids[4]  # <padding>
+    
+      word_to_id = build_vocab(train_path)
+  train_data = _file_to_word_ids(train_path, word_to_id)
+  valid_data = _file_to_word_ids(valid_path, word_to_id)
+  test_data = _file_to_word_ids(test_path, word_to_id)
+  vocabulary = len(word_to_id)
+  return train_data, valid_data, test_data, vocabulary
+    
+      if FLAGS.data_set == 'ptb':
+    model_str = 'Model'
+  else:
+    model_str = 'model'
+    
+        publicKey, privateKey = generateKey(keySize)
+    print('\nWriting public key to file %s_pubkey.txt...' % name)
+    with open('%s_pubkey.txt' % name, 'w') as fo:
+        fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
+    
+    from .hash_table import HashTable
+    
+    '''
+The number of partitions of a number n into at least k parts equals the number of partitions into exactly k parts
+plus the number of partitions into at least k-1 parts. Subtracting 1 from each part of a partition of n into k parts
+gives a partition of n-k into k parts. These two facts together are used for this algorithm.
+'''
+def partition(m):
+	memo = [[0 for _ in xrange(m)] for _ in xrange(m+1)]
+	for i in xrange(m+1):
+		memo[i][0] = 1
+    
+    import numpy as np
+    
+        Using log and roots can be perceived as tools for penalizing big
+    erors. However, using appropriate metrics depends on the situations,
+    and types of data
+'''
+    
+            a += a
+        b >>= 1
+    
+    handleSlideshow(dom)
 
     
-        def receive_friend_request(self, friend_id):
-        pass
+    #
+# Test code
+#
     
-        def take_spot(self, spot):
-        self.spots_taken.append(spot)
+    p = Point(4.0, -3.2)
     
-            Accessing a node updates its position to the front of the LRU list.
-        '''
-        node = self.lookup[query]
-        if node is None:
-            return None
-        self.linked_list.move_to_front(node)
-        return node.results
+    cur.execute('insert into people (name_last, age) values ('Yeltsin',   72)')
+cur.execute('insert into people (name_last, age) values ('Putin',     51)')
     
-    
-def with_confirmation(proc, TIMEOUT):
-    '''Ensures that command can be fixed when confirmation enabled.'''
-    _set_confirmation(proc, True)
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
     
     
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
-    history_not_changed(proc, TIMEOUT)
+  def Start( self ):
+    self._keepalive_thread.start()
+    
+      _assert_rejects( f, { 'text' : 'This is an unimportant taco',
+                        'kind' : 'WARNING' } )
+  _assert_accepts( f, { 'text' : 'This taco will be shown',
+                        'kind' : 'ERROR' } )
     
     
-@pytest.fixture(params=[(python_3, False),
-                        (python_3, True),
-                        (python_2, False)])
-def proc(request, spawnu, TIMEOUT):
-    container, instant_mode = request.param
-    proc = spawnu(*container)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(init_zshrc.format(
-        u'--enable-experimental-instant-mode' if instant_mode else ''))
-    proc.sendline(u'zsh')
-    if instant_mode:
-        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
-    return proc
+def KeywordsFromSyntaxListOutput_CppSyntax_test():
+  expected_keywords = (
+    'int_fast32_t', 'FILE', 'size_t', 'bitor', 'typedef', 'const', 'struct',
+    'uint8_t', 'fpos_t', 'thread_local', 'unsigned', 'uint_least16_t', 'do',
+    'intptr_t', 'uint_least64_t', 'return', 'auto', 'void', '_Complex',
+    'break', '_Alignof', 'not', 'using', '_Static_assert', '_Thread_local',
+    'public', 'uint_fast16_t', 'this', 'continue', 'char32_t', 'int16_t',
+    'intmax_t', 'static', 'clock_t', 'sizeof', 'int_fast64_t', 'mbstate_t',
+    'try', 'xor', 'uint_fast32_t', 'int_least8_t', 'div_t', 'volatile',
+    'template', 'char16_t', 'new', 'ldiv_t', 'int_least16_t', 'va_list',
+    'uint_least8_t', 'goto', 'noreturn', 'enum', 'static_assert', 'bitand',
+    'compl', 'imaginary', 'jmp_buf', 'throw', 'asm', 'ptrdiff_t', 'uint16_t',
+    'or', 'uint_fast8_t', '_Bool', 'int32_t', 'float', 'private', 'restrict',
+    'wint_t', 'operator', 'not_eq', '_Imaginary', 'alignas', 'union', 'long',
+    'uint_least32_t', 'int_least64_t', 'friend', 'uintptr_t', 'int8_t', 'else',
+    'export', 'int_fast8_t', 'catch', 'true', 'case', 'default', 'double',
+    '_Noreturn', 'signed', 'typename', 'while', 'protected', 'wchar_t',
+    'wctrans_t', 'uint64_t', 'delete', 'and', 'register', 'false', 'int',
+    'uintmax_t', 'off_t', 'char', 'int64_t', 'int_fast16_t', 'DIR', '_Atomic',
+    'time_t', 'xor_eq', 'namespace', 'virtual', 'complex', 'bool', 'mutable',
+    'if', 'int_least32_t', 'sig_atomic_t', 'and_eq', 'ssize_t', 'alignof',
+    '_Alignas', '_Generic', 'extern', 'class', 'typeid', 'short', 'for',
+    'uint_fast64_t', 'wctype_t', 'explicit', 'or_eq', 'switch', 'uint32_t',
+    'inline' )
     
-    apt_get_help = b'''apt 1.0.10.2ubuntu1 for amd64 compiled on Oct  5 2015 15:55:05
-Usage: apt-get [options] command
-       apt-get [options] install|remove pkg1 [pkg2 ...]
-       apt-get [options] source pkg1 [pkg2 ...]
+                self._start_queue_management_thread()
+            self._adjust_process_count()
+            return f
+    submit.__doc__ = _base.Executor.submit.__doc__
     
+                f = _base.Future()
+            w = _WorkItem(f, fn, args, kwargs)
     
-@pytest.mark.parametrize('script, output', [
-    ('brew link sshfs', output),
-    ('cat output', output),
-    ('brew install sshfs', '')])
-def test_not_match(script, output):
-    command = Command(script, output)
-    assert not match(command)
+    def main():
+    for name, fn in [('sequential',
+                      functools.partial(download_urls_sequential, URLS)),
+                     ('processes',
+                      functools.partial(download_urls_with_executor,
+                                        URLS,
+                                        ProcessPoolExecutor(10))),
+                     ('threads',
+                      functools.partial(download_urls_with_executor,
+                                        URLS,
+                                        ThreadPoolExecutor(10)))]:
+        sys.stdout.write('%s: ' % name.ljust(12))
+        start = time.time()
+        url_map = fn()
+        sys.stdout.write('%.2f seconds (%d of %d downloaded)\n' %
+                         (time.time() - start, len(url_map), len(URLS)))
     
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
+    # A shorter title for the navigation bar.  Default is the same as html_title.
+#html_short_title = None
     
-        function_name = module.params.get('function_name')
-    if function_name:
-        lambda_facts[function_name] = {}
-        lambda_facts[function_name].update(config_details(client, module)[function_name])
-        lambda_facts[function_name].update(alias_details(client, module)[function_name])
-        lambda_facts[function_name].update(policy_details(client, module)[function_name])
-        lambda_facts[function_name].update(version_details(client, module)[function_name])
-        lambda_facts[function_name].update(mapping_details(client, module)[function_name])
-    else:
-        lambda_facts.update(config_details(client, module))
+            print('None input')
+        assert_raises(TypeError, merge_sort.sort, None)
     
-            if module.check_mode:
-            firewall_policy_id = get_firewall_policy(oneandone_conn, firewall_id)
-            if (firewall_rules and firewall_policy_id):
-                return True
-            return False
+            print('Success: test_selection_sort\n')
     
-            for _process in processes:
-            monitoring_policy_process = oneandone.client.Process(
-                process=_process['process'],
-                alert_if=_process['alert_if'],
-                email_notification=_process['email_notification']
-            )
-            monitoring_policy_processes.append(monitoring_policy_process)
+            print('Test: Enqueue on empty stack')
+        print('Test: Enqueue on non-empty stack')
+        print('Test: Multiple enqueue in a row')
+        num_items = 3
+        for i in range(0, num_items):
+            queue.enqueue(i)
     
-        try:
-        client.login(username=module.params['ipa_user'],
-                     password=module.params['ipa_pass'])
-        changed, hbacrule = ensure(module, client)
-        module.exit_json(changed=changed, hbacrule=hbacrule)
-    except Exception as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
+        def test_sort_stack(self, stack):
+        print('Test: Empty stack')
+        sorted_stack = self.get_sorted_stack(stack, [])
+        assert_equal(sorted_stack.pop(), None)
     
-        elif not running and state == 'monitored':
-        if module.check_mode:
-            module.exit_json(changed=True)
-        status = run_command('monitor')
-        if status not in ['not monitored']:
-            module.exit_json(changed=True, name=name, state=state)
-        module.fail_json(msg='%s process not monitored' % name, status=status)
+        unreachable_ops = []
+    for op in graph.get_operations():
+      is_unreachable = False
+      all_names = [x.name for x in op.inputs] + [x.name for x in op.outputs]
+      for name in all_names:
+        if name not in seen_tensors:
+          is_unreachable = True
+      if is_unreachable:
+        unreachable_ops.append(op)
+    return unreachable_ops
     
-        if device is None:
-        module.exit_json(changed=False)
+        feature = convert_single_example(ex_index, example, label_list,
+                                     max_seq_length, tokenizer)
+    
+      if FLAGS.do_predict:
+    predict_examples = processor.get_test_examples(FLAGS.data_dir)
+    if FLAGS.use_tpu:
+      # Discard batch remainder if running on TPU
+      n = len(predict_examples)
+      predict_examples = predict_examples[:(n - n % FLAGS.predict_batch_size)]
