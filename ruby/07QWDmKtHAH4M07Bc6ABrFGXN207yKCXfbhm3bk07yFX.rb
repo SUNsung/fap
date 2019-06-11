afc9def3a0eb9cd3d0a1398c1edb3b7ed0d9d541
SUNsung <weatherius@gmail.com>
2019-06-11 21:38:35 +0300

    Rewrite [07QWDmKtHAH4M07Bc6ABrFGXN207yKCXfbhm3bk07yFX.rb] File in: 2019-06-11 21:38:34

--- ruby/07QWDmKtHAH4M07Bc6ABrFGXN207yKCXfbhm3bk07yFX.rb
+++ ruby/07QWDmKtHAH4M07Bc6ABrFGXN207yKCXfbhm3bk07yFX.rb
@@ -1,196 +1,108 @@
 
         
-            Discourse::Cors.apply_headers(cors_origins, env, headers) if cors_origins
-    
-      describe '#<<' do
-    it 'normalizes directive name' do
-      builder << {
-        script_src: ['symbol_underscore'],
-        'script-src': ['symbol_dash'],
-        'script_src' => ['string_underscore'],
-        'script-src' => ['string_dash'],
-      }
-    
-    # == Schema Information
-#
-# Table name: drafts
-#
-#  id         :integer          not null, primary key
-#  user_id    :integer          not null
-#  draft_key  :string           not null
-#  data       :text             not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  sequence   :integer          default(0), not null
-#  revisions  :integer          default(1), not null
-#
-# Indexes
-#
-#  index_drafts_on_user_id_and_draft_key  (user_id,draft_key)
-#
-
-    
-        def html_response?(headers)
-      headers['Content-Type'] && headers['Content-Type'] =~ /html/
+            def url
+      @url ||= URL.parse request.base_url
     end
-  end
-end
-
     
-      context 'with a theme' do
-    let!(:theme) {
-      Fabricate(:theme).tap do |t|
-        settings = <<~YML
-          extend_content_security_policy:
-            type: list
-            default: 'script-src: from-theme.com'
-        YML
-        t.set_field(target: :settings, name: :yaml, value: settings)
-        t.save!
-      end
-    }
-    
-      belongs_to :user
-  after_save :trigger_badges
-    
-            page =
-          if args.last.is_a?(Hash) && args.last[:page]
-            args.last[:page]
-          else
-            1
-          end
-    
-              issue.label_names.each do |label_name|
-            # Although unlikely it's technically possible for an issue to be
-            # given a label that was created and assigned after we imported all
-            # the project's labels.
-            next unless (label_id = label_finder.id_for(label_name))
-    
-            def collection_method
-          :lfs_objects
+            css('label', 'h2 > em', 'h3 > em').each do |node|
+          node.name = 'code'
         end
     
-            attr_reader :attributes
+        if authenticated && resource = warden.user(resource_name)
+      flash[:alert] = I18n.t('devise.failure.already_authenticated')
+      redirect_to after_sign_in_path_for(resource)
+    end
+  end
     
-            '#{route.request_method} #{route.path}' if route
-      end
+        def password_change(record, opts={})
+      devise_mail(record, :password_change, opts)
     end
   end
 end
 
     
-        resp.body =
-      '<html><head>' +
-      '<title>404 Not Found</title>' +
-      '</head><body>' +
-      '<h1>Not found</h1>' +
-      'The requested URL #{html_escape(request.resource)} was not found on this server.<p><hr>' +
-      '</body></html>'
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
     
-            # Sends a kerberos request through the connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [Integer] the number of bytes sent
-        # @raise [RuntimeError] if the transport protocol is unknown
-        # @raise [NotImplementedError] if the transport protocol isn't supported
-        def send_request(req)
-          connect
+        pod 'ObjCPod', path: 'ObjCPod'
+    pod 'SwiftPod', path: 'SwiftPod'
+    pod 'MixedPod', path: 'MixedPod'
+    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
     
-              # Encodes the name_type field
-          #
-          # @return [String]
-          def encode_name_type
-            [name_type].pack('N')
-          end
-    
-              # @!attribute type
-          #   @return [Integer] The algorithm used to generate the checksum
-          attr_accessor :type
-          # @!attribute checksum
-          #   @return [String] The checksum itself
-          attr_accessor :checksum
-    
-              # Rex::Proto::Kerberos::Model::EncKdcResponse encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'EncKdcResponse encoding not supported'
-          end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of an encrypted message.
-        class EncryptedData < Element
-          # @!attribute name_type
-          #   @return [Integer] The encryption algorithm
-          attr_accessor :etype
-          # @!attribute kvno
-          #   @return [Integer] The version number of the key
-          attr_accessor :kvno
-          # @!attribute cipher
-          #   @return [String] The enciphered text
-          attr_accessor :cipher
-    
-              # Decodes the enc_part
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::EncryptedData]
-          def decode_enc_part(input)
-            Rex::Proto::Kerberos::Model::EncryptedData.decode(input.value[0])
-          end
-        end
-      end
-    end
-  end
+    class Rack::Builder
+  include Sinatra::Delegator
 end
+
     
-              # Decodes the realm field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_realm(input)
-            input.value[0].value
-          end
-    
-          def actual_path
-        $PROGRAM_NAME
+          def origin(env)
+        env['HTTP_ORIGIN'] || env['HTTP_X_ORIGIN']
       end
+    
+          def redirect(env)
+        request = Request.new(env)
+        warn env, 'attack prevented by #{self.class}'
+        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
+      end
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
     end
+    
+      it 'should not override the header if already set' do
+    mock_app with_headers('Content-Security-Policy' => 'default-src: none')
+    expect(get('/', {}, 'wants' => 'text/html').headers['Content-Security-Policy']).to eq('default-src: none')
   end
 end
 
     
-      before_action { @server = organization.servers.present.find_by_permalink!(params[:server_id]) }
-  before_action { params[:id] && @credential = @server.credentials.find_by_key!(params[:id]) }
+    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
+
     
-      def remove_from_queue
-    if @message.queued_message && !@message.queued_message.locked?
-      @message.queued_message.destroy
-    end
-    redirect_to_with_json organization_server_message_path(organization, @server, @message.id)
-  end
-    
-      def toggle_ssl
-    @track_domain.update(:ssl_enabled => !@track_domain.ssl_enabled)
-    redirect_to_with_json [organization, @server, :track_domains], :notice => 'SSL settings for #{@track_domain.full_name} updated successfully.'
-  end
-    
-      def update
-    @organization_user = organization.user_assignment(@user)
-    if @organization_user.update(params.require(:organization_user).permit(:admin))
-      redirect_to_with_json [organization, :users], :notice => 'Permissions for #{@organization_user.user.name} have been updated successfully.'
-    else
-      render_form_errors 'edit', @organization_user
+        def get_web_content(url)
+      raw_uri           = URI.parse url
+      proxy             = ENV['http_proxy']
+      if proxy
+        proxy_uri       = URI.parse(proxy)
+        https           = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).new raw_uri.host, raw_uri.port
+      else
+        https           = Net::HTTP.new raw_uri.host, raw_uri.port
+      end
+      https.use_ssl     = true
+      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request           = Net::HTTP::Get.new raw_uri.request_uri
+      data              = https.request request
     end
   end
     
-      def create
-    @webhook = @server.webhooks.build(safe_params)
-    if @webhook.save
-      redirect_to_with_json [organization, @server, :webhooks]
-    else
-      render_form_errors 'new', @webhook
+        def initialize(tag_name, markup, tokens)
+      attributes = ['class', 'src', 'width', 'height', 'title']
+    
+        def tmux_split_command
+      path = if tab.root?
+               '#{Tmuxinator::Config.default_path_option} #{tab.root}'
+             end
+      '#{project.tmux} splitw #{path} -t #{tab.tmux_window_target}'
     end
-  end
+    
+          # Sorted list of all .yml files, including duplicates
+      def configs
+        directories.map do |directory|
+          Dir['#{directory}/**/*.yml'].map do |path|
+            path.gsub('#{directory}/', '').gsub('.yml', '')
+          end
+        end.flatten.sort
+      end
+    
+              after(:all) do
+            puts @session
+            Kernel.system 'tmux kill-session -t #{@session}'
+          end
+    
+        Thank you for installing tmuxinator.
