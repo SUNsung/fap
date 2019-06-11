91938594750a5f5e5cbc3407b8e10933a8d03242
SUNsung <weatherius@gmail.com>
2019-06-11 22:52:01 +0300

    Rewrite [36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py] File in: 2019-06-11 22:52:00

--- pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
+++ pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
@@ -1,26 +1,32 @@
 
         
-        
-class RangeMinValueValidator(MinValueValidator):
-    def compare(self, a, b):
-        return a.lower is None or a.lower < b
-    message = _('Ensure that this range is completely greater than or equal to %(limit_value)s.')
-
+                self.title = match1(html, r'<meta property='og:title' content='([^']*)'')
     
-    from django.contrib.sessions.backends.base import (
-    CreateError, SessionBase, UpdateError,
-)
-from django.core.exceptions import SuspiciousOperation
-from django.db import DatabaseError, IntegrityError, router, transaction
-from django.utils import timezone
-from django.utils.functional import cached_property
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
     
-        For complete documentation on using Sessions in your code, consult
-    the sessions documentation that is shipped with Django (also available
-    on the Django Web site).
-    '''
-    objects = SessionManager()
+        def to_sec(course_id, chap_id, mod):
+        ep = 'http://www.icourses.cn/jpk/viewCharacterDetail2.action?courseId={}&characId={}&mod={}'
+        req = post_content(ep.format(course_id, chap_id, mod), post_data={})
+        return req
     
+            elif re.search(r'i\.imgur\.com/', self.url):
+            # direct image
+            _, container, size = url_info(self.url)
+            self.streams = {
+                'original': {
+                    'src': [self.url],
+                    'size': size,
+                    'container': container
+                }
+            }
+            self.title = r1(r'i\.imgur\.com/([^./]*)', self.url)
     
-sys.path.insert(0, os.path.abspath('_ext'))
-sys.path.insert(0, os.path.abspath('../homeassistant'))
+    class Infoq(VideoExtractor):
+    name = 'InfoQ'
+    
+                if ret[i, 0] != i_label:  # 样本的类别发生了改变
+                changed = True
+    
+        return o
