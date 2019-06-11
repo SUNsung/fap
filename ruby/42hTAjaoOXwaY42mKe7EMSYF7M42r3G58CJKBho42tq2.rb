dfe8158a3be06077ccea825c4d7847c06e773e9f
SUNsung <weatherius@gmail.com>
2019-06-11 17:03:05 +0300

    Rewrite [42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb] File in: 2019-06-11 17:03:04

--- ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
+++ ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
@@ -1,196 +1,95 @@
 
         
-                # Returns the instance variables as a hash of key-value pairs.
-        def instance_variables_hash
-          instance_variables.inject({}) do |acc, iv|
-            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
-            acc
-          end
-        end
-    
-            # This is the method called to provision the system. This method
-        # is expected to do whatever necessary to provision the system (create files,
-        # SSH, etc.)
-        def provision!
-        end
-    
-            # This contains all the registered provider capabilities.
-        #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :provider_capabilities
-    
-            # Sentinel value denoting that a value has not been set.
-        UNSET_VALUE = Object.new
-    
-      #
-  # Split the URI into the resource being requested and its query string.
-  #
-  def update_uri_parts
-    # If it has a query string, get the parts.
-    if ((self.raw_uri) and (md = self.raw_uri.match(/(.+?)\?(.*)$/)))
-      self.uri_parts['QueryString'] = parse_cgi_qstring(md[2])
-      self.uri_parts['Resource']    = md[1]
-    # Otherwise, just assume that the URI is equal to the resource being
-    # requested.
-    else
-      self.uri_parts['QueryString'] = {}
-      self.uri_parts['Resource']    = self.raw_uri
-    end
-    
-      #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
-  #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
-    
-              # Encodes the msg_type field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_msg_type
-            bn = OpenSSL::BN.new(msg_type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-              # Encodes the pvno field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_pvno
-            bn = OpenSSL::BN.new(pvno.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
-    
-    
-module OctopressLiquidFilters
-    
-          unless file.file?
-        return 'File #{file} could not be found'
-      end
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
-        end
-        video += '</video>'
-      else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
-      end
-    end
-    
-        if input_type == 'pleaserun'
-      # Special case for pleaserun that all parameters are considered the 'command'
-      # to run through pleaserun.
-      input.input(args)
-    else
-      # Each remaining command line parameter is used as an 'input' argument.
-      # For directories, this means paths. For things like gem and python, this
-      # means package name or paths to the packages (rails, foo-1.0.gem, django,
-      # bar/setup.py, etc)
-      args.each do |arg|
-        input.input(arg)
-      end
-    end
-    
-          if param == :flag
-        # Automatically make 'flag' (boolean) options tunable with '--[no-]...'
-        flag = flag.collect { |f| '--[no-]#{type}-#{f.gsub(/^--/, '')}' }
-      else
-        flag = flag.collect { |f| '--#{type}-#{f.gsub(/^--/, '')}' }
-      end
-    
-      option '--registry', 'NPM_REGISTRY',
-    'The npm registry to use instead of the default.'
-    
-      option '--identifier-prefix', 'IDENTIFIER_PREFIX',
-    'Reverse domain prefix prepended to package identifier, ' \
-    'ie. 'org.great.my'. If this is omitted, the identifer ' \
-    'will be the package name.'
-  option '--payload-free', :flag, 'Define no payload, assumes use of script options.',
-    :default => false
-  option '--ownership', 'OWNERSHIP',
-    '--ownership option passed to pkgbuild. Defaults to 'recommended'. ' \
-    'See pkgbuild(1).', :default => 'recommended' do |value|
-    if !OWNERSHIP_OPTIONS.include?(value)
-      raise ArgumentError, 'osxpkg-ownership value of '#{value}' is invalid. ' \
-        'Must be one of #{OWNERSHIP_OPTIONS.join(', ')}'
-    end
-    value
-  end
-    
-        # This value is used later in the template for PKGINFO
-    size = safesystemout('du', '-sk', build_path).split(/\s+/)[0].to_i * 1024
-    builddate = Time.new.to_i
-    
-      # Output a zipfile.
-  def output(output_path)
-    output_check(output_path)
-    realpath = Pathname.new(output_path).realdirpath.to_s
-    ::Dir.chdir(staging_path) do
-      safesystem('zip', '-9r', realpath, '.')
-    end
-  end # def output
-end # class FPM::Package::Tar
-
-    
-        return process.exit_code
-  end # def execmd
-    
-    desc 'Clean up files.'
-task :clean do |t|
-  FileUtils.rm_rf 'doc'
-  FileUtils.rm_rf 'tmp'
-  FileUtils.rm_rf 'pkg'
-  FileUtils.rm_rf 'public'
-  FileUtils.rm 'test/debug.log' rescue nil
-  FileUtils.rm 'test/paperclip.db' rescue nil
-  Dir.glob('paperclip-*.gem').each{|f| FileUtils.rm f }
+        # Mimic how the proposed change would first execute a couple of checks and
+# proceed to process with Liquid if necessary
+def conditional_liquid(content)
+  return content if content.nil? || content.empty?
+  return content unless content.include?('{%') || content.include?('{{')
+  always_liquid(content)
 end
-
+    }
+    }
+    }
     
-    Given 'I empty the application.js file' do
-  cd('.') do
-    transform_file('app/assets/javascripts/application.js') do |content|
-      ''
+      </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+    module Jekyll
+  module Filters
+    module GroupingFilters
+      # Group an array of items by a property
+      #
+      # input - the inputted Enumerable
+      # property - the property
+      #
+      # Returns an array of Hashes, each looking something like this:
+      #  {'name'  => 'larry'
+      #   'items' => [...] } # all the items where `property` == 'larry'
+      def group_by(input, property)
+        if groupable?(input)
+          groups = input.group_by { |item| item_property(item, property).to_s }
+          grouped_array(groups)
+        else
+          input
+        end
+      end
+    
+          case command.method_name
+      when 'sh'
+        error_callback = proc { |string_value| closure_argument_value = string_value }
+        command_param = parameter_map[:command]
+        log_param = parameter_map[:log]
+        action_return = Fastlane::FastFile.sh(command_param, log: log_param, error_callback: error_callback)
+      end
+    
+            result = Fastlane::FastFile.new.parse('lane :test do
+          add_git_tag ({
+            tag: '#{tag}',
+            grouping: 'grouping',
+            build_number: 'build_number',
+            prefix: 'prefix',
+          })
+        end').runner.execute(:test)
+    
+    # for each blog URL, check if rss URL exists
+matches.each do |match|
+  name = match[0]
+  web_url = match[1]
+    
+          File.open('bower.json', 'w') do |f|
+        f.puts JSON.pretty_generate(spec)
+      end
     end
   end
 end
+
     
-        def type_from_file_contents
-      type_from_mime_magic || type_from_file_command
-    rescue Errno::ENOENT => e
-      Paperclip.log('Error while determining content type: #{e}')
-      SENSIBLE_DEFAULT
-    end
+    Then(/the output matches '([^']*)' followed by '([^']*)'/) do |expected, followedby|
+  expect(@output).to match(/#{expected}.*#{followedby}/m)
+end
     
-        def define_setter
-      name = @name
-      @klass.send :define_method, '#{@name}=' do |file|
-        send(name).assign(file)
+          def using_default_scm?
+        return @using_default_scm if defined? @using_default_scm
+        @using_default_scm = (fetch(:scm) == DEFAULT_GIT)
       end
-    end
+    
+          def set(key, value=nil, &block)
+        @trusted_keys << key if trusted? && !@trusted_keys.include?(key)
+        remember_location(key)
+        values[key] = block || value
+        trace_set(key)
+        values[key]
+      end
