a924ecf70efcfdd75c29b5524f4ad1f4154e010e
SUNsung <weatherius@gmail.com>
2019-06-11 08:17:24 +0300

    Rewrite [04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp] File in: 2019-06-11 08:17:24

--- cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
+++ cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
@@ -1,179 +1,227 @@
 
         
-        PyArray_Descr NPyBfloat16_Descr = {
-    PyObject_HEAD_INIT(nullptr) & PyBfloat16_Type,  // typeobj
-    // We must register bfloat16 with a kind other than 'f', because numpy
-    // considers two types with the same kind and size to be equal, but
-    // float16 != bfloat16.
-    'V',  // kind
-    // TODO(phawkins): there doesn't seem to be a way of guaranteeing a type
-    // character is unique.
-    'E',                                                  // type
-    '=',                                                  // byteorder
-    NPY_NEEDS_PYAPI | NPY_USE_GETITEM | NPY_USE_SETITEM,  // hasobject
-    0,                                                    // type_num
-    sizeof(bfloat16),                                     // elsize
-    alignof(bfloat16),                                    // alignment
-    nullptr,                                              // subarray
-    nullptr,                                              // fields
-    nullptr,                                              // names
-    &NPyBfloat16_ArrFuncs,                                // f
-};
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    namespace tensorflow {
-    }
-    
-    // Safe containers for an owned TF_Status. On destruction, the handle
-// will be deleted by TF_DeleteStatus.
-using Safe_TF_StatusPtr = std::unique_ptr<TF_Status, detail::TFStatusDeleter>;
-Safe_TF_StatusPtr make_safe(TF_Status* status);
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-      const tensorflow::OpRegistrationData* op_reg_data;
-  auto status =
-      tensorflow::OpRegistry::Global()->LookUp(node_def.op(), &op_reg_data);
-  if (!status.ok()) {
-    LOG(WARNING) << 'Op ' << node_def.op() << ' not found: ' << status;
-    return '';
-  }
-  AddDefaultsToNodeDef(op_reg_data->op_def, &node_def);
-    
-    // Functions for getting information about kernels registered in the binary.
-#ifndef TENSORFLOW_PYTHON_UTIL_KERNEL_REGISTRY_H_
-#define TENSORFLOW_PYTHON_UTIL_KERNEL_REGISTRY_H_
-    
-    #ifndef TENSORFLOW_STREAM_EXECUTOR_DEVICE_OPTIONS_H_
-#define TENSORFLOW_STREAM_EXECUTOR_DEVICE_OPTIONS_H_
-    
-    
-    { private:
-  SE_DISALLOW_COPY_AND_ASSIGN(FftSupport);
-};
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    void HostTimer::StartNow() { start_time_ = clock::now(); }
-    
-    #include 'atom/browser/native_window_views.h'
-    
-    #include 'atom/browser/api/event_emitter.h'
-    
-    
-    {}  // namespace api
-    
-      // content::QuotaPermissionContext:
-  void RequestQuotaPermission(const content::StorageQuotaParams& params,
-                              int render_process_id,
-                              const PermissionCallback& callback) override;
-    
-    
-    {}  // namespace auto_updater
+        
+    {}  // namespace tensorflow
 
     
-      // net::URLRequestJobFactory::ProtocolHandler:
-  net::URLRequestJob* MaybeCreateJob(
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const override;
-  bool IsSafeRedirectTarget(const GURL& location) const override;
+    namespace stream_executor {
+namespace host {
+    }
+    }
     
-    void URLRequestAboutJob::Start() {
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(&URLRequestAboutJob::StartAsync,
-                                weak_ptr_factory_.GetWeakPtr()));
+    
+    {
+    {}  // namespace host
+}  // namespace stream_executor
+    
+    #endif  // TENSORFLOW_STREAM_EXECUTOR_HOST_HOST_TIMER_H_
+
+    
+    #if !defined(PREPOSITION) && !defined(VERB)
+#  error define one or more of PREPOSITION, VERB
+#endif
+    
+        void emitDiagnosticLoc(clang::FullSourceLoc Loc, clang::PresumedLoc PLoc,
+                           clang::DiagnosticsEngine::Level Level,
+                           ArrayRef<clang::CharSourceRange> Ranges) override {}
+    
+    using namespace swift;
+using namespace Mangle;
+    
+    
+    {
+    {        vst1q_s16(dsty + j, dy);
+        vst1q_s16(dstx + j, dx);
+        vst1q_s32(mag + j, norml);
+        vst1q_s32(mag + j + 4, normh);
+    }
+    if (j != width)
+    {
+        j = width - 8;
+        goto ColFilter3x3CannyL2Loop;
+    }
 }
     
-    void OffscreenViewProxy::SetBitmap(const SkBitmap& bitmap) {
-  if (view_bounds_.width() == bitmap.width() &&
-      view_bounds_.height() == bitmap.height() && observer_) {
-    view_bitmap_.reset(new SkBitmap(bitmap));
-    observer_->OnProxyViewPaint(view_bounds_);
-  }
+    #define CONTSRC2 dstStride == src0Stride && \
+                 dstStride == src1Stride &&
+#define CONTSRC3 dstStride == src0Stride && \
+                 dstStride == src1Stride && \
+                 dstStride == src2Stride &&
+#define CONTSRC4 dstStride == src0Stride && \
+                 dstStride == src1Stride && \
+                 dstStride == src2Stride && \
+                 dstStride == src3Stride &&
+    
+    u8 cornerScore(const u8* ptr, const ptrdiff_t pixel[])
+{
+    const s32 K = 8, N = 16 + K + 1;
+    s32 k, v = ptr[0];
+    s16 d[(N + 7) & ~7];
+    for( k = 0; k < N; k++ )
+        d[k] = (s16)(v - ptr[pixel[k]]);
+    }
+    
+                    int32x4_t ln042 = vmlaq_s32(ln04, lane2, vc6s32);
+                int32x4_t lsw = vmlaq_s32(ln042, ln13, vc4s32);
+    
+    
+    {} // namespace CAROTENE_NS
+
+    
+                uint32x4_t el8shr01l =  vaddl_u16(vget_low_u16(el8shr0), vget_low_u16(el8shr1));
+            uint32x4_t el8shr01h =  vaddl_u16(vget_high_u16(el8shr0), vget_high_u16(el8shr1));
+    
+                if(!x) {
+                // make border
+                if (border == BORDER_MODE_REPLICATE) {
+                    tpprev1 = vextq_s16(tnext2, tnext2, 7);
+                    tpprev1 = vsetq_lane_s16(vgetq_lane_s16(tpprev1, 1),tpprev1, 0);
+    }
+    }
+    
+    std::string Clipboard::GetText() {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  base::string16 text;
+  clipboard->ReadText(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
+  return base::UTF16ToUTF8(text);
 }
     
-    #include 'ui/aura/client/drag_drop_client.h'
-#include 'ui/aura/window.h'
-#include 'ui/base/dragdrop/drag_drop_types.h'
-#include 'ui/base/dragdrop/file_info.h'
-#include 'ui/base/dragdrop/os_exchange_data.h'
-#include 'ui/display/screen.h'
-#include 'ui/gfx/geometry/point.h'
-#include 'ui/views/button_drag_utils.h'
-#include 'ui/views/widget/widget.h'
-#include 'url/gurl.h'
+    #ifndef CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+#define CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
     
-    // A '1' in this array means the character is white space.  A '1' or
-// '2' means the character ends a name or command.
-static const char specialChars[256] = {
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0,   // 0x
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1x
-  1, 0, 0, 0, 0, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 2,   // 2x
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0,   // 3x
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 4x
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0,   // 5x
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 6x
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0,   // 7x
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 8x
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 9x
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // ax
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // bx
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // cx
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // dx
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // ex
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0    // fx
+    bool MenuDelegate::HasIcon(int command_id) {
+  if (command_id < 0)
+    return false;
+    }
+    
+       bool IsCommandIdChecked(int command_id) const override;
+   bool IsCommandIdEnabled(int command_id) const override;
+    
+    class NwAppGetDataPathFunction : public NWSyncExtensionFunction {
+ public:
+  NwAppGetDataPathFunction(){}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwAppGetDataPathFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.App.getDataPath', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwAppGetDataPathFunction);
 };
     
-    void PreScanOutputDev::drawImageMask(GfxState *state, Object * /*ref*/, Stream *str,
-				     int width, int height, GBool /*invert*/,
-				     GBool /*interpolate*/, GBool inlineImg) {
-  int i, j;
+      void __set_message(const std::string& val);
+    
+          /* perform the first step.  this will tell us if we
+      ** have a result set or not and how wide it is.
+      */
+      rc = sqlite3_step(pStmt);
+      /* if we have a result set... */
+      if (SQLITE_ROW == rc) {
+        /* if we have a callback... */
+        if (xCallback != nullptr) {
+          /* allocate space for col name ptr, value ptr, and type */
+          int nCol = sqlite3_column_count(pStmt);
+          void* pData = sqlite3_malloc(3 * nCol * sizeof(const char*) + 1);
+          if (pData == nullptr) {
+            rc = SQLITE_NOMEM;
+          } else {
+            const auto** azCols = reinterpret_cast<const char**>(
+                pData); /* Names of result columns */
+            const char** azVals = &azCols[nCol]; /* Results */
+            auto* aiTypes =
+                reinterpret_cast<int*>(&azVals[nCol]); /* Result types */
+            int i;
+            assert(sizeof(int) <= sizeof(char*));
+            /* save off ptrs to column names */
+            for (i = 0; i < nCol; i++) {
+              azCols[i] = const_cast<char*>(sqlite3_column_name(pStmt, i));
+            }
+            do {
+              /* extract the data and data types */
+              for (i = 0; i < nCol; i++) {
+                aiTypes[i] = sqlite3_column_type(pStmt, i);
+                azVals[i] = (char*)sqlite3_column_text(pStmt, i);
+                if ((azVals[i] == nullptr) && (aiTypes[i] != SQLITE_NULL)) {
+                  rc = SQLITE_NOMEM;
+                  break; /* from for */
+                }
+              } /* end for */
+    }
+    }
+    }
     }
     
-    Sound::~Sound()
-{
-  delete fileName;
-  streamObj->free();
-  delete streamObj;
+    
+    {
+    {struct mei_version {
+  uint32_t important_details[7];
+  uint16_t major;
+  uint16_t minor;
+  uint16_t hotfix;
+  uint16_t build;
+  uint16_t r_major;
+  uint16_t r_minor;
+  uint16_t r_hotfix;
+  uint16_t r_build;
+  uint16_t codes[6];
+};
+}
+}
+
+    
+    TEST_F(iokitRegistry, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from iokit_registry');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'name', NormalType}
+  //      {'class', NormalType}
+  //      {'id', IntType}
+  //      {'parent', IntType}
+  //      {'busy_state', IntType}
+  //      {'retain_count', IntType}
+  //      {'depth', IntType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
 }
     
-    #include 'StdinCachedFile.h'
+    class KernelInfo : public testing::Test {
+  protected:
+    void SetUp() override {
+      setUpEnvironment();
+    }
+};
     
-    class StdinCacheLoader : public CachedFileLoader {
+    class keychainItems : public testing::Test {
+ protected:
+  void SetUp() override {
+    setUpEnvironment();
+  }
+};
+    
+    
+    // out_of_range.106
+    try
+    {
+        // try to use an array index with leading '0'
+        json::reference ref = j.at('/array/01'_json_pointer);
+    }
+    catch (json::parse_error& e)
+    {
+        std::cout << e.what() << '\n';
     }
     
-    PDFDoc *
-StdinPDFDocBuilder::buildPDFDoc(const GooString &uri, GooString *ownerPassword,
-                                    GooString *userPassword, void *guiDataA)
-{
-  Object obj;
-    }
-    
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'ID3D10ShaderResourceView*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-    
-        bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    
-    
-    {    for (UINT i = 0; i < NUM_BACK_BUFFERS; i++)
-        if (g_mainRenderTargetResource[i]) { g_mainRenderTargetResource[i]->Release(); g_mainRenderTargetResource[i] = NULL; }
-}
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
+        // create an object from std::unordered_map
+    std::unordered_map<const char*, double> c_umap
+    {
+        {'one', 1.2}, {'two', 2.3}, {'three', 3.4}
+    };
+    json j_umap(c_umap);
