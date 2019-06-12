46c1a74652d550ac4eebf0962cb01cddb041bfdd
SUNsung <weatherius@gmail.com>
2019-06-12 15:03:55 +0300

    Rewrite [11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py] File in: 2019-06-12 15:03:55

--- pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
+++ pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
@@ -1,148 +1,57 @@
 
         
-        def generateKey(keySize):
-    print('Generating prime p...')
-    p = rabinMiller.generateLargePrime(keySize)
-    print('Generating prime q...')
-    q = rabinMiller.generateLargePrime(keySize)
-    n = p * q
+        import numpy as np
     
-                if new_key is None:
-                break
+    from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
     
-                return self.dp[x][y]
     
-    def not32(i):
-	i_str = format(i,'032b')
-	new_str = ''
-	for c in i_str:
-		new_str += '1' if c=='0' else '0'
-	return int(new_str,2)
-    
-        return score
+def testMap():
+    check(b'\x96'
+          b'\xde\x00\x00'
+          b'\xde\x00\x01\xc0\xc2'
+          b'\xde\x00\x02\xc0\xc2\xc3\xc2'
+          b'\xdf\x00\x00\x00\x00'
+          b'\xdf\x00\x00\x00\x01\xc0\xc2'
+          b'\xdf\x00\x00\x00\x02\xc0\xc2\xc3\xc2', ({}, {None: False},
+                                                    {True: False,
+                                                     None: False}, {},
+                                                    {None: False},
+                                                    {True: False,
+                                                     None: False}))
 
     
-    * @author chinmoy159
-* @version 1.0 dated 10/08/2017
-'''
-    
-    
-'''
-* Wondering how this method works !
-* It's pretty simple.
-* Let's say you need to calculate a ^ b
-* RULE 1 : a * b = (a+a) * (b/2) ---- example : 4 * 4 = (4+4) * (4/2) = 8 * 2
-* RULE 2 : IF b is ODD, then ---- a * b = a + (a * (b - 1)) :: where (b - 1) is even.
-* Once b is even, repeat the process to get a * b
-* Repeat the process till b = 1 OR b = 0, because a*1 = a AND a*0 = 0
-*
-* As far as the modulo is concerned,
-* the fact : (a+b) % c = ((a%c) + (b%c)) % c
-* Now apply RULE 1 OR 2, whichever is required.
-'''
+    fig.savefig('pandas_logo.svg')
+fig.savefig('pandas_logo.png')
 
     
     
-# ALTERNATIVE METHODS  
-# ctbi= characters that must be in password
-# i= how many letters or characters the password length will be 
-def password_generator(ctbi, i):
-  # Password generator = full boot with random_number, random_letters, and random_character FUNCTIONS
-  pass  # Put your code here...
-    
-            status_line = 'HTTP/{version} {status} {reason}'.format(
-            version=version,
-            status=original.status,
-            reason=original.reason
-        )
-        headers = [status_line]
-        try:
-            # `original.msg` is a `http.client.HTTPMessage` on Python 3
-            # `_headers` is a 2-tuple
-            headers.extend(
-                '%s: %s' % header for header in original.msg._headers)
-        except AttributeError:
-            # and a `httplib.HTTPMessage` on Python 2.x
-            # `headers` is a list of `name: val<CRLF>`.
-            headers.extend(h.strip() for h in original.msg.headers)
-    
-        def __init__(self, groups, env=Environment(), **kwargs):
-        '''
-        :param groups: names of processor groups to be applied
-        :param env: Environment
-        :param kwargs: additional keyword arguments for processors
-    
-        package_name = '(builtin)'
-    
-        # Auth
-    def get_auth_plugins(self):
-        return [plugin for plugin in self if issubclass(plugin, AuthPlugin)]
+def test_bin8():
+    header = b'\xc4'
+    data = b''
+    b = packb(data, use_bin_type=True)
+    assert len(b) == len(data) + 2
+    assert b[0:2] == header + b'\x00'
+    assert b[2:] == data
+    assert unpackb(b) == data
     
     
-def test_current_version():
-    version = Environment().config['__meta__']['httpie']
-    assert version == __version__
-
+def test_read_map_header():
+    unpacker = Unpacker()
+    unpacker.feed(packb({'a': 'A'}))
+    assert unpacker.read_map_header() == 1
+    assert unpacker.unpack() == B'a'
+    assert unpacker.unpack() == B'A'
+    try:
+        unpacker.unpack()
+        assert 0, 'should raise exception'
+    except OutOfData:
+        assert 1, 'okay'
     
-    
-def rst_filenames():
-    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
-        if '.tox' not in root:
-            for filename in fnmatch.filter(filenames, '*.rst'):
-                yield os.path.join(root, filename)
-    
-    from httpie import __version__
-from httpie.compat import is_windows
-    
-    The Fast R-CNN head produces a feature representation of the RoI for the purpose
-of bounding box classification and regression. The box output module converts
-the feature representation into classification and regression predictions.
-'''
-    
-    
-def _generate_anchors(base_size, scales, aspect_ratios):
-    '''Generate anchor (reference) windows by enumerating aspect ratios X
-    scales wrt a reference (0, 0, base_size - 1, base_size - 1) window.
-    '''
-    anchor = np.array([1, 1, base_size, base_size], dtype=np.float) - 1
-    anchors = _ratio_enum(anchor, aspect_ratios)
-    anchors = np.vstack(
-        [_scale_enum(anchors[i, :], scales) for i in range(anchors.shape[0])]
-    )
-    return anchors
-    
-    
-def rpn(model):
-    logger.warn(
-        'Deprecated: use `MODEL.TYPE: generalized_rcnn` with '
-        '`MODEL.RPN_ONLY: True`'
-    )
-    return generalized_rcnn(model)
-    
-        model.AddMetrics(['retnet_fg_num', 'retnet_bg_num'])
-    # ==========================================================================
-    # bbox regression loss - SelectSmoothL1Loss for multiple anchors at a location
-    # ==========================================================================
-    for lvl in range(k_min, k_max + 1):
-        suffix = 'fpn{}'.format(lvl)
-        bbox_loss = model.net.SelectSmoothL1Loss(
-            [
-                'retnet_bbox_pred_' + suffix,
-                'retnet_roi_bbox_targets_' + suffix,
-                'retnet_roi_fg_bbox_locs_' + suffix, 'retnet_fg_num'
-            ],
-            'retnet_loss_bbox_' + suffix,
-            beta=cfg.RETINANET.BBOX_REG_BETA,
-            scale=model.GetLossScale() * cfg.RETINANET.BBOX_REG_WEIGHT
-        )
-        gradients.append(bbox_loss)
-        losses.append('retnet_loss_bbox_' + suffix)
-    
-    
-class GenerateProposalLabelsOp(object):
-    
-    from detectron.core.config import cfg
-import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
-import detectron.roi_data.retinanet as retinanet_roi_data
-import detectron.roi_data.rpn as rpn_roi_data
-import detectron.utils.blob as blob_utils
+        # 5 ok for read_size=16, while 6 glibc detected *** python: double free or
+    # corruption (fasttop):
