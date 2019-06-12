f42f511377b083739bd6dcebd4bbf97d47b2475d
SUNsung <weatherius@gmail.com>
2019-06-12 09:43:59 +0300

    Rewrite [06w4J6o4o3y8Q06vFvtO1A3kOY06WsJPLGCC7tU06of6.rb] File in: 2019-06-12 09:43:58

--- ruby/06w4J6o4o3y8Q06vFvtO1A3kOY06WsJPLGCC7tU06of6.rb
+++ ruby/06w4J6o4o3y8Q06vFvtO1A3kOY06WsJPLGCC7tU06of6.rb
@@ -1,107 +1,150 @@
 
         
-        def pr(url)
-  if url.end_with?(FORWARD_SLASH)
-    url
-  else
-    url_dir = File.dirname(url)
-    url_dir.end_with?(FORWARD_SLASH) ? url_dir : '#{url_dir}/'
-  end
-end
-    
-    Jekyll::PluginManager.require_from_bundler
-    
-          def after_feature_element(feature_element)
-        @timings[feature_element_timing_key(feature_element)] = Time.now - @timings[feature_element_timing_key(feature_element)]
-        @io.print ' (#{@timings[feature_element_timing_key(feature_element)]}s)'
-      end
-    
-          def jekyll
-        JekyllDrop.global
-      end
-    
-            def id_for_already_imported_cache(issue)
-          issue.number
-        end
-    
-              lfs_objects.each do |object|
-            yield object
+                    # Prepend a hidden field to make sure something will be sent back to the
+            # server if all radio buttons are unchecked.
+            if options.fetch('include_hidden', true)
+              hidden_field + rendered_collection
+            else
+              rendered_collection
+            end
           end
-        rescue StandardError => e
-          Rails.logger.error('The Lfs import process failed. #{e.message}')
-        end
+    
+              def render_component(builder)
+            builder.radio_button + builder.label
+          end
       end
     end
   end
 end
 
     
-            expose_attribute :id, :login
-    
-            if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
-        end
-    
-        def initialize(name = nil, path = nil, type = nil)
-      self.name = name
-      self.path = path
-      self.type = type
-    
-        def options
-      @options ||= self.class.options.deep_dup.tap do |options|
-        options.merge! base_url: base_url, root_url: root_url,
-                       root_path: root_path, initial_paths: initial_paths,
-                       version: self.class.version, release: self.class.release
-    
-            css('br', 'hr', '.material-icons', '.header-link', '.breadcrumb').remove
-    
-            css('div.badges').each do |node|
-          node.name = 'p'
-        end
-    
-      # Setup the root logger with the Rails log level and the desired set of
-  # appenders. The list of appenders to use should be set in the environment
-  # specific configuration file.
-  #
-  # For example, in a production application you would not want to log to
-  # STDOUT, but you would want to send an email for 'error' and 'fatal'
-  # messages:
-  #
-  # => config/environments/production.rb
-  #
-  #     config.log_to = %w[file email]
-  #
-  # In development you would want to log to STDOUT and possibly to a file:
-  #
-  # => config/environments/development.rb
-  #
-  #     config.log_to = %w[stdout file]
-  #
-  Logging.logger.root.appenders = config.log_to unless config.log_to.empty?
-    
-      describe '#read_all' do
-    let(:post) { FactoryGirl.create(:status_message) }
-    
-        describe 'other streams' do
-      it 'redirects to the login page' do
-        %i[activity followed_tags liked mentioned multi].each do |stream_path|
-          get stream_path
-          expect(response).to be_redirect
-          expect(response).to redirect_to new_user_session_path
-        end
+                  Time.utc(
+                default[:year], default[:month], default[:day],
+                default[:hour], default[:min], default[:sec]
+              )
+            end
+          end
       end
     end
   end
 end
 
     
-    def system!(*args)
-  system(*args) || abort('\n== Command #{args} failed ==')
-end
+              label_tag(name_and_id['id'], content, options)
+        end
     
-      # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+    module ActionView
+  module Helpers
+    module Tags # :nodoc:
+      class TextField < Base # :nodoc:
+        include Placeholderable
+    
+      def self.req_id(date, req_type, retries = 0)
+    
+      def self.period_types
+    @types ||= Enum.new(all: 1,
+                        yearly: 2,
+                        monthly: 3,
+                        weekly: 4,
+                        daily: 5,
+                        quarterly: 6)
+  end
+    
+      describe '#<<' do
+    it 'normalizes directive name' do
+      builder << {
+        script_src: ['symbol_underscore'],
+        'script-src': ['symbol_dash'],
+        'script_src' => ['string_underscore'],
+        'script-src' => ['string_dash'],
+      }
+    
+        def clear_theme_extensions_cache!
+      cache.clear
+    end
+    
+      # topic_reply_count is a count of posts in other users' topics
+  def update_topic_reply_count
+    self.topic_reply_count = Topic
+      .joins('INNER JOIN posts ON topics.id = posts.topic_id AND topics.user_id <> posts.user_id')
+      .where('posts.deleted_at IS NULL AND posts.user_id = ?', self.user_id)
+      .distinct
+      .count
+  end
+    
+              if env[Auth::DefaultCurrentUserProvider::BAD_TOKEN]
+            headers['Discourse-Logged-Out'] = '1'
+          end
+    
+        if user_token.auth_token != token && user_token.prev_auth_token == token && user_token.auth_token_seen
+      changed_rows = UserAuthToken
+        .where('rotated_at < ?', 1.minute.ago)
+        .where(id: user_token.id, prev_auth_token: token)
+        .update_all(auth_token_seen: false)
+    
+        def current_url
+      context[:url]
+    end
+    
+        def name=(value)
+      @name = value.try :strip
+    end
+    
+            options.merge!(additional_options)
+        options.freeze
+      end
+    end
+    
+            css('pre[language]').each do |node|
+          node['data-language'] = node['language'].sub(/\Ats/, 'typescript').strip
+          node['data-language'] = 'html' if node.content.start_with?('<')
+          node.remove_attribute('language')
+          node.remove_attribute('format')
+        end
+    
+      included do
+    before_action :set_session_activity
+  end
+    
+    class RemoteUnfollowsController < ApplicationController
+  layout 'modal'
+    
+    World(RemoteCommandHelpers)
+
+    
+    World(VagrantHelpers)
+
+    
+        def configure_scm
+      Capistrano::Configuration::SCMResolver.new.resolve
+    end
+    
+          def gets
+        return unless $stdin.tty?
+    
+          def each
+        servers_by_key.values.each { |server| yield server }
+      end
+    
+    module Capistrano
+  class Configuration
+    # Decorates a Variables object to additionally perform an optional set of
+    # user-supplied validation rules. Each rule for a given key is invoked
+    # immediately whenever `set` is called with a value for that key.
+    #
+    # If `set` is called with a callable value or a block, validation is not
+    # performed immediately. Instead, the validation rules are invoked the first
+    # time `fetch` is used to access the value.
+    #
+    # A rule is simply a block that accepts two arguments: key and value. It is
+    # up to the rule to raise an exception when it deems the value is invalid
+    # (or just print a warning).
+    #
+    # Rules can be registered using the DSL like this:
+    #
+    #   validate(:my_key) do |key, value|
+    #     # rule goes here
+    #   end
+    #
+    class ValidatedVariables < SimpleDelegator
+      include Capistrano::ProcHelpers
