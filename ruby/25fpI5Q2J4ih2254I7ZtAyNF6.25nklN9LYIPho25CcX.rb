06d6361c5f22d7e4c606839102c69964faa4f2b1
SUNsung <weatherius@gmail.com>
2019-06-13 00:31:16 +0300

    Rewrite [25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb] File in: 2019-06-13 00:31:16

--- ruby/25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb
+++ ruby/25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb
@@ -1,150 +1,20 @@
 
         
-            def handle_ssl_error!(e)
-      # SSL errors are very common when the Ruby or OpenSSL installation is somehow broken
-      # We want to show a nice error message to the user here
-      # We have over 20 GitHub issues just for this one error:
-      #   https://github.com/fastlane/fastlane/search?q=errno%3D0+state%3DSSLv3+read+server&type=Issues
-      suggest_ruby_reinstall(e)
-      display_user_error!(e, e.to_s)
+              private
+    
+        def format_path(path)
+      path.to_s.remove File.join(File.expand_path('.'), '')
     end
     
-          it 'passes the deprecated pathspec parameter to path parameter' do
-        with_verbose(true) do
-          allow(Fastlane::Actions).to receive(:sh).with(anything, { log: true }).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(pathspec: 'myfile.txt')
-          end').runner.execute(:test)
-        end
-      end
-    
-    if git.modified_files.include?('snapshot/lib/assets/SnapshotHelperXcode8.swift')
-  warn('You modified `SnapshotHelperXcode8.swift`, make sure to update the version number at the bottom of the file to notify users about the new helper file.')
-end
-    
-          command :revoke_expired do |c|
-        c.syntax = 'fastlane cert revoke_expired'
-        c.description = 'Revoke expired iOS code signing certificates'
-    
-          OptionParser.new do |opts|
-        opts.banner = 'See full documentation at http://capistranorb.com/.'
-        opts.separator ''
-        opts.separator 'Install capistrano in a project:'
-        opts.separator '    bundle exec cap install [STAGES=qa,staging,production,...]'
-        opts.separator ''
-        opts.separator 'Show available tasks:'
-        opts.separator '    bundle exec cap -T'
-        opts.separator ''
-        opts.separator 'Invoke (or simulate invoking) a task:'
-        opts.separator '    bundle exec cap [--dry-run] STAGE TASK'
-        opts.separator ''
-        opts.separator 'Advanced options:'
-    
-        def add_filter(filter=nil, &block)
-      if block
-        raise ArgumentError, 'Both a block and an object were given' if filter
-    
-          def extract_options(array)
-        array.last.is_a?(::Hash) ? array.pop : {}
-      end
-    end
-  end
-end
-
-    
-          def masked_token?(token)
-        token.length == TOKEN_LENGTH * 2
-      end
-    
-          def escape_string(str)
-        str = @escaper.escape_url(str)        if @url
-        str = @escaper.escape_html(str)       if @html
-        str = @escaper.escape_javascript(str) if @javascript
-        str
-      end
-    end
-  end
-end
-
-    
-        it 'sets a session authenticity token if one does not exist' do
-      session = {}
-      allow(Rack::Protection::AuthenticityToken).to receive(:random_token).and_return(token)
-      allow_any_instance_of(Rack::Protection::AuthenticityToken).to receive(:mask_token).and_return(masked_token)
-      Rack::Protection::AuthenticityToken.token(session)
-      expect(session[:csrf]).to eq(token)
-    end
-  end
-    
-        it 'can still be displayed' do
-      expect { visit spree.edit_admin_order_path(order) }.not_to raise_error
-    end
-  end
-    
-            def empty
-          authorize! :update, @order, order_token
-          @order.empty!
-          render plain: nil, status: 204
+            # Remove dead links (e.g. ngRepeat)
+        css('a.type-hint').each do |node|
+          node.name = 'code'
+          node.remove_attribute 'href'
         end
     
-            def create
-          authorize! :create, Product
-          params[:product][:available_on] ||= Time.current
-          set_up_shipping_category
-    
-          def variant_attributes
-        if @current_user_roles&.include?('admin')
-          @@variant_attributes + [:cost_price]
-        else
-          @@variant_attributes
-        end
-      end
+        def dependencies
+      @dependencies ||= [Cask::CaskLoader.load('adobe-air')]
     end
-  end
-end
-
     
-    # A pleaserun package.
-#
-# This does not currently support 'output'
-class FPM::Package::PleaseRun < FPM::Package
-  # TODO(sissel): Implement flags.
-    
-      # Helper for user lookup
-  def uid2user(uid)
-    begin
-      pwent = Etc.getpwuid(uid)
-      return pwent.name
-    rescue ArgumentError => e
-      # Invalid user id? No user? Return the uid.
-      logger.warn('Failed to find username for uid #{uid}')
-      return uid.to_s
-    end
-  end # def uid2user
-    
-          # TODO(sissel): preinstall/postinstall
-      # strip @prefix, since BASEDIR will set prefix via the pkginfo file
-      IO.popen('pkgproto #{staging_path}/#{@prefix}=').each_line do |line|
-        type, klass, path, mode, user, group = line.split
-    
-        # Unpack the tarball to the staging path
-    args = ['-cf', output_path, '-C', staging_path]
-    tar_compression_flag(output_path).tap do |flag|
-      args << flag unless flag.nil?
-    end
-    args << '.'
-    
-          case
-      when value.is_a?(String), value.is_a?(Symbol)
-        %W(--#{opt} #{value})
-      when value.is_a?(Array)
-        value.map { |v| %W(--#{opt} #{v}) }
-      when value.is_a?(TrueClass)
-        '--#{opt}'
-      when value.is_a?(FalseClass)
-        '--no-#{opt}'
-      else
-        fail TypeError, 'Unexpected type: #{value.class}'
-      end
-    end
-  end
+        result && attributes_match if @expected_attrs
+    result &&= commands_match if commands
