835648688f150e8f51e409a8160f3aacd44ee3ac
SUNsung <weatherius@gmail.com>
2019-06-12 12:34:44 +0300

    Rewrite [39vkayoFfuS8g3928E3.zM.7.I39GglMFJMHoWE39wKG.cpp] File in: 2019-06-12 12:34:44

--- cccccc/39vkayoFfuS8g3928E3.zM.7.I39GglMFJMHoWE39wKG.cpp
+++ cccccc/39vkayoFfuS8g3928E3.zM.7.I39GglMFJMHoWE39wKG.cpp
@@ -1,88 +1,256 @@
 
         
-        
-void Base::Call(const std::string& method, const base::ListValue& arguments,
-                content::RenderFrameHost* rvh) {
-  NOTREACHED() << 'Uncatched call in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
-}
+            http://www.apache.org/licenses/LICENSE-2.0
     
-    v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
-                                           int object_id,
-                                           const std::string& type,
-                                           const std::string& method,
-                                           v8::Handle<v8::Value> args) {
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  scoped_ptr<V8ValueConverter> converter(V8ValueConverter::create());
-    }
+      for (const auto& node : item_.MainOpsFanin()) {
+    PrintNodeInfo(node, properties, debug, os);
+  }
+  for (const auto& node : item_.EnqueueOpsFanin()) {
+    PrintNodeInfo(node, properties, debug, os);
+  }
+    
+    #include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // We define the PY_ARRAY_UNIQUE_SYMBOL in this .cc file and provide an
+// ImportNumpy function to populate it.
+#define TF_IMPORT_NUMPY
+    
+    #ifndef PyObject_HEAD
+struct _object;
+typedef _object PyObject;
+#endif
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
     
-    {}  // namespace nw
+    {
+    {}  // namespace host
+}  // namespace stream_executor
 
     
-      if (type == 'separator') {
-    menu_item_ = gtk_separator_menu_item_new();
-  } else {
-    if (type == 'checkbox') {
-      menu_item_ = gtk_check_menu_item_new();
-      bool checked;
-      if (option.GetBoolean('checked', &checked))
-        SetChecked(checked);
-    } else {
-      menu_item_ = gtk_image_menu_item_new();
-      std::string icon;
-      if (option.GetString('icon', &icon))
-        SetIcon(icon);
-    }
+    using std::chrono::duration_cast;
+    
+    // This file contains declarations relating to kernel cache configuration
+// parameters recognized by the StreamExecutor.
+#ifndef TENSORFLOW_STREAM_EXECUTOR_KERNEL_CACHE_CONFIG_H_
+#define TENSORFLOW_STREAM_EXECUTOR_KERNEL_CACHE_CONFIG_H_
+    
+    namespace caffe {
     }
     
-      static void DoJob(extensions::ExtensionService* service, std::string extension_id);
- protected:
-  ~NwAppQuitFunction() override;
+      virtual inline const char* type() const { return 'Bias'; }
+  virtual inline int MinBottomBlobs() const { return 1; }
+  virtual inline int MaxBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
     
-    class NwClipboardClearSyncFunction : public NWSyncExtensionFunction {
+      virtual inline const char* type() const { return 'Convolution'; }
+    
+      Blob<int> offsets;
+  Blob<int> src_strides_;
+  Blob<int> dest_strides_;
+    
+    #ifdef USE_CUDNN
+/*
+ * @brief cuDNN implementation of DeConvolutionLayer.
+ *        Fallback to DeConvolutionLayer for CPU mode.
+ *
+ * cuDNN accelerates deconvolution through forward kernels for filtering and
+ * bias plus backward kernels for the gradient w.r.t. the filters, biases, and
+ * inputs. Caffe + cuDNN further speeds up the computation through forward
+ * parallelism across groups and backward parallelism across gradients.
+*/
+template <typename Dtype>
+class CuDNNDeconvolutionLayer : public DeconvolutionLayer<Dtype> {
  public:
-  NwClipboardClearSyncFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
+  explicit CuDNNDeconvolutionLayer(const LayerParameter& param)
+    : DeconvolutionLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+                          const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+                       const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNDeconvolutionLayer();
     }
     
-    #include <vector>
+    #ifdef USE_CUDNN
+template <typename Dtype>
+class CuDNNLCNLayer : public LRNLayer<Dtype> {
+ public:
+  explicit CuDNNLCNLayer(const LayerParameter& param)
+      : LRNLayer<Dtype>(param), handles_setup_(false), tempDataSize(0),
+        tempData1(NULL), tempData2(NULL) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNLCNLayer();
+    }
     
-        // following code is modified from `DesktopCaptureChooseDesktopMediaFunctionBase::OnPickerDialogResults`
-    // in chrome/browser/extensions/api/desktop_capture/desktop_capture_base.cc
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-        protected:
-      ~NwScreenGetScreensFunction() override {}
-      DECLARE_EXTENSION_FUNCTION('nw.Screen.getScreens', UNKNOWN)
+    /* TessWinding */
+#define GLU_TESS_WINDING_ODD               100130
+#define GLU_TESS_WINDING_NONZERO           100131
+#define GLU_TESS_WINDING_POSITIVE          100132
+#define GLU_TESS_WINDING_NEGATIVE          100133
+#define GLU_TESS_WINDING_ABS_GEQ_TWO       100134
     
-    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
-  for (unsigned int i = 0; i < args_.size(); i++) {
-    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
-      return args_[i].value.string;
+    
+    {    // put the inner array into the outer array
+    writePSFmt('{0:d} array 1 index {1:d} 2 index put\n',
+	       innerSize, outer);
+    line = col = 0;
+    writePS((char *)(useASCIIHex ? 'dup 0 <' : 'dup 0 <~'));
+    for (;;) {
+      do {
+	c = str->getChar();
+      } while (c == '\n' || c == '\r');
+      if (c == (useASCIIHex ? '>' : '~') || c == EOF) {
+	break;
+      }
+      if (c == 'z') {
+	writePSChar(c);
+	++col;
+      } else {
+	writePSChar(c);
+	++col;
+	for (i = 1; i <= (useASCIIHex ? 1 : 4); ++i) {
+	  do {
+	    c = str->getChar();
+	  } while (c == '\n' || c == '\r');
+	  if (c == (useASCIIHex ? '>' : '~') || c == EOF) {
+	    break;
+	  }
+	  writePSChar(c);
+	  ++col;
+	}
+      }
+      if (c == (useASCIIHex ? '>' : '~') || c == EOF) {
+	break;
+      }
+      // each line is: 'dup nnnnn <~...data...~> put<eol>'
+      // so max data length = 255 - 20 = 235
+      // chunks are 1 or 4 bytes each, so we have to stop at 232
+      // but make it 225 just to be safe
+      if (col > 225) {
+	writePS((char *)(useASCIIHex ? '> put\n' : '~> put\n'));
+	++line;
+	if (line >= innerSize) break;
+	writePSFmt((char *)(useASCIIHex ? 'dup {0:d} <' : 'dup {0:d} <~'), line);
+	col = 0;
+      }
+    }
+    if (c == (useASCIIHex ? '>' : '~') || c == EOF) {
+      writePS((char *)(useASCIIHex ? '> put\n' : '~> put\n'));
+      if (useRLE) {
+	++line;
+	writePSFmt('{0:d} <> put\n', line);
+      } else {
+	writePS('pop\n');
+      }
+      break;
+    }
+    writePS('pop\n');
+    size -= innerSize;
+  }
+  writePS('pop\n');
+  str->close();
+    
+      // skip leading whitespace and comments
+  comment = gFalse;
+  while (1) {
+    if ((c = getChar()) == EOF) {
+      buf[0] = '\0';
+      *length = 0;
+      return gFalse;
+    }
+    if (comment) {
+      if (c == '\x0a' || c == '\x0d') {
+	comment = gFalse;
+      }
+    } else if (c == '%') {
+      comment = gTrue;
+    } else if (specialChars[c] != 1) {
+      break;
     }
   }
-  return '';
-}
     
-    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
+      // Get direction
+  PageTransitionDirection getDirection() { return direction; }
     
-    namespace grpc {
+      XRef *xref;			// the xref table for this PDF file
+  Lexer *lexer;			// input stream
+  GBool allowStreams;		// parse stream objects?
+  Object buf1, buf2;		// next two tokens
+  int inlineImg;		// set when inline image data is encountered
+    
+    #include <config.h>
+    
+      //
+  // Parse media clip data
+  //
+  if (obj->dictLookup('C', &tmp2)->isDict()) { // media clip
+    hasClip = gTrue;
+    if (tmp2.dictLookup('S', &tmp)->isName()) {
+      if (!strcmp(tmp.getName(), 'MCD')) { // media clip data
+        Object obj1, obj2;
+	if (tmp2.dictLookup('D', &obj1)->isDict()) {
+	  if (obj1.dictLookup('F', &obj2)->isString()) {
+	    fileName = obj2.getString()->copy();
+	  }
+	  obj2.free();
+	  if (obj1.dictLookup('EF', &obj2)->isDict()) {
+	    Object embedded;
+	    if (obj2.dictLookup('F', &embedded)->isStream()) {
+	      isEmbedded = gTrue;
+	      embeddedStream = embedded.getStream();
+	      // 'copy' stream
+	      embeddedStream->incRef();
+	    }
+	    embedded.free();
+	  }
+	  obj2.free();
+    }
+    }
+    }
     }
     
-    #include <utility>
+    #include <config.h>
     
-    // From a non-empty container, returns a pointer to a random element.
-template <typename C>
-const typename C::value_type* RandomElement(const C& container) {
-  GPR_ASSERT(!container.empty());
-  auto it = container.begin();
-  std::advance(it, std::rand() % container.size());
-  return &(*it);
+    DHTReplaceNodeTask::DHTReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
+                                       const std::shared_ptr<DHTNode>& newNode)
+    : bucket_(bucket),
+      newNode_(newNode),
+      numRetry_(0),
+      timeout_(DHT_MESSAGE_TIMEOUT)
+{
 }
     
+    public:
+  DHTRoutingTable(const std::shared_ptr<DHTNode>& localNode);
     
-    {    std::cout << null << '\n';
-    std::cout << *res2.first << ' ' << std::boolalpha << res2.second << '\n';
+      Time serializedTime_;
+    
+    std::shared_ptr<DHTTask>
+DHTTaskFactoryImpl::createNodeLookupTask(const unsigned char* targetID)
+{
+  auto task = std::make_shared<DHTNodeLookupTask>(targetID);
+  setCommonProperty(task);
+  return task;
 }
