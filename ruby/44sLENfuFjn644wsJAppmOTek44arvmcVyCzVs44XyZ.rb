2096539cfcac56e793f79c1906e78e907c26cac2
SUNsung <weatherius@gmail.com>
2019-06-12 04:38:53 +0300

    Rewrite [44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb] File in: 2019-06-12 04:38:53

--- ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
+++ ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
@@ -1,170 +1,127 @@
 
         
-            # This returns whether the guest is ready to work. If this returns
-    # `false`, then {#detect!} should be called in order to detect the
-    # guest OS.
-    #
-    # @return [Boolean]
-    def ready?
-      !!capability_host_chain
+                  def datetime_selector(options, html_options)
+            datetime = options.fetch(:selected) { value || default_datetime(options) }
+            @auto_index ||= nil
+    
+        # for test and sidekiq
+    def async=(val)
+      @async = val
+    end
+    
+          if period_type == :all
+        DB.exec <<~SQL
+          UPDATE user_stats s
+          SET likes_given         = d.likes_given,
+              likes_received      = d.likes_received,
+              topic_count         = d.topic_count,
+              post_count          = d.post_count
+    
+        @omniauth.before_request_phase do |env|
+      # If the user is trying to reconnect to an existing account, store in session
+      request = ActionDispatch::Request.new(env)
+      request.session[:auth_reconnect] = !!request.params['reconnect']
+    end
+  end
+    
+        # remove old drafts
+    delete_drafts_older_than_n_days = SiteSetting.delete_drafts_older_than_n_days.days.ago
+    Draft.where('updated_at < ?', delete_drafts_older_than_n_days).destroy_all
+  end
+    
+        Extension.theme_extensions(theme_ids).each { |extension| builder << extension }
+    Extension.plugin_extensions.each { |extension| builder << extension }
+    builder << Extension.site_setting_extension
+    
+          extensions
+    end
+    
+      it 'can be extended by plugins' do
+    plugin = Class.new(Plugin::Instance) do
+      attr_accessor :enabled
+      def enabled?
+        @enabled
+      end
+    end.new(nil, '#{Rails.root}/spec/fixtures/plugins/csp_extension/plugin.rb')
+    
+      def search
+    RailsMultisite::ConnectionManagement.with_hostname(params[:hostname]) do
+    
+        cookies.delete('authentication_data')
+    cookies.delete(TOKEN_COOKIE)
+  end
+    
+        change.down do
+      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
+      Mention.where(mentions_container_type: 'Comment').destroy_all
+      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
     end
   end
 end
 
     
-            # Called after the configuration is finalized and loaded to validate
-        # this object.
-        #
-        # @param [Environment] env Vagrant::Environment object of the
-        #   environment that this configuration has been loaded into. This
-        #   gives you convenient access to things like the the root path
-        #   and so on.
-        # @param [ErrorRecorder] errors
-        def validate(env, errors)
-        end
-      end
+    describe ConversationsController, :type => :controller do
+  describe '#index' do
+    before do
+      @person = alice.contacts.first.person
+      hash = {
+        :author => @person,
+        :participant_ids => [alice.person.id, @person.id],
+        :subject => 'not spam',
+        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
+      }
+      @conv1 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+    
+      describe '#new' do
+    before do
+      sign_in alice, scope: :user
+    end
+    
+          it_behaves_like 'on a visible post'
+    end
+    
+        it 'redirects to /stream for a mobile user' do
+      request.headers['X_MOBILE_DEVICE'] = true
+      post :create, params: {user: {remember_me: '0', username: @user.username, password: 'evankorth'}}
+      expect(response).to be_redirect
+      expect(response.location).to match /^#{stream_url}\??$/
     end
   end
-end
+    
+      gem 'bacon'
+  gem 'mocha'
+  gem 'mocha-on-bacon'
+  gem 'prettybacon'
+  gem 'webmock'
+    
+        pod 'ObjCPod', path: 'ObjCPod'
+    pod 'SwiftPod', path: 'SwiftPod'
+    pod 'MixedPod', path: 'MixedPod'
+    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
+    
+    require_relative '../lib/bootstrap/environment'
+    
+          explicit_plugins_specs.each do |spec|
+        packet_gem.add(spec.name)
+      end
+    
+        puts('Packaging plugins for offline usage')
+    
+      it 'does object equality on config_hash and pipeline_id' do
+    another_exact_pipeline = described_class.new(source, pipeline_id, ordered_config_parts, settings)
+    expect(subject).to eq(another_exact_pipeline)
+    
+      not_found do
+    send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), {:status => 404})
+  end
+    
+    module Jekyll
+    
+    Liquid::Template.register_tag('include_code', Jekyll::IncludeCodeTag)
 
     
-            # This is an internal initialize function that should never be
-        # overridden. It is used to initialize some common internal state
-        # that is used in a provider.
-        def _initialize(name, machine)
-          initialize_capabilities!(
-            name.to_sym,
-            { name.to_sym => [Class.new, nil] },
-            Vagrant.plugin('2').manager.provider_capabilities,
-            machine,
-          )
-        end
-      end
-    end
-  end
-end
-
-    
-        def load_imports
-      if options.show_tasks && Rake::Task.task_defined?('load:defaults')
-        invoke 'load:defaults'
-        set(:stage, '')
-        Dir[deploy_config_path].each { |f| add_import f }
-      end
-    
-          private
-    
-    set_if_empty :format, :airbrussh
-set_if_empty :log_level, :debug
-    
-    @@ chat
-<pre id='chat'></pre>
-<form>
-  <input id='msg' placeholder='type message here...' />
-</form>
-    
-          <div id='post'>
-        <h3 id='post-info'>POST</h3>
-        <% if req.POST and not req.POST.empty? %>
-          <table class='req'>
-            <tr>
-              <th>Variable</th>
-              <th>Value</th>
-            </tr>
-            <% req.POST.sort_by { |k, v| k.to_s }.each { |key, val| %>
-            <tr>
-              <td><%=h key %></td>
-              <td class='code'><div><%=h val.inspect %></div></td>
-            </tr>
-            <% } %>
-          </table>
-        <% else %>
-          <p class='no-data'>No POST data.</p>
-        <% end %>
-        <div class='clear'></div>
-      </div> <!-- /POST -->
-    <% end %>
-    
-          # Creates a masked version of the authenticity token that varies
-      # on each request. The masking is used to mitigate SSL attacks
-      # like BREACH.
-      def mask_token(token)
-        token = decode_token(token)
-        one_time_pad = SecureRandom.random_bytes(token.length)
-        encrypted_token = xor_byte_strings(one_time_pad, token)
-        masked_token = one_time_pad + encrypted_token
-        encode_token(masked_token)
-      end
-    
-            reaction
-      end
-    
-        it 'Reads referrer from Host header when Referer header is relative' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
-      expect(subject.referrer(env)).to eq('foo.com')
-    end
-    
-      context 'with redirect reaction' do
-    before(:each) do
-      mock_app do
-        use Rack::Protection::CookieTossing, :reaction => :redirect
-        run DummyApp
-      end
-    end
-    
-            it 'contains elements' do
-          within('.additional-info') do
-            expect(page).to have_content('complete')
-            expect(page).to have_content('spree')
-            expect(page).to have_content('backorder')
-            expect(page).to have_content('balance due')
-          end
-        end
-      end
-    end
-  end
-    
-              def render_order(result)
-            if result.success?
-              render_serialized_payload { serialized_current_order }
-            else
-              render_error_payload(result.error)
-            end
-          end
-    
-              def shipping_rates_service
-            Spree::Api::Dependencies.storefront_checkout_get_shipping_rates_service.constantize
-          end
-    
-      it 'calls Hooks.commands_from' do
-    expect(Tmuxinator::Hooks).to receive(:commands_from).
-      with(kind_of(Tmuxinator::Project), hook_name).once
-    project.send('hook_#{hook_name}')
-  end
-    
-    describe Tmuxinator::Pane do
-  let(:klass) { described_class }
-  let(:instance) { klass.new(index, project, window, *commands) }
-  # let(:index) { 'vim' }
-  # let(:project) { 0 }
-  # let(:tab) { nil }
-  # let(:commands) { nil }
-  let(:index) { 0 }
-  let(:project) { double }
-  let(:window) { double }
-  let(:commands) { ['vim', 'bash'] }
-    
-        context 'base index present' do
-      before do
-        allow(project).to receive_messages(get_base_index: '1')
-      end
-    
-      if base_index = options.fetch(:base_index) { 1 }
-    standard_options << 'base-index #{base_index}'
-  end
-    
-          def valid_standard_project?(name)
-        return false unless name
-        raise 'Project #{name} doesn't exist.' unless exists?(name: name)
-        true
-      end
+    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
