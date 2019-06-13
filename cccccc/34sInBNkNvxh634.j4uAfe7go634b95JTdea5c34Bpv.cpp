84db2931c270e0234001829289da7a766887452a
SUNsung <weatherius@gmail.com>
2019-06-13 23:01:43 +0300

    Rewrite [34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp] File in: 2019-06-13 23:01:43

--- cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
+++ cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
@@ -1,149 +1,197 @@
 
         
-            const QString &getAppName() const { return appName; }
-    const QIcon &getAppIcon() const { return appIcon; }
-    const QIcon &getTrayAndWindowIcon() const { return trayAndWindowIcon; }
-    const QString &getTitleAddText() const { return titleAddText; }
-    
-        secp256k1_pubkey_load(ctx, &pt, point);
-    secp256k1_scalar_set_b32(&s, scalar, &overflow);
-    if (overflow || secp256k1_scalar_is_zero(&s)) {
-        ret = 0;
+          case psModePS:
+    // rotate, translate, and scale page
+    imgWidth = imgURX - imgLLX;
+    imgHeight = imgURY - imgLLY;
+    x1 = (int)floor(state->getX1());
+    y1 = (int)floor(state->getY1());
+    x2 = (int)ceil(state->getX2());
+    y2 = (int)ceil(state->getY2());
+    width = x2 - x1;
+    height = y2 - y1;
+    tx = ty = 0;
+    // rotation and portrait/landscape mode
+    if (rotate0 >= 0) {
+      rotate = (360 - rotate0) % 360;
+      landscape = gFalse;
     } else {
-        unsigned char x[32];
-        unsigned char y[1];
-        secp256k1_sha256_t sha;
+      rotate = (360 - state->getRotate()) % 360;
+      if (rotate == 0 || rotate == 180) {
+	if (width > height && width > imgWidth) {
+	  rotate += 90;
+	  landscape = gTrue;
+	} else {
+	  landscape = gFalse;
+	}
+      } else { // rotate == 90 || rotate == 270
+	if (height > width && height > imgWidth) {
+	  rotate = 270 - rotate;
+	  landscape = gTrue;
+	} else {
+	  landscape = gFalse;
+	}
+      }
+    }
+    writePSFmt('%%PageOrientation: {0:s}\n',
+	       landscape ? 'Landscape' : 'Portrait');
+    writePS('pdfStartPage\n');
+    if (rotate == 0) {
+      imgWidth2 = imgWidth;
+      imgHeight2 = imgHeight;
+    } else if (rotate == 90) {
+      writePS('90 rotate\n');
+      ty = -imgWidth;
+      imgWidth2 = imgHeight;
+      imgHeight2 = imgWidth;
+    } else if (rotate == 180) {
+      writePS('180 rotate\n');
+      imgWidth2 = imgWidth;
+      imgHeight2 = imgHeight;
+      tx = -imgWidth;
+      ty = -imgHeight;
+    } else { // rotate == 270
+      writePS('270 rotate\n');
+      tx = -imgHeight;
+      imgWidth2 = imgHeight;
+      imgHeight2 = imgWidth;
+    }
+    // shrink or expand
+    if (xScale0 > 0 && yScale0 > 0) {
+      xScale = xScale0;
+      yScale = yScale0;
+    } else if ((globalParams->getPSShrinkLarger() &&
+	 (width > imgWidth2 || height > imgHeight2)) ||
+	(globalParams->getPSExpandSmaller() &&
+	 (width < imgWidth2 && height < imgHeight2))) {
+      xScale = (double)imgWidth2 / (double)width;
+      yScale = (double)imgHeight2 / (double)height;
+      if (yScale < xScale) {
+	xScale = yScale;
+      } else {
+	yScale = xScale;
+      }
+    } else {
+      xScale = yScale = 1;
+    }
+    // deal with odd bounding boxes or clipping
+    if (clipLLX0 < clipURX0 && clipLLY0 < clipURY0) {
+      tx -= xScale * clipLLX0;
+      ty -= yScale * clipLLY0;
+    } else {
+      tx -= xScale * x1;
+      ty -= yScale * y1;
+    }
+    // center
+    if (tx0 >= 0 && ty0 >= 0) {
+      tx += rotate == 0 ? tx0 : ty0;
+      ty += rotate == 0 ? ty0 : -tx0;
+    } else if (globalParams->getPSCenter()) {
+      if (clipLLX0 < clipURX0 && clipLLY0 < clipURY0) {
+	tx += (imgWidth2 - xScale * (clipURX0 - clipLLX0)) / 2;
+	ty += (imgHeight2 - yScale * (clipURY0 - clipLLY0)) / 2;
+      } else {
+	tx += (imgWidth2 - xScale * width) / 2;
+	ty += (imgHeight2 - yScale * height) / 2;
+      }
+    }
+    tx += rotate == 0 ? imgLLX : imgLLY;
+    ty += rotate == 0 ? imgLLY : -imgLLX;
+    if (tx != 0 || ty != 0) {
+      writePSFmt('{0:.6g} {1:.6g} translate\n', tx, ty);
+    }
+    if (xScale != 1 || yScale != 1) {
+      writePSFmt('{0:.6f} {1:.6f} scale\n', xScale, yScale);
+    }
+    if (clipLLX0 < clipURX0 && clipLLY0 < clipURY0) {
+      writePSFmt('{0:.6g} {1:.6g} {2:.6g} {3:.6g} re W\n',
+		 clipLLX0, clipLLY0, clipURX0 - clipLLX0, clipURY0 - clipLLY0);
+    } else {
+      writePSFmt('{0:d} {1:d} {2:d} {3:d} re W\n', x1, y1, x2 - x1, y2 - y1);
     }
     
-        BOOST_CHECK(obj.pushKVs(obj2));
+    //========================================================================
+//
+// Modified under the Poppler project - http://poppler.freedesktop.org
+//
+// All changes made under the Poppler project to this file are licensed
+// under GPL version 2 or later
+//
+// Copyright (C) 2006 Scott Turner <scotty1024@mac.com>
+//
+// To see a description of the changes please see the Changelog file that
+// came with your tarball or type make ChangeLog if you are building from git
+//
+//========================================================================
     
-    
-    {    CRIPEMD160();
-    CRIPEMD160& Write(const unsigned char* data, size_t len);
-    void Finalize(unsigned char hash[OUTPUT_SIZE]);
-    CRIPEMD160& Reset();
-};
-    
-    ROTATE_ARGS
-	movdqa	XTMP3, XTMP2	; XTMP3 = W[-2] {BBAA}
-    mov	y0, e		; y0 = e
-    mov	y1, a		; y1 = a
-    ror	y0, (25-11)	; y0 = e >> (25-11)
-	movdqa	XTMP4, XTMP2	; XTMP4 = W[-2] {BBAA}
-    xor	y0, e		; y0 = e ^ (e >> (25-11))
-    ror	y1, (22-13)	; y1 = a >> (22-13)
-    mov	y2, f		; y2 = f
-    xor	y1, a		; y1 = a ^ (a >> (22-13)
-    ror	y0, (11-6)	; y0 = (e >> (11-6)) ^ (e >> (25-6))
-	psrlq	XTMP2, 17	; XTMP2 = W[-2] ror 17 {xBxA}
-    xor	y2, g		; y2 = f^g
-	psrlq	XTMP3, 19	; XTMP3 = W[-2] ror 19 {xBxA}
-    xor	y0, e		; y0 = e ^ (e >> (11-6)) ^ (e >> (25-6))
-    and	y2, e		; y2 = (f^g)&e
-	psrld	XTMP4, 10	; XTMP4 = W[-2] >> 10 {BBAA}
-    ror	y1, (13-2)	; y1 = (a >> (13-2)) ^ (a >> (22-2))
-    xor	y1, a		; y1 = a ^ (a >> (13-2)) ^ (a >> (22-2))
-    xor	y2, g		; y2 = CH = ((f^g)&e)^g
-    ror	y0, 6		; y0 = S1 = (e>>6) & (e>>11) ^ (e>>25)
-	pxor	XTMP2, XTMP3
-    add	y2, y0		; y2 = S1 + CH
-    ror	y1, 2		; y1 = S0 = (a>>2) ^ (a>>13) ^ (a>>22)
-    add	y2, [rsp + _XFER + 2*4]	; y2 = k + w + S1 + CH
-	pxor	XTMP4, XTMP2	; XTMP4 = s1 {xBxA}
-    mov	y0, a		; y0 = a
-    add	h, y2		; h = h + S1 + CH + k + w
-    mov	y2, a		; y2 = a
-	pshufb	XTMP4, SHUF_00BA	; XTMP4 = s1 {00BA}
-    or	y0, c		; y0 = a|c
-    add	d, h		; d = d + h + S1 + CH + k + w
-    and	y2, c		; y2 = a&c
-	paddd	XTMP0, XTMP4	; XTMP0 = {..., ..., W[1], W[0]}
-    and	y0, b		; y0 = (a|c)&b
-    add	h, y1		; h = h + S1 + CH + k + w + S0
-	;; compute high s1
-	pshufd	XTMP2, XTMP0, 01010000b	; XTMP2 = W[-2] {DDCC}
-    or	y0, y2		; y0 = MAJ = (a|c)&b)|(a&c)
-    add	h, y0		; h = h + S1 + CH + k + w + S0 + MAJ
-    
-      iter->SeekToFirst();
-  ASSERT_EQ(IterStatus(iter), 'a->va');
-  iter->Next();
-  ASSERT_EQ(IterStatus(iter), 'b->vb');
-  iter->Next();
-  ASSERT_EQ(IterStatus(iter), 'c->vc');
-  iter->Next();
-  ASSERT_EQ(IterStatus(iter), '(invalid)');
-  iter->SeekToFirst();
-  ASSERT_EQ(IterStatus(iter), 'a->va');
-  iter->Prev();
-  ASSERT_EQ(IterStatus(iter), '(invalid)');
-    
-    TegraUnaryOp_Invoker(bitwiseNot, bitwiseNot)
-#define TEGRA_UNARYOP(type, op, src1, sz1, dst, sz, w, h) \
-( \
-    CAROTENE_NS::isSupportedConfiguration() ? \
-    parallel_for_(Range(0, h), \
-    TegraGenOp_##op##_Invoker<const type, type>(src1, sz1, dst, sz, w, h), \
-    (w * h) / static_cast<double>(1<<16)), \
-    CV_HAL_ERROR_OK \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
-    
-    #ifdef CAROTENE_NEON
-    // in this case we can use the following scheme:
-    // dst[p] = (src[p] + dst[p]) >> 1
-    // which is faster
-    if (alpha == 0.5f)
-    {
-        internal::vtransform(size,
-                             srcBase, srcStride,
-                             dstBase, dstStride,
-                             dstBase, dstStride,
-                             AccumulateWeightedHalf());
+    GBool PageAttrs::readBox(Dict *dict, char *key, PDFRectangle *box) {
+  PDFRectangle tmp;
+  double t;
+  Object obj1, obj2;
+  GBool ok;
     }
     
-    void add(const Size2D &size,
-         const u8 * src0Base, ptrdiff_t src0Stride,
-         const u8 * src1Base, ptrdiff_t src1Stride,
-         s16 *dstBase, ptrdiff_t dstStride,
-         CONVERT_POLICY)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-    }
+      // Get the next object from the input stream.
+  Object *getObj(Object *obj, Guchar *fileKey = NULL,
+		 CryptAlgorithm encAlgorithm = cryptRC4, int keyLength = 0,
+		 int objNum = 0, int objGen = 0);
+  
+  Object *getObj(Object *obj, Guchar *fileKey,
+     CryptAlgorithm encAlgorithm, int keyLength,
+     int objNum, int objGen, std::set<int> *fetchOriginatorNums);
     
-    #ifdef CAROTENE_NEON
+    private:
     
-    #define SPLIT4ALPHA(sgn,bits) void split4(const Size2D &size,                                        \
-                                          const sgn##bits * srcBase, ptrdiff_t srcStride,            \
-                                          sgn##bits * dst3Base, ptrdiff_t dst3Stride,                \
-                                          sgn##bits * dst1Base, ptrdiff_t dst1Stride)                \
-{                                                                                                    \
-    internal::assertSupportedConfiguration();                                                        \
-    (void)size;                                                                                      \
-    (void)srcBase;                                                                                   \
-    (void)srcStride;                                                                                 \
-    (void)dst3Base;                                                                                  \
-    (void)dst3Stride;                                                                                \
-    (void)dst1Base;                                                                                  \
-    (void)dst1Stride;                                                                                \
-}
+      // Check the document's encryption.  If the document is encrypted,
+  // this will first try <ownerPassword> and <userPassword> (in
+  // 'batch' mode), and if those fail, it will attempt to request a
+  // password from the user.  This is the high-level function that
+  // calls the lower level functions for the specific security handler
+  // (requesting a password three times, etc.).  Returns true if the
+  // document can be opened (if it's unencrypted, or if a correct
+  // password is obtained); false otherwise (encrypted and no correct
+  // password).
+  GBool checkEncryption(GooString *ownerPassword,
+			GooString *userPassword);
     
-             vline_f32 = vld1q_f32(_src + i + 4);
+      fileName = NULL;
+  samplingRate = 0.0;
+  channels = 1;
+  bitsPerSample = 8;
+  encoding = soundRaw;
     
-    #include 'common.hpp'
-#include 'saturate_cast.hpp'
-#include 'separable_filter.hpp'
+      // Get the Splash object.
+  Splash *getSplash() { return splash; }
     
-    inline uint8x8_t vqtbl1_u8 (uint8x16_t a, uint8x8_t b)
-{
-#ifdef __aarch64__
-    // AArch64 supports this natively
-    return ::vqtbl1_u8(a, b);
-#else
-    union { uint8x16_t v; uint8x8x2_t w; } u = { a };
-    return vtbl2_u8(u.w, b);
-#endif
+    #ifndef STDINCACHELOADER_H
+#define STDINCACHELOADER_H
+    
+        Rational result{ prat };
+    destroyrat(prat);
+    
+            if (m_bInv
+            && ((wParam == IDC_CHOP) || (wParam == IDC_SIN) || (wParam == IDC_COS) || (wParam == IDC_TAN) || (wParam == IDC_LN) || (wParam == IDC_DMS)
+                || (wParam == IDC_DEGREES) || (wParam == IDC_SINH) || (wParam == IDC_COSH) || (wParam == IDC_TANH)))
+        {
+            m_bInv = false;
+        }
+    
+            std::wstring value;
+    
+    #ifndef MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
+#define MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
+#include <dmlc/parameter.h>
+#include <string>
+#include <utility>
+#include <vector>
+#include 'mxnet/ndarray.h'
+    
+    // C callback that can be used by TVM to extract
+// the WrapAsyncCall function.
+extern 'C' MXNET_DLL int MXTVMBridge(TVMFunctionHandle pregister) {
+  using tvm::runtime::PackedFunc;
+  const PackedFunc& fregister =
+      *static_cast<PackedFunc*>(pregister);
+  fregister('WrapAsyncCall', PackedFunc(mxnet::WrapAsyncCall));
+  return 0;
 }
