025d9f2bd0b038118461a32ff7a9017d8bf3437c
SUNsung <weatherius@gmail.com>
2019-06-13 14:49:50 +0300

    Rewrite [00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp] File in: 2019-06-13 14:49:50

--- cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
+++ cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
@@ -1,197 +1,126 @@
 
         
-        namespace nwapi {
+        #define IPC_MESSAGE_START ShellMsgStart
+    
+    
+bool MenuDelegate::GetAcceleratorForCommandId(
+      int command_id,
+      ui::Accelerator* accelerator) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
     }
     
-    Base::Base(int id,
-           const base::WeakPtr<ObjectManager>& object_manager,
-           const base::DictionaryValue& option,
-	   const std::string& extension_id)
-    : extension_id_(extension_id),
-      id_(id),
-      delay_destruction_(false),
-      pending_destruction_(false),
-      object_manager_(object_manager) {
-}
+       bool IsCommandIdChecked(int command_id) const override;
+   bool IsCommandIdEnabled(int command_id) const override;
     
-    v8::Handle<v8::Value> CallObjectMethod(int routing_id,
-                                       int object_id,
-                                       const std::string& type,
-                                       const std::string& method,
-                                       v8::Handle<v8::Value> args) {
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  scoped_ptr<V8ValueConverter> converter(V8ValueConverter::create());
-    }
+    #include 'base/logging.h'
+#include 'base/strings/string_util.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/nw/src/api/menu/menu.h'
     
+    #include 'base/files/file_path.h'
+#include 'base/strings/string_util.h'
+#include 'base/strings/utf_string_conversions.h'
+#include 'base/threading/thread_restrictions.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/nw/src/api/menu/menu.h'
+#include 'content/nw/src/nw_base.h'
+#include 'content/nw/src/nw_content.h'
+#include 'content/nw/src/nw_package.h'
+#include 'ui/base/accelerators/accelerator.h'
+#include 'ui/gfx/image/image_skia_operations.h'
+#include 'ui/events/event_constants.h'//for modifier key code
+#include 'base/logging.h'
     
-    {  *icon = item->icon_;
-  return true;
-}
-    
-    void MenuItem::OnClick(GtkWidget* widget) {
-  if (block_active_)
-    return;
-    }
-    
-        bool WriteImage(ClipboardData& data) {
-      DCHECK(data.type == TYPE_PNG || data.type == TYPE_JPEG);
-      std::string content = *(data.data);
-    }
-    
-    
-    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.callObjectMethod', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwObjCallObjectMethodFunction);
-};
-    
-          rect = gfx_display.work_area();
-      DisplayGeometry& work_area = displayResult->work_area;
-      work_area.x = rect.x();
-      work_area.y = rect.y();
-      work_area.width = rect.width();
-      work_area.height = rect.height();
-    
-      // implement nw.Screen.initEventListeners()
-  class NwScreenInitEventListenersFunction: public NWSyncExtensionFunction {
-    public:
-      NwScreenInitEventListenersFunction();
-      bool RunNWSync(base::ListValue* response, std::string* error) override;
+    namespace extensions {
     }
     
     
-    {			totalEncodingTime += image.GetEncodingTimeMs();
-			}
-    
-    bool PointsAreClose(IntPoint pt1, IntPoint pt2, double distSqrd)
-{
-    double Dx = (double)pt1.X - pt2.X;
-    double dy = (double)pt1.Y - pt2.Y;
-    return ((Dx * Dx) + (dy * dy) <= distSqrd);
-}
-//------------------------------------------------------------------------------
-    
-    void opus_fft_neon(const kiss_fft_state *st,
-                   const kiss_fft_cpx *fin,
-                   kiss_fft_cpx *fout);
-    
-    /** Compile-time conversion of float constant to 16-bit value */
-#define QCONST16(x,bits) ((opus_val16)(.5+(x)*(((opus_val32)1)<<(bits))))
-    
-    /*The number of bits to output at a time.*/
-# define EC_SYM_BITS   (8)
-/*The total number of bits in each of the state registers.*/
-# define EC_CODE_BITS  (32)
-/*The maximum symbol value.*/
-# define EC_SYM_MAX    ((1U<<EC_SYM_BITS)-1)
-/*Bits to shift by to move a symbol into the high-order position.*/
-# define EC_CODE_SHIFT (EC_CODE_BITS-EC_SYM_BITS-1)
-/*Carry bit of the high-order range symbol.*/
-# define EC_CODE_TOP   (((opus_uint32)1U)<<(EC_CODE_BITS-1))
-/*Low-order bit of the high-order range symbol.*/
-# define EC_CODE_BOT   (EC_CODE_TOP>>EC_SYM_BITS)
-/*The number of bits available for the last, partial symbol in the code field.*/
-# define EC_CODE_EXTRA ((EC_CODE_BITS-2)%EC_SYM_BITS+1)
+    {} // namespace extensions
 #endif
 
     
-    #include 'leveldb/export.h'
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.destroy', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwObjDestroyFunction);
+};
+    
+        std::unique_ptr<base::ListValue> args = nwapi::nw__screen::OnSourceAdded::Create(
+      src.id.ToString(),
+      base::UTF16ToUTF8(src.name),
+      index,
+      type,
+      src.id.type == content::DesktopMediaID::TYPE_SCREEN && GetPrimaryMonitorIndex() == index);
+    
+    TEST(MovableMessageTest, SelfMoveAssignment) {
+  // The `self` reference is necessary to defeat -Wself-move.
+  protobuf_unittest::TestAllTypes message, &self = message;
+  TestUtil::SetAllFields(&message);
+  message = std::move(self);
+  TestUtil::ExpectAllFieldsSet(message);
+}
+    
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
+    
+    int main(int argc, const char** argv) {
+  FileInputStream fin(STDIN_FILENO);
+  GzipInputStream in(&fin);
+    }
+    
+    class Proto3DataStripper : public DataStripper {
+ private:
+  virtual bool ShouldBeClear(const FieldDescriptor *field) {
+    return field->type() == FieldDescriptor::TYPE_GROUP ||
+           field->is_extension();
+  }
+};
+    
+    #include 'google/protobuf/message.h'
+#include 'google/protobuf/descriptor.h'
+#include 'google/protobuf/descriptor.pb.h'
+    
+    namespace HPHP {
+    }
     
     
+    {///////////////////////////////////////////////////////////////////////////////
+}
+
+    
+    #ifndef incl_HPHP_OUTPUT_FILE_H_
+#define incl_HPHP_OUTPUT_FILE_H_
+    
+    
+    {  StructuredLogEntry cols;
+  fillCols(cols);
+  cols.setStr('event_name', event);
+  cols.setInt('priority', priority);
+  StructuredLog::log('hhvm_perf_warning', cols);
+}
+    
+        // change element at index 1 (second element) to 'second'
+    array.at(1) = 'second';
+    
+        // out_of_range.401
+    try
     {
+        // try to use a an invalid array index
+        json::const_reference ref = j.at('/array/4'_json_pointer);
+    }
+    catch (json::out_of_range& e)
     {
-    {      default: {
-        char buf[40];
-        snprintf(buf, sizeof(buf), 'unknown record type %u', record_type);
-        ReportCorruption(
-            (fragment.size() + (in_fragmented_record ? scratch->size() : 0)),
-            buf);
-        in_fragmented_record = false;
-        scratch->clear();
-        break;
-      }
+        std::cout << e.what() << '\n';
     }
-  }
-  return false;
-}
-    
-      // Delete the specified 'file' from the specified 'level'.
-  void DeleteFile(int level, uint64_t file) {
-    deleted_files_.insert(std::make_pair(level, file));
-  }
-    
-    
-    {    if (diff_index >= min_length) {
-      // Do not shorten if one string is a prefix of the other
-    } else {
-      uint8_t diff_byte = static_cast<uint8_t>((*start)[diff_index]);
-      if (diff_byte < static_cast<uint8_t>(0xff) &&
-          diff_byte + 1 < static_cast<uint8_t>(limit[diff_index])) {
-        (*start)[diff_index]++;
-        start->resize(diff_index + 1);
-        assert(Compare(*start, limit) < 0);
-      }
-    }
-  }
-    
-    static Status DoWriteStringToFile(Env* env, const Slice& data,
-                                  const std::string& fname, bool should_sync) {
-  WritableFile* file;
-  Status s = env->NewWritableFile(fname, &file);
-  if (!s.ok()) {
-    return s;
-  }
-  s = file->Append(data);
-  if (s.ok() && should_sync) {
-    s = file->Sync();
-  }
-  if (s.ok()) {
-    s = file->Close();
-  }
-  delete file;  // Will auto-close if we did not close above
-  if (!s.ok()) {
-    env->DeleteFile(fname);
-  }
-  return s;
-}
-    
-      typedef SkipList<const char*, KeyComparator> Table;
-    
-      // Return an iterator for the specified file number (the corresponding
-  // file length must be exactly 'file_size' bytes).  If 'tableptr' is
-  // non-null, also sets '*tableptr' to point to the Table object
-  // underlying the returned iterator, or to nullptr if no Table object
-  // underlies the returned iterator.  The returned '*tableptr' object is owned
-  // by the cache and should not be deleted, and is valid for as long as the
-  // returned iterator is live.
-  Iterator* NewIterator(const ReadOptions& options, uint64_t file_number,
-                        uint64_t file_size, Table** tableptr = nullptr);
-    
-    #include 'leveldb/export.h'
-#include 'leveldb/iterator.h'
-    
-        peerAnnounceStorage->setTaskQueue(taskQueue.get());
-    peerAnnounceStorage->setTaskFactory(taskFactory.get());
-    
-    namespace {
-const size_t NUM_CONCURRENT_TASK = 15;
-} // namespace
-    
-    bool DHTTokenTracker::validateToken(const std::string& token,
-                                    const unsigned char* infoHash,
-                                    const std::string& ipaddr,
-                                    uint16_t port) const
-{
-  for (auto& elem : secret_) {
-    if (generateToken(infoHash, ipaddr, port, elem) == token) {
-      return true;
-    }
-  }
-  return false;
-}
-    
-    bool DHTUnknownMessage::isReply() const { return false; }
-    
-    #include <string>
-#include <set>
-#include <algorithm>
-#include <vector>
