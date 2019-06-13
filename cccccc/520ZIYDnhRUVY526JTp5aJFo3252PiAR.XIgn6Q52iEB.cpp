e1747769bd7843e9e55c3ef0bbc19e7e89ec9e8b
SUNsung <weatherius@gmail.com>
2019-06-13 10:43:38 +0300

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2019-06-13 10:43:37

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,393 +1,133 @@
 
         
-        namespace atom {
+                for (; j < roiw16; j += 16)
+        {
+            internal::prefetch(src + j);
+            internal::prefetch(dst + j);
+            uint8x16_t v_src = vld1q_u8(src + j);
+            int16x8_t v_dst0 = vld1q_s16(dst + j), v_dst1 = vld1q_s16(dst + j + 8);
+            int16x8_t v_src0 = vreinterpretq_s16_u16(vmovl_u8(vget_low_u8(v_src)));
+            int16x8_t v_src1 = vreinterpretq_s16_u16(vmovl_u8(vget_high_u8(v_src)));
     }
     
-    
-    {}  // namespace atom
-
-    
-     private:
-  atom::RenderProcessPreferences preferences_;
-    
-     private:
-  ~AtomQuotaPermissionContext() override;
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(URLRequestAboutJob);
-};
-    
-    #include 'base/files/file_util.h'
-#include 'base/logging.h'
-#include 'base/mac/mac_logging.h'
-#include 'base/posix/eintr_wrapper.h'
-#include 'base/process/launch.h'
-#include 'base/strings/sys_string_conversions.h'
-    
-    #include 'base/process/launch.h'
-#include 'base/strings/stringprintf.h'
-#include 'base/win/scoped_handle.h'
-#include 'sandbox/win/src/nt_internals.h'
-#include 'sandbox/win/src/win_utils.h'
-#include 'ui/base/win/shell.h'
-    
-    #define GTEST_TEST_NO_FATAL_FAILURE_(statement, fail) \
-  GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
-  if (::testing::internal::AlwaysTrue()) { \
-    ::testing::internal::HasNewFatalFailureHelper gtest_fatal_failure_checker; \
-    GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
-    if (gtest_fatal_failure_checker.has_new_fatal_failure()) { \
-      goto GTEST_CONCAT_TOKEN_(gtest_label_testnofatal_, __LINE__); \
-    } \
-  } else \
-    GTEST_CONCAT_TOKEN_(gtest_label_testnofatal_, __LINE__): \
-      fail('Expected: ' #statement ' doesn't generate new fatal ' \
-           'failures in the current thread.\n' \
-           '  Actual: it does.')
-    
-      void depart() {
-    if (link_.depart()) delete value_;
-  }
-    
-    // AddRef<T>::type is T if T is a reference; otherwise it's T&.  This
-// is the same as tr1::add_reference<T>::type.
-template <typename T>
-struct AddRef { typedef T& type; };  // NOLINT
-template <typename T>
-struct AddRef<T&> { typedef T& type; };  // NOLINT
-    
-    #include <algorithm>
-    
-    
-    {    // Now, we have i <= n/i < n.
-    // If n is divisible by i, n is not prime.
-    if (n % i == 0) return false;
-  }
-    
-        E* element = new E(old_head->element());
-    delete old_head;
-    
-      const dmlc::RowBlock<IndexType>& Value() const override {
-    return out_;
-  }
-    
-    /*! \brief try to do efficient pruning */
-template<typename DType, typename RType>
-struct WXQSummary : public WQSummary<DType, RType> {
-  // redefine entry type
-  using Entry = typename WQSummary<DType, RType>::Entry;
-  // constructor
-  WXQSummary(Entry *data, size_t size)
-      : WQSummary<DType, RType>(data, size) {}
-  // check if the block is large chunk
-  inline static bool CheckLarge(const Entry &e, RType chunk) {
-    return  e.RMinNext() > e.RMaxPrev() + chunk;
-  }
-  // set prune
-  inline void SetPrune(const WQSummary<DType, RType> &src, size_t maxsize) {
-    if (src.size <= maxsize) {
-      this->CopyFrom(src); return;
-    }
-    RType begin = src.data[0].rmax;
-    // n is number of points exclude the min/max points
-    size_t n = maxsize - 2, nbig = 0;
-    // these is the range of data exclude the min/max point
-    RType range = src.data[src.size - 1].rmin - begin;
-    // prune off zero weights
-    if (range == 0.0f || maxsize <= 2) {
-      // special case, contain only two effective data pts
-      this->data[0] = src.data[0];
-      this->data[1] = src.data[src.size - 1];
-      this->size = 2;
-      return;
-    } else {
-      range = std::max(range, static_cast<RType>(1e-3f));
-    }
-    // Get a big enough chunk size, bigger than range / n
-    // (multiply by 2 is a safe factor)
-    const RType chunk = 2 * range / n;
-    // minimized range
-    RType mrange = 0;
+        void operator() (const typename VecTraits<u32>::vec64 & v_src0,
+                     const typename VecTraits<u32>::vec64 & v_src1,
+                     typename VecTraits<u32>::vec64 & v_dst) const
     {
-      // first scan, grab all the big chunk
-      // moving block index, exclude the two ends.
-      size_t bid = 0;
-      for (size_t i = 1; i < src.size - 1; ++i) {
-        // detect big chunk data point in the middle
-        // always save these data points.
-        if (CheckLarge(src.data[i], chunk)) {
-          if (bid != i - 1) {
-            // accumulate the range of the rest points
-            mrange += src.data[i].RMaxPrev() - src.data[bid].RMinNext();
-          }
-          bid = i; ++nbig;
-        }
-      }
-      if (bid != src.size - 2) {
-        mrange += src.data[src.size-1].RMaxPrev() - src.data[bid].RMinNext();
-      }
+        float32x2_t vs1 = vcvt_f32_u32(v_src0);
+        float32x2_t vs2 = vcvt_f32_u32(v_src1);
     }
-    // assert: there cannot be more than n big data points
-    if (nbig >= n) {
-      // see what was the case
-      LOG(INFO) << ' check quantile stats, nbig=' << nbig << ', n=' << n;
-      LOG(INFO) << ' srcsize=' << src.size << ', maxsize=' << maxsize
-                << ', range=' << range << ', chunk=' << chunk;
-      src.Print();
-      CHECK(nbig < n) << 'quantile: too many large chunk';
+    
+    
+    {        if (!m[-1])         CANNY_PUSH(m - 1);
+        if (!m[1])          CANNY_PUSH(m + 1);
+        if (!m[-mapstep-1]) CANNY_PUSH(m - mapstep - 1);
+        if (!m[-mapstep])   CANNY_PUSH(m - mapstep);
+        if (!m[-mapstep+1]) CANNY_PUSH(m - mapstep + 1);
+        if (!m[mapstep-1])  CANNY_PUSH(m + mapstep - 1);
+        if (!m[mapstep])    CANNY_PUSH(m + mapstep);
+        if (!m[mapstep+1])  CANNY_PUSH(m + mapstep + 1);
     }
-    this->data[0] = src.data[0];
-    this->size = 1;
-    // The counter on the rest of points, to be selected equally from small chunks.
-    n = n - nbig;
-    // find the rest of point
-    size_t bid = 0, k = 1, lastidx = 0;
-    for (size_t end = 1; end < src.size; ++end) {
-      if (end == src.size - 1 || CheckLarge(src.data[end], chunk)) {
-        if (bid != end - 1) {
-          size_t i = bid;
-          RType maxdx2 = src.data[end].RMaxPrev() * 2;
-          for (; k < n; ++k) {
-            RType dx2 =  2 * ((k * mrange) / n + begin);
-            if (dx2 >= maxdx2) break;
-            while (i < end &&
-                   dx2 >= src.data[i + 1].rmax + src.data[i + 1].rmin) ++i;
-            if (i == end) break;
-            if (dx2 < src.data[i].RMinNext() + src.data[i + 1].RMaxPrev()) {
-              if (i != lastidx) {
-                this->data[this->size++] = src.data[i]; lastidx = i;
-              }
-            } else {
-              if (i + 1 != lastidx) {
-                this->data[this->size++] = src.data[i + 1]; lastidx = i + 1;
-              }
-            }
-          }
-        }
-        if (lastidx != end) {
-          this->data[this->size++] = src.data[end];
-          lastidx = end;
-        }
-        bid = end;
-        // shift base by the gap
-        begin += src.data[bid].RMinNext() - src.data[bid].RMaxPrev();
-      }
+    
+    
+    {    void operator() (const T * src0, const T * src1, u8 * dst) const
+    {
+        dst[0] = src0[0] > src1[0] ? 255 : 0;
     }
-  }
-};
-/*!
- * \brief traditional GK summary
- */
-template<typename DType, typename RType>
-struct GKSummary {
-  /*! \brief an entry in the sketch summary */
-  struct Entry {
-    /*! \brief minimum rank */
-    RType rmin;
-    /*! \brief maximum rank */
-    RType rmax;
-    /*! \brief the value of data */
-    DType value;
-    // constructor
-    Entry() = default;
-    // constructor
-    Entry(RType rmin, RType rmax, DType value)
-        : rmin(rmin), rmax(rmax), value(value) {}
-  };
-  /*! \brief input data queue before entering the summary */
-  struct Queue {
-    // the input queue
-    std::vector<DType> queue;
-    // end of the queue
-    size_t qtail;
-    // push data to the queue
-    inline void Push(DType x, RType w) {
-      queue[qtail++] = x;
-    }
-    inline void MakeSummary(GKSummary *out) {
-      std::sort(queue.begin(), queue.begin() + qtail);
-      out->size = qtail;
-      for (size_t i = 0; i < qtail; ++i) {
-        out->data[i] = Entry(i + 1, i + 1, queue[i]);
-      }
-    }
-  };
-  /*! \brief data field */
-  Entry *data;
-  /*! \brief number of elements in the summary */
-  size_t size;
-  GKSummary(Entry *data, size_t size)
-      : data(data), size(size) {}
-  /*! \brief the maximum error of the summary */
-  inline RType MaxError() const {
-    RType res = 0;
-    for (size_t i = 1; i < size; ++i) {
-      res = std::max(data[i].rmax - data[i-1].rmin, res);
-    }
-    return res;
-  }
-  /*! \return maximum rank in the summary */
-  inline RType MaxRank() const {
-    return data[size - 1].rmax;
-  }
-  /*!
-   * \brief copy content from src
-   * \param src source sketch
-   */
-  inline void CopyFrom(const GKSummary &src) {
-    size = src.size;
-    std::memcpy(data, src.data, sizeof(Entry) * size);
-  }
-  inline void CheckValid(RType eps) const {
-    // assume always valid
-  }
-  /*! \brief used for debug purpose, print the summary */
-  inline void Print() const {
-    for (size_t i = 0; i < size; ++i) {
-      LOG(CONSOLE) << 'x=' << data[i].value << '\t'
-                   << '[' << data[i].rmin << ',' << data[i].rmax << ']';
-    }
-  }
-  /*!
-   * \brief set current summary to be pruned summary of src
-   *        assume data field is already allocated to be at least maxsize
-   * \param src source summary
-   * \param maxsize size we can afford in the pruned sketch
-   */
-  inline void SetPrune(const GKSummary &src, size_t maxsize) {
-    if (src.size <= maxsize) {
-      this->CopyFrom(src); return;
-    }
-    const RType max_rank = src.MaxRank();
-    this->size = maxsize;
-    data[0] = src.data[0];
-    size_t n = maxsize - 1;
-    RType top = 1;
-    for (size_t i = 1; i < n; ++i) {
-      RType k = (i * max_rank) / n;
-      while (k > src.data[top + 1].rmax) ++top;
-      // assert src.data[top].rmin <= k
-      // because k > src.data[top].rmax >= src.data[top].rmin
-      if ((k - src.data[top].rmin) < (src.data[top+1].rmax - k)) {
-        data[i] = src.data[top];
-      } else {
-        data[i] = src.data[top + 1];
-      }
-    }
-    data[n] = src.data[src.size - 1];
-  }
-  inline void SetCombine(const GKSummary &sa,
-                         const GKSummary &sb) {
-    if (sa.size == 0) {
-      this->CopyFrom(sb); return;
-    }
-    if (sb.size == 0) {
-      this->CopyFrom(sa); return;
-    }
-    CHECK(sa.size > 0 && sb.size > 0) << 'invalid input for merge';
-    const Entry *a = sa.data, *a_end = sa.data + sa.size;
-    const Entry *b = sb.data, *b_end = sb.data + sb.size;
-    this->size = sa.size + sb.size;
-    RType aprev_rmin = 0, bprev_rmin = 0;
-    Entry *dst = this->data;
-    while (a != a_end && b != b_end) {
-      if (a->value < b->value) {
-        *dst = Entry(bprev_rmin + a->rmin,
-                     a->rmax + b->rmax - 1, a->value);
-        aprev_rmin = a->rmin;
-        ++dst; ++a;
-      } else {
-        *dst = Entry(aprev_rmin + b->rmin,
-                     b->rmax + a->rmax - 1, b->value);
-        bprev_rmin = b->rmin;
-        ++dst; ++b;
-      }
-    }
-    if (a != a_end) {
-      RType bprev_rmax = (b_end - 1)->rmax;
-      do {
-        *dst = Entry(bprev_rmin + a->rmin, bprev_rmax + a->rmax, a->value);
-        ++dst; ++a;
-      } while (a != a_end);
-    }
-    if (b != b_end) {
-      RType aprev_rmax = (a_end - 1)->rmax;
-      do {
-        *dst = Entry(aprev_rmin + b->rmin, aprev_rmax + b->rmax, b->value);
-        ++dst; ++b;
-      } while (b != b_end);
-    }
-    CHECK(dst == data + size) << 'bug in combine';
-  }
 };
     
-      /*! \return the default evaluation metric for the objective */
-  virtual const char* DefaultEvalMetric() const = 0;
-  // the following functions are optional, most of time default implementation is good enough
-  /*!
-   * \brief transform prediction values, this is only called when Prediction is called
-   * \param io_preds prediction values, saves to this vector as well
-   */
-  virtual void PredTransform(HostDeviceVector<bst_float> *io_preds) {}
-    
-      void Write(const SparsePage& page, dmlc::Stream* fo) override {
-    const auto& offset_vec = page.offset.HostVector();
-    const auto& data_vec = page.data.HostVector();
-    CHECK(offset_vec.size() != 0 && offset_vec[0] == 0);
-    CHECK_EQ(offset_vec.back(), data_vec.size());
-    fo->Write(offset_vec);
-    min_index_ = page.base_rowid;
-    fo->Write(&min_index_, sizeof(min_index_));
-    index_.data.resize(data_vec.size());
-    value_.data.resize(data_vec.size());
+    void assertSupportedConfiguration(bool parametersSupported)
+{
+    if (!isSupportedConfiguration()) {
+        std::cerr << 'internal error: attempted to use an unavailable function' << std::endl;
+        std::abort();
+    }
     }
     
-      bool Read(SparsePage* page,
-            dmlc::SeekStream* fi,
-            const std::vector<bst_uint>& sorted_index_set) override {
-    if (!fi->Read(&disk_offset_)) return false;
-    auto& offset_vec = page->offset.HostVector();
-    auto& data_vec = page->data.HostVector();
-    // setup the offset
-    offset_vec.clear();
-    offset_vec.push_back(0);
-    for (unsigned int fid : sorted_index_set) {
-      CHECK_LT(fid + 1, disk_offset_.size());
-      size_t size = disk_offset_[fid + 1] - disk_offset_[fid];
-      offset_vec.push_back(offset_vec.back() + size);
+    #define CVT_FUNC(T1, T2, SIMD_SIZE, CVTINIT, CVTROW)                            \
+    void convert(const Size2D &,                                                \
+                 const T1 *, ptrdiff_t,                                         \
+                 T2 *, ptrdiff_t)                                               \
+    {                                                                           \
+        internal::assertSupportedConfiguration();                               \
     }
-    data_vec.resize(offset_vec.back());
-    // read in the data
-    size_t begin = fi->Tell();
-    size_t curr_offset = 0;
-    for (size_t i = 0; i < sorted_index_set.size();) {
-      bst_uint fid = sorted_index_set[i];
-      if (disk_offset_[fid] != curr_offset) {
-        CHECK_GT(disk_offset_[fid], curr_offset);
-        fi->Seek(begin + disk_offset_[fid] * sizeof(Entry));
-        curr_offset = disk_offset_[fid];
-      }
-      size_t j, size_to_read = 0;
-      for (j = i; j < sorted_index_set.size(); ++j) {
-        if (disk_offset_[sorted_index_set[j]] == disk_offset_[fid] + size_to_read) {
-          size_to_read += offset_vec[j + 1] - offset_vec[j];
-        } else {
-          break;
+    
+    
+    {            vst1q_s16(drow + x - 8, t0);
         }
-      }
+    
+    namespace leveldb {
+namespace log {
     }
     }
     
-      void PredictInteractionContributions(DMatrix* p_fmat, std::vector<bst_float>* out_contribs,
-                                       const gbm::GBTreeModel& model, unsigned ntree_limit,
-                                       bool approximate) override {
-    const MetaInfo& info = p_fmat->Info();
-    const int ngroup = model.param.num_output_group;
-    size_t ncolumns = model.param.num_feature;
-    const unsigned row_chunk = ngroup * (ncolumns + 1) * (ncolumns + 1);
-    const unsigned mrow_chunk = (ncolumns + 1) * (ncolumns + 1);
-    const unsigned crow_chunk = ngroup * (ncolumns + 1);
+    class SequentialFile;
+    
+      // Return the key for the current entry.  The underlying storage for
+  // the returned slice is valid only until the next modification of
+  // the iterator.
+  // REQUIRES: Valid()
+  virtual Slice key() const = 0;
+    
+    
+    {  // OK status has a null state_.  Otherwise, state_ is a new[] array
+  // of the following form:
+  //    state_[0..3] == length of message
+  //    state_[4]    == code
+  //    state_[5..]  == message
+  const char* state_;
+};
+    
+    // Some commands are not affecting the state machine state of the calc flow. But these are more of
+// some gui mode kind of settings (eg Inv button, or Deg,Rad , Back etc.). This list is getting bigger & bigger
+// so we abstract this as a separate routine. Note: There is another side to this. Some commands are not
+// gui mode setting to begin with, but once it is discovered it is invalid and we want to behave as though it
+// was never inout, we need to revert the state changes made as a result of this test
+bool IsGuiSettingOpCode(OpCode opCode)
+{
+    if (IsOpInRange(opCode, IDM_HEX, IDM_BIN) || IsOpInRange(opCode, IDM_QWORD, IDM_BYTE) || IsOpInRange(opCode, IDM_DEG, IDM_GRAD))
+    {
+        return true;
+    }
     }
     
-        left += 1;
-    right -= 15;
+    class CUnaryCommand final : public IUnaryCommand
+{
+public:
+    CUnaryCommand(int command);
+    CUnaryCommand(int command1, int command2);
+    const std::shared_ptr<CalculatorVector<int>>& GetCommands() const override;
+    CalculationManager::CommandType GetCommandType() const override;
+    void SetCommand(int command) override;
+    void SetCommands(int command1, int command2) override;
+    void Accept(_In_ ISerializeCommandVisitor& commandVisitor) override;
+    }
+    
+    class IBinaryCommand : public IOperatorCommand
+{
+public:
+    virtual void SetCommand(int command) override = 0;
+    virtual int GetCommand() const = 0;
+};
+    
+            DUPRAT(term, rat_one);
+        divrat(&term, *pn, precision);
+        subrat(&term, tmp, precision);
+    
+    
+    {protected:
+    std::string       _key;
+    float            _from, _to;
+    float            _delta;
+};
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+    
+    NS_CC_END
