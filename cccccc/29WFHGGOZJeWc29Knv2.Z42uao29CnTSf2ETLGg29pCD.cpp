220f255b61c6be1a5b91700b08abc33c03e5a011
SUNsung <weatherius@gmail.com>
2019-06-14 02:07:44 +0300

    Rewrite [29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp] File in: 2019-06-14 02:07:44

--- cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
+++ cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
@@ -1,235 +1,111 @@
 
         
-        
-    {private:
-    Ui::OpenURIDialog *ui;
-};
+        AboutProtocolHandler::~AboutProtocolHandler() {}
+    
+      // net::URLRequestJobFactory::ProtocolHandler:
+  net::URLRequestJob* MaybeCreateJob(
+      net::URLRequest* request,
+      net::NetworkDelegate* network_delegate) const override;
+  bool IsSafeRedirectTarget(const GURL& location) const override;
     
     
-    {}
+    {}  // namespace atom
     
-    void test_bad_scalar(void) {
-    unsigned char s_zero[32] = { 0 };
-    unsigned char s_overflow[32] = {
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe,
-        0xba, 0xae, 0xdc, 0xe6, 0xaf, 0x48, 0xa0, 0x3b,
-        0xbf, 0xd2, 0x5e, 0x8c, 0xd0, 0x36, 0x41, 0x41
-    };
-    unsigned char s_rand[32] = { 0 };
-    unsigned char output[32];
-    secp256k1_scalar rand;
-    secp256k1_pubkey point;
+    void DragFileItems(const std::vector<base::FilePath>& files,
+                   const gfx::Image& icon,
+                   gfx::NativeView view);
+    
+        for (int inputIdx = 0; inputIdx < def_.input_size() / kNumTensorsPerInput;
+         ++inputIdx) {
+      input_blob_names.push_back(I(inputIdx * kNumTensorsPerInput));
+      output_blob_names.push_back(GI(inputIdx * kNumTensorsPerInput + 2));
     }
+    input_blob_names.push_back(GO(2));
     
-        /* Check bad contexts and NULLs for recovery */
-    ecount = 0;
-    CHECK(secp256k1_ecdsa_recover(none, &recpubkey, &recsig, message) == 0);
-    CHECK(ecount == 1);
-    CHECK(secp256k1_ecdsa_recover(sign, &recpubkey, &recsig, message) == 0);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(vrfy, &recpubkey, &recsig, message) == 1);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, message) == 1);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(both, NULL, &recsig, message) == 0);
-    CHECK(ecount == 3);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, NULL, message) == 0);
-    CHECK(ecount == 4);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, NULL) == 0);
-    CHECK(ecount == 5);
-    
-    
-// Detect when a type is not a wchar_t string
-template<typename T> struct is_wchar { typedef int tinyformat_wchar_is_not_supported; };
-template<> struct is_wchar<wchar_t*> {};
-template<> struct is_wchar<const wchar_t*> {};
-template<int n> struct is_wchar<const wchar_t[n]> {};
-template<int n> struct is_wchar<wchar_t[n]> {};
-    
-        UniValue v3;
-    BOOST_CHECK(v3.setNumStr('32482348723847471234'));
-    BOOST_CHECK_THROW(v3.get_int64(), std::runtime_error);
-    BOOST_CHECK(v3.setNumStr('1000'));
-    BOOST_CHECK_EQUAL(v3.get_int64(), 1000);
-    
-    // Bech32 is a string encoding format used in newer address types.
-// The output consists of a human-readable part (alphanumeric), a
-// separator character (1), and a base32 data section, the last
-// 6 characters of which are a checksum.
-//
-// For more information, see BIP 173.
-    
-    /** A hasher class for RIPEMD-160. */
-class CRIPEMD160
-{
-private:
-    uint32_t s[5];
-    unsigned char buf[64];
-    uint64_t bytes;
-    }
-    
-    void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
-                                        std::string* dst) const {
-  // We rely on the fact that the code in table.cc does not mind us
-  // adjusting keys[].
-  Slice* mkey = const_cast<Slice*>(keys);
-  for (int i = 0; i < n; i++) {
-    mkey[i] = ExtractUserKey(keys[i]);
-    // TODO(sanjay): Suppress dups?
-  }
-  user_policy_->CreateFilter(keys, n, dst);
-}
-    
-      // Return an internal key (suitable for passing to an internal iterator)
-  Slice internal_key() const { return Slice(kstart_, end_ - kstart_); }
-    
-      dict->lookup('Size', &obj1);
-  if (obj1.isArray() && obj1.arrayGetLength() == 2) {
-    obj1.arrayGet(0, &obj2);
-    width = obj2.getNum();
-    obj2.free();
-    obj1.arrayGet(1, &obj2);
-    height = obj2.getNum();
-    obj2.free();
-    writePSFmt('%%ImageDimensions: {0:.6g} {1:.6g}\n', width, height);
-  }
-  obj1.free();
-    
-    
-    {  PDFRectangle mediaBox;
-  PDFRectangle cropBox;
-  GBool haveCropBox;
-  PDFRectangle bleedBox;
-  PDFRectangle trimBox;
-  PDFRectangle artBox;
-  int rotate;
-  Object lastModified;
-  Object boxColorInfo;
-  Object group;
-  Object metadata;
-  Object pieceInfo;
-  Object separationInfo;
-  Object resources;
+    template <typename T, class Context>
+class BernoulliJSDGradientOp final : public Operator<Context> {
+ public:
+  USE_SIMPLE_CTOR_DTOR(BernoulliJSDGradientOp);
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  bool RunOnDevice() override;
 };
     
+    // Given a MutableIterator to the start of a block, run DetectParagraphs on
+// that block and commit the results to the underlying ROW and BLOCK structs,
+// saving the ParagraphModels in models.  Caller owns the models.
+// We use unicharset during the function to answer questions such as 'is the
+// first letter of this word upper case?'
+void DetectParagraphs(int debug_level,
+                      bool after_text_recognition,
+                      const MutableIterator *block_start,
+                      GenericVector<ParagraphModel *> *models);
     
-    {  GooList intervals;
+    // Parses the given box file string into a page_number, utf8_str, and
+// bounding_box. Returns true on a successful parse.
+bool ParseBoxFileStr(const char* boxfile_str, int* page_number,
+                     STRING* utf8_str, TBOX* bounding_box);
+    
+    #include 'ocrpara.h'
+#include 'host.h'  // For NearlyEqual()
+    
+    
+    {  UnicharAmbigsVector dang_ambigs_;
+  UnicharAmbigsVector replace_ambigs_;
+  GenericVector<UnicharIdVector *> one_to_one_definite_ambigs_;
+  GenericVector<UnicharIdVector *> ambigs_for_adaption_;
+  GenericVector<UnicharIdVector *> reverse_ambigs_for_adaption_;
 };
+    
+    #endif  // THIRD_PARTY_TESSERACT_CCUTIL_DOUBLEPTR_H_
 
     
-      // skip over stream data
-  if (Lexer::LOOK_VALUE_NOT_CACHED != lexer->lookCharLastValueCached) {
-      // take into account the fact that we've cached one value
-      pos = pos - 1;
-      lexer->lookCharLastValueCached = Lexer::LOOK_VALUE_NOT_CACHED;
+      int operator==(const KDPair<Key, Data>& other) const {
+    return key == other.key;
   }
-  lexer->setPos(pos + length);
     
-    
-    {
-    {
-    {      for (int j = i; j > 0; j--) {
-        keys[j] = keys[j - 1];
-        items[j] = items[j - 1];
-      }
-      
-      keys[0] = keyHit;
-      items[0] = itemHit;
-      return itemHit;
+    Transliterator* RemoveTransliterator::clone(void) const {
+    Transliterator* result = new RemoveTransliterator();
+    if (result != NULL && getFilter() != 0) {
+        result->adoptFilter((UnicodeFilter*)(getFilter()->clone()));
     }
-  }
-  return 0;
+    return result;
 }
     
-    class PopplerObjectCache
+        RegexCompile     compiler(This, status);
+    compiler.compile(regex, pe, status);
+    
+    U_NAMESPACE_END
+    
+    U_NAMESPACE_END
+    
+        switch (ruleMode)
+    {
+    // if the mode is day-of-month, the day of month is given
+    case DOM_MODE:
+        ruleDayOfMonth = ruleDay;
+        break;
+    }
+    
+    int32_t SimpleDateFormat::matchQuarterString(const UnicodeString& text,
+                              int32_t start,
+                              UCalendarDateFields field,
+                              const UnicodeString* data,
+                              int32_t dataCount,
+                              Calendar& cal) const
 {
-  public:
-    PopplerObjectCache (int cacheSizeA, XRef *xrefA);
-    ~PopplerObjectCache();
+    int32_t i = 0;
+    int32_t count = dataCount;
     }
     
+    #include 'unicode/uniset.h'
+#include 'unicode/udat.h'
+#include 'cmemory.h'
+#include 'uassert.h'
+#include 'ucln_in.h'
+#include 'umutex.h'
     
-    {  void addElement (double elapsed);
-  int getCount () { return count; }
-  double getTotal () { return total; }
-  double getMin () { return max; }
-  double getMax () { return max; }
-private:
-  int count;			// size of <elems> array
-  double total;			// number of elements in array
-  double min;			// reference count
-  double max;			// reference count
-};
-    
-      GooString* getContentType() { return contentType; }
-  GooString* getFileName() { return fileName; }
-    
-    /**
- * @brief Intel HECI GUID for the update application.
- *
- * This GUID can be used on every OS over the HECI protocol to inspect the
- * FW version.
- */
-const std::vector<uint8_t> kMEIUpdateGUID{
-    232, 205, 157, 48, 177, 204, 98, 64, 143, 120, 96, 1, 21, 163, 67, 39,
-};
-    
-    namespace osquery {
-namespace table_tests {
+        int32_t length = fFlagAndLength;
+    if (length > getCapacity() && reallocate(length, 0) == NULL) {
+        setToBogus();
+        return;
     }
-    }
-    
-    
-    {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    TEST(ByteTest, SetGetHighLowBit) {
-  unsigned char byte_value = 0x37;
-  Byte value(&byte_value);
-  value.set_value_high_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
-  value.set_value_low_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
-}
-    
-    namespace apollo {
-namespace drivers {
-namespace conti_radar {
-    }
-    }
-    }
-    
-    void ObjectQualityInfo60C::Parse(const std::uint8_t* bytes, int32_t length,
-                                 ContiRadar* conti_radar) const {
-  int obj_id = object_id(bytes, length);
-    }
-    
-      MatrixXd mat_golden(10, 10);
-  // clang-format off
-  mat_golden <<
-     20, -15,   6,  -1,   0,   0,   0,   0,   0,   0,
-    -15,  20, -15,   6,  -1,   0,   0,   0,   0,   0,
-      6, -15,  20, -15,   6,  -1,   0,   0,   0,   0,
-     -1,   6, -15,  20, -15,   6,  -1,   0,   0,   0,
-      0,  -1,   6, -15,  20, -15,   6,  -1,   0,   0,
-      0,   0,  -1,   6, -15,  20, -15,   6,  -1,   0,
-      0,   0,   0,  -1,   6, -15,  20, -15,   6,  -1,
-      0,   0,   0,   0,  -1,   6, -15,  19, -12,   3,
-      0,   0,   0,   0,   0,  -1,   6, -12,  10,  -3,
-      0,   0,   0,   0,   0,   0,  -1,   3,  -3,   1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-    #include 'modules/canbus/vehicle/gem/gem_message_manager.h'
-    
-    
-    {  double ret = x * 0.001000;
-  return ret;
-}
