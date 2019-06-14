ede28a14593b53911aeab06e77d96c72c024097e
SUNsung <weatherius@gmail.com>
2019-06-14 13:26:11 +0300

    Rewrite [30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py] File in: 2019-06-14 13:26:10

--- pyton/30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py
+++ pyton/30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py
@@ -1,56 +1,84 @@
 
         
-        GRADE_COLOR_1 = COLOR_HEADS = RED_E
-GRADE_COLOR_2 = COLOR_TAILS = BLUE_C
+        
+class Session(AbstractBaseSession):
+    '''
+    Django provides full support for anonymous sessions. The session
+    framework lets you store and retrieve arbitrary data on a
+    per-site-visitor basis. It stores data on the server side and
+    abstracts the sending and receiving of cookies. Cookies contain a
+    session ID -- not the data itself.
     
-    class Dodecahedron(Mobject1D):
-    def generate_points(self):
-        phi = (1 + np.sqrt(5)) / 2
-        x = np.array([1, 0, 0])
-        y = np.array([0, 1, 0])
-        z = np.array([0, 0, 1])
-        v1, v2 = (phi, 1/phi, 0), (phi, -1/phi, 0)
-        vertex_pairs = [
-            (v1, v2),
-            (x+y+z, v1),
-            (x+y-z, v1),
-            (x-y+z, v2),
-            (x-y-z, v2),
-        ]
-        five_lines_points = Mobject(*[
-            Line(pair[0], pair[1], density = 1.0/self.epsilon)
-            for pair in vertex_pairs
-        ]).points
-        #Rotate those 5 edges into all 30.
-        for i in range(3):
-            perm = [j%3 for j in range(i, i+3)]
-            for b in [-1, 1]:
-                matrix = b*np.array([x[perm], y[perm], z[perm]])
-                self.add_points(np.dot(five_lines_points, matrix))
-        self.pose_at_angle()
-        self.set_color(GREEN)
+        :param client: AWS API client reference (boto3)
+    :param module: Ansible module reference
+    :return dict:
+    '''
     
-        def coords_to_center(self, n, k):
-        x_offset = self.cell_width * (k + self.nrows / 2.0 - n / 2.0)
-        y_offset = self.cell_height * (self.nrows - n)
-        return self.bottom_left + x_offset * RIGHT + y_offset * UP
+            :returns: `set` of unsaved files
+        '''
+        save_state = self.aug.get('/augeas/save')
+        self.aug.set('/augeas/save', 'noop')
+        # Existing Errors
+        ex_errs = self.aug.match('/augeas//error')
+        try:
+            # This is a noop save
+            self.aug.save()
+        except (RuntimeError, IOError):
+            self._log_save_errors(ex_errs)
+            # Erase Save Notes
+            self.save_notes = ''
+            raise errors.PluginError(
+                'Error saving files, check logs for more info.')
     
-        def add_braces_and_labels(self):
-        for attr in 'horizontal_parts', 'vertical_parts':
-            if not hasattr(self, attr):
-                continue
-            parts = getattr(self, attr)
-            for subattr in 'braces', 'labels':
-                if hasattr(parts, subattr):
-                    self.add(getattr(parts, subattr))
+        @certbot_util.patch_get_utility()
+    def test_noninteractive(self, mock_util):
+        mock_util().menu.side_effect = errors.MissingCommandlineFlag('no vhost default')
+        try:
+            self._call(self.vhosts)
+        except errors.MissingCommandlineFlag as e:
+            self.assertTrue('vhost ambiguity' in str(e))
+    
+            self.assertTrue(self.addr_default.conflicts(self.addr))
+        self.assertTrue(self.addr_default.conflicts(self.addr1))
+        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
     
     
-def random_bright_color():
-    color = random_color()
-    curr_rgb = color_to_rgb(color)
-    new_rgb = interpolate(
-        curr_rgb, np.ones(len(curr_rgb)), 0.5
-    )
-    return Color(rgb=new_rgb)
+def setup(app):
+    app.add_config_value('edit_on_github_project', '', True)
+    app.add_config_value('edit_on_github_branch', 'master', True)
+    app.add_config_value('edit_on_github_src_path', '', True)  # 'eg' 'docs/'
+    app.connect('html-page-context', html_page_context)
+
     
-            self.assertEqual(response.status_code, 405)
+        return result
+    
+    
+class GroupTagKeyNotFound(Exception):
+    pass
+    
+            # Removing unique constraint on 'GroupTagValue', fields ['project_id',
+        # 'group_id', 'environment_id', '_key', '_value']
+        db.delete_unique(
+            u'tagstore_grouptagvalue', [
+                'project_id', 'group_id', 'environment_id', 'key', 'value'])
+    
+            # Adding model 'GroupTagValue'
+        db.create_table(u'tagstore_grouptagvalue', (
+            ('_value', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['tagstore.TagValue'], db_column='value')),
+            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
+            ('environment_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)),
+            ('times_seen', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
+            ('_key', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['tagstore.TagKey'], db_column='key')),
+            ('first_seen', self.gf('django.db.models.fields.DateTimeField')(
+                null=True, db_index=True)),
+            ('group_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
+            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
+            ('last_seen', self.gf('django.db.models.fields.DateTimeField')(
+                null=True, db_index=True)),
+        ))
+        db.send_create_signal('tagstore', ['GroupTagValue'])
+    
+    from datetime import timedelta
+from django.utils import timezone
