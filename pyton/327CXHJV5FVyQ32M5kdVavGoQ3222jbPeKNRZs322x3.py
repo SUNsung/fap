1f94d283be289c78e7141970749e0fa77422203f
SUNsung <weatherius@gmail.com>
2019-06-14 18:40:53 +0300

    Rewrite [327CXHJV5FVyQ32M5kdVavGoQ3222jbPeKNRZs322x3.py] File in: 2019-06-14 18:40:52

--- pyton/327CXHJV5FVyQ32M5kdVavGoQ3222jbPeKNRZs322x3.py
+++ pyton/327CXHJV5FVyQ32M5kdVavGoQ3222jbPeKNRZs322x3.py
@@ -1,125 +1,277 @@
 
         
-        
-@pytest.fixture(autouse=True)
-def usage_tracker_io(usage_tracker):
-    io = StringIO()
-    usage_tracker.return_value \
-                 .open.return_value \
-                 .__enter__.return_value = io
-    return io
+              # Initialize the variables with the current batch data
+      batch_indices = list(
+          range(chunk, min(chunk + self.hparams.batch_size, len(test_pred))))
+    
+        # CONTROLLER -> GENERATOR -> RATES
+    # (u(t) -> gen(t) -> factors(t) -> rates(t) -> p(x_t|z_t) )
+    self.controller_outputs = u_t = [None] * num_steps
+    self.con_ics = con_state = None
+    self.con_states = con_states = [None] * num_steps
+    self.con_outs = con_outs = [None] * num_steps
+    self.gen_inputs = gen_inputs = [None] * num_steps
+    if co_dim > 0:
+      # gen_cell_class here for l2 penalty recurrent weights
+      # didn't split the cell_weight scale here, because I doubt it matters
+      con_cell = gen_cell_class(hps.con_dim,
+                                input_weight_scale=hps.cell_weight_scale,
+                                rec_weight_scale=hps.cell_weight_scale,
+                                clip_value=hps.cell_clip_value,
+                                recurrent_collections=['l2_con_reg'])
+      with tf.variable_scope('con', reuse=False):
+        self.con_ics = tf.tile(
+            tf.Variable(tf.zeros([1, hps.con_dim*con_cell.state_multiplier]),
+                        name='c0'),
+            tf.stack([batch_size, 1]))
+        self.con_ics.set_shape([None, con_cell.state_size]) # tile loses shape
+        con_states[-1] = self.con_ics
+    
+      rates, x0s, inputs = \
+      generate_data(rnn, T=T, E=E, x0s=x0s, P_sxn=P_sxn,
+                    input_magnitude=input_magnitude,
+                    input_times=input_times)
+    
+    flags = tf.app.flags
+flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
+                    'Directory for saving data.')
+flags.DEFINE_string('datafile_name', 'conditioned_rnn_data',
+                    'Name of data file for input case.')
+flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
+flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
+flags.DEFINE_integer('C', 400, 'Number of conditions')
+flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
+flags.DEFINE_float('train_percentage', 4.0/5.0,
+                   'Percentage of train vs validation trials')
+flags.DEFINE_integer('nreplications', 10,
+                     'Number of spikifications of the same underlying rates.')
+flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
+flags.DEFINE_float('x0_std', 1.0,
+                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
+flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
+flags.DEFINE_float('dt', 0.010, 'Time bin')
+flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
+FLAGS = flags.FLAGS
+    
+      samples = []
+  for sequence_id in xrange(min(len(arr), max_num_to_print)):
+    buffer_str = ' '.join(
+        [str(id_to_word[index]) for index in arr[sequence_id, :]])
+    samples.append(buffer_str)
+  return samples
     
     
-@pytest.mark.functional
-def test_with_confirmation(proc, TIMEOUT):
-    with_confirmation(proc, TIMEOUT)
+def create_dis_pretrain_op(hparams, dis_loss, global_step):
+  '''Create a train op for pretraining.'''
+  with tf.name_scope('pretrain_generator'):
+    optimizer = tf.train.AdamOptimizer(hparams.dis_pretrain_learning_rate)
+    dis_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('dis')
+    ]
+    if FLAGS.dis_update_share_embedding and FLAGS.dis_share_embedding:
+      shared_embedding = [
+          v for v in tf.trainable_variables()
+          if v.op.name == 'gen/decoder/rnn/embedding'
+      ][0]
+      dis_vars.append(shared_embedding)
+    dis_grads = tf.gradients(dis_loss, dis_vars)
+    dis_grads_clipped, _ = tf.clip_by_global_norm(dis_grads,
+                                                  FLAGS.grad_clipping)
+    dis_pretrain_op = optimizer.apply_gradients(
+        zip(dis_grads_clipped, dis_vars), global_step=global_step)
+    return dis_pretrain_op
+    
+    '''Model utilities.'''
     
     
-@pytest.mark.parametrize('command, packages', [
-    (Command('vim', 'vim: command not found'),
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('sudo vim', 'vim: command not found'),
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('vim', 'The program 'vim' is currently not installed. You can install it by typing: sudo apt install vim'),
-     [('vim', 'main'), ('vim-tiny', 'main')])])
-def test_match(mocker, command, packages):
-    mocker.patch('thefuck.rules.apt_get.which', return_value=None)
-    mocker.patch('thefuck.rules.apt_get._get_packages',
-                 create=True, return_value=packages)
+UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-apache-conf-digest.txt'
+'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
+    
+        def test_eq(self):
+        self.assertTrue(self.vhost1b == self.vhost1)
+        self.assertFalse(self.vhost1 == self.vhost2)
+        self.assertEqual(str(self.vhost1b), str(self.vhost1))
+        self.assertFalse(self.vhost1b == 1234)
+    
+    # If false, no module index is generated.
+#html_domain_indices = True
+    
+        # Invert the predicted soft masks
+    masks_inv = masks_hf[:, :, :, ::-1]
     
     
-@pytest.mark.parametrize('script, output', [
-    ('brew link sshfs', output),
-    ('cat output', output),
-    ('brew install sshfs', '')])
-def test_not_match(script, output):
-    command = Command(script, output)
-    assert not match(command)
-    
-    X -= X.mean(axis=0)
-X /= X.std(axis=0)
-    
-            start = time.time()
-        func(X, n_jobs=-1)
-        multi_core.append(time.time() - start)
-    
-    File: sparsity_benchmark.py
-Function: benchmark_dense_predict at line 51
-Total time: 0.532979 s
-    
-        This is called by sphinx.ext.linkcode
-    
-    # Plot the results (= shape of the data points cloud)
-plt.figure(1)  # two clusters
-plt.title('Outlier detection on a real data set (boston housing)')
-plt.scatter(X1[:, 0], X1[:, 1], color='black')
-bbox_args = dict(boxstyle='round', fc='0.8')
-arrow_args = dict(arrowstyle='->')
-plt.annotate('several confounded points', xy=(24, 19),
-             xycoords='data', textcoords='data',
-             xytext=(13, 10), bbox=bbox_args, arrowprops=arrow_args)
-plt.xlim((xx1.min(), xx1.max()))
-plt.ylim((yy1.min(), yy1.max()))
-plt.legend((legend1_values_list[0].collections[0],
-            legend1_values_list[1].collections[0],
-            legend1_values_list[2].collections[0]),
-           (legend1_keys_list[0], legend1_keys_list[1], legend1_keys_list[2]),
-           loc='upper center',
-           prop=matplotlib.font_manager.FontProperties(size=12))
-plt.ylabel('accessibility to radial highways')
-plt.xlabel('pupil-teacher ratio by town')
-    
-    The dataset is generated using the ``make_biclusters`` function, which
-creates a matrix of small values and implants bicluster with large
-values. The rows and columns are then shuffled and passed to the
-Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
-make biclusters contiguous shows how accurately the algorithm found
-the biclusters.
+def cityscapes_to_coco_without_person_rider(cityscapes_id):
+    lookup = {
+        0: 0,  # ... background
+        1: 2,  # bicycle
+        2: 3,  # car
+        3: -1,  # person (ignore)
+        4: 7,  # train
+        5: 8,  # truck
+        6: 4,  # motorcycle
+        7: 6,  # bus
+        8: -1,  # rider (ignore)
+    }
+    return lookup[cityscapes_id]
     
     
-#----------------------------------------------------------------------
-# Visualize the clustering
-def plot_clustering(X_red, labels, title=None):
-    x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
-    X_red = (X_red - x_min) / (x_max - x_min)
+def _whctrs(anchor):
+    '''Return width, height, x center, and y center for an anchor (window).'''
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[1] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
     
-    Dorin Comaniciu and Peter Meer, 'Mean Shift: A robust approach toward
-feature space analysis'. IEEE Transactions on Pattern Analysis and
-Machine Intelligence. 2002. pp. 603-619.
     
-        @classmethod
-    def marshal(cls, input_data):
-        return pickle.dumps(input_data)
+def mask_rcnn_frozen_features(model):
+    logger.warn('Deprecated: use `TRAIN.FREEZE_CONV_BODY: True` instead')
+    return build_generic_detection_model(
+        model,
+        get_func(cfg.MODEL.CONV_BODY),
+        add_roi_box_head_func=get_func(cfg.FAST_RCNN.ROI_BOX_HEAD),
+        add_roi_mask_head_func=get_func(cfg.MRCNN.ROI_MASK_HEAD),
+        freeze_conv_body=True
+    )
     
-        def draw_landmarks(self, color_id=3, radius=1):
-        ''' Draw the facial landmarks '''
-        color = self.colors[color_id]
-        for alignment in self.alignments:
-            landmarks = alignment['landmarksXY']
-            logger.trace('Drawing Landmarks: (landmarks: %s, color: %s, radius: %s)',
-                         landmarks, color, radius)
-            for (pos_x, pos_y) in landmarks:
-                cv2.circle(self.image,  # pylint: disable=no-member
-                           (pos_x, pos_y),
-                           radius,
-                           color,
-                           -1)
     
-        def __init__(self):
-        gpu_stats = GPUStats(log=False)
+def add_single_scale_rpn_outputs(model, blob_in, dim_in, spatial_scale):
+    '''Add RPN outputs to a single scale model (i.e., no FPN).'''
+    anchors = generate_anchors(
+        stride=1. / spatial_scale,
+        sizes=cfg.RPN.SIZES,
+        aspect_ratios=cfg.RPN.ASPECT_RATIOS
+    )
+    num_anchors = anchors.shape[0]
+    dim_out = dim_in
+    # RPN hidden representation
+    model.Conv(
+        blob_in,
+        'conv_rpn',
+        dim_in,
+        dim_out,
+        kernel=3,
+        pad=1,
+        stride=1,
+        weight_init=gauss_fill(0.01),
+        bias_init=const_fill(0.0)
+    )
+    model.Relu('conv_rpn', 'conv_rpn')
+    # Proposal classification scores
+    model.Conv(
+        'conv_rpn',
+        'rpn_cls_logits',
+        dim_in,
+        num_anchors,
+        kernel=1,
+        pad=0,
+        stride=1,
+        weight_init=gauss_fill(0.01),
+        bias_init=const_fill(0.0)
+    )
+    # Proposal bbox regression deltas
+    model.Conv(
+        'conv_rpn',
+        'rpn_bbox_pred',
+        dim_in,
+        4 * num_anchors,
+        kernel=1,
+        pad=0,
+        stride=1,
+        weight_init=gauss_fill(0.01),
+        bias_init=const_fill(0.0)
+    )
     
-            folder:     folder alignments file is stored in
-        filename:   Filename of alignments file excluding extension. If a
-                    valid extension is provided, then it will be used to
-                    decide the serializer, and the serializer argument will
-                    be ignored.
-        serializer: If provided, this will be the format that the data is
-                    saved in (if data is to be saved). Can be 'json', 'pickle'
-                    or 'yaml'
+    
+def _sample_rois(roidb, im_scale, batch_idx):
+    '''Generate a random sample of RoIs comprising foreground and background
+    examples.
     '''
-    # pylint: disable=too-many-public-methods
-    def __init__(self, folder, filename='alignments', serializer='json'):
-        logger.debug('Initializing %s: (folder: '%s', filename: '%s', serializer: '%s')',
-                     self.__class__.__name__, folder, filename, serializer)
-        self.serializer = self.get_serializer(filename, serializer)
-        self.file = self.get_location(folder, filename)
+    rois_per_image = int(cfg.TRAIN.BATCH_SIZE_PER_IM)
+    fg_rois_per_image = int(np.round(cfg.TRAIN.FG_FRACTION * rois_per_image))
+    max_overlaps = roidb['max_overlaps']
+    
+    from detectron.core.config import cfg
+import detectron.utils.blob as blob_utils
+import detectron.utils.boxes as box_utils
+import detectron.utils.segms as segm_utils
+    
+    
+def get_minibatch(roidb):
+    '''Given a roidb, construct a minibatch sampled from it.'''
+    # We collect blobs from each image onto a list and then concat them into a
+    # single tensor, hence we initialize each blob to an empty list
+    blobs = {k: [] for k in get_minibatch_blob_names()}
+    # Get the input image blob, formatted for caffe2
+    im_blob, im_scales = _get_image_blob(roidb)
+    blobs['data'] = im_blob
+    if cfg.RPN.RPN_ON:
+        # RPN-only or end-to-end Faster/Mask R-CNN
+        valid = rpn_roi_data.add_rpn_blobs(blobs, im_scales, roidb)
+    elif cfg.RETINANET.RETINANET_ON:
+        im_width, im_height = im_blob.shape[3], im_blob.shape[2]
+        # im_width, im_height corresponds to the network input: padded image
+        # (if needed) width and height. We pass it as input and slice the data
+        # accordingly so that we don't need to use SampleAsOp
+        valid = retinanet_roi_data.add_retinanet_blobs(
+            blobs, im_scales, roidb, im_width, im_height
+        )
+    else:
+        # Fast R-CNN like models trained on precomputed proposals
+        valid = fast_rcnn_roi_data.add_fast_rcnn_blobs(blobs, im_scales, roidb)
+    return blobs, valid
+    
+        def test_size_exceptions(self):
+        A = np.random.randn(2, 256, 42, 86).astype(np.float32)
+        I = np.array(np.random.permutation(10), dtype=np.int32)
+        with self.assertRaises(RuntimeError):
+            self._run_op_test(A, I)
+    
+    
+def ignore_sigpipe():
+    # Restore default behavior for SIGPIPE instead of raising
+    # an exception when encountered.
+    if not IS_WINDOWS_PLATFORM:
+        signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
+    
+    
+def get_service_name_from_network_mode(network_mode):
+    return get_source_name_from_network_mode(network_mode, 'service')
+    
+        def create_volume(self, name, driver=None, opts=None, external=None, custom_name=False):
+        if external:
+            custom_name = True
+            if isinstance(external, six.text_type):
+                name = external
+    
+        def test_single_path_from_env(self):
+        with mock.patch.dict(os.environ):
+            os.environ['COMPOSE_FILE'] = 'one.yml'
+            environment = Environment.from_env_file('.')
+            assert get_config_path_from_options('.', {}, environment) == ['one.yml']
+    
+    import unittest
+    
+    
+class VerboseProxyTestCase(unittest.TestCase):
+    
+        def test_sort_service_dicts_circular_imports(self):
+        services = [
+            {
+                'links': ['redis'],
+                'name': 'web'
+            },
+            {
+                'name': 'redis',
+                'links': ['web']
+            },
+        ]
+    
+        def test_parallel_execute(self):
+        results, errors = parallel_execute(
+            objects=[1, 2, 3, 4, 5],
+            func=lambda x: x * 2,
+            get_name=six.text_type,
+            msg='Doubling',
+        )
