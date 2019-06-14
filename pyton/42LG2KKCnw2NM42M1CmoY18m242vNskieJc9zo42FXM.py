8465a37f1501c25b5a1d97ca19118cbcfd608a4c
SUNsung <weatherius@gmail.com>
2019-06-14 14:08:03 +0300

    Rewrite [42LG2KKCnw2NM42M1CmoY18m242vNskieJc9zo42FXM.py] File in: 2019-06-14 14:08:03

--- pyton/42LG2KKCnw2NM42M1CmoY18m242vNskieJc9zo42FXM.py
+++ pyton/42LG2KKCnw2NM42M1CmoY18m242vNskieJc9zo42FXM.py
@@ -1,29 +1,59 @@
 
         
-        
-def check_alphabetical(lines):
-    '''
-    checks if all entries per section are in alphabetical order based in entry title
-    '''
-    sections = {}
-    section_line_num = {}
-    for line_num, line in enumerate(lines):
-        if line.startswith(anchor):
-            category = line.split(anchor)[1].strip()
-            sections[category] = []
-            section_line_num[category] = line_num
-            continue
-        if not line.startswith('|') or line.startswith('|---'):
-            continue
-        raw_title = [x.strip() for x in line.split('|')[1:-1]][0]
-        title_re_match = link_re.match(raw_title)
-        if title_re_match:
-            sections[category].append(title_re_match.group(1).upper())
+                else:
+            colision_resolution = self._colision_resolution(key, data)
+            if colision_resolution is not None:
+                self._set_value(colision_resolution, data)
+            else:
+                self.rehashing()
+                self.insert_data(data)
+    
+            if state == 'absent':
+            if exists:
+                conn.delete_cache_subnet_group(group_name)
+                changed = True
+        else:
+            if not exists:
+                new_group = conn.create_cache_subnet_group(group_name, cache_subnet_group_description=group_description, subnet_ids=group_subnets)
+                changed = True
+            else:
+                changed_group = conn.modify_cache_subnet_group(group_name, cache_subnet_group_description=group_description, subnet_ids=group_subnets)
+                changed = True
+    
+            elif desired_state == 'disabled':
+            if current_state == HOST_ABSENT:
+                self.fail(msg='absent host cannot be put in disabled state')
+            elif current_state in [HOST_STATES.MONITORED, HOST_STATES.OFFLINE]:
+                if one.host.status(host.ID, HOST_STATUS.DISABLED):
+                    self.wait_for_host_state(host, [HOST_STATES.DISABLED])
+                    result['changed'] = True
+                else:
+                    self.fail(msg='could not disable host')
+            elif current_state in [HOST_STATES.DISABLED]:
+                pass
+            else:
+                self.fail(msg='unknown host state %s, cowardly refusing to change state to disable' % current_state_name)
+    
+    # Ensure rule is absent
+- ipa_hbacrule:
+    name: rule_to_be_deleted
+    state: absent
+    ipa_host: ipa.example.com
+    ipa_user: admin
+    ipa_pass: topsecret
+'''
+    
+        # get the attributes
+    model_handle = model.get('mh')
     
     
-def get_setting_target(node):
-    # target nodes are placed next to the node in the doc tree
-    return node.parent[node.parent.index(node) + 1]
+# ===========================================
+# Module execution.
+#
+    
+    
+def collect_scrapy_settings_refs(app, doctree):
+    env = app.builder.env
     
     # Scrapy version
 import pkgutil
@@ -32,8 +62,90 @@ version_info = tuple(int(v) if v.isdigit() else v
                      for v in __version__.split('.'))
 del pkgutil
     
-        # default settings to be used for this command instead of global defaults
-    default_settings = {}
-    
         def syntax(self):
-        return '[options] <url>'
+        return '[options] <spider>'
+    
+        def run(self, args, opts):
+        if len(args) != 1 or not is_url(args[0]):
+            raise UsageError()
+        cb = lambda x: self._print_response(x, opts)
+        request = Request(args[0], callback=cb, dont_filter=True)
+        # by default, let the framework handle redirects,
+        # i.e. command handles all codes expect 3xx
+        if not opts.no_redirect:
+            request.meta['handle_httpstatus_list'] = SequenceExclude(range(300, 400))
+        else:
+            request.meta['handle_httpstatus_all'] = True
+    
+        def _list_templates(self):
+        print('Available templates:')
+        for filename in sorted(os.listdir(self.templates_dir)):
+            if filename.endswith('.tmpl'):
+                print('  %s' % splitext(filename)[0])
+    
+            self.crawler_process.crawl(spidercls, **opts.spargs)
+        self.crawler_process.start()
+    
+        def post_process(self, output):
+        for x in output:
+            if isinstance(x, (BaseItem, dict)):
+                for arg in self.args:
+                    if not arg in x:
+                        raise ContractFail(''%s' field is missing' % arg)
+
+    
+        def process_response(self, request, response, spider):
+        if request.meta.get('dont_merge_cookies', False):
+            return response
+    
+        def _is_tar(self, response):
+        archive = BytesIO(response.body)
+        try:
+            tar_file = tarfile.open(name=mktemp(), fileobj=archive)
+        except tarfile.ReadError:
+            return
+    
+    
+def KeywordsFromSyntaxListOutput_PhpSyntax_ContainsPreProc_test():
+  assert_that( syntax_parse._KeywordsFromSyntaxListOutput(
+                   ContentsOfTestFile( 'php_syntax' ) ),
+               has_items( 'skip', 'function' ) )
+    
+            print('None input')
+        assert_raises(TypeError, insertion_sort.sort, None)
+    
+            print('Two or more elements')
+        data = [5, 1, 7, 2, 6, -3, 5, 7, -1]
+        assert_equal(merge_sort.sort(data), sorted(data))
+    
+    
+def main():
+    test = TestQueueFromStacks()
+    test.test_queue_from_stacks()
+    
+    
+def main():
+    test = TestSortStack()
+    test.test_sort_stack(MyStack())
+    test.test_sort_stack(MyStackSimplified())
+    
+    
+class TestStack(object):
+    
+    		if current.leftChild is None and current.rightChild is None:
+			if current is self.root:
+				self.root = None
+			elif isLeft:
+				parent.leftChild = None
+			else:
+				parent.rightChild = None
+    
+    
+		print('Test: insert checking with pre order traversal')
+		expectVal = [50, 30, 10, 7, 25, 40, 38, 70, 60, 80]
+		assert_equal(myTree.printPreOrder(), expectVal)
+		expectVal = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91]
+		assert_equal(myTree2.printPreOrder(), expectVal)
+    
+        def __len__(self):
+        return len(self.array)
