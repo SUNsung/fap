25b9ea24813fe8267d6fd262a6fd81807a6a2385
SUNsung <weatherius@gmail.com>
2019-06-14 20:35:10 +0300

    Rewrite [182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb] File in: 2019-06-14 20:35:10

--- ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
+++ ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
@@ -1,127 +1,147 @@
 
         
-          </body>
-</html>
-HTML
-    
-        def process(args)
-      arg_is_present? args, '--server', 'The --server command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
-                          use the 'build' subcommand.'
-      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
-                          '--watch'.'
-      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
-                          the '--watch' switch.'
-      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
-                          favour of 'highlighter'.'
-      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
-                          your config files.'
-      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
-                          config files.'
-      no_subcommand(args)
-    end
-    
-          attr_accessor :page, :layout, :content, :paginator
-      attr_accessor :highlighter_prefix, :highlighter_suffix
-    
-      private
-    
-        render_empty
+          def self.increment!(type, opts = nil)
+    perform_increment!(redis_key(type), opts)
   end
     
-      def load_export
-    @export = Export.new(current_account)
+          RailsMultisite::ConnectionManagement.with_connection(db) do
+        begin
+          warning_job = @reactor.queue(@timeout) do
+            Rails.logger.error ''#{desc}' is still running after #{@timeout} seconds on db #{db}, this process may need to be restarted!'
+          end if !non_block
+          job.call
+        rescue => ex
+          Discourse.handle_job_exception(ex, message: 'Running deferred code '#{desc}'')
+        ensure
+          warning_job&.cancel
+        end
+      end
+    rescue => ex
+      Discourse.handle_job_exception(ex, message: 'Processing deferred code queue')
+    ensure
+      ActiveRecord::Base.connection_handler.clear_active_connections!
+    end
   end
     
-          def run
-        UI.puts report
+        it 'rejects invalid directives and ones that are not allowed to be extended' do
+      builder << {
+        invalid_src: ['invalid'],
+      }
+    
+        # if you need to test this and are having ssl issues see:
+    #  http://stackoverflow.com/questions/6756460/openssl-error-using-omniauth-specified-ssl-path-but-didnt-work
+    # OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
+    @omniauth = OmniAuth::Builder.new(app) do
+      Discourse.authenticators.each do |authenticator|
+        authenticator.register_middleware(self)
       end
-    
-    Then(/^references in the remote repo are listed$/) do
-  expect(@output).to include('refs/heads/master')
-end
-    
-        def dry_run
-      ['--dry-run', '-n',
-       'Do a dry run without executing actions',
-       lambda do |_value|
-         Configuration.env.set(:sshkit_backend, SSHKit::Backend::Printer)
-       end]
     end
     
-            @response = (gets || '').chomp
+      def build(theme_ids)
+    builder = Builder.new
+    
+          if !current_user
+        @env[BAD_TOKEN] = true
+        begin
+          limiter.performed!
+        rescue RateLimiter::LimitExceeded
+          raise Discourse::InvalidAccess.new(
+            'Invalid Access',
+            nil,
+            delete_cookie: TOKEN_COOKIE
+          )
+        end
       end
+    elsif @env['HTTP_DISCOURSE_LOGGED_IN']
+      @env[BAD_TOKEN] = true
+    end
     
-          private
+      before_destroy do
+    UserAuthToken.log(action: 'destroy',
+                      user_auth_token_id: self.id,
+                      user_id: self.user_id,
+                      user_agent: self.user_agent,
+                      client_ip: self.client_ip,
+                      auth_token: self.auth_token)
+  end
     
-            def delta(first, second, alignment = :left)
-          case alignment
-          when :left
-            first.column - second.column
-          when :right
-            first.last_column - second.last_column
-          else
-            0
+            # This is called as a last-minute hook that allows the configuration
+        # object to finalize itself before it will be put into use. This is
+        # a useful place to do some defaults in the case the user didn't
+        # configure something or so on.
+        #
+        # An example of where this sort of thing is used or has been used:
+        # the 'vm' configuration key uses this to make sure that at least
+        # one sub-VM has been defined: the default VM.
+        #
+        # The configuration object is expected to mutate itself.
+        def finalize!
+          # Default implementation is to do nothing.
+        end
+    
+            # This is what is called on the class to actually execute it. Any
+        # subclasses should implement this method and do any option parsing
+        # and validation here.
+        def execute
+        end
+    
+            # This returns all the registered guests.
+        #
+        # @return [Hash]
+        def hosts
+          Registry.new.tap do |result|
+            @registered.each do |plugin|
+              result.merge!(plugin.components.hosts)
+            end
           end
         end
     
-          # Calls the given block for each condition node in the `when` branch.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_condition
-        return conditions.to_enum(__method__) unless block_given?
+              components.providers.register(name.to_sym) do
+            [block.call, options]
+          end
     
-          dir = if File.directory?(file_or_dir)
-              file_or_dir
-            else
-              File.dirname(file_or_dir)
-            end
-      @path_cache[dir] ||= ConfigLoader.configuration_file_for(dir)
-      path = @path_cache[dir]
-      @object_cache[path] ||= begin
-                                print 'For #{dir}: ' if ConfigLoader.debug?
-                                ConfigLoader.configuration_from_file(path)
-                              end
+        def extract_to_dir(unpack_dir, basename:, verbose:)
+      system_command! AIR_APPLICATION_INSTALLER,
+                      args:    ['-silent', '-location', unpack_dir, path],
+                      verbose: verbose
     end
   end
 end
 
     
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
-  end
-    
-        def render(context)
-      file_dir = (context.registers[:site].source || 'source')
-      file_path = Pathname.new(file_dir).expand_path
-      file = file_path + @file
-    
-    When /^(?:|I )fill in '([^']*)' for '([^']*)'$/ do |value, field|
-  fill_in(field, :with => value)
-end
-    
-        def path
-      @file.respond_to?(:path) ? @file.path : @file
+        if info.include?('repository')
+      self.url = info['repository']['url']
+    else
+      self.url = 'https://npmjs.org/package/#{self.name}'
     end
     
-        # Returns an extension based on the content type. e.g. 'jpeg' for
-    # 'image/jpeg'. If the style has a specified format, it will override the
-    # content-type detection.
-    #
-    # Each mime type generally has multiple extensions associated with it, so
-    # if the extension from the original filename is one of these extensions,
-    # that extension is used, otherwise, the first in the list is used.
-    def content_type_extension attachment, style_name
-      mime_type = MIME::Types[attachment.content_type]
-      extensions_for_mime_type = unless mime_type.empty?
-        mime_type.first.extensions
-      else
-        []
+      option '--php-dir', 'PHP_DIR',
+    'Specify php dir relative to prefix if differs from pear default (pear/php)'
+    
+      # Default specfile generator just makes one specfile, whatever that is for
+  # this package.
+  def generate_specfile(builddir)
+    paths = []
+    logger.info('PWD: #{File.join(builddir, unpack_data_to)}')
+    fileroot = File.join(builddir, unpack_data_to)
+    Dir.chdir(fileroot) do
+      Find.find('.') do |p|
+        next if p == '.'
+        paths << p
       end
+    end
+    logger.info(paths[-1])
+    manifests = %w{package.pp package/remove.pp}
+    
+      def architecture
+    case @architecture
+    when nil, 'native'
+      @architecture = %x{uname -p}.chomp
+    end
+    # 'all' is a valid arch according to
+    # http://www.bolthole.com/solaris/makeapackage.html
+    
+        unless source_stat.symlink?
+      File.chmod(mode, destination)
+    end
+  end # def copy_metadata
