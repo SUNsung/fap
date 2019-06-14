ac30e25e0da3e9a578116b97dcc8b0967a1a3b3e
SUNsung <weatherius@gmail.com>
2019-06-14 11:13:18 +0300

    Rewrite [30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py] File in: 2019-06-14 11:13:18

--- pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
+++ pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
@@ -1,148 +1,129 @@
 
         
-        
-python_3 = (u'thefuck/python3-bash',
-            u'FROM python:3',
-            u'sh')
+            def __init__(self, groups, env=Environment(), **kwargs):
+        '''
+        :param groups: names of processor groups to be applied
+        :param env: Environment
+        :param kwargs: additional keyword arguments for processors
+    
+        def iter_body(self):
+        return self.msg.iter_body(self.chunk_size)
     
     
-@pytest.fixture(params=[(python_3, False),
-                        (python_3, True),
-                        (python_2, False)])
-def proc(request, spawnu, TIMEOUT):
-    container, instant_mode = request.param
-    proc = spawnu(*container)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(init_zshrc.format(
-        u'--enable-experimental-instant-mode' if instant_mode else ''))
-    proc.sendline(u'zsh')
-    if instant_mode:
-        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
-    return proc
+def test_credentials_in_url_auth_flag_has_priority(httpbin_both):
+    '''When credentials are passed in URL and via -a at the same time,
+     then the ones from -a are used.'''
+    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
+                   auth='user:wrong')
+    r = http('--auth=user:password', 'GET', url)
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
     
-    match_output = '''
-Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
-Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
-Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
-Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
-Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
-Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
-Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
-Fetched 89.2 kB in 0s (122 kB/s)
-Reading package lists... Done
-Building dependency tree
-Reading state information... Done
-8 packages can be upgraded. Run 'apt list --upgradable' to see them.
-'''
     
-    You can download from:
-  https://osxfuse.github.io/
-Error: An unsatisfied requirement failed this build.'''
+def test_unicode_json_item_verbose(httpbin):
+    r = http('--verbose', '--json',
+             'POST', httpbin.url + '/post', u'test=%s' % UNICODE)
+    assert HTTP_OK in r
+    assert UNICODE in r
     
-        def read(self, n=None):
-        return ''
-    
-    def findTestCases(module, prefix='test', sortUsing=util.three_way_cmp,
-                  suiteClass=suite.TestSuite):
-    return _makeLoader(prefix, sortUsing, suiteClass).loadTestsFromModule(\
-        module)
+        def test_provider_shall_update_affected_subscribers_with_published_subscription(cls):
+        pro = Provider()
+        pub = Publisher(pro)
+        sub1 = Subscriber('sub 1 name', pro)
+        sub1.subscribe('sub 1 msg 1')
+        sub1.subscribe('sub 1 msg 2')
+        sub2 = Subscriber('sub 2 name', pro)
+        sub2.subscribe('sub 2 msg 1')
+        sub2.subscribe('sub 2 msg 2')
+        with patch.object(sub1, 'run') as mock_subscriber1_run, patch.object(sub2, 'run') as mock_subscriber2_run:
+            pro.update()
+            cls.assertEqual(mock_subscriber1_run.call_count, 0)
+            cls.assertEqual(mock_subscriber2_run.call_count, 0)
+        pub.publish('sub 1 msg 1')
+        pub.publish('sub 1 msg 2')
+        pub.publish('sub 2 msg 1')
+        pub.publish('sub 2 msg 2')
+        with patch.object(sub1, 'run') as mock_subscriber1_run, patch.object(sub2, 'run') as mock_subscriber2_run:
+            pro.update()
+            expected_sub1_calls = [call('sub 1 msg 1'), call('sub 1 msg 2')]
+            mock_subscriber1_run.assert_has_calls(expected_sub1_calls)
+            expected_sub2_calls = [call('sub 2 msg 1'), call('sub 2 msg 2')]
+            mock_subscriber2_run.assert_has_calls(expected_sub2_calls)
 
     
-            If maxBytes is zero, rollover never occurs.
+        def test_frozen_pool(self):
+        with ObjectPool(self.sample_queue) as pool:
+            self.assertEqual(pool, 'first')
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
+    
+        def test_car_adapter_shall_make_loud_noise(self):
+        car = Car()
+        car_adapter = Adapter(car, make_noise=car.make_noise)
+        noise = car_adapter.make_noise(1)
+        expected_noise = 'vroom!'
+        self.assertEqual(noise, expected_noise)
+    
+        def test_given_standby_on_message_switchover_shall_call_hsm_methods(cls):
+        with patch.object(cls.hsm, '_perform_switchover') as mock_perform_switchover, patch.object(
+            cls.hsm, '_check_mate_status'
+        ) as mock_check_mate_status, patch.object(
+            cls.hsm, '_send_switchover_response'
+        ) as mock_send_switchover_response, patch.object(
+            cls.hsm, '_next_state'
+        ) as mock_next_state:
+            cls.hsm.on_message('switchover')
+            cls.assertEqual(mock_perform_switchover.call_count, 1)
+            cls.assertEqual(mock_check_mate_status.call_count, 1)
+            cls.assertEqual(mock_send_switchover_response.call_count, 1)
+            cls.assertEqual(mock_next_state.call_count, 1)
+    
+    Port of the Java example of Dependency Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050)
+    
+    
+class BaseRegisteredClass(metaclass=RegistryHolder):
+    '''
+    Any class that will inherits from BaseRegisteredClass will be included
+    inside the dict RegistryHolder.REGISTRY, the key being the name of the
+    class and the associated value, the class itself.
+    '''
+    
+        def amount(self, val):
+        print(val, end=' ')
+        return self
+    
+            As an alternative you might even in case of success
+        call the next handler.
         '''
-        # If rotation/rollover is wanted, it doesn't make sense to use another
-        # mode. If for example 'w' were specified, then if there were multiple
-        # runs of the calling application, the logs from previous runs would be
-        # lost if the 'w' is respected, because the log file would be truncated
-        # on each run.
-        if maxBytes > 0:
-            mode = 'a'
-        BaseRotatingHandler.__init__(self, filename, mode, encoding, delay)
-        self.maxBytes = maxBytes
-        self.backupCount = backupCount
+        res = self.check_range(request)
+        if not res and self.successor:
+            self.successor.handle(request)
     
-    # Function to return the operator module
-def get_operator_module():
-    return operator
+    STOP_WORDS = frozenset(('a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'can',
+                        'for', 'from', 'have', 'if', 'in', 'is', 'it', 'may',
+                        'not', 'of', 'on', 'or', 'tbd', 'that', 'the', 'this',
+                        'to', 'us', 'we', 'when', 'will', 'with', 'yet',
+                        'you', 'your', '的', '了', '和'))
     
-    def mul(a, b):
-    time.sleep(0.5 * random.random())
-    return a * b
+        def pairfilter(self, wp):
+        return (wp.flag in self.pos_filt and len(wp.word.strip()) >= 2
+                and wp.word.lower() not in self.stop_words)
     
     
-def hang_up(signal, frame):
-    raise HangUpException()
+if sys.platform.startswith('java'):
+    char_state_tab_P, start_P, trans_P, emit_P = load_model()
+else:
+    from .char_state_tab import P as char_state_tab_P
+    from .prob_start import P as start_P
+    from .prob_trans import P as trans_P
+    from .prob_emit import P as emit_P
     
-    It has been modified to mimic the behaviour of
-https://golang.org/pkg/time/#ParseDuration
-'''
-# MIT LICENSE
-#
-# Permission is hereby granted, free of charge, to any person
-# obtaining a copy of this software and associated documentation files
-# (the 'Software'), to deal in the Software without restriction,
-# including without limitation the rights to use, copy, modify, merge,
-# publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so,
-# subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-from __future__ import absolute_import
-from __future__ import unicode_literals
+    log_f = open('1.log','wb')
+log_f.write(words.encode('utf-8'))
     
-        def test_multiple_path_from_env_custom_separator(self):
-        with mock.patch.dict(os.environ):
-            os.environ['COMPOSE_PATH_SEPARATOR'] = '^'
-            os.environ['COMPOSE_FILE'] = 'c:\\one.yml^.\\semi;colon.yml'
-            environment = Environment.from_env_file('.')
-            assert get_config_path_from_options(
-                '.', {}, environment
-            ) == ['c:\\one.yml', '.\\semi;colon.yml']
-    
-        def test_tls_ca_cert_explicit(self):
-        options = {
-            '--tlscacert': self.ca_cert, '--tls': True,
-            '--tlsverify': True
-        }
-        result = tls_config_from_options(options)
-        assert isinstance(result, docker.tls.TLSConfig)
-        assert result.ca_cert == options['--tlscacert']
-        assert result.verify is True
-    
-        @pytest.mark.skipif(not IS_WINDOWS_PLATFORM, reason='Needs pywin32')
-    def test_windows_pipe_error_no_data(self, mock_logging):
-        import pywintypes
-        with pytest.raises(errors.ConnectionError):
-            with handle_connection_errors(mock.Mock(api_version='1.22')):
-                raise pywintypes.error(232, 'WriteFile', 'The pipe is being closed.')
-    
-    
-class ConsoleWarningFormatterTestCase(unittest.TestCase):
-    
-            assert expected == actual
-    
-        def test_sort_service_dicts_3(self):
-        services = [
-            {
-                'name': 'child'
-            },
-            {
-                'name': 'parent',
-                'links': ['child']
-            },
-            {
-                'links': ['parent'],
-                'name': 'grandparent'
-            },
-        ]
+    jieba.enable_parallel(4)
