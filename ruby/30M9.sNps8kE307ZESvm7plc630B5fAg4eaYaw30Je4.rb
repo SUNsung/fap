bdaeb587bdc80be99566e34c9a2325132b512380
SUNsung <weatherius@gmail.com>
2019-06-15 09:29:11 +0300

    Rewrite [30M9.sNps8kE307ZESvm7plc630B5fAg4eaYaw30Je4.rb] File in: 2019-06-15 09:29:10

--- ruby/30M9.sNps8kE307ZESvm7plc630B5fAg4eaYaw30Je4.rb
+++ ruby/30M9.sNps8kE307ZESvm7plc630B5fAg4eaYaw30Je4.rb
@@ -1,87 +1,153 @@
 
         
-                def preload_pipeline_warnings
-          # This preloads the number of warnings for every pipeline, ensuring
-          # that Ci::Pipeline#has_warnings? doesn't execute any additional
-          # queries.
-          @pipeline.number_of_warnings
-        end
-    
-    module Gitlab
-  module GithubImport
-    module Importer
-      class LfsObjectsImporter
-        include ParallelScheduling
-    
-            # We inject the page number here to make sure that all importers always
-        # start where they left off. Simply starting over wouldn't work for
-        # repositories with a lot of data (e.g. tens of thousands of comments).
-        options = collection_options.merge(page: page_counter.current)
-    
-            # Initializes the system. Any subclasses MUST make sure this
-        # method is called on the parent. Therefore, if a subclass overrides
-        # `initialize`, then you must call `super`.
-        def initialize(vm)
-          @vm = vm
-        end
-    
-              @registered.each do |plugin|
-            hosts.merge!(plugin.host.to_hash)
-          end
-    
-            # This is the method called to provision the system. This method
-        # is expected to do whatever necessary to provision the system (create files,
-        # SSH, etc.)
-        def provision!
-        end
-    
-    Then(/^the failure task will run$/) do
-  failed = TestApp.shared_path.join('failed')
-  run_vagrant_command(test_file_exists(failed))
+        # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
 end
     
-          # Returns the else branch of the `case` statement, if any.
-      #
-      # @return [Node] the else branch node of the `case` statement
-      # @return [nil] if the case statement does not have an else branch.
-      def else_branch
-        node_parts[-1]
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+        def resource_params
+      params.require(:email_domain_block).permit(:domain)
+    end
+  end
+end
+
+    
+      def process_salmon
+    SalmonWorker.perform_async(@account.id, payload.force_encoding('UTF-8'))
+  end
+end
+
+    
+      def update
+    setting.data = params[:data]
+    setting.save!
+    
+      UPDATE_SIGN_IN_HOURS = 24
+    
+      UPDATE_SIGN_IN_HOURS = 24
+    
+        def test_code_around_binding_with_script_lines__
+      with_script_lines do |script_lines|
+        Tempfile.create('irb') do |f|
+          code = 'IRB::WorkSpace.new(binding)\n'
+          script_lines[f.path] = code.split(/^/)
+    
+      def pretty_print(q) # :nodoc:
+    q.group 2, '[SpecificFile:', ']' do
+      q.breakable
+      q.text @path
+    end
+  end
+    
+          after do
+        @client.close
+        @server.close
       end
     
-          # The body of the method definition.
-      #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
-      end
+      def test_tailcall_condition_block
+    bug = '[ruby-core:78015] [Bug #12905]'
     
-    module RuboCop
-  module Cop
-    # This auto-corrects gem dependency order
-    class OrderedGemCorrector
-      extend OrderedGemNode
+      def test_singleton_method_via_eigenclass
+    util_parser <<-RUBY
+class C
+   class << self
+     def a() end
+   end
+end
+    RUBY
     
-                1.upto(3) do |i|
-              next if !arguments[i] || arguments[i].hash_type?
-    
-              if variable.explicit_block_local_variable?
-            message
-          else
-            augment_message(message, variable)
-          end
+              actual = with_term { IRB::Color.colorize_code(code, complete: false) }
+          assert_equal(result, actual, 'Case: colorize_code(#{code.dump}, complete: false)\nResult: #{humanized_literal(actual)}')
+        else
+          actual = with_term { IRB::Color.colorize_code(code) }
+          assert_equal(code, actual)
         end
-    
-        def script_url_for(gist_id, filename)
-      url = 'https://gist.github.com/#{gist_id}.js'
-      url = '#{url}?file=#{filename}' unless filename.nil? or filename.empty?
-      url
+      end
     end
     
-        def render(context)
-      file_dir = (context.registers[:site].source || 'source')
-      file_path = Pathname.new(file_dir).expand_path
-      file = file_path + @file
+          foo
+    
+            if specs.size > 0
+          specs
+        else
+          raise LogStash::PluginManager::PluginNotFoundError, 'Cannot find plugins matching: `#{plugin_pattern}`'
+        end
+      end.flatten
+    end
+    
+        private
+    def uncompress(source)
+      temporary_directory = Stud::Temporary.pathname
+      LogStash::Util::Zip.extract(source, temporary_directory, LOGSTASH_PATTERN_RE)
+      temporary_directory
+    rescue Zip::Error => e
+      # OK Zip's handling of file is bit weird, if the file exist but is not a valid zip, it will raise
+      # a `Zip::Error` exception with a file not found message...
+      raise InvalidPackError, 'Cannot uncompress the zip: #{source}'
+    end
+    
+      def create
+    @broadcast = Broadcast.new(broadcast_params)
+    redirect_to '/internal/broadcasts'
+  end
+    
+        def last?
+      index == tab.panes.length - 1
+    end
+    
+      describe '#hook_on_project_start' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_start' }
+    end
+  end
+  describe '#hook_on_project_first_start' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_first_start' }
+    end
+  end
+  describe '#hook_on_project_restart' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_restart' }
+    end
+  end
+  describe '#hook_on_project_exit' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_exit' }
+    end
+  end
+  describe '#hook_on_project_stop' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_stop' }
+    end
+  end
+end
+
+    
+            windows, _, s0 = Open3.capture3(<<-CMD)
+          tmux list-windows -t #{session}\
+          -F '#W \#{window_layout} \#{window_active} \#{pane_current_path}'
+        CMD
+        panes, _, s1 = Open3.capture3(<<-CMD)
+          tmux list-panes -s -t #{session} -F '#W \#{pane_current_path}'
+        CMD
+        tmux_options, _, s2 = Open3.capture3(<<-CMD)
+          tmux show-options -t #{session}
+        CMD
+        project_root = tmux_options[/^default-path '(.+)'$/, 1]
