7fa307c3ecbf0dfcb62a5b5cd80b8316efb225a0
SUNsung <weatherius@gmail.com>
2019-06-15 04:46:44 +0300

    Rewrite [46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py] File in: 2019-06-15 04:46:44

--- pyton/46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py
+++ pyton/46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py
@@ -1,111 +1,191 @@
 
         
-            self.vocab_size, self.relata_dim = 0, 0
-    self.path_to_index = None
-    self.path_dim = 0
+        apt_get_help = b'''apt 1.0.10.2ubuntu1 for amd64 compiled on Oct  5 2015 15:55:05
+Usage: apt-get [options] command
+       apt-get [options] install|remove pkg1 [pkg2 ...]
+       apt-get [options] source pkg1 [pkg2 ...]
     
     
-def _extract_last_relevant(output, seq_lengths):
-  '''Get the last relevant LSTM output cell for each batch instance.
-    
-        # Convert to full tensors, not lists of tensors in time dim.
-    gen_states = list_t_bxn_to_tensor_bxtxn(gen_states)
-    factors = list_t_bxn_to_tensor_bxtxn(factors)
-    out_dist_params = list_t_bxn_to_tensor_bxtxn(out_dist_params)
-    if self.hps.ic_dim > 0:
-      # select first time point
-      prior_g0_mean = prior_g0_mean[0]
-      prior_g0_logvar = prior_g0_logvar[0]
-      post_g0_mean = post_g0_mean[0]
-      post_g0_logvar = post_g0_logvar[0]
-    if self.hps.co_dim > 0:
-      controller_outputs = list_t_bxn_to_tensor_bxtxn(controller_outputs)
+Invalid choice: 'scn', maybe you meant:
     
     
-class CharsVocabulary(Vocabulary):
-  '''Vocabulary containing character-level information.'''
-    
-    FLAGS = tf.flags.FLAGS
-# General flags.
-tf.flags.DEFINE_string('mode', 'eval',
-                       'One of [sample, eval, dump_emb, dump_lstm_emb]. '
-                       ''sample' mode samples future word predictions, using '
-                       'FLAGS.prefix as prefix (prefix could be left empty). '
-                       ''eval' mode calculates perplexity of the '
-                       'FLAGS.input_data. '
-                       ''dump_emb' mode dumps word and softmax embeddings to '
-                       'FLAGS.save_dir. embeddings are dumped in the same '
-                       'order as words in vocabulary. All words in vocabulary '
-                       'are dumped.'
-                       'dump_lstm_emb dumps lstm embeddings of FLAGS.sentence '
-                       'to FLAGS.save_dir.')
-tf.flags.DEFINE_string('pbtxt', '',
-                       'GraphDef proto text file used to construct model '
-                       'structure.')
-tf.flags.DEFINE_string('ckpt', '',
-                       'Checkpoint directory used to fill model values.')
-tf.flags.DEFINE_string('vocab_file', '', 'Vocabulary file.')
-tf.flags.DEFINE_string('save_dir', '',
-                       'Used for 'dump_emb' mode to save word embeddings.')
-# sample mode flags.
-tf.flags.DEFINE_string('prefix', '',
-                       'Used for 'sample' mode to predict next words.')
-tf.flags.DEFINE_integer('max_sample_words', 100,
-                        'Sampling stops either when </S> is met or this number '
-                        'of steps has passed.')
-tf.flags.DEFINE_integer('num_samples', 3,
-                        'Number of samples to generate for the prefix.')
-# dump_lstm_emb mode flags.
-tf.flags.DEFINE_string('sentence', '',
-                       'Used as input for 'dump_lstm_emb' mode.')
-# eval mode flags.
-tf.flags.DEFINE_string('input_data', '',
-                       'Input data files for eval model.')
-tf.flags.DEFINE_integer('max_eval_steps', 1000000,
-                        'Maximum mumber of steps to run 'eval' mode.')
-    
-      def __init__(self, test_data_name='wsc273'):
-    vocab_file = os.path.join(FLAGS.data_dir, 'vocab.txt')
-    self.vocab = utils.CharsVocabulary(vocab_file, 50)
-    assert test_data_name in ['pdp60', 'wsc273'], (
-        'Test data must be pdp60 or wsc273, got {}'.format(test_data_name))
-    self.test_data_name = test_data_name
-    
-      batch_of_indices_predictions = []
+class AbstractBaseSession(models.Model):
+    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
+    session_data = models.TextField(_('session data'))
+    expire_date = models.DateTimeField(_('expire date'), db_index=True)
     
     
-def find_all_ngrams(dataset, n):
-  '''Generate a list of all ngrams.'''
-  return zip(*[dataset[i:] for i in xrange(n)])
-    
-    
-def init_app(app):
-    '''Register database functions with the Flask app. This is called by
-    the application factory.
+class DjangoTemplates(EngineMixin, BaseRenderer):
     '''
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+    Load Django templates from the built-in widget templates in
+    django/forms/templates and from apps' 'templates' directory.
+    '''
+    backend = DjangoTemplates
+    
+        '''
+    if isinstance(kernel_size, int):
+        kernel_size = [kernel_size] * 2
+    if isinstance(strides, int):
+        strides = [strides] * 4
+    
+    
+# # Test
+# def attention_flow_2(h, u, T=None, J=None, d=None, name=None, reuse=None):
+#     '''Attention Flow Match Layer
+#     Input shape:
+#         h: [N, T, d]  # 原文中的 shape 为 [N, T, 2d], 因为经过了 bi-LSTM, 维度扩了一倍
+#         u: [N, J, d]
+#     Output shape:
+#         [N, T, 4d]
+# 
+#     Args:
+#         h: context encoding     shape: [N, T, d]
+#         u: question encoding    shape: [N, J, d]
+#         T(int): context length
+#         J(int): question length
+#         d(int): features size
+#         name(str):
+#         reuse(bool):
+# 
+#     Returns:
+# 
+#     '''
+#     print('Test')
+#     d = d or int(h.get_shape()[-1])
+#     T = T or int(h.get_shape()[-2])
+#     J = J or int(u.get_shape()[-2])
+# 
+#     with tf.variable_scope(name or 'attention_flow', reuse=reuse):
+#         h_expand = tf.tile(tf.expand_dims(h, axis=2), [1, 1, J, 1])
+#         u_expand = tf.tile(tf.expand_dims(u, axis=1), [1, T, 1, 1])
+#         hu = tf.multiply(h_expand, u_expand)  # [N, T, J, d]
+#         h_u_hu = tf.concat([h_expand, u_expand, hu], axis=-1)  # [N, T, J, 3d]
+#         W_s = get_w([3 * d, 1])  # [3d, 1]
+# 
+#         # similarity matrix
+#         # S = tf.reshape(tf.einsum('ntjd,do->ntjo', h_u_hu, W_s), [-1, T, J])
+#         # 以上操作等价于
+#         S = tf.reshape(tf.matmul(tf.reshape(h_u_hu, [-1, 3 * d]), W_s), [-1, T, J])
+# 
+#         # 得到 S 后，下面的操作就与 `attention_flow_self` 一样了
+# 
+#         # u_tilde(u~): context to question attended query vectors
+#         u_tilde = tf.matmul(softmax(S), u)  # [N, T, d]
+# 
+#         # h_tilde(h~): question to context attended query vectors
+#         b = tf.reduce_max(S, axis=2)  # [N, T]
+#         b = softmax(b, axis=-1)  # [N, T]
+#         b = tf.expand_dims(b, axis=1)  # [N, 1, T]
+#         h_tilde = tf.matmul(b, h)  # [N, 1, d]
+#         h_tilde = tf.tile(h_tilde, [1, T, 1])  # [N, T, d]
+# 
+#         g = tf.concat([h, u_tilde, h * u_tilde, h * h_tilde], axis=-1)  # [N, T, 4d]
+# 
+#     return g
 
     
+    import six
     
-def test_logout(client, auth):
-    auth.login()
+            backlogged, size = None, 0
+        from sentry.monitoring.queues import backend
+        if backend is not None:
+            size = backend.get_size('default')
+            backlogged = size > 0
     
-        assert 'closed' in str(e)
+        def forwards(self, orm):
+        # Adding model 'TagValue'
+        db.create_table(u'tagstore_tagvalue', (
+            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
+            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
+            ('_key', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['tagstore.TagKey'], db_column='key_id')),
+            ('value', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('data', self.gf('sentry.db.models.fields.gzippeddict.GzippedDictField')(null=True, blank=True)),
+            ('times_seen', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
+            ('last_seen', self.gf('django.db.models.fields.DateTimeField')(
+                null=True, db_index=True)),
+            ('first_seen', self.gf('django.db.models.fields.DateTimeField')(
+                null=True, db_index=True)),
+        ))
+        db.send_create_signal('tagstore', ['TagValue'])
     
-        def __init__(self, request, key):
-        form_matches = request.form.getlist(key)
-        buf = [
-            'You tried to access the file '%s' in the request.files '
-            'dictionary but it does not exist.  The mimetype for the request '
-            'is '%s' instead of 'multipart/form-data' which means that no '
-            'file contents were transmitted.  To fix this error you should '
-            'provide enctype='multipart/form-data' in your form.'
-            % (key, request.mimetype)
-        ]
-        if form_matches:
-            buf.append(
-                '\n\nThe browser instead transmitted some file names. '
-                'This was submitted: %s' % ', '.join(''%s'' % x for x in form_matches)
-            )
-        self.msg = ''.join(buf)
+            # Changing field 'TagKey.environment_id'
+        db.alter_column(u'tagstore_tagkey', 'environment_id', self.gf(
+            'sentry.db.models.fields.bounded.BoundedBigIntegerField')(null=True))
+    
+        models = {
+        'tagstore.eventtag': {
+            'Meta': {'unique_together': '(('project_id', 'event_id', 'key', 'value'),)', 'object_name': 'EventTag', 'index_together': '(('project_id', 'key', 'value'), ('group_id', 'key', 'value'))'},
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'event_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
+            'group_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
+            'value': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagValue']', 'db_column': ''value_id''})
+        },
+        'tagstore.grouptagkey': {
+            'Meta': {'unique_together': '(('project_id', 'group_id', '_key'),)', 'object_name': 'GroupTagKey'},
+            '_key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
+            'group_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'values_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'})
+        },
+        'tagstore.grouptagvalue': {
+            'Meta': {'unique_together': '(('project_id', 'group_id', '_key', '_value'),)', 'object_name': 'GroupTagValue', 'index_together': '(('project_id', '_key', '_value', 'last_seen'),)'},
+            '_key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
+            '_value': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagValue']', 'db_column': ''value_id''}),
+            'first_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
+            'group_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'last_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'times_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'})
+        },
+        'tagstore.tagkey': {
+            'Meta': {'unique_together': '(('project_id', 'environment_id', 'key'),)', 'object_name': 'TagKey'},
+            'environment_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'status': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'}),
+            'values_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'})
+        },
+        'tagstore.tagvalue': {
+            'Meta': {'unique_together': '(('project_id', '_key', 'value'),)', 'object_name': 'TagValue', 'index_together': '(('project_id', '_key', 'last_seen'),)'},
+            '_key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
+            'data': ('sentry.db.models.fields.gzippeddict.GzippedDictField', [], {'null': 'True', 'blank': 'True'}),
+            'first_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'last_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'times_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+    
+        min_project_id = 0
+    max_project_id = Project.objects.aggregate(x=Max('id'))['x'] or 0
+    step = 1000
+    while min_project_id < max_project_id:
+        queryset = Group.objects.filter(
+            last_seen__gte=now - timedelta(days=1),
+            project__gte=min_project_id,
+            project__lt=min_project_id + step,
+            platform__isnull=False,
+        ).values_list('platform', 'project_id').distinct()
+    
+    import sentry
+    
+    
+@instrumented_task(
+    name='sentry.tasks.process_buffer.process_pending',
+    queue='buffers.process_pending',
+)
+def process_pending(partition=None):
+    '''
+    Process pending buffers.
+    '''
+    from sentry import buffer
+    from sentry.app import locks
