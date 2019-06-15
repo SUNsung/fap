1f96a17004e3cff2d997e247dbca2514636290ab
SUNsung <weatherius@gmail.com>
2019-06-15 20:45:51 +0300

    Rewrite [13OgOfwHoZNqk13VGo4CRrhCVM131J4H5T.LUw13sQG.cpp] File in: 2019-06-15 20:45:50

--- cccccc/13OgOfwHoZNqk13VGo4CRrhCVM131J4H5T.LUw13sQG.cpp
+++ cccccc/13OgOfwHoZNqk13VGo4CRrhCVM131J4H5T.LUw13sQG.cpp
@@ -1,313 +1,90 @@
 
         
-                for (; j < roiw32; j += 32)
-        {
-            internal::prefetch(src0 + j);
-            internal::prefetch(src1 + j);
-            uint8x16_t v_src00 = vld1q_u8(src0 + j), v_src01 = vld1q_u8(src0 + j + 16);
-            uint8x16_t v_src10 = vld1q_u8(src1 + j), v_src11 = vld1q_u8(src1 + j + 16);
-            vst1q_u16(dst + j, vaddl_u8(vget_low_u8(v_src00), vget_low_u8(v_src10)));
-            vst1q_u16(dst + j + 8, vaddl_u8(vget_high_u8(v_src00), vget_high_u8(v_src10)));
-            vst1q_u16(dst + j + 16, vaddl_u8(vget_low_u8(v_src01), vget_low_u8(v_src11)));
-            vst1q_u16(dst + j + 24, vaddl_u8(vget_high_u8(v_src01), vget_high_u8(v_src11)));
-        }
-        for (; j < roiw8; j += 8)
-        {
-            uint8x8_t v_src0 = vld1_u8(src0 + j);
-            uint8x8_t v_src1 = vld1_u8(src1 + j);
-            vst1q_u16(dst + j, vaddl_u8(v_src0, v_src1));
-        }
-    
-            //left&right borders
-        if (borderType != BORDER_MODE_CONSTANT)
-            for (s32 k = 0; k < cn; ++k)
-            {
-                lanea[-cn+k] = lanea[idx_l + k];
-                lanea[colsn+k] = lanea[idx_r + k];
-            }
-    
-                int16x8_t vxnext = vld1q_s16(_dx + j + 8);
-            int16x8_t vynext = vld1q_s16(_dy + j + 8);
-    
-    #define SPLIT4ALPHA(sgn,bits) void split4(const Size2D &size,                                        \
-                                          const sgn##bits * srcBase, ptrdiff_t srcStride,            \
-                                          sgn##bits * dst3Base, ptrdiff_t dst3Stride,                \
-                                          sgn##bits * dst1Base, ptrdiff_t dst1Stride)                \
-{                                                                                                    \
-    internal::assertSupportedConfiguration();                                                        \
-    (void)size;                                                                                      \
-    (void)srcBase;                                                                                   \
-    (void)srcStride;                                                                                 \
-    (void)dst3Base;                                                                                  \
-    (void)dst3Stride;                                                                                \
-    (void)dst1Base;                                                                                  \
-    (void)dst1Stride;                                                                                \
-}
-    
-    
-    {
-    {
-    {            v_src0 = internal::vld1(src0 + x);
-            v_src1 = internal::vld1(src1 + x);
-            op(v_src0, v_src1, v_dst);
-            internal::vst1(dst + x, v_dst);
-            x+=8;
-        }
-    }
-};
-    
-    void assertSupportedConfiguration(bool parametersSupported)
-{
-    if (!isSupportedConfiguration()) {
-        std::cerr << 'internal error: attempted to use an unavailable function' << std::endl;
-        std::abort();
-    }
-    }
-    
-             int32x4_t vline1_s32 = vmovl_s16(vget_low_s16(vline1_s16));
-         int32x4_t vline2_s32 = vmovl_s16(vget_high_s16(vline1_s16));
-         int32x4_t vline3_s32 = vmovl_s16(vget_low_s16(vline2_s16));
-         int32x4_t vline4_s32 = vmovl_s16(vget_high_s16(vline2_s16));
-    
-    #ifdef CAROTENE_NEON
-    if (shift >= 16)
-    {
-        if (cpolicy == CONVERT_POLICY_WRAP)
-        {
-            size_t roiw16 = size.width >= 15 ? size.width - 15 : 0;
-            size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-            int16x8_t v_zero = vdupq_n_s16(0);
-    }
-    }
-    
-        for( ptrdiff_t y = 0; y < rows; y++ )
-    {
-        const u8* v0 = 0;
-        const u8* v1 = internal::getRowPtr(srcBase, srcStride, y);
-        const u8* v2 = 0;
-        // make border
-        if (border == BORDER_MODE_REFLECT101) {
-            v0 = internal::getRowPtr(srcBase, srcStride, y > 0 ? y-1 : y+1);
-            v2 = internal::getRowPtr(srcBase, srcStride, y < rows-1 ? y+1 : rows > 1 ? rows-2 : 0);
-        } else  if (border == BORDER_MODE_CONSTANT) {
-            v0 = y > 0 ? internal::getRowPtr(srcBase, srcStride, y-1) : tmp;
-            v2 =  y < rows-1 ? internal::getRowPtr(srcBase, srcStride, y+1) : tmp;
-        } else {
-            v0 = internal::getRowPtr(srcBase, srcStride, y > 0 ? y-1 : 0);
-            v2 = internal::getRowPtr(srcBase, srcStride, y < rows-1 ? y+1 : rows > 0 ? rows-1 : 0);
-        }
-        s16* drow = internal::getRowPtr(dstBase, dstStride, y);
-    }
-    
-    
-template <typename Dtype>
-class LayerRegisterer {
- public:
-  LayerRegisterer(const string& type,
-                  shared_ptr<Layer<Dtype> > (*creator)(const LayerParameter&)) {
-    // LOG(INFO) << 'Registering layer type: ' << type;
-    LayerRegistry<Dtype>::AddCreator(type, creator);
+          DefaultCacheKey CKey(Key, &DCache.CBs);
+  auto Entry = DCache.Entries.find(CKey);
+  if (Entry != DCache.Entries.end()) {
+    if (Entry->second == Value)
+      return;
+    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
+    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
+    DCache.Entries.erase(Entry);
   }
-};
     
-    /**
- * @brief Computes @f$ y = |x| @f$
- *
- * @param bottom input Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the inputs @f$ x @f$
- * @param top output Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the computed outputs @f$ y = |x| @f$
- */
-template <typename Dtype>
-class AbsValLayer : public NeuronLayer<Dtype> {
- public:
-  explicit AbsValLayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+    CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
+  llvm::SmallString<32> NameBuf(Name);
+  cache_attributes_t Attrs = {
+    CACHE_ATTRIBUTES_VERSION_2,
+    CBs.keyHashCB,
+    CBs.keyIsEqualCB,
+    nullptr,
+    CBs.keyDestroyCB,
+    CBs.valueReleaseCB,
+    nullptr,
+    nullptr,
+    CBs.UserData,
+    CBs.valueRetainCB,
+  };
     }
     
-    #include 'caffe/layers/loss_layer.hpp'
-    
-      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-      /**
-   * @brief Computes the Contrastive error gradient w.r.t. the inputs.
-   *
-   * Computes the gradients with respect to the two input vectors (bottom[0] and
-   * bottom[1]), but not the similarity label (bottom[2]).
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
-   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
-   *      as @f$ \lambda @f$ is the coefficient of this layer's output
-   *      @f$\ell_i@f$ in the overall Net loss
-   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
-   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
-   *      (*Assuming that this top Blob is not used as a bottom (input) by any
-   *      other layer of the Net.)
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$a@f$; Backward fills their diff with
-   *      gradients if propagate_down[0]
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$b@f$; Backward fills their diff with gradients if
-   *      propagate_down[1]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #ifdef USE_CUDNN
-template <typename Dtype>
-class CuDNNLCNLayer : public LRNLayer<Dtype> {
- public:
-  explicit CuDNNLCNLayer(const LayerParameter& param)
-      : LRNLayer<Dtype>(param), handles_setup_(false), tempDataSize(0),
-        tempData1(NULL), tempData2(NULL) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNLCNLayer();
-    }
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    namespace caffe {
-    }
-    
-      // Array holding scores for each orientation id [0,3].
-  // Orientation ids [0..3] map to [0, 270, 180, 90] degree orientations of the
-  // page respectively, where the values refer to the amount of clockwise
-  // rotation to be applied to the page for the text to be upright and readable.
-  float orientations[4];
-  // Script confidence scores for each of 4 possible orientations.
-  float scripts_na[4][kMaxNumberOfScripts];
-    
-    namespace tesseract {
-  class BoolParam;
-  class DoubleParam;
-  class IntParam;
-  class StringParam;
-  class Tesseract;
-}
-    
-    
-    {  // Restore the pointer to original blamer bundle and combine blamer
-  // information recorded in the splits.
-  if (orig_bb != nullptr) {
-    orig_bb->JoinBlames(*word->blamer_bundle, *word2->blamer_bundle,
-                        wordrec_debug_blamer);
-    delete word->blamer_bundle;
-    word->blamer_bundle = orig_bb;
-  }
-  word->SetupBoxWord();
-  word->reject_map.initialise(word->box_word->length());
-  delete word2;
-}
-    
-    // Factory to build a BoxWord from a TWERD using the DENORMs on each blob to
-// switch back to original image coordinates.
-BoxWord* BoxWord::CopyFromNormalized(TWERD* tessword) {
-  BoxWord* boxword = new BoxWord();
-  // Count the blobs.
-  boxword->length_ = tessword->NumBlobs();
-  // Allocate memory.
-  boxword->boxes_.reserve(boxword->length_);
-    }
-    
-    // Update the other members if the cost is lower.
-void DPPoint::UpdateIfBetter(int64_t cost, int32_t steps, const DPPoint* prev,
-                             int32_t n, int32_t sig_x, int64_t sig_xsq) {
-  if (cost < total_cost_) {
-    total_cost_ = cost;
-    total_steps_ = steps;
-    best_prev_ = prev;
-    n_ = n;
-    sig_x_ = sig_x;
-    sig_xsq_ = sig_xsq;
-  }
-}
-    
-    DHTResponseMessage::~DHTResponseMessage() = default;
-    
-    
-    {  virtual bool finished() = 0;
-};
-    
-    namespace aria2 {
-    }
-    
-    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createReplaceNodeTask(
-    const std::shared_ptr<DHTBucket>& bucket,
-    const std::shared_ptr<DHTNode>& newNode)
-{
-  auto task = std::make_shared<DHTReplaceNodeTask>(bucket, newNode);
-  task->setTimeout(timeout_);
-  setCommonProperty(task);
-  return task;
-}
-    
-      virtual void addPeriodicTask2(const std::shared_ptr<DHTTask>& task) = 0;
-    
-      DHTTaskExecutor periodicTaskQueue2_;
-    
-    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
-                                             std::chrono::seconds interval)
-    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
-{
-}
-    
-    #ifndef __STDC_FORMAT_MACROS
-#define __STDC_FORMAT_MACROS
+    #ifndef PREPOSITION
+#  define PREPOSITION(Word)
 #endif
-#ifndef GFLAGS
-#include <cstdio>
-int main() {
-  fprintf(stderr, 'Please install gflags to run rocksdb tools\n');
-  return 1;
+    
+    
+    {  return 0;
 }
-#else
     
-      if (bytes_left_ >= num_bytes) {
-    bytes_left_ -= num_bytes;
-    return 0;
+    const char *Action::getClassName(Kind AC) {
+  switch (AC) {
+  case Kind::Input:  return 'input';
+  case Kind::CompileJob:  return 'compile';
+  case Kind::InterpretJob:  return 'interpret';
+  case Kind::BackendJob:  return 'backend';
+  case Kind::MergeModuleJob:  return 'merge-module';
+  case Kind::ModuleWrapJob:  return 'modulewrap';
+  case Kind::AutolinkExtractJob:  return 'swift-autolink-extract';
+  case Kind::REPLJob:  return 'repl';
+  case Kind::LinkJob:  return 'link';
+  case Kind::GenerateDSYMJob:  return 'generate-dSYM';
+  case Kind::VerifyDebugInfoJob:  return 'verify-debug-info';
+  case Kind::GeneratePCHJob:  return 'generate-pch';
   }
-  // The frequency to get time inside DB mutex is less than one per refill
-  // interval.
-  auto time_now = NowMicrosMonotonic(env);
-    
-    // This is an example interface of external-compaction algorithm.
-// Compaction algorithm can be implemented outside the core-RocksDB
-// code by using the pluggable compaction APIs that RocksDb provides.
-class Compactor : public EventListener {
- public:
-  // Picks and returns a compaction task given the specified DB
-  // and column family.  It is the caller's responsibility to
-  // destroy the returned CompactionTask.  Returns 'nullptr'
-  // if it cannot find a proper compaction task.
-  virtual CompactionTask* PickCompaction(
-      DB* db, const std::string& cf_name) = 0;
     }
     
-      // Write a key OUTSIDE of this transaction.
-  // Does not affect txn since this is an unrelated key.  If we wrote key 'abc'
-  // here, the transaction would fail to commit.
-  s = db->Put(write_options, 'xyz', 'zzz');
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
     
-      // The operation (high-level action) that the current thread is involved.
-  const OperationType operation_type;
-    
-    namespace rocksdb {
+    double ObjectExtendedInfo60D::object_length(const std::uint8_t* bytes,
+                                            int32_t length) const {
+  Byte t0(bytes + 6);
+  int32_t x = t0.get_byte(0, 8);
     }
+    
+    GemMessageManager::GemMessageManager() {
+  // Control Messages
+  AddSendProtocolData<Accelcmd67, true>();
+  AddSendProtocolData<Brakecmd6b, true>();
+  AddSendProtocolData<Globalcmd69, true>();
+  AddSendProtocolData<Headlightcmd76, true>();
+  AddSendProtocolData<Horncmd78, true>();
+  AddSendProtocolData<Shiftcmd65, true>();
+  AddSendProtocolData<Steeringcmd6d, true>();
+  AddSendProtocolData<Turncmd63, true>();
+  AddSendProtocolData<Wipercmd90, true>();
+    }
+    
+    #include 'modules/canbus/vehicle/gem/protocol/accel_rpt_68.h'
+    
+    // config detail: {'name': 'motor_current', 'offset': 0.0, 'precision': 0.001,
+// 'len': 32, 'is_signed_var': False, 'physical_range': '[0|4294967.295]',
+// 'bit': 7, 'type': 'double', 'order': 'motorola', 'physical_unit': 'amps'}
+double Brakemotorrpt170::motor_current(const std::uint8_t* bytes,
+                                       int32_t length) const {
+  Byte t0(bytes + 0);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    Hornrpt79::Hornrpt79() {}
+const int32_t Hornrpt79::ID = 0x79;
+    
+      x <<= 24;
+  x >>= 24;
