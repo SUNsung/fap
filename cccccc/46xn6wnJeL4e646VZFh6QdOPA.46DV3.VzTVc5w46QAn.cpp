125d404c1384da0e45206bacfe75492e520d8ed6
SUNsung <weatherius@gmail.com>
2019-06-15 10:33:25 +0300

    Rewrite [46xn6wnJeL4e646VZFh6QdOPA.46DV3.VzTVc5w46QAn.cpp] File in: 2019-06-15 10:33:24

--- cccccc/46xn6wnJeL4e646VZFh6QdOPA.46DV3.VzTVc5w46QAn.cpp
+++ cccccc/46xn6wnJeL4e646VZFh6QdOPA.46DV3.VzTVc5w46QAn.cpp
@@ -1,166 +1,377 @@
 
         
-        // Generate destructors.
-#include 'ipc/struct_destructor_macros.h'
-#include 'content/nw/src/common/common_message_generator.h'
+          gfx::Point GetCursorScreenPoint();
+  display::Display GetPrimaryDisplay();
+  std::vector<display::Display> GetAllDisplays();
+  display::Display GetDisplayNearestPoint(const gfx::Point& point);
+  display::Display GetDisplayMatching(const gfx::Rect& match_rect);
     
-    #include <string>
+    #include 'atom/browser/api/trackable_object.h'
+#include 'atom/browser/net/atom_network_delegate.h'
+#include 'native_mate/arguments.h'
+#include 'native_mate/handle.h'
     
-    // Call method of an object in browser and return the result.
-// function CallObjectMethod(id, type, method, args);
-v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
-                                           int object_id,
-                                           const std::string& type,
-                                           const std::string& method,
-                                           v8::Handle<v8::Value> args);
+      // content::QuotaPermissionContext:
+  void RequestQuotaPermission(const content::StorageQuotaParams& params,
+                              int render_process_id,
+                              const PermissionCallback& callback) override;
     
-    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
- public:
-  NwMenuGetNSStringFWithFixupFunction() {}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
     
- protected:
-  ~NwMenuGetNSStringFWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
+    {  DISALLOW_COPY_AND_ASSIGN(AsarProtocolHandler);
 };
     
-     protected:
-  ResponseAction Run() override;
-  ~NwObjCallObjectMethodAsyncFunction() override;
-    
-    
-    {  private:
-    DISALLOW_COPY_AND_ASSIGN(NwScreenIsMonitorStartedFunction);
-  };
-    
-    
-    {    return result;
-}
-    
-    #include <vector>
-#include 'Ratpack/ratpak.h'
-    
-        Number::Number(PNUMBER p) noexcept
-        : m_sign{ p->sign }
-        , m_exp{ p->exp }
-        , m_mantissa{}
-    {
-        m_mantissa.reserve(p->cdigit);
-        copy(p->mant, p->mant + p->cdigit, back_inserter(m_mantissa));
+    namespace atom {
     }
     
-        // to be changed when pszexp is back
-    tokens->GetString(&generatedExpression);
-    // Prefixing and suffixing the special Unicode markers to ensure that the expression
-    // in the history doesn't get broken for RTL languages
-    spHistoryItem->historyItemVector.expression = L'\u202d' + generatedExpression + L'\u202c';
-    spHistoryItem->historyItemVector.result = wstring(result);
-    addedIndex = AddItem(spHistoryItem);
+    // Get basic type definitions.
+#define IPC_MESSAGE_IMPL
+#include 'content/nw/src/common/common_message_generator.h'
+    
+    class ObjectManager;
+    
+    class BaseEvent {
+  friend class EventListener;
+  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
+    }
+    
+    namespace nw {
+    }
     
     
-    {
-    {    private:
-        std::vector<std::shared_ptr<HISTORYITEM>> m_historyItems;
-        const size_t m_maxHistorySize;
-    };
-}
-
+    {  DECLARE_EXTENSION_FUNCTION('nw.App.clearCache', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwAppClearCacheFunction);
+};
     
-    #pragma once
-/****************************Module*Header***********************************\
-* Module Name: CalcEngine.h
-*
-* Module Description:
-*       The class definition for the Calculator's engine class CCalcEngine
-*
-* Warnings:
-*
-* Created: 17-Jan-2008
-*
-\****************************************************************************/
+    bool NwObjCallObjectMethodFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
+    }
     
-    #include <caffe/proto/caffe.pb.h>
-#include <dmlc/parameter.h>
-#include <dmlc/base.h>
-#include <dmlc/json.h>
-#include <dmlc/logging.h>
-#include <dmlc/type_traits.h>
-#include <google/protobuf/message.h>
-#include <google/protobuf/text_format.h>
+      // implement nw.Screen.registerStream()
+  class NwScreenRegisterStreamFunction : public NWSyncExtensionFunction {
+  public:
+    NwScreenRegisterStreamFunction();
+    bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
     
-    template<typename xpu>
-void Dequantize2BitKernelLaunch(mshadow::Stream<xpu> *s, const std::vector<mxnet::TBlob> &inputs,
-                                const float threshold) {
-  mxnet::op::mxnet_op::Kernel<dequantize_2bit, xpu>
-  ::Launch(s,
-          inputs[1].Size(),         // original size
-          inputs[1].dptr<float>(),  // out array
-          inputs[0].dptr<float>(),  // compressed array
-          -1 *threshold,            // negative threshold
-          threshold);               // positive threshold
-}
+    // Returns a new environment that stores its data in memory and delegates
+// all non-file-storage tasks to base_env. The caller must delete the result
+// when it is no longer needed.
+// *base_env must remain live while the result is in use.
+LEVELDB_EXPORT Env* NewMemEnv(Env* base_env);
     
-     private:
-  /*!
-   * \brief denotes the type of gradient compression which has been set
-   */
-  CompressionType type_;
     
-      // Check all input and output matrices have the same number
-  // of columns and the output matrix has the right number of rows
-  int ncols = static_cast<int>(out.size(1));
-  int nrows = 1;
-  for (auto & ts : ts_arr) {
-    CHECK_EQ(ncols, static_cast<int>(ts.size(1)))
-      << 'All input and output matrices must have the same number of columns.';
-    nrows *= ts.size(0);
-  }
-  CHECK_EQ(nrows, static_cast<int>(out.size(0)));
+    {}  // namespace leveldb
     
-     private:
-  inline void Init(mshadow::Stream<gpu> *s,
-                   const std::vector<TBlob> &in_data,
-                   const std::vector<TBlob> &out_data) {
-    using namespace mshadow;
-    #if CUDNN_MAJOR >= 5
-    format_ = CUDNN_TENSOR_NCHW;
-    #endif
-    CHECK_EQ(in_data.size(), 2U);
-    CHECK_EQ(out_data.size(), 3U);
-    if (!init_cudnn_) {
-      init_cudnn_ = true;
-      Tensor<gpu, 4, DType> data = in_data[st::kData].get<gpu, 4, DType>(s);
-      Tensor<gpu, 4, DType> out = out_data[st::kOut].get<gpu, 4, DType>(s);
-      CUDNN_CALL(cudnnCreateSpatialTransformerDescriptor(&st_desc_));
-      CUDNN_CALL(cudnnCreateTensorDescriptor(&in_desc_));
-      CUDNN_CALL(cudnnCreateTensorDescriptor(&out_desc_));
-      CUDNN_CALL(cudnnSetTensor4dDescriptor(in_desc_,
-                                            format_,
-                                            dtype_,
-                                            data.size(0),
-                                            data.size(1),
-                                            data.size(2),
-                                            data.size(3)));
-      CUDNN_CALL(cudnnSetTensor4dDescriptor(out_desc_,
-                                            format_,
-                                            dtype_,
-                                            out.size(0),
-                                            out.size(1),
-                                            out.size(2),
-                                            out.size(3)));
-      if (param_.sampler_type == st::kBilinear) {
-        int dim[] = {static_cast<int>(out.size(0)), static_cast<int>(out.size(1)),
-                     static_cast<int>(out.size(2)), static_cast<int>(out.size(3))};
-        CUDNN_CALL(cudnnSetSpatialTransformerNdDescriptor(st_desc_,
-                                                          sampler_,
-                                                          dtype_,
-                                                          4,
-                                                          dim));
+    
+    {    // Linear search (within restart block) for first key >= target
+    SeekToRestartPoint(left);
+    while (true) {
+      if (!ParseNextKey()) {
+        return;
+      }
+      if (Compare(key_, target) >= 0) {
+        return;
       }
     }
   }
     
-    DMLC_REGISTER_PARAMETER(IdentityAttachKLSparseRegParam);
+    TEST(HASH, SignedUnsignedIssue) {
+  const uint8_t data1[1] = {0x62};
+  const uint8_t data2[2] = {0xc3, 0x97};
+  const uint8_t data3[3] = {0xe2, 0x99, 0xa5};
+  const uint8_t data4[4] = {0xe1, 0x80, 0xb9, 0x32};
+  const uint8_t data5[48] = {
+      0x01, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x00,
+      0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x18, 0x28, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  };
+    }
+    
+      Status ReadValue(int i, std::string* val) const {
+    std::string key_space, value_space;
+    Slice key = Key(i, &key_space);
+    Value(i, &value_space);
+    ReadOptions options;
+    return db_->Get(options, key, val);
+  }
+    
+    /* TessError */
+#define GLU_TESS_ERROR1                    100151
+#define GLU_TESS_ERROR2                    100152
+#define GLU_TESS_ERROR3                    100153
+#define GLU_TESS_ERROR4                    100154
+#define GLU_TESS_ERROR5                    100155
+#define GLU_TESS_ERROR6                    100156
+#define GLU_TESS_ERROR7                    100157
+#define GLU_TESS_ERROR8                    100158
+#define GLU_TESS_MISSING_BEGIN_POLYGON     100151
+#define GLU_TESS_MISSING_BEGIN_CONTOUR     100152
+#define GLU_TESS_MISSING_END_POLYGON       100153
+#define GLU_TESS_MISSING_END_CONTOUR       100154
+#define GLU_TESS_COORD_TOO_LARGE           100155
+#define GLU_TESS_NEED_COMBINE_CALLBACK     100156
+    
+      // read a token
+  i = 0;
+  buf[i++] = c;
+  if (c == '(') {
+    backslash = gFalse;
+    while ((c = lookChar()) != EOF) {
+      consumeChar();
+      if (i < size) {
+	buf[i++] = c;
+      }
+      if (c == '\\') {
+	backslash = gTrue;
+      } else if (!backslash && c == ')') {
+	break;
+      } else {
+	backslash = gFalse;
+      }
+    }
+  } else if (c == '<') {
+    while ((c = lookChar()) != EOF) {
+      consumeChar();
+      if (i < size && specialChars[c] != 1) {
+	buf[i++] = c;
+      }
+      if (c == '>') {
+	break;
+      }
+    }
+  } else if (c != '[' && c != ']') {
+    while ((c = lookChar()) != EOF && !specialChars[c]) {
+      consumeChar();
+      if (i < size) {
+	buf[i++] = c;
+      }
+    }
+  }
+  // Zero terminate token string
+  buf[i] = '\0';
+  // Return length of token
+  *length = i;
+    
+    #include 'goo/gtypes.h'
+#include 'goo/GooList.h'
+#include 'goo/GooString.h'
+#include 'Object.h'
+    
+      // get direction
+  if (dict->lookup('M', &obj)->isName()) {
+    const char *m = obj.getName();
+    
+    if (strcmp('I', m) == 0)
+      direction = transitionInward;
+    else if (strcmp('O', m) == 0)
+      direction = transitionOutward;
+  }
+  obj.free();
+    
+    Stream *Parser::makeStream(Object *dict, Guchar *fileKey,
+			   CryptAlgorithm encAlgorithm, int keyLength,
+			   int objNum, int objGen, std::set<int> *fetchOriginatorNums) {
+  Object obj;
+  BaseStream *baseStr;
+  Stream *str;
+  Guint pos, endPos, length;
+    }
+    
+    
+    {    int num, gen;
+};
+    
+    private:
+    
+      Sound *copy();
+    
+    GBool SplashOutputDev::maskedImageSrc(void *data, SplashColorPtr colorLine,
+				      Guchar *alphaLine) {
+  SplashOutMaskedImageData *imgData = (SplashOutMaskedImageData *)data;
+  Guchar *p, *aq;
+  SplashColor maskColor;
+  SplashColorPtr q, col;
+  GfxRGB rgb;
+  GfxGray gray;
+#if SPLASH_CMYK
+  GfxCMYK cmyk;
+#endif
+  Guchar alpha;
+  int nComps, x;
+    }
+    
+    #ifndef STDINCACHELOADER_H
+#define STDINCACHELOADER_H
+    
+            if (checkpoint.Contains(versionPropertyName))
+            version = checkpoint[versionPropertyName].Value<size_t>();
+        
+        auto learnerState = checkpoint[learnersPropertyName].Value<std::vector<DictionaryValue>>();
+        auto externalState = checkpoint[externalStatePropertyName].Value<Dictionary>();
+    
+            static FunctionPtr Deserialize(const Dictionary& dictionary,
+            const std::unordered_map<std::wstring, Variable>& uidToVariableMap,
+            const CNTK::DeviceDescriptor& device);
+    
+        ParameterInitializer GlorotUniformInitializer(double scale, int outputRank, int filterRank, unsigned long seed)
+    {
+        return CreateInitializer(Microsoft::MSR::CNTK::GlorotUniformInitializerTypeName, scale, outputRank, filterRank, seed);
+    }
+    
+    // Rand based on Mersenne Twister.
+// We use our own distribution in order to match baselines between different operating systems,
+// because uniform_distribution is not guaranteed to provide the same numbers on different platforms.
+// TODO: Switching to Boost would eliminate this problem.
+static inline size_t RandMT(const size_t begin, const size_t end, std::mt19937_64& rng)
+{
+    const size_t randomNumber = rng();
+    return begin + randomNumber % (end - begin);
+}
+    
+        double ElapsedSeconds();
+    
+    template class EditDistanceErrorNode<float>;
+template class EditDistanceErrorNode<double>;
+    
+    // Copies internal accumulator to the output.
+template <class ElemType>
+void EpochAccumulatorNode<ElemType>::CopyAccumulatorToValue()
+{
+    // Value gets resized in UpdateFunctionValuesSize that is called in BeforeForwardProp. Resize fills matrix with NaN
+    // values, so m_value matrix cannot be used as persistent storage between ForwardProp calls.
+    Value().SetValue(*m_accumulator);
+}
+    
+    template <class ElemType>
+struct MemRequestInfo
+{
+    DEVICEID_TYPE deviceId;                     // which device to allocate data 
+    std::vector<shared_ptr<Matrix<ElemType>>*> pMatrixPtrs;    // memory pointers 
+    size_t matrixSize;                          // memory size 
+    bool mbScale;                               // whether the memory shall be scaled by minibatch size 
+    bool isWorkSpace;                           // workspace memory or not, by workspace we indicate whether a memory space will be released very shortly after allocation 
+    int allocStep;                              // at what step counter memory allocation is requested 
+    int releaseStep;                            // at what step counter memory release is requested  
+    int memoryId;                               // integer indexing the memory buffer ID 
+    MemRequestInfo(DEVICEID_TYPE deviceId, shared_ptr<Matrix<ElemType>>*pMatrixPtr, size_t matrixSize, bool mbScale, bool isWorkSpace, int allocStep)
+        :deviceId(deviceId), matrixSize(matrixSize), mbScale(mbScale), isWorkSpace(isWorkSpace), allocStep(allocStep), releaseStep(INT_MAX), memoryId(-1)
+    {
+        pMatrixPtrs.push_back(pMatrixPtr);
+    }
+    void SetReleaseStep(int step) { releaseStep = step; }
+    void SetMemoryId(int id) { memoryId = id;  }
+};
+    
+        case URX_ONECHAR:
+    case URX_ONECHAR_I:
+        if (val < 0x20) {
+            printf('%#x', val);
+        } else {
+            printf(''%s'', CStr(UnicodeString(val))());
+        }
+        break;
+    
+    UBool
+RuleBasedCollator::initMaxExpansions(UErrorCode &errorCode) const {
+    umtx_initOnce(tailoring->maxExpansionsInitOnce, computeMaxExpansions, tailoring, errorCode);
+    return U_SUCCESS(errorCode);
+}
+    
+    U_CAPI int8_t U_EXPORT2
+uhash_compareScriptSet(UElement key0, UElement key1) {
+    icu::ScriptSet *s0 = static_cast<icu::ScriptSet *>(key0.pointer);
+    icu::ScriptSet *s1 = static_cast<icu::ScriptSet *>(key1.pointer);
+    int32_t diff = s0->countMembers() - s1->countMembers();
+    if (diff != 0) return diff;
+    int32_t i0 = s0->nextSetBit(0);
+    int32_t i1 = s1->nextSetBit(0);
+    while ((diff = i0-i1) == 0 && i0 > 0) {
+        i0 = s0->nextSetBit(i0+1);
+        i1 = s1->nextSetBit(i1+1);
+    }
+    return (int8_t)diff;
+}
+    
+    U_CAPI UBool U_EXPORT2
+uhash_compareScriptSet(const UElement key1, const UElement key2);
+    
+    #endif /* #if !UCONFIG_NO_BREAK_ITERATION */
+
+    
+    U_CDECL_BEGIN
+static UBool U_CALLCONV
+smpdtfmt_cleanup(void)
+{
+    return SimpleDateFormatStaticSets::cleanup();
+}
+    
+    /**
+ * UnicodeReplacer API
+ */
+UnicodeString& StringReplacer::toReplacerPattern(UnicodeString& rule,
+                                                 UBool escapeUnprintable) const {
+    rule.truncate(0);
+    UnicodeString quoteBuf;
+    }
+    
+      void update();
+    
+      virtual ~DHTTaskQueueImpl();
+    
+    bool DNSCache::CacheEntry::contains(const std::string& addr) const
+{
+  return find(addr) != addrEntries_.end();
+}
+    
+      void markBad(const std::string& hostname, const std::string& ipaddr,
+               uint16_t port);
+    
+        // the following call will not add an object, because there is already
+    // a value stored at key 'B'
+    auto res2 = null.emplace('B', 'c');
+    
+    
+    {  Byte t1(bytes + 5);
+  uint32_t t = t1.get_byte(6, 2);
+  x <<= 2;
+  x |= t;
+  double ret = x * CLUSTER_VREL_RES + CLUSTER_VREL_LONG_MIN;
+  return ret;
+}
+    
+    double ObjectExtendedInfo60D::object_width(const std::uint8_t* bytes,
+                                           int32_t length) const {
+  Byte t0(bytes + 7);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    // config detail: {'name': 'output_value', 'offset': 0.0, 'precision': 0.001,
+// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 39,
+// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
+double Brakerpt6c::output_value(const std::uint8_t* bytes,
+                                int32_t length) const {
+  Byte t0(bytes + 4);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    #include 'glog/logging.h'
+    
+    
+    {
+    {
+    {
+    {  Headlight_rpt_77::Commanded_valueType ret =
+      static_cast<Headlight_rpt_77::Commanded_valueType>(x);
+  return ret;
+}
+}  // namespace gem
+}  // namespace canbus
+}  // namespace apollo
