c4c800b68c0c77b1df9cfdbdeef47d0e81b6aa23
SUNsung <weatherius@gmail.com>
2019-06-15 19:52:11 +0300

    Rewrite [27iIMGyMvuuQ627w743Y.ZXxg27QghUVbLsN227KI.py] File in: 2019-06-15 19:52:11

--- pyton/27iIMGyMvuuQ627w743Y.ZXxg27QghUVbLsN227KI.py
+++ pyton/27iIMGyMvuuQ627w743Y.ZXxg27QghUVbLsN227KI.py
@@ -1,92 +1,54 @@
 
         
-            def log_date_time_string(self):
-        '''Return the current time formatted for logging.'''
-        now = time.time()
-        year, month, day, hh, mm, ss, x, y, z = time.localtime(now)
-        s = '%02d/%3s/%04d %02d:%02d:%02d' % (
-                day, self.monthname[month], year, hh, mm, ss)
-        return s
+            with tf.gfile.Open(filename) as f:
+      idx = 0
+      for line in f:
+        word_name = line.strip()
+        if word_name == '<S>':
+          self._bos = idx
+        elif word_name == '</S>':
+          self._eos = idx
+        elif word_name == '<UNK>':
+          self._unk = idx
+        if word_name == '!!!MAXTERMID':
+          continue
     
-    # Same, but for 3.x to 2.x
-REVERSE_IMPORT_MAPPING = dict((v, k) for (k, v) in IMPORT_MAPPING.items())
-assert len(REVERSE_IMPORT_MAPPING) == len(IMPORT_MAPPING)
-REVERSE_NAME_MAPPING = dict((v, k) for (k, v) in NAME_MAPPING.items())
-assert len(REVERSE_NAME_MAPPING) == len(NAME_MAPPING)
+      Args:
+    logits:  tf.float32 Tensor of the logits of shape [batch_size,
+      sequence_length, vocab_size].
+    targets:  tf.int32 Tensor of the sequence target of shape [batch_size,
+      sequence_length].
+    present:  tf.bool Tensor indicating the presence or absence of the token
+      of shape [batch_size, sequence_length].
     
-        def test_long(self):
-        # Long integers
-        self.check_tokenize('x = 0', '''\
-    NAME       'x'           (1, 0) (1, 1)
-    OP         '='           (1, 2) (1, 3)
-    NUMBER     '0'           (1, 4) (1, 5)
-    ''')
-        self.check_tokenize('x = 0xfffffffffff', '''\
-    NAME       'x'           (1, 0) (1, 1)
-    OP         '='           (1, 2) (1, 3)
-    NUMBER     '0xfffffffffff' (1, 4) (1, 17)
-    ''')
-        self.check_tokenize('x = 123141242151251616110', '''\
-    NAME       'x'           (1, 0) (1, 1)
-    OP         '='           (1, 2) (1, 3)
-    NUMBER     '123141242151251616110' (1, 4) (1, 25)
-    ''')
-        self.check_tokenize('x = -15921590215012591', '''\
-    NAME       'x'           (1, 0) (1, 1)
-    OP         '='           (1, 2) (1, 3)
-    OP         '-'           (1, 4) (1, 5)
-    NUMBER     '15921590215012591' (1, 5) (1, 22)
-    ''')
+        def delete(self, session_key=None):
+        if session_key is None:
+            if self.session_key is None:
+                return
+            session_key = self.session_key
+        self._cache.delete(self.cache_key_prefix + session_key)
     
-        # Load the records from the pickle data stream.
-    file.seek(0)
-    memos = DBUnpickler(file, conn).load()
+        @classmethod
+    def get_model_class(cls):
+        # Avoids a circular import and allows importing SessionStore when
+        # django.contrib.sessions is not in INSTALLED_APPS.
+        from django.contrib.sessions.models import Session
+        return Session
     
-    # A simple generator function
-def baz():
-    for i in range(10):
-        yield i*i
+      for group in itervalues( group_name_to_group ):
+    parent_names = GetParentNames( group )
     
-            return predicate
+        # For pickling to work, the __module__ variable needs to be set to the frame
+    # where the named tuple is created.  Bypass this step in enviroments where
+    # sys._getframe is not defined (Jython for example).
+    if hasattr(_sys, '_getframe'):
+        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
     
-    from uuid import uuid4
-    
-    from sentry.utils.safe import safe_execute
-from sentry.tasks.base import instrumented_task
-    
-    logger = logging.getLogger('sentry.scheduler')
-    
-        def test_cloning_propperty_innate_values(self):
-        sample_object_1 = self.prototype.clone()
-        sample_object_2 = self.prototype.clone()
-        self.assertEqual(sample_object_1.value, sample_object_2.value)
-    
-        def test_sales_manager_shall_not_talk_through_proxy_with_delay(cls):
-        cls.ntp.busy = 'No'
-        start_time = time()
-        cls.ntp.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-This Sales Manager will not talk to you whether he/she is busy or not\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time * 10), expected_execution_time)
-    
-        def update(self):
-        for msg in self.msg_queue:
-            for sub in self.subscribers.get(msg, []):
-                sub.run(msg)
-        self.msg_queue = []
-    
-    
-class Action(object):
-    def __init__(self, name):
-        self.name = name
-    
-        def check_range(self, request):
-        start, end = self.get_interval_from_db()
-        if start <= request < end:
-            print('request {} handled in handler 2'.format(request))
-            return True
+    def _python_exit():
+    global _shutdown
+    _shutdown = True
+    items = list(_threads_queues.items())
+    for t, q in items:
+        q.put(None)
+    for t, q in items:
+        t.join()
