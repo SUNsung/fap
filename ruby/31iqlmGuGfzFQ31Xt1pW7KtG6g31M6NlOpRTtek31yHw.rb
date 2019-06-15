00c9914e24a3310c8b78323222e01843b5831bb6
SUNsung <weatherius@gmail.com>
2019-06-15 13:15:51 +0300

    Rewrite [31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb] File in: 2019-06-15 13:15:51

--- ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
+++ ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
@@ -1,70 +1,34 @@
 
         
-              def print_summary(features)
-        @io.puts
-        print_stats(features, @options)
-        print_snippets(@options)
-        print_passing_wip(@options)
-      end
-    end
+        begin
+  require 'cucumber/rake/task'
+  Cucumber::Rake::Task.new(:features) do |t|
+    t.profile = 'travis'
+  end
+  Cucumber::Rake::Task.new(:'features:html', 'Run Cucumber features and produce HTML output') do |t|
+    t.profile = 'html_report'
+  end
+rescue LoadError
+  desc 'Cucumber rake task not available'
+  task :features do
+    abort 'Cucumber rake task is not available. Be sure to install cucumber as a gem or plugin'
   end
 end
     
-        def no_subcommand(args)
-      unless args.empty? ||
-          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
-        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
-                          Run `jekyll help` to find out more.'
-        abort
-      end
+    FORWARD_SLASH = '/'.freeze
+    
+    require 'benchmark/ips'
+require 'jekyll'
+require 'json'
+    
+          #
+    
+        # advance scanner to pos after the next match of pattern and return the match
+    def scan_next(pattern)
+      return unless @s.scan_until(pattern)
+      @s.matched
     end
     
-        def set_report_note
-      @report_note = ReportNote.find(params[:id])
-    end
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
   end
-end
-
-    
-      private
-    
-      def update
-    if verify_payload?
-      process_salmon
-      head 202
-    elsif payload.present?
-      render plain: signature_verification_failure_reason, status: 401
-    else
-      head 400
-    end
-  end
-    
-      def encoded_challenge
-    HTMLEntities.new.encode(params['hub.challenge'])
-  end
-    
-      included do
-    before_action :authenticate_user!
-    before_action :load_export
-  end
-    
-      # Configure an appender that will write log events to STDOUT. A colorized
-  # pattern layout is used to format the log events into strings before
-  # writing.
-  Logging.appenders.stdout('stdout',
-                           auto_flushing: true,
-                           layout:        Logging.layouts.pattern(
-                             pattern:      pattern,
-                             color_scheme: 'bright'
-                           )
-                          ) if config.log_to.include? 'stdout'
-    
-      describe '#new' do
-    before do
-      sign_in alice, scope: :user
-    end
-    
-        context 'for a logged out user' do
-      before do
-        sign_out :user
-      end
