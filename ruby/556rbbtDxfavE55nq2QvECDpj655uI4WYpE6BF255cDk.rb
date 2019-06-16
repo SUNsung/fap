82e1a8c3e3327ecc51c7b3a5a402318583aa084c
SUNsung <weatherius@gmail.com>
2019-06-16 06:20:14 +0300

    Rewrite [556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb] File in: 2019-06-16 06:20:14

--- ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
+++ ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
@@ -1,172 +1,68 @@
 
         
-              File.write(new_path, '1')
-      false
+          protected
+    
+        def reset_password_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :reset_password_instructions, opts)
     end
     
-          it 'handles the exclude_dirs parameter with no elements correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: [])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
-      end
+          private
     
-    # Here be helper
-    
-            c.action do |args, options|
-          Cert.config = FastlaneCore::Configuration.create(Cert::Options.available_options, options.__hash__)
-          Cert::Runner.new.launch
-        end
-      end
-    
-        # This will detect the proper guest OS for the machine and set up
-    # the class to actually execute capabilities.
-    def detect!
-      guest_name = @machine.config.vm.guest
-      initialize_capabilities!(guest_name, @guests, @capabilities, @machine)
-    rescue Errors::CapabilityHostExplicitNotDetected => e
-      raise Errors::GuestExplicitNotDetected, value: e.extra_data[:value]
-    rescue Errors::CapabilityHostNotDetected
-      raise Errors::GuestNotDetected
-    end
-    
-    module Vagrant
-  module Plugin
-    module V1
-      # This is the base class for a CLI command.
-      class Command
-        include Util::SafePuts
-    
-            # Halt the machine. This method should gracefully shut down the
-        # operating system. This method will cause `vagrant halt` and associated
-        # commands to _block_, meaning that if the machine doesn't halt
-        # in a reasonable amount of time, this method should just return.
-        #
-        # If when this method returns, the machine's state isn't 'powered_off,'
-        # Vagrant will proceed to forcefully shut the machine down.
-        def halt
-          raise BaseError, _key: :unsupported_halt
-        end
-    
-            # This contains all the registered host capabilities.
-        #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :host_capabilities
-    
-        class Main < Clamp::Command
-      subcommand 'list', 'List all installed Logstash plugins', LogStash::PluginManager::List
-      subcommand 'install', 'Install a Logstash plugin', LogStash::PluginManager::Install
-      subcommand 'remove', 'Remove a Logstash plugin', LogStash::PluginManager::Remove
-      subcommand 'update', 'Update a plugin', LogStash::PluginManager::Update
-      subcommand 'pack', 'Package currently installed plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Pack
-      subcommand 'unpack', 'Unpack packaged plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Unpack
-      subcommand 'generate', 'Create the foundation for a new plugin', LogStash::PluginManager::Generate
-      subcommand 'uninstall', 'Uninstall a plugin. Deprecated: Please use remove instead', LogStash::PluginManager::Remove
-      subcommand 'prepare-offline-pack', 'Create an archive of specified plugins to use for offline installation', LogStash::PluginManager::PrepareOfflinePack
-    end
-  end
-end
-    
-        def self.find_by_name_with_wildcards(pattern)
-      re = transform_pattern_into_re(pattern)
-      ::Gem::Specification.find_all.select do |specification|
-        specification.name =~ re
-      end
-    end
-    
-            if Utils::HttpClient.remote_file_exist?(uri)
-          PluginManager.ui.debug('Found package at: #{uri}')
-          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
-        else
-          PluginManager.ui.debug('Package not found at: #{uri}')
-          return nil
-        end
-      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
-        # This probably means there is a firewall in place of the proxy is not correctly configured.
-        # So lets skip this strategy but log a meaningful errors.
-        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
-    
-      namespace :vm do
-    
-        context 'with a specific plugin' do
-      let(:plugin_name) { 'logstash-input-stdin' }
-      it 'list the plugin and display the plugin name' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
-        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
-      end
-    
-        context 'update all the plugins' do
-      it 'has executed successfully' do
-        logstash.run_command_in_path('bin/logstash-plugin update --no-verify')
-        expect(logstash).to have_installed?(plugin_name, '0.1.1')
-      end
+        unless env['devise.skip_trackable']
+      warden.session(scope)['last_request_at'] = Time.now.utc.to_i
     end
   end
 end
 
     
-    module URI
-  ;
-  class << self
-# Does the char code correspond to an alpha-numeric char.
-# isAlphaNumeric('a'.ord) => true
-# isAlphaNumeric(''.ord) => false
-    def isAlphaNumeric(cc)
-      # a - z
-      if (97 <= cc && cc <= 122);
-        return true
-      end
-      # A - Z
-      if (65 <= cc && cc <= 90);
-        return true
-      end
-      # 0 - 9
-      if (48 <= cc && cc <= 57);
-        return true
-      end
-    
-          def is_edit_page
-        false
-      end
-    
-          def string_to_code string
-        # sha bytes
-        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
-        # Thanks donpark's IdenticonUtil.java for this.
-        # Match the following Java code
-        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
-        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
-    
-    $contexts = []
-    
-        # Test page_header_from_content(@content)
-    actual = @view.title
-    assert_equal '1 & 2', actual
-  end
-    
-        post '/edit/PG', :page => 'PG', :content => '바뀐 text', :message => 'ghi'
-    follow_redirect!
-    assert last_response.ok?
-    
-    # Read command line options into `options` hash
-begin
-  opts.parse!
-rescue OptionParser::InvalidOption
-  puts 'gollum: #{$!.message}'
-  puts 'gollum: try 'gollum --help' for more information'
-  exit
-end
-    
-        def emoji(name)
-      if emoji = Gemojione.index.find_by_name(name)
-        IO.read(EMOJI_PATHNAME.join('#{emoji['unicode']}.png'))
-      else
-        fail ArgumentError, 'emoji `#{name}' not found'
-      end
+        # Return modules for the mapping.
+    def modules
+      @modules ||= to.respond_to?(:devise_modules) ? to.devise_modules : []
     end
     
-      s.name              = 'gollum'
-  s.version           = '4.1.4'
-  s.date              = '2018-10-01'
-  s.rubyforge_project = 'gollum'
-  s.license           = 'MIT'
+      def maxwidth_or_default
+    (params[:maxwidth].presence || 400).to_i
+  end
+    
+      before_action :require_user!
+    
+        define_method provider do
+      @user = User.find_for_oauth(request.env['omniauth.auth'], current_user)
+    
+      def apply_header_remaining
+    response.headers['X-RateLimit-Remaining'] = rate_limit_remaining
+  end
+    
+      private
+    
+          # Returns an array of all the when branches in the `case` statement.
+      #
+      # @return [Array<WhenNode>] an array of `when` nodes
+      def when_branches
+        node_parts[1...-1]
+      end
+    
+            def delimiter_delta
+          return 0 if first.same_line?(second)
+          return 0 if first.delimiter != second.delimiter
+    
+            private
+    
+            field_options.merge!(readonly: options[:readonly],
+                             disabled: options[:disabled],
+                             size: options[:size])
+      end
+    
+            # write header and data to target file.
+        target_file.write(header)
+        target_file.write(data)
+      end
+      FileUtils.mv(temporary_file_name, target_path)
+    ensure
+      file.close()
+      target_file.close()
+    end
+  end
+    
+        args.flatten!.compact!
