0d097255acff3c07476d22e531b0f6534c77b443
SUNsung <weatherius@gmail.com>
2019-06-16 13:52:19 +0300

    Rewrite [20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp] File in: 2019-06-16 13:52:19

--- cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
+++ cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
@@ -1,227 +1,199 @@
 
         
-        void Base::CallSync(const std::string& method,
-                    const base::ListValue& arguments,
-                    base::ListValue* result) {
-  NOTREACHED() << 'Uncatched callAsync in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
+        #include <QIcon>
+#include <QPixmap>
+#include <QString>
+    
+    #ifndef SECP256K1_FIELD_INNER5X52_IMPL_H
+#define SECP256K1_FIELD_INNER5X52_IMPL_H
+    
+    const char* InternalKeyComparator::Name() const {
+  return 'leveldb.InternalKeyComparator';
 }
     
-       void Call(const std::string& method,
-                    const base::ListValue& arguments) override;
-   void CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result) override;
     
-    Menu::Menu(int id,
-           const base::WeakPtr<ObjectManager>& object_manager,
-           const base::DictionaryValue& option,
-           const std::string& extension_id)
-  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
-  Create(option);
+    {  // No copying allowed
+  LookupKey(const LookupKey&);
+  void operator=(const LookupKey&);
+};
+    
+    
+// Owned filenames have the form:
+//    dbname/CURRENT
+//    dbname/LOCK
+//    dbname/LOG
+//    dbname/LOG.old
+//    dbname/MANIFEST-[0-9]+
+//    dbname/[0-9]+.(log|sst|ldb)
+bool ParseFileName(const std::string& fname,
+                   uint64_t* number,
+                   FileType* type) {
+  Slice rest(fname);
+  if (rest == 'CURRENT') {
+    *number = 0;
+    *type = kCurrentFile;
+  } else if (rest == 'LOCK') {
+    *number = 0;
+    *type = kDBLockFile;
+  } else if (rest == 'LOG' || rest == 'LOG.old') {
+    *number = 0;
+    *type = kInfoLogFile;
+  } else if (rest.starts_with('MANIFEST-')) {
+    rest.remove_prefix(strlen('MANIFEST-'));
+    uint64_t num;
+    if (!ConsumeDecimalNumber(&rest, &num)) {
+      return false;
+    }
+    if (!rest.empty()) {
+      return false;
+    }
+    *type = kDescriptorFile;
+    *number = num;
+  } else {
+    // Avoid strtoull() to keep filename format independent of the
+    // current locale
+    uint64_t num;
+    if (!ConsumeDecimalNumber(&rest, &num)) {
+      return false;
+    }
+    Slice suffix = rest;
+    if (suffix == Slice('.log')) {
+      *type = kLogFile;
+    } else if (suffix == Slice('.sst') || suffix == Slice('.ldb')) {
+      *type = kTableFile;
+    } else if (suffix == Slice('.dbtmp')) {
+      *type = kTempFile;
+    } else {
+      return false;
+    }
+    *number = num;
+  }
+  return true;
 }
     
-    #endif  // CONTENT_NW_SRC_API_MENU_MENU_DELEGATE_H_
+        try
+    {
+        coshrat(&prat, RATIONAL_BASE, RATIONAL_PRECISION);
+    }
+    catch (uint32_t error)
+    {
+        destroyrat(prat);
+        throw(error);
+    }
+    
+    void CHistoryCollector::AddBinOpToHistory(int nOpCode, bool fNoRepetition)
+{
+    int iCommandEnd = AddCommand(std::make_shared<CBinaryCommand>(nOpCode));
+    m_lastBinOpStartIndex = IchAddSzToEquationSz(L' ', -1);
+    }
+    
+        Number::Number(PNUMBER p) noexcept
+        : m_sign{ p->sign }
+        , m_exp{ p->exp }
+        , m_mantissa{}
+    {
+        m_mantissa.reserve(p->cdigit);
+        copy(p->mant, p->mant + p->cdigit, back_inserter(m_mantissa));
+    }
+    
+    
+    {    ProcessCommandWorker(wParam);
+}
+    
+    COpndCommand::COpndCommand(shared_ptr<CalculatorVector<int>> const& commands, bool fNegative, bool fDecimal, bool fSciFmt)
+    : m_commands(commands)
+    , m_fNegative(fNegative)
+    , m_fSciFmt(fSciFmt)
+    , m_fDecimal(fDecimal)
+    , m_fInitialized(false)
+    , m_value{}
+{
+}
+    
+    
+    {    friend class CalculatorEngineTests::CalcEngineTests;
+};
 
     
-    #include <vector>
-    
-    class NwObjDestroyFunction : public NWSyncExtensionFunction {
- public:
-  NwObjDestroyFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    
-    {  private:
-    DISALLOW_COPY_AND_ASSIGN(NwScreenIsMonitorStartedFunction);
-  };
-    
-    	// ----------------------------------------------------------------------------------------------------
-	// initialization of encoding state from a prior encoding using encoding bits
-	// [a_uiSourceH,a_uiSourceV] is the location of the block in a_pimageSource
-	// a_paucEncodingBits is the place to read the prior encoding
-	// a_imageformat is used to determine how to interpret a_paucEncodingBits
-	// a_errormetric was used for the prior encoding
-	//
-	void Block4x4::InitFromEtcEncodingBits(Image::Format a_imageformat,
-											unsigned int a_uiSourceH, unsigned int a_uiSourceV,
-											unsigned char *a_paucEncodingBits,
-											Image *a_pimageSource,
-											ErrorMetric a_errormetric)
-	{
-		Block4x4();
-    }
-    
-    		// return true if final iteration was performed
-		inline void PerformEncodingIteration(float a_fEffort)
-		{
-			m_pencoding->PerformIteration(a_fEffort);
-		}
-    
-    /** 16x16 multiply-add where the result fits in 32 bits */
-#undef MAC16_16
-static OPUS_INLINE opus_val32 MAC16_16_armv5e(opus_val32 c, opus_val16 a,
- opus_val16 b)
+    NarratorAnnouncement ^ CalculatorAnnouncement::GetMemoryItemChangedAnnouncement(String ^ announcement)
 {
-  int res;
-  __asm__(
-      '#MAC16_16\n\t'
-      'smlabb %0, %1, %2, %3;\n'
-      : '=r'(res)
-      : 'r'(a), 'r'(b), 'r'(c)
-  );
-  return res;
+    return ref new NarratorAnnouncement(
+        announcement, CalculatorActivityIds::MemoryItemChanged, AutomationNotificationKind::ActionCompleted, AutomationNotificationProcessing::MostRecent);
 }
-#define MAC16_16(c, a, b) (MAC16_16_armv5e(c, a, b))
-    
-    #undef silk_CLZ32
-static OPUS_INLINE opus_int32 silk_CLZ32_armv5(opus_int32 in32)
-{
-  int res;
-  __asm__(
-      '#silk_CLZ32\n\t'
-      'clz %0, %1\n\t'
-      : '=r'(res)
-      : 'r'(in32)
-  );
-  return res;
-}
-#define silk_CLZ32(in32) (silk_CLZ32_armv5(in32))
-    
-    ::opencensus::stats::MeasureInt64 RpcServerSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcServerReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerServerLatency();
-::opencensus::stats::MeasureInt64 RpcServerCompletedRpcs();
-    
-    #include <grpc/support/cpu.h>
     
     
     {
-    {}  // namespace load_reporter
-}  // namespace grpc
+    {        static NarratorAnnouncement ^ GetOpenParenthesisCountChangedAnnouncement(Platform::String ^ announcement);
+        static NarratorAnnouncement ^ GetNoRightParenthesisAddedAnnouncement(Platform::String ^ announcement);
+    };
+}
+
     
-    #include 'src/cpp/server/load_reporter/get_cpu_stats.h'
+    DependencyProperty ^ NarratorNotifier::s_announcementProperty;
     
-    PerBalancerStore* LoadDataStore::FindPerBalancerStore(
-    const string& hostname, const string& lb_id) const {
-  auto it = per_host_stores_.find(hostname);
-  if (it != per_host_stores_.end()) {
-    const PerHostStore& per_host_store = it->second;
-    return per_host_store.FindPerBalancerStore(lb_id);
-  } else {
-    return nullptr;
+            property NarratorAnnouncement^ Announcement
+        {
+            NarratorAnnouncement^ get() { return GetAnnouncement(this); }
+            void set(NarratorAnnouncement^ value)
+            {
+                SetAnnouncement(this, value);
+            }
+        }
+    
+    DHTReplaceNodeTask::DHTReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
+                                       const std::shared_ptr<DHTNode>& newNode)
+    : bucket_(bucket),
+      newNode_(newNode),
+      numRetry_(0),
+      timeout_(DHT_MESSAGE_TIMEOUT)
+{
+}
+    
+    namespace aria2 {
+    }
+    
+      const std::shared_ptr<DHTNode>& getLocalNode() const { return localNode_; }
+    
+    DHTRoutingTableSerializer::DHTRoutingTableSerializer(int family)
+    : family_(family)
+{
+}
+    
+    public:
+  DHTRoutingTableSerializer(int family);
+    
+    
+    {  virtual bool finished() = 0;
+};
+    
+      DHTMessageFactory* factory_;
+    
+    void DHTTaskQueueImpl::executeTask()
+{
+  A2_LOG_DEBUG('Updating periodicTaskQueue1');
+  periodicTaskQueue1_.update();
+  A2_LOG_DEBUG('Updating periodicTaskQueue2');
+  periodicTaskQueue2_.update();
+  A2_LOG_DEBUG('Updating immediateTaskQueue');
+  immediateTaskQueue_.update();
+}
+    
+    DHTUnknownMessage::DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                                     const unsigned char* data, size_t length,
+                                     const std::string& ipaddr, uint16_t port)
+    : DHTMessage(localNode, std::shared_ptr<DHTNode>()),
+      length_(length),
+      ipaddr_(ipaddr),
+      port_(port)
+{
+  if (length_ == 0) {
+    data_ = nullptr;
+  }
+  else {
+    data_ = new unsigned char[length];
+    memcpy(data_, data, length);
   }
 }
     
-    
-    {  // Schedule the given callback for execution.
-  virtual void Add(const std::function<void()>& callback) = 0;
-};
-    
-    using apollo::common::ErrorCode;
-    
-    void ClusterGeneralInfo701::Parse(const std::uint8_t* bytes, int32_t length,
-                                  ContiRadar* conti_radar) const {
-  auto obs = conti_radar->add_contiobs();
-  obs->set_clusterortrack(true);
-  obs->set_obstacle_id(obstacle_id(bytes, length));
-  obs->set_longitude_dist(longitude_dist(bytes, length));
-  obs->set_lateral_dist(lateral_dist(bytes, length));
-  obs->set_longitude_vel(longitude_vel(bytes, length));
-  obs->set_lateral_vel(lateral_vel(bytes, length));
-  obs->set_rcs(rcs(bytes, length));
-  obs->set_dynprop(dynprop(bytes, length));
-  double timestamp = apollo::common::time::Clock::NowInSeconds();
-  auto header = obs->mutable_header();
-  header->CopyFrom(conti_radar->header());
-  header->set_timestamp_sec(timestamp);
-}
-    
-    
-    {
-    {
-    {}  // namespace conti_radar
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    #include 'modules/localization/msf/local_map/base_map/base_map_matrix.h'
-    
-    
-    { protected:
-  SpeedLimit speed_limit_;
-};
-    
-      constraint.AddBoundary(index_list, lower_bound, upper_bound);
-  const auto mat = constraint.inequality_constraint_matrix();
-  const auto bd = constraint.inequality_constraint_boundary();
-    
-    // config detail: {'name': 'commanded_value', 'offset': 0.0, 'precision': 0.001,
-// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 23,
-// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
-double Accelrpt68::commanded_value(const std::uint8_t* bytes,
-                                   int32_t length) const {
-  Byte t0(bytes + 2);
-  int32_t x = t0.get_byte(0, 8);
+    namespace aria2 {
     }
-    
-    #include 'gtest/gtest.h'
-    
-    
-    {
-    {
-    {
-    {  double ret = x * 0.001000;
-  return ret;
-}
-}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
-
-    
-    #include 'gtest/gtest.h'
-    
-    
-    {
-    {
-    {
-    {  Horn_rpt_79::Manual_inputType ret =
-      static_cast<Horn_rpt_79::Manual_inputType>(x);
-  return ret;
-}
-}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
-
-    
-    // config detail: {'name': 'longitude_degrees', 'offset': 0.0, 'precision': 1.0,
-// 'len': 8, 'is_signed_var': True, 'physical_range': '[-128|127]', 'bit': 31,
-// 'type': 'int', 'order': 'motorola', 'physical_unit': 'deg'}
-int Latlonheadingrpt82::longitude_degrees(const std::uint8_t* bytes,
-                                          int32_t length) const {
-  Byte t0(bytes + 3);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-      Type type_;
-    
-    
-    {  FLAGS_minloglevel = prev;
-}
-    
-    #include <folly/Benchmark.h>
-#include <folly/FileUtil.h>
-#include <folly/init/Init.h>
-#include <folly/json.h>
-    
-     private:
-  // Matches packed_sync_pointer.  Must be > max number of local
-  // counts.  This is the max number of threads that can access this
-  // atomic_shared_ptr at once before we start blocking.
-  static constexpr unsigned EXTERNAL_OFFSET{0x2000};
-  // Bit signifying aliased constructor
-  static constexpr unsigned ALIASED_PTR{0x4000};
