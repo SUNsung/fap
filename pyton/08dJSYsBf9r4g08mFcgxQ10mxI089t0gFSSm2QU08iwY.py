905ec2ed724cc7f155ad1bd9beaa9d0784fcaa3c
SUNsung <weatherius@gmail.com>
2019-06-16 05:52:28 +0300

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2019-06-16 05:52:28

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,194 +1,215 @@
 
         
-        
-def delete(module, connection, name):
-    ''' Delete an Elasticache backup. '''
-    try:
-        response = connection.delete_snapshot(SnapshotName=name)
-        changed = True
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == 'SnapshotNotFoundFault':
-            response = {}
-            changed = False
-        elif e.response['Error']['Code'] == 'InvalidSnapshotState':
-            module.fail_json(msg='Error: InvalidSnapshotState. The snapshot is not in an available state or failed state to allow deletion.'
-                             'You may need to wait a few minutes.')
+            def get(self, query):
+        '''Get the stored query result from the cache.
+    
+            (2016-01, shopping), 25
+        (2016-01, shopping), 100
+        (2016-01, gas), 50
+        '''
+        timestamp, category, amount = line.split('\t')
+        period = self. extract_year_month(timestamp)
+        if period == self.current_year_month():
+            yield (period, category), amount
+    
+    
+class ArrayMaxLengthValidator(MaxLengthValidator):
+    message = ngettext_lazy(
+        'List contains %(show_value)d item, it should contain no more than %(limit_value)d.',
+        'List contains %(show_value)d items, it should contain no more than %(limit_value)d.',
+        'limit_value')
+    
+        def save(self, must_create=False):
+        if self.session_key is None:
+            return self.create()
+        if must_create:
+            func = self._cache.add
+        elif self._cache.get(self.cache_key) is not None:
+            func = self._cache.set
         else:
-            module.fail_json(msg='Unable to delete the snapshot.', exception=traceback.format_exc())
-    return response, changed
+            raise UpdateError
+        result = func(self.cache_key,
+                      self._get_session(no_load=must_create),
+                      self.get_expiry_age())
+        if must_create and not result:
+            raise CreateError
     
-            try:
-            lambda_facts.update(function_list=client.list_functions(**params)['Functions'])
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                lambda_facts.update(function_list=[])
-            else:
-                module.fail_json_aws(e, msg='Trying to get function list')
+        lastmod = None
+    all_sites_lastmod = True
+    urls = []
+    for site in maps:
+        try:
+            if callable(site):
+                site = site()
+            urls.extend(site.get_urls(page=page, site=req_site,
+                                      protocol=req_protocol))
+            if all_sites_lastmod:
+                site_lastmod = getattr(site, 'latest_lastmod', None)
+                if site_lastmod is not None:
+                    site_lastmod = (
+                        site_lastmod.utctimetuple() if isinstance(site_lastmod, datetime.datetime)
+                        else site_lastmod.timetuple()
+                    )
+                    lastmod = site_lastmod if lastmod is None else max(lastmod, site_lastmod)
+                else:
+                    all_sites_lastmod = False
+        except EmptyPage:
+            raise Http404('Page %s empty' % page)
+        except PageNotAnInteger:
+            raise Http404('No page '%s'' % page)
+    response = TemplateResponse(request, template_name, {'urlset': urls},
+                                content_type=content_type)
+    if all_sites_lastmod and lastmod is not None:
+        # if lastmod is defined for all sites, set header so as
+        # ConditionalGetMiddleware is able to send 304 NOT MODIFIED
+        response['Last-Modified'] = http_date(timegm(lastmod))
+    return response
+
     
-    import traceback
+        References:
+        tflearn.prelu
+    '''
+    if channel_shared:
+        alpha_shape = get_shape(x)[-1:]
+    else:
+        alpha_shape = [1]
     
+        with tf.variable_scope(name or 'conv2d', reuse=reuse):
+        W, b = get_wb(kernel_shape)
     
-DOCUMENTATION = '''
----
-module: ipa_role
-author: Thomas Krahn (@Nosmoht)
-short_description: Manage FreeIPA role
-description:
-- Add, modify and delete a role within FreeIPA server using FreeIPA API
-options:
-  cn:
-    description:
-    - Role name.
-    - Can not be changed as it is the unique identifier.
-    required: true
-    aliases: ['name']
-  description:
-    description:
-    - A description of this role-group.
-  group:
-    description:
-    - List of group names assign to this role.
-    - If an empty list is passed all assigned groups will be unassigned from the role.
-    - If option is omitted groups will not be checked or changed.
-    - If option is passed all assigned groups that are not passed will be unassigned from the role.
-  host:
-    description:
-    - List of host names to assign.
-    - If an empty list is passed all assigned hosts will be unassigned from the role.
-    - If option is omitted hosts will not be checked or changed.
-    - If option is passed all assigned hosts that are not passed will be unassigned from the role.
-  hostgroup:
-    description:
-    - List of host group names to assign.
-    - If an empty list is passed all assigned host groups will be removed from the role.
-    - If option is omitted host groups will not be checked or changed.
-    - If option is passed all assigned hostgroups that are not passed will be unassigned from the role.
-  privilege:
-    description:
-    - List of privileges granted to the role.
-    - If an empty list is passed all assigned privileges will be removed.
-    - If option is omitted privileges will not be checked or changed.
-    - If option is passed all assigned privileges that are not passed will be removed.
-    version_added: '2.4'
-  service:
-    description:
-    - List of service names to assign.
-    - If an empty list is passed all assigned services will be removed from the role.
-    - If option is omitted services will not be checked or changed.
-    - If option is passed all assigned services that are not passed will be removed from the role.
-  state:
-    description: State to ensure
-    default: 'present'
-    choices: ['present', 'absent']
-  user:
-    description:
-    - List of user names to assign.
-    - If an empty list is passed all assigned users will be removed from the role.
-    - If option is omitted users will not be checked or changed.
-extends_documentation_fragment: ipa.documentation
-version_added: '2.3'
+    # 如果一个 ngram 都没找到，gensim 会报错
+#   其实可以返回一个 0 向量的，它内部实际上是从一个 0 向量开始累加的；
+#   但返回时做了一个判断——如果依然是 0 向量，则报错
+# print(model.wv['z'])
+r'''
+Traceback (most recent call last):
+  File 'D:/OneDrive/workspace/github/DL-Notes-for-Interview/code/工具库/gensim/FastText.py', line 53, in <module>
+    print(model.wv['z'])
+  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 336, in __getitem__
+    return self.get_vector(entities)
+  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 454, in get_vector
+    return self.word_vec(word)
+  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 1989, in word_vec
+    raise KeyError('all ngrams for word %s absent from model' % word)
+KeyError: 'all ngrams for word z absent from model'
 '''
     
-        return device
-    
-        Returns a function to set the real IP.
+        Returns:
+        pred_masks (ndarray): R x K x M x M array of class specific soft masks
+            output by the network (must be processed by segm_results to convert
+            into hard masks in the original image coordinate space)
     '''
-    ip_to_mock = None
+    M = cfg.MRCNN.RESOLUTION
+    if boxes.shape[0] == 0:
+        pred_masks = np.zeros((0, M, M), np.float32)
+        return pred_masks
     
-        def test_human_shall_speak(self):
-        noise = self.human.speak()
-        expected_noise = ''hello''
-        self.assertEqual(noise, expected_noise)
     
-        pprint(contributions)
-    
-        parameter
+def filter_for_training(roidb):
+    '''Remove roidb entries that have no usable RoIs based on config settings.
     '''
+    def is_valid(entry):
+        # Valid images have:
+        #   (1) At least one foreground RoI OR
+        #   (2) At least one background RoI
+        overlaps = entry['max_overlaps']
+        # find boxes with sufficient overlap
+        fg_inds = np.where(overlaps >= cfg.TRAIN.FG_THRESH)[0]
+        # Select background RoIs as those within [BG_THRESH_LO, BG_THRESH_HI)
+        bg_inds = np.where((overlaps < cfg.TRAIN.BG_THRESH_HI) &
+                           (overlaps >= cfg.TRAIN.BG_THRESH_LO))[0]
+        # image is only valid if such boxes exist
+        valid = len(fg_inds) > 0 or len(bg_inds) > 0
+        if cfg.MODEL.KEYPOINTS_ON:
+            # If we're training for keypoints, exclude images with no keypoints
+            valid = valid and entry['has_visible_keypoints']
+        return valid
     
     
-def parse_args(args=None):
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('version', type=str, help='Pandas version (0.23.0)')
-    return parser.parse_args(args)
+def _write_voc_results_files(json_dataset, all_boxes, salt):
+    filenames = []
+    image_set_path = voc_info(json_dataset)['image_set_path']
+    assert os.path.exists(image_set_path), \
+        'Image set path does not exist: {}'.format(image_set_path)
+    with open(image_set_path, 'r') as f:
+        image_index = [x.strip() for x in f.readlines()]
+    # Sanity check that order of images in json dataset matches order in the
+    # image set
+    roidb = json_dataset.get_roidb()
+    for i, entry in enumerate(roidb):
+        index = os.path.splitext(os.path.split(entry['image'])[1])[0]
+        assert index == image_index[i]
+    for cls_ind, cls in enumerate(json_dataset.classes):
+        if cls == '__background__':
+            continue
+        logger.info('Writing VOC results for: {}'.format(cls))
+        filename = _get_voc_results_file_template(json_dataset,
+                                                  salt).format(cls)
+        filenames.append(filename)
+        assert len(all_boxes[cls_ind]) == len(image_index)
+        with open(filename, 'wt') as f:
+            for im_ind, index in enumerate(image_index):
+                dets = all_boxes[cls_ind][im_ind]
+                if type(dets) == list:
+                    assert len(dets) == 0, \
+                        'dets should be numpy.ndarray or empty list'
+                    continue
+                # the VOCdevkit expects 1-based indices
+                for k in range(dets.shape[0]):
+                    f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
+                            format(index, dets[k, -1],
+                                   dets[k, 0] + 1, dets[k, 1] + 1,
+                                   dets[k, 2] + 1, dets[k, 3] + 1))
+    return filenames
     
-    ax = fig.add_axes((0.63 + 0.18, 0.1, 0.16, 0.8))
-y = np.row_stack((fnx(), fnx(), fnx()))
-x = np.arange(10)
-y1, y2, y3 = fnx(), fnx(), fnx()
-ax.stackplot(x, y1, y2, y3)
-ax.set_xticks([])
-ax.set_yticks([])
-    
-    
-ABCIndex = create_pandas_abc_type('ABCIndex', '_typ', ('index', ))
-ABCInt64Index = create_pandas_abc_type('ABCInt64Index', '_typ',
-                                       ('int64index', ))
-ABCUInt64Index = create_pandas_abc_type('ABCUInt64Index', '_typ',
-                                        ('uint64index', ))
-ABCRangeIndex = create_pandas_abc_type('ABCRangeIndex', '_typ',
-                                       ('rangeindex', ))
-ABCFloat64Index = create_pandas_abc_type('ABCFloat64Index', '_typ',
-                                         ('float64index', ))
-ABCMultiIndex = create_pandas_abc_type('ABCMultiIndex', '_typ',
-                                       ('multiindex', ))
-ABCDatetimeIndex = create_pandas_abc_type('ABCDatetimeIndex', '_typ',
-                                          ('datetimeindex', ))
-ABCTimedeltaIndex = create_pandas_abc_type('ABCTimedeltaIndex', '_typ',
-                                           ('timedeltaindex', ))
-ABCPeriodIndex = create_pandas_abc_type('ABCPeriodIndex', '_typ',
-                                        ('periodindex', ))
-ABCCategoricalIndex = create_pandas_abc_type('ABCCategoricalIndex', '_typ',
-                                             ('categoricalindex', ))
-ABCIntervalIndex = create_pandas_abc_type('ABCIntervalIndex', '_typ',
-                                          ('intervalindex', ))
-ABCIndexClass = create_pandas_abc_type('ABCIndexClass', '_typ',
-                                       ('index', 'int64index', 'rangeindex',
-                                        'float64index', 'uint64index',
-                                        'multiindex', 'datetimeindex',
-                                        'timedeltaindex', 'periodindex',
-                                        'categoricalindex', 'intervalindex'))
-    
-    Libraries are expected to implement a few pytest fixtures to provide data
-for the tests. The fixtures may be located in either
-    
-        # FILL
-    # - color, fillType
-    ('background-color: red', {'fill': {'fgColor': 'FF0000',
-                                        'patternType': 'solid'}}),
-    ('background-color: #ff0000', {'fill': {'fgColor': 'FF0000',
-                                            'patternType': 'solid'}}),
-    ('background-color: #f0a', {'fill': {'fgColor': 'FF00AA',
-                                         'patternType': 'solid'}}),
-    # BORDER
-    # - style
-    ('border-style: solid',
-     {'border': {'top': {'style': 'medium'},
-                 'bottom': {'style': 'medium'},
-                 'left': {'style': 'medium'},
-                 'right': {'style': 'medium'}}}),
-    ('border-style: solid; border-width: thin',
-     {'border': {'top': {'style': 'thin'},
-                 'bottom': {'style': 'thin'},
-                 'left': {'style': 'thin'},
-                 'right': {'style': 'thin'}}}),
+        k_max = cfg.FPN.RPN_MAX_LEVEL  # coarsest level of pyramid
+    k_min = cfg.FPN.RPN_MIN_LEVEL  # finest level of pyramid
+    assert len(blobs_in) == k_max - k_min + 1
+    for lvl in range(k_min, k_max + 1):
+        bl_in = blobs_in[k_max - lvl]  # blobs_in is in reversed order
+        sc = spatial_scales[k_max - lvl]  # in reversed order
+        slvl = str(lvl)
     
     
-def test_lines_with_compression(compression):
+def keypoint_rcnn_frozen_features(model):
+    logger.warn('Deprecated: use `TRAIN.FREEZE_CONV_BODY: True` instead')
+    return build_generic_detection_model(
+        model,
+        get_func(cfg.MODEL.CONV_BODY),
+        add_roi_box_head_func=get_func(cfg.FAST_RCNN.ROI_BOX_HEAD),
+        add_roi_keypoint_head_func=get_func(cfg.KRCNN.ROI_KEYPOINTS_HEAD),
+        freeze_conv_body=True
+    )
     
-        data = b'x' * 65535
-    b = packb(data, use_bin_type=True)
-    assert len(b) == len(data) + 3
-    assert b[0:1] == header
-    assert b[1:3] == b'\xff\xff'
-    assert b[3:] == data
-    assert unpackb(b) == data
     
+_RENAME = {
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up4convs':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up4convs',
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up',
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0upshare':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v0upshare',
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0up':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v0up',
+    # Removed head_builder module in favor of the more specific fast_rcnn name
+    'head_builder.add_roi_2mlp_head':
+        'fast_rcnn_heads.add_roi_2mlp_head',
+}
     
-def test_incorrect_type_array():
-    unpacker = Unpacker()
-    unpacker.feed(packb(1))
-    try:
-        unpacker.read_array_header()
-        assert 0, 'should raise exception'
-    except UnexpectedTypeException:
-        assert 1, 'okay'
+        if not model.train or cfg.MODEL.FASTER_RCNN:
+        # Proposals are needed during:
+        #  1) inference (== not model.train) for RPN only and Faster R-CNN
+        #  OR
+        #  2) training for Faster R-CNN
+        # Otherwise (== training for RPN only), proposals are not needed
+        model.net.Sigmoid('rpn_cls_logits', 'rpn_cls_probs')
+        model.GenerateProposals(
+            ['rpn_cls_probs', 'rpn_bbox_pred', 'im_info'],
+            ['rpn_rois', 'rpn_roi_probs'],
+            anchors=anchors,
+            spatial_scale=spatial_scale
+        )
