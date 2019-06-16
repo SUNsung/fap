d18d6d571d9935ce9f68a8b03c94467c4386a247
SUNsung <weatherius@gmail.com>
2019-06-16 21:13:14 +0300

    Rewrite [05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb] File in: 2019-06-16 21:13:14

--- ruby/05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb
+++ ruby/05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb
@@ -1,126 +1,143 @@
 
         
-        module Jekyll
-  module Commands
-    class NewTheme < Jekyll::Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:'new-theme') do |c|
-            c.syntax 'new-theme NAME'
-            c.description 'Creates a new Jekyll theme scaffold'
-            c.option 'code_of_conduct', \
-                     '-c', '--code-of-conduct', \
-                     'Include a Code of Conduct. (defaults to false)'
+            def rescue_unknown_error(e)
+      action_completed(@program[:name], status: FastlaneCore::ActionCompletionStatus::FAILED, exception: e)
     
-                  EM.start_server(
-                opts['host'],
-                opts['livereload_port'],
-                HttpAwareConnection,
-                opts
-              ) do |ws|
-                handle_websockets_event(ws)
-              end
-    
-        def order_top_with_notification_levels(score)
-      -'COALESCE(topic_users.notification_level, 1) DESC, COALESCE(category_users.notification_level, 1) DESC, COALESCE(top_topics.#{score}, 0) DESC, topics.bumped_at DESC'
-    end
-    
-    # == Schema Information
-#
-# Table name: application_requests
-#
-#  id       :integer          not null, primary key
-#  date     :date             not null
-#  req_type :integer          not null
-#  count    :integer          default(0), not null
-#
-# Indexes
-#
-#  index_application_requests_on_date_and_req_type  (date,req_type) UNIQUE
-#
-
-    
-        if env['REQUEST_METHOD'] == ('OPTIONS') && env['HTTP_ACCESS_CONTROL_REQUEST_METHOD']
-      return [200, Discourse::Cors.apply_headers(cors_origins, env, {}), []]
-    end
-    
-      def self.headings
-    @headings ||= [:likes_received,
-                   :likes_given,
-                   :topics_entered,
-                   :topic_count,
-                   :post_count,
-                   :posts_read,
-                   :days_visited]
-  end
-    
-    class Draft < ActiveRecord::Base
-  NEW_TOPIC ||= 'new_topic'
-  NEW_PRIVATE_MESSAGE ||= 'new_private_message'
-  EXISTING_TOPIC ||= 'topic_'
-    
-          theme_ids = env[:resolved_theme_ids]
-      headers['Content-Security-Policy'] = policy(theme_ids) if SiteSetting.content_security_policy
-      headers['Content-Security-Policy-Report-Only'] = policy(theme_ids) if SiteSetting.content_security_policy_report_only
-    
-      def set_cache_control_headers
-    if Rails.env.development?
-      response.headers['Last-Modified'] = Time.zone.now.httpdate
-      immutable_for(1.second)
-    else
-      response.headers['Last-Modified'] = last_modified.httpdate if last_modified
-      immutable_for(1.year)
-    end
-  end
-end
-
-    
-        no_cookies
-    
-      def hub_topic_uri
-    @_hub_topic_uri ||= Addressable::URI.parse(hub_topic).normalize
-  end
-    
-      def request_locale
-    preferred_locale || compatible_locale
-  end
-    
-          post_install_messages.compact.each do |message|
-        PluginManager.ui.info(message)
+          def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :tag,
+                                       env_name: 'FL_GIT_TAG_TAG',
+                                       description: 'Define your own tag text. This will replace all other parameters',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :grouping,
+                                       env_name: 'FL_GIT_TAG_GROUPING',
+                                       description: 'Is used to keep your tags organised under one 'folder'',
+                                       default_value: 'builds'),
+          FastlaneCore::ConfigItem.new(key: :prefix,
+                                       env_name: 'FL_GIT_TAG_PREFIX',
+                                       description: 'Anything you want to put in front of the version number (e.g. 'v')',
+                                       default_value: ''),
+          FastlaneCore::ConfigItem.new(key: :postfix,
+                                       env_name: 'FL_GIT_TAG_POSTFIX',
+                                       description: 'Anything you want to put at the end of the version number (e.g. '-RC1')',
+                                       default_value: ''),
+          FastlaneCore::ConfigItem.new(key: :build_number,
+                                       env_name: 'FL_GIT_TAG_BUILD_NUMBER',
+                                       description: 'The build number. Defaults to the result of increment_build_number if you\'re using it',
+                                       default_value: Actions.lane_context[Actions::SharedValues::BUILD_NUMBER],
+                                       default_value_dynamic: true,
+                                       is_string: false),
+          FastlaneCore::ConfigItem.new(key: :message,
+                                       env_name: 'FL_GIT_TAG_MESSAGE',
+                                       description: 'The tag message. Defaults to the tag's name',
+                                       default_value_dynamic: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :commit,
+                                       env_name: 'FL_GIT_TAG_COMMIT',
+                                       description: 'The commit or object where the tag will be set. Defaults to the current HEAD',
+                                       default_value_dynamic: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :force,
+                                       env_name: 'FL_GIT_TAG_FORCE',
+                                       description: 'Force adding the tag',
+                                       optional: true,
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :sign,
+                                       env_name: 'FL_GIT_TAG_SIGN',
+                                       description: 'Make a GPG-signed tag, using the default e-mail address's key',
+                                       optional: true,
+                                       is_string: false,
+                                       default_value: false)
+        ]
       end
     
-    @@ layout
-<html>
-  <head>
-    <title>Sinatra + Sidekiq</title>
-    <body>
-      <%= yield %>
-    </body>
-</html>
+          it 'allows you to specify your own tag' do
+        tag = '2.0.0'
     
-    chdir APP_ROOT do
-  # This script is a starting point to setup your application.
-  # Add necessary setup steps to this file.
+          it 'adds company_id param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            company_id: 'COMPANY ID'
+          )
+        end').runner.execute(:test)
     
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+            expect(result).to include(' -rule #{rule.shellescape} ')
+        expect(result).to include(' -disable-rule #{rule.shellescape} ')
+      end
     
-    desc 'Move source to source.old, install source theme updates, replace source/_includes/navigation.html with source.old's navigation'
-task :update_source, :theme do |t, args|
-  theme = args.theme || 'classic'
-  if File.directory?('#{source_dir}.old')
-    puts '## Removed existing #{source_dir}.old directory'
-    rm_r '#{source_dir}.old', :secure=>true
+    describe Fastlane::Actions do
+  describe '#sh' do
+    before do
+      allow(FastlaneCore::Helper).to receive(:sh_enabled?).and_return(true)
+    end
+    
+        os = 'other'
+    shelljoin_testcases.each do |testcase|
+      it testcase['it'] + ': ' + testcase['it_result'][os] do
+        array = testcase['input']
+        expect_correct_implementation_to_be_called(array, :shelljoin, os)
+        joined = array.shelljoin
+        expect(joined).to eq(testcase['expect'][os])
+      end
+    end
   end
-  mkdir '#{source_dir}.old'
-  cp_r '#{source_dir}/.', '#{source_dir}.old'
-  puts '## Copied #{source_dir} into #{source_dir}.old/'
-  cp_r '#{themes_dir}/'+theme+'/source/.', source_dir, :remove_destination=>true
-  cp_r '#{source_dir}.old/_includes/custom/.', '#{source_dir}/_includes/custom/', :remove_destination=>true
-  cp '#{source_dir}.old/favicon.png', source_dir
-  mv '#{source_dir}/index.html', '#{blog_index_dir}', :force=>true if blog_index_dir != source_dir
-  cp '#{source_dir}.old/index.html', source_dir if blog_index_dir != source_dir && File.exists?('#{source_dir}.old/index.html')
-  puts '## Updated #{source_dir} ##'
 end
+    
+    # The * turns the array into a parameter list
+# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
+# We need to use that, because otherwise invocations like
+# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
+exec(*exec_arr)
+
+    
+      describe :render do
+    let(:screenshots) { [] }
+    
+      # Default specfile generator just makes one specfile, whatever that is for
+  # this package.
+  def generate_specfile(builddir)
+    paths = []
+    logger.info('PWD: #{File.join(builddir, unpack_data_to)}')
+    fileroot = File.join(builddir, unpack_data_to)
+    Dir.chdir(fileroot) do
+      Find.find('.') do |p|
+        next if p == '.'
+        paths << p
+      end
+    end
+    logger.info(paths[-1])
+    manifests = %w{package.pp package/remove.pp}
+    
+        # Add the tar compression flag if necessary
+    tar_compression_flag(input_path).tap do |flag|
+      args << flag unless flag.nil?
+    end
+    
+        # A hook method for authorization libraries to scope the collection. By
+    # default, we just return the same collection. The returned scope is used
+    # as the starting point for all queries to the db in the controller.
+    #
+    # @param [ActiveRecord::Relation] collection The collection the user is
+    #        attempting to view.
+    #
+    # @param [Symbol] action The name of the action to perform. Usually this will be
+    #        one of the `ActiveAdmin::Auth::*` symbols. Defaults to `Auth::READ` if
+    #        no action passed in.
+    #
+    # @return [ActiveRecord::Relation] A new collection, scoped to the
+    #          objects that the current user has access to.
+    def scope_collection(collection, action = Auth::READ)
+      collection
+    end
+    
+        end
+  end
+end
+
+    
+    module ActiveAdmin
+  module BatchActions
