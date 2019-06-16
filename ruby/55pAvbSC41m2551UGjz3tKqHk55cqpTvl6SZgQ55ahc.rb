0b25a7e257c16c092d2292d5906e45aaf0c0e2ba
SUNsung <weatherius@gmail.com>
2019-06-16 10:25:16 +0300

    Rewrite [55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb] File in: 2019-06-16 10:25:15

--- ruby/55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb
+++ ruby/55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb
@@ -1,121 +1,32 @@
 
         
-            it 'returns a Glyphicon icon element with an addidional class' do
-      icon = icon_tag('glyphicon-help', class: 'text-info')
-      expect(icon).to be_html_safe
-      expect(Nokogiri(icon).at('span.glyphicon.glyphicon-help.text-info')).to be_a Nokogiri::XML::Element
-    end
-    
-          def check
-        create_event :payload => {}
-      end
-    end
-    
-        it 'works for queued jobs' do
-      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
-    end
+          def self.link(title)
+    title.downcase.gsub(/(?!-)\W /, '-').gsub(' ', '-').gsub(/(?!-)\W/, '')
   end
     
-          expect(exporter.as_json[:links]).to eq([{ :source => guid_order(agent_list, :jane_weather_agent), :receiver => guid_order(agent_list, :jane_rain_notifier_agent) }])
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
     end
     
-        @taoe, Thread.abort_on_exception = Thread.abort_on_exception, false
-    @oso, @ose, $stdout, $stderr = $stdout, $stderr, StringIO.new, StringIO.new
-    
-          expect(Utils.unindent('Hello\n  I am indented')).to eq('Hello\n  I am indented')
-    
-        it 'should generate the correct events url' do
-      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/trackings')
-    end
-    
-    LogStash::Bundler.setup!
-    
-        def post_render(page)
-      OctopressFilters::post_filter(page)
-    end
-  end
-    
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
-    end
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
+          def main_part_classes
+        if cookies['sidebar-minimized'] == 'true'
+          'col-12 sidebar-collapsed'
+        else
+          'col-9 offset-3 col-md-10 offset-md-2'
         end
-        video += '</video>'
-      else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
       end
-    end
     
-    # Add our files (should be in the current directory):
-package.input('my-executable=/usr/bin/')
-package.input('my-library.so=/usr/lib/')
+    task default: :test
     
-        # Publish the package.
-    repo_path = build_path('#{name}_repo')
-    safesystem('pkgrepo', 'create', repo_path)
-    safesystem('pkgrepo', 'set', '-s', repo_path, 'publisher/prefix=#{attributes[:p5p_publisher]}')
-    safesystem('pkgsend', '-s', repo_path,
-      'publish', '-d', '#{staging_path}', '#{build_path}/#{name}.p5m')
-    safesystem('pkgrecv', '-s', repo_path, '-a',
-      '-d', build_path('#{name}.p5p'), name)
-    
-        pear_cmd = 'pear -c #{config} remote-info #{input_package}'
-    logger.info('Fetching package information', :package => input_package, :command => pear_cmd)
-    name = %x{#{pear_cmd} | sed -ne '/^Package\s*/s/^Package\s*//p'}.chomp
-    self.name = '#{attributes[:pear_package_name_prefix]}-#{name}'
-    self.version = %x{#{pear_cmd} | sed -ne '/^Installed\s*/s/^Installed\s*//p'}.chomp
-    self.description  = %x{#{pear_cmd} | sed -ne '/^Summary\s*/s/^Summary\s*//p'}.chomp
-    logger.debug('Package info', :name => self.name, :version => self.version,
-                  :description => self.description)
-    
-    require 'pleaserun/cli'
-    
-    
-    # Convert the 'package directory' built above to a real solaris package.
-    safesystem('pkgtrans', '-s', build_path, output_path, name)
-    safesystem('cp', '#{build_path}/#{output_path}', output_path)
-  end # def output
-    
-        # Write the scripts, too.
-    write_scripts
-    
-        desc 'Package #{@name}' unless ::Rake.application.last_description
-    
-      def create
-    @credential = @server.credentials.build(params.require(:credential).permit(:type, :name, :hold))
-    if @credential.save
-      redirect_to_with_json [organization, @server, :credentials]
-    else
-      render_form_errors 'new', @credential
-    end
-  end
-    
-      include WithinOrganization
-    
-    $LOAD_PATH.unshift 'lib'
-require 'resque/tasks'
-    
-        # Given a Ruby object, returns a string suitable for storage in a
-    # queue.
-    def encode(object)
-      Resque.encode(object)
-    end
+                respond_with(@order, default_template: :show, status: 201)
+          else
+            @order = Spree::Order.create!(user: current_api_user, store: current_store)
+            if Cart::Update.call(order: @order, params: order_params).success?
+              respond_with(@order, default_template: :show, status: 201)
+            else
+              invalid_resource!(@order)
+            end
+          end
+        end
