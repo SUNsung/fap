45d8e69298d9bdc6ddd021502237a6bce7bca46a
SUNsung <weatherius@gmail.com>
2019-06-16 14:19:59 +0300

    Rewrite [52RMToyg1i652BueimxFjGL.52ydx52wfGGls528EM.cpp] File in: 2019-06-16 14:19:58

--- cccccc/52RMToyg1i652BueimxFjGL.52ydx52wfGGls528EM.cpp
+++ cccccc/52RMToyg1i652BueimxFjGL.52ydx52wfGGls528EM.cpp
@@ -1,266 +1,183 @@
 
         
-        Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+        
+    {			totalEncodingTime += image.GetEncodingTimeMs();
+			}
     
+    struct DoublePoint
+{
+  double X;
+  double Y;
+  DoublePoint(double x = 0, double y = 0) : X(x), Y(y) {}
+  DoublePoint(IntPoint ip) : X((double)ip.X), Y((double)ip.Y) {}
+};
+//------------------------------------------------------------------------------
     
-    {  DCHECK(PyDict_Check(code_to_exc_type_map));
-  PyObject* key;
-  PyObject* value;
-  Py_ssize_t pos = 0;
-  while (PyDict_Next(code_to_exc_type_map, &pos, &key, &value)) {
-    TF_Code code = static_cast<TF_Code>(PyLong_AsLong(key));
-    singleton_->exc_types_[code] = value;
-    // The exception classes should also have the lifetime of the process, but
-    // incref just in case.
-    Py_INCREF(value);
-  }
-}
+    typedef float celt_sig;
+typedef float celt_norm;
+typedef float celt_ener;
     
-    
-    {}  // namespace tensorflow
-    
-    
-    {}  // namespace tensorflow
-
-    
-      const tensorflow::OpRegistrationData* op_reg_data;
-  auto status =
-      tensorflow::OpRegistry::Global()->LookUp(node_def.op(), &op_reg_data);
-  if (!status.ok()) {
-    LOG(WARNING) << 'Op ' << node_def.op() << ' not found: ' << status;
-    return '';
-  }
-  AddDefaultsToNodeDef(op_reg_data->op_def, &node_def);
-    
-    // Returns the kernel class name required to execute <node_def> on the device
-// type of <node_def.device>, or an empty string if the kernel class is not
-// found or the device name is invalid.
-string TryFindKernelClass(const string& serialized_node_def);
-    
-    #include 'tensorflow/stream_executor/device_memory.h'
-#include 'tensorflow/stream_executor/platform/logging.h'
-    
-      // Indicates a preference for more shared memory than L1 cache.
-  kPreferShared,
-    
-        if (!found) {
-      KeyMap::iterator it = keymap.find(upperText);
-      if (it != keymap.end()) {
-        keyName = it->second;
-        found = true;
-      }
-    }
-    
-    
-    {} // namespace extensions
+    #undef USUB32
+#define USUB32(a, b) USUB32_(a, b, __FILE__, __LINE__)
+static OPUS_INLINE unsigned int USUB32_(opus_uint64 a, opus_uint64 b, char *file, int line)
+{
+   opus_uint64 res;
+   if (!VERIFY_UINT(a) || !VERIFY_UINT(b))
+   {
+      fprintf (stderr, 'USUB32: inputs are not uint32: %llu %llu in %s: line %d\n', a, b, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
 #endif
-
-    
-          std::unique_ptr<SkBitmap> bitmap(new SkBitmap());
-      if (data.type == TYPE_PNG &&
-        !gfx::PNGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size(), bitmap.get())) {
-        error_ = 'Failed to decode as PNG';
-        return false;
-      } else if (data.type == TYPE_JPEG) {
-        std::unique_ptr<SkBitmap> tmp_bitmap = gfx::JPEGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size());
-        if (tmp_bitmap == NULL) {
-          error_ = 'Failed to decode as JPEG';
-          return false;
-        }
-        bitmap = std::move(tmp_bitmap);
-      }
-    
-    // Internal macro for implementing {EXPECT|ASSERT}_PRED_FORMAT2.
-// Don't use this in your code.
-#define GTEST_PRED_FORMAT2_(pred_format, v1, v2, on_failure)\
-  GTEST_ASSERT_(pred_format(#v1, #v2, v1, v2), \
-                on_failure)
-    
-    #endif  // GTEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
-
-    
-    using ::testing::EmptyTestEventListener;
-using ::testing::InitGoogleTest;
-using ::testing::Test;
-using ::testing::TestCase;
-using ::testing::TestEventListeners;
-using ::testing::TestInfo;
-using ::testing::TestPartResult;
-using ::testing::UnitTest;
-    
-    
-    {  // Can we set the MyString to NULL?
-  s.Set(NULL);
-  EXPECT_STREQ(NULL, s.c_string());
+   }
+   if (a<b)
+   {
+      fprintf (stderr, 'USUB32: inputs underflow: %llu < %llu in %s: line %d\n', a, b, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = a-b;
+   if (!VERIFY_UINT(res))
+   {
+      fprintf (stderr, 'USUB32: output is not uint32: %llu - %llu = %llu in %s: line %d\n', a, b, res, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips+=2;
+   return res;
 }
-
     
-    DMLC_REGISTER_PARAMETER(CaffeDataParam);
+    #if !defined(_mfrngcode_H)
+# define _mfrngcode_H (1)
+# include 'entcode.h'
     
-    /*!
- *  Copyright (c) 2015 by Contributors
- * \file iter_prefetcher.h
- * \brief define a prefetcher using threaditer to keep k batch fetched
- */
-#ifndef MXNET_IO_ITER_PREFETCHER_H_
-#define MXNET_IO_ITER_PREFETCHER_H_
+    #define silk_SUB_SAT16(a, b)                (opus_int16)silk_SAT16( silk_SUB32( (opus_int32)(a), (b) ) )
+#define silk_SUB_SAT64(a, b)                ((((a)-(b)) & 0x8000000000000000LL) == 0 ?                                               \
+                                            (( (a) & ((b)^0x8000000000000000LL) & 0x8000000000000000LL) ? silk_int64_MIN : (a)-(b)) : \
+                                            ((((a)^0x8000000000000000LL) & (b)  & 0x8000000000000000LL) ? silk_int64_MAX : (a)-(b)) )
     
-    /*!
- * Copyright (c) 2015 by Contributors
- * \file kvstore.cc
- * \brief implement kv_store
- */
-#include <mxnet/kvstore.h>
-#include <stdlib.h>
-#include <dmlc/logging.h>
-#include './kvstore_local.h'
+    // Prints the given value to the given ostream.  If the value is a
+// protocol message, its debug string is printed; if it's an enum or
+// of a type implicitly convertible to BiggestInt, it's printed as an
+// integer; otherwise the bytes in the value are printed.  This is
+// what UniversalPrinter<T>::Print() does when it knows nothing about
+// type T and T has neither << operator nor PrintTo().
+//
+// A user can override this behavior for a class type Foo by defining
+// a << operator in the namespace where Foo is defined.
+//
+// We put this operator in namespace 'internal2' instead of 'internal'
+// to simplify the implementation, as much code in 'internal' needs to
+// use << in STL, which would conflict with our own << were it defined
+// in 'internal'.
+//
+// Note that this operator<< takes a generic std::basic_ostream<Char,
+// CharTraits> type instead of the more restricted std::ostream.  If
+// we define it to take an std::ostream instead, we'll get an
+// 'ambiguous overloads' compiler error when trying to print a type
+// Foo that supports streaming to std::basic_ostream<Char,
+// CharTraits>, as the compiler cannot tell whether
+// operator<<(std::ostream&, const T&) or
+// operator<<(std::basic_stream<Char, CharTraits>, const Foo&) is more
+// specific.
+template <typename Char, typename CharTraits, typename T>
+::std::basic_ostream<Char, CharTraits>& operator<<(
+    ::std::basic_ostream<Char, CharTraits>& os, const T& x) {
+  TypeWithoutFormatter<T,
+      (internal::IsAProtocolMessage<T>::value ? kProtobuf :
+       internal::ImplicitlyConvertible<const T&, internal::BiggestInt>::value ?
+       kConvertibleToInteger : kOtherType)>::PrintValue(x, &os);
+  return os;
+}
     
-    template<typename xpu, typename OP, bool reverse>
-void EvalScalar_(const TBlob &lhs, const real_t &rhs,
-                 TBlob *ret, RunContext ctx) {
-  using namespace mshadow::expr;
-  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
-  CHECK_EQ(ret->type_flag_, lhs.type_flag_)
-    << 'Only support input/output with the same data type';
-  if (reverse) {
-    MSHADOW_TYPE_SWITCH(ret->type_flag_, DType, {
-      ret->FlatTo2D<xpu, DType>(s)
-        = F<typename OP::mshadow_op>(scalar(DType(rhs)), lhs.FlatTo2D<xpu, DType>(s));
-    });
-  } else {
-    MSHADOW_TYPE_SWITCH(ret->type_flag_, DType, {
-      ret->FlatTo2D<xpu, DType>(s)
-        = F<typename OP::mshadow_op>(lhs.FlatTo2D<xpu, DType>(s), scalar(DType(rhs)));
-    });
+    // This helper class is used by {ASSERT|EXPECT}_NO_FATAL_FAILURE to check if a
+// statement generates new fatal failures. To do so it registers itself as the
+// current test part result reporter. Besides checking if fatal failures were
+// reported, it only delegates the reporting to the former result reporter.
+// The original result reporter is restored in the destructor.
+// INTERNAL IMPLEMENTATION - DO NOT USE IN A USER PROGRAM.
+class GTEST_API_ HasNewFatalFailureHelper
+    : public TestPartResultReporterInterface {
+ public:
+  HasNewFatalFailureHelper();
+  virtual ~HasNewFatalFailureHelper();
+  virtual void ReportTestPartResult(const TestPartResult& result);
+  bool has_new_fatal_failure() const { return has_new_fatal_failure_; }
+ private:
+  bool has_new_fatal_failure_;
+  TestPartResultReporterInterface* original_reporter_;
+    }
+    
+      // Signals that the death test did not die as expected.
+  virtual void Abort(AbortReason reason) = 0;
+    
+      // Constructs a FloatingPoint from a raw floating-point number.
+  //
+  // On an Intel CPU, passing a non-normalized NAN (Not a Number)
+  // around may change its bits, although the new value is guaranteed
+  // to be also a NAN.  Therefore, don't expect this constructor to
+  // preserve the bits in x when x is a NAN.
+  explicit FloatingPoint(const RawType& x) { u_.value_ = x; }
+    
+      template <GTEST_10_TYPENAMES_(U)>
+  tuple& CopyFrom(const GTEST_10_TUPLE_(U)& t) {
+    f0_ = t.f0_;
+    f1_ = t.f1_;
+    f2_ = t.f2_;
+    f3_ = t.f3_;
+    f4_ = t.f4_;
+    f5_ = t.f5_;
+    f6_ = t.f6_;
+    f7_ = t.f7_;
+    f8_ = t.f8_;
+    f9_ = t.f9_;
+    return *this;
   }
+    
+    int main(int argc, char **argv) {
+  InitGoogleTest(&argc, argv);
+    }
+    
+    #include 'goo/gtypes.h'
+#include 'goo/GooList.h'
+#include 'goo/GooString.h'
+#include 'Object.h'
+    
+      // Get direction
+  PageTransitionDirection getDirection() { return direction; }
+    
+      // Destructor.
+  virtual ~PreScanOutputDev();
+    
+    ProfileData::ProfileData() {
+	count = 0;
+	total = 0.0;
+	min = 0.0;
+	max = 0.0;
 }
     
-    /*!
- *  Copyright (c) 2018 by Contributors
- * \file transformer.cc
- * \brief CPU implementation of the operators used in Transformer
- */
-#include <mxnet/base.h>
-#include './transformer-inl.h'
-#include '../tensor/elemwise_unary_op.h'
-    
-    namespace mxnet {
-namespace op {
-template<>
-Operator* CreateOp<cpu>(CropParam param) {
-  return new CropOp<cpu>(param);
-}
-    }
+    void SplashOutputDev::paintTransparencyGroup(GfxState * /*state*/, double * /*bbox*/) {
+  SplashBitmap *tBitmap;
+  SplashTransparencyGroup *transpGroup;
+  GBool isolated;
+  int tx, ty;
     }
     
+      virtual ~SplashAxialPattern();
     
+    
+    {};
+    
+    
+    // exception type_error.304
+    try
     {
-    {
-    { private:
-  inline void Init(mshadow::Stream<gpu> *s,
-                   const std::vector<TBlob> &in_data,
-                   const std::vector<TBlob> &out_data) {
-    using namespace mshadow;
-    CHECK_EQ(in_data.size(), 1U);
-    CHECK_EQ(out_data.size(), 2U);
-    if (!init_cudnn_) {
-      init_cudnn_ = true;
-      Tensor<gpu, 4, DType> data = in_data[lrn_enum::kData].get<gpu, 4, DType>(s);
-      Tensor<gpu, 4, DType> out = out_data[lrn_enum::kOut].get<gpu, 4, DType>(s);
-      unsigned lrn_n = param_.nsize;
-      double alpha = param_.alpha;
-      double beta = param_.beta;
-      double lrn_k = param_.knorm;
-      CHECK_EQ(data.shape_, out.shape_);
-      CUDNN_CALL(cudnnCreateLRNDescriptor(&lrn_desc_));
-      CUDNN_CALL(cudnnSetLRNDescriptor(lrn_desc_,
-                                       lrn_n,
-                                       alpha,
-                                       beta,
-                                       lrn_k));
-      CUDNN_CALL(cudnnCreateTensorDescriptor(&shape_desc_));
-      CUDNN_CALL(cudnnSetTensor4dDescriptor(shape_desc_,
-                                            CUDNN_TENSOR_NCHW,
-                                            dtype_,
-                                            data.shape_[0],
-                                            data.shape_[1],
-                                            data.shape_[2],
-                                            data.shape_[3]));
+        // use at() on a non-array type
+        json str = 'I am a string';
+        str.at(0) = 'Another string';
     }
-  }
-  bool init_cudnn_;
-  LRNParam param_;
-  cudnnDataType_t dtype_;
-  cudnnLRNDescriptor_t lrn_desc_;
-  cudnnTensorDescriptor_t shape_desc_;
-};  // class CuDNNLocalResponseNormOp
-}  // namespace op
-}  // namespace mxnet
-#endif  // MXNET_OPERATOR_CUDNN_LRN_INL_H_
-
-    
-    
+    catch (json::type_error& e)
     {
-    {NNVM_REGISTER_OP(IdentityAttachKLSparseReg)
-.set_attr<nnvm::FSetInputVarAttrOnCompose>('FSetInputVarAttrOnCompose',
-    [](const nnvm::NodeAttrs& attrs, nnvm::NodePtr var, const int index) {
-      if (var->attrs.dict.find('__init__') != var->attrs.dict.end()) return;
-      if (index == 1) {
-        var->attrs.dict['__init__'] = '[\'zero\', {}]';
-      }
-    });
-}  // namespace op
-}  // namespace mxnet
-    
-    namespace CalcEngine
-{
-    class Number
-    {
-    public:
-        Number() noexcept;
-        Number(int32_t sign, int32_t exp, std::vector<uint32_t> const& mantissa) noexcept;
+        std::cout << e.what() << '\n';
     }
-    }
-    
-        Rational Sin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Cos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Tan(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ASin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ACos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ATan(Rational const& rat, ANGLE_TYPE angletype);
-    
-        class CalculatorHistory : public IHistoryDisplay
-    {
-    public:
-        CalculatorHistory(const size_t maxSize);
-        unsigned int AddToHistory(
-            _In_ std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> const& spTokens,
-            _In_ std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> const& spCommands,
-            std::wstring_view result);
-        std::vector<std::shared_ptr<HISTORYITEM>> const& GetHistory();
-        std::shared_ptr<HISTORYITEM> const& GetHistoryItem(unsigned int uIdx);
-        void ClearHistory();
-        unsigned int AddItem(_In_ std::shared_ptr<HISTORYITEM> const& spHistoryItem);
-        bool RemoveItem(unsigned int uIdx);
-        size_t MaxHistorySize() const
-        {
-            return m_maxHistorySize;
-        }
-        ~CalculatorHistory(void);
-    }
-    
-    void COpndCommand::ClearAllAndAppendCommand(CalculationManager::Command command)
-{
-    m_commands->Clear();
-    m_commands->Append(static_cast<int>(command));
-    m_fSciFmt = false;
-    m_fNegative = false;
-    m_fDecimal = false;
-}
-    
-    using namespace CalculatorApp::Common::Automation;
-using namespace Windows::UI::Xaml::Automation;
-using namespace Windows::UI::Xaml::Automation::Peers;
-using namespace Windows::UI::Xaml::Controls;
