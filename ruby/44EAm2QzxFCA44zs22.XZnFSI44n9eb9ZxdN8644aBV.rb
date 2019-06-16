be37038018c47cde0ff7f46e2858e36235aa8e7e
SUNsung <weatherius@gmail.com>
2019-06-16 21:41:14 +0300

    Rewrite [44EAm2QzxFCA44zs22.XZnFSI44n9eb9ZxdN8644aBV.rb] File in: 2019-06-16 21:41:13

--- ruby/44EAm2QzxFCA44zs22.XZnFSI44n9eb9ZxdN8644aBV.rb
+++ ruby/44EAm2QzxFCA44zs22.XZnFSI44n9eb9ZxdN8644aBV.rb
@@ -1,115 +1,183 @@
 
         
-              return did_show if did_show
-    
-              value_detector.find_folders(options)
-        end
-    
-      it 'asks to accept conflicts when the scenario was modified' do
-    DefaultScenarioImporter.seed(user)
-    agent = user.agents.where(name: 'Rain Notifier').first
-    agent.options['expected_receive_period_in_days'] = 9001
-    agent.save!
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This Scenario already exists in your system.')
-    expect(page).to have_text('9001')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
-  end
-    
-        it 'outputs a structure containing name, description, the date, all agents & their links' do
-      data = exporter.as_json
-      expect(data[:name]).to eq(name)
-      expect(data[:description]).to eq(description)
-      expect(data[:source_url]).to eq(source_url)
-      expect(data[:guid]).to eq(guid)
-      expect(data[:schema_version]).to eq(1)
-      expect(data[:tag_fg_color]).to eq(tag_fg_color)
-      expect(data[:tag_bg_color]).to eq(tag_bg_color)
-      expect(data[:icon]).to eq(icon)
-      expect(Time.parse(data[:exported_at])).to be_within(2).of(Time.now.utc)
-      expect(data[:links]).to eq([{ :source => guid_order(agent_list, :jane_weather_agent), :receiver => guid_order(agent_list, :jane_rain_notifier_agent)}])
-      expect(data[:control_links]).to eq([])
-      expect(data[:agents]).to eq(agent_list.sort_by{|a| a.guid}.map { |agent| exporter.agent_as_json(agent) })
-      expect(data[:agents].all? { |agent_json| agent_json[:guid].present? && agent_json[:type].present? && agent_json[:name].present? }).to be_truthy
+        def global_require
+  JSON.pretty_generate(DATA)
+end
     
     
-    it 'work without the FAILED_JOBS_TO_KEEP env variable' do
-      old = ENV['FAILED_JOBS_TO_KEEP']
-      ENV['FAILED_JOBS_TO_KEEP'] = nil
-      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(0)
-      ENV['FAILED_JOBS_TO_KEEP'] = old
+def pathutil_relative
+  Pathutil.new(DOC_PATH).relative_path_from(COL_PATH).to_s
+end
+    
+    $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+    
+      #
+  # More advanced [] that does downcase comparison.
+  #
+  def [](key)
+    begin
+      rv = self.fetch(key)
+    rescue IndexError
+      rv = nil
     end
-  end
-    
-      describe '#working?' do
-    it 'should not be working until the first event was received' do
-      expect(@checker).not_to be_working
-      @checker.last_receive_at = Time.now
-      expect(@checker).to be_working
+    if (rv == nil)
+      begin
+        rv = self.dcase_hash[key.downcase]
+      rescue IndexError
+        rv = nil
+      end
     end
-  end
     
-              # Retrieves the element instance fields
+      #
+  # Updates the various parts of the HTTP response command string.
+  #
+  def update_cmd_parts(str)
+    if (md = str.match(/HTTP\/(.+?)\s+(\d+)\s?(.+?)\r?\n?$/))
+      self.message = md[3].gsub(/\r/, '')
+      self.code    = md[2].to_i
+      self.proto   = md[1]
+    else
+      raise RuntimeError, 'Invalid response command string', caller
+    end
+    
+        if res[2][IAX_IE_APPARENT_ADDR]
+      r_fam, r_port, r_addr = res[2][IAX_IE_APPARENT_ADDR].unpack('nnA4')
+      r_addr = r_addr.unpack('C*').map{|x| x.to_s }.join('.')
+      dprint('REGACK: Registered from address #{r_addr}:#{r_port}')
+    end
+    
+    
+  #
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
+  }
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module CredentialCache
+        # This class provides a representation of credential times stored in the Kerberos Credential Cache.
+        class Time < Element
+          # @!attribute auth_time
+          #   @return [Integer]
+          attr_accessor :auth_time
+          # @!attribute start_time
+          #   @return [Integer]
+          attr_accessor :start_time
+          # @!attribute end_time
+          #   @return [Integer]
+          attr_accessor :end_time
+          # @!attribute renew_till
+          #   @return [Integer]
+          attr_accessor :renew_till
+    
+              # Encodes the Rex::Proto::Kerberos::Model::KdcRequest into an ASN.1 String
           #
-          # @return [Array]
-          def attributes
-            self.class.attributes
+          # @return [String]
+          def encode
+            pvno_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_pvno], 1, :CONTEXT_SPECIFIC)
+            msg_type_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_msg_type], 2, :CONTEXT_SPECIFIC)
+            pa_data_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_pa_data], 3, :CONTEXT_SPECIFIC)
+            req_body_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_req_body], 4, :CONTEXT_SPECIFIC)
+            seq = OpenSSL::ASN1::Sequence.new([pvno_asn1, msg_type_asn1, pa_data_asn1, req_body_asn1])
+            seq_asn1 = OpenSSL::ASN1::ASN1Data.new([seq], msg_type, :APPLICATION)
+            seq_asn1.to_der
           end
     
-              # Decodes a Rex::Proto::Kerberos::Model::KdcRequest
+              # Decodes the enc_auth_data field
           #
           # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode_asn1(input)
-            input.value[0].value.each do |val|
-              case val.tag
-              when 1
-                self.pvno = decode_asn1_pvno(val)
-              when 2
-                self.msg_type = decode_asn1_msg_type(val)
-              when 3
-                self.pa_data  = decode_asn1_pa_data(val)
-              when 4
-                self.req_body = decode_asn1_req_body(val)
-              else
-                raise ::RuntimeError, 'Failed to decode KdcRequest SEQUENCE'
-              end
-            end
+          # @return [Rex::Proto::Kerberos::Model::EncryptedData]
+          def decode_enc_auth_data(input)
+            Rex::Proto::Kerberos::Model::EncryptedData.decode(input.value[0])
           end
-    
-                self
-          end
-    
-        pod 'Alamofire', path: '../Alamofire Example/Alamofire'
-    
-            END_OF_OUTPUT_SIGNAL = '\n\r'.freeze
-    
-      gem.add_development_dependency 'danger'
-  gem.add_development_dependency 'mocha'
-  gem.add_development_dependency 'rspec'
-  gem.add_development_dependency 'rubocop', '0.48.1'
+        end
+      end
+    end
+  end
 end
 
     
-      def exists?(type, path)
-    %Q{[ -#{type} '#{path}' ]}
-  end
+              # Decodes the crealm field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [String]
+          def decode_crealm(input)
+            input.value[0].value
+          end
     
-          def ask_question
-        $stdout.print question
-        $stdout.flush
+    def prev_version
+  return prev_feature + '.0' if source_version.end_with? '.0'
+  source_version.gsub(/\d+$/) { |s| s.to_i - 1 }
+end
+    
+          default_options :escape => :html,
+        :escaper => defined?(EscapeUtils) ? EscapeUtils : self
+    
+          def title
+        'Comparison of #{@page.title}'
       end
     
-    %i(git_strategy hg_strategy svn_strategy).each do |strategy|
-  validate(strategy) do |key, _value|
-    warn(
-      '[Deprecation Warning] #{key} is deprecated and will be removed in '\
-      'Capistrano 3.7.0.\n'\
-      'https://github.com/capistrano/capistrano/blob/master/UPGRADING-3.7.md'
-    )
-  end
+          def previous_link
+        label = '&laquo; Previous'
+        if @page_num == 1
+          %(<span class='disabled'>#{label}</span>)
+        else
+          link = url('/history/#{@page.name}?page=#{@page_num-1}')
+          %(<a href='#{link}' hotkey='h'>#{label}</a>)
+        end
+      end
+    
+          def escaped_name
+        CGI.escape(@name)
+      end
+    
+          # Finds header node inside Nokogiri::HTML document.
+      #
+      def find_header_node(doc)
+        case @page.format
+          when :asciidoc
+            doc.css('div#gollum-root > h1:first-child')
+          when :org
+            doc.css('div#gollum-root > p.title:first-child')
+          when :pod
+            doc.css('div#gollum-root > a.dummyTopAnchor:first-child + h1')
+          when :rest
+            doc.css('div#gollum-root > div > div > h1:first-child')
+          else
+            doc.css('div#gollum-root > h1:first-child')
+        end
+      end
+    
+        assert_no_match /New/, last_response.body, ''New' link not blocked in pages template'
+    
+    def gemspec_file
+  '#{name}.gemspec'
 end
+    
+      s.authors  = ['Tom Preston-Werner', 'Rick Olson']
+  s.email    = 'tom@github.com'
+  s.homepage = 'http://github.com/gollum/gollum'
+    
+      def self.assets_path
+    ::File.expand_path('gollum/public', ::File.dirname(__FILE__))
+  end
