d82ca38f06e45eaf9a8d4f6993cecfde512289bb
SUNsung <weatherius@gmail.com>
2019-06-16 10:49:40 +0300

    Rewrite [26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp] File in: 2019-06-16 10:49:39

--- cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
+++ cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
@@ -1,7 +1,127 @@
 
         
-          ~DHTTaskExecutor();
+        private:
+    NetworkStyle(const QString &appName, const int iconColorHueShift, const int iconColorSaturationReduction, const char *titleAddText);
     
-      void setTaskQueue(DHTTaskQueue* taskQueue);
+        ~reverse_lock() {
+        templock.lock();
+        templock.swap(lock);
+    }
     
-      virtual void executeTask() = 0;
+    
+    {    secp256k1_ecdsa_recoverable_signature_load(ctx, &r, &s, &recid, signature);
+    VERIFY_CHECK(recid >= 0 && recid < 4);  /* should have been caught in parse_compact */
+    secp256k1_scalar_set_b32(&m, msg32, NULL);
+    if (secp256k1_ecdsa_sig_recover(&ctx->ecmult_ctx, &r, &s, &q, &m, recid)) {
+        secp256k1_pubkey_save(pubkey, &q);
+        return 1;
+    } else {
+        memset(pubkey, 0, sizeof(*pubkey));
+        return 0;
+    }
+}
+    
+    bool UniValue::get_bool() const
+{
+    if (typ != VBOOL)
+        throw std::runtime_error('JSON value is not a boolean as expected');
+    return getBool();
+}
+    
+      // Compaction range falls after files
+  Compact('r', 'z');
+  ASSERT_EQ('1,1,1', FilesPerLevel());
+    
+    TEST(FormatTest, InternalKeyShortestSuccessor) {
+  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
+            ShortSuccessor(IKey('foo', 100, kTypeValue)));
+  ASSERT_EQ(IKey('\xff\xff', 100, kTypeValue),
+            ShortSuccessor(IKey('\xff\xff', 100, kTypeValue)));
+}
+    
+    //========================================================================
+//
+// Modified under the Poppler project - http://poppler.freedesktop.org
+//
+// All changes made under the Poppler project to this file are licensed
+// under GPL version 2 or later
+//
+// Copyright (C) 2006 Scott Turner <scotty1024@mac.com>
+// Copyright (C) 2008 Albert Astals Cid <aacid@kde.org>
+//
+// To see a description of the changes please see the Changelog file that
+// came with your tarball or type make ChangeLog if you are building from git
+//
+//========================================================================
+    
+      // Get the next PostScript token.  Returns false at end-of-stream.
+  GBool getToken(char *buf, int size, int *length);
+    
+    #ifdef USE_GCC_PRAGMAS
+#pragma implementation
+#endif
+    
+    //------------------------------------------------------------------------
+// ProfileData
+//------------------------------------------------------------------------
+    
+      if (obj->dictLookup('F', &tmp)->isInt()) {
+    int t = tmp.getInt();
+    
+    switch(t) {
+    case 0: fittingPolicy = fittingMeet; break;
+    case 1: fittingPolicy = fittingSlice; break;
+    case 2: fittingPolicy = fittingFill; break;
+    case 3: fittingPolicy = fittingScroll; break;
+    case 4: fittingPolicy = fittingHidden; break;
+    case 5: fittingPolicy = fittingUndefined; break;
+    }
+  }
+  tmp.free();
+    
+    #endif /* _RENDITION_H_ */
+
+    
+    void *ExternalSecurityHandler::getAuthData() {
+  void *authData;
+    }
+    
+      virtual void *makeAuthData(GooString *ownerPassword,
+			     GooString *userPassword);
+  virtual void *getAuthData();
+  virtual void freeAuthData(void *authData);
+  virtual GBool authorize(void *authData);
+  virtual int getPermissionFlags() { return permFlags; }
+  virtual GBool getOwnerPasswordOk() { return ownerPasswordOk; }
+  virtual Guchar *getFileKey() { return fileKey; }
+  virtual int getFileKeyLength() { return fileKeyLength; }
+  virtual int getEncVersion() { return encVersion; }
+  virtual int getEncRevision() { return encRevision; }
+  virtual CryptAlgorithm getEncAlgorithm() { return encAlgorithm; }
+    
+    class Sound
+{
+public:
+  // Try to parse the Object s
+  static Sound *parseSound(Object *s);
+    }
+    
+      imgMaskData.imgStr = new ImageStream(str, width, 1, 1);
+  imgMaskData.imgStr->reset();
+  imgMaskData.invert = invert ? 0 : 1;
+  imgMaskData.width = width;
+  imgMaskData.height = height;
+  imgMaskData.y = 0;
+    
+    public:
+    
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
+    }
+    
+    
+    {}
+
+    
+    #include 'hphp/runtime/base/url.h'
+#include 'hphp/util/assertions.h'
