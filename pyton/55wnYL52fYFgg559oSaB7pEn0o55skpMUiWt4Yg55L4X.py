f4b09769733b00bd337e0446a12d00feacdecf19
SUNsung <weatherius@gmail.com>
2019-06-17 15:38:27 +0300

    Rewrite [55wnYL52fYFgg559oSaB7pEn0o55skpMUiWt4Yg55L4X.py] File in: 2019-06-17 15:38:26

--- pyton/55wnYL52fYFgg559oSaB7pEn0o55skpMUiWt4Yg55L4X.py
+++ pyton/55wnYL52fYFgg559oSaB7pEn0o55skpMUiWt4Yg55L4X.py
@@ -1,232 +1,200 @@
 
         
-          print(header)
-    
-        h = tf.matmul(self.input_vec, self.weights1)
-    self.output = h
-    
-      plt.subplot(nrows,2,13+subplot_cidx)
-  plt.imshow(means_nxt, aspect='auto', interpolation='nearest')
-  plt.title(col_title + ' Means')
-    
-    # save down the network outputs (may be useful later)
-train_outputs_u, valid_outputs_u = split_list_by_inds(outs_e,
-                                                      train_inds,
-                                                      valid_inds)
-train_outputs_u = np.array(train_outputs_u)
-valid_outputs_u = np.array(valid_outputs_u)
-    
-      def evaluate(self):
-    '''Evaluate the current ensemble.'''
-    # Attach word probabilities and correctness label to each substitution
-    ensembled_probs = sum(self.all_probs) / len(self.all_probs)
-    scorings = []
-    for i, sentence in enumerate(self.sentences):
-      correctness = self.labels[i]
-      word_probs = ensembled_probs[i, :len(sentence)]
-      joint_prob = np.prod(word_probs, dtype=np.float64)
-    
-      Yields:
-    Pairs of the batched data, each a matrix of shape [batch_size, num_steps].
-    The second element of the tuple is the same data time-shifted to the
-    right by one.
+            proc.sendline(u'git h')
+    assert proc.expect([TIMEOUT, u'git: 'h' is not a git command.'])
     
     
-def generate_logs(sess, model, log, id_to_word, feed):
-  '''Impute Sequences using the model for a particular feed and send it to
-  logs.
-  '''
-  # Impute Sequences.
-  [p, inputs_eval, sequence_eval] = sess.run(
-      [model.present, model.inputs, model.fake_sequence], feed_dict=feed)
+@pytest.mark.parametrize('command, packages', [
+    (Command('vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('vim', 'The program 'vim' is currently not installed. You can install it by typing: sudo apt install vim'),
+     [('vim', 'main'), ('vim-tiny', 'main')])])
+def test_match(mocker, command, packages):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=None)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
     
-    '''We calculate n-Grams from the training text. We will use this as an
-evaluation metric.'''
+    Commands:
+   update - Retrieve new lists of packages
+   upgrade - Perform an upgrade
+   install - Install new packages (pkg is libc6 not libc6.deb)
+   remove - Remove packages
+   autoremove - Remove automatically all unused packages
+   purge - Remove packages and config files
+   source - Download source archives
+   build-dep - Configure build-dependencies for source packages
+   dist-upgrade - Distribution upgrade, see apt-get(8)
+   dselect-upgrade - Follow dselect selections
+   clean - Erase downloaded archive files
+   autoclean - Erase old downloaded archive files
+   check - Verify that there are no broken dependencies
+   changelog - Download and display the changelog for the given package
+   download - Download the binary package into the current directory
     
-    filenames = {
-    'bin': 'youtube-dl',
-    'exe': 'youtube-dl.exe',
-    'tar': 'youtube-dl-%s.tar.gz' % version}
-build_dir = os.path.join('..', '..', 'build', version)
-for key, filename in filenames.items():
-    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
-    fn = os.path.join(build_dir, filename)
-    with open(fn, 'rb') as f:
-        data = f.read()
-    if not data:
-        raise ValueError('File %s is empty!' % fn)
-    sha256sum = hashlib.sha256(data).hexdigest()
-    new_version[key] = (url, sha256sum)
-    
-        ie_htmls = []
-    for ie in youtube_dl.list_extractors(age_limit=None):
-        ie_html = '<b>{}</b>'.format(ie.IE_NAME)
-        ie_desc = getattr(ie, 'IE_DESC', None)
-        if ie_desc is False:
-            continue
-        elif ie_desc is not None:
-            ie_html += ': {}'.format(ie.IE_DESC)
-        if not ie.working():
-            ie_html += ' (Currently broken)'
-        ie_htmls.append('<li>{}</li>'.format(ie_html))
-    
-    import io
-import optparse
-import re
-    
-    lazy_extractors_filename = sys.argv[1]
-if os.path.exists(lazy_extractors_filename):
-    os.remove(lazy_extractors_filename)
-    
-    # The master toctree document.
-master_doc = 'index'
+    dynamodb                                 | dynamodbstreams
+ec2                                      | ecr
+'''
     
     
-class TestAgeRestriction(unittest.TestCase):
-    def _assert_restricted(self, url, filename, age, old_age=None):
-        self.assertTrue(_download_restricted(url, filename, old_age))
-        self.assertFalse(_download_restricted(url, filename, age))
+no_such_subcommand_old = '''No such subcommand
     
-        def tearDown(self):
-        if os.path.exists(self.test_dir):
-            shutil.rmtree(self.test_dir)
+        def __eq__(self, other):
+        return (
+            isinstance(other, self.__class__) and
+            self.keys == other.keys and
+            self.messages == other.messages and
+            self.strict == other.strict
+        )
     
-                for tc_num, tc in enumerate(test_cases):
-                tc_res_dict = res_dict['entries'][tc_num]
-                # First, check test cases' data against extracted data alone
-                expect_info_dict(self, tc_res_dict, tc.get('info_dict', {}))
-                # Now, check downloaded file consistency
-                tc_filename = get_tc_filename(tc)
-                if not test_case.get('params', {}).get('skip_download', False):
-                    self.assertTrue(os.path.exists(tc_filename), msg='Missing file ' + tc_filename)
-                    self.assertTrue(tc_filename in finished_hook_called)
-                    expected_minsize = tc.get('file_minsize', 10000)
-                    if expected_minsize is not None:
-                        if params.get('test'):
-                            expected_minsize = max(expected_minsize, 10000)
-                        got_fsize = os.path.getsize(tc_filename)
-                        assertGreaterEqual(
-                            self, got_fsize, expected_minsize,
-                            'Expected %s to be at least %s, but it\'s only %s ' %
-                            (tc_filename, format_bytes(expected_minsize),
-                                format_bytes(got_fsize)))
-                    if 'md5' in tc:
-                        md5_for_file = _file_md5(tc_filename)
-                        self.assertEqual(tc['md5'], md5_for_file)
-                # Finally, check test cases' data again but this time against
-                # extracted data from info JSON file written during processing
-                info_json_fn = os.path.splitext(tc_filename)[0] + '.info.json'
-                self.assertTrue(
-                    os.path.exists(info_json_fn),
-                    'Missing info file %s' % info_json_fn)
-                with io.open(info_json_fn, encoding='utf-8') as infof:
-                    info_dict = json.load(infof)
-                expect_info_dict(self, info_dict, tc.get('info_dict', {}))
-        finally:
-            try_rm_tcs_files()
-            if is_playlist and res_dict is not None and res_dict.get('entries'):
-                # Remove all other files that may have been extracted if the
-                # extractor returns full results even with extract_flat
-                res_tcs = [{'info_dict': e} for e in res_dict['entries']]
-                try_rm_tcs_files(res_tcs)
-    
-                    m = re.search(r'(?<=\s)u[\''](?!\)|,|$)', code)
-                if m is not None:
-                    self.assertTrue(
-                        m is None,
-                        'u present in %s, around %s' % (
-                            fn, code[m.start() - 10:m.end() + 10]))
-    
-        <command> <path>
-    
-        def verify_get_called(self):
-        self.assertTrue(self.handler.get_called)
-    
-            class Foo(unittest.TestCase):
-            def test_2(self): pass
-            def test_3(self): pass
-            def test_1(self): pass
-    
-    PYTHON3_OSERROR_EXCEPTIONS = (
-    'BrokenPipeError',
-    'ChildProcessError',
-    'ConnectionAbortedError',
-    'ConnectionError',
-    'ConnectionRefusedError',
-    'ConnectionResetError',
-    'FileExistsError',
-    'FileNotFoundError',
-    'InterruptedError',
-    'IsADirectoryError',
-    'NotADirectoryError',
-    'PermissionError',
-    'ProcessLookupError',
-    'TimeoutError',
-)
+    import pytest
     
     
-def test_main():
-    support.run_unittest(
-        BZ2FileTest,
-        BZ2CompressorTest,
-        BZ2DecompressorTest,
-        CompressDecompressTest,
-        OpenTest,
+def info():
+    '''Generate information for a bug report.'''
+    try:
+        platform_info = {
+            'system': platform.system(),
+            'release': platform.release(),
+        }
+    except IOError:
+        platform_info = {
+            'system': 'Unknown',
+            'release': 'Unknown',
+        }
+    
+    # TODO: response is the only one
+    
+    
+    {    # Server Error.
+    500: ('internal_server_error', 'server_error', '/o\\', '✗'),
+    501: ('not_implemented',),
+    502: ('bad_gateway',),
+    503: ('service_unavailable', 'unavailable'),
+    504: ('gateway_timeout',),
+    505: ('http_version_not_supported', 'http_version'),
+    506: ('variant_also_negotiates',),
+    507: ('insufficient_storage',),
+    509: ('bandwidth_limit_exceeded', 'bandwidth'),
+    510: ('not_extended',),
+    511: ('network_authentication_required', 'network_auth', 'network_authentication'),
+}
+    
+    # One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [(master_doc, 'requests', u'Requests Documentation', [author], 1)]
+    
+        create_snapshot(
+        module=module,
+        state=state,
+        description=description,
+        wait=wait,
+        wait_timeout=wait_timeout,
+        ec2=ec2,
+        volume_id=volume_id,
+        instance_id=instance_id,
+        snapshot_id=snapshot_id,
+        device_name=device_name,
+        snapshot_tags=snapshot_tags,
+        last_snapshot_min_age=last_snapshot_min_age
     )
-    support.reap_children()
     
-    # Add the html version.  This converts the message into a multipart/alternative
-# container, with the original text message as the first part and the new html
-# message as the second part.
-asparagus_cid = make_msgid()
-msg.add_alternative('''\
-<html>
-  <head></head>
-  <body>
-    <p>Salut!</p>
-    <p>Cela ressemble à un excellent
-        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
-            recipie
-        </a> déjeuner.
-    </p>
-    <img src='cid:{asparagus_cid}' />
-  </body>
-</html>
-'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
-# note that we needed to peel the <> off the msgid for use in the html.
     
-        @classmethod
-    def _from_sequence(cls, scalars, dtype=None, copy=False):
-        return cls(scalars)
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            auth_token=dict(
+                type='str',
+                default=os.environ.get('ONEANDONE_AUTH_TOKEN')),
+            api_url=dict(
+                type='str',
+                default=os.environ.get('ONEANDONE_API_URL')),
+            name=dict(type='str'),
+            firewall_policy=dict(type='str'),
+            description=dict(type='str'),
+            rules=dict(type='list', default=[]),
+            add_server_ips=dict(type='list', default=[]),
+            remove_server_ips=dict(type='list', default=[]),
+            add_rules=dict(type='list', default=[]),
+            remove_rules=dict(type='list', default=[]),
+            wait=dict(type='bool', default=True),
+            wait_timeout=dict(type='int', default=600),
+            wait_interval=dict(type='int', default=5),
+            state=dict(type='str', default='present', choices=['present', 'absent', 'update']),
+        ),
+        supports_check_mode=True
+    )
     
-        quat = np.empty(euler.shape[:-1] + (4,), dtype=np.float64)
-    quat[..., 0] = cj * cc + sj * ss
-    quat[..., 3] = cj * sc - sj * cs
-    quat[..., 2] = -(cj * ss + sj * cc)
-    quat[..., 1] = cj * cs - sj * sc
-    return quat
+            for _port in ports:
+            monitoring_policy_port = oneandone.client.Port(
+                protocol=_port['protocol'],
+                port=_port['port'],
+                alert_if=_port['alert_if'],
+                email_notification=_port['email_notification']
+            )
+            monitoring_policy_ports.append(monitoring_policy_port)
     
-        The roulette wheel has 37 spots. If the bet is 0 and a 0 comes up,
-    you win a reward of 35. If the parity of your bet matches the parity
-    of the spin, you win 1. Otherwise you receive a reward of -1.
     
-        - register() themselves and save the returned ID
-    - unregister() themselves upon close()
-    - include a __del__ method which close()'s the object
-    '''
+class DNSZoneIPAClient(IPAClient):
+    def __init__(self, module, host, port, protocol):
+        super(DNSZoneIPAClient, self).__init__(module, host, port, protocol)
     
-    # TODO: don't hardcode sizeof_int here
-def _bigint_from_bytes(bytes):
-    sizeof_int = 4
-    padding = sizeof_int - len(bytes) % sizeof_int
-    bytes += b'\0' * padding
-    int_count = int(len(bytes) / sizeof_int)
-    unpacked = struct.unpack('{}I'.format(int_count), bytes)
-    accum = 0
-    for i, val in enumerate(unpacked):
-        accum += 2 ** (sizeof_int * 8 * i) * val
-    return accum
+    import traceback
     
-    from gym import error
-from gym.utils import atomic_write
-from gym.utils.json_utils import json_encode_np
+        module = AnsibleModule(
+        argument_spec=dict(
+            component=dict(required=True, aliases=['name']),
+            version=dict(required=True),
+            token=dict(required=True, no_log=True),
+            state=dict(required=True, choices=['started', 'finished', 'failed']),
+            hosts=dict(required=False, default=[socket.gethostname()], aliases=['host']),
+            env=dict(required=False),
+            owner=dict(required=False),
+            description=dict(required=False),
+            message=dict(required=False),
+            source_system=dict(required=False, default='ansible'),
+            validate_certs=dict(default='yes', type='bool'),
+            url=dict(required=False, default='https://api.bigpanda.io'),
+        ),
+        supports_check_mode=True,
+    )
+    
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+    
+    current_path = os.path.dirname(os.path.abspath(__file__))
+root_path = os.path.abspath(os.path.join(current_path, os.pardir, os.pardir, os.pardir))
+data_path = os.path.abspath(os.path.join(root_path, os.pardir, os.pardir, 'data', 'gae_proxy'))
+if not os.path.isdir(data_path):
+    data_path = current_path
+    
+    import os, json, imp
+here = os.path.abspath(os.path.dirname(__file__))
+proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
+try:
+    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
+except:
+    README = ''
+CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
+VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
+    
+    
+def icourses_playlist_new(url, page=None):
+    # 2 helpers using same interface in the js code
+    def to_chap(course_id, chap_id, mod):
+        ep = 'http://www.icourses.cn/jpk/viewCharacterDetail2.action?courseId={}&characId={}&mod={}'
+        req = post_content(ep.format(course_id, chap_id, mod), post_data={})
+        return req
+    
+            # cookie handler
+        ssl_context = request.HTTPSHandler(
+            context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
+        cookie_handler = request.HTTPCookieProcessor()
+        opener = request.build_opener(ssl_context, cookie_handler)
+        opener.addheaders = [
+            ('Referer', self.url),
+            ('Cookie',
+             'CloudFront-Policy=%s;CloudFront-Signature=%s;CloudFront-Key-Pair-Id=%s' % (scp, scs, sck))
+        ]
+        request.install_opener(opener)
