e933cc5a823ca9470f66fedb7ff4e54c84390fd9
SUNsung <weatherius@gmail.com>
2019-06-17 23:08:45 +0300

    Rewrite [57bVX3eG4wGjs571yf6ldkl8WM57Rdq9Nbtsdms57H1g.rb] File in: 2019-06-17 23:08:44

--- ruby/57bVX3eG4wGjs571yf6ldkl8WM57Rdq9Nbtsdms57H1g.rb
+++ ruby/57bVX3eG4wGjs571yf6ldkl8WM57Rdq9Nbtsdms57H1g.rb
@@ -1,143 +1,141 @@
 
         
-            # test only
-    def stopped?
-      !@thread&.alive?
-    end
+            self.last_flush = Time.now.utc
     
-      def initialize(app, options = nil)
-    @app = app
-    if GlobalSetting.enable_cors && GlobalSetting.cors_origin.present?
-      @global_origins = GlobalSetting.cors_origin.split(',').map(&:strip)
+          if period_type == :all
+        DB.exec <<~SQL
+          UPDATE user_stats s
+          SET likes_given         = d.likes_given,
+              likes_received      = d.likes_received,
+              topic_count         = d.topic_count,
+              post_count          = d.post_count
+    
+    class ContentSecurityPolicy
+  class Default
+    attr_reader :directives
+    
+          SiteSetting.content_security_policy_collect_reports = false
+      report_uri = parse(policy)['report-uri']
+      expect(report_uri).to eq(nil)
     end
   end
     
-      def self.cleanup!
-    DB.exec(<<~SQL)
-      DELETE FROM drafts
-       WHERE sequence < (
-        SELECT MAX(s.sequence)
-          FROM draft_sequences s
-         WHERE s.draft_key = drafts.draft_key
-           AND s.user_id = drafts.user_id
-      )
-    SQL
-    
-          @directives[directive].delete(:none) if @directives[directive].count > 1
+        def ==(other)
+      other.name == name && other.path == path && other.type == type
     end
     
-              extension[directive] ||= []
-          extension[directive] << source
-        end
+        def error?
+      code == 0 || code != 404 && code != 403 && code >= 400 && code <= 599
+    end
+    
+          def inherited(subclass)
+        super
+    
+          def mod
+        return @mod if defined?(@mod)
+        @mod = slug[/api\/([\w\-\.]+)\//, 1]
+        @mod.remove! 'angular2.' if @mod
+        @mod
       end
     end
   end
 end
 
     
-          theme.destroy!
-    
-        cookies.delete('authentication_data')
-    cookies.delete(TOKEN_COOKIE)
-  end
-    
-            # Executes a command and returns true if the command succeeded,
-        # and false otherwise. By default, this executes as a normal user,
-        # and it is up to the communicator implementation if they expose an
-        # option for running tests as an administrator.
-        #
-        # @see #execute
-        def test(command, opts=nil)
-        end
-      end
+        # The path used after resending confirmation instructions.
+    def after_resending_confirmation_instructions_path_for(resource_name)
+      is_navigational_format? ? new_session_path(resource_name) : '/'
     end
-  end
-end
-
     
-              # Persist through the set of invalid methods
-          this_invalid  = @__invalid_methods || Set.new
-          other_invalid = other.instance_variable_get(:'@__invalid_methods') || Set.new
-          result.instance_variable_set(:'@__invalid_methods', this_invalid + other_invalid)
+        def reset_password_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :reset_password_instructions, opts)
+    end
     
-            # This returns all registered pushes.
-        #
-        # @return [Registry]
-        def pushes
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.pushes)
-            end
-          end
-        end
+          def remember_cookie_values(resource)
+        options = { httponly: true }
+        options.merge!(forget_cookie_values(resource))
+        options.merge!(
+          value: resource.class.serialize_into_cookie(resource),
+          expires: resource.remember_expires_at
+        )
+      end
     
-            # This should return an action callable for the given name.
-        #
-        # @param [Symbol] name Name of the action.
-        # @return [Object] A callable action sequence object, whether it
-        #   is a proc, object, etc.
-        def action(name)
-          nil
-        end
-    
-      # An irb inspector
-  #
-  # In order to create your own custom inspector there are two things you
-  # should be aware of:
-  #
-  # Inspector uses #inspect_value, or +inspect_proc+, for output of return values.
-  #
-  # This also allows for an optional #init+, or +init_proc+, which is called
-  # when the inspector is activated.
-  #
-  # Knowing this, you can create a rudimentary inspector as follows:
-  #
-  #     irb(main):001:0> ins = IRB::Inspector.new(proc{ |v| 'omg! #{v}' })
-  #     irb(main):001:0> IRB.CurrentContext.inspect_mode = ins # => omg! #<IRB::Inspector:0x007f46f7ba7d28>
-  #     irb(main):001:0> 'what?' #=> omg! what?
-  #
-  class Inspector
-    # Default inspectors available to irb, this includes:
+        # Creates configuration values for Devise and for the given module.
     #
-    # +:pp+::       Using Kernel#pretty_inspect
-    # +:yaml+::     Using YAML.dump
-    # +:marshal+::  Using Marshal.dump
-    INSPECTORS = {}
+    #   Devise::Models.config(Devise::Models::DatabaseAuthenticatable, :stretches)
+    #
+    # The line above creates:
+    #
+    #   1) An accessor called Devise.stretches, which value is used by default;
+    #
+    #   2) Some class methods for your model Model.stretches and Model.stretches=
+    #      which have higher priority than Devise.stretches;
+    #
+    #   3) And an instance method stretches.
+    #
+    # To add the class methods you need to have a module ClassMethods defined
+    # inside the given class.
+    #
+    def self.config(mod, *accessors) #:nodoc:
+      class << mod; attr_accessor :available_configs; end
+      mod.available_configs = accessors
     
-    # File.open('ao.ppm', 'w') do |fp|
-  # $fp = fp
-  printf('P6\n')
-  printf('%d %d\n', IMAGE_WIDTH, IMAGE_HEIGHT)
-  printf('255\n')
-  Scene.new.render(IMAGE_WIDTH, IMAGE_HEIGHT, NSUBSAMPLES)
-# end
-    
-      config.vm.define 'freebsd10' do |freebsd10|
-    freebsd10.vm.box = 'tjay/freebsd-10.1'
-  end
-    
-    # Add our files (should be in the current directory):
-package.input('my-executable=/usr/bin/')
-package.input('my-library.so=/usr/lib/')
-    
-        logger.info('Installing pear package', :package => input_package,
-                  :directory => staging_path)
-    ::Dir.chdir(staging_path) do
-      safesystem('pear', '-c', config, 'install', '-n', '-f', input_package)
-    end
-    
-        cleanup_staging
-    # Tell 'dir' to input '.' and chdir/prefix will help it figure out the
-    # rest.
-    dir.input('.')
-    @staging_path = dir.staging_path
-    dir.cleanup_build
-  end # def input
-    
-        task(options.name) do |_, task_args|
-      block.call(*[options, task_args].first(block.arity)) if block_given?
-      abort('Must specify args') unless options.respond_to?(:args)
-      @args = options.delete_field(:args)
-      run_cli
+        def str_to_byte_pos(pos)
+      @s.string.slice(0, pos).bytesize
     end
   end
+end
+    
+      # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
+    
+    describe ConversationsController, :type => :controller do
+  describe '#index' do
+    before do
+      @person = alice.contacts.first.person
+      hash = {
+        :author => @person,
+        :participant_ids => [alice.person.id, @person.id],
+        :subject => 'not spam',
+        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
+      }
+      @conv1 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+    
+      describe '#new' do
+    before do
+      sign_in alice, scope: :user
+    end
+    
+          it 'should not create the participation' do
+        post :create, params: {post_id: @post.id}
+        expect(alice.participations.where(:target_id => @post.id)).not_to exist
+        expect(response.code).to eq('403')
+      end
+    end
+  end
+    
+              expect(gon['preloads'][:mentioned_person][:name]).to eq(alice.person.name)
+          expect(gon['preloads'][:mentioned_person][:handle]).to eq(alice.person.diaspora_handle)
+        end
+      end
+    end
+    
+            self.arguments = [
+          CLAide::Argument.new('NAME', false),
+        ]
+    
+      subject { instance }
+    
+        context '$SHELL is not set' do
+      before do
+        allow(ENV).to receive(:[]).with('SHELL') { nil }
+      end
+    
+        context 'nested collections' do
+      let(:command1) { 'cd /tmp/' }
+      let(:command2) { 'ls' }
