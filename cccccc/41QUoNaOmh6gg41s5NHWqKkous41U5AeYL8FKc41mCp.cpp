ecf1e09bd5e58f42f125c81765d14f9f6f17fc73
SUNsung <weatherius@gmail.com>
2019-06-17 13:36:30 +0300

    Rewrite [41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp] File in: 2019-06-17 13:36:30

--- cccccc/41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp
+++ cccccc/41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp
@@ -1,118 +1,57 @@
 
         
-        #include <google/protobuf/compiler/command_line_interface.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/io/printer.h>
+        
+bool ZeroCopyOutputStream::WriteAliasedRaw(const void* /* data */,
+                                           int /* size */) {
+  GOOGLE_LOG(FATAL) << 'This ZeroCopyOutputStream doesn't support aliasing. '
+                'Reaching here usually means a ZeroCopyOutputStream '
+                'implementation bug.';
+  return false;
+}
     
-    #include <ostream>
-#include <stdio.h>
-#include <string>
-#include <utility>
+    const Status Status::OK = Status();
+const Status Status::CANCELLED = Status(error::CANCELLED, '');
+const Status Status::UNKNOWN = Status(error::UNKNOWN, '');
     
-      virtual bool Generate(const FileDescriptor* file,
-                        const string& parameter,
-                        GeneratorContext* context,
-                        string* error) const {
-    FileDescriptorProto new_file;
-    file->CopyTo(&new_file);
-    SchemaGroupStripper::StripFile(file, &new_file);
+    namespace google {
+namespace protobuf {
+namespace internal {
+namespace {
+    }
+    }
+    }
+    }
+    
+    #endif  // GOOGLE_PROTOBUF_TEMPLATE_UTIL_H_
+
+    
+    
+    {  // Yes || Yes == true.
+  value = or_<true_, true_>::value;
+  EXPECT_TRUE(value);
+  // Yes || No == true.
+  value = or_<true_, false_>::value;
+  EXPECT_TRUE(value);
+  // No || Yes == true.
+  value = or_<false_, true_>::value;
+  EXPECT_TRUE(value);
+  // No || No == false.
+  value = or_<false_, false_>::value;
+  EXPECT_FALSE(value);
+}
+    
+    namespace google {
+namespace protobuf {
+namespace util {
+    }
+    }
     }
     
       {
-    // Write the new address book back to disk.
-    fstream output(argv[1], ios::out | ios::trunc | ios::binary);
-    if (!address_book.SerializeToOstream(&output)) {
-      cerr << 'Failed to write address book.' << endl;
+    // Read the existing address book.
+    fstream input(argv[1], ios::in | ios::binary);
+    if (!address_book.ParseFromIstream(&input)) {
+      cerr << 'Failed to parse address book.' << endl;
       return -1;
     }
   }
-    
-    #endif  // GOOGLE_PROTOBUF_COMPILER_OBJECTIVEC_ENUM_H__
-
-    
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-    
-    
-    {  Byte t1(bytes + 5);
-  uint32_t t = t1.get_byte(6, 2);
-  x <<= 2;
-  x |= t;
-  double ret = x * CLUSTER_VREL_RES + CLUSTER_VREL_LONG_MIN;
-  return ret;
-}
-    
-    TEST_F(SpeedLimitTest, GetSpeedLimitByS) {
-  EXPECT_EQ(speed_limit_.speed_limit_points().size(), 100);
-  double s = 0.0;
-  const double ds = 0.01;
-  while (s < 99.0) {
-    double v_limit = speed_limit_.GetSpeedLimitByS(s);
-    }
-    }
-    
-      MatrixXd mat_golden(20, 10);
-  // clang-format off
-  mat_golden <<
-   -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0, -1,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  1,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0, -1,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  1,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0, -1,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  1,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0, -1,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  1,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0, -1,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  1,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0, -1,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  1,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0, -1,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  1,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0, -1,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  1,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-      kernel.AddSecondOrderDerivativeMatrix(init_derivative, 1.0);
-    
-    void SplineSegKernel::CalculateDerivative(const uint32_t num_params) {
-  kernel_derivative_ = Eigen::MatrixXd::Zero(num_params, num_params);
-  for (int r = 1; r < kernel_derivative_.rows(); ++r) {
-    for (int c = 1; c < kernel_derivative_.cols(); ++c) {
-      kernel_derivative_(r, c) = r * c / (r + c - 1.0);
-    }
-  }
-}
-    
-    namespace apollo {
-namespace canbus {
-namespace gem {
-    }
-    }
-    }
-    
-      Brakerpt6c brake;
-  brake.Parse(bytes, length, &chassis_detail);
-    
-    
-    {
-    {
-    {
-    {  Headlight_rpt_77::Commanded_valueType ret =
-      static_cast<Headlight_rpt_77::Commanded_valueType>(x);
-  return ret;
-}
-}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
-
-    
-    
-    {  Horn_rpt_79::Output_valueType ret =
-      static_cast<Horn_rpt_79::Output_valueType>(x);
-  return ret;
-}
