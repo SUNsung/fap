b8d1e6c03306af3290abe420fe3c884a4d74e72b
SUNsung <weatherius@gmail.com>
2019-06-17 17:57:36 +0300

    Rewrite [18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py] File in: 2019-06-17 17:57:35

--- pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
+++ pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
@@ -1,244 +1,33 @@
 
         
-        
-class ArrayMinLengthValidator(MinLengthValidator):
-    message = ngettext_lazy(
-        'List contains %(show_value)d item, it should contain no fewer than %(limit_value)d.',
-        'List contains %(show_value)d items, it should contain no fewer than %(limit_value)d.',
-        'limit_value')
+            plt.clf()
+    xx = range(0, n * step, step)
+    plt.title('Lasso regression on sample dataset (%d features)' % n_features)
+    plt.plot(xx, scikit_results, 'b-', label='scikit-learn')
+    plt.plot(xx, glmnet_results, 'r-', label='glmnet')
+    plt.legend()
+    plt.xlabel('number of samples to classify')
+    plt.ylabel('Time (s)')
+    plt.show()
     
+            start = time.time()
+        func(X, n_jobs=-1)
+        multi_core.append(time.time() - start)
     
-class BaseSessionManager(models.Manager):
-    def encode(self, session_dict):
-        '''
-        Return the given session dictionary serialized and encoded as a string.
-        '''
-        session_store_class = self.model.get_session_store_class()
-        return session_store_class().encode(session_dict)
+    Second example
+--------------
+The second example shows the ability of the Minimum Covariance Determinant
+robust estimator of covariance to concentrate on the main mode of the data
+distribution: the location seems to be well estimated, although the covariance
+is hard to estimate due to the banana-shaped distribution. Anyway, we can
+get rid of some outlying observations.
+The One-Class SVM is able to capture the real data structure, but the
+difficulty is to adjust its kernel bandwidth parameter so as to obtain
+a good compromise between the shape of the data scatter matrix and the
+risk of over-fitting the data.
     
-        The Django sessions framework is entirely cookie-based. It does
-    not fall back to putting session IDs in URLs. This is an intentional
-    design decision. Not only does that behavior make URLs ugly, it makes
-    your site vulnerable to session-ID theft via the 'Referer' header.
-    
-        lastmod = None
-    all_sites_lastmod = True
-    urls = []
-    for site in maps:
-        try:
-            if callable(site):
-                site = site()
-            urls.extend(site.get_urls(page=page, site=req_site,
-                                      protocol=req_protocol))
-            if all_sites_lastmod:
-                site_lastmod = getattr(site, 'latest_lastmod', None)
-                if site_lastmod is not None:
-                    site_lastmod = (
-                        site_lastmod.utctimetuple() if isinstance(site_lastmod, datetime.datetime)
-                        else site_lastmod.timetuple()
-                    )
-                    lastmod = site_lastmod if lastmod is None else max(lastmod, site_lastmod)
-                else:
-                    all_sites_lastmod = False
-        except EmptyPage:
-            raise Http404('Page %s empty' % page)
-        except PageNotAnInteger:
-            raise Http404('No page '%s'' % page)
-    response = TemplateResponse(request, template_name, {'urlset': urls},
-                                content_type=content_type)
-    if all_sites_lastmod and lastmod is not None:
-        # if lastmod is defined for all sites, set header so as
-        # ConditionalGetMiddleware is able to send 304 NOT MODIFIED
-        response['Last-Modified'] = http_date(timegm(lastmod))
-    return response
-
-    
-    ROOT = Path(__file__).parent
-    
-    
-@pytest.fixture
-def httpbin_secure(httpbin_secure):
-    return prepare_url(httpbin_secure)
-
-    
-            except MaxRetryError as e:
-            if isinstance(e.reason, ConnectTimeoutError):
-                # TODO: Remove this in 3.0.0: see #2811
-                if not isinstance(e.reason, NewConnectionError):
-                    raise ConnectTimeout(e, request=request)
-    
-        expires = None
-    if morsel['max-age']:
-        try:
-            expires = int(time.time() + int(morsel['max-age']))
-        except ValueError:
-            raise TypeError('max-age: %s must be integer' % morsel['max-age'])
-    elif morsel['expires']:
-        time_template = '%a, %d-%b-%Y %H:%M:%S GMT'
-        expires = calendar.timegm(
-            time.strptime(morsel['expires'], time_template)
-        )
-    return create_cookie(
-        comment=morsel['comment'],
-        comment_url=bool(morsel['comment']),
-        discard=False,
-        domain=morsel['domain'],
-        expires=expires,
-        name=morsel.key,
-        path=morsel['path'],
-        port=None,
-        rest={'HttpOnly': morsel['httponly']},
-        rfc2109=False,
-        secure=bool(morsel['secure']),
-        value=morsel.value,
-        version=morsel['version'] or 0,
-    )
-    
-            # It's possible that users might accidentally send a Request object.
-        # Guard against that specific failure case.
-        if isinstance(request, Request):
-            raise ValueError('You can only send PreparedRequests.')
-    
-            if is_ipv4_address(parsed.hostname):
-            for proxy_ip in no_proxy:
-                if is_valid_cidr(proxy_ip):
-                    if address_in_network(parsed.hostname, proxy_ip):
-                        return True
-                elif parsed.hostname == proxy_ip:
-                    # If no_proxy ip was defined in plain IP notation instead of cidr notation &
-                    # matches the IP of the index
-                    return True
-        else:
-            host_with_port = parsed.hostname
-            if parsed.port:
-                host_with_port += ':{}'.format(parsed.port)
-    
-        :param url: URL for the new :class:`Request` object.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    
-            if now - self.lastmark >= 3:
-            self.lastmark = now
-            qps = len(self.tail) / sum(self.tail)
-            print('samplesize={0} concurrent={1} qps={2:0.2f}'.format(len(self.tail), self.concurrent, qps))
-    
-    from scrapy.utils.conf import arglist_to_dict
-from scrapy.exceptions import UsageError
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
-                          help='set spider argument (may be repeated)')
-        parser.add_option('-o', '--output', metavar='FILE',
-                          help='dump scraped items into FILE (use - for stdout)')
-        parser.add_option('-t', '--output-format', metavar='FORMAT',
-                          help='format to use for dumping items with -o')
-    
-            if not aws_access_key_id:
-            aws_access_key_id = settings['AWS_ACCESS_KEY_ID']
-        if not aws_secret_access_key:
-            aws_secret_access_key = settings['AWS_SECRET_ACCESS_KEY']
-    
-        def __init__(self, message, severity=SEVERITY_CRITICAL, url=None):
-        assert severity in self.SEVERITY_LEVELS
-        self.message = six.text_type(message)
-        self.severity = severity
-        self.url = url
-    
-            # Adding index on 'TagValue', fields ['project_id', '_key', 'last_seen']
-        db.create_index(u'tagstore_tagvalue', ['project_id', 'key', 'last_seen'])
-    
-            # Removing unique constraint on 'TagKey', fields ['project_id', 'environment_id', 'key']
-        db.delete_unique(u'tagstore_tagkey', ['project_id', 'environment_id', 'key'])
-    
-    
-class Migration(SchemaMigration):
-    
-    
-@instrumented_task(name='sentry.tasks.clear_expired_resolutions',
-                   time_limit=15, soft_time_limit=10)
-def clear_expired_resolutions(release_id):
-    '''
-    This should be fired when ``release_id`` is created, and will indicate to
-    the system that any pending resolutions older than the given release can now
-    be safely transitioned to resolved.
-    
-    logger = logging.getLogger('sentry.scheduler')
-    
-    
-if __name__ == '__main__':
-    
-    
-import numpy as np
-    
-        # 计算forward值
-    x, d = data_set()
-    rl.forward(x[0])
-    rl.forward(x[1])
-    
-    # 求取sensitivity map
-    sensitivity_array = np.ones(rl.state_list[-1].shape,
-                                dtype=np.float64)
-    # 计算梯度
-    rl.backward(sensitivity_array, IdentityActivator())
-    
-    # 检查梯度
-    epsilon = 10e-4
-    for i in range(rl.W.shape[0]):
-        for j in range(rl.W.shape[1]):
-            rl.W[i,j] += epsilon
-            rl.reset_state()
-            rl.forward(x[0])
-            rl.forward(x[1])
-            err1 = error_function(rl.state_list[-1])
-            rl.W[i,j] -= 2*epsilon
-            rl.reset_state()
-            rl.forward(x[0])
-            rl.forward(x[1])
-            err2 = error_function(rl.state_list[-1])
-            expect_grad = (err1 - err2) / (2 * epsilon)
-            rl.W[i,j] += epsilon
-            print('weights(%d,%d): expected - actural %f - %f' % (
-                i, j, expect_grad, rl.gradient[i,j]))
-    
-    # 转化关系， 数组转化为矩阵
-randMat = mat(randArray)
-'''
-.I 表示对矩阵求逆(可以利用矩阵的初等变换)
-   意义：逆矩阵是一个判断相似性的工具。逆矩阵A与列向量p相乘后，将得到列向量q，q的第i个分量表示p与A的第i个列向量的相似度。
-   参考案例链接：
-   https://www.zhihu.com/question/33258489
-   http://blog.csdn.net/vernice/article/details/48506027
-.T 表示对矩阵转置(行列颠倒)
-    * 等同于: .transpose()
-.A 返回矩阵基于的数组
-    参考案例链接：
-    http://blog.csdn.net/qq403977698/article/details/47254539
-'''
-invRandMat = randMat.I
-TraRandMat = randMat.T
-ArrRandMat = randMat.A
-# 输出结果
-print('randArray=(%s) \n' % type(randArray), randArray)
-print('randMat=(%s) \n' % type(randMat), randMat)
-print('invRandMat=(%s) \n' % type(invRandMat), invRandMat)
-print('TraRandMat=(%s) \n' % type(TraRandMat), TraRandMat)
-print('ArrRandMat=(%s) \n' % type(ArrRandMat), ArrRandMat)
-# 矩阵和逆矩阵 进行求积 (单位矩阵，对角线都为1嘛，理论上4*4的矩阵其他的都为0)
-myEye = randMat*invRandMat
-# 误差
-print(myEye - eye(4))
-    
-            for index in self.dataList:
-            wDelta += float(labels[index]) * X[index, :]  # wDelta += label*dataSet
-        eta = 1.0/(2.0*self.t)       # calc new: eta
-        # calc new: w = (1.0 - 1/t)*w + (eta/k)*wDelta
-        wMat = (1.0 - 1.0/self.t)*wMat + (eta/self.k)*wDelta
-        for mapperNum in range(1, self.numMappers+1):
-            yield (mapperNum, ['w', wMat.tolist()[0]])    # 发出 w
-            if self.t < self.options.iterations:
-                yield (mapperNum, ['t', self.t+1])        # 增量 T
-                for j in range(self.k/self.numMappers):   # emit random ints for mappers iid
-                    yield (mapperNum, ['x', random.randint(shape(self.data)[0])])
+    import numpy as np
+import matplotlib.pyplot as plt
+# Though the following import is not directly being used, it is required
+# for 3D projection to work
+from mpl_toolkits.mplot3d import Axes3D
