dab000ff9da8b2de87e850b76d392ed264270ff9
SUNsung <weatherius@gmail.com>
2019-06-18 22:28:37 +0300

    Rewrite [27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py] File in: 2019-06-18 22:28:36

--- pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
+++ pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
@@ -1,77 +1,157 @@
 
         
-        index_title = 0
-index_desc = 1
-index_auth = 2
-index_https = 3
-index_cors = 4
-index_link = 5
-num_segments = 5
+          Args:
+    model: The currently trained path-based model.
+    session: The current TensorFlow session.
+    instances: The current set instances.
+    labels: The current set labels.
+    set_name: The current set name (train/validation/test).
+    classes: The class label names.
     
-    # One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'face_recognition',
-     u'Face Recognition Documentation',
-     [u'Adam Geitgey'], 1)
-]
-    
-    
-def run_test(setup, test, iterations_per_test=5, tests_to_run=10):
-    fastest_execution = min(timeit.Timer(test, setup=setup).repeat(tests_to_run, iterations_per_test))
-    execution_time = fastest_execution / iterations_per_test
-    fps = 1.0 / execution_time
-    return execution_time, fps
-    
-    known_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
-]
-    
-        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
-    context = multiprocessing
-    if 'forkserver' in multiprocessing.get_all_start_methods():
-        context = multiprocessing.get_context('forkserver')
-    
-    while True:
-    print('Capturing image.')
-    # Grab a single frame of video from the RPi camera as a numpy array
-    camera.capture(output, format='rgb')
-    
-    import face_recognition
-import picamera
-import numpy as np
+      Args:
+    data_fname: The filename of the file from which to read the data.
+  Returns:
+    A dictionary whose keys will vary depending on dataset (but should
+    always contain the keys 'train_data' and 'valid_data') and whose
+    values are numpy arrays.
+  '''
     
     
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+def _DumpSentenceEmbedding(sentence, vocab):
+  '''Predict next words using the given prefix words.
     
-    # It false, will not define \strong, \code, 	itleref, \crossref ... but only
-# \sphinxstrong, ..., \sphinxtitleref, ... To help avoid clash with user added
-# packages.
-#
-# latex_keep_old_macro_names = True
+      for _, value in gen_ngrams_dict.iteritems():
+    total_ngrams_produced += value
     
-    from homeassistant.core import HomeAssistant
-from homeassistant.util.temperature import convert as convert_temperature
-from homeassistant.const import PRECISION_HALVES, PRECISION_TENTHS
+        state_fwd = cell_fwd.zero_state(FLAGS.batch_size, tf.float32)
+    state_bwd = cell_bwd.zero_state(FLAGS.batch_size, tf.float32)
     
     
-def __feet_to_meters(feet: float) -> float:
-    '''Convert feet to meters.'''
-    return feet * 0.3048
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
+    proc.sendline(u'fish')
+    return proc
     
-    from homeassistant import const
+    Commands:
+   update - Retrieve new lists of packages
+   upgrade - Perform an upgrade
+   install - Install new packages (pkg is libc6 not libc6.deb)
+   remove - Remove packages
+   autoremove - Remove automatically all unused packages
+   purge - Remove packages and config files
+   source - Download source archives
+   build-dep - Configure build-dependencies for source packages
+   dist-upgrade - Distribution upgrade, see apt-get(8)
+   dselect-upgrade - Follow dselect selections
+   clean - Erase downloaded archive files
+   autoclean - Erase old downloaded archive files
+   check - Verify that there are no broken dependencies
+   changelog - Download and display the changelog for the given package
+   download - Download the binary package into the current directory
     
-        changed = True  # 标记样本类别是否改变
-    n_iter = 0  # 记录迭代次数
-    while changed and n_iter < max_iter:
-        changed = False
-        n_iter += 1
+    You can download from:
+  https://osxfuse.github.io/
+Error: An unsatisfied requirement failed this build.'''
     
-            return self._call(x)
+            assert certdata, 'cert file %r is broken' % certfile
+        import ctypes
+        import ctypes.wintypes
+        class CERT_CONTEXT(ctypes.Structure):
+            _fields_ = [
+                ('dwCertEncodingType', ctypes.wintypes.DWORD),
+                ('pbCertEncoded', ctypes.POINTER(ctypes.wintypes.BYTE)),
+                ('cbCertEncoded', ctypes.wintypes.DWORD),
+                ('pCertInfo', ctypes.c_void_p),
+                ('hCertStore', ctypes.c_void_p),]
+        X509_ASN_ENCODING = 0x1
+        CERT_STORE_ADD_ALWAYS = 4
+        CERT_STORE_PROV_SYSTEM = 10
+        CERT_STORE_OPEN_EXISTING_FLAG = 0x4000
+        CERT_SYSTEM_STORE_CURRENT_USER = 1 << 16
+        CERT_SYSTEM_STORE_LOCAL_MACHINE = 2 << 16
+        CERT_FIND_SUBJECT_STR = 8 << 16 | 7
+        crypt32 = ctypes.windll.crypt32
+        ca_exists = False
+        store_handle = None
+        pCertCtx = None
+        ret = 0
+        for store in (CERT_SYSTEM_STORE_LOCAL_MACHINE, CERT_SYSTEM_STORE_CURRENT_USER):
+            try:
+                store_handle = crypt32.CertOpenStore(CERT_STORE_PROV_SYSTEM, 0, None, CERT_STORE_OPEN_EXISTING_FLAG | store, u'root')
+                if not store_handle:
+                    if store == CERT_SYSTEM_STORE_CURRENT_USER and not ca_exists:
+                        xlog.warning('CertUtil.import_windows_ca failed: could not open system cert store')
+                        return False
+                    else:
+                        continue
+    
+        if sys.platform == 'win32':
+        win32_lib = os.path.abspath( os.path.join(python_path, 'lib', 'win32'))
+        sys.path.append(win32_lib)
+    elif sys.platform.startswith('linux'):
+        linux_lib = os.path.abspath( os.path.join(python_path, 'lib', 'linux'))
+        sys.path.append(linux_lib)
+    elif sys.platform == 'darwin':
+        darwin_lib = os.path.abspath( os.path.join(python_path, 'lib', 'darwin'))
+        sys.path.append(darwin_lib)
+        extra_lib = '/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python'
+        sys.path.append(extra_lib)
+    
+            Using setter/getter methods is deprecated. Use o.channel instead.'''
+    
+        def test_html_escape_on_error(self):
+        result = self.send_typical_request(
+            b'<script>alert('hello')</script> / HTTP/1.1')
+        result = b''.join(result)
+        text = '<script>alert('hello')</script>'
+        self.assertIn(html.escape(text, quote=False).encode('ascii'), result)
+    
+    def _make_failed_load_tests(name, exception, suiteClass):
+    message = 'Failed to call load_tests:\n%s' % (traceback.format_exc(),)
+    return _make_failed_test(
+        name, exception, suiteClass, message)
+    
+    
+def getatime(filename):
+    '''Return the last access time of a file, reported by os.stat().'''
+    return os.stat(filename).st_atime
+    
+        The decoding-related arguments have the same semantics as those of
+    bytes.decode().
+    '''
+    resource = _normalize_path(resource)
+    package = _get_package(package)
+    with open_text(package, resource, encoding, errors) as fp:
+        return fp.read()
+    
+        def testSeekBackwardsBytesIO(self):
+        with BytesIO(self.DATA) as bio:
+            with BZ2File(bio) as bz2f:
+                bz2f.read(500)
+                bz2f.seek(-150, 1)
+                self.assertEqual(bz2f.read(), self.TEXT[500-150:])
+    
+    
+def main():
+    import io
+    import pprint
+    
+    # Send the message via local SMTP server.
+with smtplib.SMTP('localhost') as s:
+    s.send_message(msg)
+
+    
+    def handleSlide(slide):
+    handleSlideTitle(slide.getElementsByTagName('title')[0])
+    handlePoints(slide.getElementsByTagName('point'))
+    
+            try:
+            print(pool.apply(f, (5,)))
+        except ZeroDivisionError:
+            print('\tGot ZeroDivisionError as expected from pool.apply()')
+        else:
+            raise AssertionError('expected ZeroDivisionError')
