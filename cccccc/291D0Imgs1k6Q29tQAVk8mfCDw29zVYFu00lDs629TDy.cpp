6df1c0ce9b19f920983d66f40776c185bdf14973
SUNsung <weatherius@gmail.com>
2019-06-18 08:26:20 +0300

    Rewrite [291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp] File in: 2019-06-18 08:26:20

--- cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
+++ cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
@@ -1,147 +1,285 @@
 
         
-        class OpenURIDialog : public QDialog
-{
-    Q_OBJECT
-    }
-    
-    double UniValue::get_real() const
-{
-    if (typ != VNUM)
-        throw std::runtime_error('JSON value is not a number as expected');
-    double retval;
-    if (!ParseDouble(getValStr(), &retval))
-        throw std::runtime_error('JSON double out of range');
-    return retval;
-}
-    
-        UniValue v2(UniValue::VSTR);
-    BOOST_CHECK(v2.isStr());
-    
-    #include <stdint.h>
-#include <stdlib.h>
-    
-    
-    {  // (e) Do a non-sync write; should fail
-  w.sync = false;
-  ASSERT_TRUE(!db_->Put(w, 'k3', 'v3').ok());
-  ASSERT_EQ('v1', Get('k1'));
-  ASSERT_EQ('NOT_FOUND', Get('k2'));
-  ASSERT_EQ('NOT_FOUND', Get('k3'));
-}
-    
-    // A comparator for internal keys that uses a specified comparator for
-// the user key portion and breaks ties by decreasing sequence number.
-class InternalKeyComparator : public Comparator {
- private:
-  const Comparator* user_comparator_;
- public:
-  explicit InternalKeyComparator(const Comparator* c) : user_comparator_(c) { }
-  virtual const char* Name() const;
-  virtual int Compare(const Slice& a, const Slice& b) const;
-  virtual void FindShortestSeparator(
-      std::string* start,
-      const Slice& limit) const;
-  virtual void FindShortSuccessor(std::string* key) const;
-    }
-    
-      Status Verify(int start_idx, int num_vals,
-                ExpectedVerifResult expected) const {
-    std::string val;
-    std::string value_space;
-    Status s;
-    for (int i = start_idx; i < start_idx + num_vals && s.ok(); i++) {
-      Value(i, &value_space);
-      s = ReadValue(i, &val);
-      if (expected == VAL_EXPECT_NO_ERROR) {
-        if (s.ok()) {
-          ASSERT_EQ(value_space, val);
-        }
-      } else if (s.ok()) {
-        fprintf(stderr, 'Expected an error at %d, but was OK\n', i);
-        s = Status::IOError(dbname_, 'Expected value error:');
-      } else {
-        s = Status::OK();  // An expected error
-      }
-    }
-    return s;
+          // Assignment releases the old value and acquires the new.
+  template <typename U> linked_ptr& operator=(linked_ptr<U> const& ptr) {
+    depart();
+    copy(&ptr);
+    return *this;
   }
     
-    // Return the name of a temporary file owned by the db named 'dbname'.
-// The result will be prefixed with 'dbname'.
-extern std::string TempFileName(const std::string& dbname, uint64_t number);
+    template<typename Dtype>
+void DelCaffeBlobs(std::vector< ::caffe::Blob<Dtype>*>* v, int n_num) {
+  for (index_t i=0; i < n_num; ++i)
+    delete v->at(i);
+}
     
-    void SimpleCSRSource::LoadBinary(dmlc::Stream* fi) {
-  int tmagic;
-  CHECK(fi->Read(&tmagic, sizeof(tmagic)) == sizeof(tmagic)) << 'invalid input file format';
-  CHECK_EQ(tmagic, kMagic) << 'invalid format, magic number mismatch';
-  info.LoadBinary(fi);
-  fi->Read(&page_.offset.HostVector());
-  fi->Read(&page_.data.HostVector());
+    
+    {
+    {
+    {  // creat mean image.
+  inline void CreateMeanImg(void) {
+    if (param_.verbose) {
+      LOG(INFO) << 'Cannot find ' << param_.mean_img
+                << ': create mean image, this will take some time...';
+    }
+    double start = dmlc::GetTime();
+    size_t imcnt = 1;  // NOLINT(*)
+    CHECK(this->Next_()) << 'input iterator failed.';
+    meanimg_.Resize(outimg_.shape_);
+    mshadow::Copy(meanimg_, outimg_);
+    while (this->Next_()) {
+      meanimg_ += outimg_;
+      imcnt += 1;
+      double elapsed = dmlc::GetTime() - start;
+      if (imcnt % 10000L == 0 && param_.verbose) {
+        LOG(INFO) << imcnt << ' images processed, ' << elapsed << ' sec elapsed';
+      }
+    }
+    meanimg_ *= (1.0f / imcnt);
+    // save as mxnet python compatible format.
+    TBlob tmp = meanimg_;
+    {
+      std::unique_ptr<dmlc::Stream> fo(dmlc::Stream::Create(param_.mean_img.c_str(), 'w'));
+      NDArray::Save(fo.get(),
+                    {NDArray(tmp, 0)},
+                    {'mean_img'});
+    }
+    if (param_.verbose) {
+      LOG(INFO) << 'Save mean image to ' << param_.mean_img << '..';
+    }
+    meanfile_ready_ = true;
+    this->BeforeFirst();
+  }
+};
+}  // namespace io
+}  // namespace mxnet
+#endif  // MXNET_IO_ITER_NORMALIZE_H_
+
+    
+    
+    {
+    {
+    { private:
+  /*! \brief output data */
+  DataBatch *out_;
+  /*! \brief queue to be recycled */
+  std::queue<DataBatch*> recycle_queue_;
+};
+}  // namespace io
+}  // namespace mxnet
+#endif  // MXNET_IO_ITER_PREFETCHER_H_
+
+    
+    KVStore* KVStore::Create(const char *type_name) {
+  std::string tname = type_name;
+  std::transform(tname.begin(), tname.end(), tname.begin(), ::tolower);
+  KVStore* kv = nullptr;
+  bool use_device_comm = false;
+  auto has = [tname](const std::string& pattern) {
+    return tname.find(pattern) != std::string::npos;
+  };
+  if (has('device')) {
+    use_device_comm = true;
+  }
+    }
+    
+    /*!
+ *  Copyright (c) 2017 by Contributors
+ *  \file krprod.h
+ *  \brief Core function for Khatri-Rao product
+ *  \author Jencir Lee, Chris Swierczewski
+ */
+#ifndef MXNET_OPERATOR_CONTRIB_KRPROD_H_
+#define MXNET_OPERATOR_CONTRIB_KRPROD_H_
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include 'mshadow/tensor.h'
+#include '../operator_common.h'
+#include '../c_lapack_api.h'
+    
+    MXNET_REGISTER_OP_PROPERTY(IdentityAttachKLSparseReg, IdentityAttachKLSparseRegProp)
+.describe('Apply a sparse regularization to the output a sigmoid activation function.')
+.add_argument('data', 'NDArray-or-Symbol', 'Input data.')
+.add_arguments(IdentityAttachKLSparseRegParam::__FIELDS__());
+    
+                if (m_varKind == VariableKind::Input)
+            {
+                for (auto dim : m_shape.Dimensions())
+                {
+                    if (dim == 0)
+                        InvalidArgument('Variable '%S' has invalid shape '%S'.', AsString().c_str(), m_shape.AsString().c_str());
+                }
+            }
+    
+    
+    {private:
+    shared_ptr<Matrix<ElemType>> m_maxIndexes0, m_maxIndexes1;
+    shared_ptr<Matrix<ElemType>> m_maxValues;
+    int m_topK;
+};
+    
+            if (aliasInfo.releaseCount >= aliasInfo.totalCount)
+            LogicError('number of alias instances exceeded expectation');
+    
+        // ComputationNode derived classes are guaranteed to have a MBLayout
+    if (isFinalValidationPass && !HasMBLayout())
+        InvalidArgument('%ls: Input [%s] must operate on minibatches.', NodeDescription().c_str(), string(shapeB).c_str());
+    
+    int32_t
+RuleBasedCollator::internalGetShortDefinitionString(const char *locale,
+                                                    char *buffer, int32_t capacity,
+                                                    UErrorCode &errorCode) const {
+    if(U_FAILURE(errorCode)) { return 0; }
+    if(buffer == NULL ? capacity != 0 : capacity < 0) {
+        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
+        return 0;
+    }
+    if(locale == NULL) {
+        locale = internalGetLocaleID(ULOC_VALID_LOCALE, errorCode);
+    }
+    }
+    
+    static UBool copyAsSuperscript(
+        const UnicodeString &s,
+        int32_t beginIndex,
+        int32_t endIndex,
+        UnicodeString &result,
+        UErrorCode &status) {
+    if (U_FAILURE(status)) {
+        return FALSE;
+    }
+    for (int32_t i = beginIndex; i < endIndex;) {
+        UChar32 c = s.char32At(i);
+        int32_t digit = u_charDigitValue(c);
+        if (digit < 0) {
+            status = U_INVALID_CHAR_FOUND;
+            return FALSE;
+        }
+        result.append(kSuperscriptDigits[digit]);
+        i += U16_LENGTH(c);
+    }
+    return TRUE;
 }
     
     #endif
+
     
-        // see if we have already split on this feature,
-    // if so we undo that split so we can redo it for this node
-    unsigned path_index = 0;
-    for (; path_index <= unique_depth; ++path_index) {
-      if (static_cast<unsigned>(unique_path[path_index].feature_index) == split_index) break;
+    
+    {private:
+    int32_t fMax;
+    int32_t fMin;
+};
+    
+        /**
+     * Returns TRUE if this class can format positiveValue using
+     * the given range of digit counts.
+     *
+     * @param positiveValue the value to format
+     * @param range the acceptable range of digit counts.
+     */
+    static UBool canFormat(
+            int32_t positiveValue, const IntDigitCountRange &range);
+    
+    /**
+ * Implement UnicodeFunctor
+ */
+void StringMatcher::setData(const TransliterationRuleData* d) {
+    data = d;
+    int32_t i = 0;
+    while (i<pattern.length()) {
+        UChar32 c = pattern.char32At(i);
+        UnicodeFunctor* f = data->lookup(c);
+        if (f != NULL) {
+            f->setData(data);
+        }
+        i += U16_LENGTH(c);
     }
-    if (path_index != unique_depth + 1) {
-      incoming_zero_fraction = unique_path[path_index].zero_fraction;
-      incoming_one_fraction = unique_path[path_index].one_fraction;
-      UnwindPath(unique_path, unique_depth, path_index);
-      unique_depth -= 1;
-    }
-    
-    #include <folly/GLog.h>
-    
-    namespace folly {
-    }
-    
-    using UriTuple = std::tuple<
-    const std::string&,
-    const std::string&,
-    const std::string&,
-    const std::string&,
-    uint16_t,
-    const std::string&,
-    const std::string&,
-    const std::string&>;
-    
-      VirtualExecutor(const VirtualExecutor&) = delete;
-  VirtualExecutor& operator=(const VirtualExecutor&) = delete;
-    
-        // Check if we need to push a batch from local -> global
-    auto batchcount = EXTERNAL_OFFSET / 2;
-    if (get_local_count(newlocal) > batchcount) {
-      CountedDetail::inc_shared_count(newlocal.get(), batchcount);
-      putOwnedBase(newlocal.get(), batchcount, order);
-    }
-    
-      void reset(const std::shared_ptr<T>& p = nullptr) {
-    // Allocate each Holder in a different CoreRawAllocator stripe to
-    // prevent false sharing. Their control blocks will be adjacent
-    // thanks to allocate_shared().
-    for (auto slot : folly::enumerate(slots_)) {
-      auto alloc = getCoreAllocator<Holder, kNumSlots>(slot.index);
-      auto holder = std::allocate_shared<Holder>(alloc, p);
-      *slot = std::shared_ptr<T>(holder, p.get());
-    }
-  }
-    
-    size_t qfind_first_byte_of_bitset(
-    const StringPieceLite haystack,
-    const StringPieceLite needles) {
-  std::bitset<256> s;
-  for (auto needle : needles) {
-    s[(uint8_t)needle] = true;
-  }
-  for (size_t index = 0; index < haystack.size(); ++index) {
-    if (s[(uint8_t)haystack[index]]) {
-      return index;
-    }
-  }
-  return std::string::npos;
 }
+    
+    
+    {  EsdCanClient esd_can_client;
+  EXPECT_TRUE(esd_can_client.Init(param));
+  EXPECT_EQ(esd_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
+  std::vector<CanFrame> frames;
+  int32_t num = 0;
+  EXPECT_EQ(esd_can_client.Send(frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  EXPECT_EQ(esd_can_client.Receive(&frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
+  CanFrame can_frame;
+  frames.push_back(can_frame);
+  EXPECT_EQ(esd_can_client.SendSingleFrame(frames),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  esd_can_client.Stop();
+}
+    
+    TEST(ByteTest, CopyConstructor) {
+  unsigned char byte_value = 0xFF;
+  Byte value(&byte_value);
+  Byte another_value(value);
+  EXPECT_EQ(another_value.to_hex_string(), value.to_hex_string());
+  EXPECT_EQ(another_value.to_binary_string(), value.to_binary_string());
+}
+    
+    int ObjectQualityInfo60C::probexist(const std::uint8_t* bytes,
+                                    int32_t length) const {
+  Byte t0(bytes + 6);
+  int32_t x = t0.get_byte(5, 3);
+    }
+    
+      MatrixXd mat_golden(20, 10);
+  // clang-format off
+  mat_golden <<
+    -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+     1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+     2, -1,  0,  0,  0,  0,  0,  0,  0,  0,
+    -2,  1,  0,  0,  0,  0,  0,  0,  0,  0,
+    -1,  2, -1,  0,  0,  0,  0,  0,  0,  0,
+     1, -2,  1,  0,  0,  0,  0,  0,  0,  0,
+     0, -1,  2, -1,  0,  0,  0,  0,  0,  0,
+     0,  1, -2,  1,  0,  0,  0,  0,  0,  0,
+     0,  0, -1,  2, -1,  0,  0,  0,  0,  0,
+     0,  0,  1, -2,  1,  0,  0,  0,  0,  0,
+     0,  0,  0, -1,  2, -1,  0,  0,  0,  0,
+     0,  0,  0,  1, -2,  1,  0,  0,  0,  0,
+     0,  0,  0,  0, -1,  2, -1,  0,  0,  0,
+     0,  0,  0,  0,  1, -2,  1,  0,  0,  0,
+     0,  0,  0,  0,  0, -1,  2, -1,  0,  0,
+     0,  0,  0,  0,  0,  1, -2,  1,  0,  0,
+     0,  0,  0,  0,  0,  0, -1,  2, -1,  0,
+     0,  0,  0,  0,  0,  0,  1, -2,  1,  0,
+     0,  0,  0,  0,  0,  0,  0, -1,  2, -1,
+     0,  0,  0,  0,  0,  0,  0,  1, -2,  1;
+  // clang-format on
+  EXPECT_EQ(mat, mat_golden);
+    
+    void SplineSegKernel::CalculateThirdOrderDerivative(const uint32_t num_params) {
+  kernel_third_order_derivative_ =
+      Eigen::MatrixXd::Zero(num_params, num_params);
+  for (int r = 3; r < kernel_third_order_derivative_.rows(); ++r) {
+    for (int c = 3; c < kernel_third_order_derivative_.cols(); ++c) {
+      kernel_third_order_derivative_(r, c) =
+          (r * r - r) * (r - 2) * (c * c - c) * (c - 2) / (r + c - 5.0);
+    }
+  }
+}
+    
+    
+    {  auto &brakerpt = chassis_detail.gem().brake_rpt_6c();
+  EXPECT_DOUBLE_EQ(brakerpt.manual_input(), 0.258);
+  EXPECT_DOUBLE_EQ(brakerpt.commanded_value(), 0.772);
+  EXPECT_DOUBLE_EQ(brakerpt.output_value(), 4.37);
+  EXPECT_EQ(brakerpt.brake_on_off(), Brake_rpt_6c::BRAKE_ON_OFF_ON);
+}
+    
+    // config detail: {'name': 'commanded_value', 'enum': {0:
+// 'COMMANDED_VALUE_HEADLIGHTS_OFF', 1: 'COMMANDED_VALUE_LOW_BEAMS', 2:
+// 'COMMANDED_VALUE_HIGH_BEAMS'}, 'precision': 1.0, 'len': 8, 'is_signed_var':
+// False, 'offset': 0.0, 'physical_range': '[0|2]', 'bit': 15, 'type': 'enum',
+// 'order': 'motorola', 'physical_unit': ''}
+Headlight_rpt_77::Commanded_valueType Headlightrpt77::commanded_value(
+    const std::uint8_t* bytes, int32_t length) const {
+  Byte t0(bytes + 1);
+  int32_t x = t0.get_byte(0, 8);
+    }
