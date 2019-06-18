5d48f875090b3e844daadb9c84e3ba456477633a
SUNsung <weatherius@gmail.com>
2019-06-18 08:58:28 +0300

    Rewrite [01cWKUdTnqkwA01xLV.CskJXS.01no9FxPrT.QM01BTg.py] File in: 2019-06-18 08:58:28

--- pyton/01cWKUdTnqkwA01xLV.CskJXS.01no9FxPrT.QM01BTg.py
+++ pyton/01cWKUdTnqkwA01xLV.CskJXS.01no9FxPrT.QM01BTg.py
@@ -1,88 +1,171 @@
 
         
-        html_theme = 'flask'
-html_theme_options = {'index_sidebar_logo': False}
-html_context = {
-    'project_links': [
-        ProjectLink('Donate to Pallets', 'https://palletsprojects.com/donate'),
-        ProjectLink('Flask Website', 'https://palletsprojects.com/p/flask/'),
-        ProjectLink('PyPI releases', 'https://pypi.org/project/Flask/'),
-        ProjectLink('Source Code', 'https://github.com/pallets/flask/'),
-        ProjectLink('Issue Tracker', 'https://github.com/pallets/flask/issues/'),
-    ]
-}
-html_sidebars = {
-    'index': ['project.html', 'localtoc.html', 'versions.html', 'searchbox.html'],
-    '**': ['localtoc.html', 'relations.html', 'versions.html', 'searchbox.html'],
-}
-singlehtml_sidebars = {'index': ['project.html', 'versions.html', 'localtoc.html']}
-html_static_path = ['_static']
-html_favicon = '_static/flask-icon.png'
-html_logo = '_static/flask-logo-sidebar.png'
-html_title = 'Flask Documentation ({})'.format(version)
-html_show_sourcelink = False
-html_domain_indices = False
+            def receive_friend_request(self, friend_id):
+        pass
     
-        # apply the blueprints to the app
-    from flaskr import auth, blog
+        def extract_year_month(self, timestamp):
+        '''Return the year and month portions of the timestamp.'''
+        ...
     
-            :param obj: an import name or object
+        with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    
+        # test that successful registration redirects to the login page
+    response = client.post('/auth/register', data={'username': 'a', 'password': 'a'})
+    assert 'http://localhost/auth/login' == response.headers['Location']
+    
+        :param s: JSON string to deserialize.
+    :param app: App instance to use to configure the JSON decoder.
+        Uses ``current_app`` if not given, and falls back to the default
+        encoder when not in an app context.
+    :param kwargs: Extra arguments passed to :func:`json.dumps`.
+    
+            def _fail(self, *args, **kwargs):
+            raise RuntimeError(
+                'Signalling support is unavailable because the blinker'
+                ' library is not installed.'
+            )
+    
         '''
-        if isinstance(obj, string_types):
-            obj = import_string(obj)
-        for key in dir(obj):
-            if key.isupper():
-                self[key] = getattr(obj, key)
     
-    First, we fix a training set and increase the number of
-samples. Then we plot the computation time as function of
-the number of samples.
-    
-        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
-    plt.plot(sample_sizes, one_core, label='one core')
-    plt.plot(sample_sizes, multi_core, label='multi core')
-    plt.xlabel('n_samples')
-    plt.ylabel('Time (s)')
-    plt.title('Parallel %s' % func.__name__)
-    plt.legend()
-    
-    ratio = scikits_time / scipy_time
-    
-        # TASK: Build a vectorizer / classifier pipeline that filters out tokens
-    # that are too rare or too frequent
-    
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
-    
-    data, rows, columns = make_biclusters(
-    shape=(300, 300), n_clusters=5, noise=5,
-    shuffle=False, random_state=0)
-    
-    We add observation noise to these waveforms. We generate very sparse
-noise: only 6% of the time points contain noise. As a result, the
-l1 norm of this noise (ie 'cityblock' distance) is much smaller than it's
-l2 norm ('euclidean' distance). This can be seen on the inter-class
-distance matrices: the values on the diagonal, that characterize the
-spread of the class, are much bigger for the Euclidean distance than for
-the cityblock distance.
-    
-    In a first step, the hierarchical clustering is performed without connectivity
-constraints on the structure and is solely based on distance, whereas in
-a second step the clustering is restricted to the k-Nearest Neighbors
-graph: it's a hierarchical clustering with structure prior.
+        '''
+    return path.replace('\\', '\\\\\\')
     
     
-class TestSetOfStacks(object):
+class TestAutoContentTypeAndAcceptHeaders:
+    '''
+    Test that Accept and Content-Type correctly defaults to JSON,
+    but can still be overridden. The same with Content-Type when --form
+    -f is used.
     
     
-class TestSortStack(object):
+def rst_filenames():
+    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
+        if '.tox' not in root:
+            for filename in fnmatch.filter(filenames, '*.rst'):
+                yield os.path.join(root, filename)
     
-            print('Test: One element')
-        top = Node(5)
-        stack = Stack(top)
-        assert_equal(stack.pop(), 5)
-        assert_equal(stack.peek(), None)
     
-            print('Success: test_end_to_end')
+def test_follow_redirect_output_options(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=h',
+             '--history-print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 2
+    assert 'HTTP/1.1 302 FOUND' not in r
+    assert HTTP_OK in r
+    
+    
+def test_unicode_basic_auth(httpbin):
+    # it doesn't really authenticate us because httpbin
+    # doesn't interpret the utf8-encoded auth
+    http('--verbose', '--auth', u'test:%s' % UNICODE,
+         httpbin.url + u'/basic-auth/test/' + UNICODE)
+    
+    RETURN = '''
+response_metadata:
+  description: response metadata about the snapshot
+  returned: always
+  type: dict
+  sample:
+    http_headers:
+      content-length: 1490
+      content-type: text/xml
+      date: Tue, 07 Feb 2017 16:43:04 GMT
+      x-amzn-requestid: 7f436dea-ed54-11e6-a04c-ab2372a1f14d
+    http_status_code: 200
+    request_id: 7f436dea-ed54-11e6-a04c-ab2372a1f14d
+    retry_attempts: 0
+snapshot:
+  description: snapshot data
+  returned: always
+  type: dict
+  sample:
+    auto_minor_version_upgrade: true
+    cache_cluster_create_time: 2017-02-01T17:43:58.261000+00:00
+    cache_cluster_id: test-please-delete
+    cache_node_type: cache.m1.small
+    cache_parameter_group_name: default.redis3.2
+    cache_subnet_group_name: default
+    engine: redis
+    engine_version: 3.2.4
+    node_snapshots:
+      cache_node_create_time: 2017-02-01T17:43:58.261000+00:00
+      cache_node_id: 0001
+      cache_size:
+    num_cache_nodes: 1
+    port: 11211
+    preferred_availability_zone: us-east-1d
+    preferred_maintenance_window: wed:03:00-wed:04:00
+    snapshot_name: deletesnapshot
+    snapshot_retention_limit: 0
+    snapshot_source: manual
+    snapshot_status: creating
+    snapshot_window: 10:00-11:00
+    vpc_id: vpc-c248fda4
+changed:
+  description: if a snapshot has been created, deleted, or copied
+  returned: always
+  type: bool
+  sample:
+    changed: true
+'''
+    
+        invocations = dict(
+        aliases='alias_details',
+        all='all_details',
+        config='config_details',
+        mappings='mapping_details',
+        policy='policy_details',
+        versions='version_details',
+    )
+    
+    RETURN = '''
+subscriptions:
+    description: List of subscriptions.
+    returned: When view is set to subscriptions.
+    type: list
+    sample: ['mysubscription', 'mysubscription2']
+topic:
+    description: Name of topic. Used to filter subscriptions.
+    returned: Always
+    type: str
+    sample: 'mytopic'
+topics:
+    description: List of topics.
+    returned: When view is set to topics.
+    type: list
+    sample: ['mytopic', 'mytopic2']
+'''
+    
+    - heroku_collaborator:
+    api_key: YOUR_API_KEY
+    user: '{{ item.user }}'
+    apps: '{{ item.apps | default(apps) }}'
+    suppress_invitation: '{{ item.suppress_invitation | default(suppress_invitation) }}'
+    state: '{{ item.state | default('present') }}'
+  with_items:
+    - { user: 'a.b@example.com' }
+    - { state: 'absent', user: 'b.c@example.com', suppress_invitation: false }
+    - { user: 'x.y@example.com', apps: ['heroku-example-app'] }
+'''
+    
+    HAS_ONEANDONE_SDK = True
+    
+        try:
+        client.login(username=module.params['ipa_user'],
+                     password=module.params['ipa_pass'])
+        changed, hbacrule = ensure(module, client)
+        module.exit_json(changed=changed, hbacrule=hbacrule)
+    except Exception as e:
+        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
+    
+    from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils._text import to_native
+from ansible.module_utils.urls import fetch_url
+    
+    
+if __name__ == '__main__':
+    main()
