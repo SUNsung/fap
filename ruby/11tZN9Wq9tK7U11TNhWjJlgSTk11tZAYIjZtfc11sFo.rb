66df906aafe742baf1621955e0629e970896aa7d
SUNsung <weatherius@gmail.com>
2019-06-18 20:52:44 +0300

    Rewrite [11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb] File in: 2019-06-18 20:52:43

--- ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
+++ ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
@@ -1,138 +1,80 @@
 
         
-              def test_clears_up_previous_helpers
-        @controller.process(:with_symbol)
-        assert_equal 'I respond to bare_a: false', @controller.response_body
-      end
-    
-      # open rmcpplus_request
-  def self.create_ipmi_session_open_request(console_session_id)
-    head = [
-      0x06, 0x00, 0xff, 0x07,   # RMCP Header
-      0x06,                     # RMCP+ Authentication Type
-      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
-      0x00, 0x00, 0x00, 0x00,   # Session ID
-      0x00, 0x00, 0x00, 0x00    # Sequence Number
-    ].pack('C*')
-    
-                k3 = OpenSSL::HMAC.digest('MD5', k1, checksum)
-    
-                int
-          end
-    
-              # Decodes the key from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [EncryptionKey]
-          def decode_key(input)
-            Rex::Proto::Kerberos::Model::EncryptionKey.decode(input.value[0])
-          end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a Kerberos KRB-ERROR (response error)
-        # message definition.
-        class KrbError < Element
-          # @!attribute pvno
-          #   @return [Integer] The protocol version number
-          attr_accessor :pvno
-          # @!attribute msg_type
-          #   @return [Integer] The type of a protocol message
-          attr_accessor :msg_type
-          # @!attribute ctime
-          #   @return [Time] The current time of the client's host
-          attr_accessor :ctime
-          # @!attribute cusec
-          #   @return [Integer] The microseconds part of the client timestamp
-          attr_accessor :cusec
-          # @!attribute stime
-          #   @return [Time] The current time of the server
-          attr_accessor :stime
-          # @!attribute susec
-          #   @return [Integer] The microseconds part of the server timestamp
-          attr_accessor :susec
-          # @!attribute error_code
-          #   @return [Integer] The error request returned by kerberos or the server when a request fails
-          attr_accessor :error_code
-          # @!attribute crealm
-          #   @return [String] The realm part of the client's principal identifier
-          attr_accessor :crealm
-          # @!attribute cname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the client's principal identifier
-          attr_accessor :cname
-          # @!attribute realm
-          #   @return [String] The realm part of the server's principal identifier
-          attr_accessor :realm
-          # @!attribute sname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
-          attr_accessor :sname
-          # @!attribute e_data
-          #   @return [String] additional data about the error (ASN.1 encoded data)
-          attr_accessor :e_data
-    
-    def piece(n, a, nb)
-  nb.each{|x|
-    a[n] = x
-    if n == NP-1
-      $p << [a.sort]
-    else
-      nbc=nb.dup
-      [-ROW, -1, 1, ROW].each{|d|
-        if x+d > 0 and not a.include?(x+d) and not nbc.include?(x+d)
-          nbc << x+d
+                # issue - An instance of `Gitlab::GithubImport::Representation::Issue`
+        # project - An instance of `Project`
+        # client - An instance of `Gitlab::GithubImport::Client`
+        def initialize(issue, project, client)
+          @issue = issue
+          @project = project
+          @client = client
+          @label_finder = LabelFinder.new(project)
         end
-      }
-      nbc.delete x
-      piece(n+1,a[0..n],nbc)
-    end
-  }
-end
     
-          # Main menu tree menu
-      def main_menu_tree(text, icon: nil, sub_menu: nil, url: '#')
-        content_tag :li, class: 'sidebar-menu-item d-block w-100' do
-          main_menu_item(text, url: url, icon: icon) +
-            render(partial: 'spree/admin/shared/sub_menu/#{sub_menu}')
-        end
-      end
+          # Imports all the objects in sequence in the current thread.
+      def sequential_import
+        each_object_to_import do |object|
+          repr = representation_class.from_api_response(object)
     
-    desc 'Runs all tests in all Spree engines'
-task test: :test_app do
-  SPREE_GEMS.each do |gem_name|
-    Dir.chdir('#{File.dirname(__FILE__)}/#{gem_name}') do
-      sh 'rspec'
-    end
+      # The default layout used by the appenders.
+  pattern = '[%d] %-5l PID-%p TID-%t %c: %m\n'
+  layout = Logging.layouts.pattern(pattern: pattern)
+    
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
+  end
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
   end
 end
     
-            def new; end
+      class FetchWebfinger < Base
+    def perform(*_args)
+      # don't do real discovery in cucumber
+    end
+  end
+end
+
     
-            def current
-          @order = find_current_order
-          if @order
-            respond_with(@order, default_template: :show, locals: { root_object: @order })
-          else
-            head :no_content
-          end
-        end
+          get :index, params: {a_id: @aspect.id, page: '1'}, format: :json
+      save_fixture(response.body, 'aspects_manage_contacts_json')
+    end
     
-                render_order(result)
-          end
+          def extract_renamed_path_destination(file)
+        return file.gsub(/{.* => (.*)}/, '\1').gsub(/.* => (.*)/, '\1')
+      end
     
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+        get '/pages'
+    
+      test 'displays_latest_changes' do
+    get('/latest_changes')
+    body = last_response.body
+    
+      def app
+    Precious::App
+  end
+end
+    
+          def stop_template
+        asset_path 'template-stop.erb'
+      end
+    
+          it 'should load and validate the project' do
+        project_config = File.join(fixtures_dir, 'sample.yml')
+        expect(described_class.validate(project_config: project_config)).to \
+          be_a Tmuxinator::Project
+      end
+    
+      describe '.shell?' do
+    context '$SHELL is set' do
+      before do
+        allow(ENV).to receive(:[]).with('SHELL') { 'vim' }
+      end
+    
+        def list
+      say 'tmuxinator projects:'
+      if options[:newline]
+        say Tmuxinator::Config.configs.join('\n')
       else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+        print_in_columns Tmuxinator::Config.configs
       end
-    end
-  end
-end
-    
-    
-    
-        def poster
-      'poster='#{@poster}'' if @poster
     end
