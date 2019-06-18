08d522f899f4adcdc3402604d42d48342aecac59
SUNsung <weatherius@gmail.com>
2019-06-18 19:16:08 +0300

    Rewrite [07qAM.H6Brusc075flLpjROHAc073fIacIetV1I07pvM.cpp] File in: 2019-06-18 19:16:08

--- cccccc/07qAM.H6Brusc075flLpjROHAc073fIacIetV1I07pvM.cpp
+++ cccccc/07qAM.H6Brusc075flLpjROHAc073fIacIetV1I07pvM.cpp
@@ -1,160 +1,130 @@
 
         
-            if (borderType == BORDER_MODE_CONSTANT)
-        for (s32 k = 0; k < cn; ++k)
-        {
-            lanea[-cn+k] = borderValue;
-            lanea[colsn+k] = borderValue;
-            laneA[-cn+k] = borderValue;
-            laneA[colsn+k] = borderValue;
-            laneb[-cn+k] = borderValue;
-            laneb[colsn+k] = borderValue;
-            laneB[-cn+k] = borderValue;
-            laneB[colsn+k] = borderValue;
-        }
+            fp->fPatIdx   = 0;
+    fp->fInputIdx = startIdx;
     
-            result += (double)vget_lane_s64(vadd_s64(vget_low_s64(ws), vget_high_s64(ws)), 0);
+    U_NAMESPACE_END
     
-                    uint16x8_t vSum_0_4  = vaddq_u16(vLane0.val[0], vLane2.val[0]);
-                uint16x8_t vSum_1_5  = vaddq_u16(vLane0.val[1], vLane2.val[1]);
-                uint16x8_t vSum_2_6  = vaddq_u16(vLane0.val[2], vLane2.val[2]);
-                uint16x8_t vSum_3_7  = vaddq_u16(vLane0.val[3], vLane2.val[3]);
-    
-    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
-inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
-inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
-    
-    void integral(const Size2D &size,
-              const u8 * srcBase, ptrdiff_t srcStride,
-              u32 * sumBase, ptrdiff_t sumStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    uint32x4_t v_zero = vmovq_n_u32(0u);
-    }
-    
-     public:
-  /*! \brief cuda kernel argument descriptor */
-  struct ArgType {
-    /*! \brief whether argument is NDArray */
-    bool is_ndarray;
-    /*! \brief whether argument is constant (input) */
-    bool is_const;
-    /*! \brief data type of argument */
-    mshadow::TypeFlag dtype;
-  };
-  /*! \brief Cuda kernel */
-  class Kernel {
-   public:
-    /*! \brief Launch the kernel */
-    void Launch(const Context& ctx, const std::vector<dmlc::any>& args,
-                uint32_t grid_dim_x, uint32_t grid_dim_y, uint32_t grid_dim_z,
-                uint32_t block_dim_x, uint32_t block_dim_y, uint32_t block_dim_z,
-                uint32_t shared_mem);
-    /*! \brief kernel interface signature */
-    const std::vector<ArgType>& signature() { return signature_; }
-    }
-    
-    #endif  // PLUGIN_CAFFE_CAFFE_COMMON_H_
-
-    
-    
-    {
-    {
-    {  /*!
-   * \brief Worker threads.
-   */
-  std::vector<std::thread> worker_threads_;
-  /*!
-   * \brief Startup synchronization objects
-   */
-  std::list<std::shared_ptr<dmlc::ManualEvent>> ready_events_;
-  /*!
-   * \brief Disallow default construction.
-   */
-  ThreadPool() = delete;
-  /*!
-   * \brief Disallow copy construction and assignment.
-   */
-  DISALLOW_COPY_AND_ASSIGN(ThreadPool);
+    class U_I18N_API SharedPluralRules : public SharedObject {
+public:
+    SharedPluralRules(PluralRules *prToAdopt) : ptr(prToAdopt) { }
+    virtual ~SharedPluralRules();
+    const PluralRules *operator->() const { return ptr; }
+    const PluralRules &operator*() const { return *ptr; }
+private:
+    PluralRules *ptr;
+    SharedPluralRules(const SharedPluralRules &);
+    SharedPluralRules &operator=(const SharedPluralRules &);
 };
-}  // namespace engine
-}  // namespace mxnet
-#endif  // MXNET_ENGINE_THREAD_POOL_H_
-
     
-    template<typename xpu>
-void Quantize2BitKernelLaunch(mshadow::Stream<xpu> *s, const std::vector<mxnet::TBlob> &inputs,
-                              const float threshold) {
-  mxnet::op::mxnet_op::Kernel<quantize_2bit, xpu>
-    ::Launch(s,
-            inputs[2].Size(),         // compressed array size
-            inputs[0].Size(),         // original size
-            inputs[2].dptr<float>(),  // compressed array
-            inputs[0].dptr<float>(),  // original array
-            inputs[1].dptr<float>(),  // residual array
-            -1 *threshold,            // negative threshold
-            threshold);               // positive threshold
+    
+    {    /**
+     * Formats positiveValue using the given range of digit counts.
+     * Always uses standard digits '0' through '9'. Formatted value is
+     * left padded with '0' as necessary to achieve minimum digit count.
+     * Does not produce any grouping separators or trailing decimal point.
+     * Calling format to format a value with a particular digit count range
+     * when canFormat indicates that the same value and digit count range
+     * cannot be formatted results in undefined behavior.
+     *
+     * @param positiveValue the value to format
+     * @param range the acceptable range of digit counts.
+     */
+    static UnicodeString &format(
+            int32_t positiveValue,
+            const IntDigitCountRange &range,
+            UnicodeString &appendTo);
+    
+};
+    
+    UnicodeSet *SimpleDateFormatStaticSets::getIgnorables(UDateFormatField fieldIndex)
+{
+    UErrorCode status = U_ZERO_ERROR;
+    umtx_initOnce(gSimpleDateFormatStaticSetsInitOnce, &smpdtfmt_initSets, status);
+    if (U_FAILURE(status)) {
+        return NULL;
+    }
+    
+    switch (fieldIndex) {
+        case UDAT_YEAR_FIELD:
+        case UDAT_MONTH_FIELD:
+        case UDAT_DATE_FIELD:
+        case UDAT_STANDALONE_DAY_FIELD:
+        case UDAT_STANDALONE_MONTH_FIELD:
+            return gStaticSets->fDateIgnorables;
+            
+        case UDAT_HOUR_OF_DAY1_FIELD:
+        case UDAT_HOUR_OF_DAY0_FIELD:
+        case UDAT_MINUTE_FIELD:
+        case UDAT_SECOND_FIELD:
+        case UDAT_HOUR1_FIELD:
+        case UDAT_HOUR0_FIELD:
+            return gStaticSets->fTimeIgnorables;
+            
+        default:
+            return gStaticSets->fOtherIgnorables;
+    }
 }
     
-    class GradientCompression {
- public:
-  GradientCompression();
+        /**
+     * Remove any match data.  This must be called before performing a
+     * set of matches with this segment.
+     */
+    void resetMatch();
+    
+    
+    {        UnicodeReplacer* r = data->lookupReplacer(c);
+        if (r == NULL) {
+            ICU_Utility::appendToRule(rule, c, FALSE, escapeUnprintable, quoteBuf);
+        } else {
+            UnicodeString buf;
+            r->toReplacerPattern(buf, escapeUnprintable);
+            buf.insert(0, (UChar)0x20);
+            buf.append((UChar)0x20);
+            ICU_Utility::appendToRule(rule, buf,
+                                      TRUE, escapeUnprintable, quoteBuf);
+        }
     }
     
-    /*!
- * Copyright (c) 2015 by Contributors
- * \file crop.cc
- * \brief
- * \author Wei Wu
-*/
+    std::shared_ptr<DHTBucket>
+DHTRoutingTable::getBucketFor(const std::shared_ptr<DHTNode>& node) const
+{
+  return getBucketFor(node->getID());
+}
     
-    MXNET_REGISTER_OP_PROPERTY(_NDArray, NDArrayOpProp)
-.describe('Stub for implementing an operator implemented in native frontend language with ndarray.')
-.add_argument('data', 'NDArray-or-Symbol[]', 'Input data for the custom operator.')
-.add_arguments(NDArrayOpParam::__FIELDS__());
     
-      /**
-   * Use the Iterate method for iterating over options that are stored as
-   * objects in runtime options (e.g. FilesMatch). This function iterates over
-   * the settings passed as ini/hdf, calls back to, generally, the constructor
-   * of the object in question.
-   *
-   * Note: For now, we are not `ini_get()` enabling these type of options as
-   * it is not trivial to come up with a non-hacky and workable way to store
-   * the data correctly. Also, as usual, Hdf takes priority.
-   */
-  static void Iterate(std::function<void (const IniSettingMap&,
-                                          const Hdf&,
-                                          const std::string&)> cb,
-                      const IniSettingMap &ini, const Hdf& config,
-                      const std::string &name, const bool prepend_hhvm = true);
-    
-      path_str += strlen(prefix);
-  path_len -= strlen(prefix);
-    
-    #include 'hphp/util/perf-event.h'
-    
-    namespace HPHP {
-    }
-    
-    #include 'hphp/util/stack-trace.h'
-    
-    #include 'hphp/runtime/base/perf-warning-inl.h'
-    
-    #endif // incl_HPHP_PIPE_H_
+    {} // namespace aria2
 
     
-    #pragma once
+    void DHTTaskFactoryImpl::setCommonProperty(
+    const std::shared_ptr<DHTAbstractTask>& task)
+{
+  task->setRoutingTable(routingTable_);
+  task->setMessageDispatcher(dispatcher_);
+  task->setMessageFactory(factory_);
+  task->setTaskQueue(taskQueue_);
+  task->setLocalNode(localNode_);
+}
     
-    #pragma once
+    bool DHTTokenTracker::validateToken(const std::string& token,
+                                    const unsigned char* infoHash,
+                                    const std::string& ipaddr,
+                                    uint16_t port) const
+{
+  for (auto& elem : secret_) {
+    if (generateToken(infoHash, ipaddr, port, elem) == token) {
+      return true;
+    }
+  }
+  return false;
+}
     
-    class DHTBucket;
-class DHTPingReplyMessage;
+    void DHTTokenUpdateCommand::preProcess()
+{
+  if (getDownloadEngine()->getRequestGroupMan()->downloadFinished() ||
+      getDownloadEngine()->isHaltRequested()) {
+    enableExit();
+  }
+}
     
-      void setLocalNode(const std::shared_ptr<DHTNode>& localNode);
-    
-      virtual ~DHTTaskQueueImpl();
-    
-    #include <cstring>
-#include <cstdlib>
+    bool DHTUnknownMessage::send() { return true; }
