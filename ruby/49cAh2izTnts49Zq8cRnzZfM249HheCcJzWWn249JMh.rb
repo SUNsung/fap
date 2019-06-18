425dc70c9a3984601b84694245f76baec287b852
SUNsung <weatherius@gmail.com>
2019-06-18 06:52:33 +0300

    Rewrite [49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb] File in: 2019-06-18 06:52:33

--- ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
+++ ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
@@ -1,98 +1,84 @@
 
         
-                def initialize(object_name, method_name, template_object, checked_value, unchecked_value, options)
-          @checked_value   = checked_value
-          @unchecked_value = unchecked_value
-          super(object_name, method_name, template_object, options)
-        end
+            private
     
-                case options[:default]
-            when nil
-              Time.current
-            when Date, Time
-              options[:default]
-            else
-              default = options[:default].dup
-    
-            redirect_to admin_report_path(@report), notice: I18n.t('admin.report_notes.created_msg')
-      else
-        @report_notes = @report.notes.latest
-        @report_history = @report.history
-        @form = Form::StatusBatch.new
-    
-    module UserTrackingConcern
-  extend ActiveSupport::Concern
-    
-      # if rss_url already in existing opml file, use that; otherwise, do a lookup
-  rss_url = nil
-  if File.exist?(OUTPUT_FILENAME)
-    xml = Nokogiri::XML(File.open(OUTPUT_FILENAME))
-    existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first
-    if existing_blog
-      rss_url = existing_blog.attr('xmlUrl')
-      puts '#{name}: ALREADY HAVE'
+      class TestSubHash < TestHash
+    class SubHash < Hash
+      def reject(*)
+        super
+      end
     end
+    
+        unless SCANNER_EVENT_TABLE.key?(:ignored_sp)
+      SCANNER_EVENT_TABLE[:ignored_sp] = 1
+      SCANNER_EVENTS << :ignored_sp
+      EVENTS << :ignored_sp
+    end
+    
+      def test_using_wrong_argument
+    bug = '[ruby-dev:50270] [Bug #13956]'
+    pattern = /expected Module/
+    assert_separately([], '#{<<-'begin;'}\n#{<<-'end;'}')
+    bug = ''#{bug.dump}
+    pattern = /#{pattern}/
+    begin;
+      assert_raise_with_message(TypeError, pattern, bug) {
+        using(1) do end
+      }
+    end;
+    assert_separately([], '#{<<-'begin;'}\n#{<<-'end;'}')
+    bug = ''#{bug.dump}
+    pattern = /#{pattern}/
+    begin;
+      assert_raise_with_message(TypeError, pattern, bug) {
+        Module.new {using(1) {}}
+      }
+    end;
   end
     
-      def down
-    remove_index :share_visibilities, name: :shareable_and_user_id
-    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id
-  end
+            gv = eval('global_variables', bind).collect{|m| m.to_s}
+        lv = eval('local_variables', bind).collect{|m| m.to_s}
+        iv = eval('instance_variables', bind).collect{|m| m.to_s}
+        cv = eval('self.class.constants', bind).collect{|m| m.to_s}
     
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
+          require 'rack'
+      puts 'EXEC: \#{caller.grep(/load/).empty? ? 'exec' : 'load'}'
+      puts 'ARGS: \#{$0} \#{ARGV.join(' ')}'
+      puts 'RACK: \#{RACK}'
+      process_title = `ps -o args -p \#{Process.pid}`.split('\n', 2).last.strip
+      puts 'PROCESS: \#{process_title}'
+    RUBY
+    
+    def r(val); *a = yield(); test_ok(a == val, 2); end
+r([nil]){next}
+r([nil]){next nil}
+r([1]){next 1}
+r([]){next []}
+r([1]){next [1]}
+r([nil]){next [nil]}
+r([[]]){next [[]]}
+r([1,2]){next [1,2]}
+r([]){next [*[]]}
+r([1]){next [*[1]]}
+r([1,2]){next [*[1,2]]}
+    
+        config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=#{2.days.to_i}'
+    }
+  else
+    config.action_controller.perform_caching = false
+    
+      # Disable request forgery protection in test environment.
+  config.action_controller.allow_forgery_protection = false
+    
+        it 'accepts jsfiddle link' do
+      liquid = generate_new_liquid(jsfiddle_link)
+      rendered_jsfiddle_iframe = liquid.render
+      Approvals.verify(rendered_jsfiddle_iframe, name: 'jsfiddle_liquid_tag', format: :html)
+    end
+    
+      def user_is_author?
+    record.user_id == user.id
   end
 end
-    
-        sign_in(alice, scope: :user)
-  end
-    
-      describe '#index' do
-    context 'with a private post' do
-      before do
-        @alices_aspect = alice.aspects.where(name: 'generic').first
-        @post = alice.post(:status_message, text: 'hey', to: @alices_aspect.id)
-      end
-    
-        h = {}
-    attrs.each do | e |
-    
-      private
-    
-    require 'pleaserun/cli'
-    
-      # Generate the proper tar flags based on the path name.
-  def tar_compression_flag(path)
-    case path
-      when /\.tar\.bz2$/
-        return '-j'
-      when /\.tar\.gz$|\.tgz$/
-        return '-z'
-      when /\.tar\.xz$/
-        return '-J'
-      else
-        return nil
-    end
-  end # def tar_compression_flag
-end # class FPM::Package::Tar
-
-    
-        # use dir to set stuff up properly, mainly so I don't have to reimplement
-    # the chdir/prefix stuff special for zip.
-    dir = convert(FPM::Package::Dir)
-    if attributes[:chdir]
-      dir.attributes[:chdir] = File.join(build_path, attributes[:chdir])
-    else
-      dir.attributes[:chdir] = build_path
-    end
-    
-    # Like the ::Gem::Package::TarWriter but contains some backports and bug fixes
-class FPM::Util::TarWriter < ::Gem::Package::TarWriter
-  if FPM::Issues::TarWriter.has_issues_with_split_name?
-    def split_name(name)
-      if name.bytesize > 256 then
-        raise ::Gem::Package::TooLongFileName.new('File \'#{name}\' has a too long path (should be 256 or less)')
-      end
