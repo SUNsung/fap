7d969f0d2c728fcebd9bd026471dffabc508046c
SUNsung <weatherius@gmail.com>
2019-06-18 04:15:26 +0300

    Rewrite [36p7FkcUA9YY36IJbhF4wSHt.36R0OlifRujMQ36MeG.rb] File in: 2019-06-18 04:15:26

--- ruby/36p7FkcUA9YY36IJbhF4wSHt.36R0OlifRujMQ36MeG.rb
+++ ruby/36p7FkcUA9YY36IJbhF4wSHt.36R0OlifRujMQ36MeG.rb
@@ -1,79 +1,102 @@
 
         
-          #
-  # Initializes an HTTP packet header class that inherits from a Hash base
-  # class.
-  #
-  def initialize
-    self.dcase_hash = {}
-    
+            def email_changed(record, opts={})
+      devise_mail(record, :email_changed, opts)
     end
-end
-end
-end
-
     
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a Kerberos AuthorizationData data
-        # definition.
-        class AuthorizationData < Element
-          # @!attribute elements
-          #   @return [Hash{Symbol => <Integer, String>}] The type of the authorization data
-          #   @option [Integer] :type
-          #   @option [String] :data
-          attr_accessor :elements
+    begin
+  require 'bundler/inline'
+rescue LoadError => e
+  $stderr.puts 'Bundler version 1.10 or later is required. Please update your Bundler'
+  raise e
+end
     
-              # Encodes the pvno field
+              # Determine if we require a local Vagrant environment. There are
+          # two cases that we require a local environment:
           #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_pvno
-            bn = OpenSSL::BN.new(pvno.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-              # Rex::Proto::Kerberos::Model::KrbError encoding isn't supported
+          #   * We're asking for ANY/EVERY VM (no names given).
           #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'KrbError encoding not supported'
-          end
+          #   * We're asking for specific VMs, at least once of which
+          #     is NOT in the local machine index.
+          #
+          requires_local_env = false
+          requires_local_env = true if names.empty?
+          requires_local_env ||= names.any? { |n|
+            !@env.machine_index.include?(n)
+          }
+          raise Errors::NoEnvironmentError if requires_local_env && !@env.root_path
     
-    end
-
-    
-      def index
-    @address_endpoints = @server.address_endpoints.order(:address).to_a
-  end
-    
-      private
-    
-      def get_messages(scope)
-    if scope == 'held'
-      options = {:where => {:held => 1}}
-    else
-      options = {:where => {:scope => scope, :spam => false}, :order => :timestamp, :direction => 'desc'}
-    
-      def organization
-    if [:edit, :update, :delete, :destroy].include?(action_name.to_sym)
-      @organization ||= params[:org_permalink] ? current_user.organizations_scope.find_by_permalink!(params[:org_permalink]) : nil
-    end
-  end
-  helper_method :organization
-    
-      def new
-    @server = organization.servers.build
-  end
-    
-    
-      smtp_endpoints = server.smtp_endpoints.order(:name).to_a
-      if smtp_endpoints.present?
-        s << '<optgroup label='SMTP Endpoints'>'
-        for endpoint in smtp_endpoints
-          value = '#{endpoint.class}##{endpoint.uuid}'
-          selected = value == selected_value ? 'selected='selected'' : ''
-          s << '<option value='#{value}' #{selected}>#{endpoint.description}</option>'
+            # Called after the configuration is finalized and loaded to validate
+        # this object.
+        #
+        # @param [Machine] machine Access to the machine that is being
+        #   validated.
+        # @return [Hash]
+        def validate(machine)
+          return { self.to_s => _detected_errors }
         end
-        s << '</optgroup>'
+    
+    module Vagrant
+  module Plugin
+    module V2
+      # This class maintains a list of all the registered plugins as well
+      # as provides methods that allow querying all registered components of
+      # those plugins as a single unit.
+      class Manager
+        attr_reader :registered
+    
+        using Magic
+    
+    Given(/^(\d+) valid existing releases$/) do |num|
+  a_day = 86_400 # in seconds
+  offset = -(a_day * num.to_i)
+  num.to_i.times do
+    run_vagrant_command('mkdir -p #{TestApp.release_path(TestApp.timestamp(offset))}')
+    offset += a_day
+  end
+end
+    
+    set_if_empty :format, :airbrussh
+set_if_empty :log_level, :debug
+    
+          def date
+        first = page.last_version
+        return Time.now.strftime(DATE_FORMAT) unless first
+        first.authored_date.strftime(DATE_FORMAT)
+      end
+    
+        # don't use h1 or it will be promoted to replace file name
+    # which doesn't generate a normal header link
+    @wiki.write_page(page, :markdown, '## ' + text,
+                     { :name => 'user1', :email => 'user1' });
+    
+        # Title is based on file name when h1_title is false.
+    actual = @view.title
+    assert_equal 'H1', title
+  end
+end
+
+    
+      # Mount Action Cable outside main process or domain
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+    
+        it 'accepts jsfiddle link' do
+      liquid = generate_new_liquid(jsfiddle_link)
+      rendered_jsfiddle_iframe = liquid.render
+      Approvals.verify(rendered_jsfiddle_iframe, name: 'jsfiddle_liquid_tag', format: :html)
+    end
+    
+      def delete_confirm?
+    edit?
+  end
+    
+      context 'hook value is string' do
+    before { project.yaml[hook_name] = 'echo 'on hook'' }
+    
+      describe '.editor?' do
+    context '$EDITOR is set' do
+      before do
+        allow(ENV).to receive(:[]).with('EDITOR') { 'vim' }
       end
