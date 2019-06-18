a60decfd2b9fa5504be7730c3125a5cc97fa1a30
SUNsung <weatherius@gmail.com>
2019-06-18 23:30:30 +0300

    Rewrite [284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py] File in: 2019-06-18 23:30:30

--- pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
+++ pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
@@ -1,223 +1,40 @@
 
         
-            def can_fit_in_spot(self, spot):
-        return spot.size == VehicleSize.LARGE
-    
-    
-class Transaction(object):
-    
-        assert proc.expect([TIMEOUT, u'test'])
-    
-    
-@pytest.mark.functional
-def test_with_confirmation(proc, TIMEOUT):
-    with_confirmation(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'echo test')
-    
-    no_match_output = '''
-Listing... Done
-'''
-    
-    
-def is_setting_index(node):
-    if node.tagname == 'index':
-        # index entries for setting directives look like:
-        # [(u'pair', u'SETTING_NAME; setting', u'std:setting-SETTING_NAME', '')]
-        entry_type, info, refid = node['entries'][0][:3]
-        return entry_type == 'pair' and info.endswith('; setting')
-    return False
-    
-            slots = int(self.slots)
-        if slots > 1:
-            urls = [url.replace('localhost', '127.0.0.%d' % (x + 1)) for x in range(slots)]
-        else:
-            urls = [url]
-    
-    # Declare top-level shortcuts
-from scrapy.spiders import Spider
-from scrapy.http import Request, FormRequest
-from scrapy.selector import Selector
-from scrapy.item import Item, Field
-    
-            if opts.logfile:
-            self.settings.set('LOG_ENABLED', True, priority='cmdline')
-            self.settings.set('LOG_FILE', opts.logfile, priority='cmdline')
-    
-    
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
-    
-    
-class Command(ScrapyCommand):
-    
-    from scrapy.commands import ScrapyCommand
-from scrapy.exceptions import UsageError
-    
-        def _print_response(self, response, opts):
-        if opts.headers:
-            self._print_headers(response.request.headers, b'>')
-            print('>')
-            self._print_headers(response.headers, b'<')
-        else:
-            self._print_bytes(response.body)
-    
-            try:
-            spidercls = self.crawler_process.spider_loader.load(name)
-        except KeyError:
-            pass
-        else:
-            # if spider already exists and not --force then halt
-            if not opts.force:
-                print('Spider %r already exists in module:' % name)
-                print('  %s' % spidercls.__module__)
-                return
-        template_file = self._find_template(opts.template)
-        if template_file:
-            self._genspider(module, name, domain, opts.template, template_file)
-            if opts.edit:
-                self.exitcode = os.system('scrapy edit '%s'' % name)
-    
-    from scrapy.commands import ScrapyCommand
-from scrapy.settings import BaseSettings
-    
-        name = 'scrapes'
-    
-            # If no credentials could be found anywhere,
-        # consider this an anonymous connection request by default;
-        # unless 'anon' was set explicitly (True/False).
-        anon = kw.get('anon')
-        if anon is None and not aws_access_key_id and not aws_secret_access_key:
-            kw['anon'] = True
-        self.anon = kw.get('anon')
-    
-        protocol = ScrapyHTTPPageGetter
-    waiting = 1
-    noisy = False
-    followRedirect = False
-    afterFoundGet = False
-    
-    ## All tokens go to the parser (unless skip() is called in that rule)
-# on a particular 'channel'.  The parser tunes to a particular channel
-# so that whitespace etc... can go to the parser on a 'hidden' channel.
-DEFAULT_CHANNEL = 0
-    
-    
-def cityscapes_to_coco(cityscapes_id):
-    lookup = {
-        0: 0,  # ... background
-        1: 2,  # bicycle
-        2: 3,  # car
-        3: 1,  # person
-        4: 7,  # train
-        5: 8,  # truck
-        6: 4,  # motorcycle
-        7: 6,  # bus
-        8: -1,  # rider (-1 means rand init)
-    }
-    return lookup[cityscapes_id]
-    
-        # Histogram of ground-truth objects
-    gt_hist = np.zeros((len(classes)), dtype=np.int)
-    for entry in roidb:
-        gt_inds = np.where(
-            (entry['gt_classes'] > 0) & (entry['is_crowd'] == 0))[0]
-        gt_classes = entry['gt_classes'][gt_inds]
-        gt_hist += np.histogram(gt_classes, bins=hist_bins)[0]
-    logger.debug('Ground-truth class histogram:')
-    for i, v in enumerate(gt_hist):
-        logger.debug(
-            '{:d}{:s}: {:d}'.format(
-                i, classes[i].rjust(char_len), v))
-    logger.debug('-' * char_len)
-    logger.debug(
-        '{:s}: {:d}'.format(
-            'total'.rjust(char_len), np.sum(gt_hist)))
+        with open('update/versions.json', 'w') as jsonf:
+    json.dump(versions_info, jsonf, indent=4, sort_keys=True)
 
     
-    
-def _mkanchors(ws, hs, x_ctr, y_ctr):
-    '''Given a vector of widths (ws) and heights (hs) around a center
-    (x_ctr, y_ctr), output a set of anchors (windows).
-    '''
-    ws = ws[:, np.newaxis]
-    hs = hs[:, np.newaxis]
-    anchors = np.hstack(
-        (
-            x_ctr - 0.5 * (ws - 1),
-            y_ctr - 0.5 * (hs - 1),
-            x_ctr + 0.5 * (ws - 1),
-            y_ctr + 0.5 * (hs - 1)
-        )
-    )
-    return anchors
+        with open('supportedsites.html', 'w', encoding='utf-8') as sitesf:
+        sitesf.write(template)
     
     
-def _add_fast_rcnn_head(
-    model, add_roi_box_head_func, blob_in, dim_in, spatial_scale_in
-):
-    '''Add a Fast R-CNN head to the model.'''
-    blob_frcn, dim_frcn = add_roi_box_head_func(
-        model, blob_in, dim_in, spatial_scale_in
-    )
-    fast_rcnn_heads.add_fast_rcnn_outputs(model, blob_frcn, dim_frcn)
-    if model.train:
-        loss_gradients = fast_rcnn_heads.add_fast_rcnn_losses(model)
-    else:
-        loss_gradients = None
-    return loss_gradients
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
     
+        def test_cache(self):
+        ydl = FakeYDL({
+            'cachedir': self.test_dir,
+        })
+        c = Cache(ydl)
+        obj = {'x': 1, 'y': ['ä', '\\a', True]}
+        self.assertEqual(c.load('test_cache', 'k.'), None)
+        c.store('test_cache', 'k.', obj)
+        self.assertEqual(c.load('test_cache', 'k2'), None)
+        self.assertFalse(_is_empty(self.test_dir))
+        self.assertEqual(c.load('test_cache', 'k.'), obj)
+        self.assertEqual(c.load('test_cache', 'y'), None)
+        self.assertEqual(c.load('test_cache2', 'k.'), None)
+        c.remove()
+        self.assertFalse(os.path.exists(self.test_dir))
+        self.assertEqual(c.load('test_cache', 'k.'), None)
     
-_RENAME = {
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up4convs':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up4convs',
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up',
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0upshare':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v0upshare',
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0up':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v0up',
-    # Removed head_builder module in favor of the more specific fast_rcnn name
-    'head_builder.add_roi_2mlp_head':
-        'fast_rcnn_heads.add_roi_2mlp_head',
-}
+    import sys
+import os
+import subprocess
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     
-    
-def add_single_scale_rpn_losses(model):
-    '''Add losses for a single scale RPN model (i.e., no FPN).'''
-    # Spatially narrow the full-sized RPN label arrays to match the feature map
-    # shape
-    model.net.SpatialNarrowAs(
-        ['rpn_labels_int32_wide', 'rpn_cls_logits'], 'rpn_labels_int32'
-    )
-    for key in ('targets', 'inside_weights', 'outside_weights'):
-        model.net.SpatialNarrowAs(
-            ['rpn_bbox_' + key + '_wide', 'rpn_bbox_pred'], 'rpn_bbox_' + key
-        )
-    loss_rpn_cls = model.net.SigmoidCrossEntropyLoss(
-        ['rpn_cls_logits', 'rpn_labels_int32'],
-        'loss_rpn_cls',
-        scale=model.GetLossScale()
-    )
-    loss_rpn_bbox = model.net.SmoothL1Loss(
-        [
-            'rpn_bbox_pred', 'rpn_bbox_targets', 'rpn_bbox_inside_weights',
-            'rpn_bbox_outside_weights'
-        ],
-        'loss_rpn_bbox',
-        beta=1. / 9.,
-        scale=model.GetLossScale()
-    )
-    loss_gradients = blob_utils.get_loss_gradients(
-        model, [loss_rpn_cls, loss_rpn_bbox]
-    )
-    model.AddLosses(['loss_rpn_cls', 'loss_rpn_bbox'])
-    return loss_gradients
+            self.server_process.terminate()
+        self.server_process.communicate()
