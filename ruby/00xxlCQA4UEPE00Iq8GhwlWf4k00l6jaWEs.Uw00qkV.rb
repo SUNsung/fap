40b1450342d3d702cd2352f827a10ad2d78a344f
SUNsung <weatherius@gmail.com>
2019-06-19 17:16:33 +0300

    Rewrite [00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb] File in: 2019-06-19 17:16:31

--- ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
+++ ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
@@ -1,10 +1,63 @@
 
         
-          class SendPublic < Base
-    def perform(*_args)
-      # don't federate in cucumber
-    end
+          attr_reader :id
+  attr_reader :post_id
+  def initialize(id = nil, post_id = nil); @id, @post_id = id, post_id end
+  def to_key; id ? [id] : nil end
+  def save; @id = 1; @post_id = 1 end
+  def persisted?; @id.present? end
+  def to_param; @id && @id.to_s; end
+  def name
+    @id.nil? ? 'new #{self.class.name.downcase}' : '#{self.class.name.downcase} ##{@id}'
   end
     
-      # Install JavaScript dependencies if using Yarn
-  # system('bin/yarn')
+          it 'passes an environment Hash' do
+        expect_command({ 'PATH' => '/usr/local/bin' }, 'git', 'commit')
+        Fastlane::Actions.sh({ 'PATH' => '/usr/local/bin' }, 'git', 'commit')
+      end
+    
+        os = 'other'
+    shelljoin_testcases.each do |testcase|
+      it testcase['it'] + ': ' + testcase['it_result'][os] do
+        array = testcase['input']
+        expect_correct_implementation_to_be_called(array, :shelljoin, os)
+        joined = array.shelljoin
+        expect(joined).to eq(testcase['expect'][os])
+      end
+    end
+  end
+end
+    
+          it 'keeps the specified screenshots folder' do
+        expect(options[:screenshots_path]).to eq('./screenshots')
+      end
+    
+            # Executes a command on the remote machine with administrative
+        # privileges. See {#execute} for documentation, as the API is the
+        # same.
+        #
+        # @see #execute
+        def sudo(command, opts=nil)
+        end
+    
+            # Allows setting options from a hash. By default this simply calls
+        # the `#{key}=` method on the config class with the value, which is
+        # the expected behavior most of the time.
+        #
+        # This is expected to mutate itself.
+        #
+        # @param [Hash] options A hash of options to set on this configuration
+        #   key.
+        def set_options(options)
+          options.each do |key, value|
+            send('#{key}=', value)
+          end
+        end
+    
+            # Capture all bad configuration calls and save them for an error
+        # message later during validation.
+        def method_missing(name, *args, &block)
+          return super if @__finalized
+    
+              nil
+        end
