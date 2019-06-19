e9679fabe28bff6ebd5c7520b9078e3735b6668d
SUNsung <weatherius@gmail.com>
2019-06-19 16:01:16 +0300

    Rewrite [24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb] File in: 2019-06-19 16:01:16

--- ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
+++ ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
@@ -1,23 +1,23 @@
 
         
-              def test_helpers_with_module_through_block
-        @controller = AbstractHelpersBlock.new
-        @controller.process(:with_module)
-        assert_equal 'Module Included', @controller.response_body
-      end
-    end
+                # This method is expected to return a class that is used for
+        # configuring the provisioner. This return value is expected to be
+        # a subclass of {Config}.
+        #
+        # @return [Config]
+        def self.config_class
+        end
     
-      # Add groups and the proper project name to the output.
-  project_name 'Homebrew'
-  add_group 'Cask', %r{^/cask/}
-  add_group 'Commands', [%r{/cmd/}, %r{^/dev-cmd/}]
-  add_group 'Extensions', %r{^/extend/}
-  add_group 'OS', [%r{^/extend/os/}, %r{^/os/}]
-  add_group 'Requirements', %r{^/requirements/}
-  add_group 'Scripts', [
-    %r{^/brew.rb$},
-    %r{^/build.rb$},
-    %r{^/postinstall.rb$},
-    %r{^/test.rb$},
-  ]
-end
+    namespace :gem do
+  def version
+    require 'spree/core/version'
+    Spree.version
+  end
+    
+              def render_order(result)
+            if result.success?
+              render_serialized_payload { serialized_current_order }
+            else
+              render_error_payload(result.error)
+            end
+          end
