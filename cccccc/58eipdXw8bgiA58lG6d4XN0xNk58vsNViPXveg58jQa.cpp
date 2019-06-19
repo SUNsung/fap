dc44c5d2d6c748f21e5a94bfaf3780be5abf0632
SUNsung <weatherius@gmail.com>
2019-06-19 14:29:57 +0300

    Rewrite [58eipdXw8bgiA58lG6d4XN0xNk58vsNViPXveg58jQa.cpp] File in: 2019-06-19 14:29:57

--- cccccc/58eipdXw8bgiA58lG6d4XN0xNk58vsNViPXveg58jQa.cpp
+++ cccccc/58eipdXw8bgiA58lG6d4XN0xNk58vsNViPXveg58jQa.cpp
@@ -1,67 +1,237 @@
 
         
-        TEST(MovableMessageTest, SelfMoveAssignment) {
-  // The `self` reference is necessary to defeat -Wself-move.
-  protobuf_unittest::TestAllTypes message, &self = message;
-  TestUtil::SetAllFields(&message);
-  message = std::move(self);
-  TestUtil::ExpectAllFieldsSet(message);
-}
+        Status ModelAnalyzer::GenerateReport(bool debug, bool assume_valid_feeds,
+                                     std::ostream& os) {
+  GraphProperties properties(item_);
+  TF_RETURN_IF_ERROR(properties.InferStatically(assume_valid_feeds));
+    }
     
-      // Empty inputs.
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+      // Initializes the NumPy descriptor.
+  PyArray_InitArrFuncs(&NPyBfloat16_ArrFuncs);
+  NPyBfloat16_ArrFuncs.getitem = NPyBfloat16_GetItem;
+  NPyBfloat16_ArrFuncs.setitem = NPyBfloat16_SetItem;
+  NPyBfloat16_ArrFuncs.copyswapn = NPyBfloat16_CopySwapN;
+  NPyBfloat16_ArrFuncs.copyswap = NPyBfloat16_CopySwap;
+  NPyBfloat16_ArrFuncs.nonzero = NPyBfloat16_NonZero;
+  NPyBfloat16_ArrFuncs.fill = NPyBfloat16_Fill;
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // Returns the kernel class name required to execute <node_def> on the device
+// type of <node_def.device>, or an empty string if the kernel class is not
+// found or the device name is invalid.
+string TryFindKernelClass(const string& serialized_node_def);
+    
+    #ifndef TESSERACT_CCMAIN_OSDETECT_H_
+#define TESSERACT_CCMAIN_OSDETECT_H_
     
     
-    {
-    {
-    {}  // namespace io
-}  // namespace protobuf
-}  // namespace google
+    {  TBLOB* blob;
+  Tesseract* tesseract;
+  BLOB_CHOICE_LIST** choices;
+};
+    
+    struct PARA;
+    
+    // A CostFunc that takes the variance of step into account in the cost.
+int64_t DPPoint::CostWithVariance(const DPPoint* prev) {
+  if (prev == nullptr || prev == this) {
+    UpdateIfBetter(0, 1, nullptr, 0, 0, 0);
+    return 0;
+  }
+    }
+    
+    enum AmbigType {
+  NOT_AMBIG,        // the ngram pair is not ambiguous
+  REPLACE_AMBIG,    // ocred ngram should always be substituted with correct
+  DEFINITE_AMBIG,   // add correct ngram to the classifier results (1-1)
+  SIMILAR_AMBIG,    // use pairwise classifier for ocred/correct pair (1-1)
+  CASE_AMBIG,       // this is a case ambiguity (1-1)
+    }
+    
+    #endif  // MXNET_USE_CUDA && MXNET_ENABLE_CUDA_RTC
+#endif  // MXNET_RTC_H_
 
     
-    void MapLiteTestUtil::ExpectMapFieldsModified(
-    const unittest::TestMapLite& message) {
-  MapTestUtilImpl::ExpectMapFieldsModified<unittest::MapEnumLite,
-                                           unittest::MAP_ENUM_BAR_LITE,
-                                           unittest::MAP_ENUM_FOO_LITE>(
-      message);
-}
+    #ifndef PLUGIN_CAFFE_CAFFE_COMMON_H_
+#define PLUGIN_CAFFE_CAFFE_COMMON_H_
     
-    TEST(StatusOr, TestPointerValue) {
-  const int kI = 0;
-  StatusOr<const int*> thing(&kI);
-  EXPECT_EQ(&kI, thing.ValueOrDie());
-}
-    
-    class Proto3DataStripper : public DataStripper {
- private:
-  virtual bool ShouldBeClear(const FieldDescriptor *field) {
-    return field->type() == FieldDescriptor::TYPE_GROUP ||
-           field->is_extension();
-  }
-};
-    
-    
-    {
-    {
-    {}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-    
-    #ifndef GOOGLE_PROTOBUF_COMPILER_OBJECTIVEC_ENUM_H__
-#define GOOGLE_PROTOBUF_COMPILER_OBJECTIVEC_ENUM_H__
-    
-    // Possible normalization methods. Use NEGATIVE values as these also
-// double up as markers for the last sub-classifier.
-enum NormalizationMode {
-  NM_BASELINE = -3,         // The original BL normalization mode.
-  NM_CHAR_ISOTROPIC = -2,   // Character normalization but isotropic.
-  NM_CHAR_ANISOTROPIC = -1  // The original CN normalization mode.
-};
-    
-    
-    {    while (next_num_ < kMaxNaturalNumberValue) {
-      n = GetBinaryReversedInteger(next_num_++);
-      if (n < N_) break;
+    /*!
+ * \brief Thread pool.
+ */
+class ThreadPool {
+ public:
+  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
+  struct SetReadyOnDestroy {
+    explicit inline SetReadyOnDestroy(const std::shared_ptr<dmlc::ManualEvent>& event)
+      : event_(event) {
     }
-    return (next_num_ > kMaxNaturalNumberValue) ? kInvalidVal : n;
+    inline ~SetReadyOnDestroy() {
+      if (event_) {
+        event_->signal();
+      }
+    }
+    std::shared_ptr<dmlc::ManualEvent>  event_;
+  };
+    }
+    
+    
+    {
+    {.add_argument('data', 'Symbol or Symbol[]', 'Tensor or List of Tensors, the second input '
+'will be used as crop_like shape reference')
+.add_arguments(CropParam::__FIELDS__())
+.set_key_var_num_args('num_args');
+}  // namespace op
+}  // namespace mxnet
+
+    
+    #pragma once
+    
+    
+    {                unpackedShape = unpackedShape.AppendShape({ packedDataLayout->GetNumSequences() });
+            }
+            else if (!sampleDynamicAxes.empty())
+                LogicError('A PackedValue object that does not have a layout cannot have any dynamic axes.');
+    
+    // GetHmmData - Get the HMM definition for SE training
+// hmm - HMM definition
+// returns - true if succeed
+bool DataReader::GetHmmData(msra::asr::simplesenonehmm* hmm)
+{
+    bool bRet = true;
+    for (size_t i = 0; i < m_ioNames.size(); i++)
+        bRet &= m_dataReaders[m_ioNames[i]]->GetHmmData(hmm);
+    return bRet;
+}
+    
+    public:
+    ScopeTimer(size_t verbosity, const std::string& message)
+        : m_verbosity(verbosity), m_message(message)
+    {
+        if (m_verbosity > 2)
+        {
+            m_aggregateTimer.Start();
+        }
+    }
+    
+    // ===================================================================
+// ComputationNetworkFromFile
+// scripting wrapper to construct ComputationNetwork from file (aka 'Load')
+// ===================================================================
+    
+            if (
+            // Test condition 1.
+            (dimsA.size() == 3 && dimsA[0] == 1 && dimsA[1] == 1) &&
+            // Test condition 2.
+            (dimsB.size() == 2 && dimsB[1] == 1) &&
+            (dimsC.size() == 2 && dimsC[1] == 1) &&
+            // Test condition 3. and condition 4.
+            (dimsB[0] == dimsC[0] && dimsB[0] == dimsA[2])
+            )
+        {
+            // for error messages
+            string dimsBstring = string(Input(1)->GetSampleLayout());
+            string dimsCstring = string(Input(2)->GetSampleLayout());
+    }
+    
+    // TODO: can this be static?
+template <class ElemType>
+void Microsoft::MSR::CNTK::UpdateRunningAverage(ComputationNode<ElemType>& newInput,
+                                                TensorView<ElemType>& runningAverage, size_t& runningCount)
+{
+    FrameRange fr(newInput.GetMBLayout());
+    // Set gaps to zero, since we are reducing in time.
+    newInput.MaskMissingValueColumnsToZero(fr);
+    }
+    
+    IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_Init();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data);
+    
+    // The data is first compressed with stb_compress() to reduce source code size,
+// then encoded in Base85 to fit in a string so we can fit roughly 4 bytes of compressed data into 5 bytes of source code (suggested by @mmalex)
+// (If we used 32-bits constants it would require take 11 bytes of source code to encode 4 bytes, and be endianness dependent)
+// Note that even with compression, the output array is likely to be bigger than the binary file..
+// Load compressed TTF fonts with ImGui::GetIO().Fonts->AddFontFromMemoryCompressedTTF()
+    
+    // Compatibility:
+// - std::string support is only guaranteed to work from C++11.
+//   If you try to use it pre-C++11, please share your findings (w/ info about compiler/architecture)
+    
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_IMPL_API void     ImGui_ImplDX10_InvalidateDeviceObjects();
+IMGUI_IMPL_API bool     ImGui_ImplDX10_CreateDeviceObjects();
+
+    
+    
+    {    // Create texture sampler
+    {
+        D3D10_SAMPLER_DESC desc;
+        ZeroMemory(&desc, sizeof(desc));
+        desc.Filter = D3D10_FILTER_MIN_MAG_MIP_LINEAR;
+        desc.AddressU = D3D10_TEXTURE_ADDRESS_WRAP;
+        desc.AddressV = D3D10_TEXTURE_ADDRESS_WRAP;
+        desc.AddressW = D3D10_TEXTURE_ADDRESS_WRAP;
+        desc.MipLODBias = 0.f;
+        desc.ComparisonFunc = D3D10_COMPARISON_ALWAYS;
+        desc.MinLOD = 0.f;
+        desc.MaxLOD = 0.f;
+        g_pd3dDevice->CreateSamplerState(&desc, &g_pFontSampler);
+    }
+}
+    
+    // Implemented features:
+//  [X] Renderer: User texture binding. Use 'D3D12_GPU_DESCRIPTOR_HANDLE' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+//  [X] Renderer: Support for large meshes (64k+ vertices) with 16-bits indices.
+// Issues:
+//  [ ] 64-bit only for now! (Because sizeof(ImTextureId) == sizeof(void*)). See github.com/ocornut/imgui/pull/301
+    
+    IMGUI_IMPL_API bool     ImGui_ImplDX9_Init(IDirect3DDevice9* device);
+IMGUI_IMPL_API void     ImGui_ImplDX9_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplDX9_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
+    
+      void set_max_delayed_write_rate(uint64_t write_rate) {
+    // avoid divide 0
+    if (write_rate == 0) {
+      write_rate = 1u;
+    }
+    max_delayed_write_rate_ = write_rate;
+    // update delayed_write_rate_ as well
+    delayed_write_rate_ = write_rate;
   }
+    
+      // Attempt to read a key using the snapshot.  This will fail since
+  // the previous write outside this txn conflicts with this read.
+  read_options.snapshot = snapshot;
+  s = txn->GetForUpdate(read_options, 'abc', &value);
+  assert(s.IsBusy());
+    
+    
+    {  virtual ~Checkpoint() {}
+};
+    
+      // Amount of data to build up in memory (backed by an unsorted log
+  // on disk) before converting to a sorted on-disk file.
+  //
+  // Larger values increase performance, especially during bulk loads.
+  // Up to two write buffers may be held in memory at the same time,
+  // so you may wish to adjust this parameter to control memory usage.
+  // Also, a larger write buffer will result in a longer recovery time
+  // the next time the database is opened.
+  //
+  // Default: 4MB
+  size_t write_buffer_size;
