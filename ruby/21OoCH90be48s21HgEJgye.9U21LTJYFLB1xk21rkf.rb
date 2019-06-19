f15170ef1e94b7ff075e338104904424885de015
SUNsung <weatherius@gmail.com>
2019-06-19 12:41:21 +0300

    Rewrite [21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb] File in: 2019-06-19 12:41:21

--- ruby/21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb
+++ ruby/21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb
@@ -1,168 +1,100 @@
 
         
-            # This returns whether the guest is ready to work. If this returns
-    # `false`, then {#detect!} should be called in order to detect the
-    # guest OS.
-    #
-    # @return [Boolean]
-    def ready?
-      !!capability_host_chain
-    end
-  end
-end
-
-    
-            # Returns the instance variables as a hash of key-value pairs.
-        def instance_variables_hash
-          instance_variables.inject({}) do |acc, iv|
-            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
-            acc
-          end
-        end
-    
-            # Set the name of the plugin. The moment that this is called, the
-        # plugin will be registered and available. Before this is called, a
-        # plugin does not exist. The name must be unique among all installed
-        # plugins.
-        #
-        # @param [String] name Name of the plugin.
-        # @return [String] The name of the plugin.
-        def self.name(name=UNSET_VALUE)
-          # Get or set the value first, so we have a name for logging when
-          # we register.
-          result = get_or_set(:name, name)
-    
-            # This method is expected to return a class that is used for
-        # configuring the provisioner. This return value is expected to be
-        # a subclass of {Config}.
-        #
-        # @return [Config]
-        def self.config_class
-        end
-    
-            # This contains all the push implementations by name.
-        #
-        # @return [Registry<Symbol, Array<Class, Hash>>]
-        attr_reader :pushes
-    
-    module Vagrant
-  module Plugin
-    module V2
-      # This is the base class for a configuration key defined for
-      # V2. Any configuration key plugins for V2 should inherit from this
-      # class.
-      class Config
-        # This constant represents an unset value. This is useful so it is
-        # possible to know the difference between a configuration value that
-        # was never set, and a value that is nil (explicitly). Best practice
-        # is to initialize all variables to this value, then the {#merge}
-        # method below will 'just work' in many cases.
-        UNSET_VALUE = Object.new
-    
-              @registered.each do |plugin|
-            plugin.components.guest_capabilities.each do |guest, caps|
-              results[guest].merge!(caps)
-            end
-          end
-    
-            # This should return the state of the machine within this provider.
-        # The state must be an instance of {MachineState}. Please read the
-        # documentation of that class for more information.
-        #
-        # @return [MachineState]
-        def state
-          nil
-        end
-    
-        def __internal_state
-      {
-        items: @items,
-        results_cache: @results_cache
-      }
-    end
-  end
-end
-
-    
-      # :stopdoc:
-  def _dispatch_0() nil end
-  def _dispatch_1(a) a end
-  def _dispatch_2(a, b) a end
-  def _dispatch_3(a, b, c) a end
-  def _dispatch_4(a, b, c, d) a end
-  def _dispatch_5(a, b, c, d, e) a end
-  def _dispatch_6(a, b, c, d, e, f) a end
-  def _dispatch_7(a, b, c, d, e, f, g) a end
-  # :startdoc:
-    
-    mkpieces
-mkboard
-$p[4] = [$p[4][0]]
-$pnum = (0...$p.length).to_a
-setpiece([],0)
-
-    
-      class PostToService < Base
-    def perform(*_args)
-      # don't post to services in cucumber
-    end
-  end
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-        it 'supports a limit per_page parameter' do
-      2.times { FactoryGirl.create(:notification, :recipient => alice, :target => @post) }
-      get :index, params: {per_page: 2}
-      expect(assigns[:notifications].count).to eq(2)
-    end
-    
-            it 'doesn\'t allow the user to reshare the post again' do
-          post_request!
-          expect(response.code).to eq('422')
-          expect(response.body).to eq(I18n.t('reshares.create.error'))
+            all_assertions do |a|
+      [ints, floats].each do |values|
+        values.combination(2).to_a.product(values).each do |(from, to), search|
+          check_bsearch_values(from..to, search, a)
+          check_bsearch_values(from...to, search, a)
         end
       end
-    
-          def call(env)
-        status, headers, body = super
-        response = Rack::Response.new(body, status, headers)
-        request = Rack::Request.new(env)
-        remove_bad_cookies(request, response)
-        response.finish
-      end
-    
-        post('/', {'csrf_param' => token}, 'rack.session' => {:csrf => token})
-    expect(last_response).to be_ok
-  end
-    
-    desc 'Test the paperclip plugin under all supported Rails versions.'
-task :all do |t|
-  if ENV['BUNDLE_GEMFILE']
-    exec('rake spec && cucumber')
-  else
-    exec('rm -f gemfiles/*.lock')
-    Rake::Task['appraisal:gemfiles'].execute
-    Rake::Task['appraisal:install'].execute
-    exec('rake appraisal')
-  end
-end
-    
-    When /^(?:|I )go to (.+)$/ do |page_name|
-  visit path_to(page_name)
-end
-    
-        EMPTY_TYPE = 'inode/x-empty'
-    SENSIBLE_DEFAULT = 'application/octet-stream'
-    
-        def define
-      define_flush_errors
-      define_getters
-      define_setter
-      define_query
-      register_new_attachment
-      add_active_record_callbacks
-      add_paperclip_callbacks
-      add_required_validations
     end
+  end
+    
+      def test_popenv_r_enc_enc_in_opt2
+    IO.popen([EnvUtil.rubybin, '-e', 'putc 0xa1'], 'r', external_encoding: 'shift_jis', internal_encoding: 'euc-jp') {|f|
+      assert_equal(Encoding::Shift_JIS, f.external_encoding)
+      assert_equal(Encoding::EUC_JP, f.internal_encoding)
+      s = f.read
+      assert_equal(Encoding::EUC_JP, s.encoding)
+      assert_equal('\x8e\xa1'.force_encoding('euc-jp'), s)
+    }
+  end
+    
+      _                                                     UScore
+  '/*NULL*/'                                            NULL
+  empty?
+  eql?
+  respond_to?                                           Respond_to
+  respond_to_missing?                                   Respond_to_missing
+  <IFUNC>
+  <CFUNC>
+  core#set_method_alias
+  core#set_variable_alias
+  core#undef_method
+  core#define_method
+  core#define_singleton_method
+  core#set_postexe
+  core#hash_merge_ptr
+  core#hash_merge_kwd
+  core#raise
+    
+              unless task = @tasks.shift
+            worker.quit
+            return nil
+          end
+          if @options[:separate] and not bang
+            worker.quit
+            worker = launch_worker
+          end
+          worker.run(task, type)
+          @test_count += 1
+    
+      caveats <<~EOS
+    Installation or Uninstallation may fail with Exit Code 19 (Conflicting Processes running) if Browsers, Safari Notification Service or SIMBL Services (e.g. Flashlight) are running or Adobe Creative Cloud or any other Adobe Products are already installed. See Logs in /Library/Logs/Adobe/Installers if Installation or Uninstallation fails, to identifify the conflicting processes.
+  EOS
+end
+
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
+    end
+    
+      class ImageTag < Liquid::Tag
+    @img = nil
+    
+          Dir.chdir(code_path) do
+        code = file.read
+        @filetype = file.extname.sub('.','') if @filetype.nil?
+        title = @title ? '#{@title} (#{file.basename})' : file.basename
+        url = '/#{code_dir}/#{@file}'
+        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
+        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
+        TemplateWrapper::safe_wrap(source)
+      end
+    end
+  end
