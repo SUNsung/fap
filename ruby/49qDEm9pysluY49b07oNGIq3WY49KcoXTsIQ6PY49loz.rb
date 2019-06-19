d543872fc8f37fcec10cc18613e5f5467e941e05
SUNsung <weatherius@gmail.com>
2019-06-19 19:56:15 +0300

    Rewrite [49qDEm9pysluY49b07oNGIq3WY49KcoXTsIQ6PY49loz.rb] File in: 2019-06-19 19:56:14

--- ruby/49qDEm9pysluY49b07oNGIq3WY49KcoXTsIQ6PY49loz.rb
+++ ruby/49qDEm9pysluY49b07oNGIq3WY49KcoXTsIQ6PY49loz.rb
@@ -1,124 +1,202 @@
 
         
-                      # Rename :minute and :second to :min and :sec
-              default[:min] ||= default[:minute]
-              default[:sec] ||= default[:second]
+            it 'creates an agent with a source and a receiver' do
+      visit '/'
+      page.find('a', text: 'Agents').trigger(:mouseover)
+      click_on('New Agent')
     
-          delegate_to Type
+      it 'asks to accept conflicts when the scenario was modified' do
+    DefaultScenarioImporter.seed(user)
+    agent = user.agents.where(name: 'Rain Notifier').first
+    agent.options['expected_receive_period_in_days'] = 9001
+    agent.save!
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
+    click_on 'Start Import'
+    expect(page).to have_text('This Scenario already exists in your system.')
+    expect(page).to have_text('9001')
+    check('I confirm that I want to import these Agents.')
+    click_on 'Finish Import'
+    expect(page).to have_text('Import successful!')
+  end
     
-        attr_reader :filters
-    
-        def effective_path
-      @effective_path ||= effective_url.path
+        it 'returns a label 'Missing Gems' if a given agent has dependencies missing' do
+      stub(@agent).dependencies_missing? { true }
+      label = working(@agent)
+      expect(label).to be_html_safe
+      expect(Nokogiri(label).text).to eq 'Missing Gems'
     end
+    
+          it 'generates a DOT script' do
+        expect(agents_dot(@agents)).to match(%r{
+          \A
+          digraph \x20 'Agent \x20 Event \x20 Flow' \{
+            node \[ [^\]]+ \];
+            edge \[ [^\]]+ \];
+            (?<foo>\w+) \[label=foo\];
+            \k<foo> -> (?<bar1>\w+) \[style=dashed\];
+            \k<foo> -> (?<bar2>\w+) \[color='\#999999'\];
+            \k<bar1> \[label=bar1\];
+            \k<bar2> \[label=bar2,style='rounded,dashed',color='\#999999',fontcolor='\#999999'\];
+            \k<bar2> -> (?<bar3>\w+) \[style=dashed,color='\#999999'\];
+            \k<bar3> \[label=bar3\];
+          \}
+          \z
+        }x)
+      end
+    
+        it 'should provide the since attribute after the first run' do
+      time = (Time.now-1.minute).iso8601
+      @checker.memory[:last_event] = time
+      @checker.save
+      expect(@checker.reload.send(:query_parameters)).to eq({:query => {:since => time}})
+    end
+  end
+    
+    Rails.application.initialize!
+    
+      private
+    
+      def future_expires
+    Time.now.utc + lease_seconds_or_default
+  end
+    
+      private
+    
+      def user_needs_sign_in_update?
+    user_signed_in? && (current_user.current_sign_in_at.nil? || current_user.current_sign_in_at < UPDATE_SIGN_IN_HOURS.hours.ago)
   end
 end
 
     
-        self.params = {}
-    self.headers = { 'User-Agent' => 'DevDocs' }
-    self.force_gzip = false
+            # Receives a Kerberos Response over a tcp connection
+        #
+        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] the kerberos message response
+        # @raise [RuntimeError] if the response can't be processed
+        # @raise [EOFError] if expected data can't be read
+        def recv_response_tcp
+          length_raw = connection.get_once(4, timeout)
+          unless length_raw && length_raw.length == 4
+            raise ::RuntimeError, 'Kerberos Client: failed to read response'
+          end
+          length = length_raw.unpack('N')[0]
     
-            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
-          node.before(node.children).remove
-        end
+              # Encodes the components field
+          #
+          # @return [String]
+          def encode_components
+            encoded = ''
     
-        def deconstruct_keys(keys)
-      C.keys = keys
-      @obj
-    end
-  end
+              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
+          #
+          # @return [String] encoded time
+          def encode
+            encoded = ''
+            encoded << encode_auth_time
+            encoded << encode_start_time
+            encoded << encode_end_time
+            encoded << encode_renew_time
     
-    $testnum=0
-$ntest=0
-$failed = 0
-class Progress
-  def initialize
-    @color = nil
-    @tty = nil
-    @quiet = nil
-    @verbose = nil
-    ARGV.each do |arg|
-      case arg
-      when /\A--color(?:=(?:always|(auto)|(never)|(.*)))?\z/
-        warn 'unknown --color argument: #$3' if $3
-        @color = $1 ? nil : !$2
-      when /\A--tty(=(?:yes|(no)|(.*)))?\z/
-        warn 'unknown --tty argument: #$3' if $3
-        @tty = !$1 || !$2
-        true
-      when /\A-(q|-quiet)\z/
-        @quiet = true
-      when /\A-(v|-verbose)\z/
-        @verbose = true
-      end
-    end
-    @tty = STDERR.tty? && !STDOUT.tty? && /dumb/ !~ ENV['TERM'] if @tty.nil?
-    @eol = @tty && !@verbose ? '\r\e[K\r' : '\n'
-    case @color
-    when nil
-      @color = @tty
-    end
-    if @color
-      # dircolors-like style
-      colors = (colors = ENV['TEST_COLORS']) ? Hash[colors.scan(/(\w+)=([^:\n]*)/)] : {}
-      begin
-        File.read(File.join(__dir__, '../test/colors')).scan(/(\w+)=([^:\n]*)/) do |n, c|
-          colors[n] ||= c
-        end
-      rescue
-      end
-      @passed = '\e[;#{colors['pass'] || '32'}m'
-      @failed = '\e[;#{colors['fail'] || '31'}m'
-      @reset = '\e[m'
-    else
-      @passed = @failed = @reset = ''
-    end
-    extend(Rotator) if @tty
-  end
-    
-    undef printf
-alias printf printf_orig
-
-    
-          def get_installer_for(plugin_name)
-        uri = pack_uri(plugin_name)
-    
-      def execute
-    signal_deprecation_warning_for_pack
-    
-      it 'does object equality on config_hash and pipeline_id' do
-    another_exact_pipeline = described_class.new(source, pipeline_id, ordered_config_parts, settings)
-    expect(subject).to eq(another_exact_pipeline)
-    
-              it 'allow to install a specific version' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version 0.1.0 logstash-filter-qatest')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest', '0.1.0')
+              # Encodes the authenticator field
+          #
+          # @return [String]
+          def encode_authenticator
+            authenticator.encode
           end
         end
       end
-    
-    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
-
-    
-        def poster
-      'poster='#{@poster}'' if @poster
     end
-    
-    Given /^(?:|I )am on (.+)$/ do |page_name|
-  visit path_to(page_name)
+  end
 end
     
-      def migration_name
-    'add_attachment_#{attachment_names.join('_')}_to_#{name.underscore.pluralize}'
+                seq.to_der
+          end
+    
+              # Decodes the cipher from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Sting]
+          def decode_cipher(input)
+            input.value[0].value
+          end
+    
+              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_asn1_pvno(input)
+            input.value[0].value.to_i
+          end
+    
+                self
+          end
+    
+    require_relative '../lib/bootstrap/environment'
+    
+      it 'records when the config was read' do
+    expect(subject.read_at).to be <= Time.now
   end
     
-        # Returns the underscored, pluralized version of the class name.
-    # e.g. 'users' for the User class.
-    # NOTE: The arguments need to be optional, because some tools fetch
-    # all class names. Calling #class will return the expected class.
-    def class attachment = nil, style_name = nil
-      return super() if attachment.nil? && style_name.nil?
-      plural_cache.underscore_and_pluralize_class(attachment.instance.class)
+          options = {:debug => ENV['LS_QA_DEBUG']}
+      puts 'Destroying #{machines}'
+      LogStash::VagrantHelpers.destroy(machines, options)
+      puts 'Bootstrapping #{machines}'
+      LogStash::VagrantHelpers.bootstrap(machines, options)
     end
+    
+        context 'without a specific plugin' do
+      it 'display a list of plugins' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list')
+        expect(result.stdout.split('\n').size).to be > 1
+      end
+    
+        def paragraphize(input)
+      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
+    end
+  end
+end
+    
+        # Initializes a new CategoryFeed.
+    #
+    #  +base+         is the String path to the <source>.
+    #  +category_dir+ is the String path between <source> and the category folder.
+    #  +category+     is the category currently being processed.
+    def initialize(site, base, category_dir, category)
+      @site = site
+      @base = base
+      @dir  = category_dir
+      @name = 'atom.xml'
+      self.process(@name)
+      # Read the YAML data from the layout page.
+      self.read_yaml(File.join(base, '_includes/custom'), 'category_feed.xml')
+      self.data['category']    = category
+      # Set the title for this page.
+      title_prefix             = site.config['category_title_prefix'] || 'Category: '
+      self.data['title']       = '#{title_prefix}#{category}'
+      # Set the meta-description for this page.
+      meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
+      self.data['description'] = '#{meta_description_prefix}#{category}'
+    
+        def render(context)
+      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
+        gist, file = parts[1].strip, parts[2].strip
+      else
+        gist, file = @text.strip, ''
+      end
+      if gist.empty?
+        ''
+      else
+        script_url = script_url_for gist, file
+        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
+        html_output_for script_url, code
+      end
+    end
+    
+    module Jekyll
+    
+    Liquid::Template.register_tag('include_code', Jekyll::IncludeCodeTag)
+
+    
+          unless file.file?
+        return 'File #{file} could not be found'
+      end
