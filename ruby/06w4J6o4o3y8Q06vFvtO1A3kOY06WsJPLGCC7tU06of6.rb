fe3b68e9f49eb5087ab2a9f54f319be2daeaa7f1
SUNsung <weatherius@gmail.com>
2019-06-20 14:11:39 +0300

    Rewrite [06w4J6o4o3y8Q06vFvtO1A3kOY06WsJPLGCC7tU06of6.rb] File in: 2019-06-20 14:11:39

--- ruby/06w4J6o4o3y8Q06vFvtO1A3kOY06WsJPLGCC7tU06of6.rb
+++ ruby/06w4J6o4o3y8Q06vFvtO1A3kOY06WsJPLGCC7tU06of6.rb
@@ -1,150 +1,151 @@
 
         
-                    # Prepend a hidden field to make sure something will be sent back to the
-            # server if all radio buttons are unchecked.
-            if options.fetch('include_hidden', true)
-              hidden_field + rendered_collection
-            else
-              rendered_collection
-            end
-          end
-    
-              def render_component(builder)
-            builder.radio_button + builder.label
-          end
+              command.args.each do |arg|
+        arg_value = arg.value
+        if arg.value_type.to_s.to_sym == :string_closure
+          closure = proc { |string_value| closure_argument_value = string_value }
+          arg_value = closure
+        end
+        parameter_map[arg.name.to_sym] = arg_value
       end
+    
+          @launch_event_sent = true
+      builder = AnalyticsEventBuilder.new(
+        p_hash: launch_context.p_hash,
+        session_id: session_id,
+        action_name: nil,
+        fastlane_client_language: launch_context.fastlane_client_language
+      )
+    
+            expect(result).to eq('git tag -am #{message.shellescape} --force #{tag.shellescape}')
+      end
+    
+        # wrap in double quotes if contains space
+    if str =~ /\s/
+      # double quotes have to be doubled if will be quoted
+      str.gsub!(''', '''')
+      return ''' + str + '''
+    else
+      return str
     end
   end
+  module_function :shellescape
 end
 
     
-                  Time.utc(
-                default[:year], default[:month], default[:day],
-                default[:hour], default[:min], default[:sec]
-              )
-            end
-          end
-      end
+        # Returns the specified or detected guest type name.
+    #
+    # @return [Symbol]
+    def name
+      capability_host_chain[0][0]
     end
+    
+            # Execute a command on the remote machine. The exact semantics
+        # of this method are up to the implementor, but in general the
+        # users of this class will expect this to be a shell.
+        #
+        # This method gives you no way to write data back to the remote
+        # machine, so only execute commands that don't expect input.
+        #
+        # @param [String] command Command to execute.
+        # @yield [type, data] Realtime output of the command being executed.
+        # @yieldparam [String] type Type of the output. This can be
+        #   `:stdout`, `:stderr`, etc. The exact types are up to the
+        #   implementor.
+        # @yieldparam [String] data Data for the given output.
+        # @return [Integer] Exit code of the command.
+        def execute(command, opts=nil)
+        end
+    
+            # Configures the given list of networks on the virtual machine.
+        #
+        # The networks parameter will be an array of hashes where the hashes
+        # represent the configuration of a network interface. The structure
+        # of the hash will be roughly the following:
+        #
+        # {
+        #   type:      :static,
+        #   ip:        '192.168.33.10',
+        #   netmask:   '255.255.255.0',
+        #   interface: 1
+        # }
+        #
+        def configure_networks(networks)
+          raise BaseError, _key: :unsupported_configure_networks
+        end
+    
+            protected
+    
+            # This contains all the command plugins by name, and returns
+        # the command class and options. The command class is wrapped
+        # in a Proc so that it can be lazy loaded.
+        #
+        # @return [Registry<Symbol, Array<Proc, Hash>>]
+        attr_reader :commands
+    
+        # Returns an array populated with the keys of this object.
+    #
+    # @return [Array]
+    def keys
+      @items.keys
+    end
+    
+    get '/' do
+  stats = Sidekiq::Stats.new
+  @failed = stats.failed
+  @processed = stats.processed
+  @messages = $redis.lrange('sinkiq-example-messages', 0, -1)
+  erb :index
+end
+    
+    # Use this to fill in an entire form with data from a table. Example:
+#
+#   When I fill in the following:
+#     | Account Number | 5002       |
+#     | Expiry date    | 2009-11-01 |
+#     | Note           | Nice guy   |
+#     | Wants Email?   |            |
+#
+# TODO: Add support for checkbox, select og option
+# based on naming conventions.
+#
+When /^(?:|I )fill in the following:$/ do |fields|
+  fields.rows_hash.each do |name, value|
+    When %{I fill in '#{name}' with '#{value}'}
   end
 end
-
     
-              label_tag(name_and_id['id'], content, options)
+    module RailsCommandHelpers
+  def framework_version?(version_string)
+    framework_version =~ /^#{version_string}/
+  end
+    
+      def migration_file_name
+    '#{migration_name}.rb'
+  end
+    
+        def type_from_file_contents
+      type_from_mime_magic || type_from_file_command
+    rescue Errno::ENOENT => e
+      Paperclip.log('Error while determining content type: #{e}')
+      SENSIBLE_DEFAULT
+    end
+    
+        private
+    
+          it { expect(parsed_json.keys).to include('url') }
+      it { expect(parsed_json.keys).to include('thumb') }
+      it { expect(parsed_json['url']).to be_nil }
+      it { expect(parsed_json['thumb'].keys).to include('url') }
+      it { expect(parsed_json['thumb']['url']).to be_nil }
+    
+    When /^I save the datamapper record$/ do
+  @instance.save
+end
+    
+          def check_content_type_blacklist!(new_file)
+        content_type = new_file.content_type
+        if content_type_blacklist && blacklisted_content_type?(content_type)
+          raise CarrierWave::IntegrityError, I18n.translate(:'errors.messages.content_type_blacklist_error', content_type: content_type)
         end
-    
-    module ActionView
-  module Helpers
-    module Tags # :nodoc:
-      class TextField < Base # :nodoc:
-        include Placeholderable
-    
-      def self.req_id(date, req_type, retries = 0)
-    
-      def self.period_types
-    @types ||= Enum.new(all: 1,
-                        yearly: 2,
-                        monthly: 3,
-                        weekly: 4,
-                        daily: 5,
-                        quarterly: 6)
-  end
-    
-      describe '#<<' do
-    it 'normalizes directive name' do
-      builder << {
-        script_src: ['symbol_underscore'],
-        'script-src': ['symbol_dash'],
-        'script_src' => ['string_underscore'],
-        'script-src' => ['string_dash'],
-      }
-    
-        def clear_theme_extensions_cache!
-      cache.clear
-    end
-    
-      # topic_reply_count is a count of posts in other users' topics
-  def update_topic_reply_count
-    self.topic_reply_count = Topic
-      .joins('INNER JOIN posts ON topics.id = posts.topic_id AND topics.user_id <> posts.user_id')
-      .where('posts.deleted_at IS NULL AND posts.user_id = ?', self.user_id)
-      .distinct
-      .count
-  end
-    
-              if env[Auth::DefaultCurrentUserProvider::BAD_TOKEN]
-            headers['Discourse-Logged-Out'] = '1'
-          end
-    
-        if user_token.auth_token != token && user_token.prev_auth_token == token && user_token.auth_token_seen
-      changed_rows = UserAuthToken
-        .where('rotated_at < ?', 1.minute.ago)
-        .where(id: user_token.id, prev_auth_token: token)
-        .update_all(auth_token_seen: false)
-    
-        def current_url
-      context[:url]
-    end
-    
-        def name=(value)
-      @name = value.try :strip
-    end
-    
-            options.merge!(additional_options)
-        options.freeze
       end
-    end
-    
-            css('pre[language]').each do |node|
-          node['data-language'] = node['language'].sub(/\Ats/, 'typescript').strip
-          node['data-language'] = 'html' if node.content.start_with?('<')
-          node.remove_attribute('language')
-          node.remove_attribute('format')
-        end
-    
-      included do
-    before_action :set_session_activity
-  end
-    
-    class RemoteUnfollowsController < ApplicationController
-  layout 'modal'
-    
-    World(RemoteCommandHelpers)
-
-    
-    World(VagrantHelpers)
-
-    
-        def configure_scm
-      Capistrano::Configuration::SCMResolver.new.resolve
-    end
-    
-          def gets
-        return unless $stdin.tty?
-    
-          def each
-        servers_by_key.values.each { |server| yield server }
-      end
-    
-    module Capistrano
-  class Configuration
-    # Decorates a Variables object to additionally perform an optional set of
-    # user-supplied validation rules. Each rule for a given key is invoked
-    # immediately whenever `set` is called with a value for that key.
-    #
-    # If `set` is called with a callable value or a block, validation is not
-    # performed immediately. Instead, the validation rules are invoked the first
-    # time `fetch` is used to access the value.
-    #
-    # A rule is simply a block that accepts two arguments: key and value. It is
-    # up to the rule to raise an exception when it deems the value is invalid
-    # (or just print a warning).
-    #
-    # Rules can be registered using the DSL like this:
-    #
-    #   validate(:my_key) do |key, value|
-    #     # rule goes here
-    #   end
-    #
-    class ValidatedVariables < SimpleDelegator
-      include Capistrano::ProcHelpers
