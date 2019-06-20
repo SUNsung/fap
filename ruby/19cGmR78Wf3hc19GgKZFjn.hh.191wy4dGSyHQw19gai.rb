ca9c9d26584fcbc8b3338eaa20976a55fc6ccf0e
SUNsung <weatherius@gmail.com>
2019-06-20 11:12:49 +0300

    Rewrite [19cGmR78Wf3hc19GgKZFjn.hh.191wy4dGSyHQw19gai.rb] File in: 2019-06-20 11:12:49

--- ruby/19cGmR78Wf3hc19GgKZFjn.hh.191wy4dGSyHQw19gai.rb
+++ ruby/19cGmR78Wf3hc19GgKZFjn.hh.191wy4dGSyHQw19gai.rb
@@ -1,48 +1,26 @@
 
         
-          def report
-    @report ||= JSON.parse(request.body.read)['csp-report'].slice(
-      'blocked-uri',
-      'disposition',
-      'document-uri',
-      'effective-directive',
-      'original-policy',
-      'referrer',
-      'script-sample',
-      'status-code',
-      'violated-directive',
-      'line-number',
-      'source-file'
-    )
-  end
-    
+            def require_local_account!
+      redirect_to admin_account_path(@account.id) unless @account.local? && @account.user.present?
     end
     
-          @directives[directive] ||= []
+      private
     
-        THEME_SETTING = 'extend_content_security_policy'
-    
-    require 'builder'
-require 'feedbag'
-require 'json'
-require 'nokogiri'
-    
-        w.uid = 'git'
-    w.gid = 'git'
-    
-      def failed_order
-    params[:order] || 'desc'
+      def payload
+    @_payload ||= request.body.read
   end
     
-      it 'exits with Resque::TermException when using TERM_CHILD and not forking' do
-    old_job_per_fork = ENV['FORK_PER_JOB']
-    begin
-      ENV['FORK_PER_JOB'] = 'false'
-      worker_pid, child_pid, _result = start_worker(0, true)
-      assert_equal worker_pid, child_pid, 'child_pid should equal worker_pid, since we are not forking'
-      assert Resque.redis.lpop( 'sigterm-test:ensure_block_executed' ), 'post cleanup did not occur. SIGKILL sent too early?'
-    ensure
-      ENV['FORK_PER_JOB'] = old_job_per_fork
-    end
+      def rate_limit_reset
+    (request_time + reset_period_offset).iso8601(6)
   end
-end
+    
+      included do
+    before_action :set_user_activity
+  end
+    
+    require 'active_support/core_ext/string/inflections'
+require 'cocoapods/command/spec/create'
+require 'cocoapods/command/spec/lint'
+require 'cocoapods/command/spec/which'
+require 'cocoapods/command/spec/cat'
+require 'cocoapods/command/spec/edit'
