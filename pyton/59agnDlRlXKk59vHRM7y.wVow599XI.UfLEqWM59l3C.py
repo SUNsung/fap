2013db001a7364298648857e740717a01bcdc3ab
SUNsung <weatherius@gmail.com>
2019-06-21 07:31:42 +0300

    Rewrite [59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py] File in: 2019-06-21 07:31:41

--- pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
+++ pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
@@ -1,118 +1,247 @@
 
         
         
-def score_euclidean(a, b):
-    '''计算两个点之间的欧式距离'''
-    s = np.sqrt(np.sum(np.power(a - b, 2)))
-    return s
-    
-        for word in index.keys():
-        word_freq[word] = len(index[word])
-    
-    
-def conv2d(x, kernel_size, out_channels,
-           act_fn=relu,
-           strides=1,
-           padding='SAME',
-           name=None,
-           reuse=None):
-    '''2-D 卷积层
-    Input shape:  [batch_size, in_h, in_w, in_channels]
-    Output shape: [batch_size, out_h, out_w, out_channels]
+def test_when_cant_configure_automatically(shell_pid, shell, logs):
+    shell_pid.return_value = 12
+    shell.how_to_configure.return_value = ShellConfiguration(
+        content='eval $(thefuck --alias)',
+        path='/tmp/.bashrc',
+        reload='bash',
+        can_configure_automatically=False)
+    main()
+    logs.how_to_configure_alias.assert_called_once()
     
     
-def get_shape(x):
-    '''
-    References:
-        tflearn.utils.get_incoming_shape
-    '''
-    if isinstance(x, (tf.Tensor, tf.SparseTensor)):
-        return x.get_shape().as_list()
-    elif type(x) in [np.array, np.ndarray, list, tuple]:
-        return list(np.shape(x))
-    else:
-        raise Exception('Invalid `x`.')
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'tcsh')
+    proc.sendline(u'setenv PYTHONIOENCODING utf8')
+    proc.sendline(u'eval `thefuck --alias`')
+    return proc
     
-    # Required dataset entry keys
-_IM_DIR = 'image_directory'
-_ANN_FN = 'annotation_file'
-    
-    logger = logging.getLogger(__name__)
-    
-    
-def add_fpn_ResNet101_conv5_P2only_body(model):
-    return add_fpn_onto_conv_body(
-        model,
-        ResNet.add_ResNet101_conv5_body,
-        fpn_level_info_ResNet101_conv5,
-        P2only=True
-    )
-    
-        # add the stem (by default, conv1 and pool1 with bn; can support gn)
-    p, dim_in = globals()[cfg.RESNETS.STEM_FUNC](model, 'data')
-    
-    The keypoint head produces a feature representation of the RoI for the purpose
-of keypoint prediction. The keypoint output module converts the feature
-representation into keypoint heatmaps.
+    no_match_output = '''
+Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
+Get:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease [89.2 kB]
+Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
+Get:4 http://security.ubuntu.com/ubuntu zesty-security InRelease [89.2 kB]
+Hit:5 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
+Hit:6 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
+Hit:7 https://download.docker.com/linux/ubuntu zesty InRelease
+Get:8 http://us.archive.ubuntu.com/ubuntu zesty-updates/main i386 Packages [232 kB]
+Get:9 http://us.archive.ubuntu.com/ubuntu zesty-updates/main amd64 Packages [235 kB]
+Get:10 http://us.archive.ubuntu.com/ubuntu zesty-updates/main amd64 DEP-11 Metadata [55.2 kB]
+Get:11 http://us.archive.ubuntu.com/ubuntu zesty-updates/main DEP-11 64x64 Icons [32.3 kB]
+Get:12 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe amd64 Packages [156 kB]
+Get:13 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe i386 Packages [156 kB]
+Get:14 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe amd64 DEP-11 Metadata [175 kB]
+Get:15 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe DEP-11 64x64 Icons [253 kB]
+Get:16 http://us.archive.ubuntu.com/ubuntu zesty-updates/multiverse amd64 DEP-11 Metadata [5,840 B]
+Get:17 http://us.archive.ubuntu.com/ubuntu zesty-backports/universe amd64 DEP-11 Metadata [4,588 B]
+Get:18 http://security.ubuntu.com/ubuntu zesty-security/main amd64 DEP-11 Metadata [12.7 kB]
+Get:19 http://security.ubuntu.com/ubuntu zesty-security/main DEP-11 64x64 Icons [17.6 kB]
+Get:20 http://security.ubuntu.com/ubuntu zesty-security/universe amd64 DEP-11 Metadata [21.6 kB]
+Get:21 http://security.ubuntu.com/ubuntu zesty-security/universe DEP-11 64x64 Icons [47.7 kB]
+Get:22 http://security.ubuntu.com/ubuntu zesty-security/multiverse amd64 DEP-11 Metadata [208 B]
+Fetched 1,673 kB in 0s (1,716 kB/s)
+Reading package lists... Done
+Building dependency tree
+Reading state information... Done
+All packages are up to date.
 '''
     
-            if cfg.RPN.RPN_ON:
-            # Add the RPN head
-            head_loss_gradients['rpn'] = rpn_heads.add_generic_rpn_outputs(
-                model, blob_conv, dim_conv, spatial_scale_conv
-            )
+    
+@pytest.mark.skipif(_is_not_okay_to_test(),
+                    reason='No need to run if there\'s no formula')
+def test_get_new_command(brew_no_available_formula):
+    assert get_new_command(Command('brew install elsticsearch',
+                                   brew_no_available_formula))\
+        == 'brew install elasticsearch'
+    
+    if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+    
+        x_train = np.empty((num_train_samples, 3, 32, 32), dtype='uint8')
+    y_train = np.empty((num_train_samples,), dtype='uint8')
+    
+        # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    '''
+    dirname = os.path.join('datasets', 'fashion-mnist')
+    base = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/'
+    files = ['train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz',
+             't10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz']
     
     
-def add_single_scale_rpn_outputs(model, blob_in, dim_in, spatial_scale):
-    '''Add RPN outputs to a single scale model (i.e., no FPN).'''
-    anchors = generate_anchors(
-        stride=1. / spatial_scale,
-        sizes=cfg.RPN.SIZES,
-        aspect_ratios=cfg.RPN.ASPECT_RATIOS
-    )
-    num_anchors = anchors.shape[0]
-    dim_out = dim_in
-    # RPN hidden representation
-    model.Conv(
-        blob_in,
-        'conv_rpn',
-        dim_in,
-        dim_out,
-        kernel=3,
-        pad=1,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
-    model.Relu('conv_rpn', 'conv_rpn')
-    # Proposal classification scores
-    model.Conv(
-        'conv_rpn',
-        'rpn_cls_logits',
-        dim_in,
-        num_anchors,
-        kernel=1,
-        pad=0,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
-    # Proposal bbox regression deltas
-    model.Conv(
-        'conv_rpn',
-        'rpn_bbox_pred',
-        dim_in,
-        4 * num_anchors,
-        kernel=1,
-        pad=0,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
+def test_imdb():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = imdb.load_data()
+        (x_train, y_train), (x_test, y_test) = imdb.load_data(maxlen=40)
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        word_index = imdb.get_word_index()
+        assert isinstance(word_index, dict)
     
-    from detectron.core.config import cfg
-from detectron.datasets import json_dataset
-from detectron.datasets import roidb as roidb_utils
-import detectron.modeling.FPN as fpn
-import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
-import detectron.utils.blob as blob_utils
+    model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+    
+    print('Evaluate IRNN...')
+model = Sequential()
+model.add(SimpleRNN(hidden_units,
+                    kernel_initializer=initializers.RandomNormal(stddev=0.001),
+                    recurrent_initializer=initializers.Identity(gain=1.0),
+                    activation='relu',
+                    input_shape=x_train.shape[1:]))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+rmsprop = RMSprop(lr=learning_rate)
+model.compile(loss='categorical_crossentropy',
+              optimizer=rmsprop,
+              metrics=['accuracy'])
+    
+    classification_layers = [
+    Dense(128),
+    Activation('relu'),
+    Dropout(0.5),
+    Dense(num_classes),
+    Activation('softmax')
+]
+    
+    
+def create_network(n_dense=6,
+                   dense_units=16,
+                   activation='selu',
+                   dropout=AlphaDropout,
+                   dropout_rate=0.1,
+                   kernel_initializer='lecun_normal',
+                   optimizer='adam',
+                   num_classes=1,
+                   max_words=max_words):
+    '''Generic function to create a fully-connected neural network.
+    
+        with pytest.raises(AssertionError):
+        conv_utils.conv_output_length(32, 5, 'diagonal', 2)
+    
+        def list_targets(self):
+        '''Lists the existing targets for the rule in AWS'''
+        try:
+            targets = self.client.list_targets_by_rule(Rule=self.name)
+        except botocore.exceptions.ClientError as e:
+            error_code = e.response.get('Error', {}).get('Code')
+            if error_code == 'ResourceNotFoundException':
+                return []
+            self.module.fail_json_aws(e, msg='Could not find target for rule %s' % self.name)
+        except botocore.exceptions.BotoCoreError as e:
+            self.module.fail_json_aws(e, msg='Could not find target for rule %s' % self.name)
+        return self._snakify(targets)['targets']
+    
+    options:
+  query:
+    description:
+      - Specifies the resource type for which to gather facts.  Leave blank to retrieve all facts.
+    required: true
+    choices: [ 'aliases', 'all', 'config', 'mappings', 'policy', 'versions' ]
+    default: 'all'
+  function_name:
+    description:
+      - The name of the lambda function for which facts are requested.
+    aliases: [ 'function', 'name']
+  event_source_arn:
+    description:
+      - For query type 'mappings', this is the Amazon Resource Name (ARN) of the Amazon Kinesis or DynamoDB stream.
+author: Pierre Jodouin (@pjodouin)
+requirements:
+    - boto3
+extends_documentation_fragment:
+  - aws
+  - ec2
+'''
+    
+            firewall_policy = oneandone_conn.remove_firewall_server(
+            firewall_id=firewall_id,
+            server_ip_id=server_ip_id)
+        return firewall_policy
+    except Exception as e:
+        module.fail_json(msg=str(e))
+    
+            elif desired_state == 'enabled':
+            if current_state == HOST_ABSENT:
+                self.allocate_host()
+                host = self.get_host_by_name(host_name)
+                self.wait_for_host_state(host, [HOST_STATES.MONITORED])
+            elif current_state in [HOST_STATES.DISABLED, HOST_STATES.OFFLINE]:
+                if one.host.status(host.ID, HOST_STATUS.ENABLED):
+                    self.wait_for_host_state(host, [HOST_STATES.MONITORED])
+                    result['changed'] = True
+                else:
+                    self.fail(msg='could not enable host')
+            elif current_state in [HOST_STATES.MONITORED]:
+                pass
+            else:
+                self.fail(msg='unknown host state %s, cowardly refusing to change state to enable' % current_state_name)
+    
+        params['api_key'] = module.params['token']
+    
+    
+def load_data(file_path):
+    '''加载数据
+        源数据格式为多行，每行为两个浮点数，分别表示 (x,y)
+    '''
+    data = []
+    with open(file_path, 'r', encoding='utf-8') as fr:
+        for line in fr.read().splitlines():
+            line_float = list(map(float, line.split('\t')))
+            data.append(line_float)
+    data = np.array(data)
+    return data
+    
+    
+def hard_sigmoid(x):
+    '''
+    x = 0.                  x < -2.5
+      = 1.                  x > 2.5
+      = 0.2 * x + 0.5       otherwise
+    '''
+    x = (0.2 * x) + 0.5
+    x = tf.clip_by_value(x, 0., 1.)
+    return x
+    
+            a = permute(a, [0, 2, 1])  # [batch_size, n_step, n_input]
+        o = tf.multiply(x, a)  # # [batch_size, n_step, n_input]
+    
+    
+def highway_conv2d(x, kernel_size,
+                   act_fn=relu,
+                   strides=1,
+                   padding='SAME',
+                   carry_bias=-1.0,
+                   name=None):
+    '''用于 conv2d 的 highway
+    Input shape:  [batch_size, in_h, in_w, in_channels]
+    Output shape: [batch_size, in_h, in_w, in_channels]
+    
+    
+def permute(x, perm):
+    '''
+    Examples:
+        x.shape == [128, 32, 1]
+        x = permute(x, [0, 2, 1])
+        x.shape == [128, 1, 32]
