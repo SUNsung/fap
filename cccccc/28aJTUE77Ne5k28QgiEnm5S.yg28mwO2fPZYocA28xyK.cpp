3407237ba666c2871bbd59d519b0465419485c2d
SUNsung <weatherius@gmail.com>
2019-06-22 00:16:47 +0300

    Rewrite [28aJTUE77Ne5k28QgiEnm5S.yg28mwO2fPZYocA28xyK.cpp] File in: 2019-06-22 00:16:47

--- cccccc/28aJTUE77Ne5k28QgiEnm5S.yg28mwO2fPZYocA28xyK.cpp
+++ cccccc/28aJTUE77Ne5k28QgiEnm5S.yg28mwO2fPZYocA28xyK.cpp
@@ -1,228 +1,78 @@
 
         
-        private:
-    reverse_lock(reverse_lock const&);
-    reverse_lock& operator=(reverse_lock const&);
+          // Post 'open' event.
+  static void EmitOpenEvent(const std::string& path);
     
-    bool ParseInt32(const std::string& str, int32_t *out)
-{
-    if (!ParsePrechecks(str))
-        return false;
-    char *endp = NULL;
-    errno = 0; // strtol will not set errno if valid
-    long int n = strtol(str.c_str(), &endp, 10);
-    if(out) *out = (int32_t)n;
-    // Note that strtol returns a *long int*, so even if strtol doesn't report a over/underflow
-    // we still have to check that the returned value is within the range of an *int32_t*. On 64-bit
-    // platforms the size of these types may be different.
-    return endp && *endp == 0 && !errno &&
-        n >= std::numeric_limits<int32_t>::min() &&
-        n <= std::numeric_limits<int32_t>::max();
-}
     
-        /* Check for (correctly reporting) a parsing error if the initial
-       JSON construct is followed by more stuff.  Note that whitespace
-       is, of course, exempt.  */
-    
-    std::string TempFileName(const std::string& dbname, uint64_t number) {
-  assert(number > 0);
-  return MakeFileName(dbname, number, 'dbtmp');
-}
-    
-    #include <condition_variable>
-#include <mutex>
-#include <string>
-#include <system_error>
-#include <vector>
-    
-    <details>
-    
-    template <>
-void GluOp<float, CPUContext>::ComputeGlu(
-    const int M,
-    const int split_dim,
-    const int N,
-    const float* Xdata,
-    float* Ydata) {
-  const int xStride = 2 * split_dim * N;
-  const int yStride = split_dim * N;
-  for (int i = 0; i < M; ++i) {
-    const int idx = i * xStride;
-    const int idy = i * yStride;
-    for (int j = 0; j < split_dim; ++j) {
-      const int jN = j * N;
-      const int jdx1 = idx + jN;
-      const int jdx2 = idx + (j + split_dim) * N;
-      const int jdy = idy + jN;
-      for (int k = 0; k < N; ++k) {
-        const float x1 = Xdata[jdx1 + k];
-        const float x2 = Xdata[jdx2 + k];
-        Ydata[jdy + k] = x1 * sigmoid(x2);
-      }
-    }
-  }
-}
-    
-    template <typename T, class Context>
-class BernoulliJSDGradientOp final : public Operator<Context> {
- public:
-  USE_SIMPLE_CTOR_DTOR(BernoulliJSDGradientOp);
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-  bool RunOnDevice() override;
+    {  DISALLOW_COPY_AND_ASSIGN(Base);
 };
     
-    REGISTER_CUDA_OPERATOR(LC1D, LocallyConnectedOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(
-    LC1DGradient,
-    LocallyConnectedGradientOp<float, CUDAContext>);
-    
-    int os_detect_blobs(const GenericVector<int>* allowed_scripts,
-                    BLOBNBOX_CLIST* blob_list,
-                    OSResults* osr,
-                    tesseract::Tesseract* tess);
+    void Menu::RemoveKeys() {
+  if (!focus_manager_) return;
+    }
     
     
-    {}  // namespace tesseract.
+    {	
+};
+    
+    #ifndef CHECK_OVERFLOW_OP
+#  define CHECK_OVERFLOW_OP(a,op,b) /* noop */
+#endif
+    
+    #undef silk_CLZ32
+static OPUS_INLINE opus_int32 silk_CLZ32_armv5(opus_int32 in32)
+{
+  int res;
+  __asm__(
+      '#silk_CLZ32\n\t'
+      'clz %0, %1\n\t'
+      : '=r'(res)
+      : 'r'(in32)
+  );
+  return res;
+}
+#define silk_CLZ32(in32) (silk_CLZ32_armv5(in32))
+    
+                result = rhs << result;
+            break;
+    
+    
+    {private:
+    void ReinitHistory();
+    int IchAddSzToEquationSz(std::wstring_view str, int icommandIndex);
+    void TruncateEquationSzFromIch(int ich);
+    void SetExpressionDisplay();
+    void InsertSzInEquationSz(std::wstring_view str, int icommandIndex, int ich);
+    std::shared_ptr<CalculatorVector<int>> GetOperandCommandsFromString(std::wstring_view numStr);
+};
 
     
-      // Adds an element with a weight of 1.
-  void add(double x, double y);
-  // Adds an element with a specified weight.
-  void add(double x, double y, double weight);
-  // Adds a whole LLSQ.
-  void add(const LLSQ& other);
-  // Deletes an element with a weight of 1.
-  void remove(double x, double y);
-  int32_t count() const {  // no of elements
-    return static_cast<int>(total_weight + 0.5);
-  }
-    
-    class ParagraphModel;
-    
-    
-    {  // WARNING! Keep data as the first element! KDPairInc and KDPairDec depend
-  // on the order of these elements so they can downcast pointers appropriately
-  // for use by GenericHeap::Reshuffle.
-  Data data;
-  Key key;
-};
-// Specialization of KDPair to provide operator< for sorting in increasing order
-// and recasting of data pointers for use with DoublePtr.
-template <typename Key, typename Data>
-struct KDPairInc : public KDPair<Key, Data> {
-  KDPairInc() = default;
-  KDPairInc(Key k, Data d) : KDPair<Key, Data>(k, d) {}
-  // Operator< facilitates sorting in increasing order.
-  int operator<(const KDPairInc<Key, Data>& other) const {
-    return this->key < other.key;
-  }
-  // Returns the input Data pointer recast to a KDPairInc pointer.
-  // Just casts a pointer to the first element to a pointer to the whole struct.
-  static KDPairInc* RecastDataPointer(Data* data_ptr) {
-    return reinterpret_cast<KDPairInc*>(data_ptr);
-  }
-};
-// Specialization of KDPair to provide operator< for sorting in decreasing order
-// and recasting of data pointers for use with DoublePtr.
-template <typename Key, typename Data>
-struct KDPairDec : public KDPair<Key, Data> {
-  KDPairDec() = default;
-  KDPairDec(Key k, Data d) : KDPair<Key, Data>(k, d) {}
-  // Operator< facilitates sorting in decreasing order by using operator> on
-  // the key values.
-  int operator<(const KDPairDec<Key, Data>& other) const {
-    return this->key > other.key;
-  }
-  // Returns the input Data pointer recast to a KDPairDec pointer.
-  // Just casts a pointer to the first element to a pointer to the whole struct.
-  static KDPairDec* RecastDataPointer(Data* data_ptr) {
-    return reinterpret_cast<KDPairDec*>(data_ptr);
-  }
-};
-    
-            const ElementType *currentp = source;
-        const ElementType *lastp = source + sampleCount * sampleSize;
-        size_t destIndex = 0;
-        while (currentp < lastp)
-        {
-            size_t index = sampleSize;
-            bool found = false;
-            for (size_t i = 0; i < sampleSize; i++)
-            {
-                if (*currentp == (ElementType)1)
-                {
-                    if (found)
-                        RuntimeError('CopyDenseToOneHot: Cannot convert to onehot vector; more than one non-zero value in the sample.');
-    }
-    }
-    }
-    
-                    // Sequence dynamic axes are 'Ordered' as opposed to the batch axis which is unordered.
-                bool hasSequenceAxis = (std::find_if(sampleDynamicAxes.begin(), sampleDynamicAxes.end(), [](const Axis& axis) {return axis.IsOrdered(); }) != sampleDynamicAxes.end());
-                if (hasSequenceAxis)
-                    unpackedShape = unpackedShape.AppendShape({ packedDataLayout->GetNumTimeSteps() });
-                else if ((packedDataLayout->GetNumTimeSteps() != 1) || packedDataLayout->HasSequenceBeyondBegin())
-                    LogicError('A PackedValue object with no sequence dynamic axis, must have a layout with exactly one time step and no sequences beginning in the past.');
-    
-    
-    {            if ((m_varKind == VariableKind::Parameter) || (m_varKind == VariableKind::Constant))
-            {
-                if (m_shape.HasFreeDimension())
-                    InvalidArgument('Parameter/Constant '%S' has invalid shape '%S'; it is illegal for a Parameter/Constant to have a FreeDimension.', AsString().c_str(), m_shape.AsString().c_str());
-            }
-        }
-    
-        // Releases the mutex
-    void Release()
+    void ascalerat(_Inout_ PRAT* pa, ANGLE_TYPE angletype, int32_t precision)
+{
+    switch (angletype)
     {
-        assert(m_fd != -1);
-        // removing file
-        unlink(m_fileName.c_str());
-        // Note: file is intentionally removed *before* releasing the lock
-        // to ensure that locked file isn't deleted by the non-owner of the lock
-        m_lock.l_type = F_UNLCK;
-        // Now removing the lock and closing the file descriptor
-        // waiting processes will be notified
-        int rc = fcntl(m_fd, F_SETLKW, &m_lock);
-        if (rc == FCNTL_ERROR)
-        {
-            RuntimeError('Mutex Release: Failed to release mutex %s', m_fileName.c_str());
-        }
-        close(m_fd);
-        m_fd = -1;
+    case ANGLE_RAD:
+        break;
+    case ANGLE_DEG:
+        divrat(pa, two_pi, precision);
+        mulrat(pa, rat_360, precision);
+        break;
+    case ANGLE_GRAD:
+        divrat(pa, two_pi, precision);
+        mulrat(pa, rat_400, precision);
+        break;
     }
-    
-    // Rand based on Mersenne Twister.
-// We use our own distribution in order to match baselines between different operating systems,
-// because uniform_distribution is not guaranteed to provide the same numbers on different platforms.
-// TODO: Switching to Boost would eliminate this problem.
-static inline size_t RandMT(const size_t begin, const size_t end, std::mt19937_64& rng)
-{
-    const size_t randomNumber = rng();
-    return begin + randomNumber % (end - begin);
 }
     
-    private:
-  APCCollection();
-  ~APCCollection();
-  static APCHandle::Pair WrapArray(APCHandle::Pair, CollectionType);
-    
-    #include 'hphp/runtime/base/ini-setting.h'
-#include 'hphp/runtime/base/array-iterator.h'
-#include 'hphp/util/logger.h'
-    
-      // only accept paths with the glob:// prefix
-  if (strncmp(path_str, prefix, strlen(prefix)) != 0) {
-    return nullptr;
-  }
-    
-    #endif // HPHP_GLOB_STREAM_WRAPPER_H
-
-    
-    #ifndef incl_HPHP_PIPE_H_
-#define incl_HPHP_PIPE_H_
-    
-      explicit UrlFile(const char *method = 'GET', const Array& headers = null_array,
-                   const String& postData = null_string,
-                   int maxRedirect = HttpClient::defaultMaxRedirect,
-                   int timeout = -1, bool ignoreErrors = false);
+    //-----------------------------------------------------------------------------
+//
+//  FUNCTION: rootrat
+//
+//  PARAMETERS: y prat representation of number to take the root of
+//              n prat representation of the root to take.
+//
+//  RETURN: bth root of a in rat form.
+//
+//  EXPLANATION: This is now a stub function to powrat().
+//
+//-----------------------------------------------------------------------------
