34e453bd0e128f7f75aa762ba9b31e6054cf118f
SUNsung <weatherius@gmail.com>
2019-06-22 22:24:47 +0300

    Rewrite [42XUZSY3nAkZo420mg5kq6nfRU42oCEKhAEz6EA42p.8.cpp] File in: 2019-06-22 22:24:46

--- cccccc/42XUZSY3nAkZo420mg5kq6nfRU42oCEKhAEz6EA42p.8.cpp
+++ cccccc/42XUZSY3nAkZo420mg5kq6nfRU42oCEKhAEz6EA42p.8.cpp
@@ -1,309 +1,89 @@
 
         
-          // WindowListObserver:
-  void OnWindowAllClosed() override;
+          // Set the maximum number of read-only files that will be opened.
+  // Must be called before creating an Env.
+  static void SetReadOnlyFDLimit(int limit);
     
     
-    {}  // namespace api
-    
-    #include <vector>
-    
-      // Finds out the TrackableObject from its ID in weak map.
-  static T* FromWeakMapID(v8::Isolate* isolate, int32_t id) {
-    if (!weak_map_)
-      return nullptr;
+    {  // Write footer
+  if (ok()) {
+    Footer footer;
+    footer.set_metaindex_handle(metaindex_block_handle);
+    footer.set_index_handle(index_block_handle);
+    std::string footer_encoding;
+    footer.EncodeTo(&footer_encoding);
+    r->status = r->file->Append(footer_encoding);
+    if (r->status.ok()) {
+      r->offset += footer_encoding.size();
     }
-    
-    #include <map>
-#include <string>
-    
-    #endif  // ATOM_BROWSER_LIB_POWER_OBSERVER_H_
-
-    
-    
-    {}  // namespace atom
-    
-    void MapLiteTestUtil::ExpectMapFieldsSetInitialized(
-    const unittest::TestMapLite& message) {
-  MapTestUtilImpl::ExpectMapFieldsSetInitialized<unittest::MapEnumLite,
-                                                 unittest::MAP_ENUM_FOO_LITE>(
-      message);
-}
-    
-    int main(int argc, const char** argv) {
-  FileOutputStream fout(STDOUT_FILENO);
-  GzipOutputStream out(&fout);
-  int readlen;
-    }
-    
-    
-    {    std::ofstream ofs(output_file);
-    ofs << dataset.SerializeAsString();
-    ofs.close();
   }
-    
-    
-    {
-    {
-    {}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-    
-    #include 'google/protobuf/message.h'
-#include 'google/protobuf/descriptor.h'
-#include 'google/protobuf/descriptor.pb.h'
-    
-    namespace caffe {
-    }
-    
-    
-    {}  // namespace caffe
-    
-      virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
-    
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    
-    {}  // namespace caffe
-    
-    /**
- * @brief Convolves the input image with a bank of learned filters,
- *        and (optionally) adds biases.
- *
- *   Caffe convolves by reduction to matrix multiplication. This achieves
- *   high-throughput and generality of input and filter dimensions but comes at
- *   the cost of memory for matrices. This makes use of efficiency in BLAS.
- *
- *   The input is 'im2col' transformed to a channel K' x H x W data matrix
- *   for multiplication with the N x K' x H x W filter matrix to yield a
- *   N' x H x W output matrix that is then 'col2im' restored. K' is the
- *   input channel * kernel height * kernel width dimension of the unrolled
- *   inputs so that the im2col matrix has a column for each input region to
- *   be filtered. col2im restores the output spatial structure by rolling up
- *   the output channel N' columns of the output matrix.
- */
-template <typename Dtype>
-class ConvolutionLayer : public BaseConvolutionLayer<Dtype> {
- public:
-  /**
-   * @param param provides ConvolutionParameter convolution_param,
-   *    with ConvolutionLayer options:
-   *  - num_output. The number of filters.
-   *  - kernel_size / kernel_h / kernel_w. The filter dimensions, given by
-   *  kernel_size for square filters or kernel_h and kernel_w for rectangular
-   *  filters.
-   *  - stride / stride_h / stride_w (\b optional, default 1). The filter
-   *  stride, given by stride_size for equal dimensions or stride_h and stride_w
-   *  for different strides. By default the convolution is dense with stride 1.
-   *  - pad / pad_h / pad_w (\b optional, default 0). The zero-padding for
-   *  convolution, given by pad for equal dimensions or pad_h and pad_w for
-   *  different padding. Input padding is computed implicitly instead of
-   *  actually padding.
-   *  - dilation (\b optional, default 1). The filter
-   *  dilation, given by dilation_size for equal dimensions for different
-   *  dilation. By default the convolution has dilation 1.
-   *  - group (\b optional, default 1). The number of filter groups. Group
-   *  convolution is a method for reducing parameterization by selectively
-   *  connecting input and output channels. The input and output channel dimensions must be divisible
-   *  by the number of groups. For group @f$ \geq 1 @f$, the
-   *  convolutional filters' input and output channels are separated s.t. each
-   *  group takes 1 / group of the input channels and makes 1 / group of the
-   *  output channels. Concretely 4 input channels, 8 output channels, and
-   *  2 groups separate input channels 1-2 and output channels 1-4 into the
-   *  first group and input channels 3-4 and output channels 5-8 into the second
-   *  group.
-   *  - bias_term (\b optional, default true). Whether to have a bias.
-   *  - engine: convolution has CAFFE (matrix multiplication) and CUDNN (library
-   *    kernels + stream parallelism) engines.
-   */
-  explicit ConvolutionLayer(const LayerParameter& param)
-      : BaseConvolutionLayer<Dtype>(param) {}
-    }
-    
-    template <typename Dtype>
-class CropLayer : public Layer<Dtype> {
- public:
-  explicit CropLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    
-    {}  // namespace caffe
-    
-    
-    {  size_t *workspace_fwd_sizes_;
-  size_t *workspace_bwd_data_sizes_;
-  size_t *workspace_bwd_filter_sizes_;
-  size_t workspaceSizeInBytes;  // size of underlying storage
-  void *workspaceData;  // underlying storage
-  void **workspace;  // aliases into workspaceData
-};
-#endif
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    
-    
-        Transliterator::_registerFactory(UnicodeString(TRUE, ::CURR_ID, -1),
-                                     RemoveTransliterator_create, integerToken(0));
-    
-    U_CAPI UBool U_EXPORT2
-uhash_equalsScriptSet(const UElement key1, const UElement key2) {
-    icu::ScriptSet *s1 = static_cast<icu::ScriptSet *>(key1.pointer);
-    icu::ScriptSet *s2 = static_cast<icu::ScriptSet *>(key2.pointer);
-    return (*s1 == *s2);
+  return r->status;
 }
     
-    class U_I18N_API SharedCalendar : public SharedObject {
-public:
-    SharedCalendar(Calendar *calToAdopt) : ptr(calToAdopt) { }
-    virtual ~SharedCalendar();
-    const Calendar *get() const { return ptr; }
-    const Calendar *operator->() const { return ptr; }
-    const Calendar &operator*() const { return *ptr; }
-private:
-    Calendar *ptr;
-    SharedCalendar(const SharedCalendar &);
-    SharedCalendar &operator=(const SharedCalendar &);
-};
+     private:
+  char* AllocateFallback(size_t bytes);
+  char* AllocateNewBlock(size_t block_bytes);
     
-    
-class U_I18N_API SharedDateFormatSymbols : public SharedObject {
-public:
-    SharedDateFormatSymbols(
-            const Locale &loc, const char *type, UErrorCode &status)
-            : dfs(loc, type, status) { }
-    virtual ~SharedDateFormatSymbols();
-    const DateFormatSymbols &get() const { return dfs; }
-private:
-    DateFormatSymbols dfs;
-    SharedDateFormatSymbols(const SharedDateFormatSymbols &);
-    SharedDateFormatSymbols &operator=(const SharedDateFormatSymbols &);
-};
-    
-        /**
-     * No limits on significant digits.
-     */
-    SignificantDigitInterval()
-            : fMax(INT32_MAX), fMin(0) { }
-    
-        // We must ensure fSharedNumberFormatters is allocated.
-    if (fSharedNumberFormatters == NULL) {
-        fSharedNumberFormatters = allocSharedNumberFormatters();
-        if (fSharedNumberFormatters == NULL) {
-            status = U_MEMORY_ALLOCATION_ERROR;
-            return;
-        }
-    }
-    const SharedNumberFormat *newFormat = createSharedNumberFormat(fmt.orphan());
-    if (newFormat == NULL) {
-        status = U_MEMORY_ALLOCATION_ERROR;
-        return;
-    }
-    for (int i=0; i<fields.length(); i++) {
-        UChar field = fields.charAt(i);
-        // if the pattern character is unrecognized, signal an error and bail out
-        UDateFormatField patternCharIndex = DateFormatSymbols::getPatternCharIndex(field);
-        if (patternCharIndex == UDAT_FIELD_COUNT) {
-            status = U_INVALID_FORMAT_ERROR;
-            newFormat->deleteIfZeroRefCount();
-            return;
-        }
-    }
-    
-    CollationKey::CollationKey()
-    : UObject(), fFlagAndLength(0),
-      fHashCode(kEmptyHashCode)
-{
+    TEST(ProtocolDataTest, CheckSum) {
+  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
+  const uint8_t result =
+      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
+  EXPECT_EQ(0xE7, result);
 }
     
-        /**
-     * Sets U_ILLEGAL_ARGUMENT_ERROR if the keyword is not a plural form.
-     *
-     * @param keyword for example 'few' or 'other'
-     * @return the index of the plural form corresponding to the keyword
-     */
-    static int32_t indexFromString(const char *keyword, UErrorCode &errorCode);
-    
-    
-    // out_of_range.106
-    try
-    {
-        // try to use an array index with leading '0'
-        json::reference ref = j.at('/array/01'_json_pointer);
+    void ObjectExtendedInfo60D::Parse(const std::uint8_t* bytes, int32_t length,
+                                  ContiRadar* conti_radar) const {
+  int obj_id = object_id(bytes, length);
+  for (int i = 0; i < conti_radar->contiobs_size(); ++i) {
+    if (conti_radar->contiobs(i).obstacle_id() == obj_id) {
+      auto obs = conti_radar->mutable_contiobs(i);
+      obs->set_longitude_accel(longitude_accel(bytes, length));
+      obs->set_lateral_accel(lateral_accel(bytes, length));
+      obs->set_oritation_angle(oritation_angle(bytes, length));
+      obs->set_length(object_length(bytes, length));
+      obs->set_width(object_width(bytes, length));
+      obs->set_obstacle_class(obstacle_class(bytes, length));
+      break;
     }
-    catch (json::parse_error& e)
-    {
-        std::cout << e.what() << '\n';
+  }
+  // auto conti_obs = conti_radar->mutable_contiobs(object_id(bytes, length));
+}
+    
+    RcsThreshold RadarState201::rcs_threshold(const std::uint8_t* bytes,
+                                          int32_t length) const {
+  Byte t0(bytes + 7);
+  uint32_t x = t0.get_byte(2, 3);
     }
     
-        // create a JSON number from an anonymous enum
-    enum { enum_value = 17 };
-    json j_enum(enum_value);
+    
+    { protected:
+  SpeedLimit speed_limit_;
+};
     
     
-    {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
+    {  MatrixXd bd_golden(20, 1);
+  bd_golden << -100, 1, -100, 1, -100, 1, -100, 1, -100, 1, -100, 1, -100, 1,
+      -100, 1, -100, 1, -100, 1;
+  EXPECT_EQ(bd, bd_golden);
+}
     
-      x <<= 8;
+      } else if (type == 'derivative') {
+    for (uint32_t r = 1; r < num_params; ++r) {
+      for (uint32_t c = 1; c < num_params; ++c) {
+        (*term_matrix)(r, c) = x_pow[r + c - 1];
+      }
+    }
+    (*term_matrix).block(0, 0, num_params, 1) =
+        Eigen::MatrixXd::Zero(num_params, 1);
+    (*term_matrix).block(0, 0, 1, num_params) =
+        Eigen::MatrixXd::Zero(1, num_params);
+    
+      Byte t2(bytes + 6);
+  t = t2.get_byte(0, 8);
+  x <<= 8;
   x |= t;
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    
-    {  double ret = x * 0.001000;
-  return ret;
-}
-    
-    
-    {  acc.Parse(bytes, length, &chassis_detail);
-  EXPECT_DOUBLE_EQ(chassis_detail.gem().accel_rpt_68().manual_input(), 0.258);
-  EXPECT_DOUBLE_EQ(chassis_detail.gem().accel_rpt_68().commanded_value(),
-                   0.772);
-  EXPECT_DOUBLE_EQ(chassis_detail.gem().accel_rpt_68().output_value(), 4.37);
-}
-    
-    #include 'gtest/gtest.h'
-    
-    
-    {
-    {
-    {
-    {  Headlight_rpt_77::Commanded_valueType ret =
-      static_cast<Headlight_rpt_77::Commanded_valueType>(x);
-  return ret;
-}
-}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
+      Byte t2(bytes + 2);
+  t = t2.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
