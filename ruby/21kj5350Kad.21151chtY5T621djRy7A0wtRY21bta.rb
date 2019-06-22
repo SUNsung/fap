a269ee8f27db101cd862fa8f82c4f61b50f40ea1
SUNsung <weatherius@gmail.com>
2019-06-22 07:41:10 +0300

    Rewrite [21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb] File in: 2019-06-22 07:41:09

--- ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
+++ ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
@@ -1,33 +1,69 @@
 
         
-        module Admin
-  class ReportNotesController < BaseController
-    before_action :set_report_note, only: [:destroy]
+            private
     
-      private
+        attr_accessor :name, :type, :path
     
-      def compatible_locale
-    http_accept_language.compatible_language_from(available_locales)
-  end
-    
-        @save_to.each { |_, v| FileUtils.mkdir_p(v) }
-    
-        def initialize(*args)
-      @s = StringScanner.new(*args)
+        def as_json
+      @pages
     end
     
-          expect(Notification.find(note.id).unread).to eq(true)
-    end
-  end
+          private
     
-            if Utils::HttpClient.remote_file_exist?(uri)
-          PluginManager.ui.debug('Found package at: #{uri}')
-          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
+          def mailer_sender(mapping, sender = :from)
+        default_sender = default_params[sender]
+        if default_sender.present?
+          default_sender.respond_to?(:to_proc) ? instance_eval(&default_sender) : default_sender
+        elsif Devise.mailer_sender.is_a?(Proc)
+          Devise.mailer_sender.call(mapping.name)
         else
-          PluginManager.ui.debug('Package not found at: #{uri}')
-          return nil
+          Devise.mailer_sender
         end
-      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
-        # This probably means there is a firewall in place of the proxy is not correctly configured.
-        # So lets skip this strategy but log a meaningful errors.
-        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
+      end
+    
+        def default_used_helpers(options)
+      singularizer = lambda { |s| s.to_s.singularize.to_sym }
+    
+              recoverable.reset_password_token = original_token if recoverable.reset_password_token.present?
+          recoverable
+        end
+    
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
+  end
+    
+            reaction
+      end
+    
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
+    end
+    
+        headers = get('/', {}, 'wants' => 'text/html').headers
+    expect(headers['Content-Security-Policy']).to be_nil
+    expect(headers['Content-Security-Policy-Report-Only']).to eq('connect-src 'self'; default-src none; img-src 'self'; report-uri /my_amazing_csp_report_parser; script-src 'self'; style-src 'self'')
+  end
+    
+    module RuboCop
+  # Handles caching of configurations and association of inspected
+  # ruby files to configurations.
+  class ConfigStore
+    def initialize
+      # @options_config stores a config that is specified in the command line.
+      # This takes precedence over configs located in any directories
+      @options_config = nil
+    
+              ignored_end_pos = if ignored_loc.respond_to?(:heredoc_body)
+                              ignored_loc.heredoc_end.end_pos
+                            else
+                              ignored_loc.expression.end_pos
+                            end
+          ignored_end_pos >= node.source_range.end_pos
+        end
+      end
+    
+            def define_method_call?(variable)
+          call, = *variable.scope.node
+          _, method, = *call
