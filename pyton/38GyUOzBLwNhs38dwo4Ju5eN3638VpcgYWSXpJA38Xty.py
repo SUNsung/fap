3e47641a94267a6f171cd5bcb2f59a56cbb9da06
SUNsung <weatherius@gmail.com>
2019-06-23 01:13:56 +0300

    Rewrite [38GyUOzBLwNhs38dwo4Ju5eN3638VpcgYWSXpJA38Xty.py] File in: 2019-06-23 01:13:56

--- pyton/38GyUOzBLwNhs38dwo4Ju5eN3638VpcgYWSXpJA38Xty.py
+++ pyton/38GyUOzBLwNhs38dwo4Ju5eN3638VpcgYWSXpJA38Xty.py
@@ -1,99 +1,73 @@
 
         
-                Is equivalent to the following::
-    
-    import logging
-import sys
-    
-        def should_set_cookie(self, app, session):
-        '''Used by session backends to determine if a ``Set-Cookie`` header
-        should be set for this session cookie for this response. If the session
-        has been modified, the cookie is set. If the session is permanent and
-        the ``SESSION_REFRESH_EACH_REQUEST`` config is true, the cookie is
-        always set.
+        
+class RedirectFallbackMiddleware(MiddlewareMixin):
+    # Defined as class-level attributes to be subclassing-friendly.
+    response_gone_class = HttpResponseGone
+    response_redirect_class = HttpResponsePermanentRedirect
     
     
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
-
+class BaseSessionManager(models.Manager):
+    def encode(self, session_dict):
+        '''
+        Return the given session dictionary serialized and encoded as a string.
+        '''
+        session_store_class = self.model.get_session_store_class()
+        return session_store_class().encode(session_dict)
     
-        :param source: the source code of the template to be
-                   rendered
-    :param context: the variables that should be available in the
-                    context of the template.
-    '''
-    ctx = _app_ctx_stack.top
-    ctx.app.update_template_context(context)
-    return _render(ctx.app.jinja_env.from_string(source), context, ctx.app)
-
+        def setUp(self):
+        from acme.errors import MissingNonce
+        self.response = mock.MagicMock(headers={})
+        self.response.request.method = 'FOO'
+        self.error = MissingNonce(self.response)
     
-        def _targets_to_put(self):
-        '''Returns a list of targets that need to be updated or added remotely'''
-        remote_targets = self.rule.list_targets()
-        return [t for t in self.targets if t not in remote_targets]
+    .. code-block:: bash
+   :caption: To acquire a certificate for ``example.com``
+    
+    # If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+    
+    from aiohttp import web
+    
+            # Describe TTL status after being enabled again.
+        response = testutil.send_describe_dynamodb_ttl_request(TEST_DDB_TABLE_NAME_3)
+        assert response.status_code == 200
+        assert json.loads(response._content)['TimeToLiveDescription']['TimeToLiveStatus'] == 'ENABLED'
+    
+    # sender thread and queue
+SENDER_THREAD = None
+EVENT_QUEUE = queue.Queue()
+    
+        # cleanup cache
+    clean_cache(file_pattern=TMP_DOWNLOAD_FILE_PATTERN,
+        last_clean_time=last_cache_cleanup_time,
+        max_age=TMP_DOWNLOAD_CACHE_MAX_AGE)
+    # TODO: delete only if cache_time is over
+    rm_rf(folder)
     
     
-DOCUMENTATION = '''
----
-module: elasticache_subnet_group
-version_added: '2.0'
-short_description: manage Elasticache subnet groups
-description:
-     - Creates, modifies, and deletes Elasticache subnet groups. This module has a dependency on python-boto >= 2.5.
-options:
-  state:
-    description:
-      - Specifies whether the subnet should be present or absent.
-    required: true
-    default: present
-    choices: [ 'present' , 'absent' ]
-  name:
-    description:
-      - Database subnet group identifier.
-    required: true
-  description:
-    description:
-      - Elasticache subnet group description. Only set when a new group is added.
-  subnets:
-    description:
-      - List of subnet IDs that make up the Elasticache subnet group.
-author: 'Tim Mahoney (@timmahoney)'
-extends_documentation_fragment:
-    - aws
-    - ec2
-'''
+# ---------------
+# Lambda metrics
+# ---------------
     
-        try:
-        region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
-        iam = boto3_conn(module, conn_type='client', resource='iam', region=region, endpoint=ec2_url, **aws_connect_kwargs)
-    except botocore.exceptions.ClientError as e:
-        module.fail_json(msg='Boto3 Client Error - ' + str(e.msg))
     
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
+def main():
+    test = TestQueueFromStacks()
+    test.test_queue_from_stacks()
     
-            if remove_rules:
-            chk_changed = False
-            for rule_id in remove_rules:
-                if module.check_mode:
-                    chk_changed |= _remove_firewall_rule(module,
-                                                         oneandone_conn,
-                                                         firewall_policy['id'],
-                                                         rule_id)
+    		while current is not None and current.data is not data:
+			parent = current
+			if data < current.data:
+				current = current.leftChild
+				isLeft = True 
+			else:
+				current = current.rightChild
+				isLeft = False
     
-    #An example for a source nat
     
-            if user is not None:
-            changed = client.modify_if_diff(name, ipa_hbacrule.get('memberuser_user', []), user,
-                                            client.hbacrule_add_user,
-                                            client.hbacrule_remove_user, 'user') or changed
+class PriorityQueue(object):
