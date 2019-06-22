2fabc7edd65dfac2df6aadb7317fdbda0fec47d0
SUNsung <weatherius@gmail.com>
2019-06-22 18:37:37 +0300

    Rewrite [34AxRGOZF9eiQ34vk0.3swxNIQ34ul8diSirTWQ34fDM.rb] File in: 2019-06-22 18:37:36

--- ruby/34AxRGOZF9eiQ34vk0.3swxNIQ34ul8diSirTWQ34fDM.rb
+++ ruby/34AxRGOZF9eiQ34vk0.3swxNIQ34ul8diSirTWQ34fDM.rb
@@ -1,59 +1,14 @@
 
         
-            if successfully_sent?(resource)
-      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
-    end
-  end
-    
-      def unknown_action!(msg)
-    logger.debug '[Devise] #{msg}' if logger
-    raise AbstractController::ActionNotFound, msg
-  end
-    
-      def test_returns_success
-    Warden.test_mode!
-    
-          # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
-      end
-    
-          if options.has_key?(:only)
-        @used_routes = self.routes & Array(options[:only]).map(&singularizer)
-      elsif options[:skip] == :all
-        @used_routes = []
-      else
-        @used_routes = self.routes - Array(options[:skip]).map(&singularizer)
-      end
+            it 'can be turned off' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'false' }
+      DefaultScenarioImporter.import(user)
     end
     
-    @@ chat
-<pre id='chat'></pre>
-<form>
-  <input id='msg' placeholder='type message here...' />
-</form>
+        @scheduler = Rufus::Scheduler.new
     
-      # escape unicode
-  content.gsub!(/./) { |c| c.bytesize > 1 ? '\\u{#{c.codepoints.first.to_s(16)}}' : c }
-    
-          def random_string(secure = defined? SecureRandom)
-        secure ? SecureRandom.hex(16) : '%032x' % rand(2**128-1)
-      rescue NotImplementedError
-        random_string false
-      end
-    
-      it 'sets a new csrf token for the session in env, even after a 'safe' request' do
-    get('/', {}, {})
-    expect(env['rack.session'][:csrf]).not_to be_nil
-  end
-    
-        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      subject.options[:allow_empty_referrer] = false
-      expect(subject.referrer(env)).to be_nil
+        it 'should generate the correct projects url' do
+      expect(@checker.send(:projects_url)).to eq('https://basecamp.com/12345/api/v1/projects.json')
     end
