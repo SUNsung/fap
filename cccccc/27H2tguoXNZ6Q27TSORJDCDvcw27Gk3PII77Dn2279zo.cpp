db5e7afe8bf38904ec02e6a25c60ed142db34e47
SUNsung <weatherius@gmail.com>
2019-06-22 20:47:14 +0300

    Rewrite [27H2tguoXNZ6Q27TSORJDCDvcw27Gk3PII77Dn2279zo.cpp] File in: 2019-06-22 20:47:13

--- cccccc/27H2tguoXNZ6Q27TSORJDCDvcw27Gk3PII77Dn2279zo.cpp
+++ cccccc/27H2tguoXNZ6Q27TSORJDCDvcw27Gk3PII77Dn2279zo.cpp
@@ -1,201 +1,227 @@
 
         
-        
-    {  // Moving to a message on the arena should lead to a copy.
-  *message2_on_arena = std::move(message1);
-  EXPECT_NE(nested, &message2_on_arena->optional_nested_message());
-  TestUtil::ExpectAllFieldsSet(message1);
-  TestUtil::ExpectAllFieldsSet(*message2_on_arena);
+        /*!
+ * \brief The result holder of storage type of each NodeEntry in the graph.
+ * \note Stored under graph.attrs['storage_type'], provided by Pass 'InferStorageType'
+ *
+ * \code
+ *  Graph g = ApplyPass(src_graph, 'InferStorageType');
+ *  const StorageVector& stypes = g.GetAttr<StorageTypeVector>('storage_type');
+ *  // get storage type by entry id
+ *  int entry_type = stypes[g.indexed_graph().entry_id(my_entry)];
+ * \endcode
+ *
+ * \sa FInferStorageType
+ */
+using StorageTypeVector = std::vector<int>;
+    
+    // Initialization funciton called by caffeOp & caffeLoss
+template<typename Dtype>
+void InitCaffeBlobs(std::vector< ::caffe::Blob<Dtype>*>* v, int n_num) {
+  for (index_t i=0; i < n_num; ++i)
+    v->push_back(new ::caffe::Blob<Dtype>());
 }
     
-    namespace google {
-namespace protobuf {
-namespace util {
-namespace error {
-inline string CodeEnumToString(error::Code code) {
-  switch (code) {
-    case OK:
-      return 'OK';
-    case CANCELLED:
-      return 'CANCELLED';
-    case UNKNOWN:
-      return 'UNKNOWN';
-    case INVALID_ARGUMENT:
-      return 'INVALID_ARGUMENT';
-    case DEADLINE_EXCEEDED:
-      return 'DEADLINE_EXCEEDED';
-    case NOT_FOUND:
-      return 'NOT_FOUND';
-    case ALREADY_EXISTS:
-      return 'ALREADY_EXISTS';
-    case PERMISSION_DENIED:
-      return 'PERMISSION_DENIED';
-    case UNAUTHENTICATED:
-      return 'UNAUTHENTICATED';
-    case RESOURCE_EXHAUSTED:
-      return 'RESOURCE_EXHAUSTED';
-    case FAILED_PRECONDITION:
-      return 'FAILED_PRECONDITION';
-    case ABORTED:
-      return 'ABORTED';
-    case OUT_OF_RANGE:
-      return 'OUT_OF_RANGE';
-    case UNIMPLEMENTED:
-      return 'UNIMPLEMENTED';
-    case INTERNAL:
-      return 'INTERNAL';
-    case UNAVAILABLE:
-      return 'UNAVAILABLE';
-    case DATA_LOSS:
-      return 'DATA_LOSS';
-  }
-    }
-    }
-    }
-    }
+        // Caffe seems to understand phase inside an 'include {}' block
+    if (!param_.prototxt.has_phase()) {
+      if (param_.prototxt.include().size()) {
+        if (param_.prototxt.include(0).has_phase()) {
+          param_.prototxt.set_phase(param_.prototxt.include(0).phase());
+        }
+      }
     }
     
     
-    {
-    {    reflection->MutableUnknownFields(message)->Clear();
-  }
- private:
-  virtual bool ShouldBeClear(const FieldDescriptor *field) = 0;
+    {  g.attrs['storage_id'] = std::make_shared<nnvm::any>(std::move(storage_id));
+  g.attrs['storage_inplace_index'] = std::make_shared<nnvm::any>(
+      std::move(storage_inplace_index));
+  g.attrs['addto_entry'] = std::make_shared<nnvm::any>(std::move(addto_entry));
+  g.attrs['skip_plus_node'] = std::make_shared<nnvm::any>(std::move(skip_plus_node));
+  return g;
+}
+    
+      /*!
+   * \brief decodes parameters of gc from a string and assigns them to member variables
+   */
+  void DecodeParams(const std::string &s);
+    
+    namespace mxnet {
+    }
+    
+    // relu
+MXNET_OPERATOR_REGISTER_UNARY(_contrib_div_sqrt_dim)
+.describe(R'code(Rescale the input by the square root of the channel dimension.
+    
+    MXNET_REGISTER_OP_PROPERTY(IdentityAttachKLSparseReg, IdentityAttachKLSparseRegProp)
+.describe('Apply a sparse regularization to the output a sigmoid activation function.')
+.add_argument('data', 'NDArray-or-Symbol', 'Input data.')
+.add_arguments(IdentityAttachKLSparseRegParam::__FIELDS__());
+    
+    template<typename xpu>
+void ConcatCompute(const nnvm::NodeAttrs& attrs, const OpContext& ctx,
+                   const std::vector<TBlob>& inputs,
+                   const std::vector<OpReqType>& req,
+                   const std::vector<TBlob>& outputs) {
+  const ConcatParam& param = nnvm::get<ConcatParam>(attrs.parsed);
+  MSHADOW_TYPE_SWITCH(inputs[concat_enum::kData0].type_flag_, DType, {
+    ConcatOp<xpu, DType> op;
+    op.Init(param);
+    op.Forward(ctx, inputs, req, outputs);
+  });
+}
+    
+    
+    { private:
+  RowBlock<IndexType> out_;
+  std::unique_ptr<Parser<IndexType> > parser_;
+  uint32_t num_col_;
+  std::vector<size_t> offset_;
+  std::vector<IndexType> dense_index_;
+  std::vector<xgboost::bst_float> dense_value_;
 };
     
-    std::string ReadFile(const std::string& name) {
-  std::ifstream file(name.c_str());
-  GOOGLE_CHECK(file.is_open()) << 'Couldn't find file ''
-      << name
-      << '', please make sure you are running this command from the benchmarks'
-      << ' directory.\n';
-  return std::string((std::istreambuf_iterator<char>(file)),
-                     std::istreambuf_iterator<char>());
+    inline std::vector<std::vector<unsigned>>
+FastFeatureGrouping(const GHistIndexMatrix& gmat,
+                    const ColumnMatrix& colmat,
+                    const tree::TrainParam& param) {
+  const size_t nrow = gmat.row_ptr.size() - 1;
+  const size_t nfeature = gmat.cut.row_ptr.size() - 1;
+    }
+    
+    namespace dmlc {
+DMLC_REGISTRY_ENABLE(::xgboost::LinearUpdaterReg);
+}  // namespace dmlc
+    
+    void
+RuleBasedCollator::setFastLatinOptions(CollationSettings &ownedSettings) const {
+    ownedSettings.fastLatinOptions = CollationFastLatin::getOptions(
+            data, ownedSettings,
+            ownedSettings.fastLatinPrimaries, UPRV_LENGTHOF(ownedSettings.fastLatinPrimaries));
 }
     
-    REGISTER_CPU_OPERATOR(GatherRangesToDense, GatherRangesToDenseOp<CPUContext>);
-NO_GRADIENT(GatherRangesToDense);
-    
-    OPERATOR_SCHEMA(GivenTensorStringFill)
-    .NumInputs(0, 1)
-    .NumOutputs(1)
-    .AllowInplace({{0, 0}})
-    .Arg(
-        'values',
-        'The value for the elements of the output tensor.',
-        true /* required */)
-    .Arg(
-        'shape',
-        'The shape of the output tensor.'
-        'Cannot set the shape argument and pass in an input at the same time.')
-    .Arg(
-        'extra_shape',
-        'The additional dimensions appended at the end of the shape indicated'
-        'by the input blob.'
-        'Cannot set the extra_shape argument when there is no input blob.')
-    .Arg(
-        'input_as_shape',
-        '1D tensor containing the desired output shape. First input must be in CPU context.')
-    .TensorInferenceFunction(
-        FillerTensorInference<TensorProto_DataType_STRING>);
-    
-    OPERATOR_SCHEMA(Glu)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-Applies gated linear unit to the input Tensor X. The output Y is half the size
-of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
-[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
-X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
-)DOC')
-    .Input(0, 'X', '1D input tensor')
-    .Output(0, 'Y', '1D output tensor');
-    
-    #include 'caffe2/core/context.h'
-#include 'caffe2/core/logging.h'
-#include 'caffe2/core/operator.h'
-#include 'caffe2/utils/math.h'
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_IMPL_API void     ImGui_Marmalade_InvalidateDeviceObjects();
-IMGUI_IMPL_API bool     ImGui_Marmalade_CreateDeviceObjects();
-    
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-    
-    IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_Init();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data);
-    
-    int main(int, char**)
-{
-    // Setup window
-    glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit())
-        return 1;
-    GLFWwindow* window = glfwCreateWindow(1280, 720, 'Dear ImGui GLFW+OpenGL2 example', NULL, NULL);
-    if (window == NULL)
-        return 1;
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
+    UnicodeString &ScientificNumberFormatter::format(
+        const Formattable &number,
+        UnicodeString &appendTo,
+        UErrorCode &status) const {
+    if (U_FAILURE(status)) {
+        return appendTo;
     }
-    
-    // Main code
-int main(int, char**)
-{
-    // Create application window
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T('ImGui Example'), NULL };
-    ::RegisterClassEx(&wc);
-    HWND hwnd = ::CreateWindow(wc.lpszClassName, _T('Dear ImGui DirectX10 Example'), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
-    }
-    
-        //
-    // convert 'absolutes' to 'diffs'
-    //
-    Vec2 p = copyConfig->getControlPointAtIndex(0);
-    for (ssize_t i = 1; i < copyConfig->count(); ++i)
-    {
-        Vec2 current = copyConfig->getControlPointAtIndex(i);
-        Vec2 diff = current - p;
-        copyConfig->replaceControlPoint(diff, i);
-    }
-    
-    // DeccelAmplitude
-    
-    
-Ripple3D* Ripple3D::clone() const
-{
-    // no copy constructor
-    auto a = new (std::nothrow) Ripple3D();
-    a->initWithDuration(_duration, _gridSize, _position, _radius, _waves, _amplitude);
-    a->autorelease();
-    return a;
+    UnicodeString original;
+    FieldPositionIterator fpi;
+    fDecimalFormat->format(number, original, &fpi, status);
+    return fStyle->format(
+            original,
+            fpi,
+            fPreExponent,
+            *fStaticSets,
+            appendTo,
+            status);
 }
     
-        /** 
-    @brief Initializes an action with duration and grid size.
-    @param gridSize Specify the grid size of the FlipX3D action.
-    @param duration Specify the duration of the FlipX3D action. It's a value in seconds.
-    @return If the initialization success, return true; otherwise, return false.
-    */
-    virtual bool initWithSize(const Size& gridSize, float duration);
+    UnicodeString&
+SelectFormat::format(const Formattable& obj,
+                   UnicodeString& appendTo,
+                   FieldPosition& pos,
+                   UErrorCode& status) const
+{
+    if (U_FAILURE(status)) {
+        return appendTo;
+    }
+    if (obj.getType() == Formattable::kString) {
+        return format(obj.getString(status), appendTo, pos, status);
+    } else {
+        status = U_ILLEGAL_ARGUMENT_ERROR;
+        return appendTo;
+    }
+}
     
-        /** Removes all actions matching at least one bit in flags and the target.
-     *
-     * @param flags     The flag field to match the actions' flags based on bitwise AND.
-     * @param target    A certain target.
-     * @js NA
+    #define LOW_A             ((UChar)0x0061)
+#define LOW_B             ((UChar)0x0062)
+#define LOW_C             ((UChar)0x0063)
+#define LOW_D             ((UChar)0x0064)
+#define LOW_E             ((UChar)0x0065)
+#define LOW_F             ((UChar)0x0066)
+#define LOW_G             ((UChar)0x0067)
+#define LOW_H             ((UChar)0x0068)
+#define LOW_I             ((UChar)0x0069)
+#define LOW_J             ((UChar)0x006a)
+#define LOW_K             ((UChar)0x006B)
+#define LOW_L             ((UChar)0x006C)
+#define LOW_M             ((UChar)0x006D)
+#define LOW_N             ((UChar)0x006E)
+#define LOW_O             ((UChar)0x006F)
+#define LOW_P             ((UChar)0x0070)
+#define LOW_Q             ((UChar)0x0071)
+#define LOW_R             ((UChar)0x0072)
+#define LOW_S             ((UChar)0x0073)
+#define LOW_T             ((UChar)0x0074)
+#define LOW_U             ((UChar)0x0075)
+#define LOW_V             ((UChar)0x0076)
+#define LOW_W             ((UChar)0x0077)
+#define LOW_X             ((UChar)0x0078)
+#define LOW_Y             ((UChar)0x0079)
+#define LOW_Z             ((UChar)0x007A)
+    
+    #if !UCONFIG_NO_BREAK_ITERATION
+    
+    U_NAMESPACE_END
+    
+    #include 'unicode/utypes.h'
+#include 'sharedobject.h'
+    
+    // Create a collation key from a bit array.
+CollationKey::CollationKey(const uint8_t* newValues, int32_t count)
+    : UObject(), fFlagAndLength(count),
+      fHashCode(kInvalidHashCode)
+{
+    if (count < 0 || (newValues == NULL && count != 0) ||
+            (count > getCapacity() && reallocate(count, 0) == NULL)) {
+        setToBogus();
+        return;
+    }
+    }
+    
+        /**
+     * @param keyword for example 'few' or 'other'
+     * @return the index of the plural form corresponding to the keyword, or OTHER
      */
-    virtual void removeActionsByFlags(unsigned int flags, Node *target);
+    static int32_t indexOrOtherIndexFromString(const UnicodeString &keyword) {
+        int32_t i = indexOrNegativeFromString(keyword);
+        return i >= 0 ? i : OTHER;
+    }
     
-     @code
-     // scaleA and scaleB are equivalents
-     auto scaleA = ScaleTo::create(2, 3);                 // (duration, to)
-     auto scaleB = ActionTween::create(2, 'scale', 1, 3); // (duration, key, from, to)
- @endcode
+    void DHTReplaceNodeTask::startup() { sendMessage(); }
+    
+    bool DHTResponseMessage::isReply() const { return true; }
+    
+    #include 'TimeA2.h'
+    
+    namespace aria2 {
+    }
+    
+    #include <memory>
+    
+    void DHTTokenUpdateCommand::process()
+{
+  try {
+    tokenTracker_->updateTokenSecret();
+  }
+  catch (RecoverableException& e) {
+    A2_LOG_ERROR_EX(EX_EXCEPTION_CAUGHT, e);
+  }
+}
+    
+    #include 'a2functional.h'
+    
+    
+    {    // exception out_of_range.401
+    try
+    {
+        // try to write at a nonexisting key
+        object.at('the fast') = 'il rapido';
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+}
