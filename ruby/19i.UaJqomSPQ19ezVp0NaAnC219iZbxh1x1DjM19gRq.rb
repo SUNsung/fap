0739328f6f21c88e8d7986ab7995d3049957b835
SUNsung <weatherius@gmail.com>
2019-06-23 00:52:31 +0300

    Rewrite [19i.UaJqomSPQ19ezVp0NaAnC219iZbxh1x1DjM19gRq.rb] File in: 2019-06-23 00:52:30

--- ruby/19i.UaJqomSPQ19ezVp0NaAnC219iZbxh1x1DjM19gRq.rb
+++ ruby/19i.UaJqomSPQ19ezVp0NaAnC219iZbxh1x1DjM19gRq.rb
@@ -1,169 +1,49 @@
 
         
-                  lfs_objects.each do |object|
-            yield object
-          end
-        rescue StandardError => e
-          Rails.logger.error('The Lfs import process failed. #{e.message}')
-        end
+          # Use a different logger for distributed setups.
+  # require 'syslog/logger'
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+    
+      factory :noroot_project, class: Tmuxinator::Project do
+    transient do
+      file { yaml_load('spec/fixtures/noroot.yml') }
+    end
+    
+            expect(project.startup_pane).to eq('sample:0.4')
       end
     end
   end
-end
-
     
-            def id_for_already_imported_cache(note)
-          note.id
-        end
-      end
+            options = {
+          args: project_options[:args],
+          custom_name: project_options[:custom_name],
+          force_attach: attach,
+          force_detach: detach,
+          name: project_options[:name],
+          project_config: project_options[:project_config]
+        }
+    
+      before { instance.extend Tmuxinator::WemuxSupport }
+    
+      expansion(:inspection) {
+    {
+      :inspected => o.inspected == 1 ? true : false,
+      :spam => o.spam == 1 ? true : false,
+      :spam_score => o.spam_score.to_f,
+      :threat => o.threat == 1 ? true : false,
+      :threat_details => o.threat_details
+    }
+  }
+    
+      before_action { @server = organization.servers.present.find_by_permalink!(params[:server_id]) }
+  before_action { params[:id] && @address_endpoint = @server.address_endpoints.find_by_uuid!(params[:id]) }
+    
+      def create
+    @smtp_endpoint = @server.smtp_endpoints.build(safe_params)
+    if @smtp_endpoint.save
+      flash[:notice] = params[:return_notice] if params[:return_notice].present?
+      redirect_to_with_json [:return_to, [organization, @server, :smtp_endpoints]]
+    else
+      render_form_errors 'new', @smtp_endpoint
     end
   end
-end
-
-    
-              new(hash)
-        end
-    
-        def extract_to_dir(unpack_dir, basename:, verbose:)
-      system_command! AIR_APPLICATION_INSTALLER,
-                      args:    ['-silent', '-location', unpack_dir, path],
-                      verbose: verbose
-    end
-  end
-end
-
-    
-        def URIAddEncodedOctetToBuffer(octet, result, index)
-      result[index] = 37; # Char code of '%'.
-      index         += 1
-      result[index] = @@hexCharCodeArray[octet >> 4];
-      index         += 1
-      result[index] = @@hexCharCodeArray[octet & 0x0F];
-      index += 1
-      return index;
-    end
-    
-    opts = OptionParser.new do |opts|
-  # define program name (although this defaults to the name of the file, just in case...)
-  opts.program_name = 'gollum'
-    
-  # set basic info for the '--help' command (options will be appended automatically from the below definitions)
-  opts.banner = '
-  Gollum is a multi-format Wiki Engine/API/Frontend.
-    
-  Usage:
-      gollum [options] [git-repo]
-    
-  Arguments:
-      [git-repo]                     Path to the git repository being served. If not specified, current working directory is used.
-  
-  Notes:
-      Paths for all options are relative to <git-repo> unless absolute.
-      This message is only a basic description. For more information, please visit:
-          https://github.com/gollum/gollum
-  
-  OPTIONS'
-  
-  # define gollum options  
-  opts.separator ''
-  opts.separator '  Major:'
-  
-  opts.on('-h', '--host [HOST]', 'Specify the hostname or IP address to listen on. Default: '0.0.0.0'.') do |host|
-    options[:bind] = host
-  end
-  opts.on('-p', '--port [PORT]', 'Specify the port to bind Gollum with. Default: '4567'.') do |port|
-    begin
-      # don't use 'port.to_i' here... it doesn't raise errors which might result in a nice confusion later on
-      options[:port] = Integer(port)
-    rescue ArgumentError
-      puts 'Error: '#{port}' is not a valid port number.'
-      exit 1
-    end
-  end
-  opts.on('-c', '--config [FILE]', 'Specify path to the Gollum's configuration file.') do |file|
-    options[:config] = file
-  end
-  opts.on('-r', '--ref [REF]', 'Specify the branch to serve. Default: 'master'.') do |ref|
-    wiki_options[:ref] = ref
-  end
-  opts.on('-a', '--adapter [ADAPTER]', 'Launch Gollum using a specific git adapter. Default: 'grit'.') do |adapter|
-    Gollum::GIT_ADAPTER = adapter
-  end
-  opts.on('--bare', 'Declare '<git-repo>' to be bare. This is only necessary when using the grit adapter.') do
-    wiki_options[:repo_is_bare] = true
-  end
-  opts.on('-b', '--base-path [PATH]', 'Specify the leading portion of all Gollum URLs (path info). Default: '/'.',
-    'Example: setting this to '/wiki' will make the wiki accessible under 'http://localhost:4567/wiki/'.') do |base_path|
-      
-    # first trim a leading slash, if any
-    base_path.sub!(/^\/+/, '')
-    
-        def emoji(name)
-      if emoji = Gemojione.index.find_by_name(name)
-        IO.read(EMOJI_PATHNAME.join('#{emoji['unicode']}.png'))
-      else
-        fail ArgumentError, 'emoji `#{name}' not found'
-      end
-    end
-    
-      s.name              = 'gollum'
-  s.version           = '4.1.4'
-  s.date              = '2018-10-01'
-  s.rubyforge_project = 'gollum'
-  s.license           = 'MIT'
-    
-    # Set ruby to UTF-8 mode
-# This is required for Ruby 1.8.7 which gollum still supports.
-$KCODE = 'U' if RUBY_VERSION[0, 3] == '1.8'
-    
-    desc 'Deploy website via rsync'
-task :rsync do
-  exclude = ''
-  if File.exists?('./rsync-exclude')
-    exclude = '--exclude-from '#{File.expand_path('./rsync-exclude')}''
-  end
-  puts '## Deploying website via Rsync'
-  ok_failed system('rsync -avze 'ssh -p #{ssh_port}' #{exclude} #{rsync_args} #{'--delete' unless rsync_delete == false} #{public_dir}/ #{ssh_user}:#{document_root}')
-end
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-        def get_cache_file_for(gist, file)
-      bad_chars = /[^a-zA-Z0-9\-_.]/
-      gist      = gist.gsub bad_chars, ''
-      file      = file.gsub bad_chars, ''
-      md5       = Digest::MD5.hexdigest '#{gist}-#{file}'
-      File.join @cache_folder, '#{gist}-#{file}-#{md5}.cache'
-    end
-    
-    When /^(?:|I )press '([^']*)'$/ do |button|
-  click_button(button)
-end
-    
-        def blank_name?
-      @filepath.nil? || @filepath.empty?
-    end
