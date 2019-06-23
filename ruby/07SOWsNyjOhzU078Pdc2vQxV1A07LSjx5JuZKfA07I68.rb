bc18d48afa7326a6d4ed02d43e85d88c776b0b4b
SUNsung <weatherius@gmail.com>
2019-06-23 22:23:06 +0300

    Rewrite [07SOWsNyjOhzU078Pdc2vQxV1A07LSjx5JuZKfA07I68.rb] File in: 2019-06-23 22:23:06

--- ruby/07SOWsNyjOhzU078Pdc2vQxV1A07LSjx5JuZKfA07I68.rb
+++ ruby/07SOWsNyjOhzU078Pdc2vQxV1A07LSjx5JuZKfA07I68.rb
@@ -1,132 +1,135 @@
 
         
-        desc 'Open an irb session preloaded with this library'
-task :console do
-  sh 'irb -rubygems -r ./lib/#{name}.rb'
+              unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
+      end
+    end
+    
+        def to_json
+      JSON.generate(as_json)
+    end
+  end
 end
 
     
-    def pr(url)
-  if url.end_with?(FORWARD_SLASH)
-    url
-  else
-    url_dir = File.dirname(url)
-    url_dir.end_with?(FORWARD_SLASH) ? url_dir : '#{url_dir}/'
+        def mime_type
+      headers['Content-Type'] || 'text/plain'
+    end
+    
+        # we assume that the first file that requires 'sinatra' is the
+    # app_file. all other path related options are calculated based
+    # on this path by default.
+    set :app_file, caller_files.first || $0
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
+    
+    require 'clamp'
+require 'pluginmanager/util'
+require 'pluginmanager/gemfile'
+require 'pluginmanager/install'
+require 'pluginmanager/remove'
+require 'pluginmanager/list'
+require 'pluginmanager/update'
+require 'pluginmanager/pack'
+require 'pluginmanager/unpack'
+require 'pluginmanager/generate'
+require 'pluginmanager/prepare_offline_pack'
+require 'pluginmanager/proxy_support'
+configure_proxy
+    
+    describe LogStash::Config::PipelineConfig do
+  let(:source) { LogStash::Config::Source::Local }
+  let(:pipeline_id) { :main }
+  let(:ordered_config_parts) do
+    [
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/1', 0, 0, 'input { generator1 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/2', 0, 0,  'input { generator2 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/3', 0, 0, 'input { generator3 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/4', 0, 0, 'input { generator4 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/5', 0, 0, 'input { generator5 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/6', 0, 0, 'input { generator6 }'),
+      org.logstash.common.SourceWithMetadata.new('string', 'config_string', 0, 0, 'input { generator1 }'),
+    ]
   end
-end
     
-    def global_require
-  JSON.pretty_generate(DATA)
-end
-    
-            def connect(websocket, handshake)
-          @connections_count += 1
-          if @connections_count == 1
-            message = 'Browser connected'
-            message += ' over SSL/TLS' if handshake.secure?
-            Jekyll.logger.info 'LiveReload:', message
+      describe 'on #{logstash.hostname}' do
+    context 'with a direct internet connection' do
+      context 'when the plugin exist' do
+        context 'from a local `.GEM` file' do
+          let(:gem_name) { 'logstash-filter-qatest-0.1.1.gem' }
+          let(:gem_path_on_vagrant) { '/tmp/#{gem_name}' }
+          before(:each) do
+            logstash.download('https://rubygems.org/gems/#{gem_name}', gem_path_on_vagrant)
           end
-          websocket.send(
-            JSON.dump(
-              :command    => 'hello',
-              :protocols  => ['http://livereload.com/protocols/official-7'],
-              :serverName => 'jekyll'
-            )
-          )
     
-          # A single page of data and the corresponding page number.
-      Page = Struct.new(:objects, :number)
-    
-            def representation_class
-          Representation::Issue
+          # the per_page_dropdown is used on index pages like orders, products, promotions etc.
+      # this method generates the select_tag
+      def per_page_dropdown
+        # there is a config setting for admin_products_per_page, only for the orders page
+        if @products && per_page_default = Spree::Config.admin_products_per_page
+          per_page_options = []
+          5.times do |amount|
+            per_page_options << (amount + 1) * Spree::Config.admin_products_per_page
+          end
+        else
+          per_page_default = Spree::Config.admin_orders_per_page
+          per_page_options = %w{15 30 45 60}
         end
     
-          # Returns the identifier to use for cache keys.
-      #
-      # For issues and pull requests this will be 'Issue' or 'MergeRequest'
-      # respectively. For diff notes this will return 'MergeRequest', for
-      # regular notes it will either return 'Issue' or 'MergeRequest' depending
-      # on what type of object the note belongs to.
-      def cache_key_type
-        if object.respond_to?(:issuable_type)
-          object.issuable_type
-        elsif object.respond_to?(:noteable_type)
-          object.noteable_type
-        else
-          raise(
-            TypeError,
-            'Instances of #{object.class} are not supported'
-          )
+            def create
+          authorize! :create, Spree::Order
+          if can?(:admin, Spree::Order)
+            order_user = if @current_user_roles.include?('admin') && order_params[:user_id]
+                           Spree.user_class.find(order_params[:user_id])
+                         else
+                           current_api_user
+                         end
+    
+    Vagrant.configure('2') do |config|
+  # All Vagrant configuration is done here. The most common configuration
+  # options are documented and commented below. For a complete reference,
+  # please see the online documentation at vagrantup.com.
+    
+              checksums.keys.each do |path|
+            add_path(tar, '/' + path, File.join(staging_path, path))
+          end
         end
       end
+    end
+  end # def output
     
-              hash = {
-            iid: issue.number,
-            title: issue.title,
-            description: issue.body,
-            milestone_number: issue.milestone&.number,
-            state: issue.state == 'open' ? :opened : :closed,
-            assignees: issue.assignees.map do |u|
-              Representation::User.from_api_response(u)
-            end,
-            label_names: issue.labels.map(&:name),
-            author: user,
-            created_at: issue.created_at,
-            updated_at: issue.updated_at,
-            pull_request: issue.pull_request ? true : false
-          }
+        # Licenses could include more than one.
+    # Speaking of just taking the first entry of the field:
+    # A crude thing to do, but I suppose it's better than nothing.
+    # -- Daniel Haskin, 3/24/2015
+    self.license = control['license'][0] || self.license
     
-        def absolute_url_string?(str)
-      str =~ SCHEME_RGX
+        if attributes[:pear_php_dir]
+      logger.info('Setting php_dir', :php_dir => attributes[:pear_php_dir])
+      safesystem('pear', '-c', config, 'config-set', 'php_dir', '#{staging_path(installroot)}/#{attributes[:pear_php_dir]}')
     end
     
-        def parse_as_document
-      document = Nokogiri::HTML.parse @content, nil, 'UTF-8'
-      @title = document.at_css('title').try(:content)
-      document
-    end
-    
-            css('a.is-button > h3').each do |node|
-          node.parent.content = node.content
-        end
-    
-            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
-          node.before(node.children).remove
-        end
-    
-      def show
-    @status = status_finder.status
-    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
-  end
-    
-    class Api::PushController < Api::BaseController
-  include SignatureVerification
-    
-      def rate_limit_limit
-    api_throttle_data[:limit].to_s
-  end
-    
-      included do
-    before_action :set_session_activity
-  end
-    
-      def update?
-    edit?
-  end
-    
-      factory :nameless_window_project, class: Tmuxinator::Project do
-    transient do
-      file { yaml_load('spec/fixtures/nameless_window.yml') }
-    end
-    
-    describe Tmuxinator::Pane do
-  let(:klass) { described_class }
-  let(:instance) { klass.new(index, project, window, *commands) }
-  # let(:index) { 'vim' }
-  # let(:project) { 0 }
-  # let(:tab) { nil }
-  # let(:commands) { nil }
-  let(:index) { 0 }
-  let(:project) { double }
-  let(:window) { double }
-  let(:commands) { ['vim', 'bash'] }
+      def output(output_path)
+    self.scripts.each do |name, path|
+      case name
+        when 'pre-install'
+          safesystem('cp', path, './preinstall')
+          File.chmod(0755, './preinstall')
+        when 'post-install'
+          safesystem('cp', path, './postinstall')
+          File.chmod(0755, './postinstall')
+        when 'pre-uninstall'
+          raise FPM::InvalidPackageConfiguration.new(
+            'pre-uninstall is not supported by Solaris packages'
+          )
+        when 'post-uninstall'
+          raise FPM::InvalidPackageConfiguration.new(
+            'post-uninstall is not supported by Solaris packages'
+          )
+      end # case name
+    end # self.scripts.each
