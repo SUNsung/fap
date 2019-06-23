e7fe866c5f808953f54e7593d3430d175d539562
SUNsung <weatherius@gmail.com>
2019-06-23 11:51:46 +0300

    Rewrite [32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb] File in: 2019-06-23 11:51:46

--- ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
+++ ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
@@ -1,245 +1,43 @@
 
         
-        module FastlaneCore
-  class AnalyticsSession
-    GA_TRACKING = 'UA-121171860-1'
-    
-          context 'when specify log_path' do
-        context 'when command is archive' do
-          let(:command) { 'archive' }
-          it '--log-path option is not present' do
-            expect do
-              Fastlane::FastFile.new.parse('lane :test do
-                carthage(command: '#{command}', log_path: 'bla.log')
-              end').runner.execute(:test)
-            end.to raise_error('Log path option is available only for 'build', 'bootstrap', and 'update' command.')
-          end
-        end
-    
-        # [String] the name of the environment variable, which is only used if no other values were found
-    attr_accessor :env_name
-    
-    module Cert
-  class CommandsGenerator
-    include Commander::Methods
-    
-            def sidekiq_worker_class
-          ImportNoteWorker
-        end
-    
-          # project - An instance of `Project`.
-      # object - The object to look up or set a database ID for.
-      def initialize(project, object)
-        @project = project
-        @object = object
-      end
-    
-          # Returns the ID to use for the cache used for checking if an object has
-      # already been imported or not.
-      #
-      # object - The object we may want to import.
-      def id_for_already_imported_cache(object)
-        raise NotImplementedError
-      end
-    
-            # Builds a new issue using a Hash that was built from a JSON payload.
-        def self.from_json_hash(raw_hash)
-          hash = Representation.symbolize_hash(raw_hash)
-    
-          it 'deactivates an existing user' do
-        visit admin_users_path
-        expect(page).to have_no_text('inactive')
-        find(:css, 'a[href='/admin/users/#{users(:bob).id}/deactivate']').click
-        expect(page).to have_text('inactive')
-        users(:bob).reload
-        expect(users(:bob)).not_to be_active
-      end
-    
-        context 'when this scenario has never been seen before' do
-      describe '#import' do
-        it 'makes a new scenario' do
-          expect {
-            scenario_import.import(:skip_agents => true)
-          }.to change { users(:bob).scenarios.count }.by(1)
-    
-        context '#run' do
-      before do
-        mock(@agent_runner).run_workers
-      end
-    
-        it 'unregisters deleted SchedulerAgents' do
-      @scheduler.schedule_scheduler_agents
-    
-      describe '#recursively_interpolate_jsonpaths' do
-    it 'interpolates all string values in a structure' do
-      struct = {
-        :int => 5,
-        :string => 'this <escape $.works>',
-        :array => ['<works>', 'now', '<$.there.world>'],
-        :deep => {
-          :string => 'hello <there.world>',
-          :hello => :world
-        }
-      }
-      data = { :there => { :world => 'WORLD' }, :works => 'should work' }
-      expect(Utils.recursively_interpolate_jsonpaths(struct, data)).to eq({
-        :int => 5,
-        :string => 'this should+work',
-        :array => ['should work', 'now', 'WORLD'],
-        :deep => {
-          :string => 'hello WORLD',
-          :hello => :world
-        }
-      })
-    end
-  end
-    
-      describe 'up' do
-    it 'should update extract and template options for an existing WebsiteAgent' do
-      expect(agent.options).to include('extract' => old_extract,
-                                       'template' => old_template)
-      ConvertWebsiteAgentTemplateForMerge.new.up
-      agent.reload
-      expect(agent.options).to include('extract' => new_extract,
-                                       'template' => new_template)
-    end
-  end
-    
-      def translation_scope
-    'devise.omniauth_callbacks'
-  end
-end
-
-    
-      def respond_to_on_destroy
-    # We actually need to hardcode this as Rails default responder doesn't
-    # support returning empty response on GET request
-    respond_to do |format|
-      format.all { head :no_content }
-      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
-    end
-  end
-end
-
-    
-          # Sign in a user bypassing the warden callbacks and stores the user
-      # straight in session. This option is useful in cases the user is already
-      # signed in, but we want to refresh the credentials in session.
-      #
-      # Examples:
-      #
-      #   bypass_sign_in @user, scope: :user
-      #   bypass_sign_in @user
-      def bypass_sign_in(resource, scope: nil)
-        scope ||= Devise::Mapping.find_scope!(resource)
-        expire_data_after_sign_in!
-        warden.session_serializer.store(resource, scope)
-      end
-    
-          def remove_domain_from_uri(uri)
-        [uri.path.sub(/\A\/+/, '/'), uri.query].compact.join('?')
-      end
-    
-          generate_helpers!(Devise::URL_HELPERS)
-    
-          if failed_attributes.any?
-        fail Devise::Models::MissingAttribute.new(failed_attributes)
-      end
+            # See {CapabilityHost#capability}
+    def capability(*args)
+      super
+    rescue Errors::CapabilityNotFound => e
+      raise Errors::GuestCapabilityNotFound,
+        cap: e.extra_data[:cap],
+        guest: name
+    rescue Errors::CapabilityInvalid => e
+      raise Errors::GuestCapabilityInvalid,
+        cap: e.extra_data[:cap],
+        guest: name
     end
     
-          # Checks whether the user session has expired based on configured time.
-      def timedout?(last_access)
-        !timeout_in.nil? && last_access && last_access <= timeout_in.ago
-      end
+    module Vagrant
+  module Plugin
+    module V2
+      # This class maintains a list of all the registered plugins as well
+      # as provides methods that allow querying all registered components of
+      # those plugins as a single unit.
+      class Manager
+        attr_reader :registered
     
-        AIR_APPLICATION_INSTALLER =
-      '/Applications/Utilities/Adobe AIR Application Installer.app/Contents/MacOS/Adobe AIR Application Installer'
-    
-    FIRST     = -> l { LEFT[RIGHT[l]] }
-IF        = -> b { b }
-LEFT      = -> p { p[-> x { -> y { x } } ] }
-RIGHT     = -> p { p[-> x { -> y { y } } ] }
-IS_EMPTY  = LEFT
-REST      = -> l { RIGHT[RIGHT[l]] }
-    
-    # Include LoggerSilence from ActiveSupport. This is needed to silent assets
-# requests with `config.assets.quiet`, because the default silence method of
-# the logging gem is no-op. See: https://github.com/TwP/logging/issues/11
-Logging::Logger.send :alias_method, :local_level, :level
-Logging::Logger.send :alias_method, :local_level=, :level=
-Logging::Logger.send :include, LoggerSilence
-
-    
-      class Notification < ApplicationRecord
-  end
-    
-    def await_condition &condition
-  start_time = Time.zone.now
-  until condition.call
-    return false if (Time.zone.now - start_time) > Capybara.default_max_wait_time
-    sleep 0.05
-  end
-  true
-end
-
-    
-    describe ParticipationsController, :type => :controller do
-  before do
-    allow(@controller).to receive(:current_user).and_return(alice)
-    sign_in alice, scope: :user
-  end
-    
-          it 'it doesn't call toggle_hidden_shareable' do
-        expect(@controller.current_user).not_to receive(:toggle_hidden_shareable).with(an_instance_of(StatusMessage))
-        begin
-          put :update, params: {id: 42, post_id: @status.id}, format: :js
-        rescue ActiveRecord::RecordNotFound
-        end
-      end
+        # Return the number of elements in this registry.
+    #
+    # @return [Integer]
+    def length
+      @items.keys.length
     end
-  end
-end
-
+    alias_method :size, :length
     
-    if $0 == __FILE__
-  begin
-    LogStash::PluginManager::Main.run('bin/logstash-plugin', ARGV)
-  rescue LogStash::PluginManager::Error => e
-    $stderr.puts(e.message)
-    exit(1)
-  end
-end
-
+      # fetch data
+  fields = {
+    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
+    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
+    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
+  }
     
-              it 'successfully install the plugin when verification is disabled' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest')
-          end
-    
-        context 'with a specific plugin' do
-      let(:plugin_name) { 'logstash-input-stdin' }
-      it 'list the plugin and display the plugin name' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
-        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
-      end
-    
-    desc 'list tasks'
-task :list do
-  puts 'Tasks: #{(Rake::Task.tasks - [Rake::Task[:list]]).join(', ')}'
-  puts '(type rake -T for more detail)\n\n'
-end
-
-    
-      get(/.+/) do
-    send_sinatra_file(request.path) {404}
-  end
-    
-      # Summary is used on the Archive pages to return the first block of content from a post.
-  def summary(input)
-    if input.index(/\n\n/)
-      input.split(/\n\n/)[0]
-    else
-      input
+        it 'denies requests with sneaky encoded session cookies' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.%73ession=SESSION_TOKEN'
+      expect(last_response).not_to be_ok
     end
-  end
