b476e668f1219f6dfefc22321188b3cbf1ceb5ac
SUNsung <weatherius@gmail.com>
2019-06-23 09:21:33 +0300

    Rewrite [43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py] File in: 2019-06-23 09:21:32

--- pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
+++ pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
@@ -1,391 +1,43 @@
 
         
-            cert = None
-    if args.cert:
-        cert = args.cert
-        if args.cert_key:
-            cert = cert, args.cert_key
+                    try:
+                self.working_appid_list.remove(appid)
+            except:
+                pass
     
-    import requests.auth
+        if __hostsdeny__ and netloc.endswith(__hostsdeny__):
+        start_response('403 Forbidden', [('Content-Type', 'text/html')])
+        yield message_html('403 Hosts Deny', 'Hosts Deny(%r)' % netloc, detail='共用appid因为资源有限，限制观看视频和文件下载等消耗资源过多的访问，请使用自己的appid <a href=' https://github.com/XX-net/XX-Net/wiki/Register-Google-appid' target='_blank'>帮助</a> ')
+        raise StopIteration
     
+    # begin[licence]
+#
+# [The 'BSD licence']
+# Copyright (c) 2005-2008 Terence Parr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# end[licence]
     
-@pytest.mark.parametrize('argument_name', ['--auth-type', '-A'])
-def test_digest_auth(httpbin_both, argument_name):
-    r = http(argument_name + '=digest', '--auth=user:password',
-             'GET', httpbin_both.url + '/digest-auth/auth/user/password')
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
-    
-        plugin_manager.register(Plugin)
-    try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            BASIC_AUTH_HEADER_VALUE,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
-    
-        config['implicit_content_type'] = 'form'
-    config.save()
-    config.load()
-    assert 'implicit_content_type' not in config
-    assert config['default_options'] == ['--form']
-    
-    
-@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
-def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
-    r = http(follow_flag, httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 1
-    assert HTTP_OK in r
-    
-    
-def test_unicode_basic_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--verbose', '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/basic-auth/test/' + UNICODE)
-    
-        def test_upload_multiple_fields_with_the_same_name(self, httpbin):
-        r = http('--form', '--verbose', 'POST', httpbin.url + '/post',
-                 'test-file@%s' % FILE_PATH_ARG,
-                 'test-file@%s' % FILE_PATH_ARG)
-        assert HTTP_OK in r
-        assert r.count('Content-Disposition: form-data; name='test-file';'
-                       ' filename='%s'' % os.path.basename(FILE_PATH)) == 2
-        # Should be 4, but is 3 because httpbin
-        # doesn't seem to support filed field lists
-        assert r.count(FILE_CONTENT) in [3, 4]
-        assert r.count('Content-Type: text/plain') == 2
-    
-        if args.auth_plugin:
-        session.auth = {
-            'type': args.auth_plugin.auth_type,
-            'raw_auth': args.auth_plugin.raw_auth,
-        }
-    elif session.auth:
-        kwargs['auth'] = session.auth
-    
-        json_output = {}
-    if mod_params['view'] == 'topics':
-        json_output['topics'] = list_func(pubsub_client.list_topics())
-    elif mod_params['view'] == 'subscriptions':
-        if mod_params['topic']:
-            t = pubsub_client.topic(mod_params['topic'])
-            json_output['subscriptions'] = list_func(t.list_subscriptions())
-        else:
-            json_output['subscriptions'] = list_func(pubsub_client.list_subscriptions())
-    
-    DOCUMENTATION = '''
----
-module: gcspanner
-version_added: '2.3'
-short_description: Create and Delete Instances/Databases on Spanner
-description:
-    - Create and Delete Instances/Databases on Spanner.
-      See U(https://cloud.google.com/spanner/docs) for an overview.
-requirements:
-  - python >= 2.6
-  - google-auth >= 0.5.0
-  - google-cloud-spanner >= 0.23.0
-notes:
-  - Changing the configuration on an existing instance is not supported.
-author:
-  - Tom Melendez (@supertom) <tom@supertom.com>
-options:
-  configuration:
-    description:
-       - Configuration the instance should use.
-       - Examples are us-central1, asia-east1 and europe-west1.
-    required: yes
-  instance_id:
-    description:
-       - GCP spanner instance name.
-    required: yes
-  database_name:
-    description:
-       - Name of database contained on the instance.
-  force_instance_delete:
-    description:
-       - To delete an instance, this argument must exist and be true (along with state being equal to absent).
-    type: bool
-    default: 'no'
-  instance_display_name:
-    description:
-       - Name of Instance to display.
-       - If not specified, instance_id will be used instead.
-  node_count:
-    description:
-       - Number of nodes in the instance.
-    default: 1
-  state:
-    description:
-    - State of the instance or database. Applies to the most granular resource.
-    - If a C(database_name) is specified we remove it.
-    - If only C(instance_id) is specified, that is what is removed.
-    choices: [ absent, present ]
-    default: present
-'''
-    
-        result['delete_rule'] = list()
-    result['delete_rule_rc'] = list()
-    for rule in current_rules:
-        if rule not in desired_rules:
-            deletions += 1
-            if not module.check_mode:
-                result['delete_rule'].append(rule)
-                rc = gateway.del_nat_rule(**rule)
-                result['delete_rule_rc'].append(rc)
-            result['changed'] = True
-    result['rules_deleted'] = deletions
-    
-    
-def get_role_diff(client, ipa_role, module_role):
-    return client.get_diff(ipa_data=ipa_role, module_data=module_role)
-    
-        # Send the data to bigpanda
-    data = json.dumps(body)
-    headers = {'Authorization': 'Bearer %s' % token, 'Content-Type': 'application/json'}
-    try:
-        response, info = fetch_url(module, request_url, data=data, headers=headers)
-        if info['status'] == 200:
-            module.exit_json(changed=True, **deployment)
-        else:
-            module.fail_json(msg=json.dumps(info))
-    except Exception as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-    
-        module = AnsibleModule(
-        argument_spec=dict(
-            token=dict(required=True, no_log=True),
-            environment=dict(required=True),
-            user=dict(required=False),
-            repo=dict(required=False),
-            revision=dict(required=False),
-            url=dict(required=False, default='https://api.honeybadger.io/v1/deploys'),
-            validate_certs=dict(default='yes', type='bool'),
-        ),
-        supports_check_mode=True
-    )
-    
-            if module.check_mode:
-            module.exit_json(changed=True)
-        rc, out, err = module.run_command([le_path, 'rm', log])
-    
-        elif not running and state == 'monitored':
-        if module.check_mode:
-            module.exit_json(changed=True)
-        status = run_command('monitor')
-        if status not in ['not monitored']:
-            module.exit_json(changed=True, name=name, state=state)
-        module.fail_json(msg='%s process not monitored' % name, status=status)
-    
-        xml = '''<?xml version='1.0' encoding='UTF-8'?>
-        <rs:model-request throttlesize='5'
-        xmlns:rs='http://www.ca.com/spectrum/restful/schema/request'
-        xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
-        xsi:schemaLocation='http://www.ca.com/spectrum/restful/schema/request ../../../xsd/Request.xsd'>
-            <rs:target-models>
-            <rs:models-search>
-                <rs:search-criteria xmlns='http://www.ca.com/spectrum/restful/schema/filter'>
-                    <action-models>
-                        <filtered-models>
-                            <and>
-                                <equals>
-                                    <model-type>SearchManager</model-type>
-                                </equals>
-                                <greater-than>
-                                    <attribute id='0x129fa'>
-                                        <value>{mh_min}</value>
-                                    </attribute>
-                                </greater-than>
-                                <less-than>
-                                    <attribute id='0x129fa'>
-                                        <value>{mh_max}</value>
-                                    </attribute>
-                                </less-than>
-                            </and>
-                        </filtered-models>
-                        <action>FIND_DEV_MODELS_BY_IP</action>
-                        <attribute id='AttributeID.NETWORK_ADDRESS'>
-                            <value>{search_ip}</value>
-                        </attribute>
-                    </action-models>
-                </rs:search-criteria>
-            </rs:models-search>
-            </rs:target-models>
-            <rs:requested-attribute id='0x12d7f' /> <!--Network Address-->
-        </rs:model-request>
-        '''.format(search_ip=device_ip, mh_min=landscape_min, mh_max=landscape_max)
-    
-    
-DOCUMENTATION = '''
-    
-    
-def plot_feature_errors(all_errors, batch_size, all_components, data):
-    plt.figure()
-    plot_results(all_components, all_errors['pca'], label='PCA')
-    plot_results(all_components, all_errors['ipca'],
-                 label='IncrementalPCA, bsize=%i' % batch_size)
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm error vs. n_components\n'
-                 'LFW, size %i x %i' % data.shape)
-    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
-    plt.ylabel('Mean absolute error')
-    
-        # TASK: Build a vectorizer / classifier pipeline that filters out tokens
-    # that are too rare or too frequent
-    pipeline = Pipeline([
-        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
-        ('clf', LinearSVC(C=1000)),
-    ])
-    
-    fit_data = data[np.argsort(model.row_labels_)]
-fit_data = fit_data[:, np.argsort(model.column_labels_)]
-    
-    
-# Plot the distances
-for index, metric in enumerate(['cosine', 'euclidean', 'cityblock']):
-    avg_dist = np.zeros((n_clusters, n_clusters))
-    plt.figure(figsize=(5, 4.5))
-    for i in range(n_clusters):
-        for j in range(n_clusters):
-            avg_dist[i, j] = pairwise_distances(X[y == i], X[y == j],
-                                                metric=metric).mean()
-    avg_dist /= avg_dist.max()
-    for i in range(n_clusters):
-        for j in range(n_clusters):
-            plt.text(i, j, '%5.3f' % avg_dist[i, j],
-                     verticalalignment='center',
-                     horizontalalignment='center')
-    
-    plt.figure(figsize=(12, 12))
-    
-    import numpy as np
-from sklearn.cluster import MeanShift, estimate_bandwidth
-from sklearn.datasets.samples_generator import make_blobs
-    
-                    for k in range(boxes.shape[0]):
-                    score = boxes[k, -1]
-                    mask = masks[:, :, k]
-                    pngname = os.path.join(
-                        'results',
-                        basename + '_' + clss + '_{}.png'.format(k))
-                    # write txt
-                    fid_txt.write('{} {} {}\n'.format(pngname, clss_id, score))
-                    # save mask
-                    cv2.imwrite(os.path.join(output_dir, pngname), mask * 255)
-    logger.info('Evaluating...')
-    cityscapes_eval.main([])
-    return None
-
-    
-        # Eqn.(1) in FPN paper
-    target_lvls = np.floor(lvl0 + np.log2(s / s0 + 1e-6))
-    target_lvls = np.clip(target_lvls, k_min, k_max)
-    return target_lvls
-    
-    
-# ---------------------------------------------------------------------------- #
-# RPN and Faster R-CNN outputs and losses
-# ---------------------------------------------------------------------------- #
-    
-        # rois are in [[batch_idx, x0, y0, x1, y2], ...] format
-    # Combine predictions across all levels and retain the top scoring
-    rois = np.concatenate([blob.data for blob in roi_inputs])
-    scores = np.concatenate([blob.data for blob in score_inputs]).squeeze()
-    inds = np.argsort(-scores)[:post_nms_topN]
-    rois = rois[inds, :]
-    return rois
-    
-    
-def get_fast_rcnn_blob_names(is_training=True):
-    '''Fast R-CNN blob names.'''
-    # rois blob: holds R regions of interest, each is a 5-tuple
-    # (batch_idx, x1, y1, x2, y2) specifying an image batch index and a
-    # rectangle (x1, y1, x2, y2)
-    blob_names = ['rois']
-    if is_training:
-        # labels_int32 blob: R categorical labels in [0, ..., K] for K
-        # foreground classes plus background
-        blob_names += ['labels_int32']
-    if is_training:
-        # bbox_targets blob: R bounding-box regression targets with 4
-        # targets per class
-        blob_names += ['bbox_targets']
-        # bbox_inside_weights blob: At most 4 targets per roi are active
-        # this binary vector sepcifies the subset of active targets
-        blob_names += ['bbox_inside_weights']
-        blob_names += ['bbox_outside_weights']
-    if is_training and cfg.MODEL.MASK_ON:
-        # 'mask_rois': RoIs sampled for training the mask prediction branch.
-        # Shape is (#masks, 5) in format (batch_idx, x1, y1, x2, y2).
-        blob_names += ['mask_rois']
-        # 'roi_has_mask': binary labels for the RoIs specified in 'rois'
-        # indicating if each RoI has a mask or not. Note that in some cases
-        # a *bg* RoI will have an all -1 (ignore) mask associated with it in
-        # the case that no fg RoIs can be sampled. Shape is (batchsize).
-        blob_names += ['roi_has_mask_int32']
-        # 'masks_int32' holds binary masks for the RoIs specified in
-        # 'mask_rois'. Shape is (#fg, M * M) where M is the ground truth
-        # mask size.
-        blob_names += ['masks_int32']
-    if is_training and cfg.MODEL.KEYPOINTS_ON:
-        # 'keypoint_rois': RoIs sampled for training the keypoint prediction
-        # branch. Shape is (#instances, 5) in format (batch_idx, x1, y1, x2,
-        # y2).
-        blob_names += ['keypoint_rois']
-        # 'keypoint_locations_int32': index of keypoint in
-        # KRCNN.HEATMAP_SIZE**2 sized array. Shape is (#instances). Used in
-        # SoftmaxWithLoss.
-        blob_names += ['keypoint_locations_int32']
-        # 'keypoint_weights': weight assigned to each target in
-        # 'keypoint_locations_int32'. Shape is (#instances). Used in
-        # SoftmaxWithLoss.
-        blob_names += ['keypoint_weights']
-        # 'keypoint_loss_normalizer': optional normalization factor to use if
-        # cfg.KRCNN.NORMALIZE_BY_VISIBLE_KEYPOINTS is False.
-        blob_names += ['keypoint_loss_normalizer']
-    if cfg.FPN.FPN_ON and cfg.FPN.MULTILEVEL_ROIS:
-        # Support for FPN multi-level rois without bbox reg isn't
-        # implemented (... and may never be implemented)
-        k_max = cfg.FPN.ROI_MAX_LEVEL
-        k_min = cfg.FPN.ROI_MIN_LEVEL
-        # Same format as rois blob, but one per FPN level
-        for lvl in range(k_min, k_max + 1):
-            blob_names += ['rois_fpn' + str(lvl)]
-        blob_names += ['rois_idx_restore_int32']
-        if is_training:
-            if cfg.MODEL.MASK_ON:
-                for lvl in range(k_min, k_max + 1):
-                    blob_names += ['mask_rois_fpn' + str(lvl)]
-                blob_names += ['mask_rois_idx_restore_int32']
-            if cfg.MODEL.KEYPOINTS_ON:
-                for lvl in range(k_min, k_max + 1):
-                    blob_names += ['keypoint_rois_fpn' + str(lvl)]
-                blob_names += ['keypoint_rois_idx_restore_int32']
-    return blob_names
-    
-        blobs['keypoint_rois'] = sampled_fg_rois
-    blobs['keypoint_locations_int32'] = heats.astype(np.int32, copy=False)
-    blobs['keypoint_weights'] = weights
-    
-    '''Construct minibatches for Detectron networks.'''
-    
-    
-class BatchPermutationOpTest(unittest.TestCase):
-    def _run_op_test(self, X, I, check_grad=False):
-        with core.DeviceScope(core.DeviceOption(caffe2_pb2.CUDA, 0)):
-            op = core.CreateOperator('BatchPermutation', ['X', 'I'], ['Y'])
-            workspace.FeedBlob('X', X)
-            workspace.FeedBlob('I', I)
-        workspace.RunOperatorOnce(op)
-        Y = workspace.FetchBlob('Y')
-    
-    
-class OutOfData(UnpackException):
-    pass
+            raise NotImplementedError
