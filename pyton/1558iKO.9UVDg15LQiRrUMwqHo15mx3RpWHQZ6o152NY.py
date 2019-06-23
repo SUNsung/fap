f9d10a3fa62e5d48cd65d5ddf6e671c5b140be57
SUNsung <weatherius@gmail.com>
2019-06-23 12:19:17 +0300

    Rewrite [1558iKO.9UVDg15LQiRrUMwqHo15mx3RpWHQZ6o152NY.py] File in: 2019-06-23 12:19:17

--- pyton/1558iKO.9UVDg15LQiRrUMwqHo15mx3RpWHQZ6o152NY.py
+++ pyton/1558iKO.9UVDg15LQiRrUMwqHo15mx3RpWHQZ6o152NY.py
@@ -1,79 +1,136 @@
 
         
-            author = proj_info['author'],
-    author_email = proj_info['author_email'],
-    url = proj_info['url'],
-    license = proj_info['license'],
+        
+class Supervisor(Employee):
     
-    from ..common import *
+        def remove_user(self, user_id):
+        pass
     
-            for i in link_list:
-            self.stream_types.append({'id': str(i[0])})
-            self.streams[i[0]] = {'url': i[1]}
+        def reducer(self, key, values):
+        '''Sum values for each key.
+    
+        def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.friend_ids = []
+    
+        def get(self, key):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                return item.value
+        raise KeyError('Key not found')
+    
+        # Used for issues that may cause the system to operate in a degraded (but
+    # still operational) state, as well as configuration options that are set
+    # in unexpected ways or deprecated in future versions.
+    SEVERITY_WARNING = 'warning'
+    
+            backlogged, size = None, 0
+        from sentry.monitoring.queues import backend
+        if backend is not None:
+            size = backend.get_size('default')
+            backlogged = size > 0
+    
+            # Adding index on 'EventTag', fields ['environment_id', 'key', 'value']
+        db.create_index(u'tagstore_eventtag', ['environment_id', 'key_id', 'value_id'])
+    
+            # Changing field 'GroupTagValue.project_id'
+        db.alter_column(u'tagstore_grouptagvalue', 'project_id', self.gf(
+            'sentry.db.models.fields.bounded.BoundedPositiveIntegerField')())
+    
+        class Meta:
+        app_label = 'tagstore'
+        unique_together = (('project_id', 'event_id', 'key', 'value'), )
+        index_together = (
+            ('project_id', 'key', 'value'),
+            ('group_id', 'key', 'value'),
+        )
+    
+    logger = logging.getLogger('sentry.scheduler')
+    
+        # Histogram of ground-truth objects
+    gt_hist = np.zeros((len(classes)), dtype=np.int)
+    for entry in roidb:
+        gt_inds = np.where(
+            (entry['gt_classes'] > 0) & (entry['is_crowd'] == 0))[0]
+        gt_classes = entry['gt_classes'][gt_inds]
+        gt_hist += np.histogram(gt_classes, bins=hist_bins)[0]
+    logger.debug('Ground-truth class histogram:')
+    for i, v in enumerate(gt_hist):
+        logger.debug(
+            '{:d}{:s}: {:d}'.format(
+                i, classes[i].rjust(char_len), v))
+    logger.debug('-' * char_len)
+    logger.debug(
+        '{:s}: {:d}'.format(
+            'total'.rjust(char_len), np.sum(gt_hist)))
+
     
     
-class CNTV(VideoExtractor):
-    name = 'CNTV.com'
-    stream_types = [
-        {'id': '1', 'video_profile': '1280x720_2000kb/s', 'map_to': 'chapters4'},
-        {'id': '2', 'video_profile': '1280x720_1200kb/s', 'map_to': 'chapters3'},
-        {'id': '3', 'video_profile': '640x360_850kb/s', 'map_to': 'chapters2'},
-        {'id': '4', 'video_profile': '480x270_450kb/s', 'map_to': 'chapters'},
-        {'id': '5', 'video_profile': '320x180_200kb/s', 'map_to': 'lowChapters'},
-    ]
+def bottleneck_transformation(
+    model,
+    blob_in,
+    dim_in,
+    dim_out,
+    stride,
+    prefix,
+    dim_inner,
+    dilation=1,
+    group=1
+):
+    '''Add a bottleneck transformation to the model.'''
+    # In original resnet, stride=2 is on 1x1.
+    # In fb.torch resnet, stride=2 is on 3x3.
+    (str1x1, str3x3) = (stride, 1) if cfg.RESNETS.STRIDE_1X1 else (1, stride)
     
-        mime, ext, size = url_info(real_url)
     
-    # (api_key, method, ext, page)
-tmpl_api_call = (
-    'https://api.flickr.com/services/rest?'
-    '&format=json&nojsoncallback=1'
-    # UNCOMMENT FOR TESTING
-    #'&per_page=5'
-    '&per_page=500'
-    # this parameter CANNOT take control of 'flickr.galleries.getPhotos'
-    # though the doc said it should.
-    # it's always considered to be 500
-    '&api_key=%s'
-    '&method=flickr.%s'
-    '&extras=url_sq,url_q,url_t,url_s,url_n,url_m,url_z,url_c,url_l,url_h,url_k,url_o,media'
-    '%s&page=%d'
-)
+def _mkanchors(ws, hs, x_ctr, y_ctr):
+    '''Given a vector of widths (ws) and heights (hs) around a center
+    (x_ctr, y_ctr), output a set of anchors (windows).
+    '''
+    ws = ws[:, np.newaxis]
+    hs = hs[:, np.newaxis]
+    anchors = np.hstack(
+        (
+            x_ctr - 0.5 * (ws - 1),
+            y_ctr - 0.5 * (hs - 1),
+            x_ctr + 0.5 * (ws - 1),
+            y_ctr + 0.5 * (hs - 1)
+        )
+    )
+    return anchors
     
-        title = match1(html, r'<title>([^<]{1,9999})</title>')
     
-        if page is None:
-        page = get_content(url)
-    chap_patt = r'<h3>.+?id='parent_row_(\d+)'.+?onclick='(\w+)\((.+)\)''
-    to_chap_patt = r'this,(\d+),(\d+),(\d)'
-    show_sec_patt = r'this,(\d+),(\d+)'
-    res_patt = r'res_showResDetail\(\'(\d+)\',\'.+?\',\'\d+\',\'mp4\',\'(\d+)\'\)'
-    l = re.findall(chap_patt, page)
-    for i in l:
-        if i[1] == 'ajaxtocharac':
-            hit = re.search(to_chap_patt, i[2])
-            page = to_chap(hit.group(1), hit.group(2), hit.group(3))
-            hit_list = re.findall(res_patt, page)
-            if hit_list:
-                return get_playlist(hit_list[0][0], hit_list[0][1])
-            for hit in hit_list:
-                print(hit)
-        elif i[1] == 'showSectionNode2':
-            hit = re.search(show_sec_patt, i[2])
-            page = show_sec(hit.group(1), hit.group(2))
-            # print(page)
-            patt = r'ajaxtosection\(this,(\d+),(\d+),(\d+)\)'
-            hit_list = re.findall(patt, page)
-            # print(hit_list)
-            for hit in hit_list:
-                page = to_sec(hit[0], hit[1], hit[2])
-                vlist = re.findall(res_patt, page)
-                if vlist:
-                    return get_playlist(vlist[0][0], vlist[0][1])
-    raise Exception('No video found in this playlist')
+def collect(inputs, is_training):
+    cfg_key = 'TRAIN' if is_training else 'TEST'
+    post_nms_topN = cfg[cfg_key].RPN_POST_NMS_TOP_N
+    k_max = cfg.FPN.RPN_MAX_LEVEL
+    k_min = cfg.FPN.RPN_MIN_LEVEL
+    num_lvls = k_max - k_min + 1
+    roi_inputs = inputs[:num_lvls]
+    score_inputs = inputs[num_lvls:]
+    if is_training:
+        score_inputs = score_inputs[:-2]
     
-        def extract(self, **kwargs):
-        if 'stream_id' in kwargs and kwargs['stream_id']:
-            i = kwargs['stream_id']
-            if 'size' not in self.streams[i]:
-                self.streams[i]['size'] = urls_size(self.streams[i]['src'])
+    logger = logging.getLogger(__name__)
+    
+    
+def add_keypoint_rcnn_blobs(
+    blobs, roidb, fg_rois_per_image, fg_inds, im_scale, batch_idx
+):
+    '''Add Mask R-CNN keypoint specific blobs to the given blobs dictionary.'''
+    # Note: gt_inds must match how they're computed in
+    # datasets.json_dataset._merge_proposal_boxes_into_roidb
+    gt_inds = np.where(roidb['gt_classes'] > 0)[0]
+    max_overlaps = roidb['max_overlaps']
+    gt_keypoints = roidb['gt_keypoints']
+    
+    from detectron.core.config import assert_and_infer_cfg
+from detectron.core.config import cfg
+from detectron.core.config import merge_cfg_from_file
+from detectron.core.config import merge_cfg_from_list
+from detectron.datasets.roidb import combined_roidb_for_training
+from detectron.roi_data.loader import RoIDataLoader
+from detectron.utils.logging import setup_logging
+from detectron.utils.timer import Timer
