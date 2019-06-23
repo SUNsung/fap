08c45db633bbb80af4fa49cd8cf4d3b79a8dee6d
SUNsung <weatherius@gmail.com>
2019-06-23 03:22:12 +0300

    Rewrite [27iIMGyMvuuQ627w743Y.ZXxg27QghUVbLsN227KI.py] File in: 2019-06-23 03:22:11

--- pyton/27iIMGyMvuuQ627w743Y.ZXxg27QghUVbLsN227KI.py
+++ pyton/27iIMGyMvuuQ627w743Y.ZXxg27QghUVbLsN227KI.py
@@ -1,54 +1,110 @@
 
         
-            with tf.gfile.Open(filename) as f:
-      idx = 0
-      for line in f:
-        word_name = line.strip()
-        if word_name == '<S>':
-          self._bos = idx
-        elif word_name == '</S>':
-          self._eos = idx
-        elif word_name == '<UNK>':
-          self._unk = idx
-        if word_name == '!!!MAXTERMID':
-          continue
+                (category1, 1), product4
+        (category1, 2), product1
+        (category1, 3), product2
+        (category2, 3), product1
+        (category2, 7), product3
+        '''
+        category, product_id = key
+        quantity = value
+        yield (category, quantity), product_id
     
-      Args:
-    logits:  tf.float32 Tensor of the logits of shape [batch_size,
-      sequence_length, vocab_size].
-    targets:  tf.int32 Tensor of the sequence target of shape [batch_size,
-      sequence_length].
-    present:  tf.bool Tensor indicating the presence or absence of the token
-      of shape [batch_size, sequence_length].
+        def bfs(self, source, dest):
+        if source is None:
+            return False
+        queue = deque()
+        queue.append(source)
+        source.visit_state = State.visited
+        while queue:
+            node = queue.popleft()
+            print(node)
+            if dest is node:
+                return True
+            for adjacent_node in node.adj_nodes.values():
+                if adjacent_node.visit_state == State.unvisited:
+                    queue.append(adjacent_node)
+                    adjacent_node.visit_state = State.visited
+        return False
     
-        def delete(self, session_key=None):
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        self._cache.delete(self.cache_key_prefix + session_key)
+        try:
+        citext_oids = get_citext_oids(connection.alias)
+        array_type = psycopg2.extensions.new_array_type(citext_oids, 'citext[]', psycopg2.STRING)
+        psycopg2.extensions.register_type(array_type, None)
+    except ProgrammingError:
+        # citext is not available on the database.
+        #
+        # The same comments in the except block of the above call to
+        # register_hstore() also apply here.
+        pass
+
     
-        @classmethod
-    def get_model_class(cls):
-        # Avoids a circular import and allows importing SessionStore when
-        # django.contrib.sessions is not in INSTALLED_APPS.
-        from django.contrib.sessions.models import Session
-        return Session
+        def create(self):
+        while True:
+            self._session_key = self._get_new_session_key()
+            try:
+                # Save immediately to ensure we have a unique entry in the
+                # database.
+                self.save(must_create=True)
+            except CreateError:
+                # Key wasn't unique. Try again.
+                continue
+            self.modified = True
+            return
     
-      for group in itervalues( group_name_to_group ):
-    parent_names = GetParentNames( group )
     
-        # For pickling to work, the __module__ variable needs to be set to the frame
-    # where the named tuple is created.  Bypass this step in enviroments where
-    # sys._getframe is not defined (Jython for example).
-    if hasattr(_sys, '_getframe'):
-        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+@html_safe
+class BoundWidget:
+    '''
+    A container class used for iterating over widgets. This is useful for
+    widgets that have choices. For example, the following can be used in a
+    template:
     
-    def _python_exit():
-    global _shutdown
-    _shutdown = True
-    items = list(_threads_queues.items())
-    for t, q in items:
-        q.put(None)
-    for t, q in items:
-        t.join()
+    from django.conf import settings
+from django.template.backends.django import DjangoTemplates
+from django.template.loader import get_template
+from django.utils.functional import cached_property
+from django.utils.module_loading import import_string
+    
+    # The name for this set of Sphinx documents.  If None, it defaults to
+# '<project> v<release> documentation'.
+#html_title = None
+    
+    
+@click.command()
+@click.argument('known_people_folder')
+@click.argument('image_to_check')
+@click.option('--cpus', default=1, help='number of CPU cores to use in parallel (can speed up processing lots of images). -1 means 'use all in system'')
+@click.option('--tolerance', default=0.6, help='Tolerance for face comparisons. Default is 0.6. Lower this if you get multiple matches for the same person.')
+@click.option('--show-distance', default=False, type=bool, help='Output face distance. Useful for tweaking tolerance setting.')
+def main(known_people_folder, image_to_check, cpus, tolerance, show_distance):
+    known_names, known_face_encodings = scan_known_people(known_people_folder)
+    
+    requirements = [
+    'face_recognition_models>=0.3.0',
+    'Click>=6.0',
+    'dlib>=19.7',
+    'numpy',
+    'Pillow'
+]
+    
+        # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+        # Or instead, use the known face with the smallest distance to the new face
+    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+    best_match_index = np.argmin(face_distances)
+    if matches[best_match_index]:
+        name = known_face_names[best_match_index]
+    
+        assert len(kernel_size) == 2
+    assert len(strides) == 4
+    
+        References:
+        K.repeat()
+        tf.tile()
+    '''
+    assert x.get_shape().ndims == 2
+    x = tf.expand_dims(x, axis=1)  # -> [batch_size, 1, n_input]
+    return tf.tile(x, [1, n, 1])  # -> [batch_size, n, n_input]
