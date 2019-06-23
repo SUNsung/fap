d03ec3e7be271e448c932c9a5b94905a22ee9bff
SUNsung <weatherius@gmail.com>
2019-06-23 05:40:33 +0300

    Rewrite [24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb] File in: 2019-06-23 05:40:33

--- ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
+++ ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
@@ -1,108 +1,62 @@
 
         
-                def sidekiq_worker_class
-          ImportDiffNoteWorker
-        end
-    
-            def importer_class
-          LfsObjectImporter
-        end
-    
-            # Builds an issue from a GitHub API response.
-        #
-        # issue - An instance of `Sawyer::Resource` containing the issue
-        #         details.
-        def self.from_api_response(issue)
-          user =
-            if issue.user
-              Representation::User.from_api_response(issue.user)
-            end
-    
-          private
-    
-      def update
-    if verify_payload?
-      process_salmon
-      head 202
-    elsif payload.present?
-      render plain: signature_verification_failure_reason, status: 401
-    else
-      head 400
-    end
-  end
-    
-      def update
-    setting.data = params[:data]
-    setting.save!
-    
-      def api_throttle_data
-    most_limited_type, = request.env['rack.attack.throttle_data'].min_by { |_, v| v[:limit] }
-    request.env['rack.attack.throttle_data'][most_limited_type]
-  end
-    
-      def lock_options
-    { redis: Redis.current, key: 'media_download:#{params[:id]}' }
-  end
-    
-    task :gemspec => 'rack-protection.gemspec'
-task :default => :spec
-task :test    => :spec
-
-    
-          def remove_bad_cookies(request, response)
-        return if bad_cookies.empty?
-        paths = cookie_paths(request.path)
-        bad_cookies.each do |name|
-          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
-        end
+            def later(desc = nil, db = RailsMultisite::ConnectionManagement.current_db, &blk)
+      if @async
+        start_thread if !@thread&.alive? && !@paused
+        @queue << [db, blk, desc]
+      else
+        blk.call
       end
+    end
     
-      it 'prevents ajax requests without a valid token' do
-    expect(post('/', {}, 'HTTP_X_REQUESTED_WITH' => 'XMLHttpRequest')).not_to be_ok
+      def report
+    @report ||= JSON.parse(request.body.read)['csp-report'].slice(
+      'blocked-uri',
+      'disposition',
+      'document-uri',
+      'effective-directive',
+      'original-policy',
+      'referrer',
+      'script-sample',
+      'status-code',
+      'violated-directive',
+      'line-number',
+      'source-file'
+    )
   end
     
-        it 'Returns nil when Referer header is invalid' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
-      expect(subject.referrer(env)).to be_nil
-    end
-  end
-end
-
+    describe ContentSecurityPolicy::Builder do
+  let(:builder) { described_class.new }
     
-          expected_header = <<-END.chomp
-rack.%2573ession=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-END
-      expect(last_response.headers['Set-Cookie']).to eq(expected_header)
-    end
-  end
+    class ContentSecurityPolicy
+  class Default
+    attr_reader :directives
     
-          PluginManager.ui.info('Install successful')
-    rescue ::Bundler::BundlerError => e
-      raise PluginManager::InstallError.new(e), 'An error occurred went installing plugins'
-    ensure
-      FileUtils.rm_rf(uncompressed_path) if uncompressed_path && Dir.exist?(uncompressed_path)
+      def self.bundle(langs)
+    result = File.read(HIGHLIGHTJS_DIR + 'highlight.min.js')
+    langs.each do |lang|
+      begin
+        result << '\n' << File.read(HIGHLIGHTJS_DIR + 'languages/#{lang}.min.js')
+      rescue Errno::ENOENT
+        # no file, don't care
+      end
     end
     
-      # retrieve only the latest spec for all locally installed plugins
-  # @return [Hash] result hash {plugin_name.downcase => plugin_spec}
-  def find_latest_gem_specs
-    LogStash::PluginManager.all_installed_plugins_gem_specs(gemfile).inject({}) do |result, spec|
-      previous = result[spec.name.downcase]
-      result[spec.name.downcase] = previous ? [previous, spec].max_by{|s| s.version} : spec
-      result
-    end
+        builder.build
   end
 end
-
     
-        before do
-      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
-      # Logstash won't update when we have a pinned version in the gemfile so we remove them
-      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
-      expect(logstash).to have_installed?(plugin_name, previous_version)
+        def clear_theme_extensions_cache!
+      cache.clear
     end
+    
+          keyword = params.require(:keyword)
+      data = SvgSprite.search(keyword)
+    
+            def collection_method
+          :issues_comments
+        end
+    
+      def index
+    @broadcasts = Broadcast.all
+  end
