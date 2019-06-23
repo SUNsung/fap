42d36b8c4d9baf04e0fe959d85325d25df9b2e62
SUNsung <weatherius@gmail.com>
2019-06-23 03:43:57 +0300

    Rewrite [36k8wUQo12Ih.36Eev1ZDXFexs36zgHR6KbaF7Y36XJb.py] File in: 2019-06-23 03:43:56

--- pyton/36k8wUQo12Ih.36Eev1ZDXFexs36zgHR6KbaF7Y36XJb.py
+++ pyton/36k8wUQo12Ih.36Eev1ZDXFexs36zgHR6KbaF7Y36XJb.py
@@ -1,198 +1,276 @@
 
         
-        # TODO: ensure that history changes.
+            def score(self):
+        min_over = sys.MAXSIZE
+        max_under = -sys.MAXSIZE
+        for score in self.possible_scores():
+            if self.BLACKJACK < score < min_over:
+                min_over = score
+            elif max_under < score <= self.BLACKJACK:
+                max_under = score
+        return max_under if max_under != -sys.MAXSIZE else min_over
+    
+            (2016-01, shopping), 125
+        (2016-01, gas), 50
+        '''
+        total = sum(values)
+        self.handle_budget_notifications(key, total)
+        yield key, sum(values)
+    
+        def reducer(self, key, values):
+        '''Sum values for each key.
+    
+        for prime in lowPrimes:
+        if (num % prime) == 0:
+            return False
+    
+            while self.values[new_key] is not None and self.values[new_key] != key:
+            new_key = self.__hash_double_function(key, data, i) if \
+                self.balanced_factor() >= self.lim_charge else None
+            if new_key is None: break 
+            else: i += 1
+    
+    try:
+	raw_input		#Python 2
+except NameError:
+	raw_input = input	#Python 3
+    
+    This is a pure Python implementation of Dynamic Programming solution to the longest_sub_array problem.
+    
+        for i in range(1, n+1):
+        for j in range(1, s+1):
+            dp[i][j]= dp[i][j-1]
+    
+    # Mock test below
+if False: # change to true to run this test case.
+    import sklearn.datasets as ds
+    dataset = ds.load_iris()
+    k = 3
+    heterogeneity = []
+    initial_centroids = get_initial_centroids(dataset['data'], k, seed=0)
+    centroids, cluster_assignment = kmeans(dataset['data'], k, initial_centroids, maxiter=400,
+                                        record_heterogeneity=heterogeneity, verbose=True)
+    plot_heterogeneity(heterogeneity, k)
 
     
     
-init_zshrc = u'''echo '
-export SHELL=/usr/bin/zsh
-export HISTFILE=~/.zsh_history
-echo > $HISTFILE
-export SAVEHIST=100
-export HISTSIZE=100
-eval $(thefuck --alias {})
-setopt INC_APPEND_HISTORY
-echo 'instant mode ready: $THEFUCK_INSTANT_MODE'
-' > ~/.zshrc'''
+def b_expo(a, b):
+    res = 1
+    while b > 0:
+        if b&1:
+            res *= a
+    
+        try:
+        citext_oids = get_citext_oids(connection.alias)
+        array_type = psycopg2.extensions.new_array_type(citext_oids, 'citext[]', psycopg2.STRING)
+        psycopg2.extensions.register_type(array_type, None)
+    except ProgrammingError:
+        # citext is not available on the database.
+        #
+        # The same comments in the except block of the above call to
+        # register_hstore() also apply here.
+        pass
+
     
     
-@pytest.mark.parametrize('command, new_command, packages', [
-    (Command('vim', ''), 'sudo apt-get install vim && vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('convert', ''), 'sudo apt-get install imagemagick && convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')]),
-    (Command('sudo vim', ''), 'sudo apt-get install vim && sudo vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('sudo convert', ''), 'sudo apt-get install imagemagick && sudo convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')])])
-def test_get_new_command(mocker, command, new_command, packages):
-    mocker.patch('thefuck.rules.apt_get._get_packages',
-                 create=True, return_value=packages)
+class BaseSessionManager(models.Manager):
+    def encode(self, session_dict):
+        '''
+        Return the given session dictionary serialized and encoded as a string.
+        '''
+        session_store_class = self.model.get_session_store_class()
+        return session_store_class().encode(session_dict)
     
-    match_output = '''
-Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
-Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
-Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
-Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
-Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
-Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
-Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
-Fetched 89.2 kB in 0s (122 kB/s)
-Reading package lists... Done
-Building dependency tree
-Reading state information... Done
-8 packages can be upgraded. Run 'apt list --upgradable' to see them.
-'''
+        if section is not None:
+        if section not in sitemaps:
+            raise Http404('No sitemap available for section: %r' % section)
+        maps = [sitemaps[section]]
+    else:
+        maps = sitemaps.values()
+    page = request.GET.get('p', 1)
     
-            next_prime_gt = next_prime(value % self.size_table) \
-            if not check_prime(value % self.size_table) else value % self.size_table  #gt = bigger than
-        return next_prime_gt - (data % next_prime_gt)
-    
-            while self.values[new_key] is not None \
-                and self.values[new_key] != key:
-            i += 1
-            new_key = self.hash_function(key + i*i) if not \
-                self.balanced_factor() >= self.lim_charge else None
-    
-    	for n in xrange(m+1):
-		for k in xrange(1, m):
-			memo[n][k] += memo[n][k-1]
-			if n-k > 0:
-				memo[n][k] += memo[n-k-1][k]
-    
-    This is a pure Python implementation of Dynamic Programming solution to the longest increasing subsequence of a given sequence.
-    
-        dp = [[False for x in range(s+1)]for y in range(n+1)]
-    
-            a += a
-        b >>= 1
-    
-        it = 0
-    
-    input data sparsity: 0.050000
-true coef sparsity: 0.000100
-test data sparsity: 0.027400
-model sparsity: 0.000024
-r^2 on test data (dense model) : 0.233651
-r^2 on test data (sparse model) : 0.233651
-Wrote profile results to sparsity_benchmark.py.lprof
-Timer unit: 1e-06 s
-    
-        # TASK: Build a grid search to find out whether unigrams or bigrams are
-    # more useful.
-    # Fit the pipeline on the training set using grid search for the parameters
-    parameters = {
-        'vect__ngram_range': [(1, 1), (1, 2)],
-    }
-    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
-    grid_search.fit(docs_train, y_train)
-    
-    # Now predict the value of the digit on the second half:
-expected = digits.target[n_samples // 2:]
-predicted = classifier.predict(data[n_samples // 2:])
-    
-    # Code source: Gaël Varoquaux
-# Modified for documentation by Jaques Grobler
-# License: BSD 3 clause
+        def css_classes(self, extra_classes=None):
+        '''
+        Return a string of space-separated CSS classes for this field.
+        '''
+        if hasattr(extra_classes, 'split'):
+            extra_classes = extra_classes.split()
+        extra_classes = set(extra_classes or [])
+        if self.errors and hasattr(self.form, 'error_css_class'):
+            extra_classes.add(self.form.error_css_class)
+        if self.field.required and hasattr(self.form, 'required_css_class'):
+            extra_classes.add(self.form.required_css_class)
+        return ' '.join(extra_classes)
     
     
-def make_data(random_state, n_samples_per_center, grid_size, scale):
-    random_state = check_random_state(random_state)
-    centers = np.array([[i, j]
-                        for i in range(grid_size)
-                        for j in range(grid_size)])
-    n_clusters_true, n_features = centers.shape
+class DjangoTemplates(EngineMixin, BaseRenderer):
+    '''
+    Load Django templates from the built-in widget templates in
+    django/forms/templates and from apps' 'templates' directory.
+    '''
+    backend = DjangoTemplates
     
-        for name, algorithm in clustering_algorithms:
-        t0 = time.time()
+        def iter_lines(self, chunk_size):
+        '''Return an iterator over the body yielding (`line`, `line_feed`).'''
+        raise NotImplementedError()
     
-        def setUp(self):
-        from acme.errors import PollError
-        self.timeout = PollError(
-            exhausted=set([mock.sentinel.AR]),
-            updated={})
-        self.invalid = PollError(exhausted=set(), updated={
-            mock.sentinel.AR: mock.sentinel.AR2})
-    
-        def test_default_decoder(self):
-        from acme.fields import RFC3339Field
-        self.assertEqual(
-            self.decoded, RFC3339Field.default_decoder(self.encoded))
-    
-    The JWS implementation in josepy only implements the base JOSE standard. In
-order to support the new header fields defined in ACME, this module defines some
-ACME-specific classes that layer on top of josepy.
-'''
-import josepy as jose
+        config['implicit_content_type'] = 'form'
+    config.save()
+    config.load()
+    assert 'implicit_content_type' not in config
+    assert config['default_options'] == ['--form']
     
     
-KEY = jose.JWKRSA.load(test_util.load_vector('rsa512_key.pem'))
-    
-            # Placeholder for augeas
-        self.aug = None
-    
-        @mock.patch('certbot_apache.display_ops.display_util')
-    @certbot_util.patch_get_utility()
-    @mock.patch('certbot_apache.display_ops.logger')
-    def test_small_display(self, mock_logger, mock_util, mock_display_util):
-        mock_display_util.WIDTH = 20
-        mock_util().menu.return_value = (display_util.OK, 0)
-        self._call(self.vhosts)
-    
-            self.assertTrue(self.vhost1.same_server(self.vhost2))
-        self.assertTrue(no_name1.same_server(no_name2))
-    
-    .. caution::
-   You should protect these API credentials as you would the password to your
-   CloudXNS account. Users who can read this file can use these credentials to
-   issue arbitrary API calls on your behalf. Users who can cause Certbot to run
-   using these credentials can complete a ``dns-01`` challenge to acquire new
-   certificates or revoke existing certificates for associated domains, even if
-   those domains aren't being managed by this server.
+def has_docutils():
+    try:
+        # noinspection PyUnresolvedReferences
+        import docutils
+        return True
+    except ImportError:
+        return False
     
     
+def test_unicode_json_item(httpbin):
+    r = http('--json', 'POST', httpbin.url + '/post', u'test=%s' % UNICODE)
+    assert HTTP_OK in r
+    assert r.json['json'] == {'test': UNICODE}
     
-            if not self.in_field(col, row):
-            return
-        if isinstance(char, Point):
-            self.field[row][col] = char
-        elif char is None:
-            if background:
-                self.field[row][col].background = background
-            if color:
-                self.field[row][col].foreground = color
+        @property
+    def path(self):
+        '''Return the config file path creating basedir, if needed.'''
+        path = self._get_path()
+        try:
+            os.makedirs(os.path.dirname(path), mode=0o700)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+        return path
+    
+            # builtin function:
+        self.assertEqual(len.__qualname__, 'len')
+        self.assertEqual(time.time.__qualname__, 'time')
+    
+            # Reference: http://hoohoo.ncsa.uiuc.edu/cgi/env.html
+        # XXX Much of the following could be prepared ahead of time!
+        env = copy.deepcopy(os.environ)
+        env['SERVER_SOFTWARE'] = self.version_string()
+        env['SERVER_NAME'] = self.server.server_name
+        env['GATEWAY_INTERFACE'] = 'CGI/1.1'
+        env['SERVER_PROTOCOL'] = self.protocol_version
+        env['SERVER_PORT'] = str(self.server.server_port)
+        env['REQUEST_METHOD'] = self.command
+        uqrest = urllib.parse.unquote(rest)
+        env['PATH_INFO'] = uqrest
+        env['PATH_TRANSLATED'] = self.translate_path(uqrest)
+        env['SCRIPT_NAME'] = scriptname
+        if query:
+            env['QUERY_STRING'] = query
+        env['REMOTE_ADDR'] = self.client_address[0]
+        authorization = self.headers.get('authorization')
+        if authorization:
+            authorization = authorization.split()
+            if len(authorization) == 2:
+                import base64, binascii
+                env['AUTH_TYPE'] = authorization[0]
+                if authorization[0].lower() == 'basic':
+                    try:
+                        authorization = authorization[1].encode('ascii')
+                        authorization = base64.decodebytes(authorization).\
+                                        decode('ascii')
+                    except (binascii.Error, UnicodeError):
+                        pass
+                    else:
+                        authorization = authorization.split(':')
+                        if len(authorization) == 2:
+                            env['REMOTE_USER'] = authorization[0]
+        # XXX REMOTE_IDENT
+        if self.headers.get('content-type') is None:
+            env['CONTENT_TYPE'] = self.headers.get_content_type()
         else:
-            self.field[row][col] = Point(char=char, foreground=color, background=background)
-    
-        def _split_paragraphs(text):
-        answer = []
-        paragraph = ''
-        for line in text.splitlines():
-            if line == '':
-                answer.append(paragraph)
-                paragraph = ''
+            env['CONTENT_TYPE'] = self.headers['content-type']
+        length = self.headers.get('content-length')
+        if length:
+            env['CONTENT_LENGTH'] = length
+        referer = self.headers.get('referer')
+        if referer:
+            env['HTTP_REFERER'] = referer
+        accept = []
+        for line in self.headers.getallmatchingheaders('accept'):
+            if line[:1] in '\t\n\r ':
+                accept.append(line.strip())
             else:
-                paragraph += line+'\n'
-        answer.append(paragraph)
-        return answer
+                accept = accept + line[7:].split(',')
+        env['HTTP_ACCEPT'] = ','.join(accept)
+        ua = self.headers.get('user-agent')
+        if ua:
+            env['HTTP_USER_AGENT'] = ua
+        co = filter(None, self.headers.get_all('cookie', []))
+        cookie_str = ', '.join(co)
+        if cookie_str:
+            env['HTTP_COOKIE'] = cookie_str
+        # XXX Other HTTP_* headers
+        # Since we're setting the env in the parent, provide empty
+        # values to override previously set values
+        for k in ('QUERY_STRING', 'REMOTE_HOST', 'CONTENT_LENGTH',
+                  'HTTP_USER_AGENT', 'HTTP_COOKIE', 'HTTP_REFERER'):
+            env.setdefault(k, '')
     
-            answer = ''.join('%s\n' % x for x in sorted(answer))
-        return answer
+        def test_file_display(self):
+        for handler in (self.dialog.idle_credits,
+                        self.dialog.idle_readme,
+                        self.dialog.idle_news):
+            self.error.message = ''
+            self.view.called = False
+            with self.subTest(handler=handler):
+                handler()
+                self.assertEqual(self.error.message, '')
+                self.assertEqual(self.view.called, True)
     
-        _adapter_name = 'tldr'
-    _output_format = 'code'
-    _cache_needed = True
-    _repository_url = 'https://github.com/tldr-pages/tldr'
-    _cheatsheet_files_prefix = 'pages/*/'
-    _cheatsheet_files_extension = '.md'
+    # Generic implementation of splitext, to be parametrized with
+# the separators
+def _splitext(p, sep, altsep, extsep):
+    '''Split the extension from a pathname.
     
-        color_style = color_style or 'native'
-    lexer_class = languages_data.LEXER['bash']
-    if '/' in topic:
-        section_name = topic.split('/', 1)[0].lower()
-        section_name = languages_data.get_lexer_name(section_name)
-        lexer_class = languages_data.LEXER.get(section_name, lexer_class)
-        if section_name == 'php':
-            answer = '<?\n%s?>\n' % answer
+        def testEOFError(self):
+        bz2d = BZ2Decompressor()
+        text = bz2d.decompress(self.DATA)
+        self.assertRaises(EOFError, bz2d.decompress, b'anything')
+        self.assertRaises(EOFError, bz2d.decompress, b'')
+    
+    # We can extract the richest alternative in order to display it:
+richest = msg.get_body()
+partfiles = {}
+if richest['content-type'].maintype == 'text':
+    if richest['content-type'].subtype == 'plain':
+        for line in richest.get_content().splitlines():
+            print(line)
+        sys.exit()
+    elif richest['content-type'].subtype == 'html':
+        body = richest
+    else:
+        print('Don't know how to display {}'.format(richest.get_content_type()))
+        sys.exit()
+elif richest['content-type'].content_type == 'multipart/related':
+    body = richest.get_body(preferencelist=('html'))
+    for part in richest.iter_attachments():
+        fn = part.get_filename()
+        if fn:
+            extension = os.path.splitext(part.get_filename())[1]
+        else:
+            extension = mimetypes.guess_extension(part.get_content_type())
+        with tempfile.NamedTemporaryFile(suffix=extension, delete=False) as f:
+            f.write(part.get_content())
+            # again strip the <> to go from email form of cid to html form.
+            partfiles[part['content-id'][1:-1]] = f.name
+else:
+    print('Don't know how to display {}'.format(richest.get_content_type()))
+    sys.exit()
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+    # The magic_html_parser has to rewrite the href='cid:....' attributes to
+    # point to the filenames in partfiles.  It also has to do a safety-sanitize
+    # of the html.  It could be written using html.parser.
+    f.write(magic_html_parser(body.get_content(), partfiles))
+webbrowser.open(f.name)
+os.remove(f.name)
+for fn in partfiles.values():
+    os.remove(fn)
