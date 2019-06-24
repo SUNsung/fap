4fb05a619284dbdca3dd45ed7bbf07c3496fd4f4
SUNsung <weatherius@gmail.com>
2019-06-25 00:44:02 +0300

    Rewrite [58t8Qst.rivv258zpISd8bJ7jg58.gYtqC9SAP258fea.rb] File in: 2019-06-25 00:44:02

--- ruby/58t8Qst.rivv258zpISd8bJ7jg58.gYtqC9SAP258fea.rb
+++ ruby/58t8Qst.rivv258zpISd8bJ7jg58.gYtqC9SAP258fea.rb
@@ -1,39 +1,33 @@
 
         
-              # private
-    
-          def has_sidebar
-        @sidebar = (@page.sidebar || false) if @sidebar.nil? && @page
-        !!@sidebar
+              def test_controller_raise_error_about_missing_helper
+        e = assert_raise(AbstractController::Helpers::MissingHelperError) { AbstractInvalidHelpers.helper(:missing) }
+        assert_equal 'Missing helper file helpers/missing_helper.rb', e.message
       end
     
-    # Make sure we're in the test dir, the tests expect that to be the current
-# directory.
-TEST_DIR  = File.join(File.dirname(__FILE__), *%w[.])
+          handle_unknown_error!(e)
+    end
     
-        # TODO: Remove to_url once write_page changes are merged.
-    @wiki.write_page('ééééé'.to_url, :markdown, '한글 text', commit_details)
-    page = @wiki.page('eeeee')
-    assert_equal '한글 text', utf8(page.raw_data)
-  end
+          it 'should not be fooled by 10 local code signing identities available' do
+        allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
+        allow(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return('     10 valid identities found\n')
+        expect(FastlaneCore::UI).not_to(receive(:error))
     
-    desc 'Create a release build and push to rubygems'
-task :release => :build do
-  unless `git branch` =~ /^\* master$/
-    puts 'You must be on the master branch to release!'
-    exit!
-  end
-  sh 'git commit --allow-empty -a -m 'Release #{version}''
-  sh 'git pull --rebase origin master'
-  sh 'git tag v#{version}'
-  sh 'git push origin master'
-  sh 'git push origin v#{version}'
-  sh 'gem push pkg/#{name}-#{version}.gem'
-end
+          def self.return_value
+        # If your method provides a return value, you can describe here what it does
+      end
     
-      s.add_dependency 'gollum-lib', '~> 4.2', '>= 4.2.10'
-  s.add_dependency 'kramdown', '~> 1.9.0'
-  s.add_dependency 'sinatra', '~> 1.4', '>= 1.4.4'
-  s.add_dependency 'mustache', ['>= 0.99.5', '< 1.0.0']
-  s.add_dependency 'useragent', '~> 0.16.2'
-  s.add_dependency 'gemojione', '~> 3.2'
+          if new_email != @user.email
+        @user.update!(
+          unconfirmed_email: new_email,
+          # Regenerate the confirmation token:
+          confirmation_token: nil
+        )
+    
+        private
+    
+      private
+    
+      private
+    
+      UPDATE_SIGN_IN_HOURS = 24
