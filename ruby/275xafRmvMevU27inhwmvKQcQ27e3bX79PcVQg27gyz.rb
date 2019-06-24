d20281eded31673bd2096618f62ecfb9fcb3bd92
SUNsung <weatherius@gmail.com>
2019-06-24 22:55:51 +0300

    Rewrite [275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb] File in: 2019-06-24 22:55:51

--- ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
+++ ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
@@ -1,162 +1,94 @@
 
         
-          context 'as an admin' do
-    before :each do
-      login_as(users(:jane))
-    end
-    
-      it 'requires a URL or file uplaod' do
-    visit new_scenario_imports_path
-    click_on 'Start Import'
-    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
-  end
-    
-        it 'returns a correct icon tag for GitHub' do
-      icon = omniauth_provider_icon(:github)
-      expect(icon).to be_html_safe
-      elem = Nokogiri(icon).at('i.fa.fa-github')
-      expect(elem).to be_a Nokogiri::XML::Element
-    end
-    
-      describe '.seed' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
-    end
-    
-          it 'handles signals' do
-        @agent_runner.instance_variable_set(:@signal_queue, ['TERM'])
-        @agent_runner.run
+            # Example
+    #
+    #     Inspector.def_inspector(key, init_p=nil){|v| v.inspect}
+    #     Inspector.def_inspector([key1,..], init_p=nil){|v| v.inspect}
+    #     Inspector.def_inspector(key, inspector)
+    #     Inspector.def_inspector([key1,...], inspector)
+    def self.def_inspector(key, arg=nil, &block)
+      if block_given?
+        inspector = IRB::Inspector(block, arg)
+      else
+        inspector = arg
       end
-    end
     
-        it 'should work with the human task agent' do
-      valid_params = {
-        'expected_receive_period_in_days' => 2,
-        'trigger_on' => 'event',
-        'hit' =>
-          {
-            'assignments' => 1,
-            'title' => 'Sentiment evaluation',
-            'description' => 'Please rate the sentiment of this message: '<$.message>'',
-            'reward' => 0.05,
-            'lifetime_in_seconds' => 24 * 60 * 60,
-            'questions' =>
-              [
-                {
-                  'type' => 'selection',
-                  'key' => 'sentiment',
-                  'name' => 'Sentiment',
-                  'required' => 'true',
-                  'question' => 'Please select the best sentiment value:',
-                  'selections' =>
-                    [
-                      { 'key' => 'happy', 'text' => 'Happy' },
-                      { 'key' => 'sad', 'text' => 'Sad' },
-                      { 'key' => 'neutral', 'text' => 'Neutral' }
-                    ]
-                },
-                {
-                  'type' => 'free_text',
-                  'key' => 'feedback',
-                  'name' => 'Have any feedback for us?',
-                  'required' => 'false',
-                  'question' => 'Feedback',
-                  'default' => 'Type here...',
-                  'min_length' => '2',
-                  'max_length' => '2000'
-                }
-              ]
-          }
-      }
-      @agent = Agents::HumanTaskAgent.new(:name => 'somename', :options => valid_params)
-      @agent.user = users(:jane)
-      LiquidMigrator.convert_all_agent_options(@agent)
-      expect(@agent.reload.options['hit']['description']).to eq('Please rate the sentiment of this message: '{{message}}'')
-    end
-  end
-end
-    
-        stub_request(:get, /trackings/).to_return(
-      :body => File.read(Rails.root.join('spec/data_fixtures/aftership.json')),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    
-            if mod
-          if name == 'Index'
-            return slug.split('/')[1..-2].join('/')
-          elsif name == 'Angular'
-            return slug.split('/').last.split('-').first
-          end
+            File.open destination, 'w' do |io|
+          io.write metadata
         end
+      else
+        basename = File.basename path, '.gem'
+        target_dir = File.expand_path basename, options[:target]
     
-      def up_down(change)
-    change.up do
-      Mention.update_all(mentions_container_type: 'Post')
-      change_column :mentions, :mentions_container_type, :string, null: false
-      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
+          # Only uses first 72 characters of string
+      ('12345678'*9).crypt('$2a$04$0WVaz0pV3jzfZ5G5tpmHWu').should == '$2a$04$0WVaz0pV3jzfZ5G5tpmHWukj/ORBnsMjCGpST/zCJnAypc7eAbutK'
+      ('12345678'*10).crypt('$2a$04$0WVaz0pV3jzfZ5G5tpmHWu').should == '$2a$04$0WVaz0pV3jzfZ5G5tpmHWukj/ORBnsMjCGpST/zCJnAypc7eAbutK'
+    
+        # `#colorize_code` is supported only for Ruby 2.5+. It just returns the original code in 2.4-.
+    def colorize_code_supported?
+      Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.5.0')
     end
     
-    RSpec::Matchers.define :have_path_in do |expected|
-  match do |actual|
-    await_condition { expected.include? actual.current_path }
+        assert_equal(true, RespondTo::Sub.new.respond_to?(:foo))
   end
     
-    describe ContactsController, :type => :controller do
-  describe '#index' do
-    before do
-      AppConfig.chat.enabled = true
-      @aspect = bob.aspects.create(:name => 'another aspect')
-      bob.share_with alice.person, @aspect
-      bob.share_with eve.person, @aspect
-      sign_in bob, scope: :user
+    if defined?(RbConfig)
+  module RbConfig
+    @ruby = EnvUtil.rubybin
+    class << self
+      undef ruby if method_defined?(:ruby)
+      attr_reader :ruby
     end
-    
-        it 'generates a jasmine fixture', :fixture => true do
-      get :bookmarklet
-      save_fixture(html_for('body'), 'bookmarklet')
-    end
-    
-        it 'marks a notification as unread if it is told to' do
-      note = FactoryGirl.create(:notification)
-      expect(Notification).to receive(:where).and_return([note])
-      expect(note).to receive(:set_read_state).with(false)
-      get :update, params: {id: note.id, set_unread: 'true'}, format: :json
-    end
-    
-          # Returns the keyword of the `case` statement as a string.
-      #
-      # @return [String] the keyword of the `case` statement
-      def keyword
-        'case'
-      end
-    
-          # The body of the method definition.
-      #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
-      end
-    
-            private
-    
-            def correct(processed_source, node,
-                    previous_declaration, comments_as_separators)
-          @processed_source = processed_source
-          @comments_as_separators = comments_as_separators
-    
-            private
-    
-      puts '\n== Restarting application server =='
-  system! 'bin/rails restart'
+    dir = File.dirname(ruby)
+    CONFIG['bindir'] = dir
+    Gem::ConfigMap[:bindir] = dir if defined?(Gem::ConfigMap)
+  end
 end
-
     
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+        mkcdtmpdir do
+      assert_not_nil(f = File.open('symbolic', 'w'))
+      f.close
+      assert_not_nil(f = File.open('numeric',  File::WRONLY|File::TRUNC|File::CREAT))
+      f.close
+      assert_not_nil(f = File.open('hash-symbolic', :mode => 'w'))
+      f.close
+      assert_not_nil(f = File.open('hash-numeric', :mode => File::WRONLY|File::TRUNC|File::CREAT), feature4742)
+      f.close
+      assert_nothing_raised(bug6055) {f = File.open('hash-symbolic', binmode: true)}
+      f.close
+    end
+  end
+    
+      def test_font_helper_without_suffix
+    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
+  end
+    
+          def page_name
+        @name.gsub('-', ' ')
+      end
+    
+          def previous_link
+      end
+    
+          def has_path
+        !@path.nil?
+      end
+    
+          def editable
+        @editable
+      end
+    
+      def app
+    Precious::App
+  end
+end
+    
+      # puts '\n== Copying sample files =='
+  # unless File.exist?('config/database.yml')
+  #   cp 'config/database.yml.sample', 'config/database.yml'
+  # end
+    
+      # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
