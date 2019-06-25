ee0557b8c52e92870f964786c4faac6946318687
SUNsung <weatherius@gmail.com>
2019-06-25 21:22:10 +0300

    Rewrite [37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb] File in: 2019-06-25 21:22:10

--- ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
+++ ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
@@ -1,108 +1,13 @@
 
         
-              def self.details
-        list = <<-LIST.markdown_list
-          `grouping` is just to keep your tags organised under one 'folder', defaults to 'builds'
-          `lane` is the name of the current fastlane lane
-          `prefix` is anything you want to stick in front of the version number, e.g. 'v'
-          `postfix` is anything you want to stick at the end of the version number, e.g. '-RC1'
-          `build_number` is the build number, which defaults to the value emitted by the `increment_build_number` action
-        LIST
+              delegate_to Type
     
-            expect(result).to include(' oclint -report-type=pmd -o=report_path.xml ')
-        expect(result).to include(' -max-priority-1=10 ')
-        expect(result).to include(' -max-priority-2=20 ')
-        expect(result).to include(' -max-priority-3=30 ')
-        expect(result).to include(' -rc=LONG_LINE=200 -rc=LONG_METHOD=200 ')
-        expect(result).to include(' -rule DoubleNegative -rule DeadCode ')
-        expect(result).to include(' -disable-rule GotoStatement -disable-rule ShortVariableName ')
-        expect(result).to include(' -list-enabled-rules ')
-        expect(result).to include(' -enable-clang-static-analyzer ')
-        expect(result).to include(' -enable-global-analysis ')
-        expect(result).to include(' -allow-duplicated-violations ')
-      end
+        def execute
+      raise PluginManager::FileNotFoundError, 'Can't file local file #{local_file}' unless ::File.exist?(local_file)
+      raise PluginManager::InvalidPackError, 'Invalid format, the pack must be in zip format' unless valid_format?(local_file)
     
-        it 'recognizes an array as the first element of a command' do
-      message = 'A message'
-      command = command_from_args(['/usr/local/bin/git', 'git'], 'commit', '-m', message)
-      expect(command).to eq('/usr/local/bin/git commit -m #{message.shellescape}')
+    shared_examples 'logstash update' do |logstash|
+  describe 'logstash-plugin update on #{logstash.hostname}' do
+    before :each do
+      logstash.install({:version => LOGSTASH_VERSION})
     end
-  end
-end
-    
-        # wrap in double quotes if contains space
-    if str =~ /\s/
-      # double quotes have to be doubled if will be quoted
-      str.gsub!(''', '''')
-      return ''' + str + '''
-    else
-      return str
-    end
-  end
-  module_function :shellescape
-end
-
-    
-    describe 'monkey patch of Array.shelljoin (via CrossplatformShellwords)' do
-  describe 'on Windows' do
-    before(:each) do
-      allow(FastlaneCore::Helper).to receive(:windows?).and_return(true)
-    end
-    
-          it 'keeps the specified screenshots folder' do
-        expect(options[:screenshots_path]).to eq('./screenshots')
-      end
-    
-      get '/' => 'test#index'
-end
-    
-            if options[:bypass]
-          ActiveSupport::Deprecation.warn(<<-DEPRECATION.strip_heredoc, caller)
-          [Devise] bypass option is deprecated and it will be removed in future version of Devise.
-          Please use bypass_sign_in method instead.
-          Example:
-    
-    module Devise
-  module Controllers
-    # Provide the ability to store a location.
-    # Used to redirect back to a desired path after sign in.
-    # Included by default in all controllers.
-    module StoreLocation
-      # Returns and delete (if it's navigational format) the url stored in the session for
-      # the given scope. Useful for giving redirect backs after sign up:
-      #
-      # Example:
-      #
-      #   redirect_to stored_location_for(:user) || root_path
-      #
-      def stored_location_for(resource_or_scope)
-        session_key = stored_location_key_for(resource_or_scope)
-    
-          private
-    
-          def remember_me!
-        self.remember_token ||= self.class.remember_token if respond_to?(:remember_token)
-        self.remember_created_at ||= Time.now.utc
-        save(validate: false) if self.changed?
-      end
-    
-          redirect_to admin_account_path(@account.id), notice: I18n.t('admin.accounts.change_email.changed_msg')
-    end
-    
-      def hub_lease_seconds
-    params['hub.lease_seconds']
-  end
-    
-    class Api::SalmonController < Api::BaseController
-  include SignatureVerification
-    
-      def update
-    if subscription.verify(body, request.headers['HTTP_X_HUB_SIGNATURE'])
-      ProcessingWorker.perform_async(@account.id, body.force_encoding('UTF-8'))
-    end
-    
-      def update
-    setting.data = params[:data]
-    setting.save!
-    
-      private
