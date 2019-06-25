02582269246f680d96c06a5af15c8448ba4cdd26
SUNsung <weatherius@gmail.com>
2019-06-25 20:07:16 +0300

    Rewrite [07eHDabRh9l7I07XjtYcmgd2mo07XcIuR1rKf0M07gHx.py] File in: 2019-06-25 20:07:16

--- pyton/07eHDabRh9l7I07XjtYcmgd2mo07XcIuR1rKf0M07gHx.py
+++ pyton/07eHDabRh9l7I07XjtYcmgd2mo07XcIuR1rKf0M07gHx.py
@@ -1,116 +1,113 @@
 
         
-            def __init__(self, debuglevel=0):
-        self._init_github_account()
-        https_handler = make_HTTPS_handler({}, debuglevel=debuglevel)
-        self._opener = compat_urllib_request.build_opener(https_handler)
-    
-    entry_template = textwrap.dedent('''
-    <entry>
-        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
-        <title>New version @VERSION@</title>
-        <link href='http://rg3.github.io/youtube-dl' />
-        <content type='xhtml'>
-            <div xmlns='http://www.w3.org/1999/xhtml'>
-                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
-            </div>
-        </content>
-        <author>
-            <name>The youtube-dl maintainers</name>
-        </author>
-        <updated>@TIMESTAMP@</updated>
-    </entry>
-    ''')
-    
-    options = helptext[helptext.index('  General Options:') + 19:]
-options = re.sub(r'(?m)^  (\w.+)$', r'## \1', options)
-options = '# OPTIONS\n' + options + '\n'
-    
-    from __future__ import unicode_literals
+        import codecs
+import subprocess
     
     
-class RangeMaxValueValidator(MaxValueValidator):
-    def compare(self, a, b):
-        return a.upper is None or a.upper > b
-    message = _('Ensure that this range is completely less than or equal to %(limit_value)s.')
+def _is_empty(d):
+    return not bool(os.listdir(d))
     
-            r = None
-        try:
-            r = Redirect.objects.get(site=current_site, old_path=full_path)
-        except Redirect.DoesNotExist:
-            pass
-        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
-            try:
-                r = Redirect.objects.get(
-                    site=current_site,
-                    old_path=request.get_full_path(force_append_slash=True),
-                )
-            except Redirect.DoesNotExist:
-                pass
-        if r is not None:
-            if r.new_path == '':
-                return self.response_gone_class()
-            return self.response_redirect_class(r.new_path)
+        def test_module_exec(self):
+        if sys.version_info >= (2, 7):  # Python 2.6 doesn't support package execution
+            subprocess.check_call([sys.executable, '-m', 'youtube_dl', '--version'], cwd=rootDir, stdout=_DEV_NULL)
     
-        def delete(self, session_key=None):
-        super().delete(session_key)
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        self._cache.delete(self.cache_key_prefix + session_key)
+        def debug(self, msg):
+        pass
     
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.heroku import HerokuHelper
+        def test_comments(self):
+        'Skipping: Not yet fully implemented'
+        return
+        jsi = JSInterpreter('''
+        function x() {
+            var x = /* 1 + */ 2;
+            var y = /* 30
+            * 40 */ 50;
+            return x + y;
+        }
+        ''')
+        self.assertEqual(jsi.call_function('x'), 52)
     
-    try:
-    import oneandone.client
-except ImportError:
-    HAS_ONEANDONE_SDK = False
+        def test_secondary_proxy_https(self):
+        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
+        if params is None:
+            return
+        ydl = FakeYDL()
+        req = compat_urllib_request.Request('https://yt-dl.org/ip')
+        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
+        self.assertEqual(
+            ydl.urlopen(req).read().decode('utf-8'),
+            params['secondary_server_ip'])
     
-            return (changed, monitoring_policy)
-    except Exception as ex:
-        module.fail_json(msg=str(ex))
+        def test_allsubtitles(self):
+        self.DL.params['writesubtitles'] = True
+        self.DL.params['allsubtitles'] = True
+        subtitles = self.getSubtitles()
+        self.assertEqual(set(subtitles.keys()), set(['en']))
+        self.assertEqual(md5(subtitles['en']), '78206b8d8a0cfa9da64dc026eea48961')
     
-    try:
-    from pyone import HOST_STATES, HOST_STATUS
-except ImportError:
-    pass  # handled at module utils
+    current_path = os.path.dirname(os.path.abspath(__file__))
+root_path = os.path.abspath( os.path.join(current_path, os.pardir, os.pardir))
+data_path = os.path.abspath(os.path.join(root_path, os.pardir, os.pardir, 'data'))
+module_data_path = os.path.join(data_path, 'gae_proxy')
+python_path = os.path.abspath( os.path.join(root_path, 'python27', '1.0'))
     
+        global pteredor_is_running, usable
+    pteredor_is_running = probe_nat
+    prober = teredo_prober(probe_nat=probe_nat)
     
-class FunctionDictsTest(FuncAttrsTest):
-    def test_setting_dict_to_invalid(self):
-        self.cannot_set_attr(self.b, '__dict__', None, TypeError)
-        from collections import UserDict
-        d = UserDict({'known_attr': 7})
-        self.cannot_set_attr(self.fi.a.__func__, '__dict__', d, TypeError)
+    - RecognitionException
+  - MismatchedRangeException
+  - MismatchedSetException
+    - MismatchedNotSetException
+    .
+  - MismatchedTokenException
+  - MismatchedTreeNodeException
+  - NoViableAltException
+  - EarlyExitException
+  - FailedPredicateException
+  .
+.
     
-            Paths are sorted before being imported to ensure reproducible execution
-        order even on filesystems with non-alphabetical ordering like ext3/4.
-        '''
-        set_implicit_top = False
-        if top_level_dir is None and self._top_level_dir is not None:
-            # make top_level_dir optional if called from load_tests in a package
-            top_level_dir = self._top_level_dir
-        elif top_level_dir is None:
-            set_implicit_top = True
-            top_level_dir = start_dir
+                    else:
+                    self.c = self.input.LA(1)
     
-    # Same, but for 3.x to 2.x
-REVERSE_IMPORT_MAPPING = dict((v, k) for (k, v) in IMPORT_MAPPING.items())
-assert len(REVERSE_IMPORT_MAPPING) == len(IMPORT_MAPPING)
-REVERSE_NAME_MAPPING = dict((v, k) for (k, v) in NAME_MAPPING.items())
-assert len(REVERSE_NAME_MAPPING) == len(NAME_MAPPING)
+    PROJ_METADATA = '%s.json' % PROJ_NAME
     
-    def handleSlideshow(slideshow):
-    print('<html>')
-    handleSlideshowTitle(slideshow.getElementsByTagName('title')[0])
-    slides = slideshow.getElementsByTagName('slide')
-    handleToc(slides)
-    handleSlides(slides)
-    print('</html>')
+                elif opt in ('-V', '--version'):
+                # Display version.
+                log.println('you-get:', log.BOLD)
+                log.println('    version:  {}'.format(__version__))
+                if head is not None:
+                    log.println('    branch:   {}\n    commit:   {}'.format(*head))
+                else:
+                    log.println('    branch:   {}\n    commit:   {}'.format('(stable)', '(tag v{})'.format(__version__)))
     
-        # Get and print results
-    print('Unordered results:')
-    for i in range(len(TASKS1)):
-        print('\t', done_queue.get())
+    class Bigthink(VideoExtractor):
+    name = 'Bigthink'
+    
+            assert vid
+    
+    from ..common import *
+    
+    site_info = 'CKPlayer General'
+download = ckplayer_download
+download_playlist = playlist_not_supported('ckplayer')
+
+    
+            else:
+            # gallery image
+            content = get_content(self.url)
+            image = json.loads(match1(content, r'image\s*:\s*({.*}),'))
+            ext = image['ext']
+            self.streams = {
+                'original': {
+                    'src': ['http://i.imgur.com/%s%s' % (image['hash'], ext)],
+                    'size': image['size'],
+                    'container': ext[1:]
+                },
+                'thumbnail': {
+                    'src': ['http://i.imgur.com/%ss%s' % (image['hash'], '.jpg')],
+                    'container': 'jpg'
+                }
+            }
+            self.title = image['title'] or image['hash']
