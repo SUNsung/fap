3955a29326dc17105c5124e9a4f287199745069a
SUNsung <weatherius@gmail.com>
2019-06-25 06:17:30 +0300

    Rewrite [28jlJ2pxlb78w28yPYmjj56tmc28601MxPMY7X628RkE.cpp] File in: 2019-06-25 06:17:29

--- cccccc/28jlJ2pxlb78w28yPYmjj56tmc28601MxPMY7X628RkE.cpp
+++ cccccc/28jlJ2pxlb78w28yPYmjj56tmc28601MxPMY7X628RkE.cpp
@@ -1,3 +1,227 @@
 
         
-        
+        #endif  // TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+
+    
+    #include <iomanip>
+#include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/tensor_shape.pb.h'
+#include 'tensorflow/core/grappler/costs/graph_properties.h'
+#include 'tensorflow/core/grappler/grappler_item.h'
+    
+    
+    {}  // namespace tensorflow
+
+    
+    namespace tensorflow {
+    }
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // Creates a numpy array in 'ret' which either aliases the content of 't' or has
+// a copy.
+Status TensorToNdarray(const Tensor& t, PyObject** ret);
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    
+    {}  // namespace tensorflow
+
+    
+    #include 'tensorflow/stream_executor/platform.h'
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    namespace stream_executor {
+    }
+    
+    bool Converter<Dictionary>::FromV8(v8::Isolate* isolate,
+                                   v8::Local<v8::Value> val,
+                                   Dictionary* out) {
+  if (!val->IsObject() || val->IsFunction())
+    return false;
+  *out = Dictionary(isolate, v8::Local<v8::Object>::Cast(val));
+  return true;
+}
+    
+    #include 'base/macros.h'
+#include 'native_mate/converter.h'
+    
+    // For advanced use cases, we allow callers to request the unparsed Arguments
+// object and poke around in it directly.
+inline bool GetNextArgument(Arguments* args,
+                            int create_flags,
+                            bool is_first,
+                            Arguments* result) {
+  *result = *args;
+  return true;
+}
+inline bool GetNextArgument(Arguments* args,
+                            int create_flags,
+                            bool is_first,
+                            Arguments** result) {
+  *result = args;
+  return true;
+}
+    
+     protected:
+  friend class base::RefCounted<RefCountedPersistent<T>>;
+    
+    
+    {    // |wrapper| may be empty in some extreme cases, e.g., when
+    // Object.prototype.constructor is overwritten.
+    v8::Local<v8::Object> wrapper;
+    if (!templ->InstanceTemplate()
+             ->NewInstance(isolate->GetCurrentContext())
+             .ToLocal(&wrapper)) {
+      // The current wrappable object will be no longer managed by V8. Delete
+      // this now.
+      delete this;
+      return;
+    }
+    InitWith(isolate, wrapper);
+  }
+    
+    namespace mate {
+    }
+    
+        // Get current directory.
+    *current_directory =
+        base::FilePath(msg.substr(first_null + 1, second_null - first_null));
+    
+      g_signal_connect(registrar_proxy_, 'notify::g-name-owner',
+                   G_CALLBACK(OnNameOwnerChangedThunk), this);
+    
+    // Generate constructors.
+#include 'ipc/struct_constructor_macros.h'
+#include 'content/nw/src/common/common_message_generator.h'
+    
+    IPC_MESSAGE_ROUTED3(ShellViewHostMsg_Allocate_Object,
+                    int /* object id */,
+                    std::string /* type name */,
+                    base::DictionaryValue /* option */)
+    
+    public:
+  EventListener(int id,
+                const base::WeakPtr<DispatcherHost>& dispatcher_host,
+                const base::DictionaryValue& option);
+    
+    bool MenuDelegate::IsItemForCommandIdDynamic(int command_id) const {
+  if (command_id < 0)
+    return false;
+    }
+    
+    
+    {  focus_manager_ = NULL;
+}
+    
+      if (type == 'separator') {
+    menu_item_ = gtk_separator_menu_item_new();
+  } else {
+    if (type == 'checkbox') {
+      menu_item_ = gtk_check_menu_item_new();
+      bool checked;
+      if (option.GetBoolean('checked', &checked))
+        SetChecked(checked);
+    } else {
+      menu_item_ = gtk_image_menu_item_new();
+      std::string icon;
+      if (option.GetString('icon', &icon))
+        SetIcon(icon);
+    }
+    }
+    
+        bool Write(ClipboardData& data) {
+      switch(data.type) {
+        case TYPE_TEXT:
+        return WriteText(data);
+        break;
+        case TYPE_HTML:
+        return WriteHTML(data);
+        break;
+        case TYPE_RTF:
+        return WriteRTF(data);
+        break;
+        case TYPE_PNG:
+        case TYPE_JPEG:
+        return WriteImage(data);
+        break;
+        case TYPE_NONE:
+        NOTREACHED();
+        return false;
+      }
+      NOTREACHED();
+      return false;
+    }
+    
+    NwObjCallObjectMethodFunction::~NwObjCallObjectMethodFunction() {
+}
+    
+     protected:
+  ResponseAction Run() override;
+  ~NwObjCallObjectMethodAsyncFunction() override;
+    
+            b2Triangle* buffer = new b2Triangle[vNum-2];
+        int32 bufferSize = 0;
+        float32* xrem = new float32[vNum];
+        float32* yrem = new float32[vNum];
+        for (int32 i = 0; i < vNum; ++i) {
+            xrem[i] = xv[i];
+            yrem[i] = yv[i];
+        }
+		
+		int xremLength = vNum;
+		
+        while (vNum > 3) {
+            // Find an ear
+            int32 earIndex = -1;
+			//float32 earVolume = -1.0f;
+			float32 earMaxMinCross = -10.0f;
+            for (int32 i = 0; i < vNum; ++i) {
+                if (IsEar(i, xrem, yrem, vNum)) {
+					int32 lower = remainder(i-1,vNum);
+					int32 upper = remainder(i+1,vNum);
+					b2Vec2 d1(xrem[upper]-xrem[i],yrem[upper]-yrem[i]);
+					b2Vec2 d2(xrem[i]-xrem[lower],yrem[i]-yrem[lower]);
+					b2Vec2 d3(xrem[lower]-xrem[upper],yrem[lower]-yrem[upper]);
+    }
+    }
+    }
+    
+    
+    {#if defined (__cplusplus)
+}
+#endif
+    
+    #   define C_MULC(m,a,b) \
+      do{ (m).r = ADD32(S_MUL((a).r,(b).r) , S_MUL((a).i,(b).i)); \
+          (m).i = SUB32(S_MUL((a).i,(b).r) , S_MUL((a).r,(b).i)); }while(0)
+    
+       - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+    
+    /** Change a 32-bit value into a 16-bit value. The value is assumed to fit in 16-bit, otherwise the result is undefined */
+#define EXTRACT16(x) ((opus_val16)(x))
+/** Change a 16-bit value into a 32-bit value */
+#define EXTEND32(x) ((opus_val32)(x))
+    
+    /*Constants used by the entropy encoder/decoder.*/
+    
+    /* Multiply-accumulate macros that allow overflow in the addition (ie, no asserts in debug mode) */
+#define silk_MLA_ovflw(a32, b32, c32)       silk_ADD32_ovflw((a32), (opus_uint32)(b32) * (opus_uint32)(c32))
+#define silk_SMLABB_ovflw(a32, b32, c32)    (silk_ADD32_ovflw((a32) , ((opus_int32)((opus_int16)(b32))) * (opus_int32)((opus_int16)(c32))))
