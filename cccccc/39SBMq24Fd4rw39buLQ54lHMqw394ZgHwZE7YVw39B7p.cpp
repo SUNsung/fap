9c9b6ee53fab2d3baf5e65bb8ab04c6f07d78231
SUNsung <weatherius@gmail.com>
2019-06-25 13:53:18 +0300

    Rewrite [39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp] File in: 2019-06-25 13:53:17

--- cccccc/39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp
+++ cccccc/39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp
@@ -1,118 +1,86 @@
 
         
-        #include <google/protobuf/python/python_protobuf.h>
+        #include 'stdafx.h'
+#include 'CNTKLibrary.h'
+#include 'Utils.h'
+#include 'ReaderShim.h'
+#include 'DataReader.h'
     
-    TEST(CSharpEnumValue, PascalCasedPrefixStripping) {
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO__BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'FOO_BAR_BAZ'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'Foo_BarBaz'));
-  EXPECT_EQ('Bar', GetEnumValueName('FO_O', 'FOO_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('FOO', 'F_O_O_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
-  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO'));
-  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO___'));
-  // Identifiers can't start with digits
-  EXPECT_EQ('_2Bar', GetEnumValueName('Foo', 'FOO_2_BAR'));
-  EXPECT_EQ('_2', GetEnumValueName('Foo', 'FOO___2'));
+      /**
+   * @brief Helper to access config parsers via the registry
+   *
+   * This may return a nullptr if an exception is thrown attempting to retrieve
+   * the requested config parser.
+   *
+   * @param parser is the string name of the parser that you want
+   *
+   * @return a shared pointer to the config parser plugin
+   */
+  static const std::shared_ptr<ConfigParserPlugin> getParser(
+      const std::string& parser);
+    
+    Expected<int32_t, DatabaseError> Database::getInt32Or(
+    const std::string& domain,
+    const std::string& key,
+    const int32_t default_value) {
+  auto result = getInt32(domain, key);
+  if (!result && result.getError() == DatabaseError::KeyNotFound) {
+    return default_value;
+  }
+  return result;
 }
     
-    ExtensionGenerator* ImmutableGeneratorFactory::NewExtensionGenerator(
-    const FieldDescriptor* descriptor) const {
-  if (HasDescriptorMethods(descriptor->file(), context_->EnforceLite())) {
-    return new ImmutableExtensionGenerator(descriptor, context_);
-  } else {
-    return new ImmutableExtensionLiteGenerator(descriptor, context_);
+    
+    {  for (auto& line : doc.GetArray()) {
+    log.push_back({
+        static_cast<StatusLogSeverity>(line['s'].GetInt()),
+        line['f'].GetString(),
+        line['i'].GetUint64(),
+        line['m'].GetString(),
+        line['c'].GetString(),
+        line['u'].GetUint64(),
+        line['h'].GetString(),
+    });
   }
 }
     
-    #include <google/protobuf/compiler/objectivec/objectivec_helpers.h>
-#include <google/protobuf/testing/googletest.h>
-#include <gtest/gtest.h>
     
-      // Parsing accepts an fractional digits as long as they fit into nano
-  // precision.
-  EXPECT_TRUE(TimeUtil::FromString('1970-01-01T00:00:00.1Z', &time));
-  EXPECT_EQ(100000000, TimeUtil::TimestampToNanoseconds(time));
-  EXPECT_TRUE(TimeUtil::FromString('1970-01-01T00:00:00.0001Z', &time));
-  EXPECT_EQ(100000, TimeUtil::TimestampToNanoseconds(time));
-  EXPECT_TRUE(TimeUtil::FromString('1970-01-01T00:00:00.0000001Z', &time));
-  EXPECT_EQ(100, TimeUtil::TimestampToNanoseconds(time));
+    {} // namespace osquery
+
     
-            static float f = 0.0f;
-        ImGui::Text('Hello, world!');
-        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);
-        ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / io.Framerate, io.Framerate);
-        ImGui::ShowDemoWindow(NULL);
+      /// The plugin may perform some tear down, release, not required.
+  virtual void tearDown() {}
     
-        // Popups, Modals, Tooltips
-    IMGUI_API void          OpenPopupEx(ImGuiID id);
-    IMGUI_API void          ClosePopupToLevel(int remaining, bool apply_focus_to_window_under);
-    IMGUI_API void          ClosePopupsOverWindow(ImGuiWindow* ref_window);
-    IMGUI_API bool          IsPopupOpen(ImGuiID id); // Test for id within current popup stack level (currently begin-ed into); this doesn't scan the whole popup stack!
-    IMGUI_API bool          BeginPopupEx(ImGuiID id, ImGuiWindowFlags extra_flags);
-    IMGUI_API void          BeginTooltipEx(ImGuiWindowFlags extra_flags, bool override_previous_tooltip = true);
-    IMGUI_API ImGuiWindow*  GetFrontMostPopupModal();
-    IMGUI_API ImVec2        FindBestWindowPosForPopup(ImGuiWindow* window);
-    IMGUI_API ImVec2        FindBestWindowPosForPopupEx(const ImVec2& ref_pos, const ImVec2& size, ImGuiDir* last_dir, const ImRect& r_outer, const ImRect& r_avoid, ImGuiPopupPositionPolicy policy = ImGuiPopupPositionPolicy_Default);
+    GTEST_TEST(InMemoryDatabaseTest, test_keys_search) {
+  auto db = std::make_unique<InMemoryDatabase>('test');
+  ASSERT_FALSE(db->open().isError());
+  ASSERT_FALSE(db->putInt32(kPersistentSettings, 'key_1', 1).isError());
+  ASSERT_FALSE(db->putInt32(kPersistentSettings, 'key_2', 2).isError());
+  ASSERT_FALSE(db->putInt32(kPersistentSettings, 'key_3', 3).isError());
+  ASSERT_FALSE(db->putInt32(kPersistentSettings, 'kEy_1', 4).isError());
+  ASSERT_FALSE(db->putInt32(kPersistentSettings, 'kEy_2', 5).isError());
+  auto result_all = db->getKeys(kPersistentSettings);
+  EXPECT_TRUE(result_all);
+  EXPECT_EQ((*result_all).size(), 5);
+  auto result_some = db->getKeys(kPersistentSettings, 'key');
+  EXPECT_TRUE(result_some);
+  EXPECT_EQ((*result_some).size(), 3);
+}
     
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+    using json = nlohmann::json;
     
-    
-    {    // 3. Show another simple window.
-    if (show_another_window)
+        // out_of_range.403
+    try
     {
-        ImGui::Begin('Another Window', &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-        ImGui::Text('Hello from another window!');
-        if (ImGui::Button('Close Me'))
-            show_another_window = false;
-        ImGui::End();
+        // try to use a JSON pointer to an nonexistent object key
+        json::const_reference ref = j.at('/foo'_json_pointer);
     }
-}
-    
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
-    
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-    #include 'modules/drivers/canbus/can_client/socket/socket_can_client_raw.h'
-    
-    
-    {  int ret = x;
-  return ret;
-}
-    
-    RcsThreshold RadarState201::rcs_threshold(const std::uint8_t* bytes,
-                                          int32_t length) const {
-  Byte t0(bytes + 7);
-  uint32_t x = t0.get_byte(2, 3);
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
     }
     
-    namespace apollo {
-namespace localization {
-namespace msf {
-    }
-    }
-    }
-    
-      const double init_derivative = 0.1;
-  constraint.AddSecondDerivativeBoundary(init_derivative, index_list,
-                                         lower_bound, upper_bound);
-  const auto mat = constraint.inequality_constraint_matrix();
-  const auto bd = constraint.inequality_constraint_boundary();
-    
-    Brakemotorrpt170::Brakemotorrpt170() {}
-const int32_t Brakemotorrpt170::ID = 0x70;
-    
-    #include 'modules/drivers/canbus/common/byte.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
-    
-      x <<= 0;
-  x >>= 0;
+        // add values
+    auto res1 = object.emplace('three', 3);
+    null.emplace('A', 'a');
+    null.emplace('B', 'b');
