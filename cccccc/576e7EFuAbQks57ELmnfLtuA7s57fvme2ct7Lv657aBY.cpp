38b5f58530f8825407a7a54dce3d5216af3219f6
SUNsung <weatherius@gmail.com>
2019-06-25 18:25:16 +0300

    Rewrite [576e7EFuAbQks57ELmnfLtuA7s57fvme2ct7Lv657aBY.cpp] File in: 2019-06-25 18:25:16

--- cccccc/576e7EFuAbQks57ELmnfLtuA7s57fvme2ct7Lv657aBY.cpp
+++ cccccc/576e7EFuAbQks57ELmnfLtuA7s57fvme2ct7Lv657aBY.cpp
@@ -1,106 +1,87 @@
 
         
-          Storage(GenericSignature *genericSig,
-          ArrayRef<Type> replacementTypes,
-          ArrayRef<ProtocolConformanceRef> conformances);
+        /*!
+ * \brief Registry entry for linear updater.
+ */
+struct LinearUpdaterReg
+    : public dmlc::FunctionRegEntryBase<LinearUpdaterReg,
+                                        std::function<LinearUpdater*()> > {};
     
-      bool isRecord() const {
-    assert(isValid());
-    return !Decl.isNull() && Decl.is<const clang::RecordDecl *>();
-  }
-  const clang::RecordDecl *getRecord() const {
-    assert(isRecord());
-    return Decl.get<const clang::RecordDecl *>();
-  }
-    
-        void emitDiagnosticMessage(clang::FullSourceLoc Loc,
-                               clang::PresumedLoc PLoc,
-                               clang::DiagnosticsEngine::Level Level,
-                               StringRef Message,
-                               ArrayRef<clang::CharSourceRange> Ranges,
-                               clang::DiagOrStoredDiag Info) override {
-      if (isInSwiftBuffers(Loc)) {
-        // FIXME: Ideally, we'd report non-suppressed diagnostics on synthetic
-        // buffers, printing their names (eg. <swift-imported-modules>:...) but
-        // this risks printing _excerpts_ of those buffers to stderr too; at
-        // present the synthetic buffers are 'large blocks of null bytes' which
-        // we definitely don't want to print out. So until we have some clever
-        // way to print the name but suppress printing excerpts, we just replace
-        // the Loc with an invalid one here, which suppresses both.
-        Loc = clang::FullSourceLoc();
-        if (shouldSuppressDiagInSwiftBuffers(Info))
-          return;
-      }
-      callback(Loc, Level, Message);
+        XGBOOST_DEVICE bool operator==(const Iterator &other) const {
+      return i_ >= other.i_;
+    }
+    XGBOOST_DEVICE bool operator!=(const Iterator &other) const {
+      return i_ < other.i_;
     }
     
+    namespace xgboost {
+namespace metric {
+    }
+    }
     
+    bool Channel::close()
+{
+    if (closed)
     {
-    {} // namespace test
-} // namespace c10d
+        return false;
+    }
+    swTraceLog(SW_TRACE_CHANNEL, 'channel closed');
+    closed = true;
+    while (!producer_queue.empty())
+    {
+        Coroutine *co = pop_coroutine(PRODUCER);
+        co->resume();
+    }
+    while (!consumer_queue.empty())
+    {
+        Coroutine *co = pop_coroutine(CONSUMER);
+        co->resume();
+    }
+    return true;
+}
 
     
-    namespace caffe2 {
-    }
+    //	swSignal_add(SIGINT, user_signal);
     
-    REGISTER_CPU_OPERATOR(
-    MergeSingleMapFeatureTensorsGradient,
-    MergeSingleListOrMapFeatureTensorsGradientOp<CPUContext>);
-OPERATOR_SCHEMA(MergeSingleMapFeatureTensorsGradient)
-    .SetDoc(
-        'Explode given multi-feature tensors with map features into '
-        'multiple single-feature tensor.' +
-        doc)
-    .NumInputs([](int n) { return n >= 3 && n % 2 == 1; })
-    .NumOutputs([](int n) { return n >= 1; })
-    .Input(0, 'in1_lengths', '.lengths')
-    .Input(1, 'in1_presence', '.presence')
-    .Input(2, 'out_values_values_grad', '.values.values_grad')
-    .Output(0, 'in1_values_grad', '.values_grad');
-REGISTER_GRADIENT(
-    MergeSingleMapFeatureTensors,
-    GetMergeSingleMapFeatureTensorsGradient);
     
-    template <typename T, class Context>
-class BernoulliJSDGradientOp final : public Operator<Context> {
- public:
-  USE_SIMPLE_CTOR_DTOR(BernoulliJSDGradientOp);
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-  bool RunOnDevice() override;
+    {    private:
+        redisAsyncContext * m_ctx;
+        QSocketNotifier * m_read;
+        QSocketNotifier * m_write;
 };
     
-    REGISTER_CUDA_OPERATOR(LC1D, LocallyConnectedOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(
-    LC1DGradient,
-    LocallyConnectedGradientOp<float, CUDAContext>);
+        pid_t server_pid = create_server();
     
-    				cmderOptions.cmderCfgRoot = cmderCfgRoot;
-    
-    
-    { private:
-  RowBlock<IndexType> out_;
-  std::unique_ptr<Parser<IndexType> > parser_;
-  uint32_t num_col_;
-  std::vector<size_t> offset_;
-  std::vector<IndexType> dense_index_;
-  std::vector<xgboost::bst_float> dense_value_;
-};
-    
-    /*!
- * \brief Registry entry for tree updater.
- */
-struct GradientBoosterReg
-    : public dmlc::FunctionRegEntryBase<
-  GradientBoosterReg,
-  std::function<GradientBooster* (const std::vector<std::shared_ptr<DMatrix> > &cached_mats,
-                                  bst_float base_margin)> > {
-};
+            if (pid == 0)
+        {
+            exit(0);
+        }
     
     
     {
-    {    ++right;
-    SPAN_ASSERT_TRUE(right > left, status_);
-    SPAN_ASSERT_TRUE(left < right, status_);
-    SPAN_ASSERT_TRUE(left <= right, status_);
-  }
+    {protected:
+    swServer serv;
+    vector<swListenPort *> ports;
+    string host;
+    int port;
+    int mode;
+    int events;
 };
+}
+
+    
+        unlink(sock1_path);
+    unlink(sock2_path);
+    
+        swSignalfd_init();
+    swSignal_add(SIGUSR1,sig_usr1);
+    swSignalfd_setup(SwooleG.main_reactor);
+    
+        /**
+     * 协程2
+     */
+    Coroutine::create([](void *arg)
+    {
+        G_a.x = 100;
+        G_a.y = nullptr;
+    });
