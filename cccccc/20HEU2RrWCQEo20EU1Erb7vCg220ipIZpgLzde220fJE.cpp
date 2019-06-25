719ff5cf7e40fe13d1f7c852614bd6865f41bd36
SUNsung <weatherius@gmail.com>
2019-06-25 10:57:51 +0300

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2019-06-25 10:57:50

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,167 +1,100 @@
 
         
-        namespace caffe {
+            void NDArrayView::SetValue(int8_t value)
+    {
+        if (IsSparse())
+            LogicError('NDArrayView::SetValue: Setting a NDArrayView contents to a scalar is only allowed for objects with dense storage format.');
     }
     
-    /**
- * @brief Provides base for data layers that feed blobs to the Net.
- *
- * TODO(dox): thorough documentation for Forward and proto params.
- */
-template <typename Dtype>
-class BaseDataLayer : public Layer<Dtype> {
- public:
-  explicit BaseDataLayer(const LayerParameter& param);
-  // LayerSetUp: implements common data layer setup functionality, and calls
-  // DataLayerSetUp to do special data layer setup for individual layer types.
-  // This method may not be overridden except by the BasePrefetchingDataLayer.
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {}
-  // Data layers have no bottoms, so reshaping is trivial.
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {}
+    #include 'stdafx.h'
+#include 'CNTKLibrary.h'
+#include 'Utils.h'
+#include 'Learner.h'
+#include 'PerformanceProfiler.h'
+#include 'CompositeFunction.h'
+#include 'Serialization.h'
+    
+        std::string m_name; // lock name
+    HANDLE m_handle;
+    
+        double ElapsedSeconds();
+    
+    
+    {        // ensure none of the specified inputs reference back into the cloned set
+        // The function we extract must be separable.
+        for (let& input : inputNodes)
+            for (let& node : ComputationNodeBase::EnumerateNodes(vector<ComputationNodeBasePtr>{input})) // check all indirect inputs of each specified input
+            {
+                let iter = dependentSet.find(input);
+                if (iter != dependentSet.end() && *iter != input)
+                    InvalidArgument('CloneFunction: specified function input %ls recursively depends on %ls inside the function.', input->NodeDescription().c_str(), node->NodeDescription().c_str());
+            }
     }
     
-    #endif  // CAFFE_BNLL_LAYER_HPP_
-
+    // -----------------------------------------------------------------------
+// (deprecated) PerDimMeanVarNormalizationNode (feature, mean, invStdDev)
+// Computes
+//   output = (feature - mean) .* invStdDev
+// where mean and invStdDev are meant to be single elements while features
+// is minibatch data.
+// Deprecated since it can be trivially expressed in BrainScript.
+// -----------------------------------------------------------------------
     
-    #include 'caffe/layers/loss_layer.hpp'
-    
-      // algorithms for forward and backwards convolutions
-  cudnnConvolutionFwdAlgo_t *fwd_algo_;
-  cudnnConvolutionBwdFilterAlgo_t *bwd_filter_algo_;
-  cudnnConvolutionBwdDataAlgo_t *bwd_data_algo_;
-    
-      bool Read(SparsePage* page,
-            dmlc::SeekStream* fi,
-            const std::vector<bst_uint>& sorted_index_set) override {
-    if (!fi->Read(&disk_offset_)) return false;
-    auto& offset_vec = page->offset.HostVector();
-    auto& data_vec = page->data.HostVector();
-    // setup the offset
-    offset_vec.clear();
-    offset_vec.push_back(0);
-    for (unsigned int fid : sorted_index_set) {
-      CHECK_LT(fid + 1, disk_offset_.size());
-      size_t size = disk_offset_[fid + 1] - disk_offset_[fid];
-      offset_vec.push_back(offset_vec.back() + size);
+        fPattern          = NULL; // will be set later
+    fPatternString    = NULL; // may be set later
+    fCompiledPat      = new UVector64(fDeferredStatus);
+    fGroupMap         = new UVector32(fDeferredStatus);
+    fSets             = new UVector(fDeferredStatus);
+    fInitialChars     = new UnicodeSet;
+    fInitialChars8    = new Regex8BitSet;
+    fNamedCaptureMap  = uhash_open(uhash_hashUnicodeString,     // Key hash function
+                                   uhash_compareUnicodeString,  // Key comparator function
+                                   uhash_compareLong,           // Value comparator function
+                                   &fDeferredStatus);
+    if (U_FAILURE(fDeferredStatus)) {
+        return;
     }
-    data_vec.resize(offset_vec.back());
-    // read in the data
-    size_t begin = fi->Tell();
-    size_t curr_offset = 0;
-    for (size_t i = 0; i < sorted_index_set.size();) {
-      bst_uint fid = sorted_index_set[i];
-      if (disk_offset_[fid] != curr_offset) {
-        CHECK_GT(disk_offset_[fid], curr_offset);
-        fi->Seek(begin + disk_offset_[fid] * sizeof(Entry));
-        curr_offset = disk_offset_[fid];
-      }
-      size_t j, size_to_read = 0;
-      for (j = i; j < sorted_index_set.size(); ++j) {
-        if (disk_offset_[sorted_index_set[j]] == disk_offset_[fid] + size_to_read) {
-          size_to_read += offset_vec[j + 1] - offset_vec[j];
-        } else {
-          break;
-        }
-      }
-    }
+    if (fCompiledPat == NULL  || fGroupMap == NULL || fSets == NULL ||
+            fInitialChars == NULL || fInitialChars8 == NULL || fNamedCaptureMap == NULL) {
+        fDeferredStatus = U_MEMORY_ALLOCATION_ERROR;
+        return;
     }
     
-     public:
-  /*!
-   * \brief Remove leading and trailing whitespaces from a given string
-   * \param str string
-   * \return Copy of str with leading and trailing whitespaces removed
-   */
-  static std::string TrimWhitespace(const std::string& str) {
-    const auto first_char = str.find_first_not_of(' \t\n\r');
-    const auto last_char = str.find_last_not_of(' \t\n\r');
-    if (first_char == std::string::npos) {
-      // Every character in str is a whitespace
-      return std::string();
+    void
+FixedSortKeyByteSink::AppendBeyondCapacity(const char *bytes, int32_t /*n*/, int32_t length) {
+    // buffer_ != NULL && bytes != NULL && n > 0 && appended_ > capacity_
+    // Fill the buffer completely.
+    int32_t available = capacity_ - length;
+    if (available > 0) {
+        uprv_memcpy(buffer_ + length, bytes, available);
     }
-    CHECK_NE(last_char, std::string::npos);
-    const auto substr_len = last_char + 1 - first_char;
-    return str.substr(first_char, substr_len);
-  }
-    
-      // 4. One-line comment
-  ASSERT_TRUE(parser.ParseKeyValuePair('learning_rate = 0.3   # small step',
-                                       &key, &value));
-  ASSERT_EQ(key, 'learning_rate');
-  ASSERT_EQ(value, '0.3');
-  // Note: '#' in path won't be accepted correctly unless the whole path is
-  // wrapped with quotes. This is important for external memory.
-  ASSERT_TRUE(parser.ParseKeyValuePair('data = dmatrix.libsvm#dtrain.cache',
-                                       &key, &value));
-  ASSERT_EQ(key, 'data');
-  ASSERT_EQ(value, 'dmatrix.libsvm');  // cache was silently ignored
-    
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////n
-    }
-    
-      typedef std::pair<const char*, std::string> InfoEntry;
-  typedef std::vector<InfoEntry> InfoVec;
-    
-    #include 'hphp/runtime/base/file.h'
-#include 'hphp/runtime/base/mem-file.h'
-#include 'hphp/runtime/base/stream-wrapper.h'
-#include <folly/String.h>
-#include <folly/portability/SysStat.h>
-#include <folly/portability/Unistd.h>
-    
-    struct GlobStreamWrapper final : Stream::Wrapper {
-  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-  req::ptr<Directory> opendir(const String& path) override;
-};
-    
-      // implementing File
-  bool open(const String& filename, const String& mode) override;
-  bool close() override;
-  int64_t readImpl(char *buffer, int64_t length) override;
-  int getc() override;
-  int64_t writeImpl(const char *buffer, int64_t length) override;
-  bool seek(int64_t offset, int whence = SEEK_SET) override;
-  int64_t tell() override;
-  bool eof() override;
-  bool rewind() override;
-  bool flush() override;
-  bool truncate(int64_t size) override;
-    
-      static Data& getMutableData() { return data_; }
-    
-    
-    {} // namespace aria2
-    
-    DHTRoutingTableSerializer::~DHTRoutingTableSerializer() = default;
-    
-    
-    {} // namespace aria2
-    
-    class DHTNode;
-class DHTRoutingTable;
-class DHTMessageDispatcher;
-class DHTMessageFactory;
-class DHTTaskQueue;
-class DHTAbstractTask;
-    
-    void DHTTaskQueueImpl::executeTask()
-{
-  A2_LOG_DEBUG('Updating periodicTaskQueue1');
-  periodicTaskQueue1_.update();
-  A2_LOG_DEBUG('Updating periodicTaskQueue2');
-  periodicTaskQueue2_.update();
-  A2_LOG_DEBUG('Updating immediateTaskQueue');
-  immediateTaskQueue_.update();
 }
     
-      std::string generateToken(const unsigned char* infoHash,
-                            const std::string& ipaddr, uint16_t port,
-                            const unsigned char* secret) const;
+    #endif /* !UCONFIG_NO_FORMATTING */
+
     
-    #include <memory>
+        /**
+     * Returns TRUE if this class can format positiveValue using
+     * the given range of digit counts.
+     *
+     * @param positiveValue the value to format
+     * @param range the acceptable range of digit counts.
+     */
+    static UBool canFormat(
+            int32_t positiveValue, const IntDigitCountRange &range);
+    
+    U_NAMESPACE_BEGIN
+    
+    /**
+ * UnicodeFunctor API.  Cast 'this' to a UnicodeMatcher* pointer
+ * and return the pointer.
+ */
+UnicodeMatcher* StringMatcher::toMatcher() const {
+  StringMatcher  *nonconst_this = const_cast<StringMatcher *>(this);
+  UnicodeMatcher *nonconst_base = static_cast<UnicodeMatcher *>(nonconst_this);
+  
+  return nonconst_base;
+}
+    
+    
+    {};
