2a9e3dc11764df33e8d29bc1dd2859e13f05a06f
SUNsung <weatherius@gmail.com>
2019-06-26 02:01:38 +0300

    Rewrite [11Dd9.KjcEX6M11IRAxCTBPutI11IC5yC3oRCVU11P.W.py] File in: 2019-06-26 02:01:37

--- pyton/11Dd9.KjcEX6M11IRAxCTBPutI11IC5yC3oRCVU11P.W.py
+++ pyton/11Dd9.KjcEX6M11IRAxCTBPutI11IC5yC3oRCVU11P.W.py
@@ -1,117 +1,96 @@
 
         
         
-class Supervisor(Employee):
+class FileModeWarning(RequestsWarning, DeprecationWarning):
+    '''A file was opened in text mode, but Requests determined its binary length.'''
+    pass
+    
+        def __iter__(self):
+        return (casedkey for casedkey, mappedvalue in self._store.values())
+    
+        return inner
+    
+        def test_repr(self):
+        assert repr(self.lookup_dict) == '<lookup 'test'>'
+    
+            Comment:                   'italic #8f5902', # class: 'c'
+        Comment.Preproc:           'noitalic',       # class: 'cp'
+    
+    # -------
+# Pythons
+# -------
+    
+    # TODO: response is the only one
+    
+    This module contains the primary objects that power Requests.
+'''
+    
+        Handles decoding/encoding between RFC3339 strings and aware (not
+    naive) `datetime.datetime` objects
+    (e.g. ``datetime.datetime.now(pytz.utc)``).
+    
+    # http://docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+    
+        def step(self, action):
+        assert self.action_space.contains(action)
+        if self.np_random.rand() < self.slip:
+            action = not action  # agent slipped, reverse action taken
+        if action:  # 'backwards': go back to the beginning, get small reward
+            reward = self.small
+            self.state = 0
+        elif self.state < self.n - 1:  # 'forwards': go up along the chain
+            reward = 0
+            self.state += 1
+        else:  # 'forwards': stay at the end of the chain, collect large reward
+            reward = self.large
+        done = False
+        return self.state, reward, done, {}
+    
+    # TODO: don't hardcode sizeof_int here
+def _bigint_from_bytes(bytes):
+    sizeof_int = 4
+    padding = sizeof_int - len(bytes) % sizeof_int
+    bytes += b'\0' * padding
+    int_count = int(len(bytes) / sizeof_int)
+    unpacked = struct.unpack('{}I'.format(int_count), bytes)
+    accum = 0
+    for i, val in enumerate(unpacked):
+        accum += 2 ** (sizeof_int * 8 * i) * val
+    return accum
+    
+        def test_reversed_addition_3rows(self):
+        env = alg.reversed_addition.ReversedAdditionEnv(base=3, rows=3)
+        input_expected = [
+            ([[1,1,0],[0,1,1]], [2, 2]),
+            ([[1,1,2],[0,1,1]], [1,0,1]),
+        ]
+        for (input_grid, expected_target) in input_expected:
+            self.assertEqual(env.target_from_input_data(input_grid), expected_target)
+    
+                # Physically correct is to always apply friction_limit until speed is equal.
+            # But dt is finite, that will lead to oscillations if difference is already near zero.
+            f_force *= 205000*SIZE*SIZE  # Random coefficient to cut oscillations in few steps (have no effect on friction_limit)
+            p_force *= 205000*SIZE*SIZE
+            force = np.sqrt(np.square(f_force) + np.square(p_force))
+    
+      if use_tpu:
+    optimizer = tf.contrib.tpu.CrossShardOptimizer(optimizer)
+    
+      return model_fn
     
     
-def prepare_url(value):
-    # Issue #1483: Make sure the URL always has a trailing slash
-    httpbin_url = value.url.rstrip('/') + '/'
+def write_instance_to_example_files(instances, tokenizer, max_seq_length,
+                                    max_predictions_per_seq, output_files):
+  '''Create TF example files from `TrainingInstance`s.'''
+  writers = []
+  for output_file in output_files:
+    writers.append(tf.python_io.TFRecordWriter(output_file))
     
-    '''
-Requests HTTP Library
-~~~~~~~~~~~~~~~~~~~~~
-    
-                if isinstance(e.reason, _ProxyError):
-                raise ProxyError(e, request=request)
-    
-        See https://github.com/requests/requests/issues/1979.
-    '''
-    text_401 = (b'HTTP/1.1 401 UNAUTHORIZED\r\n'
-                b'Content-Length: 0\r\n'
-                b'WWW-Authenticate: Digest nonce='6bf5d6e4da1ce66918800195d6b9130d''
-                b', opaque='372825293d1c26955496c80ed6426e9e', '
-                b'realm='me@kennethreitz.com', qop=auth\r\n\r\n')
-    
-                if isinstance(fp, (str, bytes, bytearray)):
-                fdata = fp
-            elif hasattr(fp, 'read'):
-                fdata = fp.read()
-            elif fp is None:
-                continue
-            else:
-                fdata = fp
-    
-    
-    {    # Server Error.
-    500: ('internal_server_error', 'server_error', '/o\\', '✗'),
-    501: ('not_implemented',),
-    502: ('bad_gateway',),
-    503: ('service_unavailable', 'unavailable'),
-    504: ('gateway_timeout',),
-    505: ('http_version_not_supported', 'http_version'),
-    506: ('variant_also_negotiates',),
-    507: ('insufficient_storage',),
-    509: ('bandwidth_limit_exceeded', 'bandwidth'),
-    510: ('not_extended',),
-    511: ('network_authentication_required', 'network_auth', 'network_authentication'),
-}
-    
-            # .netrc might have more auth for us on our new host.
-        new_auth = get_netrc_auth(url) if self.trust_env else None
-        if new_auth is not None:
-            prepared_request.prepare_auth(new_auth)
-    
-            self.concurrent -= 1
-        return ''
-    
-        def __enter__(self):
-        from scrapy.utils.test import get_testenv
-        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
-        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                     env=get_testenv())
-        self.proc.stdout.readline()
-    
-        def long_desc(self):
-        return ('Edit a spider using the editor defined in the EDITOR environment'
-                ' variable or else the EDITOR setting')
-    
-        def _print_headers(self, headers, prefix):
-        for key, values in headers.items():
-            for value in values:
-                self._print_bytes(prefix + b' ' + key + b': ' + value)
-    
-        def add_requests(self, lvl, new_reqs):
-        old_reqs = self.requests.get(lvl, [])
-        self.requests[lvl] = old_reqs + new_reqs
-    
-        def run(self, args, opts):
-        if len(args) != 1:
-            raise UsageError()
-        filename = args[0]
-        if not os.path.exists(filename):
-            raise UsageError('File not found: %s\n' % filename)
-        try:
-            module = _import_file(filename)
-        except (ImportError, ValueError) as e:
-            raise UsageError('Unable to load %r: %s\n' % (filename, e))
-        spclasses = list(iter_spider_classes(module))
-        if not spclasses:
-            raise UsageError('No spider found in file: %s\n' % filename)
-        spidercls = spclasses.pop()
-    
-        def run(self, args, opts):
-        settings = self.crawler_process.settings
-        if opts.get:
-            s = settings.get(opts.get)
-            if isinstance(s, BaseSettings):
-                print(json.dumps(s.copy_to_dict()))
-            else:
-                print(s)
-        elif opts.getbool:
-            print(settings.getbool(opts.getbool))
-        elif opts.getint:
-            print(settings.getint(opts.getint))
-        elif opts.getfloat:
-            print(settings.getfloat(opts.getfloat))
-        elif opts.getlist:
-            print(settings.getlist(opts.getlist))
-
-    
-        def syntax(self):
-        return '[-v]'
-    
-        # with patch('homeassistant.config.async_hass_config_yaml',
-    #            Mock(return_value=mock_coro(config))):
-    #     yield from hass.services.async_call(
-    #         automation.DOMAIN, automation.SERVICE_RELOAD, blocking=True)
+    test_ci = functools.partial(__check_path__, './ci')
