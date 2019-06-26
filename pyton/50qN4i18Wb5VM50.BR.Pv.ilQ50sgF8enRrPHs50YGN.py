671732733c806cc8ba8e87772a8d6bd6aa099bb2
SUNsung <weatherius@gmail.com>
2019-06-27 00:18:51 +0300

    Rewrite [50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py] File in: 2019-06-27 00:18:51

--- pyton/50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py
+++ pyton/50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py
@@ -1,114 +1,148 @@
 
         
-        from httpie.compat import is_windows
-from httpie.config import DEFAULT_CONFIG_DIR, Config
+        
+def testFixnum():
+    check(b'\x92\x93\x00\x40\x7f\x93\xe0\xf0\xff', ((0,
+                                                     64,
+                                                     127, ),
+                                                    (-32,
+                                                     -16,
+                                                     -1, ), ))
     
-        def get_converters(self):
-        return [plugin for plugin in self
-                if issubclass(plugin, ConverterPlugin)]
-    
-        >>> humanize_bytes(1)
-    '1 B'
-    >>> humanize_bytes(1024, precision=1)
-    '1.0 kB'
-    >>> humanize_bytes(1024 * 123, precision=1)
-    '123.0 kB'
-    >>> humanize_bytes(1024 * 12342, precision=1)
-    '12.1 MB'
-    >>> humanize_bytes(1024 * 12342, precision=2)
-    '12.05 MB'
-    >>> humanize_bytes(1024 * 1234, precision=2)
-    '1.21 MB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
-    '1.31 GB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
-    '1.3 GB'
+        for i, filename in enumerate(files, 1):
+        out = str(dest.joinpath(filename))
+        link = urllib.request.urljoin(base, filename)
+        urllib.request.urlretrieve(link, out)
+        print('Downloaded {link} to {out} [{i}/{N}]'.format(
+            link=link, out=out, i=i, N=N
+        ))
     
     
-def patharg(path):
-    '''
-    Back slashes need to be escaped in ITEM args,
-    even in Windows paths.
+@pytest.mark.parametrize('ties_method', [
+    'average', 'min', 'max', 'first', 'dense'])
+@pytest.mark.parametrize('ascending', [True, False])
+@pytest.mark.parametrize('na_option', ['keep', 'top', 'bottom'])
+@pytest.mark.parametrize('pct', [True, False])
+@pytest.mark.parametrize('vals', [
+    ['bar', 'bar', 'foo', 'bar', 'baz'],
+    ['bar', np.nan, 'foo', np.nan, 'baz']
+])
+def test_rank_object_raises(ties_method, ascending, na_option,
+                            pct, vals):
+    df = DataFrame({'key': ['foo'] * 5, 'val': vals})
     
-        def test_implicit_GET_with_headers(self, httpbin):
-        r = http(httpbin.url + '/headers', 'Foo:bar')
-        assert HTTP_OK in r
-        assert r.json['headers']['Foo'] == 'bar'
+    Libraries are expected to implement a few pytest fixtures to provide data
+for the tests. The fixtures may be located in either
     
-    from httpie import ExitStatus
-from httpie.core import main
+        ('border-top-style: dotted',
+     {'border': {'top': {'style': 'mediumDashDotDot'}}}),
+    ('border-top-style: dotted; border-top-width: thin',
+     {'border': {'top': {'style': 'dotted'}}}),
+    ('border-top-style: dashed',
+     {'border': {'top': {'style': 'mediumDashed'}}}),
+    ('border-top-style: dashed; border-top-width: thin',
+     {'border': {'top': {'style': 'dashed'}}}),
+    ('border-top-style: double',
+     {'border': {'top': {'style': 'double'}}}),
+    # - color
+    ('border-style: solid; border-color: #0000ff',
+     {'border': {'top': {'style': 'medium', 'color': '0000FF'},
+                 'right': {'style': 'medium', 'color': '0000FF'},
+                 'bottom': {'style': 'medium', 'color': '0000FF'},
+                 'left': {'style': 'medium', 'color': '0000FF'}}}),
+    ('border-top-style: double; border-top-color: blue',
+     {'border': {'top': {'style': 'double', 'color': '0000FF'}}}),
+    ('border-top-style: solid; border-top-color: #06c',
+     {'border': {'top': {'style': 'medium', 'color': '0066CC'}}}),
+    # ALIGNMENT
+    # - horizontal
+    ('text-align: center',
+     {'alignment': {'horizontal': 'center'}}),
+    ('text-align: left',
+     {'alignment': {'horizontal': 'left'}}),
+    ('text-align: right',
+     {'alignment': {'horizontal': 'right'}}),
+    ('text-align: justify',
+     {'alignment': {'horizontal': 'justify'}}),
+    # - vertical
+    ('vertical-align: top',
+     {'alignment': {'vertical': 'top'}}),
+    ('vertical-align: text-top',
+     {'alignment': {'vertical': 'top'}}),
+    ('vertical-align: middle',
+     {'alignment': {'vertical': 'center'}}),
+    ('vertical-align: bottom',
+     {'alignment': {'vertical': 'bottom'}}),
+    ('vertical-align: text-bottom',
+     {'alignment': {'vertical': 'bottom'}}),
+    # - wrap_text
+    ('white-space: nowrap',
+     {'alignment': {'wrap_text': False}}),
+    ('white-space: pre',
+     {'alignment': {'wrap_text': False}}),
+    ('white-space: pre-line',
+     {'alignment': {'wrap_text': False}}),
+    ('white-space: normal',
+     {'alignment': {'wrap_text': True}}),
+    # NUMBER FORMAT
+    ('number-format: 0%',
+     {'number_format': {'format_code': '0%'}}),
+])
+def test_css_to_excel(css, expected):
+    convert = CSSToExcelConverter()
+    assert expected == convert(css)
     
     
-@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
-def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
-    r = http(follow_flag, httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 1
-    assert HTTP_OK in r
-    
-            if self.about:
-            self['__meta__']['about'] = self.about
-    
-            # host:port => host_port
-        hostname = hostname.replace(':', '_')
-        path = os.path.join(config_dir,
-                            SESSIONS_DIR_NAME,
-                            hostname,
-                            session_name + '.json')
-    
-        def stop(self):
-        '''Stop reporting on next tick.'''
-        self._should_stop.set()
-    
-        def reset_appid(self):
-        # called by web_control
-        with self.lock:
-            self.working_appid_list = list()
-            for appid in self.config.GAE_APPIDS:
-                if not appid:
-                    self.config.GAE_APPIDS.remove(appid)
-                    continue
-                self.working_appid_list.append(appid)
-            self.not_exist_appids = []
-            self.out_of_quota_appids = []
-        self.last_reset_time = time.time()
+def test_bin16():
+    header = b'\xc5'
+    data = b'x' * 256
+    b = packb(data, use_bin_type=True)
+    assert len(b) == len(data) + 3
+    assert b[0:1] == header
+    assert b[1:3] == b'\x01\x00'
+    assert b[3:] == data
+    assert unpackb(b) == data
     
     
-IPv4 = CheckNetwork('IPv4')
-IPv4.urls = [
-            'https://www.microsoft.com',
-            'https://www.apple.com',
-            'https://code.jquery.com',
-            'https://cdn.bootcss.com',
-            'https://cdnjs.cloudflare.com']
-IPv4.triger_check_network()
+def test_incorrect_type_array():
+    unpacker = Unpacker()
+    unpacker.feed(packb(1))
+    try:
+        unpacker.read_array_header()
+        assert 0, 'should raise exception'
+    except UnexpectedTypeException:
+        assert 1, 'okay'
     
+        def __init__(self, image, alignments, original_roi=None):
+        logger.debug('Initializing %s: (alignments: %s, original_roi: %s)',
+                     self.__class__.__name__, alignments, original_roi)
+        self.image = image
+        self.alignments = alignments
+        self.roi = original_roi
+        self.colors = {1: (255, 0, 0),
+                       2: (0, 255, 0),
+                       3: (0, 0, 255),
+                       4: (255, 255, 0),
+                       5: (255, 0, 255),
+                       6: (0, 255, 255)}
+        logger.debug('Initialized %s', self.__class__.__name__)
     
-    def __str__(self):
-        exp = ', expected %s' % self.expecting
-        if self.expecting == INVALID_TOKEN_TYPE:
-            exp = ''
+        new_shape = shape[:3] + [int(shape[3] / (scale ** 2))]
+    var_x = initializer(new_shape, dtype)
+    var_x = tf.transpose(var_x, perm=[2, 0, 1, 3])
+    var_x = tf.image.resize_nearest_neighbor(var_x, size=(shape[0] * scale, shape[1] * scale))
+    var_x = tf.space_to_depth(var_x, block_size=scale)
+    var_x = tf.transpose(var_x, perm=[1, 2, 0, 3])
+    return var_x
     
-                if point.name == 'p':
-                link = point.find('a')
-                if link is not None:
-                    link = clean_pdf_link(link.attrs['href'])
-                    ext = get_extension(link)
-                    print(ext)
-                    if not ext in forbidden_extensions:
-                        print(shorten_title(point.text) + ' (' + link + ')')
-                        try:
-                            name = clean_text(point.text.split('[' + ext + ']')[0])
-                            fullname = '.'.join((name, ext))
-                            if not os.path.exists('/'.join((current_directory, fullname)) ):
-                                download_pdf(link, current_directory, '.'.join((name, ext)))
-                        except KeyboardInterrupt:
-                            try:
-                                print('Press Ctrl-C in 1 second to quit')
-                                time.sleep(1)
-                            except KeyboardInterrupt:
-                                print('Cancelling..')
-                                break
-                        except:
-                            failures.append(point.text)
-                        
-        point = point.next_sibling          
+            dest_format = self.get_dest_format()
+        if len(self.args.alignments_file) == 1:
+            retval = AlignmentData(self.args.alignments_file[0], dest_format)
+        else:
+            retval = [AlignmentData(a_file, dest_format) for a_file in self.args.alignments_file]
+        logger.debug('Alignments: %s', retval)
+        return retval
+    
+            logger.trace('Loaded aligned face: %s', {key: val
+                                                 for key, val in self.aligned.items()
+                                                 if key != 'face'})
