3b628c70a4ec48026bd51dfd05d2457db8ef4c1c
SUNsung <weatherius@gmail.com>
2019-06-26 12:34:00 +0300

    Rewrite [21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb] File in: 2019-06-26 12:33:59

--- ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
+++ ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
@@ -1,137 +1,39 @@
 
         
-        # == Schema Information
-#
-# Table name: directory_items
-#
-#  id             :integer          not null, primary key
-#  period_type    :integer          not null
-#  user_id        :integer          not null
-#  likes_received :integer          not null
-#  likes_given    :integer          not null
-#  topics_entered :integer          not null
-#  topic_count    :integer          not null
-#  post_count     :integer          not null
-#  created_at     :datetime
-#  updated_at     :datetime
-#  days_visited   :integer          default(0), not null
-#  posts_read     :integer          default(0), not null
-#
-# Indexes
-#
-#  index_directory_items_on_days_visited             (days_visited)
-#  index_directory_items_on_likes_given              (likes_given)
-#  index_directory_items_on_likes_received           (likes_received)
-#  index_directory_items_on_period_type_and_user_id  (period_type,user_id) UNIQUE
-#  index_directory_items_on_post_count               (post_count)
-#  index_directory_items_on_posts_read               (posts_read)
-#  index_directory_items_on_topic_count              (topic_count)
-#  index_directory_items_on_topics_entered           (topics_entered)
-#
-
+              builder << nil
+      builder << 123
+      builder << 'string'
+      builder << []
+      builder << {}
     
-      def self.version(lang_string)
-    (@lang_string_cache ||= {})[lang_string] ||=
-      Digest::SHA1.hexdigest(bundle lang_string.split('|'))
-  end
-    
-        private
-    
-        # doing this as early as possible so we have an
-    # accurate counter
-    if queue_start = env['HTTP_X_REQUEST_START']
-      queue_start = queue_start.split('t=')[1].to_f
-      queue_time = (Time.now.to_f - queue_start)
-      env['REQUEST_QUEUE_SECONDS'] = queue_time
+    class ContentSecurityPolicy
+  class Middleware
+    def initialize(app)
+      @app = app
     end
     
-          # Yields the supplied block, responding to any rate limit errors.
-      #
-      # The exact strategy used for handling rate limiting errors depends on
-      # whether we are running in parallel mode or not. For more information see
-      # `#rate_or_wait_for_rate_limit`.
-      def with_rate_limit
-        return yield unless rate_limiting_enabled?
-    
-            def sidekiq_worker_class
-          ImportIssueWorker
-        end
-    
-                rows << {
-              label_id: label_id,
-              target_id: target_id,
-              target_type: issue.issuable_type,
-              created_at: time,
-              updated_at: time
-            }
-          end
-    
-            def collection_method
-          :lfs_objects
-        end
-    
-          private
-    
-          # Returns true if the given object has already been imported, false
-      # otherwise.
-      #
-      # object - The object to check.
-      def already_imported?(object)
-        id = id_for_already_imported_cache(object)
-    
-            expose_attribute :id, :login
-    
-        def initialize
-      @pages = {}
+          script_srcs = parse(policy)['script-src']
+      expect(script_srcs).to include(*%w[
+        https://s3-cdn.com/assets/
+        https://s3-cdn.com/brotli_asset/
+        https://cdn.com/highlight-js/
+        https://cdn.com/javascripts/
+        https://cdn.com/plugins/
+        https://cdn.com/theme-javascripts/
+        http://test.localhost/extra-locales/
+      ])
     end
-    
-    require 'active_support/subscriber'
-    
-            css('h1 + code').each do |node|
-          node.before('<p></p>')
-          while node.next_element.name == 'code'
-            node.previous_element << ' '
-            node.previous_element << node.next_element
-          end
-          node.previous_element.prepend_child(node)
-        end
-    
-      # staged_path not available in Installer/Uninstall Stanza, workaround by nesting with preflight/postflight
-  # see https://github.com/Homebrew/homebrew-cask/pull/8887
-  # and https://github.com/Homebrew/homebrew-cask-versions/pull/296
-    
-          def page_dir
-        @page_dir
-      end
-    
-      test 'extract destination file name in case of path renaming' do
-    view = Precious::Views::LatestChanges.new
-    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
-    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
   end
     
-        # Title is based on file name when h1_title is false.
-    actual = @view.title
-    assert_equal 'H1', title
-  end
-end
-
+        api = !!(@env[API_KEY_ENV]) || !!(@env[USER_API_KEY_ENV])
     
-    Given /^I update my user view to include the attachment$/ do
-  steps %{
-    Given I overwrite 'app/views/users/show.html.erb' with:
-      '''
-      <p>Name: <%= @user.name %></p>
-      <p>Attachment: <%= image_tag @user.attachment.url %></p>
-      '''
-  }
-end
+              response.commit!
     
-    Given /^(?:|I )am on (.+)$/ do |page_name|
-  visit path_to(page_name)
-end
+        if user_token.auth_token != token && user_token.prev_auth_token == token && user_token.auth_token_seen
+      changed_rows = UserAuthToken
+        .where('rotated_at < ?', 1.minute.ago)
+        .where(id: user_token.id, prev_auth_token: token)
+        .update_all(auth_token_seen: false)
     
-        # The aspect ratio of the dimensions.
-    def aspect
-      width / height
-    end
+      task :all => [:readmes, :index]
+end
