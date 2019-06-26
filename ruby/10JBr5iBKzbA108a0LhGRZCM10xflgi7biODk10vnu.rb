dc34cd8abafa30b6fd4a1e301fbc19c8a068f895
SUNsung <weatherius@gmail.com>
2019-06-26 21:00:21 +0300

    Rewrite [10JBr5iBKzbA108a0LhGRZCM10xflgi7biODk10vnu.rb] File in: 2019-06-26 21:00:21

--- ruby/10JBr5iBKzbA108a0LhGRZCM10xflgi7biODk10vnu.rb
+++ ruby/10JBr5iBKzbA108a0LhGRZCM10xflgi7biODk10vnu.rb
@@ -1,172 +1,172 @@
 
         
-          url = File.dirname(url)
-  url == FORWARD_SLASH ? url : '#{url}/'
-end
-    
-    # No trailing slash
-Benchmark.ips do |x|
-  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
-  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
-  x.compare!
-end
-
-    
-            # rubocop:disable Metrics/AbcSize
-        def process(args, opts)
-          if !args || args.empty?
-            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
-          end
-    
-    end
-
-    
-    describe Rufus::Scheduler do
-  before :each do
-    Agent.delete_all
-    
-      def remove_duplicates
-    where = 'WHERE s1.user_id = s2.user_id AND s1.shareable_id = s2.shareable_id AND '\
-      's1.shareable_type = s2.shareable_type AND s1.id > s2.id'
-    if AppConfig.postgres?
-      execute('DELETE FROM share_visibilities AS s1 USING share_visibilities AS s2 #{where}')
-    else
-      execute('DELETE s1 FROM share_visibilities s1, share_visibilities s2 #{where}')
-    end
-  end
-end
-
-    
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
-    end
-  end
-end
-
-    
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
-    end
-  end
-end
-
-    
-        it 'redirects to /stream for a mobile user' do
-      request.headers['X_MOBILE_DEVICE'] = true
-      post :create, params: {user: {remember_me: '0', username: @user.username, password: 'evankorth'}}
-      expect(response).to be_redirect
-      expect(response.location).to match /^#{stream_url}\??$/
-    end
-  end
-    
-      def run_vagrant_command(command)
-    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
-    return [stdout, stderr] if status.success?
-    raise VagrantSSHCommandError, status
-  end
-end
-    
-          def servers_by_key
-        @servers_by_key ||= {}
-      end
-    
-          # The body of the method definition.
-      #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
-      end
-    
-            def key_delta(alignment = :left)
-          return 0 if first.same_line?(second)
-          return 0 if keyword_splat? && alignment == :right
-    
-            def swap_range(corrector, range1, range2)
-          src1 = range1.source
-          src2 = range2.source
-          corrector.replace(range1, src2)
-          corrector.replace(range2, src1)
-        end
-      end
-    end
-  end
-end
-
-    
-            def message_for_normal_block(variable, all_arguments)
-          if all_arguments.none?(&:referenced?) &&
-             !define_method_call?(variable)
-            if all_arguments.count > 1
-              'You can omit all the arguments if you don't care about them.'
-            else
-              'You can omit the argument if you don't care about it.'
-            end
-          else
-            message_for_underscore_prefix(variable)
+                  # Go through each VM and yield it!
+          vms.each do |old_vm|
+            # We get a new VM from the environment here to avoid potentially
+            # stale VMs (if there was a config reload on the environment
+            # or something).
+            vm = @env.vms[old_vm.name]
+            yield vm
           end
         end
     
-          def preference_field_options(options)
-        field_options = case options[:type]
-                        when :integer
-                          {
-                            size: 10,
-                            class: 'input_integer form-control'
-                          }
-                        when :boolean
-                          {}
-                        when :string
-                          {
-                            size: 10,
-                            class: 'input_string form-control'
-                          }
-                        when :password
-                          {
-                            size: 10,
-                            class: 'password_string form-control'
-                          }
-                        when :text
-                          {
-                            rows: 15,
-                            cols: 85,
-                            class: 'form-control'
-                          }
-                        else
-                          {
-                            size: 10,
-                            class: 'input_string form-control'
-                          }
-                        end
+            # This method is called if the underlying machine ID changes. Providers
+        # can use this method to load in new data for the actual backing
+        # machine or to realize that the machine is now gone (the ID can
+        # become `nil`). No parameters are given, since the underlying machine
+        # is simply the machine instance given to this object. And no
+        # return value is necessary.
+        def machine_id_changed
+        end
     
-          # Single main menu item
-      def main_menu_item(text, url: nil, icon: nil)
-        link_to url, 'data-toggle': 'collapse', class: 'd-flex w-100 p-3 position-relative align-items-center' do
-          content_tag(:span, nil, class: 'icon icon-#{icon} mr-2') +
-            content_tag(:span, ' #{text}', class: 'text') +
-            content_tag(:span, nil, class: 'icon icon-chevron-left position-absolute')
+        def destroy
+      authorize @email_domain_block, :destroy?
+      @email_domain_block.destroy!
+      log_action :destroy, @email_domain_block
+      redirect_to admin_email_domain_blocks_path, notice: I18n.t('admin.email_domain_blocks.destroyed_msg')
+    end
+    
+      before_action :require_user!
+    
+      def export_data
+    raise 'Override in controller'
+  end
+    
+    class RemoteUnfollowsController < ApplicationController
+  layout 'modal'
+    
+      # body
+  xml.tag!('body') do
+    xml.tag!('outline', text: TITLE, title: TITLE) do
+      blogs.each do |blog|
+        xml.tag!('outline', type: 'rss', text: blog.name, title: blog.name,
+          xmlUrl: blog.rss_url, htmlUrl: blog.web_url)
+      end
+    end
+  end
+end
+    
+    describe ContactsController, :type => :controller do
+  describe '#index' do
+    before do
+      AppConfig.chat.enabled = true
+      @aspect = bob.aspects.create(:name => 'another aspect')
+      bob.share_with alice.person, @aspect
+      bob.share_with eve.person, @aspect
+      sign_in bob, scope: :user
+    end
+    
+          @conv2 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
+             .increase_unread(alice)
+    
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+    describe StreamsController, :type => :controller do
+  include_context :gon
+    
+              current_range = declaration_with_comment(node)
+          previous_range = declaration_with_comment(previous_declaration)
+    
+          def preference_field_for(form, field, options)
+        case options[:type]
+        when :integer
+          form.text_field(field, preference_field_options(options))
+        when :boolean
+          form.check_box(field, preference_field_options(options))
+        when :string
+          form.text_field(field, preference_field_options(options))
+        when :password
+          form.password_field(field, preference_field_options(options))
+        when :text
+          form.text_area(field, preference_field_options(options))
+        else
+          form.text_field(field, preference_field_options(options))
         end
       end
     
-            def load_order_with_lock
-          load_order(true)
-        end
+      desc 'Build all spree gems'
+  task :build do
+    pkgdir = File.expand_path('../pkg', __FILE__)
+    FileUtils.mkdir_p pkgdir
     
-              def shipping_rates
-            result = shipping_rates_service.call(order: spree_current_order)
+              def find_spree_current_order
+            Spree::Api::Dependencies.storefront_current_order_finder.constantize.new.execute(
+              store: spree_current_store,
+              user: spree_current_user,
+              token: order_token,
+              currency: current_currency
+            )
+          end
     
-          @@address_attributes = [
-        :id, :firstname, :lastname, :full_name, :address1, :address2, :city,
-        :zipcode, :phone, :company, :alternative_phone, :country_id, :state_id,
-        :state_name, :state_text
-      ]
+    # and write them both.
+begin
+  output_packages.each do |output_package|
+    output = output_package.to_s
+    output_package.output(output)
+    
+        if attributes[:before_install_given?] or attributes[:after_install_given?]
+      write_scripts
+      args += ['--scripts', scripts_path]
+    end
+    args << output_path
+    
+      def to_s(format=nil)
+    # Default format if nil
+    # git_1.7.9.3-1-amd64.pkg.tar.xz
+    return super(format.nil? ? 'NAME-FULLVERSION-ARCH.EXTENSION' : format)
+  end # def to_s
+    
+        if attributes[:pear_data_dir]
+      logger.info('Setting data_dir', :data_dir => attributes[:pear_data_dir])
+      safesystem('pear', '-c', config, 'config-set', 'data_dir', '#{staging_path(installroot)}/#{attributes[:pear_data_dir]}')
+    end
+    
+        File.write(build_path('build-info'), `pkg_info -X pkg_install | egrep '^(MACHINE_ARCH|OPSYS|OS_VERSION|PKGTOOLS_VERSION)'`)
+    
+          base = staging_path(File.join(attributes[:prefix], '#{platform.platform}/#{platform.target_version || 'default'}'))
+      target = File.join(base, 'files')
+      actions_script = File.join(base, 'install_actions.sh')
+      ::PleaseRun::Installer.install_files(platform, target, false)
+      ::PleaseRun::Installer.write_actions(platform, actions_script)
+    end
+    
+        # Unpack the tarball to the staging path
+    args = ['-cf', output_path, '-C', staging_path]
+    tar_compression_flag(output_path).tap do |flag|
+      args << flag unless flag.nil?
+    end
+    args << '.'
+    
+      private
+    
+          def default_project(name)
+        '#{directory}/#{name}.yml'
+      end
+    
+        it 'lists the commands' do
+      out, _err = capture_io { cli.start }
+      expected = %w(commands
+                    completions
+                    new
+                    edit
+                    open
+                    start
+                    stop
+                    local
+                    debug
+                    copy
+                    delete
+                    implode
+                    version
+                    doctor
+                    list)
+      expect(out).to eq '#{expected.join('\n')}\n'
+    end
+  end
