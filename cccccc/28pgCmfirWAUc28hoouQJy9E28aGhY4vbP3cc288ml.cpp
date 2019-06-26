766ded1f7dd0efcb06ba5e3c088323cbe0826cdc
SUNsung <weatherius@gmail.com>
2019-06-27 02:43:08 +0300

    Rewrite [28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp] File in: 2019-06-27 02:43:08

--- cccccc/28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp
+++ cccccc/28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp
@@ -1,295 +1,102 @@
 
         
-        class Clipboard : public Base {
- public:
-  Clipboard(int id,
-            const base::WeakPtr<DispatcherHost>& dispatcher_host,
-            const base::DictionaryValue& option);
-  ~Clipboard() override;
-    }
+        Status WriteBatchBase::DeleteRange(ColumnFamilyHandle* column_family,
+                                   const SliceParts& begin_key,
+                                   const SliceParts& end_key) {
+  std::string begin_key_buf, end_key_buf;
+  Slice begin_key_slice(begin_key, &begin_key_buf);
+  Slice end_key_slice(end_key, &end_key_buf);
+  return DeleteRange(column_family, begin_key_slice, end_key_slice);
+}
     
-    #endif  // CONTENT_NW_SRC_API_MENU_MENU_DELEGATE_H_
+      // When an actor (column family) requests a stop token, all writes will be
+  // stopped until the stop token is released (deleted)
+  std::unique_ptr<WriteControllerToken> GetStopToken();
+  // When an actor (column family) requests a delay token, total delay for all
+  // writes to the DB will be controlled under the delayed write rate. Every
+  // write needs to call GetDelay() with number of bytes writing to the DB,
+  // which returns number of microseconds to sleep.
+  std::unique_ptr<WriteControllerToken> GetDelayToken(
+      uint64_t delayed_write_rate);
+  // When an actor (column family) requests a moderate token, compaction
+  // threads will be increased
+  std::unique_ptr<WriteControllerToken> GetCompactionPressureToken();
+    
+    #include 'rocksdb/db.h'
+    
+    
+    {
+    {  // Returns the approximate memory usage of different types in the input
+  // list of DBs and Cache set.  For instance, in the output map
+  // usage_by_type, usage_by_type[kMemTableTotal] will store the memory
+  // usage of all the mem-tables from all the input rocksdb instances.
+  //
+  // Note that for memory usage inside Cache class, we will
+  // only report the usage of the input 'cache_set' without
+  // including those Cache usage inside the input list 'dbs'
+  // of DBs.
+  static Status GetApproximateMemoryUsageByType(
+      const std::vector<DB*>& dbs,
+      const std::unordered_set<const Cache*> cache_set,
+      std::map<MemoryUtil::UsageType, uint64_t>* usage_by_type);
+};
+}  // namespace rocksdb
+#endif  // !ROCKSDB_LITE
 
     
+    public:
+    static const char DEFAULT_NAMESPACE_SEPARATOR = ':';
+    static const char DEFAULT_KEYS_GLOB_PATTERN = '*';
+    static const bool DEFAULT_LUA_KEYS_LOADING = false;
+    static const uint DEFAULT_DB_SCAN_LIMIT = 20;
     
-    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.setListSync', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwClipboardSetListSyncFunction);
+    protected:
+    int roleKey(const QByteArray &role) const;
+    
+    
+    {  bool isIndexValid(const QModelIndex& index) const;
 };
     
-    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
- public:
-  NwMenuGetNSStringWithFixupFunction(){}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    
- protected:
-  ~NwMenuGetNSStringWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
-};
-    
-     protected:
-  ~NwObjDestroyFunction() override;
-    
-      // implement nw.Screen.startMonitor()
-  class NwScreenStartMonitorFunction : public NWSyncExtensionFunction {
-  public:
-    NwScreenStartMonitorFunction();
-    bool RunNWSync(base::ListValue* response, std::string* error) override;
+        class AutocompleteModel : public BaseListModel
+    {
+        Q_OBJECT
     }
     
-    namespace caffe2 {
-    }
+    private slots:
+    void requestFinished(QNetworkReply*);
     
-    Each feature has fixed lengths which are passed as lengths argument and a
-separate tensor will be produced for each feature.
-i.e. DATA.dim(1) = len(lengths) = NumOuptuts.
+      auto animal = sample::GetAnimal(builder.GetBufferPointer());
     
-    )DOC')
-    .Arg(
-        'values',
-        '*(type depends on dtype, Required=True)* The value of the elements to go in the *output* tensor.',
-        true /* required */)
-    .Arg(
-        'dtype',
-        'The data type for the elements of the output tensor. Strictly must be one of the types from DataType enum in TensorProto.')
-    .Arg(
-        'shape',
-        '*(type: [int])* Desired shape of the *output* tensor.')
-    .Arg(
-        'extra_shape',
-        '*(type: [int])* The additional dimensions appended at the end of the *shape* indicated by the input blob. Cannot set the *extra_shape* argument when there is no input blob.')
-    .Arg(
-        'input_as_shape',
-        '*(type: bool; default: False)* set to *True* to use the *input* as shape. First, input must be in CPU context.')
-    .Input(
-        0,
-        'input',
-        '(Optional) 1D tensor specifying the shape of the output. Must be used with *input_as_shape=True*')
-    .Output(
-        0,
-        'output',
-        'Output tensor with desired dimension filled with specified data. If the shape argument is set, this is the shape specified, and if the *input* exists and *input_as_shape=True*, it is the shape specified by the *input* tensor.')
-    .TensorInferenceFunction(FillerTensorInference<>);
+      // We now have a FlatBuffer we can store on disk or send over a network.
     
-    				cmderOptions.cmderCfgRoot = cmderCfgRoot;
+    #include 'monster_generated.h'  // Already includes 'flatbuffers/flatbuffers.h'.
     
-    // TODO(jorlow): Many of these belong more in the environment class rather than
-//               here. We should try moving them and see if it affects perf.
+    #ifdef _WIN32
+  #define TEST_ASSERT_FUNC(exp) TestEq(exp, true, #exp, __FILE__, __LINE__, __FUNCTION__)
+  #define TEST_EQ_FUNC(exp, val) TestEq(exp, val, #exp, __FILE__, __LINE__, __FUNCTION__)
+#else
+  #define TEST_ASSERT_FUNC(exp) TestEq(exp, true, #exp, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+  #define TEST_EQ_FUNC(exp, val) TestEq(exp, val, #exp, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+#endif
     
-    namespace leveldb {
-    }
-    
-      // Delete everything
-  for (int i = 0; i < kCount; i++) {
-    ASSERT_OK(db_->Delete(WriteOptions(), Key(i)));
+      if (flatbuffers::FileExists(filename.c_str())) {
+    std::cout << 'File ' << filename << ' exists.\n';
+  } else {
+    std::cout << 'File ' << filename << ' does not exist.\n';
+    return EXIT_FAILURE;
   }
-  ASSERT_OK(dbi->TEST_CompactMemTable());
     
-      // Implementations of the DB interface
-  virtual Status Put(const WriteOptions&, const Slice& key, const Slice& value);
-  virtual Status Delete(const WriteOptions&, const Slice& key);
-  virtual Status Write(const WriteOptions& options, WriteBatch* updates);
-  virtual Status Get(const ReadOptions& options, const Slice& key,
-                     std::string* value);
-  virtual Iterator* NewIterator(const ReadOptions&);
-  virtual const Snapshot* GetSnapshot();
-  virtual void ReleaseSnapshot(const Snapshot* snapshot);
-  virtual bool GetProperty(const Slice& property, std::string* value);
-  virtual void GetApproximateSizes(const Range* range, int n, uint64_t* sizes);
-  virtual void CompactRange(const Slice* begin, const Slice* end);
+    void message_builder_tests();
     
-    TEST(FormatTest, InternalKey_EncodeDecode) {
-  const char* keys[] = {'', 'k', 'hello', 'longggggggggggggggggggggg'};
-  const uint64_t seq[] = {1,
-                          2,
-                          3,
-                          (1ull << 8) - 1,
-                          1ull << 8,
-                          (1ull << 8) + 1,
-                          (1ull << 16) - 1,
-                          1ull << 16,
-                          (1ull << 16) + 1,
-                          (1ull << 32) - 1,
-                          1ull << 32,
-                          (1ull << 32) + 1};
-  for (int k = 0; k < sizeof(keys) / sizeof(keys[0]); k++) {
-    for (int s = 0; s < sizeof(seq) / sizeof(seq[0]); s++) {
-      TestKey(keys[k], seq[s], kTypeValue);
-      TestKey('hello', 1, kTypeDeletion);
-    }
+      static const char *test_locale() {
+    return one_time_init_.has_locale_ ? nullptr
+                                      : one_time_init_.test_locale_.c_str();
   }
-}
     
-    #include 'db/dbformat.h'
-#include 'port/port.h'
-#include 'util/logging.h'
-#include 'util/testharness.h'
-    
-    
-    {        // Update font info
-        FT_Size_Metrics metrics = Face->size->metrics;
-        Info.PixelHeight = (uint32_t)pixel_height;
-        Info.Ascender = (float)FT_CEIL(metrics.ascender);
-        Info.Descender = (float)FT_CEIL(metrics.descender);
-        Info.LineSpacing = (float)FT_CEIL(metrics.height);
-        Info.LineGap = (float)FT_CEIL(metrics.height - metrics.ascender + metrics.descender);
-        Info.MaxAdvanceWidth = (float)FT_CEIL(metrics.max_advance);
-    }
-    
-    struct GLFWwindow;
-    
-    // Main code
-int main(int, char**)
-{
-    // Create application window
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T('ImGui Example'), NULL };
-    ::RegisterClassEx(&wc);
-    HWND hwnd = ::CreateWindow(wc.lpszClassName, _T('Dear ImGui DirectX12 Example'), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
-    }
-    
-        // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
-    
-    Speed* Speed::create(ActionInterval* action, float speed)
-{
-    Speed *ret = new (std::nothrow) Speed();
-    if (ret && ret->initWithAction(action, speed))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    CC_SAFE_DELETE(ret);
-    return nullptr;
-}
-    
-        /** Replace the interior action.
-     *
-     * @param action The new action, it will replace the running action.
-     */
-    void setInnerAction(ActionInterval *action);
-    /** Return the interior action.
-     *
-     * @return The interior action.
-     */
-    ActionInterval* getInnerAction() const { return _innerAction; }
-    
-    
-    {    for (i = 0; i < (_gridSize.width+1); ++i)
-    {
-        for (j = 0; j < (_gridSize.height+1); ++j)
-        {
-            Vec3 v = getOriginalVertex(Vec2(i, j));
-            Vec2 vect = _position - Vec2(v.x,v.y);
-            float r = vect.getLength();
-            
-            if (r < _radius)
-            {
-                r = _radius - r;
-                float rate = powf(r / _radius, 2);
-                v.z += (sinf( time*(float)M_PI * _waves * 2 + r * 0.1f) * _amplitude * _amplitudeRate * rate);
-            }
-            
-            setVertex(Vec2(i, j), v);
-        }
-    }
-}
-    
-    CallFuncN * CallFuncN::create(const std::function<void(Node*)> &func)
-{
-    auto ret = new (std::nothrow) CallFuncN();
-    }
-    
-        /** Set the selector target.
-     *
-     * @param sel The selector target.
-     */
-    void setTargetCallback(Ref* sel)
-    {
-        if (sel != _selectorTarget)
-        {
-            CC_SAFE_RETAIN(sel);
-            CC_SAFE_RELEASE(_selectorTarget);
-            _selectorTarget = sel;
-        }
-    }
-    //
-    // Overrides
-    //
-    /**
-     * @param time In seconds.
-     */
-    virtual void update(float time) override;
-    virtual CallFunc* reverse() const override;
-    virtual CallFunc* clone() const override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    CallFunc()
-    : _selectorTarget(nullptr)
-    , _callFunc(nullptr)
-    , _function(nullptr)
-    {
-    }
-    virtual ~CallFunc();
-    
-    
-    {    if (element->actions->num == 0)
-    {
-        if (_currentTarget == element)
-        {
-            _currentTargetSalvaged = true;
-        }
-        else
-        {
-            deleteHashElement(element);
-        }
-    }
-}
-    
-    
-    {// end of actions group
-/// @}
-    
-    float Animation::getDuration(void) const
-{
-    return _totalDelayUnits * _delayPerUnit;
-}
-    
-        // shader stuff
-    setGLProgramState(GLProgramState::getOrCreateWithGLProgramName(GLProgram::SHADER_NAME_POSITION_TEXTURE_COLOR_NO_MVP, texture));
-    
-    #include '2d/CCNode.h'
-#include 'base/CCProtocols.h'
-#include 'base/ccTypes.h'
-#include 'renderer/CCQuadCommand.h'
-    
-    
-    {    //turn the result into simply polygon (AKA, fix overlap)
-    
-    //clamp into the specified rect
-    ClipperLib::Clipper cl;
-    cl.StrictlySimple(true);
-    cl.AddPath(p->Contour, ClipperLib::ptSubject, true);
-    //create the clipping rect
-    ClipperLib::Path clamp;
-    clamp.push_back(ClipperLib::IntPoint(0, 0));
-    clamp.push_back(ClipperLib::IntPoint(rect.size.width/_scaleFactor * PRECISION, 0));
-    clamp.push_back(ClipperLib::IntPoint(rect.size.width/_scaleFactor * PRECISION, rect.size.height/_scaleFactor * PRECISION));
-    clamp.push_back(ClipperLib::IntPoint(0, rect.size.height/_scaleFactor * PRECISION));
-    cl.AddPath(clamp, ClipperLib::ptClip, true);
-    cl.Execute(ClipperLib::ctIntersection, out);
-    
-    std::vector<Vec2> outPoints;
-    ClipperLib::PolyNode* p2 = out.GetFirst();
-    while(p2->IsHole()){
-        p2 = p2->GetNext();
-    }
-    for(const auto& pt : p2->Contour)
-    {
-        outPoints.push_back(Vec2(pt.X/PRECISION, pt.Y/PRECISION));
-    }
-    return outPoints;
-}
+      // to ensure it is correct, we now generate text back from the binary,
+  // and compare the two:
+  std::string jsongen1;
+  if (!GenerateText(parser1, parser1.builder_.GetBufferPointer(), &jsongen1)) {
+    printf('Couldn't serialize parsed data to JSON!\n');
+    return 1;
+  }
