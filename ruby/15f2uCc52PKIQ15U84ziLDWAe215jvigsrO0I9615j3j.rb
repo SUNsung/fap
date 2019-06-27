d783d52b3edb6bb4f59164436c753eae0b22f2cd
SUNsung <weatherius@gmail.com>
2019-06-27 15:23:14 +0300

    Rewrite [15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb] File in: 2019-06-27 15:23:14

--- ruby/15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb
+++ ruby/15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb
@@ -1,105 +1,28 @@
 
         
-              if period_type == :all
-        DB.exec <<~SQL
-          UPDATE user_stats s
-          SET likes_given         = d.likes_given,
-              likes_received      = d.likes_received,
-              topic_count         = d.topic_count,
-              post_count          = d.post_count
-    
-        @omniauth.before_request_phase do |env|
-      # If the user is trying to reconnect to an existing account, store in session
-      request = ActionDispatch::Request.new(env)
-      request.session[:auth_reconnect] = !!request.params['reconnect']
-    end
-  end
-    
-          script_srcs = parse(policy)['script-src']
-      expect(script_srcs).to include(*%w[
-        https://s3-cdn.com/assets/
-        https://s3-cdn.com/brotli_asset/
-        https://cdn.com/highlight-js/
-        https://cdn.com/javascripts/
-        https://cdn.com/plugins/
-        https://cdn.com/theme-javascripts/
-        http://test.localhost/extra-locales/
-      ])
-    end
-  end
-    
-          if api_key.allowed_ips.present? && !api_key.allowed_ips.any? { |ip| ip.include?(request.ip) }
-        Rails.logger.warn('[Unauthorized API Access] username: #{api_username}, IP address: #{request.ip}')
-        return nil
-      end
-    
-      def self.distance(loc1, loc2)
-    lat1_rad, lon1_rad = loc1[0] * RAD_PER_DEG, loc1[1] * RAD_PER_DEG
-    lat2_rad, lon2_rad = loc2[0] * RAD_PER_DEG, loc2[1] * RAD_PER_DEG
-    
-        def execute(command: nil, target_object: nil)
-      action_name = command.method_name
-      action_class_ref = class_ref_for_action(named: action_name)
-      parameter_map = {}
-      closure_argument_value = nil
-    
-          it 'adds docset_feed_url param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            docset_feed_url: 'http://docset-feed-url.com'
-          )
-        end').runner.execute(:test)
-    
-          it 'builds with multiple dependencies' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              command: 'build',
-              dependencies: ['TestDependency1', 'TestDependency2']
-            )
-          end').runner.execute(:test)
-    
-          it 'handles extension and extensions parameters correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', extension: 'rb', extensions: ['m', 'h'])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --include=\\*.{rb,m,h}')
-      end
-    
-        describe 'shell escaping' do
-      let(:keychain_name) { 'keychain with spaces.keychain' }
-      let(:shell_escaped_name) { keychain_name.shellescape }
-      let(:name_regex) { Regexp.new(Regexp.escape(shell_escaped_name)) }
-    
-        attr_accessor :name, :type, :path
-    
-        def initialize_stubs
-      self.class.stubs.each do |path, block|
-        Typhoeus.stub(url_for(path)).and_return do
-          Typhoeus::Response.new \
-            effective_url: url_for(path),
-            code: 200,
-            headers: { 'Content-Type' => 'text/html' },
-            body: self.instance_exec(&block)
-        end
-      end
+            def insert(index, *names)
+      @filters.insert assert_index(index), *filter_const(names)
     end
     
-          @terminal_width = if !tty?
-        nil
-      elsif ENV['COLUMNS']
-        ENV['COLUMNS'].to_i
-      else
-        `stty size`.scan(/\d+/).last.to_i
-      end
-    rescue
-      @terminal_width = nil
+        def to_json
+      JSON.generate(as_json)
     end
   end
 end
 
+    
+              node.remove_attribute('path')
+          node.remove_attribute('region')
+          node.remove_attribute('linenums')
+          node.remove_attribute('title')
+          node.remove_attribute('language')
+          node.remove_attribute('hidecopy')
+          node.remove_attribute('class')
+        end
+    
+            css('.row-margin', '.ng-cloak').each do |node|
+          node.before(node.children).remove
+        end
     
           def get_type
         if slug.start_with?('guide/')
@@ -116,122 +39,192 @@ end
         end
       end
     
-            css('pre > code').each do |node|
-          node['class'] ||= ''
-          lang = if node['class'].include?('lang-html') || node.content =~ /\A</
-            'html'
-          elsif node['class'].include?('lang-css')
-            'css'
-          elsif node['class'].include?('lang-js') || node['class'].include?('lang-javascript')
-            'javascript'
+            css('header').each do |node|
+          node.before(node.children).remove
+        end
+    
+    
+  #
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
+  }
+    
+              # Encodes the Rex::Proto::Kerberos::CredentialCache::Principal into an String
+          #
+          # @return [String] encoded principal
+          def encode
+            encoded = ''
+            encoded << encode_name_type
+            encoded << [components.length].pack('N')
+            encoded << encode_realm
+            encoded << encode_components
+    
+              # Encodes the type
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_type(type)
+            bn = OpenSSL::BN.new(type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+              # Encodes a Rex::Proto::Kerberos::Model::Checksum into an ASN.1 String
+          #
+          # @return [String]
+          def encode
+            elems = []
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_type], 0, :CONTEXT_SPECIFIC)
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_checksum], 1, :CONTEXT_SPECIFIC)
+    
+              # Decodes the cname field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Rex::Proto::Kerberos::Type::PrincipalName]
+          def decode_cname(input)
+            Rex::Proto::Kerberos::Model::PrincipalName.decode(input.value[0])
           end
-          node.parent['data-language'] = lang if lang
     
-            # This is called as a last-minute hook that allows the configuration
-        # object to finalize itself before it will be put into use. This is
-        # a useful place to do some defaults in the case the user didn't
-        # configure something or so on.
-        #
-        # An example of where this sort of thing is used or has been used:
-        # the 'vm' configuration key uses this to make sure that at least
-        # one sub-VM has been defined: the default VM.
-        #
-        # The configuration object is expected to mutate itself.
-        def finalize!
-          # Default implementation is to do nothing.
-        end
+              # Decodes a Rex::Proto::Kerberos::Model::KrbError from an String
+          #
+          # @param input [String] the input to decode from
+          def decode_string(input)
+            asn1 = OpenSSL::ASN1.decode(input)
     
-            # This contains all the provider plugins by name, and returns
-        # the provider class and options.
-        #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :providers
+        signature0 = (<<~'end;').unpack('m')[0]
+      MEQCIEOTY/hD7eI8a0qlzxkIt8LLZ8uwiaSfVbjX2dPAvN11AiAQdCYx56Fq
+      QdBp1B4sxJoA8jvODMMklMyBKVmudboA6A==
+    end;
+    assert_equal true, p256.verify('SHA256', signature0, data)
+    signature1 = signature0.succ
+    assert_equal false, p256.verify('SHA256', signature1, data)
+  end
     
-            # This is called as a last-minute hook that allows the configuration
-        # object to finalize itself before it will be put into use. This is
-        # a useful place to do some defaults in the case the user didn't
-        # configure something or so on.
-        #
-        # An example of where this sort of thing is used or has been used:
-        # the 'vm' configuration key uses this to make sure that at least
-        # one sub-VM has been defined: the default VM.
-        #
-        # The configuration object is expected to mutate itself.
-        def finalize!
-          # Default implementation is to do nothing.
-        end
-    
-    module Vagrant
-  module Plugin
-    module V2
-      # This is the superclass for all V2 plugins.
-      class Plugin
-        # Special marker that can be used for action hooks that matches
-        # all action sequences.
-        ALL_ACTIONS = :__all_actions__
-    
-            # This is an internal initialize function that should never be
-        # overridden. It is used to initialize some common internal state
-        # that is used in a provider.
-        def _initialize(name, machine)
-          initialize_capabilities!(
-            name.to_sym,
-            { name.to_sym => [Class.new, nil] },
-            Vagrant.plugin('2').manager.provider_capabilities,
-            machine,
-          )
-        end
-      end
+      it 'raises Errno::EAFNOSUPPORT or Errno::EPROTONOSUPPORT when given an invalid address family' do
+    begin
+      UDPSocket.new(666)
+    rescue Errno::EAFNOSUPPORT, Errno::EPROTONOSUPPORT => e
+      [Errno::EAFNOSUPPORT, Errno::EPROTONOSUPPORT].should include(e.class)
+    else
+      raise 'expected Errno::EAFNOSUPPORT or Errno::EPROTONOSUPPORT exception raised'
     end
   end
 end
 
     
-    # Now, add our init-scripts, systemd services, and so on:
-pleaserun = package.convert(FPM::Package::PleaseRun)
-pleaserun.input ['/usr/bin/my-executable', '--foo-from', 'bar']
+    module TestIRB
+  class TestColor < Test::Unit::TestCase
+    CLEAR     = '\e[0m'
+    BOLD      = '\e[1m'
+    UNDERLINE = '\e[4m'
+    REVERSE   = '\e[7m'
+    RED       = '\e[31m'
+    GREEN     = '\e[32m'
+    YELLOW    = '\e[33m'
+    BLUE      = '\e[34m'
+    MAGENTA   = '\e[35m'
+    CYAN      = '\e[36m'
     
-              extension_header[TAR_TYPEFLAG_OFFSET] = 'x'
-          extension_header[TAR_LENGTH_OFFSET_START..TAR_LENGTH_OFFSET_END] = extension_data.length.to_s(8).rjust(12, '0')
-          extension_header = checksum_header(extension_header)
+            def initialize(argv)
+          @use_regex = argv.flag?('regex')
+          @show_all = argv.flag?('show-all')
+          @query = argv.shift_argument
+          @query = @query.gsub('.podspec', '') unless @query.nil?
+          super
+        end
     
-      # Extract name and version from PackageInfo XML
-  def extract_info(package)
-    build_path('expand').tap do |path|
-      doc = REXML::Document.new File.open(File.join(path, 'PackageInfo'))
-      pkginfo_elem = doc.elements['pkg-info']
-      identifier = pkginfo_elem.attribute('identifier').value
-      self.version = pkginfo_elem.attribute('version').value
-      # set name to the last dot element of the identifier
-      self.name = identifier.split('.').last
-      logger.info('inferring name #{self.name} from pkg-id #{identifier}')
+      at_exit do
+    if ENV['KEEP_RUNNING']
+      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
+      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
+    else
+      vagrant_cli_command('destroy -f')
     end
-  end # def extract_info
+  end
     
-        # Licenses could include more than one.
-    # Speaking of just taking the first entry of the field:
-    # A crude thing to do, but I suppose it's better than nothing.
-    # -- Daniel Haskin, 3/24/2015
-    self.license = control['license'][0] || self.license
-    
-      option '--php-bin', 'PHP_BIN',
-    'Specify php executable path if differs from the os used for packaging'
-    
-        # Unpack the tarball to the staging path
-    args = ['-cf', output_path, '-C', staging_path]
-    tar_compression_flag(output_path).tap do |flag|
-      args << flag unless flag.nil?
+        def install_plugin(plugin, load_hooks: true, load_immediately: false)
+      installer.install(plugin,
+                        load_hooks: load_hooks,
+                        load_immediately: load_immediately)
     end
-    args << '.'
     
-    # Use a zip as a package.
-#
-# This provides no metadata. Both input and output are supported.
-class FPM::Package::Zip < FPM::Package
-    
-          def self.has_issues_with_add_symlink?
-        return !::Gem::Package::TarWriter.public_instance_methods.include?(:add_symlink)
+            true
       end
-    end # module TarWriter
-  end # module Issues
-end # module FPM
+    
+    # We use a special :_default_git value so that SCMResolver can tell whether the
+# default has been replaced by the user via `set`.
+set_if_empty :scm, Capistrano::Configuration::SCMResolver::DEFAULT_GIT
+set_if_empty :branch, 'master'
+set_if_empty :deploy_to, -> { '/var/www/#{fetch(:application)}' }
+set_if_empty :tmp_dir, '/tmp'
+    
+    describe Tmuxinator::Pane do
+  let(:klass) { described_class }
+  let(:instance) { klass.new(index, project, window, *commands) }
+  # let(:index) { 'vim' }
+  # let(:project) { 0 }
+  # let(:tab) { nil }
+  # let(:commands) { nil }
+  let(:index) { 0 }
+  let(:project) { double }
+  let(:window) { double }
+  let(:commands) { ['vim', 'bash'] }
+    
+    describe Tmuxinator::Window do
+  let(:project) { double }
+  let(:panes) { ['vim', nil, 'top'] }
+  let(:window_name) { 'editor' }
+  let(:synchronize) { false }
+  let(:yaml) do
+    {
+      window_name => {
+        'pre' => [
+          'echo 'I get run in each pane.  Before each pane command!'',
+          nil
+        ],
+        'synchronize' => synchronize,
+        'layout' => 'main-vertical',
+        'panes' => panes
+      }
+    }
+  end
+  let(:yaml_root) do
+    {
+      'editor' => {
+        'root' => '/project/override',
+        'root?' => true,
+        'pre' => [
+          'echo 'I get run in each pane.  Before each pane command!'',
+          nil
+        ],
+        'layout' => 'main-vertical',
+        'panes' => panes
+      }
+    }
+  end
+    
+          def new_project_with_session(name, session)
+        if Tmuxinator::Config.version < 1.6
+          raise 'Creating projects from sessions is unsupported\
+            for tmux version 1.5 or lower.'
+        end
+    
+      describe '#name' do
+    it { expect(instance.name).to eq 'wemux' }
+  end
