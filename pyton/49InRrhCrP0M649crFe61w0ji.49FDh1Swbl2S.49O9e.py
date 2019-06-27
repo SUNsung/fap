68950e5e3ca244d24e2b6545d693644cd784caa4
SUNsung <weatherius@gmail.com>
2019-06-27 05:13:35 +0300

    Rewrite [49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py] File in: 2019-06-27 05:13:34

--- pyton/49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py
+++ pyton/49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py
@@ -1,54 +1,204 @@
 
         
-            print('-' * 20)
+        
+class Transaction(object):
     
-    #
-# Function used to calculate result
-#
     
-            print('Test: One element stack')
-        sorted_stack = self.get_sorted_stack(stack, [1])
-        assert_equal(sorted_stack.pop(), 1)
+class HitCounts(MRJob):
     
-    		print('Success: test_max_min_nodes')
+        def bfs(self, source, dest):
+        if source is None:
+            return False
+        queue = deque()
+        queue.append(source)
+        source.visit_state = State.visited
+        while queue:
+            node = queue.popleft()
+            print(node)
+            if dest is node:
+                return True
+            for adjacent_node in node.adj_nodes.values():
+                if adjacent_node.visit_state == State.unvisited:
+                    queue.append(adjacent_node)
+                    adjacent_node.visit_state = State.visited
+        return False
     
-            # N.B. np.random.randint draws from [A, B) while random.randint draws from [A,B]
-        val = self.np_random.randint(0, self.n - 1)
-        if val == action == 0:
-            reward = self.n - 2.0
-        elif val != 0 and action != 0 and val % 2 == action % 2:
-            reward = 1.0
-        else:
-            reward = -1.0
-        return 0, reward, False, {}
+        def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
     
-            next_id = self.generate_next_id()
-        self.closeables[next_id] = closeable
-        return next_id
     
-    def create_seed(a=None, max_bytes=8):
-    '''Create a strong random seed. Otherwise, Python 2 would seed using
-    the system time, which might be non-robust especially in the
-    presence of concurrency.
+class QuadraticProbing(HashTable):
+    '''
+        Basic Hash Table example with open addressing using Quadratic Probing 
+    '''
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
     
-    def add_new_rollouts(spec_ids, overwrite):
-    environments = [spec for spec in envs.registry.all() if spec._entry_point is not None]
-    if spec_ids:
-        environments = [spec for spec in environments if spec.id in spec_ids]
-        assert len(environments) == len(spec_ids), 'Some specs not found'
-    with open(ROLLOUT_FILE) as data_file:
-        rollout_dict = json.load(data_file)
-    modified = False
-    for spec in environments:
-        if not overwrite and spec.id in rollout_dict:
-            logger.debug('Rollout already exists for {}. Skipping.'.format(spec.id))
-        else:
-            modified = update_rollout_dict(spec, rollout_dict) or modified
     
-    def key_press(key, mod):
-    global human_agent_action, human_wants_restart, human_sets_pause
-    if key==0xff0d: human_wants_restart = True
-    if key==32: human_sets_pause = not human_sets_pause
-    a = int( key - ord('0') )
-    if a <= 0 or a >= ACTIONS: return
-    human_agent_action = a
+        '''
+        loop over all possible splits for the decision tree. find the best split.
+        if no split exists that is less than 2 * error for the entire array
+        then the data set is not split and the average for the entire array is used as the predictor
+        '''
+        for i in range(len(X)):
+            if len(X[:i]) < self.min_leaf_size:
+                continue
+            elif len(X[i:]) < self.min_leaf_size:
+                continue
+            else:
+                error_left = self.mean_squared_error(X[:i], np.mean(y[:i]))
+                error_right = self.mean_squared_error(X[i:], np.mean(y[i:]))
+                error = error_left + error_right
+                if error < min_error:
+                    best_split = i
+                    min_error = error
+    
+    # Mock test below
+if False: # change to true to run this test case.
+    import sklearn.datasets as ds
+    dataset = ds.load_iris()
+    k = 3
+    heterogeneity = []
+    initial_centroids = get_initial_centroids(dataset['data'], k, seed=0)
+    centroids, cluster_assignment = kmeans(dataset['data'], k, initial_centroids, maxiter=400,
+                                        record_heterogeneity=heterogeneity, verbose=True)
+    plot_heterogeneity(heterogeneity, k)
+
+    
+        Those are used for calculating differences between
+    predicted values and actual values.
+    
+    
+@pytest.fixture
+def httpbin(httpbin):
+    return prepare_url(httpbin)
+    
+        def run(self):
+        try:
+            self.server_sock = self._create_socket_and_bind()
+            # in case self.port = 0
+            self.port = self.server_sock.getsockname()[1]
+            self.ready_event.set()
+            self._handle_requests()
+    
+    _proxy_combos = []
+for prefix, schemes in _schemes_by_var_prefix:
+    for scheme in schemes:
+        _proxy_combos.append(('{}_proxy'.format(prefix), scheme))
+    
+            with Server.basic_response_server(wait_to_close_event=block_server) as (host, port):
+            sock = socket.socket()
+            sock.connect((host, port))
+            sock.sendall(b'send something')
+            time.sleep(2.5)
+            sock.sendall(b'still alive')
+            block_server.set()  # release server block
+    
+    import pytest
+from requests import compat
+from requests.cookies import RequestsCookieJar
+from requests.structures import CaseInsensitiveDict
+from requests.utils import (
+    address_in_network, dotted_netmask, extract_zipped_paths,
+    get_auth_from_url, _parse_content_type_header, get_encoding_from_headers,
+    get_encodings_from_content, get_environ_proxies,
+    guess_filename, guess_json_utf, is_ipv4_address,
+    is_valid_cidr, iter_slices, parse_dict_header,
+    parse_header_links, prepend_scheme_if_needed,
+    requote_uri, select_proxy, should_bypass_proxies, super_len,
+    to_key_val_list, to_native_string,
+    unquote_header_value, unquote_unreserved,
+    urldefragauth, add_dict_to_cookiejar, set_environ)
+from requests._internal_utils import unicode_is_ascii
+    
+    # If false, no module index is generated.
+# texinfo_domain_indices = True
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
+    
+        def long_desc(self):
+        return ('Edit a spider using the editor defined in the EDITOR environment'
+                ' variable or else the EDITOR setting')
+    
+        def post_process(self, output):
+        occurrences = 0
+        for x in output:
+            if isinstance(x, self.obj_type):
+                occurrences += 1
+    
+    
+class ScrapyHTTPClientFactory(HTTPClientFactory):
+    '''Scrapy implementation of the HTTPClientFactory overwriting the
+    serUrl method to make use of our Url object that cache the parse
+    result.
+    '''
+    
+    
+class AjaxCrawlMiddleware(object):
+    '''
+    Handle 'AJAX crawlable' pages marked as crawlable via meta tag.
+    For more info see https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
+    '''
+    
+        def _debug_cookie(self, request, spider):
+        if self.debug:
+            cl = [to_native_str(c, errors='replace')
+                  for c in request.headers.getlist('Cookie')]
+            if cl:
+                cookies = '\n'.join('Cookie: {}\n'.format(c) for c in cl)
+                msg = 'Sending cookies to: {}\n{}'.format(request, cookies)
+                logger.debug(msg, extra={'spider': spider})
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    def _worker(executor_reference, work_queue):
+    try:
+        while True:
+            work_item = work_queue.get(block=True)
+            if work_item is not None:
+                work_item.run()
+                continue
+            executor = executor_reference()
+            # Exit if:
+            #   - The interpreter is shutting down OR
+            #   - The executor that owns the worker has been collected OR
+            #   - The executor that owns the worker has been shutdown.
+            if _shutdown or executor is None or executor._shutdown:
+                # Notice other workers
+                work_queue.put(None)
+                return
+            del executor
+    except BaseException:
+        _base.LOGGER.critical('Exception in worker', exc_info=True)
+    
+        def test_pending_calls_race(self):
+        # Issue #14406: multi-threaded race condition when waiting on all
+        # futures.
+        event = threading.Event()
+        def future_func():
+            event.wait()
+        oldswitchinterval = sys.getcheckinterval()
+        sys.setcheckinterval(1)
+        try:
+            fs = set(self.executor.submit(future_func) for i in range(100))
+            event.set()
+            futures.wait(fs, return_when=futures.ALL_COMPLETED)
+        finally:
+            sys.setcheckinterval(oldswitchinterval)
