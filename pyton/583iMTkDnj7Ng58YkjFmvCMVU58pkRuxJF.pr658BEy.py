32ecca4da4ea5661b2c1c37c0bb9a57aa0e590e8
SUNsung <weatherius@gmail.com>
2019-06-28 00:56:20 +0300

    Rewrite [583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py] File in: 2019-06-28 00:56:19

--- pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
+++ pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
@@ -1,216 +1,237 @@
 
         
-            objects = BaseSessionManager()
+            def __hash_double_function(self, key, data, increment):
+        return (increment * self.__hash_function_2(key, data)) % self.size_table
     
-    
-class SessionManager(BaseSessionManager):
-    use_in_migrations = True
-    
-        if section is not None:
-        if section not in sitemaps:
-            raise Http404('No sitemap available for section: %r' % section)
-        maps = [sitemaps[section]]
-    else:
-        maps = sitemaps.values()
-    page = request.GET.get('p', 1)
-    
-        # Retrieve any AWS settings from the environment.
-    region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module)
-    
-        json_output = {}
-    if mod_params['view'] == 'topics':
-        json_output['topics'] = list_func(pubsub_client.list_topics())
-    elif mod_params['view'] == 'subscriptions':
-        if mod_params['topic']:
-            t = pubsub_client.topic(mod_params['topic'])
-            json_output['subscriptions'] = list_func(t.list_subscriptions())
-        else:
-            json_output['subscriptions'] = list_func(pubsub_client.list_subscriptions())
-    
-        def __init__(self):
-    
-    
-DOCUMENTATION = '''
----
-module: airbrake_deployment
-version_added: '1.2'
-author: 'Bruce Pennypacker (@bpennypacker)'
-short_description: Notify airbrake about app deployments
-description:
-   - Notify airbrake about app deployments (see http://help.airbrake.io/kb/api-2/deploy-tracking)
-options:
-  token:
-    description:
-      - API token.
-    required: true
-  environment:
-    description:
-      - The airbrake environment name, typically 'production', 'staging', etc.
-    required: true
-  user:
-    description:
-      - The username of the person doing the deployment
-    required: false
-  repo:
-    description:
-      - URL of the project repository
-    required: false
-  revision:
-    description:
-      - A hash, number, tag, or other identifier showing what revision was deployed
-    required: false
-  url:
-    description:
-      - Optional URL to submit the notification to. Use to send notifications to Airbrake-compliant tools like Errbit.
-    required: false
-    default: 'https://airbrake.io/deploys.txt'
-    version_added: '1.5'
-  validate_certs:
-    description:
-      - If C(no), SSL certificates for the target url will not be validated. This should only be used
-        on personally controlled sites using self-signed certificates.
-    required: false
-    default: 'yes'
-    type: bool
-    
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.six.moves.urllib.parse import urlencode
-from ansible.module_utils._text import to_native
-from ansible.module_utils.urls import fetch_url
-    
-    import re
-from ansible.module_utils.basic import AnsibleModule
-    
-    from ..common import *
-    
-    #----------------------------------------------------------------------
-def makeMimi(upid):
-    '''From http://cdn37.atwikiimg.com/sitescript/pub/dksitescript/FC2.site.js
-    Also com.hps.util.fc2.FC2EncrptUtil.makeMimiLocal
-    L110'''
-    strSeed = 'gGddgPfeaf_gzyr'
-    prehash = upid + '_' + strSeed
-    return md5(prehash.encode('utf-8')).hexdigest()
-    
-        url = list(set([
-        unicodize(str.replace(i, '\\/', '/'))
-        for i in re.findall(r'<meta property='og:video:secure_url' content='(.*?)'>', html)
-    ]))
-    
-    
-site_info = 'huomao.com'
-download = huomaotv_download
-download_playlist = playlist_not_supported('huomao')
+            return new_key
 
     
-        print_info(site_info, title, ext, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge = merge)
+    This is a pure Python implementation of Dynamic Programming solution to the longest_sub_array problem.
     
-        stream_types = [
-        {'id': 'video'},
-        {'id': 'audio'},
-        {'id': 'slides'}
-    ]
+        for i in range(1, s+1):
+        dp[0][i] = False
+    
+    	startLength = len(bitString)
+	bitString += '1'
+	while len(bitString) % 512 != 448:
+		bitString += '0'
+	lastPart = format(startLength,'064b')
+	bitString += rearrange(lastPart[32:]) + rearrange(lastPart[:32]) 
+	return bitString
+    
+    def revise_centroids(data, k, cluster_assignment):
+    new_centroids = []
+    for i in range(k):
+        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
+        member_data_points = data[cluster_assignment==i]
+        # Compute the mean of the data points. Fill in the blank (RHS only)
+        centroid = member_data_points.mean(axis=0)
+        new_centroids.append(centroid)
+    new_centroids = np.array(new_centroids)
+    
+    return new_centroids
     
     
-class ArbitraryFunctionAttrTest(FuncAttrsTest):
-    def test_set_attr(self):
-        self.b.known_attr = 7
-        self.assertEqual(self.b.known_attr, 7)
-        try:
-            self.fi.a.known_attr = 7
-        except AttributeError:
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
+    history_changed(proc, TIMEOUT, u'echo test')
+    
+      aws help
+  aws <command> help
+  aws <command> <subcommand> help
+aws: error: argument command: Invalid choice, valid choices are:
+    
+    
+@pytest.fixture
+def brew_already_installed():
+    return '''Warning: git-2.3.5 already installed'''
+    
+            if 'latency' in request.args:
+            latency = float(request.args['latency'][0])
+            reactor.callLater(latency, self._finish, request)
+            return NOT_DONE_YET
+    
+        def __init__(self):
+        self.settings = None  # set in scrapy.cmdline
+    
+        def _print_response(self, response, opts):
+        if opts.headers:
+            self._print_headers(response.request.headers, b'>')
+            print('>')
+            self._print_headers(response.headers, b'<')
+        else:
+            self._print_bytes(response.body)
+    
+            try:
+            spidercls = self.crawler_process.spider_loader.load(name)
+        except KeyError:
             pass
         else:
-            self.fail('setting attributes on methods should raise error')
+            # if spider already exists and not --force then halt
+            if not opts.force:
+                print('Spider %r already exists in module:' % name)
+                print('  %s' % spidercls.__module__)
+                return
+        template_file = self._find_template(opts.template)
+        if template_file:
+            self._genspider(module, name, domain, opts.template, template_file)
+            if opts.edit:
+                self.exitcode = os.system('scrapy edit '%s'' % name)
     
-        def log_date_time_string(self):
-        '''Return the current time formatted for logging.'''
-        now = time.time()
-        year, month, day, hh, mm, ss, x, y, z = time.localtime(now)
-        s = '%02d/%3s/%04d %02d:%02d:%02d' % (
-                day, self.monthname[month], year, hh, mm, ss)
-        return s
+    class Command(ScrapyCommand):
+    
+            try:
+            self.max_bound = int(self.args[2])
+        except IndexError:
+            self.max_bound = float('inf')
+    
+        def __init__(self, request, timeout=180):
+        self._url = urldefrag(request.url)[0]
+        # converting to bytes to comply to Twisted interface
+        self.url = to_bytes(self._url, encoding='ascii')
+        self.method = to_bytes(request.method, encoding='ascii')
+        self.body = request.body or None
+        self.headers = Headers(request.headers)
+        self.response_headers = None
+        self.timeout = request.meta.get('download_timeout') or timeout
+        self.start_time = time()
+        self.deferred = defer.Deferred().addCallback(self._build_response, request)
+    
+        text = html.remove_tags_with_content(text, ('script', 'noscript'))
+    text = html.replace_entities(text)
+    text = html.remove_comments(text)
+    return _ajax_crawlable_re.search(text) is not None
+
+    
+    from scrapy.utils.python import without_none_values
+    
+        log.info(n_iter)  # 迭代次数
+    return centers, ret
     
     
-def open_text(package: Package,
-              resource: Resource,
-              encoding: str = 'utf-8',
-              errors: str = 'strict') -> TextIO:
-    '''Return a file-like object opened for text reading of the resource.'''
-    resource = _normalize_path(resource)
-    package = _get_package(package)
-    reader = _get_resource_reader(package)
-    if reader is not None:
-        return TextIOWrapper(reader.open_resource(resource), encoding, errors)
-    _check_location(package)
-    absolute_package_path = os.path.abspath(package.__spec__.origin)
-    package_path = os.path.dirname(absolute_package_path)
-    full_path = os.path.join(package_path, resource)
-    try:
-        return open(full_path, mode='r', encoding=encoding, errors=errors)
-    except OSError:
-        # Just assume the loader is a resource loader; all the relevant
-        # importlib.machinery loaders are and an AttributeError for
-        # get_data() will make it clear what is needed from the loader.
-        loader = cast(ResourceLoader, package.__spec__.loader)
-        data = None
-        if hasattr(package.__spec__.loader, 'get_data'):
-            with suppress(OSError):
-                data = loader.get_data(full_path)
-        if data is None:
-            package_name = package.__spec__.name
-            message = '{!r} resource not found in {!r}'.format(
-                resource, package_name)
-            raise FileNotFoundError(message)
-        else:
-            return TextIOWrapper(BytesIO(data), encoding, errors)
+def parametric_relu(x, channel_shared=False, alpha_init=constant(0.), name='parametric_relu', reuse=None):
+    '''参数化 ReLU
     
-        print('Pickled records:')
-    pprint.pprint(memos)
     
-    [1] http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718
+def dense(x, n_unit, act_fn=relu, name=None, reuse=None):
+    '''全连接层
+    Input shape:  [batch_size, n_input]
+    Output shape: [batch_size, n_unit]
     
-        current = roi_feat
-    for i in range(cfg.FAST_RCNN.NUM_STACKED_CONVS):
-        current = model.ConvGN(
-            current, 'head_conv' + str(i + 1), dim_in, hidden_dim, 3,
-            group_gn=get_group_gn(hidden_dim),
-            stride=1, pad=1,
-            weight_init=('MSRAFill', {}),
-            bias_init=('ConstantFill', {'value': 0.}))
-        current = model.Relu(current, current)
-        dim_in = hidden_dim
     
-            if cfg.FPN.FPN_ON:
-            # After adding the RPN head, restrict FPN blobs and scales to
-            # those used in the RoI heads
-            blob_conv, spatial_scale_conv = _narrow_to_fpn_roi_levels(
-                blob_conv, spatial_scale_conv
-            )
+
     
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+        in_channels = int(x.get_shape()[-1])
+    kernel_shape = list(kernel_size) + [in_channels, in_channels]
     
-        # Select background RoIs as those within [BG_THRESH_LO, BG_THRESH_HI)
-    bg_inds = np.where(
-        (max_overlaps < cfg.TRAIN.BG_THRESH_HI) &
-        (max_overlaps >= cfg.TRAIN.BG_THRESH_LO)
-    )[0]
-    # Compute number of background RoIs to take from this image (guarding
-    # against there being fewer than desired)
-    bg_rois_per_this_image = rois_per_image - fg_rois_per_this_image
-    bg_rois_per_this_image = np.minimum(bg_rois_per_this_image, bg_inds.size)
-    # Sample foreground regions without replacement
-    if bg_inds.size > 0:
-        bg_inds = npr.choice(
-            bg_inds, size=bg_rois_per_this_image, replace=False
+    
+def foo():
+    print('foo')
+    
+    prefix_map = {
+    # backend path prefix: path to the `models` parent model used
+    'sentry.tagstore.legacy': 'sentry.tagstore.legacy',
+    'sentry.tagstore.v2': 'sentry.tagstore.v2',
+    'sentry.tagstore.snuba': 'sentry.tagstore.v2',
+}
+    
+            # Removing index on 'EventTag', fields ['group_id', 'key', 'value']
+        db.delete_index(u'tagstore_eventtag', ['group_id', 'key', 'value'])
+    
+            # Removing unique constraint on 'GroupTagKey', fields ['project_id',
+        # 'group_id', 'environment_id', '_key']
+        db.delete_unique(
+            u'tagstore_grouptagkey', [
+                'project_id', 'group_id', 'environment_id', 'key_id'])
+    
+        models = {
+        'tagstore.eventtag': {
+            'Meta': {'unique_together': '(('project_id', 'event_id', 'key', 'value'),)', 'object_name': 'EventTag', 'index_together': '(('project_id', 'key', 'value'), ('group_id', 'key', 'value'))'},
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'event_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
+            'group_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
+            'value': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagValue']', 'db_column': ''value_id''})
+        },
+        'tagstore.grouptagkey': {
+            'Meta': {'unique_together': '(('project_id', 'group_id', '_key'),)', 'object_name': 'GroupTagKey'},
+            '_key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
+            'group_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'values_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'})
+        },
+        'tagstore.grouptagvalue': {
+            'Meta': {'unique_together': '(('project_id', 'group_id', '_key', '_value'),)', 'object_name': 'GroupTagValue', 'index_together': '(('project_id', '_key', '_value', 'last_seen'),)'},
+            '_key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
+            '_value': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagValue']', 'db_column': ''value_id''}),
+            'first_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
+            'group_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'last_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'times_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'})
+        },
+        'tagstore.tagkey': {
+            'Meta': {'unique_together': '(('project_id', 'environment_id', 'key'),)', 'object_name': 'TagKey'},
+            'environment_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'null': 'True'}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'status': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'}),
+            'values_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'})
+        },
+        'tagstore.tagvalue': {
+            'Meta': {'unique_together': '(('project_id', '_key', 'value'),)', 'object_name': 'TagValue', 'index_together': '(('project_id', '_key', 'last_seen'),)'},
+            '_key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
+            'data': ('sentry.db.models.fields.gzippeddict.GzippedDictField', [], {'null': 'True', 'blank': 'True'}),
+            'first_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
+            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
+            'last_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
+            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
+            'times_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+    
+    
+@instrumented_task(
+    name='sentry.tagstore.tasks.delete_tag_key',
+    queue='cleanup',
+    default_retry_delay=60 * 5,
+    max_retries=MAX_RETRIES
+)
+@retry(exclude=(DeleteAborted, ))
+def delete_tag_key(object_id, model, transaction_id=None, **kwargs):
+    from sentry import deletions
+    
+        class Meta:
+        app_label = 'tagstore'
+        unique_together = (('project_id', 'event_id', 'key', 'value'), )
+        index_together = (
+            ('project_id', 'key', 'value'),
+            ('group_id', 'key', 'value'),
         )
     
-        fg_inds = np.where(labels >= 1)[0]
-    bg_inds = np.where(anchor_to_gt_max < cfg.RETINANET.NEGATIVE_OVERLAP)[0]
-    labels[bg_inds] = 0
-    num_fg, num_bg = len(fg_inds), len(bg_inds)
+    from sentry.utils.safe import safe_execute
+from sentry.tasks.base import instrumented_task
+    
+        GroupResolution.objects.filter(id__in=[r.id for r in resolution_list]).update(
+        release=release,
+        type=GroupResolution.Type.in_release,
+        status=GroupResolution.Status.resolved,
+    )
+    
+        min_project_id = 0
+    max_project_id = Project.objects.aggregate(x=Max('id'))['x'] or 0
+    step = 1000
+    while min_project_id < max_project_id:
+        queryset = Group.objects.filter(
+            last_seen__gte=now - timedelta(days=1),
+            project__gte=min_project_id,
+            project__lt=min_project_id + step,
+            platform__isnull=False,
+        ).values_list('platform', 'project_id').distinct()
