c8a845d1d85dd6cff7aa5aa1e0f81ac249b98bd8
SUNsung <weatherius@gmail.com>
2019-06-27 18:54:27 +0300

    Rewrite [07mVwIVWuxhG.07tEnGJFxgLXE075t38QtvVw07kpz.rb] File in: 2019-06-27 18:54:26

--- ruby/07mVwIVWuxhG.07tEnGJFxgLXE075t38QtvVw07kpz.rb
+++ ruby/07mVwIVWuxhG.07tEnGJFxgLXE075t38QtvVw07kpz.rb
@@ -1,50 +1,17 @@
 
         
-                    @template_object.label(@object_name, @sanitized_attribute_name, @text, html_options, &block)
-          end
-        end
-    
-    require 'action_view/helpers/tags/collection_helpers'
-    
-              def default_datetime(options)
-            return if options[:include_blank] || options[:prompt]
-    
-            def render
-          options = @options.stringify_keys
-          options['size'] = options['maxlength'] unless options.key?('size')
-          options['type'] ||= field_type
-          options['value'] = options.fetch('value') { value_before_type_cast } unless field_type == 'file'
-          add_default_name_and_id(options)
-          tag('input', options)
-        end
-    
-      def update
-    setting.data = params[:data]
-    setting.save!
-    
-      def export_filename
-    '#{controller_name}.csv'
-  end
-end
-
-    
-      private
-    
-      private
-    
-          def lines
-        lines = []
-        @diff.diff.split('\n')[2..-1].each_with_index do |line, line_index|
-          lines << { :line  => line,
-                     :class => line_class(line),
-                     :ldln  => left_diff_line_number(0, line),
-                     :rdln  => right_diff_line_number(0, line) }
-        end if @diff
-        lines
+            def load_imports
+      if options.show_tasks && Rake::Task.task_defined?('load:defaults')
+        invoke 'load:defaults'
+        set(:stage, '')
+        Dir[deploy_config_path].each { |f| add_import f }
       end
     
-          def upload_dest
-        @upload_dest
-      end
+          class Properties
+        def initialize
+          @properties = {}
+        end
     
-        EMOJI_PATHNAME = Pathname.new(Gemojione.images_path).freeze
+          # Decorate Variables#set to add validation behavior.
+      def set(key, value=nil, &block)
+        assert_value_or_block_not_both(value, block)
