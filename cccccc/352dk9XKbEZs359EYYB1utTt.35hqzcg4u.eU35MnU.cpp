6804de4c8d6e0cccbddf66d8a068d620b1fc9474
SUNsung <weatherius@gmail.com>
2019-06-28 05:05:35 +0300

    Rewrite [352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp] File in: 2019-06-28 05:05:35

--- cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
+++ cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
@@ -1,264 +1,219 @@
 
         
-        bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv);
+        #include <grpc/support/log.h>
     
-    namespace atom {
-    }
     
-    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
- public:
-  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
-  ~AtomJavaScriptDialogManager() override;
-    }
+    {}  // namespace grpc
     
-    Delegate* AutoUpdater::GetDelegate() {
-  return delegate_;
-}
-    
-    OffscreenViewProxy::OffscreenViewProxy(views::View* view) : view_(view) {
-  view_bitmap_.reset(new SkBitmap);
-}
-    
-    #include 'atom/browser/relauncher.h'
-    
-    void DragFileItems(const std::vector<base::FilePath>& files,
-                   const gfx::Image& icon,
-                   gfx::NativeView view) {
-  // Set up our OLE machinery
-  ui::OSExchangeData data;
-    }
-    
-    class PlatformStyle;
-class WalletModel;
-    
-    #endif // BITCOIN_REVERSELOCK_H
+    #endif  // STORAGE_LEVELDB_UTIL_ENV_POSIX_TEST_HELPER_H_
 
     
-        mov	SRND, 2
-loop2:
-    paddd	X0, [TBL + 0*16]
-    movdqa	[rsp + _XFER], X0
-    DO_ROUND	0
-    DO_ROUND	1
-    DO_ROUND	2
-    DO_ROUND	3
-    paddd	X1, [TBL + 1*16]
-    movdqa	[rsp + _XFER], X1
-    add	TBL, 2*16
-    DO_ROUND	0
-    DO_ROUND	1
-    DO_ROUND	2
-    DO_ROUND	3
+    class WritableFile;
     
-      iter->Seek('b');
-  ASSERT_EQ(IterStatus(iter), '(invalid)');
+    #ifndef STORAGE_LEVELDB_DB_VERSION_SET_H_
+#define STORAGE_LEVELDB_DB_VERSION_SET_H_
     
-    // Generate param traits write methods.
-#include 'ipc/param_traits_write_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
+      // The size of the database changes caused by this batch.
+  //
+  // This number is tied to implementation details, and may change across
+  // releases. It is intended for LevelDB usage metrics.
+  size_t ApproximateSize() const;
     
-    // Create a Shell and returns its routing id.
-IPC_SYNC_MESSAGE_ROUTED2_1(ShellViewHostMsg_CreateShell,
-                           std::string /* url */,
-                           base::DictionaryValue /* manifest */,
-                           int /* result */)
-    
-    #include 'base/basictypes.h'
-#include '../dispatcher_host.h'
-    
-      WebView* view = frame->view();
-  if (!view)
-    return NULL;  // can happen during closing.
-    
-    void Clipboard::SetText(std::string& text) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  ui::Clipboard::ObjectMap map;
-  map[ui::Clipboard::CBF_TEXT].push_back(
-      std::vector<char>(text.begin(), text.end()));
-  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
-}
-    
-      if (!item->enable_shortcut_)
-    return false;
-    
-    void Menu::Create(const base::DictionaryValue& option) {
-  gtk_accel_group = NULL;
-  std::string type;
-  if (option.GetString('type', &type) && type == 'menubar')
-    menu_ = gtk_menu_bar_new();
-  else
-    menu_ = gtk_menu_new();
-    }
-    
-    #include 'content/nw/src/api/menuitem/menuitem.h'
-    
-    bool NwAppClearCacheFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  content::BrowsingDataRemover* remover = content::BrowserContext::GetBrowsingDataRemover(
-      Profile::FromBrowserContext(context_));
-    }
-    
-    NwObjDestroyFunction::~NwObjDestroyFunction() {
-}
-    
-    
-    {    private:
-      DISALLOW_COPY_AND_ASSIGN(NwScreenGetScreensFunction);      
-  };
-    
-    TEST(MovableMessageTest, MoveSameArena) {
-  Arena arena;
-    }
-    
-    // Author: kenton@google.com (Kenton Varda)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
-    
-    void MapLiteTestUtil::SetMapFieldsInitialized(unittest::TestMapLite* message) {
-  MapTestUtilImpl::SetMapFieldsInitialized(message);
-}
-    
-    TEST(StatusOr, TestCopyCtorStatusNotOkConverting) {
-  StatusOr<int>    original(Status::CANCELLED);
-  StatusOr<double> copy(original);
-  EXPECT_EQ(original.status(), copy.status());
-}
-    
-    namespace google {
-namespace protobuf {
-namespace internal {
-namespace {
-    }
-    }
-    }
-    }
-    
-    // Author: brianolson@google.com (Brian Olson)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
-//
-// Test program to verify that GzipInputStream is compatible with command line
-// gunzip or java.util.zip.GzipInputStream
-//
-// Reads gzip stream on standard input and writes decompressed data to standard
-// output.
-    
-        for (size_t i = 0; i < set_fields.size(); i++) {
-      const FieldDescriptor* field = set_fields[i];
-      if (ShouldBeClear(field)) {
-        reflection->ClearField(message, field);
-        continue;
-      }
-      if (field->type() == FieldDescriptor::TYPE_MESSAGE) {
-        if (field->is_repeated()) {
-          for (int j = 0; j < reflection->FieldSize(*message, field); j++) {
-            StripMessage(reflection->MutableRepeatedMessage(message, field, j));
-          }
-        } else {
-          StripMessage(reflection->MutableMessage(message, field));
-        }
-      }
-    }
-    
-    std::string ReadFile(const std::string& name) {
-  std::ifstream file(name.c_str());
-  GOOGLE_CHECK(file.is_open()) << 'Couldn't find file ''
-      << name
-      << '', please make sure you are running this command from the benchmarks'
-      << ' directory.\n';
-  return std::string((std::istreambuf_iterator<char>(file)),
-                     std::istreambuf_iterator<char>());
-}
-    
-        std::vector<std::pair<string,string>> option_pairs;
-    ParseGeneratorParameter(parameter, &option_pairs);
-    
-    
-    {
-    {    cout << 'Is this a mobile, home, or work phone? ';
-    string type;
-    getline(cin, type);
-    if (type == 'mobile') {
-      phone_number->set_type(tutorial::Person::MOBILE);
-    } else if (type == 'home') {
-      phone_number->set_type(tutorial::Person::HOME);
-    } else if (type == 'work') {
-      phone_number->set_type(tutorial::Person::WORK);
-    } else {
-      cout << 'Unknown phone type.  Using default.' << endl;
-    }
-  }
-  *person->mutable_last_updated() = TimeUtil::SecondsToTimestamp(time(NULL));
-}
-    
-    
-    {
-    {
-    {      switch (phone_number.type()) {
-        case tutorial::Person::MOBILE:
-          cout << '  Mobile phone #: ';
-          break;
-        case tutorial::Person::HOME:
-          cout << '  Home phone #: ';
-          break;
-        case tutorial::Person::WORK:
-          cout << '  Work phone #: ';
-          break;
-        default:
-          cout << '  Unknown phone #: ';
-          break;
-      }
-      cout << phone_number.number() << endl;
-    }
-    if (person.has_last_updated()) {
-      cout << '  Updated: ' << TimeUtil::ToString(person.last_updated()) << endl;
-    }
-  }
-}
-    
-      // An enumeration of the three reasons that a test might be aborted.
-  enum AbortReason {
-    TEST_ENCOUNTERED_RETURN_STATEMENT,
-    TEST_THREW_EXCEPTION,
-    TEST_DID_NOT_DIE
-  };
-    
-      // RemoveFileName returns the directory path with the filename removed.
-  // Example: FilePath('path/to/file').RemoveFileName() returns 'path/to/'.
-  // If the FilePath is 'a_file' or '/a_file', RemoveFileName returns
-  // FilePath('./') or, on Windows, FilePath('.\\'). If the filepath does
-  // not have a file, like 'just/a/dir/', it returns the FilePath unmodified.
-  // On Windows platform, '\' is the path separator, otherwise it is '/'.
-  FilePath RemoveFileName() const;
-    
-    #include <algorithm>
-    
-      // Trivial case 2: even numbers
-  if (n % 2 == 0) return n == 2;
-    
-        bool Trainer::TrainMinibatch(const std::unordered_map<Variable, ValuePtr>& arguments, bool isSweepEndInArguments, std::unordered_map<Variable, ValuePtr>& outputsToFetch, const DeviceDescriptor& computeDevice /*= DeviceDescriptor::UseDefaultDevice()*/)
-    {
-#ifndef  CNTK_UWP
-        auto profMinibatch = Microsoft::MSR::CNTK::ScopeProfile(Microsoft::MSR::CNTK::profilerEvtMainMinibatch);
-#endif
-    }
-    
-                if ((oldOutputRank != SentinelValueForInferParamInitRank) && (oldOutputRank != outputRank))
-                InvalidArgument('Output rank of a non-uniform random initialier cannot be overridden if it has been already specified!');
-    
-            std::wstring AsString() const;
-        std::shared_ptr<VariableFields> Clone() const;
-        FunctionPtr Owner() const;
-    
-    namespace Microsoft { namespace MSR { namespace CNTK {
-    }
-    }
-    }
-    
-    class Clock
+    Rational RationalMath::ASin(Rational const& rat, ANGLE_TYPE angletype)
 {
-public:
-    static long long GetTimeStamp();
-    static long long GetTicksPerSecond();
+    PRAT prat = rat.ToPRAT();
+    }
+    
+    static constexpr wchar_t DEFAULT_DEC_SEPARATOR = L'.';
+static constexpr wchar_t DEFAULT_GRP_SEPARATOR = L',';
+static constexpr wstring_view DEFAULT_GRP_STR = L'3;0';
+static constexpr wstring_view DEFAULT_NUMBER_STR = L'0';
+    
+    StandardPeers::AutomationNotificationKind NotificationHost::GetWindowsNotificationKind(CustomPeers::AutomationNotificationKind customKindType)
+{
+    switch (customKindType)
+    {
+    case CustomPeers::AutomationNotificationKind::ItemAdded:
+        return StandardPeers::AutomationNotificationKind::ItemAdded;
+    }
+    }
+    
+    #pragma once
+    
+    
+    {        auto maskMatrix = GetMatrix();
+        size_t rowOffset = offset[0];
+        size_t colOffset = offset[1];
+        size_t sliceRowLength = (shape[0] != NDShape::InferredDimension) ? shape[0] : (maskMatrix->GetNumRows() - rowOffset);
+        size_t sliceColLength = (shape[1] != NDShape::InferredDimension) ? shape[1] : (maskMatrix->GetNumCols() - colOffset);
+        if ((rowOffset == 0) && (sliceRowLength == maskMatrix->GetNumRows()))
+            maskMatrix->ColumnSlice(colOffset, sliceColLength).SetValue((char)maskKind);
+        else
+        {
+            // Since Matrix does not support strides in the row dimension, we will need to create separate slices for each column
+            for (size_t i = colOffset; i < (colOffset + sliceColLength); ++i)
+            {
+                auto column = maskMatrix->ColumnSlice(i, 1);
+                column.Reshape(1, maskMatrix->GetNumRows());
+                column.ColumnSlice(rowOffset, sliceRowLength).SetValue((char)maskKind);
+            }
+        }
+    }
+    
+        template <typename T> 
+    inline std::string GetVersionsString(size_t currentVersion, size_t dictVersion)
+    {
+        std::stringstream info;
+        info << 'Current ' << Typename<T>() << ' version = ' << currentVersion 
+             << ', Dictionary version = ' << dictVersion;
+        return info.str();
+    }
+    
+        private:
+        bool m_isReadOnly;
+        NDShape m_sampleShape;
+        std::vector<Axis> m_sampleDynamicAxes;
+        NDShape m_unpackedShape;
+    
+    
+    {
+    {    private:
+        // Disallow copy and move construction and assignment
+        VariableFields(const VariableFields&) = delete; VariableFields& operator=(const VariableFields& other) = delete; VariableFields(VariableFields&&) = delete; VariableFields& operator=(VariableFields&&) = delete;
+    };
+}
+
+    
+    // GetHmmData - Get the HMM definition for SE training
+// hmm - HMM definition
+// returns - true if succeed
+bool DataReader::GetHmmData(msra::asr::simplesenonehmm* hmm)
+{
+    bool bRet = true;
+    for (size_t i = 0; i < m_ioNames.size(); i++)
+        bRet &= m_dataReaders[m_ioNames[i]]->GetHmmData(hmm);
+    return bRet;
+}
+    
+            if (cols0 * wordsInEachSample != rows1)
+            LogicError('LookupTableNode: rows of input 1 is not a multiple of cols of input 0. This usually happens when the feature dimension is not specified as that in the network definition of look-up-table dimension size.');
+    
+    using json = nlohmann::json;
+    
+      if (client_options_.timeout_) {
+    timer_.async_wait(
+        [=](boost_system::error_code const& ec) { timeoutHandler(ec); });
+  }
+    
+    enum class RocksdbMigrationError {
+  InvalidArgument = 1,
+  FailToOpen = 2,
+  FailToGetVersion = 3,
+  NoMigrationFromCurrentVersion = 5,
+  MigrationLogicError = 6,
+  FailToOpenSrcDatabase = 7,
+  FailMoveDatabase = 8,
 };
+    
+    Status LoggerPlugin::call(const PluginRequest& request,
+                          PluginResponse& response) {
+  std::vector<StatusLogLine> intermediate_logs;
+  if (request.count('string') > 0) {
+    return this->logString(request.at('string'));
+  } else if (request.count('snapshot') > 0) {
+    return this->logSnapshot(request.at('snapshot'));
+  } else if (request.count('init') > 0) {
+    deserializeIntermediateLog(request, intermediate_logs);
+    this->setProcessName(request.at('init'));
+    this->init(this->name(), intermediate_logs);
+    return Status(0);
+  } else if (request.count('status') > 0) {
+    deserializeIntermediateLog(request, intermediate_logs);
+    return this->logStatus(intermediate_logs);
+  } else if (request.count('event') > 0) {
+    return this->logEvent(request.at('event'));
+  } else if (request.count('action') && request.at('action') == 'features') {
+    size_t features = 0;
+    features |= (usesLogStatus()) ? LOGGER_FEATURE_LOGSTATUS : 0;
+    features |= (usesLogEvent()) ? LOGGER_FEATURE_LOGEVENT : 0;
+    return Status(static_cast<int>(features));
+  } else {
+    return Status(1, 'Unsupported call to logger plugin');
+  }
+}
+    
+    
+    {  /**
+   * @brief The host identifier at the time when logs are flushed.
+   *
+   * There is occasionally a delay between logging a status and decorating
+   * with the host identifier. In most cases the identifier is static so this
+   * does not matter. In some cases the host identifier causes database lookups.
+   */
+  std::string identifier;
+};
+    
+    
+    {} // namespace osquery
+
+    
+    #include 'modules/drivers/canbus/can_client/esd/esd_can_client.h'
+    
+    
+    {  SocketCanClientRaw socket_can_client;
+  EXPECT_TRUE(socket_can_client.Init(param));
+  EXPECT_EQ(socket_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
+  std::vector<CanFrame> frames;
+  int32_t num = 0;
+  EXPECT_EQ(socket_can_client.Send(frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  EXPECT_EQ(socket_can_client.Receive(&frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
+  CanFrame can_frame;
+  frames.push_back(can_frame);
+  EXPECT_EQ(socket_can_client.SendSingleFrame(frames),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  socket_can_client.Stop();
+}
+    
+    #include 'gtest/gtest.h'
+    
+    class SpeedLimitTest : public ::testing::Test {
+ public:
+  virtual void SetUp() {
+    speed_limit_.Clear();
+    for (int i = 0; i < 100; ++i) {
+      std::pair<double, double> sp;
+      sp.first = i * 1.0;
+      sp.second = (i % 2 == 0) ? 5.0 : 10.0;
+      speed_limit_.AppendSpeedLimit(sp.first, sp.second);
+    }
+  }
+    }
+    
+      MatrixXd offset_golden = MatrixXd::Zero(10, 1);
+  offset_golden(0, 0) = -2.0 * 1.0 *
+                            (init_derivative + init_second_derivative * 0.1) /
+                            std::pow(0.1, 5) -
+                        6.0 * 1.0 * init_derivative / std::pow(0.1, 5);
+  offset_golden(1, 0) = 2.0 * 1.0 * init_derivative / std::pow(0.1, 5);
+    
+    TEST_F(Accelrpt68Test, reset) {
+  Accelrpt68 acc;
+  int32_t length = 8;
+  ChassisDetail chassis_detail;
+  uint8_t bytes[8] = {0x01, 0x02, 0x03, 0x04, 0x11, 0x12, 0x13, 0x14};
+    }
+    
+      Byte t1(bytes + 5);
+  int32_t t = t1.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
+    
+    
+    {  auto &brakerpt = chassis_detail.gem().brake_rpt_6c();
+  EXPECT_DOUBLE_EQ(brakerpt.manual_input(), 0.258);
+  EXPECT_DOUBLE_EQ(brakerpt.commanded_value(), 0.772);
+  EXPECT_DOUBLE_EQ(brakerpt.output_value(), 4.37);
+  EXPECT_EQ(brakerpt.brake_on_off(), Brake_rpt_6c::BRAKE_ON_OFF_ON);
+}
