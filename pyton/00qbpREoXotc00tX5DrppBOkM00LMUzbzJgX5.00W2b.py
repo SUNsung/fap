d998fd1f0ebc789294c375e8ba513bac3319e5ce
SUNsung <weatherius@gmail.com>
2019-06-28 15:45:50 +0300

    Rewrite [00qbpREoXotc00tX5DrppBOkM00LMUzbzJgX5.00W2b.py] File in: 2019-06-28 15:45:50

--- pyton/00qbpREoXotc00tX5DrppBOkM00LMUzbzJgX5.00W2b.py
+++ pyton/00qbpREoXotc00tX5DrppBOkM00LMUzbzJgX5.00W2b.py
@@ -1,72 +1,28 @@
 
         
-            Args:
-        x(tf.Tensor):
-        kernel_size(int or list of int):
-        out_channels(int):
-        act_fn(function):
-        strides(int or list of int):
-        padding(str):
-        name(str):
-        reuse(bool):
+                # install new cert
+        xlog.info('Add cert to database $HOME/.pki/nssdb')
+        cmd_line = 'certutil -d sql:$HOME/.pki/nssdb -A -t 'C,,' -n '%s' -i '%s'' % (common_name, ca_file)
+        os.system(cmd_line)
+        return True
     
+        def write_ip(self, ip, host, handshake):
+        with self.lock:
+            self.out_fd.write('%s %s gws %d 0 0\n' % (ip, host, handshake))
+            self.out_fd.flush()
     
-class Dense(object):
-    '''全连接层的类实现，方便 reuse'''
+    import simple_http_client
+from xlog import getLogger
+xlog = getLogger('gae_proxy')
     
+    # Get a reference to the Raspberry Pi camera.
+# If this fails, make sure you have a camera connected to the RPi and that you
+# enabled your camera in raspi-config and rebooted first.
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
     
-def get_params_dict():
-    '''以字典形式获取所有 trainable 参数'''
-    param_dict = dict()
-    for var in tf.trainable_variables():
-        param_dict[var.name] = {'shape': list(map(int, var.shape)),
-                                'number': int(reduce(mul, var.shape, 1))}
-    return param_dict
-    
-        This environment is described in section 6.1 of:
-    A Bayesian Framework for Reinforcement Learning by Malcolm Strens (2000)
-    http://ceit.aut.ac.ir/~shiry/lecture/machine-learning/papers/BRL-2000.pdf
+        :param rect: a dlib 'rect' object
+    :return: a plain tuple representation of the rect in (top, right, bottom, left) order
     '''
-    def __init__(self, n=5, slip=0.2, small=2, large=10):
-        self.n = n
-        self.slip = slip  # probability of 'slipping' an action
-        self.small = small  # payout for 'backwards' action
-        self.large = large  # payout at end of chain for 'forwards' action
-        self.state = 0  # Start at beginning of the chain
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Discrete(self.n)
-        self.seed()
-    
-        def test_reversed_addition_3rows(self):
-        env = alg.reversed_addition.ReversedAdditionEnv(base=3, rows=3)
-        input_expected = [
-            ([[1,1,0],[0,1,1]], [2, 2]),
-            ([[1,1,2],[0,1,1]], [1,0,1]),
-        ]
-        for (input_grid, expected_target) in input_expected:
-            self.assertEqual(env.target_from_input_data(input_grid), expected_target)
-    
-        def draw(self, viewer, draw_particles=True):
-        if draw_particles:
-            for p in self.particles:
-                viewer.draw_polyline(p.poly, color=p.color, linewidth=5)
-        for obj in self.drawlist:
-            for f in obj.fixtures:
-                trans = f.body.transform
-                path = [trans*v for v in f.shape.vertices]
-                viewer.draw_polygon(path, color=obj.color)
-                if 'phase' not in obj.__dict__: continue
-                a1 = obj.phase
-                a2 = obj.phase + 1.2  # radians
-                s1 = math.sin(a1)
-                s2 = math.sin(a2)
-                c1 = math.cos(a1)
-                c2 = math.cos(a2)
-                if s1>0 and s2>0: continue
-                if s1>0: c1 = np.sign(c1)
-                if s2>0: c2 = np.sign(c2)
-                white_poly = [
-                    (-WHEEL_W*SIZE, +WHEEL_R*c1*SIZE), (+WHEEL_W*SIZE, +WHEEL_R*c1*SIZE),
-                    (+WHEEL_W*SIZE, +WHEEL_R*c2*SIZE), (-WHEEL_W*SIZE, +WHEEL_R*c2*SIZE)
-                    ]
-                viewer.draw_polygon([trans*v for v in white_poly], color=WHEEL_WHITE)
+    return rect.top(), rect.right(), rect.bottom(), rect.left()
