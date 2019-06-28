28e167cac3095f5e9020638eaf8ac222839be41e
SUNsung <weatherius@gmail.com>
2019-06-28 04:45:51 +0300

    Rewrite [327CXHJV5FVyQ32M5kdVavGoQ3222jbPeKNRZs322x3.py] File in: 2019-06-28 04:45:50

--- pyton/327CXHJV5FVyQ32M5kdVavGoQ3222jbPeKNRZs322x3.py
+++ pyton/327CXHJV5FVyQ32M5kdVavGoQ3222jbPeKNRZs322x3.py
@@ -1,134 +1,49 @@
 
         
-        
-def generateKey(keySize):
-    print('Generating prime p...')
-    p = rabinMiller.generateLargePrime(keySize)  # select large prime number.
-    e_1 = primitiveRoot(p)  # one primitive root on modulo p.
-    d = random.randrange(3, p)  # private_key -> have to be greater than 2 for safety.
-    e_2 = cryptoMath.findModInverse(pow(e_1, d, p), p)
-    
-    The problem is :
-Given two strings A and B. Find the minimum number of operations to string B such that A = B. The permitted operations are removal,  insertion, and substitution.
-'''
-from __future__ import print_function
-    
-        return res
+            auth.login()
+    # current user can't modify other user's post
+    assert client.post('/1/update').status_code == 403
+    assert client.post('/1/delete').status_code == 403
+    # current user doesn't see edit link
+    assert b'href='/1/update'' not in client.get('/').data
     
     
-def is_setting_index(node):
-    if node.tagname == 'index':
-        # index entries for setting directives look like:
-        # [(u'pair', u'SETTING_NAME; setting', u'std:setting-SETTING_NAME', '')]
-        entry_type, info, refid = node['entries'][0][:3]
-        return entry_type == 'pair' and info.endswith('; setting')
-    return False
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    '''Update a post if the current user is the author.'''
+    post = get_post(id)
     
-    __all__ = ['__version__', 'version_info', 'twisted_version',
-           'Spider', 'Request', 'FormRequest', 'Selector', 'Item', 'Field']
     
-        def long_desc(self):
-        '''A long description of the command. Return short description when not
-        available. It cannot contain newlines, since contents will be formatted
-        by optparser which removes newlines and wraps text.
-        '''
-        return self.short_desc()
+  def Start( self ):
+    request_data = BuildRequestData( self._buffer_number )
+    if self._extra_data:
+      request_data.update( self._extra_data )
+    request_data[ 'event_name' ] = self._event_name
     
-            editor = self.settings['EDITOR']
-        try:
-            spidercls = self.crawler_process.spider_loader.load(args[0])
-        except KeyError:
-            return self._err('Spider not found: %s' % args[0])
     
-        def run(self, args, opts):
-        if len(args) != 1 or not is_url(args[0]):
-            raise UsageError()
-        cb = lambda x: self._print_response(x, opts)
-        request = Request(args[0], callback=cb, dont_filter=True)
-        # by default, let the framework handle redirects,
-        # i.e. command handles all codes expect 3xx
-        if not opts.no_redirect:
-            request.meta['handle_httpstatus_list'] = SequenceExclude(range(300, 400))
-        else:
-            request.meta['handle_httpstatus_all'] = True
+  def _ThreadMain( self ):
+    while True:
+      time.sleep( self._ping_interval_seconds )
     
-        @property
-    def templates_dir(self):
-        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
-            join(scrapy.__path__[0], 'templates')
-        return join(_templates_base_dir, 'spiders')
-
+          current_group = SyntaxGroup( match.group( 'group_name' ),
+                                   [ match.group( 'content' ).strip() ] )
+    else:
+      if looking_for_group:
+        continue
     
-            for p in js_path:
-            if 'mtool' in p or 'mcore' in p:
-                js_text = get_content(p)
-                hit = re.search(r'\(\'(.+?)\',(\d+),(\d+),\'(.+?)\'\.split\(\'\|\'\),\d+,\{\}\)', js_text)
     
-        assert received == os.path.getsize(temp_filepath), '%s == %s' % (received, os.path.getsize(temp_filepath))
+  def done( self ):
+    return self._done
     
-                if ret[i, 0] != i_label:  # 样本的类别发生了改变
-                changed = True
+    def _python_exit():
+    global _shutdown
+    _shutdown = True
+    items = list(_threads_queues.items())
+    for t, q in items:
+        q.put(None)
+    for t, q in items:
+        t.join()
     
-        Returns:
-        dict
-    '''
-    tokens = tokenizer.tokenize(txt)
-    tokens = [word_clean(token) for token in tokens]
-    
-        with tf.variable_scope(name or 'attention_for_dense', reuse=reuse):
-        W, b = get_wb([n_input, n_input])
-    
-            # h_tilde(h~): question to context attended query vectors
-        b = tf.reduce_max(S, axis=2)  # [N, T]
-        b = softmax(b, axis=-1)  # [N, T]
-        b = tf.expand_dims(b, axis=1)  # [N, 1, T]
-        h_tilde = tf.matmul(b, h)  # [N, 1, d]
-        h_tilde = tf.tile(h_tilde, [1, T, 1])  # [N, T, d]
-    
-        def __call__(self, x):
-        '''
-        Args:
-            x: 注意 x.dtype == float32
-        '''
-        # x = tf.cast(x, dtype=tf.float32)  # 交给外部处理
-        loss_regularization = 0.
-        if self.l1:
-            loss_regularization += tf.reduce_sum(self.l1 * tf.abs(x))
-        if self.l2:
-            loss_regularization += tf.reduce_sum(self.l2 * tf.square(x))
-        return loss_regularization
-    
-        References:
-        K.repeat()
-        tf.tile()
-    '''
-    assert x.get_shape().ndims == 2
-    x = tf.expand_dims(x, axis=1)  # -> [batch_size, 1, n_input]
-    return tf.tile(x, [1, n, 1])  # -> [batch_size, n, n_input]
-    
-    # 如果一个 ngram 都没找到，gensim 会报错
-#   其实可以返回一个 0 向量的，它内部实际上是从一个 0 向量开始累加的；
-#   但返回时做了一个判断——如果依然是 0 向量，则报错
-# print(model.wv['z'])
-r'''
-Traceback (most recent call last):
-  File 'D:/OneDrive/workspace/github/DL-Notes-for-Interview/code/工具库/gensim/FastText.py', line 53, in <module>
-    print(model.wv['z'])
-  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 336, in __getitem__
-    return self.get_vector(entities)
-  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 454, in get_vector
-    return self.word_vec(word)
-  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 1989, in word_vec
-    raise KeyError('all ngrams for word %s absent from model' % word)
-KeyError: 'all ngrams for word z absent from model'
-'''
-    
-    from __future__ import absolute_import
-    
-    from sentry.exceptions import DeleteAborted
-from sentry.tasks.base import instrumented_task, retry
-from sentry.tasks.deletion import MAX_RETRIES
-    
-        __repr__ = sane_repr('event_id', 'key_id', 'value_id')
-    
-    import sentry
+    def sequential():
+    return list(map(is_prime, PRIMES))
