77685b23ade95bcaa2e4ada28d3da832ecf0f1ab
SUNsung <weatherius@gmail.com>
2019-06-28 17:28:42 +0300

    Rewrite [45KpZ8.vblN9Y45SbsPToDILuM45I.xRgGPWlfM45IxC.py] File in: 2019-06-28 17:28:42

--- pyton/45KpZ8.vblN9Y45SbsPToDILuM45I.xRgGPWlfM45IxC.py
+++ pyton/45KpZ8.vblN9Y45SbsPToDILuM45I.xRgGPWlfM45IxC.py
@@ -1,54 +1,234 @@
 
         
-        current_path = os.path.dirname(os.path.abspath(__file__))
-root_path = os.path.abspath(os.path.join(current_path, os.pardir, os.pardir, os.pardir))
-data_path = os.path.abspath(os.path.join(root_path, os.pardir, os.pardir, 'data', 'gae_proxy'))
-if not os.path.isdir(data_path):
-    data_path = current_path
-    
-            elif isinstance(e, EarlyExitException):
-            msg = 'required (...)+ loop did not match anything at input ' \
-                  + self.getTokenErrorDisplay(e.token)
-    
-    test_face_landmarks = '''
-landmarks = face_recognition.face_landmarks(image, face_locations=face_locations)[0]
-'''
-    
-    print('I found {} face(s) in this photograph.'.format(len(face_locations)))
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
+            def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
     
     
-def image_files_in_folder(folder):
-    return [os.path.join(folder, f) for f in os.listdir(folder) if re.match(r'.*\.(jpg|jpeg|png)', f, flags=re.I)]
+class JWS(jose.JWS):
+    '''ACME-specific JWS. Includes none, url, and kid in protected header.'''
+    signature_cls = Signature
+    __slots__ = jose.JWS._orig_slots  # pylint: disable=no-member
     
-        # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-    
-    
-def print_result(filename, name, distance, show_distance=False):
-    if show_distance:
-        print('{},{},{}'.format(filename, name, distance))
+        good_nonce = jose.encode_b64jose(b'foo')
+    wrong_nonce = u'F'
+    # Following just makes sure wrong_nonce is wrong
+    try:
+        jose.b64decode(wrong_nonce)
+    except (ValueError, TypeError):
+        assert True
     else:
-        print('{},{}'.format(filename, name))
+        assert False  # pragma: no cover
     
-            self.assertEqual(result.exit_code, 0)
-        self.assertTrue(target_string in result.output)
+       # Cloudflare API credentials used by Certbot
+   dns_cloudflare_email = cloudflare@example.com
+   dns_cloudflare_api_key = 0123456789abcdef0123456789abcdef01234567
     
-        # 图片上传失败，输出以下html代码
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
+    # The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = u'0'
+# The full version, including alpha/beta/rc tags.
+release = u'0'
+    
+        if unit_1 == LENGTH_MILES:
+        meters = __miles_to_meters(value)
+    elif unit_1 == LENGTH_FEET:
+        meters = __feet_to_meters(value)
+    elif unit_1 == LENGTH_KILOMETERS:
+        meters = __kilometers_to_meters(value)
+    
+        def set_ip_to_mock(value):
+        nonlocal ip_to_mock
+        ip_to_mock = value
+    
+    
+def im_conv_body_only(model, im, target_scale, target_max_size):
+    '''Runs `model.conv_body_net` on the given image `im`.'''
+    im_blob, im_scale, _im_info = blob_utils.get_image_blob(
+        im, target_scale, target_max_size
+    )
+    workspace.FeedBlob(core.ScopedName('data'), im_blob)
+    workspace.RunNet(model.conv_body_net.Proto().name)
+    return im_scale
+    
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+    
+    
+def evaluate_boxes(
+    json_dataset,
+    all_boxes,
+    output_dir,
+    use_salt=True,
+    cleanup=True,
+    use_matlab=False
+):
+    salt = '_{}'.format(str(uuid.uuid4())) if use_salt else ''
+    filenames = _write_voc_results_files(json_dataset, all_boxes, salt)
+    _do_python_eval(json_dataset, salt, output_dir)
+    if use_matlab:
+        _do_matlab_eval(json_dataset, salt, output_dir)
+    if cleanup:
+        for filename in filenames:
+            shutil.copy(filename, output_dir)
+            os.remove(filename)
+    return None
+    
+    
+def _generate_anchors(base_size, scales, aspect_ratios):
+    '''Generate anchor (reference) windows by enumerating aspect ratios X
+    scales wrt a reference (0, 0, base_size - 1, base_size - 1) window.
     '''
+    anchor = np.array([1, 1, base_size, base_size], dtype=np.float) - 1
+    anchors = _ratio_enum(anchor, aspect_ratios)
+    anchors = np.vstack(
+        [_scale_enum(anchors[i, :], scales) for i in range(anchors.shape[0])]
+    )
+    return anchors
     
-    # Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+        # ==========================================================================
+    # bbox tower if not sharing features with the classification tower with
+    # logits and prob prediction
+    # ==========================================================================
+    if not cfg.RETINANET.SHARE_CLS_BBOX_TOWER:
+        for lvl in range(k_min, k_max + 1):
+            bl_in = blobs_in[k_max - lvl]  # blobs_in is in reversed order
+            for nconv in range(cfg.RETINANET.NUM_CONVS):
+                suffix = 'n{}_fpn{}'.format(nconv, lvl)
+                dim_in, dim_out = dim_in, dim_in
+                if lvl == k_min:
+                    bl_out = model.Conv(
+                        bl_in,
+                        'retnet_bbox_conv_' + suffix,
+                        dim_in,
+                        dim_out,
+                        3,
+                        stride=1,
+                        pad=1,
+                        weight_init=('GaussianFill', {
+                            'std': 0.01
+                        }),
+                        bias_init=('ConstantFill', {
+                            'value': 0.
+                        })
+                    )
+                else:
+                    bl_out = model.ConvShared(
+                        bl_in,
+                        'retnet_bbox_conv_' + suffix,
+                        dim_in,
+                        dim_out,
+                        3,
+                        stride=1,
+                        pad=1,
+                        weight='retnet_bbox_conv_n{}_fpn{}_w'.format(
+                            nconv, k_min
+                        ),
+                        bias='retnet_bbox_conv_n{}_fpn{}_b'.format(
+                            nconv, k_min
+                        )
+                    )
+                bl_in = model.Relu(bl_out, bl_out)
+                # Add octave scales and aspect ratio
+                # At least 1 convolution for dealing different aspect ratios
+                bl_feat = bl_in
+            bbox_feat_list.append(bl_feat)
+    # Depending on the features [shared/separate] for bbox, add prediction layer
+    for i, lvl in enumerate(range(k_min, k_max + 1)):
+        bbox_pred = 'retnet_bbox_pred_fpn{}'.format(lvl)
+        bl_feat = bbox_feat_list[i]
+        if lvl == k_min:
+            model.Conv(
+                bl_feat,
+                bbox_pred,
+                dim_in,
+                bbox_regr_dim * A,
+                3,
+                pad=1,
+                stride=1,
+                weight_init=('GaussianFill', {
+                    'std': 0.01
+                }),
+                bias_init=('ConstantFill', {
+                    'value': 0.
+                })
+            )
+        else:
+            model.ConvShared(
+                bl_feat,
+                bbox_pred,
+                dim_in,
+                bbox_regr_dim * A,
+                3,
+                pad=1,
+                stride=1,
+                weight='retnet_bbox_pred_fpn{}_w'.format(k_min),
+                bias='retnet_bbox_pred_fpn{}_b'.format(k_min)
+            )
+    
+    
+def get_minibatch(roidb):
+    '''Given a roidb, construct a minibatch sampled from it.'''
+    # We collect blobs from each image onto a list and then concat them into a
+    # single tensor, hence we initialize each blob to an empty list
+    blobs = {k: [] for k in get_minibatch_blob_names()}
+    # Get the input image blob, formatted for caffe2
+    im_blob, im_scales = _get_image_blob(roidb)
+    blobs['data'] = im_blob
+    if cfg.RPN.RPN_ON:
+        # RPN-only or end-to-end Faster/Mask R-CNN
+        valid = rpn_roi_data.add_rpn_blobs(blobs, im_scales, roidb)
+    elif cfg.RETINANET.RETINANET_ON:
+        im_width, im_height = im_blob.shape[3], im_blob.shape[2]
+        # im_width, im_height corresponds to the network input: padded image
+        # (if needed) width and height. We pass it as input and slice the data
+        # accordingly so that we don't need to use SampleAsOp
+        valid = retinanet_roi_data.add_retinanet_blobs(
+            blobs, im_scales, roidb, im_width, im_height
+        )
+    else:
+        # Fast R-CNN like models trained on precomputed proposals
+        valid = fast_rcnn_roi_data.add_fast_rcnn_blobs(blobs, im_scales, roidb)
+    return blobs, valid
+    
+    delim = text_type(args.delimiter)
+cutall = args.cutall
+hmm = args.hmm
+fp = open(args.filename, 'r') if args.filename else sys.stdin
+    
+    
+def ChineseAnalyzer(stoplist=STOP_WORDS, minsize=1, stemfn=stem, cachesize=50000):
+    return (ChineseTokenizer() | LowercaseFilter() |
+            StopFilter(stoplist=stoplist, minsize=minsize) |
+            StemFilter(stemfn=stemfn, ignore=None, cachesize=cachesize))
+
+    
+            prev_states_expect_next = set(
+            (y for x in prev_states for y in trans_p[x].keys()))
+        obs_states = set(
+            states.get(obs[t], all_states)) & prev_states_expect_next
+    
+    if opt.withWeight is None:
+    withWeight = False
+else:
+    if int(opt.withWeight) is 1:
+        withWeight = True
+    else:
+        withWeight = False
+    
+    pattern = os.path.join(sys.argv[1],'*.txt') 
+print('read '+pattern)
+    
+        def testTokenize_NOHMM(self):
+        for content in test_contents:
+            result = jieba.tokenize(content,HMM=False)
+            assert isinstance(result, types.GeneratorType), 'Test Tokenize Generator error'
+            result = list(result)
+            assert isinstance(result, list), 'Test Tokenize error on content: %s' % content
+            for tk in result:
+                print('word %s\t\t start: %d \t\t end:%d' % (tk[0],tk[1],tk[2]), file=sys.stderr)
+        print('testTokenize_NOHMM', file=sys.stderr)
