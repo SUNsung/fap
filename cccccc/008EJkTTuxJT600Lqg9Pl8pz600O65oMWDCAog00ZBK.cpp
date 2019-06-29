62d047816f33cfaa564f1fbb2300ae7ea14e1cfa
SUNsung <weatherius@gmail.com>
2019-06-29 09:01:58 +0300

    Rewrite [008EJkTTuxJT600Lqg9Pl8pz600O65oMWDCAog00ZBK.cpp] File in: 2019-06-29 09:01:57

--- cccccc/008EJkTTuxJT600Lqg9Pl8pz600O65oMWDCAog00ZBK.cpp
+++ cccccc/008EJkTTuxJT600Lqg9Pl8pz600O65oMWDCAog00ZBK.cpp
@@ -1,76 +1,186 @@
 
         
-            Rational Sin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Cos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Tan(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ASin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ACos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ATan(Rational const& rat, ANGLE_TYPE angletype);
-    
-    
-    {    return result;
-}
-    
-        if (radixtype >= HEX_RADIX && radixtype <= BIN_RADIX)
-    {
-        m_radix = NRadixFromRadixType(radixtype);
-        // radixtype is not even saved
-    }
-    
-    class IUnaryCommand : public IOperatorCommand
-{
-public:
-    virtual const std::shared_ptr<CalculatorVector<int>>& GetCommands() const = 0;
-    virtual void SetCommands(int command1, int command2) = 0;
-};
-    
-    TEST(SparsePage, PushCSCAfterTranspose) {
-  const int n_entries = 9;
-  std::unique_ptr<DMatrix> dmat = CreateSparsePageDMatrix(n_entries, 64UL);
-  const int ncols = dmat->Info().num_col_;
-  SparsePage page; // Consolidated sparse page
-  for (const auto &batch : dmat->GetRowBatches()) {
-    // Transpose each batch and push
-    SparsePage tmp = batch.GetTranspose(ncols);
-    page.PushCSC(tmp);
-  }
-    }
-    
-    /*!
- * \brief builder for histograms of gradient statistics
- */
-class GHistBuilder {
- public:
-  // initialize builder
-  inline void Init(size_t nthread, uint32_t nbins) {
-    nthread_ = nthread;
-    nbins_ = nbins;
-    thread_init_.resize(nthread_);
-  }
-    }
-    
-      std::vector<std::vector<unsigned>> groups;
-  std::vector<std::vector<bool>> conflict_marks;
-  std::vector<size_t> group_nnz;
-  std::vector<size_t> group_conflict_cnt;
-  const auto max_conflict_cnt
-    = static_cast<size_t>(param.max_conflict_rate * nrow);
-    
-    #include <dmlc/registry.h>
-#include <xgboost/base.h>
-#include <xgboost/data.h>
-#include <xgboost/generic_parameters.h>
-#include <functional>
-#include <string>
-#include <utility>
-#include <vector>
-#include '../../src/gbm/gblinear_model.h'
-#include '../../src/common/host_device_vector.h'
-    
-    
-    {}  // namespace xgboost
-#endif  // XGBOOST_TREE_UPDATER_H_
+        
+    {}  // namespace mate
 
     
-        // change element at index 1 (second element) to 'second'
-    array.at(1) = 'second';
+    PersistentDictionary::~PersistentDictionary() {}
+    
+      status = napi_get_cb_info(env, info, &argc, args, NULL, NULL);
+  if (status != napi_ok)
+    return NULL;
+    
+    namespace mate {
+    }
+    
+      template <typename T>
+  bool Get(const base::StringPiece& key, T* out) const {
+    // Check for existence before getting, otherwise this method will always
+    // returns true when T == v8::Local<v8::Value>.
+    v8::Local<v8::Context> context = isolate_->GetCurrentContext();
+    v8::Local<v8::String> v8_key = StringToV8(isolate_, key);
+    if (!internal::IsTrue(GetHandle()->Has(context, v8_key)))
+      return false;
+    }
+    
+    namespace mate {
+    }
+    
+    
+    {
+    {}  // namespace load_reporter
+}  // namespace grpc
+
+    
+    
+    {  // Verify that the size of the key space not touched by the reads
+  // is pretty much unchanged.
+  const int64_t final_other_size = Size(Key(n), Key(kCount));
+  ASSERT_LE(final_other_size, initial_other_size + 1048576);
+  ASSERT_GE(final_other_size, initial_other_size / 5 - 1048576);
+}
+    
+    
+    {  KeyComparator comparator_;
+  int refs_;
+  Arena arena_;
+  Table table_;
+};
+    
+    #include 'db/dbformat.h'
+#include 'leveldb/cache.h'
+#include 'leveldb/table.h'
+#include 'port/port.h'
+    
+    // A Table is a sorted map from strings to strings.  Tables are
+// immutable and persistent.  A Table may be safely accessed from
+// multiple threads without external synchronization.
+class LEVELDB_EXPORT Table {
+ public:
+  // Attempt to open the table that is stored in bytes [0..file_size)
+  // of 'file', and read the metadata entries necessary to allow
+  // retrieving data from the table.
+  //
+  // If successful, returns ok and sets '*table' to the newly opened
+  // table.  The client should delete '*table' when no longer needed.
+  // If there was an error while initializing the table, sets '*table'
+  // to nullptr and returns a non-ok status.  Does not take ownership of
+  // '*source', but the client must ensure that 'source' remains live
+  // for the duration of the returned table's lifetime.
+  //
+  // *file must remain live while this Table is in use.
+  static Status Open(const Options& options, RandomAccessFile* file,
+                     uint64_t file_size, Table** table);
+    }
+    
+    #ifndef STORAGE_LEVELDB_TABLE_BLOCK_BUILDER_H_
+#define STORAGE_LEVELDB_TABLE_BLOCK_BUILDER_H_
+    
+      void StartBlock(uint64_t block_offset);
+  void AddKey(const Slice& key);
+  Slice Finish();
+    
+    char* Arena::AllocateAligned(size_t bytes) {
+  const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
+  static_assert((align & (align - 1)) == 0,
+                'Pointer size should be a power of 2');
+  size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1);
+  size_t slop = (current_mod == 0 ? 0 : align - current_mod);
+  size_t needed = bytes + slop;
+  char* result;
+  if (needed <= alloc_bytes_remaining_) {
+    result = alloc_ptr_ + slop;
+    alloc_ptr_ += needed;
+    alloc_bytes_remaining_ -= needed;
+  } else {
+    // AllocateFallback always returned aligned memory
+    result = AllocateFallback(bytes);
+  }
+  assert((reinterpret_cast<uintptr_t>(result) & (align - 1)) == 0);
+  return result;
+}
+    
+    int main(int, char**)
+{
+    // Setup Allegro
+    al_init();
+    al_install_keyboard();
+    al_install_mouse();
+    al_init_primitives_addon();
+    al_set_new_display_flags(ALLEGRO_RESIZABLE);
+    ALLEGRO_DISPLAY* display = al_create_display(1280, 720);
+    al_set_window_title(display, 'Dear ImGui Allegro 5 example');
+    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+    al_register_event_source(queue, al_get_display_event_source(display));
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_mouse_event_source());
+    }
+    
+    // Forward declarations of helper functions
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void CreateRenderTarget();
+void CleanupRenderTarget();
+void WaitForLastSubmittedFrame();
+FrameContext* WaitForNextFrameResources();
+void ResizeSwapChain(HWND hWnd, int width, int height);
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    
+    bool    ImGui_ImplDX10_Init(ID3D10Device* device)
+{
+    // Setup back-end capabilities flags
+    ImGuiIO& io = ImGui::GetIO();
+    io.BackendRendererName = 'imgui_impl_dx10';
+    io.BackendFlags |= ImGuiBackendFlags_RendererHasVtxOffset;  // We can honor the ImDrawCmd::VtxOffset field, allowing for large meshes.
+    }
+    
+    static void ImGui_ImplGlfw_UpdateMousePosAndButtons()
+{
+    // Update buttons
+    ImGuiIO& io = ImGui::GetIO();
+    for (int i = 0; i < IM_ARRAYSIZE(io.MouseDown); i++)
+    {
+        // If a mouse press event came, always pass it as 'mouse held this frame', so we don't miss click-release events that are shorter than 1 frame.
+        io.MouseDown[i] = g_MouseJustPressed[i] || glfwGetMouseButton(g_Window, i) != 0;
+        g_MouseJustPressed[i] = false;
+    }
+    }
+    
+    
+    {        IM_ASSERT(wd->Frames == NULL);
+        wd->Frames = (ImGui_ImplVulkanH_Frame*)IM_ALLOC(sizeof(ImGui_ImplVulkanH_Frame) * wd->ImageCount);
+        wd->FrameSemaphores = (ImGui_ImplVulkanH_FrameSemaphores*)IM_ALLOC(sizeof(ImGui_ImplVulkanH_FrameSemaphores) * wd->ImageCount);
+        memset(wd->Frames, 0, sizeof(wd->Frames[0]) * wd->ImageCount);
+        memset(wd->FrameSemaphores, 0, sizeof(wd->FrameSemaphores[0]) * wd->ImageCount);
+        for (uint32_t i = 0; i < wd->ImageCount; i++)
+            wd->Frames[i].Backbuffer = backbuffers[i];
+    }
+    if (old_swapchain)
+        vkDestroySwapchainKHR(device, old_swapchain, allocator);
+    
+    public:
+    static const char DEFAULT_NAMESPACE_SEPARATOR = ':';
+    static const char DEFAULT_KEYS_GLOB_PATTERN = '*';
+    static const bool DEFAULT_LUA_KEYS_LOADING = false;
+    static const uint DEFAULT_DB_SCAN_LIMIT = 20;
+    
+    
+    {  try {
+    m_connection->connect(false);
+  } catch (RedisClient::Connection::Exception&) {
+    emit error(QCoreApplication::translate(
+        'RDM', 'Invalid Connection. Check connection settings.'));
+    return;
+  }
+}
+    
+      virtual QString getName() const = 0;
+    
+    QHash<int, QByteArray> TabViewModel::roleNames() const {
+  QHash<int, QByteArray> roles;
+  roles[tabIndex] = 'tabIndex';
+  roles[tabName] = 'tabName';
+  return roles;
+}
+    
+      QHash<int, QByteArray> roleNames() const override;
