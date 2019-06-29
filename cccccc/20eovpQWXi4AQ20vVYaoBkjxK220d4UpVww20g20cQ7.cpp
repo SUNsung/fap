3f1f0bfa0c10afb6508577a52eea416fe24acea7
SUNsung <weatherius@gmail.com>
2019-06-29 06:16:51 +0300

    Rewrite [20eovpQWXi4AQ20vVYaoBkjxK220d4UpVww20g20cQ7.cpp] File in: 2019-06-29 06:16:50

--- cccccc/20eovpQWXi4AQ20vVYaoBkjxK220d4UpVww20g20cQ7.cpp
+++ cccccc/20eovpQWXi4AQ20vVYaoBkjxK220d4UpVww20g20cQ7.cpp
@@ -1,280 +1,137 @@
 
         
-        // Call method of an object in browser.
+            StringRef Line = RawText.substr(0, Pos);
+    Lines.push_back(Line);
+    if (!IsFirstLine) {
+      size_t NonWhitespacePos = RawText.find_first_not_of(' ');
+      if (NonWhitespacePos != StringRef::npos)
+        WhitespaceToTrim =
+            std::min(WhitespaceToTrim,
+                     static_cast<unsigned>(NonWhitespacePos));
+    }
+    IsFirstLine = false;
+    
+    
+    {}%
+    
+    #include 'swift/Demangling/StandardTypesMangling.def'
+    
+    // Tell browser we have an uncaughtException from node.
+IPC_MESSAGE_ROUTED1(ShellViewHostMsg_UncaughtException,
+                    std::string /* err */)
+    
+      static void Call(content::Shell* shell,
+                   const std::string& method,
+                   const base::ListValue& arguments,
+                   base::ListValue* result,
+                   DispatcherHost* dispatcher_host);
+    
+      scoped_ptr<base::Value> value_args(
+      converter->FromV8Value(args, isolate->GetCurrentContext()));
+  if (!value_args.get() ||
+      !value_args->IsType(base::Value::TYPE_LIST))
+    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
+        'Unable to convert 'args' passed to CallObjectMethod')));
+    
+    // Call method of an object in browser and return the result.
 // function CallObjectMethod(id, type, method, args);
-v8::Handle<v8::Value> CallObjectMethod(int routing_id,
-                                       int object_id,
-                                       const std::string& type,
-                                       const std::string& method,
-                                       v8::Handle<v8::Value> args);
+v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
+                                           int object_id,
+                                           const std::string& type,
+                                           const std::string& method,
+                                           v8::Handle<v8::Value> args);
     
-    
-    {  *accelerator = item->accelerator_;
-  return true;
-}
-    
-    bool MenuItem::CanHandleAccelerators() const {
-  return enable_shortcut_ && is_enabled_;
-}
-    
-      remover->AddObserver(this);
-  remover->RemoveAndReply(base::Time(), base::Time::Max(),
-                          content::BrowsingDataRemover::DATA_TYPE_CACHE,
-                          content::BrowsingDataRemover::ORIGIN_TYPE_UNPROTECTED_WEB,
-                          this);
-  // BrowsingDataRemover deletes itself.
-  base::MessageLoopCurrent::ScopedNestableTaskAllower allow;
-    
-        bool ReadHTML(ClipboardData& data) {
-      DCHECK(data.type == TYPE_HTML);
-      base::string16 text;
-      std::string src_url;
-      uint32_t fragment_start, fragment_end;
-      clipboard_->ReadHTML(ui::CLIPBOARD_TYPE_COPY_PASTE, &text, &src_url, &fragment_start, &fragment_end);
-      data.data.reset(new std::string(base::UTF16ToUTF8(text)));
-      return true;
-    }
-    
-    namespace CAROTENE_NS {
-    }
-    
-            // do vertical convolution
-        size_t x = 0;
-        const size_t bcols = y + 3 < size.height ? colsn : (colsn - 8);
-        for( ; x <= bcols; x += 8 )
-        {
-            internal::prefetch(srow0 + x);
-            internal::prefetch(srow1 + x);
-            internal::prefetch(srow2 + x);
-            internal::prefetch(srow3 + x);
-            internal::prefetch(srow4 + x);
-    }
-    
-    #define MERGE_QUAD(sgn, bits, n) { \
-                                     FILL_LINES##n(PREF, sgn##bits) \
-                                     MERGE_ASM##n(sgn, bits) \
-                                 }
-    
-    
-    {
-    {} // namespace internal
-} // namespace CAROTENE_NS
-
-    
-        ptrdiff_t width = (ptrdiff_t)size.width, height = (ptrdiff_t)size.height;
-    static const vshrq_s32_func vshrq_s32_a[33] =
-    {
-        vshrq_s32<0>,
-        vshrq_s32<1>,
-        vshrq_s32<2>,
-        vshrq_s32<3>,
-        vshrq_s32<4>,
-        vshrq_s32<5>,
-        vshrq_s32<6>,
-        vshrq_s32<7>,
-        vshrq_s32<8>,
-        vshrq_s32<9>,
-        vshrq_s32<10>,
-        vshrq_s32<11>,
-        vshrq_s32<12>,
-        vshrq_s32<13>,
-        vshrq_s32<14>,
-        vshrq_s32<15>,
-        vshrq_s32<16>,
-        vshrq_s32<17>,
-        vshrq_s32<18>,
-        vshrq_s32<19>,
-        vshrq_s32<20>,
-        vshrq_s32<21>,
-        vshrq_s32<22>,
-        vshrq_s32<23>,
-        vshrq_s32<24>,
-        vshrq_s32<25>,
-        vshrq_s32<26>,
-        vshrq_s32<27>,
-        vshrq_s32<28>,
-        vshrq_s32<29>,
-        vshrq_s32<30>,
-        vshrq_s32<31>,
-        vshrq_s32<32>
-    };
-    vshrq_s32_func vshrq_s32_p = vshrq_s32_a[scale];
-    
-    
-    {        process(src, j, size.width, i,
-                minVal, minLocPtr, minLocCount, minLocCapacity,
-                maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
-    }
-    
-    
-    {
-    {
-    {             vec128  vs = internal::vld1q( src + i);
-             vec128 vr1 = internal::vld1q(rng1 + i);
-             vec128 vr2 = internal::vld1q(rng2 + i);
-            uvec128 vd1 = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
-                     vs = internal::vld1q( src + i + 16/sizeof(T));
-                    vr1 = internal::vld1q(rng1 + i + 16/sizeof(T));
-                    vr2 = internal::vld1q(rng2 + i + 16/sizeof(T));
-            uvec128 vd2 = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
-            vnst(dst + i, vd1, vd2);
-        }
-        vtail<T, sizeof(T)>::inRange(src, rng1, rng2, dst, i, size.width);
-        for( ; i < size.width; i++ )
-            dst[i] = (u8)(-(rng1[i] <= src[i] && src[i] <= rng2[i]));
-    }
-}
-    
-            int16x8_t tcurr1 = vmovq_n_s16(0x0);
-        int16x8_t tnext1 = vmovq_n_s16(0x0);
-        int16x8_t tprev1 = vmovq_n_s16(0x0);
-        int16x8_t tpprev1 = vmovq_n_s16(0x0);
-        int16x8_t tppprev1 = vmovq_n_s16(0x0);
-    
-    #include 'caffe/proto/caffe.pb.h'
-#include 'caffe/util/format.hpp'
-#include 'caffe/util/math_functions.hpp'
-    
-      /**
-   * @brief Compute the volume of a slice; i.e., the product of dimensions
-   *        among a range of axes.
-   *
-   * @param start_axis The first axis to include in the slice.
-   *
-   * @param end_axis The first axis to exclude from the slice.
-   */
-  inline int count(int start_axis, int end_axis) const {
-    CHECK_LE(start_axis, end_axis);
-    CHECK_GE(start_axis, 0);
-    CHECK_GE(end_axis, 0);
-    CHECK_LE(start_axis, num_axes());
-    CHECK_LE(end_axis, num_axes());
-    int count = 1;
-    for (int i = start_axis; i < end_axis; ++i) {
-      count *= shape(i);
-    }
-    return count;
+    void Clipboard::CallSync(const std::string& method,
+                         const base::ListValue& arguments,
+                         base::ListValue* result) {
+  if (method == 'Get') {
+    result->AppendString(GetText());
+  } else {
+    NOTREACHED() << 'Invalid call to Clipboard method:' << method
+                 << ' arguments:' << arguments;
   }
-  /**
-   * @brief Compute the volume of a slice spanning from a particular first
-   *        axis to the final axis.
-   *
-   * @param start_axis The first axis to include in the slice.
-   */
-  inline int count(int start_axis) const {
-    return count(start_axis, num_axes());
-  }
+}
     
-    /** @brief Fills a Blob with values @f$ x \in [0, 1] @f$
- *         such that @f$ \forall i \sum_j x_{ij} = 1 @f$.
- */
-template <typename Dtype>
-class PositiveUnitballFiller : public Filler<Dtype> {
+    namespace nwapi {
+    }
+    
+      ui::KeyboardCode keyval = ui::VKEY_UNKNOWN;
+    
+    ExtensionFunction::ResponseAction
+NwObjCallObjectMethodAsyncFunction::Run() {
+  EXTENSION_FUNCTION_VALIDATE(args_);
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
+    }
+    
+    grpc_error* CensusClientCallData::Init(grpc_call_element* elem,
+                                       const grpc_call_element_args* args) {
+  path_ = grpc_slice_ref_internal(args->path);
+  start_time_ = absl::Now();
+  method_ = GetMethod(&path_);
+  qualified_method_ = absl::StrCat('Sent.', method_);
+  GRPC_CLOSURE_INIT(&on_done_recv_message_, OnDoneRecvMessageCb, elem,
+                    grpc_schedule_on_exec_ctx);
+  GRPC_CLOSURE_INIT(&on_done_recv_trailing_metadata_,
+                    OnDoneRecvTrailingMetadataCb, elem,
+                    grpc_schedule_on_exec_ctx);
+  return GRPC_ERROR_NONE;
+}
+    
+    MeasureDouble RpcClientServerLatency() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcClientServerLatencyMeasureName,
+      'Time between first byte of request received to last byte of response '
+      'sent, or terminal error (propagated from the server)',
+      kUnitMilliseconds);
+  return measure;
+}
+    
+    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_MEASURES_H
+#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_MEASURES_H
+    
+    
+    {}  // namespace
+    
+    namespace grpc {
+namespace load_reporter {
+    }
+    }
+    
+    // A thread pool interface for running callbacks.
+class ThreadPoolInterface {
  public:
-  explicit PositiveUnitballFiller(const FillerParameter& param)
-      : Filler<Dtype>(param) {}
-  virtual void Fill(Blob<Dtype>* blob) {
-    Dtype* data = blob->mutable_cpu_data();
-    DCHECK(blob->count());
-    caffe_rng_uniform<Dtype>(blob->count(), 0, 1, blob->mutable_cpu_data());
-    // We expect the filler to not be called very frequently, so we will
-    // just use a simple implementation
-    int dim = blob->count() / blob->shape(0);
-    CHECK(dim);
-    for (int i = 0; i < blob->shape(0); ++i) {
-      Dtype sum = 0;
-      for (int j = 0; j < dim; ++j) {
-        sum += data[i * dim + j];
-      }
-      for (int j = 0; j < dim; ++j) {
-        data[i * dim + j] /= sum;
-      }
+  virtual ~ThreadPoolInterface() {}
     }
-    CHECK_EQ(this->filler_param_.sparse(), -1)
-         << 'Sparsity not supported by this Filler.';
+    
+    namespace dmlc {
+namespace data {
+    }
+    }
+    
+          ASSERT_TRUE(input == output);
+    
+      auto inst = page[0];
+  ASSERT_EQ(inst.size(), 2);
+  for (auto entry : inst) {
+    ASSERT_EQ(entry.index, 0);
   }
-};
     
-      virtual inline const char* type() const { return 'Accuracy'; }
-  virtual inline int ExactNumBottomBlobs() const { return 2; }
     
-      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+    {}  // namespace xgboost
     
-    namespace caffe {
+      void UpdatePredictionCache(
+      const gbm::GBTreeModel& model,
+      std::vector<std::unique_ptr<TreeUpdater>>* updaters,
+      int num_new_trees) override {
+    int old_ntree = model.trees.size() - num_new_trees;
+    // update cache entry
+    for (auto& kv : cache_) {
+      PredictionCacheEntry& e = kv.second;
     }
-    
-    /**
- * @brief Takes at least two Blob%s and concatenates them along either the num
- *        or channel dimension, outputting the result.
- */
-template <typename Dtype>
-class ConcatLayer : public Layer<Dtype> {
- public:
-  explicit ConcatLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
     }
-    
-    
-    {}  // namespace caffe
-    
-      bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnLRNDescriptor_t norm_desc_;
-  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
-    
-    namespace caffe {
-    }
-    
-    
-/// Recursive
-/// Time Complexity: O(n)
-/// Space Complexity: O(n)
-    
-    private:
-    void __inorderTraversal(TreeNode* node, vector<int> &res){
-    }
-    
-                while(cur != NULL){
-                stack.push(cur);
-                cur = cur->left;
-            }
-    
-    int main() {
-    }
-    
-    
-    {        vector<int> res;
-        __postorderTraversal(root, res);
-        return res;
-    }
-    
-            vector<int> res;
-        if(root == NULL)
-            return res;
-    
-    #endif // D_DHT_ROUTING_TABLE_H
-
-    
-    namespace aria2 {
-    }
-    
-    class DHTRoutingTableSerializer {
-private:
-  int family_;
-    }
-    
-    #include 'common.h'
-    
-    #endif // D_DHT_TASK_FACTORY_IMPL_H
-
-    
-    
-    {  void updateTokenSecret();
-};
