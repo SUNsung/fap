a87a8f1fd2895791ec0451e6e70ef01264657373
SUNsung <weatherius@gmail.com>
2019-06-29 07:55:56 +0300

    Rewrite [55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb] File in: 2019-06-29 07:55:56

--- ruby/55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb
+++ ruby/55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb
@@ -1,284 +1,77 @@
-require 'benchmark/ips'
-require 'jekyll'
-require 'json'
-    
-    # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
-    
-      </body>
-</html>
-HTML
-CONTENT_NOT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-        # Releases a local lock on a machine. This does not acquire any locks
-    # so make sure to lock around it.
-    #
-    # @param [String] id
-    def unlocked_release(id)
-      lock_file = @machine_locks[id]
-      if lock_file
-        lock_file.close
-        begin
-          File.delete(lock_file.path)
-        rescue Errno::EACCES
-          # Another process is probably opened it, no problem.
-        end
-    
-              # Set all of our instance variables on the new class
-          [self, other].each do |obj|
-            obj.instance_variables.each do |key|
-              # Ignore keys that start with a double underscore. This allows
-              # configuration classes to still hold around internal state
-              # that isn't propagated.
-              if !key.to_s.start_with?('@__')
-                result.instance_variable_set(key, obj.instance_variable_get(key))
-              end
-            end
-          end
-    
-            # Returns the internal data associated with this plugin. This
-        # should NOT be called by the general public.
-        #
-        # @return [Hash]
-        def self.data
-          @data ||= {}
-        end
-    
-        # Iterate over the keyspace.
-    def each(&block)
-      @items.each do |key, _|
-        yield key, get(key)
-      end
-    end
-    
-    describe ContactsController, :type => :controller do
-  describe '#index' do
-    before do
-      AppConfig.chat.enabled = true
-      @aspect = bob.aspects.create(:name => 'another aspect')
-      bob.share_with alice.person, @aspect
-      bob.share_with eve.person, @aspect
-      sign_in bob, scope: :user
-    end
-    
-          get :index, params: {conversation_id: @conv1.id}
-      save_fixture(html_for('body'), 'conversations_read')
-    end
-  end
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-        describe 'filter notifications' do
-      it 'supports filtering by notification type' do
-        FactoryGirl.create(:notification, :recipient => alice, :type => 'Notifications::StartedSharing')
-        get :index, params: {type: 'started_sharing'}
-        expect(assigns[:notifications].count).to eq(1)
-      end
-    
-        context 'with an authenticated user' do
-      before do
-        sign_in(bob, scope: :user)
-        allow(@controller).to receive(:current_user).and_return(bob)
-      end
-    
-          # Checks whether this node body is a void context.
-      #
-      # @return [Boolean] whether the `def` node body is a void context
-      def void_context?
-        method?(:initialize) || assignment_method?
-      end
-    
-    module RuboCop
-  module AST
-    # A node extension for `for` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `for` nodes within RuboCop.
-    class ForNode < Node
-      # Returns the keyword of the `for` statement as a string.
-      #
-      # @return [String] the keyword of the `until` statement
-      def keyword
-        'for'
-      end
-    
-          # This is used for duck typing with `pair` nodes which also appear as
-      # `hash` elements.
-      #
-      # @return [false]
-      def hash_rocket?
-        false
-      end
-    
-            names[name] -= 1
-        enabled_cops += 1
-      end
-    
-    module RuboCop
-  # Handles caching of configurations and association of inspected
-  # ruby files to configurations.
-  class ConfigStore
-    def initialize
-      # @options_config stores a config that is specified in the command line.
-      # This takes precedence over configs located in any directories
-      @options_config = nil
-    
-          def string_to_code string
-        # sha bytes
-        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
-        # Thanks donpark's IdenticonUtil.java for this.
-        # Match the following Java code
-        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
-        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
-    
-          def js # custom js
-        @js
-      end
-    
-          def has_sidebar
-        if @sidebar
-          @sidebar.formatted_data.strip.empty? ? false : true
-        else
-          @sidebar = (@page.sidebar || false)
-          !!@sidebar
-        end
-      end
-    
-        post '/edit/A', :header => 'header',
-         :footer            => 'footer', :page => 'A', :sidebar => 'sidebar', :message => 'def'
-    follow_redirect!
-    assert_equal '/A', last_request.fullpath
-    assert last_response.ok?
-    
-        post '/edit/' + CGI.escape('한글'), :page => 'k', :content => '바뀐 text',
-         :format                            => 'markdown', :message => 'ghi'
-    follow_redirect!
-    assert last_response.ok?
-    
-      begin
-    require 'gollum-lib'
-    wiki = Gollum::Wiki.new(gollum_path, wiki_options)
-    if !wiki.exist? then
-      raise Gollum::InvalidGitRepositoryError
-    end
-    if wiki_options[:plantuml_url]
-      Gollum::Filter::PlantUML.configure do |config|
-        puts 'Using #{wiki_options[:plantuml_url]} as PlantUML endpoint'
-        config.url = wiki_options[:plantuml_url]
-      end
-    end
-    puts
-    puts 'Loaded Gollum wiki at:'
-    puts '#{File.expand_path(gollum_path).inspect}'
-    puts
-    puts 'Example API calls:'
-    puts %(    page = wiki.page('page-name'))
-    puts %(    # => <Gollum::Page>)
-    puts
-    puts %(    page.raw_data)
-    puts %(    # => '# My wiki page')
-    puts
-    puts %(    page.formatted_data)
-    puts %(    # => '<h1>My wiki page</h1>')
-    puts
-    puts 'Full API documentation at:'
-    puts 'https://github.com/gollum/gollum-lib'
-    puts
-    IRB.start_session(binding)
-  rescue Gollum::InvalidGitRepositoryError, Gollum::NoSuchPathError
-    puts 'Invalid Gollum wiki at #{File.expand_path(gollum_path).inspect}'
-    exit 0
-  end
-else
-  require 'gollum/app'
-  Precious::App.set(:gollum_path, gollum_path)
-  Precious::App.set(:wiki_options, wiki_options)
-  Precious::App.settings.mustache[:templates] = wiki_options[:template_dir] if wiki_options[:template_dir]
-    
-      desc 'Install all spree gems'
-  task install: :build do
-    for_each_gem do |gem_path|
-      Bundler.with_clean_env do
-        sh 'gem install #{gem_path}'
-      end
-    end
-  end
-    
-          @@variant_attributes = [
-        :id, :name, :sku, :price, :weight, :height, :width, :depth, :is_master,
-        :slug, :description, :track_inventory
-      ]
-    
-    module RailsCommandHelpers
-  def framework_version?(version_string)
-    framework_version =~ /^#{version_string}/
-  end
-    
-    class PaperclipGenerator < ActiveRecord::Generators::Base
-  desc 'Create a migration to add paperclip-specific fields to your model. ' +
-       'The NAME argument is the name of your model, and the following ' +
-       'arguments are the name of the attachments'
-    
-    begin
-  # Use mime/types/columnar if available, for reduced memory usage
-  require 'mime/types/columnar'
-rescue LoadError
-  require 'mime/types'
-end
-    
-        def self.register(klass, attachment_name, attachment_options)
-      instance.register(klass, attachment_name, attachment_options)
-    end
-    
-        # Gives a Geometry representing the given height and width
-    def initialize(width = nil, height = nil, modifier = nil)
-      if width.is_a?(Hash)
-        options = width
-        @height = options[:height].to_f
-        @width = options[:width].to_f
-        @modifier = options[:modifier]
-        @orientation = options[:orientation].to_i
-      else
-        @height = height.to_f
-        @width  = width.to_f
-        @modifier = modifier
-      end
-    end
-    
-        def define_instance_getter
-      name = @name
-      options = @options
-    
-        private
-    
-          expect('.border-width-explicit').to have_rule(rule)
-    end
-  end
-    
-      context 'called with two sizes' do
-    it 'applies to alternating sides' do
-      rule = 'margin: 2px 3px'
-    
-      context 'called with arguments (2, $value: 4em 6em)' do
-    it 'outputs sextuple the second value from the default scale' do
-      expect('.two-double-value').to have_rule('font-size: 3.125em')
-    end
-  end
-end
 
+        
+              command_return = ActionCommandReturn.new(
+        return_value: action_return,
+        return_value_type: action_class_ref.return_type,
+        closure_argument_value: closure_argument_value
+      )
+      return command_return
+    end
     
-      context 'when user does not own the secret' do
-    let(:user) { build(:user) }
+          it 'should not be fooled by 10 local code signing identities available' do
+        allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
+        allow(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return('     10 valid identities found\n')
+        expect(FastlaneCore::UI).not_to(receive(:error))
+    
+            FastlaneCore::CommanderGenerator.new.generate(Cert::Options.available_options, command: c)
+    
+      before_action :set_account
+  respond_to :txt
+    
+      before_action :require_user!
+    
+      Devise.omniauth_configs.each_key do |provider|
+    provides_callback_for provider
+  end
+    
+      def create
+    @account = unfollow_attempt.try(:target_account)
+    
+            if echo?
+          $stdin.gets
+        else
+          $stdin.noecho(&:gets).tap { $stdout.print '\n' }
+        end
+      rescue Errno::EIO
+        # when stdio gets closed
+        return
+      end
+    
+            if built_in_scm_name?
+          load_built_in_scm
+        else
+          # Compatibility with existing 3.x third-party SCMs
+          register_legacy_scm_hooks
+          load_legacy_scm_by_name
+        end
+      end
+    
+        initialize_with { Tmuxinator::Project.new(file) }
+  end
+    
+      it 'calls Hooks.commands_from' do
+    expect(Tmuxinator::Hooks).to receive(:commands_from).
+      with(kind_of(Tmuxinator::Project), hook_name).once
+    project.send('hook_#{hook_name}')
+  end
+    
+        context 'installed' do
+      let(:version) { '2.4' }
+      it { is_expected.to eq version.to_f }
+    end
+  end
+    
+      describe '.shell?' do
+    context '$SHELL is set' do
+      before do
+        allow(ENV).to receive(:[]).with('SHELL') { 'vim' }
+      end
+    
+        desc 'copy [EXISTING] [NEW]', COMMANDS[:copy]
+    map 'c' => :copy
+    map 'cp' => :copy
+    
+        context 'new project already exists' do
+      before do
+        allow(Thor::LineEditor).to receive_messages(readline: 'y')
+      end
