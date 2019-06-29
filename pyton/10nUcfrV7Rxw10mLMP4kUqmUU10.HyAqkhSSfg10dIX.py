66838c7621671039a3d8af0383d06952d85fece2
SUNsung <weatherius@gmail.com>
2019-06-29 18:15:52 +0300

    Rewrite [10nUcfrV7Rxw10mLMP4kUqmUU10.HyAqkhSSfg10dIX.py] File in: 2019-06-29 18:15:51

--- pyton/10nUcfrV7Rxw10mLMP4kUqmUU10.HyAqkhSSfg10dIX.py
+++ pyton/10nUcfrV7Rxw10mLMP4kUqmUU10.HyAqkhSSfg10dIX.py
@@ -1,128 +1,7 @@
 
         
-            proc.sendline(u'ehco test')
-    
-    
-def plot_feature_times(all_times, batch_size, all_components, data):
-    plt.figure()
-    plot_results(all_components, all_times['pca'], label='PCA')
-    plot_results(all_components, all_times['ipca'],
-                 label='IncrementalPCA, bsize=%i' % batch_size)
-    plt.legend(loc='upper left')
-    plt.suptitle('Algorithm runtime vs. n_components\n \
-                 LFW, size %i x %i' % data.shape)
-    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
-    plt.ylabel('Time (seconds)')
-    
-    plt.figure('scikit-learn Ward's method benchmark results')
-plt.imshow(np.log(ratio), aspect='auto', origin='lower')
-plt.colorbar()
-plt.contour(ratio, levels=[1, ], colors='k')
-plt.yticks(range(len(n_features)), n_features.astype(np.int))
-plt.ylabel('N features')
-plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
-plt.xlabel('N samples')
-plt.title('Scikit's time, in units of scipy time (log)')
-plt.show()
-
-    
-        revision is a git commit reference (hash or name)
-    
-    For each class of models we make the model complexity vary through the choice
-of relevant model parameters and measure the influence on both computational
-performance (latency) and predictive power (MSE or Hamming Loss).
-'''
-    
-    In both examples below, the main result is that the empirical covariance
-estimate, as a non-robust one, is highly influenced by the heterogeneous
-structure of the observations. Although the robust covariance estimate is
-able to focus on the main mode of the data distribution, it sticks to the
-assumption that the data should be Gaussian distributed, yielding some biased
-estimation of the data structure, but yet accurate to some extent.
-The One-Class SVM does not assume any parametric form of the data distribution
-and can therefore model the complex shape of the data much better.
-    
-    iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-    
-    import numpy as np
-import matplotlib.pyplot as plt
-    
-    import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-    
-            colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a',
-                                             '#f781bf', '#a65628', '#984ea3',
-                                             '#999999', '#e41a1c', '#dede00']),
-                                      int(max(y_pred) + 1))))
-        plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[y_pred])
-    
-    # KMeans
-ax = fig.add_subplot(1, 3, 1)
-for k, col in zip(range(n_clusters), colors):
-    my_members = k_means_labels == k
-    cluster_center = k_means_cluster_centers[k]
-    ax.plot(X[my_members, 0], X[my_members, 1], 'w',
-            markerfacecolor=col, marker='.')
-    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-            markeredgecolor='k', markersize=6)
-ax.set_title('KMeans')
-ax.set_xticks(())
-ax.set_yticks(())
-plt.text(-3.5, 1.8,  'train time: %.2fs\ninertia: %f' % (
-    t_batch, k_means.inertia_))
-    
-    
-def setup(app):
-    app.add_crossref_type(
-        directivename = 'setting',
-        rolename      = 'setting',
-        indextemplate = 'pair: %s; setting',
-    )
-    app.add_crossref_type(
-        directivename = 'signal',
-        rolename      = 'signal',
-        indextemplate = 'pair: %s; signal',
-    )
-    app.add_crossref_type(
-        directivename = 'command',
-        rolename      = 'command',
-        indextemplate = 'pair: %s; command',
-    )
-    app.add_crossref_type(
-        directivename = 'reqmeta',
-        rolename      = 'reqmeta',
-        indextemplate = 'pair: %s; reqmeta',
-    )
-    app.add_role('source', source_role)
-    app.add_role('commit', commit_role)
-    app.add_role('issue', issue_role)
-    app.add_role('rev', rev_role)
-    
-            if now - self.lastmark >= 3:
-            self.lastmark = now
-            qps = len(self.tail) / sum(self.tail)
-            print('samplesize={0} concurrent={1} qps={2:0.2f}'.format(len(self.tail), self.concurrent, qps))
-    
-        def __init__(self, *a, **kw):
-        super(QPSSpider, self).__init__(*a, **kw)
-        if self.qps is not None:
-            self.qps = float(self.qps)
-            self.download_delay = 1 / self.qps
-        elif self.download_delay is not None:
-            self.download_delay = float(self.download_delay)
-    
-    # Ignore noisy twisted deprecation warnings
-import warnings
-warnings.filterwarnings('ignore', category=DeprecationWarning, module='twisted')
-del warnings
-    
-    from scrapy.commands import ScrapyCommand
-from scrapy.contracts import ContractsManager
-from scrapy.utils.misc import load_object
-from scrapy.utils.conf import build_component_list
+        
+class ScrapyCommand(object):
     
         def process_options(self, args, opts):
         ScrapyCommand.process_options(self, args, opts)
@@ -147,72 +26,67 @@ from scrapy.utils.conf import build_component_list
                                                                   tuple(valid_output_formats)))
             self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
     
-            editor = self.settings['EDITOR']
-        try:
-            spidercls = self.crawler_process.spider_loader.load(args[0])
-        except KeyError:
-            return self._err('Spider not found: %s' % args[0])
+            sfile = sys.modules[spidercls.__module__].__file__
+        sfile = sfile.replace('.pyc', '.py')
+        self.exitcode = os.system('%s '%s'' % (editor, sfile))
+
     
-        def _print_bytes(self, bytes_):
-        bytes_writer = sys.stdout if six.PY2 else sys.stdout.buffer
-        bytes_writer.write(bytes_ + b'\n')
-    
-            if opts.meta:
-            try:
-                opts.meta = json.loads(opts.meta)
-            except ValueError:
-                raise UsageError('Invalid -m/--meta value, pass a valid json string to -m or --meta. ' \
-                                'Example: --meta='{\'foo\' : \'bar\'}'', print_help=False)
-    
-        def process_options(self, args, opts):
-        ScrapyCommand.process_options(self, args, opts)
-        try:
-            opts.spargs = arglist_to_dict(opts.spargs)
-        except ValueError:
-            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
-        if opts.output:
-            if opts.output == '-':
-                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
-            else:
-                self.settings.set('FEED_URI', opts.output, priority='cmdline')
-            feed_exporters = without_none_values(self.settings.getwithbase('FEED_EXPORTERS'))
-            valid_output_formats = feed_exporters.keys()
-            if not opts.output_format:
-                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
-            if opts.output_format not in valid_output_formats:
-                raise UsageError('Unrecognized output format '%s', set one'
-                                 ' using the '-t' switch or as a file extension'
-                                 ' from the supported list %s' % (opts.output_format,
-                                                                  tuple(valid_output_formats)))
-            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
-    
-    if twisted_version >= (14, 0, 0):
-    
-            self._signer = None
-        if is_botocore():
-            import botocore.auth
-            import botocore.credentials
-            kw.pop('anon', None)
-            if kw:
-                raise TypeError('Unexpected keyword arguments: %s' % kw)
-            if not self.anon:
-                SignerCls = botocore.auth.AUTH_TYPE_MAPS['s3']
-                self._signer = SignerCls(botocore.credentials.Credentials(
-                    aws_access_key_id, aws_secret_access_key))
+        def _print_response(self, response, opts):
+        if opts.headers:
+            self._print_headers(response.request.headers, b'>')
+            print('>')
+            self._print_headers(response.headers, b'<')
         else:
-            _S3Connection = _get_boto_connection()
-            try:
-                self.conn = _S3Connection(
-                    aws_access_key_id, aws_secret_access_key, **kw)
-            except Exception as ex:
-                raise NotConfigured(str(ex))
+            self._print_bytes(response.body)
     
-            Same as Twisted's private _sslverify.ClientTLSOptions,
-        except that VerificationError, CertificateError and ValueError
-        exceptions are caught, so that the connection is not closed, only
-        logging warnings.
-        '''
+        def _genspider(self, module, name, domain, template_name, template_file):
+        '''Generate the spider module, based on the given template'''
+        tvars = {
+            'project_name': self.settings.get('BOT_NAME'),
+            'ProjectName': string_camelcase(self.settings.get('BOT_NAME')),
+            'module': module,
+            'name': name,
+            'domain': domain,
+            'classname': '%sSpider' % ''.join(s.capitalize() \
+                for s in module.split('_'))
+        }
+        if self.settings.get('NEWSPIDER_MODULE'):
+            spiders_module = import_module(self.settings['NEWSPIDER_MODULE'])
+            spiders_dir = abspath(dirname(spiders_module.__file__))
+        else:
+            spiders_module = None
+            spiders_dir = '.'
+        spider_file = '%s.py' % join(spiders_dir, module)
+        shutil.copyfile(template_file, spider_file)
+        render_templatefile(spider_file, **tvars)
+        print('Created spider %r using template %r ' % (name, \
+            template_name), end=('' if spiders_module else '\n'))
+        if spiders_module:
+            print('in module:\n  %s.%s' % (spiders_module.__name__, module))
     
-            if self.slot.start_requests is not None:
-            # not all start requests are handled
-            return False
+            dfd.addBoth(lambda _: logger.info('Spider closed (%(reason)s)',
+                                          {'reason': reason},
+                                          extra={'spider': spider}))
+    
+            backlogged, size = None, 0
+        from sentry.monitoring.queues import backend
+        if backend is not None:
+            size = backend.get_size('default')
+            backlogged = size > 0
+    
+            # Removing index on 'EventTag', fields ['environment_id', 'key', 'value']
+        db.delete_index(u'tagstore_eventtag', ['environment_id', 'key_id', 'value_id'])
+    
+    
+class Migration(SchemaMigration):
+    
+    :copyright: (c) 2010-2017 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+'''
+from __future__ import absolute_import
+    
+    
+@instrumented_task(name='sentry.tasks.activity.send_activity_notifications',
+                   queue='activity.notify')
+def send_activity_notifications(activity_id):
+    from sentry.models import Activity
