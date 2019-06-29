6346ee8cce68e78d3cc05c75bc97e73e5819c1d6
SUNsung <weatherius@gmail.com>
2019-06-29 15:25:10 +0300

    Rewrite [53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp] File in: 2019-06-29 15:25:10

--- cccccc/53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp
+++ cccccc/53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp
@@ -1,189 +1,188 @@
 
         
-        void swift::printOpaquePrefixMap(raw_ostream &out, void *_root,
-                         void (*printNodeData)(raw_ostream &out, void *node)) {
-  auto root = reinterpret_cast<Node*>(_root);
-  if (!root) {
-    out << '(empty)\n';
-    return;
-  }
-  TreePrinter(out, *printNodeData).print(root, ChildKind::Root);
-}
+          bool has_leaders;   // does the line contain leader dots (.....)?
+  bool has_drop_cap;  // does the line have a drop cap?
+  int pix_ldistance;  // distance to the left pblock boundary in pixels
+  int pix_rdistance;  // distance to the right pblock boundary in pixels
+  float pix_xheight;  // guessed xheight for the line
+  int average_interword_space; // average space between words in pixels.
     
-      if (clangDiag.getID() == clang::diag::err_module_not_built &&
-      CurrentImport && clangDiag.getArgStdStr(0) == CurrentImport->getName()) {
-    SourceLoc loc = DiagLoc;
-    if (clangDiag.getLocation().isValid())
-      loc = resolveSourceLocation(clangDiag.getSourceManager(),
-                                  clangDiag.getLocation());
+      // Sets the destination filename and enables images to be written to a PDF
+  // on destruction.
+  void WritePDF(const char* filename) {
+    if (pixaGetCount(pixa_) > 0) {
+      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, 'AllDebugImages', filename);
+      pixaClear(pixa_);
     }
-    
-    // Section 6.2: Decoding procedure
-    
-    AuthPropertyIterator& AuthPropertyIterator::operator++() {
-  grpc_auth_property_iterator iter = {ctx_, index_, name_};
-  property_ = grpc_auth_property_iterator_next(&iter);
-  ctx_ = iter.ctx;
-  index_ = iter.index;
-  name_ = iter.name;
-  return *this;
-}
-    
-    #include 'src/cpp/ext/filters/census/client_filter.h'
-    
-    // A CallData class will be created for every grpc call within a channel. It is
-// used to store data and methods specific to that call. CensusClientCallData is
-// thread-compatible, however typically only 1 thread should be interacting with
-// a call at a time.
-class CensusClientCallData : public CallData {
- public:
-  // Maximum size of trace context is sent on the wire.
-  static constexpr uint32_t kMaxTraceContextLen = 64;
-  // Maximum size of tags that are sent on the wire.
-  static constexpr uint32_t kMaxTagsLen = 2048;
-    }
-    
-    MeasureDouble RpcServerReceivedBytesPerRpc() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcServerReceivedBytesPerRpcMeasureName,
-      'Total bytes received across all messages per RPC', kUnitBytes);
-  return measure;
-}
-    
-    #include <grpc/support/port_platform.h>
-    
-    #include 'absl/strings/string_view.h'
-#include 'absl/time/clock.h'
-#include 'absl/time/time.h'
-#include 'include/grpc/grpc_security.h'
-#include 'src/cpp/ext/filters/census/channel_filter.h'
-#include 'src/cpp/ext/filters/census/context.h'
-    
-    #include <grpc/support/port_platform.h>
-    
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  FILETIME idle, kernel, user;
-  if (GetSystemTimes(&idle, &kernel, &user) != 0) {
-    total = FiletimeToInt(kernel) + FiletimeToInt(user);
-    busy = total - FiletimeToInt(idle);
   }
-  return std::make_pair(busy, total);
+    
+    /*!
+ * Copyright (c) 2016 by Contributors
+ * \file caffe_fieldentry.h
+ * \brief Implement FieldEntry<caffe::LayerParameter>
+ * \author Haoran Wang
+ */
+#ifndef PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
+#define PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
+    
+     private:
+  /*! \brief base iterator */
+  std::unique_ptr<IIterator<DataInst> > base_;
+  /*! whether mean image is ready */
+  bool meanfile_ready_;
+  /*! \brief output data */
+  DataInst out_;
+  // normalize parameter.
+  ImageNormalizeParam param_;
+  /*! \brief mean image, if needed */
+  mshadow::TensorContainer<cpu, 3> meanimg_;
+  /*! \brief temp space for output image */
+  mshadow::TensorContainer<cpu, 3> outimg_;
+  /*! \brief random numeber engine */
+  common::RANDOM_ENGINE rnd_;
+  // random magic number of this iterator
+  static const int kRandMagic = 0;
+    
+      ~PrefetcherIter() {
+    while (recycle_queue_.size() != 0) {
+      DataBatch *batch = recycle_queue_.front();
+      recycle_queue_.pop();
+      delete batch;
+    }
+    delete out_;
+    iter.Destroy();
+  }
+    
+    #ifndef MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
+#define MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
+#include <dmlc/parameter.h>
+#include <string>
+#include <utility>
+#include <vector>
+#include 'mxnet/ndarray.h'
+    
+    
+    {  if (has('dist')) {
+#if MXNET_USE_DIST_KVSTORE
+    kv = new kvstore::KVStoreDist(use_device_comm);
+    if (!has('_async') && kv->IsWorkerNode() && kv->get_rank() == 0) {
+      // configure the server to be the sync mode
+      kv->SendCommandToServers(static_cast<int>(kvstore::CommandType::kSyncMode), '');
+    }
+#else
+    LOG(FATAL) << 'compile with USE_DIST_KVSTORE=1 to use ' << tname;
+    return nullptr;
+#endif  // MXNET_USE_DIST_KVSTORE
+  } else {
+    if (has('nccl')) {
+#if MXNET_USE_NCCL
+      kv = new kvstore::KVStoreNCCL();
+#else
+      LOG(FATAL) << 'compile with USE_NCCL=1 to use ' << tname;
+      return nullptr;
+#endif
+    } else {
+      kv =  new kvstore::KVStoreLocal(use_device_comm);
+    }
+  }
+  kv->type_ = tname;
+  return kv;
 }
     
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
     
-            // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin('Another Window', &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text('Hello from another window!');
-            if (ImGui::Button('Close Me'))
-                show_another_window = false;
-            ImGui::End();
-        }
-    
-    IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_Init();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data);
-    
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'ID3D10ShaderResourceView*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-//  [X] Renderer: Support for large meshes (64k+ vertices) with 16-bits indices.
-    
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'GLuint' OpenGL texture identifier as void*/ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-//  [x] Renderer: Desktop GL only: Support for large meshes (64k+ vertices) with 16-bits indices.
-    
-    
-    {    // Rendering
-    ImGui::Render();
-    SDL_GL_MakeCurrent(g_Window, g_GLContext);
-    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-    glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    glClear(GL_COLOR_BUFFER_BIT);
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    SDL_GL_SwapWindow(g_Window);
-}
+    {
+    {.add_argument('data', 'Symbol or Symbol[]', 'Tensor or List of Tensors, the second input '
+'will be used as crop_like shape reference')
+.add_arguments(CropParam::__FIELDS__())
+.set_key_var_num_args('num_args');
+}  // namespace op
+}  // namespace mxnet
 
     
-        // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
     
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
-    
-        // By using D3DCompile() from <d3dcompiler.h> / d3dcompiler.lib, we introduce a dependency to a given version of d3dcompiler_XX.dll (see D3DCOMPILER_DLL_A)
-    // If you would like to use this DX10 sample code but remove this dependency you can:
-    //  1) compile once, save the compiled shader blobs into a file or source code and pass them to CreateVertexShader()/CreatePixelShader() [preferred solution]
-    //  2) use code to detect any version of the DLL and grab a pointer to D3DCompile from the DLL.
-    // See https://github.com/ocornut/imgui/pull/638 for sources and details.
-    
-    static void ImGui_ImplDX11_CreateFontsTexture()
-{
-    // Build texture atlas
-    ImGuiIO& io = ImGui::GetIO();
-    unsigned char* pixels;
-    int width, height;
-    io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
-    }
-    
-        // Upload vertex/index data into a single contiguous GPU buffer
-    void* vtx_resource, *idx_resource;
-    D3D12_RANGE range;
-    memset(&range, 0, sizeof(D3D12_RANGE));
-    if (fr->VertexBuffer->Map(0, &range, &vtx_resource) != S_OK)
-        return;
-    if (fr->IndexBuffer->Map(0, &range, &idx_resource) != S_OK)
-        return;
-    ImDrawVert* vtx_dst = (ImDrawVert*)vtx_resource;
-    ImDrawIdx* idx_dst = (ImDrawIdx*)idx_resource;
-    for (int n = 0; n < draw_data->CmdListsCount; n++)
     {
-        const ImDrawList* cmd_list = draw_data->CmdLists[n];
-        memcpy(vtx_dst, cmd_list->VtxBuffer.Data, cmd_list->VtxBuffer.Size * sizeof(ImDrawVert));
-        memcpy(idx_dst, cmd_list->IdxBuffer.Data, cmd_list->IdxBuffer.Size * sizeof(ImDrawIdx));
-        vtx_dst += cmd_list->VtxBuffer.Size;
-        idx_dst += cmd_list->IdxBuffer.Size;
-    }
-    fr->VertexBuffer->Unmap(0, &range);
-    fr->IndexBuffer->Unmap(0, &range);
-    
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'LPDIRECT3DTEXTURE9' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-//  [X] Renderer: Support for large meshes (64k+ vertices) with 16-bits indices.
-    
-    
-    {    inline ImDrawListSplitter() { Clear(); }
-    inline void                 Clear() { _Current = 0; _Count = 1; } // Do not clear Channels[] so our allocations are reused next frame
-    IMGUI_API void              ClearFreeMemory();
-    IMGUI_API void              Split(ImDrawList* draw_list, int count);
-    IMGUI_API void              Merge(ImDrawList* draw_list);
-    IMGUI_API void              SetCurrentChannel(ImDrawList* draw_list, int channel_idx);
+    {
+    {  bool init_cudnn_;
+  cudnnDataType_t dtype_;
+  cudnnSpatialTransformerDescriptor_t st_desc_;
+  cudnnTensorDescriptor_t in_desc_;
+  cudnnTensorDescriptor_t out_desc_;
+  cudnnSamplerType_t sampler_;
+  #if CUDNN_MAJOR >= 5
+  cudnnTensorFormat_t format_;
+  #endif
+  BilinearSamplerParam param_;
 };
+#endif  // __CUDACC__ && CUDNN
+}  // namespace op
+}  // namespace mxnet
     
-    int main()
-{
-    // create JSON array
-    json array = {'first', '2nd', 'third', 'fourth'};
+    namespace mxnet {
+namespace op {
+template<typename DType>
+class CuDNNLocalResponseNormOp : public Operator {
+ public:
+  explicit CuDNNLocalResponseNormOp(LRNParam param) {
+    param_ = param;
+    init_cudnn_ = false;
+    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
+  }
+    }
+    }
     }
     
-        // out_of_range.109
-    try
-    {
-        // try to use an array index that is not a number
-        json::const_reference ref = j.at('/array/one'_json_pointer);
+    DMLC_REGISTER_PARAMETER(NativeOpParam);
+    
+    ScientificNumberFormatter *ScientificNumberFormatter::createInstance(
+            DecimalFormat *fmtToAdopt,
+            Style *styleToAdopt,
+            UErrorCode &status) {
+    LocalPointer<DecimalFormat> fmt(fmtToAdopt);
+    LocalPointer<Style> style(styleToAdopt);
+    if (U_FAILURE(status)) {
+        return NULL;
     }
-    catch (json::parse_error& e)
-    {
-        std::cout << e.what() << '\n';
+    ScientificNumberFormatter *result =
+            new ScientificNumberFormatter(
+                    fmt.getAlias(),
+                    style.getAlias(),
+                    status);
+    if (result == NULL) {
+        status = U_MEMORY_ALLOCATION_ERROR;
+        return NULL;
     }
+    fmt.orphan();
+    style.orphan();
+    if (U_FAILURE(status)) {
+        delete result;
+        return NULL;
+    }
+    return result;
+}
+    
+    #endif /* #if !UCONFIG_NO_BREAK_ITERATION */
+
+    
+    U_NAMESPACE_BEGIN
+    
+    #endif
+
+    
+        // Compare the date to the starting and ending rules.+1 = date>rule, -1
+    // = date<rule, 0 = date==rule.
+    int32_t startCompare = compareToRule((int8_t)month, (int8_t)monthLength, (int8_t)prevMonthLength,
+                                         (int8_t)day, (int8_t)dayOfWeek, millis,
+                                         startTimeMode == UTC_TIME ? -rawOffset : 0,
+                                         startMode, (int8_t)startMonth, (int8_t)startDayOfWeek,
+                                         (int8_t)startDay, startTime);
+    int32_t endCompare = 0;
+    
+    
+    {    return result;
+}
+#endif
+    
+        /**
+     * ICU 'poor man's RTTI', returns a UClassID for this class.
+     */
+    static UClassID U_EXPORT2 getStaticClassID();
