4a08a1e9c98e096fb43ef1b1bc0b309334d88881
SUNsung <weatherius@gmail.com>
2019-07-01 00:58:51 +0300

    Rewrite [06SmRr8sizEMc068kC1sjeo1Ko064esCRSiQYvU06dzn.cpp] File in: 2019-07-01 00:58:50

--- cccccc/06SmRr8sizEMc068kC1sjeo1Ko064esCRSiQYvU06dzn.cpp
+++ cccccc/06SmRr8sizEMc068kC1sjeo1Ko064esCRSiQYvU06dzn.cpp
@@ -1,273 +1,254 @@
 
         
-        RequirementEnvironment::RequirementEnvironment(
-                                           DeclContext *conformanceDC,
-                                           GenericSignature *reqSig,
-                                           ProtocolDecl *proto,
-                                           ClassDecl *covariantSelf,
-                                           ProtocolConformance *conformance)
-    : reqSig(reqSig) {
-  ASTContext &ctx = conformanceDC->getASTContext();
+            typedef ptrdiff_t  stride_t;
+    
+        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0,
+                     const typename internal::VecTraits<T>::vec64 & v_src1,
+                     typename internal::VecTraits<T>::vec64 & v_dst) const
+    {
+        typename internal::VecTraits<T>::vec64 v_min = internal::vmin(v_src0, v_src1);
+        typename internal::VecTraits<T>::vec64 v_max = internal::vmax(v_src0, v_src1);
+        v_dst = internal::vqsub(v_max, v_min);
     }
     
-    namespace clang {
-  class RecordDecl;
-  class TypedefNameDecl;
+    #define IMPL_ADDWEIGHTED(type)                                \
+void addWeighted(const Size2D &,                              \
+                 const type *, ptrdiff_t,                     \
+                 const type *, ptrdiff_t,                     \
+                 type *, ptrdiff_t,                           \
+                 f32, f32, f32)                               \
+{                                                             \
+    internal::assertSupportedConfiguration();                 \
 }
     
-    namespace clang {
-class Sema;
-class NamedDecl;
-class TypeDecl;
-class FunctionDecl;
+    #include 'common.hpp'
+    
+     *Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+    
+    
+    {
+    {
+    {            // make shift
+            prevx = currx;
+            currx = nextx;
+        }
+    }
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)border;
+    (void)borderValue;
+#endif
 }
     
-    #ifndef SWIFT_FRONTENDTOOL_REFERENCEDEPENDENCIES_H
-#define SWIFT_FRONTENDTOOL_REFERENCEDEPENDENCIES_H
+    REGISTER_CPU_OPERATOR(EnforceFinite, EnforceFiniteOp<CPUContext>);
+    
+    **Result**
+    
+    REGISTER_CUDA_OPERATOR(LC3D, LocallyConnectedOp<float, CUDAContext>);
+REGISTER_CUDA_OPERATOR(
+    LC3DGradient,
+    LocallyConnectedGradientOp<float, CUDAContext>);
+    
+    #include <vector>
+#include 'Ratpack/ratpak.h'
+    
+    // WARNING: IDC_SIGN is a special unary op but still this doesn't catch this. Caller has to be aware
+// of it and catch it themselves or not needing this
+bool IsUnaryOpCode(OpCode opCode)
+{
+    return IsOpInRange(opCode, IDC_UNARYFIRST, IDC_UNARYLAST);
+}
+    
+                /* Return complement. */
+        case IDC_COM:
+            if (m_radix == 10 && !m_fIntegerMode)
+            {
+                result = -(RationalMath::Integer(rat) + 1);
+            }
+            else
+            {
+                result = rat ^ m_chopNumbers[m_numwidth];
+            }
+            break;
+    
+    #include <array>
+#include 'ICalcDisplay.h'
+#include 'IHistoryDisplay.h'
+#include 'Rational.h'
     
     
-    {  Alignment getAlignment() const {
-    return Align;
+    {                    if (fBadExponent)
+                    {
+                        throw(CALC_E_DOMAIN);
+                    }
+                }
+                else
+                {
+                    // If the exponent is not odd disregard the sign.
+                    sign = 1;
+                }
+    
+    void ascalerat(_Inout_ PRAT* pa, ANGLE_TYPE angletype, int32_t precision)
+{
+    switch (angletype)
+    {
+    case ANGLE_RAD:
+        break;
+    case ANGLE_DEG:
+        divrat(pa, two_pi, precision);
+        mulrat(pa, rat_360, precision);
+        break;
+    case ANGLE_GRAD:
+        divrat(pa, two_pi, precision);
+        mulrat(pa, rat_400, precision);
+        break;
+    }
+}
+    
+    void modrat(PRAT* pa, PRAT b)
+{
+    // contrary to remrat(X, 0) returning 0, modrat(X, 0) must return X
+    if (zerrat(b))
+    {
+        return;
+    }
+    }
+    
+    {
+    // Only do the multiply if it isn't zero.
+    if (!zernum((*pa)->pp))
+    {
+        mulnumx(&((*pa)->pp), b->pp);
+        mulnumx(&((*pa)->pq), b->pq);
+        trimit(pa, precision);
+    }
+    else
+    {
+        // If it is zero, blast a one in the denominator.
+        DUPNUM(((*pa)->pq), num_one);
+    }
+    }
+    
+    /*!
+ * \file graph_attr_types.h
+ * \brief Data structures that can appear in graph attributes.
+ */
+#ifndef MXNET_GRAPH_ATTR_TYPES_H_
+#define MXNET_GRAPH_ATTR_TYPES_H_
+    
+    namespace mxnet {
+namespace op {
+namespace caffe {
+    }
+    }
+    }
+    
+    MXNET_REGISTER_IO_ITER(CaffeDataIter)
+.describe('Create MxNet iterator for a Caffe data layer.')
+.add_arguments(CaffeDataParam::__FIELDS__())
+.add_arguments(PrefetcherParam::__FIELDS__())
+.set_body([]() {
+    return new CaffeDataIterWrapper();
+});
+    
+        if (param_.mean_r > 0.0f || param_.mean_g > 0.0f ||
+        param_.mean_b > 0.0f || param_.mean_a > 0.0f) {
+      // subtract mean per channel
+      data[0] -= param_.mean_r;
+      if (data.shape_[0] >= 3) {
+        data[1] -= param_.mean_g;
+        data[2] -= param_.mean_b;
+      }
+      if (data.shape_[0] == 4) {
+        data[3] -= param_.mean_a;
+      }
+    } else if (!meanfile_ready_ || param_.mean_img.length() == 0) {
+      // do not subtract anything
+    } else {
+      CHECK(meanfile_ready_);
+      data -= meanimg_;
+    }
+    
+      virtual bool Next(void) {
+    if (out_ != nullptr) {
+      recycle_queue_.push(out_); out_ = nullptr;
+    }
+    // do recycle
+    if (recycle_queue_.size() == param_.prefetch_buffer) {
+      DataBatch *old_batch =  recycle_queue_.front();
+      // can be more efficient on engine
+      for (NDArray& arr : old_batch->data) {
+        arr.WaitToWrite();
+      }
+      recycle_queue_.pop();
+      iter.Recycle(&old_batch);
+    }
+    return iter.Next(&out_);
   }
-  
-  llvm::PointerType *getType() const {
-    return cast<llvm::PointerType>(Addr->getType());
+  virtual const DataBatch &Value(void) const {
+    return *out_;
+  }
+    
+      /*!
+   * \brief sets two bit gradient compression
+   * \param threshold float value used for thresholding gradients
+   */
+  void SetTwoBitCompression(const float threshold);
+    
+    
+    {
+    {NNVM_REGISTER_OP(IdentityAttachKLSparseReg)
+.set_attr<nnvm::FSetInputVarAttrOnCompose>('FSetInputVarAttrOnCompose',
+    [](const nnvm::NodeAttrs& attrs, nnvm::NodePtr var, const int index) {
+      if (var->attrs.dict.find('__init__') != var->attrs.dict.end()) return;
+      if (index == 1) {
+        var->attrs.dict['__init__'] = '[\'zero\', {}]';
+      }
+    });
+}  // namespace op
+}  // namespace mxnet
+    
+    namespace HPHP { namespace HHBBC {
+    }
+    }
+    
+    struct Assembler {
+    }
+    
+    struct ObjectData;
+struct Object;
+    
+      const char* data = filename.data();
+  int data_len = filename.length();
+  bool base64 = false;
+  if (strncmp(data, 'data:', sizeof('data:') - 1)) {
+    return nullptr;
+  }
+  data += sizeof('data:') - 1;
+  data_len -= sizeof('data:') - 1;
+    
+    #ifndef incl_HPHP_PERF_EVENT_H_
+#define incl_HPHP_PERF_EVENT_H_
+    
+    #include 'hphp/util/stack-trace.h'
+    
+    
+    {  size_t operator()(const copy_ptr<Map>& m) const {
+    auto ret = m->size();
+    for (auto& kv : *m) {
+      ret = folly::hash::hash_combine(ret, kv.first.get());
+      ret = folly::hash::hash_combine(ret,
+                                      static_cast<int64_t>(kv.second.m_type));
+      if (!isNullType(kv.second.m_type)) {
+        ret = folly::hash::hash_combine(ret, kv.second.m_data.num);
+      }
+    }
+    return ret;
   }
 };
-    
-    
-    {  // Moving messages on two different arenas should lead to a copy.
-  *message2_on_arena = std::move(*message1_on_arena);
-  EXPECT_NE(nested, &message2_on_arena->optional_nested_message());
-  TestUtil::ExpectAllFieldsSet(*message1_on_arena);
-  TestUtil::ExpectAllFieldsSet(*message2_on_arena);
-}
-    
-      desired_output_for_decode = 'ABCD__EfghI_j';
-  expected = string('\x64\x80\xC5\xA1\x0', 5);
-  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
-                                                     desired_output_for_decode);
-  EXPECT_EQ(expected, result);
-    
-    #include <google/protobuf/stubs/logging.h>
-#include <google/protobuf/stubs/common.h>
-    
-    string Status::ToString() const {
-  if (error_code_ == error::OK) {
-    return 'OK';
-  } else {
-    if (error_message_.empty()) {
-      return error::CodeEnumToString(error_code_);
-    } else {
-      return error::CodeEnumToString(error_code_) + ':' +
-          error_message_;
-    }
-  }
-}
-    
-    TEST(StructurallyValidTest, ValidUTF8String) {
-  // On GCC, this string can be written as:
-  //   'abcd 1234 - \u2014\u2013\u2212'
-  // MSVC seems to interpret \u differently.
-  string valid_str('abcd 1234 - \342\200\224\342\200\223\342\210\222 - xyz789');
-  EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data(),
-                                      valid_str.size()));
-  // Additional check for pointer alignment
-  for (int i = 1; i < 8; ++i) {
-    EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data() + i,
-                                        valid_str.size() - i));
-  }
-}
-    
-    string StripProto(string filename) {
-  return filename.substr(0, filename.rfind('.proto'));
-}
-    
-     private:
-  void ScrubEnum(EnumDescriptorProto *enum_type) {
-    if (enum_type->value(0).number() != 0) {
-      bool has_zero = false;
-      for (int j = 0; j < enum_type->value().size(); j++) {
-        if (enum_type->value(j).number() == 0) {
-          EnumValueDescriptorProto temp_enum_value;
-          temp_enum_value.CopyFrom(enum_type->value(j));
-          enum_type->mutable_value(j)->CopyFrom(enum_type->value(0));
-          enum_type->mutable_value(0)->CopyFrom(temp_enum_value);
-          has_zero = true;
-          break;
-        }
-      }
-      if (!has_zero) {
-        enum_type->mutable_value()->Add();
-        for (int i = enum_type->mutable_value()->size() - 1; i > 0; i--) {
-          enum_type->mutable_value(i)->CopyFrom(
-              *enum_type->mutable_value(i - 1));
-        }
-        enum_type->mutable_value(0)->set_number(0);
-        enum_type->mutable_value(0)->set_name('ADDED_ZERO_VALUE_' +
-                                              std::to_string(total_added_++));
-      }
-    }
-    }
-    
-    
-    {
-    {
-    {      switch (phone_number.type()) {
-        case tutorial::Person::MOBILE:
-          cout << '  Mobile phone #: ';
-          break;
-        case tutorial::Person::HOME:
-          cout << '  Home phone #: ';
-          break;
-        case tutorial::Person::WORK:
-          cout << '  Work phone #: ';
-          break;
-        default:
-          cout << '  Unknown phone #: ';
-          break;
-      }
-      cout << phone_number.number() << endl;
-    }
-    if (person.has_last_updated()) {
-      cout << '  Updated: ' << TimeUtil::ToString(person.last_updated()) << endl;
-    }
-  }
-}
-    
-    void Tesseract::PrerecAllWordsPar(const GenericVector<WordData>& words) {
-  // Prepare all the blobs.
-  GenericVector<BlobData> blobs;
-  for (int w = 0; w < words.size(); ++w) {
-    if (words[w].word->ratings != nullptr &&
-        words[w].word->ratings->get(0, 0) == nullptr) {
-      for (int s = 0; s < words[w].lang_words.size(); ++s) {
-        Tesseract* sub = s < sub_langs_.size() ? sub_langs_[s] : this;
-        const WERD_RES& word = *words[w].lang_words[s];
-        for (int b = 0; b < word.chopped_word->NumBlobs(); ++b) {
-          blobs.push_back(BlobData(b, sub, word));
-        }
-      }
-    }
-  }
-  // Pre-classify all the blobs.
-  if (tessedit_parallelize > 1) {
-#ifdef _OPENMP
-#pragma omp parallel for num_threads(10)
-#endif  // _OPENMP
-    for (int b = 0; b < blobs.size(); ++b) {
-      *blobs[b].choices =
-          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
-    }
-  } else {
-    // TODO(AMD) parallelize this.
-    for (int b = 0; b < blobs.size(); ++b) {
-      *blobs[b].choices =
-          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
-    }
-  }
-}
-    
-    // Main entry point for Paragraph Detection Algorithm.
-//
-// Given a set of equally spaced textlines (described by row_infos),
-// Split them into paragraphs.  See http://goto/paragraphstalk
-//
-// Output:
-//   row_owners - one pointer for each row, to the paragraph it belongs to.
-//   paragraphs - this is the actual list of PARA objects.
-//   models - the list of paragraph models referenced by the PARA objects.
-//            caller is responsible for deleting the models.
-void DetectParagraphs(int debug_level,
-                      GenericVector<RowInfo> *row_infos,
-                      GenericVector<PARA *> *row_owners,
-                      PARA_LIST *paragraphs,
-                      GenericVector<ParagraphModel *> *models);
-    
-      // Get the value of the top (smallest, defined by operator< ) element.
-  const Pair& PeekTop() const {
-    return heap_[0];
-  }
-  // Get the value of the worst (largest, defined by operator< ) element.
-  const Pair& PeekWorst() const { return heap_[IndexOfWorst()]; }
-    
-      static void OnDoneRecvTrailingMetadataCb(void* user_data, grpc_error* error);
-    
-    MeasureInt64 RpcServerSentMessagesPerRpc() {
-  static const auto measure =
-      MeasureInt64::Register(kRpcServerSentMessagesPerRpcMeasureName,
-                             'Number of messages sent per RPC', kCount);
-  return measure;
-}
-    
-    namespace grpc {
-    }
-    
-    namespace {
-    }
-    
-    #ifndef GRPC_INTERNAL_CPP_UTIL_CORE_STATS_H
-#define GRPC_INTERNAL_CPP_UTIL_CORE_STATS_H
-    
-    void TimepointHR2Timespec(const high_resolution_clock::time_point& from,
-                          gpr_timespec* to) {
-  high_resolution_clock::duration deadline = from.time_since_epoch();
-  seconds secs = duration_cast<seconds>(deadline);
-  if (from == high_resolution_clock::time_point::max() ||
-      secs.count() >= gpr_inf_future(GPR_CLOCK_REALTIME).tv_sec ||
-      secs.count() < 0) {
-    *to = gpr_inf_future(GPR_CLOCK_REALTIME);
-    return;
-  }
-  nanoseconds nsecs = duration_cast<nanoseconds>(deadline - secs);
-  to->tv_sec = static_cast<int64_t>(secs.count());
-  to->tv_nsec = static_cast<int32_t>(nsecs.count());
-  to->clock_type = GPR_CLOCK_REALTIME;
-}
-    
-      Hash hash(HASH_TYPE_SHA1);
-  hash.update(hn.c_str(), hn.size());
-  auto hn_hash = hash.digest();
-    
-      fpack.platform_ = 'freebsd';
-  if (isPlatform(PlatformType::TYPE_FREEBSD)) {
-    EXPECT_TRUE(fpack.checkPlatform());
-  } else {
-    EXPECT_FALSE(fpack.checkPlatform());
-  }
-    
-    ExpectedSuccess<DatabaseError> RocksdbDatabase::putRawBytes(
-    const std::string& domain,
-    const std::string& key,
-    const std::string& value) {
-  auto handle = getHandle(domain);
-  if (handle) {
-    std::shared_ptr<Handle> handle_ptr = handle.take();
-    return putRawBytesInternal(handle_ptr.get(), key, value);
-  }
-  return handle.takeError();
-}
-    
-    
-    {  fs::rename(src_path, dst_path, ec);
-  if (!ec) {
-    return createError(RocksdbMigrationError::FailMoveDatabase)
-           << 'Move failed: ' << ec.value() << ' ' << ec.message();
-  }
-  return Success();
-}
-    
-    #include <osquery/utils/expected/expected.h>
-    
-      // Initialize the distributed plugin, if necessary
-  if (!FLAGS_disable_distributed) {
-    initActivePlugin('distributed', FLAGS_distributed_plugin);
-  }
-    
-    TEST_F(RocksdbDatabaseTest, test_open) {
-  auto path = randomDBPath();
-  auto db = std::make_unique<RocksdbDatabase>('test', path_);
-  auto result = db->open();
-  EXPECT_TRUE(result);
-  db->close();
-}
