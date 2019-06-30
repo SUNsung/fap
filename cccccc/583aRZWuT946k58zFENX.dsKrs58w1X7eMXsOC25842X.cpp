b953edd9ce4dc56ce1919037fdbacbba920d6173
SUNsung <weatherius@gmail.com>
2019-06-30 12:24:57 +0300

    Rewrite [583aRZWuT946k58zFENX.dsKrs58w1X7eMXsOC25842X.cpp] File in: 2019-06-30 12:24:57

--- cccccc/583aRZWuT946k58zFENX.dsKrs58w1X7eMXsOC25842X.cpp
+++ cccccc/583aRZWuT946k58zFENX.dsKrs58w1X7eMXsOC25842X.cpp
@@ -1,37 +1,89 @@
 
         
-        // Hash function for PyBfloat16. We use the identity function, which is a weak
-// hash function.
-HashType PyBfloat16_Hash(PyObject* self) {
-  bfloat16 x = reinterpret_cast<PyBfloat16*>(self)->value;
-  return x.value;
+            // Ignore first non-switch arg if it's not a standalone package.
+    bool ignore_arg = !package->self_extract();
+    for (unsigned i = 1; i < argv.size(); ++i) {
+      if (ignore_arg && argv[i] == args[0]) {
+        ignore_arg = false;
+        continue;
+      }
+    }
+    
+      virtual void Call(const std::string& method,
+                    const base::ListValue& arguments,
+                    content::RenderFrameHost* rvh = nullptr);
+  virtual void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result);
+    
+    v8::Handle<v8::Value> DeallocateObject(int routing_id,
+                                       int object_id) {
+  RenderThread::Get()->Send(new ShellViewHostMsg_Deallocate_Object(
+      routing_id, object_id));
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  return v8::Undefined(isolate);
 }
     
-    #include <Python.h>
     
-    namespace tensorflow {
+    {}  // namespace remote
+    
+    namespace nwapi {
     }
     
-    namespace stream_executor {
-namespace cuda {
-    }
+    namespace nwapi {
     }
     
-      // Computes real-to-complex FFT in forward direction.
-  virtual bool DoFft(Stream *stream, Plan *plan,
-                     const DeviceMemory<float> &input,
-                     DeviceMemory<std::complex<float>> *output) = 0;
-  virtual bool DoFft(Stream *stream, Plan *plan,
-                     const DeviceMemory<double> &input,
-                     DeviceMemory<std::complex<double>> *output) = 0;
     
-    namespace stream_executor {
-namespace host {
-    }
+    {}  // namespace nw
+
+    
+        std::string error() {
+      return error_;
     }
     
-      // Begins the timer at the present point in the stream.
-  bool Start(Stream *stream);
+    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringWithFixupFunction(){}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     
-      // Indicates a preference for more L1 cache than shared memory.
-  kPreferL1,
+ protected:
+  ~NwMenuGetNSStringWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
+};
+    
+    NwObjDestroyFunction::~NwObjDestroyFunction() {
+}
+    
+    #endif //NW_SRC_API_NW_SCREEN_API_H_
+
+    
+    
+    {        Rectangle<T> operator/(const T value) const;
+    };
+    
+    #include <openpose/pose/poseParameters.hpp>
+#include <openpose/pose/poseParametersRender.hpp>
+    
+    namespace op
+{
+    struct PersonEntry
+    {
+        long long counterLastDetection;
+        std::vector<cv::Point2f> keypoints;
+        std::vector<char> status;
+        /*
+        PersonEntry(long long _last_frame, 
+                    std::vector<cv::Point2f> _keypoints,
+                    std::vector<char> _active):
+                    last_frame(_last_frame), keypoints(_keypoints),
+                    active(_active)
+                    {}
+        */
+    };
+    class OP_API PersonIdExtractor
+    {
+    }
+    }
