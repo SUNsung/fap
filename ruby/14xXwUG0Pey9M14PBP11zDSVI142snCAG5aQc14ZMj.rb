f61cf901fb3fb938105c6f8a5d11cb02fbf00e8c
SUNsung <weatherius@gmail.com>
2019-07-01 00:08:10 +0300

    Rewrite [14xXwUG0Pey9M14PBP11zDSVI142snCAG5aQc14ZMj.rb] File in: 2019-07-01 00:08:09

--- ruby/14xXwUG0Pey9M14PBP11zDSVI142snCAG5aQc14ZMj.rb
+++ ruby/14xXwUG0Pey9M14PBP11zDSVI142snCAG5aQc14ZMj.rb
@@ -1,154 +1,137 @@
 
         
-        # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
+        end
+
     
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+        it 'does not output links to other agents outside of the incoming set' do
+      Link.create!(:source_id => agents(:jane_weather_agent).id, :receiver_id => agents(:jane_website_agent).id)
+      Link.create!(:source_id => agents(:jane_website_agent).id, :receiver_id => agents(:jane_rain_notifier_agent).id)
     
-    class Devise::OmniauthCallbacksController < DeviseController
-  prepend_before_action { request.env['devise.skip_timeout'] = true }
+        it 'should raise error when invalid response arrives' do
+      stub(HTTParty).post { {'blah' => 'blah'} }
+      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Invalid response from Boxcar:/)
+    end
     
-      # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
+        json
   end
     
-          def remember_cookie_values(resource)
-        options = { httponly: true }
-        options.merge!(forget_cookie_values(resource))
-        options.merge!(
-          value: resource.class.serialize_into_cookie(resource),
-          expires: resource.remember_expires_at
-        )
-      end
     
-                if class_mod.respond_to?(:available_configs)
-              available_configs = class_mod.available_configs
-              available_configs.each do |config|
-                next unless options.key?(config)
-                send(:'#{config}=', options.delete(config))
-              end
-            end
+  #
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
+  }
+    
+            # Receives a Kerberos Response over a tcp connection
+        #
+        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] the kerberos message response
+        # @raise [RuntimeError] if the response can't be processed
+        # @raise [EOFError] if expected data can't be read
+        def recv_response_tcp
+          length_raw = connection.get_once(4, timeout)
+          unless length_raw && length_raw.length == 4
+            raise ::RuntimeError, 'Kerberos Client: failed to read response'
+          end
+          length = length_raw.unpack('N')[0]
+    
+              # Decodes the auth_time field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Time]
+          def decode_auth_time(input)
+            input.value[0].value
           end
     
-    require 'devise/hooks/timeoutable'
+              # Encodes the msg_type field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_msg_type
+            bn = OpenSSL::BN.new(msg_type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
     
-    module RuboCop
-  module AST
-    # A node extension for `def` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `def` nodes within RuboCop.
-    class DefNode < Node
-      include ParameterizedNode
-      include MethodIdentifierPredicates
+      before_action { @server = organization.servers.present.find_by_permalink!(params[:server_id]) }
+  before_action { params[:id] && @credential = @server.credentials.find_by_key!(params[:id]) }
     
-    module Jekyll
-    
-    module Jekyll
-  class GistTag < Liquid::Tag
-    def initialize(tag_name, text, token)
-      super
-      @text           = text
-      @cache_disabled = false
-      @cache_folder   = File.expand_path '../.gist-cache', File.dirname(__FILE__)
-      FileUtils.mkdir_p @cache_folder
+      before_action do
+    if params[:server_id]
+      @server = organization.servers.present.find_by_permalink!(params[:server_id])
+      params[:id] && @ip_pool_rule = @server.ip_pool_rules.find_by_uuid!(params[:id])
+    else
+      params[:id] && @ip_pool_rule = organization.ip_pool_rules.find_by_uuid!(params[:id])
     end
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-        def render(context)
-      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
-      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
-      file = code_path + @file
-    
-      # Extracts raw content DIV from template, used for page description as {{ content }}
-  # contains complete sub-template code on main page level
-  def raw_content(input)
-    /<div class='entry-content'>(?<content>[\s\S]*?)<\/div>\s*<(footer|\/article)>/ =~ input
-    return (content.nil?) ? input : content
   end
     
-        it 'accepts jsfiddle link with a custom-tab parameter' do
-      expect do
-        generate_new_liquid(jsfiddle_link_with_custom_tabs)
-      end.not_to raise_error
+      def check
+    if @track_domain.check_dns
+      redirect_to_with_json [organization, @server, :track_domains], :notice => 'Your CNAME for #{@track_domain.full_name} looks good!'
+    else
+      redirect_to_with_json [organization, @server, :track_domains], :alert => 'There seems to be something wrong with your DNS record. Check documentation for information.'
     end
-    
-          it 'unmutes the parent comment if already muted' do
-        sign_in original_commenter
-        parent_comment_by_og.update(receive_notifications: false)
-        patch '/comment_mutes/#{parent_comment_by_og.id}', params: { comment: { receive_notifications: 'true' } }
-        expect(parent_comment_by_og.reload.receive_notifications).to eq true
-      end
-    end
-    
-      def create
-    @broadcast = Broadcast.new(broadcast_params)
-    redirect_to '/internal/broadcasts'
   end
     
-        it 'doesn't call the processor if the condition method returns false' do
-      uploader_class.process :resize => [200, 300], :if => :false?
-      uploader_class.process :fancy, :if => :false?
-      expect(uploader).to receive(:false?).with('test.jpg').twice.and_return(false)
-      expect(uploader).not_to receive(:resize)
-      expect(uploader).not_to receive(:fancy)
-      uploader.process!('test.jpg')
+      def style_width(width, options = {})
+    width = 100 if width > 100.0
+    width = 0 if width < 0.0
+    style = 'width:#{width}%;'
+    if options[:color]
+      if width >= 100
+        style += ' background-color:#e2383a;'
+      elsif width >= 90
+        style += ' background-color:#e8581f;'
+      end
+    end
+    style
+  end
+    
+        def each(&block)
+      @cookies.each(&block)
     end
     
-    When /^I delete the datamapper record$/ do
-  @instance.destroy
+              if args.include?(:all)
+            namespace_inheritable(:rescue_all, true)
+            namespace_inheritable :all_rescue_handler, handler
+          elsif args.include?(:grape_exceptions)
+            namespace_inheritable(:rescue_all, true)
+            namespace_inheritable(:rescue_grape_exceptions, true)
+          else
+            handler_type =
+              case options[:rescue_subclasses]
+              when nil, true
+                :rescue_handlers
+              else
+                :base_only_rescue_handlers
+              end
+    
+            presenter = env[Grape::Env::API_ENDPOINT].entity_class_for_obj(presented_message, present_options)
+    
+          expect('.border-width-explicit').to have_rule(rule)
+    end
+  end
+    
+          expect('.all-buttons-hover').to have_ruleset(ruleset)
+    end
+  end
 end
 
     
-          ##
-      # Override this method in your uploader to provide a white list of extensions which
-      # are allowed to be uploaded. Compares the file's extension case insensitive.
-      # Furthermore, not only strings but Regexp are allowed as well.
-      #
-      # When using a Regexp in the white list, `\A` and `\z` are automatically added to
-      # the Regexp expression, also case insensitive.
-      #
-      # === Returns
-      #
-      # [NilClass, String, Regexp, Array[String, Regexp]] a white list of extensions which are allowed to be uploaded
-      #
-      # === Examples
-      #
-      #     def extension_whitelist
-      #       %w(jpg jpeg gif png)
-      #     end
-      #
-      # Basically the same, but using a Regexp:
-      #
-      #     def extension_whitelist
-      #       [/jpe?g/, 'gif', 'png']
-      #     end
-      #
-      def extension_whitelist; end
+      context 'called with two sizes' do
+    it 'applies to height and width' do
+      rule = 'height: 2em; width: 1em;'
