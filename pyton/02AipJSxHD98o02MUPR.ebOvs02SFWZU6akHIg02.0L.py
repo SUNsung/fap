4c3139c25136efcc1d762b60b10b508d9e7a5d2f
SUNsung <weatherius@gmail.com>
2019-06-30 13:45:58 +0300

    Rewrite [02AipJSxHD98o02MUPR.ebOvs02SFWZU6akHIg02.0L.py] File in: 2019-06-30 13:45:58

--- pyton/02AipJSxHD98o02MUPR.ebOvs02SFWZU6akHIg02.0L.py
+++ pyton/02AipJSxHD98o02MUPR.ebOvs02SFWZU6akHIg02.0L.py
@@ -1,174 +1,47 @@
 
         
-        
-if len(sys.argv) <= 1:
-    print('Specify the version number as parameter')
-    sys.exit()
-version = sys.argv[1]
+                return '\r\n'.join(headers)
     
-    with open('devscripts/lazy_load_template.py', 'rt') as f:
-    module_template = f.read()
-    
-    '''
+        def format_headers(self, headers):
+        for p in self.enabled_plugins:
+            headers = p.format_headers(headers)
+        return headers
     
     
-class TestAgeRestriction(unittest.TestCase):
-    def _assert_restricted(self, url, filename, age, old_age=None):
-        self.assertTrue(_download_restricted(url, filename, old_age))
-        self.assertFalse(_download_restricted(url, filename, age))
-    
-        def test_cache(self):
-        ydl = FakeYDL({
-            'cachedir': self.test_dir,
-        })
-        c = Cache(ydl)
-        obj = {'x': 1, 'y': ['ä', '\\a', True]}
-        self.assertEqual(c.load('test_cache', 'k.'), None)
-        c.store('test_cache', 'k.', obj)
-        self.assertEqual(c.load('test_cache', 'k2'), None)
-        self.assertFalse(_is_empty(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), obj)
-        self.assertEqual(c.load('test_cache', 'y'), None)
-        self.assertEqual(c.load('test_cache2', 'k.'), None)
-        c.remove()
-        self.assertFalse(os.path.exists(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), None)
+def test_basic_auth(httpbin_both):
+    r = http('--auth=user:password',
+             'GET', httpbin_both + '/basic-auth/user/password')
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
     
     
-if __name__ == '__main__':
-    unittest.main()
+def test_current_version():
+    version = Environment().config['__meta__']['httpie']
+    assert version == __version__
 
     
-        def test_allsubtitles(self):
-        self.DL.params['writesubtitles'] = True
-        self.DL.params['allsubtitles'] = True
-        subtitles = self.getSubtitles()
-        self.assertEqual(set(subtitles.keys()), set(['en']))
-        self.assertEqual(md5(subtitles['en']), 'acaca989e24a9e45a6719c9b3d60815c')
+    
+@mock.patch('httpie.core.get_response')
+def test_error_traceback(get_response):
+    exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    with raises(ConnectionError):
+        main(['--ignore-stdin', '--traceback', 'www.google.com'])
     
     
-def explain_template_loading_attempts(app, template, attempts):
-    '''This should help developers understand what failed'''
-    info = ['Locating template '%s':' % template]
-    total_found = 0
-    blueprint = None
-    reqctx = _request_ctx_stack.top
-    if reqctx is not None and reqctx.request.blueprint is not None:
-        blueprint = reqctx.request.blueprint
+def test_unicode_headers(httpbin):
+    # httpbin doesn't interpret utf8 headers
+    r = http(httpbin.url + '/headers', u'Test:%s' % UNICODE)
+    assert HTTP_OK in r
     
-    
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
+        return o
 
     
-        def __init__(cls, name, bases, d):
-        super(MethodViewType, cls).__init__(name, bases, d)
     
-        The request object is a :class:`~werkzeug.wrappers.Request` subclass and
-    provides all of the attributes Werkzeug defines plus a few Flask
-    specific ones.
-    '''
+class Dense(object):
+    '''全连接层的类实现，方便 reuse'''
     
-        This fixture will run the testcase twice, once with and once without the
-    limitation/mock.
-    '''
-    if not request.param:
-        return
-    
-    
-def computing_error(last_press_time, target_board_x, target_board_y, last_piece_x, last_piece_y, temp_piece_x,
-                    temp_piece_y):
-    '''
-    计算跳跃实际误差
-    '''
-    target_distance = math.sqrt(
-        (target_board_x - last_piece_x) ** 2 + (target_board_y - last_piece_y) ** 2)  # 上一轮目标跳跃距离
-    actual_distance = math.sqrt((temp_piece_x - last_piece_x) ** 2 + (temp_piece_y - last_piece_y) ** 2)  # 上一轮实际跳跃距离
-    jump_error_value = math.sqrt((target_board_x - temp_piece_x) ** 2 + (target_board_y - temp_piece_y) ** 2)  # 跳跃误差
-    
-                # 修掉圆顶的时候一条线导致的小 bug，这个颜色判断应该 OK，暂时不提出来
-            if abs(pixel[0] - last_pixel[0]) \
-                    + abs(pixel[1] - last_pixel[1]) \
-                    + abs(pixel[2] - last_pixel[2]) > 10:
-                board_x_sum += j
-                board_x_c += 1
-    
-    
-if __name__ == '__main__':
-  testutils.main()
-
-    
-    '''As a simple example of Python Fire, a Widget serves no clear purpose.'''
-    
-      Args:
-    *positional: The functions to be used for parsing positional arguments.
-    **named: The functions to be used for parsing named arguments.
-  Returns:
-    The decorated function, which now has metadata telling Fire how to perform.
-  '''
-  def _Decorator(fn):
-    parse_fns = GetParseFns(fn)
-    parse_fns['positional'] = positional
-    parse_fns['named'].update(named)
-    _SetMetadata(fn, FIRE_PARSE_FNS, parse_fns)
-    return fn
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-        if not isinstance(result, six.string_types):
-      max_distance += value.count('0')  # Leading 0s are stripped.
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-      def testFireTraceElementAsStringWithTarget(self):
-    el = trace.FireTraceElement(
-        component='Example',
-        action='Created toy',
-        target='Beaker',
-    )
-    self.assertEqual(str(el), 'Created toy 'Beaker'')
-    
-        This is a longer description of the docstring. It spans multiple lines, as
-    is allowed.
-    
-    
-def IsValue(component):
-  return isinstance(component, VALUE_TYPES)
-
-    
-    import inspect
-    
-        Used in display width computations. Control sequences have display width 0.
-    
-    
-def _GetTermSizePosix():
-  '''Returns the Posix terminal x and y dimensions.'''
-  # pylint: disable=g-import-not-at-top
-  import fcntl
-  # pylint: disable=g-import-not-at-top
-  import struct
-  # pylint: disable=g-import-not-at-top
-  import termios
-    
-      # Try UTF-8 because the other encodings could be extended ASCII. It would
-  # be exceptional if a valid extended ascii encoding with extended chars
-  # were also a valid UITF-8 encoding.
-  try:
-    return string.decode('utf8')
-  except UnicodeError:
-    # Not a UTF-8 encoding.
-    pass
+    tf_float = tf.float32
+zeros = tf.initializers.zeros
+truncated_normal = tf.initializers.truncated_normal
