06a2f81a6fc86c0b9ac9057202e0c4bda2ad93c1
SUNsung <weatherius@gmail.com>
2019-06-30 05:27:57 +0300

    Rewrite [25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py] File in: 2019-06-30 05:27:57

--- pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
+++ pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
@@ -1,72 +1,129 @@
 
         
-        
-def add_error(line_num, message):
-    '''adds an error to the dynamic error list'''
-    err = '(L{:03d}) {}'.format(line_num + 1, message)
-    errors.append(err)
+        import jieba
+import re
     
-        if text.endswith('>'):
-        words, text = text[:-1].rsplit('<', 1)
-        words = words.strip()
-    else:
-        words = None
+    seg_list = jieba.cut('我来到北京清华大学', cut_all=True)
+print('Full Mode: ' + '/ '.join(seg_list))  # 全模式
     
-        @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    import jieba
+jieba.enable_parallel(4)
+import jieba.analyse
+from optparse import OptionParser
     
+    log_f = open('1.log','wb')
+log_f.write(words.encode('utf-8'))
     
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    '''Clear existing data and create new tables.'''
-    init_db()
-    click.echo('Initialized the database.')
+        # For training, we want a lot of parallel reading and shuffling.
+    # For eval, we want no shuffling and parallel reading doesn't matter.
+    if is_training:
+      d = tf.data.Dataset.from_tensor_slices(tf.constant(input_files))
+      d = d.repeat()
+      d = d.shuffle(buffer_size=len(input_files))
     
-    import pytest
+          feature = InputFeatures(
+          unique_id=unique_id,
+          example_index=example_index,
+          doc_span_index=doc_span_index,
+          tokens=tokens,
+          token_to_orig_map=token_to_orig_map,
+          token_is_max_context=token_is_max_context,
+          input_ids=input_ids,
+          input_mask=input_mask,
+          segment_ids=segment_ids,
+          start_position=start_position,
+          end_position=end_position,
+          is_impossible=example.is_impossible)
     
+        predict_file = os.path.join(FLAGS.output_dir, 'predict.tf_record')
+    run_classifier.file_based_convert_examples_to_features(
+        predict_examples, label_list, FLAGS.max_seq_length, tokenizer,
+        predict_file)
     
-@pytest.mark.parametrize(
-    ('username', 'password', 'message'),
-    (
-        ('', '', b'Username is required.'),
-        ('a', '', b'Password is required.'),
-        ('test', 'test', b'already registered'),
-    ),
+    flags.DEFINE_integer('max_predictions_per_seq', 20,
+                     'Maximum number of masked LM predictions per sequence.')
+    
+        def feed(self, aBuf):
+        for c in aBuf:
+            # PY3K: aBuf is a byte array, so c is an int, not a byte
+            for codingSM in self._mCodingSM:
+                if not codingSM:
+                    continue
+                if not codingSM.active:
+                    continue
+                codingState = codingSM.next_state(wrap_ord(c))
+                if codingState == constants.eError:
+                    codingSM.active = False
+                    self._mActiveSM -= 1
+                    if self._mActiveSM <= 0:
+                        self._mState = constants.eNotMe
+                        return self.get_state()
+                elif codingState == constants.eItsMe:
+                    self._mState = constants.eFoundIt
+                    self._mDetectedCharset = codingSM.get_coding_state_machine()  # nopep8
+                    return self.get_state()
+    
+    ISO2022KR_cls = (
+2,0,0,0,0,0,0,0,  # 00 - 07
+0,0,0,0,0,0,0,0,  # 08 - 0f
+0,0,0,0,0,0,0,0,  # 10 - 17
+0,0,0,1,0,0,0,0,  # 18 - 1f
+0,0,0,0,3,0,0,0,  # 20 - 27
+0,4,0,0,0,0,0,0,  # 28 - 2f
+0,0,0,0,0,0,0,0,  # 30 - 37
+0,0,0,0,0,0,0,0,  # 38 - 3f
+0,0,0,5,0,0,0,0,  # 40 - 47
+0,0,0,0,0,0,0,0,  # 48 - 4f
+0,0,0,0,0,0,0,0,  # 50 - 57
+0,0,0,0,0,0,0,0,  # 58 - 5f
+0,0,0,0,0,0,0,0,  # 60 - 67
+0,0,0,0,0,0,0,0,  # 68 - 6f
+0,0,0,0,0,0,0,0,  # 70 - 77
+0,0,0,0,0,0,0,0,  # 78 - 7f
+2,2,2,2,2,2,2,2,  # 80 - 87
+2,2,2,2,2,2,2,2,  # 88 - 8f
+2,2,2,2,2,2,2,2,  # 90 - 97
+2,2,2,2,2,2,2,2,  # 98 - 9f
+2,2,2,2,2,2,2,2,  # a0 - a7
+2,2,2,2,2,2,2,2,  # a8 - af
+2,2,2,2,2,2,2,2,  # b0 - b7
+2,2,2,2,2,2,2,2,  # b8 - bf
+2,2,2,2,2,2,2,2,  # c0 - c7
+2,2,2,2,2,2,2,2,  # c8 - cf
+2,2,2,2,2,2,2,2,  # d0 - d7
+2,2,2,2,2,2,2,2,  # d8 - df
+2,2,2,2,2,2,2,2,  # e0 - e7
+2,2,2,2,2,2,2,2,  # e8 - ef
+2,2,2,2,2,2,2,2,  # f0 - f7
+2,2,2,2,2,2,2,2,  # f8 - ff
 )
-def test_register_validate_input(client, username, password, message):
-    response = client.post(
-        '/auth/register', data={'username': username, 'password': password}
-    )
-    assert message in response.data
     
-        if app:
-        bp = app.blueprints.get(request.blueprint) if request else None
-        kwargs.setdefault(
-            'cls', bp.json_decoder if bp and bp.json_decoder else app.json_decoder
-        )
-    else:
-        kwargs.setdefault('cls', JSONDecoder)
+    # 128  --> 0.79
+# 256  --> 0.92
+# 512  --> 0.986
+# 1024 --> 0.99944
+# 2048 --> 0.99999
+#
+# Idea Distribution Ratio = 0.98653 / (1-0.98653) = 73.24
+# Random Distribution Ration = 512 / (2350-512) = 0.279.
+# 
+# Typical Distribution Ratio  
     
-            editor = self.settings['EDITOR']
-        try:
-            spidercls = self.crawler_process.spider_loader.load(args[0])
-        except KeyError:
-            return self._err('Spider not found: %s' % args[0])
+    # 128  --> 0.42261
+# 256  --> 0.57851
+# 512  --> 0.74851
+# 1024 --> 0.89384
+# 2048 --> 0.97583
+#
+# Idea Distribution Ratio = 0.74851/(1-0.74851) =2.98
+# Random Distribution Ration = 512/(5401-512)=0.105
+#
+# Typical Distribution Ratio about 25% of Ideal one, still much higher than RDR
     
-        def short_desc(self):
-        return 'Fetch a URL using the Scrapy downloader'
-    
-        requires_project = False
-    default_settings = {'LOG_ENABLED': False,
-                        'SPIDER_LOADER_WARN_ONLY': True}
-    
-            'A TLS/SSL connection established with [this method] may
-         understand the SSLv3, TLSv1, TLSv1.1 and TLSv1.2 protocols.'
-        '''
-    
-        text = html.remove_tags_with_content(text, ('script', 'noscript'))
-    text = html.replace_entities(text)
-    text = html.remove_comments(text)
-    return _ajax_crawlable_re.search(text) is not None
+    Latin5CyrillicModel = {
+  'charToOrderMap': latin5_CharToOrderMap,
+  'precedenceMatrix': RussianLangModel,
+  'mTypicalPositiveRatio': 0.976601,
+  'keepEnglishLetter': False,
+  'charsetName': 'ISO-8859-5'
+}
