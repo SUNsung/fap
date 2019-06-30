655195fd56011151915a24218a0cb174cc1bcfc7
SUNsung <weatherius@gmail.com>
2019-06-30 16:05:08 +0300

    Rewrite [13cpAayw8vE613UvzFPhZ9tpM13TXo3C0GRxao13bYI.rb] File in: 2019-06-30 16:05:08

--- ruby/13cpAayw8vE613UvzFPhZ9tpM13TXo3C0GRxao13bYI.rb
+++ ruby/13cpAayw8vE613UvzFPhZ9tpM13TXo3C0GRxao13bYI.rb
@@ -1,20 +1,37 @@
-  def test_feed_xml_processing_instructions
-    with_restful_routing(:scrolls) do
-      get :index, params: { id: 'feed_with_xml_processing_instructions' }
-      assert_match %r{<\?xml-stylesheet [^\?]*type='text/css'}, @response.body
-      assert_match %r{<\?xml-stylesheet [^\?]*href='t\.css'}, @response.body
+
+        
+        WITH_LIQUID = <<-LIQUID.freeze
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
+pharetra tempus. {{ author }} et metus fermentum, eu cursus lorem
+mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
+    
+    def graceful_require
+  Jekyll::External.require_with_graceful_fail('json')
+  JSON.pretty_generate(DATA)
+end
+    
+      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
+    cmd = g.split('-').last
+    p.command(cmd.to_sym) do |c|
+      c.syntax cmd
+      c.action do
+        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
+          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
+      end
     end
   end
     
-    # No trailing slash
-Benchmark.ips do |x|
-  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
-  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
-  x.compare!
-end
-
-    
-          #
+          def initialize(runtime, path_or_io, options)
+        @runtime = runtime
+        @snippets_input = []
+        @io = ensure_io(path_or_io)
+        @prefixes = options[:prefixes] || {}
+        @delayed_messages = []
+        @options = options
+        @exceptions = []
+        @indent = 0
+        @timings = {}
+      end
     
                   EM.start_server(
                 opts['host'],
@@ -25,170 +42,115 @@ end
                 handle_websockets_event(ws)
               end
     
-          def grouped_array(groups)
-        groups.each_with_object([]) do |item, array|
-          array << {
-            'name'  => item.first,
-            'items' => item.last,
-            'size'  => item.last.size,
-          }
-        end
+        def process(args)
+      arg_is_present? args, '--server', 'The --server command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
+                          use the 'build' subcommand.'
+      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
+                          '--watch'.'
+      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
+                          the '--watch' switch.'
+      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
+                          favour of 'highlighter'.'
+      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
+                          your config files.'
+      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
+                          config files.'
+      no_subcommand(args)
+    end
+    
+        def filter_const(name)
+      if name.is_a? Array
+        name.map &method(:filter_const)
+      else
+        Docs.const_get '#{name}_filter'.camelize
       end
+    end
+    
+    module Docs
+  class PageDb
+    attr_reader :pages
+    
+    require 'active_support/subscriber'
+    
+      def verify_payload?
+    payload.present? && VerifySalmonService.new.call(payload)
+  end
+    
+      def encoded_challenge
+    HTMLEntities.new.encode(params['hub.challenge'])
+  end
+    
+    module Localized
+  extend ActiveSupport::Concern
+    
+      def rate_limit_limit
+    api_throttle_data[:limit].to_s
+  end
+    
+      # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # config.action_dispatch.rack_cache = true
+    
+    module Workers
+  class PublishToHub < Base
+    def perform(*_args)
+      # don't publish to pubsubhubbub in cucumber
+    end
+  end
+    
+        it 'generates a jasmine fixture', :fixture => true do
+      contact = alice.contact_for(bob.person)
+      aspect = alice.aspects.create(:name => 'people')
+      contact.aspects << aspect
+      contact.save
+      get :new, params: {person_id: bob.person.id}
+      save_fixture(html_for('body'), 'status_message_new')
     end
   end
 end
 
     
-    
-IAX2_DEFAULT_PORT = 4569
-    
-      def self.create_ipmi_getchannel_probe
-    [   # Get Channel Authentication Capabilities
-      0x06, 0x00, 0xff, 0x07, # RMCP Header
-      0x00, 0x00, 0x00, 0x00,
-      0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x20, 0x18,
-      0xc8, 0x81, 0x00, 0x38, 0x8e, 0x04, 0xb5
-    ].pack('C*')
+          it 'should not create the participation' do
+        post :create, params: {post_id: @post.id}
+        expect(alice.participations.where(:target_id => @post.id)).not_to exist
+        expect(response.code).to eq('403')
+      end
+    end
   end
     
-            # Sends a kerberos request, and reads the response through the connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] The kerberos message
-        # @raise [RuntimeError] if the transport protocol is unknown or the response can't be parsed.
-        # @raise [NotImplementedError] if the transport protocol isn't supported
-        def send_recv(req)
-          send_request(req)
-          res = recv_response
+        it 'redirects to / for a mobile user' do
+      request.headers['X_MOBILE_DEVICE'] = true
+      delete :destroy
+      expect(response).to redirect_to root_path
+    end
+  end
     
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
-          #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
+              expect(gon['preloads'][:mentioned_person][:name]).to eq(alice.person.name)
+          expect(gon['preloads'][:mentioned_person][:handle]).to eq(alice.person.diaspora_handle)
+        end
+      end
+    end
     
-              private
-    
-              # Encodes the data
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_data(data)
-            OpenSSL::ASN1::OctetString.new(data)
+          Dir.chdir(file_path) do
+        contents = file.read
+        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
+          contents = $1.lstrip
+        end
+        contents = pre_filter(contents)
+        if @raw
+          contents
+        else
+          partial = Liquid::Template.parse(contents)
+          context.stack do
+            partial.render(context)
           end
         end
       end
     end
   end
 end
-    
-              # Encodes the cipher
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_cipher
-            OpenSSL::ASN1::OctetString.new(cipher)
-          end
-    
-              # Decodes the crealm field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_crealm(input)
-            input.value[0].value
-          end
-    
-                  1: # 1
-              2: # 2
-           => 3: IRB::WorkSpace.new(binding) # 3
-              4: # 4
-              5: # 5
-    
-      # :stopdoc:
-  def _dispatch_0() nil end
-  def _dispatch_1(a) a end
-  def _dispatch_2(a, b) a end
-  def _dispatch_3(a, b, c) a end
-  def _dispatch_4(a, b, c, d) a end
-  def _dispatch_5(a, b, c, d, e) a end
-  def _dispatch_6(a, b, c, d, e, f) a end
-  def _dispatch_7(a, b, c, d, e, f, g) a end
-  # :startdoc:
-    
-      def description
-    <<-EOF
-The unpack command allows you to examine the contents of a gem or modify
-them to help diagnose a bug.
-    
-      platform_is_not :windows do
-    it 'with :GETRUSAGE_BASED_CLOCK_PROCESS_CPUTIME_ID reports 1 microsecond' do
-      Process.clock_getres(:GETRUSAGE_BASED_CLOCK_PROCESS_CPUTIME_ID, :nanosecond).should == 1_000
-    end
-  end
-    
-          Thread.pass until port
-    
-            scan(code, allow_last_error: !complete) do |token, str, expr|
-          in_symbol = symbol_state.scan_token(token)
-          str.each_line do |line|
-            line = Reline::Unicode.escape_for_print(line)
-            if seq = dispatch_seq(token, expr, line, in_symbol: in_symbol)
-              colored << seq.map { |s| '\e[#{s}m' }.join('')
-              colored << line.sub(/\Z/, clear)
-            else
-              colored << line
-            end
-          end
-          length += str.bytesize
-        end
-    
-        def tokenize
-      parse().sort_by(&:pos).map(&:tok)
-    end
-    
-          @right_diff_line_number = nil
-    
-          def sidebar
-        if @sidebar.nil?
-          if page = @page.sidebar
-            @sidebar = page.text_data
-          else
-            @sidebar = false
-          end
-        end
-        @sidebar
-      end
-    
-        assert_match /Delete this Page/, last_response.body, ''Delete this Page' link is blocked in page template'
-    assert_match /New/,              last_response.body, ''New' button is blocked in page template'
-    assert_match /Upload/,           last_response.body, ''Upload' link is blocked in page template'
-    assert_match /Rename/,           last_response.body, ''Rename' link is blocked in page template'
-    assert_match /Edit/,             last_response.body, ''Edit' link is blocked in page template'
-    
-    context 'Precious::Helpers' do
-  include Precious::Helpers
-    
-        class MapGollum
-      def initialize(base_path)
-        @mg = Rack::Builder.new do
-          
-          map '/#{base_path}' do
-            run Precious::App
-          end
-          map '/' do
-            run Proc.new { [302, { 'Location' => '/#{base_path}' }, []] }
-          end
-          map '/*' do
-            run Proc.new { [302, { 'Location' => '/#{base_path}' }, []] }
-          end
-          
-        end
-      end
-    
-        it 'accepts jsfiddle link' do
-      liquid = generate_new_liquid(jsfiddle_link)
-      rendered_jsfiddle_iframe = liquid.render
-      Approvals.verify(rendered_jsfiddle_iframe, name: 'jsfiddle_liquid_tag', format: :html)
-    end
