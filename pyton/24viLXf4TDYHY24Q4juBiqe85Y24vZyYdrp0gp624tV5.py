e85f3a5db9eddcaec1187c84abe45d3d94c39894
SUNsung <weatherius@gmail.com>
2019-06-30 13:27:49 +0300

    Rewrite [24viLXf4TDYHY24Q4juBiqe85Y24vZyYdrp0gp624tV5.py] File in: 2019-06-30 13:27:49

--- pyton/24viLXf4TDYHY24Q4juBiqe85Y24vZyYdrp0gp624tV5.py
+++ pyton/24viLXf4TDYHY24Q4juBiqe85Y24vZyYdrp0gp624tV5.py
@@ -1,254 +1,27 @@
 
         
-        
-class ArrayMaxLengthValidator(MaxLengthValidator):
-    message = ngettext_lazy(
-        'List contains %(show_value)d item, it should contain no more than %(limit_value)d.',
-        'List contains %(show_value)d items, it should contain no more than %(limit_value)d.',
-        'limit_value')
+            @warningregistry
+    def test_loadTestsFromModule__use_load_tests_other_bad_keyword(self):
+        m = types.ModuleType('m')
+        class MyTestCase(unittest.TestCase):
+            def test(self):
+                pass
+        m.testcase_1 = MyTestCase
     
-            return True
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licence]
+    Cela ressemble à un excellent recipie[1] déjeuner.
     
     
-    def getTokenErrorDisplay(self, t):
-        '''
-        How should a token be displayed in an error message? The default
-        is to display just the text, but during development you might
-        want to have a lot of information spit out.  Override in that case
-        to use t.toString() (which, for CommonToken, dumps everything about
-        the token). This is better than forcing you to override a method in
-        your token objects because you don't have to go modify your lexer
-        so that it creates a new Java type.
-        '''
-        
-        s = t.text
-        if s is None:
-            if t.type == EOF:
-                s = '<EOF>'
-            else:
-                s = '<'+t.type+'>'
-    
-    
-    def size(self):
-        return self.n
-    
-    :copyright: (c) 2010-2017 by the Sentry Team, see AUTHORS for more details.
-:license: BSD, see LICENSE for more details.
-'''
-    
-            # Deleting model 'TagKey'
-        db.delete_table(u'tagstore_tagkey')
-    
-            # Adding index on 'EventTag', fields ['group_id', 'key', 'value']
-        db.create_index(u'tagstore_eventtag', ['group_id', 'key_id', 'value_id'])
-    
-            # Removing index on 'EventTag', fields ['project_id', 'key', 'value']
-        db.delete_index(u'tagstore_eventtag', ['project_id', 'key_id', 'value_id'])
-    
-        def backwards(self, orm):
-        # Removing unique constraint on 'EventTag', fields ['project_id',
-        # 'event_id', 'key', 'value']
-        db.delete_unique(u'tagstore_eventtag', ['project_id', 'event_id', 'key_id', 'value_id'])
-    
-        # Flag to indicate if this migration is too risky
-    # to run online and needs to be coordinated for offline
-    is_dangerous = True
-    
-    :copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
-:license: BSD, see LICENSE for more details.
-'''
-from __future__ import absolute_import
-    
-    :copyright: (c) 2010-2015 by the Sentry Team, see AUTHORS for more details.
-:license: BSD, see LICENSE for more details.
-'''
-    
-        def test_a_observer_list_shall_be_empty_initially(cls):
-        cls.assertEqual(len(cls.s._observers), 0)
-    
-        def test_shall_toggle_from_fm_to_am(self):
-        self.radio.toggle_amfm()
-        state = self.radio.state.name
-        expected_state_name = 'AM'
-        self.assertEqual(state, expected_state_name)
+if __name__ == '__main__':
+    main()
 
     
-        def test_extended_property_values_cloning(self):
-        sample_object_1 = self.prototype.clone()
-        sample_object_1.some_value = 'test string'
-        sample_object_2 = self.prototype.clone()
-        self.assertRaises(AttributeError, lambda: sample_object_2.some_value)
+    dom = xml.dom.minidom.parseString(document)
     
-        def test_human_shall_speak(self):
-        noise = self.human.speak()
-        expected_noise = ''hello''
-        self.assertEqual(noise, expected_noise)
-    
-        fftv = Publisher(message_center)
-    
-    https://en.wikipedia.org/wiki/Blackboard_system
-'''
-    
-            # dictionary that will be used to determine which static method is
-        # to be executed but that will be also used to store possible param
-        # value
-        self._static_method_choices = {'param_value_1': self._static_method_1, 'param_value_2': self._static_method_2}
-    
-        @staticmethod
-    def check_range(request):
-        if 0 <= request < 10:
-            print('request {} handled in handler 0'.format(request))
-            return True
+    DB_FILE = 'mydb'
     
     
-def cityscapes_to_coco_without_person_rider(cityscapes_id):
-    lookup = {
-        0: 0,  # ... background
-        1: 2,  # bicycle
-        2: 3,  # car
-        3: -1,  # person (ignore)
-        4: 7,  # train
-        5: 8,  # truck
-        6: 4,  # motorcycle
-        7: 6,  # bus
-        8: -1,  # rider (ignore)
-    }
-    return lookup[cityscapes_id]
-    
-        'Flipping' an entry means that that image and associated metadata (e.g.,
-    ground truth boxes and object proposals) are horizontally flipped.
-    '''
-    flipped_roidb = []
-    for entry in roidb:
-        width = entry['width']
-        boxes = entry['boxes'].copy()
-        oldx1 = boxes[:, 0].copy()
-        oldx2 = boxes[:, 2].copy()
-        boxes[:, 0] = width - oldx2 - 1
-        boxes[:, 2] = width - oldx1 - 1
-        assert (boxes[:, 2] >= boxes[:, 0]).all()
-        flipped_entry = {}
-        dont_copy = ('boxes', 'segms', 'gt_keypoints', 'flipped')
-        for k, v in entry.items():
-            if k not in dont_copy:
-                flipped_entry[k] = v
-        flipped_entry['boxes'] = boxes
-        flipped_entry['segms'] = segm_utils.flip_segms(
-            entry['segms'], entry['height'], entry['width']
-        )
-        if dataset.keypoints is not None:
-            flipped_entry['gt_keypoints'] = keypoint_utils.flip_keypoints(
-                dataset.keypoints, dataset.keypoint_flip_map,
-                entry['gt_keypoints'], entry['width']
-            )
-        flipped_entry['flipped'] = True
-        flipped_roidb.append(flipped_entry)
-    roidb.extend(flipped_roidb)
-    
-    
-# ---------------------------------------------------------------------------- #
-# FPN level info for stages 5, 4, 3, 2 for select models (more can be added)
-# ---------------------------------------------------------------------------- #
-    
-    from detectron.core.config import cfg
-from detectron.utils.c2 import const_fill
-from detectron.utils.c2 import gauss_fill
-import detectron.modeling.ResNet as ResNet
-import detectron.utils.blob as blob_utils
-    
-    
-def build_data_parallel_model(model, single_gpu_build_func):
-    '''Build a data parallel model given a function that builds the model on a
-    single GPU.
-    '''
-    if model.only_build_forward_pass:
-        single_gpu_build_func(model)
-    elif model.train:
-        all_loss_gradients = _build_forward_graph(model, single_gpu_build_func)
-        # Add backward pass on all GPUs
-        model.AddGradientOperators(all_loss_gradients)
-        if cfg.NUM_GPUS > 1:
-            _add_allreduce_graph(model)
-        for gpu_id in range(cfg.NUM_GPUS):
-            # After allreduce, all GPUs perform SGD updates on their identical
-            # params and gradients in parallel
-            with c2_utils.NamedCudaScope(gpu_id):
-                add_single_gpu_param_update_ops(model, gpu_id)
-    else:
-        # Test-time network operates on single GPU
-        # Test-time parallelism is implemented through multiprocessing
-        with c2_utils.NamedCudaScope(model.target_gpu_id):
-            single_gpu_build_func(model)
-    
-        if len(data.shape) == 1:
-        ret = np.empty((count, ), dtype=data.dtype)
-        ret.fill(fill)
-        ret[inds] = data
-    else:
-        ret = np.empty((count, ) + data.shape[1:], dtype=data.dtype)
-        ret.fill(fill)
-        ret[inds, :] = data
-    return ret
-    
-        # Optionally add Keypoint R-CNN blobs
-    if cfg.MODEL.KEYPOINTS_ON:
-        keypoint_rcnn_roi_data.add_keypoint_rcnn_blobs(
-            blob_dict, roidb, fg_rois_per_image, fg_inds, im_scale, batch_idx
-        )
-    
-    
-def get_minibatch_blob_names(is_training=True):
-    '''Return blob names in the order in which they are read by the data loader.
-    '''
-    # data blob: holds a batch of N images, each with 3 channels
-    blob_names = ['data']
-    if cfg.RPN.RPN_ON:
-        # RPN-only or end-to-end Faster R-CNN
-        blob_names += rpn_roi_data.get_rpn_blob_names(is_training=is_training)
-    elif cfg.RETINANET.RETINANET_ON:
-        blob_names += retinanet_roi_data.get_retinanet_blob_names(
-            is_training=is_training
-        )
-    else:
-        # Fast R-CNN like models trained on precomputed proposals
-        blob_names += fast_rcnn_roi_data.get_fast_rcnn_blob_names(
-            is_training=is_training
-        )
-    return blob_names
-    
-    
-def _get_retinanet_blobs(
-        foas, all_anchors, gt_boxes, gt_classes, im_width, im_height):
-    total_anchors = all_anchors.shape[0]
-    logger.debug('Getting mad blobs: im_height {} im_width: {}'.format(
-        im_height, im_width))
+def display_temp(hass: HomeAssistant, temperature: float, unit: str,
+                 precision: float) -> float:
+    '''Convert temperature into preferred units/precision for display.'''
+    temperature_unit = unit
+    ha_unit = hass.config.units.temperature_unit
