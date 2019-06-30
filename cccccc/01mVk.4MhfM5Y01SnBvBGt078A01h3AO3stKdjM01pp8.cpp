70a0e2a73b626cb86242d1995bd858750be79ee9
SUNsung <weatherius@gmail.com>
2019-06-30 19:49:08 +0300

    Rewrite [01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp] File in: 2019-06-30 19:49:08

--- cccccc/01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp
+++ cccccc/01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp
@@ -1,268 +1,97 @@
 
         
-        class OpenURIDialog : public QDialog
-{
-    Q_OBJECT
-    }
-    
-    #ifndef SECP256K1_FIELD_INNER5X52_IMPL_H
-#define SECP256K1_FIELD_INNER5X52_IMPL_H
-    
-    // Value types encoded as the last component of internal keys.
-// DO NOT CHANGE THESE ENUM VALUES: they are embedded in the on-disk
-// data structures.
-enum ValueType {
-  kTypeDeletion = 0x0,
-  kTypeValue = 0x1
-};
-// kValueTypeForSeek defines the ValueType that should be passed when
-// constructing a ParsedInternalKey object for seeking to a particular
-// sequence number (since we sort sequence numbers in decreasing order
-// and the value type is embedded as the low 8 bits in the sequence
-// number in internal keys, we need to use the highest-numbered
-// ValueType, not the lowest).
-static const ValueType kValueTypeForSeek = kTypeValue;
-    
-      Slice in(encoded);
-  ParsedInternalKey decoded('', 0, kTypeValue);
-    
-    Status DumpTable(Env* env, const std::string& fname, WritableFile* dst) {
-  uint64_t file_size;
-  RandomAccessFile* file = NULL;
-  Table* table = NULL;
-  Status s = env->GetFileSize(fname, &file_size);
-  if (s.ok()) {
-    s = env->NewRandomAccessFile(fname, &file);
-  }
-  if (s.ok()) {
-    // We use the default comparator, which may or may not match the
-    // comparator used in this database. However this should not cause
-    // problems since we only use Table operations that do not require
-    // any comparisons.  In particular, we do not call Seek or Prev.
-    s = Table::Open(Options(), file, file_size, &table);
-  }
-  if (!s.ok()) {
-    delete table;
-    delete file;
-    return s;
-  }
-    }
-    
-    bool FaultInjectionTestEnv::IsFileCreatedSinceLastDirSync(
-    const std::string& filename) {
-  MutexLock l(&mutex_);
-  return new_files_since_last_dir_sync_.find(filename) !=
-         new_files_since_last_dir_sync_.end();
+        v8::Local<v8::Value> Converter<Dictionary>::ToV8(v8::Isolate* isolate,
+                                                 Dictionary val) {
+  return val.GetHandle();
 }
     
-    static std::string MakeFileName(const std::string& name, uint64_t number,
-                                const char* suffix) {
-  char buf[100];
-  snprintf(buf, sizeof(buf), '/%06llu.%s',
-           static_cast<unsigned long long>(number),
-           suffix);
-  return name + buf;
+    namespace mate {
+    }
+    
+      WrappableBase* object;
+  {
+    // Don't continue if the constructor throws an exception.
+    v8::TryCatch try_catch(isolate);
+    object = internal::InvokeFactory(args, factory);
+    if (try_catch.HasCaught()) {
+      try_catch.ReThrow();
+      return;
+    }
+  }
+    
+    // Classes for generating and storing an argument pack of integer indices
+// (based on well-known 'indices trick', see: http://goo.gl/bKKojn):
+template <size_t... indices>
+struct IndicesHolder {};
+    
+    #ifndef NATIVE_MATE_NATIVE_MATE_OBJECT_TEMPLATE_BUILDER_H_
+#define NATIVE_MATE_NATIVE_MATE_OBJECT_TEMPLATE_BUILDER_H_
+    
+    
+    {  *result = notification_callback.Run(parsed_command_line, current_directory)
+                ? TRUE
+                : FALSE;
+  return true;
 }
     
-    // Make the CURRENT file point to the descriptor file with the
-// specified number.
-extern Status SetCurrentFile(Env* env, const std::string& dbname,
-                             uint64_t descriptor_number);
+    TEST(JavaDocCommentTest, Escaping) {
+  EXPECT_EQ('foo /&#42; bar *&#47; baz', EscapeJavadoc('foo /* bar */ baz'));
+  EXPECT_EQ('foo /&#42;&#47; baz', EscapeJavadoc('foo /*/ baz'));
+  EXPECT_EQ('{&#64;foo}', EscapeJavadoc('{@foo}'));
+  EXPECT_EQ('&lt;i&gt;&amp;&lt;/i&gt;', EscapeJavadoc('<i>&</i>'));
+  EXPECT_EQ('foo&#92;u1234bar', EscapeJavadoc('foo\\u1234bar'));
+  EXPECT_EQ('&#64;deprecated', EscapeJavadoc('@deprecated'));
+}
+    
+      desired_output_for_decode = 'ABCD__EfghI_j';
+  expected = string('\x64\x80\xC5\xA1\x0', 5);
+  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
+                                                     desired_output_for_decode);
+  EXPECT_EQ(expected, result);
+    
+    void MapLiteTestUtil::SetMapFields(unittest::TestMapLite* message) {
+  MapTestUtilImpl::SetMapFields<unittest::MapEnumLite,
+                                unittest::MAP_ENUM_BAR_LITE,
+                                unittest::MAP_ENUM_BAZ_LITE>(message);
+}
+    
+    Status::Status() : error_code_(error::OK) {
+}
     
     
     {
-    {   private:
-    friend class CudaModule;
-    /*!
-     * \brief constructor
-     * \param mod module of this kernel
-     * \param mangled_name mangled kernel name
-     * \param signature kernel argument signature
-     */
-    Kernel(const std::shared_ptr<Chunk>& mod,
-           const std::string& mangled_name,
-           const std::vector<ArgType>& signature);
-    /*! \brief mangled kernel name */
-    std::string mangled_name_;
-    /*! \brief kernel argument signature */
-    std::vector<ArgType> signature_;
-    /*! \brief module of this kernel */
-    std::shared_ptr<Chunk> mod_;
-    /*! \brief cached kernel function on each device */
-    std::unordered_map<int, CUfunction> func_;
-  };
-  /*!
-   * \brief CudaModule constructor
-   * \param source cuda source code.
-   * \param exports export symbols before mangling.
-   */
-  CudaModule(const char* source,
-             const std::vector<std::string>& options,
-             const std::vector<std::string>& exports)
-      : ptr_(std::make_shared<Chunk>(source, options, exports)) {}
-  /*!
-   * \brief Get cuda kernal from module by name
-   * \param name kernel name
-   * \param signature kernel signature
-   * \return shared pointer to cuda kernel
-   */
-  std::shared_ptr<Kernel> GetKernel(const std::string& name,
-                                    const std::vector<ArgType>& signature);
+    {
+    {}  // namespace util
+}  // namespace protobuf
+}  // namespace google
+    
+    using namespace std;
+    
+    
+    { private:
+  const EnumDescriptor* descriptor_;
+  std::vector<const EnumValueDescriptor*> base_values_;
+  std::vector<const EnumValueDescriptor*> all_values_;
+  std::set<const EnumValueDescriptor*> alias_values_to_skip_;
+  const string name_;
 };
     
-    struct CaffeDataParam : public dmlc::Parameter<CaffeDataParam> {
-  /*! \brief protobuf text */
-  ::caffe::LayerParameter prototxt;
-  /*! \brief number of iterations per epoch */
-  int num_examples;
-  /*! \brief data mode */
-  bool flat;
-    }
+    // InitXXX function with 'install_callbacks=true': install GLFW callbacks. They will call user's previously installed callbacks, if any.
+// InitXXX function with 'install_callbacks=false': do not install GLFW callbacks. You will need to call them yourself from your own GLFW callbacks.
+IMGUI_IMPL_API void     ImGui_ImplGlfw_MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+IMGUI_IMPL_API void     ImGui_ImplGlfw_ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+IMGUI_IMPL_API void     ImGui_ImplGlfw_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+IMGUI_IMPL_API void     ImGui_ImplGlfw_CharCallback(GLFWwindow* window, unsigned int c);
+
     
-     private:
-  /*!
-   * \brief Wait for all started threads to signal that they're ready
-   */
-  void WaitForReady() {
-    for (const std::shared_ptr<dmlc::ManualEvent>& ptr : ready_events_) {
-      ptr->wait();
-    }
-  }
-    
-      void Run(const RunContext& rctx) {
-    // setup DLTensor
-    for (size_t i = 0; i < array_loc_.size(); ++i) {
-      values_[array_loc_[i]].v_handle =
-          const_cast<DLTensor*>(&(array_data_[i].data().dltensor()));
-    }
-    // run the packed function
-    TVMRetValue rv;
-    TVMArgs args(&values_[0], &type_codes_[0], values_.size());
-    if (ctx().dev_type == Context::kGPU) {
-#if MXNET_USE_CUDA
-      // pass stream via last argument.
-      void* strm = static_cast<void*>(rctx.get_stream<gpu>()->stream_);
-      int dev_type = kDLGPU;
-      fset_stream_(dev_type, rctx.ctx.dev_id, strm);
-      func_.CallPacked(args, &rv);
-      fset_stream_(dev_type, rctx.ctx.dev_id, nullptr);
-#else
-      LOG(FATAL) << 'Please compile with CUDA enabled for cuda features';
-#endif
-    } else {
-      func_.CallPacked(args, &rv);
-    }
-  }
-    
-    namespace CNTK
-{
-    class UDFUtils
-    {
-    public:
-    }
-    }
-    
-        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange& fr) override
-    {
-        auto sliceInputValue  = InputRef(0).ValueFor(fr);
-        auto sliceOutputValue =           ValueFor(fr); // row vector
-    }
-    
-            // log(2+rank)
-        *m_urlDiscount0 += 2.0;
-        m_urlDiscount0->InplaceLog();
-        *m_urlDiscount1 += 2.0;
-        m_urlDiscount1->InplaceLog();
-        // gain/log(2+rank)
-        m_urlGain0->AssignElementDivisionOf(*m_urlGain0, *m_urlDiscount0);
-        m_urlGain1->AssignElementDivisionOf(*m_urlGain1, *m_urlDiscount1);
-    
-                // check with expected values
-            Value().TransferFromDeviceToDevice(m_deviceId, CPUDEVICE, true);
-            if (!ISCLOSE(Value()(0, 0), 1.0, EPSILON) ||
-                !ISCLOSE(Value()(0, 1), 2.0, EPSILON) ||
-                !ISCLOSE(Value()(1, 1), 2.0, EPSILON))
-                throw('LSTMNode forward computation error');
-    
-    template <class ElemType>
-void EpochAccumulatorNode<ElemType>::ForwardPropNonLooping()
-{
-    TensorView<ElemType> accumulator = EnsureAccumlator();
-    UpdateRunningAverage(InputRef(0), accumulator, m_numSamples);
-    CopyAccumulatorToValue();
-}
-    
-    template<class ElemType>
-void OptimizedRNNStackNode<ElemType>::TransposeHelper(const MatrixBasePtr matX, const TensorShape &shapeX, MatrixBasePtr matY, TensorShape &shapeY)
-{
-    // This function transposes the second and third axes of the input (X), creating a transposed copy in the output (Y).
-    //
-    // In 'frame mode', CNTK will present the data with the final two axes being the recurrent axis followed by the 
-    // 'minibatch axis'. CUDNN expects these to be in the reverse order, which is accomplished by TransposeHelper().
-    }
-    
-    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
+            D3D12_RESOURCE_BARRIER barrier = {};
+        barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+        barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+        barrier.Transition.pResource   = pTexture;
+        barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+        barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
+        barrier.Transition.StateAfter  = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
     
     
-    {  double ret = x * OBJECT_DIST_RES + OBJECT_DIST_LONG_MIN;
-  return ret;
-}
-    
-      MatrixXd mat_golden(10, 10);
-  // clang-format off
-  mat_golden <<
-   1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-  -1,  1,  0,  0,  0,  0,  0,  0,  0,  0,
-   0, -1,  1,  0,  0,  0,  0,  0,  0,  0,
-   0,  0, -1,  1,  0,  0,  0,  0,  0,  0,
-   0,  0,  0, -1,  1,  0,  0,  0,  0,  0,
-   0,  0,  0,  0, -1,  1,  0,  0,  0,  0,
-   0,  0,  0,  0,  0, -1,  1,  0,  0,  0,
-   0,  0,  0,  0,  0,  0, -1,  1,  0,  0,
-   0,  0,  0,  0,  0,  0,  0, -1,  1,  0,
-   0,  0,  0,  0,  0,  0,  0,  0, -1,  1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-      const auto mat = kernel.kernel_matrix();
-  const auto offset = kernel.offset_matrix();
-    
-    namespace apollo {
-namespace planning {
-    }
-    }
-    
-    
-    {
-    {  } else {
-    for (uint32_t r = 3; r < num_params; ++r) {
-      for (uint32_t c = 3; c < num_params; ++c) {
-        (*term_matrix)(r, c) = x_pow[r + c - 5];
-      }
-    }
-    (*term_matrix).block(0, 0, num_params, 3) =
-        Eigen::MatrixXd::Zero(num_params, 3);
-    (*term_matrix).block(0, 0, 3, num_params) =
-        Eigen::MatrixXd::Zero(3, num_params);
-  }
-}
-    
-    class Accelrpt68Test : public ::testing::Test {
- public:
-  virtual void SetUp() {}
-};
-    
-      Byte t2(bytes + 6);
-  t = t2.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-    // config detail: {'name': 'manual_input', 'enum': {0: 'MANUAL_INPUT_OFF', 1:
-// 'MANUAL_INPUT_ON'}, 'precision': 1.0, 'len': 8, 'is_signed_var': False,
-// 'offset': 0.0, 'physical_range': '[0|1]', 'bit': 7, 'type': 'enum', 'order':
-// 'motorola', 'physical_unit': ''}
-Horn_rpt_79::Manual_inputType Hornrpt79::manual_input(const std::uint8_t* bytes,
-                                                      int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 8);
+    {        ImGui::Render();
     }
