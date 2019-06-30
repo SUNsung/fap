40c65292af4d37f0ec37e2f3eefbf8ed568e26a5
SUNsung <weatherius@gmail.com>
2019-06-30 04:34:51 +0300

    Rewrite [57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py] File in: 2019-06-30 04:34:51

--- pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
+++ pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
@@ -1,195 +1,131 @@
 
         
-          word_pairs = curr_features['pair']
-  return word_pairs
+            def __init__(self, name):
+        self.name = name
     
-    DATASET: The top level dictionary is simply name (string -> dictionary).
-The nested dictionary is the DATA DICTIONARY, which has the following keys:
-  'train_data' and 'valid_data', whose values are the corresponding training
-    and validation data with shape
-    ExTxD, E - # examples, T - # time steps, D - # dimensions in data.
-  The data dictionary also has a few more keys:
-    'train_ext_input' and 'valid_ext_input', if there are know external inputs
-      to the system being modeled, these take on dimensions:
-      ExTxI, E - # examples, T - # time steps, I = # dimensions in input.
-   'alignment_matrix_cxf' - If you are using multiple days data, it's possible
-     that one can align the channels (see manuscript).  If so each dataset will
-     contain this matrix, which will be used for both the input adapter and the
-     output adapter for each dataset. These matrices, if provided, must be of
-     size [data_dim x factors] where data_dim is the number of neurons recorded
-     on that day, and factors is chosen and set through the '--factors' flag.
-   'alignment_bias_c' - See alignment_matrix_cxf.  This bias will used to
-     the offset for the alignment transformation.  It will *subtract* off the
-     bias from the data, so pca style inits can align factors across sessions.
+        def __eq__(self, other):
+        return (
+            isinstance(other, self.__class__) and
+            self.keys == other.keys and
+            self.messages == other.messages and
+            self.strict == other.strict
+        )
     
-      input_title = ''
-  if 'controller_outputs' in model_vals.keys():
-    input_title += ' Controller Output'
-    plt.subplot(nrows,2,3+subplot_cidx)
-    u_t = model_vals['controller_outputs'][0:-1]
-    plot_time_series(u_t, bidx, n_to_plot=n_to_plot, color='c', scale=1.0,
-                     title=col_title + input_title)
-    
-      # Build and run the model, for varying purposes.
-  config = tf.ConfigProto(allow_soft_placement=True,
-                          log_device_placement=False)
-  if FLAGS.allow_gpu_growth:
-    config.gpu_options.allow_growth = True
-  sess = tf.Session(config=config)
-  with sess.as_default():
-    with tf.device(hps.device):
-      if kind == 'train':
-        train(hps, datasets)
-      elif kind == 'posterior_sample_and_average':
-        write_model_runs(hps, datasets, hps.output_filename_stem,
-                         push_mean=False)
-      elif kind == 'posterior_push_mean':
-        write_model_runs(hps, datasets, hps.output_filename_stem,
-                         push_mean=True)
-      elif kind == 'prior_sample':
-        write_model_samples(hps, datasets, hps.output_filename_stem)
-      elif kind == 'write_model_params':
-        write_model_parameters(hps, hps.output_filename_stem, datasets)
-      else:
-        assert False, ('Kind %s is not implemented. ' % kind)
-    
-      # First generate all firing rates. in the next loop, generate all
-  # replications this allows the random state for rate generation to be
-  # independent of n_replications.
-  dataset_name = 'dataset_N' + str(N) + '_S' + str(S)
-  if S < N:
-    dataset_name += '_n' + str(n+1)
+        def value(self):
+        '''
+        Return the value for this BoundField, using the initial value if
+        the form is not bound or the data otherwise.
+        '''
+        data = self.initial
+        if self.form.is_bound:
+            data = self.field.bound_data(self.data, data)
+        return self.field.prepare_value(data)
     
     
-class IntegrationToBoundModel:
-  def __init__(self, N):
-    scale = 0.8 / float(N**0.5)
-    self.N = N
-    self.Wh_nxn = tf.Variable(tf.random_normal([N, N], stddev=scale))
-    self.b_1xn = tf.Variable(tf.zeros([1, N]))
-    self.Bu_1xn = tf.Variable(tf.zeros([1, N]))
-    self.Wro_nxo = tf.Variable(tf.random_normal([N, 1], stddev=scale))
-    self.bro_o = tf.Variable(tf.zeros([1]))
+class DjangoTemplates(EngineMixin, BaseRenderer):
+    '''
+    Load Django templates from the built-in widget templates in
+    django/forms/templates and from apps' 'templates' directory.
+    '''
+    backend = DjangoTemplates
     
-      for batch in range(num_batches):
-    x = np.zeros([batch_size, num_steps], dtype=np.int32)
-    y = np.zeros([batch_size, num_steps], dtype=np.int32)
-    w = np.zeros([batch_size, num_steps], dtype=np.float)
-    
-      Args:
-    gen_logits: Generator logits.
-    gen_labels:  Labels for the correct token.
-    dis_predictions:  Discriminator predictions.
-    is_real_input:  Tensor indicating whether the label is present.
-    
-      Args;
-    hparams:  Hyperparameters for the MaskGAN.
-    inputs:  tf.int32 Tensor of the sequence input of shape [batch_size,
-      sequence_length].
-    present:  tf.bool Tensor indicating the presence or absence of the token
-      of shape [batch_size, sequence_length].
-    is_training:  Whether the model is training.
-    is_validating:  Whether the model is being run in validation mode for
-      calculating the perplexity.
-    reuse (Optional):  Whether to reuse the model.
+    from .compat import is_py2, builtin_str, str
     
     
-def create_dis_train_op(hparams, dis_loss, global_step):
-  '''Create Discriminator train op.'''
-  with tf.name_scope('train_discriminator'):
-    dis_optimizer = tf.train.AdamOptimizer(hparams.dis_learning_rate)
-    dis_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('dis')
-    ]
-    if FLAGS.dis_update_share_embedding and FLAGS.dis_share_embedding:
-      shared_embedding = [
-          v for v in tf.trainable_variables()
-          if v.op.name == 'gen/decoder/rnn/embedding'
-      ][0]
-      dis_vars.append(shared_embedding)
-    print('\nOptimizing Discriminator vars:')
-    for v in dis_vars:
-      print(v)
-    dis_grads = tf.gradients(dis_loss, dis_vars)
-    dis_grads_clipped, _ = tf.clip_by_global_norm(dis_grads,
-                                                  FLAGS.grad_clipping)
-    dis_train_op = dis_optimizer.apply_gradients(
-        zip(dis_grads_clipped, dis_vars), global_step=global_step)
-    return dis_train_op, dis_grads_clipped, dis_vars
-    
-        ## Discriminator Models.
-    if (FLAGS.discriminator_model == 'rnn_nas' or
-        FLAGS.discriminator_model == 'rnn_zaremba' or
-        FLAGS.discriminator_model == 'rnn_vd' or
-        FLAGS.discriminator_model == 'cnn'):
-      load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-      print('Restoring Discriminator from %s.' % load_ckpt)
-      tf.logging.info('Restoring Discriminator from %s.' % load_ckpt)
-      dis_init_saver = init_savers['dis_init_saver']
-      dis_init_saver.restore(sess, load_ckpt)
-    
-        lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
-                 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
-                 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
-                 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
-                 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331,
-                 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
-                 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467,
-                 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563,
-                 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631,
-                 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709,
-                 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
-                 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877,
-                 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967,
-                 971, 977, 983, 991, 997]
-    
-    	for n in xrange(m+1):
-		for k in xrange(1, m):
-			memo[n][k] += memo[n][k-1]
-			if n-k > 0:
-				memo[n][k] += memo[n-k-1][k]
-    
-    def centroid_pairwise_dist(X,centroids):
-    return pairwise_distances(X,centroids,metric='euclidean')
-    
-        difference = predict - actual
-    square_diff = np.square(difference)
-    
-    min_length = 8
-max_length = 16
-password = ''.join(random.choice(chars) for x in range(random.randint(min_length, max_length)))
-print('Password: ' + password)
-print('[ If you are thinking of using this passsword, You better save it. ]')
-    
-        # Test single image
-    x = np.random.uniform(0, 255, (10, 10, 3))
-    inputs = Input(shape=x.shape)
-    outputs = Lambda(utils.preprocess_input, output_shape=x.shape)(inputs)
-    model = Model(inputs, outputs)
-    assert model.predict(x[np.newaxis])[0].shape == x.shape
-    
-    # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
-    scores = model.evaluate(x_test, y_test, verbose=0)
-print('IRNN test score:', scores[0])
-print('IRNN test accuracy:', scores[1])
+@pytest.fixture
+def httpbin_secure(httpbin_secure):
+    return prepare_url(httpbin_secure)
 
     
-    model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(num_classes, activation='softmax'))
+        def _create_socket_and_bind(self):
+        sock = socket.socket()
+        sock.bind((self.host, self.port))
+        sock.listen(0)
+        return sock
     
-    print('Loading data...')
-(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
-                                                         test_split=0.2)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
+                        resp = HTTPResponse.from_httplib(
+                        r,
+                        pool=conn,
+                        connection=low_conn,
+                        preload_content=False,
+                        decode_content=False
+                    )
+                except:
+                    # If we hit any problems here, clean up the connection.
+                    # Then, reraise so that we can handle the actual exception.
+                    low_conn.close()
+                    raise
     
-        `log(cosh(x))` is approximately equal to `(x ** 2) / 2` for small `x` and
-    to `abs(x) - log(2)` for large `x`. This means that 'logcosh' works mostly
-    like the mean squared error, but will not be so strongly affected by the
-    occasional wildly incorrect prediction.
+    import pytest
+    
+        # Redirection.
+    300: ('multiple_choices',),
+    301: ('moved_permanently', 'moved', '\\o-'),
+    302: ('found',),
+    303: ('see_other', 'other'),
+    304: ('not_modified',),
+    305: ('use_proxy',),
+    306: ('switch_proxy',),
+    307: ('temporary_redirect', 'temporary_moved', 'temporary'),
+    308: ('permanent_redirect',
+          'resume_incomplete', 'resume',),  # These 2 to be removed in 3.0
+    
+        @pytest.mark.parametrize('scheme', ('http://', 'HTTP://', 'hTTp://', 'HttP://'))
+    def test_mixed_case_scheme_acceptable(self, httpbin, scheme):
+        s = requests.Session()
+        s.proxies = getproxies()
+        parts = urlparse(httpbin('get'))
+        url = scheme + parts.netloc + parts.path
+        r = requests.Request('GET', url)
+        r = s.send(r.prepare())
+        assert r.status_code == 200, 'failed for scheme {}'.format(scheme)
+    
+    from ..common import *
+from hashlib import md5
+from urllib.parse import urlparse
+import re
+    
+    def get_url_of_largest(info, api_key, size):
+    if info['media'] == 'photo':
+        sizes = size_suffixes
+        if size in sizes:
+            sizes = sizes[sizes.index(size):]
+        for suffix in sizes:
+            if 'url_' + suffix in info:
+                return info['url_' + suffix].replace('\\', '')
+        return None
+    else:
+        return get_orig_video_source(api_key, info['id'], info['secret'])
+    
+    def giphy_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
+    
+    def ifeng_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+# old pattern /uuid.shtml
+# now it could be #uuid
+    id = r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', url)
+    if id:
+        return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
+    
+    :copyright: (c) 2010-2017 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+'''
+    
+    :copyright: (c) 2010-2017 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+'''
+from __future__ import absolute_import
+    
+        complete_apps = ['tagstore']
+
+    
+    print(','.join(tags))
+
+    
+    while True:
+    line = sys.stdin.readline()
+    if line=='':
+        break
+    line = line.strip()
+    for word in jieba.cut(line):
+        print(word)
