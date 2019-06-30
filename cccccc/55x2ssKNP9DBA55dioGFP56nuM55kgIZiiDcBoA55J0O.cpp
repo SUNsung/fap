99715e659a3d27a0a39741f35ad9895999b34dca
SUNsung <weatherius@gmail.com>
2019-06-30 14:16:53 +0300

    Rewrite [55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp] File in: 2019-06-30 14:16:52

--- cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
+++ cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
@@ -1,103 +1,313 @@
 
         
-        void PrintHeaderServerMethodSync(grpc_generator::Printer *printer,
-                                 const grpc_generator::Method *method,
-                                 std::map<grpc::string, grpc::string> *vars) {
-  (*vars)['Method'] = method->name();
-  (*vars)['Request'] = method->input_type_name();
-  (*vars)['Response'] = method->output_type_name();
-  printer->Print(method->GetLeadingComments('//').c_str());
-  if (method->NoStreaming()) {
-    printer->Print(*vars,
-                   'virtual ::grpc::Status $Method$('
-                   '::grpc::ServerContext* context, const $Request$* request, '
-                   '$Response$* response);\n');
-  } else if (ClientOnlyStreaming(method)) {
-    printer->Print(*vars,
-                   'virtual ::grpc::Status $Method$('
-                   '::grpc::ServerContext* context, '
-                   '::grpc::ServerReader< $Request$>* reader, '
-                   '$Response$* response);\n');
-  } else if (ServerOnlyStreaming(method)) {
-    printer->Print(*vars,
-                   'virtual ::grpc::Status $Method$('
-                   '::grpc::ServerContext* context, const $Request$* request, '
-                   '::grpc::ServerWriter< $Response$>* writer);\n');
-  } else if (method->BidiStreaming()) {
-    printer->Print(
-        *vars,
-        'virtual ::grpc::Status $Method$('
-        '::grpc::ServerContext* context, '
-        '::grpc::ServerReaderWriter< $Response$, $Request$>* stream);'
-        '\n');
-  }
-  printer->Print(method->GetTrailingComments('//').c_str());
+        template <typename T> struct wAdd
+{
+    typedef T type;
+    }
+    
+    struct BitwiseOr
+{
+    typedef u8 type;
+    }
+    
+            //horizontal convolution (last 3 lines)
+        x = 0;
+        f32* dstb = internal::getRowPtr(dstBase, dstStride, i-1);
+        f32* dstc = internal::getRowPtr(dstBase, dstStride, i);
+        for (; x <= colsn - 4; x += 4)
+        {
+            internal::prefetch(laneA + x + cn);
+            internal::prefetch(laneB + x + cn);
+            internal::prefetch(lanea + x + cn);
+box3x3f32_horiz_ll:
+            float32x4_t lane0a = vld1q_f32(laneA + x - cn);
+            float32x4_t lane2a = vld1q_f32(laneA + x + cn);
+            float32x4_t lane1a = vld1q_f32(laneA + x);
+    }
+    
+    
+    {
+    {        for (; dj < size.width; sj += 2, ++dj)
+        {
+            dst[dj] = src[sj + coi];
+        }
+    }
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)coi;
+#endif
 }
     
-      // Generate table constructors, conditioned on its members' types.
-  void GenTableBuilders(const StructDef &struct_def,
-                        std::string *code_ptr) {
-    std::string &code = *code_ptr;
-    code += 'def ' + NormalizedName(struct_def) +
-            'Start(b_:flatbuffers_builder):\n    b_.StartObject(' +
-            NumToString(struct_def.fields.vec.size()) + ')\n';
-    for (auto it = struct_def.fields.vec.begin();
-        it != struct_def.fields.vec.end(); ++it) {
-      auto &field = **it;
-      if (field.deprecated) continue;
-      auto offset = it - struct_def.fields.vec.begin();
-      code += 'def ' + NormalizedName(struct_def) + 'Add' +
-              MakeCamel(NormalizedName(field)) + '(b_:flatbuffers_builder, ' +
-              NormalizedName(field) + ':' + LobsterType(field.value.type) +
-              '):\n    b_.Prepend' + GenMethod(field.value.type) + 'Slot(' +
-              NumToString(offset) + ', ' + NormalizedName(field) + ', ' +
-              field.value.constant + ')\n';
-      if (field.value.type.base_type == BASE_TYPE_VECTOR) {
-        code += 'def ' + NormalizedName(struct_def) + 'Start' +
-                MakeCamel(NormalizedName(field)) +
-                'Vector(b_:flatbuffers_builder, n_:int):\n    b_.StartVector(';
-        auto vector_type = field.value.type.VectorType();
-        auto alignment = InlineAlignment(vector_type);
-        auto elem_size = InlineSize(vector_type);
-        code += NumToString(elem_size) + ', n_, ' + NumToString(alignment) +
-                ')\n';
-        if (vector_type.base_type != BASE_TYPE_STRUCT ||
-            !vector_type.struct_def->fixed) {
-          code += 'def ' + NormalizedName(struct_def) + 'Create' +
-                  MakeCamel(NormalizedName(field)) +
-                  'Vector(b_:flatbuffers_builder, v_:[' +
-                  LobsterType(vector_type) + ']):\n    b_.StartVector(' +
-                  NumToString(elem_size) + ', v_.length, ' +
-                  NumToString(alignment) +
-                  ')\n    reverse(v_) e_: b_.Prepend' +
-                  GenMethod(vector_type) +
-                  '(e_)\n    b_.EndVector(v_.length)\n';
-        }
+    template<typename T>
+struct OpCmpGT
+{
+    typedef T type;
+    }
+    
+            int32x2_t vs1 = vmov_n_s32(0);
+        int32x2_t vs2 = vmov_n_s32(0);
+        int32x2_t vs3 = vmov_n_s32(0);
+        int32x2_t vs4 = vmov_n_s32(0);
+    
+        std::vector<u8> _buf((size.width+16)*3*(sizeof(ptrdiff_t) + sizeof(u8)) + 128);
+    u8* buf[3];
+    buf[0] = &_buf[0]; buf[1] = buf[0] + size.width; buf[2] = buf[1] + size.width;
+    ptrdiff_t* cpbuf[3];
+    cpbuf[0] = (ptrdiff_t*)internal::alignPtr(buf[2] + size.width, sizeof(ptrdiff_t)) + 1;
+    cpbuf[1] = cpbuf[0] + size.width + 1;
+    cpbuf[2] = cpbuf[1] + size.width + 1;
+    memset(buf[0], 0, size.width*3);
+    
+            for (; j < roiw_tail; j += step_tail, js += step_tail3, jd -= step_tail3)
+        {
+            vec64 v_src = vld3(src + js), v_dst;
+            v_dst.val[0] = vrev64(v_src.val[0]);
+            v_dst.val[1] = vrev64(v_src.val[1]);
+            v_dst.val[2] = vrev64(v_src.val[2]);
+    }
+    
+                uint32x4_t el4h = vaddq_u32(el8shr01l, el8shr01h);
+    
+    		virtual void SetEncodingBits(void) = 0;
+    
+    		typedef struct
+		{
+			unsigned green1a : 3;
+			unsigned red1 : 4;
+			unsigned detect1 : 1;
+			//
+			unsigned blue1b : 2;
+			unsigned detect3 : 1;
+			unsigned blue1a : 1;
+			unsigned green1b : 1;
+			unsigned detect2 : 3;
+			//
+			unsigned green2a : 3;
+			unsigned red2 : 4;
+			unsigned blue1c : 1;
+			//
+			unsigned db : 1;
+			unsigned diff : 1;
+			unsigned da : 1;
+			unsigned blue2 : 4;
+			unsigned green2b : 1;
+			//
+			unsigned int selectors;
+		} H;
+    
+    						// if best CW so far
+						if (fCWError < ptry->m_fError)
+						{
+							ptry->m_uiCW = uiCW;
+							for (unsigned int uiPixel = 0; uiPixel < 8; uiPixel++)
+							{
+								ptry->m_auiSelectors[uiPixel] = auiPixelSelectors[uiPixel];
+							}
+							ptry->m_fError = fCWError;
+						}
+    
+        if (offset < 0) {
+      // Relative to end of file; make it absolute
+      if (-offset > file_size) {
+        offset = 0;
+      } else {
+        offset = file_size + offset;
       }
     }
-    code += 'def ' + NormalizedName(struct_def) +
-            'End(b_:flatbuffers_builder):\n    b_.EndObject()\n\n';
-  }
-    
-    template <class Builder>
-void RetrieveRPC(MonsterStorage::Stub *stub) {
-  Builder fbb;
-  grpc::ClientContext context;
-  fbb.Clear();
-  auto stat_offset = CreateStat(fbb, fbb.CreateString('Fred'));
-  fbb.Finish(stat_offset);
-  auto request = MessageBuilder(std::move(fbb)).ReleaseMessage<Stat>();
+    if (offset > file_size) {
+      offset = file_size;
+    }
+    if (offset + bytes_to_corrupt > file_size) {
+      bytes_to_corrupt = file_size - offset;
     }
     
-      // clang-format off
-  #if !defined(FLATBUFFERS_CPP98_STL)
-  // clang-format on
-  GrpcLikeMessageBuilder &operator=(GrpcLikeMessageBuilder &&other) {
-    // Construct temporary and swap idiom
-    GrpcLikeMessageBuilder temp(std::move(other));
-    Swap(temp);
-    return *this;
+      Status AddRecord(const Slice& slice);
+    
+      // Add an entry into memtable that maps key to value at the
+  // specified sequence number and with the specified type.
+  // Typically value will be empty if type==kTypeDeletion.
+  void Add(SequenceNumber seq, ValueType type, const Slice& key,
+           const Slice& value);
+    
+      // Return the latest node with a key < key.
+  // Return head_ if there is no such node.
+  Node* FindLessThan(const Key& key) const;
+    
+    #include <map>
+#include <set>
+#include <vector>
+    
+      // Approximate size of user data packed per block.  Note that the
+  // block size specified here corresponds to uncompressed data.  The
+  // actual size of the unit read from disk may be smaller if
+  // compression is enabled.  This parameter can be changed dynamically.
+  size_t block_size = 4 * 1024;
+    
+      // If the database contains a mapping for 'key', erase it.  Else do nothing.
+  void Delete(const Slice& key);
+    
+    
+    {  const char* data_;
+  size_t size_;
+  uint32_t restart_offset_;  // Offset in data_ of restart array
+  bool owned_;               // Block owns data_[]
+};
+    
+      void Insert(int key, int value, int charge = 1) {
+    cache_->Release(cache_->Insert(EncodeKey(key), EncodeValue(value), charge,
+                                   &CacheTest::Deleter));
   }
-  // clang-format off
-  #endif  // !defined(FLATBUFFERS_CPP98_STL)
-  // clang-format on
+    
+      // Get file to use for testing.
+  std::string test_dir;
+  ASSERT_OK(env_->GetTestDirectory(&test_dir));
+  std::string test_file_name = test_dir + '/open_on_read.txt';
+  WritableFile* writable_file;
+  ASSERT_OK(env_->NewWritableFile(test_file_name, &writable_file));
+    
+    #pragma once
+    
+    void CHistoryCollector::AddOpenBraceToHistory()
+{
+    AddCommand(std::make_shared<CParentheses>(IDC_OPENP));
+    int ichOpndStart = IchAddSzToEquationSz(CCalcEngine::OpCodeToString(IDC_OPENP), -1);
+    PushLastOpndStart(ichOpndStart);
+    }
+    
+        Number::Number(PNUMBER p) noexcept
+        : m_sign{ p->sign }
+        , m_exp{ p->exp }
+        , m_mantissa{}
+    {
+        m_mantissa.reserve(p->cdigit);
+        copy(p->mant, p->mant + p->cdigit, back_inserter(m_mantissa));
+    }
+    
+        // to be changed when pszexp is back
+    tokens->GetString(&generatedExpression);
+    // Prefixing and suffixing the special Unicode markers to ensure that the expression
+    // in the history doesn't get broken for RTL languages
+    spHistoryItem->historyItemVector.expression = L'\u202d' + generatedExpression + L'\u202c';
+    spHistoryItem->historyItemVector.result = wstring(result);
+    addedIndex = AddItem(spHistoryItem);
+    
+        class CalculatorHistory : public IHistoryDisplay
+    {
+    public:
+        CalculatorHistory(const size_t maxSize);
+        unsigned int AddToHistory(
+            _In_ std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> const& spTokens,
+            _In_ std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> const& spCommands,
+            std::wstring_view result);
+        std::vector<std::shared_ptr<HISTORYITEM>> const& GetHistory();
+        std::shared_ptr<HISTORYITEM> const& GetHistoryItem(unsigned int uIdx);
+        void ClearHistory();
+        unsigned int AddItem(_In_ std::shared_ptr<HISTORYITEM> const& spHistoryItem);
+        bool RemoveItem(unsigned int uIdx);
+        size_t MaxHistorySize() const
+        {
+            return m_maxHistorySize;
+        }
+        ~CalculatorHistory(void);
+    }
+    
+            CalcInput(wchar_t decSymbol)
+            : m_hasExponent(false)
+            , m_hasDecimal(false)
+            , m_decPtIndex(0)
+            , m_decSymbol(decSymbol)
+            , m_base()
+            , m_exponent()
+        {
+        }
+    
+    TEST(ByteTest, SetGetHighLowBit) {
+  unsigned char byte_value = 0x37;
+  Byte value(&byte_value);
+  value.set_value_high_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
+  value.set_value_low_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
+}
+    
+    
+    {  int ret = x;
+  return ret;
+}
+    
+    void ObjectGeneralInfo60B::Parse(const std::uint8_t* bytes, int32_t length,
+                                 ContiRadar* conti_radar) const {
+  int obj_id = object_id(bytes, length);
+  auto conti_obs = conti_radar->add_contiobs();
+  conti_obs->set_clusterortrack(false);
+  conti_obs->set_obstacle_id(obj_id);
+  conti_obs->set_longitude_dist(longitude_dist(bytes, length));
+  conti_obs->set_lateral_dist(lateral_dist(bytes, length));
+  conti_obs->set_longitude_vel(longitude_vel(bytes, length));
+  conti_obs->set_lateral_vel(lateral_vel(bytes, length));
+  conti_obs->set_rcs(rcs(bytes, length));
+  conti_obs->set_dynprop(dynprop(bytes, length));
+  double timestamp = apollo::common::time::Clock::NowInSeconds();
+  auto header = conti_obs->mutable_header();
+  header->CopyFrom(conti_radar->header());
+  header->set_timestamp_sec(timestamp);
+}
+    
+    int ObjectQualityInfo60C::lateral_accel_rms(const std::uint8_t* bytes,
+                                            int32_t length) const {
+  Byte t0(bytes + 4);
+  int32_t x = t0.get_byte(2, 5);
+    }
+    
+    TEST(TestPiecewiseLinearConstraint, add_boundary) {
+  PiecewiseLinearConstraint constraint(10, 0.1);
+  std::vector<uint32_t> index_list;
+  std::vector<double> lower_bound;
+  std::vector<double> upper_bound;
+  for (uint32_t i = 0; i < 10; ++i) {
+    index_list.push_back(i);
+    lower_bound.push_back(1.0);
+    upper_bound.push_back(100.0);
+  }
+    }
+    
+    
+    {  // Report Messages
+  AddRecvProtocolData<Accelrpt68, true>();
+  AddRecvProtocolData<Brakemotorrpt170, true>();
+  AddRecvProtocolData<Brakemotorrpt271, true>();
+  AddRecvProtocolData<Brakemotorrpt372, true>();
+  AddRecvProtocolData<Brakerpt6c, true>();
+  AddRecvProtocolData<Datetimerpt83, true>();
+  AddRecvProtocolData<Globalrpt6a, true>();
+  AddRecvProtocolData<Headlightrpt77, true>();
+  AddRecvProtocolData<Hornrpt79, true>();
+  AddRecvProtocolData<Latlonheadingrpt82, true>();
+  AddRecvProtocolData<Parkingbrakestatusrpt80, true>();
+  AddRecvProtocolData<Shiftrpt66, true>();
+  AddRecvProtocolData<Steeringmotorrpt173, true>();
+  AddRecvProtocolData<Steeringmotorrpt274, true>();
+  AddRecvProtocolData<Steeringmotorrpt375, true>();
+  AddRecvProtocolData<Steeringrpt16e, true>();
+  AddRecvProtocolData<Turnrpt64, true>();
+  AddRecvProtocolData<Vehiclespeedrpt6f, true>();
+  AddRecvProtocolData<Wheelspeedrpt7a, true>();
+  AddRecvProtocolData<Wiperrpt91, true>();
+  AddRecvProtocolData<Yawraterpt81, true>();
+}
+    
+      Byte t3(bytes + 7);
+  t = t3.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
+    
+    #include 'modules/drivers/canbus/common/byte.h'
+#include 'modules/drivers/canbus/common/canbus_consts.h'
+    
+    using ::apollo::drivers::canbus::Byte;
