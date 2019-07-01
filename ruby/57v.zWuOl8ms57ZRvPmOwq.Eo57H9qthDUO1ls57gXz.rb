975a3311e0d21807c4bce8312201a35b1c781f41
SUNsung <weatherius@gmail.com>
2019-07-01 23:28:15 +0300

    Rewrite [57v.zWuOl8ms57ZRvPmOwq.Eo57H9qthDUO1ls57gXz.rb] File in: 2019-07-01 23:28:14

--- ruby/57v.zWuOl8ms57ZRvPmOwq.Eo57H9qthDUO1ls57gXz.rb
+++ ruby/57v.zWuOl8ms57ZRvPmOwq.Eo57H9qthDUO1ls57gXz.rb
@@ -1,141 +1,30 @@
 
         
-          def report
-    @report ||= JSON.parse(request.body.read)['csp-report'].slice(
-      'blocked-uri',
-      'disposition',
-      'document-uri',
-      'effective-directive',
-      'original-policy',
-      'referrer',
-      'script-sample',
-      'status-code',
-      'violated-directive',
-      'line-number',
-      'source-file'
-    )
-  end
+        # These drivers are only used for testing driver switching.
+# They don't actually need to process javascript so use RackTest
     
-      def self.cleanup!
-    DB.exec(<<~SQL)
-      DELETE FROM drafts
-       WHERE sequence < (
-        SELECT MAX(s.sequence)
-          FROM draft_sequences s
-         WHERE s.draft_key = drafts.draft_key
-           AND s.user_id = drafts.user_id
-      )
-    SQL
-    
-    # == Schema Information
-#
-# Table name: user_stats
-#
-#  user_id                  :integer          not null, primary key
-#  topics_entered           :integer          default(0), not null
-#  time_read                :integer          default(0), not null
-#  days_visited             :integer          default(0), not null
-#  posts_read_count         :integer          default(0), not null
-#  likes_given              :integer          default(0), not null
-#  likes_received           :integer          default(0), not null
-#  topic_reply_count        :integer          default(0), not null
-#  new_since                :datetime         not null
-#  read_faq                 :datetime
-#  first_post_created_at    :datetime
-#  post_count               :integer          default(0), not null
-#  topic_count              :integer          default(0), not null
-#  bounce_score             :float            default(0.0), not null
-#  reset_bounce_score_after :datetime
-#  flags_agreed             :integer          default(0), not null
-#  flags_disagreed          :integer          default(0), not null
-#  flags_ignored            :integer          default(0), not null
-#  first_unread_at          :datetime         not null
-#
-
-    
-        if user.admin && user.last_seen_at.nil? && user.is_singular_admin?
-      Jobs.enqueue(:enable_bootstrap_mode, user_id: user.id)
+      context 'with custom selector' do
+    it 'should use the custom selector' do
+      Capybara.add_selector(:level) do
+        xpath { |num| './/*[@id='ancestor#{num}']' }
+      end
+      el = @session.find(:css, '#child')
+      expect(el.ancestor(:level, 1)[:id]).to eq 'ancestor1'
+      expect(el.ancestor(:level, 3)[:id]).to eq 'ancestor3'
     end
   end
     
-              relation.update_all(update)
-        end
-      end
+        it 'doesn't find invisible elements when `true`' do
+      expect do
+        @session.find_by_id('hidden_via_ancestor', visible: true)
+      end.to raise_error(Capybara::ElementNotFound)
     end
   end
 end
 
     
-            def importer_class
-          IssueAndLabelLinksImporter
-        end
-    
-            # issue - An instance of `Gitlab::GithubImport::Representation::Issue`
-        # project - An instance of `Project`
-        # client - An instance of `Gitlab::GithubImport::Client`
-        def initialize(issue, project, client)
-          @issue = issue
-          @project = project
-          @client = client
-          @label_finder = LabelFinder.new(project)
-        end
-    
-          # Returns true if the given object has already been imported, false
-      # otherwise.
-      #
-      # object - The object to check.
-      def already_imported?(object)
-        id = id_for_already_imported_cache(object)
-    
-          def initialize(app)
-        @app = app
-      end
-    
-          DIRECTIVES = %i(base_uri child_src connect_src default_src
-                      font_src form_action frame_ancestors frame_src
-                      img_src manifest_src media_src object_src
-                      plugin_types referrer reflected_xss report_to
-                      report_uri require_sri_for sandbox script_src
-                      style_src worker_src).freeze
-    
-      class OfflinePluginPackager
-    LOGSTASH_DIR = 'logstash'
-    DEPENDENCIES_DIR = ::File.join(LOGSTASH_DIR, 'dependencies')
-    
-          # Try to add the gems to the current gemfile and lock file, if successful
-      # both of them will be updated. This injector is similar to Bundler's own injector class
-      # minus the support for additionals source and doing local resolution only.
-      ::Bundler::LogstashInjector.inject!(pack)
-    
-      # We compare the before the update and after the update
-  def display_updated_plugins(previous_gem_specs_map)
-    update_count = 0
-    find_latest_gem_specs.values.each do |spec|
-      name = spec.name.downcase
-      if previous_gem_specs_map.has_key?(name)
-        if spec.version != previous_gem_specs_map[name].version
-          puts('Updated #{spec.name} #{previous_gem_specs_map[name].version.to_s} to #{spec.version.to_s}')
-          update_count += 1
-        end
-      else
-        puts('Installed #{spec.name} #{spec.version.to_s}')
-        update_count += 1
-      end
+      context 'aria_label attribute with Capybara.enable_aria_label' do
+    it 'should find when true' do
+      Capybara.enable_aria_label = true
+      expect(@session.find_link('Go to simple')[:href]).to match %r{/with_simple_html$}
     end
-    
-        desc 'Bootstrap all the VM's used for this tests'
-    task :setup, :platform do |t, args|
-      config   = PlatformConfig.new
-      experimental = (ENV['LS_QA_EXPERIMENTAL_OS'].to_s.downcase || 'false') == 'true'
-      machines = config.select_names_for(args[:platform], {'experimental' => experimental})
-    
-              it 'successfully install the plugin when verification is disabled' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest')
-          end
-    
-          it 'display a list of installed plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --installed')
-        expect(result.stdout.split('\n').size).to be > 1
-      end
