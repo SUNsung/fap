a0ca7947ac3479e983e0dfef4cbd5e1b6a3cae3a
SUNsung <weatherius@gmail.com>
2019-07-01 11:22:56 +0300

    Rewrite [58pKwUqBRGu4Y58SROCZwavgfw58zrZHueczYpw58L4e.cpp] File in: 2019-07-01 11:22:56

--- cccccc/58pKwUqBRGu4Y58SROCZwavgfw58zrZHueczYpw58L4e.cpp
+++ cccccc/58pKwUqBRGu4Y58SROCZwavgfw58zrZHueczYpw58L4e.cpp
@@ -1,168 +1,239 @@
 
         
-        
-    {}  // namespace
+        #include <QIcon>
+#include <QPixmap>
+#include <QString>
     
-      std::map<uv_timer_t*, base::OnceClosure> tasks_;
-    
-    #include 'atom/browser/api/event_emitter.h'
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(RenderProcessPreferences);
-};
-    
-    class WebRequest : public mate::TrackableObject<WebRequest> {
- public:
-  static mate::Handle<WebRequest> Create(v8::Isolate* isolate,
-                                         AtomBrowserContext* browser_context);
+    SECP256K1_INLINE static void secp256k1_fe_sqr_inner(uint64_t *r, const uint64_t *a) {
+/**
+ * Registers: rdx:rax = multiplication accumulator
+ *            r9:r8   = c
+ *            rcx:rbx = d
+ *            r10-r14 = a0-a4
+ *            r15     = M (0xfffffffffffff)
+ *            rdi     = r
+ *            rsi     = a / t?
+ */
+  uint64_t tmp1, tmp2, tmp3;
+__asm__ __volatile__(
+    'movq 0(%%rsi),%%r10\n'
+    'movq 8(%%rsi),%%r11\n'
+    'movq 16(%%rsi),%%r12\n'
+    'movq 24(%%rsi),%%r13\n'
+    'movq 32(%%rsi),%%r14\n'
+    'movq $0xfffffffffffff,%%r15\n'
     }
     
-    #include 'atom/browser/atom_quota_permission_context.h'
+    int secp256k1_ecdsa_recoverable_signature_convert(const secp256k1_context* ctx, secp256k1_ecdsa_signature* sig, const secp256k1_ecdsa_recoverable_signature* sigin) {
+    secp256k1_scalar r, s;
+    int recid;
+    }
     
-      AtomQuotaPermissionContext();
+    BOOST_AUTO_TEST_CASE(univalue_constructor)
+{
+    UniValue v1;
+    BOOST_CHECK(v1.isNull());
+    }
     
-    void OffscreenViewProxy::OnEvent(ui::Event* event) {
-  if (view_) {
-    view_->OnEvent(event);
+    /** Encode a Bech32 string. Returns the empty string in case of failure. */
+std::string Encode(const std::string& hrp, const std::vector<uint8_t>& values);
+    
+    void InternalKeyComparator::FindShortestSeparator(
+      std::string* start,
+      const Slice& limit) const {
+  // Attempt to shorten the user portion of the key
+  Slice user_start = ExtractUserKey(*start);
+  Slice user_limit = ExtractUserKey(limit);
+  std::string tmp(user_start.data(), user_start.size());
+  user_comparator_->FindShortestSeparator(&tmp, user_limit);
+  if (tmp.size() < user_start.size() &&
+      user_comparator_->Compare(user_start, tmp) < 0) {
+    // User key has become shorter physically, but larger logically.
+    // Tack on the earliest possible number to the shortened user key.
+    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
+    assert(this->Compare(*start, tmp) < 0);
+    assert(this->Compare(tmp, limit) < 0);
+    start->swap(tmp);
   }
 }
     
-    #ifndef ATOM_BROWSER_UI_COCOA_ROOT_VIEW_MAC_H_
-#define ATOM_BROWSER_UI_COCOA_ROOT_VIEW_MAC_H_
+    class FileNameTest { };
     
-    #endif  // ATOM_BROWSER_UI_COCOA_VIEWS_DELEGATE_MAC_H_
+    
+    {  // Moving messages on the same arena should lead to swapped pointers.
+  *message2_on_arena = std::move(*message1_on_arena);
+  EXPECT_EQ(nested, &message2_on_arena->optional_nested_message());
+}
+    
+    Status::Status() : error_code_(error::OK) {
+}
+    
+    #endif  // PROTOBUF_BENCHMARKS_UTIL_DATA_PROTO2_TO_PROTO3_UTIL_H_
 
     
-        private:
-        bool ShouldWriteUpdate(size_t update) const
-        {
-            if (m_frequency == 0)
-            {
-                // Geometric schedule - write at every 2^(i) steps, with i = 1, 2, 3, ...
-                return ((update + 1) & update) == 0;
-            }
+    class EnumGenerator {
+ public:
+  explicit EnumGenerator(const EnumDescriptor* descriptor);
+  ~EnumGenerator();
     }
     
-            static Dictionary Serialize(const FunctionPtr& f);
-    
-            // 'initializer' must be a random initializer
-        auto initializerType = initializer[InitializerTypeAttributeName].Value<std::wstring>();
-        if ((initializerType != Microsoft::MSR::CNTK::UniformInitializerTypeName) &&
-            (initializerType != Microsoft::MSR::CNTK::BilinearInitializerTypeName) &&
-            (initializerType != Microsoft::MSR::CNTK::ConstantInitializerTypeName))
-        {
-            int oldOutputRank = initializer[OutputRankAttributeName].Value<int>();
-            int oldFilterRank = initializer[FilterRankAttributeName].Value<int>();
+    // Tests that an exit code describes a normal exit with a given exit code.
+class GTEST_API_ ExitedWithCode {
+ public:
+  explicit ExitedWithCode(int exit_code);
+  bool operator()(int exit_status) const;
+ private:
+  // No implementation - assignment is unsupported.
+  void operator=(const ExitedWithCode& other);
     }
     
-    
-    {    return supportsDistributedMBRead;
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22, typename T23, typename T24, typename T25,
+    typename T26, typename T27, typename T28, typename T29, typename T30,
+    typename T31, typename T32, typename T33, typename T34, typename T35,
+    typename T36, typename T37>
+internal::ValueArray37<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
+    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
+    T29, T30, T31, T32, T33, T34, T35, T36, T37> Values(T1 v1, T2 v2, T3 v3,
+    T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12,
+    T13 v13, T14 v14, T15 v15, T16 v16, T17 v17, T18 v18, T19 v19, T20 v20,
+    T21 v21, T22 v22, T23 v23, T24 v24, T25 v25, T26 v26, T27 v27, T28 v28,
+    T29 v29, T30 v30, T31 v31, T32 v32, T33 v33, T34 v34, T35 v35, T36 v36,
+    T37 v37) {
+  return internal::ValueArray37<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
+      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
+      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37>(v1, v2, v3,
+      v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19,
+      v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33,
+      v34, v35, v36, v37);
 }
     
-    template <typename TVector>
-inline void RandomShuffleMT(TVector& v, std::mt19937_64& rng)
-{
-    foreach_index(currentLocation, v)
-    {
-        // Pick a random location and swap with current one
-        const size_t randomLocation = RandMT(0, v.size(), rng);
-        std::swap(v[currentLocation], v[randomLocation]);
-    }
-}
+      // Same as above, but you can choose the interception scope of this object.
+  ScopedFakeTestPartResultReporter(InterceptMode intercept_mode,
+                                   TestPartResultArray* result);
     
-        EditDistanceErrorNode(const ScriptableObjects::IConfigRecordPtr configp)
-        : EditDistanceErrorNode(configp->Get(L'deviceId'), L'<placeholder>', configp->Get(L'subPen'), configp->Get(L'delPen'), configp->Get(L'insPen'), configp->Get(L'squashInputs'), {})
-    {
-        AttachInputsFromConfig(configp, this->GetExpectedNumInputs());
-        m_tokensToIgnore = ScriptableObjects::ConfigArray::FlattenedVectorFrom<size_t>(configp->Get(L'tokensToIgnore'));
-    }
     
-        // initialize by reading a matrix from a text file
-    void InitFromFile(const std::wstring& initFromFilePath);
     
-        if (!semi) {
-      // only media type (type/subtype,data)
-      ssize_t media_len = comma - data;
-      meta_len -= media_len;
-      data += media_len;
-    } else if (slash && slash < semi) {
-      // media type + param (type/subtype;param,data)
-      ssize_t media_len = semi - data;
-      meta_len -= media_len;
-      data += media_len;
-    } else {
-      // no media type (;base64,data)
-      if (semi != data // ex. foo;base64,data
-          || meta_len != sizeof(';base64') - 1 // ex. ;something,data
-          || memcmp(data, ';base64',
-                    sizeof(';base64') - 1)) { // ex. ;base65,data
-          raise_warning('rfc2397: invalid meta data');
+    // Helpers for suppressing warnings on unreachable code or constant
+// condition.
+    
+    // A sample program demonstrating using Google C++ testing framework.
+//
+// Author: wan@google.com (Zhanyong Wan)
+    
+    
+// This sample shows how to write a more complex unit test for a class
+// that has multiple member functions.
+//
+// Usually, it's a good idea to have one test for each method in your
+// class.  You don't have to do that exactly, but it helps to keep
+// your tests organized.  You may also throw in additional tests as
+// needed.
+    
+    
+    {    assertx(data == comma || data == semi);
+    // eat parameters, and figure out if we have ';base64'
+    while (semi && (data == semi)) {
+      data++;
+      meta_len--;
+      char* equals = (char*)memchr(data, '=', meta_len);
+      semi = (char*)memchr(data, ';', meta_len);
+      if (!equals || (semi && semi < data)) {
+        // no equals, so either 'base64' or its bad
+        if (meta_len != sizeof('base64') - 1 ||
+            memcmp(data, 'base64', sizeof('base64')-1)) {
+          raise_warning('rfc2396: invalid parameter');
           return nullptr;
         }
+        // it's 'base64', we're done
+        base64 = true;
+        meta_len -= sizeof('base64') - 1;
+        data += sizeof('base64') - 1;
+        break;
+      }
+      // there's a parameter
+      if (semi) {
+        meta_len -= semi - data + 1;
+        data = semi;
+      } /* else, we're done with meta */
+    }
+  }
+  data = comma + 1;
+  data_len -= 1;
+  String decoded;
+    
+    #include 'hphp/runtime/base/glob-stream-wrapper.h'
+    
+    /*
+ * Attempt to log an entry to the perf warning service.
+ *
+ * If StructuredLog::enabled() returns false or this event is discarded by the
+ * effective sample rate, nothing will be logged. If both of those checks pass,
+ * fillCols will be passed a StructuredLogEntry& to populate, which will then
+ * be logged. The column names 'event_name' and 'priority' are reserved and
+ * will be overwritten is fillCols() sets them.
+ *
+ * The effective sample rate is determined by Eval.PerfWarningSampleRate * rate
+ * (or kDefaultPerfWarningRate for the overloads that don't take a rate). If
+ * the effective sample rate is 0, all events will be discarded.
+ */
+template<typename F>
+void logPerfWarning(folly::StringPiece event, F fillCols);
+template<typename F>
+void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
+    
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
     }
     
-    struct GlobStreamWrapper final : Stream::Wrapper {
-  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-  req::ptr<Directory> opendir(const String& path) override;
+    
+    {///////////////////////////////////////////////////////////////////////////////
+}
+    
+    #pragma once
+    
+      // put and get from non-default column family
+  s = db->Put(WriteOptions(), handles[1], Slice('key'), Slice('value'));
+  assert(s.ok());
+  std::string value;
+  s = db->Get(ReadOptions(), handles[1], Slice('key'), &value);
+  assert(s.ok());
+    
+    #include 'rocksdb/db.h'
+#include 'rocksdb/slice.h'
+#include 'rocksdb/options.h'
+    
+    
+    {  virtual std::string GetPrintableOptions() const = 0;
 };
     
-    #ifndef incl_HPHP_PERF_EVENT_H_
-#define incl_HPHP_PERF_EVENT_H_
     
-    template<typename F>
-void logPerfWarning(folly::StringPiece event, F fillCols) {
-  logPerfWarningImpl(event, 1, kDefaultPerfWarningRate, fillCols);
-}
-template<typename F>
-void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols) {
-  logPerfWarningImpl(event, 1, rate, fillCols);
-}
+    { private:
+  DB* db_;
+  const Snapshot* snapshot_;
+};
     
-        // TODO: Hide OS mouse cursor if ImGui is drawing it
-    // s3ePointerSetInt(S3E_POINTER_HIDE_CURSOR,(io.MouseDrawCursor ? 0 : 1));
     
-            // Find index from cfg.DstFont (we allow the user to set cfg.DstFont. Also it makes casual debugging nicer than when storing indices)
-        src_tmp.DstIndex = -1;
-        for (int output_i = 0; output_i < atlas->Fonts.Size && src_tmp.DstIndex == -1; output_i++)
-            if (cfg.DstFont == atlas->Fonts[output_i])
-                src_tmp.DstIndex = output_i;
-        IM_ASSERT(src_tmp.DstIndex != -1); // cfg.DstFont not pointing within atlas->Fonts[] array?
-        if (src_tmp.DstIndex == -1)
-            return false;
+    {  virtual ~Checkpoint() {}
+};
     
-    namespace ImGuiFreeType
-{
-    // Hinting greatly impacts visuals (and glyph sizes).
-    // When disabled, FreeType generates blurrier glyphs, more or less matches the stb's output.
-    // The Default hinting mode usually looks good, but may distort glyphs in an unusual way.
-    // The Light hinting mode generates fuzzier glyphs but better matches Microsoft's rasterizer.
+    // Options to control the behavior of a database (passed to
+// DB::Open). A LevelDBOptions object can be initialized as though
+// it were a LevelDB Options object, and then it can be converted into
+// a RocksDB Options object.
+struct LevelDBOptions {
+  // -------------------
+  // Parameters that affect behavior
     }
     
-    IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForOpenGL(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForVulkan(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplGlfw_NewFrame();
+    #pragma once
     
-        // stream signature
-    stb_out(0x57); stb_out(0xbc);
-    stb_out2(0);
-    
-    void ResetDevice()
-{
-    ImGui_ImplDX9_InvalidateDeviceObjects();
-    HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
-    if (hr == D3DERR_INVALIDCALL)
-        IM_ASSERT(0);
-    ImGui_ImplDX9_CreateDeviceObjects();
-}
-    
-    // **DO NOT USE THIS CODE IF YOUR CODE/ENGINE IS USING MODERN OPENGL (SHADERS, VBO, VAO, etc.)**
-// **Prefer using the code in the example_sdl_opengl3/ folder**
-// See imgui_impl_sdl.cpp for details.
-    
-    static ImGui_ImplVulkanH_Window g_MainWindowData;
-static uint32_t                 g_MinImageCount = 2;
-static bool                     g_SwapChainRebuild = false;
-static int                      g_SwapChainResizeWidth = 0;
-static int                      g_SwapChainResizeHeight = 0;
+      // Block current thread until condition variable is notified by a call to
+  // Notify() or NotifyAll().  Wait() will be called with mutex locked.
+  // Returns OK if notified.
+  // Returns non-OK if TransactionDB should stop waiting and fail the operation.
+  // May return OK spuriously even if not notified.
+  virtual Status Wait(std::shared_ptr<TransactionDBMutex> mutex) = 0;
