5320bfed3ece8c5cb0d3efa3b700f565b28bbeee
SUNsung <weatherius@gmail.com>
2019-07-01 12:12:59 +0300

    Rewrite [289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py] File in: 2019-07-01 12:12:58

--- pyton/289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py
+++ pyton/289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py
@@ -1,139 +1,91 @@
 
         
-        
-def finalize_headers(headers):
-    final_headers = {}
-    for name, value in headers.items():
-        if value is not None:
+            READY = 0
+    IN_PROGRESS = 1
+    COMPLETE = 2
+    
+        def __init__(self):
+        self.head = None
+        self.tail = None
     
     
-def test_default_options_overwrite(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['json'] == {'foo': 'bar'}
+class PagesDataStore(object):
     
+            if self.values[key] is None:
+            self._set_value(key, data)
     
-def test_unicode_headers_verbose(httpbin):
-    # httpbin doesn't interpret utf8 headers
-    r = http('--verbose', httpbin.url + '/headers', u'Test:%s' % UNICODE)
-    assert HTTP_OK in r
-    assert UNICODE in r
+    The problem is :
+Given two strings A and B. Find the minimum number of operations to string B such that A = B. The permitted operations are removal,  insertion, and substitution.
+'''
+from __future__ import print_function
     
-        @property
-    def cookies(self):
-        jar = RequestsCookieJar()
-        for name, cookie_dict in self['cookies'].items():
-            jar.set_cookie(create_cookie(
-                name, cookie_dict.pop('value'), **cookie_dict))
-        jar.clear_expired_cookies()
-        return jar
-    
-        with gzip.open(paths[3], 'rb') as imgpath:
-        x_test = np.frombuffer(imgpath.read(), np.uint8,
-                               offset=16).reshape(len(y_test), 28, 28)
-    
-    
-def l1_l2(l1=0.01, l2=0.01):
-    return L1L2(l1=l1, l2=l2)
-    
-            # Instantiate the base model (or 'template' model).
-        # We recommend doing this with under a CPU device scope,
-        # so that the model's weights are hosted on CPU memory.
-        # Otherwise they may end up hosted on a GPU, which would
-        # complicate weight sharing.
-        with tf.device('/cpu:0'):
-            model = Xception(weights=None,
-                             input_shape=(height, width, 3),
-                             classes=num_classes)
-    
-    # Stack of Transposed Conv2D blocks
-# Notes:
-# 1) Use Batch Normalization before ReLU on deep networks
-# 2) Use UpSampling2D as alternative to strides>1
-# - faster but not as good as strides>1
-for filters in layer_filters[::-1]:
-    x = Conv2DTranspose(filters=filters,
-                        kernel_size=kernel_size,
-                        strides=2,
-                        activation='relu',
-                        padding='same')(x)
-    
-    # create complete model
-model = Sequential(feature_layers + classification_layers)
-    
-    num_classes = np.max(y_train) + 1
-print(num_classes, 'classes')
-    
-    mse = MSE = mean_squared_error
-mae = MAE = mean_absolute_error
-mape = MAPE = mean_absolute_percentage_error
-msle = MSLE = mean_squared_logarithmic_error
-kld = KLD = kullback_leibler_divergence
-cosine = cosine_proximity
-    
-    plt.figure('scikit-learn Ward's method benchmark results')
-plt.imshow(np.log(ratio), aspect='auto', origin='lower')
-plt.colorbar()
-plt.contour(ratio, levels=[1, ], colors='k')
-plt.yticks(range(len(n_features)), n_features.astype(np.int))
-plt.ylabel('N features')
-plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
-plt.xlabel('N samples')
-plt.title('Scikit's time, in units of scipy time (log)')
-plt.show()
+        return diff
 
     
-        # Print and plot the confusion matrix
-    cm = metrics.confusion_matrix(y_test, y_predicted)
-    print(cm)
+        def train(self, X, y):
+        '''
+        train:
+        @param X: a one dimensional numpy array
+        @param y: a one dimensional numpy array. 
+        The contents of y are the labels for the corresponding X values
     
-    from sklearn import datasets
-from sklearn.utils import shuffle
-from sklearn.metrics import mean_squared_error
-from sklearn.svm.classes import NuSVR
-from sklearn.ensemble.gradient_boosting import GradientBoostingRegressor
-from sklearn.linear_model.stochastic_gradient import SGDClassifier
-from sklearn.metrics import hamming_loss
-    
-    # Create a graph capturing local connectivity. Larger number of neighbors
-# will give more homogeneous clusters to the cost of computation
-# time. A very large number of neighbors gives more evenly distributed
-# cluster sizes, but may not impose the local manifold structure of
-# the data
-knn_graph = kneighbors_graph(X, 30, include_self=False)
-    
-    These images how similar features are merged together using
-feature agglomeration.
+      3. find centroids and clusters using kmeans function.
+  
+        centroids, cluster_assignment = kmeans(
+            X, 
+            k, 
+            initial_centroids, 
+            maxiter=400,
+            record_heterogeneity=heterogeneity, 
+            verbose=True # whether to print logs in console or not.(default=False)
+            )
+  
+  
+  4. Plot the loss function, hetrogeneity values for every iteration saved in hetrogeneity list.
+        plot_heterogeneity(
+            heterogeneity, 
+            k
+        )
+  
+  5. Have fun..
+  
 '''
-print(__doc__)
+from __future__ import print_function
+from sklearn.metrics import pairwise_distances
+import numpy as np
     
-    digits = datasets.load_digits(n_class=10)
-X = digits.data
-y = digits.target
-n_samples, n_features = X.shape
+        return res
     
-    '''
-Dependency Injection (DI) is a technique whereby one object supplies the dependencies (services)
-to another object (client).
-It allows to decouple objects: no need to change client code simply because an object it depends on
-needs to be changed to a different one. (Open/Closed principle)
+    # add noise
+y += 0.01 * np.random.normal((n_samples,))
     
-        @property
-    def _lazy_property(self):
-        if not hasattr(self, attr):
-            setattr(self, attr, fn(self))
-        return getattr(self, attr)
+    Sentiment analysis can be casted as a binary text classification problem,
+that is fitting a linear classifier on features extracted from the text
+of the user messages so as to guess wether the opinion of the author is
+positive or negative.
     
-        def test_object(queue):
-        pool = ObjectPool(queue, True)
-        print('Inside func: {}'.format(pool.item))
+    Sentiment analysis can be casted as a binary text classification problem,
+that is fitting a linear classifier on features extracted from the text
+of the user messages so as to guess wether the opinion of the author is
+positive or negative.
     
-    ''
+        plt.imshow(avg_dist, interpolation='nearest', cmap=plt.cm.gnuplot2,
+               vmin=0)
+    plt.xticks(range(n_clusters), labels, rotation=45)
+    plt.yticks(range(n_clusters), labels)
+    plt.colorbar()
+    plt.suptitle('Interclass %s distances' % metric, size=18)
+    plt.tight_layout()
     
-    from __future__ import print_function
-import time
+    # Code source: Gaël Varoquaux
+# Modified for documentation by Jaques Grobler
+# License: BSD 3 clause
     
-        def test_parrot_eng_localization(self):
-        self.assertEqual(self.e.get('parrot'), 'parrot')
+    identic = np.logical_not(different)
+ax.plot(X[identic, 0], X[identic, 1], 'w',
+        markerfacecolor='#bbbbbb', marker='.')
+ax.plot(X[different, 0], X[different, 1], 'w',
+        markerfacecolor='m', marker='.')
+ax.set_title('Difference')
+ax.set_xticks(())
+ax.set_yticks(())
