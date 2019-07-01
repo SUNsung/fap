1154ac56f062e088838f2a42e4358c79785c23c8
SUNsung <weatherius@gmail.com>
2019-07-01 15:04:26 +0300

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2019-07-01 15:04:25

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,119 +1,135 @@
 
         
-            def can_fit_vehicle(self, vehicle):
-        if self.vehicle is not None:
-            return False
-        return vehicle.can_fit_in_spot(self)
+        
+@pytest.mark.functional
+def test_select_command_with_arrows(proc, TIMEOUT):
+    select_command_with_arrows(proc, TIMEOUT)
+    history_changed(proc, TIMEOUT, u'git help')
+    
+    containers = (('thefuck/python3-fish',
+               u'''FROM python:3
+                   # Use jessie-backports since it has the fish package. See here for details:
+                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
+                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
+                   RUN apt-get update
+                   RUN apt-get install -yy fish''',
+               u'fish'),
+              ('thefuck/python2-fish',
+               u'''FROM python:2
+                   # Use jessie-backports since it has the fish package. See here for details:
+                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
+                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
+                   RUN apt-get update
+                   RUN apt-get install -yy fish''',
+               u'fish'))
     
     
-class HitCounts(MRJob):
+init_zshrc = u'''echo '
+export SHELL=/usr/bin/zsh
+export HISTFILE=~/.zsh_history
+echo > $HISTFILE
+export SAVEHIST=100
+export HISTSIZE=100
+eval $(thefuck --alias {})
+setopt INC_APPEND_HISTORY
+echo 'instant mode ready: $THEFUCK_INSTANT_MODE'
+' > ~/.zshrc'''
     
-      If my_dataset (filename) stem is in the directory, the read routine will try
-  and load it.  The datasets dictionary will then look like
-  dataset['first_day'] -> (first day data dictionary)
-  dataset['second_day'] -> (first day data dictionary)
     
-    # write out the dataset
-write_datasets(FLAGS.save_dir, FLAGS.datafile_name, datasets)
-print ('Saved to ', os.path.join(FLAGS.save_dir,
-                                 FLAGS.datafile_name + '_' + dataset_name))
+@pytest.mark.parametrize('command, packages, which', [
+    (Command('a_bad_cmd', 'a_bad_cmd: command not found'),
+     [], None),
+    (Command('vim', ''), [], None),
+    (Command('', ''), [], None),
+    (Command('vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim'),
+    (Command('sudo vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim')])
+def test_not_match(mocker, command, packages, which):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=which)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
+    
+    
+@pytest.mark.parametrize('script, output, help_text, result', [
+    ('apt-get isntall vim', invalid_operation('isntall'),
+     apt_get_help, 'apt-get install vim'),
+    ('apt saerch vim', invalid_operation('saerch'),
+     apt_help, 'apt search vim'),
+])
+def test_get_new_command(set_help, output, script, help_text, result):
+    set_help(help_text)
+    assert get_new_command(Command(script, output))[0] == result
 
     
-          # Cut sentences into patches of shape processable by the LM.
-      batch_size, num_timesteps = self.shape
-      word_patches = utils.cut_to_patches(sentences, batch_size, num_timesteps)
-      probs = self._score_patches(word_patches)
     
-      Args:
-    values: Value given by the Wasserstein Discriminator of shape [batch_size,
-      sequence_length] to an imputed batch (real and fake).
-    is_real_input: tf.bool Tensor of shape [batch_size, sequence_length]. If
-      true, it indicates that the label is known.
+def test_mnist():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        assert len(x_train) == len(y_train) == 60000
+        assert len(x_test) == len(y_test) == 10000
     
-    EXTRA_ARGS = {
-    'recode-video': ['--arguments', 'mp4 flv ogg webm mkv', '--exclusive'],
-    }
-    
-    signature = hexlify(rsa.pkcs1.sign(json.dumps(versions_info, sort_keys=True).encode('utf-8'), privkey, 'SHA-256')).decode()
-print('signature: ' + signature)
-    
-    
-def main():
-    with open('supportedsites.html.in', 'r', encoding='utf-8') as tmplf:
-        template = tmplf.read()
-    
-        with open(ZSH_COMPLETION_FILE, 'w') as f:
-        f.write(template)
-    
-    # The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'default'
-    
-    
-if __name__ == '__main__':
-    unittest.main()
+        # Returns
+        A normalized copy of the array.
+    '''
+    l2 = np.atleast_1d(np.linalg.norm(x, order, axis))
+    l2[l2 == 0] = 1
+    return x / np.expand_dims(l2, axis)
 
     
-            jsi = JSInterpreter('''
-        function f() {
-            var x = '/*';
-            var y = 1 /* comment */ + 2;
-            return y;
-        }
-        ''')
-        self.assertEqual(jsi.call_function('f'), 3)
+        @property
+    def recurrent_dropout(self):
+        return self.cell.recurrent_dropout
     
-        def test_secondary_proxy_https(self):
-        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL()
-        req = compat_urllib_request.Request('https://yt-dl.org/ip')
-        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
-        self.assertEqual(
-            ydl.urlopen(req).read().decode('utf-8'),
-            params['secondary_server_ip'])
+    x_test_inp = layers.Input(shape=x_test.shape[1:])
+test_out = cnn_layers(x_test_inp)
+test_model = keras.models.Model(inputs=x_test_inp, outputs=test_out)
     
-        if function_name:
-        return {function_name: camel_dict_to_snake_dict(lambda_facts)}
+    Given a training dataset of corrupted data as input and
+true signal as output, a denoising autoencoder can recover the
+hidden structure to generate clean data.
+    
+    arxiv:1504.00941v2 [cs.NE] 7 Apr 2015
+http://arxiv.org/pdf/1504.00941v2.pdf
     
     
-def protocol_to_string(protocol):
-    protocol = protocol_to_tuple(protocol)
-    if protocol[0] is True:
-        return 'Tcp'
-    elif protocol[1] is True:
-        return 'Udp'
-    elif protocol[2] is True:
-        return 'Icmp'
-    elif protocol[3] is True:
-        return 'Other'
-    elif protocol[4] is True:
-        return 'Any'
+def test_invalid_data_format():
+    with pytest.raises(ValueError):
+        K.normalize_data_format('channels_middle')
     
-            rule['original_port'] = str(rule.get('original_port', 'any')).lower()
-        rule['original_ip'] = rule.get('original_ip', 'any').lower()
-        rule['translated_ip'] = rule.get('translated_ip', 'any').lower()
-        rule['translated_port'] = str(rule.get('translated_port', 'any')).lower()
-        rule['protocol'] = rule.get('protocol', 'any').lower()
-        rule['rule_type'] = rule.get('rule_type', 'DNAT').lower()
+            if 'latency' in request.args:
+            latency = float(request.args['latency'][0])
+            reactor.callLater(latency, self._finish, request)
+            return NOT_DONE_YET
     
-    RETURN = '''
-zone:
-  description: DNS zone as returned by IPA API.
-  returned: always
-  type: dict
-'''
+        name = 'qps'
+    benchurl = 'http://localhost:8880/'
     
-        device = dict(
-        model_handle=model_handle,
-        address=device_ip,
-        landscape=model_landscape,
-    )
-    
-            if self.crawler_process.bootstrap_failed:
-            self.exitcode = 1
-
+        def process_options(self, args, opts):
+        ScrapyCommand.process_options(self, args, opts)
+        try:
+            opts.spargs = arglist_to_dict(opts.spargs)
+        except ValueError:
+            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
+        if opts.output:
+            if opts.output == '-':
+                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
+            else:
+                self.settings.set('FEED_URI', opts.output, priority='cmdline')
+            feed_exporters = without_none_values(
+                self.settings.getwithbase('FEED_EXPORTERS'))
+            valid_output_formats = feed_exporters.keys()
+            if not opts.output_format:
+                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
+            if opts.output_format not in valid_output_formats:
+                raise UsageError('Unrecognized output format '%s', set one'
+                                 ' using the '-t' switch or as a file extension'
+                                 ' from the supported list %s' % (opts.output_format,
+                                                                  tuple(valid_output_formats)))
+            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
     
     
 def sanitize_module_name(module_name):
@@ -126,81 +142,43 @@ def sanitize_module_name(module_name):
         module_name = 'a' + module_name
     return module_name
     
-        def get_callback_from_rules(self, spider, response):
-        if getattr(spider, 'rules', None):
-            for rule in spider.rules:
-                if rule.link_extractor.matches(response.url):
-                    return rule.callback or 'parse'
-        else:
-            logger.error('No CrawlSpider rules found in spider %(spider)r, '
-                         'please specify a callback to use for parsing',
-                         {'spider': spider.name})
     
-        def run(self, args, opts):
-        if opts.verbose:
-            versions = scrapy_components_versions()
-            width = max(len(n) for (n, _) in versions)
-            patt = '%-{}s : %s'.format(width)
-            for name, version in versions:
-                print(patt % (name, version))
-        else:
-            print('Scrapy %s' % scrapy.__version__)
-    
-            try:
-            self.max_bound = int(self.args[2])
-        except IndexError:
-            self.max_bound = float('inf')
-    
-        from zope.interface.declarations import implementer
-    
-            # Confirmed GoAgent CA exist
-        if not os.path.exists(CertUtil.ca_keyfile):
-            if os.path.exists(CertUtil.ca_certfile):
-                # update old unsafe CA file
-                xlog.info('update CA file storage format')
-                if hasattr(OpenSSL.crypto, 'X509StoreContext'):
-                    os.rename(CertUtil.ca_certfile, CertUtil.ca_keyfile)
-                else:
-                    xlog.warning('users may need to re-import CA file')
-                    CertUtil.generate_ca_file()
-            else:
-                xlog.info('no GAE CA file exist in XX-Net data dir')
-    
-        noarch_lib = os.path.abspath( os.path.join(python_path, 'lib', 'noarch'))
-    sys.path.append(noarch_lib)
-    
-    
-    def error(self, nvae):
-        '''A hook for debugging interface'''
-        pass
-    
-            
-    def computeContextSensitiveRuleFOLLOW(self):
-        '''
-        Compute the context-sensitive FOLLOW set for current rule.
-        This is set of token types that can follow a specific rule
-        reference given a specific call chain.  You get the set of
-        viable tokens that can possibly come next (lookahead depth 1)
-        given the current call chain.  Contrast this with the
-        definition of plain FOLLOW for rule r:
-    
-        References:
-        tflearn.prelu
+class ScrapyHTTPClientFactory(HTTPClientFactory):
+    '''Scrapy implementation of the HTTPClientFactory overwriting the
+    serUrl method to make use of our Url object that cache the parse
+    result.
     '''
-    if channel_shared:
-        alpha_shape = get_shape(x)[-1:]
-    else:
-        alpha_shape = [1]
     
     
-# TODO(huay)
-def conv1d():
+def _test():
     ''''''
+    file_path = r'./data.txt'
     
-        References:
-        K.repeat()
-        tf.tile()
-    '''
-    assert x.get_shape().ndims == 2
-    x = tf.expand_dims(x, axis=1)  # -> [batch_size, 1, n_input]
-    return tf.tile(x, [1, n, 1])  # -> [batch_size, n, n_input]
+        如果需要 reuse 推荐使用类实现的 `Dense`
+    
+    
+
+    
+    
+def multi_highway_conv2d(x, kernel_size, n_layer,
+                         act_fn=relu,
+                         strides=1,
+                         padding='SAME',
+                         carry_bias=-1.0,
+                         name=None):
+    '''多层 highway_conv2d'''
+    if isinstance(kernel_size, int):
+        kernel_size = [kernel_size] * n_layer
+    
+    # 因为 'a', 'aa', 'aaa' 中都只含有 '<a' ，所以它们直积上都只是 '<a'
+print(model.wv['a'])
+print(model.wv['aa'])
+print(model.wv['aaa'])
+print(model.wv['<a'])
+'''
+[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
+[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
+[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
+[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
+'''
+print()
