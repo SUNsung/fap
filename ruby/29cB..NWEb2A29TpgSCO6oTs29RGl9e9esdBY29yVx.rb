35f99c7f27bba8b8038b7415669a188de4e5a0ac
SUNsung <weatherius@gmail.com>
2019-07-01 16:50:17 +0300

    Rewrite [29cB..NWEb2A29TpgSCO6oTs29RGl9e9esdBY29yVx.rb] File in: 2019-07-01 16:50:15

--- ruby/29cB..NWEb2A29TpgSCO6oTs29RGl9e9esdBY29yVx.rb
+++ ruby/29cB..NWEb2A29TpgSCO6oTs29RGl9e9esdBY29yVx.rb
@@ -1,124 +1,57 @@
 
         
-            private
+        class Api::Web::SettingsController < Api::Web::BaseController
+  respond_to :json
     
-      def self.path
-    '/highlight-js/#{Discourse.current_hostname}/#{version SiteSetting.highlighted_languages}.js'
-  end
-end
-
-    
-          response.headers['Last-Modified'] = 10.years.ago.httpdate
-      response.headers['Content-Length'] = svg_sprite.bytesize.to_s
-      immutable_for 1.year
-    
-          log(action: 'miss token',
-          user_id: user_token&.user_id,
-          auth_token: token,
-          user_agent: opts && opts[:user_agent],
-          path: opts && opts[:path],
-          client_ip: opts && opts[:client_ip])
-    
-      # Under Phusion Passenger smart spawning, we need to reopen all IO streams
-  # after workers have forked.
-  #
-  # The rolling file appender uses shared file locks to ensure that only one
-  # process will roll the log file. Each process writing to the file must have
-  # its own open file descriptor for `flock` to function properly. Reopening
-  # the file descriptors after forking ensures that each worker has a unique
-  # file descriptor.
-  if defined? PhusionPassenger
-    PhusionPassenger.on_event(:starting_worker_process) do |forked|
-      Logging.reopen if forked
-    end
-  end
-end
-    
-      describe '#destroy' do
-    before do
-      @message = bob.post(:status_message, text: 'hey', to: @alices_aspect.id)
-      @like = alice.like!(@message)
-    end
-    
-          it 'returns an array of reshares for a post' do
-        bob.reshare!(@post)
-        get :index, params: {post_id: @post.id}, format: :json
-        expect(JSON.parse(response.body).map {|h| h['id'] }).to eq(@post.reshares.map(&:id))
-      end
-    
-      # Integration tests
-  gem 'diffy'
-  gem 'clintegracon'
-    
-            def self.options
-          [[
-            '--short', 'Only print the path relative to the cache root'
-          ]].concat(super)
-        end
-    
-              versions.each do |version|
-            spec = source.specification(set.name, version)
-            paths += '#{pathname_from_spec(spec, source)}\n'
-          end
-        end
-    
-    RSpec.describe JSFiddleTag, type: :liquid_template do
-  describe '#link' do
-    let(:jsfiddle_link) { 'http://jsfiddle.net/link2twenty/v2kx9jcd' }
-    let(:jsfiddle_link_with_custom_tabs) { 'http://jsfiddle.net/link2twenty/v2kx9jcd result,html,css' }
-    
-      def user_is_author?
-    record.user_id == user.id
-  end
-end
-
-    
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
-  end
-    
-    class ConfigTag < Liquid::Tag
-  def initialize(tag_name, options, tokens)
-    super
-    options = options.split(' ').map {|i| i.strip }
-    @key = options.slice!(0)
-    @tag = nil
-    @classname = nil
-    options.each do |option|
-      @tag = $1 if option =~ /tag:(\S+)/ 
-      @classname = $1 if option =~ /classname:(\S+)/
-    end
-  end
-    
-      class IncludeArrayTag < Liquid::Tag
-    Syntax = /(#{Liquid::QuotedFragment}+)/
-    def initialize(tag_name, markup, tokens)
-      if markup =~ Syntax
-        @array_name = $1
+          if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: provider_id.capitalize) if is_navigational_format?
       else
-        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
-      end
-    
-    describe Tmuxinator::Pane do
-  let(:klass) { described_class }
-  let(:instance) { klass.new(index, project, window, *commands) }
-  # let(:index) { 'vim' }
-  # let(:project) { 0 }
-  # let(:tab) { nil }
-  # let(:commands) { nil }
-  let(:index) { 0 }
-  let(:project) { double }
-  let(:window) { double }
-  let(:commands) { ['vim', 'bash'] }
-    
-              if yes?('Are you sure you want to delete #{project}?(y/n)', :red)
-            FileUtils.rm(config)
-            say 'Deleted #{project}'
-          end
-        else
-          say '#{project} does not exist!'
-        end
+        session['devise.#{provider}_data'] = request.env['omniauth.auth']
+        redirect_to new_user_registration_url
       end
     end
+  end
+    
+      private
+    
+        process_font_assets
+    process_stylesheet_assets
+    process_javascript_assets
+    store_version
+  end
+    
+    Then(/^the default stage files are created$/) do
+  staging = TestApp.test_app_path.join('config/deploy/staging.rb')
+  production = TestApp.test_app_path.join('config/deploy/production.rb')
+  expect(File.exist?(staging)).to be true
+  expect(File.exist?(production)).to be true
+end
+    
+        def top_level_tasks
+      if tasks_without_stage_dependency.include?(@top_level_tasks.first)
+        @top_level_tasks
+      else
+        @top_level_tasks.unshift(ensure_stage.to_s)
+      end
+    end
+    
+          def each
+        servers_by_key.values.each { |server| yield server }
+      end
+    
+          # Register a validation rule for the given key.
+      def validate(key, &validator)
+        vs = (validators[key] || [])
+        vs << validator
+        validators[key] = vs
+      end
+    
+          # Internal use only.
+      def peek(key, default=nil, &block)
+        value = fetch_for(key, default, &block)
+        while callable_without_parameters?(value)
+          value = (values[key] = value.call)
+        end
+        value
+      end
