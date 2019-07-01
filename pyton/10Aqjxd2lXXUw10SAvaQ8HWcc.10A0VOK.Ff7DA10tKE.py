7d03cfa8568370a3a638def8113cd1fc4382cf19
SUNsung <weatherius@gmail.com>
2019-07-02 01:41:41 +0300

    Rewrite [10Aqjxd2lXXUw10SAvaQ8HWcc.10A0VOK.Ff7DA10tKE.py] File in: 2019-07-02 01:41:40

--- pyton/10Aqjxd2lXXUw10SAvaQ8HWcc.10A0VOK.Ff7DA10tKE.py
+++ pyton/10Aqjxd2lXXUw10SAvaQ8HWcc.10A0VOK.Ff7DA10tKE.py
@@ -1,107 +1,278 @@
 
         
-        import random
+            # This be set automatically once the plugin has been loaded.
+    package_name = None
     
     
-class RangeMaxValueValidator(MaxValueValidator):
-    def compare(self, a, b):
-        return a.upper is None or a.upper > b
-    message = _('Ensure that this range is completely less than or equal to %(limit_value)s.')
+def test_default_options_overwrite(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['json'] == {'foo': 'bar'}
+    
+        def test_POST_explicit_JSON_auto_JSON_accept(self, httpbin):
+        r = http('--json', 'POST', httpbin.url + '/post')
+        assert HTTP_OK in r
+        assert r.json['headers']['Accept'] == JSON_ACCEPT
+        # Make sure Content-Type gets set even with no data.
+        # https://github.com/jakubroztocil/httpie/issues/137
+        assert 'application/json' in r.json['headers']['Content-Type']
+    
+        exc = Timeout('Request timed out')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR_TIMEOUT
+    assert error_msg == 'Request timed out (30s).'
+
+    
+    import numpy as np
+from scipy.cluster import hierarchy
+import matplotlib.pyplot as plt
+    
+    fit_data = data[np.argsort(model.row_labels_)]
+fit_data = fit_data[:, np.argsort(model.column_labels_)]
+    
+    This example is commented in the
+:ref:`tutorial section of the user manual <introduction>`.
+    
+    
+# Plot clustering results
+for index, metric in enumerate(['cosine', 'euclidean', 'cityblock']):
+    model = AgglomerativeClustering(n_clusters=n_clusters,
+                                    linkage='average', affinity=metric)
+    model.fit(X)
+    plt.figure()
+    plt.axes([0, 0, 1, 1])
+    for l, c in zip(np.arange(model.n_clusters), 'rgbk'):
+        plt.plot(X[model.labels_ == l].T, c=c, alpha=.5)
+    plt.axis('tight')
+    plt.axis('off')
+    plt.suptitle('AgglomerativeClustering(affinity=%s)' % metric, size=20)
+    
+    np.random.seed(0)
+    
+    # #############################################################################
+# Compute clustering with MeanShift
+    
+    import numpy as np
+import matplotlib.pyplot as plt
+    
+        test_suite = 'tests',
+    
+        # Get (branch, commit) if running from a git repo.
+    head = git.get_head(kwargs['repo_path'])
+    
+        def prepare(self, **kwargs):
+    
+    
+    def download_by_id(self, vid = '', title = None, output_dir='.', merge=True, info_only=False,**kwargs):
+        '''self, str->None
+        
+        Keyword arguments:
+        self: self
+        vid: The video ID for BokeCC cloud, something like
+        FE3BB999594978049C33DC5901307461
+        
+        Calls the prepare() to download the video.
+        
+        If no title is provided, this method shall try to find a proper title
+        with the information providin within the
+        returned content of the API.'''
+    
+        if '_text' in dictified['video'][0]['size'][0]:  #size exists for 1 piece
+        video_dict['size'] = sum([int(i['size'][0]['_text']) for i in dictified['video']])
+    
+        elif 'subject' in url:
+        titles = re.findall(r'data-title='([^']*)'>', html)
+        song_id = re.findall(r'<li class='song-item' id='([^']*)'', html)
+        song_ssid = re.findall(r'data-ssid='([^']*)'', html)
+        get_song_url = 'http://music.douban.com/j/songlist/get_song_url'
+    
+            if s: self.streams['video'] = {'url': s }
+        if mp3: self.streams['audio'] = { 'url': mp3 }
+        if pdf: self.streams['slides'] = { 'url': pdf }
     
     from django.conf import settings
-from django.template.backends.django import DjangoTemplates
-from django.template.loader import get_template
-from django.utils.functional import cached_property
-from django.utils.module_loading import import_string
+from django.core.exceptions import ImproperlyConfigured
+    
+            # Removing index on 'EventTag', fields ['environment_id', 'key', 'value']
+        db.delete_index(u'tagstore_eventtag', ['environment_id', 'key_id', 'value_id'])
+    
+            # Removing index on 'TagValue', fields ['project_id', '_key', 'last_seen']
+        db.delete_index(u'tagstore_tagvalue', ['project_id', 'key_id', 'last_seen'])
+    
+        fftv.publish('cartoon')
+    fftv.publish('music')
+    fftv.publish('ads')
+    fftv.publish('movie')
+    fftv.publish('cartoon')
+    fftv.publish('cartoon')
+    fftv.publish('movie')
+    fftv.publish('blank')
+    
+        def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+        '''
+            Here the name of the class is used as key but it could be any class
+            parameter.
+        '''
+        cls.REGISTRY[new_cls.__name__] = new_cls
+        return new_cls
+    
+        x1 = 'x1'
+    x2 = 'x2'
+    
+        # Compute masks at different aspect ratios
+    for aspect_ratio in cfg.TEST.MASK_AUG.ASPECT_RATIOS:
+        masks_ar = im_detect_mask_aspect_ratio(model, im, aspect_ratio, boxes)
+        masks_ts.append(masks_ar)
+    
+        # For other levels add top-down and lateral connections
+    for i in range(num_backbone_stages - 1):
+        add_topdown_lateral_module(
+            model,
+            output_blobs[i],             # top-down blob
+            lateral_input_blobs[i + 1],  # lateral blob
+            output_blobs[i + 1],         # next output blob
+            fpn_dim,                     # output dimension
+            fpn_dim_lateral[i + 1]       # lateral input dimension
+        )
     
     
-class PollErrorTest(unittest.TestCase):
-    '''Tests for acme.errors.PollError.'''
-    
-            '''
-        super(AugeasConfigurator, self).recovery_routine()
-        # Need to reload configuration after these changes take effect
-        self.aug.load()
-    
-    # Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named 'default.css' will overwrite the builtin 'default.css'.
-html_static_path = ['_static']
-    
-    
-Named Arguments
----------------
-    
-        # Close the env and write monitor result info to disk
-    env.close()
-
-    
-        return output_spec
+def add_roi_Xconv1fc_head(model, blob_in, dim_in, spatial_scale):
+    '''Add a X conv + 1fc head, as a reference if not using GroupNorm'''
+    hidden_dim = cfg.FAST_RCNN.CONV_HEAD_DIM
+    roi_size = cfg.FAST_RCNN.ROI_XFORM_RESOLUTION
+    roi_feat = model.RoIFeatureTransform(
+        blob_in,
+        'roi_feat',
+        blob_rois='rois',
+        method=cfg.FAST_RCNN.ROI_XFORM_METHOD,
+        resolution=roi_size,
+        sampling_ratio=cfg.FAST_RCNN.ROI_XFORM_SAMPLING_RATIO,
+        spatial_scale=spatial_scale
+    )
     
     
-if __name__ == '__main__':
-  flags.mark_flag_as_required('input_file')
-  flags.mark_flag_as_required('vocab_file')
-  flags.mark_flag_as_required('bert_config_file')
-  flags.mark_flag_as_required('init_checkpoint')
-  flags.mark_flag_as_required('output_file')
-  tf.app.run()
-
+def add_roi_pose_head_v1convX(model, blob_in, dim_in, spatial_scale):
+    '''Add a Mask R-CNN keypoint head. v1convX design: X * (conv).'''
+    hidden_dim = cfg.KRCNN.CONV_HEAD_DIM
+    kernel_size = cfg.KRCNN.CONV_HEAD_KERNEL
+    pad_size = kernel_size // 2
+    current = model.RoIFeatureTransform(
+        blob_in,
+        '_[pose]_roi_feat',
+        blob_rois='keypoint_rois',
+        method=cfg.KRCNN.ROI_XFORM_METHOD,
+        resolution=cfg.KRCNN.ROI_XFORM_RESOLUTION,
+        sampling_ratio=cfg.KRCNN.ROI_XFORM_SAMPLING_RATIO,
+        spatial_scale=spatial_scale
+    )
     
-        def run(self):
-        time.sleep(1)
-        self.output().open('w').close()
+    def add_training_inputs(model, roidb=None):
+    '''Create network input ops and blobs used for training. To be called
+    *after* model_builder.create().
+    '''
+    # Implementation notes:
+    #   Typically, one would create the input ops and then the rest of the net.
+    #   However, creating the input ops depends on loading the dataset, which
+    #   can take a few minutes for COCO.
+    #   We prefer to avoid waiting so debugging can fail fast.
+    #   Thus, we create the net *without input ops* prior to loading the
+    #   dataset, and then add the input ops after loading the dataset.
+    #   Since we defer input op creation, we need to do a little bit of surgery
+    #   to place the input ops at the start of the network op list.
+    assert model.train, 'Training inputs can only be added to a trainable model'
+    if roidb is not None:
+        # To make debugging easier you can set cfg.DATA_LOADER.NUM_THREADS = 1
+        model.roi_data_loader = RoIDataLoader(
+            roidb,
+            num_loaders=cfg.DATA_LOADER.NUM_THREADS,
+            minibatch_queue_size=cfg.DATA_LOADER.MINIBATCH_QUEUE_SIZE,
+            blobs_queue_capacity=cfg.DATA_LOADER.BLOBS_QUEUE_CAPACITY
+        )
+    orig_num_op = len(model.net._net.op)
+    blob_names = roi_data_minibatch.get_minibatch_blob_names(is_training=True)
+    for gpu_id in range(cfg.NUM_GPUS):
+        with c2_utils.NamedCudaScope(gpu_id):
+            for blob_name in blob_names:
+                workspace.CreateBlob(core.ScopedName(blob_name))
+            model.net.DequeueBlobs(
+                model.roi_data_loader._blobs_queue_name, blob_names
+            )
+    # A little op surgery to move input ops to the start of the net
+    diff = len(model.net._net.op) - orig_num_op
+    new_op = model.net._net.op[-diff:] + model.net._net.op[:-diff]
+    del model.net._net.op[:]
+    model.net._net.op.extend(new_op)
     
-        def run(self):
-        print('{task} says: Hello world!'.format(task=self.__class__.__name__))
-    
-        def output(self):
-        return luigi.LocalTarget(path='/tmp/_docs-%s.ldj' % self.task_id)
-    
-        def mapper(self, line):
-        for word in line.strip().split():
-            yield word, 1
-    
-          - Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
-      - Initially, let p equal 2, the smallest prime number.
-      - Enumerate the multiples of p by counting to n from 2p in increments of p, and mark them in the list (these will be 2p, 
-        3p, 4p, ...; the p itself should not be marked).
-      - Find the first number greater than p in the list that is not marked. If there was no such number, stop. Otherwise, let 
-        p now equal this new number (which is the next prime), and repeat from step 3.
-      - When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.
-'''
-import sys
-    
-    # Description   : This will scan the current directory and all subdirectories and display the size.
-    
-    for host in HOSTS:
-    result = []
-    for command in COMMANDS:
-        ssh = subprocess.Popen(['ssh', '%s' % host, command],
-                               shell=False,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-        result.append(ssh.stdout.readlines())
-    print('--------------- ' + host + ' --------------- ')
-    for res in result:
-        if not res:
-            print(ssh.stderr.readlines())
-            break
+        # ==========================================================================
+    # cls loss - depends on softmax/sigmoid outputs
+    # ==========================================================================
+    for lvl in range(k_min, k_max + 1):
+        suffix = 'fpn{}'.format(lvl)
+        cls_lvl_logits = 'retnet_cls_pred_' + suffix
+        if not cfg.RETINANET.SOFTMAX:
+            cls_focal_loss = model.net.SigmoidFocalLoss(
+                [
+                    cls_lvl_logits, 'retnet_cls_labels_' + suffix,
+                    'retnet_fg_num'
+                ],
+                ['fl_{}'.format(suffix)],
+                gamma=cfg.RETINANET.LOSS_GAMMA,
+                alpha=cfg.RETINANET.LOSS_ALPHA,
+                scale=model.GetLossScale(),
+                num_classes=model.num_classes - 1
+            )
+            gradients.append(cls_focal_loss)
+            losses.append('fl_{}'.format(suffix))
         else:
-            print(res)
-
+            cls_focal_loss, gated_prob = model.net.SoftmaxFocalLoss(
+                [
+                    cls_lvl_logits, 'retnet_cls_labels_' + suffix,
+                    'retnet_fg_num'
+                ],
+                ['fl_{}'.format(suffix), 'retnet_prob_{}'.format(suffix)],
+                gamma=cfg.RETINANET.LOSS_GAMMA,
+                alpha=cfg.RETINANET.LOSS_ALPHA,
+                scale=model.GetLossScale(),
+                num_classes=model.num_classes
+            )
+            gradients.append(cls_focal_loss)
+            losses.append('fl_{}'.format(suffix))
     
-    if __name__=='__main__':
-    game()
-
+    from detectron.core.config import cfg
+from detectron.modeling.generate_anchors import generate_anchors
+from detectron.utils.c2 import const_fill
+from detectron.utils.c2 import gauss_fill
+import detectron.modeling.FPN as FPN
+import detectron.utils.blob as blob_utils
     
-            self.inputNotice = Text(self.TNotebook1_t0)
-        self.inputNotice.place(relx=0.02, rely=0.28, relheight=0.64
-                , relwidth=0.68)
-        self.inputNotice.configure(background='white')
-        self.inputNotice.configure(font='TkTextFont')
-        self.inputNotice.configure(selectbackground='#c4c4c4')
-        self.inputNotice.configure(width=396)
-        self.inputNotice.configure(wrap=WORD)
+    import logging
     
-    # Modifications		: 
+    from detectron.core.config import cfg
+import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
+import detectron.roi_data.retinanet as retinanet_roi_data
+import detectron.roi_data.rpn as rpn_roi_data
+import detectron.utils.blob as blob_utils
+    
+    
+class BatchPermutationOpTest(unittest.TestCase):
+    def _run_op_test(self, X, I, check_grad=False):
+        with core.DeviceScope(core.DeviceOption(caffe2_pb2.CUDA, 0)):
+            op = core.CreateOperator('BatchPermutation', ['X', 'I'], ['Y'])
+            workspace.FeedBlob('X', X)
+            workspace.FeedBlob('I', I)
+        workspace.RunOperatorOnce(op)
+        Y = workspace.FetchBlob('Y')
+    
+    
+def test_fixarray():
+    check_array(1, 0)
+    check_array(1, (1 << 4) - 1)
+    
+        expected = series.groupby(groupers).mean()
+    
+        s = s.sort_index()
