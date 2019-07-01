7b82ed64a0d37ecc8572f5ed3b997ba7222a0bc7
SUNsung <weatherius@gmail.com>
2019-07-01 04:00:49 +0300

    Rewrite [33uEWL2KXe4g33o7rjUtegd6w33Ly8RAImwQWU33xk.cpp] File in: 2019-07-01 04:00:49

--- cccccc/33uEWL2KXe4g33o7rjUtegd6w33Ly8RAImwQWU33xk.cpp
+++ cccccc/33uEWL2KXe4g33o7rjUtegd6w33Ly8RAImwQWU33xk.cpp
@@ -1,200 +1,78 @@
 
         
-            void addWeighted(const Size2D &size,
-                     const s16 * src0Base, ptrdiff_t src0Stride,
-                     const s16 * src1Base, ptrdiff_t src1Stride,
-                     s16 * dstBase, ptrdiff_t dstStride,
-                     f32 alpha, f32 beta, f32 gamma);
-    
-    #include <cstdlib>
-#include <iostream>
-    
-    template <typename T>
-inline T *getRowPtr(T *base, ptrdiff_t stride, size_t row)
-{
-    char *baseRaw = const_cast<char *>(reinterpret_cast<const char *>(base));
-    return reinterpret_cast<T *>(baseRaw + ptrdiff_t(row) * stride);
-}
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(s32, s8, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src1]]                              \n\t'
-             'vld1.32 {d2-d3}, [%[src2]]                              \n\t'
-             'vqmovn.s32 d4, q0                                       \n\t'
-             'vqmovn.s32 d5, q1                                       \n\t'
-             'vqmovn.s16  d6, q2                                      \n\t'
-             'vst1.8 {d6}, [%[dst]]                                   \n\t'
-             : /*no output*/
-             : [src1] 'r' (_src + i + 0),
-               [src2] 'r' (_src + i + 4),
-               [dst] 'r' (_dst + i)
-             : 'd0','d1','d2','d3','d4','d5','d6'
-         );
-     }
-})
-#else
-CVT_FUNC(s32, s8, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         int32x4_t vline1_s32 = vld1q_s32(_src + i);
-         int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
-    }
-    }
-    
-            while(i + 16 <= size.width)
-        {
-            size_t lim = std::min(i + DOT_UINT_BLOCKSIZE, size.width) - 16;
-    }
-    
-    // If the max-min top of a unicharset char is bigger than kMaxCharTopRange
-// then the char top cannot be used to judge misfits or suggest a new top.
-const int kMaxCharTopRange = 48;
-    
-      // Computes all the cross product distances of the points perpendicular to
-  // the given direction, ignoring distances outside of the give distance range,
-  // storing the actual (signed) cross products in distances_.
-  void ComputeConstrainedDistances(const FCOORD& direction,
-                                   double min_dist, double max_dist);
-    
-    class LLSQ {
- public:
-  LLSQ() {  // constructor
-    clear();  // set to zeros
-  }
-  void clear();  // initialize
-    }
-    
-    
-    { private:
-  tesseract::ParagraphJustification justification_;
-  int margin_;
-  int first_indent_;
-  int body_indent_;
-  int tolerance_;
-};
-    
-    
-    { private:
-  CensusContext context_;
-  // Metadata elements for tracing and census stats data.
-  grpc_linked_mdelem stats_bin_;
-  grpc_linked_mdelem tracing_bin_;
-  // Client method.
-  absl::string_view method_;
-  std::string qualified_method_;
-  grpc_slice path_;
-  // The recv trailing metadata callbacks.
-  grpc_metadata_batch* recv_trailing_metadata_;
-  grpc_closure* initial_on_done_recv_trailing_metadata_;
-  grpc_closure on_done_recv_trailing_metadata_;
-  // recv message
-  grpc_closure* initial_on_done_recv_message_;
-  grpc_closure on_done_recv_message_;
-  // Start time (for measuring latency).
-  absl::Time start_time_;
-  // Server elapsed time in nanoseconds.
-  uint64_t elapsed_time_;
-  // The received message--may be null.
-  grpc_core::OrphanablePtr<grpc_core::ByteStream>* recv_message_;
-  // Number of messages in this RPC.
-  uint64_t recv_message_count_;
-  uint64_t sent_message_count_;
-  // Buffer needed for grpc_slice to reference when adding trace context
-  // metatdata to outgoing message.
-  char tracing_buf_[kMaxTraceContextLen];
-};
-    
-    // Server
-MeasureDouble RpcServerSentBytesPerRpc() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcServerSentBytesPerRpcMeasureName,
-      'Total bytes sent across all messages per RPC', kUnitBytes);
-  return measure;
-}
-    
-    #endif /* GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_MEASURES_H */
+        #endif
 
     
-    CreateThreadPoolFunc g_ctp_impl = CreateDefaultThreadPoolImpl;
+    #ifdef CAROTENE_NEON
+#include <arm_neon.h>
+#include 'intrinsics.hpp'
+#endif
     
-    namespace grpc {
-namespace load_reporter {
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
+CVT_FUNC(u16, s8, 16,
+    register uint8x16_t v127 asm ('q4') = vmovq_n_u8(127);,
+{
+    for (size_t i = 0; i < w; i += 16)
+    {
+        internal::prefetch(_src + i);
+        __asm__ (
+            'vld1.8 {d0-d1}, [%[src1]]                             \n\t'
+            'vqmovn.u16 d4, q0                                     \n\t'
+            'vld1.8 {d2-d3}, [%[src2]]                             \n\t'
+            'vqmovn.u16 d5, q1                                     \n\t'
+            'vmin.u8 q3, q2, q4                                    \n\t'
+            'vst1.8 {d6-d7}, [%[dst]]                              \n\t'
+            : /*no output*/
+            : [src1] 'r' (_src + i),
+              [src2] 'r' (_src + i + 8),
+              [dst] 'r' (_dst + i + 0),
+              'w' (v127)
+            : 'd0','d1','d2','d3','d4','d5','d6','d7'
+         );
+    }
+})
+#else
+CVT_FUNC(u16, s8, 16,
+    uint8x8_t v127 = vmov_n_u8(127);,
+{
+    for (size_t i = 0; i < w; i += 16)
+    {
+        internal::prefetch(_src + i);
+        uint16x8_t vline1_u16 = vld1q_u16(_src + i);
+        uint16x8_t vline2_u16 = vld1q_u16(_src + i + 8);
     }
     }
     
-    #include 'src/proto/grpc/core/stats.pb.h'
-    
-      std::string Key(int i) {
-    char buf[100];
-    snprintf(buf, sizeof(buf), 'key%06d', i);
-    return std::string(buf);
-  }
-    
-      // Reports dropped bytes to the reporter.
-  // buffer_ must be updated to remove the dropped bytes prior to invocation.
-  void ReportCorruption(uint64_t bytes, const char* reason);
-  void ReportDrop(uint64_t bytes, const Status& reason);
-    
-    namespace leveldb {
+    template <typename T>
+inline void inRangeCheck(const Size2D &_size,
+                         const T * srcBase, ptrdiff_t srcStride,
+                         const T * rng1Base, ptrdiff_t rng1Stride,
+                         const T * rng2Base, ptrdiff_t rng2Stride,
+                         u8 * dstBase, ptrdiff_t dstStride)
+{
+    typedef typename internal::VecTraits<T>::vec128 vec128;
+    typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
     }
     
-    #include 'db/db_impl.h'
-#include 'leveldb/db.h'
-#include 'leveldb/env.h'
-#include 'util/testharness.h'
+            s16 prevx = 0, currx = 0, nextx = 0;
+        ptrdiff_t x = 0;
+        const ptrdiff_t bwidth = y + 2 < height ? width : (width - 8);
     
-      Status(const Status& rhs);
-  Status& operator=(const Status& rhs);
+      // localnode
+  // 8bytes reserved
+  WRITE_CHECK(fp, zero, 8);
+  // 20bytes localnode ID
+  WRITE_CHECK(fp, localNode_->getID(), DHT_ID_LENGTH);
+  // 4bytes reserved
+  WRITE_CHECK(fp, zero, 4);
     
-    // A Table is a sorted map from strings to strings.  Tables are
-// immutable and persistent.  A Table may be safely accessed from
-// multiple threads without external synchronization.
-class LEVELDB_EXPORT Table {
- public:
-  // Attempt to open the table that is stored in bytes [0..file_size)
-  // of 'file', and read the metadata entries necessary to allow
-  // retrieving data from the table.
-  //
-  // If successful, returns ok and sets '*table' to the newly opened
-  // table.  The client should delete '*table' when no longer needed.
-  // If there was an error while initializing the table, sets '*table'
-  // to nullptr and returns a non-ok status.  Does not take ownership of
-  // '*source', but the client must ensure that 'source' remains live
-  // for the duration of the returned table's lifetime.
-  //
-  // *file must remain live while this Table is in use.
-  static Status Open(const Options& options, RandomAccessFile* file,
-                     uint64_t file_size, Table** table);
-    }
+      virtual void startup() = 0;
     
-      // The size of the database changes caused by this batch.
-  //
-  // This number is tied to implementation details, and may change across
-  // releases. It is intended for LevelDB usage metrics.
-  size_t ApproximateSize() const;
+    DHTTaskQueueImpl::DHTTaskQueueImpl()
+    : periodicTaskQueue1_(NUM_CONCURRENT_TASK),
+      periodicTaskQueue2_(NUM_CONCURRENT_TASK),
+      immediateTaskQueue_(NUM_CONCURRENT_TASK)
+{
+}
     
-    
-    {}  // namespace leveldb
-    
-      // We do not emit the index entry for a block until we have seen the
-  // first key for the next data block.  This allows us to use shorter
-  // keys in the index block.  For example, consider a block boundary
-  // between the keys 'the quick brown fox' and 'the who'.  We can use
-  // 'the r' as the key for the index block entry since it is >= all
-  // entries in the first block and < all entries in subsequent
-  // blocks.
-  //
-  // Invariant: r->pending_index_entry is true only if data_block is empty.
-  bool pending_index_entry;
-  BlockHandle pending_handle;  // Handle to add to index block
-    
-    static const int kBlockSize = 4096;
+    #endif // D_DHT_TASK_QUEUE_IMPL_H
