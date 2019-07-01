fb35ba573aa56850ca45e70dcfe771e10b5c3f5d
SUNsung <weatherius@gmail.com>
2019-07-01 14:40:30 +0300

    Rewrite [54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb] File in: 2019-07-01 14:40:30

--- ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
+++ ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
@@ -1,21 +1,67 @@
 
         
-        Then(/^the current directory will be a symlink to the release$/) do
-  run_vagrant_command(exists?('e', TestApp.current_path))
-end
+              expect(exporter.as_json[:control_links]).to eq([{ :controller => guid_order(agent_list, :jane_rain_notifier_agent), :control_target => guid_order(agent_list, :jane_weather_agent) }])
+    end
+  end
     
-        def dry_run
-      ['--dry-run', '-n',
-       'Do a dry run without executing actions',
-       lambda do |_value|
-         Configuration.env.set(:sshkit_backend, SSHKit::Backend::Printer)
-       end]
+      after :each do
+    @scheduler.shutdown(:wait)
+    
+        it 'should raise an exception when encountering complex JSONPaths' do
+      expect { LiquidMigrator.convert_string('Received <$.content.text.*> from <$.content.name> .') }.
+        to raise_error('JSONPath '$.content.text.*' is too complex, please check your migration.')
+    end
+  end
+    
+        it 'should generate the correct specific tracking url' do
+      @checker.options['path'] = 'trackings/usps/9361289878905919630610'
+      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/trackings/usps/9361289878905919630610')
     end
     
-          def gets
-        return unless $stdin.tty?
+    require 'open-uri'
+require 'json'
+require 'strscan'
+require 'forwardable'
+require 'term/ansicolor'
+require 'fileutils'
     
-    MESSAGE
-      end
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
+  end
     
-    set_if_empty :pty, false
+        def each(&block)
+      @cookies.each(&block)
+    end
+    
+    module Grape
+  module DSL
+    module Helpers
+      extend ActiveSupport::Concern
+      include Grape::DSL::Configuration
+    
+    Before do |scenario|
+  scenario.source_tag_names.each do |tag|
+    driver_name = tag.sub(/^@/, '').to_sym
+    Capybara.current_driver = driver_name if Capybara.drivers.key?(driver_name)
+  end
+end
+
+    
+      it 'should match correctly normalized title' do
+    uri = Addressable::URI.parse('/with_title')
+    uri.query_values = { title: ' &nbsp; with space &nbsp;title   ' }
+    @session.visit(uri.to_s)
+    @session.assert_title('  with space  title')
+    expect { @session.assert_title('with space title') }.to raise_error(Capybara::ExpectationNotMet)
+  end
+    
+        it 'should find disabled buttons when :all' do
+      expect(@session.find_button('Disabled button', disabled: :all).value).to eq('Disabled button')
+    end
+  end
+    
+      it 'should be false if the given link is on the page' do
+    expect(@session).not_to have_no_link('foo')
+    expect(@session).not_to have_no_link('awesome title')
+    expect(@session).not_to have_no_link('A link', href: '/with_simple_html')
+  end
