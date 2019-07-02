c8001957d94b4600c28d8b2227ee198b77108b79
SUNsung <weatherius@gmail.com>
2019-07-02 19:13:59 +0300

    Rewrite [21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb] File in: 2019-07-02 19:13:59

--- ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
+++ ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
@@ -1,113 +1,77 @@
 
         
-          describe '#relative_distance_of_time_in_words' do
-    it 'in the past' do
-      expect(relative_distance_of_time_in_words(Time.now-5.minutes)).to eq('5m ago')
-    end
-    
-        it 'defauls foreground and background colors' do
-      scenario.tag_fg_color = nil
-      scenario.tag_bg_color = nil
-      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
-    end
-  end
-    
-          expect(@scheduler.scheduler_agent_jobs.map(&:scheduler_agent)).to eq([@agent2])
-    end
-    
-      describe '#check' do
-    it 'should check that initial run creates an event' do
-      @checker.memory[:last_updated_at] = '2016-03-15T14:01:05+00:00'
-      expect { @checker.check }.to change { Event.count }.by(1)
-    end
-  end
-end
-
-    
-      # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
-  end
-    
-      # GET /resource/unlock/new
-  def new
-    self.resource = resource_class.new
-  end
-    
-            routes.each do |module_name, actions|
-          [:path, :url].each do |path_or_url|
-            actions.each do |action|
-              action = action ? '#{action}_' : ''
-              method = :'#{action}#{module_name}_#{path_or_url}'
-    
-          def initialize_from_record(record)
-        @scope_name = Devise::Mapping.find_scope!(record)
-        @resource   = instance_variable_set('@#{devise_mapping.name}', record)
-      end
-    
-        alias :name :singular
-    
-          module ClassMethods
-        # Create the cookie key using the record id and remember_token
-        def serialize_into_cookie(record)
-          [record.to_key, record.rememberable_value, Time.now.utc.to_f.to_s]
+                    c.action do |args, opts|
+              Jekyll::Commands::NewTheme.process(args, opts)
+            end
+          end
         end
     
-          private
+        def arg_is_present?(args, deprecated_argument, message)
+      deprecation_message(message) if args.include?(deprecated_argument)
+    end
     
+      def verify_payload?
+    payload.present? && VerifySalmonService.new.call(payload)
+  end
     
+      private
     
-                  <ol start='<%= frame.lineno %>' class='context' id='<%= id %>'
-                  onclick='toggle(<%= id %>);'>
-                <li class='context-line' id='context-<%= id %>'><code><%=
-                  h frame.context_line %></code></li>
-              </ol>
+      def set_locale
+    I18n.locale = default_locale
+    I18n.locale = current_user.locale if user_signed_in?
+  rescue I18n::InvalidLocale
+    I18n.locale = default_locale
+  end
     
-    namespace :doc do
-  task :readmes do
-    Dir.glob 'lib/rack/protection/*.rb' do |file|
-      excluded_files = %w[lib/rack/protection/base.rb lib/rack/protection/version.rb]
-      next if excluded_files.include?(file)
-      doc  = File.read(file)[/^  module Protection(\n)+(    #[^\n]*\n)*/m].scan(/^ *#(?!#) ?(.*)\n/).join('\n')
-      file = 'doc/#{file[4..-4].tr('/_', '-')}.rdoc'
-      Dir.mkdir 'doc' unless File.directory? 'doc'
-      puts 'writing #{file}'
-      File.open(file, 'w') { |f| f << doc }
+      # if rss_url already in existing opml file, use that; otherwise, do a lookup
+  rss_url = nil
+  if File.exist?(OUTPUT_FILENAME)
+    xml = Nokogiri::XML(File.open(OUTPUT_FILENAME))
+    existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first
+    if existing_blog
+      rss_url = existing_blog.attr('xmlUrl')
+      puts '#{name}: ALREADY HAVE'
     end
   end
     
-          def warn(env, message)
-        return unless options[:logging]
-        l = options[:logger] || env['rack.logger'] || ::Logger.new(env['rack.errors'])
-        l.warn(message)
+      def test_image_helper
+    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
+  end
+    
+        it 'generates a jasmine fixture', :fixture => true do
+      get :bookmarklet
+      save_fixture(html_for('body'), 'bookmarklet')
+    end
+    
+          it_behaves_like 'on a visible post'
+    end
+    
+        context 'on a post you can't see' do
+      before do
+        sign_in(eve, scope: :user)
       end
     
-            self
-      end
+        streams = {
+      liked:         Stream::Likes,
+      mentioned:     Stream::Mention,
+      followed_tags: Stream::FollowedTag,
+      activity:      Stream::Activity
+    }
     
-            def ignore_empty_blocks?
-          cop_config['IgnoreEmptyBlocks']
-        end
+      desc 'Install all spree gems'
+  task install: :build do
+    for_each_gem do |gem_path|
+      Bundler.with_clean_env do
+        sh 'gem install #{gem_path}'
       end
     end
   end
-end
-
     
-      def to_s(format=nil)
-    return super('NAME_FULLVERSION_ARCH.TYPE') if format.nil?
-    return super(format)
-  end
-    
-          prefix = ''
-      if name.bytesize > 100 then
-        parts = name.split('/', -1) # parts are never empty here
-        name = parts.pop            # initially empty for names with a trailing slash ('foo/.../bar/')
-        prefix = parts.join('/')    # if empty, then it's impossible to split (parts is empty too)
-        while !parts.empty? && (prefix.bytesize > 155 || name.empty?)
-          name = parts.pop + '/' + name
-          prefix = parts.join('/')
-        end
+          @@order_attributes = [
+        :id, :number, :item_total, :total, :ship_total, :state, :adjustment_total,
+        :user_id, :created_at, :updated_at, :completed_at, :payment_total,
+        :shipment_state, :payment_state, :email, :special_instructions, :channel,
+        :included_tax_total, :additional_tax_total, :display_included_tax_total,
+        :display_additional_tax_total, :tax_total, :currency, :considered_risky,
+        :canceler_id
+      ]
