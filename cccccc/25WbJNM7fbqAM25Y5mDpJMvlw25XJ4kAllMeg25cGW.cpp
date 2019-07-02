7dea321c22d74de3efa3ba4f69b2d2ce809d918e
SUNsung <weatherius@gmail.com>
2019-07-02 14:52:13 +0300

    Rewrite [25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp] File in: 2019-07-02 14:52:13

--- cccccc/25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp
+++ cccccc/25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp
@@ -1,204 +1,155 @@
 
         
-          struct IndentScope {
-    TreePrinter *Printer;
-    size_t OldLength;
-    IndentScope(TreePrinter *printer, StringRef indent)
-        : Printer(printer), OldLength(printer->Indent.size()) {
-      Printer->Indent += indent;
-    }
-    ~IndentScope() { Printer->Indent.resize(OldLength); }
-  };
+        		// Shifting the edge inward by b2_toiSlop should
+		// not cause the plane to pass the centroid.
+		if ((d.x < 0.0f)||(d.y < 0.0f)){
+			noError = false;
+			error = 6;
+		}
     
-        StringRef Line = RawText.substr(0, Pos);
-    Lines.push_back(Line);
-    if (!IsFirstLine) {
-      size_t NonWhitespacePos = RawText.find_first_not_of(' ');
-      if (NonWhitespacePos != StringRef::npos)
-        WhitespaceToTrim =
-            std::min(WhitespaceToTrim,
-                     static_cast<unsigned>(NonWhitespacePos));
-    }
-    IsFirstLine = false;
+      Compression level can be specified in parameter level. At the moment,
+  only level 1 and level 2 are supported.
+  Level 1 is the fastest compression and generally useful for short data.
+  Level 2 is slightly slower but it gives better compression ratio.
     
-    
-    {
-    {    void emitNote(clang::FullSourceLoc Loc, StringRef Message) override {
-      // We get invalid note locations when trying to describe where a module
-      // is imported and the actual location is in Swift. We also want to ignore
-      // things like 'in module X imported from <swift-imported-modules>'.
-      if (Loc.isInvalid() || isInSwiftBuffers(Loc))
-        return;
-      emitDiagnosticMessage(Loc, {}, clang::DiagnosticsEngine::Note, Message,
-                            {}, {});
-    }
-  };
-} // end anonymous namespace
-    
-      bool isInstanceMember() const {
-    return isImportAsMember() && selfIndex.hasValue();
-  }
-    
-    std::pair<FrontendInputsAndOutputs, std::set<StringRef>>
-ArgsToFrontendInputsConverter::createInputFilesConsumingPrimaries(
-    std::set<StringRef> primaryFiles) {
-  bool hasAnyPrimaryFiles = !primaryFiles.empty();
-    }
-    
-    #include 'swift/AST/DiagnosticConsumer.h'
-#include 'swift/AST/DiagnosticEngine.h'
-#include 'swift/Frontend/FrontendOptions.h'
-#include 'llvm/ADT/SetVector.h'
-#include 'llvm/Option/ArgList.h'
-#include <set>
-    
-    #ifndef STORAGE_LEVELDB_DB_LOG_FORMAT_H_
-#define STORAGE_LEVELDB_DB_LOG_FORMAT_H_
-    
-      // Read until size drops significantly.
-  std::string limit_key = Key(n);
-  for (int read = 0; true; read++) {
-    ASSERT_LT(read, 100) << 'Taking too long to compact';
-    Iterator* iter = db_->NewIterator(ReadOptions());
-    for (iter->SeekToFirst();
-         iter->Valid() && iter->key().ToString() < limit_key; iter->Next()) {
-      // Drop data
-    }
-    delete iter;
-    // Wait a little bit to allow any triggered compactions to complete.
-    Env::Default()->SleepForMicroseconds(1000000);
-    uint64_t size = Size(Key(0), Key(n));
-    fprintf(stderr, 'iter %3d => %7.3f MB [other %7.3f MB]\n', read + 1,
-            size / 1048576.0, Size(Key(n), Key(kCount)) / 1048576.0);
-    if (size <= initial_size / 10) {
-      break;
-    }
-  }
-    
-    TEST(CorruptionTest, TableFileIndexData) {
-  Build(10000);  // Enough to build multiple Tables
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
-  dbi->TEST_CompactMemTable();
-    }
-    
-      ~Writer();
-    
-    class Env;
-    
-      // Check that the file exists.
-  ASSERT_TRUE(env_->FileExists('/dir/f'));
-  ASSERT_OK(env_->GetFileSize('/dir/f', &file_size));
-  ASSERT_EQ(0, file_size);
-  ASSERT_OK(env_->GetChildren('/dir', &children));
-  ASSERT_EQ(1, children.size());
-  ASSERT_EQ('f', children[0]);
-    
-      // Clear all updates buffered in this batch.
-  void Clear();
-    
-    #include <stddef.h>
-#include <stdint.h>
-    
-    #ifndef STORAGE_LEVELDB_TABLE_FILTER_BLOCK_H_
-#define STORAGE_LEVELDB_TABLE_FILTER_BLOCK_H_
-    
-    void TableBuilder::Flush() {
-  Rep* r = rep_;
-  assert(!r->closed);
-  if (!ok()) return;
-  if (r->data_block.empty()) return;
-  assert(!r->pending_index_entry);
-  WriteBlock(&r->data_block, &r->pending_handle);
-  if (ok()) {
-    r->pending_index_entry = true;
-    r->status = r->file->Flush();
-  }
-  if (r->filter_block != nullptr) {
-    r->filter_block->StartBlock(r->offset);
-  }
-}
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    
-    {
-    {        s1 %= ADLER_MOD, s2 %= ADLER_MOD;
-        buflen -= blocklen;
-        blocklen = 5552;
-    }
-    return (s2 << 16) + s1;
-}
-    
-    // Helper functions
-    
-    
-    {        ImGui::Render();
-    }
-    
-    int main(int, char**)
-{
-    // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
-    {
-        printf('Error: %s\n', SDL_GetError());
-        return -1;
-    }
-    }
-    
-    // All the ImGui_ImplVulkanH_XXX structures/functions are optional helpers used by the demo. 
-// Your real engine/app may not use them.
-static void SetupVulkanWindow(ImGui_ImplVulkanH_Window* wd, VkSurfaceKHR surface, int width, int height)
-{
-    wd->Surface = surface;
-    }
-    
-    bool ImGui_ImplSDL2_InitForVulkan(SDL_Window* window)
-{
-#if !SDL_HAS_VULKAN
-    IM_ASSERT(0 && 'Unsupported');
+    #ifdef FIXED_POINT
+/*#  define KISS_FFT_COS(phase)  TRIG_UPSCALE*floor(MIN(32767,MAX(-32767,.5+32768 * cos (phase))))
+#  define KISS_FFT_SIN(phase)  TRIG_UPSCALE*floor(MIN(32767,MAX(-32767,.5+32768 * sin (phase))))*/
+#  define KISS_FFT_COS(phase)  floor(.5+TWID_MAX*cos (phase))
+#  define KISS_FFT_SIN(phase)  floor(.5+TWID_MAX*sin (phase))
+#  define HALF_OF(x) ((x)>>1)
+#elif defined(USE_SIMD)
+#  define KISS_FFT_COS(phase) _mm_set1_ps( cos(phase) )
+#  define KISS_FFT_SIN(phase) _mm_set1_ps( sin(phase) )
+#  define HALF_OF(x) ((x)*_mm_set1_ps(.5f))
+#else
+#  define KISS_FFT_COS(phase) (kiss_fft_scalar) cos(phase)
+#  define KISS_FFT_SIN(phase) (kiss_fft_scalar) sin(phase)
+#  define HALF_OF(x) ((x)*.5f)
 #endif
-    return ImGui_ImplSDL2_Init(window);
+    
+    #define SHR16(a, shift) SHR16_(a, shift, __FILE__, __LINE__)
+static OPUS_INLINE short SHR16_(int a, int shift, char *file, int line)
+{
+   int res;
+   if (!VERIFY_SHORT(a) || !VERIFY_SHORT(shift))
+   {
+      fprintf (stderr, 'SHR16: inputs are not short: %d >> %d in %s: line %d\n', a, shift, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = a>>shift;
+   if (!VERIFY_SHORT(res))
+   {
+      fprintf (stderr, 'SHR16: output is not short: %d in %s: line %d\n', res, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips++;
+   return res;
+}
+#define SHL16(a, shift) SHL16_(a, shift, __FILE__, __LINE__)
+static OPUS_INLINE short SHL16_(int a, int shift, char *file, int line)
+{
+   int res;
+   if (!VERIFY_SHORT(a) || !VERIFY_SHORT(shift))
+   {
+      fprintf (stderr, 'SHL16: inputs are not short: %d %d in %s: line %d\n', a, shift, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = a<<shift;
+   if (!VERIFY_SHORT(res))
+   {
+      fprintf (stderr, 'SHL16: output is not short: %d in %s: line %d\n', res, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips++;
+   return res;
 }
     
-    #include <vector>
     
-    TEST(ProtocolDataTest, CheckSum) {
-  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
-  const uint8_t result =
-      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
-  EXPECT_EQ(0xE7, result);
+    {    // exception out_of_range.401
+    try
+    {
+        // try to write beyond the array limit
+        array.at(5) = 'sixth';
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
 }
+
     
-    TEST(ByteTest, SetGetHighLowBit) {
-  unsigned char byte_value = 0x37;
-  Byte value(&byte_value);
-  value.set_value_high_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
-  value.set_value_low_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
-}
+    #include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
     
-    int ClusterGeneralInfo701::dynprop(const std::uint8_t* bytes,
-                                   int32_t length) const {
-  Byte t0(bytes + 6);
-  uint32_t x = t0.get_byte(0, 3);
-  int ret = x;
+    
+    {  int ret = x;
   return ret;
 }
     
+    BaseMapMatrix::BaseMapMatrix() {}
     
-    {  x <<= 2;
-  x |= t;
-  double ret = x * OBJECT_VREL_RES + OBJECT_VREL_LONG_MIN;
-  return ret;
+        auto it_lower = std::lower_bound(
+        speed_limit_.speed_limit_points().begin(),
+        speed_limit_.speed_limit_points().end(), s,
+        [](const std::pair<double, double>& point, const double curr_s) {
+          return point.first < curr_s;
+        });
+    
+      MatrixXd mat_golden(20, 10);
+  // clang-format off
+  mat_golden <<
+   -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0, -1,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  1,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0, -1,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  1,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0, -1,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  1,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0, -1,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  1,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0, -1,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  1,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0, -1,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  1,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0, -1,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  1,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0, -1,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  1,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0, -1,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  1;
+  // clang-format on
+  EXPECT_EQ(mat, mat_golden);
+    
+    #include 'modules/planning/math/smoothing_spline/spline_1d_seg.h'
+    
+    GemMessageManager::GemMessageManager() {
+  // Control Messages
+  AddSendProtocolData<Accelcmd67, true>();
+  AddSendProtocolData<Brakecmd6b, true>();
+  AddSendProtocolData<Globalcmd69, true>();
+  AddSendProtocolData<Headlightcmd76, true>();
+  AddSendProtocolData<Horncmd78, true>();
+  AddSendProtocolData<Shiftcmd65, true>();
+  AddSendProtocolData<Steeringcmd6d, true>();
+  AddSendProtocolData<Turncmd63, true>();
+  AddSendProtocolData<Wipercmd90, true>();
+    }
+    
+    void Brakemotorrpt170::Parse(const std::uint8_t* bytes, int32_t length,
+                             ChassisDetail* chassis) const {
+  chassis->mutable_gem()->mutable_brake_motor_rpt_1_70()->set_motor_current(
+      motor_current(bytes, length));
+  chassis->mutable_gem()->mutable_brake_motor_rpt_1_70()->set_shaft_position(
+      shaft_position(bytes, length));
 }
     
     
@@ -206,12 +157,24 @@ static void SetupVulkanWindow(ImGui_ImplVulkanH_Window* wd, VkSurfaceKHR surface
   return ret;
 }
     
+    #include 'gtest/gtest.h'
     
-    {
-    {
-    {}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
-
+    // config detail: {'name': 'override_status', 'enum': {0:
+// 'OVERRIDE_STATUS_NOT_OVERRIDDEN', 1: 'OVERRIDE_STATUS_OVERRIDDEN'},
+// 'precision': 1.0, 'len': 1, 'is_signed_var': False, 'offset': 0.0,
+// 'physical_range': '[0|1]', 'bit': 1, 'type': 'enum', 'order': 'motorola',
+// 'physical_unit': ''}
+Global_rpt_6a::Override_statusType Globalrpt6a::override_status(
+    const std::uint8_t* bytes, int32_t length) const {
+  Byte t0(bytes + 0);
+  int32_t x = t0.get_byte(1, 1);
+    }
     
-    #include 'glog/logging.h'
+    // config detail: {'name': 'latitude_seconds', 'offset': 0.0, 'precision': 1.0,
+// 'len': 8, 'is_signed_var': True, 'physical_range': '[-128|127]', 'bit': 23,
+// 'type': 'int', 'order': 'motorola', 'physical_unit': 'sec'}
+int Latlonheadingrpt82::latitude_seconds(const std::uint8_t* bytes,
+                                         int32_t length) const {
+  Byte t0(bytes + 2);
+  int32_t x = t0.get_byte(0, 8);
+    }
