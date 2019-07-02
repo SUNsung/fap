cc8372fa14d7dd772e61124437b9cc665d2458bf
SUNsung <weatherius@gmail.com>
2019-07-02 17:30:56 +0300

    Rewrite [30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py] File in: 2019-07-02 17:30:56

--- pyton/30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py
+++ pyton/30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py
@@ -1,84 +1,110 @@
 
         
-        
-class Session(AbstractBaseSession):
+        batch_size = 128
+num_classes = 10
+epochs = 12
+log_dir = './logs'
+    
+        with gzip.open(paths[0], 'rb') as lbpath:
+        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+    
+    from keras.models import Sequential, Model
+from keras.layers import Dense, Input, Average
+from keras.utils import np_utils
+from keras.utils import test_utils
+from keras import regularizers
+from keras import backend as K
+    
+        ```python
+    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
+    > labels
+    array([0, 2, 1, 2, 0])
+    # `to_categorical` converts this into a matrix with as many
+    # columns as there are classes. The number of rows
+    # stays the same.
+    > to_categorical(labels)
+    array([[ 1.,  0.,  0.],
+           [ 0.,  0.,  1.],
+           [ 0.,  1.,  0.],
+           [ 0.,  0.,  1.],
+           [ 1.,  0.,  0.]], dtype=float32)
+    ```
     '''
-    Django provides full support for anonymous sessions. The session
-    framework lets you store and retrieve arbitrary data on a
-    per-site-visitor basis. It stores data on the server side and
-    abstracts the sending and receiving of cookies. Cookies contain a
-    session ID -- not the data itself.
     
-        :param client: AWS API client reference (boto3)
-    :param module: Ansible module reference
-    :return dict:
-    '''
+    arxiv:1504.00941v2 [cs.NE] 7 Apr 2015
+http://arxiv.org/pdf/1504.00941v2.pdf
     
-            :returns: `set` of unsaved files
-        '''
-        save_state = self.aug.get('/augeas/save')
-        self.aug.set('/augeas/save', 'noop')
-        # Existing Errors
-        ex_errs = self.aug.match('/augeas//error')
-        try:
-            # This is a noop save
-            self.aug.save()
-        except (RuntimeError, IOError):
-            self._log_save_errors(ex_errs)
-            # Erase Save Notes
-            self.save_notes = ''
-            raise errors.PluginError(
-                'Error saving files, check logs for more info.')
-    
-        @certbot_util.patch_get_utility()
-    def test_noninteractive(self, mock_util):
-        mock_util().menu.side_effect = errors.MissingCommandlineFlag('no vhost default')
-        try:
-            self._call(self.vhosts)
-        except errors.MissingCommandlineFlag as e:
-            self.assertTrue('vhost ambiguity' in str(e))
-    
-            self.assertTrue(self.addr_default.conflicts(self.addr))
-        self.assertTrue(self.addr_default.conflicts(self.addr1))
-        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
+    print('Loading data...')
+(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
+                                                         test_split=0.2)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
     
     
-def setup(app):
-    app.add_config_value('edit_on_github_project', '', True)
-    app.add_config_value('edit_on_github_branch', 'master', True)
-    app.add_config_value('edit_on_github_src_path', '', True)  # 'eg' 'docs/'
-    app.connect('html-page-context', html_page_context)
+class HTTPMessage(object):
+    '''Abstract class for HTTP messages.'''
+    
+    import requests.auth
+    
+    
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
+    
+        plugin_manager.register(Plugin)
+    try:
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+            '--auth',
+            BASIC_AUTH_HEADER_VALUE,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
+    
+    
+def test_default_options_overwrite(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['json'] == {'foo': 'bar'}
+    
+    
+@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
+@pytest.mark.parametrize('filename', filenames)
+def test_rst_file_syntax(filename):
+    p = subprocess.Popen(
+        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    err = p.communicate()[1]
+    assert p.returncode == 0, err.decode('utf8')
 
     
-        return result
+    from httpie import __version__
+from httpie.compat import is_windows
     
-    
-class GroupTagKeyNotFound(Exception):
-    pass
-    
-            # Removing unique constraint on 'GroupTagValue', fields ['project_id',
-        # 'group_id', 'environment_id', '_key', '_value']
-        db.delete_unique(
-            u'tagstore_grouptagvalue', [
-                'project_id', 'group_id', 'environment_id', 'key', 'value'])
-    
-            # Adding model 'GroupTagValue'
-        db.create_table(u'tagstore_grouptagvalue', (
-            ('_value', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                to=orm['tagstore.TagValue'], db_column='value')),
-            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
-            ('environment_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)),
-            ('times_seen', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
-            ('_key', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                to=orm['tagstore.TagKey'], db_column='key')),
-            ('first_seen', self.gf('django.db.models.fields.DateTimeField')(
-                null=True, db_index=True)),
-            ('group_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
-            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
-            ('last_seen', self.gf('django.db.models.fields.DateTimeField')(
-                null=True, db_index=True)),
-        ))
-        db.send_create_signal('tagstore', ['GroupTagValue'])
-    
-    from datetime import timedelta
-from django.utils import timezone
+            credentials = {'username': None, 'password': None}
+        try:
+            # New style
+            plugin.raw_auth = auth['raw_auth']
+        except KeyError:
+            # Old style
+            credentials = {
+                'username': auth['username'],
+                'password': auth['password'],
+            }
+        else:
+            if plugin.auth_parse:
+                from httpie.input import parse_auth
+                parsed = parse_auth(plugin.raw_auth)
+                credentials = {
+                    'username': parsed.key,
+                    'password': parsed.value,
+                }
