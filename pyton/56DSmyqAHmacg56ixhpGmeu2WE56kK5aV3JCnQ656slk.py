ba5d9c8471cdc777ff1bade1db15df36f2a23d20
SUNsung <weatherius@gmail.com>
2019-07-02 22:55:41 +0300

    Rewrite [56DSmyqAHmacg56ixhpGmeu2WE56kK5aV3JCnQ656slk.py] File in: 2019-07-02 22:55:41

--- pyton/56DSmyqAHmacg56ixhpGmeu2WE56kK5aV3JCnQ656slk.py
+++ pyton/56DSmyqAHmacg56ixhpGmeu2WE56kK5aV3JCnQ656slk.py
@@ -1,231 +1,56 @@
 
         
-            def login(self, username='test', password='test'):
-        return self._client.post(
-            '/auth/login', data={'username': username, 'password': password}
-        )
+            def __delitem__(self, key):
+        del self._store[key.lower()]
     
-        try:
-        try:
-            with _Mgr():
-                raise AssertionError()
-        except:
-            raise
-    except TypeError:
-        BROKEN_PYPY_CTXMGR_EXIT = True
-    except AssertionError:
-        pass
+    # Syntax sugar.
+_ver = sys.version_info
     
-            try:
-            with open(filename) as json_file:
-                obj = json.loads(json_file.read())
-        except IOError as e:
-            if silent and e.errno in (errno.ENOENT, errno.EISDIR):
-                return False
-            e.strerror = 'Unable to load configuration file (%s)' % e.strerror
-            raise
-        return self.from_mapping(obj)
+    usage:
     
-        tag = to_json
-    
-            if '.' not in rv:
-            # Chrome doesn't allow names without a '.'
-            # this should only come up with localhost
-            # hack around this by not setting the name, and show a warning
-            warnings.warn(
-                ''{rv}' is not a valid cookie domain, it must contain a '.'.'
-                ' Add an entry to your hosts file, for example'
-                ' '{rv}.localdomain', and use that instead.'.format(rv=rv)
-            )
-            app.config['SESSION_COOKIE_DOMAIN'] = False
-            return None
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-l', '--list', dest='list', action='store_true',
+            help='List available templates')
+        parser.add_option('-e', '--edit', dest='edit', action='store_true',
+            help='Edit spider after creating it')
+        parser.add_option('-d', '--dump', dest='dump', metavar='TEMPLATE',
+            help='Dump template to standard output')
+        parser.add_option('-t', '--template', dest='template', default='basic',
+            help='Uses a custom template.')
+        parser.add_option('--force', dest='force', action='store_true',
+            help='If the spider already exists, overwrite it with the template')
     
     
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
-
-    
-        print('Generating e that is relatively prime to (p - 1) * (q - 1)...')
-    while True:
-        e = random.randrange(2 ** (keySize - 1), 2 ** (keySize))
-        if cryptoMath.gcd(e, (p - 1) * (q - 1)) == 1:
-            break
-    
-        def hash_function(self, key):
-        return key % self.size_table
-    
-    This is a pure Python implementation of Dynamic Programming solution to the longest_sub_array problem.
-    
-        for i in range(1, n+1):
-        for j in range(1, s+1):
-            dp[i][j]= dp[i][j-1]
-    
-    from matplotlib import pyplot as plt
-def plot_heterogeneity(heterogeneity, k):
-    plt.figure(figsize=(7,4))
-    plt.plot(heterogeneity, linewidth=4)
-    plt.xlabel('# Iterations')
-    plt.ylabel('Heterogeneity')
-    plt.title('Heterogeneity of clustering over time, K={0:d}'.format(k))
-    plt.rcParams.update({'font.size': 16})
-    plt.show()
-    
-    min_length = 8
-max_length = 16
-password = ''.join(random.choice(chars) for x in range(random.randint(min_length, max_length)))
-print('Password: ' + password)
-print('[ If you are thinking of using this passsword, You better save it. ]')
-    
-        get_item_parameters = pytest.mark.parametrize(
-        'key, value', (
-            ('bad_gateway', 502),
-            ('not_a_key', None)
-        )
-    )
-    
-            Keyword:                   'bold #004461',   # class: 'k'
-        Keyword.Constant:          'bold #004461',   # class: 'kc'
-        Keyword.Declaration:       'bold #004461',   # class: 'kd'
-        Keyword.Namespace:         'bold #004461',   # class: 'kn'
-        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
-        Keyword.Reserved:          'bold #004461',   # class: 'kr'
-        Keyword.Type:              'bold #004461',   # class: 'kt'
-    
-    if is_py2:
-    from urllib import (
-        quote, unquote, quote_plus, unquote_plus, urlencode, getproxies,
-        proxy_bypass, proxy_bypass_environment, getproxies_environment)
-    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urldefrag
-    from urllib2 import parse_http_list
-    import cookielib
-    from Cookie import Morsel
-    from StringIO import StringIO
-    from collections import Callable, Mapping, MutableMapping, OrderedDict
-    
-    from .structures import LookupDict
-    
-    EXAMPLES = '''
-- cloudwatchevent_rule:
-    name: MyCronTask
-    schedule_expression: 'cron(0 20 * * ? *)'
-    description: Run my scheduled task
-    targets:
-      - id: MyTargetId
-        arn: arn:aws:lambda:us-east-1:123456789012:function:MyFunction
-    
-    
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-    
-            if usergroup is not None:
-            changed = client.modify_if_diff(name, ipa_hbacrule.get('memberuser_group', []), usergroup,
-                                            client.hbacrule_add_user,
-                                            client.hbacrule_remove_user, 'group') or changed
-    else:
-        if ipa_hbacrule:
-            changed = True
-            if not module.check_mode:
-                client.hbacrule_del(name=name)
-    
-    
-def global_function():
-    def inner_function():
-        class LocalClass:
-            pass
-        global inner_global_function
-        def inner_global_function():
-            def inner_function2():
-                pass
-            return inner_function2
-        return LocalClass
-    return lambda: inner_function
-    
-    for excname in PYTHON3_IMPORTERROR_EXCEPTIONS:
-    REVERSE_NAME_MAPPING[('builtins', excname)] = ('exceptions', 'ImportError')
-
-    
-    
-class Dummy_about_dialog():
-    # Dummy class for testing file display functions.
-    idle_credits = About.show_idle_credits
-    idle_readme = About.show_readme
-    idle_news = About.show_idle_news
-    # Called by the above
-    display_file_text = About.display_file_text
-    _utest = True
-    
-        f1 = manager.Foo1()
-    f1.f()
-    f1.g()
-    assert not hasattr(f1, '_h')
-    assert sorted(f1._exposed_) == sorted(['f', 'g'])
-    
-        if not app.config.edit_on_github_project:
-        warnings.warn('edit_on_github_project not specified')
-        return
-    if not doctree:
-        warnings.warn('doctree is None')
-        return
-    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
-    show_url = get_github_url(app, 'blob', path)
-    edit_url = get_github_url(app, 'edit', path)
-    
-    # Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'Home-Assistant', 'Home Assistant Documentation',
-     author, 'Home Assistant', 'Open-source home automation platform.',
-     'Miscellaneous'),
-]
-    
-        if unit_1 == unit_2 or unit_1 not in VALID_UNITS:
-        return value
-    
-    
-    
-        in_channels = get_shape(x)[-1]
-    kernel_shape = list(kernel_size) + [in_channels, out_channels]  # [kernel_h, kernel_w, in_channels, out_channels]
-    
-    References:
-    tensorlayer.layers.DenseLayer
-    keras.layers.Dense
-'''
-import tensorflow as tf
-    
-        def __init__(self, l1=0., l2=0.):
+    @implementer(IPolicyForHTTPS)
+    class BrowserLikeContextFactory(ScrapyClientContextFactory):
         '''
-        Args:
-            l1(float): L1 正则化的系数
-            l2(float): L2 正则化的系数
-        '''
-        self.l1 = np.asarray(l1, dtype=np.float32)
-        self.l2 = np.asarray(l2, dtype=np.float32)
+        Twisted-recommended context factory for web clients.
     
-    # 如果一个 ngram 都没找到，gensim 会报错
-#   其实可以返回一个 0 向量的，它内部实际上是从一个 0 向量开始累加的；
-#   但返回时做了一个判断——如果依然是 0 向量，则报错
-# print(model.wv['z'])
-r'''
-Traceback (most recent call last):
-  File 'D:/OneDrive/workspace/github/DL-Notes-for-Interview/code/工具库/gensim/FastText.py', line 53, in <module>
-    print(model.wv['z'])
-  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 336, in __getitem__
-    return self.get_vector(entities)
-  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 454, in get_vector
-    return self.word_vec(word)
-  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 1989, in word_vec
-    raise KeyError('all ngrams for word %s absent from model' % word)
-KeyError: 'all ngrams for word z absent from model'
-'''
+        def __init__(self, headers):
+        self._headers = headers
+    
+        long_description = README,
+    
+        if '_text' in dictified['flashvars'][0]:
+        video_dict['flashvars'] = dictified['flashvars'][0]['_text'].strip()
+    
+        html = get_content(rebuilt_url(url))
+    info = json.loads(match1(html, r'qualities':({.+?}),''))
+    title = match1(html, r''video_title'\s*:\s*'([^']+)'') or \
+            match1(html, r''title'\s*:\s*'([^']+)'')
+    title = unicodize(title)
+    
+    >>> class Subclass(custom4.Custom): pass
+...
+>>> s = Subclass()
+>>> s.cycle = [s]
+>>> s.cycle.append(s.cycle)
+>>> x = object()
+>>> s.x = x
+>>> del s
+>>> sys.getrefcount(x)
+3
+>>> ignore = gc.collect()
+>>> sys.getrefcount(x)
+2
