f28f52d0c5c2d7b36a3f39f90d6baad788f640f4
SUNsung <weatherius@gmail.com>
2019-07-02 23:19:20 +0300

    Rewrite [35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py] File in: 2019-07-02 23:19:20

--- pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
+++ pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
@@ -1,107 +1,125 @@
 
         
-        
-@pytest.mark.functional
-def test_with_confirmation(proc, TIMEOUT):
-    with_confirmation(proc, TIMEOUT)
+        x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+    
+        # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    '''
+    dirname = 'cifar-10-batches-py'
+    origin = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+    path = get_file(dirname, origin=origin, untar=True)
+    
+        # Arguments
+        label_mode: one of 'fine', 'coarse'.
     
     
-@pytest.mark.parametrize('command, new_command, packages', [
-    (Command('vim', ''), 'sudo apt-get install vim && vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('convert', ''), 'sudo apt-get install imagemagick && convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')]),
-    (Command('sudo vim', ''), 'sudo apt-get install vim && sudo vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('sudo convert', ''), 'sudo apt-get install imagemagick && sudo convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')])])
-def test_get_new_command(mocker, command, new_command, packages):
-    mocker.patch('thefuck.rules.apt_get._get_packages',
-                 create=True, return_value=packages)
+def test_regularization_shared_model():
+    dense_layer = Dense(num_classes,
+                        kernel_regularizer=regularizers.l1(),
+                        activity_regularizer=regularizers.l1())
     
-    match_output = '''
-Listing... Done
-heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
-resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
-squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
-unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
-'''
+    if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
     
-    import six
-from . import backend as K
-from .utils.generic_utils import serialize_keras_object
-from .utils.generic_utils import deserialize_keras_object
-    
-        # Test single image
-    x = np.random.uniform(0, 255, (10, 10, 3))
-    xint = x.astype('int32')
-    assert utils.preprocess_input(x).shape == x.shape
-    assert utils.preprocess_input(xint).shape == xint.shape
+    To use this with Keras, we make a dataset out of elements
+of the form (input batch, output batch). From there, we
+create a one-shot iterator and a graph node corresponding
+to its get_next() method. Its components are then provided
+to the network's Input layer and the Model.compile() method,
+respectively.
     
     import keras
-from keras import backend as K
-from keras import layers
 from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
     
-        def test_list(self):
-        assert list(self.case_insensitive_dict) == ['Accept']
+    history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size, verbose=1)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
     
-            :param url: The URL to connect to.
-        :param proxies: (optional) A Requests-style dictionary of proxies used on this request.
-        :rtype: urllib3.ConnectionPool
-        '''
-        proxy = select_proxy(url, proxies)
+    # If true, show page references after internal links.
+#
+# latex_show_pagerefs = False
     
-    try:
-    import simplejson as json
-except ImportError:
-    import json
     
-        if isinstance(cookies, dict):
-        cookiejar = cookiejar_from_dict(
-            cookies, cookiejar=cookiejar, overwrite=False)
-    elif isinstance(cookies, cookielib.CookieJar):
-        try:
-            cookiejar.update(cookies)
-        except AttributeError:
-            for cookie_in_jar in cookies:
-                cookiejar.set_cookie(cookie_in_jar)
+def __meters_to_kilometers(meters: float) -> float:
+    '''Convert meters to kilometers.'''
+    return meters * 0.001
+
     
-    ``response``:
-    The response generated from a Request.
-'''
-HOOKS = ['response']
+        elif bump_type == 'patch':
+        # Convert 0.67.3 to 0.67.4
+        # Convert 0.67.3.b5 to 0.67.3
+        # Convert 0.67.3.dev0 to 0.67.3
+        to_change['dev'] = None
+        to_change['pre'] = None
     
-    # A unique identification for the text.
-# epub_uid = ''
+    # Repeat the entire benchmark this many times (on different ports)
+# This gives JITs time to warm up, etc.  Pypy needs 3-5 runs at
+# --n=15000 for its JIT to reach full effectiveness
+define('num_runs', type=int, default=1)
     
-        author = proj_info['author'],
-    author_email = proj_info['author_email'],
-    url = proj_info['url'],
-    license = proj_info['license'],
+    from tornado import gen
+from tornado.options import options, define, parse_command_line
     
-            if not title:
-            self.title = vid
+        def start_tree(self, tree, filename):
+        self.found_future_import = False
     
-    __all__ = ['cbs_download']
+    This module will not work correctly when `.HTTPServer`'s multi-process
+mode is used.
     
-    __all__ = ['ckplayer_download']
+    import os
     
-        return result
+        def test_twitter_show_user(self):
+        response = self.fetch('/twitter/client/show_user?name=somebody')
+        response.rethrow()
+        self.assertEqual(
+            json_decode(response.body), {'name': 'Somebody', 'screen_name': 'somebody'}
+        )
     
-        Args:
-        x(tf.Tensor):
-        kernel_size(int or list of int):
-        out_channels(int):
-        act_fn(function):
-        strides(int or list of int):
-        padding(str):
-        name(str):
-        reuse(bool):
+        total_loss = masked_lm_loss + next_sentence_loss
     
-    if word_vec.any():  #
-    word_vec = word_vec / max(1, ngrams_found)
-else:  # 如果一个 ngram 都没找到，gensim 会报错；个人认为把 0 向量传出来也可以
-    raise KeyError('all ngrams for word %s absent from model' % word_unk)
+      # When we created the data, we kept track of the alignment between original
+  # (whitespace tokenized) tokens and our WordPiece tokenized tokens. So
+  # now `orig_text` contains the span of our original text corresponding to the
+  # span that we predicted.
+  #
+  # However, `orig_text` may contain extra characters that we don't want in
+  # our prediction.
+  #
+  # For example, let's say:
+  #   pred_text = steve smith
+  #   orig_text = Steve Smith's
+  #
+  # We don't want to return `orig_text` because it contains the extra ''s'.
+  #
+  # We don't want to return `pred_text` because it's already been normalized
+  # (the SQuAD eval script also does punctuation stripping/lower casing but
+  # our tokenizer does additional normalization like stripping accent
+  # characters).
+  #
+  # What we really want to return is 'Steve Smith'.
+  #
+  # Therefore, we have to apply a semi-complicated alignment heruistic between
+  # `pred_text` and `orig_text` to get a character-to-charcter alignment. This
+  # can fail in certain cases in which case we just return `orig_text`.
