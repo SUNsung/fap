542336ec3222e2c37f20289e612b91e06ee63fd9
SUNsung <weatherius@gmail.com>
2019-07-04 01:02:08 +0300

    Rewrite [02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py] File in: 2019-07-04 01:02:08

--- pyton/02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py
+++ pyton/02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py
@@ -1,130 +1,142 @@
 
         
-        
-def load_data(label_mode='fine'):
-    '''Loads CIFAR100 dataset.
+        # Autoencoder = Encoder + Decoder
+# Instantiate Autoencoder Model
+autoencoder = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+autoencoder.summary()
     
-        def decode(self, value):
-        if value != self.resource_type:
-            raise jose.DeserializationError(
-                'Wrong resource type: {0} instead of {1}'.format(
-                    value, self.resource_type))
-        return value
-
-    
-    # Add any extra paths that contain custom files (such as robots.txt or
-# .htaccess) here, relative to this directory. These files are copied
-# directly to the root of the documentation.
-#html_extra_path = []
+    print('Convert class vector to binary class matrix '
+      '(for use with categorical_crossentropy)')
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+print('y_train shape:', y_train.shape)
+print('y_test shape:', y_test.shape)
     
     
-UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-apache-conf-digest.txt'
-'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
+print('\nBuilding network 2...')
+model2 = create_network(num_classes=num_classes, **network2)
+    
+        @property
+    def config(self):
+        if not hasattr(self, '_config'):
+            self._config = Config(directory=self.config_dir)
+            if self._config.is_new():
+                self._config.save()
+            else:
+                self._config.load()
+        return self._config
+    
+        # If both `auth_parse` and `prompt_password` are set to `True`,
+    # and the value of `-a` lacks the password part,
+    # then the user will be prompted to type the password in.
+    prompt_password = True
+    
+        @staticmethod
+    def make_header(username, password):
+        credentials = u'%s:%s' % (username, password)
+        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
+        return 'Basic %s' % token
+    
+        def get_converters(self):
+        return [plugin for plugin in self
+                if issubclass(plugin, ConverterPlugin)]
     
     
-def run_test(setup, test, iterations_per_test=5, tests_to_run=10):
-    fastest_execution = min(timeit.Timer(test, setup=setup).repeat(tests_to_run, iterations_per_test))
-    execution_time = fastest_execution / iterations_per_test
-    fps = 1.0 / execution_time
-    return execution_time, fps
-    
-        # Display the results
-    for top, right, bottom, left in face_locations:
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-    
-    # Load the jpg file into a numpy array
-image = face_recognition.load_image_file('biden.jpg')
-    
-        # Bail out when the video file ends
-    if not ret:
-        break
-    
-            self.assertEqual(result.exit_code, 0)
-        self.assertTrue(target_string in result.output)
-    
-        if not app.config.edit_on_github_project:
-        warnings.warn('edit_on_github_project not specified')
-        return
-    if not doctree:
-        warnings.warn('doctree is None')
-        return
-    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
-    show_url = get_github_url(app, 'blob', path)
-    edit_url = get_github_url(app, 'edit', path)
-    
-    
-def __meters_to_kilometers(meters: float) -> float:
-    '''Convert meters to kilometers.'''
-    return meters * 0.001
-
-    
-        return set_ip_to_mock
+@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
+@pytest.mark.parametrize('filename', filenames)
+def test_rst_file_syntax(filename):
+    p = subprocess.Popen(
+        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    err = p.communicate()[1]
+    assert p.returncode == 0, err.decode('utf8')
 
     
     
-def linear_dense(x, n_unit, name=None, reuse=None):
-    '''线性全连接层
-    Input shape:  [batch_size, n_input]
-    Output shape: [batch_size, n_unit]
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
     
+            if add_server_ips:
+            if module.check_mode:
+                _check_mode(module, _add_server_ips(module,
+                                                    oneandone_conn,
+                                                    firewall_policy['id'],
+                                                    add_server_ips))
     
-def multi_highway_dense(x, n_layer, act_fn=relu, carry_bias=-1.0, name=None):
-    '''多层 highway_dense
-    Input shape:  [batch_size, n_input]
-    Output shape: [batch_size, n_input]
-    '''
-    name = name or 'highway_dense'
-    for i in range(n_layer):
-        x = highway_dense(x, act_fn=act_fn, carry_bias=carry_bias, name='{}-{}'.format(name, i))
-    
-            # 得到 S 后，下面的操作就与 `attention_flow_self` 一样了
-    
-    
-def get_wb(shape,
-           w_initializer=truncated_normal,
-           b_initializer=zeros,
-           w_regularizer=l2_regularizer,
-           b_regularizer=None,  # 一般不对偏置做权重惩罚，可能会导致欠拟合
-           name=None):
-    ''''''
-    name = '' if name is None else name + '_'
-    W = tf.get_variable(name + 'W', shape=shape,
-                        dtype=tf_float, initializer=w_initializer, regularizer=w_regularizer)
-    b = tf.get_variable(name + 'b', shape=shape[-1:],
-                        dtype=tf_float, initializer=b_initializer, regularizer=b_regularizer)
-    return W, b
-    
-                with open('htmlout.html', 'w') as out:
-                out.write(header)
-    
-    
-class TagKeyNotFound(Exception):
+    try:
+    from pyvcloud.schema.vcd.v1_5.schemas.vcloud.networkType import FirewallRuleType
+    from pyvcloud.schema.vcd.v1_5.schemas.vcloud.networkType import ProtocolsType
+except ImportError:
+    # normally set a flag here but it will be caught when testing for
+    # the existence of pyvcloud (see module_utils/vca.py).  This just
+    # protects against generating an exception at runtime
     pass
     
-    from django.db import models
-from django.utils import timezone
+    # Ensure rule with certain limitations
+- ipa_hbacrule:
+    name: allow_all_developers_access_to_db
+    description: Allow all developers to access any database from any host
+    hostgroup:
+    - db-server
+    usergroup:
+    - developers
+    state: present
+    ipa_host: ipa.example.com
+    ipa_user: admin
+    ipa_pass: topsecret
     
-            # Adding model 'GroupTagKey'
-        db.create_table(u'tagstore_grouptagkey', (
-            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
-            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
-            ('group_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
-            ('environment_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)),
-            ('_key', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                to=orm['tagstore.TagKey'], db_column='key_id')),
-            ('values_seen', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
-        ))
-        db.send_create_signal('tagstore', ['GroupTagKey'])
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
     
-        This is currenlty only used for ``in_next_release`` resolutions.
-    '''
-    try:
-        release = Release.objects.get_from_cache(
-            id=release_id,
-        )
-    except Release.DoesNotExist:
-        return
+        SUMMARY_COMMAND = ('summary', 'summary -B')[is_version_higher_than_5_18()]
+    
+    
+class _BenchSpider(scrapy.Spider):
+    '''A spider that follows all links'''
+    name = 'follow'
+    total = 10000
+    show = 20
+    baseurl = 'http://localhost:8998'
+    link_extractor = LinkExtractor()
+    
+        def _maybe_fire_closing(self):
+        if self.closing and not self.inprogress:
+            if self.nextcall:
+                self.nextcall.cancel()
+                if self.heartbeat.running:
+                    self.heartbeat.stop()
+            self.closing.callback(None)
+    
+        def _debug_cookie(self, request, spider):
+        if self.debug:
+            cl = [to_native_str(c, errors='replace')
+                  for c in request.headers.getlist('Cookie')]
+            if cl:
+                cookies = '\n'.join('Cookie: {}\n'.format(c) for c in cl)
+                msg = 'Sending cookies to: {}\n{}'.format(request, cookies)
+                logger.debug(msg, extra={'spider': spider})
+    
+    
+def setup(app):
+    app.add_config_value('edit_on_github_project', '', True)
+    app.add_config_value('edit_on_github_branch', 'master', True)
+    app.add_config_value('edit_on_github_src_path', '', True)  # 'eg' 'docs/'
+    app.connect('html-page-context', html_page_context)
+
+    
+        if unit_1 == LENGTH_MILES:
+        meters = __miles_to_meters(value)
+    elif unit_1 == LENGTH_FEET:
+        meters = __feet_to_meters(value)
+    elif unit_1 == LENGTH_KILOMETERS:
+        meters = __kilometers_to_meters(value)
+    
+    from aiohttp import web
