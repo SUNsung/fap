e566057def1d147996dcc80d038cabd5992dc26d
SUNsung <weatherius@gmail.com>
2019-07-03 12:31:13 +0300

    Rewrite [5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb] File in: 2019-07-03 12:31:13

--- ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
+++ ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
@@ -1,127 +1,88 @@
 
         
-              action_return = run(
-        action_named: action_name,
-        action_class_ref: action_class_ref,
-        parameter_map: parameter_map
-      )
-    
-        def show_message
-      UI.message('Sending anonymous analytics information')
-      UI.message('Learn more at https://docs.fastlane.tools/#metrics')
-      UI.message('No personal or sensitive data is sent.')
-      UI.message('You can disable this by adding `opt_out_usage` at the top of your Fastfile')
+            it 'works for running jobs' do
+      job.locked_at = Time.now
+      job.locked_by = 'test'
+      expect(status(job)).to eq('<span class='label label-info'>running</span>')
     end
     
-            display_user_error!(e, error_info)
-      else
-        # Pass the error instead of a message so that the inspector can do extra work to simplify the query
-        show_github_issues(e) if should_show_github_issues
-    
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            tag: '#{tag}',
-            message: '#{message}',
-            force: true
-          })
-        end').runner.execute(:test)
-    
-          it 'works with all parameters' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-              select_regex: /.*/,
-              exclude_regex: /Test.m/,
-              report_type: 'pmd',
-              report_path: 'report_path.xml',
-              max_priority_1: 10,
-              max_priority_2: 20,
-              max_priority_3: 30,
-              thresholds: ['LONG_LINE=200', 'LONG_METHOD=200'],
-              enable_rules: ['DoubleNegative', 'DeadCode'],
-              disable_rules: ['GotoStatement', 'ShortVariableName'],
-              list_enabled_rules: true,
-              enable_clang_static_analyzer: true,
-              enable_global_analysis: true,
-              allow_duplicated_violations: true
-            )
-          end').runner.execute(:test)
-    
-            FastlaneCore::CertChecker.wwdr_certificate_installed?
+        context '#run' do
+      before do
+        mock(@agent_runner).run_workers
       end
     
-          describe 'running with fastlane' do
-        before do
-          allow(FastlaneCore::Helper).to receive(:fastlane_enabled?).and_return(true)
-          allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return('./fastlane')
+      let :valid_options do
+    {
+      'name' => 'XKCD',
+      'expected_update_period_in_days' => '2',
+      'type' => 'html',
+      'url' => '{{ url | default: 'http://xkcd.com/' }}',
+      'mode' => 'on_change',
+      'extract' => old_extract,
+      'template' => old_template
+    }
+  end
     
-          it 'splits correctly' do
-        expected = [
-          'One',
-          'Two',
-          'Three',
-          'Four Token',
-          'Or',
-          'Newlines',
-          'Everywhere'
-        ]
-        expect(generator.split_keywords(keywords)).to eq(expected)
+      describe 'path request must exist' do
+    it 'should check that validation added if path does not exist' do
+      opts = @opts.tap { |o| o.delete('path') }
+      @checker = Agents::AftershipAgent.new(:name => 'tectonic', :options => opts)
+      @checker.user = users(:bob)
+      expect(@checker.save).to eq false
+      expect(@checker.errors.full_messages.first).to eq('You need to specify a path request')
+    end
+  end
+    
+        it 'should provide the since attribute after the first run' do
+      time = (Time.now-1.minute).iso8601
+      @checker.memory[:last_event] = time
+      @checker.save
+      expect(@checker.reload.send(:query_parameters)).to eq({:query => {:since => time}})
+    end
+  end
+    
+        it 'should require access token' do
+      @checker.options['user_credentials'] = nil
+      expect(@checker).not_to be_valid
+    end
+  end
+    
+        def dependencies
+      @dependencies ||= [Cask::CaskLoader.load('adobe-air')]
+    end
+    
+    # define charCodeAt on String
+class String
+  def charCodeAt(k)
+    # use scan, nil check, and unpack instead of ord for 1.8
+    # 1.9 can simply use self[k].ord
+    # http://stackoverflow.com/questions/7793177/split-utf8-string-regardless-of-ruby-version
+    c = self.scan(/./mu)[k]
+    return nil if c.nil?
+    c.unpack('U')[0]
+  end
+end
+    
+          def has_footer
+        @footer = (@page.footer || false) if @footer.nil? && @page
+        !!@footer
       end
+    
+          def previous_link
+      end
+    
+      s.add_dependency 'gollum-lib', '~> 4.2', '>= 4.2.10'
+  s.add_dependency 'kramdown', '~> 1.9.0'
+  s.add_dependency 'sinatra', '~> 1.4', '>= 1.4.4'
+  s.add_dependency 'mustache', ['>= 0.99.5', '< 1.0.0']
+  s.add_dependency 'useragent', '~> 0.16.2'
+  s.add_dependency 'gemojione', '~> 3.2'
+    
+        def initialize(dir, existing, attempted, message = nil)
+      @dir            = dir
+      @existing_path  = existing
+      @attempted_path = attempted
+      super(message || 'Cannot write #{@dir}/#{@attempted_path}, found #{@dir}/#{@existing_path}.')
     end
   end
 end
-
-    
-    module Gitlab
-  module GithubImport
-    module Importer
-      class DiffNotesImporter
-        include ParallelScheduling
-    
-              hash = {
-            iid: issue.number,
-            title: issue.title,
-            description: issue.body,
-            milestone_number: issue.milestone&.number,
-            state: issue.state == 'open' ? :opened : :closed,
-            assignees: issue.assignees.map do |u|
-              Representation::User.from_api_response(u)
-            end,
-            label_names: issue.labels.map(&:name),
-            author: user,
-            created_at: issue.created_at,
-            updated_at: issue.updated_at,
-            pull_request: issue.pull_request ? true : false
-          }
-    
-            # Builds a user from a GitHub API response.
-        #
-        # user - An instance of `Sawyer::Resource` containing the user details.
-        def self.from_api_response(user)
-          new(id: user.id, login: user.login)
-        end
-    
-            transaction.action = action_name(env)
-        transaction.act_upon_results
-    
-      def maxheight_or_default
-    params[:maxheight].present? ? params[:maxheight].to_i : nil
-  end
-end
-
-    
-          if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: provider_id.capitalize) if is_navigational_format?
-      else
-        session['devise.#{provider}_data'] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
-      end
-    end
-  end
-    
-    module Localized
-  extend ActiveSupport::Concern
-    
-      auto_updates true
-  depends_on cask: 'homebrew/cask-versions/adobe-photoshop-lightroom600'
