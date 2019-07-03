de8a6f61d21012406b41ad91ae09e80510774633
SUNsung <weatherius@gmail.com>
2019-07-03 05:22:33 +0300

    Rewrite [55UWDAAJndGQM55ePONtUlzRDE55.1d.PGn4h4.55ouq.cpp] File in: 2019-07-03 05:22:33

--- cccccc/55UWDAAJndGQM55ePONtUlzRDE55.1d.PGn4h4.55ouq.cpp
+++ cccccc/55UWDAAJndGQM55ePONtUlzRDE55.1d.PGn4h4.55ouq.cpp
@@ -1,75 +1,95 @@
 
         
-            // Setup time step
-    INT64 current_time;
-    ::QueryPerformanceCounter((LARGE_INTEGER *)&current_time);
-    io.DeltaTime = (float)(current_time - g_Time) / g_TicksPerSecond;
-    g_Time = current_time;
+        int32 remainder(int32 x, int32 modulus);
+int32 TriangulatePolygon(float32* xv, float32* yv, int32 vNum, b2Triangle* results);
+bool IsEar(int32 i, float32* xv, float32* yv, int32 xvLength); //Not for external use
+int32 PolygonizeTriangles(b2Triangle* triangulated, int32 triangulatedLength, b2Polygon* polys, int32 polysLength);
+int32 DecomposeConvex(b2Polygon* p, b2Polygon* results, int32 maxPolys);
+//void DecomposeConvexAndAddTo(b2Polygon* p, b2Body* bd, b2FixtureDef* prototype);
     
-    #pragma once
     
-    void ImGui_ImplGLUT_MotionFunc(int x, int y)
-{
-    ImGuiIO& io = ImGui::GetIO();
-    io.MousePos = ImVec2((float)x, (float)y);
-}
+    {};
+    
+    		inline SourceAlphaMix GetSourceAlphaMix(void)
+		{
+			return m_sourcealphamix;
+		}
+    
+    
+    {} // namespace Etc
 
     
-        // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
+    void SimplifyPolygon(const Path &in_poly, Paths &out_polys, PolyFillType fillType = pftEvenOdd);
+void SimplifyPolygons(const Paths &in_polys, Paths &out_polys, PolyFillType fillType = pftEvenOdd);
+void SimplifyPolygons(Paths &polys, PolyFillType fillType = pftEvenOdd);
     
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-        // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
-    
-    public:
-  DHTResponseMessage(const std::shared_ptr<DHTNode>& localNode,
-                     const std::shared_ptr<DHTNode>& remoteNode,
-                     const std::string& transactionID);
-    
-      std::unique_ptr<DHTBucketTreeNode> root_;
-    
-    
-    {} // namespace aria2
+    #endif /* KISS_FFT_GUTS_H */
 
     
-    class DHTNode;
+    #define SCALEIN(a)      (a)
+#define SCALEOUT(a)     (a)
     
-    void DHTTaskFactoryImpl::setRoutingTable(DHTRoutingTable* routingTable)
+    /* a32 + (b32 * (c32 >> 16)) >> 16 */
+#undef silk_SMLAWT
+#define silk_SMLAWT(a, b, c) ((a) + silk_SMULWT(b, c))
+    
+    /* a32 + (opus_int32)((opus_int16)(b32)) * (opus_int32)((opus_int16)(c32)) output have to be 32bit int */
+#undef silk_SMLABB
+static OPUS_INLINE opus_int32 silk_SMLABB_armv5e(opus_int32 a, opus_int32 b,
+ opus_int32 c)
 {
-  routingTable_ = routingTable;
+  int res;
+  __asm__(
+      '#silk_SMLABB\n\t'
+      'smlabb %0, %1, %2, %3\n\t'
+      : '=r'(res)
+      : '%r'(b), 'r'(c), 'r'(a)
+  );
+  return res;
+}
+#define silk_SMLABB(a, b, c) (silk_SMLABB_armv5e(a, b, c))
+    
+    #endif // HPHP_FILE_STREAM_WRAPPER_H
+
+    
+    
+    {  // if the function was called via FCallBuiltin, we'll get a bogus name as
+  // the stack frame will be wrong
+  ActRec* ar = g_context->getStackFrame();
+  const char* fn = (ar != nullptr)
+    ? ar->func()->name()->data()
+    : 'OPTIMIZED_BUILTIN';
+  raise_warning('%s(%s): failed to open stream: '
+                'wrapper does not support stream open',
+                fn, filename.data());
+  return nullptr;
 }
     
-      void setMessageDispatcher(DHTMessageDispatcher* dispatcher);
+    struct GlobStreamWrapper final : Stream::Wrapper {
+  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+  req::ptr<Directory> opendir(const String& path) override;
+};
     
-      std::string generateToken(const unsigned char* infoHash,
-                            const std::string& ipaddr, uint16_t port,
-                            const unsigned char* secret) const;
-    
-    
-    {    // out_of_range.404
-    try
-    {
-        // try to use a JSON pointer that cannot be resolved
-        json::const_reference ref = j.at('/number/foo'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+    template<typename F>
+void logLowPriPerfWarning(folly::StringPiece event, F fillCols) {
+  logPerfWarningImpl(event, 0, kDefaultPerfWarningRate, fillCols);
 }
+template<typename F>
+void logLowPriPerfWarning(folly::StringPiece event, int64_t rate, F fillCols) {
+  logPerfWarningImpl(event, 0, rate, fillCols);
+}
+    
+    private:
+  bool m_get;
+  bool m_ignoreErrors;
+  const char* m_method;
+  Array m_headers;
+  String m_postData;
+  int m_maxRedirect;
+  int m_timeout;
+  std::string m_error;
+  StringBuffer m_response;
+  Array m_responseHeaders;
+    
+    //////////////////////////////////////////////////////////////////////
