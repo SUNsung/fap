17eaa5b306b5eac3760967d545b2ac89bd3ffe5d
SUNsung <weatherius@gmail.com>
2019-07-03 13:00:38 +0300

    Rewrite [14gtqrLR6WMNU1444ceFhhzF0614rzgWOjTaJLU14lg0.py] File in: 2019-07-03 13:00:37

--- pyton/14gtqrLR6WMNU1444ceFhhzF0614rzgWOjTaJLU14lg0.py
+++ pyton/14gtqrLR6WMNU1444ceFhhzF0614rzgWOjTaJLU14lg0.py
@@ -1,178 +1,37 @@
 
         
-        # Dependency labels used in the paths.
-DEPLABELS = [
-    'PAD',     'UNK',       'ROOT',    'abbrev',    'acomp', 'advcl',
-    'advmod',  'agent',     'amod',    'appos',     'attr',  'aux',
-    'auxpass', 'cc',        'ccomp',   'complm',    'conj',  'cop',
-    'csubj',   'csubjpass', 'dep',     'det',       'dobj',  'expl',
-    'infmod',  'iobj',      'mark',    'mwe',       'nc',    'neg',
-    'nn',      'npadvmod',  'nsubj',   'nsubjpass', 'num',   'number',
-    'p',       'parataxis', 'partmod', 'pcomp',     'pobj',  'poss',
-    'preconj', 'predet',    'prep',    'prepc',     'prt',   'ps',
-    'purpcl',  'quantmod',  'rcmod',   'ref',       'rel',   'suffix',
-    'title',   'tmod',      'xcomp',   'xsubj',
-]
+            Returns:
+      A numpy array with the same shape as rates_bxtxd, but with the event
+      counts.
+    '''
     
-        if hps.output_dist == 'poisson':
-      out_dist_params = np.zeros([E_to_process, T, D])
-    elif hps.output_dist == 'gaussian':
-      out_dist_params = np.zeros([E_to_process, T, D+D])
-    else:
-      assert False, 'NIY'
-    
-      # Convert from figure to an numpy array width x height x 3 (last for RGB)
-  f.canvas.draw()
-  data = np.fromstring(f.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-  data_wxhx3 = data.reshape(f.canvas.get_width_height()[::-1] + (3,))
-  plt.close()
+    ## Sample Generation.
+## Binary and setup FLAGS.
+tf.app.flags.DEFINE_enum('sample_mode', 'TRAIN',
+                         [SAMPLE_TRAIN, SAMPLE_VALIDATION],
+                         'Dataset to sample from.')
+tf.app.flags.DEFINE_string('output_path', '/tmp', 'Model output directory.')
+tf.app.flags.DEFINE_boolean(
+    'output_masked_logs', False,
+    'Whether to display for human evaluation (show masking).')
+tf.app.flags.DEFINE_integer('number_epochs', 1,
+                            'The number of epochs to produce.')
     
       Returns:
-    gen_train_op: Generator training op.
+    loss: Scalar tf.float32 total loss.
   '''
-  del hparams
-  with tf.name_scope('train_generator'):
-    if FLAGS.generator_optimizer == 'sgd':
-      gen_optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    elif FLAGS.generator_optimizer == 'adam':
-      gen_optimizer = tf.train.AdamOptimizer(learning_rate)
-    else:
-      raise NotImplementedError
-    gen_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
-    ]
-    print('\nOptimizing Generator vars:')
-    for v in gen_vars:
-      print(v)
+  cross_entropy_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+      labels=gen_labels, logits=gen_logits)
+  gan_loss = -tf.log(dis_predictions)
+  loss_matrix = tf.where(is_real_input, cross_entropy_loss, gan_loss)
+  return tf.reduce_mean(loss_matrix)
     
-      ## Load weights from language model checkpoint.
-  if FLAGS.language_model_ckpt_dir:
-    if FLAGS.maskgan_ckpt is None:
-      ## Generator Models.
-      if FLAGS.generator_model == 'rnn_nas':
-        load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-        print('Restoring Generator from %s.' % load_ckpt)
-        tf.logging.info('Restoring Generator from %s.' % load_ckpt)
-        gen_init_saver = init_savers['gen_init_saver']
-        gen_init_saver.restore(sess, load_ckpt)
-    
-      ## Attention.
-  if FLAGS.attention_option is not None:
-    decoder_attention_keys = [
-        v for v in tf.trainable_variables()
-        if v.op.name == 'dis/decoder/attention_keys/weights'
-    ][0]
-    decoder_attention_construct_weights = [
-        v for v in tf.trainable_variables()
-        if v.op.name == 'dis/decoder/rnn/attention_construct/weights'
-    ][0]
-    
-        # https://en.wikipedia.org/wiki/Playfair_cipher#Description
-    for char1, char2 in chunker(plaintext, 2):
-        row1, col1 = divmod(table.index(char1), 5)
-        row2, col2 = divmod(table.index(char2), 5)
-    
-    def main():
-    print('Making key files...')
-    makeKeyFiles('rsa', 1024)
-    print('Key files generation successful.')
-    
-        def bulk_insert(self, values):
-        i = 1
-        self.__aux_list = values
-        for value in values:
-            self.insert_data(value)
-            self._step_by_step(i)
-            i += 1
-    
-            return new_key
-
-    
-        def __init__(self, arr):
-        # we need a list not a string, so do something to change the type
-        self.array = arr.split(',')
-        print(('the input array is:', self.array))
+      return variable_mapping
     
     
-def b_expo_mod(a, b, c):
-    res = 0
-    while b > 0:
-        if b&1:
-            res = ((res%c) + (a%c)) % c
-    
-    
-def select_command_with_arrows(proc, TIMEOUT):
-    '''Ensures that command can be selected with arrow keys.'''
-    _set_confirmation(proc, True)
-    
-    
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'echo test')
-    
-    
-@pytest.mark.parametrize('script, output, help_text, result', [
-    ('apt-get isntall vim', invalid_operation('isntall'),
-     apt_get_help, 'apt-get install vim'),
-    ('apt saerch vim', invalid_operation('saerch'),
-     apt_help, 'apt search vim'),
-])
-def test_get_new_command(set_help, output, script, help_text, result):
-    set_help(help_text)
-    assert get_new_command(Command(script, output))[0] == result
-
-    
-    match_output = '''
-Listing... Done
-heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
-resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
-squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
-unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
-'''
-    
-      aws help
-  aws <command> help
-  aws <command> <subcommand> help
-aws: error: argument command: Invalid choice, valid choices are:
-    
-    
-output = '''sshfs: OsxfuseRequirement unsatisfied!
-    
-    
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_match(brew_no_available_formula, brew_already_installed,
-               brew_install_no_argument):
-    assert match(Command('brew install elsticsearch',
-                         brew_no_available_formula))
-    assert not match(Command('brew install git',
-                             brew_already_installed))
-    assert not match(Command('brew install', brew_install_no_argument))
-    
-    # input image dimensions
-img_rows, img_cols = 28, 28
-    
-    
-def l2(l=0.01):
-    return L1L2(l2=l)
-    
-    
-def test_mnist():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        assert len(x_train) == len(y_train) == 60000
-        assert len(x_test) == len(y_test) == 10000
-    
-        E.g. for use with categorical_crossentropy.
-    
-        # Input shape
-        5D tensor with shape:
-        `(samples, timesteps, channels, rows, cols)` if data_format='channels_first'
-        or 5D tensor with shape:
-        `(samples, timesteps, rows, cols, channels)` if data_format='channels_last'.
-    
-    Noise + Data ---> Denoising Autoencoder ---> Data
+def _create_attention_score_fn(name,
+                               num_units,
+                               attention_option,
+                               reuse,
+                               dtype=tf.float32):
+  '''Different ways to compute attention scores.
