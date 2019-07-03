56d0bc7e1fd20d616bc986fcada2d89d5329eca7
SUNsung <weatherius@gmail.com>
2019-07-03 09:43:55 +0300

    Rewrite [36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py] File in: 2019-07-03 09:43:55

--- pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
+++ pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
@@ -1,106 +1,108 @@
 
         
         
-@pytest.fixture(autouse=True)
-def shell(mocker):
-    shell = mocker.patch('thefuck.entrypoints.not_configured.shell',
-                         new_callable=MagicMock)
-    shell.get_history.return_value = []
-    shell.how_to_configure.return_value = ShellConfiguration(
-        content='eval $(thefuck --alias)',
-        path='/tmp/.bashrc',
-        reload='bash',
-        can_configure_automatically=True)
-    return shell
+def naughty_strings(filepath=FILEPATH):
+    '''Get the list of naughty_strings.
+    
+    from .theplatform import theplatform_download_by_pid
+    
+        print_info(site_info, title, 'm3u8', float('inf'))
+    
+    def ifeng_download_by_id(id, title = None, output_dir = '.', merge = True, info_only = False):
+    assert r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', id), id
+    url = 'http://vxml.ifengimg.com/video_info_new/%s/%s/%s.xml' % (id[-2], id[-2:], id)
+    xml = get_html(url, 'utf-8')
+    title = r1(r'Name='([^']+)'', xml)
+    title = unescape_html(title)
+    url = r1(r'VideoPlayUrl='([^']+)'', xml)
+    from random import randint
+    r = randint(10, 19)
+    url = url.replace('http://wideo.ifeng.com/', 'http://ips.ifeng.com/wideo.ifeng.com/')
+    type, ext, size = url_info(url)
+    
+        stream_types = [
+        {'id': 'video'},
+        {'id': 'audio'},
+        {'id': 'slides'}
+    ]
     
     
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'echo test')
+class Fixed(jose.Field):
+    '''Fixed field.'''
     
-    
-@pytest.mark.functional
-def test_with_confirmation(proc, TIMEOUT):
-    with_confirmation(proc, TIMEOUT)
-    
-        invocations = dict(
-        aliases='alias_details',
-        all='all_details',
-        config='config_details',
-        mappings='mapping_details',
-        policy='policy_details',
-        versions='version_details',
-    )
-    
-    
-if __name__ == '__main__':
-    main()
+        @classmethod
+    # pylint: disable=arguments-differ,too-many-arguments
+    def sign(cls, payload, key, alg, nonce, url=None, kid=None):
+        # Per ACME spec, jwk and kid are mutually exclusive, so only include a
+        # jwk field if kid is not provided.
+        include_jwk = kid is None
+        return super(JWS, cls).sign(payload, key=key, alg=alg,
+                                    protect=frozenset(['nonce', 'url', 'kid', 'jwk', 'alg']),
+                                    nonce=nonce, url=url, kid=kid,
+                                    include_jwk=include_jwk)
 
     
-        def role_del(self, name):
-        return self._post_json(method='role_del', name=name)
-    
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.urls import fetch_url
-from ansible.module_utils.six.moves.urllib.parse import urlencode
-    
-            change_applied = False
-        if self.state == 'present':
-            if rc != 0:
-                self.module.fail_json(msg='Failed to %s feature %s.'
-                                          ' icinga2 command returned %s' % (feature_enable_str,
-                                                                            self.feature_name,
-                                                                            out))
-    
-            if not query_log_status(module, le_path, log):
-            module.fail_json(msg='failed to follow '%s': %s' % (log, err.strip()))
+    # http://docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
     
     
-def delete(resource):
-    return request(resource, xml=None, method='DELETE')
+UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-apache-conf-digest.txt'
+'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
     
     
-def send_deploy_event(module, key, revision_id, deployed_by='Ansible', deployed_to=None, repository=None):
-    '''Send a deploy event to Stackdriver'''
-    deploy_api = 'https://event-gateway.stackdriver.com/v1/deployevent'
+Examples
+--------
     
-        def test_weak_etag_match(self):
-        computed_etag = ''xyzzy1''
-        etags = 'W/'xyzzy1''
-        self.check_url(
-            '/cache/' + computed_etag, method='GET',
-            headers=[('If-None-Match', etags)],
-            expected_status=304)
     
-    define('ioloop', type=str, default=None)
+# -- Options for HTML output ----------------------------------------------
     
-    from tornado.options import options, define, parse_command_line
-from tornado.template import Template
     
-    if __name__ == '__main__':
-    # This sys.path manipulation must come before our imports (as much
-    # as possible - if we introduced a tornado.sys or tornado.os
-    # module we'd be in trouble), or else our imports would become
-    # relative again despite the future import.
-    #
-    # There is a separate __main__ block at the end of the file to call main().
-    if sys.path[0] == os.path.dirname(__file__):
-        del sys.path[0]
+def setup(app):
+    app.add_config_value('edit_on_github_project', '', True)
+    app.add_config_value('edit_on_github_branch', 'master', True)
+    app.add_config_value('edit_on_github_src_path', '', True)  # 'eg' 'docs/'
+    app.connect('html-page-context', html_page_context)
+
     
-            :arg httputil.HTTPServerRequest request: current HTTP request.
-        :arg kwargs: additional keyword arguments passed by routing implementation.
-        :returns: an instance of `~.httputil.HTTPMessageDelegate` that will be used to
-            process the request.
-        '''
-        raise NotImplementedError()
+    # If false, no index is generated.
+#
+# html_use_index = True
     
-            # This timeout needs to be fairly generous for pypy due to jit
-        # warmup costs.
-        for i in range(40):
-            if autoreload_proc.poll() is not None:
-                break
-            time.sleep(0.1)
-        else:
-            autoreload_proc.kill()
-            raise Exception('subprocess failed to terminate')
+    from packaging.version import Version
+    
+    
+class PackValueError(PackException, ValueError):
+    pass
+
+    
+    
+def test_exceeding_unpacker_read_size():
+    dumpf = io.BytesIO()
+    
+    define('port', default=8888)
+define('num_chunks', default=1000)
+define('chunk_size', default=2048)
+    
+    if os.name == 'nt':
+    from tornado.platform.windows import set_close_exec
+else:
+    from tornado.platform.posix import set_close_exec
+    
+        c-ares fails to resolve some names when ``family`` is ``AF_UNSPEC``,
+    so it is only recommended for use in ``AF_INET`` (i.e. IPv4).  This is
+    the default for ``tornado.simple_httpclient``, but other libraries
+    may default to ``AF_UNSPEC``.
+    
+    
+class GoogleLoginHandler(RequestHandler, GoogleOAuth2Mixin):
+    def initialize(self, test):
+        self.test = test
+        self._OAUTH_REDIRECT_URI = test.get_url('/client/login')
+        self._OAUTH_AUTHORIZE_URL = test.get_url('/google/oauth2/authorize')
+        self._OAUTH_ACCESS_TOKEN_URL = test.get_url('/google/oauth2/token')
