c08323cae69e6a53ef1e023c0a6baea1bf7d6c4e
SUNsung <weatherius@gmail.com>
2019-07-03 22:43:08 +0300

    Rewrite [568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py] File in: 2019-07-03 22:43:08

--- pyton/568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py
+++ pyton/568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py
@@ -1,226 +1,127 @@
 
         
-            def __init__(self):
-        Resource.__init__(self)
-        self.concurrent = 0
-        self.tail = deque(maxlen=100)
-        self._reset_stats()
+        # choose a random port to avoid colliding with TIME_WAIT sockets left over
+# from previous runs.
+define('min_port', type=int, default=8000)
+define('max_port', type=int, default=9000)
     
-        def _err(self, msg):
-        sys.stderr.write(msg + os.linesep)
-        self.exitcode = 1
+        logging.warning('Starting fetch with curl client')
+    curl_client = CurlAsyncHTTPClient()
+    curl_client.fetch('http://localhost:%d/' % options.port,
+                      callback=callback)
+    IOLoop.current().start()
     
-        def _genspider(self, module, name, domain, template_name, template_file):
-        '''Generate the spider module, based on the given template'''
-        tvars = {
-            'project_name': self.settings.get('BOT_NAME'),
-            'ProjectName': string_camelcase(self.settings.get('BOT_NAME')),
-            'module': module,
-            'name': name,
-            'domain': domain,
-            'classname': '%sSpider' % ''.join(s.capitalize() \
-                for s in module.split('_'))
-        }
-        if self.settings.get('NEWSPIDER_MODULE'):
-            spiders_module = import_module(self.settings['NEWSPIDER_MODULE'])
-            spiders_dir = abspath(dirname(spiders_module.__file__))
+    
+def main():
+    parse_command_line()
+    if options.dump:
+        print(tmpl.code)
+        sys.exit(0)
+    t = Timer(render)
+    results = t.timeit(options.num) / options.num
+    print('%0.3f ms per iteration' % (results * 1000))
+    
+        def _sock_state_cb(self, fd: int, readable: bool, writable: bool) -> None:
+        state = (IOLoop.READ if readable else 0) | (IOLoop.WRITE if writable else 0)
+        if not state:
+            self.io_loop.remove_handler(fd)
+            del self.fds[fd]
+        elif fd in self.fds:
+            self.io_loop.update_handler(fd, state)
+            self.fds[fd] = state
         else:
-            spiders_module = None
-            spiders_dir = '.'
-        spider_file = '%s.py' % join(spiders_dir, module)
-        shutil.copyfile(template_file, spider_file)
-        render_templatefile(spider_file, **tvars)
-        print('Created spider %r using template %r ' % (name, \
-            template_name), end=('' if spiders_module else '\n'))
-        if spiders_module:
-            print('in module:\n  %s.%s' % (spiders_module.__name__, module))
+            self.io_loop.add_handler(fd, self._handle_events, state)
+            self.fds[fd] = state
+    
+        def tearDown(self):
+        asyncio.set_event_loop_policy(self.orig_policy)
+        self.executor.shutdown()
+    
+        def test_json_decode(self):
+        # json_decode accepts both bytes and unicode, but strings it returns
+        # are always unicode.
+        self.assertEqual(json_decode(b''foo''), u'foo')
+        self.assertEqual(json_decode(u''foo''), u'foo')
+    
+            print('Empty input')
+        assert_equal(merge_sort.sort([]), [])
     
     
-class Command(ScrapyCommand):
+if __name__ == '__main__':
+    main()
     
-        try:
-        # XXX: this try-except is not needed in Twisted 17.0.0+ because
-        # it requires pyOpenSSL 0.16+.
-        from OpenSSL.SSL import SSL_CB_HANDSHAKE_DONE, SSL_CB_HANDSHAKE_START
-    except ImportError:
-        SSL_CB_HANDSHAKE_START = 0x10
-        SSL_CB_HANDSHAKE_DONE = 0x20
+    		while current is not None and current.data is not data:
+			parent = current
+			if data < current.data:
+				current = current.leftChild
+				isLeft = True 
+			else:
+				current = current.rightChild
+				isLeft = False
     
-        def test_func_default_args(self):
-        def first_func(a, b):
-            return a+b
-        def second_func(a=1, b=2):
-            return a+b
-        self.assertEqual(first_func.__defaults__, None)
-        self.assertEqual(second_func.__defaults__, (1, 2))
-        first_func.__defaults__ = (1, 2)
-        self.assertEqual(first_func.__defaults__, (1, 2))
-        self.assertEqual(first_func(), 3)
-        self.assertEqual(first_func(3), 5)
-        self.assertEqual(first_func(3, 5), 8)
-        del second_func.__defaults__
-        self.assertEqual(second_func.__defaults__, None)
-        try:
-            second_func()
-        except TypeError:
-            pass
+            self.simple_event = self.autopatch(g.stats, 'simple_event')
+    
+        def test_del_msg_failure_with_sender(self):
+        '''Del_msg fails: Returns 200 and does not set del_on_recipient.'''
+        message = MagicMock(spec=Message)
+        message.name = 'msg_3'
+        message.to_id = self.id + 1
+        message.del_on_recipient = False
+    
+        def test_nosigning_login(self):
+        res = self.do_login(
+            headers={
+                signing.SIGNATURE_UA_HEADER: None,
+                signing.SIGNATURE_BODY_HEADER: None,
+            },
+            expect_errors=True,
+        )
+        self.assert_403_response(res, 'signing.ua.invalid.invalid_format')
+    
+            # Only controls chosen in the no-variant case.
+        for variant, percentage in FeatureState.DEFAULT_CONTROL_GROUPS.items():
+            count = counters['no_variants'][variant]
+            # The variant percentage is expressed as a part of 100, so we need
+            # to calculate the fraction-of-1 percentage and scale it
+            # accordingly.
+            scaled_percentage = float(count) / (FeatureState.NUM_BUCKETS / 100)
+            self.assertEqual(scaled_percentage, percentage)
+        for variant, percentage in three_variants.items():
+            count = counters['three_variants'][variant]
+            scaled_percentage = float(count) / (FeatureState.NUM_BUCKETS / 100)
+            self.assertEqual(scaled_percentage, percentage)
+        for variant, percentage in three_variants_more.items():
+            count = counters['three_variants_more'][variant]
+            scaled_percentage = float(count) / (FeatureState.NUM_BUCKETS / 100)
+            self.assertEqual(scaled_percentage, percentage)
+    
+            # Scenario: call is successful; pass customer_ip
+        return_value = create_authorization_hold(self.customer_id,
+                                                 self.payment_profile_id,
+                                                 self.amount, 12345,
+                                                 '127.0.0.1')
+        self.assertTrue(CreateRequest.called)
+        args, kwargs = CreateRequest.call_args
+        self.assertEqual(kwargs['extraOptions'], {'x_customer_ip': '127.0.0.1'})
+        self.assertEqual(return_value, self.transaction_id)
+    
+        def test_no_resize(self):
+        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
+                      height=800)
+        url = self.provider.resize_image(image)
+        self.assertEqual(url, 'https://example.com/a.jpg')
+    
+        def _assert_validity(self, body, header, success, error, **expected):
+        request = MagicMock(body=body, headers={})
+        if header:
+            request.headers[signing.SIGNATURE_BODY_HEADER] = header
+        signature = signing.valid_post_signature(request)
+        self.assertEqual(signature.is_valid(), bool(success))
+        if error:
+            self.assertIn(error.code, [code for code, _ in signature.errors])
         else:
-            self.fail('__defaults__ does not update; deleting it does not '
-                      'remove requirement')
-    
-            if not top_level_dir in sys.path:
-            # all test modules must be importable from the top level directory
-            # should we *unconditionally* put the start directory in first
-            # in sys.path to minimise likelihood of conflicts between installed
-            # modules and development versions?
-            sys.path.insert(0, top_level_dir)
-        self._top_level_dir = top_level_dir
-    
-        def test_loadTestsFromModule__pattern(self):
-        m = types.ModuleType('m')
-        class MyTestCase(unittest.TestCase):
-            def test(self):
-                pass
-        m.testcase_1 = MyTestCase
-    
-    [1] http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718
-    
-    while True:
-    line = input()
-    if line == '':
-        break
-    buffer += line
-    if sqlite3.complete_statement(buffer):
-        try:
-            buffer = buffer.strip()
-            cur.execute(buffer)
-    
-    con = sqlite3.connect(DB_FILE)
-cur = con.cursor()
-cur.execute('''
-        create table people
-        (
-          name_last      varchar(20),
-          age            integer
-        )
-        ''')
-    
-    # A shorter title for the navigation bar.  Default is the same as
-# html_title.
-#html_short_title = None
-    
-    # Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
-
-    
-    for i, face_distance in enumerate(face_distances):
-    print('The test image has a distance of {:.2} from known image #{}'.format(face_distance, i))
-    print('- With a normal cutoff of 0.6, would the test image match the known image? {}'.format(face_distance < 0.6))
-    print('- With a very strict cutoff of 0.5, would the test image match the known image? {}'.format(face_distance < 0.5))
-    print()
-
-    
-        unknown_encodings = face_recognition.face_encodings(unknown_image)
-    
-        # Apply some eyeliner
-    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    
-    
-def contains(name):
-    '''Determine if the dataset is in the catalog.'''
-    return name in _DATASETS.keys()
-    
-        'Flipping' an entry means that that image and associated metadata (e.g.,
-    ground truth boxes and object proposals) are horizontally flipped.
-    '''
-    flipped_roidb = []
-    for entry in roidb:
-        width = entry['width']
-        boxes = entry['boxes'].copy()
-        oldx1 = boxes[:, 0].copy()
-        oldx2 = boxes[:, 2].copy()
-        boxes[:, 0] = width - oldx2 - 1
-        boxes[:, 2] = width - oldx1 - 1
-        assert (boxes[:, 2] >= boxes[:, 0]).all()
-        flipped_entry = {}
-        dont_copy = ('boxes', 'segms', 'gt_keypoints', 'flipped')
-        for k, v in entry.items():
-            if k not in dont_copy:
-                flipped_entry[k] = v
-        flipped_entry['boxes'] = boxes
-        flipped_entry['segms'] = segm_utils.flip_segms(
-            entry['segms'], entry['height'], entry['width']
-        )
-        if dataset.keypoints is not None:
-            flipped_entry['gt_keypoints'] = keypoint_utils.flip_keypoints(
-                dataset.keypoints, dataset.keypoint_flip_map,
-                entry['gt_keypoints'], entry['width']
-            )
-        flipped_entry['flipped'] = True
-        flipped_roidb.append(flipped_entry)
-    roidb.extend(flipped_roidb)
-    
-    
-def VGG_CNN_M_1024_rpn_frozen_features(model):
-    return build_generic_detection_model(
-        model,
-        VGG_CNN_M_1024.add_VGG_CNN_M_1024_conv5_body,
-        freeze_conv_body=True
-    )
-    
-    from detectron.core.config import cfg
-from detectron.modeling.generate_anchors import generate_anchors
-from detectron.utils.c2 import const_fill
-from detectron.utils.c2 import gauss_fill
-import detectron.modeling.FPN as FPN
-import detectron.utils.blob as blob_utils
-    
-        # rois are in [[batch_idx, x0, y0, x1, y2], ...] format
-    # Combine predictions across all levels and retain the top scoring
-    rois = np.concatenate([blob.data for blob in roi_inputs])
-    scores = np.concatenate([blob.data for blob in score_inputs]).squeeze()
-    inds = np.argsort(-scores)[:post_nms_topN]
-    rois = rois[inds, :]
-    return rois
-    
-        def forward(self, inputs, outputs):
-        '''See modeling.detector.GenerateProposalLabels for inputs/outputs
-        documentation.
-        '''
-        # During training we reuse the data loader code. We populate roidb
-        # entries on the fly using the rois generated by RPN.
-        # im_info: [[im_height, im_width, im_scale], ...]
-        rois = inputs[0].data
-        roidb = blob_utils.deserialize(inputs[1].data)
-        im_info = inputs[2].data
-        im_scales = im_info[:, 2]
-        output_blob_names = fast_rcnn_roi_data.get_fast_rcnn_blob_names()
-        # For historical consistency with the original Faster R-CNN
-        # implementation we are *not* filtering crowd proposals.
-        # This choice should be investigated in the future (it likely does
-        # not matter).
-        json_dataset.add_proposals(roidb, rois, im_scales, crowd_thresh=0)
-        roidb_utils.add_bbox_regression_targets(roidb)
-        blobs = {k: [] for k in output_blob_names}
-        fast_rcnn_roi_data.add_fast_rcnn_blobs(blobs, im_scales, roidb)
-        for i, k in enumerate(output_blob_names):
-            blob_utils.py_op_copy_blob(blobs[k], outputs[i])
-
-    
-        clss = bbox_target_data[:, 0]
-    bbox_targets = blob_utils.zeros((clss.size, 4 * num_bbox_reg_classes))
-    bbox_inside_weights = blob_utils.zeros(bbox_targets.shape)
-    inds = np.where(clss > 0)[0]
-    for ind in inds:
-        cls = int(clss[ind])
-        start = 4 * cls
-        end = start + 4
-        bbox_targets[ind, start:end] = bbox_target_data[ind, 1:]
-        bbox_inside_weights[ind, start:end] = (1.0, 1.0, 1.0, 1.0)
-    return bbox_targets, bbox_inside_weights
-    
-        blobs['keypoint_rois'] = sampled_fg_rois
-    blobs['keypoint_locations_int32'] = heats.astype(np.int32, copy=False)
-    blobs['keypoint_weights'] = weights
+            self.assertEqual(len(signature.errors), 0)
+        has_mac = expected.pop('has_mac', False)
+        for k, v in expected.iteritems():
+            got = getattr(signature, k)
+            self.assertEqual(got, v, 'signature.%s: %s != %s' % (k, got, v))
