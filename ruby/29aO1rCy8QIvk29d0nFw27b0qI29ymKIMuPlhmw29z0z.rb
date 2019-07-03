55bf3563e826e7de527ae9ee69ad0fd9b8c80a7c
SUNsung <weatherius@gmail.com>
2019-07-03 16:46:51 +0300

    Rewrite [29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb] File in: 2019-07-03 16:46:50

--- ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
+++ ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
@@ -1,88 +1,91 @@
 
         
-                  def hidden_field_for_checkbox(options)
-            @unchecked_value ? tag('input', options.slice('name', 'disabled', 'form').merge!('type' => 'hidden', 'value' => @unchecked_value)) : ''.html_safe
+        if pathutil_relative == native_relative
+  Benchmark.ips do |x|
+    x.report('pathutil') { pathutil_relative }
+    x.report('native')   { native_relative }
+    x.compare!
+  end
+else
+  print 'PATHUTIL: '
+  puts pathutil_relative
+  print 'NATIVE:   '
+  puts native_relative
+end
+
+    
+        def process(args)
+      arg_is_present? args, '--server', 'The --server command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
+                          use the 'build' subcommand.'
+      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
+                          '--watch'.'
+      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
+                          the '--watch' switch.'
+      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
+                          favour of 'highlighter'.'
+      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
+                          your config files.'
+      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
+                          config files.'
+      no_subcommand(args)
+    end
+    
+        return str
+  end
+    
+    end
+end
+end
+
+    
+        # Convert a-law into signed PCM
+    when IAX_CODEC_G711_ALAW
+      Rex::Proto::IAX2::Codecs::ALaw.decode(buff)
+    
+                res = checksum + encrypted
+            res
           end
+        end
       end
     end
   end
 end
-
     
-              def label(label_html_options = {}, &block)
-            html_options = @input_html_options.slice(:index, :namespace).merge(label_html_options)
-            html_options[:for] ||= @input_html_options[:id] if @input_html_options[:id]
+              # Encodes the type field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_type
+            bn = OpenSSL::BN.new(type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
     
-            def render(&block)
-          render_collection_for(RadioButtonBuilder, &block)
-        end
-    
-              def default_datetime(options)
-            return if options[:include_blank] || options[:prompt]
-    
-              content_is_options = content_or_options.is_a?(Hash)
-          if content_is_options
-            options.merge! content_or_options
-            @content = nil
-          else
-            @content = content_or_options
+              # Decodes the type from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_type(input)
+            input.value[0].value.to_i
           end
     
-    module ActionView
-  module Helpers
-    module Tags # :nodoc:
-      class TextField < Base # :nodoc:
-        include Placeholderable
+              # Encodes the etype
+          #
+          # @return [OpenSSL::ASN1::Sequence]
+          def encode_etype
+            encoded_types = []
+            etype.each do |member|
+              bn = OpenSSL::BN.new(member.to_s)
+              int = OpenSSL::ASN1::Integer.new(bn)
+              encoded_types << int
+            end
     
-        def log_processed(name)
-      puts green '    #{name}'
+                decode_asn1(asn1)
+          end
+    
+      describe '.some_class_method' do
+    it 'does something' do
+      # ...
     end
-    
-      # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-    
-      describe '#new' do
-    before do
-      sign_in alice, scope: :user
-    end
-    
-          it 'should not be catched when it is unexpected' do
-        @target = alice.post :status_message, text: 'AWESOME', to: @alices_aspect.id
-        allow(alice).to receive(:like!).and_raise('something')
-        allow(@controller).to receive(:current_user).and_return(alice)
-        expect { post :create, params: like_hash, format: :json }.to raise_error('something')
-      end
-    end
-  end
-    
-          it 'it calls toggle_hidden_shareable' do
-        expect(@controller.current_user).to receive(:toggle_hidden_shareable).with(an_instance_of(StatusMessage))
-        put :update, params: {id: 42, post_id: @status.id}, format: :js
-      end
-    end
-    
-      # Array of configuration files
-  attr_accessor :config_files
-    
-            # If it's not a null record, do extension hash.
-        if(typeflag != '\0')
-          extension_header = header.dup()
-    
-        pkgdata = {
-      'arch' => architecture,
-      'name' => name,
-      'version' => pkg_version,
-      'comment' => description,
-      'desc' => description,
-      'origin' => pkg_origin,
-      'maintainer' => maintainer,
-      'www' => url,
-      # prefix is required, but it doesn't seem to matter
-      'prefix' => '/',
-    }
-    
-        # Make one file. The installscript can unpack itself.
-    `cat #{install_script} #{payload} > #{output_path}`
-    FileUtils.chmod('+x', output_path)
   end
