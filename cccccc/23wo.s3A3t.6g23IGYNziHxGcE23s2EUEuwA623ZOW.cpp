4c66c460f249c8239b7107933c5708e06288577a
SUNsung <weatherius@gmail.com>
2019-07-04 01:29:03 +0300

    Rewrite [23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp] File in: 2019-07-04 01:29:03

--- cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
+++ cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
@@ -1,217 +1,303 @@
 
         
-        
-bool ZeroCopyOutputStream::WriteAliasedRaw(const void* /* data */,
-                                           int /* size */) {
-  GOOGLE_LOG(FATAL) << 'This ZeroCopyOutputStream doesn't support aliasing. '
-                'Reaching here usually means a ZeroCopyOutputStream '
-                'implementation bug.';
-  return false;
+        CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
+  return new DefaultCache(CBs);
 }
     
-    TEST(TemplateUtilTest, TestTemplateTypeEquals) {
-  // Check that the TemplateTypeEquals works correctly.
-  bool value = false;
+    void CacheImpl::removeAll() {
+  cache_remove_all(static_cast<cache_t*>(Impl));
+}
+    
+      if (!wasInline) delete[] oldBegin;
+    
+    namespace {
+  // Quasi-lexicographic order: string length first, then string data.
+  // Since we don't care about the actual length, we can use this, which
+  // lets us ignore the string data a larger proportion of the time.
+  struct SortByLengthComparator {
+    bool operator()(StringRef lhs, StringRef rhs) const {
+      return (lhs.size() < rhs.size() ||
+              (lhs.size() == rhs.size() && lhs < rhs));
     }
+  };
+} // end anonymous namespace
     
-    
-    {
-    {    return true;
+      static CFPointeeInfo forRecord(bool isConst, const clang::RecordDecl *decl) {
+    assert(decl);
+    CFPointeeInfo info;
+    info.IsValid = true;
+    info.IsConst = isConst;
+    info.Decl = decl;
+    return info;
   }
- private:
-  bool CanGenerate(const FileDescriptor* file) const {
-    if (GetPool()->FindFileByName(file->name()) != nullptr) {
-      return false;
-    }
-    for (int j = 0; j < file->dependency_count(); j++) {
-      if (GetPool()->FindFileByName(file->dependency(j)->name()) == nullptr) {
-        return false;
-      }
-    }
-    for (int j = 0; j < file->public_dependency_count(); j++) {
-      if (GetPool()->FindFileByName(
-          file->public_dependency(j)->name()) == nullptr) {
-        return false;
-      }
-    }
-    for (int j = 0; j < file->weak_dependency_count(); j++) {
-      if (GetPool()->FindFileByName(
-          file->weak_dependency(j)->name()) == nullptr) {
-        return false;
-      }
-    }
-    return true;
+    
+    /// Translate the given operator character into its mangled form.
+///
+/// Current operator characters:   @/=-+*%<>!&|^~ and the special operator '..'
+char Mangle::translateOperatorChar(char op) {
+  switch (op) {
+    case '&': return 'a'; // 'and'
+    case '@': return 'c'; // 'commercial at sign'
+    case '/': return 'd'; // 'divide'
+    case '=': return 'e'; // 'equal'
+    case '>': return 'g'; // 'greater'
+    case '<': return 'l'; // 'less'
+    case '*': return 'm'; // 'multiply'
+    case '!': return 'n'; // 'negate'
+    case '|': return 'o'; // 'or'
+    case '+': return 'p'; // 'plus'
+    case '?': return 'q'; // 'question'
+    case '%': return 'r'; // 'remainder'
+    case '-': return 's'; // 'subtract'
+    case '~': return 't'; // 'tilde'
+    case '^': return 'x'; // 'xor'
+    case '.': return 'z'; // 'zperiod' (the z is silent)
+    default:
+      return op;
   }
-};
+}
     
-    #include <string>
-#include <set>
-#include <vector>
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/io/printer.h>
+    bool ArgsToFrontendInputsConverter::readInputFilesFromCommandLine() {
+  bool hadDuplicates = false;
+  for (const Arg *A :
+       Args.filtered(options::OPT_INPUT, options::OPT_primary_file)) {
+    hadDuplicates = addFile(A->getValue()) || hadDuplicates;
+  }
+  return false; // FIXME: Don't bail out for duplicates, too many tests depend
+  // on it.
+}
+    
+      /// Returns the newly set-up FrontendInputsAndOutputs, as well as a set of
+  /// any unused primary files (those that do not correspond to an input).
+  std::pair<FrontendInputsAndOutputs, std::set<StringRef>>
+  createInputFilesConsumingPrimaries(std::set<StringRef> primaryFiles);
+    
+        f64 dotProduct(const Size2D &size,
+                   const f32 * src0Base, ptrdiff_t src0Stride,
+                   const f32 * src1Base, ptrdiff_t src1Stride);
+    
+        struct Size2D {
+        Size2D() : width(0), height(0) {}
+        Size2D(size_t width_, size_t height_) : width(width_), height(height_) {}
+    }
+    
+    struct BitwiseAnd
+{
+    typedef u8 type;
+    }
+    
+            //tail
+        if (lookRight == 0 || i != width)
+        {
+            uint8x8_t tail0 = vld1_u8(src + (width - 9));//can't get left 1 pixel another way if width==8*k+1
+            uint8x8_t tail2 = vtbl1_u8(vld1_u8(src + (width - 8 + lookRight)), vtmask);
+            uint8x8_t tail1 = vext_u8(vreinterpret_u8_u64(vshl_n_u64(vreinterpret_u64_u8(tail0), 8*6)), tail2, 7);
+    }
     
     
     {
+    {        for (; sj < size.width; ++sj, syj += 2, dj += 4)
+        {
+            dst[dj] = srcy[syj];
+            dst[dj + 1] = srcu[sj];
+            dst[dj + 2] = srcy[syj + 1];
+            dst[dj + 3] = srcv[sj];
+        }
+    }
+#else
+    (void)size;
+    (void)srcyBase;
+    (void)srcyStride;
+    (void)srcuBase;
+    (void)srcuStride;
+    (void)srcvBase;
+    (void)srcvStride;
+    (void)dstBase;
+    (void)dstStride;
+#endif
+}
+    
+    #ifdef CAROTENE_NEON
+    if (shift >= 16)
     {
-    {  /*!
-   * \brief Worker threads.
-   */
-  std::vector<std::thread> worker_threads_;
-  /*!
-   * \brief Startup synchronization objects
-   */
-  std::list<std::shared_ptr<dmlc::ManualEvent>> ready_events_;
-  /*!
-   * \brief Disallow default construction.
-   */
-  ThreadPool() = delete;
-  /*!
-   * \brief Disallow copy construction and assignment.
-   */
-  DISALLOW_COPY_AND_ASSIGN(ThreadPool);
-};
-}  // namespace engine
-}  // namespace mxnet
-#endif  // MXNET_ENGINE_THREAD_POOL_H_
+        if (cpolicy == CONVERT_POLICY_WRAP)
+        {
+            size_t roiw16 = size.width >= 15 ? size.width - 15 : 0;
+            size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+            int16x8_t v_zero = vdupq_n_s16(0);
+    }
+    }
+    
+    
+    {} // namespace CAROTENE_NS
 
     
-    namespace mxnet {
-namespace io {
-/*!
- * \brief OpenCV based Image augmenter,
- *  The augmenter can contain internal temp state.
- */
-class ImageAugmenter {
- public:
-  /*!
-   *  \brief Initialize the Operator by setting the parameters
-   *  This function need to be called before all other functions.
-   *  \param kwargs the keyword arguments parameters
-   */
-  virtual void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) = 0;
-  /*!
-   * \brief augment src image.
-   *   this function is not thread safe, and will only be called by one thread
-   *   however, it will tries to re-use memory space as much as possible
-   * \param src the source image
-   * \param prnd pointer to random number generator.
-   * \return The processed image.
-   */
-  virtual cv::Mat Process(const cv::Mat &src, std::vector<float> *label,
-                          common::RANDOM_ENGINE *prnd) = 0;
-  // virtual destructor
-  virtual ~ImageAugmenter() {}
-  /*!
-   * \brief factory function
-   * \param name Name of the augmenter
-   * \return The created augmenter.
-   */
-  static ImageAugmenter* Create(const std::string& name);
-};
-    }
+                vst1_u8((u8 *)&mask, vmovn_u16(vcombine_u16(vmovn_u32(v_mask0), vmovn_u32(v_mask1))));
+    
+                    __asm__ __volatile__ (
+                    'vld4.32 {d0, d2, d4, d6}, [%[in0]]!                      \n\t'
+                    'vld4.32 {d1, d3, d5, d7}, [%[in0]]                       \n\t'
+                    'vld4.32 {d8, d10, d12, d14}, [%[in4]]!                   \n\t'
+                    'vld4.32 {d9, d11, d13, d15}, [%[in4]]                    \n\t'
+                    'vadd.i32 q0, q4                                          \n\t'
+                    'vadd.i32 q1, q5                                          \n\t'
+                    'vadd.i32 q2, q6                                          \n\t'
+                    'vadd.i32 q3, q7                                          \n\t'
+                    'vld4.32 {d16, d18, d20, d22}, [%[in1]]!                  \n\t'
+                    'vld4.32 {d17, d19, d21, d23}, [%[in1]]                   \n\t'
+                    'vld4.32 {d8, d10, d12, d14}, [%[in3]]!                   \n\t'
+                    'vld4.32 {d9, d11, d13, d15}, [%[in3]]                    \n\t'
+                    'vadd.i32 q4, q8                                          \n\t'
+                    'vadd.i32 q5, q9                                          \n\t'
+                    'vadd.i32 q6, q10                                         \n\t'
+                    'vadd.i32 q7, q11                                         \n\t'
+                    'vld4.32 {d16, d18, d20, d22}, [%[in2],:256]              \n\t'
+                    'vld4.32 {d17, d19, d21, d23}, [%[in22],:256]             \n\t'
+                    'vmla.i32 q0, q4, %q[c4]                                  \n\t'
+                    'vmla.i32 q1, q5, %q[c4]                                  \n\t'
+                    'vmla.i32 q2, q6, %q[c4]                                  \n\t'
+                    'vmla.i32 q3, q7, %q[c4]                                  \n\t'
+                    'vmla.i32 q0, q8, %q[c6]                                  \n\t'
+                    'vmla.i32 q1, q9, %q[c6]                                  \n\t'
+                    'vmla.i32 q2, q10, %q[c6]                                 \n\t'
+                    'vmla.i32 q3, q11, %q[c6]                                 \n\t'
+                    'vrshrn.i32 d8, q0, #8                                    \n\t'
+                    'vrshrn.i32 d9, q1, #8                                    \n\t'
+                    'vrshrn.i32 d10, q2, #8                                   \n\t'
+                    'vrshrn.i32 d11, q3, #8                                   \n\t'
+                   'vst4.16 {d8-d11}, [%[out]]                                \n\t'
+                    : [in0] '=r' (lidx0),
+                      [in1] '=r' (lidx1),
+                      [in3] '=r' (lidx3),
+                      [in4] '=r' (lidx4)
+                    : [out] 'r' (dst + x),
+                      '0' (lidx0),
+                      '1' (lidx1),
+                      '2' (lidx3),
+                      '3' (lidx4),
+                      [in2] 'r' (lane + x),
+                      [in22] 'r' (lane + x + 4*2),
+                      [c4] 'w' (vc4s32), [c6] 'w' (vc6s32)
+                    : 'd0','d1','d2','d3','d4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23'
+                );
+*/
+            for (; x <= colsn - 4; x += 4)
+            {
+                internal::prefetch(lane + x);
     }
     
-        dword(xfx_formater.instruction);
+    #include <carotene/definitions.hpp>
+    
+    // ##########################################################
+    
+    workspace.ResetWorkspace()
+    
+    <details>
+    
+    namespace {
+float sigmoid(const float x) {
+  if (x >= 0) {
+    return 1. / (1. + exp(-x));
+  } else {
+    const float exp_x = exp(x);
+    return exp_x / (1 + exp_x);
   }
-  void EmitXFXForm(const uint8_t op,
-                   const RegNumber rs,
-                   const uint16_t mask,
-                   const uint16_t xo,
-                   const uint8_t rsv = 0) {
+}
+} // namespace
     
-      // @todo: check allow_url_include?
+    namespace caffe2 {
+    }
+    
+    void CensusClientCallData::OnDoneRecvTrailingMetadataCb(void* user_data,
+                                                        grpc_error* error) {
+  grpc_call_element* elem = reinterpret_cast<grpc_call_element*>(user_data);
+  CensusClientCallData* calld =
+      reinterpret_cast<CensusClientCallData*>(elem->call_data);
+  GPR_ASSERT(calld != nullptr);
+  if (error == GRPC_ERROR_NONE) {
+    GPR_ASSERT(calld->recv_trailing_metadata_ != nullptr);
+    FilterTrailingMetadata(calld->recv_trailing_metadata_,
+                           &calld->elapsed_time_);
+  }
+  GRPC_CLOSURE_RUN(calld->initial_on_done_recv_trailing_metadata_,
+                   GRPC_ERROR_REF(error));
+}
+    
+     private:
+  // Parses the next field from the incoming buffer and stores the parsed value
+  // in a GrpcTraceContext struct.  If it does not recognize the field ID it
+  // will return 0, otherwise it returns the number of bytes read.
+  static size_t ParseField(absl::string_view buf, GrpcTraceContext* tc) {
+    // TODO: Add support for multi-byte field IDs.
+    if (buf.empty()) {
+      return 0;
+    }
+    // Field ID is always the first byte in a field.
+    uint32_t field_id = buf[0];
+    size_t bytes_read = kFieldIdSize;
+    switch (field_id) {
+      case kTraceIdField:
+        bytes_read += kTraceIdSize;
+        if (bytes_read > buf.size()) {
+          return 0;
+        }
+        memcpy(tc->trace_id, &buf[kFieldIdSize],
+               opencensus::trace::TraceId::kSize);
+        break;
+      case kSpanIdField:
+        bytes_read += kSpanIdSize;
+        if (bytes_read > buf.size()) {
+          return 0;
+        }
+        memcpy(tc->span_id, &buf[kFieldIdSize],
+               opencensus::trace::SpanId::kSize);
+        break;
+      case kTraceOptionsField:
+        bytes_read += kTraceOptionsSize;
+        if (bytes_read > buf.size()) {
+          return 0;
+        }
+        memcpy(tc->trace_options, &buf[kFieldIdSize],
+               opencensus::trace::TraceOptions::kSize);
+        break;
+      default:  // Invalid field ID
+        return 0;
+    }
+    }
+    
+    #ifdef GPR_APPLE
+    
+    const std::set<PerBalancerStore*>* PerHostStore::GetAssignedStores(
+    const grpc::string& lb_id) const {
+  auto it = assigned_stores_.find(lb_id);
+  if (it == assigned_stores_.end()) return nullptr;
+  return &(it->second);
+}
+    
+    TEST(MemEnvTest, LargeWrite) {
+  const size_t kWriteSize = 300 * 1024;
+  char* scratch = new char[kWriteSize * 2];
+    }
     
     
-    {  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
+    { private:
+  const FilterPolicy* policy_;
+  const char* data_;    // Pointer to filter data (at block-start)
+  const char* offset_;  // Pointer to beginning of offset array (at block-end)
+  size_t num_;          // Number of entries in offset array
+  size_t base_lg_;      // Encoding parameter (see kFilterBaseLg in .cc file)
 };
     
-    #include 'hphp/runtime/base/type-string.h'
-    
-    const StaticString
-  s_wrapper_type('wrapper_type'),
-  s_stream_type('stream_type'),
-  s_mode('mode'),
-  s_unread_bytes('unread_bytes'),
-  s_seekable('seekable'),
-  s_timed_out('timed_out'),
-  s_blocked('blocked'),
-  s_eof('eof'),
-  s_plainfile('plainfile'),
-  s_dir('dir'),
-  s_r('r');
-    
-    req::ptr<Directory> GlobStreamWrapper::opendir(const String& path) {
-  const char* prefix = 'glob://';
-  const char* path_str = path.data();
-  int path_len = path.length();
+    namespace leveldb {
     }
     
-    /**
- * For php://output, a simple wrapper of g_context->out().
- */
-struct OutputFile : File {
-  DECLARE_RESOURCE_ALLOCATION(OutputFile);
+      void DumpFilter() {
+    fprintf(stderr, 'F(');
+    for (size_t i = 0; i + 1 < filter_.size(); i++) {
+      const unsigned int c = static_cast<unsigned int>(filter_[i]);
+      for (int j = 0; j < 8; j++) {
+        fprintf(stderr, '%c', (c & (1 << j)) ? '1' : '.');
+      }
     }
-    
-      void setProxy(const String& proxy_host, int proxy_port,
-                const String& proxy_user, const String& proxy_pass);
-  bool open(const String& filename, const String& mode) override;
-  int64_t writeImpl(const char *buffer, int64_t length) override;
-  bool seekable() override { return false; }
-  bool flush() override;
-  Variant getWrapperMetaData() override { return Variant(m_responseHeaders); }
-  String getLastError();
-    
-    bool ImGui::VSliderScalar(const char* label, const ImVec2& size, ImGuiDataType data_type, void* v, const void* v_min, const void* v_max, const char* format, float power)
-{
-    ImGuiWindow* window = GetCurrentWindow();
-    if (window->SkipItems)
-        return false;
-    }
-    
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'CIwTexture*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-    
-    // InitXXX function with 'install_callbacks=true': install GLFW callbacks. They will call user's previously installed callbacks, if any.
-// InitXXX function with 'install_callbacks=false': do not install GLFW callbacks. You will need to call them yourself from your own GLFW callbacks.
-IMGUI_IMPL_API void     ImGui_ImplGlfw_MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_CharCallback(GLFWwindow* window, unsigned int c);
-
-    
-        // Setup Platform/Renderer bindings
-    ImGui_ImplAllegro5_Init(display);
-    
-        // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    
-    bool ImGui_ImplDX9_Init(IDirect3DDevice9* device)
-{
-    // Setup back-end capabilities flags
-    ImGuiIO& io = ImGui::GetIO();
-    io.BackendRendererName = 'imgui_impl_dx9';
-    io.BackendFlags |= ImGuiBackendFlags_RendererHasVtxOffset;  // We can honor the ImDrawCmd::VtxOffset field, allowing for large meshes.
-    }
-    
-        // Backup GL state
-    GLint last_texture; glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
-    GLint last_polygon_mode[2]; glGetIntegerv(GL_POLYGON_MODE, last_polygon_mode);
-    GLint last_viewport[4]; glGetIntegerv(GL_VIEWPORT, last_viewport);
-    GLint last_scissor_box[4]; glGetIntegerv(GL_SCISSOR_BOX, last_scissor_box);
-    glPushAttrib(GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT | GL_TRANSFORM_BIT);
-    
-        // Setup viewport:
-    {
-        VkViewport viewport;
-        viewport.x = 0;
-        viewport.y = 0;
-        viewport.width = (float)fb_width;
-        viewport.height = (float)fb_height;
-        viewport.minDepth = 0.0f;
-        viewport.maxDepth = 1.0f;
-        vkCmdSetViewport(command_buffer, 0, 1, &viewport);
-    }
+    fprintf(stderr, ')\n');
+  }
