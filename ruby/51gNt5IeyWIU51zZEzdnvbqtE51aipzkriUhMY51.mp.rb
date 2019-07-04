a77f5062c58e76e5de8ba397151596e5df47bef8
SUNsung <weatherius@gmail.com>
2019-07-04 22:58:06 +0300

    Rewrite [51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb] File in: 2019-07-04 22:58:05

--- ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
+++ ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
@@ -1,50 +1,94 @@
 
         
-            SPLIT_INTS = /(?<=\d)\.(?=[\s\d])/.freeze
+            remove_duplicates
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
     
-        attr_reader :filters
-    
-        def type=(value)
-      @type = value.try :strip
+      def up_down(change)
+    change.up do
+      Mention.update_all(mentions_container_type: 'Post')
+      change_column :mentions, :mentions_container_type, :string, null: false
+      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
     end
     
-        def error?
-      code == 0 || code != 404 && code != 403 && code >= 400 && code <= 599
+    describe ContactsController, :type => :controller do
+  describe '#index' do
+    before do
+      AppConfig.chat.enabled = true
+      @aspect = bob.aspects.create(:name => 'another aspect')
+      bob.share_with alice.person, @aspect
+      bob.share_with eve.person, @aspect
+      sign_in bob, scope: :user
     end
     
-            css('h1 + code').each do |node|
-          node.before('<p></p>')
-          while node.next_element.name == 'code'
-            node.previous_element << ' '
-            node.previous_element << node.next_element
+        it 'returns an empty array for a post with no likes' do
+      get :index, params: {post_id: @message.id}
+      expect(JSON.parse(response.body)).to eq([])
+    end
+    
+        it 'supports a limit per_page parameter' do
+      2.times { FactoryGirl.create(:notification, :recipient => alice, :target => @post) }
+      get :index, params: {per_page: 2}
+      expect(assigns[:notifications].count).to eq(2)
+    end
+    
+      describe '#destroy' do
+    let(:post) { FactoryGirl.create(:status_message) }
+    
+      describe '#create' do
+    it 'redirects to /stream for a non-mobile user' do
+      post :create, params: {user: {remember_me: '0', username: @user.username, password: 'evankorth'}}
+      expect(response).to be_redirect
+      expect(response.location).to match /^#{stream_url}\??$/
+    end
+    
+      opts.on('-r', '--redis [HOST:PORT]', 'Redis connection string') do |host|
+    Resque.redis = host
+  end
+    
+    # Here's our ActiveRecord class
+class Repository < ActiveRecord::Base
+  # This will be called by a worker when a job needs to be processed
+  def self.perform(id, method, *args)
+    find(id).send(method, *args)
+  end
+    
+          # When a job fails, a new instance of your Failure backend is created
+      # and #save is called.
+      #
+      # This is where you POST or PUT or whatever to your Failure service.
+      def save
+      end
+    
+          def self.count(queue = nil, class_name = nil)
+        if queue
+          if class_name
+            n = 0
+            each(0, count(queue), queue, class_name) { n += 1 }
+            n
+          else
+            data_store.num_failed(queue).to_i
           end
-          node.previous_element.prepend_child(node)
+        else
+          total = 0
+          queues.each { |q| total += count(q) }
+          total
         end
-    
-          def include_default_entry?
-        INDEX.add?([name, type].join(';')) ? true : false # ¯\_(ツ)_/¯
       end
     
-    post '/' do
-  connections.each { |out| out << 'data: #{params[:msg]}\n\n' }
-  204 # response without entity body
-end
-    
-          def has_vector?(request, headers)
-        return false if request.xhr?
-        return false if options[:allow_if] && options[:allow_if].call(request.env)
-        return false unless headers['Content-Type'].to_s.split(';', 2).first =~ /^\s*application\/json\s*$/
-        origin(request.env).nil? and referrer(request.env) != request.host
+          failure_hooks(plugin).each do |hook|
+        if hook.to_s.end_with?('failure')
+          raise LintError, '#{plugin}.#{hook} is not namespaced'
+        end
       end
-    
-        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      subject.options[:allow_empty_referrer] = false
-      expect(subject.referrer(env)).to be_nil
     end
     
-          run DummyApp
-    end
+      def failed_size
+    @failed_size ||= Resque::Failure.count(params[:queue], params[:class])
+  end
     
-      # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+      def assert_exception_caught(result)
+    refute_nil result
+    assert !result.start_with?('Finished Normally'), 'Job Finished normally.  (sleep parameter to LongRunningJob not long enough?)'
+    assert result.start_with?('Caught TermException'), 'TermException exception not raised in child.'
+  end
