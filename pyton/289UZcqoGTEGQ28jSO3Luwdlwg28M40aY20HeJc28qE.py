ea226fba21e959e8c63334bb50304317ba731f30
SUNsung <weatherius@gmail.com>
2019-07-04 08:27:25 +0300

    Rewrite [289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py] File in: 2019-07-04 08:27:24

--- pyton/289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py
+++ pyton/289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py
@@ -1,91 +1,38 @@
 
         
-            READY = 0
-    IN_PROGRESS = 1
-    COMPLETE = 2
+            Validates that the username is not already taken. Hashes the
+    password for security.
+    '''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
     
-        def __init__(self):
-        self.head = None
-        self.tail = None
+        with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
     
+        # login request set the user_id in the session
+    # check that the user is loaded from the session
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == 'test'
     
-class PagesDataStore(object):
-    
-            if self.values[key] is None:
-            self._set_value(key, data)
-    
-    The problem is :
-Given two strings A and B. Find the minimum number of operations to string B such that A = B. The permitted operations are removal,  insertion, and substitution.
-'''
-from __future__ import print_function
-    
-        return diff
-
-    
-        def train(self, X, y):
-        '''
-        train:
-        @param X: a one dimensional numpy array
-        @param y: a one dimensional numpy array. 
-        The contents of y are the labels for the corresponding X values
-    
-      3. find centroids and clusters using kmeans function.
-  
-        centroids, cluster_assignment = kmeans(
-            X, 
-            k, 
-            initial_centroids, 
-            maxiter=400,
-            record_heterogeneity=heterogeneity, 
-            verbose=True # whether to print logs in console or not.(default=False)
+            if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)',
+                (title, body, g.user['id']),
             )
-  
-  
-  4. Plot the loss function, hetrogeneity values for every iteration saved in hetrogeneity list.
-        plot_heterogeneity(
-            heterogeneity, 
-            k
-        )
-  
-  5. Have fun..
-  
-'''
-from __future__ import print_function
-from sklearn.metrics import pairwise_distances
-import numpy as np
+            db.commit()
+            return redirect(url_for('blog.index'))
     
-        return res
     
-    # add noise
-y += 0.01 * np.random.normal((n_samples,))
+class TagTuple(JSONTag):
+    __slots__ = ()
+    key = ' t'
     
-    Sentiment analysis can be casted as a binary text classification problem,
-that is fitting a linear classifier on features extracted from the text
-of the user messages so as to guess wether the opinion of the author is
-positive or negative.
-    
-    Sentiment analysis can be casted as a binary text classification problem,
-that is fitting a linear classifier on features extracted from the text
-of the user messages so as to guess wether the opinion of the author is
-positive or negative.
-    
-        plt.imshow(avg_dist, interpolation='nearest', cmap=plt.cm.gnuplot2,
-               vmin=0)
-    plt.xticks(range(n_clusters), labels, rotation=45)
-    plt.yticks(range(n_clusters), labels)
-    plt.colorbar()
-    plt.suptitle('Interclass %s distances' % metric, size=18)
-    plt.tight_layout()
-    
-    # Code source: Gaël Varoquaux
-# Modified for documentation by Jaques Grobler
-# License: BSD 3 clause
-    
-    identic = np.logical_not(different)
-ax.plot(X[identic, 0], X[identic, 1], 'w',
-        markerfacecolor='#bbbbbb', marker='.')
-ax.plot(X[different, 0], X[different, 1], 'w',
-        markerfacecolor='m', marker='.')
-ax.set_title('Difference')
-ax.set_xticks(())
-ax.set_yticks(())
+        Implements cookie based sessions based on itsdangerous.
