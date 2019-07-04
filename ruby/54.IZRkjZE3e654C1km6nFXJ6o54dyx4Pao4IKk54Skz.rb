9be569da2134b83bfd88d872a60a533f6ce258b1
SUNsung <weatherius@gmail.com>
2019-07-04 13:16:57 +0300

    Rewrite [54.IZRkjZE3e654C1km6nFXJ6o54dyx4Pao4IKk54Skz.rb] File in: 2019-07-04 13:16:57

--- ruby/54.IZRkjZE3e654C1km6nFXJ6o54dyx4Pao4IKk54Skz.rb
+++ ruby/54.IZRkjZE3e654C1km6nFXJ6o54dyx4Pao4IKk54Skz.rb
@@ -1,34 +1,48 @@
-  def model_name
-    ActiveModel::Name.new(self.class)
-  end
-end
+
+        
+                # Download a file from the remote machine to the local machine.
+        #
+        # @param [String] from Path of the file on the remote machine.
+        # @param [String] to Path of where to save the file locally.
+        def download(from, to)
+        end
     
-        execute 'INSERT INTO share_visibilities (user_id, shareable_id, shareable_type) ' \
-            'SELECT post_visibility.user_id, photos.id, 'Photo' FROM photos ' \
-            'INNER JOIN posts ON posts.guid = photos.status_message_guid AND posts.type = 'StatusMessage' ' \
-            'LEFT OUTER JOIN share_visibilities ON share_visibilities.shareable_id = photos.id ' \
-            'INNER JOIN share_visibilities AS post_visibility ON post_visibility.shareable_id = posts.id ' \
-            'WHERE photos.public = false AND share_visibilities.shareable_id IS NULL ' \
-            'AND post_visibility.shareable_type = 'Post''
-  end
+            # The logger for this class.
+        LOGGER = Log4r::Logger.new('vagrant::plugin::v1::plugin')
     
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
+            # This returns all synced folder implementations.
+        #
+        # @return [Registry]
+        def synced_folders
+          Registry.new.tap do |result|
+            @registered.each do |plugin|
+              result.merge!(plugin.components.synced_folders)
+            end
+          end
+        end
+    
+        private
+    
+          PluginManager.ui.info('Install successful')
+    rescue ::Bundler::BundlerError => e
+      raise PluginManager::InstallError.new(e), 'An error occurred went installing plugins'
+    ensure
+      FileUtils.rm_rf(uncompressed_path) if uncompressed_path && Dir.exist?(uncompressed_path)
+    end
+    
+        puts('Updating #{filtered_plugins.collect(&:name).join(', ')}') unless filtered_plugins.empty?
+    
+          # Checks whether this case statement has an `else` branch.
+      #
+      # @return [Boolean] whether the `case` statement has an `else` branch
+      def else?
+        loc.else
+      end
     end
   end
 end
 
     
-    describe LikesController, type: :controller do
-  before do
-    @alices_aspect = alice.aspects.where(:name => 'generic').first
-    @bobs_aspect = bob.aspects.where(:name => 'generic').first
-    
-      describe '#create' do
-    it 'redirects to /stream for a non-mobile user' do
-      post :create, params: {user: {remember_me: '0', username: @user.username, password: 'evankorth'}}
-      expect(response).to be_redirect
-      expect(response.location).to match /^#{stream_url}\??$/
-    end
+          def below_array_length?(node)
+        node.values.length < min_size_config
+      end
