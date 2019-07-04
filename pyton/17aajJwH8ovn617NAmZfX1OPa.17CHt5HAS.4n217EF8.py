f95ff6d9df2e42731bcf1df2c579c58642bab934
SUNsung <weatherius@gmail.com>
2019-07-04 22:12:53 +0300

    Rewrite [17aajJwH8ovn617NAmZfX1OPa.17CHt5HAS.4n217EF8.py] File in: 2019-07-04 22:12:52

--- pyton/17aajJwH8ovn617NAmZfX1OPa.17CHt5HAS.4n217EF8.py
+++ pyton/17aajJwH8ovn617NAmZfX1OPa.17CHt5HAS.4n217EF8.py
@@ -1,147 +1,60 @@
 
         
-            good_nonce = jose.encode_b64jose(b'foo')
-    wrong_nonce = u'F'
-    # Following just makes sure wrong_nonce is wrong
-    try:
-        jose.b64decode(wrong_nonce)
-    except (ValueError, TypeError):
-        assert True
-    else:
-        assert False  # pragma: no cover
+                (2016-01, shopping), 125
+        (2016-01, gas), 50
+        '''
+        total = sum(values)
+        self.handle_budget_notifications(key, total)
+        yield key, sum(values)
+    
+    from mrjob.job import MRJob
+    
+        def get(self, query):
+        '''Get the stored query result from the cache.
+    
+        def iter_lines(self, chunk_size):
+        yield self.body, b''
+    
+    from httpie.input import SEP_CREDENTIALS
+from httpie.plugins import AuthPlugin, plugin_manager
+from utils import http, HTTP_OK
     
     
-UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-apache-conf-digest.txt'
-'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
+@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
+def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
+    r = http(follow_flag, httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 1
+    assert HTTP_OK in r
     
-            self.assertEqual(self.vhosts[5], self._call(self.vhosts))
+        def test_upload_ok(self, httpbin):
+        r = http('--form', '--verbose', 'POST', httpbin.url + '/post',
+                 'test-file@%s' % FILE_PATH_ARG, 'foo=bar')
+        assert HTTP_OK in r
+        assert 'Content-Disposition: form-data; name='foo'' in r
+        assert 'Content-Disposition: form-data; name='test-file';' \
+               ' filename='%s'' % os.path.basename(FILE_PATH) in r
+        assert FILE_CONTENT in r
+        assert ''foo': 'bar'' in r
+        assert 'Content-Type: text/plain' in r
     
-       certbot certonly \\
-     --dns-cloudxns \\
-     --dns-cloudxns-credentials ~/.secrets/certbot/cloudxns.ini \\
-     --dns-cloudxns-propagation-seconds 60 \\
-     -d example.com
-    
-        # The default request version.  This only affects responses up until
-    # the point where the request line is parsed, so it mainly decides what
-    # the client gets back when sending a malformed request line.
-    # Most web servers default to HTTP 0.9, i.e. don't send a status line.
-    default_request_version = 'HTTP/0.9'
-    
-    
-class Dummy_about_dialog():
-    # Dummy class for testing file display functions.
-    idle_credits = About.show_idle_credits
-    idle_readme = About.show_readme
-    idle_news = About.show_idle_news
-    # Called by the above
-    display_file_text = About.display_file_text
-    _utest = True
-    
-        def _generate_symbols(self, grammar_file, target_symbol_py_file):
-        proc = subprocess.Popen([sys.executable,
-                                 GEN_SYMBOL_FILE,
-                                 grammar_file,
-                                 target_symbol_py_file], stderr=subprocess.PIPE)
-        stderr = proc.communicate()[1]
-        return proc.returncode, stderr
+        @cookies.setter
+    def cookies(self, jar):
+        '''
+        :type jar: CookieJar
+        '''
+        # http://docs.python.org/2/library/cookielib.html#cookie-objects
+        stored_attrs = ['value', 'path', 'secure', 'expires']
+        self['cookies'] = {}
+        for cookie in jar:
+            self['cookies'][cookie.name] = {
+                attname: getattr(cookie, attname)
+                for attname in stored_attrs
+            }
     
     
-# Are two open files really referencing the same file?
-# (Not necessarily the same file descriptor!)
-def sameopenfile(fp1, fp2):
-    '''Test whether two open file objects reference the same file'''
-    s1 = os.fstat(fp1)
-    s2 = os.fstat(fp2)
-    return samestat(s1, s2)
+def image_files_in_folder(folder):
+    return [os.path.join(folder, f) for f in os.listdir(folder) if re.match(r'.*\.(jpg|jpeg|png)', f, flags=re.I)]
     
-    
-def _get_resource_reader(
-        package: ModuleType) -> Optional[resources_abc.ResourceReader]:
-    # Return the package's loader if it's a ResourceReader.  We can't use
-    # a issubclass() check here because apparently abc.'s __subclasscheck__()
-    # hook wants to create a weak reference to the object, but
-    # zipimport.zipimporter does not support weak references, resulting in a
-    # TypeError.  That seems terrible.
-    spec = package.__spec__
-    if hasattr(spec.loader, 'get_resource_reader'):
-        return cast(resources_abc.ResourceReader,
-                    spec.loader.get_resource_reader(spec.name))
-    return None
-    
-    
-def main():
-    parser = ArgumentParser(description='''\
-Send the contents of a directory as a MIME message.
-Unless the -o option is given, the email is sent by forwarding to your local
-SMTP server, which then does the normal delivery process.  Your local machine
-must be running an SMTP server.
-''')
-    parser.add_argument('-d', '--directory',
-                        help='''Mail the contents of the specified directory,
-                        otherwise use the current directory.  Only the regular
-                        files in the directory are sent, and we don't recurse to
-                        subdirectories.''')
-    parser.add_argument('-o', '--output',
-                        metavar='FILE',
-                        help='''Print the composed message to FILE instead of
-                        sending the message to the SMTP server.''')
-    parser.add_argument('-s', '--sender', required=True,
-                        help='The value of the From: header (required)')
-    parser.add_argument('-r', '--recipient', required=True,
-                        action='append', metavar='RECIPIENT',
-                        default=[], dest='recipients',
-                        help='A To: header value (at least one required)')
-    args = parser.parse_args()
-    directory = args.directory
-    if not directory:
-        directory = '.'
-    # Create the message
-    msg = EmailMessage()
-    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
-    msg['To'] = ', '.join(args.recipients)
-    msg['From'] = args.sender
-    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
-    
-    ##
-    
-    def pow3(x):
-    return x ** 3
-    
-        result = False
-    try:
-        ret = urlopen(url, timeout=2)
-        result = (ret.code == 200)
-    except HTTPError as e:
-        print(e, file=sys.stderr)
-    except URLError as e:
-        print(e, file=sys.stderr)
-    except timeout as e:
-        print(e, file=sys.stderr)
-    except Exception as e:
-        print(e, file=sys.stderr)
-    
-        async def doread(self, rfile):
-        self.is_reading = True
-        try:
-            await self.load_flows_from_path(ctx.options.rfile)
-        except exceptions.FlowReadException as e:
-            raise exceptions.OptionsError(e) from e
-        finally:
-            self.is_reading = False
-    
-            If only two clauses are specified, the pattern is set to match
-        universally (i.e. '.*'). Example:
-    
-    
-def ckey(attrs: Dict[str, str], f: http.HTTPFlow) -> TOrigin:
-    '''
-        Returns a (domain, port, path) tuple.
-    '''
-    domain = f.request.host
-    path = '/'
-    if 'domain' in attrs:
-        domain = attrs['domain']
-    if 'path' in attrs:
-        path = attrs['path']
-    return (domain, f.request.port, path)
+        # Apply some eyeliner
+    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
+    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
