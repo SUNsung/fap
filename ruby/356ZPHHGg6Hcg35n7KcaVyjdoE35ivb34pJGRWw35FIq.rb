5d99bf20f5f1e573813a7cb217e36e2d6a550d1c
SUNsung <weatherius@gmail.com>
2019-07-04 16:25:37 +0300

    Rewrite [356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb] File in: 2019-07-04 16:25:37

--- ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
+++ ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
@@ -1,370 +1,96 @@
 
         
-            private_constant :GA_TRACKING
-    attr_accessor :session_id
-    attr_accessor :client
+        module Gitlab
+  module GithubImport
+    module Importer
+      class NotesImporter
+        include ParallelScheduling
     
-          it 'add a cache_builds flag to command if cache_builds is set to true' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              cache_builds: true
-            )
-          end').runner.execute(:test)
-    
-            FastlaneCore::CertChecker.wwdr_certificate_installed?
+    module Devise
+  module Controllers
+    # Create url helpers to be used with resource/scope configuration. Acts as
+    # proxies to the generated routes created by devise.
+    # Resource param can be a string or symbol, a class, or an instance object.
+    # Example using a :user resource:
+    #
+    #   new_session_path(:user)      => new_user_session_path
+    #   session_path(:user)          => user_session_path
+    #   destroy_session_path(:user)  => destroy_user_session_path
+    #
+    #   new_password_path(:user)     => new_user_password_path
+    #   password_path(:user)         => user_password_path
+    #   edit_password_path(:user)    => edit_user_password_path
+    #
+    #   new_confirmation_path(:user) => new_user_confirmation_path
+    #   confirmation_path(:user)     => user_confirmation_path
+    #
+    # Those helpers are included by default to ActionController::Base.
+    #
+    # In case you want to add such helpers to another class, you can do
+    # that as long as this new class includes both url_helpers and
+    # mounted_helpers. Example:
+    #
+    #     include Rails.application.routes.url_helpers
+    #     include Rails.application.routes.mounted_helpers
+    #
+    module UrlHelpers
+      def self.remove_helpers!
+        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
+          remove_method method
+        end
       end
     
-    shellescape_testcases = [
-  # baseline
-  {
-    'it' => '(#1) on simple string',
-    'it_result' => {
-      'windows' => 'doesn't change it',
-      'other'   => 'doesn't change it'
-    },
-    'str' => 'normal_string_without_spaces',
-    'expect' => {
-      'windows' => 'normal_string_without_spaces',
-      'other'   => 'normal_string_without_spaces'
-    }
-  },
-  {
-    'it' => '(#2) on empty string',
-    'it_result' => {
-      'windows' => 'wraps it in double quotes',
-      'other'   => 'wraps it in single quotes'
-    },
-    'str' => '',
-    'expect' => {
-      'windows' => '''',
-      'other'   => '\'\''
-    }
-  },
-  # spaces
-  {
-    'it' => '(#3) on string with spaces',
-    'it_result' => {
-      'windows' => 'wraps it in double quotes',
-      'other'   => 'escapes spaces with <backslash>'
-    },
-    'str' => 'string with spaces',
-    'expect' => {
-      'windows' => ''string with spaces'',
-      'other'   => 'string\ with\ spaces'
-    }
-  },
-  # double quotes
-  {
-    'it' => '(#4) on simple string that is already wrapped in double quotes',
-    'it_result' => {
-      'windows' => 'doesn't touch it',
-      'other'   => 'escapes the double quotes with <backslash>'
-    },
-    'str' => ''normal_string_without_spaces'',
-    'expect' => {
-      'windows' => ''normal_string_without_spaces'',
-      'other'   => '\'normal_string_without_spaces\''
-    }
-  },
-  {
-    'it' => '(#5) on string with spaces that is already wrapped in double quotes',
-    'it_result' => {
-      'windows' => 'wraps in double quotes and duplicates existing double quotes',
-      'other'   => 'escapes the double quotes and spaces with <backslash>'
-    },
-    'str' => ''string with spaces already wrapped in double quotes'',
-    'expect' => {
-      'windows' => ''''string with spaces already wrapped in double quotes'''',
-      'other'   => '\'string\ with\ spaces\ already\ wrapped\ in\ double\ quotes\''
-    }
-  },
-  {
-    'it' => '(#6) on string with spaces and double quotes',
-    'it_result' => {
-      'windows' => 'wraps in double quotes and duplicates existing double quotes',
-      'other'   => 'escapes the double quotes and spaces with <backslash>'
-    },
-    'str' => 'string with spaces and 'double' quotes',
-    'expect' => {
-      'windows' => ''string with spaces and ''double'' quotes'',
-      'other'   => 'string\ with\ spaces\ and\ \'double\'\ quotes'
-    }
-  },
-  # https://github.com/ruby/ruby/blob/ac543abe91d7325ace7254f635f34e71e1faaf2e/test/test_shellwords.rb#L64-L65
-  {
-    'it' => '(#7) on simple int',
-    'it_result' => {
-      'windows' => 'doesn't change it',
-      'other'   => 'doesn't change it'
-    },
-    'str' => 3,
-    'expect' => {
-      'windows' => '3',
-      'other'   => '3'
-    }
-  },
-  # single quotes
-  {
-    'it' => '(#8) on simple string that is already wrapped in single quotes',
-    'it_result' => {
-      'windows' => 'doesn't touch it',
-      'other'   => 'escapes the single quotes with <backslash>'
-    },
-    'str' => ''normal_string_without_spaces'',
-    'expect' => {
-      'windows' => ''normal_string_without_spaces'',
-      'other'   => '\\'normal_string_without_spaces\\''
-    }
-  },
-  {
-    'it' => '(#9) on string with spaces that is already wrapped in single quotes',
-    'it_result' => {
-      'windows' => 'wraps in double quotes',
-      'other'   => 'escapes the single quotes and spaces with <backslash>'
-    },
-    'str' => ''string with spaces already wrapped in single quotes'',
-    'expect' => {
-      'windows' => '\''string with spaces already wrapped in single quotes'\'',
-      'other'   => '\\'string\\ with\\ spaces\\ already\\ wrapped\\ in\\ single\\ quotes\\''
-    }
-  },
-  {
-    'it' => '(#10) string with spaces and single quotes',
-    'it_result' => {
-      'windows' => 'wraps in double quotes and leaves single quotes',
-      'other'   => 'escapes the single quotes and spaces with <backslash>'
-    },
-    'str' => 'string with spaces and 'single' quotes',
-    'expect' => {
-      'windows' => '\'string with spaces and 'single' quotes\'',
-      'other'   => 'string\ with\ spaces\ and\ \\\'single\\\'\ quotes'
-    }
-  },
-  {
-    'it' => '(#11) string with spaces and <backslash>',
-    'it_result' => {
-      'windows' => 'wraps in double quotes and escapes the backslash with backslash',
-      'other'   => 'escapes the spaces and the backslash (which in results in quite a lot of them)'
-    },
-    'str' => 'string with spaces and \\ in it',
-    'expect' => {
-      'windows' => '\'string with spaces and \\ in it\'',
-      'other'   => 'string\\ with\\ spaces\\ and\\ \\\\\\ in\\ it'
-    }
-  },
-  {
-    'it' => '(#12) string with spaces and <slash>',
-    'it_result' => {
-      'windows' => 'wraps in double quotes',
-      'other'   => 'escapes the spaces'
-    },
-    'str' => 'string with spaces and / in it',
-    'expect' => {
-      'windows' =>  '\'string with spaces and / in it\'',
-      'other'   => 'string\\ with\\ spaces\\ and\\ /\\ in\\ it'
-    }
-  },
-  {
-    'it' => '(#13) string with spaces and parens',
-    'it_result' => {
-      'windows' => 'wraps in double quotes',
-      'other'   => 'escapes the spaces and parens'
-    },
-    'str' => 'string with spaces and (parens) in it',
-    'expect' => {
-      'windows' => '\'string with spaces and (parens) in it\'',
-      'other'   => 'string\\ with\\ spaces\\ and\\ \\(parens\\)\\ in\\ it'
-    }
-  },
-  {
-    'it' => '(#14) string with spaces, single quotes and parens',
-    'it_result' => {
-      'windows' => 'wraps in double quotes',
-      'other'   => 'escapes the spaces, single quotes and parens'
-    },
-    'str' => 'string with spaces and 'quotes' (and parens) in it',
-    'expect' => {
-      'windows' => '\'string with spaces and 'quotes' (and parens) in it\'',
-      'other'   => 'string\\ with\\ spaces\\ and\\ \\'quotes\\'\\ \\(and\\ parens\\)\\ in\\ it'
-    }
-  }
-]
+        # Creates configuration values for Devise and for the given module.
+    #
+    #   Devise::Models.config(Devise::Models::DatabaseAuthenticatable, :stretches)
+    #
+    # The line above creates:
+    #
+    #   1) An accessor called Devise.stretches, which value is used by default;
+    #
+    #   2) Some class methods for your model Model.stretches and Model.stretches=
+    #      which have higher priority than Devise.stretches;
+    #
+    #   3) And an instance method stretches.
+    #
+    # To add the class methods you need to have a module ClassMethods defined
+    # inside the given class.
+    #
+    def self.config(mod, *accessors) #:nodoc:
+      class << mod; attr_accessor :available_configs; end
+      mod.available_configs = accessors
     
-          it 'creates a new user' do
-        visit new_admin_user_path
-        fill_in 'Email', with: 'test@test.com'
-        fill_in 'Username', with: 'usertest'
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: '12345678'
-        click_on 'Create User'
-        expect(page).to have_text('User 'usertest' was successfully created.')
-        expect(page).to have_text('test@test.com')
+              recoverable.reset_password_token = original_token if recoverable.reset_password_token.present?
+          recoverable
+        end
+    
+      puts '== Installing dependencies =='
+  system! 'gem install bundler --conservative'
+  system('bundle check') || system!('bundle install')
+    
+      # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+    
+      attr_accessor :directories
+    
+      dont_obsolete_paths = []
+  option '--dont-obsolete', 'DONT_OBSOLETE_PATH',
+    'A file path for which to 'dont-obsolete' in the built PackageInfo. ' \
+    'Can be specified multiple times.' do |path|
+      dont_obsolete_paths << path
+    end
+    
+    class FPM::Package::Pacman < FPM::Package
+    
+    
+    
+    require 'pleaserun/cli'
+    
+          def self.has_issues_with_add_symlink?
+        return !::Gem::Package::TarWriter.public_instance_methods.include?(:add_symlink)
       end
-    
-      it 'renders the import form' do
-    visit new_scenario_imports_path
-    expect(page).to have_text('Import a Public Scenario')
-  end
-    
-        it 'is OK with no hl' do
-      stub(params).[](:hl) { nil }
-      expect((1..10).select { |i| highlighted?(i) }).to be_empty
-    end
-  end
-end
-
-    
-      describe 'DotHelper::DotDrawer' do
-    describe '#id' do
-      it 'properly escapes double quotaion and backslash' do
-        expect(DotHelper::DotDrawer.draw(foo: '') {
-          id('hello\\'')
-        }).to eq(''hello\\\\\\''')
-      end
-    end
-  end
-end
-
-    
-      describe '#relative_distance_of_time_in_words' do
-    it 'in the past' do
-      expect(relative_distance_of_time_in_words(Time.now-5.minutes)).to eq('5m ago')
-    end
-    
-        it 'optionally supports treating values that start with '$' as raw JSONPath' do
-      expect(Utils.interpolate_jsonpaths('$.there.world', payload)).to eq('$.there.world')
-      expect(Utils.interpolate_jsonpaths('$.there.world', payload, :leading_dollarsign_is_jsonpath => true)).to eq('WORLD')
-    end
-  end
-    
-      let :valid_options do
-    {
-      'name' => 'XKCD',
-      'expected_update_period_in_days' => '2',
-      'type' => 'html',
-      'url' => '{{ url | default: 'http://xkcd.com/' }}',
-      'mode' => 'on_change',
-      'extract' => old_extract,
-      'template' => old_template
-    }
-  end
-    
-      describe 'path request must exist' do
-    it 'should check that validation added if path does not exist' do
-      opts = @opts.tap { |o| o.delete('path') }
-      @checker = Agents::AftershipAgent.new(:name => 'tectonic', :options => opts)
-      @checker.user = users(:bob)
-      expect(@checker.save).to eq false
-      expect(@checker.errors.full_messages.first).to eq('You need to specify a path request')
-    end
-  end
-    
-        it 'should raise error when response has an error' do
-      stub(HTTParty).post { {'error' => {'message' => 'Sample error'}} }
-      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Sample error/)
-    end
-  end
-end
-
-    
-      attr_accessor :dcase_hash # :nodoc:
-    
-      #
-  # HTTP POST request class wrapper.
-  #
-  class Post < Request
-    def initialize(uri = '/', proto = DefaultProtocol)
-      super('POST', uri, proto)
-    end
-  end
-    
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
-          #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        class EncKdcResponse < Element
-          # @!attribute key
-          #   @return [Rex::Proto::Kerberos::Model::EncryptionKey] The session key
-          attr_accessor :key
-          # @!attribute last_req
-          #   @return [Array<Rex::Proto::Kerberos::Model::LastRequest>] This field is returned by the KDC and specifies the time(s)
-          #   of the last request by a principal
-          attr_accessor :last_req
-          # @!attribute nonce
-          #   @return [Integer] random number
-          attr_accessor :nonce
-          # @!attribute key_expiration
-          #   @return [Time] The key-expiration field is part of the response from the
-          #   KDC and specifies the time that the client's secret key is due to expire
-          attr_accessor :key_expiration
-          # @!attribute flags
-          #   @return [Integer] This field indicates which of various options were used or
-          #   requested when the ticket was issued
-          attr_accessor :flags
-          # @!attribute auth_time
-          #   @return [Time] the time of initial authentication for the named principal
-          attr_accessor :auth_time
-          # @!attribute start_time
-          #   @return [Time] Specifies the time after which the ticket is valid
-          attr_accessor :start_time
-          # @!attribute end_time
-          #   @return [Time] This field contains the time after which the ticket will
-          #   not be honored (its expiration time)
-          attr_accessor :end_time
-          # @!attribute renew_till
-          #   @return [Time] This field is only present in tickets that have the
-          #   RENEWABLE flag set in the flags field.  It indicates the maximum
-          #   endtime that may be included in a renewal
-          attr_accessor :renew_till
-          # @!attribute srealm
-          #   @return [String] The realm part of the server's principal identifier
-          attr_accessor :srealm
-          # @!attribute sname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
-          attr_accessor :sname
-    
-              # Decodes the cusec field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_cusec(input)
-            input.value[0].value
-          end
-    
-        assert_syntax_error(%q{
-      case _
-      in '#{a}': a
-      end
-    }, /symbol literal with interpolation is not allowed/)
-    
-      # This method is called when the parser found syntax error.
-  def compile_error(msg)
-  end
-    
-    call_argument_test(true, Proc.new{||})
-call_argument_test(true, Proc.new{||}, 1)
-call_argument_test(true, Proc.new{|a,|}, 1)
-call_argument_test(true, Proc.new{|a,|})
-call_argument_test(true, Proc.new{|a,|}, 1,2)
-    
-    def to_boolean(proc)
-  IF[proc][true][false]
-end
-    
-        # force a disk sync before running bundler
-    gemfile.save
-    
-          it 'display a list of installed plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --installed')
-        expect(result.stdout.split('\n').size).to be > 1
-      end
+    end # module TarWriter
+  end # module Issues
+end # module FPM
