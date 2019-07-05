1ac7c55b04d966fab12b14b57ff5876f5c770bde
SUNsung <weatherius@gmail.com>
2019-07-05 07:34:29 +0300

    Rewrite [4438ApNC1QfUk447svL6EbHYQk44DizFzEMbT1644egQ.rb] File in: 2019-07-05 07:34:28

--- ruby/4438ApNC1QfUk447svL6EbHYQk44DizFzEMbT1644egQ.rb
+++ ruby/4438ApNC1QfUk447svL6EbHYQk44DizFzEMbT1644egQ.rb
@@ -1,52 +1,109 @@
 
         
-        CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+        #############################################################################
+#
+# Helper functions
+#
+#############################################################################
     
-        def deprecation_message(message)
-      Jekyll.logger.warn 'Deprecation:', message
+    require 'benchmark/ips'
+require 'jekyll'
+require 'json'
+    
+    if pathutil_relative == native_relative
+  Benchmark.ips do |x|
+    x.report('pathutil') { pathutil_relative }
+    x.report('native')   { native_relative }
+    x.compare!
+  end
+else
+  print 'PATHUTIL: '
+  puts pathutil_relative
+  print 'NATIVE:   '
+  puts native_relative
+end
+
+    
+      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
+  p.option 'destination', '-d', '--destination [DIR]',
+    'Destination directory (defaults to ./_site)'
+  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
+  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
+    'Plugins directory (defaults to ./_plugins)'
+  p.option 'layouts_dir', '--layouts DIR', String,
+    'Layouts directory (defaults to ./_layouts)'
+  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+    
+    module Jekyll
+  module Drops
+    class UnifiedPayloadDrop < Drop
+      mutable true
+    
+        open_dry_run_modal(agent)
+    click_on('Dry Run')
+    expect(page).to have_text('Dry Run started')
+    expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabLog']')
+  end
+end
+
+    
+      it 'imports a scenario which requires a service' do
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'spec/data_fixtures/twitter_scenario.json'))
+    click_on 'Start Import'
+    check('I confirm that I want to import these Agents.')
+    expect { click_on 'Finish Import' }.to change(Scenario, :count).by(1)
+    expect(page).to have_text('Import successful!')
+  end
+end
+
+    
+        it 'works for queued jobs' do
+      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
+    end
+  end
+    
+      describe 'initialization' do
+    it 'is initialized with an attributes hash' do
+      expect(ScenarioImport.new(:url => 'http://google.com').url).to eq('http://google.com')
+    end
+  end
+    
+      describe 'migrating the 'make_message' format' do
+    it 'should work' do
+      expect(LiquidMigrator.convert_make_message('<message>')).to eq('{{message}}')
+      expect(LiquidMigrator.convert_make_message('<new.message>')).to eq('{{new.message}}')
+      expect(LiquidMigrator.convert_make_message('Hello <world>. How is <nested.life>')).to eq('Hello {{world}}. How is {{nested.life}}')
+    end
+  end
+    
+        it 'cleans up old logs when there are more than log_length' do
+      stub(AgentLog).log_length { 4 }
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 1')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 2')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 3')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 4')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 4')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 1')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 5')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 5')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 2')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 6')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 6')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 3')
     end
     
-          private
+      describe '#working?' do
+    it 'should not be working until the first event was received' do
+      expect(@checker).not_to be_working
+      @checker.last_receive_at = Time.now
+      expect(@checker).to be_working
+    end
+  end
     
-          case command.method_name
-      when 'sh'
-        error_callback = proc { |string_value| closure_argument_value = string_value }
-        command_param = parameter_map[:command]
-        log_param = parameter_map[:log]
-        action_return = Fastlane::FastFile.sh(command_param, log: log_param, error_callback: error_callback)
-      end
+        def self.can_extract?(path)
+      mime_type = 'application/vnd.adobe.air-application-installer-package+zip'
+      path.magic_number.match?(/.{59}#{Regexp.escape(mime_type)}/)
+    end
     
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            grouping: '#{grouping}',
-            build_number: #{specified_build_number},
-          })
-        end').runner.execute(:test)
-    
-          it 'adds create_docset param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            create_docset: true
-          )
-        end').runner.execute(:test)
-    
-        # An empty argument will be skipped, so return empty quotes.
-    # https://github.com/ruby/ruby/blob/a6413848153e6c37f6b0fea64e3e871460732e34/lib/shellwords.rb#L142-L143
-    return ''''.dup if str.empty?
-    
-    require 'colored'
-require 'shellwords'
+    namespace :bower do
