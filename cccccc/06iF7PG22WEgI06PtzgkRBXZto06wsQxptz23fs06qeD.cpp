52ecf786b73d6f4a6223837cb23ae6b5dccbb8ff
SUNsung <weatherius@gmail.com>
2019-07-06 02:37:08 +0300

    Rewrite [06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp] File in: 2019-07-06 02:37:08

--- cccccc/06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp
+++ cccccc/06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp
@@ -1,184 +1,128 @@
 
         
-          // get rid of the array and index
-  if (mode == psModeForm || inType3Char || preload) {
-    if (!inlineImg) writePS('pop ');
-    writePS('pop pop\n');
+        // Reads all boxes from the string. Otherwise, as ReadAllBoxes.
+// continue_on_failure allows reading to continue even if an invalid box is
+// encountered and will return true if it succeeds in reading some boxes.
+// It otherwise gives up and returns false on encountering an invalid box.
+bool ReadMemBoxes(int target_page, bool skip_blanks, const char* box_data,
+                  bool continue_on_failure,
+                  GenericVector<TBOX>* boxes,
+                  GenericVector<STRING>* texts,
+                  GenericVector<STRING>* box_texts,
+                  GenericVector<int>* pages);
+    
+    // Class to hold a Pixa collection of debug images with captions and save them
+// to a PDF file.
+class DebugPixa {
+ public:
+  // TODO(rays) add another constructor with size control.
+  DebugPixa() {
+    pixa_ = pixaCreate(0);
+    fonts_ = bmfCreate(nullptr, 14);
+  }
+  // If the filename_ has been set and there are any debug images, they are
+  // written to the set filename_.
+  ~DebugPixa() {
+    pixaDestroy(&pixa_);
+    bmfDestroy(&fonts_);
+  }
     }
     
-      // Reserve room for terminating '\0'
-  size--;
+    // Computes the Otsu threshold(s) for the given histogram.
+// Also returns H = total count in histogram, and
+// omega0 = count of histogram below threshold.
+int OtsuStats(const int* histogram, int* H_out, int* omega0_out);
     
-    #endif /* PAGE_TRANSITION_H */
+    // A smart pointer class that implements a double-ended pointer. Each end
+// points to the other end. The copy constructor and operator= have MOVE
+// semantics, meaning that the relationship with the other end moves to the
+// destination of the copy, leaving the source unattached.
+// For this reason both the copy constructor and the operator= take a non-const
+// reference argument, and the const reference versions cannot be used.
+// DoublePtr is useful to incorporate into structures that are part of a
+// collection such as GenericVector or STL containers, where reallocs can
+// relocate the members. DoublePtr is also useful in a GenericHeap, where it
+// can correctly maintain the pointer to an element of the heap despite it
+// getting moved around on the heap.
+class DoublePtr {
+ public:
+  DoublePtr() : other_end_(nullptr) {}
+  // Copy constructor steals the partner off src and is therefore a non
+  // const reference arg.
+  // Copying a const DoublePtr generates a compiler error.
+  DoublePtr(DoublePtr& src) {
+    other_end_ = src.other_end_;
+    if (other_end_ != nullptr) {
+      other_end_->other_end_ = this;
+      src.other_end_ = nullptr;
+    }
+  }
+  // Operator= steals the partner off src, and therefore needs src to be a non-
+  // const reference.
+  // Assigning from a const DoublePtr generates a compiler error.
+  void operator=(DoublePtr& src) {
+    Disconnect();
+    other_end_ = src.other_end_;
+    if (other_end_ != nullptr) {
+      other_end_->other_end_ = this;
+      src.other_end_ = nullptr;
+    }
+  }
+    }
+    
+    #endif  // TESSERACT_CCUTIL_GENERICHEAP_H_
 
     
+    #include <limits>
     
-    {  GBool mono;
-  GBool gray;
-  GBool transparency;
-  GBool gdi;
-  PSLevel level;		// PostScript level (1, 2, separation)
-  GBool level1PSBug;		// gTrue if it uses a feature not supported in PSOutputDev
-};
-    
-    #include <config.h>
-    
-      GBool isReverseVideo() { return reverseVideo; }
-  void setReverseVideo(GBool reverseVideoA) { reverseVideo = reverseVideoA; }
-    
-    
-    
-    public:
-    
-    void ScientificNumberFormatter::getPreExponent(
-        const DecimalFormatSymbols &dfs, UnicodeString &preExponent) {
-    preExponent.append(dfs.getConstSymbol(
-            DecimalFormatSymbols::kExponentMultiplicationSymbol));
-    preExponent.append(dfs.getConstSymbol(DecimalFormatSymbols::kOneDigitSymbol));
-    preExponent.append(dfs.getConstSymbol(DecimalFormatSymbols::kZeroDigitSymbol));
+    // Range() returns generators providing sequences of values in a range.
+//
+// Synopsis:
+// Range(start, end)
+//   - returns a generator producing a sequence of values {start, start+1,
+//     start+2, ..., }.
+// Range(start, end, step)
+//   - returns a generator producing a sequence of values {start, start+step,
+//     start+step+step, ..., }.
+// Notes:
+//   * The generated sequences never include end. For example, Range(1, 5)
+//     returns a generator producing a sequence {1, 2, 3, 4}. Range(1, 9, 2)
+//     returns a generator producing {1, 3, 5, 7}.
+//   * start and end must have the same type. That type may be any integral or
+//     floating-point type or a user defined type satisfying these conditions:
+//     * It must be assignable (have operator=() defined).
+//     * It must have operator+() (operator+(int-compatible type) for
+//       two-operand version).
+//     * It must have operator<() defined.
+//     Elements in the resulting sequences will also have that type.
+//   * Condition start < end must be satisfied in order for resulting sequences
+//     to contain any elements.
+//
+template <typename T, typename IncrementT>
+internal::ParamGenerator<T> Range(T start, T end, IncrementT step) {
+  return internal::ParamGenerator<T>(
+      new internal::RangeGenerator<T, IncrementT>(start, end, step));
 }
     
-    
-    {        return handlePrev(offset, status);
-    }
-    
-    // SharedBreakIterator encapsulates a shared BreakIterator. Because
-// BreakIterator has mutable semantics, clients must ensure that all uses
-// of a particular shared BreakIterator is protected by the same mutex
-// ensuring that only one thread at a time gets access to that shared
-// BreakIterator. Clients can accomplish this by creating a mutex for all
-// uses of break iterator within a particular class. Then objects of that
-// class may then freely share break iterators among themselves. However,
-// these shared break iterators must never be exposed outside of that class.
-class U_I18N_API SharedBreakIterator : public SharedObject {
-public:
-    SharedBreakIterator(BreakIterator *biToAdopt);
-    virtual ~SharedBreakIterator();
-    }
-    
-    
-IntDigitCountRange::IntDigitCountRange(int32_t min, int32_t max) {
-    fMin = min < 0 ? 0 : min;
-    fMax = max < fMin ? fMin : max;
-}
-    
-    /* Initialize the fields we use to disambiguate ambiguous years. Separate
- * so we can call it from readObject().
- */
-void SimpleDateFormat::initializeDefaultCentury()
-{
-  if(fCalendar) {
-    fHaveDefaultCentury = fCalendar->haveDefaultCentury();
-    if(fHaveDefaultCentury) {
-      fDefaultCenturyStart = fCalendar->defaultCenturyStart();
-      fDefaultCenturyStartYear = fCalendar->defaultCenturyStartYear();
-    } else {
-      fDefaultCenturyStart = DBL_MIN;
-      fDefaultCenturyStartYear = -1;
-    }
+      FilePath& operator=(const FilePath& rhs) {
+    Set(rhs);
+    return *this;
   }
-}
     
-        /**
-     * Implement UnicodeMatcher
-     * Returns TRUE if this matcher will match a character c, where c
-     * & 0xFF == v, at offset, in the forward direction (with limit >
-     * offset).  This is used by <tt>RuleBasedTransliterator</tt> for
-     * indexing.
-     * @param v    the given value
-     * @return     TRUE if this matcher will match a character c, 
-     *             where c & 0xFF == v
-     */
-    virtual UBool matchesIndexValue(uint8_t v) const;
+      // Now, we have that n is odd and n >= 3.
     
-            static const size_t s_serializationVersion = 0;
+    // A sample program demonstrating using Google C++ testing framework.
+//
+// Author: wan@google.com (Zhanyong Wan)
     
-            const NDShape& Shape() const override { return m_unpackedShape; }
-        DeviceDescriptor Device() const override { return m_isPacked ? m_packedData->Device() : Value::Device(); }
-        DataType GetDataType() const override { return m_isPacked ? m_packedData->GetDataType() : Value::GetDataType(); }
-        StorageFormat GetStorageFormat() const override { return m_isPacked? m_packedData->GetStorageFormat() : Value::GetStorageFormat(); }
-        bool IsReadOnly() const override { return m_isPacked ? m_packedData->IsReadOnly() : Value::IsReadOnly(); }
     
-        // Acquires the mutex. If 'wait' is true and mutex is acquired by someone else then
-    // function waits until mutex is released
-    // Returns false if !wait and lock cannot be acquired, or in case of a system error that prevents us from acquiring the lock.
-    bool Acquire(bool wait)
+    // out_of_range.106
+    try
     {
-        mode_t mask = umask(0);
+        // try to use an array index with leading '0'
+        json::reference ref = j.at('/array/01'_json_pointer);
     }
-    
-    void DataReader::CopyMBLayoutTo(MBLayoutPtr pMBLayout)
-{
-    // BUGBUG: This copies all data reader's layout info on top of each other, keeping only the last one; likely not what was intended.
-    for (size_t i = 0; i < m_ioNames.size(); i++)
-        m_dataReaders[m_ioNames[i]]->CopyMBLayoutTo(pMBLayout);
-}
-    
-    using namespace std;
-    
-    TEST(ByteTest, SetGetHighLowBit) {
-  unsigned char byte_value = 0x37;
-  Byte value(&byte_value);
-  value.set_value_high_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
-  value.set_value_low_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
-}
-    
-    
-    {  for (int i = 0; i < conti_radar->contiobs_size(); ++i) {
-    if (conti_radar->contiobs(i).obstacle_id() == obj_id) {
-      auto obs = conti_radar->mutable_contiobs(i);
-      obs->set_longitude_dist_rms(
-          LINEAR_RMS[longitude_dist_rms(bytes, length)]);
-      obs->set_lateral_dist_rms(LINEAR_RMS[lateral_dist_rms(bytes, length)]);
-      obs->set_longitude_vel_rms(LINEAR_RMS[longitude_vel_rms(bytes, length)]);
-      obs->set_lateral_vel_rms(LINEAR_RMS[lateral_vel_rms(bytes, length)]);
-      obs->set_longitude_accel_rms(
-          LINEAR_RMS[longitude_accel_rms(bytes, length)]);
-      obs->set_lateral_accel_rms(LINEAR_RMS[lateral_accel_rms(bytes, length)]);
-      obs->set_oritation_angle_rms(
-          ANGLE_RMS[oritation_angle_rms(bytes, length)]);
-      obs->set_probexist(PROBOFEXIST[probexist(bytes, length)]);
-      obs->set_meas_state(meas_state(bytes, length));
-      break;
-    }
-  }
-}
-    
-      MatrixXd mat_golden(10, 10);
-  // clang-format off
-  mat_golden <<
-    0.2,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0, 0.2,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0, 0.2,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0, 0.2,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0, 0.2,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0, 0.2,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0, 0.2,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0, 0.2,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0, 0.2,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0, 0.2;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-    const PolynomialXd& Spline1dSeg::spline_func() const { return spline_func_; }
-    
-      Byte t1(bytes + 5);
-  int32_t t = t1.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-    #include 'modules/canbus/vehicle/gem/protocol/accel_rpt_68.h'
-    
-    // config detail: {'name': 'usr_can_read_errors', 'offset': 0.0,
-// 'precision': 1.0, 'len': 16, 'is_signed_var': False, 'physical_range':
-// '[0|65535]', 'bit': 55, 'type': 'int', 'order': 'motorola', 'physical_unit':
-// ''}
-int Globalrpt6a::usr_can_read_errors(const std::uint8_t* bytes,
-                                     int32_t length) const {
-  Byte t0(bytes + 6);
-  int32_t x = t0.get_byte(0, 8);
+    catch (json::parse_error& e)
+    {
+        std::cout << e.what() << '\n';
     }
