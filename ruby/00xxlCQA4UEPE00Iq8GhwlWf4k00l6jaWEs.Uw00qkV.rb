20acea1dd84ef70b71a0a45ce093e5a1ff629356
SUNsung <weatherius@gmail.com>
2019-07-05 11:37:52 +0300

    Rewrite [00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb] File in: 2019-07-05 11:37:51

--- ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
+++ ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
@@ -1,63 +1,116 @@
 
         
-          attr_reader :id
-  attr_reader :post_id
-  def initialize(id = nil, post_id = nil); @id, @post_id = id, post_id end
-  def to_key; id ? [id] : nil end
-  def save; @id = 1; @post_id = 1 end
-  def persisted?; @id.present? end
-  def to_param; @id && @id.to_s; end
-  def name
-    @id.nil? ? 'new #{self.class.name.downcase}' : '#{self.class.name.downcase} ##{@id}'
-  end
-    
-          it 'passes an environment Hash' do
-        expect_command({ 'PATH' => '/usr/local/bin' }, 'git', 'commit')
-        Fastlane::Actions.sh({ 'PATH' => '/usr/local/bin' }, 'git', 'commit')
-      end
-    
-        os = 'other'
-    shelljoin_testcases.each do |testcase|
-      it testcase['it'] + ': ' + testcase['it_result'][os] do
-        array = testcase['input']
-        expect_correct_implementation_to_be_called(array, :shelljoin, os)
-        joined = array.shelljoin
-        expect(joined).to eq(testcase['expect'][os])
-      end
-    end
-  end
+        def liquid_escape(markdown)
+  markdown.gsub(%r!(`{[{%].+[}%]}`)!, '{% raw %}\\1{% endraw %}')
 end
     
-          it 'keeps the specified screenshots folder' do
-        expect(options[:screenshots_path]).to eq('./screenshots')
+    # -------------------------------------------------------------------
+# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
+# -------------------------------------------------------------------
+    
+          #
+    
+              new_theme_name = args.join('_')
+          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
+    
+                  Jekyll.logger.info 'LiveReload address:',
+                                 'http://#{opts['host']}:#{opts['livereload_port']}'
+            end
+          end
+          @thread.abort_on_exception = true
+        end
+    
+            def id_for_already_imported_cache(issue)
+          issue.number
+        end
+    
+                rows << {
+              label_id: label_id,
+              target_id: target_id,
+              target_type: issue.issuable_type,
+              created_at: time,
+              updated_at: time
+            }
+          end
+    
+          # Returns the identifier to use for cache keys.
+      #
+      # For issues and pull requests this will be 'Issue' or 'MergeRequest'
+      # respectively. For diff notes this will return 'MergeRequest', for
+      # regular notes it will either return 'Issue' or 'MergeRequest' depending
+      # on what type of object the note belongs to.
+      def cache_key_type
+        if object.respond_to?(:issuable_type)
+          object.issuable_type
+        elsif object.respond_to?(:noteable_type)
+          object.noteable_type
+        else
+          raise(
+            TypeError,
+            'Instances of #{object.class} are not supported'
+          )
+        end
       end
     
-            # Executes a command on the remote machine with administrative
-        # privileges. See {#execute} for documentation, as the API is the
-        # same.
-        #
-        # @see #execute
-        def sudo(command, opts=nil)
-        end
+          def parallel?
+        @parallel
+      end
     
-            # Allows setting options from a hash. By default this simply calls
-        # the `#{key}=` method on the config class with the value, which is
-        # the expected behavior most of the time.
-        #
-        # This is expected to mutate itself.
-        #
-        # @param [Hash] options A hash of options to set on this configuration
-        #   key.
-        def set_options(options)
-          options.each do |key, value|
-            send('#{key}=', value)
+        private
+    
+          @report_note = current_account.report_notes.new(resource_params)
+      @report = @report_note.report
+    
+      def show
+    @status = status_finder.status
+    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
+  end
+    
+      def update
+    if verify_payload?
+      process_salmon
+      head 202
+    elsif payload.present?
+      render plain: signature_verification_failure_reason, status: 401
+    else
+      head 400
+    end
+  end
+    
+      private
+    
+    class Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token
+    
+      def apply_header_reset
+    response.headers['X-RateLimit-Reset'] = rate_limit_reset
+  end
+    
+        reset
+  end
+    
+        if (p)
+      # Create an instance of the handler for this resource
+      handler = p[0].new(self, *p[2])
+    
+              # Encodes the realm field
+          #
+          # @return [String]
+          def encode_realm
+            encoded = ''
+            encoded << [realm.length].pack('N')
+            encoded << realm
+    
+              # @!attribute type
+          #   @return [Integer] The algorithm used to generate the checksum
+          attr_accessor :type
+          # @!attribute checksum
+          #   @return [String] The checksum itself
+          attr_accessor :checksum
+    
+              include Rex::Proto::Kerberos::Crypto
+          include Rex::Proto::Kerberos::Model
+    
+                int
           end
-        end
-    
-            # Capture all bad configuration calls and save them for an error
-        # message later during validation.
-        def method_missing(name, *args, &block)
-          return super if @__finalized
-    
-              nil
-        end
