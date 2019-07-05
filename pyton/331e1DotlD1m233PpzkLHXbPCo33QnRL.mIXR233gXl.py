637a285bdb3b3dceb1971b9e2cb886ae4830290c
SUNsung <weatherius@gmail.com>
2019-07-06 01:14:29 +0300

    Rewrite [331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py] File in: 2019-07-06 01:14:29

--- pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
+++ pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
@@ -1,71 +1,275 @@
 
         
-            def bfs(self, source, dest):
-        if source is None:
-            return False
-        queue = deque()
-        queue.append(source)
-        source.visit_state = State.visited
-        while queue:
-            node = queue.popleft()
-            print(node)
-            if dest is node:
-                return True
-            for adjacent_node in node.adj_nodes.values():
-                if adjacent_node.visit_state == State.unvisited:
-                    queue.append(adjacent_node)
-                    adjacent_node.visit_state = State.visited
-        return False
+        
+iv = key = [0x20, 0x15] + 14 * [0]
     
-    import pylons
-from pylons.i18n.translation import _get_translator
-from routes.util import URLGenerator
-from pylons import url
-import baseplate.events
-import pkg_resources
-import paste.fixture
-import paste.script.appinstall
-from paste.deploy import loadapp
+    versions_info['signature'] = signature
+with open('update/versions.json', 'w') as versionsf:
+    json.dump(versions_info, versionsf, indent=4, sort_keys=True)
+
     
-            # Make get_ids return a valid payment_method_id
-        get_ids.return_value.append(payment_method_id)
-        # Assign arbitrary CustomerID, which comes from Authorize
-        get_id.return_value = 1000
-        create_authorization_hold.return_value = transaction_id
+    module_contents.append(
+    '_ALL_CLASSES = [{0}]'.format(', '.join(names)))
     
-        def test_dict(self):
-        self.assertEquals({}, ConfigValue.dict(str, str)(''))
-        self.assertEquals({'a': ''}, ConfigValue.dict(str, str)('a'))
-        self.assertEquals({'a': 3}, ConfigValue.dict(str, int)('a: 3'))
-        self.assertEquals({'a': 3, 'b': 4},
-                          ConfigValue.dict(str, int)('a: 3, b: 4'))
-        self.assertEquals({'a': (3, 5), 'b': (4, 6)},
-                          ConfigValue.dict(
-                              str, ConfigValue.tuple_of(int), delim=';')
-                          ('a: 3, 5;  b: 4, 6'))
+    import io
+import optparse
+import os
+import sys
     
-        def test_is_superuser(self):
-        perm_set = PermissionSet()
-        self.assertFalse(perm_set.is_superuser())
-        perm_set[perm_set.ALL] = True
-        self.assertTrue(perm_set.is_superuser())
-        perm_set[perm_set.ALL] = False
-        self.assertFalse(perm_set.is_superuser())
     
-        def test_signing(self):
-        epoch_time = 1234567890
-        header = self.make_sig_header(
-            '{'user': 'reddit', 'password': 'hunter2'}',
-            epoch=epoch_time,
+def filter_options(readme):
+    ret = ''
+    in_options = False
+    for line in readme.split('\n'):
+        if line.startswith('# '):
+            if line[2:].startswith('OPTIONS'):
+                in_options = True
+            else:
+                in_options = False
+    
+    from test.helper import (
+    assertGreaterEqual,
+    expect_warnings,
+    get_params,
+    gettestcases,
+    expect_info_dict,
+    try_rm,
+    report_warning,
+)
+    
+    from test.helper import assertRegexpMatches
+    
+        dp = [[False for x in range(s+1)]for y in range(n+1)]
+    
+    def assign_clusters(data, centroids):
+    
+    # Compute distances between each data point and the set of centroids:
+    # Fill in the blank (RHS only)
+    distances_from_centroids = centroid_pairwise_dist(data,centroids)
+    
+    # Compute cluster assignments for each data point:
+    # Fill in the blank (RHS only)
+    cluster_assignment = np.argmin(distances_from_centroids,axis=1)
+    
+    return cluster_assignment
+    
+    
+'''
+* Wondering how this method works !
+* It's pretty simple.
+* Let's say you need to calculate a ^ b
+* RULE 1 : a * b = (a+a) * (b/2) ---- example : 4 * 4 = (4+4) * (4/2) = 8 * 2
+* RULE 2 : IF b is ODD, then ---- a * b = a + (a * (b - 1)) :: where (b - 1) is even.
+* Once b is even, repeat the process to get a * b
+* Repeat the process till b = 1 OR b = 0, because a*1 = a AND a*0 = 0
+*
+* As far as the modulo is concerned,
+* the fact : (a+b) % c = ((a%c) + (b%c)) % c
+* Now apply RULE 1 OR 2, whichever is required.
+'''
+
+    
+    # frequency taken from http://en.wikipedia.org/wiki/Letter_frequency
+englishLetterFreq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97,
+                     'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25,
+                     'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36,
+                     'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29,
+                     'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10,
+                     'Z': 0.07}
+ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    
+@functools.lru_cache()
+def get_hstore_oids(connection_alias):
+    '''Return hstore and hstore array OIDs.'''
+    with connections[connection_alias].cursor() as cursor:
+        cursor.execute(
+            'SELECT t.oid, typarray '
+            'FROM pg_type t '
+            'JOIN pg_namespace ns ON typnamespace = ns.oid '
+            'WHERE typname = 'hstore''
         )
-        self.assertEqual(
-            header,
-            '1:test:1:1234567890:'
-            '0fc3d90d83ac7433a5376c17f2aea9b470c368740c91c513e819e3a4980349de'
-        )
+        oids = []
+        array_oids = []
+        for row in cursor:
+            oids.append(row[0])
+            array_oids.append(row[1])
+        return tuple(oids), tuple(array_oids)
     
-        def test_unpad_evil_message(self):
-        from r2.lib.tracking import _unpad_message
-        evil = ('a' * 88) + chr(57)
-        result = _unpad_message(evil)
-        self.assertEquals(result, '')
+        class Meta:
+        abstract = True
+        verbose_name = _('session')
+        verbose_name_plural = _('sessions')
+    
+        class Meta(AbstractBaseSession.Meta):
+        db_table = 'django_session'
+
+    
+    
+@functools.lru_cache()
+def get_default_renderer():
+    renderer_class = import_string(settings.FORM_RENDERER)
+    return renderer_class()
+    
+        fpath = os.path.join(path, 'train')
+    x_train, y_train = load_batch(fpath, label_key=label_mode + '_labels')
+    
+        with gzip.open(paths[0], 'rb') as lbpath:
+        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+    
+    # input image dimensions
+img_rows, img_cols = 28, 28
+    
+    print('Vectorizing sequence data...')
+tokenizer = Tokenizer(num_words=max_words)
+x_train = tokenizer.sequences_to_matrix(x_train, mode='binary')
+x_test = tokenizer.sequences_to_matrix(x_test, mode='binary')
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+    
+    Compares the performance of a simple MLP using two
+different activation functions: RELU and SELU
+on the Reuters newswire topic classification task.
+    
+    
+def test_conv_output_length():
+    assert conv_utils.conv_output_length(None, 7, 'same', 1) is None
+    assert conv_utils.conv_output_length(224, 7, 'same', 1) == 224
+    assert conv_utils.conv_output_length(224, 7, 'same', 2) == 112
+    assert conv_utils.conv_output_length(32, 5, 'valid', 1) == 28
+    assert conv_utils.conv_output_length(32, 5, 'valid', 2) == 14
+    assert conv_utils.conv_output_length(32, 5, 'causal', 1) == 32
+    assert conv_utils.conv_output_length(32, 5, 'causal', 2) == 16
+    assert conv_utils.conv_output_length(32, 5, 'full', 1) == 36
+    assert conv_utils.conv_output_length(32, 5, 'full', 2) == 18
+    
+    from .base_layer import Layer
+from .base_layer import Node
+from .. import backend as K
+from ..legacy import interfaces
+from ..utils.generic_utils import unpack_singleton
+    
+    
+def to_native_string(string, encoding='ascii'):
+    '''Given a string object, regardless of type, returns a representation of
+    that string in the native string type, encoding and decoding where
+    necessary. This assumes ASCII unless told otherwise.
+    '''
+    if isinstance(string, builtin_str):
+        out = string
+    else:
+        if is_py2:
+            out = string.encode(encoding)
+        else:
+            out = string.decode(encoding)
+    
+    import pytest
+from requests.compat import urljoin
+    
+        def test_lower_items(self):
+        assert list(self.case_insensitive_dict.lower_items()) == [('accept', 'application/json')]
+    
+        @classmethod
+    def text_response_server(cls, text, request_timeout=0.5, **kwargs):
+        def text_response_handler(sock):
+            request_content = consume_socket_content(sock, timeout=request_timeout)
+            sock.send(text.encode('utf-8'))
+    
+    import pytest
+    
+    
+@pytest.mark.parametrize(
+    'value, expected', (
+        (
+            '<http:/.../front.jpeg>; rel=front; type='image/jpeg'',
+            [{'url': 'http:/.../front.jpeg', 'rel': 'front', 'type': 'image/jpeg'}]
+        ),
+        (
+            '<http:/.../front.jpeg>',
+            [{'url': 'http:/.../front.jpeg'}]
+        ),
+        (
+            '<http:/.../front.jpeg>;',
+            [{'url': 'http:/.../front.jpeg'}]
+        ),
+        (
+            '<http:/.../front.jpeg>; type='image/jpeg',<http://.../back.jpeg>;',
+            [
+                {'url': 'http:/.../front.jpeg', 'type': 'image/jpeg'},
+                {'url': 'http://.../back.jpeg'}
+            ]
+        ),
+        (
+            '',
+            []
+        ),
+    ))
+def test_parse_header_links(value, expected):
+    assert parse_header_links(value) == expected
+    
+        :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
+    
+        def test_default_decoder_raises_deserialization_error(self):
+        from acme.fields import RFC3339Field
+        self.assertRaises(
+            jose.DeserializationError, RFC3339Field.default_decoder, '')
+    
+    # Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#html_theme_options = {}
+    
+    REWRITE_HTTPS_ARGS_WITH_END = [
+    '^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[END,NE,R=permanent]']
+'''Apache version >= 2.3.9 rewrite rule arguments used for redirections to
+    https vhost'''
+    
+    # If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+    
+    ISO2022JP_st = (
+eStart,     3,eError,eStart,eStart,eStart,eStart,eStart,# 00-07
+eStart,eStart,eError,eError,eError,eError,eError,eError,# 08-0f
+eError,eError,eError,eError,eItsMe,eItsMe,eItsMe,eItsMe,# 10-17
+eItsMe,eItsMe,eItsMe,eItsMe,eItsMe,eItsMe,eError,eError,# 18-1f
+eError,     5,eError,eError,eError,     4,eError,eError,# 20-27
+eError,eError,eError,     6,eItsMe,eError,eItsMe,eError,# 28-2f
+eError,eError,eError,eError,eError,eError,eItsMe,eItsMe,# 30-37
+eError,eError,eError,eItsMe,eError,eError,eError,eError,# 38-3f
+eError,eError,eError,eError,eItsMe,eError,eStart,eStart,# 40-47
+)
+    
+    # 128  --> 0.77094
+# 256  --> 0.85710
+# 512  --> 0.92635
+# 1024 --> 0.97130
+# 2048 --> 0.99431
+#
+# Ideal Distribution Ratio = 0.92635 / (1-0.92635) = 12.58
+# Random Distribution Ration = 512 / (2965+62+83+86-512) = 0.191
+#
+# Typical Distribution Ratio, 25% of IDR
+    
+            # return its order if it is hiragana
+        if len(aBuf) > 1:
+            second_char = wrap_ord(aBuf[1])
+            if (first_char == 0xA4) and (0xA1 <= second_char <= 0xF3):
+                return second_char - 0xA1, charLen
+    
+    # 255: Control characters that usually does not exist in any text
+# 254: Carriage/Return
+# 253: symbol (punctuation) that does not belong to word
+# 252: 0 - 9
