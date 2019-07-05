660739101c025e065ae6a9131b62f8ff7d732d61
SUNsung <weatherius@gmail.com>
2019-07-05 21:58:22 +0300

    Rewrite [01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py] File in: 2019-07-05 21:58:22

--- pyton/01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py
+++ pyton/01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py
@@ -1,21 +1,117 @@
 
         
         
-def setup(app):
-    app.add_config_value('edit_on_github_project', '', True)
-    app.add_config_value('edit_on_github_branch', 'master', True)
-    app.add_config_value('edit_on_github_src_path', '', True)  # 'eg' 'docs/'
-    app.connect('html-page-context', html_page_context)
-
+if __name__ == '__main__':
+    ''''''
+    # _test()
     
-            request[KEY_REAL_IP] = ip_address(ip_to_mock)
+        def _call(self, x):
+        ''''''
+        o = self.act_fn(tf.matmul(x, self.W) + self.b)
+        return o
+    
+            print('Success: test_queue_from_stacks')
+    
+    def main():
+    testing = TestBinaryTree()
+    testing.test_insert_traversals()
+    testing.test_max_min_nodes()
+    testing.test_delete()
+    
+if __name__=='__main__':
+    main()
+    
+                self.assertEqual(res.status, 200)
+            self.assertTrue(message.del_on_recipient)
+    
+    import unittest
+    
+    import unittest
+    
+        def test_no_resize(self):
+        image = dict(url='http://s3.amazonaws.com/a.jpg', width=200,
+                      height=800)
+        url = self.provider.resize_image(image)
+        self.assertEqual(url, 'https://unsplash.it/200/400')
+    
+        def test_reddit_ios_integration(self):
+        user_agent = ('Reddit/Version 1.1/Build 1106/iOS Version 9.3.2 '
+                      '(Build 13F69)')
+        outs = detect(user_agent)
+        self.assertEqual(outs['browser']['name'], RedditIOSDetector.name)
+    
+        def reset(self):
+        '''Reset the stats.'''
+        self.stats = []
+        self.cpu_number = 0
     
     
-def is_code(line, indent_depth = 4):
-    '''returns the indent depth, 0 means not code in markup'''
-    if line.startswith(' ' * indent_depth):
-        return len(line) - len(line.lstrip(' '))
-    return 0
+def main():
+    '''Main entry point for Glances.
     
-            #Sorting all filenames in the directory, to maintain the order of the PDF
-        allmd.sort()
+        def update(self, process_list):
+        '''Update the AMP'''
+        # Get the Nginx status
+        logger.debug('{}: Update stats using status URL {}'.format(self.NAME, self.get('status_url')))
+        res = requests.get(self.get('status_url'))
+        if res.ok:
+            # u'Active connections: 1 \nserver accepts handled requests\n 1 1 1 \nReading: 0 Writing: 1 Waiting: 0 \n'
+            self.set_result(res.text.rstrip())
+        else:
+            logger.debug('{}: Can not grab status URL {} ({})'.format(self.NAME, self.get('status_url'), res.reason))
+    
+            History is stored as a list for tuple: [(date, value), ...]
+        '''
+        self._name = name
+        self._description = description
+        self._value = None
+        self._history_max_size = history_max_size
+        self._history = []
+    
+                # XXX *BSDs: Segmentation fault (core dumped)
+            # -- https://bitbucket.org/al45tair/netifaces/issues/15
+            if not BSD:
+                try:
+                    # -B @ overwrite the dynamic IPv4 choice
+                    if zeroconf_bind_address == '0.0.0.0':
+                        zeroconf_bind_address = self.find_active_ip_address()
+                except KeyError:
+                    # Issue #528 (no network interface available)
+                    pass
+    
+        def _update_event(self, event_index, event_state, event_type, event_value,
+                      proc_list, proc_desc, peak_time):
+        '''Update an event in the list'''
+        if event_state == 'OK' or event_state == 'CAREFUL':
+            # Reset the automatic process sort key
+            self.reset_process_sort()
+    
+            # Loop over plugins to export
+        for plugin in self.plugins_to_export():
+            if isinstance(all_stats[plugin], list):
+                for stat in all_stats[plugin]:
+                    # First line: header
+                    if self.first_line:
+                        csv_header += ('{}_{}_{}'.format(
+                            plugin, self.get_item_key(stat), item) for item in stat)
+                    # Others lines: stats
+                    csv_data += itervalues(stat)
+            elif isinstance(all_stats[plugin], dict):
+                # First line: header
+                if self.first_line:
+                    fieldnames = iterkeys(all_stats[plugin])
+                    csv_header += ('{}_{}'.format(plugin, fieldname)
+                                   for fieldname in fieldnames)
+                # Others lines: stats
+                csv_data += itervalues(all_stats[plugin])
+    
+        def init(self):
+        '''Init the connection to the Riemann server.'''
+        if not self.export_enable:
+            return None
+        try:
+            client = bernhard.Client(host=self.host, port=self.port)
+            return client
+        except Exception as e:
+            logger.critical('Connection to Riemann failed : %s ' % e)
+            return None
