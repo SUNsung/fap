18e6abdbe4cf1e0631cd7f4e7046a467fec4f31e
SUNsung <weatherius@gmail.com>
2019-07-05 06:59:47 +0300

    Rewrite [49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb] File in: 2019-07-05 06:59:47

--- ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
+++ ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
@@ -1,84 +1,136 @@
 
         
-            private
+        class CommentRelevance
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
     
-      class TestSubHash < TestHash
-    class SubHash < Hash
-      def reject(*)
-        super
-      end
+      def test_feed_entry_type_option_specified
+    with_restful_routing(:scrolls) do
+      get :index, params: { id: 'entry_type_options' }
+      assert_select 'entry link[rel=alternate][type=\'text/xml\']'
     end
-    
-        unless SCANNER_EVENT_TABLE.key?(:ignored_sp)
-      SCANNER_EVENT_TABLE[:ignored_sp] = 1
-      SCANNER_EVENTS << :ignored_sp
-      EVENTS << :ignored_sp
-    end
-    
-      def test_using_wrong_argument
-    bug = '[ruby-dev:50270] [Bug #13956]'
-    pattern = /expected Module/
-    assert_separately([], '#{<<-'begin;'}\n#{<<-'end;'}')
-    bug = ''#{bug.dump}
-    pattern = /#{pattern}/
-    begin;
-      assert_raise_with_message(TypeError, pattern, bug) {
-        using(1) do end
-      }
-    end;
-    assert_separately([], '#{<<-'begin;'}\n#{<<-'end;'}')
-    bug = ''#{bug.dump}
-    pattern = /#{pattern}/
-    begin;
-      assert_raise_with_message(TypeError, pattern, bug) {
-        Module.new {using(1) {}}
-      }
-    end;
   end
     
-            gv = eval('global_variables', bind).collect{|m| m.to_s}
-        lv = eval('local_variables', bind).collect{|m| m.to_s}
-        iv = eval('instance_variables', bind).collect{|m| m.to_s}
-        cv = eval('self.class.constants', bind).collect{|m| m.to_s}
-    
-          require 'rack'
-      puts 'EXEC: \#{caller.grep(/load/).empty? ? 'exec' : 'load'}'
-      puts 'ARGS: \#{$0} \#{ARGV.join(' ')}'
-      puts 'RACK: \#{RACK}'
-      process_title = `ps -o args -p \#{Process.pid}`.split('\n', 2).last.strip
-      puts 'PROCESS: \#{process_title}'
-    RUBY
-    
-    def r(val); *a = yield(); test_ok(a == val, 2); end
-r([nil]){next}
-r([nil]){next nil}
-r([1]){next 1}
-r([]){next []}
-r([1]){next [1]}
-r([nil]){next [nil]}
-r([[]]){next [[]]}
-r([1,2]){next [1,2]}
-r([]){next [*[]]}
-r([1]){next [*[1]]}
-r([1,2]){next [*[1,2]]}
-    
-        config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=#{2.days.to_i}'
-    }
-  else
-    config.action_controller.perform_caching = false
-    
-      # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
-    
-        it 'accepts jsfiddle link' do
-      liquid = generate_new_liquid(jsfiddle_link)
-      rendered_jsfiddle_iframe = liquid.render
-      Approvals.verify(rendered_jsfiddle_iframe, name: 'jsfiddle_liquid_tag', format: :html)
+      describe '#relative_distance_of_time_in_words' do
+    it 'in the past' do
+      expect(relative_distance_of_time_in_words(Time.now-5.minutes)).to eq('5m ago')
     end
     
-      def user_is_author?
-    record.user_id == user.id
+          context '#restart_dead_workers' do
+        before do
+          mock.instance_of(HuginnScheduler).run!
+          mock.instance_of(DelayedJobWorker).run!
+          @agent_runner.send(:run_workers)
+    
+      describe '#sort_tuples!' do
+    let(:tuples) {
+      time = Time.now
+      [
+        [2, 'a', time - 1],  # 0
+        [2, 'b', time - 1],  # 1
+        [1, 'b', time - 1],  # 2
+        [1, 'b', time],      # 3
+        [1, 'a', time],      # 4
+        [2, 'a', time + 1],  # 5
+        [2, 'a', time],      # 6
+      ]
+    }
+    
+      let :new_extract do
+    {
+      'url' => { 'css' => '#comic img', 'value' => '@src' },
+      'title' => { 'css' => '#comic img', 'value' => '@alt' },
+      'hovertext' => { 'css' => '#comic img', 'value' => '@title', 'hidden' => true }
+    }
+  end
+    
+        it 'should raise error when response has an error' do
+      stub(HTTParty).post { {'error' => {'message' => 'Sample error'}} }
+      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Sample error/)
+    end
   end
 end
+
+    
+          attr_reader :scope_name, :resource
+    
+            # Generate a token checking if one does not already exist in the database.
+        def remember_token #:nodoc:
+          loop do
+            token = Devise.friendly_token
+            break token unless to_adapter.find_first({ remember_token: token })
+          end
+        end
+    
+        def extract_to_dir(unpack_dir, basename:, verbose:)
+      system_command! AIR_APPLICATION_INSTALLER,
+                      args:    ['-silent', '-location', unpack_dir, path],
+                      verbose: verbose
+    end
+  end
+end
+
+    
+      def ruby_prefix
+    RbConfig::CONFIG['prefix']
+  end
+    
+            private
+    
+    if $0 == __FILE__
+  begin
+    LogStash::PluginManager::Main.run('bin/logstash-plugin', ARGV)
+  rescue LogStash::PluginManager::Error => e
+    $stderr.puts(e.message)
+    exit(1)
+  end
+end
+
+    
+      # retrieve only the latest spec for all locally installed plugins
+  # @return [Hash] result hash {plugin_name.downcase => plugin_spec}
+  def find_latest_gem_specs
+    LogStash::PluginManager.all_installed_plugins_gem_specs(gemfile).inject({}) do |result, spec|
+      previous = result[spec.name.downcase]
+      result[spec.name.downcase] = previous ? [previous, spec].max_by{|s| s.version} : spec
+      result
+    end
+  end
+end
+
+    
+        desc 'Halt all VM's involved in the acceptance test round'
+    task :halt, :platform do |t, args|
+      config   = PlatformConfig.new
+      experimental = (ENV['LS_QA_EXPERIMENTAL_OS'].to_s.downcase || 'false') == 'true'
+      machines = config.select_names_for(args[:platform], {'experimental' => experimental})
+    
+          # Checks whether this node body is a void context.
+      #
+      # @return [Boolean] whether the `def` node body is a void context
+      def void_context?
+        method?(:initialize) || assignment_method?
+      end
+    
+            def delta(first, second, alignment = :left)
+          case alignment
+          when :left
+            first.column - second.column
+          when :right
+            first.last_column - second.last_column
+          else
+            0
+          end
+        end
+    
+        # Outputs the post.date as formatted html, with hooks for CSS styling.
+    #
+    #  +date+ is the date object to format as HTML.
+    #
+    # Returns string
+    def date_to_html_string(date)
+      result = '<span class='month'>' + date.strftime('%b').upcase + '</span> '
+      result << date.strftime('<span class='day'>%d</span> ')
+      result << date.strftime('<span class='year'>%Y</span> ')
+      result
+    end
