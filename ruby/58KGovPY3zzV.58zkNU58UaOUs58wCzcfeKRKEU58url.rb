cc77cdc19a91a26bcc128f2e82d43a7a66782b75
SUNsung <weatherius@gmail.com>
2019-07-05 09:43:23 +0300

    Rewrite [58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb] File in: 2019-07-05 09:43:23

--- ruby/58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb
+++ ruby/58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb
@@ -1,62 +1,126 @@
 
         
-            self.req_types.each do |key, i|
-      query = self.where(req_type: i)
-      s['#{key}_total']   = query.sum(:count)
-      s['#{key}_30_days'] = query.where('date > ?', 30.days.ago).sum(:count)
-      s['#{key}_7_days']  = query.where('date > ?', 7.days.ago).sum(:count)
-    end
-    
-        def later(desc = nil, db = RailsMultisite::ConnectionManagement.current_db, &blk)
-      if @async
-        start_thread if !@thread&.alive? && !@paused
-        @queue << [db, blk, desc]
-      else
-        blk.call
-      end
-    end
-    
-          DB.exec(<<~SQL, id: d.id, sequence: sequence, data: data)
-        UPDATE drafts
-           SET sequence = :sequence
-             , data = :data
-             , revisions = revisions + 1
-         WHERE id = :id
-      SQL
-    else
-      Draft.create!(user_id: user.id, draft_key: key, data: data, sequence: sequence)
-    end
-    
-      def not_modified?
-    cache_time =
-      begin
-        Time.rfc2822(request.env['HTTP_IF_MODIFIED_SINCE'])
-      rescue ArgumentError
-        nil
-      end
-    
-      def show
-    @status = status_finder.status
-    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
-  end
-    
-      def process_salmon
-    SalmonWorker.perform_async(@account.id, payload.force_encoding('UTF-8'))
+          def safely_remove_file(_path)
+    run_vagrant_command('rm #{test_file}')
+  rescue
+    VagrantHelpers::VagrantSSHCommandError
   end
 end
+    
+        def dry_run
+      ['--dry-run', '-n',
+       'Do a dry run without executing actions',
+       lambda do |_value|
+         Configuration.env.set(:sshkit_backend, SSHKit::Backend::Printer)
+       end]
+    end
+    
+            if echo?
+          $stdin.gets
+        else
+          $stdin.noecho(&:gets).tap { $stdout.print '\n' }
+        end
+      rescue Errno::EIO
+        # when stdio gets closed
+        return
+      end
+    
+        def self.find_by_name_with_wildcards(pattern)
+      re = transform_pattern_into_re(pattern)
+      ::Gem::Specification.find_all.select do |specification|
+        specification.name =~ re
+      end
+    end
+    
+    # ECMA-262, section 15.1.3
+    def Encode(uri, unescape)
+      uriLength = uri.length;
+      # We are going to pass result to %StringFromCharCodeArray
+      # which does not expect any getters/setters installed
+      # on the incoming array.
+      result    = Array.new(uriLength);
+      index = 0;
+      k = -1;
+      while ((k+=1) < uriLength) do
+        cc1 = uri.charCodeAt(k);
+        next if cc1.nil?
+        if (self.send(unescape, cc1))
+          result[index] = cc1;
+          index += 1
+        else
+          if (cc1 >= 0xDC00 && cc1 <= 0xDFFF);
+            throw('URI malformed')
+          end
+          if (cc1 < 0xD800 || cc1 > 0xDBFF)
+            index = URIEncodeSingle(cc1, result, index);
+          else
+            k+=1;
+            if (k == uriLength);
+              throw('URI malformed')
+            end
+            cc2 = uri.charCodeAt(k);
+            if (cc2 < 0xDC00 || cc2 > 0xDFFF);
+              throw('URI malformed')
+            end
+            index = URIEncodePair(cc1, cc2, result, index);
+          end
+        end
+      end
+      # use .compact to get rid of nils from charCodeAt
+      # return %StringFromCharCodeArray(result);
+      # 'c' = 8 bit signed char
+      # http://www.ruby-doc.org/core-1.9.3/Array.html#method-i-pack
+      return result.compact.pack 'c*'
+    end
+  end # class << self
+end # module
 
     
-      private
+          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
+      def left_shift int, shift
+        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
+        # 1>>31, 2**32
+        (r & 2147483648) == 0 ? r : r - 4294967296
+      end
     
-      private
+        post '/edit/A', :header => 'header',
+         :footer            => 'footer', :page => 'A', :sidebar => 'sidebar', :message => 'def'
+    follow_redirect!
+    assert_equal '/A', last_request.fullpath
+    assert last_response.ok?
     
-      def test_fetch2
-    assert_equal(:bar, @h.fetch(0, :foo) { :bar })
+    context 'Precious::Views::LatestChanges' do
+  include Rack::Test::Methods
+  
+  def app
+    Precious::App
+  end
+  
+  setup do
+    @path = cloned_testpath('examples/lotr.git')
+    @wiki = Gollum::Wiki.new(@path)
+    Precious::App.set(:gollum_path, @path)
+    Precious::App.set(:wiki_options, {:latest_changes_count => 10})
   end
     
-      # Show full error reports.
-  config.consider_all_requests_local = true
+        def forbid(msg = 'Forbidden. This wiki is set to no-edit mode.')
+      @message = msg
+      status 403
+      halt mustache :error
+    end
     
-      # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+      def send_sinatra_file(path, &missing_file_block)
+    file_path = File.join(File.dirname(__FILE__), 'public',  path)
+    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
+    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+  end
+    
+        def render(context)
+      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
+      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
+      file = code_path + @file
+    
+        def render(context)
+      file_dir = (context.registers[:site].source || 'source')
+      file_path = Pathname.new(file_dir).expand_path
+      file = file_path + @file
