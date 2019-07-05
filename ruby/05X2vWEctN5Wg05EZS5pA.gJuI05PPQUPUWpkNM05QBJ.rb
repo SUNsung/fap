cf2ee1976e2cdd031c5901021227c60caf0a930b
SUNsung <weatherius@gmail.com>
2019-07-05 08:39:46 +0300

    Rewrite [05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb] File in: 2019-07-05 08:39:46

--- ruby/05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb
+++ ruby/05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb
@@ -1,82 +1,65 @@
 
         
-            def as_json
-      @pages
-    end
+          private
     
-        def parse_as_fragment
-      Nokogiri::HTML.fragment @content, 'UTF-8'
+      def hub_secret
+    params['hub.secret']
+  end
+    
+      def update
+    if verify_payload?
+      process_salmon
+      head 202
+    elsif payload.present?
+      render plain: signature_verification_failure_reason, status: 401
+    else
+      head 400
     end
+  end
+    
+      def session_needs_update?
+    !current_session.nil? && current_session.updated_at < UPDATE_SIGN_IN_HOURS.hours.ago
   end
 end
 
     
-        def mime_type
-      headers['Content-Type'] || 'text/plain'
-    end
+    # Thanks in announcement ===============================================
     
-        private
+    get '/' do
+  halt erb(:login) unless params[:user]
+  erb :chat, :locals => { :user => params[:user].gsub(/\W/, '') }
+end
     
-            css('h1 + code').each do |node|
-          node.before('<p></p>')
-          while node.next_element.name == 'code'
-            node.previous_element << ' '
-            node.previous_element << node.next_element
+            DIRECTIVES.each do |d|
+          if options.key?(d)
+            directives << '#{d.to_s.sub(/_/, '-')} #{options[d]}'
           end
-          node.previous_element.prepend_child(node)
         end
     
-          def mod
-        return @mod if defined?(@mod)
-        @mod = slug[/api\/([\w\-\.]+)\//, 1]
-        @mod.remove! 'angular2.' if @mod
-        @mod
-      end
-    end
-  end
-end
-
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   XSS
+    # Supported browsers:: all
+    # More infos::         http://en.wikipedia.org/wiki/Cross-site_scripting
+    #
+    # Automatically escapes Rack::Request#params so they can be embedded in HTML
+    # or JavaScript without any further issues. Calls +html_safe+ on the escaped
+    # strings if defined, to avoid double-escaping in Rails.
+    #
+    # Options:
+    # escape:: What escaping modes to use, should be Symbol or Array of Symbols.
+    #          Available: :html (default), :javascript, :url
+    class EscapedParams < Base
+      extend Rack::Utils
     
-            doc
-      end
-    end
-  end
-end
-
-    
-      # Code Quality
-  gem 'inch_by_inch'
-  gem 'rubocop'
-    
-        if rbenv_prefix = prefix_from_bin('rbenv')
-      prefixes << rbenv_prefix
+        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
+      env = {'HTTP_HOST' => 'foo.com'}
+      subject.options[:allow_empty_referrer] = false
+      expect(subject.referrer(env)).to be_nil
     end
     
-            def initialize(argv)
-          @pod_name = argv.shift_argument
-          @short_output = argv.flag?('short')
-          super
-        end
-    
-          def report
-        <<-EOS
-    
-            self.description = <<-DESC
-          Lints the spec-repo `NAME`. If a directory is provided it is assumed
-          to be the root of a repo. Finally, if `NAME` is not provided this
-          will lint all the spec-repos known to CocoaPods.
-        DESC
-    
-    LogStash::Bundler.setup!
-    
-    require 'bootstrap/environment'
-    
-    task :spec    => 'spec:all'
-task :default => :spec
-    
-        before do
-      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
-      # Logstash won't update when we have a pinned version in the gemfile so we remove them
-      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
-      expect(logstash).to have_installed?(plugin_name, previous_version)
+        it 'denies requests with duplicate session cookies' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
+      expect(last_response).not_to be_ok
     end
