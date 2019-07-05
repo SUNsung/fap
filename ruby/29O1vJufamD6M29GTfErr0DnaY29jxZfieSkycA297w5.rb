15a59bbb54551d46af03ce98f5e07e45f4c07877
SUNsung <weatherius@gmail.com>
2019-07-05 04:52:49 +0300

    Rewrite [29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb] File in: 2019-07-05 04:52:49

--- ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
+++ ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
@@ -1,61 +1,40 @@
 
         
-        require 'action_view/helpers/tags/collection_helpers'
+          attr_accessor :comments, :comment_ids
+  def comments_attributes=(attributes); end
     
-                options = options.dup
-            options[:field_name]           = @method_name
-            options[:include_position]     = true
-            options[:prefix]             ||= @object_name
-            options[:index]                = @auto_index if @auto_index && !options.has_key?(:index)
+          super
+    end
     
-          def self.symbols
-        type_klass::SET.symbols
+          def call
+        ask_question
+        value_or_default
       end
+    
+          attr_reader :validators
+    
+    # We use a special :_default_git value so that SCMResolver can tell whether the
+# default has been replaced by the user via `set`.
+set_if_empty :scm, Capistrano::Configuration::SCMResolver::DEFAULT_GIT
+set_if_empty :branch, 'master'
+set_if_empty :deploy_to, -> { '/var/www/#{fetch(:application)}' }
+set_if_empty :tmp_dir, '/tmp'
+    
+    # This is basically a copy of the original bundler 'bundle' shim
+# with the addition of the loading of our Bundler patches that
+# modify Bundler's caching behaviour.
+    
+      # retrieve only the latest spec for all locally installed plugins
+  # @return [Hash] result hash {plugin_name.downcase => plugin_spec}
+  def find_latest_gem_specs
+    LogStash::PluginManager.all_installed_plugins_gem_specs(gemfile).inject({}) do |result, spec|
+      previous = result[spec.name.downcase]
+      result[spec.name.downcase] = previous ? [previous, spec].max_by{|s| s.version} : spec
+      result
     end
   end
 end
 
-    
-          def index
-        render template: 'some/template', locals: { foo: 'less than 3' }
-      end
-    end
-    
-    class Api::Web::SettingsController < Api::Web::BaseController
-  respond_to :json
-    
-      def test_font_helper_with_suffix_sharp
-    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
-  end
-    
-    require 'bundler/cli'
-require 'bundler/friendly_errors'
-    
-    if $0 == __FILE__
-  begin
-    LogStash::PluginManager::Main.run('bin/logstash-plugin', ARGV)
-  rescue LogStash::PluginManager::Error => e
-    $stderr.puts(e.message)
-    exit(1)
-  end
-end
-
-    
-        def prepare_package(explicit_plugins, temp_path)
-      FileUtils.mkdir_p(::File.join(temp_path, LOGSTASH_DIR))
-      FileUtils.mkdir_p(::File.join(temp_path, DEPENDENCIES_DIR))
-    
-            if Utils::HttpClient.remote_file_exist?(uri)
-          PluginManager.ui.debug('Found package at: #{uri}')
-          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
-        else
-          PluginManager.ui.debug('Package not found at: #{uri}')
-          return nil
-        end
-      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
-        # This probably means there is a firewall in place of the proxy is not correctly configured.
-        # So lets skip this strategy but log a meaningful errors.
-        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
     
         def user_feedback_string_for(action, platform, machines, options={})
       experimental_string = options['experimental'] ? 'experimental' : 'non experimental'
@@ -63,75 +42,31 @@ end
       '#{message} for #{platform}: #{machines}' if !platform.nil?
     end
     
-        after(:all) do
+              it 'successfully install the plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest')
+          end
+    
+        after :each do
       logstash.uninstall
     end
     
-        before do
-      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
-      # Logstash won't update when we have a pinned version in the gemfile so we remove them
-      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
-      expect(logstash).to have_installed?(plugin_name, previous_version)
-    end
-    
-    # Set some attributes
-package.name = 'my-service'
-package.version = '1.0'
-    
-          if @command.inputs
-        mandatory(@command.input_type == 'dir', '--inputs is only valid with -s dir')
+          # Returns an array of all the when branches in the `case` statement.
+      #
+      # @return [Array<WhenNode>] an array of `when` nodes
+      def when_branches
+        node_parts[1...-1]
       end
     
-        # npm installs dependencies in the module itself, so if you do
-    # 'npm install express' it installs dependencies (like 'connect')
-    # to: node_modules/express/node_modules/connect/...
-    #
-    # To that end, I don't think we necessarily need to include
-    # any automatic dependency information since every 'npm install'
-    # is fully self-contained. That's why you don't see any bother, yet,
-    # to include the package's dependencies in here.
-    #
-    # It's possible someone will want to decouple that in the future,
-    # but I will wait for that feature request.
-  end
+          # Checks whether this node body is a void context.
+      #
+      # @return [Boolean] whether the `def` node body is a void context
+      def void_context?
+        method?(:initialize) || assignment_method?
+      end
     
-      option '--postinstall-action', 'POSTINSTALL_ACTION',
-    'Post-install action provided in package metadata. ' \
-    'Optionally one of '#{POSTINSTALL_ACTIONS.join('', '')}'.' do |value|
-    if !POSTINSTALL_ACTIONS.include?(value)
-      raise ArgumentError, 'osxpkg-postinstall-action value of '#{value}' is invalid. ' \
-        'Must be one of #{POSTINSTALL_ACTIONS.join(', ')}'
-    end
-    value
-  end
-    
-        # easy_install will put stuff in @tmpdir/packagename/, so find that:
-    #  @tmpdir/somepackage/setup.py
-    dirs = ::Dir.glob(File.join(target, '*'))
-    if dirs.length != 1
-      raise 'Unexpected directory layout after easy_install. Maybe file a bug? The directory is #{build_path}'
-    end
-    return dirs.first
-  end # def download
-    
-      # Returns the path to the tar file containing the packed up staging directory
-  def payload
-    payload_tar = build_path('payload.tar')
-    logger.info('Creating payload tar ', :path => payload_tar)
-    
-      def default_output
-    v = version
-    v = '#{epoch}:#{v}' if epoch
-    if iteration
-      '#{name}_#{v}-#{iteration}_#{architecture}.#{type}'
-    else
-      '#{name}_#{v}_#{architecture}.#{type}'
-    end
-  end # def default_output
-end # class FPM::Deb
-    
-        stdout_r_str = nil
-    exit_code = execmd(args, :stdin=>false, :stderr=>false) do |stdout|
-      stdout_r_str = stdout.read
-    end
-    success = (exit_code == 0)
+              lambda do |corrector|
+            swap_range(corrector, current_range, previous_range)
+          end
+        end
