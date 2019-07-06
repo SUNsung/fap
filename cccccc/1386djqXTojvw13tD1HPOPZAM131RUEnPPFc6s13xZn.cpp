21e91e9080c303af75577942096e2d1e67e732c9
SUNsung <weatherius@gmail.com>
2019-07-06 22:18:16 +0300

    Rewrite [1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp] File in: 2019-07-06 22:18:15

--- cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
+++ cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
@@ -1,202 +1,243 @@
 
         
-        namespace internal {
+            struct Margin {
+        Margin() : left(0), right(0), top(0), bottom(0) {}
+        Margin(size_t left_, size_t right_, size_t top_, size_t bottom_)
+            : left(left_), right(right_), top(top_), bottom(bottom_) {}
     }
     
-     private:
-  std::vector<TestPartResult> array_;
     
-    #define ASSERT_STREQ(expected, actual) \
-  ASSERT_PRED_FORMAT2(::testing::internal::CmpHelperSTREQ, expected, actual)
-#define ASSERT_STRNE(s1, s2) \
-  ASSERT_PRED_FORMAT2(::testing::internal::CmpHelperSTRNE, s1, s2)
-#define ASSERT_STRCASEEQ(expected, actual) \
-  ASSERT_PRED_FORMAT2(::testing::internal::CmpHelperSTRCASEEQ, expected, actual)
-#define ASSERT_STRCASENE(s1, s2)\
-  ASSERT_PRED_FORMAT2(::testing::internal::CmpHelperSTRCASENE, s1, s2)
-    
-    #if GTEST_HAS_EXCEPTIONS
-# include <stdexcept>
-#endif
-    
-    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
-    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
-    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
-    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
-    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15>
-struct Templates15 {
-  typedef TemplateSel<T1> Head;
-  typedef Templates14<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
-      T15> Tail;
+    {    void operator() (const u8 * src0, const u8 * src1, u8 * dst) const
+    {
+        dst[0] = src0[0] | src1[0];
+    }
 };
     
-    TEST(ListenersTest, DoesNotLeak) {
-  Water* water = new Water;
-  delete water;
-}
+            const f32* ln0 = idx_rm1 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rm1) : tmp;
+        const f32* ln1 = internal::getRowPtr(srcBase, srcStride, i);
+        const f32* ln2 = internal::getRowPtr(srcBase, srcStride, i + 1);
+        const f32* ln3 = idx_rp2 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rp2) : tmp;
+    
+    #ifndef CAROTENE_SRC_COMMON_HPP
+#define CAROTENE_SRC_COMMON_HPP
     
     
-    {			PathCombine(userConEmuCfgPath, userConfigDirPath, L'ConEmu-%COMPUTERNAME%.xml');
-			ExpandEnvironmentStrings(userConEmuCfgPath, userConEmuCfgPath, sizeof(userConEmuCfgPath) / sizeof(userConEmuCfgPath[0]));
-		}
-	}
-	else if (PathFileExists(userCfgPath)) // config/user_conemu.xml exists, use it. 
-	{
-		if (cfgRoot.length() == 0) // '/c [path]' was NOT specified
-		{
-			if (PathFileExists(cfgPath)) // vendor/conemu-maximus5/ConEmu.xml exists, copy vendor/conemu-maximus5/ConEmu.xml to config/user_conemu.xml.
-			{
-				if (!CopyFile(cfgPath, userCfgPath, FALSE))
-				{
-					MessageBox(NULL,
-						(GetLastError() == ERROR_ACCESS_DENIED)
-						? L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/user-conemu.xml! Access Denied.'
-						: L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/user-conemu.xml!', MB_TITLE, MB_ICONSTOP);
-					exit(1);
-				}
-			}
-			else // vendor/conemu-maximus5/ConEmu.xml does not exist, copy config/user-conemu.xml to vendor/conemu-maximus5/ConEmu.xml
-			{
-				if (!CopyFile(userCfgPath, cfgPath, FALSE))
-				{
-					MessageBox(NULL,
-						(GetLastError() == ERROR_ACCESS_DENIED)
-						? L'Failed to copy config/user-conemu.xml file to vendor/conemu-maximus5/ConEmu.xml! Access Denied.'
-						: L'Failed to copy config/user-conemu.xml file to vendor/conemu-maximus5/ConEmu.xml!', MB_TITLE, MB_ICONSTOP);
-					exit(1);
-				}
-			}
-		}
-		else // '/c [path]' was specified, don't copy anything and use existing user_conemu.xml to start comemu.
-		{
-			PathCombine(userConEmuCfgPath, userConfigDirPath, L'user-ConEmu.xml');
-		}
-	}
-	else if (cfgRoot.length() == 0) // '/c [path]' was NOT specified 
-	{
-		if (PathFileExists(cfgPath)) // vendor/conemu-maximus5/ConEmu.xml exists, copy vendor/conemu-maximus5/ConEmu.xml to config/user_conemu.xml
-		{
-			if (!CopyFile(cfgPath, userCfgPath, FALSE))
-			{
-				MessageBox(NULL,
-					(GetLastError() == ERROR_ACCESS_DENIED)
-					? L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/user-conemu.xml! Access Denied.'
-					: L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/user-conemu.xml!', MB_TITLE, MB_ICONSTOP);
-				exit(1);
-			}
-			else // vendor/ConEmu.xml.default config exists, copy Cmder vendor/ConEmu.xml.default file to vendor/conemu-maximus5/ConEmu.xml.
-			{
-				if (!CopyFile(defaultCfgPath, cfgPath, FALSE))
-				{
-					MessageBox(NULL,
-						(GetLastError() == ERROR_ACCESS_DENIED)
-						? L'Failed to copy vendor/ConEmu.xml.default file to vendor/conemu-maximus5/ConEmu.xml! Access Denied.'
-						: L'Failed to copy vendor/ConEmu.xml.default file to vendor/conemu-maximus5/ConEmu.xml!', MB_TITLE, MB_ICONSTOP);
-					exit(1);
-				}
-			}
-		}
-		else {
-			if (!CopyFile(defaultCfgPath, cfgPath, FALSE))
-			{
-				MessageBox(NULL,
-					(GetLastError() == ERROR_ACCESS_DENIED)
-					? L'Failed to copy vendor/ConEmu.xml.default file to vendor/conemu-maximus5/ConEmu.xml! Access Denied.'
-					: L'Failed to copy vendor/ConEmu.xml.default file to vendor/conemu-maximus5/ConEmu.xml!', MB_TITLE, MB_ICONSTOP);
-				exit(1);
-			}
-		}
-	}
-	else if (PathFileExists(cfgPath)) // vendor/conemu-maximus5/ConEmu.xml exists, copy vendor/conemu-maximus5/ConEmu.xml to config/user_conemu.xml
-	{
-		if (!CopyFile(cfgPath, userCfgPath, FALSE))
-		{
-			MessageBox(NULL,
-				(GetLastError() == ERROR_ACCESS_DENIED)
-				? L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/user-conemu.xml! Access Denied.'
-				: L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/user-conemu.xml!', MB_TITLE, MB_ICONSTOP);
-			exit(1);
-		}
-    
-    
-    {  state->transform(x0, y0, x1, y1);
-  *x1 += tx;
-  *y1 += ty;
-  if (rotate == 90) {
-    t = *x1;
-    *x1 = -*y1;
-    *y1 = t;
-  } else if (rotate == 180) {
-    *x1 = -*x1;
-    *y1 = -*y1;
-  } else if (rotate == 270) {
-    t = *x1;
-    *x1 = *y1;
-    *y1 = -t;
-  }
-  *x1 *= xScale;
-  *y1 *= yScale;
-}
-    
-    private:
-    
-      pageObj.initDict(pageDict);
-  pageRef = pageRefA;
-    
-    #include 'goo/gtypes.h'
-#include 'goo/GooList.h'
-#include 'goo/GooString.h'
-#include 'Object.h'
-    
-    #ifdef USE_GCC_PRAGMAS
-#pragma implementation
+    {
+    {         vst1q_u16(_dst + i, vcombine_u16(vline1_u16, vline2_u16));
+     }
+})
 #endif
     
-    Stream *Parser::makeStream(Object *dict, Guchar *fileKey,
-			   CryptAlgorithm encAlgorithm, int keyLength,
-			   int objNum, int objGen, std::set<int> *fetchOriginatorNums) {
-  Object obj;
-  BaseStream *baseStr;
-  Stream *str;
-  Guint pos, endPos, length;
+            size_t i = 0;
+        while(i + 4 <= size.width)
+        {
+            size_t lim = std::min(i + DOT_FLOAT_BLOCKSIZE, size.width) - 4;
+            float32x4_t v_sum = vdupq_n_f32(0.0f);
     }
     
-      // Get stream.
-  Stream *getStream() { return lexer->getStream(); }
+            cornerpos[-1] = ncorners;
+    
+                tc = vreinterpretq_s16_u16(vsubl_u8(x4, x2));
+            tnext = vreinterpretq_s16_u16(vsubl_u8(x2, x0));
+            tnext2 = vsubq_s16(tc, tnext);
+    
+    namespace caffe2 {
+namespace {
+    }
+    }
+    
+    ```
     
     
-    {  PDFDoc *doc;
-};
+    {
+    {} // namespace
+} // namespace caffe2
+
     
-    Sound *Sound::parseSound(Object *obj)
-{
-  // let's try to see if this Object is a Sound, according to the PDF specs
-  // (section 9.2)
-  Stream *str = NULL;
-  // the Object must be a Stream
-  if (obj->isStream()) {
-    str = obj->getStream();
-  } else {
-    return NULL;
-  }
-  // the Stream must have a Dict
-  Dict *dict = str->getDict();
-  if (dict == NULL)
-    return NULL;
-  Object tmp;
-  // the Dict must have the 'R' key of type num
-  dict->lookup('R', &tmp);
-  if (tmp.isNum()) {
-    return new Sound(obj);
-  } else {
-    return NULL;
+              vector<TensorShape> out(1);
+          switch (order) {
+            case StorageOrder::NCHW:
+              out[0] = CreateTensorShape(
+                  vector<int>{N, C * kernel_h * kernel_w, out_h, out_w},
+                  TensorProto::FLOAT);
+              break;
+            case StorageOrder::NHWC:
+              out[0] = CreateTensorShape(
+                  vector<int>{N, out_h, out_w, kernel_h * kernel_w * C},
+                  TensorProto::FLOAT);
+              break;
+            default:
+              CAFFE_THROW('Unknown storage order: ', order);
+          }
+    
+    REGISTER_CUDA_OPERATOR(LC1D, LocallyConnectedOp<float, CUDAContext>);
+REGISTER_CUDA_OPERATOR(
+    LC1DGradient,
+    LocallyConnectedGradientOp<float, CUDAContext>);
+    
+      kFullType = 1,
+    
+    #endif  // STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
+
+    
+      // Create a reader that will return log records from '*file'.
+  // '*file' must remain live while this Reader is in use.
+  //
+  // If 'reporter' is non-null, it is notified whenever some data is
+  // dropped due to a detected corruption.  '*reporter' must remain
+  // live while this Reader is in use.
+  //
+  // If 'checksum' is true, verify checksums if available.
+  //
+  // The Reader will start reading at the first record located at physical
+  // position >= initial_offset within the file.
+  Reader(SequentialFile* file, Reporter* reporter, bool checksum,
+         uint64_t initial_offset);
+    
+    template <typename Key, class Comparator>
+inline void SkipList<Key, Comparator>::Iterator::Prev() {
+  // Instead of using explicit 'prev' links, we just search for the
+  // last node that falls before key.
+  assert(Valid());
+  node_ = list_->FindLessThan(node_->key);
+  if (node_ == list_->head_) {
+    node_ = nullptr;
   }
 }
     
+    #include 'db/dbformat.h'
+#include 'leveldb/cache.h'
+#include 'leveldb/table.h'
+#include 'port/port.h'
     
-    
-    namespace rocksdb {
+    namespace leveldb {
     }
+    
+    inline Status::Status(const Status& rhs) {
+  state_ = (rhs.state_ == nullptr) ? nullptr : CopyState(rhs.state_);
+}
+inline Status& Status::operator=(const Status& rhs) {
+  // The following condition catches both aliasing (when this == &rhs),
+  // and the common case where both rhs and *this are ok.
+  if (state_ != rhs.state_) {
+    delete[] state_;
+    state_ = (rhs.state_ == nullptr) ? nullptr : CopyState(rhs.state_);
+  }
+  return *this;
+}
+inline Status& Status::operator=(Status&& rhs) noexcept {
+  std::swap(state_, rhs.state_);
+  return *this;
+}
+    
+      // Calls (*handle_result)(arg, ...) with the entry found after a call
+  // to Seek(key).  May not make such a call if filter policy says
+  // that key is not present.
+  Status InternalGet(const ReadOptions&, const Slice& key, void* arg,
+                     void (*handle_result)(void* arg, const Slice& k,
+                                           const Slice& v));
+    
+      // Advanced operation: flush any buffered key/value pairs to file.
+  // Can be used to ensure that two adjacent entries never live in
+  // the same data block.  Most clients should not need to use this method.
+  // REQUIRES: Finish(), Abandon() have not been called
+  void Flush();
+    
+    namespace leveldb {
+    }
+    
+    static const int kBlockSize = 4096;
+    
+    class Arena {
+ public:
+  Arena();
+    }
+    
+    namespace leveldb {
+    }
+    
+    /* QuadricNormal */
+#define GLU_SMOOTH                         100000
+#define GLU_FLAT                           100001
+#define GLU_NONE                           100002
+    
+    void PSOutputDev::updateCharSpace(GfxState *state) {
+  writePSFmt('{0:.6g} Tc\n', state->getCharSpace());
+}
+    
+      GBool inType3Char;		// inside a Type 3 CharProc
+  GooString *t3String;		// Type 3 content string
+  double t3WX, t3WY,		// Type 3 character parameters
+         t3LLX, t3LLY, t3URX, t3URY;
+  GBool t3Cacheable;		// cleared if char is not cacheable
+  GBool t3NeedsRestore;		// set if a 'q' operator was issued
+  GBool forceRasterize;		// forces the page to be rasterized into a image before printing
+  GBool displayText;		// displayText
+    
+      number = index - base + interval->first;
+  switch (interval->style) {
+  case Interval::Arabic:
+    snprintf (buffer, sizeof(buffer), '%d', number);
+    number_string.append(buffer);
+    break;
+  case Interval::LowercaseRoman:
+    toRoman(number, &number_string, gFalse);
+    break;
+  case Interval::UppercaseRoman:
+    toRoman(number, &number_string, gTrue);
+    break;
+  case Interval::UppercaseLatin:
+  case Interval::LowercaseLatin:
+    number = 0;
+    break;
+  case Interval::None:
+    break;
+  }
+    
+    class Parser {
+public:
+    }
+    
+      //----- special access
+    
+      fileName = NULL;
+  samplingRate = 0.0;
+  channels = 1;
+  bitsPerSample = 8;
+  encoding = soundRaw;
+    
+        fontsrc = new SplashFontSrc;
+    if (fileName)
+      fontsrc->setFile(fileName, gFalse);
+    else
+      fontsrc->setBuf(tmpBuf, tmpBufLen, gTrue);
+    
+    #endif
+    
+      // put and get from non-default column family
+  s = db->Put(WriteOptions(), handles[1], Slice('key'), Slice('value'));
+  assert(s.ok());
+  std::string value;
+  s = db->Get(ReadOptions(), handles[1], Slice('key'), &value);
+  assert(s.ok());
+    
+      MyFilter filter;
+    
+      delete txn;
+  // Clear snapshot from read options since it is no longer valid
+  read_options.snapshot = nullptr;
+  snapshot = nullptr;
+    
+    #include <cstdio>
+#include <string>
+    
+      // Lookup page cache by page identifier
+  //
+  // page_key   Page identifier
+  // buf        Buffer where the data should be copied
+  // size       Size of the page
+  virtual Status Lookup(const Slice& key, std::unique_ptr<char[]>* data,
+                        size_t* size) = 0;
     
      private:
   void Init(
@@ -212,14 +253,94 @@ struct Templates15 {
     }
   }
     
-    #ifndef ROCKSDB_LITE
     
-      virtual ~OptimisticTransactionDB() {}
+    {}  // namespace rocksdb
     
-      // Attempt to acquire lock.  If timeout is non-negative, operation may be
-  // failed after this many microseconds.
-  // Returns OK on success,
-  //         TimedOut if timed out,
-  //         or other Status on failure.
-  // If returned status is OK, TransactionDB will eventually call UnLock().
-  virtual Status TryLockFor(int64_t timeout_time) = 0;
+    #endif  // ROCKSDB_LITE
+
+    
+    /*!
+    Tests model's implementation of QAbstractItemModel::parent()
+ */
+void ModelTest::parent()
+{
+//     qDebug() << 'p';
+    // Make sure the model won't crash and will return an invalid QModelIndex
+    // when asked for the parent of an invalid index.
+    QVERIFY( model->parent ( QModelIndex() ) == QModelIndex() );
+    }
+    
+    QString ServerConfig::keysPattern() const
+{
+    return param<QString>('keys_pattern', QString(DEFAULT_KEYS_GLOB_PATTERN));
+}
+    
+        /* Advanced settings */
+    Q_PROPERTY(QString keysPattern READ keysPattern WRITE setKeysPattern)
+    Q_PROPERTY(QString namespaceSeparator READ namespaceSeparator WRITE setNamespaceSeparator)
+    Q_PROPERTY(uint executeTimeout READ executeTimeout WRITE setExecutionTimeout)
+    Q_PROPERTY(uint connectionTimeout READ connectionTimeout WRITE setConnectionTimeout)
+    Q_PROPERTY(bool luaKeysLoading READ luaKeysLoading WRITE setLuaKeysLoading)
+    Q_PROPERTY(bool overrideClusterHost READ overrideClusterHost WRITE setClusterHostOverride)
+    Q_PROPERTY(uint databaseScanLimit READ databaseScanLimit WRITE setDatabaseScanLimit)
+    
+    void SortFilterProxyModel::setSortRole(const QByteArray &role)
+{
+    if (m_sortRole != role) {
+        m_sortRole = role;
+        if (m_complete)
+            QSortFilterProxyModel::setSortRole(roleKey(role));
+    }
+}
+    
+      QObject::connect(m_connection.data(), &RedisClient::Connection::connected,
+                   [this, weekPointer]() {
+                     if (!weekPointer) {
+                       return;
+                     }
+    }
+    
+    
+    {  foo_.Bar2(1, 3);  // Mismatch arguments and mismatch With()
+  foo_.Bar2(2, 1);
+}
+    
+    // Step 3. Call RUN_ALL_TESTS() in main().
+//
+// We do this by linking in src/gtest_main.cc file, which consists of
+// a main() function which calls RUN_ALL_TESTS() for us.
+//
+// This runs all the tests you've defined, prints the result, and
+// returns 0 if successful, or 1 otherwise.
+//
+// Did you notice that we didn't register the tests?  The
+// RUN_ALL_TESTS() macro magically knows about all the tests we
+// defined.  Isn't this convenient?
+
+    
+    TEST(GTestMainTest, ShouldSucceed) {
+}
+    
+    TEST(NoFatalFailureTest, AssertNoFatalFailure) {
+  ASSERT_NO_FATAL_FAILURE(;);
+  ASSERT_NO_FATAL_FAILURE(SUCCEED());
+  ASSERT_NO_FATAL_FAILURE(Subroutine());
+  ASSERT_NO_FATAL_FAILURE({ SUCCEED(); });
+}
+    
+    // Define a matcher that matches a value that evaluates in boolean
+// context to true.  Useful for types that define 'explicit operator
+// bool' operators and so can't be compared for equality with true
+// and false.
+MATCHER(IsTrue, negation ? 'is false' : 'is true') {
+  return static_cast<bool>(arg);
+}
+    
+    
+    {  EXPECT_EQ(3, c.Decrement());
+}
+    
+    TEST(LosslessArithmeticConvertibleTest, FloatingPointToBool) {
+  EXPECT_FALSE((LosslessArithmeticConvertible<float, bool>::value));
+  EXPECT_FALSE((LosslessArithmeticConvertible<double, bool>::value));
+}
