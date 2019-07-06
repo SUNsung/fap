4f8d2f59132f83bf90cf782a1b528eed44df5b2d
SUNsung <weatherius@gmail.com>
2019-07-06 03:40:31 +0300

    Rewrite [54SsP1hfpVhWQ54rPdwKeNbV3o54NuxtpaYbCo54WmJ.py] File in: 2019-07-06 03:40:31

--- pyton/54SsP1hfpVhWQ54rPdwKeNbV3o54NuxtpaYbCo54WmJ.py
+++ pyton/54SsP1hfpVhWQ54rPdwKeNbV3o54NuxtpaYbCo54WmJ.py
@@ -1,114 +1,134 @@
 
         
-            return rabinMiller(num)
+        
+def main():
+    argument_spec = HerokuHelper.heroku_argument_spec()
+    argument_spec.update(
+        user=dict(required=True, type='str'),
+        apps=dict(required=True, type='list'),
+        suppress_invitation=dict(default=False, type='bool'),
+        state=dict(default='present', type='str', choices=['present', 'absent']),
+    )
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True
+    )
     
+            if module.check_mode:
+            mp_id = get_monitoring_policy(oneandone_conn, monitoring_policy_id)
+            if (monitoring_policy_processes and mp_id):
+                return True
+            return False
     
-class QuadraticProbing(HashTable):
-    '''
-        Basic Hash Table example with open addressing using Quadratic Probing 
-    '''
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        # Max concurrency is limited by global CONCURRENT_REQUESTS setting
+    max_concurrent_requests = 8
+    # Requests per second goal
+    qps = None # same as: 1 / download_delay
+    download_delay = None
+    # time in seconds to delay server responses
+    latency = None
+    # number of slots to create
+    slots = 1
     
-                self.decision_boundary = X[best_split]
-            self.left = Decision_Tree(depth = self.depth - 1, min_leaf_size = self.min_leaf_size)
-            self.right = Decision_Tree(depth = self.depth - 1, min_leaf_size = self.min_leaf_size)
-            self.left.train(left_X, left_y)
-            self.right.train(right_X, right_y)
-        else:
-            self.prediction = np.mean(y)
+        def _list_templates(self):
+        print('Available templates:')
+        for filename in sorted(os.listdir(self.templates_dir)):
+            if filename.endswith('.tmpl'):
+                print('  %s' % splitext(filename)[0])
     
-    * @author chinmoy159
-* @version 1.0 dated 10/08/2017
-'''
+    if twisted_version >= (14, 0, 0):
+    # ClientTLSOptions requires a recent-enough version of Twisted.
+    # Not having ScrapyClientTLSOptions should not matter for older
+    # Twisted versions because it is not used in the fallback
+    # ScrapyClientContextFactory.
     
-        return wrapped_view
+        def process(self, new_face):
+        ''' Sharpen using the requested technique '''
+        amount = self.config['amount'] / 100.0
+        kernel_center = self.get_kernel_size(new_face, self.config['radius'])
+        new_face = getattr(self, self.config['method'])(new_face, kernel_center, amount)
+        return new_face
     
+            self.status()
+        self.pbar = self.progress_bar()
     
-def attach_enctype_error_multidict(request):
-    '''Since Flask 0.8 we're monkeypatching the files object in case a
-    request is detected that does not use multipart form data but the files
-    object is accessed.
-    '''
-    oldcls = request.files.__class__
+        def draw_landmarks(self, color_id=3, radius=1):
+        ''' Draw the facial landmarks '''
+        color = self.colors[color_id]
+        for alignment in self.alignments:
+            landmarks = alignment['landmarksXY']
+            logger.trace('Drawing Landmarks: (landmarks: %s, color: %s, radius: %s)',
+                         landmarks, color, radius)
+            for (pos_x, pos_y) in landmarks:
+                cv2.circle(self.image,  # pylint: disable=no-member
+                           (pos_x, pos_y),
+                           radius,
+                           color,
+                           -1)
     
+        def decoder(self):
+        ''' Decoder Network '''
+        input_ = Input(shape=(4, 4, self.encoder_dim))
+        var_x = input_
+        var_x = self.blocks.upscale(var_x, 512)
+        var_x = self.blocks.upscale(var_x, 256)
+        var_x = self.blocks.upscale(var_x, 128)
+        var_x = self.blocks.upscale(var_x, 64)
+        var_x = Conv2D(3, kernel_size=5, padding='same', activation='sigmoid')(var_x)
+        outputs = [var_x]
     
-def test_preprocess_input():
-    # Test image batch with float and int image input
-    x = np.random.uniform(0, 255, (2, 10, 10, 3))
-    xint = x.astype('int32')
-    assert utils.preprocess_input(x).shape == x.shape
-    assert utils.preprocess_input(xint).shape == xint.shape
+        def gen_cli_arguments(self, command):
+        ''' Return the generated cli arguments for
+            the selected command '''
+        for option in self.gen_command_options(command):
+            optval = str(option.get('value', '').get())
+            opt = option['opts'][0]
+            if command in ('extract', 'convert') and opt == '-o':
+                get_images().pathoutput = optval
+            if optval in ('False', ''):
+                continue
+            elif optval == 'True':
+                yield (opt, )
+            else:
+                if option.get('nargs', None):
+                    if '\'' in optval:
+                        optval = [arg[1:-1] for arg in re.findall(r'\'.+?\'', optval)]
+                    else:
+                        optval = optval.split(' ')
+                    opt = [opt] + optval
+                else:
+                    opt = (opt, optval)
+                yield opt
+
     
-    network2 = {
-    'n_dense': 6,
-    'dense_units': 16,
-    'activation': 'selu',
-    'dropout': AlphaDropout,
-    'dropout_rate': 0.1,
-    'kernel_initializer': 'lecun_normal',
-    'optimizer': 'sgd'
-}
+        def compute_output_shape(self, input_shape):
+        ''' If you are using 'channels_last' configuration'''
+        input_shape = self.input_spec[0].shape
+        in_width, in_height = input_shape[2], input_shape[1]
+        kernel_width, kernel_height  = self.kernel_size, self.kernel_size
     
-            return table.draw()
+        def __call__(self, text, **kargs):
+        words = jieba.tokenize(text, mode='search')
+        token = Token()
+        for (w, start_pos, stop_pos) in words:
+            if not accepted_chars.match(w) and len(w) <= 1:
+                continue
+            token.original = token.text = w
+            token.pos = start_pos
+            token.startchar = start_pos
+            token.endchar = stop_pos
+            yield token
     
+    PROB_START_P = 'prob_start.p'
+PROB_TRANS_P = 'prob_trans.p'
+PROB_EMIT_P = 'prob_emit.p'
+CHAR_STATE_TAB_P = 'char_state_tab.p'
     
-def set_signal_handler_to_shutdown():
-    set_signal_handler(shutdown)
+            prev_states_expect_next = set(
+            (y for x in prev_states for y in trans_p[x].keys()))
+        obs_states = set(
+            states.get(obs[t], all_states)) & prev_states_expect_next
     
+    print('speed %s bytes/second' % (len(content)/tm_cost))
     
-def timeparse(sval):
-    '''Parse a time expression, returning it as a number of seconds.  If
-    possible, the return value will be an `int`; if this is not
-    possible, the return will be a `float`.  Returns `None` if a time
-    expression cannot be parsed from the given string.
-    
-        def download_all(self, version):
-        files = {
-            'docker-compose-Darwin-x86_64': None,
-            'docker-compose-Linux-x86_64': None,
-            'docker-compose-Windows-x86_64.exe': None,
-        }
-    
-        def test_format_return(self):
-        expected = repr({'Id': 'ok'})
-        actual = verbose_proxy.format_return({'Id': 'ok'}, 2)
-        assert expected == actual
-    
-    
-class TestSortService(object):
-    def test_sort_service_dicts_1(self):
-        services = [
-            {
-                'links': ['redis'],
-                'name': 'web'
-            },
-            {
-                'name': 'grunt'
-            },
-            {
-                'name': 'redis'
-            }
-        ]
-    
-    
-_exc = {}
-    
-        def predict(self, doc1, doc2):
-        x1 = self.get_features([doc1], max_length=self.max_length)
-        x2 = self.get_features([doc2], max_length=self.max_length)
-        scores = self.model.predict([x1, x2])
-    
-        if mode == 'train':
-        if train_loc == None or dev_loc == None:
-            print('Train mode requires paths to training and development data sets.')
-            sys.exit(1)
-        train(train_loc, dev_loc, shape, settings)
-    elif mode == 'evaluate':
-        if dev_loc == None:
-            print('Evaluate mode requires paths to test data set.')
-            sys.exit(1)
-        correct, total = evaluate(dev_loc, shape)
-        print(correct, '/', total, correct / total)
-    else:
-        demo(shape)
+    print('speed' , len(content)/tm_cost, ' bytes/second')
