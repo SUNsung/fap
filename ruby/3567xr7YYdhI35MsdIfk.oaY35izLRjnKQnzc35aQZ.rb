568b8a6efbc3656d9f3b96c765fbdc512ea328a3
SUNsung <weatherius@gmail.com>
2019-07-06 08:13:45 +0300

    Rewrite [3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb] File in: 2019-07-06 08:13:45

--- ruby/3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb
+++ ruby/3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb
@@ -1,98 +1,123 @@
 
         
-          # This method is called when weak warning is produced by the parser.
-  # +fmt+ and +args+ is printf style.
-  def warn(fmt, *args)
+              ',
+                  period_type: period_types[period_type],
+                  since: since,
+                  like_type: UserAction::LIKE,
+                  was_liked_type: UserAction::WAS_LIKED,
+                  new_topic_type: UserAction::NEW_TOPIC,
+                  reply_type: UserAction::REPLY,
+                  regular_post_type: Post.types[:regular]
+    
+        SCRIPT_ASSET_DIRECTORIES = [
+      # [dir, can_use_s3_cdn, can_use_cdn]
+      ['/assets/',             true, true],
+      ['/brotli_asset/',       true, true],
+      ['/extra-locales/',      false, false],
+      ['/highlight-js/',       false, true],
+      ['/javascripts/',        false, true],
+      ['/plugins/',            false, true],
+      ['/theme-javascripts/',  false, true],
+      ['/svg-sprite/',         false, true],
+    ]
+    
+        cookies[TOKEN_COOKIE] = cookie_hash(@user_token.unhashed_auth_token)
+    unstage_user(user)
+    make_developer_admin(user)
+    enable_bootstrap_mode(user)
+    @env[CURRENT_USER_KEY] = user
   end
     
-          # Only uses first 29 characters of salt
-      'mypassword'.crypt('$2a$04$0WVaz0pV3jzfZ5G5tpmHWuB').should == '$2a$04$0WVaz0pV3jzfZ5G5tpmHWuBQGbkjzgtSc3gJbmdy0GAGMa45MFM2.'
-    end
+    require 'method_profiler'
     
-          Thread.pass until port
+    describe ScenarioImportsController do
+  let(:user) { users(:bob) }
     
-      it 'initializes a new UDPSocket using an Integer' do
-    @socket = UDPSocket.new(Socket::AF_INET)
-    @socket.should be_an_instance_of(UDPSocket)
-  end
-    
-        def on_comma(tok, data)
-      @lex_state = EXPR_BEG | EXPR_LABEL if (EXPR_ARG_ANY & @lex_state) != 0
-      data << Token.new(lineno, column, __method__, tok, @lex_state)
-    end
-    
-          open('iso-2022-jp.txt', 'wb:iso-2022-jp', xml: :attr) {|f| f.print '&<>'\''; f.puts '\u4E02\u3042' }
-      content = File.read('iso-2022-jp.txt', :mode=>'rb:ascii-8bit')
-      assert_equal('\'&amp;&lt;&gt;&quot;'&#x4E02;\e$B$\'\e(B\n\''.force_encoding('ascii-8bit'), content)
-    
-      def test_new_method_on_subclass
-    sub = FooSub.new
-    assert_raise(NoMethodError) { sub.z }
-    assert_equal('FooExt#z', FooExtClient.invoke_z_on(sub))
-    assert_raise(NoMethodError) { sub.z }
-  end
-    
-      def test_ungetc2
-    f = false
-    pipe(proc do |w|
-      Thread.pass until f
-      w.write('1' * 10000)
-      w.close
-    end, proc do |r|
-      r.ungetc('0' * 10000)
-      f = true
-      assert_equal('0' * 10000 + '1' * 10000, r.read)
-    end)
-  end
-    
-            def self.options
-          [[
-            '--short', 'Only print the path relative to the cache root'
-          ]].concat(super)
-        end
-    
-        # regression tests for https://github.com/spree/spree/issues/6888
-    context 'per page dropdown', js: true do
-      before do
-        select '45', from: 'per_page'
-        wait_for_ajax
-        expect(page).to have_select('per_page', selected: '45')
-        expect(page).to have_selector(:css, 'select.per-page-selected-45')
-      end
-    
-          @@stock_location_attributes = [
-        :id, :name, :address1, :address2, :city, :state_id, :state_name,
-        :country_id, :zipcode, :phone, :active
-      ]
-    
-        # Returns the scaling and cropping geometries (in string-based ImageMagick format)
-    # neccessary to transform this Geometry into the Geometry given. If crop is true,
-    # then it is assumed the destination Geometry will be the exact final resolution.
-    # In this case, the source Geometry is scaled so that an image containing the
-    # destination Geometry would be completely filled by the source image, and any
-    # overhanging image would be cropped. Useful for square thumbnail images. The cropping
-    # is weighted at the center of the Geometry.
-    def transformation_to dst, crop = false
-      if crop
-        ratio = Geometry.new( dst.width / self.width, dst.height / self.height )
-        scale_geometry, scale = scaling(dst, ratio)
-        crop_geometry         = cropping(dst, ratio, scale)
-      else
-        scale_geometry        = dst.to_s
-      end
-    
-        def raise_because_imagemagick_missing
-      raise Errors::CommandNotFoundError.new('Could not run the `identify` command. Please install ImageMagick.')
+        it 'is OK with no hl' do
+      stub(params).[](:hl) { nil }
+      expect((1..10).select { |i| highlighted?(i) }).to be_empty
     end
   end
 end
 
     
-        # Returns the timestamp as defined by the <attachment>_updated_at field
-    # in the server default time zone unless :use_global_time_zone is set
-    # to false.  Note that a Rails.config.time_zone change will still
-    # invalidate any path or URL that uses :timestamp.  For a
-    # time_zone-agnostic timestamp, use #updated_at.
-    def timestamp attachment, style_name
-      attachment.instance_read(:updated_at).in_time_zone(attachment.time_zone).to_s
+        it 'defauls foreground and background colors' do
+      scenario.tag_fg_color = nil
+      scenario.tag_bg_color = nil
+      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
+    end
+  end
+    
+      describe 'up' do
+    it 'should update extract and template options for an existing WebsiteAgent' do
+      expect(agent.options).to include('extract' => old_extract,
+                                       'template' => old_template)
+      ConvertWebsiteAgentTemplateForMerge.new.up
+      agent.reload
+      expect(agent.options).to include('extract' => new_extract,
+                                       'template' => new_template)
+    end
+  end
+    
+      end
+    
+      # Check if there is no signed in user before doing the sign out.
+  #
+  # If there is no signed in user, it will set the flash message and redirect
+  # to the after_sign_out path.
+  def verify_signed_out_user
+    if all_signed_out?
+      set_flash_message! :notice, :already_signed_out
+    
+    module Devise
+  module Controllers
+    # Provide sign in and sign out functionality.
+    # Included by default in all controllers.
+    module SignInOut
+      # Return true if the given scope is signed in session. If no scope given, return
+      # true if any scope is signed in. This will run authentication hooks, which may
+      # cause exceptions to be thrown from this method; if you simply want to check
+      # if a scope has already previously been authenticated without running
+      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
+      def signed_in?(scope=nil)
+        [scope || Devise.mappings.keys].flatten.any? do |_scope|
+          warden.authenticate?(scope: _scope)
+        end
+      end
+    
+          def devise_mapping
+        @devise_mapping ||= Devise.mappings[scope_name]
+      end
+    
+    module Devise
+  module Models
+    class MissingAttribute < StandardError
+      def initialize(attributes)
+        @attributes = attributes
+      end
+    
+          def time_from_json(value)
+        if value =~ /\A\d+\.\d+\Z/
+          Time.at(value.to_f)
+        else
+          Time.parse(value) rescue nil
+        end
+      end
+    
+        def log_status(status)
+      puts bold status
+    end
+    
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+    
+        private
+    
+        def define_query
+      name = @name
+      @klass.send :define_method, '#{@name}?' do
+        send(name).file?
+      end
     end
