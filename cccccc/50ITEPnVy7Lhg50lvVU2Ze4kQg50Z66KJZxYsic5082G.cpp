6400dec4f5bfc054aa0452144b4094cf6dd2033d
SUNsung <weatherius@gmail.com>
2019-07-06 16:49:48 +0300

    Rewrite [50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp] File in: 2019-07-06 16:49:48

--- cccccc/50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp
+++ cccccc/50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp
@@ -1,176 +1,214 @@
 
         
-        #define REGISTER_LAYER_CLASS(type)                                             \
-  template <typename Dtype>                                                    \
-  shared_ptr<Layer<Dtype> > Creator_##type##Layer(const LayerParameter& param) \
-  {                                                                            \
-    return shared_ptr<Layer<Dtype> >(new type##Layer<Dtype>(param));           \
-  }                                                                            \
-  REGISTER_LAYER_CREATOR(type, Creator_##type##Layer)
+        PersistentDictionary::PersistentDictionary(const PersistentDictionary& other) =
+    default;
     
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
+    ObjectTemplateBuilder& ObjectTemplateBuilder::SetPropertyImpl(
+    const base::StringPiece& name,
+    v8::Local<v8::FunctionTemplate> getter,
+    v8::Local<v8::FunctionTemplate> setter) {
+  template_->SetAccessorProperty(StringToSymbol(isolate_, name), getter,
+                                 setter);
+  return *this;
+}
     
+    bool Converter<v8::Local<v8::Value>>::FromV8(v8::Isolate* isolate,
+                                             v8::Local<v8::Value> val,
+                                             v8::Local<v8::Value>* out) {
+  *out = val;
+  return true;
+}
     
-    { protected:
-  TransformationParameter transform_param_;
-  shared_ptr<DataTransformer<Dtype> > data_transformer_;
-  bool output_labels_;
-};
+    #ifndef NATIVE_MATE_NATIVE_MATE_HANDLE_H_
+#define NATIVE_MATE_NATIVE_MATE_HANDLE_H_
     
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    bool DownloadItem::CanResume() const {
+  return download_item_->CanResume();
+}
     
-      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    namespace crash_reporter {
+extern const char kCrashpadProcess[];
+}
     
-      bool handles_setup_;
-  cudnnHandle_t* handle_;
-  cudaStream_t*  stream_;
-    
-      bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnLRNDescriptor_t norm_desc_;
-  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
-    
-    #include 'caffe/layers/neuron_layer.hpp'
-#include 'caffe/layers/relu_layer.hpp'
-    
-    #ifdef USE_CUDNN
-/**
- * @brief cuDNN implementation of SoftmaxLayer.
- *        Fallback to SoftmaxLayer for CPU mode.
- */
-template <typename Dtype>
-class CuDNNSoftmaxLayer : public SoftmaxLayer<Dtype> {
- public:
-  explicit CuDNNSoftmaxLayer(const LayerParameter& param)
-      : SoftmaxLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNSoftmaxLayer();
+            for ( ; x < width; ++x)
+        {
+            // make extrapolation for the last elements
+            if (x + 1 >= width)
+            {
+                if (border == BORDER_MODE_CONSTANT)
+                {
+                    nextx[0] = borderValue;
+                    nextx[1] = borderValue;
+                    nextx[2] = borderValue;
+                }
+                else if (border == BORDER_MODE_REPLICATE)
+                {
+                    nextx[0] = srow0[x];
+                    nextx[1] = srow1[x];
+                    nextx[2] = srow2[x];
+                }
+            }
+            else
+            {
+                nextx[0] = srow0 ? srow0[x + 1] : borderValue;
+                nextx[1] =         srow1[x + 1]              ;
+                nextx[2] = srow2 ? srow2[x + 1] : borderValue;
+            }
     }
     
-    	wchar_t commandStr[MAX_PATH + 20] = { 0 };
-	wchar_t baseCommandStr[MAX_PATH + 20] = { 0 };
-	if (!single) {
-		swprintf_s(baseCommandStr, L'\'%s\'', exePath);
-	}
-	else {
-		swprintf_s(baseCommandStr, L'\'%s\' /single', exePath);
-	}
-    
-    namespace leveldb {
+                    if( d & 1 )
+                {
+                    s32 vt = v - threshold, count = 0;
     }
     
-      // Get initial measurement of the space we will be reading.
-  const int64_t initial_size = Size(Key(0), Key(n));
-  const int64_t initial_other_size = Size(Key(n), Key(kCount));
-    
-      Writer(const Writer&) = delete;
-  Writer& operator=(const Writer&) = delete;
-    
-      // Pick level and inputs for a new compaction.
-  // Returns nullptr if there is no compaction to be done.
-  // Otherwise returns a pointer to a heap-allocated object that
-  // describes the compaction.  Caller should delete the result.
-  Compaction* PickCompaction();
-    
-      // Change the options used by this builder.  Note: only some of the
-  // option fields can be changed after construction.  If a field is
-  // not allowed to change dynamically and its value in the structure
-  // passed to the constructor is different from its value in the
-  // structure passed to this method, this method will return an error
-  // without changing any fields.
-  Status ChangeOptions(const Options& options);
+        for( ptrdiff_t y = 0; y < rows; y++ )
+    {
+        const u8* v0 = 0;
+        const u8* v1 = internal::getRowPtr(srcBase, srcStride, y);
+        const u8* v2 = 0;
+        // make border
+        if (border == BORDER_MODE_REFLECT101) {
+            v0 = internal::getRowPtr(srcBase, srcStride, y > 0 ? y-1 : y+1);
+            v2 = internal::getRowPtr(srcBase, srcStride, y < rows-1 ? y+1 : rows > 1 ? rows-2 : 0);
+        } else  if (border == BORDER_MODE_CONSTANT) {
+            v0 = y > 0 ? internal::getRowPtr(srcBase, srcStride, y-1) : tmp;
+            v2 = y < rows-1 ? internal::getRowPtr(srcBase, srcStride, y+1) : tmp;
+        } else {
+            v0 = internal::getRowPtr(srcBase, srcStride, y > 0 ? y-1 : 0);
+            v2 = internal::getRowPtr(srcBase, srcStride, y < rows-1 ? y+1 : rows > 0 ? rows-1 : 0);
+        }
+        s16* drow = internal::getRowPtr(dstBase, dstStride, y);
+    }
     
     
-    {    // TODO(postrelease): Add stats and other meta blocks
-    WriteBlock(&meta_index_block, &metaindex_block_handle);
-  }
-    
-      // Returns an estimate of the total memory usage of data allocated
-  // by the arena.
-  size_t MemoryUsage() const {
-    return memory_usage_.load(std::memory_order_relaxed);
-  }
-    
-      Insert(100, 101);
-  ASSERT_EQ(101, Lookup(100));
-  ASSERT_EQ(-1, Lookup(200));
-  ASSERT_EQ(-1, Lookup(300));
-    
-        // change an array element
-    j.at('/array/1'_json_pointer) = 21;
-    // output the changed array
-    std::cout << j['array'] << '\n';
-    
-        // output element with JSON pointer '/number'
-    std::cout << j.at('/number'_json_pointer) << '\n';
-    // output element with JSON pointer '/string'
-    std::cout << j.at('/string'_json_pointer) << '\n';
-    // output element with JSON pointer '/array'
-    std::cout << j.at('/array'_json_pointer) << '\n';
-    // output element with JSON pointer '/array/1'
-    std::cout << j.at('/array/1'_json_pointer) << '\n';
-    
-    
-    // ============
-    // string types
-    // ============
-    
-    #include 'rocksdb/write_batch_base.h'
-    
-      // close db
-  for (auto handle : handles) {
-    delete handle;
-  }
-  delete db;
-    
-    #include 'rocksdb/db.h'
-#include 'rocksdb/slice.h'
-#include 'rocksdb/options.h'
-    
-    using namespace rocksdb;
-    
-    
-    {// Factor method to create a new persistent cache
-Status NewPersistentCache(Env* const env, const std::string& path,
-                          const uint64_t size,
-                          const std::shared_ptr<Logger>& log,
-                          const bool optimized_for_nvm,
-                          std::shared_ptr<PersistentCache>* cache);
-}  // namespace rocksdb
+    {b2PolyNode* b2PolyNode::GetRightestConnection(b2Vec2& incomingDir){
+	b2Vec2 diff = position-incomingDir;
+	b2PolyNode temp(diff);
+	b2PolyNode* res = GetRightestConnection(&temp);
+	b2Assert(res);
+	return res;
+}
+}
 
     
-    // Returns the current memory usage of the specified DB instances.
-class MemoryUtil {
+    	void EncodeMipmaps(float *a_pafSourceRGBA,
+		unsigned int a_uiSourceWidth,
+		unsigned int a_uiSourceHeight,
+		Image::Format a_format,
+		ErrorMetric a_eErrMetric,
+		float a_fEffort,
+		unsigned int a_uiJobs,
+		unsigned int a_uiMaxJobs,
+		unsigned int a_uiMaxMipmaps,
+		unsigned int a_uiMipFilterFlags,
+		RawImage* a_pMipmaps,
+		int *a_piEncodingTime_ms, bool a_bVerboseOutput = false);
+    
+      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.*/
+    
+      const dmlc::RowBlock<IndexType>& Value() const override {
+    return out_;
+  }
+    
+    
+    {}  // namespace xgboost
+#endif  // XGBOOST_TREE_UPDATER_H_
+
+    
+    #ifndef XGBOOST_COMMON_HOST_DEVICE_VECTOR_H_
+#define XGBOOST_COMMON_HOST_DEVICE_VECTOR_H_
+    
+    #include <dmlc/parameter.h>
+#include '../common/common.h'
+    
+    TreeUpdater* TreeUpdater::Create(const std::string& name, LearnerTrainParam const* tparam) {
+  auto *e = ::dmlc::Registry< ::xgboost::TreeUpdaterReg>::Get()->Find(name);
+  if (e == nullptr) {
+    LOG(FATAL) << 'Unknown tree updater ' << name;
+  }
+  auto p_updater = (e->body)();
+  p_updater->tparam_ = tparam;
+  return p_updater;
+}
+    
+    /*! \brief pruner that prunes a tree after growing finishes */
+class TreePruner: public TreeUpdater {
  public:
-  enum UsageType : int {
-    // Memory usage of all the mem-tables.
-    kMemTableTotal = 0,
-    // Memory usage of those un-flushed mem-tables.
-    kMemTableUnFlushed = 1,
-    // Memory usage of all the table readers.
-    kTableReadersTotal = 2,
-    // Memory usage by Cache.
-    kCacheTotal = 3,
-    kNumUsageTypes = 4
-  };
+  TreePruner() {
+    syncher_.reset(TreeUpdater::Create('sync', tparam_));
+  }
+  // set training parameter
+  void Init(const std::vector<std::pair<std::string, std::string> >& args) override {
+    param_.InitAllowUnknown(args);
+    syncher_->Init(args);
+  }
+  // update the tree, do pruning
+  void Update(HostDeviceVector<GradientPair> *gpair,
+              DMatrix *p_fmat,
+              const std::vector<RegTree*> &trees) override {
+    // rescale learning rate according to size of trees
+    float lr = param_.learning_rate;
+    param_.learning_rate = lr / trees.size();
+    for (auto tree : trees) {
+      this->DoPrune(*tree);
     }
+    param_.learning_rate = lr;
+    syncher_->Update(gpair, p_fmat, trees);
+  }
+    }
+    
+    using apollo::drivers::canbus::Byte;
+    
+    double ObjectExtendedInfo60D::object_length(const std::uint8_t* bytes,
+                                            int32_t length) const {
+  Byte t0(bytes + 6);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    int ObjectQualityInfo60C::oritation_angle_rms(const std::uint8_t* bytes,
+                                              int32_t length) const {
+  Byte t0(bytes + 4);
+  int32_t x = t0.get_byte(0, 2);
+    }
+    
+    
+    {  switch (x) {
+    case 0x0:
+      return OUTPUT_TYPE_NONE;
+    case 0x1:
+      return OUTPUT_TYPE_OBJECTS;
+    case 0x2:
+      return OUTPUT_TYPE_CLUSTERS;
+    default:
+      return OUTPUT_TYPE_ERROR;
+  }
+}
+    
+      kernel.AddSecondOrderDerivativeMatrix(init_derivative, 1.0);
+    
+    // config detail: {'name': 'output_value', 'enum': {0:
+// 'OUTPUT_VALUE_HEADLIGHTS_OFF', 1: 'OUTPUT_VALUE_LOW_BEAMS', 2:
+// 'OUTPUT_VALUE_HIGH_BEAMS'}, 'precision': 1.0, 'len': 8, 'is_signed_var':
+// False, 'offset': 0.0, 'physical_range': '[0|2]', 'bit': 23, 'type': 'enum',
+// 'order': 'motorola', 'physical_unit': ''}
+Headlight_rpt_77::Output_valueType Headlightrpt77::output_value(
+    const std::uint8_t* bytes, int32_t length) const {
+  Byte t0(bytes + 2);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    
+    {  Horn_rpt_79::Commanded_valueType ret =
+      static_cast<Horn_rpt_79::Commanded_valueType>(x);
+  return ret;
+}
