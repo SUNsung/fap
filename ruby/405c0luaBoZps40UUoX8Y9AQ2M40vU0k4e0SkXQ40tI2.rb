fc23f0a410594c3ab3bbbbec7b9d00eafc27b481
SUNsung <weatherius@gmail.com>
2019-07-07 08:54:45 +0300

    Rewrite [405c0luaBoZps40UUoX8Y9AQ2M40vU0k4e0SkXQ40tI2.rb] File in: 2019-07-07 08:54:45

--- ruby/405c0luaBoZps40UUoX8Y9AQ2M40vU0k4e0SkXQ40tI2.rb
+++ ruby/405c0luaBoZps40UUoX8Y9AQ2M40vU0k4e0SkXQ40tI2.rb
@@ -1,178 +1,37 @@
 
         
-                  # Generate default options for collection helpers, such as :checked and
-          # :disabled.
-          def default_html_options_for_collection(item, value)
-            html_options = @html_options.dup
+        module Docs
+  class Filter < ::HTML::Pipeline::Filter
+    def css(*args)
+      doc.css(*args)
+    end
     
-              content = if block_given?
-            @template_object.capture(builder, &block)
-          elsif @content.present?
-            @content.to_s
-          else
-            render_component(builder)
-          end
+        def path
+      @path ||= url.path
+    end
     
-    require 'action_view/helpers/tags/placeholderable'
+            css('.content', 'article', '.api-header', 'section', '.instance-member').each do |node|
+          node.before(node.children).remove
+        end
     
-    # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
+            # Remove root-level <div>
+        while div = at_css('h1 + div')
+          div.before(div.children)
+          div.remove
+        end
     
-              new_theme_name = args.join('_')
-          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
-    
-          def site
-        @site_drop ||= SiteDrop.new(@obj)
-      end
-    
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            grouping: '#{grouping}',
-            build_number: #{specified_build_number},
-          })
-        end').runner.execute(:test)
-    
-          it 'handles the exclude_dirs parameter with multiple  elements correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: ['.bundle', 'Packages/'])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --exclude-dir .bundle --exclude-dir Packages/')
+            doc
       end
     end
   end
 end
 
     
-          it 'works with all parameters' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-              select_regex: /.*/,
-              exclude_regex: /Test.m/,
-              report_type: 'pmd',
-              report_path: 'report_path.xml',
-              max_priority_1: 10,
-              max_priority_2: 20,
-              max_priority_3: 30,
-              thresholds: ['LONG_LINE=200', 'LONG_METHOD=200'],
-              enable_rules: ['DoubleNegative', 'DeadCode'],
-              disable_rules: ['GotoStatement', 'ShortVariableName'],
-              list_enabled_rules: true,
-              enable_clang_static_analyzer: true,
-              enable_global_analysis: true,
-              allow_duplicated_violations: true
-            )
-          end').runner.execute(:test)
+      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
+  gem.executables   = %w(cap capify)
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ['lib']
     
-          it 'uses the correct command to import it' do
-        # We have to execute *something* using ` since otherwise we set expectations to `nil`, which is not healthy
-        `ls`
-    
-        def run
-      program :name, 'cert'
-      program :version, Fastlane::VERSION
-      program :description, 'CLI for \'cert\' - Create new iOS code signing certificates'
-      program :help, 'Author', 'Felix Krause <cert@krausefx.com>'
-      program :help, 'Website', 'https://fastlane.tools'
-      program :help, 'Documentation', 'https://docs.fastlane.tools/actions/cert/'
-      program :help_formatter, :compact
-    
-        # This returns whether the guest is ready to work. If this returns
-    # `false`, then {#detect!} should be called in order to detect the
-    # guest OS.
-    #
-    # @return [Boolean]
-    def ready?
-      !!capability_host_chain
-    end
-  end
-end
-
-    
-            # Execute a command on the remote machine. The exact semantics
-        # of this method are up to the implementor, but in general the
-        # users of this class will expect this to be a shell.
-        #
-        # This method gives you no way to write data back to the remote
-        # machine, so only execute commands that don't expect input.
-        #
-        # @param [String] command Command to execute.
-        # @yield [type, data] Realtime output of the command being executed.
-        # @yieldparam [String] type Type of the output. This can be
-        #   `:stdout`, `:stderr`, etc. The exact types are up to the
-        #   implementor.
-        # @yieldparam [String] data Data for the given output.
-        # @return [Integer] Exit code of the command.
-        def execute(command, opts=nil)
-        end
-    
-            # This is called as a last-minute hook that allows the configuration
-        # object to finalize itself before it will be put into use. This is
-        # a useful place to do some defaults in the case the user didn't
-        # configure something or so on.
-        #
-        # An example of where this sort of thing is used or has been used:
-        # the 'vm' configuration key uses this to make sure that at least
-        # one sub-VM has been defined: the default VM.
-        #
-        # The configuration object is expected to mutate itself.
-        def finalize!
-          # Default implementation is to do nothing.
-        end
-    
-            # This is the method called to when the system is being destroyed
-        # and allows the provisioners to engage in any cleanup tasks necessary.
-        def cleanup
-        end
-      end
-    end
-  end
-end
-
-    
-            # Parses the options given an OptionParser instance.
-        #
-        # This is a convenience method that properly handles duping the
-        # originally argv array so that it is not destroyed.
-        #
-        # This method will also automatically detect '-h' and '--help'
-        # and print help. And if any invalid options are detected, the help
-        # will be printed, as well.
-        #
-        # If this method returns `nil`, then you should assume that help
-        # was printed and parsing failed.
-        def parse_options(opts=nil)
-          # make sure optparse doesn't use POSIXLY_CORRECT parsing
-          ENV['POSIXLY_CORRECT'] = nil
-    
-            # Defines a capability for the given provider. The block should return
-        # a class/module that has a method with the capability name, ready
-        # to be executed. This means that if it is an instance method,
-        # the block should return an instance of the class.
-        #
-        # @param [String] provider The name of the provider
-        # @param [String] cap The name of the capability
-        def self.provider_capability(provider, cap, &block)
-          components.provider_capabilities[provider.to_sym].register(cap.to_sym, &block)
-          nil
-        end
-    
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
-      end
-    end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
-  end
-end
-
-    
-      # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+        def backtrace_pattern
+      loc = Rake.application.find_rakefile_location
+      return unless loc
