7739f9ff695dd1a4b3a7736557a5e94754f53df3
SUNsung <weatherius@gmail.com>
2019-07-07 06:08:07 +0300

    Rewrite [56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb] File in: 2019-07-07 06:08:07

--- ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
+++ ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
@@ -1,53 +1,150 @@
 
         
-                [
-          'This will automatically tag your build with the following format: `<grouping>/<lane>/<prefix><build_number>`, where:'.markdown_preserve_newlines,
-          list,
-          'For example, for build 1234 in the 'appstore' lane, it will tag the commit with `builds/appstore/1234`.'
-        ].join('\n')
+          # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+    
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
+  end
+    
+        class Main < Clamp::Command
+      subcommand 'list', 'List all installed Logstash plugins', LogStash::PluginManager::List
+      subcommand 'install', 'Install a Logstash plugin', LogStash::PluginManager::Install
+      subcommand 'remove', 'Remove a Logstash plugin', LogStash::PluginManager::Remove
+      subcommand 'update', 'Update a plugin', LogStash::PluginManager::Update
+      subcommand 'pack', 'Package currently installed plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Pack
+      subcommand 'unpack', 'Unpack packaged plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Unpack
+      subcommand 'generate', 'Create the foundation for a new plugin', LogStash::PluginManager::Generate
+      subcommand 'uninstall', 'Uninstall a plugin. Deprecated: Please use remove instead', LogStash::PluginManager::Remove
+      subcommand 'prepare-offline-pack', 'Create an archive of specified plugins to use for offline installation', LogStash::PluginManager::PrepareOfflinePack
+    end
+  end
+end
+    
+            if explicit_plugins.any? { |spec| filename =~ /^#{spec.name}/ }
+          FileUtils.mv(gem_file, ::File.join(explicit_path, filename))
+        else
+          FileUtils.mv(gem_file, ::File.join(dependencies_path, filename))
+        end
       end
+    end
     
-            message = 'builds/test/#{build_number}#{postfix} (fastlane)'
-        tag = 'builds/test/#{build_number}#{postfix}'
-        expect(result).to eq('git tag -am #{message.shellescape} #{tag.shellescape}')
+      it 'does object equality on config_hash and pipeline_id' do
+    another_exact_pipeline = described_class.new(source, pipeline_id, ordered_config_parts, settings)
+    expect(subject).to eq(another_exact_pipeline)
+    
+            def swap_range(corrector, range1, range2)
+          src1 = range1.source
+          src2 = range2.source
+          corrector.replace(range1, src2)
+          corrector.replace(range2, src1)
+        end
       end
-    
-          it 'adds options param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            options: '--use-single-star --keep-intermediate-files --search-undocumented-doc'
-          )
-        end').runner.execute(:test)
-    
-            keychain = 'keychain with spaces.keychain'
-        cmd = %r{curl -f -o (([A-Z]\:)?\/.+) https://developer\.apple\.com/certificationauthority/AppleWWDRCA.cer && security import \1 -k #{Regexp.escape(keychain.shellescape)}}
-        require 'open3'
-    
-      it 'uses the replacement character' do
-    ec = Encoding::Converter.new('utf-8', 'us-ascii', :invalid => :replace, :undef => :replace)
-    ec.replacement = '!'
-    dest = ''
-    status = ec.primitive_convert '中文123', dest
-    
-        @exception2.readagain_bytes.encoding.should == Encoding::BINARY
+    end
   end
 end
 
     
-      it 'returns a replica of UTF-16BE' do
-    name = @prefix + 'UTF-16-BE'
-    e = Encoding::UTF_16BE.replicate(name)
-    e.name.should == name
-    'a'.force_encoding(e).valid_encoding?.should be_false
-    '\x30\x42'.force_encoding(e).valid_encoding?.should be_true
-    '\x80'.force_encoding(e).valid_encoding?.should be_false
+          def ignored_node?(node)
+        # Same object found in array?
+        ignored_nodes.any? { |n| n.equal?(node) }
+      end
+    
+      # Get the version of this package
+  def version
+    if instance_variable_defined?(:@version) && !@version.nil?
+      return @version
+    elsif attributes[:version_given?]
+      # 'version_given?' will be true in cases where the
+      # fpm command-line tool has been given '-v' or '--version' settings
+      # We do this check because the default version is '1.0'
+      # on the fpm command line.
+      return attributes.fetch(:version)
+    end
+    
+              extension_header[TAR_TYPEFLAG_OFFSET] = 'x'
+          extension_header[TAR_LENGTH_OFFSET_START..TAR_LENGTH_OFFSET_END] = extension_data.length.to_s(8).rjust(12, '0')
+          extension_header = checksum_header(extension_header)
+    
+        # extract Payload
+    safesystem('tar -xz -f #{expand_dir}/Payload -C #{staging_path}')
+  end # def input
+    
+      # Add a new source to this package.
+  # The exact behavior depends on the kind of package being managed.
+  #
+  # For instance:
+  #
+  # * for FPM::Package::Dir, << expects a path to a directory or files.
+  # * for FPM::Package::RPM, << expects a path to an rpm.
+  #
+  # The idea is that you can keep pumping in new things to a package
+  # for later conversion or output.
+  #
+  # Implementations are expected to put files relevant to the 'input' in the
+  # staging_path
+  def input(pacman_pkg_path)
+    control = {}
+    # Unpack the control tarball
+    safesystem(tar_cmd, '-C', staging_path, '-xf', pacman_pkg_path)
+    pkginfo = staging_path('.PKGINFO')
+    mtree = staging_path('.MTREE')
+    install = staging_path('.INSTALL')
+    
+        # Remove the stuff we don't want
+    delete_these = ['.depdb', '.depdblock', '.filemap', '.lock', '.channel', 'cache', 'temp', 'download', '.channels', '.registry']
+    Find.find(staging_path) do |path|
+      if File.file?(path)
+        logger.info('replacing staging_path in file', :replace_in => path, :staging_path => staging_path)
+        begin
+          content = File.read(path).gsub(/#{Regexp.escape(staging_path)}/, '')
+          File.write(path, content)
+        rescue ArgumentError => e
+          logger.warn('error replacing staging_path in file', :replace_in => path, :error => e)
+        end
+      end
+      FileUtils.rm_r(path) if delete_these.include?(File.basename(path))
+    end
+    
+      def install_script
+    path = build_path('installer.sh')
+    File.open(path, 'w') do |file|
+      file.write template('sh.erb').result(binding)
+    end
+    path
   end
     
-    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
-  file_shared_path = TestApp.shared_path.join(file)
-  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
-  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
-end
+      def default_output
+    v = version
+    v = '#{epoch}:#{v}' if epoch
+    if iteration
+      '#{name}_#{v}-#{iteration}_#{architecture}.#{type}'
+    else
+      '#{name}_#{v}_#{architecture}.#{type}'
+    end
+  end # def default_output
+end # class FPM::Deb
+    
+    describe Tmuxinator::Pane do
+  let(:klass) { described_class }
+  let(:instance) { klass.new(index, project, window, *commands) }
+  # let(:index) { 'vim' }
+  # let(:project) { 0 }
+  # let(:tab) { nil }
+  # let(:commands) { nil }
+  let(:index) { 0 }
+  let(:project) { double }
+  let(:window) { double }
+  let(:commands) { ['vim', 'bash'] }
+    
+      def attributes_match
+    expect(@actual).to have_attributes(@expected_attrs)
+  end
+    
+      if base_index = options.fetch(:base_index) { 1 }
+    standard_options << 'base-index #{base_index}'
+  end
+    
+          it { is_expected.to be true }
+    end
