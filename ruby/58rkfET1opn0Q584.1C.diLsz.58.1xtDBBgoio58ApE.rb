c98fbf54bcd7580f22aee56c9a7735b282ef17a6
SUNsung <weatherius@gmail.com>
2019-07-07 22:53:25 +0300

    Rewrite [58rkfET1opn0Q584.1C.diLsz.58.1xtDBBgoio58ApE.rb] File in: 2019-07-07 22:53:25

--- ruby/58rkfET1opn0Q584.1C.diLsz.58.1xtDBBgoio58ApE.rb
+++ ruby/58rkfET1opn0Q584.1C.diLsz.58.1xtDBBgoio58ApE.rb
@@ -1,110 +1,29 @@
 
         
-        module ActionView
-  module Helpers
-    module Tags # :nodoc:
-      class CollectionRadioButtons < Base # :nodoc:
-        include CollectionHelpers
-    
-                case options[:default]
-            when nil
-              Time.current
-            when Date, Time
-              options[:default]
-            else
-              default = options[:default].dup
-    
-      gem.add_development_dependency 'danger'
-  gem.add_development_dependency 'mocha'
-  gem.add_development_dependency 'rspec'
-  gem.add_development_dependency 'rubocop', '0.48.1'
-end
-
-    
-            filter = Object.new
-        def filter.filter(servers)
-          block.call(servers)
-        end
-      elsif !filter.respond_to? :filter
-        raise TypeError, 'Provided custom filter <#{filter.inspect}> does ' \
-                         'not have a public 'filter' method'
-      end
-      @custom_filters ||= []
-      @custom_filters << filter
-    end
-    
-          def using_default_scm?
-        return @using_default_scm if defined? @using_default_scm
-        @using_default_scm = (fetch(:scm) == DEFAULT_GIT)
-      end
-    
-          ServerKey = Struct.new(:hostname, :port)
-    
-    class Rack::Builder
-  include Sinatra::Delegator
-end
-
-    
-      # fetch data
-  fields = {
-    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
-    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
-    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
-  }
-    
-          def encrypt(value)
-        options[:encryptor].hexdigest value.to_s
-      end
-    
-          def call(env)
-        status, headers, body = super
-        response = Rack::Response.new(body, status, headers)
-        request = Rack::Request.new(env)
-        remove_bad_cookies(request, response)
-        response.finish
-      end
-    
-        it 'Reads referrer from Host header when Referer header is relative' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
-      expect(subject.referrer(env)).to eq('foo.com')
-    end
-    
-    # Set some attributes
-package.name = 'my-service'
-package.version = '1.0'
-    
-        pkgdata = {
-      'arch' => architecture,
-      'name' => name,
-      'version' => pkg_version,
-      'comment' => description,
-      'desc' => description,
-      'origin' => pkg_origin,
-      'maintainer' => maintainer,
-      'www' => url,
-      # prefix is required, but it doesn't seem to matter
-      'prefix' => '/',
-    }
-    
-      option '--validate', :flag, 'Validate with pkg install',
-    :default => true
-    
-        # This value is used later in the template for PKGINFO
-    size = safesystemout('du', '-sk', build_path).split(/\s+/)[0].to_i * 1024
-    builddate = Time.new.to_i
-    
-      # Where we keep metadata and post install scripts and such
-  def fpm_meta_path
-    @fpm_meta_path ||= begin
-                         path = File.join(staging_path, '.fpm')
-                         FileUtils.mkdir_p(path)
-                         path
-                       end
+          def serialize_options(resource)
+    methods = resource_class.authentication_keys.dup
+    methods = methods.keys if methods.is_a?(Hash)
+    methods << :password if resource.respond_to?(:password)
+    { methods: methods, only: [:password] }
   end
-end
-
     
-      # Input a zipfile.
-  def input(input_path)
-    # use part of the filename as the package name
-    self.name = File.extname(input_path)[1..-1]
+      # POST /resource/unlock
+  def create
+    self.resource = resource_class.send_unlock_instructions(resource_params)
+    yield resource if block_given?
+    
+    module UnpackStrategy
+  class Air
+    include UnpackStrategy
+    
+        not_matching_pipeline = described_class.new(source, pipeline_id, [], settings)
+    expect(subject).not_to eq(not_matching_pipeline)
+    
+    def system!(*args)
+  system(*args) || abort('\n== Command #{args} failed ==')
+end
+    
+    desc 'Default: run unit tests.'
+task :default => [:clean, :all]
+    
+        private
