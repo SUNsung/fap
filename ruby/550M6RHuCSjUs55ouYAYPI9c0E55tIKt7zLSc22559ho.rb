e6f11789c5b54a4c13a84845fb42fdd09962a6e1
SUNsung <weatherius@gmail.com>
2019-07-07 18:53:47 +0300

    Rewrite [550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb] File in: 2019-07-07 18:53:46

--- ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
+++ ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
@@ -1,92 +1,140 @@
 
         
-        Car = Struct.new(:color)
+        def pr(url)
+  if url.end_with?(FORWARD_SLASH)
+    url
+  else
+    url_dir = File.dirname(url)
+    url_dir.end_with?(FORWARD_SLASH) ? url_dir : '#{url_dir}/'
+  end
+end
     
-      #
-  # Initializes an HTTP packet header class that inherits from a Hash base
-  # class.
-  #
-  def initialize
-    self.dcase_hash = {}
-    
-      attr_accessor :client
-  attr_accessor :oseq, :iseq
-  attr_accessor :scall, :dcall
-  attr_accessor :codec, :state
-  attr_accessor :ring_start, :ring_finish
-  attr_accessor :itime
-  attr_accessor :queue
-  attr_accessor :audio_hook
-  attr_accessor :audio_buff
-  attr_accessor :time_limit
-  attr_accessor :busy
-    
-              # Encodes the start_time field
-          #
-          # @return [String]
-          def encode_start_time
-            [start_time].pack('N')
-          end
-    
-                checksum = OpenSSL::HMAC.digest('MD5', k1, data_encrypt)
-    
-              # Encodes the options field
-          #
-          # @return [OpenSSL::ASN1::BitString]
-          def encode_options
-            OpenSSL::ASN1::BitString.new([options].pack('N'))
-          end
-    
-              # Rex::Proto::Kerberos::Model::Checksum decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'Checksum decoding not supported'
-          end
-    
-              # Decodes the kvno from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_kvno(input)
-            input.value[0].value.to_i
-          end
-    
-              # Decodes the Rex::Proto::Kerberos::Model::KdcResponse from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode KdcResponse, invalid input'
-            end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an
-          # OpenSSL::ASN1::Sequence
-          #
-          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
-          def decode_asn1(input)
-            seq_values = input.value
-            self.type = decode_type(seq_values[0])
-            self.value = decode_value(seq_values[1])
-          end
-    
-        it 'generates the aspects_manage fixture', :fixture => true do
-      get :index, params: {a_id: @aspect.id}
-      save_fixture(html_for('body'), 'aspects_manage')
+        # If you've clearned the pin, use bumped_at, otherwise put it at the top
+    def order_nocategory_with_pinned_sql
+      -'CASE
+        WHEN topics.pinned_globally
+         AND (COALESCE(topics.pinned_at, '#{lowest_date}') > COALESCE(tu.cleared_pinned_at, '#{lowest_date}'))
+          THEN topics.pinned_at + interval '9999 years'
+          ELSE topics.bumped_at
+       END DESC'
     end
     
-          it 'it doesn't call toggle_hidden_shareable' do
-        expect(@controller.current_user).not_to receive(:toggle_hidden_shareable).with(an_instance_of(StatusMessage))
-        begin
-          put :update, params: {id: 42, post_id: @status.id}, format: :js
-        rescue ActiveRecord::RecordNotFound
+        # a poor man's upsert
+    id = where(date: date, req_type: req_type_id).pluck(:id).first
+    id ||= create!(date: date, req_type: req_type_id, count: 0).id
+    
+        it 'rejects invalid directives and ones that are not allowed to be extended' do
+      builder << {
+        invalid_src: ['invalid'],
+      }
+    
+          @directives.each do |directive, sources|
+        if sources.is_a?(Array)
+          policy.public_send(directive, *sources)
+        else
+          policy.public_send(directive, sources)
+        end
+      end
+    
+        def find_theme_extensions(theme_ids)
+      extensions = []
+    
+          render plain: svg_sprite, disposition: nil, content_type: 'application/javascript'
+    end
+  end
+    
+              io.close if io rescue nil
+    
+    module Docs
+  class PageDb
+    attr_reader :pages
+    
+        private
+    
+            css('.example-title + pre').each do |node|
+          node['name'] = node.previous_element.content.strip
+          node.previous_element.remove
+        end
+    
+      # Get message for given
+  def find_message(kind, options = {})
+    options[:scope] ||= translation_scope
+    options[:default] = Array(options[:default]).unshift(kind.to_sym)
+    options[:resource_name] = resource_name
+    options = devise_i18n_options(options)
+    I18n.t('#{options[:resource_name]}.#{kind}', options)
+  end
+    
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
+    
+              if mod.const_defined?('ClassMethods')
+            class_mod = mod.const_get('ClassMethods')
+            extend class_mod
+    
+      def after_sign_in_path_for(resource)
+    if resource.email_verified?
+      root_path
+    else
+      finish_signup_path
+    end
+  end
+end
+
+    
+      private
+    
+          def right_diff_line_number(id, line)
+        if line =~ /^@@/
+          m, ri                   = *line.match(/\+(\d+)/)
+          @right_diff_line_number = ri.to_i
+          @current_line_number    = @right_diff_line_number
+          ret                     = '...'
+        elsif line[0] == ?-
+          ret = ' '
+        elsif line[0] == ?+
+          ret                     = @right_diff_line_number.to_s
+          @right_diff_line_number += 1
+          @current_line_number    = @right_diff_line_number - 1
+        else
+          ret                     = @right_diff_line_number.to_s
+          @right_diff_line_number += 1
+          @current_line_number    = @right_diff_line_number - 1
+        end
+        ret
+      end
+    end
+  end
+end
+
+    
+          def allow_uploads
+        @allow_uploads
+      end
+    
+          def string_to_code string
+        # sha bytes
+        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
+        # Thanks donpark's IdenticonUtil.java for this.
+        # Match the following Java code
+        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
+        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
+    
+      def for_each_gem
+    SPREE_GEMS.each do |gem_name|
+      yield 'pkg/spree_#{gem_name}-#{version}.gem'
+    end
+    yield 'pkg/spree-#{version}.gem'
+  end
+    
+            def order_id
+          super || params[:id]
         end
       end
     end
@@ -94,38 +142,10 @@
 end
 
     
-    Then(/^git wrapper permissions are 0700$/) do
-  permissions_test = %Q([ $(stat -c '%a' #{TestApp.git_wrapper_path.shellescape}) == '700' ])
-  _stdout, _stderr, status = vagrant_cli_command('ssh -c #{permissions_test.shellescape}')
-    
-    Given(/^(\d+) valid existing releases$/) do |num|
-  a_day = 86_400 # in seconds
-  offset = -(a_day * num.to_i)
-  num.to_i.times do
-    run_vagrant_command('mkdir -p #{TestApp.release_path(TestApp.timestamp(offset))}')
-    offset += a_day
-  end
-end
-    
-          def warn_add_git_to_capfile
-        $stderr.puts(<<-MESSAGE)
-[Deprecation Notice] Future versions of Capistrano will not load the Git SCM
-plugin by default. To silence this deprecation warning, add the following to
-your Capfile after `require 'capistrano/deploy'`:
-    
-          def set(key, value=nil, &block)
-        @trusted_keys << key if trusted? && !@trusted_keys.include?(key)
-        remember_location(key)
-        values[key] = block || value
-        trace_set(key)
-        values[key]
-      end
-    
-            def destroy
-          @line_item = find_line_item
-          Spree::Dependencies.cart_remove_line_item_service.constantize.call(order: @order, line_item: @line_item)
-    
-    __END__
-    
-      puts '\n== Removing old logs and tempfiles =='
-  system! 'bin/rails log:clear tmp:clear'
+            # Dynamically defines our stores checkout steps to ensure we check authorization on each step.
+        Order.checkout_steps.keys.each do |step|
+          define_method step do
+            find_order
+            authorize! :update, @order, params[:token]
+          end
+        end
