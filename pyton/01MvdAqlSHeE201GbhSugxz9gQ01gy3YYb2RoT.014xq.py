5484efd190244b3465458b0ef895fd87f3a5dad5
SUNsung <weatherius@gmail.com>
2019-07-08 02:31:28 +0300

    Rewrite [01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py] File in: 2019-07-08 02:31:27

--- pyton/01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py
+++ pyton/01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py
@@ -1,103 +1,95 @@
 
         
-            SPOTS_PER_ROW = 10
+        
+@pytest.mark.parametrize('command, new_command, packages', [
+    (Command('vim', ''), 'sudo apt-get install vim && vim',
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('convert', ''), 'sudo apt-get install imagemagick && convert',
+     [('imagemagick', 'main'),
+      ('graphicsmagick-imagemagick-compat', 'universe')]),
+    (Command('sudo vim', ''), 'sudo apt-get install vim && sudo vim',
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo convert', ''), 'sudo apt-get install imagemagick && sudo convert',
+     [('imagemagick', 'main'),
+      ('graphicsmagick-imagemagick-compat', 'universe')])])
+def test_get_new_command(mocker, command, new_command, packages):
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
     
-        def current_year_month(self):
-        '''Return the current year and month.'''
-        ...
+     install - install packages
+ remove  - remove packages
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer),
-            self.mr(mapper=self.mapper_sort,
-                    reducer=self.reducer_identity),
-        ]
-    
-    # A shorter title for the navigation bar.  Default is the same as html_title.
-#
-# html_short_title = None
-    
-        content = re.sub('MAJOR_VERSION = .*\n',
-                     'MAJOR_VERSION = {}\n'.format(major),
-                     content)
-    content = re.sub('MINOR_VERSION = .*\n',
-                     'MINOR_VERSION = {}\n'.format(minor),
-                     content)
-    content = re.sub('PATCH_VERSION = .*\n',
-                     'PATCH_VERSION = '{}'\n'.format(patch),
-                     content)
+      aws help
+  aws <command> help
+  aws <command> <subcommand> help
+aws: error: argument operation: Invalid choice, valid choices are:
     
     
-class UnpackValueError(UnpackException, ValueError):
-    pass
-    
-    
-def test_getitem_callable():
-    # GH 12533
-    s = pd.Series(4, index=list('ABCD'))
-    result = s[lambda x: 'A']
-    assert result == s.loc['A']
-    
-        ('smaller', None, '10pt'),
-    ('smaller', '18pt', '15pt'),
-    ('larger', None, '{pt:f}pt'.format(pt=14.4)),
-    ('larger', '15pt', '18pt'),
-])
-def test_css_relative_font_size(size, relative_to, resolved):
-    if relative_to is None:
-        inherited = None
-    else:
-        inherited = {'font-size': relative_to}
-    assert_resolves('font-size: {size}'.format(size=size),
-                    {'font-size': resolved}, inherited=inherited)
+@pytest.mark.parametrize('before, after', [
+    ('brew install sshfs',
+     'brew cask install osxfuse && brew install sshfs')])
+def test_get_new_command(before, after):
+    command = Command(before, output)
+    assert get_new_command(command) == after
 
     
-        # FILL
-    # - color, fillType
-    ('background-color: red', {'fill': {'fgColor': 'FF0000',
-                                        'patternType': 'solid'}}),
-    ('background-color: #ff0000', {'fill': {'fgColor': 'FF0000',
-                                            'patternType': 'solid'}}),
-    ('background-color: #f0a', {'fill': {'fgColor': 'FF00AA',
-                                         'patternType': 'solid'}}),
-    # BORDER
-    # - style
-    ('border-style: solid',
-     {'border': {'top': {'style': 'medium'},
-                 'bottom': {'style': 'medium'},
-                 'left': {'style': 'medium'},
-                 'right': {'style': 'medium'}}}),
-    ('border-style: solid; border-width: thin',
-     {'border': {'top': {'style': 'thin'},
-                 'bottom': {'style': 'thin'},
-                 'left': {'style': 'thin'},
-                 'right': {'style': 'thin'}}}),
+    
+@pytest.mark.skipif(_is_not_okay_to_test(),
+                    reason='No need to run if there\'s no formula')
+def test_get_new_command(brew_no_available_formula):
+    assert get_new_command(Command('brew install elsticsearch',
+                                   brew_no_available_formula))\
+        == 'brew install elasticsearch'
     
     
-def test_str8():
-    header = b'\xd9'
-    data = b'x' * 32
-    b = packb(data.decode(), use_bin_type=True)
-    assert len(b) == len(data) + 2
-    assert b[0:2] == header + b'\x20'
-    assert b[2:] == data
-    assert unpackb(b) == data
+@pytest.mark.parametrize('command', [
+    Command('cargo buid', no_such_subcommand_old),
+    Command('cargo buils', no_such_subcommand)])
+def test_match(command):
+    assert match(command)
+    
+            os.chdir(str(tmpdir))
+        reset(path)
+    
+    from django.conf import settings
+from django.template.backends.django import DjangoTemplates
+from django.template.loader import get_template
+from django.utils.functional import cached_property
+from django.utils.module_loading import import_string
     
     
-def test_read_map_header():
-    unpacker = Unpacker()
-    unpacker.feed(packb({'a': 'A'}))
-    assert unpacker.read_map_header() == 1
-    assert unpacker.unpack() == B'a'
-    assert unpacker.unpack() == B'A'
-    try:
-        unpacker.unpack()
-        assert 0, 'should raise exception'
-    except OutOfData:
-        assert 1, 'okay'
+def add_or_delete_heroku_collaborator(module, client):
+    user = module.params['user']
+    state = module.params['state']
+    affected_apps = []
+    result_state = False
+    
+        module = AnsibleModule(argument_spec, supports_check_mode=True)
+    
+        def get_status():
+        '''Return the status of the process in monit, or the empty string if not present.'''
+        rc, out, err = module.run_command('%s %s' % (MONIT, SUMMARY_COMMAND), check_rc=True)
+        for line in out.split('\n'):
+            # Sample output lines:
+            # Process 'name'    Running
+            # Process 'name'    Running - restart pending
+            parts = parse(line.split())
+            if parts != '':
+                return parts
+    
+        if model.get('error'):
+        module.fail_json(msg='error checking device: %s' % model.get('error'))
     
     
-def gen_binary_data(idx):
-    return binarydata[:idx % 300]
+def _test():
+    ''''''
+    file_path = r'./data.txt'
+    
+    
+def parametric_relu(x, channel_shared=False, alpha_init=constant(0.), name='parametric_relu', reuse=None):
+    '''参数化 ReLU
+    
+        assert len(kernel_size) == 2
+    assert len(strides) == 4
+    
+        return x
