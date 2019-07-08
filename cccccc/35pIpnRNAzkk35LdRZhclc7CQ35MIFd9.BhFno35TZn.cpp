ec676ab9510d1010b0aab9530c84584fddb1d762
SUNsung <weatherius@gmail.com>
2019-07-08 18:55:31 +0300

    Rewrite [35pIpnRNAzkk35LdRZhclc7CQ35MIFd9.BhFno35TZn.cpp] File in: 2019-07-08 18:55:30

--- cccccc/35pIpnRNAzkk35LdRZhclc7CQ35MIFd9.BhFno35TZn.cpp
+++ cccccc/35pIpnRNAzkk35LdRZhclc7CQ35MIFd9.BhFno35TZn.cpp
@@ -1,182 +1,160 @@
 
         
-        // Names that corresponds to a single input parameter.
-class ParamNames {
+        class CensusChannelData : public ChannelData {
  public:
-  // Create param based on Arg.
-  ParamNames(const string& name, const string& rename_to) : name_(name) {
-    rename_to_ = AvoidPythonReserved(rename_to);
-  }
-    }
-    
-    REGISTER_OP('ShapelessOp');
-    
-    
-    {
-    {    output() = 'A(m, 0) == A(m-1, 1)';
-  }
+  grpc_error* Init(grpc_channel_element* elem,
+                   grpc_channel_element_args* args) override;
 };
     
-    void PyExceptionRegistry::Init(PyObject* code_to_exc_type_map) {
-  DCHECK(singleton_ == nullptr) << 'PyExceptionRegistry::Init() already called';
-  singleton_ = new PyExceptionRegistry;
-    }
+    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
+::opencensus::stats::MeasureDouble RpcClientServerLatency();
+::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
     
-    #include 'tensorflow/stream_executor/cuda/cuda_platform_id.h'
     
-    // Utility class to allow Platform objects to manage cached StreamExecutors.
-// Thread-safe.
-class ExecutorCache {
- public:
-  ExecutorCache() {}
-    }
-    
-    enum GARBAGE_LEVEL
-{
-  G_NEVER_CRUNCH,
-  G_OK,
-  G_DODGY,
-  G_TERRIBLE
+    {  // Schedule the given callback for execution.
+  virtual void Add(const std::function<void()>& callback) = 0;
 };
     
-     private:
-  // Gets the up to the first 3 prefixes from s (split by _).
-  // For example, tesseract_foo_bar will be split into tesseract,foo and bar.
-  void GetPrefixes(const char* s, STRING* level_one,
-                   STRING* level_two, STRING* level_three);
+    using grpc::core::Bucket;
+using grpc::core::Histogram;
+using grpc::core::Metric;
+using grpc::core::Stats;
+    
+    #include <grpc/support/time.h>
+#include <grpcpp/support/config.h>
+#include <grpcpp/support/time.h>
+    
+    int64_t VMTOC::getIndex(uint64_t elem) {
+  auto pos = m_map.find(elem);
+  if (pos != m_map.end()) {
+    return pos->second;
+  }
+  return LLONG_MIN;
+}
+    
+          MD_form_t md_formater {{
+        rc,
+        static_cast<uint32_t>(sh >> 5),                         // sh5
+        xop,
+        static_cast<uint32_t>(((mb >> 5) | (mb << 1)) & 0x3F),  // me5 || me0:4
+        static_cast<uint32_t>(sh & 0x1F),                       // sh0:4
+        static_cast<uint32_t>(ra),
+        static_cast<uint32_t>(rs),
+        op
+      }};
+    
+      /**
+   * Fill up vector with summary information.
+   */
+  virtual void debuggerInfo(InfoVec& /*info*/) {}
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    #include 'hphp/runtime/base/file.h'
+#include 'hphp/runtime/base/mem-file.h'
+#include 'hphp/runtime/base/stream-wrapper.h'
+#include <folly/String.h>
+#include <folly/portability/SysStat.h>
+#include <folly/portability/Unistd.h>
+    
+    #ifndef incl_HPHP_OUTPUT_FILE_H_
+#define incl_HPHP_OUTPUT_FILE_H_
     
     
-/**********************************************************************
- * recog_word_recursive
+    {  StructuredLogEntry cols;
+  fillCols(cols);
+  cols.setStr('event_name', event);
+  cols.setInt('priority', priority);
+  StructuredLog::log('hhvm_perf_warning', cols);
+}
+    
+    void logAHMSubMapWarning(folly::StringPiece mapName) {
+  StackTrace st;
+  logPerfWarning(
+    'AtomicHashMap overflow',
+    [&](StructuredLogEntry& cols) {
+      cols.setStr('map_name', mapName);
+      cols.setStackTrace('stack', st);
+    }
+  );
+}
+    
+      // overriding ResourceData
+  const String& o_getClassNameHook() const override { return classnameof(); }
+    
+    
+    
+    #include 'hphp/runtime/base/user-attributes.h'
+#include 'hphp/runtime/base/tv-comparisons.h'
+#include 'hphp/runtime/vm/disas.h'
+    
+        // output element with key 'the ugly'
+    std::cout << object.at('the ugly') << '\n';
+    
+    /**
+ * @brief Utility method to get value for specified key.
  *
- * Convert the word to tess form and pass it to the tess segmenter.
- * Convert the output back to editor form.
- **********************************************************************/
-void Tesseract::recog_word_recursive(WERD_RES *word) {
-  int word_length = word->chopped_word->NumBlobs();  // no of blobs
-  if (word_length > MAX_UNDIVIDED_LENGTH) {
-    return split_and_recog_word(word);
+ * Docker supports querying primary columns by prefix. This is preserved when
+ * querying thought OSQuery.
+ *
+ * For example the following should return same result as long as there is only
+ * one container with 'id' that starts with '12345678':
+ *   SELECT * FROM docker_containers WHERE id = '1234567890abcdef'
+ *   SELECT * FROM docker_containers WHERE id = '12345678'
+ *
+ * @param tree Property tree response from docker.
+ * @param set Set that might contain prefix values.
+ * @param key Key to look for in the property tree.
+ */
+std::string getValue(const pt::ptree& tree,
+                     const std::set<std::string>& set,
+                     const std::string& key) {
+  std::string value = tree.get<std::string>(key, '');
+  if (boost::starts_with(value, 'sha256:')) {
+    value.erase(0, 7);
   }
-  cc_recog(word);
-  word_length = word->rebuild_word->NumBlobs();  // No of blobs in output.
+  if (set.empty()) {
+    return value; // Return value from tree, if set is empty
+  }
     }
     
-      // Clean up the bounding boxes from the polygonal approximation by
-  // expanding slightly, then clipping to the blobs from the original_word
-  // that overlap. If not null, the block provides the inverse rotation.
-  void ClipToOriginalWord(const BLOCK* block, WERD* original_word);
+        std::string pack;
+    auto stat = genPack(name->second, value->second, pack);
+    response.push_back({{name->second, pack}});
+    return stat;
+  } else if (action->second == 'update') {
+    auto source = request.find('source');
+    auto data = request.find('data');
+    if (source == request.end() || data == request.end()) {
+      return Status(1, 'Missing source or data');
+    }
     
-    // Solve the dynamic programming problem for the given array of points, with
-// the given size and cost function.
-// Steps backwards are limited to being between min_step and max_step
-// inclusive.
-// The return value is the tail of the best path.
-DPPoint* DPPoint::Solve(int min_step, int max_step, bool debug,
-                        CostFunc cost_func, int size, DPPoint* points) {
-  if (size <= 0 || max_step < min_step || min_step >= size)
-    return nullptr;  // Degenerate, but not necessarily an error.
-  ASSERT_HOST(min_step > 0);  // Infinite loop possible if this is not true.
-  if (debug)
-    tprintf('min = %d, max=%d\n',
-            min_step, max_step);
-  // Evaluate the total cost at each point.
-  for (int i = 0; i < size; ++i) {
-    for (int offset = min_step; offset <= max_step; ++offset) {
-      DPPoint* prev = offset <= i ? points + i - offset : nullptr;
-      int64_t new_cost = (points[i].*cost_func)(prev);
-      if (points[i].best_prev_ != nullptr && offset > min_step * 2 &&
-          new_cost > points[i].total_cost_)
-        break;  // Find only the first minimum if going over twice the min.
+    #include <osquery/utils/system/env.h>
+    
+      virtual ExpectedSuccess<DatabaseError> open() = 0;
+  virtual void close() = 0;
+    
+    Expected<std::string, DatabaseError> RocksdbDatabase::getString(
+    const std::string& domain, const std::string& key) {
+  auto result = getRawBytes(domain, key);
+  if (result) {
+    std::string result_str = result.take();
+    if (BOOST_UNLIKELY(validateInt32StorageBuffer(result_str))) {
+      auto type_error = createError(RocksdbError::UnexpectedValueType)
+                        << 'Fetching string as integer';
+      LOG(ERROR) << type_error.getMessage().c_str();
+      assert(false);
+      return createError(DatabaseError::KeyNotFound, std::move(type_error));
     }
-    points[i].total_cost_ += points[i].local_cost_;
-    if (debug) {
-      tprintf('At point %d, local cost=%d, total_cost=%d, steps=%d\n',
-              i, points[i].local_cost_, points[i].total_cost_,
-              points[i].total_steps_);
-    }
+    return result_str;
   }
-  // Now find the end of the best path and return it.
-  int best_cost = points[size - 1].total_cost_;
-  int best_end = size - 1;
-  for (int end = best_end - 1; end >= size - min_step; --end) {
-    int cost = points[end].total_cost_;
-    if (cost < best_cost) {
-      best_cost = cost;
-      best_end = end;
-    }
-  }
-  return points + best_end;
+  return result.takeError();
 }
     
-    template<>
-Operator* CreateOp<cpu>(CaffeOpParam param, int dtype) {
-  Operator *op = NULL;
-  switch (dtype) {
-  case mshadow::kFloat32:
-    op = new CaffeOp<cpu, float>(param);
-    break;
-  case mshadow::kFloat64:
-    op = new CaffeOp<cpu, double>(param);
-    break;
-  case mshadow::kFloat16:
-    LOG(FATAL) << 'float16 layer is not supported by caffe';
-    break;
-  default:
-    LOG(FATAL) << 'Unsupported type ' << dtype;
-  }
-  return op;
-}
+    #include <unordered_map>
     
-    MXNET_DLL int MXCVcopyMakeBorder(NDArrayHandle src,
-                                 const int top,
-                                 const int bot,
-                                 const int left,
-                                 const int right,
-                                 const int type,
-                                 const double value,
-                                 NDArrayHandle *out) {
-  API_BEGIN();
-  NDArray ndsrc = *static_cast<NDArray*>(src);
-  CHECK_EQ(ndsrc.shape().ndim(), 3);
-  CHECK_EQ(ndsrc.ctx(), Context::CPU());
-  CHECK_EQ(ndsrc.dtype(), mshadow::kUint8);
-    }
-    
-    namespace mxnet {
-namespace engine {
-    }
-    }
-    
-    Graph DetectInplaceAddTo(Graph g) {
-  nnvm::StorageVector storage_id =
-      g.MoveCopyAttr<nnvm::StorageVector>('storage_id');
-  std::vector<int> storage_inplace_index =
-      g.MoveCopyAttr<std::vector<int> >('storage_inplace_index');
-  static const Op* ewise_plus_op = Op::Get('_grad_add');
-  auto& idx = g.indexed_graph();
-  // reference cont.
-  std::vector<int> ref_count(idx.num_node_entries(), 0);
-  std::vector<int> addto_entry(idx.num_node_entries(), 0);
-  std::vector<int> skip_plus_node(idx.num_nodes(), 0);
-    }
-    
-    #include '../common/utils.h'
-    
-    /*!
- *  Copyright (c) 2015 by Contributors
- * \file iter_batchloader.h
- * \brief define a batch adapter to create tblob batch
- */
-#ifndef MXNET_IO_ITER_BATCHLOADER_H_
-#define MXNET_IO_ITER_BATCHLOADER_H_
-    
-    /*!
- * \brief Iterator that normalize a image.
- *  It also applies a few augmention before normalization.
- */
-class ImageNormalizeIter : public IIterator<DataInst> {
- public:
-  explicit ImageNormalizeIter(IIterator<DataInst> *base)
-      : base_(base), meanfile_ready_(false) {
-  }
-    }
+    #pragma once
