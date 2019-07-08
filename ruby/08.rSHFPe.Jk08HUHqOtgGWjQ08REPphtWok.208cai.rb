2712fb19a9eb182ad8b0c2276b9d2d7e9e5e0de2
SUNsung <weatherius@gmail.com>
2019-07-08 16:57:51 +0300

    Rewrite [08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb] File in: 2019-07-08 16:57:51

--- ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
+++ ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
@@ -1,40 +1,47 @@
 
         
-        DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
-    
-    
-def pathutil_relative
-  Pathutil.new(DOC_PATH).relative_path_from(COL_PATH).to_s
-end
-    
-            # For a description of the protocol see
-        # http://feedback.livereload.com/knowledgebase/articles/86174-livereload-protocol
-        def reload(pages)
-          pages.each do |p|
-            json_message = JSON.dump(
-              :command => 'reload',
-              :path    => p.url,
-              :liveCSS => true
-            )
-    
-                    @env.machine_names.each do |machine_name|
-                  if machine_name =~ regex
-                    machines << get_machine.call(machine_name)
-                  end
-                end
-    
-    When /^(?:|I )fill in '([^']*)' with '([^']*)'$/ do |field, value|
-  fill_in(field, :with => value)
-end
-    
-      def migration_version
-    if Rails.version.start_with? '5'
-      '[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]'
+            if rbenv_prefix = prefix_from_bin('rbenv')
+      prefixes << rbenv_prefix
     end
-  end
-end
-
     
-        def initialize(filepath)
-      @filepath = filepath
+        def tmux_layout_command
+      '#{project.tmux} select-layout -t #{tmux_window_target} #{layout}'
+    end
+    
+      chain :with_commands do |*expected|
+    @commands = expected
+  end
+  alias_method :and_commands, :with_commands
+    
+          # Sorted list of all .yml files, including duplicates
+      def configs
+        directories.map do |directory|
+          Dir['#{directory}/**/*.yml'].map do |path|
+            path.gsub('#{directory}/', '').gsub('.yml', '')
+          end
+        end.flatten.sort
+      end
+    
+        context 'only $XDG_CONFIG_HOME/tmuxinator exists' do
+      it 'is #xdg' do
+        allow(described_class).to receive(:environment?).and_return false
+        allow(described_class).to receive(:xdg?).and_return true
+        allow(described_class).to receive(:home?).and_return false
+    
+      before do
+    allow(project).to receive_messages(
+      tmux: 'tmux',
+      name: 'test',
+      base_index: 1,
+      pane_base_index: 0,
+      root: '/project/tmuxinator',
+      root?: true
+    )
+  end
+    
+      describe '#local' do
+    before do
+      allow(Tmuxinator::Config).to receive_messages(validate: project)
+      allow(Tmuxinator::Config).to receive_messages(version: 1.9)
+      allow(Kernel).to receive(:exec)
     end
