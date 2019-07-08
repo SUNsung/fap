5119fd8348f96585c226f757884ba0b7ac3a7149
SUNsung <weatherius@gmail.com>
2019-07-08 12:41:24 +0300

    Rewrite [40FRxkVhFqa3640KPTPcQygE2.40mvALLTO8xXs408oz.py] File in: 2019-07-08 12:41:23

--- pyton/40FRxkVhFqa3640KPTPcQygE2.40mvALLTO8xXs408oz.py
+++ pyton/40FRxkVhFqa3640KPTPcQygE2.40mvALLTO8xXs408oz.py
@@ -1,219 +1,242 @@
 
         
-            def score(self):
-        total_value = 0
-        for card in self.cards:
-            total_value += card.value
-        return total_value
+        from utils import write_datasets
+from synthetic_data_utils import add_alignment_projections, generate_data
+from synthetic_data_utils import generate_rnn, get_train_n_valid_inds
+from synthetic_data_utils import nparray_and_transpose
+from synthetic_data_utils import spikify_data, gaussify_data, split_list_by_inds
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.signal
     
-        def handle_budget_notifications(self, key, total):
-        '''Call notification API if nearing or exceeded budget.'''
-        ...
+        test_data = utils.parse_commonsense_reasoning_test(test_data_name)
+    self.question_ids, self.sentences, self.labels = test_data
+    self.all_probs = []  # aggregate single-model prediction here.
     
-    from enum import Enum
-    
-    new_version = {}
-    
-    
-def filter_options(readme):
-    ret = ''
-    in_options = False
-    for line in readme.split('\n'):
-        if line.startswith('# '):
-            if line[2:].startswith('OPTIONS'):
-                in_options = True
-            else:
-                in_options = False
-    
-            def print_skipping(reason):
-            print('Skipping %s: %s' % (test_case['name'], reason))
-        if not ie.working():
-            print_skipping('IE marked as not _WORKING')
-            return
-    
-    rootDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    
-        def _get_ip(self, protocol):
-        if self._SKIP_SOCKS_TEST:
-            return '127.0.0.1'
+      def word_to_id(self, word):
+    if word in self._word_to_id:
+      return self._word_to_id[word]
+    else:
+      if word.lower() in self._word_to_id:
+        return self._word_to_id[word.lower()]
+    return self.unk
     
     
-class UnaccentExtension(CreateExtension):
+def imdb_raw_data(data_path=None):
+  '''Load IMDB raw data from data directory 'data_path'.
+  Reads IMDB tf record files containing integer ids,
+  and performs mini-batching of the inputs.
+  Args:
+    data_path: string path to the directory where simple-examples.tgz has
+      been extracted.
+  Returns:
+    tuple (train_data, valid_data)
+    where each of the data objects can be passed to IMDBIterator.
+  '''
     
-        def delete(self, session_key=None):
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        self._cache.delete(self.cache_key_prefix + session_key)
+        Args:
+      hparams:  Hyperparameters for the MaskGAN.
+      data: Data to evaluate.
+      id_to_word: Dictionary of indices to words.
+      log_dir: Log directory.
+      output_file:  Output file for the samples.
+  '''
+  # Boolean indicating operational mode.
+  is_training = False
     
-        def create(self):
-        while True:
-            self._session_key = self._get_new_session_key()
-            try:
-                # Save immediately to ensure we have a unique entry in the
-                # database.
-                self.save(must_create=True)
-            except CreateError:
-                # Key wasn't unique. Try again.
-                continue
-            self.modified = True
-            return
+      # Cumulative Discounted Returns.  The true value function V*(s).
+  cumulative_rewards = []
+  for t in xrange(FLAGS.sequence_length):
+    cum_value = tf.zeros(shape=[FLAGS.batch_size])
+    for s in xrange(t, FLAGS.sequence_length):
+      cum_value += missing_list[s] * np.power(gamma, (s - t)) * rewards_list[s]
+    cumulative_rewards.append(cum_value)
+  cumulative_rewards = tf.stack(cumulative_rewards, axis=1)
     
-        For complete documentation on using Sessions in your code, consult
-    the sessions documentation that is shipped with Django (also available
-    on the Django Web site).
-    '''
-    objects = SessionManager()
+      if not FLAGS.dis_share_embedding:
+    decoder_embedding = [
+        v for v in tf.trainable_variables()
+        if v.op.name == 'dis/decoder/rnn/embedding'
+    ][0]
+  decoder_lstm_w_0 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'dis/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel'
+  ][0]
+  decoder_lstm_b_0 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'dis/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias'
+  ][0]
+  decoder_lstm_w_1 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'dis/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel'
+  ][0]
+  decoder_lstm_b_1 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'dis/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias'
+  ][0]
     
-            loader.testNamePatterns = ['*My*']
-        self.assertEqual(loader.getTestCaseNames(MyTest), ['test_1', 'test_2'])
+      return decoder_fn
     
-            Returns true if the buffer is up to capacity. This method can be
-        overridden to implement custom flushing strategies.
-        '''
-        return (len(self.buffer) >= self.capacity)
+        invocations = dict(
+        aliases='alias_details',
+        all='all_details',
+        config='config_details',
+        mappings='mapping_details',
+        policy='policy_details',
+        versions='version_details',
+    )
     
-        def _copy_file_without_generated_symbols(self, source_file, dest_file):
-        with open(source_file) as fp:
-            lines = fp.readlines()
-        with open(dest_file, 'w') as fp:
-            fp.writelines(lines[:lines.index('#--start constants--\n') + 1])
-            fp.writelines(lines[lines.index('#--end constants--\n'):])
+    - name: Create a host and adjust its template
+  one_host:
+    name: host2
+    cluster_name: default
+    template:
+        LABELS:
+            - gold
+            - ssd
+        RESERVED_CPU: -100
+'''
     
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
     
-# Split a path in root and extension.
-# The extension is everything starting at the last dot in the last
-# pathname component; the root is everything before that.
-# It is always true that root + ext == p.
+        from sklearn.tree import DecisionTreeRegressor
     
-            # Log all prober confidences if none met MINIMUM_THRESHOLD
-        if self.logger.getEffectiveLevel() == logging.DEBUG:
-            if self.result['encoding'] is None:
-                self.logger.debug('no probers hit minimum threshold')
-                for group_prober in self._charset_probers:
-                    if not group_prober:
-                        continue
-                    if isinstance(group_prober, CharSetGroupProber):
-                        for prober in group_prober.probers:
-                            self.logger.debug('%s %s confidence = %s',
-                                              prober.charset_name,
-                                              prober.language,
-                                              prober.get_confidence())
-                    else:
-                        self.logger.debug('%s %s confidence = %s',
-                                          prober.charset_name,
-                                          prober.language,
-                                          prober.get_confidence())
-        return self.result
-
+        fn = os.path.relpath(fn,
+                         start=os.path.dirname(__import__(package).__file__))
+    try:
+        lineno = inspect.getsourcelines(obj)[1]
+    except Exception:
+        lineno = ''
+    return url_fmt.format(revision=revision, package=package,
+                          path=fn, lineno=lineno)
     
-        def feed(self, byte_str):
-        for c in byte_str:
-            coding_state = self.coding_sm.next_state(c)
-            if coding_state == MachineState.ERROR:
-                self._state = ProbingState.NOT_ME
-                break
-            elif coding_state == MachineState.ITS_ME:
-                self._state = ProbingState.FOUND_IT
-                break
-            elif coding_state == MachineState.START:
-                if self.coding_sm.get_current_charlen() >= 2:
-                    self._num_mb_chars += 1
-    
-            return TLSConfig(
-            client_cert=client_cert, verify=verify, ca_cert=ca_cert,
-            assert_hostname=False if skip_hostname_check else None,
-            ssl_version=tls_version
-        )
-    
-    
-def get_named_volumes(services):
-    volume_specs = [
-        VolumeSpec.parse(volume)
-        for service in services.values()
-        for volume in service.get('volumes', [])
-    ]
-    names = {
-        spec.external
-        for spec in volume_specs
-        if spec.is_named_volume
-    }
-    return {name: {'external': True} for name in names}
-    
-        def download_from_appveyor(self, branch_name, filename):
-        print('Downloading {} from appveyor'.format(filename))
-        url = '{base}/dist%2F{filename}?branch={branch_name}'.format(
-            base=self.base_appveyor_url, filename=filename, branch_name=branch_name
-        )
-        full_dest = os.path.join(REPO_ROOT, self.destination, filename)
-        return self._download(url, full_dest)
+    print('consensus score: {:.1f}'.format(score))
     
     
-def build_config(contents, **kwargs):
-    return load(build_config_details(contents, **kwargs))
+# Plot the distances
+for index, metric in enumerate(['cosine', 'euclidean', 'cityblock']):
+    avg_dist = np.zeros((n_clusters, n_clusters))
+    plt.figure(figsize=(5, 4.5))
+    for i in range(n_clusters):
+        for j in range(n_clusters):
+            avg_dist[i, j] = pairwise_distances(X[y == i], X[y == j],
+                                                metric=metric).mean()
+    avg_dist /= avg_dist.max()
+    for i in range(n_clusters):
+        for j in range(n_clusters):
+            plt.text(i, j, '%5.3f' % avg_dist[i, j],
+                     verticalalignment='center',
+                     horizontalalignment='center')
     
-    from mitmproxy import exceptions
-from mitmproxy import flowfilter
-from mitmproxy import ctx
+        plt.figure(figsize=(6, 4))
+    for i in range(X_red.shape[0]):
+        plt.text(X_red[i, 0], X_red[i, 1], str(y[i]),
+                 color=plt.cm.nipy_spectral(labels[i] / 10.),
+                 fontdict={'weight': 'bold', 'size': 9})
     
-        def run(self, f, is_request):
-        if self.max_size:
-            r = f.request if is_request else f.response
-            try:
-                expected_size = http1.expected_http_body_size(
-                    f.request, f.response if not is_request else None
-                )
-            except exceptions.HttpException:
-                f.reply.kill()
-                return
-            if expected_size and not r.raw_content and not (0 <= expected_size <= self.max_size):
-                # r.stream may already be a callable, which we want to preserve.
-                r.stream = r.stream or True
-                ctx.log.info('Streaming {} {}'.format('response from' if not is_request else 'request to', f.request.host))
+    Evaluate the ability of k-means initializations strategies to make
+the algorithm convergence robust as measured by the relative standard
+deviation of the inertia of the clustering (i.e. the sum of squared
+distances to the nearest cluster center).
     
-            if log.log_tier(ctx.options.termlog_verbosity) >= log.log_tier(e.level):
-            click.secho(
-                e.msg,
-                file=outfile,
-                fg=dict(error='red', warn='yellow',
-                        alert='magenta').get(e.level),
-                dim=(e.level == 'debug'),
-                err=(e.level == 'error')
-            )
-
+    import random
+import threading
+import time
+import os
+from front_base.random_get_slice import RandomGetSlice
     
-        class SrgbChunk(KaitaiStruct):
+        data = response.content
+    response_headers = response.headers
+    if 'content-encoding' not in response_headers and len(response.content) < URLFETCH_DEFLATE_MAXSIZE and response_headers.get('content-type', '').startswith(('text/', 'application/json', 'application/javascript')):
+        if 'gzip' in accept_encoding:
+            response_headers['Content-Encoding'] = 'gzip'
+            compressobj = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, 0)
+            dataio = BytesIO()
+            dataio.write('\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\xff')
+            dataio.write(compressobj.compress(data))
+            dataio.write(compressobj.flush())
+            dataio.write(struct.pack('<LL', zlib.crc32(data) & 0xFFFFFFFFL, len(data) & 0xFFFFFFFFL))
+            data = dataio.getvalue()
+        elif 'deflate' in accept_encoding:
+            response_headers['Content-Encoding'] = 'deflate'
+            data = zlib.compress(data)[2:-4]
+    if data:
+         response_headers['Content-Length'] = str(len(data))
+    response_headers_data = zlib.compress('\n'.join('%s:%s' % (k.title(), v) for k, v in response_headers.items() if not k.startswith('x-google-')))[2:-4]
+    if 'rc4' not in options:
+        start_response('200 OK', [('Content-Type', __content_type__)])
+        yield struct.pack('!hh', int(response.status_code), len(response_headers_data))+response_headers_data
+        yield data
+    else:
+        start_response('200 OK', [('Content-Type', __content_type__), ('X-GOA-Options', 'rc4')])
+        yield struct.pack('!hh', int(response.status_code), len(response_headers_data))
+        yield rc4crypt(response_headers_data, __password__)
+        yield rc4crypt(data, __password__)
     
-    		page.addToken(0x08, 'Categories')
-		page.addToken(0x09, 'Category')
-		page.addToken(0x0A, 'Complete')
-		page.addToken(0x0B, 'DateCompleted')
-		page.addToken(0x0C, 'DueDate')
-		page.addToken(0x0D, 'UTCDueDate')
-		page.addToken(0x0E, 'Importance')
-		page.addToken(0x0F, 'Recurrence')
-		page.addToken(0x10, 'Recurrence_Type')
-		page.addToken(0x11, 'Recurrence_Start')
-		page.addToken(0x12, 'Recurrence_Until')
-		page.addToken(0x13, 'Recurrence_Occurrences')
-		page.addToken(0x14, 'Recurrence_Interval')
-		page.addToken(0x15, 'Recurrence_DayOfMonth')
-		page.addToken(0x16, 'Recurrence_DayOfWeek')
-		page.addToken(0x17, 'Recurrence_WeekOfMonth')
-		page.addToken(0x18, 'Recurrence_MonthOfYear')
-		page.addToken(0x19, 'Recurrence_Regenerate')
-		page.addToken(0x1A, 'Recurrence_DeadOccur')
-		page.addToken(0x1B, 'ReminderSet')
-		page.addToken(0x1C, 'ReminderTime')
-		page.addToken(0x1D, 'Sensitivity')
-		page.addToken(0x1E, 'StartDate')
-		page.addToken(0x1F, 'UTCStartDate')
-		page.addToken(0x20, 'Subject')
-		page.addToken(0x22, 'OrdinalDate')
-		page.addToken(0x23, 'SubOrdinalDate')
-		page.addToken(0x24, 'CalendarType')
-		page.addToken(0x25, 'IsLeapMonth')
-		page.addToken(0x26, 'FirstDayOfWeek')
-		self.codePages.append(page)
-		# endregion
+    # Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
+    
+    # Required dataset entry keys
+_IM_DIR = 'image_directory'
+_ANN_FN = 'annotation_file'
+    
+    
+def _do_matlab_eval(json_dataset, salt, output_dir='output'):
+    import subprocess
+    logger.info('-----------------------------------------------------')
+    logger.info('Computing results with the official MATLAB eval code.')
+    logger.info('-----------------------------------------------------')
+    info = voc_info(json_dataset)
+    path = os.path.join(
+        cfg.ROOT_DIR, 'detectron', 'datasets', 'VOCdevkit-matlab-wrapper')
+    cmd = 'cd {} && '.format(path)
+    cmd += '{:s} -nodisplay -nodesktop '.format(cfg.MATLAB)
+    cmd += '-r 'dbstop if error; '
+    cmd += 'voc_eval(\'{:s}\',\'{:s}\',\'{:s}\',\'{:s}\'); quit;'' \
+       .format(info['devkit_path'], 'comp4' + salt, info['image_set'],
+               output_dir)
+    logger.info('Running:\n{}'.format(cmd))
+    subprocess.call(cmd, shell=True)
+    
+    ... -> RoI ----\                               /-> box cls output -> cls loss
+                -> RoIFeatureXform -> box head
+... -> Feature /                               \-> box reg output -> reg loss
+       Map
+    
+    
+def _build_forward_graph(model, single_gpu_build_func):
+    '''Construct the forward graph on each GPU.'''
+    all_loss_gradients = {}  # Will include loss gradients from all GPUs
+    # Build the model on each GPU with correct name and device scoping
+    for gpu_id in range(cfg.NUM_GPUS):
+        with c2_utils.NamedCudaScope(gpu_id):
+            all_loss_gradients.update(single_gpu_build_func(model))
+    return all_loss_gradients
+    
+        assert len(blobs_in) == k_max - k_min + 1
+    bbox_feat_list = []
+    cls_pred_dim = (
+        model.num_classes if cfg.RETINANET.SOFTMAX else (model.num_classes - 1)
+    )
+    # unpacked bbox feature and add prediction layers
+    bbox_regr_dim = (
+        4 * (model.num_classes - 1) if cfg.RETINANET.CLASS_SPECIFIC_BBOX else 4
+    )
+    
+        if fg_inds.shape[0] > 0:
+        # Class labels for the foreground rois
+        mask_class_labels = blobs['labels_int32'][fg_inds]
+        masks = blob_utils.zeros((fg_inds.shape[0], M**2), int32=True)
+    
+    # Example usage:
+# data_loader_benchmark.par \
+#   NUM_GPUS 2 \
+#   TRAIN.DATASETS '('voc_2007_trainval',)' \
+#   TRAIN.PROPOSAL_FILES /path/to/voc_2007_trainval/proposals.pkl \
+#   DATA_LOADER.NUM_THREADS 4 \
+#   DATA_LOADER.MINIBATCH_QUEUE_SIZE 64 \
+#   DATA_LOADER.BLOBS_QUEUE_CAPACITY 8
