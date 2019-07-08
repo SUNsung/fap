0147f831c1cc9a22e3d799faee46b94818c37ee4
SUNsung <weatherius@gmail.com>
2019-07-09 00:48:29 +0300

    Rewrite [39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py] File in: 2019-07-09 00:48:28

--- pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
+++ pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
@@ -1,131 +1,41 @@
 
         
-            with gzip.open(paths[2], 'rb') as lbpath:
-        y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+            for trials in range(5):
+        a = random.randrange(2, num - 1)
+        v = pow(a, s, num)
+        if v != 1:
+            i = 0
+            while v != (num - 1):
+                if i == t - 1:
+                    return False
+                else:
+                    i = i + 1
+                    v = (v ** 2) % num
+    return True
     
-        # Test single image
-    x = np.random.uniform(0, 255, (10, 10, 3))
-    inputs = Input(shape=x.shape)
-    outputs = Lambda(utils.preprocess_input, output_shape=x.shape)(inputs)
-    model = Model(inputs, outputs)
-    assert model.predict(x[np.newaxis])[0].shape == x.shape
-    
-    
-def create_multi_input_model_from(layer1, layer2):
-    input_1 = Input(shape=(data_dim,))
-    input_2 = Input(shape=(data_dim,))
-    out1 = layer1(input_1)
-    out2 = layer2(input_2)
-    out = Average()([out1, out2])
-    model = Model([input_1, input_2], out)
-    model.add_loss(K.mean(out2))
-    model.add_loss(1)
-    model.add_loss(1)
-    return model
-    
-    model1 = create_network(num_classes=num_classes, **network1)
-history_model1 = model1.fit(x_train,
-                            y_train,
-                            batch_size=batch_size,
-                            epochs=epochs,
-                            verbose=1,
-                            validation_split=0.1)
-    
-        def triger_check_network(self, fail=False, force=False):
-        time_now = time.time()
-        if not force:
-            if self._checking_num > 0:
-                return
+    try:
+	xrange		#Python 2
+except NameError:
+	xrange = range	#Python 3
     
     
-def gae_application(environ, start_response):
-    cookie = environ.get('HTTP_COOKIE', '')
-    options = environ.get('HTTP_X_GOA_OPTIONS', '')
-    if environ['REQUEST_METHOD'] == 'GET' and not cookie:
-        if '204' in environ['QUERY_STRING']:
-            start_response('204 No Content', [])
-            yield ''
-        else:
-            timestamp = long(os.environ['CURRENT_VERSION_ID'].split('.')[1])/2**28
-            ctime = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(timestamp+8*3600))
-            html = u'GoAgent Python Server %s \u5df2\u7ecf\u5728\u5de5\u4f5c\u4e86\uff0c\u90e8\u7f72\u65f6\u95f4 %s\n' % (__version__, ctime)
-            start_response('200 OK', [('Content-Type', 'text/plain; charset=utf-8')])
-            yield html.encode('utf8')
-        raise StopIteration
-    
-    ## All tokens go to the parser (unless skip() is called in that rule)
-# on a particular 'channel'.  The parser tunes to a particular channel
-# so that whitespace etc... can go to the parser on a 'hidden' channel.
-DEFAULT_CHANNEL = 0
-    
+        '''
+        loop over all possible splits for the decision tree. find the best split.
+        if no split exists that is less than 2 * error for the entire array
+        then the data set is not split and the average for the entire array is used as the predictor
+        '''
+        for i in range(len(X)):
+            if len(X[:i]) < self.min_leaf_size:
+                continue
+            elif len(X[i:]) < self.min_leaf_size:
+                continue
             else:
-            return self.c
+                error_left = self.mean_squared_error(X[:i], np.mean(y[:i]))
+                error_right = self.mean_squared_error(X[i:], np.mean(y[i:]))
+                error = error_left + error_right
+                if error < min_error:
+                    best_split = i
+                    min_error = error
     
-            self._state.errorRecovery = False
-        self.input.consume()
-    
-    
-class HsmMethodTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.hsm = HierachicalStateMachine()
-    
-    
-class Professor(AbstractExpert):
-    @property
-    def is_eager_to_contribute(self):
-        return True if self.blackboard.common_state['problems'] > 100 else False
-    
-        def __init__(self, successor=None):
-        self.successor = successor
-    
-    parser = ArgumentParser(usage='%s -m jieba [options] filename' % sys.executable, description='Jieba command line interface.', epilog='If no filename specified, use STDIN instead.')
-parser.add_argument('-d', '--delimiter', metavar='DELIM', default=' / ',
-                    nargs='?', const=' ',
-                    help='use DELIM instead of ' / ' for word delimiter; or a space if it is used without DELIM')
-parser.add_argument('-p', '--pos', metavar='DELIM', nargs='?', const='_',
-                    help='enable POS tagging; if DELIM is specified, use DELIM instead of '_' for POS delimiter')
-parser.add_argument('-D', '--dict', help='use DICT as dictionary')
-parser.add_argument('-u', '--user-dict',
-                    help='use USER_DICT together with the default dictionary or DICT (if specified)')
-parser.add_argument('-a', '--cut-all',
-                    action='store_true', dest='cutall', default=False,
-                    help='full pattern cutting (ignored with POS tagging)')
-parser.add_argument('-n', '--no-hmm', dest='hmm', action='store_false',
-                    default=True, help='don't use the Hidden Markov Model')
-parser.add_argument('-q', '--quiet', action='store_true', default=False,
-                    help='don't print loading messages to stderr')
-parser.add_argument('-V', '--version', action='version',
-                    version='Jieba ' + jieba.__version__)
-parser.add_argument('filename', nargs='?', help='input file')
-    
-        iterkeys = lambda d: iter(d.keys())
-    itervalues = lambda d: iter(d.values())
-    iteritems = lambda d: iter(d.items())
-    
-    
-def viterbi(obs, states, start_p, trans_p, emit_p):
-    V = [{}]  # tabular
-    path = {}
-    for y in states:  # init
-        V[0][y] = start_p[y] + emit_p[y].get(obs[0], MIN_FLOAT)
-        path[y] = [y]
-    for t in xrange(1, len(obs)):
-        V.append({})
-        newpath = {}
-        for y in states:
-            em_p = emit_p[y].get(obs[t], MIN_FLOAT)
-            (prob, state) = max(
-                [(V[t - 1][y0] + trans_p[y0].get(y, MIN_FLOAT) + em_p, y0) for y0 in PrevStatus[y]])
-            V[t][y] = prob
-            newpath[y] = path[state] + [y]
-        path = newpath
-    
-            for y in obs_states:
-            prob, state = max((V[t - 1][y0] + trans_p[y0].get(y, MIN_INF) +
-                               emit_p[y].get(obs[t], MIN_FLOAT), y0) for y0 in prev_states)
-            V[t][y] = prob
-            mem_path[t][y] = state
-    
-    log_f = open('1.log','w')
-log_f.write(' / '.join(map(str, words)))
+        for word in wordList:
+        pattern = getWordPattern(word)
