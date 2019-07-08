c981d8584ce9a468e32c5b456c83368cf3f1adb7
SUNsung <weatherius@gmail.com>
2019-07-08 21:28:08 +0300

    Rewrite [289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py] File in: 2019-07-08 21:28:07

--- pyton/289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py
+++ pyton/289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py
@@ -1,38 +1,104 @@
 
         
-            Validates that the username is not already taken. Hashes the
-    password for security.
-    '''
+        
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    '''Log in a registered user by adding the user id to the session.'''
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
         error = None
-    
-        with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-    
-        # login request set the user_id in the session
-    # check that the user is loaded from the session
-    with client:
-        client.get('/')
-        assert session['user_id'] == 1
-        assert g.user['username'] == 'test'
-    
-            if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)',
-                (title, body, g.user['id']),
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
     
     
-class TagTuple(JSONTag):
-    __slots__ = ()
-    key = ' t'
+@pytest.fixture
+def app():
+    '''Create and configure a new app instance for each test.'''
+    # create a temporary file to isolate the database for each test
+    db_fd, db_path = tempfile.mkstemp()
+    # create the app with common test config
+    app = create_app({'TESTING': True, 'DATABASE': db_path})
     
-        Implements cookie based sessions based on itsdangerous.
+            if not title:
+            error = 'Title is required.'
+    
+    
+class Blueprint(_PackageBoundObject):
+    '''Represents a blueprint, a collection of routes and other
+    app-related functions that can be registered on a real application
+    later.
+    
+    
+def finalize_headers(headers):
+    final_headers = {}
+    for name, value in headers.items():
+        if value is not None:
+    
+    from httpie.compat import is_windows
+from httpie.config import DEFAULT_CONFIG_DIR, Config
+    
+        '''
+    return path.replace('\\', '\\\\\\')
+    
+    
+@mock.patch('httpie.input.AuthCredentials._getpass',
+            new=lambda self, prompt: 'password')
+def test_password_prompt(httpbin):
+    r = http('--auth', 'user',
+             'GET', httpbin.url + '/basic-auth/user/password')
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
+    
+    
+def test_unicode_json_item(httpbin):
+    r = http('--json', 'POST', httpbin.url + '/post', u'test=%s' % UNICODE)
+    assert HTTP_OK in r
+    assert r.json['json'] == {'test': UNICODE}
+    
+        @property
+    def path(self):
+        '''Return the config file path creating basedir, if needed.'''
+        path = self._get_path()
+        try:
+            os.makedirs(os.path.dirname(path), mode=0o700)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+        return path
+    
+        '''
+    
+        @classmethod
+    # pylint: disable=arguments-differ,too-many-arguments
+    def sign(cls, payload, key, alg, nonce, url=None, kid=None):
+        # Per ACME spec, jwk and kid are mutually exclusive, so only include a
+        # jwk field if kid is not provided.
+        include_jwk = kid is None
+        return super(JWS, cls).sign(payload, key=key, alg=alg,
+                                    protect=frozenset(['nonce', 'url', 'kid', 'jwk', 'alg']),
+                                    nonce=nonce, url=url, kid=kid,
+                                    include_jwk=include_jwk)
+
+    
+    from acme import test_util
+    
+    # If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
+    
+    AUGEAS_LENS_DIR = pkg_resources.resource_filename(
+    'certbot_apache', 'augeas_lens')
+'''Path to the Augeas lens directory'''
+    
+       certbot certonly \\
+     --dns-cloudxns \\
+     --dns-cloudxns-credentials ~/.secrets/certbot/cloudxns.ini \\
+     -d example.com
+    
+        print('Done!')
+    if failures:
+        print('Some downloads have failed:')
+        for fail in failures:
+            print('> ' + fail)
