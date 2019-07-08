53a439cf397ea117ccd59899c2cc6f9439d98c66
SUNsung <weatherius@gmail.com>
2019-07-08 17:36:49 +0300

    Rewrite [55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb] File in: 2019-07-08 17:36:49

--- ruby/55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb
+++ ruby/55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb
@@ -1,62 +1,87 @@
 
         
-                # This method will split the argv given into three parts: the
-        # flags to this command, the subcommand, and the flags to the
-        # subcommand. For example:
-        #
-        #     -v status -h -v
-        #
-        # The above would yield 3 parts:
-        #
-        #     ['-v']
-        #     'status'
-        #     ['-h', '-v']
-        #
-        # These parts are useful because the first is a list of arguments
-        # given to the current command, the second is a subcommand, and the
-        # third are the commands given to the subcommand.
-        #
-        # @return [Array] The three parts.
-        def split_main_and_subcommand(argv)
-          # Initialize return variables
-          main_args   = nil
-          sub_command = nil
-          sub_args    = []
+        describe 'CommonMark' do
+  it 'passes spec' do
     
-            # Initializes the communicator with the machine that we will be
-        # communicating with. This base method does nothing (it doesn't
-        # even store the machine in an instance variable for you), so you're
-        # expected to override this and do something with the machine if
-        # you care about it.
-        #
-        # @param [Machine] machine The machine this instance is expected to
-        #   communicate with.
-        def initialize(machine)
-        end
+        Logster.add_to_env(request.env, 'CSP Report', report)
+    Rails.logger.warn('CSP Violation: '#{report['blocked-uri']}'')
     
-            # Called to change the hostname of the virtual machine.
-        def change_host_name(name)
-          raise BaseError, _key: :unsupported_host_name
-        end
+        it 'rejects invalid directives and ones that are not allowed to be extended' do
+      builder << {
+        invalid_src: ['invalid'],
+      }
+    
+        # if you need to test this and are having ssl issues see:
+    #  http://stackoverflow.com/questions/6756460/openssl-error-using-omniauth-specified-ssl-path-but-didnt-work
+    # OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
+    @omniauth = OmniAuth::Builder.new(app) do
+      Discourse.authenticators.each do |authenticator|
+        authenticator.register_middleware(self)
       end
     end
+    
+      def self.languages
+    langs = Dir.glob(HIGHLIGHTJS_DIR + 'languages/*.js').map do |path|
+      File.basename(path)[0..-8]
+    end
+    
+        def base_url
+      @base_url || Discourse.base_url
+    end
+    attr_writer :base_url
   end
-end
-
     
-            # Returns the internal data associated with this plugin. This
-        # should NOT be called by the general public.
-        #
-        # @return [Hash]
-        def self.data
-          @data ||= {}
+          Theme.where(id: Theme.transform_ids(theme_ids)).find_each do |theme|
+        theme.cached_settings.each do |setting, value|
+          extensions << build_theme_extension(value) if setting.to_s == THEME_SETTING
         end
-    
-        it 'should allow to adjust the delay' do
-      @session.accept_alert wait: 10 do
-        @session.click_link('Open slow alert')
       end
-      expect(@session).to have_xpath('//a[@id='open-slow-alert' and @opened='true']')
+    
+                  subscriber = Lograge::LogSubscribers::ActionController.new
+              payload = ActiveSupport::HashWithIndifferentAccess.new(
+                controller: self.class.name,
+                action: action_name,
+                params: request.filtered_parameters,
+                headers: request.headers,
+                format: request.format.ref,
+                method: request.request_method,
+                path: request.fullpath,
+                view_runtime: view_runtime * 1000.0,
+                db_runtime: db_runtime * 1000.0,
+                timings: timings,
+                status: response.status
+              )
+    
+          # Returns the keyword of the `case` statement as a string.
+      #
+      # @return [String] the keyword of the `case` statement
+      def keyword
+        'case'
+      end
+    
+          # The receiver of the method definition, if any.
+      #
+      # @return [Node, nil] the receiver of the method definition, or `nil`.
+      def receiver
+        node_parts[3]
+      end
+    
+          # Returns the value of this `hash` element.
+      #
+      # @note For keyword splats, this returns the whole node
+      #
+      # @return [Node] the value of the hash element
+      def value
+        node_parts[1]
+      end
+    
+            def swap_range(corrector, range1, range2)
+          src1 = range1.source
+          src2 = range2.source
+          corrector.replace(range1, src2)
+          corrector.replace(range2, src1)
+        end
+      end
     end
   end
 end
