da6eb45fb7fd4df993b6bb4282ff19eada01e0b9
SUNsung <weatherius@gmail.com>
2019-07-10 00:25:59 +0300

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2019-07-10 00:25:59

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,109 +1,195 @@
 
         
-        log_file = os.path.join(data_path, 'ipv6_tunnel.log')
+            def _colision_resolution(self, key, data=None):
+        new_key = self.hash_function(key + 1)
     
-    ##
-# imaginary tree navigation type; traverse 'get child' link
-DOWN = 2
-##
-#imaginary tree navigation type; finish with a child list
-UP = 3
+            if self.depth == 1:
+            self.prediction = np.mean(y)
+            return
     
+        difference = predict - actual
+    numerator = np.sum(difference) / len(predict) 
+    denumerator =  np.sum(actual) / len(predict)
+    print(numerator)
+    print(denumerator)
     
-class ParserRuleReturnScope(RuleReturnScope):
-    '''
-    Rules that return more than a single value must return an object
-    containing all the values.  Besides the properties defined in
-    RuleLabelScope.predefinedRulePropertiesScope there may be user-defined
-    return values.  This class simply defines the minimum properties that
-    are always defined and methods to access the others that might be
-    available depending on output option such as template and tree.
-    
-            if args:
-            if 'gui' in conf and conf['gui']:
-                # Enter GUI mode.
-                from .gui import gui_main
-                gui_main(*args, **conf)
-            else:
-                # Enter console mode.
-                from .console import console_main
-                console_main(*args, **conf)
-    
-    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
-    host = r1(r'host=([^&]*)', html)
-    assert host
-    type = r1(r'videofiletype=([^&]*)', html)
-    assert type
-    vid = r1(r'&stream_name=([^&]*)', html)
-    assert vid
-    dir_str = r1(r'&dir=([^&]*)', html).strip()
-    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
-    _, ext, size = url_info(url)
-    print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge = merge)
-    
-            link_list = self.get_streams_by_id(account_number, video_id)
-    
-    #----------------------------------------------------------------------
-def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    '''wrapper'''
-    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
-    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
-    hostname = urlparse(url).hostname
-    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
-        return False
-    upid = match1(url, r'.+/content/(\w+)')
-    
-    # looks that flickr won't return urls for all sizes
-# we required in 'extras field without a acceptable header
-dummy_header = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
-}
-def get_content_headered(url):
-    return get_content(url, dummy_header)
-    
-    __all__ = ['giphy_download']
-    
-    
-def huomaotv_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    room_id_pattern = r'huomao.com/(\d+)'
-    room_id = match1(url, room_id_pattern)
-    html = get_content(get_mobile_room_url(room_id))
-    
-    def ifeng_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-# old pattern /uuid.shtml
-# now it could be #uuid
-    id = r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', url)
-    if id:
-        return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
-    
-        def test_default_decoder_raises_deserialization_error(self):
-        from acme.fields import RFC3339Field
-        self.assertRaises(
-            jose.DeserializationError, RFC3339Field.default_decoder, '')
-    
-        @classmethod
-    # pylint: disable=arguments-differ,too-many-arguments
-    def sign(cls, payload, key, alg, nonce, url=None, kid=None):
-        # Per ACME spec, jwk and kid are mutually exclusive, so only include a
-        # jwk field if kid is not provided.
-        include_jwk = kid is None
-        return super(JWS, cls).sign(payload, key=key, alg=alg,
-                                    protect=frozenset(['nonce', 'url', 'kid', 'jwk', 'alg']),
-                                    nonce=nonce, url=url, kid=kid,
-                                    include_jwk=include_jwk)
+    if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
     
-        # Additional stuff for the LaTeX preamble.
-    #'preamble': '',
+    if __name__ == '__main__':
+    main()
+
+    
+            # builtin classmethod:
+        self.assertEqual(dict.fromkeys.__qualname__, 'dict.fromkeys')
+        self.assertEqual(float.__getformat__.__qualname__,
+                         'float.__getformat__')
+    
+    REVERSE_NAME_MAPPING.update({
+    ('_functools', 'reduce'): ('__builtin__', 'reduce'),
+    ('tkinter.filedialog', 'FileDialog'): ('FileDialog', 'FileDialog'),
+    ('tkinter.filedialog', 'LoadFileDialog'): ('FileDialog', 'LoadFileDialog'),
+    ('tkinter.filedialog', 'SaveFileDialog'): ('FileDialog', 'SaveFileDialog'),
+    ('tkinter.simpledialog', 'SimpleDialog'): ('SimpleDialog', 'SimpleDialog'),
+    ('xmlrpc.server', 'ServerHTMLDoc'): ('DocXMLRPCServer', 'ServerHTMLDoc'),
+    ('xmlrpc.server', 'XMLRPCDocGenerator'):
+        ('DocXMLRPCServer', 'XMLRPCDocGenerator'),
+    ('xmlrpc.server', 'DocXMLRPCRequestHandler'):
+        ('DocXMLRPCServer', 'DocXMLRPCRequestHandler'),
+    ('xmlrpc.server', 'DocXMLRPCServer'):
+        ('DocXMLRPCServer', 'DocXMLRPCServer'),
+    ('xmlrpc.server', 'DocCGIXMLRPCRequestHandler'):
+        ('DocXMLRPCServer', 'DocCGIXMLRPCRequestHandler'),
+    ('http.server', 'SimpleHTTPRequestHandler'):
+        ('SimpleHTTPServer', 'SimpleHTTPRequestHandler'),
+    ('http.server', 'CGIHTTPRequestHandler'):
+        ('CGIHTTPServer', 'CGIHTTPRequestHandler'),
+    ('_socket', 'socket'): ('socket', '_socketobject'),
+})
+    
+            self._copy_file_without_generated_symbols(SYMBOL_FILE, output)
     
     
+def open_binary(package: Package, resource: Resource) -> BinaryIO:
+    '''Return a file-like object opened for binary reading of the resource.'''
+    resource = _normalize_path(resource)
+    package = _get_package(package)
+    reader = _get_resource_reader(package)
+    if reader is not None:
+        return reader.open_resource(resource)
+    _check_location(package)
+    absolute_package_path = os.path.abspath(package.__spec__.origin)
+    package_path = os.path.dirname(absolute_package_path)
+    full_path = os.path.join(package_path, resource)
+    try:
+        return open(full_path, mode='rb')
+    except OSError:
+        # Just assume the loader is a resource loader; all the relevant
+        # importlib.machinery loaders are and an AttributeError for
+        # get_data() will make it clear what is needed from the loader.
+        loader = cast(ResourceLoader, package.__spec__.loader)
+        data = None
+        if hasattr(package.__spec__.loader, 'get_data'):
+            with suppress(OSError):
+                data = loader.get_data(full_path)
+        if data is None:
+            package_name = package.__spec__.name
+            message = '{!r} resource not found in {!r}'.format(
+                resource, package_name)
+            raise FileNotFoundError(message)
+        else:
+            return BytesIO(data)
     
     
-def __meters_to_miles(meters: float) -> float:
-    '''Convert meters to miles.'''
-    return meters * 0.000621371
+class DBUnpickler(pickle.Unpickler):
+    
+    # Add the html version.  This converts the message into a multipart/alternative
+# container, with the original text message as the first part and the new html
+# message as the second part.
+asparagus_cid = make_msgid()
+msg.add_alternative('''\
+<html>
+  <head></head>
+  <body>
+    <p>Salut!</p>
+    <p>Cela ressemble à un excellent
+        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
+            recipie
+        </a> déjeuner.
+    </p>
+    <img src='cid:{asparagus_cid}' />
+  </body>
+</html>
+'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
+# note that we needed to peel the <> off the msgid for use in the html.
+    
+    from argparse import ArgumentParser
+    
+    '''Unpack a MIME message into a directory of files.'''
+    
+    def handleSlideTitle(title):
+    print('<h2>%s</h2>' % getText(title.childNodes))
+    
+    # Function to return the operator module
+def get_operator_module():
+    return operator
+    
+            try:
+            print(list(pool.imap(f, list(range(10)))))
+        except ZeroDivisionError:
+            print('\tGot ZeroDivisionError as expected from list(pool.imap())')
+        else:
+            raise AssertionError('expected ZeroDivisionError')
+    
+            # Adding index on 'EventTag', fields ['environment_id', 'key', 'value']
+        db.create_index(u'tagstore_eventtag', ['environment_id', 'key_id', 'value_id'])
+    
+            # Adding unique constraint on 'EventTag', fields ['event_id', 'key', 'value']
+        db.create_unique(u'tagstore_eventtag', ['event_id', 'key_id', 'value_id'])
+    
+        class Meta:
+        app_label = 'tagstore'
+        unique_together = (('project_id', 'event_id', 'key', 'value'), )
+        index_together = (
+            ('project_id', 'key', 'value'),
+            ('group_id', 'key', 'value'),
+        )
+    
+        for resolution in resolution_list:
+        try:
+            activity = Activity.objects.filter(
+                group=resolution.group_id,
+                type=Activity.SET_RESOLVED_IN_RELEASE,
+                ident=resolution.id,
+            ).order_by('-datetime')[0]
+        except IndexError:
+            continue
+    
+        try:
+        with lock.acquire():
+            buffer.process_pending(partition=partition)
+    except UnableToAcquireLock as error:
+        logger.warning('process_pending.fail', extra={'error': error, 'partition': partition})
+    
+        def proxy_callable(self, call_name, *args, **kwargs):
+        self.log.info('%s %s <- %s',
+                      self.obj_name,
+                      call_name,
+                      format_call(args, kwargs))
+    
+        def test_single_path_from_env(self):
+        with mock.patch.dict(os.environ):
+            os.environ['COMPOSE_FILE'] = 'one.yml'
+            environment = Environment.from_env_file('.')
+            assert get_config_path_from_options('.', {}, environment) == ['one.yml']
+    
+    
+class UnquotePathTest(unittest.TestCase):
+    def test_no_quotes(self):
+        assert unquote_path('hello') == 'hello'
+    
+    
+def test_minute_as_float():
+    assert timeparse.timeparse('1.5m') == 90
+    
+    if __name__ == '__main__':
+    setup(name='wtfpython',
+          version='0.2',
+          description='What the f*ck Python!',
+          author='Satwik Kansal',
+          maintainer='Satwik Kansal',
+          maintainer_email='satwikkansal@gmail.com',
+          url='https://github.com/satwikkansal/wtfpython',
+          platforms='any',
+          license='WTFPL 2.0',
+          long_description='An interesting collection of subtle & tricky Python Snippets'
+                           ' and features.',
+          keywords='wtfpython gotchas snippets tricky',
+          packages=find_packages(),
+          entry_points = {
+              'console_scripts': ['wtfpython = wtf_python.main:load_and_read']
+          },
+          classifiers=[
+              'Development Status :: 4 - Beta',
