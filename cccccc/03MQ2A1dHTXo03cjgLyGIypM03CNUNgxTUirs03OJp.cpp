1bff4edda0f9f7cc8d6de014fd8edde111eb4e14
SUNsung <weatherius@gmail.com>
2019-07-09 07:05:12 +0300

    Rewrite [03MQ2A1dHTXo03cjgLyGIypM03CNUNgxTUirs03OJp.cpp] File in: 2019-07-09 07:05:11

--- cccccc/03MQ2A1dHTXo03cjgLyGIypM03CNUNgxTUirs03OJp.cpp
+++ cccccc/03MQ2A1dHTXo03cjgLyGIypM03CNUNgxTUirs03OJp.cpp
@@ -1,416 +1,110 @@
 
         
-        void DiverseStackBase::pushNewStorageSlow(std::size_t needed) {
-  bool wasInline = isAllocatedInline();
-    }
+        const Status Status::OK = Status();
+const Status Status::CANCELLED = Status(error::CANCELLED, '');
+const Status Status::UNKNOWN = Status(error::UNKNOWN, '');
     
-    #ifndef PREPOSITION
-#  define PREPOSITION(Word)
-#endif
+    // Copyright 2008 Google Inc. All Rights Reserved.
+// Author: xpeng@google.com (Peter Peng)
     
-    class TreePrinter {
-  llvm::raw_ostream &Out;
-  void (&PrintNodeData)(llvm::raw_ostream &out, void *node);
-  SmallString<40> Indent;
-public:
-  TreePrinter(llvm::raw_ostream &out,
-              void (&printNodeData)(llvm::raw_ostream &out, void *node))
-    : Out(out), PrintNodeData(printNodeData) {}
-    }
+    #include <google/protobuf/io/gzip_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
     
-    bool Mangle::needsPunycodeEncoding(StringRef str) {
-  for (unsigned char c : str) {
-    if (!isValidSymbolChar(c))
-      return true;
+    // Author: brianolson@google.com (Brian Olson)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+//
+// Test program to verify that GzipOutputStream is compatible with command line
+// gzip or java.util.zip.GzipOutputStream
+//
+// Reads data on standard input and writes compressed gzip stream to standard
+// output.
+    
+      static const APCCollection* fromHandle(const APCHandle* handle) {
+    assertx(handle->checkInvariants());
+    assertx(handle->kind() == APCKind::SharedCollection);
+    static_assert(offsetof(APCCollection, m_handle) == 0, '');
+    return reinterpret_cast<const APCCollection*>(handle);
   }
-  return false;
-}
     
-    /// Reencode well-formed UTF-8 as UTF-32.
-///
-/// This entry point is only called from compiler-internal entry points, so does
-/// only minimal validation. In particular, it does *not* check for overlong
-/// encodings.
-/// If \p mapNonSymbolChars is true, non-symbol ASCII characters (characters
-/// except [$_a-zA-Z0-9]) are also encoded like non-ASCII unicode characters.
-/// Returns false if \p InputUTF8 contains surrogate code points.
-static bool convertUTF8toUTF32(llvm::StringRef InputUTF8,
-                               std::vector<uint32_t> &OutUTF32,
-                               bool mapNonSymbolChars) {
-  auto ptr = InputUTF8.begin();
-  auto end = InputUTF8.end();
-  while (ptr < end) {
-    uint8_t first = *ptr++;
-    if (first < 0x80) {
-      if (Mangle::isValidSymbolChar(first) || !mapNonSymbolChars) {
-        OutUTF32.push_back(first);
-      } else {
-        OutUTF32.push_back((uint32_t)first + 0xD800);
+    #include 'hphp/runtime/base/stream-wrapper.h'
+#include 'hphp/runtime/base/runtime-error.h'
+    
+      bool valid() const { return !isClosed(); }
+    
+    void record_perf_mem_event(PerfEvent kind, const perf_event_sample* sample);
+    
+    #include 'hphp/util/struct-log.h'
+    
+    
+    
+    #ifndef incl_HPHP_URL_H_
+#define incl_HPHP_URL_H_
+    
+    
+    {  size_t operator()(const copy_ptr<Map>& m) const {
+    auto ret = m->size();
+    for (auto& kv : *m) {
+      ret = folly::hash::hash_combine(ret, kv.first.get());
+      ret = folly::hash::hash_combine(ret,
+                                      static_cast<int64_t>(kv.second.m_type));
+      if (!isNullType(kv.second.m_type)) {
+        ret = folly::hash::hash_combine(ret, kv.second.m_data.num);
       }
-    } else if (first < 0xC0) {
-      // Invalid continuation byte.
-      return false;
-    } else if (first < 0xE0) {
-      // Two-byte sequence.
-      if (ptr == end)
-        return false;
-      uint8_t second = *ptr++;
-      if (!isContinuationByte(second))
-        return false;
-      OutUTF32.push_back(((first & 0x1F) << 6) | (second & 0x3F));
-    } else if (first < 0xF0) {
-      // Three-byte sequence.
-      if (end - ptr < 2)
-        return false;
-      uint8_t second = *ptr++;
-      uint8_t third = *ptr++;
-      if (!isContinuationByte(second) || !isContinuationByte(third))
-        return false;
-      OutUTF32.push_back(((first & 0xF) << 12) | ((second & 0x3F) << 6)
-                         | ( third  & 0x3F      ));
-    } else if (first < 0xF8) {
-      // Four-byte sequence.
-      if (end - ptr < 3)
-        return false;
-      uint8_t second = *ptr++;
-      uint8_t third = *ptr++;
-      uint8_t fourth = *ptr++;
-      if (!isContinuationByte(second) || !isContinuationByte(third)
-          || !isContinuationByte(fourth))
-        return false;
-      OutUTF32.push_back(((first & 0x7) << 18) | ((second & 0x3F) << 12)
-                         | ((third  & 0x3F) <<  6)
-                         | ( fourth & 0x3F       ));
-    } else {
-      // Unused sequence length.
-      return false;
     }
+    return ret;
   }
-  return true;
-}
-    
-      if (!Files.empty() && !hasAnyPrimaryFiles) {
-    Optional<std::vector<std::string>> userSuppliedNamesOrErr =
-        OutputFilesComputer::getOutputFilenamesFromCommandLineOrFilelist(Args,
-                                                                         Diags);
-    if (userSuppliedNamesOrErr && userSuppliedNamesOrErr->size() == 1)
-      result.setIsSingleThreadedWMO(true);
-  }
-    
-    namespace swift {
-namespace irgen {
-    }
-    }
-    
-    #if GTEST_HAS_GLOBAL_WSTRING
-  // Converts the given wide string to a narrow string using the UTF-8
-  // encoding, and streams the result to this Message object.
-  Message& operator <<(const ::wstring& wstr);
-#endif  // GTEST_HAS_GLOBAL_WSTRING
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5>
-void PrintTo(const ::std::tr1::tuple<T1, T2, T3, T4, T5>& t,
-             ::std::ostream* os) {
-  PrintTupleTo(t, os);
-}
-    
-    
-    {  const T1 v1_;
-  const T2 v2_;
-  const T3 v3_;
-  const T4 v4_;
-  const T5 v5_;
-  const T6 v6_;
-  const T7 v7_;
-  const T8 v8_;
-  const T9 v9_;
-  const T10 v10_;
-  const T11 v11_;
-  const T12 v12_;
-  const T13 v13_;
-  const T14 v14_;
-  const T15 v15_;
-  const T16 v16_;
-  const T17 v17_;
-  const T18 v18_;
-  const T19 v19_;
-  const T20 v20_;
-  const T21 v21_;
-  const T22 v22_;
-  const T23 v23_;
-  const T24 v24_;
-  const T25 v25_;
-  const T26 v26_;
-  const T27 v27_;
-  const T28 v28_;
-  const T29 v29_;
-  const T30 v30_;
-  const T31 v31_;
-  const T32 v32_;
-  const T33 v33_;
-  const T34 v34_;
-  const T35 v35_;
-  const T36 v36_;
-  const T37 v37_;
-  const T38 v38_;
-  const T39 v39_;
-  const T40 v40_;
-  const T41 v41_;
-  const T42 v42_;
-  const T43 v43_;
 };
     
-    #include <limits.h>
-#include 'sample1.h'
-#include 'gtest/gtest.h'
+        // writing access
     
-    
-#endif  // GTEST_SAMPLES_SAMPLE2_H_
-
-    
-    
-    {
-    {    private:
-        int32_t m_sign;
-        int32_t m_exp;
-        std::vector<uint32_t> m_mantissa;
-    };
-}
-
-    
-        struct HISTORYITEMVECTOR
-    {
-        std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> spTokens;
-        std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> spCommands;
-        std::wstring expression;
-        std::wstring result;
-    };
-    
-            private:
-            // ICollectionView
-            // Not implemented methods
-            virtual WF::IAsyncOperation<
-                Windows::UI::Xaml::Data::LoadMoreItemsResult> ^ LoadMoreItemsAsync(unsigned int) = Windows::UI::Xaml::Data::ICollectionView::LoadMoreItemsAsync
-            {
-                throw ref new Platform::NotImplementedException();
-            }
-            virtual bool MoveCurrentToFirst() = Windows::UI::Xaml::Data::ICollectionView::MoveCurrentToFirst
-            {
-                throw ref new Platform::NotImplementedException();
-            }
-            virtual bool MoveCurrentToLast() = Windows::UI::Xaml::Data::ICollectionView::MoveCurrentToLast
-            {
-                throw ref new Platform::NotImplementedException();
-            }
-            virtual bool MoveCurrentToNext() = Windows::UI::Xaml::Data::ICollectionView::MoveCurrentToNext
-            {
-                throw ref new Platform::NotImplementedException();
-            }
-            virtual bool MoveCurrentToPrevious() = Windows::UI::Xaml::Data::ICollectionView::MoveCurrentToPrevious
-            {
-                throw ref new Platform::NotImplementedException();
-            }
-            property Windows::Foundation::Collections::IObservableVector<Platform::Object ^> ^ CollectionGroups {
-                virtual Windows::Foundation::Collections::IObservableVector<
-                    Platform::Object ^> ^ get() = Windows::UI::Xaml::Data::ICollectionView::CollectionGroups::get
-                {
-                    return ref new Platform::Collections::Vector<Platform::Object ^>();
-                }
-            } property bool HasMoreItems
-            {
-                virtual bool get() = Windows::UI::Xaml::Data::ICollectionView::HasMoreItems::get
-                {
-                    return false;
-                }
-            }
-    
-    bool LiveRegionHost::IsHostAvailable()
+    int main()
 {
-    // LiveRegion is always available.
-    return true;
+    // create a JSON value
+    const json j =
+    {
+        {'number', 1}, {'string', 'foo'}, {'array', {1, 2}}
+    };
+    }
+    
+    TEST_F(PacksTests, test_discovery_zero_state) {
+  Pack pack('discovery_pack', getPackWithDiscovery().doc());
+  auto stats = pack.getStats();
+  EXPECT_EQ(stats.total, 0U);
+  EXPECT_EQ(stats.hits, 0U);
+  EXPECT_EQ(stats.misses, 0U);
 }
     
-            static NarratorAnnouncement ^ GetCategoryNameChangedAnnouncement(Platform::String ^ announcement);
-    
-        private:
-        static void OnAnnouncementChanged(
-            _In_ Windows::UI::Xaml::DependencyObject ^ dependencyObject,
-            _In_ Windows::UI::Xaml::DependencyPropertyChangedEventArgs ^ eventArgs);
-    
-    
-    {
-    {
-    {  inline void PutChar(char ch) {
-    out_buf += ch;
-    if (out_buf.length() >= kBufferSize) Flush();
-  }
-  inline void Flush(void) {
-    if (out_buf.length() != 0) {
-      fp->Write(&out_buf[0], out_buf.length());
-      out_buf.clear();
-    }
-  }
-};
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_BASE64_H_
-
-    
-    #if DMLC_ENABLE_STD_THREAD
-namespace xgboost {
-namespace data {
-    }
+    namespace osquery {
     }
     
-    // Finally register the objective function.
-// After it succeeds you can try use xgboost with objective=mylogistic
-XGBOOST_REGISTER_OBJECTIVE(MyLogistic, 'mylogistic')
-.describe('User defined logistic regression plugin')
-.set_body([]() { return new MyLogistic(); });
+    static const int kMaxLogMessageBufferSize = 65536;
     
+    void RocksdbMigration::buildMigrationMap() {
+  // No migration map at this stage, will be added as separate commit
+}
     
-    {
-    {      if (size_to_read != 0) {
-        CHECK_EQ(fi->Read(dmlc::BeginPtr(data_vec) + offset_vec[i],
-                          size_to_read * sizeof(Entry)),
-                 size_to_read * sizeof(Entry))
-            << 'Invalid SparsePage file';
-        curr_offset += size_to_read;
-      }
-      i = j;
-    }
-    // seek to end of record
-    if (curr_offset != disk_offset_.back()) {
-      fi->Seek(begin + disk_offset_.back() * sizeof(Entry));
-    }
-    return true;
-  }
-    
-    
-    {
-    {    for (const auto &batch : (*dmat)->GetRowBatches()) {
-      for (int i = 0; i < batch.Size(); i++) {
-        auto inst = batch[i];
-        for (int j = 0; i < inst.size(); i++) {
-          ASSERT_EQ(inst[j].fvalue, 1.5);
-        }
-      }
-    }
-    delete dmat;
+    void Initializer::initWorkerWatcher(const std::string& name) const {
+  if (isWorker()) {
+    initWorker(name);
+  } else {
+    // The watcher will forever monitor and spawn additional workers.
+    // This initialize will handle work for processes without watchdogs too.
+    initWatcher();
   }
 }
-
     
-    TEST(MetaInfo, GetSet) {
-  xgboost::MetaInfo info;
-    }
-    
-      const std::string tmp_binfile = tempdir.path + '/csr_source.binary';
-  dmat->SaveToLocalFile(tmp_binfile);
-  xgboost::DMatrix * dmat_read = xgboost::DMatrix::Load(tmp_binfile, true, false);
-    
-      std::string LoadConfigFile(const std::string& path) {
-    std::ifstream fin(path, std::ios_base::in | std::ios_base::binary);
-    CHECK(fin) << 'Failed to open: ' << path;
-    std::string content{std::istreambuf_iterator<char>(fin),
-                        std::istreambuf_iterator<char>()};
-    return content;
-  }
-    
-      // 2. Key-value pairs
-  ASSERT_TRUE(parser.ParseKeyValuePair('booster = gbtree', &key, &value));
-  ASSERT_EQ(key, 'booster');
-  ASSERT_EQ(value, 'gbtree');
-  ASSERT_TRUE(parser.ParseKeyValuePair('n_gpus = 2', &key, &value));
-  ASSERT_EQ(key, 'n_gpus');
-  ASSERT_EQ(value, '2');
-  ASSERT_TRUE(parser.ParseKeyValuePair('monotone_constraints = (1,0,-1)',
-                                       &key, &value));
-  ASSERT_EQ(key, 'monotone_constraints');
-  ASSERT_EQ(value, '(1,0,-1)');
-  // whitespace should not matter
-  ASSERT_TRUE(parser.ParseKeyValuePair('  objective=binary:logistic',
-                                       &key, &value));
-  ASSERT_EQ(key, 'objective');
-  ASSERT_EQ(value, 'binary:logistic');
-  ASSERT_TRUE(parser.ParseKeyValuePair('tree_method\t=\thist  ', &key, &value));
-  ASSERT_EQ(key, 'tree_method');
-  ASSERT_EQ(value, 'hist');
-    
-    /*!
- * \brief Input stream that support additional PeekRead
- *  operation, besides read.
+    /**
+ * @brief An internal severity set mapping to Glog's LogSeverity levels.
  */
-class PeekableInStream : public dmlc::Stream {
- public:
-  explicit PeekableInStream(dmlc::Stream* strm)
-      : strm_(strm), buffer_ptr_(0) {}
-    }
+enum StatusLogSeverity {
+  O_INFO = 0,
+  O_WARNING = 1,
+  O_ERROR = 2,
+  O_FATAL = 3,
+};
     
-    namespace apollo {
-namespace drivers {
-namespace conti_radar {
-    }
-    }
-    }
+    #include 'plugin.h'
     
-      MatrixXd mat_golden(10, 10);
-  // clang-format off
-  mat_golden <<
-      6, -4,  1,  0,  0,  0,  0,  0,  0,  0,
-     -4,  6, -4,  1,  0,  0,  0,  0,  0,  0,
-      1, -4,  6, -4,  1,  0,  0,  0,  0,  0,
-      0,  1, -4,  6, -4,  1,  0,  0,  0,  0,
-      0,  0,  1, -4,  6, -4,  1,  0,  0,  0,
-      0,  0,  0,  1, -4,  6, -4,  1,  0,  0,
-      0,  0,  0,  0,  1, -4,  6, -4,  1,  0,
-      0,  0,  0,  0,  0,  1, -4,  6, -4,  1,
-      0,  0,  0,  0,  0,  0,  1, -4,  5, -2,
-      0,  0,  0,  0,  0,  0,  0,  1, -2,  1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-    Spline1dSeg::Spline1dSeg(const uint32_t order) {
-  SetSplineFunc(PolynomialXd(order));
-}
-    
-      } else if (type == 'second_order') {
-    for (uint32_t r = 2; r < num_params; ++r) {
-      for (uint32_t c = 2; c < num_params; ++c) {
-        (*term_matrix)(r, c) = x_pow[r + c - 3];
-      }
-    }
-    (*term_matrix).block(0, 0, num_params, 2) =
-        Eigen::MatrixXd::Zero(num_params, 2);
-    (*term_matrix).block(0, 0, 2, num_params) =
-        Eigen::MatrixXd::Zero(2, num_params);
-    
-    
-    {
-    {
-    {}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
-
-    
-    // config detail: {'name': 'commanded_value', 'offset': 0.0, 'precision': 0.001,
-// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 23,
-// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
-double Brakerpt6c::commanded_value(const std::uint8_t* bytes,
-                                   int32_t length) const {
-  Byte t0(bytes + 2);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-      Brakerpt6c brake;
-  brake.Parse(bytes, length, &chassis_detail);
-    
-    // config detail: {'name': 'commanded_value', 'enum': {0: 'COMMANDED_VALUE_OFF',
-// 1: 'COMMANDED_VALUE_ON'}, 'precision': 1.0, 'len': 8, 'is_signed_var': False,
-// 'offset': 0.0, 'physical_range': '[0|1]', 'bit': 15, 'type': 'enum', 'order':
-// 'motorola', 'physical_unit': ''}
-Horn_rpt_79::Commanded_valueType Hornrpt79::commanded_value(
-    const std::uint8_t* bytes, int32_t length) const {
-  Byte t0(bytes + 1);
-  int32_t x = t0.get_byte(0, 8);
-    }
+      /// Allow the plugin to introspect into the registered name (for logging).
+  virtual void setName(const std::string& name) final;
