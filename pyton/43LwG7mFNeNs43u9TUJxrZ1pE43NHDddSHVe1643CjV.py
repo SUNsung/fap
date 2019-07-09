0216f07d37b03977345925fdc9ee1638f6d5488a
SUNsung <weatherius@gmail.com>
2019-07-09 07:58:46 +0300

    Rewrite [43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py] File in: 2019-07-09 07:58:46

--- pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
+++ pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
@@ -1,22 +1,184 @@
 
         
-        fp.close()
-
-    
-    
-def ChineseAnalyzer(stoplist=STOP_WORDS, minsize=1, stemfn=stem, cachesize=50000):
-    return (ChineseTokenizer() | LowercaseFilter() |
-            StopFilter(stoplist=stoplist, minsize=minsize) |
-            StemFilter(stemfn=stemfn, ignore=None, cachesize=cachesize))
-
-    
-    parser = OptionParser(USAGE)
-parser.add_option('-k', dest='topK')
-opt, args = parser.parse_args()
-    
-    import jieba
-import time
-import glob
-import sys
+        import sys
 import os
-import random
+import textwrap
+    
+    
+# find the correct sorting and add the required base classes so that sublcasses
+# can be correctly created
+classes = _ALL_CLASSES[:-1]
+ordered_cls = []
+while classes:
+    for c in classes[:]:
+        bases = set(c.__bases__) - set((object, InfoExtractor, SearchInfoExtractor))
+        stop = False
+        for b in bases:
+            if b not in classes and b not in ordered_cls:
+                if b.__name__ == 'GenericIE':
+                    exit()
+                classes.insert(0, b)
+                stop = True
+        if stop:
+            break
+        if all(b in ordered_cls for b in bases):
+            ordered_cls.append(c)
+            classes.remove(c)
+            break
+ordered_cls.append(_ALL_CLASSES[-1])
+    
+    with io.open(README_FILE, encoding='utf-8') as f:
+    oldreadme = f.read()
+    
+    # -- Options for HTML output ----------------------------------------------
+    
+        def test_socks4(self):
+        self.assertTrue(isinstance(self._get_ip('socks4'), compat_str))
+    
+        def test_allsubtitles(self):
+        self.DL.params['writesubtitles'] = True
+        self.DL.params['allsubtitles'] = True
+        subtitles = self.getSubtitles()
+        self.assertEqual(set(subtitles.keys()), set(['nl']))
+        self.assertEqual(md5(subtitles['nl']), 'fc6435027572b63fb4ab143abd5ad3f4')
+    
+            monitoring_policy_process = oneandone.client.Process(
+            process=process['process'],
+            alert_if=process['alert_if'],
+            email_notification=process['email_notification']
+        )
+    
+    ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+    
+    #example for a DNAT
+- hosts: localhost
+  connection: local
+  tasks:
+   - vca_nat:
+       instance_id: 'b15ff1e5-1024-4f55-889f-ea0209726282'
+       vdc_name: 'benz_ansible'
+       state: 'present'
+       nat_rules:
+         - rule_type: DNAT
+           original_ip: 203.0.113.23
+           original_port: 22
+           translated_ip: 192.0.2.42
+           translated_port: 22
+    
+    
+DOCUMENTATION = '''
+---
+module: honeybadger_deployment
+author: 'Benjamin Curtis (@stympy)'
+version_added: '2.2'
+short_description: Notify Honeybadger.io about app deployments
+description:
+   - Notify Honeybadger.io about app deployments (see http://docs.honeybadger.io/article/188-deployment-tracking)
+options:
+  token:
+    description:
+      - API token.
+    required: true
+  environment:
+    description:
+      - The environment name, typically 'production', 'staging', etc.
+    required: true
+  user:
+    description:
+      - The username of the person doing the deployment
+  repo:
+    description:
+      - URL of the project repository
+  revision:
+    description:
+      - A hash, number, tag, or other identifier showing what revision was deployed
+  url:
+    description:
+      - Optional URL to submit the notification to.
+    default: 'https://api.honeybadger.io/v1/deploys'
+  validate_certs:
+    description:
+      - If C(no), SSL certificates for the target url will not be validated. This should only be used
+        on personally controlled sites using self-signed certificates.
+    type: bool
+    default: 'yes'
+    
+        @staticmethod
+    def _get_cert(commonname, isip=False, sans=None):
+        cert = OpenSSL.crypto.X509()
+        cert.set_version(2)
+        # setting the only serial number, the browser will refused fixed serial number when cert updated.
+        serial_number = int((int(time.time() - CertUtil.serial_reduce) + random.random()) * 100)
+        while 1:
+            try:
+                cert.set_serial_number(serial_number)
+            except OpenSSL.SSL.Error:
+                serial_number += 1
+            else:
+                break
+        subj = cert.get_subject()
+        subj.countryName = 'CN'
+        subj.stateOrProvinceName = 'Internet'
+        subj.localityName = 'Cernet'
+        subj.organizationalUnitName = '%s Branch' % CertUtil.ca_vendor
+        subj.commonName = commonname
+        subj.organizationName = commonname
+        cert.gmtime_adj_notBefore(-600) #avoid crt time error warning
+        cert.gmtime_adj_notAfter(CertUtil.cert_validity)
+        cert.set_issuer(CertUtil.ca_subject)
+        if CertUtil.cert_publickey:
+            pkey = CertUtil.cert_publickey
+        else:
+            pkey = OpenSSL.crypto.PKey()
+            pkey.generate_key(OpenSSL.crypto.TYPE_RSA, 2048)
+        cert.set_pubkey(pkey)
+    
+    current_path = os.path.dirname(os.path.abspath(__file__))
+root_path = os.path.abspath(os.path.join(current_path, os.pardir, os.pardir, os.pardir))
+data_path = os.path.abspath(os.path.join(root_path, os.pardir, os.pardir, 'data', 'gae_proxy'))
+if not os.path.isdir(data_path):
+    data_path = current_path
+    
+            if self.token is None:
+            return 'UnwantedTokenException(found=%s%s)' % (None, exp)
+    
+    
+    def getErrorMessage(self, e, tokenNames):
+        msg = None
+        
+        if isinstance(e, MismatchedTokenException):
+            msg = 'mismatched character ' \
+                  + self.getCharErrorDisplay(e.c) \
+                  + ' expecting ' \
+                  + self.getCharErrorDisplay(e.expecting)
+    
+    
+# 使用我们的训练数据集对线性单元进行训练
+def train_linear_unit():
+    '''
+    Desc:
+        使用训练数据集对我们的线性单元进行训练
+    Args:
+        None
+    Returns:
+        lu —— 返回训练好的线性单元
+    '''
+    # 创建感知器对象，输入参数的个数也就是特征数为 1（工作年限）
+    lu = LinearUnit(1)
+    # 获取构建的数据集
+    input_vecs, labels = get_training_dataset()
+    # 训练感知器，迭代 10 轮，学习率为 0.01
+    lu.train(input_vecs, labels, 10, 0.01)
+    # 返回训练好的线性单元
+    return lu
+    
+       # create a tuple with the values to be used by reducer
+   # and encode it with base64 to avoid potential trouble with '\t' and '\n' used
+   # as default separators in Hadoop Streaming
+   producedvalue = base64.b64encode(pickle.dumps((E.T*E, E.T*D*e)))
+    
+            self.chars += len(line) + 1  # +1 for newline
+        self.words += sum(1 for word in line.split() if word.strip())
+        self.lines += 1
