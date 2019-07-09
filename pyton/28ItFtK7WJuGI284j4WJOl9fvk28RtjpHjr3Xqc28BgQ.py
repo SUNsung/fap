23c75883618d4ed500fe8cb27e925c3eeab2f7e0
SUNsung <weatherius@gmail.com>
2019-07-09 14:46:34 +0300

    Rewrite [28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py] File in: 2019-07-09 14:46:33

--- pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
+++ pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
@@ -1,43 +1,190 @@
 
         
-            long_description = README,
+            # Test that writing over the input data works predictably
+    for mode in ['torch', 'tf']:
+        x = np.random.uniform(0, 255, (2, 10, 10, 3))
+        xint = x.astype('int')
+        x2 = utils.preprocess_input(x, mode=mode)
+        xint2 = utils.preprocess_input(xint)
+        assert_allclose(x, x2)
+        assert xint.astype('float').max() != xint2.max()
+    # Caffe mode works differently from the others
+    x = np.random.uniform(0, 255, (2, 10, 10, 3))
+    xint = x.astype('int')
+    x2 = utils.preprocess_input(x, data_format='channels_last', mode='caffe')
+    xint2 = utils.preprocess_input(xint)
+    assert_allclose(x, x2[..., ::-1])
+    assert xint.astype('float').max() != xint2.max()
     
-        html = get_content(url)
-    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
-    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
+        loaded_model = keras.models.load_model(model_filename)
+        loaded_model.predict(np.random.rand(128, 2))
     
-        def __init__(self):
-        super().__init__()
-        self.api_data = None
+    # Save the model weights.
+weight_path = os.path.join(tempfile.gettempdir(), 'saved_wt.h5')
+train_model.save_weights(weight_path)
     
-    	xml = get_html('http://www.ehow.com/services/video/series.xml?demand_ehow_videoid=%s' % vid)
+    print('Vectorizing sequence data...')
+tokenizer = Tokenizer(num_words=max_words)
+x_train = tokenizer.sequences_to_matrix(x_train, mode='binary')
+x_test = tokenizer.sequences_to_matrix(x_test, mode='binary')
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
     
-	from xml.dom.minidom import parseString
-	doc = parseString(xml)
-	tab = doc.getElementsByTagName('related')[0].firstChild
+                gc.collect()
+            print('- benchmarking Lasso')
+            clf = Lasso(alpha=alpha, fit_intercept=False,
+                        precompute=precompute)
+            tstart = time()
+            clf.fit(X, Y)
+            lasso_results.append(time() - tstart)
     
-    site_info = 'Giphy.com'
-download = giphy_download
-download_playlist = playlist_not_supported('giphy')
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
+plt.show()
 
     
-    def ifeng_download_by_id(id, title = None, output_dir = '.', merge = True, info_only = False):
-    assert r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', id), id
-    url = 'http://vxml.ifengimg.com/video_info_new/%s/%s/%s.xml' % (id[-2], id[-2:], id)
-    xml = get_html(url, 'utf-8')
-    title = r1(r'Name='([^']+)'', xml)
-    title = unescape_html(title)
-    url = r1(r'VideoPlayUrl='([^']+)'', xml)
-    from random import randint
-    r = randint(10, 19)
-    url = url.replace('http://wideo.ifeng.com/', 'http://ips.ifeng.com/wideo.ifeng.com/')
-    type, ext, size = url_info(url)
+        # start time
+    tstart = datetime.now()
+    clf = DecisionTreeRegressor()
+    clf.fit(X, Y).predict(X)
+    delta = (datetime.now() - tstart)
+    # stop time
     
-        def extract(self, **kwargs):
-        if 'stream_id' in kwargs and kwargs['stream_id']:
-            i = kwargs['stream_id']
-            if 'size' not in self.streams[i]:
-                self.streams[i]['size'] = urls_size(self.streams[i]['src'])
     
-            pdf = match1(content, r'name='filename'\s*value='([^']+\.pdf)'')
-        if pdf: pdf = 'http://res.infoq.com/downloads/pdfdownloads/%s' % pdf
+# Initialize random generator
+np.random.seed(0)
+    
+    We selected two sets of two variables from the Boston housing data set
+as an illustration of what kind of analysis can be done with several
+outlier detection tools. For the purpose of visualization, we are working
+with two-dimensional examples, but one should be aware that things are
+not so trivial in high-dimension, as it will be pointed out.
+    
+    We add observation noise to these waveforms. We generate very sparse
+noise: only 6% of the time points contain noise. As a result, the
+l1 norm of this noise (ie 'cityblock' distance) is much smaller than it's
+l2 norm ('euclidean' distance). This can be seen on the inter-class
+distance matrices: the values on the diagonal, that characterize the
+spread of the class, are much bigger for the Euclidean distance than for
+the cityblock distance.
+    
+    datasets = [
+    (noisy_circles, {'n_clusters': 2}),
+    (noisy_moons, {'n_clusters': 2}),
+    (varied, {'n_neighbors': 2}),
+    (aniso, {'n_neighbors': 2}),
+    (blobs, {}),
+    (no_structure, {})]
+    
+    import time as time
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.datasets.samples_generator import make_swiss_roll
+    
+        # 'The specifier name is a ``dotted name'' that may resolve either to
+    # a module, a test case class, a TestSuite instance, a test method
+    # within a test case class, or a callable object which returns a
+    # TestCase or TestSuite instance.'
+    #
+    # Is ValueError raised in response to an empty name?
+    def test_loadTestsFromNames__empty_name(self):
+        loader = unittest.TestLoader()
+    
+    
+# Are two filenames really pointing to the same file?
+def samefile(f1, f2):
+    '''Test whether two pathnames reference the same actual file'''
+    s1 = os.stat(f1)
+    s2 = os.stat(f2)
+    return samestat(s1, s2)
+    
+        If a name, the module is imported.  If the passed or imported module
+    object is not a package, raise an exception.
+    '''
+    if hasattr(package, '__spec__'):
+        if package.__spec__.submodule_search_locations is None:
+            raise TypeError('{!r} is not a package'.format(
+                package.__spec__.name))
+        else:
+            return package
+    else:
+        module = import_module(package)
+        if module.__spec__.submodule_search_locations is None:
+            raise TypeError('{!r} is not a package'.format(package))
+        else:
+            return module
+    
+        # Update a record, just for good measure.
+    cursor.execute('UPDATE memos SET task='learn italian' WHERE key=1')
+    
+    # Add the html version.  This converts the message into a multipart/alternative
+# container, with the original text message as the first part and the new html
+# message as the second part.
+asparagus_cid = make_msgid()
+msg.add_alternative('''\
+<html>
+  <head></head>
+  <body>
+    <p>Salut!</p>
+    <p>Cela ressemble à un excellent
+        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
+            recipie
+        </a> déjeuner.
+    </p>
+    <img src='cid:{asparagus_cid}' />
+  </body>
+</html>
+'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
+# note that we needed to peel the <> off the msgid for use in the html.
+    
+    # We can extract the richest alternative in order to display it:
+richest = msg.get_body()
+partfiles = {}
+if richest['content-type'].maintype == 'text':
+    if richest['content-type'].subtype == 'plain':
+        for line in richest.get_content().splitlines():
+            print(line)
+        sys.exit()
+    elif richest['content-type'].subtype == 'html':
+        body = richest
+    else:
+        print('Don't know how to display {}'.format(richest.get_content_type()))
+        sys.exit()
+elif richest['content-type'].content_type == 'multipart/related':
+    body = richest.get_body(preferencelist=('html'))
+    for part in richest.iter_attachments():
+        fn = part.get_filename()
+        if fn:
+            extension = os.path.splitext(part.get_filename())[1]
+        else:
+            extension = mimetypes.guess_extension(part.get_content_type())
+        with tempfile.NamedTemporaryFile(suffix=extension, delete=False) as f:
+            f.write(part.get_content())
+            # again strip the <> to go from email form of cid to html form.
+            partfiles[part['content-id'][1:-1]] = f.name
+else:
+    print('Don't know how to display {}'.format(richest.get_content_type()))
+    sys.exit()
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+    # The magic_html_parser has to rewrite the href='cid:....' attributes to
+    # point to the filenames in partfiles.  It also has to do a safety-sanitize
+    # of the html.  It could be written using html.parser.
+    f.write(magic_html_parser(body.get_content(), partfiles))
+webbrowser.open(f.name)
+os.remove(f.name)
+for fn in partfiles.values():
+    os.remove(fn)
+    
+        # Get and print some more results
+    for i in range(len(TASKS2)):
+        print('\t', done_queue.get())
