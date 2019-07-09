0cb883c7fae792d1db13b0329a39606af42ff681
SUNsung <weatherius@gmail.com>
2019-07-09 21:42:39 +0300

    Rewrite [588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py] File in: 2019-07-09 21:42:38

--- pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
+++ pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
@@ -1,63 +1,15 @@
 
         
-            def _remote_state(self):
-        '''Returns the remote state from AWS'''
-        description = self.rule.describe()
-        if not description:
-            return
-        return description['state']
+        
+def extract_array(obj, extract_numpy=False):
+    '''
+    Extract the ndarray or ExtensionArray from a Series or Index.
     
-            if state == 'absent' and user in heroku_collaborator_list:
-            if not module.check_mode:
-                heroku_app.remove_collaborator(user)
-            affected_apps += [app]
-            result_state = True
-        elif state == 'present' and user not in heroku_collaborator_list:
-            if not module.check_mode:
-                heroku_app.add_collaborator(user_id_or_email=user, silent=module.params['suppress_invitation'])
-            affected_apps += [app]
-            result_state = True
+        if compression == 'zip':
+        pytest.skip(
+            '{compression} is not supported '
+            'for to_csv'.format(compression=compression)
+        )
     
-        for rule in desired_rules:
-        if rule not in current_rules:
-            additions += 1
-            if not module.check_mode:
-                gateway.add_nat_rule(**rule)
-            result['changed'] = True
-    result['rules_added'] = additions
-    
-    
-class DNSZoneIPAClient(IPAClient):
-    def __init__(self, module, host, port, protocol):
-        super(DNSZoneIPAClient, self).__init__(module, host, port, protocol)
-    
-        module = AnsibleModule(
-        argument_spec=dict(
-            component=dict(required=True, aliases=['name']),
-            version=dict(required=True),
-            token=dict(required=True, no_log=True),
-            state=dict(required=True, choices=['started', 'finished', 'failed']),
-            hosts=dict(required=False, default=[socket.gethostname()], aliases=['host']),
-            env=dict(required=False),
-            owner=dict(required=False),
-            description=dict(required=False),
-            message=dict(required=False),
-            source_system=dict(required=False, default='ansible'),
-            validate_certs=dict(default='yes', type='bool'),
-            url=dict(required=False, default='https://api.bigpanda.io'),
-        ),
-        supports_check_mode=True,
-    )
-    
-        if module.check_mode:
-        device = dict(
-            model_handle=None,
-            address=device_ip,
-            landscape='0x%x' % int(module.params.get('landscape'), 16))
-        module.exit_json(changed=True, device=device)
-    
-        parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
-    parser.add_argument('-d', action='store', dest='directory')
-    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
-    parser.add_argument('--overwrite', action='store_true', default = False)    
-    results = parser.parse_args()
+            # Make sure explicit encode_html_chars=True does the encoding.
+        helper(html_encoded, encode_html_chars=True)
