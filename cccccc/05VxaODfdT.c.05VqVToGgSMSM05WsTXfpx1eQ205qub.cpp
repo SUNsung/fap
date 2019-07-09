19e6d997d54e152304d776f3fcd81b2e8ea0b29e
SUNsung <weatherius@gmail.com>
2019-07-09 14:10:33 +0300

    Rewrite [05VxaODfdT.c.05VqVToGgSMSM05WsTXfpx1eQ205qub.cpp] File in: 2019-07-09 14:10:33

--- cccccc/05VxaODfdT.c.05VqVToGgSMSM05WsTXfpx1eQ205qub.cpp
+++ cccccc/05VxaODfdT.c.05VqVToGgSMSM05WsTXfpx1eQ205qub.cpp
@@ -1,306 +1,182 @@
 
         
-        
-    {  return false;
-}
-    
-    Delegate* AutoUpdater::GetDelegate() {
-  return delegate_;
-}
-    
-    #ifndef ATOM_BROWSER_LIB_POWER_OBSERVER_H_
-#define ATOM_BROWSER_LIB_POWER_OBSERVER_H_
-    
-    void URLRequestAboutJob::Start() {
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(&URLRequestAboutJob::StartAsync,
-                                weak_ptr_factory_.GetWeakPtr()));
-}
-    
-    #include 'base/files/file_util.h'
-#include 'base/logging.h'
-#include 'base/mac/mac_logging.h'
-#include 'base/posix/eintr_wrapper.h'
-#include 'base/process/launch.h'
-#include 'base/strings/sys_string_conversions.h'
-    
-    Each feature has fixed lengths which are passed as lengths argument and a
-separate tensor will be produced for each feature.
-i.e. DATA.dim(1) = len(lengths) = NumOuptuts.
-    
-    namespace {
-float sigmoid(const float x) {
-  if (x >= 0) {
-    return 1. / (1. + exp(-x));
-  } else {
-    const float exp_x = exp(x);
-    return exp_x / (1 + exp_x);
+          for (const auto& node : item_.MainOpsFanin()) {
+    PrintNodeInfo(node, properties, debug, os);
   }
-}
-} // namespace
+  for (const auto& node : item_.EnqueueOpsFanin()) {
+    PrintNodeInfo(node, properties, debug, os);
+  }
     
-    REGISTER_CUDA_OPERATOR(LC2D, LocallyConnectedOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(
-    LC2DGradient,
-    LocallyConnectedGradientOp<float, CUDAContext>);
+    // Safe containers for an owned TF_Status. On destruction, the handle
+// will be deleted by TF_DeleteStatus.
+using Safe_TF_StatusPtr = std::unique_ptr<TF_Status, detail::TFStatusDeleter>;
+Safe_TF_StatusPtr make_safe(TF_Status* status);
     
-    b2Polygon TraceEdge(b2Polygon* p){
-	b2PolyNode* nodes = new b2PolyNode[p->nVertices*p->nVertices];//overkill, but sufficient (order of mag. is right)
-	int32 nNodes = 0;
+    namespace stream_executor {
+namespace cuda {
+    }
     }
     
-    const int32 MAX_CONNECTED = 32;
-const float32 COLLAPSE_DIST_SQR = CMP_EPSILON*CMP_EPSILON;//0.1f;//1000*CMP_EPSILON*1000*CMP_EPSILON;
-	
-class b2PolyNode{
+      static const unsigned kMask = 0xf;  // Mask of all available flags.
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    namespace CalcEngine::RationalMath
+{
+    Rational Frac(Rational const& rat);
+    Rational Integer(Rational const& rat);
+    }
+    
+        auto result = resStream.str();
+    
+                    if (fMsb)
+                {
+                    temp = (temp ^ m_chopNumbers[m_numwidth]) + 1;
+    }
+    
+    namespace CurrencyConversionManager
+{
+    enum class Command
+    {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Decimal,
+        Negate,
+        Backspace,
+        Clear,
+        None
+    };
+}
+    
+    class CUnaryCommand final : public IUnaryCommand
+{
 public:
-	b2Vec2 position;
-	b2PolyNode* connected[MAX_CONNECTED];
-	int32 nConnected;
-	bool visited;
+    CUnaryCommand(int command);
+    CUnaryCommand(int command1, int command2);
+    const std::shared_ptr<CalculatorVector<int>>& GetCommands() const override;
+    CalculationManager::CommandType GetCommandType() const override;
+    void SetCommand(int command) override;
+    void SetCommands(int command1, int command2) override;
+    void Accept(_In_ ISerializeCommandVisitor& commandVisitor) override;
     }
     
-    		// intermediate encoding
-		Mode			m_mode;
+        // Calculate the following use the Powers of Powers rule:
+    // px ^ (yNum/yDenom) == px ^ yNum ^ (1/yDenom)
+    // 1. For px ^ yNum, we call powratcomp directly which will call ratpowi32
+    //    and store the result in pxPowNum
+    // 2. For pxPowNum ^ (1/yDenom), we call powratcomp
+    // 3. Validate the result of 2 by adding/subtracting 0.5, flooring and call powratcomp with yDenom
+    //    on the floored result.
     
-    				unsigned int uiSelector1 = ptryBest1->m_auiSelectors[uiPixelOrder];
-				unsigned int uiSelector2 = ptryBest2->m_auiSelectors[uiPixelOrder];
+    //-----------------------------------------------------------------------------
+//
+//    FUNCTION: divrat
+//
+//    ARGUMENTS: pointer to a rational a second rational.
+//
+//    RETURN: None, changes first pointer.
+//
+//    DESCRIPTION: Does the rational equivalent of *pa /= b.
+//    Assumes radix is the radix of both numbers.
+//
+//-----------------------------------------------------------------------------
     
-    #define UNIT_QUANT_SHIFT 2
-#define UNIT_QUANT_FACTOR (1 << UNIT_QUANT_SHIFT)
-    
-      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
-  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-  POSSIBILITY OF SUCH DAMAGE.*/
-    
-    static OPUS_INLINE opus_val16 SIG2WORD16_generic(celt_sig x)
-{
-   x = PSHR32(x, SIG_SHIFT);
-   x = MAX32(x, -32768);
-   x = MIN32(x, 32767);
-   return EXTRACT16(x);
+    //--------------------------------------------------------------------------------
+//
+//  inputText()
+//
+//--------------------------------------------------------------------------------
+UText *RegexMatcher::inputText() const {
+    return fInputText;
 }
-#define SIG2WORD16(x) (SIG2WORD16_generic(x))
     
-    /* (a32 * b32) >> 16 */
-#undef silk_SMULWW
-static OPUS_INLINE opus_int32 silk_SMULWW_armv4(opus_int32 a, opus_int32 b)
-{
-  unsigned rd_lo;
-  int rd_hi;
-  __asm__(
-    '#silk_SMULWW\n\t'
-    'smull %0, %1, %2, %3\n\t'
-    : '=&r'(rd_lo), '=&r'(rd_hi)
-    : '%r'(a), 'r'(b)
-  );
-  return (rd_hi<<16)+(rd_lo>>16);
+        /**
+     * System registration hook.
+     */
+    static void registerIDs();
+    
+    UBool
+SelectFormat::operator!=(const Format& other) const {
+    return  !operator==(other);
 }
-#define silk_SMULWW(a, b) (silk_SMULWW_armv4(a, b))
     
-        void ProgressWriter::UpdateDistributedSync(size_t samples, const ValuePtr& accumulatedMetric)
-    {
-        m_distributedSync->Update(samples, nullptr, accumulatedMetric,
-            [this](const std::pair<size_t, size_t> samples, std::pair<size_t, size_t> updates,
-                   const std::pair<double, double> /*aggregateLoss*/, std::pair<double, double> aggregateMetric)
-        {
-            OnWriteDistributedSyncUpdate(samples, updates, aggregateMetric);
-        });
+    #include 'unicode/utypes.h'
+#include 'sharedobject.h'
+    
+    
+    {    UDate firstTransitionTime = firstTransition->getTime();
+    if (base < firstTransitionTime || (inclusive && base == firstTransitionTime)) {
+        result = *firstTransition;
     }
-    
-    void ProgressWriter::WriteTrainingSummary(const ValuePtr& accumulatedLoss, const ValuePtr& accumulatedMetric)
-    {
-        m_training->WriteSummary(
-            accumulatedLoss, accumulatedMetric,
-            [this](size_t samples, size_t updates, size_t summaries, double aggregateLoss, double aggregateMetric,
-                   uint64_t elapsedMs)
-            {
-                OnWriteTrainingSummary(samples, updates, summaries, aggregateLoss, aggregateMetric, elapsedMs);
-            });
+    UDate stdDate, dstDate;
+    UBool stdAvail = stdRule->getNextStart(base, dstRule->getRawOffset(), dstRule->getDSTSavings(), inclusive, stdDate);
+    UBool dstAvail = dstRule->getNextStart(base, stdRule->getRawOffset(), stdRule->getDSTSavings(), inclusive, dstDate);
+    if (stdAvail && (!dstAvail || stdDate < dstDate)) {
+        result.setTime(stdDate);
+        result.setFrom((const TimeZoneRule&)*dstRule);
+        result.setTo((const TimeZoneRule&)*stdRule);
+        return TRUE;
     }
+    if (dstAvail && (!stdAvail || dstDate < stdDate)) {
+        result.setTime(dstDate);
+        result.setFrom((const TimeZoneRule&)*stdRule);
+        result.setTo((const TimeZoneRule&)*dstRule);
+        return TRUE;
+    }
+    return FALSE;
+}
     
-        void Trainer::UpdateTrainingProgress(size_t numSamples, const ValuePtr& loss, const ValuePtr& evalCriterion,
-                                         const DeviceDescriptor& computeDevice)
-    {
-        if (numSamples == 0)
-        {
-            return;
+    /**
+ * Implement UnicodeFunctor
+ */
+void StringMatcher::setData(const TransliterationRuleData* d) {
+    data = d;
+    int32_t i = 0;
+    while (i<pattern.length()) {
+        UChar32 c = pattern.char32At(i);
+        UnicodeFunctor* f = data->lookup(c);
+        if (f != NULL) {
+            f->setData(data);
         }
+        i += U16_LENGTH(c);
     }
-    
-        void SetAliasInfo(
-        const unordered_map<AliasNodePtr, unordered_set<AliasNodePtr>>& groupMap,
-        const unordered_map<AliasNodePtr, AliasNodePtr>& rootLookupMap)
-    {
-        m_aliasLookup.clear();
-        for (const auto& pair : groupMap)
-        {
-            m_aliasGroups.insert(std::make_pair(pair.first, AliasInfo(pair.second.size())));
-    }
-    }
-    
-            // Allegro's implementation of al_draw_indexed_prim() for DX9 is completely broken. Unindex our buffers ourselves.
-        // FIXME-OPT: Unfortunately Allegro doesn't support 32-bits packed colors so we have to convert them to 4 float as well..
-        static ImVector<ImDrawVertAllegro> vertices;
-        vertices.resize(cmd_list->IdxBuffer.Size);
-        for (int i = 0; i < cmd_list->IdxBuffer.Size; i++)
-        {
-            const ImDrawVert* src_v = &cmd_list->VtxBuffer[cmd_list->IdxBuffer[i]];
-            ImDrawVertAllegro* dst_v = &vertices[i];
-            dst_v->pos = src_v->pos;
-            dst_v->uv = src_v->uv;
-            unsigned char* c = (unsigned char*)&src_v->col;
-            dst_v->col = al_map_rgba(c[0], c[1], c[2], c[3]);
-        }
-    
-        // Glut has 1 function for characters and one for 'special keys'. We map the characters in the 0..255 range and the keys above.
-    io.KeyMap[ImGuiKey_Tab]         = '\t'; // == 9 == CTRL+I
-    io.KeyMap[ImGuiKey_LeftArrow]   = 256 + GLUT_KEY_LEFT;
-    io.KeyMap[ImGuiKey_RightArrow]  = 256 + GLUT_KEY_RIGHT;
-    io.KeyMap[ImGuiKey_UpArrow]     = 256 + GLUT_KEY_UP;
-    io.KeyMap[ImGuiKey_DownArrow]   = 256 + GLUT_KEY_DOWN;
-    io.KeyMap[ImGuiKey_PageUp]      = 256 + GLUT_KEY_PAGE_UP;
-    io.KeyMap[ImGuiKey_PageDown]    = 256 + GLUT_KEY_PAGE_DOWN;
-    io.KeyMap[ImGuiKey_Home]        = 256 + GLUT_KEY_HOME;
-    io.KeyMap[ImGuiKey_End]         = 256 + GLUT_KEY_END;
-    io.KeyMap[ImGuiKey_Insert]      = 256 + GLUT_KEY_INSERT;
-    io.KeyMap[ImGuiKey_Delete]      = 127;
-    io.KeyMap[ImGuiKey_Backspace]   = 8;  // == CTRL+H
-    io.KeyMap[ImGuiKey_Space]       = ' ';
-    io.KeyMap[ImGuiKey_Enter]       = 13; // == CTRL+M
-    io.KeyMap[ImGuiKey_Escape]      = 27;
-    io.KeyMap[ImGuiKey_A]           = 'A';
-    io.KeyMap[ImGuiKey_C]           = 'C';
-    io.KeyMap[ImGuiKey_V]           = 'V';
-    io.KeyMap[ImGuiKey_X]           = 'X';
-    io.KeyMap[ImGuiKey_Y]           = 'Y';
-    io.KeyMap[ImGuiKey_Z]           = 'Z';
-    
-    
-    {        for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.Size; cmd_i++)
-        {
-            const ImDrawCmd* pcmd = &cmd_list->CmdBuffer[cmd_i];
-            if (pcmd->UserCallback)
-            {
-                pcmd->UserCallback(cmd_list, pcmd);
-            }
-            else
-            {
-                // FIXME: Not honoring ClipRect fields.
-                CIwMaterial* pCurrentMaterial = IW_GX_ALLOC_MATERIAL();
-                pCurrentMaterial->SetShadeMode(CIwMaterial::SHADE_FLAT);
-                pCurrentMaterial->SetCullMode(CIwMaterial::CULL_NONE);
-                pCurrentMaterial->SetFiltering(false);
-                pCurrentMaterial->SetAlphaMode(CIwMaterial::ALPHA_BLEND);
-                pCurrentMaterial->SetDepthWriteMode(CIwMaterial::DEPTH_WRITE_NORMAL);
-                pCurrentMaterial->SetAlphaTestMode(CIwMaterial::ALPHATEST_DISABLED);
-                pCurrentMaterial->SetTexture((CIwTexture*)pcmd->TextureId);
-                IwGxSetMaterial(pCurrentMaterial);
-                IwGxDrawPrims(IW_GX_TRI_LIST, (uint16*)idx_buffer, pcmd->ElemCount);
-            }
-            idx_buffer += pcmd->ElemCount;
-        }
-        IwGxFlush();
-    }
-    
-    namespace ImGuiFreeType
-{
-    // Hinting greatly impacts visuals (and glyph sizes).
-    // When disabled, FreeType generates blurrier glyphs, more or less matches the stb's output.
-    // The Default hinting mode usually looks good, but may distort glyphs in an unusual way.
-    // The Light hinting mode generates fuzzier glyphs but better matches Microsoft's rasterizer.
-    }
-    
-                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox('Another Window', &show_another_window);
-    
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-    #include 'imgui.h'
-#include 'imgui_impl_sdl.h'
-#include 'imgui_impl_opengl3.h'
-#include <stdio.h>
-#include <emscripten.h>
-#include <SDL.h>
-#include <SDL_opengles2.h>
-    
-    static void ImGui_ImplSDL2_UpdateGamepads()
-{
-    ImGuiIO& io = ImGui::GetIO();
-    memset(io.NavInputs, 0, sizeof(io.NavInputs));
-    if ((io.ConfigFlags & ImGuiConfigFlags_NavEnableGamepad) == 0)
-        return;
-    }
-    
-        std::unique_ptr<DHTRoutingTable> routingTable;
-    
-    std::string DHTResponseMessage::toString() const
-{
-  return fmt('dht response %s TransactionID=%s Remote:%s(%u), id=%s, v=%s, %s',
-             getMessageType().c_str(), util::toHex(getTransactionID()).c_str(),
-             getRemoteNode()->getIPAddress().c_str(),
-             getRemoteNode()->getPort(),
-             util::toHex(getRemoteNode()->getID(), DHT_ID_LENGTH).c_str(),
-             util::torrentPercentEncode(getVersion()).c_str(),
-             toStringOptional().c_str());
 }
     
-    #include 'DHTAbstractMessage.h'
-#include 'A2STR.h'
-#include 'ValueBase.h'
     
-      // localnode
-  // 8bytes reserved
-  readBytes(fp, buf, buf.size(), 8);
-  // localnode ID
-  readBytes(fp, buf, buf.size(), DHT_ID_LENGTH);
-  auto localNode = std::make_shared<DHTNode>(buf);
-  // 4bytes reserved
-  readBytes(fp, buf, buf.size(), 4);
+    {  pack_count = 0;
+  get().removePack('unrestricted_pack');
+  get().packs(([&pack_count](const Pack& pack) { pack_count++; }));
+  EXPECT_EQ(pack_count, 0U);
+}
     
-    void DHTTaskExecutor::update()
-{
-  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
-                                  std::mem_fn(&DHTTask::finished)),
-                   execTasks_.end());
-  int r;
-  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
-    r = numConcurrent_ - execTasks_.size();
+    
+    {  fpack.platform_ = 'windows';
+  if (isPlatform(PlatformType::TYPE_WINDOWS)) {
+    EXPECT_TRUE(fpack.checkPlatform());
+  } else {
+    EXPECT_FALSE(fpack.checkPlatform());
   }
-  else {
-    r = 0;
-  }
-  while (r && !queue_.empty()) {
-    std::shared_ptr<DHTTask> task = queue_.front();
-    queue_.pop_front();
-    task->startup();
-    if (!task->finished()) {
-      execTasks_.push_back(task);
-      --r;
+}
+    
+    ExpectedSuccess<DatabaseError> RocksdbDatabase::putStringsUnsafe(
+    const std::string& domain,
+    const std::vector<std::pair<std::string, std::string>>& data) {
+  auto handle = getHandle(domain);
+  if (handle) {
+    std::shared_ptr<Handle> handle_ptr = handle.take();
+    rocksdb::WriteBatch batch;
     }
+    }
+    
+      rj::Document doc;
+  if (doc.Parse(request.at('log').c_str()).HasParseError()) {
+    return;
   }
-  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
-                   static_cast<unsigned int>(getExecutingTaskSize()),
-                   static_cast<unsigned int>(getQueueSize())));
-}
-    
-    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
-                                             std::chrono::seconds interval)
-    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
-{
-}
-    
-    DNSCache::CacheEntry::~CacheEntry() = default;
