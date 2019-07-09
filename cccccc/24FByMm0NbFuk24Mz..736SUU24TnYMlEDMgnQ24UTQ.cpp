584523d5f5216c047e7926828e9773187864a62a
SUNsung <weatherius@gmail.com>
2019-07-09 04:12:26 +0300

    Rewrite [24FByMm0NbFuk24Mz..736SUU24TnYMlEDMgnQ24UTQ.cpp] File in: 2019-07-09 04:12:25

--- cccccc/24FByMm0NbFuk24Mz..736SUU24TnYMlEDMgnQ24UTQ.cpp
+++ cccccc/24FByMm0NbFuk24Mz..736SUU24TnYMlEDMgnQ24UTQ.cpp
@@ -1,247 +1,58 @@
 
         
         
-    {}  // end namespace tensorflow
+    {	}
+    
+    
+#   define S_MUL(a,b) MULT16_32_Q15(b, a)
+    
+    typedef float opus_val16;
+typedef float opus_val32;
+    
+    /** 16x16 multiplication where the result fits in 32 bits */
+#undef MULT16_16
+static OPUS_INLINE opus_val32 MULT16_16_armv5e(opus_val16 a, opus_val16 b)
+{
+  int res;
+  __asm__(
+      '#MULT16_16\n\t'
+      'smulbb %0, %1, %2;\n'
+      : '=r'(res)
+      : 'r'(a), 'r'(b)
+  );
+  return res;
+}
+#define MULT16_16(a, b) (MULT16_16_armv5e(a, b))
+    
+    /*The number of bits to output at a time.*/
+# define EC_SYM_BITS   (8)
+/*The total number of bits in each of the state registers.*/
+# define EC_CODE_BITS  (32)
+/*The maximum symbol value.*/
+# define EC_SYM_MAX    ((1U<<EC_SYM_BITS)-1)
+/*Bits to shift by to move a symbol into the high-order position.*/
+# define EC_CODE_SHIFT (EC_CODE_BITS-EC_SYM_BITS-1)
+/*Carry bit of the high-order range symbol.*/
+# define EC_CODE_TOP   (((opus_uint32)1U)<<(EC_CODE_BITS-1))
+/*Low-order bit of the high-order range symbol.*/
+# define EC_CODE_BOT   (EC_CODE_TOP>>EC_SYM_BITS)
+/*The number of bits available for the last, partial symbol in the code field.*/
+# define EC_CODE_EXTRA ((EC_CODE_BITS-2)%EC_SYM_BITS+1)
+#endif
 
     
-    
-    {}  // namespace tensorflow
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    string TryFindKernelClass(const string& serialized_node_def) {
-  tensorflow::NodeDef node_def;
-  if (!node_def.ParseFromString(serialized_node_def)) {
-    LOG(WARNING) << 'Error parsing node_def';
-    return '';
-  }
-    }
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    #ifndef TENSORFLOW_STREAM_EXECUTOR_HOST_HOST_TIMER_H_
-#define TENSORFLOW_STREAM_EXECUTOR_HOST_HOST_TIMER_H_
-    
-    // This enum represents potential configurations of L1/shared memory when
-// running a particular kernel. These values represent user preference, and
-// the runtime is not required to respect these choices.
-enum class KernelCacheConfig {
-  // Indicates no preference for device L1/shared memory configuration.
-  kNoPreference,
-    }
-    
-      DefaultCacheKey CKey(Key, &DCache.CBs);
-  auto Entry = DCache.Entries.find(CKey);
-  if (Entry != DCache.Entries.end()) {
-    if (Entry->second == Value)
-      return;
-    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
-    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
-    DCache.Entries.erase(Entry);
-  }
-    
-    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
-  int Ret = cache_get_and_retain(static_cast<cache_t*>(Impl),
-                                 const_cast<void*>(Key), Value_out);
-  return Ret == 0;
+    /* (a32 * b32) >> 16 */
+#undef silk_SMULWW
+static OPUS_INLINE opus_int32 silk_SMULWW_armv4(opus_int32 a, opus_int32 b)
+{
+  unsigned rd_lo;
+  int rd_hi;
+  __asm__(
+    '#silk_SMULWW\n\t'
+    'smull %0, %1, %2, %3\n\t'
+    : '=&r'(rd_lo), '=&r'(rd_hi)
+    : '%r'(a), 'r'(b)
+  );
+  return (rd_hi<<16)+(rd_lo>>16);
 }
-    
-    void swift::printOpaquePrefixMap(raw_ostream &out, void *_root,
-                         void (*printNodeData)(raw_ostream &out, void *node)) {
-  auto root = reinterpret_cast<Node*>(_root);
-  if (!root) {
-    out << '(empty)\n';
-    return;
-  }
-  TreePrinter(out, *printNodeData).print(root, ChildKind::Root);
-}
-    
-    %# Ignore the following admonition; it applies to the resulting .cpp file only
-//// Automatically Generated From UnicodeExtendedGraphemeClusters.cpp.gyb.
-//// Do Not Edit Directly!
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-//===----------------------------------------------------------------------===//
-    
-    StringRef importer::getCFTypeName(
-            const clang::TypedefNameDecl *decl) {
-  if (auto pointee = CFPointeeInfo::classifyTypedef(decl)) {
-    auto name = decl->getName();
-    if (pointee.isRecord() || pointee.isTypedef())
-      if (name.endswith(SWIFT_CFTYPE_SUFFIX))
-        return name.drop_back(strlen(SWIFT_CFTYPE_SUFFIX));
-    }
-    }
-    
-    char Mangle::getStandardTypeSubst(StringRef TypeName) {
-#define STANDARD_TYPE(KIND, MANGLING, TYPENAME)      \
-  if (TypeName == #TYPENAME) {                       \
-    return #MANGLING[0];                             \
-  }
-    }
-    
-      /// Returns the newly set-up FrontendInputsAndOutputs, as well as a set of
-  /// any unused primary files (those that do not correspond to an input).
-  std::pair<FrontendInputsAndOutputs, std::set<StringRef>>
-  createInputFilesConsumingPrimaries(std::set<StringRef> primaryFiles);
-    
-    namespace swift {
-    }
-    
-    MeasureDouble RpcClientReceivedBytesPerRpc() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcClientReceivedBytesPerRpcMeasureName,
-      'Total bytes received across all response messages per RPC', kUnitBytes);
-  return measure;
-}
-    
-    namespace grpc {
-    }
-    
-        virtual void UpdateArguments(ChannelArguments* args) override {
-      args->SetInt(name_, value_);
-    }
-    virtual void UpdatePlugins(
-        std::vector<std::unique_ptr<ServerBuilderPlugin>>* plugins) override {}
-    
-    ThreadPoolInterface* CreateDefaultThreadPoolImpl() {
-  int cores = gpr_cpu_num_cores();
-  if (!cores) cores = 4;
-  return new DynamicThreadPool(cores);
-}
-    
-    #include 'src/cpp/server/load_reporter/get_cpu_stats.h'
-    
-        string precision = config(L'precision', 'float');
-    
-    
-    {    ~ScopeTimer()
-    {
-        if (m_verbosity > 2)
-        {
-            m_aggregateTimer.Stop();
-            double time = m_aggregateTimer.ElapsedSeconds();
-            fprintf(stderr, m_message.c_str(), time);
-        }
-    }
-};
-    
-    
-    {
-    {        if (isFinalValidationPass)
-            if (!(Input(1)->GetSampleMatrixNumRows() == Input(2)->GetSampleMatrixNumRows() && // position dependent and pair scores have same number of labels
-                  Input(0)->GetSampleMatrixNumRows() == Input(1)->GetSampleMatrixNumRows() &&
-                  Input(0)->GetSampleMatrixNumCols() == Input(1)->GetSampleMatrixNumCols() && // position dependent and pair scores have the same observation numbers
-                  Input(2)->GetSampleMatrixNumCols() == Input(2)->GetSampleMatrixNumRows()))
-            {
-                LogicError('The Matrix<ElemType>  dimension in the SequenceDecoderNode operation does not match.');
-            }
-        // BUGBUG: No SetDims()?
-        m_sampleLayout = TensorShape();
-    }
-};
-    
-    public:
-    // this constructor is always run (all other constructors call this one)
-    LearnableParameter(DEVICEID_TYPE deviceId, const wstring& name) :
-        Base(deviceId, name)
-    {
-        SetLearningRateMultiplier(1.0f); // enable normal learning by default
-        MarkValueNonSharable();
-        m_initString = L'fromValue'; // default init is with 0; typically overwritten
-        m_initValue = 0;
-        m_regMultiplier = 1.0f; // enable reg in update by default
-    }
-    LearnableParameter(DEVICEID_TYPE deviceId, const wstring& name, const TensorShape& shape) :
-        LearnableParameter(deviceId, name)
-    {
-        InitShape(shape);
-        LazyInitParameters();
-    }
-    LearnableParameter(DEVICEID_TYPE deviceId, const wstring& name, size_t rows, size_t cols) :
-        LearnableParameter(deviceId, name, TensorShape(rows, cols))
-    {
-    }
-    LearnableParameter(const ScriptableObjects::IConfigRecordPtr configp);
-    
-            // ensure enough storage
-        m_transposedOutput->Resize(this->Value().GetNumRows(), m_transposedInput->GetNumCols());
-    
-      std::atomic<int> total_stopped_;
-  std::atomic<int> total_delayed_;
-  std::atomic<int> total_compaction_pressure_;
-  uint64_t bytes_left_;
-  uint64_t last_refill_time_;
-  // write rate set when initialization or by `DBImpl::SetDBOptions`
-  uint64_t max_delayed_write_rate_;
-  // current write rate
-  uint64_t delayed_write_rate_;
-    
-    #endif  // ROCKSDB_LITE
-
-    
-    struct DumpOptions {
-  // Database that will be dumped
-  std::string db_path;
-  // File location that will contain dump output
-  std::string dump_location;
-  // Don't include db information header in the dump
-  bool anonymous = false;
-};
-    
-    // Supported only for Leveled compaction
-Status SuggestCompactRange(DB* db, ColumnFamilyHandle* column_family,
-                           const Slice* begin, const Slice* end);
-Status SuggestCompactRange(DB* db, const Slice* begin, const Slice* end);
-    
-      // Instead of creating a snapshot, take ownership of the input snapshot.
-  ManagedSnapshot(DB* db, const Snapshot* _snapshot);
-    
-    class DB;
-    
-    #pragma once
-// lua headers
-extern 'C' {
-#include <lauxlib.h>
-#include <lua.h>
-#include <lualib.h>
-}
-    
-    
-    {
-    {  // Returns the approximate memory usage of different types in the input
-  // list of DBs and Cache set.  For instance, in the output map
-  // usage_by_type, usage_by_type[kMemTableTotal] will store the memory
-  // usage of all the mem-tables from all the input rocksdb instances.
-  //
-  // Note that for memory usage inside Cache class, we will
-  // only report the usage of the input 'cache_set' without
-  // including those Cache usage inside the input list 'dbs'
-  // of DBs.
-  static Status GetApproximateMemoryUsageByType(
-      const std::vector<DB*>& dbs,
-      const std::unordered_set<const Cache*> cache_set,
-      std::map<MemoryUtil::UsageType, uint64_t>* usage_by_type);
-};
-}  // namespace rocksdb
-#endif  // !ROCKSDB_LITE
-
-    
-    // TransactionDBMutex and TransactionDBCondVar APIs allows applications to
-// implement custom mutexes and condition variables to be used by a
-// TransactionDB when locking keys.
-//
-// To open a TransactionDB with a custom TransactionDBMutexFactory, set
-// TransactionDBOptions.custom_mutex_factory.
+#define silk_SMULWW(a, b) (silk_SMULWW_armv4(a, b))
