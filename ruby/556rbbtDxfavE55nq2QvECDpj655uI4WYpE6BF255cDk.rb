e03bcffd003cb27aee186d8c425cbe01bf0534aa
SUNsung <weatherius@gmail.com>
2019-07-10 09:25:22 +0300

    Rewrite [556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb] File in: 2019-07-10 09:25:22

--- ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
+++ ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
@@ -1,79 +1,82 @@
 
         
-        # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
-    
-              theme.create!
-          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
-                             ' is ready for you in #{theme.path.to_s.cyan}!'
-          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
-        end
-        # rubocop:enable Metrics/AbcSize
-      end
-    end
+            headers
   end
 end
+    
+          theme.destroy!
+    
+        # Security: safe due to route constraint
+    cache_file = '#{DISK_CACHE_PATH}/#{params[:digest]}.js'
+    
+    # == Schema Information
+#
+# Table name: user_stats
+#
+#  user_id                  :integer          not null, primary key
+#  topics_entered           :integer          default(0), not null
+#  time_read                :integer          default(0), not null
+#  days_visited             :integer          default(0), not null
+#  posts_read_count         :integer          default(0), not null
+#  likes_given              :integer          default(0), not null
+#  likes_received           :integer          default(0), not null
+#  topic_reply_count        :integer          default(0), not null
+#  new_since                :datetime         not null
+#  read_faq                 :datetime
+#  first_post_created_at    :datetime
+#  post_count               :integer          default(0), not null
+#  topic_count              :integer          default(0), not null
+#  bounce_score             :float            default(0.0), not null
+#  reset_bounce_score_after :datetime
+#  flags_agreed             :integer          default(0), not null
+#  flags_disagreed          :integer          default(0), not null
+#  flags_ignored            :integer          default(0), not null
+#  first_unread_at          :datetime         not null
+#
 
     
-      if ENV['HOMEBREW_INTEGRATION_TEST']
-    command_name '#{ENV['HOMEBREW_INTEGRATION_TEST']} (#{$PROCESS_ID})'
-    
-        # Extract each header value pair
-    header.split(/\r\n/mn).each { |str|
-      if (md = str.match(/^(.+?)\s*:\s*(.+?)\s*$/))
-        if (self[md[1]])
-          self[md[1]] << ', ' + md[2]
-        else
-          self[md[1]] = md[2]
-        end
-      end
-    }
+    namespace :doc do
+  task :readmes do
+    Dir.glob 'lib/rack/protection/*.rb' do |file|
+      excluded_files = %w[lib/rack/protection/base.rb lib/rack/protection/version.rb]
+      next if excluded_files.include?(file)
+      doc  = File.read(file)[/^  module Protection(\n)+(    #[^\n]*\n)*/m].scan(/^ *#(?!#) ?(.*)\n/).join('\n')
+      file = 'doc/#{file[4..-4].tr('/_', '-')}.rdoc'
+      Dir.mkdir 'doc' unless File.directory? 'doc'
+      puts 'writing #{file}'
+      File.open(file, 'w') { |f| f << doc }
+    end
   end
     
-          when IAX_CTRL_PROCEED
-        dprint('PROCEED')
+      it 'should set the Content Security Policy' do
+    expect(
+      get('/', {}, 'wants' => 'text/html').headers['Content-Security-Policy']
+    ).to eq('connect-src 'self'; default-src none; img-src 'self'; script-src 'self'; style-src 'self'')
+  end
     
-    require 'rex/proto/ipmi/utils'
+    class SinatraStaticServer < Sinatra::Base
     
-              # Encodes the options field
-          #
-          # @return [OpenSSL::ASN1::BitString]
-          def encode_options
-            OpenSSL::ASN1::BitString.new([options].pack('N'))
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncKdcResponse from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
-    
-              # Encodes a Rex::Proto::Kerberos::Model::EncryptedData into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            elems = []
-            etype_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_etype], 0, :CONTEXT_SPECIFIC)
-            elems << etype_asn1
-    
-              # Rex::Proto::Kerberos::Model::LastRequest encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'LastRequest encoding not supported'
-          end
-    
-        def version
-      ['--version', '-V',
-       'Display the program version.',
-       lambda do |_value|
-         puts 'Capistrano Version: #{Capistrano::VERSION} (Rake Version: #{Rake::VERSION})'
-         exit
-       end]
+    class ConfigTag < Liquid::Tag
+  def initialize(tag_name, options, tokens)
+    super
+    options = options.split(' ').map {|i| i.strip }
+    @key = options.slice!(0)
+    @tag = nil
+    @classname = nil
+    options.each do |option|
+      @tag = $1 if option =~ /tag:(\S+)/ 
+      @classname = $1 if option =~ /classname:(\S+)/
     end
+  end
     
-          class ValidatedQuestion < Question
-        def initialize(validator)
-          @validator = validator
-        end
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+    
+      # mknod is __xmknod in glibc a wrapper around mknod to handle
+  # various stat struct formats. See bits/stat.h in glibc source
+  begin
+    attach_function :mknod, :mknod, [:string, :uint, :ulong], :int
+  rescue FFI::NotFoundError
+    # glibc/io/xmknod.c int __xmknod (int vers, const char *path, mode_t mode, dev_t *dev)
+    attach_function :xmknod, :__xmknod, [:int, :string, :uint, :pointer], :int
+  end
