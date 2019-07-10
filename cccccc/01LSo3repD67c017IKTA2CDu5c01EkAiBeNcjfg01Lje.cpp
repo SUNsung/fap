9cafb5d47e2f63b50f593060b1f0a0db839ccca1
SUNsung <weatherius@gmail.com>
2019-07-10 10:04:43 +0300

    Rewrite [01LSo3repD67c017IKTA2CDu5c01EkAiBeNcjfg01Lje.cpp] File in: 2019-07-10 10:04:43

--- cccccc/01LSo3repD67c017IKTA2CDu5c01EkAiBeNcjfg01Lje.cpp
+++ cccccc/01LSo3repD67c017IKTA2CDu5c01EkAiBeNcjfg01Lje.cpp
@@ -1,187 +1,130 @@
 
         
-        		inline bool HasBorderPixels(void)
-		{
-			return m_boolBorderPixels;
-		}
+        #define TEGRA_MAX(src1, sz1, src2, sz2, dst, sz, w, h) \
+( \
+    CAROTENE_NS::isSupportedConfiguration() ? \
+    CAROTENE_NS::max(CAROTENE_NS::Size2D(w, h), \
+                     src1, sz1, \
+                     src2, sz2, \
+                     dst, sz), \
+    CV_HAL_ERROR_OK \
+    : CV_HAL_ERROR_NOT_IMPLEMENTED \
+)
     
-    // Constants:
-//  for (int i = 1; i< 32; ++i)
-//    printf('static const int cospi_%d_64 = %.0f;\n', i,
-//           round(16384 * cos(i*M_PI/64)));
-// Note: sin(k*Pi/64) = cos((32-k)*Pi/64)
-static const tran_high_t cospi_1_64  = 16364;
-static const tran_high_t cospi_2_64  = 16305;
-static const tran_high_t cospi_3_64  = 16207;
-static const tran_high_t cospi_4_64  = 16069;
-static const tran_high_t cospi_5_64  = 15893;
-static const tran_high_t cospi_6_64  = 15679;
-static const tran_high_t cospi_7_64  = 15426;
-static const tran_high_t cospi_8_64  = 15137;
-static const tran_high_t cospi_9_64  = 14811;
-static const tran_high_t cospi_10_64 = 14449;
-static const tran_high_t cospi_11_64 = 14053;
-static const tran_high_t cospi_12_64 = 13623;
-static const tran_high_t cospi_13_64 = 13160;
-static const tran_high_t cospi_14_64 = 12665;
-static const tran_high_t cospi_15_64 = 12140;
-static const tran_high_t cospi_16_64 = 11585;
-static const tran_high_t cospi_17_64 = 11003;
-static const tran_high_t cospi_18_64 = 10394;
-static const tran_high_t cospi_19_64 = 9760;
-static const tran_high_t cospi_20_64 = 9102;
-static const tran_high_t cospi_21_64 = 8423;
-static const tran_high_t cospi_22_64 = 7723;
-static const tran_high_t cospi_23_64 = 7005;
-static const tran_high_t cospi_24_64 = 6270;
-static const tran_high_t cospi_25_64 = 5520;
-static const tran_high_t cospi_26_64 = 4756;
-static const tran_high_t cospi_27_64 = 3981;
-static const tran_high_t cospi_28_64 = 3196;
-static const tran_high_t cospi_29_64 = 2404;
-static const tran_high_t cospi_30_64 = 1606;
-static const tran_high_t cospi_31_64 = 804;
+        void cmpGE(const Size2D &size,
+               const u16 *src0Base, ptrdiff_t src0Stride,
+               const u16 *src1Base, ptrdiff_t src1Stride,
+               u8 *dstBase, ptrdiff_t dstStride);
     
-      All rights reserved.
+        typedef int8_t   s8;
+    typedef uint8_t  u8;
+    typedef int16_t  s16;
+    typedef uint16_t u16;
+    typedef int32_t  s32;
+    typedef uint32_t u32;
+    typedef float    f32;
+    typedef int64_t  s64;
+    typedef uint64_t u64;
+    typedef double   f64;
     
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-    
-    #define MULT16_16(a, b) MULT16_16_(a, b, __FILE__, __LINE__)
-static OPUS_INLINE int MULT16_16_(int a, int b, char *file, int line)
+    struct BitwiseXor
 {
-   opus_int64 res;
-   if (!VERIFY_SHORT(a) || !VERIFY_SHORT(b))
-   {
-      fprintf (stderr, 'MULT16_16: inputs are not short: %d %d in %s: line %d\n', a, b, file, line);
-#ifdef FIXED_DEBUG_ASSERT
-      celt_assert(0);
-#endif
-   }
-   res = ((opus_int64)a)*b;
-   if (!VERIFY_INT(res))
-   {
-      fprintf (stderr, 'MULT16_16: output is not int: %d in %s: line %d\n', (int)res, file, line);
-#ifdef FIXED_DEBUG_ASSERT
-      celt_assert(0);
-#endif
-   }
-   celt_mips++;
-   return res;
-}
+    typedef u8 type;
+    }
     
-    /* a32 + (b32 * (c32 >> 16)) >> 16 */
-#undef silk_SMLAWT
-static OPUS_INLINE opus_int32 silk_SMLAWT_armv5e(opus_int32 a, opus_int32 b,
- opus_int32 c)
-{
-  int res;
-  __asm__(
-      '#silk_SMLAWT\n\t'
-      'smlawt %0, %1, %2, %3\n\t'
-      : '=r'(res)
-      : 'r'(b), 'r'(c), 'r'(a)
-  );
-  return res;
-}
-#define silk_SMLAWT(a, b, c) (silk_SMLAWT_armv5e(a, b, c))
+                int32x4_t normh = vmovl_s16(vget_high_s16(norm));
+            int32x4_t norml = vmovl_s16(vget_low_s16(norm));
     
+    #include <limits>
     
+        for (size_t i = 0; i < size.height; ++i)
     {
-    {    private:
-        int32_t m_sign;
-        int32_t m_exp;
-        std::vector<uint32_t> m_mantissa;
-    };
+        s16* dst = internal::getRowPtr(dstBase, dstStride, i);
+        //vertical convolution
+        ptrdiff_t idx_rm2 = internal::borderInterpolate(i - 2, size.height, borderType, borderMargin.top, borderMargin.bottom);
+        ptrdiff_t idx_rm1 = internal::borderInterpolate(i - 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
+        ptrdiff_t idx_rp1 = internal::borderInterpolate(i + 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
+        ptrdiff_t idx_rp2 = internal::borderInterpolate(i + 2, size.height, borderType, borderMargin.top, borderMargin.bottom);
+    }
+    
+    void Laplacian1OpenCV(const Size2D &size,
+                      const u8 * srcBase, ptrdiff_t srcStride,
+                      s16 * dstBase, ptrdiff_t dstStride,
+                      BORDER_MODE border, u8 borderValue)
+{
+    internal::assertSupportedConfiguration(isLaplacianOpenCVSupported(size, border));
+#ifdef CAROTENE_NEON
+    ptrdiff_t rows = size.height, cols = size.width;
+    }
+    
+    
+    { private:
+  App();
+  DISALLOW_COPY_AND_ASSIGN(App);
+};
+    
+    void Clipboard::CallSync(const std::string& method,
+                         const base::ListValue& arguments,
+                         base::ListValue* result) {
+  if (method == 'Get') {
+    result->AppendString(GetText());
+  } else {
+    NOTREACHED() << 'Invalid call to Clipboard method:' << method
+                 << ' arguments:' << arguments;
+  }
 }
+    
+    
+    {} // namespace nwapi
 
     
-            CommandLN = 108,
-        CommandLOG = 109,
-        CommandSQRT = 110,
-        CommandSQR = 111,
-        CommandCUB = 112,
-        CommandFAC = 113,
-        CommandREC = 114,
-        CommandDMS = 115,
-        CommandCUBEROOT = 116, // x ^ 1/3
-        CommandPOW10 = 117,    // 10 ^ x
-        CommandPERCENT = 118,
-    
-    ScientificNumberFormatter::ScientificNumberFormatter(
-        DecimalFormat *fmtToAdopt, Style *styleToAdopt, UErrorCode &status)
-        : fPreExponent(),
-          fDecimalFormat(fmtToAdopt),
-          fStyle(styleToAdopt),
-          fStaticSets(NULL) {
-    if (U_FAILURE(status)) {
-        return;
+        int menu_id;
+    if (option.GetInteger('submenu', &menu_id))
+      SetSubmenu(dispatcher_host()->GetApiObject<Menu>(menu_id));
+    std::string key;
+    if (option.GetString('key',&key)){
+      enable_shortcut = true;
+      std::string modifiers = '';
+      option.GetString('modifiers',&modifiers);
+      modifiers_mask = GdkModifierType(0);
+      if (modifiers.size() != 0){
+        if (modifiers.find('ctrl') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_CONTROL_MASK);
+        }
+        if (modifiers.find('alt') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_MOD1_MASK);
+        }
+        if (modifiers.find('super') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_SUPER_MASK);
+        }
+        if (modifiers.find('meta') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_META_MASK);
+        }
+        
+        if (modifiers.find('shift') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_SHIFT_MASK);
+        }
     }
-    if (fDecimalFormat == NULL || fStyle == NULL) {
-        status = U_ILLEGAL_ARGUMENT_ERROR;
-        return;
-    }
-    const DecimalFormatSymbols *sym = fDecimalFormat->getDecimalFormatSymbols();
-    if (sym == NULL) {
-        status = U_ILLEGAL_ARGUMENT_ERROR;
-        return;
-    }
-    getPreExponent(*sym, fPreExponent);
-    fStaticSets = DecimalFormatStaticSets::getStaticSets(status);
-}
-    
-    int32_t ScriptSet::hashCode() const {
-    int32_t hash = 0;
-    for (int32_t i=0; i<UPRV_LENGTHOF(bits); i++) {
-        hash ^= bits[i];
-    }
-    return hash;
-}
-    
-    /**
- * An interval of allowed significant digit counts.
- */
-class U_I18N_API SignificantDigitInterval : public UMemory {
-public:
     }
     
-    #include 'unicode/utypes.h'
+     protected:
+  ~NwClipboardReadAvailableTypesFunction() override;
     
-    #include 'smallintformatter.h'
-    
-    #include 'unicode/utypes.h'
-    
-    #ifndef __STANDARDPLURAL_H__
-#define __STANDARDPLURAL_H__
-    
-        if (conf.isNull()) continue;
-    
-    #include 'app/models/connectionconf.h'
-#include 'bulk-operations/connections.h'
-#include 'connections-tree/model.h'
-#include 'treeoperations.h'
-    
-        void classBegin();
-    void componentComplete();
-    
-     public:
-  TabModel(QSharedPointer<RedisClient::Connection> connection, int dbIndex);
-    
-    void TabViewModel::closeTab(int i) {
-  if (!isIndexValid(index(i, 0))) return;
+    int StressScavengeObserver::NextLimit(int min) {
+  int max = FLAG_stress_scavenge;
+  if (min >= max) {
+    return max;
+  }
     }
     
-     public slots:
-  void openTab(QSharedPointer<RedisClient::Connection> connection,
-               int dbIndex = 0);
-    
-    
-    {}
-Updater::~Updater()
-{
-    delete manager;
-}
-    
-    #include <QtCore/QObject>
-#include <QtNetwork/QNetworkAccessManager>
+     private:
+  FRIEND_TEST(WorkListTest, SegmentCreate);
+  FRIEND_TEST(WorkListTest, SegmentPush);
+  FRIEND_TEST(WorkListTest, SegmentPushPop);
+  FRIEND_TEST(WorkListTest, SegmentIsEmpty);
+  FRIEND_TEST(WorkListTest, SegmentIsFull);
+  FRIEND_TEST(WorkListTest, SegmentClear);
+  FRIEND_TEST(WorkListTest, SegmentFullPushFails);
+  FRIEND_TEST(WorkListTest, SegmentEmptyPopFails);
+  FRIEND_TEST(WorkListTest, SegmentUpdateFalse);
+  FRIEND_TEST(WorkListTest, SegmentUpdate);
