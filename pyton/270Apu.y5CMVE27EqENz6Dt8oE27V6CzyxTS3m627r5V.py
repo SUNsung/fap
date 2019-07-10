656c62f3ed0705d2f51851dbb9684fa9ecfc5a27
SUNsung <weatherius@gmail.com>
2019-07-10 19:36:50 +0300

    Rewrite [270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py] File in: 2019-07-10 19:36:49

--- pyton/270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py
+++ pyton/270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py
@@ -1,162 +1,45 @@
 
         
         
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
     
-    import io
-import optparse
-import os
-import sys
+        exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR
+    assert error_msg == (
+        'ConnectionError: '
+        'Connection aborted while doing GET request to URL: '
+        'http://www.google.com')
     
-    from test.helper import try_rm
-    
-    rootDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    
-    
-class CITextExtension(CreateExtension):
-    
-        def delete(self, session_key=None):
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        self._cache.delete(self.cache_key_prefix + session_key)
-    
-        @property
-    def auto_id(self):
-        '''
-        Calculate and return the ID attribute for this BoundField, if the
-        associated Form has specified auto_id. Return an empty string otherwise.
-        '''
-        auto_id = self.form.auto_id  # Boolean or string
-        if auto_id and '%s' in str(auto_id):
-            return auto_id % self.html_name
-        elif auto_id:
-            return self.html_name
-        return ''
-    
-    from .compat import is_py2, builtin_str, str
-    
-            # because special names such as Name.Class, Name.Function, etc.
-        # are not recognized as such later in the parsing, we choose them
-        # to look the same as ordinary variables.
-        Name:                      '#000000',        # class: 'n'
-        Name.Attribute:            '#c4a000',        # class: 'na' - to be revised
-        Name.Builtin:              '#004461',        # class: 'nb'
-        Name.Builtin.Pseudo:       '#3465a4',        # class: 'bp'
-        Name.Class:                '#000000',        # class: 'nc' - to be revised
-        Name.Constant:             '#000000',        # class: 'no' - to be revised
-        Name.Decorator:            '#888',           # class: 'nd' - to be revised
-        Name.Entity:               '#ce5c00',        # class: 'ni'
-        Name.Exception:            'bold #cc0000',   # class: 'ne'
-        Name.Function:             '#000000',        # class: 'nf'
-        Name.Property:             '#000000',        # class: 'py'
-        Name.Label:                '#f57900',        # class: 'nl'
-        Name.Namespace:            '#000000',        # class: 'nn' - to be revised
-        Name.Other:                '#000000',        # class: 'nx'
-        Name.Tag:                  'bold #004461',   # class: 'nt' - like a keyword
-        Name.Variable:             '#000000',        # class: 'nv' - to be revised
-        Name.Variable.Class:       '#000000',        # class: 'vc' - to be revised
-        Name.Variable.Global:      '#000000',        # class: 'vg' - to be revised
-        Name.Variable.Instance:    '#000000',        # class: 'vi' - to be revised
-    
-        def __ne__(self, other):
-        return not self == other
-
-    
-    '''
-requests.compat
-~~~~~~~~~~~~~~~
-    
-    :copyright: (c) 2012 by Kenneth Reitz.
-:license: Apache2, see LICENSE for more details.
-'''
-    
-        def __init__(self):
-        self.settings = None  # set in scrapy.cmdline
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-l', '--list', dest='list', action='store_true',
-                          help='only list contracts, without checking them')
-        parser.add_option('-v', '--verbose', dest='verbose', default=False, action='store_true',
-                          help='print contract tests for all spiders')
-    
-        def process_options(self, args, opts):
-        ScrapyCommand.process_options(self, args, opts)
+            credentials = {'username': None, 'password': None}
         try:
-            opts.spargs = arglist_to_dict(opts.spargs)
-        except ValueError:
-            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
-        if opts.output:
-            if opts.output == '-':
-                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
-            else:
-                self.settings.set('FEED_URI', opts.output, priority='cmdline')
-            feed_exporters = without_none_values(
-                self.settings.getwithbase('FEED_EXPORTERS'))
-            valid_output_formats = feed_exporters.keys()
-            if not opts.output_format:
-                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
-            if opts.output_format not in valid_output_formats:
-                raise UsageError('Unrecognized output format '%s', set one'
-                                 ' using the '-t' switch or as a file extension'
-                                 ' from the supported list %s' % (opts.output_format,
-                                                                  tuple(valid_output_formats)))
-            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
-    
-            # set Content-Length based len of body
-        if self.body is not None:
-            self.headers['Content-Length'] = len(self.body)
-            # just in case a broken http/1.1 decides to keep connection alive
-            self.headers.setdefault('Connection', 'close')
-        # Content-Length must be specified in POST method even with no body
-        elif self.method == b'POST':
-            self.headers['Content-Length'] = 0
-    
-        def _has_ajax_crawlable_variant(self, response):
-        '''
-        Return True if a page without hash fragment could be 'AJAX crawlable'
-        according to https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
-        '''
-        body = response.text[:self.lookup_bytes]
-        return _has_ajaxcrawlable_meta(body)
-    
-        def process_request(self, request, spider):
-        if request.meta.get('dont_merge_cookies', False):
-            return
-    
-    import josepy as jose
-import pytz
-    
-        def ensure_augeas_state(self):
-        '''Makes sure that all Augeas dom changes are written to files to avoid
-        loss of configuration directives when doing additional augeas parsing,
-        causing a possible augeas.load() resulting dom reset
-        '''
-    
-    # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-    
-    Certbot will emit a warning if it detects that the credentials file can be
-accessed by other users on your system. The warning reads 'Unsafe permissions
-on credentials configuration file', followed by the path to the credentials
-file. This warning will be emitted each time Certbot uses the credentials file,
-including for renewal, and cannot be silenced except by addressing the issue
-(e.g., by using a command like ``chmod 600`` to restrict access to the file).
+            # New style
+            plugin.raw_auth = auth['raw_auth']
+        except KeyError:
+            # Old style
+            credentials = {
+                'username': auth['username'],
+                'password': auth['password'],
+            }
+        else:
+            if plugin.auth_parse:
+                from httpie.input import parse_auth
+                parsed = parse_auth(plugin.raw_auth)
+                credentials = {
+                    'username': parsed.key,
+                    'password': parsed.value,
+                }
     
     
-def parametric_relu(x, channel_shared=False, alpha_init=constant(0.), name='parametric_relu', reuse=None):
-    '''参数化 ReLU
+_numpy_dtypes = [
+    np.dtype(dtype) for dtype in (numeric_dtypes + datetime_dtypes + string_dtypes)
+]
+_dtypes = _numpy_dtypes + extension_dtypes
     
-    
-def permute(x, perm):
-    '''
-    Examples:
-        x.shape == [128, 32, 1]
-        x = permute(x, [0, 2, 1])
-        x.shape == [128, 1, 32]
+        def time_timedelta_plus_datetime(self, df):
+        df['timedelta'] + df['datetime64']
