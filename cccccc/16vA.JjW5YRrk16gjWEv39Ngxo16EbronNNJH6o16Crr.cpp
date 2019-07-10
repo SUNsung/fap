54576cbd4c702567a4a6c6c9d474f6cebc9acbb6
SUNsung <weatherius@gmail.com>
2019-07-10 23:42:14 +0300

    Rewrite [16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp] File in: 2019-07-10 23:42:14

--- cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
+++ cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
@@ -1,244 +1,234 @@
 
         
-          /// Retrieve the array of replacement types, which line up with the
-  /// generic parameters.
-  ///
-  /// Note that the types may be null, for cases where the generic parameter
-  /// is concrete but hasn't been queried yet.
-  ArrayRef<Type> getReplacementTypes() const {
-    return llvm::makeArrayRef(getTrailingObjects<Type>(),
-                              getNumReplacementTypes());
-  }
+        
+    {}  // namespace internal
     
-      assert(capacity % 16 == 0 && 'not allocating multiple of alignment');
-    
-        StringRef Line = RawText.substr(0, Pos);
-    Lines.push_back(Line);
-    if (!IsFirstLine) {
-      size_t NonWhitespacePos = RawText.find_first_not_of(' ');
-      if (NonWhitespacePos != StringRef::npos)
-        WhitespaceToTrim =
-            std::min(WhitespaceToTrim,
-                     static_cast<unsigned>(NonWhitespacePos));
-    }
-    IsFirstLine = false;
-    
-    %{
-    }
-    
-    
-    
-    const char *Demangle::getNodeKindString(swift::Demangle::Node::Kind k) {
-  switch (k) {
-#define NODE(ID)                                                               \
-  case Node::Kind::ID:                                                         \
-    return #ID;
-#include 'swift/Demangling/DemangleNodes.def'
-  }
-  return 'Demangle::Node::Kind::???';
+    v8::Local<v8::Object> Promise::GetHandle() const {
+  return resolver_->GetPromise();
 }
     
-    #include 'ArgsToFrontendOutputsConverter.h'
-#include 'swift/AST/DiagnosticsFrontend.h'
-#include 'swift/Basic/Defer.h'
-#include 'swift/Frontend/FrontendOptions.h'
-#include 'swift/Option/Options.h'
-#include 'swift/Parse/Lexer.h'
-#include 'swift/Strings.h'
-#include 'llvm/Option/Arg.h'
-#include 'llvm/Option/ArgList.h'
-#include 'llvm/Option/Option.h'
-#include 'llvm/Support/ErrorHandling.h'
-#include 'llvm/Support/FileSystem.h'
-#include 'llvm/Support/LineIterator.h'
-#include 'llvm/Support/Path.h'
+      // Call object._init if we have one.
+  v8::Local<v8::Function> init;
+  if (Dictionary(isolate, wrapper).Get('_init', &init))
+    init->Call(isolate->GetCurrentContext(), wrapper, 0, nullptr).IsEmpty();
     
-        Transliterator::_registerFactory(UnicodeString(TRUE, ::CURR_ID, -1),
-                                     RemoveTransliterator_create, integerToken(0));
-    
-        UBool numeric = settings->isNumeric();
-    if(equalPrefixLength > 0) {
-        if((equalPrefixLength != leftLength &&
-                    data->isUnsafeBackward(left[equalPrefixLength], numeric)) ||
-                (equalPrefixLength != rightLength &&
-                    data->isUnsafeBackward(right[equalPrefixLength], numeric))) {
-            // Identical prefix: Back up to the start of a contraction or reordering sequence.
-            while(--equalPrefixLength > 0 &&
-                    data->isUnsafeBackward(left[equalPrefixLength], numeric)) {}
-        }
-        // Notes:
-        // - A longer string can compare equal to a prefix of it if only ignorables follow.
-        // - With a backward level, a longer string can compare less-than a prefix of it.
-    }
-    
-    ScientificNumberFormatter *ScientificNumberFormatter::createMarkupInstance(
-        const Locale &locale,
-        const UnicodeString &beginMarkup,
-        const UnicodeString &endMarkup,
-        UErrorCode &status) {
-    return createInstance(
-            static_cast<DecimalFormat *>(
-                    DecimalFormat::createScientificInstance(locale, status)),
-            new MarkupStyle(beginMarkup, endMarkup),
-            status);
+    v8::Local<v8::Value> Converter<v8::Local<v8::Object>>::ToV8(
+    v8::Isolate* isolate,
+    v8::Local<v8::Object> val) {
+  return val;
 }
     
-    static const UChar SELECT_KEYWORD_OTHER[] = {LOW_O, LOW_T, LOW_H, LOW_E, LOW_R, 0};
     
-    #endif
-
+    {  DISALLOW_COPY_AND_ASSIGN(Wrappable);
+};
     
-    // Called by TimeZone::createDefault(), then clone() inside a Mutex - be careful.
-SimpleTimeZone &
-SimpleTimeZone::operator=(const SimpleTimeZone &right)
+    #include 'base/bind.h'
+#include 'base/logging.h'
+#include 'base/strings/utf_string_conversions.h'
+#include 'base/task/post_task.h'
+#include 'content/public/browser/browser_context.h'
+#include 'content/public/browser/browser_task_traits.h'
+#include 'content/public/browser/browser_thread.h'
+#include 'content/public/browser/resource_context.h'
+#include 'crypto/nss_util.h'
+#include 'crypto/nss_util_internal.h'
+#include 'net/base/net_errors.h'
+#include 'net/cert/nss_cert_database.h'
+#include 'net/cert/x509_certificate.h'
+    
+      // Set/Get the badge count.
+  bool SetBadgeCount(int count);
+  int GetBadgeCount();
+    
+    void absDiff(const Size2D &size,
+             const u16 *src0Base, ptrdiff_t src0Stride,
+             const u16 *src1Base, ptrdiff_t src1Stride,
+             u16 *dstBase, ptrdiff_t dstStride)
 {
-    if (this != &right)
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    internal::vtransform(size,
+                         src0Base, src0Stride,
+                         src1Base, src1Stride,
+                         dstBase, dstStride, AbsDiff<u16>());
+#else
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
+#endif
+}
+    
+    
+    {    void operator() (const u8 * src0, const u8 * src1, u8 * dst) const
     {
-        TimeZone::operator=(right);
-        rawOffset      = right.rawOffset;
-        startMonth     = right.startMonth;
-        startDay       = right.startDay;
-        startDayOfWeek = right.startDayOfWeek;
-        startTime      = right.startTime;
-        startTimeMode  = right.startTimeMode;
-        startMode      = right.startMode;
-        endMonth       = right.endMonth;
-        endDay         = right.endDay;
-        endDayOfWeek   = right.endDayOfWeek;
-        endTime        = right.endTime;
-        endTimeMode    = right.endTimeMode;
-        endMode        = right.endMode;
-        startYear      = right.startYear;
-        dstSavings     = right.dstSavings;
-        useDaylight    = right.useDaylight;
-        clearTransitionRules();
+        dst[0] = beta * src1[0] + alpha * src0[0];
     }
-    return *this;
-}
+};
     
-    UBool
-SmallIntFormatter::canFormat(
-        int32_t positiveValue, const IntDigitCountRange &range) {
-    return (positiveValue < gMaxFastInt && range.getMin() <= 4);
-}
-    
-    int32_t StandardPlural::indexOrNegativeFromString(const char *keyword) {
-    switch (*keyword++) {
-    case 'f':
-        if (uprv_strcmp(keyword, 'ew') == 0) {
-            return FEW;
-        }
-        break;
-    case 'm':
-        if (uprv_strcmp(keyword, 'any') == 0) {
-            return MANY;
-        }
-        break;
-    case 'o':
-        if (uprv_strcmp(keyword, 'ther') == 0) {
-            return OTHER;
-        } else if (uprv_strcmp(keyword, 'ne') == 0) {
-            return ONE;
-        }
-        break;
-    case 't':
-        if (uprv_strcmp(keyword, 'wo') == 0) {
-            return TWO;
-        }
-        break;
-    case 'z':
-        if (uprv_strcmp(keyword, 'ero') == 0) {
-            return ZERO;
-        }
-        break;
-    default:
-        break;
+    namespace CAROTENE_NS {
     }
-    return -1;
+    
+        f32 alpha, beta, gamma;
+    typedef typename TypeTraits<T>::wide wtype;
+    wAdd<wtype> wideAdd;
+    wAdd(f32 _alpha, f32 _beta, f32 _gamma):
+        alpha(_alpha), beta(_beta), gamma(_gamma),
+        wideAdd(_alpha, _beta, _gamma) {}
+    
+            int32x2_t vs2 = vqneg_s32(vqadd_s32(vget_low_s32(vs), vget_high_s32(vs)));
+    
+                vec128 v_src = vld3q(src + js), v_dst;
+            v_src.val[0] = vrev64q(v_src.val[0]);
+            v_src.val[1] = vrev64q(v_src.val[1]);
+            v_src.val[2] = vrev64q(v_src.val[2]);
+    
+    # define TYPED_TEST_P(CaseName, TestName) \
+  namespace GTEST_CASE_NAMESPACE_(CaseName) { \
+  template <typename gtest_TypeParam_> \
+  class TestName : public CaseName<gtest_TypeParam_> { \
+   private: \
+    typedef CaseName<gtest_TypeParam_> TestFixture; \
+    typedef gtest_TypeParam_ TypeParam; \
+    virtual void TestBody(); \
+  }; \
+  static bool gtest_##TestName##_defined_ GTEST_ATTRIBUTE_UNUSED_ = \
+      GTEST_TYPED_TEST_CASE_P_STATE_(CaseName).AddTestName(\
+          __FILE__, __LINE__, #CaseName, #TestName); \
+  } \
+  template <typename gtest_TypeParam_> \
+  void GTEST_CASE_NAMESPACE_(CaseName)::TestName<gtest_TypeParam_>::TestBody()
+    
+    // This flag enables using colors in terminal output. Available values are
+// 'yes' to enable colors, 'no' (disable colors), or 'auto' (the default)
+// to let Google Test decide.
+GTEST_DECLARE_string_(color);
+    
+    // A function to convert T* into linked_ptr<T>
+// Doing e.g. make_linked_ptr(new FooBarBaz<type>(arg)) is a shorter notation
+// for linked_ptr<FooBarBaz<type> >(new FooBarBaz<type>(arg))
+template <typename T>
+linked_ptr<T> make_linked_ptr(T* ptr) {
+  return linked_ptr<T>(ptr);
 }
     
-        /**
-     * Limit offset, in the match text, of the <em>rightmost</em>
-     * match.
-     */
-    int32_t matchLimit;
+      // Returns true iff n is a prime number.
+  virtual bool IsPrime(int n) const = 0;
     
-        // TODO: Hide OS mouse cursor if ImGui is drawing it
-    // s3ePointerSetInt(S3E_POINTER_HIDE_CURSOR,(io.MouseDrawCursor ? 0 : 1));
+      // Try to divide n by every odd number i, starting from 3
+  for (int i = 3; ; i += 2) {
+    // We only have to try i up to the squre root of n
+    if (i > n/i) break;
+    }
     
-        // Read keyboard modifiers inputs
-    io.KeyCtrl = (::GetKeyState(VK_CONTROL) & 0x8000) != 0;
-    io.KeyShift = (::GetKeyState(VK_SHIFT) & 0x8000) != 0;
-    io.KeyAlt = (::GetKeyState(VK_MENU) & 0x8000) != 0;
-    io.KeySuper = false;
-    // io.KeysDown[], io.MousePos, io.MouseDown[], io.MouseWheel: filled by the WndProc handler below.
     
-    IMGUI_IMPL_API bool     ImGui_Marmalade_Init(bool install_callbacks);
-IMGUI_IMPL_API void     ImGui_Marmalade_Shutdown();
-IMGUI_IMPL_API void     ImGui_Marmalade_NewFrame();
-IMGUI_IMPL_API void     ImGui_Marmalade_RenderDrawData(ImDrawData* draw_data);
+    {    return new_queue;
+  }
     
-    // **DO NOT USE THIS CODE IF YOUR CODE/ENGINE IS USING MODERN OPENGL (SHADERS, VBO, VAO, etc.)**
-// **Prefer using the code in imgui_impl_opengl3.cpp**
-// This code is mostly provided as a reference to learn how ImGui integration works, because it is shorter to read.
-// If your code is using GL3+ context or any semi modern OpenGL calls, using this is likely to make everything more
-// complicated, will require your code to reset every single OpenGL attributes to their initial state, and might
-// confuse your GPU driver.
-// The GL2 code is unable to reset attributes or even call e.g. 'glUseProgram(0)' because they don't exist in that API.
+    /* QuadricCallback */
+/*      GLU_ERROR */
     
-    // Called by Init/NewFrame/Shutdown
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateFontsTexture();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyFontsTexture();
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateDeviceObjects();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyDeviceObjects();
+    
+    {  resDict->lookup('XObject', &xObjDict);
+  if (xObjDict.isDict()) {
+    for (i = 0; i < xObjDict.dictGetLength(); ++i) {
+      xObjDict.dictGetValNF(i, &xObjRef);
+      xObjDict.dictGetVal(i, &xObj);
+      if (xObj.isStream()) {
+	xObj.streamGetDict()->lookup('Subtype', &subtypeObj);
+	if (subtypeObj.isName('Form')) {
+	  if (xObjRef.isRef()) {
+	    setupForm(xObjRef.getRef(), &xObj);
+	  } else {
+	    error(-1, 'Form in resource dict is not an indirect reference');
+	  }
+	}
+	subtypeObj.free();
+      }
+      xObj.free();
+      xObjRef.free();
+    }
+  }
+  xObjDict.free();
+}
+    
+    
+    {  return gTrue;
+}
+    
+      if (!trans || !trans->isDict ()) {
+    ok = gFalse;
+    return;
+  }
+    
+      check(state->getFillColorSpace(), state->getFillColor(),
+	state->getFillOpacity(), state->getBlendMode());
+  gdi = gFalse;
+  if ((level == psLevel1 || level == psLevel1Sep) &&
+      state->getFillColorSpace()->getMode() == csPattern) {
+    level1PSBug = gTrue;
+  }
+    
+    ProfileData::ProfileData() {
+	count = 0;
+	total = 0.0;
+	min = 0.0;
+	max = 0.0;
+}
+    
+    class ProfileData {
+public:
+    }
+    
+      GooString* getContentType() { return contentType; }
+  GooString* getFileName() { return fileName; }
+    
+    //------------------------------------------------------------------------
+    
+    #ifdef _WIN32
+#include <fcntl.h> // for O_BINARY
+#include <io.h>    // for setmode
+#endif
+#include <stdio.h>
+    
+            const std::unordered_map<StreamInformation, MinibatchData>& GetNextMinibatch(
+            size_t minibatchSizeInSamples,
+            size_t minibatchSizeInSequences,
+            size_t numberOfWorkers,
+            size_t workerRank,
+            const DeviceDescriptor& device = DeviceDescriptor::UseDefaultDevice()) override;
+    
+    
+    {        const auto& type = dict[typeKey].Value<std::wstring>();
+        if (type != typeValue) 
+        {
+            const auto& version = GetVersion(dict);
+            LogicError('Unexpected '%ls':'%ls' in place of '%ls':'%ls' (%s).',
+                       typeKey.c_str(), type.c_str(), typeKey.c_str(), typeValue.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
+        }
+    }
+    
+    
+    {
+    {        mutable bool m_isPacked;
+        mutable NDArrayViewPtr m_packedData;
+        mutable std::shared_ptr<Microsoft::MSR::CNTK::MBLayout> m_packedDataLayout;
+    };
+}
 
     
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+            if (m_valueInitializer)
+            clone->SetValueInitialization(*m_valueInitializer, *m_valueInitializationDevice);
     
-    #include 'imgui.h'
-#include 'imgui_impl_dx9.h'
-#include 'imgui_impl_win32.h'
-#include <d3d9.h>
-#define DIRECTINPUT_VERSION 0x0800
-#include <dinput.h>
-#include <tchar.h>
+    #include <cassert>
+#include <string>
     
-        // Upload vertex/index data into a single contiguous GPU buffer
-    D3D11_MAPPED_SUBRESOURCE vtx_resource, idx_resource;
-    if (ctx->Map(g_pVB, 0, D3D11_MAP_WRITE_DISCARD, 0, &vtx_resource) != S_OK)
-        return;
-    if (ctx->Map(g_pIB, 0, D3D11_MAP_WRITE_DISCARD, 0, &idx_resource) != S_OK)
-        return;
-    ImDrawVert* vtx_dst = (ImDrawVert*)vtx_resource.pData;
-    ImDrawIdx* idx_dst = (ImDrawIdx*)idx_resource.pData;
-    for (int n = 0; n < draw_data->CmdListsCount; n++)
+        virtual bool OutputUsedInComputingInputNodesGradients() const override
     {
-        const ImDrawList* cmd_list = draw_data->CmdLists[n];
-        memcpy(vtx_dst, cmd_list->VtxBuffer.Data, cmd_list->VtxBuffer.Size * sizeof(ImDrawVert));
-        memcpy(idx_dst, cmd_list->IdxBuffer.Data, cmd_list->IdxBuffer.Size * sizeof(ImDrawIdx));
-        vtx_dst += cmd_list->VtxBuffer.Size;
-        idx_dst += cmd_list->IdxBuffer.Size;
-    }
-    ctx->Unmap(g_pVB, 0);
-    ctx->Unmap(g_pIB, 0);
-    
-        int64_t firstScreenCssLayoutTime;
-    
-    class RenderObject;
-    
-    #include <string>
-    
-    #endif  // CORE_RENDER_ACTION_RENDER_ACTION_CREATEBODY_H_
-
-    
-    #include 'core/render/action/render_action_interface.h'
-    
-    #include <string>
-    
-    class RenderActionRemoveElement : public RenderAction {
- public:
-  explicit RenderActionRemoveElement(const std::string &page_id,
-                                     const std::string &ref);
+        // The DiagTimesNode does not require its output value for computing
+        // the gradients of its input nodes
+        return false;
     }
