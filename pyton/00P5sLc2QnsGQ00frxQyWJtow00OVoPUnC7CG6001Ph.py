4d3cf954b5d0f67e6286b76c2642877f235b94e7
SUNsung <weatherius@gmail.com>
2019-07-11 02:49:58 +0300

    Rewrite [00P5sLc2QnsGQ00frxQyWJtow00OVoPUnC7CG6001Ph.py] File in: 2019-07-11 02:49:57

--- pyton/00P5sLc2QnsGQ00frxQyWJtow00OVoPUnC7CG6001Ph.py
+++ pyton/00P5sLc2QnsGQ00frxQyWJtow00OVoPUnC7CG6001Ph.py
@@ -1,64 +1,199 @@
 
         
         
-def _get_most_recent_snapshot(snapshots, max_snapshot_age_secs=None, now=None):
+class AddRequest(object):
+    
+        def park_vehicle(self, vehicle):
+        for level in self.levels:
+            if level.park_vehicle(vehicle):
+                return True
+        return False
+    
+            (2016-01, url0), 1
+        (2016-01, url0), 1
+        (2016-01, url1), 1
+        '''
+        url = self.extract_url(line)
+        period = self.extract_year_month(line)
+        yield (period, url), 1
+    
+        def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
+    
+        def crawl(self):
+        while True:
+            page = self.data_store.extract_max_priority_page()
+            if page is None:
+                break
+            if self.data_store.crawled_similar(page.signature):
+                self.data_store.reduce_priority_link_to_crawl(page.url)
+            else:
+                self.crawl_page(page)
+            page = self.data_store.extract_max_priority_page()
+
+    
+        def test_delete___dict__(self):
+        try:
+            del self.b.__dict__
+        except TypeError:
+            pass
+        else:
+            self.fail('deleting function dictionary should raise TypeError')
+    
+            You normally don't need to override this method; see the class
+        __doc__ string for information on how to handle specific HTTP
+        commands such as GET and POST.
+    
+        Note that not all entries are resources.  Specifically, directories are
+    not considered resources.  Use `is_resource()` on each entry returned here
+    to check if it is a resource or not.
     '''
-    Gets the most recently created snapshot and optionally filters the result
-    if the snapshot is too old
-    :param snapshots: list of snapshots to search
-    :param max_snapshot_age_secs: filter the result if its older than this
-    :param now: simulate time -- used for unit testing
-    :return:
+    package = _get_package(package)
+    reader = _get_resource_reader(package)
+    if reader is not None:
+        return reader.contents()
+    # Is the package a namespace package?  By definition, namespace packages
+    # cannot have resources.  We could use _check_location() and catch the
+    # exception, but that's extra work, so just inline the check.
+    elif package.__spec__.origin is None or not package.__spec__.has_location:
+        return ()
+    else:
+        package_directory = Path(package.__spec__.origin).parent
+        return os.listdir(package_directory)
+
+    
+        # Submit tasks
+    for task in TASKS1:
+        task_queue.put(task)
+    
+    con.commit()
+    
+        def setUp(self):
+        from acme.errors import MissingNonce
+        self.response = mock.MagicMock(headers={})
+        self.response.request.method = 'FOO'
+        self.error = MissingNonce(self.response)
+    
+    # Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
+    
+        return getattr(command_class, command)
+    
+    
+def update_init_py_version(version):
+    path = os.path.join(REPO_ROOT, 'compose', '__init__.py')
+    with open(path, 'r') as f:
+        contents = f.read()
+    contents = re.sub(r'__version__ = '[0-9a-z.-]+'', '__version__ = '{}''.format(version), contents)
+    with open(path, 'w') as f:
+        f.write(contents)
+    
+        @pytest.mark.skipif(not IS_WINDOWS_PLATFORM, reason='Needs pywin32')
+    def test_windows_pipe_error_encoding_issue(self, mock_logging):
+        import pywintypes
+        with pytest.raises(errors.ConnectionError):
+            with handle_connection_errors(mock.Mock(api_version='1.22')):
+                raise pywintypes.error(9999, 'WriteFile', 'I use weird characters \xe9')
+    
+            sorted_services = sort_service_dicts(services)
+        assert len(sorted_services) == 3
+        assert sorted_services[0]['name'] == 'child'
+        assert sorted_services[1]['name'] == 'parent'
+        assert sorted_services[2]['name'] == 'grandparent'
+    
+        def open_file(self, path):
+        if path.startswith('+'):
+            path = path[1:]
+            mode = 'ab'
+        else:
+            mode = 'wb'
+        path = os.path.expanduser(path)
+        return open(path, mode)
+    
+    # These get over-ridden by the save execution context. Keep them around so we
+# can log directly.
+realstdout = sys.stdout
+realstderr = sys.stderr
+    
+        def collect_commands(self, addon):
+        for i in dir(addon):
+            if not i.startswith('__'):
+                o = getattr(addon, i)
+                try:
+                    is_command = hasattr(o, 'command_path')
+                except Exception:
+                    pass  # hasattr may raise if o implements __getattr__.
+                else:
+                    if is_command:
+                        try:
+                            self.add(o.command_path, o)
+                        except exceptions.CommandError as e:
+                            self.master.log.warn(
+                                'Could not load command %s: %s' % (o.command_path, e)
+                            )
+    
+            self.blocks = []
+        while True:
+            _ = self._root.Block(self._io, self, self._root)
+            self.blocks.append(_)
+            if  ((self._io.is_eof()) or (_.block_type == self._root.BlockType.end_of_file)) :
+                break
+    
+        def dequeueString(self, length=None):
+        if ( length != None):
+            currentByte = 0x00
+            strReturn = ''
+            for i in range(0, length):
+                # TODO: Improve this handling. We are technically UTF-8, meaning
+                # that characters could be more than one byte long. This will fail if we have
+                # characters outside of the US-ASCII range
+                if ( self.qsize() == 0 ):
+                    break
+                currentByte = self.dequeueAndLog()
+                strReturn += chr(currentByte)
+    
+        def send(self, msg, force=False):
+        if self.state not in {'start', 'taken'}:
+            raise exceptions.ControlException(
+                'Reply is {}, but expected it to be start or taken.'.format(self.state)
+            )
+        if self.has_message and not force:
+            raise exceptions.ControlException('There is already a reply message.')
+        self.value = msg
+    
+    For x = 11:
+[1, 1, 1, 1, 1]
+ 3  5  7  9  11  # 11 is odd, it's included in the list
+    
+    
+# Python solution with only one table for B (~196ms):
+def multiply(self, a, b):
     '''
-    if len(snapshots) == 0:
-        return None
-    
-    EXAMPLES = '''
-# Add or change a subnet group
-- elasticache_subnet_group:
-    state: present
-    name: norwegian-blue
-    description: My Fancy Ex Parrot Subnet Group
-    subnets:
-      - subnet-aaaaaaaa
-      - subnet-bbbbbbbb
-    
-    
-def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            auth_token=dict(
-                type='str',
-                default=os.environ.get('ONEANDONE_AUTH_TOKEN')),
-            api_url=dict(
-                type='str',
-                default=os.environ.get('ONEANDONE_API_URL')),
-            name=dict(type='str'),
-            firewall_policy=dict(type='str'),
-            description=dict(type='str'),
-            rules=dict(type='list', default=[]),
-            add_server_ips=dict(type='list', default=[]),
-            remove_server_ips=dict(type='list', default=[]),
-            add_rules=dict(type='list', default=[]),
-            remove_rules=dict(type='list', default=[]),
-            wait=dict(type='bool', default=True),
-            wait_timeout=dict(type='int', default=600),
-            wait_interval=dict(type='int', default=5),
-            state=dict(type='str', default='present', choices=['present', 'absent', 'update']),
-        ),
-        supports_check_mode=True
-    )
-    
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-    
-        params = {}
-    params['revision_id'] = revision_id
-    params['deployed_by'] = deployed_by
-    if deployed_to:
-        params['deployed_to'] = deployed_to
-    if repository:
-        params['repository'] = repository
-    
-            if not conditionals:
-            break
+    :type A: List[List[int]]
+    :type B: List[List[int]]
+    :rtype: List[List[int]]
+    '''
+    if a is None or b is None: return None
+    m, n, l = len(a), len(a[0]), len(b[0])
+    if len(b) != n:
+        raise Exception('A's column number must be equal to B's row number.')
+    c = [[0 for _ in range(l)] for _ in range(m)]
+    table_b = {}
+    for k, row in enumerate(b):
+        table_b[k] = {}
+        for j, eleB in enumerate(row):
+            if eleB: table_b[k][j] = eleB
+    for i, row in enumerate(a):
+        for k, eleA in enumerate(row):
+            if eleA:
+                for j, eleB in table_b[k].iteritems():
+                    c[i][j] += eleA * eleB
+    return c
