dfd5d7c2ed02a0796f404b14be0da5acc7ac5b57
SUNsung <weatherius@gmail.com>
2019-07-10 12:50:22 +0300

    Rewrite [51XMLvMFixebE51v94uxOzh7.651Ehks1A2.qM251Cnz.cpp] File in: 2019-07-10 12:50:21

--- cccccc/51XMLvMFixebE51v94uxOzh7.651Ehks1A2.qM251Cnz.cpp
+++ cccccc/51XMLvMFixebE51v94uxOzh7.651Ehks1A2.qM251Cnz.cpp
@@ -1,222 +1,271 @@
 
         
-        /**
- * @brief Applies common transformations to the input data, such as
- * scaling, mirroring, substracting the image mean...
- */
-template <typename Dtype>
-class DataTransformer {
- public:
-  explicit DataTransformer(const TransformationParameter& param, Phase phase);
-  virtual ~DataTransformer() {}
+        IPC_MESSAGE_ROUTED3(ShellViewHostMsg_Call_Static_Method,
+                    std::string /* type name */,
+                    std::string /* method name */,
+                    base::ListValue /* arguments */)
+    
+        base::FilePath shortcutPath(path);
+    result->AppendBoolean(base::win::CreateOrUpdateShortcutLink(shortcutPath, props, 
+      base::PathExists(shortcutPath) ? base::win::SHORTCUT_UPDATE_EXISTING : base::win::SHORTCUT_CREATE_ALWAYS));
+#else
+    result->AppendBoolean(false);
+#endif
+    return;
+  } else if (method == 'GetPackage') {
+    result->AppendString(shell->GetPackage()->package_string());
+    return;
+  } else if (method == 'SetCrashDumpDir') {
+    std::string path;
+    arguments.GetString(0, &path);
+    //FIXME: result->AppendBoolean(SetCrashDumpPath(path.c_str()));
+    return;
+  } else if (method == 'RegisterGlobalHotKey') {
+    int object_id = -1;
+    arguments.GetInteger(0, &object_id);
+    Shortcut* shortcut =
+        static_cast<Shortcut*>(DispatcherHost::GetApiObject(object_id));
+    bool success = GlobalShortcutListener::GetInstance()->RegisterAccelerator(
+                       shortcut->GetAccelerator(), shortcut);
+    if (!success)
+      shortcut->OnFailed('Register global desktop keyboard shortcut failed.');
+    
+    #include 'base/memory/weak_ptr.h'
+    
+    class EventListener : public Base {
+  std::map<int, BaseEvent*> listerners_;
     }
     
     
-    { protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
-   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
-   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
-   *      the computed outputs @f$
-   *       y_n = \arg\max\limits_i x_{ni}
-   *      @f$ (for @f$ K = 1 @f$).
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  /// @brief Not implemented (non-differentiable function)
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-    NOT_IMPLEMENTED;
+bool MenuDelegate::GetAcceleratorForCommandId(
+      int command_id,
+      ui::Accelerator* accelerator) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+    }
+    
+    class NwAppClearAppCacheFunction : public NWSyncExtensionFunction {
+ public:
+  NwAppClearAppCacheFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+          if (data.type == TYPE_PNG &&
+         !gfx::PNGCodec::EncodeBGRASkBitmap(bitmap, false, &encoded_image)) {
+        LOG(INFO) << 'NwClipboardGetSyncFunction::RunSync(' << nwapi::nw__clipboard::ToString(data.type) << ') failed when converting to PNG';
+        error_ = 'Failed to encode as PNG';
+        return false;
+      } else if (data.type == TYPE_JPEG &&
+                 !gfx::JPEGCodec::Encode(bitmap, kQuality, &encoded_image)) {
+        LOG(INFO) << 'NwClipboardGetSyncFunction::RunSync(' << nwapi::nw__clipboard::ToString(data.type) << ') failed when converting to JPEG';
+        error_ = 'Failed to encode as JPEG';
+        return false;
+      }
+    
+    
+    {  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
+  manager->OnAllocateObject(id, type, *options, extension_id());
+  return true;
+}
+    
+    
+    {  std::vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'SubGradient',
+        '',
+        std::vector<std::string>{GO(0), I(0), I(1)},
+        std::vector<std::string>{GI(0), GI(1)});
   }
-  bool out_max_val_;
-  size_t top_k_;
-  bool has_axis_;
-  int axis_;
 };
     
-    #include <utility>
-#include <vector>
+    REGISTER_CPU_OPERATOR(
+    MergeSingleMapFeatureTensors,
+    MergeSingleMapFeatureTensorsOp<CPUContext>);
+OPERATOR_SCHEMA(MergeSingleMapFeatureTensors)
+    .SetDoc(
+        'Merge given single-feature tensors with map features into one '
+        'multi-feature tensor.' +
+        doc)
+    .NumInputs([](int n) { return n >= 4 && n % 4 == 0; })
+    .NumOutputs(5)
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_keys', '.keys')
+    .Input(2, 'in1_values', '.values')
+    .Input(3, 'in1_presence', '.presence')
+    .Output(0, 'out_lengths', '.lengths')
+    .Output(1, 'out_keys', '.keys')
+    .Output(2, 'out_values_lengths', '.values.lengths')
+    .Output(3, 'out_values_keys', '.values.keys')
+    .Output(4, 'out_values_values', '.values.values')
+    .Arg('feature_ids', 'feature ids');
     
-    #endif  // CAFFE_BNLL_LAYER_HPP_
-
+    OPERATOR_SCHEMA(GatherRangesToDense)
+    .NumInputs(2, 3)
+    .NumOutputs(1, INT_MAX)
+    .SetDoc(R'DOC(
+Given DATA tensor of rank 1, and RANGES tensor of rank 3, gather values
+corresponding to each range into a separate output tensor. If the optional input
+KEY tensor is also given, the output will be sorted by KEY for each example.
     
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
+    OPERATOR_SCHEMA(Glu)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .SetDoc(R'DOC(
+Applies gated linear unit to the input Tensor X. The output Y is half the size
+of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
+[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
+X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
+)DOC')
+    .Input(0, 'X', '1D input tensor')
+    .Output(0, 'Y', '1D output tensor');
     
-    #include <vector>
+    #include 'caffe2/core/context.h'
+#include 'caffe2/core/logging.h'
+#include 'caffe2/core/operator.h'
+#include 'caffe2/utils/math.h'
     
-    // Values() allows generating tests from explicitly specified list of
-// parameters.
-//
-// Synopsis:
-// Values(T v1, T v2, ..., T vN)
-//   - returns a generator producing sequences with elements v1, v2, ..., vN.
-//
-// For example, this instantiates tests from test case BarTest each
-// with values 'one', 'two', and 'three':
-//
-// INSTANTIATE_TEST_CASE_P(NumSequence, BarTest, Values('one', 'two', 'three'));
-//
-// This instantiates tests from test case BazTest each with values 1, 2, 3.5.
-// The exact type of values will depend on the type of parameter in BazTest.
-//
-// INSTANTIATE_TEST_CASE_P(FloatingNumbers, BazTest, Values(1, 2, 3.5));
-//
-// Currently, Values() supports from 1 to 50 parameters.
-//
-template <typename T1>
-internal::ValueArray1<T1> Values(T1 v1) {
-  return internal::ValueArray1<T1>(v1);
+    REGISTER_CUDA_OPERATOR(LC1D, LocallyConnectedOp<float, CUDAContext>);
+REGISTER_CUDA_OPERATOR(
+    LC1DGradient,
+    LocallyConnectedGradientOp<float, CUDAContext>);
+    
+            friend bool operator==(Rational const& lhs, Rational const& rhs);
+        friend bool operator!=(Rational const& lhs, Rational const& rhs);
+        friend bool operator<(Rational const& lhs, Rational const& rhs);
+        friend bool operator>(Rational const& lhs, Rational const& rhs);
+        friend bool operator<=(Rational const& lhs, Rational const& rhs);
+        friend bool operator>=(Rational const& lhs, Rational const& rhs);
+    
+            explicit Number(PNUMBER p) noexcept;
+        PNUMBER ToPNUMBER() const;
+    
+    
+    {
+    {                    iDenominatorSign = -1;
+                }
+            }
+    
+        enum class Command
+    {
+        // Commands for programmer calculators are omitted.
+        CommandDEG = 321,
+        CommandRAD = 322,
+        CommandGRAD = 323,
+        CommandDegrees = 324,
+        CommandHYP = 325,
+    }
+    
+    {
+    PRAT fact = nullptr;
+    PRAT frac = nullptr;
+    PRAT neg_rat_one = nullptr;
+    }
+    
+    void DHTRoutingTable::showBuckets() const
+{
+  /*
+    for(std::deque<std::shared_ptr<DHTBucket> >::const_iterator itr =
+    buckets_.begin(); itr != buckets_.end(); ++itr) {
+    cerr << 'prefix = ' << (*itr)->getPrefixLength() << ', '
+    << 'nodes = ' << (*itr)->countNode() << endl;
+    }
+  */
 }
     
-      const InterceptMode intercept_mode_;
-  TestPartResultReporterInterface* old_reporter_;
-  TestPartResultArray* const result_;
+      void moveBucketTail(const std::shared_ptr<DHTNode>& node);
     
+        DHTRoutingTableDeserializer deserializer(family);
+    const std::string& dhtFile = e->getOption()->get(
+        family == AF_INET ? PREF_DHT_FILE_PATH : PREF_DHT_FILE_PATH6);
+    try {
+      deserializer.deserialize(dhtFile);
+      localNode = deserializer.getLocalNode();
+    }
+    catch (RecoverableException& e) {
+      A2_LOG_ERROR_EX(
+          fmt('Exception caught while loading DHT routing table from %s',
+              dhtFile.c_str()),
+          e);
+    }
+    if (!localNode) {
+      localNode = std::make_shared<DHTNode>();
+    }
     
-    {  return AssertionFailure() << pred_text << '('
-                            << e1 << ', '
-                            << e2 << ') evaluates to false, where'
-                            << '\n' << e1 << ' evaluates to ' << v1
-                            << '\n' << e2 << ' evaluates to ' << v2;
+    OrbitCamera::OrbitCamera()
+: _radius(0.0)
+, _deltaRadius(0.0)
+, _angleZ(0.0)
+, _deltaAngleZ(0.0)
+, _angleX(0.0)
+, _deltaAngleX(0.0)
+, _radZ(0.0)
+, _radDeltaZ(0.0)
+, _radX(0.0)
+, _radDeltaX(0.0)
+{
+}
+OrbitCamera::~OrbitCamera()
+{
 }
     
-    // Gets the content of the stringstream's buffer as an std::string.  Each '\0'
-// character in the buffer is replaced with '\\0'.
-GTEST_API_ std::string StringStreamToString(::std::stringstream* stream);
+    NS_CC_END
     
-      // The default c'tor constructs a NULL string.
-  MyString() : c_string_(NULL) {}
     
-    AuthPropertyIterator::AuthPropertyIterator(
-    const grpc_auth_property* property, const grpc_auth_property_iterator* iter)
-    : property_(property),
-      ctx_(iter->ctx),
-      index_(iter->index),
-      name_(iter->name) {}
-    
-    #include <grpc/support/port_platform.h>
-    
-    void CensusClientCallData::StartTransportStreamOpBatch(
-    grpc_call_element* elem, TransportStreamOpBatch* op) {
-  if (op->send_initial_metadata() != nullptr) {
-    census_context* ctxt = op->get_census_context();
-    GenerateClientContext(
-        qualified_method_, &context_,
-        (ctxt == nullptr) ? nullptr : reinterpret_cast<CensusContext*>(ctxt));
-    size_t tracing_len = TraceContextSerialize(context_.Context(), tracing_buf_,
-                                               kMaxTraceContextLen);
-    if (tracing_len > 0) {
-      GRPC_LOG_IF_ERROR(
-          'census grpc_filter',
-          grpc_metadata_batch_add_tail(
-              op->send_initial_metadata()->batch(), &tracing_bin_,
-              grpc_mdelem_from_slices(
-                  GRPC_MDSTR_GRPC_TRACE_BIN,
-                  grpc_slice_from_copied_buffer(tracing_buf_, tracing_len))));
-    }
-    grpc_slice tags = grpc_empty_slice();
-    // TODO: Add in tagging serialization.
-    size_t encoded_tags_len = StatsContextSerialize(kMaxTagsLen, &tags);
-    if (encoded_tags_len > 0) {
-      GRPC_LOG_IF_ERROR(
-          'census grpc_filter',
-          grpc_metadata_batch_add_tail(
-              op->send_initial_metadata()->batch(), &stats_bin_,
-              grpc_mdelem_from_slices(GRPC_MDSTR_GRPC_TAGS_BIN, tags)));
-    }
-  }
-    }
-    
-    #endif /* GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_MEASURES_H */
-
-    
-    // A CallData class will be created for every grpc call within a channel. It is
-// used to store data and methods specific to that call. CensusServerCallData is
-// thread-compatible, however typically only 1 thread should be interacting with
-// a call at a time.
-class CensusServerCallData : public CallData {
- public:
-  // Maximum size of server stats that are sent on the wire.
-  static constexpr uint32_t kMaxServerStatsLen = 16;
-    }
-    
-    Status ProtoServerReflection::GetFileContainingSymbol(
-    ServerContext* context, const grpc::string& symbol,
-    ServerReflectionResponse* response) {
-  if (descriptor_pool_ == nullptr) {
-    return Status::CANCELLED;
-  }
-    }
-    
-    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
-    const grpc::string& name, int value) {
-  class IntOption final : public ServerBuilderOption {
-   public:
-    IntOption(const grpc::string& name, int value)
-        : name_(name), value_(value) {}
-    }
-    }
-    
-    #include <grpc/support/port_platform.h>
-    
-    void ProtoToCoreStats(const grpc::core::Stats& proto, grpc_stats_data* core) {
-  memset(core, 0, sizeof(*core));
-  for (const auto& m : proto.metrics()) {
-    switch (m.value_case()) {
-      case Metric::VALUE_NOT_SET:
-        break;
-      case Metric::kCount:
-        for (int i = 0; i < GRPC_STATS_COUNTER_COUNT; i++) {
-          if (m.name() == grpc_stats_counter_name[i]) {
-            core->counters[i] = m.count();
-            break;
-          }
-        }
-        break;
-      case Metric::kHistogram:
-        for (int i = 0; i < GRPC_STATS_HISTOGRAM_COUNT; i++) {
-          if (m.name() == grpc_stats_histogram_name[i]) {
-            const auto& h = m.histogram();
-            bool valid = true;
-            if (grpc_stats_histo_buckets[i] != h.buckets_size()) valid = false;
-            for (int j = 0; valid && j < h.buckets_size(); j++) {
-              if (grpc_stats_histo_bucket_boundaries[i][j] !=
-                  h.buckets(j).start()) {
-                valid = false;
-              }
-            }
-            if (!valid) {
-              gpr_log(GPR_ERROR,
-                      'Found histogram %s but shape is different from proto',
-                      m.name().c_str());
-            }
-            for (int j = 0; valid && j < h.buckets_size(); j++) {
-              core->histograms[grpc_stats_histo_start[i] + j] =
-                  h.buckets(j).count();
-            }
-          }
-        }
-        break;
-    }
-  }
+    {    a->autorelease();
+    return a;
 }
     
-    int ClusterQualityInfo702::longitude_dist_rms(const std::uint8_t* bytes,
-                                              int32_t length) const {
-  Byte t0(bytes + 1);
-  int32_t x = t0.get_byte(3, 5);
+        /** Removes an action given its tag and the target.
+     *
+     * @param tag       The action's tag.
+     * @param target    A certain target.
+     */
+    virtual void removeActionByTag(int tag, Node *target);
+    
+    /** Removes all actions given its tag and the target.
+     *
+     * @param tag       The actions' tag.
+     * @param target    A certain target.
+     * @js NA
+     */
+    virtual void removeAllActionsByTag(int tag, Node *target);
+    
+    class StencilStateManager;
+/**
+ *  @addtogroup _2d
+ *  @{
+ */
+/** ClippingNode is a subclass of Node.
+ * It draws its content (children) clipped using a stencil.
+ * The stencil is an other Node that will not be drawn.
+ * The clipping is done using the alpha part of the stencil (adjusted with an alphaThreshold).
+ */
+class CC_DLL ClippingNode : public Node
+{
+public:
+    /** Creates and initializes a clipping node without a stencil.
+     *
+     * @return An autorelease ClippingNode.
+     */
+    static ClippingNode* create();
+    
+    /** Creates and initializes a clipping node with an other node as its stencil.
+     * The stencil node will be retained.
+     * @param stencil The stencil node.
+     */
+    static ClippingNode* create(Node *stencil);
+    }
     }
     
-      x <<= 3;
-  x |= t;
+    void ClippingRectangleNode::setClippingRegion(const Rect &clippingRegion)
+{
+    _clippingRegion = clippingRegion;
+}
     
-      Byte t1(bytes + 3);
-  int32_t t = t1.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
+    The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+    
+    #include <string>
+#include 'base/ccTypes.h'
+#include 'base/ccUTF8.h'
