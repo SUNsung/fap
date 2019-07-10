acdc0e05835a5c9ab6fee64f1412a48a8f8984a6
SUNsung <weatherius@gmail.com>
2019-07-10 18:53:38 +0300

    Rewrite [34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp] File in: 2019-07-10 18:53:38

--- cccccc/34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp
+++ cccccc/34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp
@@ -1,202 +1,309 @@
 
         
-        
-    {}  // namespace grpc
+            http://www.apache.org/licenses/LICENSE-2.0
     
-     private:
-  Status ListService(ServerContext* context,
-                     reflection::v1alpha::ListServiceResponse* response);
+    string TryFindKernelClass(const string& serialized_node_def) {
+  tensorflow::NodeDef node_def;
+  if (!node_def.ParseFromString(serialized_node_def)) {
+    LOG(WARNING) << 'Error parsing node_def';
+    return '';
+  }
+    }
+    
+    #include 'tensorflow/core/framework/graph.pb.h'
+#include 'tensorflow/core/platform/types.h'
+    
+      // Begins the timer at the present point in the stream.
+  bool Start(Stream *stream);
+    
+    v8::Local<v8::Value> Arguments::ThrowTypeError(
+    const std::string& message) const {
+  isolate_->ThrowException(
+      v8::Exception::TypeError(StringToV8(isolate_, message)));
+  return v8::Undefined(isolate_);
+}
     
     
     {}  // namespace
     
+    // This specialization allows people to construct function templates directly if
+// they need to do fancier stuff.
+template <>
+struct CallbackTraits<v8::Local<v8::FunctionTemplate>> {
+  static v8::Local<v8::FunctionTemplate> CreateTemplate(
+      v8::Local<v8::FunctionTemplate> templ) {
+    return templ;
+  }
+};
     
-    {
-    {}  // namespace load_reporter
-}  // namespace grpc
-    
-    
-    {}  // namespace grpc
+    #endif  // NATIVE_MATE_NATIVE_MATE_PROMISE_H_
 
     
-    #include 'src/proto/grpc/core/stats.pb.h'
+      // Retrieve the v8 wrapper object cooresponding to this object.
+  v8::Local<v8::Object> GetWrapper() const;
+  v8::MaybeLocal<v8::Object> GetWrapper(v8::Isolate* isolate) const;
     
-            // (start, end) values in the current window to be reported.
-        std::pair<double, double> m_loss;
-        std::pair<double, double> m_metric;
-        std::pair<size_t, size_t> m_samples;
-        std::pair<size_t, size_t> m_updates;
-    
-            bool result = (!m_distributed) ?
-            TrainLocalMinibatch(GetInputs(arguments), outputsToFetch, IsAtSweepEnd(arguments), computeDevice) :
-            TrainDistributedMinibatch(GetInputs(arguments), outputsToFetch, IsAtSweepEnd(arguments), computeDevice);
-    
-                if ((oldFilterRank != SentinelValueForInferParamInitRank) && (oldFilterRank != filterRank))
-                InvalidArgument('Filer rank of a non-uniform random initialier cannot be overridden if it has been already specified!');
-    
-    #pragma once
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS // 'secure' CRT not available on all platforms  --add this at the top of all CPP files that give 'function or variable may be unsafe' warnings
-#endif
-#ifdef _WIN32
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif // NOMINMAX
-#pragma comment(lib, 'Dbghelp.lib')
-#else
-#include <execinfo.h>
-#include <cxxabi.h>
+    #if defined(OS_POSIX) && !defined(OS_ANDROID)
+  // Exposed for testing.  We use a timeout on Linux, and in tests we want
+  // this timeout to be short.
+  NotifyResult NotifyOtherProcessWithTimeout(
+      const base::CommandLine& command_line,
+      int retry_attempts,
+      const base::TimeDelta& timeout,
+      bool kill_unresponsive);
+  NotifyResult NotifyOtherProcessWithTimeoutOrCreate(
+      const base::CommandLine& command_line,
+      int retry_attempts,
+      const base::TimeDelta& timeout);
+  void OverrideCurrentPidForTesting(base::ProcessId pid);
+  void OverrideKillCallbackForTesting(
+      const base::RepeatingCallback<void(int)>& callback);
 #endif
     
+    #if defined(OS_MACOSX)
+bool ReplaceOldSingletonLock(const base::FilePath& symlink_content,
+                             const base::FilePath& lock_path) {
+  // Try taking an flock(2) on the file. Failure means the lock is taken so we
+  // should quit.
+  base::ScopedFD lock_fd(HANDLE_EINTR(
+      open(lock_path.value().c_str(), O_RDWR | O_CREAT | O_SYMLINK, 0644)));
+  if (!lock_fd.is_valid()) {
+    PLOG(ERROR) << 'Could not open singleton lock';
+    return false;
+  }
+    }
     
+    
+    {  content::ContentMainParams params(&delegate);
+  params.instance = instance;
+  params.sandbox_info = &sandbox_info;
+  electron::AtomCommandLine::Init(arguments.argc, arguments.argv);
+  return content::ContentMain(params);
+}
+    
+    #define TEGRA_MERGE(src, dst, len, cn) \
+( \
+    CAROTENE_NS::isSupportedConfiguration() ? \
+        cn == 2 ? \
+        CAROTENE_NS::combine2(CAROTENE_NS::Size2D(len, 1), \
+                              src[0], len, \
+                              src[1], len, \
+                              dst, len), \
+        CV_HAL_ERROR_OK : \
+        cn == 3 ? \
+        CAROTENE_NS::combine3(CAROTENE_NS::Size2D(len, 1), \
+                              src[0], len, \
+                              src[1], len, \
+                              src[2], len, \
+                              dst, len), \
+        CV_HAL_ERROR_OK : \
+        cn == 4 ? \
+        CAROTENE_NS::combine4(CAROTENE_NS::Size2D(len, 1), \
+                              src[0], len, \
+                              src[1], len, \
+                              src[2], len, \
+                              src[3], len, \
+                              dst, len), \
+        CV_HAL_ERROR_OK : \
+        CV_HAL_ERROR_NOT_IMPLEMENTED \
+    : CV_HAL_ERROR_NOT_IMPLEMENTED \
+)
+    
+        void operator() (const typename VecTraits<T>::vec128 & v_src0,
+                     const typename VecTraits<T>::vec128 & v_src1,
+                     typename VecTraits<T>::vec128 & v_dst) const
     {
-    {            if (map.size() > RAND_MAX * (size_t) RAND_MAX)
-                RuntimeError('RandomOrdering: too large training set: need to change to different random generator!');
-            srand((unsigned int) seed);
-            size_t retries = 0;
-            foreach_index (t, map)
-            {
-                for (int tries = 0; tries < 5; tries++)
+        typename VecTraits<wtype>::vec128 vrl, vrh;
+        wideAdd(vmovl( vget_low(v_src0)), vmovl( vget_low(v_src1)), vrl);
+        wideAdd(vmovl(vget_high(v_src0)), vmovl(vget_high(v_src1)), vrh);
+    }
+    
+    void bitwiseNot(const Size2D &size,
+                const u8 *srcBase, ptrdiff_t srcStride,
+                u8 *dstBase, ptrdiff_t dstStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
+    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+    }
+    
+                int16x8_t vabsx = vabsq_s16(vx);
+            int16x8_t vabsy = vabsq_s16(vy);
+    
+    #include 'common.hpp'
+    
+                    if( mask[0] == 0 )
                 {
-                    // swap current pos with a random position
-                    // Random positions are limited to t+randomizationrange.
-                    // This ensures some locality suitable for paging with a sliding window.
-                    const size_t tbegin = max((size_t) t, randomizationrange / 2) - randomizationrange / 2; // range of window  --TODO: use bounds() function above
-                    const size_t tend = min(t + randomizationrange / 2, map.size());
-                    assert(tend >= tbegin);                  // (guard against potential numeric-wraparound bug)
-                    const size_t trand = rand(tbegin, tend); // random number within windows
-                    assert((size_t) t <= trand + randomizationrange / 2 && trand < (size_t) t + randomizationrange / 2);
-                    // if range condition is fulfilled then swap
-                    if (trand <= map[t] + randomizationrange / 2 && map[t] < trand + randomizationrange / 2 && (size_t) t <= map[trand] + randomizationrange / 2 && map[trand] < (size_t) t + randomizationrange / 2)
+                    if (mask[1] != 0)
                     {
-                        std::swap(map[t], map[trand]);
-                        break;
+                        j -= 8;
+                        ptr -= 8;
                     }
-                    // but don't multi-swap stuff out of its range (for swapping positions that have been swapped before)
-                    // instead, try again with a different random number
-                    retries++;
+                    continue;
                 }
-            }
-            fprintf(stderr, 'RandomOrdering: %lu retries for %lu elements (%.1f%%) to ensure window condition\n', (unsigned long) retries, (unsigned long) map.size(), 100.0 * retries / map.size());
-            // ensure the window condition
-            foreach_index (t, map)
-                assert((size_t) t <= map[t] + randomizationrange / 2 && map[t] < (size_t) t + randomizationrange / 2);
-#if 0 // and a live check since I don't trust myself here yet
-            foreach_index (t, map) if (!((size_t) t <= map[t] + randomizationrange/2 && map[t] < (size_t) t + randomizationrange/2))
-            {
-                fprintf (stderr, 'RandomOrdering: windowing condition violated %d -> %d\n', t, map[t]);
-                LogicError('RandomOrdering: windowing condition violated');
-            }
-#endif
-#if 0 // test whether it is indeed a unique complete sequence
-            auto map2 = map;
-            ::sort (map2.begin(), map2.end());
-            foreach_index (t, map2) assert (map2[t] == (size_t) t);
-#endif
-            fprintf(stderr, 'RandomOrdering: recached sequence for seed %d: %d, %d, ...\n', (int) seed, (int) map[0], (int) map[1]);
-            currentseed = seed;
-        }
-        return map; // caller can now access it through operator[]
-    }
     
-            // also 'edit' all nodes that have updated *inputs*
-        // All nodes that take inputs that have been edited must have their inputs updated.
-        // Since we do not update the model in-place, we must also create replacements for these.
-        // That is achieved by recursively including all parents of edits into the set of edits.
-        let parents = net.CreateParentsMap();
-        deque<ComputationNodeBasePtr> workList; // work list for recursion
-        for (let& replacement : replacements)
-            workList.push_back(replacement.first);
-        while (!workList.empty())
-        {
-            let node = workList.front();
-            workList.pop_front();
-            // loop over the node's parents
-            for (let& parent : parents.find(node)->second)
-            {
-                // 'edit' (clone) the parent if not yet
-                if (replacements.find(parent) != replacements.end())
-                    continue; // already a known replacement
-                // we must 'edit' the parent since it depends on a replaced input
-                replacements[parent] = parent->Duplicate();
-                // and put this parent into the workList, so that we will gets its parent in turn, etc.
-                workList.push_back(parent);
-#if 0 //def _DEBUG
-                fprintf(stderr, '\t%ls = %ls() --> relink %ls\n', parent->NodeName().c_str(), parent->OperationName().c_str(), replacements[parent]->NodeName().c_str());
-#endif
-            }
-        }
-        if (TraceLevel() > 0)
-            fprintf(stderr, 'Edit: %d out of %d nodes were either edited or need to be relinked.\n', (int)replacements.size(), (int)net.GetTotalNumberOfNodes());
-        // Now the keys of replacements[] define the set of all nodes that must be relinked.
+                    int32x4_t lane0 = vld1q_s32(lane + x - 2);
+                int32x4_t lane4 = vld1q_s32(lane + x + 2);
+                int32x4_t lane1 = vld1q_s32(lane + x - 1);
+                int32x4_t lane3 = vld1q_s32(lane + x + 1);
+                int32x4_t lane2 = vld1q_s32(lane + x + 0);
     
-                auto numFrames = pMBLayout->GetNumSequenceFramesInCurrentMB(sequence);
-    
-    // -----------------------------------------------------------------------
-// LookupTableNode (embedding matrix, bag-of-word representation of the inputs)
-// Implements an embedding. The input vector can consist of multiple stacked
-// This is a tensor product where the matrix width may be an integer fraction of the features.
-// If it is, then the matrix will be replicated.
-// This is the same as if the input data were a tensor where the same matrix is applied to each column of the tensor.
-// TimesNode can do that.
-// -----------------------------------------------------------------------
-    
-    TEST(ByteTest, SetValue) {
-  unsigned char byte_value = 0x1A;
-  Byte value(&byte_value);
-  value.set_value(0x06, 3, 3);
-  EXPECT_EQ(0x32, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x06, 0, 8);
-  EXPECT_EQ(0x06, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x06, 0, 10);
-  EXPECT_EQ(0x06, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x06, 1, 7);
-  EXPECT_EQ(0x0C, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x07, 1, 1);
-  EXPECT_EQ(0x1A, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x07, -1, 1);
-  EXPECT_EQ(0x1A, value.get_byte());
+    NarratorAnnouncement ^ CalculatorAnnouncement::GetDisplayUpdatedAnnouncement(String ^ announcement)
+{
+    return ref new NarratorAnnouncement(
+        announcement, CalculatorActivityIds::DisplayUpdated, AutomationNotificationKind::Other, AutomationNotificationProcessing::ImportantMostRecent);
 }
     
+    using namespace CalculatorApp::Common::Automation;
+using namespace Platform;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Automation;
+using namespace Windows::UI::Xaml::Automation::Peers;
     
-    {  x <<= 2;
-  x |= t;
-  double ret = x * OBJECT_VREL_RES + OBJECT_VREL_LONG_MIN;
-  return ret;
-}
+      void BeforeFirst() override {
+    parser_->BeforeFirst();
+  }
     
-    void Spline1dSeg::SetSplineFunc(const PolynomialXd& spline_func) {
-  spline_func_ = spline_func;
-  derivative_ = PolynomialXd::DerivedFrom(spline_func_);
-  second_order_derivative_ = PolynomialXd::DerivedFrom(derivative_);
-  third_order_derivative_ = PolynomialXd::DerivedFrom(second_order_derivative_);
-}
-    
-    namespace apollo {
-namespace canbus {
-namespace gem {
+    /*!
+ * \brief template for all quantile sketch algorithm
+ *        that uses merge/prune scheme
+ * \tparam DType type of data content
+ * \tparam RType type of rank
+ * \tparam TSummary actual summary data structure it uses
+ */
+template<typename DType, typename RType, class TSummary>
+class QuantileSketchTemplate {
+ public:
+  /*! \brief type of summary type */
+  using Summary = TSummary;
+  /*! \brief the entry type */
+  using Entry = typename Summary::Entry;
+  /*! \brief same as summary, but use STL to backup the space */
+  struct SummaryContainer : public Summary {
+    std::vector<Entry> space;
+    SummaryContainer(const SummaryContainer &src) : Summary(nullptr, src.size) {
+      this->space = src.space;
+      this->data = dmlc::BeginPtr(this->space);
     }
+    SummaryContainer() : Summary(nullptr, 0) {
     }
+    /*! \brief reserve space for summary */
+    inline void Reserve(size_t size) {
+      if (size > space.size()) {
+        space.resize(size);
+        this->data = dmlc::BeginPtr(space);
+      }
+    }
+    /*!
+     * \brief set the space to be merge of all Summary arrays
+     * \param begin beginning position in the summary array
+     * \param end ending position in the Summary array
+     */
+    inline void SetMerge(const Summary *begin,
+                         const Summary *end) {
+      CHECK(begin < end) << 'can not set combine to empty instance';
+      size_t len = end - begin;
+      if (len == 1) {
+        this->Reserve(begin[0].size);
+        this->CopyFrom(begin[0]);
+      } else if (len == 2) {
+        this->Reserve(begin[0].size + begin[1].size);
+        this->SetMerge(begin[0], begin[1]);
+      } else {
+        // recursive merge
+        SummaryContainer lhs, rhs;
+        lhs.SetCombine(begin, begin + len / 2);
+        rhs.SetCombine(begin + len / 2, end);
+        this->Reserve(lhs.size + rhs.size);
+        this->SetCombine(lhs, rhs);
+      }
+    }
+    /*!
+     * \brief do elementwise combination of summary array
+     *        this[i] = combine(this[i], src[i]) for each i
+     * \param src the source summary
+     * \param max_nbyte maximum number of byte allowed in here
+     */
+    inline void Reduce(const Summary &src, size_t max_nbyte) {
+      this->Reserve((max_nbyte - sizeof(this->size)) / sizeof(Entry));
+      SummaryContainer temp;
+      temp.Reserve(this->size + src.size);
+      temp.SetCombine(*this, src);
+      this->SetPrune(temp, space.size());
+    }
+    /*! \brief return the number of bytes this data structure cost in serialization */
+    inline static size_t CalcMemCost(size_t nentry) {
+      return sizeof(size_t) + sizeof(Entry) * nentry;
+    }
+    /*! \brief save the data structure into stream */
+    template<typename TStream>
+    inline void Save(TStream &fo) const {  // NOLINT(*)
+      fo.Write(&(this->size), sizeof(this->size));
+      if (this->size != 0) {
+        fo.Write(this->data, this->size * sizeof(Entry));
+      }
+    }
+    /*! \brief load data structure from input stream */
+    template<typename TStream>
+    inline void Load(TStream &fi) {  // NOLINT(*)
+      CHECK_EQ(fi.Read(&this->size, sizeof(this->size)), sizeof(this->size));
+      this->Reserve(this->size);
+      if (this->size != 0) {
+        CHECK_EQ(fi.Read(this->data, this->size * sizeof(Entry)),
+                 this->size * sizeof(Entry));
+      }
+    }
+  };
+  /*!
+   * \brief initialize the quantile sketch, given the performance specification
+   * \param maxn maximum number of data points can be feed into sketch
+   * \param eps accuracy level of summary
+   */
+  inline void Init(size_t maxn, double eps) {
+    LimitSizeLevel(maxn, eps, &nlevel, &limit_size);
+    // lazy reserve the space, if there is only one value, no need to allocate space
+    inqueue.queue.resize(1);
+    inqueue.qtail = 0;
+    data.clear();
+    level.clear();
+  }
     }
     
-      Byte t3(bytes + 7);
-  t = t3.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
+      bool Read(SparsePage* page,
+            dmlc::SeekStream* fi,
+            const std::vector<bst_uint>& sorted_index_set) override {
+    if (!fi->Read(&disk_offset_)) return false;
+    this->LoadIndexValue(fi);
+    auto& offset_vec = page->offset.HostVector();
+    auto& data_vec = page->data.HostVector();
+    offset_vec.clear();
+    offset_vec.push_back(0);
+    for (bst_uint cid : sorted_index_set) {
+      offset_vec.push_back(
+          offset_vec.back() + disk_offset_[cid + 1] - disk_offset_[cid]);
+    }
+    data_vec.resize(offset_vec.back());
+    CHECK_EQ(index_.data.size(), value_.data.size());
+    CHECK_EQ(index_.data.size(), disk_offset_.back());
+    }
     
     
-    {
-    {
-    {
-    {  Brake_rpt_6c::Brake_on_offType ret =
-      static_cast<Brake_rpt_6c::Brake_on_offType>(x);
-  return ret;
-}
-}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
+    {/*!
+ * \brief Macro to register objective function.
+ *
+ * \code
+ * // example of registering a objective
+ * XGBOOST_REGISTER_OBJECTIVE(LinearRegression, 'reg:squarederror')
+ * .describe('Linear regression objective')
+ * .set_body([]() {
+ *     return new RegLossObj(LossType::kLinearSquare);
+ *   });
+ * \endcode
+ */
+#define XGBOOST_REGISTER_OBJECTIVE(UniqueId, Name)                      \
+  static DMLC_ATTRIBUTE_UNUSED ::xgboost::ObjFunctionReg &              \
+  __make_ ## ObjFunctionReg ## _ ## UniqueId ## __ =                    \
+      ::dmlc::Registry< ::xgboost::ObjFunctionReg>::Get()->__REGISTER__(Name)
+}  // namespace xgboost
+#endif  // XGBOOST_OBJECTIVE_H_
