49404a995602eed0e158b34a594909906e7d0822
SUNsung <weatherius@gmail.com>
2019-07-10 08:40:31 +0300

    Rewrite [226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp] File in: 2019-07-10 08:40:30

--- cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
+++ cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
@@ -1,136 +1,89 @@
 
         
-        
-    {  GTEST_DISALLOW_COPY_AND_ASSIGN_(TestPartResultArray);
-};
-    
-    // INTERNAL IMPLEMENTATION - DO NOT USE IN USER CODE.
-//
-// Expands to the namespace name that the type-parameterized tests for
-// the given type-parameterized test case are defined in.  The exact
-// name of the namespace is subject to change without notice.
-# define GTEST_CASE_NAMESPACE_(TestCaseName) \
-  gtest_case_##TestCaseName##_
-    
-      ::std::stringstream actual_ss;
-  actual_ss << std::setprecision(std::numeric_limits<RawType>::digits10 + 2)
-            << actual;
-    
-    // Helper function for implementing {EXPECT|ASSERT}_PRED2.  Don't use
-// this in your code.
-template <typename Pred,
-          typename T1,
-          typename T2>
-AssertionResult AssertPred2Helper(const char* pred_text,
-                                  const char* e1,
-                                  const char* e2,
-                                  Pred pred,
-                                  const T1& v1,
-                                  const T2& v2) {
-  if (pred(v1, v2)) return AssertionSuccess();
+        TEST(MovableMessageTest, MoveFromArena) {
+  Arena arena;
     }
     
-     private:
-  template <typename U>
-  friend class linked_ptr;
+    // Author: kenton@google.com (Kenton Varda)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
     
-    // Tests factorial of negative numbers.
-TEST(FactorialTest, Negative) {
-  // This test is named 'Negative', and belongs to the 'FactorialTest'
-  // test case.
-  EXPECT_EQ(1, Factorial(-5));
-  EXPECT_EQ(1, Factorial(-1));
-  EXPECT_GT(Factorial(-10), 0);
+    const Status Status::OK = Status();
+const Status Status::CANCELLED = Status(error::CANCELLED, '');
+const Status Status::UNKNOWN = Status(error::UNKNOWN, '');
+    
+      // Test different types are false.
+  value = type_equals_<float, int>::value;
+  EXPECT_FALSE(value);
+    
+    #include <google/protobuf/io/gzip_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+    
+      while (true) {
+    void* outptr;
+    int outlen;
+    bool ok;
+    do {
+      ok = out.Next(&outptr, &outlen);
+      if (!ok) {
+        break;
+      }
+    } while (outlen <= 0);
+    readlen = read(STDIN_FILENO, outptr, outlen);
+    if (readlen <= 0) {
+      out.BackUp(outlen);
+      break;
     }
+    if (readlen < outlen) {
+      out.BackUp(outlen - readlen);
+    }
+  }
     
-      s.Set(kHelloString);
-  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
+    #include 'addressbook.pb.h'
     
-      // Gets the last element of the queue, or NULL if the queue is empty.
-  QueueNode<E>* Last() { return last_; }
-  const QueueNode<E>* Last() const { return last_; }
+      // Add the full names of registered services
+  void SetServiceList(const std::vector<grpc::string>* services);
     
-      // Allocated bytes in this space.  Garbage bytes that were not found due to
-  // concurrent sweeping are counted as being allocated!  The bytes in the
-  // current linear allocation area (between top and limit) are also counted
-  // here.
-  size_t Size() override { return accounting_stats_.Size(); }
+    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
+  uint64_t busy = 0, total = 0;
+  host_cpu_load_info_data_t cpuinfo;
+  mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
+  if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO,
+                      (host_info_t)&cpuinfo, &count) == KERN_SUCCESS) {
+    for (int i = 0; i < CPU_STATE_MAX; i++) total += cpuinfo.cpu_ticks[i];
+    busy = total - cpuinfo.cpu_ticks[CPU_STATE_IDLE];
+  }
+  return std::make_pair(busy, total);
+}
+    
+    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
+  uint64_t busy = 0, total = 0;
+  gpr_log(GPR_ERROR,
+          'Platforms other than Linux, Windows, and MacOS are not supported.');
+  return std::make_pair(busy, total);
+}
+    
+    #include <windows.h>
+#include <cstdint>
+    
+            friend Rational operator&(Rational lhs, Rational const& rhs);
+        friend Rational operator|(Rational lhs, Rational const& rhs);
+        friend Rational operator^(Rational lhs, Rational const& rhs);
     
     
     {
-    {}  // namespace internal
-}  // namespace v8
-    
-    int Sweeper::ParallelSweepSpace(AllocationSpace identity,
-                                int required_freed_bytes, int max_pages) {
-  int max_freed = 0;
-  int pages_freed = 0;
-  Page* page = nullptr;
-  while ((page = GetSweepingPageSafe(identity)) != nullptr) {
-    int freed = ParallelSweepPage(page, identity);
-    pages_freed += 1;
-    DCHECK_GE(freed, 0);
-    max_freed = Max(max_freed, freed);
-    if ((required_freed_bytes) > 0 && (max_freed >= required_freed_bytes))
-      return max_freed;
-    if ((max_pages > 0) && (pages_freed >= max_pages)) return max_freed;
-  }
-  return max_freed;
+    {    private:
+        int32_t m_sign;
+        int32_t m_exp;
+        std::vector<uint32_t> m_mantissa;
+    };
 }
+
     
-      // labels
-  void print(const Label* L);
-  void bind_to(Label* L, int pos);
+    #include 'pch.h'
+#include 'LiveRegionHost.h'
     
-    void FrameDescription::SetCallerPc(unsigned offset, intptr_t value) {
-  SetFrameSlot(offset, value);
+    String ^ NarratorAnnouncement::ActivityId::get()
+{
+    return m_activityId;
 }
-    
-      void CallForDeoptimization(Address target, int deopt_id);
-    
-    #define SSE2_INSTRUCTION_LIST(V) \
-  V(packsswb, 66, 0F, 63)        \
-  V(packssdw, 66, 0F, 6B)        \
-  V(packuswb, 66, 0F, 67)        \
-  V(paddb, 66, 0F, FC)           \
-  V(paddw, 66, 0F, FD)           \
-  V(paddd, 66, 0F, FE)           \
-  V(paddsb, 66, 0F, EC)          \
-  V(paddsw, 66, 0F, ED)          \
-  V(paddusb, 66, 0F, DC)         \
-  V(paddusw, 66, 0F, DD)         \
-  V(pand, 66, 0F, DB)            \
-  V(pcmpeqb, 66, 0F, 74)         \
-  V(pcmpeqw, 66, 0F, 75)         \
-  V(pcmpeqd, 66, 0F, 76)         \
-  V(pcmpgtb, 66, 0F, 64)         \
-  V(pcmpgtw, 66, 0F, 65)         \
-  V(pcmpgtd, 66, 0F, 66)         \
-  V(pmaxsw, 66, 0F, EE)          \
-  V(pmaxub, 66, 0F, DE)          \
-  V(pminsw, 66, 0F, EA)          \
-  V(pminub, 66, 0F, DA)          \
-  V(pmullw, 66, 0F, D5)          \
-  V(por, 66, 0F, EB)             \
-  V(psllw, 66, 0F, F1)           \
-  V(pslld, 66, 0F, F2)           \
-  V(psraw, 66, 0F, E1)           \
-  V(psrad, 66, 0F, E2)           \
-  V(psrlw, 66, 0F, D1)           \
-  V(psrld, 66, 0F, D2)           \
-  V(psubb, 66, 0F, F8)           \
-  V(psubw, 66, 0F, F9)           \
-  V(psubd, 66, 0F, FA)           \
-  V(psubsb, 66, 0F, E8)          \
-  V(psubsw, 66, 0F, E9)          \
-  V(psubusb, 66, 0F, D8)         \
-  V(psubusw, 66, 0F, D9)         \
-  V(punpcklbw, 66, 0F, 60)       \
-  V(punpcklwd, 66, 0F, 61)       \
-  V(punpckldq, 66, 0F, 62)       \
-  V(punpcklqdq, 66, 0F, 6C)      \
-  V(punpckhbw, 66, 0F, 68)       \
-  V(punpckhwd, 66, 0F, 69)       \
-  V(punpckhdq, 66, 0F, 6A)       \
-  V(punpckhqdq, 66, 0F, 6D)      \
-  V(pxor, 66, 0F, EF)
