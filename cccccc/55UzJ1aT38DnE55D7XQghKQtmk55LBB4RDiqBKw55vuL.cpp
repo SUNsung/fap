6076708cc43ae75da62b491e733818191f4dd6ab
SUNsung <weatherius@gmail.com>
2019-07-12 02:02:14 +0300

    Rewrite [55UzJ1aT38DnE55D7XQghKQtmk55LBB4RDiqBKw55vuL.cpp] File in: 2019-07-12 02:02:14

--- cccccc/55UzJ1aT38DnE55D7XQghKQtmk55LBB4RDiqBKw55vuL.cpp
+++ cccccc/55UzJ1aT38DnE55D7XQghKQtmk55LBB4RDiqBKw55vuL.cpp
@@ -1,150 +1,206 @@
 
         
-        bool Converter<Dictionary>::FromV8(v8::Isolate* isolate,
-                                   v8::Local<v8::Value> val,
-                                   Dictionary* out) {
-  if (!val->IsObject() || val->IsFunction())
+        
+    {  int delta = this - prev;
+  int32_t n = prev->n_ + 1;
+  int32_t sig_x = prev->sig_x_ + delta;
+  int64_t sig_xsq = prev->sig_xsq_ + delta * delta;
+  int64_t cost = (sig_xsq - sig_x * sig_x / n) / n;
+  cost += prev->total_cost_;
+  UpdateIfBetter(cost, prev->total_steps_ + 1, prev, n, sig_x, sig_xsq);
+  return cost;
+}
+    
+      // Fills in two ambiguity tables (replaceable and dangerous) with information
+  // read from the ambigs file. An ambiguity table is an array of lists.
+  // The array is indexed by a class id. Each entry in the table provides
+  // a list of potential ambiguities which can start with the corresponding
+  // character. For example the ambiguity 'rn -> m', would be located in the
+  // table at index of unicharset.unichar_to_id('r').
+  // In 1-1 ambiguities (e.g. s -> S, 1 -> I) are recorded in
+  // one_to_one_definite_ambigs_. This vector is also indexed by the class id
+  // of the wrong part of the ambiguity and each entry contains a vector of
+  // unichar ids that are ambiguous to it.
+  // encoder_set is used to encode the ambiguity strings, undisturbed by new
+  // unichar_ids that may be created by adding the ambigs.
+  void LoadUnicharAmbigs(const UNICHARSET& encoder_set,
+                         TFile *ambigs_file, int debug_level,
+                         bool use_ambigs_for_adaption, UNICHARSET *unicharset);
+    
+      // The UNICHARMAP is represented as a tree whose nodes are of type
+  // UNICHARMAP_NODE.
+  struct UNICHARMAP_NODE {
+    }
+    
+    bool GetVarint64(Slice* input, uint64_t* value) {
+  const char* p = input->data();
+  const char* limit = p + input->size();
+  const char* q = GetVarint64Ptr(p, limit, value);
+  if (q == nullptr) {
     return false;
-  *out = Dictionary(isolate, v8::Local<v8::Object>::Cast(val));
-  return true;
-}
-    
-    v8::Local<v8::Value> Converter<Promise>::ToV8(v8::Isolate* isolate,
-                                              Promise val) {
-  return val.GetHandle();
-}
-    
-    ObjectTemplateBuilder::~ObjectTemplateBuilder() {}
-    
-    bool CertificateManagerModel::ImportServerCert(
-    const net::ScopedCERTCertificateList& certificates,
-    net::NSSCertDatabase::TrustBits trust_bits,
-    net::NSSCertDatabase::ImportCertFailureList* not_imported) {
-  return cert_db_->ImportServerCert(certificates, trust_bits, not_imported);
-}
-    
-    template <typename T>
-bool ConvertFromV8(v8::Isolate* isolate,
-                   v8::Local<v8::Value> input,
-                   T* result) {
-  return Converter<T>::FromV8(isolate, input, result);
-}
-    
-    // Like Dictionary, but stores object in persistent handle so you can keep it
-// safely on heap.
-class PersistentDictionary : public Dictionary {
- public:
-  PersistentDictionary();
-  PersistentDictionary(v8::Isolate* isolate, v8::Local<v8::Object> object);
-  PersistentDictionary(const PersistentDictionary& other);
-  ~PersistentDictionary() override;
-    }
-    
-    namespace {
-    }
-    
-    static void secp256k1_ecdsa_recoverable_signature_save(secp256k1_ecdsa_recoverable_signature* sig, const secp256k1_scalar* r, const secp256k1_scalar* s, int recid) {
-    if (sizeof(secp256k1_scalar) == 32) {
-        memcpy(&sig->data[0], r, 32);
-        memcpy(&sig->data[32], s, 32);
-    } else {
-        secp256k1_scalar_get_b32(&sig->data[0], r);
-        secp256k1_scalar_get_b32(&sig->data[32], s);
-    }
-    sig->data[64] = recid;
-}
-    
-    const std::vector<UniValue>& UniValue::getValues() const
-{
-    if (typ != VOBJ && typ != VARR)
-        throw std::runtime_error('JSON value is not an object or array as expected');
-    return values;
-}
-    
-    /** A hasher class for RIPEMD-160. */
-class CRIPEMD160
-{
-private:
-    uint32_t s[5];
-    unsigned char buf[64];
-    uint64_t bytes;
-    }
-    
-    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
-  size_t usize = user_key.size();
-  size_t needed = usize + 13;  // A conservative estimate
-  char* dst;
-  if (needed <= sizeof(space_)) {
-    dst = space_;
   } else {
-    dst = new char[needed];
+    *input = Slice(q, limit - q);
+    return true;
   }
-  start_ = dst;
-  dst = EncodeVarint32(dst, usize + 8);
-  kstart_ = dst;
-  memcpy(dst, user_key.data(), usize);
-  dst += usize;
-  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
-  dst += 8;
-  end_ = dst;
 }
     
-      ~LookupKey();
+    // Standard Put... routines append to a string
+void PutFixed32(std::string* dst, uint32_t value);
+void PutFixed64(std::string* dst, uint64_t value);
+void PutVarint32(std::string* dst, uint32_t value);
+void PutVarint64(std::string* dst, uint64_t value);
+void PutLengthPrefixedSlice(std::string* dst, const Slice& value);
     
-    class FileNameTest { };
+    WriteBatch::Handler::~Handler() = default;
     
-      // other boxes
-  bleedBox = cropBox;
-  readBox(dict, 'BleedBox', &bleedBox);
-  trimBox = cropBox;
-  readBox(dict, 'TrimBox', &trimBox);
-  artBox = cropBox;
-  readBox(dict, 'ArtBox', &artBox);
+                // There should be at most 2 dims we cannot drop
+            auto numDimsThatCannotBeDropped = std::count_if(dimsToDrop.begin(), dimsToDrop.end(), [](const bool& val) {
+                return !val;
+            });
     
     
-    {  PDFRectangle mediaBox;
-  PDFRectangle cropBox;
-  GBool haveCropBox;
-  PDFRectangle bleedBox;
-  PDFRectangle trimBox;
-  PDFRectangle artBox;
-  int rotate;
-  Object lastModified;
-  Object boxColorInfo;
-  Object group;
-  Object metadata;
-  Object pieceInfo;
-  Object separationInfo;
-  Object resources;
+    {
+    {            if (map.size() > RAND_MAX * (size_t) RAND_MAX)
+                RuntimeError('RandomOrdering: too large training set: need to change to different random generator!');
+            srand((unsigned int) seed);
+            size_t retries = 0;
+            foreach_index (t, map)
+            {
+                for (int tries = 0; tries < 5; tries++)
+                {
+                    // swap current pos with a random position
+                    // Random positions are limited to t+randomizationrange.
+                    // This ensures some locality suitable for paging with a sliding window.
+                    const size_t tbegin = max((size_t) t, randomizationrange / 2) - randomizationrange / 2; // range of window  --TODO: use bounds() function above
+                    const size_t tend = min(t + randomizationrange / 2, map.size());
+                    assert(tend >= tbegin);                  // (guard against potential numeric-wraparound bug)
+                    const size_t trand = rand(tbegin, tend); // random number within windows
+                    assert((size_t) t <= trand + randomizationrange / 2 && trand < (size_t) t + randomizationrange / 2);
+                    // if range condition is fulfilled then swap
+                    if (trand <= map[t] + randomizationrange / 2 && map[t] < trand + randomizationrange / 2 && (size_t) t <= map[trand] + randomizationrange / 2 && map[trand] < (size_t) t + randomizationrange / 2)
+                    {
+                        std::swap(map[t], map[trand]);
+                        break;
+                    }
+                    // but don't multi-swap stuff out of its range (for swapping positions that have been swapped before)
+                    // instead, try again with a different random number
+                    retries++;
+                }
+            }
+            fprintf(stderr, 'RandomOrdering: %lu retries for %lu elements (%.1f%%) to ensure window condition\n', (unsigned long) retries, (unsigned long) map.size(), 100.0 * retries / map.size());
+            // ensure the window condition
+            foreach_index (t, map)
+                assert((size_t) t <= map[t] + randomizationrange / 2 && map[t] < (size_t) t + randomizationrange / 2);
+#if 0 // and a live check since I don't trust myself here yet
+            foreach_index (t, map) if (!((size_t) t <= map[t] + randomizationrange/2 && map[t] < (size_t) t + randomizationrange/2))
+            {
+                fprintf (stderr, 'RandomOrdering: windowing condition violated %d -> %d\n', t, map[t]);
+                LogicError('RandomOrdering: windowing condition violated');
+            }
+#endif
+#if 0 // test whether it is indeed a unique complete sequence
+            auto map2 = map;
+            ::sort (map2.begin(), map2.end());
+            foreach_index (t, map2) assert (map2[t] == (size_t) t);
+#endif
+            fprintf(stderr, 'RandomOrdering: recached sequence for seed %d: %d, %d, ...\n', (int) seed, (int) map[0], (int) map[1]);
+            currentseed = seed;
+        }
+        return map; // caller can now access it through operator[]
+    }
+    
+    // not in the cache yet: create it (or not if no such member)
+void /*CustomConfigRecord::*/ ComputationNetwork::LazyCreateConfigMember(const wstring& id) const /*override*/
+{
+    auto iter = m_nameToNodeMap.find(id);
+    if (iter == m_nameToNodeMap.end())
+    {
+        // workaround to allow to access members with '.' inside: change to _
+        for (iter = m_nameToNodeMap.begin(); iter != m_nameToNodeMap.end(); iter++)
+            if (msra::strfun::ReplaceAll<wstring>(iter->first, L'.', L'_') == id)
+                break;
+        if (iter == m_nameToNodeMap.end())
+            return; // no such node
+    }
+    const ComputationNodeBasePtr& node = iter->second;
+    // TODO: What is the expressionPath?
+    let& nodeName = node->NodeName();   // failFn lambda below holds a copy of the name for the error message. Let's not hold an unneccessary shared_ptr to the node, risking cycles & stuff.
+    auto valuep = ConfigValuePtr(node, [nodeName](const std::wstring &) { LogicError('ComputationNetwork: Failed to retrieve node '%ls'.', nodeName.c_str()); }, node->NodeName());
+    InsertConfigMember(id, move(valuep));
+}
+    
+    
+    {        stt = firstLbl;
+        stp = lastLbl;
+    };
+    
+    template <class ElemType>
+void EpochAccumulatorNode<ElemType>::Validate(bool isFinalValidationPass)
+{
+    Base::Validate(isFinalValidationPass);
+    SetDims(Input(0)->GetSampleLayout(), HasMBLayout());
+}
+    
+        size_t maxSeqLength = seq[sequenceOrder[0]].GetNumTimeSteps();
+    // BUGBUG: This forces the sequences to fit, due to a very bad convention in the evaldll interface.
+    if (maxSeqLength > mb->GetNumTimeSteps())
+        maxSeqLength = mb->GetNumTimeSteps();
+    
+      if (comma != data) {
+    // we have meta
+    ssize_t meta_len = comma - data;
+    data_len -= meta_len;
+    char* semi = (char*)memchr(data, ';', meta_len);
+    char* slash = (char*)memchr(data, '/', meta_len);
+    }
+    
+    
+    {  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
 };
     
-      // get direction
-  if (dict->lookup('M', &obj)->isName()) {
-    const char *m = obj.getName();
+    void PlainDirectory::rewind() {
+  ::rewinddir(m_dir);
+}
     
-    if (strcmp('I', m) == 0)
-      direction = transitionInward;
-    else if (strcmp('O', m) == 0)
-      direction = transitionOutward;
-  }
-  obj.free();
+    struct GlobStreamWrapper final : Stream::Wrapper {
+  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+  req::ptr<Directory> opendir(const String& path) override;
+};
     
-    class ProfileData {
-public:
+      Pipe();
+  virtual ~Pipe();
+    
+    namespace rocksdb {
     }
     
-    //------------------------------------------------------------------------
+      // When an actor (column family) requests a stop token, all writes will be
+  // stopped until the stop token is released (deleted)
+  std::unique_ptr<WriteControllerToken> GetStopToken();
+  // When an actor (column family) requests a delay token, total delay for all
+  // writes to the DB will be controlled under the delayed write rate. Every
+  // write needs to call GetDelay() with number of bytes writing to the DB,
+  // which returns number of microseconds to sleep.
+  std::unique_ptr<WriteControllerToken> GetDelayToken(
+      uint64_t delayed_write_rate);
+  // When an actor (column family) requests a moderate token, compaction
+  // threads will be increased
+  std::unique_ptr<WriteControllerToken> GetCompactionPressureToken();
     
-      //----- Type 3 font operators
-  virtual void type3D0(GfxState *state, double wx, double wy);
-  virtual void type3D1(GfxState *state, double wx, double wy,
-		       double llx, double lly, double urx, double ury);
+      // Load the options file.
+  DBOptions loaded_db_opt;
+  std::vector<ColumnFamilyDescriptor> loaded_cf_descs;
+  s = LoadLatestOptions(kDBPath, Env::Default(), &loaded_db_opt,
+                        &loaded_cf_descs);
+  assert(s.ok());
+  assert(loaded_db_opt.create_if_missing == db_opt.create_if_missing);
     
-    #include <config.h>
+    // Supported only for Leveled compaction
+Status SuggestCompactRange(DB* db, ColumnFamilyHandle* column_family,
+                           const Slice* begin, const Slice* end);
+Status SuggestCompactRange(DB* db, const Slice* begin, const Slice* end);
     
-    #ifndef STDINCACHELOADER_H
-#define STDINCACHELOADER_H
     
-    #include 'StdinPDFDocBuilder.h'
-#include 'CachedFile.h'
-#include 'StdinCachedFile.h'
+    {  virtual std::string GetPrintableOptions() const = 0;
+};
