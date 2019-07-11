eb99ba14a6ed70782aca47882e64a3de942a4f22
SUNsung <weatherius@gmail.com>
2019-07-11 06:34:17 +0300

    Rewrite [02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py] File in: 2019-07-11 06:34:16

--- pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
+++ pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
@@ -1,219 +1,62 @@
 
         
-        
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
+            def __init__(self, employee_id, name):
+        super(Operator, self).__init__(employee_id, name, Rank.SUPERVISOR)
     
-        with io.open(infile, encoding='utf-8') as inf:
-        issue_template_tmpl = inf.read()
     
-        def gen_ies_md(ies):
-        for ie in ies:
-            ie_md = '**{0}**'.format(ie.IE_NAME)
-            ie_desc = getattr(ie, 'IE_DESC', None)
-            if ie_desc is False:
-                continue
-            if ie_desc is not None:
-                ie_md += ': {0}'.format(ie.IE_DESC)
-            if not ie.working():
-                ie_md += ' (Currently broken)'
-            yield ie_md
+class Car(Vehicle):
     
-        def test_array_access(self):
-        jsi = JSInterpreter('function f(){var x = [1,2,3]; x[0] = 4; x[0] = 5; x[2] = 7; return x;}')
-        self.assertEqual(jsi.call_function('f'), [5, 2, 7])
-    
-    from test.helper import (
-    FakeYDL,
-    get_params,
-)
-from youtube_dl.compat import (
-    compat_str,
-    compat_urllib_request,
-)
-    
+            Accessing a node updates its position to the front of the LRU list.
         '''
-    # Serialize JSON data, if needed.
-    data = args.data
-    auto_json = data and not args.form
-    if (args.json or auto_json) and isinstance(data, dict):
-        if data:
-            data = json.dumps(data)
-        else:
-            # We need to set data to an empty string to prevent requests
-            # from assigning an empty list to `response.request.data`.
-            data = ''
-    
-        def __str__(self):
-        defaults = dict(type(self).__dict__)
-        actual = dict(defaults)
-        actual.update(self.__dict__)
-        actual['config'] = self.config
-        return repr_dict_nice(dict(
-            (key, value)
-            for key, value in actual.items()
-            if not key.startswith('_'))
-        )
-    
-            Use `self.raw_auth` to access the raw value passed through
-        `--auth, -a`.
+        node = self.lookup[query]
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
     
     
-def test_credentials_in_url_auth_flag_has_priority(httpbin_both):
-    '''When credentials are passed in URL and via -a at the same time,
-     then the ones from -a are used.'''
-    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
-                   auth='user:wrong')
-    r = http('--auth=user:password', 'GET', url)
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
+class PagesDataStore(object):
     
+        if not app.config.edit_on_github_project:
+        warnings.warn('edit_on_github_project not specified')
+        return
+    if not doctree:
+        warnings.warn('doctree is None')
+        return
+    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
+    show_url = get_github_url(app, 'blob', path)
+    edit_url = get_github_url(app, 'edit', path)
     
-def has_docutils():
-    try:
-        # noinspection PyUnresolvedReferences
-        import docutils
-        return True
-    except ImportError:
-        return False
+    _LOGGER = logging.getLogger(__name__)
     
+        def initialize(self, **kwargs: Any) -> None:  # type: ignore
+        self.is_current = False
+        loop = asyncio.new_event_loop()
+        try:
+            super(AsyncIOLoop, self).initialize(loop, **kwargs)
+        except Exception:
+            # If initialize() does not succeed (taking ownership of the loop),
+            # we have to close it.
+            loop.close()
+            raise
     
-def test_unicode_url_query_arg_item(httpbin):
-    r = http(httpbin.url + '/get', u'test==%s' % UNICODE)
-    assert HTTP_OK in r
-    assert r.json['args'] == {'test': UNICODE}, r
-    
-        def test_request_body_from_file_by_path_no_data_items_allowed(
-            self, httpbin):
-        env = MockEnvironment(stdin_isatty=False)
-        r = http('POST', httpbin.url + '/post', '@' + FILE_PATH_ARG, 'foo=bar',
-                 env=env, error_exit_ok=True)
-        assert 'cannot be mixed' in r.stderr
-
-    
-            if ext == '.htm':  # Python 3
-            ext = '.html'
-    
-        n_features = 10
-    list_n_samples = np.linspace(100, 1000000, 5).astype(np.int)
-    lasso_results, lars_lasso_results = compute_bench(alpha, list_n_samples,
-                                            [n_features], precompute=True)
-    
-        print('============================================')
-    print('Warning: this is going to take a looong time')
-    print('============================================')
+        def test_merge_sort(self):
+        merge_sort = MergeSort()
     
     
 if __name__ == '__main__':
-    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
-    # block to be able to use a multi-core grid search that also works under
-    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
-    # The multiprocessing module is used as the backend of joblib.Parallel
-    # that is used when n_jobs != 1 in GridSearchCV
+    main()
     
-    The data is generated with the ``make_checkerboard`` function, then
-shuffled and passed to the Spectral Biclustering algorithm. The rows
-and columns of the shuffled matrix are rearranged to show the
-biclusters found by the algorithm.
+    		elif current.rightChild is None:
+			if current is self.root:
+				self.root = current.leftChild
+			elif isLeft:
+				parent.leftChild = current.leftChild
+			else:
+				parent.rightChild = current.leftChild
     
-    for name, label in [('Setosa', 0),
-                    ('Versicolour', 1),
-                    ('Virginica', 2)]:
-    ax.text3D(X[y == label, 3].mean(),
-              X[y == label, 0].mean(),
-              X[y == label, 2].mean() + 2, name,
-              horizontalalignment='center',
-              bbox=dict(alpha=.2, edgecolor='w', facecolor='w'))
-# Reorder the labels to have colors matching the cluster results
-y = np.choose(y, [1, 2, 0]).astype(np.float)
-ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y, edgecolor='k')
-    
-    colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(n_clusters_), colors):
-    my_members = labels == k
-    cluster_center = cluster_centers[k]
-    plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.show()
-
-    
-    # Authors : Vincent Michel, 2010
-#           Alexandre Gramfort, 2010
-#           Gael Varoquaux, 2010
-# License: BSD 3 clause
-    
-        # TODO: decoder/encoder should accept cls? Otherwise, subclassing
-    # JSONObjectWithFields is tricky...
-    header_cls = Header
-    header = jose.Field(
-        'header', omitempty=True, default=header_cls(),
-        decoder=header_cls.from_json)
-    
-    
-class JWSTest(unittest.TestCase):
-    '''Tests for acme.jws.JWS.'''
-    
-    Certbot will emit a warning if it detects that the credentials file can be
-accessed by other users on your system. The warning reads 'Unsafe permissions
-on credentials configuration file', followed by the path to the credentials
-file. This warning will be emitted each time Certbot uses the credentials file,
-including for renewal, and cannot be silenced except by addressing the issue
-(e.g., by using a command like ``chmod 600`` to restrict access to the file).
-    
-        def _forwards(self, orm):
-        'Write your forwards methods here.'
-        for release in RangeQuerySetWrapperWithProgressBar(
-            orm.Release.objects.exclude(new_groups=0)
-        ):
-            projects = list(release.projects.values_list('id', flat=True))
-            if len(projects) > 1:
-                # do something fancy where we look at Group.first_release
-                # to calculate ReleaseProject.new_group
-                for p_id in projects:
-                    new_groups = orm.Group.objects.filter(
-                        first_release=release, project_id=p_id
-                    ).count()
-                    if not new_groups:
-                        continue
-                    orm.ReleaseProject.objects.filter(
-                        release_id=release.id, project_id=p_id
-                    ).update(new_groups=new_groups)
-            elif len(projects) == 1:
-                # copy Release.new_groups to ReleaseProject.new_group
-                orm.ReleaseProject.objects.filter(
-                    release_id=release.id, project_id=projects[0]
-                ).update(new_groups=release.new_groups)
-    
-            # Adding model 'DSymApp'
-        db.create_table(
-            'sentry_dsymapp', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'project', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.Project']
-                    )
-                ), ('app_id', self.gf('django.db.models.fields.CharField')(max_length=64)),
-                ('sync_id', self.gf('django.db.models.fields.CharField')(max_length=64, null=True)),
-                ('data', self.gf('jsonfield.fields.JSONField')(default={})), (
-                    'platform',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(
-                        default=0
-                    )
-                ), (
-                    'last_synced',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ), (
-                    'date_added',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['DSymApp'])
+    	def test_max_min_nodes (self):
+		myTree = BinaryTree()
+		myTree.insert(5)
+		myTree.insert(1)
+		myTree.insert(21)
