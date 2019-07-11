ddbad2ade7943053a88e72ac815c703e9b63a324
SUNsung <weatherius@gmail.com>
2019-07-11 16:53:04 +0300

    Rewrite [29oy22GCk8VH2292ew4yplYk.29qIEtNk0pHpM29FpK.cpp] File in: 2019-07-11 16:53:04

--- cccccc/29oy22GCk8VH2292ew4yplYk.29qIEtNk0pHpM29FpK.cpp
+++ cccccc/29oy22GCk8VH2292ew4yplYk.29qIEtNk0pHpM29FpK.cpp
@@ -1,178 +1,242 @@
 
         
-        inline absl::string_view GetMethod(const grpc_slice* path) {
-  if (GRPC_SLICE_IS_EMPTY(*path)) {
-    return '';
-  }
-  // Check for leading '/' and trim it if present.
-  return absl::StripPrefix(absl::string_view(reinterpret_cast<const char*>(
-                                                 GRPC_SLICE_START_PTR(*path)),
-                                             GRPC_SLICE_LENGTH(*path)),
-                           '/');
+        // Generate param traits log methods.
+#include 'ipc/param_traits_log_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+
+    
+      base::WaitableEvent done(false, false);
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      base::Bind(&SetProxyConfigCallback, &done,
+                 make_scoped_refptr(context_getter), config));
+  done.Wait();
+    
+      // Try to close all windows (then will cause whole app to quit).
+  static void CloseAllWindows(bool force = false, bool quit = false);
+    
+       void Call(const std::string& method,
+                    const base::ListValue& arguments) override;
+   void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) override;
+    
+    
+namespace nwapi {
+    }
+    
+    #include 'content/nw/src/api/menu/menu.h'
+    
+    #include 'content/nw/src/api/menuitem/menuitem.h'
+    
+    void MenuItem::SetLabel(const std::string& label) {
+  label_ = label;
+  gtk_menu_item_set_label(GTK_MENU_ITEM(menu_item_), label.c_str());
 }
     
-    ::opencensus::stats::MeasureInt64 RpcServerSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcServerReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerServerLatency();
-::opencensus::stats::MeasureInt64 RpcServerCompletedRpcs();
+        if (screens) {
+      std::unique_ptr<DesktopMediaList> screen_media_list =
+        std::make_unique<NativeDesktopMediaList>(
+          content::DesktopMediaID::TYPE_SCREEN,
+          webrtc::DesktopCapturer::CreateScreenCapturer(options));
+      media_list_.push_back(std::move(screen_media_list));
+    }
     
-    #endif  // !defined(GPR_LINUX) && !defined(GPR_WINDOWS) && !defined(GPR_APPLE)
-
-    
-    #endif  // GPR_WINDOWS
-
-    
-    
-    {}  // namespace grpc
-    
-    /*!
- * \brief The result holder of storage type of each NodeEntry in the graph.
- * \note Stored under graph.attrs['storage_type'], provided by Pass 'InferStorageType'
- *
- * \code
- *  Graph g = ApplyPass(src_graph, 'InferStorageType');
- *  const StorageVector& stypes = g.GetAttr<StorageTypeVector>('storage_type');
- *  // get storage type by entry id
- *  int entry_type = stypes[g.indexed_graph().entry_id(my_entry)];
- * \endcode
- *
- * \sa FInferStorageType
- */
-using StorageTypeVector = std::vector<int>;
-    
-    #ifndef PLUGIN_CAFFE_CAFFE_COMMON_H_
-#define PLUGIN_CAFFE_CAFFE_COMMON_H_
-    
-    namespace mxnet {
-namespace io {
-/*! \return the parameter of default augmenter */
-std::vector<dmlc::ParamFieldInfo> ListDefaultAugParams();
-std::vector<dmlc::ParamFieldInfo> ListDefaultDetAugParams();
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_IO_IMAGE_AUGMENTER_H_
-
-    
-      /*!
-   * \brief encodes parameters of gc into a string
-   */
-  std::string EncodeParams();
+      /**
+   * /brief Customize set method for LayerParameter
+   * /tparam value string of caffe's layer configuration
+   * */
+  virtual void Set(void *head, const std::string &value) const {
+    caffe::NetParameter net_param;
+    if (!ReadProtoFromTextContent(value, &net_param))
+      CHECK(false)<< 'Caffe Net Prototxt: ' << value << 'Initialized Failed';
+    }
     
     
     {
-    {// for reverse seq
-DECL_SCALAR(DEVICE, Plus, EvalScalar_, false)
-DECL_SCALAR(DEVICE, Minus, EvalScalar_, false)
-DECL_SCALAR(DEVICE, Mul, EvalScalar_, false)
-DECL_SCALAR(DEVICE, Div, EvalScalar_, false)
-}  // namespace ndarray
+    {}  // namespace exec
 }  // namespace mxnet
+
     
-       out = data / sqrt(data.shape[-1])
-    
-    Operator* NativeOpProp::CreateOperator(Context ctx) const {
-  DO_BIND_DISPATCH(CreateOp, param_);
-}
-    
-    template<>
-Operator* CreateOp<gpu>(NDArrayOpParam param) {
-  return new NDArrayOp<gpu>(param);
-}
-#endif  // MXNET_USE_CUDA
-    
-    /*
- * If Trace::hhbbc_time >= 1, print some stats about the program to a
- * temporary file.  If it's greater than or equal to 2, also dump it
- * to stdout.
- */
-void print_stats(const Index&, const php::Program&);
-    
-    template<class T>
-void fillCollection(T* coll, const APCArray* ar) {
-  for (auto i = uint32_t{0}; i < ar->size(); ++i) {
-    coll->add(*ar->getValue(i)->toLocal().asTypedValue());
+    #include <algorithm>
+#include <vector>
+#include './bilinear_sampler-inl.h'
+namespace mxnet {
+namespace op {
+#if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 5
+template<typename DType>
+class CuDNNBilinearSamplerOp : public Operator {
+ public:
+  explicit CuDNNBilinearSamplerOp(BilinearSamplerParam param) {
+    this->param_ = param;
+    init_cudnn_ = false;
+    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
+    sampler_ = CUDNN_SAMPLER_BILINEAR;
   }
+    }
+    }
+    }
+    
+    DMLC_REGISTER_PARAMETER(NativeOpParam);
+    
+    // only include thrust-related files if host_device_vector.h
+// is included from a .cu file
+#ifdef __CUDACC__
+#include <thrust/device_ptr.h>
+#endif  // __CUDACC__
+    
+      // init thread buffers
+  inline void InitThreadTemp(int nthread, int num_feature) {
+    int prev_thread_temp_size = thread_temp.size();
+    if (prev_thread_temp_size < nthread) {
+      thread_temp.resize(nthread, RegTree::FVec());
+      for (int i = prev_thread_temp_size; i < nthread; ++i) {
+        thread_temp[i].Init(num_feature);
+      }
+    }
+  }
+  inline void PredLoopSpecalize(DMatrix* p_fmat,
+                                std::vector<bst_float>* out_preds,
+                                const gbm::GBTreeModel& model, int num_group,
+                                unsigned tree_begin, unsigned tree_end) {
+    const MetaInfo& info = p_fmat->Info();
+    const int nthread = omp_get_max_threads();
+    InitThreadTemp(nthread, model.param.num_feature);
+    std::vector<bst_float>& preds = *out_preds;
+    CHECK_EQ(model.param.size_leaf_vector, 0)
+        << 'size_leaf_vector is enforced to 0 so far';
+    CHECK_EQ(preds.size(), p_fmat->Info().num_row_ * num_group);
+    // start collecting the prediction
+    for (const auto &batch : p_fmat->GetRowBatches()) {
+      // parallel over local batch
+      constexpr int kUnroll = 8;
+      const auto nsize = static_cast<bst_omp_uint>(batch.Size());
+      const bst_omp_uint rest = nsize % kUnroll;
+#pragma omp parallel for schedule(static)
+      for (bst_omp_uint i = 0; i < nsize - rest; i += kUnroll) {
+        const int tid = omp_get_thread_num();
+        RegTree::FVec& feats = thread_temp[tid];
+        int64_t ridx[kUnroll];
+        SparsePage::Inst inst[kUnroll];
+        for (int k = 0; k < kUnroll; ++k) {
+          ridx[k] = static_cast<int64_t>(batch.base_rowid + i + k);
+        }
+        for (int k = 0; k < kUnroll; ++k) {
+          inst[k] = batch[i + k];
+        }
+        for (int k = 0; k < kUnroll; ++k) {
+          for (int gid = 0; gid < num_group; ++gid) {
+            const size_t offset = ridx[k] * num_group + gid;
+            preds[offset] += this->PredValue(
+                inst[k], model.trees, model.tree_info, gid,
+                info.GetRoot(ridx[k]), &feats, tree_begin, tree_end);
+          }
+        }
+      }
+      for (bst_omp_uint i = nsize - rest; i < nsize; ++i) {
+        RegTree::FVec& feats = thread_temp[0];
+        const auto ridx = static_cast<int64_t>(batch.base_rowid + i);
+        auto inst = batch[i];
+        for (int gid = 0; gid < num_group; ++gid) {
+          const size_t offset = ridx * num_group + gid;
+          preds[offset] +=
+              this->PredValue(inst, model.trees, model.tree_info, gid,
+                              info.GetRoot(ridx), &feats, tree_begin, tree_end);
+        }
+      }
+    }
+  }
+    
+    class DHTRoutingTableDeserializer {
+private:
+  int family_;
+    }
+    
+    std::pair<std::vector<std::unique_ptr<Command>>,
+          std::vector<std::unique_ptr<Command>>>
+DHTSetup::setup(DownloadEngine* e, int family)
+{
+  std::vector<std::unique_ptr<Command>> tempCommands;
+  std::vector<std::unique_ptr<Command>> tempRoutineCommands;
+  if ((family != AF_INET && family != AF_INET6) ||
+      (family == AF_INET && DHTRegistry::isInitialized()) ||
+      (family == AF_INET6 && DHTRegistry::isInitialized6())) {
+    return {};
+  }
+  try {
+    // load routing table and localnode id here
+    std::shared_ptr<DHTNode> localNode;
+    }
+    }
+    
+    namespace aria2 {
+    }
+    
+    void DHTTaskExecutor::update()
+{
+  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
+                                  std::mem_fn(&DHTTask::finished)),
+                   execTasks_.end());
+  int r;
+  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
+    r = numConcurrent_ - execTasks_.size();
+  }
+  else {
+    r = 0;
+  }
+  while (r && !queue_.empty()) {
+    std::shared_ptr<DHTTask> task = queue_.front();
+    queue_.pop_front();
+    task->startup();
+    if (!task->finished()) {
+      execTasks_.push_back(task);
+      --r;
+    }
+  }
+  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
+                   static_cast<unsigned int>(getExecutingTaskSize()),
+                   static_cast<unsigned int>(getQueueSize())));
 }
     
-    //////////////////////////////////////////////////////////////////////
-    
-    
-    {  auto entry = m_it.second();
-  assertx(entry.isString());
-  return HHVM_FN(dirname)(entry.toString());
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port) const
+{
+  return generateToken(infoHash, ipaddr, port, secret_[0]);
 }
     
-    #include 'hphp/runtime/base/file.h'
-#include 'hphp/runtime/base/mem-file.h'
-#include 'hphp/runtime/base/stream-wrapper.h'
-#include <folly/String.h>
-#include <folly/portability/SysStat.h>
-#include <folly/portability/Unistd.h>
     
-    
-    {  StructuredLogEntry cols;
-  fillCols(cols);
-  cols.setStr('event_name', event);
-  cols.setInt('priority', priority);
-  StructuredLog::log('hhvm_perf_warning', cols);
-}
-    
-    void clearThreadLocalIO();
-void setThreadLocalIO(FILE* in, FILE* out, FILE* err);
-    
-    
-    {  private:
-    uint32_t  bits[6];
+    {    private:
+        redisAsyncContext * m_ctx;
+        QSocketNotifier * m_read;
+        QSocketNotifier * m_write;
 };
     
-    SharedBreakIterator::SharedBreakIterator(
-        BreakIterator *biToAdopt) : ptr(biToAdopt) { }
+        private:
+        void finish() { emit finished(); }
     
-    U_NAMESPACE_END
     
-    /**
- * Implement UnicodeMatcher
- */
-void StringMatcher::addMatchSetTo(UnicodeSet& toUnionTo) const {
-    UChar32 ch;
-    for (int32_t i=0; i<pattern.length(); i+=U16_LENGTH(ch)) {
-        ch = pattern.char32At(i);
-        const UnicodeMatcher* matcher = data->lookupMatcher(ch);
-        if (matcher == NULL) {
-            toUnionTo.add(ch);
-        } else {
-            matcher->addMatchSetTo(toUnionTo);
-        }
+    {    for (i = 0; i < READ_THREAD_N; i++)
+    {
+        threads[i].thread->join();
+        threads[i].pipe.close(&threads[i].pipe);
+        delete threads[i].thread;
     }
 }
     
-    bool ImGui_Marmalade_CreateDeviceObjects()
-{
-    // Build texture atlas
-    ImGuiIO& io = ImGui::GetIO();
-    unsigned char* pixels;
-    int width, height;
-    io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
-    }
+        SwooleG.use_signalfd = 1;
     
-    void CleanupRenderTarget()
-{
-    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+        /**
+     * 协程2
+     */
+    Coroutine::create([](void *arg)
+    {
+        G_a.x = 100;
+        G_a.y = nullptr;
+    });
+    
+    
+    {    return SW_ERR;
 }
-    
-        // Build atlas
-    unsigned char* tex_pixels = NULL;
-    int tex_w, tex_h;
-    io.Fonts->GetTexDataAsRGBA32(&tex_pixels, &tex_w, &tex_h);
-    
-        // For the browser using Emscripten, we are going to use WebGL1 with GL ES2. See the Makefile. for requirement details.
-    // It is very likely the generated file won't work in many browsers. Firefox is the only sure bet, but I have successfully
-    // run this code on Chrome for Android for example.
-    const char* glsl_version = '#version 100';
-    //const char* glsl_version = '#version 300 es';
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
