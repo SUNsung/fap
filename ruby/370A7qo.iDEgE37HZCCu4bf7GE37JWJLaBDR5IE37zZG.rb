463f2fa4d8032011fa61ff33d469548d943294a1
SUNsung <weatherius@gmail.com>
2019-07-12 06:56:16 +0300

    Rewrite [370A7qo.iDEgE37HZCCu4bf7GE37JWJLaBDR5IE37zZG.rb] File in: 2019-07-12 06:56:15

--- ruby/370A7qo.iDEgE37HZCCu4bf7GE37JWJLaBDR5IE37zZG.rb
+++ ruby/370A7qo.iDEgE37HZCCu4bf7GE37JWJLaBDR5IE37zZG.rb
@@ -1,64 +1,179 @@
-    if !PostRequests.include?(self.method)
-      if param_string.size > 0
-        str << '?' + param_string
-      end
+
+        
+        Nullam luctus fermentum est id blandit. Phasellus consectetur ullamcorper
+ligula, {% if author == 'Jane Doe' %} at finibus eros laoreet id. {% else %}
+Etiam sit amet est in libero efficitur.{% endif %}
+tristique. Ut nec magna augue. Quisque ut fringilla lacus, ac dictum enim.
+Aliquam vel ornare mauris. Suspendisse ornare diam tempor nulla facilisis
+aliquet. Sed ultrices placerat ultricies.
+LIQUID
+    
+    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
+COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
+    
+          #
+    
+            # rubocop:disable Metrics/AbcSize
+        def process(args, opts)
+          if !args || args.empty?
+            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
+          end
+    
+            def initialize
+          @websockets = []
+          @connections_count = 0
+          @started_event = Utils::ThreadEvent.new
+          @stopped_event = Utils::ThreadEvent.new
+        end
+    
+        def process(args)
+      arg_is_present? args, '--server', 'The --server command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
+                          use the 'build' subcommand.'
+      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
+                          '--watch'.'
+      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
+                          the '--watch' switch.'
+      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
+                          favour of 'highlighter'.'
+      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
+                          your config files.'
+      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
+                          config files.'
+      no_subcommand(args)
     end
-    str
+    
+            parsed_expr = parse_expression(expression)
+        @context.stack do
+          groups = input.group_by do |item|
+            @context[variable] = item
+            parsed_expr.render(@context)
+          end
+          grouped_array(groups)
+        end
+      end
+    
+        def order_basic_bumped
+      'CASE WHEN (topics.pinned_at IS NOT NULL) THEN 0 ELSE 1 END, topics.bumped_at DESC'
+    end
+    
+        req_type_id = req_types[req_type]
+    
+        def pause
+      stop!
+      @paused = true
+    end
+    
+      def self.period_types
+    @types ||= Enum.new(all: 1,
+                        yearly: 2,
+                        monthly: 3,
+                        weekly: 4,
+                        daily: 5,
+                        quarterly: 6)
   end
     
-            # Closes the connection
-        def close
-          if connection
-            connection.shutdown
-            connection.close unless connection.closed?
-          end
+        it 'rejects invalid directives and ones that are not allowed to be extended' do
+      builder << {
+        invalid_src: ['invalid'],
+      }
     
-              # Encodes the auth_time field
-          #
-          # @return [String]
-          def encode_auth_time
-            [auth_time].pack('N')
-          end
+          return response unless html_response?(headers)
+      ContentSecurityPolicy.base_url = request.host_with_port if Rails.env.development?
     
-              # Encodes the ticket field
-          #
-          # @return [String]
-          def encode_ticket
-            ticket.encode
-          end
+      def self.ensure_consistency!(last_seen = 1.hour.ago)
+    reset_bounce_scores
+    update_view_counts(last_seen)
+    update_first_unread(last_seen)
+  end
     
-                res = ''
-            case etype
-            when RC4_HMAC
-              res = encrypt_rc4_hmac(data, key, 5)
-            else
-              raise ::NotImplementedError, 'EncryptedData schema is not supported'
+          if api_key.user
+        api_key.user if !api_username || (api_key.user.username_lower == api_username.downcase)
+      elsif api_username
+        User.find_by(username_lower: api_username.downcase)
+      elsif user_id = header_api_key? ? @env[HEADER_API_USER_ID] : request['api_user_id']
+        User.find_by(id: user_id.to_i)
+      elsif external_id = header_api_key? ? @env[HEADER_API_USER_EXTERNAL_ID] : request['api_user_external_id']
+        SingleSignOnRecord.find_by(external_id: external_id.to_s).try(:user)
+      end
+    end
+  end
+    
+            # Builds an issue from a GitHub API response.
+        #
+        # issue - An instance of `Sawyer::Resource` containing the issue
+        #         details.
+        def self.from_api_response(issue)
+          user =
+            if issue.user
+              Representation::User.from_api_response(issue.user)
             end
     
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
+      it 'imports a scenario that does not exist yet' do
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
+    click_on 'Start Import'
+    expect(page).to have_text('This scenario has a few agents to get you started. Feel free to change them or delete them as you see fit!')
+    expect(page).not_to have_text('This Scenario already exists in your system.')
+    check('I confirm that I want to import these Agents.')
+    click_on 'Finish Import'
+    expect(page).to have_text('Import successful!')
+  end
     
-              # Decodes the Rex::Proto::Kerberos::Model::KdcRequest from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode KdcRequest, invalid input'
-            end
+      describe '#style_colors' do
+    it 'returns a css style-formated version of the scenario foreground and background colors' do
+      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
+    end
     
-              # @!attribute type
-          #   @return [Integer] The type of value
-          attr_accessor :type
-          # @!attribute value
-          #   @return [Time] the time of the last request
-          attr_accessor :value
+    describe LiquidMigrator do
+  describe 'converting JSONPath strings' do
+    it 'should work' do
+      expect(LiquidMigrator.convert_string('$.data', true)).to eq('{{data}}')
+      expect(LiquidMigrator.convert_string('$.data.test', true)).to eq('{{data.test}}')
+      expect(LiquidMigrator.convert_string('$first_title', true)).to eq('{{first_title}}')
+    end
+    
+          @log.level = -1
+      expect(@log).not_to be_valid
+      expect(@log).to have(1).error_on(:level)
+    
+        def str_to_byte_pos(pos)
+      @s.string.slice(0, pos).bytesize
+    end
+  end
+end
+    
+      # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+    
+      config.active_support.test_order = :random
+    
+      def test_image_helper
+    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
+  end
+    
+            def declaration_with_comment(node)
+          buffer = processed_source.buffer
+          begin_pos = get_source_range(node, comments_as_separators).begin_pos
+          end_line = buffer.line_for_position(node.loc.expression.end_pos)
+          end_pos = buffer.line_range(end_line).end_pos
+          Parser::Source::Range.new(buffer, begin_pos, end_pos)
+        end
+    
+              ignored_end_pos = if ignored_loc.respond_to?(:heredoc_body)
+                              ignored_loc.heredoc_end.end_pos
+                            else
+                              ignored_loc.expression.end_pos
+                            end
+          ignored_end_pos >= node.source_range.end_pos
+        end
+      end
+    
+            def message_for_underscore_prefix(variable)
+          'If it's necessary, use `_` or `_#{variable.name}` ' \
+          'as an argument name to indicate that it won't be used.'
+        end
