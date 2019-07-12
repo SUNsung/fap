eee45b4cf3af5b418be2b826e9a41c5dc091744a
SUNsung <weatherius@gmail.com>
2019-07-12 04:03:23 +0300

    Rewrite [35QvFVI5T0WoY35Qtylh9ddWAg35NB4MVqbDOlg35aZ.py] File in: 2019-07-12 04:03:22

--- pyton/35QvFVI5T0WoY35Qtylh9ddWAg35NB4MVqbDOlg35aZ.py
+++ pyton/35QvFVI5T0WoY35Qtylh9ddWAg35NB4MVqbDOlg35aZ.py
@@ -1,63 +1,30 @@
 
         
-            @property
-    def content_type(self):
-        '''Return the message content type.'''
-        ct = self._orig.headers.get('Content-Type', '')
-        if not isinstance(ct, str):
-            ct = ct.decode('utf8')
-        return ct
+        
+# def get_all_subset(tokens: list):
+#     '''获取 tokens 的所有非空子集'''
+#     tokens = set(tokens)
+#
+#     ret = []
+#     for i in range(1, len(tokens) + 1):
+#         ret.extend(list(combinations(tokens, i)))
+#
+#     return ret
+    
+        with tf.variable_scope(name, reuse=reuse):
+        alpha = get_w(alpha_shape, w_initializer=alpha_init, name='alpha')
+        # o = relu(x) + 0.5 * tf.multiply(alpha, x - tf.abs(x))  # TFLearn
+        o = leaky_relu(x, alpha)  # TensorLayer / <Deep Learning>
+    
+            a = permute(a, [0, 2, 1])  # [batch_size, n_step, n_input]
+        o = tf.multiply(x, a)  # # [batch_size, n_step, n_input]
+    
+    References:
+    keras.regularizers
+'''
+import tensorflow as tf
+import numpy as np
     
     
-class Conversion(object):
-    
-        def iter_body(self):
-    
-    
-# noinspection PyAbstractClass
-class BuiltinAuthPlugin(AuthPlugin):
-    
-    
-def load_json_preserve_order(s):
-    return json.loads(s, object_pairs_hook=OrderedDict)
-    
-    
-def test_credentials_in_url(httpbin_both):
-    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
-                   auth='user:password')
-    r = http('GET', url)
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
-    
-        def test_POST_form_auto_Content_Type(self, httpbin):
-        r = http('--form', 'POST', httpbin.url + '/post')
-        assert HTTP_OK in r
-        assert ''Content-Type': 'application/x-www-form-urlencoded' in r
-    
-    
-@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
-def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
-    r = http(follow_flag, httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 1
-    assert HTTP_OK in r
-    
-    
-def trim_filename_if_needed(filename, directory='.', extra=0):
-    max_len = get_filename_max_length(directory) - extra
-    if len(filename) > max_len:
-        filename = trim_filename(filename, max_len)
-    return filename
-    
-      Returns:
-    True if the line at 'linenum' is inside something that expects arguments
-    of function types.
-  '''
-  line = clean_lines.elided[linenum]
-  return (Match(r'^\s*MOCK_(CONST_)?METHOD\d+(_T)?\(', line) or
-          (linenum >= 2 and
-           (Match(r'^\s*MOCK_(?:CONST_)?METHOD\d+(?:_T)?\((?:\S+,)?\s*$',
-                  clean_lines.elided[linenum - 1]) or
-            Match(r'^\s*MOCK_(?:CONST_)?METHOD\d+(?:_T)?\(\s*$',
-                  clean_lines.elided[linenum - 2]) or
-            Search(r'\bstd::m?function\s*\<\s*$',
-                   clean_lines.elided[linenum - 1]))))
+def foo():
+    print('foo')
