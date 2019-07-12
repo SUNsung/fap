931d3a5a54ed71425a83997a0aa531f820377a14
SUNsung <weatherius@gmail.com>
2019-07-12 15:07:10 +0300

    Rewrite [44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp] File in: 2019-07-12 15:07:10

--- cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
+++ cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
@@ -1,207 +1,139 @@
 
         
-        private:
-    NetworkStyle(const QString &appName, const int iconColorHueShift, const int iconColorSaturationReduction, const char *titleAddText);
+        public:
+    explicit OpenURIDialog(QWidget *parent);
+    ~OpenURIDialog();
     
-    protected Q_SLOTS:
-    void accept();
-    
-    private:
-    reverse_lock(reverse_lock const&);
-    reverse_lock& operator=(reverse_lock const&);
-    
-      // When user keys are different, but correctly ordered
-  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('hello', 200, kTypeValue)));
-    
-    // Called on every log record (each one of which is a WriteBatch)
-// found in a kDescriptorFile.
-static void VersionEditPrinter(uint64_t pos, Slice record, WritableFile* dst) {
-  std::string r = '--- offset ';
-  AppendNumberTo(&r, pos);
-  r += '; ';
-  VersionEdit edit;
-  Status s = edit.DecodeFrom(record);
-  if (!s.ok()) {
-    r += s.ToString();
-    r.push_back('\n');
-  } else {
-    r += edit.DebugString();
-  }
-  dst->Append(r);
-}
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    std::unique_ptr<php::Unit> parse_unit(php::Program& prog,
-                                      std::unique_ptr<UnitEmitter> ue);
-    
-    void Assembler::limmediate(const Reg64& rt, int64_t imm64,
-                           ImmType immt, bool immMayChange) {
-  static_assert(
-      std::is_unsigned<
-        decltype(HPHP::RuntimeOption::EvalPPC64MinTOCImmSize)>::value,
-      'RuntimeOption::EvalPPC64MinTOCImmSize is expected to be unsigned.');
-  always_assert(HPHP::RuntimeOption::EvalPPC64MinTOCImmSize <= 64);
-    }
-    
-        dword(vx_formater.instruction);
-  }
-  //TODO(rcardoso): Unimplemented instruction formaters
-  void EmitXSForm(const uint8_t op,
-                  const RegNumber rt,
-                  const RegNumber ra,
-                  const uint8_t sh,
-                  const uint16_t xop,
-                  const bool rc = 0){
-    
-    
-    {  // We had a counted inner array---we need to do an O(N) copy to get the
-  // collection into the request local heap.
-  auto const apcArr = APCArray::fromHandle(m_arrayHandle);
-  auto const col = Object::attach(collections::alloc(m_colType));
-  switch (m_colType) {
-  case CollectionType::ImmVector:
-  case CollectionType::Vector:
-    fillCollection(static_cast<BaseVector*>(col.get()), apcArr);
-    break;
-  case CollectionType::ImmSet:
-  case CollectionType::Set:
-    fillCollection(static_cast<BaseSet*>(col.get()), apcArr);
-    break;
-  case CollectionType::ImmMap:
-  case CollectionType::Map:
-    fillMap(static_cast<BaseMap*>(col.get()), apcArr);
-    break;
-  case CollectionType::Pair:
-    always_assert(0);
-    break;
-  }
-  return col;
-}
-    
-    const StaticString
-  s_wrapper_type('wrapper_type'),
-  s_stream_type('stream_type'),
-  s_mode('mode'),
-  s_unread_bytes('unread_bytes'),
-  s_seekable('seekable'),
-  s_timed_out('timed_out'),
-  s_blocked('blocked'),
-  s_eof('eof'),
-  s_plainfile('plainfile'),
-  s_dir('dir'),
-  s_r('r');
-    
-    void logAHMSubMapWarning(folly::StringPiece mapName) {
-  StackTrace st;
-  logPerfWarning(
-    'AtomicHashMap overflow',
-    [&](StructuredLogEntry& cols) {
-      cols.setStr('map_name', mapName);
-      cols.setStackTrace('stack', st);
-    }
-  );
-}
-    
-      Pipe();
-  virtual ~Pipe();
-    
-    
-    
-    #endif  // ROCKSDB_LITE
+    #endif // BITCOIN_REVERSELOCK_H
 
     
-    namespace {
-// A dummy compaction filter
-class DummyCompactionFilter : public CompactionFilter {
+        BOOST_CHECK(v.setBool(false));
+    BOOST_CHECK_EQUAL(v.isBool(), true);
+    BOOST_CHECK_EQUAL(v.isTrue(), false);
+    BOOST_CHECK_EQUAL(v.isFalse(), true);
+    BOOST_CHECK_EQUAL(v.getBool(), false);
+    
+    // Filter policy wrapper that converts from internal keys to user keys
+class InternalFilterPolicy : public FilterPolicy {
+ private:
+  const FilterPolicy* const user_policy_;
  public:
-  virtual ~DummyCompactionFilter() {}
-  virtual bool Filter(int level, const Slice& key, const Slice& existing_value,
-                      std::string* new_value, bool* value_changed) const {
-    return false;
+  explicit InternalFilterPolicy(const FilterPolicy* p) : user_policy_(p) { }
+  virtual const char* Name() const;
+  virtual void CreateFilter(const Slice* keys, int n, std::string* dst) const;
+  virtual bool KeyMayMatch(const Slice& key, const Slice& filter) const;
+};
+    
+      inline int offset(const int n, const int c = 0, const int h = 0,
+      const int w = 0) const {
+    CHECK_GE(n, 0);
+    CHECK_LE(n, num());
+    CHECK_GE(channels(), 0);
+    CHECK_LE(c, channels());
+    CHECK_GE(height(), 0);
+    CHECK_LE(h, height());
+    CHECK_GE(width(), 0);
+    CHECK_LE(w, width());
+    return ((n * channels() + c) * height() + h) * width() + w;
   }
-  virtual const char* Name() const { return 'DummyCompactionFilter'; }
-};
-    }
     
-    std::string kDBPath = '/tmp/rocksdb_transaction_example';
-    
-    #include 'rocksdb/db.h'
-    
-    
-    { protected:
-  virtual ~Snapshot();
-};
-    
-    #include <map>
-#include <string>
-#include <unordered_set>
-#include <vector>
-    
-    
-    {    // Rendering parameters
-    const auto FACE_DEFAULT_ALPHA_KEYPOINT = POSE_DEFAULT_ALPHA_KEYPOINT;
-    const auto FACE_DEFAULT_ALPHA_HEAT_MAP = POSE_DEFAULT_ALPHA_HEAT_MAP;
-}
-    
-    namespace op
-{
-    // This worker will do 3-D rendering
-    class OP_API GuiAdam : public Gui
-    {
-    public:
-        GuiAdam(const Point<int>& outputSize, const bool fullScreen,
-                const std::shared_ptr<std::atomic<bool>>& isRunningSharedPtr,
-                const std::shared_ptr<std::pair<std::atomic<bool>, std::atomic<int>>>& videoSeekSharedPtr = nullptr,
-                const std::vector<std::shared_ptr<PoseExtractorNet>>& poseExtractorNets = {},
-                const std::vector<std::shared_ptr<FaceExtractorNet>>& faceExtractorNets = {},
-                const std::vector<std::shared_ptr<HandExtractorNet>>& handExtractorNets = {},
-                const std::vector<std::shared_ptr<Renderer>>& renderers = {},
-                const DisplayMode displayMode = DisplayMode::DisplayAll,
-                const std::shared_ptr<const TotalModel>& totalModel = nullptr,
-                const std::string& adamRenderedVideoPath = '');
-    }
-    }
-    
-    #include <openpose/pose/poseParameters.hpp>
-#include <openpose/pose/poseParametersRender.hpp>
-    
-    template <class UIntType, size_t w, size_t s, size_t r>
-struct StateSize<std::subtract_with_carry_engine<UIntType, w, s, r>> {
-  // [rand.eng.sub]: r * ceil(w / 32)
-  using type = std::integral_constant<size_t, r*((w + 31) / 32)>;
-};
+    #define INSTANTIATE_LAYER_GPU_BACKWARD(classname) \
+  template void classname<float>::Backward_gpu( \
+      const std::vector<Blob<float>*>& top, \
+      const std::vector<bool>& propagate_down, \
+      const std::vector<Blob<float>*>& bottom); \
+  template void classname<double>::Backward_gpu( \
+      const std::vector<Blob<double>*>& top, \
+      const std::vector<bool>& propagate_down, \
+      const std::vector<Blob<double>*>& bottom)
     
       /**
-   * Returns a random uint32_t given a specific RNG
+   * @brief Return whether 'anonymous' top blobs are created automatically
+   *        by the layer.
+   *
+   * If this method returns true, Net::Init will create enough 'anonymous' top
+   * blobs to fulfill the requirement specified by ExactNumTopBlobs() or
+   * MinTopBlobs().
    */
-  template <class RNG, class /* EnableIf */ = ValidRNG<RNG>>
-  static uint32_t rand32(RNG&& rng) {
-    return rng();
-  }
+  virtual inline bool AutoTopBlobs() const { return false; }
     
-    /// Returns the best real CacheLocality information available
-static CacheLocality getSystemLocalityInfo() {
-  if (kIsLinux) {
-    try {
-      return CacheLocality::readFromSysfs();
-    } catch (...) {
-      // keep trying
+    #include 'caffe/layers/loss_layer.hpp'
+    
+      Blob<int> offsets;
+  Blob<int> src_strides_;
+  Blob<int> dest_strides_;
+    
+    
+    {  size_t *workspace_fwd_sizes_;
+  size_t *workspace_bwd_data_sizes_;
+  size_t *workspace_bwd_filter_sizes_;
+  size_t workspaceSizeInBytes;  // size of underlying storage
+  void *workspaceData;  // underlying storage
+  void **workspace;  // aliases into workspaceData
+};
+#endif
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+        // Default Precision to use for Rational calculations
+    inline constexpr int32_t RATIONAL_PRECISION = 128;
+    
+    #include <vector>
+#include 'Ratpack/ratpak.h'
+    
+    
+    {    m_HistoryCollector.ClearHistoryLine(errorString);
+}
+
+    
+    class CBinaryCommand final : public IBinaryCommand
+{
+public:
+    CBinaryCommand(int command);
+    void SetCommand(int command) override;
+    int GetCommand() const override;
+    CalculationManager::CommandType GetCommandType() const override;
+    void Accept(_In_ ISerializeCommandVisitor& commandVisitor) override;
     }
-  }
-    }
     
-    #include <cstdint>
-#include <limits>
-#include <random>
-#include <set>
+    class IUnaryCommand : public IOperatorCommand
+{
+public:
+    virtual const std::shared_ptr<CalculatorVector<int>>& GetCommands() const = 0;
+    virtual void SetCommands(int command1, int command2) = 0;
+};
     
-      writeIntegerString<uint8_t, 3>(octets[0], &buf);
-  *(buf++) = '.';
-  writeIntegerString<uint8_t, 3>(octets[1], &buf);
-  *(buf++) = '.';
-  writeIntegerString<uint8_t, 3>(octets[2], &buf);
-  *(buf++) = '.';
-  writeIntegerString<uint8_t, 3>(octets[3], &buf);
+    //-----------------------------------------------------------------------------
+//  Package Title  ratpak
+//  File           itrans.c
+//  Copyright      (C) 1995-96 Microsoft
+//  Date           01-16-95
+//
+//
+//  Description
+//
+//     Contains inverse sin, cos, tan functions for rationals
+//
+//  Special Information
+//
+//-----------------------------------------------------------------------------
+#include 'ratpak.h'
+    
+            DUPNUM(n2, num_one);
+    
+    //-----------------------------------------------------------------------------
+//
+//  FUNCTION: rootrat
+//
+//  PARAMETERS: y prat representation of number to take the root of
+//              n prat representation of the root to take.
+//
+//  RETURN: bth root of a in rat form.
+//
+//  EXPLANATION: This is now a stub function to powrat().
+//
+//-----------------------------------------------------------------------------
