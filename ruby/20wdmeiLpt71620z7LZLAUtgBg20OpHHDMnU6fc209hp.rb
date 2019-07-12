f0f592a26680556941e804a20621a7c464f3ea8d
SUNsung <weatherius@gmail.com>
2019-07-12 10:58:19 +0300

    Rewrite [20wdmeiLpt71620z7LZLAUtgBg20OpHHDMnU6fc209hp.rb] File in: 2019-07-12 10:58:19

--- ruby/20wdmeiLpt71620z7LZLAUtgBg20OpHHDMnU6fc209hp.rb
+++ ruby/20wdmeiLpt71620z7LZLAUtgBg20OpHHDMnU6fc209hp.rb
@@ -1,57 +1,90 @@
-      class MergeRequest < ActiveRecord::Base
-        self.table_name = 'merge_requests'
+
+        
+          private
     
-              pipelines.each do |pipeline|
-            self.new(pipeline).tap do |preloader|
-              preloader.preload_commit_authors
-              preloader.preload_pipeline_warnings
-              preloader.preload_stages_warnings
-            end
+      # Helper for use in before_actions where no authentication is required.
+  #
+  # Example:
+  #   before_action :require_no_authentication, only: :new
+  def require_no_authentication
+    assert_is_devise_resource!
+    return unless is_navigational_format?
+    no_input = devise_mapping.no_input_strategies
+    
+    module Devise
+  module Controllers
+    # Provide sign in and sign out functionality.
+    # Included by default in all controllers.
+    module SignInOut
+      # Return true if the given scope is signed in session. If no scope given, return
+      # true if any scope is signed in. This will run authentication hooks, which may
+      # cause exceptions to be thrown from this method; if you simply want to check
+      # if a scope has already previously been authenticated without running
+      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
+      def signed_in?(scope=nil)
+        [scope || Devise.mappings.keys].flatten.any? do |_scope|
+          warden.authenticate?(scope: _scope)
+        end
+      end
+    
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
+    
+          protected
+    
+            # Generate a token checking if one does not already exist in the database.
+        def remember_token #:nodoc:
+          loop do
+            token = Devise.friendly_token
+            break token unless to_adapter.find_first({ remember_token: token })
           end
         end
     
-            def sidekiq_worker_class
-          ImportIssueWorker
-        end
-    
-    module Gitlab
-  module GithubImport
-    module Importer
-      class NotesImporter
-        include ParallelScheduling
-    
-    <script>
-  // reading
-  var es = new EventSource('/stream');
-  es.onmessage = function(e) { $('#chat').append(e.data + '\n') };
-    
-      # insert data
-  fields.each do |field, values|
-    updated = '  s.#{field} = ['
-    updated << values.map { |v| '\n    %p' % v }.join(',')
-    updated << '\n  ]'
-    content.sub!(/  s\.#{field} = \[\n(    .*\n)*  \]/, updated)
-  end
-    
-          def empty_cookie(host, path)
-        {:value => '', :domain => host, :path => path, :expires => Time.at(0)}
+          def timeout_in
+        self.class.timeout_in
       end
     
-          expected_header = <<-END.chomp
-rack.%2573ession=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-END
-      expect(last_response.headers['Set-Cookie']).to eq(expected_header)
-    end
-  end
+      # :call-seq:
+  #   def_single_delegator(accessor, method, new_name=method)
+  #
+  # Defines a method _method_ which delegates to _accessor_ (i.e. it calls
+  # the method of the same name in _accessor_).  If _new_name_ is
+  # provided, it is used as the name for the delegate method.
+  def def_single_delegator(accessor, method, ali = method)
+    gen = Forwardable._delegator_method(self, accessor, method, ali)
     
-        it 'accepts jsfiddle link with a / at the end' do
-      jsfiddle_link = 'http://jsfiddle.net/link2twenty/v2kx9jcd/'
-      expect do
-        generate_new_liquid(jsfiddle_link)
-      end.not_to raise_error
+    require_relative 'nop'
+require_relative '../ext/loader'
+    
+    module IRB
+  # A class that wraps the current state of the irb session, including the
+  # configuration of IRB.conf.
+  class Context
+    # Creates a new IRB context.
+    #
+    # The optional +input_method+ argument:
+    #
+    # +nil+::     uses stdin or Reidline or Readline
+    # +String+::  uses a File
+    # +other+::   uses this as InputMethod
+    def initialize(irb, workspace = nil, input_method = nil, output_method = nil)
+      @irb = irb
+      if workspace
+        @workspace = workspace
+      else
+        @workspace = WorkSpace.new
+      end
+      @thread = Thread.current if defined? Thread
+    
+      module HistorySavingAbility # :nodoc:
+    def HistorySavingAbility.extended(obj)
+      IRB.conf[:AT_EXIT].push proc{obj.save_history}
+      obj.load_history
+      obj
     end
