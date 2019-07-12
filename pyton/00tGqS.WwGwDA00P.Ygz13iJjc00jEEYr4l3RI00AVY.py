19c9c92e43df10f29e0e897adac1fd54b6bb02fa
SUNsung <weatherius@gmail.com>
2019-07-12 08:07:49 +0300

    Rewrite [00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py] File in: 2019-07-12 08:07:49

--- pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
+++ pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
@@ -1,171 +1,164 @@
 
         
-            Returns:
-      The test predictions along with their scores.
-    '''
-    test_pred = [0] * len(inputs)
+        if S < N:
+  # Note that this isn't necessary for this synthetic example, but
+  # it's useful to see how the input factor matrices were initialized
+  # for actual neurophysiology data.
+  datasets = add_alignment_projections(datasets, npcs=FLAGS.npcs)
     
-        # You probably do not want the LL associated values when pushing the mean
-    # instead of sampling.
-    model_runs['costs'] = costs
-    model_runs['nll_bound_vaes'] = nll_bound_vaes
-    model_runs['nll_bound_iwaes'] = nll_bound_iwaes
-    model_runs['train_steps'] = train_steps
-    return model_runs
-    
-    DATA_DIR = '/tmp/rnn_synth_data_v1.0/'
-DATA_FILENAME_STEM = 'chaotic_rnn_inputs_g1p5'
-LFADS_SAVE_DIR = '/tmp/lfads_chaotic_rnn_inputs_g1p5/'
-CO_DIM = 1
-DO_CAUSAL_CONTROLLER = False
-DO_FEED_FACTORS_TO_CONTROLLER = True
-CONTROLLER_INPUT_LAG = 1
-PRIOR_AR_AUTOCORRELATION = 10.0
-PRIOR_AR_PROCESS_VAR = 0.1
-DO_TRAIN_PRIOR_AR_ATAU = True
-DO_TRAIN_PRIOR_AR_NVAR = True
-CI_ENC_DIM = 128
-CON_DIM = 128
-CO_PRIOR_VAR_SCALE = 0.1
-KL_INCREASE_STEPS = 2000
-L2_INCREASE_STEPS = 2000
-L2_GEN_SCALE = 2000.0
-L2_CON_SCALE = 0.0
-# scale of regularizer on time correlation of inferred inputs
-CO_MEAN_CORR_SCALE = 0.0
-KL_IC_WEIGHT = 1.0
-KL_CO_WEIGHT = 1.0
-KL_START_STEP = 0
-L2_START_STEP = 0
-IC_PRIOR_VAR_MIN = 0.1
-IC_PRIOR_VAR_SCALE = 0.1
-IC_PRIOR_VAR_MAX = 0.1
-IC_POST_VAR_MIN = 0.0001      # protection from KL blowing up
-    
-    def get_data_batch(batch_size, T, rng, u_std):
-  u_bxt = rng.randn(batch_size, T) * u_std
-  running_sum_b = np.zeros([batch_size])
-  labels_bxt = np.zeros([batch_size, T])
-  for t in xrange(T):
-    running_sum_b += u_bxt[:, t]
-    labels_bxt[:, t] += running_sum_b
-  labels_bxt = np.clip(labels_bxt, -1, 1)
-  return u_bxt, labels_bxt
-    
-    # just one dataset here
-datasets = {}
-dataset_name = 'dataset_N' + str(N)
-datasets[dataset_name] = data
-    
-      # Now for each dataset, we regress the channel data onto the top
-  # pcs, and this will be our alignment matrix for that dataset.
-  # |B - A*W|^2
-  for name, dataset in datasets.items():
-    cidx_s = channel_idxs[name][0]
-    cidx_f = channel_idxs[name][1]
-    all_data_zm_chxtc = all_data_zm_nxtc[cidx_s:cidx_f,:] # ch for channel
-    W_chxp, _, _, _ = \
-        np.linalg.lstsq(all_data_zm_chxtc.T, all_data_pca_pxtc.T)
-    dataset['alignment_matrix_cxf'] = W_chxp
-    alignment_bias_cx1 = all_data_mean_nx1[cidx_s:cidx_f]
-    dataset['alignment_bias_c'] = np.squeeze(alignment_bias_cx1, axis=1)
-    
-      def __init__(self, filepattern, vocab):
-    '''Initialize LM1BDataset reader.
-    
-          scorings.append(dict(
-          correctness=correctness,
-          sentence=sentence,
-          joint_prob=joint_prob,
-          word_probs=word_probs))
-    scoring_mode = 'full' if self.test_data_name == 'pdp60' else 'partial'
-    return utils.compare_substitutions(
-        self.question_ids, scorings, scoring_mode)
+        states_t_bxn, outputs_t_bxn = sess.run([states_t, outputs_t],
+                                           feed_dict=feed_dict)
+    states_nxt = np.transpose(np.squeeze(np.asarray(states_t_bxn)))
+    outputs_t_bxn = np.squeeze(np.asarray(outputs_t_bxn))
+    r_sxt = np.dot(P_nxn, states_nxt)
     
     
-def recursive_length(item):
-  '''Recursively determine the total number of elements in nested list.'''
-  if type(item) == list:
-    return sum(recursive_length(subitem) for subitem in item)
-  else:
-    return 1.
+def create_discriminator(hparams,
+                         sequence,
+                         is_training,
+                         reuse=None,
+                         initial_state=None,
+                         inputs=None,
+                         present=None):
+  '''Create the Discriminator model specified by the FLAGS and hparams.
     
-        # Group maintain averages op.
-    if averages_op:
-      gen_train_op = tf.group(maximize_op, averages_op)
-    else:
-      gen_train_op = maximize_op
+      # Cumulative Discounted Returns.  The true value function V*(s).
+  cumulative_rewards = []
+  for t in xrange(FLAGS.sequence_length):
+    cum_value = tf.zeros(shape=[FLAGS.batch_size])
+    for s in xrange(t, FLAGS.sequence_length):
+      cum_value += missing_list[s] * np.power(gamma, (s - t)) * rewards_list[s]
+    cumulative_rewards.append(cum_value)
+  cumulative_rewards = tf.stack(cumulative_rewards, axis=1)
     
-        def database_backwards(self, app_label, schema_editor, from_state, to_state):
-        schema_editor.execute('DROP EXTENSION %s' % schema_editor.quote_name(self.name))
-        # Clear cached, stale oids.
-        get_hstore_oids.cache_clear()
-        get_citext_oids.cache_clear()
+      Returns:
+    variable_mapping:  Dictionary with Key: ckpt_name, Value: model_varself.
+  '''
+  assert (FLAGS.discriminator_model == 'bidirectional_zaremba' or
+          FLAGS.discriminator_model == 'bidirectional_vd')
+  assert hparams.dis_num_layers == 2
     
-        def __eq__(self, other):
-        return (
-            isinstance(other, self.__class__) and
-            self.keys == other.keys and
-            self.messages == other.messages and
-            self.strict == other.strict
-        )
+        with tf.variable_scope('rnn') as vs:
+      outputs, _, _ = tf.contrib.rnn.static_bidirectional_rnn(
+          cell_fwd, cell_bwd, rnn_inputs, state_fwd, state_bwd, scope=vs)
     
-            r = None
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+    
+            if module.params.get('next_marker'):
+            params['Marker'] = module.params.get('next_marker')
         try:
-            r = Redirect.objects.get(site=current_site, old_path=full_path)
-        except Redirect.DoesNotExist:
-            pass
-        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
-            try:
-                r = Redirect.objects.get(
-                    site=current_site,
-                    old_path=request.get_full_path(force_append_slash=True),
-                )
-            except Redirect.DoesNotExist:
-                pass
-        if r is not None:
-            if r.new_path == '':
-                return self.response_gone_class()
-            return self.response_redirect_class(r.new_path)
-    
-        def delete(self, session_key=None):
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        self._cache.delete(self.cache_key_prefix + session_key)
-    
-            if data is None:
-            s = self._get_session_from_db()
-            if s:
-                data = self.decode(s.session_data)
-                self._cache.set(self.cache_key, data, self.get_expiry_age(expiry=s.expire_date))
+            lambda_facts.update(aliases=client.list_aliases(FunctionName=function_name, **params)['Aliases'])
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                lambda_facts.update(aliases=[])
             else:
-                data = {}
-        return data
+                module.fail_json_aws(e, msg='Trying to get aliases')
+    else:
+        module.fail_json(msg='Parameter function_name required for query=aliases.')
     
-        def create(self):
-        while True:
-            self._session_key = self._get_new_session_key()
-            try:
-                # Save immediately to ensure we have a unique entry in the
-                # database.
-                self.save(must_create=True)
-            except CreateError:
-                # Key wasn't unique. Try again.
-                continue
-            self.modified = True
-            return
+        # build list of params
+    params = {}
     
-    OLD_REWRITE_HTTPS_ARGS = [
-    ['^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[L,QSA,R=permanent]'],
-    ['^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[END,QSA,R=permanent]']]
+        # Build the deployment object we return
+    deployment = dict(token=token, url=url)
+    deployment.update(body)
+    if 'errorMessage' in deployment:
+        message = deployment.pop('errorMessage')
+        deployment['message'] = message
+    
+    # Copyright 2014 Benjamin Curtis <benjamin.curtis@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+    
+        def manage(self):
+        rc, out = self._exec(['list'])
+        if rc != 0:
+            self.module.fail_json(msg='Unable to list icinga2 features. '
+                                      'Ensure icinga2 is installed and present in binary path.')
+    
+        if running and state == 'stopped':
+        if module.check_mode:
+            module.exit_json(changed=True)
+        status = run_command('stop')
+        if status in ['not monitored'] or 'stop pending' in status:
+            module.exit_json(changed=True, name=name, state=state)
+        module.fail_json(msg='%s process not stopped' % name, status=status)
+    
+        if root.get('total-models') == '0':
+        return None
+    
+        def syntax(self):
+        return '[options] <url>'
+    
+                # trustRoot set to platformTrust() will use the platform's root CAs.
+            #
+            # This means that a website like https://www.cacert.org will be rejected
+            # by default, since CAcert.org CA certificate is seldom shipped.
+            return optionsForClientTLS(hostname.decode('ascii'),
+                                       trustRoot=platformTrust(),
+                                       extraCertificateOptions={
+                                            'method': self._ssl_method,
+                                       })
+    
+            # If no credentials could be found anywhere,
+        # consider this an anonymous connection request by default;
+        # unless 'anon' was set explicitly (True/False).
+        anon = kw.get('anon')
+        if anon is None and not aws_access_key_id and not aws_secret_access_key:
+            kw['anon'] = True
+        self.anon = kw.get('anon')
+    
+        def _debug_cookie(self, request, spider):
+        if self.debug:
+            cl = [to_native_str(c, errors='replace')
+                  for c in request.headers.getlist('Cookie')]
+            if cl:
+                cookies = '\n'.join('Cookie: {}\n'.format(c) for c in cl)
+                msg = 'Sending cookies to: {}\n{}'.format(request, cookies)
+                logger.debug(msg, extra={'spider': spider})
     
     
-Examples
---------
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
+
     
     
-# -- Options for LaTeX output ---------------------------------------------
+KEY = jose.JWKRSA.load(test_util.load_vector('rsa512_key.pem'))
     
+            self.vhosts.append(
+            obj.VirtualHost(
+                'path', 'aug_path', set([obj.Addr.fromstring('*:80')]),
+                False, False,
+                'wildcard.com', set(['*.wildcard.com'])))
+    
+       # Cloudflare API credentials used by Certbot
+   dns_cloudflare_email = cloudflare@example.com
+   dns_cloudflare_api_key = 0123456789abcdef0123456789abcdef01234567
+    
+        def reset(self):
+        return 0
+
+    
+        Args:
+        a (Optional[int, str]): None seeds from an operating system specific randomness source.
+        max_bytes: Maximum number of bytes to use in the seed.
     '''
+    # Adapted from https://svn.python.org/projects/python/tags/r32/Lib/random.py
+    if a is None:
+        a = _bigint_from_bytes(os.urandom(max_bytes))
+    elif isinstance(a, str):
+        a = a.encode('utf8')
+        a += hashlib.sha512(a).digest()
+        a = _bigint_from_bytes(a[:max_bytes])
+    elif isinstance(a, integer_types):
+        a = a % 2**(8 * max_bytes)
+    else:
+        raise error.Error('Invalid type for seed: {} ({})'.format(type(a), a))
+    
+    class StatsRecorder(object):
+    def __init__(self, directory, file_prefix, autoreset=False, env_id=None):
+        self.autoreset = autoreset
+        self.env_id = env_id
+    
+        def render(self, mode=None):
+        pass
