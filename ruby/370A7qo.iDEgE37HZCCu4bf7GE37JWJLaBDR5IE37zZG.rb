3a93a67726e0961eb341d4a2789e0dab0161a97c
SUNsung <weatherius@gmail.com>
2019-07-12 20:25:49 +0300

    Rewrite [370A7qo.iDEgE37HZCCu4bf7GE37JWJLaBDR5IE37zZG.rb] File in: 2019-07-12 20:25:49

--- ruby/370A7qo.iDEgE37HZCCu4bf7GE37JWJLaBDR5IE37zZG.rb
+++ ruby/370A7qo.iDEgE37HZCCu4bf7GE37JWJLaBDR5IE37zZG.rb
@@ -1,179 +1,180 @@
 
         
-        Nullam luctus fermentum est id blandit. Phasellus consectetur ullamcorper
-ligula, {% if author == 'Jane Doe' %} at finibus eros laoreet id. {% else %}
-Etiam sit amet est in libero efficitur.{% endif %}
-tristique. Ut nec magna augue. Quisque ut fringilla lacus, ac dictum enim.
-Aliquam vel ornare mauris. Suspendisse ornare diam tempor nulla facilisis
-aliquet. Sed ultrices placerat ultricies.
-LIQUID
+            if resource.errors.empty?
+      set_flash_message! :notice, :unlocked
+      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
     
-    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
-COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
+          protected
     
-          #
+          when IAX_CTRL_PROCEED
+        dprint('PROCEED')
     
-            # rubocop:disable Metrics/AbcSize
-        def process(args, opts)
-          if !args || args.empty?
-            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
+    =begin
+ +------+----------------+-------------------------------------------+
+   | HEX  | NAME           | DESCRIPTION                               |
+   +------+----------------+-------------------------------------------+
+   | HEX  | NAME           | DESCRIPTION                               |
+   | 0x01 | CALLED NUMBER  | Number/extension being called             |
+   | 0x02 | CALLING NUMBER | Calling number                            |
+   | 0x03 | CALLING ANI    | Calling number ANI for billing            |
+   | 0x04 | CALLING NAME   | Name of caller                            |
+   | 0x05 | CALLED CONTEXT | Context for number                        |
+   | 0x06 | USERNAME       | Username (peer or user) for               |
+   |      |                | authentication                            |
+   | 0x07 | PASSWORD       | Password for authentication               |
+   | 0x08 | CAPABILITY     | Actual CODEC capability                   |
+   | 0x09 | FORMAT         | Desired CODEC format                      |
+   | 0x0a | LANGUAGE       | Desired language                          |
+   | 0x0b | VERSION        | Protocol version                          |
+   | 0x0c | ADSICPE        | CPE ADSI capability                       |
+   | 0x0d | DNID           | Originally dialed DNID                    |
+   | 0x0e | AUTHMETHODS    | Authentication method(s)                  |
+   | 0x0f | CHALLENGE      | Challenge data for MD5/RSA                |
+   | 0x10 | MD5 RESULT     | MD5 challenge result                      |
+   | 0x11 | RSA RESULT     | RSA challenge result                      |
+   | 0x12 | APPARENT ADDR  | Apparent address of peer                  |
+   | 0x13 | REFRESH        | When to refresh registration              |
+   | 0x14 | DPSTATUS       | Dialplan status                           |
+   | 0x15 | CALLNO         | Call number of peer                       |
+   | 0x16 | CAUSE          | Cause                                     |
+   | 0x17 | IAX UNKNOWN    | Unknown IAX command                       |
+   | 0x18 | MSGCOUNT       | How many messages waiting                 |
+   | 0x19 | AUTOANSWER     | Request auto-answering                    |
+   | 0x1a | MUSICONHOLD    | Request musiconhold with QUELCH           |
+   | 0x1b | TRANSFERID     | Transfer Request Identifier               |
+   | 0x1c | RDNIS          | Referring DNIS                            |
+   | 0x1d | Reserved       | Reserved for future use                   |
+   | 0x1e | Reserved       | Reserved for future use                   |
+   | 0x1f | DATETIME       | Date/Time                                 |
+   | 0x20 | Reserved       | Reserved for future use                   |
+   | 0x21 | Reserved       | Reserved for future use                   |
+   | 0x22 | Reserved       | Reserved for future use                   |
+   | 0x23 | Reserved       | Reserved for future use                   |
+   | 0x24 | Reserved       | Reserved for future use                   |
+   | 0x25 | Reserved       | Reserved for future use                   |
+   | 0x26 | CALLINGPRES    | Calling presentation                      |
+   | 0x27 | CALLINGTON     | Calling type of number                    |
+   | 0x28 | CALLINGTNS     | Calling transit network select            |
+   | 0x29 | SAMPLINGRATE   | Supported sampling rates                  |
+   | 0x2a | CAUSECODE      | Hangup cause                              |
+   | 0x2b | ENCRYPTION     | Encryption format                         |
+   | 0x2c | ENCKEY         | Reserved for future Use                   |
+   | 0x2d | CODEC PREFS    | CODEC Negotiation                         |
+   | 0x2e | RR JITTER      | Received jitter, as in RFC 3550           |
+   | 0x2f | RR LOSS        | Received loss, as in RFC 3550             |
+   | 0x30 | RR PKTS        | Received frames                           |
+   | 0x31 | RR DELAY       | Max playout delay for received frames in  |
+   |      |                | ms                                        |
+   | 0x32 | RR DROPPED     | Dropped frames (presumably by jitter      |
+   |      |                | buffer)                                   |
+   | 0x33 | RR OOO         | Frames received Out of Order              |
+   | 0x34 | OSPTOKEN       | OSP Token Block                           |
+   +------+----------------+-------------------------------------------+
+=end
+    
+                encoded
           end
     
-            def initialize
-          @websockets = []
-          @connections_count = 0
-          @started_event = Utils::ThreadEvent.new
-          @stopped_event = Utils::ThreadEvent.new
-        end
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a Kerberos AuthorizationData data
+        # definition.
+        class AuthorizationData < Element
+          # @!attribute elements
+          #   @return [Hash{Symbol => <Integer, String>}] The type of the authorization data
+          #   @option [Integer] :type
+          #   @option [String] :data
+          attr_accessor :elements
     
-        def process(args)
-      arg_is_present? args, '--server', 'The --server command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
-                          use the 'build' subcommand.'
-      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
-                          '--watch'.'
-      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
-                          the '--watch' switch.'
-      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
-                          favour of 'highlighter'.'
-      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
-                          your config files.'
-      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
-                          config files.'
-      no_subcommand(args)
-    end
-    
-            parsed_expr = parse_expression(expression)
-        @context.stack do
-          groups = input.group_by do |item|
-            @context[variable] = item
-            parsed_expr.render(@context)
+                seq_values.each do |val|
+              case val.tag
+              when 0
+                self.etype = decode_etype(val)
+              when 1
+                self.kvno = decode_kvno(val)
+              when 2
+                self.cipher = decode_cipher(val)
+              else
+                raise ::RuntimeError, 'Failed to decode EncryptedData SEQUENCE'
+              end
+            end
           end
-          grouped_array(groups)
-        end
-      end
     
-        def order_basic_bumped
-      'CASE WHEN (topics.pinned_at IS NOT NULL) THEN 0 ELSE 1 END, topics.bumped_at DESC'
-    end
+                int
+          end
     
-        req_type_id = req_types[req_type]
-    
-        def pause
-      stop!
-      @paused = true
-    end
-    
-      def self.period_types
-    @types ||= Enum.new(all: 1,
-                        yearly: 2,
-                        monthly: 3,
-                        weekly: 4,
-                        daily: 5,
-                        quarterly: 6)
-  end
-    
-        it 'rejects invalid directives and ones that are not allowed to be extended' do
-      builder << {
-        invalid_src: ['invalid'],
-      }
-    
-          return response unless html_response?(headers)
-      ContentSecurityPolicy.base_url = request.host_with_port if Rails.env.development?
-    
-      def self.ensure_consistency!(last_seen = 1.hour.ago)
-    reset_bounce_scores
-    update_view_counts(last_seen)
-    update_first_unread(last_seen)
-  end
-    
-          if api_key.user
-        api_key.user if !api_username || (api_key.user.username_lower == api_username.downcase)
-      elsif api_username
-        User.find_by(username_lower: api_username.downcase)
-      elsif user_id = header_api_key? ? @env[HEADER_API_USER_ID] : request['api_user_id']
-        User.find_by(id: user_id.to_i)
-      elsif external_id = header_api_key? ? @env[HEADER_API_USER_EXTERNAL_ID] : request['api_user_external_id']
-        SingleSignOnRecord.find_by(external_id: external_id.to_s).try(:user)
-      end
-    end
-  end
-    
-            # Builds an issue from a GitHub API response.
-        #
-        # issue - An instance of `Sawyer::Resource` containing the issue
-        #         details.
-        def self.from_api_response(issue)
-          user =
-            if issue.user
-              Representation::User.from_api_response(issue.user)
+                res = ''
+            case etype
+            when RSA_MD5
+              res = checksum_rsa_md5(data)
+            else
+              raise ::NotImplementedError, 'EncryptedData schema is not supported'
             end
     
-      it 'imports a scenario that does not exist yet' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This scenario has a few agents to get you started. Feel free to change them or delete them as you see fit!')
-    expect(page).not_to have_text('This Scenario already exists in your system.')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
-  end
-    
-      describe '#style_colors' do
-    it 'returns a css style-formated version of the scenario foreground and background colors' do
-      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
-    end
-    
-    describe LiquidMigrator do
-  describe 'converting JSONPath strings' do
-    it 'should work' do
-      expect(LiquidMigrator.convert_string('$.data', true)).to eq('{{data}}')
-      expect(LiquidMigrator.convert_string('$.data.test', true)).to eq('{{data.test}}')
-      expect(LiquidMigrator.convert_string('$first_title', true)).to eq('{{first_title}}')
-    end
-    
-          @log.level = -1
-      expect(@log).not_to be_valid
-      expect(@log).to have(1).error_on(:level)
-    
-        def str_to_byte_pos(pos)
-      @s.string.slice(0, pos).bytesize
+              # Decodes the e_data from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [String]
+          def decode_e_data(input)
+            input.value[0].value
+          end
+        end
+      end
     end
   end
 end
     
-      # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+    puts '\nUnable to find an RSS feed for the following blogs:'
+puts '==================================================='
+unavailable.each do |b|
+  puts '#{b.name} | #{b.web_url}'
+end
+puts '==================================================='
+
     
-      config.active_support.test_order = :random
+      # set version
+  content.sub! /(s\.version.*=\s+).*/, '\\1\'#{Rack::Protection::VERSION}\''
     
-      def test_image_helper
-    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
+          default_options :escape => :html,
+        :escaper => defined?(EscapeUtils) ? EscapeUtils : self
+    
+        post('/', {'csrf_param' => token}, 'rack.session' => {:csrf => token})
+    expect(last_response).to be_ok
   end
     
-            def declaration_with_comment(node)
-          buffer = processed_source.buffer
-          begin_pos = get_source_range(node, comments_as_separators).begin_pos
-          end_line = buffer.line_for_position(node.loc.expression.end_pos)
-          end_pos = buffer.line_range(end_line).end_pos
-          Parser::Source::Range.new(buffer, begin_pos, end_pos)
-        end
+        it 'redirects requests with sneaky encoded session cookies' do
+      get '/path', {}, 'HTTP_COOKIE' => 'rack.%73ession=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
+      expect(last_response).to be_redirect
+      expect(last_response.location).to eq('/path')
+    end
+  end
     
-              ignored_end_pos = if ignored_loc.respond_to?(:heredoc_body)
-                              ignored_loc.heredoc_end.end_pos
-                            else
-                              ignored_loc.expression.end_pos
-                            end
-          ignored_end_pos >= node.source_range.end_pos
-        end
-      end
+    LogStash::Bundler.setup!
     
-            def message_for_underscore_prefix(variable)
-          'If it's necessary, use `_` or `_#{variable.name}` ' \
-          'as an argument name to indicate that it won't be used.'
-        end
+        def execute
+      raise PluginManager::FileNotFoundError, 'Can't file local file #{local_file}' unless ::File.exist?(local_file)
+      raise PluginManager::InvalidPackError, 'Invalid format, the pack must be in zip format' unless valid_format?(local_file)
+    
+        # remove any version constrain from the Gemfile so the plugin(s) can be updated to latest version
+    # calling update without requirements will remove any previous requirements
+    plugins = plugins_to_update(previous_gem_specs_map)
+    # Skipping the major version validation when using a local cache as we can have situations
+    # without internet connection.
+    filtered_plugins = plugins.map { |plugin| gemfile.find(plugin) }
+      .compact
+      .reject { |plugin| REJECTED_OPTIONS.any? { |key| plugin.options.has_key?(key) } }
+      .each   { |plugin| gemfile.update(plugin.name) }
+    
+        desc 'Run one single machine acceptance test'
+    task :single, :machine do |t, args|
+      ENV['LS_VAGRANT_HOST']  = args[:machine]
+      exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/**/**/*_spec.rb']]))
+    end
+  end
+end
