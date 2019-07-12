a18699b5b760b2b1c33fe36c7c486c12d2759ae3
SUNsung <weatherius@gmail.com>
2019-07-12 03:35:26 +0300

    Rewrite [32AwfiJCQMpcU32SouDb18HJ4I32tFsMP8HJB2632jk.rb] File in: 2019-07-12 03:35:26

--- ruby/32AwfiJCQMpcU32SouDb18HJ4I32tFsMP8HJB2632jk.rb
+++ ruby/32AwfiJCQMpcU32SouDb18HJ4I32tFsMP8HJB2632jk.rb
@@ -1,152 +1,159 @@
 
         
-              return class_ref
-    end
+        # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
     
-          it 'adds docset_bundle_filename param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            docset_bundle_filename: 'DocSet bundle filename'
-          )
-        end').runner.execute(:test)
-    
-          it 'bootstraps with a single dependency' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              command: 'bootstrap',
-              dependencies: ['TestDependency']
-            )
-          end').runner.execute(:test)
-    
-    # confirms that the escaped string that is generated actually
-# gets turned back into the source string by the actual shell.
-# abuses a `grep` (or `find`) error message because that should be cross platform
-def confirm_shell_unescapes_string_correctly(string, escaped)
-  compare_string = string.to_s.dup
-    
-        describe 'when folders are specified in options' do
-      let(:options) { { screenshots_path: './screenshots', metadata_path: './metadata' } }
-    
-        private_constant :AIR_APPLICATION_INSTALLER
-    
-      def test_each_char
-    a = [e('\xa4\xa2'), 'b', e('\xa4\xa4'), 'c']
-    s = '\xa4\xa2b\xa4\xa4c'.force_encoding('euc-jp')
-    assert_equal(a, s.each_char.to_a, '[ruby-dev:33211] #{encdump s}.each_char.to_a')
-  end
-    
-    def main
-  @ruby = File.expand_path('miniruby')
-  @verbose = false
-  $VERBOSE = false
-  $stress = false
-  @color = nil
-  @tty = nil
-  @quiet = false
-  dir = nil
-  quiet = false
-  tests = nil
-  ARGV.delete_if {|arg|
-    case arg
-    when /\A--ruby=(.*)/
-      @ruby = $1
-      @ruby.gsub!(/^([^ ]*)/){File.expand_path($1)}
-      @ruby.gsub!(/(\s+-I\s*)((?!(?:\.\/)*-(?:\s|\z))\S+)/){$1+File.expand_path($2)}
-      @ruby.gsub!(/(\s+-r\s*)(\.\.?\/\S+)/){$1+File.expand_path($2)}
-      true
-    when /\A--sets=(.*)/
-      tests = Dir.glob('#{File.dirname($0)}/test_{#{$1}}*.rb').sort
-      puts tests.map {|path| File.basename(path) }.inspect
-      true
-    when /\A--dir=(.*)/
-      dir = $1
-      true
-    when /\A(--stress|-s)/
-      $stress = true
-    when /\A--color(?:=(?:always|(auto)|(never)|(.*)))?\z/
-      warn 'unknown --color argument: #$3' if $3
-      @color = $1 ? nil : !$2
-      true
-    when /\A--tty(=(?:yes|(no)|(.*)))?\z/
-      warn 'unknown --tty argument: #$3' if $3
-      @tty = !$1 || !$2
-      true
-    when /\A(-q|--q(uiet))\z/
-      quiet = true
-      @quiet = true
-      true
-    when /\A(-v|--v(erbose))\z/
-      @verbose = true
-    when /\A(-h|--h(elp)?)\z/
-      puts(<<-End)
-Usage: #{File.basename($0, '.*')} --ruby=PATH [--sets=NAME,NAME,...]
-        --sets=NAME,NAME,...        Name of test sets.
-        --dir=DIRECTORY             Working directory.
-                                    default: /tmp/bootstraptestXXXXX.tmpwd
-        --color[=WHEN]              Colorize the output.  WHEN defaults to 'always'
-                                    or can be 'never' or 'auto'.
-    -s, --stress                    stress test.
-    -v, --verbose                   Output test name before exec.
-    -q, --quiet                     Don\'t print header message.
-    -h, --help                      Print this message and quit.
-End
-      exit true
-    when /\A-j/
-      true
-    else
-      false
-    end
-  }
-  if tests and not ARGV.empty?
-    $stderr.puts '--tests and arguments are exclusive'
-    exit false
-  end
-  tests ||= ARGV
-  tests = Dir.glob('#{File.dirname($0)}/test_*.rb').sort if tests.empty?
-  pathes = tests.map {|path| File.expand_path(path) }
-    
-      def self.compare_dump_and_load i1, dumper, loader
-    dump = dumper.call(i1)
-    return i1 unless dump
-    i2 = loader.call(dump)
-    
-        def fit_error xys
-      y_bar  = sigma(xys) { |x, y| y } / xys.size.to_f
-      ss_tot = sigma(xys) { |x, y| (y    - y_bar) ** 2 }
-      ss_err = sigma(xys) { |x, y| (yield(x) - y) ** 2 }
-    
-        metaclass = class << self; self; end
-    
-            def call_hook(id,*additional)
-          @hooks[id] ||= []
-          @hooks[id].each{|hook| hook[self,additional] }
-          self
-        end
-    
-          def link_to_clone_promotion(promotion, options = {})
-        options[:data] = { action: 'clone', 'original-title': Spree.t(:clone) }
-        options[:class] = 'btn btn-warning btn-sm with-tip'
-        options[:method] = :post
-        options[:icon] = :clone
-        button_link_to '', clone_admin_promotion_path(promotion), options
+          def print_feature_element_name(feature_element)
+        @io.print '\n#{feature_element.location}  Scenario: #{feature_element.name} '
+        @io.flush
       end
     
-              if Cart::Update.call(order: @order, params: order_params).success?
-            user_id = params[:order][:user_id]
-            if current_api_user.has_spree_role?('admin') && user_id
-              @order.associate_user!(Spree.user_class.find(user_id))
-            end
-            respond_with(@order, default_template: :show)
-          else
-            invalid_resource!(@order)
-          end
+        def initial_page?
+      root_page? || context[:initial_paths].include?(subpath)
+    end
+    
+        def as_json
+      @pages
+    end
+    
+          module ClassMethods
+        def internal_urls
+          @internal_urls
         end
     
-          @@address_attributes = [
-        :id, :firstname, :lastname, :full_name, :address1, :address2, :city,
-        :zipcode, :phone, :company, :alternative_phone, :country_id, :state_id,
-        :state_name, :state_text
-      ]
+            css('br', 'hr', '.material-icons', '.header-link', '.breadcrumb').remove
+    
+        def password_change(record, opts={})
+      devise_mail(record, :password_change, opts)
+    end
+  end
+end
+
+    
+      private
+    
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
+        end
+    
+        # Returns the specified or detected guest type name.
+    #
+    # @return [Symbol]
+    def name
+      capability_host_chain[0][0]
+    end
+    
+            # This is called to upgrade this V1 config to V2. The parameter given
+        # is the full V2 configuration object, so you can do anything to it
+        # that you want.
+        #
+        # No return value is expected, modifications should be made directly
+        # to the new V2 object.
+        #
+        # @param [V2::Root] new
+        def upgrade(new)
+        end
+    
+              # Setup the options hash
+          options ||= {}
+    
+            # Converts this configuration object to JSON.
+        def to_json(*a)
+          instance_variables_hash.to_json(*a)
+        end
+    
+              @registered.each do |plugin|
+            plugin.components.provider_capabilities.each do |provider, caps|
+              results[provider].merge!(caps)
+            end
+          end
+    
+            # Defines additional command line commands available by key. The key
+        # becomes the subcommand, so if you register a command 'foo' then
+        # 'vagrant foo' becomes available.
+        #
+        # @param [String] name Subcommand key.
+        def self.command(name, **opts, &block)
+          # Validate the name of the command
+          if name.to_s !~ /^[-a-z0-9]+$/i
+            raise InvalidCommandName, 'Commands can only contain letters, numbers, and hyphens'
+          end
+    
+            # This is called early, before a machine is instantiated, to check
+        # if this provider is usable. This should return true or false.
+        #
+        # If raise_error is true, then instead of returning false, this
+        # should raise an error with a helpful message about why this
+        # provider cannot be used.
+        #
+        # @param [Boolean] raise_error If true, raise exception if not usable.
+        # @return [Boolean]
+        def self.usable?(raise_error=false)
+          # Return true by default for backwards compat since this was
+          # introduced long after providers were being written.
+          true
+        end
+    
+        # Like #{merge} but merges into self.
+    def merge!(other)
+      @items.merge!(other.__internal_state[:items])
+      self
+    end
+    
+        def URIAddEncodedOctetToBuffer(octet, result, index)
+      result[index] = 37; # Char code of '%'.
+      index         += 1
+      result[index] = @@hexCharCodeArray[octet >> 4];
+      index         += 1
+      result[index] = @@hexCharCodeArray[octet & 0x0F];
+      index += 1
+      return index;
+    end
+    
+          def versions
+        i = @versions.size + 1
+        @versions.map do |v|
+          i -= 1
+          { :id        => v.id,
+            :id7       => v.id[0..6],
+            :num       => i,
+            :author    => v.author.name.respond_to?(:force_encoding) ? v.author.name.force_encoding('UTF-8') : v.author.name,
+            :message   => v.message.respond_to?(:force_encoding) ? v.message.force_encoding('UTF-8') : v.message,
+            :date      => v.authored_date.strftime('%B %d, %Y'),
+            :gravatar  => Digest::MD5.hexdigest(v.author.email.strip.downcase),
+            :identicon => self._identicon_code(v.author.email),
+            :date_full => v.authored_date,
+            :files     => v.stats.files.map { |f,*rest|
+              page_path = extract_renamed_path_destination(f)
+              page_path = remove_page_extentions(page_path)
+              { :file => f,
+                :link => '#{page_path}/#{v.id}'
+              }
+            }
+          }
+        end
+      end
+    
+        page2 = 'page2'
+    user2 = 'user2'
+    @wiki.write_page(page2, :markdown, '',
+                     { :name => user2, :email => user2 });
+    
+      s.add_development_dependency 'rack-test', '~> 0.6.2'
+  s.add_development_dependency 'shoulda', '~> 3.5.0'
+  s.add_development_dependency 'minitest-reporters', '~> 0.14.16'
+  s.add_development_dependency 'twitter_cldr', '~> 3.2.0'
+  s.add_development_dependency 'mocha', '~> 1.1.0'
+  s.add_development_dependency 'test-unit', '~> 3.1.0'
+  s.add_development_dependency 'webrick', '~> 1.3.1'
+    
+      # Install JavaScript dependencies if using Yarn
+  # system('bin/yarn')
