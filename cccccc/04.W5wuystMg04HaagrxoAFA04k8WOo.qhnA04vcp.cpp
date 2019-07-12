e038c1e8aef09a224bbfae3f7f9eb80b9c269f7c
SUNsung <weatherius@gmail.com>
2019-07-12 23:58:06 +0300

    Rewrite [04.W5wuystMg04HaagrxoAFA04k8WOo.qhnA04vcp.cpp] File in: 2019-07-12 23:58:06

--- cccccc/04.W5wuystMg04HaagrxoAFA04k8WOo.qhnA04vcp.cpp
+++ cccccc/04.W5wuystMg04HaagrxoAFA04k8WOo.qhnA04vcp.cpp
@@ -1,171 +1,107 @@
 
         
-        Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+        #include 'caffe/blob.hpp'
+#include 'caffe/proto/caffe.pb.h'
+#include 'caffe/syncedmem.hpp'
+#include 'caffe/util/math_functions.hpp'
     
-    #include <Python.h>
+      vector<shared_ptr<Batch<Dtype> > > prefetch_;
+  BlockingQueue<Batch<Dtype>*> prefetch_free_;
+  BlockingQueue<Batch<Dtype>*> prefetch_full_;
+  Batch<Dtype>* prefetch_current_;
     
-      // Creates a batched FFT plan with scratch allocator.
-  //
-  // stream:          The GPU stream in which the FFT runs.
-  // rank:            Dimensionality of the transform (1, 2, or 3).
-  // elem_count:      Array of size rank, describing the size of each dimension.
-  // input_embed, output_embed:
-  //                  Pointer of size rank that indicates the storage dimensions
-  //                  of the input/output data in memory. If set to null_ptr all
-  //                  other advanced data layout parameters are ignored.
-  // input_stride:    Indicates the distance (number of elements; same below)
-  //                  between two successive input elements.
-  // input_distance:  Indicates the distance between the first element of two
-  //                  consecutive signals in a batch of the input data.
-  // output_stride:   Indicates the distance between two successive output
-  //                  elements.
-  // output_distance: Indicates the distance between the first element of two
-  //                  consecutive signals in a batch of the output data.
-  virtual std::unique_ptr<Plan> CreateBatchedPlanWithScratchAllocator(
-      Stream *stream, int rank, uint64 *elem_count, uint64 *input_embed,
-      uint64 input_stride, uint64 input_distance, uint64 *output_embed,
-      uint64 output_stride, uint64 output_distance, Type type,
-      bool in_place_fft, int batch_count,
-      ScratchAllocator *scratch_allocator) = 0;
-    
-    PLATFORM_DEFINE_ID(kHostPlatformId);
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #include <condition_variable>
-#include <mutex>
-#include <string>
-#include <system_error>
-#include <vector>
-    
-    #include 'caffe2/operators/glu_op.h'
-    
-    
-    {} // namespace caffe2
-    
-    REGISTER_CUDA_OPERATOR(LC3D, LocallyConnectedOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(
-    LC3DGradient,
-    LocallyConnectedGradientOp<float, CUDAContext>);
-    
-    
-    {template<typename IndexType, typename DType = real_t>
-Parser<IndexType> *
-CreateDenseLibSVMParser(const std::string& path,
-                        const std::map<std::string, std::string>& args,
-                        unsigned part_index,
-                        unsigned num_parts) {
-  CHECK_NE(args.count('num_col'), 0) << 'expect num_col in dense_libsvm';
-  return new DensifyParser<IndexType>(
-            Parser<IndexType>::Create(path.c_str(), part_index, num_parts, 'libsvm'),
-           uint32_t(atoi(args.at('num_col').c_str())));
-}
-}  // namespace data
-    
-    /*!
- * \brief Input stream that support additional PeekRead
- *  operation, besides read.
+    /**
+ * @brief Index into the input blob along its first axis.
+ *
+ * This layer can be used to select, reorder, and even replicate examples in a
+ * batch.  The second blob is cast to int and treated as an index into the
+ * first axis of the first blob.
  */
-class PeekableInStream : public dmlc::Stream {
+template <typename Dtype>
+class BatchReindexLayer : public Layer<Dtype> {
  public:
-  explicit PeekableInStream(dmlc::Stream* strm)
-      : strm_(strm), buffer_ptr_(0) {}
+  explicit BatchReindexLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     }
     
-    #if defined(__CUDACC__)
     
-    
-    {
-    {}  // namespace obj
-}  // namespace xgboost
-    
-    // Returns an Env that translates paths such that the root directory appears to
-// be chroot_dir. chroot_dir should refer to an existing directory.
-Env* NewChrootEnv(Env* base_env, const std::string& chroot_dir);
-    
-    
-    {  mutable int count_ = 0;
-  mutable int merge_count_ = 0;
+    { protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual inline bool reverse_dimensions() { return false; }
+  virtual void compute_output_shape();
 };
     
-    std::string kDBPath = '/tmp/rocksdb_simple_example';
     
-    // Abstract handle to particular state of a DB.
-// A Snapshot is an immutable object and can therefore be safely
-// accessed from multiple threads without any external synchronization.
-//
-// To Create a Snapshot, call DB::GetSnapshot().
-// To Destroy a Snapshot, call DB::ReleaseSnapshot(snapshot).
-class Snapshot {
+    {}  // namespace caffe
+    
+    #ifdef USE_CUDNN
+/**
+ * @brief CuDNN acceleration of SigmoidLayer.
+ */
+template <typename Dtype>
+class CuDNNSigmoidLayer : public SigmoidLayer<Dtype> {
  public:
-  // returns Snapshot's sequence number
-  virtual SequenceNumber GetSequenceNumber() const = 0;
+  explicit CuDNNSigmoidLayer(const LayerParameter& param)
+      : SigmoidLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNSigmoidLayer();
     }
     
-      // Builds an openable snapshot of RocksDB on the same disk, which
-  // accepts an output directory on the same disk, and under the directory
-  // (1) hard-linked SST files pointing to existing live SST files
-  // SST files will be copied if output directory is on a different filesystem
-  // (2) a copied manifest files and other files
-  // The directory should not already exist and will be created by this API.
-  // The directory will be an absolute path
-  // log_size_for_flush: if the total log file size is equal or larger than
-  // this value, then a flush is triggered for all the column families. The
-  // default value is 0, which means flush is always triggered. If you move
-  // away from the default, the checkpoint may not contain up-to-date data
-  // if WAL writing is not always enabled.
-  // Flush will always trigger if it is 2PC.
-  virtual Status CreateCheckpoint(const std::string& checkpoint_dir,
-                                  uint64_t log_size_for_flush = 0);
+      DHTTaskQueue* taskQueue_;
     
-    #include 'rocksdb/utilities/lua/rocks_lua_custom_library.h'
+      ~DHTTaskExecutor();
     
+      void setTaskQueue(DHTTaskQueue* taskQueue);
     
-    {  EsdCanClient esd_can_client;
-  EXPECT_TRUE(esd_can_client.Init(param));
-  EXPECT_EQ(esd_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
-  std::vector<CanFrame> frames;
-  int32_t num = 0;
-  EXPECT_EQ(esd_can_client.Send(frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  EXPECT_EQ(esd_can_client.Receive(&frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
-  CanFrame can_frame;
-  frames.push_back(can_frame);
-  EXPECT_EQ(esd_can_client.SendSingleFrame(frames),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  esd_can_client.Stop();
+    #include 'DHTTaskQueue.h'
+#include 'DHTTaskExecutor.h'
+    
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port,
+                                           const unsigned char* secret) const
+{
+  unsigned char src[DHT_ID_LENGTH + COMPACT_LEN_IPV6 + SECRET_SIZE];
+  memset(src, 0, sizeof(src));
+  int compactlen = bittorrent::packcompact(src + DHT_ID_LENGTH, ipaddr, port);
+  if (compactlen == 0) {
+    throw DL_ABORT_EX(fmt('Token generation failed: ipaddr=%s, port=%u',
+                          ipaddr.c_str(), port));
+  }
+  memcpy(src, infoHash, DHT_ID_LENGTH);
+  memcpy(src + DHT_ID_LENGTH + COMPACT_LEN_IPV6, secret, SECRET_SIZE);
+  unsigned char md[20];
+  message_digest::digest(md, sizeof(md), MessageDigest::sha1().get(), src,
+                         sizeof(src));
+  return std::string(&md[0], &md[sizeof(md)]);
 }
     
-    using ::apollo::canbus::ChassisDetail;
     
-    #include 'gtest/gtest.h'
+    {} // namespace aria2
     
-    double ObjectGeneralInfo60B::longitude_dist(const std::uint8_t* bytes,
-                                            int32_t length) const {
-  Byte t0(bytes + 1);
-  int32_t x = t0.get_byte(0, 8);
-    }
+    #include 'DHTNode.h'
+#include 'util.h'
+#include 'a2functional.h'
     
-    const PolynomialXd& Spline1dSeg::Derivative() const { return derivative_; }
+    public:
+  // _remoteNode is always null
+  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                    const unsigned char* data, size_t length,
+                    const std::string& ipaddr, uint16_t port);
     
-    
-    {
-    {
-    {
-    {  double ret = x * 0.001000;
-  return ret;
+    bool DNSCache::CacheEntry::contains(const std::string& addr) const
+{
+  return find(addr) != addrEntries_.end();
 }
-}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
