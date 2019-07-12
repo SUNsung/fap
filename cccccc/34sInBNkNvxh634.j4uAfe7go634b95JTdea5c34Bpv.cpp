d8778610516d05643cb6db155f3c239741aed7c4
SUNsung <weatherius@gmail.com>
2019-07-13 01:16:53 +0300

    Rewrite [34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp] File in: 2019-07-13 01:16:53

--- cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
+++ cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
@@ -1,197 +1,269 @@
 
         
-          case psModePS:
-    // rotate, translate, and scale page
-    imgWidth = imgURX - imgLLX;
-    imgHeight = imgURY - imgLLY;
-    x1 = (int)floor(state->getX1());
-    y1 = (int)floor(state->getY1());
-    x2 = (int)ceil(state->getX2());
-    y2 = (int)ceil(state->getY2());
-    width = x2 - x1;
-    height = y2 - y1;
-    tx = ty = 0;
-    // rotation and portrait/landscape mode
-    if (rotate0 >= 0) {
-      rotate = (360 - rotate0) % 360;
-      landscape = gFalse;
-    } else {
-      rotate = (360 - state->getRotate()) % 360;
-      if (rotate == 0 || rotate == 180) {
-	if (width > height && width > imgWidth) {
-	  rotate += 90;
-	  landscape = gTrue;
-	} else {
-	  landscape = gFalse;
-	}
-      } else { // rotate == 90 || rotate == 270
-	if (height > width && height > imgWidth) {
-	  rotate = 270 - rotate;
-	  landscape = gTrue;
-	} else {
-	  landscape = gFalse;
-	}
-      }
+        Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    // Safe container for an owned PyObject. On destruction, the reference count of
+// the contained object will be decremented.
+using Safe_PyObjectPtr = std::unique_ptr<PyObject, detail::PyDecrefDeleter>;
+Safe_PyObjectPtr make_safe(PyObject* o);
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include 'tensorflow/python/util/kernel_registry.h'
+    
+    PLATFORM_DEFINE_ID(kCudaPlatformId);
+    
+    namespace stream_executor {
+namespace host {
     }
-    writePSFmt('%%PageOrientation: {0:s}\n',
-	       landscape ? 'Landscape' : 'Portrait');
-    writePS('pdfStartPage\n');
-    if (rotate == 0) {
-      imgWidth2 = imgWidth;
-      imgHeight2 = imgHeight;
-    } else if (rotate == 90) {
-      writePS('90 rotate\n');
-      ty = -imgWidth;
-      imgWidth2 = imgHeight;
-      imgHeight2 = imgWidth;
-    } else if (rotate == 180) {
-      writePS('180 rotate\n');
-      imgWidth2 = imgWidth;
-      imgHeight2 = imgHeight;
-      tx = -imgWidth;
-      ty = -imgHeight;
-    } else { // rotate == 270
-      writePS('270 rotate\n');
-      tx = -imgHeight;
-      imgWidth2 = imgHeight;
-      imgHeight2 = imgWidth;
-    }
-    // shrink or expand
-    if (xScale0 > 0 && yScale0 > 0) {
-      xScale = xScale0;
-      yScale = yScale0;
-    } else if ((globalParams->getPSShrinkLarger() &&
-	 (width > imgWidth2 || height > imgHeight2)) ||
-	(globalParams->getPSExpandSmaller() &&
-	 (width < imgWidth2 && height < imgHeight2))) {
-      xScale = (double)imgWidth2 / (double)width;
-      yScale = (double)imgHeight2 / (double)height;
-      if (yScale < xScale) {
-	xScale = yScale;
-      } else {
-	yScale = xScale;
-      }
-    } else {
-      xScale = yScale = 1;
-    }
-    // deal with odd bounding boxes or clipping
-    if (clipLLX0 < clipURX0 && clipLLY0 < clipURY0) {
-      tx -= xScale * clipLLX0;
-      ty -= yScale * clipLLY0;
-    } else {
-      tx -= xScale * x1;
-      ty -= yScale * y1;
-    }
-    // center
-    if (tx0 >= 0 && ty0 >= 0) {
-      tx += rotate == 0 ? tx0 : ty0;
-      ty += rotate == 0 ? ty0 : -tx0;
-    } else if (globalParams->getPSCenter()) {
-      if (clipLLX0 < clipURX0 && clipLLY0 < clipURY0) {
-	tx += (imgWidth2 - xScale * (clipURX0 - clipLLX0)) / 2;
-	ty += (imgHeight2 - yScale * (clipURY0 - clipLLY0)) / 2;
-      } else {
-	tx += (imgWidth2 - xScale * width) / 2;
-	ty += (imgHeight2 - yScale * height) / 2;
-      }
-    }
-    tx += rotate == 0 ? imgLLX : imgLLY;
-    ty += rotate == 0 ? imgLLY : -imgLLX;
-    if (tx != 0 || ty != 0) {
-      writePSFmt('{0:.6g} {1:.6g} translate\n', tx, ty);
-    }
-    if (xScale != 1 || yScale != 1) {
-      writePSFmt('{0:.6f} {1:.6f} scale\n', xScale, yScale);
-    }
-    if (clipLLX0 < clipURX0 && clipLLY0 < clipURY0) {
-      writePSFmt('{0:.6g} {1:.6g} {2:.6g} {3:.6g} re W\n',
-		 clipLLX0, clipLLY0, clipURX0 - clipLLX0, clipURY0 - clipLLY0);
-    } else {
-      writePSFmt('{0:d} {1:d} {2:d} {3:d} re W\n', x1, y1, x2 - x1, y2 - y1);
     }
     
-    //========================================================================
-//
-// Modified under the Poppler project - http://poppler.freedesktop.org
-//
-// All changes made under the Poppler project to this file are licensed
-// under GPL version 2 or later
-//
-// Copyright (C) 2006 Scott Turner <scotty1024@mac.com>
-//
-// To see a description of the changes please see the Changelog file that
-// came with your tarball or type make ChangeLog if you are building from git
-//
-//========================================================================
-    
-    GBool PageAttrs::readBox(Dict *dict, char *key, PDFRectangle *box) {
-  PDFRectangle tmp;
-  double t;
-  Object obj1, obj2;
-  GBool ok;
+    // Allows to represent a value that is either a host scalar or a scalar stored
+// on the GPU device.
+template <typename ElemT>
+class HostOrDeviceScalar {
+ public:
+  // Not marked as explicit because when using this constructor, we usually want
+  // to set this to a compile-time constant.
+  HostOrDeviceScalar(ElemT value) : value_(value), is_pointer_(false) {}
+  explicit HostOrDeviceScalar(const DeviceMemory<ElemT>& pointer)
+      : pointer_(pointer), is_pointer_(true) {
+    CHECK_EQ(1, pointer.ElementCount());
+  }
     }
     
-      // Get the next object from the input stream.
-  Object *getObj(Object *obj, Guchar *fileKey = NULL,
-		 CryptAlgorithm encAlgorithm = cryptRC4, int keyLength = 0,
-		 int objNum = 0, int objGen = 0);
-  
-  Object *getObj(Object *obj, Guchar *fileKey,
-     CryptAlgorithm encAlgorithm, int keyLength,
-     int objNum, int objGen, std::set<int> *fetchOriginatorNums);
+    #ifndef TENSORFLOW_STREAM_EXECUTOR_LIB_DEMANGLE_H_
+#define TENSORFLOW_STREAM_EXECUTOR_LIB_DEMANGLE_H_
     
-    private:
+        struct Size2D {
+        Size2D() : width(0), height(0) {}
+        Size2D(size_t width_, size_t height_) : width(width_), height(height_) {}
+    }
     
-      // Check the document's encryption.  If the document is encrypted,
-  // this will first try <ownerPassword> and <userPassword> (in
-  // 'batch' mode), and if those fail, it will attempt to request a
-  // password from the user.  This is the high-level function that
-  // calls the lower level functions for the specific security handler
-  // (requesting a password three times, etc.).  Returns true if the
-  // document can be opened (if it's unencrypted, or if a correct
-  // password is obtained); false otherwise (encrypted and no correct
-  // password).
-  GBool checkEncryption(GooString *ownerPassword,
-			GooString *userPassword);
     
-      fileName = NULL;
-  samplingRate = 0.0;
-  channels = 1;
-  bitsPerSample = 8;
-  encoding = soundRaw;
+    {
+    {}}
     
-      // Get the Splash object.
-  Splash *getSplash() { return splash; }
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
+CVT_FUNC(s32, s8, 8,
+,
+{
+     for (size_t i = 0; i < w; i += 8)
+     {
+         internal::prefetch(_src + i);
+         __asm__ (
+             'vld1.32 {d0-d1}, [%[src1]]                              \n\t'
+             'vld1.32 {d2-d3}, [%[src2]]                              \n\t'
+             'vqmovn.s32 d4, q0                                       \n\t'
+             'vqmovn.s32 d5, q1                                       \n\t'
+             'vqmovn.s16  d6, q2                                      \n\t'
+             'vst1.8 {d6}, [%[dst]]                                   \n\t'
+             : /*no output*/
+             : [src1] 'r' (_src + i + 0),
+               [src2] 'r' (_src + i + 4),
+               [dst] 'r' (_dst + i)
+             : 'd0','d1','d2','d3','d4','d5','d6'
+         );
+     }
+})
+#else
+CVT_FUNC(s32, s8, 8,
+,
+{
+     for (size_t i = 0; i < w; i += 8)
+     {
+         internal::prefetch(_src + i);
+         int32x4_t vline1_s32 = vld1q_s32(_src + i);
+         int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
+    }
+    }
     
-    #ifndef STDINCACHELOADER_H
-#define STDINCACHELOADER_H
-    
-        Rational result{ prat };
-    destroyrat(prat);
-    
-            if (m_bInv
-            && ((wParam == IDC_CHOP) || (wParam == IDC_SIN) || (wParam == IDC_COS) || (wParam == IDC_TAN) || (wParam == IDC_LN) || (wParam == IDC_DMS)
-                || (wParam == IDC_DEGREES) || (wParam == IDC_SINH) || (wParam == IDC_COSH) || (wParam == IDC_TANH)))
-        {
-            m_bInv = false;
+                for (; j < size.width; j++)
+            {
+                dst[j] = internal::saturate_cast<u8>((src[j] >> shift));
+            }
         }
+        else // CONVERT_POLICY_WRAP
+        {
+            for (; j < roiw16; j += 16)
+            {
+                internal::prefetch(src + j);
+                int16x8_t v_src0 = vshrq_n_s16(vld1q_s16(src + j), shift),
+                          v_src1 = vshrq_n_s16(vld1q_s16(src + j + 8), shift);
+                int8x16_t v_dst = vcombine_s8(vmovn_s16(v_src0),
+                                              vmovn_s16(v_src1));
+                vst1q_u8(dst + j, vreinterpretq_u8_s8(v_dst));
+            }
+            for (; j < roiw8; j += 8)
+            {
+                int16x8_t v_src = vshrq_n_s16(vld1q_s16(src + j), shift);
+                vst1_u8(dst + j, vreinterpret_u8_s8(vmovn_s16(v_src)));
+            }
     
-            std::wstring value;
+    #include <limits>
     
-    #ifndef MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
-#define MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
-#include <dmlc/parameter.h>
-#include <string>
-#include <utility>
-#include <vector>
-#include 'mxnet/ndarray.h'
+                    uint8x16_t m0 =   vandq_u8(vcgtq_s8(x0, v2), vcgtq_s8(x1, v2));
+                uint8x16_t m1 =   vandq_u8(vcgtq_s8(v1, x0), vcgtq_s8(v1, x1));
+                m0 = vorrq_u8(m0, vandq_u8(vcgtq_s8(x1, v2), vcgtq_s8(x2, v2)));
+                m1 = vorrq_u8(m1, vandq_u8(vcgtq_s8(v1, x1), vcgtq_s8(v1, x2)));
+                m0 = vorrq_u8(m0, vandq_u8(vcgtq_s8(x2, v2), vcgtq_s8(x3, v2)));
+                m1 = vorrq_u8(m1, vandq_u8(vcgtq_s8(v1, x2), vcgtq_s8(v1, x3)));
+                m0 = vorrq_u8(m0, vandq_u8(vcgtq_s8(x3, v2), vcgtq_s8(x0, v2)));
+                m1 = vorrq_u8(m1, vandq_u8(vcgtq_s8(v1, x3), vcgtq_s8(v1, x0)));
+                m0 = vorrq_u8(m0, m1);
     
-    // C callback that can be used by TVM to extract
-// the WrapAsyncCall function.
-extern 'C' MXNET_DLL int MXTVMBridge(TVMFunctionHandle pregister) {
-  using tvm::runtime::PackedFunc;
-  const PackedFunc& fregister =
-      *static_cast<PackedFunc*>(pregister);
-  fregister('WrapAsyncCall', PackedFunc(mxnet::WrapAsyncCall));
-  return 0;
+    template <typename T>
+inline void inRangeCheck(const Size2D &_size,
+                         const T * srcBase, ptrdiff_t srcStride,
+                         const T * rng1Base, ptrdiff_t rng1Stride,
+                         const T * rng2Base, ptrdiff_t rng2Stride,
+                         u8 * dstBase, ptrdiff_t dstStride)
+{
+    typedef typename internal::VecTraits<T>::vec128 vec128;
+    typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
+    }
+    
+                if(x) {
+                xx0 = xx1;
+                xx1 = xx2;
+            } else {
+                xx1 = x1;
+                // make border
+                    if (border == BORDER_MODE_REPLICATE || border == BORDER_MODE_REFLECT)
+                    {
+                        xx1 = vset_lane_u8(vget_lane_u8(x1, 0),x1, 7);
+                    }
+                    else if (border == BORDER_MODE_CONSTANT)
+                    {
+                        xx1 = vset_lane_u8(borderValue, x1, 7);
+                    }
+                    else if (border == BORDER_MODE_REFLECT101)
+                    {
+                        xx1 = vset_lane_u8(vget_lane_u8(x1, 1),x1, 7);
+                    }
+            }
+            xx2 = x1;
+    
+    #include <google/protobuf/stubs/logging.h>
+#include <google/protobuf/stubs/common.h>
+    
+    TEST(StructurallyValidTest, InvalidUTF8String) {
+  const string invalid_str('abcd\xA0\xB0\xA0\xB0\xA0\xB0 - xyz789');
+  EXPECT_FALSE(IsStructurallyValidUTF8(invalid_str.data(),
+                                       invalid_str.size()));
+  // Additional check for pointer alignment
+  for (int i = 1; i < 8; ++i) {
+    EXPECT_FALSE(IsStructurallyValidUTF8(invalid_str.data() + i,
+                                         invalid_str.size() - i));
+  }
 }
+    
+    
+// Abbreviations: true_type and false_type are structs that represent boolean
+// true and false values. Also define the boost::mpl versions of those names,
+// true_ and false_.
+typedef integral_constant<bool, true>  true_type;
+typedef integral_constant<bool, false> false_type;
+typedef true_type  true_;
+typedef false_type false_;
+    
+    #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+    
+    int main(int argc, char *argv[]) {
+  if (argc % 2 == 0 || argc == 1) {
+    std::cerr << 'Usage: [input_files] [output_file_names] where ' <<
+        'input_files are one to one mapping to output_file_names.' <<
+        std::endl;
+    return 1;
+  }
+    }
+    
+    // This function fills in a Person message based on user input.
+void PromptForAddress(tutorial::Person* person) {
+  cout << 'Enter person ID number: ';
+  int id;
+  cin >> id;
+  person->set_id(id);
+  cin.ignore(256, '\n');
+    }
+    
+    AuthPropertyIterator::AuthPropertyIterator(
+    const grpc_auth_property* property, const grpc_auth_property_iterator* iter)
+    : property_(property),
+      ctx_(iter->ctx),
+      index_(iter->index),
+      name_(iter->name) {}
+    
+    class CensusChannelData : public ChannelData {
+ public:
+  grpc_error* Init(grpc_channel_element* elem,
+                   grpc_channel_element_args* args) override;
+};
+    
+      void StartTransportStreamOpBatch(grpc_call_element* elem,
+                                   TransportStreamOpBatch* op) override;
+    
+      explicit CensusContext(absl::string_view name)
+      : span_(::opencensus::trace::Span::StartSpan(name)) {}
+    
+    ::opencensus::stats::MeasureInt64 RpcServerSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcServerSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcServerReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcServerReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcServerServerLatency();
+::opencensus::stats::MeasureInt64 RpcServerCompletedRpcs();
+    
+    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_SERVER_FILTER_H
+#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_SERVER_FILTER_H
+    
+    Status ProtoServerReflection::GetFileContainingSymbol(
+    ServerContext* context, const grpc::string& symbol,
+    ServerReflectionResponse* response) {
+  if (descriptor_pool_ == nullptr) {
+    return Status::CANCELLED;
+  }
+    }
+    
+    #include <grpc/support/port_platform.h>
+    
+    const std::set<PerBalancerStore*>* LoadDataStore::GetAssignedStores(
+    const grpc::string& hostname, const grpc::string& lb_id) {
+  auto it = per_host_stores_.find(hostname);
+  if (it == per_host_stores_.end()) return nullptr;
+  return it->second.GetAssignedStores(lb_id);
+}
+    
+    #if TARGET_OS_IPHONE
+GRPC_XMACRO_ITEM(isWWAN, IsWWAN)
+#endif
+GRPC_XMACRO_ITEM(reachable, Reachable)
+GRPC_XMACRO_ITEM(transientConnection, TransientConnection)
+GRPC_XMACRO_ITEM(connectionRequired, ConnectionRequired)
+GRPC_XMACRO_ITEM(connectionOnTraffic, ConnectionOnTraffic)
+GRPC_XMACRO_ITEM(interventionRequired, InterventionRequired)
+GRPC_XMACRO_ITEM(connectionOnDemand, ConnectionOnDemand)
+GRPC_XMACRO_ITEM(isLocalAddress, IsLocalAddress)
+GRPC_XMACRO_ITEM(isDirect, IsDirect)
+
+    
+        // output changed array
+    std::cout << array << '\n';
+    
+        // output element with JSON pointer '/number'
+    std::cout << j.at('/number'_json_pointer) << '\n';
+    // output element with JSON pointer '/string'
+    std::cout << j.at('/string'_json_pointer) << '\n';
+    // output element with JSON pointer '/array'
+    std::cout << j.at('/array'_json_pointer) << '\n';
+    // output element with JSON pointer '/array/1'
+    std::cout << j.at('/array/1'_json_pointer) << '\n';
+    
+    using json = nlohmann::json;
