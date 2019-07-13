20e3a468a2104fb3caa0addfe92df547bc7def7e
SUNsung <weatherius@gmail.com>
2019-07-13 17:04:48 +0300

    Rewrite [32on2Gk1fYKr232ocDQQhqEGpk32cBnTCxGrNWw32Lu..cpp] File in: 2019-07-13 17:04:47

--- cccccc/32on2Gk1fYKr232ocDQQhqEGpk32cBnTCxGrNWw32Lu..cpp
+++ cccccc/32on2Gk1fYKr232ocDQQhqEGpk32cBnTCxGrNWw32Lu..cpp
@@ -1,100 +1,235 @@
 
         
-        #ifndef CHROME_BROWSER_UI_VIEWS_FRAME_GLOBAL_MENU_BAR_REGISTRAR_X11_H_
-#define CHROME_BROWSER_UI_VIEWS_FRAME_GLOBAL_MENU_BAR_REGISTRAR_X11_H_
+        #include <carotene/definitions.hpp>
+#include <stdint.h>
+#include <cstddef>
     
-    napi_value Init(napi_env env, napi_value exports) {
-  napi_status status;
-  napi_property_descriptor descriptors[] = {
-      {'Print', NULL, Print, NULL, NULL, NULL, napi_default, NULL}};
-    }
-    
-    v8::MaybeLocal<v8::Object> WrappableBase::GetWrapper(
-    v8::Isolate* isolate) const {
-  if (!wrapper_.IsEmpty())
-    return v8::MaybeLocal<v8::Object>(
-        v8::Local<v8::Object>::New(isolate, wrapper_));
-  else
-    return v8::MaybeLocal<v8::Object>();
-}
-    
-    // Arguments is a wrapper around v8::FunctionCallbackInfo that integrates
-// with Converter to make it easier to marshall arguments and return values
-// between V8 and C++.
-class Arguments {
- public:
-  Arguments();
-  explicit Arguments(const v8::FunctionCallbackInfo<v8::Value>& info);
-  ~Arguments();
-    }
-    
-    namespace mate {
-    }
-    
-    U_NAMESPACE_END
-    
-        if(attributeHasBeenSetExplicitly(UCOL_ALTERNATE_HANDLING)) {
-        appendAttribute(result, 'A', getAttribute(UCOL_ALTERNATE_HANDLING, errorCode), errorCode);
-    }
-    // ATTR_VARIABLE_TOP not supported because 'B' was broken.
-    // See ICU tickets #10372 and #10386.
-    if(attributeHasBeenSetExplicitly(UCOL_CASE_FIRST)) {
-        appendAttribute(result, 'C', getAttribute(UCOL_CASE_FIRST, errorCode), errorCode);
-    }
-    if(attributeHasBeenSetExplicitly(UCOL_NUMERIC_COLLATION)) {
-        appendAttribute(result, 'D', getAttribute(UCOL_NUMERIC_COLLATION, errorCode), errorCode);
-    }
-    if(attributeHasBeenSetExplicitly(UCOL_CASE_LEVEL)) {
-        appendAttribute(result, 'E', getAttribute(UCOL_CASE_LEVEL, errorCode), errorCode);
-    }
-    if(attributeHasBeenSetExplicitly(UCOL_FRENCH_COLLATION)) {
-        appendAttribute(result, 'F', getAttribute(UCOL_FRENCH_COLLATION, errorCode), errorCode);
-    }
-    // Note: UCOL_HIRAGANA_QUATERNARY_MODE is deprecated and never changes away from default.
-    length = uloc_getKeywordValue(resultLocale, 'collation', subtag, UPRV_LENGTHOF(subtag), &errorCode);
-    appendSubtag(result, 'K', subtag, length, errorCode);
-    length = uloc_getLanguage(resultLocale, subtag, UPRV_LENGTHOF(subtag), &errorCode);
-    appendSubtag(result, 'L', subtag, length, errorCode);
-    if(attributeHasBeenSetExplicitly(UCOL_NORMALIZATION_MODE)) {
-        appendAttribute(result, 'N', getAttribute(UCOL_NORMALIZATION_MODE, errorCode), errorCode);
-    }
-    length = uloc_getCountry(resultLocale, subtag, UPRV_LENGTHOF(subtag), &errorCode);
-    appendSubtag(result, 'R', subtag, length, errorCode);
-    if(attributeHasBeenSetExplicitly(UCOL_STRENGTH)) {
-        appendAttribute(result, 'S', getAttribute(UCOL_STRENGTH, errorCode), errorCode);
-    }
-    length = uloc_getVariant(resultLocale, subtag, UPRV_LENGTHOF(subtag), &errorCode);
-    appendSubtag(result, 'V', subtag, length, errorCode);
-    length = uloc_getScript(resultLocale, subtag, UPRV_LENGTHOF(subtag), &errorCode);
-    appendSubtag(result, 'Z', subtag, length, errorCode);
-    
-    static UBool copyAsSuperscript(
-        const UnicodeString &s,
-        int32_t beginIndex,
-        int32_t endIndex,
-        UnicodeString &result,
-        UErrorCode &status) {
-    if (U_FAILURE(status)) {
-        return FALSE;
-    }
-    for (int32_t i = beginIndex; i < endIndex;) {
-        UChar32 c = s.char32At(i);
-        int32_t digit = u_charDigitValue(c);
-        if (digit < 0) {
-            status = U_INVALID_CHAR_FOUND;
-            return FALSE;
+                vst1q_s32(dsta + x, wa);
+            vst1q_s32(dstb + x, wb);
+            vst1q_s32(dstc + x, wc);
         }
-        result.append(kSuperscriptDigits[digit]);
-        i += U16_LENGTH(c);
+        if(x < colsn)
+        {
+            x = colsn-4;
+            goto box3x3s32_horiz_ll;
+        }
     }
-    return TRUE;
+    else
+    {
+        //horizontal convolution (last 2 lines)
+        s32* dstb = internal::getRowPtr(dstBase, dstStride, i-1);
+        size_t x = 0;
+        for (; x <= colsn - 4; x += 4)
+        {
+            internal::prefetch(laneA + x + cn);
+            internal::prefetch(laneB + x + cn);
+box3x3s32_horiz_last2:
+            int32x4_t lane0a = vld1q_s32(laneA + x - cn);
+            int32x4_t lane2a = vld1q_s32(laneA + x + cn);
+            int32x4_t lane1a = vld1q_s32(laneA + x);
+    
+    
+    {        if (!m[-1])         CANNY_PUSH(m - 1);
+        if (!m[1])          CANNY_PUSH(m + 1);
+        if (!m[-mapstep-1]) CANNY_PUSH(m - mapstep - 1);
+        if (!m[-mapstep])   CANNY_PUSH(m - mapstep);
+        if (!m[-mapstep+1]) CANNY_PUSH(m - mapstep + 1);
+        if (!m[mapstep-1])  CANNY_PUSH(m + mapstep - 1);
+        if (!m[mapstep])    CANNY_PUSH(m + mapstep);
+        if (!m[mapstep+1])  CANNY_PUSH(m + mapstep + 1);
+    }
+    
+    #define SPLIT_QUAD(sgn, bits, n) { \
+                                     internal::prefetch(src + sj); \
+                                     vec128 v_src = vld##n##q_##sgn##bits(src + sj); \
+                                     FILL_LINES##n(VST1Q, sgn##bits) \
+                                 }
+    
+                for (; j < size.width; j++)
+            {
+                dst[j] = internal::saturate_cast<u8>((src[j] >> shift));
+            }
+        }
+        else // CONVERT_POLICY_WRAP
+        {
+            for (; j < roiw16; j += 16)
+            {
+                internal::prefetch(src + j);
+                int16x8_t v_src0 = vshrq_n_s16(vld1q_s16(src + j), shift),
+                          v_src1 = vshrq_n_s16(vld1q_s16(src + j + 8), shift);
+                int8x16_t v_dst = vcombine_s8(vmovn_s16(v_src0),
+                                              vmovn_s16(v_src1));
+                vst1q_u8(dst + j, vreinterpretq_u8_s8(v_dst));
+            }
+            for (; j < roiw8; j += 8)
+            {
+                int16x8_t v_src = vshrq_n_s16(vld1q_s16(src + j), shift);
+                vst1_u8(dst + j, vreinterpret_u8_s8(vmovn_s16(v_src)));
+            }
+    
+    #define DOT_FLOAT_BLOCKSIZE (1 << 13)
+    f64 result = 0.0;
+    for (size_t row = 0; row < size.height; ++row)
+    {
+        const f32 * src0 = internal::getRowPtr(src0Base, src0Stride, row);
+        const f32 * src1 = internal::getRowPtr(src1Base, src1Stride, row);
+    }
+    
+            int16x8_t tcurr1 = vmovq_n_s16(0x0);
+        int16x8_t tnext1 = vmovq_n_s16(0x0);
+        int16x8_t tprev1 = vmovq_n_s16(0x0);
+        int16x8_t tpprev1 = vmovq_n_s16(0x0);
+        int16x8_t tppprev1 = vmovq_n_s16(0x0);
+    
+    #endif  // TESSERACT_CCMAIN_OSDETECT_H_
+
+    
+    // Parses the given box file string into a page_number, utf8_str, and
+// bounding_box. Returns true on a successful parse.
+bool ParseBoxFileStr(const char* boxfile_str, int* page_number,
+                     STRING* utf8_str, TBOX* bounding_box);
+    
+    ELISTIZE(PARA)
+    
+    Determining whether a character is CURRENTLY rejected depends on implicit
+understanding of the SEQUENCE of possible calls. The flags are defined and
+grouped in the REJ_FLAGS enum. These groupings are used in determining a
+characters CURRENT rejection status. Basically, a character is ACCEPTED if
+    
+    #include 'errcode.h'
+    
+    #ifndef TESSERACT_CCUTIL_KDPAIR_H_
+#define TESSERACT_CCUTIL_KDPAIR_H_
+    
+    #if GTEST_OS_WINDOWS_MOBILE
+  // Windows CE does not have the 'ANSI' versions of Win32 APIs. To be
+  // able to pass strings to Win32 APIs on CE we need to convert them
+  // to 'Unicode', UTF-16.
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10>
+struct Types10 {
+  typedef T1 Head;
+  typedef Types9<T2, T3, T4, T5, T6, T7, T8, T9, T10> Tail;
+};
+    
+    
+    {  // <TechnicalDetails>
+  //
+  // EXPECT_EQ(expected, actual) is the same as
+  //
+  //   EXPECT_TRUE((expected) == (actual))
+  //
+  // except that it will print both the expected value and the actual
+  // value when the assertion fails.  This is very helpful for
+  // debugging.  Therefore in this case EXPECT_EQ is preferred.
+  //
+  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
+  // and is thus more general.
+  //
+  // </TechnicalDetails>
 }
     
-    U_NAMESPACE_END
+     public:
+  // Clones a 0-terminated C string, allocating memory using new.
+  static const char* CloneCString(const char* a_c_string);
     
-    #include 'unicode/uniset.h'
-#include 'unicode/udat.h'
-#include 'cmemory.h'
-#include 'uassert.h'
-#include 'ucln_in.h'
-#include 'umutex.h'
+        E* element = new E(old_head->element());
+    delete old_head;
+    
+    
+    {    Rational Sinh(Rational const& rat);
+    Rational Cosh(Rational const& rat);
+    Rational Tanh(Rational const& rat);
+    Rational ASinh(Rational const& rat);
+    Rational ACosh(Rational const& rat);
+    Rational ATanh(Rational const& rat);
+}
+
+    
+            bool IsZero() const;
+    
+        class CalculatorHistory : public IHistoryDisplay
+    {
+    public:
+        CalculatorHistory(const size_t maxSize);
+        unsigned int AddToHistory(
+            _In_ std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> const& spTokens,
+            _In_ std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> const& spCommands,
+            std::wstring_view result);
+        std::vector<std::shared_ptr<HISTORYITEM>> const& GetHistory();
+        std::shared_ptr<HISTORYITEM> const& GetHistoryItem(unsigned int uIdx);
+        void ClearHistory();
+        unsigned int AddItem(_In_ std::shared_ptr<HISTORYITEM> const& spHistoryItem);
+        bool RemoveItem(unsigned int uIdx);
+        size_t MaxHistorySize() const
+        {
+            return m_maxHistorySize;
+        }
+        ~CalculatorHistory(void);
+    }
+    
+    TEST_F(ConfigTests, test_plugin_reconfigure) {
+  auto& rf = RegistryFactory::get();
+  // Add a configuration plugin (could be any plugin) that will react to
+  // config updates.
+  rf.registry('config_parser')
+      ->add('placebo', std::make_shared<PlaceboConfigParserPlugin>());
+    }
+    
+      Expected<int32_t, DatabaseError> getInt32(const std::string& domain,
+                                            const std::string& key) override;
+  Expected<std::string, DatabaseError> getString(
+      const std::string& domain, const std::string& key) override;
+    
+    ExpectedSuccess<DatabaseError> RocksdbDatabase::putString(
+    const std::string& domain,
+    const std::string& key,
+    const std::string& value) {
+  auto result = putRawBytes(domain, key, value);
+  if (result) {
+    return Success();
+  }
+  return result.takeError();
+}
+    
+    
+    {}
+    
+      /** @brief Virtual method which should implement custom logging.
+   *
+   *  LoggerPlugin::logString should be implemented by a subclass of
+   *  LoggerPlugin which needs to log a string in a custom way.
+   *
+   *  @return an instance of osquery::Status which indicates the success or
+   *  failure of the operation.
+   */
+  virtual Status logString(const std::string& s) = 0;
+    
+    PluginResponse tableRowsToPluginResponse(const TableRows& rows) {
+  PluginResponse result;
+  for (const auto& row : rows) {
+    result.push_back(static_cast<Row>(*row));
+  }
+  return result;
+}
+    
+      /**
+   * @brief Bind this plugin to an external plugin reference.
+   *
+   * Allow a specialized plugin type to act when an external plugin is
+   * registered (e.g., a TablePlugin will attach the table name).
+   *
+   * @param name The broadcasted name of the plugin.
+   * @param info The routing info for the owning extension.
+   */
+  static Status addExternal(const std::string& name,
+                            const PluginResponse& info) {
+    (void)name;
+    (void)info;
+    return Status::success();
+  }
+    
+      /// Given a query, return the list of scanned tables.
+  virtual Status getQueryTables(const std::string& query,
+                                std::vector<std::string>& tables) const = 0;
