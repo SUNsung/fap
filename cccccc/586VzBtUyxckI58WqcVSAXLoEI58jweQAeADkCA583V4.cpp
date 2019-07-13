3169a33cdabf54e9099f2db52ce7887519fc0652
SUNsung <weatherius@gmail.com>
2019-07-13 13:49:31 +0300

    Rewrite [586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp] File in: 2019-07-13 13:49:31

--- cccccc/586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp
+++ cccccc/586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp
@@ -1,34 +1,119 @@
 
         
-                /**
-         * Move assignment.
-         * Similar to Rectangle<T>(Rectangle<T>&& rectangle).
-         * @param rectangle Rectangle to be moved.
-         * @return The resulting Rectangle.
-         */
-        Rectangle<T>& operator=(Rectangle<T>&& rectangle);
-    
-    namespace op
-{
-    // This worker will do 3-D rendering
-    class OP_API GuiAdam : public Gui
+            void operator() (const typename internal::VecTraits<T>::vec128 & v_src0,
+                     const typename internal::VecTraits<T>::vec128 & v_src1,
+                     typename internal::VecTraits<T>::vec128 & v_dst) const
     {
-    public:
-        GuiAdam(const Point<int>& outputSize, const bool fullScreen,
-                const std::shared_ptr<std::atomic<bool>>& isRunningSharedPtr,
-                const std::shared_ptr<std::pair<std::atomic<bool>, std::atomic<int>>>& videoSeekSharedPtr = nullptr,
-                const std::vector<std::shared_ptr<PoseExtractorNet>>& poseExtractorNets = {},
-                const std::vector<std::shared_ptr<FaceExtractorNet>>& faceExtractorNets = {},
-                const std::vector<std::shared_ptr<HandExtractorNet>>& handExtractorNets = {},
-                const std::vector<std::shared_ptr<Renderer>>& renderers = {},
-                const DisplayMode displayMode = DisplayMode::DisplayAll,
-                const std::shared_ptr<const TotalModel>& totalModel = nullptr,
-                const std::string& adamRenderedVideoPath = '');
-    }
+        v_dst = internal::vaddq(v_src0, v_src1);
     }
     
     
-    // Constant parameters
-    const auto HAND_CCN_DECREASE_FACTOR = 8.f;
-    const std::string HAND_PROTOTXT{'hand/pose_deploy.prototxt'};
-    const std::string HAND_TRAINED_MODEL{'hand/pose_iter_102000.caffemodel'};
+    {        vs1 = vmlaq_f32(vgamma, vs1, valpha);
+        vs1 = vmlaq_f32(vs1, vs2, vbeta);
+        v_dst = vcvtq_u32_f32(vs1);
+    }
+    
+    
+    {            vx = vxnext;
+            vy = vynext;
+        }
+        int32x4_t norml = vmull_s16(vget_low_s16(vx), vget_low_s16(vx));
+        int32x4_t normh = vmull_s16(vget_high_s16(vy), vget_high_s16(vy));
+    
+    #include 'common.hpp'
+    
+    
+    {
+    {        for( k = 0; k < ncorners; k++ )
+        {
+            j = cornerpos[k];
+            s32 score = prev[j];
+            if( !nonmax_suppression ||
+                    (score > prev[j+1] && score > prev[j-1] &&
+                     score > pprev[j-1] && score > pprev[j] && score > pprev[j+1] &&
+                     score > curr[j-1] && score > curr[j] && score > curr[j+1]) )
+            {
+                keypoints->push((f32)j, (f32)(i-1), 7.f, -1, (f32)score);
+            }
+        }
+    }
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)keypoints;
+    (void)threshold;
+    (void)nonmax_suppression;
+#endif
+}
+    
+        if (borderType == BORDER_MODE_CONSTANT)
+        for (s32 k = 0; k < cn; ++k)
+        {
+            lane[-cn+k] = borderValue;
+            lane[-cn-cn+k] = borderValue;
+            lane[colsn+k] = borderValue;
+            lane[colsn+cn+k] = borderValue;
+        }
+    
+        Size2D size(_size);
+    if (srcStride == dstStride &&
+        srcStride == rng1Stride &&
+        srcStride == rng2Stride &&
+        srcStride == (ptrdiff_t)(size.width))
+    {
+        size.width *= size.height;
+        size.height = 1;
+    }
+    const size_t width = size.width & ~( 32/sizeof(T) - 1 );
+    
+        for (ptrdiff_t y = 0; y < height; ++y)
+    {
+        const u8 * srow0 = y == 0 && border == BORDER_MODE_CONSTANT ? NULL : internal::getRowPtr(srcBase, srcStride, std::max<ptrdiff_t>(y - 1, 0));
+        const u8 * srow1 = internal::getRowPtr(srcBase, srcStride, y);
+        const u8 * srow2 = y + 1 == height && border == BORDER_MODE_CONSTANT ? NULL : internal::getRowPtr(srcBase, srcStride, std::min(y + 1, height - 1));
+        u8 * drow = internal::getRowPtr(dstBase, dstStride, y);
+    }
+    
+      ~Reader();
+    
+      struct KeyComparator {
+    const InternalKeyComparator comparator;
+    explicit KeyComparator(const InternalKeyComparator& c) : comparator(c) {}
+    int operator()(const char* a, const char* b) const;
+  };
+    
+      static Iterator* BlockReader(void*, const ReadOptions&, const Slice&);
+    
+      // Support for iterating over the contents of a batch.
+  Status Iterate(Handler* handler) const;
+    
+      size_t size() const { return size_; }
+  Iterator* NewIterator(const Comparator* comparator);
+    
+    class BlockBuilder {
+ public:
+  explicit BlockBuilder(const Options* options);
+    }
+    
+      // Returns an estimate of the total memory usage of data allocated
+  // by the arena.
+  size_t MemoryUsage() const {
+    return memory_usage_.load(std::memory_order_relaxed);
+  }
+    
+      // drop column family
+  s = db->DropColumnFamily(handles[1]);
+  assert(s.ok());
+    
+      // Commit.  Since the snapshot was advanced, the write done outside of the
+  // transaction does not prevent this transaction from Committing.
+  s = txn->Commit();
+  assert(s.ok());
+  delete txn;
+  // Clear snapshot from read options since it is no longer valid
+  read_options.snapshot = nullptr;
+    
+    #ifndef ROCKSDB_LITE
+    
+    #include 'rocksdb/utilities/lua/rocks_lua_custom_library.h'
