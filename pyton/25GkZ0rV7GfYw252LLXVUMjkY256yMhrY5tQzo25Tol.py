8dec29c11871348d8a4c029f3d2e3d1e51a2e0c7
SUNsung <weatherius@gmail.com>
2019-07-13 17:49:27 +0300

    Rewrite [25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py] File in: 2019-07-13 17:49:27

--- pyton/25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py
+++ pyton/25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py
@@ -1,201 +1,138 @@
 
         
-        
-IPv4 = CheckNetwork('IPv4')
-IPv4.urls = [
-            'https://www.microsoft.com',
-            'https://www.apple.com',
-            'https://code.jquery.com',
-            'https://cdn.bootcss.com',
-            'https://cdnjs.cloudflare.com']
-IPv4.triger_check_network()
+                if opts.logfile:
+            self.settings.set('LOG_ENABLED', True, priority='cmdline')
+            self.settings.set('LOG_FILE', opts.logfile, priority='cmdline')
+    
+    from six.moves.urllib.parse import urlencode
+    
+    from scrapy.utils.spider import iter_spider_classes
+from scrapy.commands import ScrapyCommand
+from scrapy.exceptions import UsageError
+from scrapy.utils.conf import arglist_to_dict
+from scrapy.utils.python import without_none_values
+    
+    
+    
+            assert len(self.args) in [1, 2, 3]
+        self.obj_name = self.args[0] or None
+        self.obj_type = self.objects[self.obj_name]
+    
+        from scrapy.core.downloader.tls import ScrapyClientTLSOptions, DEFAULT_CIPHERS
     
         try:
-        # hide console in MS windows
-        startupinfo = subprocess.STARTUPINFO()
-        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        startupinfo.wShowWindow = subprocess.SW_HIDE
+        # XXX: this try-except is not needed in Twisted 17.0.0+ because
+        # it requires pyOpenSSL 0.16+.
+        from OpenSSL.SSL import SSL_CB_HANDSHAKE_DONE, SSL_CB_HANDSHAKE_START
+    except ImportError:
+        SSL_CB_HANDSHAKE_START = 0x10
+        SSL_CB_HANDSHAKE_DONE = 0x20
     
+    from scrapy.http import Headers
+from scrapy.utils.httpobj import urlparse_cached
+from scrapy.utils.python import to_bytes
+from scrapy.responsetypes import responsetypes
     
-class RC4FileObject(object):
-    '''fileobj for rc4'''
-    def __init__(self, stream, key):
-        self.__stream = stream
-        self.__cipher = _Crypto_Cipher_ARC4_new(key) if key else lambda x:x
-    def __getattr__(self, attr):
-        if attr not in ('__stream', '__cipher'):
-            return getattr(self.__stream, attr)
-    def read(self, size=-1):
-        return self.__cipher.encrypt(self.__stream.read(size))
+            # XXX: Google parses at least first 100k bytes; scrapy's redirect
+        # middleware parses first 4k. 4k turns out to be insufficient
+        # for this middleware, and parsing 100k could be slow.
+        # We use something in between (32K) by default.
+        self.lookup_bytes = settings.getint('AJAXCRAWL_MAXSIZE', 32768)
     
-    ## Anything on different channel than DEFAULT_CHANNEL is not parsed
-# by parser.
-HIDDEN_CHANNEL = 99
+    See documentation in docs/topics/downloader-middleware.rst
+'''
     
-        You can insert stuff, replace, and delete chunks.  Note that the
-    operations are done lazily--only if you convert the buffer to a
-    String.  This is very efficient because you are not moving data around
-    all the time.  As the buffer of tokens is converted to strings, the
-    toString() method(s) check to see if there is an operation at the
-    current index.  If so, the operation is done and then normal String
-    rendering continues on the buffer.  This is like having multiple Turing
-    machine instruction streams (programs) operating on a single input tape. :)
+    # Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, 'home-assistant.tex', 'Home Assistant Documentation',
+     'Home Assistant Team', 'manual'),
+]
     
-    
-############################################################################
-#
-# token implementations
-#
-# Token
-# +- CommonToken
-# \- ClassicToken
-#
-############################################################################
-    
-        def test_unset_attr(self):
-        for func in [self.b, self.fi.a]:
-            try:
-                func.non_existent_attr
-            except AttributeError:
-                pass
-            else:
-                self.fail('using unknown attributes should raise '
-                          'AttributeError')
-    
-    
-class TestLoader(object):
+        Returns a function to set the real IP.
     '''
-    This class is responsible for loading tests according to various criteria
-    and returning them wrapped in a TestSuite
+    ip_to_mock = None
+    
+    
+# ---------------------------------------------------------------------------- #
+# FPN level info for stages 5, 4, 3, 2 for select models (more can be added)
+# ---------------------------------------------------------------------------- #
+    
+    
+def _generate_anchors(base_size, scales, aspect_ratios):
+    '''Generate anchor (reference) windows by enumerating aspect ratios X
+    scales wrt a reference (0, 0, base_size - 1, base_size - 1) window.
     '''
-    testMethodPrefix = 'test'
-    sortTestMethodsUsing = staticmethod(util.three_way_cmp)
-    testNamePatterns = None
-    suiteClass = suite.TestSuite
-    _top_level_dir = None
+    anchor = np.array([1, 1, base_size, base_size], dtype=np.float) - 1
+    anchors = _ratio_enum(anchor, aspect_ratios)
+    anchors = np.vstack(
+        [_scale_enum(anchors[i, :], scales) for i in range(anchors.shape[0])]
+    )
+    return anchors
     
-            for button, printer, name in button_sources:
-            with self.subTest(name=name):
-                printer._Printer__setup()
-                button.invoke()
-                get = dialog._current_textview.viewframe.textframe.text.get
-                lines = printer._Printer__lines
-                if len(lines) < 2:
-                    self.fail(name + ' full text was not found')
-                self.assertEqual(lines[0], get('1.0', '1.end'))
-                self.assertEqual(lines[1], get('2.0', '2.end'))
-                dialog._current_textview.destroy()
-    
-        def testSeekBackwards(self):
-        self.createTempFile()
-        with BZ2File(self.filename) as bz2f:
-            bz2f.read(500)
-            bz2f.seek(-150, 1)
-            self.assertEqual(bz2f.read(), self.TEXT[500-150:])
-    
-    # Now the header items can be accessed as a dictionary, and any non-ASCII will
-# be converted to unicode:
-print('To:', msg['to'])
-print('From:', msg['from'])
-print('Subject:', msg['subject'])
-    
-            message = 'Background workers haven't checked in recently. '
-        if backlogged:
-            message += 'It seems that you have a backlog of %d tasks. Either your workers aren't running or you need more capacity.' % size
-        else:
-            message += 'This is likely an issue with your configuration or the workers aren't running.'
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
     
     
-class GroupTagValueNotFound(Exception):
-    pass
+class CollectAndDistributeFpnRpnProposalsOp(object):
+    def __init__(self, train):
+        self._train = train
+    
+    import logging
+    
+        cache_key = str(stride) + str(anchor_sizes) + str(anchor_aspect_ratios)
+    if cache_key in _threadlocal_foa.cache:
+        return _threadlocal_foa.cache[cache_key]
+    
+    
+def get_minibatch(roidb):
+    '''Given a roidb, construct a minibatch sampled from it.'''
+    # We collect blobs from each image onto a list and then concat them into a
+    # single tensor, hence we initialize each blob to an empty list
+    blobs = {k: [] for k in get_minibatch_blob_names()}
+    # Get the input image blob, formatted for caffe2
+    im_blob, im_scales = _get_image_blob(roidb)
+    blobs['data'] = im_blob
+    if cfg.RPN.RPN_ON:
+        # RPN-only or end-to-end Faster/Mask R-CNN
+        valid = rpn_roi_data.add_rpn_blobs(blobs, im_scales, roidb)
+    elif cfg.RETINANET.RETINANET_ON:
+        im_width, im_height = im_blob.shape[3], im_blob.shape[2]
+        # im_width, im_height corresponds to the network input: padded image
+        # (if needed) width and height. We pass it as input and slice the data
+        # accordingly so that we don't need to use SampleAsOp
+        valid = retinanet_roi_data.add_retinanet_blobs(
+            blobs, im_scales, roidb, im_width, im_height
+        )
+    else:
+        # Fast R-CNN like models trained on precomputed proposals
+        valid = fast_rcnn_roi_data.add_fast_rcnn_blobs(blobs, im_scales, roidb)
+    return blobs, valid
+    
+    
+if __name__ == '__main__':
+    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+    logger = setup_logging(__name__)
+    logger.setLevel(logging.DEBUG)
+    logging.getLogger('detectron.roi_data.loader').setLevel(logging.INFO)
+    np.random.seed(cfg.RNG_SEED)
+    args = parse_args()
+    logger.info('Called with args:')
+    logger.info(args)
+    if args.cfg_file is not None:
+        merge_cfg_from_file(args.cfg_file)
+    if args.opts is not None:
+        merge_cfg_from_list(args.opts)
+    assert_and_infer_cfg()
+    logger.info('Running with config:')
+    logger.info(pprint.pformat(cfg))
+    main(args)
 
     
-            # Removing unique constraint on 'EventTag', fields ['event_id', 'key', 'value']
-        db.delete_unique(u'tagstore_eventtag', ['event_id', 'key', 'value'])
     
-    from ycm.client.base_request import BaseRequest
-    
-    
-  def _ThreadMain( self ):
-    while True:
-      time.sleep( self._ping_interval_seconds )
-    
-    from ycm.tests.test_utils import MockVimModule
-MockVimModule()
-    
-    
-def MockAsyncServerResponseInProgress():
-  '''Return a fake future object that is incomplete. Suitable for mocking a
-  response future within a client request. For example:
-    
-    
-def KeywordsFromSyntaxListOutput_PhpSyntax_ContainsFunctions_test():
-  assert_that( syntax_parse._KeywordsFromSyntaxListOutput(
-                   ContentsOfTestFile( 'php_syntax' ) ),
-               has_items( 'array_change_key_case' ) )
-    
-        try:
-      result = self.fn( *self.args, **self.kwargs )
-    except BaseException:
-      e = sys.exc_info()[ 1 ]
-      self.future.set_exception( e )
-    else:
-      self.future.set_result( result )
-    
-        >>> Point = namedtuple('Point', 'x y')
-    >>> Point.__doc__                   # docstring for the new class
-    'Point(x, y)'
-    >>> p = Point(11, y=22)             # instantiate with positional args or keywords
-    >>> p[0] + p[1]                     # indexable like a plain tuple
-    33
-    >>> x, y = p                        # unpack like a regular tuple
-    >>> x, y
-    (11, 22)
-    >>> p.x + p.y                       # fields also accessable by name
-    33
-    >>> d = p._asdict()                 # convert to a dictionary
-    >>> d['x']
-    11
-    >>> Point(**d)                      # convert from a dictionary
-    Point(x=11, y=22)
-    >>> p._replace(x=100)               # _replace() is like str.replace() but targets named fields
-    Point(x=100, y=22)
-    
-    # These benchmarks are delicate.  They hit various fast-paths in the gen
-# machinery in order to stay synchronous so we don't need an IOLoop.
-# This removes noise from the results, but it's easy to change things
-# in a way that completely invalidates the results.
-    
-    define('url', default='ws://localhost:9001')
-define('name', default='Tornado')
-    
-    _watched_files = set()
-_reload_hooks = []
-_reload_attempted = False
-_io_loops = weakref.WeakKeyDictionary()  # type: ignore
-_autoreload_is_main = False
-_original_argv = None  # type: Optional[List[str]]
-_original_spec = None
-    
-            .. versionchanged:: 4.1
-           Config files are now always interpreted as utf-8 instead of
-           the system default encoding.
-    
-        def _handle_events(self, fd: int, events: int) -> None:
-        read_fd = pycares.ARES_SOCKET_BAD
-        write_fd = pycares.ARES_SOCKET_BAD
-        if events & IOLoop.READ:
-            read_fd = fd
-        if events & IOLoop.WRITE:
-            write_fd = fd
-        self.channel.process_fd(read_fd, write_fd)
-    
-            p = Popen(
-            [sys.executable, '-m', 'testapp'],
-            stdout=subprocess.PIPE,
-            cwd=self.path,
-            env=dict(os.environ, PYTHONPATH=pythonpath),
-            universal_newlines=True,
-        )
-        out = p.communicate()[0]
-        self.assertEqual(out, 'Starting\nStarting\n')
+def extract_array(obj, extract_numpy=False):
+    '''
+    Extract the ndarray or ExtensionArray from a Series or Index.
