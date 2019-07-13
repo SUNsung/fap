e0552b6e429ebb01f915d1747f39297b211db298
SUNsung <weatherius@gmail.com>
2019-07-13 09:13:16 +0300

    Rewrite [04iMHcyqbpM.o04RXOrc8T8Nqw04CzQ2drcwCrE04Z52.cpp] File in: 2019-07-13 09:13:15

--- cccccc/04iMHcyqbpM.o04RXOrc8T8Nqw04CzQ2drcwCrE04Z52.cpp
+++ cccccc/04iMHcyqbpM.o04RXOrc8T8Nqw04CzQ2drcwCrE04Z52.cpp
@@ -1,273 +1,75 @@
 
         
-        namespace tensorflow {
-    }
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    #include 'tensorflow/core/framework/op.h'
-#include 'tensorflow/core/framework/op_kernel.h'
-    
-    REGISTER_OP('Invalid')
-    .Attr('invalid attr: int32')  // invalid since the name has a space.
-    .Doc(R'doc(
-An op to test that invalid ops do not successfully generate invalid python code.
-)doc');
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #include 'tensorflow/stream_executor/device_memory.h'
-#include 'tensorflow/stream_executor/platform/logging.h'
-    
-    /**
- * An RAII-style reverse lock. Unlocks on construction and locks on destruction.
- */
-template<typename Lock>
-class reverse_lock
-{
-public:
-    }
-    
-        secp256k1_fe_sqr(&zz, &a->z);                       /* z = Z1^2 */
-    u1 = a->x; secp256k1_fe_normalize_weak(&u1);        /* u1 = U1 = X1*Z2^2 (1) */
-    secp256k1_fe_mul(&u2, &b->x, &zz);                  /* u2 = U2 = X2*Z1^2 (1) */
-    s1 = a->y; secp256k1_fe_normalize_weak(&s1);        /* s1 = S1 = Y1*Z2^3 (1) */
-    secp256k1_fe_mul(&s2, &b->y, &zz);                  /* s2 = Y2*Z1^2 (1) */
-    secp256k1_fe_mul(&s2, &s2, &a->z);                  /* s2 = S2 = Y2*Z1^3 (1) */
-    t = u1; secp256k1_fe_add(&t, &u2);                  /* t = T = U1+U2 (2) */
-    m = s1; secp256k1_fe_add(&m, &s2);                  /* m = M = S1+S2 (2) */
-    secp256k1_fe_sqr(&rr, &t);                          /* rr = T^2 (1) */
-    secp256k1_fe_negate(&m_alt, &u2, 1);                /* Malt = -X2*Z1^2 */
-    secp256k1_fe_mul(&tt, &u1, &m_alt);                 /* tt = -U1*U2 (2) */
-    secp256k1_fe_add(&rr, &tt);                         /* rr = R = T^2-U1*U2 (3) */
-    /** If lambda = R/M = 0/0 we have a problem (except in the 'trivial'
-     *  case that Z = z1z2 = 0, and this is special-cased later on). */
-    degenerate = secp256k1_fe_normalizes_to_zero(&m) &
-                 secp256k1_fe_normalizes_to_zero(&rr);
-    /* This only occurs when y1 == -y2 and x1^3 == x2^3, but x1 != x2.
-     * This means either x1 == beta*x2 or beta*x1 == x2, where beta is
-     * a nontrivial cube root of one. In either case, an alternate
-     * non-indeterminate expression for lambda is (y1 - y2)/(x1 - x2),
-     * so we set R/M equal to this. */
-    rr_alt = s1;
-    secp256k1_fe_mul_int(&rr_alt, 2);       /* rr = Y1*Z2^3 - Y2*Z1^3 (2) */
-    secp256k1_fe_add(&m_alt, &u1);          /* Malt = X1*Z2^2 - X2*Z1^2 */
-    
-    int secp256k1_ecdh(const secp256k1_context* ctx, unsigned char *result, const secp256k1_pubkey *point, const unsigned char *scalar) {
-    int ret = 0;
-    int overflow = 0;
-    secp256k1_gej res;
-    secp256k1_ge pt;
-    secp256k1_scalar s;
-    VERIFY_CHECK(ctx != NULL);
-    ARG_CHECK(result != NULL);
-    ARG_CHECK(point != NULL);
-    ARG_CHECK(scalar != NULL);
-    }
-    
-    #endif  // STORAGE_LEVELDB_DB_DBFORMAT_H_
-
-    
-    static std::string ShortSuccessor(const std::string& s) {
-  std::string result = s;
-  InternalKeyComparator(BytewiseComparator()).FindShortSuccessor(&result);
-  return result;
+        Status WriteBatchBase::DeleteRange(const SliceParts& begin_key,
+                                   const SliceParts& end_key) {
+  std::string begin_key_buf, end_key_buf;
+  Slice begin_key_slice(begin_key, &begin_key_buf);
+  Slice end_key_slice(end_key, &end_key_buf);
+  return DeleteRange(begin_key_slice, end_key_slice);
 }
     
-    namespace remote {
-    }
     
+    {}  // namespace rocksdb
     
-namespace nwapi {
-    }
+      // put and get from non-default column family
+  s = db->Put(WriteOptions(), handles[1], Slice('key'), Slice('value'));
+  assert(s.ok());
+  std::string value;
+  s = db->Get(ReadOptions(), handles[1], Slice('key'), &value);
+  assert(s.ok());
     
-    class BaseEvent {
-  friend class EventListener;
-  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
-    }
+      // Transaction could not commit since the write outside of the txn conflicted
+  // with the read!
+  assert(s.IsBusy());
     
-    #include 'base/logging.h'
-#include 'base/strings/string_util.h'
-#include 'base/values.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/nw/src/api/menu/menu.h'
+    #pragma once
     
-    void NwAppCloseAllWindowsFunction::DoJob(AppWindowRegistry* registry, std::string id) {
-  if (base::FeatureList::IsEnabled(::features::kNWNewWin)) {
-    chrome::CloseAllBrowsers();
-  }
-  AppWindowRegistry::AppWindowList windows =
-    registry->GetAppWindowsForApp(id);
-    }
+      // Builds an openable snapshot of RocksDB on the same disk, which
+  // accepts an output directory on the same disk, and under the directory
+  // (1) hard-linked SST files pointing to existing live SST files
+  // SST files will be copied if output directory is on a different filesystem
+  // (2) a copied manifest files and other files
+  // The directory should not already exist and will be created by this API.
+  // The directory will be an absolute path
+  // log_size_for_flush: if the total log file size is equal or larger than
+  // this value, then a flush is triggered for all the column families. The
+  // default value is 0, which means flush is always triggered. If you move
+  // away from the default, the checkpoint may not contain up-to-date data
+  // if WAL writing is not always enabled.
+  // Flush will always trigger if it is 2PC.
+  virtual Status CreateCheckpoint(const std::string& checkpoint_dir,
+                                  uint64_t log_size_for_flush = 0);
     
-    NwObjCreateFunction::NwObjCreateFunction() {
-}
-    
-        protected:
-      ~NwScreenInitEventListenersFunction() override {}
-      DECLARE_EXTENSION_FUNCTION('nw.Screen.initEventListeners', UNKNOWN)
-    
-    TEST_P(FooTest, DoesBlah) {
-  // Inside a test, access the test parameter with the GetParam() method
-  // of the TestWithParam<T> class:
-  EXPECT_TRUE(foo.Blah(GetParam()));
-  ...
-}
-    
-    #define EXPECT_NONFATAL_FAILURE_ON_ALL_THREADS(statement, substr) \
-  do {\
-    ::testing::TestPartResultArray gtest_failures;\
-    ::testing::internal::SingleFailureChecker gtest_checker(\
-        &gtest_failures, ::testing::TestPartResult::kNonFatalFailure, \
-        (substr));\
-    {\
-      ::testing::ScopedFakeTestPartResultReporter gtest_reporter(\
-          ::testing::ScopedFakeTestPartResultReporter::INTERCEPT_ALL_THREADS, \
-          &gtest_failures);\
-      if (::testing::internal::AlwaysTrue()) { statement; }\
-    }\
-  } while (::testing::internal::AlwaysFalse())
-    
-    template <GTEST_9_TYPENAMES_(T)>
-class GTEST_9_TUPLE_(T) {
+    class OptimisticTransactionDB : public StackableDB {
  public:
-  template <int k> friend class gtest_internal::Get;
+  // Open an OptimisticTransactionDB similar to DB::Open().
+  static Status Open(const Options& options, const std::string& dbname,
+                     OptimisticTransactionDB** dbptr);
     }
     
-     private:
-  // Creates a node with a given element value.  The next pointer is
-  // set to NULL.
-  explicit QueueNode(const E& an_element) : element_(an_element), next_(NULL) {}
+    #include <jni.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
     
-        // Select GPU
-    {
-        uint32_t gpu_count;
-        err = vkEnumeratePhysicalDevices(g_Instance, &gpu_count, NULL);
-        check_vk_result(err);
-        IM_ASSERT(gpu_count > 0);
-    }
-    
-    IMGUI_IMPL_API bool     ImGui_ImplDX10_Init(ID3D10Device* device);
-IMGUI_IMPL_API void     ImGui_ImplDX10_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplDX10_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplDX10_RenderDrawData(ImDrawData* draw_data);
-    
-    // About GLSL version:
-// The 'glsl_version' initialization parameter defaults to '#version 150' if NULL.
-// Only override if your GL version doesn't handle this GLSL version. Keep NULL if unsure!
-    
-                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox('Another Window', &show_another_window);
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-        // Setup Platform/Renderer bindings
-    ImGui_ImplWin32_Init(hwnd);
-    ImGui_ImplDX9_Init(g_pd3dDevice);
-    
-    
-    {    InputTextCallback_UserData cb_user_data;
-    cb_user_data.Str = str;
-    cb_user_data.ChainCallback = callback;
-    cb_user_data.ChainCallbackUserData = user_data;
-    return InputText(label, (char*)str->c_str(), str->capacity() + 1, flags, InputTextCallback, &cb_user_data);
+    bool StressScavengeObserver::HasRequestedGC() const {
+  return has_requested_gc_;
 }
     
     
-    {    printf('DestroyContext()\n');
-    ImGui::DestroyContext();
-    return 0;
-}
-
+    {    global_pool_.Swap(other.global_pool_);
+  }
     
-        // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
-    
-    #include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
-    
-    
-    {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-      MatrixXd mat_golden(20, 10);
-  // clang-format off
-  mat_golden <<
-    -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-     1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-     1, -1,  0,  0,  0,  0,  0,  0,  0,  0,
-    -1,  1,  0,  0,  0,  0,  0,  0,  0,  0,
-     0,  1, -1,  0,  0,  0,  0,  0,  0,  0,
-     0, -1,  1,  0,  0,  0,  0,  0,  0,  0,
-     0,  0,  1, -1,  0,  0,  0,  0,  0,  0,
-     0,  0, -1,  1,  0,  0,  0,  0,  0,  0,
-     0,  0,  0,  1, -1,  0,  0,  0,  0,  0,
-     0,  0,  0, -1,  1,  0,  0,  0,  0,  0,
-     0,  0,  0,  0,  1, -1,  0,  0,  0,  0,
-     0,  0,  0,  0, -1,  1,  0,  0,  0,  0,
-     0,  0,  0,  0,  0,  1, -1,  0,  0,  0,
-     0,  0,  0,  0,  0, -1,  1,  0,  0,  0,
-     0,  0,  0,  0,  0,  0,  1, -1,  0,  0,
-     0,  0,  0,  0,  0,  0, -1,  1,  0,  0,
-     0,  0,  0,  0,  0,  0,  0,  1, -1,  0,
-     0,  0,  0,  0,  0,  0,  0, -1,  1,  0,
-     0,  0,  0,  0,  0,  0,  0,  0,  1, -1,
-     0,  0,  0,  0,  0,  0,  0,  0, -1,  1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-    #include 'modules/planning/math/smoothing_spline/spline_1d_seg.h'
-    
-    #include 'modules/canbus/vehicle/gem/protocol/accel_cmd_67.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_cmd_6b.h'
-#include 'modules/canbus/vehicle/gem/protocol/global_cmd_69.h'
-#include 'modules/canbus/vehicle/gem/protocol/headlight_cmd_76.h'
-#include 'modules/canbus/vehicle/gem/protocol/horn_cmd_78.h'
-#include 'modules/canbus/vehicle/gem/protocol/shift_cmd_65.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_cmd_6d.h'
-#include 'modules/canbus/vehicle/gem/protocol/turn_cmd_63.h'
-#include 'modules/canbus/vehicle/gem/protocol/wiper_cmd_90.h'
-    
-    namespace apollo {
-namespace canbus {
-namespace gem {
-    }
+    namespace v8 {
+namespace internal {
     }
     }
     
-      Byte t1(bytes + 1);
-  int32_t t = t1.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-    // config detail: {'name': 'pacmod_status', 'enum': {0:
-// 'PACMOD_STATUS_CONTROL_DISABLED', 1: 'PACMOD_STATUS_CONTROL_ENABLED'},
-// 'precision': 1.0, 'len': 1, 'is_signed_var': False, 'offset': 0.0,
-// 'physical_range': '[0|1]', 'bit': 0, 'type': 'enum', 'order': 'motorola',
-// 'physical_unit': ''}
-Global_rpt_6a::Pacmod_statusType Globalrpt6a::pacmod_status(
-    const std::uint8_t* bytes, int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 1);
+      BIND(&if_lhsisnotnumber);
+  {
+    // No checks on rhs are done yet. We just know lhs is not a number or Smi.
+    Label if_lhsisoddball(this), if_lhsisnotoddball(this);
+    Node* lhs_instance_type = LoadInstanceType(lhs);
+    Node* lhs_is_oddball = InstanceTypeEqual(lhs_instance_type, ODDBALL_TYPE);
+    Branch(lhs_is_oddball, &if_lhsisoddball, &if_lhsisnotoddball);
     }
