9d6fab2f138852411d2f55b5af99a3e59c237a68
SUNsung <weatherius@gmail.com>
2019-07-13 20:19:25 +0300

    Rewrite [540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py] File in: 2019-07-13 20:19:25

--- pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
+++ pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
@@ -1,111 +1,161 @@
 
         
-            yield (x, y, w)
+        
+init_zshrc = u'''echo '
+export SHELL=/usr/bin/zsh
+export HISTFILE=~/.zsh_history
+echo > $HISTFILE
+export SAVEHIST=100
+export HISTSIZE=100
+eval $(thefuck --alias {})
+setopt INC_APPEND_HISTORY
+echo 'instant mode ready: $THEFUCK_INSTANT_MODE'
+' > ~/.zshrc'''
+    
+    Options:
+  -h  This help text.
+  -q  Loggable output - no progress indicator
+  -qq No output except for errors
+  -d  Download only - do NOT install or unpack archives
+  -s  No-act. Perform ordering simulation
+  -y  Assume Yes to all queries and do not prompt
+  -f  Attempt to correct a system with broken dependencies in place
+  -m  Attempt to continue if archives are unlocatable
+  -u  Show a list of upgraded packages as well
+  -b  Build the source package after fetching it
+  -V  Show verbose version numbers
+  -c=? Read this configuration file
+  -o=? Set an arbitrary configuration option, eg -o dir::cache=/tmp
+See the apt-get(8), sources.list(5) and apt.conf(5) manual
+pages for more information and options.
+                       This APT has Super Cow Powers.
+'''
+apt_get_operations = ['update', 'upgrade', 'install', 'remove', 'autoremove',
+                      'purge', 'source', 'build-dep', 'dist-upgrade',
+                      'dselect-upgrade', 'clean', 'autoclean', 'check',
+                      'changelog', 'download']
+    
+            def reset(path):
+            os.mkdir('d')
+            with tarfile.TarFile(path, 'w') as archive:
+                for file in ('a', 'b', 'c', 'd/e'):
+                    with open(file, 'w') as f:
+                        f.write('*')
+    
+    
+class RequestsWarning(Warning):
+    '''Base warning for Requests.'''
+    pass
+    
+        def test_copy(self):
+        copy = self.case_insensitive_dict.copy()
+        assert copy is not self.case_insensitive_dict
+        assert copy == self.case_insensitive_dict
+    
+    if is_py2:
+    from urllib import (
+        quote, unquote, quote_plus, unquote_plus, urlencode, getproxies,
+        proxy_bypass, proxy_bypass_environment, getproxies_environment)
+    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urldefrag
+    from urllib2 import parse_http_list
+    import cookielib
+    from Cookie import Morsel
+    from StringIO import StringIO
+    from collections import Callable, Mapping, MutableMapping, OrderedDict
+    
+    
+def test_idna_with_version_attribute(mocker):
+    '''Verify we're actually setting idna version when it should be available.'''
+    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
+    assert info()['idna'] == {'version': '2.6'}
 
     
-    import tensorflow as tf
-from models import bidirectional
-from models import bidirectional_vd
+        def prepare(self):
+        '''Constructs a :class:`PreparedRequest <PreparedRequest>` for transmission and returns it.'''
+        p = PreparedRequest()
+        p.prepare(
+            method=self.method,
+            url=self.url,
+            headers=self.headers,
+            files=self.files,
+            data=self.data,
+            json=self.json,
+            params=self.params,
+            auth=self.auth,
+            cookies=self.cookies,
+            hooks=self.hooks,
+        )
+        return p
     
-        # Critic loss calculated from the estimated value function \hat{V}(s)
-    # versus the true value function V*(s).
-    critic_loss = create_critic_loss(cumulative_rewards, estimated_values,
-                                     present)
+            # Resolve redirects if allowed.
+        history = [resp for resp in gen] if allow_redirects else []
     
+        def test_preserve_last_key_case(self):
+        cid = CaseInsensitiveDict({
+            'Accept': 'application/json',
+            'user-Agent': 'requests',
+        })
+        cid.update({'ACCEPT': 'application/json'})
+        cid['USER-AGENT'] = 'requests'
+        keyset = frozenset(['ACCEPT', 'USER-AGENT'])
+        assert frozenset(i[0] for i in cid.items()) == keyset
+        assert frozenset(cid.keys()) == keyset
+        assert frozenset(cid) == keyset
     
-def assign_percent_real(session, percent_real_update, new_rate, current_rate):
-  '''Run assign operation where the we load the current_rate of percent
-  real into a Tensorflow variable.
-    
-            # attn_fun
-        scores = _attn_add_fun(score_v, keys, query)
-      elif attention_option == 'luong':
-        # reshape query: [batch_size, 1, num_units]
-        query = tf.reshape(query, [-1, 1, num_units])
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-      post_vim_message.assert_has_exact_calls( [
-    call( 'On the first day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'A test file in a Command-T', warning=False, truncate=True ),
-    call( 'On the second day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'Two popup menus, and a test file in a Command-T',
-          warning=False,
-          truncate=True ),
-  ] )
-
+    # The master toctree document.
+master_doc = 'index'
     
     
-  def raise_for_status( self ):
-    if self._exception:
-      raise self._exception
+Credentials
+-----------
+    
+    # One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'certbot-dns-digitalocean', u'certbot-dns-digitalocean Documentation',
+     [author], 1)
+]
     
     
-def _worker( executor_reference, work_queue ):
-  try:
-    while True:
-      work_item = work_queue.get( block=True )
-      if work_item is not None:
-        work_item.run()
-        continue
-      executor = executor_reference()
-      # Exit if:
-      #   - The executor that owns the worker has been collected OR
-      #   - The executor that owns the worker has been shutdown.
-      if executor is None or executor._shutdown:
-        # Notice other workers
-        work_queue.put( None )
-        return
-      del executor
-  except BaseException:
-    _base.LOGGER.critical( 'Exception in worker', exc_info=True )
+def linear_dense(x, n_unit, name=None, reuse=None):
+    '''线性全连接层
+    Input shape:  [batch_size, n_input]
+    Output shape: [batch_size, n_unit]
     
-        Args:
-        pending_work_items: A dict mapping work ids to _WorkItems e.g.
-            {5: <_WorkItem...>, 6: <_WorkItem...>, ...}
-        work_ids: A queue.Queue of work ids e.g. Queue([5, 6, ...]). Work ids
-            are consumed and the corresponding _WorkItems from
-            pending_work_items are transformed into _CallItems and put in
-            call_queue.
-        call_queue: A multiprocessing.Queue that will be filled with _CallItems
-            derived from _WorkItems.
+        公式
+        `o = H(x, W)T(x, W) + x(1 - T(x, W))`
+    其中
+        H, T = dense
     '''
-    while True:
-        if call_queue.full():
-            return
-        try:
-            work_id = work_ids.get(block=False)
-        except queue.Empty:
-            return
-        else:
-            work_item = pending_work_items[work_id]
+    n_input = int(x.get_shape()[-1])
+    with tf.variable_scope(name or 'highway_dense'):
+        W, b = get_wb([n_input, n_input])
     
-      with open(csvfile, 'rb') as f:
-      reader = csv.reader(f)
-      comments = {rows[0]:rows[1] for rows in reader}
-  
-  for kind in ['Daemons', 'Agents']:
-    for filename in glob.glob(location % kind):
-      if not filename.endswith('com.apple.jetsamproperties.Mac.plist'):
-        p = LoadPlist(filename)
-        if p:
-          e = (filename, GetLabel(p), ''%s',%s' % GetProgram(p), GetStatus(p), ''%s'' % GetComment(p))
-          print('%s,%s,%s,%s,%s' % e)
-        else:
-          print('Could not load %s' % filename)
+    References:
+    [paper] Bidirectional Attention Flow for Machine Comprehension (https://arxiv.org/abs/1611.01603)
+    [github/DuReader] https://github.com/baidu/DuReader/blob/master/tensorflow/layers/match_layer.py
+    [github/BiDAF(PyTorch)] https://github.com/jojonki/BiDAF/blob/master/layers/bidaf.py
+'''
+    
+    
+def get_w(shape,
+          w_initializer=truncated_normal,
+          w_regularizer=l2_regularizer,
+          name=None):
+    name = name or 'W'
+    W = tf.get_variable(name, shape, dtype=tf_float, initializer=w_initializer,
+                        regularizer=w_regularizer)
+    return W
+    
+      Returns:
+    The filename with the common suffix removed.
+  '''
+  for suffix in itertools.chain(
+      ('%s.%s' % (test_suffix.lstrip('_'), ext)
+       for test_suffix, ext in itertools.product(_test_suffixes, GetNonHeaderExtensions())),
+      ('%s.%s' % (suffix, ext)
+       for suffix, ext in itertools.product(['inl', 'imp', 'internal'], GetHeaderExtensions()))):
+    if (filename.endswith(suffix) and len(filename) > len(suffix) and
+        filename[-len(suffix) - 1] in ('-', '_')):
+      return filename[:-len(suffix) - 1]
+  return os.path.splitext(filename)[0]
