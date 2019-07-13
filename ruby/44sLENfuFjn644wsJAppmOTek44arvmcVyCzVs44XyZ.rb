6807a3a31a9c3a9ac36bd3e70618b3c0650242a9
SUNsung <weatherius@gmail.com>
2019-07-13 23:35:08 +0300

    Rewrite [44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb] File in: 2019-07-13 23:35:07

--- ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
+++ ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
@@ -1,127 +1,93 @@
 
         
-                  def datetime_selector(options, html_options)
-            datetime = options.fetch(:selected) { value || default_datetime(options) }
-            @auto_index ||= nil
-    
-        # for test and sidekiq
-    def async=(val)
-      @async = val
-    end
-    
-          if period_type == :all
-        DB.exec <<~SQL
-          UPDATE user_stats s
-          SET likes_given         = d.likes_given,
-              likes_received      = d.likes_received,
-              topic_count         = d.topic_count,
-              post_count          = d.post_count
-    
-        @omniauth.before_request_phase do |env|
-      # If the user is trying to reconnect to an existing account, store in session
-      request = ActionDispatch::Request.new(env)
-      request.session[:auth_reconnect] = !!request.params['reconnect']
-    end
-  end
-    
-        # remove old drafts
-    delete_drafts_older_than_n_days = SiteSetting.delete_drafts_older_than_n_days.days.ago
-    Draft.where('updated_at < ?', delete_drafts_older_than_n_days).destroy_all
-  end
-    
-        Extension.theme_extensions(theme_ids).each { |extension| builder << extension }
-    Extension.plugin_extensions.each { |extension| builder << extension }
-    builder << Extension.site_setting_extension
-    
-          extensions
-    end
-    
-      it 'can be extended by plugins' do
-    plugin = Class.new(Plugin::Instance) do
-      attr_accessor :enabled
-      def enabled?
-        @enabled
-      end
-    end.new(nil, '#{Rails.root}/spec/fixtures/plugins/csp_extension/plugin.rb')
-    
-      def search
-    RailsMultisite::ConnectionManagement.with_hostname(params[:hostname]) do
-    
-        cookies.delete('authentication_data')
-    cookies.delete(TOKEN_COOKIE)
-  end
-    
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
-    end
-  end
+        # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
 end
 
     
-    describe ConversationsController, :type => :controller do
-  describe '#index' do
-    before do
-      @person = alice.contacts.first.person
-      hash = {
-        :author => @person,
-        :participant_ids => [alice.person.id, @person.id],
-        :subject => 'not spam',
-        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
-      }
-      @conv1 = Conversation.create(hash)
-      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
-             .increase_unread(alice)
-      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
-             .increase_unread(alice)
-    
-      describe '#new' do
-    before do
-      sign_in alice, scope: :user
-    end
-    
-          it_behaves_like 'on a visible post'
-    end
-    
-        it 'redirects to /stream for a mobile user' do
-      request.headers['X_MOBILE_DEVICE'] = true
-      post :create, params: {user: {remember_me: '0', username: @user.username, password: 'evankorth'}}
-      expect(response).to be_redirect
-      expect(response.location).to match /^#{stream_url}\??$/
-    end
+    if pathutil_relative == native_relative
+  Benchmark.ips do |x|
+    x.report('pathutil') { pathutil_relative }
+    x.report('native')   { native_relative }
+    x.compare!
   end
-    
-      gem 'bacon'
-  gem 'mocha'
-  gem 'mocha-on-bacon'
-  gem 'prettybacon'
-  gem 'webmock'
-    
-        pod 'ObjCPod', path: 'ObjCPod'
-    pod 'SwiftPod', path: 'SwiftPod'
-    pod 'MixedPod', path: 'MixedPod'
-    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
-    
-    require_relative '../lib/bootstrap/environment'
-    
-          explicit_plugins_specs.each do |spec|
-        packet_gem.add(spec.name)
-      end
-    
-        puts('Packaging plugins for offline usage')
-    
-      it 'does object equality on config_hash and pipeline_id' do
-    another_exact_pipeline = described_class.new(source, pipeline_id, ordered_config_parts, settings)
-    expect(subject).to eq(another_exact_pipeline)
-    
-      not_found do
-    send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), {:status => 404})
-  end
-    
-    module Jekyll
-    
-    Liquid::Template.register_tag('include_code', Jekyll::IncludeCodeTag)
+else
+  print 'PATHUTIL: '
+  puts pathutil_relative
+  print 'NATIVE:   '
+  puts native_relative
+end
 
     
-    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
+              new_theme_name = args.join('_')
+          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
+    
+    if defined?(ActionMailer)
+  class Devise::Mailer < Devise.parent_mailer.constantize
+    include Devise::Mailers::Helpers
+    
+    class TestApp < Rails::Application
+  config.root = File.dirname(__FILE__)
+  config.session_store :cookie_store, key: 'cookie_store_key'
+  secrets.secret_token    = 'secret_token'
+  secrets.secret_key_base = 'secret_key_base'
+  config.eager_load = false
+    
+          def expire_data_after_sign_in!
+        # session.keys will return an empty array if the session is not yet loaded.
+        # This is a bug in both Rack and Rails.
+        # A call to #empty? forces the session to be loaded.
+        session.empty?
+        session.keys.grep(/^devise\./).each { |k| session.delete(k) }
+      end
+    
+            # Attempt to find a user by its reset_password_token to reset its
+        # password. If a user is found and token is still valid, reset its password and automatically
+        # try saving the record. If not user is found, returns a new user
+        # containing an error in reset_password_token attribute.
+        # Attributes must contain reset_password_token, password and confirmation
+        def reset_password_by_token(attributes={})
+          original_token       = attributes[:reset_password_token]
+          reset_password_token = Devise.token_generator.digest(self, :reset_password_token, original_token)
+    
+        if run? && ARGV.any?
+      require 'optparse'
+      OptionParser.new { |op|
+        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
+        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
+        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
+        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
+        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
+        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
+      }.parse!(ARGV.dup)
+    end
+  end
+    
+      # insert data
+  fields.each do |field, values|
+    updated = '  s.#{field} = ['
+    updated << values.map { |v| '\n    %p' % v }.join(',')
+    updated << '\n  ]'
+    content.sub!(/  s\.#{field} = \[\n(    .*\n)*  \]/, updated)
+  end
+    
+          # Creates a masked version of the authenticity token that varies
+      # on each request. The masking is used to mitigate SSL attacks
+      # like BREACH.
+      def mask_token(token)
+        token = decode_token(token)
+        one_time_pad = SecureRandom.random_bytes(token.length)
+        encrypted_token = xor_byte_strings(one_time_pad, token)
+        masked_token = one_time_pad + encrypted_token
+        encode_token(masked_token)
+      end
+    
+      def update?
+    edit?
+  end
