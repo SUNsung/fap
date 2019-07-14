2314e8150898e58f8137012d0766945c32d48ed9
SUNsung <weatherius@gmail.com>
2019-07-14 11:37:06 +0300

    Rewrite [58Q4eWZC1eotw58q9gU5RV8cp.58cEBcQ0Jvgw58col.cpp] File in: 2019-07-14 11:37:06

--- cccccc/58Q4eWZC1eotw58q9gU5RV8cp.58cEBcQ0Jvgw58col.cpp
+++ cccccc/58Q4eWZC1eotw58q9gU5RV8cp.58cEBcQ0Jvgw58col.cpp
@@ -1,409 +1,120 @@
 
         
-        # endif  // NDEBUG for EXPECT_DEBUG_DEATH
-#endif  // GTEST_HAS_DEATH_TEST
-    
-    // Streams a Message to an ostream.
-inline std::ostream& operator <<(std::ostream& os, const Message& sb) {
-  return os << sb.GetString();
-}
-    
-    
-    {  // Assuming T is defined in namespace foo, in the next statement,
-  // the compiler will consider all of:
-  //
-  //   1. foo::operator<< (thanks to Koenig look-up),
-  //   2. ::operator<< (as the current namespace is enclosed in ::),
-  //   3. testing::internal2::operator<< (thanks to the using statement above).
-  //
-  // The operator<< whose type matches T best will be picked.
-  //
-  // We deliberately allow #2 to be a candidate, as sometimes it's
-  // impossible to define #1 (e.g. when foo is ::std, defining
-  // anything in it is undefined behavior unless you are a compiler
-  // vendor.).
-  *os << value;
-}
-    
-    // Two overloaded helpers for checking at compile time whether an
-// expression is a null pointer literal (i.e. NULL or any 0-valued
-// compile-time integral constant).  Their return values have
-// different sizes, so we can use sizeof() to test which version is
-// picked by the compiler.  These helpers have no implementations, as
-// we only need their signatures.
-//
-// Given IsNullLiteralHelper(x), the compiler will pick the first
-// version if x can be implicitly converted to Secret*, and pick the
-// second version otherwise.  Since Secret is a secret and incomplete
-// type, the only expression a user can write that has type Secret* is
-// a null pointer literal.  Therefore, we know that x is a null
-// pointer literal if and only if the first version is picked by the
-// compiler.
-char IsNullLiteralHelper(Secret* p);
-char (&IsNullLiteralHelper(...))[2];  // NOLINT
-    
-    // A unique type used as the default value for the arguments of class
-// template Types.  This allows us to simulate variadic templates
-// (e.g. Types<int>, Type<int, double>, and etc), which C++ doesn't
-// support directly.
-struct None {};
-    
-      virtual int GetNextPrime(int p) const {
-    for (int n = p + 1; n > 0; n++) {
-      if (IsPrime(n)) return n;
-    }
-    }
-    
-    namespace grpc {
-    }
-    
-      CensusClientCallData()
-      : recv_trailing_metadata_(nullptr),
-        initial_on_done_recv_trailing_metadata_(nullptr),
-        initial_on_done_recv_message_(nullptr),
-        elapsed_time_(0),
-        recv_message_(nullptr),
-        recv_message_count_(0),
-        sent_message_count_(0) {
-    memset(&stats_bin_, 0, sizeof(grpc_linked_mdelem));
-    memset(&tracing_bin_, 0, sizeof(grpc_linked_mdelem));
-    memset(&path_, 0, sizeof(grpc_slice));
-    memset(&on_done_recv_trailing_metadata_, 0, sizeof(grpc_closure));
-    memset(&on_done_recv_message_, 0, sizeof(grpc_closure));
+          inline Dtype diff_at(const vector<int>& index) const {
+    return cpu_diff()[offset(index)];
   }
     
-    ::opencensus::stats::MeasureInt64 RpcServerSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcServerReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerServerLatency();
-::opencensus::stats::MeasureInt64 RpcServerCompletedRpcs();
-    
-    
-    {}  // namespace grpc
-    
-    #endif  // GPR_APPLE
-
-    
-    using std::chrono::duration_cast;
-using std::chrono::high_resolution_clock;
-using std::chrono::nanoseconds;
-using std::chrono::seconds;
-using std::chrono::system_clock;
-    
-    class EnvPosixTest;
-    
-    static char* CopyString(const std::string& str) {
-  char* result = reinterpret_cast<char*>(malloc(sizeof(char) * str.size()));
-  memcpy(result, str.data(), sizeof(char) * str.size());
-  return result;
-}
-    
-    inline bool ParseInternalKey(const Slice& internal_key,
-                             ParsedInternalKey* result) {
-  const size_t n = internal_key.size();
-  if (n < 8) return false;
-  uint64_t num = DecodeFixed64(internal_key.data() + n - 8);
-  uint8_t c = num & 0xff;
-  result->sequence = num >> 8;
-  result->type = static_cast<ValueType>(c);
-  result->user_key = Slice(internal_key.data(), n - 8);
-  return (c <= static_cast<uint8_t>(kTypeValue));
-}
-    
-    Block::Block(const BlockContents& contents)
-    : data_(contents.data.data()),
-      size_(contents.data.size()),
-      owned_(contents.heap_allocated) {
-  if (size_ < sizeof(uint32_t)) {
-    size_ = 0;  // Error marker
-  } else {
-    size_t max_restarts_allowed = (size_ - sizeof(uint32_t)) / sizeof(uint32_t);
-    if (NumRestarts() > max_restarts_allowed) {
-      // The size is too small for NumRestarts()
-      size_ = 0;
-    } else {
-      restart_offset_ = size_ - (1 + NumRestarts()) * sizeof(uint32_t);
-    }
-  }
-}
-    
-    // Standard Get... routines parse a value from the beginning of a Slice
-// and advance the slice past the parsed value.
-bool GetVarint32(Slice* input, uint32_t* value);
-bool GetVarint64(Slice* input, uint64_t* value);
-bool GetLengthPrefixedSlice(Slice* input, Slice* result);
-    
-    int main(int argc, char** argv) { return leveldb::test::RunAllTests(); }
-
-    
-    
-    {  ASSERT_EQ(Hash(0, 0, 0xbc9f1d34), 0xbc9f1d34);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data1), sizeof(data1), 0xbc9f1d34),
-      0xef1345c4);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data2), sizeof(data2), 0xbc9f1d34),
-      0x5b663814);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data3), sizeof(data3), 0xbc9f1d34),
-      0x323c078f);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data4), sizeof(data4), 0xbc9f1d34),
-      0xed21633a);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data5), sizeof(data5), 0x12345678),
-      0xf333dabb);
-}
-    
-    
-    {}  // namespace leveldb
-
-    
-      ASSERT_TRUE(!Overlaps('a', 'b'));
-  ASSERT_TRUE(!Overlaps('z1', 'z2'));
-  ASSERT_TRUE(Overlaps('a', 'p'));
-  ASSERT_TRUE(Overlaps('a', 'q'));
-  ASSERT_TRUE(Overlaps('a', 'z'));
-  ASSERT_TRUE(Overlaps('p', 'p1'));
-  ASSERT_TRUE(Overlaps('p', 'q'));
-  ASSERT_TRUE(Overlaps('p', 'z'));
-  ASSERT_TRUE(Overlaps('p1', 'p2'));
-  ASSERT_TRUE(Overlaps('p1', 'z'));
-  ASSERT_TRUE(Overlaps('q', 'q'));
-  ASSERT_TRUE(Overlaps('q', 'q1'));
-    
-    #include <algorithm>
-#include <cstdint>
-#include <string>
-#include <type_traits>
-    
-      // Read until size drops significantly.
-  std::string limit_key = Key(n);
-  for (int read = 0; true; read++) {
-    ASSERT_LT(read, 100) << 'Taking too long to compact';
-    Iterator* iter = db_->NewIterator(ReadOptions());
-    for (iter->SeekToFirst();
-         iter->Valid() && iter->key().ToString() < limit_key; iter->Next()) {
-      // Drop data
-    }
-    delete iter;
-    // Wait a little bit to allow any triggered compactions to complete.
-    Env::Default()->SleepForMicroseconds(1000000);
-    uint64_t size = Size(Key(0), Key(n));
-    fprintf(stderr, 'iter %3d => %7.3f MB [other %7.3f MB]\n', read + 1,
-            size / 1048576.0, Size(Key(n), Key(kCount)) / 1048576.0);
-    if (size <= initial_size / 10) {
-      break;
-    }
-  }
-    
-    namespace leveldb {
-    }
-    
-    #include 'util/arena.h'
-#include 'util/random.h'
-    
-    
-    
-    /*
- * If Trace::hhbbc_time >= 1, print some stats about the program to a
- * temporary file.  If it's greater than or equal to 2, also dump it
- * to stdout.
+    /**
+ * @brief Applies common transformations to the input data, such as
+ * scaling, mirroring, substracting the image mean...
  */
-void print_stats(const Index&, const php::Program&);
-    
-    // Deserializing an array could give back a different ArrayKind than we need,
-// so we have to go with the slow case of calling a collection constructor.
-NEVER_INLINE
-Object createFromSerialized(CollectionType colType, APCHandle* handle) {
-  auto const col = Object::attach(collections::alloc(colType));
-  auto const arr = handle->toLocal();
-  switch (colType) {
-  case CollectionType::ImmVector:
-  case CollectionType::Vector:
-    static_cast<BaseVector*>(col.get())->init(arr);
-    break;
-  case CollectionType::ImmSet:
-  case CollectionType::Set:
-    static_cast<BaseSet*>(col.get())->init(arr);
-    break;
-  case CollectionType::ImmMap:
-  case CollectionType::Map:
-    static_cast<BaseMap*>(col.get())->init(arr);
-    break;
-  case CollectionType::Pair:
-    not_reached();
-    break;
-  }
-  return col;
-}
-    
-    
-    {    assertx(data == comma || data == semi);
-    // eat parameters, and figure out if we have ';base64'
-    while (semi && (data == semi)) {
-      data++;
-      meta_len--;
-      char* equals = (char*)memchr(data, '=', meta_len);
-      semi = (char*)memchr(data, ';', meta_len);
-      if (!equals || (semi && semi < data)) {
-        // no equals, so either 'base64' or its bad
-        if (meta_len != sizeof('base64') - 1 ||
-            memcmp(data, 'base64', sizeof('base64')-1)) {
-          raise_warning('rfc2396: invalid parameter');
-          return nullptr;
-        }
-        // it's 'base64', we're done
-        base64 = true;
-        meta_len -= sizeof('base64') - 1;
-        data += sizeof('base64') - 1;
-        break;
-      }
-      // there's a parameter
-      if (semi) {
-        meta_len -= semi - data + 1;
-        data = semi;
-      } /* else, we're done with meta */
-    }
-  }
-  data = comma + 1;
-  data_len -= 1;
-  String decoded;
-    
-    #include 'hphp/runtime/base/file.h'
-#include 'hphp/runtime/base/mem-file.h'
-#include 'hphp/runtime/base/stream-wrapper.h'
-#include <folly/String.h>
-#include <folly/portability/SysStat.h>
-#include <folly/portability/Unistd.h>
-    
-    
-    {  auto glob = HHVM_FN(glob)(String(path_str, path_len, CopyString));
-  if (!glob.isArray()) {
-    return nullptr;
-  }
-  return req::make<ArrayDirectory>(glob.toArray());
-}
-    
-        std::unique_ptr<DHTMessageReceiver> messageReceiver;
-    
-    DHTResponseMessage::DHTResponseMessage(
-    const std::shared_ptr<DHTNode>& localNode,
-    const std::shared_ptr<DHTNode>& remoteNode,
-    const std::string& transactionID)
-    : DHTAbstractMessage(localNode, remoteNode, transactionID)
-{
-}
-    
-    void DHTRoutingTableSerializer::serialize(const std::string& filename)
-{
-  A2_LOG_INFO(fmt('Saving DHT routing table to %s.', filename.c_str()));
-  std::string filenameTemp = filename;
-  filenameTemp += '__temp';
-  BufferedFile fp(filenameTemp.c_str(), BufferedFile::WRITE);
-  if (!fp) {
-    throw DL_ABORT_EX(
-        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
-  }
-  char header[8];
-  memset(header, 0, sizeof(header));
-  // magic
-  header[0] = 0xa1u;
-  header[1] = 0xa2u;
-  // format ID
-  header[2] = 0x02u;
-  // version
-  header[6] = 0;
-  header[7] = 0x03u;
+template <typename Dtype>
+class DataTransformer {
+ public:
+  explicit DataTransformer(const TransformationParameter& param, Phase phase);
+  virtual ~DataTransformer() {}
     }
     
-      void setLocalNode(const std::shared_ptr<DHTNode>& localNode);
     
-    std::string DHTUnknownMessage::toString() const
-{
-  size_t sampleLength = 8;
-  if (length_ < sampleLength) {
-    sampleLength = length_;
-  }
-  return fmt('dht unknown Remote:%s(%u) length=%lu, first 8 bytes(hex)=%s',
-             ipaddr_.c_str(), port_, static_cast<unsigned long>(length_),
-             util::toHex(data_, sampleLength).c_str());
-}
+    {}  // namespace caffe
     
-    public:
-  DNSCache();
-  DNSCache(const DNSCache& c);
-  ~DNSCache();
+      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     
-      void query(ExtensionResponse& _return, const std::string& sql) {
-    // Your implementation goes here
-    printf('query\n');
-  }
     
-    #include 'Extension.h'
-#include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/server/TSimpleServer.h>
-#include <thrift/transport/TServerSocket.h>
-#include <thrift/transport/TBufferTransports.h>
-    
-    #include <smartmontools/libsmartctl.h>
-    
-    void tableCompletionFunction(char const* prefix, linenoiseCompletions* lc) {
-  auto tables = osquery::RegistryFactory::get().names('table');
-  size_t index = 0;
-    }
-    
-    TEST_F(PerfOutputTests,
-       impl_consumeWrappedMessagesFromCircularBuffer_without_wrapping) {
-  using WrappedMessage = ebpf::PerfOutput<TestMessage>::WrappedMessage;
-  auto const test_size = std::size_t{9};
-  auto buf = std::vector<ebpf::impl::ByteType>(
-      sizeof(WrappedMessage) * test_size + 128, 0);
-  auto buf_ptr = &buf[0];
-  for (std::size_t i = 0; i < test_size; ++i) {
-    auto wrapped = WrappedMessage{};
-    wrapped.msg.a_ = i + 1;
-    wrapped.msg.b_ = i * 2 + 2;
-    wrapped.msg.c_ = 'j';
-    wrapped.msg.d_ = 'k';
-    wrapped.size = sizeof(TestMessage);
-    wrapped.header.type = PERF_RECORD_SAMPLE;
-    wrapped.header.size = sizeof(WrappedMessage);
-    auto const wrapped_ptr =
-        reinterpret_cast<ebpf::impl::ByteType const*>(&wrapped);
-    std::copy(wrapped_ptr, wrapped_ptr + sizeof(WrappedMessage), buf_ptr);
-    buf_ptr += sizeof(WrappedMessage);
-  }
-  auto dst = std::vector<TestMessage>{};
-  auto status =
-      ebpf::impl::consumeWrappedMessagesFromCircularBuffer<WrappedMessage>(
-          &buf[0],
-          0,
-          static_cast<std::size_t>(buf_ptr - &buf[0]),
-          buf.size(),
-          dst);
-  ASSERT_FALSE(status.isError());
-  ASSERT_EQ(dst.size(), test_size);
-  for (std::size_t i = 0; i < test_size; ++i) {
-    EXPECT_EQ(dst[i].a_, static_cast<int>(i + 1));
-    EXPECT_EQ(dst[i].b_, static_cast<int>(i * 2 + 2));
-    EXPECT_EQ(dst[i].c_, 'j');
-    EXPECT_EQ(dst[i].d_, 'k');
-  }
-}
-    
-    // Sanity check integration test for iokit_devicetree
-// Spec file: specs/darwin/iokit_devicetree.table
-    
-    class KernelModules : public testing::Test {
- protected:
-  void SetUp() override {
-    setUpEnvironment();
-  }
+    {  /**
+   * @brief Computes the error gradient w.r.t. the BNLL inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x}
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 };
     
-    namespace osquery {
-namespace table_tests {
+    #ifdef USE_CUDNN
+template <typename Dtype>
+class CuDNNLCNLayer : public LRNLayer<Dtype> {
+ public:
+  explicit CuDNNLCNLayer(const LayerParameter& param)
+      : LRNLayer<Dtype>(param), handles_setup_(false), tempDataSize(0),
+        tempData1(NULL), tempData2(NULL) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNLCNLayer();
     }
+    
+    
+    {}  // namespace caffe
+    
+    // A CostFunc that takes the variance of step into account in the cost.
+int64_t DPPoint::CostWithVariance(const DPPoint* prev) {
+  if (prev == nullptr || prev == this) {
+    UpdateIfBetter(0, 1, nullptr, 0, 0, 0);
+    return 0;
+  }
     }
+    
+      REJMAP &operator=(const REJMAP &source);
+    
+    namespace php { struct Unit; struct Program; }
+    
+      void branchFar(CodeAddress c,
+                 ConditionCode cc,
+                 LinkReg lr = LinkReg::DoNotTouch,
+                 ImmType immt = ImmType::TocOnly,
+                 bool immMayChange = false) {
+    branchFar(c, BranchParams::convertCC(cc), lr, immt, immMayChange);
+  }
+    
+    APCHandle::Pair APCCollection::WrapArray(APCHandle::Pair inner,
+                                         CollectionType colType) {
+  auto const col = new APCCollection;
+  col->m_arrayHandle = inner.handle;
+  col->m_colType = colType;
+  return { &col->m_handle, inner.size + sizeof(APCCollection) };
+}
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    struct Directory;
+    
+    private:
+  bool m_get;
+  bool m_ignoreErrors;
+  const char* m_method;
+  Array m_headers;
+  String m_postData;
+  int m_maxRedirect;
+  int m_timeout;
+  std::string m_error;
+  StringBuffer m_response;
+  Array m_responseHeaders;
+    
+    
+    {  auto ret = s_maps.insert(m_map);
+  if (ret.second) {
+    FTRACE(3, 'Adding User Attributes {}; {} out of {}\n',
+           user_attrs(this),
+           s_maps.size(),
+           s_count.load(std::memory_order_relaxed));
+  } else {
+    m_map = *ret.first;
+  }
+}
