65f220448e66c21c19e81de61cdfc24a5d5e976e
SUNsung <weatherius@gmail.com>
2019-07-14 22:24:26 +0300

    Rewrite [57T9773pt8kA657On0oT6tzC.Y57Cx0kbCIqeJ.57Koc.cpp] File in: 2019-07-14 22:24:22

--- cccccc/57T9773pt8kA657On0oT6tzC.Y57Cx0kbCIqeJ.57Koc.cpp
+++ cccccc/57T9773pt8kA657On0oT6tzC.Y57Cx0kbCIqeJ.57Koc.cpp
@@ -1,408 +1,114 @@
 
         
-        #endif // BITCOIN_QT_NETWORKSTYLE_H
+        namespace HPHP { struct UnitEmitter; }
+namespace HPHP { namespace HHBBC {
+    }
+    }
+    
+    // Deserializing an array could give back a different ArrayKind than we need,
+// so we have to go with the slow case of calling a collection constructor.
+NEVER_INLINE
+Object createFromSerialized(CollectionType colType, APCHandle* handle) {
+  auto const col = Object::attach(collections::alloc(colType));
+  auto const arr = handle->toLocal();
+  switch (colType) {
+  case CollectionType::ImmVector:
+  case CollectionType::Vector:
+    static_cast<BaseVector*>(col.get())->init(arr);
+    break;
+  case CollectionType::ImmSet:
+  case CollectionType::Set:
+    static_cast<BaseSet*>(col.get())->init(arr);
+    break;
+  case CollectionType::ImmMap:
+  case CollectionType::Map:
+    static_cast<BaseMap*>(col.get())->init(arr);
+    break;
+  case CollectionType::Pair:
+    not_reached();
+    break;
+  }
+  return col;
+}
+    
+    #endif // HPHP_FILE_STREAM_WRAPPER_H
 
     
-    namespace Ui {
-    class OpenURIDialog;
-}
+    #include 'hphp/util/perf-event.h'
+    
+    #include <folly/String.h>
+    
+    #include 'hphp/runtime/base/perf-warning-inl.h'
+    
+    protected:
+  FILE *m_stream;
+  char *m_buffer;       // For setbuffer.  Needed to reduce mmap
+                        // contention due to how glibc allocates memory
+                        // for buffered io.
+    
+    private:
+  bool m_get;
+  bool m_ignoreErrors;
+  const char* m_method;
+  Array m_headers;
+  String m_postData;
+  int m_maxRedirect;
+  int m_timeout;
+  std::string m_error;
+  StringBuffer m_response;
+  Array m_responseHeaders;
     
     /**
- * An RAII-style reverse lock. Unlocks on construction and locks on destruction.
+ * The schedule is an iterable collection of Packs. When you iterate through
+ * a schedule, you only get the packs that should be running on the host that
+ * you're currently operating on.
  */
-template<typename Lock>
-class reverse_lock
-{
-public:
+class Schedule : private boost::noncopyable {
+ public:
+  /// Under the hood, the schedule is just a list of the Pack objects
+  using container = std::vector<PackRef>;
     }
     
-    #endif /* SECP256K1_MODULE_RECOVERY_MAIN_H */
-
+      /// Inspect the refresh rate.
+  size_t getRefresh() const;
     
-    #define TINYFORMAT_ARGTYPES(n) TINYFORMAT_ARGTYPES_ ## n
-#define TINYFORMAT_VARARGS(n) TINYFORMAT_VARARGS_ ## n
-#define TINYFORMAT_PASSARGS(n) TINYFORMAT_PASSARGS_ ## n
-#define TINYFORMAT_PASSARGS_TAIL(n) TINYFORMAT_PASSARGS_TAIL_ ## n
-    
-    int UniValue::get_int() const
-{
-    if (typ != VNUM)
-        throw std::runtime_error('JSON value is not an integer as expected');
-    int32_t retval;
-    if (!ParseInt32(getValStr(), &retval))
-        throw std::runtime_error('JSON integer out of range');
-    return retval;
-}
-    
-    /** Decode a Bech32 string. Returns (hrp, data). Empty hrp means failure. */
-std::pair<std::string, std::vector<uint8_t>> Decode(const std::string& str);
-    
-    public:
-    static const size_t OUTPUT_SIZE = 20;
-    
-    /*
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Copyright (c) 2012, Intel Corporation 
-; 
-; All rights reserved. 
-; 
-; Redistribution and use in source and binary forms, with or without
-; modification, are permitted provided that the following conditions are
-; met: 
-; 
-; * Redistributions of source code must retain the above copyright
-;   notice, this list of conditions and the following disclaimer.  
-; 
-; * Redistributions in binary form must reproduce the above copyright
-;   notice, this list of conditions and the following disclaimer in the
-;   documentation and/or other materials provided with the
-;   distribution. 
-; 
-; * Neither the name of the Intel Corporation nor the names of its
-;   contributors may be used to endorse or promote products derived from
-;   this software without specific prior written permission. 
-; 
-; 
-; THIS SOFTWARE IS PROVIDED BY INTEL CORPORATION 'AS IS' AND ANY
-; EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-; PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL INTEL CORPORATION OR
-; CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-; PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-; LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
-; Example YASM command lines:
-; Windows:  yasm -Xvc -f x64 -rnasm -pnasm -o sha256_sse4.obj -g cv8 sha256_sse4.asm
-; Linux:    yasm -f x64 -f elf64 -X gnu -g dwarf2 -D LINUX -o sha256_sse4.o sha256_sse4.asm
-;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
-; This code is described in an Intel White-Paper:
-; 'Fast SHA-256 Implementations on Intel Architecture Processors'
-;
-; To find it, surf to http://www.intel.com/p/en_US/embedded 
-; and search for that title.
-; The paper is expected to be released roughly at the end of April, 2012
-;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; This code schedules 1 blocks at a time, with 4 lanes per block
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    
-    
-    {
-    {
-    {
-    {
-    {}  // namespace
-}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
-    
-    TEST(ByteSourceTest, LimitByteSource) {
-  StringPiece data('Hello world!');
-  MockByteSource source(data, 3);
-  LimitByteSource limit_source(&source, 6);
-  EXPECT_EQ(6, limit_source.Available());
-  limit_source.Skip(1);
-  EXPECT_EQ(5, limit_source.Available());
-    }
-    
-    // Author: brianolson@google.com (Brian Olson)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
-//
-// Test program to verify that GzipInputStream is compatible with command line
-// gunzip or java.util.zip.GzipInputStream
-//
-// Reads gzip stream on standard input and writes decompressed data to standard
-// output.
-    
-    int main(int argc, char *argv[]) {
-  if (argc % 2 == 0 || argc == 1) {
-    std::cerr << 'Usage: [input_files] [output_file_names] where ' <<
-        'input_files are one to one mapping to output_file_names.' <<
-        std::endl;
-    return 1;
-  }
-    }
-    
-      {
-    // Write the new address book back to disk.
-    fstream output(argv[1], ios::out | ios::trunc | ios::binary);
-    if (!address_book.SerializeToOstream(&output)) {
-      cerr << 'Failed to write address book.' << endl;
-      return -1;
-    }
+      fpack.platform_ = 'posix';
+  if (isPlatform(PlatformType::TYPE_POSIX) ||
+      isPlatform(PlatformType::TYPE_LINUX) ||
+      isPlatform(PlatformType::TYPE_OSX) ||
+      isPlatform(PlatformType::TYPE_FREEBSD)) {
+    EXPECT_TRUE(fpack.checkPlatform());
+  } else {
+    EXPECT_FALSE(fpack.checkPlatform());
   }
     
-    
-    { private:
-  const EnumDescriptor* descriptor_;
-  std::vector<const EnumValueDescriptor*> base_values_;
-  std::vector<const EnumValueDescriptor*> all_values_;
-  std::set<const EnumValueDescriptor*> alias_values_to_skip_;
-  const string name_;
-};
-    
-    
-    {}  // namespace grpc
-    
-    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
-constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
-constexpr size_t TraceContextEncoding::kVersionIdSize;
-constexpr size_t TraceContextEncoding::kFieldIdSize;
-constexpr size_t TraceContextEncoding::kVersionIdOffset;
-constexpr size_t TraceContextEncoding::kVersionId;
-    
-     private:
-  // Parses the next field from the incoming buffer and stores the parsed value
-  // in a GrpcTraceContext struct.  If it does not recognize the field ID it
-  // will return 0, otherwise it returns the number of bytes read.
-  static size_t ParseField(absl::string_view buf, GrpcTraceContext* tc) {
-    // TODO: Add support for multi-byte field IDs.
-    if (buf.empty()) {
-      return 0;
-    }
-    // Field ID is always the first byte in a field.
-    uint32_t field_id = buf[0];
-    size_t bytes_read = kFieldIdSize;
-    switch (field_id) {
-      case kTraceIdField:
-        bytes_read += kTraceIdSize;
-        if (bytes_read > buf.size()) {
-          return 0;
-        }
-        memcpy(tc->trace_id, &buf[kFieldIdSize],
-               opencensus::trace::TraceId::kSize);
-        break;
-      case kSpanIdField:
-        bytes_read += kSpanIdSize;
-        if (bytes_read > buf.size()) {
-          return 0;
-        }
-        memcpy(tc->span_id, &buf[kFieldIdSize],
-               opencensus::trace::SpanId::kSize);
-        break;
-      case kTraceOptionsField:
-        bytes_read += kTraceOptionsSize;
-        if (bytes_read > buf.size()) {
-          return 0;
-        }
-        memcpy(tc->trace_options, &buf[kFieldIdSize],
-               opencensus::trace::TraceOptions::kSize);
-        break;
-      default:  // Invalid field ID
-        return 0;
-    }
-    }
-    
-    
-    {}  // namespace
-    
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  host_cpu_load_info_data_t cpuinfo;
-  mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
-  if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO,
-                      (host_info_t)&cpuinfo, &count) == KERN_SUCCESS) {
-    for (int i = 0; i < CPU_STATE_MAX; i++) total += cpuinfo.cpu_ticks[i];
-    busy = total - cpuinfo.cpu_ticks[CPU_STATE_IDLE];
+    Expected<int32_t, DatabaseError> Database::getInt32Or(
+    const std::string& domain,
+    const std::string& key,
+    const int32_t default_value) {
+  auto result = getInt32(domain, key);
+  if (!result && result.getError() == DatabaseError::KeyNotFound) {
+    return default_value;
   }
-  return std::make_pair(busy, total);
-}
-    
-    PerBalancerStore* PerHostStore::FindPerBalancerStore(
-    const grpc::string& lb_id) const {
-  return per_balancer_stores_.find(lb_id) != per_balancer_stores_.end()
-             ? per_balancer_stores_.find(lb_id)->second.get()
-             : nullptr;
-}
-    
-    #endif  // STORAGE_LEVELDB_DB_LOG_FORMAT_H_
-
-    
-      int height = RandomHeight();
-  if (height > GetMaxHeight()) {
-    for (int i = GetMaxHeight(); i < height; i++) {
-      prev[i] = head_;
-    }
-    // It is ok to mutate max_height_ without any synchronization
-    // with concurrent readers.  A concurrent reader that observes
-    // the new value of max_height_ will see either the old value of
-    // new level pointers from head_ (nullptr), or a new value set in
-    // the loop below.  In the former case the reader will
-    // immediately drop to the next level since nullptr sorts after all
-    // keys.  In the latter case the reader will use the new node.
-    max_height_.store(height, std::memory_order_relaxed);
-  }
-    
-      int level_;
-  uint64_t max_output_file_size_;
-  Version* input_version_;
-  VersionEdit edit_;
-    
-      const char kWrite2Data[] = 'Write #2 data';
-  ASSERT_OK(WriteStringToFile(env_, kWrite2Data, kTestFileName));
-    
-    
-    {  const FilterPolicy* policy_;
-  std::string keys_;             // Flattened key contents
-  std::vector<size_t> start_;    // Starting index in keys_ of each key
-  std::string result_;           // Filter data computed so far
-  std::vector<Slice> tmp_keys_;  // policy_->CreateFilter() argument
-  std::vector<uint32_t> filter_offsets_;
-};
-    
-    char* Arena::AllocateNewBlock(size_t block_bytes) {
-  char* result = new char[block_bytes];
-  blocks_.push_back(result);
-  memory_usage_.fetch_add(block_bytes + sizeof(char*),
-                          std::memory_order_relaxed);
   return result;
 }
     
-    inline char* Arena::Allocate(size_t bytes) {
-  // The semantics of what to return are a bit messy if we allow
-  // 0-byte allocations, so we disallow them here (we don't need
-  // them for our internal use).
-  assert(bytes > 0);
-  if (bytes <= alloc_bytes_remaining_) {
-    char* result = alloc_ptr_;
-    alloc_ptr_ += bytes;
-    alloc_bytes_remaining_ -= bytes;
-    return result;
-  }
-  return AllocateFallback(bytes);
+    ExpectedSuccess<RocksdbMigrationError> migrateRocksDBDatabase(
+    const std::string& path);
+    
+      /** @brief Virtual method which should implement custom logging.
+   *
+   *  LoggerPlugin::logString should be implemented by a subclass of
+   *  LoggerPlugin which needs to log a string in a custom way.
+   *
+   *  @return an instance of osquery::Status which indicates the success or
+   *  failure of the operation.
+   */
+  virtual Status logString(const std::string& s) = 0;
+    
+    GTEST_TEST(InMemoryDatabaseTest, test_open) {
+  auto db = std::make_unique<InMemoryDatabase>('test');
+  auto result = db->open();
+  EXPECT_TRUE(result);
+  db->close();
 }
-    
-    TEST(EnvWindowsTest, TestOpenOnRead) {
-  // Write some test data to a single file that will be opened |n| times.
-  std::string test_dir;
-  ASSERT_OK(env_->GetTestDirectory(&test_dir));
-  std::string test_file = test_dir + '/open_on_read.txt';
-    }
-    
-        // print values
-    std::cout << object << '\n';
-    std::cout << null << '\n';
-    
-    #endif // OPENPOSE_GUI_GUI_ADAM_HPP
-#endif
-
-    
-        template<typename T>
-    Rectangle<T>::Rectangle(Rectangle<T>&& rectangle)
-    {
-        try
-        {
-            x = rectangle.x;
-            y = rectangle.y;
-            width = rectangle.width;
-            height = rectangle.height;
-        }
-        catch (const std::exception& e)
-        {
-            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
-        }
-    }
-    
-          case 0xF6:
-        { data++;
-          int mod, regop, rm;
-          get_modrm(*data, &mod, &regop, &rm);
-          if (regop == eax) {
-            AppendToBuffer('test_b ');
-            data += PrintRightByteOperand(data);
-            int32_t imm = *data;
-            AppendToBuffer(',0x%x', imm);
-            data++;
-          } else {
-            UnimplementedInstruction();
-          }
-        }
-        break;
-    
-    void MacroAssembler::LeaveApiExitFrame() {
-  mov(esp, ebp);
-  pop(ebp);
-    }
-    
-    #define SSSE3_INSTRUCTION_LIST(V) \
-  V(phaddd, 66, 0F, 38, 02)       \
-  V(phaddw, 66, 0F, 38, 01)       \
-  V(pshufb, 66, 0F, 38, 00)       \
-  V(psignb, 66, 0F, 38, 08)       \
-  V(psignw, 66, 0F, 38, 09)       \
-  V(psignd, 66, 0F, 38, 0A)
-    
-    
-    {  flatbuffers::grpc::MessageBuilder mb_;
-};
-    
-    #ifndef GRPC_CUSTOM_STRING
-#  include <string>
-#  define GRPC_CUSTOM_STRING std::string
-#endif
-    
-      // We now have a FlatBuffer that can be stored on disk or sent over a network.
-    
-      if (jsongen != jsonfile) {
-    printf('%s----------------\n%s', jsongen.c_str(), jsonfile.c_str());
-  }
-    
-    
-    {      GenReceiver(struct_def, code_ptr);
-      code += MakeCamel(NormalizedName(field)) + 'Length()\n';
-      code += OffsetPrefix(field);
-      code += std::string(Indent) + Indent + 'return ' + SelfData + ':VectorLen(o)\n';
-      code += std::string(Indent) + End;
-      code += std::string(Indent) + 'return 0\n';
-      code += EndFunc;
-    }
-    
-      // Get a struct by initializing an existing struct.
-  // Specific to Table.
-  void GetStructFieldOfTable(const StructDef &struct_def,
-                             const FieldDef &field,
-                             std::string *code_ptr) {
-    std::string &code = *code_ptr;
-    GenReceiver(struct_def, code_ptr);
-    code += MakeCamel(NormalizedName(field));
-    code += '(self):';
-    code += OffsetPrefix(field);
-    if (field.value.type.struct_def->fixed) {
-      code += Indent + Indent + Indent + 'x = o + self._tab.Pos\n';
-    } else {
-      code += Indent + Indent + Indent;
-      code += 'x = self._tab.Indirect(o + self._tab.Pos)\n';
-    }
-    code += Indent + Indent + Indent;
-    code += 'from .' + TypeName(field) + ' import ' + TypeName(field) + '\n';
-    code += Indent + Indent + Indent + 'obj = ' + TypeName(field) + '()\n';
-    code += Indent + Indent + Indent + 'obj.Init(self._tab.Bytes, x)\n';
-    code += Indent + Indent + Indent + 'return obj\n';
-    code += Indent + Indent + 'return None\n\n';
-  }
-    
-    std::string TextMakeRule(const Parser &parser, const std::string &path,
-                         const std::string &file_name) {
-  if (!parser.builder_.GetSize() || !parser.root_struct_def_) return '';
-  std::string filebase =
-      flatbuffers::StripPath(flatbuffers::StripExtension(file_name));
-  std::string make_rule = TextFileName(path, filebase) + ': ' + file_name;
-  auto included_files =
-      parser.GetIncludedFilesRecursive(parser.root_struct_def_->file);
-  for (auto it = included_files.begin(); it != included_files.end(); ++it) {
-    make_rule += ' ' + *it;
-  }
-  return make_rule;
-}
-    
-    void message_builder_tests();
