72ad450a27036fec175ca3b49b0a630dd2da4196
SUNsung <weatherius@gmail.com>
2019-07-15 02:42:51 +0300

    Rewrite [191MyHlxP3Z9U19kledSUPjpQ1961MaYY4mcc192O.cpp] File in: 2019-07-15 02:42:51

--- cccccc/191MyHlxP3Z9U19kledSUPjpQ1961MaYY4mcc192O.cpp
+++ cccccc/191MyHlxP3Z9U19kledSUPjpQ1961MaYY4mcc192O.cpp
@@ -1,222 +1,121 @@
 
         
-        IPC_SYNC_MESSAGE_ROUTED3_1(ShellViewHostMsg_Call_Static_Method_Sync,
-                           std::string /* type name */,
-                           std::string /* method name */,
-                           base::ListValue /* arguments */,
-                           base::ListValue /* result */)
+        #ifndef CAROTENE_TYPES_HPP
+#define CAROTENE_TYPES_HPP
     
-       void Call(const std::string& method,
-                    const base::ListValue& arguments) override;
-   void CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result) override;
-    
-        bool Read(ClipboardData& data) {
-      switch(data.type) {
-        case TYPE_TEXT:
-        return ReadText(data);
-        break;
-        case TYPE_HTML:
-        return ReadHTML(data);
-        break;
-        case TYPE_RTF:
-        return ReadRTF(data);
-        break;
-        case TYPE_PNG:
-        case TYPE_JPEG:
-        return ReadImage(data);
-        break;
-        case TYPE_NONE:
-        NOTREACHED();
-        return false;
-      }
-      NOTREACHED();
-      return false;      
-    }
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35,
-    typename T36, typename T37, typename T38, typename T39, typename T40,
-    typename T41, typename T42, typename T43, typename T44, typename T45,
-    typename T46, typename T47>
-internal::ValueArray47<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-    T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42, T43,
-    T44, T45, T46, T47> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7,
-    T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15,
-    T16 v16, T17 v17, T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23,
-    T24 v24, T25 v25, T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31,
-    T32 v32, T33 v33, T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39,
-    T40 v40, T41 v41, T42 v42, T43 v43, T44 v44, T45 v45, T46 v46, T47 v47) {
-  return internal::ValueArray47<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39,
-      T40, T41, T42, T43, T44, T45, T46, T47>(v1, v2, v3, v4, v5, v6, v7, v8,
-      v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23,
-      v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37,
-      v38, v39, v40, v41, v42, v43, v44, v45, v46, v47);
-}
-    
-    
-    
-    // The variables defined in the type-parameterized test macros are
-// static as typically these macros are used in a .h file that can be
-// #included in multiple translation units linked together.
-# define TYPED_TEST_CASE_P(CaseName) \
-  static ::testing::internal::TypedTestCasePState \
-      GTEST_TYPED_TEST_CASE_P_STATE_(CaseName)
-    
-      // Compares two wide C strings, ignoring case.  Returns true iff they
-  // have the same content.
-  //
-  // Unlike wcscasecmp(), this function can handle NULL argument(s).
-  // A NULL C string is considered different to any non-NULL wide C string,
-  // including the empty string.
-  // NB: The implementations on different platforms slightly differ.
-  // On windows, this method uses _wcsicmp which compares according to LC_CTYPE
-  // environment variable. On GNU platform this method uses wcscasecmp
-  // which compares according to LC_CTYPE category of the current locale.
-  // On MacOS X, it uses towlower, which also uses LC_CTYPE category of the
-  // current locale.
-  static bool CaseInsensitiveWideCStringEquals(const wchar_t* lhs,
-                                               const wchar_t* rhs);
-    
-    // SameSizeTuplePrefixComparator<k, k>::Eq(t1, t2) returns true if the
-// first k fields of t1 equals the first k fields of t2.
-// SameSizeTuplePrefixComparator(k1, k2) would be a compiler error if
-// k1 != k2.
-template <int kSize1, int kSize2>
-struct SameSizeTuplePrefixComparator;
-    
-    #include 'gtest/internal/gtest-port.h'
-    
-      bool check_for_leaks = false;
-  if (argc > 1 && strcmp(argv[1], '--check_for_leaks') == 0 )
-    check_for_leaks = true;
-  else
-    printf('%s\n', 'Run this program with --check_for_leaks to enable '
-           'custom leak checking in the tests.');
-    
-    #include 'sample2.h'
-    
-    
-    {  // Sets the 0-terminated C string this MyString object represents.
-  void Set(const char* c_string);
-};
-    
-      grpc_error* Init(grpc_call_element* elem,
-                   const grpc_call_element_args* args) override;
-    
-    
-    {}  // namespace grpc
-    
-      ::opencensus::trace::SpanContext ToSpanContext() const {
-    return ::opencensus::trace::SpanContext(
-        ::opencensus::trace::TraceId(trace_id),
-        ::opencensus::trace::SpanId(span_id),
-        ::opencensus::trace::TraceOptions(trace_options));
-  }
-    
-    Status ProtoServerReflection::GetFileByName(
-    ServerContext* context, const grpc::string& filename,
-    ServerReflectionResponse* response) {
-  if (descriptor_pool_ == nullptr) {
-    return Status::CANCELLED;
-  }
-    }
-    
-    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
-    const grpc::string& name, const grpc::string& value) {
-  class StringOption final : public ServerBuilderOption {
-   public:
-    StringOption(const grpc::string& name, const grpc::string& value)
-        : name_(name), value_(value) {}
-    }
-    }
-    
-    #endif  // !GRPC_CUSTOM_DEFAULT_THREAD_POOL
-
-    
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  FILETIME idle, kernel, user;
-  if (GetSystemTimes(&idle, &kernel, &user) != 0) {
-    total = FiletimeToInt(kernel) + FiletimeToInt(user);
-    busy = total - FiletimeToInt(idle);
-  }
-  return std::make_pair(busy, total);
-}
-    
-    // A thread pool interface for running callbacks.
-class ThreadPoolInterface {
- public:
-  virtual ~ThreadPoolInterface() {}
-    }
-    
-    
-    {}  // namespace leveldb
-    
-    TEST(CorruptionTest, TableFile) {
-  Build(100);
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
-  dbi->TEST_CompactMemTable();
-  dbi->TEST_CompactRange(0, nullptr, nullptr);
-  dbi->TEST_CompactRange(1, nullptr, nullptr);
-    }
-    
-    // Options that control read operations
-struct LEVELDB_EXPORT ReadOptions {
-  ReadOptions() = default;
-    }
-    
-    #endif  // STORAGE_LEVELDB_INCLUDE_WRITE_BATCH_H_
-
-    
-      // Finish building the block and return a slice that refers to the
-  // block contents.  The returned slice will remain valid for the
-  // lifetime of this builder or until Reset() is called.
-  Slice Finish();
-    
-    
-    {    AutomationProperties::SetName(m_host, announcement->Announcement);
-    AutomationPeer ^ peer = FrameworkElementAutomationPeer::FromElement(m_host);
-    if (peer != nullptr)
+        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0,
+                     const typename internal::VecTraits<T>::vec64 & v_src1,
+                     typename internal::VecTraits<T>::vec64 & v_dst) const
     {
-        peer->RaiseAutomationEvent(AutomationEvents::LiveRegionChanged);
+        typename internal::VecTraits<T>::vec64 v_min = internal::vmin(v_src0, v_src1);
+        typename internal::VecTraits<T>::vec64 v_max = internal::vmax(v_src0, v_src1);
+        v_dst = internal::vqsub(v_max, v_min);
     }
-}
+    
+        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0,
+                     const typename internal::VecTraits<T>::vec64 & v_src1,
+                     typename internal::VecTraits<T>::vec64 & v_dst) const
+    {
+        v_dst = internal::vadd(v_src0, v_src1);
+    }
+    
+    
+    {    }
+    
+    
+    {
+    {} // namespace internal
+} // namespace CAROTENE_NS
 
     
-    NarratorAnnouncement ^ CalculatorAnnouncement::GetUpdateCurrencyRatesAnnouncement(String ^ announcement)
+     *Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+    
+    void fillMinMaxLocs(const Size2D & size,
+                    const u32 * srcBase, ptrdiff_t srcStride,
+                    u32 minVal, size_t * minLocPtr, s32 & minLocCount, s32 minLocCapacity,
+                    u32 maxVal, size_t * maxLocPtr, s32 & maxLocCount, s32 maxLocCapacity)
 {
-    return ref new NarratorAnnouncement(
-        announcement,
-        CalculatorActivityIds::UpdateCurrencyRates,
-        AutomationNotificationKind::ActionCompleted,
-        AutomationNotificationProcessing::ImportantMostRecent);
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+    }
+    
+    #ifdef CAROTENE_NEON
+    
+    
+    {
+    {        for (; j < size.width; ++j)
+            sqsum[j] = (prev += src[j]*src[j]) + prevSqSum[j];
+    }
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)sqsumBase;
+    (void)sqsumStride;
+#endif
 }
     
-    namespace CalculatorApp::Common::Automation
-{
-    // These enum types are copied from the types available in
-    // Windows::UI::Xaml::Automation::Peers in the RS3 SDK.
-    // When this app switches to min version RS3, these custom
-    // enums should be removed and the Windows types should be used
-    // instead.
-    // TODO - MSFT 12735088
-public
-    enum class AutomationNotificationKind
+    
     {
-        ItemAdded = 0,
-        ItemRemoved = 1,
-        ActionCompleted = 2,
-        ActionAborted = 3,
-        Other = 4
-    };
+    {
+    {				if (PathFileExists(szArgList[i]))
+				{
+					cmderOptions.cmderStart = szArgList[i];
+					i++;
+				}
+				else
+				{
+					MessageBox(NULL, L'Unrecognized parameter.\n\nValid options:\n\n    /c [CMDER User Root Path]\n\n    /task [ConEmu Task Name]\n\n    [/start [Start in Path] | [Start in Path]]\n\n    /single\n\n    /m\n\n    /x [ConEmu extra arguments]\n\nor\n\n    /register [USER | ALL]\n\nor\n\n    /unregister [USER | ALL]\n', MB_TITLE, MB_OK);
+					cmderOptions.error = true;
+				}
+			}
+			else
+			{
+				MessageBox(NULL, L'Unrecognized parameter.\n\nValid options:\n\n    /c [CMDER User Root Path]\n\n    /task [ConEmu Task Name]\n\n    [/start [Start in Path] | [Start in Path]]\n\n    /single\n\n    /m\n\n    /x [ConEmu extra arguments]\n\nor\n\n    /register [USER | ALL]\n\nor\n\n    /unregister [USER | ALL]\n', MB_TITLE, MB_OK);
+				cmderOptions.error = true;
+			}
+		}
+	}
+    
+    /* NurbsSampling */
+#define GLU_OBJECT_PARAMETRIC_ERROR        100208
+#define GLU_OBJECT_PARAMETRIC_ERROR_EXT    100208
+#define GLU_OBJECT_PATH_LENGTH             100209
+#define GLU_OBJECT_PATH_LENGTH_EXT         100209
+#define GLU_PATH_LENGTH                    100215
+#define GLU_PARAMETRIC_ERROR               100216
+#define GLU_DOMAIN_DISTANCE                100217
+    
+    private:
+    
+      // Return a list of links.
+  Links *getLinks(Catalog *catalog);
+    
+    #include 'goo/gtypes.h'
+#include 'goo/GooList.h'
+#include 'goo/GooString.h'
+#include 'Object.h'
+    
+    #include 'XRef.h'
+    
+    //------------------------------------------------------------------------
+// ProfileData
+//------------------------------------------------------------------------
+    
+      Sound *copy();
+    
+    static void splashOutBlendMultiply(SplashColorPtr src, SplashColorPtr dest,
+				   SplashColorPtr blend, SplashColorMode cm) {
+  int i;
     }
+    
+    
+    {  obj.initNull();
+  CachedFile *cachedFile = new CachedFile(new StdinCacheLoader(), NULL);
+  return new PDFDoc(new CachedFileStream(cachedFile, 0, gFalse,
+                                         cachedFile->getLength(), &obj),
+                    ownerPassword, userPassword);
+}
