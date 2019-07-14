fb83841e0064622eb8923145a594622cccf85737
SUNsung <weatherius@gmail.com>
2019-07-14 12:22:07 +0300

    Rewrite [21jOFRzSmrwdA21MYcaGEvJmls21dr2lL4dZNY21p.a.cpp] File in: 2019-07-14 12:22:07

--- cccccc/21jOFRzSmrwdA21MYcaGEvJmls21dr2lL4dZNY21p.a.cpp
+++ cccccc/21jOFRzSmrwdA21MYcaGEvJmls21dr2lL4dZNY21p.a.cpp
@@ -1,161 +1,83 @@
 
         
-        #include <google/protobuf/stubs/logging.h>
-#include <google/protobuf/stubs/common.h>
+        #if defined(OS_MACOSX) || defined(OS_LINUX)
+  void ShowAboutPanel();
+  void SetAboutPanelOptions(const base::DictionaryValue& options);
+#endif
     
-    void MapLiteTestUtil::ExpectMapFieldsSetInitialized(
-    const unittest::TestMapLite& message) {
-  MapTestUtilImpl::ExpectMapFieldsSetInitialized<unittest::MapEnumLite,
-                                                 unittest::MAP_ENUM_FOO_LITE>(
-      message);
-}
-    
-    #include <google/protobuf/stubs/bytestream.h>
-    
-    TEST(TemplateUtilTest, TestIdentity) {
-  EXPECT_TRUE(
-      (type_equals_<GOOGLE_NAMESPACE::identity_<int>::type, int>::value));
-  EXPECT_TRUE(
-      (type_equals_<GOOGLE_NAMESPACE::identity_<void>::type, void>::value));
-}
-    
-        for (size_t i = 0; i < set_fields.size(); i++) {
-      const FieldDescriptor* field = set_fields[i];
-      if (ShouldBeClear(field)) {
-        reflection->ClearField(message, field);
-        continue;
-      }
-      if (field->type() == FieldDescriptor::TYPE_MESSAGE) {
-        if (field->is_repeated()) {
-          for (int j = 0; j < reflection->FieldSize(*message, field); j++) {
-            StripMessage(reflection->MutableRepeatedMessage(message, field, j));
-          }
-        } else {
-          StripMessage(reflection->MutableMessage(message, field));
-        }
-      }
-    }
-    
-    int main(int argc, char *argv[]) {
-  if (argc % 2 == 0 || argc == 1) {
-    std::cerr << 'Usage: [input_files] [output_file_names] where ' <<
-        'input_files are one to one mapping to output_file_names.' <<
-        std::endl;
-    return 1;
+    // Read a message from a socket fd, with an optional timeout.
+// If |timeout| <= 0 then read immediately.
+// Return number of bytes actually read, or -1 on error.
+ssize_t ReadFromSocket(int fd,
+                       char* buf,
+                       size_t bufsize,
+                       const base::TimeDelta& timeout) {
+  if (timeout > base::TimeDelta()) {
+    int rv = WaitSocketForRead(fd, timeout);
+    if (rv <= 0)
+      return rv;
   }
     }
     
-      EnumGenerator(const EnumGenerator&) = delete;
-  EnumGenerator& operator=(const EnumGenerator&) = delete;
     
-    
-    {	LRESULT lr = SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)'Environment', SMTO_ABORTIFHUNG | SMTO_NOTIMEOUTIFNOTHUNG, 5000, NULL);
-	lr = SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)L'Environment', SMTO_ABORTIFHUNG | SMTO_NOTIMEOUTIFNOTHUNG, 5000, NULL); // For Windows >= 8
-}
-    
-    
-    {
-    {}}
-    
-    Array Directory::getMetaData() {
-  return make_map_array(
-    s_wrapper_type, s_plainfile, // PHP5 compatibility
-    s_stream_type,  s_dir,
-    s_mode,         s_r,
-    s_unread_bytes, 0,
-    s_seekable,     false,
-    s_timed_out,    false,
-    s_blocked,      true,
-    s_eof,          isEof()
-  );
-}
-    
-    struct Directory;
-    
-    
-    {///////////////////////////////////////////////////////////////////////////////
-}
-    
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////
+namespace nwapi {
     }
     
-    /**
- * url based files.
- */
-struct UrlFile : MemFile {
-  DECLARE_RESOURCE_ALLOCATION(UrlFile);
-    }
+    #include 'content/nw/src/api/menu/menu.h'
     
-    extern const osqueryConstants g_osquery_constants;
-    
-    /**
- * @brief Queries SMART devices on the system by autodetection and explicit
- * storage controller arguments.
- *
- * @param client libsmartctl client
- * @param walk_func function that walks the system devices and runs the handler
- * function on each device
- * @param results reference to QueryData to store results in
- */
-void querySmartDevices(
-    libsmartctl::ClientInterface& client,
-    std::function<void(
-        std::function<void(const std::string&, hardwareDriver*)>)> walk_func,
-    QueryData& results);
-    
-    
-    {
-    {
-    {  return results;
-}
-} // namespace tables
-} // namespace osquery
-
-    
-    #include <osquery/tests/integration/tables/helper.h>
-    
-    TEST_F(KernelInfo, test_sanity) {
-  QueryData data = execute_query('select * from kernel_info');
-  ValidatatioMap row_map = {{'version', NonEmptyString},
-                            {'arguments', NormalType},
-                            {'path', NormalType},
-                            {'device', NonEmptyString}};
-  validate_rows(data, row_map);
-}
-    
-    TEST_F(kernelPanics, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from kernel_panics');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'path', NormalType}
-  //      {'time', NormalType}
-  //      {'registers', NormalType}
-  //      {'frame_backtrace', NormalType}
-  //      {'module_backtrace', NormalType}
-  //      {'dependencies', NormalType}
-  //      {'name', NormalType}
-  //      {'os_version', NormalType}
-  //      {'kernel_version', NormalType}
-  //      {'system_model', NormalType}
-  //      {'uptime', IntType}
-  //      {'last_loaded', NormalType}
-  //      {'last_unloaded', NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
-}
-    
-    class keychainItems : public testing::Test {
- protected:
-  void SetUp() override {
-    setUpEnvironment();
-  }
+    static KeyMap keymap = {
+  {'`'    , 'Backquote'},
+  {'\\'   , 'Backslash'},
+  {'['    , 'BracketLeft'},
+  {']'    , 'BracketRight'},
+  {','    , 'Comma'},
+  {'='    , 'Equal'},
+  {'-'    , 'Minus'},
+  {'.'    , 'Period'},
+  {'''    , 'Quote'},
+  {';'    , 'Semicolon'},
+  {'/'    , 'Slash'},
+  {'\n'   , 'Enter'},
+  {'\t'   , 'Tab'},
+  {'UP'   , 'ArrowUp'},
+  {'DOWN' , 'ArrowDown'},
+  {'LEFT' , 'ArrowLeft'},
+  {'RIGHT', 'ArrowRight'},
+  {'ESC'  , 'Escape'},
+  {'MEDIANEXTTRACK', 'MediaTrackNext'},
+  {'MEDIAPREVTRACK', 'MediaTrackPrevious'}
 };
+    
+    
+    {#endif
+}  // namespace nwapi
+    
+    #include 'chrome/browser/devtools/devtools_window.h'
+#include 'chrome/browser/extensions/devtools_util.h'
+#include 'chrome/browser/extensions/extension_service.h'
+#include 'content/nw/src/api/menuitem/menuitem.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/public/browser/render_view_host.h'
+#include 'content/public/browser/web_contents.h'
+#include 'extensions/browser/extension_system.h'
+#include 'extensions/common/error_utils.h'
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.allocateId', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwObjAllocateIdFunction);
+};
+    
+      private:
+    int GetPrimaryMonitorIndex();
+    // DesktopMediaListObserver implementation.
+    void OnSourceAdded(DesktopMediaList* list, int index) override;
+    void OnSourceRemoved(DesktopMediaList* list, int index) override;
+    void OnSourceMoved(DesktopMediaList* list, int old_index, int new_index) override;
+    void OnSourceNameChanged(DesktopMediaList* list, int index) override;
+    void OnSourceThumbnailChanged(DesktopMediaList* list, int index) override;
+    
+    	if (cfgRoot.length() != 0)
+	{
+		swprintf_s(args, L'%s  -loadcfgfile \'%s\'', args, userConEmuCfgPath);
+	}
