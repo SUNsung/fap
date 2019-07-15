6867d6e520b0f415589d99f3670be403157c617b
SUNsung <weatherius@gmail.com>
2019-07-15 15:24:49 +0300

    Rewrite [21kQH68umkcKM21iFTQVud299Q21QtMQIjl2iso21kPr.cpp] File in: 2019-07-15 15:24:48

--- cccccc/21kQH68umkcKM21iFTQVud299Q21QtMQIjl2iso21kPr.cpp
+++ cccccc/21kQH68umkcKM21iFTQVud299Q21QtMQIjl2iso21kPr.cpp
@@ -1,261 +1,70 @@
 
         
-          // Wrap TrackableObject into a class that SupportsUserData.
-  void AttachAsUserData(base::SupportsUserData* wrapped);
+          if (argc > 1) {
+    napi_throw_error(env, NULL,
+                     'Wrong number of arguments, expected single argument');
+  }
     
-    #include 'atom/browser/atom_quota_permission_context.h'
-    
-      std::wstring out;
-  out.push_back(L''');
-  for (size_t i = 0; i < arg.size(); ++i) {
-    if (arg[i] == '\\') {
-      // Find the extent of this run of backslashes.
-      size_t start = i, end = start + 1;
-      for (; end < arg.size() && arg[end] == '\\'; ++end) {
-      }
-      size_t backslash_count = end - start;
-    }
+    // static
+void CertificateManagerModel::DidGetCertDBOnUIThread(
+    net::NSSCertDatabase* cert_db,
+    bool is_user_db_available,
+    const CreationCallback& callback) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
     }
     
-      aura::Window* root_window = view->GetRootWindow();
-  if (!root_window || !aura::client::GetDragDropClient(root_window))
-    return;
-    
-    
-    {  RenderThread::Get()->Send(new ShellViewHostMsg_Allocate_Object(
-      routing_id,
-      object_id,
-      type,
-      *static_cast<base::DictionaryValue*>(value_option.get())));
-  return v8::Undefined(isolate);
-}
-    
-    void MenuItem::SetIcon(const std::string& icon) {
-  if (icon.empty()) {
-    gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menu_item_), NULL); 
-  } else {
-    gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menu_item_),
-                                  gtk_image_new_from_file(icon.c_str()));
-    gtk_image_menu_item_set_always_show_image(GTK_IMAGE_MENU_ITEM(menu_item_),
-                                              TRUE);
-  }
-}
-    
-    NwAppQuitFunction::~NwAppQuitFunction() {
-}
-    
-    namespace extensions {
+    // Arguments is a wrapper around v8::FunctionCallbackInfo that integrates
+// with Converter to make it easier to marshall arguments and return values
+// between V8 and C++.
+class Arguments {
+ public:
+  Arguments();
+  explicit Arguments(const v8::FunctionCallbackInfo<v8::Value>& info);
+  ~Arguments();
     }
     
-    
-    {  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
-  manager->OnDeallocateObject(id);
-  return true;
-}
-    
-    
-    {    private:
-      DISALLOW_COPY_AND_ASSIGN(NwScreenGetScreensFunction);      
-  };
-    
-    int32 remainder(int32 x, int32 modulus);
-int32 TriangulatePolygon(float32* xv, float32* yv, int32 vNum, b2Triangle* results);
-bool IsEar(int32 i, float32* xv, float32* yv, int32 xvLength); //Not for external use
-int32 PolygonizeTriangles(b2Triangle* triangulated, int32 triangulatedLength, b2Polygon* polys, int32 polysLength);
-int32 DecomposeConvex(b2Polygon* p, b2Polygon* results, int32 maxPolys);
-//void DecomposeConvexAndAddTo(b2Polygon* p, b2Body* bd, b2FixtureDef* prototype);
-    
-    #include <string.h>
-    
-    
-    {  friend inline bool operator== (const IntPoint& a, const IntPoint& b)
-  {
-    return a.X == b.X && a.Y == b.Y;
-  }
-  friend inline bool operator!= (const IntPoint& a, const IntPoint& b)
-  {
-    return a.X != b.X  || a.Y != b.Y; 
-  }
-};
-//------------------------------------------------------------------------------
-    
-      Compression level can be specified in parameter level. At the moment,
-  only level 1 and level 2 are supported.
-  Level 1 is the fastest compression and generally useful for short data.
-  Level 2 is slightly slower but it gives better compression ratio.
-    
-    #endif /* ARCH_H */
-
-    
-    #define silk_LIMIT( a, limit1, limit2)      ((limit1) > (limit2) ? ((a) > (limit1) ? (limit1) : ((a) < (limit2) ? (limit2) : (a))) \
-                                                                 : ((a) > (limit2) ? (limit2) : ((a) < (limit1) ? (limit1) : (a))))
-    
-    #ifndef SILK_MACROS_ARMv4_H
-#define SILK_MACROS_ARMv4_H
-    
-      static bool isInitialized6() { return data6_.initialized; }
-    
-    
-    {  static const std::string R;
+    template <>
+struct Converter<PersistentDictionary> {
+  static bool FromV8(v8::Isolate* isolate,
+                     v8::Local<v8::Value> val,
+                     PersistentDictionary* out);
 };
     
-      void getClosestKNodes(std::vector<std::shared_ptr<DHTNode>>& nodes,
-                        const unsigned char* key) const;
-    
-      virtual void startup() = 0;
-    
-    DHTTaskExecutor::~DHTTaskExecutor() = default;
-    
-    #endif // D_DHT_TASK_EXECUTOR_H
-
-    
-        std::vector<AddrEntry>::iterator find(const std::string& addr);
-    
-            virtual void update();
-    
-    namespace op
-{
-    struct PersonEntry
-    {
-        long long counterLastDetection;
-        std::vector<cv::Point2f> keypoints;
-        std::vector<char> status;
-        /*
-        PersonEntry(long long _last_frame, 
-                    std::vector<cv::Point2f> _keypoints,
-                    std::vector<char> _active):
-                    last_frame(_last_frame), keypoints(_keypoints),
-                    active(_active)
-                    {}
-        */
-    };
-    class OP_API PersonIdExtractor
-    {
-    }
+      static bool FromV8(v8::Isolate* isolate,
+                     v8::Local<v8::Value> val,
+                     ScopedPersistent<T>* out) {
+    v8::Local<T> converted;
+    if (!Converter<v8::Local<T>>::FromV8(isolate, val, &converted))
+      return false;
     }
     
-        template<typename T>
-    Point<T>& Point<T>::operator+=(const Point<T>& point)
-    {
-        try
-        {
-            x += point.x;
-            y += point.y;
-            // Return
-            return *this;
-        }
-        catch (const std::exception& e)
-        {
-            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
-            return *this;
-        }
-    }
+    // Implemented features:
+//  [X] Renderer: User texture binding. Use 'ALLEGRO_BITMAP*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+//  [X] Platform: Clipboard support (from Allegro 5.1.12)
+//  [X] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'.
+// Issues:
+//  [ ] Renderer: The renderer is suboptimal as we need to unindex our buffers and convert vertices manually.
+//  [ ] Platform: Missing gamepad support.
     
-    static void TransposeBlock(Complex data[kBlockSize]) {
-  for (int i = 0; i < kBlockEdge; i++) {
-    for (int j = 0; j < i; j++) {
-      std::swap(data[kBlockEdge * i + j], data[kBlockEdge * j + i]);
-    }
-  }
-}
+    //---- Include imgui_user.h at the end of imgui.h as a convenience
+//#define IMGUI_INCLUDE_IMGUI_USER_H
     
-    #endif  // GUETZLI_ENTROPY_ENCODE_H_
-
+        // Create the D3DDevice
+    ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
+    g_d3dpp.Windowed = TRUE;
+    g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+    g_d3dpp.EnableAutoDepthStencil = TRUE;
+    g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+    g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;           // Present with vsync
+    //g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;   // Present without vsync, maximum unthrottled framerate
+    if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
+        return false;
     
-      // performs: {a,b} <- {a-b, a+b}, without saturation
-#define BUTTERFLY(a, b) do {   \
-  SUB((a), (b));               \
-  ADD((b), (b));               \
-  ADD((b), (a));               \
-} while (0)
+            cmdList->CopyTextureRegion(&dstLocation, 0, 0, 0, &srcLocation, NULL);
+        cmdList->ResourceBarrier(1, &barrier);
     
-      void JumpToByteBoundary() {
-    while (put_bits <= 56) {
-      int c = (put_buffer >> 56) & 0xff;
-      EmitByte(c);
-      put_buffer <<= 8;
-      put_bits += 8;
-    }
-    if (put_bits < 64) {
-      int padmask = 0xff >> (64 - put_bits);
-      int c = ((put_buffer >> 56) & ~padmask) | padmask;
-      EmitByte(c);
-    }
-    put_buffer = 0;
-    put_bits = 64;
-  }
-    
-    void SaveQuantTables(const int q[3][kDCTBlockSize], JPEGData* jpg) {
-  const size_t kTableSize = kDCTBlockSize * sizeof(q[0][0]);
-  jpg->quant.clear();
-  int num_tables = 0;
-  for (size_t i = 0; i < jpg->components.size(); ++i) {
-    JPEGComponent* comp = &jpg->components[i];
-    // Check if we have this quant table already.
-    bool found = false;
-    for (int j = 0; j < num_tables; ++j) {
-      if (memcmp(&q[i][0], &jpg->quant[j].values[0], kTableSize) == 0) {
-        comp->quant_idx = j;
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      JPEGQuantTable table;
-      memcpy(&table.values[0], &q[i][0], kTableSize);
-      table.precision = 0;
-      for (int k = 0; k < kDCTBlockSize; ++k) {
-        assert(table.values[k] >= 0);
-        assert(table.values[k] < (1 << 16));
-        if (table.values[k] > 0xff) {
-          table.precision = 1;
-        }
-      }
-      table.index = num_tables;
-      comp->quant_idx = num_tables;
-      jpg->quant.push_back(table);
-      ++num_tables;
-    }
-  }
-}
-    
-    #include 'guetzli/jpeg_error.h'
-    
-    // Reads the Define Quantization Table (DQT) marker segment and fills in *jpg
-// with the parsed data.
-bool ProcessDQT(const uint8_t* data, const size_t len, size_t* pos,
-                JPEGData* jpg) {
-  const size_t start_pos = *pos;
-  VERIFY_LEN(2);
-  size_t marker_len = ReadUint16(data, pos);
-  if (marker_len == 2) {
-    fprintf(stderr, 'DQT marker: no quantization table found\n');
-    jpg->error = JPEG_EMPTY_DQT;
-    return false;
-  }
-  while (*pos < start_pos + marker_len && jpg->quant.size() < kMaxQuantTables) {
-    VERIFY_LEN(1);
-    int quant_table_index = ReadUint8(data, pos);
-    int quant_table_precision = quant_table_index >> 4;
-    quant_table_index &= 0xf;
-    VERIFY_INPUT(quant_table_index, 0, 3, QUANT_TBL_INDEX);
-    VERIFY_LEN((quant_table_precision ? 2 : 1) * kDCTBlockSize);
-    JPEGQuantTable table;
-    table.index = quant_table_index;
-    table.precision = quant_table_precision;
-    for (int i = 0; i < kDCTBlockSize; ++i) {
-      int quant_val = quant_table_precision ?
-          ReadUint16(data, pos) :
-          ReadUint8(data, pos);
-      VERIFY_INPUT(quant_val, 1, 65535, QUANT_VAL);
-      table.values[kJPEGNaturalOrder[i]] = quant_val;
-    }
-    table.is_last = (*pos == start_pos + marker_len);
-    jpg->quant.push_back(table);
-  }
-  VERIFY_MARKER_END();
-  return true;
-}
-    
-    
-    {}  // namespace
+    // Implemented features:
+//  [X] Renderer: Support for large meshes (64k+ vertices) with 16-bits indices.
+// Missing features:
+//  [ ] Renderer: User texture binding. Changes of ImTextureID aren't supported by this binding! See https://github.com/ocornut/imgui/pull/914
