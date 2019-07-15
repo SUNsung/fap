0c679ea60c852525c965f99a4e4b684c2298d150
SUNsung <weatherius@gmail.com>
2019-07-15 05:34:45 +0300

    Rewrite [03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb] File in: 2019-07-15 05:34:44

--- ruby/03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb
+++ ruby/03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb
@@ -1,110 +1,51 @@
 
         
-        if pathutil_relative == native_relative
-  Benchmark.ips do |x|
-    x.report('pathutil') { pathutil_relative }
-    x.report('native')   { native_relative }
-    x.compare!
-  end
-else
-  print 'PATHUTIL: '
-  puts pathutil_relative
-  print 'NATIVE:   '
-  puts native_relative
-end
-
+        module AbstractController
+  module Testing
+    class ControllerWithHelpers < AbstractController::Base
+      include AbstractController::Helpers
+      include AbstractController::Rendering
+      include ActionView::Rendering
     
-    module Jekyll
-  module Commands
-    class NewTheme < Jekyll::Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:'new-theme') do |c|
-            c.syntax 'new-theme NAME'
-            c.description 'Creates a new Jekyll theme scaffold'
-            c.option 'code_of_conduct', \
-                     '-c', '--code-of-conduct', \
-                     'Include a Code of Conduct. (defaults to false)'
+          # Try to add the gems to the current gemfile and lock file, if successful
+      # both of them will be updated. This injector is similar to Bundler's own injector class
+      # minus the support for additionals source and doing local resolution only.
+      ::Bundler::LogstashInjector.inject!(pack)
     
-          def grouped_array(groups)
-        groups.each_with_object([]) do |item, array|
-          array << {
-            'name'  => item.first,
-            'items' => item.last,
-            'size'  => item.last.size,
-          }
-        end
+    class LogStash::PluginManager::Update < LogStash::PluginManager::Command
+  REJECTED_OPTIONS = [:path, :git, :github]
+  # These are local gems used by LS and needs to be filtered out of other plugin gems
+  NON_PLUGIN_LOCAL_GEMS = ['logstash-core', 'logstash-core-plugin-api']
+    
+          @@order_attributes = [
+        :id, :number, :item_total, :total, :ship_total, :state, :adjustment_total,
+        :user_id, :created_at, :updated_at, :completed_at, :payment_total,
+        :shipment_state, :payment_state, :email, :special_instructions, :channel,
+        :included_tax_total, :additional_tax_total, :display_included_tax_total,
+        :display_additional_tax_total, :tax_total, :currency, :considered_risky,
+        :canceler_id
+      ]
+    
+    ## -- Rsync Deploy config -- ##
+# Be sure your public key is listed in your server's ~/.ssh/authorized_keys file
+ssh_user       = 'user@domain.com'
+ssh_port       = '22'
+document_root  = '~/website.com/'
+rsync_delete   = false
+rsync_args     = ''  # Any extra arguments to pass to rsync
+deploy_default = 'rsync'
+    
+        def render(context)
+      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
+        gist, file = parts[1].strip, parts[2].strip
+      else
+        gist, file = @text.strip, ''
+      end
+      if gist.empty?
+        ''
+      else
+        script_url = script_url_for gist, file
+        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
+        html_output_for script_url, code
       end
     end
-  end
-end
-
-    
-        if processes.stdout.lines.any? { |line| line =~ %r{^\d+\t\d\tcom.apple.SafariNotificationAgent$} }
-      system_command '/usr/bin/killall', args: ['-kill', 'SafariNotificationAgent']
-    end
-    
-          # Returns an array of all the when branches in the `case` statement.
-      #
-      # @return [Array<WhenNode>] an array of `when` nodes
-      def when_branches
-        node_parts[1...-1]
-      end
-    
-          # The name of the defined method as a symbol.
-      #
-      # @return [Symbol] the name of the defined method
-      def method_name
-        node_parts[2]
-      end
-    
-          dir = if File.directory?(file_or_dir)
-              file_or_dir
-            else
-              File.dirname(file_or_dir)
-            end
-      @path_cache[dir] ||= ConfigLoader.configuration_file_for(dir)
-      path = @path_cache[dir]
-      @object_cache[path] ||= begin
-                                print 'For #{dir}: ' if ConfigLoader.debug?
-                                ConfigLoader.configuration_from_file(path)
-                              end
-    end
-  end
-end
-
-    
-      config.vm.define :smartos do |smartos|
-    smartos.vm.box = 'smartos-base1310-64-virtualbox-20130806.box'
-    smartos.vm.box_url = 'http://dlc-int.openindiana.org/aszeszo/vagrant/smartos-base1310-64-virtualbox-20130806.box'
-  end
-    
-    class FPM::Package::NPM < FPM::Package
-  class << self
-    include FPM::Util
-  end
-  # Flags '--foo' will be accessable  as attributes[:npm_foo]
-  option '--bin', 'NPM_EXECUTABLE',
-    'The path to the npm executable you wish to run.', :default => 'npm'
-    
-        ::Dir.mkdir(File.join(builddir, 'manifests'))
-    manifests.each do |manifest|
-      dir = File.join(builddir, 'manifests', File.dirname(manifest))
-      logger.info('manifests targeting: #{dir}')
-      ::Dir.mkdir(dir) if !File.directory?(dir)
-    
-            # dependency name prefixing is optional, if enabled, a name 'foo' will
-        # become 'python-foo' (depending on what the python_package_name_prefix
-        # is)
-        name = fix_name(name) if attributes[:python_fix_dependencies?]
-    
-        # Make one file. The installscript can unpack itself.
-    `cat #{install_script} #{payload} > #{output_path}`
-    FileUtils.chmod('+x', output_path)
-  end
-    
-      if FPM::Issues::TarWriter.has_issues_with_add_symlink?
-    # Backport Symlink Support to TarWriter
-    # https://github.com/rubygems/rubygems/blob/4a778c9c2489745e37bcc2d0a8f12c601a9c517f/lib/rubygems/package/tar_writer.rb#L239-L253
-    def add_symlink(name, target, mode)
-      check_closed
