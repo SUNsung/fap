58d827d71b2ec52ccf72ed250bdeff953f8d390c
SUNsung <weatherius@gmail.com>
2019-07-16 02:25:28 +0300

    Rewrite [00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp] File in: 2019-07-16 02:25:28

--- cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
+++ cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
@@ -1,314 +1,258 @@
 
         
-        // hidden_ops should be a list of Op names that should get a leading _
-// in the output. Prints the output to stdout.
-// Optional fourth argument is the name of the original C++ source file
-// where the ops' REGISTER_OP() calls reside.
-void PrintPythonOps(const OpList& ops, const ApiDefMap& api_defs,
-                    const std::vector<string>& hidden_ops, bool require_shapes,
-                    const string& source_file_name = '');
+        int hdf5_load_int(hid_t loc_id, const string& dataset_name);
+void hdf5_save_int(hid_t loc_id, const string& dataset_name, int i);
+string hdf5_load_string(hid_t loc_id, const string& dataset_name);
+void hdf5_save_string(hid_t loc_id, const string& dataset_name,
+                      const string& s);
     
-    #include <iomanip>
-#include 'tensorflow/core/framework/op.h'
-#include 'tensorflow/core/framework/tensor_shape.pb.h'
-#include 'tensorflow/core/grappler/costs/graph_properties.h'
-#include 'tensorflow/core/grappler/grappler_item.h'
+        // Every two iterations we are reading the second file,
+    // which has the same labels, but data is offset by total data size,
+    // which is 2400 (see generate_sample_data).
+    int file_offset = (iter % 4 < 2) ? 0 : 2400;
     
-      void Compute(OpKernelContext* context) override {
-    // Output a scalar string.
-    Tensor* output_tensor = nullptr;
-    OP_REQUIRES_OK(context,
-                   context->allocate_output(0, TensorShape(), &output_tensor));
-    auto output = output_tensor->scalar<string>();
-    }
     
-    void NPyBfloat16_CopySwap(void* dst, void* src, int swap, void* arr) {
-  if (!src) {
-    return;
+    {  // Since boost 1.53 boost.python will convert str and bytes
+  // to std::string but will convert std::string to str. Here we
+  // force a bytes object to be returned. When this object
+  // is passed back to the NCCL constructor boost.python will
+  // correctly convert the bytes to std::string automatically
+  PyObject* py_uid = PyBytes_FromString(uid.c_str());
+  return bp::object(bp::handle<>(py_uid));
+#else
+  // automatic conversion is correct for python 2.
+  return bp::object(uid);
+#endif
+}
+#endif
+    
+    SolverAction::Enum SignalHandler::CheckForSignals() const {
+  if (GotSIGHUP()) {
+    return SIGHUP_action_;
   }
-  memcpy(dst, src, sizeof(uint16_t));
-  if (swap) {
-    ByteSwap16(dst);
+  if (GotSIGINT()) {
+    return SIGINT_action_;
   }
+  return SolverAction::NONE;
 }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#ifndef TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
+      N = std::min<int>(labels_.size(), N);
+  std::vector<int> maxN = Argmax(output, N);
+  std::vector<Prediction> predictions;
+  for (int i = 0; i < N; ++i) {
+    int idx = maxN[i];
+    predictions.push_back(std::make_pair(labels_[idx], output[idx]));
+  }
     
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-      // Creates a 2d FFT plan with scratch allocator.
-  virtual std::unique_ptr<Plan> Create2dPlanWithScratchAllocator(
-      Stream *stream, uint64 num_x, uint64 num_y, Type type, bool in_place_fft,
-      ScratchAllocator *scratch_allocator) = 0;
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    
-    {    struct KeypointStore {
-        virtual void push(f32 kpX, f32 kpY, f32 kpSize, f32 kpAngle=-1, f32 kpResponse=0, s32 kpOctave=0, s32 kpClass_id=-1) = 0;
-        virtual ~KeypointStore() {};
-    };
-}
-    
-        void operator() (const uint8x16_t & v_src0, const uint8x16_t & v_src1,
-                     uint8x16_t & v_dst) const
-    {
-        uint16x8_t v_src0_p = vmovl_u8(vget_low_u8(v_src0));
-        uint16x8_t v_src1_p = vmovl_u8(vget_low_u8(v_src1));
-        float32x4_t v_dst0f = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_low_u16(v_src1_p))), v_beta),
-                                        v_alpha, vcvtq_f32_u32(vmovl_u16(vget_low_u16(v_src0_p))));
-        float32x4_t v_dst1f = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_high_u16(v_src1_p))), v_beta),
-                                        v_alpha, vcvtq_f32_u32(vmovl_u16(vget_high_u16(v_src0_p))));
-        uint16x8_t v_dst0 = vcombine_u16(vmovn_u32(vcvtq_u32_f32(v_dst0f)),
-                                         vmovn_u32(vcvtq_u32_f32(v_dst1f)));
+      Datum datum;
+  datum.set_channels(1);
+  datum.set_height(rows);
+  datum.set_width(cols);
+  LOG(INFO) << 'A total of ' << num_items << ' items.';
+  LOG(INFO) << 'Rows: ' << rows << ' Cols: ' << cols;
+  for (int item_id = 0; item_id < num_items; ++item_id) {
+    image_file.read(pixels, rows * cols);
+    label_file.read(&label, 1);
+    datum.set_data(pixels, rows*cols);
+    datum.set_label(label);
+    string key_str = caffe::format_int(item_id, 8);
+    datum.SerializeToString(&value);
     }
     
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const u8* src = internal::getRowPtr(srcBase, srcStride, i);
-        u8* dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t j = 0;
-    }
-    
-        inline void operator()(const u8* src, s16* dstx, s16* dsty, ptrdiff_t width)
-    {
-        uint8x8_t l = vtbl1_u8(vld1_u8(src - lookLeft), vfmask);
-        ptrdiff_t i = 0;
-        for (; i < width - 8 + lookRight; i += 8)
-        {
-            internal::prefetch(src + i);
-            uint8x8_t l18u = vld1_u8(src + i + 1);
-    }
-    }
-    
-    ptrdiff_t borderInterpolate(ptrdiff_t _p, size_t _len, BORDER_MODE borderType, size_t startMargin, size_t endMargin)
-{
-    ptrdiff_t p = _p + (ptrdiff_t)startMargin;
-    size_t len = _len + startMargin + endMargin;
-    if( (size_t)p < len )
-        return _p;
-    else if( borderType == BORDER_MODE_REPLICATE )
-        p = p < 0 ? 0 : (ptrdiff_t)len - 1;
-    else if( borderType == BORDER_MODE_REFLECT || borderType == BORDER_MODE_REFLECT101 )
-    {
-        s32 delta = borderType == BORDER_MODE_REFLECT101;
-        if( len == 1 )
-            return 0;
-        do
-        {
-            if( p < 0 )
-                p = -p - 1 + delta;
-            else
-                p = (ptrdiff_t)len - 1 - (p - (ptrdiff_t)len) - delta;
-        }
-        while( (size_t)p >= len );
-    }
-    else if( borderType == BORDER_MODE_WRAP )
-    {
-        if( p < 0 )
-            p -= ((p-(ptrdiff_t)len+1)/(ptrdiff_t)len)*(ptrdiff_t)len;
-        if( p >= (ptrdiff_t)len )
-            p %= (ptrdiff_t)len;
-    }
-    else if( borderType == BORDER_MODE_CONSTANT )
-        p = -1;
-    else
-        internal::assertSupportedConfiguration(false);
-    return p - (ptrdiff_t)startMargin;
-}
-    
-                uint32x4_t vlx1 = vmvnq_u32(vequ1);
-    
-    f64 dotProduct(const Size2D &_size,
-               const s8 * src0Base, ptrdiff_t src0Stride,
-               const s8 * src1Base, ptrdiff_t src1Stride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    Size2D size(_size);
-    if (src0Stride == src1Stride &&
-        src0Stride == (ptrdiff_t)(size.width))
-    {
-        size.width *= size.height;
-        size.height = 1;
-    }
+    /**
+ * @brief A wrapper around SyncedMemory holders serving as the basic
+ *        computational unit through which Layer%s, Net%s, and Solver%s
+ *        interact.
+ *
+ * TODO(dox): more thorough description.
+ */
+template <typename Dtype>
+class Blob {
+ public:
+  Blob()
+       : data_(), diff_(), count_(0), capacity_(0) {}
     }
     
     
-    {    minLocCount >>= 1;
-    maxLocCount >>= 1;
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)minVal;
-    (void)minLocPtr;
-    (void)minLocCount;
-    (void)minLocCapacity;
-    (void)maxVal;
-    (void)maxLocPtr;
-    (void)maxLocCount;
-    (void)maxLocCapacity;
-#endif
-}
-    
-    
-    {            vec128 v_src = vld1q(src + js);
-            vec128 v_dst = vrev64q(v_src);
-            v_dst = vcombine(vget_high(v_dst), vget_low(v_dst));
-            vst1q(dst + jd - step_base, v_dst);
-        }
-        for (; js < roiw_tail; js += step_tail, jd -= step_tail)
-        {
-            vec64 v_src = vld1(src + js);
-            vst1(dst + jd - step_tail, vrev64(v_src));
-        }
-    
-    
-    {
-    {
-    {
-    {                vst1_s16(dst + x, ls);
-            }
-            break;
-        }
-        for (s32 h = 0; h < cn; ++h)
-        {
-            s32* ln = lane + h;
-            s16* dt = dst + h;
-            for (size_t k = x; k < colsn; k += cn)
-            {
-                dt[k] = (s16)((ln[k-2*cn] + ln[k+2*cn] + 4*(ln[k-cn] + ln[k+cn]) + 6*ln[k] + (1<<7))>>8);
-            }
-        }
-    }
-#else
-    (void)srcBase;
-    (void)srcStride;
-    (void)dstBase;
-    (void)dstStride;
-    (void)borderValue;
-    (void)borderMargin;
-#endif
-}
-    
-    inline uint8x8_t vqtbl1_u8 (uint8x16_t a, uint8x8_t b)
-{
-#ifdef __aarch64__
-    // AArch64 supports this natively
-    return ::vqtbl1_u8(a, b);
-#else
-    union { uint8x16_t v; uint8x8x2_t w; } u = { a };
-    return vtbl2_u8(u.w, b);
-#endif
-}
-    
-        // output changed array
-    std::cout << object << '\n';
-    
-        // out_of_range.403
-    try
-    {
-        // try to use a JSON pointer to an nonexistent object key
-        json::const_reference ref = j.at('/foo'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-    
-        if (!q.value.HasMember('interval')) {
-      query.interval = FLAGS_schedule_default_interval;
+    {  inline bst_int GetConstraint(bst_uint featureid) const {
+    if (featureid < params_.monotone_constraints.size()) {
+      return params_.monotone_constraints[featureid];
     } else {
-      query.interval = JSON::valueToSize(q.value['interval']);
+      return 0;
+    }
+  }
+};
+    
+    
+    {
+    {void SparsePageWriter::Alloc(std::shared_ptr<SparsePage>* out_page) {
+  CHECK(*out_page == nullptr);
+  if (num_free_buffer_ != 0) {
+    out_page->reset(new SparsePage());
+    --num_free_buffer_;
+  } else {
+    CHECK(qrecycle_.Pop(out_page));
+  }
+}
+}  // namespace data
+}  // namespace xgboost
+    
+    TEST(SparsePageDMatrix, ColAccess) {
+  dmlc::TemporaryDirectory tempdir;
+  const std::string tmp_file = tempdir.path + '/simple.libsvm';
+  CreateSimpleTestData(tmp_file);
+  xgboost::DMatrix * dmat = xgboost::DMatrix::Load(
+    tmp_file + '#' + tmp_file + '.cache', true, false);
     }
     
     
-    {  rf.registry('config_parser')->remove('placebo');
-}
-    
-    
-    {  EXPECT_EQ(pack_count, 1U);
-  c.reset();
-}
-    
-    std::map<std::string, std::string> getTestConfigMap(const std::string& file) {
-  std::string content;
-  auto const filepath = getTestConfigDirectory() / file;
-  auto status = readFile(filepath, content);
-  EXPECT_TRUE(status.ok())
-      << 'Could not read file: ' << boost::io::quoted(filepath.string())
-      << ', because: ' << status.what();
-  std::map<std::string, std::string> config;
-  config['awesome'] = content;
-  return config;
-}
-    
-    
-    {} // namespace osquery
+    {
+    {
+    {
+    {    LOG(CONSOLE) << '======== Monitor: ' << label << ' ========';
+    for (auto &kv : statistics_map) {
+      if (kv.second.count == 0) {
+        LOG(WARNING) <<
+            'Timer for ' << kv.first << ' did not get stopped properly.';
+        continue;
+      }
+      LOG(CONSOLE) << kv.first << ': ' << kv.second.timer.ElapsedSeconds()
+                   << 's, ' << kv.second.count << ' calls @ '
+                   << std::chrono::duration_cast<std::chrono::microseconds>(
+                          kv.second.timer.elapsed / kv.second.count)
+                          .count()
+                   << 'us';
+    }
+    self_timer.Stop();
+  }
+  void Init(std::string label) { this->label = label; }
+  void Start(const std::string &name) {
+    if (ConsoleLogger::ShouldLog(ConsoleLogger::LV::kDebug)) {
+      statistics_map[name].timer.Start();
+    }
+  }
+  void Stop(const std::string &name) {
+    if (ConsoleLogger::ShouldLog(ConsoleLogger::LV::kDebug)) {
+      auto &stats = statistics_map[name];
+      stats.timer.Stop();
+      stats.count++;
+    }
+  }
+  void StartCuda(const std::string &name) {
+    if (ConsoleLogger::ShouldLog(ConsoleLogger::LV::kDebug)) {
+      auto &stats = statistics_map[name];
+      stats.timer.Start();
+#if defined(XGBOOST_USE_NVTX) && defined(__CUDACC__)
+      stats.nvtx_id = nvtxRangeStartA(name.c_str());
+#endif  // defined(XGBOOST_USE_NVTX) && defined(__CUDACC__)
+    }
+  }
+  void StopCuda(const std::string &name) {
+    if (ConsoleLogger::ShouldLog(ConsoleLogger::LV::kDebug)) {
+      auto &stats = statistics_map[name];
+      stats.timer.Stop();
+      stats.count++;
+#if defined(XGBOOST_USE_NVTX) && defined(__CUDACC__)
+      nvtxRangeEnd(stats.nvtx_id);
+#endif  // defined(XGBOOST_USE_NVTX) && defined(__CUDACC__)
+    }
+  }
+};
+}  // namespace common
+}  // namespace xgboost
 
     
-    template <typename T>
-Expected<T, DatabaseError> InMemoryDatabase::getValue(const std::string& domain,
-                                                      const std::string& key) {
-  debug_only::verifyTrue(is_open_, 'database is not open');
-  if (!is_open_) {
-    return createError(DatabaseError::DbIsNotOpen) << 'Database is closed';
+    /*! \brief training parameter for regression */
+struct LearnerModelParam : public dmlc::Parameter<LearnerModelParam> {
+  /* \brief global bias */
+  bst_float base_score;
+  /* \brief number of features  */
+  unsigned num_feature;
+  /* \brief number of classes, if it is multi-class classification  */
+  int num_class;
+  /*! \brief Model contain additional properties */
+  int contain_extra_attrs;
+  /*! \brief Model contain eval metrics */
+  int contain_eval_metrics;
+  /*! \brief reserved field */
+  int reserved[29];
+  /*! \brief constructor */
+  LearnerModelParam() {
+    std::memset(this, 0, sizeof(LearnerModelParam));
+    base_score = 0.5f;
   }
-  auto storage_iter = storage_.find(domain);
-  if (storage_iter == storage_.end()) {
-    return domainNotFoundError(domain);
+  // declare parameters
+  DMLC_DECLARE_PARAMETER(LearnerModelParam) {
+    DMLC_DECLARE_FIELD(base_score)
+        .set_default(0.5f)
+        .describe('Global bias of the model.');
+    DMLC_DECLARE_FIELD(num_feature)
+        .set_default(0)
+        .describe(
+            'Number of features in training data,'
+            ' this parameter will be automatically detected by learner.');
+    DMLC_DECLARE_FIELD(num_class).set_default(0).set_lower_bound(0).describe(
+        'Number of class option for multi-class classifier. '
+        ' By default equals 0 and corresponds to binary classifier.');
   }
-  std::lock_guard<std::mutex> lock(storage_iter->second->getMutex());
-  auto result = storage_iter->second->get(key);
-  if (result) {
-    DataType value = result.take();
-    if (value.type() == typeid(T)) {
-      return boost::get<T>(value);
-    } else {
-      auto error = createError(DatabaseError::KeyNotFound)
-                   << 'Requested wrong type for: ' << domain << ':' << key
-                   << ' stored type: ' << value.type().name()
-                   << ' requested type '
-                   << boost::core::demangle(typeid(T).name());
-      LOG(ERROR) << error.getMessage();
-      debug_only::fail(error.getMessage().c_str());
-      return std::move(error);
+};
+    
+    class CallbackManager
+{
+public:
+    inline void append(swCallback cb, void *private_data)
+    {
+        list_.push_back(new Callback(cb, private_data));
     }
-  }
-  return result.takeError();
-}
+    inline void prepend(swCallback cb, void *private_data)
+    {
+        list_.push_front(new Callback(cb, private_data));
+    }
+    inline void execute()
+    {
+        while (!list_.empty())
+        {
+            Callback *task = list_.front();
+            list_.pop_front();
+            task->callback(task->private_data);
+            delete task;
+        }
+    }
+protected:
+    std::list<Callback *> list_;
+};
     
-      // This method bypass type validation and will silently update value
-  // even if type was changed (e.g int->string)
-  ExpectedSuccess<DatabaseError> putStringsUnsafe(
-      const std::string& domain,
-      const std::vector<std::pair<std::string, std::string>>& data) override;
+    static inline void coro_test(std::initializer_list<std::pair<coroutine_func_t, void*>> args)
+{
+    int complete_num = 0;
+    }
     
-    ExpectedSuccess<DatabaseError> RocksdbDatabase::putRawBytes(
-    const std::string& domain,
-    const std::string& key,
-    const std::string& value) {
-  auto handle = getHandle(domain);
-  if (handle) {
-    std::shared_ptr<Handle> handle_ptr = handle.take();
-    return putRawBytesInternal(handle_ptr.get(), key, value);
-  }
-  return handle.takeError();
-}
+            for (int i = 0; i < 5; ++i)
+        {
+            std::string addr2 = System::gethostbyname('www.baidu.com', AF_INET);
+            ASSERT_NE(addr2, '');
+        }
+    
+        //udp ipv4
+    if (req->info.type == SW_EVENT_UDP)
+    {
+        inet_ntop(AF_INET6, &packet->info.addr.inet_v4.sin_addr, address, sizeof(address));
+        data = packet->data;
+        length = packet->length;
+        port = ntohs(packet->info.addr.inet_v4.sin_port);
+    }
+    //udp ipv6
+    else if (req->info.type == SW_EVENT_UDP6)
+    {
+        inet_ntop(AF_INET6, &packet->info.addr.inet_v6.sin6_addr, address, sizeof(address));
+        data = packet->data;
+        length = packet->length;
+        port = ntohs(packet->info.addr.inet_v6.sin6_port);
+    }
+    //unix dgram
+    else if (req->info.type == SW_EVENT_UNIX_DGRAM)
+    {
+        strcpy(address, packet->info.addr.un.sun_path);
+        data = packet->data;
+        length = packet->length;
+    }
