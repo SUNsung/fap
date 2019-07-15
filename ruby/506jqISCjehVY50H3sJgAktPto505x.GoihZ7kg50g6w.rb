61e796de6aca73e634bde5c71b7d424524d30482
SUNsung <weatherius@gmail.com>
2019-07-15 21:15:50 +0300

    Rewrite [506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb] File in: 2019-07-15 21:15:50

--- ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
+++ ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
@@ -1,124 +1,50 @@
 
         
-            def order_by_category_sql(dir)
-      -'CASE WHEN categories.id = #{SiteSetting.uncategorized_category_id.to_i} THEN '' ELSE categories.name END #{dir}'
+        if dblib = with_config('dbm-type', nil)
+  dblib = dblib.split(/[ ,]+/)
+else
+  dblib = %w(libc db db2 db1 db6 db5 db4 db3 gdbm_compat gdbm qdbm)
+end
+    
+    create_makefile('digest/rmd160')
+
+    
+    $defs << '-DHAVE_CONFIG_H'
+    
+        def finish # :nodoc:
+      @sha2.digest!
     end
+    private :finish
     
-    describe ContentSecurityPolicy::Builder do
-  let(:builder) { described_class.new }
-    
-        # remove old drafts
-    delete_drafts_older_than_n_days = SiteSetting.delete_drafts_older_than_n_days.days.ago
-    Draft.where('updated_at < ?', delete_drafts_older_than_n_days).destroy_all
+      # Sets the last +Error+ of the current executing +Thread+ to +error+
+  def self.last_error= error
+    Thread.current[:__DL2_LAST_ERROR__] = error
+    Thread.current[:__FIDDLE_LAST_ERROR__] = error
   end
     
-        def script_assets(base = base_url, s3_cdn = GlobalSetting.s3_cdn_url, cdn = GlobalSetting.cdn_url)
-      SCRIPT_ASSET_DIRECTORIES.map do |dir, can_use_s3_cdn, can_use_cdn|
-        if can_use_s3_cdn && s3_cdn
-          s3_cdn + dir
-        elsif can_use_cdn && cdn
-          cdn + dir
+        def split_arguments(arguments, sep=',')
+      return [] if arguments.strip == 'void'
+      arguments.scan(/([\w\*\s]+\(\*\w*\)\(.*?\)|[\w\*\s\[\]]+)(?:#{sep}\s*|$)/).collect {|m| m[0]}
+    end
+    
+        def parse_bind_options(opts)
+      h = {}
+      while( opt = opts.shift() )
+        case opt
+        when :stdcall, :cdecl
+          h[:call_type] = opt
+        when :carried, :temp, :temporal, :bind
+          h[:callback_type] = opt
+          h[:carrier] = opts.shift()
         else
-          base + dir
+          h[opt] = true
         end
       end
+      h
     end
+    private :parse_bind_options
     
-        langs.sort
-  end
-    
-        Extension.theme_extensions(theme_ids).each { |extension| builder << extension }
-    Extension.plugin_extensions.each { |extension| builder << extension }
-    builder << Extension.site_setting_extension
-    
-        def site_setting_extension
-      { script_src: SiteSetting.content_security_policy_script_src.split('|') }
+        def to_s() # :nodoc:
+      super(@size)
     end
-    
-      def show
-    raise Discourse::NotFound unless last_modified.present?
-    return render body: nil, status: 304 if not_modified?
-    
-      def update_time_read!
-    UserStat.update_time_read!(id)
-  end
-    
-          @env[USER_API_KEY_ENV] = true
-    end
-    
-          def cache_key_iid
-        if object.respond_to?(:noteable_id)
-          object.noteable_id
-        elsif object.respond_to?(:iid)
-          object.iid
-        else
-          raise(
-            TypeError,
-            'Instances of #{object.class} are not supported'
-          )
-        end
-      end
-    end
-  end
-end
-
-    
-          def action_for_grape(env)
-        endpoint = env[ENDPOINT_KEY]
-        route = endpoint.route rescue nil
-    
-        it 'works for queued jobs' do
-      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
-    end
-  end
-    
-    end
-
-    
-        it 'strips punctuation' do
-      expect(AgentsExporter.new(:name => 'foo,bar').filename).to eq('foo-bar.json')
-    end
-    
-          expect(@scheduler.scheduler_agent_jobs.map(&:scheduler_agent)).to eq([@agent1])
-    end
-  end
-end
-
-    
-        it 'should revert extract and template options for an updated WebsiteAgent' do
-      expect(agent.options).to include('extract' => new_extract,
-                                       'template' => new_template)
-      ConvertWebsiteAgentTemplateForMerge.new.down
-      agent.reload
-      expect(agent.options).to include('extract' => reverted_extract,
-                                       'template' => reverted_template)
-    end
-  end
-end
-
-    
-            @user.send_confirmation_instructions
-      end
-    
-      def maxheight_or_default
-    params[:maxheight].present? ? params[:maxheight].to_i : nil
-  end
-end
-
-    
-          if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: provider_id.capitalize) if is_navigational_format?
-      else
-        session['devise.#{provider}_data'] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
-      end
-    end
-  end
-    
-      def set_locale
-    I18n.locale = default_locale
-    I18n.locale = current_user.locale if user_signed_in?
-  rescue I18n::InvalidLocale
-    I18n.locale = default_locale
   end
