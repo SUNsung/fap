b8f572774f94c345b6395a7337c534b04a7a9b7d
SUNsung <weatherius@gmail.com>
2019-07-15 07:34:30 +0300

    Rewrite [37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb] File in: 2019-07-15 07:34:29

--- ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
+++ ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
@@ -1,13 +1,82 @@
 
         
-              delegate_to Type
+              it 'yields command output' do
+        expect_command('ls', '-la', exitstatus: 1, output: 'Heeeelp! Something went wrong.')
+        Fastlane::Actions.sh('ls', '-la') do |status, result|
+          expect(status.exitstatus).to eq(1)
+          expect(result).to eq('Heeeelp! Something went wrong.')
+        end
+      end
     
-        def execute
-      raise PluginManager::FileNotFoundError, 'Can't file local file #{local_file}' unless ::File.exist?(local_file)
-      raise PluginManager::InvalidPackError, 'Invalid format, the pack must be in zip format' unless valid_format?(local_file)
+        # [Boolean] Set if the variable is sensitive, such as a password or API token, to prevent echoing when prompted for the parameter
+    # If a default value exists, it won't be used during code generation as default values can read from environment variables.
+    attr_accessor :sensitive
     
-    shared_examples 'logstash update' do |logstash|
-  describe 'logstash-plugin update on #{logstash.hostname}' do
-    before :each do
-      logstash.install({:version => LOGSTASH_VERSION})
+          it 'should not be fooled by 10 local code signing identities available' do
+        allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
+        allow(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return('     10 valid identities found\n')
+        expect(FastlaneCore::UI).not_to(receive(:error))
+    
+    exec_arr = ['fastlane', tool_name] + ARGV
+    
+            it 'sets up screenshots folder in current folder' do
+          expect(options[:screenshots_path]).to eq('./screenshots')
+        end
+    
+      describe :split_keywords do
+    context 'only commas' do
+      let(:keywords) { 'One,Two, Three, Four Token,' }
+    
+          def self.authors
+        # So no one will ever forget your contribution to fastlane :) You are awesome btw!
+        ['Your GitHub/Twitter Name']
+      end
+    
+            Redis::Cache.with do |redis|
+          redis.multi do |m|
+            m.sadd(key, value)
+            m.expire(key, timeout)
+          end
+        end
+      end
+    
+    module Gitlab
+  module GithubImport
+    module Representation
+      class User
+        include ToHash
+        include ExposeAttribute
+    
+          def action_for_grape(env)
+        endpoint = env[ENDPOINT_KEY]
+        route = endpoint.route rescue nil
+    
+      def test_imaginary
+    assert_equal [],
+                 scan('imaginary', '')
+    assert_equal ['1i', '10ri', '10.0i', '10.1ri'],
+                 scan('imaginary', 'm(1i,10ri,10.0i,10.1ri)')
+  end
+    
+        a = %w(a a)
+    b = a.uniq {|v| v }
+    assert_equal(%w(a a), a)
+    assert(a.none?(&:frozen?))
+    assert_equal(%w(a), b)
+    assert(b.none?(&:frozen?))
+  end
+    
+    untrace_var :$x
+    
+    # Get bundled gems on load path
+Dir.glob('#{src_testdir}/../gems/*/*.gemspec')
+  .reject {|f| f =~ /minitest|test-unit|power_assert/ }
+  .map {|f| $LOAD_PATH.unshift File.join(File.dirname(f), 'lib') }
+    
+          t1 = Tempfile.new('expected'); t1.puts d1; t1.close
+      t2 = Tempfile.new('actual'); t2.puts d2; t2.close
+      system('diff -u #{t1.path} #{t2.path}') # use diff if available
+      exit(1)
     end
+    i2
+  end
