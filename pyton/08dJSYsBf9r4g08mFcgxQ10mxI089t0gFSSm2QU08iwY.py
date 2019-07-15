d7e43f5a3537c91b0ec6a9d8ba1e7548ae6aa4e9
SUNsung <weatherius@gmail.com>
2019-07-16 02:57:50 +0300

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2019-07-16 02:57:49

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,292 +1,100 @@
 
         
-        
-parser = youtube_dl.parseOpts()[0]
-build_completion(parser)
-
-    
-    try:
-    input = raw_input
-except NameError:
-    pass
-    
-        infile, outfile = args
-    
-        with io.open(infile, encoding='utf-8') as inf:
-        issue_template_tmpl = inf.read()
-    
-        outfile, = args
-    
-        fileopts = []
-    for opt in opts_file:
-        if opt._short_opts:
-            fileopts.extend(opt._short_opts)
-        if opt._long_opts:
-            fileopts.extend(opt._long_opts)
+            # we only want to be able to train generation for the combined model
+    discriminator.trainable = False
+    fake, aux = discriminator(fake)
+    combined = Model([latent, image_class], [fake, aux])
     
     
-from test.helper import FakeYDL
-from youtube_dl.cache import Cache
+def gradient_ascent(x, iterations, step, max_loss=None):
+    for i in range(iterations):
+        loss_value, grad_values = eval_loss_and_grads(x)
+        if max_loss is not None and loss_value > max_loss:
+            break
+        print('..Loss value at', i, ':', loss_value)
+        x += step * grad_values
+    return x
     
-        auth.login()
-    response = client.get('/')
-    assert b'test title' in response.data
-    assert b'by test on 2018-01-01' in response.data
-    assert b'test\nbody' in response.data
-    assert b'href='/1/update'' in response.data
+        # Note on using statefulness in RNNs
+        You can set RNN layers to be 'stateful', which means that the states
+        computed for the samples in one batch will be reused as initial states
+        for the samples in the next batch. This assumes a one-to-one mapping
+        between samples in different successive batches.
     
-    # Available datasets
-_DATASETS = {
-    'cityscapes_fine_instanceonly_seg_train': {
-        _IM_DIR:
-            _DATA_DIR + '/cityscapes/images',
-        _ANN_FN:
-            _DATA_DIR + '/cityscapes/annotations/instancesonly_gtFine_train.json',
-        _RAW_DIR:
-            _DATA_DIR + '/cityscapes/raw'
-    },
-    'cityscapes_fine_instanceonly_seg_val': {
-        _IM_DIR:
-            _DATA_DIR + '/cityscapes/images',
-        # use filtered validation as there is an issue converting contours
-        _ANN_FN:
-            _DATA_DIR + '/cityscapes/annotations/instancesonly_filtered_gtFine_val.json',
-        _RAW_DIR:
-            _DATA_DIR + '/cityscapes/raw'
-    },
-    'cityscapes_fine_instanceonly_seg_test': {
-        _IM_DIR:
-            _DATA_DIR + '/cityscapes/images',
-        _ANN_FN:
-            _DATA_DIR + '/cityscapes/annotations/instancesonly_gtFine_test.json',
-        _RAW_DIR:
-            _DATA_DIR + '/cityscapes/raw'
-    },
-    'coco_2014_train': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_train2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_train2014.json'
-    },
-    'coco_2014_val': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_val2014.json'
-    },
-    'coco_2014_minival': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_minival2014.json'
-    },
-    'coco_2014_valminusminival': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_valminusminival2014.json'
-    },
-    'coco_2015_test': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test2015.json'
-    },
-    'coco_2015_test-dev': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test-dev2015.json'
-    },
-    'coco_2017_test': {  # 2017 test uses 2015 test images
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test2017.json',
-        _IM_PREFIX:
-            'COCO_test2015_'
-    },
-    'coco_2017_test-dev': {  # 2017 test-dev uses 2015 test images
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test-dev2017.json',
-        _IM_PREFIX:
-            'COCO_test2015_'
-    },
-    'coco_stuff_train': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_train2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/coco_stuff_train.json'
-    },
-    'coco_stuff_val': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/coco_stuff_val.json'
-    },
-    'keypoints_coco_2014_train': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_train2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_train2014.json'
-    },
-    'keypoints_coco_2014_val': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_val2014.json'
-    },
-    'keypoints_coco_2014_minival': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_minival2014.json'
-    },
-    'keypoints_coco_2014_valminusminival': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_valminusminival2014.json'
-    },
-    'keypoints_coco_2015_test': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test2015.json'
-    },
-    'keypoints_coco_2015_test-dev': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test-dev2015.json'
-    },
-    'voc_2007_train': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2007/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2007/annotations/voc_2007_train.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2007/VOCdevkit2007'
-    },
-    'voc_2007_val': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2007/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2007/annotations/voc_2007_val.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2007/VOCdevkit2007'
-    },
-    'voc_2007_test': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2007/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2007/annotations/voc_2007_test.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2007/VOCdevkit2007'
-    },
-    'voc_2012_train': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2012/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2012/annotations/voc_2012_train.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2012/VOCdevkit2012'
-    },
-    'voc_2012_val': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2012/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2012/annotations/voc_2012_val.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2012/VOCdevkit2012'
+    
+def set_epsilon(e):
+    '''Sets the value of the fuzz factor used in numeric expressions.
+    
+        def on_train_end(self, logs=None):
+        '''Called at the end of training.
+    
+        def compute_mask(self, inputs, mask=None):
+        if not self.mask_zero:
+            return None
+        output_mask = K.not_equal(inputs, 0)
+        return output_mask
+    
+        # Arguments
+        model: target model for the conversion.
+    '''
+    # Note: SeparableConvolution not included
+    # since only supported by TF.
+    conv_classes = {
+        'Conv1D',
+        'Conv2D',
+        'Conv3D',
+        'Conv2DTranspose',
     }
-}
+    to_assign = []
+    for layer in model.layers:
+        if layer.__class__.__name__ in conv_classes:
+            original_kernel = K.get_value(layer.kernel)
+            converted_kernel = convert_kernel(original_kernel)
+            to_assign.append((layer.kernel, converted_kernel))
+    K.batch_set_value(to_assign)
     
-        # Histogram of ground-truth objects
-    gt_hist = np.zeros((len(classes)), dtype=np.int)
-    for entry in roidb:
-        gt_inds = np.where(
-            (entry['gt_classes'] > 0) & (entry['is_crowd'] == 0))[0]
-        gt_classes = entry['gt_classes'][gt_inds]
-        gt_hist += np.histogram(gt_classes, bins=hist_bins)[0]
-    logger.debug('Ground-truth class histogram:')
-    for i, v in enumerate(gt_hist):
-        logger.debug(
-            '{:d}{:s}: {:d}'.format(
-                i, classes[i].rjust(char_len), v))
-    logger.debug('-' * char_len)
-    logger.debug(
-        '{:s}: {:d}'.format(
-            'total'.rjust(char_len), np.sum(gt_hist)))
-
+            with self.tasks():
+            run_deletion(deletion.id)
     
-    ... -> RoI ----\
-                -> RoIFeatureXform -> keypoint head -> keypoint output -> loss
-... -> Feature /
-       Map
+            if request.method != 'POST':
+            return render_to_response('sentry/auth-link-identity.html', request=request, context={
+                'organization': organization,
+                'provider': integration.get_provider(),
+            })
     
     
-def add_single_gpu_param_update_ops(model, gpu_id):
-    # Learning rate of 0 is a dummy value to be set properly at the
-    # start of training
-    lr = model.param_init_net.ConstantFill(
-        [], 'lr', shape=[1], value=0.0
-    )
-    one = model.param_init_net.ConstantFill(
-        [], 'one', shape=[1], value=1.0
-    )
-    wd = model.param_init_net.ConstantFill(
-        [], 'wd', shape=[1], value=cfg.SOLVER.WEIGHT_DECAY
-    )
-    # weight decay of GroupNorm's parameters
-    wd_gn = model.param_init_net.ConstantFill(
-        [], 'wd_gn', shape=[1], value=cfg.SOLVER.WEIGHT_DECAY_GN
-    )
-    for param in model.TrainableParams(gpu_id=gpu_id):
-        logger.debug('param ' + str(param) + ' will be updated')
-        param_grad = model.param_to_grad[param]
-        # Initialize momentum vector
-        param_momentum = model.param_init_net.ConstantFill(
-            [param], param + '_momentum', value=0.0
-        )
-        if param in model.biases:
-            # Special treatment for biases (mainly to match historical impl.
-            # details):
-            # (1) Do not apply weight decay
-            # (2) Use a 2x higher learning rate
-            model.Scale(param_grad, param_grad, scale=2.0)
-        elif param in model.gn_params:
-            # Special treatment for GroupNorm's parameters
-            model.WeightedSum([param_grad, one, param, wd_gn], param_grad)
-        elif cfg.SOLVER.WEIGHT_DECAY > 0:
-            # Apply weight decay to non-bias weights
-            model.WeightedSum([param_grad, one, param, wd], param_grad)
-        # Update param_grad and param_momentum in place
-        model.net.MomentumSGDUpdate(
-            [param_grad, param_momentum, lr, param],
-            [param_grad, param_momentum, param],
-            momentum=cfg.SOLVER.MOMENTUM
-        )
-
+class DebugErrorPageEmbedView(View):
+    def _get_project_key(self):
+        return ProjectKey.objects.filter(
+            project=settings.SENTRY_PROJECT,
+        )[0]
     
+            # Adding model 'TagValue'
+        db.create_table(u'tagstore_tagvalue', (
+            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
+            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
+            ('environment_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)),
+            ('_key', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['tagstore.TagKey'], db_column='key')),
+            ('value', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('data', self.gf('sentry.db.models.fields.gzippeddict.GzippedDictField')(null=True, blank=True)),
+            ('times_seen', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
+            ('last_seen', self.gf('django.db.models.fields.DateTimeField')(
+                null=True, db_index=True)),
+            ('first_seen', self.gf('django.db.models.fields.DateTimeField')(
+                null=True, db_index=True)),
+        ))
+        db.send_create_signal('tagstore', ['TagValue'])
     
-def add_keypoint_rcnn_blobs(
-    blobs, roidb, fg_rois_per_image, fg_inds, im_scale, batch_idx
-):
-    '''Add Mask R-CNN keypoint specific blobs to the given blobs dictionary.'''
-    # Note: gt_inds must match how they're computed in
-    # datasets.json_dataset._merge_proposal_boxes_into_roidb
-    gt_inds = np.where(roidb['gt_classes'] > 0)[0]
-    max_overlaps = roidb['max_overlaps']
-    gt_keypoints = roidb['gt_keypoints']
+            # Removing unique constraint on 'TagValue', fields ['project_id', '_key', 'value']
+        db.delete_unique(u'tagstore_tagvalue', ['project_id', 'key_id', 'value'])
     
-        for i in range(masks.shape[0]):
-        cls = int(mask_class_labels[i])
-        start = M**2 * cls
-        end = start + M**2
-        # Ignore background instance
-        # (only happens when there is no fg samples in an image)
-        if cls > 0:
-            mask_targets[i, start:end] = masks[i, :]
+            # Changing field 'EventTag.group_id'
+        db.alter_column(u'tagstore_eventtag', 'group_id', self.gf(
+            'sentry.db.models.fields.bounded.BoundedPositiveIntegerField')())
+    
+    from django.db import models, router, connections
+from django.utils import timezone
+    
+    from sentry.utils.safe import safe_execute
+from sentry.tasks.base import instrumented_task
