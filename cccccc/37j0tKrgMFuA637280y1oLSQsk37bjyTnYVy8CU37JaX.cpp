d08ece210b86058b01b3a411fe3acdc3c5c7e41d
SUNsung <weatherius@gmail.com>
2019-07-16 00:26:04 +0300

    Rewrite [37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp] File in: 2019-07-16 00:26:04

--- cccccc/37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp
+++ cccccc/37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp
@@ -1,86 +1,312 @@
 
         
-        ::opencensus::stats::MeasureInt64 RpcServerSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcServerReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerServerLatency();
-::opencensus::stats::MeasureInt64 RpcServerCompletedRpcs();
+        #include <QIcon>
+#include <QPixmap>
+#include <QString>
     
-    
-    {    uint8_t version = buf[kVersionIdOffset];
-    uint32_t fieldID = buf[kServerElapsedTimeOffset];
-    if (version != kVersionId || fieldID != kServerElapsedTimeField) {
-      *time = 0;
-      return kEncodeDecodeFailure;
+        ~reverse_lock() {
+        templock.lock();
+        templock.swap(lock);
     }
-    *time = absl::little_endian::Load64(
-        &buf[kServerElapsedTimeOffset + kFieldIdSize]);
-    return kRpcServerStatsSize;
-  }
     
-      void StartTransportStreamOpBatch(grpc_call_element* elem,
-                                   TransportStreamOpBatch* op) override;
-    
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  host_cpu_load_info_data_t cpuinfo;
-  mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
-  if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO,
-                      (host_info_t)&cpuinfo, &count) == KERN_SUCCESS) {
-    for (int i = 0; i < CPU_STATE_MAX; i++) total += cpuinfo.cpu_ticks[i];
-    busy = total - cpuinfo.cpu_ticks[CPU_STATE_IDLE];
-  }
-  return std::make_pair(busy, total);
+    int UniValue::get_int() const
+{
+    if (typ != VNUM)
+        throw std::runtime_error('JSON value is not an integer as expected');
+    int32_t retval;
+    if (!ParseInt32(getValStr(), &retval))
+        throw std::runtime_error('JSON integer out of range');
+    return retval;
 }
     
-    #include <windows.h>
-#include <cstdint>
+    /** A hasher class for RIPEMD-160. */
+class CRIPEMD160
+{
+private:
+    uint32_t s[5];
+    unsigned char buf[64];
+    uint64_t bytes;
+    }
     
-    #include <grpc/support/log.h>
+        add	rsp, STACK_SIZE
     
-    #include 'util/logging.h'
+    #endif  // STORAGE_LEVELDB_DB_DBFORMAT_H_
+
     
-        // ReadPhysicalRecord may have only had an empty trailer remaining in its
-    // internal buffer. Calculate the offset of the next physical record now
-    // that it has returned, properly accounting for its header size.
-    uint64_t physical_record_offset =
-        end_of_buffer_offset_ - buffer_.size() - kHeaderSize - fragment.size();
+    TEST(FormatTest, InternalKeyShortestSuccessor) {
+  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
+            ShortSuccessor(IKey('foo', 100, kTypeValue)));
+  ASSERT_EQ(IKey('\xff\xff', 100, kTypeValue),
+            ShortSuccessor(IKey('\xff\xff', 100, kTypeValue)));
+}
     
-    WriteBatch::Handler::~Handler() = default;
+    Status TestWritableFile::SyncParent() {
+  Status s = SyncDir(GetDirName(state_.filename_));
+  if (s.ok()) {
+    env_->DirWasSynced();
+  }
+  return s;
+}
     
-    // Read the block identified by 'handle' from 'file'.  On failure
-// return non-OK.  On success fill *result and return OK.
-Status ReadBlock(RandomAccessFile* file, const ReadOptions& options,
-                 const BlockHandle& handle, BlockContents* result);
+    std::string TempFileName(const std::string& dbname, uint64_t number) {
+  assert(number > 0);
+  return MakeFileName(dbname, number, 'dbtmp');
+}
     
-    class CorruptionTest {
- public:
-  CorruptionTest()
-      : db_(nullptr),
-        dbname_('/memenv/corruption_test'),
-        tiny_cache_(NewLRUCache(100)) {
-    options_.env = &env_;
-    options_.block_cache = tiny_cache_;
-    DestroyDB(dbname_, options_);
+        {
+      std::unique_ptr<io::ZeroCopyOutputStream> output(
+          generator_context->Open(*basename + '.pb.cc'));
+      io::Printer printer(output.get(), '$', nullptr);
+      printer.Print('\n');
+    }
+    
+      if (child_stdin_ != -1) {
+    // Child did not finish reading input before it closed the output.
+    // Presumably it exited with an error.
+    close(child_stdin_);
+    child_stdin_ = -1;
+  }
+    
+      // If return value is negative it's an error
+  PROTOBUF_MUST_USE_RESULT int PushLimit(const char* ptr, int limit) {
+    GOOGLE_DCHECK(limit >= 0);
+    limit += static_cast<int>(ptr - buffer_end_);
+    limit_end_ = buffer_end_ + (std::min)(0, limit);
+    auto old_limit = limit_;
+    limit_ = limit;
+    return old_limit - limit;
+  }
+    
+    void MessageDifferencer::StreamReporter::ReportUnknownFieldIgnored(
+    const Message& message1, const Message& message2,
+    const std::vector<SpecificField>& field_path) {
+  printer_->Print('ignored: ');
+  PrintPath(field_path, true, message1);
+  if (CheckPathChanged(field_path)) {
+    printer_->Print(' -> ');
+    PrintPath(field_path, false, message2);
+  }
+  printer_->Print('\n');  // Print for newlines.
+}
+    
+        case FieldDescriptorProto::TYPE_UINT32:
+    case FieldDescriptorProto::TYPE_UINT64:
+    case FieldDescriptorProto::TYPE_FIXED32:
+    case FieldDescriptorProto::TYPE_FIXED64: {
+      uint64 max_value = kuint64max;
+      if (field->type() == FieldDescriptorProto::TYPE_UINT32 ||
+          field->type() == FieldDescriptorProto::TYPE_FIXED32) {
+        max_value = kuint32max;
+      }
+    }
+    
+          const Message& message_string_string = mf_string_string.Get(i);
+      std::string key_string_string =
+          message_string_string.GetReflection()->GetString(
+              message_string_string, fd_map_string_string_key);
+      std::string value_string_string =
+          message_string_string.GetReflection()->GetString(
+              message_string_string, fd_map_string_string_value);
+      EXPECT_EQ(value_string_string, StrFunc(Int(key_string_string), 5));
+    
+    protected:
+	static void _bind_methods();
+    
+    
+    {	BSP_Tree();
+	BSP_Tree(const Variant &p_variant);
+	BSP_Tree(const PoolVector<Face3> &p_faces, real_t p_error_radius = 0);
+	BSP_Tree(const Vector<Node> &p_nodes, const Vector<Plane> &p_planes, const AABB &p_aabb, real_t p_error_radius = 0);
+	~BSP_Tree();
+};
+    
+    
+    {
+    {		OS::get_singleton()->delay_usec(1000);
+	}
+}
+    
+    void OpCheck::Init(const std::vector<mxnet::NDArray> &inputs_,
+                   const std::vector<mxnet::NDArray> &outputs_) {
+  auto ctx = inputs_[0].ctx();
+  CHECK(!MKLDNNStream::Get()->HasOps());
+  for (size_t i = 0; i < inputs_.size(); i++) {
+    NDArray data = inputs_[i];
+    inputs.emplace_back(data.shape(), ctx, false, data.dtype());
+    if (data.IsMKLDNNData() && data.IsView())
+        data = data.Reorder2Default();
+    auto mem = data.GetMKLDNNData();
+    inputs[i].CopyFrom(*mem);
+  }
+  for (size_t i = 0; i < outputs_.size(); i++) {
+    outputs.emplace_back(outputs_[i].shape(), ctx,
+                         false, outputs_[i].dtype());
+    if (backward) {
+      auto mem = outputs_[i].GetMKLDNNData();
+      outputs[i].CopyFrom(*mem);
+    }
+  }
+  MKLDNNStream::Get()->Submit();
+}
+    
+    struct MultiAllFiniteParam : public dmlc::Parameter<MultiAllFiniteParam> {
+  int num_arrays;
+  bool init_output;
+  DMLC_DECLARE_PARAMETER(MultiAllFiniteParam) {
+    DMLC_DECLARE_FIELD(num_arrays)
+    .set_default(1)
+    .describe('Number of arrays.');
+    DMLC_DECLARE_FIELD(init_output)
+    .set_default(true)
+    .describe('Initialize output to 1.');
+  }
+};
+    
+    /*!
+ *  Copyright (c) 2019 by Contributors
+ * \file all_finite.cc 
+ * \brief operator for checking if a group of array is all finite
+ * \author Clement Fuji Tsang
+ */
+#include './all_finite-inl.h'
+#include <cmath>
+    
+        // bbox: [num_rois, 5]
+    mxnet::TShape bshape = in_shape->at(deformablepsroipool::kBox);
+    CHECK_EQ(bshape.ndim(), 2) << 'bbox should be a 2D tensor of shape [batch, 5]';
+    CHECK_EQ(bshape[1], 5) << 'bbox should be a 2D tensor of shape [batch, 5]';
+    
+    template<typename AttrType, typename FInfer>
+bool ApplyOpInferAttr(const nnvm::Graph& g,
+                      const FInfer& finfer,
+                      const NodeAttrs& attrs,
+                      const uint32_t nid,
+                      std::vector<AttrType>* in_attrs,
+                      std::vector<AttrType>* out_attrs,
+                      DispatchMode* dispatch_mode) {
+  return finfer(attrs, in_attrs, out_attrs);
+}
+    
+    /*!
+ *  Copyright (c) 2017 by Contributors
+ * \file quantize-inl.h
+ * \brief implementation of quantize operation
+ */
+#ifndef MXNET_OPERATOR_QUANTIZATION_QUANTIZE_INL_H_
+#define MXNET_OPERATOR_QUANTIZATION_QUANTIZE_INL_H_
+    
+      const Context &ctx = handle.ctx;
+  auto&& device = storage_managers_.at(ctx.dev_type);
+  std::shared_ptr<storage::StorageManager> manager = device.Get(
+      ctx.real_dev_id(), []() {
+        LOG(FATAL) <<  'Cannot Free space to a device you have not allocated';
+        return nullptr;
+      });
+    
+      // lhs or rhs or both params were not specified
+  if (!param.lhs_axes.has_value() || !param.rhs_axes.has_value()) {
+    CHECK_EQ(lhs_shape.ndim(), rhs_shape.ndim())
+      << 'Operand of shape ' << lhs_shape << ' cannot be broadcasted to ' << rhs_shape;
+    }
+    
+      auto user_src_iter_memory = null_memory_;
+  if (L == 1) {
+    user_src_iter_memory = (*concat_iter_memory)[layer_index];
+  } else {
+    user_src_iter_memory = (*concat_iter_memory)[L + layer_index];
+    std::vector<void*> src_l_data;
+    std::vector<mkldnn::memory::dims> src_l_dim;
+    for (int l = 0; l < L; l++) {
+      src_l_data.push_back(reinterpret_cast<DType *>
+          ((*concat_iter_memory)[l + layer_index].get_data_handle()));
+      src_l_dim.push_back({1, 1, nstates, N, H});
+    }
+    ConcatData(mkldnn::memory::format::ldsnc, mkldnn::memory::format::ldsnc, src_l_dim,
+        {L, 1, nstates, N, H}, mkldnn_dtype, 0, src_l_data, user_src_iter_memory);
+  }
+  (*hcx_memory)[layer_index].set_data_handle(user_src_iter_memory.get_data_handle());
+    
+    namespace swoole {
+//-------------------------------------------------------------------------------
+namespace cpp_string
+{
+template<typename ...Args>
+inline std::string format(const char *format, Args ...args)
+{
+    size_t size = snprintf(nullptr, 0, format, args...) + 1; // Extra space for '\0'
+    std::unique_ptr<char[]> buf(new char[size]);
+    snprintf(buf.get(), size, format, args...);
+    return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
+}
     }
     }
     
-      // Create a writer that will append data to '*dest'.
-  // '*dest' must have initial length 'dest_length'.
-  // '*dest' must remain live while this Writer is in use.
-  Writer(WritableFile* dest, uint64_t dest_length);
+    #include <netdb.h>
+#include <arpa/inet.h>
     
-      // Returns an estimate of the number of bytes of data in use by this
-  // data structure. It is safe to call when MemTable is being modified.
-  size_t ApproximateMemoryUsage();
-    
-    double ObjectGeneralInfo60B::longitude_dist(const std::uint8_t* bytes,
-                                            int32_t length) const {
-  Byte t0(bytes + 1);
-  int32_t x = t0.get_byte(0, 8);
+        zval retval;
+    zend_op_array *new_op_array;
+    ZVAL_NULL(&dummy);
+    if (zend_hash_add(&EG(included_files), opened_path, &dummy))
+    {
+        new_op_array = zend_compile_file(&file_handle, ZEND_REQUIRE);
+        zend_destroy_file_handle(&file_handle);
+    }
+    else
+    {
+        new_op_array = NULL;
+        zend_file_handle_dtor(&file_handle);
+    }
+    zend_string_release(opened_path);
+    if (!new_op_array)
+    {
+        return false;
     }
     
-    TEST(TestPiecewiseLinearKernel, add_regularization) {
-  PiecewiseLinearKernel kernel(10, 0.1);
+        swListenPort *port = swServer_add_port(&serv, SW_SOCK_TCP, '127.0.0.1', 9501);
+    port->open_eof_check = 0;
+    //config
+    port->backlog = 128;
+    memcpy(port->protocol.package_eof, SW_STRL('\r\n\r\n'));  //开启eof检测，启用buffer区
+    
+    
+    {    SwooleG.main_reactor->once = false;
+}
+    
+    
+    {#ifdef SW_CONTEXT_PROTECT_STACK_PAGE
+    uint32_t protect_page = get_protect_stack_page();
+    if (protect_page)
+    {
+        if (protect_stack(stack_, stack_size, protect_page))
+        {
+            protect_page_ = protect_page;
+        }
+    }
+#endif
+}
+    
+    static inline uchar swoole_event_add(int fd, int events, int fdtype)
+{
+    return SwooleG.main_reactor->add(SwooleG.main_reactor, fd, fdtype | events) == SW_OK;
+}
+    
+    uchar swoole_timer_exists(long timer_id)
+{
+    if (!SwooleG.timer.initialized)
+    {
+        swWarn('no timer');
+        return false;
+    }
+    auto tnode = swTimer_get(&SwooleG.timer, timer_id);
+    return (tnode && !tnode->removed);
+}
+    
+        pid_t server_pid = create_server();
+    
+        for (i = 0; i < READ_THREAD_N; i++)
+    {
+        int ret = swPipeUnsock_create(&threads[i].pipe, 1, SOCK_DGRAM);
+        ASSERT_EQ(ret, 0);
+        threads[i].thread = new std::thread(thread_read, i);
     }
