f6dbf7d0ab88c67e81ccf2ce6365eb15a04d34b3
SUNsung <weatherius@gmail.com>
2019-07-16 21:44:41 +0300

    Rewrite [00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb] File in: 2019-07-16 21:44:41

--- ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
+++ ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
@@ -1,116 +1,150 @@
 
         
-        def liquid_escape(markdown)
-  markdown.gsub(%r!(`{[{%].+[}%]}`)!, '{% raw %}\\1{% endraw %}')
-end
-    
-    # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
-    
-          #
-    
-              new_theme_name = args.join('_')
-          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
-    
-                  Jekyll.logger.info 'LiveReload address:',
-                                 'http://#{opts['host']}:#{opts['livereload_port']}'
-            end
-          end
-          @thread.abort_on_exception = true
-        end
-    
-            def id_for_already_imported_cache(issue)
-          issue.number
-        end
-    
-                rows << {
-              label_id: label_id,
-              target_id: target_id,
-              target_type: issue.issuable_type,
-              created_at: time,
-              updated_at: time
-            }
-          end
-    
-          # Returns the identifier to use for cache keys.
-      #
-      # For issues and pull requests this will be 'Issue' or 'MergeRequest'
-      # respectively. For diff notes this will return 'MergeRequest', for
-      # regular notes it will either return 'Issue' or 'MergeRequest' depending
-      # on what type of object the note belongs to.
-      def cache_key_type
-        if object.respond_to?(:issuable_type)
-          object.issuable_type
-        elsif object.respond_to?(:noteable_type)
-          object.noteable_type
-        else
-          raise(
-            TypeError,
-            'Instances of #{object.class} are not supported'
-          )
-        end
-      end
-    
-          def parallel?
-        @parallel
-      end
-    
-        private
-    
-          @report_note = current_account.report_notes.new(resource_params)
-      @report = @report_note.report
-    
-      def show
-    @status = status_finder.status
-    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
+          def self.invite_by_email(email, invited_by, topic = nil, group_ids = nil, custom_message = nil)
+    create_invite_by_email(email, invited_by,
+      topic: topic,
+      group_ids: group_ids,
+      custom_message: custom_message,
+      send_email: true
+    )
   end
     
-      def update
-    if verify_payload?
-      process_salmon
-      head 202
-    elsif payload.present?
-      render plain: signature_verification_failure_reason, status: 401
-    else
-      head 400
+      context '#destroy' do
+    it 'should return the right response for an invalid group_id' do
+      max_id = Group.maximum(:id).to_i
+      delete '/admin/groups/#{max_id + 1}.json'
+      expect(response.status).to eq(404)
+    end
+    
+      describe '#status' do
+    it 'works for failed jobs' do
+      job.failed_at = Time.now
+      expect(status(job)).to eq('<span class='label label-danger'>failed</span>')
+    end
+    
+            it 'kills no long active workers' do
+          mock.instance_of(HuginnScheduler).run!
+          mock.instance_of(DelayedJobWorker).run!
+          @agent_runner.send(:run_workers)
+          AgentRunner.class_variable_set(:@@agents, [DelayedJobWorker])
+          mock.instance_of(HuginnScheduler).stop!
+          @agent_runner.send(:run_workers)
+        end
+      end
+    
+      after(:each) do
+    @rufus_scheduler.shutdown(:wait)
+  end
+    
+      describe 'migrating a hash' do
+    it 'should convert every attribute' do
+      expect(LiquidMigrator.convert_hash({'a' => '$.data', 'b' => 'This is a <$.test>'})).to eq(
+                                  {'a' => '$.data', 'b' => 'This is a {{test}}'}
+      )
+    end
+    it 'should work with leading_dollarsign_is_jsonpath' do
+      expect(LiquidMigrator.convert_hash({'a' => '$.data', 'b' => 'This is a <$.test>'}, leading_dollarsign_is_jsonpath: true)).to eq(
+                                  {'a' => '{{data}}', 'b' => 'This is a {{test}}'}
+      )
+    end
+    it 'should use the corresponding *_path attributes when using merge_path_attributes'do
+      expect(LiquidMigrator.convert_hash({'a' => 'default', 'a_path' => '$.data'}, {leading_dollarsign_is_jsonpath: true, merge_path_attributes: true})).to eq(
+                                  {'a' => '{{data}}'}
+      )
+    end
+    it 'should raise an exception when encountering complex JSONPaths' do
+      expect { LiquidMigrator.convert_hash({'b' => 'This is <$.complex[2]>'}) }.
+        to raise_error('JSONPath '$.complex[2]' is too complex, please check your migration.')
     end
   end
     
-      private
+        @opts = {
+      'api_key' => '800deeaf-e285-9d62-bc90-j999c1973cc9',
+      'path' => 'trackings'
+    }
     
-    class Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token
+          # Remembers the given resource by setting up a cookie
+      def remember_me(resource)
+        return if request.env['devise.skip_storage']
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.remember_me!
+        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
+      end
     
-      def apply_header_reset
-    response.headers['X-RateLimit-Reset'] = rate_limit_reset
+            routes.each do |module_name, actions|
+          [:path, :url].each do |path_or_url|
+            actions.each do |action|
+              action = action ? '#{action}_' : ''
+              method = :'#{action}#{module_name}_#{path_or_url}'
+    
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
+    
+        def default_controllers(options)
+      mod = options[:module] || 'devise'
+      @controllers = Hash.new { |h,k| h[k] = '#{mod}/#{k}' }
+      @controllers.merge!(options[:controllers]) if options[:controllers]
+      @controllers.each { |k,v| @controllers[k] = v.to_s }
+    end
+    
+          def rsync_post(machine, opts)
+        if opts.key?(:chown) && !opts[:chown]
+          return
+        end
+    
+      let(:iso_env) do
+    # We have to create a Vagrantfile so there is a root path
+    env = isolated_environment
+    env.vagrantfile('')
+    env.create_vagrant_env
   end
     
-        reset
+        it 'should receive a valid address' do
+      allow(provider_config).to receive(:compose).and_return(false)
+      allow(platform).to receive(:windows?).and_return(false)
+      allow(platform).to receive(:darwin?).and_return(false)
+      allow(driver_obj).to receive(:created?).and_return(true)
+      allow(driver_obj).to receive(:state).and_return(:running)
+      allow(driver_obj).to receive(:execute).with(:get_network_config).and_return(result)
+      allow(driver_obj).to receive(:inspect_container).and_return(network_settings)
+    
+      def test_font_helper_without_suffix
+    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
   end
     
-        if (p)
-      # Create an instance of the handler for this resource
-      handler = p[0].new(self, *p[2])
+    def capture_logging(lvl=Logger::INFO)
+  old = Sidekiq.logger
+  begin
+    out = StringIO.new
+    logger = Logger.new(out)
+    logger.level = lvl
+    Sidekiq.logger = logger
+    yield
+    out.string
+  ensure
+    Sidekiq.logger = old
+  end
+end
     
-              # Encodes the realm field
-          #
-          # @return [String]
-          def encode_realm
-            encoded = ''
-            encoded << [realm.length].pack('N')
-            encoded << realm
+    describe 'job scheduling' do
+  describe 'middleware' do
+    class SomeScheduledWorker
+      include Sidekiq::Worker
+      sidekiq_options :queue => :custom_queue
+      def perform(x)
+      end
+    end
     
-              # @!attribute type
-          #   @return [Integer] The algorithm used to generate the checksum
-          attr_accessor :type
-          # @!attribute checksum
-          #   @return [String] The checksum itself
-          attr_accessor :checksum
+    describe 'Sidekiq::Testing' do
+  describe 'require/load sidekiq/testing.rb' do
+    before do
+      require 'sidekiq/testing'
+    end
     
-              include Rex::Proto::Kerberos::Crypto
-          include Rex::Proto::Kerberos::Model
-    
-                int
-          end
+    include Sidekiq::Util
