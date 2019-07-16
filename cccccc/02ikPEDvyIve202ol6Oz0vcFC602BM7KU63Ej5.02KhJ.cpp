ab6208249c0e6aa6507a1a7627bf2470dde38551
SUNsung <weatherius@gmail.com>
2019-07-17 00:30:43 +0300

    Rewrite [02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp] File in: 2019-07-17 00:30:42

--- cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
+++ cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
@@ -1,411 +1,53 @@
 
         
-        
-    {        inline size_t total() const
-        {
-            return width * height;
-        }
-    };
-    
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const u8* src = internal::getRowPtr(srcBase, srcStride, i);
-        u8* dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t j = 0;
-    }
-    
-    #include 'common.hpp'
-#include 'saturate_cast.hpp'
-    
-            size_t i = 0;
-        while(i + 4 <= size.width)
-        {
-            size_t lim = std::min(i + DOT_FLOAT_BLOCKSIZE, size.width) - 4;
-            float32x4_t v_sum = vdupq_n_f32(0.0f);
-    }
-    
-        v1k0 = vextq_s16(d0_7, d8_15, 4);
-    ak0 = vminq_s16(ak0, v1k0);
-    bk0 = vmaxq_s16(bk0, v1k0);
-    
-    void fillMinMaxLocs(const Size2D & size,
-                    const u32 * srcBase, ptrdiff_t srcStride,
-                    u32 minVal, size_t * minLocPtr, s32 & minLocCount, s32 minLocCapacity,
-                    u32 maxVal, size_t * maxLocPtr, s32 & maxLocCount, s32 maxLocCapacity)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-    }
-    
-                    ptrdiff_t* lidx0 = lane + x - 2*4;
-                ptrdiff_t* lidx1 = lane + x - 1*4;
-                ptrdiff_t* lidx3 = lane + x + 1*4;
-                ptrdiff_t* lidx4 = lane + x + 2*4;
-    
-    #include 'glog/logging.h'
-#include 'google/protobuf/text_format.h'
-#include 'stdint.h'
-    
-      void Transform(const Datum& datum, Dtype* transformed_data);
-  // Tranformation parameters
-  TransformationParameter param_;
-    
-    
-    {}  // namespace caffe
-    
-     protected:
-  // Helper functions that abstract away the column buffer and gemm arguments.
-  // The last argument in forward_cpu_gemm is so that we can skip the im2col if
-  // we just called weight_cpu_gemm with the same input.
-  void forward_cpu_gemm(const Dtype* input, const Dtype* weights,
-      Dtype* output, bool skip_im2col = false);
-  void forward_cpu_bias(Dtype* output, const Dtype* bias);
-  void backward_cpu_gemm(const Dtype* input, const Dtype* weights,
-      Dtype* output);
-  void weight_cpu_gemm(const Dtype* input, const Dtype* output, Dtype*
-      weights);
-  void backward_cpu_bias(Dtype* bias, const Dtype* input);
-    
-      virtual inline const char* type() const { return 'BatchNorm'; }
-  virtual inline int ExactNumBottomBlobs() const { return 1; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
-    
-     protected:
-  /// @copydoc BNLLLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    #ifdef USE_CUDNN
-/*
- * @brief cuDNN implementation of DeConvolutionLayer.
- *        Fallback to DeConvolutionLayer for CPU mode.
- *
- * cuDNN accelerates deconvolution through forward kernels for filtering and
- * bias plus backward kernels for the gradient w.r.t. the filters, biases, and
- * inputs. Caffe + cuDNN further speeds up the computation through forward
- * parallelism across groups and backward parallelism across gradients.
-*/
-template <typename Dtype>
-class CuDNNDeconvolutionLayer : public DeconvolutionLayer<Dtype> {
- public:
-  explicit CuDNNDeconvolutionLayer(const LayerParameter& param)
-    : DeconvolutionLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-                          const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-                       const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNDeconvolutionLayer();
-    }
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of SigmoidLayer.
- */
-template <typename Dtype>
-class CuDNNSigmoidLayer : public SigmoidLayer<Dtype> {
- public:
-  explicit CuDNNSigmoidLayer(const LayerParameter& param)
-      : SigmoidLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNSigmoidLayer();
-    }
-    
-    void CensusClientCallData::Destroy(grpc_call_element* elem,
-                                   const grpc_call_final_info* final_info,
-                                   grpc_closure* then_call_closure) {
-  const uint64_t request_size = GetOutgoingDataSize(final_info);
-  const uint64_t response_size = GetIncomingDataSize(final_info);
-  double latency_ms = absl::ToDoubleMilliseconds(absl::Now() - start_time_);
-  ::opencensus::stats::Record(
-      {{RpcClientSentBytesPerRpc(), static_cast<double>(request_size)},
-       {RpcClientReceivedBytesPerRpc(), static_cast<double>(response_size)},
-       {RpcClientRoundtripLatency(), latency_ms},
-       {RpcClientServerLatency(),
-        ToDoubleMilliseconds(absl::Nanoseconds(elapsed_time_))},
-       {RpcClientSentMessagesPerRpc(), sent_message_count_},
-       {RpcClientReceivedMessagesPerRpc(), recv_message_count_}},
-      {{ClientMethodTagKey(), method_},
-       {ClientStatusTagKey(), StatusCodeToString(final_info->final_status)}});
-  grpc_slice_unref_internal(path_);
-  context_.EndSpan();
+        void Clear() {
+  std::lock_guard<std::mutex> lg(g_mutex);
+  if (GetState() == STATE_SHUTDOWN || GetState() == STATE_UNINITIALIZED) {
+    return;
+  }
+  TaskManager::CleanUp();
+  TimingWheel::CleanUp();
+  scheduler::CleanUp();
+  service_discovery::TopologyManager::CleanUp();
+  transport::Transport::CleanUp();
+  StopLogger();
+  SetState(STATE_SHUTDOWN);
 }
     
-      enum FieldSize {
-    kServerElapsedTimeSize = 8,
-  };
+      common::SLPoint GetStopSL(const ObjectStop& stop_decision,
+                            const ReferenceLine& reference_line) const;
     
-    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
-    const grpc::string& name, int value) {
-  class IntOption final : public ServerBuilderOption {
-   public:
-    IntOption(const grpc::string& name, int value)
-        : name_(name), value_(value) {}
-    }
-    }
-    
-    #include 'src/cpp/util/core_stats.h'
-    
-    // Initialization funciton called by caffeOp & caffeLoss
-template<typename Dtype>
-void InitCaffeBlobs(std::vector< ::caffe::Blob<Dtype>*>* v, int n_num) {
-  for (index_t i=0; i < n_num; ++i)
-    v->push_back(new ::caffe::Blob<Dtype>());
-}
-    
-     private:
-  /*!
-   * \brief Wait for all started threads to signal that they're ready
-   */
-  void WaitForReady() {
-    for (const std::shared_ptr<dmlc::ManualEvent>& ptr : ready_events_) {
-      ptr->wait();
-    }
+      // 7
+  chassis_.set_fuel_range_m(0);
+  // 8
+  if (chassis_detail.gem().has_accel_rpt_68() &&
+      chassis_detail.gem().accel_rpt_68().has_output_value()) {
+    chassis_.set_throttle_percentage(
+        static_cast<float>(chassis_detail.gem().accel_rpt_68().output_value()));
+  } else {
+    chassis_.set_throttle_percentage(0);
+  }
+  // 9
+  if (chassis_detail.gem().has_brake_rpt_6c() &&
+      chassis_detail.gem().brake_rpt_6c().has_output_value()) {
+    chassis_.set_brake_percentage(
+        static_cast<float>(chassis_detail.gem().brake_rpt_6c().output_value()));
+  } else {
+    chassis_.set_brake_percentage(0);
   }
     
     
-    {
-    {/*! \brief typedef the factory function of data iterator */
-typedef std::function<ImageAugmenter *()> ImageAugmenterFactory;
-/*!
- * \brief Registry entry for DataIterator factory functions.
- */
-struct ImageAugmenterReg
-    : public dmlc::FunctionRegEntryBase<ImageAugmenterReg,
-                                        ImageAugmenterFactory> {
-};
-//--------------------------------------------------------------
-// The following part are API Registration of Iterators
-//--------------------------------------------------------------
-/*!
- * \brief Macro to register image augmenter
- *
- * \code
- * // example of registering a mnist iterator
- * REGISTER_IMAGE_AUGMENTER(aug_default)
- * .describe('default augmenter')
- * .set_body([]() {
- *     return new DefaultAugmenter();
- *   });
- * \endcode
- */
-#define MXNET_REGISTER_IMAGE_AUGMENTER(name)                            \
-  DMLC_REGISTRY_REGISTER(::mxnet::io::ImageAugmenterReg, ImageAugmenterReg, name)
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_USE_OPENCV
-    
-    /*!
- * \brief Iterator that normalize a image.
- *  It also applies a few augmention before normalization.
- */
-class ImageNormalizeIter : public IIterator<DataInst> {
- public:
-  explicit ImageNormalizeIter(IIterator<DataInst> *base)
-      : base_(base), meanfile_ready_(false) {
-  }
-    }
-    
-      /*!
-  * \brief Issues dequantize operation to be scheduled by the engine
-  * Decompresses `from` into `to` using current parameters of `type` and `threshold`
-  * \param from the ndarray containing quantized data
-  * \param to the target ndarray which contains final dequantized data
-  * \param priority Priority of the action.
-  */
-  void Dequantize(const mxnet::NDArray &from, mxnet::NDArray *to, const int priority);
-    
-    
-    {
-    {// for reverse seq
-DECL_SCALAR(DEVICE, Plus, EvalScalar_, false)
-DECL_SCALAR(DEVICE, Minus, EvalScalar_, false)
-DECL_SCALAR(DEVICE, Mul, EvalScalar_, false)
-DECL_SCALAR(DEVICE, Div, EvalScalar_, false)
-}  // namespace ndarray
-}  // namespace mxnet
-    
-      ~CuDNNSpatialTransformerOp() {
-    if (init_cudnn_) {
-      CUDNN_CALL(cudnnDestroySpatialTransformerDescriptor(st_desc_));
-      CUDNN_CALL(cudnnDestroyTensorDescriptor(in_desc_));
-      CUDNN_CALL(cudnnDestroyTensorDescriptor(out_desc_));
-    }
-  }
-    
-    
-         case URX_DOLLAR_D:                   //  $, test for End of Line, in UNIX_LINES mode.
-            if (fp->fInputIdx >= fAnchorLimit) {
-                // Off the end of input.  Success.
-                fHitEnd = TRUE;
-                fRequireEnd = TRUE;
-                break;
-            } else {
-                UTEXT_SETNATIVEINDEX(fInputText, fp->fInputIdx);
-                UChar32 c = UTEXT_NEXT32(fInputText);
-                // Either at the last character of input, or off the end.
-                if (c == 0x0a && UTEXT_GETNATIVEINDEX(fInputText) == fAnchorLimit) {
-                    fHitEnd = TRUE;
-                    fRequireEnd = TRUE;
-                    break;
-                }
-            }
-    
-    void SearchIterator::reset()
-{
-    UErrorCode status = U_ZERO_ERROR;
-    setMatchNotFound();
-    setOffset(0, status);
-    m_search_->isOverlap          = FALSE;
-    m_search_->isCanonicalMatch   = FALSE;
-    m_search_->elementComparisonType = 0;
-    m_search_->isForwardSearching = TRUE;
-    m_search_->reset              = TRUE;
-}
-    
-    U_NAMESPACE_BEGIN
-    
-    
-    {        int32_t length = other.getLength();
-        if (length > getCapacity() && reallocate(length, 0) == NULL) {
-            return setToBogus();
-        }
-        if (length > 0) {
-            uprv_memcpy(getBytes(), other.getBytes(), length);
-        }
-        fFlagAndLength = (fFlagAndLength & 0x80000000) | length;
-        fHashCode = other.fHashCode;
-    }
-    
-    
-    {        UnicodeReplacer* r = data->lookupReplacer(c);
-        if (r == NULL) {
-            ICU_Utility::appendToRule(rule, c, FALSE, escapeUnprintable, quoteBuf);
-        } else {
-            UnicodeString buf;
-            r->toReplacerPattern(buf, escapeUnprintable);
-            buf.insert(0, (UChar)0x20);
-            buf.append((UChar)0x20);
-            ICU_Utility::appendToRule(rule, buf,
-                                      TRUE, escapeUnprintable, quoteBuf);
-        }
-    }
-    
-      // When flush happens, it determines whether to trigger compaction. If
-  // triggered_writes_stop is true, it will also set the retry flag of
-  // compaction-task to true.
-  void OnFlushCompleted(
-      DB* db, const FlushJobInfo& info) override {
-    CompactionTask* task = PickCompaction(db, info.cf_name);
-    if (task != nullptr) {
-      if (info.triggered_writes_stop) {
-        task->retry_on_fail = true;
-      }
-      // Schedule compaction in a different thread.
-      ScheduleCompaction(task);
-    }
-  }
-    
-      // atomically apply a set of updates
-  {
-    WriteBatch batch;
-    batch.Delete('key1');
-    batch.Put('key2', value);
-    s = db->Write(WriteOptions(), &batch);
-  }
-    
-    class DB;
-    
-    namespace rocksdb {
-    }
-    
-    #pragma once
-// lua headers
-extern 'C' {
-#include <lauxlib.h>
-#include <lua.h>
-#include <lualib.h>
-}
-    
-    /*
- * Class:     org_rocksdb_BackupableDBOptions
- * Method:    setCallbackTriggerIntervalSize
- * Signature: (JJ)V
- */
-void Java_org_rocksdb_BackupableDBOptions_setCallbackTriggerIntervalSize(
-    JNIEnv* /*env*/, jobject /*jobj*/, jlong jhandle,
-    jlong jcallback_trigger_interval_size) {
-  auto* bopt = reinterpret_cast<rocksdb::BackupableDBOptions*>(jhandle);
-  bopt->callback_trigger_interval_size =
-      static_cast<uint64_t>(jcallback_trigger_interval_size);
-}
-    
-        for (i = 1; i < 1024; i++)
-    {
-        uint32_t key = (rand() % (20000 * 37));
-        if (key % 37 == 0)
-        {
-            continue;
-        }
-        int ret = (int) (long) swRbtree_find(tree, key);
-        ASSERT_EQ(ret, 0);
-    }
-    
-        m_adapter.setContext(m_ctx);
-    
-        private:
-        void finish() { emit finished(); }
-    
-        inline void* pop_data()
-    {
-        if (data_queue.size() == 0)
-        {
-            return nullptr;
-        }
-        void *data = data_queue.front();
-        data_queue.pop();
-        return data;
-    }
-    
-    public:
-    explicit LRUCache(size_t capacity)
-    {
-        cache_capacity = capacity;
-    }
-    
-    
-    {    coro_test_wait(&complete_num, args.size());
-}
-    
-    struct defer_task
-{
-    swCallback callback;
-    void *data;
+    { protected:
+  std::unique_ptr<PullOverScenario> scenario_;
 };
     
+    class StageParkingTest : public ::testing::Test {
+ public:
+  virtual void SetUp() {
+    config_.set_stage_type(ScenarioConfig::VALET_PARKING_PARKING);
+  }
+    }
     
-    {    int ret;
-    char resp_data[SW_BUFFER_SIZE_STD];
-    int n = snprintf(resp_data, SW_BUFFER_SIZE_STD, (char *) 'Server: %.*s\n', (int) data.length, (char *) data.buffer);
-    ret = this->send(fd, resp_data, (uint32_t) n);
-    if (ret < 0)
-    {
-        printf('send to client fail. errno=%d\n', errno);
-    }
-    else
-    {
-        printf('send %d bytes to client success. data=%s\n', n, resp_data);
-    }
-    DataBuffer task_data('hello world\n');
-    this->task(task_data);
-//    this->close(fd);
+    
+    {  EXPECT_EQ(expect, real);
 }
