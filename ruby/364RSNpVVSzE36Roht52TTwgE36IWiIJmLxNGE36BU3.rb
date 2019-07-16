d50f45265d1a846816d0a9184aad09efb4d69e43
SUNsung <weatherius@gmail.com>
2019-07-16 20:34:52 +0300

    Rewrite [364RSNpVVSzE36Roht52TTwgE36IWiIJmLxNGE36BU3.rb] File in: 2019-07-16 20:34:51

--- ruby/364RSNpVVSzE36Roht52TTwgE36IWiIJmLxNGE36BU3.rb
+++ ruby/364RSNpVVSzE36Roht52TTwgE36IWiIJmLxNGE36BU3.rb
@@ -1,116 +1,133 @@
 
         
-                result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            tag: '#{tag}',
-            message: '#{message}',
-            force: true
-          })
-        end').runner.execute(:test)
-    
-          it 'adds logformat param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            logformat: '1'
-          )
-        end').runner.execute(:test)
-    
-    # Windows implementation
-module WindowsShellwords
-  def shellescape(str)
-    str = str.to_s
-    
-    puts('[WARNING] You are calling #{tool_name} directly. Usage of the tool name without the `fastlane` prefix is deprecated in fastlane 2.0'.yellow)
-puts('Please update your scripts to use `fastlane #{tool_name} #{full_params}` instead.'.yellow)
-    
-        it 'allows to delete a user' do
-      visit admin_users_path
-      find(:css, 'a[href='/admin/users/#{users(:bob).id}']').click
-      expect(page).to have_text('User 'bob' was deleted.')
-      expect(page).to have_no_text('bob@example.com')
-    end
-    
-      describe '#status' do
-    it 'works for failed jobs' do
-      job.failed_at = Time.now
-      expect(status(job)).to eq('<span class='label label-danger'>failed</span>')
-    end
-    
-          expect(data[:agents][guid_order(agent_list, :jane_weather_agent)]).not_to have_key(:propagate_immediately) # can't receive events
-      expect(data[:agents][guid_order(agent_list, :jane_rain_notifier_agent)]).not_to have_key(:schedule) # can't be scheduled
-    end
-    
-        it 'should ignore strings which just contain a JSONPath' do
-      expect(LiquidMigrator.convert_string('$.data')).to eq('$.data')
-      expect(LiquidMigrator.convert_string('$first_title')).to eq('$first_title')
-      expect(LiquidMigrator.convert_string(' $.data', true)).to eq(' $.data')
-      expect(LiquidMigrator.convert_string('lorem $.data', true)).to eq('lorem $.data')
-    end
-    it 'should raise an exception when encountering complex JSONPaths' do
-      expect { LiquidMigrator.convert_string('$.data.test.*', true) }.
-        to raise_error('JSONPath '$.data.test.*' is too complex, please check your migration.')
-    end
-  end
-    
-          Utils.sort_tuples!(tuples)
-      expect(tuples).to eq expected
-    end
-    
-        it 'should generate the correct specific tracking url' do
-      @checker.options['path'] = 'trackings/usps/9361289878905919630610'
-      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/trackings/usps/9361289878905919630610')
-    end
-    
-      describe '#working?' do
-    it 'should not be working until the first event was received' do
-      expect(@checker).not_to be_working
-      @checker.last_receive_at = Time.now
-      expect(@checker).to be_working
-    end
-  end
-    
-        def add(path, content)
-      @pages[path] = content
-    end
-    
-          def process_response(response)
-        super.merge! response_effective_path: response.effective_path, response_path: response.path
+                subject.exec
       end
     
-            # Remove ng-* attributes
-        css('*').each do |node|
-          node.attributes.each_key do |attribute|
-            node.remove_attribute(attribute) if attribute.start_with? 'ng-'
-          end
+          expect(finder.find('test-template')).to be_present
+    end
+    
+      subject(:finder) { described_class.new(project, 'files/', '.html', categories) }
+    
+      it 'allows user to expand diff' do
+    page.within find('[id='2f6fcd96b88b36ce98c38da085c795a27d92a3dd']') do
+      click_link 'Click to expand it.'
+    
+            [query, filters]
+      end
+    
+            def self.default_options
+          ::Commit.max_diff_options.merge(ignore_whitespace_change: false, expanded: false, include_stats: true)
         end
     
-    task :gemspec => 'rack-protection.gemspec'
-task :default => :spec
-task :test    => :spec
-
-    
-      it 'denies post requests with wrong X-CSRF-Token header' do
-    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => bad_token)
-    expect(last_response).not_to be_ok
-  end
-    
-        it 'adds the correct Set-Cookie header' do
-      get '/some/path', {}, 'HTTP_COOKIE' => 'rack.%73ession=EVIL_SESSION_TOKEN; rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
-    
-    module LogStash
-  module PluginManager
-  end
-end
-    
-          post_install_messages.compact.each do |message|
-        PluginManager.ui.info(message)
+            uses_from_macos('foo', after: :mojave)
       end
     
-    shared_examples 'logstash update' do |logstash|
-  describe 'logstash-plugin update on #{logstash.hostname}' do
-    before :each do
-      logstash.install({:version => LOGSTASH_VERSION})
+          expect(spec.deps.first.name).to eq('foo')
     end
+    
+        it 'sets itself to be the resource's owner' do
+      subject.resource('foo') { url 'foo-1.0' }
+      subject.owner = owner
+      subject.resources.each_value do |r|
+        expect(r.owner).to eq(subject)
+      end
+    end
+    
+      describe '#message' do
+    its(:message) { is_expected.to match(/Java is required to install this formula./) }
+  end
+    
+          def version
+        # may return a version string
+        # that is guessed based on the compiler, so do not
+        # use it in order to check if Xcode is installed.
+        if @version ||= detect_version
+          ::Version.new @version
+        else
+          ::Version::NULL
+        end
+      end
+    
+          raise CaskError,
+            'Cask #{@cask} depends on hardware architecture being one of ' \
+            '[#{@cask.depends_on.arch.map(&:to_s).join(', ')}], ' \
+            'but you are running #{@current_arch}'
+    end
+    
+          path = if timestamp == :latest
+        Pathname.glob(metadata_versioned_path(version: version).join('*')).max
+      else
+        timestamp = new_timestamp if timestamp == :now
+        metadata_versioned_path(version: version).join(timestamp)
+      end
+    
+          order.payments.with_state('checkout').where.not(id: id).each do |payment|
+        payment.invalidate! unless payment.store_credit?
+      end
+    end
+  end
+end
+
+    
+              order.create_proposed_shipments unless shipments_attrs.present?
+    
+          order.reload.next!
+      order.update_with_updater!
+      order.finalize!
+    
+        (all_reception_statuses - ['awaiting']).each do |invalid_transition_status|
+      context 'return_item has a reception status of #{invalid_transition_status}' do
+        it_behaves_like 'an invalid state transition', invalid_transition_status, 'received'
+      end
+    end
+  end
+    
+            def show
+          authorize! :show, @order, order_token
+          @address = find_address
+          respond_with(@address)
+        end
+    
+            def show
+          @option_type = Spree::OptionType.accessible_by(current_ability, :show).find(params[:id])
+          respond_with(@option_type)
+        end
+    
+            def perform_payment_action(action, *args)
+          authorize! action, Payment
+          @payment.send('#{action}!', *args)
+          respond_with(@payment, default_template: :show)
+        end
+    
+          it 'flashes a message containing the token' do
+        post '/users/api_secrets', params: { api_secret: valid_params }
+        expect(flash[:notice]).to include(ApiSecret.last.secret)
+        expect(flash[:error]).to be_nil
+      end
+    end
+    
+      def organization
+    @user = Organization.find(params[:id]) || not_found
+    
+      def legacy_article_social_image(article)
+    cache_key = 'article-social-img-#{article}-#{article.updated_at}-#{article.comments_count}'
+    
+      def satellite_tweet!
+    @article.tags.find_each do |tag|
+      BufferUpdate.buff!(@article.id, twitter_buffer_text, tag.buffer_profile_id_code, 'twitter', tag.id) if tag.buffer_profile_id_code.present?
+    end
+    @article.update(last_buffered: Time.current)
+  end
+    
+            get '/ints', values: 'a b c d'
+        expect(last_response.status).to eq(200)
+        expect(JSON.parse(last_response.body)).to eq([0, 0, 0, 0])
+      end
+    
+                namespace_stackable(:params, name[:full_name].to_s => opts)
+          end
+        end
+      end
+    end
+  end
+end
