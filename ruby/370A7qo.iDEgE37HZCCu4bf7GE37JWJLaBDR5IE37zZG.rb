ad428b4a3ce2514237bd66155acf3e51a233b632
SUNsung <weatherius@gmail.com>
2019-07-16 18:43:56 +0300

    Rewrite [370A7qo.iDEgE37HZCCu4bf7GE37JWJLaBDR5IE37zZG.rb] File in: 2019-07-16 18:43:56

--- ruby/370A7qo.iDEgE37HZCCu4bf7GE37JWJLaBDR5IE37zZG.rb
+++ ruby/370A7qo.iDEgE37HZCCu4bf7GE37JWJLaBDR5IE37zZG.rb
@@ -1,180 +1,248 @@
 
         
-            if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+              # there might be more information available in the exception
+      if message_template == :email_reject_invalid_post && e.message.size > 6
+        message_template = :email_reject_invalid_post_specified
+        template_args[:post_error] = e.message
+      end
+    
+        # doing this as early as possible so we have an
+    # accurate counter
+    if queue_start = env['HTTP_X_REQUEST_START']
+      queue_start = queue_start.split('t=')[1].to_f
+      queue_time = (Time.now.to_f - queue_start)
+      env['REQUEST_QUEUE_SECONDS'] = queue_time
     end
-  end
     
-          protected
+          expect(ApplicationRequest.page_view_anon.first.count).to eq(2)
+      expect(ApplicationRequest.page_view_crawler.first.count).to eq(1)
+      expect(ApplicationRequest.page_view_anon_mobile.first.count).to eq(1)
     
-          when IAX_CTRL_PROCEED
-        dprint('PROCEED')
+        after do
+      DiscoursePluginRegistry.reset!
+      LocaleSiteSetting.reset!
+    end
     
-    =begin
- +------+----------------+-------------------------------------------+
-   | HEX  | NAME           | DESCRIPTION                               |
-   +------+----------------+-------------------------------------------+
-   | HEX  | NAME           | DESCRIPTION                               |
-   | 0x01 | CALLED NUMBER  | Number/extension being called             |
-   | 0x02 | CALLING NUMBER | Calling number                            |
-   | 0x03 | CALLING ANI    | Calling number ANI for billing            |
-   | 0x04 | CALLING NAME   | Name of caller                            |
-   | 0x05 | CALLED CONTEXT | Context for number                        |
-   | 0x06 | USERNAME       | Username (peer or user) for               |
-   |      |                | authentication                            |
-   | 0x07 | PASSWORD       | Password for authentication               |
-   | 0x08 | CAPABILITY     | Actual CODEC capability                   |
-   | 0x09 | FORMAT         | Desired CODEC format                      |
-   | 0x0a | LANGUAGE       | Desired language                          |
-   | 0x0b | VERSION        | Protocol version                          |
-   | 0x0c | ADSICPE        | CPE ADSI capability                       |
-   | 0x0d | DNID           | Originally dialed DNID                    |
-   | 0x0e | AUTHMETHODS    | Authentication method(s)                  |
-   | 0x0f | CHALLENGE      | Challenge data for MD5/RSA                |
-   | 0x10 | MD5 RESULT     | MD5 challenge result                      |
-   | 0x11 | RSA RESULT     | RSA challenge result                      |
-   | 0x12 | APPARENT ADDR  | Apparent address of peer                  |
-   | 0x13 | REFRESH        | When to refresh registration              |
-   | 0x14 | DPSTATUS       | Dialplan status                           |
-   | 0x15 | CALLNO         | Call number of peer                       |
-   | 0x16 | CAUSE          | Cause                                     |
-   | 0x17 | IAX UNKNOWN    | Unknown IAX command                       |
-   | 0x18 | MSGCOUNT       | How many messages waiting                 |
-   | 0x19 | AUTOANSWER     | Request auto-answering                    |
-   | 0x1a | MUSICONHOLD    | Request musiconhold with QUELCH           |
-   | 0x1b | TRANSFERID     | Transfer Request Identifier               |
-   | 0x1c | RDNIS          | Referring DNIS                            |
-   | 0x1d | Reserved       | Reserved for future use                   |
-   | 0x1e | Reserved       | Reserved for future use                   |
-   | 0x1f | DATETIME       | Date/Time                                 |
-   | 0x20 | Reserved       | Reserved for future use                   |
-   | 0x21 | Reserved       | Reserved for future use                   |
-   | 0x22 | Reserved       | Reserved for future use                   |
-   | 0x23 | Reserved       | Reserved for future use                   |
-   | 0x24 | Reserved       | Reserved for future use                   |
-   | 0x25 | Reserved       | Reserved for future use                   |
-   | 0x26 | CALLINGPRES    | Calling presentation                      |
-   | 0x27 | CALLINGTON     | Calling type of number                    |
-   | 0x28 | CALLINGTNS     | Calling transit network select            |
-   | 0x29 | SAMPLINGRATE   | Supported sampling rates                  |
-   | 0x2a | CAUSECODE      | Hangup cause                              |
-   | 0x2b | ENCRYPTION     | Encryption format                         |
-   | 0x2c | ENCKEY         | Reserved for future Use                   |
-   | 0x2d | CODEC PREFS    | CODEC Negotiation                         |
-   | 0x2e | RR JITTER      | Received jitter, as in RFC 3550           |
-   | 0x2f | RR LOSS        | Received loss, as in RFC 3550             |
-   | 0x30 | RR PKTS        | Received frames                           |
-   | 0x31 | RR DELAY       | Max playout delay for received frames in  |
-   |      |                | ms                                        |
-   | 0x32 | RR DROPPED     | Dropped frames (presumably by jitter      |
-   |      |                | buffer)                                   |
-   | 0x33 | RR OOO         | Frames received Out of Order              |
-   | 0x34 | OSPTOKEN       | OSP Token Block                           |
-   +------+----------------+-------------------------------------------+
-=end
+            post '/admin/groups.json', params: params
     
-                encoded
-          end
+              s3_helper.expects(:s3_bucket).returns(s3_bucket).at_least_once
+          s3_bucket.expects(:object).with('uploads/default/original/1X/#{upload.sha1}.png').returns(s3_object)
+          s3_object.expects(:acl).returns(s3_object)
+          s3_object.expects(:put).with(acl: 'private').returns(s3_object)
     
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a Kerberos AuthorizationData data
-        # definition.
-        class AuthorizationData < Element
-          # @!attribute elements
-          #   @return [Hash{Symbol => <Integer, String>}] The type of the authorization data
-          #   @option [Integer] :type
-          #   @option [String] :data
-          attr_accessor :elements
+        # The path used after confirmation.
+    def after_confirmation_path_for(resource_name, resource)
+      if signed_in?(resource_name)
+        signed_in_root_path(resource)
+      else
+        new_session_path(resource_name)
+      end
+    end
     
-                seq_values.each do |val|
-              case val.tag
-              when 0
-                self.etype = decode_etype(val)
-              when 1
-                self.kvno = decode_kvno(val)
-              when 2
-                self.cipher = decode_cipher(val)
-              else
-                raise ::RuntimeError, 'Failed to decode EncryptedData SEQUENCE'
-              end
-            end
-          end
+          def extract_path_from_location(location)
+        uri = parse_uri(location)
     
-                int
-          end
-    
-                res = ''
-            case etype
-            when RSA_MD5
-              res = checksum_rsa_md5(data)
-            else
-              raise ::NotImplementedError, 'EncryptedData schema is not supported'
-            end
-    
-              # Decodes the e_data from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_e_data(input)
-            input.value[0].value
-          end
+    module Devise
+  module Controllers
+    # Create url helpers to be used with resource/scope configuration. Acts as
+    # proxies to the generated routes created by devise.
+    # Resource param can be a string or symbol, a class, or an instance object.
+    # Example using a :user resource:
+    #
+    #   new_session_path(:user)      => new_user_session_path
+    #   session_path(:user)          => user_session_path
+    #   destroy_session_path(:user)  => destroy_user_session_path
+    #
+    #   new_password_path(:user)     => new_user_password_path
+    #   password_path(:user)         => user_password_path
+    #   edit_password_path(:user)    => edit_user_password_path
+    #
+    #   new_confirmation_path(:user) => new_user_confirmation_path
+    #   confirmation_path(:user)     => user_confirmation_path
+    #
+    # Those helpers are included by default to ActionController::Base.
+    #
+    # In case you want to add such helpers to another class, you can do
+    # that as long as this new class includes both url_helpers and
+    # mounted_helpers. Example:
+    #
+    #     include Rails.application.routes.url_helpers
+    #     include Rails.application.routes.mounted_helpers
+    #
+    module UrlHelpers
+      def self.remove_helpers!
+        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
+          remove_method method
         end
+      end
+    
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
+    
+          def devise_mapping
+        @devise_mapping ||= Devise.mappings[scope_name]
+      end
+    
+    module RuboCop
+  module Cop
+    module Layout
+      # This cop checks for the placement of the closing parenthesis
+      # in a method call that passes a HEREDOC string as an argument.
+      # It should be placed at the end of the line containing the
+      # opening HEREDOC tag.
+      #
+      # @example
+      #   # bad
+      #
+      #      foo(<<-SQL
+      #        bar
+      #      SQL
+      #      )
+      #
+      #      foo(<<-SQL, 123, <<-NOSQL,
+      #        bar
+      #      SQL
+      #        baz
+      #      NOSQL
+      #      )
+      #
+      #      foo(
+      #        bar(<<-SQL
+      #          baz
+      #        SQL
+      #        ),
+      #        123,
+      #      )
+      #
+      #   # good
+      #
+      #      foo(<<-SQL)
+      #        bar
+      #      SQL
+      #
+      #      foo(<<-SQL, 123, <<-NOSQL)
+      #        bar
+      #      SQL
+      #        baz
+      #      NOSQL
+      #
+      #      foo(
+      #        bar(<<-SQL),
+      #          baz
+      #        SQL
+      #        123,
+      #      )
+      #
+      class HeredocArgumentClosingParenthesis < Cop
+        include RangeHelp
+    
+            def avoid_autocorrect?(args)
+          args.last.hash_type? && args.last.braces? &&
+            braces_will_be_removed?(args)
+        end
+    
+      it 'does not hang SpaceAfterPunctuation and SpaceInsideParens' do
+    create_file('example.rb', 'some_method(a, )')
+    Timeout.timeout(10) do
+      expect(cli.run(%w[--auto-correct])).to eq(0)
+    end
+    expect($stderr.string).to eq('')
+    expect(IO.read('example.rb')).to eq(<<~RUBY)
+      # frozen_string_literal: true
+    
+        context 'outside a class' do
+      it 'doesn't register an offense if a method is defined' do
+        expect_no_offenses(<<~RUBY)
+          class << A
+            #{modifier}
+            define_method(:method1) do
+            end
+          end
+        RUBY
+      end
+    
+        context 'with EnforcedStyle: require_for_equality_operators_only' do
+      let(:cop_config) do
+        { 'EnforcedStyle' => 'require_for_equality_operators_only' }
+      end
+    
+          # Checks whether this `block` literal belongs to a lambda.
+      #
+      # @return [Boolean] whether the `block` literal belongs to a lambda
+      def lambda?
+        send_node.method?(:lambda)
+      end
+    
+          # Checks whether this node body is a void context.
+      # Always `true` for `for`.
+      #
+      # @return [true] whether the `for` node body is a void context
+      def void_context?
+        true
+      end
+    
+    
+    {      # Checks whether the `hash` literal is delimited by curly braces.
+      #
+      # @return [Boolean] whether the `hash` literal is enclosed in braces
+      def braces?
+        loc.end&.is?('}')
       end
     end
   end
 end
-    
-    puts '\nUnable to find an RSS feed for the following blogs:'
-puts '==================================================='
-unavailable.each do |b|
-  puts '#{b.name} | #{b.web_url}'
-end
-puts '==================================================='
 
     
-      # set version
-  content.sub! /(s\.version.*=\s+).*/, '\\1\'#{Rack::Protection::VERSION}\''
-    
-          default_options :escape => :html,
-        :escaper => defined?(EscapeUtils) ? EscapeUtils : self
-    
-        post('/', {'csrf_param' => token}, 'rack.session' => {:csrf => token})
-    expect(last_response).to be_ok
+      it 'should be true if the given selector matches the element' do
+    expect(@element.assert_matches_selector(:css, '.number')).to be true
   end
     
-        it 'redirects requests with sneaky encoded session cookies' do
-      get '/path', {}, 'HTTP_COOKIE' => 'rack.%73ession=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
-      expect(last_response).to be_redirect
-      expect(last_response.location).to eq('/path')
+        it 'should find disabled buttons when :all' do
+      expect(@session.find_button('Disabled button', disabled: :all).value).to eq('Disabled button')
     end
   end
     
-    LogStash::Bundler.setup!
-    
-        def execute
-      raise PluginManager::FileNotFoundError, 'Can't file local file #{local_file}' unless ::File.exist?(local_file)
-      raise PluginManager::InvalidPackError, 'Invalid format, the pack must be in zip format' unless valid_format?(local_file)
-    
-        # remove any version constrain from the Gemfile so the plugin(s) can be updated to latest version
-    # calling update without requirements will remove any previous requirements
-    plugins = plugins_to_update(previous_gem_specs_map)
-    # Skipping the major version validation when using a local cache as we can have situations
-    # without internet connection.
-    filtered_plugins = plugins.map { |plugin| gemfile.find(plugin) }
-      .compact
-      .reject { |plugin| REJECTED_OPTIONS.any? { |key| plugin.options.has_key?(key) } }
-      .each   { |plugin| gemfile.update(plugin.name) }
-    
-        desc 'Run one single machine acceptance test'
-    task :single, :machine do |t, args|
-      ENV['LS_VAGRANT_HOST']  = args[:machine]
-      exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/**/**/*_spec.rb']]))
-    end
+    Capybara::SpecHelper.spec '#find_by_id' do
+  before do
+    @session.visit('/with_html')
   end
+    
+        it 'doesn't find a download link if download is false' do
+      expect { @session.find_link('Download Me', download: false) }.to raise_error Capybara::ElementNotFound
+    end
+    
+    Rake::TestTask.new do |test|
+  test.verbose = true
+  test.libs << 'test'
+  test.libs << 'lib'
+  test.test_files = FileList['test/**/*_test.rb']
 end
+    
+          def self.each(offset = 0, limit = self.count, queue = :failed, class_name = nil, order = 'desc')
+        items = all(offset, limit, queue)
+        items = [items] unless items.is_a? Array
+        reversed = false
+        if order.eql? 'desc'
+          items.reverse!
+          reversed = true
+        end
+        items.each_with_index do |item, i|
+          if !class_name || (item['payload'] && item['payload']['class'] == class_name)
+            id = reversed ? (items.length - 1) + (offset - i) : offset + i
+            yield id, item
+          end
+        end
+      end
+    
+      if !defined?(RUBY_ENGINE) || RUBY_ENGINE != 'jruby'
+    it 'old signal handling just kills off the child' do
+      _worker_pid, child_pid, result = start_worker(0, false)
+      assert_nil result
+      assert_child_not_running child_pid
+    end
