e7f4a60b85c4e9ebe9397193560d9ae41de81572
SUNsung <weatherius@gmail.com>
2019-07-17 03:25:19 +0300

    Rewrite [507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py] File in: 2019-07-17 03:25:19

--- pyton/507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py
+++ pyton/507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py
@@ -1,117 +1,151 @@
 
         
-            def __init__(self, message_id, message, timestamp):
-        self.message_id = message_id
-        self.message = message
-        self.timestamp = timestamp
+        
+@pytest.mark.usefixtures('no_memoize', 'os_environ_pathsep')
+@pytest.mark.parametrize('path, pathsep', [
+    ('/foo:/bar:/baz:/foo/bar', ':'),
+    (r'C:\\foo;C:\\bar;C:\\baz;C:\\foo\\bar', ';')])
+def test_get_all_executables_pathsep(path, pathsep):
+    with patch('thefuck.utils.Path') as Path_mock:
+        get_all_executables()
+        Path_mock.assert_has_calls([call(p) for p in path.split(pathsep)], True)
     
-        def can_fit_in_spot(self, spot):
-        return spot.size == VehicleSize.LARGE
+        @pytest.mark.parametrize('side_effect, expected_info, warn', [
+        ([u'3.5.9'], u'Generic Shell 3.5.9', False),
+        ([OSError], u'Generic Shell', True),
+    ])
+    def test_info(self, side_effect, expected_info, warn, shell, mocker):
+        warn_mock = mocker.patch('thefuck.shells.generic.warn')
+        shell._get_version = mocker.Mock(side_effect=side_effect)
+        assert shell.info() == expected_info
+        assert warn_mock.called is warn
+        assert shell._get_version.called
+
     
-    fit_data = data[np.argsort(model.row_labels_)]
-fit_data = fit_data[:, np.argsort(model.column_labels_)]
-    
-    from sklearn import datasets, cluster
-from sklearn.feature_extraction.image import grid_to_graph
-    
-    # equal bins face
-regular_values = np.linspace(0, 256, n_clusters + 1)
-regular_labels = np.searchsorted(regular_values, face) - 1
-regular_values = .5 * (regular_values[1:] + regular_values[:-1])  # mean
-regular_face = np.choose(regular_labels.ravel(), regular_values, mode='clip')
-regular_face.shape = face.shape
-plt.figure(3, figsize=(3, 2.2))
-plt.imshow(regular_face, cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
-    
-    # Unevenly sized blobs
-X_filtered = np.vstack((X[y == 0][:500], X[y == 1][:100], X[y == 2][:10]))
-y_pred = KMeans(n_clusters=3,
-                random_state=random_state).fit_predict(X_filtered)
-    
-    # #############################################################################
-# Generate sample data
-np.random.seed(0)
-    
-        # 读取文件
-    for f in files_list:
-        txt = open(f).read()
-        # words = word_tokenize(txt)
-        words = tokenizer.tokenize(txt)
-        # creating inverted index data structure
-        for word in words:
-            word = word_clean(word)  # 单词清洗
-            if word not in index:
-                index[word] = {f}
-            else:
-                index[word].add(f)
+        def _get_history_line(self, command_script):
+        return u'- cmd: {}\n   when: {}\n'.format(command_script, int(time()))
     
     
-def elu(x):
-    '''指数线性单元'''
-    return tf.nn.elu(x)
+@for_app('pyenv')
+def match(command):
+    return 'pyenv: no such command' in command.output
     
-    
-def dense(x, n_unit, act_fn=relu, name=None, reuse=None):
-    '''全连接层
-    Input shape:  [batch_size, n_input]
-    Output shape: [batch_size, n_unit]
-    
-        return o
-    
-    import tensorflow as tf
-import keras.backend as K
-    
-    sum_ngrams = 0
-for s in sentences:
-    for w in s:
-        w = w.lower()
-        # from gensim.models._utils_any2vec import compute_ngrams
-        ret = compute_ngrams(w, min_ngrams, max_ngrams)
-        print(ret)
-        sum_ngrams += len(ret)
-'''
-['<h', 'he', 'el', 'll', 'lo', 'o>', '<he', 'hel', 'ell', 'llo', 'lo>', '<hel', 'hell', 'ello', 'llo>']
-['<w', 'wo', 'or', 'rl', 'ld', 'd>', '<wo', 'wor', 'orl', 'rld', 'ld>', '<wor', 'worl', 'orld', 'rld>']
-['<!', '!>', '<!>']
-['<i', 'i>', '<i>']
-['<a', 'am', 'm>', '<am', 'am>', '<am>']
-['<h', 'hu', 'ua', 'ay', 'y>', '<hu', 'hua', 'uay', 'ay>', '<hua', 'huay', 'uay>']
-['<.', '.>', '<.>']
-'''
-assert sum_ngrams == len(model.wv.vectors_ngrams)
-print(sum_ngrams)  # 57
-print()
-    
-            if start == end:
-            return path
-        shortest = None
-        for node in self.graph.get(start, []):
-            if node not in path:
-                newpath = self.find_shortest_path(node, end, path[:])
-                if newpath:
-                    if not shortest or len(newpath) < len(shortest):
-                        shortest = newpath
-        return shortest
-    
-    *TL;DR80
-Decouples an abstraction from its implementation.
+    The most similar choice to 'lis' is:
+    list
 '''
     
-        def __init__(self, text):
-        self._text = text
     
-        def test_car_adapter_shall_make_very_loud_noise(self):
-        car = Car()
-        car_adapter = Adapter(car, make_noise=car.make_noise)
-        noise = car_adapter.make_noise(10)
-        expected_noise = 'vroom!!!!!!!!!!'
+@eager
+def _parse_apt_get_and_cache_operations(help_text_lines):
+    is_commands_list = False
+    for line in help_text_lines:
+        line = line.decode().strip()
+        if is_commands_list:
+            if not line:
+                return
     
-        jim = Subscriber('jim', message_center)
-    jim.subscribe('cartoon')
-    jack = Subscriber('jack', message_center)
-    jack.subscribe('music')
-    gee = Subscriber('gee', message_center)
-    gee.subscribe('movie')
-    vani = Subscriber('vani', message_center)
-    vani.subscribe('movie')
-    vani.unsubscribe('movie')
+    import numpy as np
+    
+        # simulate file handle
+    json = '{'a': 'foo”', 'b': 'bar'}\n{'a': 'foo', 'b': 'bar'}\n'
+    json = StringIO(json)
+    result = read_json(json, lines=True)
+    expected = DataFrame([['foo\u201d', 'bar'], ['foo', 'bar']], columns=['a', 'b'])
+    assert_frame_equal(result, expected)
+    
+    import logging
+from tornado.curl_httpclient import CurlAsyncHTTPClient
+from tornado.simple_httpclient import SimpleAsyncHTTPClient
+from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line
+from tornado.web import RequestHandler, Application
+    
+    # These benchmarks are delicate.  They hit various fast-paths in the gen
+# machinery in order to stay synchronous so we don't need an IOLoop.
+# This removes noise from the results, but it's easy to change things
+# in a way that completely invalidates the results.
+    
+        def finish_tree(self, tree, filename):
+        if self.found_future_import:
+            return
+        if not isinstance(tree, pytree.Node):
+            # Empty files (usually __init__.py) show up as a single Leaf
+            # instead of a Node, so leave them alone
+            return
+        first_stmt = tree.children[0]
+        if is_docstring(first_stmt):
+            # Skip a line and add the import after the docstring
+            tree.insert_child(1, Newline())
+            pos = 2
+        elif first_stmt.prefix:
+            # No docstring, but an initial comment (perhaps a #! line).
+            # Transfer the initial comment to a new blank line.
+            newline = Newline()
+            newline.prefix = first_stmt.prefix
+            first_stmt.prefix = ''
+            tree.insert_child(0, newline)
+            pos = 1
+        else:
+            # No comments or docstring, just insert at the start
+            pos = 0
+        tree.insert_child(pos, self.new_future_import(None))
+        tree.insert_child(pos + 1, Newline())  # terminates the import stmt
+
+    
+    
+class OpenCoroutineHandler(TestWebSocketHandler):
+    def initialize(self, test, **kwargs):
+        super(OpenCoroutineHandler, self).initialize(**kwargs)
+        self.test = test
+        self.open_finished = False
+    
+    
+def _check_file(modify_times: Dict[str, float], path: str) -> None:
+    try:
+        modified = os.stat(path).st_mtime
+    except Exception:
+        return
+    if path not in modify_times:
+        modify_times[path] = modified
+        return
+    if modify_times[path] != modified:
+        gen_log.info('%s modified; restarting server', path)
+        _reload()
+    
+        def add_callback_from_signal(
+        self, callback: Callable, *args: Any, **kwargs: Any
+    ) -> None:
+        try:
+            self.asyncio_loop.call_soon_threadsafe(
+                self._run_callback, functools.partial(callback, *args, **kwargs)
+            )
+        except RuntimeError:
+            pass
+    
+    
+def _is_punctuation(char):
+  '''Checks whether `chars` is a punctuation character.'''
+  cp = ord(char)
+  # We treat all non-letter/number ASCII as punctuation.
+  # Characters such as '^', '$', and '`' are not in the Unicode
+  # Punctuation class but we treat them as punctuation anyways, for
+  # consistency.
+  if ((cp >= 33 and cp <= 47) or (cp >= 58 and cp <= 64) or
+      (cp >= 91 and cp <= 96) or (cp >= 123 and cp <= 126)):
+    return True
+  cat = unicodedata.category(char)
+  if cat.startswith('P'):
+    return True
+  return False
+
+    
+          # Only the last two dimensions are relevant (`seq_length` and `width`), so
+      # we broadcast among the first dimensions, which is typically just
+      # the batch size.
+      position_broadcast_shape = []
+      for _ in range(num_dims - 2):
+        position_broadcast_shape.append(1)
+      position_broadcast_shape.extend([seq_length, width])
+      position_embeddings = tf.reshape(position_embeddings,
+                                       position_broadcast_shape)
+      output += position_embeddings
