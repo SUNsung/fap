a94f1267b38ff3667abc25d302e2b0fb7c269439
SUNsung <weatherius@gmail.com>
2019-07-17 18:49:36 +0300

    Rewrite [48hrIFD4uv9lg48zgtdHKWFb.484KtOdM0Z8648U0a.rb] File in: 2019-07-17 18:49:35

--- ruby/48hrIFD4uv9lg48zgtdHKWFb.484KtOdM0Z8648U0a.rb
+++ ruby/48hrIFD4uv9lg48zgtdHKWFb.484KtOdM0Z8648U0a.rb
@@ -1,75 +1,108 @@
 
         
-              def self.category
-        :source_control
-      end
+          # localization is fast, but this allows us to avoid
+  # calling it in a loop which adds up
+  def self.translations
+    {
+      original_poster: I18n.t(:original_poster),
+      most_recent_poster: I18n.t(:most_recent_poster),
+      frequent_poster: I18n.t(:frequent_poster)
+    }
+  end
     
-          it 'adds publish_docset param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            publish_docset: true
-          )
-        end').runner.execute(:test)
+      def list_private_messages_tag(user)
+    list = private_messages_for(user, :all)
+    list = list.joins('JOIN topic_tags tt ON tt.topic_id = topics.id
+                      JOIN tags t ON t.id = tt.tag_id AND t.name = '#{@options[:tags][0]}'')
+    create_list(:private_messages, {}, list)
+  end
     
-          it 'sets the platform to iOS' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              platform: 'iOS'
-            )
-          end').runner.execute(:test)
+          request.env['DISCOURSE_RATE_LIMITERS'] = [limiter10, limiter60]
+      request.env['DISCOURSE_ASSET_RATE_LIMITERS'] = [limiter_assets10]
     
-          it 'handles no extension or extensions parameters' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.')
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
-      end
+        return can_not_modify_automatic if group.automatic
     
-        def help_default_value
-      return '#{self.default_value} *'.strip if self.default_value_dynamic
-      return '' if self.default_value.nil?
-      return '''' if self.default_value.instance_of?(String) && self.default_value.empty?
-      return ':#{self.default_value}' if self.default_value.instance_of?(Symbol)
+            css('pre').each do |node|
+          node['data-language'] = 'js'
+          node['data-language'] = node['class'][/language-(\w+)/, 1] if node['class']
+          node.content = node.content
+        end
     
-          it 'splits correctly' do
-        expected = [
-          'One',
-          'Two',
-          'Three',
-          'Four Token',
-          'Or',
-          'Newlines',
-          'Everywhere'
-        ]
-        expect(generator.split_keywords(keywords)).to eq(expected)
-      end
+        def normalized_path
+      path == '' ? '/' : path
+    end
+    
+            at_css('#module-header').tap do |node|
+          heading = at_css('.caption')
+          heading.name = 'h1'
+          node.before(heading)
+          node.before(node.children).remove
+        end
+    
+      describe 'regressions should not occur', :if => program_exists?('rpmbuild') do
+    before :each do
+      @tempfile_handle =
+      @target = Stud::Temporary.pathname
+      subject.name = 'name'
+      subject.version = '1.23'
+    end
+    
+      describe '#description' do
+    it_behaves_like :Default, description.gsub(/^#/, '').to_sym, 'no description given'
+    it_behaves_like :Mutator, description.gsub(/^#/, '').to_sym
+  end
+    
+        if @attributes.include?(:prefix)
+      installdir = staging_path(@attributes[:prefix])
+    else
+      installdir = staging_path
+    end
+    
+      # Take a flat package as input
+  def input(input_path)
+    # TODO: Fail if it's a Distribution pkg or old-fashioned
+    expand_dir = File.join(build_path, 'expand')
+    # expand_dir must not already exist for pkgutil --expand
+    safesystem('pkgutil --expand #{input_path} #{expand_dir}')
+    
+        # Make one file. The installscript can unpack itself.
+    `cat #{install_script} #{payload} > #{output_path}`
+    FileUtils.chmod('+x', output_path)
+  end
+    
+    When /^I turn off class caching$/ do
+  cd('.') do
+    file = 'config/environments/test.rb'
+    config = IO.read(file)
+    config.gsub!(%r{^\s*config.cache_classes.*$},
+                 'config.cache_classes = false')
+    File.open(file, 'w'){|f| f.write(config) }
+  end
+end
+    
+        # Returns the width and height in a format suitable to be passed to Geometry.parse
+    def to_s
+      s = ''
+      s << width.to_i.to_s if width > 0
+      s << 'x#{height.to_i}' if height > 0
+      s << modifier.to_s
+      s
+    end
+    
+              it 'raise an integrity error' do
+            is_expected.to raise_error(CarrierWave::IntegrityError)
+          end
+        end
+    
+    Given /^the class has a method called 'reverse' that reverses the contents of a file$/ do
+  @klass.class_eval do
+    def reverse
+      text = File.read(current_path)
+      File.open(current_path, 'w') { |f| f.write(text.reverse) }
     end
   end
 end
-
     
-          tests.each do |code, result|
-        if colorize_code_supported?
-          actual = with_term { IRB::Color.colorize_code(code, complete: true) }
-          assert_equal(result, actual, 'Case: colorize_code(#{code.dump}, complete: true)\nResult: #{humanized_literal(actual)}')
-    
-          def pretty_print(q)
-        q.group(2, '#<#{self.class}:', '>') {
-          q.breakable
-          q.text('#{event}@#{pos[0]}:#{pos[1]}')
-          q.breakable
-          q.text('token: ')
-          tok.pretty_print(q)
-          if message
-            q.breakable
-            q.text('message: ')
-            q.text(message)
-          end
-        }
+          included do
+        before :cache, :check_content_type_blacklist!
       end
-    
-      # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
