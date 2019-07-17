2971dc970b86bd9c8eb2adfa129f9f0bb36d533f
SUNsung <weatherius@gmail.com>
2019-07-17 23:59:16 +0300

    Rewrite [1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp] File in: 2019-07-17 23:59:16

--- cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
+++ cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
@@ -1,346 +1,194 @@
 
         
-            struct Margin {
-        Margin() : left(0), right(0), top(0), bottom(0) {}
-        Margin(size_t left_, size_t right_, size_t top_, size_t bottom_)
-            : left(left_), right(right_), top(top_), bottom(bottom_) {}
-    }
-    
-    
-    {    void operator() (const u8 * src0, const u8 * src1, u8 * dst) const
+            void KeyChord::Modifiers(Settings::KeyModifiers const& value)
     {
-        dst[0] = src0[0] | src1[0];
+        _modifiers = value;
     }
+    
+    int UniValue::get_int() const
+{
+    if (typ != VNUM)
+        throw std::runtime_error('JSON value is not an integer as expected');
+    int32_t retval;
+    if (!ParseInt32(getValStr(), &retval))
+        throw std::runtime_error('JSON integer out of range');
+    return retval;
+}
+    
+    // Return the name of the old info log file for 'dbname'.
+extern std::string OldInfoLogFileName(const std::string& dbname);
+    
+    namespace google {
+namespace protobuf {
+    }
+    }
+    
+    #define GOOGLE_CHECK_GET_FLOAT(arg, value, err)                        \
+    float value;                                            \
+    if (!CheckAndGetFloat(arg, &value)) {                   \
+      return err;                                          \
+    }                                                       \
+    
+    
+    {  void* mem = options_.block_alloc(size);
+  Block* b = new (mem) Block(size, last_block);
+  space_allocated_.fetch_add(size, std::memory_order_relaxed);
+  return b;
+}
+    
+    
+    { private:
+  class NewDeleteCapture {
+   public:
+    // TOOD(xiaofeng): Implement this for opensource protobuf.
+    void Hook() {}
+    void Unhook() {}
+    int alloc_count() { return 0; }
+    int free_count() { return 0; }
+  } capture_alloc;
 };
     
-            const f32* ln0 = idx_rm1 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rm1) : tmp;
-        const f32* ln1 = internal::getRowPtr(srcBase, srcStride, i);
-        const f32* ln2 = internal::getRowPtr(srcBase, srcStride, i + 1);
-        const f32* ln3 = idx_rp2 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rp2) : tmp;
-    
-    #ifndef CAROTENE_SRC_COMMON_HPP
-#define CAROTENE_SRC_COMMON_HPP
-    
     
     {
-    {         vst1q_u16(_dst + i, vcombine_u16(vline1_u16, vline2_u16));
-     }
-})
-#endif
+    {
+    {}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
     
-            size_t i = 0;
-        while(i + 4 <= size.width)
+    
+    {  format(
+      'static const int $constant_name$ = $number$;\n'
+      '$1$ ::$proto_ns$::internal::ExtensionIdentifier< $extendee$,\n'
+      '    ::$proto_ns$::internal::$type_traits$, $field_type$, $packed$ >\n'
+      '  ${2$$name$$}$;\n',
+      qualifier, descriptor_);
+}
+    
+    namespace atu = annotation_test_util;
+    
+      // Verify that we can open the file.  Note that this also has the side-effect
+  // of verifying that we are not canonicalizing away any non-existent
+  // directories.
+  std::unique_ptr<io::ZeroCopyInputStream> stream(OpenDiskFile(disk_file));
+  if (stream == NULL) {
+    return CANNOT_OPEN;
+  }
+    
+    
+    {	locked = false;
+}
+    
+    			for (int i = 0; i < mesh_merge.faces.size(); i++) {
+				if (mesh_merge.faces[i].inside)
+					continue;
+				for (int j = 0; j < 3; j++) {
+					result.faces.write[outside_count].vertices[j] = mesh_merge.points[mesh_merge.faces[i].points[j]];
+					result.faces.write[outside_count].uvs[j] = mesh_merge.faces[i].uvs[j];
+				}
+    }
+    
+    		} else if (p_event->is_action('ui_home')) {
+    
+    				Basis shape_inertia_tensor = shape->get_moment_of_inertia(mass).to_diagonal_matrix();
+				Transform shape_transform = get_shape_transform(i);
+				Basis shape_basis = shape_transform.basis.orthonormalized();
+    
+    	for (List<PropertyInfo>::Element *E = pinfo.front(); E; E = E->next()) {
+		const PropertyInfo &pi = E->get();
+    }
+    
+    
+    {	return p_length;
+}
+    
+    String InputEventJoypadButton::as_text() const {
+    }
+    
+    
+    {	ERR_FAIL_COND(p_align < 1);
+	mem_ptr = Memory::alloc_static(p_size + p_align, 'PoolAllocator()');
+	uint8_t *mem8 = (uint8_t *)mem_ptr;
+	uint64_t ofs = (uint64_t)mem8;
+	if (ofs % p_align)
+		mem8 += p_align - (ofs % p_align);
+	create_pool(mem8, p_size, p_max_entries);
+	needs_locking = p_needs_locking;
+	align = p_align;
+}
+    
+    
+    { private:
+  std::mutex mutex_;
+  std::list<std::weak_ptr<TimerTask>> task_list_;
+};
+    
+    #include 'modules/common/proto/pnc_point.pb.h'
+#include 'modules/planning/proto/planning.pb.h'
+    
+    
+    {  bool is_parked = is_on_parking_lane || is_at_road_edge;
+  return is_parked && obstacle->IsStatic();
+}
+    
+      // set scenario_type in PlanningContext
+  auto* scenario = PlanningContext::Instance()
+                       ->mutable_planning_status()
+                       ->mutable_scenario();
+  scenario->Clear();
+  scenario->set_scenario_type(scenario_type());
+    
+    
+    {  ScenarioConfig config;
+  EXPECT_TRUE(apollo::cyber::common::GetProtoFromFile(
+      FLAGS_scenario_valet_parking_config_file, &config));
+  ScenarioContext context;
+  scenario_.reset(new ValetParkingScenario(config, &context));
+  EXPECT_EQ(scenario_->scenario_type(), ScenarioConfig::VALET_PARKING);
+}
+    
+    std::unique_ptr<Stage> BareIntersectionUnprotectedScenario::CreateStage(
+    const ScenarioConfig::StageConfig& stage_config) {
+  if (s_stage_factory_.Empty()) {
+    RegisterStages();
+  }
+  auto ptr = s_stage_factory_.CreateObjectOrNull(stage_config.stage_type(),
+                                                 stage_config);
+  if (ptr) {
+    ptr->SetContext(&context_);
+  }
+  return ptr;
+}
+    
+    #include <string>
+#include <openpose/core/macros.hpp>
+#include <openpose/core/point.hpp>
+    
+    
+    {        if (err != NULL) {
+            *err = error;
+        }
+    }
+    
+    namespace op
+{
+    const auto HAND_MAX_HANDS = 2*POSE_MAX_PEOPLE;
+    }
+    
+            Array<long long> extractIdsLockThread(const Array<float>& poseKeypoints, const cv::Mat& cvMatInput,
+                                              const unsigned long long imageViewIndex,
+                                              const long long frameId);
+    
+        template<typename T>
+    std::string Rectangle<T>::toString() const
+    {
+        try
         {
-            size_t lim = std::min(i + DOT_FLOAT_BLOCKSIZE, size.width) - 4;
-            float32x4_t v_sum = vdupq_n_f32(0.0f);
+            return '[' + std::to_string(x) + ', ' + std::to_string(y) + ', ' + std::to_string(width) + ', ' + std::to_string(height) + ']';
+        }
+        catch (const std::exception& e)
+        {
+            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
+            return '';
+        }
     }
-    
-            cornerpos[-1] = ncorners;
-    
-                tc = vreinterpretq_s16_u16(vsubl_u8(x4, x2));
-            tnext = vreinterpretq_s16_u16(vsubl_u8(x2, x0));
-            tnext2 = vsubq_s16(tc, tnext);
-    
-    namespace caffe2 {
-namespace {
-    }
-    }
-    
-    ```
-    
-    
-    {
-    {} // namespace
-} // namespace caffe2
-
-    
-              vector<TensorShape> out(1);
-          switch (order) {
-            case StorageOrder::NCHW:
-              out[0] = CreateTensorShape(
-                  vector<int>{N, C * kernel_h * kernel_w, out_h, out_w},
-                  TensorProto::FLOAT);
-              break;
-            case StorageOrder::NHWC:
-              out[0] = CreateTensorShape(
-                  vector<int>{N, out_h, out_w, kernel_h * kernel_w * C},
-                  TensorProto::FLOAT);
-              break;
-            default:
-              CAFFE_THROW('Unknown storage order: ', order);
-          }
-    
-    REGISTER_CUDA_OPERATOR(LC1D, LocallyConnectedOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(
-    LC1DGradient,
-    LocallyConnectedGradientOp<float, CUDAContext>);
-    
-      kFullType = 1,
-    
-    #endif  // STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
-
-    
-      // Create a reader that will return log records from '*file'.
-  // '*file' must remain live while this Reader is in use.
-  //
-  // If 'reporter' is non-null, it is notified whenever some data is
-  // dropped due to a detected corruption.  '*reporter' must remain
-  // live while this Reader is in use.
-  //
-  // If 'checksum' is true, verify checksums if available.
-  //
-  // The Reader will start reading at the first record located at physical
-  // position >= initial_offset within the file.
-  Reader(SequentialFile* file, Reporter* reporter, bool checksum,
-         uint64_t initial_offset);
-    
-    template <typename Key, class Comparator>
-inline void SkipList<Key, Comparator>::Iterator::Prev() {
-  // Instead of using explicit 'prev' links, we just search for the
-  // last node that falls before key.
-  assert(Valid());
-  node_ = list_->FindLessThan(node_->key);
-  if (node_ == list_->head_) {
-    node_ = nullptr;
-  }
-}
-    
-    #include 'db/dbformat.h'
-#include 'leveldb/cache.h'
-#include 'leveldb/table.h'
-#include 'port/port.h'
-    
-    namespace leveldb {
-    }
-    
-    inline Status::Status(const Status& rhs) {
-  state_ = (rhs.state_ == nullptr) ? nullptr : CopyState(rhs.state_);
-}
-inline Status& Status::operator=(const Status& rhs) {
-  // The following condition catches both aliasing (when this == &rhs),
-  // and the common case where both rhs and *this are ok.
-  if (state_ != rhs.state_) {
-    delete[] state_;
-    state_ = (rhs.state_ == nullptr) ? nullptr : CopyState(rhs.state_);
-  }
-  return *this;
-}
-inline Status& Status::operator=(Status&& rhs) noexcept {
-  std::swap(state_, rhs.state_);
-  return *this;
-}
-    
-      // Calls (*handle_result)(arg, ...) with the entry found after a call
-  // to Seek(key).  May not make such a call if filter policy says
-  // that key is not present.
-  Status InternalGet(const ReadOptions&, const Slice& key, void* arg,
-                     void (*handle_result)(void* arg, const Slice& k,
-                                           const Slice& v));
-    
-      // Advanced operation: flush any buffered key/value pairs to file.
-  // Can be used to ensure that two adjacent entries never live in
-  // the same data block.  Most clients should not need to use this method.
-  // REQUIRES: Finish(), Abandon() have not been called
-  void Flush();
-    
-    namespace leveldb {
-    }
-    
-    static const int kBlockSize = 4096;
-    
-    class Arena {
- public:
-  Arena();
-    }
-    
-    namespace leveldb {
-    }
-    
-    /* QuadricNormal */
-#define GLU_SMOOTH                         100000
-#define GLU_FLAT                           100001
-#define GLU_NONE                           100002
-    
-    void PSOutputDev::updateCharSpace(GfxState *state) {
-  writePSFmt('{0:.6g} Tc\n', state->getCharSpace());
-}
-    
-      GBool inType3Char;		// inside a Type 3 CharProc
-  GooString *t3String;		// Type 3 content string
-  double t3WX, t3WY,		// Type 3 character parameters
-         t3LLX, t3LLY, t3URX, t3URY;
-  GBool t3Cacheable;		// cleared if char is not cacheable
-  GBool t3NeedsRestore;		// set if a 'q' operator was issued
-  GBool forceRasterize;		// forces the page to be rasterized into a image before printing
-  GBool displayText;		// displayText
-    
-      number = index - base + interval->first;
-  switch (interval->style) {
-  case Interval::Arabic:
-    snprintf (buffer, sizeof(buffer), '%d', number);
-    number_string.append(buffer);
-    break;
-  case Interval::LowercaseRoman:
-    toRoman(number, &number_string, gFalse);
-    break;
-  case Interval::UppercaseRoman:
-    toRoman(number, &number_string, gTrue);
-    break;
-  case Interval::UppercaseLatin:
-  case Interval::LowercaseLatin:
-    number = 0;
-    break;
-  case Interval::None:
-    break;
-  }
-    
-    class Parser {
-public:
-    }
-    
-      //----- special access
-    
-      fileName = NULL;
-  samplingRate = 0.0;
-  channels = 1;
-  bitsPerSample = 8;
-  encoding = soundRaw;
-    
-        fontsrc = new SplashFontSrc;
-    if (fileName)
-      fontsrc->setFile(fileName, gFalse);
-    else
-      fontsrc->setBuf(tmpBuf, tmpBufLen, gTrue);
-    
-    #endif
-    
-      // put and get from non-default column family
-  s = db->Put(WriteOptions(), handles[1], Slice('key'), Slice('value'));
-  assert(s.ok());
-  std::string value;
-  s = db->Get(ReadOptions(), handles[1], Slice('key'), &value);
-  assert(s.ok());
-    
-      MyFilter filter;
-    
-      delete txn;
-  // Clear snapshot from read options since it is no longer valid
-  read_options.snapshot = nullptr;
-  snapshot = nullptr;
-    
-    #include <cstdio>
-#include <string>
-    
-      // Lookup page cache by page identifier
-  //
-  // page_key   Page identifier
-  // buf        Buffer where the data should be copied
-  // size       Size of the page
-  virtual Status Lookup(const Slice& key, std::unique_ptr<char[]>* data,
-                        size_t* size) = 0;
-    
-     private:
-  void Init(
-      const std::string& lua_script,
-      const std::vector<std::shared_ptr<RocksLuaCustomLibrary>>& libraries) {
-    if (lua_state_) {
-      luaL_openlibs(lua_state_);
-      for (const auto& library : libraries) {
-        luaL_openlib(lua_state_, library->Name(), library->Lib(), 0);
-        library->CustomSetup(lua_state_);
-      }
-      luaL_dostring(lua_state_, lua_script.c_str());
-    }
-  }
-    
-    
-    {}  // namespace rocksdb
-    
-    #endif  // ROCKSDB_LITE
-
-    
-    /*!
-    Tests model's implementation of QAbstractItemModel::parent()
- */
-void ModelTest::parent()
-{
-//     qDebug() << 'p';
-    // Make sure the model won't crash and will return an invalid QModelIndex
-    // when asked for the parent of an invalid index.
-    QVERIFY( model->parent ( QModelIndex() ) == QModelIndex() );
-    }
-    
-    QString ServerConfig::keysPattern() const
-{
-    return param<QString>('keys_pattern', QString(DEFAULT_KEYS_GLOB_PATTERN));
-}
-    
-        /* Advanced settings */
-    Q_PROPERTY(QString keysPattern READ keysPattern WRITE setKeysPattern)
-    Q_PROPERTY(QString namespaceSeparator READ namespaceSeparator WRITE setNamespaceSeparator)
-    Q_PROPERTY(uint executeTimeout READ executeTimeout WRITE setExecutionTimeout)
-    Q_PROPERTY(uint connectionTimeout READ connectionTimeout WRITE setConnectionTimeout)
-    Q_PROPERTY(bool luaKeysLoading READ luaKeysLoading WRITE setLuaKeysLoading)
-    Q_PROPERTY(bool overrideClusterHost READ overrideClusterHost WRITE setClusterHostOverride)
-    Q_PROPERTY(uint databaseScanLimit READ databaseScanLimit WRITE setDatabaseScanLimit)
-    
-    void SortFilterProxyModel::setSortRole(const QByteArray &role)
-{
-    if (m_sortRole != role) {
-        m_sortRole = role;
-        if (m_complete)
-            QSortFilterProxyModel::setSortRole(roleKey(role));
-    }
-}
-    
-      QObject::connect(m_connection.data(), &RedisClient::Connection::connected,
-                   [this, weekPointer]() {
-                     if (!weekPointer) {
-                       return;
-                     }
-    }
-    
-    
-    {  foo_.Bar2(1, 3);  // Mismatch arguments and mismatch With()
-  foo_.Bar2(2, 1);
-}
-    
-    // Step 3. Call RUN_ALL_TESTS() in main().
-//
-// We do this by linking in src/gtest_main.cc file, which consists of
-// a main() function which calls RUN_ALL_TESTS() for us.
-//
-// This runs all the tests you've defined, prints the result, and
-// returns 0 if successful, or 1 otherwise.
-//
-// Did you notice that we didn't register the tests?  The
-// RUN_ALL_TESTS() macro magically knows about all the tests we
-// defined.  Isn't this convenient?
-
-    
-    TEST(GTestMainTest, ShouldSucceed) {
-}
-    
-    TEST(NoFatalFailureTest, AssertNoFatalFailure) {
-  ASSERT_NO_FATAL_FAILURE(;);
-  ASSERT_NO_FATAL_FAILURE(SUCCEED());
-  ASSERT_NO_FATAL_FAILURE(Subroutine());
-  ASSERT_NO_FATAL_FAILURE({ SUCCEED(); });
-}
-    
-    // Define a matcher that matches a value that evaluates in boolean
-// context to true.  Useful for types that define 'explicit operator
-// bool' operators and so can't be compared for equality with true
-// and false.
-MATCHER(IsTrue, negation ? 'is false' : 'is true') {
-  return static_cast<bool>(arg);
-}
-    
-    
-    {  EXPECT_EQ(3, c.Decrement());
-}
-    
-    TEST(LosslessArithmeticConvertibleTest, FloatingPointToBool) {
-  EXPECT_FALSE((LosslessArithmeticConvertible<float, bool>::value));
-  EXPECT_FALSE((LosslessArithmeticConvertible<double, bool>::value));
-}
