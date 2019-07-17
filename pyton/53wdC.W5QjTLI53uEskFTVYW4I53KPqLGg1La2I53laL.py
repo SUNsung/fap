2206baf5aa9147625c322c7aca1227a965ec14d3
SUNsung <weatherius@gmail.com>
2019-07-17 06:27:29 +0300

    Rewrite [53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py] File in: 2019-07-17 06:27:29

--- pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
+++ pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
@@ -1,182 +1,62 @@
 
         
-                self.A = str(A)
-        self.B = str(B)
+            for category, entries in sections.items():
+        if sorted(entries) != entries:
+            add_error(section_line_num[category], '{} section is not in alphabetical order'.format(category))
     
-        return score
-
+            if aa_policy_id and aa_policy_id != current_aa_policy_id:
+            self._modify_aa_policy(
+                self.clc,
+                self.module,
+                acct_alias,
+                server.id,
+                aa_policy_id)
+            changed = True
+        return changed
     
-            a += a
-        b >>= 1
-    
-        def __init__(self, msg, with_headers=True, with_body=True,
-                 on_body_chunk_downloaded=None):
-        '''
-        :param msg: a :class:`models.HTTPMessage` subclass
-        :param with_headers: if `True`, headers will be included
-        :param with_body: if `True`, body will be included
-    
-    
-def test_credentials_in_url_auth_flag_has_priority(httpbin_both):
-    '''When credentials are passed in URL and via -a at the same time,
-     then the ones from -a are used.'''
-    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
-                   auth='user:wrong')
-    r = http('--auth=user:password', 'GET', url)
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
-    
-        return inner
+    # balancer member attributes extraction regexp:
+EXPRESSION = r'(b=([\w\.\-]+)&w=(https?|ajp|wss?|ftp|[sf]cgi)://([\w\.\-]+):?(\d*)([/\w\.\-]*)&?[\w\-\=]*)'
+# Apache2 server version extraction regexp:
+APACHE_VERSION_EXPRESSION = r'SERVER VERSION: APACHE/([\d.]+)'
     
     
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
-
-    
-        return out
-    
-                        resp = HTTPResponse.from_httplib(
-                        r,
-                        pool=conn,
-                        connection=low_conn,
-                        preload_content=False,
-                        decode_content=False
-                    )
-                except:
-                    # If we hit any problems here, clean up the connection.
-                    # Then, reraise so that we can handle the actual exception.
-                    low_conn.close()
-                    raise
-    
-    from requests.help import info
-    
-    
-class TestUnquoteHeaderValue:
-    
-            # Decode unicode from given encoding.
-        try:
-            content = str(self.content, encoding, errors='replace')
-        except (LookupError, TypeError):
-            # A LookupError is raised if the encoding was not found which could
-            # indicate a misspelling or similar mistake.
-            #
-            # A TypeError can be raised if encoding is None
-            #
-            # So we try blindly encoding.
-            content = str(self.content, errors='replace')
-    
-        # Redirection.
-    300: ('multiple_choices',),
-    301: ('moved_permanently', 'moved', '\\o-'),
-    302: ('found',),
-    303: ('see_other', 'other'),
-    304: ('not_modified',),
-    305: ('use_proxy',),
-    306: ('switch_proxy',),
-    307: ('temporary_redirect', 'temporary_moved', 'temporary'),
-    308: ('permanent_redirect',
-          'resume_incomplete', 'resume',),  # These 2 to be removed in 3.0
-    
-        :param url: URL for the new :class:`Request` object.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    
-    # Documents to append as an appendix to all manuals.
-# texinfo_appendices = []
-    
-        function_name = module.params.get('function_name')
-    if function_name:
-        params = dict()
-        if module.params.get('max_items'):
-            params['MaxItems'] = module.params.get('max_items')
-    
-    - oneandone_firewall_policy:
-    auth_token: oneandone_private_api_key
-    state: update
-    firewall_policy: ansible-firewall-policy
-    name: ansible-firewall-policy-updated
-    description: Testing creation of firewall policies with ansible - updated
-    
-            elif desired_state == 'enabled':
-            if current_state == HOST_ABSENT:
-                self.allocate_host()
-                host = self.get_host_by_name(host_name)
-                self.wait_for_host_state(host, [HOST_STATES.MONITORED])
-            elif current_state in [HOST_STATES.DISABLED, HOST_STATES.OFFLINE]:
-                if one.host.status(host.ID, HOST_STATUS.ENABLED):
-                    self.wait_for_host_state(host, [HOST_STATES.MONITORED])
-                    result['changed'] = True
-                else:
-                    self.fail(msg='could not enable host')
-            elif current_state in [HOST_STATES.MONITORED]:
-                pass
-            else:
-                self.fail(msg='unknown host state %s, cowardly refusing to change state to enable' % current_state_name)
-    
-        for (index, rule) in enumerate(desired_rules):
-        try:
-            if rule != current_rules[index]:
-                updates.append((index, rule))
-        except IndexError:
-            additions.append(rule)
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ipa import IPAClient, ipa_argument_spec
-from ansible.module_utils._text import to_native
-    
-        module = AnsibleModule(
+def main():
+    module = AnsibleModule(
         argument_spec=dict(
-            component=dict(required=True, aliases=['name']),
-            version=dict(required=True),
-            token=dict(required=True, no_log=True),
-            state=dict(required=True, choices=['started', 'finished', 'failed']),
-            hosts=dict(required=False, default=[socket.gethostname()], aliases=['host']),
-            env=dict(required=False),
-            owner=dict(required=False),
-            description=dict(required=False),
-            message=dict(required=False),
-            source_system=dict(required=False, default='ansible'),
-            validate_certs=dict(default='yes', type='bool'),
-            url=dict(required=False, default='https://api.bigpanda.io'),
+            config=dict(required=False),
+            name=dict(required=True),
+            password=dict(required=False, no_log=True),
+            state=dict(required=False, choices=['present', 'absent'], default='present'),
+            enabled=dict(required=False, type='bool'),
+            token=dict(required=False, no_log=True),
+            url=dict(required=False, default='http://localhost:8080'),
+            user=dict(required=False)
         ),
+        mutually_exclusive=[
+            ['password', 'token'],
+            ['config', 'enabled'],
+        ],
         supports_check_mode=True,
     )
     
-    EXAMPLES = '''
-- honeybadger_deployment:
-    token: AAAAAA
-    environment: staging
-    user: ansible
-    revision: b6826b8
-    repo: 'git@github.com:user/repo.git'
-'''
+        merged_items = collections.OrderedDict()
     
-        if model.get('error'):
-        module.fail_json(msg='error checking device: %s' % model.get('error'))
+    - name: Get rid of an old entry
+  ldap_entry:
+    dn: ou=stuff,dc=example,dc=com
+    state: absent
+    server_uri: ldap://localhost/
+    bind_dn: cn=admin,dc=example,dc=com
+    bind_pw: password
     
-            This function first checks for save errors, if none are found,
-        all configuration changes made will be saved. According to the
-        function parameters. If an exception is raised, a new checkpoint
-        was not created.
+        # --- run command ---
+    try:
+        ipmi_cmd = command.Command(
+            bmc=name, userid=user, password=password, port=port
+        )
+        module.debug('ipmi instantiated - name: '%s'' % name)
     
-    Certbot will emit a warning if it detects that the credentials file can be
-accessed by other users on your system. The warning reads 'Unsafe permissions
-on credentials configuration file', followed by the path to the credentials
-file. This warning will be emitted each time Certbot uses the credentials file,
-including for renewal, and cannot be silenced except by addressing the issue
-(e.g., by using a command like ``chmod 600`` to restrict access to the file).
+    
+FILEPATH = os.path.join(
+    os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'blns.txt')
+'''Path to the file'''
