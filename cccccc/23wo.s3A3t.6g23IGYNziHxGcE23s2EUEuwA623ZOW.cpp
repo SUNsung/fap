6a45ede4fadc575b07ebba6b83de122cd6330869
SUNsung <weatherius@gmail.com>
2019-07-17 14:22:00 +0300

    Rewrite [23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp] File in: 2019-07-17 14:21:59

--- cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
+++ cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
@@ -1,303 +1,128 @@
 
         
-        CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
-  return new DefaultCache(CBs);
-}
-    
-    void CacheImpl::removeAll() {
-  cache_remove_all(static_cast<cache_t*>(Impl));
-}
-    
-      if (!wasInline) delete[] oldBegin;
-    
-    namespace {
-  // Quasi-lexicographic order: string length first, then string data.
-  // Since we don't care about the actual length, we can use this, which
-  // lets us ignore the string data a larger proportion of the time.
-  struct SortByLengthComparator {
-    bool operator()(StringRef lhs, StringRef rhs) const {
-      return (lhs.size() < rhs.size() ||
-              (lhs.size() == rhs.size() && lhs < rhs));
-    }
-  };
-} // end anonymous namespace
-    
-      static CFPointeeInfo forRecord(bool isConst, const clang::RecordDecl *decl) {
-    assert(decl);
-    CFPointeeInfo info;
-    info.IsValid = true;
-    info.IsConst = isConst;
-    info.Decl = decl;
-    return info;
-  }
-    
-    /// Translate the given operator character into its mangled form.
-///
-/// Current operator characters:   @/=-+*%<>!&|^~ and the special operator '..'
-char Mangle::translateOperatorChar(char op) {
-  switch (op) {
-    case '&': return 'a'; // 'and'
-    case '@': return 'c'; // 'commercial at sign'
-    case '/': return 'd'; // 'divide'
-    case '=': return 'e'; // 'equal'
-    case '>': return 'g'; // 'greater'
-    case '<': return 'l'; // 'less'
-    case '*': return 'm'; // 'multiply'
-    case '!': return 'n'; // 'negate'
-    case '|': return 'o'; // 'or'
-    case '+': return 'p'; // 'plus'
-    case '?': return 'q'; // 'question'
-    case '%': return 'r'; // 'remainder'
-    case '-': return 's'; // 'subtract'
-    case '~': return 't'; // 'tilde'
-    case '^': return 'x'; // 'xor'
-    case '.': return 'z'; // 'zperiod' (the z is silent)
-    default:
-      return op;
-  }
-}
-    
-    bool ArgsToFrontendInputsConverter::readInputFilesFromCommandLine() {
-  bool hadDuplicates = false;
-  for (const Arg *A :
-       Args.filtered(options::OPT_INPUT, options::OPT_primary_file)) {
-    hadDuplicates = addFile(A->getValue()) || hadDuplicates;
-  }
-  return false; // FIXME: Don't bail out for duplicates, too many tests depend
-  // on it.
-}
-    
-      /// Returns the newly set-up FrontendInputsAndOutputs, as well as a set of
-  /// any unused primary files (those that do not correspond to an input).
-  std::pair<FrontendInputsAndOutputs, std::set<StringRef>>
-  createInputFilesConsumingPrimaries(std::set<StringRef> primaryFiles);
-    
-        f64 dotProduct(const Size2D &size,
-                   const f32 * src0Base, ptrdiff_t src0Stride,
-                   const f32 * src1Base, ptrdiff_t src1Stride);
-    
-        struct Size2D {
-        Size2D() : width(0), height(0) {}
-        Size2D(size_t width_, size_t height_) : width(width_), height(height_) {}
-    }
-    
-    struct BitwiseAnd
-{
-    typedef u8 type;
-    }
-    
-            //tail
-        if (lookRight == 0 || i != width)
-        {
-            uint8x8_t tail0 = vld1_u8(src + (width - 9));//can't get left 1 pixel another way if width==8*k+1
-            uint8x8_t tail2 = vtbl1_u8(vld1_u8(src + (width - 8 + lookRight)), vtmask);
-            uint8x8_t tail1 = vext_u8(vreinterpret_u8_u64(vshl_n_u64(vreinterpret_u64_u8(tail0), 8*6)), tail2, 7);
-    }
+        #include 'content/nw/src/api/bindings_common.h'
     
     
-    {
-    {        for (; sj < size.width; ++sj, syj += 2, dj += 4)
-        {
-            dst[dj] = srcy[syj];
-            dst[dj + 1] = srcu[sj];
-            dst[dj + 2] = srcy[syj + 1];
-            dst[dj + 3] = srcv[sj];
-        }
-    }
-#else
-    (void)size;
-    (void)srcyBase;
-    (void)srcyStride;
-    (void)srcuBase;
-    (void)srcuStride;
-    (void)srcvBase;
-    (void)srcvStride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
-}
-    
-    #ifdef CAROTENE_NEON
-    if (shift >= 16)
-    {
-        if (cpolicy == CONVERT_POLICY_WRAP)
-        {
-            size_t roiw16 = size.width >= 15 ? size.width - 15 : 0;
-            size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-            int16x8_t v_zero = vdupq_n_s16(0);
-    }
-    }
-    
-    
-    {} // namespace CAROTENE_NS
-
-    
-                vst1_u8((u8 *)&mask, vmovn_u16(vcombine_u16(vmovn_u32(v_mask0), vmovn_u32(v_mask1))));
-    
-                    __asm__ __volatile__ (
-                    'vld4.32 {d0, d2, d4, d6}, [%[in0]]!                      \n\t'
-                    'vld4.32 {d1, d3, d5, d7}, [%[in0]]                       \n\t'
-                    'vld4.32 {d8, d10, d12, d14}, [%[in4]]!                   \n\t'
-                    'vld4.32 {d9, d11, d13, d15}, [%[in4]]                    \n\t'
-                    'vadd.i32 q0, q4                                          \n\t'
-                    'vadd.i32 q1, q5                                          \n\t'
-                    'vadd.i32 q2, q6                                          \n\t'
-                    'vadd.i32 q3, q7                                          \n\t'
-                    'vld4.32 {d16, d18, d20, d22}, [%[in1]]!                  \n\t'
-                    'vld4.32 {d17, d19, d21, d23}, [%[in1]]                   \n\t'
-                    'vld4.32 {d8, d10, d12, d14}, [%[in3]]!                   \n\t'
-                    'vld4.32 {d9, d11, d13, d15}, [%[in3]]                    \n\t'
-                    'vadd.i32 q4, q8                                          \n\t'
-                    'vadd.i32 q5, q9                                          \n\t'
-                    'vadd.i32 q6, q10                                         \n\t'
-                    'vadd.i32 q7, q11                                         \n\t'
-                    'vld4.32 {d16, d18, d20, d22}, [%[in2],:256]              \n\t'
-                    'vld4.32 {d17, d19, d21, d23}, [%[in22],:256]             \n\t'
-                    'vmla.i32 q0, q4, %q[c4]                                  \n\t'
-                    'vmla.i32 q1, q5, %q[c4]                                  \n\t'
-                    'vmla.i32 q2, q6, %q[c4]                                  \n\t'
-                    'vmla.i32 q3, q7, %q[c4]                                  \n\t'
-                    'vmla.i32 q0, q8, %q[c6]                                  \n\t'
-                    'vmla.i32 q1, q9, %q[c6]                                  \n\t'
-                    'vmla.i32 q2, q10, %q[c6]                                 \n\t'
-                    'vmla.i32 q3, q11, %q[c6]                                 \n\t'
-                    'vrshrn.i32 d8, q0, #8                                    \n\t'
-                    'vrshrn.i32 d9, q1, #8                                    \n\t'
-                    'vrshrn.i32 d10, q2, #8                                   \n\t'
-                    'vrshrn.i32 d11, q3, #8                                   \n\t'
-                   'vst4.16 {d8-d11}, [%[out]]                                \n\t'
-                    : [in0] '=r' (lidx0),
-                      [in1] '=r' (lidx1),
-                      [in3] '=r' (lidx3),
-                      [in4] '=r' (lidx4)
-                    : [out] 'r' (dst + x),
-                      '0' (lidx0),
-                      '1' (lidx1),
-                      '2' (lidx3),
-                      '3' (lidx4),
-                      [in2] 'r' (lane + x),
-                      [in22] 'r' (lane + x + 4*2),
-                      [c4] 'w' (vc4s32), [c6] 'w' (vc6s32)
-                    : 'd0','d1','d2','d3','d4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23'
-                );
-*/
-            for (; x <= colsn - 4; x += 4)
-            {
-                internal::prefetch(lane + x);
-    }
-    
-    #include <carotene/definitions.hpp>
-    
-    // ##########################################################
-    
-    workspace.ResetWorkspace()
-    
-    <details>
-    
-    namespace {
-float sigmoid(const float x) {
-  if (x >= 0) {
-    return 1. / (1. + exp(-x));
-  } else {
-    const float exp_x = exp(x);
-    return exp_x / (1 + exp_x);
-  }
-}
-} // namespace
-    
-    namespace caffe2 {
-    }
-    
-    void CensusClientCallData::OnDoneRecvTrailingMetadataCb(void* user_data,
-                                                        grpc_error* error) {
-  grpc_call_element* elem = reinterpret_cast<grpc_call_element*>(user_data);
-  CensusClientCallData* calld =
-      reinterpret_cast<CensusClientCallData*>(elem->call_data);
-  GPR_ASSERT(calld != nullptr);
-  if (error == GRPC_ERROR_NONE) {
-    GPR_ASSERT(calld->recv_trailing_metadata_ != nullptr);
-    FilterTrailingMetadata(calld->recv_trailing_metadata_,
-                           &calld->elapsed_time_);
-  }
-  GRPC_CLOSURE_RUN(calld->initial_on_done_recv_trailing_metadata_,
-                   GRPC_ERROR_REF(error));
-}
-    
-     private:
-  // Parses the next field from the incoming buffer and stores the parsed value
-  // in a GrpcTraceContext struct.  If it does not recognize the field ID it
-  // will return 0, otherwise it returns the number of bytes read.
-  static size_t ParseField(absl::string_view buf, GrpcTraceContext* tc) {
-    // TODO: Add support for multi-byte field IDs.
-    if (buf.empty()) {
-      return 0;
-    }
-    // Field ID is always the first byte in a field.
-    uint32_t field_id = buf[0];
-    size_t bytes_read = kFieldIdSize;
-    switch (field_id) {
-      case kTraceIdField:
-        bytes_read += kTraceIdSize;
-        if (bytes_read > buf.size()) {
-          return 0;
-        }
-        memcpy(tc->trace_id, &buf[kFieldIdSize],
-               opencensus::trace::TraceId::kSize);
-        break;
-      case kSpanIdField:
-        bytes_read += kSpanIdSize;
-        if (bytes_read > buf.size()) {
-          return 0;
-        }
-        memcpy(tc->span_id, &buf[kFieldIdSize],
-               opencensus::trace::SpanId::kSize);
-        break;
-      case kTraceOptionsField:
-        bytes_read += kTraceOptionsSize;
-        if (bytes_read > buf.size()) {
-          return 0;
-        }
-        memcpy(tc->trace_options, &buf[kFieldIdSize],
-               opencensus::trace::TraceOptions::kSize);
-        break;
-      default:  // Invalid field ID
-        return 0;
-    }
-    }
-    
-    #ifdef GPR_APPLE
-    
-    const std::set<PerBalancerStore*>* PerHostStore::GetAssignedStores(
-    const grpc::string& lb_id) const {
-  auto it = assigned_stores_.find(lb_id);
-  if (it == assigned_stores_.end()) return nullptr;
-  return &(it->second);
-}
-    
-    TEST(MemEnvTest, LargeWrite) {
-  const size_t kWriteSize = 300 * 1024;
-  char* scratch = new char[kWriteSize * 2];
-    }
-    
-    
-    { private:
-  const FilterPolicy* policy_;
-  const char* data_;    // Pointer to filter data (at block-start)
-  const char* offset_;  // Pointer to beginning of offset array (at block-end)
-  size_t num_;          // Number of entries in offset array
-  size_t base_lg_;      // Encoding parameter (see kFilterBaseLg in .cc file)
+    {protected:
+  BaseEvent(){}
+  virtual ~BaseEvent(){}
 };
     
-    namespace leveldb {
+       bool HasIcon(int command_id) override;
+    
+    void Menu::Append(MenuItem* menu_item) {
+  menu_items.push_back(menu_item);
+  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    menu_item->UpdateKeys(gtk_accel_group);
+  }
+  gtk_menu_shell_append(GTK_MENU_SHELL(menu_), menu_item->menu_item_);
+}
+    
+      if (menu_item->submenu_)
+    menu_model_->InsertSubMenuAt(pos, menu_item->id(), menu_item->label_,
+                                 menu_item->submenu_->menu_model_.get());
+  else if (menu_item->type_ == 'normal')
+    menu_model_->InsertItemAt(pos, menu_item->id(), menu_item->label_);
+  else if (menu_item->type_ == 'checkbox')
+    menu_model_->InsertCheckItemAt(pos, menu_item->id(), menu_item->label_);
+  else if (menu_item->type_ == 'separator')
+    menu_model_->InsertSeparatorAt(pos, ui::NORMAL_SEPARATOR);
+    
+    #include 'base/run_loop.h'
+#include 'content/public/browser/browsing_data_remover.h'
+#include 'extensions/browser/extension_function.h'
+    
+    class NwObjCallObjectMethodSyncFunction : public NWSyncExtensionFunction {
+ public:
+  NwObjCallObjectMethodSyncFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-      void DumpFilter() {
-    fprintf(stderr, 'F(');
-    for (size_t i = 0; i + 1 < filter_.size(); i++) {
-      const unsigned int c = static_cast<unsigned int>(filter_[i]);
-      for (int j = 0; j < 8; j++) {
-        fprintf(stderr, '%c', (c & (1 << j)) ? '1' : '.');
-      }
+    void NwDesktopCaptureMonitor::OnSourceAdded(DesktopMediaList* list, int index) {
+    DesktopMediaList::Source src = list->GetSource(index);
     }
-    fprintf(stderr, ')\n');
+    
+    
+    {    // This function call won't return, and will engage in an infinite loop, processing events from the browser, and dispatching them.
+    emscripten_set_main_loop_arg(main_loop, NULL, 0, true);
+}
+    
+            // Rendering
+        FrameContext* frameCtxt = WaitForNextFrameResources();
+        UINT backBufferIdx = g_pSwapChain->GetCurrentBackBufferIndex();
+        frameCtxt->CommandAllocator->Reset();
+    
+        // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    
+    
+    {        // Create the constant buffer
+        {
+            D3D10_BUFFER_DESC desc;
+            desc.ByteWidth = sizeof(VERTEX_CONSTANT_BUFFER);
+            desc.Usage = D3D10_USAGE_DYNAMIC;
+            desc.BindFlags = D3D10_BIND_CONSTANT_BUFFER;
+            desc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
+            desc.MiscFlags = 0;
+            g_pd3dDevice->CreateBuffer(&desc, NULL, &g_pVertexConstantBuffer);
+        }
+    }
+    
+    // Implemented features:
+//  [X] Renderer: User texture binding. Use 'LPDIRECT3DTEXTURE9' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+//  [X] Renderer: Support for large meshes (64k+ vertices) with 16-bits indices.
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    #include <stdarg.h>
+    
+    class StageParking : public Stage {
+ public:
+  explicit StageParking(const ScenarioConfig::StageConfig& config)
+      : Stage(config) {}
+    }
+    
+    /**
+ * @file
+ **/
+    
+    namespace apollo {
+namespace planning {
+namespace scenario {
+namespace valet_parking {
+class StageApproachingParkingSpotTest : public ::testing::Test {
+ public:
+  virtual void SetUp() {
+    config_.set_stage_type(
+        ScenarioConfig::VALET_PARKING_APPROACHING_PARKING_SPOT);
   }
+    }
+    }
+    }
+    }
+    }
+    
+    #include <string>
+#include <vector>
+    
+    TEST(PredictionThreadPoolTest, global_for_each) {
+  std::vector<int> expect = {1, 2, 3, 4, 5, 6, 7, 8};
+  std::vector<int> real = {1, 2, 3, 4, 5, 6, 7, 8};
+    }
+    
+    std::unique_ptr<Stage> StopSignUnprotectedScenario::CreateStage(
+    const ScenarioConfig::StageConfig& stage_config) {
+  if (s_stage_factory_.Empty()) {
+    RegisterStages();
+  }
+  auto ptr = s_stage_factory_.CreateObjectOrNull(stage_config.stage_type(),
+                                                 stage_config);
+  if (ptr) {
+    ptr->SetContext(&context_);
+  }
+  return ptr;
+}
