77e0d7e57a4b9023125e86f68b71563e2fee1097
SUNsung <weatherius@gmail.com>
2019-07-17 16:41:20 +0300

    Rewrite [162DdPZugselA16dYVt6buLtiA16hf7orJSY8Ak16AQ5.py] File in: 2019-07-17 16:41:20

--- pyton/162DdPZugselA16dYVt6buLtiA16hf7orJSY8Ak16AQ5.py
+++ pyton/162DdPZugselA16dYVt6buLtiA16hf7orJSY8Ak16AQ5.py
@@ -1,231 +1,304 @@
 
         
-        signature = hexlify(rsa.pkcs1.sign(json.dumps(versions_info, sort_keys=True).encode('utf-8'), privkey, 'SHA-256')).decode()
-print('signature: ' + signature)
+            username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _('A user with that username already exists.'),
+        },
+    )
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    email = models.EmailField(_('email address'), blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
     
+        def test_create_user_is_staff(self):
+        email = 'normal@normal.com'
+        user = User.objects.create_user('user', email, is_staff=True)
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.username, 'user')
+        self.assertTrue(user.is_staff)
     
-def main():
-    with open('supportedsites.html.in', 'r', encoding='utf-8') as tmplf:
-        template = tmplf.read()
+        def to_python(self, value):
+        return uuid.UUID(value)
     
-    # General information about the project.
-project = u'youtube-dl'
-copyright = u'2014, Ricardo Garcia Gonzalez'
-    
-        def test_subtitles_in_page(self):
-        self.url = 'http://www.democracynow.org/2015/7/3/this_flag_comes_down_today_bree'
-        self.DL.params['writesubtitles'] = True
-        self.DL.params['allsubtitles'] = True
-        subtitles = self.getSubtitles()
-        self.assertEqual(set(subtitles.keys()), set(['en']))
-        self.assertEqual(md5(subtitles['en']), 'acaca989e24a9e45a6719c9b3d60815c')
-    
-        # Raises
-        ValueError: in case of invalid `label_mode`.
+        The setUp() method sets the current working dir to the temporary tree.
+    It'll be removed when cleaning up.
     '''
-    if label_mode not in ['fine', 'coarse']:
-        raise ValueError('`label_mode` must be one of `'fine'`, `'coarse'`.')
     
-        with gzip.open(paths[0], 'rb') as lbpath:
-        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+            # Getting the pointer for this field.
+        fld_ptr = capi.get_feat_field_defn(feat.ptr, index)
+        if not fld_ptr:
+            raise GDALException('Cannot create OGR Field, invalid pointer given.')
+        self.ptr = fld_ptr
     
-        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
-        loss = MSE_MAE_loss(0.3)
-        inputs = keras.layers.Input((2,))
-        outputs = keras.layers.Dense(1, name='model_output')(inputs)
-        model = keras.models.Model(inputs, outputs)
-        model.compile(optimizer='sgd', loss={'model_output': loss})
-        model.fit(np.random.rand(256, 2), np.random.rand(256, 1))
-        model.save(model_filename)
+        with pytest.raises(AssertionError):
+        conv_utils.conv_output_length(18, 5, 'diagonal', 2)
     
-    Denoising is one of the classic applications of autoencoders.
-The denoising process removes unwanted noise that corrupted the
-true signal.
+        # not specified `validation_steps`
+    with pytest.raises(ValueError):
+        model.fit_generator(custom_generator(),
+                            steps_per_epoch=STEPS_PER_EPOCH,
+                            validation_data=custom_generator(),
+                            validation_steps=None,
+                            max_queue_size=10,
+                            workers=1,
+                            use_multiprocessing=False)
     
-    print('Vectorizing sequence data...')
-tokenizer = Tokenizer(num_words=max_words)
-x_train = tokenizer.sequences_to_matrix(x_train, mode='binary')
-x_test = tokenizer.sequences_to_matrix(x_test, mode='binary')
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
+            params = self._canonical_to_params(
+            weights=[
+                self.kernel_r,
+                self.kernel_z,
+                self.kernel_h,
+                self.recurrent_kernel_r,
+                self.recurrent_kernel_z,
+                self.recurrent_kernel_h,
+            ],
+            biases=[
+                self.bias_r_i,
+                self.bias_z_i,
+                self.bias_h_i,
+                self.bias_r,
+                self.bias_z,
+                self.bias_h,
+            ],
+        )
+        outputs, h = self._cudnn_gru(
+            inputs,
+            input_h=input_h,
+            params=params,
+            is_training=True)
+    
+    from .conv_utils import convert_kernel
+from .. import backend as K
+import numpy as np
     
     
-def deserialize(name, custom_objects=None):
-    return deserialize_keras_object(name,
-                                    module_objects=globals(),
-                                    custom_objects=custom_objects,
-                                    printable_module_name='loss function')
+class CharacterTable(object):
+    '''Given a set of characters:
+    + Encode them to a one-hot integer representation
+    + Decode the one-hot or integer representation to their character output
+    + Decode a vector of probabilities to their character output
+    '''
+    def __init__(self, chars):
+        '''Initialize character table.
     
-            headers.insert(0, request_line)
-        headers = '\r\n'.join(headers).strip()
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-l', '--list', dest='list', action='store_true',
+                          help='only list contracts, without checking them')
+        parser.add_option('-v', '--verbose', dest='verbose', default=False, action='store_true',
+                          help='print contract tests for all spiders')
+    
+        def test_walk_modules_egg(self):
+        egg = os.path.join(os.path.dirname(__file__), 'test.egg')
+        sys.path.append(egg)
+        try:
+            mods = walk_modules('testegg')
+            expected = [
+                'testegg.spiders',
+                'testegg.spiders.a',
+                'testegg.spiders.b',
+                'testegg'
+            ]
+            self.assertEqual(set([m.__name__ for m in mods]), set(expected))
+        finally:
+            sys.path.remove(egg)
+    
+    from scrapy.utils.misc import arg_to_iter
+from scrapy.utils.url import (
+    url_is_from_any_domain, url_has_any_extension,
+)
     
     
-class Conversion(object):
+_scrapy_root_handler = None
+    
+    from tests import tests_datadir
+    
+        global pteredor_is_running, usable
+    pteredor_is_running = probe_nat
+    prober = teredo_prober(probe_nat=probe_nat)
+    
+        # Calling directly the omnifunc may move the cursor position. This is the
+    # case with the default Vim omnifunc for C-family languages
+    # (ccomplete#Complete) which calls searchdecl to find a declaration. This
+    # function is supposed to move the cursor to the found declaration but it
+    # doesn't when called through the omni completion mapping (CTRL-X CTRL-O).
+    # So, we restore the cursor position after the omnifunc calls.
+    line, column = vimsupport.CurrentLineAndColumn()
     
     
-def get_stream_type(env, args):
-    '''Pick the right stream type based on `env` and `args`.
-    Wrap it in a partial with the type-specific args so that
-    we don't need to think what stream we are dealing with.
+@YouCompleteMeInstance( { 'g:ycm_cache_omnifunc': 0,
+                          'g:ycm_semantic_triggers': TRIGGERS } )
+def OmniCompleter_GetCompletions_NoCache_ObjectListObject_test( ycm ):
+  def Omnifunc( findstart, base ):
+    if findstart:
+      return 5
+    return { 'words': [
+      {
+        'word': 'a',
+        'abbr': 'ABBR',
+        'menu': 'MENU',
+        'info': 'INFO',
+        'kind': 'K'
+      },
+      {
+        'word': 'test',
+        'abbr': 'ABBRTEST',
+        'menu': 'MENUTEST',
+        'info': 'INFOTEST',
+        'kind': 'T'
+      }
+    ] }
     
-        # If both `auth_parse` and `prompt_password` are set to `True`,
-    # and the value of `-a` lacks the password part,
-    # then the user will be prompted to type the password in.
-    prompt_password = True
+        # build a new DiagnosticFilter merging all filters
+    #  for the provided filetypes
+    spec = []
+    for filetype in filetypes:
+      type_specific = self._all_filters.get( filetype, [] )
+      spec.extend( type_specific )
     
-        # noinspection PyMethodOverriding
-    def get_auth(self, username, password):
-        return requests.auth.HTTPDigestAuth(username, password)
+    
+def YouCompleteMeInstance( custom_options = {} ):
+  '''Defines a decorator function for tests that passes a unique YouCompleteMe
+  instance as a parameter. This instance is initialized with the default options
+  `DEFAULT_CLIENT_OPTIONS`. Use the optional parameter |custom_options| to give
+  additional options and/or override the already existing ones.
+    
+    from ycm.client.base_request import BaseRequest, BuildRequestData
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+def _HandlePollResponse( response, diagnostics_handler ):
+  if isinstance( response, list ):
+    for notification in response:
+      if 'message' in notification:
+        PostVimMessage( notification[ 'message' ],
+                        warning = False,
+                        truncate = True )
+      elif 'diagnostics' in notification:
+        diagnostics_handler.UpdateWithNewDiagnosticsForFile(
+          notification[ 'filepath' ],
+          notification[ 'diagnostics' ] )
+  elif response is False:
+    # Don't keep polling for this file
+    return False
+  # else any truthy response means 'nothing to see here; poll again in a
+  # while'
+    
+    
+def _SyntaxGroupsFromOutput( syntax_output ):
+  group_name_to_group = _CreateInitialGroupMap()
+  lines               = syntax_output.split( '\n' )
+  looking_for_group   = True
+    
+    
+def FormatDebugInfoResponse_Completer_ServerNotRunningWithNoLogfiles_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'completer' ][ 'servers' ][ 0 ].update( {
+    'is_running': False,
+    'logfiles': []
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'Completer name completer debug information:\n'
+      '  Server name not running\n'
+      '  Server name executable: /path/to/executable\n'
+      '  No logfiles available\n'
+      '  Server name key: value\n'
+      '  Key: value\n'
+    )
+  )
 
     
-        def get_formatters_grouped(self):
-        groups = {}
-        for group_name, group in groupby(
-                self.get_formatters(),
-                key=lambda p: getattr(p, 'group_name', 'format')):
-            groups[group_name] = list(group)
-        return groups
+      opts = { 'filter_diagnostics' : {
+    'java,c,cs' : { 'regex' : '.*taco.*' } } }
+    
+    Executor.submit() called:
+- creates a uniquely numbered _WorkItem and adds it to the 'Work Items' dict
+- adds the id of the _WorkItem to the 'Work Ids' queue
     
     
-def patharg(path):
     '''
-    Back slashes need to be escaped in ITEM args,
-    even in Windows paths.
+    def __init__(self, nS, nA, P, isd):
+        self.P = P
+        self.isd = isd
+        self.lastaction = None # for rendering
+        self.nS = nS
+        self.nA = nA
     
+            return self.observation, reward[0], done, {'number': self.number, 'guesses': self.guess_count}
     
-def test_default_options_overwrite(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['json'] == {'foo': 'bar'}
+    from gym import core, spaces
+from gym.utils import seeding
     
-        def test_request_body_from_file_by_path(self, httpbin):
-        r = http('--verbose',
-                 'POST', httpbin.url + '/post', '@' + FILE_PATH_ARG)
-        assert HTTP_OK in r
-        assert FILE_CONTENT in r, r
-        assert ''Content-Type': 'text/plain'' in r
+                  'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 2',
     
+    github_rest_api = 'https://api.github.com/users/{}'
     
-class LookupDict(dict):
-    '''Dictionary lookup object.'''
+        def test_register_username_taken(self):
+        with patch.object(
+            Account, '_by_name', return_value=MagicMock(_deleted=False)
+        ):
+            res = self.do_register()
+            self.assert_failure(res, 'USERNAME_TAKEN')
     
-            self.host = host
-        self.port = port
-        self.requests_to_handle = requests_to_handle
+        def test_vote_event_with_prev(self):
+        self.patch_liveconfig('events_collector_vote_sample_rate', 1.0)
+        upvote = MagicMock(name='upvote',
+                           is_automatic_initial_vote=False,
+                           data={'rank': MagicMock()})
+        upvote.previous_vote = MagicMock(name='previous_vote',
+                                         is_upvote=False, is_downvote=True)
+        g.events.vote_event(upvote)
     
-    # ---------
-# Specifics
-# ---------
+    from r2.tests import RedditTestCase
     
-    import sys
+        def test_resize(self):
+        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
+                      height=800)
     
-            with server as address:
-            sock1 = socket.socket()
-            sock2 = socket.socket()
+        def test_alien_blue_detector(self):
+        user_agent = 'AlienBlue/2.9.10.0.2 CFNetwork/758.4.3 Darwin/15.5.0'
+        outs = detect(user_agent)
+        self.assertEqual(outs['browser']['name'], AlienBlueDetector.name)
     
-        def __getstate__(self):
-        # Consume everything; accessing the content attribute makes
-        # sure the content has been fully read.
-        if not self._content_consumed:
-            self.content
+        def test_encrypt(self):
+        from r2.lib.tracking import _encrypt, SALT_SIZE
+        encrypted = _encrypt(
+            'a' * SALT_SIZE,
+            MESSAGE,
+            SECRET,
+        )
+        self.assertEquals(encrypted, ENCRYPTED)
     
-    - oneandone_firewall_policy:
-    auth_token: oneandone_private_api_key
-    firewall_policy: ansible-firewall-policy-updated
-    remove_rules:
-     - rule_id #1
-     - rule_id #2
-     - ...
-    wait: true
-    wait_timeout: 500
-    state: update
-    
-                # the cluster
-            if host.CLUSTER_ID != self.get_parameter('cluster_id'):
-                if one.cluster.addhost(self.get_parameter('cluster_id'), host.ID):
-                    result['changed'] = True
-                else:
-                    self.fail(msg='failed to update the host cluster')
-    
-    
-def protocol_to_type(protocol):
-    try:
-        protocols = ProtocolsType()
-        setattr(protocols, protocol, True)
-        return protocols
-    except AttributeError:
-        raise VcaError('The value in protocol is not valid')
-    
-        def role_mod(self, name, item):
-        return self._post_json(method='role_mod', name=name, item=item)
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    
-class Icinga2FeatureHelper:
-    def __init__(self, module):
-        self.module = module
-        self._icinga2 = module.get_bin_path('icinga2', True)
-        self.feature_name = self.module.params['name']
-        self.state = self.module.params['state']
-    
-    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
-    host = r1(r'host=([^&]*)', html)
-    assert host
-    type = r1(r'videofiletype=([^&]*)', html)
-    assert type
-    vid = r1(r'&stream_name=([^&]*)', html)
-    assert vid
-    dir_str = r1(r'&dir=([^&]*)', html).strip()
-    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
-    _, ext, size = url_info(url)
-    print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge = merge)
-    
-            for i in html_json['sources']:
-            if 'src' in i:  #to avoid KeyError
-                if i['src'].startswith('https'):
-                    link_list.append((str(i['height']), i['src']))
-    
-            html = get_content(api_url)
-        self.tree = ET.ElementTree(ET.fromstring(html))
-    
-    from ..common import *
-    
-        def prepare(self, **kwargs):
-        if re.search(r'imgur\.com/a/', self.url):
-            # album
-            content = get_content(self.url)
-            album = match1(content, r'album\s*:\s*({.*}),') or \
-                    match1(content, r'image\s*:\s*({.*}),')
-            album = json.loads(album)
-            count = album['album_images']['count']
-            images = album['album_images']['images']
-            ext = images[0]['ext']
-            self.streams = {
-                'original': {
-                    'src': ['http://i.imgur.com/%s%s' % (i['hash'], ext)
-                            for i in images],
-                    'size': sum([i['size'] for i in images]),
-                    'container': ext[1:]
-                },
-                'thumbnail': {
-                    'src': ['http://i.imgur.com/%ss%s' % (i['hash'], '.jpg')
-                            for i in images],
-                    'container': 'jpg'
-                }
-            }
-            self.title = album['title']
-    
-        def extract(self, **kwargs):
-        for i in self.streams:
-            s = self.streams[i]
-            _, s['container'], s['size'] = url_info(s['url'])
-            s['src'] = [s['url']]
+        def test_different_ports(self):
+        u = UrlParser('http://example.com')
+        u2 = UrlParser('http://example.com:8000')
+        u3 = UrlParser('http://example.com:8008')
+        self.assertNotEquals(u, u2)
+        self.assertNotEquals(u2, u3)
