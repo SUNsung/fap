ff2ac4bf7c8e80e13d3e3108a7c64b375c011d8c
SUNsung <weatherius@gmail.com>
2019-07-17 20:58:19 +0300

    Rewrite [04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py] File in: 2019-07-17 20:58:19

--- pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
+++ pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
@@ -1,113 +1,135 @@
 
         
         
-def is_setting_index(node):
-    if node.tagname == 'index':
-        # index entries for setting directives look like:
-        # [(u'pair', u'SETTING_NAME; setting', u'std:setting-SETTING_NAME', '')]
-        entry_type, info, refid = node['entries'][0][:3]
-        return entry_type == 'pair' and info.endswith('; setting')
-    return False
+def render_function(function, method=True):
+    subblocks = []
+    signature = get_function_signature(function, method=method)
+    if method:
+        signature = signature.replace(
+            clean_module_name(function.__module__) + '.', '')
+    subblocks.append('### ' + function.__name__ + '\n')
+    subblocks.append(code_snippet(signature))
+    docstring = function.__doc__
+    if docstring:
+        if ('backend' in signature and
+                '{{np_implementation}}' in docstring):
+            docstring = add_np_implementation(function, docstring)
+        subblocks.append(process_docstring(docstring))
+    return '\n\n'.join(subblocks)
     
-        # Max concurrency is limited by global CONCURRENT_REQUESTS setting
-    max_concurrent_requests = 8
-    # Requests per second goal
-    qps = None # same as: 1 / download_delay
-    download_delay = None
-    # time in seconds to delay server responses
-    latency = None
-    # number of slots to create
-    slots = 1
+    # we add a GlobalAveragePooling1D, which will average the embeddings
+# of all words in the document
+model.add(GlobalAveragePooling1D())
     
-        def short_desc(self):
-        return 'Edit spider'
+    # compute final accuracy on training and test sets
+y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
+tr_acc = compute_accuracy(tr_y, y_pred)
+y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
+te_acc = compute_accuracy(te_y, y_pred)
     
-        def short_desc(self):
-        return 'Run a self-contained spider (without creating a project)'
     
-    class Command(ScrapyCommand):
+dummy_docstring = '''Multiplies 2 tensors (and/or variables) and returns a *tensor*.
+    
+                # Arguments
+                y_true: Tensor, batch_wise labels
+                y_pred: Tensor, batch_wise predictions
+    
+    from .compat import is_py2, builtin_str, str
+    
+        def inner(*suffix):
+        return urljoin(httpbin_url, '/'.join(suffix))
+    
+        @pytest.fixture(autouse=True)
+    def setup(self):
+        '''CaseInsensitiveDict instance with 'Accept' header.'''
+        self.case_insensitive_dict = CaseInsensitiveDict()
+        self.case_insensitive_dict['Accept'] = 'application/json'
+    
+    '''
+requests.hooks
+~~~~~~~~~~~~~~
+    
+            with Server.basic_response_server(wait_to_close_event=block_server) as (host, port):
+            sock = socket.socket()
+            sock.connect((host, port))
+            sock.sendall(b'send something')
+            time.sleep(2.5)
+            sock.sendall(b'still alive')
+            block_server.set()  # release server block
+    
+            # reset stats on high iter-request times caused by client restarts
+        if delta > 3: # seconds
+            self._reset_stats()
+            return ''
+    
+    # Apply monkey patches to fix issues in external libraries
+from . import _monkeypatches
+del _monkeypatches
     
     
 class Command(ScrapyCommand):
     
+        def syntax(self):
+        return '[options] <name> <domain>'
     
-def _parse(url):
-    ''' Return tuple of (scheme, netloc, host, port, path),
-    all in bytes except for port which is int.
-    Assume url is from Request.url, which was passed via safe_url_string
-    and is ascii-only.
+            e.g.:
+        @returns request
+        @returns request 2
+        @returns request 2 10
+        @returns request 0 10
     '''
-    url = url.strip()
-    parsed = urlparse(url)
-    return _parsed_url_args(parsed)
     
-            print('Two or more elements')
-        data = [5, 1, 7, 2, 6, -3, 5, 7, -10]
-        assert_equal(func(data), sorted(data))
+        def _is_zip(self, response):
+        archive = BytesIO(response.body)
+        try:
+            zip_file = zipfile.ZipFile(archive)
+        except zipfile.BadZipfile:
+            return
     
-            print('Test: Enqueue on empty stack')
-        print('Test: Enqueue on non-empty stack')
-        print('Test: Multiple enqueue in a row')
-        num_items = 3
-        for i in range(0, num_items):
-            queue.enqueue(i)
+            {'id': 'mp4sd',    'container': 'mp4', 'video_profile': '标清'},
+        # obsolete?
+        {'id': 'flv',      'container': 'flv', 'video_profile': '标清'},
+        {'id': 'mp4',      'container': 'mp4', 'video_profile': '标清'},
+    ]
     
-            print('Test: Two or more element stack (general case)')
-        num_items = 10
-        numbers = [randint(0, 10) for x in range(num_items)]
-        sorted_stack = self.get_sorted_stack(stack, numbers)
-        sorted_numbers = []
-        for _ in range(num_items):
-            sorted_numbers.append(sorted_stack.pop())
-        assert_equal(sorted_numbers, sorted(numbers, reverse=True))
+    # TBD
+def main_dev(**kwargs):
+    '''Main entry point.
+    you-get-dev
+    '''
     
-    		print('Test: min node')
-		assert_equal(myTree.minNode(), 1)
+    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
+    host = r1(r'host=([^&]*)', html)
+    assert host
+    type = r1(r'videofiletype=([^&]*)', html)
+    assert type
+    vid = r1(r'&stream_name=([^&]*)', html)
+    assert vid
+    dir_str = r1(r'&dir=([^&]*)', html).strip()
+    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
+    _, ext, size = url_info(url)
+    print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge = merge)
     
+            self.extract(**kwargs)
     
-class PriorityQueue(object):
+    from ..common import *
+import urllib.parse
     
-        def load(self, loader):
-        loader.add_option(
-            'termlog_verbosity', str, 'info',
-            'Log verbosity.',
-            choices=log.LogTierOrder
-        )
-    
-        def configure(self, updated):
-        # FIXME: We're doing this because our proxy core is terminally confused
-        # at the moment. Ideally, we should be able to check if we're in
-        # reverse proxy mode at the HTTP layer, so that scripts can put the
-        # proxy in reverse proxy mode for specific requests.
-        if 'upstream_auth' in updated:
-            if ctx.options.upstream_auth is None:
-                self.auth = None
-            else:
-                self.auth = parse_upstream_auth(ctx.options.upstream_auth)
-    
-    
-    class IhdrChunk(KaitaiStruct):
-        def __init__(self, _io, _parent=None, _root=None):
-            self._io = _io
-            self._parent = _parent
-            self._root = _root if _root else self
-            self.width = self._io.read_u4be()
-            self.height = self._io.read_u4be()
-            self.bit_depth = self._io.read_u1()
-            self.color_type = self._root.ColorType(self._io.read_u1())
-            self.compression_method = self._io.read_u1()
-            self.filter_method = self._io.read_u1()
-            self.interlace_method = self._io.read_u1()
-    
-        def take(self):
-        '''
-        Scripts or other parties make 'take' a reply out of a normal flow.
-        For example, intercepted flows are taken out so that the connection thread does not proceed.
-        '''
-        if self.state != 'start':
-            raise exceptions.ControlException(
-                'Reply is {}, but expected it to be start.'.format(self.state)
-            )
-        self._state = 'taken'
-    
-    class BiDi:
+    # (api_key, method, ext, page)
+tmpl_api_call = (
+    'https://api.flickr.com/services/rest?'
+    '&format=json&nojsoncallback=1'
+    # UNCOMMENT FOR TESTING
+    #'&per_page=5'
+    '&per_page=500'
+    # this parameter CANNOT take control of 'flickr.galleries.getPhotos'
+    # though the doc said it should.
+    # it's always considered to be 500
+    '&api_key=%s'
+    '&method=flickr.%s'
+    '&extras=url_sq,url_q,url_t,url_s,url_n,url_m,url_z,url_c,url_l,url_h,url_k,url_o,media'
+    '%s&page=%d'
+)
