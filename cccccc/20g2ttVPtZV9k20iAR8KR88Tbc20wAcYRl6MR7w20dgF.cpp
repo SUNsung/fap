ed415181f77faf911fcdbf61a32338219cd31ed4
SUNsung <weatherius@gmail.com>
2019-07-17 15:57:29 +0300

    Rewrite [20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp] File in: 2019-07-17 15:57:29

--- cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
+++ cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
@@ -1,253 +1,97 @@
 
         
-        #include 'swift/AST/RequirementEnvironment.h'
-#include 'swift/AST/ASTContext.h'
-#include 'swift/AST/Decl.h'
-#include 'swift/AST/DeclContext.h'
-#include 'swift/AST/GenericSignature.h'
-#include 'swift/AST/GenericSignatureBuilder.h'
-#include 'swift/AST/ProtocolConformance.h'
-#include 'swift/AST/Types.h'
-#include 'llvm/ADT/Statistic.h'
+        
+#ifdef __cplusplus
+class GLUnurbs;
+class GLUquadric;
+class GLUtesselator;
+#else
+typedef struct GLUnurbs GLUnurbs;
+typedef struct GLUquadric GLUquadric;
+typedef struct GLUtesselator GLUtesselator;
+#endif
     
-    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
-  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
-  llvm::sys::ScopedLock L(DCache.Mux);
+      // skip leading whitespace and comments
+  comment = gFalse;
+  while (1) {
+    if ((c = getChar()) == EOF) {
+      buf[0] = '\0';
+      *length = 0;
+      return gFalse;
+    }
+    if (comment) {
+      if (c == '\x0a' || c == '\x0d') {
+	comment = gFalse;
+      }
+    } else if (c == '%') {
+      comment = gTrue;
+    } else if (specialChars[c] != 1) {
+      break;
+    }
+  }
+    
+      // contents
+  pageDict->lookupNF('Contents', &contents);
+  if (!(contents.isRef() || contents.isArray() ||
+	contents.isNull())) {
+    error(-1, 'Page contents object (page %d) is wrong type (%s)',
+	  num, contents.getTypeName());
+    contents.free();
+    goto err1;
+  }
+    
+      Gfx *createGfx(OutputDev *out, double hDPI, double vDPI,
+		 int rotate, GBool useMediaBox, GBool crop,
+		 int sliceX, int sliceY, int sliceW, int sliceH,
+		 GBool printing, Catalog *catalog,
+		 GBool (*abortCheckCbk)(void *data),
+		 void *abortCheckCbkData,
+		 GBool (*annotDisplayDecideCbk)(Annot *annot, void *user_data),
+		 void *annotDisplayDecideCbkData);
+    
+    class PageLabelInfo {
+public:
+  PageLabelInfo(Object *tree, int numPages);
+  ~PageLabelInfo();
+  GBool labelToIndex(GooString *label, int *index);
+  GBool indexToLabel(int index, GooString *label);
     }
     
-    void CacheImpl::removeAll() {
-  cache_remove_all(static_cast<cache_t*>(Impl));
-}
-    
-    
-    {  OutLines.append(Lines.begin(), Lines.end());
-}
-
-    
-      static CFPointeeInfo forTypedef(const clang::TypedefNameDecl *decl) {
-    assert(decl);
-    CFPointeeInfo info;
-    info.IsValid = true;
-    info.IsConst = false;
-    info.Decl = decl;
-    return info;
+      // get sacle
+  if (dict->lookup('SS', &obj)->isReal()) {
+    scale = obj.getReal();
   }
+  obj.free();
     
-      TemporaryFile() {
-    path = tmppath();
-  }
+    //========================================================================
+//
+// Modified under the Poppler project - http://poppler.freedesktop.org
+//
+// All changes made under the Poppler project to this file are licensed
+// under GPL version 2 or later
+//
+// Copyright (C) 2009 Carlos Garcia Campos <carlosgc@gnome.org>
+// Copyright (C) 2010 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2011 William Bader <williambader@hotmail.com>
+//
+// To see a description of the changes please see the Changelog file that
+// came with your tarball or type make ChangeLog if you are building from git
+//
+//========================================================================
+    
+    void MediaParameters::parseMediaScreenParameters(Object* obj) {
+  Object tmp;
+    }
     
     
-    {  std::vector<OperatorDef> GetGradientDefs() override {
-    return SingleGradientDef(
-        'SubGradient',
-        '',
-        std::vector<std::string>{GO(0), I(0), I(1)},
-        std::vector<std::string>{GI(0), GI(1)});
-  }
+    {  Object encryptDict;
+  XpdfSecurityHandler *xsh;
+  void *docData;
+  int permFlags;
+  Guchar fileKey[16];
+  int fileKeyLength;
+  int encVersion;
+  CryptAlgorithm encAlgorithm;
+  GBool ok;
 };
-    
-    workspace.ResetWorkspace()
-    
-      // This and other putatively are the same, so call the (permanent) callback
-  // for each blob index where the bounding boxes match.
-  // The callback is deleted on completion.
-  void ProcessMatchedBlobs(const TWERD& other, TessCallback1<int>* cb) const;
-    
-    bool ParagraphModel::Comparable(const ParagraphModel &other) const {
-  if (justification_ != other.justification_)
-    return false;
-  if (justification_ == JUSTIFICATION_CENTER ||
-      justification_ == JUSTIFICATION_UNKNOWN)
-    return true;
-  int tolerance = (tolerance_ + other.tolerance_) / 4;
-  return NearlyEqual(margin_ + first_indent_,
-                     other.margin_ + other.first_indent_, tolerance) &&
-         NearlyEqual(margin_ + body_indent_,
-                     other.margin_ + other.body_indent_, tolerance);
-}
-    
-    //////////////////////////////////////////////////////////////////////
-    
-      if (comma != data) {
-    // we have meta
-    ssize_t meta_len = comma - data;
-    data_len -= meta_len;
-    char* semi = (char*)memchr(data, ';', meta_len);
-    char* slash = (char*)memchr(data, '/', meta_len);
-    }
-    
-    #include 'hphp/util/stack-trace.h'
-    
-    template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, F fillCols);
-template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
-    
-      explicit PlainFile(FILE *stream = nullptr,
-                     bool nonblocking = false,
-                     const String& wrapper_type = null_string,
-                     const String& stream_type = null_string);
-  explicit PlainFile(int fd,
-                     bool nonblocking = false,
-                     const String& wrapper = null_string,
-                     const String& stream_type = null_string);
-  ~PlainFile() override;
-    
-    
-    {};
-    
-    ExtensionResponse::ExtensionResponse(const ExtensionResponse& other20) {
-  status = other20.status;
-  response = other20.response;
-  __isset = other20.__isset;
-}
-ExtensionResponse& ExtensionResponse::operator=(const ExtensionResponse& other21) {
-  status = other21.status;
-  response = other21.response;
-  __isset = other21.__isset;
-  return *this;
-}
-void ExtensionResponse::printTo(std::ostream& out) const {
-  using ::apache::thrift::to_string;
-  out << 'ExtensionResponse(';
-  out << 'status=' << to_string(status);
-  out << ', ' << 'response=' << to_string(response);
-  out << ')';
-}
-    
-      void __set_uuid(const ExtensionRouteUUID val);
-    
-    namespace osquery {
-namespace tables {
-    }
-    }
-    
-    
-    {
-    {
-    {  for (const auto& share_line : osquery::split(content, '\n')) {
-    genNFSShare(share_line, results);
-  }
-  return results;
-}
-}
-}
-
-    
-        if (type == nullptr) {
-      found = true;
-      return;
-    }
-    
-    Expected<int, PosixError> syscall(struct perf_event_attr* attr,
-                                  pid_t pid,
-                                  int cpu,
-                                  int group_fd,
-                                  unsigned long const flags);
-    
-    
-    {
-    {} // namespace table_tests
-} // namespace osquery
-
-    
-    TEST_F(KernelInfo, test_sanity) {
-  QueryData data = execute_query('select * from kernel_info');
-  ValidatatioMap row_map = {{'version', NonEmptyString},
-                            {'arguments', NormalType},
-                            {'path', NormalType},
-                            {'device', NonEmptyString}};
-  validate_rows(data, row_map);
-}
-    
-    #include <osquery/tests/integration/tables/helper.h>
-    
-    TEST_F(KernelModules, test_sanity) {
-  QueryData data = execute_query('select * from kernel_modules');
-  ASSERT_GT(data.size(), 0ul);
-  ValidatatioMap row_map = {
-      {'name', NonEmptyString},
-      {'size', NonNegativeInt},
-      {'used_by', NonEmptyString},
-      {'status', NonEmptyString},
-      {'address', NonNegativeInt},
-  };
-  validate_rows(data, row_map);
-}
-    
-    TEST_F(keychainAcls, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from keychain_acls');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'keychain_path', NormalType}
-  //      {'authorizations', NormalType}
-  //      {'path', NormalType}
-  //      {'description', NormalType}
-  //      {'label', NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
-}
-    
-    TEST_F(keychainItems, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from keychain_items');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'label', NormalType}
-  //      {'description', NormalType}
-  //      {'comment', NormalType}
-  //      {'created', NormalType}
-  //      {'modified', NormalType}
-  //      {'type', NormalType}
-  //      {'path', NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
-}
-    
-        // create string from std::string
-    std::string s_stdstring = 'The quick brown fox jumps over the lazy dog.';
-    json j_stdstring(s_stdstring);
-    
-    #include 'rocksdb/cache.h'
-#include 'rocksdb/compaction_filter.h'
-#include 'rocksdb/db.h'
-#include 'rocksdb/options.h'
-#include 'rocksdb/slice.h'
-#include 'rocksdb/table.h'
-#include 'rocksdb/utilities/options_util.h'
-    
-    std::string kDBPath = '/tmp/rocksdb_simple_example';
-    
-      // Lookup page cache by page identifier
-  //
-  // page_key   Page identifier
-  // buf        Buffer where the data should be copied
-  // size       Size of the page
-  virtual Status Lookup(const Slice& key, std::unique_ptr<char[]>* data,
-                        size_t* size) = 0;
-    
-      // Instead of creating a snapshot, take ownership of the input snapshot.
-  ManagedSnapshot(DB* db, const Snapshot* _snapshot);
-    
-    class OptimisticTransactionDB : public StackableDB {
- public:
-  // Open an OptimisticTransactionDB similar to DB::Open().
-  static Status Open(const Options& options, const std::string& dbname,
-                     OptimisticTransactionDB** dbptr);
-    }
+#endif // ENABLE_PLUGINS
