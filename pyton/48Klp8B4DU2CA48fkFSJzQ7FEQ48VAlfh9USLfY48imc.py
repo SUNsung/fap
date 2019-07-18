171714b9361723f5f2eb41b1c74485d0a506da1b
SUNsung <weatherius@gmail.com>
2019-07-18 19:31:12 +0300

    Rewrite [48Klp8B4DU2CA48fkFSJzQ7FEQ48VAlfh9USLfY48imc.py] File in: 2019-07-18 19:31:12

--- pyton/48Klp8B4DU2CA48fkFSJzQ7FEQ48VAlfh9USLfY48imc.py
+++ pyton/48Klp8B4DU2CA48fkFSJzQ7FEQ48VAlfh9USLfY48imc.py
@@ -1,122 +1,114 @@
 
         
-        if S < N:
-  # Note that this isn't necessary for this synthetic example, but
-  # it's useful to see how the input factor matrices were initialized
-  # for actual neurophysiology data.
-  datasets = add_alignment_projections(datasets, npcs=FLAGS.npcs)
+            Returns:
+        dict
+    '''
+    tokens = tokenizer.tokenize(txt)
+    tokens = [word_clean(token) for token in tokens]
     
-          # Cut sentences into patches of shape processable by the LM.
-      batch_size, num_timesteps = self.shape
-      word_patches = utils.cut_to_patches(sentences, batch_size, num_timesteps)
-      probs = self._score_patches(word_patches)
+            if n_dim == 2:
+            return tf.nn.softmax(x)
+        else:
+            e = tf.exp(x - tf.reduce_max(x, axis=axis, keepdims=True))
+            s = tf.reduce_sum(e, axis=axis, keepdims=True)
+            return e / s
+    '''
+    return tf.nn.softmax(x, axis=axis)
     
-      Args:
-    gen_logits: Generator logits.
-    gen_labels:  Labels for the correct token.
-    dis_predictions:  Discriminator predictions.
-    is_real_input:  Tensor indicating whether the label is present.
+            Use attention **after** lstm:
+            ```
+            tf.reset_default_graph()
     
-      Args:
-    hparams:  Hyperparameters for the MaskGAN.
-    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
-    is_training:  Whether the model is training.
-    reuse (Optional):  Whether to reuse the model.
+        return x
     
-        ## Discriminator Models.
-    if (FLAGS.discriminator_model == 'rnn_nas' or
-        FLAGS.discriminator_model == 'rnn_zaremba' or
-        FLAGS.discriminator_model == 'rnn_vd' or
-        FLAGS.discriminator_model == 'cnn'):
-      load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-      print('Restoring Discriminator from %s.' % load_ckpt)
-      tf.logging.info('Restoring Discriminator from %s.' % load_ckpt)
-      dis_init_saver = init_savers['dis_init_saver']
-      dis_init_saver.restore(sess, load_ckpt)
-    
-      for key, _ in gen_ngrams_dict.iteritems():
-    if key in train_ngrams_dict:
-      unique_ngrams_in_train += 1
-  return float(unique_ngrams_in_train) / float(total_ngrams_produced)
-
-    
-          BaseRequest().GetDataFromHandler( 'healthy', display_message = False )
-
-    
-    from future.utils import itervalues
-import re
-from ycm import vimsupport
+    References：
+    [1509.01626] Character-level Convolutional Networks for Text Classification https://arxiv.org/abs/1509.01626
+'''
     
     
-def _assert_accepts( filter, text ):
-  _assert_accept_equals( filter, text, True )
+def get_params_dict():
+    '''以字典形式获取所有 trainable 参数'''
+    param_dict = dict()
+    for var in tf.trainable_variables():
+        param_dict[var.name] = {'shape': list(map(int, var.shape)),
+                                'number': int(reduce(mul, var.shape, 1))}
+    return param_dict
     
-      def submit( self, fn, *args, **kwargs ):
+        # list of integers
+    result = s.iloc[[0, 2, 3, 4, 5]]
+    expected = s.reindex(s.index[[0, 2, 3, 4, 5]])
+    assert_series_equal(result, expected)
+    
+    
+def make_data():
+    return [decimal.Decimal(random.random()) for _ in range(100)]
+    
+    Note:
+    
+            sorted_df = frame.copy()
+        sorted_df.sort_values(by='A', ascending=False, inplace=True)
+        expected = frame.sort_values(by='A', ascending=False)
+        assert_frame_equal(sorted_df, expected)
+    
+    
+  def ComputeCandidatesInner( self, request_data ):
+    if not self._omnifunc:
+      return []
+    
+      with MockVimBuffers( [ current_buffer ], [ current_buffer ], ( 1, 6 ) ):
+    ycm.SendCompletionRequest()
+    assert_that(
+      ycm.GetCompletionResponse(),
+      has_entries( {
+        'completions': [ { 'word': 'CDtEF', 'equal': 1 } ],
+        'completion_start_column': 6
+      } )
+    )
+    
+    
+class EventNotification( BaseRequest ):
+  def __init__( self, event_name, buffer_number = None, extra_data = None ):
+    super( EventNotification, self ).__init__()
+    self._event_name = event_name
+    self._buffer_number = buffer_number
+    self._extra_data = extra_data
+    self._response_future = None
+    self._cached_response = None
+    
+    
+# This class can be used to keep the ycmd server alive for the duration of the
+# life of the client. By default, ycmd shuts down if it doesn't see a request in
+# a while.
+class YcmdKeepalive( object ):
+  def __init__( self, ping_interval_seconds = 60 * 10 ):
+    self._keepalive_thread = Thread( target = self._ThreadMain )
+    self._keepalive_thread.daemon = True
+    self._ping_interval_seconds = ping_interval_seconds
+    
+      # Ignore 'contained' argument in first position.
+  if words[ 0 ] == 'contained':
+    words = words[ 1: ]
+    
+    
+def FormatDebugInfoResponse_ExtraConfFoundAndLoaded_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'extra_conf' ].update( {
+    'is_loaded': True,
+    'path': '/path/to/extra/conf'
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'Extra configuration file found and loaded\n'
+      'Extra configuration path: /path/to/extra/conf\n'
+    )
+  )
+    
+      def shutdown( self, wait=True ):
     with self._shutdown_lock:
-      if self._shutdown:
-        raise RuntimeError( 'cannot schedule new futures after shutdown' )
-    
-            Args:
-            wait: If True then shutdown will not return until all running
-                futures have finished executing and the resources used by the
-                executor have been reclaimed.
-        '''
-        pass
-    
-      # `query_layer` = [B*F, N*H]
-  query_layer = tf.layers.dense(
-      from_tensor_2d,
-      num_attention_heads * size_per_head,
-      activation=query_act,
-      name='query',
-      kernel_initializer=create_initializer(initializer_range))
-    
-        # The mask has 1 for real tokens and 0 for padding tokens. Only real
-    # tokens are attended to.
-    input_mask = [1] * len(input_ids)
-    
-    
-class EscCharSetProber(CharSetProber):
-    def __init__(self):
-        CharSetProber.__init__(self)
-        self._mCodingSM = [
-            CodingStateMachine(HZSMModel),
-            CodingStateMachine(ISO2022CNSMModel),
-            CodingStateMachine(ISO2022JPSMModel),
-            CodingStateMachine(ISO2022KRSMModel)
-        ]
-        self.reset()
-    
-    ISO2022KRSMModel = {'classTable': ISO2022KR_cls,
-                    'classFactor': 6,
-                    'stateTable': ISO2022KR_st,
-                    'charLenTable': ISO2022KRCharLenTable,
-                    'name': 'ISO-2022-KR'}
-    
-    # Character Mapping Table:
-Latin7_CharToOrderMap = (
-255,255,255,255,255,255,255,255,255,255,254,255,255,254,255,255,  # 00
-255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,  # 10
-253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,  # 20
-252,252,252,252,252,252,252,252,252,252,253,253,253,253,253,253,  # 30
-253, 82,100,104, 94, 98,101,116,102,111,187,117, 92, 88,113, 85,  # 40
- 79,118,105, 83, 67,114,119, 95, 99,109,188,253,253,253,253,253,  # 50
-253, 72, 70, 80, 81, 60, 96, 93, 89, 68,120, 97, 77, 86, 69, 55,  # 60
- 78,115, 65, 66, 58, 76,106,103, 87,107,112,253,253,253,253,253,  # 70
-255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,  # 80
-255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,  # 90
-253,233, 90,253,253,253,253,253,253,253,253,253,253, 74,253,253,  # a0
-253,253,253,253,247,248, 61, 36, 46, 71, 73,253, 54,253,108,123,  # b0
-110, 31, 51, 43, 41, 34, 91, 40, 52, 47, 44, 53, 38, 49, 59, 39,  # c0
- 35, 48,250, 37, 33, 45, 56, 50, 84, 57,120,121, 17, 18, 22, 15,  # d0
-124,  1, 29, 20, 21,  3, 32, 13, 25,  5, 11, 16, 10,  6, 30,  4,  # e0
-  9,  8, 14,  7,  2, 12, 28, 23, 42, 24, 64, 75, 19, 26, 27,253,  # f0
-)
-    
-    Win1250HungarianModel = {
-  'charToOrderMap': win1250HungarianCharToOrderMap,
-  'precedenceMatrix': HungarianLangModel,
-  'mTypicalPositiveRatio': 0.947368,
-  'keepEnglishLetter': True,
-  'charsetName': 'windows-1250'
-}
+      self._shutdown = True
+      self._work_queue.put( None )
+    if wait:
+      for t in self._threads:
+        t.join()
+  shutdown.__doc__ = _base.Executor.shutdown.__doc__
