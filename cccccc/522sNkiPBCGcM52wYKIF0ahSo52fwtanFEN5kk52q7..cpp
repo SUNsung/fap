3c4f8460a2bae467c894970d1288535774bc4e6f
SUNsung <weatherius@gmail.com>
2019-07-18 19:58:38 +0300

    Rewrite [522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp] File in: 2019-07-18 19:58:38

--- cccccc/522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp
+++ cccccc/522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp
@@ -1,112 +1,151 @@
 
         
-        		void Init(Block4x4 *a_pblockParent,
-					ColorFloatRGBA *a_pafrgbaSource,
-    
-    		static const unsigned int BYTES_PER_BLOCK = 8;
-    
-    #ifndef C_ADD
-#define  C_ADD( res, a,b)\
-    do { \
-            CHECK_OVERFLOW_OP((a).r,+,(b).r)\
-            CHECK_OVERFLOW_OP((a).i,+,(b).i)\
-            (res).r=(a).r+(b).r;  (res).i=(a).i+(b).i; \
-    }while(0)
-#define  C_SUB( res, a,b)\
-    do { \
-            CHECK_OVERFLOW_OP((a).r,-,(b).r)\
-            CHECK_OVERFLOW_OP((a).i,-,(b).i)\
-            (res).r=(a).r-(b).r;  (res).i=(a).i-(b).i; \
-    }while(0)
-#define C_ADDTO( res , a)\
-    do { \
-            CHECK_OVERFLOW_OP((res).r,+,(a).r)\
-            CHECK_OVERFLOW_OP((res).i,+,(a).i)\
-            (res).r += (a).r;  (res).i += (a).i;\
-    }while(0)
-    
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-    
-    #undef SIG2WORD16
-static OPUS_INLINE opus_val16 SIG2WORD16_armv6(opus_val32 x)
-{
-   celt_sig res;
-   __asm__(
-       '#SIG2WORD16\n\t'
-       'ssat %0, #16, %1, ASR #12\n\t'
-       : '=r'(res)
-       : 'r'(x+2048)
-   );
-   return EXTRACT16(res);
-}
-#define SIG2WORD16(x) (SIG2WORD16_armv6(x))
-    
-    /** Add two 16-bit values */
-#define ADD16(a,b) ((opus_val16)((opus_val16)(a)+(opus_val16)(b)))
-/** Subtract two 16-bit values */
-#define SUB16(a,b) ((opus_val16)(a)-(opus_val16)(b))
-/** Add two 32-bit values */
-#define ADD32(a,b) ((opus_val32)(a)+(opus_val32)(b))
-/** Subtract two 32-bit values */
-#define SUB32(a,b) ((opus_val32)(a)-(opus_val32)(b))
-    
-    void DHTReplaceNodeTask::onReceived(const DHTPingReplyMessage* message)
-{
-  A2_LOG_INFO(fmt('ReplaceNode: Ping reply received from %s.',
-                  message->getRemoteNode()->toString().c_str()));
-  setFinished(true);
+        Cookies::Cookies(v8::Isolate* isolate, AtomBrowserContext* browser_context)
+    : browser_context_(browser_context) {
+  Init(isolate);
+  cookie_change_subscription_ =
+      browser_context_->cookie_change_notifier()->RegisterCookieChangeCallback(
+          base::BindRepeating(&Cookies::OnCookieChanged,
+                              base::Unretained(this)));
 }
     
-    DHTResponseMessage::DHTResponseMessage(
-    const std::shared_ptr<DHTNode>& localNode,
-    const std::shared_ptr<DHTNode>& remoteNode,
-    const std::string& transactionID)
-    : DHTAbstractMessage(localNode, remoteNode, transactionID)
-{
+    namespace base {
+class DictionaryValue;
 }
     
-    public:
-  DHTRoutingTable(const std::shared_ptr<DHTNode>& localNode);
     
-    void DHTTaskFactoryImpl::setRoutingTable(DHTRoutingTable* routingTable)
-{
-  routingTable_ = routingTable;
-}
+    {}  // namespace electron
     
-      virtual void process() CXX11_OVERRIDE;
-    
-    #include 'DHTMessage.h'
-    
-    #include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
-    
-    void ObjectExtendedInfo60D::Parse(const std::uint8_t* bytes, int32_t length,
-                                  ContiRadar* conti_radar) const {
-  int obj_id = object_id(bytes, length);
-  for (int i = 0; i < conti_radar->contiobs_size(); ++i) {
-    if (conti_radar->contiobs(i).obstacle_id() == obj_id) {
-      auto obs = conti_radar->mutable_contiobs(i);
-      obs->set_longitude_accel(longitude_accel(bytes, length));
-      obs->set_lateral_accel(lateral_accel(bytes, length));
-      obs->set_oritation_angle(oritation_angle(bytes, length));
-      obs->set_length(object_length(bytes, length));
-      obs->set_width(object_width(bytes, length));
-      obs->set_obstacle_class(obstacle_class(bytes, length));
-      break;
+    v8::Local<v8::Promise> ShowMessageBox(
+    const electron::MessageBoxSettings& settings,
+    mate::Arguments* args) {
+  v8::Isolate* isolate = args->isolate();
+  electron::util::Promise promise(isolate);
+  v8::Local<v8::Promise> handle = promise.GetHandle();
     }
+    
+    #include <string>
+#include <vector>
+    
+    class ObjectManager;
+    
+    #endif  // CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+
+    
+    class BaseEvent {
+  friend class EventListener;
+  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
+    }
+    
+    #include 'base/logging.h'
+#include 'base/strings/string_util.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/nw/src/api/menu/menu.h'
+    
+    namespace nw {
+    }
+    
+          if (data.type == TYPE_PNG &&
+         !gfx::PNGCodec::EncodeBGRASkBitmap(bitmap, false, &encoded_image)) {
+        LOG(INFO) << 'NwClipboardGetSyncFunction::RunSync(' << nwapi::nw__clipboard::ToString(data.type) << ') failed when converting to PNG';
+        error_ = 'Failed to encode as PNG';
+        return false;
+      } else if (data.type == TYPE_JPEG &&
+                 !gfx::JPEGCodec::Encode(bitmap, kQuality, &encoded_image)) {
+        LOG(INFO) << 'NwClipboardGetSyncFunction::RunSync(' << nwapi::nw__clipboard::ToString(data.type) << ') failed when converting to JPEG';
+        error_ = 'Failed to encode as JPEG';
+        return false;
+      }
+    
+        if (windows) {
+      std::unique_ptr<DesktopMediaList> window_media_list =
+        std::make_unique<NativeDesktopMediaList>(
+          content::DesktopMediaID::TYPE_WINDOW,
+          webrtc::DesktopCapturer::CreateWindowCapturer(options));
+      media_list_.push_back(std::move(window_media_list));
+    }
+    
+      protected:
+    ~NwScreenRegisterStreamFunction() override {}
+    DECLARE_EXTENSION_FUNCTION('nw.Screen.registerStream', UNKNOWN)
+    
+    
+    {  virtual TLSSessionSide getSide() const = 0;
+  virtual bool getVerifyPeer() const = 0;
+  virtual void setVerifyPeer(bool) = 0;
+};
+    
+      if (state_ == st_closing || state_ == st_closed || state_ == st_error) {
+    auto nread = decBuf_.size();
+    if (nread) {
+      assert(nread < len);
+      memcpy(data, decBuf_.data(), nread);
+      decBuf_.clear();
+      A2_LOG_DEBUG('WinTLS: Sending out decrypted buffer after EOF');
+      return nread;
+    }
+    }
+    
+    
+    {} // namespace aria2
+    
+    namespace {
+const int MAX_RETRY = 2;
+} // namespace
+    
+      void showBuckets() const;
+    
+    #include <algorithm>
+    
+    #include <vector>
+#include <deque>
+#include <memory>
+    
+    struct TestCanParam {
+  CANCardParameter conf;
+  bool is_first_agent = false;
+  int32_t recv_cnt = 0;
+  int32_t recv_err_cnt = 0;
+  int32_t send_cnt = 0;
+  int32_t send_err_cnt = 0;
+  int32_t send_lost_cnt = 0;
+  int32_t send_time = 0;
+  int32_t recv_time = 0;
+  CanClient *can_client = nullptr;
+    }
+    
+    void DistanceApproachIPOPTInterfaceTest::ProblemSetup() {
+  // obstacles_edges_num_ = 4 * Eigen::MatrixXi::Ones(obstacles_num_, 1);
+  obstacles_edges_num_ = Eigen::MatrixXi(obstacles_num_, 1);
+  obstacles_edges_num_ << 2, 1, 2, 1;
+  obstacles_edges_sum_ = obstacles_edges_num_.sum();
+  Eigen::MatrixXd l_warm_up_ =
+      Eigen::MatrixXd::Ones(obstacles_edges_sum_, horizon_ + 1);
+  Eigen::MatrixXd n_warm_up_ =
+      Eigen::MatrixXd::Ones(4 * obstacles_num_, horizon_ + 1);
+  ptop_.reset(new DistanceApproachIPOPTInterface(
+      horizon_, ts_, ego_, xWS_, uWS_, l_warm_up_, n_warm_up_, x0_, xf_,
+      last_time_u_, XYbounds_, obstacles_edges_num_, obstacles_num_,
+      obstacles_A_, obstacles_b_, planner_open_space_config_));
+}
+    
+    
+    {  can_sender_->Update();
+  const int32_t flag =
+      CHECK_RESPONSE_STEER_UNIT_FLAG | CHECK_RESPONSE_SPEED_UNIT_FLAG;
+  if (!CheckResponse(flag, true)) {
+    AERROR << 'Failed to switch to COMPLETE_AUTO_DRIVE mode.';
+    Emergency();
+    set_chassis_error_code(Chassis::CHASSIS_ERROR);
+    return ErrorCode::CANBUS_ERROR;
   }
-  // auto conti_obs = conti_radar->mutable_contiobs(object_id(bytes, length));
+  set_driving_mode(Chassis::COMPLETE_AUTO_DRIVE);
+  AINFO << 'Switch to COMPLETE_AUTO_DRIVE mode ok.';
+  return ErrorCode::OK;
 }
     
-    #include 'glog/logging.h'
     
-    void Hornrpt79::Parse(const std::uint8_t* bytes, int32_t length,
-                      ChassisDetail* chassis) const {
-  chassis->mutable_gem()->mutable_horn_rpt_79()->set_output_value(
-      output_value(bytes, length));
-  chassis->mutable_gem()->mutable_horn_rpt_79()->set_commanded_value(
-      commanded_value(bytes, length));
-  chassis->mutable_gem()->mutable_horn_rpt_79()->set_manual_input(
-      manual_input(bytes, length));
+    {  EXPECT_EQ(expect, real);
 }
+*/
