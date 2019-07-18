2746e70f80edf66cacb15f4fa46cc0f89a554a0f
SUNsung <weatherius@gmail.com>
2019-07-18 13:49:06 +0300

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2019-07-18 13:49:06

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,138 +1,30 @@
 
         
-        begin
-  require 'cucumber/rake/task'
-  Cucumber::Rake::Task.new(:features) do |t|
-    t.profile = 'travis'
-  end
-  Cucumber::Rake::Task.new(:'features:html', 'Run Cucumber features and produce HTML output') do |t|
-    t.profile = 'html_report'
-  end
-rescue LoadError
-  desc 'Cucumber rake task not available'
-  task :features do
-    abort 'Cucumber rake task is not available. Be sure to install cucumber as a gem or plugin'
-  end
-end
-    
-    if pathutil_relative == native_relative
-  Benchmark.ips do |x|
-    x.report('pathutil') { pathutil_relative }
-    x.report('native')   { native_relative }
-    x.compare!
-  end
-else
-  print 'PATHUTIL: '
-  puts pathutil_relative
-  print 'NATIVE:   '
-  puts native_relative
-end
-
-    
-    module Jekyll
-  module Deprecator
-    extend self
-    
-          # The base cache key to use for storing/retrieving issuable IDs.
-      CACHE_KEY = 'github-import/issuable-finder/%{project}/%{type}/%{iid}'.freeze
-    
-    module Gitlab
-  module QueryLimiting
-    # Middleware for reporting (or raising) when a request performs more than a
-    # certain amount of database queries.
-    class Middleware
-      CONTROLLER_KEY = 'action_controller.instance'.freeze
-      ENDPOINT_KEY = 'api.endpoint'.freeze
-    
-    describe DefaultScenarioImporter do
-  let(:user) { users(:bob) }
-  describe '.import' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      mock(DefaultScenarioImporter).seed(is_a(User))
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'true' }
-      DefaultScenarioImporter.import(user)
-    end
-    
-      it 'schould register the schedules with the rufus scheduler and run' do
-    mock(@rufus_scheduler).join
-    scheduler = HuginnScheduler.new
-    scheduler.setup!(@rufus_scheduler, Mutex.new)
-    scheduler.run
+            wait_for_requests
   end
     
-        it 'should work with nested hashes' do
-      @agent.options['very'] = {'nested' => '$.value'}
-      LiquidMigrator.convert_all_agent_options(@agent)
-      expect(@agent.reload.options).to eq({'auth_token' => 'token', 'color' => 'yellow', 'very' => {'nested' => '{{value}}'}, 'notify' => false, 'room_name' => 'test', 'username' => '{{username}}', 'message' => '{{message}}'})
-    end
+      describe '#can_resend_invite?' do
+    context 'when project_member is invited' do
+      before do
+        expect(project_member).to receive(:invite?).and_return(true)
+      end
     
-        it 'should generate the correct specific tracking url' do
-      @checker.options['path'] = 'trackings/usps/9361289878905919630610'
-      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/trackings/usps/9361289878905919630610')
-    end
+              if slug.start_with?('docs/syntax_and_semantics')
+            name.prepend '#{slug.split('/')[2].titleize}: ' if slug.split('/').length > 3
+          elsif slug.split('/').length > 1
+            chapter = slug.split('/')[1].titleize.capitalize
+            name.prepend '#{chapter}: ' unless name == chapter
+          end
     
-        alias_method :blank?, :empty?
-    
-        def assert_index(index)
-      i = index.is_a?(Integer) ? index : @filters.index(filter_const(index))
-      raise 'No such filter to insert: #{index}' unless i
-      i
-    end
-  end
-end
-
-    
-        def document?
-      @content =~ DOCUMENT_RGX
-    end
-    
-        def options
-      @options ||= self.class.options.deep_dup.tap do |options|
-        options.merge! base_url: base_url, root_url: root_url,
-                       root_path: root_path, initial_paths: initial_paths,
-                       version: self.class.version, release: self.class.release
-    
-        def test_default_config
-      assert_equal(true, @context.use_colorize?)
-    end
-  end
-end
-
-    
-      platform_is_not :solaris, :aix do
-    it 'with Process::CLOCK_REALTIME reports at least 1 microsecond' do
-      Process.clock_getres(Process::CLOCK_REALTIME, :nanosecond).should <= 1_000
-    end
-  end
-    
-      basis[1] = basis[2].vcross(basis[0])
-  basis[1] = basis[1].vnormalize
-end
-    
-      task :all => [:readmes, :index]
-end
-    
-        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      subject.options[:allow_empty_referrer] = false
-      expect(subject.referrer(env)).to be_nil
-    end
-    
-        # also set the env LOGSTASH_HOME
-    LOGSTASH_HOME = ENV['LOGSTASH_HOME'] = ::File.expand_path(::File.join(__FILE__, '..', '..', '..'))
-    
-    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
-Gem.use_paths(LogStash::Environment.logstash_gem_home)
-    
-        def execute
-      raise PluginManager::FileNotFoundError, 'Can't file local file #{local_file}' unless ::File.exist?(local_file)
-      raise PluginManager::InvalidPackError, 'Invalid format, the pack must be in zip format' unless valid_format?(local_file)
-    
-        puts('Unpacking #{package_file}')
-    
-      namespace :acceptance do
-    desc 'Run all acceptance'
-    task :all do
-      exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/**/*_spec.rb']]))
-    end
+            css('pre > code').each do |node|
+          node['class'] ||= ''
+          lang = if node['class'].include?('lang-html') || node.content =~ /\A</
+            'html'
+          elsif node['class'].include?('lang-css')
+            'css'
+          elsif node['class'].include?('lang-js') || node['class'].include?('lang-javascript')
+            'javascript'
+          end
+          node.parent['data-language'] = lang if lang
+          node.before(node.children).remove
+        end
