f9ee46b0a86ea58df97f561dfe25845b328df6ac
SUNsung <weatherius@gmail.com>
2019-07-18 05:22:13 +0300

    Rewrite [32M4oa56aTQeU32wtwDr8pzzpA32Uc5oKrftvqs32NyZ.py] File in: 2019-07-18 05:22:13

--- pyton/32M4oa56aTQeU32wtwDr8pzzpA32Uc5oKrftvqs32NyZ.py
+++ pyton/32M4oa56aTQeU32wtwDr8pzzpA32Uc5oKrftvqs32NyZ.py
@@ -1,137 +1,63 @@
 
         
-            for batch_idx, es_idx in enumerate(trial_batches(E_to_process,
-                                                     hps.batch_size)):
-      print('Running trial batch %d with %d trials' % (batch_idx+1,
-                                                       len(es_idx)))
-      data_bxtxd, ext_input_bxtxi = self.get_batch(data_extxd,
-                                                   ext_input_extxi,
-                                                   batch_size=batch_size,
-                                                   example_idxs=es_idx)
-      model_values = self.eval_model_runs_batch(data_name, data_bxtxd,
-                                                ext_input_bxtxi,
-                                                do_eval_cost=True,
-                                                do_average_batch=False)
+        
+def _lookup_req_object(name):
+    top = _request_ctx_stack.top
+    if top is None:
+        raise RuntimeError(_request_ctx_err_msg)
+    return getattr(top, name)
+    
+            return session.modified or (
+            session.permanent and app.config['SESSION_REFRESH_EACH_REQUEST']
+        )
+    
+        :copyright: 2010 Pallets
+    :license: BSD-3-Clause
+'''
+from werkzeug.exceptions import BadRequest
+from werkzeug.wrappers import Request as RequestBase
+from werkzeug.wrappers import Response as ResponseBase
+from werkzeug.wrappers.json import JSONMixin as _JSONMixin
+    
+        # make sure we're not leaking a request context since we are
+    # testing flask internally in debug mode in a few cases
+    leaks = []
+    while flask._request_ctx_stack.top is not None:
+        leaks.append(flask._request_ctx_stack.pop())
+    assert leaks == []
+    
+        app = flask.Flask(__name__, instance_path=str(modules_tmpdir))
+    assert app.instance_path == str(modules_tmpdir)
     
     
-# OVERFITTING
-# Dropout is done on the input data, on controller inputs (from
-# encoder), on outputs from generator to factors.
-flags.DEFINE_float('keep_prob', KEEP_PROB, 'Dropout keep probability.')
-# It appears that the system will happily fit spikes (blessing or
-# curse, depending).  You may not want this.  Jittering the spikes a
-# bit will help (-/+ bin size, as specified here).
-flags.DEFINE_integer('temporal_spike_jitter_width',
-                     TEMPORAL_SPIKE_JITTER_WIDTH,
-                     'Shuffle spikes around this window.')
+def rbf_kernels(X, n_jobs):
+    return pairwise_kernels(X, metric='rbf', n_jobs=n_jobs, gamma=0.1)
     
-      # Turn rates, noisy_data, and input into numpy arrays.
-  rates_train = nparray_and_transpose(rates_train)
-  rates_valid = nparray_and_transpose(rates_valid)
-  noisy_data_train = nparray_and_transpose(noisy_data_train)
-  noisy_data_valid = nparray_and_transpose(noisy_data_valid)
-  input_train = nparray_and_transpose(input_train)
-  inputs_valid = nparray_and_transpose(inputs_valid)
+    Typical output
+--------------
     
-    # pick which RNN is used on each trial
-rnn_to_use = rng.randint(2, size=E)
-ext_input = np.repeat(np.expand_dims(rnn_to_use, axis=1), ntimesteps, axis=1)
-ext_input = np.expand_dims(ext_input, axis=2)  # these are 'a's' in the paper
+    '''
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: Simplified BSD
     
-      if len(dataset_dict) == 0:
-    raise ValueError('Failed to load any datasets, are you sure that the '
-                     ''--data_dir' and '--data_filename_stem' flag values '
-                     'are correct?')
-    
-      # Output variables only for the Generator.  Discriminator output biases
-  # will begin randomly initialized.
-  if model == 'gen':
-    softmax_b = [
-        v for v in tf.trainable_variables() if v.op.name == 'gen/rnn/softmax_b'
-    ][0]
-    
-          # Now calculate the attention-weighted vector.
-      alignments = tf.expand_dims(alignments, 2)
-      context_vector = tf.reduce_sum(alignments * values, [1])
-      context_vector.set_shape([None, num_units])
-    
-          predictions = tf.transpose(predictions, [1, 0, 2])
-      return tf.squeeze(predictions, axis=2)
-
-    
-        def __call__(self, y_true, y_pred):
-        return (self.mse_fraction * losses.mse(y_true, y_pred) +
-                (1 - self.mse_fraction) * losses.mae(y_true, y_pred))
-    
-        # Arguments
-        y: class vector to be converted into a matrix
-            (integers from 0 to num_classes).
-        num_classes: total number of classes.
-        dtype: The data type expected by the input, as a string
-            (`float32`, `float64`, `int32`...)
+        if 'Content-Encoding' in headers:
+        if headers['Content-Encoding'] == 'deflate':
+            payload = zlib.decompress(payload, -zlib.MAX_WBITS)
+            headers['Content-Length'] = str(len(payload))
+            del headers['Content-Encoding']
     
     
-def mean_squared_logarithmic_error(y_true, y_pred):
-    first_log = K.log(K.clip(y_pred, K.epsilon(), None) + 1.)
-    second_log = K.log(K.clip(y_true, K.epsilon(), None) + 1.)
-    return K.mean(K.square(first_log - second_log), axis=-1)
+    def mark(self):
+        state = (self.p, self.line, self.charPositionInLine)
+        try:
+            self._markers[self.markDepth] = state
+        except IndexError:
+            self._markers.append(state)
+        self.markDepth += 1
+        
+        self.lastMarker = self.markDepth
+        
+        return self.lastMarker
     
-        return inner
-    
-        styles = {
-        # No corresponding class for the following:
-        #Text:                     '', # class:  ''
-        Whitespace:                'underline #f8f8f8',      # class: 'w'
-        Error:                     '#a40000 border:#ef2929', # class: 'err'
-        Other:                     '#000000',                # class 'x'
-    }
-    
-    import idna
-import urllib3
-import chardet
-    
-    
-def dispatch_hook(key, hooks, hook_data, **kwargs):
-    '''Dispatches a hook dictionary on a given piece of data.'''
-    hooks = hooks or {}
-    hooks = hooks.get(key)
-    if hooks:
-        if hasattr(hooks, '__call__'):
-            hooks = [hooks]
-        for hook in hooks:
-            _hook_data = hook(hook_data, **kwargs)
-            if _hook_data is not None:
-                hook_data = _hook_data
-    return hook_data
-
-    
-    _codes = {
-    }
-    
-            :rtype: dict
-        '''
-        # Gather clues from the surrounding environment.
-        if self.trust_env:
-            # Set environment's proxies.
-            no_proxy = proxies.get('no_proxy') if proxies is not None else None
-            env_proxies = get_environ_proxies(url, no_proxy=no_proxy)
-            for (k, v) in env_proxies.items():
-                proxies.setdefault(k, v)
-    
-        return lambda_facts
-    
-            if state == 'absent' and user in heroku_collaborator_list:
-            if not module.check_mode:
-                heroku_app.remove_collaborator(user)
-            affected_apps += [app]
-            result_state = True
-        elif state == 'present' and user not in heroku_collaborator_list:
-            if not module.check_mode:
-                heroku_app.add_collaborator(user_id_or_email=user, silent=module.params['suppress_invitation'])
-            affected_apps += [app]
-            result_state = True
-    
-        gateway = vca.get_gateway(vdc_name, gateway_name)
-    if not gateway:
-        module.fail_json(msg='Not able to find the gateway %s, please check '
-                             'the gateway_name param' % gateway_name)
+            You don't have to implement but it's nice to know where a Token
+        comes from if you have include files etc... on the input.'''
