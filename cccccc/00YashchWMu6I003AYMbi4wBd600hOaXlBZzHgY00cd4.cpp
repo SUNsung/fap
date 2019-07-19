85e618bbe392afa7298daaa721d9b7f6c1472064
SUNsung <weatherius@gmail.com>
2019-07-19 09:49:08 +0300

    Rewrite [00YashchWMu6I003AYMbi4wBd600hOaXlBZzHgY00cd4.cpp] File in: 2019-07-19 09:49:07

--- cccccc/00YashchWMu6I003AYMbi4wBd600hOaXlBZzHgY00cd4.cpp
+++ cccccc/00YashchWMu6I003AYMbi4wBd600hOaXlBZzHgY00cd4.cpp
@@ -1,105 +1,241 @@
 
         
-            void setModel(WalletModel *model);
-    void setAddress_SM(const QString &address);
-    void setAddress_VM(const QString &address);
-    
-        CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 0));
-    CHECK(!secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
-    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 1));
-    CHECK(secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
-    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 2));
-    CHECK(!secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
-    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 3));
-    CHECK(!secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
-    
-        BOOST_CHECK(v.setBool(false));
-    BOOST_CHECK_EQUAL(v.isBool(), true);
-    BOOST_CHECK_EQUAL(v.isTrue(), false);
-    BOOST_CHECK_EQUAL(v.isFalse(), true);
-    BOOST_CHECK_EQUAL(v.getBool(), false);
-    
-    namespace bech32
-{
+          // Iterate over the declarations and infer required availability on
+  // a per-platform basis.
+  std::map<PlatformKind, InferredAvailability> Inferred;
+  for (const Decl *D : InferredFromDecls) {
+    for (const DeclAttribute *Attr : D->getAttrs()) {
+      auto *AvAttr = dyn_cast<AvailableAttr>(Attr);
+      if (!AvAttr || AvAttr->isInvalid())
+        continue;
+    }
     }
     
-    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
-  result->append(key.user_key.data(), key.user_key.size());
-  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
+    namespace swift {
+    }
+    
+    
+    {} // end namespace swift
+    
+    
+    {}
+#endif // SWIFT_AST_DIAGNOSTIC_SUPPRESSION_H
+
+    
+    
+    {} // end namespace swift
+    
+    DMatrix* DMatrix::Create(dmlc::Parser<uint32_t>* parser,
+                         const std::string& cache_prefix,
+                         const size_t page_size) {
+  if (cache_prefix.length() == 0) {
+    std::unique_ptr<data::SimpleCSRSource> source(new data::SimpleCSRSource());
+    source->CopyFrom(parser);
+    return DMatrix::Create(std::move(source), cache_prefix);
+  } else {
+#if DMLC_ENABLE_STD_THREAD
+    if (!data::SparsePageSource::CacheExist(cache_prefix, '.row.page')) {
+      data::SparsePageSource::CreateRowPage(parser, cache_prefix, page_size);
+    }
+    std::unique_ptr<data::SparsePageSource> source(
+        new data::SparsePageSource(cache_prefix, '.row.page'));
+    return DMatrix::Create(std::move(source), cache_prefix);
+#else
+    LOG(FATAL) << 'External memory is not enabled in mingw';
+    return nullptr;
+#endif  // DMLC_ENABLE_STD_THREAD
+  }
 }
     
-    // Filter policy wrapper that converts from internal keys to user keys
-class InternalFilterPolicy : public FilterPolicy {
- private:
-  const FilterPolicy* const user_policy_;
+    namespace xgboost {
+namespace data {
+// Used for external memory.
+class SparsePageDMatrix : public DMatrix {
  public:
-  explicit InternalFilterPolicy(const FilterPolicy* p) : user_policy_(p) { }
-  virtual const char* Name() const;
-  virtual void CreateFilter(const Slice* keys, int n, std::string* dst) const;
-  virtual bool KeyMayMatch(const Slice& key, const Slice& filter) const;
-};
+  explicit SparsePageDMatrix(std::unique_ptr<DataSource>&& source,
+                             std::string cache_info)
+      : row_source_(std::move(source)), cache_info_(std::move(cache_info)) {}
+  virtual ~SparsePageDMatrix() = default;
+    }
+    }
+    }
     
-    
-    {  // When limit user key is prefix of start user key
-  ASSERT_EQ(IKey('foobar', 100, kTypeValue),
-            Shorten(IKey('foobar', 100, kTypeValue),
-                    IKey('foo', 200, kTypeValue)));
+    SparsePageWriter::SparsePageWriter(
+    const std::vector<std::string>& name_shards,
+    const std::vector<std::string>& format_shards,
+    size_t extra_buffer_capacity)
+    : num_free_buffer_(extra_buffer_capacity + name_shards.size()),
+      clock_ptr_(0),
+      workers_(name_shards.size()),
+      qworkers_(name_shards.size()) {
+  CHECK_EQ(name_shards.size(), format_shards.size());
+  // start writer threads
+  for (size_t i = 0; i < name_shards.size(); ++i) {
+    std::string name_shard = name_shards[i];
+    std::string format_shard = format_shards[i];
+    auto* wqueue = &qworkers_[i];
+    workers_[i].reset(new std::thread(
+        [this, name_shard, format_shard, wqueue] () {
+          std::unique_ptr<dmlc::Stream> fo(
+              dmlc::Stream::Create(name_shard.c_str(), 'w'));
+          std::unique_ptr<SparsePageFormat> fmt(
+              SparsePageFormat::Create(format_shard));
+          fo->Write(format_shard);
+          std::shared_ptr<SparsePage> page;
+          while (wqueue->Pop(&page)) {
+            if (page == nullptr) break;
+            fmt->Write(*page, fo.get());
+            qrecycle_.Push(std::move(page));
+          }
+          fo.reset(nullptr);
+          LOG(INFO) << 'SparsePage::Writer Finished writing to ' << name_shard;
+        }));
+  }
 }
     
-    #include <ImfEnvmapAttribute.h>
-    
-    FastHufDecoder::FastHufDecoder
-    (const char *&table,
-     int numBytes,
-     int minSymbol,
-     int maxSymbol,
-     int rleSymbol)
-:
-    _rleSymbol (rleSymbol),
-    _numSymbols (0),
-    _minCodeLength (255),
-    _maxCodeLength (0),
-    _idToSymbol (0)
-{
-    //
-    // List of symbols that we find with non-zero code lengths
-    // (listed in the order we find them). Store these in the
-    // same format as the code book stores codes + lengths - 
-    // low 6 bits are the length, everything above that is
-    // the symbol.
-    //
+    TEST(SparsePageDMatrix, ColAccess) {
+  dmlc::TemporaryDirectory tempdir;
+  const std::string tmp_file = tempdir.path + '/simple.libsvm';
+  CreateSimpleTestData(tmp_file);
+  xgboost::DMatrix * dmat = xgboost::DMatrix::Load(
+    tmp_file + '#' + tmp_file + '.cache', true, false);
     }
     
-    void GenericInputFile::readMagicNumberAndVersionField(OPENEXR_IMF_INTERNAL_NAMESPACE::IStream& is, int& version)
-{
-    //
-    // Read the magic number and the file format version number.
-    // Then check if we can read the rest of this file.
-    //
+        // divide up the condition_fraction among the recursive calls
+    bst_float hot_condition_fraction = condition_fraction;
+    bst_float cold_condition_fraction = condition_fraction;
+    if (condition > 0 && split_index == condition_feature) {
+      cold_condition_fraction = 0;
+      unique_depth -= 1;
+    } else if (condition < 0 && split_index == condition_feature) {
+      hot_condition_fraction *= hot_zero_fraction;
+      cold_condition_fraction *= cold_zero_fraction;
+      unique_depth -= 1;
     }
     
+    #include <string>
     
-class GenericOutputFile
-{
-    public:
-        IMF_EXPORT
-        virtual ~GenericOutputFile() {}
+       protected:
+    void SetNonDefaultPosition(const std::vector<int> &qexpand, DMatrix *p_fmat,
+                               const RegTree &tree) override {
+      // step 2, classify the non-default data into right places
+      std::vector<unsigned> fsplits;
+      for (int nid : qexpand) {
+        if (!tree[nid].IsLeaf()) {
+          fsplits.push_back(tree[nid].SplitIndex());
+        }
+      }
+      // get the candidate split index
+      std::sort(fsplits.begin(), fsplits.end());
+      fsplits.resize(std::unique(fsplits.begin(), fsplits.end()) - fsplits.begin());
+      while (fsplits.size() != 0 && fsplits.back() >= p_fmat->Info().num_col_) {
+        fsplits.pop_back();
+      }
+      // bitmap is only word concurrent, set to bool first
+      {
+        auto ndata = static_cast<bst_omp_uint>(this->position_.size());
+        boolmap_.resize(ndata);
+        #pragma omp parallel for schedule(static)
+        for (bst_omp_uint j = 0; j < ndata; ++j) {
+            boolmap_[j] = 0;
+        }
+      }
+      for (const auto &batch : p_fmat->GetSortedColumnBatches()) {
+        for (auto fid : fsplits) {
+          auto col = batch[fid];
+          const auto ndata = static_cast<bst_omp_uint>(col.size());
+          #pragma omp parallel for schedule(static)
+          for (bst_omp_uint j = 0; j < ndata; ++j) {
+            const bst_uint ridx = col[j].index;
+            const bst_float fvalue = col[j].fvalue;
+            const int nid = this->DecodePosition(ridx);
+            if (!tree[nid].IsLeaf() && tree[nid].SplitIndex() == fid) {
+              if (fvalue < tree[nid].SplitCond()) {
+                if (!tree[nid].DefaultLeft()) boolmap_[ridx] = 1;
+              } else {
+                if (tree[nid].DefaultLeft()) boolmap_[ridx] = 1;
+              }
+            }
+          }
+        }
+      }
     }
     
-    	    int zerun = getBits (8, c, lc, p) + SHORTEST_LONG_RUN;
+    // implementing configure.
+template<typename PairIter>
+inline void Learner::Configure(PairIter begin, PairIter end) {
+  std::vector<std::pair<std::string, std::string> > vec(begin, end);
+  this->Configure(vec);
+}
+    
+        // add additional parameters
+    // These are cosntraints that need to be satisfied.
+    if (tparam_.dsplit == DataSplitMode::kAuto && rabit::IsDistributed()) {
+      tparam_.dsplit = DataSplitMode::kRow;
+    }
+    
+    IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_Init(const char* glsl_version = NULL);
+IMGUI_IMPL_API void     ImGui_ImplOpenGL3_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL3_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
+    
+        // Create Logical Device (with 1 queue)
+    {
+        int device_extension_count = 1;
+        const char* device_extensions[] = { 'VK_KHR_swapchain' };
+        const float queue_priority[] = { 1.0f };
+        VkDeviceQueueCreateInfo queue_info[1] = {};
+        queue_info[0].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queue_info[0].queueFamilyIndex = g_QueueFamily;
+        queue_info[0].queueCount = 1;
+        queue_info[0].pQueuePriorities = queue_priority;
+        VkDeviceCreateInfo create_info = {};
+        create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        create_info.queueCreateInfoCount = sizeof(queue_info) / sizeof(queue_info[0]);
+        create_info.pQueueCreateInfos = queue_info;
+        create_info.enabledExtensionCount = device_extension_count;
+        create_info.ppEnabledExtensionNames = device_extensions;
+        err = vkCreateDevice(g_PhysicalDevice, &create_info, g_Allocator, &g_Device);
+        check_vk_result(err);
+        vkGetDeviceQueue(g_Device, g_QueueFamily, 0, &g_Queue);
+    }
+    
+        // Setup Platform/Renderer bindings
+    ImGui_ImplGLUT_Init();
+    ImGui_ImplGLUT_InstallFuncs();
+    ImGui_ImplOpenGL2_Init();
+    
+        // Setup Vulkan
+    uint32_t extensions_count = 0;
+    SDL_Vulkan_GetInstanceExtensions(window, &extensions_count, NULL);
+    const char** extensions = new const char*[extensions_count];
+    SDL_Vulkan_GetInstanceExtensions(window, &extensions_count, extensions);
+    SetupVulkan(extensions, extensions_count);
+    delete[] extensions;
     
     
-//-----------------------------------------------------------------------------
-//
-//	Low-level file input and output for OpenEXR.
-//
-//-----------------------------------------------------------------------------
+    {    switch (msg)
+    {
+    case WM_SIZE:
+        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+        {
+            CleanupRenderTarget();
+            g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+            CreateRenderTarget();
+        }
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        return 0;
+    }
+    return ::DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
     
-    		  case OPENEXR_IMF_INTERNAL_NAMESPACE::HALF:
-    
-    //-----------------------------------------------------------------------------
-//
-//	class InputFile -- a scanline-based interface that can be used
-//	to read both scanline-based and tiled OpenEXR image files.
-//
-//-----------------------------------------------------------------------------
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
