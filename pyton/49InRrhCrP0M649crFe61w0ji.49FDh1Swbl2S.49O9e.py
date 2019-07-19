3818b60c011ee3b43e1b2de22da46755bab70a64
SUNsung <weatherius@gmail.com>
2019-07-19 22:52:34 +0300

    Rewrite [49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py] File in: 2019-07-19 22:52:33

--- pyton/49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py
+++ pyton/49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py
@@ -1,204 +1,194 @@
 
         
-        
-class Transaction(object):
+          This function can be used as an initialization function (referred to as
+  init_fn in TensorFlow documentation) which is called in a Session after
+  initializating all variables. When used as an init_fn, this will load
+  a pre-trained checkpoint that is compatible with the VGGish model
+  definition. Only variables defined by VGGish will be loaded.
     
+    from tensorflow.python.platform import app
+from delf.python.detect_to_retrieve import aggregation_extraction
+from delf.python.detect_to_retrieve import dataset
     
-class HitCounts(MRJob):
+          # Start input enqueue threads.
+      coord = tf.train.Coordinator()
+      threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+      start = time.clock()
+      for i, image_path in enumerate(image_paths):
+        # Write to log-info once in a while.
+        if i == 0:
+          tf.logging.info('Starting to detect objects in images...')
+        elif i % _STATUS_CHECK_ITERATIONS == 0:
+          elapsed = (time.clock() - start)
+          tf.logging.info(
+              'Processing image %d out of %d, last %d '
+              'images took %f seconds', i, num_images, _STATUS_CHECK_ITERATIONS,
+              elapsed)
+          start = time.clock()
     
-        def bfs(self, source, dest):
-        if source is None:
-            return False
-        queue = deque()
-        queue.append(source)
-        source.visit_state = State.visited
-        while queue:
-            node = queue.popleft()
-            print(node)
-            if dest is node:
-                return True
-            for adjacent_node in node.adj_nodes.values():
-                if adjacent_node.visit_state == State.unvisited:
-                    queue.append(adjacent_node)
-                    adjacent_node.visit_state = State.visited
-        return False
+      # Global Average Precision.
+  print('**********************************************')
+  print('(Public)  Global Average Precision: %f' %
+        metrics.GlobalAveragePrecision(public_predictions, public_solution))
+  print('(Private) Global Average Precision: %f' %
+        metrics.GlobalAveragePrecision(private_predictions, private_solution))
     
-        def set(self, key, value):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                item.value = value
-                return
-        self.table[hash_index].append(Item(key, value))
+        def validate(self, value):
+        pass
     
-    
-class QuadraticProbing(HashTable):
-    '''
-        Basic Hash Table example with open addressing using Quadratic Probing 
-    '''
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-    
+        def test_get(self):
         '''
-        loop over all possible splits for the decision tree. find the best split.
-        if no split exists that is less than 2 * error for the entire array
-        then the data set is not split and the average for the entire array is used as the predictor
+        Test a view that simply renders a template on GET
         '''
-        for i in range(len(X)):
-            if len(X[:i]) < self.min_leaf_size:
-                continue
-            elif len(X[i:]) < self.min_leaf_size:
-                continue
-            else:
-                error_left = self.mean_squared_error(X[:i], np.mean(y[:i]))
-                error_right = self.mean_squared_error(X[i:], np.mean(y[i:]))
-                error = error_left + error_right
-                if error < min_error:
-                    best_split = i
-                    min_error = error
+        self._assert_about(AboutTemplateView.as_view()(self.rf.get('/about/')))
     
-    # Mock test below
-if False: # change to true to run this test case.
-    import sklearn.datasets as ds
-    dataset = ds.load_iris()
-    k = 3
-    heterogeneity = []
-    initial_centroids = get_initial_centroids(dataset['data'], k, seed=0)
-    centroids, cluster_assignment = kmeans(dataset['data'], k, initial_centroids, maxiter=400,
-                                        record_heterogeneity=heterogeneity, verbose=True)
-    plot_heterogeneity(heterogeneity, k)
+    
+class FixDecimalInputMixin:
+    
+        def test_sum_star_exception(self):
+        msg = 'Star cannot be used with filter. Please specify a field.'
+        with self.assertRaisesMessage(ValueError, msg):
+            Count('*', filter=Q(age=40))
+    
+        def get_with_params(self):
+        with_params = []
+        if self.autosummarize is not None:
+            with_params.append('autosummarize = %s' % ('on' if self.autosummarize else 'off'))
+        if self.pages_per_range is not None:
+            with_params.append('pages_per_range = %d' % self.pages_per_range)
+        return with_params
+    
+            for version in versions:
+            with self.subTest(version=version):
+                ops = FakePostGISOperations(version[0])
+                actual = ops.spatial_version
+                self.assertEqual(version[1:], actual)
+    
+        @property
+    def layer_name(self):
+        'Return the name of the layer for the feature.'
+        name = capi.get_feat_name(self._layer._ldefn)
+        return force_str(name, self.encoding, strings_only=True)
+    
+        def __getitem__(self, index):
+        'Get the Feature at the specified index.'
+        if isinstance(index, int):
+            # An integer index was given -- we cannot do a check based on the
+            # number of features because the beginning and ending feature IDs
+            # are not guaranteed to be 0 and len(layer)-1, respectively.
+            if index < 0:
+                raise IndexError('Negative indices are not allowed on OGR Layers.')
+            return self._make_feature(index)
+        elif isinstance(index, slice):
+            # A slice was given
+            start, stop, stride = index.indices(self.num_feat)
+            return [self._make_feature(fid) for fid in range(start, stop, stride)]
+        else:
+            raise TypeError('Integers and slices may only be used when indexing OGR Layers.')
+    
+            certfile = os.path.join(CertUtil.ca_certdir, commonname + '.crt')
+        with open(certfile, 'wb') as fp:
+            fp.write(OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, cert))
+            if CertUtil.cert_publickey is None:
+                fp.write(OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, pkey))
+        return certfile
+    
+        unexpectedType = property(getUnexpectedType)
+    
+    
+            one.return_value = bid
+    
+        def test_cookie_unchanged(self):
+        self._setSessionCookie(days_old=60)
+        old_session = c.cookies[g.login_cookie].value
+        upgrade_cookie_security()
+        self.assertTrue(c.cookies[g.login_cookie].dirty)
+        self.assertEqual(old_session, c.cookies[g.login_cookie].value)
+    
+        # Control chars break out of quotes in multiple browsers
+    def test_control_chars(self):
+        testcase = u'*{font-family:'foobar\x03;color:red;';}'
+        self.assertInvalid(testcase)
+    
+            post = Link(is_self=True, selftext='''
+Some text here.
+https://example.com
+https://reddit.com/a.PNG''')
+        url = _get_scrape_url(post)
+        self.assertEqual(url, 'https://reddit.com/a.PNG')
+    
+        def test_no_resize(self):
+        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
+                      height=800)
+        url = self.provider.resize_image(image)
+        self.assertEqual(url, 'https://example.com/a.jpg')
+    
+            for width in (108, 216, 320, 640, 960, 1080):
+            url = self.provider.resize_image(image, width)
+            self.assertEqual(url, 'http://s3.amazonaws.com/a.jpg')
 
     
-        Those are used for calculating differences between
-    predicted values and actual values.
+    from r2.lib.souptest import (
+    souptest_fragment,
+    SoupDetectedCrasherError,
+    SoupError,
+    SoupSyntaxError,
+    SoupUnexpectedCDataSectionError,
+    SoupUnexpectedCommentError,
+    SoupUnsupportedAttrError,
+    SoupUnsupportedEntityError,
+    SoupUnsupportedNodeError,
+    SoupUnsupportedSchemeError,
+    SoupUnsupportedTagError,
+)
+    
+        def test_padding_roundtrip(self):
+        from r2.lib.tracking import _unpad_message, _pad_message
+        tested = _unpad_message(_pad_message(MESSAGE))
+        self.assertEquals(MESSAGE, tested)
+    
+        def test_incorrect_anchoring(self):
+        self.assertIsNotSafeRedditUrl('http://www.%s.whatever.com/' % g.domain)
+    
+        def select_renderer(self, request, renderers, format_suffix=None):
+        '''
+        Given a request and a list of renderers, return a two-tuple of:
+        (renderer, media type).
+        '''
+        # Allow URL style format override.  eg. '?format=json
+        format_query_param = self.settings.URL_FORMAT_OVERRIDE
+        format = format_suffix or request.query_params.get(format_query_param)
     
     
-@pytest.fixture
-def httpbin(httpbin):
-    return prepare_url(httpbin)
-    
-        def run(self):
-        try:
-            self.server_sock = self._create_socket_and_bind()
-            # in case self.port = 0
-            self.port = self.server_sock.getsockname()[1]
-            self.ready_event.set()
-            self._handle_requests()
-    
-    _proxy_combos = []
-for prefix, schemes in _schemes_by_var_prefix:
-    for scheme in schemes:
-        _proxy_combos.append(('{}_proxy'.format(prefix), scheme))
-    
-            with Server.basic_response_server(wait_to_close_event=block_server) as (host, port):
-            sock = socket.socket()
-            sock.connect((host, port))
-            sock.sendall(b'send something')
-            time.sleep(2.5)
-            sock.sendall(b'still alive')
-            block_server.set()  # release server block
-    
-    import pytest
-from requests import compat
-from requests.cookies import RequestsCookieJar
-from requests.structures import CaseInsensitiveDict
-from requests.utils import (
-    address_in_network, dotted_netmask, extract_zipped_paths,
-    get_auth_from_url, _parse_content_type_header, get_encoding_from_headers,
-    get_encodings_from_content, get_environ_proxies,
-    guess_filename, guess_json_utf, is_ipv4_address,
-    is_valid_cidr, iter_slices, parse_dict_header,
-    parse_header_links, prepend_scheme_if_needed,
-    requote_uri, select_proxy, should_bypass_proxies, super_len,
-    to_key_val_list, to_native_string,
-    unquote_header_value, unquote_unreserved,
-    urldefragauth, add_dict_to_cookiejar, set_environ)
-from requests._internal_utils import unicode_is_ascii
-    
-    # If false, no module index is generated.
-# texinfo_domain_indices = True
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
-                          help='set spider argument (may be repeated)')
-        parser.add_option('-o', '--output', metavar='FILE',
-                          help='dump scraped items into FILE (use - for stdout)')
-        parser.add_option('-t', '--output-format', metavar='FORMAT',
-                          help='format to use for dumping items with -o')
-    
-        def long_desc(self):
-        return ('Edit a spider using the editor defined in the EDITOR environment'
-                ' variable or else the EDITOR setting')
-    
-        def post_process(self, output):
-        occurrences = 0
-        for x in output:
-            if isinstance(x, self.obj_type):
-                occurrences += 1
-    
-    
-class ScrapyHTTPClientFactory(HTTPClientFactory):
-    '''Scrapy implementation of the HTTPClientFactory overwriting the
-    serUrl method to make use of our Url object that cache the parse
-    result.
+class BasePermission(metaclass=BasePermissionMetaclass):
+    '''
+    A base class from which all permission classes should inherit.
     '''
     
+        @property
+    def data(self):
+        if not _hasattr(self, '_full_data'):
+            self._load_data_and_files()
+        return self._full_data
     
-class AjaxCrawlMiddleware(object):
+    
+@register.filter
+def break_long_headers(header):
     '''
-    Handle 'AJAX crawlable' pages marked as crawlable via meta tag.
-    For more info see https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
+    Breaks headers longer than 160 characters (~page length)
+    when possible (are comma separated)
     '''
+    if len(header) > 160 and ',' in header:
+        header = mark_safe('<br> ' + ', <br>'.join(header.split(',')))
+    return header
+
     
-        def _debug_cookie(self, request, spider):
-        if self.debug:
-            cl = [to_native_str(c, errors='replace')
-                  for c in request.headers.getlist('Cookie')]
-            if cl:
-                cookies = '\n'.join('Cookie: {}\n'.format(c) for c in cl)
-                msg = 'Sending cookies to: {}\n{}'.format(request, cookies)
-                logger.debug(msg, extra={'spider': spider})
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    def _worker(executor_reference, work_queue):
-    try:
-        while True:
-            work_item = work_queue.get(block=True)
-            if work_item is not None:
-                work_item.run()
-                continue
-            executor = executor_reference()
-            # Exit if:
-            #   - The interpreter is shutting down OR
-            #   - The executor that owns the worker has been collected OR
-            #   - The executor that owns the worker has been shutdown.
-            if _shutdown or executor is None or executor._shutdown:
-                # Notice other workers
-                work_queue.put(None)
-                return
-            del executor
-    except BaseException:
-        _base.LOGGER.critical('Exception in worker', exc_info=True)
-    
-        def test_pending_calls_race(self):
-        # Issue #14406: multi-threaded race condition when waiting on all
-        # futures.
-        event = threading.Event()
-        def future_func():
-            event.wait()
-        oldswitchinterval = sys.getcheckinterval()
-        sys.setcheckinterval(1)
-        try:
-            fs = set(self.executor.submit(future_func) for i in range(100))
-            event.set()
-            futures.wait(fs, return_when=futures.ALL_COMPLETED)
-        finally:
-            sys.setcheckinterval(oldswitchinterval)
+class Tag(models.Model):
+    '''
+    Tags have a descriptive slug, and are attached to an arbitrary object.
+    '''
+    tag = models.SlugField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    tagged_item = GenericForeignKey('content_type', 'object_id')
