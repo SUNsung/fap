a782c8ef48dae130715392cbeb0e4d2fb9832f57
SUNsung <weatherius@gmail.com>
2019-07-19 17:37:40 +0300

    Rewrite [16d8t4NGZs5Jk16Xw1rGe1HZU16bK5G.eUoQWk16KYa.cpp] File in: 2019-07-19 17:37:40

--- cccccc/16d8t4NGZs5Jk16Xw1rGe1HZU16bK5G.eUoQWk16KYa.cpp
+++ cccccc/16d8t4NGZs5Jk16Xw1rGe1HZU16bK5G.eUoQWk16KYa.cpp
@@ -1,105 +1,264 @@
 
         
-        
-	std::wstring cmderStart = path;
-	std::wstring cmderTask = taskName;
+        // Get the python wrappers for a list of ops in a OpList.
+// `op_list_buf` should be a pointer to a buffer containing
+// the binary encoded OpList proto, and `op_list_len` should be the
+// length of that buffer.
+string GetPythonWrappers(const char* op_list_buf, size_t op_list_len);
     
-    std::string InternalKey::DebugString() const {
-  std::string result;
-  ParsedInternalKey parsed;
-  if (ParseInternalKey(rep_, &parsed)) {
-    result = parsed.DebugString();
-  } else {
-    result = '(bad)';
-    result.append(EscapeString(rep_));
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    #include 'tensorflow/stream_executor/platform.h'
+    
+    namespace stream_executor {
+namespace host {
+    }
+    }
+    
+      bool is_pointer() const { return is_pointer_; }
+  const DeviceMemory<ElemT>& pointer() const {
+    CHECK(is_pointer());
+    return pointer_;
   }
-  return result;
+  const ElemT& value() const {
+    CHECK(!is_pointer());
+    return value_;
+  }
+    
+    // Get basic type definitions.
+#define IPC_MESSAGE_IMPL
+#include 'content/nw/src/common/common_message_generator.h'
+    
+    EventListener::EventListener(int id,
+  const base::WeakPtr<DispatcherHost>& dispatcher_host,
+  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    }
+    
+    class EventListener : public Base {
+  std::map<int, BaseEvent*> listerners_;
+    }
+    
+    void Menu::Call(const std::string& method,
+                const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  if (method == 'Append') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    Append(object_manager()->GetApiObject<MenuItem>(object_id));
+  } else if (method == 'Insert') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Insert(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Remove') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Remove(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Popup') {
+    int x = 0;
+    arguments.GetInteger(0, &x);
+    int y = 0;
+    arguments.GetInteger(1, &y);
+    content::WebContents* web_contents = content::WebContents::FromRenderFrameHost(rvh);
+    DCHECK(web_contents);
+    zoom::ZoomController* zoom_controller = zoom::ZoomController::FromWebContents(web_contents);
+    }
+    }
+    
+        void Reset() {
+      scw_->Reset();
+    }
+    
+    class NwClipboardReadAvailableTypesFunction : public NWSyncExtensionFunction {
+ public:
+  NwClipboardReadAvailableTypesFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+     protected:
+  ~NwObjDestroyFunction() override;
+    
+      bool NwScreenIsMonitorStartedFunction::RunNWSync(base::ListValue* response, std::string* error) {
+    response->AppendBoolean(NwDesktopCaptureMonitor::GetInstance()->IsStarted());
+    return true;
+  }
+    
+      // implement nw.Screen.initEventListeners()
+  class NwScreenInitEventListenersFunction: public NWSyncExtensionFunction {
+    public:
+      NwScreenInitEventListenersFunction();
+      bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    void get_btree_superblock_and_txn_for_backfilling(
+        cache_conn_t *cache_conn,
+        cache_account_t *backfill_account,
+        scoped_ptr_t<real_superblock_t> *got_superblock_out,
+        scoped_ptr_t<txn_t> *txn_out) {
+    txn_t *txn = new txn_t(cache_conn, read_access_t::read);
+    txn_out->init(txn);
+    txn->set_account(backfill_account);
+    }
+    
+        // Kick off index post construction
+    coro_t::spawn_sometime(std::bind(&rdb_protocol::resume_construct_sindex,
+                                     *sindex_id,
+                                     key_range_t::universe(),
+                                     this,
+                                     drainer.lock()));
+    
+    namespace unittest {
+    }
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22, typename T23, typename T24, typename T25,
+    typename T26, typename T27, typename T28, typename T29, typename T30,
+    typename T31, typename T32, typename T33, typename T34, typename T35,
+    typename T36, typename T37, typename T38, typename T39, typename T40,
+    typename T41, typename T42, typename T43, typename T44, typename T45,
+    typename T46, typename T47>
+internal::ValueArray47<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
+    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
+    T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42, T43,
+    T44, T45, T46, T47> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7,
+    T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15,
+    T16 v16, T17 v17, T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23,
+    T24 v24, T25 v25, T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31,
+    T32 v32, T33 v33, T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39,
+    T40 v40, T41 v41, T42 v42, T43 v43, T44 v44, T45 v45, T46 v46, T47 v47) {
+  return internal::ValueArray47<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
+      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
+      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39,
+      T40, T41, T42, T43, T44, T45, T46, T47>(v1, v2, v3, v4, v5, v6, v7, v8,
+      v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23,
+      v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37,
+      v38, v39, v40, v41, v42, v43, v44, v45, v46, v47);
 }
     
-      // When user keys are misordered
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('bar', 99, kTypeValue)));
+      // Many linked_ptr operations may change p.link_ for some linked_ptr
+  // variable p in the same circle as this object.  Therefore we need
+  // to prevent two such operations from occurring concurrently.
+  //
+  // Note that different types of linked_ptr objects can coexist in a
+  // circle (e.g. linked_ptr<Base>, linked_ptr<Derived1>, and
+  // linked_ptr<Derived2>).  Therefore we must use a single mutex to
+  // protect all linked_ptr objects.  This can create serious
+  // contention in production code, but is acceptable in a testing
+  // framework.
     
-      // Record metadata for testing initial offset functionality
-  static size_t initial_offset_record_sizes_[];
-  static uint64_t initial_offset_last_record_offsets_[];
-  static int num_initial_offset_records_;
+      // Compares two wide C strings, ignoring case.  Returns true iff they
+  // have the same content.
+  //
+  // Unlike wcscasecmp(), this function can handle NULL argument(s).
+  // A NULL C string is considered different to any non-NULL wide C string,
+  // including the empty string.
+  // NB: The implementations on different platforms slightly differ.
+  // On windows, this method uses _wcsicmp which compares according to LC_CTYPE
+  // environment variable. On GNU platform this method uses wcscasecmp
+  // which compares according to LC_CTYPE category of the current locale.
+  // On MacOS X, it uses towlower, which also uses LC_CTYPE category of the
+  // current locale.
+  static bool CaseInsensitiveWideCStringEquals(const wchar_t* lhs,
+                                               const wchar_t* rhs);
     
-    #include 'db/version_set.h'
-#include 'util/coding.h'
-    
-      /**
-   * Use the Iterate method for iterating over options that are stored as
-   * objects in runtime options (e.g. FilesMatch). This function iterates over
-   * the settings passed as ini/hdf, calls back to, generally, the constructor
-   * of the object in question.
-   *
-   * Note: For now, we are not `ini_get()` enabling these type of options as
-   * it is not trivial to come up with a non-hacky and workable way to store
-   * the data correctly. Also, as usual, Hdf takes priority.
-   */
-  static void Iterate(std::function<void (const IniSettingMap&,
-                                          const Hdf&,
-                                          const std::string&)> cb,
-                      const IniSettingMap &ini, const Hdf& config,
-                      const std::string &name, const bool prepend_hhvm = true);
-    
-    struct GlobStreamWrapper final : Stream::Wrapper {
-  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-  req::ptr<Directory> opendir(const String& path) override;
-};
-    
-    template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, F fillCols);
-template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
-    
-    
-    {  /*!
-   * \brief Create a linear updater given name
-   * \param name Name of the linear updater.
-   */
-  static LinearUpdater* Create(const std::string& name);
-};
-    
-    
-    { private:
-  RowBlock<IndexType> out_;
-  std::unique_ptr<Parser<IndexType> > parser_;
-  uint32_t num_col_;
-  std::vector<size_t> offset_;
-  std::vector<IndexType> dense_index_;
-  std::vector<xgboost::bst_float> dense_value_;
-};
-    
-      inline std::vector<Elem>::const_iterator begin() const {  // NOLINT
-    return elem_of_each_node_.begin();
+      template <GTEST_2_TYPENAMES_(U)>
+  tuple& CopyFrom(const GTEST_2_TUPLE_(U)& t) {
+    f0_ = t.f0_;
+    f1_ = t.f1_;
+    return *this;
   }
     
-    #if DMLC_ENABLE_STD_THREAD
-namespace xgboost {
-namespace data {
-    }
-    }
+        Rational result{ prat };
+    destroyrat(prat);
     
-    
-    {    int nindex = index_.num_chunk();
-    int nvalue = value_.num_chunk();
-    int ntotal = nindex + nvalue;
-    #pragma omp parallel for schedule(dynamic, 1) num_threads(nthread_)
-    for (int i = 0; i < ntotal; ++i) {
-      if (i < nindex) {
-        index_.Decompress(i);
-      } else {
-        value_.Decompress(i - nindex);
-      }
-    }
-  }
+    /// <summary>
+/// Initializes the singleton application object.  This is the first line of authored code
+/// executed, and as such is the logical equivalent of main() or WinMain().
+/// </summary>
+App::App()
+{
+    InitializeComponent();
+    Suspending += ref new SuspendingEventHandler(this, &App::OnSuspending);
+}
     
     
     {
-    {bool SimpleDMatrix::SingleColBlock() const { return true; }
-}  // namespace data
-}  // namespace xgboost
+    {
+    {                auto iMantissa = distance(intItr, intEnd) + matches.length(2);
+                if (iMantissa > iMaxMantissa)
+                {
+                    iError = IDS_ERR_INPUT_OVERFLOW;
+                }
+            }
+        }
+        else
+        {
+            iError = IDS_ERR_UNK_CH;
+        }
+    }
+    else
+    {
+        for (const wchar_t& c : numberString)
+        {
+            if (radix == 16)
+            {
+                if (!(iswdigit(c) || (c >= L'A' && c <= L'F')))
+                {
+                    iError = IDS_ERR_UNK_CH;
+                }
+            }
+            else if (c < L'0' || c >= L'0' + radix)
+            {
+                iError = IDS_ERR_UNK_CH;
+            }
+        }
+    }
+    
+    CurrencyHttpClient::CurrencyHttpClient()
+    : m_client(ref new HttpClient())
+    , m_responseLanguage(L'en-US')
+{
+}
+    
+        CalculationManager::Command CalculatorManager::GetCurrentDegreeMode()
+    {
+        if (m_currentDegreeMode == Command::CommandNULL)
+        {
+            m_currentDegreeMode = Command::CommandDEG;
+        }
+        return m_currentDegreeMode;
+    }
+    
+    #endif
+
+    
+      int permFlags;
+  GBool ownerPasswordOk;
+  Guchar fileKey[16];
+  int fileKeyLength;
+  int encVersion;
+  int encRevision;
+  GBool encryptMetadata;
+  CryptAlgorithm encAlgorithm;
+    
+      SoundKind getSoundKind() { return kind; }
+  GooString *getFileName() { return fileName; }
+  double getSamplingRate() { return samplingRate; }
+  int getChannels() { return channels; }
+  int getBitsPerSample() { return bitsPerSample; }
+  SoundEncoding getEncoding() { return encoding; }
