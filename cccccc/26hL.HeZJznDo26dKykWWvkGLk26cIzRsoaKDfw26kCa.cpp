9379c49a3068c801750a283e531fa2c6454d7e0a
SUNsung <weatherius@gmail.com>
2019-07-19 05:18:25 +0300

    Rewrite [26hL.HeZJznDo26dKykWWvkGLk26cIzRsoaKDfw26kCa.cpp] File in: 2019-07-19 05:18:24

--- cccccc/26hL.HeZJznDo26dKykWWvkGLk26cIzRsoaKDfw26kCa.cpp
+++ cccccc/26hL.HeZJznDo26dKykWWvkGLk26cIzRsoaKDfw26kCa.cpp
@@ -1,244 +1,66 @@
 
         
-        #if defined(__APPLE__)
-#include 'Darwin/Cache-Mac.cpp'
-#else
+              union RawDistance raw_distance;
+      raw_distance.bytes[0] = raw->blocks[i].data[k];
+      raw_distance.bytes[1] = raw->blocks[i].data[k + 1];
     
-    CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
-  llvm::SmallString<32> NameBuf(Name);
-  cache_attributes_t Attrs = {
-    CACHE_ATTRIBUTES_VERSION_2,
-    CBs.keyHashCB,
-    CBs.keyIsEqualCB,
-    nullptr,
-    CBs.keyDestroyCB,
-    CBs.valueReleaseCB,
-    nullptr,
-    nullptr,
-    CBs.UserData,
-    CBs.valueRetainCB,
-  };
-    }
-    
-      std::size_t capacity = End - Allocated;
-  std::size_t requiredCapacity = capacity + needed;
-  do {
-    capacity = 2 * capacity + 16;
-  } while (capacity < requiredCapacity);
-    
-    #ifndef DIRECTIONAL_PREPOSITION
-#  define DIRECTIONAL_PREPOSITION(Word) PREPOSITION(Word)
-#endif
-    
-    void
-swift::trimLeadingWhitespaceFromLines(StringRef RawText,
-                                      unsigned WhitespaceToTrim,
-                                      SmallVectorImpl<StringRef> &OutLines) {
-  SmallVector<StringRef, 8> Lines;
-    }
-    
-    
-    {  bool isTypedef() const {
-    assert(isValid());
-    return !Decl.isNull() && Decl.is<const clang::TypedefNameDecl *>();
-  }
-  const clang::TypedefNameDecl *getTypedef() const {
-    assert(isTypedef());
-    return Decl.get<const clang::TypedefNameDecl *>();
-  }
-};
-    
-    /// Translate the given operator character into its mangled form.
-///
-/// Current operator characters:   @/=-+*%<>!&|^~ and the special operator '..'
-char Mangle::translateOperatorChar(char op) {
-  switch (op) {
-    case '&': return 'a'; // 'and'
-    case '@': return 'c'; // 'commercial at sign'
-    case '/': return 'd'; // 'divide'
-    case '=': return 'e'; // 'equal'
-    case '>': return 'g'; // 'greater'
-    case '<': return 'l'; // 'less'
-    case '*': return 'm'; // 'multiply'
-    case '!': return 'n'; // 'negate'
-    case '|': return 'o'; // 'or'
-    case '+': return 'p'; // 'plus'
-    case '?': return 'q'; // 'question'
-    case '%': return 'r'; // 'remainder'
-    case '-': return 's'; // 'subtract'
-    case '~': return 't'; // 'tilde'
-    case '^': return 'x'; // 'xor'
-    case '.': return 'z'; // 'zperiod' (the z is silent)
-    default:
-      return op;
-  }
+    TEST(CyberNodeTest, create_reader) {
+  EXPECT_TRUE(apollo::cyber::OK());
+  apollo::cyber::proto::Chatter chat;
+  apollo::cyber::PyNode node('listener');
+  pr = node.create_reader('channel/chatter', chat.GetTypeName());
+  EXPECT_EQ('apollo.cyber.proto.Chatter', chat.GetTypeName());
+  EXPECT_NE(pr, nullptr);
+  pr->register_func(cbfun);
+  delete pr;
+  pr = nullptr;
 }
-    
-    //========================================================================
-//
-// Modified under the Poppler project - http://poppler.freedesktop.org
-//
-// All changes made under the Poppler project to this file are licensed
-// under GPL version 2 or later
-//
-// Copyright (C) 2006 Scott Turner <scotty1024@mac.com>
-//
-// To see a description of the changes please see the Changelog file that
-// came with your tarball or type make ChangeLog if you are building from git
-//
-//========================================================================
-    
-    //------------------------------------------------------------------------
-// Page
-//------------------------------------------------------------------------
-    
-      if (obj->dictLookup('V', &tmp)->isInt()) {
-    volume = tmp.getInt();
-  }
-  tmp.free();
-    
-      ExternalSecurityHandler(PDFDoc *docA, Object *encryptDictA,
-			  XpdfSecurityHandler *xshA);
-  virtual ~ExternalSecurityHandler();
-    
-    Sound::~Sound()
-{
-  delete fileName;
-  streamObj->free();
-  delete streamObj;
-}
-    
-      int numRetry_;
-    
-    
-    {  const int clen = bittorrent::getCompactLength(family_);
-  // nodes
-  for (std::vector<std::shared_ptr<DHTNode>>::const_iterator i = nodes_.begin(),
-                                                             eoi = nodes_.end();
-       i != eoi; ++i) {
-    const std::shared_ptr<DHTNode>& node = *i;
-    // Write IP address + port in Compact IP-address/port info form.
-    unsigned char compactPeer[COMPACT_LEN_IPV6];
-    int compactlen = bittorrent::packcompact(compactPeer, node->getIPAddress(),
-                                             node->getPort());
-    if (compactlen != clen) {
-      memset(compactPeer, 0, clen);
-    }
-    uint8_t clen1 = clen;
-    // 1byte compact peer format length
-    WRITE_CHECK(fp, &clen1, sizeof(clen1));
-    // 7bytes reserved
-    WRITE_CHECK(fp, zero, 7);
-    // clen bytes compact peer
-    WRITE_CHECK(fp, compactPeer, static_cast<size_t>(clen));
-    // 24-clen bytes reserved
-    WRITE_CHECK(fp, zero, static_cast<size_t>(24 - clen));
-    // 20bytes: node ID
-    WRITE_CHECK(fp, node->getID(), DHT_ID_LENGTH);
-    // 4bytes reserved
-    WRITE_CHECK(fp, zero, 4);
-  }
-  if (fp.close() == EOF) {
-    throw DL_ABORT_EX(
-        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
-  }
-  if (!File(filenameTemp).renameTo(filename)) {
-    throw DL_ABORT_EX(
-        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
-  }
-  A2_LOG_INFO('DHT routing table was saved successfully');
-}
-    
-    #include 'common.h'
-    
-    namespace aria2 {
-    }
-    
-    
-    {  void updateTokenSecret();
-};
-    
-    #endif // D_DHT_TOKEN_UPDATE_COMMAND_H
-
-    
-        bool add(const std::string& addr);
-    
-      // Will be called while on the write thread before the write executes.  If
-  // this function returns a non-OK status, the write will be aborted and this
-  // status will be returned to the caller of DB::Write().
-  virtual Status Callback(DB* db) = 0;
-    
-      uint64_t sleep_debt = 0;
-  uint64_t time_since_last_refill = 0;
-  if (last_refill_time_ != 0) {
-    if (last_refill_time_ > time_now) {
-      sleep_debt = last_refill_time_ - time_now;
-    } else {
-      time_since_last_refill = time_now - last_refill_time_;
-      bytes_left_ +=
-          static_cast<uint64_t>(static_cast<double>(time_since_last_refill) /
-                                kMicrosPerSecond * delayed_write_rate_);
-      if (time_since_last_refill >= kRefillInterval &&
-          bytes_left_ > num_bytes) {
-        // If refill interval already passed and we have enough bytes
-        // return without extra sleeping.
-        last_refill_time_ = time_now;
-        bytes_left_ -= num_bytes;
-        return 0;
-      }
-    }
-  }
-    
-      // Initialize pointer options for each column family
-  for (size_t i = 0; i < loaded_cf_descs.size(); ++i) {
-    auto* loaded_bbt_opt = reinterpret_cast<BlockBasedTableOptions*>(
-        loaded_cf_descs[0].options.table_factory->GetOptions());
-    // Expect the same as BlockBasedTableOptions will be loaded form file.
-    assert(loaded_bbt_opt->block_size == bbt_opts.block_size);
-    // However, block_cache needs to be manually initialized as documented
-    // in rocksdb/utilities/options_util.h.
-    loaded_bbt_opt->block_cache = cache;
-  }
-  // In addition, as pointer options are initialized with default value,
-  // we need to properly initialized all the pointer options if non-defalut
-  // values are used before calling DB::Open().
-  assert(loaded_cf_descs[0].options.compaction_filter == nullptr);
-  loaded_cf_descs[0].options.compaction_filter = compaction_filter.get();
-    
-      PinnableSlice pinnable_val;
-  db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key1', &pinnable_val);
-  assert(s.IsNotFound());
-  // Reset PinnableSlice after each use and before each reuse
-  pinnable_val.Reset();
-  db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key2', &pinnable_val);
-  assert(pinnable_val == 'value');
-  pinnable_val.Reset();
-  // The Slice pointed by pinnable_val is not valid after this point
     
     
     {
-    {}  // namespace experimental
-}  // namespace rocksdb
+    {}  // namespace planning
+}  // namespace apollo
 
     
-      // Instead of creating a snapshot, take ownership of the input snapshot.
-  ManagedSnapshot(DB* db, const Snapshot* _snapshot);
+      void SendThreadFunc() {
+    using common::ErrorCode;
+    using common::time::AsInt64;
+    using common::time::Clock;
+    using common::time::micros;
+    AINFO << 'Send thread starting...';
+    TestCanParam *param = param_ptr();
+    CanClient *client = param->can_client;
+    std::vector<CanFrame> frames;
+    frames.resize(MAX_CAN_SEND_FRAME_LEN);
+    }
     
-      // Comparator used to define the order of keys in the table.
-  // Default: a comparator that uses lexicographic byte-wise ordering
-  //
-  // REQUIRES: The client must ensure that the comparator supplied
-  // here has the same name and orders keys *exactly* the same as the
-  // comparator provided to previous open calls on the same DB.
-  const Comparator* comparator;
-    
-    /*
- * Class:     org_rocksdb_BackupableDBOptions
- * Method:    callbackTriggerIntervalSize
- * Signature: (J)J
+    /**
+ * @brief Cross product between two 2-D vectors from the common start point,
+ *        and end at two other points.
+ * @param start_point The common start point of two vectors in 2-D.
+ * @param end_point_1 The end point of the first vector.
+ * @param end_point_2 The end point of the second vector.
+ *
+ * @return The cross product result.
  */
-jlong Java_org_rocksdb_BackupableDBOptions_callbackTriggerIntervalSize(
-    JNIEnv* /*env*/, jobject /*jobj*/, jlong jhandle) {
-  auto* bopt = reinterpret_cast<rocksdb::BackupableDBOptions*>(jhandle);
-  return static_cast<jlong>(bopt->callback_trigger_interval_size);
-}
+double CrossProd(const Vec2d &start_point, const Vec2d &end_point_1,
+                 const Vec2d &end_point_2);
+    
+    /**
+ * @file
+ **/
+    
+    void TrafficLightUnprotectedLeftTurnScenario::Init() {
+  if (init_) {
+    return;
+  }
+    }
+    
+    
+    {
+    {
+    {
+    {}  // namespace traffic_light
+}  // namespace scenario
+}  // namespace planning
+}  // namespace apollo
