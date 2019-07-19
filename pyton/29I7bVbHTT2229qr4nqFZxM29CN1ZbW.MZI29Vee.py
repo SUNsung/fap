9803f77c5d5e46044cc51c72656458ab3f952513
SUNsung <weatherius@gmail.com>
2019-07-20 00:34:21 +0300

    Rewrite [29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py] File in: 2019-07-20 00:34:21

--- pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
+++ pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
@@ -1,58 +1,164 @@
 
         
-        
-def main():
-    parser = optparse.OptionParser(usage='%prog OUTFILE.md')
-    options, args = parser.parse_args()
-    if len(args) != 1:
-        parser.error('Expected an output filename')
+        class YouTube(VideoExtractor):
+    name = 'YouTube'
     
-    # General information about the project.
-project = u'youtube-dl'
-copyright = u'2014, Ricardo Garcia Gonzalez'
+            if self.tree.find('result').text != '1':
+            log.wtf('API result says failed!')
+            raise 
     
+    #----------------------------------------------------------------------
+#helper
+#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python
+def dictify(r,root=True):
+    if root:
+        return {r.tag : dictify(r, False)}
+    d=copy(r.attrib)
+    if r.text:
+        d['_text']=r.text
+    for x in r.findall('./*'):
+        if x.tag not in d:
+            d[x.tag]=[]
+        d[x.tag].append(dictify(x,False))
+    return d
     
-# And add them to TestDownload
-for n, test_case in enumerate(defs):
-    tname = 'test_' + str(test_case['name'])
-    i = 1
-    while hasattr(TestDownload, tname):
-        tname = 'test_%s_%d' % (test_case['name'], i)
-        i += 1
-    test_method = generator(test_case, tname)
-    test_method.__name__ = str(tname)
-    ie_list = test_case.get('add_ie')
-    test_method.add_ie = ie_list and ','.join(ie_list)
-    setattr(TestDownload, test_method.__name__, test_method)
-    del test_method
+    __all__ = ['dailymotion_download']
     
-    from youtube_dl.jsinterp import JSInterpreter
+    def ehow_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+	
+	assert re.search(r'http://www.ehow.com/video_', url), 'URL you entered is not supported'
     
+    # Dict of object file names with shared library names to check after building.
+# This is to ensure that we ended up dynamically linking with the shared
+# library paths and versions we expected.  For example:
+#   EXPECTED_SHARED_LIBS['_tkinter.so'] = [
+#                       '/Library/Frameworks/Tcl.framework/Versions/8.5/Tcl',
+#                       '/Library/Frameworks/Tk.framework/Versions/8.5/Tk']
+EXPECTED_SHARED_LIBS = {}
     
-class TestDailymotionSubtitles(BaseTestSubtitles):
-    url = 'http://www.dailymotion.com/video/xczg00'
-    IE = DailymotionIE
+            text.configure(state='normal') # enable text editing
+        text.delete('1.0','end')
+        text.insert('1.0',test_spec['msg'])
+        text.configure(state='disabled') # preserve read-only property
     
-        # < Rotation > #
-    # The old rotation method would rotate the image to find a face, then
-    # store the rotated landmarks along with a rotation value to tell the
-    # convert process that it had to rotate the frame to find the landmarks.
-    # This is problematic for numerous reasons. The process now rotates the
-    # landmarks to correctly correspond with the original frame. The below are
-    # functions to convert legacy alignments to the currently supported
-    # infrastructure.
-    # This can eventually be removed
-    
-        def build(self):
-        ''' Build the session graph '''
-        logger.debug('Building session graph')
-        self.toolbar_place(self)
-        self.plotcanvas.draw()
-        logger.debug('Built session graph')
+        def __call__(self, event, args):
+        if self.closed:
+            return
+        self.seen.append((event, args))
+        if event in self.raise_on_events:
+            raise self.exc_type('saw event ' + event)
     
     
-class Color(Adjustment):
-    ''' Seamless clone the swapped face into the old face with cv2
-        NB: This probably isn't the best place for this, but it doesn't work well and
-        and does not have a natural home, so here for now.
+(Frozen_ILLoadModuleTests,
+ Source_ILLoadModuleTests
+ ) = test_util.test_both(InspectLoaderLoadModuleTests,
+                         InspectLoaderSubclass=SPLIT_IL,
+                         init=init,
+                         util=util)
+    
+            expected = {9: 1, 18: 2, 19: 2, 27: 3, 28: 3, 29: 3, 36: 4, 37: 4,
+                    38: 4, 39: 4, 45: 5, 46: 5, 47: 5, 48: 5, 49: 5, 54: 6,
+                    55: 6, 56: 6, 57: 6, 58: 6, 59: 6, 63: 7, 64: 7, 65: 7,
+                    66: 7, 67: 7, 68: 7, 69: 7, 72: 8, 73: 8, 74: 8, 75: 8,
+                    76: 8, 77: 8, 78: 8, 79: 8, 81: 9, 82: 9, 83: 9, 84: 9,
+                    85: 9, 86: 9, 87: 9, 88: 9, 89: 9}
+        actual = {g: v for v in range(10) for g in range(v * 9, v * 10)}
+        self.assertEqual(g, 'Global variable')
+        self.assertEqual(actual, expected)
+    
+        def test_named_expression_assignment_06(self):
+        (z := (y := (x := 0)))
+    
+        def test_compile_suite(self):
+        st = parser.suite('x = 2; y = x + 3')
+        code = parser.compilest(st)
+        globs = {}
+        exec(code, globs)
+        self.assertEqual(globs['y'], 5)
+    
+        Accepts a dataclass or an instance of one. Tuple elements are of
+    type Field.
     '''
+    
+        ''' Returns a parsed version of Python's sys.version as tuple
+        (name, version, branch, revision, buildno, builddate, compiler)
+        referring to the Python implementation name, version, branch,
+        revision, build number, build date/time as string and the compiler
+        identification string.
+    
+        def __init__(self, filenames=(), strict=True):
+        if not inited:
+            init()
+        self.encodings_map = _encodings_map_default.copy()
+        self.suffix_map = _suffix_map_default.copy()
+        self.types_map = ({}, {}) # dict for (non-strict, strict)
+        self.types_map_inv = ({}, {})
+        for (ext, type) in _types_map_default.items():
+            self.add_type(type, ext, True)
+        for (ext, type) in _common_types_default.items():
+            self.add_type(type, ext, False)
+        for name in filenames:
+            self.read(name, strict)
+    
+            Result is tuple of 2 ints (message count, mailbox size)
+        '''
+        retval = self._shortcmd('STAT')
+        rets = retval.split()
+        if self._debugging: print('*stat*', repr(rets))
+        numMessages = int(rets[1])
+        sizeMessages = int(rets[2])
+        return (numMessages, sizeMessages)
+    
+    if args.quiet:
+    jieba.setLogLevel(60)
+if args.pos:
+    import jieba.posseg
+    posdelim = args.pos
+    def cutfunc(sentence, _, HMM=True):
+        for w, f in jieba.posseg.cut(sentence, HMM):
+            yield w + posdelim + f
+else:
+    cutfunc = jieba.cut
+    
+    if sys.platform.startswith('java'):
+    start_P, trans_P, emit_P = load_model()
+else:
+    from .prob_start import P as start_P
+    from .prob_trans import P as trans_P
+    from .prob_emit import P as emit_P
+    
+            for y in obs_states:
+            prob, state = max((V[t - 1][y0] + trans_p[y0].get(y, MIN_INF) +
+                               emit_p[y].get(obs[t], MIN_FLOAT), y0) for y0 in prev_states)
+            V[t][y] = prob
+            mem_path[t][y] = state
+    
+    if withWeight is True:
+    for tag in tags:
+        print('tag: %s\t\t weight: %f' % (tag[0],tag[1]))
+else:
+    print(','.join(tags))
+
+    
+    tags = jieba.analyse.extract_tags(content,topK=topK)
+    
+        def initialize_plane_and_field(self):
+        self.initialize_plane()
+        self.initialize_vector_field()
+        field = self.vector_field
+        field.set_opacity(0.35)
+        self.add(self.plane, field)
+    
+    
+class AltHeatRHS(Scene):
+    def construct(self):
+        formula = TexMobject(
+            '{\\alpha \\over 2}', '\\Big(',
+            'T({x} - 1, {t}) + T({x} + 1, {t})'
+            '\\Big)',
+            tex_to_color_map={
+                '{x}': GREEN,
+                '{t}': YELLOW,
+            }
+        )
+        self.add(formula)
