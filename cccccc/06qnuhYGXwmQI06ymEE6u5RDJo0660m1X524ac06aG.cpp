a703f19394e90b5a31f2bb3da27947e0e27b2914
SUNsung <weatherius@gmail.com>
2019-07-19 08:35:00 +0300

    Rewrite [06qnuhYGXwmQI06ymEE6u5RDJo0660m1X524ac06aG.cpp] File in: 2019-07-19 08:34:59

--- cccccc/06qnuhYGXwmQI06ymEE6u5RDJo0660m1X524ac06aG.cpp
+++ cccccc/06qnuhYGXwmQI06ymEE6u5RDJo0660m1X524ac06aG.cpp
@@ -1,236 +1,256 @@
 
         
-          // This random number generator facade hides boost and CUDA rng
-  // implementation from one another (for cross-platform compatibility).
-  class RNG {
-   public:
-    RNG();
-    explicit RNG(unsigned int seed);
-    explicit RNG(const RNG&);
-    RNG& operator=(const RNG&);
-    void* generator();
-   private:
-    class Generator;
-    shared_ptr<Generator> generator_;
-  };
+          /// Add delayed missing witnesses for the given normal protocol conformance.
+  void addDelayedMissingWitnesses(NormalProtocolConformance *conformance,
+                                  ArrayRef<ValueDecl*> witnesses);
     
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the predictions @f$ x @f$, a Blob with values in
-   *      @f$ [-\infty, +\infty] @f$ indicating the predicted score for each of
-   *      the @f$ K = CHW @f$ classes. Each @f$ x_n @f$ is mapped to a predicted
-   *      label @f$ \hat{l}_n @f$ given by its maximal index:
-   *      @f$ \hat{l}_n = \arg\max\limits_k x_{nk} @f$
-   *   -# @f$ (N \times 1 \times 1 \times 1) @f$
-   *      the labels @f$ l @f$, an integer-valued Blob with values
-   *      @f$ l_n \in [0, 1, 2, ..., K - 1] @f$
-   *      indicating the correct class label among the @f$ K @f$ classes
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
-   *      the computed accuracy: @f$
-   *        \frac{1}{N} \sum\limits_{n=1}^N \delta\{ \hat{l}_n = l_n \}
-   *      @f$, where @f$
-   *      \delta\{\mathrm{condition}\} = \left\{
-   *         \begin{array}{lr}
-   *            1 & \mbox{if condition} \\
-   *            0 & \mbox{otherwise}
-   *         \end{array} \right.
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+    #include 'llvm/Support/Compiler.h'
+#include 'swift/Runtime/Config.h'
+    
+    namespace swift {
     }
     
-    
-    { private:
-  struct pair_sort_first {
-    bool operator()(const std::pair<int, int> &left,
-                    const std::pair<int, int> &right) {
-      return left.first < right.first;
+    namespace caffe2 {
     }
-  };
-  void check_batch_reindex(int initial_num, int final_num,
-                           const Dtype* ridx_data);
-};
     
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
+      // Ensure nomnigraph can handle this change
+  auto nn = caffe2::convertToNNModule(net);
+  auto new_net = caffe2::convertToCaffe2Proto(nn);
     
-    #include <vector>
     
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-                           const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-                            const vector<bool>& propagate_down,
-                            const vector<Blob<Dtype>*>& bottom);
+    {  dropout->eval();
+  y = dropout(x);
+  ASSERT_EQ(y.sum().item<float>(), 100);
+}
     
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+      /// Applies batch normalization on the `input` using the given `mean` and
+  /// `variance` statistics.
+  Tensor pure_forward(
+      const Tensor& input,
+      const Tensor& mean,
+      const Tensor& variance);
     
-    #endif  // CAFFE_CUDNN_LRN_LAYER_HPP_
-
+    OPERATOR_SCHEMA(GatherRangesToDense)
+    .NumInputs(2, 3)
+    .NumOutputs(1, INT_MAX)
+    .SetDoc(R'DOC(
+Given DATA tensor of rank 1, and RANGES tensor of rank 3, gather values
+corresponding to each range into a separate output tensor. If the optional input
+KEY tensor is also given, the output will be sorted by KEY for each example.
     
-    #ifdef USE_CUDNN
-/*
- * @brief cuDNN implementation of PoolingLayer.
- *        Fallback to PoolingLayer for CPU mode.
-*/
-template <typename Dtype>
-class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
+    template <typename T> class GenericVector;
+template <typename T> class GenericVector;
+    
+    // A smart pointer class that implements a double-ended pointer. Each end
+// points to the other end. The copy constructor and operator= have MOVE
+// semantics, meaning that the relationship with the other end moves to the
+// destination of the copy, leaving the source unattached.
+// For this reason both the copy constructor and the operator= take a non-const
+// reference argument, and the const reference versions cannot be used.
+// DoublePtr is useful to incorporate into structures that are part of a
+// collection such as GenericVector or STL containers, where reallocs can
+// relocate the members. DoublePtr is also useful in a GenericHeap, where it
+// can correctly maintain the pointer to an element of the heap despite it
+// getting moved around on the heap.
+class DoublePtr {
  public:
-  explicit CuDNNPoolingLayer(const LayerParameter& param)
-      : PoolingLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNPoolingLayer();
-  // Currently, cuDNN does not support the extra top blob.
-  virtual inline int MinTopBlobs() const { return -1; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
+  DoublePtr() : other_end_(nullptr) {}
+  // Copy constructor steals the partner off src and is therefore a non
+  // const reference arg.
+  // Copying a const DoublePtr generates a compiler error.
+  DoublePtr(DoublePtr& src) {
+    other_end_ = src.other_end_;
+    if (other_end_ != nullptr) {
+      other_end_->other_end_ = this;
+      src.other_end_ = nullptr;
     }
-    
-    #include 'rect.h'    // for TBOX
-#include 'strngs.h'  // for STRING
-    
-    // Reads all boxes from the string. Otherwise, as ReadAllBoxes.
-// continue_on_failure allows reading to continue even if an invalid box is
-// encountered and will return true if it succeeds in reading some boxes.
-// It otherwise gives up and returns false on encountering an invalid box.
-bool ReadMemBoxes(int target_page, bool skip_blanks, const char* box_data,
-                  bool continue_on_failure,
-                  GenericVector<TBOX>* boxes,
-                  GenericVector<STRING>* texts,
-                  GenericVector<STRING>* box_texts,
-                  GenericVector<int>* pages);
-    
-      // Deletes the box with the given index, and shuffles up the rest.
-  // Recomputes the bounding box.
-  void DeleteBox(int index);
-    
-    #endif  // TESSERACT_CCMAIN_OTSUTHR_H_
-
-    
-    // A useful base class to facilitate the common operation of sorting a vector
-// of owned pointer data using a separate key. This class owns its data pointer,
-// deleting it when it has finished with it, and providing copy constructor and
-// operator= that have move semantics so that the data does not get copied and
-// only a single instance of KDPtrPair holds a specific data pointer.
-template <typename Key, typename Data>
-class KDPtrPair {
- public:
-  KDPtrPair() : data_(nullptr) {}
-  KDPtrPair(Key k, Data* d) : data_(d), key_(k) {}
-  // Copy constructor steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  KDPtrPair(KDPtrPair& src) : data_(src.data_), key_(src.key_) {
-    src.data_ = nullptr;
   }
-  // Destructor deletes data, assuming it is the sole owner.
-  ~KDPtrPair() {
-    delete this->data_;
-    this->data_ = nullptr;
-  }
-  // Operator= steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  void operator=(KDPtrPair& src) {
-    delete this->data_;
-    this->data_ = src.data_;
-    src.data_ = nullptr;
-    this->key_ = src.key_;
+  // Operator= steals the partner off src, and therefore needs src to be a non-
+  // const reference.
+  // Assigning from a const DoublePtr generates a compiler error.
+  void operator=(DoublePtr& src) {
+    Disconnect();
+    other_end_ = src.other_end_;
+    if (other_end_ != nullptr) {
+      other_end_->other_end_ = this;
+      src.other_end_ = nullptr;
+    }
   }
     }
     
-      // Return a pointer to the object identified by id.
-  // If we haven't yet loaded the object, use loader to load it.
-  // If loader fails to load it, record a nullptr entry in the cache
-  // and return nullptr -- further attempts to load will fail (even
-  // with a different loader) until DeleteUnusedObjects() is called.
-  // We delete the given loader.
-  T *Get(STRING id,
-         TessResultCallback<T *> *loader) {
-    T *retval = nullptr;
-    mu_.Lock();
-    for (int i = 0; i < cache_.size(); i++) {
-      if (id == cache_[i].id) {
-        retval = cache_[i].object;
-        if (cache_[i].object != nullptr) {
-          cache_[i].count++;
-        }
-        mu_.Unlock();
-        delete loader;
-        return retval;
-      }
+    // Compute the distance between the given feature vector and the last
+// Set feature vector.
+double IntFeatureDist::DebugFeatureDistance(
+    const GenericVector<int>& features) const {
+  const int num_test_features = features.size();
+  const double denominator = total_feature_weight_ + num_test_features;
+  double misses = denominator;
+  for (int i = 0; i < num_test_features; ++i) {
+    const int index = features[i];
+    const double weight = 1.0;
+    INT_FEATURE_STRUCT f = feature_map_->InverseMapFeature(features[i]);
+    tprintf('Testing feature weight %g:', weight);
+    f.print();
+    if (features_[index]) {
+      // A perfect match.
+      misses -= 2.0 * weight;
+      tprintf('Perfect hit\n');
+    } else if (features_delta_one_[index]) {
+      misses -= 1.5 * weight;
+      tprintf('-1 hit\n');
+    } else if (features_delta_two_[index]) {
+      // A near miss.
+      misses -= 1.0 * weight;
+      tprintf('-2 hit\n');
+    } else {
+      tprintf('Total miss\n');
     }
-    cache_.push_back(ReferenceCount());
-    ReferenceCount &rc = cache_.back();
-    rc.id = id;
-    retval = rc.object = loader->Run();
-    rc.count = (retval != nullptr) ? 1 : 0;
-    mu_.Unlock();
-    return retval;
   }
-    
-    #ifndef PLUGIN_CAFFE_CAFFE_COMMON_H_
-#define PLUGIN_CAFFE_CAFFE_COMMON_H_
-    
-    KVStore* KVStore::Create(const char *type_name) {
-  std::string tname = type_name;
-  std::transform(tname.begin(), tname.end(), tname.begin(), ::tolower);
-  KVStore* kv = nullptr;
-  bool use_device_comm = false;
-  auto has = [tname](const std::string& pattern) {
-    return tname.find(pattern) != std::string::npos;
-  };
-  if (has('device')) {
-    use_device_comm = true;
-  }
+  tprintf('Features present:');
+  for (int i = 0; i < size_; ++i) {
+    if (features_[i]) {
+      INT_FEATURE_STRUCT f = feature_map_->InverseMapFeature(i);
+      f.print();
     }
+  }
+  tprintf('\nMinus one features:');
+  for (int i = 0; i < size_; ++i) {
+    if (features_delta_one_[i]) {
+      INT_FEATURE_STRUCT f = feature_map_->InverseMapFeature(i);
+      f.print();
+    }
+  }
+  tprintf('\nMinus two features:');
+  for (int i = 0; i < size_; ++i) {
+    if (features_delta_two_[i]) {
+      INT_FEATURE_STRUCT f = feature_map_->InverseMapFeature(i);
+      f.print();
+    }
+  }
+  tprintf('\n');
+  return misses / denominator;
+}
+    
+        /**
+     * Sets the global Timestamp to be 'newTime'.
+     */
+    void setNewTimestamp(ServiceContext* opCtx, const Timestamp& newTime);
+    
+    TEST(MatchExpressionParserTest, NearSphereParsesSuccessfullyWhenAllowed) {
+    auto query = fromjson('{a: {$nearSphere: {$geometry: {type: 'Point', coordinates: [0, 0]}}}}');
+    boost::intrusive_ptr<ExpressionContextForTest> expCtx(new ExpressionContextForTest());
+    ASSERT_OK(MatchExpressionParser::parse(query,
+                                           expCtx,
+                                           ExtensionsCallbackNoop(),
+                                           MatchExpressionParser::AllowedFeatures::kGeoNear)
+                  .getStatus());
+}
+    
+                _collection = _database->getCollection(&_opCtx, nss());
+            if (_collection) {
+                _database->dropCollection(&_opCtx, nss()).transitional_ignore();
+            }
+            _collection = _database->createCollection(&_opCtx, nss());
+    
+        BSONObj keyPattern = BSON('a' << 1 << 'b' << 1);
+    createIndex(collection,
+                BSON('name'
+                     << 'a_1_b_1'
+                     << 'ns'
+                     << _nss.ns()
+                     << 'key'
+                     << keyPattern
+                     << 'v'
+                     << static_cast<int>(kIndexVersion)))
+        .transitional_ignore();
+    
+            // Set up count, and run
+        auto params = makeCountScanParams(&_opCtx, getIndex(ctx.db(), BSON('a' << 1)));
+        params.startKey = BSON('' << 2);
+        params.startKeyInclusive = false;
+        params.endKey = BSON('' << 3);
+        params.endKeyInclusive = true;
+    
+    RemoveTransliterator::~RemoveTransliterator() {}
     
     
     {
-    {.add_argument('data', 'Symbol or Symbol[]', 'Tensor or List of Tensors, the second input '
-'will be used as crop_like shape reference')
-.add_arguments(CropParam::__FIELDS__())
-.set_key_var_num_args('num_args');
-}  // namespace op
-}  // namespace mxnet
+    {
+    {
+    {       static double TimeConstant2Momentum(double timeConstant, size_t syncPeroid)
+       {
+           return exp(-((double)syncPeroid) / timeConstant);
+       }
+       static double Momentum2TimeConstant(double bm, size_t syncPeroid)
+       {
+           if (bm >= 1.0 || bm < 0.0)
+           {
+               InvalidArgument('Unexpected block momentum (%.2f). Block momentum should be in the range of [0,1)\n', bm);
+           }
+           return -(double)syncPeroid / log(bm); 
+       }
+    };
+} } }
 
     
-    namespace mxnet {
-namespace op {
-template<>
-Operator *CreateOp<cpu>(NativeOpParam param) {
-  return new NativeOp<cpu>(param);
-}
+                    // evaluate only scalar parameters
+                vector<void*> params = EvaluateParameters(node, baseName, id, parameter.size() - id, pass);
+                id = 0; // reset counter because the params array starts at zero
+                size_t kernelWidth = ((NDLNode<ElemType>*) params[id++])->GetScalar();
+                size_t kernelHeight = ((NDLNode<ElemType>*) params[id++])->GetScalar();
+                size_t outputChannels = ((NDLNode<ElemType>*) params[id++])->GetScalar();
+                size_t horizontalSubsample = ((NDLNode<ElemType>*) params[id++])->GetScalar();
+                size_t verticalSubsample = ((NDLNode<ElemType>*) params[id++])->GetScalar();
+                assert(id == 5);
+    
+        // Init - Builder Initialize for multiple data sets
+    // config - [in] configuration parameters for the network builder
+    virtual void Init(const ConfigParameters& config)
+    {
+        ConfigParameters newConfig;
+        ConfigValue networkConfig = config('networkDescription', '');
+        ConfigValue dumpFileName = config('dumpFileName', '');
+        DEVICEID_TYPE deviceId = DeviceFromConfig(config);
+        unsigned long randomSeedOffset = config('randomSeedOffset', '0');
+        auto executionEngine = new NDLBuilderImpl<ElemType>(deviceId, randomSeedOffset);
+    }
+    
+        // ResolveVariablesInSingleLine - In this method we replace all substrings of 'configLine' of the form '$varName$'
+    //     (where varName is a variable name), with the value of the 'varName' variable in config.
+    //     We search up the config tree for the value, and we throw an error if we don't find it.
+    //     Note that this process is recursive.  Take the following example: A=1; B=$A$; C=$B$.
+    //     In this example, calling ResolveVariables with $B$, would see B=$A$, then look up the value
+    //     of A and see A=1, and it would then replace the string '$B$' with the string '1'.
+    //     Note that this method ignores comments in 'configString' (though they should probably already be
+    //     removed from 'configString' before calling this method).
+    //     Variables must begin with a letter; e.g. $1 does not trigger.
+    //     If an opening '$' is found without a closing '$', an exception is thrown.
+    // BUGBUG: This does not allow to deliver a string value to a config parameter that contains a $ sign followed by a letter.
+    // configString - the string that you would like to resolve variables in.
+    // returns: A copy of 'configString' with all the variables resolved.
+    std::string ResolveVariablesInSingleLine(const std::string& configLine) const
+    {
+        // ensure that this method was called on a single line (eg, no newline characters exist in 'configLine').
+        if (configLine.find_first_of('\n') != std::string::npos)
+            LogicError('ResolveVariablesInSingleLine() should not be called with a string containing a newline character');
+    }
+    
+    #endif 
+    
+    #define CNTK_CHECKPOINT_VERSION_1 1     // 1 -> no version number 
+#define CNTK_CHECKPOINT_VERSION_2 2      
+#define CNTK_CHECKPOINT_VERSION_3 3     // float smoothed gradients for float16/half parameters
+#define CURRENT_CNTK_CHECKPOINT_VERSION CNTK_CHECKPOINT_VERSION_3
+    
+    private:
+    bool IsInitialized() const { return m_initialized; }
+    void Initialize(const std::vector<Matrix<ElemType>*>& gradients, int numEvalNodes)
+    {
+        int deviceId = gradients[0]->GetDeviceId();
+        for (size_t i = 0; i < gradients.size(); i++)
+        {
+            // Make sure none of the gradient matrixes are sparse - we currently do not support aggregation of sparse gradient matrices
+            if (gradients[i]->GetMatrixType() != DENSE)
+                RuntimeError('Gradient aggregation for sparse gradient matrices is currently unsupported!');
     }
     }
-    
-    public:
-  DHTResponseMessage(const std::shared_ptr<DHTNode>& localNode,
-                     const std::shared_ptr<DHTNode>& remoteNode,
-                     const std::string& transactionID);
-    
-    
-    {} // namespace aria2
-
-    
-      Time getSerializedTime() const { return serializedTime_; }
-    
-    void DHTTaskQueueImpl::addPeriodicTask2(const std::shared_ptr<DHTTask>& task)
-{
-  periodicTaskQueue2_.addTask(task);
-}
-    
-      virtual void process() CXX11_OVERRIDE;
-    
-    bool DHTUnknownMessage::send() { return true; }
