4dbc80076041e31f044e7602da72469f0d3e23da
SUNsung <weatherius@gmail.com>
2019-07-19 16:27:41 +0300

    Rewrite [22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb] File in: 2019-07-19 16:27:41

--- ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
+++ ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
@@ -1,44 +1,82 @@
 
         
-            %w[iOS macOS].each do |platform|
-        abstract_target '#{platform} Pods' do
-            project '#{platform} Modules.xcodeproj'
+            # Get the implementation class for the given Converter.
+    # Returns the Converter instance implementing the given Converter.
+    # klass - The Class of the Converter to fetch.
+    def find_converter_instance(klass)
+      @find_converter_instance ||= {}
+      @find_converter_instance[klass] ||= begin
+        converters.find { |converter| converter.instance_of?(klass) } || \
+          raise('No Converters found for #{klass}')
+      end
+    end
     
-          private
+        context 'include tag with variable and liquid filters' do
+      setup do
+        site = fixture_site('pygments' => true).tap(&:read).tap(&:render)
+        post = site.posts.docs.find do |p|
+          p.basename.eql? '2013-12-17-include-variable-filters.markdown'
+        end
+        @content = post.output
+      end
     
-    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
-Gem.use_paths(LogStash::Environment.logstash_gem_home)
-    
-      parameter 'file', 'the package file name', :attribute_name => :package_file, :required => true
-    
-        desc 'Run one single machine acceptance test'
-    task :single, :machine do |t, args|
-      ENV['LS_VAGRANT_HOST']  = args[:machine]
-      exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/**/**/*_spec.rb']]))
+      p.action do |args, _|
+    if args.empty?
+      Jekyll.logger.error 'A subcommand is required.'
+      puts p
+      abort
+    else
+      subcommand = args.first
+      unless p.has_command? subcommand
+        Jekyll.logger.abort_with 'fatal: 'jekyll #{args.first}' could not' \
+          ' be found. You may need to install the jekyll-#{args.first} gem' \
+          ' or a related gem to be able to use this subcommand.'
+      end
     end
   end
 end
 
     
-              it 'successfully install the plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install logstash-filter-qatest')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest')
-          end
-    
-          # Returns the delta between this element's value and the argument's.
-      #
-      # @note Keyword splats always return a delta of 0
-      #
-      # @return [Integer] the delta between the two values
-      def value_delta(other)
-        HashElementDelta.new(self, other).value_delta
-      end
-    
-            def declaration_with_comment(node)
-          buffer = processed_source.buffer
-          begin_pos = get_source_range(node, comments_as_separators).begin_pos
-          end_line = buffer.line_for_position(node.loc.expression.end_pos)
-          end_pos = buffer.line_range(end_line).end_pos
-          Parser::Source::Range.new(buffer, begin_pos, end_pos)
+              theme.create!
+          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
+                             ' is ready for you in #{theme.path.to_s.cyan}!'
+          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
         end
+        # rubocop:enable Metrics/AbcSize
+      end
+    end
+  end
+end
+
+    
+      def send_reset_password_instructions
+    Workers::ResetPassword.perform_async(self.id)
+  end
+    
+          def render_error(error_description, detailed_error=nil)
+        @error_description = error_description
+        @detailed_error = detailed_error
+        if request.format == :mobile
+          render 'api/openid_connect/error/error.mobile', layout: 'application.mobile'
+        else
+          render 'api/openid_connect/error/error', layout: 'with_header_with_footer'
+        end
+      end
+    end
+  end
+end
+
+    
+        redirect_to stream_path
+  end
+end
+
+    
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+      def like_service
+    @like_service ||= LikeService.new(current_user)
+  end
+end
