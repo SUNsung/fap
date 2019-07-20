c85408d2712fd22dd950165e6320b6a146558879
SUNsung <weatherius@gmail.com>
2019-07-20 04:15:42 +0300

    Rewrite [30M9.sNps8kE307ZESvm7plc630B5fAg4eaYaw30Je4.rb] File in: 2019-07-20 04:15:42

--- ruby/30M9.sNps8kE307ZESvm7plc630B5fAg4eaYaw30Je4.rb
+++ ruby/30M9.sNps8kE307ZESvm7plc630B5fAg4eaYaw30Je4.rb
@@ -1,108 +1,90 @@
 
         
-              def reset!(attrs = {})
-        update_raw_data!(
-          {
-            asset_token: nil,
-            type_of_file: nil,
-            url: nil,
-            original_file_name: nil
-          }.merge(attrs)
-        )
-      end
+        set :vmware_utility_name, 'vagrant-vmware-utility'
+set :vmware_utility_version, '1.0.7'
     
-          # it 'allows the creation of a new language' do
-      #   version.create_languages!(['German', 'English_CA'])
-      #   expect(version.name['German']).to eq('yep, that's the name')
-      #   expect(version.name['English_CA']).to eq('yep, that's the name')
-      # end
-    end
-    
-            value = arguments.first
-        value = yield if value.nil? && block_given?
-    
-            os_version = Snapshot.config[:ios_version] || Snapshot::LatestOsVersion.version(os)
-    
-          def self.details
-        [
-          'This will register an iOS device with the Developer Portal so that you can include it in your provisioning profiles.',
-          'This is an optimistic action, in that it will only ever add a device to the member center. If the device has already been registered within the member center, it will be left alone in the member center.',
-          'The action will connect to the Apple Developer Portal using the username you specified in your `Appfile` with `apple_id`, but you can override it using the `:username` option.'
-        ].join('\n')
-      end
-    
-        it 'can use the git_url short flag from tool options' do
-      stub_commander_runner_args(['change_password', '-r', 'git@github.com:you/your_repo.git'])
-    
-      describe '#depends_on' do
-    it 'allows specifying dependencies' do
-      subject.depends_on('foo')
-      expect(subject.deps.first.name).to eq('foo')
-    end
-    
-          before do
-        allow(subject).to receive(:possible_javas).and_return([java])
-      end
-    
-          def self.from_symbol(sym)
-        str = SYMBOLS.fetch(sym) do
-          raise ArgumentError, 'unknown version #{sym.inspect}'
+                  if cache.file?
+                @logger.info('Loading cached node_name...')
+                @config.node_name = cache.read.strip
+              end
+            end
+          end
         end
-        new(str)
-      end
     
-          if create && !subdir.directory?
-        odebug 'Creating metadata subdirectory #{subdir}.'
-        subdir.mkpath
-      end
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
+    end
     
-              @registry[key_for(type, name)] = PluginSpecification.new(type, name, klazz.ruby_class.java_class)
-        else
-          @registry[key_for(type, name)] = PluginSpecification.new(type, name, klass.javaClass)
+    # Add the test directory to the load path
+$:.unshift File.expand_path('../../', __FILE__)
+    
+            port_name = '#{@machine.config.ssh.guest_port}/tcp'
+        network   = driver.inspect_container(@machine.id)['NetworkSettings']
+    
+        it 'raises an exception if virtualbox is not available' do
+      allow(VagrantPlugins::ProviderVirtualBox::Driver::Meta).to receive(:new).
+        and_raise(Vagrant::Errors::VirtualBoxNotDetected)
+    
+              filter.each do |trigger|
+            index = nil
+            match = false
+            if trigger.only_on
+              trigger.only_on.each do |o|
+                if o.match(guest_name.to_s)
+                  # trigger matches on current guest, so we're fine to use it
+                  match = true
+                  break
+                end
+              end
+              # no matches found, so don't use trigger for guest
+              index = triggers.index(trigger) unless match == true
+            end
+    
+          subject.action(machine, 'up')
+      subject.run
+    end
+    
+        it 'fails if it encounters an error' do
+      allow(machine.state).to receive(:id).and_return(:running)
+      allow(provision).to receive(:provision).and_raise('Nope!')
+      allow(VagrantPlugins::Shell::Provisioner).to receive(:new).
+        and_return(provision)
+    
+        context 'when using '%' in a private_key_path' do
+      let(:private_key_path) { '/tmp/percent%folder' }
+      let(:ssh_info) {{
+        host: 'localhost',
+        port: 2222,
+        username: 'vagrant',
+        private_key_path: [private_key_path],
+        compression: true,
+        dsa_authentication: true
+      }}
+    
+              @env.ui.info('')
         end
-      elsif !exists?(type, name)
-        specification_klass = type == :universal ? UniversalPluginSpecification : PluginSpecification
-        @registry[key_for(type, name)] = specification_klass.new(type, name, klass)
-      else
-        logger.debug('Ignoring, plugin already added to the registry', :name => name, :type => type, :klass => klass)
+    
+        ScratchPad.record []
+    while true
+      begin
+        ScratchPad << enum.next
+      rescue StopIteration
+        break
       end
     end
     
-    # This is basically a copy of the original bundler 'bundle' shim
-# with the addition of the loading of our Bundler patches that
-# modify Bundler's caching behaviour.
-    
-        before do
-      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
-      # Logstash won't update when we have a pinned version in the gemfile so we remove them
-      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
-      expect(logstash).to have_installed?(plugin_name, previous_version)
-    end
-    
-        def gem_config_path(gem_name, relative_config_path)
-      spec = Gem::Specification.find_by_name(gem_name)
-      File.join(spec.gem_dir, relative_config_path)
-    rescue Gem::LoadError => e
-      raise Gem::LoadError,
-            'Unable to find gem #{gem_name}; is the gem installed? #{e}'
-    end
+      before :each do
+    @data = '12345abcde'
+    @zip = [31, 139, 8, 0, 44, 220, 209, 71, 0, 3, 51, 52, 50, 54, 49, 77,
+            76, 74, 78, 73, 5, 0, 157, 5, 0, 36, 10, 0, 0, 0].pack('C*')
+    @io = StringIO.new @zip
+    ScratchPad.clear
   end
-end
-
     
-    module RuboCop
-  module Cop
-    # Monkey-patch Cop for tests to provide easy access to messages and
-    # highlights.
-    class Cop
-      def messages
-        offenses.sort.map(&:message)
-      end
-    
-          # Checks whether this node body is a void context.
-      # Always `true` for `for`.
-      #
-      # @return [true] whether the `for` node body is a void context
-      def void_context?
-        true
+        describe 'with a multi-byte character' do
+      it 'prepends the character to the stream' do
+        @gz.ungetc 'ŷ'
+        @gz.read.should == 'ŷ12345abcde'
       end
