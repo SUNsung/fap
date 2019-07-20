cfbc33524bf9c0f8b7aaf4adbe0c95334bf35d42
SUNsung <weatherius@gmail.com>
2019-07-21 02:43:19 +0300

    Rewrite [34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp] File in: 2019-07-21 02:43:19

--- cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
+++ cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
@@ -1,269 +1,122 @@
 
         
-        Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+        // Get basic type definitions.
+#define IPC_MESSAGE_IMPL
+#include 'content/nw/src/common/common_message_generator.h'
     
-    // Safe container for an owned PyObject. On destruction, the reference count of
-// the contained object will be decremented.
-using Safe_PyObjectPtr = std::unique_ptr<PyObject, detail::PyDecrefDeleter>;
-Safe_PyObjectPtr make_safe(PyObject* o);
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#include 'tensorflow/python/util/kernel_registry.h'
+    {}  // namespace nwapi
     
-    PLATFORM_DEFINE_ID(kCudaPlatformId);
-    
-    namespace stream_executor {
-namespace host {
-    }
+    namespace nwapi {
     }
     
-    // Allows to represent a value that is either a host scalar or a scalar stored
-// on the GPU device.
-template <typename ElemT>
-class HostOrDeviceScalar {
- public:
-  // Not marked as explicit because when using this constructor, we usually want
-  // to set this to a compile-time constant.
-  HostOrDeviceScalar(ElemT value) : value_(value), is_pointer_(false) {}
-  explicit HostOrDeviceScalar(const DeviceMemory<ElemT>& pointer)
-      : pointer_(pointer), is_pointer_(true) {
-    CHECK_EQ(1, pointer.ElementCount());
+    #include 'base/values.h'
+#include 'components/zoom/zoom_controller.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/nw/src/api/menuitem/menuitem.h'
+#include 'content/public/browser/web_contents.h'
+#include 'content/public/common/page_zoom.h'
+#include 'ui/views/controls/menu/menu_runner.h'
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.clearSync', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwClipboardClearSyncFunction);
+};
+    
+      NwScreenDisplayObserver::~NwScreenDisplayObserver() {
+    display::Screen* screen = display::Screen::GetScreen();
+    if (screen) {
+      screen->RemoveObserver(this);
+    }
   }
+    
+      // implement nw.Screen.stopMonitor()
+  class NwScreenStopMonitorFunction : public NWSyncExtensionFunction {
+  public:
+    NwScreenStopMonitorFunction();
+    bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-    #ifndef TENSORFLOW_STREAM_EXECUTOR_LIB_DEMANGLE_H_
-#define TENSORFLOW_STREAM_EXECUTOR_LIB_DEMANGLE_H_
+              auto wx_md_0 = mkldnn::memory::desc(
+              { weights_layer_tz_0 }, mkldnn_dtype, mkldnn::memory::format::ldgoi);
+          auto wx_memory_0 =
+              mkldnn::memory({ wx_md_0, cpu_engine });
+          auto wh_md_0 = mkldnn::memory::desc(
+              { weights_iter_tz_0 }, mkldnn_dtype, mkldnn::memory::format::ldgoi);
+          auto wh_memory_0 =
+              mkldnn::memory({ wh_md_0, cpu_engine });
+          if (D == 2) {
+            DType* wx_0 = workptr;  //  D * ngates * I * H
+            wx_memory_0.set_data_handle(wx_0);
+            DType* wh_0 = wx_0 + D * ngates * I * H;  //  D * ngates * H * H
+            wh_memory_0.set_data_handle(wh_0);
+            workptr = wh_0 + D * ngates * H * H;
+          }
+          op.concat_weight_memory.push_back(wx_memory_0);
+          op.concat_weight_memory.push_back(wh_memory_0);
     
-        struct Size2D {
-        Size2D() : width(0), height(0) {}
-        Size2D(size_t width_, size_t height_) : width(width_), height(height_) {}
-    }
+      auto state = Imperative::Get()->Invoke(Context::CPU(), attrs, ndinputs, ndoutputs);
+  if (Imperative::Get()->is_recording()) {
+    Imperative::Get()->RecordOp(std::move(attrs), ndinputs, ndoutputs, state);
+  }
+    
+    #endif  // MXNET_GRAPH_ATTR_TYPES_H_
+
+    
+      void setBorder(int edge, double border);
     
     
     {
-    {}}
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(s32, s8, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src1]]                              \n\t'
-             'vld1.32 {d2-d3}, [%[src2]]                              \n\t'
-             'vqmovn.s32 d4, q0                                       \n\t'
-             'vqmovn.s32 d5, q1                                       \n\t'
-             'vqmovn.s16  d6, q2                                      \n\t'
-             'vst1.8 {d6}, [%[dst]]                                   \n\t'
-             : /*no output*/
-             : [src1] 'r' (_src + i + 0),
-               [src2] 'r' (_src + i + 4),
-               [dst] 'r' (_dst + i)
-             : 'd0','d1','d2','d3','d4','d5','d6'
-         );
-     }
-})
-#else
-CVT_FUNC(s32, s8, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         int32x4_t vline1_s32 = vld1q_s32(_src + i);
-         int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
+    {
+    {      YGNodeFreeRecursive(root);
     }
-    }
-    
-                for (; j < size.width; j++)
-            {
-                dst[j] = internal::saturate_cast<u8>((src[j] >> shift));
-            }
-        }
-        else // CONVERT_POLICY_WRAP
-        {
-            for (; j < roiw16; j += 16)
-            {
-                internal::prefetch(src + j);
-                int16x8_t v_src0 = vshrq_n_s16(vld1q_s16(src + j), shift),
-                          v_src1 = vshrq_n_s16(vld1q_s16(src + j + 8), shift);
-                int8x16_t v_dst = vcombine_s8(vmovn_s16(v_src0),
-                                              vmovn_s16(v_src1));
-                vst1q_u8(dst + j, vreinterpretq_u8_s8(v_dst));
-            }
-            for (; j < roiw8; j += 8)
-            {
-                int16x8_t v_src = vshrq_n_s16(vld1q_s16(src + j), shift);
-                vst1_u8(dst + j, vreinterpret_u8_s8(vmovn_s16(v_src)));
-            }
-    
-    #include <limits>
-    
-                    uint8x16_t m0 =   vandq_u8(vcgtq_s8(x0, v2), vcgtq_s8(x1, v2));
-                uint8x16_t m1 =   vandq_u8(vcgtq_s8(v1, x0), vcgtq_s8(v1, x1));
-                m0 = vorrq_u8(m0, vandq_u8(vcgtq_s8(x1, v2), vcgtq_s8(x2, v2)));
-                m1 = vorrq_u8(m1, vandq_u8(vcgtq_s8(v1, x1), vcgtq_s8(v1, x2)));
-                m0 = vorrq_u8(m0, vandq_u8(vcgtq_s8(x2, v2), vcgtq_s8(x3, v2)));
-                m1 = vorrq_u8(m1, vandq_u8(vcgtq_s8(v1, x2), vcgtq_s8(v1, x3)));
-                m0 = vorrq_u8(m0, vandq_u8(vcgtq_s8(x3, v2), vcgtq_s8(x0, v2)));
-                m1 = vorrq_u8(m1, vandq_u8(vcgtq_s8(v1, x3), vcgtq_s8(v1, x0)));
-                m0 = vorrq_u8(m0, m1);
-    
-    template <typename T>
-inline void inRangeCheck(const Size2D &_size,
-                         const T * srcBase, ptrdiff_t srcStride,
-                         const T * rng1Base, ptrdiff_t rng1Stride,
-                         const T * rng2Base, ptrdiff_t rng2Stride,
-                         u8 * dstBase, ptrdiff_t dstStride)
-{
-    typedef typename internal::VecTraits<T>::vec128 vec128;
-    typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
-    }
-    
-                if(x) {
-                xx0 = xx1;
-                xx1 = xx2;
-            } else {
-                xx1 = x1;
-                // make border
-                    if (border == BORDER_MODE_REPLICATE || border == BORDER_MODE_REFLECT)
-                    {
-                        xx1 = vset_lane_u8(vget_lane_u8(x1, 0),x1, 7);
-                    }
-                    else if (border == BORDER_MODE_CONSTANT)
-                    {
-                        xx1 = vset_lane_u8(borderValue, x1, 7);
-                    }
-                    else if (border == BORDER_MODE_REFLECT101)
-                    {
-                        xx1 = vset_lane_u8(vget_lane_u8(x1, 1),x1, 7);
-                    }
-            }
-            xx2 = x1;
-    
-    #include <google/protobuf/stubs/logging.h>
-#include <google/protobuf/stubs/common.h>
-    
-    TEST(StructurallyValidTest, InvalidUTF8String) {
-  const string invalid_str('abcd\xA0\xB0\xA0\xB0\xA0\xB0 - xyz789');
-  EXPECT_FALSE(IsStructurallyValidUTF8(invalid_str.data(),
-                                       invalid_str.size()));
-  // Additional check for pointer alignment
-  for (int i = 1; i < 8; ++i) {
-    EXPECT_FALSE(IsStructurallyValidUTF8(invalid_str.data() + i,
-                                         invalid_str.size() - i));
   }
 }
-    
-    
-// Abbreviations: true_type and false_type are structs that represent boolean
-// true and false values. Also define the boost::mpl versions of those names,
-// true_ and false_.
-typedef integral_constant<bool, true>  true_type;
-typedef integral_constant<bool, false> false_type;
-typedef true_type  true_;
-typedef false_type false_;
-    
-    #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-    
-    int main(int argc, char *argv[]) {
-  if (argc % 2 == 0 || argc == 1) {
-    std::cerr << 'Usage: [input_files] [output_file_names] where ' <<
-        'input_files are one to one mapping to output_file_names.' <<
-        std::endl;
-    return 1;
-  }
-    }
-    
-    // This function fills in a Person message based on user input.
-void PromptForAddress(tutorial::Person* person) {
-  cout << 'Enter person ID number: ';
-  int id;
-  cin >> id;
-  person->set_id(id);
-  cin.ignore(256, '\n');
-    }
-    
-    AuthPropertyIterator::AuthPropertyIterator(
-    const grpc_auth_property* property, const grpc_auth_property_iterator* iter)
-    : property_(property),
-      ctx_(iter->ctx),
-      index_(iter->index),
-      name_(iter->name) {}
-    
-    class CensusChannelData : public ChannelData {
- public:
-  grpc_error* Init(grpc_channel_element* elem,
-                   grpc_channel_element_args* args) override;
-};
-    
-      void StartTransportStreamOpBatch(grpc_call_element* elem,
-                                   TransportStreamOpBatch* op) override;
-    
-      explicit CensusContext(absl::string_view name)
-      : span_(::opencensus::trace::Span::StartSpan(name)) {}
-    
-    ::opencensus::stats::MeasureInt64 RpcServerSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcServerReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerServerLatency();
-::opencensus::stats::MeasureInt64 RpcServerCompletedRpcs();
-    
-    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_SERVER_FILTER_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_SERVER_FILTER_H
-    
-    Status ProtoServerReflection::GetFileContainingSymbol(
-    ServerContext* context, const grpc::string& symbol,
-    ServerReflectionResponse* response) {
-  if (descriptor_pool_ == nullptr) {
-    return Status::CANCELLED;
-  }
-    }
-    
-    #include <grpc/support/port_platform.h>
-    
-    const std::set<PerBalancerStore*>* LoadDataStore::GetAssignedStores(
-    const grpc::string& hostname, const grpc::string& lb_id) {
-  auto it = per_host_stores_.find(hostname);
-  if (it == per_host_stores_.end()) return nullptr;
-  return it->second.GetAssignedStores(lb_id);
-}
-    
-    #if TARGET_OS_IPHONE
-GRPC_XMACRO_ITEM(isWWAN, IsWWAN)
-#endif
-GRPC_XMACRO_ITEM(reachable, Reachable)
-GRPC_XMACRO_ITEM(transientConnection, TransientConnection)
-GRPC_XMACRO_ITEM(connectionRequired, ConnectionRequired)
-GRPC_XMACRO_ITEM(connectionOnTraffic, ConnectionOnTraffic)
-GRPC_XMACRO_ITEM(interventionRequired, InterventionRequired)
-GRPC_XMACRO_ITEM(connectionOnDemand, ConnectionOnDemand)
-GRPC_XMACRO_ITEM(isLocalAddress, IsLocalAddress)
-GRPC_XMACRO_ITEM(isDirect, IsDirect)
 
     
-        // output changed array
-    std::cout << array << '\n';
+        /// <summary>
+    /// Gets a global variable and returns the <see cref='JsValueRef' /> result.
+    /// </summary>
+    /// <param name='szPropertyName'>The property name from global to get.</param>
+    /// <param name='result'>[Out] The global property value.</param>
+    /// <returns>
+    /// JsNoError is no error, else a JsErrorCode with the appropriate error.
+    /// </returns>
+    JsErrorCode GetGlobalVariable(const wchar_t* szPropertyName, JsValueRef* result);
     
-        // output element with JSON pointer '/number'
-    std::cout << j.at('/number'_json_pointer) << '\n';
-    // output element with JSON pointer '/string'
-    std::cout << j.at('/string'_json_pointer) << '\n';
-    // output element with JSON pointer '/array'
-    std::cout << j.at('/array'_json_pointer) << '\n';
-    // output element with JSON pointer '/array/1'
-    std::cout << j.at('/array/1'_json_pointer) << '\n';
+    #include 'pch.h'
     
-    using json = nlohmann::json;
+    #define IfFailRetNullPtr(v) \
+    { \
+        JsErrorCode status = (v); \
+        if (status != JsNoError) \
+        { \
+            ChakraStringResult stringResult; \
+            stringResult.ErrorCode = status; \
+            return stringResult; \
+        } \
+    }
+    
+    struct property_Microsoft_UI_Composition_Effects_IBorderEffect
+{ struct named {
+    struct ExtendX
+    {
+        struct name { static constexpr std::wstring_view value{ L'ExtendX'sv }; };
+        using property_type = winrt::Microsoft::UI::Composition::Effects::CanvasEdgeBehavior;
+        using target_type = winrt::Microsoft::UI::Composition::Effects::IBorderEffect;
+    }
+    }
+    }
+    
+      const auto bubblingEventTypesConstants =
+      GetExportedCustomBubblingEventTypeConstants();
+  if (!bubblingEventTypesConstants.empty())
+    constants['bubblingEventTypes'] = bubblingEventTypesConstants;
+  const auto directEventTypesConstants =
+      GetExportedCustomDirectEventTypeConstants();
+  if (!directEventTypesConstants.empty())
+    constants['directEventTypes'] = directEventTypesConstants;
+    
+    namespace Microsoft::React::Test {
+    }
