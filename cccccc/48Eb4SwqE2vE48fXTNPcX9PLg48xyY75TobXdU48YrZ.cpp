401e34ca486d1296a29bc5b41b980187bd673651
SUNsung <weatherius@gmail.com>
2019-07-20 11:55:17 +0300

    Rewrite [48Eb4SwqE2vE48fXTNPcX9PLg48xyY75TobXdU48YrZ.cpp] File in: 2019-07-20 11:55:17

--- cccccc/48Eb4SwqE2vE48fXTNPcX9PLg48xyY75TobXdU48YrZ.cpp
+++ cccccc/48Eb4SwqE2vE48fXTNPcX9PLg48xyY75TobXdU48YrZ.cpp
@@ -1,267 +1,236 @@
 
         
-          // Parallel training
-  int solver_count_;
-  int solver_rank_;
-  bool multiprocess_;
-    
-     protected:
-  /// @copydoc AbsValLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-      int channel_axis_;
-  int num_;
-  int channels_;
-  int group_;
-  int out_spatial_dim_;
-  int weight_offset_;
-  int num_output_;
-  bool bias_term_;
-  bool is_1x1_;
-  bool force_nd_im2col_;
-    
-    /**
- * @brief Normalizes the input to have 0-mean and/or unit (1) variance across
- *        the batch.
- *
- * This layer computes Batch Normalization as described in [1]. For each channel
- * in the data (i.e. axis 1), it subtracts the mean and divides by the variance,
- * where both statistics are computed across both spatial dimensions and across
- * the different examples in the batch.
- *
- * By default, during training time, the network is computing global
- * mean/variance statistics via a running average, which is then used at test
- * time to allow deterministic outputs for each input. You can manually toggle
- * whether the network is accumulating or using the statistics via the
- * use_global_stats option. For reference, these statistics are kept in the
- * layer's three blobs: (0) mean, (1) variance, and (2) moving average factor.
- *
- * Note that the original paper also included a per-channel learned bias and
- * scaling factor. To implement this in Caffe, define a `ScaleLayer` configured
- * with `bias_term: true` after each `BatchNormLayer` to handle both the bias
- * and scaling factor.
- *
- * [1] S. Ioffe and C. Szegedy, 'Batch Normalization: Accelerating Deep Network
- *     Training by Reducing Internal Covariate Shift.' arXiv preprint
- *     arXiv:1502.03167 (2015).
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class BatchNormLayer : public Layer<Dtype> {
- public:
-  explicit BatchNormLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+        void FallBackCompute(FCompute fn, const nnvm::NodeAttrs &attrs,
+                     const OpContext &ctx,
+                     const std::vector<NDArray> &inputs,
+                     const std::vector<OpReqType> &req,
+                     const std::vector<NDArray> &outputs) {
+  std::vector<TBlob> in_blobs(inputs.size());
+  std::vector<NDArray> in_bufs;
+  for (size_t i = 0; i < in_blobs.size(); i++) {
+    // If the input data isn't stored in the default format, we shouldn't
+    // call data() directly, which will change the layout of the NDArray.
+    // Instead, we should save the converted data in another NDArray.
+    // TODO(zhengda) we should use temp space to save the converted data.
+    if (inputs[i].IsDefaultData()) {
+      in_blobs[i] = inputs[i].data();
+    } else {
+      if (in_bufs.empty())
+        in_bufs.reserve(inputs.size());
+      in_bufs.push_back(inputs[i].Reorder2Default());
+      in_blobs[i] = in_bufs.back().data();
     }
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2+)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x_1 @f$
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x_2 @f$
-   *   -# ...
-   *   - K @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x_K @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (KN \times C \times H \times W) @f$ if axis == 0, or
-   *      @f$ (N \times KC \times H \times W) @f$ if axis == 1:
-   *      the concatenated output @f$
-   *        y = [\begin{array}{cccc} x_1 & x_2 & ... & x_K \end{array}]
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-      // algorithms for forward and backwards convolutions
-  cudnnConvolutionFwdAlgo_t *fwd_algo_;
-  cudnnConvolutionBwdFilterAlgo_t *bwd_filter_algo_;
-  cudnnConvolutionBwdDataAlgo_t *bwd_data_algo_;
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of SigmoidLayer.
- */
-template <typename Dtype>
-class CuDNNSigmoidLayer : public SigmoidLayer<Dtype> {
- public:
-  explicit CuDNNSigmoidLayer(const LayerParameter& param)
-      : SigmoidLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNSigmoidLayer();
-    }
-    
-    // Returns a new x-height maximally compatible with the result in word_res.
-// See comment above for overall algorithm.
-float Tesseract::ComputeCompatibleXheight(WERD_RES *word_res,
-                                          float* baseline_shift) {
-  STATS top_stats(0, UINT8_MAX);
-  STATS shift_stats(-UINT8_MAX, UINT8_MAX);
-  int bottom_shift = 0;
-  int num_blobs = word_res->rebuild_word->NumBlobs();
-  do {
-    top_stats.clear();
-    shift_stats.clear();
-    for (int blob_id = 0; blob_id < num_blobs; ++blob_id) {
-      TBLOB* blob = word_res->rebuild_word->blobs[blob_id];
-      UNICHAR_ID class_id = word_res->best_choice->unichar_id(blob_id);
-      if (unicharset.get_isalpha(class_id) ||
-          unicharset.get_isdigit(class_id)) {
-        int top = blob->bounding_box().top() + bottom_shift;
-        // Clip the top to the limit of normalized feature space.
-        if (top >= INT_FEAT_RANGE)
-          top = INT_FEAT_RANGE - 1;
-        int bottom = blob->bounding_box().bottom() + bottom_shift;
-        int min_bottom, max_bottom, min_top, max_top;
-        unicharset.get_top_bottom(class_id, &min_bottom, &max_bottom,
-                                  &min_top, &max_top);
-        // Chars with a wild top range would mess up the result so ignore them.
-        if (max_top - min_top > kMaxCharTopRange)
-          continue;
-        int misfit_dist = std::max((min_top - x_ht_acceptance_tolerance) - top,
-                            top - (max_top + x_ht_acceptance_tolerance));
-        int height = top - kBlnBaselineOffset;
-        if (debug_x_ht_level >= 2) {
-          tprintf('Class %s: height=%d, bottom=%d,%d top=%d,%d, actual=%d,%d: ',
-                  unicharset.id_to_unichar(class_id),
-                  height, min_bottom, max_bottom, min_top, max_top,
-                  bottom, top);
-        }
-        // Use only chars that fit in the expected bottom range, and where
-        // the range of tops is sensibly near the xheight.
-        if (min_bottom <= bottom + x_ht_acceptance_tolerance &&
-            bottom - x_ht_acceptance_tolerance <= max_bottom &&
-            min_top > kBlnBaselineOffset &&
-            max_top - kBlnBaselineOffset >= kBlnXHeight &&
-            misfit_dist > 0) {
-          // Compute the x-height position using proportionality between the
-          // actual height and expected height.
-          int min_xht = DivRounded(height * kBlnXHeight,
-                                   max_top - kBlnBaselineOffset);
-          int max_xht = DivRounded(height * kBlnXHeight,
-                                   min_top - kBlnBaselineOffset);
-          if (debug_x_ht_level >= 2) {
-            tprintf(' xht range min=%d, max=%d\n', min_xht, max_xht);
-          }
-          // The range of expected heights gets a vote equal to the distance
-          // of the actual top from the expected top.
-          for (int y = min_xht; y <= max_xht; ++y)
-            top_stats.add(y, misfit_dist);
-        } else if ((min_bottom > bottom + x_ht_acceptance_tolerance ||
-                    bottom - x_ht_acceptance_tolerance > max_bottom) &&
-                   bottom_shift == 0) {
-          // Get the range of required bottom shift.
-          int min_shift = min_bottom - bottom;
-          int max_shift = max_bottom - bottom;
-          if (debug_x_ht_level >= 2) {
-            tprintf(' bottom shift min=%d, max=%d\n', min_shift, max_shift);
-          }
-          // The range of expected shifts gets a vote equal to the min distance
-          // of the actual bottom from the expected bottom, spread over the
-          // range of its acceptance.
-          int misfit_weight = abs(min_shift);
-          if (max_shift > min_shift)
-            misfit_weight /= max_shift - min_shift;
-          for (int y = min_shift; y <= max_shift; ++y)
-            shift_stats.add(y, misfit_weight);
-        } else {
-          if (bottom_shift == 0) {
-            // Things with bottoms that are already ok need to say so, on the
-            // 1st iteration only.
-            shift_stats.add(0, kBlnBaselineOffset);
-          }
-          if (debug_x_ht_level >= 2) {
-            tprintf(' already OK\n');
-          }
-        }
-      }
-    }
-    if (shift_stats.get_total() > top_stats.get_total()) {
-      bottom_shift = IntCastRounded(shift_stats.median());
-      if (debug_x_ht_level >= 2) {
-        tprintf('Applying bottom shift=%d\n', bottom_shift);
-      }
-    }
-  } while (bottom_shift != 0 &&
-           top_stats.get_total() < shift_stats.get_total());
-  // Baseline shift is opposite sign to the bottom shift.
-  *baseline_shift = -bottom_shift / word_res->denorm.y_scale();
-  if (debug_x_ht_level >= 2) {
-    tprintf('baseline shift=%g\n', *baseline_shift);
   }
-  if (top_stats.get_total() == 0)
-    return bottom_shift != 0 ? word_res->x_height : 0.0f;
-  // The new xheight is just the median vote, which is then scaled out
-  // of BLN space back to pixel space to get the x-height in pixel space.
-  float new_xht = top_stats.median();
-  if (debug_x_ht_level >= 2) {
-    tprintf('Median xht=%f\n', new_xht);
-    tprintf('Mode20:A: New x-height = %f (norm), %f (orig)\n',
-            new_xht, new_xht / word_res->denorm.y_scale());
+  MKLDNNStream::Get()->Submit();
+    }
+    
+    namespace mxnet {
+namespace op {
+    }
+    }
+    
+    /*!
+ *  Copyright (c) 2019 by Contributors
+ * \file all_finite.cc 
+ * \brief operator for checking if a group of array is all finite
+ * \author Clement Fuji Tsang
+ */
+#include './all_finite-inl.h'
+#include <cmath>
+    
+    /*!
+ *  Copyright (c) 2019 by Contributors
+ * \file amp_graph_pass.cc
+ * \brief graph pass regarding AMP
+ * \author Clement Fuji Tsang
+ */
+#include <nnvm/graph.h>
+#include <nnvm/pass.h>
+#include <mxnet/op_attr_types.h>
+    
+          // [start, end) interval for spatial sampling
+      const DType* offset_bottom_rois = bottom_rois + n * 5;
+      index_t roi_batch_ind = offset_bottom_rois[0];
+      DType roi_start_w = static_cast<DType>(round(offset_bottom_rois[1])) * spatial_scale - 0.5;
+      DType roi_start_h = static_cast<DType>(round(offset_bottom_rois[2])) * spatial_scale - 0.5;
+      DType roi_end_w = static_cast<DType>(round(offset_bottom_rois[3]) + 1.) * spatial_scale - 0.5;
+      DType roi_end_h = static_cast<DType>(round(offset_bottom_rois[4]) + 1.) * spatial_scale - 0.5;
+    
+      if ((*out_attrs)[0].ndim() > 0) {
+    dshape[0] = ((*out_attrs)[0])[0];
+    SHAPE_ASSIGN_CHECK(*in_attrs, 0, dshape);
   }
-  // The xheight must change by at least x_ht_min_change to be used.
-  if (fabs(new_xht - kBlnXHeight) >= x_ht_min_change)
-    return new_xht / word_res->denorm.y_scale();
-  else
-    return bottom_shift != 0 ? word_res->x_height : 0.0f;
+    
+    template<int req>
+struct IndexArrayKernel {
+  MSHADOW_XINLINE static void Map(int i,
+                                  int64_t* out_data,
+                                  const int n,
+                                  const int64_t* workspace) {
+    for (ptrdiff_t j = 0; j < n; j++) {
+      int64_t upper = workspace[ptrdiff_t(2) * j];
+      int64_t lower = workspace[ptrdiff_t(2) * j + ptrdiff_t(1)];
+      KERNEL_ASSIGN(out_data[ptrdiff_t(i) * ptrdiff_t(n) + j], req, (i % upper) / lower);
+    }
+  }
+};
+    
+        std::vector<int64_t> index_products = IndexArrayComputeIndexProducts(inshape);
+    
+    BSONObj getErrorLabels(const OperationSessionInfoFromClient& sessionOptions,
+                       const std::string& commandName,
+                       ErrorCodes::Error code,
+                       bool hasWriteConcernError) {
+    BSONArrayBuilder labelArray;
+    }
+    
+    namespace mongo {
+    }
+    
+        StageType stageType() const final {
+        return STAGE_COUNT;
+    }
+    
+    TEST(MatchExpressionParserTest, GeoNearFailsToParseWhenDisallowed) {
+    auto query = fromjson('{a: {$geoNear: {$geometry: {type: 'Point', coordinates: [0, 0]}}}}');
+    boost::intrusive_ptr<ExpressionContextForTest> expCtx(new ExpressionContextForTest());
+    ASSERT_NOT_OK(MatchExpressionParser::parse(query, expCtx).getStatus());
 }
     
-    struct OSResults {
-  OSResults() : unicharset(nullptr) {
-    for (int i = 0; i < 4; ++i) {
-      for (int j = 0; j < kMaxNumberOfScripts; ++j)
-        scripts_na[i][j] = 0;
-      orientations[i] = 0;
-    }
-  }
-  void update_best_orientation();
-  // Set the estimate of the orientation to the given id.
-  void set_best_orientation(int orientation_id);
-  // Update/Compute the best estimate of the script assuming the given
-  // orientation id.
-  void update_best_script(int orientation_id);
-  // Return the index of the script with the highest score for this orientation.
-  TESS_API int get_best_script(int orientation_id) const;
-  // Accumulate scores with given OSResults instance and update the best script.
-  void accumulate(const OSResults& osr);
+        if (auto comment = cmd.getComment()) {
+        aggregationBuilder.append(kCommentField, comment.get());
     }
     
-    // Reads all boxes from the string. Otherwise, as ReadAllBoxes.
-// continue_on_failure allows reading to continue even if an invalid box is
-// encountered and will return true if it succeeds in reading some boxes.
-// It otherwise gives up and returns false on encountering an invalid box.
-bool ReadMemBoxes(int target_page, bool skip_blanks, const char* box_data,
-                  bool continue_on_failure,
-                  GenericVector<TBOX>* boxes,
-                  GenericVector<STRING>* texts,
-                  GenericVector<STRING>* box_texts,
-                  GenericVector<int>* pages);
+            for (const auto& response : shardResponses) {
+            auto status = response.swResponse.getStatus();
+            if (status.isOK()) {
+                status = getStatusFromCommandResult(response.swResponse.getValue().data);
+                if (status.isOK()) {
+                    long long shardCount = response.swResponse.getValue().data['n'].numberLong();
+                    shardSubTotal.appendNumber(response.shardId.toString(), shardCount);
+                    total += shardCount;
+                    continue;
+                }
+            }
+    }
     
-    #endif  // TESSERACT_CCUTIL_QRSEQUENCE_H_
+    void Top::append(BSONObjBuilder& b) {
+    stdx::lock_guard<SimpleMutex> lk(_lock);
+    _appendToUsageMap(b, _usage);
+}
+    
+    class CallbackManager
+{
+public:
+    inline void append(swCallback cb, void *private_data)
+    {
+        list_.push_back(new Callback(cb, private_data));
+    }
+    inline void prepend(swCallback cb, void *private_data)
+    {
+        list_.push_front(new Callback(cb, private_data));
+    }
+    inline void execute()
+    {
+        while (!list_.empty())
+        {
+            Callback *task = list_.front();
+            list_.pop_front();
+            task->callback(task->private_data);
+            delete task;
+        }
+    }
+protected:
+    std::list<Callback *> list_;
+};
+    
+    uchar swoole_timer_exists(long timer_id)
+{
+    if (!SwooleG.timer.initialized)
+    {
+        swWarn('no timer');
+        return false;
+    }
+    auto tnode = swTimer_get(&SwooleG.timer, timer_id);
+    return (tnode && !tnode->removed);
+}
+    
+    class coro_test
+{
+public:
+    coro_test(coroutine_func_t _fn, void *_arg, int *_complete_num) : fn(_fn), arg(_arg), complete_num(_complete_num)
+    {
+    }
+    }
+    
+    
+    {            ASSERT_EQ(addr1, addr3);
+            ASSERT_EQ(addr2, addr4);
+        }
+    
+    bool Channel::push(void *data, double timeout)
+{
+    Coroutine *current_co = Coroutine::get_current_safe();
+    if (closed)
+    {
+        return false;
+    }
+    if (is_full() || !producer_queue.empty())
+    {
+        timer_msg_t msg;
+        msg.error = false;
+        msg.timer = NULL;
+        if (timeout > 0)
+        {
+            long msec = (long) (timeout * 1000);
+            msg.chan = this;
+            msg.type = PRODUCER;
+            msg.co = current_co;
+            msg.timer = swTimer_add(&SwooleG.timer, msec, 0, &msg, timer_callback);
+        }
+    }
+    }
+    
+    struct DataBuffer
+{
+    size_t length;
+    void *buffer;
+    }
+    
+        for (i = 0; i < MAP_SIZE; i++)
+    {
+        pkt = (swFdInfo *) malloc(sizeof(swFdInfo));
+        pkt->key = i;
+        pkt->fd = i * 37;
+        swHashMap_add_int(ht, pkt->fd, pkt);
+        lists[i] = pkt;
+    }
+    
+    
+#ifndef callback_h
+#define callback_h
+#include 'mars/comm/thread/thread.h'
+#include 'message_queue.h'
+#include 'mars/comm/xlogger/xlogger.h'
+#include 'boost/bind.hpp'
+    
+    class ShortLinkInterface {
+  public:
+	virtual ~ShortLinkInterface(){};
+    }
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    #include 'mars/comm/thread/mutex.h'
+#include 'mars/comm/singleton.h'
+    
+    const void* AutoBuffer::PosPtr() const {
+    return ((unsigned char*)Ptr()) + Pos();
+}
+    
+        void throw_exception( std::exception const & e ) {
+        xfatal2(TSF'boost exception:%_', e.what());
+    }
