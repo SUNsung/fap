aca36d82ef304585a2f3aa9e75715a9512ca351b
SUNsung <weatherius@gmail.com>
2019-07-20 13:39:42 +0300

    Rewrite [44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb] File in: 2019-07-20 13:39:42

--- ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
+++ ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
@@ -1,93 +1,84 @@
 
         
-        # No trailing slash
-Benchmark.ips do |x|
-  path = '/some/very/very/long/path/to/a/file/i/like/'
-  x.report('pre_pr:#{path}')    { pre_pr(path) }
-  x.report('pr:#{path}')        { pr(path) }
-  x.report('envygeeks:#{path}') { pr(path) }
-  x.compare!
-end
-
-    
-    if pathutil_relative == native_relative
-  Benchmark.ips do |x|
-    x.report('pathutil') { pathutil_relative }
-    x.report('native')   { native_relative }
-    x.compare!
-  end
-else
-  print 'PATHUTIL: '
-  puts pathutil_relative
-  print 'NATIVE:   '
-  puts native_relative
-end
-
-    
-              new_theme_name = args.join('_')
-          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
-    
-    if defined?(ActionMailer)
-  class Devise::Mailer < Devise.parent_mailer.constantize
-    include Devise::Mailers::Helpers
-    
-    class TestApp < Rails::Application
-  config.root = File.dirname(__FILE__)
-  config.session_store :cookie_store, key: 'cookie_store_key'
-  secrets.secret_token    = 'secret_token'
-  secrets.secret_key_base = 'secret_key_base'
-  config.eager_load = false
-    
-          def expire_data_after_sign_in!
-        # session.keys will return an empty array if the session is not yet loaded.
-        # This is a bug in both Rack and Rails.
-        # A call to #empty? forces the session to be loaded.
-        session.empty?
-        session.keys.grep(/^devise\./).each { |k| session.delete(k) }
-      end
-    
-            # Attempt to find a user by its reset_password_token to reset its
-        # password. If a user is found and token is still valid, reset its password and automatically
-        # try saving the record. If not user is found, returns a new user
-        # containing an error in reset_password_token attribute.
-        # Attributes must contain reset_password_token, password and confirmation
-        def reset_password_by_token(attributes={})
-          original_token       = attributes[:reset_password_token]
-          reset_password_token = Devise.token_generator.digest(self, :reset_password_token, original_token)
-    
-        if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
+              get :render_with_object_location
+      assert_equal 'http://www.nextangle.com/customers/1', @response.headers['Location']
     end
   end
     
-      # insert data
-  fields.each do |field, values|
-    updated = '  s.#{field} = ['
-    updated << values.map { |v| '\n    %p' % v }.join(',')
-    updated << '\n  ]'
-    content.sub!(/  s\.#{field} = \[\n(    .*\n)*  \]/, updated)
-  end
+      def test_browser_check_with_any_any
+    @request.accept = 'application/json, application/xml'
+    get :json_xml_or_html
+    assert_equal 'JSON', @response.body
     
-          # Creates a masked version of the authenticity token that varies
-      # on each request. The masking is used to mitigate SSL attacks
-      # like BREACH.
-      def mask_token(token)
-        token = decode_token(token)
-        one_time_pad = SecureRandom.random_bytes(token.length)
-        encrypted_token = xor_byte_strings(one_time_pad, token)
-        masked_token = one_time_pad + encrypted_token
-        encode_token(masked_token)
+    class RenderersTest < ActionController::TestCase
+  class XmlRenderable
+    def to_xml(options)
+      options[:root] ||= 'i-am-xml'
+      '<#{options[:root]}/>'
+    end
+  end
+  class JsonRenderable
+    def as_json(options = {})
+      hash = { a: :b, c: :d, e: :f }
+      hash.except!(*options[:except]) if options[:except]
+      hash
+    end
+    
+          def subscribed(callback, pattern = nil, monotonic: false, &block)
+        subscriber = notifier.subscribe(pattern, callback, monotonic: monotonic)
+        yield
+      ensure
+        unsubscribe(subscriber)
       end
     
-      def update?
-    edit?
+          # @return [String, Nil] the dSYM path, if one exists
+      #
+      attr_reader :dsym_path
+    
+            # @return [Hash<String, TargetInstallationResult>] pod_target_installation_results
+        #         Hash of pod target name to installation results.
+        #
+        attr_reader :pod_target_installation_results
+    
+        # @return [Config] the current config instance creating one if needed.
+    #
+    def self.instance
+      @instance ||= new
+    end
+    
+        it 'sets the Pods group\'s location path to ${PODS_ROOT}' do
+      XCConfigIntegrator.integrate(@pod_bundle, [@target])
+      @project['Pods'].path.should.equal @pod_bundle.relative_pods_root_path.to_s
+    end
+    
+            # Initialize a new instance.
+        #
+        # @param [Sandbox] sandbox @see #sandbox
+        # @param [Hash{String => TargetInstallationResult}] aggregate_target_installation_results @see #aggregate_target_installation_results
+        # @param [Hash{String => TargetInstallationResult}] pod_target_installation_results @see #pod_target_installation_results
+        # @param [ProjectMetadataCache] metadata_cache @see #metadata_cache
+        #
+        def initialize(sandbox, aggregate_target_installation_results, pod_target_installation_results, metadata_cache)
+          @sandbox = sandbox
+          @aggregate_target_installation_results = aggregate_target_installation_results
+          @pod_target_installation_results = pod_target_installation_results
+          @metadata_cache = metadata_cache
+        end
+    
+      module Generator
+    autoload :Acknowledgements,        'cocoapods/generator/acknowledgements'
+    autoload :Markdown,                'cocoapods/generator/acknowledgements/markdown'
+    autoload :Plist,                   'cocoapods/generator/acknowledgements/plist'
+    autoload :BridgeSupport,           'cocoapods/generator/bridge_support'
+    autoload :Constant,                'cocoapods/generator/constant'
+    autoload :CopyResourcesScript,     'cocoapods/generator/copy_resources_script'
+    autoload :DummySource,             'cocoapods/generator/dummy_source'
+    autoload :EmbedFrameworksScript,   'cocoapods/generator/embed_frameworks_script'
+    autoload :FileList,                'cocoapods/generator/file_list'
+    autoload :Header,                  'cocoapods/generator/header'
+    autoload :InfoPlistFile,           'cocoapods/generator/info_plist_file'
+    autoload :ModuleMap,               'cocoapods/generator/module_map'
+    autoload :PrefixHeader,            'cocoapods/generator/prefix_header'
+    autoload :UmbrellaHeader,          'cocoapods/generator/umbrella_header'
+    autoload :AppTargetHelper,         'cocoapods/generator/app_target_helper'
   end
