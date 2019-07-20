3efadfb02f8f73ab02238cb93a09fc069093278d
SUNsung <weatherius@gmail.com>
2019-07-20 20:54:41 +0300

    Rewrite [55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb] File in: 2019-07-20 20:54:40

--- ruby/55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb
+++ ruby/55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb
@@ -1,77 +1,82 @@
 
         
-              command_return = ActionCommandReturn.new(
-        return_value: action_return,
-        return_value_type: action_class_ref.return_type,
-        closure_argument_value: closure_argument_value
-      )
-      return command_return
+            it 'raises an exception when there is no version in podspec' do
+      expect do
+        Fastlane::Helper::PodspecHelper.new.parse('')
+      end.to raise_error('Could not find version in podspec content ''')
     end
     
-          it 'should not be fooled by 10 local code signing identities available' do
-        allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
-        allow(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return('     10 valid identities found\n')
-        expect(FastlaneCore::UI).not_to(receive(:error))
-    
-            FastlaneCore::CommanderGenerator.new.generate(Cert::Options.available_options, command: c)
-    
-      before_action :set_account
-  respond_to :txt
-    
-      before_action :require_user!
-    
-      Devise.omniauth_configs.each_key do |provider|
-    provides_callback_for provider
-  end
-    
-      def create
-    @account = unfollow_attempt.try(:target_account)
-    
-            if echo?
-          $stdin.gets
-        else
-          $stdin.noecho(&:gets).tap { $stdout.print '\n' }
-        end
-      rescue Errno::EIO
-        # when stdio gets closed
+          # Show message when self.modified_values is empty
+      if self.modified_values.empty?
+        UI.important('No values defined in '#{self.configfile_path}'')
         return
       end
     
-            if built_in_scm_name?
-          load_built_in_scm
-        else
-          # Compatibility with existing 3.x third-party SCMs
-          register_legacy_scm_hooks
-          load_legacy_scm_by_name
-        end
+          def self.details
+        [
+          'Returns the current build number of either the live or testflight version - it is useful for getting the build_number of the current or ready-for-sale app version, and it also works on non-live testflight version.',
+          'If you need to handle more build-trains please see `latest_testflight_build_number`.'
+        ].join('\n')
       end
     
-        initialize_with { Tmuxinator::Project.new(file) }
+        def fetch_app_platform(required: true)
+      result = config[:app_platform]
+      result ||= FastlaneCore::IpaFileAnalyser.fetch_app_platform(config[:ipa]) if config[:ipa]
+      if required
+        result ||= UI.input('Please enter the app's platform (appletvos, ios, osx): ')
+        UI.user_error!('App Platform must be ios, appletvos, or osx') unless ['ios', 'appletvos', 'osx'].include?(result)
+        UI.verbose('App Platform (#{result})')
+      end
+      return result
+    end
   end
+end
+
     
-      it 'calls Hooks.commands_from' do
-    expect(Tmuxinator::Hooks).to receive(:commands_from).
-      with(kind_of(Tmuxinator::Project), hook_name).once
-    project.send('hook_#{hook_name}')
+          def self.all(filter: {}, includes: nil, limit: nil, sort: nil)
+        resps = Spaceship::ConnectAPI.get_bundle_ids(filter: filter, includes: includes).all_pages
+        return resps.map(&:to_models).flatten
+      end
+    
+          def self.find(email: nil, includes: nil)
+        return all(filter: { email: email }, includes: includes)
+      end
+    end
   end
+end
+
     
-        context 'installed' do
-      let(:version) { '2.4' }
-      it { is_expected.to eq version.to_f }
+    # Load in helpers
+require 'unit/support/dummy_communicator'
+require 'unit/support/dummy_provider'
+require 'unit/support/shared/base_context'
+require 'unit/support/shared/action_synced_folders_context'
+require 'unit/support/shared/capability_helpers_context'
+require 'unit/support/shared/plugin_command_context'
+require 'unit/support/shared/virtualbox_context'
+    
+          expect(result[0]).to be_kind_of(Vagrant::BoxMetadata)
+      expect(result[1]).to be_kind_of(Vagrant::BoxMetadata::Version)
+      expect(result[2]).to be_kind_of(Vagrant::BoxMetadata::Provider)
+    
+        context 'when provider validation is ignored' do
+      before do
+        configure do |config|
+          config.vm.box = 'base'
+          config.vm.box_version = '1.0'
+          config.vm.define :guest1
+          config.vm.define :guest2
+    
+          expect(subject).to receive(:fire)
+      subject.fire_triggers(:up, :before, 'guest', :action)
     end
   end
     
-      describe '.shell?' do
-    context '$SHELL is set' do
-      before do
-        allow(ENV).to receive(:[]).with('SHELL') { 'vim' }
-      end
-    
-        desc 'copy [EXISTING] [NEW]', COMMANDS[:copy]
-    map 'c' => :copy
-    map 'cp' => :copy
-    
-        context 'new project already exists' do
-      before do
-        allow(Thor::LineEditor).to receive_messages(readline: 'y')
-      end
+      # This holds a global lock for the duration of the block. This should
+  # be invoked around anything that is modifying process state (such as
+  # environmental variables).
+  def self.global_lock
+    @@global_lock.synchronize do
+      return yield
+    end
+  end
