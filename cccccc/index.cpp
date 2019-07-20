803126bb3c843578ce44c958ec6afb2e6353d584
SUNsung <weatherius@gmail.com>
2019-07-20 03:09:40 +0300

    Rewrite [index.cpp] File in: 2019-07-20 03:09:40

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,352 +1,116 @@
 
         
-        namespace testing {
-    }
+        
+    {  return ThrowTypeError(base::StringPrintf(
+      'Error processing argument at index %d, conversion failure from %s',
+      next_, V8TypeAsString(isolate_, (*info_)[next_]).c_str()));
+}
     
-    // Helper function for implementing {EXPECT|ASSERT}_PRED5.  Don't use
-// this in your code.
-template <typename Pred,
-          typename T1,
-          typename T2,
-          typename T3,
-          typename T4,
-          typename T5>
-AssertionResult AssertPred5Helper(const char* pred_text,
-                                  const char* e1,
-                                  const char* e2,
-                                  const char* e3,
-                                  const char* e4,
-                                  const char* e5,
-                                  Pred pred,
-                                  const T1& v1,
-                                  const T2& v2,
-                                  const T3& v3,
-                                  const T4& v4,
-                                  const T5& v5) {
-  if (pred(v1, v2, v3, v4, v5)) return AssertionSuccess();
-    }
+    // CallbackHolder and CallbackHolderBase are used to pass a base::Callback from
+// CreateFunctionTemplate through v8 (via v8::FunctionTemplate) to
+// DispatchToCallback, where it is invoked.
     
-    #ifndef GTEST_INCLUDE_GTEST_INTERNAL_GTEST_LINKED_PTR_H_
-#define GTEST_INCLUDE_GTEST_INTERNAL_GTEST_LINKED_PTR_H_
-    
-    // Returns true iff n is a prime number.
-bool IsPrime(int n) {
-  // Trivial case 1: small numbers
-  if (n <= 1) return false;
-    }
-    
-    
-    {  // Sets the 0-terminated C string this MyString object represents.
-  void Set(const char* c_string);
+    // Specialization for base::Callback.
+template <typename T>
+struct CallbackTraits<base::Callback<T>> {
+  static v8::Local<v8::FunctionTemplate> CreateTemplate(
+      v8::Isolate* isolate,
+      const base::Callback<T>& callback) {
+    return CreateFunctionTemplate(isolate, callback);
+  }
 };
     
+      template <typename P, typename C>
+  void SetWeak(P* parameter, C callback) {
+    handle_.SetWeak(parameter, callback);
+  }
     
-    {    Rational Sinh(Rational const& rat);
-    Rational Cosh(Rational const& rat);
-    Rational Tanh(Rational const& rat);
-    Rational ASinh(Rational const& rat);
-    Rational ACosh(Rational const& rat);
-    Rational ATanh(Rational const& rat);
-}
-
+      switch (chrome::AttemptToNotifyRunningChrome(remote_window_, false)) {
+    case chrome::NOTIFY_SUCCESS:
+      return PROCESS_NOTIFIED;
+    case chrome::NOTIFY_FAILED:
+      remote_window_ = NULL;
+      return PROCESS_NONE;
+    case chrome::NOTIFY_WINDOW_HUNG:
+      // Fall through and potentially terminate the hung browser.
+      break;
+  }
     
-    #include <vector>
-#include 'Ratpack/ratpak.h'
+      fetcher_ = net::URLFetcher::Create(formated_url, request_type, this,
+                                     MISSING_TRAFFIC_ANNOTATION);
+  fetcher_->SaveResponseWithWriter(base::WrapUnique(new ResponsePiper(this)));
     
-    // Some commands are not affecting the state machine state of the calc flow. But these are more of
-// some gui mode kind of settings (eg Inv button, or Deg,Rad , Back etc.). This list is getting bigger & bigger
-// so we abstract this as a separate routine. Note: There is another side to this. Some commands are not
-// gui mode setting to begin with, but once it is discovered it is invalid and we want to behave as though it
-// was never inout, we need to revert the state changes made as a result of this test
-bool IsGuiSettingOpCode(OpCode opCode)
-{
-    if (IsOpInRange(opCode, IDM_HEX, IDM_BIN) || IsOpInRange(opCode, IDM_QWORD, IDM_BYTE) || IsOpInRange(opCode, IDM_DEG, IDM_GRAD))
-    {
-        return true;
-    }
-    }
-    
-            // No new command should not be added before CommandSign, 80
-        // If it is needed, the following two functions need to be revised too.
-        // CalculatorManager::MapCommandForSerialize(Command command);
-        // CalculatorManager::MapCommandForDeSerialize(unsigned char command);
-        CommandSIGN = 80,
-        CommandCLEAR = 81,
-        CommandCENTR = 82,
-        CommandBACK = 83,
-    
-    
-    {private:
-    void ReinitHistory();
-    int IchAddSzToEquationSz(std::wstring_view str, int icommandIndex);
-    void TruncateEquationSzFromIch(int ich);
-    void SetExpressionDisplay();
-    void InsertSzInEquationSz(std::wstring_view str, int icommandIndex, int ich);
-    std::shared_ptr<CalculatorVector<int>> GetOperandCommandsFromString(std::wstring_view numStr);
-};
-
-    
-    //-----------------------------------------------------------------------------
-//
-//  FUNCTION: atanhrat
-//
-//  ARGUMENTS:  x PRAT representation of number to take the inverse
-//              hyperbolic tangent of
-//
-//  RETURN: atanh of x in PRAT form.
-//
-//  EXPLANATION: This uses
-//
-//             1     x+1
-//  atanh(x) = -*ln(----)
-//             2     x-1
-//
-//-----------------------------------------------------------------------------
-    
-        template <typename ElementType>
-    std::tuple<size_t, size_t, size_t> Value::ValidateSparseCSCAndGetIndexBufferSizes(const Variable& outputVariable)
-    {
-        auto varShape = outputVariable.Shape();
-        if (varShape.IsUnknown() || varShape.HasInferredDimension())
-            InvalidArgument('The outputVariable '%S' shape '%S' is of unknown shape or has inferred dimension for at least one axis.',
-                outputVariable.AsString().c_str(), varShape.AsString().c_str());
-    }
-    
-        ParameterInitializer TruncatedNormalInitializer(double scale, unsigned long seed)
-    {
-        return CreateInitializer(Microsoft::MSR::CNTK::TruncNormalInitializerTypeName, scale, seed);
-    }
-    
-    #pragma once
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS // 'secure' CRT not available on all platforms  --add this at the top of all CPP files that give 'function or variable may be unsafe' warnings
-#endif
-#ifdef _WIN32
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif // NOMINMAX
-#pragma comment(lib, 'Dbghelp.lib')
-#else
-#include <execinfo.h>
-#include <cxxabi.h>
-#endif
-    
-        virtual void CopyTo(ComputationNodeBasePtr nodeP, const std::wstring& newName, const CopyNodeFlags flags) const override
-    {
-        Base::CopyTo(nodeP, newName, flags);
-        if (flags & CopyNodeFlags::copyNodeValue)
-        {
-            auto node = dynamic_pointer_cast<DiagTimesNode<ElemType>>(nodeP);
-            node->m_innerproduct->SetValue(*m_innerproduct);
-            node->m_rightGradient->SetValue(*m_rightGradient);
-        }
-    }
-    // request matrices that are needed for gradient computation
-    virtual void RequestMatricesBeforeBackprop(MatrixPool& matrixPool)
-    {
-        Base::RequestMatricesBeforeBackprop(matrixPool);
-        RequestMatrixFromPool(m_innerproduct, matrixPool);
-        RequestMatrixFromPool(m_rightGradient, matrixPool);
-    }
-    
-        /*TODO: merge with call site*/ void BackpropToLeft(Matrix<ElemType>& inputFunctionValues, Matrix<ElemType>& inputGradientValues, Matrix<ElemType>& gradientValues)
-    {
-        size_t rows1 = inputFunctionValues.GetNumRows(), cols1 = inputFunctionValues.GetNumCols();
-        size_t rowsp = gradientValues.GetNumRows(), colsp = gradientValues.GetNumCols();
-        int wordsInEachSample = rows1 / inputGradientValues.GetNumCols();
-    }
-    
-    
-    {            for (const auto& child : pair.second)
-            {
-                if (rootLookupMap.find(child) == rootLookupMap.end())
-                    InvalidArgument('group nodes should be in lookupMap');
-            }
-        }
-    
-    TEST(ProtocolDataTest, CheckSum) {
-  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
-  const uint8_t result =
-      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
-  EXPECT_EQ(0xE7, result);
+    void PdfViewerUI::RenderFrameCreated(content::RenderFrameHost* rfh) {
+  int render_process_id = rfh->GetProcess()->GetID();
+  int render_frame_id = rfh->GetRoutingID();
+  int render_view_id = rfh->GetRenderViewHost()->GetRoutingID();
+  auto resource_context =
+      web_contents()->GetBrowserContext()->GetResourceContext();
+  auto callback =
+      base::BindOnce(&PdfViewerUI::OnPdfStreamCreated, base::Unretained(this));
+  resource_requester_ = new ResourceRequester(std::move(callback));
+  base::PostTaskWithTraits(
+      FROM_HERE, {BrowserThread::IO},
+      base::BindOnce(&ResourceRequester::StartRequest, resource_requester_,
+                     GURL(src_), GURL(kPdfViewerUIOrigin), render_process_id,
+                     render_view_id, render_frame_id, resource_context));
 }
     
-    int ObjectExtendedInfo60D::obstacle_class(const std::uint8_t* bytes,
-                                          int32_t length) const {
-  Byte t0(bytes + 3);
-  int32_t x = t0.get_byte(0, 3);
+    napi_value Print(napi_env env, napi_callback_info info) {
+  size_t argc = 1;
+  napi_value args[1];
+  napi_status status;
     }
     
-      Byte t1(bytes + 3);
-  int32_t t = t1.get_byte(0, 8);
+      if (need_two_pt_correction_ && raw_distance <= 2500) {
+    distance_corr_x =
+        (corrections.dist_correction - corrections.dist_correction_x) *
+            (xx - 2.4) / 22.64 +
+        corrections.dist_correction_x;  // 22.64 = 25.04 - 2.4
+    distance_corr_y =
+        (corrections.dist_correction - corrections.dist_correction_y) *
+            (yy - 1.93) / 23.11 +
+        corrections.dist_correction_y;  // 23.11 = 25.04 - 1.93
+  } else {
+    distance_corr_x = distance_corr_y = corrections.dist_correction;
+  }
     
-    TEST(TestPiecewiseLinearConstraint, add_monotone_inequality_constraint) {
-  PiecewiseLinearConstraint constraint(10, 0.1);
-  constraint.AddMonotoneInequalityConstraint();
-  const auto mat = constraint.inequality_constraint_matrix();
-  const auto bd = constraint.inequality_constraint_boundary();
+    TEST(RecordTest, iterator_test) {
+  uint64_t msg_num = 200;
+  uint64_t begin_time = 100000000;
+  uint64_t step_time = 100000000;  // 100ms
+  uint64_t end_time = begin_time + step_time * (msg_num - 1);
+  ConstructRecord(msg_num, begin_time, step_time);
     }
     
-      kernel.AddThirdOrderDerivativeMatrix(init_derivative, init_second_derivative,
-                                       1.0);
-    
-    void Spline1dSeg::SetParams(const std::vector<double>& params) {
-  SetSplineFunc(PolynomialXd(params));
-}
-    
-    
-    {
-    {
-    {
-    {  double ret = x * 0.001000;
-  return ret;
-}
-}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
-
-    
-    // config detail: {'name': 'manual_input', 'enum': {0:
-// 'MANUAL_INPUT_HEADLIGHTS_OFF', 1: 'MANUAL_INPUT_LOW_BEAMS', 2:
-// 'MANUAL_INPUT_HIGH_BEAMS'}, 'precision': 1.0, 'len': 8, 'is_signed_var':
-// False, 'offset': 0.0, 'physical_range': '[0|2]', 'bit': 7, 'type': 'enum',
-// 'order': 'motorola', 'physical_unit': ''}
-Headlight_rpt_77::Manual_inputType Headlightrpt77::manual_input(
-    const std::uint8_t* bytes, int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-    #include <openpose/pose/poseParameters.hpp>
-#include <openpose/pose/poseParametersRender.hpp>
-    
-    
-    {
-    {        DELETE_COPY(PersonIdExtractor);
-    };
-}
-    
-        template<typename T>
-    Rectangle<T>& Rectangle<T>::operator=(const Rectangle<T>& rectangle)
-    {
-        try
-        {
-            x = rectangle.x;
-            y = rectangle.y;
-            width = rectangle.width;
-            height = rectangle.height;
-            // Return
-            return *this;
-        }
-        catch (const std::exception& e)
-        {
-            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
-            return *this;
-        }
-    }
-    
-        float i = sinf(za) * cosf(xa) * r + _center.x;
-    float j = sinf(za) * sinf(xa) * r + _center.y;
-    float k = cosf(za) * r + _center.z;
-    
-    
-    /** Pauses the target: all running actions and newly added actions will be paused.
-     *
-     * @param target    A certain target.
-     */
-    virtual void pauseTarget(Node *target);
-    
-        /**
-     * set the data to be a pointer to a number of Quads
-     * the member verts will not be released when this PolygonInfo destructs
-     * as the verts memory are managed by other objects
-     * @param quad  a pointer to the V3F_C4B_T2F_Quad quads
-     */
-    void setQuads(V3F_C4B_T2F_Quad *quads, int numberOfQuads);
-    
-    #include '2d/CCComponent.h'
-    
-    
-    {    virtual int* getHorizontalKerningForTextUTF32(const std::u32string& text, int &outNumLetters) const = 0;
-    
-    CC_DEPRECATED_ATTRIBUTE inline int* getHorizontalKerningForTextUTF16(const std::u16string& text, int &outNumLetters) const
-    {
-        std::u32string utf32;
-        StringUtils::UTF16ToUTF32(text, utf32);
-        return getHorizontalKerningForTextUTF32(utf32, outNumLetters);
-    }
-    
-    virtual int getFontMaxHeight() const { return 0; }
-};
-    
-    
-    {  FLAGS_minloglevel = prev;
-}
-    
-    
-    {  std::string arguments;
-  ASSERT_TRUE(getTracepointArguments(
-      'folly', 'test_static_tracepoint_array', 0, arguments));
-  std::array<int, 3> expected{{sizeof(void*), sizeof(int), sizeof(void*)}};
-  checkTracepointArguments(arguments, expected);
-}
-    
-    exception_wrapper::exception_wrapper(std::exception_ptr ptr) noexcept
-    : exception_wrapper{} {
-  if (ptr) {
-    if (auto e = get_std_exception_(ptr)) {
-      LOG(DFATAL)
-          << 'Performance error: Please construct exception_wrapper with a '
-             'reference to the std::exception along with the '
-             'std::exception_ptr.';
-      *this = exception_wrapper{std::move(ptr), *e};
-    } else {
-      Unknown uk;
-      *this = exception_wrapper{ptr, uk};
-    }
+    Poller::Poller() {
+  if (!Init()) {
+    AERROR << 'Poller init failed!';
+    Clear();
   }
 }
     
-    #include <stdexcept>
-    
-    
-    {} // namespace detail
-    
-    
-    {} // namespace folly
-
-    
-    #include <zlib.h>
-    
-      auto func = Getcpu::Func(dlsym(h, '__vdso_getcpu'));
-  if (func == nullptr) {
-    // technically a null result could either be a failure or a successful
-    // lookup of a symbol with the null value, but the second can't actually
-    // happen for this symbol.  No point holding the handle forever if
-    // we don't need the code
-    dlclose(h);
-  }
-    
-      void reset(const std::shared_ptr<T>& p = nullptr) {
-    auto newslots = folly::make_unique<Slots>();
-    // Allocate each Holder in a different CoreRawAllocator stripe to
-    // prevent false sharing. Their control blocks will be adjacent
-    // thanks to allocate_shared().
-    for (auto slot : folly::enumerate(newslots->slots_)) {
-      auto alloc = getCoreAllocator<Holder, kNumSlots>(slot.index);
-      auto holder = std::allocate_shared<Holder>(alloc, p);
-      *slot = std::shared_ptr<T>(holder, p.get());
-    }
+    int main(int argc, char *argv[]) {
+  apollo::cyber::Init('cyber_python');
+  apollo::cyber::proto::Chatter chat;
+  apollo::cyber::PyNode node('listener');
+  pr = node.create_reader('channel/chatter', chat.GetTypeName());
+  pr->register_func(cbfun);
     }
     
-    template <template <typename> class Atom = std::atomic>
-class counted_ptr_internals : public counted_ptr_base<Atom> {
+    static const cv::Scalar kBoxColorMap[] = {
+    cv::Scalar(0, 0, 0),        // 0
+    cv::Scalar(128, 128, 128),  // 1
+    cv::Scalar(255, 0, 0),      // 2
+    cv::Scalar(0, 255, 0),      // 3
+    cv::Scalar(0, 0, 255),      // 4
+    cv::Scalar(255, 255, 0),    // 5
+    cv::Scalar(0, 255, 255),    // 6
+    cv::Scalar(255, 0, 255),    // 7
+    cv::Scalar(255, 255, 255),  // 8
+};
+    
+    class PathLaneBorrowDecider : public Decider {
  public:
-  template <typename T, typename... Args>
-  static counted_ptr<T, Atom> make_ptr(Args&&... args) {
-    return make_counted<Atom, T>(std::forward<Args...>(args...));
-  }
-  template <typename T>
-  using CountedPtr = counted_ptr<T, Atom>;
-  typedef void counted_base;
+  explicit PathLaneBorrowDecider(const TaskConfig& config);
     }
