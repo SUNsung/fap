bdf2a421a51fe4af7eb2e1351028045350eeb835
SUNsung <weatherius@gmail.com>
2019-07-20 09:18:49 +0300

    Rewrite [26sUWxogB.Vc626S62PPP1Lpjc26VhlMtZMTxOg26rFi.py] File in: 2019-07-20 09:18:49

--- pyton/26sUWxogB.Vc626S62PPP1Lpjc26VhlMtZMTxOg26rFi.py
+++ pyton/26sUWxogB.Vc626S62PPP1Lpjc26VhlMtZMTxOg26rFi.py
@@ -1,91 +1,67 @@
 
         
-                Emit key value pairs of the form:
-    
-        def __init__(self, url, contents, child_urls):
-        self.url = url
-        self.contents = contents
-        self.child_urls = child_urls
-        self.signature = self.create_signature()
-    
-            $ curl -XDELETE 'localhost:9200/example_index'
-        $ curl -XDELETE 'localhost:9200/update_log/_query?q=target_index:example_index'
-    
-        .. code:: console
-    
-        Example luigi configuration::
-    
-        def requires(self):
-        '''
-        This task's dependencies:
+        from .__version__ import __title__, __description__, __url__, __version__
+from .__version__ import __build__, __author__, __author_email__, __license__
+from .__version__ import __copyright__, __cake__
     
     
-def get_dataproc_client():
-    return _dataproc_client
+class Timeout(RequestException):
+    '''The request timed out.
     
-        def create_index(self):
-        '''
-        Override to provide code for creating the target index.
+        def test_lower_items(self):
+        assert list(self.case_insensitive_dict.lower_items()) == [('accept', 'application/json')]
     
+            # because special names such as Name.Class, Name.Function, etc.
+        # are not recognized as such later in the parsing, we choose them
+        # to look the same as ordinary variables.
+        Name:                      '#000000',        # class: 'n'
+        Name.Attribute:            '#c4a000',        # class: 'na' - to be revised
+        Name.Builtin:              '#004461',        # class: 'nb'
+        Name.Builtin.Pseudo:       '#3465a4',        # class: 'bp'
+        Name.Class:                '#000000',        # class: 'nc' - to be revised
+        Name.Constant:             '#000000',        # class: 'no' - to be revised
+        Name.Decorator:            '#888',           # class: 'nd' - to be revised
+        Name.Entity:               '#ce5c00',        # class: 'ni'
+        Name.Exception:            'bold #cc0000',   # class: 'ne'
+        Name.Function:             '#000000',        # class: 'nf'
+        Name.Property:             '#000000',        # class: 'py'
+        Name.Label:                '#f57900',        # class: 'nl'
+        Name.Namespace:            '#000000',        # class: 'nn' - to be revised
+        Name.Other:                '#000000',        # class: 'nx'
+        Name.Tag:                  'bold #004461',   # class: 'nt' - like a keyword
+        Name.Variable:             '#000000',        # class: 'nv' - to be revised
+        Name.Variable.Class:       '#000000',        # class: 'vc' - to be revised
+        Name.Variable.Global:      '#000000',        # class: 'vg' - to be revised
+        Name.Variable.Instance:    '#000000',        # class: 'vi' - to be revised
     
-class PublisherACL(object):
+    Available hooks:
+    
+    def test_fragment_update_on_redirect():
+    '''Verify we only append previous fragment if one doesn't exist on new
+    location. If a new fragment is encountered in a Location header, it should
+    be added to all subsequent requests.
     '''
-    Represents the publisher ACL and provides methods
-    to query the ACL for given operations
-    '''
-    def __init__(self, blacklist):
-        self.blacklist = blacklist
     
-        ^realm
-        The realm required by htdigest authentication.
+        @pytest.mark.parametrize(
+        'mode, warnings_num', (
+            ('r', 1),
+            ('rb', 0),
+        ))
+    def test_file(self, tmpdir, mode, warnings_num, recwarn):
+        file_obj = tmpdir.join('test.txt')
+        file_obj.write('Test')
+        with file_obj.open(mode) as fd:
+            assert super_len(fd) == 4
+        assert len(recwarn) == warnings_num
     
-        try:
-        conn_info['hostname'] = __opts__['mysql_auth']['hostname']
-        conn_info['username'] = __opts__['mysql_auth']['username']
-        conn_info['password'] = __opts__['mysql_auth']['password']
-        conn_info['database'] = __opts__['mysql_auth']['database']
+            diff_against=dict(choices=['startup', 'intended', 'running']),
+        diff_ignore_lines=dict(type='list'),
+    )
     
-        for item in _config['txt']:
-        changes_key = 'txt.' + salt.utils.stringutils.to_unicode(item)
-        if _config['txt'][item].startswith('grains.'):
-            grain = _config['txt'][item][7:]
-            grain_index = None
-            square_bracket = grain.find('[')
-            if square_bracket != -1 and grain[-1] == ']':
-                grain_index = int(grain[square_bracket+1:-1])
-                grain = grain[:square_bracket]
-    
-        Specify thresholds for percent used and only emit a beacon
-    if it is exceeded.
-    
-    log = logging.getLogger(__name__)
-    
-    log = logging.getLogger(__name__)
-    
-        .. code-block:: yaml
-    
-    
-def list_(bank):
-    '''
-    Return an iterable object containing all entries stored in the specified bank.
-    '''
-    try:
-        _, keys = api.kv.get(bank + '/', keys=True, separator='/')
-    except Exception as exc:
-        raise SaltCacheError(
-            'There was an error getting the key '{0}': {1}'.format(
-                bank, exc
-            )
-        )
-    if keys is None:
-        keys = []
-    else:
-        # Any key could be a branch and a leaf at the same time in Consul
-        # so we have to return a list of unique names only.
-        out = set()
-        for key in keys:
-            out.add(key[len(bank) + 1:].rstrip('/'))
-        keys = list(out)
-    return keys
-    
-                super(YourSubClass, self).start()
+        def test_enable_asup(self):
+        ''' a more interesting test '''
+        data = self.mock_args()
+        set_module_args(data)
+        with pytest.raises(AnsibleExitJson) as exc:
+            self.get_asup_mock_object('asup').apply()
+        assert exc.value.args[0]['changed']
