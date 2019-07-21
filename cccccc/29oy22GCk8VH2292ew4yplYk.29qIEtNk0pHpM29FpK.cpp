83d8cf710fff52003c534b844bb6ab3db1a0f015
SUNsung <weatherius@gmail.com>
2019-07-22 01:54:58 +0300

    Rewrite [29oy22GCk8VH2292ew4yplYk.29qIEtNk0pHpM29FpK.cpp] File in: 2019-07-22 01:54:58

--- cccccc/29oy22GCk8VH2292ew4yplYk.29qIEtNk0pHpM29FpK.cpp
+++ cccccc/29oy22GCk8VH2292ew4yplYk.29qIEtNk0pHpM29FpK.cpp
@@ -1,242 +1,100 @@
 
         
-        // Generate param traits log methods.
-#include 'ipc/param_traits_log_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
-
+          // Absolute value of x
+  // Works correctly for unsigned types and
+  // for special floating point values.
+  // Note: 0.0 and -0.0 are not differentiated by Abs (Abs(0.0) is -0.0),
+  // which should be OK: see the comment for Max above.
+  template<typename T>
+  static T Abs(const T x) {
+    return x > T(0) ? x : -x;
+  }
     
-      base::WaitableEvent done(false, false);
-  BrowserThread::PostTask(
-      BrowserThread::IO, FROM_HERE,
-      base::Bind(&SetProxyConfigCallback, &done,
-                 make_scoped_refptr(context_getter), config));
-  done.Wait();
+      EXPECT_EQ(4, decode_data.num_entries());
     
-      // Try to close all windows (then will cause whole app to quit).
-  static void CloseAllWindows(bool force = false, bool quit = false);
-    
-       void Call(const std::string& method,
-                    const base::ListValue& arguments) override;
-   void CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result) override;
-    
-    
-namespace nwapi {
-    }
-    
-    #include 'content/nw/src/api/menu/menu.h'
-    
-    #include 'content/nw/src/api/menuitem/menuitem.h'
-    
-    void MenuItem::SetLabel(const std::string& label) {
-  label_ = label;
-  gtk_menu_item_set_label(GTK_MENU_ITEM(menu_item_), label.c_str());
-}
-    
-        if (screens) {
-      std::unique_ptr<DesktopMediaList> screen_media_list =
-        std::make_unique<NativeDesktopMediaList>(
-          content::DesktopMediaID::TYPE_SCREEN,
-          webrtc::DesktopCapturer::CreateScreenCapturer(options));
-      media_list_.push_back(std::move(screen_media_list));
-    }
-    
-      /**
-   * /brief Customize set method for LayerParameter
-   * /tparam value string of caffe's layer configuration
-   * */
-  virtual void Set(void *head, const std::string &value) const {
-    caffe::NetParameter net_param;
-    if (!ReadProtoFromTextContent(value, &net_param))
-      CHECK(false)<< 'Caffe Net Prototxt: ' << value << 'Initialized Failed';
-    }
+    #include <google/protobuf/io/zero_copy_stream.h>
     
     
     {
-    {}  // namespace exec
-}  // namespace mxnet
-
+    {  // No default clause, clang will abort if a code is missing from
+  // above switch.
+  return 'UNKNOWN';
+}
+}  // namespace error.
     
-    #include <algorithm>
-#include <vector>
-#include './bilinear_sampler-inl.h'
-namespace mxnet {
-namespace op {
-#if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 5
-template<typename DType>
-class CuDNNBilinearSamplerOp : public Operator {
+        for (int i = 0; i < dataset.payload_size(); i++) {
+      message->ParseFromString(dataset.payload(i));
+      Proto3DataStripper stripper;
+      stripper.StripMessage(message);
+      dataset.set_payload(i, message->SerializeAsString());
+    }
+    
+      void pollAndSchedule(int task_id);
+  void schedule(int task_id, bool run_inline = false) noexcept;
+  void reset() override;
+  virtual void finishRun();
+  void parentCallback(int parent_id);
+  bool isInlineTask(int parent_id, int child_id) const;
+    
+      /// The learned bias.
+  /// Only defined if the `affine` option was `true` upon construction.
+  Tensor bias;
+    
+    ```
+    
+    template <typename T, class Context>
+class BernoulliJSDOp final : public Operator<Context> {
  public:
-  explicit CuDNNBilinearSamplerOp(BilinearSamplerParam param) {
-    this->param_ = param;
-    init_cudnn_ = false;
-    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
-    sampler_ = CUDNN_SAMPLER_BILINEAR;
-  }
-    }
-    }
-    }
-    
-    DMLC_REGISTER_PARAMETER(NativeOpParam);
-    
-    // only include thrust-related files if host_device_vector.h
-// is included from a .cu file
-#ifdef __CUDACC__
-#include <thrust/device_ptr.h>
-#endif  // __CUDACC__
-    
-      // init thread buffers
-  inline void InitThreadTemp(int nthread, int num_feature) {
-    int prev_thread_temp_size = thread_temp.size();
-    if (prev_thread_temp_size < nthread) {
-      thread_temp.resize(nthread, RegTree::FVec());
-      for (int i = prev_thread_temp_size; i < nthread; ++i) {
-        thread_temp[i].Init(num_feature);
-      }
-    }
-  }
-  inline void PredLoopSpecalize(DMatrix* p_fmat,
-                                std::vector<bst_float>* out_preds,
-                                const gbm::GBTreeModel& model, int num_group,
-                                unsigned tree_begin, unsigned tree_end) {
-    const MetaInfo& info = p_fmat->Info();
-    const int nthread = omp_get_max_threads();
-    InitThreadTemp(nthread, model.param.num_feature);
-    std::vector<bst_float>& preds = *out_preds;
-    CHECK_EQ(model.param.size_leaf_vector, 0)
-        << 'size_leaf_vector is enforced to 0 so far';
-    CHECK_EQ(preds.size(), p_fmat->Info().num_row_ * num_group);
-    // start collecting the prediction
-    for (const auto &batch : p_fmat->GetRowBatches()) {
-      // parallel over local batch
-      constexpr int kUnroll = 8;
-      const auto nsize = static_cast<bst_omp_uint>(batch.Size());
-      const bst_omp_uint rest = nsize % kUnroll;
-#pragma omp parallel for schedule(static)
-      for (bst_omp_uint i = 0; i < nsize - rest; i += kUnroll) {
-        const int tid = omp_get_thread_num();
-        RegTree::FVec& feats = thread_temp[tid];
-        int64_t ridx[kUnroll];
-        SparsePage::Inst inst[kUnroll];
-        for (int k = 0; k < kUnroll; ++k) {
-          ridx[k] = static_cast<int64_t>(batch.base_rowid + i + k);
-        }
-        for (int k = 0; k < kUnroll; ++k) {
-          inst[k] = batch[i + k];
-        }
-        for (int k = 0; k < kUnroll; ++k) {
-          for (int gid = 0; gid < num_group; ++gid) {
-            const size_t offset = ridx[k] * num_group + gid;
-            preds[offset] += this->PredValue(
-                inst[k], model.trees, model.tree_info, gid,
-                info.GetRoot(ridx[k]), &feats, tree_begin, tree_end);
-          }
-        }
-      }
-      for (bst_omp_uint i = nsize - rest; i < nsize; ++i) {
-        RegTree::FVec& feats = thread_temp[0];
-        const auto ridx = static_cast<int64_t>(batch.base_rowid + i);
-        auto inst = batch[i];
-        for (int gid = 0; gid < num_group; ++gid) {
-          const size_t offset = ridx * num_group + gid;
-          preds[offset] +=
-              this->PredValue(inst, model.trees, model.tree_info, gid,
-                              info.GetRoot(ridx), &feats, tree_begin, tree_end);
-        }
-      }
-    }
-  }
-    
-    class DHTRoutingTableDeserializer {
-private:
-  int family_;
-    }
-    
-    std::pair<std::vector<std::unique_ptr<Command>>,
-          std::vector<std::unique_ptr<Command>>>
-DHTSetup::setup(DownloadEngine* e, int family)
-{
-  std::vector<std::unique_ptr<Command>> tempCommands;
-  std::vector<std::unique_ptr<Command>> tempRoutineCommands;
-  if ((family != AF_INET && family != AF_INET6) ||
-      (family == AF_INET && DHTRegistry::isInitialized()) ||
-      (family == AF_INET6 && DHTRegistry::isInitialized6())) {
-    return {};
-  }
-  try {
-    // load routing table and localnode id here
-    std::shared_ptr<DHTNode> localNode;
-    }
-    }
-    
-    namespace aria2 {
-    }
-    
-    void DHTTaskExecutor::update()
-{
-  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
-                                  std::mem_fn(&DHTTask::finished)),
-                   execTasks_.end());
-  int r;
-  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
-    r = numConcurrent_ - execTasks_.size();
-  }
-  else {
-    r = 0;
-  }
-  while (r && !queue_.empty()) {
-    std::shared_ptr<DHTTask> task = queue_.front();
-    queue_.pop_front();
-    task->startup();
-    if (!task->finished()) {
-      execTasks_.push_back(task);
-      --r;
-    }
-  }
-  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
-                   static_cast<unsigned int>(getExecutingTaskSize()),
-                   static_cast<unsigned int>(getQueueSize())));
-}
-    
-    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
-                                           const std::string& ipaddr,
-                                           uint16_t port) const
-{
-  return generateToken(infoHash, ipaddr, port, secret_[0]);
-}
-    
-    
-    {    private:
-        redisAsyncContext * m_ctx;
-        QSocketNotifier * m_read;
-        QSocketNotifier * m_write;
+  USE_SIMPLE_CTOR_DTOR(BernoulliJSDOp);
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  bool RunOnDevice() override;
 };
     
-        private:
-        void finish() { emit finished(); }
-    
-    
-    {    for (i = 0; i < READ_THREAD_N; i++)
-    {
-        threads[i].thread->join();
-        threads[i].pipe.close(&threads[i].pipe);
-        delete threads[i].thread;
-    }
+    namespace tesseract {
+class Tesseract;
 }
     
-        SwooleG.use_signalfd = 1;
     
-        /**
-     * 协程2
-     */
-    Coroutine::create([](void *arg)
-    {
-        G_a.x = 100;
-        G_a.y = nullptr;
-    });
+    {}  // namespace tesseract.
+
     
-    
-    {    return SW_ERR;
+    // Update the other members if the cost is lower.
+void DPPoint::UpdateIfBetter(int64_t cost, int32_t steps, const DPPoint* prev,
+                             int32_t n, int32_t sig_x, int64_t sig_xsq) {
+  if (cost < total_cost_) {
+    total_cost_ = cost;
+    total_steps_ = steps;
+    best_prev_ = prev;
+    n_ = n;
+    sig_x_ = sig_x;
+    sig_xsq_ = sig_xsq;
+  }
 }
+    
+      // Returns the covariance.
+  double covariance() const {
+    if (total_weight > 0.0)
+      return (sigxy - sigx * sigy / total_weight) / total_weight;
+    else
+      return 0.0;
+  }
+  double x_variance() const {
+    if (total_weight > 0.0)
+      return (sigxx - sigx * sigx / total_weight) / total_weight;
+    else
+      return 0.0;
+  }
+  double y_variance() const {
+    if (total_weight > 0.0)
+      return (sigyy - sigy * sigy / total_weight) / total_weight;
+    else
+      return 0.0;
+  }
+    
+    
+    {  UnicharAmbigsVector dang_ambigs_;
+  UnicharAmbigsVector replace_ambigs_;
+  GenericVector<UnicharIdVector *> one_to_one_definite_ambigs_;
+  GenericVector<UnicharIdVector *> ambigs_for_adaption_;
+  GenericVector<UnicharIdVector *> reverse_ambigs_for_adaption_;
+};
