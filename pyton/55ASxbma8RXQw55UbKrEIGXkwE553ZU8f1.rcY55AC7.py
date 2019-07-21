0de03cd0ac37f4a48e490a2e8a5227b005e65956
SUNsung <weatherius@gmail.com>
2019-07-21 07:04:56 +0300

    Rewrite [55ASxbma8RXQw55UbKrEIGXkwE553ZU8f1.rcY55AC7.py] File in: 2019-07-21 07:04:56

--- pyton/55ASxbma8RXQw55UbKrEIGXkwE553ZU8f1.rcY55AC7.py
+++ pyton/55ASxbma8RXQw55UbKrEIGXkwE553ZU8f1.rcY55AC7.py
@@ -1,101 +1,197 @@
 
         
-            messages = {
-        'missing_keys': _('Some keys were missing: %(keys)s'),
-        'extra_keys': _('Some unknown keys were provided: %(keys)s'),
-    }
-    strict = False
+        import os
+import pytest
+from thefuck.shells import Bash
     
-        @classmethod
-    def get_model_class(cls):
-        # Avoids a circular import and allows importing SessionStore when
-        # django.contrib.sessions is not in INSTALLED_APPS.
-        from django.contrib.sessions.models import Session
-        return Session
+        def instant_mode_alias(self, alias_name):
+        if os.environ.get('THEFUCK_INSTANT_MODE', '').lower() == 'true':
+            mark = USER_COMMAND_MARK + '\b' * len(USER_COMMAND_MARK)
+            return '''
+                export PS1='{user_command_mark}$PS1';
+                {app_alias}
+            '''.format(user_command_mark=mark,
+                       app_alias=self.app_alias(alias_name))
+        else:
+            log_path = os.path.join(
+                gettempdir(), 'thefuck-script-log-{}'.format(uuid4().hex))
+            return '''
+                export THEFUCK_INSTANT_MODE=True;
+                export THEFUCK_OUTPUT_LOG={log};
+                thefuck --shell-logger {log};
+                rm {log};
+                exit
+            '''.format(log=log_path)
     
-        def test_default_decoder_raises_deserialization_error(self):
-        from acme.fields import RFC3339Field
-        self.assertRaises(
-            jose.DeserializationError, RFC3339Field.default_decoder, '')
+        def how_to_configure(self):
+        return self._create_shell_configuration(
+            content=u'thefuck --alias | source',
+            path='~/.config/fish/config.fish',
+            reload='fish')
     
-            # Force reload if files were modified
-        # This is needed to recalculate augeas directive span
-        if save_files:
-            for sf in save_files:
-                self.aug.remove('/files/'+sf)
-            self.aug.load()
-        if title and not temporary:
-            self.finalize_checkpoint(title)
+    (c) 2016, Aaron Christianson
+http://github.com/ninjaaron/fast-entry_points
+'''
+from setuptools.command import easy_install
+import re
+TEMPLATE = r'''\
+# -*- coding: utf-8 -*-
+# EASY-INSTALL-ENTRY-SCRIPT: '{3}','{4}','{5}'
+__requires__ = '{3}'
+import re
+import sys
     
-        def test_eq(self):
-        self.assertTrue(self.vhost1b == self.vhost1)
-        self.assertFalse(self.vhost1 == self.vhost2)
-        self.assertEqual(str(self.vhost1b), str(self.vhost1))
-        self.assertFalse(self.vhost1b == 1234)
+    misspelled_subcommand = '''\
+az provider: 'lis' is not in the 'az provider' command group. See 'az provider --help'.
     
-    # If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-#html_use_opensearch = ''
-    
-    # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-    
-    # The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-version = u'0'
-# The full version, including alpha/beta/rc tags.
-release = u'0'
+            if form._meta.labels and class_name in form._meta.labels:
+            label = form._meta.labels[class_name]
+        else:
+            label = label_for_field(field, form._meta.model, model_admin, form=form)
     
     
-class ChineseTokenizer(Tokenizer):
+class RunInTmpDirMixin:
+    '''
+    Allow i18n tests that need to generate .po/.mo files to run in an isolated
+    temporary filesystem tree created by tempfile.mkdtemp() that contains a
+    clean copy of the relevant test code.
     
-        def __cut_detail(self, sentence):
-        blocks = re_han_detail.split(sentence)
-        for blk in blocks:
-            if re_han_detail.match(blk):
-                for word in self.__cut(blk):
-                    yield word
-            else:
-                tmp = re_skip_detail.split(blk)
-                for x in tmp:
-                    if x:
-                        if re_num.match(x):
-                            yield pair(x, 'm')
-                        elif re_eng.match(x):
-                            yield pair(x, 'eng')
-                        else:
-                            yield pair(x, 'x')
-    
-    parser = OptionParser(USAGE)
-parser.add_option('-k', dest='topK')
-opt, args = parser.parse_args()
-    
-    content = open(file_name, 'rb').read()
-    
-    import jieba
-import jieba.analyse
-from optparse import OptionParser
-    
-    nmf = decomposition.NMF(n_components=n_topic).fit(tfidf)
-print('done in %0.3fs.' % (time.time() - t0))
+    nb_examples = 100
+in_encoder = encoder_input_data[:nb_examples]
+in_decoder = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
     
     
+for seq_index in range(100):
+    # Take one sequence (part of the training set)
+    # for trying out decoding.
+    input_seq = encoder_input_data[seq_index: seq_index + 1]
+    decoded_sentence = decode_sequence(input_seq)
+    print('-')
+    print('Input sentence:', input_texts[seq_index])
+    print('Decoded sentence:', decoded_sentence)
 
     
-    t2 = time.time()
-tm_cost = t2-t1
+    - Load the original image.
+- Define a number of processing scales (i.e. image shapes),
+    from smallest to largest.
+- Resize the original image to the smallest scale.
+- For every scale, starting with the smallest (i.e. current one):
+    - Run gradient ascent
+    - Upscale image to the next scale
+    - Reinject the detail that was lost at upscaling time
+- Stop when we are back to the original size.
     
-            reset_mocap2body_xpos(sim)
-        sim.data.mocap_pos[:] = sim.data.mocap_pos + pos_delta
-        sim.data.mocap_quat[:] = sim.data.mocap_quat + quat_delta
+    '''
+Note:
+batch_size is highly sensitive.
+Only 2 epochs are needed as the dataset is very small.
+'''
     
-    import sys, gym, time
+    print('Train...')
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=15,
+          validation_data=(x_test, y_test))
+score, acc = model.evaluate(x_test, y_test,
+                            batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
+    
+        # - Produce data on 4 worker threads, consume on main thread:
+    #   - All worker threads share the SAME generator
+    model.predict_generator(custom_generator(),
+                            steps=STEPS,
+                            max_queue_size=10,
+                            workers=WORKERS,
+                            use_multiprocessing=False)
+    
+        def build(self, input_shape):
+        self.embeddings = self.add_weight(
+            shape=(self.input_dim, self.output_dim),
+            initializer=self.embeddings_initializer,
+            name='embeddings',
+            regularizer=self.embeddings_regularizer,
+            constraint=self.embeddings_constraint,
+            dtype=self.dtype)
+        self.built = True
+    
+    
+max_timeout = 5
+    
+    ## All tokens go to the parser (unless skip() is called in that rule)
+# on a particular 'channel'.  The parser tunes to a particular channel
+# so that whitespace etc... can go to the parser on a 'hidden' channel.
+DEFAULT_CHANNEL = 0
+    
+            
+        if input is not None:
+            self.input = input
+            self.index = input.index()
+    
+    
+    def toString(self, start=None, stop=None):
+        '''
+        Return the text of all tokens from start to stop, inclusive.
+        If the stream does not buffer all the tokens then it can just
+        return '' or null;  Users should not access $ruleLabel.text in
+        an action of course in that case.
+    
+        def test_multiple_runs(self):
+        # Bug 8527 reported that multiple calls produced empty
+        # __pycache__/__pycache__ directories.
+        self.assertRunOK('-q', self.pkgdir)
+        # Verify the __pycache__ directory contents.
+        self.assertTrue(os.path.exists(self.pkgdir_cachedir))
+        cachecachedir = os.path.join(self.pkgdir_cachedir, '__pycache__')
+        self.assertFalse(os.path.exists(cachecachedir))
+        # Call compileall again.
+        self.assertRunOK('-q', self.pkgdir)
+        self.assertTrue(os.path.exists(self.pkgdir_cachedir))
+        self.assertFalse(os.path.exists(cachecachedir))
+    
+            self.checkraises(TypeError, ' ', 'join')
+        self.checkraises(TypeError, ' ', 'join', None)
+        self.checkraises(TypeError, ' ', 'join', 7)
+        self.checkraises(TypeError, ' ', 'join', [1, 2, bytes()])
+        try:
+            def f():
+                yield 4 + ''
+            self.fixtype(' ').join(f())
+        except TypeError as e:
+            if '+' not in str(e):
+                self.fail('join() ate exception message')
+        else:
+            self.fail('exception not raised')
+    
+    class EOFTestCase(unittest.TestCase):
+    def test_EOFC(self):
+        expect = 'EOL while scanning string literal (<string>, line 1)'
+        try:
+            eval(''''this is a test\
+            ''')
+        except SyntaxError as msg:
+            self.assertEqual(str(msg), expect)
+        else:
+            raise support.TestFailed
+    
+            if not self.tmp_dir:
+            # When tests are run from the Python build directory, it is best practice
+            # to keep the test files in a subfolder.  This eases the cleanup of leftover
+            # files using the 'make distclean' command.
+            if sysconfig.is_python_build():
+                self.tmp_dir = sysconfig.get_config_var('abs_builddir')
+                if self.tmp_dir is None:
+                    # bpo-30284: On Windows, only srcdir is available. Using
+                    # abs_builddir mostly matters on UNIX when building Python
+                    # out of the source tree, especially when the source tree
+                    # is read only.
+                    self.tmp_dir = sysconfig.get_config_var('srcdir')
+                self.tmp_dir = os.path.join(self.tmp_dir, 'build')
+            else:
+                self.tmp_dir = tempfile.gettempdir()
+    
+        def test_block_add_hook(self):
+        self.do_test('test_block_add_hook')
