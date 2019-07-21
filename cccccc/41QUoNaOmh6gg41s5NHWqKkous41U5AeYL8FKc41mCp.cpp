4e5ba4f742d91ea4d482dde637e812784aebfd08
SUNsung <weatherius@gmail.com>
2019-07-21 23:07:39 +0300

    Rewrite [41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp] File in: 2019-07-21 23:07:38

--- cccccc/41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp
+++ cccccc/41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp
@@ -1,57 +1,184 @@
 
         
-        
-bool ZeroCopyOutputStream::WriteAliasedRaw(const void* /* data */,
-                                           int /* size */) {
-  GOOGLE_LOG(FATAL) << 'This ZeroCopyOutputStream doesn't support aliasing. '
-                'Reaching here usually means a ZeroCopyOutputStream '
-                'implementation bug.';
-  return false;
+          const tflite::Tensor* tensor = tflite::Create1dTensor(100);
+  const flatbuffers::Vector<flatbuffers::Offset<tflite::Buffer>>* buffers =
+      tflite::CreateBuffers();
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    struct OpData {
+  // IDs are the arbitrary identifiers used by TF Lite to identify and access
+  // memory buffers.
+  int fft_integer_working_area_id = kTensorNotAllocated;
+  int fft_double_working_area_id = kTensorNotAllocated;
+};
+    
+    #include 'tensorflow/core/framework/bounds_check.h'
+#include 'tensorflow/core/framework/op_kernel.h'
+#include 'tensorflow/core/framework/register_types.h'
+#include 'tensorflow/core/framework/tensor.h'
+#include 'tensorflow/core/lib/core/threadpool.h'
+    
+    
+    {  TF_CHECK_OK(GpuLaunchKernel(DynamicStitchKernel<T>, config.block_count,
+                              config.thread_per_block, 0, gpu_device.stream(),
+                              slice_size, output_size, input_indices,
+                              input_ptrs, output));
 }
     
-    const Status Status::OK = Status();
-const Status Status::CANCELLED = Status(error::CANCELLED, '');
-const Status Status::UNKNOWN = Status(error::UNKNOWN, '');
+    #include <gtest/gtest.h>
+#include 'tensorflow/lite/c/c_api_internal.h'
+#include 'tensorflow/lite/delegates/nnapi/nnapi_delegate.h'
+#include 'tensorflow/lite/model.h'
+#include 'tensorflow/lite/tools/evaluation/proto/evaluation_config.pb.h'
+#include 'tensorflow/lite/tools/evaluation/proto/evaluation_stages.pb.h'
+#include 'tensorflow/lite/tools/evaluation/utils.h'
     
-    namespace google {
-namespace protobuf {
-namespace internal {
-namespace {
-    }
-    }
-    }
-    }
+        http://www.apache.org/licenses/LICENSE-2.0
     
-    #endif  // GOOGLE_PROTOBUF_TEMPLATE_UTIL_H_
-
+      // Naive cost estimate based on the given operations count and total
+  // input/output tensor sizes of the given op_info combined.
+  Costs PredictOpCountBasedCost(double operations, const OpInfo& op_info) const;
     
     
-    {  // Yes || Yes == true.
-  value = or_<true_, true_>::value;
-  EXPECT_TRUE(value);
-  // Yes || No == true.
-  value = or_<true_, false_>::value;
-  EXPECT_TRUE(value);
-  // No || Yes == true.
-  value = or_<false_, true_>::value;
-  EXPECT_TRUE(value);
-  // No || No == false.
-  value = or_<false_, false_>::value;
-  EXPECT_FALSE(value);
-}
+    {
+    {}  // namespace functor
+}  // namespace tensorflow
     
-    namespace google {
-namespace protobuf {
-namespace util {
-    }
-    }
-    }
+    TF_CALL_int32(REGISTER_GATHER_ND_GPU);
+TF_CALL_int64(REGISTER_GATHER_ND_GPU);
+TF_CALL_GPU_NUMBER_TYPES(REGISTER_GATHER_ND_GPU);
+TF_CALL_complex64(REGISTER_GATHER_ND_GPU);
+TF_CALL_complex128(REGISTER_GATHER_ND_GPU);
     
-      {
-    // Read the existing address book.
-    fstream input(argv[1], ios::in | ios::binary);
-    if (!address_book.ParseFromIstream(&input)) {
-      cerr << 'Failed to parse address book.' << endl;
-      return -1;
-    }
+    net::NSSCertDatabase* GetNSSCertDatabaseForResourceContext(
+    content::ResourceContext* context,
+    base::OnceCallback<void(net::NSSCertDatabase*)> callback) {
+  // This initialization is not thread safe. This CHECK ensures that this code
+  // is only run on a single thread.
+  CHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
+  if (!g_nss_cert_database) {
+    // Linux has only a single persistent slot compared to ChromeOS's separate
+    // public and private slot.
+    // Redirect any slot usage to this persistent slot on Linux.
+    g_nss_cert_database = new net::NSSCertDatabase(
+        crypto::ScopedPK11Slot(
+            crypto::GetPersistentNSSKeySlot()) /* public slot */,
+        crypto::ScopedPK11Slot(
+            crypto::GetPersistentNSSKeySlot()) /* private slot */);
   }
+  return g_nss_cert_database;
+}
+    
+    v8::Local<v8::Value> Converter<const char*>::ToV8(v8::Isolate* isolate,
+                                                  const char* val) {
+  return v8::String::NewFromUtf8(isolate, val, v8::NewStringType::kNormal)
+      .ToLocalChecked();
+}
+    
+    
+    { private:
+  v8::Local<v8::Object> object_;
+};
+    
+     private:
+  ObjectTemplateBuilder& SetImpl(const base::StringPiece& name,
+                                 v8::Local<v8::Data> val);
+  ObjectTemplateBuilder& SetPropertyImpl(
+      const base::StringPiece& name,
+      v8::Local<v8::FunctionTemplate> getter,
+      v8::Local<v8::FunctionTemplate> setter);
+    
+      static Promise Create(v8::Isolate* isolate);
+  static Promise Create();
+    
+    
+    { private:
+  HANDLE mutex_;
+  DISALLOW_COPY_AND_ASSIGN(AutoUnlockMutex);
+};
+    
+            inps.getUMatVector(inputs);
+        outs.getUMatVector(outputs);
+    
+    
+    static CV_ALWAYS_INLINE
+    void integral_64_operations_avx512(const __m512i *srcs,
+                                       __m512i *sums,       const __m512i *sums_above,
+                                       __m512i *sqsums,     const __m512i *sqsums_above,
+                                       __mmask64 data_mask,
+                                       __m512i &sum_accumulator, __m512i &sqsum_accumulator)
+    {
+       __m512i src_64byte_chunk = read_64_bytes(srcs, data_mask);
+    }
+    
+        if (fps <= 0)
+    {
+        MSG(cerr << 'MFX: Invalid FPS passed to encoder' << endl);
+        return;
+    }
+    
+    
+    {        netExec = plugin.LoadNetwork(net, {});
+        infRequest = netExec.CreateInferRequest();
+    }
+    catch (const std::exception& ex)
+    {
+        CV_Error(Error::StsAssert, format('Failed to initialize Inference Engine backend: %s', ex.what()));
+    }
+    
+        auto groupedString = groupedStream.str();
+    wstring result(groupedString.rbegin(), groupedString.rend());
+    // Add the right (fractional or exponential) part of the number to the final string.
+    if (hasDecimal)
+    {
+        result += displayString.substr(dec);
+    }
+    else if (hasExponent)
+    {
+        result += displayString.substr(exp);
+    }
+    
+    CurrencyHttpClient::CurrencyHttpClient()
+    : m_client(ref new HttpClient())
+    , m_responseLanguage(L'en-US')
+{
+}
+    
+    /****************************Module*Header***********************************
+* Module Name: EngineStrings.h
+*
+* Module Description:
+*       Resource String ID's for the private strings used by Engine. Internal to Engine related code
+*         not required by the clients
+*
+* Warnings:
+*
+* Created: 13-Feb-2008
+*
+\****************************************************************************/
+    
+        // Set up all the post rounding stuff.
+    bool useSciForm = false;
+    int32_t eout = exponent - 1; // Displayed exponent.
+    MANTTYPE* pmant = pnum->mant + pnum->cdigit - 1;
+    // Case where too many digits are to the left of the decimal or
+    // FMT_SCIENTIFIC or FMT_ENGINEERING was specified.
+    if ((format == FMT_SCIENTIFIC) || (format == FMT_ENGINEERING))
+    {
+        useSciForm = true;
+        if (eout != 0)
+        {
+            if (format == FMT_ENGINEERING)
+            {
+                exponent = (eout % 3);
+                eout -= exponent;
+                exponent++;
+    }
+    }
+    }
+    
+    #include 'pch.h'
+#include 'HorizontalNoOverflowStackPanel.h'
