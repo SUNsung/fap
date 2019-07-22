cb973030b6aa2c451071cf323e137e019f00ee93
SUNsung <weatherius@gmail.com>
2019-07-22 23:12:41 +0300

    Rewrite [24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb] File in: 2019-07-22 23:12:41

--- ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
+++ ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
@@ -1,62 +1,78 @@
 
         
-            def later(desc = nil, db = RailsMultisite::ConnectionManagement.current_db, &blk)
-      if @async
-        start_thread if !@thread&.alive? && !@paused
-        @queue << [db, blk, desc]
-      else
-        blk.call
-      end
-    end
-    
-      def report
-    @report ||= JSON.parse(request.body.read)['csp-report'].slice(
-      'blocked-uri',
-      'disposition',
-      'document-uri',
-      'effective-directive',
-      'original-policy',
-      'referrer',
-      'script-sample',
-      'status-code',
-      'violated-directive',
-      'line-number',
-      'source-file'
-    )
-  end
-    
-    describe ContentSecurityPolicy::Builder do
-  let(:builder) { described_class.new }
-    
-    class ContentSecurityPolicy
-  class Default
-    attr_reader :directives
-    
-      def self.bundle(langs)
-    result = File.read(HIGHLIGHTJS_DIR + 'highlight.min.js')
-    langs.each do |lang|
-      begin
-        result << '\n' << File.read(HIGHLIGHTJS_DIR + 'languages/#{lang}.min.js')
-      rescue Errno::ENOENT
-        # no file, don't care
-      end
-    end
-    
-        builder.build
-  end
-end
-    
-        def clear_theme_extensions_cache!
-      cache.clear
-    end
-    
-          keyword = params.require(:keyword)
-      data = SvgSprite.search(keyword)
-    
-            def collection_method
-          :issues_comments
+                # if the guest has a guest path scrubber capability, use it
+        if machine.guest.capability?(:rsync_scrub_guestpath)
+          guestpath = machine.guest.capability(:rsync_scrub_guestpath, opts)
         end
     
-      def index
-    @broadcasts = Broadcast.all
+      let(:guest)   { double('guest') }
+  let(:machine) { iso_env.machine(iso_env.machine_names[0], :dummy) }
+    
+        it 'returns the updated box info within constraints' do
+      metadata = Vagrant::BoxMetadata.new(StringIO.new(<<-RAW))
+      {
+        'name': 'foo',
+        'versions': [
+          {
+            'version': '1.0'
+          },
+          {
+            'version': '1.1',
+            'providers': [
+              {
+                'name': 'virtualbox',
+                'url': 'bar'
+              }
+            ]
+          },
+          {
+            'version': '1.4',
+            'providers': [
+              {
+                'name': 'virtualbox',
+                'url': 'bar'
+              }
+            ]
+          }
+        ]
+      }
+      RAW
+    
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
+    end
+    
+    describe VagrantPlugins::Chef::Cap::Windows::ChefInstalled do
+  include_context 'unit'
+    
+            # Create a new driver instance
+        #
+        # @param [Vagrant::Machine] machine Machine instance for this driver
+        def initialize(machine)
+          if !Vagrant::Util::Which.which('docker-compose')
+            raise Errors::DockerComposeNotInstalledError
+          end
+          super()
+          @machine = machine
+          @data_directory = Pathname.new(machine.env.local_data_path).
+            join('docker-compose')
+          @data_directory.mkpath
+          @logger = Log4r::Logger.new('vagrant::docker::driver::compose')
+          @compose_lock = Mutex.new
+          @logger.debug('Docker compose driver initialize for machine `#{@machine.name}` (`#{@machine.id}`)')
+          @logger.debug('Data directory for composition file `#{@data_directory}`')
+        end
+    
+          oink = MiniTest::Mock.new
+      oink.expect(:multi, [0, 1])
+      pork = ConnectionPool.new(size: 1) { oink }
+    
+      def new_manager(opts)
+    Sidekiq::Manager.new(opts)
   end
+    
+    def Process.rss
+  `ps -o rss= -p #{Process.pid}`.chomp.to_i
+end
