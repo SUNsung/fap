2438589bee09ff4c346571fac811d4ef64c735d7
SUNsung <weatherius@gmail.com>
2019-07-22 07:49:20 +0300

    Rewrite [03tsrILecRwRM03wrPfIT3GhIQ03JiSIW2.BhI03cfk.py] File in: 2019-07-22 07:49:20

--- pyton/03tsrILecRwRM03wrPfIT3GhIQ03JiSIW2.BhI03cfk.py
+++ pyton/03tsrILecRwRM03wrPfIT3GhIQ03JiSIW2.BhI03cfk.py
@@ -1,151 +1,144 @@
 
         
-            def remaining_cards(self):
-        return len(self.cards) - self.deal_index
+            # Separating the 'table of contents' from the contents (blocks)
+    table_of_contents = ''.join(read_me.split('- - -')[0])
+    blocks = ''.join(read_me.split('- - -')[1]).split('\n# ')
+    for i in range(len(blocks)):
+        if i == 0:
+            blocks[i] = blocks[i] + '\n'
+        else:
+            blocks[i] = '# ' + blocks[i] + '\n'
     
-        def extract_year_month(self, timestamp):
-        '''Return the year and month portions of the timestamp.'''
-        ...
+        @staticmethod
+    def format_speed(speed):
+        if speed is None:
+            return '%10s' % '---b/s'
+        return '%10s' % ('%s/s' % format_bytes(speed))
     
-    
-class UserGraphService(object):
-    
-        def _hash_function(self, key):
-        return key % self.size
-    
-      # Print header
-  header = empty_cell + ' '
-  header += ''.join([' %{0}s '.format(columnwidth) % label
-                     for label in short_labels])
-    
-      # Convert from figure to an numpy array width x height x 3 (last for RGB)
-  f.canvas.draw()
-  data = np.fromstring(f.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-  data_wxhx3 = data.reshape(f.canvas.get_width_height()[::-1] + (3,))
-  plt.close()
-    
-      Args:
-    flags: From tf.app.flags
-    
-      def encode_chars(self, sentence):
-    chars_ids = [self.word_to_char_ids(cur_word)
-                 for cur_word in sentence.split()]
-    return np.vstack([self.bos_chars] + chars_ids + [self.eos_chars])
+        _TESTS = [{
+        'url': 'http://www.southpark.nl/full-episodes/s18e06-freemium-isnt-free',
+        'info_dict': {
+            'title': 'Freemium Isn\'t Free',
+            'description': 'Stan is addicted to the new Terrance and Phillip mobile game.',
+        },
+        'playlist_mincount': 3,
+    }]
     
     
-_SPECIAL_CHAR_MAP = {
-    '\xe2\x80\x98': '\'',
-    '\xe2\x80\x99': '\'',
-    '\xe2\x80\x9c': ''',
-    '\xe2\x80\x9d': ''',
-    '\xe2\x80\x93': '-',
-    '\xe2\x80\x94': '-',
-    '\xe2\x88\x92': '-',
-    '\xce\x84': '\'',
-    '\xc2\xb4': '\'',
-    '`': '\''
-}
+parser = youtube_dl.parseOpts()[0]
+build_completion(parser)
+
     
-      if model == 'dis':
-    assert FLAGS.discriminator_model == 'rnn_nas'
-    assert hparams.dis_num_layers == 2
+    print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
     
-    containers = (('thefuck/python3-tcsh',
-               u'''FROM python:3
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'),
-              ('thefuck/python2-tcsh',
-               u'''FROM python:2
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'))
     
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
+# find the correct sorting and add the required base classes so that sublcasses
+# can be correctly created
+classes = _ALL_CLASSES[:-1]
+ordered_cls = []
+while classes:
+    for c in classes[:]:
+        bases = set(c.__bases__) - set((object, InfoExtractor, SearchInfoExtractor))
+        stop = False
+        for b in bases:
+            if b not in classes and b not in ordered_cls:
+                if b.__name__ == 'GenericIE':
+                    exit()
+                classes.insert(0, b)
+                stop = True
+        if stop:
+            break
+        if all(b in ordered_cls for b in bases):
+            ordered_cls.append(c)
+            classes.remove(c)
+            break
+ordered_cls.append(_ALL_CLASSES[-1])
     
+    # -- General configuration ------------------------------------------------
+    
+        @classmethod
+    def change_cdn(cls, url):
+        # if the cnd_url starts with an ip addr, it should be youku's old CDN
+        # which rejects http requests randomly with status code > 400
+        # change it to the dispatcher of aliCDN can do better
+        # at least a little more recoverable from HTTP 403
+        if cls.dispatcher_url in url:
+            return url
+        elif 'k.youku.com' in url:
+            return url
+        else:
+            url_seg_list = list(urllib.parse.urlsplit(url))
+            url_seg_list[1] = cls.dispatcher_url
+            return urllib.parse.urlunsplit(url_seg_list)
+    
+    from xml.etree import cElementTree as ET
+from copy import copy
+from ..common import *
+#----------------------------------------------------------------------
+def ckplayer_get_info_by_xml(ckinfo):
+    '''str->dict
+    Information for CKPlayer API content.'''
+    e = ET.XML(ckinfo)
+    video_dict = {'title': '',
+                  #'duration': 0,
+                  'links': [],
+                  'size': 0,
+                  'flashvars': '',}
+    dictified = dictify(e)['ckplayer']
+    if 'info' in dictified:
+        if '_text' in dictified['info'][0]['title'][0]:  #title
+            video_dict['title'] = dictified['info'][0]['title'][0]['_text'].strip()
+    
+        url = list(set([
+        unicodize(str.replace(i, '\\/', '/'))
+        for i in re.findall(r'<meta property='og:video:secure_url' content='(.*?)'>', html)
+    ]))
+    
+            The original container is renamed to a temporary name so that data
+        volumes can be copied to the new container, before the original
+        container is removed.
         '''
     
     
-def test_default_options_overwrite(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['json'] == {'foo': 'bar'}
+class MissingDigests(Exception):
+    def __init__(self, needs_push, needs_pull):
+        self.needs_push = needs_push
+        self.needs_pull = needs_pull
+    
+        @mock.patch('compose.network.Network.true_name', lambda n: n.full_name)
+    def test_check_remote_network_config_options_mismatch(self):
+        net = Network(None, 'compose_test', 'net1', 'overlay')
+        with pytest.raises(NetworkConfigChangedError) as e:
+            check_remote_network_config({'Driver': 'overlay', 'Options': {
+                'com.docker.network.driver.foo': 'baz'
+            }}, net)
+    
+            events = list(progress_stream.stream_output(events, output))
+        assert len(output.getvalue()) > 0
     
     
-@mock.patch('httpie.core.get_response')
-def test_timeout(get_response):
-    def error(msg, *args, **kwargs):
-        global error_msg
-        error_msg = msg % args
+class TestSudokuValidator(unittest.TestCase):
+    '''[summary]
+    Test for the file sudoku_validator.py
     
-        if args.auth_plugin:
-        session.auth = {
-            'type': args.auth_plugin.auth_type,
-            'raw_auth': args.auth_plugin.raw_auth,
-        }
-    elif session.auth:
-        kwargs['auth'] = session.auth
-    
-        if not opts and not args:
-        # Display help.
-        print(_help)
-        # Enter GUI mode.
-        #from .gui import gui_main
-        #gui_main()
-    else:
-        conf = {}
-        for opt, arg in opts:
-            if opt in ('-h', '--help'):
-                # Display help.
-                print(_help)
-    
-    from ..common import *
-    
-    site_info = 'Dailymotion.com'
-download = dailymotion_download
-download_playlist = playlist_not_supported('dailymotion')
-
-    
-    site_info = 'FC2Video'
-download = fc2video_download
-download_playlist = playlist_not_supported('fc2video')
-
+        def _rotate_right(self, parent_node: Node, child_index: int):
+        '''
+        Take key from left brother of the child and transfer to the child
+        '''
+        parent_key = parent_node.keys[child_index - 1]
+        new_parent_key = parent_node.children[child_index - 1].keys.pop()
+        parent_node.children[child_index].keys.insert(0, parent_key)
+        parent_node.keys[child_index - 1] = new_parent_key
     
     
-    {
-    {pattern_inline_title = r'<title>([^<]*)</title>'
-pattern_inline_api_key = r'api\.site_key\s*=\s*'([^']+)''
-pattern_inline_img_url = r''url':'([^']+)','key':'[^']+'}}'
-pattern_inline_NSID = r''nsid'\s*:\s*'([^']+)''
-pattern_inline_video_mark = r'('mediaType':'video')'
-    
-            # cookie handler
-        ssl_context = request.HTTPSHandler(
-            context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
-        cookie_handler = request.HTTPCookieProcessor()
-        opener = request.build_opener(ssl_context, cookie_handler)
-        opener.addheaders = [
-            ('Referer', self.url),
-            ('Cookie',
-             'CloudFront-Policy=%s;CloudFront-Signature=%s;CloudFront-Key-Pair-Id=%s' % (scp, scs, sck))
-        ]
-        request.install_opener(opener)
-    
-    from ..utils import get_w, get_shape
-from ..initializers import constant
-    
-        Args:
-        x:
-        c_embed_size:
-        share_cnn_weights:
-        name:
-        reuse:
+import random
