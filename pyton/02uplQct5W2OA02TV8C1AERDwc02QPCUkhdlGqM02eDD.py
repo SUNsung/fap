debce5f4fdaf83381f7d37202ffee9423579633e
SUNsung <weatherius@gmail.com>
2019-07-22 06:13:48 +0300

    Rewrite [02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py] File in: 2019-07-22 06:13:47

--- pyton/02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py
+++ pyton/02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py
@@ -1,142 +1,200 @@
 
         
-        # Autoencoder = Encoder + Decoder
-# Instantiate Autoencoder Model
-autoencoder = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
-autoencoder.summary()
-    
-    print('Convert class vector to binary class matrix '
-      '(for use with categorical_crossentropy)')
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-print('y_train shape:', y_train.shape)
-print('y_test shape:', y_test.shape)
-    
-    
-print('\nBuilding network 2...')
-model2 = create_network(num_classes=num_classes, **network2)
-    
-        @property
-    def config(self):
-        if not hasattr(self, '_config'):
-            self._config = Config(directory=self.config_dir)
-            if self._config.is_new():
-                self._config.save()
-            else:
-                self._config.load()
-        return self._config
-    
-        # If both `auth_parse` and `prompt_password` are set to `True`,
-    # and the value of `-a` lacks the password part,
-    # then the user will be prompted to type the password in.
-    prompt_password = True
-    
-        @staticmethod
-    def make_header(username, password):
-        credentials = u'%s:%s' % (username, password)
-        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
-        return 'Basic %s' % token
-    
-        def get_converters(self):
-        return [plugin for plugin in self
-                if issubclass(plugin, ConverterPlugin)]
-    
-    
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
+        
+def main():
+    ''' main entry point for module execution
+    '''
+    backup_spec = dict(
+        filename=dict(),
+        dir_path=dict(type='path')
     )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
-
+    argument_spec = dict(
+        src=dict(type='path'),
     
+        def test_action_with_complex_and_complex_args(self):
+        m = ModuleArgsParser(dict(action=dict(module='copy', args=dict(src='a', dest='b'))))
+        mod, args, to = m.parse()
+        self._debug(mod, args, to)
     
-def test_follow_all_output_options_used_for_redirects(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 3
-    assert HTTP_OK not in r
+            testrole/tasks/main.yml:
+        - include: 'include1.yml'
+          static: no
     
-            if add_server_ips:
-            if module.check_mode:
-                _check_mode(module, _add_server_ips(module,
-                                                    oneandone_conn,
-                                                    firewall_policy['id'],
-                                                    add_server_ips))
+        wapi = WapiModule(module)
+    # to check for vendor specific dhcp option
+    ib_spec = check_vendor_specific_dhcp_option(module, ib_spec)
     
-    try:
-    from pyvcloud.schema.vcd.v1_5.schemas.vcloud.networkType import FirewallRuleType
-    from pyvcloud.schema.vcd.v1_5.schemas.vcloud.networkType import ProtocolsType
-except ImportError:
-    # normally set a flag here but it will be caught when testing for
-    # the existence of pyvcloud (see module_utils/vca.py).  This just
-    # protects against generating an exception at runtime
-    pass
-    
-    # Ensure rule with certain limitations
-- ipa_hbacrule:
-    name: allow_all_developers_access_to_db
-    description: Allow all developers to access any database from any host
-    hostgroup:
-    - db-server
-    usergroup:
-    - developers
+    - name: Configure interface in access and trunk mode using aggregate
+  junos_l2_interface:
+    aggregate:
+    - name: ge-0/0/1
+      description: test-interface-access
+      mode: access
+      access_vlan: red
+    - name: ge-0/0/2
+      description: test-interface-trunk
+      mode: trunk
+      trunk_vlans:
+      - blue
+      - green
+      native_vlan: 100
+    active: True
     state: present
-    ipa_host: ipa.example.com
-    ipa_user: admin
-    ipa_pass: topsecret
+'''
     
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
+        Async friendly.
+    '''
+    data[msg['key']] = msg['value']
+    await store.async_save(data)
+    connection.send_message(websocket_api.result_message(
+        msg['id'],
+    ))
     
-        SUMMARY_COMMAND = ('summary', 'summary -B')[is_version_higher_than_5_18()]
+        assert 0 == len(calls)
     
+        result = await async_setup_component(hass, 'snips', {
+        'snips': {
+            'feedback_sounds': 'on',
+            'probability': 'none',
+            'site_ids': 'default'
+        },
+    })
+    assert not result
     
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
+        def test_invalidcalib(self):
+        '''Test invalid sensor values.'''
+        self.hass.states.set('test.indoortemp', '10',
+                             {ATTR_UNIT_OF_MEASUREMENT: TEMP_CELSIUS})
+        self.hass.states.set('test.outdoortemp', '10',
+                             {ATTR_UNIT_OF_MEASUREMENT: TEMP_CELSIUS})
+        self.hass.states.set('test.indoorhumidity', '0',
+                             {ATTR_UNIT_OF_MEASUREMENT: '%'})
     
-        def _maybe_fire_closing(self):
-        if self.closing and not self.inprogress:
-            if self.nextcall:
-                self.nextcall.cancel()
-                if self.heartbeat.running:
-                    self.heartbeat.stop()
-            self.closing.callback(None)
+        mock_discover, mock_platform = yield from mock_discovery(hass, discover)
     
-        def _debug_cookie(self, request, spider):
-        if self.debug:
-            cl = [to_native_str(c, errors='replace')
-                  for c in request.headers.getlist('Cookie')]
-            if cl:
-                cookies = '\n'.join('Cookie: {}\n'.format(c) for c in cl)
-                msg = 'Sending cookies to: {}\n{}'.format(request, cookies)
-                logger.debug(msg, extra={'spider': spider})
+      def apply_gradients(self, grads_and_vars, global_step=None, name=None):
+    '''See base class.'''
+    assignments = []
+    for (grad, param) in grads_and_vars:
+      if grad is None or param is None:
+        continue
     
+      @classmethod
+  def get_unreachable_ops(cls, graph, outputs):
+    '''Finds all of the tensors in graph that are unreachable from outputs.'''
+    outputs = cls.flatten_recursive(outputs)
+    output_to_op = collections.defaultdict(list)
+    op_to_all = collections.defaultdict(list)
+    assign_out_to_in = collections.defaultdict(list)
     
-def setup(app):
-    app.add_config_value('edit_on_github_project', '', True)
-    app.add_config_value('edit_on_github_branch', 'master', True)
-    app.add_config_value('edit_on_github_src_path', '', True)  # 'eg' 'docs/'
-    app.connect('html-page-context', html_page_context)
+        per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+    loss = tf.reduce_mean(per_example_loss)
+    
+      def test_is_whitespace(self):
+    self.assertTrue(tokenization._is_whitespace(u' '))
+    self.assertTrue(tokenization._is_whitespace(u'\t'))
+    self.assertTrue(tokenization._is_whitespace(u'\r'))
+    self.assertTrue(tokenization._is_whitespace(u'\n'))
+    self.assertTrue(tokenization._is_whitespace(u'\u00A0'))
+    
+        :param arg1: Description of arg1.
+    :type arg1: str.
+    :param arg2: Description of arg2.
+    :type arg2: bool.
+    :returns:  int -- description of the return value.
+    :raises: AttributeError, KeyError
+    '''
+    docstring_info = docstrings.parse(docstring)
+    expected_docstring_info = DocstringInfo(
+        summary='Docstring summary.',
+        description='This is a longer description of the docstring. It spans '
+        'across multiple lines.',
+        args=[
+            ArgInfo(name='arg1', type='str',
+                    description='Description of arg1.'),
+            ArgInfo(name='arg2', type='bool',
+                    description='Description of arg2.'),
+        ],
+        returns='int -- description of the return value.',
+        raises='AttributeError, KeyError',
+    )
+    self.assertEqual(docstring_info, expected_docstring_info)
+    
+      fish_source = '''function __fish_using_command
+    set cmd (commandline -opc)
+    for i in (seq (count $cmd) 1)
+        switch $cmd[$i]
+        case '-*'
+        case '*'
+            if [ $cmd[$i] = $argv[1] ]
+                return 0
+            else
+                return 1
+            end
+        end
+    end
+    return 1
+end
+    
+    if __name__ == '__main__':
+  main()
 
     
-        if unit_1 == LENGTH_MILES:
-        meters = __miles_to_meters(value)
-    elif unit_1 == LENGTH_FEET:
-        meters = __feet_to_meters(value)
-    elif unit_1 == LENGTH_KILOMETERS:
-        meters = __kilometers_to_meters(value)
+      def testSetParseFnsDefaultsFromFire(self):
+    # Fire should use the decorator to know how to parse string arguments.
+    self.assertEqual(core.Fire(WithDefaults, command=['example1']), (10, int))
+    self.assertEqual(core.Fire(WithDefaults, command=['example1', '10']),
+                     (10, float))
+    self.assertEqual(core.Fire(WithDefaults, command=['example1', '13']),
+                     (13, float))
+    self.assertEqual(core.Fire(WithDefaults, command=['example1', '14.0']),
+                     (14, float))
     
-    from aiohttp import web
+        return bot_params
+
+    
+        piece_x_sum = 0
+    piece_x_c = 0
+    piece_y_max = 0
+    scan_x_border = int(w / 8)  # 扫描棋子时的左右边界
+    scan_start_y = 0  # 扫描的起始 y 坐标
+    im_pixel = im.load()
+    # 以 50px 步长，尝试探测 scan_start_y
+    for i in range(int(h / 3), int(h * 2 / 3), 50):
+        last_pixel = im_pixel[0, i]
+        for j in range(1, w):
+            pixel = im_pixel[j, i]
+            # 不是纯色的线，则记录 scan_start_y 的值，准备跳出循环
+            if pixel[0] != last_pixel[0] or pixel[1] != last_pixel[1] or pixel[2] != last_pixel[2]:
+                scan_start_y = i - 50
+                break
+        if scan_start_y:
+            break
+    # print('scan_start_y: {}'.format(scan_start_y))
+    
+    
+fig = plt.figure()
+pull_screenshot()
+img = np.array(Image.open('autojump.png'))
+im = plt.imshow(img, animated=True)
+    
+    # Enable and configure HTTP caching (disabled by default)
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+    
+    def alter_content(request: HttpRequest, content: bytes) -> bytes:
+    first_paragraph_text = get_content_description(content, request)
+    return content.replace(request.placeholder_open_graph_description.encode('utf-8'),
+                           first_paragraph_text.encode('utf-8'))
+    
+        user_file = list(request.FILES.values())[0]
+    file_size = user_file._get_size()
+    if settings.MAX_FILE_UPLOAD_SIZE * 1024 * 1024 < file_size:
+        return json_error(_('Uploaded file is larger than the allowed limit of %s MB') % (
+            settings.MAX_FILE_UPLOAD_SIZE))
+    check_upload_within_quota(user_profile.realm, file_size)
