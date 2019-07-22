d2a036fe5a39e896c1d8837a66571bdb77c0d272
SUNsung <weatherius@gmail.com>
2019-07-22 08:19:16 +0300

    Rewrite [59VY7al3tuVdQ59QSs1DfgtPzs59LVNVyu6mIeM59TsA.cpp] File in: 2019-07-22 08:19:16

--- cccccc/59VY7al3tuVdQ59QSs1DfgtPzs59LVNVyu6mIeM59TsA.cpp
+++ cccccc/59VY7al3tuVdQ59QSs1DfgtPzs59LVNVyu6mIeM59TsA.cpp
@@ -1,44 +1,226 @@
 
         
-        
-    {    Lock& lock;
-    Lock templock;
-};
+            std::array<CalcEngine::Rational, NUM_WIDTH_LENGTH> m_chopNumbers;      // word size enforcement
+    std::array<std::wstring, NUM_WIDTH_LENGTH> m_maxDecimalValueStrings;   // maximum values represented by a given word width based off m_chopNumbers
+    static std::unordered_map<std::wstring, std::wstring> s_engineStrings; // the string table shared across all instances
+    wchar_t m_decimalSeparator;
+    wchar_t m_groupSeparator;
     
-      // Simulate no-space errors while this pointer is non-NULL.
-  port::AtomicPointer no_space_;
+            *this = Rational{ lhsRat };
+        destroyrat(lhsRat);
     
-    inline LookupKey::~LookupKey() {
-  if (start_ != space_) delete[] start_;
+                bool TryParseWebResponses(
+                _In_ Platform::String ^ staticDataJson,
+                _In_ Platform::String ^ allRatiosJson,
+                _Inout_ std::vector<UCM::CurrencyStaticData>& staticData,
+                _Inout_ CurrencyRatioMap& allRatiosData);
+            bool TryParseStaticData(_In_ Platform::String ^ rawJson, _Inout_ std::vector<UCM::CurrencyStaticData>& staticData);
+            bool TryParseAllRatiosData(_In_ Platform::String ^ rawJson, _Inout_ CurrencyRatioMap& allRatiosData);
+            concurrency::task<void> FinalizeUnits(_In_ const std::vector<UCM::CurrencyStaticData>& staticData, _In_ const CurrencyRatioMap& ratioMap);
+            void GuaranteeSelectedUnits();
+    
+    
+    {
+    {        std::shared_ptr<UCM::IUnitConverterVMCallback> m_vmCallback;
+        std::vector<std::tuple<std::wstring, UCM::Unit>> m_suggestedList;
+        std::wstring m_curValue;
+    };
+}
+
+    
+    // Gets the number in memory for UI to keep it persisted and set it again to a different instance
+// of CCalcEngine. Otherwise it will get destructed with the CalcEngine
+unique_ptr<Rational> CCalcEngine::PersistedMemObject()
+{
+    return move(m_memoryValue);
 }
     
-    #ifndef GRPC_CUSTOM_DEFAULT_THREAD_POOL
+        class CalculatorHistory : public IHistoryDisplay
+    {
+    public:
+        CalculatorHistory(const size_t maxSize);
+        unsigned int AddToHistory(
+            _In_ std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> const& spTokens,
+            _In_ std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> const& spCommands,
+            std::wstring_view result);
+        std::vector<std::shared_ptr<HISTORYITEM>> const& GetHistory();
+        std::shared_ptr<HISTORYITEM> const& GetHistoryItem(unsigned int uIdx);
+        void ClearHistory();
+        unsigned int AddItem(_In_ std::shared_ptr<HISTORYITEM> const& spHistoryItem);
+        bool RemoveItem(unsigned int uIdx);
+        size_t MaxHistorySize() const
+        {
+            return m_maxHistorySize;
+        }
+        ~CalculatorHistory(void);
+    }
     
-    #include <grpc/support/port_platform.h>
+      Status NewRandomRWFile(const std::string& fname,
+                         std::unique_ptr<RandomRWFile>* result,
+                         const EnvOptions& options) override {
+    auto status_and_enc_path = EncodePathWithNewBasename(fname);
+    if (!status_and_enc_path.first.ok()) {
+      return status_and_enc_path.first;
+    }
+    return EnvWrapper::NewRandomRWFile(status_and_enc_path.second, result,
+                                       options);
+  }
     
-    #include <grpc/impl/codegen/port_platform.h>
+    /*
+ * Class:     org_rocksdb_CompactionJobStats
+ * Method:    fileFsyncNanos
+ * Signature: (J)J
+ */
+jlong Java_org_rocksdb_CompactionJobStats_fileFsyncNanos(
+    JNIEnv*, jclass, jlong jhandle) {
+  auto* compact_job_stats =
+      reinterpret_cast<rocksdb::CompactionJobStats*>(jhandle);
+  return static_cast<jlong>(
+      compact_job_stats->file_fsync_nanos);
+}
     
-    TEST(AutoCompactTest, ReadHalf) { DoReads(kCount / 2); }
+    /*
+ * Class:     org_rocksdb_CompressionOptions
+ * Method:    enabled
+ * Signature: (J)Z
+ */
+jboolean Java_org_rocksdb_CompressionOptions_enabled(
+    JNIEnv*, jobject, jlong jhandle) {
+  auto* opt = reinterpret_cast<rocksdb::CompressionOptions*>(jhandle);
+  return static_cast<bool>(opt->enabled);
+}
+/*
+ * Class:     org_rocksdb_CompressionOptions
+ * Method:    disposeInternal
+ * Signature: (J)V
+ */
+void Java_org_rocksdb_CompressionOptions_disposeInternal(
+    JNIEnv*, jobject, jlong jhandle) {
+  delete reinterpret_cast<rocksdb::CompressionOptions*>(jhandle);
+}
+
     
-      // Offset at which to start looking for the first record to return
-  uint64_t const initial_offset_;
+    /*
+ * Class:     org_rocksdb_EnvOptions
+ * Method:    setWritableFileMaxBufferSize
+ * Signature: (JJ)V
+ */
+void Java_org_rocksdb_EnvOptions_setWritableFileMaxBufferSize(
+    JNIEnv*, jobject, jlong jhandle, jlong writable_file_max_buffer_size) {
+  ENV_OPTIONS_SET_SIZE_T(jhandle, writable_file_max_buffer_size);
+}
     
-    #include <string>
     
-      // Compress blocks using the specified compression algorithm.  This
-  // parameter can be changed dynamically.
-  //
-  // Default: kSnappyCompression, which gives lightweight but fast
-  // compression.
-  //
-  // Typical speeds of kSnappyCompression on an Intel(R) Core(TM)2 2.4GHz:
-  //    ~200-500MB/s compression
-  //    ~400-800MB/s decompression
-  // Note that these speeds are significantly faster than most
-  // persistent storage speeds, and therefore it is typically never
-  // worth switching to kNoCompression.  Even if the input data is
-  // incompressible, the kSnappyCompression implementation will
-  // efficiently detect that and will switch to uncompressed mode.
-  CompressionType compression = kSnappyCompression;
+    {  rocksdb::RocksDBExceptionJni::ThrowNew(env, s);
+  return nullptr;
+}
     
-    #include 'leveldb/cache.h'
+    /*
+ * Class:     org_rocksdb_SstFileManager
+ * Method:    disposeInternal
+ * Signature: (J)V
+ */
+void Java_org_rocksdb_SstFileManager_disposeInternal(JNIEnv* /*env*/,
+                                                     jobject /*jobj*/,
+                                                     jlong jhandle) {
+  auto* sptr_sst_file_manager =
+      reinterpret_cast<std::shared_ptr<rocksdb::SstFileManager>*>(jhandle);
+  delete sptr_sst_file_manager;
+}
+
+    
+    #include <jni.h>
+#include <functional>
+#include <memory>
+    
+    /*
+ * Class:     org_rocksdb_ThreadStatus
+ * Method:    getOperationStageName
+ * Signature: (B)Ljava/lang/String;
+ */
+jstring Java_org_rocksdb_ThreadStatus_getOperationStageName(
+    JNIEnv* env, jclass, jbyte joperation_stage_value) {
+  auto name = rocksdb::ThreadStatus::GetOperationStageName(
+      rocksdb::OperationStageJni::toCppOperationStage(joperation_stage_value));
+  return rocksdb::JniUtil::toJavaString(env, &name, true);
+}
+    
+      bool readWithPriority(T& item, size_t priority) {
+    return queues_[priority].readIfNotEmpty(item);
+  }
+    
+      Fingerprint& update(StringPiece str) {
+    // TODO(tudorb): We could be smart and do update64 or update32 if aligned
+    for (auto c : str) {
+      update8(uint8_t(c));
+    }
+    return *this;
+  }
+    
+    
+    {  // Restart the I/O thread
+  restartThread();
+}
+    
+    
+    {
+    {    M map2(map);
+    M map3(std::move(map));
+    map = map2;
+    map2.clear();
+    map2 = std::move(map3);
+  }
+  EXPECT_TRUE(ranHasher);
+  EXPECT_TRUE(ranEqual);
+  EXPECT_TRUE(ranAlloc);
+  EXPECT_TRUE(ranDealloc);
+}
+    
+    template <class Alloc>
+void Arena<Alloc>::merge(Arena<Alloc>&& other) {
+  blocks_.splice_after(blocks_.before_begin(), other.blocks_);
+  other.blocks_.clear();
+  other.ptr_ = other.end_ = nullptr;
+  totalAllocatedSize_ += other.totalAllocatedSize_;
+  other.totalAllocatedSize_ = 0;
+}
+    
+    
+    {  Sem sem;
+  std::array<std::thread, nthreads> threads;
+  for (auto i = 0ull; i < rounds; ++i) {
+    std::vector<uint64_t> wait_seq;
+    std::vector<uint64_t> wake_seq;
+    WaitForAll ready(nthreads); // first nthreads waits, then nthreads posts
+    for (auto thi = 0ull; thi < nthreads; ++thi) {
+      threads[thi] = std::thread([&, thi] {
+        sem.wait(
+            [&, thi] { wait_seq.push_back(thi), ready.post(); },
+            [&, thi] { wake_seq.push_back(thi); });
+      });
+    }
+    ready.wait(); // first nthreads waits, then nthreads posts
+    for (auto thi = 0ull; thi < nthreads; ++thi) {
+      sem.post();
+    }
+    for (auto thi = 0ull; thi < nthreads; ++thi) {
+      threads[thi].join();
+    }
+    EXPECT_EQ(nthreads, wait_seq.size());
+    EXPECT_EQ(nthreads, wake_seq.size());
+    switch (wake_policy(sem)) {
+      case SemaphoreWakePolicy::Fifo:
+        break;
+      case SemaphoreWakePolicy::Lifo:
+        std::reverse(wake_seq.begin(), wake_seq.end());
+        break;
+    }
+    EXPECT_EQ(wait_seq, wake_seq);
+  }
+}
+    
+      virtual void importKeysFromRdb(ConnectionsTree::DatabaseItem& ns) override;
+    
+      if (m_operation->isRunning()) {
+    return false;
+  }
