586f7fd0c042fb8021f3641c2f4a5fa71c5669f4
SUNsung <weatherius@gmail.com>
2019-07-23 16:01:41 +0300

    Rewrite [09d2Z4pU8VvVk097fBwdamG2A09F.jZ68dUvKo09UxJ.py] File in: 2019-07-23 16:01:41

--- pyton/09d2Z4pU8VvVk097fBwdamG2A09F.jZ68dUvKo09UxJ.py
+++ pyton/09d2Z4pU8VvVk097fBwdamG2A09F.jZ68dUvKo09UxJ.py
@@ -1,151 +1,176 @@
 
         
-        
-    {    # Options that need a file parameter
-    'download-archive': ['--require-parameter'],
-    'cookies': ['--require-parameter'],
-    'load-info': ['--require-parameter'],
-    'batch-file': ['--require-parameter'],
-}
+                return {
+            'id': video_id,
+            'title': title,
+            'formats': formats,
+            'comment_count': int_or_none(item.get('comments')),
+            'description': item.get('description'),
+            'duration': int_or_none(item.get('duration')),
+            'thumbnails': thumbnails,
+            'timestamp': int_or_none(item.get('date')),
+            'uploader': item.get('owner_id'),
+            'view_count': int_or_none(item.get('views')),
+        }
+
     
-    new_version = {}
+        _TESTS = [{
+        'url': 'http://www.southparkstudios.dk/full-episodes/s18e07-grounded-vindaloop',
+        'info_dict': {
+            'title': 'Grounded Vindaloop',
+            'description': 'Butters is convinced he\'s living in a virtual reality.',
+        },
+        'playlist_mincount': 3,
+    }, {
+        'url': 'http://www.southparkstudios.dk/collections/2476/superhero-showdown/1',
+        'only_matching': True,
+    }, {
+        'url': 'http://www.southparkstudios.nu/collections/2476/superhero-showdown/1',
+        'only_matching': True,
+    }]
+
     
-        with io.open(infile, encoding='utf-8') as inf:
-        readme = inf.read()
+        bug_text = re.search(
+        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
+    dev_text = re.search(
+        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING YOUTUBE-DL',
+        readme).group(1)
     
-    import io
-import optparse
-import os
-import sys
+    if __name__ == '__main__':
+    main()
+
     
-            params = get_params(test_case.get('params', {}))
-        params['outtmpl'] = tname + '_' + params['outtmpl']
-        if is_playlist and 'playlist' not in test_case:
-            params.setdefault('extract_flat', 'in_playlist')
-            params.setdefault('skip_download', True)
+        outfile, = args
     
-        def test_socks4(self):
-        self.assertTrue(isinstance(self._get_ip('socks4'), compat_str))
+    # Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
     
-        dct = dict(__instancecheck__=_check, __subclasscheck__=_check)
-    meta = type('ABCBase', (type, ), dct)
-    return meta(name, tuple(), dct)
     
-        ('border-top-style: dotted',
-     {'border': {'top': {'style': 'mediumDashDotDot'}}}),
-    ('border-top-style: dotted; border-top-width: thin',
-     {'border': {'top': {'style': 'dotted'}}}),
-    ('border-top-style: dashed',
-     {'border': {'top': {'style': 'mediumDashed'}}}),
-    ('border-top-style: dashed; border-top-width: thin',
-     {'border': {'top': {'style': 'dashed'}}}),
-    ('border-top-style: double',
-     {'border': {'top': {'style': 'double'}}}),
-    # - color
-    ('border-style: solid; border-color: #0000ff',
-     {'border': {'top': {'style': 'medium', 'color': '0000FF'},
-                 'right': {'style': 'medium', 'color': '0000FF'},
-                 'bottom': {'style': 'medium', 'color': '0000FF'},
-                 'left': {'style': 'medium', 'color': '0000FF'}}}),
-    ('border-top-style: double; border-top-color: blue',
-     {'border': {'top': {'style': 'double', 'color': '0000FF'}}}),
-    ('border-top-style: solid; border-top-color: #06c',
-     {'border': {'top': {'style': 'medium', 'color': '0066CC'}}}),
-    # ALIGNMENT
-    # - horizontal
-    ('text-align: center',
-     {'alignment': {'horizontal': 'center'}}),
-    ('text-align: left',
-     {'alignment': {'horizontal': 'left'}}),
-    ('text-align: right',
-     {'alignment': {'horizontal': 'right'}}),
-    ('text-align: justify',
-     {'alignment': {'horizontal': 'justify'}}),
-    # - vertical
-    ('vertical-align: top',
-     {'alignment': {'vertical': 'top'}}),
-    ('vertical-align: text-top',
-     {'alignment': {'vertical': 'top'}}),
-    ('vertical-align: middle',
-     {'alignment': {'vertical': 'center'}}),
-    ('vertical-align: bottom',
-     {'alignment': {'vertical': 'bottom'}}),
-    ('vertical-align: text-bottom',
-     {'alignment': {'vertical': 'bottom'}}),
-    # - wrap_text
-    ('white-space: nowrap',
-     {'alignment': {'wrap_text': False}}),
-    ('white-space: pre',
-     {'alignment': {'wrap_text': False}}),
-    ('white-space: pre-line',
-     {'alignment': {'wrap_text': False}}),
-    ('white-space: normal',
-     {'alignment': {'wrap_text': True}}),
-    # NUMBER FORMAT
-    ('number-format: 0%',
-     {'number_format': {'format_code': '0%'}}),
+def _download_restricted(url, filename, age):
+    ''' Returns true if the file has been downloaded '''
+    
+        def test_app_alias(self, shell):
+        assert 'setenv TF_SHELL tcsh' in shell.app_alias('fuck')
+        assert 'alias fuck' in shell.app_alias('fuck')
+        assert 'alias FUCK' in shell.app_alias('FUCK')
+        assert 'thefuck' in shell.app_alias('fuck')
+    
+        def how_to_configure(self):
+        return self._create_shell_configuration(
+            content=u'thefuck --alias | source',
+            path='~/.config/fish/config.fish',
+            reload='fish')
+    
+            In most of shells we change history on shell-level, but not
+        all shells support it (Fish).
+    
+        @memoize
+    def get_aliases(self):
+        raw_aliases = os.environ.get('TF_SHELL_ALIASES', '').split('\n')
+        return dict(self._parse_alias(alias)
+                    for alias in raw_aliases if alias and '=' in alias)
+    
+        import sys
+    from mymodule import entry_function
+    sys.exit(entry_function())
+    
+    
+@pytest.mark.parametrize('command, result', [
+    (Command('az providers list', misspelled_command), ['az provider list']),
+    (Command('az provider lis', misspelled_subcommand), ['az provider list'])
 ])
-def test_css_to_excel(css, expected):
-    convert = CSSToExcelConverter()
-    assert expected == convert(css)
+def test_get_new_command(command, result):
+    assert get_new_command(command) == result
+
     
-            # explicitly ensure file was compressed.
-        with tm.decompress_file(path, compression) as fh:
-            result = fh.read().decode('utf8')
-        assert_frame_equal(df, pd.read_json(result))
+    from django.utils.deprecation import RemovedInDjango31Warning
+from django.utils.functional import cached_property
+from django.utils.inspect import method_has_no_args
+from django.utils.translation import gettext_lazy as _
     
+            if form._meta.labels and class_name in form._meta.labels:
+            label = form._meta.labels[class_name]
+        else:
+            label = label_for_field(field, form._meta.model, model_admin, form=form)
     
-def test_bin32():
-    header = b'\xc6'
-    data = b'x' * 65536
-    b = packb(data, use_bin_type=True)
-    assert len(b) == len(data) + 5
-    assert b[0:1] == header
-    assert b[1:5] == b'\x00\x01\x00\x00'
-    assert b[5:] == data
-    assert unpackb(b) == data
+        def has_perms(self, perm_list, obj=None):
+        '''
+        Return True if the user has each of the specified permissions. If
+        object is passed, check if the user has all required perms for it.
+        '''
+        return all(self.has_perm(perm, obj) for perm in perm_list)
     
-        # 40 ok for read_size=1024, while 50 introduces errors
-    # 7000 ok for read_size=1024*1024, while 8000 leads to glibc detected ***
-    # python: double free or corruption (!prev):
-    
-    
-  def Start( self ):
-    request_data = BuildRequestData()
-    request_data.update( { 'filetypes': self.filetypes } )
-    self._response = self.PostDataToHandler( request_data,
-                                             'semantic_completion_available' )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+        def _assertPoKeyword(self, keyword, expected_value, haystack, use_quotes=True):
+        q = '''
+        if use_quotes:
+            expected_value = ''%s'' % expected_value
+            q = '''
+        needle = '%s %s' % (keyword, expected_value)
+        expected_value = re.escape(expected_value)
+        return self.assertTrue(
+            re.search('^%s %s' % (keyword, expected_value), haystack, re.MULTILINE),
+            'Could not find %(q)s%(n)s%(q)s in generated PO file' % {'n': needle, 'q': q}
+        )
     
     
-def LevelWarnings_test():
-  opts = _JavaFilter( { 'level' : 'warning' } )
-  f = _CreateFilterForTypes( opts, [ 'java' ] )
+# ### The Field sub-classes for each OGR Field type. ###
+class OFTInteger(Field):
+    _bit64 = False
     
-        try:
-      result = self.fn( *self.args, **self.kwargs )
-    except BaseException:
-      e = sys.exc_info()[ 1 ]
-      self.future.set_exception( e )
-    else:
-      self.future.set_result( result )
+            if response.status == 503:
+            # out of quota
+            if 'gws' not in server_type and 'Google Frontend' not in server_type and 'GFE' not in server_type:
+                xlog.warn('503 but server type:%s', server_type)
+                return False
+            else:
+                return True
     
-            self._invoke_callbacks()
-        return True
+                # Is there a bitwise operation to do this?
+            if v == 0xFFFF:
+                v = -1
     
-    |======================= In-process =====================|== Out-of-process ==|
     
-    import functools
-import time
-import timeit
-import sys
+class AbstractExpert(object):
     
-    def with_thread_pool_executor():
-    with ThreadPoolExecutor(10) as executor:
-        return list(executor.map(is_prime, PRIMES))
+        # And can also be undone at will
+    >>> for cmd in reversed(command_stack):
+    ...     cmd.undo()
+    renaming baz.txt to bar.txt
+    renaming bar.txt to foo.txt
+    
+    from __future__ import print_function
+    
+        @Transactional will rollback to entry-state upon exceptions.
+    '''
+    
+    
+def convert_to_text(data):
+    print('[CONVERT]')
+    return '{} as text'.format(data)
+    
+        # After we've accessed `relatives`
+    >>> sorted(Jhon.__dict__.items())
+    [('call_count2', 0), ..., ('relatives', 'Many relatives.')]
+    
+    *TL;DR
+Allows the interface of an existing class to be used as another interface.
+'''
+    
+    *TL;DR
+Decouples an abstraction from its implementation.
+'''
+    
+        def add(self, graphic):
+        self.graphics.append(graphic)
+    
+    
+class ComputerFacade(object):
+    '''
+    Represents a facade for various computer parts.
+    '''
+    def __init__(self):
+        self.cpu = CPU()
+        self.memory = Memory()
+        self.ssd = SolidStateDrive()
