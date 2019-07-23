1feefcac402ec745c0ac1b4e06102bf35a47de17
SUNsung <weatherius@gmail.com>
2019-07-23 11:01:25 +0300

    Rewrite [56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb] File in: 2019-07-23 11:01:24

--- ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
+++ ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
@@ -1,44 +1,32 @@
 
         
-          # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-    
-      def test_font_helper_with_suffix_sharp
-    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
+          def force?
+    flag? '--force'
   end
     
-        class Main < Clamp::Command
-      subcommand 'list', 'List all installed Logstash plugins', LogStash::PluginManager::List
-      subcommand 'install', 'Install a Logstash plugin', LogStash::PluginManager::Install
-      subcommand 'remove', 'Remove a Logstash plugin', LogStash::PluginManager::Remove
-      subcommand 'update', 'Update a plugin', LogStash::PluginManager::Update
-      subcommand 'pack', 'Package currently installed plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Pack
-      subcommand 'unpack', 'Unpack packaged plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Unpack
-      subcommand 'generate', 'Create the foundation for a new plugin', LogStash::PluginManager::Generate
-      subcommand 'uninstall', 'Uninstall a plugin. Deprecated: Please use remove instead', LogStash::PluginManager::Remove
-      subcommand 'prepare-offline-pack', 'Create an archive of specified plugins to use for offline installation', LogStash::PluginManager::PrepareOfflinePack
-    end
-  end
-end
+          # Find a bottle built for a previous version of macOS.
+      def find_older_compatible_tag(tag)
+        begin
+          tag_version = MacOS::Version.from_symbol(tag)
+        rescue ArgumentError
+          return
+        end
     
-            if explicit_plugins.any? { |spec| filename =~ /^#{spec.name}/ }
-          FileUtils.mv(gem_file, ::File.join(explicit_path, filename))
-        else
-          FileUtils.mv(gem_file, ::File.join(dependencies_path, filename))
+              find_every_method_call_by_name(body_node, :system).each do |method|
+            next unless match = regex_match_group(parameters(method).first, /^(env|export)(\s+)?/)
+    
+              find_method_with_args(body_node, :system, 'cargo', 'build') do
+            problem 'use \'cargo\', \'install\', \'--root\', prefix, \'--path\', \'.\''
+          end
         end
       end
     end
-    
-      it 'does object equality on config_hash and pipeline_id' do
-    another_exact_pipeline = described_class.new(source, pipeline_id, ordered_config_parts, settings)
-    expect(subject).to eq(another_exact_pipeline)
-    
-            def swap_range(corrector, range1, range2)
-          src1 = range1.source
-          src2 = range2.source
-          corrector.replace(range1, src2)
-          corrector.replace(range2, src1)
+    module FormulaAuditStrict
+      class Text < FormulaCop
+        def audit_formula(_node, _class_node, _parent_class_node, body_node)
+          find_method_with_args(body_node, :go_resource) do
+            problem '`go_resource`s are deprecated. Please ask upstream to implement Go vendoring'
+          end
         end
       end
     end
@@ -46,105 +34,139 @@ end
 end
 
     
-          def ignored_node?(node)
-        # Same object found in array?
-        ignored_nodes.any? { |n| n.equal?(node) }
-      end
-    
-      # Get the version of this package
-  def version
-    if instance_variable_defined?(:@version) && !@version.nil?
-      return @version
-    elsif attributes[:version_given?]
-      # 'version_given?' will be true in cases where the
-      # fpm command-line tool has been given '-v' or '--version' settings
-      # We do this check because the default version is '1.0'
-      # on the fpm command line.
-      return attributes.fetch(:version)
+          allow(OS).to receive(:mac?).and_return(true)
+      allow(OS::Mac).to receive(:version).and_return(OS::Mac::Version.new(sierra_os_version))
     end
     
-              extension_header[TAR_TYPEFLAG_OFFSET] = 'x'
-          extension_header[TAR_LENGTH_OFFSET_START..TAR_LENGTH_OFFSET_END] = extension_data.length.to_s(8).rjust(12, '0')
-          extension_header = checksum_header(extension_header)
+          remaining + non_options
+    end
     
-        # extract Payload
-    safesystem('tar -xz -f #{expand_dir}/Payload -C #{staging_path}')
-  end # def input
+    describe Cask::Cmd::Cache, :cask do
+  let(:local_transmission) {
+    Cask::CaskLoader.load(cask_path('local-transmission'))
+  }
     
-      # Add a new source to this package.
-  # The exact behavior depends on the kind of package being managed.
-  #
-  # For instance:
-  #
-  # * for FPM::Package::Dir, << expects a path to a directory or files.
-  # * for FPM::Package::RPM, << expects a path to an rpm.
-  #
-  # The idea is that you can keep pumping in new things to a package
-  # for later conversion or output.
-  #
-  # Implementations are expected to put files relevant to the 'input' in the
-  # staging_path
-  def input(pacman_pkg_path)
-    control = {}
-    # Unpack the control tarball
-    safesystem(tar_cmd, '-C', staging_path, '-xf', pacman_pkg_path)
-    pkginfo = staging_path('.PKGINFO')
-    mtree = staging_path('.MTREE')
-    install = staging_path('.INSTALL')
+          unless system curl_executable, '--silent', '--fail', '--output', '/dev/null', package_url
+        package_blob = <<~JSON
+          {'name': '#{bintray_package}',
+           'public_download_numbers': true,
+           'public_stats': true}
+        JSON
+        curl '--silent', '--fail', '--user', '#{bintray_user}:#{bintray_key}',
+             '--header', 'Content-Type: application/json',
+             '--data', package_blob, bintray_repo_url
+        puts
+      end
     
-        # Remove the stuff we don't want
-    delete_these = ['.depdb', '.depdblock', '.filemap', '.lock', '.channel', 'cache', 'temp', 'download', '.channels', '.registry']
-    Find.find(staging_path) do |path|
-      if File.file?(path)
-        logger.info('replacing staging_path in file', :replace_in => path, :staging_path => staging_path)
-        begin
-          content = File.read(path).gsub(/#{Regexp.escape(staging_path)}/, '')
-          File.write(path, content)
-        rescue ArgumentError => e
-          logger.warn('error replacing staging_path in file', :replace_in => path, :error => e)
+      def version(val = nil)
+    @version ||= begin
+      version = detect_version(val)
+      version.null? ? nil : version
+    end
+  end
+    
+        def log_http_get_files(files, from, cached = false)
+      return if files.empty?
+      s = '  #{'CACHED ' if cached}GET #{files.length} files from #{from} #{files * ' '}...'
+      if cached
+        puts dark green s
+      else
+        puts dark cyan s
+      end
+    end
+    
+      # Code is not reloaded between requests.
+  config.cache_classes = true
+    
+      # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+    
+    LogStash::Bundler.setup!
+    
+        def initialize(plugins_to_package, target)
+      @plugins_to_package = Array(plugins_to_package)
+      @target = target
+    
+          PluginManager.ui.info('Install successful')
+    rescue ::Bundler::BundlerError => e
+      raise PluginManager::InstallError.new(e), 'An error occurred went installing plugins'
+    ensure
+      FileUtils.rm_rf(uncompressed_path) if uncompressed_path && Dir.exist?(uncompressed_path)
+    end
+    
+        puts('Updating #{filtered_plugins.collect(&:name).join(', ')}') unless filtered_plugins.empty?
+    
+              it 'successfully install the plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-dns')
+          end
+        end
+    
+        def shipping_eq_billing_address?
+      bill_address == ship_address
+    end
+    
+          def gateway_error(error)
+        text = if error.is_a? ActiveMerchant::Billing::Response
+                 error.params['message'] || error.params['response_reason_text'] || error.message
+               elsif error.is_a? ActiveMerchant::ConnectionError
+                 Spree.t(:unable_to_connect_to_gateway)
+               else
+                 error.to_s
+               end
+        logger.error(Spree.t(:gateway_error))
+        logger.error('  #{error.to_yaml}')
+        raise Core::GatewayError, text
+      end
+    
+      def self.check_unused_translations
+    self.used_translations ||= []
+    self.unused_translation_messages = []
+    self.unused_translations = []
+    load_translations(translations)
+    translation_diff = unused_translations - used_translations
+    translation_diff.each do |translation|
+      Spree.unused_translation_messages << '#{translation} (#{I18n.locale})'
+    end
+  end
+    
+            def create
+          authorize! :create, Image
+          @image = scope.images.new(image_params)
+          if @image.save
+            respond_with(@image, status: 201, default_template: :show)
+          else
+            invalid_resource!(@image)
+          end
+        end
+    
+            def create
+          authorize! :create, Spree::OptionType
+          @option_type = Spree::OptionType.new(option_type_params)
+          if @option_type.save
+            render :show, status: 201
+          else
+            invalid_resource!(@option_type)
+          end
+        end
+    
+            def product_property_params
+          params.require(:product_property).permit(permitted_product_properties_attributes)
         end
       end
-      FileUtils.rm_r(path) if delete_these.include?(File.basename(path))
     end
-    
-      def install_script
-    path = build_path('installer.sh')
-    File.open(path, 'w') do |file|
-      file.write template('sh.erb').result(binding)
-    end
-    path
   end
+end
+
     
-      def default_output
-    v = version
-    v = '#{epoch}:#{v}' if epoch
-    if iteration
-      '#{name}_#{v}-#{iteration}_#{architecture}.#{type}'
-    else
-      '#{name}_#{v}_#{architecture}.#{type}'
-    end
-  end # def default_output
-end # class FPM::Deb
-    
-    describe Tmuxinator::Pane do
-  let(:klass) { described_class }
-  let(:instance) { klass.new(index, project, window, *commands) }
-  # let(:index) { 'vim' }
-  # let(:project) { 0 }
-  # let(:tab) { nil }
-  # let(:commands) { nil }
-  let(:index) { 0 }
-  let(:project) { double }
-  let(:window) { double }
-  let(:commands) { ['vim', 'bash'] }
-    
-      def attributes_match
-    expect(@actual).to have_attributes(@expected_attrs)
-  end
-    
-      if base_index = options.fetch(:base_index) { 1 }
-    standard_options << 'base-index #{base_index}'
-  end
-    
-          it { is_expected.to be true }
-    end
+            def create
+          authorize! :create, Property
+          @property = Spree::Property.new(property_params)
+          if @property.save
+            respond_with(@property, status: 201, default_template: :show)
+          else
+            invalid_resource!(@property)
+          end
+        end
