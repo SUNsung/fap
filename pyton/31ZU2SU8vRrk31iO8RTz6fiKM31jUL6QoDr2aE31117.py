eef6748dff8284a982863e85077481310c7be70c
SUNsung <weatherius@gmail.com>
2019-07-24 09:34:36 +0300

    Rewrite [31ZU2SU8vRrk31iO8RTz6fiKM31jUL6QoDr2aE31117.py] File in: 2019-07-24 09:34:36

--- pyton/31ZU2SU8vRrk31iO8RTz6fiKM31jUL6QoDr2aE31117.py
+++ pyton/31ZU2SU8vRrk31iO8RTz6fiKM31jUL6QoDr2aE31117.py
@@ -1,161 +1,262 @@
 
         
-            def _hash_function(self, key):
-        return key % self.size
+        This is a simple smoke test of a local install of VGGish and its associated
+downloaded files. We create a synthetic sound, extract log mel spectrogram
+features, run them through VGGish, post-process the embedding ouputs, and
+check some simple statistics of the results, allowing for variations that
+might occur due to platform/version differences in the libraries we use.
     
+      # Run K-means clustering.
+  def _get_input_fn():
+    '''Helper function to create input function and hook for training.
     
-class Cache(object):
+      aggregation_extraction.ExtractAggregatedRepresentationsToFiles(
+      image_names=image_list,
+      features_dir=cmd_args.features_dir,
+      aggregation_config_path=cmd_args.aggregation_config_path,
+      mapping_path=cmd_args.index_mapping_path,
+      output_aggregation_dir=cmd_args.output_aggregation_dir)
     
-            self.assertTrue(refund_transaction.called)
-        self.assertTrue(promotion_log_add.called)
-        queries_unset.assert_called_once_with(self.campaign)
-        emailer_refunded_promo.assert_called_once_with(self.link)
-        self.assertTrue(success)
+    cmd_args = None
     
-            for width in (108, 216, 320, 640, 960, 1080):
-            url = self.provider.resize_image(image, width)
-            self.assertEqual(url, 'https://unsplash.it/%d/%d' % (width,
-                width*2))
+      Returns:
+    Function that receives an image and returns detection results.
+  '''
+  tf.saved_model.loader.load(
+      sess, [tf.saved_model.tag_constants.SERVING],
+      model_dir,
+      import_scope=import_scope)
+  import_scope_prefix = import_scope + '/' if import_scope is not None else ''
+  input_images = sess.graph.get_tensor_by_name('%sinput_images:0' %
+                                               import_scope_prefix)
+  boxes = sess.graph.get_tensor_by_name('%sdetection_boxes:0' %
+                                        import_scope_prefix)
+  scores = sess.graph.get_tensor_by_name('%sdetection_scores:0' %
+                                         import_scope_prefix)
+  class_indices = sess.graph.get_tensor_by_name('%sdetection_classes:0' %
+                                                import_scope_prefix)
+    
+            feature_io.WriteToFile(out_desc_fullpath, locations_out,
+                               feature_scales_out, descriptors_out,
+                               attention_out)
+    
+      tf.logging.info('Found %d inliers' % sum(inliers))
+    
+        def test_get_version_error(self, shell, Popen):
+        Popen.return_value.stdout.read.side_effect = OSError
+        with pytest.raises(OSError):
+            shell._get_version()
+        assert Popen.call_args[0][0] == ['bash', '-c', 'echo $BASH_VERSION']
 
     
-        def test_different_queries(self):
-        u = UrlParser('http://example.com/')
-        u2 = UrlParser('http://example.com/?foo')
-        u3 = UrlParser('http://example.com/?foo=bar')
-        self.assertNotEquals(u, u2)
-        self.assertNotEquals(u2, u3)
-    
-            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertTrue('non_field_errors' in response.data)
-    
-        def __str__(self):
-        global str_called
-        str_called = True
-        return 'An example'
-    
-        def test_prefetch_related_excluding_instance_from_original_queryset(self):
-        '''
-        Regression test for https://github.com/encode/django-rest-framework/issues/4661
-        '''
-        view = UserUpdate.as_view()
-        pk = self.user.pk
-        groups_pk = self.groups[0].pk
-        request = factory.put('/', {'username': 'exclude', 'groups': [groups_pk]}, format='json')
-        response = view(request, pk=pk)
-        assert User.objects.get(pk=pk).groups.count() == 1
-        expected = {
-            'id': pk,
-            'username': 'exclude',
-            'groups': [1],
-            'email': 'tom@example.com'
-        }
-        assert response.data == expected
-
-    
-            # data_1
-        serializer = Serializer(instance, data=input_data_1, partial=True)
-        assert serializer.is_valid()
-        assert len(serializer.validated_data) == 1
-        assert serializer.validated_data['extra_field'] == 2
-        assert serializer.errors == {}
+        def instant_mode_alias(self, alias_name):
+        if os.environ.get('THEFUCK_INSTANT_MODE', '').lower() == 'true':
+            mark = USER_COMMAND_MARK + '\b' * len(USER_COMMAND_MARK)
+            return '''
+                export PS1='{user_command_mark}$PS1';
+                {app_alias}
+            '''.format(user_command_mark=mark,
+                       app_alias=self.app_alias(alias_name))
+        else:
+            log_path = os.path.join(
+                gettempdir(), 'thefuck-script-log-{}'.format(uuid4().hex))
+            return '''
+                export THEFUCK_INSTANT_MODE=True;
+                export THEFUCK_OUTPUT_LOG={log};
+                thefuck --shell-logger {log};
+                rm {log};
+                exit
+            '''.format(log=log_path)
     
     
-class FileUploadParser(BaseParser):
+@pytest.mark.usefixtures('no_memoize')
+@pytest.mark.parametrize('script, output', [
+    ('vom file.py', 'vom: not found'),
+    ('fucck', 'fucck: not found'),
+    ('puthon', ''puthon' is not recognized as an internal or external command'),
+    ('got commit', 'got: command not found')])
+def test_match(mocker, script, output):
+    mocker.patch('thefuck.rules.no_command.which', return_value=None)
+    
+    
+@classmethod
+def get_args(cls, dist, header=None):
     '''
-    Parser for file upload data.
+    Yield write_script() argument tuples for a distribution's
+    console_scripts and gui_scripts entry points.
     '''
-    media_type = '*/*'
-    errors = {
-        'unhandled': 'FileUpload parse error - none of upload handlers can handle the stream',
-        'no_filename': 'Missing filename. Request should include a Content-Disposition header with a filename parameter.',
-    }
+    if header is None:
+        header = cls.get_header()
+    spec = str(dist.as_requirement())
+    for type_ in 'console', 'gui':
+        group = type_ + '_scripts'
+        for name, ep in dist.get_entry_map(group).items():
+            # ensure_safe_name
+            if re.search(r'[\\/]', name):
+                raise ValueError('Path separators not allowed in script names')
+            script_text = TEMPLATE.format(
+                          ep.module_name, ep.attrs[0], '.'.join(ep.attrs),
+                          spec, group, name)
+            args = cls._get_script_args(type_, name, header, script_text)
+            for res in args:
+                yield res
     
-        def __init__(self, detail=None, code=None, available_renderers=None):
-        self.available_renderers = available_renderers
-        super().__init__(detail, code)
+    misspelled_subcommand = '''\
+az provider: 'lis' is not in the 'az provider' command group. See 'az provider --help'.
     
+    from twisted import version as _txv
+twisted_version = (_txv.major, _txv.minor, _txv.micro)
     
-class EUCTWDistributionAnalysis(CharDistributionAnalysis):
-    def __init__(self):
-        CharDistributionAnalysis.__init__(self)
-        self._mCharToFreqOrder = EUCTWCharToFreqOrder
-        self._mTableSize = EUCTW_TABLE_SIZE
-        self._mTypicalDistributionRatio = EUCTW_TYPICAL_DISTRIBUTION_RATIO
+    import scrapy
+from scrapy.commands import ScrapyCommand
+from scrapy.linkextractors import LinkExtractor
     
-        def get_confidence(self):
-        st = self.get_state()
-        if st == constants.eFoundIt:
-            return 0.99
-        elif st == constants.eNotMe:
-            return 0.01
-        bestConf = 0.0
-        self._mBestGuessProber = None
-        for prober in self._mProbers:
-            if not prober:
-                continue
-            if not prober.active:
-                if constants._debug:
-                    sys.stderr.write(prober.get_charset_name()
-                                     + ' not active\n')
-                continue
-            cf = prober.get_confidence()
-            if constants._debug:
-                sys.stderr.write('%s confidence = %s\n' %
-                                 (prober.get_charset_name(), cf))
-            if bestConf < cf:
-                bestConf = cf
-                self._mBestGuessProber = prober
-        if not self._mBestGuessProber:
-            return 0.0
-        return bestConf
-#        else:
-#            self._mBestGuessProber = self._mProbers[0]
-#            return self._mBestGuessProber.get_confidence()
+        def process_options(self, args, opts):
+        ScrapyCommand.process_options(self, args, opts)
+        try:
+            opts.spargs = arglist_to_dict(opts.spargs)
+        except ValueError:
+            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
+        if opts.output:
+            if opts.output == '-':
+                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
+            else:
+                self.settings.set('FEED_URI', opts.output, priority='cmdline')
+            feed_exporters = without_none_values(
+                self.settings.getwithbase('FEED_EXPORTERS'))
+            valid_output_formats = feed_exporters.keys()
+            if not opts.output_format:
+                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
+            if opts.output_format not in valid_output_formats:
+                raise UsageError('Unrecognized output format '%s', set one'
+                                 ' using the '-t' switch or as a file extension'
+                                 ' from the supported list %s' % (opts.output_format,
+                                                                  tuple(valid_output_formats)))
+            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
+    
+            sfile = sys.modules[spidercls.__module__].__file__
+        sfile = sfile.replace('.pyc', '.py')
+        self.exitcode = os.system('%s '%s'' % (editor, sfile))
 
     
-        def get_charset_name(self):
-        return None
     
-        def next_state(self, c):
-        # for each byte we get its class
-        # if it is first byte, we also get byte length
-        # PY3K: aBuf is a byte stream, so c is an int, not a byte
-        byteCls = self._mModel['classTable'][wrap_ord(c)]
-        if self._mCurrentState == eStart:
-            self._mCurrentBytePos = 0
-            self._mCurrentCharLen = self._mModel['charLenTable'][byteCls]
-        # from byte's class and stateTable, we get its next state
-        curr_state = (self._mCurrentState * self._mModel['classFactor']
-                      + byteCls)
-        self._mCurrentState = self._mModel['stateTable'][curr_state]
-        self._mCurrentBytePos += 1
-        return self._mCurrentState
+def sanitize_module_name(module_name):
+    '''Sanitize the given module name, by replacing dashes and points
+    with underscores and prefixing it with a letter if it doesn't start
+    with one
+    '''
+    module_name = module_name.replace('-', '_').replace('.', '_')
+    if module_name[0] not in string.ascii_letters:
+        module_name = 'a' + module_name
+    return module_name
     
-    # 128  --> 0.79
-# 256  --> 0.92
-# 512  --> 0.986
-# 1024 --> 0.99944
-# 2048 --> 0.99999
-#
-# Idea Distribution Ratio = 0.98653 / (1-0.98653) = 73.24
-# Random Distribution Ration = 512 / (2350-512) = 0.279.
-# 
-# Typical Distribution Ratio  
+    from scrapy.http import Headers
+from scrapy.utils.httpobj import urlparse_cached
+from scrapy.utils.python import to_bytes
+from scrapy.responsetypes import responsetypes
     
-    GB2312_TABLE_SIZE = 3760
+        @classmethod
+    def from_crawler(cls, crawler):
+        o = cls(crawler.settings.getfloat('DOWNLOAD_TIMEOUT'))
+        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
+        return o
     
-    # 0 : illegal
-# 1 : very unlikely
-# 2 : normal
-# 3 : very likely
-Latin1ClassModel = (
-    # UDF OTH ASC ASS ACV ACO ASV ASO
-    0,  0,  0,  0,  0,  0,  0,  0,  # UDF
-    0,  3,  3,  3,  3,  3,  3,  3,  # OTH
-    0,  3,  3,  3,  3,  3,  3,  3,  # ASC
-    0,  3,  3,  3,  1,  1,  3,  3,  # ASS
-    0,  3,  3,  3,  1,  2,  1,  2,  # ACV
-    0,  3,  3,  3,  3,  3,  3,  3,  # ACO
-    0,  3,  1,  3,  1,  1,  1,  3,  # ASV
-    0,  3,  1,  3,  1,  1,  3,  3,  # ASO
-)
+        def spider_opened(self, spider):
+        self.storage.open_spider(spider)
+    
+    # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
+# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
+# positive matches at the risk of more false negatives.
+    
+    
+@click.command()
+@click.argument('image_to_check')
+@click.option('--cpus', default=1, help='number of CPU cores to use in parallel. -1 means 'use all in system'')
+@click.option('--model', default='hog', help='Which face detection model to use. Options are 'hog' or 'cnn'.')
+def main(image_to_check, cpus, model):
+    # Multi-core processing only supported on Python 3.4 or greater
+    if (sys.version_info < (3, 4)) and cpus != 1:
+        click.echo('WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!')
+        cpus = 1
+    
+    # To run this, you need a Raspberry Pi 2 (or greater) with face_recognition and
+# the picamera[array] module installed.
+# You can follow this installation instructions to get your RPi set up:
+# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
+    
+            face_encoding_a1 = api.face_encodings(img_a1)[0]
+        face_encoding_a2 = api.face_encodings(img_a2)[0]
+        face_encoding_a3 = api.face_encodings(img_a3)[0]
+        face_encoding_b1 = api.face_encodings(img_b1)[0]
+    
+        # Display the resulting image
+    cv2.imshow('Video', frame)
+    
+                # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+    
+        # Quit
+    cv2.destroyAllWindows()
+
+    
+    
+__all__ = [
+    'shell_escape',
+    'unnest',
+    'dedup',
+    'run',
+    'load_path',
+    'partialclass',
+    'to_text',
+    'to_bytes',
+    'locale_encoding',
+    'chunked',
+    'take',
+    'divide',
+    'getpreferredencoding',
+    'decode_for_output',
+    'get_canonical_encoding_name',
+    'get_wrapped_stream',
+    'StreamWrapper',
+]
+    
+    # Internal, to tell whether the command line session is interactive.
+try:
+    SESSION_IS_INTERACTIVE = _isatty(sys.stdout.fileno())
+except UnsupportedOperation:
+    SESSION_IS_INTERACTIVE = _isatty(sys.stdout)
+    
+            @cerberus.Validator.schema.setter
+        def schema(self, schema):
+            if schema is None:
+                self._schema = None
+            elif self.is_child:
+                self._schema = schema
+            elif isinstance(schema, cerberus.schema.DefinitionSchema):
+                self._schema = schema
+            else:
+                self._schema = cerberus.schema.UnvalidatedSchema(schema)
+    
+    __version__ = '0.5.1'
+    
+        Returns
+    -------
+    str
+        The code to be evaluated by the shell
+    '''
+    from jinja2 import Environment, FileSystemLoader
+    if shell in [None, 'auto']:
+        shell = get_auto_shell()
+    if not isinstance(shell, Shell):
+        shell = Shell[shell]
+    prog_name = prog_name or click.get_current_context().find_root().info_name
+    env_name = env_name or '_%s_COMPLETE' % prog_name.upper().replace('-', '_')
+    extra_env = extra_env if extra_env else {}
+    env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)))
+    template = env.get_template('%s.j2' % shell.name)
+    return template.render(prog_name=prog_name, complete_var=env_name, extra_env=extra_env)
+    
+        downloadable_extensions = source_extensions + ('.whl',)
