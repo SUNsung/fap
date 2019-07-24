eb1cd99c439be6f5e17e4eb3e6660d89c292c4ff
SUNsung <weatherius@gmail.com>
2019-07-24 10:30:41 +0300

    Rewrite [46nQseP1r3TZM465cdHvAgjn2M46Uw4KTSV.xmg46kFE.rb] File in: 2019-07-24 10:30:40

--- ruby/46nQseP1r3TZM465cdHvAgjn2M46Uw4KTSV.xmg46kFE.rb
+++ ruby/46nQseP1r3TZM465cdHvAgjn2M46Uw4KTSV.xmg46kFE.rb
@@ -1,212 +1,105 @@
 
         
-          def serialize_options(resource)
-    methods = resource_class.authentication_keys.dup
-    methods = methods.keys if methods.is_a?(Hash)
-    methods << :password if resource.respond_to?(:password)
-    { methods: methods, only: [:password] }
-  end
-    
-      devise_scope :user do
-    get '/some/route' => 'some_devise_controller'
-  end
-    
-    class ApplicationController < ActionController::Base
-end
-    
-    module Devise
-  module Controllers
-    # A module that may be optionally included in a controller in order
-    # to provide remember me behavior. Useful when signing in is done
-    # through a callback, like in OmniAuth.
-    module Rememberable
-      # Return default cookie values retrieved from session options.
-      def self.cookie_values
-        Rails.configuration.session_options.slice(:path, :domain, :secure)
-      end
-    
-            @email = headers[:to]
-        headers
-      end
-    
-            def send_reset_password_instructions_notification(token)
-          send_devise_notification(:reset_password_instructions, token, {})
-        end
-    
+              # Initialize a new instance
       #
-  # Calls the associated block with the name of every file and directory listed
-  # as arguments, then recursively on their subdirectories, and so on.
-  #
-  # Returns an enumerator if no block is given.
-  #
-  # See the +Find+ module documentation for an example.
-  #
-  def find(*paths, ignore_error: true) # :yield: path
-    block_given? or return enum_for(__method__, *paths, ignore_error: ignore_error)
+      # @param [Sandbox] sandbox see #sandbox
+      # @param [String] sandbox_root see #sandbox_root
+      # @param [Xcodeproj::Project] pods_project see #pods_project
+      # @param [Array<UmbrellaTargetDescription>] umbrella_targets see #umbrella_targets
+      #
+      def initialize(sandbox, sandbox_root, pods_project, umbrella_targets)
+        @sandbox = sandbox
+        @sandbox_root = sandbox_root
+        @pods_project = pods_project
+        @umbrella_targets = umbrella_targets
+      end
     
-      def in_addr(addr)
-    case addr
-    when Array
-      octets = addr
-    else
-      m = RE_IPV4ADDRLIKE.match(addr) or return nil
-      octets = m.captures
-    end
-    octets.inject(0) { |i, s|
-      (n = s.to_i) < 256 or raise InvalidAddressError, 'invalid address'
-      s.match(/\A0./) and raise InvalidAddressError, 'zero-filled number in IPv4 address is ambiguous'
-      i << 8 | n
-    }
-  end
+          # Initialize a new instance
+      #
+      # @param  [PodTarget, AggregateTarget] target @see target
+      #
+      def initialize(target)
+        @target = target
+        @headers = [
+          Header.new(target.umbrella_header_path.basename, true),
+        ]
+      end
     
-            rex = Regexp.new('#{Regexp.quote(file_name)}(\.o|\.rb)?')
-        return false if $'.find{|f| f =~ rex}
-    
-        # Loads the given file in the current session's context and evaluates it.
-    #
-    # See Irb#suspend_input_method for more information.
-    def load_file(path, priv = nil)
-      irb.suspend_name(path, File.basename(path)) do
-    
-            self.description = <<-DESC
-          Shows the content of the pods cache as a YAML tree output, organized by pod.
-          If `NAME` is given, only the caches for that pod will be included in the output.
-        DESC
-    
-            if sets.count == 1
-          set = sets.first
-        elsif sets.map(&:name).include?(spec)
-          set = sets.find { |s| s.name == spec }
-        else
-          names = sets.map(&:name) * ', '
-          raise Informative, 'More than one spec found for '#{spec}':\n#{names}'
+            it 'returns non vendored framework input and output paths by config' do
+          @pod_target.stubs(:should_build?).returns(true)
+          @pod_target.stubs(:build_type).returns(Target::BuildType.dynamic_framework)
+          @target.framework_paths_by_config['Debug'].should == [
+            Target::FrameworkPaths.new('${BUILT_PRODUCTS_DIR}/BananaLib/BananaLib.framework'),
+          ]
+          @target.framework_paths_by_config['Release'].should == [
+            Target::FrameworkPaths.new('${BUILT_PRODUCTS_DIR}/BananaLib/BananaLib.framework'),
+          ]
         end
     
-      action :message do
-    title 'Send a message'
-    description 'This action allows you to send a message by providing the appropriate options'
-    # Acceptable Parameters
-    param :to, 'The e-mail addresses of the recipients (max 50)', :type => Array
-    param :cc, 'The e-mail addresses of any CC contacts (max 50)', :type => Array
-    param :bcc, 'The e-mail addresses of any BCC contacts (max 50)', :type => Array
-    param :from, 'The e-mail address for the From header', :type => String
-    param :sender, 'The e-mail address for the Sender header', :type => String
-    param :subject, 'The subject of the e-mail', :type => String
-    param :tag, 'The tag of the e-mail', :type => String
-    param :reply_to, 'Set the reply-to address for the mail', :type => String
-    param :plain_body, 'The plain text body of the e-mail', :type => String
-    param :html_body, 'The HTML body of the e-mail', :type => String
-    param :attachments, 'An array of attachments for this e-mail', :type => Array
-    param :headers, 'A hash of additional headers', :type => Hash
-    param :bounce, 'Is this message a bounce?', :type => :boolean
-    # Errors
-    error 'ValidationError', 'The provided data was not sufficient to send an email', :attributes => {:errors => 'A hash of error details'}
-    error 'NoRecipients', 'There are no recipients defined to receive this message'
-    error 'NoContent', 'There is no content defined for this e-mail'
-    error 'TooManyToAddresses', 'The maximum number of To addresses has been reached (maximum 50)'
-    error 'TooManyCCAddresses', 'The maximum number of CC addresses has been reached (maximum 50)'
-    error 'TooManyBCCAddresses', 'The maximum number of BCC addresses has been reached (maximum 50)'
-    error 'FromAddressMissing', 'The From address is missing and is required'
-    error 'UnauthenticatedFromAddress', 'The From address is not authorised to send mail from this server'
-    error 'AttachmentMissingName', 'An attachment is missing a name'
-    error 'AttachmentMissingData', 'An attachment is missing data'
-    # Return
-    returns Hash
-    # Action
-    action do
-      attributes = {}
-      attributes[:to] = params.to
-      attributes[:cc] = params.cc
-      attributes[:bcc] = params.bcc
-      attributes[:from] = params.from
-      attributes[:sender] = params.sender
-      attributes[:subject] = params.subject
-      attributes[:reply_to] = params.reply_to
-      attributes[:plain_body] = params.plain_body
-      attributes[:html_body] = params.html_body
-      attributes[:bounce] = params.bounce ? true : false
-      attributes[:tag] = params.tag
-      attributes[:custom_headers] = params.headers
-      attributes[:attachments] = []
-      (params.attachments || []).each do |attachment|
-        next unless attachment.is_a?(Hash)
-        attributes[:attachments] << {:name => attachment['name'], :content_type => attachment['content_type'], :data => attachment['data'], :base64 => true}
+            if language == :swift
+          source_file = project.path.dirname.+('#{name}/main.swift')
+          source_file.parent.mkpath
+          import_statement = pod_target.should_build? && pod_target.defines_module? ? 'import #{pod_target.product_module_name}\n' : ''
+          source_file.open('w') { |f| f << import_statement }
+        else
+          source_file = project.path.dirname.+('#{name}/main.m')
+          source_file.parent.mkpath
+          import_statement = if pod_target.should_build? && pod_target.defines_module?
+                               '@import #{pod_target.product_module_name};\n'
+                             else
+                               header_name = '#{pod_target.product_module_name}/#{pod_target.product_module_name}.h'
+                               if pod_target.sandbox.public_headers.root.+(header_name).file?
+                                 '#import <#{header_name}>\n'
+                               else
+                                 ''
+                               end
+                             end
+          source_file.open('w') do |f|
+            f << '@import Foundation;\n'
+            f << '@import UIKit;\n' if platform == :ios || platform == :tvos
+            f << '@import Cocoa;\n' if platform == :osx
+            f << '#{import_statement}int main() {}\n'
+          end
+        end
+        source_file
       end
-      message = OutgoingMessagePrototype.new(identity.server, request.ip, 'api', attributes)
-      message.credential = identity
-      if message.valid?
-        result = message.create_messages
-        {:message_id => message.message_id, :messages => result}
+    
+              it 'saves the xcconfig' do
+            path = temporary_directory + 'sample.xcconfig'
+            @generator.save_as(path)
+            generated = Xcodeproj::Config.new(path)
+            generated.class.should == Xcodeproj::Config
+          end
+    
+            # @return [Hash<String, TargetInstallationResult>] pod_target_installation_results
+        #         Hash of pod target name to installation results.
+        #
+        attr_reader :pod_target_installation_results
+    
+    # The project root directory
+$root = ::File.dirname(__FILE__)
+    
+    ERR
+      end
+    end
+    
+      class IncludeArrayTag < Liquid::Tag
+    Syntax = /(#{Liquid::QuotedFragment}+)/
+    def initialize(tag_name, markup, tokens)
+      if markup =~ Syntax
+        @array_name = $1
       else
-        error message.errors.first
+        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
       end
-    end
+    
+      # Returns a title cased string based on John Gruber's title case http://daringfireball.net/2008/08/title_case_update
+  def titlecase(input)
+    input.titlecase
   end
     
-      expansion(:details) {
-    {
-      :rcpt_to => o.rcpt_to,
-      :mail_from => o.mail_from,
-      :subject => o.subject,
-      :message_id => o.message_id,
-      :timestamp => o.timestamp.to_f,
-      :direction => o.scope,
-      :size => o.size,
-      :bounce => o.bounce,
-      :bounce_for_id => o.bounce_for_id,
-      :tag => o.tag,
-      :received_with_ssl => o.received_with_ssl
-    }
-  }
-    
-      def update
-    if @credential.update(params.require(:credential).permit(:name, :hold))
-      redirect_to_with_json [organization, @server, :credentials]
-    else
-      render_form_errors 'edit', @credential
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
     end
-  end
-    
-    end
-
-    
-      def attachment
-    if @message.attachments.size > params[:attachment].to_i
-      attachment = @message.attachments[params[:attachment].to_i]
-      send_data attachment.body, :content_type => attachment.mime_type, :disposition => 'download', :filename => attachment.filename
-    else
-      redirect_to attachments_organization_server_message_path(organization, @server, @message.id), :alert => 'Attachment not found. Choose an attachment from the list below.'
-    end
-  end
-    
-      def update
-    @organization_obj = current_user.organizations_scope.find(organization.id)
-    if @organization_obj.update(params.require(:organization).permit(:name, :time_zone))
-      redirect_to_with_json organization_settings_path(@organization_obj), :notice => 'Settings for #{@organization_obj.name} have been saved successfully.'
-    else
-      render_form_errors 'edit', @organization_obj
-    end
-  end
-    
-      def toggle_ssl
-    @track_domain.update(:ssl_enabled => !@track_domain.ssl_enabled)
-    redirect_to_with_json [organization, @server, :track_domains], :notice => 'SSL settings for #{@track_domain.full_name} updated successfully.'
-  end
-    
-      def update
-    @organization_user = organization.user_assignment(@user)
-    if @organization_user.update(params.require(:organization_user).permit(:admin))
-      redirect_to_with_json [organization, :users], :notice => 'Permissions for #{@organization_user.user.name} have been updated successfully.'
-    else
-      render_form_errors 'edit', @organization_user
-    end
-  end
-    
-      def create
-    @webhook = @server.webhooks.build(safe_params)
-    if @webhook.save
-      redirect_to_with_json [organization, @server, :webhooks]
-    else
-      render_form_errors 'new', @webhook
-    end
-  end
