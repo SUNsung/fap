6ebccc151c53e53eb7e55029c4cc46f2c7852390
SUNsung <weatherius@gmail.com>
2019-07-24 12:43:25 +0300

    Rewrite [35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py] File in: 2019-07-24 12:43:24

--- pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
+++ pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
@@ -1,190 +1,272 @@
 
         
-            def __init__(self, employee_id, name):
-        super(Operator, self).__init__(employee_id, name, Rank.OPERATOR)
+        from django.contrib.auth.decorators import login_required, permission_required
+from django.core import mail
+from django.forms import fields
+from django.forms.forms import Form, ValidationError
+from django.forms.formsets import BaseFormSet, formset_factory
+from django.http import (
+    HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed,
+    HttpResponseNotFound, HttpResponseRedirect,
+)
+from django.shortcuts import render
+from django.template import Context, Template
+from django.test import Client
+from django.utils.decorators import method_decorator
     
+    from django.conf import settings
+from django.contrib.gis.db.backends.base.operations import (
+    BaseSpatialOperations,
+)
+from django.contrib.gis.db.backends.utils import SpatialOperator
+from django.contrib.gis.db.models import GeometryField, RasterField
+from django.contrib.gis.gdal import GDALRaster
+from django.contrib.gis.geos.geometry import GEOSGeometryBase
+from django.contrib.gis.geos.prototypes.io import wkb_r
+from django.contrib.gis.measure import Distance
+from django.core.exceptions import ImproperlyConfigured
+from django.db.backends.postgresql.operations import DatabaseOperations
+from django.db.models import Func, Value
+from django.db.utils import NotSupportedError, ProgrammingError
+from django.utils.functional import cached_property
+from django.utils.version import get_version_tuple
     
-class Cache(object):
+        def test_deconstruction(self):
+        index = HashIndex(fields=['title'], name='test_title_hash', fillfactor=80)
+        path, args, kwargs = index.deconstruct()
+        self.assertEqual(path, 'django.contrib.postgres.indexes.HashIndex')
+        self.assertEqual(args, ())
+        self.assertEqual(kwargs, {'fields': ['title'], 'name': 'test_title_hash', 'fillfactor': 80})
     
-        @cached_property
-    def initial(self):
-        data = self.form.get_initial_for_field(self.field, self.name)
-        # If this is an auto-generated default date, nix the microseconds for
-        # standardized handling. See #22502.
-        if (isinstance(data, (datetime.datetime, datetime.time)) and
-                not self.field.widget.supports_microseconds):
-            data = data.replace(microsecond=0)
-        return data
+        def setUp(self):
+        self._cwd = os.getcwd()
+        self.work_dir = tempfile.mkdtemp(prefix='i18n_')
+        # Resolve symlinks, if any, in test directory paths.
+        self.test_dir = os.path.realpath(os.path.join(self.work_dir, self.work_subdir))
+        copytree(os.path.join(source_code_dir, self.work_subdir), self.test_dir)
+        # Step out of the temporary working tree before removing it to avoid
+        # deletion problems on Windows. Cleanup actions registered with
+        # addCleanup() are called in reverse so preserve this ordering.
+        self.addCleanup(self._rmrf, self.test_dir)
+        self.addCleanup(os.chdir, self._cwd)
+        os.chdir(self.test_dir)
     
-        return (x_train, y_train), (x_test, y_test)
-
+        def test_multiple_dependencies(self):
+        raw = [
+            ('s1', ('s1_db', ['alpha'])),
+            ('s2', ('s2_db', ['bravo'])),
+            ('s3', ('s3_db', ['charlie'])),
+            ('s4', ('s4_db', ['delta'])),
+        ]
+        dependencies = {
+            'alpha': ['bravo', 'delta'],
+            'bravo': ['charlie'],
+            'delta': ['charlie'],
+        }
     
-        E.g. for use with categorical_crossentropy.
-    
-    # define two groups of layers: feature (convolutions) and classification (dense)
-feature_layers = [
-    Conv2D(filters, kernel_size,
-           padding='valid',
-           input_shape=input_shape),
-    Activation('relu'),
-    Conv2D(filters, kernel_size),
-    Activation('relu'),
-    MaxPooling2D(pool_size=pool_size),
-    Dropout(0.25),
-    Flatten(),
-]
-    
-        # Finalize headers.
-    headers = get_default_headers(args)
-    if base_headers:
-        headers.update(base_headers)
-    headers.update(args.headers)
-    headers = finalize_headers(headers)
-    
-            # Keyword arguments > stream.encoding > default utf8
-        if self.stdin_encoding is None:
-            self.stdin_encoding = getattr(
-                self.stdin, 'encoding', None) or 'utf8'
-        if self.stdout_encoding is None:
-            actual_stdout = self.stdout
-            if is_windows:
-                # noinspection PyUnresolvedReferences
-                from colorama import AnsiToWin32
-                if isinstance(self.stdout, AnsiToWin32):
-                    actual_stdout = self.stdout.wrapped
-            self.stdout_encoding = getattr(
-                actual_stdout, 'encoding', None) or 'utf8'
-    
-        def format_body(self, content, mime):
-        if is_valid_mime(mime):
-            for p in self.enabled_plugins:
-                content = p.format_body(content, mime)
-        return content
-
-    
-        # If both `auth_parse` and `prompt_password` are set to `True`,
-    # and the value of `-a` lacks the password part,
-    # then the user will be prompted to type the password in.
-    prompt_password = True
-    
-    import pytest
-    
-        def _close_server_sock_ignore_errors(self):
-        try:
-            self.server_sock.close()
-        except IOError:
-            pass
-    
-            # because special names such as Name.Class, Name.Function, etc.
-        # are not recognized as such later in the parsing, we choose them
-        # to look the same as ordinary variables.
-        Name:                      '#000000',        # class: 'n'
-        Name.Attribute:            '#c4a000',        # class: 'na' - to be revised
-        Name.Builtin:              '#004461',        # class: 'nb'
-        Name.Builtin.Pseudo:       '#3465a4',        # class: 'bp'
-        Name.Class:                '#000000',        # class: 'nc' - to be revised
-        Name.Constant:             '#000000',        # class: 'no' - to be revised
-        Name.Decorator:            '#888',           # class: 'nd' - to be revised
-        Name.Entity:               '#ce5c00',        # class: 'ni'
-        Name.Exception:            'bold #cc0000',   # class: 'ne'
-        Name.Function:             '#000000',        # class: 'nf'
-        Name.Property:             '#000000',        # class: 'py'
-        Name.Label:                '#f57900',        # class: 'nl'
-        Name.Namespace:            '#000000',        # class: 'nn' - to be revised
-        Name.Other:                '#000000',        # class: 'nx'
-        Name.Tag:                  'bold #004461',   # class: 'nt' - like a keyword
-        Name.Variable:             '#000000',        # class: 'nv' - to be revised
-        Name.Variable.Class:       '#000000',        # class: 'vc' - to be revised
-        Name.Variable.Global:      '#000000',        # class: 'vg' - to be revised
-        Name.Variable.Instance:    '#000000',        # class: 'vi' - to be revised
-    
-    elif is_py3:
-    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
-    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
-    from http import cookiejar as cookielib
-    from http.cookies import Morsel
-    from io import StringIO
-    from collections import OrderedDict
-    from collections.abc import Callable, Mapping, MutableMapping
+     The ds_driver keyword is used internally when a ctypes pointer
+ is passed in directly.
     
         @property
-    def unverifiable(self):
-        return self.is_unverifiable()
+    def width(self):
+        '''
+        Width (X axis) in pixels of the band.
+        '''
+        return capi.get_band_xsize(self._ptr)
     
-    # Custom sidebar templates, maps document names to template names.
-html_sidebars = {
-    'index': ['sidebarintro.html', 'sourcelink.html', 'searchbox.html', 'hacks.html'],
-    '**': [
-        'sidebarlogo.html',
-        'localtoc.html',
-        'relations.html',
-        'sourcelink.html',
-        'searchbox.html',
-        'hacks.html',
-    ],
-}
+            for stream in self.stream_types:
+            for i in vs:
+                if self.stream_to_bid[stream['id']] == i['bid']:
+                    video_links=i['fs'] #now in i['flvs'] not in i['fs']
+                    if not i['fs'][0]['l'].startswith('/'):
+                        tmp = getVrsEncodeCode(i['fs'][0]['l'])
+                        if tmp.endswith('mp4'):
+                             video_links = i['flvs']
+                    self.stream_urls[stream['id']] = video_links
+                    size = 0
+                    for l in video_links:
+                        size += l['b']
+                    self.streams[stream['id']] = {'container': stream['container'], 'video_profile': stream['video_profile'], 'size' : size}
+                    break
     
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
+    #----------------------------------------------------------------------
+def ckplayer_download(url, output_dir = '.', merge = False, info_only = False, is_xml = True, **kwargs):
+    if is_xml:  #URL is XML URL
+        try:
+            title = kwargs['title']
+        except:
+            title = ''
+        try:
+            headers = kwargs['headers']  #headers provided
+            ckinfo = get_content(url, headers = headers)
+        except NameError:
+            ckinfo = get_content(url)
+        
+        ckplayer_download_by_xml(ckinfo, output_dir, merge, 
+                                info_only, title = title)
     
-    # Split data in train set and test set
-n_samples = X.shape[0]
-X_train, y_train = X[:n_samples // 2], y[:n_samples // 2]
-X_test, y_test = X[n_samples // 2:], y[n_samples // 2:]
-print('test data sparsity: %f' % sparsity_ratio(X_test))
-    
-    
-def _get_git_revision():
-    try:
-        revision = subprocess.check_output(REVISION_CMD.split()).strip()
-    except (subprocess.CalledProcessError, OSError):
-        print('Failed to execute git to get revision')
-        return None
-    return revision.decode('utf-8')
-    
-        # TASK: Build a vectorizer / classifier pipeline that filters out tokens
-    # that are too rare or too frequent
-    pipeline = Pipeline([
-        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
-        ('clf', LinearSVC(C=1000)),
-    ])
-    
-    ######################################################################
-# Run the clustering and plot
-    
-    # This is a very simple benchmark to give you an idea of how fast each step of face recognition will run on your system.
-# Notice that face detection gets very slow at large image sizes. So you might consider running face detection on a
-# scaled down version of your image and then running face encodings on the the full size image.
-    
-    # Load the jpg file into a numpy array
-image = face_recognition.load_image_file('biden.jpg')
-    
-        if os.path.isdir(image_to_check):
-        if cpus == 1:
-            [test_image(image_file, model) for image_file in image_files_in_folder(image_to_check)]
-        else:
-            process_images_in_process_pool(image_files_in_folder(image_to_check), cpus, model)
-    else:
-        test_image(image_to_check, model)
-    
-            # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
-    
-    
-if __name__ == '__main__':
-    main()
+    site_info = 'CNTV.com'
+download = cntv_download
+download_playlist = playlist_not_supported('cntv')
 
     
-    import face_recognition
-from flask import Flask, jsonify, request, redirect
+    def dailymotion_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    '''Downloads Dailymotion videos by URL.
+    '''
     
-        pil_image.show()
+    __all__ = ['douban_download']
+    
+    	for video in tab.childNodes:
+		if re.search(contentid, video.attributes['link'].value):
+			url = video.attributes['flv'].value
+			break
+    
+    #----------------------------------------------------------------------
+def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    '''wrapper'''
+    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
+    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
+    hostname = urlparse(url).hostname
+    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
+        return False
+    upid = match1(url, r'.+/content/(\w+)')
+    
+            assert result['type'] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result['title'] == '192.168.1.100'
+        assert result['data'] == {
+            CONF_IP_ADDRESS: '192.168.1.100',
+            CONF_PASSWORD: 'password',
+            CONF_PORT: 8080,
+            CONF_SSL: True,
+            CONF_SCAN_INTERVAL: 60,
+        }
+    
+        All IHC devices have an associated IHC resource. IHCDevice handled the
+    registration of the IHC controller callback when the IHC resource changes.
+    Derived classes must implement the on_ihc_change method
+    '''
+    
+    
+async def test_if_not_fires_if_event_data_not_matches(hass, calls):
+    '''Test firing of event if no match.'''
+    assert await async_setup_component(hass, automation.DOMAIN, {
+        automation.DOMAIN: {
+            'trigger': {
+                'platform': 'event',
+                'event_type': 'test_event',
+                'event_data': {'some_attr': 'some_value'}
+            },
+            'action': {
+                'service': 'test.automation',
+            }
+        }
+    })
+    
+    
+async def test_if_not_fires_for_leave_on_zone_enter(hass, calls):
+    '''Test for not firing on zone enter.'''
+    hass.states.async_set('geo_location.entity', 'hello', {
+        'latitude': 32.881011,
+        'longitude': -117.234758,
+        'source': 'test_source'
+    })
+    await hass.async_block_till_done()
+    
+    
+async def test_if_fires_on_zone_enter(hass, calls):
+    '''Test for firing on zone enter.'''
+    context = Context()
+    hass.states.async_set('test.entity', 'hello', {
+        'latitude': 32.881011,
+        'longitude': -117.234758
+    })
+    await hass.async_block_till_done()
+    
+        assert len(calls) == 1
+    assert calls[0].domain == 'light'
+    assert calls[0].service == 'turn_on'
+    assert calls[0].data['entity_id'] == 'light.kitchen'
+    assert 'confidenceScore' not in calls[0].data
+    assert 'site_id' not in calls[0].data
+    
+    
+class DeleteGroupTest(TestCase):
+    def test_simple(self):
+        project = self.create_project()
+        group = self.create_group(
+            project=project,
+        )
+        event = self.create_event(group=group)
+        EventMapping.objects.create(
+            project_id=project.id,
+            event_id='a' * 32,
+            group_id=group.id,
+        )
+        EventAttachment.objects.create(
+            event_id=event.event_id,
+            group_id=event.group_id,
+            project_id=event.project_id,
+            file=File.objects.create(
+                name='hello.png',
+                type='image/png',
+            ),
+            name='hello.png',
+        )
+        UserReport.objects.create(
+            group_id=group.id,
+            project_id=event.project_id,
+            name='Jane Doe',
+        )
+        key = 'key'
+        value = 'value'
+        tk = tagstore.create_tag_key(
+            project_id=project.id,
+            environment_id=self.environment.id,
+            key=key
+        )
+        tv = tagstore.create_tag_value(
+            project_id=project.id,
+            environment_id=self.environment.id,
+            key=key,
+            value=value
+        )
+        tagstore.create_event_tags(
+            event_id=event.id,
+            group_id=group.id,
+            project_id=project.id,
+            environment_id=self.environment.id,
+            tags=[
+                (tk.key, tv.value),
+            ],
+        )
+        GroupAssignee.objects.create(
+            group=group,
+            project=project,
+            user=self.user,
+        )
+        GroupHash.objects.create(
+            project=project,
+            group=group,
+            hash=uuid4().hex,
+        )
+        GroupMeta.objects.create(
+            group=group,
+            key='foo',
+            value='bar',
+        )
+        GroupRedirect.objects.create(
+            group_id=group.id,
+            previous_group_id=1,
+        )
+    
+            # Adding unique constraint on 'TagKey', fields ['project_id', 'environment_id', 'key']
+        db.create_unique(u'tagstore_tagkey', ['project_id', 'environment_id', 'key'])
+    
+        if not resolution_list:
+        return
+    
+    from sentry.constants import VALID_PLATFORMS
+from sentry.models import Group, Project, ProjectPlatform
+from sentry.tasks.base import instrumented_task
