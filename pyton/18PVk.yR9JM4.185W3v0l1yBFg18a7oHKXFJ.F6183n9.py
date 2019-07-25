fb600f5707655c6bec2147692b3cef188f849462
SUNsung <weatherius@gmail.com>
2019-07-25 11:58:21 +0300

    Rewrite [18PVk.yR9JM4.185W3v0l1yBFg18a7oHKXFJ.F6183n9.py] File in: 2019-07-25 11:58:21

--- pyton/18PVk.yR9JM4.185W3v0l1yBFg18a7oHKXFJ.F6183n9.py
+++ pyton/18PVk.yR9JM4.185W3v0l1yBFg18a7oHKXFJ.F6183n9.py
@@ -1,28 +1,110 @@
 
         
-            # Generate canonical proposals from shifted anchors
-    # Enumerate all shifted positions on the (H, W) grid
-    fpn_max_size = cfg.FPN.COARSEST_STRIDE * np.ceil(
-        cfg.TRAIN.MAX_SIZE / float(cfg.FPN.COARSEST_STRIDE)
-    )
-    field_size = int(np.ceil(fpn_max_size / float(stride)))
-    shifts = np.arange(0, field_size) * stride
-    shift_x, shift_y = np.meshgrid(shifts, shifts)
-    shift_x = shift_x.ravel()
-    shift_y = shift_y.ravel()
-    shifts = np.vstack((shift_x, shift_y, shift_x, shift_y)).transpose()
+        
+try:
+    if int(pkg_resources.get_distribution('pip').version.split('.')[0]) < 6:
+        print('pip older than 6.0 not supported, please upgrade pip with:\n\n'
+              '    pip install -U pip')
+        sys.exit(-1)
+except pkg_resources.DistributionNotFound:
+    pass
     
+        def test_gin_index(self):
+        # Ensure the table is there and doesn't have an index.
+        self.assertNotIn('field', self.get_constraints(IntegerArrayModel._meta.db_table))
+        # Add the index
+        index_name = 'integer_array_model_field_gin'
+        index = GinIndex(fields=['field'], name=index_name)
+        with connection.schema_editor() as editor:
+            editor.add_index(IntegerArrayModel, index)
+        constraints = self.get_constraints(IntegerArrayModel._meta.db_table)
+        # Check gin index was added
+        self.assertEqual(constraints[index_name]['type'], GinIndex.suffix)
+        # Drop the index
+        with connection.schema_editor() as editor:
+            editor.remove_index(IntegerArrayModel, index)
+        self.assertNotIn(index_name, self.get_constraints(IntegerArrayModel._meta.db_table))
     
-def add_mask_rcnn_blobs(blobs, sampled_boxes, roidb, im_scale, batch_idx):
-    '''Add Mask R-CNN specific blobs to the input blob dictionary.'''
-    # Prepare the mask targets by associating one gt mask to each training roi
-    # that has a fg (non-bg) class label.
-    M = cfg.MRCNN.RESOLUTION
-    polys_gt_inds = np.where(
-        (roidb['gt_classes'] > 0) & (roidb['is_crowd'] == 0)
-    )[0]
-    polys_gt = [roidb['segms'][i] for i in polys_gt_inds]
-    boxes_from_polys = segm_utils.polys_to_boxes(polys_gt)
-    fg_inds = np.where(blobs['labels_int32'] > 0)[0]
-    roi_has_mask = blobs['labels_int32'].copy()
-    roi_has_mask[roi_has_mask > 0] = 1
+        Parameters
+    ----------
+    im : PIL image
+        Input image.
+    flatten : bool
+        If true, convert the output to grey-scale.
+    mode : str, optional
+        Mode to convert image to, e.g. ``'RGB'``.  See the Notes of the
+        `imread` docstring for more details.
+    
+        xx = range(0, n * step, step)
+    plt.figure('scikit-learn tree benchmark results')
+    plt.subplot(211)
+    plt.title('Learning with varying number of samples')
+    plt.plot(xx, scikit_classifier_results, 'g-', label='classification')
+    plt.plot(xx, scikit_regressor_results, 'r-', label='regression')
+    plt.legend(loc='upper left')
+    plt.xlabel('number of samples')
+    plt.ylabel('Time (s)')
+    
+    legend1_values_list = list(legend1.values())
+legend1_keys_list = list(legend1.keys())
+    
+    The data is generated with the ``make_checkerboard`` function, then
+shuffled and passed to the Spectral Biclustering algorithm. The rows
+and columns of the shuffled matrix are rearranged to show the
+biclusters found by the algorithm.
+    
+    for connectivity in (None, knn_graph):
+    for n_clusters in (30, 3):
+        plt.figure(figsize=(10, 4))
+        for index, linkage in enumerate(('average',
+                                         'complete',
+                                         'ward',
+                                         'single')):
+            plt.subplot(1, 4, index + 1)
+            model = AgglomerativeClustering(linkage=linkage,
+                                            connectivity=connectivity,
+                                            n_clusters=n_clusters)
+            t0 = time.time()
+            model.fit(X)
+            elapsed_time = time.time() - t0
+            plt.scatter(X[:, 0], X[:, 1], c=model.labels_,
+                        cmap=plt.cm.nipy_spectral)
+            plt.title('linkage=%s\n(time %.2fs)' % (linkage, elapsed_time),
+                      fontdict=dict(verticalalignment='top'))
+            plt.axis('equal')
+            plt.axis('off')
+    
+    iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+    
+    def clean_text(text, replacements = {':': '_', ' ': '_', '/': '_', '.': '', ''': ''}):
+    for key, rep in replacements.items():
+        text = text.replace(key, rep)
+    return text    
+    
+            # Adding unique constraint on 'GroupTagKey', fields ['project_id',
+        # 'group_id', 'environment_id', '_key']
+        db.create_unique(
+            u'tagstore_grouptagkey', [
+                'project_id', 'group_id', 'environment_id', 'key'])
+    
+            # Deleting model 'EventTag'
+        db.delete_table(u'tagstore_eventtag')
+    
+            # Adding model 'GroupTagValue'
+        db.create_table(u'tagstore_grouptagvalue', (
+            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
+            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
+            ('group_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
+            ('times_seen', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
+            ('_key', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['tagstore.TagKey'], db_column='key_id')),
+            ('_value', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['tagstore.TagValue'], db_column='value_id')),
+            ('last_seen', self.gf('django.db.models.fields.DateTimeField')(
+                null=True, db_index=True)),
+            ('first_seen', self.gf('django.db.models.fields.DateTimeField')(
+                null=True, db_index=True)),
+        ))
+        db.send_create_signal('tagstore', ['GroupTagValue'])
