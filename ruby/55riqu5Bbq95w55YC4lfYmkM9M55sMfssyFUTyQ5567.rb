c26b7c39f8ba298df655a23ab7e97c22add5a412
SUNsung <weatherius@gmail.com>
2019-07-29 06:37:36 +0300

    Rewrite [55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb] File in: 2019-07-29 06:37:36

--- ruby/55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb
+++ ruby/55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb
@@ -1,6 +1,42 @@
 
         
-            def unlock_instructions(record, token, opts={})
+                review_attachment_file = UploadFile.from_path(review_attachment_path)
+        review_attachment_data = client.upload_app_review_attachment(self, review_attachment_file)
+        raw_data['appReviewInfo']['attachmentFiles']['value'] = generate_review_attachment_file(review_attachment_data, review_attachment_path)
+      end
+    
+        def itc_stub_supported_countries
+      stub_request(:get, 'https://appstoreconnect.apple.com/WebObjects/iTunesConnect.woa/ra/apps/pricing/supportedCountries').
+        to_return(status: 200, body: itc_read_fixture_file(File.join('supported_countries.json')),
+                  headers: { 'Content-Type' => 'application/json' })
+    end
+    
+          def derived_data_path
+        Snapshot.cache[:derived_data_path] ||= (Snapshot.config[:derived_data_path] || Dir.mktmpdir('snapshot_derived'))
+      end
+    
+    module Snapshot
+  # Responsible for building the fully working xcodebuild command
+  # This TestCommandGenerator supports Xcode 8's `xcodebuild` requirements
+  # It is its own object, as the logic differs for how we want to handle
+  # creating `xcodebuild` commands for Xcode 9 (see test_command_generator.rb)
+  class TestCommandGeneratorXcode8 < TestCommandGeneratorBase
+    class << self
+      def generate(device_type: nil, language: nil, locale: nil)
+        parts = prefix
+        parts << 'xcodebuild'
+        parts += options(language, locale)
+        parts += destination(device_type)
+        parts += build_settings
+        parts += actions
+        parts += suffix
+        parts += pipe(device_type, language, locale)
+    
+          expect(fake_storage).to receive(:download)
+      allow(fake_storage).to receive(:working_directory).and_return('yolo_path')
+      allow(fake_storage).to receive(:keychain_name).and_return('https://github.com/fastlane/certs')
+    
+        def unlock_instructions(record, token, opts={})
       @token = token
       devise_mail(record, :unlock_instructions, opts)
     end
@@ -13,62 +49,112 @@
         cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
       end
     
-          def _devise_route_context
-        @_devise_route_context ||= send(Devise.available_router_name)
+          # Configure default email options
+      def devise_mail(record, action, opts = {}, &block)
+        initialize_from_record(record)
+        mail headers_for(action, opts), &block
       end
+    
+        def no_input_strategies
+      self.strategies & Devise::NO_INPUT
     end
+    
+      # Pipeline settings
+  option ['--pipeline.id'], 'ID',
+    I18n.t('logstash.runner.flag.pipeline-id'),
+    :attribute_name => 'pipeline.id',
+    :default => LogStash::SETTINGS.get_default('pipeline.id')
+    
+    require_relative '../lib/bootstrap/environment'
+    
+    require 'clamp'
+require 'pluginmanager/util'
+require 'pluginmanager/gemfile'
+require 'pluginmanager/install'
+require 'pluginmanager/remove'
+require 'pluginmanager/list'
+require 'pluginmanager/update'
+require 'pluginmanager/pack'
+require 'pluginmanager/unpack'
+require 'pluginmanager/generate'
+require 'pluginmanager/prepare_offline_pack'
+require 'pluginmanager/proxy_support'
+configure_proxy
+    
+          IGNORE_GEMS_IN_PACK.each do |gem_name|
+        packet_gem.ignore(gem_name)
+      end
+    
+        it 'returns a flare tag if there is a flare tag in the list' do
+      valid_article = create(:article, tags: 'ama')
+      expect(described_class.new(valid_article).tag.name).to eq('ama')
+    end
+    
+    RSpec.describe 'Blocks', type: :request do
+  let(:user) { create(:user, :super_admin) }
+    
+    RSpec.describe 'all routes', type: :routing do
+  let(:podcast)     { create(:podcast) }
+  let(:user)        { create(:user) }
+    
+      def self.send_to_buffer(text, buffer_profile_id_code)
+    client = Buffer::Client.new(ApplicationConfig['BUFFER_ACCESS_TOKEN'])
+    client.create_update(
+      body: {
+        text:
+        text,
+        profile_ids: [
+          buffer_profile_id_code,
+        ]
+      },
+    )
   end
+    
+    # Determine default value of a given easy_install's option
+def easy_install_default(python_bin, option)
+  result = nil
+  execmd({:PYTHONPATH=>'#{example_dir}'}, python_bin, :stderr=>false) do |stdin,stdout|
+    stdin.write('from easy_install_default import default_options\n' \
+                'print default_options.#{option}\n')
+    stdin.close
+    result = stdout.read.chomp
+  end
+  return result
 end
-
     
-            # Attempt to find a user by its email. If a record is found, send new
-        # password instructions to it. If user is not found, returns a new user
-        # with an email not found error.
-        # Attributes must contain the user's email
-        def send_reset_password_instructions(attributes={})
-          recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
-          recoverable.send_reset_password_instructions if recoverable.persisted?
-          recoverable
-        end
+      # This method removes excluded files from the staging_path. Subclasses can
+  # remove the files during the input phase rather than deleting them here
+  def exclude
+    return if attributes[:excludes].nil?
     
-            @id               = id
-        @local_data_path  = raw['local_data_path']
-        @name             = raw['name']
-        @provider         = raw['provider']
-        @state            = raw['state']
-        @vagrantfile_name = raw['vagrantfile_name']
-        @vagrantfile_path = raw['vagrantfile_path']
-        # TODO(mitchellh): parse into a proper datetime
-        @updated_at       = raw['updated_at']
-        @extra_data       = raw['extra_data'] || {}
-    
-            def initialize
-          # The action hooks hash defaults to []
-          @action_hooks = Hash.new { |h, k| h[k] = [] }
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
+      option '--zonetype', 'ZONETYPE',
+    'Set the allowed zone types (global, nonglobal, both)',
+    :default => 'value=global value=nonglobal' do |value|
+      case @value
+      when 'both'
+        value = 'value=global value=nonglobal'
+      else
+        value = 'value=#{value}'
       end
-      super
-    end
+    end # value
     
+        # Licenses could include more than one.
+    # Speaking of just taking the first entry of the field:
+    # A crude thing to do, but I suppose it's better than nothing.
+    # -- Daniel Haskin, 3/24/2015
+    self.license = control['license'][0] || self.license
     
+      def iteration
+    return @iteration ? @iteration : 1
+  end
     
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
-    end
+      def build!(params)
+    # TODO(sissel): Support these somehow, perhaps with execs and files.
+    self.scripts.each do |name, path|
+      case name
+        when 'pre-install'
+        when 'post-install'
+        when 'pre-uninstall'
+        when 'post-uninstall'
+      end # case name
+    end # self.scripts.each
