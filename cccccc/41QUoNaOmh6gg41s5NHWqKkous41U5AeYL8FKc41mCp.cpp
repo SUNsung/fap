5bc69f917a044889c5fe75d908584b6e50849649
SUNsung <weatherius@gmail.com>
2019-07-29 11:27:23 +0300

    Rewrite [41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp] File in: 2019-07-29 11:27:23

--- cccccc/41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp
+++ cccccc/41QUoNaOmh6gg41s5NHWqKkous41U5AeYL8FKc41mCp.cpp
@@ -1,184 +1,58 @@
 
         
-          const tflite::Tensor* tensor = tflite::Create1dTensor(100);
-  const flatbuffers::Vector<flatbuffers::Offset<tflite::Buffer>>* buffers =
-      tflite::CreateBuffers();
+        #include <stdint.h>
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+      // Returns a new iterator over the table contents.
+  // The result of NewIterator() is initially invalid (caller must
+  // call one of the Seek methods on the iterator before using it).
+  Iterator* NewIterator(const ReadOptions&) const;
     
-    struct OpData {
-  // IDs are the arbitrary identifiers used by TF Lite to identify and access
-  // memory buffers.
-  int fft_integer_working_area_id = kTensorNotAllocated;
-  int fft_double_working_area_id = kTensorNotAllocated;
-};
-    
-    #include 'tensorflow/core/framework/bounds_check.h'
-#include 'tensorflow/core/framework/op_kernel.h'
-#include 'tensorflow/core/framework/register_types.h'
-#include 'tensorflow/core/framework/tensor.h'
-#include 'tensorflow/core/lib/core/threadpool.h'
+      // Finish building the block and return a slice that refers to the
+  // block contents.  The returned slice will remain valid for the
+  // lifetime of this builder or until Reset() is called.
+  Slice Finish();
     
     
-    {  TF_CHECK_OK(GpuLaunchKernel(DynamicStitchKernel<T>, config.block_count,
-                              config.thread_per_block, 0, gpu_device.stream(),
-                              slice_size, output_size, input_indices,
-                              input_ptrs, output));
+    {  char* result = alloc_ptr_;
+  alloc_ptr_ += bytes;
+  alloc_bytes_remaining_ -= bytes;
+  return result;
 }
     
-    #include <gtest/gtest.h>
-#include 'tensorflow/lite/c/c_api_internal.h'
-#include 'tensorflow/lite/delegates/nnapi/nnapi_delegate.h'
-#include 'tensorflow/lite/model.h'
-#include 'tensorflow/lite/tools/evaluation/proto/evaluation_config.pb.h'
-#include 'tensorflow/lite/tools/evaluation/proto/evaluation_stages.pb.h'
-#include 'tensorflow/lite/tools/evaluation/utils.h'
     
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-      // Naive cost estimate based on the given operations count and total
-  // input/output tensor sizes of the given op_info combined.
-  Costs PredictOpCountBasedCost(double operations, const OpInfo& op_info) const;
-    
-    
-    {
-    {}  // namespace functor
-}  // namespace tensorflow
-    
-    TF_CALL_int32(REGISTER_GATHER_ND_GPU);
-TF_CALL_int64(REGISTER_GATHER_ND_GPU);
-TF_CALL_GPU_NUMBER_TYPES(REGISTER_GATHER_ND_GPU);
-TF_CALL_complex64(REGISTER_GATHER_ND_GPU);
-TF_CALL_complex128(REGISTER_GATHER_ND_GPU);
-    
-    net::NSSCertDatabase* GetNSSCertDatabaseForResourceContext(
-    content::ResourceContext* context,
-    base::OnceCallback<void(net::NSSCertDatabase*)> callback) {
-  // This initialization is not thread safe. This CHECK ensures that this code
-  // is only run on a single thread.
-  CHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
-  if (!g_nss_cert_database) {
-    // Linux has only a single persistent slot compared to ChromeOS's separate
-    // public and private slot.
-    // Redirect any slot usage to this persistent slot on Linux.
-    g_nss_cert_database = new net::NSSCertDatabase(
-        crypto::ScopedPK11Slot(
-            crypto::GetPersistentNSSKeySlot()) /* public slot */,
-        crypto::ScopedPK11Slot(
-            crypto::GetPersistentNSSKeySlot()) /* private slot */);
+    {  int cached_weight = 0;
+  for (int i = 0; i < index; i++) {
+    const int weight = (i & 1 ? kLight : kHeavy);
+    int r = Lookup(i);
+    if (r >= 0) {
+      cached_weight += weight;
+      ASSERT_EQ(1000 + i, r);
+    }
   }
-  return g_nss_cert_database;
+  ASSERT_LE(cached_weight, kCacheSize + kCacheSize / 10);
 }
     
-    v8::Local<v8::Value> Converter<const char*>::ToV8(v8::Isolate* isolate,
-                                                  const char* val) {
-  return v8::String::NewFromUtf8(isolate, val, v8::NewStringType::kNormal)
-      .ToLocalChecked();
+    namespace {
+    }
+    
+    BlockBuilder::BlockBuilder(const Options* options)
+    : options_(options), restarts_(), counter_(0), finished_(false) {
+  assert(options->block_restart_interval >= 1);
+  restarts_.push_back(0);  // First restart point is at offset 0
 }
     
-    
-    { private:
-  v8::Local<v8::Object> object_;
-};
-    
-     private:
-  ObjectTemplateBuilder& SetImpl(const base::StringPiece& name,
-                                 v8::Local<v8::Data> val);
-  ObjectTemplateBuilder& SetPropertyImpl(
-      const base::StringPiece& name,
-      v8::Local<v8::FunctionTemplate> getter,
-      v8::Local<v8::FunctionTemplate> setter);
-    
-      static Promise Create(v8::Isolate* isolate);
-  static Promise Create();
-    
-    
-    { private:
-  HANDLE mutex_;
-  DISALLOW_COPY_AND_ASSIGN(AutoUnlockMutex);
-};
-    
-            inps.getUMatVector(inputs);
-        outs.getUMatVector(outputs);
-    
-    
-    static CV_ALWAYS_INLINE
-    void integral_64_operations_avx512(const __m512i *srcs,
-                                       __m512i *sums,       const __m512i *sums_above,
-                                       __m512i *sqsums,     const __m512i *sqsums_above,
-                                       __mmask64 data_mask,
-                                       __m512i &sum_accumulator, __m512i &sqsum_accumulator)
-    {
-       __m512i src_64byte_chunk = read_64_bytes(srcs, data_mask);
+        TableBuilder* builder = new TableBuilder(options, file);
+    meta->smallest.DecodeFrom(iter->key());
+    for (; iter->Valid(); iter->Next()) {
+      Slice key = iter->key();
+      meta->largest.DecodeFrom(key);
+      builder->Add(key, iter->value());
     }
     
-        if (fps <= 0)
-    {
-        MSG(cerr << 'MFX: Invalid FPS passed to encoder' << endl);
-        return;
-    }
-    
-    
-    {        netExec = plugin.LoadNetwork(net, {});
-        infRequest = netExec.CreateInferRequest();
-    }
-    catch (const std::exception& ex)
-    {
-        CV_Error(Error::StsAssert, format('Failed to initialize Inference Engine backend: %s', ex.what()));
-    }
-    
-        auto groupedString = groupedStream.str();
-    wstring result(groupedString.rbegin(), groupedString.rend());
-    // Add the right (fractional or exponential) part of the number to the final string.
-    if (hasDecimal)
-    {
-        result += displayString.substr(dec);
-    }
-    else if (hasExponent)
-    {
-        result += displayString.substr(exp);
-    }
-    
-    CurrencyHttpClient::CurrencyHttpClient()
-    : m_client(ref new HttpClient())
-    , m_responseLanguage(L'en-US')
-{
-}
-    
-    /****************************Module*Header***********************************
-* Module Name: EngineStrings.h
-*
-* Module Description:
-*       Resource String ID's for the private strings used by Engine. Internal to Engine related code
-*         not required by the clients
-*
-* Warnings:
-*
-* Created: 13-Feb-2008
-*
-\****************************************************************************/
-    
-        // Set up all the post rounding stuff.
-    bool useSciForm = false;
-    int32_t eout = exponent - 1; // Displayed exponent.
-    MANTTYPE* pmant = pnum->mant + pnum->cdigit - 1;
-    // Case where too many digits are to the left of the decimal or
-    // FMT_SCIENTIFIC or FMT_ENGINEERING was specified.
-    if ((format == FMT_SCIENTIFIC) || (format == FMT_ENGINEERING))
-    {
-        useSciForm = true;
-        if (eout != 0)
-        {
-            if (format == FMT_ENGINEERING)
-            {
-                exponent = (eout % 3);
-                eout -= exponent;
-                exponent++;
-    }
-    }
-    }
-    
-    #include 'pch.h'
-#include 'HorizontalNoOverflowStackPanel.h'
+    // Build a Table file from the contents of *iter.  The generated file
+// will be named according to meta->number.  On success, the rest of
+// *meta will be filled with metadata about the generated table.
+// If no data is present in *iter, meta->file_size will be set to
+// zero, and no Table file will be produced.
+Status BuildTable(const std::string& dbname, Env* env, const Options& options,
+                  TableCache* table_cache, Iterator* iter, FileMetaData* meta);
