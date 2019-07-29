5fc64e212df1dda2ed855b7ef3353e69819bc503
SUNsung <weatherius@gmail.com>
2019-07-29 21:46:03 +0300

    Rewrite [35W81UAsXkBv.35L6AQAF.AfQQ35a6f7sGmHPXU35Ui1.py] File in: 2019-07-29 21:46:03

--- pyton/35W81UAsXkBv.35L6AQAF.AfQQ35a6f7sGmHPXU35Ui1.py
+++ pyton/35W81UAsXkBv.35L6AQAF.AfQQ35a6f7sGmHPXU35Ui1.py
@@ -1,28 +1,111 @@
 
         
-        from uuid import uuid4
+                info = self._extract_theplatform_metadata(tp_path, content_id)
+        info.update({
+            'id': content_id,
+            'title': title,
+            'series': xpath_text(video_data, 'seriesTitle'),
+            'season_number': int_or_none(xpath_text(video_data, 'seasonNumber')),
+            'episode_number': int_or_none(xpath_text(video_data, 'episodeNumber')),
+            'duration': int_or_none(xpath_text(video_data, 'videoLength'), 1000),
+            'thumbnail': xpath_text(video_data, 'previewImageURL'),
+            'formats': formats,
+            'subtitles': subtitles,
+        })
+        return info
     
-            for platform, project_id in queryset:
-            platform = platform.lower()
-            if platform not in VALID_PLATFORMS:
-                continue
-            ProjectPlatform.objects.create_or_update(
-                project_id=project_id,
-                platform=platform,
-                values={
-                    'last_seen': now,
-                },
-            )
-        min_project_id += step
+        bug_text = re.search(
+        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
+    dev_text = re.search(
+        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING YOUTUBE-DL',
+        readme).group(1)
     
-        This **does not** guarantee that the correct value is written into the cache
-    though it will correct itself in the next update window.
-    '''
-    cutoff_dt = timezone.now() - timedelta(seconds=cutoff)
-    # TODO(dcramer): this doesnt handle deleted options (which shouldn't be allowed)
-    for option in Option.objects.filter(last_updated__gte=cutoff_dt).iterator():
-        try:
-            opt = default_manager.lookup_key(option.key)
-            default_manager.store.set_cache(opt, option.value)
-        except UnknownOption as e:
-            logger.exception(six.text_type(e))
+    
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
+    
+        with io.open(outfile, 'w', encoding='utf-8') as outf:
+        outf.write(out)
+    
+        if os.path.exists('tldr.pdf'):
+        print('\nCreated tldr.pdf in the current directory!\n')
+    
+    
+    
+        # load weights
+    if weights == 'imagenet':
+        print('K.image_dim_ordering:', K.image_dim_ordering())
+        if K.image_dim_ordering() == 'th':
+            if include_top:
+                weights_path = get_file('resnet50_weights_th_dim_ordering_th_kernels.h5',
+                                        TH_WEIGHTS_PATH,
+                                        cache_subdir='models')
+            else:
+                weights_path = get_file('resnet50_weights_th_dim_ordering_th_kernels_notop.h5',
+                                        TH_WEIGHTS_PATH_NO_TOP,
+                                        cache_subdir='models')
+            model.load_weights(weights_path)
+            if K.backend() == 'tensorflow':
+                warnings.warn('You are using the TensorFlow backend, yet you '
+                              'are using the Theano '
+                              'image dimension ordering convention '
+                              '(`image_dim_ordering='th'`). '
+                              'For best performance, set '
+                              '`image_dim_ordering='tf'` in '
+                              'your Keras config '
+                              'at ~/.keras/keras.json.')
+                convert_all_kernels_in_model(model)
+        else:
+            if include_top:
+                weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels.h5',
+                                        TF_WEIGHTS_PATH,
+                                        cache_subdir='models')
+            else:
+                weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                                        TF_WEIGHTS_PATH_NO_TOP,
+                                        cache_subdir='models')
+            model.load_weights(weights_path)
+            if K.backend() == 'theano':
+                convert_all_kernels_in_model(model)
+    return model
+    
+    
+class LogisticRegression(object):
+    '''Multi-class Logistic Regression Class
+    
+        for t in range(sample_len - 1):
+        x[t + 1] = x[t] + sigma * (y[t] - x[t]) * step
+        y[t + 1] = y[t] + (x[t] * (rho - z[t]) - y[t]) * step
+        z[t + 1] = z[t] + (x[t] * y[t] - beta * z[t]) * step
+    
+    
+if __name__ == '__main__':
+  testutils.main()
+
+    
+      def testCollectorGetsWantedWidgets(self):
+    col = collector.Collector()
+    self.assertEqual(len(col.collect_widgets()), 10)
+    
+      def testSetParseFnsNamedDefaultsFromFire(self):
+    # Fire should use the decorator to know how to parse string arguments.
+    self.assertEqual(core.Fire(WithDefaults, command=['example2']), (10, int))
+    self.assertEqual(core.Fire(WithDefaults, command=['example2', '10']),
+                     (10, float))
+    self.assertEqual(core.Fire(WithDefaults, command=['example2', '13']),
+                     (13, float))
+    self.assertEqual(core.Fire(WithDefaults, command=['example2', '14.0']),
+                     (14, float))
+    
+      def testNoPrivateMethods(self):
+    self.assertTrue(hasattr(fire, 'Fire'))
+    self.assertFalse(hasattr(fire, '_Fire'))
+    
+      @settings(max_examples=1000, deadline=1000)
+  @given(st.text(min_size=1))
+  @example('This is a one-line docstring.')
+  def test_fuzz_parse(self, value):
+    docstrings.parse(value)
