54c9ff6b4b1d1882b781ff1879b810c9acd933ea
SUNsung <weatherius@gmail.com>
2019-07-29 12:31:49 +0300

    Rewrite [351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb] File in: 2019-07-29 12:31:49

--- ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
+++ ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
@@ -1,218 +1,122 @@
 
         
-              it 'adds docset_min_xcode_version param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            docset_min_xcode_version: '6.4'
-          )
-        end').runner.execute(:test)
+          # Disable automatic flushing of the log to improve performance.
+  # config.autoflush_log = false
     
-    module CrossplatformShellwords
-  # handle switching between implementations of shellescape
-  def shellescape(str)
-    if FastlaneCore::Helper.windows?
-      WindowsShellwords.shellescape(str)
-    else
-      # using `escape` instead of expected `shellescape` here
-      # which corresponds to Shellword's `String.shellescape` implementation
-      # https://github.com/ruby/ruby/blob/1cf2bb4b2085758112503e7da7414d1ef52d4f48/lib/shellwords.rb#L216
-      Shellwords.escape(str)
-    end
-  end
-  module_function :shellescape
+      # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = false
     
-    puts('[WARNING] You are calling #{tool_name} directly. Usage of the tool name without the `fastlane` prefix is deprecated in fastlane 2.0'.yellow)
-puts('Please update your scripts to use `fastlane #{tool_name} #{full_params}` instead.'.yellow)
+          # @return [Xcodeproj::Project] The Pods Xcode project.
+      #
+      attr_reader :pods_project
     
-          it 'renders HTML' do
-        expect(render(options, screenshots)).to match(/<html>/)
-      end
-    end
-    
-            # Sends a kerberos request, and reads the response through the connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] The kerberos message
-        # @raise [RuntimeError] if the transport protocol is unknown or the response can't be parsed.
-        # @raise [NotImplementedError] if the transport protocol isn't supported
-        def send_recv(req)
-          send_request(req)
-          res = recv_response
-    
-            end
-      end
-    end
-  end
-end
-
-    
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
+              # Generates the contents of the xcconfig file and saves it to disk.
           #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
-    
-      # Berkeley DB's ndbm.h (since 1.85 at least) defines DBM_SUFFIX.
-  # Note that _DB_H_ is not defined on Mac OS X because
-  # it uses Berkeley DB 1 but ndbm.h doesn't include db.h.
-  have_db_header = have_macro('DBM_SUFFIX', hdr, hsearch)
-    
-    $objs = [ 'sha2init.#{$OBJEXT}' ]
-    
-    opt_o = nil
-opt.def_option('-o FILE', 'specify output file') {|filename|
-  opt_o = filename
-}
-    
-    module Fiddle
-  if WINDOWS
-    # Returns the last win32 +Error+ of the current executing +Thread+ or nil
-    # if none
-    def self.win32_last_error
-      Thread.current[:__FIDDLE_WIN32_LAST_ERROR__]
-    end
-    
-        # Allocates a C union the +types+ provided.
-    #
-    # When the instance is garbage collected, the C function +func+ is called.
-    def CUnionEntity.malloc(types, func=nil)
-      addr = Fiddle.malloc(CUnionEntity.size(types))
-      CUnionEntity.new(addr, types, func)
-    end
-    
-      def safely_remove_file(_path)
-    run_vagrant_command('rm #{test_file}')
-  rescue
-    VagrantHelpers::VagrantSSHCommandError
-  end
-end
-    
-          def remember_location(key)
-        location = caller.find do |line|
-          IGNORED_LOCATIONS.none? { |i| line.include?(i) }
-        end
-        (locations[key] ||= []) << location
-      end
-    
-    LogStash::Bundler.setup!
-    
-      it 'records when the config was read' do
-    expect(subject.read_at).to be <= Time.now
-  end
-    
-          context 'when the plugin doesnt exist' do
-        it 'fails to install and report an error' do
-          command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-output-impossible-plugin')
-          expect(command.stderr).to match(/Plugin not found, aborting/)
-        end
-      end
-    end
-  end
-end
-
-    
-        context 'without a specific plugin' do
-      it 'display a list of plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list')
-        expect(result.stdout.split('\n').size).to be > 1
-      end
-    
-        after :each do
-      logstash.uninstall
-    end
-    
-    
-# This is a non obvious hack,
-# EllipticalCurve are not completely implemented in JRuby 9k and the new version of SSH from the standard library
-# use them.
-#
-# Details: https://github.com/jruby/jruby-openssl/issues/105
-Net::SSH::Transport::Algorithms::ALGORITHMS.values.each { |algs| algs.reject! { |a| a =~ /^ecd(sa|h)-sha2/ } }
-Net::SSH::KnownHosts::SUPPORTED_TYPE.reject! { |t| t =~ /^ecd(sa|h)-sha2/ }
-    
-      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-=end
-    
-          def footer
-        if @footer.nil?
-          if page = @page.footer
-            @footer = page.text_data
-          else
-            @footer = false
+          # @param  [PBXNativeTarget] native_target
+          #         the native target to link the module map file into.
+          #
+          # @return [void]
+          #
+          def create_xcconfig_file(native_target)
+            native_target.build_configurations.each do |configuration|
+              next unless target.user_build_configurations.key?(configuration.name)
+              path = target.xcconfig_path(configuration.name)
+              build_settings = target.build_settings(configuration.name)
+              update_changed_file(build_settings, path)
+              target.xcconfigs[configuration.name] = build_settings.xcconfig
+              xcconfig_file_ref = add_file_to_support_group(path)
+              configuration.base_configuration_reference = xcconfig_file_ref
+            end
           end
+    
+            it 'allows specifying linkage' do
+          BuildType.new(:linkage => :dynamic).should == BuildType.dynamic_library
         end
-        @footer
+    
+          # Creates a default launchscreen storyboard.
+      #
+      # @param  [Project] project
+      #         the Xcodeproj to generate the launchscreen storyboard into.
+      #
+      # @param  [PBXNativeTarget] target
+      #         the native target to link the generated launchscreen storyboard into.
+      #
+      # @param  [Symbol] platform
+      #         the platform of the target. Can be `:ios` or `:osx`, etc.
+      #
+      # @param  [String] deployment_target
+      #         the deployment target for the platform.
+      #
+      # @param  [String] name
+      #         The name to use for the target, defaults to 'App'.
+      #
+      # @return [PBXFileReference] the created file reference of the launchscreen storyboard.
+      #
+      def self.add_launchscreen_storyboard(project, target, group, deployment_target, name = 'App')
+        launch_storyboard_file = AppTargetHelper.create_launchscreen_storyboard_file(project, deployment_target, name)
+        launch_storyboard_ref = group.new_file(launch_storyboard_file)
+        target.resources_build_phase.add_file_reference(launch_storyboard_ref)
       end
     
-      # replace name version and date
-  replace_header(head, :name)
-  replace_header(head, :version)
-  replace_header(head, :date)
-  #comment this out if your rubyforge_project has a different name
-  replace_header(head, :rubyforge_project)
+          private
     
-      begin
-    require 'gollum-lib'
-    wiki = Gollum::Wiki.new(gollum_path, wiki_options)
-    if !wiki.exist? then
-      raise Gollum::InvalidGitRepositoryError
-    end
-    if wiki_options[:plantuml_url]
-      Gollum::Filter::PlantUML.configure do |config|
-        puts 'Using #{wiki_options[:plantuml_url]} as PlantUML endpoint'
-        config.url = wiki_options[:plantuml_url]
+            it 'cleans up linked libraries and frameworks from the frameworks build phase' do
+          @pod_bundle.stubs(:build_type => Target::BuildType.dynamic_framework)
+          @target_integrator.integrate!
+          target = @target_integrator.send(:native_targets).first
+          phase = target.frameworks_build_phase
+          phase.files.find { |f| f.file_ref.path == 'Pods.framework' }.should.not.be.nil
+          phase.files.find { |f| f.file_ref.path == 'Pods-Something.framework' }.should.be.nil
+          @pod_bundle.stubs(:product_name => 'Pods-Something.framework')
+          @pod_bundle.stubs(:product_basename => 'Pods-Something')
+          @target_integrator.integrate!
+          phase.files.find { |f| f.file_ref.path == 'Pods.framework' }.should.be.nil
+          phase.files.find { |f| f.file_ref.path == 'Pods-Something.framework' }.should.not.be.nil
+        end
+    
+          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
+      def left_shift int, shift
+        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
+        # 1>>31, 2**32
+        (r & 2147483648) == 0 ? r : r - 4294967296
       end
-    end
-    puts
-    puts 'Loaded Gollum wiki at:'
-    puts '#{File.expand_path(gollum_path).inspect}'
-    puts
-    puts 'Example API calls:'
-    puts %(    page = wiki.page('page-name'))
-    puts %(    # => <Gollum::Page>)
-    puts
-    puts %(    page.raw_data)
-    puts %(    # => '# My wiki page')
-    puts
-    puts %(    page.formatted_data)
-    puts %(    # => '<h1>My wiki page</h1>')
-    puts
-    puts 'Full API documentation at:'
-    puts 'https://github.com/gollum/gollum-lib'
-    puts
-    IRB.start_session(binding)
-  rescue Gollum::InvalidGitRepositoryError, Gollum::NoSuchPathError
-    puts 'Invalid Gollum wiki at #{File.expand_path(gollum_path).inspect}'
-    exit 0
+    
+          def toc_content
+        @toc_content
+      end
+    
+      test 'extract destination file name in case of path renaming' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
+    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
   end
-else
-  require 'gollum/app'
-  Precious::App.set(:gollum_path, gollum_path)
-  Precious::App.set(:wiki_options, wiki_options)
-  Precious::App.settings.mustache[:templates] = wiki_options[:template_dir] if wiki_options[:template_dir]
     
-    # The project root directory
-$root = ::File.dirname(__FILE__)
+      s.name              = 'gollum'
+  s.version           = '4.1.4'
+  s.date              = '2018-10-01'
+  s.rubyforge_project = 'gollum'
+  s.license           = 'MIT'
     
-      class Blockquote < Liquid::Block
-    FullCiteWithTitle = /(\S.*)\s+(https?:\/\/)(\S+)\s+(.+)/i
-    FullCite = /(\S.*)\s+(https?:\/\/)(\S+)/i
-    AuthorTitle = /([^,]+),([^,]+)/
-    Author =  /(.+)/
+        it 'rejects chat channel invitation' do
+      membership = ChatChannelMembership.last
+      sign_in second_user
+      put '/chat_channel_memberships/#{membership.id}', params: {
+        chat_channel_membership: { user_action: 'reject' }
+      }
+      expect(ChatChannelMembership.find(membership.id).status).to eq('rejected')
+    end
+    
+          it 'renders second and third users if present' do
+        user2 = create(:user)
+        user3 = create(:user)
+        article.update(second_user_id: user2.id, third_user_id: user3.id)
+        get article.path
+        expect(response.body).to include '<em>with <b><a href=\'#{user2.path}\'>'
+      end
+    
+      context 'when redirected routes' do
+    include RSpec::Rails::RequestExampleGroup
+    
+      describe '#render' do
+    it 'renders the template' do
+      expect(File).to receive(:read).at_least(:once) { 'wemux ls 2>/dev/null' }
