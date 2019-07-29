c6f833b7e5518e3b363f4b5014e3bb01ba467e57
SUNsung <weatherius@gmail.com>
2019-07-29 22:53:56 +0300

    Rewrite [22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb] File in: 2019-07-29 22:53:56

--- ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
+++ ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
@@ -1,82 +1,190 @@
 
         
-            # Get the implementation class for the given Converter.
-    # Returns the Converter instance implementing the given Converter.
-    # klass - The Class of the Converter to fetch.
-    def find_converter_instance(klass)
-      @find_converter_instance ||= {}
-      @find_converter_instance[klass] ||= begin
-        converters.find { |converter| converter.instance_of?(klass) } || \
-          raise('No Converters found for #{klass}')
-      end
+          def test_numeric_fields_with_scale
+    m = NumericData.new(
+      bank_balance: 1586.43122334,
+      big_bank_balance: BigDecimal('234000567.952344'),
+      world_population: 2**62,
+      my_house_population: 3
+    )
+    assert m.save
+    
+        %w(SPATIAL FULLTEXT UNIQUE).each do |type|
+      expected = 'CREATE #{type} INDEX `index_people_on_last_name`  ON `people` (`last_name`) '
+      assert_equal expected, add_index(:people, :last_name, type: type)
     end
     
-        context 'include tag with variable and liquid filters' do
-      setup do
-        site = fixture_site('pygments' => true).tap(&:read).tap(&:render)
-        post = site.posts.docs.find do |p|
-          p.basename.eql? '2013-12-17-include-variable-filters.markdown'
+      # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+    
+            def ref
+          @symbol
         end
-        @content = post.output
-      end
+        alias to_sym ref
     
-      p.action do |args, _|
-    if args.empty?
-      Jekyll.logger.error 'A subcommand is required.'
-      puts p
-      abort
-    else
-      subcommand = args.first
-      unless p.has_command? subcommand
-        Jekyll.logger.abort_with 'fatal: 'jekyll #{args.first}' could not' \
-          ' be found. You may need to install the jekyll-#{args.first} gem' \
-          ' or a related gem to be able to use this subcommand.'
-      end
+        def purge_tombstone(grace_period)
     end
-  end
-end
-
     
-              theme.create!
-          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
-                             ' is ready for you in #{theme.path.to_s.cyan}!'
-          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
+          if message_template == :email_reject_post_too_short
+        template_args[:count] = SiteSetting.min_post_length
+      end
+    
+      def update_user_counts
+    return if @opts[:import_mode]
+    
+        if request.put?
+      params[:reviewable_priorities].each do |id, priority|
+        if !priority.nil? && Reviewable.priorities.has_value?(priority.to_i)
+          # For now, the score bonus is equal to the priority. In the future we might want
+          # to calculate it a different way.
+          PostActionType.where(id: id).update_all(
+            reviewable_priority: priority.to_i,
+            score_bonus: priority.to_f
+          )
         end
-        # rubocop:enable Metrics/AbcSize
       end
     end
-  end
-end
-
     
-      def send_reset_password_instructions
-    Workers::ResetPassword.perform_async(self.id)
+      protected
+    
+          path_name = url_method(params.slice(:category, :parent_category))
+      canonical_url '#{Discourse.base_url_no_prefix}#{public_send(path_name, *(params.slice(:parent_category, :category, :tag_id).values.map { |t| t.force_encoding('UTF-8') }))}'
+    
+    module Gitlab
+  module CryptoHelper
+    extend self
+    
+      describe '#can_resend_invite?' do
+    context 'when group_member is invited' do
+      before do
+        expect(group_member).to receive(:invite?).and_return(true)
+      end
+    
+            def diff_stats_collection
+          strong_memoize(:diff_stats) do
+            # There are scenarios where we don't need to request Diff Stats,
+            # when caching for instance.
+            next unless @include_stats
+            next unless diff_refs
+    
+      def serialize_options(resource)
+    methods = resource_class.authentication_keys.dup
+    methods = methods.keys if methods.is_a?(Hash)
+    methods << :password if resource.respond_to?(:password)
+    { methods: methods, only: [:password] }
   end
     
-          def render_error(error_description, detailed_error=nil)
-        @error_description = error_description
-        @detailed_error = detailed_error
-        if request.format == :mobile
-          render 'api/openid_connect/error/error.mobile', layout: 'application.mobile'
+      # Helper for use after calling send_*_instructions methods on a resource.
+  # If we are in paranoid mode, we always act as if the resource was valid
+  # and instructions were sent.
+  def successfully_sent?(resource)
+    notice = if Devise.paranoid
+      resource.errors.clear
+      :send_paranoid_instructions
+    elsif resource.errors.empty?
+      :send_instructions
+    end
+    
+                bypass_sign_in(user)
+          DEPRECATION
+          warden.session_serializer.store(resource, scope)
+        elsif warden.user(scope) == resource && !options.delete(:force)
+          # Do nothing. User already signed in and we are not forcing it.
+          true
         else
-          render 'api/openid_connect/error/error', layout: 'with_header_with_footer'
+          warden.set_user(resource, options.merge!(scope: scope))
         end
       end
+    
+    version = ['', 'ext/etc/'].find do |dir|
+  begin
+    break File.open(File.expand_path('../#{dir}/etc.c', __FILE__)) do |f|
+      f.gets '\n#define RUBY_ETC_VERSION '
+      f.gets[/'(.+)'/, 1]
+    end
+  rescue
+    next
+  end
+end
+    
+      def test_untainted_numeric
+    bug8945 = '[ruby-core:57346] [Bug #8945] Numerics never be tainted'
+    b = RbConfig::LIMITS['FIXNUM_MAX'] + 1
+    tainted = [0, 1.0, 1.72723e-77, b].select do |x|
+      Marshal.load(Marshal.dump(x).taint).tainted?
+    end
+    assert_empty(tainted.map {|x| [x, x.class]}, bug8945)
+  end
+    
+      describe 'in the middle of the stream' do
+    before :each do
+      @gz = Zlib::GzipReader.new(@io)
+      @gz.read 5
+    end
+    
+          it 'makes eof? false' do
+        @gz.ungetc 'ŷ'
+        @gz.eof?.should be_false
+      end
+    end
+    
+        @inflator.inflate(@zeros) do |chunk|
+      @chunks << chunk
+      break
+    end
+    
+    Then(/^directories in :linked_dirs are created in shared$/) do
+  TestApp.linked_dirs.each do |dir|
+    run_vagrant_command(test_dir_exists(TestApp.shared_path.join(dir)))
+  end
+end
+    
+      context 'when redirected routes' do
+    include RSpec::Rails::RequestExampleGroup
+    
+      def user_is_author?
+    if record.instance_of?(Article)
+      record.user_id == user.id
+    else
+      record.pluck(:user_id).uniq == [user.id]
     end
   end
-end
-
     
-        redirect_to stream_path
+          exit!('Project #{existing} doesn't exist!') \
+        unless Tmuxinator::Config.exists?(name: existing)
+    
+              after(:all) do
+            puts @session
+            Kernel.system 'tmux kill-session -t #{@session}'
+          end
+    
+        def build_panes(panes_yml)
+      return if panes_yml.nil?
+    
+        allow(window).to receive(:project).and_return project
+    allow(window).to receive(:index).and_return 0
   end
-end
-
     
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
+      describe '::parse_settings' do
+    let(:args) { ['one', 'two=three'] }
     
-      def like_service
-    @like_service ||= LikeService.new(current_user)
-  end
-end
+        def rvm?
+      yaml['rvm']
+    end
+    
+      describe '#render' do
+    it 'renders the template' do
+      expect(File).to receive(:read).at_least(:once) { 'wemux ls 2>/dev/null' }
+    
+          def xdg?
+        File.directory?(xdg)
+      end
+    
+          it 'returns the flattened command' do
+        expect(window.commands).to eq [
+          'tmux send-keys -t test:1 git\\ fetch C-m',
+          'tmux send-keys -t test:1 git\\ status C-m'
+        ]
+      end
+    end
