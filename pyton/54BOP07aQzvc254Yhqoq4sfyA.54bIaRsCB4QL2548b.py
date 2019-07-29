12551ada8bdd1b0b127ed5abade1d52a1b975d02
SUNsung <weatherius@gmail.com>
2019-07-30 00:37:09 +0300

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2019-07-30 00:37:08

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,195 +1,133 @@
 
         
-            def _colision_resolution(self, key, data=None):
-        new_key = self.hash_function(key + 1)
+            # Sorting the libraries
+    inner_blocks = sorted(blocks[0].split('##'))
+    for i in range(1 , len(inner_blocks)):
+        if inner_blocks[i][0] != '#':
+            inner_blocks[i] = '##' + inner_blocks[i]
+    inner_blocks=''.join(inner_blocks)
     
-            if self.depth == 1:
-            self.prediction = np.mean(y)
-            return
+        if args.invalidation_mode:
+        ivl_mode = args.invalidation_mode.replace('-', '_').upper()
+        invalidation_mode = py_compile.PycInvalidationMode[ivl_mode]
+    else:
+        invalidation_mode = None
     
-        difference = predict - actual
-    numerator = np.sum(difference) / len(predict) 
-    denumerator =  np.sum(actual) / len(predict)
-    print(numerator)
-    print(denumerator)
+        pl[nsstr('anEmptyList')] = NSMutableArray.alloc().init()
     
-    if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
-
+        def test_imul(self):
+        u = self.type2test([0, 1])
+        u *= 3
+        self.assertEqual(u, self.type2test([0, 1, 0, 1, 0, 1]))
+        u *= 0
+        self.assertEqual(u, self.type2test([]))
     
-    if __name__ == '__main__':
-    main()
-
+            # Test that sequences of unhashable objects can be tested for sameness:
+        self.assertCountEqual([[1, 2], [3, 4], 0], [False, [3, 4], [1, 2]])
+        # Test that iterator of unhashable objects can be tested for sameness:
+        self.assertCountEqual(iter([1, 2, [], 3, 4]),
+                              iter([1, 2, [], 3, 4]))
     
-            # builtin classmethod:
-        self.assertEqual(dict.fromkeys.__qualname__, 'dict.fromkeys')
-        self.assertEqual(float.__getformat__.__qualname__,
-                         'float.__getformat__')
+            t = threading.Thread(target=f)
+        t.start()
+        t.join()
     
-    REVERSE_NAME_MAPPING.update({
-    ('_functools', 'reduce'): ('__builtin__', 'reduce'),
-    ('tkinter.filedialog', 'FileDialog'): ('FileDialog', 'FileDialog'),
-    ('tkinter.filedialog', 'LoadFileDialog'): ('FileDialog', 'LoadFileDialog'),
-    ('tkinter.filedialog', 'SaveFileDialog'): ('FileDialog', 'SaveFileDialog'),
-    ('tkinter.simpledialog', 'SimpleDialog'): ('SimpleDialog', 'SimpleDialog'),
-    ('xmlrpc.server', 'ServerHTMLDoc'): ('DocXMLRPCServer', 'ServerHTMLDoc'),
-    ('xmlrpc.server', 'XMLRPCDocGenerator'):
-        ('DocXMLRPCServer', 'XMLRPCDocGenerator'),
-    ('xmlrpc.server', 'DocXMLRPCRequestHandler'):
-        ('DocXMLRPCServer', 'DocXMLRPCRequestHandler'),
-    ('xmlrpc.server', 'DocXMLRPCServer'):
-        ('DocXMLRPCServer', 'DocXMLRPCServer'),
-    ('xmlrpc.server', 'DocCGIXMLRPCRequestHandler'):
-        ('DocXMLRPCServer', 'DocCGIXMLRPCRequestHandler'),
-    ('http.server', 'SimpleHTTPRequestHandler'):
-        ('SimpleHTTPServer', 'SimpleHTTPRequestHandler'),
-    ('http.server', 'CGIHTTPRequestHandler'):
-        ('CGIHTTPServer', 'CGIHTTPRequestHandler'),
-    ('_socket', 'socket'): ('socket', '_socketobject'),
-})
-    
-            self._copy_file_without_generated_symbols(SYMBOL_FILE, output)
-    
-    
-def open_binary(package: Package, resource: Resource) -> BinaryIO:
-    '''Return a file-like object opened for binary reading of the resource.'''
-    resource = _normalize_path(resource)
-    package = _get_package(package)
-    reader = _get_resource_reader(package)
-    if reader is not None:
-        return reader.open_resource(resource)
-    _check_location(package)
-    absolute_package_path = os.path.abspath(package.__spec__.origin)
-    package_path = os.path.dirname(absolute_package_path)
-    full_path = os.path.join(package_path, resource)
-    try:
-        return open(full_path, mode='rb')
-    except OSError:
-        # Just assume the loader is a resource loader; all the relevant
-        # importlib.machinery loaders are and an AttributeError for
-        # get_data() will make it clear what is needed from the loader.
-        loader = cast(ResourceLoader, package.__spec__.loader)
-        data = None
-        if hasattr(package.__spec__.loader, 'get_data'):
-            with suppress(OSError):
-                data = loader.get_data(full_path)
-        if data is None:
-            package_name = package.__spec__.name
-            message = '{!r} resource not found in {!r}'.format(
-                resource, package_name)
-            raise FileNotFoundError(message)
-        else:
-            return BytesIO(data)
-    
-    
-class DBUnpickler(pickle.Unpickler):
-    
-    # Add the html version.  This converts the message into a multipart/alternative
-# container, with the original text message as the first part and the new html
-# message as the second part.
-asparagus_cid = make_msgid()
-msg.add_alternative('''\
-<html>
-  <head></head>
-  <body>
-    <p>Salut!</p>
-    <p>Cela ressemble à un excellent
-        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
-            recipie
-        </a> déjeuner.
-    </p>
-    <img src='cid:{asparagus_cid}' />
-  </body>
-</html>
-'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
-# note that we needed to peel the <> off the msgid for use in the html.
-    
-    from argparse import ArgumentParser
-    
-    '''Unpack a MIME message into a directory of files.'''
-    
-    def handleSlideTitle(title):
-    print('<h2>%s</h2>' % getText(title.childNodes))
-    
-    # Function to return the operator module
-def get_operator_module():
-    return operator
-    
-            try:
-            print(list(pool.imap(f, list(range(10)))))
-        except ZeroDivisionError:
-            print('\tGot ZeroDivisionError as expected from list(pool.imap())')
-        else:
-            raise AssertionError('expected ZeroDivisionError')
-    
-            # Adding index on 'EventTag', fields ['environment_id', 'key', 'value']
-        db.create_index(u'tagstore_eventtag', ['environment_id', 'key_id', 'value_id'])
-    
-            # Adding unique constraint on 'EventTag', fields ['event_id', 'key', 'value']
-        db.create_unique(u'tagstore_eventtag', ['event_id', 'key_id', 'value_id'])
-    
-        class Meta:
-        app_label = 'tagstore'
-        unique_together = (('project_id', 'event_id', 'key', 'value'), )
-        index_together = (
-            ('project_id', 'key', 'value'),
-            ('group_id', 'key', 'value'),
+            firstId = events[0][2]
+        self.assertSequenceEqual(
+            [
+                ('Created', ' ', firstId),
+                ('cpython._PySys_ClearAuditHooks', ' ', firstId),
+            ],
+            events,
         )
     
-        for resolution in resolution_list:
-        try:
-            activity = Activity.objects.filter(
-                group=resolution.group_id,
-                type=Activity.SET_RESOLVED_IN_RELEASE,
-                ident=resolution.id,
-            ).order_by('-datetime')[0]
-        except IndexError:
-            continue
+        def run_ignoring_vars(self, predicate, **env_vars):
+        # Runs a subprocess with -E set, even though we're passing
+        # specific environment variables
+        # Logical inversion to match predicate check to a zero return
+        # code indicating success
+        code = 'import sys; sys.stderr.write(str(sys.flags)); sys.exit(not ({}))'.format(predicate)
+        return assert_python_ok('-E', '-c', code, **env_vars)
     
-        try:
-        with lock.acquire():
-            buffer.process_pending(partition=partition)
-    except UnableToAcquireLock as error:
-        logger.warning('process_pending.fail', extra={'error': error, 'partition': partition})
+          # We want to ensure that headers appear in the right order:
+      # 1) for foo.cc, foo.h  (preferred location)
+      # 2) c system files
+      # 3) cpp system files
+      # 4) for foo.cc, foo.h  (deprecated location)
+      # 5) other google headers
+      #
+      # We classify each include statement as one of those 5 types
+      # using a number of techniques. The include_state object keeps
+      # track of the highest type seen, and complains if we see a
+      # lower type after that.
+      error_message = include_state.CheckNextIncludeOrder(
+          _ClassifyInclude(fileinfo, include, is_system))
+      if error_message:
+        error(filename, linenum, 'build/include_order', 4,
+              '%s. Should be: %s.h, c system, c++ system, other.' %
+              (error_message, fileinfo.BaseName()))
+      canonical_include = include_state.CanonicalizeAlphabeticalOrder(include)
+      if not include_state.IsInAlphabeticalOrder(
+          clean_lines, linenum, canonical_include):
+        error(filename, linenum, 'build/include_alpha', 4,
+              'Include '%s' not in alphabetical order' % include)
+      include_state.SetLastHeader(canonical_include)
     
-        def proxy_callable(self, call_name, *args, **kwargs):
-        self.log.info('%s %s <- %s',
-                      self.obj_name,
-                      call_name,
-                      format_call(args, kwargs))
+    import os
+import sys
+from itertools import chain, groupby
+from collections import OrderedDict
     
-        def test_single_path_from_env(self):
-        with mock.patch.dict(os.environ):
-            os.environ['COMPOSE_FILE'] = 'one.yml'
-            environment = Environment.from_env_file('.')
-            assert get_config_path_from_options('.', {}, environment) == ['one.yml']
+        def _freeze(self, final_text, err=False):
+        '''Stop spinner, compose last frame and 'freeze' it.'''
+        if not final_text:
+            final_text = ''
+        target = self.stderr if err else self.stdout
+        if target.closed:
+            target = sys.stderr if err else sys.stdout
+        text = to_text(final_text)
+        last_frame = self._compose_out(text, mode='last')
+        self._last_frame = decode_output(last_frame, target_stream=target)
     
     
-class UnquotePathTest(unittest.TestCase):
-    def test_no_quotes(self):
-        assert unquote_path('hello') == 'hello'
+def test__error_2():
+    v = Validator(schema={'foo': {'keysrules': {'type': 'integer'}}})
+    v.document = {'foo': {'0': 'bar'}}
+    v._error('foo', errors.KEYSRULES, ())
+    error = v._errors[0]
+    assert error.document_path == ('foo',)
+    assert error.schema_path == ('foo', 'keysrules')
+    assert error.code == 0x83
+    assert error.rule == 'keysrules'
+    assert error.constraint == {'type': 'integer'}
+    assert error.value == {'0': 'bar'}
+    assert error.info == ((),)
+    assert error.is_group_error
+    assert not error.is_logic_error
     
+        class MyValidator(Validator):
+        def _validate_root_doc(self, root_doc, field, value):
+            ''' {'type': 'boolean'} '''
+            if 'sub' not in self.root_document or len(self.root_document['sub']) != 2:
+                self._error(field, 'self.context is not the root doc!')
     
-def test_minute_as_float():
-    assert timeparse.timeparse('1.5m') == 90
+            :param fields: The fields to send as a list of (fieldname, value)
+                       tuples.
+        :param files: The files to send as a list of (fieldname, filename,
+                      file_bytes) tuple.
+        '''
+        # Adapted from packaging, which in turn was adapted from
+        # http://code.activestate.com/recipes/146306
     
-    if __name__ == '__main__':
-    setup(name='wtfpython',
-          version='0.2',
-          description='What the f*ck Python!',
-          author='Satwik Kansal',
-          maintainer='Satwik Kansal',
-          maintainer_email='satwikkansal@gmail.com',
-          url='https://github.com/satwikkansal/wtfpython',
-          platforms='any',
-          license='WTFPL 2.0',
-          long_description='An interesting collection of subtle & tricky Python Snippets'
-                           ' and features.',
-          keywords='wtfpython gotchas snippets tricky',
-          packages=find_packages(),
-          entry_points = {
-              'console_scripts': ['wtfpython = wtf_python.main:load_and_read']
-          },
-          classifiers=[
-              'Development Status :: 4 - Beta',
+        def _env_setup(self, initial_qpos):
+        for name, value in initial_qpos.items():
+            self.sim.data.set_joint_qpos(name, value)
+        utils.reset_mocap_welds(self.sim)
+        self.sim.forward()
+    
+        With inspiration from:
+    https://github.com/dennybritz/reinforcement-learning/blob/master/lib/envs/cliff_walking.py
+    
+            self.action_space = spaces.Box(low=np.array([-self.bounds]), high=np.array([self.bounds]),
+                                       dtype=np.float32)
+        self.observation_space = spaces.Discrete(4)
