f19a4a1638adb154748f448f738797d08f492e94
SUNsung <weatherius@gmail.com>
2019-07-30 07:20:17 +0300

    Rewrite [005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb] File in: 2019-07-30 07:20:16

--- ruby/005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb
+++ ruby/005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb
@@ -1,81 +1,215 @@
 
         
-              def test_invert_remove_foreign_key_with_primary_key_and_to_table_in_options
-        enable = @recorder.inverse_of :remove_foreign_key, [:dogs, to_table: :people, primary_key: 'uuid']
-        assert_equal [:add_foreign_key, [:dogs, :people, primary_key: 'uuid']], enable
+              def test_invert_remove_foreign_key_with_on_delete_on_update
+        enable = @recorder.inverse_of :remove_foreign_key, [:dogs, :people, on_delete: :nullify, on_update: :cascade]
+        assert_equal [:add_foreign_key, [:dogs, :people, on_delete: :nullify, on_update: :cascade]], enable
       end
     
-            def ==(type)
-          @symbol == type.to_sym unless type.blank?
+        %w(SPATIAL FULLTEXT UNIQUE).each do |type|
+      expected = 'ALTER TABLE `people` ADD #{type} INDEX `index_people_on_last_name`  (`last_name`)'
+      assert_sql(expected) do
+        ActiveRecord::Base.connection.change_table(:people, bulk: true) do |t|
+          t.index :last_name, type: type
         end
       end
+    end
     
-          def destroy
-        authorization = Api::OpenidConnect::Authorization.find_by(id: params[:id])
-        if authorization
-          authorization.destroy
-        else
-          flash[:error] = I18n.t('api.openid_connect.authorizations.destroy.fail', id: params[:id])
-        end
-        redirect_to api_openid_connect_user_applications_url
+          def self.delegate_to(klass)
+        self.type_klass = klass
       end
     
-      respond_to :html,
-             :js,
-             :json
+          def get_device_type_with_simctl(device_names)
+        return device_names if Helper.test?
     
-      respond_to :html, :mobile, :json
+              raise 'Device name must be 50 characters or less. \'#{name}\' has a #{name.length} character length.' if name.length > 50
     
-        opts = params.require(:conversation).permit(:subject)
-    opts[:participant_ids] = person_ids
-    opts[:message] = { text: params[:conversation][:text] }
-    @conversation = current_user.build_conversation(opts)
+            # Find target
+        target = targets.find do |t|
+          t.name == target_name
+        end
+        UI.user_error!('Cannot find target named '#{target_name}'') unless target
     
-      def create
-    emails = inviter_params[:emails].split(',').map(&:strip).uniq
+          def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :workspace,
+                                       env_name: 'IPA_WORKSPACE',
+                                       description: 'WORKSPACE Workspace (.xcworkspace) file to use to build app (automatically detected in current directory)',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :project,
+                                       env_name: 'IPA_PROJECT',
+                                       description: 'Project (.xcodeproj) file to use to build app (automatically detected in current directory, overridden by --workspace option, if passed)',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :configuration,
+                                       env_name: 'IPA_CONFIGURATION',
+                                       description: 'Configuration used to build',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :scheme,
+                                       env_name: 'IPA_SCHEME',
+                                       description: 'Scheme used to build app',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :clean,
+                                       env_name: 'IPA_CLEAN',
+                                       description: 'Clean project before building',
+                                       optional: true,
+                                       is_string: false),
+          FastlaneCore::ConfigItem.new(key: :archive,
+                                       env_name: 'IPA_ARCHIVE',
+                                       description: 'Archive project after building',
+                                       optional: true,
+                                       is_string: false),
+          FastlaneCore::ConfigItem.new(key: :destination,
+                                       env_name: 'IPA_DESTINATION',
+                                       description: 'Build destination. Defaults to current directory',
+                                       default_value_dynamic: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :embed,
+                                       env_name: 'IPA_EMBED',
+                                       description: 'Sign .ipa file with .mobileprovision',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :identity,
+                                       env_name: 'IPA_IDENTITY',
+                                       description: 'Identity to be used along with --embed',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :sdk,
+                                       env_name: 'IPA_SDK',
+                                       description: 'Use SDK as the name or path of the base SDK when building the project',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :ipa,
+                                       env_name: 'IPA_IPA',
+                                       description: 'Specify the name of the .ipa file to generate (including file extension)',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :xcconfig,
+                                       env_name: 'IPA_XCCONFIG',
+                                       description: 'Use an extra XCCONFIG file to build the app',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :xcargs,
+                                       env_name: 'IPA_XCARGS',
+                                       description: 'Pass additional arguments to xcodebuild when building the app. Be sure to quote multiple args',
+                                       optional: true,
+                                       type: :shell_string)
+        ]
+      end
     
-      def test_file_exists(path)
-    exists?('f', path)
+      it 'asks to accept conflicts when the scenario was modified' do
+    DefaultScenarioImporter.seed(user)
+    agent = user.agents.where(name: 'Rain Notifier').first
+    agent.options['expected_receive_period_in_days'] = 9001
+    agent.save!
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
+    click_on 'Start Import'
+    expect(page).to have_text('This Scenario already exists in your system.')
+    expect(page).to have_text('9001')
+    check('I confirm that I want to import these Agents.')
+    click_on 'Finish Import'
+    expect(page).to have_text('Import successful!')
   end
     
-          # Try to add the gems to the current gemfile and lock file, if successful
-      # both of them will be updated. This injector is similar to Bundler's own injector class
-      # minus the support for additionals source and doing local resolution only.
-      ::Bundler::LogstashInjector.inject!(pack)
+          def check
+        create_event :payload => {}
+      end
+    end
     
-      # retrieve only the latest spec for all locally installed plugins
-  # @return [Hash] result hash {plugin_name.downcase => plugin_spec}
-  def find_latest_gem_specs
-    LogStash::PluginManager.all_installed_plugins_gem_specs(gemfile).inject({}) do |result, spec|
-      previous = result[spec.name.downcase]
-      result[spec.name.downcase] = previous ? [previous, spec].max_by{|s| s.version} : spec
-      result
+      describe '.seed' do
+    it 'imports a set of agents to get the user going when they are first created' do
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+    end
+    
+      describe '#working?' do
+    it 'should not be working until the first event was received' do
+      expect(@checker).not_to be_working
+      @checker.last_receive_at = Time.now
+      expect(@checker).to be_working
+    end
+  end
+    
+      describe '#chef_installed' do
+    let(:version) { '15.0.0' }
+    let(:command) { 'test -x /opt/chef_solo/bin/knife&& /opt/chef_solo/bin/knife --version | grep '15.0.0'' }
+    
+    # Configure VAGRANT_CWD so that the tests never find an actual
+# Vagrantfile anywhere, or at least this minimizes those chances.
+ENV['VAGRANT_CWD'] = VAGRANT_TEST_CWD
+    
+      it 'provides the provider' do
+    expect(subject.provider).to eq(provider)
+  end
+    
+        # Returns whether or not we know how to install the provider with
+    # the given name.
+    #
+    # @return [Boolean]
+    def can_install_provider?(name)
+      host.capability?(provider_install_key(name))
+    end
+    
+      describe '#machine_index' do
+    it 'returns a machine index' do
+      expect(subject.machine_index).to be_kind_of(Vagrant::MachineIndex)
+    end
+    
+    require 'log4r'
+    
+              triggers = filter_triggers(triggers, guest_name, type)
+    
+      let(:triggers) { VagrantPlugins::Kernel_V2::TriggerConfig.new }
+  let(:hash_block) { {info: 'hi', run: {inline: 'echo 'hi''}} }
+  let(:hash_block_two) { {warn: 'WARNING!!', run_remote: {inline: 'echo 'hi''}} }
+    
+        context 'when verify_host_key is true' do
+      let(:ssh_info) {{
+        host: 'localhost',
+        port: 2222,
+        username: 'vagrant',
+        private_key_path: [private_key_path],
+        verify_host_key: true
+      }}
+    
+          it 'ensures recipient is not following sender' do
+        expect(recipient.following?(sender)).to be false
+      end
+    end
+  end
+    
+        map = map.sort { |a, b| a[0].size <=> b[0].size }.to_h
+    
+        it 'removes the following relation between user and target user' do
+      expect(user.account.following?(other_account)).to be false
+    end
+    
+        it 'renders the headers' do
+      expect(mail.subject).to eq('bob favourited your status')
+      expect(mail.to).to eq([receiver.email])
+    end
+    
+      it 'does not notify when sender is muted with hide_notifications' do
+    recipient.mute!(sender, notifications: true)
+    is_expected.to_not change(Notification, :count)
+  end
+    
+        it 'generates the contacts_json fixture', :fixture => true do
+      json = bob.contacts.map { |c|
+               ContactPresenter.new(c, bob).full_hash_with_person
+             }.to_json
+      save_fixture(json, 'contacts_json')
     end
   end
 end
 
     
-        before do
-      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
-      # Logstash won't update when we have a pinned version in the gemfile so we remove them
-      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
-      expect(logstash).to have_installed?(plugin_name, previous_version)
-    end
+      # fetch data
+  fields = {
+    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
+    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
+    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
+  }
     
-    require 'minitest/autorun'
-    
-        it 'wraps the inlined worker with middleware' do
-      Sidekiq::Testing.server_middleware do |chain|
-        chain.add AttributeMiddleware
+          def report(env)
+        warn env, 'attack reported by #{self.class}'
+        env[options[:report_key]] = true
       end
     
-      describe 'delay' do
-    require 'action_mailer'
-    class InlineFooMailer < ActionMailer::Base
-      def bar(str)
-        raise InlineError
-      end
-    end
-    
-    require_relative '../lib/sidekiq/cli'
-require_relative '../lib/sidekiq/launcher'
+      it 'accepts post requests with masked X-CSRF-Token header' do
+    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => masked_token)
+    expect(last_response).to be_ok
+  end
