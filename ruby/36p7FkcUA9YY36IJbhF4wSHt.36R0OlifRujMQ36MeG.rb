288495941b77d45c687ab24e6a0adfa69fd84ac8
SUNsung <weatherius@gmail.com>
2019-07-30 07:53:15 +0300

    Rewrite [36p7FkcUA9YY36IJbhF4wSHt.36R0OlifRujMQ36MeG.rb] File in: 2019-07-30 07:53:14

--- ruby/36p7FkcUA9YY36IJbhF4wSHt.36R0OlifRujMQ36MeG.rb
+++ ruby/36p7FkcUA9YY36IJbhF4wSHt.36R0OlifRujMQ36MeG.rb
@@ -1,85 +1,190 @@
 
         
-            assert_equal 'my_c1_m', @c1_m.c_function
+              migration2 = RemoveIndexMigration2.new
+      migration2.migrate(:up)
+      assert_not migration2.connection.index_exists?(*index_definition)
+    
+        assert_not_equal @previously_updated_at, @developer.updated_at
   end
     
-        assert_equal @a, loaded
+        config.action_mailbox.queues = ActiveSupport::InheritableOptions.new \
+      incineration: :action_mailbox_incineration, routing: :action_mailbox_routing
     
-        assert_nil encoding
-    
-        json = File.open('js/search_index.js.gz', 'rb') {|gzip|
-      Zlib::GzipReader.new(gzip).read
-    }
-    
-      def test_generate
-    @generator.generate
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
+      if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
+    
+        def find_app(apple_id: nil, app_identifier: nil)
+      if app_identifier
+        app = Spaceship::ConnectAPI::App.find(app_identifier)
+        UI.user_error!('Could not find an app by #{app_identifier}') unless app
+        return app
+      end
+    
+              has_reader = instance_methods.include?(reader)
+          has_writer = instance_methods.include?(writer)
+    
+            resps = Spaceship::ConnectAPI.get_beta_app_localizations(filter: filter, includes: includes, limit: limit, sort: sort).all_pages
+        return resps.map(&:to_models).flatten
+      end
+    
+          def self.type
+        return 'bundleIds'
+      end
+    
+      describe '.find' do
+    it 'finds a template in the Foo category' do
+      create_template!('test-template')
+    
+        def sha256(value)
+      salt = Settings.attr_encrypted_db_key_base_truncated
+      ::Digest::SHA256.base64digest('#{value}#{salt}')
+    end
+    
+      def chronic_duration_attributes
+    @chronic_duration_attributes ||= {}
+  end
+    
+      #
+  # Returns the connection oriented general handler type, in this case bind.
+  #
+  def self.general_handler_type
+    'bind'
+  end
+    
+        zip = Rex::Zip::Archive.new
+    zip.add_file('/http/' + exe_filename, exe)
+    zip.add_file(contact_full_name + '.contact', xml)
+    zip.save_to(contact_full_name + '.zip')
+    print_good('Created '#{contact_full_name}.zip'')
   end
 end
     
-        it 'marks all notifications as read' do
-      request.env['HTTP_REFERER'] = 'I wish I were spelled right'
-      FactoryGirl.create(:notification, recipient: alice, target: post)
-      FactoryGirl.create(:notification, recipient: alice, target: post)
+    =begin
+ +------+----------------+-------------------------------------------+
+   | HEX  | NAME           | DESCRIPTION                               |
+   +------+----------------+-------------------------------------------+
+   | HEX  | NAME           | DESCRIPTION                               |
+   | 0x01 | CALLED NUMBER  | Number/extension being called             |
+   | 0x02 | CALLING NUMBER | Calling number                            |
+   | 0x03 | CALLING ANI    | Calling number ANI for billing            |
+   | 0x04 | CALLING NAME   | Name of caller                            |
+   | 0x05 | CALLED CONTEXT | Context for number                        |
+   | 0x06 | USERNAME       | Username (peer or user) for               |
+   |      |                | authentication                            |
+   | 0x07 | PASSWORD       | Password for authentication               |
+   | 0x08 | CAPABILITY     | Actual CODEC capability                   |
+   | 0x09 | FORMAT         | Desired CODEC format                      |
+   | 0x0a | LANGUAGE       | Desired language                          |
+   | 0x0b | VERSION        | Protocol version                          |
+   | 0x0c | ADSICPE        | CPE ADSI capability                       |
+   | 0x0d | DNID           | Originally dialed DNID                    |
+   | 0x0e | AUTHMETHODS    | Authentication method(s)                  |
+   | 0x0f | CHALLENGE      | Challenge data for MD5/RSA                |
+   | 0x10 | MD5 RESULT     | MD5 challenge result                      |
+   | 0x11 | RSA RESULT     | RSA challenge result                      |
+   | 0x12 | APPARENT ADDR  | Apparent address of peer                  |
+   | 0x13 | REFRESH        | When to refresh registration              |
+   | 0x14 | DPSTATUS       | Dialplan status                           |
+   | 0x15 | CALLNO         | Call number of peer                       |
+   | 0x16 | CAUSE          | Cause                                     |
+   | 0x17 | IAX UNKNOWN    | Unknown IAX command                       |
+   | 0x18 | MSGCOUNT       | How many messages waiting                 |
+   | 0x19 | AUTOANSWER     | Request auto-answering                    |
+   | 0x1a | MUSICONHOLD    | Request musiconhold with QUELCH           |
+   | 0x1b | TRANSFERID     | Transfer Request Identifier               |
+   | 0x1c | RDNIS          | Referring DNIS                            |
+   | 0x1d | Reserved       | Reserved for future use                   |
+   | 0x1e | Reserved       | Reserved for future use                   |
+   | 0x1f | DATETIME       | Date/Time                                 |
+   | 0x20 | Reserved       | Reserved for future use                   |
+   | 0x21 | Reserved       | Reserved for future use                   |
+   | 0x22 | Reserved       | Reserved for future use                   |
+   | 0x23 | Reserved       | Reserved for future use                   |
+   | 0x24 | Reserved       | Reserved for future use                   |
+   | 0x25 | Reserved       | Reserved for future use                   |
+   | 0x26 | CALLINGPRES    | Calling presentation                      |
+   | 0x27 | CALLINGTON     | Calling type of number                    |
+   | 0x28 | CALLINGTNS     | Calling transit network select            |
+   | 0x29 | SAMPLINGRATE   | Supported sampling rates                  |
+   | 0x2a | CAUSECODE      | Hangup cause                              |
+   | 0x2b | ENCRYPTION     | Encryption format                         |
+   | 0x2c | ENCKEY         | Reserved for future Use                   |
+   | 0x2d | CODEC PREFS    | CODEC Negotiation                         |
+   | 0x2e | RR JITTER      | Received jitter, as in RFC 3550           |
+   | 0x2f | RR LOSS        | Received loss, as in RFC 3550             |
+   | 0x30 | RR PKTS        | Received frames                           |
+   | 0x31 | RR DELAY       | Max playout delay for received frames in  |
+   |      |                | ms                                        |
+   | 0x32 | RR DROPPED     | Dropped frames (presumably by jitter      |
+   |      |                | buffer)                                   |
+   | 0x33 | RR OOO         | Frames received Out of Order              |
+   | 0x34 | OSPTOKEN       | OSP Token Block                           |
+   +------+----------------+-------------------------------------------+
+=end
     
-        describe 'overview' do
-      it 'has a heading' do
-        assert_includes output, '---- Overview ----'
+                encoded
+          end
+    
+              # Encodes the renew_time field
+          #
+          # @return [String]
+          def encode_renew_time
+            [renew_till].pack('N')
+          end
+    
+              # Encodes the type
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_type(type)
+            bn = OpenSSL::BN.new(type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+    namespace :bower do
+    
+      describe '#body' do
+    context 'with a single expression body' do
+      let(:source) do
+        'module Foo; bar; end'
       end
     
-        it 'can retry a retry' do
-      add_retry
-      r = Sidekiq::RetrySet.new
-      assert_equal 1, r.size
-      r.first.retry
-      assert_equal 0, r.size
-      assert_equal 1, Sidekiq::Queue.new('default').size
-      job = Sidekiq::Queue.new('default').first
-      assert_equal 'bob', job.jid
-      assert_equal 1, job['retry_count']
-    end
-    
-            begin
-          Component.new.handle_exception exception
-          pass
-        rescue StandardError
-          flunk 'failed handling a nil backtrace'
+            def correct_for_blockarg_type(node)
+          lambda do |corrector|
+            range = range_with_surrounding_space(range: node.source_range,
+                                                 side: :left)
+            range = range_with_surrounding_comma(range, :left)
+            corrector.remove(range)
+          end
         end
       end
     end
   end
+end
+
     
-        class MyStopper
-      def call(worker_class, job, queue, r)
-        yield if job['args'].first.odd?
-      end
-    end
+        describe 'invalid' do
+      context 'at the beginning of a sequence' do
+        let(:pattern) { '(<(str $_) (sym $_)> ...)' }
     
-          assert SomeScheduledWorker.perform_in(1.month, 'mike')
-      job = ss.first
-      assert job['created_at']
-      refute job['enqueued_at']
+          it { expect(if_node.elsif?).to be_falsey }
     end
   end
     
-      describe 'delay' do
-    require 'action_mailer'
-    class InlineFooMailer < ActionMailer::Base
-      def bar(str)
-        raise InlineError
-      end
-    end
+      s.post_install_message = %q{
+    __________________________________________________________
+    ..........................................................
+    }
     
-    begin
-  #RubyProf::exclude_threads = [ Monitoring ]
-  #RubyProf.start
-  fire_event(:startup)
-  #Sidekiq.logger.error 'Simulating 1ms of latency between Sidekiq and redis'
-  #Toxiproxy[:redis].downstream(:latency, latency: 1).apply do
-    launcher = Sidekiq::Launcher.new(Sidekiq.options)
-    launcher.run
+    describe Tmuxinator::Pane do
+  let(:klass) { described_class }
+  let(:instance) { klass.new(index, project, window, *commands) }
+  # let(:index) { 'vim' }
+  # let(:project) { 0 }
+  # let(:tab) { nil }
+  # let(:commands) { nil }
+  let(:index) { 0 }
+  let(:project) { double }
+  let(:window) { double }
+  let(:commands) { ['vim', 'bash'] }
