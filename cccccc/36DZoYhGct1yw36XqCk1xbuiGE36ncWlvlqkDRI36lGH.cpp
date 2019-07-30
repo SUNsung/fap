858a97ded3193aeb4306437700e3ebce2a0333bb
SUNsung <weatherius@gmail.com>
2019-07-30 16:28:43 +0300

    Rewrite [36DZoYhGct1yw36XqCk1xbuiGE36ncWlvlqkDRI36lGH.cpp] File in: 2019-07-30 16:28:42

--- cccccc/36DZoYhGct1yw36XqCk1xbuiGE36ncWlvlqkDRI36lGH.cpp
+++ cccccc/36DZoYhGct1yw36XqCk1xbuiGE36ncWlvlqkDRI36lGH.cpp
@@ -1,213 +1,258 @@
 
         
-        #include <QDialog>
+            ratio = loader.GetCurrencyRatioEquality(usdUnit, fakeUnit1);
     
-    
-static void secp256k1_gej_add_ge(secp256k1_gej *r, const secp256k1_gej *a, const secp256k1_ge *b) {
-    /* Operations: 7 mul, 5 sqr, 4 normalize, 21 mul_int/add/negate/cmov */
-    static const secp256k1_fe fe_1 = SECP256K1_FE_CONST(0, 0, 0, 0, 0, 0, 0, 1);
-    secp256k1_fe zz, u1, u2, s1, s2, t, tt, m, n, q, rr;
-    secp256k1_fe m_alt, rr_alt;
-    int infinity, degenerate;
-    VERIFY_CHECK(!b->infinity);
-    VERIFY_CHECK(a->infinity == 0 || a->infinity == 1);
+        auto groupedString = groupedStream.str();
+    wstring result(groupedString.rbegin(), groupedString.rend());
+    // Add the right (fractional or exponential) part of the number to the final string.
+    if (hasDecimal)
+    {
+        result += displayString.substr(dec);
+    }
+    else if (hasExponent)
+    {
+        result += displayString.substr(exp);
     }
     
+    /****************************Module*Header***********************************
+* Module Name: EngineStrings.h
+*
+* Module Description:
+*       Resource String ID's for the private strings used by Engine. Internal to Engine related code
+*         not required by the clients
+*
+* Warnings:
+*
+* Created: 13-Feb-2008
+*
+\****************************************************************************/
     
-    {    secp256k1_scalar_set_b32(&sec, seckey, &overflow);
-    /* Fail if the secret key is invalid. */
-    if (!overflow && !secp256k1_scalar_is_zero(&sec)) {
-        unsigned char nonce32[32];
-        unsigned int count = 0;
-        secp256k1_scalar_set_b32(&msg, msg32, NULL);
-        while (1) {
-            ret = noncefp(nonce32, msg32, seckey, NULL, (void*)noncedata, count);
-            if (!ret) {
-                break;
-            }
-            secp256k1_scalar_set_b32(&non, nonce32, &overflow);
-            if (!secp256k1_scalar_is_zero(&non) && !overflow) {
-                if (secp256k1_ecdsa_sig_sign(&ctx->ecmult_gen_ctx, &r, &s, &sec, &msg, &non, &recid)) {
-                    break;
-                }
-            }
-            count++;
-        }
-        memset(nonce32, 0, 32);
-        secp256k1_scalar_clear(&msg);
-        secp256k1_scalar_clear(&non);
-        secp256k1_scalar_clear(&sec);
+            const CollectionData& coll = map.find(names[i])->second;
+    
+    //
+// Check that cursor returns no results if there are no docs within interval
+// and the first key located during initialization is above upper bound
+//
+class QueryStageCountScanNothingInIntervalFirstMatchTooHigh : public CountBase {
+public:
+    void run() {
+        dbtests::WriteContextForTests ctx(&_opCtx, ns());
     }
-    if (ret) {
-        secp256k1_ecdsa_recoverable_signature_save(signature, &r, &s, recid);
-    } else {
-        memset(signature, 0, sizeof(*signature));
     }
-    return ret;
+    
+    ScientificNumberFormatter *ScientificNumberFormatter::createMarkupInstance(
+        DecimalFormat *fmtToAdopt,
+        const UnicodeString &beginMarkup,
+        const UnicodeString &endMarkup,
+        UErrorCode &status) {
+    return createInstance(
+            fmtToAdopt,
+            new MarkupStyle(beginMarkup, endMarkup),
+            status);
 }
     
+    namespace {
+struct bio_deleter {
+  void operator()(BIO* b)
+  {
+    if (b)
+      BIO_free(b);
+  }
+};
+typedef std::unique_ptr<BIO, bio_deleter> bio_t;
+struct p12_deleter {
+  void operator()(PKCS12* p)
+  {
+    if (p)
+      PKCS12_free(p);
+  }
+};
+typedef std::unique_ptr<PKCS12, p12_deleter> p12_t;
+struct pkey_deleter {
+  void operator()(EVP_PKEY* x)
+  {
+    if (x)
+      EVP_PKEY_free(x);
+  }
+};
+typedef std::unique_ptr<EVP_PKEY, pkey_deleter> pkey_t;
+struct x509_deleter {
+  void operator()(X509* x)
+  {
+    if (x)
+      X509_free(x);
+  }
+};
+typedef std::unique_ptr<X509, x509_deleter> x509_t;
+struct x509_sk_deleter {
+  void operator()(STACK_OF(X509) * x)
+  {
+    if (x)
+      sk_X509_pop_free(x, X509_free);
+  }
+};
+typedef std::unique_ptr<STACK_OF(X509), x509_sk_deleter> x509_sk_t;
+} // namespace
     
-    {  ASSERT_TRUE(!ParseInternalKey(Slice('bar'), &decoded));
+    CredHandle* WinTLSContext::getCredHandle()
+{
+  if (cred_) {
+    return cred_.get();
+  }
+    }
+    
+    /**
+ * HTTP related preferences
+ */
+PrefPtr PREF_HTTP_USER = makePref('http-user');
+PrefPtr PREF_HTTP_PASSWD = makePref('http-passwd');
+// values: string
+PrefPtr PREF_USER_AGENT = makePref('user-agent');
+// value: string that your file system recognizes as a file name.
+PrefPtr PREF_LOAD_COOKIES = makePref('load-cookies');
+// value: string that your file system recognizes as a file name.
+PrefPtr PREF_SAVE_COOKIES = makePref('save-cookies');
+// values: true | false
+PrefPtr PREF_ENABLE_HTTP_KEEP_ALIVE = makePref('enable-http-keep-alive');
+// values: true | false
+PrefPtr PREF_ENABLE_HTTP_PIPELINING = makePref('enable-http-pipelining');
+// value: 1*digit
+PrefPtr PREF_MAX_HTTP_PIPELINING = makePref('max-http-pipelining');
+// value: string
+PrefPtr PREF_HEADER = makePref('header');
+// value: string that your file system recognizes as a file name.
+PrefPtr PREF_CERTIFICATE = makePref('certificate');
+// value: string that your file system recognizes as a file name.
+PrefPtr PREF_PRIVATE_KEY = makePref('private-key');
+// value: string that your file system recognizes as a file name.
+PrefPtr PREF_CA_CERTIFICATE = makePref('ca-certificate');
+// value: true | false
+PrefPtr PREF_CHECK_CERTIFICATE = makePref('check-certificate');
+// value: true | false
+PrefPtr PREF_USE_HEAD = makePref('use-head');
+// value: true | false
+PrefPtr PREF_HTTP_AUTH_CHALLENGE = makePref('http-auth-challenge');
+// value: true | false
+PrefPtr PREF_HTTP_NO_CACHE = makePref('http-no-cache');
+// value: true | false
+PrefPtr PREF_HTTP_ACCEPT_GZIP = makePref('http-accept-gzip');
+// value: true | false
+PrefPtr PREF_CONTENT_DISPOSITION_DEFAULT_UTF8 =
+    makePref('content-disposition-default-utf8');
+    
+    extern PrefPtr PREF_VERSION;
+extern PrefPtr PREF_HELP;
+    
+    std::string wCharToUtf8(const std::wstring& wsrc)
+{
+  int len = wCharToUtf8(nullptr, 0, wsrc.c_str());
+  if (len <= 0) {
+    abort();
+  }
+  auto buf = make_unique<char[]>((size_t)len);
+  len = wCharToUtf8(buf.get(), len, wsrc.c_str());
+  if (len <= 0) {
+    abort();
+  }
+  else {
+    return buf.get();
+  }
 }
     
-    #include <vector>
+      std::chrono::seconds timeout_;
     
+    void DHTResponseMessage::fillMessage(Dict* msgDict)
+{
+  msgDict->put(R, getResponse());
+}
     
-    { protected:
-  TransformationParameter transform_param_;
-  shared_ptr<DataTransformer<Dtype> > data_transformer_;
-  bool output_labels_;
-};
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2+)
-   *   -# @f$ (N \times ...) @f$
-   *      the inputs @f$ x_1 @f$
-   *   -# @f$ (M) @f$
-   *      the inputs @f$ x_2 @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (M \times ...) @f$:
-   *      the reindexed array @f$
-   *        y = x_1[x_2]
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    
-    {  size_t *workspace_fwd_sizes_;
-  size_t *workspace_bwd_data_sizes_;
-  size_t *workspace_bwd_filter_sizes_;
-  size_t workspaceSizeInBytes;  // size of underlying storage
-  void *workspaceData;  // underlying storage
-  void **workspace;  // aliases into workspaceData
-};
-#endif
-    
-            static Dictionary Serialize(const FunctionPtr& f);
-    
-        Constant Constant::CloneAs(DataType dataType) const
-    {
-        if (dataType != DataType::Double)
-            InvalidArgument('Constant::Clone: Cannot clone Constant '%S' with DataType '%s' to DataType '%s'.', AsString().c_str(), DataTypeName(GetDataType()), DataTypeName(dataType));
+    void DHTRoutingTable::showBuckets() const
+{
+  /*
+    for(std::deque<std::shared_ptr<DHTBucket> >::const_iterator itr =
+    buckets_.begin(); itr != buckets_.end(); ++itr) {
+    cerr << 'prefix = ' << (*itr)->getPrefixLength() << ', '
+    << 'nodes = ' << (*itr)->countNode() << endl;
     }
+  */
+}
     
-    #pragma once
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS // 'secure' CRT not available on all platforms  --add this at the top of all CPP files that give 'function or variable may be unsafe' warnings
-#endif
-#ifdef _WIN32
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif // NOMINMAX
-#pragma comment(lib, 'Dbghelp.lib')
-#else
-#include <execinfo.h>
-#include <cxxabi.h>
-#endif
-    
-    
-    {private:
-    // parameters
-    vector<ComputationNodeBasePtr> inputNodes;
-    map<wstring, ComputationNodeBasePtr> outputNodes;
-    ParameterTreatment parameterTreatment;
-    // other
-    set<ComputationNodeBasePtr> dependentSet;                                     // set of nodes that outputNodes depend on
-};
-    
-            if (Input(1)->HasMBLayout())
-        {
-            // infer rows1 as rows0
-            Input(1)->ValidateInferInputDimsFrom(TensorShape(rows0));
-            SetDims(TensorShape(rows0), true);
-        }
-        else // multiplying two straight matrices
-        {
-            size_t cols1 = Input(1)->GetAsMatrixNumCols();
-            // infer rows1 as rows0
-            Input(1)->ValidateInferInputDimsFrom(TensorShape(rows0, cols1));
-            SetDims(TensorShape(rows0, cols1), false);
-        }
-    
-        virtual void ForwardPropNonLooping() override
-    {
-        bool isInput0Sparse = Input(0)->template Is<SparseInputValue<ElemType>>();
-        bool isInput1Sparse = Input(1)->template Is<SparseInputValue<ElemType>>();
-        if (isInput0Sparse || isInput1Sparse)
-            LogicError('EditDistanceError node was not tested for sparse inputs.');
-    }
-    
-            std::vector<bool> workspaceFlagVec = {true, false};
-        for (auto& devId : m_deviceIDSet)
-        {
-            for (auto wsFlag : workspaceFlagVec)   // we allocate the workspace memory pointers first, and they are not shared with the non-workspace memory requests
-            {
-                // memAllocInfoVec is a sorted list of memory allocations from smallest to largest in memory size 
-                vector<MemAllocInfo> memAllocInfoVec;
-                int memoryCounter = 0;
-                // we start with memory request that is scalable with minibatch size(usually those require larger memory size)
-                for (auto& memInfo : memInfoVec)
-                {
-                    // check if it's the proper device
-                    if (memInfo.deviceId != devId || memInfo.isWorkSpace != wsFlag || !memInfo.mbScale)
-                        continue;
-    }
-    }
-    }
-    
-    #endif
+    #endif // D_DHT_SETUP_H
 
     
-        dword(vx_formater.instruction);
-  }
-  //TODO(rcardoso): Unimplemented instruction formaters
-  void EmitXSForm(const uint8_t op,
-                  const RegNumber rt,
-                  const RegNumber ra,
-                  const uint8_t sh,
-                  const uint16_t xop,
-                  const bool rc = 0){
+    namespace aria2 {
+    }
     
-    APCHandle::Pair APCCollection::Make(const ObjectData* obj,
-                                    APCHandleLevel level,
-                                    bool unserializeObj) {
-  auto bail = [&] {
-    return APCString::MakeSerializedObject(
-      apc_serialize(Variant(const_cast<ObjectData*>(obj)))
-    );
-  };
+    void DHTTaskExecutor::update()
+{
+  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
+                                  std::mem_fn(&DHTTask::finished)),
+                   execTasks_.end());
+  int r;
+  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
+    r = numConcurrent_ - execTasks_.size();
+  }
+  else {
+    r = 0;
+  }
+  while (r && !queue_.empty()) {
+    std::shared_ptr<DHTTask> task = queue_.front();
+    queue_.pop_front();
+    task->startup();
+    if (!task->finished()) {
+      execTasks_.push_back(task);
+      --r;
+    }
+  }
+  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
+                   static_cast<unsigned int>(getExecutingTaskSize()),
+                   static_cast<unsigned int>(getQueueSize())));
+}
+    
+    #endif // D_DHT_TASK_EXECUTOR_H
+
+    
+        ret = p.write(&p, (void*) SW_STRS('hello world1'));
+    ASSERT_GT(ret, 0);
+    ret = p.write(&p, (void*) SW_STRS('hello world2'));
+    ASSERT_GT(ret, 0);
+    ret = p.write(&p, (void*) SW_STRS('hello world3'));
+    ASSERT_GT(ret, 0);
+    
+        swListenPort *port = swServer_add_port(&serv, SW_SOCK_TCP, '127.0.0.1', 9501);
+    port->open_eof_check = 0;
+    //config
+    port->backlog = 128;
+    memcpy(port->protocol.package_eof, SW_STRL('\r\n\r\n'));  //开启eof检测，启用buffer区
+    
+    #include <sys/stat.h>
+    
+    static void thread_read(int i)
+{
+    pkg recv_pkg;
+    uint32_t tmp;
+    int ret;
+    uint32_t recv_count = 0;
+    int j = 0;
+    swPipe *sock = &threads[i].pipe;
+    int task_n = WRITE_N / READ_THREAD_N;
     }
     
     
-    {///////////////////////////////////////////////////////////////////////////////
-}
-    
-    public:
-  static void Add(InfoVec& out, const char* name, const std::string& value);
-  static void AddServerStats(InfoVec& out, const char* name,
-                             const char* statsName = nullptr);
-    
-    
-    {///////////////////////////////////////////////////////////////////////////////
-}
-    
-        redisReply * reply = static_cast<redisReply *>(r);
-    ExampleQt * ex = static_cast<ExampleQt *>(privdata);
-    if (reply == nullptr || ex == nullptr) return;
-    
-        cache.set('test', val);
-    ASSERT_EQ(cache.get('test').get(), val.get());
-    
-        pid_t server_pid = create_server();
-    
-        //create Server
-    ret = swServer_create(&serv);
-    if (ret < 0)
     {
-        swTrace('create server fail[error=%d].\n', ret);
-        exit(0);
+    {    Exception(int code) : code(code)
+    {
+        msg = swoole_strerror(code);
+    }
+};
+}
+    
+    TEST(coroutine_base, get_current)
+{
+    long _cid;
+    long cid = Coroutine::create([](void *arg)
+    {
+        auto co = Coroutine::get_current();
+        *(long *) arg = co->get_cid();
+    }, &_cid);
     }
