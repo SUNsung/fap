117325fc45e54e51b20137457477df758e0392d4
SUNsung <weatherius@gmail.com>
2019-07-31 15:31:19 +0300

    Rewrite [29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb] File in: 2019-07-31 15:31:19

--- ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
+++ ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
@@ -1,72 +1,161 @@
 
         
-          attr_accessor :comments, :comment_ids
-  def comments_attributes=(attributes); end
+              attr_reader :headers
     
-          super
-    end
+            # @private
+        # @return [Hash<PodVariant, String>]
+        #
+        def scope_by_specs
+          root_spec = variants.first.root_spec
+          specs = [root_spec]
+          specs += if root_spec.default_subspecs.empty?
+                     root_spec.subspecs.compact
+                   else
+                     root_spec.default_subspecs.map do |subspec_name|
+                       root_spec.subspec_by_name('#{root_spec.name}/#{subspec_name}')
+                     end
+          end
+          default_specs = Set.new(specs)
+          grouped_variants = group_by(&:specs)
+          all_spec_variants = grouped_variants.map { |set| set.variants.first.specs }
+          common_specs = all_spec_variants.map(&:to_set).flatten.inject(&:&)
+          omit_common_specs = common_specs.any? && common_specs.proper_superset?(default_specs)
+          scope_if_necessary(grouped_variants.map(&:scope_by_build_type)) do |variant|
+            specs = variant.specs.to_set
     
-          def call
-        ask_question
-        value_or_default
+    ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+$:.unshift((ROOT + 'lib').to_s)
+$:.unshift((ROOT + 'spec').to_s)
+    
+          it 'allows to differentiate between an exclusive variant with a specific subspec and ' \
+        'an inclusive variant with the default subspecs plus a non-default subspec' do
+        variants = PodVariantSet.new([
+          PodVariant.new([@foo_subspec], [], [], Platform.ios),
+          PodVariant.new([@root_spec, @default_subspec, @foo_subspec], [], [], Platform.ios),
+        ])
+        variants.scope_suffixes.values.should == %w(Foo .default-Foo)
       end
     
-          attr_reader :validators
+          it 'returns the path of output file for the check pod manifest file  script' do
+        @target.check_manifest_lock_script_output_file_path.should == '$(DERIVED_FILE_DIR)/Pods-checkManifestLockResult.txt'
+      end
+    end
     
-    # We use a special :_default_git value so that SCMResolver can tell whether the
-# default has been replaced by the user via `set`.
-set_if_empty :scm, Capistrano::Configuration::SCMResolver::DEFAULT_GIT
-set_if_empty :branch, 'master'
-set_if_empty :deploy_to, -> { '/var/www/#{fetch(:application)}' }
-set_if_empty :tmp_dir, '/tmp'
+            it 'removes outdated custom shell script phases' do
+          @pod_bundle.target_definition.stubs(:script_phases).returns([:name => 'Custom Script', :script => 'echo 'Hello World''])
+          @target_integrator.integrate!
+          target = @target_integrator.send(:native_targets).first
+          target.shell_script_build_phases.find { |bp| bp.name == @user_script_phase_name }.should.not.be.nil
+          @pod_bundle.target_definition.stubs(:script_phases).returns([])
+          @target_integrator.integrate!
+          target.shell_script_build_phases.find { |bp| bp.name == @user_script_phase_name }.should.be.nil
+        end
     
-    # This is basically a copy of the original bundler 'bundle' shim
-# with the addition of the loading of our Bundler patches that
-# modify Bundler's caching behaviour.
+        def pipeline_data(pipeline)
+      {
+        'id' => pipeline.pipeline_id,
+        'hash' => pipeline.lir.unique_hash,
+        'ephemeral_id' => pipeline.ephemeral_id,
+        'workers' =>  pipeline.settings.get('pipeline.workers'),
+        'batch_size' =>  pipeline.settings.get('pipeline.batch.size'),
+        'representation' => ::LogStash::Config::LIRSerializer.serialize(pipeline.lir)
+      }
+    end
     
-      # retrieve only the latest spec for all locally installed plugins
-  # @return [Hash] result hash {plugin_name.downcase => plugin_spec}
-  def find_latest_gem_specs
-    LogStash::PluginManager.all_installed_plugins_gem_specs(gemfile).inject({}) do |result, spec|
-      previous = result[spec.name.downcase]
-      result[spec.name.downcase] = previous ? [previous, spec].max_by{|s| s.version} : spec
-      result
+        def fetch_node_stats(agent, stats)
+      @global_stats.merge({
+        'http_address' => stats.get_shallow(:http_address).value,
+        'ephemeral_id' => agent.ephemeral_id
+      })
+    end
+  end
+end; end; end
+
+    
+        def find_jars(pattern)
+      require 'java'
+      jar_files = Dir.glob(pattern)
+      raise(LogStash::EnvironmentError, I18n.t('logstash.environment.missing-jars', :pattern => pattern)) if jar_files.empty?
+      jar_files
+    end
+    
+        context 'windows' do
+      windows_host_os.each do |host|
+        it '#{host} returns true' do
+          allow(LogStash::Environment).to receive(:host_os).and_return(host)
+          expect(LogStash::Environment.windows?).to be_truthy
+        end
+      end
+    
+            let(:plugin_source) do
+          %q[
+              grok {
+                match => { 'message' => '%{WORD:word}' }
+                match => { 'examplefield' => '%{NUMBER:num}' }
+                break_on_match => false
+              }
+          ]
+        end
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
+    end
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
     end
   end
 end
-
     
-        def user_feedback_string_for(action, platform, machines, options={})
-      experimental_string = options['experimental'] ? 'experimental' : 'non experimental'
-      message  = '#{action} all #{experimental_string} VM's defined in acceptance/Vagrantfile'
-      '#{message} for #{platform}: #{machines}' if !platform.nil?
-    end
-    
-              it 'successfully install the plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install logstash-filter-qatest')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest')
-          end
-    
-        after :each do
-      logstash.uninstall
-    end
-    
-          # Returns an array of all the when branches in the `case` statement.
-      #
-      # @return [Array<WhenNode>] an array of `when` nodes
-      def when_branches
-        node_parts[1...-1]
-      end
-    
-          # Checks whether this node body is a void context.
-      #
-      # @return [Boolean] whether the `def` node body is a void context
-      def void_context?
-        method?(:initialize) || assignment_method?
-      end
-    
-              lambda do |corrector|
-            swap_range(corrector, current_range, previous_range)
+            Dir.chdir(includes_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(file)
+            source = File.read(file)
+            partial = Liquid::Template.parse(source)
+            context.stack do
+              rtn = rtn + partial.render(context)
+            end
+          else
+            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
           end
         end
+      end
+      rtn
+    end
+  end
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
+    end
