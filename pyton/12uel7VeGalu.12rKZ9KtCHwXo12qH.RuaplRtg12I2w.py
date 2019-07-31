89898d1015e8e548df8228714a0eeb816ec8612d
SUNsung <weatherius@gmail.com>
2019-07-31 07:16:23 +0300

    Rewrite [12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py] File in: 2019-07-31 07:16:22

--- pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
+++ pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
@@ -1,89 +1,164 @@
 
         
-                if module.check_mode:
-            firewall_policy_id = get_firewall_policy(oneandone_conn, firewall_id)
-            if (firewall_rules and firewall_policy_id):
-                return True
-            return False
+            def test_app_alias_alter_history(self, settings, shell):
+        settings.alter_history = True
+        assert 'builtin history delete' in shell.app_alias('FUCK')
+        assert 'builtin history merge' in shell.app_alias('FUCK')
+        settings.alter_history = False
+        assert 'builtin history delete' not in shell.app_alias('FUCK')
+        assert 'builtin history merge' not in shell.app_alias('FUCK')
+    
+        def _get_version(self):
+        '''Returns the version of the current shell'''
+        proc = Popen(['bash', '-c', 'echo $BASH_VERSION'],
+                     stdout=PIPE, stderr=DEVNULL)
+        return proc.stdout.read().decode('utf-8').strip()
+
+    
+        def _script_from_history(self, line):
+        if '- cmd: ' in line:
+            return line.split('- cmd: ', 1)[1]
+        else:
+            return ''
+    
+        def encode_utf8(self, command):
+        if six.PY2:
+            return command.encode('utf8')
+        return command
+    
+        def _parse_alias(self, alias):
+        name, value = alias.split('=', 1)
+        if value[0] == value[-1] == ''' or value[0] == value[-1] == ''':
+            value = value[1:-1]
+        return name, value
+    
+        response, info = fetch_url(module=module, url=url, headers=headers, data=rawdata, method=method)
+    status = info['status']
+    content = ''
+    if response:
+        content = response.read()
+    if status == 204:
+        return True, content
+    elif status == 200 or status == 201:
+        return True, json.loads(content)
+    else:
+        return False, str(status) + ': ' + content
+    
+        def _get_policies_for_datacenter(self, p):
+        '''
+        Get the Policies for a datacenter by calling the CLC API.
+        :param p: datacenter to get policies from
+        :return: policies in the datacenter
+        '''
+        response = {}
+    
+    ---
+- name: Create Server Group
+  hosts: localhost
+  gather_facts: False
+  connection: local
+  tasks:
+    - name: Create / Verify a Server Group at CenturyLink Cloud
+      clc_group:
+        name: My Cool Server Group
+        parent: Default Group
+        state: present
+      register: clc
+    
+    LINODE_IMP_ERR = None
+try:
+    from linode_api4 import Instance, LinodeClient
+    HAS_LINODE_DEPENDENCY = True
+except ImportError:
+    LINODE_IMP_ERR = traceback.format_exc()
+    HAS_LINODE_DEPENDENCY = False
+    
+        # create a client object
+    client = MatrixClient(module.params['hs_url'])
+    if module.params['token'] is not None:
+        client.api.token = module.params['token']
+    else:
+        client.login(module.params['user_id'], module.params['password'], sync=False)
+    
+        response, info = post_sendgrid_api(module, username, password,
+                                       from_address, to_addresses, subject, body, attachments=attachments,
+                                       bcc=bcc, cc=cc, headers=headers, html_body=html_body, api_key=api_key)
+    
+        if layman.is_installed(name):
+        return False
+    
+        def log(self, e):
+        if log.log_tier(e.level) == log.log_tier('error'):
+            outfile = self.outfile or realstderr
+        else:
+            outfile = self.outfile or realstdout
+    
+        Attributes:
+        address: Remote address. Can be both a domain or an IP address.
+        ip_address: Resolved remote IP address.
+        source_address: Local IP address or client's source IP address.
+        tls_established: True if TLS is established, False otherwise
+        cert: The certificate presented by the remote during the TLS handshake
+        sni: Server Name Indication sent by the proxy during the TLS handshake
+        alpn_proto_negotiated: The negotiated application protocol
+        tls_version: TLS version
+        via: The underlying server connection (e.g. the connection to the upstream proxy in upstream proxy mode)
+        timestamp_start: Connection start timestamp
+        timestamp_tcp_setup: TCP ACK received timestamp
+        timestamp_tls_setup: TLS established timestamp
+        timestamp_end: Connection end timestamp
+    '''
     
     
-def main():
-    argument_spec = ipa_argument_spec()
-    argument_spec.update(zone_name=dict(type='str', required=True),
-                         state=dict(type='str', default='present', choices=['present', 'absent']),
-                         )
+    class TextChunk(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self.keyword = (self._io.read_bytes_term(0, False, True, True)).decode(u'iso8859-1')
+            self.text = (self._io.read_bytes_full()).decode(u'iso8859-1')
     
-        if module.params['repo']:
-        params['deploy[scm_repository]'] = module.params['repo']
+    		logging.debug('Total bytes dequeued: %d' % wbXMLBytes.bytesDequeued)
+
     
-    
-class CheckIp(front_base.check_ip.CheckIp):
-    def check_response(self, response):
-        server_type = response.headers.get('Server', '')
-        self.logger.debug('status:%d', response.status)
-        self.logger.debug('Server type:%s', server_type)
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licence]
+        def configure(self, updated):
+        if 'web_static_viewer' in updated and ctx.options.web_static_viewer:
+            flows = io.read_flows_from_paths([ctx.options.rfile])
+            p = pathlib.Path(ctx.options.web_static_viewer).expanduser()
+            self.export(p, flows)
     
     
-    def getType(self):
-        return self.type 
+class TestBombEnemy(unittest.TestCase):
+    def test_3x4(self):
+        grid1 = [
+                 ['0','E','0','0'],
+                 ['E','0','W','E'],
+                 ['0','E','0','0']
+                ]
+        self.assertEqual(3, bomb_enemy.max_killed_enemies(grid1))
     
-    DDB_KINESIS_STREAM_NAME_PREFIX = '__ddb_stream_'
+        def _remove_from_nonleaf_node(self, node: Node, key_index: int):
+        key = node.keys[key_index]
+        left_subtree = node.children[key_index]
+        if len(left_subtree.keys) > self.min_numbers_of_keys:
+            largest_key = self._find_largest_and_delete_in_left_subtree(left_subtree)
+        elif len(node.children[key_index + 1].keys) > self.min_numbers_of_keys:
+            largest_key = self._find_largest_and_delete_in_right_subtree(node.children[key_index + 1])
+        else:
+            self._merge(node, key_index, key_index + 1)
+            return self._remove_key(node, key)
     
-    # Stack with flags to indicate whether we are currently re-playing API calls.
-# (We should not be re-playing and recording at the same time)
-CURRENTLY_REPLAYING = []
-    
-        def test_extract_query_params(self):
-        path, query_params = apigateway_listener.extract_query_string_params(
-            '/foo/bar?foo=foo&bar=bar&bar=baz'
-        )
-        self.assertEqual(path, '/foo/bar')
-        self.assertEqual(query_params, {'foo': 'foo', 'bar': ['bar', 'baz']})
-    
-    
-if __name__ == '__main__':
-    main()
-    
-            print('Test: Pop on a stack to destroy it')
-        assert_equal(stacks.pop(), 'a')
-    
-    
-class TestStack(object):
-    
-    		if current.leftChild is None and current.rightChild is None:
-			if current is self.root:
-				self.root = None
-			elif isLeft:
-				parent.leftChild = None
-			else:
-				parent.rightChild = None
+    def remove_dups_wothout_set(head):
+    '''
+    Time Complexity: O(N^2)
+    Space Complexity: O(1)
+    '''
+    current = head
+    while current:
+        runner = current
+        while runner.next:
+            if runner.next.val == current.val:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        current = current.next
