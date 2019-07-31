82a80ec5defe38581c90e4869806bbd318b865d2
SUNsung <weatherius@gmail.com>
2019-07-31 08:52:06 +0300

    Rewrite [46RW7XYkNDL9A46wbJVAIeDrSc46sC5HL8VPdTo46Cn9.py] File in: 2019-07-31 08:52:05

--- pyton/46RW7XYkNDL9A46wbJVAIeDrSc46sC5HL8VPdTo46Cn9.py
+++ pyton/46RW7XYkNDL9A46wbJVAIeDrSc46sC5HL8VPdTo46Cn9.py
@@ -1,245 +1,156 @@
 
         
-        
-def _dump_loader_info(loader):
-    yield 'class: %s.%s' % (type(loader).__module__, type(loader).__name__)
-    for key, value in sorted(loader.__dict__.items()):
-        if key.startswith('_'):
-            continue
-        if isinstance(value, (tuple, list)):
-            if not all(isinstance(x, (str, text_type)) for x in value):
-                continue
-            yield '%s:' % key
-            for item in value:
-                yield '  - %s' % item
-            continue
-        elif not isinstance(value, (str, text_type, int, float, bool)):
-            continue
-        yield '%s: %r' % (key, value)
+          num_layer = len(model_layers)  # Number of layers in the MLP
+  for layer in xrange(1, num_layer):
+    model_layer = tf.keras.layers.Dense(
+        model_layers[layer],
+        kernel_regularizer=tf.keras.regularizers.l2(mlp_reg_layers[layer]),
+        activation='relu')
+    mlp_vector = model_layer(mlp_vector)
     
-        if args and kwargs:
-        raise TypeError('jsonify() behavior undefined when passed both args and kwargs')
-    elif len(args) == 1:  # single args are passed directly to dumps()
-        data = args[0]
-    else:
-        data = args or kwargs
+    # Architectural constants.
+NUM_FRAMES = 96  # Frames in input mel-spectrogram patch.
+NUM_BANDS = 64  # Frequency bands in input mel-spectrogram patch.
+EMBEDDING_SIZE = 128  # Size of embedding layer.
     
-        def _fail(self, *args, **kwargs):
-        raise RuntimeError(
-            'The session is unavailable because no secret '
-            'key was set.  Set the secret_key on the '
-            'application to something unique and secret.'
-        )
+    import vggish_params
     
-                # Since we have to open a new request context for the session
-            # handling we want to make sure that we hide out own context
-            # from the caller.  By pushing the original request context
-            # (or None) on top of this and popping it we get exactly that
-            # behavior.  It's important to not use the push and pop
-            # methods of the actual request context object since that would
-            # mean that cleanup handlers are called
-            _request_ctx_stack.push(outer_reqctx)
-            try:
-                yield sess
-            finally:
-                _request_ctx_stack.pop()
-    
-        :copyright: 2010 Pallets
-    :license: BSD-3-Clause
-'''
-import os
-import pkgutil
-import sys
-import textwrap
-    
-            cli = FlaskGroup(create_app=create_app)
-        return partial(runner.invoke, cli)
-    
-        n_samples = 2000
-    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
-    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
-                                           list_n_features, precompute=False)
-    plt.subplot(212)
-    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
-    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
-    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
-    plt.legend(loc='upper left')
-    plt.xlabel('number of features')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    plt.show()
-
+      aggregation_extraction.ExtractAggregatedRepresentationsToFiles(
+      image_names=image_list,
+      features_dir=cmd_args.features_dir,
+      aggregation_config_path=cmd_args.aggregation_config_path,
+      mapping_path=cmd_args.index_mapping_path,
+      output_aggregation_dir=cmd_args.output_aggregation_dir)
     
     
-def plot_feature_errors(all_errors, batch_size, all_components, data):
-    plt.figure()
-    plot_results(all_components, all_errors['pca'], label='PCA')
-    plot_results(all_components, all_errors['ipca'],
-                 label='IncrementalPCA, bsize=%i' % batch_size)
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm error vs. n_components\n'
-                 'LFW, size %i x %i' % data.shape)
-    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
-    plt.ylabel('Mean absolute error')
+def main(argv):
+  if len(argv) > 1:
+    raise RuntimeError('Too many command-line arguments.')
     
-    ratio = scikits_time / scipy_time
+        Args:
+      images: Uint8 array with shape (batch, height, width 3) containing a batch
+        of RGB images.
     
-                gc.collect()
-            print('max_iter', max_iter)
-            print('- benchmarking A-SGD')
-            clf = SGDRegressor(alpha=alpha / n_train, fit_intercept=False,
-                               max_iter=max_iter, learning_rate='invscaling',
-                               eta0=.002, power_t=0.05, tol=1e-3,
-                               average=(max_iter * n_train // 2))
+      # Sort predictions according to Kaggle's convention:
+  # - first by score (descending);
+  # - then by key (ascending);
+  # - then by class (ascending).
+  sorted_predictions_by_key_class = sorted(
+      predictions.items(), key=lambda item: (item[0], item[1]['class']))
+  sorted_predictions = sorted(
+      sorted_predictions_by_key_class,
+      key=lambda item: item[1]['score'],
+      reverse=True)
     
-    Sentiment analysis can be casted as a binary text classification problem,
-that is fitting a linear classifier on features extracted from the text
-of the user messages so as to guess wether the opinion of the author is
-positive or negative.
+         Structure:
+        <train_dir>/
+        ├── <person1>/
+        │   ├── <somename1>.jpeg
+        │   ├── <somename2>.jpeg
+        │   ├── ...
+        ├── <person2>/
+        │   ├── <somename1>.jpeg
+        │   └── <somename2>.jpeg
+        └── ...
     
-    Second example
---------------
-The second example shows the ability of the Minimum Covariance Determinant
-robust estimator of covariance to concentrate on the main mode of the data
-distribution: the location seems to be well estimated, although the covariance
-is hard to estimate due to the banana-shaped distribution. Anyway, we can
-get rid of some outlying observations.
-The One-Class SVM is able to capture the real data structure, but the
-difficulty is to adjust its kernel bandwidth parameter so as to obtain
-a good compromise between the shape of the data scatter matrix and the
-risk of over-fitting the data.
-    
+        # If no valid image file was uploaded, show the file upload form:
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <form method='POST' enctype='multipart/form-data'>
+      <input type='file' name='file'>
+      <input type='submit' value='Upload'>
+    </form>
     '''
-print(__doc__)
     
-    from sklearn.cluster import KMeans
-from sklearn.datasets import make_blobs
+    # Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+frame_number = 0
     
-    # Part 2: Qualitative visual inspection of the convergence
+    # Load the test image with unknown faces into a numpy array
+test_image = face_recognition.load_image_file('test_image.jpg')
     
-    # MiniBatchKMeans
-ax = fig.add_subplot(1, 3, 2)
-for k, col in zip(range(n_clusters), colors):
-    my_members = mbk_means_labels == order[k]
-    cluster_center = mbk_means_cluster_centers[order[k]]
-    ax.plot(X[my_members, 0], X[my_members, 1], 'w',
-            markerfacecolor=col, marker='.')
-    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-            markeredgecolor='k', markersize=6)
-ax.set_title('MiniBatchKMeans')
-ax.set_xticks(())
-ax.set_yticks(())
-plt.text(-3.5, 1.8, 'train time: %.2fs\ninertia: %f' %
-         (t_mini_batch, mbk.inertia_))
-    
-    
-# #############################################################################
-# Define the structure A of the data. Here a 10 nearest neighbors
-from sklearn.neighbors import kneighbors_graph
-connectivity = kneighbors_graph(X, n_neighbors=10, include_self=False)
-    
-    
-class Root(Resource):
-    
-    # Declare top-level shortcuts
-from scrapy.spiders import Spider
-from scrapy.http import Request, FormRequest
-from scrapy.selector import Selector
-from scrapy.item import Item, Field
-    
-    from six.moves.urllib.parse import urlencode
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('--get', dest='get', metavar='SETTING',
-            help='print raw setting value')
-        parser.add_option('--getbool', dest='getbool', metavar='SETTING',
-            help='print setting value, interpreted as a boolean')
-        parser.add_option('--getint', dest='getint', metavar='SETTING',
-            help='print setting value, interpreted as an integer')
-        parser.add_option('--getfloat', dest='getfloat', metavar='SETTING',
-            help='print setting value, interpreted as a float')
-        parser.add_option('--getlist', dest='getlist', metavar='SETTING',
-            help='print setting value, interpreted as a list')
-    
-    import scrapy
-from scrapy.commands import ScrapyCommand
-from scrapy.utils.versions import scrapy_components_versions
-    
-            # set Cookie header
-        request.headers.pop('Cookie', None)
-        jar.add_cookie_header(request)
-        self._debug_cookie(request, spider)
-    
-            pemfile = '/etc/ssl/certs/CA.pem'
-        new_certfile = '/usr/local/share/ca-certificates/CA.crt'
-        if not os.path.exists(pemfile) or not CertUtil.file_is_same(certfile, new_certfile):
-            if os.system('cp '%s' '%s' && update-ca-certificates' % (certfile, new_certfile)) != 0:
-                xlog.warning('install root certificate failed, Please run as administrator/root/sudo')
-    
-        global pteredor_is_running
-    pteredor_is_running = True
-    server_list = prober.eval_servers()
-    pteredor_is_running = False
-    
-    	# Track the line at which the error occurred in case this is
-	# generated from a lexer.  We need to track this since the
-        # unexpected char doesn't carry the line info.
-        self.line = None
-    
-    
-    def skip(self):
-        '''
-        Instruct the lexer to skip creating a token for current lexer rule
-        and look for another token.  nextToken() knows to keep looking when
-        a lexer rule finishes with token set to SKIP_TOKEN.  Recall that
-        if token==null at end of any token rule, it creates one for you
-        and emits it.
-        '''
-        
-        self._state.token = SKIP_TOKEN
-    
-            for item in platform_config:
-            try:
-                platform = item.get(CONF_PLATFORM)
-            except AttributeError:
-                platform = None
-    
-        #Parsing the arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dir_path', help = 'Path to tldr 'pages' directory')
-    parser.add_argument('-c', choices=['solarized-light', 'solarized-dark'], help='Color scheme of the PDF')
-    args = parser.parse_args()
-    
-    loc = args.dir_path
-    if args.c == 'solarized-light' or args.c == 'solarized-dark':
-        colorscheme = args.c
-        
-    main(loc, colorscheme)
+    # Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
 
     
     
-if __name__ == '__main__':
-    main()
+# A subprocess use to capture frames.
+def capture(read_frame_list):
+    # Get a reference to webcam #0 (the default one)
+    video_capture = cv2.VideoCapture(0)
+    # video_capture.set(3, 640)  # Width of the frames in the video stream.
+    # video_capture.set(4, 480)  # Height of the frames in the video stream.
+    # video_capture.set(5, 30) # Frame rate.
+    print('Width: %d, Height: %d, FPS: %d' % (video_capture.get(3), video_capture.get(4), video_capture.get(5)))
+    
+    
+def relu(x):
+    '''ReLU
+    `o = max(0., x)`
+    '''
+    return tf.nn.relu(x)
+    
+                # Use attention after lstm
+            x = RNNLayer(x, tf.nn.rnn_cell.LSTMCell, n_hidden=32)
+            x = attention_for_rnn(x.outputs)
+    
+    References:
+    [paper] Bidirectional Attention Flow for Machine Comprehension (https://arxiv.org/abs/1611.01603)
+    [github/DuReader] https://github.com/baidu/DuReader/blob/master/tensorflow/layers/match_layer.py
+    [github/BiDAF(PyTorch)] https://github.com/jojonki/BiDAF/blob/master/layers/bidaf.py
+'''
+    
+        def add_frame_separator(self):
+        ''' Add a separator between top and bottom frames '''
+        logger.debug('Add frame seperator')
+        sep = ttk.Frame(self.page_frame, height=2, relief=tk.RIDGE)
+        sep.pack(fill=tk.X, pady=(5, 0), side=tk.BOTTOM)
+        logger.debug('Added frame seperator')
+    
+    
+class QueueManager():
+    ''' Manage queues for availabilty across processes
+        Don't import this class directly, instead
+        import the variable: queue_manager '''
+    def __init__(self):
+        logger.debug('Initializing %s', self.__class__.__name__)
+    
+        http://stackoverflow.com/questions/3221956/
+           what-is-the-simplest-way-to-make-tooltips-
+           in-tkinter/36221216#36221216
+    
+                    if channels < self.group:
+                    raise ValueError('Input channels should be larger than group size' +
+                                     '; Received input channels: ' + str(channels) +
+                                     '; Group size: ' + str(self.group))
+    
+    from importlib import import_module
+    
+        def run(self, old_face, new_face, raw_mask):
+        ''' Perform selected adjustment on face '''
+        logger.trace('Performing color adjustment')
+        # Remove Mask for processing
+        reinsert_mask = False
+        if new_face.shape[2] == 4:
+            reinsert_mask = True
+            final_mask = new_face[:, :, -1]
+            new_face = new_face[:, :, :3]
+        new_face = self.process(old_face, new_face, raw_mask)
+        new_face = np.clip(new_face, 0.0, 1.0)
+        if reinsert_mask and new_face.shape[2] != 4:
+            # Reinsert Mask
+            new_face = np.concatenate((new_face, np.expand_dims(final_mask, axis=-1)), -1)
+        logger.trace('Performed color adjustment')
+        return new_face
 
     
-        url = options.url + '/updateReports?agent=%s' % options.name
-    update_ws = yield websocket_connect(url, None)
-    msg = yield update_ws.read_message()
-    assert msg is None
-    IOLoop.instance().stop()
-    
-        .. deprecated:: 5.0
-    
-        def run_policy_test(self, accessor, expected_type):
-        # With the default policy, non-main threads don't get an event
-        # loop.
-        self.assertRaises(
-            (RuntimeError, AssertionError), self.executor.submit(accessor).result
-        )
-        # Set the policy and we can get a loop.
-        asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
-        self.assertIsInstance(self.executor.submit(accessor).result(), expected_type)
-        # Clean up to silence leak warnings. Always use asyncio since
-        # IOLoop doesn't (currently) close the underlying loop.
-        self.executor.submit(lambda: asyncio.get_event_loop().close()).result()
+        @property
+    def kwarg_requirements(self):
+        ''' Return a dict of function name to a list of required kwargs '''
+        return dict(gaussian=['ksize', 'sigmaX'],
+                    normalized=['ksize'])
