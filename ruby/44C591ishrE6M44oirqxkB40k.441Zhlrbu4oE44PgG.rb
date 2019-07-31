64a5978bf79977904674f4d65dd9929f3f00f567
SUNsung <weatherius@gmail.com>
2019-07-31 09:21:40 +0300

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2019-07-31 09:21:40

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,30 +1,64 @@
 
         
-        module AbstractController
-  module Testing
-    class ControllerWithHelpers < AbstractController::Base
-      include AbstractController::Helpers
-      include AbstractController::Rendering
-      include ActionView::Rendering
+              if category_names.present?
+        category_names.uniq!
+        json_response[:forbidden_message] = I18n.t(
+          'tags.forbidden.restricted_to',
+          count: category_names.count,
+          tag_name: tag.name,
+          category_names: category_names.join(', ')
+        )
+      else
+        json_response[:forbidden_message] = I18n.t('tags.forbidden.in_this_category', tag_name: tag.name)
+      end
+    end
     
-      alias_method :secret?, :secret
-  alias_method :persisted?, :persisted
+      def create_backups
+    @objects.clear
     
-            self.arguments = [
-          CLAide::Argument.new('NAME', false),
-        ]
+        @history = UserHistory.find(params[:id])
+    prev = @history.previous_value
+    cur = @history.new_value
     
-          def self.options
-        options = []
-        options.concat(super.reject { |option, _| option == '--silent' })
+          record = StaffActionLogger.new(Discourse.system_user)
+        .log_theme_change(original_json, theme)
+    
+    # component must be String
+def encodeURIComponent(componentString)
+  ::URI::URIEncodeComponent(componentString)
+end
+    
+          def line_class(line)
+        if line =~ /^@@/
+          'gc'
+        elsif line =~ /^\+/
+          'gi'
+        elsif line =~ /^\-/
+          'gd'
+        else
+          ''
+        end
       end
     
-            def self.options
-          [
-            ['--regex', 'Interpret the `QUERY` as a regular expression'],
-            ['--show-all', 'Pick from all versions of the given podspec'],
-          ].concat(super)
-        end
+          attr_reader :page, :content
     
-      # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+          def escaped_name
+        CGI.escape(@name)
+      end
+    
+          def toc_content
+        @toc_content
+      end
+    
+        get '/compare/A/fc66539528eb96f21b2bbdbf557788fe8a1196ac..b26b791cb7917c4f37dd9cb4d1e0efb24ac4d26f'
+    
+    context 'Precious::Views::Page' do
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
+  end
+    
+      class Error < StandardError;
+  end
