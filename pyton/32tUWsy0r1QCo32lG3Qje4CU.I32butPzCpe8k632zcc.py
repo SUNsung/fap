96d60f2016edebdac5474c04d5fcf8677acec0d9
SUNsung <weatherius@gmail.com>
2019-07-31 20:37:23 +0300

    Rewrite [32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py] File in: 2019-07-31 20:37:23

--- pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
+++ pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
@@ -1,101 +1,140 @@
 
         
-        
-if __name__ == '__main__':
-    SalesRanker.run()
-
+            def setUp(self):
+        from acme.errors import MissingNonce
+        self.response = mock.MagicMock(headers={})
+        self.response.request.method = 'FOO'
+        self.error = MissingNonce(self.response)
+    
+    # The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+#html_favicon = None
+    
+        @mock.patch('certbot_apache.display_ops.display_util')
+    @certbot_util.patch_get_utility()
+    @mock.patch('certbot_apache.display_ops.logger')
+    def test_small_display(self, mock_logger, mock_util, mock_display_util):
+        mock_display_util.WIDTH = 20
+        mock_util().menu.return_value = (display_util.OK, 0)
+        self._call(self.vhosts)
+    
+            if record_contents:
+            # We need to remove old records in the same request
+            data['deletions'] = [
+                {
+                    'kind': 'dns#resourceRecordSet',
+                    'type': 'TXT',
+                    'name': record_name + '.',
+                    'rrdatas': record_contents,
+                    'ttl': record_ttl,
+                },
+            ]
+    
+    The path to this file can be provided interactively or using the
+``--dns-rfc2136-credentials`` command-line argument. Certbot records the
+path to this file for use during renewal, but does not store the file's contents.
+    
+    # These benchmarks are delicate.  They hit various fast-paths in the gen
+# machinery in order to stay synchronous so we don't need an IOLoop.
+# This removes noise from the results, but it's easy to change things
+# in a way that completely invalidates the results.
+    
+    '''Implementation of platform-specific functionality.
+    
+        def test_asyncio_adapter(self):
+        # This test demonstrates that when using the asyncio coroutine
+        # runner (i.e. run_until_complete), the to_asyncio_future
+        # adapter is needed. No adapter is needed in the other direction,
+        # as demonstrated by other tests in the package.
+        @gen.coroutine
+        def tornado_coroutine():
+            yield gen.moment
+            raise gen.Return(42)
     
     
-def history_not_changed(proc, TIMEOUT):
-    '''Ensures that history not changed.'''
-    proc.send('\033[A')
-    assert proc.expect([TIMEOUT, u'fuck'])
+class GoogleLoginHandler(RequestHandler, GoogleOAuth2Mixin):
+    def initialize(self, test):
+        self.test = test
+        self._OAUTH_REDIRECT_URI = test.get_url('/client/login')
+        self._OAUTH_AUTHORIZE_URL = test.get_url('/google/oauth2/authorize')
+        self._OAUTH_ACCESS_TOKEN_URL = test.get_url('/google/oauth2/token')
+    
+    		print('Test: delete')
+		myTree.delete(5)
+		assert_equal(myTree.treeIsEmpty(), True)
+		
+		print('Test: more complex deletions')
+		[myTree.insert(x) for x in range(1, 5)]
+		myTree.delete(2)
+		assert_equal(myTree.root.rightChild.data, 3)
+		print('Test: delete invalid value')
+		assert_equal(myTree.delete(100), False)
     
     
-@pytest.mark.functional
-def test_how_to_configure_alias(proc, TIMEOUT):
-    proc.sendline('unset -f fuck')
-    how_to_configure(proc, TIMEOUT)
-
-    
-    match_output = '''
-Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
-Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
-Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
-Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
-Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
-Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
-Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
-Fetched 89.2 kB in 0s (122 kB/s)
-Reading package lists... Done
-Building dependency tree
-Reading state information... Done
-8 packages can be upgraded. Run 'apt list --upgradable' to see them.
-'''
-    
-    
-def test_match():
-    command = Command('brew install sshfs', output)
-    assert match(command)
-    
-    
-def to_native_string(string, encoding='ascii'):
-    '''Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
+def text_justification(words, max_width):
     '''
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        if is_py2:
-            out = string.encode(encoding)
+    :type words: list
+    :type max_width: int
+    :rtype: list
+    '''
+    ret = []  # return value
+    row_len = 0  # current length of strs in a row
+    row_words = []  # current words in a row
+    index = 0  # the index of current word in words
+    is_first_word = True  # is current word the first in a row
+    while index < len(words):
+        while row_len <= max_width and index < len(words):
+            if len(words[index]) > max_width:
+                raise ValueError('there exists word whose length is larger than max_width')
+            tmp = row_len
+            row_words.append(words[index])
+            tmp += len(words[index])
+            if not is_first_word:
+                tmp += 1  # except for the first word, each word should have at least a ' ' before it.
+            if tmp > max_width:
+                row_words.pop()
+                break
+            row_len = tmp
+            index += 1
+            is_first_word = False
+        # here we have already got a row of str , then we should supplement enough ' ' to make sure the length is max_width.
+        row = ''
+        # if the row is the last
+        if index == len(words):
+            for word in row_words:
+                row += (word + ' ')
+            row = row[:-1]
+            row += ' ' * (max_width - len(row))
+        # not the last row and more than one word
+        elif len(row_words) != 1:
+            space_num = max_width - row_len
+            space_num_of_each_interval = space_num // (len(row_words) - 1)
+            space_num_rest = space_num - space_num_of_each_interval * (len(row_words) - 1)
+            for j in range(len(row_words)):
+                row += row_words[j]
+                if j != len(row_words) - 1:
+                    row += ' ' * (1 + space_num_of_each_interval)
+                if space_num_rest > 0:
+                    row += ' '
+                    space_num_rest -= 1
+        # row with only one word
         else:
-            out = string.decode(encoding)
-    
-        def test_repr(self):
-        assert repr(self.lookup_dict) == '<lookup 'test'>'
-    
-    if is_py2:
-    from urllib import (
-        quote, unquote, quote_plus, unquote_plus, urlencode, getproxies,
-        proxy_bypass, proxy_bypass_environment, getproxies_environment)
-    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urldefrag
-    from urllib2 import parse_http_list
-    import cookielib
-    from Cookie import Morsel
-    from StringIO import StringIO
-    from collections import Callable, Mapping, MutableMapping, OrderedDict
-    
-        Provide both the name and the version of the Python implementation
-    currently running. For example, on CPython 2.7.5 it will return
-    {'name': 'CPython', 'version': '2.7.5'}.
-    
-        def test_text_response(self):
-        '''the text_response_server sends the given text'''
-        server = Server.text_response_server(
-            'HTTP/1.1 200 OK\r\n' +
-            'Content-Length: 6\r\n' +
-            '\r\nroflol'
-        )
-    
-        def send(self, request, **kwargs):
-        '''Send a given PreparedRequest.
-    
-    intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'urllib3': ('https://urllib3.readthedocs.io/en/latest', None),
-}
+            row += row_words[0]
+            row += ' ' * (max_width - len(row))
+        ret.append(row)
+        # after a row , reset those value
+        row_len = 0
+        row_words = []
+        is_first_word = True
+    return ret
 
     
-    # 因为 'a', 'aa', 'aaa' 中都只含有 '<a' ，所以它们直积上都只是 '<a'
-print(model.wv['a'])
-print(model.wv['aa'])
-print(model.wv['aaa'])
-print(model.wv['<a'])
-'''
-[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
-[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
-[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
-[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
-'''
-print()
+        def save_tree(self, tree):
+        '''
+        Generate and save tree code to file
+        :param tree:
+        :return:
+        '''
+        signs = []
+        tree_code = ''
