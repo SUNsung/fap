518dad2e34fc7092d64b80c7345a5cfbeb4d89f8
SUNsung <weatherius@gmail.com>
2019-08-01 20:55:24 +0300

    Rewrite [56CYzaP6hRiH.56BKdED4OtAeE56T7ukFRZZtU256N48.cpp] File in: 2019-08-01 20:55:24

--- cccccc/56CYzaP6hRiH.56BKdED4OtAeE56T7ukFRZZtU256N48.cpp
+++ cccccc/56CYzaP6hRiH.56BKdED4OtAeE56T7ukFRZZtU256N48.cpp
@@ -1,266 +1,212 @@
 
         
-        REGISTER_CPU_OPERATOR(
-    MergeSingleScalarFeatureTensors,
-    MergeSingleScalarFeatureTensorsOp<CPUContext>);
-OPERATOR_SCHEMA(MergeSingleScalarFeatureTensors)
-    .SetDoc(
-        'Merge given single-feature tensors with scalar features into one '
-        'multi-feature tensor.' +
-        doc)
-    .NumInputs([](int n) { return n >= 2 && n % 2 == 0; })
-    .NumOutputs(3)
-    .Input(0, 'in1', '')
-    .Input(1, 'in1_presence', '.presence')
-    .Output(0, 'out_lengths', '.lengths')
-    .Output(1, 'out_keys', '.keys')
-    .Output(2, 'out_values', '.values')
-    .Arg('feature_ids', 'feature ids');
-    
-    OPERATOR_SCHEMA(FindDuplicateElements)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-The *FindDuplicateElements* op takes a single 1-D tensor *data* as input and returns a single 1-D output tensor *indices*. The output tensor contains the indices of the duplicate elements of the input, excluding the first occurrences. If all elements of *data* are unique, *indices* will be empty.
-    
-              const int dkernel_h = dilation_h * (kernel_h - 1) + 1;
-          const int dkernel_w = dilation_w * (kernel_w - 1) + 1;
-          CAFFE_ENFORCE(H >= dkernel_h);
-          CAFFE_ENFORCE(W >= dkernel_w);
-          const int out_h = (H + 2 * pad - dkernel_h) / stride_h + 1;
-          const int out_w = (W + 2 * pad - dkernel_w) / stride_w + 1;
-    
-    namespace grpc {
-    }
-    
-    
-    {}  // namespace grpc
-    
-    
-    {}  // namespace grpc
-
-    
-    #include 'src/cpp/server/dynamic_thread_pool.h'
-    
-    
-    {
-    {}  // namespace load_reporter
-}  // namespace grpc
-    
-    
-    {
-    {}  // namespace load_reporter
-}  // namespace grpc
-    
-    #endif  // GPR_WINDOWS
-
-    
-    namespace grpc {
-    }
-    
-    #include <stddef.h>
-#include <limits.h>
-#include 'GlobalParams.h'
-#include 'Object.h'
-#include 'Array.h'
-#include 'Dict.h'
-#include 'XRef.h'
-#include 'Link.h'
-#include 'OutputDev.h'
-#ifndef PDF_PARSER_ONLY
-#include 'Gfx.h'
-#include 'GfxState.h'
-#include 'Annot.h'
-#include 'TextOutputDev.h'
-#include 'Form.h'
-#endif
-#include 'Error.h'
-#include 'Page.h'
-#include 'Catalog.h'
-#include 'Form.h'
-    
-    //------------------------------------------------------------------------
-// Parser
-//------------------------------------------------------------------------
-    
-      //----- get info about output device
-    
-      if (obj->dictLookup('D', &tmp)->isArray()) {
-    Array * dim = tmp.getArray();
-    
-    if (dim->getLength() >= 2) {
-      Object dd;
-      if (dim->get(0, &dd)->isInt()) {
-	width = dd.getInt();
-      }
-      dd.free();
-      if (dim->get(1, &dd)->isInt()) {
-	height = dd.getInt();
-      }
-      dd.free();
-    }
-  }
-  tmp.free();
-    
-      GooString* getContentType() { return contentType; }
-  GooString* getFileName() { return fileName; }
-    
-      // Create authorization data for the specified owner and user
-  // passwords.  If the security handler doesn't support 'batch' mode,
-  // this function should return NULL.
-  virtual void *makeAuthData(GooString *ownerPassword,
-			     GooString *userPassword) = 0;
-    
-    Stream *Sound::getStream()
-{
-  return streamObj->getStream();
-}
-    
-    void SplashOutputDev::drawType3Glyph(T3FontCache *t3Font,
-				     T3FontCacheTag * /*tag*/, Guchar *data) {
-  SplashGlyphBitmap glyph;
-    }
-    
-    #include 'StdinCachedFile.h'
-    
-    class StdinCacheLoader : public CachedFileLoader {
-    }
-    
-    
-    {  obj.initNull();
-  CachedFile *cachedFile = new CachedFile(new StdinCacheLoader(), NULL);
-  return new PDFDoc(new CachedFileStream(cachedFile, 0, gFalse,
-                                         cachedFile->getLength(), &obj),
-                    ownerPassword, userPassword);
-}
-    
-        // Render command lists
-    for (int n = 0; n < draw_data->CmdListsCount; n++)
-    {
-        const ImDrawList* cmd_list = draw_data->CmdLists[n];
-    }
-    
-            ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-        ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-    
-    {        // Rendering
-        ImGui::Render();
-        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(window);
-    }
-    
-    // All the ImGui_ImplVulkanH_XXX structures/functions are optional helpers used by the demo. 
-// Your real engine/app may not use them.
-static void SetupVulkanWindow(ImGui_ImplVulkanH_Window* wd, VkSurfaceKHR surface, int width, int height)
-{
-    wd->Surface = surface;
-    }
-    
-    // InitXXX function with 'install_callbacks=true': install GLFW callbacks. They will call user's previously installed callbacks, if any.
-// InitXXX function with 'install_callbacks=false': do not install GLFW callbacks. You will need to call them yourself from your own GLFW callbacks.
-IMGUI_IMPL_API void     ImGui_ImplGlfw_MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_CharCallback(GLFWwindow* window, unsigned int c);
-
-    
-    
-    {        // see if our best match qualifies
-        if (best < 3) { // fast path literals
-            ++q;
-        } else if (best > 2  &&  best <= 0x80    &&  dist <= 0x100) {
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            stb_out(0x80 + best-1);
-            stb_out(dist-1);
-        } else if (best > 5  &&  best <= 0x100   &&  dist <= 0x4000) {
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            stb_out2(0x4000 + dist-1);
-            stb_out(best-1);
-        } else if (best > 7  &&  best <= 0x100   &&  dist <= 0x80000) {
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            stb_out3(0x180000 + dist-1);
-            stb_out(best-1);
-        } else if (best > 8  &&  best <= 0x10000 &&  dist <= 0x80000) {
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            stb_out3(0x100000 + dist-1);
-            stb_out2(best-1);
-        } else if (best > 9                      &&  dist <= 0x1000000) {
-            if (best > 65536) best = 65536;
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            if (best <= 0x100) {
-                stb_out(0x06);
-                stb_out3(dist-1);
-                stb_out(best-1);
-            } else {
-                stb_out(0x04);
-                stb_out3(dist-1);
-                stb_out2(best-1);
-            }
-        } else {  // fallback literals if no match was a balanced tradeoff
-            ++q;
-        }
-    }
-    
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'GLuint' OpenGL texture identifier as void*/ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-//  [x] Renderer: Desktop GL only: Support for large meshes (64k+ vertices) with 16-bits indices.
-    
-    
-    {    CreateRenderTarget();
-    return true;
-}
-    
-    DHTReplaceNodeTask::~DHTReplaceNodeTask() = default;
-    
-    #endif // D_DHT_RESPONSE_MESSAGE_H
-
-    
-      void moveBucketHead(const std::shared_ptr<DHTNode>& node);
-    
-      std::shared_ptr<DHTNode> localNode_;
-    
-    namespace aria2 {
-    }
-    
-    DHTTaskExecutor::DHTTaskExecutor(int numConcurrent)
-    : numConcurrent_(numConcurrent)
-{
-}
-    
-    
-    {  void setTimeout(std::chrono::seconds timeout)
-  {
-    timeout_ = std::move(timeout);
-  }
+        
+    {  tensorflow::mutex mu_;
+  absl::flat_hash_map<se::Stream*, CusolverContext> contexts_ GUARDED_BY(mu_);
 };
     
-    void DHTTaskQueueImpl::addPeriodicTask1(const std::shared_ptr<DHTTask>& task)
-{
-  periodicTaskQueue1_.addTask(task);
+      Status ExecuteOnStream(const ExecuteParams& params) override;
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    #ifndef TENSORFLOW_COMPILER_XLA_SERVICE_GPU_OUTFEED_THUNK_H_
+#define TENSORFLOW_COMPILER_XLA_SERVICE_GPU_OUTFEED_THUNK_H_
+    
+    #include 'absl/strings/str_cat.h'
+#include 'absl/strings/str_format.h'
+#include 'tensorflow/compiler/xla/service/gpu/hlo_execution_profiler.h'
+#include 'tensorflow/compiler/xla/types.h'
+#include 'tensorflow/compiler/xla/util.h'
+#include 'tensorflow/compiler/xla/xla_data.pb.h'
+#include 'tensorflow/core/platform/logging.h'
+#include 'tensorflow/core/platform/stream_executor_no_cuda.h'
+#include 'tensorflow/stream_executor/blas.h'
+#include 'tensorflow/stream_executor/device_memory.h'
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    #include <grpc/support/port_platform.h>
+    
+    
+    {
+    { private:
+  std::shared_ptr<grpc::Channel> CreateChannelWithInterceptors(
+      const string& target, const grpc::ChannelArguments& args,
+      std::vector<
+          std::unique_ptr<experimental::ClientInterceptorFactoryInterface>>
+          interceptor_creators) override {
+    grpc_channel_args channel_args;
+    args.SetChannelArgs(&channel_args);
+    return CreateChannelInternal(
+        '',
+        grpc_cronet_secure_channel_create(engine_, target.c_str(),
+                                          &channel_args, nullptr),
+        std::move(interceptor_creators));
+  }
+  void* engine_;
+};
+}  // namespace grpc
+namespace grpc_impl {
+std::shared_ptr<ChannelCredentials> CronetChannelCredentials(void* engine) {
+  return std::shared_ptr<ChannelCredentials>(
+      new grpc::CronetChannelCredentialsImpl(engine));
 }
+}  // namespace grpc_impl
+
     
-    public:
-  DHTTokenTracker();
+      void SendRpc(int num_rpcs) {
+    for (int i = 0; i < num_rpcs; i++) {
+      EchoRequest send_request;
+      EchoRequest recv_request;
+      EchoResponse send_response;
+      EchoResponse recv_response;
+      Status recv_status;
+    }
+    }
     
-    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
-                                             std::chrono::seconds interval)
-    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
+    template class PlusNode<float>;
+template class PlusNode<double>;
+template class PlusNode<half>;
+    
+    
+    {
+    {                fstream.GetMarker(FileMarker::fileMarkerEndSection, L'EMACKP');
+            }
+        }
+    private:
+       // helper function to save/load map<wstring, shared_ptr<Matrix<ElemType>> structure 
+       void SaveParameters(File& f, const map<wstring, shared_ptr<Matrix<ElemType>>>& parameters) const
+        {
+            // save sizeof(ElemType)
+            unsigned int size = sizeof(ElemType);
+            f << size;
+            // save number of pairs 
+            unsigned int numPairs = parameters.size();
+            f << numPairs;
+            for (auto& x : parameters)
+            {
+                f << x.first;
+                f << *x.second;
+            }
+            f.Flush();
+            return;
+        }
+       void LoadParameters(File& f, map<wstring, shared_ptr<Matrix<ElemType>>>& parameters, DEVICEID_TYPE deviceID)
+       {
+           unsigned int size = 0;
+           unsigned int pair = 0;
+           f >> size;
+           f >> pair;
+           if (size != sizeof(ElemType))
+           {
+               LogicError('Mismatched ElemType in loading BlockMomentumSGD checkpoint. Expecting %s, while loading element size=%d\n',
+                   sizeof(ElemType) == 4 ? 'float' : 'double',
+                   size
+                   );
+           }
+           parameters.clear();
+           for (size_t i = 0; i < pair; i++)
+           {
+               wstring name;
+               f >> name;
+               shared_ptr<Matrix<ElemType>> mat = make_shared<Matrix<ElemType>>(deviceID);
+               f >> *mat;
+               parameters[name] = mat;
+           }
+       }
+    
+    #include 'ComputationNode.h'
+#include 'InputAndParamNodes.h'
+#include 'LinearAlgebraNodes.h'
+#include 'NonlinearityNodes.h'
+#include 'ConvolutionalNodes.h'
+#include 'RecurrentNodes.h'
+#include 'PreComputeNodes.h'
+    
+    struct IPreComputeNode
 {
-}
+    // check whether node has already undergone precomputation
+    virtual bool HasComputed() const = 0;
+    // call this with 'false' at start and with 'true' at end
+    // This is used for resetting and updating from accumulators.
+    virtual void MarkComputed(const bool hasComputed) = 0;
+};
     
-    bool DHTUnknownMessage::isReply() const { return false; }
     
-    bool DNSCache::CacheEntry::operator==(const CacheEntry& e) const
+    {            if (m_useAsyncAggregation)
+            {
+                std::unique_ptr<MatrixComputeStreamEvent> mainStreamSyncEvent(MatrixComputeStreamEvent::Create(deviceId));
+                mainStreamSyncEvent->SynchronizeDataTransferFetchStreamWithEvent<float>();
+            }
+        }
+    
+    BOOST_AUTO_TEST_CASE(EvalScalarTimesDualOutputTest)
 {
-  return hostname_ == e.hostname_ && port_ == e.port_;
-}
+    std::string modelDefinition =
+        'deviceId = -1 \n'
+        'precision = \'float\' \n'
+        'traceLevel = 1 \n'
+        'run=NDLNetworkBuilder \n'
+        'NDLNetworkBuilder=[ \n'
+        'i1 = Input(1) \n'
+        'i2 = Input(1) \n'
+        'o1 = Times(Constant(3), i1, tag=\'output\') \n'
+        'o2 = Times(Constant(5), i1, tag=\'output\') \n'
+        'FeatureNodes = (i1) \n'
+        '] \n';
+    }
     
-      struct CacheEntry {
-    std::string hostname_;
-    uint16_t port_;
-    std::vector<AddrEntry> addrEntries_;
+    namespace {
+SequenceNumber ReadRecords(
+    std::unique_ptr<TransactionLogIterator>& iter,
+    int& count) {
+  count = 0;
+  SequenceNumber lastSequence = 0;
+  BatchResult res;
+  while (iter->Valid()) {
+    res = iter->GetBatch();
+    EXPECT_TRUE(res.sequence > lastSequence);
+    ++count;
+    lastSequence = res.sequence;
+    EXPECT_OK(iter->status());
+    iter->Next();
+  }
+  return res.sequence;
+}
+    }
+    
+    #if !defined(ROCKSDB_LITE) && !defined(OS_WIN)
+    
+      // put and get from non-default column family
+  s = db->Put(WriteOptions(), handles[1], Slice('key'), Slice('value'));
+  assert(s.ok());
+  std::string value;
+  s = db->Get(ReadOptions(), handles[1], Slice('key'), &value);
+  assert(s.ok());
+    
+      // Write a key in this transaction
+  txn->Put('abc', 'def');
+    
+      // initialize column families options
+  std::unique_ptr<CompactionFilter> compaction_filter;
+  compaction_filter.reset(new DummyCompactionFilter());
+  cf_descs[0].options.table_factory.reset(NewBlockBasedTableFactory(bbt_opts));
+  cf_descs[0].options.compaction_filter = compaction_filter.get();
+  cf_descs[1].options.table_factory.reset(NewBlockBasedTableFactory(bbt_opts));
+    
+    int main() {
+  // open DB
+  Options options;
+  TransactionDBOptions txn_db_options;
+  options.create_if_missing = true;
+  TransactionDB* txn_db;
     }
