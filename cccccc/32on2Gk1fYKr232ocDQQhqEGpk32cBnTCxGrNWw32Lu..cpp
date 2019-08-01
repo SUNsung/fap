0068d51ccde0ae073a3418caf7713a539c9460d8
SUNsung <weatherius@gmail.com>
2019-08-01 07:27:59 +0300

    Rewrite [32on2Gk1fYKr232ocDQQhqEGpk32cBnTCxGrNWw32Lu..cpp] File in: 2019-08-01 07:27:59

--- cccccc/32on2Gk1fYKr232ocDQQhqEGpk32cBnTCxGrNWw32Lu..cpp
+++ cccccc/32on2Gk1fYKr232ocDQQhqEGpk32cBnTCxGrNWw32Lu..cpp
@@ -1,235 +1,111 @@
 
         
-        #include <carotene/definitions.hpp>
-#include <stdint.h>
-#include <cstddef>
-    
-                vst1q_s32(dsta + x, wa);
-            vst1q_s32(dstb + x, wb);
-            vst1q_s32(dstc + x, wc);
-        }
-        if(x < colsn)
-        {
-            x = colsn-4;
-            goto box3x3s32_horiz_ll;
-        }
-    }
-    else
-    {
-        //horizontal convolution (last 2 lines)
-        s32* dstb = internal::getRowPtr(dstBase, dstStride, i-1);
-        size_t x = 0;
-        for (; x <= colsn - 4; x += 4)
-        {
-            internal::prefetch(laneA + x + cn);
-            internal::prefetch(laneB + x + cn);
-box3x3s32_horiz_last2:
-            int32x4_t lane0a = vld1q_s32(laneA + x - cn);
-            int32x4_t lane2a = vld1q_s32(laneA + x + cn);
-            int32x4_t lane1a = vld1q_s32(laneA + x);
+        #include 'base/callback.h'
+#include 'base/location.h'
+#include 'base/single_thread_task_runner.h'
+#include 'uv.h'  // NOLINT(build/include)
     
     
-    {        if (!m[-1])         CANNY_PUSH(m - 1);
-        if (!m[1])          CANNY_PUSH(m + 1);
-        if (!m[-mapstep-1]) CANNY_PUSH(m - mapstep - 1);
-        if (!m[-mapstep])   CANNY_PUSH(m - mapstep);
-        if (!m[-mapstep+1]) CANNY_PUSH(m - mapstep + 1);
-        if (!m[mapstep-1])  CANNY_PUSH(m + mapstep - 1);
-        if (!m[mapstep])    CANNY_PUSH(m + mapstep);
-        if (!m[mapstep+1])  CANNY_PUSH(m + mapstep + 1);
-    }
+    {    manager->GetCookieList(url, options, std::move(callback));
+  }
     
-    #define SPLIT_QUAD(sgn, bits, n) { \
-                                     internal::prefetch(src + sj); \
-                                     vec128 v_src = vld##n##q_##sgn##bits(src + sj); \
-                                     FILL_LINES##n(VST1Q, sgn##bits) \
-                                 }
     
-                for (; j < size.width; j++)
-            {
-                dst[j] = internal::saturate_cast<u8>((src[j] >> shift));
-            }
-        }
-        else // CONVERT_POLICY_WRAP
-        {
-            for (; j < roiw16; j += 16)
-            {
-                internal::prefetch(src + j);
-                int16x8_t v_src0 = vshrq_n_s16(vld1q_s16(src + j), shift),
-                          v_src1 = vshrq_n_s16(vld1q_s16(src + j + 8), shift);
-                int8x16_t v_dst = vcombine_s8(vmovn_s16(v_src0),
-                                              vmovn_s16(v_src1));
-                vst1q_u8(dst + j, vreinterpretq_u8_s8(v_dst));
-            }
-            for (; j < roiw8; j += 8)
-            {
-                int16x8_t v_src = vshrq_n_s16(vld1q_s16(src + j), shift);
-                vst1_u8(dst + j, vreinterpret_u8_s8(vmovn_s16(v_src)));
-            }
-    
-    #define DOT_FLOAT_BLOCKSIZE (1 << 13)
-    f64 result = 0.0;
-    for (size_t row = 0; row < size.height; ++row)
-    {
-        const f32 * src0 = internal::getRowPtr(src0Base, src0Stride, row);
-        const f32 * src1 = internal::getRowPtr(src1Base, src1Stride, row);
-    }
-    
-            int16x8_t tcurr1 = vmovq_n_s16(0x0);
-        int16x8_t tnext1 = vmovq_n_s16(0x0);
-        int16x8_t tprev1 = vmovq_n_s16(0x0);
-        int16x8_t tpprev1 = vmovq_n_s16(0x0);
-        int16x8_t tppprev1 = vmovq_n_s16(0x0);
-    
-    #endif  // TESSERACT_CCMAIN_OSDETECT_H_
-
-    
-    // Parses the given box file string into a page_number, utf8_str, and
-// bounding_box. Returns true on a successful parse.
-bool ParseBoxFileStr(const char* boxfile_str, int* page_number,
-                     STRING* utf8_str, TBOX* bounding_box);
-    
-    ELISTIZE(PARA)
-    
-    Determining whether a character is CURRENTLY rejected depends on implicit
-understanding of the SEQUENCE of possible calls. The flags are defined and
-grouped in the REJ_FLAGS enum. These groupings are used in determining a
-characters CURRENT rejection status. Basically, a character is ACCEPTED if
-    
-    #include 'errcode.h'
-    
-    #ifndef TESSERACT_CCUTIL_KDPAIR_H_
-#define TESSERACT_CCUTIL_KDPAIR_H_
-    
-    #if GTEST_OS_WINDOWS_MOBILE
-  // Windows CE does not have the 'ANSI' versions of Win32 APIs. To be
-  // able to pass strings to Win32 APIs on CE we need to convert them
-  // to 'Unicode', UTF-16.
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10>
-struct Types10 {
-  typedef T1 Head;
-  typedef Types9<T2, T3, T4, T5, T6, T7, T8, T9, T10> Tail;
+    {  DISALLOW_COPY_AND_ASSIGN(Cookies);
 };
     
+      base::DictionaryValue request;
+  int request_id = ++previous_request_id_;
+  pending_requests_.emplace(request_id, std::move(promise));
+  request.SetInteger('id', request_id);
+  request.SetString('method', method);
+  if (!command_params.empty())
+    request.Set('params',
+                base::Value::ToUniquePtrValue(command_params.Clone()));
     
-    {  // <TechnicalDetails>
-  //
-  // EXPECT_EQ(expected, actual) is the same as
-  //
-  //   EXPECT_TRUE((expected) == (actual))
-  //
-  // except that it will print both the expected value and the actual
-  // value when the assertion fails.  This is very helpful for
-  // debugging.  Therefore in this case EXPECT_EQ is preferred.
-  //
-  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
-  // and is thus more general.
-  //
-  // </TechnicalDetails>
+    #ifndef SHELL_BROWSER_API_ATOM_API_DOWNLOAD_ITEM_H_
+#define SHELL_BROWSER_API_ATOM_API_DOWNLOAD_ITEM_H_
+    
+    #include <map>
+#include <string>
+#include <vector>
+    
+    
+    {}  // namespace
+    
+    
+    {}  // namespace api
+    
+    namespace api {
+    }
+    
+    /** @brief The function renders on two NV12 planes passed drawing primitivies
+    
+        VideoWriter writer;
+    EXPECT_NO_THROW(writer.open(pipeline, apiPref, 0/*fourcc*/, 30/*fps*/, frame_size, true));
+    EXPECT_FALSE(writer.isOpened());
+    // no frames
+    EXPECT_NO_THROW(writer.release());
+    
+            __host__ __device__ __forceinline__ thresh_to_zero_func() {}
+       __host__  __device__ __forceinline__ thresh_to_zero_func(const thresh_to_zero_func& other)
+            : thresh(other.thresh) {}
+    
+            TwilightShifted(int n) : ColorMap() {
+            init(n);
+        }
+    
+    #endif // OPENCV_GAPI_GTYPE_TRAITS_HPP
+
+    
+        Mat pts0_copy = pts0.clone();
+    Mat pts1_copy = pts1.clone();
+    
+    TEST(Imgcodecs_Tiff, decode_tile_remainder)
+{
+    /* see issue #3472 - dealing with tiled images where the tile size is
+     * not a multiple of image size.
+     * The tiled images were created with 'convert' from ImageMagick,
+     * using the command 'convert <input> -define tiff:tile-geometry=128x128 -depth [8|16] <output>
+     * Note that the conversion to 16 bits expands the range from 0-255 to 0-255*255,
+     * so the test converts back but rounding errors cause small differences.
+     */
+    const string root = cvtest::TS::ptr()->get_data_path();
+    cv::Mat img = imread(root + 'readwrite/non_tiled.tif',-1);
+    ASSERT_FALSE(img.empty());
+    ASSERT_TRUE(img.channels() == 3);
+    cv::Mat tiled8 = imread(root + 'readwrite/tiled_8.tif', -1);
+    ASSERT_FALSE(tiled8.empty());
+    ASSERT_PRED_FORMAT2(cvtest::MatComparator(0, 0), img, tiled8);
+    cv::Mat tiled16 = imread(root + 'readwrite/tiled_16.tif', -1);
+    ASSERT_FALSE(tiled16.empty());
+    ASSERT_TRUE(tiled16.elemSize() == 6);
+    tiled16.convertTo(tiled8, CV_8UC3, 1./256.);
+    ASSERT_PRED_FORMAT2(cvtest::MatComparator(2, 0), img, tiled8);
+    // What about 32, 64 bit?
 }
     
-     public:
-  // Clones a 0-terminated C string, allocating memory using new.
-  static const char* CloneCString(const char* a_c_string);
     
-        E* element = new E(old_head->element());
-    delete old_head;
-    
-    
-    {    Rational Sinh(Rational const& rat);
-    Rational Cosh(Rational const& rat);
-    Rational Tanh(Rational const& rat);
-    Rational ASinh(Rational const& rat);
-    Rational ACosh(Rational const& rat);
-    Rational ATanh(Rational const& rat);
+    {    // exception out_of_range.401
+    try
+    {
+        // try to write at a nonexisting key
+        object.at('the fast') = 'il rapido';
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
 }
 
     
-            bool IsZero() const;
+        // change an array element
+    j.at('/array/1'_json_pointer) = 21;
+    // output the changed array
+    std::cout << j['array'] << '\n';
     
-        class CalculatorHistory : public IHistoryDisplay
-    {
-    public:
-        CalculatorHistory(const size_t maxSize);
-        unsigned int AddToHistory(
-            _In_ std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> const& spTokens,
-            _In_ std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> const& spCommands,
-            std::wstring_view result);
-        std::vector<std::shared_ptr<HISTORYITEM>> const& GetHistory();
-        std::shared_ptr<HISTORYITEM> const& GetHistoryItem(unsigned int uIdx);
-        void ClearHistory();
-        unsigned int AddItem(_In_ std::shared_ptr<HISTORYITEM> const& spHistoryItem);
-        bool RemoveItem(unsigned int uIdx);
-        size_t MaxHistorySize() const
-        {
-            return m_maxHistorySize;
-        }
-        ~CalculatorHistory(void);
+    int main()
+{
+    // create JSON values
+    json object = {{'one', 1}, {'two', 2}};
+    json null;
     }
-    
-    TEST_F(ConfigTests, test_plugin_reconfigure) {
-  auto& rf = RegistryFactory::get();
-  // Add a configuration plugin (could be any plugin) that will react to
-  // config updates.
-  rf.registry('config_parser')
-      ->add('placebo', std::make_shared<PlaceboConfigParserPlugin>());
-    }
-    
-      Expected<int32_t, DatabaseError> getInt32(const std::string& domain,
-                                            const std::string& key) override;
-  Expected<std::string, DatabaseError> getString(
-      const std::string& domain, const std::string& key) override;
-    
-    ExpectedSuccess<DatabaseError> RocksdbDatabase::putString(
-    const std::string& domain,
-    const std::string& key,
-    const std::string& value) {
-  auto result = putRawBytes(domain, key, value);
-  if (result) {
-    return Success();
-  }
-  return result.takeError();
-}
-    
-    
-    {}
-    
-      /** @brief Virtual method which should implement custom logging.
-   *
-   *  LoggerPlugin::logString should be implemented by a subclass of
-   *  LoggerPlugin which needs to log a string in a custom way.
-   *
-   *  @return an instance of osquery::Status which indicates the success or
-   *  failure of the operation.
-   */
-  virtual Status logString(const std::string& s) = 0;
-    
-    PluginResponse tableRowsToPluginResponse(const TableRows& rows) {
-  PluginResponse result;
-  for (const auto& row : rows) {
-    result.push_back(static_cast<Row>(*row));
-  }
-  return result;
-}
-    
-      /**
-   * @brief Bind this plugin to an external plugin reference.
-   *
-   * Allow a specialized plugin type to act when an external plugin is
-   * registered (e.g., a TablePlugin will attach the table name).
-   *
-   * @param name The broadcasted name of the plugin.
-   * @param info The routing info for the owning extension.
-   */
-  static Status addExternal(const std::string& name,
-                            const PluginResponse& info) {
-    (void)name;
-    (void)info;
-    return Status::success();
-  }
-    
-      /// Given a query, return the list of scanned tables.
-  virtual Status getQueryTables(const std::string& query,
-                                std::vector<std::string>& tables) const = 0;
