5f8aa89bae48822306835123c8222ae6c6d34320
SUNsung <weatherius@gmail.com>
2019-08-01 03:12:22 +0300

    Rewrite [19FvKaO6LLaU19zsyiSmKNtB619jzjNPEFIKRw19Zso.rb] File in: 2019-08-01 03:12:22

--- ruby/19FvKaO6LLaU19zsyiSmKNtB619jzjNPEFIKRw19Zso.rb
+++ ruby/19FvKaO6LLaU19zsyiSmKNtB619jzjNPEFIKRw19Zso.rb
@@ -1,68 +1,11 @@
 
         
-                def representation_class
-          Representation::Issue
-        end
+              private
     
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
-    end
-  end
-end
-
-    
-    def await_condition &condition
-  start_time = Time.zone.now
-  until condition.call
-    return false if (Time.zone.now - start_time) > Capybara.default_max_wait_time
-    sleep 0.05
-  end
-  true
-end
-
-    
-    describe ConversationsController, :type => :controller do
-  describe '#index' do
-    before do
-      @person = alice.contacts.first.person
-      hash = {
-        :author => @person,
-        :participant_ids => [alice.person.id, @person.id],
-        :subject => 'not spam',
-        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
-      }
-      @conv1 = Conversation.create(hash)
-      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
-             .increase_unread(alice)
-      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
-             .increase_unread(alice)
-    
-        it 'returns a 404 for a post not visible to the user' do
-      sign_in eve
-      expect {
-        get :index, params: {post_id: @message.id}
-      }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-    
-          it_behaves_like 'on a visible post'
-    end
-    
-        it 'redirects to /stream for a mobile user' do
-      request.headers['X_MOBILE_DEVICE'] = true
-      post :create, params: {user: {remember_me: '0', username: @user.username, password: 'evankorth'}}
-      expect(response).to be_redirect
-      expect(response.location).to match /^#{stream_url}\??$/
-    end
-  end
-    
-          it 'raises an error' do
-        expect {
-          put :update, params: {id: 42, post_id: @status.id}, format: :js
-        }.to raise_error ActiveRecord::RecordNotFound
+          def html?(headers)
+        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
+        options[:html_types].include? header.last[/^\w+\/\w+/]
       end
-    
-      # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+    end
+  end
+end
