2889c85b4e52ef8ee727231778387bc9a84ab75d
SUNsung <weatherius@gmail.com>
2019-08-01 14:19:28 +0300

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2019-08-01 14:19:27

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,100 +1,202 @@
 
         
-            # we only want to be able to train generation for the combined model
-    discriminator.trainable = False
-    fake, aux = discriminator(fake)
-    combined = Model([latent, image_class], [fake, aux])
+            def _add_inbound_node(self, input_tensors, output_tensors,
+                          input_masks, output_masks,
+                          input_shapes, output_shapes, arguments=None):
+        '''Internal method to create an inbound node for the layer.
+    
+    model = Sequential()
+model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Dropout(0.25))
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(MaxPooling1D(pool_size=pool_size))
+model.add(LSTM(lstm_output_size))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
     
     
-def gradient_ascent(x, iterations, step, max_loss=None):
-    for i in range(iterations):
-        loss_value, grad_values = eval_loss_and_grads(x)
-        if max_loss is not None and loss_value > max_loss:
-            break
-        print('..Loss value at', i, ':', loss_value)
-        x += step * grad_values
-    return x
-    
-        # Note on using statefulness in RNNs
-        You can set RNN layers to be 'stateful', which means that the states
-        computed for the samples in one batch will be reused as initial states
-        for the samples in the next batch. This assumes a one-to-one mapping
-        between samples in different successive batches.
+def test_conv_output_length():
+    assert conv_utils.conv_output_length(None, 7, 'same', 1) is None
+    assert conv_utils.conv_output_length(224, 7, 'same', 1) == 224
+    assert conv_utils.conv_output_length(224, 7, 'same', 2) == 112
+    assert conv_utils.conv_output_length(32, 5, 'valid', 1) == 28
+    assert conv_utils.conv_output_length(32, 5, 'valid', 2) == 14
+    assert conv_utils.conv_output_length(32, 5, 'causal', 1) == 32
+    assert conv_utils.conv_output_length(32, 5, 'causal', 2) == 16
+    assert conv_utils.conv_output_length(32, 5, 'full', 1) == 36
+    assert conv_utils.conv_output_length(32, 5, 'full', 2) == 18
     
     
-def set_epsilon(e):
-    '''Sets the value of the fuzz factor used in numeric expressions.
+def serialize(metric):
+    return serialize_keras_object(metric)
     
-        def on_train_end(self, logs=None):
-        '''Called at the end of training.
+        def get_config(self):
+        config = {'input_dim': self.input_dim,
+                  'output_dim': self.output_dim,
+                  'embeddings_initializer':
+                      initializers.serialize(self.embeddings_initializer),
+                  'embeddings_regularizer':
+                      regularizers.serialize(self.embeddings_regularizer),
+                  'activity_regularizer':
+                      regularizers.serialize(self.activity_regularizer),
+                  'embeddings_constraint':
+                      constraints.serialize(self.embeddings_constraint),
+                  'mask_zero': self.mask_zero,
+                  'input_length': self.input_length}
+        base_config = super(Embedding, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
     
-        def compute_mask(self, inputs, mask=None):
-        if not self.mask_zero:
-            return None
-        output_mask = K.not_equal(inputs, 0)
-        return output_mask
     
-        # Arguments
-        model: target model for the conversion.
+class TestSchemes:
+    
+    # sdist
+if 'bdist_wheel' not in sys.argv:
+    try:
+        # noinspection PyUnresolvedReferences
+        import argparse
+    except ImportError:
+        install_requires.append('argparse>=1.2.1')
+    
+        '''
+    is_windows = is_windows
+    config_dir = DEFAULT_CONFIG_DIR
+    stdin = sys.stdin
+    stdin_isatty = stdin.isatty()
+    stdin_encoding = None
+    stdout = sys.stdout
+    stdout_isatty = stdout.isatty()
+    stdout_encoding = None
+    stderr = sys.stderr
+    stderr_isatty = stderr.isatty()
+    colors = 256
+    if not is_windows:
+        if curses:
+            try:
+                curses.setupterm()
+                colors = curses.tigetnum('colors')
+            except curses.error:
+                pass
+    else:
+        # noinspection PyUnresolvedReferences
+        import colorama.initialise
+        stdout = colorama.initialise.wrap_stream(
+            stdout, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        stderr = colorama.initialise.wrap_stream(
+            stderr, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        del colorama
+    
+    
+class BaseStream(object):
+    '''Base HTTP message output stream class.'''
+    
+    
+def basic_auth(header=BASIC_AUTH_HEADER_VALUE):
+    
+        config['implicit_content_type'] = 'form'
+    config.save()
+    config.load()
+    assert 'implicit_content_type' not in config
+    assert config['default_options'] == ['--form']
+    
+    
+filenames = list(rst_filenames())
+assert filenames
+    
+    
+def printy(ret):
     '''
-    # Note: SeparableConvolution not included
-    # since only supported by TF.
-    conv_classes = {
-        'Conv1D',
-        'Conv2D',
-        'Conv3D',
-        'Conv2DTranspose',
+    
+            self.canvas = tk.Canvas(self, bd=0, highlightthickness=0)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    
+    logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+    
+    import cv2
+import numpy as np
+    
+    import logging
+import os
+import sys
+    
+    
+_DEFAULTS = {
+    'confidence': {
+        'default': 50,
+        'info': 'The confidence level at which the detector has succesfully found a face.\n'
+                'Higher levels will be more discriminating, lower levels will have more false '
+                'positives.',
+        'datatype': int,
+        'rounding': 5,
+        'min_max': (25, 100),
+        'choices': [],
+        'gui_radio': False,
+        'fixed': True,
     }
-    to_assign = []
-    for layer in model.layers:
-        if layer.__class__.__name__ in conv_classes:
-            original_kernel = K.get_value(layer.kernel)
-            converted_kernel = convert_kernel(original_kernel)
-            to_assign.append((layer.kernel, converted_kernel))
-    K.batch_set_value(to_assign)
-    
-            with self.tasks():
-            run_deletion(deletion.id)
-    
-            if request.method != 'POST':
-            return render_to_response('sentry/auth-link-identity.html', request=request, context={
-                'organization': organization,
-                'provider': integration.get_provider(),
-            })
+}
+
     
     
-class DebugErrorPageEmbedView(View):
-    def _get_project_key(self):
-        return ProjectKey.objects.filter(
-            project=settings.SENTRY_PROJECT,
-        )[0]
+_DEFAULTS = {
+    'lowmem': {
+        'default': False,
+        'info': 'Lower memory mode. Set to 'True' if having issues with VRAM useage.\n'
+                'NB: Models with a changed lowmem mode are not compatible with each other.',
+        'datatype': bool,
+        'rounding': None,
+        'min_max': None,
+        'choices': [],
+        'gui_radio': False,
+        'fixed': True,
+    },
+    'coverage': {
+        'default': 62.5,
+        'info': 'How much of the extracted image to train on. Generally the model is optimized'
+                '\nto the default value. Sensible values to use are:'
+                '\n\t62.5%% spans from eyebrow to eyebrow.'
+                '\n\t75.0%% spans from temple to temple.'
+                '\n\t87.5%% spans from ear to ear.'
+                '\n\t100.0%% is a mugshot.',
+        'datatype': float,
+        'rounding': 1,
+        'min_max': (62.5, 100.0),
+        'choices': [],
+        'gui_radio': False,
+        'fixed': True,
+    },
+}
+
     
-            # Adding model 'TagValue'
-        db.create_table(u'tagstore_tagvalue', (
-            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
-            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
-            ('environment_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)),
-            ('_key', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                to=orm['tagstore.TagKey'], db_column='key')),
-            ('value', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('data', self.gf('sentry.db.models.fields.gzippeddict.GzippedDictField')(null=True, blank=True)),
-            ('times_seen', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
-            ('last_seen', self.gf('django.db.models.fields.DateTimeField')(
-                null=True, db_index=True)),
-            ('first_seen', self.gf('django.db.models.fields.DateTimeField')(
-                null=True, db_index=True)),
-        ))
-        db.send_create_signal('tagstore', ['TagValue'])
-    
-            # Removing unique constraint on 'TagValue', fields ['project_id', '_key', 'value']
-        db.delete_unique(u'tagstore_tagvalue', ['project_id', 'key_id', 'value'])
-    
-            # Changing field 'EventTag.group_id'
-        db.alter_column(u'tagstore_eventtag', 'group_id', self.gf(
-            'sentry.db.models.fields.bounded.BoundedPositiveIntegerField')())
-    
-    from django.db import models, router, connections
-from django.utils import timezone
-    
-    from sentry.utils.safe import safe_execute
-from sentry.tasks.base import instrumented_task
+    def send(self, msg, toUserName=None, mediaId=None):
+    if not msg:
+        r = ReturnValue({'BaseResponse': {
+            'ErrMsg': 'No message.',
+            'Ret': -1005, }})
+    elif msg[:5] == '@fil@':
+        if mediaId is None:
+            r = self.send_file(msg[5:], toUserName)
+        else:
+            r = self.send_file(msg[5:], toUserName, mediaId)
+    elif msg[:5] == '@img@':
+        if mediaId is None:
+            r = self.send_image(msg[5:], toUserName)
+        else:
+            r = self.send_image(msg[5:], toUserName, mediaId)
+    elif msg[:5] == '@msg@':
+        r = self.send_msg(msg[5:], toUserName)
+    elif msg[:5] == '@vid@':
+        if mediaId is None:
+            r = self.send_video(msg[5:], toUserName)
+        else:
+            r = self.send_video(msg[5:], toUserName, mediaId)
+    else:
+        r = self.send_msg(msg, toUserName)
+    return r
