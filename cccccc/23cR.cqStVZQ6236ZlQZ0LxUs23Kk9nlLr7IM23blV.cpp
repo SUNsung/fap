5e55ff075b97690d1cda422a2c2193a6d9a14593
SUNsung <weatherius@gmail.com>
2019-08-02 23:49:40 +0300

    Rewrite [23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp] File in: 2019-08-02 23:49:40

--- cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
+++ cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
@@ -1,43 +1,236 @@
 
         
-        		{
-			//zero texture
-			PoolVector<uint8_t>::Write w = tex.imgdata.write();
-			ERR_FAIL_COND_V(texsize * texsize * p_color_size > tex.imgdata.size(), ret);
-			for (int i = 0; i < texsize * texsize * p_color_size; i++) {
-				w[i] = 0;
-			}
-		}
-		tex.offsets.resize(texsize);
-		for (int i = 0; i < texsize; i++) //zero offsets
-			tex.offsets.write[i] = 0;
+        #include <string>
+#include <vector>
     
-    void VideoStreamPlaybackGDNative::cleanup() {
-	if (data_struct)
-		interface->destructor(data_struct);
-	if (pcm)
-		memfree(pcm);
-	pcm = NULL;
-	time = 0;
-	num_channels = -1;
-	interface = NULL;
-	data_struct = NULL;
+    class GetIm2ColGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'Col2Im',
+        '',
+        std::vector<string>{GO(0), I(0)},
+        std::vector<string>{GI(0)});
+  }
+};
+REGISTER_GRADIENT(Im2Col, GetIm2ColGradient);
+    
+    
+    {	ImageLoader::get_recognized_extensions(p_extensions);
 }
     
-    class NavigationMesh : public Resource {
+    			if (!is_inside_tree())
+				break;
+    
+    			set_force_integration_callback(0, StringName());
+		} else {
+			const Variant *vp[2] = { &v, &fi_callback->udata };
+    
+    
+			static inline void		bind(sFace* fa,U ea,sFace* fb,U eb)
+			{
+				fa->e[ea]=(U1)eb;fa->f[ea]=fb;
+				fb->e[eb]=(U1)ea;fb->f[eb]=fa;
+			}
+			static inline void		append(sList& list,sFace* face)
+			{
+				face->l[0]	=	0;
+				face->l[1]	=	list.root;
+				if(list.root) list.root->l[0]=face;
+				list.root	=	face;
+				++list.count;
+			}
+			static inline void		remove(sList& list,sFace* face)
+			{
+				if(face->l[1]) face->l[1]->l[0]=face->l[0];
+				if(face->l[0]) face->l[0]->l[1]=face->l[1];
+				if(face==list.root) list.root=face->l[1];
+				--list.count;
+			}
+    
+    						bool on_border = false;
+						for (int x = i - 1; x <= i + 1; x++) {
+							for (int y = j - 1; y <= j + 1; y++) {
+    }
+    }
+    
+    										if (x == i && y == j)
+											continue;
+    
+    Vector<StringName> NodePath::get_names() const {
+    }
+    
+    Ref<InputEvent> InputEventScreenDrag::xformed_by(const Transform2D &p_xform, const Vector2 &p_local_ofs) const {
     }
     
     
-    {	return collided;
+    {	while (true) {
+		T tmp = static_cast<T const volatile &>(*pw);
+		if (tmp == 0)
+			return 0; // if zero, can't add to it anymore
+		if (__sync_val_compare_and_swap(pw, tmp, tmp + 1) == tmp)
+			return tmp + 1;
+	}
 }
     
-    	operator String() const;
-	bool is_empty() const;
+    
+    {				} break;
     
     
-    {		return String();
+    {		RenderTarget() :
+				fbo(0),
+				depth(0),
+				last_exposure_tick(0),
+				width(0),
+				height(0),
+				used_in_frame(false),
+				msaa(VS::VIEWPORT_MSAA_DISABLED) {
+			exposure.fbo = 0;
+			buffers.fbo = 0;
+			external.fbo = 0;
+			for (int i = 0; i < RENDER_TARGET_FLAG_MAX; i++) {
+				flags[i] = false;
+			}
+			flags[RENDER_TARGET_HDR] = true;
+			buffers.active = false;
+			buffers.effects_active = false;
+		}
+	};
+    
+      //----- initialization and control
+    
+    void
+ProfileData::addElement (double elapsed) {
+	if (count == 0) {
+		min = elapsed;
+		max = elapsed;
+	} else {
+		if (elapsed < min)
+			min = elapsed;
+		if (elapsed > max)
+			max = elapsed;
 	}
+	total += elapsed;
+	count ++;
+}
     
-    	Vector<MIDIEndpointRef> connected_sources;
     
-    #include 'gdnative/string_name.h'
+    {  if (readAttrs)
+  {
+    Object tmp;
+    Dict *dict = streamObj->getStream()->getDict();
+    dict->lookup('F', &tmp);
+    if (!tmp.isNull()) {
+      Object obj1;
+      // valid 'F' key -> external file
+      kind = soundExternal;
+      if (getFileSpecNameForPlatform (&tmp, &obj1)) {
+        fileName = obj1.getString()->copy();
+        obj1.free();
+      }
+    } else {
+      // no file specification, then the sound data have to be
+      // extracted from the stream
+      kind = soundEmbedded;
+    }
+    tmp.free();
+    // sampling rate
+    dict->lookup('R', &tmp);
+    if (tmp.isNum()) {
+      samplingRate = tmp.getNum();
+    }
+    tmp.free();
+    // sound channels
+    dict->lookup('C', &tmp);
+    if (tmp.isInt()) {
+      channels = tmp.getInt();
+    }
+    tmp.free();
+    // bits per sample
+    dict->lookup('B', &tmp);
+    if (tmp.isInt()) {
+      bitsPerSample = tmp.getInt();
+    }
+    tmp.free();
+    // encoding format
+    dict->lookup('E', &tmp);
+    if (tmp.isName())
+    {
+      const char *enc = tmp.getName();
+      if (strcmp('Raw', enc) == 0) {
+        encoding = soundRaw;
+      } else if (strcmp('Signed', enc) == 0) {
+        encoding = soundSigned;
+      } else if (strcmp('muLaw', enc) == 0) {
+        encoding = soundMuLaw;
+      } else if (strcmp('ALaw', enc) == 0) {
+        encoding = soundALaw;
+      }
+    }
+    tmp.free();
+  }
+}
+    
+    /*
+ * Class:     org_rocksdb_OptimisticTransactionDB
+ * Method:    beginTransaction_withOld
+ * Signature: (JJJ)J
+ */
+jlong Java_org_rocksdb_OptimisticTransactionDB_beginTransaction_1withOld__JJJ(
+    JNIEnv*, jobject, jlong jhandle, jlong jwrite_options_handle,
+    jlong jold_txn_handle) {
+  auto* optimistic_txn_db =
+      reinterpret_cast<rocksdb::OptimisticTransactionDB*>(jhandle);
+  auto* write_options =
+      reinterpret_cast<rocksdb::WriteOptions*>(jwrite_options_handle);
+  auto* old_txn = reinterpret_cast<rocksdb::Transaction*>(jold_txn_handle);
+  rocksdb::OptimisticTransactionOptions optimistic_txn_options;
+  rocksdb::Transaction* txn = optimistic_txn_db->BeginTransaction(
+      *write_options, optimistic_txn_options, old_txn);
+    }
+    
+    
+    {}  // namespace rocksdb
+
+    
+    /*
+ * Class:     org_rocksdb_ThreadStatus
+ * Method:    getOperationPropertyName
+ * Signature: (BI)Ljava/lang/String;
+ */
+jstring Java_org_rocksdb_ThreadStatus_getOperationPropertyName(
+    JNIEnv* env, jclass, jbyte joperation_type_value, jint jindex) {
+  auto name = rocksdb::ThreadStatus::GetOperationPropertyName(
+      rocksdb::OperationTypeJni::toCppOperationType(joperation_type_value),
+      static_cast<int>(jindex));
+  return rocksdb::JniUtil::toJavaString(env, &name, true);
+}
+    
+      const jsize len_cols = env->GetArrayLength(jcolumn_names);
+  if (env->EnsureLocalCapacity(len_cols) != 0) {
+    // out of memory
+    env->ReleaseStringUTFChars(jdb_path, db_path);
+    return nullptr;
+  }
+    
+      auto* cfOptions =
+      reinterpret_cast<rocksdb::ColumnFamilyOptions*>(jcolumn_options);
+    
+    #if GTEST_HAS_SEH && !GTEST_OS_WINDOWS_MOBILE
+// On Windows Mobile global exception handlers are not supported.
+LONG WINAPI ExitWithExceptionCode(
+    struct _EXCEPTION_POINTERS* exception_pointers) {
+  exit(exception_pointers->ExceptionRecord->ExceptionCode);
+}
+#endif
+    
+    
+// A helper program for testing how Google Test determines whether to use
+// colors in the output.  It prints 'YES' and returns 1 if Google Test
+// decides to use colors, and prints 'NO' and returns 0 otherwise.
+    
+    int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+    }
+    
+      virtual void VoidReturning(int x) = 0;
