24329c111ddbf279a6a62b66f85ed98ccd337497
SUNsung <weatherius@gmail.com>
2019-08-02 13:54:24 +0300

    Rewrite [23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp] File in: 2019-08-02 13:54:23

--- cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
+++ cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
@@ -1,59 +1,43 @@
 
         
-                case IDC_OR:
-            result |= rhs;
-            break;
+        		{
+			//zero texture
+			PoolVector<uint8_t>::Write w = tex.imgdata.write();
+			ERR_FAIL_COND_V(texsize * texsize * p_color_size > tex.imgdata.size(), ret);
+			for (int i = 0; i < texsize * texsize * p_color_size; i++) {
+				w[i] = 0;
+			}
+		}
+		tex.offsets.resize(texsize);
+		for (int i = 0; i < texsize; i++) //zero offsets
+			tex.offsets.write[i] = 0;
     
-    class ISerializeCommandVisitor;
-    
-      JSON tree;
-  s = tree.fromString(carve);
-  if (!s.ok()) {
-    VLOG(1) << 'Failed to parse carve entries: ' << s.what();
-    return;
-  }
-    
-      /*
-   * If the initial configuration includes a non-0 refresh, start an
-   * additional service that sleeps and periodically regenerates the
-   * configuration.
-   */
-  if (!FLAGS_config_check && !started_thread_ && getRefresh() > 0) {
-    Dispatcher::addService(refresh_runner_);
-    started_thread_ = true;
-  }
-    
-    #include <osquery/utils/json/json.h>
-    
-    Expected<std::string, DatabaseError> Database::getStringOr(
-    const std::string& domain,
-    const std::string& key,
-    const std::string& default_value) {
-  auto result = getString(domain, key);
-  if (!result && result.getError() == DatabaseError::KeyNotFound) {
-    return default_value;
-  }
-  return result;
+    void VideoStreamPlaybackGDNative::cleanup() {
+	if (data_struct)
+		interface->destructor(data_struct);
+	if (pcm)
+		memfree(pcm);
+	pcm = NULL;
+	time = 0;
+	num_channels = -1;
+	interface = NULL;
+	data_struct = NULL;
 }
     
-      Expected<std::vector<std::string>, DatabaseError> getKeys(
-      const std::string& domain, const std::string& prefix = '') override;
-    
-      RocksdbDatabase(std::string name) = delete;
-  explicit RocksdbDatabase(std::string name, std::string path)
-      : Database(std::move(name)), path_(std::move(path)){};
-  ~RocksdbDatabase() override {}
-    
-    void Plugin::setName(const std::string& name) {
-  if (!name_.empty() && name != name_) {
-    std::string error = 'Cannot rename plugin ' + name_ + ' to ' + name;
-    throw std::runtime_error(error);
-  }
+    class NavigationMesh : public Resource {
     }
     
-    namespace osquery {
-    }
     
-    #include <boost/filesystem.hpp>
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp>
+    {	return collided;
+}
+    
+    	operator String() const;
+	bool is_empty() const;
+    
+    
+    {		return String();
+	}
+    
+    	Vector<MIDIEndpointRef> connected_sources;
+    
+    #include 'gdnative/string_name.h'
