70ad589ee316a59c49bf34a7c9bd8c84966b4d0f
SUNsung <weatherius@gmail.com>
2019-08-02 18:58:20 +0300

    Rewrite [48kRpnvduPDTM48yjMasYhpqok48G.LBZbWC24I489G8.rb] File in: 2019-08-02 18:58:20

--- ruby/48kRpnvduPDTM48yjMasYhpqok48G.LBZbWC24I489G8.rb
+++ ruby/48kRpnvduPDTM48yjMasYhpqok48G.LBZbWC24I489G8.rb
@@ -1,139 +1,203 @@
 
         
-        # == Schema Information
-#
-# Table name: application_requests
-#
-#  id       :integer          not null, primary key
-#  date     :date             not null
-#  req_type :integer          not null
-#  count    :integer          default(0), not null
-#
-# Indexes
-#
-#  index_application_requests_on_date_and_req_type  (date,req_type) UNIQUE
-#
-
+              def tag_without_or_later(tag)
+        tag
+      end
     
-      def report
-    @report ||= JSON.parse(request.body.read)['csp-report'].slice(
-      'blocked-uri',
-      'disposition',
-      'document-uri',
-      'effective-directive',
-      'original-policy',
-      'referrer',
-      'script-sample',
-      'status-code',
-      'violated-directive',
-      'line-number',
-      'source-file'
-    )
+              if method_called_ever?(body_node, :virtualenv_create) ||
+             method_called_ever?(body_node, :virtualenv_install_with_resources)
+            find_method_with_args(body_node, :resource, 'setuptools') do
+              problem 'Formulae using virtualenvs do not need a `setuptools` resource.'
+            end
+          end
+    
+            it 'returns false if all are lower' do
+          setup_java_with_version '1.6.0_5'
+          expect(subject).not_to be_satisfied
+        end
+    
+        depends_on(deps) if add_mac_dependency?(args)
   end
     
-        delegate :base_url, to: :ContentSecurityPolicy
+        it 'ignores OS version specifications' do
+      f = formula 'foo' do
+        url 'foo-1.0'
     
-      def not_modified?
-    cache_time =
-      begin
-        Time.rfc2822(request.env['HTTP_IF_MODIFIED_SINCE'])
-      rescue ArgumentError
-        nil
+        it 'sets itself to be the resource's owner' do
+      subject.resource('foo') { url 'foo-1.0' }
+      subject.owner = owner
+      subject.resources.each_value do |r|
+        expect(r.owner).to eq(subject)
       end
-    
-      def lookup_user_api_user_and_update_key(user_api_key, client_id)
-    if api_key = UserApiKey.where(key: user_api_key, revoked_at: nil).includes(:user).first
-      unless api_key.allow?(@env)
-        raise Discourse::InvalidAccess
-      end
-    
-              instance.request = request_copy
-          original_headers&.each do |k, v|
-            instance.response.headers[k] = v
-          end
-    
-        token = SecureRandom.hex(16)
-    
-        def add(path, content)
-      @pages[path] = content
     end
     
-        def html?
-      mime_type.include? 'html'
+        def metadata_versioned_path(version: self.version)
+      cask_version = (version || :unknown).to_s
+    
+      # If a target is given, unpack there; else unpack to a temp folder.
+  # If block is given, yield to that block with `|stage|`, where stage
+  # is a {ResourceStageContext}.
+  # A target or a block must be given, but not both.
+  def unpack(target = nil)
+    mktemp(download_name) do |staging|
+      downloader.stage
+      @source_modified_time = downloader.source_modified_time
+      apply_patches
+      if block_given?
+        yield ResourceStageContext.new(self, staging)
+      elsif target
+        target = Pathname(target)
+        target.install Pathname.pwd.children
+      end
+    end
+  end
+    
+          it 'returns false' do
+        expect(node.pure?).to be(false)
+      end
     end
     
-              node.before(%(<div class='pre-title'>#{node['title']}</div>)) if node['title']
+              return false unless brace_style == 'context_dependent'
     
-            if mod
-          if name == 'Index'
-            return slug.split('/')[1..-2].join('/')
-          elsif name == 'Angular'
-            return slug.split('/').last.split('-').first
-          end
+        context 'preceding multiple captures' do
+      let(:pattern) { '(send array :push ... $_ $_)' }
+      let(:ruby) { '[1].push(2, 3, 4, 5)' }
+      let(:captured_vals) { [s(:int, 4), s(:int, 5)] }
+    
+      context 'when there's an unreferenced assignment in top level if branch ' \
+          'while the variable is referenced in the paired else branch' do
+    it 'registers an offense for the assignment in the if branch' do
+      expect_offense(<<~RUBY)
+        if flag
+          foo = 1
+          ^^^ Useless assignment to variable - `foo`.
+        else
+          puts foo
+        end
+      RUBY
+    end
+  end
+    
+          def extract_rhs(node)
+        if node.casgn_type?
+          _scope, _lhs, rhs = *node
+        elsif node.op_asgn_type?
+          _lhs, _op, rhs = *node
+        elsif node.call_type?
+          rhs = node.last_argument
+        elsif node.assignment?
+          _lhs, rhs = *node
         end
     
-            # Remove dead links (e.g. ngRepeat)
-        css('a.type-hint').each do |node|
-          node.name = 'code'
-          node.remove_attribute 'href'
+            def last_argument(node)
+          node.if_branch.children.last
         end
     
-        # Find all leaf-like paths (files, symlink, empty directories, etc)
-    # Also trim the leading path such that '#{staging_path}/' is removed from
-    # the path before returning.
+      it 'does not register offense when guard clause is before `else`' do
+    expect_no_offenses(<<~RUBY)
+      def foo
+        if cond
+          return another_object if something_different?
+        else
+          bar
+        end
+      end
+    RUBY
+  end
+    
+    module RuboCop
+  module AST
+    # A node extension for `block` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `send` nodes within RuboCop.
     #
-    # Wrapping Find.find in an Enumerator is required for sane operation in ruby 1.8.7,
-    # but requires the 'backports' gem (which is used in other places in fpm)
-    return Enumerator.new { |y| Find.find(staging_path) { |path| y << path } } \
-      .select { |path| path != staging_path } \
-      .select { |path| is_leaf.call(path) } \
-      .collect { |path| path[staging_path.length + 1.. -1] }
-  end # def files
+    # A `block` node is essentially a method send with a block. Parser nests
+    # the `send` node inside the `block` node.
+    class BlockNode < Node
+      VOID_CONTEXT_METHODS = %i[each tap].freeze
     
-        # Write +COMPACT_MANIFEST, without the 'files' section.
-    File.open(staging_path('+COMPACT_MANIFEST'), 'w+') do |file|
-      file.write(pkgdata.to_json + '\n')
-    end
-    
-        npm_flags = []
-    settings.each do |key, value|
-      # npm lets you specify settings in a .npmrc but the same key=value settings
-      # are valid as '--key value' command arguments to npm. Woo!
-      logger.debug('Configuring npm', key => value)
-      npm_flags += [ '--#{key}', value ]
-    end
-    
-        # do channel-discover if required
-    if !attributes[:pear_channel].nil?
-      logger.info('Custom channel specified', :channel => attributes[:pear_channel])
-      channel_list = safesystemout('pear', '-c', config, 'list-channels')
-      if channel_list !~ /#{Regexp.quote(attributes[:pear_channel])}/
-        logger.info('Discovering new channel', :channel => attributes[:pear_channel])
-        safesystem('pear', '-c', config, 'channel-discover', attributes[:pear_channel])
+          def record_response(response)
+        log_entries.create!(details: response.to_yaml)
       end
-      input_package = '#{attributes[:pear_channel]}/#{input_package}'
-      logger.info('Prefixing package name with channel', :package => input_package)
-    end
     
-        platforms.each do |platform|
-      logger.info('Generating service manifest.', :platform => platform.class.name)
-      platform.program = command.first
-      platform.name = attributes[:pleaserun_name]
-      platform.args = command[1..-1]
-      platform.description = if attributes[:description_given?]
-        attributes[:description]
-      else
-        platform.name
-      end
-      pleaserun_attributes.each do |attribute_name|
-        attribute = 'pleaserun_#{attribute_name}'.to_sym
-        if attributes.has_key?(attribute) and not attributes[attribute].nil?
-          platform.send('#{attribute_name}=', attributes[attribute])
+          before_action :set_content_type
+      before_action :load_user
+      before_action :authorize_for_order, if: proc { order_token.present? }
+      before_action :authenticate_user
+      before_action :load_user_roles
+    
+            def fire
+          inventory_unit.send('#{@event}!') if @event
         end
+    
+            def scope
+          @scope ||= if params[:option_type_id]
+                       Spree::OptionType.find(params[:option_type_id]).option_values.accessible_by(current_ability, :show)
+                     else
+                       Spree::OptionValue.accessible_by(current_ability, :show).load
+                     end
+        end
+    
+            def update
+          authorize! :update, @product_property
+    
+            def create
+          authorize! :create, Property
+          @property = Spree::Property.new(property_params)
+          if @property.save
+            respond_with(@property, status: 201, default_template: :show)
+          else
+            invalid_resource!(@property)
+          end
+        end
+    
+        it 'pushes messages to redis using a String class' do
+      q = Sidekiq::Queue.new('foo')
+      pre = q.size
+      jid = Sidekiq::Client.push('queue' => 'foo', 'class' => 'MyWorker', 'args' => [1, 2])
+      assert jid
+      assert_equal 24, jid.size
+      assert_equal pre + 1, q.size
+    end
+    
+      describe 'status' do
+    describe 'version' do
+      it 'displays the current Sidekiq version' do
+        assert_includes output, 'Sidekiq #{Sidekiq::VERSION}'
       end
     
-        if File.exists?(params[:output])
-      # TODO(sissel): Allow folks to choose output?
-      logger.error('Puppet module directory '#{params[:output]}' already ' \
-                    'exists. Delete it or choose another output (-p flag)')
+        it 'generates random intervals that target a configured average' do
+      with_sidekiq_option(:poll_interval_average, 10) do
+        i = 500
+        intervals = Array.new(i){ @poller.send(:random_poll_interval) }
+    
+          q = Sidekiq::Queue.new('custom_queue')
+      qs = q.size
+      assert SomeScheduledWorker.perform_in(-300, 'mike')
+      assert_equal 3, ss.size
+      assert_equal qs+1, q.size
+    
+        it 'stubs the delay call on classes' do
+      assert_equal 0, Sidekiq::Extensions::DelayedClass.jobs.size
+      Something.delay.foo(Date.today)
+      assert_equal 1, Sidekiq::Extensions::DelayedClass.jobs.size
     end
+  end
+    
+        while readable_io = IO.select([self_read])
+      signal = readable_io.first[0].gets.strip
+      handle_signal(launcher, signal)
+    end
+  #end
+rescue SystemExit => e
+  #Sidekiq.logger.error('Profiling...')
+  #result = RubyProf.stop
+  #printer = RubyProf::GraphHtmlPrinter.new(result)
+  #printer.print(File.new('output.html', 'w'), :min_percent => 1)
+  # normal
+rescue => e
+  raise e if $DEBUG
+  STDERR.puts e.message
+  STDERR.puts e.backtrace.join('\n')
+  exit 1
+end
