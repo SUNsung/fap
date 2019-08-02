c0fc62b1422cdf05db38fddbc1ef71db1a3ce281
SUNsung <weatherius@gmail.com>
2019-08-02 05:36:25 +0300

    Rewrite [58I6pfbcMDyu658YJBPnmI4zqw58CUqVCUlbnLU58Jek.cpp] File in: 2019-08-02 05:36:24

--- cccccc/58I6pfbcMDyu658YJBPnmI4zqw58CUqVCUlbnLU58Jek.cpp
+++ cccccc/58I6pfbcMDyu658YJBPnmI4zqw58CUqVCUlbnLU58Jek.cpp
@@ -1,306 +1,81 @@
 
         
-        private Q_SLOTS:
-    void on_selectFileButton_clicked();
+        /*!
+ *  Copyright (c) 2017 by Contributors
+ * \file c_api_profile.cc
+ * \brief C API of mxnet profiler and support functions
+ */
+#include <dmlc/base.h>
+#include <dmlc/logging.h>
+#include <dmlc/thread_group.h>
+#include <mxnet/kvstore.h>
+#include <stack>
+#include './c_api_common.h'
+#include '../profiler/profiler.h'
     
-    class PlatformStyle;
-class WalletModel;
     
-    static void secp256k1_hmac_sha256_initialize(secp256k1_hmac_sha256_t *hash, const unsigned char *key, size_t keylen) {
-    int n;
-    unsigned char rkey[64];
-    if (keylen <= 64) {
-        memcpy(rkey, key, keylen);
-        memset(rkey + keylen, 0, 64 - keylen);
+    {
+    {}  // namespace storage
+}  // namespace mxnet
+    
+    template<typename xpu>
+inline void BroadcastComputeImpl(const nnvm::NodeAttrs& attrs,
+                                 const OpContext& ctx,
+                                 const std::vector<TBlob>& inputs,
+                                 const std::vector<OpReqType>& req,
+                                 const std::vector<TBlob>& outputs,
+                                 const mxnet::TShape& small) {
+  using namespace mshadow;
+  using namespace mshadow::expr;
+  mxnet::TShape src_shape, dst_shape;
+  BroadcastReduceShapeCompact(outputs[0].shape_, small, &dst_shape, &src_shape);
+  Stream<xpu> *s = ctx.get_stream<xpu>();
+  MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
+    if (dst_shape.ndim() == 2) {
+      Tensor<xpu, 2, DType> out =
+        outputs[0].get_with_shape<xpu, 2, DType>(dst_shape.get<2>(), s);
+      Tensor<xpu, 2, DType> data =
+        inputs[0].get_with_shape<xpu, 2, DType>(src_shape.get<2>(), s);
+      ASSIGN_DISPATCH(out, req[0], broadcast_to(data, dst_shape));
     } else {
-        secp256k1_sha256_t sha256;
-        secp256k1_sha256_initialize(&sha256);
-        secp256k1_sha256_write(&sha256, key, keylen);
-        secp256k1_sha256_finalize(&sha256, rkey);
-        memset(rkey + 32, 0, 32);
+      const int ndim = MXNET_SPECIAL_MAX_NDIM;
+      Tensor<xpu, ndim, DType> out =
+        outputs[0].get_with_shape<xpu, ndim, DType>(dst_shape.get<ndim>(), s);
+      Tensor<xpu, ndim, DType> data =
+        inputs[0].get_with_shape<xpu, ndim, DType>(src_shape.get<ndim>(), s);
+      ASSIGN_DISPATCH(out, req[0], broadcast_to(data, dst_shape));
     }
-    }
-    
-    int secp256k1_ecdh(const secp256k1_context* ctx, unsigned char *result, const secp256k1_pubkey *point, const unsigned char *scalar) {
-    int ret = 0;
-    int overflow = 0;
-    secp256k1_gej res;
-    secp256k1_ge pt;
-    secp256k1_scalar s;
-    VERIFY_CHECK(ctx != NULL);
-    ARG_CHECK(result != NULL);
-    ARG_CHECK(point != NULL);
-    ARG_CHECK(scalar != NULL);
-    }
-    
-    static void secp256k1_ecdsa_recoverable_signature_save(secp256k1_ecdsa_recoverable_signature* sig, const secp256k1_scalar* r, const secp256k1_scalar* s, int recid) {
-    if (sizeof(secp256k1_scalar) == 32) {
-        memcpy(&sig->data[0], r, 32);
-        memcpy(&sig->data[32], s, 32);
-    } else {
-        secp256k1_scalar_get_b32(&sig->data[0], r);
-        secp256k1_scalar_get_b32(&sig->data[32], s);
-    }
-    sig->data[64] = recid;
+  });
 }
     
-    public:
-    static const size_t OUTPUT_SIZE = 20;
-    
-    // Grouping of constants.  We may want to make some of these
-// parameters set via options.
-namespace config {
-static const int kNumLevels = 7;
-    }
-    
-    void add(const Size2D &size,
-         const u16 * src0Base, ptrdiff_t src0Stride,
-         const u16 * src1Base, ptrdiff_t src1Stride,
-         u16 * dstBase, ptrdiff_t dstStride,
-         CONVERT_POLICY policy)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-        if (policy == CONVERT_POLICY_SATURATE)
-    {
-        internal::vtransform(size,
-                             src0Base, src0Stride,
-                             src1Base, src1Stride,
-                             dstBase, dstStride,
-                             AddSaturate<u16, u32>());
-    }
-    else
-    {
-        internal::vtransform(size,
-                             src0Base, src0Stride,
-                             src1Base, src1Stride,
-                             dstBase, dstStride,
-                             AddWrap<u16, u32>());
-    }
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-    (void)policy;
-#endif
+    // DO_BIND_DISPATCH comes from operator_common.h
+Operator *DeformableConvolutionProp::CreateOperatorEx(Context ctx,
+                                            mxnet::ShapeVector *in_shape,
+                                            std::vector<int> *in_type) const {
+  mxnet::ShapeVector out_shape, aux_shape;
+  std::vector<int> out_type, aux_type;
+  CHECK(InferType(in_type, &out_type, &aux_type));
+  CHECK(InferShape(in_shape, &out_shape, &aux_shape));
+  DO_BIND_DISPATCH(CreateOp, param_, (*in_type)[0], in_shape, &out_shape, ctx);
 }
     
-    
-    {
-    {} // namespace internal
-} // namespace CAROTENE_NS
-
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC1(u16, 16,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.16 {d4-d5}, [%[src]]                              \n\t'
-            'vmovl.u16 q3, d4                                       \n\t'
-            'vmovl.u16 q4, d5                                       \n\t'
-            'vcvt.f32.u32 q5, q3                                    \n\t'
-            'vcvt.f32.u32 q6, q4                                    \n\t'
-            'vmul.f32 q7, q5, q0                                    \n\t'
-            'vmul.f32 q8, q6, q0                                    \n\t'
-            'vadd.f32 q9, q7, q1                                    \n\t'
-            'vadd.f32 q10, q8, q1                                   \n\t'
-            'vcvt.s32.f32 q11, q9                                   \n\t'
-            'vcvt.s32.f32 q12, q10                                  \n\t'
-            'vqmovun.s32 d26, q11                                   \n\t'
-            'vqmovun.s32 d27, q12                                   \n\t'
-            'vst1.16 {d26-d27}, [%[dst]]                            \n\t'
-            : /*no output*/
-            : [src] 'r' (_src + i),
-              [dst] 'r' (_dst + i + 0),
-              'w' (vshift), 'w' (vscale)
-            : 'd6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25','d26','d27'
-        );
-    }
-})
-#else
-CVTS_FUNC1(u16, 16,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        uint16x8_t vline = vld1q_u16(_src + i);
-        uint32x4_t vline1_u32 = vmovl_u16(vget_low_u16 (vline));
-        uint32x4_t vline2_u32 = vmovl_u16(vget_high_u16(vline));
-        float32x4_t vline1_f32 = vcvtq_f32_u32(vline1_u32);
-        float32x4_t vline2_f32 = vcvtq_f32_u32(vline2_u32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        uint16x4_t vRes1 = vqmovun_s32(vline1_s32);
-        uint16x4_t vRes2 = vqmovun_s32(vline2_s32);
-        vst1q_u16(_dst + i, vcombine_u16(vRes1, vRes2));
-    }
-})
-#endif
-    
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const T * src = getRowPtr((const T *)srcBase, srcStride, i);
-        T * dst = getRowPtr((T *)dstBase, dstStride, (flipMode & FLIP_VERTICAL_MODE) != 0 ? size.height - i - 1 : i);
-        size_t js = 0, jd = size.width;
-    }
-    
-    TEST(CSharpEnumValue, PascalCasedPrefixStripping) {
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO__BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'FOO_BAR_BAZ'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'Foo_BarBaz'));
-  EXPECT_EQ('Bar', GetEnumValueName('FO_O', 'FOO_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('FOO', 'F_O_O_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
-  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO'));
-  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO___'));
-  // Identifiers can't start with digits
-  EXPECT_EQ('_2Bar', GetEnumValueName('Foo', 'FOO_2_BAR'));
-  EXPECT_EQ('_2', GetEnumValueName('Foo', 'FOO___2'));
-}
-    
-    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
-    
-    
-    {
-    {
-    {}  // namespace util
-}  // namespace protobuf
-}  // namespace google
-
-    
-    struct big_ {
-  char dummy[2];
-};
-    
-    class Proto3DataStripper : public DataStripper {
- private:
-  virtual bool ShouldBeClear(const FieldDescriptor *field) {
-    return field->type() == FieldDescriptor::TYPE_GROUP ||
-           field->is_extension();
+      if (x_ptr && layer_index == 0) {
+    (*x_memory)[layer_index].set_data_handle(x_ptr);
+  } else {
+    (*x_memory)[layer_index].set_data_handle((*user_src_layer_memory).get_data_handle());
   }
-};
+  (*y_memory)[layer_index].set_data_handle(y_ptr);
     
-        std::vector<std::pair<string,string>> option_pairs;
-    ParseGeneratorParameter(parameter, &option_pairs);
-    
-    REGISTER_CPU_OPERATOR(
-    MergeMultiScalarFeatureTensors,
-    MergeMultiScalarFeatureTensorsOp<CPUContext>);
-OPERATOR_SCHEMA(MergeMultiScalarFeatureTensors)
-    .SetDoc(
-        'Merge given multi-feature tensors with scalar features into one.' +
-        doc)
-    .NumInputs([](int n) { return n >= 3 && n % 3 == 0; })
-    .NumOutputs(3)
-    .Input(0, 'in1_lengths', '.lengths')
-    .Input(1, 'in1_keys', '.keys')
-    .Input(2, 'in1_values', '.values')
-    .Output(0, 'out_lengths', '.lengths')
-    .Output(1, 'out_keys', '.keys')
-    .Output(2, 'out_values', '.values');
-    
-    Contrast Example 2 with Example 1. For each data point per feature, the values
-are sorted by the corresponding KEY.
-)DOC')
-    .Input(0, 'DATA', 'Tensor of rank 1.')
-    .Input(
-        1,
-        'RANGES',
-        'Tensor of int32/int64 ranges, of dims (N, M, 2). '
-        'Where N is number of examples and M is a size of each example. '
-        'Last dimention represents a range in the format (start, lengths)')
-    .Input(2, 'KEY', 'Tensor of rank 1 and type int64.')
-    .Output(0, 'OUTPUT', '1-D tensor of size sum of range lengths')
-    .Arg('lengths', 'Expected lengths for ranges')
-    .TensorInferenceFunction([](const OperatorDef& def,
-                                const vector<TensorShape>& in) {
-      ArgumentHelper helper(def);
-      auto lengths = helper.GetRepeatedArgument<int>('lengths');
-      CAFFE_ENFORCE_EQ(in[0].dims_size(), 1, 'DATA should be 1-D tensor.');
-      CAFFE_ENFORCE_EQ(in[1].dims_size(), 3, 'RANGES should be 3-D tensor.');
-      if (in.size() > 2) {
-        CAFFE_ENFORCE_EQ(in[2].dims_size(), 1, 'KEY should be 1-D tensor.');
-      }
-      CAFFE_ENFORCE_GT(lengths.size(), 0, 'lengths should be non-empty.');
-      std::vector<TensorShape> out(lengths.size());
-      for (int i = 0; i < lengths.size(); ++i) {
-        out[i].set_data_type(in[0].data_type());
-        out[i].add_dims(in[1].dims(0));
-        out[i].add_dims(lengths[i]);
-      }
-      return out;
-    });
-    
-    
-    {} // namespace caffe2
-    
-    #pragma once
-    
-    #pragma once
-/****************************Module*Header***********************************\
-* Module Name: CalcEngine.h
-*
-* Module Description:
-*       The class definition for the Calculator's engine class CCalcEngine
-*
-* Warnings:
-*
-* Created: 17-Jan-2008
-*
-\****************************************************************************/
-    
-            void Clear();
-        bool IsEmpty()
-        {
-            return value.empty();
-        }
-    
-    // Supported only for Leveled compaction
-Status SuggestCompactRange(DB* db, ColumnFamilyHandle* column_family,
-                           const Slice* begin, const Slice* end);
-Status SuggestCompactRange(DB* db, const Slice* begin, const Slice* end);
-    
-     private:
-  void Init(
-      const std::string& lua_script,
-      const std::vector<std::shared_ptr<RocksLuaCustomLibrary>>& libraries) {
-    if (lua_state_) {
-      luaL_openlibs(lua_state_);
-      for (const auto& library : libraries) {
-        luaL_openlib(lua_state_, library->Name(), library->Lib(), 0);
-        library->CustomSetup(lua_state_);
-      }
-      luaL_dostring(lua_state_, lua_script.c_str());
+      std::vector<NDArray*> ndoutputs;
+  ndoutputs.reserve(op->num_outputs());
+  if (*outputs == nullptr) {
+    *num_outputs = op->num_outputs();
+    for (int i = 0; i < *num_outputs; ++i) ndoutputs.push_back(new NDArray());
+  } else {
+    CHECK_EQ(*num_outputs, op->num_outputs())
+        << 'CachedOp expects ' << op->num_outputs() << ' outputs, but '
+        << *num_outputs << ' was given.';
+    for (int i = 0; i < *num_outputs; ++i) {
+      ndoutputs.push_back(reinterpret_cast<NDArray*>((*outputs)[i]));
     }
   }
-    
-    #include 'rocksdb/cache.h'
-#include 'rocksdb/db.h'
