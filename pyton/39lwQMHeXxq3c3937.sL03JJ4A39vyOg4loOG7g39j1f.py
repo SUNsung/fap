20fbcd302ffd44e55955156c5bc66931e90c791d
SUNsung <weatherius@gmail.com>
2019-08-02 06:36:42 +0300

    Rewrite [39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py] File in: 2019-08-02 06:36:41

--- pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
+++ pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
@@ -1,41 +1,43 @@
 
         
-            for trials in range(5):
-        a = random.randrange(2, num - 1)
-        v = pow(a, s, num)
-        if v != 1:
-            i = 0
-            while v != (num - 1):
-                if i == t - 1:
-                    return False
-                else:
-                    i = i + 1
-                    v = (v ** 2) % num
-    return True
-    
-    try:
-	xrange		#Python 2
-except NameError:
-	xrange = range	#Python 3
+            Beyond permissions, groups are a convenient way to categorize users to
+    apply some label, or extended functionality, to them. For example, you
+    could create a group 'Special users', and you could write code that would
+    do special things to those users -- such as giving them access to a
+    members-only portion of your site, or sending them members-only email
+    messages.
+    '''
+    name = models.CharField(_('name'), max_length=150, unique=True)
+    permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('permissions'),
+        blank=True,
+    )
     
     
-        '''
-        loop over all possible splits for the decision tree. find the best split.
-        if no split exists that is less than 2 * error for the entire array
-        then the data set is not split and the average for the entire array is used as the predictor
-        '''
-        for i in range(len(X)):
-            if len(X[:i]) < self.min_leaf_size:
-                continue
-            elif len(X[i:]) < self.min_leaf_size:
-                continue
-            else:
-                error_left = self.mean_squared_error(X[:i], np.mean(y[:i]))
-                error_right = self.mean_squared_error(X[i:], np.mean(y[i:]))
-                error = error_left + error_right
-                if error < min_error:
-                    best_split = i
-                    min_error = error
+class IntConverter:
+    regex = '[0-9]+'
     
-        for word in wordList:
-        pattern = getWordPattern(word)
+             cache_page = decorator_from_middleware_with_args(CacheMiddleware)
+         # ...
+    
+            # Explicit dependencies
+        self.assertLess(ordered_sigs.index('s2'), ordered_sigs.index('s1'))
+        self.assertLess(ordered_sigs.index('s3'), ordered_sigs.index('s2'))
+    
+        def __iter__(self):
+        'Iterate over each Feature in the Layer.'
+        # ResetReading() must be called before iteration is to begin.
+        capi.reset_reading(self._ptr)
+        for i in range(self.num_feat):
+            yield Feature(capi.get_next_feature(self._ptr), self)
+    
+    from sentry.models import Identity, IdentityProvider, IdentityStatus, Integration, OrganizationIntegration
+from sentry.testutils import TestCase
+from sentry.integrations.slack.link_identity import build_linking_url
+    
+        def backwards(self, orm):
+    
+            # Changing field 'TagKey.environment_id'
+        db.alter_column(u'tagstore_tagkey', 'environment_id', self.gf(
+            'sentry.db.models.fields.bounded.BoundedBigIntegerField')(null=True))
