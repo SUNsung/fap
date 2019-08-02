7484caeedfbe8d451fdb7860157fe3d27ba7aa44
SUNsung <weatherius@gmail.com>
2019-08-03 01:54:26 +0300

    Rewrite [42.QoE2kzsLV242uHROeWOq69I421M4mDQ3Ut242LEl.cpp] File in: 2019-08-03 01:54:25

--- cccccc/42.QoE2kzsLV242uHROeWOq69I421M4mDQ3Ut242LEl.cpp
+++ cccccc/42.QoE2kzsLV242uHROeWOq69I421M4mDQ3Ut242LEl.cpp
@@ -1,331 +1,48 @@
 
         
-         private:
-  void OnMessageBoxCallback(DialogClosedCallback callback,
-                            const std::string& origin,
-                            int code,
-                            bool checkbox_checked);
-    
-    net::URLRequestJob* AboutProtocolHandler::MaybeCreateJob(
-    net::URLRequest* request,
-    net::NetworkDelegate* network_delegate) const {
-  return new URLRequestAboutJob(request, network_delegate);
-}
-    
-    URLRequestAboutJob::URLRequestAboutJob(net::URLRequest* request,
-                                       net::NetworkDelegate* network_delegate)
-    : net::URLRequestJob(request, network_delegate), weak_ptr_factory_(this) {}
-    
-    #endif  // ATOM_BROWSER_RELAUNCHER_H_
-
-    
-    namespace internal {
-    }
-    
-      button_drag_utils::SetDragImage(
-      GURL(), files[0].LossyDisplayName(), icon.AsImageSkia(), nullptr,
-      *views::Widget::GetTopLevelWidgetForNativeView(view), &data);
-    
-    #include 'base/basictypes.h'
-#include '../dispatcher_host.h'
-    
-      bool delay_destruction() { return delay_destruction_; }
-  void set_delay_destruction(bool val) { delay_destruction_ = val; }
-  bool pending_destruction() { return pending_destruction_; }
-  void set_pending_destruction (bool val) { pending_destruction_ = val; }
- protected:
-  int id_;
-  bool delay_destruction_;
-  bool pending_destruction_;
-  base::WeakPtr<ObjectManager> object_manager_;
-    
-       void Call(const std::string& method,
-                    const base::ListValue& arguments) override;
-   void CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result) override;
-    
-    
-    {} // namespace nwapi
-    
-    void Menu::Call(const std::string& method,
-                const base::ListValue& arguments,
-                content::RenderFrameHost* rvh) {
-  if (method == 'Append') {
-    int object_id = 0;
-    arguments.GetInteger(0, &object_id);
-    Append(object_manager()->GetApiObject<MenuItem>(object_id));
-  } else if (method == 'Insert') {
-    int object_id = 0;
-    arguments.GetInteger(0, &object_id);
-    int pos = 0;
-    arguments.GetInteger(1, &pos);
-    Insert(object_manager()->GetApiObject<MenuItem>(object_id), pos);
-  } else if (method == 'Remove') {
-    int object_id = 0;
-    arguments.GetInteger(0, &object_id);
-    int pos = 0;
-    arguments.GetInteger(1, &pos);
-    Remove(object_manager()->GetApiObject<MenuItem>(object_id), pos);
-  } else if (method == 'Popup') {
-    int x = 0;
-    arguments.GetInteger(0, &x);
-    int y = 0;
-    arguments.GetInteger(1, &y);
-    content::WebContents* web_contents = content::WebContents::FromRenderFrameHost(rvh);
-    DCHECK(web_contents);
-    zoom::ZoomController* zoom_controller = zoom::ZoomController::FromWebContents(web_contents);
-    }
-    }
-    
-    #ifndef CONTENT_NW_SRC_API_MENU_MENU_DELEGATE_H_
-#define CONTENT_NW_SRC_API_MENU_MENU_DELEGATE_H_
-    
-    void MenuItem::Create(const base::DictionaryValue& option) {
-  std::string type;
-  option.GetString('type', &type);
-  submenu_ = NULL;
-  gtk_accel_group = NULL;
-    }
-    
-    
-    {  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
-  manager->OnCallObjectMethod(render_frame_host(), id, type, method, *arguments);
-  return RespondNow(NoArguments());
-}
-    
-    
-    {    for (auto& media_list : media_list_) {
-      media_list->StartUpdating(this);
-    }
-  }
-    
-    #define TegraGenOp_Invoker(name, func, src_cnt, dst_cnt, scale_cnt, ...) \
-template <typename ST, typename DT> \
-class TegraGenOp_##name##_Invoker : public cv::ParallelLoopBody \
-{ \
-public: \
-    TegraGenOp_##name##_Invoker(SRC_ARG##src_cnt \
-                                DST_ARG##dst_cnt \
-                                int width_, int height_ \
-                                SCALE_ARG##scale_cnt) : \
-        cv::ParallelLoopBody(), SRC_STORE##src_cnt \
-                                DST_STORE##dst_cnt \
-                                width(width_), height(height_) \
-                                SCALE_STORE##scale_cnt {} \
-    virtual void operator()(const cv::Range& range) const \
-    { \
-        CAROTENE_NS::func(CAROTENE_NS::Size2D(width, range.end-range.start), __VA_ARGS__); \
-    } \
-private: \
-    SRC_VAR##src_cnt \
-    DST_VAR##dst_cnt \
-    int width, height; \
-    SCALE_VAR##scale_cnt \
-    const TegraGenOp_##name##_Invoker& operator= (const TegraGenOp_##name##_Invoker&); \
-};
-    
-    #define  VROW_LINE(type, n) const type * src##n = internal::getRowPtr(src##n##Base, src##n##Stride, i);
-#define  PREF_LINE(type, n) internal::prefetch(src##n + sj);
-#define VLD1Q_LINE(type, n) v_dst.val[n] = vld1q_##type(src##n + sj);
-#define  PRLD_LINE(type, n) internal::prefetch(src##n + sj); v_dst.val[n] = vld1q_##type(src##n + sj);
-#define  VLD1_LINE(type, n) v_dst.val[n] = vld1_##type(src##n + sj);
-#define   SLD_LINE(type, n) dst[dj + n] = src##n[sj];
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(s32, f32, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src]]                              \n\t'
-             'vcvt.f32.s32 q1, q0                                    \n\t'
-             'vst1.32 {d2-d3}, [%[dst]]                              \n\t'
-             : /*no output*/
-             : [src] 'r' (_src + i),
-               [dst] 'r' (_dst + i)
-             : 'd0','d1','d2','d3'//,'d4','d5'
-         );
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src]]                              \n\t'
-             'vcvt.f32.s32 q1, q0                                    \n\t'
-             'vst1.32 {d2-d3}, [%[dst]]                              \n\t'
-             : /*no output*/
-             : [src] 'r' (_src + i + 4),
-               [dst] 'r' (_dst + i + 4)
-             : 'd0','d1','d2','d3'//,'d4','d5'
-         );
-     }
-})
-#else
-CVT_FUNC(s32, f32, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         int32x4_t vline_s32 = vld1q_s32(_src + i);
-         float32x4_t vline_f32 = vcvtq_f32_s32(vline_s32);
-         vst1q_f32(_dst + i, vline_f32);
-    }
-    }
-    
-    typedef void (* flipFunc)(const Size2D &size,
-                  const void * srcBase, ptrdiff_t srcStride,
-                  void * dstBase, ptrdiff_t dstStride,
-                  FLIP_MODE flipMode);
-    
-    namespace CAROTENE_NS {
-    }
-    
-      // Get direction
-  PageTransitionDirection getDirection() { return direction; }
-    
-    class PopplerCache
-{
-  public:
-    PopplerCache(int cacheSizeA);
-    ~PopplerCache();
-    
-    /* The item returned is owned by the cache */
-    PopplerCacheItem *lookup(const PopplerCacheKey &key);
-    
-    /* The key and item pointers ownership is taken by the cache */
-    void put(PopplerCacheKey *key, PopplerCacheItem *item);
-    
-    /* The max size of the cache */
-    int size();
-    
-    /* The number of items in the cache */
-    int numberOfItems();
-    
-    /* The n-th item in the cache */
-    PopplerCacheItem *item(int index);
-    
-    /* The n-th key in the cache */
-    PopplerCacheKey *key(int index);
-  
-  private:
-    PopplerCache(const PopplerCache &cache); // not allowed
-  
-    PopplerCacheKey **keys;
-    PopplerCacheItem **items;
-    int lastValidCacheIndex;
-    int cacheSize;
-};
-    
-    
-    {  GBool mono;
-  GBool gray;
-  GBool transparency;
-  GBool gdi;
-  PSLevel level;		// PostScript level (1, 2, separation)
-  GBool level1PSBug;		// gTrue if it uses a feature not supported in PSOutputDev
-};
-    
-    #ifdef _WIN32
-  setmode(fileno(stdin), O_BINARY);
-#endif
-    
-            virtual const std::unordered_set<StreamInformation>& StreamInfos() override { return m_streamInfos; }
-    
-            static bool IsUDF(const FunctionPtr& f);
-    
-        bool hasMultipleReaders = config.Exists(L'readers');
-    // In case when deserializers are specified, use the new logic to compose them.
-    bool hasDeserializers = config.Exists(L'deserializers');
-    if (hasMultipleReaders)
+            NDLBuilderImpl(ComputationNetworkPtr computationNetwork)
     {
-        vector<wstring> ioNames = config(L'readers', ConfigRecordType::Array(stringargvector()));
-        // newer code that explicitly place multiple streams for inputs
-        for (const auto& ioName : ioNames) // inputNames should map to node names
+        m_computationNetwork = computationNetwork;
+        m_nodeEvaluator = new NDLNodeEvaluatorImpl<ElemType>(m_computationNetwork);
+    }
+    
+    // CheckFunction - check to see if we match a function name
+// string1 - [in,out] string to compare, if comparision is equal and at least half the full node name will replace with full node name
+// allowUndeterminedVariable - [out] set to true if undetermined variables (symbols yet to be defined) are allowed here
+// return - true if function name found
+bool CheckFunction(std::string& p_nodeType, bool* allowUndeterminedVariable)
+{
+    if (allowUndeterminedVariable)
+        *allowUndeterminedVariable = true; // be default we allow undetermined variables
+    }
+    
+            if (des->OperationName() == OperationNameOf(PastValueNode) || des->OperationName() == L'Delay')
         {
-            const ConfigRecordType& thisIO = config(ioName);
-            wstring readerType = thisIO(L'readerType', L'Cntk.Deserializers.TextFormat');
-    }
-    }
+            // special treament for arc with PastValue node as the children
+            // create a dummy node
+            ComputationNodeBasePtr pastValueNode = des;
+            wstring dummyName = des->GetName() + L'.dummy';
+            wstring out = msra::strfun::wstrprintf(L'node [ shape = box3d  , color = lightgray, style = \'filled\' , label = \'%ls\' ] ; \'%ls\'\n',
+                                                   (pastValueNode->GetName() + L'\\n(PastValue)').c_str(),
+                                                   dummyName.c_str());
+            line = out;
+            line += msra::strfun::wstrprintf(L'\'%ls\' -> \'%ls\' ; \n', dummyName.c_str(), srcname.c_str());
+        }
+        else if (des->OperationName() == OperationNameOf(FutureValueNode))
+        {
+            // special treament for arc with FutureValue node as the children
+            // create a dummy node
+            ComputationNodeBasePtr futureValueNode = des;
+            wstring dummyName = des->GetName() + L'.dummy';
+            wstring out = msra::strfun::wstrprintf(L'node [ shape = box3d  , color = red, style = \'filled\' , label = \'%ls\' ] ; \'%ls\'\n',
+                                                   (futureValueNode->GetName() + L'\\n(FutureValue)').c_str(),
+                                                   dummyName.c_str());
+            line = out;
+            line += msra::strfun::wstrprintf(L'\'%ls\' -> \'%ls\' ; \n', dummyName.c_str(), srcname.c_str());
+        }
+        else
+        {
+            line = msra::strfun::wstrprintf(L'\'%ls\' -> \'%ls\' ; \n', desname.c_str(), srcname.c_str());
+        }
     
-      while(true) {
-    if(!this->sync_.getPending(fname, mtype, rseqid)) {
-      iprot_->readMessageBegin(fname, mtype, rseqid);
-    }
-    if(seqid == rseqid) {
-      if (mtype == ::apache::thrift::protocol::T_EXCEPTION) {
-        ::apache::thrift::TApplicationException x;
-        x.read(iprot_);
-        iprot_->readMessageEnd();
-        iprot_->getTransport()->readEnd();
-        sentry.commit();
-        throw x;
-      }
-      if (mtype != ::apache::thrift::protocol::T_REPLY) {
-        iprot_->skip(::apache::thrift::protocol::T_STRUCT);
-        iprot_->readMessageEnd();
-        iprot_->getTransport()->readEnd();
-      }
-      if (fname.compare('query') != 0) {
-        iprot_->skip(::apache::thrift::protocol::T_STRUCT);
-        iprot_->readMessageEnd();
-        iprot_->getTransport()->readEnd();
-    }
-    }
-    }
-    
-    uint32_t InternalExtensionInfo::write(::apache::thrift::protocol::TProtocol* oprot) const {
-  uint32_t xfer = 0;
-  ::apache::thrift::protocol::TOutputRecursionTracker tracker(*oprot);
-  xfer += oprot->writeStructBegin('InternalExtensionInfo');
-    }
-    
-      // Join results..
-  for (auto& entry : hw_info) {
-    bool matched = false;
-    for (auto& row : results) {
-      auto serial = row.find('serial_number');
-      if (serial == row.end()) {
-        continue;
-      }
-    }
-    }
-    
-    TEST_F(PerfOutputTests, assigning_constructor) {
-  auto buf = std::array<char, 8>{};
-  auto from_obj = ebpf::PerfOutput<TestMessage>{};
-  from_obj.size_ = buf.size();
-  from_obj.fd_ = 42;
-  from_obj.data_ptr_ = static_cast<void*>(buf.data());
-    }
-    
-      ASSERT_GE(data.size(), 0ul);
-    
-    TEST_F(kernelExtensions, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from kernel_extensions');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'idx', IntType}
-  //      {'refs', IntType}
-  //      {'size', IntType}
-  //      {'name', NormalType}
-  //      {'version', NormalType}
-  //      {'linked_against', NormalType}
-  //      {'path', NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
-}
-    
-    class keychainAcls : public testing::Test {
- protected:
-  void SetUp() override {
-    setUpEnvironment();
-  }
-};
+            ValueContainer<ElemType>& vec = outputs[i2].m_buffer;
