2f8e70aa0f75e3b5529e0ff5534e9e768dc53670
SUNsung <weatherius@gmail.com>
2019-08-02 20:04:21 +0300

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2019-08-02 20:04:21

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,79 +1,139 @@
 
         
-            if record.timedout?(last_request_at) &&
-        !env['devise.skip_timeout'] &&
-        !proxy.remember_me_is_active?(record)
-      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
-      throw :warden, scope: scope, message: :timeout
-    end
-    
-        # Create magic predicates for verifying what module is activated by this map.
-    # Example:
-    #
-    #   def confirmable?
-    #     self.modules.include?(:confirmable)
-    #   end
-    #
-    def self.add_module(m)
-      class_eval <<-METHOD, __FILE__, __LINE__ + 1
-        def #{m}?
-          self.modules.include?(:#{m})
+              def test_invert_drop_table_without_a_block_nor_option
+        assert_raises(ActiveRecord::IrreversibleMigration) do
+          @recorder.inverse_of :drop_table, [:people_reminders]
         end
-      METHOD
-    end
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
       end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
     
-      def new
-    @broadcast = Broadcast.new
+      def test_index_in_create
+    def (ActiveRecord::Base.connection).data_source_exists?(*); false; end
+    
+        config.action_mailbox.queues = ActiveSupport::InheritableOptions.new \
+      incineration: :action_mailbox_incineration, routing: :action_mailbox_routing
+    
+          def self.symbols
+        type_klass::SET.symbols
+      end
+    end
   end
-    
-        wordsize = case @architecture
-    when nil, 'native'
-      %x{getconf LONG_BIT}.chomp # 'native' is current arch
-    when 'amd64'
-      '64'
-    when 'i386'
-      '32'
-    else
-      %x{getconf LONG_BIT}.chomp # default to native, the current arch
-    end
-    
-      # Output a pkgbuild pkg.
-  def output(output_path)
-    output_check(output_path)
-    
-        File.write(build_path('comment'),  self.description + '\n')
-    
-            self.dependencies << '#{name} #{cmp} #{version}'
-      end
-    end # if attributes[:python_dependencies?]
-  end # def load_package_info
-    
-      # Output a zipfile.
-  def output(output_path)
-    output_check(output_path)
-    realpath = Pathname.new(output_path).realdirpath.to_s
-    ::Dir.chdir(staging_path) do
-      safesystem('zip', '-9r', realpath, '.')
-    end
-  end # def output
-end # class FPM::Package::Tar
+end
 
     
-        if block_given? and opts[:stdin]
-      process.duplex = true
+    module VagrantPlugins
+  module Chef
+    module Provisioner
+      # This class is a base class where the common functionality shared between
+      # chef-solo and chef-client provisioning are stored. This is **not an actual
+      # provisioner**. Instead, {ChefSolo} or {ChefServer} should be used.
+      class Base < Vagrant.plugin('2', :provisioner)
+        include Vagrant::Util
+        include Vagrant::Util::Presence
+    
+    # Create a temporary directory where test vagrant will run. The reason we save
+# this to a constant is so we can clean it up later.
+VAGRANT_TEST_CWD = Dir.mktmpdir('vagrant-test-cwd')
+    
+          expect(a).to eq(b)
     end
+    
+            begin
+          FileUtils.cp(source, destination)
+        rescue Errno::EACCES
+          raise Errors::CopyPrivateKeyFailed,
+            source: source,
+            destination: destination
+        end
+      end
+    
+        it 'should be nil if no primary is specified in a multi-machine environment' do
+      environment = isolated_environment do |env|
+        env.vagrantfile(<<-VF)
+Vagrant.configure('2') do |config|
+  config.vm.box = 'base'
+  config.vm.define :foo
+  config.vm.define :bar
+end
+VF
+    
+    describe VagrantPlugins::ProviderVirtualBox::Provider do
+  let(:driver){ double('driver') }
+  let(:provider){ double('provider', driver: driver) }
+  let(:provider_config){ double('provider_config') }
+  let(:uid) { '1000' }
+  let(:machine){ double('machine', uid: uid, provider: provider, provider_config: provider_config) }
+    
+                widths[method] ||= 0
+            cur = entry.send(method).to_s.length
+            widths[method] = cur if cur > widths[method]
+          end
+        end
+    
+        records.each { |account| authorize(account.user, :reject?) }
+           .each { |account| SuspendAccountService.new.call(account, including_user: true, destroy: true, skip_distribution: true) }
+  end
+end
+
+    
+      private
+    
+            let(:object_json) do
+          {
+            id: 'https://example.com/actor#bar',
+            type: 'Note',
+            content: 'Lorem ipsum',
+            to: 'http://example.com/followers',
+            attributedTo: 'https://example.com/actor',
+          }
+        end
+    
+      def serializer_class
+    ProofProvider::Keybase::Serializer
+  end
+    
+      #
+  # Starts a new connecting thread
+  #
+  def add_handler(opts={})
+    
+        # Add the windows pe suffix to rexename
+    unless rexename.end_with?('.exe')
+      rexename << '.exe'
+    end
+    
+          # Output results to screen
+      print_status('     Host: #{http_proxy_host}')
+      print_status('     Port: #{http_proxy_port}')
+      print_status('     Username: #{http_proxy_username}')
+      print_status('     Password: #{http_proxy_password}')
+      print_status('')
+    end
+    
+        raw = create_payload_from_file rexe
+    
+    
+    
+    TLV_TYPE_WEBCAM_IMAGE       = TLV_META_TYPE_RAW     | (TLV_EXTENSIONS + 1)
+TLV_TYPE_WEBCAM_INTERFACE_ID= TLV_META_TYPE_UINT    | (TLV_EXTENSIONS + 2)
+TLV_TYPE_WEBCAM_QUALITY     = TLV_META_TYPE_UINT    | (TLV_EXTENSIONS + 3)
+TLV_TYPE_WEBCAM_NAME        = TLV_META_TYPE_STRING  | (TLV_EXTENSIONS + 4)
+    
+      def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name'          => 'OSX Meterpreter, Reverse HTTPS Inline',
+        'Description'   => 'Run the Meterpreter / Mettle server payload (stageless)',
+        'Author'        => [
+          'Adam Cammack <adam_cammack[at]rapid7.com>',
+          'Brent Cook <brent_cook[at]rapid7.com>',
+          'timwr'
+        ],
+        'Platform'      => 'osx',
+        'Arch'          => ARCH_X64,
+        'License'       => MSF_LICENSE,
+        'Handler'       => Msf::Handler::ReverseHttps,
+        'Session'       => Msf::Sessions::Meterpreter_x64_OSX
+      )
+    )
+  end
