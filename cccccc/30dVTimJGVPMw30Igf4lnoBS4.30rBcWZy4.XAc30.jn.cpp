a2f8b0cdab785987d71cb132a3b3b1ff645e3c55
SUNsung <weatherius@gmail.com>
2019-08-02 10:16:43 +0300

    Rewrite [30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp] File in: 2019-08-02 10:16:42

--- cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
+++ cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
@@ -1,376 +1,118 @@
 
         
-          // Build a substitution map to replace the protocol's \c Self and the type
-  // parameters of the requirement into a combined context that provides the
-  // type parameters of the conformance context and the parameters of the
-  // requirement.
-  auto selfType = cast<GenericTypeParamType>(
-      proto->getSelfInterfaceType()->getCanonicalType());
-    
-    void SILLayout::Profile(llvm::FoldingSetNodeID &id,
-                        CanGenericSignature Generics,
-                        ArrayRef<SILField> Fields) {
-  id.AddPointer(Generics.getPointer());
-  for (auto &field : Fields) {
-    id.AddPointer(field.getLoweredType().getPointer());
-    id.AddBoolean(field.isMutable());
+          // Verify outputs.
+  uint8_t* output_tensor = static_cast<uint8_t*>(stage.GetOutputs()->at(0));
+  for (int i = 0; i < kTotalElements; i++) {
+    EXPECT_EQ(output_tensor[i], static_cast<uint8_t>(6));
   }
+    
+    template <typename T>
+struct MatrixDiagPart<CPUDevice, T> {
+  static void Compute(OpKernelContext* context, const CPUDevice& device,
+                      typename TTypes<T, 3>::ConstTensor& input,
+                      typename TTypes<T>::Tensor& output,
+                      const Eigen::Index lower_diag_index,
+                      const Eigen::Index upper_diag_index,
+                      const Eigen::Index max_diag_len, const T padding_value) {
+    // 10 in cost_per_batch is from existing heuristic.
+    // TODO(penporn): Tune for the best constant in cost_per_batch.
+    const Eigen::Index num_diags = upper_diag_index - lower_diag_index + 1;
+    const Eigen::Index output_elements_in_batch = num_diags * max_diag_len;
+    const Eigen::Index cost_per_batch = 10 * output_elements_in_batch;
+    const Eigen::Index num_batches = input.dimension(0);
+    const Eigen::Index num_rows = input.dimension(1);
+    const Eigen::Index num_cols = input.dimension(2);
+    }
+    }
+    
+    
+    {  return metrics;
 }
-
     
-    %{
+    // A list of inputs of a given node of the TensorFlow/Eager graph.
+class OpInputs {
+ public:
+  explicit OpInputs(const TfLiteIntArray* indexes) {
+    for (int index : TfLiteIntArrayView(indexes)) {
+      inputs_.push_back(index);
     }
-    
-        /// Returns true if \p loc is inside one of Swift's synthetic buffers.
-    static bool isInSwiftBuffers(clang::FullSourceLoc loc) {
-      StringRef bufName = StringRef(loc.getManager().getBufferName(loc));
-      return bufName == ClangImporter::Implementation::moduleImportBufferName ||
-             bufName == ClangImporter::Implementation::bridgingHeaderBufferName;
-    }
-    
-    #include 'swift/Demangling/ManglingUtils.h'
-    
-    void GeneratePCHJobAction::anchor() {}
-
-    
-    bool ArgsToFrontendInputsConverter::readInputFilesFromCommandLine() {
-  bool hadDuplicates = false;
-  for (const Arg *A :
-       Args.filtered(options::OPT_INPUT, options::OPT_primary_file)) {
-    hadDuplicates = addFile(A->getValue()) || hadDuplicates;
+    forwardable_.resize(inputs_.size());
   }
-  return false; // FIXME: Don't bail out for duplicates, too many tests depend
-  // on it.
-}
-    
-    #undef cv_hal_split8u
-#define cv_hal_split8u TEGRA_SPLIT
-#undef cv_hal_split16u
-#define cv_hal_split16u TEGRA_SPLIT
-#undef cv_hal_split32s
-#define cv_hal_split32s TEGRA_SPLIT
-#undef cv_hal_split64s
-#define cv_hal_split64s(src, dst, len, cn) TEGRA_SPLIT64S(CAROTENE_NS::s64, src, dst, len, cn)
-    
-    
-    {    struct KeypointStore {
-        virtual void push(f32 kpX, f32 kpY, f32 kpSize, f32 kpAngle=-1, f32 kpResponse=0, s32 kpOctave=0, s32 kpClass_id=-1) = 0;
-        virtual ~KeypointStore() {};
-    };
-}
-    
-    
-    {    void operator() (const T * src0, const T * src1, T * dst) const
-    {
-        dst[0] = internal::saturate_cast<T>(src0[0] >= src1[0] ? (s64)src0[0] - src1[0] : (s64)src1[0] - src0[0]);
+  ~OpInputs() {}
     }
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+        OP_REQUIRES(
+        ctx, TensorShapeUtils::IsVector(shape_tensor.shape()),
+        errors::InvalidArgument('Input shape should be a vector, got shape: ',
+                                shape_tensor.shape().DebugString()));
+    int32 num_batches = shape_tensor.flat<int32>()(0);
+    
+    static constexpr int kMaxIterations = 1000;
+    
+    
+    
+    SECP256K1_INLINE static void secp256k1_fe_sqr_inner(uint64_t *r, const uint64_t *a) {
+/**
+ * Registers: rdx:rax = multiplication accumulator
+ *            r9:r8   = c
+ *            rcx:rbx = d
+ *            r10-r14 = a0-a4
+ *            r15     = M (0xfffffffffffff)
+ *            rdi     = r
+ *            rsi     = a / t?
+ */
+  uint64_t tmp1, tmp2, tmp3;
+__asm__ __volatile__(
+    'movq 0(%%rsi),%%r10\n'
+    'movq 8(%%rsi),%%r11\n'
+    'movq 16(%%rsi),%%r12\n'
+    'movq 24(%%rsi),%%r13\n'
+    'movq 32(%%rsi),%%r14\n'
+    'movq $0xfffffffffffff,%%r15\n'
+    }
+    
+    /** Encode a Bech32 string. Returns the empty string in case of failure. */
+std::string Encode(const std::string& hrp, const std::vector<uint8_t>& values);
+    
+        // Erase the data in small chunks
+    always_true_key_tester_t key_tester;
+    const uint64_t max_erased_per_pass = 100;
+    for (continue_bool_t done_erasing = continue_bool_t::CONTINUE;
+         done_erasing == continue_bool_t::CONTINUE;) {
+        scoped_ptr_t<txn_t> txn;
+        scoped_ptr_t<real_superblock_t> superblock;
+    }
+    
+    #if GTEST_HAS_GLOBAL_WSTRING
+  // Converts the given wide string to a narrow string using the UTF-8
+  // encoding, and streams the result to this Message object.
+  Message& operator <<(const ::wstring& wstr);
+#endif  // GTEST_HAS_GLOBAL_WSTRING
+    
+    #if GTEST_HAS_TYPED_TEST
+    
+    // Macros for testing exceptions.
+//
+//    * {ASSERT|EXPECT}_THROW(statement, expected_exception):
+//         Tests that the statement throws the expected exception.
+//    * {ASSERT|EXPECT}_NO_THROW(statement):
+//         Tests that the statement doesn't throw any exception.
+//    * {ASSERT|EXPECT}_ANY_THROW(statement):
+//         Tests that the statement throws an exception.
+    
+    
+    {  GTEST_DISALLOW_ASSIGN_(NativeArray);
 };
     
-        void operator() (const uint8x16_t & v_src0, const uint8x16_t & v_src1,
-                     uint8x16_t & v_dst) const
-    {
-        v_dst = vorrq_u8(v_src0, v_src1);
-    }
-    
-    void assertSupportedConfiguration(bool parametersSupported)
-{
-    if (!isSupportedConfiguration()) {
-        std::cerr << 'internal error: attempted to use an unavailable function' << std::endl;
-        std::abort();
-    }
-    }
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(s16, s8, 16,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.8 {d4-d5}, [%[src1]]                             \n\t'
-            'vmovl.s16 q3, d4                                      \n\t'
-            'vmovl.s16 q4, d5                                      \n\t'
-            'vcvt.f32.s32 q5, q3                                   \n\t'
-            'vcvt.f32.s32 q6, q4                                   \n\t'
-            'vmul.f32 q7, q5, q0                                   \n\t'
-            'vmul.f32 q8, q6, q0                                   \n\t'
-            'vadd.f32 q9, q7, q1                                   \n\t'
-            'vadd.f32 q10, q8, q1                                  \n\t'
-            'vcvt.s32.f32 q11, q9                                  \n\t'
-            'vcvt.s32.f32 q12, q10                                 \n\t'
-            'vqmovn.s32 d26, q11                                   \n\t'
-            'vqmovn.s32 d27, q12                                   \n\t'
-            'vqmovn.s16 d28, q13                                   \n\t'
-            'vst1.8 {d28}, [%[dst]]                                \n\t'
-            : /*no output*/
-            : [src1] 'r' (_src + i),
-              [dst] 'r' (_dst + i + 0),
-               'w'  (vscale), 'w' (vshift)
-            : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25','d26','d27','d28'
-        );
-    }
-})
-#else
-CVTS_FUNC(s16, s8, 16,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        int16x8_t vline = vld1q_s16(_src + i);
-        int32x4_t vline1_s32 = vmovl_s16(vget_low_s16 (vline));
-        int32x4_t vline2_s32 = vmovl_s16(vget_high_s16(vline));
-        float32x4_t vline1_f32 = vcvtq_f32_s32(vline1_s32);
-        float32x4_t vline2_f32 = vcvtq_f32_s32(vline2_s32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        int16x4_t vRes1 = vqmovn_s32(vline1_s32);
-        int16x4_t vRes2 = vqmovn_s32(vline2_s32);
-        int8x8_t vRes = vqmovn_s16(vcombine_s16(vRes1, vRes2));
-        vst1_s8(_dst + i, vRes);
-    }
-})
-#endif
-    
-                vec128 v_src = vld3q(src + js), v_dst;
-            v_src.val[0] = vrev64q(v_src.val[0]);
-            v_src.val[1] = vrev64q(v_src.val[1]);
-            v_src.val[2] = vrev64q(v_src.val[2]);
-    
-    // 'Joins' the blames from bundle1 and bundle2 into *this.
-void BlamerBundle::JoinBlames(const BlamerBundle& bundle1,
-                              const BlamerBundle& bundle2, bool debug) {
-  STRING debug_str;
-  IncorrectResultReason irr = incorrect_result_reason_;
-  if (irr != IRR_NO_TRUTH_SPLIT) debug_str = '';
-  if (bundle1.incorrect_result_reason_ != IRR_CORRECT &&
-      bundle1.incorrect_result_reason_ != IRR_NO_TRUTH &&
-      bundle1.incorrect_result_reason_ != IRR_NO_TRUTH_SPLIT) {
-    debug_str += 'Blame from part 1: ';
-    debug_str += bundle1.debug_;
-    irr = bundle1.incorrect_result_reason_;
+      // Smart pointer members.
+  void reset(T* ptr = NULL) {
+    depart();
+    capture(ptr);
   }
-  if (bundle2.incorrect_result_reason_ != IRR_CORRECT &&
-      bundle2.incorrect_result_reason_ != IRR_NO_TRUTH &&
-      bundle2.incorrect_result_reason_ != IRR_NO_TRUTH_SPLIT) {
-    debug_str += 'Blame from part 2: ';
-    debug_str += bundle2.debug_;
-    if (irr == IRR_CORRECT) {
-      irr = bundle2.incorrect_result_reason_;
-    } else if (irr != bundle2.incorrect_result_reason_) {
-      irr = IRR_UNKNOWN;
-    }
-  }
-  incorrect_result_reason_ = irr;
-  if (irr != IRR_CORRECT && irr != IRR_NO_TRUTH) {
-    SetBlame(irr, debug_str, nullptr, debug);
-  }
-}
-    
-    
-    {}  // namespace tesseract.
-
-    
-      // Returns the average sum of squared perpendicular error from a line
-  // through mean_point() in the direction dir.
-  double rms_orth(const FCOORD &dir) const;
-    
-    bool ParagraphModel::ValidBodyLine(int lmargin, int lindent,
-                                   int rindent, int rmargin) const {
-  switch (justification_) {
-    case JUSTIFICATION_LEFT:
-      return NearlyEqual(lmargin + lindent, margin_ + body_indent_,
-                         tolerance_);
-    case JUSTIFICATION_RIGHT:
-      return NearlyEqual(rmargin + rindent, margin_ + body_indent_,
-                         tolerance_);
-    case JUSTIFICATION_CENTER:
-      return NearlyEqual(lindent, rindent, tolerance_ * 2);
-    default:
-      // shouldn't happen
-      return false;
-  }
-}
-    
-      // Add entry to the heap, keeping the smallest item at the top, by operator<.
-  // Note that *entry is used as the source of operator=, but it is non-const
-  // to allow for a smart pointer to be contained within.
-  // Time = O(log n).
-  void Push(Pair* entry) {
-    int hole_index = heap_.size();
-    // Make a hole in the end of heap_ and sift it up to be the correct
-    // location for the new *entry. To avoid needing a default constructor
-    // for primitive types, and to allow for use of DoublePtr in the Pair
-    // somewhere, we have to incur a double copy here.
-    heap_.push_back(*entry);
-    *entry = heap_.back();
-    hole_index = SiftUp(hole_index, *entry);
-    heap_[hole_index] = *entry;
-  }
-    
-    template <typename Generator1, typename Generator2, typename Generator3,
-    typename Generator4>
-internal::CartesianProductHolder4<Generator1, Generator2, Generator3,
-    Generator4> Combine(
-    const Generator1& g1, const Generator2& g2, const Generator3& g3,
-        const Generator4& g4) {
-  return internal::CartesianProductHolder4<Generator1, Generator2, Generator3,
-      Generator4>(
-      g1, g2, g3, g4);
-}
-    
-    // A macro for testing Google Test assertions or code that's expected to
-// generate Google Test non-fatal failures.  It asserts that the given
-// statement will cause exactly one non-fatal Google Test failure with 'substr'
-// being part of the failure message.
-//
-// There are two different versions of this macro. EXPECT_NONFATAL_FAILURE only
-// affects and considers failures generated in the current thread and
-// EXPECT_NONFATAL_FAILURE_ON_ALL_THREADS does the same but for all threads.
-//
-// 'statement' is allowed to reference local variables and members of
-// the current object.
-//
-// The verification of the assertion is done correctly even when the statement
-// throws an exception or aborts the current function.
-//
-// Known restrictions:
-//   - You cannot stream a failure message to this macro.
-//
-// Note that even though the implementations of the following two
-// macros are much alike, we cannot refactor them to use a common
-// helper macro, due to some peculiarity in how the preprocessor
-// works.  If we do that, the code won't compile when the user gives
-// EXPECT_NONFATAL_FAILURE() a statement that contains a macro that
-// expands to code containing an unprotected comma.  The
-// AcceptsMacroThatExpandsToUnprotectedComma test in gtest_unittest.cc
-// catches that.
-//
-// For the same reason, we have to write
-//   if (::testing::internal::AlwaysTrue()) { statement; }
-// instead of
-//   GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement)
-// to avoid an MSVC warning on unreachable code.
-#define EXPECT_NONFATAL_FAILURE(statement, substr) \
-  do {\
-    ::testing::TestPartResultArray gtest_failures;\
-    ::testing::internal::SingleFailureChecker gtest_checker(\
-        &gtest_failures, ::testing::TestPartResult::kNonFatalFailure, \
-        (substr));\
-    {\
-      ::testing::ScopedFakeTestPartResultReporter gtest_reporter(\
-          ::testing::ScopedFakeTestPartResultReporter:: \
-          INTERCEPT_ONLY_CURRENT_THREAD, &gtest_failures);\
-      if (::testing::internal::AlwaysTrue()) { statement; }\
-    }\
-  } while (::testing::internal::AlwaysFalse())
-    
-    // Then, use TYPED_TEST_P() to define as many type-parameterized tests
-// for this type-parameterized test case as you want.
-TYPED_TEST_P(FooTest, DoesBlah) {
-  // Inside a test, refer to TypeParam to get the type parameter.
-  TypeParam n = 0;
-  ...
-}
-    
-    // The friend relationship of some of these classes is cyclic.
-// If we don't forward declare them the compiler might confuse the classes
-// in friendship clauses with same named classes on the scope.
-class Test;
-class TestCase;
-class TestInfo;
-class UnitTest;
-    
-    #define GTEST_ASSERT_(expression, on_failure) \
-  GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
-  if (const ::testing::AssertionResult gtest_ar = (expression)) \
-    ; \
-  else \
-    on_failure(gtest_ar.failure_message())
-    
-      // An enumeration of the three reasons that a test might be aborted.
-  enum AbortReason {
-    TEST_ENCOUNTERED_RETURN_STATEMENT,
-    TEST_THREW_EXCEPTION,
-    TEST_DID_NOT_DIE
-  };
-    
-      // Returns true if FilePath ends with a path separator, which indicates that
-  // it is intended to represent a directory. Returns false otherwise.
-  // This does NOT check that a directory (or file) actually exists.
-  bool IsDirectory() const;
-    
-    // Implementation #2 pre-calculates the primes and stores the result
-// in an array.
-class PreCalculatedPrimeTable : public PrimeTable {
- public:
-  // 'max' specifies the maximum number the prime table holds.
-  explicit PreCalculatedPrimeTable(int max)
-      : is_prime_size_(max + 1), is_prime_(new bool[max + 1]) {
-    CalculatePrimesUpTo(max);
-  }
-  virtual ~PreCalculatedPrimeTable() { delete[] is_prime_; }
-    }
-    
-    
-    {
-    {    // Adds the leak checker to the end of the test event listener list,
-    // after the default text output printer and the default XML report
-    // generator.
-    //
-    // The order is important - it ensures that failures generated in the
-    // leak checker's OnTestEnd() method are processed by the text and XML
-    // printers *before* their OnTestEnd() methods are called, such that
-    // they are attributed to the right test. Remember that a listener
-    // receives an OnXyzStart event *after* listeners preceding it in the
-    // list received that event, and receives an OnXyzEnd event *before*
-    // listeners preceding it.
-    //
-    // We don't need to worry about deleting the new listener later, as
-    // Google Test will do it.
-    listeners.Append(new LeakChecker);
-  }
-  return RUN_ALL_TESTS();
-}
-
-    
-     public:
-  // Gets the element in this node.
-  const E& element() const { return element_; }
-    
-    AuthPropertyIterator::~AuthPropertyIterator() {}
-    
-      CensusClientCallData()
-      : recv_trailing_metadata_(nullptr),
-        initial_on_done_recv_trailing_metadata_(nullptr),
-        initial_on_done_recv_message_(nullptr),
-        elapsed_time_(0),
-        recv_message_(nullptr),
-        recv_message_count_(0),
-        sent_message_count_(0) {
-    memset(&stats_bin_, 0, sizeof(grpc_linked_mdelem));
-    memset(&tracing_bin_, 0, sizeof(grpc_linked_mdelem));
-    memset(&path_, 0, sizeof(grpc_slice));
-    memset(&on_done_recv_trailing_metadata_, 0, sizeof(grpc_closure));
-    memset(&on_done_recv_message_, 0, sizeof(grpc_closure));
-  }
-    
-    #include <grpc/support/port_platform.h>
-    
-    // A thread pool interface for running callbacks.
-class ThreadPoolInterface {
- public:
-  virtual ~ThreadPoolInterface() {}
-    }
-    
-    using grpc::core::Bucket;
-using grpc::core::Histogram;
-using grpc::core::Metric;
-using grpc::core::Stats;
+  T* get() const { return value_; }
+  T* operator->() const { return value_; }
+  T& operator*() const { return *value_; }
