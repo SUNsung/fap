5ebc96bb7a6ef1f800dcf2b1330cafecc83c99db
SUNsung <weatherius@gmail.com>
2019-08-03 10:25:58 +0300

    Rewrite [16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp] File in: 2019-08-03 10:25:58

--- cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
+++ cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
@@ -1,234 +1,301 @@
 
         
-        
-    {}  // namespace internal
+        #include 'swift/Runtime/Config.h'
     
-    v8::Local<v8::Object> Promise::GetHandle() const {
-  return resolver_->GetPromise();
-}
-    
-      // Call object._init if we have one.
-  v8::Local<v8::Function> init;
-  if (Dictionary(isolate, wrapper).Get('_init', &init))
-    init->Call(isolate->GetCurrentContext(), wrapper, 0, nullptr).IsEmpty();
-    
-    v8::Local<v8::Value> Converter<v8::Local<v8::Object>>::ToV8(
-    v8::Isolate* isolate,
-    v8::Local<v8::Object> val) {
-  return val;
-}
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(Wrappable);
-};
-    
-    #include 'base/bind.h'
-#include 'base/logging.h'
-#include 'base/strings/utf_string_conversions.h'
-#include 'base/task/post_task.h'
-#include 'content/public/browser/browser_context.h'
-#include 'content/public/browser/browser_task_traits.h'
-#include 'content/public/browser/browser_thread.h'
-#include 'content/public/browser/resource_context.h'
-#include 'crypto/nss_util.h'
-#include 'crypto/nss_util_internal.h'
-#include 'net/base/net_errors.h'
-#include 'net/cert/nss_cert_database.h'
-#include 'net/cert/x509_certificate.h'
-    
-      // Set/Get the badge count.
-  bool SetBadgeCount(int count);
-  int GetBadgeCount();
-    
-    void absDiff(const Size2D &size,
-             const u16 *src0Base, ptrdiff_t src0Stride,
-             const u16 *src1Base, ptrdiff_t src1Stride,
-             u16 *dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, AbsDiff<u16>());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
-}
-    
-    
-    {    void operator() (const u8 * src0, const u8 * src1, u8 * dst) const
-    {
-        dst[0] = beta * src1[0] + alpha * src0[0];
-    }
-};
-    
-    namespace CAROTENE_NS {
-    }
-    
-        f32 alpha, beta, gamma;
-    typedef typename TypeTraits<T>::wide wtype;
-    wAdd<wtype> wideAdd;
-    wAdd(f32 _alpha, f32 _beta, f32 _gamma):
-        alpha(_alpha), beta(_beta), gamma(_gamma),
-        wideAdd(_alpha, _beta, _gamma) {}
-    
-            int32x2_t vs2 = vqneg_s32(vqadd_s32(vget_low_s32(vs), vget_high_s32(vs)));
-    
-                vec128 v_src = vld3q(src + js), v_dst;
-            v_src.val[0] = vrev64q(v_src.val[0]);
-            v_src.val[1] = vrev64q(v_src.val[1]);
-            v_src.val[2] = vrev64q(v_src.val[2]);
-    
-    # define TYPED_TEST_P(CaseName, TestName) \
-  namespace GTEST_CASE_NAMESPACE_(CaseName) { \
-  template <typename gtest_TypeParam_> \
-  class TestName : public CaseName<gtest_TypeParam_> { \
-   private: \
-    typedef CaseName<gtest_TypeParam_> TestFixture; \
-    typedef gtest_TypeParam_ TypeParam; \
-    virtual void TestBody(); \
-  }; \
-  static bool gtest_##TestName##_defined_ GTEST_ATTRIBUTE_UNUSED_ = \
-      GTEST_TYPED_TEST_CASE_P_STATE_(CaseName).AddTestName(\
-          __FILE__, __LINE__, #CaseName, #TestName); \
-  } \
-  template <typename gtest_TypeParam_> \
-  void GTEST_CASE_NAMESPACE_(CaseName)::TestName<gtest_TypeParam_>::TestBody()
-    
-    // This flag enables using colors in terminal output. Available values are
-// 'yes' to enable colors, 'no' (disable colors), or 'auto' (the default)
-// to let Google Test decide.
-GTEST_DECLARE_string_(color);
-    
-    // A function to convert T* into linked_ptr<T>
-// Doing e.g. make_linked_ptr(new FooBarBaz<type>(arg)) is a shorter notation
-// for linked_ptr<FooBarBaz<type> >(new FooBarBaz<type>(arg))
-template <typename T>
-linked_ptr<T> make_linked_ptr(T* ptr) {
-  return linked_ptr<T>(ptr);
-}
-    
-      // Returns true iff n is a prime number.
-  virtual bool IsPrime(int n) const = 0;
-    
-      // Try to divide n by every odd number i, starting from 3
-  for (int i = 3; ; i += 2) {
-    // We only have to try i up to the squre root of n
-    if (i > n/i) break;
-    }
-    
-    
-    {    return new_queue;
+    AvailabilityContext ASTContext::getSwift51Availability() {
+  auto target = LangOpts.Target;
+  
+  if (target.isMacOSX()) {
+    return AvailabilityContext(
+                            VersionRange::allGTE(llvm::VersionTuple(10,15,0)));
+  } else if (target.isiOS()) {
+    return AvailabilityContext(
+                            VersionRange::allGTE(llvm::VersionTuple(13,0,0)));
+  } else if (target.isWatchOS()) {
+    return AvailabilityContext(
+                            VersionRange::allGTE(llvm::VersionTuple(6,0,0)));
+  } else {
+    return AvailabilityContext::alwaysAvailable();
   }
-    
-    /* QuadricCallback */
-/*      GLU_ERROR */
-    
-    
-    {  resDict->lookup('XObject', &xObjDict);
-  if (xObjDict.isDict()) {
-    for (i = 0; i < xObjDict.dictGetLength(); ++i) {
-      xObjDict.dictGetValNF(i, &xObjRef);
-      xObjDict.dictGetVal(i, &xObj);
-      if (xObj.isStream()) {
-	xObj.streamGetDict()->lookup('Subtype', &subtypeObj);
-	if (subtypeObj.isName('Form')) {
-	  if (xObjRef.isRef()) {
-	    setupForm(xObjRef.getRef(), &xObj);
-	  } else {
-	    error(-1, 'Form in resource dict is not an indirect reference');
-	  }
-	}
-	subtypeObj.free();
-      }
-      xObj.free();
-      xObjRef.free();
-    }
-  }
-  xObjDict.free();
-}
-    
-    
-    {  return gTrue;
-}
-    
-      if (!trans || !trans->isDict ()) {
-    ok = gFalse;
-    return;
-  }
-    
-      check(state->getFillColorSpace(), state->getFillColor(),
-	state->getFillOpacity(), state->getBlendMode());
-  gdi = gFalse;
-  if ((level == psLevel1 || level == psLevel1Sep) &&
-      state->getFillColorSpace()->getMode() == csPattern) {
-    level1PSBug = gTrue;
-  }
-    
-    ProfileData::ProfileData() {
-	count = 0;
-	total = 0.0;
-	min = 0.0;
-	max = 0.0;
-}
-    
-    class ProfileData {
-public:
-    }
-    
-      GooString* getContentType() { return contentType; }
-  GooString* getFileName() { return fileName; }
-    
-    //------------------------------------------------------------------------
-    
-    #ifdef _WIN32
-#include <fcntl.h> // for O_BINARY
-#include <io.h>    // for setmode
-#endif
-#include <stdio.h>
-    
-            const std::unordered_map<StreamInformation, MinibatchData>& GetNextMinibatch(
-            size_t minibatchSizeInSamples,
-            size_t minibatchSizeInSequences,
-            size_t numberOfWorkers,
-            size_t workerRank,
-            const DeviceDescriptor& device = DeviceDescriptor::UseDefaultDevice()) override;
-    
-    
-    {        const auto& type = dict[typeKey].Value<std::wstring>();
-        if (type != typeValue) 
-        {
-            const auto& version = GetVersion(dict);
-            LogicError('Unexpected '%ls':'%ls' in place of '%ls':'%ls' (%s).',
-                       typeKey.c_str(), type.c_str(), typeKey.c_str(), typeValue.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
-        }
-    }
-    
-    
-    {
-    {        mutable bool m_isPacked;
-        mutable NDArrayViewPtr m_packedData;
-        mutable std::shared_ptr<Microsoft::MSR::CNTK::MBLayout> m_packedDataLayout;
-    };
 }
 
     
-            if (m_valueInitializer)
-            clone->SetValueInitialization(*m_valueInitializer, *m_valueInitializationDevice);
+    void ExprPatternInitialization::
+copyOrInitValueInto(SILGenFunction &SGF, SILLocation loc,
+                    ManagedValue value, bool isInit) {
+  assert(isInit && 'Only initialization is supported for refutable patterns');
+    }
     
-    #include <cassert>
+      /// getRetainN - Return a callable function for swift_retain_n.
+  Constant *getRetainN(CallInst *OrigI) {
+    if (RetainN)
+      return RetainN.get();
+    auto *ObjectPtrTy = getObjectPtrTy();
+    auto *Int32Ty = Type::getInt32Ty(getModule().getContext());
+    }
+    
+      // If we have exactly one input filename, and its extension is 'bc' or 'll',
+  // treat the input as LLVM_IR.
+  bool shouldTreatAsLLVM() const;
+  bool shouldTreatAsSIL() const;
+  bool shouldTreatAsModuleInterface() const;
+    
+    namespace c10d {
+namespace test {
+    }
+    }
+    
+    const std::string doc = R'DOC(
+  Single-feature representation:
+  - scalar features:
+    <feature full name> T
+  - list features:
+    <feature full name>.lengths int32
+    <feature full name>.values T
+  - map features:
+    <feature full name>.lengths int32
+    <feature full name>.keys K
+    <feature full name>.values V
+    
+    template <>
+void GluOp<float, CPUContext>::ComputeGlu(
+    const int M,
+    const int split_dim,
+    const int N,
+    const float* Xdata,
+    float* Ydata) {
+  const int xStride = 2 * split_dim * N;
+  const int yStride = split_dim * N;
+  for (int i = 0; i < M; ++i) {
+    const int idx = i * xStride;
+    const int idy = i * yStride;
+    for (int j = 0; j < split_dim; ++j) {
+      const int jN = j * N;
+      const int jdx1 = idx + jN;
+      const int jdx2 = idx + (j + split_dim) * N;
+      const int jdy = idy + jN;
+      for (int k = 0; k < N; ++k) {
+        const float x1 = Xdata[jdx1 + k];
+        const float x2 = Xdata[jdx2 + k];
+        Ydata[jdy + k] = x1 * sigmoid(x2);
+      }
+    }
+  }
+}
+    
+    OPERATOR_SCHEMA(Im2Col)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .SetDoc('The Im2Col operator from Matlab.')
+    .TensorInferenceFunction(
+        [](const OperatorDef& def, const vector<TensorShape>& in) {
+          ArgumentHelper helper(def);
+          auto pad = helper.GetSingleArgument<int>('pad', 0);
+          auto kernel_h = helper.GetSingleArgument<int>(
+              'kernel_h', helper.GetSingleArgument<int>('kernel', 0));
+          auto kernel_w = helper.GetSingleArgument<int>(
+              'kernel_w', helper.GetSingleArgument<int>('kernel', 0));
+          auto dilation_h = helper.GetSingleArgument<int>(
+              'dilation_h', helper.GetSingleArgument<int>('dilation', 1));
+          auto dilation_w = helper.GetSingleArgument<int>(
+              'dilation_w', helper.GetSingleArgument<int>('dilation', 1));
+          auto stride_h = helper.GetSingleArgument<int>(
+              'stride_h', helper.GetSingleArgument<int>('stride', 1));
+          auto stride_w = helper.GetSingleArgument<int>(
+              'stride_w', helper.GetSingleArgument<int>('stride', 1));
+          auto order = StringToStorageOrder(
+              helper.GetSingleArgument<string>('order', 'NCHW'));
+    }
+    
+    	ParallaxBackground *pb = Object::cast_to<ParallaxBackground>(get_parent());
+	if (pb) {
+    }
+    
+    void VideoStreamPlaybackGDNative::update_texture() {
+	PoolByteArray *pba = (PoolByteArray *)interface->get_videoframe(data_struct);
+    }
+    
+    template <class T>
+bool BSP_Tree::_test_convex(const Node *p_nodes, const Plane *p_planes, int p_current, const T &p_convex) const {
+    }
+    
+    #endif // STRING_NAME_H
+
+    
+    	Vector<snd_rawmidi_t *> connected_inputs;
+    
+    	for (int i = 0; i < connected_sources.size(); i++) {
+		MIDIEndpointRef source = connected_sources[i];
+		CFStringRef ref = NULL;
+		char name[256];
+    }
+    
+    #ifdef WINMIDI_ENABLED
+    
+    		struct BVHCmpY {
+    }
+    
+    godot_pool_vector3_array_read_access GDAPI *godot_pool_vector3_array_read_access_copy(const godot_pool_vector3_array_read_access *p_other) {
+	PoolVector<Vector3>::Read *other = (PoolVector<Vector3>::Read *)p_other;
+	return (godot_pool_vector3_array_read_access *)memnew(PoolVector<Vector3>::Read(*other));
+}
+const godot_vector3 GDAPI *godot_pool_vector3_array_read_access_ptr(const godot_pool_vector3_array_read_access *p_read) {
+	const PoolVector<Vector3>::Read *read = (const PoolVector<Vector3>::Read *)p_read;
+	return (const godot_vector3 *)read->ptr();
+}
+void GDAPI godot_pool_vector3_array_read_access_operator_assign(godot_pool_vector3_array_read_access *p_read, godot_pool_vector3_array_read_access *p_other) {
+	PoolVector<Vector3>::Read *read = (PoolVector<Vector3>::Read *)p_read;
+	PoolVector<Vector3>::Read *other = (PoolVector<Vector3>::Read *)p_other;
+	read->operator=(*other);
+}
+void GDAPI godot_pool_vector3_array_read_access_destroy(godot_pool_vector3_array_read_access *p_read) {
+	memdelete((PoolVector<Vector2>::Read *)p_read);
+}
+    
+            std::wstring ToString(uint32_t radix, NUMOBJ_FMT format, int32_t precision) const;
+        uint64_t ToUInt64_t() const;
+    
+    
+    {                for (size_t i = 0; i < length; i++)
+                {
+                    if (value[i] != L' ' && value[i] != m_numberGroupSeparator)
+                    {
+                        rawValue->append(1, value[i]);
+                    }
+                }
+            }
+    
+        Rational Rational::operator-() const
+    {
+        return Rational{ Number{ -1 * m_p.Sign(), m_p.Exp(), m_p.Mantissa() }, m_q };
+    }
+    
+    // returns a new rat structure with the acos of x->p/x->q
+extern void acosrat(_Inout_ PRAT* px, uint32_t radix, int32_t precision);
+    
+    #include 'pch.h'
+    
+        Number::Number(PNUMBER p) noexcept
+        : m_sign{ p->sign }
+        , m_exp{ p->exp }
+        , m_mantissa{}
+    {
+        m_mantissa.reserve(p->cdigit);
+        copy(p->mant, p->mant + p->cdigit, back_inserter(m_mantissa));
+    }
+    
+    static constexpr wchar_t DEFAULT_DEC_SEPARATOR = L'.';
+static constexpr wchar_t DEFAULT_GRP_SEPARATOR = L',';
+static constexpr wstring_view DEFAULT_GRP_STR = L'3;0';
+static constexpr wstring_view DEFAULT_NUMBER_STR = L'0';
+    
+            CalcInput(wchar_t decSymbol)
+            : m_hasExponent(false)
+            , m_hasDecimal(false)
+            , m_decPtIndex(0)
+            , m_decSymbol(decSymbol)
+            , m_base()
+            , m_exponent()
+        {
+        }
+    
+    NarratorAnnouncement::NarratorAnnouncement(
+    String ^ announcement,
+    String ^ activityId,
+    AutomationNotificationKind kind,
+    AutomationNotificationProcessing processing)
+    : m_announcement(announcement)
+    , m_activityId(activityId)
+    , m_kind(kind)
+    , m_processing(processing)
+{
+}
+    
+      _OutputStream &operator<< (const std::string &v) {
+    m_out << '\'' << Escape(v.c_str()) << '\'';
+    return *this;
+  }
+    
+    #include 'hphp/util/hash-map.h'
+#include 'hphp/util/lock.h'
+#include <memory>
 #include <string>
     
-        virtual bool OutputUsedInComputingInputNodesGradients() const override
-    {
-        // The DiagTimesNode does not require its output value for computing
-        // the gradients of its input nodes
-        return false;
+    template<class T, class V, bool case_sensitive, class ExtraType = int32_t>
+struct FixedStringMapBuilder {
+  using EqObject = typename std::conditional<
+    case_sensitive,
+    string_data_same,
+    string_data_isame
+  >::type;
+    }
+    
+      bool operator==(const std::string &v) const { return compare(v) == 0;}
+  bool operator!=(const std::string &v) const { return compare(v) != 0;}
+  bool operator>=(const std::string &v) const { return compare(v) >= 0;}
+  bool operator<=(const std::string &v) const { return compare(v) <= 0;}
+  bool operator> (const std::string &v) const { return compare(v) >  0;}
+  bool operator< (const std::string &v) const { return compare(v) <  0;}
+    
+    #if USE_JEMALLOC_EXTENT_HOOKS
+    
+    TRACE_SET_MOD(irlower);
+    
+    template<class vinst>
+void implArithO(Vout& v, IRLS& env, const IRInstruction* inst) {
+  auto const sf = implBinopSF<vinst>(v, env, inst);
+  v << jcc{CC_O, sf, {label(env, inst->next()), label(env, inst->taken())}};
+}
+    
+      JobContext jobContext { root, allowHipHopSyntax,
+    pathList, workerResults, result_q };
+  JobQueueDispatcher<ParseFactsWorker> dispatcher {
+    HackCFactsExtractor::get_workers_count(),
+      HackCFactsExtractor::get_workers_count(), 0, false, &jobContext};
+  dispatcher.start();
+    
+      static ArrayData* ToVec(ArrayData*, bool);
+  static ArrayData* ToDict(ArrayData*, bool);
+  static ArrayData* ToKeyset(ArrayData*, bool);
+    
+      VSR('<?hh print $_SERVER['REQUEST_URI'];',
+      '/string');
+    
+      std::thread positioning_thread_;
+    
+      apollo::cyber::WaitForShutdown();
+  delete pr;
+    
+     private:
+  void MakeDecisions(Frame* const frame,
+                     ReferenceLineInfo* const reference_line_info);
+  bool FindCrosswalks(ReferenceLineInfo* const reference_line_info);
+  bool CheckStopForObstacle(ReferenceLineInfo* const reference_line_info,
+                            const hdmap::CrosswalkInfoConstPtr crosswalk_ptr,
+                            const Obstacle& obstacle,
+                            const double stop_deceleration);
+    
+    /**
+ * @brief Cross product between two vectors.
+ *        One vector is formed by 1st and 2nd parameters of the function.
+ *        The other vector is formed by 3rd and 4th parameters of the function.
+ * @param x0 The x coordinate of the first vector.
+ * @param y0 The y coordinate of the first vector.
+ * @param x1 The x coordinate of the second vector.
+ * @param y1 The y coordinate of the second vector.
+ *
+ * @return The cross product result.
+ */
+double CrossProd(const double x0, const double y0, const double x1,
+                 const double y1);
+    
+        if (FLAGS_pre_detected_dir != '') {
+      std::string kitti_path =
+          FLAGS_pre_detected_dir + '/' + image_name + '.txt';
+      if (!LoadFromKitti(kitti_path, &frame)) {
+        AINFO << 'loading kitti result failed: ' << kitti_path;
+        continue;
+      }
+    } else {
+      base::Image8U image(cv_img.rows, cv_img.cols, base::Color::BGR);
     }
