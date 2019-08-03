0476f4d1520356c2b641b44cadf5a9128f763187
SUNsung <weatherius@gmail.com>
2019-08-03 21:58:03 +0300

    Rewrite [24KWiv0E7rV0s24Mu.0qWpPu4o24pGGnY93Q3Q24h2I.cpp] File in: 2019-08-03 21:58:02

--- cccccc/24KWiv0E7rV0s24Mu.0qWpPu4o24pGGnY93Q3Q24h2I.cpp
+++ cccccc/24KWiv0E7rV0s24Mu.0qWpPu4o24pGGnY93Q3Q24h2I.cpp
@@ -1,156 +1,181 @@
 
         
-            Rational Sin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Cos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Tan(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ASin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ACos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ATan(Rational const& rat, ANGLE_TYPE angletype);
-    
-            explicit Number(PNUMBER p) noexcept;
-        PNUMBER ToPNUMBER() const;
-    
-    #include 'Header Files/CalcEngine.h'
-#include 'Header Files/CalcUtils.h'
-    
-    void acoshrat(PRAT* px, uint32_t radix, int32_t precision)
-    
-        mulnumx(&((*pa)->pp), tmp->pq);
-    mulnumx(&(tmp->pp), (*pa)->pq);
-    remnum(&((*pa)->pp), tmp->pp, BASEX);
-    mulnumx(&((*pa)->pq), tmp->pq);
-    
-    #endif  // MXNET_GRAPH_ATTR_TYPES_H_
-
-    
-    #include <cstddef>
-#include <cstdlib>
-#include <sstream>
-#include <limits>
-#include <map>
-#include <set>
-#include <typeinfo>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <utility>
+        
+    {        g_pSwapChain->Present(1, 0); // Present with vsync
+        //g_pSwapChain->Present(0, 0); // Present without vsync
+    }
     
     
     {
-    {/*! \brief typedef the factory function of data iterator */
-typedef std::function<ImageAugmenter *()> ImageAugmenterFactory;
-/*!
- * \brief Registry entry for DataIterator factory functions.
- */
-struct ImageAugmenterReg
-    : public dmlc::FunctionRegEntryBase<ImageAugmenterReg,
-                                        ImageAugmenterFactory> {
-};
-//--------------------------------------------------------------
-// The following part are API Registration of Iterators
-//--------------------------------------------------------------
-/*!
- * \brief Macro to register image augmenter
- *
- * \code
- * // example of registering a mnist iterator
- * REGISTER_IMAGE_AUGMENTER(aug_default)
- * .describe('default augmenter')
- * .set_body([]() {
- *     return new DefaultAugmenter();
- *   });
- * \endcode
- */
-#define MXNET_REGISTER_IMAGE_AUGMENTER(name)                            \
-  DMLC_REGISTRY_REGISTER(::mxnet::io::ImageAugmenterReg, ImageAugmenterReg, name)
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_USE_OPENCV
+    {
+    {
+    {                    // Bind texture, Draw
+                    glBindTexture(GL_TEXTURE_2D, (GLuint)(intptr_t)pcmd->TextureId);
+#if IMGUI_IMPL_OPENGL_HAS_DRAW_WITH_BASE_VERTEX
+                    glDrawElementsBaseVertex(GL_TRIANGLES, (GLsizei)pcmd->ElemCount, sizeof(ImDrawIdx) == 2 ? GL_UNSIGNED_SHORT : GL_UNSIGNED_INT, (void*)(intptr_t)(pcmd->IdxOffset * sizeof(ImDrawIdx)), (GLint)pcmd->VtxOffset);
+#else
+                    glDrawElements(GL_TRIANGLES, (GLsizei)pcmd->ElemCount, sizeof(ImDrawIdx) == 2 ? GL_UNSIGNED_SHORT : GL_UNSIGNED_INT, (void*)(intptr_t)(pcmd->IdxOffset * sizeof(ImDrawIdx)));
+#endif
+                }
+            }
+        }
+    }
     
-     protected:
-  /*! \brief prefetcher parameters */
-  PrefetcherParam param_;
-  /*! \brief backend thread */
-  dmlc::ThreadedIter<DataBatch> iter;
-  /*! \brief internal batch loader */
-  std::unique_ptr<IIterator<TBlobBatch> > loader_;
+    // Implemented features:
+//  [X] Platform: Clipboard support.
+//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad'.
+//  [x] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'. FIXME: 3 cursors types are missing from GLFW.
+//  [X] Platform: Keyboard arrays indexed using GLFW_KEY_* codes, e.g. ImGui::IsKeyPressed(GLFW_KEY_SPACE).
     
-    enum class CompressionType {
-  kNone, kTwoBit
-};
+        // Show the window
+    ::ShowWindow(hwnd, SW_SHOWDEFAULT);
+    ::UpdateWindow(hwnd);
     
-    // C callback that can be used by TVM to extract
-// the WrapAsyncCall function.
-extern 'C' MXNET_DLL int MXTVMBridge(TVMFunctionHandle pregister) {
-  using tvm::runtime::PackedFunc;
-  const PackedFunc& fregister =
-      *static_cast<PackedFunc*>(pregister);
-  fregister('WrapAsyncCall', PackedFunc(mxnet::WrapAsyncCall));
-  return 0;
+    
+    {    g_fence->SetEventOnCompletion(fenceValue, g_fenceEvent);
+    WaitForSingleObject(g_fenceEvent, INFINITE);
+}
+    
+        // Restore the DX9 transform
+    g_pd3dDevice->SetTransform(D3DTS_WORLD, &last_world);
+    g_pd3dDevice->SetTransform(D3DTS_VIEW, &last_view);
+    g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &last_projection);
+    
+    #include 'imgui.h'
+#include 'imgui_impl_opengl2.h'
+#if defined(_MSC_VER) && _MSC_VER <= 1500 // MSVC 2008 or earlier
+#include <stddef.h>     // intptr_t
+#else
+#include <stdint.h>     // intptr_t
+#endif
+    
+    
+    {    printf('DestroyContext()\n');
+    ImGui::DestroyContext();
+    return 0;
 }
 
     
+    Rect Rect::unionWithRect(const Rect & rect) const
+{
+    float thisLeftX = origin.x;
+    float thisRightX = origin.x + size.width;
+    float thisTopY = origin.y + size.height;
+    float thisBottomY = origin.y;
     
+    if (thisRightX < thisLeftX)
     {
+        std::swap(thisRightX, thisLeftX);   // This rect has negative width
+    }
+    
+    if (thisTopY < thisBottomY)
     {
-    {  bool init_cudnn_;
-  cudnnDataType_t dtype_;
-  cudnnSpatialTransformerDescriptor_t st_desc_;
-  cudnnTensorDescriptor_t in_desc_;
-  cudnnTensorDescriptor_t out_desc_;
-  cudnnSamplerType_t sampler_;
-  #if CUDNN_MAJOR >= 5
-  cudnnTensorFormat_t format_;
-  #endif
-  BilinearSamplerParam param_;
-};
-#endif  // __CUDACC__ && CUDNN
-}  // namespace op
-}  // namespace mxnet
+        std::swap(thisTopY, thisBottomY);   // This rect has negative height
+    }
     
-    #include 'rocksdb/db.h'
-#include 'rocksdb/slice.h'
-#include 'rocksdb/options.h'
+    float otherLeftX = rect.origin.x;
+    float otherRightX = rect.origin.x + rect.size.width;
+    float otherTopY = rect.origin.y + rect.size.height;
+    float otherBottomY = rect.origin.y;
     
-    class MyFilter : public rocksdb::CompactionFilter {
- public:
-  bool Filter(int level, const rocksdb::Slice& key,
-              const rocksdb::Slice& existing_value, std::string* new_value,
-              bool* value_changed) const override {
-    fprintf(stderr, 'Filter(%s)\n', key.ToString().c_str());
-    ++count_;
-    assert(*value_changed == false);
-    return false;
+    if (otherRightX < otherLeftX)
+    {
+        std::swap(otherRightX, otherLeftX);   // Other rect has negative width
+    }
+    
+    if (otherTopY < otherBottomY)
+    {
+        std::swap(otherTopY, otherBottomY);   // Other rect has negative height
+    }
+    
+    float combinedLeftX = std::min(thisLeftX, otherLeftX);
+    float combinedRightX = std::max(thisRightX, otherRightX);
+    float combinedTopY = std::max(thisTopY, otherTopY);
+    float combinedBottomY = std::min(thisBottomY, otherBottomY);
+    
+    return Rect(combinedLeftX, combinedBottomY, combinedRightX - combinedLeftX, combinedTopY - combinedBottomY);
+}
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+    
+    void ActionManager::removeActionByTag(int tag, Node *target)
+{
+    CCASSERT(tag != Action::INVALID_TAG, 'Invalid tag value!');
+    CCASSERT(target != nullptr, 'target can't be nullptr!');
+    if (target == nullptr)
+    {
+        return;
+    }
+    }
+    
+        /** Resumes the target. All queued actions will be resumed.
+     *
+     * @param target    A certain target.
+     */
+    virtual void resumeTarget(Node *target);
+    
+    /** Pauses all running actions, returning a list of targets whose actions were paused.
+     *
+     * @return  A list of targets whose actions were paused.
+     */
+    virtual Vector<Node*> pauseAllRunningActions();
+    
+    /** Resume a set of targets (convenience function to reverse a pauseAllRunningActions call).
+     *
+     * @param targetsToResume   A set of targets need to be resumed.
+     */
+    virtual void resumeTargets(const Vector<Node*>& targetsToResume);
+    
+    /** Main loop of ActionManager.
+     * @param dt    In seconds.
+     */
+    virtual void update(float dt);
+    
+protected:
+    // declared in ActionManager.m
+    
+        /**
+     * get vertex count
+     * @return number of vertices
+     */
+    unsigned int getVertCount() const;
+    
+    /**
+     * get triangles count
+     * @return number of triangles
+     */
+    unsigned int getTrianglesCount() const;
+    
+    #endif // __MISCNODE_CCCLIPPING_NODE_H__
+
+    
+     protected:
+  bool loadDatabases(std::function<void(RedisClient::DatabaseList)> callback);
+    
+      virtual bool multiConnectionOperation() const = 0;
+    
+    unsigned qcompress::guessFormat(const QByteArray &val) {
+  if (val.size() > 2 && val.startsWith(QByteArray::fromHex('x1fx8b'))) {
+    return qcompress::GZIP;
   }
     }
     
-    #include <string>
+    QString ValueEditor::ExternalFormattersManager::formattersPath() {
+  if (m_formattersPath.isEmpty()) {
+    return QDir::toNativeSeparators(
+        QString('%1/%2').arg(ConfigManager::getConfigPath()).arg('formatters'));
+  } else {
+    return m_formattersPath;
+  }
+}
     
-    // Simple RAII wrapper class for Snapshot.
-// Constructing this object will create a snapshot.  Destructing will
-// release the snapshot.
-class ManagedSnapshot {
- public:
-  explicit ManagedSnapshot(DB* db);
-    }
     
-    #ifdef LUA
-#include <string>
-#include <vector>
-    
-      // Attempt to acquire lock.  Return OK on success, or other Status on failure.
-  // If returned status is OK, TransactionDB will eventually call UnLock().
-  virtual Status Lock() = 0;
-    
-    /*
- * Class:     org_rocksdb_Checkpoint
- * Method:    dispose
- * Signature: (J)V
- */
-void Java_org_rocksdb_Checkpoint_disposeInternal(JNIEnv* /*env*/,
-                                                 jobject /*jobj*/,
-                                                 jlong jhandle) {
-  auto* checkpoint = reinterpret_cast<rocksdb::Checkpoint*>(jhandle);
-  assert(checkpoint != nullptr);
-  delete checkpoint;
+    {  return m_selfPtr;
 }
