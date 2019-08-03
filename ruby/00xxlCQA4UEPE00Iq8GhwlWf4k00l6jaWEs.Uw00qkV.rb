15842d39c1e7780657bae93265af733e63b9316e
SUNsung <weatherius@gmail.com>
2019-08-03 20:16:13 +0300

    Rewrite [00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb] File in: 2019-08-03 20:16:13

--- ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
+++ ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
@@ -1,150 +1,39 @@
 
         
-          def self.invite_by_email(email, invited_by, topic = nil, group_ids = nil, custom_message = nil)
-    create_invite_by_email(email, invited_by,
-      topic: topic,
-      group_ids: group_ids,
-      custom_message: custom_message,
-      send_email: true
-    )
-  end
-    
-      context '#destroy' do
-    it 'should return the right response for an invalid group_id' do
-      max_id = Group.maximum(:id).to_i
-      delete '/admin/groups/#{max_id + 1}.json'
-      expect(response.status).to eq(404)
+            def log_file_info(s)
+      puts '    #{magenta s}'
     end
     
-      describe '#status' do
-    it 'works for failed jobs' do
-      job.failed_at = Time.now
-      expect(status(job)).to eq('<span class='label label-danger'>failed</span>')
+        def remove(key, *values)
+      set(key, Array(fetch(key)) - values)
     end
     
-            it 'kills no long active workers' do
-          mock.instance_of(HuginnScheduler).run!
-          mock.instance_of(DelayedJobWorker).run!
-          @agent_runner.send(:run_workers)
-          AgentRunner.class_variable_set(:@@agents, [DelayedJobWorker])
-          mock.instance_of(HuginnScheduler).stop!
-          @agent_runner.send(:run_workers)
-        end
+    http://capistranorb.com/documentation/advanced-features/custom-scm
+    
+          def servers_by_key
+        @servers_by_key ||= {}
       end
     
-      after(:each) do
-    @rufus_scheduler.shutdown(:wait)
-  end
+        def extract_tmux_config
+      options_hash = {}
     
-      describe 'migrating a hash' do
-    it 'should convert every attribute' do
-      expect(LiquidMigrator.convert_hash({'a' => '$.data', 'b' => 'This is a <$.test>'})).to eq(
-                                  {'a' => '$.data', 'b' => 'This is a {{test}}'}
-      )
+      factory :project_with_force_attach, class: Tmuxinator::Project do
+    transient do
+      file { yaml_load('spec/fixtures/detach.yml') }
     end
-    it 'should work with leading_dollarsign_is_jsonpath' do
-      expect(LiquidMigrator.convert_hash({'a' => '$.data', 'b' => 'This is a <$.test>'}, leading_dollarsign_is_jsonpath: true)).to eq(
-                                  {'a' => '{{data}}', 'b' => 'This is a {{test}}'}
-      )
-    end
-    it 'should use the corresponding *_path attributes when using merge_path_attributes'do
-      expect(LiquidMigrator.convert_hash({'a' => 'default', 'a_path' => '$.data'}, {leading_dollarsign_is_jsonpath: true, merge_path_attributes: true})).to eq(
-                                  {'a' => '{{data}}'}
-      )
-    end
-    it 'should raise an exception when encountering complex JSONPaths' do
-      expect { LiquidMigrator.convert_hash({'b' => 'This is <$.complex[2]>'}) }.
-        to raise_error('JSONPath '$.complex[2]' is too complex, please check your migration.')
-    end
-  end
     
-        @opts = {
-      'api_key' => '800deeaf-e285-9d62-bc90-j999c1973cc9',
-      'path' => 'trackings'
-    }
-    
-          # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
+          def show_continuation_prompt
+        say
+        print 'Press ENTER to continue.'
+        STDIN.getc
       end
     
-            routes.each do |module_name, actions|
-          [:path, :url].each do |path_or_url|
-            actions.each do |action|
-              action = action ? '#{action}_' : ''
-              method = :'#{action}#{module_name}_#{path_or_url}'
+          def version
+        if Tmuxinator::Doctor.installed?
+          tmux_version = `tmux -V`.split(' ')[1]
     
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
-    
-        def default_controllers(options)
-      mod = options[:module] || 'devise'
-      @controllers = Hash.new { |h,k| h[k] = '#{mod}/#{k}' }
-      @controllers.merge!(options[:controllers]) if options[:controllers]
-      @controllers.each { |k,v| @controllers[k] = v.to_s }
-    end
-    
-          def rsync_post(machine, opts)
-        if opts.key?(:chown) && !opts[:chown]
-          return
-        end
-    
-      let(:iso_env) do
-    # We have to create a Vagrantfile so there is a root path
-    env = isolated_environment
-    env.vagrantfile('')
-    env.create_vagrant_env
-  end
-    
-        it 'should receive a valid address' do
-      allow(provider_config).to receive(:compose).and_return(false)
-      allow(platform).to receive(:windows?).and_return(false)
-      allow(platform).to receive(:darwin?).and_return(false)
-      allow(driver_obj).to receive(:created?).and_return(true)
-      allow(driver_obj).to receive(:state).and_return(:running)
-      allow(driver_obj).to receive(:execute).with(:get_network_config).and_return(result)
-      allow(driver_obj).to receive(:inspect_container).and_return(network_settings)
-    
-      def test_font_helper_without_suffix
-    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
-  end
-    
-    def capture_logging(lvl=Logger::INFO)
-  old = Sidekiq.logger
-  begin
-    out = StringIO.new
-    logger = Logger.new(out)
-    logger.level = lvl
-    Sidekiq.logger = logger
-    yield
-    out.string
-  ensure
-    Sidekiq.logger = old
-  end
-end
-    
-    describe 'job scheduling' do
-  describe 'middleware' do
-    class SomeScheduledWorker
-      include Sidekiq::Worker
-      sidekiq_options :queue => :custom_queue
-      def perform(x)
+          it 'returns false' do
+        expect(described_class.editor?).to be_falsey
       end
     end
-    
-    describe 'Sidekiq::Testing' do
-  describe 'require/load sidekiq/testing.rb' do
-    before do
-      require 'sidekiq/testing'
-    end
-    
-    include Sidekiq::Util
+  end
