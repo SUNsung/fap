7e1d2543cca576e0048313fe90905cdf84182f98
SUNsung <weatherius@gmail.com>
2019-08-04 18:36:41 +0300

    Rewrite [12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp] File in: 2019-08-04 18:36:41

--- cccccc/12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp
+++ cccccc/12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp
@@ -1,293 +1,53 @@
 
         
-            ctx->kernel_data = new CAROTENE_NS::s16[kernel_width*kernel_height];
-    if(!ctx->kernel_data)
-        return CV_HAL_ERROR_UNKNOWN;
-    switch(kernel_type)
-    {
-    case CV_8UC1:
-        convert(ctx->ksize, (CAROTENE_NS::u8*)kernel_data, kernel_step, ctx->kernel_data, kernel_width);
-        break;
-    case CV_8SC1:
-        convert(ctx->ksize, (CAROTENE_NS::s8*)kernel_data, kernel_step, ctx->kernel_data, kernel_width);
-        break;
-    case CV_16UC1:
-        for(int j = 0; j < kernel_height; ++j)
-        {
-            std::memcpy(ctx->kernel_data + kernel_width * j, kernel_data + kernel_step * j, kernel_width * sizeof(int16_t));
-        }
-    default:
-        delete[] ctx->kernel_data;
-        delete ctx;
-        return CV_HAL_ERROR_NOT_IMPLEMENTED;
-    }
-    
-        internal::assertSupportedConfiguration();
-    
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const u8* src = internal::getRowPtr(srcBase, srcStride, i);
-        u8* dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t j = 0;
-    }
-    
-    
-    {
-    {}}
-    
-    inline float32x2_t vrsqrt_f32(float32x2_t val)
-{
-    float32x2_t e = vrsqrte_f32(val);
-    e = vmul_f32(vrsqrts_f32(vmul_f32(e, e), val), e);
-    e = vmul_f32(vrsqrts_f32(vmul_f32(e, e), val), e);
-    return e;
+        
+    {	mix_udata = p_userdata;
+	mix_callback = p_callback;
 }
     
-    
-    {                // make border
-                    if (border == BORDER_MODE_REPLICATE || border == BORDER_MODE_REFLECT)
-                    {
-                        tcurr = vsetq_lane_s16(vgetq_lane_s16(tcurr, 0),tcurr, 7);
-                    }
-                    else if (border == BORDER_MODE_CONSTANT)
-                    {
-                        tcurr = vsetq_lane_s16(borderValue, tcurr, 7);
-                    }
-                    else if (border == BORDER_MODE_REFLECT101)
-                    {
-                        tcurr = vsetq_lane_s16(vgetq_lane_s16(tcurr, 1),tcurr, 7);
-                    }
-                continue;
-            }
-    
-    #include <map>
-#include <string>
-#include <vector>
-    
-    /**
- * @brief Computes @f$ y = |x| @f$
- *
- * @param bottom input Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the inputs @f$ x @f$
- * @param top output Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the computed outputs @f$ y = |x| @f$
- */
-template <typename Dtype>
-class AbsValLayer : public NeuronLayer<Dtype> {
- public:
-  explicit AbsValLayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    /**
- * @brief Compute the index of the @f$ K @f$ max values for each datum across
- *        all dimensions @f$ (C \times H \times W) @f$.
- *
- * Intended for use after a classification layer to produce a prediction.
- * If parameter out_max_val is set to true, output is a vector of pairs
- * (max_ind, max_val) for each image. The axis parameter specifies an axis
- * along which to maximise.
- *
- * NOTE: does not implement Backwards operation.
- */
-template <typename Dtype>
-class ArgMaxLayer : public Layer<Dtype> {
- public:
-  /**
-   * @param param provides ArgMaxParameter argmax_param,
-   *     with ArgMaxLayer options:
-   *   - top_k (\b optional uint, default 1).
-   *     the number @f$ K @f$ of maximal items to output.
-   *   - out_max_val (\b optional bool, default false).
-   *     if set, output a vector of pairs (max_ind, max_val) unless axis is set then
-   *     output max_val along the specified axis.
-   *   - axis (\b optional int).
-   *     if set, maximise along the specified axis else maximise the flattened
-   *     trailing dimensions for each index of the first / num dimension.
-   */
-  explicit ArgMaxLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    /**
- * @brief Takes at least two Blob%s and concatenates them along either the num
- *        or channel dimension, outputting the result.
- */
-template <typename Dtype>
-class ConcatLayer : public Layer<Dtype> {
- public:
-  explicit ConcatLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    #endif  // CAFFE_CONV_LAYER_HPP_
+    #endif
 
     
-    #endif  // CAFFE_CROP_LAYER_HPP_
+    		md->unlock();
+    
+    #endif // MIDI_DRIVER_ALSAMIDI_H
+#endif // ALSAMIDI_ENABLED
 
     
-      vector<cudnnTensorDescriptor_t> bottom_descs_, top_descs_;
-  cudnnTensorDescriptor_t    bias_desc_;
-  cudnnFilterDescriptor_t      filter_desc_;
-  vector<cudnnConvolutionDescriptor_t> conv_descs_;
-  int bottom_offset_, top_offset_, bias_offset_;
+    #endif // MIDI_DRIVER_WINMIDI_H
+#endif // WINMIDI_ENABLED
+
     
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #include 'caffe/layers/pooling_layer.hpp'
-    
-    #include 'caffe/layers/neuron_layer.hpp'
-#include 'caffe/layers/sigmoid_layer.hpp'
-    
-    #include <dmlc/base.h>
-#include <dmlc/thread_group.h>
-#include <cstddef>
-#include <vector>
-#include <list>
-#include <thread>
-#include <utility>
-#include 'mxnet/base.h'
-    
-    
-    {
-    {/*! \brief typedef the factory function of data iterator */
-typedef std::function<ImageAugmenter *()> ImageAugmenterFactory;
-/*!
- * \brief Registry entry for DataIterator factory functions.
- */
-struct ImageAugmenterReg
-    : public dmlc::FunctionRegEntryBase<ImageAugmenterReg,
-                                        ImageAugmenterFactory> {
-};
-//--------------------------------------------------------------
-// The following part are API Registration of Iterators
-//--------------------------------------------------------------
-/*!
- * \brief Macro to register image augmenter
- *
- * \code
- * // example of registering a mnist iterator
- * REGISTER_IMAGE_AUGMENTER(aug_default)
- * .describe('default augmenter')
- * .set_body([]() {
- *     return new DefaultAugmenter();
- *   });
- * \endcode
- */
-#define MXNET_REGISTER_IMAGE_AUGMENTER(name)                            \
-  DMLC_REGISTRY_REGISTER(::mxnet::io::ImageAugmenterReg, ImageAugmenterReg, name)
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_USE_OPENCV
-    
-    template<>
-void EvalRandom<DEVICE, NegBinomialDistribution>(
-    const real_t &k,
-    const real_t &p,
-    const Resource &resource,
-    TBlob *ret,
-    RunContext ctx) {
-  typedef cpu xpu;  // No support for gpu for this distribution.
-  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
-  switch (ret->type_flag_) {
-  case mshadow::kFloat32:
-    {
-      mshadow::Random<xpu, float> *prnd = resource.get_random<xpu, float>(s);
-      mshadow::Tensor<xpu, 2, float> tmp = ret->FlatTo2D<xpu, float>(s);
-      prnd->SampleNegativeBinomial(&tmp, float(k), float(p));  // NOLINT(*)
-      break;
+    void GDAPI godot_pool_color_array_new_with_array(godot_pool_color_array *r_dest, const godot_array *p_a) {
+	PoolVector<Color> *dest = (PoolVector<Color> *)r_dest;
+	Array *a = (Array *)p_a;
+	memnew_placement(dest, PoolVector<Color>);
     }
-  case mshadow::kFloat64:
-    {
-      mshadow::Random<xpu, double> *prnd = resource.get_random<xpu, double>(s);
-      mshadow::Tensor<xpu, 2, double> tmp = ret->FlatTo2D<xpu, double>(s);
-      prnd->SampleNegativeBinomial(&tmp, double(k), double(p));  // NOLINT(*)
-      break;
-    }
-  default:
-    LOG(FATAL) << 'Random only support float32 and float64';
-  }
+    
+    #include 'core/error_macros.h'
+    
+    
+    {	return 0;
 }
+
     
-    namespace mxnet {
-namespace op {
-template<>
-Operator *CreateOp<cpu>(NativeOpParam param) {
-  return new NativeOp<cpu>(param);
-}
-    }
-    }
+        // Setup Platform/Renderer bindings
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX10_Init(g_pd3dDevice);
     
-    class CompactionPressureToken : public WriteControllerToken {
- public:
-  explicit CompactionPressureToken(WriteController* controller)
-      : WriteControllerToken(controller) {}
-  virtual ~CompactionPressureToken();
-};
-    
-    int main() {
-  // open DB
-  Options options;
-  options.create_if_missing = true;
-  DB* db;
-  OptimisticTransactionDB* txn_db;
-    }
-    
-      // Do some reads and writes to key 'x'
-  read_options.snapshot = txn_db->GetSnapshot();
-  s = txn->Get(read_options, 'x', &value);
-  txn->Put('x', 'x');
-    
-    #include 'gtest/gtest.h'
-    
-    int ObjectQualityInfo60C::longitude_vel_rms(const std::uint8_t* bytes,
-                                            int32_t length) const {
-  Byte t0(bytes + 2);
-  int32_t x = t0.get_byte(1, 5);
-    }
-    
-    #include 'gtest/gtest.h'
-    
-    double Spline1dSeg::ThirdOrderDerivative(const double x) const {
-  return third_order_derivative_(x);
-}
-    
-    // config detail: {'name': 'manual_input', 'offset': 0.0, 'precision': 0.001,
-// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 7,
-// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
-double Brakerpt6c::manual_input(const std::uint8_t* bytes,
-                                int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-    void Headlightrpt77::Parse(const std::uint8_t* bytes, int32_t length,
-                           ChassisDetail* chassis) const {
-  chassis->mutable_gem()->mutable_headlight_rpt_77()->set_output_value(
-      output_value(bytes, length));
-  chassis->mutable_gem()->mutable_headlight_rpt_77()->set_manual_input(
-      manual_input(bytes, length));
-  chassis->mutable_gem()->mutable_headlight_rpt_77()->set_commanded_value(
-      commanded_value(bytes, length));
-}
+        // Decide GL+GLSL versions
+#if __APPLE__
+    // GL 3.2 Core + GLSL 150
+    const char* glsl_version = '#version 150';
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = '#version 130';
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#endif
