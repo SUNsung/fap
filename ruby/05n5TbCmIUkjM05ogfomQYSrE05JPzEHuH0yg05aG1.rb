fedaf13e20226878549332f45f59d4995a941e1c
SUNsung <weatherius@gmail.com>
2019-08-04 20:01:30 +0300

    Rewrite [05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb] File in: 2019-08-04 20:01:30

--- ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
+++ ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
@@ -1,131 +1,127 @@
 
         
-                    first_param, second_param = parameters(method_node)
-            next if !node_equals?(first_param, 'npm') ||
-                    !node_equals?(second_param, 'install')
-    
-        it 'allows specifying dependencies after certain version' do
-      spec.uses_from_macos('foo', after: :el_capitan)
-    
-      describe '#suggestion' do
-    context 'without specific version' do
-      its(:suggestion) { is_expected.to match(/brew cask install adoptopenjdk/) }
-      its(:cask) { is_expected.to eq('adoptopenjdk') }
+            # Returns source file path.
+    def path
+      # Static file is from a collection inside custom collections directory
+      if !@collection.nil? && !@site.config['collections_dir'].empty?
+        File.join(*[@base, @site.config['collections_dir'], @dir, @name].compact)
+      else
+        File.join(*[@base, @dir, @name].compact)
+      end
     end
     
-          def minimum_version
-        case MacOS.version
-        when '10.15' then '11.0.0'
-        when '10.14' then '10.0.0'
-        when '10.13' then '9.0.0'
-        when '10.12' then '8.0.0'
-        else              '1.0.0'
+          mutable false
+    
+            # config[kramdown][syntax_higlighter] >
+        #   config[kramdown][enable_coderay] >
+        #   config[highlighter]
+        # Where `enable_coderay` is now deprecated because Kramdown
+        # supports Rouge now too.
+        def highlighter
+          return @highlighter if @highlighter
+    
+          Jekyll.logger.info 'Total allocated: #{total_allocated_output} (#{report.total_allocated} objects)'.cyan
+      Jekyll.logger.info 'Total retained:  #{total_retained_output} (#{report.total_retained} objects)'.cyan
+    
+    require 'benchmark/ips'
+require 'pathutil'
+    
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+            def handle_websockets_event(websocket)
+          websocket.onopen { |handshake| connect(websocket, handshake) }
+          websocket.onclose { disconnect(websocket) }
+          websocket.onmessage { |msg| print_message(msg) }
+          websocket.onerror { |error| log_error(error) }
+        end
+    
+    RSpec.describe UnreservedUsernameValidator, type: :validator do
+  describe '#validate' do
+    before do
+      allow(validator).to receive(:reserved_username?) { reserved_username }
+      validator.validate(account)
+    end
+    
+        before do
+      post :follow, params: { id: other_account.id }
+    end
+    
+        @disallowed_hashtags = Setting.disallowed_hashtags.nil? ? [] : Setting.disallowed_hashtags
+    @disallowed_hashtags = @disallowed_hashtags.split(' ') if @disallowed_hashtags.is_a? String
+    @disallowed_hashtags = @disallowed_hashtags.map(&:downcase)
+  end
+end
+
+    
+      private
+    
+      describe 'email' do
+    before do
+      ActionMailer::Base.deliveries.clear
+    
+      private
+    
+        valid_emails, invalid_emails = emails.partition {|email| valid_email?(email) }
+    
+      def destroy
+    if like_service.destroy(params[:id])
+      head :no_content
+    else
+      render plain: I18n.t('likes.destroy.error'), status: 404
+    end
+  end
+    
+      respond_to :html, :mobile
+  respond_to :json, :only => :show
+    
+        it 'should use the default URL if no template URL is given' do
+      template_url = 'https://github.com/CocoaPods/pod-template.git'
+      @sut.any_instance.expects(:git!).with(['clone', template_url, 'TestPod']).once
+      run_command('lib', 'create', 'TestPod')
+    end
+  end
+end
+
+    
+        def self.extended(base)
+      # Make Context methods available
+      # WORKAROUND: Bacon auto-inherits singleton methods to child contexts
+      # by using the runtime and won't include methods in modules included
+      # by the parent context. We have to ensure that the methods will be
+      # accessible by the child contexts by defining them as singleton methods.
+      TemporaryRepos.instance_methods.each do |method|
+        unbound_method = base.method(method).unbind
+        base.define_singleton_method method do |*args, &b|
+          unbound_method.bind(self).call(*args, &b)
         end
       end
     
-            new_cask.config = Config.global.merge(old_config)
-    
-    describe Cask::Cmd::Cache, :cask do
-  let(:local_transmission) {
-    Cask::CaskLoader.load(cask_path('local-transmission'))
-  }
-    
-      # When false, do not use translations for labels.
-  # config.translate_labels = true
-    
-        ActivityTracker.increment('activity:interactions')
-    
-        return if skip_distribution?
-    
-      def signed_payload
-    @signed_payload ||= Oj.dump(serialize_payload(@account, ActivityPub::UpdateSerializer, signer: @account, sign_with: @options[:sign_with]))
-  end
-end
-
-    
-      before do
-    sign_in user, scope: :user
-  end
-    
-          context 'reserved_username?' do
-        let(:reserved_username) { true }
-    
-    RSpec.describe UrlValidator, type: :validator do
-  describe '#validate_each' do
-    before do
-      allow(validator).to receive(:compliant?).with(value) { compliant }
-      validator.validate_each(record, attribute, value)
-    end
-    
-    describe FPM::Package::Snap do
-  let(:target) { Stud::Temporary.pathname + '.snap' }
-  after do
-    subject.cleanup
-    File.unlink(target) if File.exist?(target)
-  end
-    
-      config.vm.define 'freebsd10' do |freebsd10|
-    freebsd10.vm.box = 'tjay/freebsd-10.1'
-  end
-    
-      def to_s(format=nil)
-    return super('NAME_FULLVERSION_ARCH.TYPE') if format.nil?
-    return super(format)
-  end
-    
-        logger.info('Installing pear package', :package => input_package,
-                  :directory => staging_path)
-    ::Dir.chdir(staging_path) do
-      safesystem('pear', '-c', config, 'install', '-n', '-f', input_package)
-    end
-    
-      option '--group', 'GROUP',
-    'Set the group to GROUP in the prototype file.',
-    :default => 'root'
-    
-        def tmux_pre_command
-      _send_target(tab.pre.shellescape) if tab.pre
-    end
-    
-      factory :nowindows_project, class: Tmuxinator::Project do
-    transient do
-      file { yaml_load('spec/fixtures/nowindows.yml') }
-    end
-    
-      describe '#hook_on_project_start' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_start' }
-    end
-  end
-  describe '#hook_on_project_first_start' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_first_start' }
-    end
-  end
-  describe '#hook_on_project_restart' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_restart' }
-    end
-  end
-  describe '#hook_on_project_exit' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_exit' }
-    end
-  end
-  describe '#hook_on_project_stop' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_stop' }
-    end
-  end
-end
-
-    
-      describe '#socket' do
-    context 'socket path is present' do
-      before do
-        allow(project).to receive_messages(socket_path: '/tmp')
-      end
-    
-    # Copied from minitest.
-def capture_io
-  require 'stringio'
+              def add_developer_files
+            UI.message '- Adding development pod helper files' do
+              file_accessors.each do |file_accessor|
+                pod_name = file_accessor.spec.name
+                next unless sandbox.local?(pod_name)
+                root_name = Specification.root_name(pod_name)
+                paths = file_accessor.developer_files
+                next if paths.empty?
+                group = pods_project.group_for_spec(root_name, :developer)
+                paths.each do |path|
+                  ref = pods_project.add_file_reference(path, group, false)
+                  if path.extname == '.podspec'
+                    pods_project.mark_ruby_file_ref(ref)
+                  end
+                end
+              end
+            end
+          end
