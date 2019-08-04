5859c7edd7da30b3d74e00e59691e6609a47b18f
SUNsung <weatherius@gmail.com>
2019-08-04 06:12:54 +0300

    Rewrite [522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py] File in: 2019-08-04 06:12:54

--- pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
+++ pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
@@ -1,141 +1,109 @@
 
         
         
-def view_with_secure(request):
-    'A view that indicates if the request was secure'
-    response = HttpResponse()
-    response.test_was_secure_request = request.is_secure()
-    response.test_server_port = request.META.get('SERVER_PORT', 80)
-    return response
+def double_redirect_view(request):
+    'A view that redirects all requests to a redirection view'
+    return HttpResponseRedirect('/permanent_redirect_view/')
     
-        def as_oracle(self, compiler, connection, **extra_context):
-        if self.output_field.get_internal_type() == 'DurationField':
-            expression = self.get_source_expressions()[0]
-            options = self._get_repr_options()
-            from django.db.backends.oracle.functions import IntervalToSeconds, SecondsToInterval
-            return compiler.compile(
-                SecondsToInterval(self.__class__(IntervalToSeconds(expression), **options))
-            )
-        return super().as_sql(compiler, connection, **extra_context)
     
-        @property
-    def fields(self):
-        'Return a list of fields in the Feature.'
-        return [
-            force_str(
-                capi.get_field_name(capi.get_field_defn(self._layer._ldefn, i)),
-                self.encoding,
-                strings_only=True
-            ) for i in range(self.num_fields)
-        ]
+class ConsigliereInline(admin.TabularInline):
+    model = Consigliere
     
-            For raster formats using Persistent Auxiliary Metadata (PAM) services,
-        the statistics might be cached in an auxiliary file.
+        def test_case_aggregate(self):
+        agg = Sum(
+            Case(When(friends__age=40, then=F('friends__age'))),
+            filter=Q(friends__name__startswith='test'),
+        )
+        self.assertEqual(Author.objects.aggregate(age=agg)['age'], 80)
+    
+        def test_gin_parameters(self):
+        index_name = 'integer_array_gin_params'
+        index = GinIndex(fields=['field'], name=index_name, fastupdate=True, gin_pending_list_limit=64)
+        with connection.schema_editor() as editor:
+            editor.add_index(IntegerArrayModel, index)
+        constraints = self.get_constraints(IntegerArrayModel._meta.db_table)
+        self.assertEqual(constraints[index_name]['type'], 'gin')
+        self.assertEqual(constraints[index_name]['options'], ['gin_pending_list_limit=64', 'fastupdate=on'])
+        with connection.schema_editor() as editor:
+            editor.remove_index(IntegerArrayModel, index)
+        self.assertNotIn(index_name, self.get_constraints(IntegerArrayModel._meta.db_table))
+    
+    
+class RunTestsExceptionHandlingTests(unittest.TestCase):
+    def test_run_checks_raises(self):
         '''
-        # Prepare array with arguments for capi function
-        smin, smax, smean, sstd = c_double(), c_double(), c_double(), c_double()
-        stats_args = [
-            self._ptr, c_int(approximate), byref(smin), byref(smax),
-            byref(smean), byref(sstd), c_void_p(), c_void_p(),
-        ]
+        Teardown functions are run when run_checks() raises SystemCheckError.
+        '''
+        with mock.patch('django.test.runner.DiscoverRunner.setup_test_environment'), \
+                mock.patch('django.test.runner.DiscoverRunner.setup_databases'), \
+                mock.patch('django.test.runner.DiscoverRunner.build_suite'), \
+                mock.patch('django.test.runner.DiscoverRunner.run_checks', side_effect=SystemCheckError), \
+                mock.patch('django.test.runner.DiscoverRunner.teardown_databases') as teardown_databases, \
+                mock.patch('django.test.runner.DiscoverRunner.teardown_test_environment') as teardown_test_environment:
+            runner = DiscoverRunner(verbosity=0, interactive=False)
+            with self.assertRaises(SystemCheckError):
+                runner.run_tests(['test_runner_apps.sample.tests_sample.TestDjangoTestCase'])
+            self.assertTrue(teardown_databases.called)
+            self.assertTrue(teardown_test_environment.called)
     
     
-def source_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
-    ref = 'https://github.com/scrapy/scrapy/blob/master/' + text
-    set_classes(options)
-    node = nodes.reference(rawtext, text, refuri=ref, **options)
-    return [node], []
+def make_setting_element(setting_data, app, fromdocname):
+    refnode = make_refnode(app.builder, fromdocname,
+                           todocname=setting_data['docname'],
+                           targetid=setting_data['refid'],
+                           child=nodes.Text(setting_data['setting_name']))
+    p = nodes.paragraph()
+    p += refnode
     
-            # Broken links can't be fixed and
-        # I am not sure what do with the local ones.
-        if errortype.lower() in ['broken', 'local']:
-            print('Not Fixed: ' + line)
-        else:
-            # If this is a new file
-            if newfilename != _filename:
+        # Max concurrency is limited by global CONCURRENT_REQUESTS setting
+    max_concurrent_requests = 8
+    # Requests per second goal
+    qps = None # same as: 1 / download_delay
+    download_delay = None
+    # time in seconds to delay server responses
+    latency = None
+    # number of slots to create
+    slots = 1
     
-        scrapy runspider qpsclient.py --loglevel=INFO --set RANDOMIZE_DOWNLOAD_DELAY=0 --set CONCURRENT_REQUESTS=50 -a qps=10 -a latency=0.3
     
-    # Scrapy version
-import pkgutil
-__version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
-version_info = tuple(int(v) if v.isdigit() else v
-                     for v in __version__.split('.'))
-del pkgutil
+class _BenchSpider(scrapy.Spider):
+    '''A spider that follows all links'''
+    name = 'follow'
+    total = 10000
+    show = 20
+    baseurl = 'http://localhost:8998'
+    link_extractor = LinkExtractor()
     
-        def run(self, args, opts):
-        if len(args) != 1 or not is_url(args[0]):
-            raise UsageError()
-        cb = lambda x: self._print_response(x, opts)
-        request = Request(args[0], callback=cb, dont_filter=True)
-        # by default, let the framework handle redirects,
-        # i.e. command handles all codes expect 3xx
-        if not opts.no_redirect:
-            request.meta['handle_httpstatus_list'] = SequenceExclude(range(300, 400))
-        else:
-            request.meta['handle_httpstatus_all'] = True
+            self._download_http = httpdownloadhandler(settings).download_request
     
-        def run(self, args, opts):
-        settings = self.crawler_process.settings
-        if opts.get:
-            s = settings.get(opts.get)
-            if isinstance(s, BaseSettings):
-                print(json.dumps(s.copy_to_dict()))
-            else:
-                print(s)
-        elif opts.getbool:
-            print(settings.getbool(opts.getbool))
-        elif opts.getint:
-            print(settings.getint(opts.getint))
-        elif opts.getfloat:
-            print(settings.getfloat(opts.getfloat))
-        elif opts.getlist:
-            print(settings.getlist(opts.getlist))
-
+            # XXX: Google parses at least first 100k bytes; scrapy's redirect
+        # middleware parses first 4k. 4k turns out to be insufficient
+        # for this middleware, and parsing 100k could be slow.
+        # We use something in between (32K) by default.
+        self.lookup_bytes = settings.getint('AJAXCRAWL_MAXSIZE', 32768)
     
-            general form:
-        @returns request(s)/item(s) [min=1 [max]]
+        def __init__(self):
+        self._formats = {
+            'tar': self._is_tar,
+            'zip': self._is_zip,
+            'gz': self._is_gzip,
+            'bz2': self._is_bzip2
+        }
     
-        class _v19_S3Connection(S3Connection):
-        '''A dummy S3Connection wrapper that doesn't do any synchronous download'''
-        def _mexe(self, method, bucket, key, headers, *args, **kwargs):
-            return headers
+        def spider_opened(self, spider):
+        self._timeout = getattr(spider, 'download_timeout', self._timeout)
     
-        def timeout(self):
-        self.transport.loseConnection()
     
-    See documentation in docs/topics/downloader-middleware.rst
-'''
-    
-        def __init__(self, timeout=180):
-        self._timeout = timeout
-    
-    # If extensions (or modules to document with autodoc) are in another
-# directory, add these directories to sys.path here. If the directory is
-# relative to the documentation root, use os.path.abspath to make it
-# absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-    
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
-    
-        References:
-        tflearn.prelu
-    '''
-    if channel_shared:
-        alpha_shape = get_shape(x)[-1:]
-    else:
-        alpha_shape = [1]
+def rand_center(data, k):
+    '''随机采样 k 个样本作为聚类中心'''
+    centers = np.array(random.sample(list(data), k))
+    return centers
     
             a = permute(a, [0, 2, 1])  # [batch_size, n_step, n_input]
         o = tf.multiply(x, a)  # # [batch_size, n_step, n_input]
     
-    l2_regularizer = L1L2Regularizer(l2=0.01)
     
     
-def permute(x, perm):
-    '''
-    Examples:
-        x.shape == [128, 32, 1]
-        x = permute(x, [0, 2, 1])
-        x.shape == [128, 1, 32]
+            y.shape == [128, 64, 32]
+        y = permute(x, [2, 1, 0])
+        y.shape == [32, 64, 128]
