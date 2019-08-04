3c09dd76cb9e67c7086ab880d8edca64bd910818
SUNsung <weatherius@gmail.com>
2019-08-04 19:38:01 +0300

    Rewrite [313vaQKhusjM231URh34.iDTMw31w6tgg6WKLfg312rF.rb] File in: 2019-08-04 19:38:01

--- ruby/313vaQKhusjM231URh34.iDTMw31w6tgg6WKLfg312rF.rb
+++ ruby/313vaQKhusjM231URh34.iDTMw31w6tgg6WKLfg312rF.rb
@@ -1,168 +1,152 @@
-# Searches and reads file present on GitLab installation directory
-module Gitlab
-  module Template
-    module Finders
-      class GlobalTemplateFinder < BaseTemplateFinder
-        def initialize(base_dir, extension, categories = {})
-          @categories = categories
-          @extension  = extension
-          super(base_dir)
-        end
-    
-          expect(digest).to match %r{\A[A-Za-z0-9+/=]+\z}
-      expect(digest).to eq digest.strip
-    end
-  end
-    
-            [query, filters]
-      end
-    
-            def diffs
-          @diffs ||= diffable.raw_diffs(diff_options)
-        end
-    
-            if watch_ivar
-          # wait until event is proceeded
-          tries = 0
-          while tries < 5 && instance_variable_get(watch_ivar) == orig_ivar
-            seconds = 2 ** tries # sleep at most 31s in total
-            $stderr.puts 'test_win32ole_event.rb: retrying and sleeping #{seconds}s until #{watch_ivar} is changed from #{orig_ivar.inspect}...'
-            WIN32OLE_EVENT.message_loop
-            sleep(seconds)
-            tries += 1
+
+        
+                  should 'return dir correctly for index page' do
+            @page = setup_page('/contacts', 'index.html')
+            assert_equal '/contacts/', @page.dir
           end
         end
       end
     
-      def test_untainted_numeric
-    bug8945 = '[ruby-core:57346] [Bug #8945] Numerics never be tainted'
-    b = RbConfig::LIMITS['FIXNUM_MAX'] + 1
-    tainted = [0, 1.0, 1.72723e-77, b].select do |x|
-      Marshal.load(Marshal.dump(x).taint).tainted?
-    end
-    assert_empty(tainted.map {|x| [x, x.class]}, bug8945)
-  end
+    Nullam luctus fermentum est id blandit. Phasellus consectetur ullamcorper
+ligula, at finibus eros laoreet id. Etiam sit amet est in libero efficitur
+tristique. Ut nec magna augue. Quisque ut fringilla lacus, ac dictum enim.
+Aliquam vel ornare mauris. Suspendisse ornare diam tempor nulla facilisis
+aliquet. Sed ultrices placerat ultricies.
+TEXT
     
-        gz.pos.should == 0
+      Jekyll::Command.subclasses.each { |c| c.init_with_program(p) }
     
-      before :each do
-    @data = '12345abcde'
-    @zip = [31, 139, 8, 0, 44, 220, 209, 71, 0, 3, 51, 52, 50, 54, 49, 77,
-            76, 74, 78, 73, 5, 0, 157, 5, 0, 36, 10, 0, 0, 0].pack('C*')
-    @io = StringIO.new @zip
-  end
+          def after_tags(tags); end
     
-          it 'decrements pos' do
-        @gz.ungetc 'x'
-        @gz.pos.should == 4
+      describe '#find' do
+    it 'returns the full path of the found template' do
+      result = finder.find('500')
+    
+        args = msg['args'] + [msg['error_message']]
+    
+    # This class finds files in a repository by name and content
+# the result is joined and sorted by file name
+module Gitlab
+  class FileFinder
+    attr_reader :project, :ref
+    
+      describe '#can_update?' do
+    context 'when user can update_group_member' do
+      before do
+        allow(presenter).to receive(:can?).with(user, :update_group_member, presenter).and_return(true)
       end
-    end
     
-        Zlib::GzipWriter.wrap @io do |gzio|
-      gzio.write input
-    end
-    @io.string.size.should == 8176
-  end
-end
-
-    
-      it 'properly handles excessive data, in one go' do
-    # add bytes, one by one
-    data = @foo_deflated * 2
-    @z << data
-    @z.finish.should == 'foo' + @foo_deflated
-  end
-end
-
-    
-        unzipped = @inflator.inflate data.pack('C*')
-    @inflator.finish
-    
-      # ==> Configuration for :encryptable
-  # Allow you to use another encryption algorithm besides bcrypt (default). You can use
-  # :sha1, :sha512 or encryptors from others authentication tools as :clearance_sha1,
-  # :authlogic_sha512 (then you should set stretches above to 20 for default behavior)
-  # and :restful_authentication_sha1 (then you should set stretches to 10, and copy
-  # REST_AUTH_SITE_KEY to pepper).
-  #
-  # Require the `devise-encryptable` gem when using anything other than bcrypt
-  # config.encryptor = :sha512
-    
-              render 'admins/pods'
+              kubernetes_namespace.save!
         end
-        format.mobile { render 'admins/pods' }
-        format.json { render json: pods_json }
-      end
+    
+        def check_url_for_https_availability(url_to_check, user_agents: [:default])
+      problem = curl_check_http_content(url_to_check.to_s, user_agents: user_agents)
+      add_error problem if problem
+    end
+  end
+end
+
+    
+            def_node_search :conditional_dependencies, <<~EOS
+          {$(if (send (send nil? :build) ${:include? :with? :without?} $(str _))
+              (send nil? :depends_on $({str sym} _)) nil?)
+    }
+    
+      private
+    
+        def languages
+      @languages ||= [
+        *ARGV.value('language')&.split(','),
+        *ENV['HOMEBREW_LANGUAGES']&.split(','),
+        *Open3.capture2('defaults', 'read', '-g', 'AppleLanguages')[0].scan(/[^ \n'(),]+/),
+      ].uniq
     end
     
-          def fetch_public_key_from_json(string, jwt)
-        json = JSON.parse(string)
-        keys = json['keys']
-        public_key = get_key_from_kid(keys, jwt.header['kid'])
-        public_key
-      end
+    describe GitDownloadStrategy do
+  subject { described_class.new(url, name, version) }
     
-      def set_up_contacts_json
-    type = params[:set].presence
-    if params[:a_id].present?
-      type ||= 'by_aspect'
-      @aspect = current_user.aspects.find(params[:a_id])
+          # For OS::Mac::Version compatability
+      def requires_nehalem_cpu?
+        Hardware.oldest_cpu(self) == :nehalem
+      end
+      # https://en.wikipedia.org/wiki/Nehalem_(microarchitecture)
+      # Ensure any extra methods are also added to version/null.rb
+      alias requires_sse4? requires_nehalem_cpu?
+      alias requires_sse41? requires_nehalem_cpu?
+      alias requires_sse42? requires_nehalem_cpu?
+      alias requires_popcnt? requires_nehalem_cpu?
     end
-    type ||= 'receiving'
-    contacts_by_type(type).paginate(page: params[:page], per_page: 25)
-                          .map {|c| ContactPresenter.new(c, current_user).full_hash_with_person }
+  end
+end
+
+    
+      let(:local_caffeine) {
+    Cask::CaskLoader.load(cask_path('local-caffeine'))
+  }
+    
+        Feature5072 = '[ruby-core:38367]'
+    
+      it 'returns false when at EOF when there's data left in the buffer to read' do
+    gz = Zlib::GzipReader.new @io
+    gz.read(9)
+    gz.eof?.should be_false
+    gz.read
+    gz.eof?.should be_true
   end
     
-      respond_to :html, :mobile
-  respond_to :json, :only => :show
+      after :each do
+    ScratchPad.clear
+  end
     
-    require 'erb'
-require 'digest'
-require 'tempfile'
-require 'paperclip/version'
-require 'paperclip/geometry_parser_factory'
-require 'paperclip/geometry_detector_factory'
-require 'paperclip/geometry'
-require 'paperclip/processor'
-require 'paperclip/processor_helpers'
-require 'paperclip/tempfile'
-require 'paperclip/thumbnail'
-require 'paperclip/interpolations/plural_cache'
-require 'paperclip/interpolations'
-require 'paperclip/tempfile_factory'
-require 'paperclip/style'
-require 'paperclip/attachment'
-require 'paperclip/storage'
-require 'paperclip/callbacks'
-require 'paperclip/file_command_content_type_detector'
-require 'paperclip/media_type_spoof_detector'
-require 'paperclip/content_type_detector'
-require 'paperclip/glue'
-require 'paperclip/errors'
-require 'paperclip/missing_attachment_styles'
-require 'paperclip/validators'
-require 'paperclip/logger'
-require 'paperclip/helpers'
-require 'paperclip/has_attached_file'
-require 'paperclip/attachment_registry'
-require 'paperclip/filename_cleaner'
-require 'paperclip/rails_environment'
+        def recheck
+      pod = Pod.find(params[:pod_id])
+      pod.test_connection!
     
-        def names_for(klass)
-      @attachments[klass].keys
+    class ConversationsController < ApplicationController
+  before_action :authenticate_user!
+  respond_to :html, :mobile, :json, :js
+    
+    When(/^an error is raised$/) do
+  error = TestApp.shared_path.join('fail')
+  run_vagrant_command(test_file_exists(error))
+end
+    
+    Given(/^servers with the roles app and web$/) do
+  begin
+    vagrant_cli_command('up')
+  rescue
+    nil
+  end
+end
+    
+        def role_properties_for(names, &block)
+      servers.role_properties_for(names, &block)
     end
     
-        def raise_if_blank_file
-      if path.blank?
-        raise Errors::NotIdentifiedByImageMagickError.new('Cannot find the geometry of a file with a blank name')
+          def add_roles(roles)
+        Array(roles).each { |role| add_role(role) }
+        self
       end
+      alias roles= add_roles
+    
+      s.add_development_dependency 'activesupport', '< 5.0.0' # Please see issue #432
+  s.add_development_dependency 'awesome_print', '~> 1.2'
+  s.add_development_dependency 'bundler', '>= 1.3'
+  s.add_development_dependency 'coveralls', '~> 0.8'
+  s.add_development_dependency 'factory_bot', '~> 4.8'
+  s.add_development_dependency 'pry', '~> 0.10'
+  s.add_development_dependency 'rake', '~> 10.4'
+  s.add_development_dependency 'rspec', '~> 3.3'
+  s.add_development_dependency 'rubocop', '~> 0.48.1'
+  s.add_development_dependency 'simplecov', '~> 0.16'
+    
+    describe Tmuxinator::WemuxSupport do
+  let(:klass) { Class.new }
+  let(:instance) { klass.new }
+    
+        def _hashed?
+      @yaml.is_a?(Hash)
     end
     
-        def add_required_validations
-      options = Paperclip::Attachment.default_options.deep_merge(@options)
-      if options[:validate_media_type] != false
-        name = @name
-        @klass.validates_media_type_spoof_detection name,
-          :if => ->(instance){ instance.send(name).dirty? }
-      end
-    end
+      it { expect(subject.tmux_window_and_pane_target).to eql 'foo:0.1' }
+end
