18bb353f6c4db17b29e107f232daca79d51cdf5b
SUNsung <weatherius@gmail.com>
2019-08-04 04:21:42 +0300

    Rewrite [04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py] File in: 2019-08-04 04:21:42

--- pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
+++ pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
@@ -1,135 +1,61 @@
 
         
-        
-def render_function(function, method=True):
-    subblocks = []
-    signature = get_function_signature(function, method=method)
-    if method:
-        signature = signature.replace(
-            clean_module_name(function.__module__) + '.', '')
-    subblocks.append('### ' + function.__name__ + '\n')
-    subblocks.append(code_snippet(signature))
-    docstring = function.__doc__
-    if docstring:
-        if ('backend' in signature and
-                '{{np_implementation}}' in docstring):
-            docstring = add_np_implementation(function, docstring)
-        subblocks.append(process_docstring(docstring))
-    return '\n\n'.join(subblocks)
+            if args.action == 'start':
+        return start(args)
+    elif args.action == 'resume':
+        return resume(args)
+    elif args.action == 'cancel':
+        return cancel(args)
+    elif args.action == 'finalize':
+        return finalize(args)
     
-    # we add a GlobalAveragePooling1D, which will average the embeddings
-# of all words in the document
-model.add(GlobalAveragePooling1D())
+            if len(parts) == 2:
+            if parts[0] == 'container':
+                type, source = parts
+                return cls(source, 'rw', type)
     
-    # compute final accuracy on training and test sets
-y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
-tr_acc = compute_accuracy(tr_y, y_pred)
-y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
-te_acc = compute_accuracy(te_y, y_pred)
+        @property
+    def human_readable_state(self):
+        if self.is_paused:
+            return 'Paused'
+        if self.is_restarting:
+            return 'Restarting'
+        if self.is_running:
+            return 'Ghost' if self.get('State.Ghost') else self.human_readable_health_status
+        else:
+            return 'Exit %s' % self.get('State.ExitCode')
     
+            diff = len(lines) - lines[image_id]
     
-dummy_docstring = '''Multiplies 2 tensors (and/or variables) and returns a *tensor*.
+        for color_func in cycle([no_color] if monochrome else colors.rainbow()):
+        yield LogPresenter(prefix_width, color_func)
     
-                # Arguments
-                y_true: Tensor, batch_wise labels
-                y_pred: Tensor, batch_wise predictions
+    from .version import ComposeVersion
     
-    from .compat import is_py2, builtin_str, str
-    
-        def inner(*suffix):
-        return urljoin(httpbin_url, '/'.join(suffix))
-    
-        @pytest.fixture(autouse=True)
-    def setup(self):
-        '''CaseInsensitiveDict instance with 'Accept' header.'''
-        self.case_insensitive_dict = CaseInsensitiveDict()
-        self.case_insensitive_dict['Accept'] = 'application/json'
-    
-    '''
-requests.hooks
-~~~~~~~~~~~~~~
-    
-            with Server.basic_response_server(wait_to_close_event=block_server) as (host, port):
-            sock = socket.socket()
-            sock.connect((host, port))
-            sock.sendall(b'send something')
-            time.sleep(2.5)
-            sock.sendall(b'still alive')
-            block_server.set()  # release server block
-    
-            # reset stats on high iter-request times caused by client restarts
-        if delta > 3: # seconds
-            self._reset_stats()
-            return ''
-    
-    # Apply monkey patches to fix issues in external libraries
-from . import _monkeypatches
-del _monkeypatches
+                    if not volume_exists:
+                    log.info(
+                        'Creating volume '{0}' with {1} driver'.format(
+                            volume.full_name, volume.driver or 'default'
+                        )
+                    )
+                    volume.create()
+                else:
+                    check_remote_volume_config(volume.inspect(legacy=volume.legacy), volume)
+        except NotFound:
+            raise ConfigurationError(
+                'Volume %s specifies nonexistent driver %s' % (volume.name, volume.driver)
+            )
     
     
-class Command(ScrapyCommand):
+def rewrite_net(service, service_names):
+    if 'net' in service:
+        network_mode = service.pop('net')
     
-        def syntax(self):
-        return '[options] <name> <domain>'
+    For the last line of text, it should be left justified and no extra space is
+inserted between words.
     
-            e.g.:
-        @returns request
-        @returns request 2
-        @returns request 2 10
-        @returns request 0 10
-    '''
-    
-        def _is_zip(self, response):
-        archive = BytesIO(response.body)
-        try:
-            zip_file = zipfile.ZipFile(archive)
-        except zipfile.BadZipfile:
-            return
-    
-            {'id': 'mp4sd',    'container': 'mp4', 'video_profile': '标清'},
-        # obsolete?
-        {'id': 'flv',      'container': 'flv', 'video_profile': '标清'},
-        {'id': 'mp4',      'container': 'mp4', 'video_profile': '标清'},
-    ]
-    
-    # TBD
-def main_dev(**kwargs):
-    '''Main entry point.
-    you-get-dev
-    '''
-    
-    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
-    host = r1(r'host=([^&]*)', html)
-    assert host
-    type = r1(r'videofiletype=([^&]*)', html)
-    assert type
-    vid = r1(r'&stream_name=([^&]*)', html)
-    assert vid
-    dir_str = r1(r'&dir=([^&]*)', html).strip()
-    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
-    _, ext, size = url_info(url)
-    print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge = merge)
-    
-            self.extract(**kwargs)
-    
-    from ..common import *
-import urllib.parse
-    
-    # (api_key, method, ext, page)
-tmpl_api_call = (
-    'https://api.flickr.com/services/rest?'
-    '&format=json&nojsoncallback=1'
-    # UNCOMMENT FOR TESTING
-    #'&per_page=5'
-    '&per_page=500'
-    # this parameter CANNOT take control of 'flickr.galleries.getPhotos'
-    # though the doc said it should.
-    # it's always considered to be 500
-    '&api_key=%s'
-    '&method=flickr.%s'
-    '&extras=url_sq,url_q,url_t,url_s,url_n,url_m,url_z,url_c,url_l,url_h,url_k,url_o,media'
-    '%s&page=%d'
-)
+        def tearDown(self):
+        import os
+        os.remove(self.file_in_name)
+        os.remove(self.file_out_bin_name)
+        os.remove(self.file_out_name)
