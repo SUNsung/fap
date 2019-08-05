bdf930dfe0e0748dbb53ea92e794d176db798798
SUNsung <weatherius@gmail.com>
2019-08-05 08:49:28 +0300

    Rewrite [38Ie5Fh1srpU383agdJe.TyS238EIggt2iipg38U2E.py] File in: 2019-08-05 08:49:27

--- pyton/38Ie5Fh1srpU383agdJe.TyS238EIggt2iipg38U2E.py
+++ pyton/38Ie5Fh1srpU383agdJe.TyS238EIggt2iipg38U2E.py
@@ -1,247 +1,103 @@
 
         
-        - name: set the memory to 16GB and cpu to 8 core on a lust if servers
-  clc_modify_server:
-    server_ids:
-        - UC1TESTSVR01
-        - UC1TESTSVR02
-    cpu: 8
-    memory: 16
-    state: present
-'''
+            def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
     
-    import random
-import threading
-import time
-import os
-from front_base.random_get_slice import RandomGetSlice
+        def getChild(self, request, name):
+        return self
     
-        if 'Content-Encoding' in headers:
-        if headers['Content-Encoding'] == 'deflate':
-            payload = zlib.decompress(payload, -zlib.MAX_WBITS)
-            headers['Content-Length'] = str(len(payload))
-            del headers['Content-Encoding']
+        # Max concurrency is limited by global CONCURRENT_REQUESTS setting
+    max_concurrent_requests = 8
+    # Requests per second goal
+    qps = None # same as: 1 / download_delay
+    download_delay = None
+    # time in seconds to delay server responses
+    latency = None
+    # number of slots to create
+    slots = 1
     
-                if isinstance(self.input, TokenStream):
-                self.token = self.input.LT(1)
-                self.line = self.token.line
-                self.charPositionInLine = self.token.charPositionInLine
+            if opts.pidfile:
+            with open(opts.pidfile, 'w') as f:
+                f.write(str(os.getpid()) + os.linesep)
     
-            if isinstance(e, UnwantedTokenException):
-            tokenName = '<unknown>'
-            if e.expecting == EOF:
-                tokenName = 'EOF'
+    class Command(ScrapyCommand):
     
-        def setUp(self):
-        from acme.errors import MissingNonce
-        self.response = mock.MagicMock(headers={})
-        self.response.request.method = 'FOO'
-        self.error = MissingNonce(self.response)
+    from . import Contract
     
-        entry_points={
-        'console_scripts': [
-            'certbot = certbot.main:main',
-        ],
-        'certbot.plugins': [
-            'manual = certbot.plugins.manual:Authenticator',
-            'null = certbot.plugins.null:Installer',
-            'standalone = certbot.plugins.standalone:Authenticator',
-            'webroot = certbot.plugins.webroot:Authenticator',
-        ],
-    },
-)
-
+            # extract cookies from Set-Cookie and drop invalid/expired cookies
+        cookiejarkey = request.meta.get('cookiejar')
+        jar = self.jars[cookiejarkey]
+        jar.extract_cookies(response, request)
+        self._debug_set_cookie(response, spider)
     
-            # _query_soa | pylint: disable=protected-access
-        result = self.rfc2136_client._query_soa(DOMAIN)
+            # put all lines in the file into a Python list
+        strings = f.readlines()
+        
+        # above line leaves trailing newline characters; strip them out
+        strings = [x.strip(u'\n') for x in strings]
+        
+        # remove empty-lines and comments
+        strings = [x for x in strings if x and not x.startswith(u'#')]
+        
+        # insert empty string since all are being removed
+        strings.insert(0, u'')
     
-        # list of integers
-    result = s.iloc[[0, 2, 3, 4, 5]]
-    expected = s.reindex(s.index[[0, 2, 3, 4, 5]])
-    assert_series_equal(result, expected)
+            # test coding cookie
+        for encoding in ('iso-8859-15', 'utf-8'):
+            with open(filename, 'w', encoding=encoding) as fp:
+                print('# coding: %s' % encoding, file=fp)
+                print('print('euro:\u20ac')', file=fp)
+            with tokenize_open(filename) as fp:
+                self.assertEqual(fp.encoding, encoding)
+                self.assertEqual(fp.mode, 'r')
     
-        def test_pi_offset_errors(self):
-        idx = PeriodIndex(
-            ['2011-01-01', '2011-02-01', '2011-03-01', '2011-04-01'],
-            freq='D',
-            name='idx',
-        )
-        ser = pd.Series(idx)
+        def test_named_expression_invalid_11(self):
+        code = '''spam(a=1, b := 2)'''
     
-            if allow_fill and fill_value is None:
-            fill_value = self.dtype.na_value
-    
-    
-class TestArithmeticOps(BaseDecimal, base.BaseArithmeticOpsTests):
-    def check_opname(self, s, op_name, other, exc=None):
-        super().check_opname(s, op_name, other, exc=None)
-    
-            # needed to add this check for the Series constructor
-        if isinstance(dtype, type(self.dtype)) and dtype == self.dtype:
-            if copy:
-                return self.copy()
-            return self
-        return np.array([dict(x) for x in self], dtype=dtype, copy=copy)
-    
-        # all NA case
-    if has_skipna:
-        all_na = frame * np.NaN
-        r0 = getattr(all_na, opname)(axis=0)
-        r1 = getattr(all_na, opname)(axis=1)
-        if opname == 'any':
-            assert not r0.any()
-            assert not r1.any()
-        else:
-            assert r0.all()
-            assert r1.all()
-    
-            # mixed non-null / full null column
-        df = DataFrame(
-            {
-                'a': [
-                    pd.Timestamp('2012-01-01'),
-                    pd.Timestamp('2012-01-02'),
-                    pd.Timestamp('2012-01-03'),
-                ],
-                'b': [pd.NaT, pd.NaT, pd.NaT],
-            }
-        )
-    
-        # If not a dictionary or a list, the response is necessarily a
-    # scalar: boolean, number, string, etc. In this case, we print
-    # it to the user.
-    if not isinstance( self._response, ( dict, list ) ):
-      return self._HandleBasicResponse()
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
-    
-        lut = {}
-    for value in values:
-        k, v = keyval(value)
-        try:
-            s = lut[k]
-        except KeyError:
-            if use_lists:
-                s = lut[k] = list()
-            else:
-                s = lut[k] = set()
-        if use_lists:
-            s.append(v)
-        else:
-            s.add(v)
-    return dict(lut)
-    
-        path = _get_path(path)
-    if path is None:
-        raise TypeError('expected a valid path to encode')
-    if isinstance(path, six.text_type):
-        if six.PY2:
-            return b''.join(
-                (
-                    _byte(ord(c) - 0xDC00)
-                    if 0xDC00 <= ord(c) <= 0xDCFF
-                    else c.encode(_fs_encoding, _fs_encode_errors)
-                )
-                for c in path
-            )
-        return path.encode(_fs_encoding, _fs_encode_errors)
-    return path
-    
-    
-class PipfileException(PipenvFileError):
-    def __init__(self, hint=None, **kwargs):
-        from .core import project
-    
-    from pytest import mark
-    
-    
-def test_expansion_in_nested_schema():
-    schema = {'detroit': {'schema': {'anyof_regex': ['^Aladdin', 'Sane$']}}}
-    v = Validator(schema)
-    assert v.schema['detroit']['schema'] == {
-        'anyof': [{'regex': '^Aladdin'}, {'regex': 'Sane$'}]
-    }
-    
-        Parameters
-    ----------
-    complete_options : bool
-        always complete the options, even when the user hasn't typed a first dash (Default value = False)
-    match_incomplete : func
-        a function with two parameters choice and incomplete. Must return True
-        if incomplete is a correct match for choice, False otherwise.
-    '''
-    global _initialized
-    if not _initialized:
-        _patch()
-        completion_configuration.complete_options = complete_options
-        if match_incomplete is not None:
-            completion_configuration.match_incomplete = match_incomplete
-        _initialized = True
-    
-            :param req: The request to send.
-        :return: The HTTP response from PyPI (a standard library HTTPResponse).
+        def xpath(self, id):
+        '''Process an XPATH command (optional server extension) Arguments:
+        - id: Message id of article
+        Returns:
+        resp: server response if successful
+        path: directory path to article
         '''
-        handlers = []
-        if self.password_handler:
-            handlers.append(self.password_handler)
-        if self.ssl_verifier:
-            handlers.append(self.ssl_verifier)
-        opener = build_opener(*handlers)
-        return opener.open(req)
+        warnings.warn('The XPATH extension is not actively used',
+                      DeprecationWarning, 2)
     
-        def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+        queue = c_queue
     
-                                if action == 0:
-                                new_row = min(row + 1, max_row)
-                            elif action == 1:
-                                new_row = max(row - 1, 0)
-                            if action == 2 and self.desc[1 + row, 2 * col + 2] == b':':
-                                new_col = min(col + 1, max_col)
-                            elif action == 3 and self.desc[1 + row, 2 * col] == b':':
-                                new_col = max(col - 1, 0)
-                            elif action == 4:  # pickup
-                                if (pass_idx < 4 and taxi_loc == locs[pass_idx]):
-                                    new_pass_idx = 4
-                                else: # passenger not at location
-                                    reward = -10
-                            elif action == 5:  # dropoff
-                                if (taxi_loc == locs[dest_idx]) and pass_idx == 4:
-                                    new_pass_idx = dest_idx
-                                    done = True
-                                    reward = 20
-                                elif (taxi_loc in locs) and pass_idx == 4:
-                                    new_pass_idx = locs.index(taxi_loc)
-                                else: # dropoff at wrong location
-                                    reward = -10
-                            new_state = self.encode(
-                                new_row, new_col, new_pass_idx, dest_idx)
-                            P[state][action].append(
-                                (1.0, new_state, reward, done))
-        initial_state_distrib /= initial_state_distrib.sum()
-        discrete.DiscreteEnv.__init__(
-            self, num_states, num_actions, P, initial_state_distrib)
+        The returned instance will have attributes args, returncode, stdout and
+    stderr. By default, stdout and stderr are not captured, and those attributes
+    will be None. Pass stdout=PIPE and/or stderr=PIPE in order to capture them.
     
-    class MemorizeDigits(gym.Env):
-    metadata = {
-        'render.modes': ['human', 'rgb_array'],
-        'video.frames_per_second' : 60,
-        'video.res_w' : FIELD_W,
-        'video.res_h' : FIELD_H,
-    }
+            messages = []
+        self.loop.set_exception_handler(lambda loop, ctx: messages.append(ctx))
+    
+                ret[i, :] = i_label, i_score
+    
+        def test_object(queue):
+        pool = ObjectPool(queue, True)
+        print('Inside func: {}'.format(pool.item))
+    
+    
+def main():
+    ui = Ui()
+    ui.get_product_list()
+    ui.get_product_information('cheese')
+    ui.get_product_information('eggs')
+    ui.get_product_information('milk')
+    ui.get_product_information('arepas')
+    
+        graphic.add(graphic1)
+    graphic.add(graphic2)
+    
+    '''
+*What is this pattern about?
+The Decorator pattern is used to dynamically add a new feature to an
+object without changing its implementation. It differs from
+inheritance because the new feature is added only to that particular
+object, not to the entire subclass.
