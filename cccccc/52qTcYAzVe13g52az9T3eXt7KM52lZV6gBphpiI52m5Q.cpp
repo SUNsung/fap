0b584ef97fc7fb20cf1107c10614b145200425ec
SUNsung <weatherius@gmail.com>
2019-08-05 12:34:20 +0300

    Rewrite [52qTcYAzVe13g52az9T3eXt7KM52lZV6gBphpiI52m5Q.cpp] File in: 2019-08-05 12:34:19

--- cccccc/52qTcYAzVe13g52az9T3eXt7KM52lZV6gBphpiI52m5Q.cpp
+++ cccccc/52qTcYAzVe13g52az9T3eXt7KM52lZV6gBphpiI52m5Q.cpp
@@ -1,167 +1,268 @@
 
         
-        SEXP XGDMatrixSetInfo_R(SEXP handle, SEXP field, SEXP array) {
-  R_API_BEGIN();
-  int len = length(array);
-  const char *name = CHAR(asChar(field));
-  if (!strcmp('group', name)) {
-    std::vector<unsigned> vec(len);
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < len; ++i) {
-      vec[i] = static_cast<unsigned>(INTEGER(array)[i]);
+            return;
+  } else if (method == 'ClearCache') {
+    ClearCache(GetRenderProcessHost());
+    return;
+  } else if (method == 'CreateShortcut') {
+#if defined(OS_WIN)
+    base::string16 path;
+    arguments.GetString(0, &path);
+    
+    class Base {
+ public:
+  Base(int id,
+       const base::WeakPtr<ObjectManager>& manager,
+       const base::DictionaryValue& option,
+       const std::string& extension_id);
+  virtual ~Base();
     }
-    CHECK_CALL(XGDMatrixSetGroup(R_ExternalPtrAddr(handle), BeginPtr(vec), len));
+    
+    // Call method of an object in browser and return the result.
+// function CallObjectMethod(id, type, method, args);
+v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
+                                           int object_id,
+                                           const std::string& type,
+                                           const std::string& method,
+                                           v8::Handle<v8::Value> args);
+    
+    
+#include 'content/nw/src/api/event/event.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'ui/gfx/screen.h'
+    
+    
+    {
+    {    if (zoom_controller) {
+      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
+      if (zoom_factor > content::kMaximumZoomFactor) {
+        zoom_factor = content::kMaximumZoomFactor;
+      }
+      if (zoom_factor < content::kMinimumZoomFactor) {
+        zoom_factor = content::kMinimumZoomFactor;
+      }
+      x *= zoom_factor;
+      y *= zoom_factor;
+    }
+    
+    Popup(x, y, rvh);
+  } else if (method == 'EnableShowEvent') {
+    arguments.GetBoolean(0, &enable_show_event_);
   } else {
-    std::vector<float> vec(len);
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < len; ++i) {
-      vec[i] = REAL(array)[i];
-    }
-    CHECK_CALL(XGDMatrixSetFloatInfo(R_ExternalPtrAddr(handle),
-                                   CHAR(asChar(field)),
-                                   BeginPtr(vec), len));
+    NOTREACHED() << 'Invalid call to Menu method:' << method
+                 << ' arguments:' << arguments;
   }
-  R_API_END();
-  return R_NilValue;
 }
     
-    namespace xgboost {
-namespace data {
-    }
+    MenuDelegate::~MenuDelegate() {
+}
+    
+    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringWithFixupFunction(){}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
+};
+    
+     protected:
+  ~NwObjCreateFunction() override;
+    
+        std::string type;
+    switch(src.id.type) {
+    case content::DesktopMediaID::TYPE_WINDOW:
+      type = 'window';
+      break;
+    case content::DesktopMediaID::TYPE_SCREEN:
+      type = 'screen';
+      break;
+    case content::DesktopMediaID::TYPE_NONE:
+      type = 'none';
+      break;
+    default:
+      type = 'unknown';
+      break;
     }
     
-      void GetGradient(const HostDeviceVector<bst_float>& preds,
-                   const MetaInfo& info,
-                   int iter,
-                   HostDeviceVector<GradientPair>* out_gpair) override {
-    CHECK_EQ(preds.Size(), info.labels_.Size()) << 'label size predict size not match';
-    const auto& preds_h = preds.HostVector();
-    out_gpair->Resize(preds_h.size());
-    std::vector<GradientPair>& gpair = out_gpair->HostVector();
-    // quick consistency when group is not available
-    std::vector<unsigned> tgptr(2, 0); tgptr[1] = static_cast<unsigned>(info.labels_.Size());
-    const std::vector<unsigned> &gptr = info.group_ptr_.size() == 0 ? tgptr : info.group_ptr_;
-    CHECK(gptr.size() != 0 && gptr.back() == info.labels_.Size())
-        << 'group structure not consistent with #rows';
-    }
+    #include 'caffe/layer.hpp'
+#include 'caffe/layer_factory.hpp'
+#include 'caffe/layers/clip_layer.hpp'
+#include 'caffe/layers/conv_layer.hpp'
+#include 'caffe/layers/deconv_layer.hpp'
+#include 'caffe/layers/lrn_layer.hpp'
+#include 'caffe/layers/pooling_layer.hpp'
+#include 'caffe/layers/relu_layer.hpp'
+#include 'caffe/layers/sigmoid_layer.hpp'
+#include 'caffe/layers/softmax_layer.hpp'
+#include 'caffe/layers/tanh_layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
     
-    // c_api
-#include '../src/c_api/c_api.cc'
-#include '../src/c_api/c_api_error.cc'
-
-    
-    /*!
- * \brief Input stream that support additional PeekRead
- *  operation, besides read.
+    /**
+ * @brief An interface for classes that perform optimization on Net%s.
+ *
+ * Requires implementation of ApplyUpdate to compute a parameter update
+ * given the current state of the Net parameters.
  */
-class PeekableInStream : public dmlc::Stream {
+template <typename Dtype>
+class Solver {
  public:
-  explicit PeekableInStream(dmlc::Stream* strm)
-      : strm_(strm), buffer_ptr_(0) {}
+  explicit Solver(const SolverParameter& param);
+  explicit Solver(const string& param_file);
+  void Init(const SolverParameter& param);
+  void InitTrainNet();
+  void InitTestNets();
     }
     
-      template<typename Derived>
-  inline void PredLoopSpecalize(
-      DMatrix* p_fmat,
-      std::vector<bst_float>* out_preds,
-      int num_group,
-      unsigned tree_begin,
-      unsigned tree_end) {
-    const MetaInfo& info = p_fmat->Info();
-    const int nthread = omp_get_max_threads();
-    CHECK_EQ(num_group, model_.param.num_output_group);
-    InitThreadTemp(nthread);
-    std::vector<bst_float>& preds = *out_preds;
-    CHECK_EQ(model_.param.size_leaf_vector, 0)
-        << 'size_leaf_vector is enforced to 0 so far';
-    CHECK_EQ(preds.size(), p_fmat->Info().num_row_ * num_group);
-    // start collecting the prediction
-    auto* self = static_cast<Derived*>(this);
-    for (const auto &batch : p_fmat->GetRowBatches()) {
-      constexpr int kUnroll = 8;
-      const auto nsize = static_cast<bst_omp_uint>(batch.Size());
-      const bst_omp_uint rest = nsize % kUnroll;
-      #pragma omp parallel for schedule(static)
-      for (bst_omp_uint i = 0; i < nsize - rest; i += kUnroll) {
-        const int tid = omp_get_thread_num();
-        RegTree::FVec& feats = thread_temp_[tid];
-        int64_t ridx[kUnroll];
-        SparsePage::Inst inst[kUnroll];
-        for (int k = 0; k < kUnroll; ++k) {
-          ridx[k] = static_cast<int64_t>(batch.base_rowid + i + k);
-        }
-        for (int k = 0; k < kUnroll; ++k) {
-          inst[k] = batch[i + k];
-        }
-        for (int k = 0; k < kUnroll; ++k) {
-          for (int gid = 0; gid < num_group; ++gid) {
-            const size_t offset = ridx[k] * num_group + gid;
-            preds[offset] +=
-                self->PredValue(inst[k], gid, info.GetRoot(ridx[k]),
-                                &feats, tree_begin, tree_end);
-          }
-        }
-      }
-      for (bst_omp_uint i = nsize - rest; i < nsize; ++i) {
-        RegTree::FVec& feats = thread_temp_[0];
-        const auto ridx = static_cast<int64_t>(batch.base_rowid + i);
-        const SparsePage::Inst inst = batch[i];
-        for (int gid = 0; gid < num_group; ++gid) {
-          const size_t offset = ridx * num_group + gid;
-          preds[offset] +=
-              self->PredValue(inst, gid, info.GetRoot(ridx),
-                              &feats, tree_begin, tree_end);
-        }
-      }
+    void InitLog() {
+  ::google::InitGoogleLogging('');
+  ::google::InstallFailureSignalHandler();
+}
+void InitLogLevel(int level) {
+  FLAGS_minloglevel = level;
+  InitLog();
+}
+void InitLogLevelPipe(int level, bool stderr) {
+  FLAGS_minloglevel = level;
+  FLAGS_logtostderr = stderr;
+  InitLog();
+}
+void Log(const string& s) {
+  LOG(INFO) << s;
+}
+    
+      LOG(INFO) << 'Writing Training data';
+  for (int fileid = 0; fileid < kCIFARTrainBatches; ++fileid) {
+    // Open files
+    LOG(INFO) << 'Training Batch ' << fileid + 1;
+    string batchFileName = input_folder + '/data_batch_'
+      + caffe::format_int(fileid+1) + '.bin';
+    std::ifstream data_file(batchFileName.c_str(),
+        std::ios::in | std::ios::binary);
+    CHECK(data_file) << 'Unable to open train file #' << fileid + 1;
+    for (int itemid = 0; itemid < kCIFARBatchSize; ++itemid) {
+      read_image(&data_file, &label, str_buffer);
+      datum.set_label(label);
+      datum.set_data(str_buffer, kCIFARImageNBytes);
+      string out;
+      CHECK(datum.SerializeToString(&out));
+      txn->Put(caffe::format_int(fileid * kCIFARBatchSize + itemid, 5), out);
     }
   }
+  txn->Commit();
+  train_db->Close();
     
-      /*!
-   * \brief Get configuration arguments currently stored by the learner
-   * \return Key-value pairs representing configuration arguments
-   */
-  virtual const std::map<std::string, std::string>& GetConfigurationArguments() const = 0;
+      inline Dtype diff_at(const int n, const int c, const int h,
+      const int w) const {
+    return cpu_diff()[offset(n, c, h, w)];
+  }
     
-    Status WriteBatchBase::SingleDelete(ColumnFamilyHandle* column_family,
-                                    const SliceParts& key) {
-  std::string key_buf;
-  Slice key_slice(key, &key_buf);
-  return SingleDelete(column_family, key_slice);
-}
+    #include 'caffe/blob.hpp'
+#include 'caffe/common.hpp'
+#include 'caffe/proto/caffe.pb.h'
     
-    class WriteCallback {
- public:
-  virtual ~WriteCallback() {}
+    	SetEnvironmentVariable(L'CMDER_ROOT', exeDir);
+	if (wcscmp(userConfigDirPath, configDirPath) != 0)
+	{
+		SetEnvironmentVariable(L'CMDER_USER_CONFIG', userConfigDirPath);
+		SetEnvironmentVariable(L'CMDER_USER_BIN', userBinDirPath);
+	}
+    
+    struct TestCanParam {
+  CANCardParameter conf;
+  bool is_first_agent = false;
+  int32_t recv_cnt = 0;
+  int32_t recv_err_cnt = 0;
+  int32_t send_cnt = 0;
+  int32_t send_err_cnt = 0;
+  int32_t send_lost_cnt = 0;
+  int32_t send_time = 0;
+  int32_t recv_time = 0;
+  CanClient *can_client = nullptr;
     }
     
-    #include <rocksdb/compaction_filter.h>
-#include <rocksdb/db.h>
-#include <rocksdb/merge_operator.h>
-#include <rocksdb/options.h>
+    #define ALOG_MODULE_STREAM_WARN(module)                            \
+  google::LogMessage(__FILE__, __LINE__, google::WARNING).stream() \
+      << LEFT_BRACKET << module << RIGHT_BRACKET
     
-    
-    {  return 0;
-}
-
-    
-      // Attempt to read a key using the snapshot.  This will fail since
-  // the previous write outside this txn conflicts with this read.
-  read_options.snapshot = snapshot;
-  s = txn->GetForUpdate(read_options, 'abc', &value);
-  assert(s.IsBusy());
-    
-    // Returns the current memory usage of the specified DB instances.
-class MemoryUtil {
- public:
-  enum UsageType : int {
-    // Memory usage of all the mem-tables.
-    kMemTableTotal = 0,
-    // Memory usage of those un-flushed mem-tables.
-    kMemTableUnFlushed = 1,
-    // Memory usage of all the table readers.
-    kTableReadersTotal = 2,
-    // Memory usage by Cache.
-    kCacheTotal = 3,
-    kNumUsageTypes = 4
-  };
+    bool GetFileList(const std::string &path, const std::string &suffix,
+                 std::vector<std::string> *files) {
+  if (!PathExists(path)) {
+    AINFO << path << ' not exist.';
+    return false;
+  }
     }
+    
+    
+    { protected:
+  std::unique_ptr<PullOverScenario> scenario_;
+};
+    
+    
+    {  if (writer->attributes().has_proto_desc()) {
+    *proto_desc = writer->attributes().proto_desc();
+  }
+}
+    
+    /*
+ * Class:     org_rocksdb_CompactionJobStats
+ * Method:    largestOutputKeyPrefix
+ * Signature: (J)[B
+ */
+jbyteArray Java_org_rocksdb_CompactionJobStats_largestOutputKeyPrefix(
+    JNIEnv* env, jclass, jlong jhandle) {
+  auto* compact_job_stats =
+      reinterpret_cast<rocksdb::CompactionJobStats*>(jhandle);
+  return rocksdb::JniUtil::copyBytes(env,
+      compact_job_stats->largest_output_key_prefix);
+}
+    
+    /*
+ * Class:     org_rocksdb_CompressionOptions
+ * Method:    level
+ * Signature: (J)I
+ */
+jint Java_org_rocksdb_CompressionOptions_level(
+    JNIEnv*, jobject, jlong jhandle) {
+  auto* opt = reinterpret_cast<rocksdb::CompressionOptions*>(jhandle);
+  return static_cast<jint>(opt->level);
+}
+    
+    /*
+ * Class:     org_rocksdb_IngestExternalFileOptions
+ * Method:    setSnapshotConsistency
+ * Signature: (JZ)V
+ */
+void Java_org_rocksdb_IngestExternalFileOptions_setSnapshotConsistency(
+    JNIEnv*, jobject, jlong jhandle, jboolean jsnapshot_consistency) {
+  auto* options =
+      reinterpret_cast<rocksdb::IngestExternalFileOptions*>(jhandle);
+  options->snapshot_consistency = static_cast<bool>(jsnapshot_consistency);
+}
+    
+    void build_column_family_descriptor_list(
+    JNIEnv* env, jobject jcfds,
+    std::vector<rocksdb::ColumnFamilyDescriptor>& cf_descs) {
+  jmethodID add_mid = rocksdb::ListJni::getListAddMethodId(env);
+  if (add_mid == nullptr) {
+    // exception occurred accessing method
+    return;
+  }
+    }
+    
+      jlongArray jtxns = env->NewLongArray(len);
+  if (jtxns == nullptr) {
+    // exception thrown: OutOfMemoryError
+    return nullptr;
+  }
+  env->SetLongArrayRegion(jtxns, 0, len, tmp.data());
+  if (env->ExceptionCheck()) {
+    // exception thrown: ArrayIndexOutOfBoundsException
+    env->DeleteLocalRef(jtxns);
+    return nullptr;
+  }
