82e68733db58cec34fef1796fc2958db66e7be0f
SUNsung <weatherius@gmail.com>
2019-08-05 21:16:10 +0300

    Rewrite [35W81UAsXkBv.35L6AQAF.AfQQ35a6f7sGmHPXU35Ui1.py] File in: 2019-08-05 21:16:10

--- pyton/35W81UAsXkBv.35L6AQAF.AfQQ35a6f7sGmHPXU35Ui1.py
+++ pyton/35W81UAsXkBv.35L6AQAF.AfQQ35a6f7sGmHPXU35Ui1.py
@@ -1,111 +1,91 @@
 
         
-                info = self._extract_theplatform_metadata(tp_path, content_id)
-        info.update({
-            'id': content_id,
-            'title': title,
-            'series': xpath_text(video_data, 'seriesTitle'),
-            'season_number': int_or_none(xpath_text(video_data, 'seasonNumber')),
-            'episode_number': int_or_none(xpath_text(video_data, 'episodeNumber')),
-            'duration': int_or_none(xpath_text(video_data, 'videoLength'), 1000),
-            'thumbnail': xpath_text(video_data, 'previewImageURL'),
-            'formats': formats,
-            'subtitles': subtitles,
-        })
-        return info
+            # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule('/', endpoint='index')
     
-        bug_text = re.search(
-        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
-    dev_text = re.search(
-        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING YOUTUBE-DL',
-        readme).group(1)
+            :param obj: an import name or object
+        '''
+        if isinstance(obj, string_types):
+            obj = import_string(obj)
+        for key in dir(obj):
+            if key.isupper():
+                self[key] = getattr(obj, key)
+    
+        :param template_name_or_list: the name of the template to be
+                                  rendered, or an iterable with template names
+                                  the first one existing will be rendered
+    :param context: the variables that should be available in the
+                    context of the template.
+    '''
+    ctx = _app_ctx_stack.top
+    ctx.app.update_template_context(context)
+    return _render(
+        ctx.app.jinja_env.get_or_select_template(template_name_or_list),
+        context,
+        ctx.app,
+    )
+    
+        def get_images_to_push(self):
+        tags_to_push = {
+            '{}:{}'.format(NAME, self.version),
+            self.get_runtime_image_tag(Platform.ALPINE),
+            self.get_runtime_image_tag(Platform.DEBIAN),
+            self.get_ucp_test_image_tag(),
+            self.get_ucp_test_image_tag('latest'),
+        }
+        if is_tag_latest(self.version):
+            tags_to_push.add('{}:latest'.format(NAME))
+        return tags_to_push
+    
+        ports = make_port_specs(service_dict)
+    if ports:
+        container_config['Ports'] = ports
     
     
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
+def test_push_image(mock_service):
+    mock_service.options['build'] = '.'
+    mock_service.options['image'] = image_id = 'abcd'
+    mock_service.push.return_value = expected = 'sha256:thedigest'
+    mock_service.image.return_value = {'RepoDigests': []}
     
-        with io.open(outfile, 'w', encoding='utf-8') as outf:
-        outf.write(out)
+    from .utils import ScriptError
     
-        if os.path.exists('tldr.pdf'):
-        print('\nCreated tldr.pdf in the current directory!\n')
+        def load(self, loader):
+        loader.add_option(
+            'rfile', typing.Optional[str], None,
+            'Read flows from file.'
+        )
+        loader.add_option(
+            'readfile_filter', typing.Optional[str], None,
+            'Read only matching flows.'
+        )
     
-    
-    
-        # load weights
-    if weights == 'imagenet':
-        print('K.image_dim_ordering:', K.image_dim_ordering())
-        if K.image_dim_ordering() == 'th':
-            if include_top:
-                weights_path = get_file('resnet50_weights_th_dim_ordering_th_kernels.h5',
-                                        TH_WEIGHTS_PATH,
-                                        cache_subdir='models')
+        def configure(self, updated):
+        if 'stickycookie' in updated:
+            if ctx.options.stickycookie:
+                flt = flowfilter.parse(ctx.options.stickycookie)
+                if not flt:
+                    raise exceptions.OptionsError(
+                        'stickycookie: invalid filter expression: %s' % ctx.options.stickycookie
+                    )
+                self.flt = flt
             else:
-                weights_path = get_file('resnet50_weights_th_dim_ordering_th_kernels_notop.h5',
-                                        TH_WEIGHTS_PATH_NO_TOP,
-                                        cache_subdir='models')
-            model.load_weights(weights_path)
-            if K.backend() == 'tensorflow':
-                warnings.warn('You are using the TensorFlow backend, yet you '
-                              'are using the Theano '
-                              'image dimension ordering convention '
-                              '(`image_dim_ordering='th'`). '
-                              'For best performance, set '
-                              '`image_dim_ordering='tf'` in '
-                              'your Keras config '
-                              'at ~/.keras/keras.json.')
-                convert_all_kernels_in_model(model)
-        else:
-            if include_top:
-                weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels.h5',
-                                        TF_WEIGHTS_PATH,
-                                        cache_subdir='models')
-            else:
-                weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                                        TF_WEIGHTS_PATH_NO_TOP,
-                                        cache_subdir='models')
-            model.load_weights(weights_path)
-            if K.backend() == 'theano':
-                convert_all_kernels_in_model(model)
-    return model
+                self.flt = None
     
+        def configure(self, updated):
+        if 'stream_large_bodies' in updated and ctx.options.stream_large_bodies:
+            try:
+                self.max_size = human.parse_size(ctx.options.stream_large_bodies)
+            except ValueError as e:
+                raise exceptions.OptionsError(e)
     
-class LogisticRegression(object):
-    '''Multi-class Logistic Regression Class
-    
-        for t in range(sample_len - 1):
-        x[t + 1] = x[t] + sigma * (y[t] - x[t]) * step
-        y[t + 1] = y[t] + (x[t] * (rho - z[t]) - y[t]) * step
-        z[t + 1] = z[t] + (x[t] * y[t] - beta * z[t]) * step
-    
-    
-if __name__ == '__main__':
-  testutils.main()
-
-    
-      def testCollectorGetsWantedWidgets(self):
-    col = collector.Collector()
-    self.assertEqual(len(col.collect_widgets()), 10)
-    
-      def testSetParseFnsNamedDefaultsFromFire(self):
-    # Fire should use the decorator to know how to parse string arguments.
-    self.assertEqual(core.Fire(WithDefaults, command=['example2']), (10, int))
-    self.assertEqual(core.Fire(WithDefaults, command=['example2', '10']),
-                     (10, float))
-    self.assertEqual(core.Fire(WithDefaults, command=['example2', '13']),
-                     (13, float))
-    self.assertEqual(core.Fire(WithDefaults, command=['example2', '14.0']),
-                     (14, float))
-    
-      def testNoPrivateMethods(self):
-    self.assertTrue(hasattr(fire, 'Fire'))
-    self.assertFalse(hasattr(fire, '_Fire'))
-    
-      @settings(max_examples=1000, deadline=1000)
-  @given(st.text(min_size=1))
-  @example('This is a one-line docstring.')
-  def test_fuzz_parse(self, value):
-    docstrings.parse(value)
+        def call_strings(self, path: str, args: typing.Sequence[str]) -> typing.Any:
+        '''
+            Call a command using a list of string arguments. May raise CommandError.
+        '''
+        if path not in self.commands:
+            raise exceptions.CommandError('Unknown command: %s' % path)
+        return self.commands[path].call(args)
