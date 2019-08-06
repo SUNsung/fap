e244bbf197ac8c5edd378d1a6e853cc2a22e5a10
SUNsung <weatherius@gmail.com>
2019-08-07 00:13:51 +0300

    Rewrite [266ZzVGtn4Q8s26sCIvAA18xvo26L0DvfHVzVbQ26hNn.rb] File in: 2019-08-07 00:13:51

--- ruby/266ZzVGtn4Q8s26sCIvAA18xvo26L0DvfHVzVbQ26hNn.rb
+++ ruby/266ZzVGtn4Q8s26sCIvAA18xvo26L0DvfHVzVbQ26hNn.rb
@@ -1,78 +1,176 @@
 
         
-        require 'rubygems'
-require 'rake'
-require 'rdoc'
-require 'date'
-require 'yaml'
+            it 'returns a correct icon tag for other services' do
+      icon = omniauth_provider_icon(:'37signals')
+      expect(icon).to be_html_safe
+      elem = Nokogiri(icon).at('i.fa.fa-lock')
+      expect(elem).to be_a Nokogiri::XML::Element
+    end
+  end
     
-        def arg_is_present?(args, deprecated_argument, message)
-      deprecation_message(message) if args.include?(deprecated_argument)
+              @bar3 = Agents::DotBar.new(name: 'bar3').tap { |agent|
+            agent.user = users(:bob)
+            agent.sources << @bar2
+            agent.save!
+          },
+        ]
+        @foo.reload
+        @bar2.reload
+    
+      describe '#relative_distance_of_time_in_words' do
+    it 'in the past' do
+      expect(relative_distance_of_time_in_words(Time.now-5.minutes)).to eq('5m ago')
     end
     
-          private
+        it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
+    end
+  end
     
-        def insert_after(index, *names)
-      insert assert_index(index) + 1, *names
+      it 'should run scheduled agents' do
+    mock(Agent).run_schedule('every_1h')
+    mock.instance_of(IO).puts('Queuing schedule for every_1h')
+    @scheduler.send(:run_schedule, 'every_1h')
+  end
+    
+        it 'should raise an exception when encountering complex JSONPaths' do
+      @agent.options['username_path'] = '$.very.complex[*]'
+      expect { LiquidMigrator.convert_all_agent_options(@agent) }.
+        to raise_error('JSONPath '$.very.complex[*]' is too complex, please check your migration.')
     end
     
-            css('.note h3', '.warning h3').each do |node|
-          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
+        stub_request(:get, /trackings/).to_return(
+      :body => File.read(Rails.root.join('spec/data_fixtures/aftership.json')),
+      :status => 200,
+      :headers => {'Content-Type' => 'text/json'}
+    )
+    
+            a_split.each_with_index { |s, i| a_split[i] = s.to_i unless i == a_length - 1 }
+        b_split.each_with_index { |s, i| b_split[i] = s.to_i unless i == b_length - 1 }
+    
+        def as_json
+      @pages
+    end
+    
+        def parse_as_document
+      document = Nokogiri::HTML.parse @content, nil, 'UTF-8'
+      @title = document.at_css('title').try(:content)
+      document
+    end
+    
+        def effective_path
+      @effective_path ||= effective_url.path
+    end
+  end
+end
+
+    
+          if !cask.url.blank? && !cask.url.using
+        check_url_for_https_availability(cask.url, user_agents: [cask.url.user_agent])
+      end
+      check_url_for_https_availability(cask.appcast) unless cask.appcast.blank?
+      check_url_for_https_availability(cask.homepage, user_agents: [:browser]) unless cask.homepage.blank?
+    end
+    
+        it 'when lapack is used instead of OpenBLAS' do
+      expect_offense(<<~RUBY, '/homebrew-core/')
+        class Foo < Formula
+          url 'https://brew.sh/foo-1.0.tgz'
+          homepage 'https://brew.sh'
+          depends_on 'lapack'
+          ^^^^^^^^^^^^^^^^^^^ Formulae should use OpenBLAS as the default serial linear algebra library.
         end
+      RUBY
+    end
     
-                    raise Errors::VMNoMatchError if vms.empty?
-              else
-                # String name, just look for a specific VM
-                vms << @env.vms[name.to_sym]
-                raise Errors::VMNotFoundError, name: name if !vms[0]
-              end
-            end
-          else
-            vms = @env.vms_ordered
-          end
+      before_action -> { authorize_if_got_token! :read, :'read:statuses' }, only: :show
+  before_action :set_poll
+  before_action :refresh_poll
     
-              # Set all of our instance variables on the new class
-          [self, other].each do |obj|
-            obj.instance_variables.each do |key|
-              # Ignore keys that start with a double underscore. This allows
-              # configuration classes to still hold around internal state
-              # that isn't propagated.
-              if !key.to_s.start_with?('@__')
-                result.instance_variable_set(key, obj.instance_variable_get(key))
-              end
-            end
-          end
+      def perform(account_id, options = {})
+    @options = options.with_indifferent_access
+    @account = Account.find(account_id)
     
-              @registered.each do |plugin|
-            plugin.config.each do |key, klass|
-              result[key] = klass
-            end
-          end
+          expect(response).to redirect_to(settings_preferences_notifications_path)
+      user.reload
+      expect(user.settings['notification_emails']['follow']).to be true
+      expect(user.settings['interactions']['must_be_follower']).to be false
+    end
+  end
+end
+
     
-                    if provider_to_use && provider_to_use != active_provider
-                  # We found an active machine with a provider that doesn't
-                  # match the requested provider. Show an error.
-                  raise Errors::ActiveMachineWithDifferentProvider,
-                    name: active_name.to_s,
-                    active_provider: active_provider.to_s,
-                    requested_provider: provider_to_use.to_s
-                else
-                  # Use this provider and exit out of the loop. One of the
-                  # invariants [for now] is that there shouldn't be machines
-                  # with multiple providers.
-                  @logger.info('Active machine found with name #{active_name}. ' +
-                               'Using provider: #{active_provider}')
-                  provider_to_use = active_provider
-                  break
-                end
-              end
-            end
+      let(:json) do
+    {
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      id: 'foo',
+      type: 'Move',
+      actor: old_account.uri,
+      object: old_account.uri,
+      target: new_account.uri,
+    }.with_indifferent_access
+  end
     
-    module Vagrant
-  module Plugin
-    module V2
-      # This is the base class for a provider for the V2 API. A provider
-      # is responsible for creating compute resources to match the needs
-      # of a Vagrant-configured system.
-      class Provider
-        include CapabilityHost
+          it 'does not process payload if no signature exists' do
+        expect_any_instance_of(ActivityPub::LinkedDataSignature).to receive(:verify_account!).and_return(nil)
+        expect(ActivityPub::Activity).not_to receive(:factory)
+    
+        before do
+      recipient.follow!(sender)
+      ActivityPub::Activity::Undo.new(undo_json, sender).perform
+    end
+    
+          # Takes the amount in cents to capture.
+      # Can be used to capture partial amounts of a payment, and will create
+      # a new pending payment record for the remaining amount to capture later.
+      def capture!(amount = nil)
+        return true if completed?
+    
+        let(:return_item) { create(:return_item, reception_status: status) }
+    
+            private
+    
+        def last?
+      index == tab.panes.length - 1
+    end
+    
+          @name = first_key.to_s.shellescape unless first_key.nil?
+      @yaml = window_yaml.values.first
+      @project = project
+      @index = index
+      @commands = build_commands(tmux_window_command_prefix, @yaml)
+    end
+    
+      describe '#hook_on_project_start' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_start' }
+    end
+  end
+  describe '#hook_on_project_first_start' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_first_start' }
+    end
+  end
+  describe '#hook_on_project_restart' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_restart' }
+    end
+  end
+  describe '#hook_on_project_exit' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_exit' }
+    end
+  end
+  describe '#hook_on_project_stop' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_stop' }
+    end
+  end
+end
+
+    
+          def stop_template
+        asset_path 'template-stop.erb'
+      end
