4c630f33dcc27b15dfe6f7ac3ee559e4e9192b2f
SUNsung <weatherius@gmail.com>
2019-08-06 04:48:57 +0300

    Rewrite [57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py] File in: 2019-08-06 04:48:57

--- pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
+++ pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
@@ -1,146 +1,133 @@
 
         
-          Args:
-    hps: The dictionary of hyperparameters.
-    output_fname: The prefix of the file in which to save the generated
-      samples.
-    datasets: A dictionary of data dictionaries.  The dataset dict is simply a
-      name(string)-> data dictionary mapping (See top of lfads.py).
-  '''
-  if not output_fname:
-    output_fname = 'model_params'
-  else:
-    output_fname = output_fname + '_model_params'
-  fname = os.path.join(hps.lfads_save_dir, output_fname)
-  print('Writing model parameters to: ', fname)
-  # save the optimizer params as well
-  model = build_model(hps, kind='write_model_params', datasets=datasets)
-  model_params = model.eval_model_parameters(use_nested=False,
-                                             include_strs='LFADS')
-  utils.write_data(fname, model_params, compression=None)
-  print('Done.')
-    
-    flags = tf.app.flags
-flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
-                    'Directory for saving data.')
-flags.DEFINE_string('datafile_name', 'thits_data',
-                    'Name of data file for input case.')
-flags.DEFINE_string('noise_type', 'poisson', 'Noise type for data.')
-flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
-flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
-flags.DEFINE_integer('C', 100, 'Number of conditions')
-flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
-flags.DEFINE_integer('S', 50, 'Number of sampled units from RNN')
-flags.DEFINE_integer('npcs', 10, 'Number of PCS for multi-session case.')
-flags.DEFINE_float('train_percentage', 4.0/5.0,
-                   'Percentage of train vs validation trials')
-flags.DEFINE_integer('nreplications', 40,
-                     'Number of noise replications of the same underlying rates.')
-flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
-flags.DEFINE_float('x0_std', 1.0,
-                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
-flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
-flags.DEFINE_float('dt', 0.010, 'Time bin')
-flags.DEFINE_float('input_magnitude', 20.0,
-                   'For the input case, what is the value of the input?')
-flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
-FLAGS = flags.FLAGS
-    
-    # write out the dataset
-write_datasets(FLAGS.save_dir, FLAGS.datafile_name, datasets)
-print ('Saved to ', os.path.join(FLAGS.save_dir,
-                                 FLAGS.datafile_name + '_' + dataset_name))
+            # noinspection PyMethodOverriding
+    def get_auth(self, username, password):
+        return requests.auth.HTTPDigestAuth(username, password)
 
     
+            def get_auth(self, username=None, password=None):
+            assert username is None
+            assert password is None
+            assert self.raw_auth == BASIC_AUTH_HEADER_VALUE
+            return basic_auth(self.raw_auth)
     
-def split_list_by_inds(data, inds1, inds2):
-  '''Take the data, a list, and split it up based on the indices in inds1 and
-  inds2.
-  Args:
-    data: the list of data to split
-    inds1, the first list of indices
-    inds2, the second list of indices
-  Returns: a 2-tuple of two lists.
-  '''
-  if data is None or len(data) == 0:
-    return [], []
-  else:
-    dout1 = [data[i] for i in inds1]
-    dout2 = [data[i] for i in inds2]
-    return dout1, dout2
+        config['implicit_content_type'] = 'json'
+    config.save()
+    config.load()
+    assert 'implicit_content_type' not in config
+    assert not config['default_options']
     
     
-def write_unmasked_log(log, id_to_word, sequence_eval):
-  '''Helper function for logging evaluated sequences without mask.'''
-  indices_arr = np.asarray(sequence_eval)
-  samples = helper.convert_to_human_readable(id_to_word, indices_arr,
-                                             FLAGS.batch_size)
-  for sample in samples:
-    log.write(sample + '\n')
-  log.flush()
-  return samples
+def test_default_headers_case_insensitive(httpbin):
+    '''
+    <https://github.com/jakubroztocil/httpie/issues/644>
+    '''
+    r = http(
+        '--debug',
+        '--print=H',
+        httpbin.url + '/post',
+        'CONTENT-TYPE:application/json-patch+json',
+        'a=b',
+    )
+    assert 'CONTENT-TYPE: application/json-patch+json' in r
+    assert 'Content-Type' not in r
     
     
-def create_critic_train_op(hparams, critic_loss, global_step):
-  '''Create Discriminator train op.'''
-  with tf.name_scope('train_critic'):
-    critic_optimizer = tf.train.AdamOptimizer(hparams.critic_learning_rate)
-    output_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('critic')
-    ]
+@mock.patch('httpie.core.get_response')
+def test_error_traceback(get_response):
+    exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    with raises(ConnectionError):
+        main(['--ignore-stdin', '--traceback', 'www.google.com'])
     
     
-def dis_fwd_bidirectional(hparams):
-  '''Returns the *forward* PTB Variable name to MaskGAN Variable dictionary
-  mapping.  This is a highly restrictive function just for testing. This is for
-  the bidirectional_zaremba discriminator.
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
     
     
-class SessionManager(BaseSessionManager):
-    use_in_migrations = True
+class HTTPBasicAuth(AuthBase):
+    '''Attaches HTTP Basic Authentication to the given Request object.'''
     
-    else:
-    text_type = str
-    string_types = (str,)
-    xrange = range
+        :param str u_string: unicode string to check. Must be unicode
+        and not Python 2 `str`.
+    :rtype: bool
+    '''
+    assert isinstance(u_string, str)
+    try:
+        u_string.encode('ascii')
+        return True
+    except UnicodeEncodeError:
+        return False
+
     
+            try:
+            conn = self.get_connection(request.url, proxies)
+        except LocationValueError as e:
+            raise InvalidURL(e, request=request)
     
-def add_force_split(word):
-    global Force_Split_Words
-    Force_Split_Words.add(word)
+        #Creating title page
+    with open('title.html', 'w') as f:
+        f.write(header + title_content)
     
-        def __cut_detail(self, sentence):
-        blocks = re_han_detail.split(sentence)
-        for blk in blocks:
-            if re_han_detail.match(blk):
-                for word in self.__cut(blk):
-                    yield word
-            else:
-                tmp = re_skip_detail.split(blk)
-                for x in tmp:
-                    if x:
-                        if re_num.match(x):
-                            yield pair(x, 'm')
-                        elif re_eng.match(x):
-                            yield pair(x, 'eng')
-                        else:
-                            yield pair(x, 'x')
+        The following variables should be defined:
+        _HELPTEXT: A string describing what this plugin does
+        _DEFAULTS: A dictionary containing the options, defaults and meta information. The
+                   dictionary should be defined as:
+                       {<option_name>: {<metadata>}}
     
-    file_name = args[0]
+        The following keys are expected for the _DEFAULTS <metadata> dict:
+        datatype:  [required] A python type class. This limits the type of data that can be
+                   provided in the .ini file and ensures that the value is returned in the
+                   correct type to faceswap. Valid datatypes are: <class 'int'>, <class 'float'>,
+                   <class 'str'>, <class 'bool'>.
+        default:   [required] The default value for this option.
+        info:      [required] A string describing what this option does.
+        choices:   [optional] If this option's datatype is of <class 'str'> then valid
+                   selections can be defined here. This validates the option and also enables
+                   a combobox / radio option in the GUI.
+        gui_radio: [optional] If <choices> are defined, this indicates that the GUI should use
+                   radio buttons rather than a combobox to display this option.
+        min_max:   [partial] For <class 'int'> and <class 'float'> datatypes this is required
+                   otherwise it is ignored. Should be a tuple of min and max accepted values.
+                   This is used for controlling the GUI slider range. Values are not enforced.
+        rounding:  [partial] For <class 'int'> and <class 'float'> datatypes this is
+                   required otherwise it is ignored. Used for the GUI slider. For floats, this
+                   is the number of decimal places to display. For ints this is the step size.
+        fixed:     [optional] [train only]. Training configurations are fixed when the model is
+                   created, and then reloaded from the state file. Marking an item as fixed=False
+                   indicates that this value can be changed for existing models, and will override
+                   the value saved in the state file with the updated value in config. If not
+                   provided this will default to True.
+'''
     
-    log_f = open('1.log','w')
-log_f.write(' / '.join(map(str, words)))
-    
-            print('Two or more elements')
-        data = [5, 1, 7, 2, 6, -3, 5, 7, -1]
-        assert_equal(merge_sort.sort(data), sorted(data))
-    
-            print('Two or more elements')
-        data = [5, 1, 7, 2, 6, -3, 5, 7, -10]
-        assert_equal(func(data), sorted(data))
-    
-            print('Test: Multiple dequeue in a row')
-        assert_equal(queue.dequeue(), 1)
-        assert_equal(queue.dequeue(), 2)
-    
-            print('Success: test_sort_stack')
+        The following keys are expected for the _DEFAULTS <metadata> dict:
+        datatype:  [required] A python type class. This limits the type of data that can be
+                   provided in the .ini file and ensures that the value is returned in the
+                   correct type to faceswap. Valid datatypes are: <class 'int'>, <class 'float'>,
+                   <class 'str'>, <class 'bool'>.
+        default:   [required] The default value for this option.
+        info:      [required] A string describing what this option does.
+        choices:   [optional] If this option's datatype is of <class 'str'> then valid
+                   selections can be defined here. This validates the option and also enables
+                   a combobox / radio option in the GUI.
+        gui_radio: [optional] If <choices> are defined, this indicates that the GUI should use
+                   radio buttons rather than a combobox to display this option.
+        min_max:   [partial] For <class 'int'> and <class 'float'> datatypes this is required
+                   otherwise it is ignored. Should be a tuple of min and max accepted values.
+                   This is used for controlling the GUI slider range. Values are not enforced.
+        rounding:  [partial] For <class 'int'> and <class 'float'> datatypes this is
+                   required otherwise it is ignored. Used for the GUI slider. For floats, this
+                   is the number of decimal places to display. For ints this is the step size.
+        fixed:     [optional] [train only]. Training configurations are fixed when the model is
+                   created, and then reloaded from the state file. Marking an item as fixed=False
+                   indicates that this value can be changed for existing models, and will override
+                   the value saved in the state file with the updated value in config. If not
+                   provided this will default to True.
+'''
