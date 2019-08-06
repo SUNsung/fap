a013cbadd79b19d2089029673cbf1b17dca80702
SUNsung <weatherius@gmail.com>
2019-08-07 01:53:09 +0300

    Rewrite [405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb] File in: 2019-08-07 01:53:08

--- ruby/405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb
+++ ruby/405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb
@@ -1,58 +1,186 @@
 
         
-        require 'rack/test'
-require 'action_controller/railtie'
-require 'active_record'
-require 'devise/rails/routes'
-require 'devise/rails/warden_compat'
+            it 'defauls foreground and background colors' do
+      scenario.tag_fg_color = nil
+      scenario.tag_bg_color = nil
+      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
+    end
+  end
     
-      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
-     options[:store] != false && !env['devise.skip_timeoutable']
-    last_request_at = warden.session(scope)['last_request_at']
+      describe '#value_at' do
+    it 'returns the value at a JSON path' do
+      expect(Utils.value_at({ :foo => { :bar => :baz }}.to_json, 'foo.bar')).to eq('baz')
+      expect(Utils.value_at({ :foo => { :bar => { :bing => 2 } }}, 'foo.bar.bing')).to eq(2)
+      expect(Utils.value_at({ :foo => { :bar => { :bing => 2 } }}, 'foo.bar[?(@.bing == 2)].bing')).to eq(2)
+    end
     
-          included do
-        include Devise::Controllers::ScopedViews
-      end
+      let :valid_options do
+    {
+      'name' => 'XKCD',
+      'expected_update_period_in_days' => '2',
+      'type' => 'html',
+      'url' => '{{ url | default: 'http://xkcd.com/' }}',
+      'mode' => 'on_change',
+      'extract' => old_extract,
+      'template' => old_template
+    }
+  end
     
-        # Create magic predicates for verifying what module is activated by this map.
-    # Example:
-    #
-    #   def confirmable?
-    #     self.modules.include?(:confirmable)
-    #   end
-    #
-    def self.add_module(m)
-      class_eval <<-METHOD, __FILE__, __LINE__ + 1
-        def #{m}?
-          self.modules.include?(:#{m})
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
+    end
+    
+        context 'with a box name accidentally set as a URL' do
+      it 'displays a warning to the user' do
+        box_path = iso_env.box2_file(:virtualbox)
+        port = 9999
+    
+          # Returns the SSH info for accessing the Container.
+      def ssh_info
+        # If the container isn't running, we can't SSH into it
+        return nil if state.id != :running
+    
+        it 'should handle forks gracefully', :skip_windows do
+      # Doesn't need to be tested on Windows since Windows doesn't
+      # support fork(1)
+      allow(machine).to receive(:action) do |action, opts|
+        pid = fork
+        if !pid
+          # Child process
+          exit
         end
-      METHOD
-    end
     
-          def message
-        'The following attribute(s) is (are) missing on your model: #{@attributes.join(', ')}'
+            # Success, exit status 0
+        0
       end
-    end
-    
-          # Resets reset password token and send reset password instructions by email.
-      # Returns the token sent in the e-mail.
-      def send_reset_password_instructions
-        token = set_reset_password_token
-        send_reset_password_instructions_notification(token)
-    
-        private
-    
-        def set_report_note
-      @report_note = ReportNote.find(params[:id])
     end
   end
 end
 
     
-      def maxwidth_or_default
-    (params[:maxwidth].presence || 400).to_i
+        Feature5079 = '[ruby-core:38404]'
+    
+    testdata( File.dirname($0) + '/scandata', ARGV ).each do |file|
+  $stderr.print File.basename(file) + ': '
+  begin
+    ok = File.read(file)
+    s = Racc::GrammarFileScanner.new( ok )
+    sym, (val, _lineno) = s.scan
+    if printonly then
+      $stderr.puts
+      $stderr.puts val
+      next
+    end
+    
+        assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.sin('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.cos('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.tan('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.sinh('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.cosh('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.tanh('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.asin('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.atan('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.asinh('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.acosh('0') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.atanh('0') }
   end
     
-      private
+        gz.rewind
+    gz.pos.should == 0
+    gz.lineno.should == 0
+  end
     
-      private
+    describe 'GzipWriter#write' do
+  before :each do
+    @data = '12345abcde'
+    @zip = [31, 139, 8, 0, 44, 220, 209, 71, 0, 3, 51, 52, 50, 54, 49, 77,
+            76, 74, 78, 73, 5, 0, 157, 5, 0, 36, 10, 0, 0, 0].pack('C*')
+    @io = StringIO.new ''.b
+  end
+    
+        @inflator.inflate(@zeros) do |chunk|
+      @chunks << chunk
+      break
+    end
+    
+      desc 'update main and version in bower.json'
+  task :generate do
+    require 'bootstrap-sass'
+    Dir.chdir Bootstrap.gem_path do
+      spec       = JSON.parse(File.read 'bower.json')
+    
+      # Disable Rails's static asset server (Apache or nginx will already do this).
+  if config.respond_to?(:serve_static_files)
+    # rails >= 4.2
+    config.serve_static_files = true
+  elsif config.respond_to?(:serve_static_assets)
+    # rails < 4.2
+    config.serve_static_assets = true
+  end
+    
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+    
+      gem.licenses      = ['MIT']
+    
+        def load_imports
+      if options.show_tasks && Rake::Task.task_defined?('load:defaults')
+        invoke 'load:defaults'
+        set(:stage, '')
+        Dir[deploy_config_path].each { |f| add_import f }
+      end
+    
+        def add_cmdline_filter(type, values)
+      cmdline_filters << Filter.new(type, values)
+    end
+    
+        def URIEncodePair(cc1, cc2, result, index)
+      u = ((cc1 >> 6) & 0xF) + 1;
+      w = (cc1 >> 2) & 0xF;
+      x = cc1 & 3;
+      y = (cc2 >> 6) & 0xF;
+      z = cc2 & 63;
+      octets = Array.new(4);
+      octets[0] = (u >> 2) + 240;
+      octets[1] = (((u & 3) << 4) | w) + 128;
+      octets[2] = ((x << 4) | y) + 128;
+      octets[3] = z + 128;
+      return URIEncodeOctets(octets, result, index);
+    end
+    
+          # Extracts title from page if present.
+      #
+      def page_header_from_content(content)
+        doc   = build_document(content)
+        title = find_header_node(doc).inner_text.strip
+        title = nil if title.empty?
+        title
+      end
+    
+    $contexts = []
+    
+    # external
+require 'github/markup'
+require 'sanitize'
+    
+      context 'given a distribution without a META.* file' do
+    it 'should package IPC::Session' do
+      pending('Disabled on travis-ci because it always fails, and there is no way to debug it?') if is_travis
+    
+        args = [ '-B', build_path('build-info'), '-c', build_path('comment'), '-d', build_path('description'), '-f', build_path('packlist'), '-I', '/opt/local', '-p', staging_path,  '-U', '#{cwd}/#{name}-#{self.version}-#{iteration}.tgz' ]
+    safesystem('pkg_create', *args)
+    
+      def create_scripts
+    if script?(:after_install)
+      File.write(File.join(fpm_meta_path, 'after_install'), script(:after_install))
+    end
+  end
+    
+    # TODO(sissel): Add dependency checking support.
+# IIRC this has to be done as a 'checkinstall' step.
+class FPM::Package::Solaris < FPM::Package
