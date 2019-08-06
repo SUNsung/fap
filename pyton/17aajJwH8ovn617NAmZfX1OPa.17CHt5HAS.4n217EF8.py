e8032baf5a5458a826386ff21562381c998545a4
SUNsung <weatherius@gmail.com>
2019-08-06 07:55:08 +0300

    Rewrite [17aajJwH8ovn617NAmZfX1OPa.17CHt5HAS.4n217EF8.py] File in: 2019-08-06 07:55:08

--- pyton/17aajJwH8ovn617NAmZfX1OPa.17CHt5HAS.4n217EF8.py
+++ pyton/17aajJwH8ovn617NAmZfX1OPa.17CHt5HAS.4n217EF8.py
@@ -1,60 +1,152 @@
 
         
-                (2016-01, shopping), 125
-        (2016-01, gas), 50
-        '''
-        total = sum(values)
-        self.handle_budget_notifications(key, total)
-        yield key, sum(values)
-    
-    from mrjob.job import MRJob
-    
-        def get(self, query):
-        '''Get the stored query result from the cache.
-    
-        def iter_lines(self, chunk_size):
-        yield self.body, b''
-    
-    from httpie.input import SEP_CREDENTIALS
-from httpie.plugins import AuthPlugin, plugin_manager
-from utils import http, HTTP_OK
+        
+@pytest.fixture
+def get_aliases(mocker):
+    mocker.patch('thefuck.shells.shell.get_aliases',
+                 return_value=['vim', 'apt-get', 'fsck', 'fuck'])
     
     
-@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
-def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
-    r = http(follow_flag, httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 1
-    assert HTTP_OK in r
+@pytest.mark.usefixtures('isfile', 'no_memoize', 'no_cache')
+class TestTcsh(object):
+    @pytest.fixture
+    def shell(self):
+        return Tcsh()
     
-        def test_upload_ok(self, httpbin):
-        r = http('--form', '--verbose', 'POST', httpbin.url + '/post',
-                 'test-file@%s' % FILE_PATH_ARG, 'foo=bar')
-        assert HTTP_OK in r
-        assert 'Content-Disposition: form-data; name='foo'' in r
-        assert 'Content-Disposition: form-data; name='test-file';' \
-               ' filename='%s'' % os.path.basename(FILE_PATH) in r
-        assert FILE_CONTENT in r
-        assert ''foo': 'bar'' in r
-        assert 'Content-Type: text/plain' in r
+        def _parse_alias(self, alias):
+        name, value = alias.replace('alias ', '', 1).split('=', 1)
+        if value[0] == value[-1] == ''' or value[0] == value[-1] == ''':
+            value = value[1:-1]
+        return name, value
     
-        @cookies.setter
-    def cookies(self, jar):
-        '''
-        :type jar: CookieJar
-        '''
-        # http://docs.python.org/2/library/cookielib.html#cookie-objects
-        stored_attrs = ['value', 'path', 'secure', 'expires']
-        self['cookies'] = {}
-        for cookie in jar:
-            self['cookies'][cookie.name] = {
-                attname: getattr(cookie, attname)
-                for attname in stored_attrs
-            }
+        def get_aliases(self):
+        overridden = self._get_overridden_aliases()
+        functions = _get_functions(overridden)
+        raw_aliases = _get_aliases(overridden)
+        functions.update(raw_aliases)
+        return functions
     
     
-def image_files_in_folder(folder):
-    return [os.path.join(folder, f) for f in os.listdir(folder) if re.match(r'.*\.(jpg|jpeg|png)', f, flags=re.I)]
+@pytest.mark.usefixtures('no_memoize')
+@pytest.mark.parametrize('script, output', [
+    ('vom file.py', 'vom: not found'),
+    ('fucck', 'fucck: not found'),
+    ('puthon', ''puthon' is not recognized as an internal or external command'),
+    ('got commit', 'got: command not found')])
+def test_match(mocker, script, output):
+    mocker.patch('thefuck.rules.no_command.which', return_value=None)
     
-        # Apply some eyeliner
-    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
+    from thefuck.rules.pyenv_no_such_command import get_new_command, match
+from thefuck.types import Command
+    
+    from {0} import {1}
+    
+    
+def _get_operations(app):
+    proc = subprocess.Popen([app, '--help'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    lines = proc.stdout.readlines()
+    
+        # Custom icon for the DMG, shown when the DMG is mounted.
+    shutil.copy('../Icons/Disk Image.icns',
+            os.path.join(WORKDIR, 'mnt', volname, '.VolumeIcon.icns'))
+    runCommand('SetFile -a C %s/'%(
+            shellQuote(os.path.join(WORKDIR, 'mnt', volname)),))
+    
+            expected = {0: None, 1: None, 2: None, 3: None, 4: None, 5: None,
+                    6: None, 7: None, 8: None, 9: None}
+        actual = {k: None for k in range(10)}
+        self.assertEqual(actual, expected)
+        self.assertEqual(k, 'Local Variable')
+    
+        # bpo-35516: Don't replace Darwin with macOS since input release and
+    # version arguments can be different than the currently running version.
+    
+        @unittest.skipUnless(hasattr(posix, 'getresuid'),
+                         'test needs posix.getresuid()')
+    def test_getresuid(self):
+        user_ids = posix.getresuid()
+        self.assertEqual(len(user_ids), 3)
+        for val in user_ids:
+            self.assertGreaterEqual(val, 0)
+    
+        def _artcmd(self, line, file=None):
+        '''Internal: process a HEAD, BODY or ARTICLE command.'''
+        resp, lines = self._longcmd(line, file)
+        resp, art_num, message_id = self._statparse(resp)
+        return resp, ArticleInfo(art_num, message_id, lines)
+    
+    # Imports
+    
+                with support.captured_stderr() as stderr:
+                wmod.warn_explicit(
+                    'foo', UserWarning, 'bar', 1,
+                    module_globals={'__loader__': get_bad_loader(42),
+                                    '__name__': 'foobar'})
+            self.assertIn('UserWarning: foo', stderr.getvalue())
+    
+        def CheckExceptions(self):
+        # Optional DB-API extension.
+        self.assertEqual(self.cx.Warning, sqlite.Warning)
+        self.assertEqual(self.cx.Error, sqlite.Error)
+        self.assertEqual(self.cx.InterfaceError, sqlite.InterfaceError)
+        self.assertEqual(self.cx.DatabaseError, sqlite.DatabaseError)
+        self.assertEqual(self.cx.DataError, sqlite.DataError)
+        self.assertEqual(self.cx.OperationalError, sqlite.OperationalError)
+        self.assertEqual(self.cx.IntegrityError, sqlite.IntegrityError)
+        self.assertEqual(self.cx.InternalError, sqlite.InternalError)
+        self.assertEqual(self.cx.ProgrammingError, sqlite.ProgrammingError)
+        self.assertEqual(self.cx.NotSupportedError, sqlite.NotSupportedError)
+    
+                self._work_queue.put(w)
+            self._adjust_thread_count()
+            return f
+    submit.__doc__ = _base.Executor.submit.__doc__
+    
+                    await stream.write(b'GET / HTTP/1.0\r\n\r\n')
+                data = await stream.readline()
+                self.assertEqual(data, b'HTTP/1.0 200 OK\r\n')
+    
+        @staticmethod
+    def check_range(request):
+        if 0 <= request < 10:
+            print('request {} handled in handler 0'.format(request))
+            return True
+    
+        return restore
+    
+    *Where is the pattern used practically?
+    
+    ### OUTPUT ###
+# Inside with: yam
+# Outside with: yam
+# Inside func: sam
+# Outside func: sam
+
+    
+      tvars = tf.trainable_variables()
+  grads = tf.gradients(loss, tvars)
+    
+      is_per_host = tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2
+  run_config = tf.contrib.tpu.RunConfig(
+      cluster=tpu_cluster_resolver,
+      master=FLAGS.master,
+      model_dir=FLAGS.output_dir,
+      save_checkpoints_steps=FLAGS.save_checkpoints_steps,
+      tpu_config=tf.contrib.tpu.TPUConfig(
+          iterations_per_loop=FLAGS.iterations_per_loop,
+          num_shards=FLAGS.num_tpu_cores,
+          per_host_input_for_training=is_per_host))
+    
+    
+def create_tokenizer_from_hub_module(bert_hub_module_handle):
+  '''Get the vocab file and casing info from the Hub module.'''
+  with tf.Graph().as_default():
+    bert_module = hub.Module(bert_hub_module_handle)
+    tokenization_info = bert_module(signature='tokenization_info', as_dict=True)
+    with tf.Session() as sess:
+      vocab_file, do_lower_case = sess.run([tokenization_info['vocab_file'],
+                                            tokenization_info['do_lower_case']])
+  return tokenization.FullTokenizer(
+      vocab_file=vocab_file, do_lower_case=do_lower_case)
