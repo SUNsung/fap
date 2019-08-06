e2e6f4debdbd086fbc3a1d0a0ac68d220cb17d18
SUNsung <weatherius@gmail.com>
2019-08-06 03:52:04 +0300

    Rewrite [07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py] File in: 2019-08-06 03:52:03

--- pyton/07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py
+++ pyton/07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py
@@ -1,54 +1,166 @@
 
         
-                    if not res or not res.ok:
-                xlog.debug('ip:%s fail', ip)
-                continue
+            def setUp(self):
+        self.crawler = get_crawler(Spider, self.settings_dict)
+        self.spider = self.crawler._create_spider('foo')
+        self.mwman = DownloaderMiddlewareManager.from_crawler(self.crawler)
+        # some mw depends on stats collector
+        self.crawler.stats.open_spider(self.spider)
+        return self.mwman.open_spider(self.spider)
+    
+        @classmethod
+    def _get_mwlist_from_settings(cls, settings):
+        return build_component_list(
+            settings.getwithbase('DOWNLOADER_MIDDLEWARES'))
+    
+        def __init__(self, tag='a', attr='href', unique=False, process_value=None,
+                 strip=True, canonicalized=False):
+        warnings.warn(
+            'BaseSgmlLinkExtractor is deprecated and will be removed in future releases. '
+            'Please use scrapy.linkextractors.LinkExtractor',
+            ScrapyDeprecationWarning, stacklevel=2,
+        )
+        SGMLParser.__init__(self)
+        self.scan_tag = tag if callable(tag) else lambda t: t == tag
+        self.scan_attr = attr if callable(attr) else lambda a: a == attr
+        self.process_value = (lambda v: v) if process_value is None else process_value
+        self.current_link = None
+        self.unique = unique
+        self.strip = strip
+        if canonicalized:
+            self.link_key = lambda link: link.url
+        else:
+            self.link_key = lambda link: canonicalize_url(link.url,
+                                                          keep_fragments=True)
+    
+        def test_proxy_auth_empty_passwd(self):
+        os.environ['http_proxy'] = 'https://user:@proxy:3128'
+        mw = HttpProxyMiddleware()
+        req = Request('http://scrapytest.org')
+        assert mw.process_request(req, spider) is None
+        self.assertEqual(req.meta, {'proxy': 'https://proxy:3128'})
+        self.assertEqual(req.headers.get('Proxy-Authorization'), b'Basic dXNlcjo=')
+        # proxy from request.meta
+        req = Request('http://scrapytest.org', meta={'proxy': 'https://username:@proxy:3128'})
+        assert mw.process_request(req, spider) is None
+        self.assertEqual(req.meta, {'proxy': 'https://proxy:3128'})
+        self.assertEqual(req.headers.get('Proxy-Authorization'), b'Basic dXNlcm5hbWU6')
     
     
-def mask_rcnn_fcn_head_v1up4convs_gn(model, blob_in, dim_in, spatial_scale):
-    '''v1up design: 4 * (conv 3x3), convT 2x2, with GroupNorm'''
-    return mask_rcnn_fcn_head_v1upXconvs_gn(
-        model, blob_in, dim_in, spatial_scale, 4
+def configure_logging(settings=None, install_root_handler=True):
+    '''
+    Initialize logging defaults for Scrapy.
+    
+        @defer.inlineCallbacks
+    def test_redirect_follow_302(self):
+        _, out, _ = yield self.execute([self.url('/redirect-no-meta-refresh'), '-c', 'response.status'])
+        assert out.strip().endswith(b'200')
+    
+    '''
+    
+        fc_dim = cfg.FAST_RCNN.MLP_HEAD_DIM
+    model.FC(current, 'fc6', dim_in * roi_size * roi_size, fc_dim)
+    model.Relu('fc6', 'fc6')
+    return 'fc6', fc_dim
+
+    
+    
+def _whctrs(anchor):
+    '''Return width, height, x center, and y center for an anchor (window).'''
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[1] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
+    
+    
+def _build_forward_graph(model, single_gpu_build_func):
+    '''Construct the forward graph on each GPU.'''
+    all_loss_gradients = {}  # Will include loss gradients from all GPUs
+    # Build the model on each GPU with correct name and device scoping
+    for gpu_id in range(cfg.NUM_GPUS):
+        with c2_utils.NamedCudaScope(gpu_id):
+            all_loss_gradients.update(single_gpu_build_func(model))
+    return all_loss_gradients
+    
+    
+def add_single_scale_rpn_outputs(model, blob_in, dim_in, spatial_scale):
+    '''Add RPN outputs to a single scale model (i.e., no FPN).'''
+    anchors = generate_anchors(
+        stride=1. / spatial_scale,
+        sizes=cfg.RPN.SIZES,
+        aspect_ratios=cfg.RPN.ASPECT_RATIOS
+    )
+    num_anchors = anchors.shape[0]
+    dim_out = dim_in
+    # RPN hidden representation
+    model.Conv(
+        blob_in,
+        'conv_rpn',
+        dim_in,
+        dim_out,
+        kernel=3,
+        pad=1,
+        stride=1,
+        weight_init=gauss_fill(0.01),
+        bias_init=const_fill(0.0)
+    )
+    model.Relu('conv_rpn', 'conv_rpn')
+    # Proposal classification scores
+    model.Conv(
+        'conv_rpn',
+        'rpn_cls_logits',
+        dim_in,
+        num_anchors,
+        kernel=1,
+        pad=0,
+        stride=1,
+        weight_init=gauss_fill(0.01),
+        bias_init=const_fill(0.0)
+    )
+    # Proposal bbox regression deltas
+    model.Conv(
+        'conv_rpn',
+        'rpn_bbox_pred',
+        dim_in,
+        4 * num_anchors,
+        kernel=1,
+        pad=0,
+        stride=1,
+        weight_init=gauss_fill(0.01),
+        bias_init=const_fill(0.0)
     )
     
-            # Same story for the scores:
-        #   - scores are (A, H, W) format from conv output
-        #   - transpose to (H, W, A)
-        #   - reshape to (H * W * A, 1) where rows are ordered by (H, W, A)
-        #     to match the order of anchors and bbox_deltas
-        scores = scores.transpose((1, 2, 0)).reshape((-1, 1))
+        # Create new roi blobs for each FPN level
+    # (See: modeling.FPN.add_multilevel_roi_blobs which is similar but annoying
+    # to generalize to support this particular case.)
+    rois_idx_order = np.empty((0, ))
+    for output_idx, lvl in enumerate(range(lvl_min, lvl_max + 1)):
+        idx_lvl = np.where(lvls == lvl)[0]
+        blob_roi_level = rois[idx_lvl, :]
+        outputs[output_idx + 1].reshape(blob_roi_level.shape)
+        outputs[output_idx + 1].data[...] = blob_roi_level
+        rois_idx_order = np.concatenate((rois_idx_order, idx_lvl))
+    rois_idx_restore = np.argsort(rois_idx_order)
+    blob_utils.py_op_copy_blob(rois_idx_restore.astype(np.int32), outputs[-1])
+
     
-        # The bbox regression loss only averages by the number of images in the
-    # mini-batch, whereas we need to average by the total number of example
-    # anchors selected
-    # Outside weights are used to scale each element-wise loss so the final
-    # average over the mini-batch is correct
-    bbox_outside_weights = np.zeros((num_inside, 4), dtype=np.float32)
-    # uniform weighting of examples (given non-uniform sampling)
-    num_examples = np.sum(labels >= 0)
-    bbox_outside_weights[labels == 1, :] = 1.0 / num_examples
-    bbox_outside_weights[labels == 0, :] = 1.0 / num_examples
+        _distribute_rois_over_fpn_levels('rois')
+    if cfg.MODEL.MASK_ON:
+        _distribute_rois_over_fpn_levels('mask_rois')
+    if cfg.MODEL.KEYPOINTS_ON:
+        _distribute_rois_over_fpn_levels('keypoint_rois')
+
     
+        # Scale rois_fg and format as (batch_idx, x1, y1, x2, y2)
+    rois_fg *= im_scale
+    repeated_batch_idx = batch_idx * blob_utils.ones((rois_fg.shape[0], 1))
+    rois_fg = np.hstack((repeated_batch_idx, rois_fg))
     
-def serialize(obj):
-    '''Serialize a Python object using pickle and encode it as an array of
-    float32 values so that it can be feed into the workspace. See deserialize().
-    '''
-    return np.fromstring(pickle.dumps(obj), dtype=np.uint8).astype(np.float32)
-    
-    
-def UnscopeName(possibly_scoped_name):
-    '''Remove any name scoping from a (possibly) scoped name. For example,
-    convert the name 'gpu_0/foo' to 'foo'.'''
-    assert isinstance(possibly_scoped_name, string_types)
-    return possibly_scoped_name[
-        possibly_scoped_name.rfind(scope._NAMESCOPE_SEPARATOR) + 1:]
-    
-    import numpy as np
-import scipy.io as sio
-import sys
-    
-    
-if __name__ == '__main__':
-    opts = parse_args()
-    convert(opts.json_file, opts.output_dir)
+        for filename in filenames:
+        fd = codecs.open(filename, mode='r', encoding='utf-8')
+        for line in fd.readlines():
+            refs = re.findall(r'(?<=<a href=')[^']*', markdown.markdown(line))
+            for ref in refs:
+                if ref not in urls:
+                    urls.append(ref)
