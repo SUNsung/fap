9c9dc9a779c6accb016485c0a7312fa820bfb347
SUNsung <weatherius@gmail.com>
2019-08-06 20:43:42 +0300

    Rewrite [26VZqo3hMPGlU26J8izb7FDn6226wVOgQZ363oA26Q8K.rb] File in: 2019-08-06 20:43:42

--- ruby/26VZqo3hMPGlU26J8izb7FDn6226wVOgQZ363oA26Q8K.rb
+++ ruby/26VZqo3hMPGlU26J8izb7FDn6226wVOgQZ363oA26Q8K.rb
@@ -1,6 +1,22 @@
 
         
-            def no_subcommand(args)
+          test 'rails routes with namespaced controller search key' do
+    app_file 'config/routes.rb', <<-RUBY
+      Rails.application.routes.draw do
+        namespace :admin do
+          resource :post
+          resource :user_permission
+        end
+      end
+    RUBY
+    
+    multitask :default => [:test, :features]
+    
+          def feature_element_timing_key(feature_element)
+        '\'#{feature_element.name}\' (#{feature_element.location})'
+      end
+    
+        def no_subcommand(args)
       unless args.empty? ||
           args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
         deprecation_message 'Jekyll now uses subcommands instead of just switches. \
@@ -9,70 +25,69 @@
       end
     end
     
-    module Gitlab
-  module Ci
-    module Pipeline
-      # Class for preloading data associated with pipelines such as commit
-      # authors.
-      class Preloader
-        def self.preload!(pipelines)
-          ##
-          # This preloads all commits at once, because `Ci::Pipeline#commit` is
-          # using a lazy batch loading, what results in only one batched Gitaly
-          # call.
-          #
-          pipelines.each(&:commit)
+      # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both thread web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
     
-          def request_count_counter
-        @request_counter ||= Gitlab::Metrics.counter(
-          :github_importer_request_count,
-          'The number of GitHub API calls performed when importing projects'
-        )
+      # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = false
+    
+      # Node Settings
+  option ['-n', '--node.name'], 'NAME',
+    I18n.t('logstash.runner.flag.name'),
+    :attribute_name => 'node.name',
+    :default => LogStash::SETTINGS.get_default('node.name')
+    
+    # This is basically a copy of the original bundler 'bundle' shim
+# with the addition of the loading of our Bundler patches that
+# modify Bundler's caching behaviour.
+    
+    require 'clamp'
+require 'pluginmanager/util'
+require 'pluginmanager/gemfile'
+require 'pluginmanager/install'
+require 'pluginmanager/remove'
+require 'pluginmanager/list'
+require 'pluginmanager/update'
+require 'pluginmanager/pack'
+require 'pluginmanager/unpack'
+require 'pluginmanager/generate'
+require 'pluginmanager/prepare_offline_pack'
+require 'pluginmanager/proxy_support'
+configure_proxy
+    
+        context 'with a specific plugin' do
+      let(:plugin_name) { 'logstash-input-stdin' }
+      it 'list the plugin and display the plugin name' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
+      end
+    
+        context 'update all the plugins' do
+      it 'has executed successfully' do
+        logstash.run_command_in_path('bin/logstash-plugin update --no-verify')
+        expect(logstash).to have_installed?(plugin_name, '0.1.1')
       end
     end
   end
 end
 
     
-            def collection_method
-          :issues
-        end
     
-            attr_reader :attributes
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'paths'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'selectors'))
     
-          # Returns the keyword of the `case` statement as a string.
-      #
-      # @return [String] the keyword of the `case` statement
-      def keyword
-        'case'
-      end
+      def self.source_root
+    @source_root ||= File.expand_path('../templates', __FILE__)
+  end
     
-    module RuboCop
-  module AST
-    # Common functionality for nodes that can be used as hash elements:
-    # `pair`, `kwsplat`
-    module HashElementNode
-      # Returns the key of this `hash` element.
-      #
-      # @note For keyword splats, this returns the whole node
-      #
-      # @return [Node] the key of the hash element
-      def key
-        node_parts[0]
-      end
-    
-          # @object_cache maps configuration file paths to
-      # configuration objects so we only need to load them once.
-      @object_cache = {}
+        def type_from_file_command
+      @type_from_file_command ||=
+        FileCommandContentTypeDetector.new(@filepath).detect
     end
-    
-            def check_argument(variable)
-          return if allowed_block?(variable) ||
-                    allowed_keyword_argument?(variable)
-    
-      puts '== Installing dependencies =='
-  system! 'gem install bundler --conservative'
-  system('bundle check') || system!('bundle install')
-    
-      # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  end
+end
