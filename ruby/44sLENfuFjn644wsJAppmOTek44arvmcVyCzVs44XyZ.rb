cd218da6bec4f1171ab8baff67a4bb2967111097
SUNsung <weatherius@gmail.com>
2019-08-06 18:37:17 +0300

    Rewrite [44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb] File in: 2019-08-06 18:37:17

--- ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
+++ ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
@@ -1,84 +1,98 @@
 
         
-              get :render_with_object_location
-      assert_equal 'http://www.nextangle.com/customers/1', @response.headers['Location']
-    end
-  end
+        class ActivityPub::Activity::Move < ActivityPub::Activity
+  PROCESSING_COOLDOWN = 7.days.seconds
     
-      def test_browser_check_with_any_any
-    @request.accept = 'application/json, application/xml'
-    get :json_xml_or_html
-    assert_equal 'JSON', @response.body
+        describe '#perform' do
+      subject { described_class.new(json, sender) }
     
-    class RenderersTest < ActionController::TestCase
-  class XmlRenderable
-    def to_xml(options)
-      options[:root] ||= 'i-am-xml'
-      '<#{options[:root]}/>'
-    end
-  end
-  class JsonRenderable
-    def as_json(options = {})
-      hash = { a: :b, c: :d, e: :f }
-      hash.except!(*options[:except]) if options[:except]
-      hash
-    end
+          context '!reserved_username?' do
+        let(:reserved_username) { false }
     
-          def subscribed(callback, pattern = nil, monotonic: false, &block)
-        subscriber = notifier.subscribe(pattern, callback, monotonic: monotonic)
-        yield
-      ensure
-        unsubscribe(subscriber)
-      end
+          context 'if the message chain initiated by recipient, but is not direct message' do
+        let(:reply_to) { Fabricate(:status, account: recipient) }
+        let(:activity) { Fabricate(:mention, account: recipient, status: Fabricate(:status, account: sender, visibility: :direct, thread: reply_to)) }
     
-          # @return [String, Nil] the dSYM path, if one exists
-      #
-      attr_reader :dsym_path
+        context 'when parent status is private' do
+      let(:visibility) { 'private' }
     
-            # @return [Hash<String, TargetInstallationResult>] pod_target_installation_results
-        #         Hash of pod target name to installation results.
-        #
-        attr_reader :pod_target_installation_results
+    @@ layout
+<html>
+  <head>
+    <title>Super Simple Chat with Sinatra</title>
+    <meta charset='utf-8' />
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+  </head>
+  <body><%= yield %></body>
+</html>
     
-        # @return [Config] the current config instance creating one if needed.
-    #
-    def self.instance
-      @instance ||= new
+            if has_vector?(request, headers)
+          warn env, 'attack prevented by #{self.class}'
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
     end
     
-        it 'sets the Pods group\'s location path to ${PODS_ROOT}' do
-      XCConfigIntegrator.integrate(@pod_bundle, [@target])
-      @project['Pods'].path.should.equal @pod_bundle.relative_pods_root_path.to_s
-    end
+          @left_diff_line_number = nil
     
-            # Initialize a new instance.
-        #
-        # @param [Sandbox] sandbox @see #sandbox
-        # @param [Hash{String => TargetInstallationResult}] aggregate_target_installation_results @see #aggregate_target_installation_results
-        # @param [Hash{String => TargetInstallationResult}] pod_target_installation_results @see #pod_target_installation_results
-        # @param [ProjectMetadataCache] metadata_cache @see #metadata_cache
-        #
-        def initialize(sandbox, aggregate_target_installation_results, pod_target_installation_results, metadata_cache)
-          @sandbox = sandbox
-          @aggregate_target_installation_results = aggregate_target_installation_results
-          @pod_target_installation_results = pod_target_installation_results
-          @metadata_cache = metadata_cache
+        assert_no_match /Delete this Page/, last_response.body, ''Delete this Page' link not blocked in page template'
+    assert_no_match /New/,              last_response.body, ''New' button not blocked in page template'
+    assert_no_match /Upload/,           last_response.body, ''Upload' link not blocked in page template'
+    assert_no_match /Rename/,           last_response.body, ''Rename' link not blocked in page template'
+    assert_no_match /Edit/,             last_response.body, ''Edit' link not blocked in page template'
+    
+    context 'Frontend with lotr' do
+  include Rack::Test::Methods
+    
+      test 'h1 title sanitizes correctly' do
+    title = 'H1'
+    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
+    page = @wiki.page(title)
+    
+        @wiki.update_page(@wiki.page('k'), nil, nil, '다른 text', {})
+    @wiki = Gollum::Wiki.new(@path)
+    page  = @wiki.page('k')
+    assert_equal '다른 text', utf8(page.raw_data)
+    
+        EMOJI_PATHNAME = Pathname.new(Gemojione.images_path).freeze
+    
+      s.add_dependency 'gollum-lib', '~> 4.2', '>= 4.2.10'
+  s.add_dependency 'kramdown', '~> 1.9.0'
+  s.add_dependency 'sinatra', '~> 1.4', '>= 1.4.4'
+  s.add_dependency 'mustache', ['>= 0.99.5', '< 1.0.0']
+  s.add_dependency 'useragent', '~> 0.16.2'
+  s.add_dependency 'gemojione', '~> 3.2'
+    
+    # Set ruby to UTF-8 mode
+# This is required for Ruby 1.8.7 which gollum still supports.
+$KCODE = 'U' if RUBY_VERSION[0, 3] == '1.8'
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
         end
-    
-      module Generator
-    autoload :Acknowledgements,        'cocoapods/generator/acknowledgements'
-    autoload :Markdown,                'cocoapods/generator/acknowledgements/markdown'
-    autoload :Plist,                   'cocoapods/generator/acknowledgements/plist'
-    autoload :BridgeSupport,           'cocoapods/generator/bridge_support'
-    autoload :Constant,                'cocoapods/generator/constant'
-    autoload :CopyResourcesScript,     'cocoapods/generator/copy_resources_script'
-    autoload :DummySource,             'cocoapods/generator/dummy_source'
-    autoload :EmbedFrameworksScript,   'cocoapods/generator/embed_frameworks_script'
-    autoload :FileList,                'cocoapods/generator/file_list'
-    autoload :Header,                  'cocoapods/generator/header'
-    autoload :InfoPlistFile,           'cocoapods/generator/info_plist_file'
-    autoload :ModuleMap,               'cocoapods/generator/module_map'
-    autoload :PrefixHeader,            'cocoapods/generator/prefix_header'
-    autoload :UmbrellaHeader,          'cocoapods/generator/umbrella_header'
-    autoload :AppTargetHelper,         'cocoapods/generator/app_target_helper'
-  end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
+    end
