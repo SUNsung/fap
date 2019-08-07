6d95b0d1dfe0f151ce4bcf5061f35f58b7f66553
SUNsung <weatherius@gmail.com>
2019-08-07 04:42:41 +0300

    Rewrite [56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py] File in: 2019-08-07 04:42:40

--- pyton/56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py
+++ pyton/56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py
@@ -1,178 +1,55 @@
 
         
-            def __init__(self, level, row, spot_number, spot_size, vehicle_size):
-        self.level = level
-        self.row = row
-        self.spot_number = spot_number
-        self.spot_size = spot_size
-        self.vehicle_size = vehicle_size
-        self.vehicle = None
-    
-            (foo, p1), 2
-        (bar, p1), 3
-        (foo, p2), 3
-        (bar, p3), 10
-        (foo, p4), 1
-        '''
-        yield key, sum(values)
-    
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
-    
-        def move_to_front(self, node):
-        ...
-    
-        def remove_link_to_crawl(self, url):
-        '''Remove the given link from `links_to_crawl`.'''
-        pass
-    
-    names = []
-for ie in ordered_cls:
-    name = ie.__name__
-    src = build_lazy_ie(ie, name)
-    module_contents.append(src)
-    if ie in _ALL_CLASSES:
-        names.append(name)
-    
-        def test_allsubtitles(self):
-        self.DL.params['writesubtitles'] = True
-        self.DL.params['allsubtitles'] = True
-        subtitles = self.getSubtitles()
-        self.assertEqual(set(subtitles.keys()), set(['no']))
-        self.assertEqual(md5(subtitles['no']), '544fa917d3197fcbee64634559221cc2')
+                # put all lines in the file into a Python list
+        strings = f.readlines()
+        
+        # above line leaves trailing newline characters; strip them out
+        strings = [x.strip(u'\n') for x in strings]
+        
+        # remove empty-lines and comments
+        strings = [x for x in strings if x and not x.startswith(u'#')]
+        
+        # insert empty string since all are being removed
+        strings.insert(0, u'')
     
     
-@pytest.mark.parametrize(
-    ('username', 'password', 'message'),
-    (('a', 'test', b'Incorrect username.'), ('test', 'a', b'Incorrect password.')),
-)
-def test_login_validate_input(auth, username, password, message):
-    response = auth.login(username, password)
-    assert message in response.data
-    
-            if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET title = ?, body = ? WHERE id = ?', (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
-    
-        app.url_map.converters['ctx'] = ContextConverter
-    
-        # TASK: print the cross-validated scores for the each parameters set
-    # explored by the grid search
-    
-    First example
--------------
-The first example illustrates how robust covariance estimation can help
-concentrating on a relevant cluster when another one exists. Here, many
-observations are confounded into one and break down the empirical covariance
-estimation.
-Of course, some screening tools would have pointed out the presence of two
-clusters (Support Vector Machines, Gaussian Mixture Models, univariate
-outlier detection, ...). But had it been a high-dimensional example, none
-of these could be applied that easily.
-    
-                plt.subplots_adjust(bottom=0, top=.89, wspace=0,
-                                left=0, right=1)
-            plt.suptitle('n_cluster=%i, connectivity=%r' %
-                         (n_clusters, connectivity is not None), size=17)
-    
-        ax.w_xaxis.set_ticklabels([])
-    ax.w_yaxis.set_ticklabels([])
-    ax.w_zaxis.set_ticklabels([])
-    ax.set_xlabel('Petal width')
-    ax.set_ylabel('Sepal length')
-    ax.set_zlabel('Petal length')
-    ax.set_title(titles[fignum - 1])
-    ax.dist = 12
-    fignum = fignum + 1
-    
-    plt.show()
 
     
-    ######################################################################
-# Generate datasets. We choose the size big enough to see the scalability
-# of the algorithms, but not too big to avoid too long running times
+        这里实际上没有用到 J 和 d 这个参数，保留是为了与 `attention_flow()` 的参数兼容
     
-    import numpy as np
-from sklearn.cluster import MeanShift, estimate_bandwidth
-from sklearn.datasets.samples_generator import make_blobs
     
-    # MiniBatchKMeans
-ax = fig.add_subplot(1, 3, 2)
-for k, col in zip(range(n_clusters), colors):
-    my_members = mbk_means_labels == order[k]
-    cluster_center = mbk_means_cluster_centers[order[k]]
-    ax.plot(X[my_members, 0], X[my_members, 1], 'w',
-            markerfacecolor=col, marker='.')
-    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-            markeredgecolor='k', markersize=6)
-ax.set_title('MiniBatchKMeans')
-ax.set_xticks(())
-ax.set_yticks(())
-plt.text(-3.5, 1.8, 'train time: %.2fs\ninertia: %f' %
-         (t_mini_batch, mbk.inertia_))
+class PipenvUsageError(UsageError):
     
-    # Get a reference to the Raspberry Pi camera.
-# If this fails, make sure you have a camera connected to the RPi and that you
-# enabled your camera in raspi-config and rebooted first.
-camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-output = np.empty((240, 320, 3), dtype=np.uint8)
+        with monkeypatch.context() as m:
+        cerberus = __import__('cerberus')
+        m.setattr('pkg_resources.get_distribution', raise_distribution_not_found)
+        reload(cerberus)
+        assert cerberus.__version__ == 'unknown'
     
-    while video_capture.isOpened():
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
     
-    # Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
+# TODO remove 'validator' as rule parameter with the next major release
+@mark.parametrize('rule', ('check_with', 'validator'))
+def test_check_with_method(rule):
+    # https://github.com/pyeve/cerberus/issues/265
+    class MyValidator(cerberus.Validator):
+        def _check_with_oddity(self, field, value):
+            if not value & 1:
+                self._error(field, 'Must be an odd number')
     
-        # 讲识别结果以json键值对的数据结构输出
-    result = {
-        'face_found_in_image': face_found,
-        'is_picture_of_obama': is_obama
-    }
-    return jsonify(result)
+        def _drop_remaining_rules(self, *rules):
+        ''' Drops rules from the queue of the rules that still need to be
+            evaluated for the currently processed field.
+            If no arguments are given, the whole queue is emptied.
+        '''
+        if rules:
+            for rule in rules:
+                try:
+                    self._remaining_rules.remove(rule)
+                except ValueError:
+                    pass
+        else:
+            self._remaining_rules = []
     
-        # Make the eyebrows into a nightmare
-    d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
-    d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
-    d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
-    d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
-    
-    # This is a super simple (but slow) example of running face recognition on live video from your webcam.
-# There's a second example that's a little more complicated but runs faster.
-    
-                # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-    
-            # Loop through each face in this frame of video
-        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-    
-    PROJECT_NAME = 'Home Assistant'
-PROJECT_PACKAGE_NAME = 'homeassistant'
-PROJECT_AUTHOR = 'The Home Assistant Authors'
-PROJECT_COPYRIGHT = ' 2013-2018, {}'.format(PROJECT_AUTHOR)
-PROJECT_LONG_DESCRIPTION = ('Home Assistant is an open-source '
-                            'home automation platform running on Python 3. '
-                            'Track and control all devices at home and '
-                            'automate control. '
-                            'Installation in less than a minute.')
-PROJECT_GITHUB_USERNAME = 'home-assistant'
-PROJECT_GITHUB_REPOSITORY = 'home-assistant'
-    
-            if not version.is_prerelease or version.release[2] != 0:
-            to_change['release'] = _bump_release(version.release, 'minor')
+        def get_missing_message(self, param):
+        formated_choices = ['{:<12} {}'.format(k, self.choices[k] or '') for k in sorted(self.choices.keys())]
+        return 'Choose from\n  ' + '\n  '.join(formated_choices)
