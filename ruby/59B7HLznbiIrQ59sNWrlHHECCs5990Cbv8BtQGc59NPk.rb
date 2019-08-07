a6eaac9c734e88f2cd4165940378bf834fd4360d
SUNsung <weatherius@gmail.com>
2019-08-07 18:31:41 +0300

    Rewrite [59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb] File in: 2019-08-07 18:31:41

--- ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
+++ ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
@@ -1,65 +1,199 @@
 
         
-                  directory = select_directory(file_name)
-          directory ? File.join(category_directory(directory), file_name) : nil
+            context 'updating existing users' do
+      it 'follows the edit link' do
+        visit admin_users_path
+        click_on('bob')
+        expect(page).to have_text('Edit User')
+      end
+    
+        it 'opens the dry run modal even when clicking on the refresh icon' do
+      visit edit_agent_path(agent)
+      find('.agent-dry-run-button span.glyphicon').click
+      expect(page).to have_text('Event to send (Optional)')
+    end
+    
+          it 'returns an active nav link with menu when on a child page' do
+        stub(self).current_page?('/things') { false }
+        stub(self).current_page?('/things/stuff') { true }
+        nav = nav_link('Things', '/things') { nav_link('Stuff', '/things/stuff') }
+        expect(nav).to be_html_safe
+        a0 = Nokogiri(nav).at('li.dropdown.dropdown-hover.active > a[href='/things']')
+        expect(a0).to be_a Nokogiri::XML::Element
+        expect(a0.text.strip).to eq('Things')
+        a1 = Nokogiri(nav).at('li.dropdown.dropdown-hover.active > li:not(.active) > a[href='/things/stuff']')
+        expect(a1).to be_a Nokogiri::XML::Element
+        expect(a1.text.strip).to eq('Stuff')
+      end
+    end
+  end
+    
+        it 'in the future' do
+      expect(relative_distance_of_time_in_words(Time.now+5.minutes)).to eq('in 5m')
+    end
+  end
+end
+
+    
+        it 'respects an environment variable that specifies a path or URL to a different scenario' do
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
+    end
+    
+      describe '#log_length' do
+    it 'defaults to 200' do
+      expect(AgentLog.log_length).to eq(200)
+    end
+  end
+end
+
+    
+          3.times do
+        Devise.warden { |config| executed += 1 }
+      end
+    
+    class Devise::ConfirmationsController < DeviseController
+  # GET /resource/confirmation/new
+  def new
+    self.resource = resource_class.new
+  end
+    
+      def failed_strategy
+    request.respond_to?(:get_header) ? request.get_header('omniauth.error.strategy') : request.env['omniauth.error.strategy']
+  end
+    
+      def respond_to_on_destroy
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+    end
+  end
+end
+
+    
+    module Devise
+  module Controllers
+    # Provide the ability to store a location.
+    # Used to redirect back to a desired path after sign in.
+    # Included by default in all controllers.
+    module StoreLocation
+      # Returns and delete (if it's navigational format) the url stored in the session for
+      # the given scope. Useful for giving redirect backs after sign up:
+      #
+      # Example:
+      #
+      #   redirect_to stored_location_for(:user) || root_path
+      #
+      def stored_location_for(resource_or_scope)
+        session_key = stored_location_key_for(resource_or_scope)
+    
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
+    end
+    
+          def mailer_from(mapping)
+        mailer_sender(mapping, :from)
+      end
+    
+    run SinatraStaticServer
+
+    
+            Dir.chdir(includes_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(file)
+            source = File.read(file)
+            partial = Liquid::Template.parse(source)
+            context.stack do
+              rtn = rtn + partial.render(context)
+            end
+          else
+            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
+          end
         end
-    
-    module Gitlab
-  module CryptoHelper
-    extend self
-    
-            self.instance_eval(&block) if block_given?
-    
-        context 'when user cannot update_project_member' do
-      before do
-        allow(presenter).to receive(:can?).with(user, :update_project_member, presenter).and_return(false)
-        allow(presenter).to receive(:can?).with(user, :override_project_member, presenter).and_return(false)
       end
-    
-          def get_type
-        category, subcategory, subfolder, page = *slug.split('/')
-        provider = page ? subcategory : category
-        nice_provider = PROVIDER_NAME_MAP[provider] || provider.capitalize
-    
-      action :raw do
-    title 'Send a raw RFC2882 message'
-    description 'This action allows you to send us a raw RFC2822 formatted message along with the recipients that it should be sent to. This is similar to sending a message through our SMTP service.'
-    param :mail_from, 'The address that should be logged as sending the message', :type => String, :required => true
-    param :rcpt_to, 'The addresses this message should be sent to', :type => Array, :required => true
-    param :data, 'A base64 encoded RFC2822 message to send', :type => String, :required => true
-    param :bounce, 'Is this message a bounce?', :type => :boolean
-    returns Hash
-    error 'UnauthenticatedFromAddress', 'The From address is not authorised to send mail from this server'
-    action do
-      # Decode the raw message
-      raw_message = Base64.decode64(params.data)
-    
-      def check_spf_record
-    result = resolver.getresources(self.name, Resolv::DNS::Resource::IN::TXT)
-    spf_records = result.map(&:data).select { |d| d =~ /\Av=spf1/}
-    if spf_records.empty?
-      self.spf_status = 'Missing'
-      self.spf_error = 'No SPF record exists for this domain'
-    else
-      suitable_spf_records = spf_records.select { |d| d =~ /include\:\s*#{Regexp.escape(Postal.config.dns.spf_include)}/}
-      if suitable_spf_records.empty?
-        self.spf_status = 'Invalid'
-        self.spf_error = 'An SPF record exists but it doesn't include #{Postal.config.dns.spf_include}'
-        false
-      else
-        self.spf_status = 'OK'
-        self.spf_error = nil
-        true
-      end
+      rtn
     end
   end
     
-      def update
-    if @credential.update(params.require(:credential).permit(:name, :hold))
-      redirect_to_with_json [organization, @server, :credentials]
-    else
-      render_form_errors 'edit', @credential
-    end
-  end
     
+    
+        rc_files.each do |rc_file|
+      if File.readable? rc_file
+        logger.warn('Loading flags from rc file #{rc_file}')
+        rc_args.push(*Shellwords.shellsplit(File.read(rc_file)))
+      end
+    end
+    
+      option '--registry', 'NPM_REGISTRY',
+    'The npm registry to use instead of the default.'
+    
+      private
+  # return the identifier by prepending the reverse-domain prefix
+  # to the package name, else return just the name
+  def identifier
+    identifier = name.dup
+    if self.attributes[:osxpkg_identifier_prefix]
+      identifier.insert(0, '#{self.attributes[:osxpkg_identifier_prefix]}.')
+    end
+    identifier
+  end # def identifier
+    
+      public(:input)
+end # class FPM::Package::PleaseRun
+
+    
+      def default_output
+    v = version
+    v = '#{epoch}:#{v}' if epoch
+    if iteration
+      '#{name}_#{v}-#{iteration}_#{architecture}.#{type}'
+    else
+      '#{name}_#{v}_#{architecture}.#{type}'
+    end
+  end # def default_output
+end # class FPM::Deb
+    
+    # Use this to fill in an entire form with data from a table. Example:
+#
+#   When I fill in the following:
+#     | Account Number | 5002       |
+#     | Expiry date    | 2009-11-01 |
+#     | Note           | Nice guy   |
+#     | Wants Email?   |            |
+#
+# TODO: Add support for checkbox, select og option
+# based on naming conventions.
+#
+When /^(?:|I )fill in the following:$/ do |fields|
+  fields.rows_hash.each do |name, value|
+    When %{I fill in '#{name}' with '#{value}'}
+  end
+end
+    
+    # This stuff needs to be run after Paperclip is defined.
+require 'paperclip/io_adapters/registry'
+require 'paperclip/io_adapters/abstract_adapter'
+require 'paperclip/io_adapters/empty_string_adapter'
+require 'paperclip/io_adapters/identity_adapter'
+require 'paperclip/io_adapters/file_adapter'
+require 'paperclip/io_adapters/stringio_adapter'
+require 'paperclip/io_adapters/data_uri_adapter'
+require 'paperclip/io_adapters/nil_adapter'
+require 'paperclip/io_adapters/attachment_adapter'
+require 'paperclip/io_adapters/uploaded_file_adapter'
+require 'paperclip/io_adapters/uri_adapter'
+require 'paperclip/io_adapters/http_url_proxy_adapter'
+
+    
+        # Extracts the Geometry from a 'WxH,O' string
+    # Where W is the width, H is the height,
+    # and O is the EXIF orientation
+    def self.parse(string)
+      GeometryParser.new(string).make
     end
