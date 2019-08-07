604f4d1eb09027cf1e195989141b3a533e18c1fe
SUNsung <weatherius@gmail.com>
2019-08-07 15:01:30 +0300

    Rewrite [12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb] File in: 2019-08-07 15:01:29

--- ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
+++ ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
@@ -1,159 +1,129 @@
 
         
-                  find_instance_method_call(body_node, :build, :without?) do |method|
-            next unless unless_modifier?(method.parent)
-    
-              find_instance_method_call(body_node, 'Formula', :factory) do
-            problem '\'Formula.factory(name)\' is deprecated in favor of \'Formula[name]\''
-          end
-    
-      def add_mac_dependency?(args)
-    args.each { |key, version| args[key] = OS::Mac::Version.from_symbol(version) }
-    
-    describe Formula do
-  describe '#uses_from_macos' do
-    before do
-      allow(OS).to receive(:mac?).and_return(false)
-    end
-    
-      describe '#uses_from_macos' do
-    it 'allows specifying dependencies', :needs_linux do
-      subject.uses_from_macos('foo')
-    
-          raise CaskError, 'Cannot create metadata path with empty version.' if cask_version.empty?
-    
-        context 'when URL file is in middle' do
-      let(:url) { 'https://example.com/foo.tar.gz/from/this/mirror' }
-    
-      def install
-    start_time = Time.now
-    if !formula.bottle_unneeded? && !pour_bottle? && DevelopmentTools.installed?
-      Homebrew::Install.perform_build_from_source_checks
-    end
-    
-          before do
-        subject.perform
+            Rex::ThreadFactory.spawn('AESEncryption', false) {
+      c1 = OpenSSL::Cipher.new('aes-128-cfb8')
+      c1.encrypt
+      c1.key=key
+      sock.put([0].pack('N'))
+      sock.put(c1.iv=c1.random_iv)
+      buf1 = socks[0].read(4096)
+      while buf1 and buf1 != ''
+        sock.put(c1.update(buf1))
+        buf1 = socks[0].read(4096)
       end
+      sock.close()
+    }
     
-      def eligible_users
-    User.emailable
-        .where('current_sign_in_at < ?', (FREQUENCY + SIGN_IN_OFFSET).ago)
-        .where('last_emailed_at IS NULL OR last_emailed_at < ?', FREQUENCY.ago)
-  end
+        # Only try the same host/port combination once
+    phash = rhost + ':' + lport.to_s
+    return if self.listener_pairs[phash]
+    self.listener_pairs[phash] = true
+    
+        # Similarly, when we seperate large payloads into chunks to avoid the
+    # 998-byte problem mentioned above, we have to make sure that the first
+    # character of each chunk is an alpha character.  This simple algorithm
+    # will create a broken string in the case of 99 consecutive digits,
+    # slashes, and plusses in the base64 encoding, but the likelihood of
+    # that is low enough that I don't care.
+    i = 900;
+    while i < b64.length
+      while (b64[i].chr =~ %r{[0-9/+]})
+        i += 1
+      end
+      b64.insert(i,'.')
+      i += 900
+    end
+    
+    
+end
+end
 end
 
     
-      def set_poll
-    @poll = Poll.attached.find(params[:id])
-    authorize @poll.status, :show?
-  rescue Mastodon::NotPermittedError
-    raise ActiveRecord::RecordNotFound
-  end
     
-      describe 'GET #show' do
-    let(:poll) { Fabricate(:poll, status: Fabricate(:status, visibility: visibility)) }
+  #
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
+  }
     
-      def unread_message_count
-    ConversationVisibility.where(person_id: self.person_id).sum(:unread)
-  end
+    namespace :bower do
     
-        def recheck
-      pod = Pod.find(params[:pod_id])
-      pod.test_connection!
+      # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
     
-      def weekly_user_stats
-    @created_users_by_week = Hash.new{ |h,k| h[k] = [] }
-    @created_users = User.where('username IS NOT NULL and created_at IS NOT NULL')
-    @created_users.find_each do |u|
-      week = u.created_at.beginning_of_week.strftime('%Y-%m-%d')
-      @created_users_by_week[week] << u.username
-    end
-    
-          rescue_from OpenSSL::SSL::SSLError do |e|
-        validation_fail_as_json(e)
-      end
-    
-      def create
-    # Contacts autocomplete does not work the same way on mobile and desktop
-    # Mobile returns contact ids array while desktop returns person id
-    # This will have to be removed when mobile autocomplete is ported to Typeahead
-    recipients_param, column = [%i(contact_ids id), %i(person_ids person_id)].find {|param, _| params[param].present? }
-    if recipients_param
-      person_ids = current_user.contacts.mutual.where(column => params[recipients_param].split(',')).pluck(:person_id)
-    end
-    
-      def update
-    note = Notification.where(:recipient_id => current_user.id, :id => params[:id]).first
-    if note
-      note.set_read_state(params[:set_unread] != 'true' )
-    
-        def URIEncodePair(cc1, cc2, result, index)
-      u = ((cc1 >> 6) & 0xF) + 1;
-      w = (cc1 >> 2) & 0xF;
-      x = cc1 & 3;
-      y = (cc2 >> 6) & 0xF;
-      z = cc2 & 63;
-      octets = Array.new(4);
-      octets[0] = (u >> 2) + 240;
-      octets[1] = (((u & 3) << 4) | w) + 128;
-      octets[2] = ((x << 4) | y) + 128;
-      octets[3] = z + 128;
-      return URIEncodeOctets(octets, result, index);
-    end
-    
-          # Finds header node inside Nokogiri::HTML document.
-      #
-      def find_header_node(doc)
-        case @page.format
-          when :asciidoc
-            doc.css('div#gollum-root > h1:first-child')
-          when :org
-            doc.css('div#gollum-root > p.title:first-child')
-          when :pod
-            doc.css('div#gollum-root > a.dummyTopAnchor:first-child + h1')
-          when :rest
-            doc.css('div#gollum-root > div > div > h1:first-child')
-          else
-            doc.css('div#gollum-root > h1:first-child')
-        end
-      end
-    
-      s.add_development_dependency 'rack-test', '~> 0.6.2'
-  s.add_development_dependency 'shoulda', '~> 3.5.0'
-  s.add_development_dependency 'minitest-reporters', '~> 0.14.16'
-  s.add_development_dependency 'twitter_cldr', '~> 3.2.0'
-  s.add_development_dependency 'mocha', '~> 1.1.0'
-  s.add_development_dependency 'test-unit', '~> 3.1.0'
-  s.add_development_dependency 'webrick', '~> 1.3.1'
-    
-    # external
-require 'github/markup'
-require 'sanitize'
-    
-        it 'allows option overrides' do
-      q = Sidekiq::Queue.new('bar')
-      assert_equal 0, q.size
-      assert SetWorker.set(queue: :bar).perform_async(1)
-      job = q.first
-      assert_equal 'bar', job['queue']
-      assert_equal 12, job['retry']
-    end
-    
-        it 'schedules jobs' do
-      ss = Sidekiq::ScheduledSet.new
-      ss.clear
-    
-            # Specify a default error formatter.
-        def default_error_formatter(new_formatter_name = nil)
-          if new_formatter_name
-            new_formatter = Grape::ErrorFormatter.formatter_for(new_formatter_name, {})
-            namespace_inheritable(:default_error_formatter, new_formatter)
-          else
-            namespace_inheritable(:default_error_formatter)
+              begin
+            lineno = frame.lineno-1
+            lines = ::File.readlines(frame.filename)
+            frame.pre_context_lineno = [lineno-CONTEXT, 0].max
+            frame.pre_context = lines[frame.pre_context_lineno...lineno]
+            frame.context_line = lines[lineno].chomp
+            frame.post_context_lineno = [lineno+CONTEXT, lines.size].min
+            frame.post_context = lines[lineno+1..frame.post_context_lineno]
+          rescue
           end
-        end
     
-            unless presenter || env[Grape::Env::GRAPE_ROUTING_ARGS].nil?
-          # env['api.endpoint'].route does not work when the error occurs within a middleware
-          # the Endpoint does not have a valid env at this moment
-          http_codes = env[Grape::Env::GRAPE_ROUTING_ARGS][:route_info].http_codes || []
+            safe?(env) ||
+          valid_token?(session, env['HTTP_X_CSRF_TOKEN']) ||
+          valid_token?(session, Request.new(env).params[options[:authenticity_param]]) ||
+          ( options[:allow_if] && options[:allow_if].call(env) )
+      end
+    
+            modes       = Array options[:escape]
+        @escaper    = options[:escaper]
+        @html       = modes.include? :html
+        @javascript = modes.include? :javascript
+        @url        = modes.include? :url
+    
+              react_and_close(env, body) or [status, headers, body]
+        else
+          [status, headers, body]
+        end
+      end
+    
+      it 'accepts post form requests with masked authenticity_token field' do
+    post('/', {'authenticity_token' => masked_token}, 'rack.session' => session)
+    expect(last_response).to be_ok
+  end
+    
+        headers = get('/', {}, 'wants' => 'text/html').headers
+    expect(headers['Content-Security-Policy']).to eq('connect-src https://api.mybank.com; default-src none; font-src https://cdn.mybank.net; frame-src self; img-src https://cdn.mybank.net; media-src https://cdn.mybank.net; object-src https://cdn.mybank.net; report-uri /my_amazing_csp_report_parser; sandbox allow-scripts; script-src https://cdn.mybank.net; style-src https://cdn.mybank.net')
+    expect(headers['Content-Security-Policy-Report-Only']).to be_nil
+  end
+    
+          moo = MiniTest::Mock.new
+      moo.expect(:multi, [0, 1])
+      beef = ConnectionPool.new(size: 1) { moo }
+    
+    describe 'DeadSet' do
+  def dead_set
+    Sidekiq::DeadSet.new
+  end
+    
+      it 'allows until delayed scheduling of AR class methods' do
+    ss = Sidekiq::ScheduledSet.new
+    assert_equal 0, ss.size
+    MyModel.delay_until(1.day.from_now).long_class_method
+    assert_equal 1, ss.size
+  end
+    
+        DirectWorker.perform_one
+    assert_equal 1, DirectWorker.jobs.size
