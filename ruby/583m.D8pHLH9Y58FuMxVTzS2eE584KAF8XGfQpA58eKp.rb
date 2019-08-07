25a5d1ee4905b3d83405765233691f96d514fd01
SUNsung <weatherius@gmail.com>
2019-08-07 21:46:06 +0300

    Rewrite [583m.D8pHLH9Y58FuMxVTzS2eE584KAF8XGfQpA58eKp.rb] File in: 2019-08-07 21:46:06

--- ruby/583m.D8pHLH9Y58FuMxVTzS2eE584KAF8XGfQpA58eKp.rb
+++ ruby/583m.D8pHLH9Y58FuMxVTzS2eE584KAF8XGfQpA58eKp.rb
@@ -1,88 +1,63 @@
-# -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
-    
-    Jekyll::PluginManager.require_from_bundler
-    
-          def print_feature_element_name(feature_element)
-        @io.print '\n#{feature_element.location}  Scenario: #{feature_element.name} '
-        @io.flush
-      end
-    
-            def connect(websocket, handshake)
-          @connections_count += 1
-          if @connections_count == 1
-            message = 'Browser connected'
-            message += ' over SSL/TLS' if handshake.secure?
-            Jekyll.logger.info 'LiveReload:', message
-          end
-          websocket.send(
-            JSON.dump(
-              :command    => 'hello',
-              :protocols  => ['http://livereload.com/protocols/official-7'],
-              :serverName => 'jekyll'
-            )
-          )
-    
-          def unlocked_pods
-        @unlocked_pods ||= begin
-          pods = []
-          UI.titled_section('Analyzing dependencies') do
-            pods = Installer::Analyzer.new(config.sandbox, config.podfile).
-              analyze(:outdated).
-              specs_by_target.values.flatten.uniq
-          end
-          pods
-        end
-      end
-    
-      module Generator
-    autoload :Acknowledgements,        'cocoapods/generator/acknowledgements'
-    autoload :Markdown,                'cocoapods/generator/acknowledgements/markdown'
-    autoload :Plist,                   'cocoapods/generator/acknowledgements/plist'
-    autoload :BridgeSupport,           'cocoapods/generator/bridge_support'
-    autoload :Constant,                'cocoapods/generator/constant'
-    autoload :CopyResourcesScript,     'cocoapods/generator/copy_resources_script'
-    autoload :DummySource,             'cocoapods/generator/dummy_source'
-    autoload :EmbedFrameworksScript,   'cocoapods/generator/embed_frameworks_script'
-    autoload :FileList,                'cocoapods/generator/file_list'
-    autoload :Header,                  'cocoapods/generator/header'
-    autoload :InfoPlistFile,           'cocoapods/generator/info_plist_file'
-    autoload :ModuleMap,               'cocoapods/generator/module_map'
-    autoload :PrefixHeader,            'cocoapods/generator/prefix_header'
-    autoload :UmbrellaHeader,          'cocoapods/generator/umbrella_header'
-    autoload :AppTargetHelper,         'cocoapods/generator/app_target_helper'
-  end
-    
-            # @return [Hash{String=>Symbol}] A hash representing all the user build
-        #         configurations across all integration targets. Each key
-        #         corresponds to the name of a configuration and its value to
-        #         its type (`:debug` or `:release`).
-        #
-        attr_reader :build_configurations
-    
-            if podspecs.size <= 1
-          result += [license, readme, podspecs, docs]
-        else
-          # Manually add non-globbing files since there are multiple podspecs in the same folder
-          result << podspec_file
-          if license_file = spec_license
-            absolute_path = root + license_file
-            result << absolute_path if File.exist?(absolute_path)
-          end
-        end
-        result.compact.flatten.sort
-      end
-    
-          def get_installer_for(plugin_name)
-        uri = pack_uri(plugin_name)
-    
-          context 'when the plugin doesnt exist' do
-        it 'fails to install and report an error' do
-          command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-output-impossible-plugin')
-          expect(command.stderr).to match(/Plugin not found, aborting/)
-        end
-      end
+
+        
+        version = ['', 'ext/etc/'].find do |dir|
+  begin
+    break File.open(File.expand_path('../#{dir}/etc.c', __FILE__)) do |f|
+      f.gets '\n#define RUBY_ETC_VERSION '
+      f.gets[/'(.+)'/, 1]
     end
+  rescue
+    next
   end
 end
+    
+        [[1,2,3,4], [81, 2, 118, 3146]].each { |w,x,y,z|
+      obj = (x.to_f + y.to_f / z.to_f) * Math.exp(w.to_f / (x.to_f + y.to_f / z.to_f))
+      assert_equal obj, Marshal.load(Marshal.dump(obj))
+    }
+    
+      def self.set_screen_size(rows, columns)
+    raise NotImplementedError
+  end
+    
+      def test_error_handling
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.acos('2') }
+    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.log('2') }
+    assert_raise(ArgumentError) { CMath.log(2, '2') }
+    assert_raise(NoMethodError) { CMath.log(2, 2i) }
+    assert_raise(RangeError) { CMath.hypot(2i, 2i) }
+  end
+    
+    describe 'GzipReader#getc' do
+    
+        false
+  end
+    
+    if rails_env != 'development'
+  config('path vendor/bundle')
+  config('frozen true')
+  config('disable_shared_gems true')
+end
+    
+          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
+      def create
+        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
+        client = Api::OpenidConnect::OAuthApplication.register! registrar
+        render json: client.as_json(root: false)
+      end
+    
+      def index
+    comments = comment_service.find_for_post(params[:post_id])
+    respond_with do |format|
+      format.json { render json: CommentPresenter.as_collection(comments), status: 200 }
+      format.mobile { render layout: false, locals: {comments: comments} }
+    end
+  end
+    
+      def set_up_contacts
+    if params[:a_id].present?
+      @aspect = current_user.aspects.find(params[:a_id])
+      gon.preloads[:aspect] = AspectPresenter.new(@aspect).as_json
+    end
+    @contacts_size = current_user.contacts.size
+  end
