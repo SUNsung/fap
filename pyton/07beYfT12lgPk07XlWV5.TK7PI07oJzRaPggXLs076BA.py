e2c8077491fc71fe03cac2126a18dfb96fd974fe
SUNsung <weatherius@gmail.com>
2019-08-07 09:40:35 +0300

    Rewrite [07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py] File in: 2019-08-07 09:40:35

--- pyton/07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py
+++ pyton/07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py
@@ -1,166 +1,149 @@
 
         
-            def setUp(self):
-        self.crawler = get_crawler(Spider, self.settings_dict)
-        self.spider = self.crawler._create_spider('foo')
-        self.mwman = DownloaderMiddlewareManager.from_crawler(self.crawler)
-        # some mw depends on stats collector
-        self.crawler.stats.open_spider(self.spider)
-        return self.mwman.open_spider(self.spider)
+        sys.path.insert(0, dirn(dirn((os.path.abspath(__file__)))))
     
-        @classmethod
-    def _get_mwlist_from_settings(cls, settings):
-        return build_component_list(
-            settings.getwithbase('DOWNLOADER_MIDDLEWARES'))
+        def test_youtube(self):
+        self._assert_restricted('07FYdnEawAQ', '07FYdnEawAQ.mp4', 10)
     
-        def __init__(self, tag='a', attr='href', unique=False, process_value=None,
-                 strip=True, canonicalized=False):
-        warnings.warn(
-            'BaseSgmlLinkExtractor is deprecated and will be removed in future releases. '
-            'Please use scrapy.linkextractors.LinkExtractor',
-            ScrapyDeprecationWarning, stacklevel=2,
-        )
-        SGMLParser.__init__(self)
-        self.scan_tag = tag if callable(tag) else lambda t: t == tag
-        self.scan_attr = attr if callable(attr) else lambda a: a == attr
-        self.process_value = (lambda v: v) if process_value is None else process_value
-        self.current_link = None
-        self.unique = unique
-        self.strip = strip
-        if canonicalized:
-            self.link_key = lambda link: link.url
-        else:
-            self.link_key = lambda link: canonicalize_url(link.url,
-                                                          keep_fragments=True)
+    VERSION = '3.29'
     
-        def test_proxy_auth_empty_passwd(self):
-        os.environ['http_proxy'] = 'https://user:@proxy:3128'
-        mw = HttpProxyMiddleware()
-        req = Request('http://scrapytest.org')
-        assert mw.process_request(req, spider) is None
-        self.assertEqual(req.meta, {'proxy': 'https://proxy:3128'})
-        self.assertEqual(req.headers.get('Proxy-Authorization'), b'Basic dXNlcjo=')
-        # proxy from request.meta
-        req = Request('http://scrapytest.org', meta={'proxy': 'https://username:@proxy:3128'})
-        assert mw.process_request(req, spider) is None
-        self.assertEqual(req.meta, {'proxy': 'https://proxy:3128'})
-        self.assertEqual(req.headers.get('Proxy-Authorization'), b'Basic dXNlcm5hbWU6')
+        @memoize
+    def get_aliases(self):
+        proc = Popen(['tcsh', '-ic', 'alias'], stdout=PIPE, stderr=DEVNULL)
+        return dict(
+            self._parse_alias(alias)
+            for alias in proc.stdout.read().decode('utf-8').split('\n')
+            if alias and '\t' in alias)
     
     
-def configure_logging(settings=None, install_root_handler=True):
-    '''
-    Initialize logging defaults for Scrapy.
-    
-        @defer.inlineCallbacks
-    def test_redirect_follow_302(self):
-        _, out, _ = yield self.execute([self.url('/redirect-no-meta-refresh'), '-c', 'response.status'])
-        assert out.strip().endswith(b'200')
-    
-    '''
-    
-        fc_dim = cfg.FAST_RCNN.MLP_HEAD_DIM
-    model.FC(current, 'fc6', dim_in * roi_size * roi_size, fc_dim)
-    model.Relu('fc6', 'fc6')
-    return 'fc6', fc_dim
+@pytest.mark.usefixtures('no_memoize')
+@pytest.mark.parametrize('script, result', [
+    ('vom file.py', ['vim file.py']),
+    ('fucck', ['fsck']),
+    ('got commit', ['git commit', 'go commit'])])
+def test_get_new_command(script, result):
+    assert get_new_command(Command(script, '')) == result
 
     
     
-def _whctrs(anchor):
-    '''Return width, height, x center, and y center for an anchor (window).'''
-    w = anchor[2] - anchor[0] + 1
-    h = anchor[3] - anchor[1] + 1
-    x_ctr = anchor[0] + 0.5 * (w - 1)
-    y_ctr = anchor[1] + 0.5 * (h - 1)
-    return w, h, x_ctr, y_ctr
+def _get_used_executables(command):
+    for script in get_valid_history_without_current(command):
+        yield script.split(' ')[0]
     
     
-def _build_forward_graph(model, single_gpu_build_func):
-    '''Construct the forward graph on each GPU.'''
-    all_loss_gradients = {}  # Will include loss gradients from all GPUs
-    # Build the model on each GPU with correct name and device scoping
-    for gpu_id in range(cfg.NUM_GPUS):
-        with c2_utils.NamedCudaScope(gpu_id):
-            all_loss_gradients.update(single_gpu_build_func(model))
-    return all_loss_gradients
+@eager
+def _parse_apt_operations(help_text_lines):
+    is_commands_list = False
+    for line in help_text_lines:
+        line = line.decode().strip()
+        if is_commands_list and line:
+            yield line.split()[0]
+        elif line.startswith('Basic commands:') \
+                or line.startswith('Most used commands:'):
+            is_commands_list = True
+    
+    # Run training
+model.compile(optimizer='adam', loss='categorical_crossentropy')
+model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_split=0.2)
+# Save model
+model.save('cnn_s2s.h5')
+    
+        num_train, num_test = x_train.shape[0], x_test.shape[0]
+    
+    print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
     
     
-def add_single_scale_rpn_outputs(model, blob_in, dim_in, spatial_scale):
-    '''Add RPN outputs to a single scale model (i.e., no FPN).'''
-    anchors = generate_anchors(
-        stride=1. / spatial_scale,
-        sizes=cfg.RPN.SIZES,
-        aspect_ratios=cfg.RPN.ASPECT_RATIOS
-    )
-    num_anchors = anchors.shape[0]
-    dim_out = dim_in
-    # RPN hidden representation
-    model.Conv(
-        blob_in,
-        'conv_rpn',
-        dim_in,
-        dim_out,
-        kernel=3,
-        pad=1,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
-    model.Relu('conv_rpn', 'conv_rpn')
-    # Proposal classification scores
-    model.Conv(
-        'conv_rpn',
-        'rpn_cls_logits',
-        dim_in,
-        num_anchors,
-        kernel=1,
-        pad=0,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
-    # Proposal bbox regression deltas
-    model.Conv(
-        'conv_rpn',
-        'rpn_bbox_pred',
-        dim_in,
-        4 * num_anchors,
-        kernel=1,
-        pad=0,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
+    {- __return_sequences__: Boolean. Whether to return the last output
+    in the output sequence, or the full sequence.
+- __return_state__: Boolean. Whether to return the last state
+    in addition to the output.
+'''}
     
-        # Create new roi blobs for each FPN level
-    # (See: modeling.FPN.add_multilevel_roi_blobs which is similar but annoying
-    # to generalize to support this particular case.)
-    rois_idx_order = np.empty((0, ))
-    for output_idx, lvl in enumerate(range(lvl_min, lvl_max + 1)):
-        idx_lvl = np.where(lvls == lvl)[0]
-        blob_roi_level = rois[idx_lvl, :]
-        outputs[output_idx + 1].reshape(blob_roi_level.shape)
-        outputs[output_idx + 1].data[...] = blob_roi_level
-        rois_idx_order = np.concatenate((rois_idx_order, idx_lvl))
-    rois_idx_restore = np.argsort(rois_idx_order)
-    blob_utils.py_op_copy_blob(rois_idx_restore.astype(np.int32), outputs[-1])
-
+        with tempfile.NamedTemporaryFile('wb', delete=True) as f:
+        np.savez(f, x=x, y=y)
+        monkeypatch.setattr(reuters, 'get_file', lambda *args, **kwargs: f.name)
+        yield f.name
     
-        _distribute_rois_over_fpn_levels('rois')
-    if cfg.MODEL.MASK_ON:
-        _distribute_rois_over_fpn_levels('mask_rois')
-    if cfg.MODEL.KEYPOINTS_ON:
-        _distribute_rois_over_fpn_levels('keypoint_rois')
-
+    if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(
+        x_train,
+        y_train,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(x_test, y_test),
+        shuffle=True)
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and real-time data augmentation:
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by dataset std
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        zca_epsilon=1e-06,  # epsilon for ZCA whitening
+        rotation_range=0,  # randomly rotate images in 0 to 180 degrees
+        width_shift_range=0.1,  # randomly shift images horizontally
+        height_shift_range=0.1,  # randomly shift images vertically
+        shear_range=0.,  # set range for random shear
+        zoom_range=0.,  # set range for random zoom
+        channel_shift_range=0.,  # set range for random channel shifts
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
+        cval=0.,  # value used for fill_mode = 'constant'
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either 'channels_first' or 'channels_last'
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.0)
     
-        # Scale rois_fg and format as (batch_idx, x1, y1, x2, y2)
-    rois_fg *= im_scale
-    repeated_batch_idx = batch_idx * blob_utils.ones((rois_fg.shape[0], 1))
-    rois_fg = np.hstack((repeated_batch_idx, rois_fg))
     
-        for filename in filenames:
-        fd = codecs.open(filename, mode='r', encoding='utf-8')
-        for line in fd.readlines():
-            refs = re.findall(r'(?<=<a href=')[^']*', markdown.markdown(line))
-            for ref in refs:
-                if ref not in urls:
-                    urls.append(ref)
+def categorical_accuracy(y_true, y_pred):
+    return K.cast(K.equal(K.argmax(y_true, axis=-1),
+                          K.argmax(y_pred, axis=-1)),
+                  K.floatx())
+    
+            self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.embeddings_initializer = initializers.get(embeddings_initializer)
+        self.embeddings_regularizer = regularizers.get(embeddings_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.embeddings_constraint = constraints.get(embeddings_constraint)
+        self.mask_zero = mask_zero
+        self.supports_masking = mask_zero
+        self.input_length = input_length
+    
+    # Add any paths that contain custom themes here, relative to this directory.
+#html_theme_path = []
+    
+    with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+    
+        # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_frame = frame_process[:, :, ::-1]
+    
+    from certbot_apache.display_ops import select_vhost_multiple
+from certbot_apache.tests import util
+    
+       certbot certonly \\
+     --dns-rfc2136 \\
+     --dns-rfc2136-credentials ~/.secrets/certbot/rfc2136.ini \\
+     --dns-rfc2136-propagation-seconds 30 \\
+     -d example.com
