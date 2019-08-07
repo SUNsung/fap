5432eceda5f011714cb42dc6916a4a9992e21fb8
SUNsung <weatherius@gmail.com>
2019-08-07 16:54:47 +0300

    Rewrite [28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp] File in: 2019-08-07 16:54:47

--- cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
+++ cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
@@ -1,190 +1,93 @@
 
         
-        
-    {  DISALLOW_COPY_AND_ASSIGN(GlobalMenuBarRegistrarX11);
-};
-    
-    
-    { private:
-  v8::Local<v8::Promise::Resolver> resolver_;
-};
-    
-    namespace mate {
-    }
-    
-      // Creates an activity and sets it as the one currently in use.
-  void SetUserActivity(const std::string& type,
-                       const base::DictionaryValue& user_info,
-                       mate::Arguments* args);
-    
-    // static
-mate::Handle<DownloadItem> DownloadItem::Create(v8::Isolate* isolate,
-                                                download::DownloadItem* item) {
-  auto* existing = TrackableObject::FromWrappedClass(isolate, item);
-  if (existing)
-    return mate::CreateHandle(isolate, static_cast<DownloadItem*>(existing));
-    }
-    
-    	//Find node with minimum y value (max x if equal)
-	float32 minY = 1e10;
-	float32 maxX = -1e10;
-	int32 minYIndex = -1;
-	for (int32 i = 0; i < nNodes; ++i) {
-		if (nodes[i].position.y < minY && nodes[i].nConnected > 1) {
-			minY = nodes[i].position.y;
-			minYIndex = i;
-			maxX = nodes[i].position.x;
-		} else if (nodes[i].position.y == minY && nodes[i].position.x > maxX && nodes[i].nConnected > 1) {
-			minYIndex = i;
-			maxX = nodes[i].position.x;
-		}
-	}
-    
-    bool b2Triangle::IsInside(float32 _x, float32 _y){
-	if (_x < x[0] && _x < x[1] && _x < x[2]) return false;
-	if (_x > x[0] && _x > x[1] && _x > x[2]) return false;
-	if (_y < y[0] && _y < y[1] && _y < y[2]) return false;
-	if (_y > y[0] && _y > y[1] && _y > y[2]) return false;
-		
-		float32 vx2 = _x-x[0]; float32 vy2 = _y-y[0];
-		float32 vx1 = x[1]-x[0]; float32 vy1 = y[1]-y[0];
-		float32 vx0 = x[2]-x[0]; float32 vy0 = y[2]-y[0];
-		
-		float32 dot00 = vx0*vx0+vy0*vy0;
-		float32 dot01 = vx0*vx1+vy0*vy1;
-		float32 dot02 = vx0*vx2+vy0*vy2;
-		float32 dot11 = vx1*vx1+vy1*vy1;
-		float32 dot12 = vx1*vx2+vy1*vy2;
-		float32 invDenom = 1.0f / (dot00*dot11 - dot01*dot01);
-		float32 u = (dot11*dot02 - dot01*dot12)*invDenom;
-		float32 v = (dot00*dot12 - dot01*dot02)*invDenom;
-		
-		return ((u>=0)&&(v>=0)&&(u+v<=1));    
+        // static
+void CallbackHolderBase::FirstWeakCallback(
+    const v8::WeakCallbackInfo<CallbackHolderBase>& data) {
+  data.GetParameter()->v8_ref_.Reset();
+  data.SetSecondPassCallback(SecondWeakCallback);
 }
     
-    		virtual bool HasSeverelyBentDifferentialColors(void) const = 0;
-    
-    		float fLeftGrayErrorSum = 0.0f;
-		float fRightGrayErrorSum = 0.0f;
-		float fTopGrayErrorSum = 0.0f;
-		float fBottomGrayErrorSum = 0.0f;
-    
-    
-#if !defined(FFT_ARM_H)
-#define FFT_ARM_H
-    
-    #undef silk_CLZ32
-static OPUS_INLINE opus_int32 silk_CLZ32_armv5(opus_int32 in32)
-{
-  int res;
-  __asm__(
-      '#silk_CLZ32\n\t'
-      'clz %0, %1\n\t'
-      : '=r'(res)
-      : 'r'(in32)
-  );
-  return res;
+    Dictionary Dictionary::CreateEmpty(v8::Isolate* isolate) {
+  return Dictionary(isolate, v8::Object::New(isolate));
 }
-#define silk_CLZ32(in32) (silk_CLZ32_armv5(in32))
     
-    /*!
-+ * \brief The result holder of dispatch mode of each Node in the graph.
-+ * \note Stored under graph.attrs['dispatch_mode'], provided by Pass 'InferStorageType'
-+ *
-+ * \code
-+ *  Graph g = ApplyPass(src_graph, 'InferStorageType');
-+ *  const DispatchModeVector& dispatch_modes = g.GetAttr<DispatchModeVector>('dispatch_mode');
-+ *  // get dispatch mode by entry node id
-+ *  int node_type = dispatch_modes[nid];
-+ * \endcode
-+ *
-+ * \sa FInferStorageType
-+ */
-using DispatchModeVector = std::vector<DispatchMode>;
     
-    /*!
- *  Copyright (c) 2015 by Contributors
- * \file image_augmenter.h
- * \brief Interface of opencv based image augmenter
- */
-#ifndef MXNET_IO_IMAGE_AUGMENTER_H_
-#define MXNET_IO_IMAGE_AUGMENTER_H_
+    {}  // namespace mate
+
     
-    /*!
- * \brief Iterator that normalize a image.
- *  It also applies a few augmention before normalization.
- */
-class ImageNormalizeIter : public IIterator<DataInst> {
+    // This function is a convenient way to create a handle from a raw pointer
+// without having to write out the type of the object explicitly.
+template <typename T>
+mate::Handle<T> CreateHandle(v8::Isolate* isolate, T* object) {
+  return mate::Handle<T>(object->GetWrapper(), object);
+}
+    
+    // Like Dictionary, but stores object in persistent handle so you can keep it
+// safely on heap.
+class PersistentDictionary : public Dictionary {
  public:
-  explicit ImageNormalizeIter(IIterator<DataInst> *base)
-      : base_(base), meanfile_ready_(false) {
+  PersistentDictionary();
+  PersistentDictionary(v8::Isolate* isolate, v8::Local<v8::Object> object);
+  PersistentDictionary(const PersistentDictionary& other);
+  ~PersistentDictionary() override;
+    }
+    
+      // Temporary directory to hold the socket.
+  base::ScopedTempDir socket_dir_;
+    
+      std::set<std::unique_ptr<SocketReader>> readers_;
+    
+    using content::BrowserThread;
+    
+      // If we don't have any window then quitAndInstall immediately.
+  if (WindowList::IsEmpty()) {
+    auto_updater::AutoUpdater::QuitAndInstall();
+    return;
+  }
+    
+    // Constants for packing an encoded CheckedCastKind and
+// CastConsumptionKind together.
+enum {
+  // Must be large enough to store all the CheckedCastKindEncodings
+  SIL_CAST_CONSUMPTION_BIT_OFFSET = 4,
+  SIL_CHECKED_CAST_MASK =
+    (1 << SIL_CAST_CONSUMPTION_BIT_OFFSET) - 1
+};
+    
+    KNOWN_STDLIB_TYPE_DECL(Encoder, ProtocolDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(Decoder, ProtocolDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(KeyedEncodingContainer, NominalTypeDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(KeyedDecodingContainer, NominalTypeDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(RangeReplaceableCollection, ProtocolDecl, 1)
+    
+    #if defined(_WIN32)
+template <>
+const char *
+_swift_stdlib_strtoX_clocale_impl<float>(const char *str, float *result) {
+  if (swift_stringIsSignalingNaN(str)) {
+    *result = std::numeric_limits<float>::signaling_NaN();
+    return str + std::strlen(str);
   }
     }
     
-      // Initialise the Hadamard product to eye(k)
-  // where k is the number of 'factors'
-  int k = out.size(0);
-  Tensor<cpu, 2, DType> hadamard_prod(Shape2(k, k));
-  AllocSpace(&hadamard_prod);
-  hadamard_prod = 1;
-    
-        ImGuiIO& io = ImGui::GetIO();
-    
-    void    ImGui_ImplWin32_NewFrame()
-{
-    ImGuiIO& io = ImGui::GetIO();
-    IM_ASSERT(io.Fonts->IsBuilt() && 'Font atlas not built! It is generally built by the renderer back-end. Missing call to renderer _NewFrame() function? e.g. ImGui_ImplOpenGL3_NewFrame().');
+    /// An ASTVisitor for generating SIL from top-level declarations in a module.
+class LLVM_LIBRARY_VISIBILITY SILGenModule : public ASTVisitor<SILGenModule> {
+public:
+  /// The Module being constructed.
+  SILModule &M;
+  
+  /// The type converter for the module.
+  TypeConverter &Types;
+  
+  /// The Swift module we are visiting.
+  ModuleDecl *SwiftModule;
+  
+  /// TopLevelSGF - The SILGenFunction used to visit top-level code, or null if
+  /// the current source file is not a script source file.
+  SILGenFunction /*nullable*/ *TopLevelSGF;
     }
     
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_IMPL_API void     ImGui_Marmalade_InvalidateDeviceObjects();
-IMGUI_IMPL_API bool     ImGui_Marmalade_CreateDeviceObjects();
-    
-    //---- Using 32-bits vertex indices (default is 16-bits) is one way to allow large meshes with more than 64K vertices. 
-// Your renderer back-end will need to support it (most example renderer back-ends support both 16/32-bits indices).
-// Another way to allow large meshes while keeping 16-bits indices is to handle ImDrawCmd::VtxOffset in your renderer. 
-// Read about ImGuiBackendFlags_RendererHasVtxOffset for details.
-//#define ImDrawIdx unsigned int
-    
-            // Child 1: no border, enable horizontal scrollbar
-        {
-            ImGuiWindowFlags window_flags = ImGuiWindowFlags_HorizontalScrollbar | (disable_mouse_wheel ? ImGuiWindowFlags_NoScrollWithMouse : 0);
-            ImGui::BeginChild('Child1', ImVec2(ImGui::GetWindowContentRegionWidth() * 0.5f, 260), false, window_flags);
-            for (int i = 0; i < 100; i++)
-            {
-                ImGui::Text('%04d: scrollable region', i);
-                if (goto_line && line == i)
-                    ImGui::SetScrollHereY();
-            }
-            if (goto_line && line >= 100)
-                ImGui::SetScrollHereY();
-            ImGui::EndChild();
-        }
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_Init(const char* glsl_version = NULL);
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-            // Rendering
-        ImGui::EndFrame();
-        g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, false);
-        g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, false);
-        g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, false);
-        D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x*255.0f), (int)(clear_color.y*255.0f), (int)(clear_color.z*255.0f), (int)(clear_color.w*255.0f));
-        g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
-        if (g_pd3dDevice->BeginScene() >= 0)
-        {
-            ImGui::Render();
-            ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
-            g_pd3dDevice->EndScene();
-        }
-        HRESULT result = g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+    static bool needToRelocate(SectionRef S) {
+  if (!getSectionAddress(S))
+    return false;
+    }
