fec8fa6c569800755a4677b8a6e8c9e2bb69e747
SUNsung <weatherius@gmail.com>
2019-08-08 22:47:03 +0300

    Rewrite [19i.UaJqomSPQ19ezVp0NaAnC219iZbxh1x1DjM19gRq.rb] File in: 2019-08-08 22:47:03

--- ruby/19i.UaJqomSPQ19ezVp0NaAnC219iZbxh1x1DjM19gRq.rb
+++ ruby/19i.UaJqomSPQ19ezVp0NaAnC219iZbxh1x1DjM19gRq.rb
@@ -1,49 +1,149 @@
 
         
-          # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-    
-      factory :noroot_project, class: Tmuxinator::Project do
-    transient do
-      file { yaml_load('spec/fixtures/noroot.yml') }
-    end
-    
-            expect(project.startup_pane).to eq('sample:0.4')
+        module Frameit
+  class Runner
+    def initialize
+      downloader = FrameDownloader.new
+      unless downloader.frames_exist?
+        downloader.download_frames
       end
     end
+    
+          #####################################################
+      # @!group Documentation
+      #####################################################
+    
+            config.vm.provider 'foo' do |_, c|
+          c.ssh.port = 100
+        end
+      end
+    
+              with_target_vms(argv, provider: provider) do |machine|
+            if !machine.config.vm.box
+              machine.ui.output(I18n.t(
+                'vagrant.errors.box_update_no_name'))
+              next
+            end
+    
+                it 'passes force through to action_box_add as true' do
+              expect(box).to receive(:has_update?).
+                with(machine.config.vm.box_version,
+                     {download_options:
+                       {ca_cert: 'foo', ca_path: 'bar', client_cert: 'baz',
+                        insecure: true}}).
+                and_return([md, md.version('1.1'),
+                            md.version('1.1').provider('virtualbox')])
+    
+      def set_poll
+    @poll = Poll.attached.find(params[:id])
+    authorize @poll.status, :show?
+  rescue Mastodon::NotPermittedError
+    raise ActiveRecord::RecordNotFound
   end
     
-            options = {
-          args: project_options[:args],
-          custom_name: project_options[:custom_name],
-          force_attach: attach,
-          force_detach: detach,
-          name: project_options[:name],
-          project_config: project_options[:project_config]
-        }
+      def reject_follow!(follow)
+    follow.destroy
     
-      before { instance.extend Tmuxinator::WemuxSupport }
+      def forward_to_origin!
+    ActivityPub::DeliveryWorker.perform_async(
+      payload,
+      some_local_account.id,
+      @target_account.inbox_url
+    )
+  end
     
-      expansion(:inspection) {
-    {
-      :inspected => o.inspected == 1 ? true : false,
-      :spam => o.spam == 1 ? true : false,
-      :spam_score => o.spam_score.to_f,
-      :threat => o.threat == 1 ? true : false,
-      :threat_details => o.threat_details
-    }
-  }
+      describe '#call' do
+    context 'when actor is the sender'
+    context 'when actor differs from sender' do
+      let(:forwarder) { Fabricate(:account, domain: 'example.com', uri: 'http://example.com/other_account') }
     
-      before_action { @server = organization.servers.present.find_by_permalink!(params[:server_id]) }
-  before_action { params[:id] && @address_endpoint = @server.address_endpoints.find_by_uuid!(params[:id]) }
+              if node.blockarg_type?
+            correct_for_blockarg_type(node)
+          else
+            lambda do |corrector|
+              variable_name = if node.optarg_type?
+                                node.node_parts[0]
+                              else
+                                # Extract only a var name without splat (`*`)
+                                node.source.gsub(/\A\*+/, '')
+                              end
+              corrector.replace(node.loc.name, '_#{variable_name}')
+            end
+          end
+        end
     
-      def create
-    @smtp_endpoint = @server.smtp_endpoints.build(safe_params)
-    if @smtp_endpoint.save
-      flash[:notice] = params[:return_notice] if params[:return_notice].present?
-      redirect_to_with_json [:return_to, [organization, @server, :smtp_endpoints]]
-    else
-      render_form_errors 'new', @smtp_endpoint
+          let(:expected_source) { <<-RUBY }
+        def some_method(foo, bar)
+          foo + bar
+        end
+      RUBY
+    
+        it 'still points out redundant uses within the block' do
+      expect_offense(<<~RUBY)
+        class SomeClass
+          concerning :FirstThing do
+            def foo
+            end
+            private
+    
+            def heredoc_line(node, heredoc_node)
+          heredoc_body = heredoc_node.loc.heredoc_body
+          num_of_heredoc_lines =
+            heredoc_body.last_line - heredoc_body.first_line
+    
+      it 'registers an offense for `raise` guard clause not followed by ' \
+     'empty line when guard clause is after method call with argument' do
+    expect_offense(<<~'RUBY')
+      def foo
+        raise SerializationError.new('Unsupported argument type: #{argument.class.name}') unless serializer
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Add empty line after guard clause.
+        serializer.serialize(argument)
+      end
+    RUBY
+  end
+    
+        it 'returns nil if an except is provided' do
+      valid_article = create(:article, tags: 'explainlikeimfive')
+      expect(described_class.new(valid_article, 'explainlikeimfive').tag).to eq(nil)
     end
+    
+          it 'flashes a message containing the token' do
+        post '/users/api_secrets', params: { api_secret: valid_params }
+        expect(flash[:notice]).to include(ApiSecret.last.secret)
+        expect(flash[:error]).to be_nil
+      end
+    end
+    
+      def permitted_attributes
+    %i[title body_html body_markdown main_image published canonical_url
+       description allow_small_edits allow_big_edits tag_list publish_under_org
+       video video_code video_source_url video_thumbnail_url receive_notifications
+       archived]
   end
+    
+          assert_raises ArgumentError do
+        Sidekiq::Client.push('queue' => 'foo', 'class' => 42, 'args' => [1, 2])
+      end
+    
+    describe Sidekiq::Extensions do
+  before do
+    Sidekiq.redis {|c| c.flushdb }
+  end
+    
+      def new_manager(opts)
+    Sidekiq::Manager.new(opts)
+  end
+    
+          msg['locale'] = 'jp'
+      I18n.locale = I18n.default_locale
+      assert_equal :en, I18n.locale
+      mw = Sidekiq::Middleware::I18n::Server.new
+      mw.call(nil, msg, nil) do
+        assert_equal :jp, I18n.locale
+      end
+      assert_equal :en, I18n.locale
+    end
+    
+          QueueWorker.perform_async(1, 2)
+      QueueWorker.perform_async(1, 2)
+      AltQueueWorker.perform_async(1, 2)
