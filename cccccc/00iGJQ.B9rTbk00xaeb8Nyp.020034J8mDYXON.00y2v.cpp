8bba417028d74c6492d3054c8c44f1c943f12ffe
SUNsung <weatherius@gmail.com>
2019-08-08 05:37:53 +0300

    Rewrite [00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp] File in: 2019-08-08 05:37:52

--- cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
+++ cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
@@ -1,238 +1,281 @@
 
         
-            int m_nTempCom;                          /* Holding place for the last command.          */
-    int m_openParenCount;                    // Number of open parentheses.
-    std::array<int, MAXPRECDEPTH> m_nOp;     /* Holding array for parenthesis operations.    */
-    std::array<int, MAXPRECDEPTH> m_nPrecOp; /* Holding array for precedence  operations.    */
-    size_t m_precedenceOpCount;              /* Current number of precedence ops in holding. */
-    int m_nLastCom;                          // Last command entered.
-    ANGLE_TYPE m_angletype;                  // Current Angle type when in dec mode. one of deg, rad or grad
-    NUM_WIDTH m_numwidth;                    // one of qword, dword, word or byte mode.
-    int32_t m_dwWordBitWidth;                // # of bits in currently selected word size
+          uint8_t* result = allocator.AllocateMemory(1, 1);
+  TF_LITE_MICRO_EXPECT_NE(nullptr, result);
     
-    namespace CalculatorApp
-{
-    namespace Common
-    {
-        ref class AlwaysSelectedCollectionView sealed : public Windows::UI::Xaml::DependencyObject, public Windows::UI::Xaml::Data::ICollectionView
-        {
-            internal : AlwaysSelectedCollectionView(Windows::UI::Xaml::Interop::IBindableVector ^ source)
-                : m_currentPosition(-1)
-            {
-                m_source = source;
-    }
-    }
-    }
-    }
+    #include <gtest/gtest.h>
+#include 'tensorflow/lite/interpreter.h'
+#include 'tensorflow/lite/kernels/custom_ops_register.h'
+#include 'tensorflow/lite/kernels/test_util.h'
+#include 'tensorflow/lite/model.h'
     
-    NarratorAnnouncement ^ CalculatorAnnouncement::GetMemoryItemAddedAnnouncement(String ^ announcement)
-{
-    return ref new NarratorAnnouncement(
-        announcement, CalculatorActivityIds::MemoryItemAdded, AutomationNotificationKind::ItemAdded, AutomationNotificationProcessing::MostRecent);
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    
+    {  // Verify metrics.
+  EvaluationStageMetrics metrics = stage.LatestMetrics();
+  EXPECT_EQ(metrics.num_runs(), 1);
+  const auto& max_latency = metrics.process_metrics().total_latency().max_us();
+  EXPECT_GT(max_latency, 0);
+  EXPECT_LT(max_latency, 1e7);
+  EXPECT_LE(metrics.process_metrics().total_latency().last_us(), max_latency);
+  EXPECT_LE(metrics.process_metrics().total_latency().min_us(), max_latency);
+  EXPECT_GT(metrics.process_metrics().total_latency().sum_us(), max_latency);
+  EXPECT_LE(metrics.process_metrics().total_latency().avg_us(), max_latency);
+  EXPECT_EQ(
+      metrics.process_metrics().tflite_inference_metrics().num_inferences(), 2);
 }
     
-    #include <mxnet/op_attr_types.h>
-#include <vector>
+        http://www.apache.org/licenses/LICENSE-2.0
     
-    /*!
- * Copyright (c) 2016 by Contributors
- * \file caffe_fieldentry.h
- * \brief Implement FieldEntry<caffe::LayerParameter>
- * \author Haoran Wang
- */
-#ifndef PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
-#define PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
+      if (num_retvals != node_data->NumOutputs()) {
+    return tensorflow::errors::Internal(
+        'Unexpected number of outputs from EagerExecute');
+  }
     
-      virtual void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) {
-    param_.InitAllowUnknown(kwargs);
-    base_->Init(kwargs);
-    rnd_.seed(kRandMagic + param_.seed);
-    outimg_.set_pad(false);
-    meanimg_.set_pad(false);
-    if (param_.mean_img.length() != 0) {
-      std::unique_ptr<dmlc::Stream> fi(
-          dmlc::Stream::Create(param_.mean_img.c_str(), 'r', true));
-      if (fi.get() == nullptr) {
-        this->CreateMeanImg();
-      } else {
-        fi.reset(nullptr);
-        if (param_.verbose) {
-          LOG(INFO) << 'Load mean image from ' << param_.mean_img;
+    template <typename T, typename Index, int IXDIM>
+struct GatherNdSlice<GPUDevice, T, Index, IXDIM> {
+  Index operator()(const GPUDevice& d, const Index unused_slice_size,
+                   typename TTypes<int32>::Scalar Tscratch,
+                   typename TTypes<T, IXDIM + 1>::ConstTensor Tparams,
+                   typename TTypes<Index>::ConstMatrix Tindices,
+                   typename TTypes<T>::Matrix Tout) {
+    const int64 indices_size = Tindices.dimension(1);
+    const int64 out_size = Tout.size();
+    int64 s_size = Tout.dimension(1);
+    Eigen::array<int64, IXDIM> batch_strides;
+    Eigen::array<int64, IXDIM> batch_indices;
+    if (IXDIM > 0) {
+      batch_strides[size_t(IXDIM - 1)] = s_size;
+      batch_indices[size_t(IXDIM - 1)] = Tparams.dimension(IXDIM - 1);
+    }
+    for (int i = IXDIM - 1; i > 0; --i) {
+      batch_indices[i - 1] = Tparams.dimension(i - 1);
+      batch_strides[i - 1] = batch_strides[i] * Tparams.dimension(i);
+    }
+    GpuLaunchConfig config = GetGpuLaunchConfig(out_size, d);
+    }
+    }
+    
+      std::string error;
+  auto isUptodateOpt = parentUnitWriter.isUnitUpToDateForOutputFile(/*FilePath=*/filename,
+                                                                /*TimeCompareFilePath=*/filename, error);
+  if (!isUptodateOpt.hasValue()) {
+    diags.diagnose(SourceLoc(), diag::error_index_failed_status_check, error);
+    return true;
+  }
+  if (*isUptodateOpt)
+    return false;
+    
+    namespace {
+/// Infers the availability required to access a type.
+class AvailabilityInferenceTypeWalker : public TypeWalker {
+public:
+  ASTContext &AC;
+  AvailabilityContext AvailabilityInfo = AvailabilityContext::alwaysAvailable();
+    }
+    }
+    
+      void appendFunctionSignature(AnyFunctionType *fn,
+                               const ValueDecl *forDecl = nullptr);
+    
+      DiagnosticSuppression(const DiagnosticSuppression &) = delete;
+  DiagnosticSuppression &operator=(const DiagnosticSuppression &) = delete;
+    
+      bool insert(const Key1 &k1, const Key2 &k2, Value &v) {
+    const bool r1 = map1.insert(k1, k2, v);
+    const bool r2 = map2.insert(k2, k1, v);
+    assertConsistent(r1, r2);
+    return r1;
+  }
+  bool insert(const Key2 &k2, const Key1 &k1, Value &v) {
+    return insert(k1, k2, v);
+  }
+  Optional<Value> find(const Key1 &k1, const Key2 &k2) const {
+    auto v = map1.find(k1, k2);
+    assert(assertConsistent(v, map2.find(k2, k1)));
+    return v;
+  }
+  Optional<Value> find(const Key2 &k2, Key1 &k1) const { return find(k1, k2); }
+    
+    double UniValue::get_real() const
+{
+    if (typ != VNUM)
+        throw std::runtime_error('JSON value is not a number as expected');
+    double retval;
+    if (!ParseDouble(getValStr(), &retval))
+        throw std::runtime_error('JSON double out of range');
+    return retval;
+}
+    
+    #ifndef BITCOIN_CRYPTO_RIPEMD160_H
+#define BITCOIN_CRYPTO_RIPEMD160_H
+    
+    TEST(FormatTest, InternalKey_EncodeDecode) {
+  const char* keys[] = { '', 'k', 'hello', 'longggggggggggggggggggggg' };
+  const uint64_t seq[] = {
+    1, 2, 3,
+    (1ull << 8) - 1, 1ull << 8, (1ull << 8) + 1,
+    (1ull << 16) - 1, 1ull << 16, (1ull << 16) + 1,
+    (1ull << 32) - 1, 1ull << 32, (1ull << 32) + 1
+  };
+  for (int k = 0; k < sizeof(keys) / sizeof(keys[0]); k++) {
+    for (int s = 0; s < sizeof(seq) / sizeof(seq[0]); s++) {
+      TestKey(keys[k], seq[s], kTypeValue);
+      TestKey('hello', 1, kTypeDeletion);
+    }
+  }
+}
+    
+    Status DumpTable(Env* env, const std::string& fname, WritableFile* dst) {
+  uint64_t file_size;
+  RandomAccessFile* file = NULL;
+  Table* table = NULL;
+  Status s = env->GetFileSize(fname, &file_size);
+  if (s.ok()) {
+    s = env->NewRandomAccessFile(fname, &file);
+  }
+  if (s.ok()) {
+    // We use the default comparator, which may or may not match the
+    // comparator used in this database. However this should not cause
+    // problems since we only use Table operations that do not require
+    // any comparisons.  In particular, we do not call Seek or Prev.
+    s = Table::Open(Options(), file, file_size, &table);
+  }
+  if (!s.ok()) {
+    delete table;
+    delete file;
+    return s;
+  }
+    }
+    
+    namespace leveldb {
+    }
+    
+    
+template <>
+void
+EnvmapAttribute::readValueFrom (OPENEXR_IMF_INTERNAL_NAMESPACE::IStream &is, int size, int version)
+{
+    unsigned char tmp;
+    Xdr::read <StreamIO> (is, tmp);
+    _value = Envmap (tmp);
+}
+    
+        int             _rleSymbol;        // RLE symbol written by the encoder.
+                                       // This could be 65536, so beware
+                                       // when you use shorts to hold things.
+    
+    
+typedef TypedAttribute<float> FloatAttribute;
+template <> IMF_EXPORT const char *FloatAttribute::staticTypeName ();
+    
+    //-----------------------------------------------------------------------------
+//
+//      class Slice
+//      class FrameBuffer
+//
+//-----------------------------------------------------------------------------
+    
+    
+//-----------------------------------------------------------------------------
+//
+//	Low-level file input and output for OpenEXR.
+//
+//-----------------------------------------------------------------------------
+    
+                _data->tFile = new TiledInputFile (_data->header,
+                                               _data->_streamData->is,
+                                               _data->version,
+                                               _data->numThreads);
         }
-        // use python compatible ndarray store format
-        std::vector<NDArray> data;
-        std::vector<std::string> keys;
+        
+        else if(!_data->header.hasType() || _data->header.type()==SCANLINEIMAGE)
         {
-          std::unique_ptr<dmlc::Stream> fi(dmlc::Stream::Create(param_.mean_img.c_str(), 'r'));
-          NDArray::Load(fi.get(), &data, &keys);
+            _data->sFile = new ScanLineInputFile (_data->header,
+                                                  _data->_streamData->is,
+                                                  _data->numThreads);
+        }else{
+            // type set but not recognised
+            
+            THROW(IEX_NAMESPACE::ArgExc, 'InputFile cannot handle parts of type ' << _data->header.type());
         }
-        CHECK_EQ(data.size(), 1U)
-            << 'Invalid mean image file format';
-        data[0].WaitToRead();
-        mshadow::Tensor<cpu, 3> src = data[0].data().get<cpu, 3, real_t>();
-        meanimg_.Resize(src.shape_);
-        mshadow::Copy(meanimg_, src);
-        meanfile_ready_ = true;
-      }
     }
+    else
+    {
+        if(_data->header.hasType() && _data->header.type()==DEEPSCANLINE)
+        {
+            _data->isTiled=false;
+            const Box2i &dataWindow = _data->header.dataWindow();
+            _data->minY = dataWindow.min.y;
+            _data->maxY = dataWindow.max.y;
+            
+            _data->dsFile = new DeepScanLineInputFile (_data->part);
+            _data->compositor = new CompositeDeepScanLine;
+            _data->compositor->addSource(_data->dsFile);
+        }
+        else if (isTiled (_data->header.type()))
+        {
+            _data->isTiled = true;
+            _data->lineOrder = _data->header.lineOrder();
+    
+    
+void
+InputPart::rawPixelDataToBuffer (int scanLine, char *pixelData, int &pixelDataSize) const
+{
+    file->rawPixelDataToBuffer(scanLine, pixelData, pixelDataSize);
+}
+    
+    
+template <>
+const char *
+IntAttribute::staticTypeName ()
+{
+    return 'int';
+}
+    
+      std::multimap<grpc::string_ref, grpc::string_ref>* GetRecvTrailingMetadata()
+      override {
+    GPR_CODEGEN_ASSERT(false &&
+                       'It is illegal to call GetRecvTrailingMetadata on a '
+                       'method which has a Cancel notification');
+    return nullptr;
   }
     
-    struct dequantize_2bit {
-  MSHADOW_XINLINE static void Map(int i,
-                                  float *out,
-                                  float *in,
-                                  const float neg_threshold,
-                                  const float pos_threshold) {
-    // get position of dequantized value to fill
-    float *outval = out + i;
-    // gets byte which holds quantized value for this position
-    char *ch_ptr = reinterpret_cast<char *>(in + (i >> 4));
-    ch_ptr += ((i & 15) >> 2);
-    // masks used to quantize data
-    const uint8_t posbits[] = {0xc0, 0x30, 0x0c, 0x03};
-    const uint8_t negbits[] = {0x80, 0x20, 0x08, 0x02};
-    // col denotes which two bits of a byte are set for this value
-    // col=0 implies first two bits, col=3 implies last two bits,...
-    const int col = i & 3;
-    const uint8_t mask = posbits[col];
-    const uint8_t negmask = negbits[col];
-    const uint8_t masked = *ch_ptr & mask;
-    if (masked == mask) {
-      *outval = pos_threshold;
-    } else if (masked == negmask) {
-      // use posbits for mask as posbits are both 1s
-      // then compare masked with negbits to see if only negbits were set
-      *outval = neg_threshold;
-    } else {
-      *outval = 0;
-    }
-  }
+      GPR_ASSERT(WSAConnect(cli_sock, (grpc_sockaddr*)&addr, addr_len, NULL, NULL,
+                        NULL, NULL) == 0);
+  svr_sock = accept(lst_sock, (grpc_sockaddr*)&addr, &addr_len);
+  GPR_ASSERT(svr_sock != INVALID_SOCKET);
+    
+    DWORD grpc_get_default_wsa_socket_flags() { return s_wsa_socket_flags; }
+    
+    
+    {  gpr_setenv(name, value);
+  retrieved_value = gpr_getenv(name);
+  GPR_ASSERT(retrieved_value != nullptr);
+  GPR_ASSERT(strcmp(value, retrieved_value) == 0);
+  gpr_free(retrieved_value);
+}
+    
+    GPR_GLOBAL_CONFIG_DECLARE_BOOL(bool_var);
+    
+    
+    { protected:
+  std::unique_ptr<Server> server_;
+  std::unique_ptr<grpc::testing::EchoTestService::Stub> stub_;
+  std::unique_ptr<ProtoReflectionDescriptorDatabase> desc_db_;
+  std::unique_ptr<protobuf::DescriptorPool> desc_pool_;
+  std::unordered_set<string> known_files_;
+  std::unordered_set<string> known_types_;
+  const protobuf::DescriptorPool* ref_desc_pool_;
+  int port_;
+  reflection::ProtoServerReflectionPlugin plugin_;
 };
     
-      /*!
-   * \brief returns type of compression if any
-   */
-  CompressionType get_type();
-    
-      void Init(const TVMArgs& args,
-            const std::vector<int>& const_loc,
-            std::vector<Engine::VarHandle>* const_vars,
-            std::vector<Engine::VarHandle>* mutate_vars) {
-    values_.clear();
-    type_codes_.clear();
-    values_.insert(values_.end(), args.values, args.values + args.size());
-    type_codes_.insert(
-        type_codes_.end(), args.type_codes, args.type_codes + args.size());
-    }
-    
-    namespace mxnet {
-namespace op {
-template<>
-Operator* CreateOp<cpu>(CropParam param) {
-  return new CropOp<cpu>(param);
-}
-    }
-    }
-    
-      virtual void Backward(const OpContext &ctx,
-                        const std::vector<TBlob> &out_grad,
-                        const std::vector<TBlob> &in_data,
-                        const std::vector<TBlob> &out_data,
-                        const std::vector<OpReqType> &req,
-                        const std::vector<TBlob> &in_grad,
-                        const std::vector<TBlob> &aux_args) {
-    using namespace mshadow;
-    CHECK_EQ(in_data.size(), 2U);
-    CHECK_EQ(out_data.size(), 3U);
-    CHECK_EQ(out_grad.size(), 1U);
-    Stream<gpu> *s = ctx.get_stream<gpu>();
-    Tensor<gpu, 4, DType> data = in_data[st::kData].get<gpu, 4, DType>(s);
-    Tensor<gpu, 4, DType> grad = out_grad[st::kOut].get<gpu, 4, DType>(s);
-    Tensor<gpu, 4, DType> ddata = in_grad[st::kData].get<gpu, 4, DType>(s);
-    Shape<3> loc_shape = Shape3(data.size(0), 2, 3);
-    Shape<4> grid_shape = Shape4(grad.size(0), grad.size(2), grad.size(3), 2);
-    Tensor<gpu, 3, DType> dloc = in_grad[st::kLoc].get_with_shape<gpu, 3, DType>(loc_shape, s);
-    Tensor<gpu, 4, DType> grid = out_data[st::kGridSrc]
-                    .get_with_shape<gpu, 4, DType>(grid_shape, s);
-    // do not use out_grad[st::kGridSrc], because dgrid is a intermediate tensor, and not include in
-    // DeclareBackwardDependency, another, we can we reuse grid for inplace operator
-    typename DataType<DType>::ScaleType alpha = 1.0f;
-    typename DataType<DType>::ScaleType beta = 0.0f;
-    typename DataType<DType>::ScaleType alpha_dgrid = 1.0f;
-    typename DataType<DType>::ScaleType beta_dgrid = 0.0f;
-    CUDNN_CALL(cudnnSpatialTfSamplerBackward(s->dnn_handle_,
-                                             st_desc_,
-                                             &alpha,
-                                             in_desc_,
-                                             data.dptr_,
-                                             &beta,
-                                             in_desc_/*reuse in_desc_*/,
-                                             ddata.dptr_/*output*/,
-                                             &alpha_dgrid,
-                                             out_desc_/*reuse out_desc_*/,
-                                             grad.dptr_,
-                                             grid.dptr_,
-                                             &beta_dgrid,
-                                             grid.dptr_));
-    if (param_.transform_type == st::kAffine) {
-      CUDNN_CALL(cudnnSpatialTfGridGeneratorBackward(s->dnn_handle_,
-                                                     st_desc_,
-                                                     grid.dptr_,
-                                                     dloc.dptr_/*out*/));
-    }
-  }
-    
-    
-    {
-    {}  // namespace op
-}  // namespace mxnet
-
-    
-    
-    // ============
-    // number types
-    // ============
-    
-    
-    {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-    
-    TEST(ByteTest, SetGetHighLowBit) {
-  unsigned char byte_value = 0x37;
-  Byte value(&byte_value);
-  value.set_value_high_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
-  value.set_value_low_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
-}
-    
-    #include 'glog/logging.h'
-    
-      x <<= 2;
-  x |= t;
-    
-    // config detail: {'name': 'commanded_value', 'offset': 0.0, 'precision': 0.001,
-// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 23,
-// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
-double Accelrpt68::commanded_value(const std::uint8_t* bytes,
-                                   int32_t length) const {
-  Byte t0(bytes + 2);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-    namespace apollo {
-namespace canbus {
-namespace gem {
-    }
-    }
-    }
-    
-    #include 'glog/logging.h'
+      grpc::string name() override { return PLUGIN_NAME; }
