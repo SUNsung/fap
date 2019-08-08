70bddc936cf60fe18248cfc2296a2244be99ab76
SUNsung <weatherius@gmail.com>
2019-08-08 21:24:42 +0300

    Rewrite [53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py] File in: 2019-08-08 21:24:42

--- pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
+++ pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
@@ -1,244 +1,136 @@
 
         
-        
-def render_function(function, method=True):
-    subblocks = []
-    signature = get_function_signature(function, method=method)
-    if method:
-        signature = signature.replace(
-            clean_module_name(function.__module__) + '.', '')
-    subblocks.append('### ' + function.__name__ + '\n')
-    subblocks.append(code_snippet(signature))
-    docstring = function.__doc__
-    if docstring:
-        if ('backend' in signature and
-                '{{np_implementation}}' in docstring):
-            docstring = add_np_implementation(function, docstring)
-        subblocks.append(process_docstring(docstring))
-    return '\n\n'.join(subblocks)
-    
-    for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
-    for t, char in enumerate(input_text):
-        encoder_input_data[i, t, input_token_index[char]] = 1.
-    for t, char in enumerate(target_text):
-        # decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, target_token_index[char]] = 1.
-        if t > 0:
-            # decoder_target_data will be ahead by one timestep
-            # and will not include the start character.
-            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
-    
-    print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-    
-            cells = [MinimalRNNCell(32), MinimalRNNCell(64)]
-        x = keras.Input((None, 5))
-        layer = RNN(cells)
-        y = layer(x)
-    ```
-    ''',
-    'result': '''Base class for recurrent layers.
+        PY2 = sys.version_info[0] == 2
     
     
-@pytest.fixture
-def fake_downloaded_boston_path(monkeypatch):
-    num_rows = 100
-    num_cols = 10
-    rng = np.random.RandomState(123)
+def get_top_states(t_state_v, K=4):
+    return sorted(t_state_v, key=t_state_v.__getitem__, reverse=True)[:K]
     
     
-@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
-def test_normal(tensor_shape):
-    _runner(initializers.RandomNormal(mean=0, stddev=1), tensor_shape,
-            target_mean=0., target_std=1)
+if len(args) < 1:
+    print(USAGE)
+    sys.exit(1)
     
-        # Returns
-        A string, either `'channels_first'` or `'channels_last'`
+    if opt.withWeight is None:
+    withWeight = False
+else:
+    if int(opt.withWeight) is 1:
+        withWeight = True
+    else:
+        withWeight = False
     
-        def get_config(self):
-        config = {'input_dim': self.input_dim,
-                  'output_dim': self.output_dim,
-                  'embeddings_initializer':
-                      initializers.serialize(self.embeddings_initializer),
-                  'embeddings_regularizer':
-                      regularizers.serialize(self.embeddings_regularizer),
-                  'activity_regularizer':
-                      regularizers.serialize(self.activity_regularizer),
-                  'embeddings_constraint':
-                      constraints.serialize(self.embeddings_constraint),
-                  'mask_zero': self.mask_zero,
-                  'input_length': self.input_length}
-        base_config = super(Embedding, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
+    import jieba
+    
+    
+  def ShouldUseNow( self, request_data ):
+    self._omnifunc = utils.ToUnicode( vim.eval( '&omnifunc' ) )
+    if not self._omnifunc:
+      return False
+    if self.ShouldUseCache():
+      return super( OmniCompleter, self ).ShouldUseNow( request_data )
+    return self.ShouldUseNowInner( request_data )
+    
+        return _MasterDiagnosticFilter( compiled_by_type )
+    
+          BaseRequest().GetDataFromHandler( 'healthy', display_message = False )
 
     
-    Input may optionally be reversed, shown to increase performance in many tasks in:
-'Learning to Execute'
-http://arxiv.org/abs/1410.4615
-and
-'Sequence to Sequence Learning with Neural Networks'
-http://papers.nips.cc/paper/5346-sequence-to-sequence-learning-with-neural-networks.pdf
-Theoretically it introduces shorter term dependencies between source and target.
     
-        headers['Accept'] = 'application/json'
-    headers['Content-Type'] = 'application/json'
-    
-    - name: Deploy package
-  clc_blueprint_package:
-        server_ids:
-            - UC1TEST-SERVER1
-            - UC1TEST-SERVER2
-        package_id: 77abb844-579d-478d-3955-c69ab4a7ba1a
-        package_params: {}
-'''
-    
-    ---
-- name: Create Server Group
-  hosts: localhost
-  gather_facts: False
-  connection: local
-  tasks:
-    - name: Create / Verify a Server Group at CenturyLink Cloud
-      clc_group:
-        name: My Cool Server Group
-        parent: Default Group
-        state: present
-      register: clc
-    
-    RETURN = '''
-server_ids:
-    description: The list of server ids that are changed
-    returned: success
-    type: list
-    sample:
-        [
-            'UC1TEST-SVR01',
-            'UC1TEST-SVR02'
-        ]
-'''
-    
-        if state == 'present' and not pool:
-        create_pool(module, system)
-    elif state == 'present' and pool:
-        update_pool(module, system, pool)
-    elif state == 'absent' and pool:
-        delete_pool(module, pool)
-    elif state == 'absent' and not pool:
-        module.exit_json(changed=False)
-    
-        def fetch_balancer_page(self):
-        ''' Returns the balancer management html page as a string for later parsing.'''
-        page = fetch_url(self.module, str(self.url))
-        if page[1]['status'] != 200:
-            self.module.fail_json(msg='Could not get balancer page! HTTP status response: ' + str(page[1]['status']))
-        else:
-            content = page[0].read()
-            apache_version = regexp_extraction(content.upper(), APACHE_VERSION_EXPRESSION, 1)
-            if apache_version:
-                if not re.search(pattern=r'2\.4\.[\d]*', string=apache_version):
-                    self.module.fail_json(msg='This module only acts on an Apache2 2.4+ instance, current Apache2 version: ' + str(apache_version))
-                return content
-            else:
-                self.module.fail_json(msg='Could not get the Apache server version from the balancer-manager')
-    
-            issue = {
-            'project': project_name,
-            'subject': issue_subject,
-            'priority': issue_priority,
-            'status': issue_status,
-            'type': issue_type,
-            'severity': issue_severity,
-            'description': issue_description,
-            'tags': issue_tags,
-        }
-    
-    
-def main():
-    module = AnsibleModule(
-        argument_spec=gen_specs(
-            attributes=dict(default={}, type='dict'),
-            objectClass=dict(type='raw'),
-            params=dict(type='dict'),
-            state=dict(default='present', choices=['present', 'absent']),
-        ),
-        supports_check_mode=True,
-    )
-    
-        HAS_LDAP = True
-except ImportError:
-    LDAP_IMP_ERR = traceback.format_exc()
-    HAS_LDAP = False
-    
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-    
-    - name: Configure IMC using multiple XML fragments
-  imc_rest:
-    hostname: '{{ imc_hostname }}'
-    username: '{{ imc_username }}'
-    password: '{{ imc_password }}'
-    validate_certs: no
-    timeout: 120
-    content: |
-      <!-- Configure Serial-on-LAN -->
-      <configConfMo><inConfig>
-        <solIf dn='sys/rack-unit-1/sol-if' adminState='enable' speed=='115200' comport='com0'/>
-      </inConfig></configConfMo>
-    
-    
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-    
-    from copy import deepcopy
-import json
-from ansible.module_utils.basic import AnsibleModule, missing_required_lib
-from ansible.module_utils.remote_management.ucs import UCSModule, ucs_argument_spec
-    
-        strings = []
-    with open(filepath, 'r') as f:
-    
-        def setUp(self):
-        from acme.errors import MissingNonce
-        self.response = mock.MagicMock(headers={})
-        self.response.request.method = 'FOO'
-        self.error = MissingNonce(self.response)
-    
-    
-# -- Options for Texinfo output -------------------------------------------
-    
-            self.vhost1b = VirtualHost(
-            'filep', 'vh_path', set([self.addr1]), False, False, 'localhost')
-    
-        config = configobj.ConfigObj()
-    
-        entry_points={
-        'console_scripts': [
-            'certbot = certbot.main:main',
+GENERIC_RESPONSE = {
+  'clang': {
+    'has_support': True,
+    'version': 'Clang version'
+  },
+  'completer': {
+    'items': [
+      {
+        'key': 'key',
+        'value': 'value'
+      }
+    ],
+    'name': 'Completer name',
+    'servers': [
+      {
+        'address': '127.0.0.1',
+        'executable': '/path/to/executable',
+        'extras': [
+          {
+            'key': 'key',
+            'value': 'value'
+          }
         ],
-        'certbot.plugins': [
-            'manual = certbot.plugins.manual:Authenticator',
-            'null = certbot.plugins.null:Installer',
-            'standalone = certbot.plugins.standalone:Authenticator',
-            'webroot = certbot.plugins.webroot:Authenticator',
+        'is_running': True,
+        'logfiles': [
+          '/path/to/stdout/logfile',
+          '/path/to/stderr/logfile'
         ],
-    },
-)
-
+        'name': 'Server name',
+        'pid': 12345,
+        'port': 1234
+      }
+    ]
+  },
+  'extra_conf': {
+    'is_loaded': False,
+    'path': '/path/to/extra/conf'
+  },
+  'python': {
+    'executable': '/path/to/python/interpreter',
+    'version': 'Python version'
+  }
+}
     
-    .. code-block:: ini
-   :name: credentials.ini
-   :caption: Example credentials file:
+      with CurrentWorkingDirectory( unicode_dir ):
+    with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
+      with MockCompletionRequest( ServerResponse ):
+        ycm.SendCompletionRequest()
+        ok_( ycm.CompletionRequestReady() )
+        assert_that(
+          ycm.GetCompletionResponse(),
+          has_entries( {
+            'completions': empty(),
+            'completion_start_column': 1
+          } )
+        )
     
     
-@pytest.fixture()
-def context(request):
-    # Fixture request is a built-in pytest fixture describing current test request.
-    integration_test_context = nginx_context.IntegrationTestsContext(request)
-    try:
-        yield integration_test_context
-    finally:
-        integration_test_context.cleanup()
+def KeywordsFromSyntaxListOutput_JavaSyntax_test():
+  expected_keywords = (
+    'code', 'text', 'cols', 'datetime', 'disabled', 'shape', 'codetype', 'alt',
+    'compact', 'style', 'valuetype', 'short', 'finally', 'continue', 'extends',
+    'valign', 'bordercolor', 'do', 'return', 'rel', 'rules', 'void',
+    'nohref', 'abbr', 'background', 'scrolling', 'instanceof', 'name',
+    'summary', 'try', 'default', 'noshade', 'coords', 'dir', 'frame', 'usemap',
+    'ismap', 'static', 'hspace', 'vlink', 'for', 'selected', 'rev', 'vspace',
+    'content', 'method', 'version', 'volatile', 'above', 'new', 'charoff',
+    'public', 'alink', 'enum', 'codebase', 'if', 'noresize', 'interface',
+    'checked', 'byte', 'super', 'throw', 'src', 'language', 'package',
+    'standby', 'script', 'longdesc', 'maxlength', 'cellpadding', 'throws',
+    'tabindex', 'color', 'colspan', 'accesskey', 'float', 'while', 'private',
+    'height', 'boolean', 'wrap', 'prompt', 'nowrap', 'size', 'rows', 'span',
+    'clip', 'bgcolor', 'top', 'long', 'start', 'scope', 'scheme', 'type',
+    'final', 'lang', 'visibility', 'else', 'assert', 'transient', 'link',
+    'catch', 'true', 'serializable', 'target', 'lowsrc', 'this', 'double',
+    'align', 'value', 'cite', 'headers', 'below', 'protected', 'declare',
+    'classid', 'defer', 'false', 'synchronized', 'int', 'abstract', 'accept',
+    'hreflang', 'char', 'border', 'id', 'native', 'rowspan', 'charset',
+    'archive', 'strictfp', 'readonly', 'axis', 'cellspacing', 'profile',
+    'multiple', 'object', 'action', 'pagex', 'pagey', 'marginheight', 'data',
+    'class', 'frameborder', 'enctype', 'implements', 'break', 'gutter', 'url',
+    'clear', 'face', 'switch', 'marginwidth', 'width', 'left' )
+    
+        def shutdown(self, wait=True):
+        with self._shutdown_lock:
+            self._shutdown_thread = True
+        if self._queue_management_thread:
+            # Wake up queue management thread
+            self._result_queue.put(None)
+            if wait:
+                self._queue_management_thread.join()
+        # To reduce the risk of openning too many files, remove references to
+        # objects that use file descriptors.
+        self._queue_management_thread = None
+        self._call_queue = None
+        self._result_queue = None
+        self._processes = None
+    shutdown.__doc__ = _base.Executor.shutdown.__doc__
