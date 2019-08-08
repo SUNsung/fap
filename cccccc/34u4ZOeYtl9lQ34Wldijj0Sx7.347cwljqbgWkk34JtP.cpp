cdc082cc237d5ec05661e222dab75489ee49b6c0
SUNsung <weatherius@gmail.com>
2019-08-08 17:52:37 +0300

    Rewrite [34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp] File in: 2019-08-08 17:52:36

--- cccccc/34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp
+++ cccccc/34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp
@@ -1,309 +1,149 @@
 
         
-            http://www.apache.org/licenses/LICENSE-2.0
+        Dictionary::Dictionary(v8::Isolate* isolate, v8::Local<v8::Object> object)
+    : isolate_(isolate), object_(object) {}
     
-    string TryFindKernelClass(const string& serialized_node_def) {
-  tensorflow::NodeDef node_def;
-  if (!node_def.ParseFromString(serialized_node_def)) {
-    LOG(WARNING) << 'Error parsing node_def';
-    return '';
-  }
-    }
+    Promise::Promise(v8::Isolate* isolate) : isolate_(isolate) {
+  resolver_ = v8::Promise::Resolver::New(isolate);
+}
     
-    #include 'tensorflow/core/framework/graph.pb.h'
-#include 'tensorflow/core/platform/types.h'
+      v8::Local<v8::Object> GetThis() { return info_->This(); }
     
-      // Begins the timer at the present point in the stream.
-  bool Start(Stream *stream);
-    
-    v8::Local<v8::Value> Arguments::ThrowTypeError(
-    const std::string& message) const {
-  isolate_->ThrowException(
-      v8::Exception::TypeError(StringToV8(isolate_, message)));
-  return v8::Undefined(isolate_);
+    template <typename P1, typename P2, typename P3, typename P4>
+inline WrappableBase* InvokeFactory(
+    Arguments* args,
+    const base::Callback<WrappableBase*(P1, P2, P3, P4)>& callback) {
+  typename CallbackParamTraits<P1>::LocalType a1;
+  typename CallbackParamTraits<P2>::LocalType a2;
+  typename CallbackParamTraits<P3>::LocalType a3;
+  typename CallbackParamTraits<P4>::LocalType a4;
+  if (!GetNextArgument(args, 0, true, &a1) ||
+      !GetNextArgument(args, 0, false, &a2) ||
+      !GetNextArgument(args, 0, false, &a3) ||
+      !GetNextArgument(args, 0, false, &a4))
+    return nullptr;
+  return callback.Run(a1, a2, a3, a4);
 }
     
     
-    {}  // namespace
+    {}  // namespace mate
     
-    // This specialization allows people to construct function templates directly if
-// they need to do fancier stuff.
-template <>
-struct CallbackTraits<v8::Local<v8::FunctionTemplate>> {
-  static v8::Local<v8::FunctionTemplate> CreateTemplate(
-      v8::Local<v8::FunctionTemplate> templ) {
-    return templ;
+      void reset() { handle_.Reset(); }
+    
+      template <typename Sig>
+  static void SetConstructor(v8::Isolate* isolate,
+                             const base::Callback<Sig>& constructor) {
+    v8::Local<v8::FunctionTemplate> templ = CreateFunctionTemplate(
+        isolate, base::Bind(&internal::InvokeNew<Sig>, constructor));
+    templ->InstanceTemplate()->SetInternalFieldCount(1);
+    T::BuildPrototype(isolate, templ);
+    gin::PerIsolateData::From(isolate)->SetFunctionTemplate(&kWrapperInfo,
+                                                            templ);
   }
-};
     
-    #endif  // NATIVE_MATE_NATIVE_MATE_PROMISE_H_
+    #if defined(OS_WIN)
+  HWND remote_window_;               // The HWND_MESSAGE of another browser.
+  base::win::MessageWindow window_;  // The message-only window.
+  bool is_virtualized_;  // Stuck inside Microsoft Softricity VM environment.
+  HANDLE lock_file_;
+  base::FilePath user_data_dir_;
+  ShouldKillRemoteProcessCallback should_kill_remote_process_callback_;
+#elif defined(OS_POSIX) && !defined(OS_ANDROID)
+  // Start listening to the socket.
+  void StartListening(int sock);
+    
+      // The symlink lock is pointed to the hostname and process id, so other
+  // processes can find it out.
+  base::FilePath symlink_content(base::StringPrintf(
+      '%s%c%u', net::GetHostName().c_str(), kLockDelimiter, current_pid_));
+    
+    // A map to cache Python Pools per C++ pointer.
+// Pointers are not owned here, and belong to the PyDescriptorPool.
+static std::unordered_map<const DescriptorPool*, PyDescriptorPool*>*
+    descriptor_pool_map;
+    
+      // Make child side of the pipes inheritable.
+  if (!SetHandleInformation(stdin_pipe_read, HANDLE_FLAG_INHERIT,
+                            HANDLE_FLAG_INHERIT)) {
+    GOOGLE_LOG(FATAL) << 'SetHandleInformation: '
+                      << Win32ErrorMessage(GetLastError());
+  }
+  if (!SetHandleInformation(stdout_pipe_write, HANDLE_FLAG_INHERIT,
+                            HANDLE_FLAG_INHERIT)) {
+    GOOGLE_LOG(FATAL) << 'SetHandleInformation: '
+                      << Win32ErrorMessage(GetLastError());
+  }
+    
+    #endif  // GOOGLE_PROTOBUF_STUBS_MATHUTIL_H_
 
     
-      // Retrieve the v8 wrapper object cooresponding to this object.
-  v8::Local<v8::Object> GetWrapper() const;
-  v8::MaybeLocal<v8::Object> GetWrapper(v8::Isolate* isolate) const;
+      MapTestUtil::SetMapFields(&message1);
+  MapTestUtil::SetMapFields(&message2);
+  MapTestUtil::ModifyMapFields(&message2);
     
-    #if defined(OS_POSIX) && !defined(OS_ANDROID)
-  // Exposed for testing.  We use a timeout on Linux, and in tests we want
-  // this timeout to be short.
-  NotifyResult NotifyOtherProcessWithTimeout(
-      const base::CommandLine& command_line,
-      int retry_attempts,
-      const base::TimeDelta& timeout,
-      bool kill_unresponsive);
-  NotifyResult NotifyOtherProcessWithTimeoutOrCreate(
-      const base::CommandLine& command_line,
-      int retry_attempts,
-      const base::TimeDelta& timeout);
-  void OverrideCurrentPidForTesting(base::ProcessId pid);
-  void OverrideKillCallbackForTesting(
-      const base::RepeatingCallback<void(int)>& callback);
-#endif
-    
-    #if defined(OS_MACOSX)
-bool ReplaceOldSingletonLock(const base::FilePath& symlink_content,
-                             const base::FilePath& lock_path) {
-  // Try taking an flock(2) on the file. Failure means the lock is taken so we
-  // should quit.
-  base::ScopedFD lock_fd(HANDLE_EINTR(
-      open(lock_path.value().c_str(), O_RDWR | O_CREAT | O_SYMLINK, 0644)));
-  if (!lock_fd.is_valid()) {
-    PLOG(ERROR) << 'Could not open singleton lock';
-    return false;
-  }
+    TEST_F(JsonObjectWriterTest, TestWebsafeByteEncoding) {
+  ow_ = new JsonObjectWriter('', out_stream_);
+  ow_->set_use_websafe_base64_for_bytes(true);
+  ow_->StartObject('')
+      ->RenderBytes('bytes', '\x03\xef\xc0\x10')
+      ->EndObject();
     }
     
+      // Duplicate keys
     
-    {  content::ContentMainParams params(&delegate);
-  params.instance = instance;
-  params.sandbox_info = &sandbox_info;
-  electron::AtomCommandLine::Init(arguments.argc, arguments.argv);
-  return content::ContentMain(params);
+    
+bool ZeroCopyOutputStream::WriteAliasedRaw(const void* /* data */,
+                                           int /* size */) {
+  GOOGLE_LOG(FATAL) << 'This ZeroCopyOutputStream doesn't support aliasing. '
+                'Reaching here usually means a ZeroCopyOutputStream '
+                'implementation bug.';
+  return false;
 }
     
-    #define TEGRA_MERGE(src, dst, len, cn) \
-( \
-    CAROTENE_NS::isSupportedConfiguration() ? \
-        cn == 2 ? \
-        CAROTENE_NS::combine2(CAROTENE_NS::Size2D(len, 1), \
-                              src[0], len, \
-                              src[1], len, \
-                              dst, len), \
-        CV_HAL_ERROR_OK : \
-        cn == 3 ? \
-        CAROTENE_NS::combine3(CAROTENE_NS::Size2D(len, 1), \
-                              src[0], len, \
-                              src[1], len, \
-                              src[2], len, \
-                              dst, len), \
-        CV_HAL_ERROR_OK : \
-        cn == 4 ? \
-        CAROTENE_NS::combine4(CAROTENE_NS::Size2D(len, 1), \
-                              src[0], len, \
-                              src[1], len, \
-                              src[2], len, \
-                              src[3], len, \
-                              dst, len), \
-        CV_HAL_ERROR_OK : \
-        CV_HAL_ERROR_NOT_IMPLEMENTED \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
+    // Generate destructors.
+#include 'ipc/struct_destructor_macros.h'
+#include 'content/nw/src/common/common_message_generator.h'
     
-        void operator() (const typename VecTraits<T>::vec128 & v_src0,
-                     const typename VecTraits<T>::vec128 & v_src1,
-                     typename VecTraits<T>::vec128 & v_dst) const
-    {
-        typename VecTraits<wtype>::vec128 vrl, vrh;
-        wideAdd(vmovl( vget_low(v_src0)), vmovl( vget_low(v_src1)), vrl);
-        wideAdd(vmovl(vget_high(v_src0)), vmovl(vget_high(v_src1)), vrh);
-    }
+      int id() const { return id_; }
+  std::string extension_id_;
+  ObjectManager* object_manager() const { return object_manager_.get(); }
     
-    void bitwiseNot(const Size2D &size,
-                const u8 *srcBase, ptrdiff_t srcStride,
-                u8 *dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-    }
+    typedef std::map<std::string,std::string> KeyMap;
     
-                int16x8_t vabsx = vabsq_s16(vx);
-            int16x8_t vabsy = vabsq_s16(vy);
-    
-    #include 'common.hpp'
-    
-                    if( mask[0] == 0 )
-                {
-                    if (mask[1] != 0)
-                    {
-                        j -= 8;
-                        ptr -= 8;
-                    }
-                    continue;
-                }
-    
-                    int32x4_t lane0 = vld1q_s32(lane + x - 2);
-                int32x4_t lane4 = vld1q_s32(lane + x + 2);
-                int32x4_t lane1 = vld1q_s32(lane + x - 1);
-                int32x4_t lane3 = vld1q_s32(lane + x + 1);
-                int32x4_t lane2 = vld1q_s32(lane + x + 0);
-    
-    NarratorAnnouncement ^ CalculatorAnnouncement::GetDisplayUpdatedAnnouncement(String ^ announcement)
-{
-    return ref new NarratorAnnouncement(
-        announcement, CalculatorActivityIds::DisplayUpdated, AutomationNotificationKind::Other, AutomationNotificationProcessing::ImportantMostRecent);
-}
-    
-    using namespace CalculatorApp::Common::Automation;
-using namespace Platform;
-using namespace Windows::UI::Xaml;
-using namespace Windows::UI::Xaml::Automation;
-using namespace Windows::UI::Xaml::Automation::Peers;
-    
-      void BeforeFirst() override {
-    parser_->BeforeFirst();
-  }
-    
-    /*!
- * \brief template for all quantile sketch algorithm
- *        that uses merge/prune scheme
- * \tparam DType type of data content
- * \tparam RType type of rank
- * \tparam TSummary actual summary data structure it uses
- */
-template<typename DType, typename RType, class TSummary>
-class QuantileSketchTemplate {
+    class NwClipboardReadAvailableTypesFunction : public NWSyncExtensionFunction {
  public:
-  /*! \brief type of summary type */
-  using Summary = TSummary;
-  /*! \brief the entry type */
-  using Entry = typename Summary::Entry;
-  /*! \brief same as summary, but use STL to backup the space */
-  struct SummaryContainer : public Summary {
-    std::vector<Entry> space;
-    SummaryContainer(const SummaryContainer &src) : Summary(nullptr, src.size) {
-      this->space = src.space;
-      this->data = dmlc::BeginPtr(this->space);
-    }
-    SummaryContainer() : Summary(nullptr, 0) {
-    }
-    /*! \brief reserve space for summary */
-    inline void Reserve(size_t size) {
-      if (size > space.size()) {
-        space.resize(size);
-        this->data = dmlc::BeginPtr(space);
-      }
-    }
-    /*!
-     * \brief set the space to be merge of all Summary arrays
-     * \param begin beginning position in the summary array
-     * \param end ending position in the Summary array
-     */
-    inline void SetMerge(const Summary *begin,
-                         const Summary *end) {
-      CHECK(begin < end) << 'can not set combine to empty instance';
-      size_t len = end - begin;
-      if (len == 1) {
-        this->Reserve(begin[0].size);
-        this->CopyFrom(begin[0]);
-      } else if (len == 2) {
-        this->Reserve(begin[0].size + begin[1].size);
-        this->SetMerge(begin[0], begin[1]);
-      } else {
-        // recursive merge
-        SummaryContainer lhs, rhs;
-        lhs.SetCombine(begin, begin + len / 2);
-        rhs.SetCombine(begin + len / 2, end);
-        this->Reserve(lhs.size + rhs.size);
-        this->SetCombine(lhs, rhs);
-      }
-    }
-    /*!
-     * \brief do elementwise combination of summary array
-     *        this[i] = combine(this[i], src[i]) for each i
-     * \param src the source summary
-     * \param max_nbyte maximum number of byte allowed in here
-     */
-    inline void Reduce(const Summary &src, size_t max_nbyte) {
-      this->Reserve((max_nbyte - sizeof(this->size)) / sizeof(Entry));
-      SummaryContainer temp;
-      temp.Reserve(this->size + src.size);
-      temp.SetCombine(*this, src);
-      this->SetPrune(temp, space.size());
-    }
-    /*! \brief return the number of bytes this data structure cost in serialization */
-    inline static size_t CalcMemCost(size_t nentry) {
-      return sizeof(size_t) + sizeof(Entry) * nentry;
-    }
-    /*! \brief save the data structure into stream */
-    template<typename TStream>
-    inline void Save(TStream &fo) const {  // NOLINT(*)
-      fo.Write(&(this->size), sizeof(this->size));
-      if (this->size != 0) {
-        fo.Write(this->data, this->size * sizeof(Entry));
-      }
-    }
-    /*! \brief load data structure from input stream */
-    template<typename TStream>
-    inline void Load(TStream &fi) {  // NOLINT(*)
-      CHECK_EQ(fi.Read(&this->size, sizeof(this->size)), sizeof(this->size));
-      this->Reserve(this->size);
-      if (this->size != 0) {
-        CHECK_EQ(fi.Read(this->data, this->size * sizeof(Entry)),
-                 this->size * sizeof(Entry));
-      }
-    }
-  };
-  /*!
-   * \brief initialize the quantile sketch, given the performance specification
-   * \param maxn maximum number of data points can be feed into sketch
-   * \param eps accuracy level of summary
-   */
-  inline void Init(size_t maxn, double eps) {
-    LimitSizeLevel(maxn, eps, &nlevel, &limit_size);
-    // lazy reserve the space, if there is only one value, no need to allocate space
-    inqueue.queue.resize(1);
-    inqueue.qtail = 0;
-    data.clear();
-    level.clear();
-  }
+  NwClipboardReadAvailableTypesFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-      bool Read(SparsePage* page,
-            dmlc::SeekStream* fi,
-            const std::vector<bst_uint>& sorted_index_set) override {
-    if (!fi->Read(&disk_offset_)) return false;
-    this->LoadIndexValue(fi);
-    auto& offset_vec = page->offset.HostVector();
-    auto& data_vec = page->data.HostVector();
-    offset_vec.clear();
-    offset_vec.push_back(0);
-    for (bst_uint cid : sorted_index_set) {
-      offset_vec.push_back(
-          offset_vec.back() + disk_offset_[cid + 1] - disk_offset_[cid]);
-    }
-    data_vec.resize(offset_vec.back());
-    CHECK_EQ(index_.data.size(), value_.data.size());
-    CHECK_EQ(index_.data.size(), disk_offset_.back());
-    }
+    class OrientationDetector {
+ public:
+  OrientationDetector(const GenericVector<int>* allowed_scripts,
+                      OSResults* results);
+  bool detect_blob(BLOB_CHOICE_LIST* scores);
+  int get_orientation();
+ private:
+  OSResults* osr_;
+  const GenericVector<int>* allowed_scripts_;
+};
+    
+    class TBOX;
     
     
-    {/*!
- * \brief Macro to register objective function.
- *
- * \code
- * // example of registering a objective
- * XGBOOST_REGISTER_OBJECTIVE(LinearRegression, 'reg:squarederror')
- * .describe('Linear regression objective')
- * .set_body([]() {
- *     return new RegLossObj(LossType::kLinearSquare);
- *   });
- * \endcode
- */
-#define XGBOOST_REGISTER_OBJECTIVE(UniqueId, Name)                      \
-  static DMLC_ATTRIBUTE_UNUSED ::xgboost::ObjFunctionReg &              \
-  __make_ ## ObjFunctionReg ## _ ## UniqueId ## __ =                    \
-      ::dmlc::Registry< ::xgboost::ObjFunctionReg>::Get()->__REGISTER__(Name)
-}  // namespace xgboost
-#endif  // XGBOOST_OBJECTIVE_H_
+    {  // Stores all the source points in the order they were given and their
+  // halfwidths, if any.
+  GenericVector<PointWidth> pts_;
+  // Stores the computed perpendicular distances of (some of) the pts_ from a
+  // given vector (assuming it goes through the origin, making it a line).
+  // Since the distances may be a subset of the input points, and get
+  // re-ordered by the nth_item function, the original point is stored
+  // along side the distance.
+  GenericVector<DistPointPair> distances_;  // Distances of points.
+  // The squared length of the vector used to compute distances_.
+  double square_length_;
+};
+    
+    #include 'errcode.h'
+#include 'genericvector.h'
+    
+    #endif  // TESSERACT_CCUTIL_QRSEQUENCE_H_
