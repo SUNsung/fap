9d634f08e3ddfe14c75ac02fde005226b11f74f6
SUNsung <weatherius@gmail.com>
2019-08-08 06:46:51 +0300

    Rewrite [28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp] File in: 2019-08-08 06:46:51

--- cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
+++ cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
@@ -1,93 +1,171 @@
 
         
-        // static
-void CallbackHolderBase::FirstWeakCallback(
-    const v8::WeakCallbackInfo<CallbackHolderBase>& data) {
-  data.GetParameter()->v8_ref_.Reset();
-  data.SetSecondPassCallback(SecondWeakCallback);
+                std::wstring ToString(uint32_t radix, NUMOBJ_FMT format, int32_t precision) const;
+        uint64_t ToUInt64_t() const;
+    
+            // Displayed number can go through transformation. So copy it after transformation
+        gldPrevious.value = m_currentVal;
+    
+        // initialize the max dec number you can support for each of the supported bit lengths
+    // this is basically max num in that width / 2 in integer
+    assert(m_chopNumbers.size() == m_maxDecimalValueStrings.size());
+    for (size_t i = 0; i < m_chopNumbers.size(); i++)
+    {
+        auto maxVal = m_chopNumbers[i] / 2;
+        maxVal = RationalMath::Integer(maxVal);
+    }
+    
+    bool LiveRegionHost::IsHostAvailable()
+{
+    // LiveRegion is always available.
+    return true;
 }
     
-    Dictionary Dictionary::CreateEmpty(v8::Isolate* isolate) {
-  return Dictionary(isolate, v8::Object::New(isolate));
+    NarratorAnnouncement ^ CalculatorAnnouncement::GetUpdateCurrencyRatesAnnouncement(String ^ announcement)
+{
+    return ref new NarratorAnnouncement(
+        announcement,
+        CalculatorActivityIds::UpdateCurrencyRates,
+        AutomationNotificationKind::ActionCompleted,
+        AutomationNotificationProcessing::ImportantMostRecent);
 }
     
     
-    {}  // namespace mate
+    {  {
+    auto lparam = xgboost::CreateEmptyGenericParam(1, -1);
+    xgboost::Metric * metric = xgboost::Metric::Create('rmse', &lparam);
+    ASSERT_STREQ(metric->Name(), 'rmse');
+    EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
+    EXPECT_NEAR(GetMetricEval(metric,
+                              {0.1f, 0.9f, 0.1f, 0.9f},
+                              {  0,   0,   1,   1}),
+                0.6403f, 0.001f);
+    delete metric;
+  }
+}
+#endif
 
     
-    // This function is a convenient way to create a handle from a raw pointer
-// without having to write out the type of the object explicitly.
-template <typename T>
-mate::Handle<T> CreateHandle(v8::Isolate* isolate, T* object) {
-  return mate::Handle<T>(object->GetWrapper(), object);
-}
+    #else  // Not R or R with GPU.
     
-    // Like Dictionary, but stores object in persistent handle so you can keep it
-// safely on heap.
-class PersistentDictionary : public Dictionary {
- public:
-  PersistentDictionary();
-  PersistentDictionary(v8::Isolate* isolate, v8::Local<v8::Object> object);
-  PersistentDictionary(const PersistentDictionary& other);
-  ~PersistentDictionary() override;
-    }
-    
-      // Temporary directory to hold the socket.
-  base::ScopedTempDir socket_dir_;
-    
-      std::set<std::unique_ptr<SocketReader>> readers_;
-    
-    using content::BrowserThread;
-    
-      // If we don't have any window then quitAndInstall immediately.
-  if (WindowList::IsEmpty()) {
-    auto_updater::AutoUpdater::QuitAndInstall();
-    return;
+    // This is a helpful data structure to define parameters
+// You do not have to use it.
+// see http://dmlc-core.readthedocs.org/en/latest/parameter.html
+// for introduction of this module.
+struct MyLogisticParam : public dmlc::Parameter<MyLogisticParam> {
+  float scale_neg_weight;
+  // declare parameters
+  DMLC_DECLARE_PARAMETER(MyLogisticParam) {
+    DMLC_DECLARE_FIELD(scale_neg_weight).set_default(1.0f).set_lower_bound(0.0f)
+        .describe('Scale the weight of negative examples by this factor');
   }
-    
-    // Constants for packing an encoded CheckedCastKind and
-// CastConsumptionKind together.
-enum {
-  // Must be large enough to store all the CheckedCastKindEncodings
-  SIL_CAST_CONSUMPTION_BIT_OFFSET = 4,
-  SIL_CHECKED_CAST_MASK =
-    (1 << SIL_CAST_CONSUMPTION_BIT_OFFSET) - 1
 };
     
-    KNOWN_STDLIB_TYPE_DECL(Encoder, ProtocolDecl, 1)
-KNOWN_STDLIB_TYPE_DECL(Decoder, ProtocolDecl, 1)
-KNOWN_STDLIB_TYPE_DECL(KeyedEncodingContainer, NominalTypeDecl, 1)
-KNOWN_STDLIB_TYPE_DECL(KeyedDecodingContainer, NominalTypeDecl, 1)
-KNOWN_STDLIB_TYPE_DECL(RangeReplaceableCollection, ProtocolDecl, 1)
     
-    #if defined(_WIN32)
-template <>
-const char *
-_swift_stdlib_strtoX_clocale_impl<float>(const char *str, float *result) {
-  if (swift_stringIsSignalingNaN(str)) {
-    *result = std::numeric_limits<float>::signaling_NaN();
-    return str + std::strlen(str);
-  }
-    }
+    {const char kQueryField[] = 'query';
+const char kLimitField[] = 'limit';
+const char kSkipField[] = 'skip';
+const char kHintField[] = 'hint';
+const char kCollationField[] = 'collation';
+const char kExplainField[] = 'explain';
+const char kCommentField[] = 'comment';
+const char kMaxTimeMSField[] = 'maxTimeMS';
+const char kReadConcernField[] = 'readConcern';
+}  // namespace
     
-    /// An ASTVisitor for generating SIL from top-level declarations in a module.
-class LLVM_LIBRARY_VISIBILITY SILGenModule : public ASTVisitor<SILGenModule> {
+    #include 'mongo/db/storage/kv/temporary_kv_record_store.h'
+    
+    RemoveTransliterator::RemoveTransliterator() : Transliterator(UnicodeString(TRUE, ::CURR_ID, -1), 0) {}
+    
+    SearchIterator::SearchIterator(const SearchIterator &other)
+    : UObject(other)
+{   
+    m_breakiterator_            = other.m_breakiterator_;
+    m_text_                     = other.m_text_;
+    m_search_                   = (USearch *)uprv_malloc(sizeof(USearch));   
+    m_search_->breakIter        = other.m_search_->breakIter;
+    m_search_->isCanonicalMatch = other.m_search_->isCanonicalMatch;
+    m_search_->isOverlap        = other.m_search_->isOverlap;
+    m_search_->elementComparisonType = other.m_search_->elementComparisonType;
+    m_search_->matchedIndex     = other.m_search_->matchedIndex;
+    m_search_->matchedLength    = other.m_search_->matchedLength;
+    m_search_->text             = other.m_search_->text;
+    m_search_->textLength       = other.m_search_->textLength;
+}
+    
+    #include 'unicode/messagepattern.h'
+#include 'unicode/rbnf.h'
+#include 'unicode/selfmt.h'
+#include 'unicode/uchar.h'
+#include 'unicode/ucnv_err.h'
+#include 'unicode/umsg.h'
+#include 'unicode/ustring.h'
+#include 'unicode/utypes.h'
+#include 'cmemory.h'
+#include 'messageimpl.h'
+#include 'patternprops.h'
+#include 'selfmtimpl.h'
+#include 'uassert.h'
+#include 'ustrfmt.h'
+#include 'util.h'
+#include 'uvector.h'
+    
+    
+#ifndef SELFMTIMPL
+#define SELFMTIMPL
+    
+    #include 'unicode/utypes.h'
+#include 'sharedobject.h'
+    
+    
+class U_I18N_API SharedDateFormatSymbols : public SharedObject {
 public:
-  /// The Module being constructed.
-  SILModule &M;
-  
-  /// The type converter for the module.
-  TypeConverter &Types;
-  
-  /// The Swift module we are visiting.
-  ModuleDecl *SwiftModule;
-  
-  /// TopLevelSGF - The SILGenFunction used to visit top-level code, or null if
-  /// the current source file is not a script source file.
-  SILGenFunction /*nullable*/ *TopLevelSGF;
+    SharedDateFormatSymbols(
+            const Locale &loc, const char *type, UErrorCode &status)
+            : dfs(loc, type, status) { }
+    virtual ~SharedDateFormatSymbols();
+    const DateFormatSymbols &get() const { return dfs; }
+private:
+    DateFormatSymbols dfs;
+    SharedDateFormatSymbols(const SharedDateFormatSymbols &);
+    SharedDateFormatSymbols &operator=(const SharedDateFormatSymbols &);
+};
+    
+    #ifndef incl_HPHP_SYNCHRONIZABLE_H_
+#define incl_HPHP_SYNCHRONIZABLE_H_
+    
+    
+std::mutex watchdog_mut;
+static int watchdog_count = 0;
+    
+    
+    {
+    {}  // namespace planning
+}  // namespace apollo
+
+    
+        params.push_back(static_cast<float>(node['cam2world'].size()));
+    
+    class TrafficLightUnprotectedRightTurnStageStop : public Stage {
+ public:
+  explicit TrafficLightUnprotectedRightTurnStageStop(
+      const ScenarioConfig::StageConfig& config)
+      : Stage(config) {}
     }
     
-    static bool needToRelocate(SectionRef S) {
-  if (!getSectionAddress(S))
-    return false;
+    Status ControllerAgent::Init(const ControlConf *control_conf) {
+  RegisterControllers(control_conf);
+  CHECK(InitializeConf(control_conf).ok()) << 'Failed to initialize config.';
+  for (auto &controller : controller_list_) {
+    if (controller == nullptr) {
+      return Status(ErrorCode::CONTROL_INIT_ERROR, 'Controller is null.');
     }
+    if (!controller->Init(control_conf_).ok()) {
+      AERROR << 'Controller <' << controller->Name() << '> init failed!';
+      return Status(ErrorCode::CONTROL_INIT_ERROR,
+                    'Failed to init Controller:' + controller->Name());
+    }
+    AINFO << 'Controller <' << controller->Name() << '> init done!';
+  }
+  return Status::OK();
+}
