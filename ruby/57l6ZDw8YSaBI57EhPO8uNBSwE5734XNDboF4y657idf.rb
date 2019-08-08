40e37e539aa30560e6d5f2a5f5d97e72c25465be
SUNsung <weatherius@gmail.com>
2019-08-08 11:07:23 +0300

    Rewrite [57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb] File in: 2019-08-08 11:07:23

--- ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
+++ ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
@@ -1,50 +1,82 @@
 
         
-              def self.symbols
-        type_klass::SET.symbols
+              assert_match %r{Finished in.*\n2 runs, 2 assertions}, output
+      assert_no_match 'create_table(:users)', output
+    end
+    
+        class ::HelperyTestController < AbstractHelpers
+      clear_helpers
+    end
+    
+              path
+        end
+      end
+    
+      if fast_forwards.include?(name)
+    puts '#{name}: TEMP IGNORE'
+    unavailable.push(Struct::Blog.new(name, web_url, nil))
+    next
+  end
+    
+          set_preference args[0], args[1] if args.size == 2
+    end
+    
+            it 'sets the acceptance status errors' do
+          expect(return_item.acceptance_status_errors).to eq validator_errors
+        end
+      end
+    
+              # regression for #3287
+          it 'doesn't select backend rates even if they're more affordable' do
+            expect(subject.shipping_rates(package).map(&:selected)).to eq [true]
+          end
+        end
+    
+            def option_value_params
+          params.require(:option_value).permit(permitted_option_value_attributes)
+        end
       end
     end
   end
 end
 
     
-    end
-
+            def create
+          authorize! :create, Property
+          @property = Spree::Property.new(property_params)
+          if @property.save
+            respond_with(@property, status: 201, default_template: :show)
+          else
+            invalid_resource!(@property)
+          end
+        end
     
-        def handle_bounce(e)
-      # never reply to bounced emails
-      log_email_process_failure(@mail, e)
-      set_incoming_email_rejection_message(@receiver.incoming_email, I18n.t('emails.incoming.errors.bounced_email_error'))
-    end
+            def update
+          @return_authorization = order.return_authorizations.accessible_by(current_ability, :update).find(params[:id])
+          if @return_authorization.update(return_authorization_params)
+            respond_with(@return_authorization, default_template: :show)
+          else
+            invalid_resource!(@return_authorization)
+          end
+        end
     
-      # register callbacks for detailed request loggers called on every request
-  # example:
-  #
-  # Middleware::RequestTracker.detailed_request_logger(->|env, data| do
-  #   # do stuff with env and data
-  # end
-  def self.register_detailed_request_logger(callback)
-    MethodProfiler.ensure_discourse_instrumentation!
-    (@@detailed_request_loggers ||= []) << callback
-  end
-    
-        if !self.errors.full_messages.empty?
-      raise ActiveRecord::RecordNotSaved.new(self.errors.full_messages.to_sentence)
-    end
-    
-          it 'should not mark image uploads as private' do
-        fname = 'logo.jpg'
-        upload = UploadCreator.new(file_from_fixtures(fname), fname).create_for(user.id)
-        stored_upload = Upload.last
-    
-              s3_bucket.expects(:object).with('uploads/tombstone/second/original/1X/#{upload.sha1}.png').returns(s3_object)
-          s3_object.expects(:copy_from).with(copy_source: 's3-upload-bucket/uploads/second/original/1X/#{upload.sha1}.png')
-          s3_bucket.expects(:object).with('uploads/second/original/1X/#{upload.sha1}.png').returns(s3_object)
-          s3_object.expects(:delete)
-    
-          # Install the gems to make them available locally when bundler does his local resolution
-      post_install_messages = []
-      pack.gems.each do |packed_gem|
-        PluginManager.ui.debug('Installing, #{packed_gem.name}, version: #{packed_gem.version} file: #{packed_gem.file}')
-        post_install_messages << LogStash::PluginManager::GemInstaller::install(packed_gem.file, packed_gem.plugin?)
+          context 'and :python_package_name_prefix is set' do
+        it 'should prefix the package name appropriately' do
+          prefix = 'whoa'
+          subject.attributes[:python_package_name_prefix] = prefix
+          subject.input(example_dir)
+          insist { subject.name } == '#{prefix}-example'
+        end
       end
+    end
+    
+        if info.include?('repository')
+      self.url = info['repository']['url']
+    else
+      self.url = 'https://npmjs.org/package/#{self.name}'
+    end
+    
+      def to_s_extension; 'pkg'; end
+    
+        # Arch has no notion of vendor, so...
+    #self.vendor =
