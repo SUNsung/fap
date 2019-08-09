ea877f884ec649f44971c7508916a34900ab8464
SUNsung <weatherius@gmail.com>
2019-08-09 20:30:41 +0300

    Rewrite [59anTVOkBveI59LGCgTipX4A59h.ZYBhh56FE59rmj.rb] File in: 2019-08-09 20:30:41

--- ruby/59anTVOkBveI59LGCgTipX4A59h.ZYBhh56FE59rmj.rb
+++ ruby/59anTVOkBveI59LGCgTipX4A59h.ZYBhh56FE59rmj.rb
@@ -1,75 +1,17 @@
 
         
-          config.on_event :test_case_started do |event|
-    f.print_feature_element_name(event.test_case)
-    f.before_feature_element(event.test_case)
+          # GET /resource/sign_in
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
   end
     
-    module Jekyll
-  module Deprecator
-    extend self
+    require 'minitest/autorun'
     
-          def grouped_array(groups)
-        groups.each_with_object([]) do |item, array|
-          array << {
-            'name'  => item.first,
-            'items' => item.last,
-            'size'  => item.last.size,
-          }
-        end
+          # Configure default email options
+      def devise_mail(record, action, opts = {}, &block)
+        initialize_from_record(record)
+        mail headers_for(action, opts), &block
       end
-    end
-  end
-end
-
-    
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
-      end
-    end
-    
-    module Docs
-  class PageDb
-    attr_reader :pages
-    
-            css('pre.prettyprint').each do |node|
-          node.content = node.content.strip
-          node['data-language'] = 'dart' if node['class'].include?('dart')
-          node['data-language'] = 'html' if node.content.start_with?('<')
-          node.remove_attribute('class')
-        end
-    
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-    module Jekyll
-    
-    end
-Liquid::Template.register_filter OctopressLiquidFilters
