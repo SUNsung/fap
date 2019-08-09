725b7fff990fae7a35d162dbf4a6ae97b63c5b9e
SUNsung <weatherius@gmail.com>
2019-08-09 06:02:05 +0300

    Rewrite [57DLnEgISbFOo570zfwfynrsF257T43r64h2QYk57oc3.cpp] File in: 2019-08-09 06:02:04

--- cccccc/57DLnEgISbFOo570zfwfynrsF257T43r64h2QYk57oc3.cpp
+++ cccccc/57DLnEgISbFOo570zfwfynrsF257T43r64h2QYk57oc3.cpp
@@ -1,78 +1,351 @@
 
         
-        static void create_sockets(SOCKET sv[2]) {
-  SOCKET svr_sock = INVALID_SOCKET;
-  SOCKET lst_sock = INVALID_SOCKET;
-  SOCKET cli_sock = INVALID_SOCKET;
-  SOCKADDR_IN addr;
-  int addr_len = sizeof(addr);
+          void visitTupleInst(TupleInst *Inst) {
+    getBuilder().setCurrentDebugScope(getOpScope(Inst->getDebugScope()));
+    auto elements = getOpValueArray<8>(Inst->getElements());
+    auto tupleTy = getOpType(Inst->getType());
+    for (size_t i = 0, size = Inst->getElements().size(); i < size; ++i) {
+      auto elementTy = tupleTy.getTupleElementType(i);
+      if (Inst->getElement(i)->getType() != elementTy) {
+        elements[i] =
+            createCast(getOpLocation(Inst->getLoc()), elements[i], elementTy);
+      }
+    }
+    recordClonedInstruction(
+        Inst, getBuilder().createTuple(getOpLocation(Inst->getLoc()),
+                                       getOpType(Inst->getType()), elements));
+  }
+    
+      std::string error;
+  auto isUptodateOpt = parentUnitWriter.isUnitUpToDateForOutputFile(/*FilePath=*/filename,
+                                                                /*TimeCompareFilePath=*/filename, error);
+  if (!isUptodateOpt.hasValue()) {
+    diags.diagnose(SourceLoc(), diag::error_index_failed_status_check, error);
+    return true;
+  }
+  if (*isUptodateOpt)
+    return false;
+    
+    /// Merge an attribute's availability with an existing inferred availability
+/// so that the new inferred availability is at least as available as
+/// the attribute requires.
+static void mergeWithInferredAvailability(const AvailableAttr *Attr,
+                                          InferredAvailability &Inferred) {
+  Inferred.PlatformAgnostic
+    = static_cast<PlatformAgnosticAvailabilityKind>(
+      std::max(static_cast<unsigned>(Inferred.PlatformAgnostic),
+               static_cast<unsigned>(Attr->getPlatformAgnosticAvailability())));
     }
     
-    static void iomgr_platform_shutdown_background_closure(void) {}
-    
-      LOG_TEST_NAME('test_ltoa');
-    
-    
-    {}  // namespace grpc
-    
-    #include <grpcpp/security/credentials.h>
-    
-      void CompareService(const grpc::string& service) {
-    const protobuf::ServiceDescriptor* service_desc =
-        desc_pool_->FindServiceByName(service);
-    const protobuf::ServiceDescriptor* ref_service_desc =
-        ref_desc_pool_->FindServiceByName(service);
-    EXPECT_TRUE(service_desc != nullptr);
-    EXPECT_TRUE(ref_service_desc != nullptr);
-    EXPECT_EQ(service_desc->DebugString(), ref_service_desc->DebugString());
+      /// getRelease - Return a callable function for swift_release.
+  Constant *getRelease(CallInst *OrigI) {
+    if (Release)
+      return Release.get();
+    auto *ObjectPtrTy = getObjectPtrTy();
+    auto *VoidTy = Type::getVoidTy(getModule().getContext());
     }
     
-    #include <grpc/grpc.h>
-#include <grpcpp/channel.h>
-#include <grpcpp/client_context.h>
-#include <grpcpp/create_channel.h>
-#include <grpcpp/impl/server_builder_option.h>
-#include <grpcpp/impl/server_builder_plugin.h>
-#include <grpcpp/impl/server_initializer.h>
-#include <grpcpp/security/credentials.h>
-#include <grpcpp/security/server_credentials.h>
-#include <grpcpp/server.h>
-#include <grpcpp/server_builder.h>
-#include <grpcpp/server_context.h>
     
-    namespace swoole
-{
-void event_init(void)
-{
-//        SwooleG.main_reactor->setHandle(SwooleG.main_reactor, SW_FD_USER | SW_EVENT_READ, php_swoole_event_onRead);
-//        SwooleG.main_reactor->setHandle(SwooleG.main_reactor, SW_FD_USER | SW_EVENT_WRITE, php_swoole_event_onWrite);
-//        SwooleG.main_reactor->setHandle(SwooleG.main_reactor, SW_FD_USER | SW_EVENT_ERROR, php_swoole_event_onError);
+    {  /// This class provides a non-trivial .cxx_destruct method, but
+  /// its .cxx_construct is trivial.  For backwards compatibility,
+  /// when setting this flag, HasCXXStructors must be set as well.
+  HasCXXDestructorOnly = 0x00100
+};
+inline ObjCClassFlags &operator|=(ObjCClassFlags &lhs, ObjCClassFlags rhs) {
+  lhs = ObjCClassFlags(uint32_t(lhs) | uint32_t(rhs));
+  return lhs;
 }
+inline ObjCClassFlags operator|(ObjCClassFlags lhs, ObjCClassFlags rhs) {
+  return (lhs |= rhs);
+}
+    
+      DiagnosticSuppression(const DiagnosticSuppression &) = delete;
+  DiagnosticSuppression &operator=(const DiagnosticSuppression &) = delete;
+    
+    public:
+  /// See \ref SourceFileDepGraphNode::SourceFileDepGraphNode().
+  DependencyKey()
+      : kind(NodeKind::kindCount), aspect(DeclAspect::aspectCount), context(),
+        name() {}
+    
+    #ifndef SWIFT_BASIC_PRIMARYSPECIFICPATHS_H
+#define SWIFT_BASIC_PRIMARYSPECIFICPATHS_H
+    
+    #include <delayimp.h>
+#include <string.h>
+    
+      GetRenderProcessHosts(rphs);
+  for (it = rphs.begin(); it != rphs.end(); it++) {
+    RenderProcessHost* rph = *it;
+    DCHECK(rph != NULL);
     }
     
-            std::string addr1 = System::gethostbyname('www.baidu.com', AF_INET);
-        ASSERT_NE(addr1, '');
     
-            make_pair([](void *arg)
-        {
-            auto chan = (Channel *) arg;
-            int i = 1;
-            bool ret;
+void Base::Call(const std::string& method, const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  NOTREACHED() << 'Uncatched call in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
+}
+    
+       bool IsItemForCommandIdDynamic(int command_id) const override;
+   base::string16 GetLabelForCommandId(int command_id) const override;
+   bool GetIconForCommandId(int command_id,
+                                   gfx::Image* icon) const override;
+    
+    namespace nwapi {
     }
     
-        inline void* pop_data()
-    {
-        if (data_queue.size() == 0)
-        {
-            return nullptr;
+    aura::Window* Menu::GetActiveNativeView(content::RenderFrameHost* rfh) {
+  content::WebContents* web_contents =
+    content::WebContents::FromRenderFrameHost(rfh);
+  if (!web_contents) {
+    LOG(ERROR) << 'Menu: couldn't find WebContents';
+    return NULL;
+  }
+  return web_contents->GetFullscreenRenderWidgetHostView()
+             ? web_contents->GetFullscreenRenderWidgetHostView()
+                   ->GetNativeView()
+             : web_contents->GetNativeView();
+}
+    
+    
+    {  gtk_widget_show(menu_item_);
+  g_object_ref_sink(G_OBJECT(menu_item_));
+} 
+    
+          if (!(data.raw.get() && *(data.raw))) {
+        if (data.type == TYPE_PNG) {
+          encoded_image_base64.insert(0, kPNGDataUriPrefix);
+        } else {
+          DCHECK(data.type == TYPE_JPEG);
+          encoded_image_base64.insert(0, kJPEGDataUriPrefix);
         }
-        void *data = data_queue.front();
-        data_queue.pop();
-        return data;
+      }
+    
+    /// <summary>
+/// Initializes the singleton application object.  This is the first line of authored code
+/// executed, and as such is the logical equivalent of main() or WinMain().
+/// </summary>
+App::App()
+{
+    InitializeComponent();
+    Suspending += ref new SuspendingEventHandler(this, &App::OnSuspending);
+}
+    
+    
+    {
+    {            result = tempRat.ToString(radix, m_nFE, m_precision);
+        }
+        catch (uint32_t)
+        {
+        }
     }
     
-        void copy(void *_data, size_t _length)
+    class CCalcEngine
+{
+public:
+    CCalcEngine(
+        bool fPrecedence,
+        bool fIntegerMode,
+        CalculationManager::IResourceProvider* const pResourceProvider,
+        __in_opt ICalcDisplay* pCalcDisplay,
+        __in_opt std::shared_ptr<IHistoryDisplay> pHistoryDisplay);
+    void ProcessCommand(OpCode wID);
+    void DisplayError(uint32_t nError);
+    std::unique_ptr<CalcEngine::Rational> PersistedMemObject();
+    void PersistedMemObject(CalcEngine::Rational const& memObject);
+    bool FInErrorState()
     {
-        alloc(_length);
-        memcpy(buffer, _data, _length);
+        return m_bError;
     }
+    bool FInRecordingState()
+    {
+        return m_bRecord;
+    }
+    void SettingsChanged();
+    bool IsCurrentTooBigForTrig();
+    int GetCurrentRadix();
+    std::wstring GetCurrentResultForRadix(uint32_t radix, int32_t precision);
+    void ChangePrecision(int32_t precision)
+    {
+        m_precision = precision;
+        ChangeConstants(m_radix, precision);
+    }
+    std::wstring GroupDigitsPerRadix(std::wstring_view numberString, uint32_t radix);
+    std::wstring GetStringForDisplay(CalcEngine::Rational const& rat, uint32_t radix);
+    void UpdateMaxIntDigits();
+    wchar_t DecimalSeparator() const;
+    }
+    
+    //-----------------------------------------------------------------------------
+//
+//    FUNCTION: numtoi32
+//
+//    ARGUMENTS: number input and base of that number.
+//
+//    RETURN: int32_t
+//
+//    DESCRIPTION: returns the int32_t representation of the
+//    number input.  Assumes that the number is really in the
+//    base   claimed.
+//
+//-----------------------------------------------------------------------------
+int32_t numtoi32(_In_ PNUMBER pnum, uint32_t radix)
+{
+    int32_t lret = 0;
+    }
+    
+    //-----------------------------------------------------------------------------
+//
+//  NUMBER type is a representation of a generic sized generic radix number
+//
+//-----------------------------------------------------------------------------
+    
+    namespace CalcEngine
+{
+    Number::Number() noexcept
+        : Number(1, 0, { 0 })
+    {
+    }
+    }
+    
+    unsigned int CalculatorHistory::AddItem(_In_ shared_ptr<HISTORYITEM> const& spHistoryItem)
+{
+    if (m_historyItems.size() >= m_maxHistorySize)
+    {
+        m_historyItems.erase(m_historyItems.begin());
+    }
+    }
+    
+    int main()
+{
+    // create JSON object
+    json object =
+    {
+        {'the good', 'il buono'},
+        {'the bad', 'il cattivo'},
+        {'the ugly', 'il brutto'}
+    };
+    }
+    
+      Options options = CurrentOptions();
+  options.compaction_filter_factory.reset(filter);
+  options.compression = kNoCompression;
+  options.level0_file_num_compaction_trigger = 2;
+  CreateAndReopenWithCF({'pikachu'}, options);
+    
+    // WriteController is controlling write stalls in our write code-path. Write
+// stalls happen when compaction can't keep up with write rate.
+// All of the methods here (including WriteControllerToken's destructors) need
+// to be called while holding DB mutex
+class WriteController {
+ public:
+  explicit WriteController(uint64_t _delayed_write_rate = 1024u * 1024u * 32u,
+                           int64_t low_pri_rate_bytes_per_sec = 1024 * 1024)
+      : total_stopped_(0),
+        total_delayed_(0),
+        total_compaction_pressure_(0),
+        bytes_left_(0),
+        last_refill_time_(0),
+        low_pri_rate_limiter_(
+            NewGenericRateLimiter(low_pri_rate_bytes_per_sec)) {
+    set_max_delayed_write_rate(_delayed_write_rate);
+  }
+  ~WriteController() = default;
+    }
+    
+    int main() {
+  // open DB
+  Options options;
+  options.create_if_missing = true;
+  DB* db;
+  Status s = DB::Open(options, kDBPath, &db);
+  assert(s.ok());
+    }
+    
+      // atomically apply a set of updates
+  {
+    WriteBatch batch;
+    batch.Delete('key1');
+    batch.Put('key2', value);
+    s = db->Write(WriteOptions(), &batch);
+  }
+    
+      txn->Rollback();
+    
+    // Move all L0 files to target_level skipping compaction.
+// This operation succeeds only if the files in L0 have disjoint ranges; this
+// is guaranteed to happen, for instance, if keys are inserted in sorted
+// order. Furthermore, all levels between 1 and target_level must be empty.
+// If any of the above condition is violated, InvalidArgument will be
+// returned.
+Status PromoteL0(DB* db, ColumnFamilyHandle* column_family,
+                 int target_level = 1);
+    
+    
+    { protected:
+  virtual ~Snapshot();
+};
+    
+    #include <boost/filesystem.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+    
+    void ConfigRefreshRunner::start() {
+  while (!interrupted()) {
+    // Cool off and time wait the configured period.
+    // Apply this interruption initially as at t=0 the config was read.
+    pause(std::chrono::seconds(refresh_sec_));
+    // Since the pause occurs before the logic, we need to check for an
+    // interruption request.
+    if (interrupted()) {
+      return;
+    }
+    }
+    }
+    
+      /**
+   * @brief Helper to access config parsers via the registry
+   *
+   * This may return a nullptr if an exception is thrown attempting to retrieve
+   * the requested config parser.
+   *
+   * @param parser is the string name of the parser that you want
+   *
+   * @return a shared pointer to the config parser plugin
+   */
+  static const std::shared_ptr<ConfigParserPlugin> getParser(
+      const std::string& parser);
+    
+    
+    {  std::default_random_engine generator;
+  generator.seed(static_cast<unsigned int>(
+      std::chrono::high_resolution_clock::now().time_since_epoch().count()));
+  std::uniform_int_distribution<size_t> distribution(min_value, max_value);
+  return distribution(generator);
+}
+    
+    
+    {} // namespace osquery
+
+    
+    Expected<int32_t, DatabaseError> Database::getInt32Or(
+    const std::string& domain,
+    const std::string& key,
+    const int32_t default_value) {
+  auto result = getInt32(domain, key);
+  if (!result && result.getError() == DatabaseError::KeyNotFound) {
+    return default_value;
+  }
+  return result;
+}
+    
+      RocksdbDatabase(std::string name) = delete;
+  explicit RocksdbDatabase(std::string name, std::string path)
+      : Database(std::move(name)), path_(std::move(path)){};
+  ~RocksdbDatabase() override {}
+    
+      /// The line of the file emitting the status log.
+  size_t line;
