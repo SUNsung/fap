a5bd72ca8d848b931025586ac3d1f66171cae94f
SUNsung <weatherius@gmail.com>
2019-08-09 10:22:38 +0300

    Rewrite [55XeNoun9.aUw554.NZdLfDGj.55VmWyCn2BZFE550cp.cpp] File in: 2019-08-09 10:22:38

--- cccccc/55XeNoun9.aUw554.NZdLfDGj.55VmWyCn2BZFE550cp.cpp
+++ cccccc/55XeNoun9.aUw554.NZdLfDGj.55VmWyCn2BZFE550cp.cpp
@@ -1,183 +1,106 @@
 
         
-        #ifndef CHROME_BROWSER_UI_VIEWS_FRAME_GLOBAL_MENU_BAR_REGISTRAR_X11_H_
-#define CHROME_BROWSER_UI_VIEWS_FRAME_GLOBAL_MENU_BAR_REGISTRAR_X11_H_
-    
-    ObjectTemplateBuilder& ObjectTemplateBuilder::MakeDestroyable() {
-  SetMethod('destroy', base::Bind(internal::Destroyable::Destroy));
-  SetMethod('isDestroyed', base::Bind(internal::Destroyable::IsDestroyed));
-  return *this;
-}
-    
-    bool Converter<unsigned long>::FromV8(  // NOLINT(runtime/int)
-    v8::Isolate* isolate,
-    v8::Local<v8::Value> val,
-    unsigned long* out) {  // NOLINT(runtime/int)
-  if (!val->IsNumber())
-    return false;
-  return FromMaybe(val->IntegerValue(isolate->GetCurrentContext()), out);
-}
-#endif
-    
-    // This specialization allows people to construct function templates directly if
-// they need to do fancier stuff.
-template <>
-struct CallbackTraits<v8::Local<v8::FunctionTemplate>> {
-  static v8::Local<v8::FunctionTemplate> CreateTemplate(
-      v8::Local<v8::FunctionTemplate> templ) {
-    return templ;
-  }
-};
-    
-    #include 'native_mate/dictionary.h'
-#include 'native_mate/scoped_persistent.h'
-    
-    
-    {}  // namespace caffe
-#endif  // USE_HDF5
+        #endif // BITCOIN_QT_NETWORKSTYLE_H
 
     
-    template <>
-void hdf5_load_nd_dataset<float>(hid_t file_id, const char* dataset_name_,
-        int min_dim, int max_dim, Blob<float>* blob, bool reshape) {
-  hdf5_load_nd_dataset_helper(file_id, dataset_name_, min_dim, max_dim, blob,
-                              reshape);
-  herr_t status = H5LTread_dataset_float(
-    file_id, dataset_name_, blob->mutable_cpu_data());
-  CHECK_GE(status, 0) << 'Failed to read float dataset ' << dataset_name_;
+    namespace Ui {
+    class OpenURIDialog;
 }
     
-      char label_i;
-  char label_j;
-  char* pixels = new char[2 * rows * cols];
-  std::string value;
-    
-      // Dialogs
-  void requestBulkOperation(
-      QSharedPointer<RedisClient::Connection> connection, int dbIndex,
-      BulkOperations::Manager::Operation op, QRegExp keyPattern,
-      BulkOperations::AbstractOperation::OperationCallback callback);
-    
-      m_progress = 0;
-  m_errors.clear();
-    
-      series->append(QDateTime::currentDateTime().toMSecsSinceEpoch(), value);
-    
-    #define MOCK_METHOD0(m, ...) GMOCK_METHOD0_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD1(m, ...) GMOCK_METHOD1_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD2(m, ...) GMOCK_METHOD2_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD3(m, ...) GMOCK_METHOD3_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD4(m, ...) GMOCK_METHOD4_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD5(m, ...) GMOCK_METHOD5_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD6(m, ...) GMOCK_METHOD6_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD7(m, ...) GMOCK_METHOD7_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD8(m, ...) GMOCK_METHOD8_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD9(m, ...) GMOCK_METHOD9_(, , , m, __VA_ARGS__)
-#define MOCK_METHOD10(m, ...) GMOCK_METHOD10_(, , , m, __VA_ARGS__)
+    private:
+    Ui::SignVerifyMessageDialog *ui;
+    WalletModel *model;
+    const PlatformStyle *platformStyle;
     
     
-// Google Test - The Google C++ Testing and Mocking Framework
-//
-// This file implements a universal value printer that can print a
-// value of any type T:
-//
-//   void ::testing::internal::UniversalPrinter<T>::Print(value, ostream_ptr);
-//
-// A user can teach this function how to print a class type T by
-// defining either operator<<() or PrintTo() in the namespace that
-// defines T.  More specifically, the FIRST defined function in the
-// following list will be used (assuming T is defined in namespace
-// foo):
-//
-//   1. foo::PrintTo(const T&, ostream*)
-//   2. operator<<(ostream&, const T&) defined in either foo or the
-//      global namespace.
-//
-// However if T is an STL-style container then it is printed element-wise
-// unless foo::PrintTo(const T&, ostream*) is defined. Note that
-// operator<<() is ignored for container types.
-//
-// If none of the above is defined, it will print the debug string of
-// the value if it is a protocol buffer, or print the raw bytes in the
-// value otherwise.
-//
-// To aid debugging: when T is a reference type, the address of the
-// value is also printed; when T is a (const) char pointer, both the
-// pointer value and the NUL-terminated string it points to are
-// printed.
-//
-// We also provide some convenient wrappers:
-//
-//   // Prints a value to a string.  For a (const or not) char
-//   // pointer, the NUL-terminated string (but not the pointer) is
-//   // printed.
-//   std::string ::testing::PrintToString(const T& value);
-//
-//   // Prints a value tersely: for a reference type, the referenced
-//   // value (but not the address) is printed; for a (const or not) char
-//   // pointer, the NUL-terminated string (but not the pointer) is
-//   // printed.
-//   void ::testing::internal::UniversalTersePrint(const T& value, ostream*);
-//
-//   // Prints value using the type inferred by the compiler.  The difference
-//   // from UniversalTersePrint() is that this function prints both the
-//   // pointer and the NUL-terminated string for a (const or not) char pointer.
-//   void ::testing::internal::UniversalPrint(const T& value, ostream*);
-//
-//   // Prints the fields of a tuple tersely to a string vector, one
-//   // element for each field. Tuple support must be enabled in
-//   // gtest-port.h.
-//   std::vector<string> UniversalTersePrintTupleFieldsToStrings(
-//       const Tuple& value);
-//
-// Known limitation:
-//
-// The print primitives print the elements of an STL-style container
-// using the compiler-inferred type of *iter where iter is a
-// const_iterator of the container.  When const_iterator is an input
-// iterator but not a forward iterator, this inferred type may not
-// match value_type, and the print output may be incorrect.  In
-// practice, this is rarely a problem as for most containers
-// const_iterator is a forward iterator.  We'll fix this if there's an
-// actual need for it.  Note that this fix cannot rely on value_type
-// being defined as many user-defined container types don't have
-// value_type.
+    {    CRIPEMD160();
+    CRIPEMD160& Write(const unsigned char* data, size_t len);
+    void Finalize(unsigned char hash[OUTPUT_SIZE]);
+    CRIPEMD160& Reset();
+};
     
-    // Instantiates FloatingPointTest for testing *_FLOAT_EQ.
-typedef FloatingPointTest<float> FloatTest;
-    
-    // Returns whether the given character is a valid path separator.
-static bool IsPathSeparator(char c) {
-#if GTEST_HAS_ALT_PATH_SEP_
-  return (c == kPathSeparator) || (c == kAlternatePathSeparator);
-#else
-  return c == kPathSeparator;
-#endif
-}
-    
-    # endif  // _NDEBUG
-    
-    TEST(ValidateRegexTest, ReturnsTrueForValid) {
-  EXPECT_TRUE(ValidateRegex(''));
-  EXPECT_TRUE(ValidateRegex('a'));
-  EXPECT_TRUE(ValidateRegex('.*'));
-  EXPECT_TRUE(ValidateRegex('^a_+'));
-  EXPECT_TRUE(ValidateRegex('^a\\t\\&?'));
-  EXPECT_TRUE(ValidateRegex('09*$'));
-  EXPECT_TRUE(ValidateRegex('^Z$'));
-  EXPECT_TRUE(ValidateRegex('a\\^Z\\$\\(\\)\\|\\[\\]\\{\\}'));
-}
-    
-    // Prints a (const) wchar_t array of 'len' elements, starting at address
-// 'begin'.
-void UniversalPrintArray(const wchar_t* begin, size_t len, ostream* os) {
-  UniversalPrintCharArray(begin, len, os);
+    Status DumpDescriptor(Env* env, const std::string& fname, WritableFile* dst) {
+  return PrintLogContents(env, fname, VersionEditPrinter, dst);
 }
     
     
-    {    // Now, we have i <= n/i < n.
-    // If n is divisible by i, n is not prime.
-    if (n % i == 0) return false;
+    {}  // namespace leveldb
+    
+    
+    {  // Changes *key to a short string >= *key.
+  // Simple comparator implementations may return with *key unchanged,
+  // i.e., an implementation of this method that does nothing is correct.
+  virtual void FindShortSuccessor(std::string* key) const = 0;
+};
+    
+      // Create a slice that refers to the contents of 's'
+  Slice(const std::string& s) : data_(s.data()), size_(s.size()) {}
+    
+    Status Footer::DecodeFrom(Slice* input) {
+  const char* magic_ptr = input->data() + kEncodedLength - 8;
+  const uint32_t magic_lo = DecodeFixed32(magic_ptr);
+  const uint32_t magic_hi = DecodeFixed32(magic_ptr + 4);
+  const uint64_t magic = ((static_cast<uint64_t>(magic_hi) << 32) |
+                          (static_cast<uint64_t>(magic_lo)));
+  if (magic != kTableMagicNumber) {
+    return Status::Corruption('not an sstable (bad magic number)');
+  }
+    }
+    
+      if (s.ok()) {
+    // We've successfully read the footer and the index block: we're
+    // ready to serve requests.
+    Block* index_block = new Block(index_block_contents);
+    Rep* rep = new Table::Rep;
+    rep->options = options;
+    rep->file = file;
+    rep->metaindex_handle = footer.metaindex_handle();
+    rep->index_block = index_block;
+    rep->cache_id = (options.block_cache ? options.block_cache->NewId() : 0);
+    rep->filter_data = nullptr;
+    rep->filter = nullptr;
+    *table = new Table(rep);
+    (*table)->ReadMeta(footer);
   }
     
-    #include <stdio.h>
+      rv_ = gnutls_init(&sslSession_, flags);
+#else  // GNUTLS_VERSION_NUMBER >= 0x030000
+  rv_ = gnutls_init(&sslSession_, tlsContext_->getSide() == TLS_CLIENT
+                                      ? GNUTLS_CLIENT
+                                      : GNUTLS_SERVER);
+#endif // GNUTLS_VERSION_NUMBER >= 0x030000
+  if (rv_ != GNUTLS_E_SUCCESS) {
+    return TLS_ERR_ERROR;
+  }
+#ifdef A2_DISABLE_OCSP
+  if (tlsContext_->getSide() == TLS_CLIENT) {
+    // Enable session ticket extension manually because of
+    // GNUTLS_NO_EXTENSIONS.
+    rv_ = gnutls_session_ticket_enable_client(sslSession_);
+    if (rv_ != GNUTLS_E_SUCCESS) {
+      return TLS_ERR_ERROR;
+    }
+  }
+#endif // A2_DISABLE_OCSP
+    
+    bool WinTLSContext::addTrustedCACertFile(const std::string& certfile)
+{
+  A2_LOG_WARN('TLS CA bundle files are not supported. '
+              'The system trust store will be used.');
+  return false;
+}
+    
+    void DHTReplaceNodeTask::onReceived(const DHTPingReplyMessage* message)
+{
+  A2_LOG_INFO(fmt('ReplaceNode: Ping reply received from %s.',
+                  message->getRemoteNode()->toString().c_str()));
+  setFinished(true);
+}
+    
+    namespace aria2 {
+    }
+    
+    
+    // ============
+    // string types
+    // ============
