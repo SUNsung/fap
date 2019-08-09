c5f6ba038dd73b7f8e3291f7b2f276c9ebadeafe
SUNsung <weatherius@gmail.com>
2019-08-09 21:42:44 +0300

    Rewrite [57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py] File in: 2019-08-09 21:42:44

--- pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
+++ pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
@@ -1,133 +1,77 @@
 
         
-            # noinspection PyMethodOverriding
-    def get_auth(self, username, password):
-        return requests.auth.HTTPDigestAuth(username, password)
-
+        
+class TimeField(BaseTemporalField):
+    widget = TimeInput
+    input_formats = formats.get_format_lazy('TIME_INPUT_FORMATS')
+    default_error_messages = {
+        'invalid': _('Enter a valid time.')
+    }
     
-            def get_auth(self, username=None, password=None):
-            assert username is None
-            assert password is None
-            assert self.raw_auth == BASIC_AUTH_HEADER_VALUE
-            return basic_auth(self.raw_auth)
+        def test_inline_editable_pk(self):
+        response = self.client.get(reverse('admin:admin_inlines_author_add'))
+        self.assertContains(
+            response,
+            '<input class='vIntegerField' id='id_editablepkbook_set-0-manual_pk' '
+            'name='editablepkbook_set-0-manual_pk' type='number'>',
+            html=True, count=1
+        )
+        self.assertContains(
+            response,
+            '<input class='vIntegerField' id='id_editablepkbook_set-2-0-manual_pk' '
+            'name='editablepkbook_set-2-0-manual_pk' type='number'>',
+            html=True, count=1
+        )
     
-        config['implicit_content_type'] = 'json'
-    config.save()
-    config.load()
-    assert 'implicit_content_type' not in config
-    assert not config['default_options']
+        def test_object_at_custom_name_in_context_data(self):
+        # Checks 'pony' key presence in dict returned by get_context_date
+        test_view = views.CustomSingleObjectView()
+        test_view.context_object_name = 'pony'
+        context = test_view.get_context_data()
+        self.assertEqual(context['pony'], test_view.object)
     
+        def as_mysql(self, compiler, connection, **extra_context):
+        sql, params = super().as_sql(compiler, connection, **extra_context)
+        if self.output_field.get_internal_type() == 'DurationField':
+            sql = 'CAST(%s AS SIGNED)' % sql
+        return sql, params
     
-def test_default_headers_case_insensitive(httpbin):
+                try:
+                vtup = self.postgis_version_tuple()
+            except ProgrammingError:
+                raise ImproperlyConfigured(
+                    'Cannot determine PostGIS version for database '%s' '
+                    'using command 'SELECT postgis_lib_version()'. '
+                    'GeoDjango requires at least PostGIS version 2.2. '
+                    'Was the database created from a spatial database '
+                    'template?' % self.connection.settings_dict['NAME']
+                )
+            version = vtup[1:]
+        return version
+    
+        def __iter__(self):
+        'Iterate over each Feature in the Layer.'
+        # ResetReading() must be called before iteration is to begin.
+        capi.reset_reading(self._ptr)
+        for i in range(self.num_feat):
+            yield Feature(capi.get_next_feature(self._ptr), self)
+    
+        :param filepath: Optional filepath the the blns.txt file
+    :returns: The list of naughty strings
     '''
-    <https://github.com/jakubroztocil/httpie/issues/644>
-    '''
-    r = http(
-        '--debug',
-        '--print=H',
-        httpbin.url + '/post',
-        'CONTENT-TYPE:application/json-patch+json',
-        'a=b',
-    )
-    assert 'CONTENT-TYPE: application/json-patch+json' in r
-    assert 'Content-Type' not in r
     
+    This pattern aims to decouple the senders of a request from its
+receivers by allowing request to move through chained
+receivers until it is handled.
     
-@mock.patch('httpie.core.get_response')
-def test_error_traceback(get_response):
-    exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    with raises(ConnectionError):
-        main(['--ignore-stdin', '--traceback', 'www.google.com'])
-    
-    
-def test_follow_all_output_options_used_for_redirects(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 3
-    assert HTTP_OK not in r
-    
-    
-class HTTPBasicAuth(AuthBase):
-    '''Attaches HTTP Basic Authentication to the given Request object.'''
-    
-        :param str u_string: unicode string to check. Must be unicode
-        and not Python 2 `str`.
-    :rtype: bool
-    '''
-    assert isinstance(u_string, str)
-    try:
-        u_string.encode('ascii')
-        return True
-    except UnicodeEncodeError:
-        return False
-
-    
-            try:
-            conn = self.get_connection(request.url, proxies)
-        except LocationValueError as e:
-            raise InvalidURL(e, request=request)
-    
-        #Creating title page
-    with open('title.html', 'w') as f:
-        f.write(header + title_content)
-    
-        The following variables should be defined:
-        _HELPTEXT: A string describing what this plugin does
-        _DEFAULTS: A dictionary containing the options, defaults and meta information. The
-                   dictionary should be defined as:
-                       {<option_name>: {<metadata>}}
-    
-        The following keys are expected for the _DEFAULTS <metadata> dict:
-        datatype:  [required] A python type class. This limits the type of data that can be
-                   provided in the .ini file and ensures that the value is returned in the
-                   correct type to faceswap. Valid datatypes are: <class 'int'>, <class 'float'>,
-                   <class 'str'>, <class 'bool'>.
-        default:   [required] The default value for this option.
-        info:      [required] A string describing what this option does.
-        choices:   [optional] If this option's datatype is of <class 'str'> then valid
-                   selections can be defined here. This validates the option and also enables
-                   a combobox / radio option in the GUI.
-        gui_radio: [optional] If <choices> are defined, this indicates that the GUI should use
-                   radio buttons rather than a combobox to display this option.
-        min_max:   [partial] For <class 'int'> and <class 'float'> datatypes this is required
-                   otherwise it is ignored. Should be a tuple of min and max accepted values.
-                   This is used for controlling the GUI slider range. Values are not enforced.
-        rounding:  [partial] For <class 'int'> and <class 'float'> datatypes this is
-                   required otherwise it is ignored. Used for the GUI slider. For floats, this
-                   is the number of decimal places to display. For ints this is the step size.
-        fixed:     [optional] [train only]. Training configurations are fixed when the model is
-                   created, and then reloaded from the state file. Marking an item as fixed=False
-                   indicates that this value can be changed for existing models, and will override
-                   the value saved in the state file with the updated value in config. If not
-                   provided this will default to True.
+    *Examples in Python ecosystem:
+- Python's ast.NodeVisitor: https://github.com/python/cpython/blob/master/Lib/ast.py#L250
+which is then being used e.g. in tools like `pyflakes`.
+- `Black` formatter tool implements it's own: https://github.com/ambv/black/blob/master/black.py#L718
 '''
     
-        The following keys are expected for the _DEFAULTS <metadata> dict:
-        datatype:  [required] A python type class. This limits the type of data that can be
-                   provided in the .ini file and ensures that the value is returned in the
-                   correct type to faceswap. Valid datatypes are: <class 'int'>, <class 'float'>,
-                   <class 'str'>, <class 'bool'>.
-        default:   [required] The default value for this option.
-        info:      [required] A string describing what this option does.
-        choices:   [optional] If this option's datatype is of <class 'str'> then valid
-                   selections can be defined here. This validates the option and also enables
-                   a combobox / radio option in the GUI.
-        gui_radio: [optional] If <choices> are defined, this indicates that the GUI should use
-                   radio buttons rather than a combobox to display this option.
-        min_max:   [partial] For <class 'int'> and <class 'float'> datatypes this is required
-                   otherwise it is ignored. Should be a tuple of min and max accepted values.
-                   This is used for controlling the GUI slider range. Values are not enforced.
-        rounding:  [partial] For <class 'int'> and <class 'float'> datatypes this is
-                   required otherwise it is ignored. Used for the GUI slider. For floats, this
-                   is the number of decimal places to display. For ints this is the step size.
-        fixed:     [optional] [train only]. Training configurations are fixed when the model is
-                   created, and then reloaded from the state file. Marking an item as fixed=False
-                   indicates that this value can be changed for existing models, and will override
-                   the value saved in the state file with the updated value in config. If not
-                   provided this will default to True.
-'''
+        graphic.render()
+    
+    
+class ItalicWrapper(TextTag):
+    '''Wraps a tag in <i>'''
