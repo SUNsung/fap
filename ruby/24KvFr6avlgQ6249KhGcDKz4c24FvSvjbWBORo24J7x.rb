22f991abf7d7943e1a205c155e758d5bb8890b4e
SUNsung <weatherius@gmail.com>
2019-08-10 15:26:30 +0300

    Rewrite [24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb] File in: 2019-08-10 15:26:29

--- ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
+++ ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
@@ -1,78 +1,165 @@
 
         
-                # if the guest has a guest path scrubber capability, use it
-        if machine.guest.capability?(:rsync_scrub_guestpath)
-          guestpath = machine.guest.capability(:rsync_scrub_guestpath, opts)
-        end
+        #############################################################################
+#
+# Standard tasks
+#
+#############################################################################
     
-      let(:guest)   { double('guest') }
-  let(:machine) { iso_env.machine(iso_env.machine_names[0], :dummy) }
+    # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
     
-        it 'returns the updated box info within constraints' do
-      metadata = Vagrant::BoxMetadata.new(StringIO.new(<<-RAW))
-      {
-        'name': 'foo',
-        'versions': [
-          {
-            'version': '1.0'
-          },
-          {
-            'version': '1.1',
-            'providers': [
-              {
-                'name': 'virtualbox',
-                'url': 'bar'
-              }
-            ]
-          },
-          {
-            'version': '1.4',
-            'providers': [
-              {
-                'name': 'virtualbox',
-                'url': 'bar'
-              }
-            ]
-          }
-        ]
-      }
-      RAW
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-        it 'returns true if installed' do
-      expect(machine.communicate).to receive(:test).
-        with(command, sudo: true).and_return(true)
-      subject.chef_installed(machine, 'chef_solo', version)
+    task :gemspec => 'rack-protection.gemspec'
+task :default => :spec
+task :test    => :spec
+
+    
+          def masked_token?(token)
+        token.length == TOKEN_LENGTH * 2
+      end
+    
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
     end
     
-    describe VagrantPlugins::Chef::Cap::Windows::ChefInstalled do
-  include_context 'unit'
+      describe '#new_identifier' do
+    let(:source) do
+      'alias foo bar'
+    end
     
-            # Create a new driver instance
-        #
-        # @param [Vagrant::Machine] machine Machine instance for this driver
-        def initialize(machine)
-          if !Vagrant::Util::Which.which('docker-compose')
-            raise Errors::DockerComposeNotInstalledError
-          end
-          super()
-          @machine = machine
-          @data_directory = Pathname.new(machine.env.local_data_path).
-            join('docker-compose')
-          @data_directory.mkpath
-          @logger = Log4r::Logger.new('vagrant::docker::driver::compose')
-          @compose_lock = Mutex.new
-          @logger.debug('Docker compose driver initialize for machine `#{@machine.name}` (`#{@machine.id}`)')
-          @logger.debug('Data directory for composition file `#{@data_directory}`')
+            def remove_incorrect_closing_paren(node, corrector)
+          corrector.remove(
+            range_between(
+              incorrect_parenthesis_removal_begin(node),
+              incorrect_parenthesis_removal_end(node)
+            )
+          )
         end
     
-          oink = MiniTest::Mock.new
-      oink.expect(:multi, [0, 1])
-      pork = ConnectionPool.new(size: 1) { oink }
+            it_behaves_like 'invalid'
+      end
     
-      def new_manager(opts)
-    Sidekiq::Manager.new(opts)
+            expect(corrected).to eq(<<~RUBY)
+          some_method #{open}
+          #{close}
+        RUBY
+      end
+    
+      context 'when a named capture is unreferenced in top level' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        /(?<foo>\w+)/ =~ 'FOO'
+        ^^^^^^^^^^^^ Useless assignment to variable - `foo`.
+      RUBY
+    end
   end
     
-    def Process.rss
-  `ps -o rss= -p #{Process.pid}`.chomp.to_i
+            def correct_style?(node)
+          !contains_guard_clause?(node) ||
+            next_line_rescue_or_ensure?(node) ||
+            next_sibling_parent_empty_or_else?(node) ||
+            next_sibling_empty_or_guard_clause?(node)
+        end
+    
+        assert_no_match /Delete this Page/, last_response.body, ''Delete this Page' link not blocked in page template'
+    assert_no_match /New/,              last_response.body, ''New' button not blocked in page template'
+    assert_no_match /Upload/,           last_response.body, ''Upload' link not blocked in page template'
+    assert_no_match /Rename/,           last_response.body, ''Rename' link not blocked in page template'
+    assert_no_match /Edit/,             last_response.body, ''Edit' link not blocked in page template'
+    
+        post '/edit/' + CGI.escape('한글'), :page => 'k', :content => '바뀐 text',
+         :format                            => 'markdown', :message => 'ghi'
+    follow_redirect!
+    assert last_response.ok?
+    
+      # piece file back together and write
+  manifest = '  s.files = %w[\n#{files}\n  ]\n'
+  spec = [head, manifest, tail].join('  # = MANIFEST =\n')
+  File.open(gemspec_file, 'w') { |io| io.write(spec) }
+  puts 'Updated #{gemspec_file}'
 end
+    
+        def initialize(params={})
+      @thehash = default.merge(params)
+    end
+    
+        it 'logs the exception to Sidekiq.logger' do
+      Component.new.invoke_exception(:a => 1)
+      @str_logger.rewind
+      log = @str_logger.readlines
+      assert_match(/'a':1/, log[0], 'didn't include the context')
+      assert_match(/Something didn't work!/, log[1], 'didn't include the exception message')
+      assert_match(/test\/test_exception_handler.rb/, log[2], 'didn't include the backtrace')
+    end
+    
+          msg['locale'] = 'jp'
+      I18n.locale = I18n.default_locale
+      assert_equal :en, I18n.locale
+      mw = Sidekiq::Middleware::I18n::Server.new
+      mw.call(nil, msg, nil) do
+        assert_equal :jp, I18n.locale
+      end
+      assert_equal :en, I18n.locale
+    end
+    
+    describe 'job scheduling' do
+  describe 'middleware' do
+    class SomeScheduledWorker
+      include Sidekiq::Worker
+      sidekiq_options :queue => :custom_queue
+      def perform(x)
+      end
+    end
+    
+      it 'should be false if the given selector is on the page' do
+    expect { @session.assert_no_selector(:xpath, '//p') }.to raise_error(Capybara::ElementNotFound)
+    expect { @session.assert_no_selector(:css, 'p a#foo') }.to raise_error(Capybara::ElementNotFound)
+    expect { @session.assert_no_selector('//p[contains(.,'est')]') }.to raise_error(Capybara::ElementNotFound)
+  end
+    
+      context 'with an asynchronous alert' do
+    it 'should accept the alert' do
+      @session.accept_alert do
+        @session.click_link('Open delayed alert')
+      end
+      expect(@session).to have_xpath('//a[@id='open-delayed-alert' and @opened='true']')
+    end
+    
+        it 'should not find when false' do
+      Capybara.enable_aria_label = false
+      expect { @session.find_button('Mediocre Button') }.to raise_error(Capybara::ElementNotFound)
+    end
+  end
+    
+      context 'with :visible option' do
+    it 'finds invisible elements when `false`' do
+      expect(@session.find_by_id('hidden_via_ancestor', visible: false).text(:all)).to match(/with hidden ancestor/)
+    end
+    
+    task :install => [ 'redis:install', 'dtach:install' ]
+    
+        # Given an object, returns a list `after_perform` hook names.
+    def after_hooks(job)
+      get_hook_names(job, 'after_perform')
+    end
