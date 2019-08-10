1a111296ed0aa7b80bf4aec7a0060adbb7ef90a0
SUNsung <weatherius@gmail.com>
2019-08-10 15:03:34 +0300

    Rewrite [32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py] File in: 2019-08-10 15:03:34

--- pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
+++ pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
@@ -1,140 +1,82 @@
 
         
-            def setUp(self):
-        from acme.errors import MissingNonce
-        self.response = mock.MagicMock(headers={})
-        self.response.request.method = 'FOO'
-        self.error = MissingNonce(self.response)
+        
+if __name__ == '__main__':
+    ''''''
+    # _test()
     
-    # The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-#html_favicon = None
-    
-        @mock.patch('certbot_apache.display_ops.display_util')
-    @certbot_util.patch_get_utility()
-    @mock.patch('certbot_apache.display_ops.logger')
-    def test_small_display(self, mock_logger, mock_util, mock_display_util):
-        mock_display_util.WIDTH = 20
-        mock_util().menu.return_value = (display_util.OK, 0)
-        self._call(self.vhosts)
-    
-            if record_contents:
-            # We need to remove old records in the same request
-            data['deletions'] = [
-                {
-                    'kind': 'dns#resourceRecordSet',
-                    'type': 'TXT',
-                    'name': record_name + '.',
-                    'rrdatas': record_contents,
-                    'ttl': record_ttl,
-                },
-            ]
-    
-    The path to this file can be provided interactively or using the
-``--dns-rfc2136-credentials`` command-line argument. Certbot records the
-path to this file for use during renewal, but does not store the file's contents.
-    
-    # These benchmarks are delicate.  They hit various fast-paths in the gen
-# machinery in order to stay synchronous so we don't need an IOLoop.
-# This removes noise from the results, but it's easy to change things
-# in a way that completely invalidates the results.
-    
-    '''Implementation of platform-specific functionality.
-    
-        def test_asyncio_adapter(self):
-        # This test demonstrates that when using the asyncio coroutine
-        # runner (i.e. run_until_complete), the to_asyncio_future
-        # adapter is needed. No adapter is needed in the other direction,
-        # as demonstrated by other tests in the package.
-        @gen.coroutine
-        def tornado_coroutine():
-            yield gen.moment
-            raise gen.Return(42)
+        Args:
+        x(tf.Tensor):
+        kernel_size(int or list of int):
+        out_channels(int):
+        act_fn(function):
+        strides(int or list of int):
+        padding(str):
+        name(str):
+        reuse(bool):
     
     
-class GoogleLoginHandler(RequestHandler, GoogleOAuth2Mixin):
-    def initialize(self, test):
-        self.test = test
-        self._OAUTH_REDIRECT_URI = test.get_url('/client/login')
-        self._OAUTH_AUTHORIZE_URL = test.get_url('/google/oauth2/authorize')
-        self._OAUTH_ACCESS_TOKEN_URL = test.get_url('/google/oauth2/token')
-    
-    		print('Test: delete')
-		myTree.delete(5)
-		assert_equal(myTree.treeIsEmpty(), True)
-		
-		print('Test: more complex deletions')
-		[myTree.insert(x) for x in range(1, 5)]
-		myTree.delete(2)
-		assert_equal(myTree.root.rightChild.data, 3)
-		print('Test: delete invalid value')
-		assert_equal(myTree.delete(100), False)
-    
-    
-def text_justification(words, max_width):
+def permute(x, perm):
     '''
-    :type words: list
-    :type max_width: int
-    :rtype: list
-    '''
-    ret = []  # return value
-    row_len = 0  # current length of strs in a row
-    row_words = []  # current words in a row
-    index = 0  # the index of current word in words
-    is_first_word = True  # is current word the first in a row
-    while index < len(words):
-        while row_len <= max_width and index < len(words):
-            if len(words[index]) > max_width:
-                raise ValueError('there exists word whose length is larger than max_width')
-            tmp = row_len
-            row_words.append(words[index])
-            tmp += len(words[index])
-            if not is_first_word:
-                tmp += 1  # except for the first word, each word should have at least a ' ' before it.
-            if tmp > max_width:
-                row_words.pop()
-                break
-            row_len = tmp
-            index += 1
-            is_first_word = False
-        # here we have already got a row of str , then we should supplement enough ' ' to make sure the length is max_width.
-        row = ''
-        # if the row is the last
-        if index == len(words):
-            for word in row_words:
-                row += (word + ' ')
-            row = row[:-1]
-            row += ' ' * (max_width - len(row))
-        # not the last row and more than one word
-        elif len(row_words) != 1:
-            space_num = max_width - row_len
-            space_num_of_each_interval = space_num // (len(row_words) - 1)
-            space_num_rest = space_num - space_num_of_each_interval * (len(row_words) - 1)
-            for j in range(len(row_words)):
-                row += row_words[j]
-                if j != len(row_words) - 1:
-                    row += ' ' * (1 + space_num_of_each_interval)
-                if space_num_rest > 0:
-                    row += ' '
-                    space_num_rest -= 1
-        # row with only one word
-        else:
-            row += row_words[0]
-            row += ' ' * (max_width - len(row))
-        ret.append(row)
-        # after a row , reset those value
-        row_len = 0
-        row_words = []
-        is_first_word = True
-    return ret
-
+    Examples:
+        x.shape == [128, 32, 1]
+        x = permute(x, [0, 2, 1])
+        x.shape == [128, 1, 32]
     
-        def save_tree(self, tree):
-        '''
-        Generate and save tree code to file
-        :param tree:
-        :return:
-        '''
-        signs = []
-        tree_code = ''
+    # 如果一个 ngram 都没找到，gensim 会报错
+#   其实可以返回一个 0 向量的，它内部实际上是从一个 0 向量开始累加的；
+#   但返回时做了一个判断——如果依然是 0 向量，则报错
+# print(model.wv['z'])
+r'''
+Traceback (most recent call last):
+  File 'D:/OneDrive/workspace/github/DL-Notes-for-Interview/code/工具库/gensim/FastText.py', line 53, in <module>
+    print(model.wv['z'])
+  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 336, in __getitem__
+    return self.get_vector(entities)
+  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 454, in get_vector
+    return self.word_vec(word)
+  File 'D:\program\work\Python\Anaconda3\envs\tf\lib\site-packages\gensim\models\keyedvectors.py', line 1989, in word_vec
+    raise KeyError('all ngrams for word %s absent from model' % word)
+KeyError: 'all ngrams for word z absent from model'
+'''
+    
+        def get_queue(self, name, maxsize=0):
+        ''' Return a queue from the manager
+            If it doesn't exist, create it '''
+        logger.debug('QueueManager getting: '%s'', name)
+        queue = self.queues.get(name, None)
+        if not queue:
+            self.add_queue(name, maxsize)
+            queue = self.queues[name]
+        logger.debug('QueueManager got: '%s'', name)
+        return queue
+    
+            For actions that occur identically for each frame (e.g. blend_box), constants can
+        be placed into self.func_constants to be compiled at launch, then referenced for
+        each face. '''
+    def __init__(self, mask_type, output_size, predicted_available=False, **kwargs):
+        super().__init__(mask_type, output_size, predicted_available, **kwargs)
+        self.mask = self.get_mask() if not self.skip else None
+    
+                       <option_name> should always be lower text.
+                   <metadata> dictionary requirements are listed below.
+    
+        The following variables should be defined:
+        _HELPTEXT: A string describing what this plugin does
+        _DEFAULTS: A dictionary containing the options, defaults and meta information. The
+                   dictionary should be defined as:
+                       {<option_name>: {<metadata>}}
+    
+        def predict(self, face):
+        ''' Return encodings for given image from vgg_face '''
+        if face.shape[0] != self.input_size:
+            face = self.resize_face(face)
+        blob = cv2.dnn.blobFromImage(face,  # pylint: disable=no-member
+                                     1.0,
+                                     (self.input_size, self.input_size),
+                                     self.average_img,
+                                     False,
+                                     False)
+        self.model.setInput(blob)
+        preds = self.model.forward('fc7')[0, :]
+        return preds
