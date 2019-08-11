3c09729fefe054d2b615b7aadd5eb8aa6156b231
SUNsung <weatherius@gmail.com>
2019-08-11 11:58:16 +0300

    Rewrite [19cGmR78Wf3hc19GgKZFjn.hh.191wy4dGSyHQw19gai.rb] File in: 2019-08-11 11:58:15

--- ruby/19cGmR78Wf3hc19GgKZFjn.hh.191wy4dGSyHQw19gai.rb
+++ ruby/19cGmR78Wf3hc19GgKZFjn.hh.191wy4dGSyHQw19gai.rb
@@ -1,177 +1,91 @@
 
         
-                def collection_options
-          { state: 'all', sort: 'created', direction: 'asc' }
-        end
-      end
-    end
-  end
+        # Mimic how the proposed change would first execute a couple of checks and
+# proceed to process with Liquid if necessary
+def conditional_liquid(content)
+  return content if content.nil? || content.empty?
+  return content unless content.include?('{%') || content.include?('{{')
+  always_liquid(content)
 end
+    }
+    }
+    }
+    
+    # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+    
+    require 'em-websocket'
+    
+    World(RemoteCommandHelpers)
 
     
-      def test_refute_nil
-    @tc.refute_nil 42
-  end
+        def server(name, properties={})
+      servers.add_host(name, properties)
+    end
     
-    # Get bundled gems on load path
-Dir.glob('#{src_testdir}/../gems/*/*.gemspec')
-  .reject {|f| f =~ /minitest|test-unit|power_assert/ }
-  .map {|f| $LOAD_PATH.unshift File.join(File.dirname(f), 'lib') }
-    
-        # check to_binary
-    i2_bin = compare_dump_and_load(i1,
-                                   proc{|iseq|
-                                     begin
-                                       iseq.to_binary
-                                     rescue RuntimeError # not a toplevel
-                                       # STDERR.puts [:failed, $!, iseq].inspect
-                                       nil
-                                     end
-                                   },
-                                   proc{|bin|
-                                     iseq = RubyVM::InstructionSequence.load_from_binary(bin)
-                                     # STDERR.puts iseq.inspect
-                                     iseq
-                                   }) if CHECK_TO_BINARY
-    # return value
-    i2_bin if CHECK_TO_BINARY
-  end if CHECK_TO_A || CHECK_TO_BINARY
-end
-    
-      ##
-  # Add a temporary stubbed method replacing +name+ for the duration
-  # of the +block+. If +val_or_callable+ responds to #call, then it
-  # returns the result of calling it, otherwise returns the value
-  # as-is. Cleans up the stub at the end of the +block+. The method
-  # +name+ must exist before stubbing.
-  #
-  #     def test_stale_eh
-  #       obj_under_test = Something.new
-  #       refute obj_under_test.stale?
-  #
-  #       Time.stub :now, Time.at(0) do
-  #         assert obj_under_test.stale?
-  #       end
-  #     end
-    
-          def self.test_order
-        :sorted
-      end
-    
-            self.arguments = [
-          CLAide::Argument.new('NAME', false),
-        ]
-    
-            if sets.count == 1
-          set = sets.first
-        elsif sets.map(&:name).include?(spec)
-          set = sets.find { |s| s.name == spec }
-        else
-          names = sets.map(&:name) * ', '
-          raise Informative, 'More than one spec found for '#{spec}':\n#{names}'
+          def role_properties_for(rolenames)
+        roles = rolenames.to_set
+        rps = Set.new unless block_given?
+        roles_for(rolenames).each do |host|
+          host.roles.intersection(roles).each do |role|
+            [host.properties.fetch(role)].flatten(1).each do |props|
+              if block_given?
+                yield host, role, props
+              else
+                rps << (props || {}).merge(role: role, hostname: host.hostname)
+              end
+            end
+          end
         end
-    
-          @right_diff_line_number = nil
-    
-          def upload_dest
-        @upload_dest
+        block_given? ? nil : rps
       end
     
-          def next_link
+          def assert_value_or_block_not_both(value, block)
+        return if value.nil? || block.nil?
+        raise Capistrano::ValidationError,
+              'Value and block both passed to Configuration#set'
       end
+    
+        template = (@article.decorate.cached_tag_list_array & SHE_CODED_TAGS).any? ? 'shecoded' : 'article'
+    
+      def self.upbuff!(buffer_update_id, admin_id, body_text, status)
+    buffer_update = BufferUpdate.find(buffer_update_id)
+    if status == 'confirmed'
+      buffer_response = send_to_buffer(body_text, buffer_update.buffer_profile_id_code)
+      buffer_update.update!(buffer_response: buffer_response, status: status, approver_user_id: admin_id, body_text: body_text)
+    else
+      buffer_update.update!(status: status, approver_user_id: admin_id)
     end
   end
-end
-
     
-    # Commit file to wiki, overwriting previous versions of that file
-def commit_test_file(wiki, dir, filename, ext, content)
-  committer = Gollum::Committer.new(wiki, :message => 'Added testfile', :parent  => wiki.repo.head.commit)
-  committer.add_to_index(dir, filename, ext, content, true)
-    committer.after_commit do |committer, sha|
-      wiki.clear_cache
-      committer.update_working_dir(dir, filename, ext)
-    end
-  committer.commit
-end
-
-    
-      test 'creating page is blocked' do
-    Precious::App.set(:wiki_options, { allow_editing: false})
-    post '/create', :content => 'abc', :page => 'D',
-         :format             => 'markdown', :message => 'def'
-    assert !last_response.ok?
-    
-    context 'Precious::Views::Page' do
-  setup do
-    examples = testpath 'examples'
-    @path    = File.join(examples, 'test.git')
-    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
-    @wiki = Gollum::Wiki.new(@path)
-  end
-    
-      if cfg = options[:config]
-    # If the path begins with a '/' it will be considered an absolute path,
-    # otherwise it will be relative to the CWD
-    cfg = File.join(Dir.getwd, cfg) unless cfg.slice(0) == File::SEPARATOR
-    require cfg
-  end
-    
-          def link_to_with_icon(icon_name, text, url, options = {})
-        options[:class] = (options[:class].to_s + ' icon-link with-tip action-#{icon_name}').strip
-        options[:title] = text if options[:no_text]
-        text = options[:no_text] ? '' : content_tag(:span, text, class: 'text')
-        options.delete(:no_text)
-        if icon_name
-          icon = content_tag(:span, '', class: '#{'mr-2' unless text.empty?} icon icon-#{icon_name}')
-          text.insert(0, icon + ' ')
-        end
-        link_to(text.html_safe, url, options)
+          def translate_attributes(keys, **options)
+        keys.map do |key|
+          translate('#{BASE_ATTRIBUTES_KEY}.#{key}', default: key, **options)
+        end.join(', ')
       end
     
-          @@inventory_unit_attributes = [
-        :id, :lock_version, :state, :variant_id, :shipment_id,
-        :return_authorization_id
-      ]
+            opts = attrs.extract_options!.clone
+        opts[:presence] = { value: true, message: opts[:message] }
+        opts = @group.merge(opts) if @group
     
-      config.action_mailer.perform_caching = false
+            get '/ints', values: 'a b c d'
+        expect(last_response.status).to eq(200)
+        expect(JSON.parse(last_response.body)).to eq([0, 0, 0, 0])
+      end
     
-      option '--no-depends', :flag, 'Do not list any dependencies in this package',
-    :default => false
-    
-        # Iterate over all the options and set defaults
-    if self.class.respond_to?(:declared_options)
-      self.class.declared_options.each do |option|
-        option.attribute_name.tap do |attr|
-          # clamp makes option attributes available as accessor methods
-          # do --foo-bar is available as 'foo_bar'
-          # make these available as package attributes.
-          attr = '#{attr}?' if !respond_to?(attr)
-          input.attributes[attr.to_sym] = send(attr) if respond_to?(attr)
-        end
+        def write(header)
+      @cookies.select { |key, _value| @send_cookies[key] == true }.each do |name, value|
+        cookie_value = value.is_a?(Hash) ? value : { value: value }
+        Rack::Utils.set_cookie_header! header, name, cookie_value
       end
     end
     
-        it 'joins array using ;' do
-      expect(project.send('hook_#{hook_name}')).
-        to eq('echo 'on hook'; echo 'another command here'')
-    end
-  end
-end
-    
-            expect(project).to call_tmux_ls
-      end
-    
-          def sample
-        asset_path 'sample.yml'
-      end
-    
-      describe '.shell?' do
-    context '$SHELL is set' do
-      before do
-        allow(ENV).to receive(:[]).with('SHELL') { 'vim' }
-      end
-    
-      describe '#name' do
-    it { expect(instance.name).to eq 'wemux' }
-  end
+              found_code = http_codes.find do |http_code|
+            (http_code[0].to_i == env[Grape::Env::API_ENDPOINT].status) && http_code[2].respond_to?(:represent)
+          end if env[Grape::Env::API_ENDPOINT].request
