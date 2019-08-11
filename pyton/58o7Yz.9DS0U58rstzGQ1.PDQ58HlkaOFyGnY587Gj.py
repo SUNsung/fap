c071a7a4ab6cb60868580f84ea0058aee0e1dbd1
SUNsung <weatherius@gmail.com>
2019-08-11 11:30:55 +0300

    Rewrite [58o7Yz.9DS0U58rstzGQ1.PDQ58HlkaOFyGnY587Gj.py] File in: 2019-08-11 11:30:55

--- pyton/58o7Yz.9DS0U58rstzGQ1.PDQ58HlkaOFyGnY587Gj.py
+++ pyton/58o7Yz.9DS0U58rstzGQ1.PDQ58HlkaOFyGnY587Gj.py
@@ -1,113 +1,65 @@
 
         
-            return True
+                (foo, p1), 2
+        (bar, p1), 2
+        (bar, p1), 1
+        (foo, p2), 3
+        (bar, p3), 10
+        (foo, p4), 1
+        '''
+        timestamp, product_id, category, quantity = line.split('\t')
+        if self.within_past_week(timestamp):
+            yield (category, product_id), quantity
+    
+        def reducer(self, key, values):
+        total = sum(values)
+        if total == 1:
+            yield key, total
     
     
-def get_running_config(module, config=None):
-    contents = module.params['running_config']
-    if not contents:
-        if config:
-            contents = config
-        else:
-            contents = get_config(module)
-    return NetworkConfig(indent=1, contents=contents)
-    
-        @pytest.mark.parametrize('args_dict, msg', INVALID_MULTIPLE_ACTIONS)
-    def test_multiple_actions(self, args_dict, msg):
-        m = ModuleArgsParser(args_dict)
-        with pytest.raises(AnsibleParserError) as err:
-            m.parse()
+class Person(object):
     
     
-# These Exceptions are temporary, using them as flow control until we can get a better solution.
-# DO NOT USE as they will probably be removed soon.
-# We will port the action modules in our tree to use a context manager instead.
-class AnsibleAction(AnsibleRuntimeError):
-    ''' Base Exception for Action plugin flow control '''
+class HashTable(object):
     
-        def test_nios_network_ipv6_dhcp_update(self):
-        self.module.params = {'provider': None, 'state': 'present', 'ipv6network': 'fe80::/64',
-                              'comment': 'updated comment', 'extattrs': None}
+        def parse_query(self, query):
+        '''Remove markup, break text into terms, deal with typos,
+        normalize capitalization, convert to use boolean operations.
+        '''
+        ...
     
-    Finally, note that parameters of the models have been here handpicked but
-that in practice they need to be adjusted. In the absence of labelled data,
-the problem is completely unsupervised so model selection can be a challenge.
-'''
-    
-    # Plot also the training points
-plt.scatter(X[:, 0], X[:, 1], c=Y, edgecolors='k', cmap=plt.cm.Paired)
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
+        with app.app_context():
+        db = get_db()
+        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
+        assert post is None
+
     
     
-def plot_feature_times(all_times, batch_size, all_components, data):
-    plt.figure()
-    plot_results(all_components, all_times['pca'], label='PCA')
-    plot_results(all_components, all_times['ipca'],
-                 label='IncrementalPCA, bsize=%i' % batch_size)
-    plt.legend(loc='upper left')
-    plt.suptitle('Algorithm runtime vs. n_components\n \
-                 LFW, size %i x %i' % data.shape)
-    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
-    plt.ylabel('Time (seconds)')
+def _collect_previous_mask(input_tensors):
+    '''Retrieves the output mask(s) of the previous node.
     
-        def flush_queue(self, q_name):
-        ''' Empty out a specific queue '''
-        logger.debug('QueueManager flushing: '%s'', q_name)
-        queue = self.queues[q_name]
-        while not queue.empty():
-            queue.get(True, 1)
+    # we add a GlobalAveragePooling1D, which will average the embeddings
+# of all words in the document
+model.add(GlobalAveragePooling1D())
     
-            self.axis = to_list(axis)
-        self.gamma_init = initializers.get(gamma_init)
-        self.beta_init = initializers.get(beta_init)
-        self.gamma_regularizer = regularizers.get(gamma_regularizer)
-        self.beta_regularizer = regularizers.get(beta_regularizer)
-        self.epsilon = epsilon
-        self.group = group
-        self.data_format = K.normalize_data_format(data_format)
     
-        def set_defaults(self):
-        ''' Set the default values for config '''
-        logger.debug('Setting defaults')
-        current_dir = os.path.dirname(__file__)
-        for dirpath, _, filenames in os.walk(current_dir):
-            default_files = [fname for fname in filenames if fname.endswith('_defaults.py')]
-            if not default_files:
-                continue
-            base_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-            import_path = '.'.join(full_path_split(dirpath.replace(base_path, ''))[1:])
-            plugin_type = import_path.split('.')[-1]
-            for filename in default_files:
-                self.load_module(filename, import_path, plugin_type)
+def contrastive_loss(y_true, y_pred):
+    '''Contrastive loss from Hadsell-et-al.'06
+    http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    '''
+    margin = 1
+    square_pred = K.square(y_pred)
+    margin_square = K.square(K.maximum(margin - y_pred, 0))
+    return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
     
-        The following keys are expected for the _DEFAULTS <metadata> dict:
-        datatype:  [required] A python type class. This limits the type of data that can be
-                   provided in the .ini file and ensures that the value is returned in the
-                   correct type to faceswap. Valid datatypes are: <class 'int'>, <class 'float'>,
-                   <class 'str'>, <class 'bool'>.
-        default:   [required] The default value for this option.
-        info:      [required] A string describing what this option does.
-        choices:   [optional] If this option's datatype is of <class 'str'> then valid
-                   selections can be defined here. This validates the option and also enables
-                   a combobox / radio option in the GUI.
-        gui_radio: [optional] If <choices> are defined, this indicates that the GUI should use
-                   radio buttons rather than a combobox to display this option.
-        min_max:   [partial] For <class 'int'> and <class 'float'> datatypes this is required
-                   otherwise it is ignored. Should be a tuple of min and max accepted values.
-                   This is used for controlling the GUI slider range. Values are not enforced.
-        rounding:  [partial] For <class 'int'> and <class 'float'> datatypes this is
-                   required otherwise it is ignored. Used for the GUI slider. For floats, this
-                   is the number of decimal places to display. For ints this is the step size.
-        fixed:     [optional] [train only]. Training configurations are fixed when the model is
-                   created, and then reloaded from the state file. Marking an item as fixed=False
-                   indicates that this value can be changed for existing models, and will override
-                   the value saved in the state file with the updated value in config. If not
-                   provided this will default to True.
-'''
+        # Arguments
+        layers: list of layers to add to the model.
+        name: Name given to the model
     
-        The following variables should be defined:
-        _HELPTEXT: A string describing what this plugin does
-        _DEFAULTS: A dictionary containing the options, defaults and meta information. The
-                   dictionary should be defined as:
-                       {<option_name>: {<metadata>}}
+    from .. import backend as K
+from .. import initializers
+from .. import regularizers
+from .. import constraints
+from ..engine.base_layer import Layer
+from ..legacy import interfaces
+from ..utils.generic_utils import to_list
