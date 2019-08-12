74287812b67cdf4665fded0a1bdbace1d21f79d7
SUNsung <weatherius@gmail.com>
2019-08-12 15:42:59 +0300

    Rewrite [48kRpnvduPDTM48yjMasYhpqok48G.LBZbWC24I489G8.rb] File in: 2019-08-12 15:42:59

--- ruby/48kRpnvduPDTM48yjMasYhpqok48G.LBZbWC24I489G8.rb
+++ ruby/48kRpnvduPDTM48yjMasYhpqok48G.LBZbWC24I489G8.rb
@@ -1,203 +1,81 @@
 
         
-              def tag_without_or_later(tag)
-        tag
-      end
-    
-              if method_called_ever?(body_node, :virtualenv_create) ||
-             method_called_ever?(body_node, :virtualenv_install_with_resources)
-            find_method_with_args(body_node, :resource, 'setuptools') do
-              problem 'Formulae using virtualenvs do not need a `setuptools` resource.'
-            end
+                  it 'registers an offense' do
+            inspect_source(source)
+            expect(cop.offenses.size).to eq(1)
+            expect(cop.messages).to eq(['Use `Integer` instead of `#{klass}`.'])
           end
     
-            it 'returns false if all are lower' do
-          setup_java_with_version '1.6.0_5'
-          expect(subject).not_to be_satisfied
-        end
+        it 'returns a hash with the flare tag's name' do
+      expect(described_class.new(valid_article).tag_hash.value?('ama')).to be true
+    end
     
-        depends_on(deps) if add_mac_dependency?(args)
+      def listings_tweet!
+    buffer_listings_id = ApplicationConfig['BUFFER_LISTINGS_PROFILE']
+    BufferUpdate.send_to_buffer(listings_twitter_text, buffer_listings_id)
+    @listing.update(last_buffered: Time.current)
   end
     
-        it 'ignores OS version specifications' do
-      f = formula 'foo' do
-        url 'foo-1.0'
-    
-        it 'sets itself to be the resource's owner' do
-      subject.resource('foo') { url 'foo-1.0' }
-      subject.owner = owner
-      subject.resources.each_value do |r|
-        expect(r.owner).to eq(subject)
-      end
-    end
-    
-        def metadata_versioned_path(version: self.version)
-      cask_version = (version || :unknown).to_s
-    
-      # If a target is given, unpack there; else unpack to a temp folder.
-  # If block is given, yield to that block with `|stage|`, where stage
-  # is a {ResourceStageContext}.
-  # A target or a block must be given, but not both.
-  def unpack(target = nil)
-    mktemp(download_name) do |staging|
-      downloader.stage
-      @source_modified_time = downloader.source_modified_time
-      apply_patches
-      if block_given?
-        yield ResourceStageContext.new(self, staging)
-      elsif target
-        target = Pathname(target)
-        target.install Pathname.pwd.children
-      end
-    end
+      def self.send_to_buffer(text, buffer_profile_id_code)
+    client = Buffer::Client.new(ApplicationConfig['BUFFER_ACCESS_TOKEN'])
+    client.create_update(
+      body: {
+        text:
+        text,
+        profile_ids: [
+          buffer_profile_id_code,
+        ]
+      },
+    )
   end
     
-          it 'returns false' do
-        expect(node.pure?).to be(false)
-      end
+      factory :project_with_literals_as_window_name, class: Tmuxinator::Project do
+    transient do
+      file { yaml_load('spec/fixtures/sample_literals_as_window_name.yml') }
     end
     
-              return false unless brace_style == 'context_dependent'
+    describe Tmuxinator::Hooks::Project do
+  let(:project) { FactoryBot.build(:project) }
     
-        context 'preceding multiple captures' do
-      let(:pattern) { '(send array :push ... $_ $_)' }
-      let(:ruby) { '[1].push(2, 3, 4, 5)' }
-      let(:captured_vals) { [s(:int, 4), s(:int, 5)] }
-    
-      context 'when there's an unreferenced assignment in top level if branch ' \
-          'while the variable is referenced in the paired else branch' do
-    it 'registers an offense for the assignment in the if branch' do
-      expect_offense(<<~RUBY)
-        if flag
-          foo = 1
-          ^^^ Useless assignment to variable - `foo`.
-        else
-          puts foo
-        end
-      RUBY
-    end
-  end
-    
-          def extract_rhs(node)
-        if node.casgn_type?
-          _scope, _lhs, rhs = *node
-        elsif node.op_asgn_type?
-          _lhs, _op, rhs = *node
-        elsif node.call_type?
-          rhs = node.last_argument
-        elsif node.assignment?
-          _lhs, rhs = *node
-        end
-    
-            def last_argument(node)
-          node.if_branch.children.last
-        end
-    
-      it 'does not register offense when guard clause is before `else`' do
-    expect_no_offenses(<<~RUBY)
-      def foo
-        if cond
-          return another_object if something_different?
-        else
-          bar
-        end
-      end
-    RUBY
-  end
-    
-    module RuboCop
-  module AST
-    # A node extension for `block` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `send` nodes within RuboCop.
-    #
-    # A `block` node is essentially a method send with a block. Parser nests
-    # the `send` node inside the `block` node.
-    class BlockNode < Node
-      VOID_CONTEXT_METHODS = %i[each tap].freeze
-    
-          def record_response(response)
-        log_entries.create!(details: response.to_yaml)
-      end
-    
-          before_action :set_content_type
-      before_action :load_user
-      before_action :authorize_for_order, if: proc { order_token.present? }
-      before_action :authenticate_user
-      before_action :load_user_roles
-    
-            def fire
-          inventory_unit.send('#{@event}!') if @event
-        end
-    
-            def scope
-          @scope ||= if params[:option_type_id]
-                       Spree::OptionType.find(params[:option_type_id]).option_values.accessible_by(current_ability, :show)
-                     else
-                       Spree::OptionValue.accessible_by(current_ability, :show).load
-                     end
-        end
-    
-            def update
-          authorize! :update, @product_property
-    
-            def create
-          authorize! :create, Property
-          @property = Spree::Property.new(property_params)
-          if @property.save
-            respond_with(@property, status: 201, default_template: :show)
-          else
-            invalid_resource!(@property)
-          end
-        end
-    
-        it 'pushes messages to redis using a String class' do
-      q = Sidekiq::Queue.new('foo')
-      pre = q.size
-      jid = Sidekiq::Client.push('queue' => 'foo', 'class' => 'MyWorker', 'args' => [1, 2])
-      assert jid
-      assert_equal 24, jid.size
-      assert_equal pre + 1, q.size
-    end
-    
-      describe 'status' do
-    describe 'version' do
-      it 'displays the current Sidekiq version' do
-        assert_includes output, 'Sidekiq #{Sidekiq::VERSION}'
-      end
-    
-        it 'generates random intervals that target a configured average' do
-      with_sidekiq_option(:poll_interval_average, 10) do
-        i = 500
-        intervals = Array.new(i){ @poller.send(:random_poll_interval) }
-    
-          q = Sidekiq::Queue.new('custom_queue')
-      qs = q.size
-      assert SomeScheduledWorker.perform_in(-300, 'mike')
-      assert_equal 3, ss.size
-      assert_equal qs+1, q.size
-    
-        it 'stubs the delay call on classes' do
-      assert_equal 0, Sidekiq::Extensions::DelayedClass.jobs.size
-      Something.delay.foo(Date.today)
-      assert_equal 1, Sidekiq::Extensions::DelayedClass.jobs.size
-    end
-  end
-    
-        while readable_io = IO.select([self_read])
-      signal = readable_io.first[0].gets.strip
-      handle_signal(launcher, signal)
-    end
-  #end
-rescue SystemExit => e
-  #Sidekiq.logger.error('Profiling...')
-  #result = RubyProf.stop
-  #printer = RubyProf::GraphHtmlPrinter.new(result)
-  #printer.print(File.new('output.html', 'w'), :min_percent => 1)
-  # normal
-rescue => e
-  raise e if $DEBUG
-  STDERR.puts e.message
-  STDERR.puts e.backtrace.join('\n')
-  exit 1
+      it { expect(subject.tmux_window_and_pane_target).to eql 'foo:0.1' }
 end
+
+    
+      describe '#get_pane_base_index' do
+    it 'extracts pane-base-index from the global tmux window options' do
+      allow_any_instance_of(Kernel).to receive(:`).
+        with(Regexp.new('tmux.+ show-window-option -g pane-base-index')).
+        and_return('pane-base-index 3\n')
+    
+      chain :with_commands do |*expected|
+    @commands = expected
+  end
+  alias_method :and_commands, :with_commands
+    
+        def deprecation_messages
+      [
+        RBENVRVM_DEP_MSG,
+        TABS_DEP_MSG,
+        CLIARGS_DEP_MSG,
+        SYNC_DEP_MSG,
+        PRE_DEP_MSG,
+        POST_DEP_MSG
+      ]
+    end
+    
+          expect(instance.render).to match %r{wemux.ls.2>\/dev\/null}
+    end
+  end
+    
+    describe Tmuxinator::Doctor do
+  describe '.installed?' do
+    context 'tmux is installed' do
+      before do
+        allow(Kernel).to receive(:system) { true }
+      end
+    
+          it 'returns one pane in an Array' do
+        expect(window.panes.first).to be_a_pane.
+          with(index: 0).and_commands('vim')
+      end
+    end
