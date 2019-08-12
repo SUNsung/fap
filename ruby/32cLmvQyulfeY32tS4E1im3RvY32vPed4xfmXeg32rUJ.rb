49aca13b1e47e1a754090b4d4f1a9351c699d07f
SUNsung <weatherius@gmail.com>
2019-08-13 02:46:41 +0300

    Rewrite [32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb] File in: 2019-08-13 02:46:40

--- ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
+++ ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
@@ -1,102 +1,103 @@
 
         
-                subject.exec
+                # Setup and normalize the configuration:
+        #   * Create Kramdown if it doesn't exist.
+        #   * Set syntax_highlighter, detecting enable_coderay and merging
+        #       highlighter if none.
+        #   * Merge kramdown[coderay] into syntax_highlighter_opts stripping coderay_.
+        #   * Make sure `syntax_highlighter_opts` exists.
+    
+          def after_feature_element(feature_element)
+        @timings[feature_element_timing_key(feature_element)] = Time.now - @timings[feature_element_timing_key(feature_element)]
+        @io.print ' (#{@timings[feature_element_timing_key(feature_element)]}s)'
       end
+    
+            def handle_websockets_event(websocket)
+          websocket.onopen { |handshake| connect(websocket, handshake) }
+          websocket.onclose { disconnect(websocket) }
+          websocket.onmessage { |msg| print_message(msg) }
+          websocket.onerror { |error| log_error(error) }
+        end
+    
+            a_split.each_with_index { |s, i| a_split[i] = s.to_i unless i == a_length - 1 }
+        b_split.each_with_index { |s, i| b_split[i] = s.to_i unless i == b_length - 1 }
+    
+        def links
+      context[:links]
     end
     
-    describe Gitlab::BackgroundMigration::EncryptRunnersTokens, :migration, schema: 20181121111200 do
-  let(:settings) { table(:application_settings) }
-  let(:namespaces) { table(:namespaces) }
-  let(:projects) { table(:projects) }
-  let(:runners) { table(:ci_runners) }
+        def add(path, content)
+      @pages[path] = content
+    end
     
-    # This class finds files in a repository by name and content
-# the result is joined and sorted by file name
-module Gitlab
-  class FileFinder
-    attr_reader :project, :ref
-    
-      it 'returns members for nested group', :nested_groups do
-    group.add_developer(user2)
-    nested_group.request_access(user4)
-    member1 = group.add_maintainer(user1)
-    member3 = nested_group.add_maintainer(user2)
-    member4 = nested_group.add_maintainer(user3)
-    
-      def self.link(title)
-    title.downcase.gsub(/(?!-)\W /, '-').gsub(' ', '-').gsub(/(?!-)\W/, '')
+        def effective_path
+      @effective_path ||= effective_url.path
+    end
   end
+end
+
     
-    @@ layout
-<html>
-  <head>
-    <title>Super Simple Chat with Sinatra</title>
-    <meta charset='utf-8' />
-    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
-  </head>
-  <body><%= yield %></body>
-</html>
+            css('h1 + code').each do |node|
+          node.before('<p></p>')
+          while node.next_element.name == 'code'
+            node.previous_element << ' '
+            node.previous_element << node.next_element
+          end
+          node.previous_element.prepend_child(node)
+        end
     
-        def frame_class(frame)
-      if frame.filename =~ %r{lib/sinatra.*\.rb}
-        'framework'
-      elsif (defined?(Gem) && frame.filename.include?(Gem.dir)) ||
-            frame.filename =~ %r{/bin/(\w+)\z}
-        'system'
-      else
-        'app'
-      end
-    end
+            css('span.badges').each do |node|
+          node.name = 'div'
+        end
     
-          def mask_authenticity_token(session)
-        token = set_token(session)
-        mask_token(token)
-      end
+          # Generates the contents of the module.modulemap file.
+      #
+      # @return [String]
+      #
+      def generate
+        <<-MODULE_MAP.strip_heredoc
+#{module_specifier_prefix}module #{target.product_module_name}#{module_declaration_attributes} {
+  #{headers.join('\n  ')}
+    }
     
-          def random_string(secure = defined? SecureRandom)
-        secure ? SecureRandom.hex(16) : '%032x' % rand(2**128-1)
-      rescue NotImplementedError
-        random_string false
-      end
+              merged = @target.merge_embedded_pod_targets(embedded_pod_targets_for_build_configuration)
+          merged.pod_targets_for_build_configuration('Debug').should == [@pod_target]
+          merged.pod_targets_for_build_configuration('Release').should == [@pod_target, other_pod_target]
+        end
     
-          def redirect(env)
-        request = Request.new(env)
-        warn env, 'attack prevented by #{self.class}'
-        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
-      end
-    
-          def escape(object)
-        case object
-        when Hash   then escape_hash(object)
-        when Array  then object.map { |o| escape(o) }
-        when String then escape_string(object)
-        when Tempfile then object
-        else nil
+          def ==(other)
+        if other.class == self.class
+          other.source_path == @source_path && other.dsym_path == @dsym_path && other.bcsymbolmap_paths == @bcsymbolmap_paths
+        else
+          false
         end
       end
     
-      it_behaves_like 'any rack application'
-    
-          context 'with --suppress-tmux-version-warning flag' do
-        before do
-          ARGV.replace([*args, '--suppress-tmux-version-warning'])
+          def updates
+        @updates ||= begin
+          ensure_external_podspecs_present!
+          spec_sets.map do |set|
+            spec = set.specification
+            source_version = set.versions.first
+            pod_name = spec.root.name
+            lockfile_version = lockfile.version(pod_name)
+            if source_version > lockfile_version
+              matching_spec = unlocked_pods.find { |s| s.name == pod_name }
+              matching_version =
+                matching_spec ? matching_spec.version : '(unused)'
+              [pod_name, lockfile_version, matching_version, source_version]
+            end
+          end.compact.uniq
         end
+      end
     
-        def tmux_tiled_layout_command
-      '#{project.tmux} select-layout -t #{tmux_window_target} tiled'
-    end
-    
-      factory :project_with_force_detach, class: Tmuxinator::Project do
-    transient do
-      file { yaml_load('spec/fixtures/detach.yml') }
-    end
-    initialize_with { Tmuxinator::Project.new(file, force_detach: true) }
-  end
-    
-        def startup_pane
-      '#{startup_window}.#{yaml['startup_pane'] || pane_base_index}'
-    end
-    
-          def version
-        if Tmuxinator::Doctor.installed?
-          tmux_version = `tmux -V`.split(' ')[1]
+            # Displays the state of each pod.
+        #
+        # @return [void]
+        #
+        def print
+          states = %i(added deleted changed unchanged)
+          lines(states).each do |line|
+            UI.message(line, '', 2)
+          end
+        end
