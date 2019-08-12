7fa35e73f4625f58ba03c6caa48aeb1049025231
SUNsung <weatherius@gmail.com>
2019-08-12 03:46:59 +0300

    Rewrite [07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp] File in: 2019-08-12 03:46:58

--- cccccc/07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp
+++ cccccc/07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp
@@ -1,293 +1,233 @@
 
         
-        #define MERGE_QUAD(sgn, bits, n) { \
-                                     FILL_LINES##n(PREF, sgn##bits) \
-                                     MERGE_ASM##n(sgn, bits) \
-                                 }
+            QString getURI();
     
-    namespace internal {
+    int secp256k1_ecdsa_recover(const secp256k1_context* ctx, secp256k1_pubkey *pubkey, const secp256k1_ecdsa_recoverable_signature *signature, const unsigned char *msg32) {
+    secp256k1_ge q;
+    secp256k1_scalar r, s;
+    secp256k1_scalar m;
+    int recid;
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(secp256k1_ecmult_context_is_built(&ctx->ecmult_ctx));
+    ARG_CHECK(msg32 != NULL);
+    ARG_CHECK(signature != NULL);
+    ARG_CHECK(pubkey != NULL);
     }
     
-             int32x4_t vline1_s32 = vmovl_s16(vget_low_s16(vline1_s16));
-         int32x4_t vline2_s32 = vmovl_s16(vget_high_s16(vline1_s16));
-         int32x4_t vline3_s32 = vmovl_s16(vget_low_s16(vline2_s16));
-         int32x4_t vline4_s32 = vmovl_s16(vget_high_s16(vline2_s16));
+        BOOST_CHECK_EQUAL(arr[999].getValStr(), '');
     
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(u8, s32, 16,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 16)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.8 {d4-d5}, [%[src]]                              \n\t'
-            'vmovl.u8 q3, d4                                       \n\t'
-            'vmovl.u8 q4, d5                                       \n\t'
-            'vmovl.u16 q5, d6                                      \n\t'
-            'vmovl.u16 q6, d7                                      \n\t'
-            'vmovl.u16 q7, d8                                      \n\t'
-            'vmovl.u16 q8, d9                                      \n\t'
-            'vcvt.f32.u32 q9, q5                                   \n\t'
-            'vcvt.f32.u32 q10, q6                                  \n\t'
-            'vcvt.f32.u32 q11, q7                                  \n\t'
-            'vcvt.f32.u32 q12, q8                                  \n\t'
-            'vmul.f32 q13, q9, q0                                  \n\t'
-            'vmul.f32 q14, q10, q0                                 \n\t'
-            'vmul.f32 q15, q11, q0                                 \n\t'
-            'vmul.f32 q2, q12, q0                                  \n\t'
-            'vadd.f32 q3, q13, q1                                  \n\t'
-            'vadd.f32 q4, q14, q1                                  \n\t'
-            'vadd.f32 q5, q15, q1                                  \n\t'
-            'vadd.f32 q6, q2, q1                                   \n\t'
-            'vcvt.s32.f32 q7, q3                                   \n\t'
-            'vcvt.s32.f32 q8, q4                                   \n\t'
-            'vcvt.s32.f32 q9, q5                                   \n\t'
-            'vcvt.s32.f32 q10, q6                                  \n\t'
-            'vst1.32 {d14-d15}, [%[dst1]]                          \n\t'
-            'vst1.32 {d16-d17}, [%[dst2]]                          \n\t'
-            'vst1.32 {d18-d19}, [%[dst3]]                          \n\t'
-            'vst1.32 {d20-d21}, [%[dst4]]                          \n\t'
-            : /*no output*/
-            : [src] 'r' (_src + i),
-              [dst1] 'r' (_dst + i + 0),
-              [dst2] 'r' (_dst + i + 4),
-              [dst3] 'r' (_dst + i + 8),
-              [dst4] 'r' (_dst + i + 12),
-              'w'  (vscale), 'w' (vshift)
-            : 'd4','d5','d6','d7','d8','d9','d10',
-            'd11','d12','d13','d14','d15','d16','d17',
-            'd18','d19','d20','d21','d22','d23','d24',
-            'd25','d26','d27','d28','d29','d30','d31'
-        );
-    }
-})
-#else
-CVTS_FUNC(u8, s32, 16,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 16)
-    {
-        internal::prefetch(_src + i);
-        uint8x16_t vline = vld1q_u8(_src + i);
-        uint16x8_t vline1_u16 = vmovl_u8(vget_low_u8 (vline));
-        uint16x8_t vline2_u16 = vmovl_u8(vget_high_u8(vline));
-        uint32x4_t vline1_u32 = vmovl_u16(vget_low_u16 (vline1_u16));
-        uint32x4_t vline2_u32 = vmovl_u16(vget_high_u16(vline1_u16));
-        uint32x4_t vline3_u32 = vmovl_u16(vget_low_u16 (vline2_u16));
-        uint32x4_t vline4_u32 = vmovl_u16(vget_high_u16(vline2_u16));
-        float32x4_t vline1_f32 = vcvtq_f32_u32(vline1_u32);
-        float32x4_t vline2_f32 = vcvtq_f32_u32(vline2_u32);
-        float32x4_t vline3_f32 = vcvtq_f32_u32(vline3_u32);
-        float32x4_t vline4_f32 = vcvtq_f32_u32(vline4_u32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline3_f32 = vmulq_f32(vline3_f32, vscale);
-        vline4_f32 = vmulq_f32(vline4_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        vline3_f32 = vaddq_f32(vline3_f32, vshift);
-        vline4_f32 = vaddq_f32(vline4_f32, vshift);
-        int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        int32x4_t vline3_s32 = vcvtq_s32_f32(vline3_f32);
-        int32x4_t vline4_s32 = vcvtq_s32_f32(vline4_f32);
-        vst1q_s32(_dst + i + 0,  vline1_s32);
-        vst1q_s32(_dst + i + 4,  vline2_s32);
-        vst1q_s32(_dst + i + 8,  vline3_s32);
-        vst1q_s32(_dst + i + 12, vline4_s32);
-    }
-})
-#endif
-    
-    #include 'common.hpp'
-    
-                for(; j < (ptrdiff_t)size.width - 16 - 3; j += 16, ptr += 16)
-            {
-                internal::prefetch(ptr);
-                internal::prefetch(ptr + pixel[0]);
-                internal::prefetch(ptr + pixel[2]);
-    }
-    
-    void gaussianBlur5x5(const Size2D &size, s32 cn,
-                     const u16 * srcBase, ptrdiff_t srcStride,
-                     u16 * dstBase, ptrdiff_t dstStride,
-                     BORDER_MODE borderType, u16 borderValue, Margin borderMargin)
-{
-    internal::assertSupportedConfiguration(isGaussianBlur5x5Supported(size, cn, borderType));
-#ifdef CAROTENE_NEON
-    size_t colsn = size.width * cn;
-    }
-    
-      {
-    string str;
-    StringByteSink sink(&str);
-    limit_source.CopyTo(&sink, limit_source.Available());
-    EXPECT_EQ('ello ', str);
-    EXPECT_EQ(0, limit_source.Available());
-    EXPECT_EQ(6, source.Available());
+    Status DumpFile(Env* env, const std::string& fname, WritableFile* dst) {
+  FileType ftype;
+  if (!GuessType(fname, &ftype)) {
+    return Status::InvalidArgument(fname + ': unknown file type');
   }
-    
-    
-    {
-    {
-    {}  // namespace util
-}  // namespace protobuf
-}  // namespace google
-
-    
-    TEST(StatusOr, TestStatusCtor) {
-  StatusOr<int> thing(Status::CANCELLED);
-  EXPECT_FALSE(thing.ok());
-  EXPECT_EQ(Status::CANCELLED, thing.status());
-}
-    
-    TEST(StructurallyValidTest, ValidUTF8String) {
-  // On GCC, this string can be written as:
-  //   'abcd 1234 - \u2014\u2013\u2212'
-  // MSVC seems to interpret \u differently.
-  string valid_str('abcd 1234 - \342\200\224\342\200\223\342\210\222 - xyz789');
-  EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data(),
-                                      valid_str.size()));
-  // Additional check for pointer alignment
-  for (int i = 1; i < 8; ++i) {
-    EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data() + i,
-                                        valid_str.size() - i));
+  switch (ftype) {
+    case kLogFile:         return DumpLog(env, fname, dst);
+    case kDescriptorFile:  return DumpDescriptor(env, fname, dst);
+    case kTableFile:       return DumpTable(env, fname, dst);
+    default:
+      break;
   }
+  return Status::InvalidArgument(fname + ': not a dump-able file type');
 }
     
-    std::string ReadFile(const std::string& name) {
-  std::ifstream file(name.c_str());
-  GOOGLE_CHECK(file.is_open()) << 'Couldn't find file ''
-      << name
-      << '', please make sure you are running this command from the benchmarks'
-      << ' directory.\n';
-  return std::string((std::istreambuf_iterator<char>(file)),
-                     std::istreambuf_iterator<char>());
-}
+        IMF_EXPORT
+    ~FastHufDecoder ();
     
-    using google::protobuf::util::Proto3DataStripper;
     
-      virtual bool Generate(const FileDescriptor* file,
-                        const string& parameter,
-                        GeneratorContext* context,
-                        string* error) const {
-    FileDescriptorProto new_file;
-    file->CopyTo(&new_file);
-    SchemaGroupStripper::StripFile(file, &new_file);
-    }
+OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_ENTER
     
-    // Main function:  Reads the entire address book from a file and prints all
-//   the information inside.
-int main(int argc, char* argv[]) {
-  // Verify that the version of the library that we linked against is
-  // compatible with the version of the headers we compiled against.
-  GOOGLE_PROTOBUF_VERIFY_VERSION;
-    }
     
-        class Rational
-    {
-    public:
-        Rational() noexcept;
-        Rational(Number const& n) noexcept;
-        Rational(Number const& p, Number const& q) noexcept;
-        Rational(int32_t i);
-        Rational(uint32_t ui);
-        Rational(uint64_t ui);
-    }
-    
-    void CCalcEngine::InitChopNumbers()
+Rational
+guessExactFps (const Rational &fps)
 {
-    // these rat numbers are set only once and then never change regardless of
-    // base or precision changes
-    assert(m_chopNumbers.size() >= 4);
-    m_chopNumbers[0] = Rational{ rat_qword };
-    m_chopNumbers[1] = Rational{ rat_dword };
-    m_chopNumbers[2] = Rational{ rat_word };
-    m_chopNumbers[3] = Rational{ rat_byte };
+    const double e = 0.002;
     }
     
-    CalculatorHistory::CalculatorHistory(size_t maxSize)
-    : m_maxHistorySize(maxSize)
-{
-}
-    
-        static std::vector<uint32_t> DigitGroupingStringToGroupingVector(std::wstring_view groupingString);
-    std::wstring GroupDigits(std::wstring_view delimiter, std::vector<uint32_t> const& grouping, std::wstring_view displayString, bool isNumNegative = false);
-    
-    //-----------------------------------------------------------------------------
-//
-//   External functions defined in the math package.
-//
-//-----------------------------------------------------------------------------
-    
-    
+        if (isMultipartFile)
     {
-    {            if (U_SUCCESS(status)) {
-                destLen += appendGroup(groupNum, dest, status);
-            }
-        }  // End of $ capture group handling
-    }  // End of per-character loop through the replacement string.
-    
-    
-//---------------------------------------------------------------------
-//
-//   matcher(UnicodeString, err)
-//
-//---------------------------------------------------------------------
-RegexMatcher *RegexPattern::matcher(const UnicodeString &input,
-                                    UErrorCode          &status)  const {
-    RegexMatcher    *retMatcher = matcher(status);
-    if (retMatcher != NULL) {
-        retMatcher->fDeferredStatus = status;
-        retMatcher->reset(input);
-    }
-    return retMatcher;
-}
-    
-    //-------------------------------------------------------------------------------
-//
-//  ScriptSet - A bit set representing a set of scripts.
-//
-//              This class was originally used exclusively with script sets appearing
-//              as part of the spoof check whole script confusable binary data. Its
-//              use has since become more general, but the continued use to wrap
-//              prebuilt binary data does constrain the design.
-//
-//-------------------------------------------------------------------------------
-class U_I18N_API ScriptSet: public UMemory {
-  public:
-    ScriptSet();
-    ScriptSet(const ScriptSet &other);
-    ~ScriptSet();
+        if (!hasName())
+        {
+            throw IEX_NAMESPACE::ArgExc ('Headers in a multipart file should'
+                               ' have name attribute.');
+        }
     }
     
-    SharedBreakIterator::SharedBreakIterator(
-        BreakIterator *biToAdopt) : ptr(biToAdopt) { }
     
-    #include 'unicode/utypes.h'
-#include 'sharedobject.h'
     
-    class U_I18N_API SharedPluralRules : public SharedObject {
-public:
-    SharedPluralRules(PluralRules *prToAdopt) : ptr(prToAdopt) { }
-    virtual ~SharedPluralRules();
-    const PluralRules *operator->() const { return ptr; }
-    const PluralRules &operator*() const { return *ptr; }
-private:
-    PluralRules *ptr;
-    SharedPluralRules(const SharedPluralRules &);
-    SharedPluralRules &operator=(const SharedPluralRules &);
+    #include <string>
+    
+    
+    {}  // namespace caffe
+    
+    template <>
+struct NdarrayConverterGenerator::apply<Dtype*> {
+  struct type {
+    PyObject* operator() (Dtype* data) const {
+      // Just store the data pointer, and add the shape information in postcall.
+      return PyArray_SimpleNewFromData(0, NULL, NPY_DTYPE, data);
+    }
+    const PyTypeObject* get_pytype() {
+      return &PyArray_Type;
+    }
+  };
 };
     
-    U_NAMESPACE_BEGIN
+    void convert_dataset(const string& input_folder, const string& output_folder,
+    const string& db_type) {
+  scoped_ptr<db::DB> train_db(db::GetDB(db_type));
+  train_db->Open(output_folder + '/cifar10_train_' + db_type, db::NEW);
+  scoped_ptr<db::Transaction> txn(train_db->NewTransaction());
+  // Data buffer
+  int label;
+  char str_buffer[kCIFARImageNBytes];
+  Datum datum;
+  datum.set_channels(3);
+  datum.set_height(kCIFARSize);
+  datum.set_width(kCIFARSize);
+    }
     
-    // Bitwise comparison for the collation keys.
-Collator::EComparisonResult
-CollationKey::compareTo(const CollationKey& target) const
-{
-    UErrorCode errorCode = U_ZERO_ERROR;
-    return static_cast<Collator::EComparisonResult>(compareTo(target, errorCode));
+      gflags::SetUsageMessage('This script converts the MNIST dataset to\n'
+        'the lmdb/leveldb format used by Caffe to load data.\n'
+        'Usage:\n'
+        '    convert_mnist_data [FLAGS] input_image_file input_label_file '
+        'output_db_file\n'
+        'The MNIST dataset could be downloaded at\n'
+        '    http://yann.lecun.com/exdb/mnist/\n'
+        'You should gunzip them after downloading,'
+        'or directly use data/mnist/get_mnist.sh\n');
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+    
+    
+    {  int kernel_h_, kernel_w_;
+  int stride_h_, stride_w_;
+  int pad_h_, pad_w_;
+  int channels_;
+  int height_, width_;
+  int pooled_height_, pooled_width_;
+  bool global_pooling_;
+  PoolingParameter_RoundMode round_mode_;
+  Blob<Dtype> rand_idx_;
+  Blob<int> max_idx_;
+};
+    
+    // Common functions and classes from std that caffe often uses.
+using std::fstream;
+using std::ios;
+using std::isnan;
+using std::isinf;
+using std::iterator;
+using std::make_pair;
+using std::map;
+using std::ostringstream;
+using std::pair;
+using std::set;
+using std::string;
+using std::stringstream;
+using std::vector;
+    
+        /**
+     * Reattaches to the OperationContext and reacquires any storage-engine state.
+     *
+     * It is only legal to call this in the 'detached' state. On return, the cursor is left in a
+     * 'saved' state, so callers must still call restoreState to use this object.
+     */
+    virtual void reattachToOperationContext(OperationContext* opCtx) = 0;
+    
+    TEST(TopTest, CollectionDropped) {
+    Top().collectionDropped(NamespaceString('test.coll'));
 }
+    
+        pat     = RegexPattern::compile(regex, 0, pe, status);
+    matcher = pat->matcher(input, status);
+    retVal  = matcher->matches(status);
+    
+    U_CAPI void U_EXPORT2
+uhash_deleteScriptSet(void *obj);
+    
+    
+    // exception type_error.304
+    try
+    {
+        // use at() on a non-array type
+        json str = 'I am a string';
+        str.at(0) = 'Another string';
+    }
+    catch (json::type_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+        // change the string
+    j.at('/string'_json_pointer) = 'bar';
+    // output the changed string
+    std::cout << j['string'] << '\n';
+    
+        // print values
+    std::cout << object << '\n';
+    std::cout << null << '\n';
+    
+      Status ReuseWritableFile(const std::string& fname,
+                           const std::string& old_fname,
+                           std::unique_ptr<WritableFile>* result,
+                           const EnvOptions& options) override {
+    PERF_TIMER_GUARD(env_reuse_writable_file_nanos);
+    return EnvWrapper::ReuseWritableFile(fname, old_fname, result, options);
+  }
+    
+    // A merge operator that mimics Put semantics
+// Since this merge-operator will not be used in production,
+// it is implemented as a non-associative merge operator to illustrate the
+// new interface and for testing purposes. (That is, we inherit from
+// the MergeOperator class rather than the AssociativeMergeOperator
+// which would be simpler in this case).
+//
+// From the client-perspective, semantics are the same.
+class PutOperator : public MergeOperator {
+ public:
+  bool FullMerge(const Slice& /*key*/, const Slice* /*existing_value*/,
+                 const std::deque<std::string>& operand_sequence,
+                 std::string* new_value, Logger* /*logger*/) const override {
+    // Put basically only looks at the current/latest value
+    assert(!operand_sequence.empty());
+    assert(new_value != nullptr);
+    new_value->assign(operand_sequence.back());
+    return true;
+  }
+    }
+    
+    
+    {    // Check if the wait stopped due to timing out.
+    if (cv_status == std::cv_status::timeout) {
+      s = Status::TimedOut(Status::SubCode::kMutexTimeout);
+    }
+  }
+    
+      // Will be called while on the write thread before the write executes.  If
+  // this function returns a non-OK status, the write will be aborted and this
+  // status will be returned to the caller of DB::Write().
+  virtual Status Callback(DB* db) = 0;
+    
+      // atomic write
+  WriteBatch batch;
+  batch.Put(handles[0], Slice('key2'), Slice('value2'));
+  batch.Put(handles[1], Slice('key3'), Slice('value3'));
+  batch.Delete(handles[0], Slice('key'));
+  s = db->Write(WriteOptions(), &batch);
+  assert(s.ok());
+    
+    #include <rocksdb/compaction_filter.h>
+#include <rocksdb/db.h>
+#include <rocksdb/merge_operator.h>
+#include <rocksdb/options.h>
+    
+      {
+    std::string string_val;
+    // If it cannot pin the value, it copies the value to its internal buffer.
+    // The intenral buffer could be set during construction.
+    PinnableSlice pinnable_val(&string_val);
+    db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key2', &pinnable_val);
+    assert(pinnable_val == 'value');
+    // If the value is not pinned, the internal buffer must have the value.
+    assert(pinnable_val.IsPinned() || string_val == 'value');
+  }
