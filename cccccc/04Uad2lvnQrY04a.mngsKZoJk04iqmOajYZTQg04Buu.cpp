cf3eb6a7b80824b738f9ef94b4133205fae8809b
SUNsung <weatherius@gmail.com>
2019-08-12 07:36:23 +0300

    Rewrite [04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp] File in: 2019-08-12 07:36:23

--- cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
+++ cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
@@ -1,192 +1,74 @@
 
         
-          DCache.CBs.valueRetainCB(Value, nullptr);
-  DCache.Entries[CKey] = Value;
+        #include 'modules/common/proto/pnc_point.pb.h'
+#include 'modules/planning/proto/planning.pb.h'
     
-    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
-  int Ret = cache_get_and_retain(static_cast<cache_t*>(Impl),
-                                 const_cast<void*>(Key), Value_out);
-  return Ret == 0;
-}
+    #define AINFO_IF(cond) ALOG_IF(INFO, cond, MODULE_NAME)
+#define AWARN_IF(cond) ALOG_IF(WARN, cond, MODULE_NAME)
+#define AERROR_IF(cond) ALOG_IF(ERROR, cond, MODULE_NAME)
+#define AFATAL_IF(cond) ALOG_IF(FATAL, cond, MODULE_NAME)
+#define ALOG_IF(severity, cond, module) \
+  !(cond) ? (void)0                     \
+          : google::LogMessageVoidify() & ALOG_MODULE(module, severity)
     
     
     {
-    {/// Determine the imported CF type for the given typedef-name, or the empty
-/// string if this is not an imported CF type name.
-llvm::StringRef getCFTypeName(const clang::TypedefNameDecl *decl);
-}
-}
-    
-    #include 'swift/AST/DiagnosticConsumer.h'
-#include 'swift/AST/DiagnosticEngine.h'
-#include 'swift/Frontend/FrontendOptions.h'
-#include 'llvm/ADT/SetVector.h'
-#include 'llvm/Option/ArgList.h'
-#include <set>
-    
-    class Explosion;
-class LoadableTypeInfo;
-struct WitnessMetadata;
-    
-    #ifdef CAROTENE_NEON
-    // this ugly contruction is needed to avoid:
-    // /usr/lib/gcc/arm-linux-gnueabihf/4.8/include/arm_neon.h:3581:59: error: argument must be a constant
-    // return (int16x8_t)__builtin_neon_vshr_nv8hi (__a, __b, 1);
-    
-    template <typename T, typename WT>
-struct AddSaturate
-{
-    typedef T type;
-    }
-    
-            //horizontal convolution (last 3 lines)
-        x = 0;
-        f32* dstb = internal::getRowPtr(dstBase, dstStride, i-1);
-        f32* dstc = internal::getRowPtr(dstBase, dstStride, i);
-        for (; x <= colsn - 4; x += 4)
-        {
-            internal::prefetch(laneA + x + cn);
-            internal::prefetch(laneB + x + cn);
-            internal::prefetch(lanea + x + cn);
-box3x3f32_horiz_ll:
-            float32x4_t lane0a = vld1q_f32(laneA + x - cn);
-            float32x4_t lane2a = vld1q_f32(laneA + x + cn);
-            float32x4_t lane1a = vld1q_f32(laneA + x);
-    }
-    
-    #include 'common.hpp'
-    
-    
-    {            vs1 = vqadd_s32(vs1, vnz1);
-            vs2 = vqadd_s32(vs2, vnz2);
-            i += 4;
-        }
-    
-    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
-inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
-inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
-    
-    static const int kIconWidth = 16;
-static const int kIconHeight = 16;
-    
-    namespace extensions {
-NwAppQuitFunction::NwAppQuitFunction() {
-    }
-    }
-    
-          scw_->WriteImage(*bitmap);
-    
-    #endif //NW_SRC_API_NW_SCREEN_API_H_
-
-    
-    namespace CNTK
-{
-    const std::wstring versionKey = L'version';
-    const std::wstring typeKey = L'type';
-    const std::wstring uidKey = L'uid';
-    const std::wstring kindKey = L'kind';
-    const std::wstring dataTypeKey = L'data_type';
-    const std::wstring dynamicAxisKey = L'dynamic_axis';
-    const std::wstring isSparseKey = L'is_sparse';
-    const std::wstring nameKey = L'name';
-    const std::wstring needsGradientKey = L'needs_gradient';
-    const std::wstring shapeKey = L'shape';
-    const std::wstring valueKey = L'value';
-    const std::wstring opKey = L'op';
-    const std::wstring attributesKey = L'attributes';
-    const std::wstring inputsKey = L'inputs';
-    const std::wstring rootKey = L'root';
-    const std::wstring functionsKey = L'primitive_functions';
-    const std::wstring sampleCountKey = L'sample_count';
-    const std::wstring minibatchCountKey = L'minibatchCount'; // TODO: Python-style spelling
-    const std::wstring sweepCountKey = L'sweepCount';
-    const std::wstring unitKey = L'unit';
-    const std::wstring refMBSizeKey = L'ref_mb_size';
-    const std::wstring epochSizeKey = L'epoch_size';
-    const std::wstring scheduleKey = L'schedule';
-    const std::wstring learningRateScheduleKey = L'learnig_rate_schedule';
-    const std::wstring smoothedGradientsKey = L'smoothed_gradients';
-    const std::wstring noiseInjectionSeedKey = L'noise_injection_seed';
-    const std::wstring masterParameterUpdatedKey = L'master_parameter_updated';
-    const std::wstring smoothedCountKey = L'smoothed_count';
-    const std::wstring stateKey = L'state';
-    const std::wstring rngSeedKey = L'rng_seed';
-    const std::wstring rngOffsetKey = L'rng_offset';
-    const std::wstring blockFunctionCompositeKey = L'block_function_composite';
-    const std::wstring blockFunctionOpNameKey = L'block_function_op_name';
-    const std::wstring blockFunctionCompositeArgumentsMapKeysKey = L'block_function_composite_arguments_map_keys';
-    const std::wstring blockFunctionCompositeArgumentsMapValuesKey = L'block_function_composite_arguments_map_values';
-    const std::wstring internalWorkerStateKey = L'internal_worker_state';
-    const std::wstring externalWorkerStateKey = L'external_worker_state';
-    const std::wstring userDefinedStateKey = L'user_defined_state';
-    const std::wstring udfModuleNameKey = L'module';
-    const std::wstring udfFactoryMethodNameKey = L'deserialize_method';
-    const std::wstring nativeUDFKey = L'native';
-    }
-    
-    template <class ElemType>
-EpochAccumulatorNode<ElemType>::EpochAccumulatorNode(const Microsoft::MSR::ScriptableObjects::IConfigRecordPtr configp)
-    : EpochAccumulatorNode(configp->Get(L'deviceId'), L'<placeholder>')
-{
-    AttachInputsFromConfig(configp, this->GetExpectedNumInputs());
-}
-    
-        template <class ElemType>
-    MemRequestInfo<ElemType>* GetMemInfo(shared_ptr<Matrix<ElemType>> *pMatrixPtr)
     {
-        vector<MemRequestInfo<ElemType>>& memInfoVec = GetMemRequestInfoVec<ElemType>();
-        // iterate through the vector and find the pointer memInfo
-        for (auto& memInfo : memInfoVec)
-        {
-            if (memInfo.pMatrixPtrs[0] == pMatrixPtr)
-                return &memInfo;
-        }
-        return nullptr;
-    }
-    
-      if (comma != data) {
-    // we have meta
-    ssize_t meta_len = comma - data;
-    data_len -= meta_len;
-    char* semi = (char*)memchr(data, ';', meta_len);
-    char* slash = (char*)memchr(data, '/', meta_len);
-    }
-    
-      // implementing File
-  bool open(const String& filename, const String& mode) override;
-  bool close() override;
-  int64_t readImpl(char *buffer, int64_t length) override;
-  int getc() override;
-  int64_t writeImpl(const char *buffer, int64_t length) override;
-  bool seek(int64_t offset, int whence = SEEK_SET) override;
-  int64_t tell() override;
-  bool eof() override;
-  bool rewind() override;
-  bool flush() override;
-  bool truncate(int64_t size) override;
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    
-    {}
+    {}  // namespace camera
+}  // namespace perception
+}  // namespace apollo
 
     
-    #endif // incl_HPHP_PLAIN_FILE_H_
-
+     private:
+  Stage::StageStatus Process(const common::TrajectoryPoint& planning_init_point,
+                             Frame* frame) override;
     
-        // output element with JSON pointer '/number'
-    std::cout << j.at('/number'_json_pointer) << '\n';
-    // output element with JSON pointer '/string'
-    std::cout << j.at('/string'_json_pointer) << '\n';
-    // output element with JSON pointer '/array'
-    std::cout << j.at('/array'_json_pointer) << '\n';
-    // output element with JSON pointer '/array/1'
-    std::cout << j.at('/array/1'_json_pointer) << '\n';
+    namespace apollo {
+namespace planning {
+namespace scenario {
+namespace valet_parking {
+    }
+    }
+    }
+    }
     
-        // create string from a string_t value
-    json::string_t string_value = 'The quick brown fox jumps over the lazy dog.';
-    json j_string_t(string_value);
+    ErrorCode GemController::Init(
+    const VehicleParameter& params,
+    CanSender<::apollo::canbus::ChassisDetail>* const can_sender,
+    MessageManager<::apollo::canbus::ChassisDetail>* const message_manager) {
+  if (is_initialized_) {
+    AINFO << 'GemController has already been initialized.';
+    return ErrorCode::CANBUS_ERROR;
+  }
+  vehicle_params_.CopyFrom(
+      common::VehicleConfigHelper::Instance()->GetConfig().vehicle_param());
+  params_.CopyFrom(params);
+  if (!params_.has_driving_mode()) {
+    AERROR << 'Vehicle conf pb not set driving_mode.';
+    return ErrorCode::CANBUS_ERROR;
+  }
+    }
     
-        // the following call will not add an object, because there is already
-    // a value stored at key 'B'
-    auto res2 = null.emplace('B', 'c');
+    using apollo::common::Status;
+using apollo::common::time::Clock;
+    
+    
+    {  return true;
+}
+    
+    #include <string>
+    
+    /**
+ * @brief Wrap angle to [0, 2 * PI).
+ * @param angle the original value of the angle.
+ * @return The wrapped value of the angle.
+ */
+double WrapAngle(const double angle);
+    
+    
+    {
+    {    frame->detected_objects.push_back(obj);
+  }
+  fclose(fp);
+  return true;
+}
