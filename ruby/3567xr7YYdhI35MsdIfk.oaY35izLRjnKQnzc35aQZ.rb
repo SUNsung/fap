538226e2c06ce1e41f665f19417feda3fac11478
SUNsung <weatherius@gmail.com>
2019-08-13 17:37:51 +0300

    Rewrite [3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb] File in: 2019-08-13 17:37:51

--- ruby/3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb
+++ ruby/3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb
@@ -1,123 +1,83 @@
 
         
-              ',
-                  period_type: period_types[period_type],
-                  since: since,
-                  like_type: UserAction::LIKE,
-                  was_liked_type: UserAction::WAS_LIKED,
-                  new_topic_type: UserAction::NEW_TOPIC,
-                  reply_type: UserAction::REPLY,
-                  regular_post_type: Post.types[:regular]
-    
-        SCRIPT_ASSET_DIRECTORIES = [
-      # [dir, can_use_s3_cdn, can_use_cdn]
-      ['/assets/',             true, true],
-      ['/brotli_asset/',       true, true],
-      ['/extra-locales/',      false, false],
-      ['/highlight-js/',       false, true],
-      ['/javascripts/',        false, true],
-      ['/plugins/',            false, true],
-      ['/theme-javascripts/',  false, true],
-      ['/svg-sprite/',         false, true],
-    ]
-    
-        cookies[TOKEN_COOKIE] = cookie_hash(@user_token.unhashed_auth_token)
-    unstage_user(user)
-    make_developer_admin(user)
-    enable_bootstrap_mode(user)
-    @env[CURRENT_USER_KEY] = user
-  end
-    
-    require 'method_profiler'
-    
-    describe ScenarioImportsController do
-  let(:user) { users(:bob) }
-    
-        it 'is OK with no hl' do
-      stub(params).[](:hl) { nil }
-      expect((1..10).select { |i| highlighted?(i) }).to be_empty
+            should 'have the URL to the 'sanitized_path' item' do
+      assert_match %r!1\s/methods/sanitized_path\.html!, @result
     end
+    
+    $LOAD_PATH.unshift File.expand_path('lib', __dir__)
+require 'jekyll/version'
+    
+    FORWARD_SLASH = '/'.freeze
+    
+    if pathutil_relative == native_relative
+  Benchmark.ips do |x|
+    x.report('pathutil') { pathutil_relative }
+    x.report('native')   { native_relative }
+    x.compare!
   end
+else
+  print 'PATHUTIL: '
+  puts pathutil_relative
+  print 'NATIVE:   '
+  puts native_relative
 end
 
     
-        it 'defauls foreground and background colors' do
-      scenario.tag_fg_color = nil
-      scenario.tag_bg_color = nil
-      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
+            modes       = Array options[:escape]
+        @escaper    = options[:escaper]
+        @html       = modes.include? :html
+        @javascript = modes.include? :javascript
+        @url        = modes.include? :url
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   CSRF
+    # Supported browsers:: all
+    # More infos::         http://flask.pocoo.org/docs/0.10/security/#json-security
+    #                      http://haacked.com/archive/2008/11/20/anatomy-of-a-subtle-json-vulnerability.aspx
+    #
+    # JSON GET APIs are vulnerable to being embedded as JavaScript when the
+    # Array prototype has been patched to track data. Checks the referrer
+    # even on GET requests if the content type is JSON.
+    #
+    # If request includes Origin HTTP header, defers to HttpOrigin to determine
+    # if the request is safe. Please refer to the documentation for more info.
+    #
+    # The `:allow_if` option can be set to a proc to use custom allow/deny logic.
+    class JsonCsrf < Base
+      default_options :allow_if => nil
+    
+      it 'accepts post requests with masked X-CSRF-Token header' do
+    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => masked_token)
+    expect(last_response).to be_ok
+  end
+    
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
+    end
+    
+        it 'enqueues' do
+      Sidekiq.redis {|c| c.flushdb }
+      assert_equal Sidekiq.default_worker_options, MyWorker.get_sidekiq_options
+      assert MyWorker.perform_async(1, 2)
+      assert Sidekiq::Client.enqueue(MyWorker, 1, 2)
+      assert Sidekiq::Client.enqueue_to(:custom_queue, MyWorker, 1, 2)
+      assert_equal 1, Sidekiq::Queue.new('custom_queue').size
+      assert Sidekiq::Client.enqueue_to_in(:custom_queue, 3, MyWorker, 1, 2)
+      assert Sidekiq::Client.enqueue_to_in(:custom_queue, -3, MyWorker, 1, 2)
+      assert_equal 2, Sidekiq::Queue.new('custom_queue').size
+      assert Sidekiq::Client.enqueue_in(3, MyWorker, 1, 2)
+      assert QueuedWorker.perform_async(1, 2)
+      assert_equal 1, Sidekiq::Queue.new('flimflam').size
     end
   end
     
-      describe 'up' do
-    it 'should update extract and template options for an existing WebsiteAgent' do
-      expect(agent.options).to include('extract' => old_extract,
-                                       'template' => old_template)
-      ConvertWebsiteAgentTemplateForMerge.new.up
-      agent.reload
-      expect(agent.options).to include('extract' => new_extract,
-                                       'template' => new_template)
-    end
+        assert_nil dead_set.find_job('000103')
+    assert dead_set.find_job('000102')
+    assert dead_set.find_job('000101')
   end
     
-      end
-    
-      # Check if there is no signed in user before doing the sign out.
-  #
-  # If there is no signed in user, it will set the flash message and redirect
-  # to the after_sign_out path.
-  def verify_signed_out_user
-    if all_signed_out?
-      set_flash_message! :notice, :already_signed_out
-    
-    module Devise
-  module Controllers
-    # Provide sign in and sign out functionality.
-    # Included by default in all controllers.
-    module SignInOut
-      # Return true if the given scope is signed in session. If no scope given, return
-      # true if any scope is signed in. This will run authentication hooks, which may
-      # cause exceptions to be thrown from this method; if you simply want to check
-      # if a scope has already previously been authenticated without running
-      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
-      def signed_in?(scope=nil)
-        [scope || Devise.mappings.keys].flatten.any? do |_scope|
-          warden.authenticate?(scope: _scope)
-        end
-      end
-    
-          def devise_mapping
-        @devise_mapping ||= Devise.mappings[scope_name]
-      end
-    
-    module Devise
-  module Models
-    class MissingAttribute < StandardError
-      def initialize(attributes)
-        @attributes = attributes
-      end
-    
-          def time_from_json(value)
-        if value =~ /\A\d+\.\d+\Z/
-          Time.at(value.to_f)
-        else
-          Time.parse(value) rescue nil
-        end
-      end
-    
-        def log_status(status)
-      puts bold status
-    end
-    
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
-    
-        private
-    
-        def define_query
-      name = @name
-      @klass.send :define_method, '#{@name}?' do
-        send(name).file?
-      end
-    end
+          assert_equal 0, ss.size
