66c392276896db12ca2b73a2690a63d9099bd93f
SUNsung <weatherius@gmail.com>
2019-08-13 15:19:24 +0300

    Rewrite [548uJca.8Nggo54rTsWL4Qd0J.54G0N.4l.X8Tc54szm.cpp] File in: 2019-08-13 15:19:24

--- cccccc/548uJca.8Nggo54rTsWL4Qd0J.54G0N.4l.X8Tc54szm.cpp
+++ cccccc/548uJca.8Nggo54rTsWL4Qd0J.54G0N.4l.X8Tc54szm.cpp
@@ -1,153 +1,160 @@
 
         
-            if (offset < 0) {
-      // Relative to end of file; make it absolute
-      if (-offset > file_size) {
-        offset = 0;
-      } else {
-        offset = file_size + offset;
-      }
-    }
-    if (offset > file_size) {
-      offset = file_size;
-    }
-    if (offset + bytes_to_corrupt > file_size) {
-      bytes_to_corrupt = file_size - offset;
-    }
+                    m_lastVal = 0;
+            if (IsBinOpCode(m_nLastCom))
+            {
+                // We want 1 + ( to start as 1 + (0. Any number you type replaces 0. But if it is 1 + 3 (, it is
+                // treated as 1 + (3
+                m_currentVal = 0;
+            }
+            m_nTempCom = 0;
+            m_nOpCode = 0;
+            m_bChangeOp = false; // a ( is like starting a fresh sub equation
+        }
+        else
+        {
+            // Last thing keyed in was an operator. Lets do the op on a duplicate of the last entry.
+            if (IsBinOpCode(m_nLastCom))
+            {
+                m_currentVal = m_lastVal;
+            }
     
-      ~Reader();
     
-    #endif  // STORAGE_LEVELDB_DB_VERSION_SET_H_
+    {    Rational Sinh(Rational const& rat);
+    Rational Cosh(Rational const& rat);
+    Rational Tanh(Rational const& rat);
+    Rational ASinh(Rational const& rat);
+    Rational ACosh(Rational const& rat);
+    Rational ATanh(Rational const& rat);
+}
 
     
-      std::string Integer(RegTree const& tree, int32_t nid, uint32_t depth) override {
-    static std::string const kIntegerTemplate =
-        '{tabs}{nid}:[{fname}<{cond}] yes={left},no={right},missing={missing}';
-    auto cond = tree[nid].SplitCond();
-    const bst_float floored = std::floor(cond);
-    const int32_t integer_threshold
-        = (floored == cond) ? static_cast<int>(floored)
-        : static_cast<int>(floored) + 1;
-    return SplitNodeImpl(tree, nid, kIntegerTemplate,
-                         std::to_string(integer_threshold), depth);
-  }
+    //-----------------------------------------------------------------------------
+//
+//    FUNCTION: ratpowi32
+//
+//    ARGUMENTS: root as rational, power as int32_t and precision as int32_t.
+//
+//    RETURN: None root is changed.
+//
+//    DESCRIPTION: changes rational representation of root to
+//    root ** power.
+//
+//-----------------------------------------------------------------------------
     
-    RegTree ConstructTree() {
-  RegTree tree;
-  tree.ExpandNode(
-      /*nid=*/0, /*split_index=*/0, /*split_value=*/0.0f,
-      /*default_left=*/true,
-      0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
-  auto left = tree[0].LeftChild();
-  auto right = tree[0].RightChild();
-  tree.ExpandNode(
-      /*nid=*/left, /*split_index=*/1, /*split_value=*/1.0f,
-      /*default_left=*/false,
-      0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
-  tree.ExpandNode(
-      /*nid=*/right, /*split_index=*/2, /*split_value=*/2.0f,
-      /*default_left=*/false,
-      0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
-  return tree;
+    // Chops off the current equation string from the given index
+void CHistoryCollector::TruncateEquationSzFromIch(int ich)
+{
+    // Truncate commands
+    int minIdx = -1;
+    unsigned int nTokens = 0;
+    std::pair<std::wstring, int> currentPair;
+    m_spTokens->GetSize(&nTokens);
+    }
+    
+    //------------------------------------------------------------------------
+// PSOutputDev
+//------------------------------------------------------------------------
+    
+    //------------------------------------------------------------------------
+// PSOutputDev
+//------------------------------------------------------------------------
+    
+    StandardSecurityHandler::StandardSecurityHandler(PDFDoc *docA,
+						 Object *encryptDictA):
+  SecurityHandler(docA)
+{
+  Object versionObj, revisionObj, lengthObj;
+  Object ownerKeyObj, userKeyObj, permObj, fileIDObj;
+  Object fileIDObj1;
+  Object cryptFiltersObj, streamFilterObj, stringFilterObj;
+  Object cryptFilterObj, cfmObj, cfLengthObj;
+  Object encryptMetadataObj;
+    }
+    
+    Sound::~Sound()
+{
+  delete fileName;
+  streamObj->free();
+  delete streamObj;
 }
     
-    #if !defined(XGBOOST_USE_CUDA)
-int AllVisibleImpl::AllVisible() {
-  return 0;
-}
-#endif  // !defined(XGBOOST_USE_CUDA)
     
-    namespace xgboost {
-namespace common {
-    }
-    }
-    
-    class SparsePageRawFormat : public SparsePageFormat {
- public:
-  bool Read(SparsePage* page, dmlc::SeekStream* fi) override {
-    auto& offset_vec = page->offset.HostVector();
-    if (!fi->Read(&offset_vec)) return false;
-    auto& data_vec = page->data.HostVector();
-    CHECK_NE(page->offset.Size(), 0U) << 'Invalid SparsePage file';
-    data_vec.resize(offset_vec.back());
-    if (page->data.Size() != 0) {
-      CHECK_EQ(fi->Read(dmlc::BeginPtr(data_vec),
-                        (page->data).Size() * sizeof(Entry)),
-               (page->data).Size() * sizeof(Entry))
-          << 'Invalid SparsePage file';
-    }
-    return true;
+    {  switch (cm) {
+  case splashModeMono1:
+  case splashModeMono8:
+    blend[0] = dest[0];
+    break;
+  case splashModeXBGR8:
+    src[3] = 255;
+  case splashModeRGB8:
+  case splashModeBGR8:
+    cvtRGBToHSV(src[0], src[1], src[2], &hs, &ss, &vs);
+    cvtRGBToHSV(dest[0], dest[1], dest[2], &hd, &sd, &vd);
+    cvtHSVToRGB(hs, ss, vd, &blend[0], &blend[1], &blend[2]);
+    break;
+#if SPLASH_CMYK
+  case splashModeCMYK8:
+    SplashColor rgbSrc;
+    SplashColor rgbDest;
+    SplashColor rgbBlend;
+    cmykToRGB(src, rgbSrc);
+    cmykToRGB(dest, rgbDest);
+    cvtRGBToHSV(rgbSrc[0], rgbSrc[1], rgbSrc[2], &hs, &ss, &vs);
+    cvtRGBToHSV(rgbDest[0], rgbDest[1], rgbDest[2], &hd, &sd, &vd);
+    cvtHSVToRGB(hs, ss, vd, &rgbBlend[0], &rgbBlend[1], &rgbBlend[2]);
+	rgbToCMYK(rgbBlend, blend);
+    break;
+#endif
   }
+}
+    
+      GBool isReverseVideo() { return reverseVideo; }
+  void setReverseVideo(GBool reverseVideoA) { reverseVideo = reverseVideoA; }
+    
+    DECLARE_bool(disable_database);
+    
+    /**
+ * @brief The backing store key name for the executing query.
+ *
+ * The config maintains schedule statistics and tracks failed executions.
+ * On process or worker resume an initializer or config may check if the
+ * resume was the result of a failure during an executing query.
+ */
+const std::string kExecutingQuery{'executing_query'};
+const std::string kFailedQueries{'failed_queries'};
+    
+    
+    {  Status update(const std::string& source,
+                const ParserConfig& config) override {
+    source_ = source;
+    config_.clear();
+    for (const auto& entry : config) {
+      std::string content;
+      entry.second.toString(content);
+      config_[entry.first] = content;
     }
+    return Status::success();
+  }
+  std::string source_{''};
+  std::map<std::string, std::string> config_;
+};
+    
+    #include <osquery/config/tests/test_utils.h>
+    
+    #include <osquery/core/database/database.h>
+#include <osquery/utils/conversions/tryto.h>
     
     
-    {   private:
-    /*!
-     * \brief a union value of value and flag
-     *  when flag == -1, this indicate the value is missing
-     */
-    union Entry {
-      bst_float fvalue;
-      int flag;
-    };
-    std::vector<Entry> data_;
-  };
-  /*!
-   * \brief get the leaf index
-   * \param feat dense feature vector, if the feature is missing the field is set to NaN
-   * \param root_id starting root index of the instance
-   * \return the leaf index of the given feature
-   */
-  int GetLeafIndex(const FVec& feat, unsigned root_id = 0) const;
-  /*!
-   * \brief calculate the feature contributions (https://arxiv.org/abs/1706.06060) for the tree
-   * \param feat dense feature vector, if the feature is missing the field is set to NaN
-   * \param root_id starting root index of the instance
-   * \param out_contribs output vector to hold the contributions
-   * \param condition fix one feature to either off (-1) on (1) or not fixed (0 default)
-   * \param condition_feature the index of the feature to fix
-   */
-  void CalculateContributions(const RegTree::FVec& feat, unsigned root_id,
-                              bst_float* out_contribs, int condition = 0,
-                              unsigned condition_feature = 0) const;
-  /*!
-   * \brief Recursive function that computes the feature attributions for a single tree.
-   * \param feat dense feature vector, if the feature is missing the field is set to NaN
-   * \param phi dense output vector of feature attributions
-   * \param node_index the index of the current node in the tree
-   * \param unique_depth how many unique features are above the current node in the tree
-   * \param parent_unique_path a vector of statistics about our current path through the tree
-   * \param parent_zero_fraction what fraction of the parent path weight is coming as 0 (integrated)
-   * \param parent_one_fraction what fraction of the parent path weight is coming as 1 (fixed)
-   * \param parent_feature_index what feature the parent node used to split
-   * \param condition fix one feature to either off (-1) on (1) or not fixed (0 default)
-   * \param condition_feature the index of the feature to fix
-   * \param condition_fraction what fraction of the current weight matches our conditioning feature
-   */
-  void TreeShap(const RegTree::FVec& feat, bst_float* phi, unsigned node_index,
-                unsigned unique_depth, PathElement* parent_unique_path,
-                bst_float parent_zero_fraction, bst_float parent_one_fraction,
-                int parent_feature_index, int condition,
-                unsigned condition_feature, bst_float condition_fraction) const;
+    {  fs::rename(src_path, dst_path, ec);
+  if (!ec) {
+    return createError(RocksdbMigrationError::FailMoveDatabase)
+           << 'Move failed: ' << ec.value() << ' ' << ec.message();
+  }
+  return Success();
+}
     
-    #include 'imgui.h'      // IMGUI_API, ImFontAtlas
     
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-            // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin('Another Window', &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text('Hello from another window!');
-            if (ImGui::Button('Close Me'))
-                show_another_window = false;
-            ImGui::End();
-        }
-    
-        for (UINT i = 0; i < NUM_FRAMES_IN_FLIGHT; i++)
-        if (g_pd3dDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&g_frameContext[i].CommandAllocator)) != S_OK)
-            return false;
-    
-    struct ID3D10Device;
+    { public:
+  Status call(const PluginRequest& request, PluginResponse& response) override;
+};
