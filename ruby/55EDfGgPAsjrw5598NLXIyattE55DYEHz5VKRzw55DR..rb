280387aa381f368f63f383407a2d32dd15c79147
SUNsung <weatherius@gmail.com>
2019-08-13 14:37:26 +0300

    Rewrite [55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb] File in: 2019-08-13 14:37:25

--- ruby/55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb
+++ ruby/55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb
@@ -1,200 +1,62 @@
 
         
-            def aes256_gcm_encrypt(value)
-      encrypted_token = Encryptor.encrypt(AES256_GCM_OPTIONS.merge(value: value))
-      Base64.strict_encode64(encrypted_token)
-    end
-    
-    describe ProjectMemberPresenter do
-  let(:user) { double(:user) }
-  let(:project) { double(:project) }
-  let(:project_member) { double(:project_member, source: project) }
-  let(:presenter) { described_class.new(project_member, current_user: user) }
-    
-        def chronic_duration_attr_writer(virtual_attribute, source_attribute, parameters = {})
-      chronic_duration_attr_reader(virtual_attribute, source_attribute)
-    
-              # Already exists
-          expect(weather_agent_diff.agent).to eq(agents(:bob_weather_agent))
-          valid_parsed_weather_agent_data.each do |key, value|
-            next if key == :type
-            expect(weather_agent_diff.send(key).current).to eq(agents(:bob_weather_agent).send(key))
-          end
-    
-          expect(exporter.as_json[:links]).to eq([{ :source => guid_order(agent_list, :jane_weather_agent), :receiver => guid_order(agent_list, :jane_rain_notifier_agent) }])
-    end
-    
-        it 'work with set FAILED_JOBS_TO_KEEP env variable' do
-      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(-1)
-      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(0)
-      expect(@keep.id).to eq(Delayed::Job.order(:failed_at)[0].id)
-    end
-    
-      describe '#complete_project_id' do
-    it 'should return a array of hashes' do
-      expect(@checker.complete_project_id).to eq [{text: 'test (1234)', id: 1234}, {text: 'test1 (1235)', id: 1235}]
-    end
-  end
-    
-        # The path used after confirmation.
-    def after_confirmation_path_for(resource_name, resource)
-      if signed_in?(resource_name)
-        signed_in_root_path(resource)
-      else
-        new_session_path(resource_name)
-      end
-    end
-    
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
-    
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
-    
-          def mailer_from(mapping)
-        mailer_sender(mapping, :from)
-      end
-    
-        # Creates configuration values for Devise and for the given module.
-    #
-    #   Devise::Models.config(Devise::Models::DatabaseAuthenticatable, :stretches)
-    #
-    # The line above creates:
-    #
-    #   1) An accessor called Devise.stretches, which value is used by default;
-    #
-    #   2) Some class methods for your model Model.stretches and Model.stretches=
-    #      which have higher priority than Devise.stretches;
-    #
-    #   3) And an instance method stretches.
-    #
-    # To add the class methods you need to have a module ClassMethods defined
-    # inside the given class.
-    #
-    def self.config(mod, *accessors) #:nodoc:
-      class << mod; attr_accessor :available_configs; end
-      mod.available_configs = accessors
-    
-            if Devise.activerecord51?
-          def clear_reset_password_token?
-            encrypted_password_changed = respond_to?(:will_save_change_to_encrypted_password?) && will_save_change_to_encrypted_password?
-            authentication_keys_changed = self.class.authentication_keys.any? do |attribute|
-              respond_to?('will_save_change_to_#{attribute}?') && send('will_save_change_to_#{attribute}?')
-            end
-    
-        def check_https_availability
-      return unless download
-    
-                problem 'xcodebuild should be passed an explicit 'SYMROOT''
-          end
-    
-            it 'returns false if all are higher' do
-          setup_java_with_version '1.8.0_5'
-          expect(subject).not_to be_satisfied
-        end
-      end
-    
-        expect do
-      described_class.run('local-transmission', 'local-caffeine')
-    end.to output('#{transmission_location}\n#{caffeine_location}\n').to_stdout
-  end
-    
-        def quarantine
-      return if @quarantine.nil?
-      return unless Quarantine.available?
-    
-          if privs.include?('SeDebugPrivilege')
-        print_good('Got SeDebugPrivilege.')
-      else
-        print_warning('Unable to get SeDebugPrivilege.')
-      end
-    end
+        def codepoints_to_unicode(codepoints)
+  if codepoints.include?(' ')
+    codepoints.split(' ').map(&:hex).pack('U*')
+  else
+    [codepoints.hex].pack('U')
   end
 end
     
-      def run
-    res = send_request_raw(
-        'uri' => normalize_uri(target_uri.path.to_s),
-        'method' => 'GET',
-    )
-    model = check_response_fingerprint(res, Exploit::CheckCode::Detected)
-    if model != Exploit::CheckCode::Detected
-      devices = devices_list[model.to_sym]
-      devices = devices_list['ALL'.to_sym] if devices.nil? && datastore['ForceAttempt']
-      if devices != nil
-        print_good('Detected device:#{devices[:name]} #{devices[:model]}')
-        devices[:values].each { |value|
-          cookie = 'C#{value[0]}=#{'B'*value[1]}\x00'
-          res = send_request_raw(
-              'uri' => normalize_uri(target_uri.path.to_s),
-              'method' => 'GET',
-              'headers' => headers.merge('Cookie' => cookie)
-          )
-          if res != nil and res.code <= 302
-            print_good('Good response, please check host, authentication should be disabled')
-            break
-          else
-            print_error('Bad response')
-          end
-        }
-      else
-        print_error('No matching values for fingerprint #{model}')
-      end
-    else
-      print_error('Unknown device')
-    end
+        let(:follow)  { double(account: account, errors: errors) }
+    let(:errors)  { double(add: nil) }
+    let(:account) { double(nil?: _nil, local?: local, following_count: 0, followers_count: 0) }
+    let(:_nil)    { true }
+    let(:local)   { false }
+    
+      def eligible_users
+    User.emailable
+        .where('current_sign_in_at < ?', (FREQUENCY + SIGN_IN_OFFSET).ago)
+        .where('last_emailed_at IS NULL OR last_emailed_at < ?', FREQUENCY.ago)
   end
 end
 
     
-    # Pseudo-modules
-require 'msf/core/handler'
-    
-        # Create a directory for the logs
-    logs = if log_path
-             ::File.join(log_path, 'logs', 'persistence', Rex::FileUtils.clean_path(host + filenameinfo))
-           else
-             ::File.join(Msf::Config.log_directory, 'persistence', Rex::FileUtils.clean_path(host + filenameinfo))
-           end
-    
-        membership = contact.aspect_memberships.where(aspect_id: aspect.id).first
-    
-          a = $count
-      p.start
-      sleep(0.05)
-      p.terminate
-      p.kill(true)
-    
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-          get_web_content(redirected_url)
+          it 'sends email' do
+        is_expected.to change(ActionMailer::Base.deliveries, :count).by(1)
+      end
     end
     
-        def render(context)
-      includes_dir = File.join(context.registers[:site].source, '_includes')
+            let(:object_json) do
+          {
+            id: 'https://example.com/actor#bar',
+            type: 'Note',
+            content: 'Lorem ipsum',
+            to: 'http://example.com/followers',
+            attributedTo: 'https://example.com/actor',
+          }
+        end
     
-      # Improved version of Liquid's truncatewords:
-  # - Uses typographically correct ellipsis (…) insted of '...'
-  def truncatewords(input, length)
-    truncate = input.split(' ')
-    if truncate.length > length
-      truncate[0..length-1].join(' ').strip + ' &hellip;'
-    else
-      input
-    end
+    SimpleForm.setup do |config|
+  # Wrappers are used by the form builder to generate a
+  # complete input. You can remove any component from the
+  # wrapper, change the order or even add your own to the
+  # stack. The options given below are used to wrap the
+  # whole input.
+  config.wrappers :default, class: :input, hint_class: :field_with_hint, error_class: :field_with_errors do |b|
+    ## Extensions enabled by default
+    # Any of these extensions can be disabled for a
+    # given input by passing: `f.input EXTENSION_NAME => false`.
+    # You can make any of these extensions optional by
+    # renaming `b.use` to `b.optional`.
+    
+    # create and write to opml file
+xml = Builder::XmlMarkup.new(indent: 2)
+xml.instruct! :xml, version: '1.0', encoding: 'UTF-8'
+xml.tag!('opml', version: '1.0') do
+  # head
+  xml.tag!('head') do
+    xml.title TITLE
   end
+    
+        template = (@article.decorate.cached_tag_list_array & SHE_CODED_TAGS).any? ? 'shecoded' : 'article'
