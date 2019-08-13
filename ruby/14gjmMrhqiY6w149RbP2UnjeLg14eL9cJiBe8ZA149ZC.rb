434c9488feb18b8009ac58ee44219a0fd4556049
SUNsung <weatherius@gmail.com>
2019-08-13 20:16:32 +0300

    Rewrite [14gjmMrhqiY6w149RbP2UnjeLg14eL9cJiBe8ZA149ZC.rb] File in: 2019-08-13 20:16:32

--- ruby/14gjmMrhqiY6w149RbP2UnjeLg14eL9cJiBe8ZA149ZC.rb
+++ ruby/14gjmMrhqiY6w149RbP2UnjeLg14eL9cJiBe8ZA149ZC.rb
@@ -1,178 +1,61 @@
 
         
-          # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-    
-    require 'isolation/abstract_unit'
-    
-        should 'return false with no glob patterns' do
-      assert !@filter.glob_include?([], 'a.txt')
+            def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
     end
     
-          def [](key)
-        if key != 'posts' && @obj.collections.key?(key)
-          @obj.collections[key].docs
-        else
-          super(key)
-        end
-      end
+    Gem::Specification.new do |s|
+  s.name          = 'tmuxinator'
+  s.version       = Tmuxinator::VERSION
+  s.authors       = ['Allen Bargi', 'Christopher Chow']
+  s.email         = ['allen.bargi@gmail.com', 'chris@chowie.net']
+  s.description   = %q{Create and manage complex tmux sessions easily.}
+  s.summary       = %q{Create and manage complex tmux sessions easily.}
+  s.homepage      = 'https://github.com/tmuxinator/tmuxinator'
+  s.license       = 'MIT'
     
-    # Just a slash
-Benchmark.ips do |x|
-  path = '/'
-  x.report('pre_pr:#{path}')    { pre_pr(path) }
-  x.report('pr:#{path}')        { pr(path) }
-  x.report('envygeeks:#{path}') { pr(path) }
-  x.compare!
-end
-    
-    def global_require
-  JSON.pretty_generate(DATA)
-end
-    
-        def upload_purchase_review_screenshot(app_id, upload_file, content_provider_id, sso_token_for_image)
-      upload_file(app_id: app_id, upload_file: upload_file, path: '/upload/image', content_provider_id: content_provider_id, sso_token: sso_token_for_image, du_validation_rule_set: get_picture_type(upload_file))
-    end
-    
-          def setup
-        # Since September 2015 we don't get the url any more, so we have to manually build it
-        self.url = '#{HOST_URL}/#{self.asset_token}'
-      end
-    
-          # @return (Boolean) Display Trade Representative Contact Information on the Korean App Store or not
-      attr_accessor :trade_representative_is_displayed_on_app_store
-    
-        #####################################################
-    # @!group Applications
-    #####################################################
-    
-          context 'with appendix' do
-        it 'returns the current version once parsed with appendix' do
-          test_content = 'spec.version = '1.3.2.4''
-          result = @version_podspec_file.parse(test_content)
-          expect(result).to eq('1.3.2.4')
-          expect(@version_podspec_file.version_value).to eq('1.3.2.4')
-          expect(@version_podspec_file.version_match[:major]).to eq('1')
-          expect(@version_podspec_file.version_match[:minor]).to eq('3')
-          expect(@version_podspec_file.version_match[:patch]).to eq('2')
-          expect(@version_podspec_file.version_match[:appendix]).to eq('.4')
-        end
-    
-            return parts
-      end
-    
-            # @return (Array) Returns all iPods registered for this account
-        def all_ipod_touches
-          client.devices_by_class('ipod').map { |device| self.factory(device) }
-        end
-    
-          def self.fill_in_default_values(params)
-        embed = Actions.lane_context[Actions::SharedValues::SIGH_PROFILE_PATH] || ENV['SIGH_PROFILE_PATH']
-        params[:embed] ||= embed if embed
-        params
-      end
-    
-      # ==> Configuration for :lockable
-  # Defines which strategy will be used to lock an account.
-  # :failed_attempts = Locks an account after a number of failed attempts to sign in.
-  # :none            = No lock strategy. You should handle locking by yourself.
-  # config.lock_strategy = :failed_attempts
-    
-              render 'admins/pods'
-        end
-        format.mobile { render 'admins/pods' }
-        format.json { render json: pods_json }
+        def list
+      say 'tmuxinator projects:'
+      if options[:newline]
+        say Tmuxinator::Config.configs.join('\n')
+      else
+        print_in_columns Tmuxinator::Config.configs
       end
     end
     
-      private
-    
-      def inspect_source(source, file = nil)
-    RuboCop::Formatter::DisabledConfigFormatter.config_to_allow_offenses = {}
-    RuboCop::Formatter::DisabledConfigFormatter.detected_styles = {}
-    processed_source = parse_source(source, file)
-    raise 'Error parsing example code' unless processed_source.valid_syntax?
-    
-          it 'registers an offense if no method is defined' do
-        src = <<~RUBY
-          class << A
-            #{modifier}
-          end
-        RUBY
-        inspect_source(src)
-        expect(cop.offenses.size).to eq(1)
-      end
-    
-          it { expect(send_node.lambda?).to be_falsey }
-    end
-    
-          it 'does auto-correction' do
-        corrected = autocorrect_source(source)
-        expect(corrected).to eq(<<~RUBY)
-          foo def a
-            a1
-              end
-    
-        it 'auto-correct Hash.new to {} as the first parameter to a method' do
-      source = 'yadayada.map { a }.reduce Hash.new, :merge'
-      new_source = autocorrect_source(source)
-      expect(new_source).to eq('yadayada.map { a }.reduce({}, :merge)')
-    end
-    
-              it_behaves_like 'registers an offense',
-                          'Use the `lambda` method for multiline lambdas.'
-          it_behaves_like 'auto-correct', <<~RUBY
-            lambda do |hello|
-              puts hello
-            end
-          RUBY
-        end
-    
-        context 'with EnforcedStyle: forbid_for_equality_operators_only' do
-      let(:cop_config) do
-        { 'EnforcedStyle' => 'forbid_for_equality_operators_only' }
-      end
-    
-    module RuboCop
-  module AST
-    # A node extension for `for` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `for` nodes within RuboCop.
-    class ForNode < Node
-      # Returns the keyword of the `for` statement as a string.
-      #
-      # @return [String] the keyword of the `until` statement
-      def keyword
-        'for'
-      end
-    
-    module RuboCop
-  module AST
-    # A node extension for `pair` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `pair` nodes within RuboCop.
-    class PairNode < Node
-      include HashElementNode
-    
-        context 'when story is a user' do
-      it 'renders to appropriate page if user changes username' do
-        old_username = user.username
-        user.update(username: 'new_hotness_#{rand(10_000)}')
-        get '/#{old_username}/#{article.slug}'
-        expect(response.body).to redirect_to('/#{user.username}/#{article.slug}')
-      end
-    
-    class ConfigTag < Liquid::Tag
-  def initialize(tag_name, options, tokens)
-    super
-    options = options.split(' ').map {|i| i.strip }
-    @key = options.slice!(0)
-    @tag = nil
-    @classname = nil
-    options.each do |option|
-      @tag = $1 if option =~ /tag:(\S+)/ 
-      @classname = $1 if option =~ /classname:(\S+)/
+        it 'checks requirements' do
+      expect(Tmuxinator::Doctor).to receive(:installed?)
+      expect(Tmuxinator::Doctor).to receive(:editor?)
+      expect(Tmuxinator::Doctor).to receive(:shell?)
+      capture_io { cli.start }
     end
   end
     
+      factory :project_with_alias, class: Tmuxinator::Project do
+    transient do
+      file { 'spec/fixtures/sample_alias.yml' }
+    end
+    
+        it 'removes settings from args' do
+      described_class.parse_settings(args)
+      expect(args).to eq(['one'])
+    end
+  end
+    
+      if base_index = options.fetch(:base_index) { 1 }
+    standard_options << 'base-index #{base_index}'
+  end
+    
+      describe '#tmux' do
+    it { expect(instance.tmux).to eq 'wemux' }
+  end
+end
+
+    
+          it 'returns true' do
+        expect(described_class.installed?).to be_truthy
+      end
     end
