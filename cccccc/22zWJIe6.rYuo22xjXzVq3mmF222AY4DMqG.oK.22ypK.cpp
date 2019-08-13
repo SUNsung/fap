af9af8c9f54db214441bf2d190d9c3234675fccc
SUNsung <weatherius@gmail.com>
2019-08-13 22:37:28 +0300

    Rewrite [22zWJIe6.rYuo22xjXzVq3mmF222AY4DMqG.oK.22ypK.cpp] File in: 2019-08-13 22:37:27

--- cccccc/22zWJIe6.rYuo22xjXzVq3mmF222AY4DMqG.oK.22ypK.cpp
+++ cccccc/22zWJIe6.rYuo22xjXzVq3mmF222AY4DMqG.oK.22ypK.cpp
@@ -1,169 +1,172 @@
 
         
-          auto* message2_on_arena =
-      Arena::CreateMessage<protobuf_unittest::TestAllTypes>(&arena);
+          Blob<Dtype>* blob_label = new Blob<Dtype>();
+  hdf5_load_nd_dataset(file_id, HDF5_DATA_LABEL_NAME, 0, 4,
+                       blob_label, reshape);
+  this->CheckBlobEqual(*(this->blob_label_), *blob_label);
     
-    // Author: kenton@google.com (Kenton Varda)
+    #include 'caffe/filler.hpp'
+#include 'caffe/layers/inner_product_layer.hpp'
+#include 'caffe/util/math_functions.hpp'
     
-      desired_output_for_decode = 'abcdefghIJ';
-  expected = string('\x0A\x0', 2);
-  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
-                                                     desired_output_for_decode);
-  EXPECT_EQ(expected, result);
-    
-    TEST(ByteSourceTest, CopyTo) {
-  StringPiece data('Hello world!');
-  MockByteSource source(data, 3);
-  string str;
-  StringByteSink sink(&str);
-    }
-    
-    // Author: brianolson@google.com (Brian Olson)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
-//
-// Test program to verify that GzipInputStream is compatible with command line
-// gunzip or java.util.zip.GzipInputStream
-//
-// Reads gzip stream on standard input and writes decompressed data to standard
-// output.
-    
-    string StripProto(string filename) {
-  return filename.substr(0, filename.rfind('.proto'));
+    template <typename Dtype>
+void Solver<Dtype>::Restore(const char* state_file) {
+  string state_filename(state_file);
+  if (state_filename.size() >= 3 &&
+      state_filename.compare(state_filename.size() - 3, 3, '.h5') == 0) {
+    RestoreSolverStateFromHDF5(state_filename);
+  } else {
+    RestoreSolverStateFromBinaryProto(state_filename);
+  }
 }
     
-      if (argc != 2) {
-    cerr << 'Usage:  ' << argv[0] << ' ADDRESS_BOOK_FILE' << endl;
-    return -1;
+    
+    {  if (argc != 4) {
+    gflags::ShowUsageWithFlagsRestrict(argv[0],
+        'examples/mnist/convert_mnist_data');
+  } else {
+    google::InitGoogleLogging(argv[0]);
+    convert_dataset(argv[1], argv[2], argv[3], db_backend);
   }
-    
-      // Optional:  Delete all global objects allocated by libprotobuf.
-  google::protobuf::ShutdownProtobufLibrary();
-    
-        // 7. Allocate texture
-    atlas->TexHeight = (atlas->Flags & ImFontAtlasFlags_NoPowerOfTwoHeight) ? (atlas->TexHeight + 1) : ImUpperPowerOfTwo(atlas->TexHeight);
-    atlas->TexUvScale = ImVec2(1.0f / atlas->TexWidth, 1.0f / atlas->TexHeight);
-    atlas->TexPixelsAlpha8 = (unsigned char*)IM_ALLOC(atlas->TexWidth * atlas->TexHeight);
-    memset(atlas->TexPixelsAlpha8, 0, atlas->TexWidth * atlas->TexHeight);
-    
-    namespace ImGuiFreeType
-{
-    // Hinting greatly impacts visuals (and glyph sizes).
-    // When disabled, FreeType generates blurrier glyphs, more or less matches the stb's output.
-    // The Default hinting mode usually looks good, but may distort glyphs in an unusual way.
-    // The Light hinting mode generates fuzzier glyphs but better matches Microsoft's rasterizer.
-    }
-    
-    struct ID3D10Device;
-    
-    IMGUI_IMPL_API bool     ImGui_ImplDX9_Init(IDirect3DDevice9* device);
-IMGUI_IMPL_API void     ImGui_ImplDX9_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplDX9_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
-    
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_IMPL_API void     ImGui_Marmalade_InvalidateDeviceObjects();
-IMGUI_IMPL_API bool     ImGui_Marmalade_CreateDeviceObjects();
-    
-                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox('Another Window', &show_another_window);
-    
-    // Called by Init/NewFrame/Shutdown
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateFontsTexture();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyFontsTexture();
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateDeviceObjects();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyDeviceObjects();
+  return 0;
+}
+#else
+int main(int argc, char** argv) {
+  LOG(FATAL) << 'This example requires LevelDB and LMDB; ' <<
+  'compile with USE_LEVELDB and USE_LMDB.';
+}
+#endif  // USE_LEVELDB and USE_LMDB
 
     
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+    
+    {  int kernel_h_, kernel_w_;
+  int stride_h_, stride_w_;
+  int pad_h_, pad_w_;
+  int channels_;
+  int height_, width_;
+  int pooled_height_, pooled_width_;
+  bool global_pooling_;
+  PoolingParameter_RoundMode round_mode_;
+  Blob<Dtype> rand_idx_;
+  Blob<int> max_idx_;
+};
+    
+    #include 'glog/logging.h'
+#include 'google/protobuf/text_format.h'
+#include 'stdint.h'
+    
+     private:
+  // The private constructor to avoid duplicate instantiation.
+  Caffe();
+    
+      /**
+   * @brief Infers the shape of transformed_blob will have when
+   *    the transformation is applied to the data.
+   *
+   * @param datum
+   *    Datum containing the data to be transformed.
+   */
+  vector<int> InferBlobShape(const Datum& datum);
+  /**
+   * @brief Infers the shape of transformed_blob will have when
+   *    the transformation is applied to the data.
+   *    It uses the first element to infer the shape of the blob.
+   *
+   * @param datum_vector
+   *    A vector of Datum containing the data to be transformed.
+   */
+  vector<int> InferBlobShape(const vector<Datum> & datum_vector);
+  /**
+   * @brief Infers the shape of transformed_blob will have when
+   *    the transformation is applied to the data.
+   *    It uses the first element to infer the shape of the blob.
+   *
+   * @param mat_vector
+   *    A vector of Mat containing the data to be transformed.
+   */
+#ifdef USE_OPENCV
+  vector<int> InferBlobShape(const vector<cv::Mat> & mat_vector);
+  /**
+   * @brief Infers the shape of transformed_blob will have when
+   *    the transformation is applied to the data.
+   *
+   * @param cv_img
+   *    cv::Mat containing the data to be transformed.
+   */
+  vector<int> InferBlobShape(const cv::Mat& cv_img);
+#endif  // USE_OPENCV
+    
+    void NcclComm::Sync() { }
     
     
-    {        ImGui::Render();
-    }
+    {            // Let's update our weights no matter what.
+            float secondsOnCommunication = 0.0f;
+            size_t totalSamples = 0;
+            ModelAggregationProcessing(samplesSinceLastSync, learnableNodes, smoothedGradients, totalSamples, secondsOnCommunication);
+        }
     
-    void Speed::startWithTarget(Node* target)
-{
-    if (target && _innerAction)
+                b = builder.InvStdDev(input, L'InvStdOfFeatures');
+            static_pointer_cast<PreComputedNodeBase<ElemType>>(b)->SideLoadFromMatrix(contextStdDev);
+            b->SetLearningRateMultiplier(0);
+    
+    template <typename ElemType, typename ElemType2> static inline const wchar_t* ElemTypeName2();
+template <> /*static*/ inline const wchar_t* ElemTypeName2<float,half>() { return L'float,half'; }
+template <> /*static*/ inline const wchar_t* ElemTypeName2<float,double>() { return L'float,double'; }
+template <> /*static*/ inline const wchar_t* ElemTypeName2<double,half>() { return L'double,half'; }
+template <> /*static*/ inline const wchar_t* ElemTypeName2<double,float>() { return L'double,float'; }
+template <> /*static*/ inline const wchar_t* ElemTypeName2<half,float>() { return L'half,float'; }
+template <> /*static*/ inline const wchar_t* ElemTypeName2<half,double>() { return L'half,double'; }
+    
+    // This is used by NDL and the SimpleNetworkBuilder. It will not be used by BrainScript except for New{Standard}Node().
+    
+    // =======================================================================
+// helper macro to ease access to base members in presence of C++ two-phase name lookup
+// =======================================================================
+    
+            const auto& seq = pMBLayout->GetAllSequences();
+        if (seq.size() != 1)
+            RuntimeError('Only 1 output sequence supported by this API');
+    
+        float DecayCoefficient()
     {
-        Action::startWithTarget(target);
-        _innerAction->startWithTarget(target);
+        float f = 1.f;
+        switch (m_adjustLearningRateAtBeginningType)
+        {
+        case AdjustLearningRateAtBeginning::None:
+            break;
+        case AdjustLearningRateAtBeginning::Linearly:
+            f = min(f, max(0.f, (float)(m_adjustCoefficient + (1 - m_adjustCoefficient) / m_adjustMBNumber * m_parameterSyncCounter)));
+            break;
+        case AdjustLearningRateAtBeginning::Staircase:
+            f = min(f, max(0.f, (float)(m_adjustCoefficient * (m_parameterSyncCounter / m_adjustMBNumber + 1))));
+            break;
+        default:
+            break;
+        }
+        return f;
     }
-    else
-        log('Speed::startWithTarget error: target(%p) or _innerAction(%p) is nullptr!', target, _innerAction);
+    
+                // base on the seqRange, we do the decimation for lattices and related variables
+            if (m_hasLattices)
+            {
+                DecimateLattices(
+                    /*output */
+                    m_netLatticePtr, m_netBoundariesPtr, m_netExtrauttMapPtr, m_netUidPtr,
+                    /*input to be decimated */
+                    m_LatticeCache, m_BoundariesCache, m_extrauttmapCache, m_uidCache,
+                    /* what range we want ? */
+                    seqRange);
+            }
+    
+    
+    {
+    {    private:
+        /**
+         * Description of your variable here.
+         */
+        // bool mSomeVariableHere;
+    };
 }
     
-    /**
- * @addtogroup actions
- * @{
- */
-    }
-    
-    void StopGrid::cacheTargetAsGridNode()
-{
-    _gridNodeTarget = dynamic_cast<NodeGrid*> (_target);
-    CCASSERT(_gridNodeTarget, 'GridActions can only used on NodeGrid');
-}
-    
-    /** @class CallFuncN
-* @brief Calls a 'callback' with the node as the first argument. N means Node.
-* @js NA
-*/
-class CC_DLL CallFuncN : public CallFunc
-{
-public:
-    /** Creates the action with the callback of type std::function<void()>.
-     This is the preferred way to create the callback.
-     *
-     * @param func  A callback function need to be executed.
-     * @return  An autoreleased CallFuncN object.
-     */
-    static CallFuncN * create(const std::function<void(Node*)>& func);
-    }
-    
-    class Texture2D;
-/** @class Animate
- * @brief Animates a sprite given the name of an Animation.
- */
-class CC_DLL Animate : public ActionInterval
-{
-public:
-    /** Creates the action with an Animation and will restore the original frame when the animation is over.
-     *
-     * @param animation A certain animation.
-     * @return An autoreleased Animate object.
-     */
-    static Animate* create(Animation *animation);
-    }
-    
-        /** Resumes the target. All queued actions will be resumed.
-     *
-     * @param target    A certain target.
-     */
-    virtual void resumeTarget(Node *target);
-    
-    /** Pauses all running actions, returning a list of targets whose actions were paused.
-     *
-     * @return  A list of targets whose actions were paused.
-     */
-    virtual Vector<Node*> pauseAllRunningActions();
-    
-    /** Resume a set of targets (convenience function to reverse a pauseAllRunningActions call).
-     *
-     * @param targetsToResume   A set of targets need to be resumed.
-     */
-    virtual void resumeTargets(const Vector<Node*>& targetsToResume);
-    
-    /** Main loop of ActionManager.
-     * @param dt    In seconds.
-     */
-    virtual void update(float dt);
-    
-protected:
-    // declared in ActionManager.m
-    
-    
-    {    return animation;
-}
+            void trackLockThread(Array<float>& poseKeypoints, Array<long long>& poseIds, const cv::Mat& cvMatInput,
+                             const long long frameId);
