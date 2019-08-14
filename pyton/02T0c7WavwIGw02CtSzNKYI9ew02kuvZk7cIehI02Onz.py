e39aa527cb64aab7d29f858c8652936c86cb85ee
SUNsung <weatherius@gmail.com>
2019-08-14 10:14:12 +0300

    Rewrite [02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py] File in: 2019-08-14 10:14:11

--- pyton/02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py
+++ pyton/02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py
@@ -1,88 +1,224 @@
 
         
-            with patch('regenmaschine.login', return_value=mock_coro(True)):
-        result = await flow.async_step_user(user_input=conf)
+            def undo_temp_name(self, filename):
+        if filename.endswith('.part'):
+            return filename[:-len('.part')]
+        return filename
     
-        if CONF_CONDITION in action:
-        return ACTION_CHECK_CONDITION
+            self._check_formats(formats, video_id)
+        self._sort_formats(formats)
     
-        def __init__(self, api_key):
-        '''Initialize the BookSky.'''
-        self._api_key = api_key
-        self.devices = {}
-        _LOGGER.debug('Initial BloomSky device load...')
-        self.refresh_devices()
-    
-    from homeassistant.core import callback
-from . import config_flow
-from .const import DOMAIN
-    
-    
-async def test_if_fires_on_event_with_data(hass, calls):
-    '''Test the firing of events with data.'''
-    assert await async_setup_component(hass, automation.DOMAIN, {
-        automation.DOMAIN: {
-            'trigger': {
-                'platform': 'event',
-                'event_type': 'test_event',
-                'event_data': {'some_attr': 'some_value'}
-            },
-            'action': {
-                'service': 'test.automation',
-            }
+            return {
+            'id': video_id,
+            'title': title,
+            'description': video.get('long_description') or video.get(
+                'short_description'),
+            'duration': float_or_none(video.get('duration'), scale=1000),
+            'formats': formats,
+            'subtitles': subtitles,
         }
-    })
+    
+    import optparse
+import os
+from os.path import dirname as dirn
+import sys
+    
+        out = issue_template_tmpl % {'version': locals()['__version__']}
     
     
-    {
-    {
-    {            }
-        }
-    })
+def get_base_name(base):
+    if base is InfoExtractor:
+        return 'LazyLoadExtractor'
+    elif base is SearchInfoExtractor:
+        return 'LazyLoadSearchExtractor'
+    else:
+        return base.__name__
+    
+    if isinstance(helptext, bytes):
+    helptext = helptext.decode('utf-8')
+    
+        return ret
+    
+        with open(ZSH_COMPLETION_FILE, 'w') as f:
+        f.write(template)
+    
+    # Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
     
     
-async def test_setup_defined_accesspoint(hass):
-    '''Test we initiate config entry for the accesspoint.'''
-    with patch.object(hass, 'config_entries') as mock_config_entries, \
-            patch.object(hmipc, 'configured_haps', return_value=[]):
-        mock_config_entries.flow.async_init.return_value = mock_coro()
-        assert await async_setup_component(hass, hmipc.DOMAIN, {
-            hmipc.DOMAIN: {
-                hmipc.CONF_ACCESSPOINT: 'ABC123',
-                hmipc.CONF_AUTHTOKEN: '123',
-                hmipc.CONF_NAME: 'hmip',
-            }
-        }) is True
+def _mkdir(d):
+    if not os.path.exists(d):
+        os.mkdir(d)
+    
+            return [package.split()[0] for package in packages]
+    except subprocess.CalledProcessError as err:
+        if err.returncode == 1 and err.output == '':
+            return []
+        else:
+            raise err
+    
+        def instant_mode_alias(self, alias_name):
+        if os.environ.get('THEFUCK_INSTANT_MODE', '').lower() == 'true':
+            mark = ('%{' +
+                    USER_COMMAND_MARK + '\b' * len(USER_COMMAND_MARK)
+                    + '%}')
+            return '''
+                export PS1='{user_command_mark}$PS1';
+                {app_alias}
+            '''.format(user_command_mark=mark,
+                       app_alias=self.app_alias(alias_name))
+        else:
+            log_path = os.path.join(
+                gettempdir(), 'thefuck-script-log-{}'.format(uuid4().hex))
+            return '''
+                export THEFUCK_INSTANT_MODE=True;
+                export THEFUCK_OUTPUT_LOG={log};
+                thefuck --shell-logger {log};
+                rm -f {log};
+                exit
+            '''.format(log=log_path)
     
     
-def conv2d(x, kernel_size, out_channels,
-           act_fn=relu,
-           strides=1,
-           padding='SAME',
-           name=None,
-           reuse=None):
-    '''2-D 卷积层
-    Input shape:  [batch_size, in_h, in_w, in_channels]
-    Output shape: [batch_size, out_h, out_w, out_channels]
+def _get_operations(app):
+    proc = subprocess.Popen([app, '--help'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    lines = proc.stdout.readlines()
     
-            return self._call(x)
+    # Define the loss.
+loss = K.variable(0.)
+for layer_name in settings['features']:
+    # Add the L2 norm of the features of a layer to the loss.
+    if layer_name not in layer_dict:
+        raise ValueError('Layer ' + layer_name + ' not found in model.')
+    coeff = settings['features'][layer_name]
+    x = layer_dict[layer_name].output
+    # We avoid border artifacts by only involving non-border pixels in the loss.
+    scaling = K.prod(K.cast(K.shape(x), 'float32'))
+    if K.image_data_format() == 'channels_first':
+        loss += coeff * K.sum(K.square(x[:, :, 2: -2, 2: -2])) / scaling
+    else:
+        loss += coeff * K.sum(K.square(x[:, 2: -2, 2: -2, :])) / scaling
+    
+    print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+    
+                def build(self, input_shape):
+                self.kernel = self.add_weight(shape=(input_shape[-1], self.units),
+                                              initializer='uniform',
+                                              name='kernel')
+                self.recurrent_kernel = self.add_weight(
+                    shape=(self.units, self.units),
+                    initializer='uniform',
+                    name='recurrent_kernel')
+                self.built = True
+    
+        with tempfile.NamedTemporaryFile('wb', delete=True) as f:
+        np.savez(f, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+        monkeypatch.setattr(imdb, 'get_file', lambda *args, **kwargs: f.name)
+        yield f.name
+    
+    from keras import backend as K
+from keras.models import Sequential, Model
+from keras.layers import convolutional_recurrent, Input, Masking, Lambda
+from keras.utils.test_utils import layer_test
+from keras import regularizers
+    
+    
+def get(identifier):
+    if isinstance(identifier, dict):
+        config = {'class_name': str(identifier), 'config': {}}
+        return deserialize(config)
+    elif isinstance(identifier, six.string_types):
+        return deserialize(str(identifier))
+    elif callable(identifier):
+        return identifier
+    else:
+        raise ValueError('Could not interpret '
+                         'metric function identifier:', identifier)
 
     
+            # Arguments
+            name: String, name for the metric.
+        '''
     
-def multi_highway_conv2d(x, kernel_size, n_layer,
-                         act_fn=relu,
-                         strides=1,
-                         padding='SAME',
-                         carry_bias=-1.0,
-                         name=None):
-    '''多层 highway_conv2d'''
-    if isinstance(kernel_size, int):
-        kernel_size = [kernel_size] * n_layer
+            self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.embeddings_initializer = initializers.get(embeddings_initializer)
+        self.embeddings_regularizer = regularizers.get(embeddings_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.embeddings_constraint = constraints.get(embeddings_constraint)
+        self.mask_zero = mask_zero
+        self.supports_masking = mask_zero
+        self.input_length = input_length
     
+        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
+    plt.plot(sample_sizes, one_core, label='one core')
+    plt.plot(sample_sizes, multi_core, label='multi core')
+    plt.xlabel('n_samples')
+    plt.ylabel('Time (s)')
+    plt.title('Parallel %s' % func.__name__)
+    plt.legend()
     
-'''预定义好的正则化器
+                tstart = time()
+            clf.fit(X_train, y_train)
+            asgd_results[i, j, 0] = mean_squared_error(clf.predict(X_test),
+                                                       y_test)
+            asgd_results[i, j, 1] = time() - tstart
+    
+        from sklearn.tree import DecisionTreeClassifier
+    
+    In this examples we will use a movie review dataset.
+    
+    The dataset is generated using the ``make_biclusters`` function, which
+creates a matrix of small values and implants bicluster with large
+values. The rows and columns are then shuffled and passed to the
+Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
+make biclusters contiguous shows how accurately the algorithm found
+the biclusters.
+    
+    X = face.reshape((-1, 1))  # We need an (n_sample, n_feature) array
+k_means = cluster.KMeans(n_clusters=n_clusters, n_init=4)
+k_means.fit(X)
+values = k_means.cluster_centers_.squeeze()
+labels = k_means.labels_
+    
+    import numpy as np
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.datasets.samples_generator import make_blobs
+    
+        # Commands can be executed later on
+    >>> for cmd in command_stack:
+    ...     cmd.execute()
+    renaming foo.txt to bar.txt
+    renaming bar.txt to baz.txt
+    
+    *TL;DR
+Enables selecting an algorithm at runtime.
 '''
-l1_regularizer = L1L2Regularizer(l1=0.01)
     
-    from __future__ import print_function
-from socket import timeout
+        def build_size(self):
+        raise NotImplementedError
+    
+        # After we've accessed `relatives`
+    >>> sorted(Jhon.__dict__.items())
+    [('call_count2', 0), ..., ('relatives', 'Many relatives.')]
+    
+    *What does this example do?
+In this example queue.Queue is used to create the pool (wrapped in a
+custom ObjectPool object to use with the with statement), and it is
+populated with strings.
+As we can see, the first string object put in 'yam' is USED by the
+with statement. But because it is released back into the pool
+afterwards it is reused by the explicit call to sample_queue.get().
+Same thing happens with 'sam', when the ObjectPool created insided the
+function is deleted (by the GC) and the object is returned.
+    
+    '''
+Reference: https://en.wikipedia.org/wiki/Delegation_pattern
+Author: https://github.com/IuryAlves
