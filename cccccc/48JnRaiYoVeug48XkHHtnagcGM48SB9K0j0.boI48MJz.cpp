2c527474d58d6dc386db3c679cd321fb4467c8ad
SUNsung <weatherius@gmail.com>
2019-08-15 02:34:22 +0300

    Rewrite [48JnRaiYoVeug48XkHHtnagcGM48SB9K0j0.boI48MJz.cpp] File in: 2019-08-15 02:34:21

--- cccccc/48JnRaiYoVeug48XkHHtnagcGM48SB9K0j0.boI48MJz.cpp
+++ cccccc/48JnRaiYoVeug48XkHHtnagcGM48SB9K0j0.boI48MJz.cpp
@@ -1,171 +1,49 @@
 
         
-        // This function is a convenient way to create a handle from a raw pointer
-// without having to write out the type of the object explicitly.
-template <typename T>
-mate::Handle<T> CreateHandle(v8::Isolate* isolate, T* object) {
-  return mate::Handle<T>(object->GetWrapper(), object);
-}
-    
-      void reset() { handle_.Reset(); }
-    
-    bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv) {
-  const base::CommandLine::StringType dashdash(2, '-');
-  bool block_args = false;
-  for (int i = 0; i < argc; ++i) {
-    if (argv[i] == dashdash)
-      break;
-    if (block_args) {
-      return false;
-    } else if (IsUrlArg(argv[i])) {
-      block_args = true;
-    }
+          void SetRecvMessage(void* message, bool* got_message) {
+    recv_message_ = message;
+    got_message_ = got_message;
   }
-  return true;
-}
     
-    namespace electron {
-    }
+      Status RequestStream(ServerContext* context,
+                       ServerReader<EchoRequest>* reader,
+                       EchoResponse* response) override;
     
-      uv_loop_t* loop_;
+      // Sets the value of `name` variable.
+  void SetValue(const char* value);
     
+    #include <grpcpp/support/channel_arguments_impl.h>
     
-    {  // If two (usually floating point) numbers are within a certain
-  // fraction of their magnitude or within a certain absolute margin of error.
-  // This is the same as the following but faster:
-  //   WithinFraction(x, y, fraction)  ||  WithinMargin(x, y, margin)
-  // E.g. WithinFraction(0.0, 1e-10, 1e-5) is false but
-  //      WithinFractionOrMargin(0.0, 1e-10, 1e-5, 1e-5) is true.
-  template<typename T>
-  static bool WithinFractionOrMargin(const T x, const T y,
-                                     const T fraction, const T margin);
-};
+      request.set_message('You should be dead');
+  // This may succeed or fail depending on the state of the TCP connection
+  stream->Write(request);
+  // But the read will definitely fail
+  EXPECT_FALSE(stream->Read(&response));
     
-    
-    {
-    {
-    {
-    {
-    {}  // namespace
-}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
-    
-        for (int i = 0; i < dataset.payload_size(); i++) {
-      message->ParseFromString(dataset.payload(i));
-      Proto3DataStripper stripper;
-      stripper.StripMessage(message);
-      dataset.set_payload(i, message->SerializeAsString());
-    }
-    
-    #if 0
-IPC_STRUCT_TRAITS_BEGIN(extensions::DraggableRegion)
-  IPC_STRUCT_TRAITS_MEMBER(draggable)
-  IPC_STRUCT_TRAITS_MEMBER(bounds)
-IPC_STRUCT_TRAITS_END()
-#endif
-    
-      static void ClearCache(content::RenderProcessHost* render_view_host);
-  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
-                             const std::string& proxy_config);
-    
-      scoped_ptr<base::Value> value_args(
-      converter->FromV8Value(args, isolate->GetCurrentContext()));
-  if (!value_args.get() ||
-      !value_args->IsType(base::Value::TYPE_LIST))
-    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-        'Unable to convert 'args' passed to CallObjectMethodSync')));
-    
-    namespace extensions {
-NwAppQuitFunction::NwAppQuitFunction() {
-    }
-    }
-    
-    - **data**: *(batch_size, channel, height, width)*
-- **offset**: *(batch_size, num_deformable_group * kernel[0] * kernel[1] * 2, height, width)*
-- **weight**: *(num_filter, channel, kernel[0], kernel[1])*
-- **bias**: *(num_filter,)*
-- **out**: *(batch_size, num_filter, out_height, out_width)*.
-    
-          // Compute w and h at bottom
-      DType bin_size_h = roi_height / static_cast<DType>(pooled_height);
-      DType bin_size_w = roi_width / static_cast<DType>(pooled_width);
-    
-    #include <mxnet/op_attr_types.h>
-#include <mxnet/graph_attr_types.h>
-#include <mxnet/c_api.h>
-#include <nnvm/symbolic.h>
-#include <nnvm/op.h>
-#include <nnvm/graph.h>
-#include <vector>
-#include <atomic>
-#include <utility>
-#include <string>
-#include <unordered_map>
-    
-      if (param.axes.has_value()) {
-    const mxnet::Tuple<int>& axes = param.axes.value();
-    const int naxes = axes.ndim();
-    }
-    
-        uint64_t notify;
-    swEventData *task_result = &(serv.task_result[SwooleWG.id]);
-    bzero(task_result, sizeof(swEventData));
-    swPipe *task_notify_pipe = &serv.task_notify[SwooleWG.id];
-    swWorker *worker = swServer_get_worker(&serv, SwooleWG.id);
-    
-    #endif
-
-    
-    #endif
-
-    
-    bool zend::include(std::string file)
+    namespace op
 {
-    zend_file_handle file_handle;
-    int ret = php_stream_open_for_zend_ex(file.c_str(), &file_handle, USE_PATH | STREAM_OPEN_FOR_INCLUDE);
-    if (ret != SUCCESS)
+    // This worker will do 3-D rendering
+    class OP_API GuiAdam : public Gui
     {
-        return false;
+    public:
+        GuiAdam(const Point<int>& outputSize, const bool fullScreen,
+                const std::shared_ptr<std::atomic<bool>>& isRunningSharedPtr,
+                const std::shared_ptr<std::pair<std::atomic<bool>, std::atomic<int>>>& videoSeekSharedPtr = nullptr,
+                const std::vector<std::shared_ptr<PoseExtractorNet>>& poseExtractorNets = {},
+                const std::vector<std::shared_ptr<FaceExtractorNet>>& faceExtractorNets = {},
+                const std::vector<std::shared_ptr<HandExtractorNet>>& handExtractorNets = {},
+                const std::vector<std::shared_ptr<Renderer>>& renderers = {},
+                const DisplayMode displayMode = DisplayMode::DisplayAll,
+                const std::shared_ptr<const TotalModel>& totalModel = nullptr,
+                const std::string& adamRenderedVideoPath = '');
     }
     }
     
-    static inline void coro_test_create(coroutine_func_t fn, void *arg, int *complete_num)
-{
-    auto test = new coro_test(fn, arg, complete_num);
-    long cid = swoole::Coroutine::create(coro_test_fn, test);
-    ASSERT_GT(cid, 0);
-}
+            Array<long long> extractIdsLockThread(const Array<float>& poseKeypoints, const cv::Mat& cvMatInput,
+                                              const unsigned long long imageViewIndex,
+                                              const long long frameId);
     
-    int my_onPacket(swServer *serv, swEventData *req)
-{
-    int serv_sock = req->info.server_fd;
-    char *data;
-    swWorker_get_data(serv, req, &data);
-    swDgramPacket *packet = (swDgramPacket *) data;
-    }
-    
-    typedef struct
-{
-    int fd;
-    int key;
-} swFdInfo;
-    
-        SwooleG.use_signalfd = 1;
-    
-    
-    {    ASSERT_GT(cid, 0);
-    ASSERT_EQ(cid, _cid);
-}
-    
-    TEST(os_wait, wait_after_child_exit)
-{
-    coro_test([](void *arg)
-    {
-        swoole_coroutine_signal_init();
-    }
-    }
-    
-        Q_OBJECT
+        public:
+        PersonTracker(const bool mergeResults, const int levels = 3, const int patchSize = 31,
+                      const float confidenceThreshold = 0.05f, const bool trackVelocity = false,
+                      const bool scaleVarying = false, const float rescale = 640);
