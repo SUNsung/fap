4b13eecee75d1f415f55d6320d29698004c2d576
SUNsung <weatherius@gmail.com>
2019-08-14 05:34:48 +0300

    Rewrite [50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb] File in: 2019-08-14 05:34:47

--- ruby/50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb
+++ ruby/50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb
@@ -1,70 +1,148 @@
 
         
-              def monotonic_subscribe(pattern = nil, callback = nil, &block)
-        notifier.subscribe(pattern, callback, monotonic: true, &block)
+            module Models
+      class << self
+        attr_accessor :types
+        attr_accessor :types_cache
       end
     
-        DATA_URL = 'data:'.freeze
-    
-        alias_method :insert_before, :insert
-    
-        def process_response?(response)
-      raise NotImplementedError
-    end
-    
-          before do
-        allow_any_instance_of(Vagrant::Errors::VagrantError).
-          to receive(:translate_error)
-        allow(machine.ui).to receive(:error)
+          module InternalState
+        PROCESSING_EXCEPTION = 'PROCESSING_EXCEPTION'
+        MISSING_EXPORT_COMPLIANCE = 'MISSING_EXPORT_COMPLIANCE'
+        READY_FOR_BETA_TESTING = 'READY_FOR_BETA_TESTING'
+        IN_BETA_TESTING = 'IN_BETA_TESTING'
+        EXPIRED = 'EXPIRED'
+        IN_EXPORT_COMPLIANCE_REVIEW = 'IN_EXPORT_COMPLIANCE_REVIEW'
       end
     
-    
-  describe '#rsync_command' do
-    it 'returns the rsync command' do
-      expect( subject.rsync_command(machine) ).to eq('sudo rsync')
+          def self.find(email: nil, includes: nil)
+        return all(filter: { email: email }, includes: includes)
+      end
     end
   end
+end
+
     
-      subject { described_class.new(machine) }
-    
-        let(:box_output_path) { File.join(scratch, 'package.box') }
-    
-    require Vagrant.source_root.join('plugins/provisioners/chef/cap/omnios/chef_installed')
-    
-      before do
-    allow(machine).to receive(:communicate).and_return(communicator)
+          def self.all(filter: {}, includes: nil, limit: nil, sort: nil)
+        resps = Spaceship::ConnectAPI.get_devices(filter: filter, includes: includes).all_pages
+        return resps.map(&:to_models).flatten
+      end
+    end
   end
+end
+
     
-          # Add the sub-machine configuration to the loader and keys
-      vm_config_key = '#{object_id}_machine_#{name}'
-      @loader.set(vm_config_key, sub_machine.config_procs)
-      keys << vm_config_key
+        it 'includes StrictHostKeyChecking, and UserKnownHostsFile when verify_host_key is :never' do
+      ssh_info[:verify_host_key] = :never
     
-    require_relative '../config/bundler_helper'
-require 'etc'
-    
-          def delete_authorization_session_variables
-        session.delete(:client_id)
-        session.delete(:response_type)
-        session.delete(:redirect_uri)
-        session.delete(:scopes)
-        session.delete(:state)
-        session.delete(:nonce)
-      end
-    
-          def validation_fail_as_json(e)
-        render json: {error: :invalid_client_metadata, error_description: e.message}, status: 400
-      end
-    
-          def fetch_public_key(o_auth_app, jwt)
-        public_key = fetch_public_key_from_json(o_auth_app.jwks, jwt)
-        if public_key.empty? && o_auth_app.jwks_uri
-          response = Faraday.get(o_auth_app.jwks_uri)
-          public_key = fetch_public_key_from_json(response.body, jwt)
+              @machine.communicate.tap do |comm|
+            paths.each do |path|
+              if windows?
+                comm.sudo('mkdir ''#{path}'' -f')
+              else
+                comm.sudo('mkdir -p #{path}')
+                comm.sudo('chown -h #{@machine.ssh_info[:username]} #{path}')
+              end
+            end
+          end
         end
-        raise Rack::OAuth2::Server::Authorize::BadRequest(:unauthorized_client) if public_key.empty?
-        public_key
+    
+        it 'returns false if not installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(false)
+      expect(subject.chef_installed(machine, 'chef_solo', version)).to be_falsey
+    end
+  end
+end
+
+    
+            context 'machine has download options' do
+          before do
+            machine.config.vm.box_download_ca_cert = 'oof'
+            machine.config.vm.box_download_ca_path = 'rab'
+            machine.config.vm.box_download_client_cert = 'zab'
+            machine.config.vm.box_download_insecure = false
+          end
+    
+              find_every_method_call_by_name(body_node, :depends_on).each do |method|
+            key, value = destructure_hash(parameters(method).first)
+            next if key.nil? || value.nil?
+            next unless match = regex_match_group(value, /^(lua|perl|python|ruby)(\d*)/)
+    
+          # Finally, some users make their MacPorts or Fink directories
+      # read-only in order to try out Homebrew, but this doesn't work as
+      # some build scripts error out when trying to read from these now
+      # unreadable paths.
+      %w[/sw /opt/local].map { |p| Pathname.new(p) }.each do |path|
+        paths << path if path.exist? && !path.readable?
       end
     
-    class MessagesController < ApplicationController
-  before_action :authenticate_user!
+          def find_matching_tag(tag)
+        # Used primarily by developers testing beta macOS releases.
+        if OS::Mac.prerelease? && ARGV.skip_or_later_bottles?
+          generic_find_matching_tag(tag)
+        else
+          generic_find_matching_tag(tag) ||
+            find_older_compatible_tag(tag)
+        end
+      end
+    
+        it 'uses older tags when needed', :needs_macos do
+      subject[:mavericks] = 'foo'
+      expect(subject.send(:find_matching_tag, :mavericks)).to eq(:mavericks)
+      expect(subject.send(:find_matching_tag, :yosemite)).to eq(:mavericks)
+    end
+    
+        it 'sets itself to be the resource's owner' do
+      subject.resource('foo') { url 'foo-1.0' }
+      subject.owner = owner
+      subject.resources.each_value do |r|
+        expect(r.owner).to eq(subject)
+      end
+    end
+    
+      it 'properly handles Casks that are not present' do
+    expect {
+      described_class.run('notacask')
+    }.to raise_error(Cask::CaskUnavailableError)
+  end
+end
+
+    
+          # use the same cask file that was used for installation, if possible
+      installed_caskfile = @cask.installed_caskfile
+      installed_cask = installed_caskfile.exist? ? CaskLoader.load(installed_caskfile) : @cask
+    
+            # Displays the state of each pod.
+        #
+        # @return [void]
+        #
+        def print
+          states = %i(added deleted changed unchanged)
+          lines(states).each do |line|
+            UI.message(line, '', 2)
+          end
+        end
+    
+        it 'uses the specified bundle_package_type' do
+      generator = Generator::InfoPlistFile.new('1.0.0', Platform.new(:ios, '6.0'), :bndl)
+      file = temporary_directory + 'Info.plist'
+      generator.save_as(file)
+      Xcodeproj::Plist.read_from_path(file)['CFBundlePackageType'].should == 'BNDL'
+    end
+    
+          # @return [Pathname] The path of the license file as indicated in the
+      #         specification or auto-detected.
+      #
+      def license
+        spec_license || path_list.glob([GLOB_PATTERNS[:license]]).first
+      end
+    
+    #---------------------------------------------------------------------------#
+    
+        it 'should show link to new pod guide after creation' do
+      @sut.any_instance.stubs(:clone_template)
+      @sut.any_instance.stubs(:configure_template)
+      output = run_command('lib', 'create', 'TestPod')
+      output.should.include? 'https://guides.cocoapods.org/making/making-a-cocoapod'
+    end
