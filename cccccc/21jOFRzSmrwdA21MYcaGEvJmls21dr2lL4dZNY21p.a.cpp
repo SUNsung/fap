ad6ac3241dbcfa7e5a032231dd4c5536fada83f1
SUNsung <weatherius@gmail.com>
2019-08-14 11:19:53 +0300

    Rewrite [21jOFRzSmrwdA21MYcaGEvJmls21dr2lL4dZNY21p.a.cpp] File in: 2019-08-14 11:19:53

--- cccccc/21jOFRzSmrwdA21MYcaGEvJmls21dr2lL4dZNY21p.a.cpp
+++ cccccc/21jOFRzSmrwdA21MYcaGEvJmls21dr2lL4dZNY21p.a.cpp
@@ -1,83 +1,502 @@
 
         
-        #if defined(OS_MACOSX) || defined(OS_LINUX)
-  void ShowAboutPanel();
-  void SetAboutPanelOptions(const base::DictionaryValue& options);
+        
+    {    void KeyChord::Vkey(int32_t value)
+    {
+        _vkey = value;
+    }
+}
+
+    
+      // Absolute value of x
+  // Works correctly for unsigned types and
+  // for special floating point values.
+  // Note: 0.0 and -0.0 are not differentiated by Abs (Abs(0.0) is -0.0),
+  // which should be OK: see the comment for Max above.
+  template<typename T>
+  static T Abs(const T x) {
+    return x > T(0) ? x : -x;
+  }
+    
+    TEST(MessageDifferencerTest, OneofTest) {
+  // Create the testing protos
+  unittest::TestOneof2 msg1;
+  unittest::TestOneof2 msg2;
+    }
+    
+    
+    {
+    {
+    {
+    {
+    {}  // namespace
+}  // namespace csharp
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
+    
+      desired_output_for_decode = 'abcdefghIJ';
+  expected = string('\x0A\x0', 2);
+  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
+                                                     desired_output_for_decode);
+  EXPECT_EQ(expected, result);
+    
+    template<typename A>
+struct type_equals_<A, A> : public true_ {
+};
+    
+    			Ref<StyleBox> style = get_stylebox('panel', 'PopupMenu');
+			draw_style_box(style, Rect2(Point2(), get_size()));
+    
+    	Control *_gui_get_focus_owner();
+    
+    		sun.normalize();
+    
+    	static void _convert_detail_mesh_to_native_navigation_mesh(const rcPolyMeshDetail *p_detail_mesh, Ref<NavigationMesh> p_nav_mesh);
+	static void _build_recast_navigation_mesh(Ref<NavigationMesh> p_nav_mesh, EditorProgress *ep,
+			rcHeightfield *hf, rcCompactHeightfield *chf, rcContourSet *cset, rcPolyMesh *poly_mesh,
+			rcPolyMeshDetail *detail_mesh, Vector<float> &vertices, Vector<int> &indices);
+    
+    	static _FORCE_INLINE_ uint32_t hash(const String &p_string) { return p_string.hash(); }
+	static _FORCE_INLINE_ uint32_t hash(const char *p_cstr) { return hash_djb2(p_cstr); }
+	static _FORCE_INLINE_ uint32_t hash(const uint64_t p_int) { return hash_one_uint64(p_int); }
+    
+    MemoryPool::Alloc *MemoryPool::allocs = NULL;
+MemoryPool::Alloc *MemoryPool::free_list = NULL;
+uint32_t MemoryPool::alloc_count = 0;
+uint32_t MemoryPool::allocs_used = 0;
+Mutex *MemoryPool::alloc_mutex = NULL;
+    
+    void GDAPI godot_string_name_new_data(godot_string_name *r_dest, const char *p_name) {
+	StringName *dest = (StringName *)r_dest;
+	memnew_placement(dest, StringName(p_name));
+}
+    
+    #include 'register_types.h'
+    
+    	void set_description_url(const String &url);
+	String get_description_url() const;
+    
+    void grpc_set_default_iomgr_platform() {
+  grpc_set_tcp_client_impl(&grpc_windows_tcp_client_vtable);
+  grpc_set_tcp_server_impl(&grpc_windows_tcp_server_vtable);
+  grpc_set_timer_impl(&grpc_generic_timer_vtable);
+  grpc_set_pollset_vtable(&grpc_windows_pollset_vtable);
+  grpc_set_pollset_set_vtable(&grpc_windows_pollset_set_vtable);
+  grpc_set_resolver_impl(&grpc_windows_resolver_vtable);
+  grpc_set_iomgr_platform_vtable(&vtable);
+}
+    
+    #include <grpc/support/port_platform.h>
+    
+    static void test_parse_bool_value(void) {
+  LOG_TEST_NAME('test_parse_bool_value');
+    }
+    
+    int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
+
+    
+    #ifndef NDEBUG
+#define GRPC_COMBINER_DEBUG_ARGS \
+  , const char *file, int line, const char *reason
+#define GRPC_COMBINER_REF(combiner, reason) \
+  grpc_combiner_ref((combiner), __FILE__, __LINE__, (reason))
+#define GRPC_COMBINER_UNREF(combiner, reason) \
+  grpc_combiner_unref((combiner), __FILE__, __LINE__, (reason))
+#else
+#define GRPC_COMBINER_DEBUG_ARGS
+#define GRPC_COMBINER_REF(combiner, reason) grpc_combiner_ref((combiner))
+#define GRPC_COMBINER_UNREF(combiner, reason) grpc_combiner_unref((combiner))
 #endif
     
-    // Read a message from a socket fd, with an optional timeout.
-// If |timeout| <= 0 then read immediately.
-// Return number of bytes actually read, or -1 on error.
-ssize_t ReadFromSocket(int fd,
-                       char* buf,
-                       size_t bufsize,
-                       const base::TimeDelta& timeout) {
-  if (timeout > base::TimeDelta()) {
-    int rv = WaitSocketForRead(fd, timeout);
-    if (rv <= 0)
-      return rv;
+    TEST_F(HybridEnd2endTest, RawRequestStream) {
+  typedef EchoTestService::WithRawMethod_RequestStream<TestServiceImpl> SType;
+  SType service;
+  SetUpServer(&service, nullptr, nullptr, nullptr);
+  ResetStub();
+  std::thread request_stream_handler_thread(HandleRawClientStreaming<SType>,
+                                            &service, cqs_[0].get());
+  TestAllMethods();
+  request_stream_handler_thread.join();
+}
+    
+    // Testing that this pattern compiles
+TEST_F(RawEnd2EndTest, CompileTest) {
+  typedef grpc::testing::EchoTestService::WithRawMethod_Echo<
+      grpc::testing::EchoTestService::AsyncService>
+      SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+}
+    
+                    auto tempRat = m_chopNumbers[m_numwidth] >> holdVal;
+                tempRat = Integer(tempRat);
+    
+    // maximum depth you can get by precedence. It is just an array's size limit.
+static constexpr size_t MAXPRECDEPTH = 25;
+    
+    {
+    asinrat(pa, radix, precision);
+    ascalerat(pa, angletype, precision);
+}
+    
+        if (equnum((*pa)->pq, b->pq))
+    {
+        // Very special case, q's match.,
+        // make sure signs are involved in the calculation
+        // we have to do this since the optimization here is only
+        // working with the top half of the rationals.
+        (*pa)->pp->sign *= (*pa)->pq->sign;
+        (*pa)->pq->sign = 1;
+        b->pp->sign *= b->pq->sign;
+        b->pq->sign = 1;
+        addnum(&((*pa)->pp), b->pp, BASEX);
+    }
+    else
+    {
+        // Usual case q's aren't the same.
+        DUPNUM(bot, (*pa)->pq);
+        mulnumx(&bot, b->pq);
+        mulnumx(&((*pa)->pp), b->pq);
+        mulnumx(&((*pa)->pq), b->pp);
+        addnum(&((*pa)->pp), (*pa)->pq, BASEX);
+        destroynum((*pa)->pq);
+        (*pa)->pq = bot;
+        trimit(pa, precision);
+    }
+    
+    
+    {    return 0;
+}
+    
+    PSTokenizer::PSTokenizer(int (*getCharFuncA)(void *), void *dataA) {
+  getCharFunc = getCharFuncA;
+  data = dataA;
+  charBuf = -1;
+}
+    
+      // Get scale
+  double getScale() { return scale; }
+    
+      // indirect reference or integer
+  } else if (buf1.isInt()) {
+    num = buf1.getInt();
+    shift();
+    if (buf1.isInt() && buf2.isCmd('R')) {
+      obj->initRef(num, buf1.getInt());
+      shift();
+      shift();
+    } else {
+      obj->initInt(num);
+    }
+    
+      // Destructor.
+  ~Parser();
+    
+    
+    {
+    {
+    {      for (int j = i; j > 0; j--) {
+        keys[j] = keys[j - 1];
+        items[j] = items[j - 1];
+      }
+      
+      keys[0] = keyHit;
+      items[0] = itemHit;
+      return itemHit;
+    }
   }
+  return 0;
+}
+    
+    void PreScanOutputDev::drawMaskedImage(GfxState *state, Object * /*ref*/,
+				       Stream * /*str*/,
+				       int /*width*/, int /*height*/,
+				       GfxImageColorMap *colorMap,
+				       GBool /*interpolate*/,
+				       Stream * /*maskStr*/,
+				       int /*maskWidth*/, int /*maskHeight*/,
+				       GBool /*maskInvert*/, GBool /*maskInterpolate*/) {
+  GfxColorSpace *colorSpace;
+    }
+    
+    void StandardSecurityHandler::freeAuthData(void *authData) {
+  delete (StandardAuthData *)authData;
+}
+    
+     public:
+  Monitor() { self_timer.Start(); }
+    
+    /*
+ * Class:     ml_dmlc_xgboost4j_java_XGBoostJNI
+ * Method:    XGDMatrixNumRow
+ * Signature: (J)J
+ */
+JNIEXPORT jint JNICALL Java_ml_dmlc_xgboost4j_java_XGBoostJNI_XGDMatrixNumRow
+  (JNIEnv *jenv, jclass jcls, jlong jhandle, jlongArray jout) {
+  DMatrixHandle handle = (DMatrixHandle) jhandle;
+  bst_ulong result[1];
+  int ret = (jint) XGDMatrixNumRow(handle, result);
+  jenv->SetLongArrayRegion(jout, 0, 1, (const jlong *) result);
+  return ret;
+}
+    
+    
+    {
+    {#ifndef XGBOOST_USE_CUDA
+  EXPECT_EQ(GPUSet::AllVisible(), GPUSet::Empty());
+#endif
+}
+}  // namespace xgboost
+
+    
+    /*!
+ * \brief Find the maximum iterator within the iterators
+ * \param begin The begining iterator.
+ * \param end The end iterator.
+ * \return the iterator point to the maximum value.
+ * \tparam Iterator The type of the iterator.
+ */
+template<typename Iterator>
+XGBOOST_DEVICE inline Iterator FindMaxIndex(Iterator begin, Iterator end) {
+  Iterator maxit = begin;
+  for (Iterator it = begin; it != end; ++it) {
+    if (*it > *maxit) maxit = it;
+  }
+  return maxit;
+}
+    
+     public:
+  CompressedIterator() : buffer_(nullptr), symbol_bits_(0), offset_(0) {}
+  CompressedIterator(CompressedByteT *buffer, int num_symbols)
+      : buffer_(buffer), offset_(0) {
+    symbol_bits_ = detail::SymbolBits(num_symbols);
+  }
+    
+    inline void RegTree::FVec::Drop(const SparsePage::Inst& inst) {
+  for (bst_uint i = 0; i < inst.size(); ++i) {
+    if (inst[i].index >= data_.size()) continue;
+    data_[inst[i].index].flag = -1;
+  }
+}
+    
+    template <class ElemType>
+shared_ptr<ComputationNode<ElemType>> ComputationNetworkBuilder<ElemType>::DiagTimes(const ComputationNodePtr a, const ComputationNodePtr b, const std::wstring nodeName)
+{
+    return net.AddNodeToNetAndAttachInputs(New<DiagTimesNode<ElemType>>(net.GetDeviceId(), nodeName), { a, b });
+}
+    
+        {
+        if (pass > ndlPassInitial && evaluateNode)
+        {
+            std::string name = nodeParam->GetName();
+            std::wstring wname = Microsoft::MSR::CNTK::ToFixedWStringFromMultiByte(name);
+            if (nodeParam->GetType() == ndlTypeDotParameter)
+            {
+                // When we see a variable of the form 'A.B' in a macro, we need to resolve it to an actual node, by first constructing it's
+                // fully-qualified name. There are 2 possibilities:
+                // 1) 'A' was defined locally within the macro.  In this case, we must find the fully-qualified name of the node that this macro
+                //    call is being assigned to (eg, 'C' in the example 'C=Macro(X)'), and concatenate it's name with 'A.B' (eg, 'C.A.B').
+                // 2) 'A' was passed in as a parameter to a macro.  In this case, we must find the fully-qualified name of the node that
+                //    was passed in as 'A', and replace the 'A' and 'A.B' with this name.
+    }
+    }
+    }
+    
+    #include 'ConvolutionalNodes.h'
+#include 'RNNNodes.h'
+#include 'DeprecatedNodes.h'
+#include 'EvaluationNodes.h'
+#include 'InputAndParamNodes.h'
+#include 'LinearAlgebraNodes.h'
+#include 'NonlinearityNodes.h'
+#include 'PreComputeNodes.h'
+#include 'ReshapingNodes.h'
+#include 'RecurrentNodes.h'
+#include 'SpecialPurposeNodes.h'
+#include 'TrainingNodes.h'
+    
+            // loop over tokens
+        do
+        {
+            // consume separators (newline, comma, semicolon)
+            auto tokenStart = stringParse.find_first_not_of(seps, tokenEnd);
+            if (tokenStart == npos)
+            {
+                // no more tokens
+                break;
+            }
+    }
+    
+        // add a node to m_nameToNodeMap[], which is our node holder
+    // This only adds the node to the network's node set, without considering linkage.
+    // Duplicate node names are rejected.
+    ComputationNodeBasePtr AddNodeToNet(const ComputationNodeBasePtr& node)
+    {
+        auto result = m_nameToNodeMap.insert(make_pair(node->NodeName(), node));
+        if (!result.second)
+            RuntimeError('AddNodeToNet: Duplicated name for %ls %ls operation.', node->NodeName().c_str(), node->OperationName().c_str());
+        node->SetEnvironment(m_environment);
+        return node; // allows e.g. return AddNodeToNet(New...);
+    }
+    // TODO: not very nice--need to fix way more outside to get this right
+    template <class N>
+    shared_ptr<N> AddNodeToNetWithElemType(const shared_ptr<N> node)
+    {
+        return dynamic_pointer_cast<N>(AddNodeToNet(node));
+    }
+    
+            // get some additional information when doing sequence training
+        // TODO: This should not need to be called in case of wasDataRead == false, since in that case, returned values are invalid.
+        if ((criterionNode != nullptr) && (criterionNode->OperationName() == L'SequenceWithSoftmax'))
+        {
+            auto node = dynamic_pointer_cast<SequenceWithSoftmaxNode<ElemType>>(criterionNode);
+            auto latticeinput = node->getLatticePtr();
+            auto uids = node->getuidprt();
+            auto boundaries = node->getboundaryprt();
+            auto extrauttmap = node->getextrauttmap();
+    }
+    
+             virtual bool OnArrivingAtSyncPoint(
+            const std::list<ComputationNodeBasePtr>& LearnableNodes,        /* input/output: */
+            std::list<MatrixBasePtr>& smoothedGradients,                     /* input/output: under some setup, it will reset to zero*/
+            size_t  samplesSinceLastSync                                    /* input:  samples processed since last sync on this worker only */
+             )
+         {
+             Timer syncPointTimer; 
+             syncPointTimer.Start();
+             bool read2Sync=UpdateWorkerStatus(MAWorkerStatus::DataProcessing);
+             syncPointTimer.Stop();
+             m_perfReporter.OnArriveAtSyncPoint(syncPointTimer.ElapsedSeconds(),read2Sync);
     }
     
     
-namespace nwapi {
+template <class ElemType>
+std::shared_ptr<IDistGradAggregator<ElemType>> GetSimpleDistGradAggregator(
+    const MPIWrapperPtr& mpi,
+    bool useAsyncAggregation,
+    int deviceId,
+    int syncStatsTrace,
+    size_t packThresholdSizeInBytes,
+    bool useFP16AllReduce)
+{
+    if (Globals::UseV2Aggregator())
+        return std::make_shared<V2SimpleDistGradAggregator<ElemType>>(
+            mpi,
+            useAsyncAggregation,
+            deviceId,
+            syncStatsTrace,
+            ::CNTK::MPICommunicator(packThresholdSizeInBytes, useFP16AllReduce));
+    else
+        return std::make_shared<SimpleDistGradAggregator<ElemType>>(
+            mpi,
+            useAsyncAggregation,
+            deviceId,
+            syncStatsTrace,
+            packThresholdSizeInBytes);
+}
+    
+    #include 'common.h'
+    
+    namespace aria2 {
     }
     
-    #include 'content/nw/src/api/menu/menu.h'
+    std::string DHTResponseMessage::toString() const
+{
+  return fmt('dht response %s TransactionID=%s Remote:%s(%u), id=%s, v=%s, %s',
+             getMessageType().c_str(), util::toHex(getTransactionID()).c_str(),
+             getRemoteNode()->getIPAddress().c_str(),
+             getRemoteNode()->getPort(),
+             util::toHex(getRemoteNode()->getID(), DHT_ID_LENGTH).c_str(),
+             util::torrentPercentEncode(getVersion()).c_str(),
+             toStringOptional().c_str());
+}
     
-    static KeyMap keymap = {
-  {'`'    , 'Backquote'},
-  {'\\'   , 'Backslash'},
-  {'['    , 'BracketLeft'},
-  {']'    , 'BracketRight'},
-  {','    , 'Comma'},
-  {'='    , 'Equal'},
-  {'-'    , 'Minus'},
-  {'.'    , 'Period'},
-  {'''    , 'Quote'},
-  {';'    , 'Semicolon'},
-  {'/'    , 'Slash'},
-  {'\n'   , 'Enter'},
-  {'\t'   , 'Tab'},
-  {'UP'   , 'ArrowUp'},
-  {'DOWN' , 'ArrowDown'},
-  {'LEFT' , 'ArrowLeft'},
-  {'RIGHT', 'ArrowRight'},
-  {'ESC'  , 'Escape'},
-  {'MEDIANEXTTRACK', 'MediaTrackNext'},
-  {'MEDIAPREVTRACK', 'MediaTrackPrevious'}
-};
+      bool addGoodNode(const std::shared_ptr<DHTNode>& node);
+    
+    #include <vector>
+#include <string>
+#include <memory>
     
     
-    {#endif
-}  // namespace nwapi
+    {} // namespace aria2
     
-    #include 'chrome/browser/devtools/devtools_window.h'
-#include 'chrome/browser/extensions/devtools_util.h'
-#include 'chrome/browser/extensions/extension_service.h'
-#include 'content/nw/src/api/menuitem/menuitem.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/public/browser/render_view_host.h'
-#include 'content/public/browser/web_contents.h'
-#include 'extensions/browser/extension_system.h'
-#include 'extensions/common/error_utils.h'
+    const std::string& DHTUnknownMessage::getMessageType() const { return UNKNOWN; }
     
     
-    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.allocateId', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwObjAllocateIdFunction);
-};
+    {    //1
+    ret = p.read(&p, buf, sizeof(buf));
+    if (ret < 0)
+    {
+        swSysWarn('read() failed.');
+    }
+    ASSERT_GT(ret, 0);
+    ASSERT_EQ(strcmp('hello world1', buf), 0);
+    //2
+    ret = p.read(&p, buf, sizeof(buf));
+    ASSERT_GT(ret, 0);
+    ASSERT_EQ(strcmp('hello world2', buf), 0);
+    //3
+    ret = p.read(&p, buf, sizeof(buf));
+    ASSERT_GT(ret, 0);
+    ASSERT_EQ(strcmp('hello world3', buf), 0);
+}
     
-      private:
-    int GetPrimaryMonitorIndex();
-    // DesktopMediaListObserver implementation.
-    void OnSourceAdded(DesktopMediaList* list, int index) override;
-    void OnSourceRemoved(DesktopMediaList* list, int index) override;
-    void OnSourceMoved(DesktopMediaList* list, int old_index, int new_index) override;
-    void OnSourceNameChanged(DesktopMediaList* list, int index) override;
-    void OnSourceThumbnailChanged(DesktopMediaList* list, int index) override;
     
-    	if (cfgRoot.length() != 0)
-	{
-		swprintf_s(args, L'%s  -loadcfgfile \'%s\'', args, userConEmuCfgPath);
-	}
+    {    destroy_op_array(new_op_array);
+    efree(new_op_array);
+    return Z_TYPE(retval) == IS_TRUE;
+}
+    
+        serv.reactor_num = 4;
+    serv.worker_num = 2;
+    
+            memcpy(&tmp, (char*) recv_pkg.ptr + recv_pkg.size - 4, sizeof(tmp));
+        ASSERT_EQ(tmp, recv_pkg.serial_num);
+    
+        sigemptyset(&curset);
+    sigprocmask(SIG_BLOCK, NULL, &curset);
+    ret = sigismember(&curset,SIGUSR1);
+    ASSERT_EQ(ret, 1);
+    
+        public:
+        ExampleQt(const char * value, QObject * parent = 0)
+            : QObject(parent), m_value(value) {}
+    
+    
+    
+    
+    
+        const auto FACE_NUMBER_PARTS = 70u;
+    #define FACE_PAIRS_RENDER_GPU \
+        0,1,  1,2,  2,3,  3,4,  4,5,  5,6,  6,7,  7,8,  8,9,  9,10,  10,11,  11,12,  12,13,  13,14,  14,15,  15,16,  17,18,  18,19,  19,20, \
+        20,21,  22,23,  23,24,  24,25,  25,26,  27,28,  28,29,  29,30,  31,32,  32,33,  33,34,  34,35,  36,37,  37,38,  38,39,  39,40,  40,41, \
+        41,36,  42,43,  43,44,  44,45,  45,46,  46,47,  47,42,  48,49,  49,50,  50,51,  51,52,  52,53,  53,54,  54,55,  55,56,  56,57,  57,58, \
+        58,59,  59,48,  60,61,  61,62,  62,63,  63,64,  64,65,  65,66,  66,67,  67,60
+    #define FACE_SCALES_RENDER_GPU 1
+    const std::vector<unsigned int> FACE_PAIRS_RENDER {FACE_PAIRS_RENDER_GPU};
+    #define FACE_COLORS_RENDER_GPU 255.f,    255.f,    255.f
+    const std::vector<float> FACE_COLORS_RENDER{FACE_COLORS_RENDER_GPU};
+    const std::vector<float> FACE_SCALES_RENDER{FACE_SCALES_RENDER_GPU};
+    
+        template<typename T>
+    Point<T>& Point<T>::operator/=(const T value)
+    {
+        try
+        {
+            x /= value;
+            y /= value;
+            // Return
+            return *this;
+        }
+        catch (const std::exception& e)
+        {
+            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
+            return *this;
+        }
+    }
