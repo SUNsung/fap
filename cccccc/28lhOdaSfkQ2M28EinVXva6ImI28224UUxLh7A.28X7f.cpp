a7b9c1e759aed2ac09036bdfa2e294a4829a558a
SUNsung <weatherius@gmail.com>
2019-08-14 19:44:17 +0300

    Rewrite [28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp] File in: 2019-08-14 19:44:17

--- cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
+++ cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
@@ -1,171 +1,239 @@
 
         
-                std::wstring ToString(uint32_t radix, NUMOBJ_FMT format, int32_t precision) const;
-        uint64_t ToUInt64_t() const;
+        #include 'KeyChord.g.cpp'
     
-            // Displayed number can go through transformation. So copy it after transformation
-        gldPrevious.value = m_currentVal;
+            // Write received text to the Console
+        // Note: Write to the Console using WriteFile(hConsole...), not printf()/puts() to
+        // prevent partially-read VT sequences from corrupting output
+        WriteFile(hConsole, szBuffer, dwBytesRead, &dwBytesWritten, NULL);
     
-        // initialize the max dec number you can support for each of the supported bit lengths
-    // this is basically max num in that width / 2 in integer
-    assert(m_chopNumbers.size() == m_maxDecimalValueStrings.size());
-    for (size_t i = 0; i < m_chopNumbers.size(); i++)
-    {
-        auto maxVal = m_chopNumbers[i] / 2;
-        maxVal = RationalMath::Integer(maxVal);
+        QString getDataDirectory();
+    void setDataDirectory(const QString &dataDir);
+    
+    
+    
+    #endif // BITCOIN_CRYPTO_RIPEMD160_H
+
+    
+    
+    {}  // namespace config
+    
+    #include <stdio.h>
+#include 'db/dbformat.h'
+#include 'db/filename.h'
+#include 'db/log_reader.h'
+#include 'db/version_edit.h'
+#include 'db/write_batch_internal.h'
+#include 'leveldb/env.h'
+#include 'leveldb/iterator.h'
+#include 'leveldb/options.h'
+#include 'leveldb/status.h'
+#include 'leveldb/table.h'
+#include 'leveldb/write_batch.h'
+#include 'util/logging.h'
+    
+        void operator()(const changefeed_stamp_t &s) {
+        response->response = do_stamp(s, s.region, s.region.inner.left);
     }
     
-    bool LiveRegionHost::IsHostAvailable()
-{
-    // LiveRegion is always available.
-    return true;
-}
-    
-    NarratorAnnouncement ^ CalculatorAnnouncement::GetUpdateCurrencyRatesAnnouncement(String ^ announcement)
-{
-    return ref new NarratorAnnouncement(
-        announcement,
-        CalculatorActivityIds::UpdateCurrencyRates,
-        AutomationNotificationKind::ActionCompleted,
-        AutomationNotificationProcessing::ImportantMostRecent);
-}
-    
-    
-    {  {
-    auto lparam = xgboost::CreateEmptyGenericParam(1, -1);
-    xgboost::Metric * metric = xgboost::Metric::Create('rmse', &lparam);
-    ASSERT_STREQ(metric->Name(), 'rmse');
-    EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
-    EXPECT_NEAR(GetMetricEval(metric,
-                              {0.1f, 0.9f, 0.1f, 0.9f},
-                              {  0,   0,   1,   1}),
-                0.6403f, 0.001f);
-    delete metric;
-  }
-}
-#endif
-
-    
-    #else  // Not R or R with GPU.
-    
-    // This is a helpful data structure to define parameters
-// You do not have to use it.
-// see http://dmlc-core.readthedocs.org/en/latest/parameter.html
-// for introduction of this module.
-struct MyLogisticParam : public dmlc::Parameter<MyLogisticParam> {
-  float scale_neg_weight;
-  // declare parameters
-  DMLC_DECLARE_PARAMETER(MyLogisticParam) {
-    DMLC_DECLARE_FIELD(scale_neg_weight).set_default(1.0f).set_lower_bound(0.0f)
-        .describe('Scale the weight of negative examples by this factor');
-  }
-};
-    
-    
-    {const char kQueryField[] = 'query';
-const char kLimitField[] = 'limit';
-const char kSkipField[] = 'skip';
-const char kHintField[] = 'hint';
-const char kCollationField[] = 'collation';
-const char kExplainField[] = 'explain';
-const char kCommentField[] = 'comment';
-const char kMaxTimeMSField[] = 'maxTimeMS';
-const char kReadConcernField[] = 'readConcern';
-}  // namespace
-    
-    #include 'mongo/db/storage/kv/temporary_kv_record_store.h'
-    
-    RemoveTransliterator::RemoveTransliterator() : Transliterator(UnicodeString(TRUE, ::CURR_ID, -1), 0) {}
-    
-    SearchIterator::SearchIterator(const SearchIterator &other)
-    : UObject(other)
-{   
-    m_breakiterator_            = other.m_breakiterator_;
-    m_text_                     = other.m_text_;
-    m_search_                   = (USearch *)uprv_malloc(sizeof(USearch));   
-    m_search_->breakIter        = other.m_search_->breakIter;
-    m_search_->isCanonicalMatch = other.m_search_->isCanonicalMatch;
-    m_search_->isOverlap        = other.m_search_->isOverlap;
-    m_search_->elementComparisonType = other.m_search_->elementComparisonType;
-    m_search_->matchedIndex     = other.m_search_->matchedIndex;
-    m_search_->matchedLength    = other.m_search_->matchedLength;
-    m_search_->text             = other.m_search_->text;
-    m_search_->textLength       = other.m_search_->textLength;
-}
-    
-    #include 'unicode/messagepattern.h'
-#include 'unicode/rbnf.h'
-#include 'unicode/selfmt.h'
-#include 'unicode/uchar.h'
-#include 'unicode/ucnv_err.h'
-#include 'unicode/umsg.h'
-#include 'unicode/ustring.h'
-#include 'unicode/utypes.h'
-#include 'cmemory.h'
-#include 'messageimpl.h'
-#include 'patternprops.h'
-#include 'selfmtimpl.h'
-#include 'uassert.h'
-#include 'ustrfmt.h'
-#include 'util.h'
-#include 'uvector.h'
-    
-    
-#ifndef SELFMTIMPL
-#define SELFMTIMPL
-    
-    #include 'unicode/utypes.h'
-#include 'sharedobject.h'
-    
-    
-class U_I18N_API SharedDateFormatSymbols : public SharedObject {
-public:
-    SharedDateFormatSymbols(
-            const Locale &loc, const char *type, UErrorCode &status)
-            : dfs(loc, type, status) { }
-    virtual ~SharedDateFormatSymbols();
-    const DateFormatSymbols &get() const { return dfs; }
-private:
-    DateFormatSymbols dfs;
-    SharedDateFormatSymbols(const SharedDateFormatSymbols &);
-    SharedDateFormatSymbols &operator=(const SharedDateFormatSymbols &);
-};
-    
-    #ifndef incl_HPHP_SYNCHRONIZABLE_H_
-#define incl_HPHP_SYNCHRONIZABLE_H_
-    
-    
-std::mutex watchdog_mut;
-static int watchdog_count = 0;
+        void protocol_write(const write_t &write,
+                        write_response_t *response,
+                        state_timestamp_t timestamp,
+                        scoped_ptr_t<real_superblock_t> *superblock,
+                        signal_t *interruptor);
     
     
     {
-    {}  // namespace planning
-}  // namespace apollo
-
+    {                rdb_live_deletion_context_t deletion_context;
+                rdb_set(key, data, true, store.get_sindex_slice(sindex_uuid),
+                    repli_timestamp_t::distant_past,
+                    sindex_super_block.get(), &deletion_context, &response,
+                    &mod_info, static_cast<profile::trace_t *>(nullptr));
+            }
+            txn->commit();
+        }
     
-        params.push_back(static_cast<float>(node['cam2world'].size()));
+    #if GTEST_OS_SYMBIAN
+  // Streams a value (either a pointer or not) to this object.
+  template <typename T>
+  inline Message& operator <<(const T& value) {
+    StreamHelper(typename internal::is_pointer<T>::type(), value);
+    return *this;
+  }
+#else
+  // Streams a non-pointer value to this object.
+  template <typename T>
+  inline Message& operator <<(const T& val) {
+    // Some libraries overload << for STL containers.  These
+    // overloads are defined in the global namespace instead of ::std.
+    //
+    // C++'s symbol lookup rule (i.e. Koenig lookup) says that these
+    // overloads are visible in either the std namespace or the global
+    // namespace, but not other namespaces, including the testing
+    // namespace which Google Test's Message class is in.
+    //
+    // To allow STL containers (and other types that has a << operator
+    // defined in the global namespace) to be used in Google Test
+    // assertions, testing::Message must access the custom << operator
+    // from the global namespace.  With this using declaration,
+    // overloads of << defined in the global namespace and those
+    // visible via Koenig lookup are both exposed in this function.
+    using ::operator <<;
+    *ss_ << val;
+    return *this;
+  }
     
-    class TrafficLightUnprotectedRightTurnStageStop : public Stage {
+    // With this overloaded version, we allow anonymous enums to be used
+// in {ASSERT|EXPECT}_EQ when compiled with gcc 4, as anonymous enums
+// can be implicitly cast to BiggestInt.
+GTEST_API_ AssertionResult CmpHelperEQ(const char* expected_expression,
+                                       const char* actual_expression,
+                                       BiggestInt expected,
+                                       BiggestInt actual);
+    
+    // In describing the results of death tests, these terms are used with
+// the corresponding definitions:
+//
+// exit status:  The integer exit information in the format specified
+//               by wait(2)
+// exit code:    The integer code passed to exit(3), _exit(2), or
+//               returned from main()
+class GTEST_API_ DeathTest {
  public:
-  explicit TrafficLightUnprotectedRightTurnStageStop(
-      const ScenarioConfig::StageConfig& config)
-      : Stage(config) {}
+  // Create returns false if there was an error determining the
+  // appropriate action to take for the current death test; for example,
+  // if the gtest_death_test_style flag is set to an invalid value.
+  // The LastMessage method will return a more detailed message in that
+  // case.  Otherwise, the DeathTest pointer pointed to by the 'test'
+  // argument is set.  If the death test should be skipped, the pointer
+  // is set to NULL; otherwise, it is set to the address of a new concrete
+  // DeathTest object that controls the execution of the current test.
+  static bool Create(const char* statement, const RE* regex,
+                     const char* file, int line, DeathTest** test);
+  DeathTest();
+  virtual ~DeathTest() { }
     }
     
-    Status ControllerAgent::Init(const ControlConf *control_conf) {
-  RegisterControllers(control_conf);
-  CHECK(InitializeConf(control_conf).ok()) << 'Failed to initialize config.';
-  for (auto &controller : controller_list_) {
-    if (controller == nullptr) {
-      return Status(ErrorCode::CONTROL_INIT_ERROR, 'Controller is null.');
-    }
-    if (!controller->Init(control_conf_).ok()) {
-      AERROR << 'Controller <' << controller->Name() << '> init failed!';
-      return Status(ErrorCode::CONTROL_INIT_ERROR,
-                    'Failed to init Controller:' + controller->Name());
-    }
-    AINFO << 'Controller <' << controller->Name() << '> init done!';
-  }
-  return Status::OK();
+    // A function to convert T* into linked_ptr<T>
+// Doing e.g. make_linked_ptr(new FooBarBaz<type>(arg)) is a shorter notation
+// for linked_ptr<FooBarBaz<type> >(new FooBarBaz<type>(arg))
+template <typename T>
+linked_ptr<T> make_linked_ptr(T* ptr) {
+  return linked_ptr<T>(ptr);
 }
+    
+      template <typename T>
+  operator ParamGenerator<T>() const {
+    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
+        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
+        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
+        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
+        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
+        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
+        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
+        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_),
+        static_cast<T>(v24_), static_cast<T>(v25_), static_cast<T>(v26_),
+        static_cast<T>(v27_), static_cast<T>(v28_), static_cast<T>(v29_),
+        static_cast<T>(v30_), static_cast<T>(v31_), static_cast<T>(v32_),
+        static_cast<T>(v33_), static_cast<T>(v34_), static_cast<T>(v35_),
+        static_cast<T>(v36_), static_cast<T>(v37_), static_cast<T>(v38_),
+        static_cast<T>(v39_), static_cast<T>(v40_), static_cast<T>(v41_),
+        static_cast<T>(v42_)};
+    return ValuesIn(array);
+  }
+    
+      // Creates a UTF-16 wide string from the given ANSI string, allocating
+  // memory using new. The caller is responsible for deleting the return
+  // value using delete[]. Returns the wide string, or NULL if the
+  // input is NULL.
+  //
+  // The wide string is created using the ANSI codepage (CP_ACP) to
+  // match the behaviour of the ANSI versions of Win32 calls and the
+  // C runtime.
+  static LPCWSTR AnsiToUtf16(const char* c_str);
+    
+        ImGui_ImplDX9_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+    
+        // Upload texture to graphics system
+    g_FontTexture = NULL;
+    if (g_pd3dDevice->CreateTexture(width, height, 1, D3DUSAGE_DYNAMIC, D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &g_FontTexture, NULL) < 0)
+        return false;
+    D3DLOCKED_RECT tex_locked_rect;
+    if (g_FontTexture->LockRect(0, &tex_locked_rect, NULL, 0) != D3D_OK)
+        return false;
+    for (int y = 0; y < height; y++)
+        memcpy((unsigned char *)tex_locked_rect.pBits + tex_locked_rect.Pitch * y, pixels + (width * bytes_per_pixel) * y, (width * bytes_per_pixel));
+    g_FontTexture->UnlockRect(0);
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    IMGUI_IMPL_API bool     ImGui_Marmalade_Init(bool install_callbacks);
+IMGUI_IMPL_API void     ImGui_Marmalade_Shutdown();
+IMGUI_IMPL_API void     ImGui_Marmalade_NewFrame();
+IMGUI_IMPL_API void     ImGui_Marmalade_RenderDrawData(ImDrawData* draw_data);
+    
+        // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    #include <osquery/flags.h>
+#include <osquery/process/process.h>
+    
+    #ifdef WIN32
+/**
+ * @brief Handles the resource lifetime of a PSECURITY_DESCRIPTOR
+ *
+ * Class to handle the scope of a PSECURITY_DESCRIPTOR from
+ * GetSecurityInfo/GetNamedSecurityInfo class of functions (or any
+ * PSECURITY_DESCRIPTOR pointer where the buffer is allocated via LocalAlloc)
+ */
+class SecurityDescriptor {
+ public:
+  explicit SecurityDescriptor(PSECURITY_DESCRIPTOR sd) : sd_(sd) {}
+    }
+    
+    // clang-format off
+#include <LM.h>
+// clang-format on
+    
+    #include <osquery/logger.h>
+    
+      /*
+   * @brief Helper function to POST a carve to the graph endpoint.
+   *
+   * Once all of the files have been carved and the tgz has been
+   * created, we POST the carved file to an endpoint specified by the
+   * carver_start_endpoint and carver_continue_endpoint
+   */
+  Status postCarve(const boost::filesystem::path& path);
+    
+      /*
+   * If the initial configuration includes a non-0 refresh, start an
+   * additional service that sleeps and periodically regenerates the
+   * configuration.
+   */
+  if (!FLAGS_config_check && !started_thread_ && getRefresh() > 0) {
+    Dispatcher::addService(refresh_runner_);
+    started_thread_ = true;
+  }
+    
+    TEST_F(ConfigTests, test_config_backup_integrate) {
+  const auto config_enable_backup_saved = FLAGS_config_enable_backup;
+  FLAGS_config_enable_backup = true;
+    }
+    
+    
+    {} // namespace osquery
+
+    
+    #include <osquery/core/database/database.h>
