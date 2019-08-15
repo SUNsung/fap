e6c82eb813fcb95c06a7788065c89f0210ee67b3
SUNsung <weatherius@gmail.com>
2019-08-15 16:29:14 +0300

    Rewrite [356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb] File in: 2019-08-15 16:29:13

--- ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
+++ ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
@@ -1,96 +1,134 @@
 
         
-        module Gitlab
-  module GithubImport
-    module Importer
-      class NotesImporter
-        include ParallelScheduling
+            assert_kind_of Integer, m1.my_house_population
+    assert_equal 3, m1.my_house_population
     
-    module Devise
-  module Controllers
-    # Create url helpers to be used with resource/scope configuration. Acts as
-    # proxies to the generated routes created by devise.
-    # Resource param can be a string or symbol, a class, or an instance object.
-    # Example using a :user resource:
-    #
-    #   new_session_path(:user)      => new_user_session_path
-    #   session_path(:user)          => user_session_path
-    #   destroy_session_path(:user)  => destroy_user_session_path
-    #
-    #   new_password_path(:user)     => new_user_password_path
-    #   password_path(:user)         => user_password_path
-    #   edit_password_path(:user)    => edit_user_password_path
-    #
-    #   new_confirmation_path(:user) => new_user_confirmation_path
-    #   confirmation_path(:user)     => user_confirmation_path
-    #
-    # Those helpers are included by default to ActionController::Base.
-    #
-    # In case you want to add such helpers to another class, you can do
-    # that as long as this new class includes both url_helpers and
-    # mounted_helpers. Example:
-    #
-    #     include Rails.application.routes.url_helpers
-    #     include Rails.application.routes.mounted_helpers
-    #
-    module UrlHelpers
-      def self.remove_helpers!
-        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
-          remove_method method
+        class InvertibleByPartsMigration < SilentMigration
+      attr_writer :test
+      def change
+        create_table('new_horses') do |t|
+          t.column :breed, :string
+        end
+        reversible do |dir|
+          @test.yield :both
+          dir.up    { @test.yield :up }
+          dir.down  { @test.yield :down }
+        end
+        revert do
+          create_table('horses') do |t|
+            t.column :content, :text
+            t.column :remind_at, :datetime
+          end
         end
       end
-    
-        # Creates configuration values for Devise and for the given module.
-    #
-    #   Devise::Models.config(Devise::Models::DatabaseAuthenticatable, :stretches)
-    #
-    # The line above creates:
-    #
-    #   1) An accessor called Devise.stretches, which value is used by default;
-    #
-    #   2) Some class methods for your model Model.stretches and Model.stretches=
-    #      which have higher priority than Devise.stretches;
-    #
-    #   3) And an instance method stretches.
-    #
-    # To add the class methods you need to have a module ClassMethods defined
-    # inside the given class.
-    #
-    def self.config(mod, *accessors) #:nodoc:
-      class << mod; attr_accessor :available_configs; end
-      mod.available_configs = accessors
-    
-              recoverable.reset_password_token = original_token if recoverable.reset_password_token.present?
-          recoverable
-        end
-    
-      puts '== Installing dependencies =='
-  system! 'gem install bundler --conservative'
-  system('bundle check') || system!('bundle install')
-    
-      # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-    
-      attr_accessor :directories
-    
-      dont_obsolete_paths = []
-  option '--dont-obsolete', 'DONT_OBSOLETE_PATH',
-    'A file path for which to 'dont-obsolete' in the built PackageInfo. ' \
-    'Can be specified multiple times.' do |path|
-      dont_obsolete_paths << path
     end
     
-    class FPM::Package::Pacman < FPM::Package
+        config.action_mailbox.queues = ActiveSupport::InheritableOptions.new \
+      incineration: :action_mailbox_incineration, routing: :action_mailbox_routing
     
+      # Mount Action Cable outside main process or domain
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
     
+      def perform_failure(project_id, user_id, error)
+    project = Project.find(project_id)
+    user = User.find(user_id)
     
-    require 'pleaserun/cli'
+            filters = @filters.each_with_object([]) do |filter, parsed_filters|
+          match = @raw_query.split.find { |part| part =~ /\A#{filter[:name]}:/ }
+          next unless match
     
-          def self.has_issues_with_add_symlink?
-        return !::Gem::Package::TarWriter.public_instance_methods.include?(:add_symlink)
+      def output_chronic_duration_attribute(source_attribute)
+    value = attributes[source_attribute.to_s]
+    ChronicDuration.output(value, format: :short) if value
+  end
+end
+
+    
+            def self.nfs_checkexports!
+          r = Vagrant::Util::Subprocess.execute('nfsd', 'checkexports')
+          if r.exit_code != 0
+            raise Vagrant::Errors::NFSBadExports, output: r.stderr
+          end
+        end
       end
-    end # module TarWriter
-  end # module Issues
-end # module FPM
+    end
+  end
+end
+
+    
+            def windows?
+          @machine.config.vm.communicator == :winrm
+        end
+      end
+    end
+  end
+end
+
+    
+    require File.expand_path('../../../../../base', __FILE__)
+    
+            # @return [Set<String>] the names of the pods that were changed.
+        #
+        attr_reader :changed
+    
+          # @return [Bool] whether this resolved specification is by non-library targets.
+      #
+      attr_reader :used_by_non_library_targets_only
+      alias used_by_non_library_targets_only? used_by_non_library_targets_only
+    
+            # Initialize a new instance
+        #
+        # @param [Sandbox] sandbox @see #sandbox
+        # @param [String] path @see #path
+        # @param [Array<PodTarget>] pod_targets @see #pod_targets
+        # @param [Hash{String=>Symbol}] build_configurations @see #build_configurations
+        # @param [Array<Platform>] platforms @see #platforms
+        # @param [String] object_version @see #object_version
+        # @param [String] podfile_path @see #podfile_path
+        #
+        def initialize(sandbox, path, pod_targets, build_configurations, platforms,
+                       object_version, podfile_path = nil, pod_target_subproject: false)
+          @sandbox = sandbox
+          @path = path
+          @pod_targets = pod_targets
+          @build_configurations = build_configurations
+          @platforms = platforms
+          @object_version = object_version
+          @podfile_path = podfile_path
+          @pod_target_subproject = pod_target_subproject
+        end
+    
+            self.indentation_level += 2
+        @treat_titles_as_messages = true
+        yield if block_given?
+      ensure
+        @treat_titles_as_messages = false
+        self.indentation_level -= 2
+      end
+    
+    When /^(?:|I )choose '([^']*)'$/ do |field|
+  choose(field)
+end
+    
+    Before do
+  gemfile = ENV['BUNDLE_GEMFILE'].to_s
+  ENV['BUNDLE_GEMFILE'] = File.join(Dir.pwd, gemfile) unless gemfile.start_with?(Dir.pwd)
+  @framework_version = nil
+end
+    
+        def type_from_mime_magic
+      @type_from_mime_magic ||= File.open(@filepath) do |file|
+        MimeMagic.by_magic(file).try(:type)
+      end
+    end
+    
+          [ scale_geometry, crop_geometry ]
+    end
+    
+        # Hash access of interpolations. Included only for compatibility,
+    # and is not intended for normal use.
+    def self.[] name
+      method(name)
+    end
