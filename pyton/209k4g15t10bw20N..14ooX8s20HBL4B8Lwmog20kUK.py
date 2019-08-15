9a1a208492343124e734d0d942d11451f5babdb0
SUNsung <weatherius@gmail.com>
2019-08-15 23:10:39 +0300

    Rewrite [209k4g15t10bw20N..14ooX8s20HBL4B8Lwmog20kUK.py] File in: 2019-08-15 23:10:39

--- pyton/209k4g15t10bw20N..14ooX8s20HBL4B8Lwmog20kUK.py
+++ pyton/209k4g15t10bw20N..14ooX8s20HBL4B8Lwmog20kUK.py
@@ -1,70 +1,161 @@
 
         
-        
-def parse_tensorflow_examples(record, batch_size, path_to_index):
-  '''Reads TensorFlow examples from a RecordReader.
+            @classmethod
+    def basic_response_server(cls, **kwargs):
+        return cls.text_response_server(
+            'HTTP/1.1 200 OK\r\n' +
+            'Content-Length: 0\r\n\r\n',
+            **kwargs
+        )
+    
+            :param conn: The urllib3 connection object associated with the cert.
+        :param url: The requested URL.
+        :param verify: Either a boolean, in which case it controls whether we verify
+            the server's TLS certificate, or a string, in which case it must be a path
+            to a CA bundle to use
+        :param cert: The SSL certificate to verify.
+        '''
+        if url.lower().startswith('https') and verify:
+    
+        This function works best on CPython and PyPy: in particular, it probably
+    doesn't work for Jython or IronPython. Future investigation should be done
+    to work out the correct shape of the code for those platforms.
+    '''
+    implementation = platform.python_implementation()
     
     
-def compute_path_embeddings(model, session, instances):
-  '''Compute the path embeddings for all the distinct paths.
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
+    '''
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
+    
+            This function eventually generates a ``Cookie`` header from the
+        given cookies using cookielib. Due to cookielib's design, the header
+        will not be regenerated if it already exists, meaning this function
+        can only be called once for the life of the
+        :class:`PreparedRequest <PreparedRequest>` object. Any subsequent calls
+        to ``prepare_cookies`` will have no actual effect, unless the 'Cookie'
+        header is removed beforehand.
+        '''
+        if isinstance(cookies, cookielib.CookieJar):
+            self._cookies = cookies
+        else:
+            self._cookies = cookiejar_from_dict(cookies)
+    
+    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
+    title = r1(r'<title>(.*)</title>', html)
+    assert title
+    id = r1(r'flvid\s*=\s*(\d+)', html)
+    assert id
+    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    
+    from ..common import *
+    
+    from xml.etree import cElementTree as ET
+from copy import copy
+from ..common import *
+#----------------------------------------------------------------------
+def ckplayer_get_info_by_xml(ckinfo):
+    '''str->dict
+    Information for CKPlayer API content.'''
+    e = ET.XML(ckinfo)
+    video_dict = {'title': '',
+                  #'duration': 0,
+                  'links': [],
+                  'size': 0,
+                  'flashvars': '',}
+    dictified = dictify(e)['ckplayer']
+    if 'info' in dictified:
+        if '_text' in dictified['info'][0]['title'][0]:  #title
+            video_dict['title'] = dictified['info'][0]['title'][0]['_text'].strip()
+    
+    def douban_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    html = get_html(url)
+    
+    	type, ext, size = url_info(url)
+	print_info(site_info, title, type, size)
+	
+	if not info_only:
+		download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+        url = list(set([
+        unicodize(str.replace(i, '\\/', '/'))
+        for i in re.findall(r'<meta property='og:video:secure_url' content='(.*?)'>', html)
+    ]))
     
     
-def plot_lfads(train_bxtxd, train_model_vals,
-               train_ext_input_bxtxi=None, train_truth_bxtxd=None,
-               valid_bxtxd=None, valid_model_vals=None,
-               valid_ext_input_bxtxi=None, valid_truth_bxtxd=None,
-               bidx=None, cf=1.0, output_dist='poisson'):
-    
-    spiking_data_e = spikify_data(truth_data_e, rng, dt=FLAGS.dt,
-                              max_firing_rate=FLAGS.max_firing_rate)
-train_inds, valid_inds = get_train_n_valid_inds(E, train_percentage,
-                                                nreplications)
-    
-    
-def build_vocab(filename):
-  data = _read_words(filename)
-    
-      if FLAGS.dis_share_embedding:
-    assert hparams.dis_rnn_size == hparams.gen_rnn_size, (
-        'If you wish to share Discriminator/Generator embeddings, they must be'
-        ' same dimension.')
-    with tf.variable_scope('gen/rnn', reuse=True):
-      embedding = tf.get_variable('embedding',
-                                  [FLAGS.vocab_size, hparams.gen_rnn_size])
-    
-    
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'tcsh')
-    proc.sendline(u'setenv PYTHONIOENCODING utf8')
-    proc.sendline(u'eval `thefuck --alias`')
-    return proc
-    
-    
-@pytest.mark.parametrize('script, output', [
-    ('vim', invalid_operation('vim')),
-    ('apt-get', '')])
-def test_not_match(script, output):
-    assert not match(Command(script, output))
-    
-    
-@pytest.mark.parametrize('command, new_command', [
-    (Command('cargo buid', no_such_subcommand_old), 'cargo build'),
-    (Command('cargo buils', no_such_subcommand), 'cargo build')])
-def test_get_new_command(command, new_command):
-    assert get_new_command(command) == new_command
+  def EmptyInsertionText_test( self ):
+    self._Check( 0, {
+      'insertion_text':  '',
+      'menu_text':       'MENU TEXT',
+      'extra_menu_info': 'EXTRA MENU INFO',
+      'kind':            'K',
+      'detailed_info':   'DETAILED INFO',
+      'extra_data': {
+        'doc_string':    'DOC STRING',
+      },
+    }, {
+      'word'     : '',
+      'abbr'     : 'MENU TEXT',
+      'menu'     : 'EXTRA MENU INFO',
+      'kind'     : 'k',
+      'info'     : 'DETAILED INFO\nDOC STRING',
+      'equal'    : 1,
+      'dup'      : 1,
+      'empty'    : 1,
+      'user_data': '0',
+    } )
 
     
-    # encoding=utf8  
-import sys  
-try:
-    reload(sys)
-except NameError:
-    pass
-try:
-    sys.setdefaultencoding('utf8')
-except AttributeError:
-    pass
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+  def Start( self ):
+    request_data = BuildRequestData()
+    if self._extra_data:
+      request_data.update( self._extra_data )
+    self._response = self.PostDataToHandler( request_data,
+                                             'debug_info',
+                                             display_message = False )
+    
+      words = line.split()
+  if not words:
+    return []
+    
+    
+def KeywordsFromSyntaxListOutput_TypeHierarchy_test():
+  assert_that( syntax_parse._KeywordsFromSyntaxListOutput( '''
+baa xxx foo bar
+        links to Foo
+Foo xxx zoo goo
+        links to Bar
+Bar xxx qux moo
+        links to Type''' ),
+              contains_inanyorder( 'foo', 'bar', 'zoo', 'goo', 'qux', 'moo' ) )
+    
+        >>> Point = namedtuple('Point', 'x y')
+    >>> Point.__doc__                   # docstring for the new class
+    'Point(x, y)'
+    >>> p = Point(11, y=22)             # instantiate with positional args or keywords
+    >>> p[0] + p[1]                     # indexable like a plain tuple
+    33
+    >>> x, y = p                        # unpack like a regular tuple
+    >>> x, y
+    (11, 22)
+    >>> p.x + p.y                       # fields also accessable by name
+    33
+    >>> d = p._asdict()                 # convert to a dictionary
+    >>> d['x']
+    11
+    >>> Point(**d)                      # convert from a dictionary
+    Point(x=11, y=22)
+    >>> p._replace(x=100)               # _replace() is like str.replace() but targets named fields
+    Point(x=100, y=22)
