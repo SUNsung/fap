11163f2052865084aa8a6622033b7884adc692b3
SUNsung <weatherius@gmail.com>
2019-08-15 10:22:07 +0300

    Rewrite [21wjP3Jifilmk21.bs8ZuH8WbY21Stojx.scqWo21IsJ.py] File in: 2019-08-15 10:22:07

--- pyton/21wjP3Jifilmk21.bs8ZuH8WbY21Stojx.scqWo21IsJ.py
+++ pyton/21wjP3Jifilmk21.bs8ZuH8WbY21Stojx.scqWo21IsJ.py
@@ -1,28 +1,126 @@
 
         
-        # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+            def reducer(self, key, values):
+        '''Sum values for each key.
     
-    import luigi
-    
-            :return: the target output for this task.
-        :rtype: object (:py:class:`~luigi.target.Target`)
-        '''
-        return RemoteTarget(
-            '/tmp/stuff',
-            SSH_HOST
-        )
-    
-        date = luigi.DateParameter()
+        def get_people(self, ids):
+        results = []
+        for id in ids:
+            if id in self.people:
+                results.append(self.people[id])
+        return results
     
     
-class BatchNotifier(object):
-    def __init__(self, **kwargs):
-        self._config = batch_email(**kwargs)
-        self._fail_counts = collections.defaultdict(collections.Counter)
-        self._disabled_counts = collections.defaultdict(collections.Counter)
-        self._scheduling_fail_counts = collections.defaultdict(collections.Counter)
-        self._fail_expls = collections.defaultdict(_fail_queue(self._config.error_messages))
-        self._update_next_send()
+class Cache(object):
     
-    logger = logging.getLogger('luigi-interface')
+    
+class Page(object):
+    
+    site_info = 'baomihua.com'
+download = baomihua_download
+download_playlist = playlist_not_supported('baomihua')
+
+    
+            for i in self.tree.iterfind('video/quality'):
+            quality = i.attrib ['value']
+            url = i[0].attrib['playurl']
+            self.stream_types.append({'id': quality,
+                                      'video_profile': i.attrib ['desp']})
+            self.streams[quality] = {'url': url,
+                                     'video_profile': i.attrib ['desp']}
+            self.streams_sorted = [dict([('id', stream_type['id'])] + list(self.streams[stream_type['id']].items())) for stream_type in self.__class__.stream_types if stream_type['id'] in self.streams]
+    
+    
+class CNTV(VideoExtractor):
+    name = 'CNTV.com'
+    stream_types = [
+        {'id': '1', 'video_profile': '1280x720_2000kb/s', 'map_to': 'chapters4'},
+        {'id': '2', 'video_profile': '1280x720_1200kb/s', 'map_to': 'chapters3'},
+        {'id': '3', 'video_profile': '640x360_850kb/s', 'map_to': 'chapters2'},
+        {'id': '4', 'video_profile': '480x270_450kb/s', 'map_to': 'chapters'},
+        {'id': '5', 'video_profile': '320x180_200kb/s', 'map_to': 'lowChapters'},
+    ]
+    
+    site_info = 'ehow.com'
+download = ehow_download
+download_playlist = playlist_not_supported('ehow')
+
+    
+        def _init_keypoints(self):
+        '''Initialize COCO keypoint information.'''
+        self.keypoints = None
+        self.keypoint_flip_map = None
+        self.keypoints_to_id_map = None
+        self.num_keypoints = 0
+        # Thus far only the 'person' category has keypoints
+        if 'person' in self.category_to_id_map:
+            cat_info = self.COCO.loadCats([self.category_to_id_map['person']])
+        else:
+            return
+    
+    
+class ZeroEvenOpTest(unittest.TestCase):
+    
+                try:
+                # Under some conditions (e.g., dynamic memory optimization)
+                # it is possible that the network frees some blobs when they are
+                # no longer needed. Handle this case...
+                output_shape = workspace.FetchBlob(output_name).shape
+            except BaseException:
+                output_shape = '<unknown>'
+    
+    # Copyright (c) 2017-present, Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+    
+    
+def _do_python_eval(json_dataset, salt, output_dir='output'):
+    info = voc_info(json_dataset)
+    year = info['year']
+    anno_path = info['anno_path']
+    image_set_path = info['image_set_path']
+    devkit_path = info['devkit_path']
+    cachedir = os.path.join(devkit_path, 'annotations_cache')
+    aps = []
+    # The PASCAL VOC metric changed in 2010
+    use_07_metric = True if int(year) < 2010 else False
+    logger.info('VOC07 metric? ' + ('Yes' if use_07_metric else 'No'))
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+    for _, cls in enumerate(json_dataset.classes):
+        if cls == '__background__':
+            continue
+        filename = _get_voc_results_file_template(
+            json_dataset, salt).format(cls)
+        rec, prec, ap = voc_eval(
+            filename, anno_path, image_set_path, cls, cachedir, ovthresh=0.5,
+            use_07_metric=use_07_metric)
+        aps += [ap]
+        logger.info('AP for {} = {:.4f}'.format(cls, ap))
+        res_file = os.path.join(output_dir, cls + '_pr.pkl')
+        save_object({'rec': rec, 'prec': prec, 'ap': ap}, res_file)
+    logger.info('Mean AP = {:.4f}'.format(np.mean(aps)))
+    logger.info('~~~~~~~~')
+    logger.info('Results:')
+    for ap in aps:
+        logger.info('{:.3f}'.format(ap))
+    logger.info('{:.3f}'.format(np.mean(aps)))
+    logger.info('~~~~~~~~')
+    logger.info('')
+    logger.info('----------------------------------------------------------')
+    logger.info('Results computed with the **unofficial** Python eval code.')
+    logger.info('Results should be very close to the official MATLAB code.')
+    logger.info('Use `./tools/reval.py --matlab ...` for your paper.')
+    logger.info('-- Thanks, The Management')
+    logger.info('----------------------------------------------------------')
