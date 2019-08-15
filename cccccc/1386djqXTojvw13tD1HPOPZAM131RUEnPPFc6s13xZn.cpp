33484fa35ba2e8b5f1ee2a745378fb9b9b73a031
SUNsung <weatherius@gmail.com>
2019-08-15 06:02:17 +0300

    Rewrite [1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp] File in: 2019-08-15 06:02:16

--- cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
+++ cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
@@ -1,194 +1,262 @@
 
         
-            void KeyChord::Modifiers(Settings::KeyModifiers const& value)
+          // Return to the UI thread to handle opening a new browser tab.
+  ui_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(&ProcessSingleton::LinuxWatcher::HandleMessage,
+                                parent_, current_dir, tokens, this));
+  fd_watch_controller_.reset();
+    
+      // If we don't have any window then quitAndInstall immediately.
+  if (WindowList::IsEmpty()) {
+    auto_updater::AutoUpdater::QuitAndInstall();
+    return;
+  }
+    
+    v8::Local<v8::Promise> StartTracing(
+    v8::Isolate* isolate,
+    const base::trace_event::TraceConfig& trace_config) {
+  electron::util::Promise promise(isolate);
+  v8::Local<v8::Promise> handle = promise.GetHandle();
+    }
+    
+    v8::Local<v8::Promise> Debugger::SendCommand(mate::Arguments* args) {
+  electron::util::Promise promise(isolate());
+  v8::Local<v8::Promise> handle = promise.GetHandle();
+    }
+    
+    
+    {}  // namespace electron
+    
+    #include <map>
+#include <string>
+#include <vector>
+    
+      v8::Local<v8::Promise> PurchaseProduct(const std::string& product_id,
+                                         mate::Arguments* args);
+    
+     protected:
+  void PopupAt(TopLevelWindow* window,
+               int x,
+               int y,
+               int positioning_item,
+               const base::Closure& callback) override;
+  void ClosePopupAt(int32_t window_id) override;
+    
+    
+    {}  // namespace api
+    
+        base::win::ShortcutProperties props;
+    base::string16 appID;
+    if (content::Shell::GetPackage()->root()->GetString('app-id', &appID) == false)
+      content::Shell::GetPackage()->root()->GetString(switches::kmName, &appID);
+    const std::wstring appName = base::UTF8ToWide(content::Shell::GetPackage()->GetName());
+    props.set_app_id(appID);
+    
+    #include 'base/logging.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/api_messages.h'
+#include 'content/public/renderer/render_view.h'
+#include 'content/public/renderer/render_thread.h'
+#include 'content/public/renderer/v8_value_converter.h'
+#include 'third_party/WebKit/public/web/WebView.h'
+#include 'third_party/WebKit/public/web/WebLocalFrame.h'
+#include 'ui/base/resource/resource_bundle.h'
+    
+    namespace nwapi {
+    }
+    
+    
     {
-        _modifiers = value;
+    {    if (zoom_controller) {
+      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
+      if (zoom_factor > content::kMaximumZoomFactor) {
+        zoom_factor = content::kMaximumZoomFactor;
+      }
+      if (zoom_factor < content::kMinimumZoomFactor) {
+        zoom_factor = content::kMinimumZoomFactor;
+      }
+      x *= zoom_factor;
+      y *= zoom_factor;
     }
     
-    int UniValue::get_int() const
-{
-    if (typ != VNUM)
-        throw std::runtime_error('JSON value is not an integer as expected');
-    int32_t retval;
-    if (!ParseInt32(getValStr(), &retval))
-        throw std::runtime_error('JSON integer out of range');
-    return retval;
+    Popup(x, y, rvh);
+  } else if (method == 'EnableShowEvent') {
+    arguments.GetBoolean(0, &enable_show_event_);
+  } else {
+    NOTREACHED() << 'Invalid call to Menu method:' << method
+                 << ' arguments:' << arguments;
+  }
 }
     
-    // Return the name of the old info log file for 'dbname'.
-extern std::string OldInfoLogFileName(const std::string& dbname);
+      std::string type;
     
-    namespace google {
-namespace protobuf {
-    }
-    }
-    
-    #define GOOGLE_CHECK_GET_FLOAT(arg, value, err)                        \
-    float value;                                            \
-    if (!CheckAndGetFloat(arg, &value)) {                   \
-      return err;                                          \
-    }                                                       \
-    
-    
-    {  void* mem = options_.block_alloc(size);
-  Block* b = new (mem) Block(size, last_block);
-  space_allocated_.fetch_add(size, std::memory_order_relaxed);
-  return b;
+    void MenuItem::SetIcon(const std::string& icon) {
+  if (icon.empty()) {
+    gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menu_item_), NULL); 
+  } else {
+    gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menu_item_),
+                                  gtk_image_new_from_file(icon.c_str()));
+    gtk_image_menu_item_set_always_show_image(GTK_IMAGE_MENU_ITEM(menu_item_),
+                                              TRUE);
+  }
 }
     
+    void NwAppCloseAllWindowsFunction::DoJob(AppWindowRegistry* registry, std::string id) {
+  if (base::FeatureList::IsEnabled(::features::kNWNewWin)) {
+    chrome::CloseAllBrowsers();
+  }
+  AppWindowRegistry::AppWindowList windows =
+    registry->GetAppWindowsForApp(id);
+    }
     
-    { private:
-  class NewDeleteCapture {
-   public:
-    // TOOD(xiaofeng): Implement this for opensource protobuf.
-    void Hook() {}
-    void Unhook() {}
-    int alloc_count() { return 0; }
-    int free_count() { return 0; }
-  } capture_alloc;
+    NwClipboardSetListSyncFunction::NwClipboardSetListSyncFunction() {
+}
+    
+      // Ensure nomnigraph can handle this change
+  auto nn = caffe2::convertToNNModule(net);
+  auto new_net = caffe2::convertToCaffe2Proto(nn);
+    
+      Fork() {
+    pid = fork();
+    if (pid < 0) {
+      throw std::system_error(errno, std::system_category(), 'fork');
+    }
+  }
+    
+    OPERATOR_SCHEMA(Glu)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .SetDoc(R'DOC(
+Applies gated linear unit to the input Tensor X. The output Y is half the size
+of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
+[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
+X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
+)DOC')
+    .Input(0, 'X', '1D input tensor')
+    .Output(0, 'Y', '1D output tensor');
+    
+    namespace xgboost {
+namespace common {
+/*! \brief thread local entry for random. */
+struct RandomThreadLocalEntry {
+  /*! \brief the random engine instance. */
+  GlobalRandomEngine engine;
+};
+    }
+    }
+    
+    template <typename T>
+struct TestTransformRange {
+  void XGBOOST_DEVICE operator()(size_t _idx,
+                                 Span<bst_float> _out, Span<const bst_float> _in) {
+    _out[_idx] = _in[_idx];
+  }
 };
     
+      devices = GPUSet{0, 1};
+  ASSERT_TRUE(devices != GPUSet::Empty());
+  EXPECT_EQ(devices.Size(), 1);
+    
+    namespace xgboost {
+namespace common {
+/*!
+ * \brief Define mt19937 as default type Random Engine.
+ */
+using RandomEngine = std::mt19937;
+    }
+    }
+    
+    template <typename T>
+void HostDeviceVector<T>::Shard(const GPUDistribution& distribution) const { }
+    
     
     {
     {
-    {}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
+    {  inline void PutChar(char ch) {
+    out_buf += ch;
+    if (out_buf.length() >= kBufferSize) Flush();
+  }
+  inline void Flush(void) {
+    if (out_buf.length() != 0) {
+      fp->Write(&out_buf[0], out_buf.length());
+      out_buf.clear();
+    }
+  }
+};
+}  // namespace common
+}  // namespace xgboost
+#endif  // XGBOOST_COMMON_BASE64_H_
 
     
+      case errSSLClosedGraceful:
+  case errSSLClosedNoNotify:
+    closeConnection();
+    return TLS_ERR_ERROR;
     
-    {  format(
-      'static const int $constant_name$ = $number$;\n'
-      '$1$ ::$proto_ns$::internal::ExtensionIdentifier< $extendee$,\n'
-      '    ::$proto_ns$::internal::$type_traits$, $field_type$, $packed$ >\n'
-      '  ${2$$name$$}$;\n',
-      qualifier, descriptor_);
-}
-    
-    namespace atu = annotation_test_util;
-    
-      // Verify that we can open the file.  Note that this also has the side-effect
-  // of verifying that we are not canonicalizing away any non-existent
-  // directories.
-  std::unique_ptr<io::ZeroCopyInputStream> stream(OpenDiskFile(disk_file));
-  if (stream == NULL) {
-    return CANNOT_OPEN;
-  }
-    
-    
-    {	locked = false;
-}
-    
-    			for (int i = 0; i < mesh_merge.faces.size(); i++) {
-				if (mesh_merge.faces[i].inside)
-					continue;
-				for (int j = 0; j < 3; j++) {
-					result.faces.write[outside_count].vertices[j] = mesh_merge.points[mesh_merge.faces[i].points[j]];
-					result.faces.write[outside_count].uvs[j] = mesh_merge.faces[i].uvs[j];
-				}
-    }
-    
-    		} else if (p_event->is_action('ui_home')) {
-    
-    				Basis shape_inertia_tensor = shape->get_moment_of_inertia(mass).to_diagonal_matrix();
-				Transform shape_transform = get_shape_transform(i);
-				Basis shape_basis = shape_transform.basis.orthonormalized();
-    
-    	for (List<PropertyInfo>::Element *E = pinfo.front(); E; E = E->next()) {
-		const PropertyInfo &pi = E->get();
-    }
-    
-    
-    {	return p_length;
-}
-    
-    String InputEventJoypadButton::as_text() const {
-    }
-    
-    
-    {	ERR_FAIL_COND(p_align < 1);
-	mem_ptr = Memory::alloc_static(p_size + p_align, 'PoolAllocator()');
-	uint8_t *mem8 = (uint8_t *)mem_ptr;
-	uint64_t ofs = (uint64_t)mem8;
-	if (ofs % p_align)
-		mem8 += p_align - (ofs % p_align);
-	create_pool(mem8, p_size, p_max_entries);
-	needs_locking = p_needs_locking;
-	align = p_align;
-}
-    
-    
-    { private:
-  std::mutex mutex_;
-  std::list<std::weak_ptr<TimerTask>> task_list_;
-};
-    
-    #include 'modules/common/proto/pnc_point.pb.h'
-#include 'modules/planning/proto/planning.pb.h'
-    
-    
-    {  bool is_parked = is_on_parking_lane || is_at_road_edge;
-  return is_parked && obstacle->IsStatic();
-}
-    
-      // set scenario_type in PlanningContext
-  auto* scenario = PlanningContext::Instance()
-                       ->mutable_planning_status()
-                       ->mutable_scenario();
-  scenario->Clear();
-  scenario->set_scenario_type(scenario_type());
-    
-    
-    {  ScenarioConfig config;
-  EXPECT_TRUE(apollo::cyber::common::GetProtoFromFile(
-      FLAGS_scenario_valet_parking_config_file, &config));
-  ScenarioContext context;
-  scenario_.reset(new ValetParkingScenario(config, &context));
-  EXPECT_EQ(scenario_->scenario_type(), ScenarioConfig::VALET_PARKING);
-}
-    
-    std::unique_ptr<Stage> BareIntersectionUnprotectedScenario::CreateStage(
-    const ScenarioConfig::StageConfig& stage_config) {
-  if (s_stage_factory_.Empty()) {
-    RegisterStages();
-  }
-  auto ptr = s_stage_factory_.CreateObjectOrNull(stage_config.stage_type(),
-                                                 stage_config);
-  if (ptr) {
-    ptr->SetContext(&context_);
-  }
-  return ptr;
-}
-    
-    #include <string>
-#include <openpose/core/macros.hpp>
-#include <openpose/core/point.hpp>
-    
-    
-    {        if (err != NULL) {
-            *err = error;
-        }
-    }
-    
-    namespace op
+    TLSContext* TLSContext::make(TLSSessionSide side, TLSVersion minVer)
 {
-    const auto HAND_MAX_HANDS = 2*POSE_MAX_PEOPLE;
+  return new OpenSSLTLSContext(side, minVer);
+}
+    
+    
+    {  int r = select(sockfd_ + 1, nullptr, &fds, nullptr, &tv);
+  int errNum = SOCKET_ERRNO;
+  if (r == 1) {
+    return true;
+  }
+  if (r == 0) {
+    // time out
+    return false;
+  }
+  if (errNum == A2_EINPROGRESS || errNum == A2_EINTR) {
+    return false;
+  }
+  throw DL_RETRY_EX(fmt(EX_SOCKET_CHECK_WRITABLE, errorMsg(errNum).c_str()));
+#endif   // !HAVE_POLL
+}
+    
+    bool WinTLSContext::addCredentialFile(const std::string& certfile,
+                                      const std::string& keyfile)
+{
+  std::stringstream ss;
+  BufferedFile(certfile.c_str(), 'rb').transfer(ss);
+  auto data = ss.str();
+  CRYPT_DATA_BLOB blob = {(DWORD)data.length(), (BYTE*)data.c_str()};
+  if (!::PFXIsPFXBlob(&blob)) {
+    A2_LOG_ERROR('Not a valid PKCS12 file');
+    return false;
+  }
+  HCERTSTORE store =
+      ::PFXImportCertStore(&blob, L'', CRYPT_EXPORTABLE | CRYPT_USER_KEYSET);
+  if (!store_) {
+    store = ::PFXImportCertStore(&blob, nullptr,
+                                 CRYPT_EXPORTABLE | CRYPT_USER_KEYSET);
+  }
+  if (!store) {
+    A2_LOG_ERROR('Failed to import PKCS12 store');
+    return false;
+  }
+  auto ctx = ::CertEnumCertificatesInStore(store, nullptr);
+  if (!ctx) {
+    A2_LOG_ERROR('PKCS12 file does not contain certificates');
+    ::CertCloseStore(store, 0);
+    return false;
+  }
+  ::CertFreeCertificateContext(ctx);
     }
     
-            Array<long long> extractIdsLockThread(const Array<float>& poseKeypoints, const cv::Mat& cvMatInput,
-                                              const unsigned long long imageViewIndex,
-                                              const long long frameId);
+      std::chrono::seconds timeout_;
     
-        template<typename T>
-    std::string Rectangle<T>::toString() const
-    {
-        try
-        {
-            return '[' + std::to_string(x) + ', ' + std::to_string(y) + ', ' + std::to_string(width) + ', ' + std::to_string(height) + ']';
-        }
-        catch (const std::exception& e)
-        {
-            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
-            return '';
-        }
+    namespace aria2 {
+    }
+    
+      virtual std::unique_ptr<Dict> getResponse() = 0;
+    
+      std::vector<std::shared_ptr<DHTNode>> nodes_;
+    
+      void setLocalNode(const std::shared_ptr<DHTNode>& localNode);
+    
+    DHTTaskExecutor::~DHTTaskExecutor() = default;
+    
+    namespace aria2 {
     }
