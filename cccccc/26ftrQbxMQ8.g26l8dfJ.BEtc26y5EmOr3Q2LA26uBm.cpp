e7e7db5a5a90a0fa2cfc54eea3ff0b07b68f5947
SUNsung <weatherius@gmail.com>
2019-08-15 03:26:13 +0300

    Rewrite [26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp] File in: 2019-08-15 03:26:12

--- cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
+++ cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
@@ -1,251 +1,286 @@
 
         
-          // Clean up the bounding boxes from the polygonal approximation by
-  // expanding slightly, then clipping to the blobs from the original_word
-  // that overlap. If not null, the block provides the inverse rotation.
-  void ClipToOriginalWord(const BLOCK* block, WERD* original_word);
-    
-      tesseract::ParagraphJustification justification() const {
-    return justification_;
-  }
-  int margin() const { return margin_; }
-  int first_indent() const { return first_indent_; }
-  int body_indent() const { return body_indent_; }
-  int tolerance() const { return tolerance_; }
-  bool is_flush() const {
-    return (justification_ == tesseract::JUSTIFICATION_LEFT ||
-            justification_ == tesseract::JUSTIFICATION_RIGHT) &&
-        abs(first_indent_ - body_indent_) <= tolerance_;
+          const char* tmpdir = getenv('TMPDIR');
+  if (tmpdir == nullptr) {
+    tmpdir = '/tmp';
   }
     
+    Github Links:
     
-    { private:
-  CensusContext context_;
-  // Metadata elements for tracing and census stats data.
-  grpc_linked_mdelem stats_bin_;
-  grpc_linked_mdelem tracing_bin_;
-  // Client method.
-  absl::string_view method_;
-  std::string qualified_method_;
-  grpc_slice path_;
-  // The recv trailing metadata callbacks.
-  grpc_metadata_batch* recv_trailing_metadata_;
-  grpc_closure* initial_on_done_recv_trailing_metadata_;
-  grpc_closure on_done_recv_trailing_metadata_;
-  // recv message
-  grpc_closure* initial_on_done_recv_message_;
-  grpc_closure on_done_recv_message_;
-  // Start time (for measuring latency).
-  absl::Time start_time_;
-  // Server elapsed time in nanoseconds.
-  uint64_t elapsed_time_;
-  // The received message--may be null.
-  grpc_core::OrphanablePtr<grpc_core::ByteStream>* recv_message_;
-  // Number of messages in this RPC.
-  uint64_t recv_message_count_;
-  uint64_t sent_message_count_;
-  // Buffer needed for grpc_slice to reference when adding trace context
-  // metatdata to outgoing message.
-  char tracing_buf_[kMaxTraceContextLen];
-};
+    OPERATOR_SCHEMA(Glu)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .SetDoc(R'DOC(
+Applies gated linear unit to the input Tensor X. The output Y is half the size
+of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
+[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
+X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
+)DOC')
+    .Input(0, 'X', '1D input tensor')
+    .Output(0, 'Y', '1D output tensor');
     
-    #include <grpc/support/port_platform.h>
+    #endif  // TESSERACT_CCSTRUCT_DEBUGPIXA_H_
+
     
-    uint64_t FiletimeToInt(const FILETIME& ft) {
-  ULARGE_INTEGER i;
-  i.LowPart = ft.dwLowDateTime;
-  i.HighPart = ft.dwHighDateTime;
-  return i.QuadPart;
+    
+// Returns the median value of the vector, given that the values are
+// circular, with the given modulus. Values may be signed or unsigned,
+// eg range from -pi to pi (modulus 2pi) or from 0 to 2pi (modulus 2pi).
+// NOTE that the array is shuffled, but the time taken is linear.
+// An assumption is made that most of the values are spread over no more than
+// half the range, but wrap-around is accounted for if the median is near
+// the wrap-around point.
+// Cannot be a member of GenericVector, as it makes heavy used of LLSQ.
+// T must be an integer or float/double type.
+template<typename T> T MedianOfCircularValues(T modulus, GenericVector<T>* v) {
+  LLSQ stats;
+  T halfrange = static_cast<T>(modulus / 2);
+  int num_elements = v->size();
+  for (int i = 0; i < num_elements; ++i) {
+    stats.add((*v)[i], (*v)[i] + halfrange);
+  }
+  bool offset_needed = stats.y_variance() < stats.x_variance();
+  if (offset_needed) {
+    for (int i = 0; i < num_elements; ++i) {
+      (*v)[i] += halfrange;
+    }
+  }
+  int median_index = v->choose_nth_item(num_elements / 2);
+  if (offset_needed) {
+    for (int i = 0; i < num_elements; ++i) {
+      (*v)[i] -= halfrange;
+    }
+  }
+  return (*v)[median_index];
 }
     
-    ThreadPoolInterface* CreateDefaultThreadPool();
+      // A hole in the heap exists at hole_index, and we want to fill it with the
+  // given pair. SiftDown sifts the hole downward to the correct position and
+  // returns the destination index without actually putting pair there.
+  int SiftDown(int hole_index, const Pair& pair) {
+    int heap_size = heap_.size();
+    int child;
+    while ((child = LeftChild(hole_index)) < heap_size) {
+      if (child + 1 < heap_size && heap_[child + 1] < heap_[child])
+        ++child;
+      if (heap_[child] < pair) {
+        heap_[hole_index] = heap_[child];
+        hole_index = child;
+      } else {
+        break;
+      }
+    }
+    return hole_index;
+  }
     
-            case IDC_LOG: /* Functions for common log. */
-            result = Log10(rat);
-            break;
     
+    {  // WARNING! Keep data as the first element! KDPairInc and KDPairDec depend
+  // on the order of these elements so they can downcast pointers appropriately
+  // for use by GenericHeap::Reshuffle.
+  Data data;
+  Key key;
+};
+// Specialization of KDPair to provide operator< for sorting in increasing order
+// and recasting of data pointers for use with DoublePtr.
+template <typename Key, typename Data>
+struct KDPairInc : public KDPair<Key, Data> {
+  KDPairInc() = default;
+  KDPairInc(Key k, Data d) : KDPair<Key, Data>(k, d) {}
+  // Operator< facilitates sorting in increasing order.
+  int operator<(const KDPairInc<Key, Data>& other) const {
+    return this->key < other.key;
+  }
+  // Returns the input Data pointer recast to a KDPairInc pointer.
+  // Just casts a pointer to the first element to a pointer to the whole struct.
+  static KDPairInc* RecastDataPointer(Data* data_ptr) {
+    return reinterpret_cast<KDPairInc*>(data_ptr);
+  }
+};
+// Specialization of KDPair to provide operator< for sorting in decreasing order
+// and recasting of data pointers for use with DoublePtr.
+template <typename Key, typename Data>
+struct KDPairDec : public KDPair<Key, Data> {
+  KDPairDec() = default;
+  KDPairDec(Key k, Data d) : KDPair<Key, Data>(k, d) {}
+  // Operator< facilitates sorting in decreasing order by using operator> on
+  // the key values.
+  int operator<(const KDPairDec<Key, Data>& other) const {
+    return this->key > other.key;
+  }
+  // Returns the input Data pointer recast to a KDPairDec pointer.
+  // Just casts a pointer to the first element to a pointer to the whole struct.
+  static KDPairDec* RecastDataPointer(Data* data_ptr) {
+    return reinterpret_cast<KDPairDec*>(data_ptr);
+  }
+};
     
-    {        // and then add two
-        iRes += 2;
+    class QRSequenceGenerator {
+ public:
+  // Object is initialized with the size of the output range.
+  explicit QRSequenceGenerator(int N) : N_(N), next_num_(0) {
+    num_bits_ = static_cast<int>(ceil(log(static_cast<double>(N)) / log(2.0)));
+  }
     }
     
-        ResultCode Append(_In_opt_ TType item)
-    {
-        ResultCode hr = S_OK;
-        try
-        {
-            m_vector.push_back(item);
-        }
-        catch (const std::bad_alloc& /*ex*/)
-        {
-            hr = E_OUTOFMEMORY;
-        }
-        return hr;
+    
+    {}  // namespace tesseract
+
+    
+    
+    {}  // namespace mongo
+
+    
+    #include 'mongo/db/stats/top.h'
+#include 'mongo/unittest/unittest.h'
+    
+        virtual ~Base() {
+        if (!collection())
+            return;
+        WriteUnitOfWork wunit(&_opCtx);
+        _context.db()->dropCollection(&_opCtx, nss()).transitional_ignore();
+        wunit.commit();
     }
     
-            // No new command should not be added before CommandSign, 80
-        // If it is needed, the following two functions need to be revised too.
-        // CalculatorManager::MapCommandForSerialize(Command command);
-        // CalculatorManager::MapCommandForDeSerialize(unsigned char command);
-        CommandSIGN = 80,
-        CommandCLEAR = 81,
-        CommandCENTR = 82,
-        CommandBACK = 83,
+            // Set up the count stage
+        auto params = makeCountScanParams(&_opCtx, getIndex(ctx.db(), BSON('a' << 1)));
+        params.startKey = BSON('' << 3);
+        params.startKeyInclusive = false;
+        params.endKey = BSON('' << 7);
+        params.endKeyInclusive = false;
     
-    namespace parallel {
+    #ifdef REGEX_RUN_DEBUG
+    if (fTraceDebug) {
+        printf('MatchAt(startIdx=%ld)\n', startIdx);
+        printf('Original Pattern: \'%s\'\n', CStr(StringFromUText(fPattern->fPattern))());
+        printf('Input String:     \'%s\'\n\n', CStr(StringFromUText(fInputText))());
     }
+#endif
+    
+    #if !UCONFIG_NO_TRANSLITERATION
     
     #include 'unicode/regex.h'
-#include 'unicode/uniset.h'
-#include 'unicode/uchar.h'
-#include 'unicode/ustring.h'
-#include 'unicode/rbbi.h'
-#include 'unicode/utf.h'
-#include 'unicode/utf16.h'
-#include 'uassert.h'
+#include 'unicode/uclean.h'
 #include 'cmemory.h'
 #include 'cstr.h'
+#include 'uassert.h'
+#include 'uhash.h'
 #include 'uvector.h'
 #include 'uvectr32.h'
 #include 'uvectr64.h'
+#include 'regexcmp.h'
 #include 'regeximp.h'
 #include 'regexst.h'
-#include 'regextxt.h'
-#include 'ucase.h'
     
-    Format* SelectFormat::clone() const
-{
-    return new SelectFormat(*this);
+    U_NAMESPACE_BEGIN
+    
+    U_NAMESPACE_BEGIN
+    
+    #endif
+
+    
+    namespace apollo {
+namespace planning {
+    }
+    }
+    
+    #pragma once
+    
+    namespace apollo {
+namespace planning {
+    }
+    }
+    
+        params.push_back(static_cast<float>(node['world2cam'].size()));
+    
+    TEST_F(StageApproachingParkingSpotTest, Init) {
+  StageApproachingParkingSpot stage_approaching_parking_spot(config_);
+  EXPECT_EQ(stage_approaching_parking_spot.Name(),
+            ScenarioConfig::StageType_Name(config_.stage_type()));
 }
     
-    #endif // SELFMTIMPL
-//eof
-
-    
-    U_NAMESPACE_END
-    
-      virtual void Update(HostDeviceVector<GradientPair>* in_gpair, DMatrix* data,
-                      gbm::GBLinearModel* model,
-                      double sum_instance_weight) = 0;
-    
-    
-    {
-    {
-    {  inline void PutChar(char ch) {
-    out_buf += ch;
-    if (out_buf.length() >= kBufferSize) Flush();
+    inline void L2Norm(int feat_dim, float *feat_data) {
+  if (feat_dim == 0) {
+    return;
   }
-  inline void Flush(void) {
-    if (out_buf.length() != 0) {
-      fp->Write(&out_buf[0], out_buf.length());
-      out_buf.clear();
+  // feature normalization
+  float l2norm = 0.0f;
+  for (int i = 0; i < feat_dim; ++i) {
+    l2norm += feat_data[i] * feat_data[i];
+  }
+  if (l2norm == 0) {
+    float val = 1.f / std::sqrt(static_cast<float>(feat_dim));
+    for (int i = 0; i < feat_dim; ++i) {
+      feat_data[i] = val;
+    }
+  } else {
+    l2norm = std::sqrt(l2norm);
+    for (int i = 0; i < feat_dim; ++i) {
+      feat_data[i] /= l2norm;
     }
   }
-};
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_BASE64_H_
-
+}
     
+    /// Size of blocks used for POSTing data back to remote endpoints
+CLI_FLAG(uint32,
+         carver_block_size,
+         8192,
+         'Size of blocks used for POSTing data back to remote endpoints');
     
-    {
-    {/*!
- * \brief Quantile sketch use WXQSummary
- * \tparam DType type of data content
- * \tparam RType type of rank
- */
-template<typename DType, typename RType = unsigned>
-class WXQuantileSketch :
-      public QuantileSketchTemplate<DType, RType, WXQSummary<DType, RType> > {
-};
-/*!
- * \brief Quantile sketch use WQSummary
- * \tparam DType type of data content
- * \tparam RType type of rank
- */
-template<typename DType, typename RType = unsigned>
-class GKQuantileSketch :
-      public QuantileSketchTemplate<DType, RType, GKSummary<DType, RType> > {
-};
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_QUANTILE_H_
-
+      /**
+   * @brief Map a function across the set of scheduled queries
+   *
+   * @param predicate is a function which accepts two parameters, the name of
+   * the query and the ScheduledQuery struct of the queries data. predicate
+   * will be called on each currently scheduled query.
+   *
+   * @param blacklisted [optional] return blacklisted queries if true.
+   *
+   * @code{.cpp}
+   *   std::map<std::string, ScheduledQuery> queries;
+   *   Config::get().scheduledQueries(
+   *      ([&queries](std::string name, const ScheduledQuery& query) {
+   *        queries[name] = query;
+   *      }));
+   * @endcode
+   */
+  void scheduledQueries(
+      std::function<void(std::string name, const ScheduledQuery& query)>
+          predicate,
+      bool blacklisted = false) const;
     
-    #include <dmlc/registry.h>
-#include <vector>
-#include <utility>
-#include <string>
-#include <functional>
-#include './data.h'
-#include './base.h'
-#include '../../src/common/host_device_vector.h'
+    FLAG(uint64,
+     pack_refresh_interval,
+     3600,
+     'Cache expiration for a packs discovery queries');
     
+      // Return default value in case of NotFound error
+  Expected<int32_t, DatabaseError> getInt32Or(const std::string& domain,
+                                              const std::string& key,
+                                              const int32_t default_value = 0);
+  Expected<std::string, DatabaseError> getStringOr(
+      const std::string& domain,
+      const std::string& key,
+      const std::string& default_value = '');
     
-    { private:
-  MyLogisticParam param_;
-};
+    template <typename StorageType>
+void InMemoryStorage<StorageType>::put(const std::string& key,
+                                       const StorageType value) {
+  storage_[key] = value;
+}
     
+    ExpectedSuccess<DatabaseError> RocksdbDatabase::checkDbConnection() {
+  if (db_ == nullptr) {
+    return createError(DatabaseError::DbIsNotOpen, 'Database is closed');
+  }
+  return Success();
+}
     
-    { private:
-  // default chunk size.
-  static const size_t kChunkSize = 64 << 10UL;
-  // maximum chunk size.
-  static const size_t kMaxChunk = 128;
-  // bool whether use hc
-  bool use_lz4_hc_;
-  // number of threads
-  int nthread_;
-  // number of writing threads
-  int nthread_write_;
-  // raw bytes
-  size_t raw_bytes_, raw_bytes_index_, raw_bytes_value_;
-  // encoded bytes
-  size_t encoded_bytes_index_, encoded_bytes_value_;
-  /*! \brief minimum index value */
-  uint32_t min_index_;
-  /*! \brief external memory column offset */
-  std::vector<size_t> disk_offset_;
-  // internal index
-  CompressArray<StorageIndex> index_;
-  // value set.
-  CompressArray<bst_float> value_;
-};
+    #include <boost/noncopyable.hpp>
     
-    void SimpleCSRSource::CopyFrom(dmlc::Parser<uint32_t>* parser) {
-  // use qid to get group info
-  const uint64_t default_max = std::numeric_limits<uint64_t>::max();
-  uint64_t last_group_id = default_max;
-  bst_uint group_size = 0;
-  this->Clear();
-  while (parser->Next()) {
-    const dmlc::RowBlock<uint32_t>& batch = parser->Value();
-    if (batch.label != nullptr) {
-      auto& labels = info.labels_.HostVector();
-      labels.insert(labels.end(), batch.label, batch.label + batch.size);
-    }
-    if (batch.weight != nullptr) {
-      auto& weights = info.weights_.HostVector();
-      weights.insert(weights.end(), batch.weight, batch.weight + batch.size);
-    }
-    if (batch.qid != nullptr) {
-      info.qids_.insert(info.qids_.end(), batch.qid, batch.qid + batch.size);
-      // get group
-      for (size_t i = 0; i < batch.size; ++i) {
-        const uint64_t cur_group_id = batch.qid[i];
-        if (last_group_id == default_max || last_group_id != cur_group_id) {
-          info.group_ptr_.push_back(group_size);
-        }
-        last_group_id = cur_group_id;
-        ++group_size;
-      }
-    }
-    }
-    }
-    
-        std::shared_ptr<xgboost::DMatrix> *dmat =
-        static_cast<std::shared_ptr<xgboost::DMatrix> *>(handle);
-    xgboost::MetaInfo &info = (*dmat)->Info();
-    ASSERT_EQ(info.num_col_, num_cols);
-    ASSERT_EQ(info.num_row_, row);
-    ASSERT_EQ(info.num_nonzero_, num_cols * row - num_missing);
+      virtual void SetUp() {
+    boost::uuids::uuid uuid = boost::uuids::random_generator()();
+    auto random_name = boost::uuids::to_string(uuid);
+    auto path = boost::filesystem::temp_directory_path().append(random_name);
+    boost::filesystem::create_directory(path);
+    path_ = path.string();
+  }
