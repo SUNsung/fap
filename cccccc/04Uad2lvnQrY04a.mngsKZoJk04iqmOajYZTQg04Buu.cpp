2ffbeb30bebf980e69d335ba0917ffaa41d69de9
SUNsung <weatherius@gmail.com>
2019-08-15 21:20:38 +0300

    Rewrite [04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp] File in: 2019-08-15 21:20:37

--- cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
+++ cccccc/04Uad2lvnQrY04a.mngsKZoJk04iqmOajYZTQg04Buu.cpp
@@ -1,74 +1,422 @@
 
         
-        #include 'modules/common/proto/pnc_point.pb.h'
-#include 'modules/planning/proto/planning.pb.h'
-    
-    #define AINFO_IF(cond) ALOG_IF(INFO, cond, MODULE_NAME)
-#define AWARN_IF(cond) ALOG_IF(WARN, cond, MODULE_NAME)
-#define AERROR_IF(cond) ALOG_IF(ERROR, cond, MODULE_NAME)
-#define AFATAL_IF(cond) ALOG_IF(FATAL, cond, MODULE_NAME)
-#define ALOG_IF(severity, cond, module) \
-  !(cond) ? (void)0                     \
-          : google::LogMessageVoidify() & ALOG_MODULE(module, severity)
-    
-    
-    {
-    {
-    {}  // namespace camera
-}  // namespace perception
-}  // namespace apollo
-
-    
-     private:
-  Stage::StageStatus Process(const common::TrajectoryPoint& planning_init_point,
-                             Frame* frame) override;
-    
-    namespace apollo {
-namespace planning {
-namespace scenario {
-namespace valet_parking {
-    }
-    }
-    }
+        llvm::Expected<Type>
+SuperclassTypeRequest::evaluate(Evaluator &evaluator,
+                                NominalTypeDecl *nominalDecl,
+                                TypeResolutionStage stage) const {
+  assert(isa<ClassDecl>(nominalDecl) || isa<ProtocolDecl>(nominalDecl));
     }
     
-    ErrorCode GemController::Init(
-    const VehicleParameter& params,
-    CanSender<::apollo::canbus::ChassisDetail>* const can_sender,
-    MessageManager<::apollo::canbus::ChassisDetail>* const message_manager) {
-  if (is_initialized_) {
-    AINFO << 'GemController has already been initialized.';
-    return ErrorCode::CANBUS_ERROR;
+                if (elt->hasAssociatedValues() &&
+                dest->getArguments().size() == 1) {
+              SILType eltArgTy =
+                  enumTy.getEnumElementType(elt, clonedFunction.getModule());
+              SILType bbArgTy = dest->getArguments()[0]->getType();
+              if (eltArgTy != bbArgTy)
+                replaceBlockArgumentType(switchEnum->getLoc(), dest, eltArgTy);
+    }
+    
+    #include 'swift/AST/ASTContext.h'
+#include 'swift/AST/Attr.h'
+#include 'swift/AST/Decl.h'
+#include 'swift/AST/Types.h'
+#include 'swift/AST/Availability.h'
+#include 'swift/AST/PlatformKind.h'
+#include 'swift/AST/TypeWalker.h'
+#include <map>
+    
+      constexpr static KeyPathComponentHeader
+  forStructComponentWithInlineOffset(bool isLet,
+                                     unsigned offset) {
+    return KeyPathComponentHeader(
+      (_SwiftKeyPathComponentHeader_StructTag
+       << _SwiftKeyPathComponentHeader_DiscriminatorShift)
+      | validateInlineOffset(offset)
+      | isLetBit(isLet));
   }
-  vehicle_params_.CopyFrom(
-      common::VehicleConfigHelper::Instance()->GetConfig().vehicle_param());
-  params_.CopyFrom(params);
-  if (!params_.has_driving_mode()) {
-    AERROR << 'Vehicle conf pb not set driving_mode.';
-    return ErrorCode::CANBUS_ERROR;
+  
+  constexpr static KeyPathComponentHeader
+  forStructComponentWithOutOfLineOffset(bool isLet) {
+    return KeyPathComponentHeader(
+      (_SwiftKeyPathComponentHeader_StructTag
+       << _SwiftKeyPathComponentHeader_DiscriminatorShift)
+      | _SwiftKeyPathComponentHeader_OutOfLineOffsetPayload
+      | isLetBit(isLet));
   }
+    
+    protected:
+  /// Determine the total size to allocate.
+  static size_t totalSizeToAlloc(ArrayRef<Identifier> argLabels,
+                                 ArrayRef<SourceLoc> argLabelLocs,
+                                 bool hasTrailingClosure) {
+    return TrailingObjects::template totalSizeToAlloc<Identifier, SourceLoc>(
+        argLabels.size(), argLabelLocs.size());
+  }
+    
+      /// The path to which we should output a Make-style dependencies file.
+  /// It is valid whenever there are any inputs.
+  ///
+  /// Swift's compilation model means that Make-style dependencies aren't
+  /// well-suited to model fine-grained dependencies. See docs/Driver.md for
+  /// more information.
+  ///
+  /// \sa ReferenceDependenciesFilePath
+  std::string DependenciesFilePath;
+    
+      // Print statistics.
+  void print_scores(void) const;
+  void print_scores(int orientation_id) const;
+    
+    #include 'tesseractclass.h'
+#ifdef _OPENMP
+#include <omp.h>
+#endif  // _OPENMP
+    
+      // Returns the direction of the fitted line as a unit vector, using the
+  // least mean squared perpendicular distance. The line runs through the
+  // mean_point, i.e. a point p on the line is given by:
+  // p = mean_point() + lambda * vector_fit() for some real number lambda.
+  // Note that the result (0<=x<=1, -1<=y<=-1) is directionally ambiguous
+  // and may be negated without changing its meaning, since a line is only
+  // unique to a range of pi radians.
+  // Modernists prefer to think of this as an Eigenvalue problem, but
+  // Pearson had the simple solution in 1901.
+  //
+  // Note that this is equivalent to returning the Principal Component in PCA,
+  // or the eigenvector corresponding to the largest eigenvalue in the
+  // covariance matrix.
+  FCOORD vector_fit() const;
+    
+      // Transforms the given coords one step forward to normalized space, without
+  // using any block rotation or predecessor.
+  void LocalNormTransform(const TPOINT& pt, TPOINT* transformed) const;
+  void LocalNormTransform(const FCOORD& pt, FCOORD* transformed) const;
+  // Transforms the given coords forward to normalized space using the
+  // full transformation sequence defined by the block rotation, the
+  // predecessors, deepest first, and finally this. If first_norm is not nullptr,
+  // then the first and deepest transformation used is first_norm, ending
+  // with this, and the block rotation will not be applied.
+  void NormTransform(const DENORM* first_norm, const TPOINT& pt,
+                     TPOINT* transformed) const;
+  void NormTransform(const DENORM* first_norm, const FCOORD& pt,
+                     FCOORD* transformed) const;
+  // Transforms the given coords one step back to source space, without
+  // using to any block rotation or predecessor.
+  void LocalDenormTransform(const TPOINT& pt, TPOINT* original) const;
+  void LocalDenormTransform(const FCOORD& pt, FCOORD* original) const;
+  // Transforms the given coords all the way back to source image space using
+  // the full transformation sequence defined by this and its predecessors
+  // recursively, shallowest first, and finally any block re_rotation.
+  // If last_denorm is not nullptr, then the last transformation used will
+  // be last_denorm, and the block re_rotation will never be executed.
+  void DenormTransform(const DENORM* last_denorm, const TPOINT& pt,
+                       TPOINT* original) const;
+  void DenormTransform(const DENORM* last_denorm, const FCOORD& pt,
+                       FCOORD* original) const;
+    
+        void setrej_tess_failure();  //Tess generated blank
+    void setrej_small_xht();  //Small xht char/wd
+    void setrej_edge_char();  //Close to image edge
+    void setrej_1Il_conflict();  //Initial reject map
+    void setrej_postNN_1Il();  //1Il after NN
+    void setrej_rej_cblob();  //Insert duff blob
+    void setrej_mm_reject();  //Matrix matcher
+                                 //Odd repeated char
+    void setrej_bad_repetition();
+    void setrej_poor_match();  //Failed Rays heuristic
+                                 //TEMP reject_word
+    void setrej_not_tess_accepted();
+                                 //TEMP reject_word
+    void setrej_contains_blanks();
+    void setrej_bad_permuter();  //POTENTIAL reject_word
+    void setrej_hyphen();  //PostNN dubious hyph or .
+    void setrej_dubious();  //PostNN dubious limit
+    void setrej_no_alphanums();  //TEMP reject_word
+    void setrej_mostly_rej();  //TEMP reject_word
+    void setrej_xht_fixup();  //xht fixup
+    void setrej_bad_quality();  //TEMP reject_word
+    void setrej_doc_rej();  //TEMP reject_word
+    void setrej_block_rej();  //TEMP reject_word
+    void setrej_row_rej();  //TEMP reject_word
+    void setrej_unlv_rej();  //TEMP reject_word
+    void setrej_nn_accept();  //NN Flipped a char
+    void setrej_hyphen_accept();  //Good aspect ratio
+    void setrej_mm_accept();  //Matrix matcher
+                                 //Quality flip a char
+    void setrej_quality_accept();
+                                 //Accept all except blank
+    void setrej_minimal_rej_accept();
+    
+      // Removes the top element of the heap. If entry is not nullptr, the element
+  // is copied into *entry, otherwise it is discarded.
+  // Returns false if the heap was already empty.
+  // Time = O(log n).
+  bool Pop(Pair* entry) {
+    int new_size = heap_.size() - 1;
+    if (new_size < 0)
+      return false;  // Already empty.
+    if (entry != nullptr)
+      *entry = heap_[0];
+    if (new_size > 0) {
+      // Sift the hole at the start of the heap_ downwards to match the last
+      // element.
+      Pair hole_pair = heap_[new_size];
+      heap_.truncate(new_size);
+      int hole_index = SiftDown(0, hole_pair);
+      heap_[hole_index] = hole_pair;
+    } else {
+      heap_.truncate(new_size);
+    }
+    return true;
+  }
+    
+      // Main worker method that retrieves the next number in the sequence.
+  // Returns kInvalidVal if called more than N times after object initialization
+  int GetVal() {
+    const int kInvalidVal = -1;
+    const int kMaxNaturalNumberValue = 1 << num_bits_;
+    if (next_num_ >= kMaxNaturalNumberValue)
+      return kInvalidVal;
+    int n = next_num_;
     }
     
-    using apollo::common::Status;
-using apollo::common::time::Clock;
+    #include <vector>
+#include 'Ratpack/ratpak.h'
+    
+    /****************************************************************************\
+*
+* GroupDigits
+*
+* Description:
+*   This routine will take a grouping vector and the display string and
+*   add the separator according to the pattern indicated by the separator.
+*
+*   Grouping
+*   0,0      - no grouping
+*   3,0      - group every 3 digits
+*   3        - group 1st 3, then no grouping after
+*   3,0,0    - group 1st 3, then no grouping after
+*   3,2,0    - group 1st 3 and then every 2 digits
+*   4,0      - group every 4 digits
+*   5,3,2,0  - group 5, then 3, then every 2
+*   5,3,2    - group 5, then 3, then 2, then no grouping after
+*
+\***************************************************************************/
+wstring CCalcEngine::GroupDigits(wstring_view delimiter, vector<uint32_t> const& grouping, wstring_view displayString, bool isNumNegative)
+{
+    // if there's nothing to do, bail
+    if (delimiter.empty() || grouping.empty())
+    {
+        return wstring{ displayString };
+    }
+    }
+    
+        Rational operator-(Rational lhs, Rational const& rhs)
+    {
+        lhs -= rhs;
+        return lhs;
+    }
+    
+    inline constexpr auto IDS_ERRORS_FIRST = 99;
     
     
-    {  return true;
+    {    return (pnumret);
 }
     
-    #include <string>
+    template <class ElemType>
+shared_ptr<ComputationNode<ElemType>> ComputationNetworkBuilder<ElemType>::EditDistanceError(const ComputationNodePtr a, const ComputationNodePtr b, float subPen, float delPen, float insPen, bool squashInputs, vector<size_t> tokensToIgnore, const std::wstring nodeName)
+{
+    return net.AddNodeToNetAndAttachInputs(New<EditDistanceErrorNode<ElemType>>(net.GetDeviceId(), nodeName, subPen, delPen, insPen, squashInputs, tokensToIgnore), { a, b });
+}
     
-    /**
- * @brief Wrap angle to [0, 2 * PI).
- * @param angle the original value of the angle.
- * @return The wrapped value of the angle.
- */
-double WrapAngle(const double angle);
-    
-    
+    public:
+    // used as default argument to operator(id, default) to retrieve ConfigParameters
+    static const ConfigParameters& Record()
     {
-    {    frame->detected_objects.push_back(obj);
+        static ConfigParameters emptyParameters;
+        return emptyParameters;
+    }
+    // to retrieve an array, pass e.g. Array(floatargvector()) as the default value
+    template <class V>
+    static const V& Array(const V& vec)
+    {
+        return vec;
+    }
+    
+        for (auto& node : multNodes)
+    {
+        std::vector<ComputationNodeBasePtr> consumers = GetNodeConsumers(node);
+        if (consumers.size() == 1)
+        {
+            bool sigmoided = false;
+            std::wstring layerId(node->GetName());
+    }
+    }
+    
+        virtual ComputationNodeBasePtr Duplicate(const std::wstring& newName = L'', const CopyNodeFlags flags = CopyNodeFlags::copyNodeAll) const = 0;   // (called on here implemented by ComputationNode<ElemType>
+    
+    template<typename ElemType>
+template<template<typename> class ValueContainer>
+void CNTKEvalExtended<ElemType>::ForwardPassT(const std::vector<ValueBuffer<ElemType, ValueContainer> >& inputs, std::vector<ValueBuffer<ElemType, ValueContainer> >& outputs, bool resetRNN)
+{
+    if (!m_started)
+        RuntimeError('ForwardPass() called before StartForwardEvaluation()');
+    }
+    
+    
+    // Implementation of standard model averaging 
+    template<typename ElemType>
+    class BasicModelAveragingSGD : public IMASGD<ElemType>
+    {
+        typedef IMASGD<ElemType> Base; 
+        using Base::m_pMPI;
+        using Base::m_nccl;
+        using Base::DownCast;
+    }
+    
+    public:
+#define EPSILON 1e-5
+    
+        // Allocate the output values layer
+    Values<float> outputBuffer = outputLayouts.CreateBuffers<float>({ 3 });
+    
+    template <typename ElemType>
+void UpdateEpochEvaluationForAccumulatedResult(
+    std::vector<EpochCriterion>& epochEvalErrors,
+    const std::vector<ComputationNodeBasePtr>& evaluationNodes,
+    CriterionAccumulatorBase& localEpochEvalErrors,
+    std::function<bool(ComputationNodeBasePtr)> containsAccumulatedResult
+    )
+{
+    for (size_t i = 0; i < epochEvalErrors.size(); i++)
+    {
+        if (containsAccumulatedResult(evaluationNodes[i]))
+        {
+            // We don't accumulate error in epoch criterion as this node has already accumulated error for all
+            // samples that passed through network in forward pass.
+            // Since accumulators already average error, we use 1 as number of samples to avoid averaging again.
+            localEpochEvalErrors.Assign(i, 1);
+            epochEvalErrors[i] = localEpochEvalErrors.GetCriterion(i);
+        }
+    }
+}
+    
+        // Check for WSI support
+    VkBool32 res;
+    vkGetPhysicalDeviceSurfaceSupportKHR(g_PhysicalDevice, g_QueueFamily, wd->Surface, &res);
+    if (res != VK_TRUE)
+    {
+        fprintf(stderr, 'Error no WSI support on physical device 0\n');
+        exit(-1);
+    }
+    
+    void my_display_code()
+{
+    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
+    }
+    
+                ImGui::Begin('Hello, world!');                          // Create a window called 'Hello, world!' and append into it.
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+    // DirectX
+#include <d3d9.h>
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+    
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_IMPL_API void     ImGui_ImplDX10_InvalidateDeviceObjects();
+IMGUI_IMPL_API bool     ImGui_ImplDX10_CreateDeviceObjects();
+
+    
+    namespace rocksdb {
+namespace blob_db {
+    }
+    }
+    
+      Status NewDirectory(const std::string& name,
+                      std::unique_ptr<Directory>* result) override {
+    PERF_TIMER_GUARD(env_new_directory_nanos);
+    return EnvWrapper::NewDirectory(name, result);
   }
-  fclose(fp);
-  return true;
+    
+    Status WriteBatchBase::SingleDelete(ColumnFamilyHandle* column_family,
+                                    const SliceParts& key) {
+  std::string key_buf;
+  Slice key_slice(key, &key_buf);
+  return SingleDelete(column_family, key_slice);
+}
+    
+      static void CompactFiles(void* arg) {
+    std::unique_ptr<CompactionTask> task(
+        reinterpret_cast<CompactionTask*>(arg));
+    assert(task);
+    assert(task->db);
+    Status s = task->db->CompactFiles(
+        task->compact_options,
+        task->input_file_names,
+        task->output_level);
+    printf('CompactFiles() finished with status %s\n', s.ToString().c_str());
+    if (!s.ok() && !s.IsIOError() && task->retry_on_fail) {
+      // If a compaction task with its retry_on_fail=true failed,
+      // try to schedule another compaction in case the reason
+      // is not an IO error.
+      CompactionTask* new_task = task->compactor->PickCompaction(
+          task->db, task->column_family_name);
+      task->compactor->ScheduleCompaction(new_task);
+    }
+  }
+    
+    namespace {
+// A dummy compaction filter
+class DummyCompactionFilter : public CompactionFilter {
+ public:
+  virtual ~DummyCompactionFilter() {}
+  virtual bool Filter(int level, const Slice& key, const Slice& existing_value,
+                      std::string* new_value, bool* value_changed) const {
+    return false;
+  }
+  virtual const char* Name() const { return 'DummyCompactionFilter'; }
+};
+    }
+    
+      {
+    PinnableSlice pinnable_val;
+    db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key2', &pinnable_val);
+    assert(pinnable_val == 'value');
+  }
+    
+    
+    {// Factor method to create a new persistent cache
+Status NewPersistentCache(Env* const env, const std::string& path,
+                          const uint64_t size,
+                          const std::shared_ptr<Logger>& log,
+                          const bool optimized_for_nvm,
+                          std::shared_ptr<PersistentCache>* cache);
+}  // namespace rocksdb
+
+    
+      // If non-NULL, use the specified cache for blocks.
+  // If NULL, leveldb will automatically create and use an 8MB internal cache.
+  // Default: NULL
+  Cache* block_cache;
+    
+    #pragma once
+// lua headers
+extern 'C' {
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 }
