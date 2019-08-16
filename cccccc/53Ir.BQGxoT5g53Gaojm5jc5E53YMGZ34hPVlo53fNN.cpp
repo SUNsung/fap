349f755d5f53e03577287f74ad5c8360948210ce
SUNsung <weatherius@gmail.com>
2019-08-17 01:49:20 +0300

    Rewrite [53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp] File in: 2019-08-17 01:49:19

--- cccccc/53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp
+++ cccccc/53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp
@@ -1,188 +1,344 @@
 
         
-          bool has_leaders;   // does the line contain leader dots (.....)?
-  bool has_drop_cap;  // does the line have a drop cap?
-  int pix_ldistance;  // distance to the left pblock boundary in pixels
-  int pix_rdistance;  // distance to the right pblock boundary in pixels
-  float pix_xheight;  // guessed xheight for the line
-  int average_interword_space; // average space between words in pixels.
+        Promise::~Promise() {}
     
-      // Sets the destination filename and enables images to be written to a PDF
-  // on destruction.
-  void WritePDF(const char* filename) {
-    if (pixaGetCount(pixa_) > 0) {
-      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, 'AllDebugImages', filename);
-      pixaClear(pixa_);
-    }
-  }
+    #include 'base/macros.h'
+#include 'native_mate/converter.h'
     
-    /*!
- * Copyright (c) 2016 by Contributors
- * \file caffe_fieldentry.h
- * \brief Implement FieldEntry<caffe::LayerParameter>
- * \author Haoran Wang
- */
-#ifndef PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
-#define PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
-    
-     private:
-  /*! \brief base iterator */
-  std::unique_ptr<IIterator<DataInst> > base_;
-  /*! whether mean image is ready */
-  bool meanfile_ready_;
-  /*! \brief output data */
-  DataInst out_;
-  // normalize parameter.
-  ImageNormalizeParam param_;
-  /*! \brief mean image, if needed */
-  mshadow::TensorContainer<cpu, 3> meanimg_;
-  /*! \brief temp space for output image */
-  mshadow::TensorContainer<cpu, 3> outimg_;
-  /*! \brief random numeber engine */
-  common::RANDOM_ENGINE rnd_;
-  // random magic number of this iterator
-  static const int kRandMagic = 0;
-    
-      ~PrefetcherIter() {
-    while (recycle_queue_.size() != 0) {
-      DataBatch *batch = recycle_queue_.front();
-      recycle_queue_.pop();
-      delete batch;
-    }
-    delete out_;
-    iter.Destroy();
-  }
-    
-    #ifndef MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
-#define MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
-#include <dmlc/parameter.h>
-#include <string>
-#include <utility>
-#include <vector>
-#include 'mxnet/ndarray.h'
-    
-    
-    {  if (has('dist')) {
-#if MXNET_USE_DIST_KVSTORE
-    kv = new kvstore::KVStoreDist(use_device_comm);
-    if (!has('_async') && kv->IsWorkerNode() && kv->get_rank() == 0) {
-      // configure the server to be the sync mode
-      kv->SendCommandToServers(static_cast<int>(kvstore::CommandType::kSyncMode), '');
-    }
-#else
-    LOG(FATAL) << 'compile with USE_DIST_KVSTORE=1 to use ' << tname;
-    return nullptr;
-#endif  // MXNET_USE_DIST_KVSTORE
-  } else {
-    if (has('nccl')) {
-#if MXNET_USE_NCCL
-      kv = new kvstore::KVStoreNCCL();
-#else
-      LOG(FATAL) << 'compile with USE_NCCL=1 to use ' << tname;
-      return nullptr;
-#endif
+      void reset(v8::Isolate* isolate, v8::Local<T> handle) {
+    if (!handle.IsEmpty()) {
+      isolate_ = isolate;
+      handle_.Reset(isolate, handle);
     } else {
-      kv =  new kvstore::KVStoreLocal(use_device_comm);
+      reset();
     }
   }
-  kv->type_ = tname;
-  return kv;
-}
     
+    #ifndef NATIVE_MATE_NATIVE_MATE_WRAPPABLE_H_
+#define NATIVE_MATE_NATIVE_MATE_WRAPPABLE_H_
     
-    {
-    {.add_argument('data', 'Symbol or Symbol[]', 'Tensor or List of Tensors, the second input '
-'will be used as crop_like shape reference')
-.add_arguments(CropParam::__FIELDS__())
-.set_key_var_num_args('num_args');
-}  // namespace op
-}  // namespace mxnet
-
-    
-    
-    {
-    {
-    {  bool init_cudnn_;
-  cudnnDataType_t dtype_;
-  cudnnSpatialTransformerDescriptor_t st_desc_;
-  cudnnTensorDescriptor_t in_desc_;
-  cudnnTensorDescriptor_t out_desc_;
-  cudnnSamplerType_t sampler_;
-  #if CUDNN_MAJOR >= 5
-  cudnnTensorFormat_t format_;
-  #endif
-  BilinearSamplerParam param_;
-};
-#endif  // __CUDACC__ && CUDNN
-}  // namespace op
-}  // namespace mxnet
-    
-    namespace mxnet {
-namespace op {
-template<typename DType>
-class CuDNNLocalResponseNormOp : public Operator {
- public:
-  explicit CuDNNLocalResponseNormOp(LRNParam param) {
-    param_ = param;
-    init_cudnn_ = false;
-    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
-  }
-    }
-    }
+    napi_value Print(napi_env env, napi_callback_info info) {
+  size_t argc = 1;
+  napi_value args[1];
+  napi_status status;
     }
     
-    DMLC_REGISTER_PARAMETER(NativeOpParam);
+    #ifndef SHELL_APP_COMMAND_LINE_ARGS_H_
+#define SHELL_APP_COMMAND_LINE_ARGS_H_
     
-    ScientificNumberFormatter *ScientificNumberFormatter::createInstance(
-            DecimalFormat *fmtToAdopt,
-            Style *styleToAdopt,
-            UErrorCode &status) {
-    LocalPointer<DecimalFormat> fmt(fmtToAdopt);
-    LocalPointer<Style> style(styleToAdopt);
-    if (U_FAILURE(status)) {
-        return NULL;
+        // could not link videoconvert0 to matroskamux0, matroskamux0 can't handle caps video/x-raw, format=(string)RGBA
+    std::string pipeline = 'appsrc ! videoconvert ! video/x-raw, format=(string)RGBA ! matroskamux ! filesink location=test.mkv';
+    Size frame_size(640, 480);
+    
+                int32x4_t v1L = vreinterpretq_s32_u32(vshll_n_u16(vget_low_u16(v1hw),  SHIFT_BITS));
+            int32x4_t v1H = vreinterpretq_s32_u32(vshll_n_u16(vget_high_u16(v1hw), SHIFT_BITS));
+            int32x4_t v2L = vreinterpretq_s32_u32(vshll_n_u16(vget_low_u16(v2hw),  SHIFT_BITS));
+            int32x4_t v2H = vreinterpretq_s32_u32(vshll_n_u16(vget_high_u16(v2hw), SHIFT_BITS));
+    
+    
+    {        printf('[----------]\n[ GPU INFO ] \tRun test suite on %s GPU.\n[----------]\n', info.name()), fflush(stdout);
     }
-    ScientificNumberFormatter *result =
-            new ScientificNumberFormatter(
-                    fmt.getAlias(),
-                    style.getAlias(),
-                    status);
-    if (result == NULL) {
-        status = U_MEMORY_ALLOCATION_ERROR;
-        return NULL;
-    }
-    fmt.orphan();
-    style.orphan();
-    if (U_FAILURE(status)) {
-        delete result;
-        return NULL;
-    }
-    return result;
-}
-    
-    #endif /* #if !UCONFIG_NO_BREAK_ITERATION */
-
-    
-    U_NAMESPACE_BEGIN
-    
-    #endif
-
-    
-        // Compare the date to the starting and ending rules.+1 = date>rule, -1
-    // = date<rule, 0 = date==rule.
-    int32_t startCompare = compareToRule((int8_t)month, (int8_t)monthLength, (int8_t)prevMonthLength,
-                                         (int8_t)day, (int8_t)dayOfWeek, millis,
-                                         startTimeMode == UTC_TIME ? -rawOffset : 0,
-                                         startMode, (int8_t)startMonth, (int8_t)startDayOfWeek,
-                                         (int8_t)startDay, startTime);
-    int32_t endCompare = 0;
-    
-    
-    {    return result;
-}
 #endif
     
-        /**
-     * ICU 'poor man's RTTI', returns a UClassID for this class.
-     */
-    static UClassID U_EXPORT2 getStaticClassID();
+        bool writemulti(const std::vector<Mat>& img_vec, const std::vector<int>& params) CV_OVERRIDE;
+    
+    void icvCvt_Gray2BGR_8u_C1C3R( const uchar* gray, int gray_step,
+                               uchar* bgr, int bgr_step, Size size );
+void icvCvt_Gray2BGR_16u_C1C3R( const ushort* gray, int gray_step,
+                               ushort* bgr, int bgr_step, Size size );
+    
+    //! Supported logging levels and their semantic
+enum LogLevel {
+    LOG_LEVEL_SILENT = 0,              //!< for using in setLogVevel() call
+    LOG_LEVEL_FATAL = 1,               //!< Fatal (critical) error (unrecoverable internal error)
+    LOG_LEVEL_ERROR = 2,               //!< Error message
+    LOG_LEVEL_WARNING = 3,             //!< Warning message
+    LOG_LEVEL_INFO = 4,                //!< Info message
+    LOG_LEVEL_DEBUG = 5,               //!< Debug message. Disabled in the 'Release' build.
+    LOG_LEVEL_VERBOSE = 6,             //!< Verbose (trace) messages. Requires verbosity level. Disabled in the 'Release' build.
+#ifndef CV_DOXYGEN
+    ENUM_LOG_LEVEL_FORCE_INT = INT_MAX
+#endif
+};
+    
+            // Specifies whether cross references (full names that match the name part) should be computed.
+        bool m_findCrossReferences;
+    
+    
+    {	return file_opened;
+}
+    
+    #endif // ALSAMIDI_ENABLED
+
+    
+    PoolStringArray MIDIDriverWinMidi::get_connected_inputs() {
+    }
+    
+    godot_real GDAPI godot_pool_real_array_get(const godot_pool_real_array *p_self, const godot_int p_idx) {
+	const PoolVector<godot_real> *self = (const PoolVector<godot_real> *)p_self;
+	return self->get(p_idx);
+}
+    
+    godot_bool GDAPI godot_string_name_operator_less(const godot_string_name *p_self, const godot_string_name *p_other) {
+	const StringName *self = (const StringName *)p_self;
+	const StringName *other = (const StringName *)p_other;
+	return self < other;
+}
+    
+    #include 'core/os/semaphore.h'
+    
+    namespace leveldb {
+    }
+    
+    
+    {}  // namespace leveldb
+
+    
+      // Pad with zeroes to make manifest file very big.
+  {
+    uint64_t len = FileSize(old_manifest);
+    WritableFile* file;
+    ASSERT_OK(env()->NewAppendableFile(old_manifest, &file));
+    std::string zeroes(3 * 1048576 - static_cast<size_t>(len), 0);
+    ASSERT_OK(file->Append(zeroes));
+    ASSERT_OK(file->Flush());
+    delete file;
+  }
+    
+    
+    {#if !defined(NDEBUG)
+  SnapshotList* list_ = nullptr;
+#endif  // !defined(NDEBUG)
+};
+    
+      static size_t ByteSize(const WriteBatch* batch) { return batch->rep_.size(); }
+    
+      // Insert a mapping from key->value into the cache and assign it
+  // the specified charge against the total cache capacity.
+  //
+  // Returns a handle that corresponds to the mapping.  The caller
+  // must call this->Release(handle) when the returned mapping is no
+  // longer needed.
+  //
+  // When the inserted entry is no longer needed, the key and
+  // value will be passed to 'deleter'.
+  virtual Handle* Insert(const Slice& key, void* value, size_t charge,
+                         void (*deleter)(const Slice& key, void* value)) = 0;
+    
+    
+    {}  // namespace leveldb
+    
+      RandomAccessFile(const RandomAccessFile&) = delete;
+  RandomAccessFile& operator=(const RandomAccessFile&) = delete;
+    
+    inline int Slice::compare(const Slice& b) const {
+  const size_t min_len = (size_ < b.size_) ? size_ : b.size_;
+  int r = memcmp(data_, b.data_, min_len);
+  if (r == 0) {
+    if (size_ < b.size_)
+      r = -1;
+    else if (size_ > b.size_)
+      r = +1;
+  }
+  return r;
+}
+    
+    class Issue200 {};
+    
+    #include <cassert>
+#include <condition_variable>  // NOLINT
+#include <cstddef>
+#include <cstdint>
+#include <mutex>  // NOLINT
+#include <string>
+    
+      // Read the block contents as well as the type/crc footer.
+  // See table_builder.cc for the code that built this structure.
+  size_t n = static_cast<size_t>(handle.size());
+  char* buf = new char[n + kBlockTrailerSize];
+  Slice contents;
+  Status s = file->Read(handle.offset(), n + kBlockTrailerSize, &contents, buf);
+  if (!s.ok()) {
+    delete[] buf;
+    return s;
+  }
+  if (contents.size() != n + kBlockTrailerSize) {
+    delete[] buf;
+    return Status::Corruption('truncated block read');
+  }
+    
+    TEST(Coding, Varint32Truncation) {
+  uint32_t large_value = (1u << 31) + 100;
+  std::string s;
+  PutVarint32(&s, large_value);
+  uint32_t result;
+  for (size_t len = 0; len < s.size() - 1; len++) {
+    ASSERT_TRUE(GetVarint32Ptr(s.data(), s.data() + len, &result) == nullptr);
+  }
+  ASSERT_TRUE(GetVarint32Ptr(s.data(), s.data() + s.size(), &result) !=
+              nullptr);
+  ASSERT_EQ(large_value, result);
+}
+    
+            gldPrevious.fIntMath = m_fIntegerMode;
+        gldPrevious.bRecord = m_bRecord;
+        gldPrevious.bUseSep = true;
+    
+    
+    {    return m_client->GetStringAsync(metadataUri);
+}
+    
+        Rational operator<<(Rational lhs, Rational const& rhs)
+    {
+        lhs <<= rhs;
+        return lhs;
+    }
+    
+        int32_t const& Number::Exp() const
+    {
+        return m_exp;
+    }
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+        // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+    
+                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+    
+    // Include OpenGL header (without an OpenGL loader) requires a bit of fiddling
+#if defined(_WIN32) && !defined(APIENTRY)
+#define APIENTRY __stdcall                  // It is customary to use APIENTRY for OpenGL function pointer declarations on all platforms.  Additionally, the Windows OpenGL header needs APIENTRY.
+#endif
+#if defined(_WIN32) && !defined(WINGDIAPI)
+#define WINGDIAPI __declspec(dllimport)     // Some Windows OpenGL headers need this
+#endif
+#if defined(__APPLE__)
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+    
+      static Data& getMutableData6() { return data6_; }
+    
+    
+    {} // namespace aria2
+
+    
+      // number of nodes
+  READ_CHECK(fp, &temp32, sizeof(temp32));
+  uint32_t numNodes = ntohl(temp32);
+  // 4bytes reserved
+  readBytes(fp, buf, buf.size(), 4);
+    
+    
+    {} // namespace aria2
+    
+    DHTTaskExecutor::DHTTaskExecutor(int numConcurrent)
+    : numConcurrent_(numConcurrent)
+{
+}
+    
+    DHTTaskFactoryImpl::~DHTTaskFactoryImpl() = default;
+    
+    
+    {} // namespace aria2
+
+    
+    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
+                                             std::chrono::seconds interval)
+    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
+{
+}
+    
+    std::string DHTUnknownMessage::toString() const
+{
+  size_t sampleLength = 8;
+  if (length_ < sampleLength) {
+    sampleLength = length_;
+  }
+  return fmt('dht unknown Remote:%s(%u) length=%lu, first 8 bytes(hex)=%s',
+             ipaddr_.c_str(), port_, static_cast<unsigned long>(length_),
+             util::toHex(data_, sampleLength).c_str());
+}
+    
+      const std::string& find(const std::string& hostname, uint16_t port) const;
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    int64_t OutputFile::writeImpl(const char *buffer, int64_t length) {
+  assertx(length > 0);
+  if (isClosed()) return 0;
+  g_context->write(buffer, length);
+  return length;
+}
+    
+    ZipFile::~ZipFile() {
+  ZipFile::closeImpl();
+}
+    
+    
+    {private:
+  struct SavedAnnotation {
+    std::string   fileName;
+    uint64_t      offset;
+    uint32_t      length;
+  };
+  SavedAnnotation writeAnnotation(const Annotation& annotation,
+                                  bool compress = true);
+};
+    
+    /// no discovery queries, no platform restriction, fake version string
+JSON getPackWithFakeVersion() {
+  auto doc = getExamplePacksConfig();
+  return JSON::newFromValue(doc.doc()['packs']['fake_version_pack']);
+}
+    
+    Expected<int32_t, DatabaseError> Database::getInt32(const std::string& domain,
+                                                    const std::string& key) {
+  Expected<std::string, DatabaseError> string_value = getString(domain, key);
+  if (string_value) {
+    auto value = tryTo<int32_t>(*string_value);
+    if (value) {
+      return *value;
+    } else {
+      return createError(DatabaseError::FailToReadData, value.takeError())
+             << 'Failed to convert string to int';
+    }
+  } else {
+    return string_value.takeError();
+  }
+}
+    
+      assert(column_families.size() == raw_handles.size() &&
+         'can't map column families to handles');
+  if (column_families.size() != raw_handles.size()) {
+    return createError(
+        DatabaseError::FailToOpenDatabase,
+        'Fail to open database: can't map column families to handles');
+  }
+    
+      // Determine max indent needed for all flag names.
+  size_t max = 0;
+  for (const auto& flag : details) {
+    max = (max > flag.first.size()) ? max : flag.first.size();
+  }
+  // Additional index for flag values.
+  max += 6;
+    
+      /**
+   * @brief Attach a table at runtime.
+   *
+   * The SQL implementation plugin may need to manage how virtual tables are
+   * attached at run time. In the case of SQLite where a single DB object is
+   * managed, tables are enumerated and attached during initialization.
+   */
+  virtual Status attach(const std::string& /*name*/) {
+    return Status::success();
+  }
