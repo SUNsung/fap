4625536016930ca2109bd80641088560e3ca1bd9
SUNsung <weatherius@gmail.com>
2019-08-16 21:29:10 +0300

    Rewrite [23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp] File in: 2019-08-16 21:29:09

--- cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
+++ cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
@@ -1,123 +1,178 @@
 
         
-        /**
- * Changelog:
- * - March 2013, Diederik Huys:    original version
- * - November 2014, Pieter Wuille: updated to use Peter Dettman's parallel multiplication algorithm
- * - December 2014, Pieter Wuille: converted from YASM to GCC inline assembly
- */
+          // Window XIDs which want to be registered, but haven't yet been because
+  // we're waiting for the proxy to become available.
+  std::set<unsigned long> live_xids_;
     
-        secp256k1_context_set_error_callback(none, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_error_callback(sign, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_error_callback(vrfy, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_error_callback(both, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_illegal_callback(none, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_illegal_callback(sign, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_illegal_callback(vrfy, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_illegal_callback(both, counting_illegal_callback_fn, &ecount);
+    // static
+void CallbackHolderBase::SecondWeakCallback(
+    const v8::WeakCallbackInfo<CallbackHolderBase>& data) {
+  delete data.GetParameter();
+}
     
-    #endif // BITCOIN_BECH32_H
-
-    
-      SequentialFile* orig_file;
-  Status s = env->NewSequentialFile(filename, &orig_file);
-  if (!s.ok())
-    return s;
-    
-    
-    {	ClassDB::bind_method('_add_task', &BackgroundProgress::_add_task);
-	ClassDB::bind_method('_task_step', &BackgroundProgress::_task_step);
-	ClassDB::bind_method('_end_task', &BackgroundProgress::_end_task);
-	ClassDB::bind_method('_update', &BackgroundProgress::_update);
+    bool Converter<PersistentDictionary>::FromV8(v8::Isolate* isolate,
+                                             v8::Local<v8::Value> val,
+                                             PersistentDictionary* out) {
+  if (!val->IsObject())
+    return false;
+  *out = PersistentDictionary(isolate, v8::Local<v8::Object>::Cast(val));
+  return true;
 }
     
     
-    {	_update_mirroring();
-}
-    
-    	DebugDraw debug_draw;
-    
-    	static Mutex *lock;
-	static void setup();
-	static void cleanup();
-	static bool configured;
-    
-    	CFStringRef name = CFStringCreateWithCString(NULL, 'Godot', kCFStringEncodingASCII);
-	OSStatus result = MIDIClientCreate(name, NULL, NULL, &client);
-	CFRelease(name);
-	if (result != noErr) {
-		ERR_PRINTS('MIDIClientCreate failed, code: ' + itos(result));
-		return ERR_CANT_OPEN;
-	}
-    
-    #include 'core/os/midi_driver.h'
-#include 'core/vector.h'
-    
-    		Vector<int> points;
-    
-    godot_string GDAPI godot_pool_string_array_get(const godot_pool_string_array *p_self, const godot_int p_idx) {
-	const PoolVector<String> *self = (const PoolVector<String> *)p_self;
-	godot_string str;
-	String *s = (String *)&str;
-	memnew_placement(s, String);
-	*s = self->get(p_idx);
-	return str;
-}
-    
-    godot_bool GDAPI godot_string_name_operator_equal(const godot_string_name *p_self, const godot_string_name *p_other) {
-	const StringName *self = (const StringName *)p_self;
-	const StringName *other = (const StringName *)p_other;
-	return self == other;
-}
-    
-    SparsePageWriter::~SparsePageWriter() {
-  for (auto& queue : qworkers_) {
-    // use nullptr to signal termination.
-    std::shared_ptr<SparsePage> sig(nullptr);
-    queue.Push(std::move(sig));
-  }
-  for (auto& thread : workers_) {
-    thread->join();
-  }
-}
-    
-      str = tree.DumpModel(fmap, true, 'json');
-  ASSERT_NE(str.find(R'('split': 'feat_0')'), std::string::npos);
-  ASSERT_NE(str.find(R'('split': 'feat_1')'), std::string::npos);
-  ASSERT_NE(str.find(R'('split': 'feat_2')'), std::string::npos);
-    
-      Transform<>::Init(TestTransformRange<bst_float>{},
-	                Range{0, static_cast<Range::DifferenceType>(size)},
-	                TRANSFORM_GPU_RANGE)
-      .Eval(&out_vec, &in_vec);
-  std::vector<bst_float> res = out_vec.HostVector();
-    
-    // GPU version is not uploaded in CRAN anyway.
-// Specialize only when using R with CPU.
-#if XGBOOST_STRICT_R_MODE && !defined(XGBOOST_USE_CUDA)
-double LogGamma(double v);
-    
-    
-    {  bool operator==(const TreeParam& b) const {
-    return num_roots == b.num_roots && num_nodes == b.num_nodes &&
-           num_deleted == b.num_deleted && max_depth == b.max_depth &&
-           num_feature == b.num_feature &&
-           size_leaf_vector == b.size_leaf_vector;
+    {  ArgumentHolder(Arguments* args, int create_flags) : ok(false) {
+    if (index == 0 && (create_flags & HolderIsFirstArgument) &&
+        Destroyable::IsDestroyed(args)) {
+      args->ThrowError('Object has been destroyed');
+      return;
+    }
+    ok = GetNextArgument(args, create_flags, index == 0, &value);
+    if (!ok) {
+      // Ideally we would include the expected c++ type in the error
+      // message which we can access via typeid(ArgType).name()
+      // however we compile with no-rtti, which disables typeid.
+      args->ThrowError();
+    }
   }
 };
     
-    #endif // OPENPOSE_EXAMPLES_TUTORIAL_USER_POST_PROCESSING_HPP
-
+     private:
+  static gin::WrapperInfo kWrapperInfo;
     
-            Point<T> center() const;
+      // Path in file system to the cookie file.
+  base::FilePath cookie_path_;
     
-        private:
-        // PIMPL idiom
-        // http://www.cppsamples.com/common-tasks/pimpl.html
-        struct ImplGuiAdam;
-        std::shared_ptr<ImplGuiAdam> spImpl;
+    // A helper class that releases the given |mutex| while the AutoUnlockMutex is
+// in scope and immediately re-acquires it when going out of scope.
+class AutoUnlockMutex {
+ public:
+  explicit AutoUnlockMutex(HANDLE mutex) : mutex_(mutex) {
+    BOOL released = ::ReleaseMutex(mutex_);
+    DPCHECK(released);
+  }
+    }
     
-        public:
-        PersonTracker(const bool mergeResults, const int levels = 3, const int patchSize = 31,
-                      const float confidenceThreshold = 0.05f, const bool trackVelocity = false,
-                      const bool scaleVarying = false, const float rescale = 640);
+      std::map<uv_timer_t*, base::OnceClosure> tasks_;
+    
+    int UniValue::get_int() const
+{
+    if (typ != VNUM)
+        throw std::runtime_error('JSON value is not an integer as expected');
+    int32_t retval;
+    if (!ParseInt32(getValStr(), &retval))
+        throw std::runtime_error('JSON integer out of range');
+    return retval;
+}
+    
+    %define XTMP0 xmm0
+%define XTMP1 xmm1
+%define XTMP2 xmm2
+%define XTMP3 xmm3
+%define XTMP4 xmm8
+%define XFER  xmm9
+    
+    namespace leveldb {
+    }
+    
+    namespace leveldb {
+    }
+    
+        do {
+      rocksdb::SyncPoint::GetInstance()->ClearTrace();
+      rocksdb::SyncPoint::GetInstance()->DisableProcessing();
+      Options options = OptionsForLogIterTest();
+      DestroyAndReopen(options);
+      Put('key1', DummyString(1024));
+      dbfull()->Flush(FlushOptions());
+      Put('key2', DummyString(1024));
+      dbfull()->Flush(FlushOptions());
+      Put('key3', DummyString(1024));
+      dbfull()->Flush(FlushOptions());
+      Put('key4', DummyString(1024));
+      ASSERT_EQ(dbfull()->GetLatestSequenceNumber(), 4U);
+      dbfull()->FlushWAL(false);
+    }
+    
+      ~BlobIndexCompactionFilter() override {
+    RecordTick(statistics_, BLOB_DB_BLOB_INDEX_EXPIRED_COUNT, expired_count_);
+    RecordTick(statistics_, BLOB_DB_BLOB_INDEX_EXPIRED_SIZE, expired_size_);
+    RecordTick(statistics_, BLOB_DB_BLOB_INDEX_EVICTED_COUNT, evicted_count_);
+    RecordTick(statistics_, BLOB_DB_BLOB_INDEX_EVICTED_SIZE, evicted_size_);
+  }
+    
+    #include 'rocksdb/merge_operator.h'
+#include 'rocksdb/slice.h'
+#include 'utilities/merge_operators.h'
+    
+      // When flush happens, it determines whether to trigger compaction. If
+  // triggered_writes_stop is true, it will also set the retry flag of
+  // compaction-task to true.
+  void OnFlushCompleted(
+      DB* db, const FlushJobInfo& info) override {
+    CompactionTask* task = PickCompaction(db, info.cf_name);
+    if (task != nullptr) {
+      if (info.triggered_writes_stop) {
+        task->retry_on_fail = true;
+      }
+      // Schedule compaction in a different thread.
+      ScheduleCompaction(task);
+    }
+  }
+    
+      // reopen the db using the loaded options.
+  std::vector<ColumnFamilyHandle*> handles;
+  s = DB::Open(loaded_db_opt, kDBPath, loaded_cf_descs, &handles, &db);
+  assert(s.ok());
+    
+    class DB;
+    
+      /*
+   * @brief a helper variable for keeping track of the compressed tar.
+   *
+   * This variable is the absolute location of the tar archive created from
+   * zstd of the archive.
+   */
+  boost::filesystem::path compressPath_;
+    
+      // Iterate the queries (or schedule) and check platform/version/sanity.
+  for (const auto& q : obj['queries'].GetObject()) {
+    if (!q.value.IsObject()) {
+      VLOG(1) << 'The pack ' << name << ' must contain a dictionary of queries';
+      continue;
+    }
+    }
+    
+      // This funcion should completely destroy db, so after next open
+  // db should be fresh new
+  // Implementation can expect that db is closed before
+  // calling destroyDB and should crash/fail in case when db is still open
+  virtual ExpectedSuccess<DatabaseError> destroyDB() = 0;
+    
+    ExpectedSuccess<DatabaseError> InMemoryDatabase::putStringsUnsafe(
+    const std::string& domain,
+    const std::vector<std::pair<std::string, std::string>>& data) {
+  debug_only::verifyTrue(is_open_, 'database is not open');
+  auto storage_iter = storage_.find(domain);
+  if (storage_iter == storage_.end()) {
+    return domainNotFoundError(domain);
+  }
+  std::lock_guard<std::mutex> lock(storage_iter->second->getMutex());
+  for (const auto& pair : data) {
+    storage_iter->second->put(pair.first, pair.second);
+  }
+  return Success();
+}
+    
+    ExpectedSuccess<DatabaseError> RocksdbDatabase::putString(
+    const std::string& domain,
+    const std::string& key,
+    const std::string& value) {
+  auto result = putRawBytes(domain, key, value);
+  if (result) {
+    return Success();
+  }
+  return result.takeError();
+}
+    
+    #include <osquery/core/database/database.h>
+#include <osquery/database/plugins/rocksdb.h>
