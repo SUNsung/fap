b198d006488c21c154235eaf11d3532dd8414166
SUNsung <weatherius@gmail.com>
2019-08-17 02:38:28 +0300

    Rewrite [15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb] File in: 2019-08-17 02:38:27

--- ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
+++ ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
@@ -1,106 +1,201 @@
 
         
-        describe ScenarioHelper do
-  let(:scenario) { users(:bob).scenarios.build(name: 'Scene', tag_fg_color: '#AAAAAA', tag_bg_color: '#000000') }
-    
-      describe '#values_at' do
-    it 'returns arrays of matching values' do
-      expect(Utils.values_at({ :foo => { :bar => :baz }}, 'foo.bar')).to eq(%w[baz])
-      expect(Utils.values_at({ :foo => [ { :bar => :baz }, { :bar => :bing } ]}, 'foo[*].bar')).to eq(%w[baz bing])
-      expect(Utils.values_at({ :foo => [ { :bar => :baz }, { :bar => :bing } ]}, 'foo[*].bar')).to eq(%w[baz bing])
-    end
-    
-            a_split.each_with_index { |s, i| a_split[i] = s.to_i unless i == a_length - 1 }
-        b_split.each_with_index { |s, i| b_split[i] = s.to_i unless i == b_length - 1 }
-    
-        def to_a
-      @filters.dup
-    end
-    
-        def request_all(urls, &block)
-      if options[:rate_limit]
-        if @@rate_limiter
-          @@rate_limiter.limit = options[:rate_limit]
-        else
-          @@rate_limiter = RateLimiter.new(options[:rate_limit])
-          Typhoeus.before(&@@rate_limiter.to_proc)
-        end
-      end
-    
-            css('pre[name]').each do |node|
-          node.before(%(<div class='pre-title'>#{node['name']}</div>))
+        # Searches and reads file present on GitLab installation directory
+module Gitlab
+  module Template
+    module Finders
+      class GlobalTemplateFinder < BaseTemplateFinder
+        def initialize(base_dir, extension, categories = {})
+          @categories = categories
+          @extension  = extension
+          super(base_dir)
         end
     
-          before do
-        subject.perform
+        # Ensure the file is removed
+    project.bfg_object_map.remove!
+    notification_service.repository_cleanup_failure(project, user, error)
+  end
+    
+        context 'when user cannot update_project_member' do
+      before do
+        allow(presenter).to receive(:can?).with(user, :update_project_member, presenter).and_return(false)
+        allow(presenter).to receive(:can?).with(user, :override_project_member, presenter).and_return(false)
       end
     
-      private
+      def test_display_eh_ignore
+    assert @co.display?
     
-      shared_examples 'localized subject' do |*args, **kwrest|
-    it 'renders subject localized for the locale of the receiver' do
-      locale = %i(de en).sample
-      receiver.update!(locale: locale)
-      expect(mail.subject).to eq I18n.t(*args, kwrest.merge(locale: locale))
+        const = RDoc::Constant.new 'CONST', nil, nil
+    top_level.add_constant const
+    
     end
     
-      # Define the way to render check boxes / radio buttons with labels.
-  # Defaults to :nested for bootstrap config.
-  #   inline: input + label
-  #   nested: label > input
-  config.boolean_style = :nested
+      def test_completion_journey
+    @line_editor.completion_proc = proc { |word|
+      %w{
+        foo_bar
+        foo_bar_baz
+      }
+    }
+    input_keys('foo')
+    assert_byte_pointer_size('foo')
+    assert_cursor(3)
+    assert_cursor_max(3)
+    assert_line('foo')
+    input_keys('\C-n')
+    assert_byte_pointer_size('foo')
+    assert_cursor(3)
+    assert_cursor_max(3)
+    assert_line('foo')
+    input_keys('\C-n')
+    assert_byte_pointer_size('foo_bar')
+    assert_cursor(7)
+    assert_cursor_max(7)
+    assert_line('foo_bar')
+    input_keys('\C-n')
+    assert_byte_pointer_size('foo_bar_baz')
+    assert_cursor(11)
+    assert_cursor_max(11)
+    assert_line('foo_bar_baz')
+    input_keys('\C-n')
+    assert_byte_pointer_size('foo')
+    assert_cursor(3)
+    assert_cursor_max(3)
+    assert_line('foo')
+    input_keys('\C-n')
+    assert_byte_pointer_size('foo_bar')
+    assert_cursor(7)
+    assert_cursor_max(7)
+    assert_line('foo_bar')
+    input_keys('_\C-n')
+    assert_byte_pointer_size('foo_bar_')
+    assert_cursor(8)
+    assert_cursor_max(8)
+    assert_line('foo_bar_')
+    input_keys('\C-n')
+    assert_byte_pointer_size('foo_bar_baz')
+    assert_cursor(11)
+    assert_cursor_max(11)
+    assert_line('foo_bar_baz')
+    input_keys('\C-n')
+    assert_byte_pointer_size('foo_bar_')
+    assert_cursor(8)
+    assert_cursor_max(8)
+    assert_line('foo_bar_')
+  end
     
-    __END__
+    class Reline::MacroTest < Reline::TestCase
+  def setup
+    @config = Reline::Config.new
+    @line_editor = Reline::LineEditor.new(@config)
+    @line_editor.instance_variable_set(:@screen_size, [24, 80])
+    @output = @line_editor.output = File.open(IO::NULL, 'w')
+  end
     
-    namespace :doc do
-  task :readmes do
-    Dir.glob 'lib/rack/protection/*.rb' do |file|
-      excluded_files = %w[lib/rack/protection/base.rb lib/rack/protection/version.rb]
-      next if excluded_files.include?(file)
-      doc  = File.read(file)[/^  module Protection(\n)+(    #[^\n]*\n)*/m].scan(/^ *#(?!#) ?(.*)\n/).join('\n')
-      file = 'doc/#{file[4..-4].tr('/_', '-')}.rdoc'
-      Dir.mkdir 'doc' unless File.directory? 'doc'
-      puts 'writing #{file}'
-      File.open(file, 'w') { |f| f << doc }
+        private
+    
+        def log_processed(name)
+      puts green '    #{name}'
     end
-  end
     
-          def call(env)
-        request  = Request.new(env)
-        get_was  = handle(request.GET)
-        post_was = handle(request.POST) rescue nil
-        app.call env
-      ensure
-        request.GET.replace  get_was  if get_was
-        request.POST.replace post_was if post_was
-      end
-    
-      it 'denies post requests with wrong X-CSRF-Token header' do
-    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => bad_token)
-    expect(last_response).not_to be_ok
-  end
-    
-      describe '#random_string' do
-    it 'outputs a string of 32 characters' do
-      expect(subject.random_string.length).to eq(32)
-    end
-  end
-    
-      it 'should not override the header if already set' do
-    mock_app with_headers('Content-Security-Policy' => 'default-src: none')
-    expect(get('/', {}, 'wants' => 'text/html').headers['Content-Security-Policy']).to eq('default-src: none')
-  end
+      # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
 end
 
     
-          expected_header = <<-END.chomp
-rack.%2573ession=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-END
-      expect(last_response.headers['Set-Cookie']).to eq(expected_header)
+      namespace :install do
+    GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
+      desc 'Build and install #{gem} as local gem'
+      task gem => package(gem, '.gem') do
+        sh 'gem install #{package(gem, '.gem')}'
+      end
+    end
+    
+          def mask_authenticity_token(session)
+        token = set_token(session)
+        mask_token(token)
+      end
+    
+          def has_vector?(request, headers)
+        return false if request.xhr?
+        return false if options[:allow_if] && options[:allow_if].call(request.env)
+        return false unless headers['Content-Type'].to_s.split(';', 2).first =~ /^\s*application\/json\s*$/
+        origin(request.env).nil? and referrer(request.env) != request.host
+      end
+    
+              if node.blockarg_type?
+            correct_for_blockarg_type(node)
+          else
+            lambda do |corrector|
+              variable_name = if node.optarg_type?
+                                node.node_parts[0]
+                              else
+                                # Extract only a var name without splat (`*`)
+                                node.source.gsub(/\A\*+/, '')
+                              end
+              corrector.replace(node.loc.name, '_#{variable_name}')
+            end
+          end
+        end
+    
+            private
+    
+        context 'in head position in a sequence against literal (bug #5470)' do
+      let(:pattern) { '(int ($_ ...))' }
+      let(:ruby) { '42' }
+    
+          expect_offense(<<~RUBY)
+        def method(arg)
+                   ^^^ #{message}
+          1
+        end
+      RUBY
+    end
+    
+        def any_symlink?(path)
+      while path != File.dirname(path)
+        if File.symlink?(path)
+          warn 'Warning: #{path} is a symlink, which is not allowed.'
+          return true
+        end
+        path = File.dirname(path)
+      end
+      false
+    end
+    
+        def initialize(tag_name, markup, tokens)
+      @by = nil
+      @source = nil
+      @title = nil
+      if markup =~ FullCiteWithTitle
+        @by = $1
+        @source = $2 + $3
+        @title = $4.titlecase.strip
+      elsif markup =~ FullCite
+        @by = $1
+        @source = $2 + $3
+      elsif markup =~ AuthorTitle
+        @by = $1
+        @title = $2.titlecase.strip
+      elsif markup =~ Author
+        @by = $1
+      end
+      super
+    end
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
     end
   end
+end
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
+    end
