71251002e90f5acc20a2aeb8d04d57f2fc91bc63
SUNsung <weatherius@gmail.com>
2019-08-16 14:16:48 +0300

    Rewrite [207Fb8T6T2vAs20Nt0rNuPz4l620ywrufj2l6NA20oG9.py] File in: 2019-08-16 14:16:48

--- pyton/207Fb8T6T2vAs20Nt0rNuPz4l620ywrufj2l6NA20oG9.py
+++ pyton/207Fb8T6T2vAs20Nt0rNuPz4l620ywrufj2l6NA20oG9.py
@@ -1,122 +1,116 @@
 
         
-        
-def archlinux_env():
-    if utils.which('yay'):
-        pacman = 'yay'
-    elif utils.which('yaourt'):
-        pacman = 'yaourt'
-    elif utils.which('pacman'):
-        pacman = 'sudo pacman'
-    else:
-        return False, None
+            return pkg_resources.require('thefuck')[0]
     
-    version = sys.version_info[:2]
-if version < (2, 7):
-    print('thefuck requires Python version 2.7 or later' +
-          ' ({}.{} detected).'.format(*version))
-    sys.exit(-1)
-elif (3, 0) < version < (3, 4):
-    print('thefuck requires Python version 3.4 or later' +
-          ' ({}.{} detected).'.format(*version))
-    sys.exit(-1)
-    
-        @pytest.fixture(autouse=True)
-    def shell_aliases(self):
-        os.environ['TF_SHELL_ALIASES'] = (
-            'alias fuck=\'eval $(thefuck $(fc -ln -1))\'\n'
-            'alias l=\'ls -CF\'\n'
-            'alias la=\'ls -A\'\n'
-            'alias ll=\'ls -alF\'')
+    from thefuck.utils import replace_command
+from thefuck.specific.archlinux import get_pkgfile, archlinux_env
     
     
-@cache('~/.config/fish/config.fish')
-def _get_aliases(overridden):
-    aliases = {}
-    proc = Popen(['fish', '-ic', 'alias'], stdout=PIPE, stderr=DEVNULL)
-    alias_out = proc.stdout.read().decode('utf-8').strip()
-    if not alias_out:
-        return aliases
-    for alias in alias_out.split('\n'):
-        for separator in (' ', '='):
-            split_alias = alias.replace('alias ', '', 1).split(separator, 1)
-            if len(split_alias) == 2:
-                name, value = split_alias
-                break
-        else:
-            continue
-        if name not in overridden:
-            aliases[name] = value
-    return aliases
+@utils.memoize
+def get_pkgfile(command):
+    ''' Gets the packages that provide the given command using `pkgfile`.
+    
+        def test_get_version_error(self, shell, Popen):
+        Popen.return_value.stdout.read.side_effect = RuntimeError
+        with pytest.raises(RuntimeError):
+            shell._get_version()
+        assert Popen.call_args[0][0] == ['powershell.exe', '$PSVersionTable.PSVersion']
+
+    
+        def _get_history_line(self, command_script):
+        return u'#+{}\n{}\n'.format(int(time()), command_script)
     
     
-@pytest.mark.parametrize('command', [
-    Command('az providers', misspelled_command),
-    Command('az provider lis', misspelled_subcommand)])
-def test_match(command):
-    assert match(command)
+@for_app('pyenv')
+def get_new_command(command):
+    broken = re.findall(r'pyenv: no such command `([^']*)'', command.output)[0]
+    matched = [replace_argument(command.script, broken, common_typo)
+               for common_typo in COMMON_TYPOS.get(broken, [])]
+    matched.extend(replace_command(command, broken, get_pyenv_commands()))
+    return matched
+
     
-                if not res or not res.ok:
-                xlog.debug('ip:%s fail', ip)
-                continue
+    
+no_suggestions = '''\
+az provider: error: the following arguments are required: _subcommand
+usage: az provider [-h] {list,show,register,unregister,operation} ...
+'''
     
     
-IPv4 = CheckNetwork('IPv4')
-IPv4.urls = [
-            'https://www.microsoft.com',
-            'https://www.apple.com',
-            'https://code.jquery.com',
-            'https://cdn.bootcss.com',
-            'https://cdnjs.cloudflare.com']
-IPv4.triger_check_network()
+def main():
+    '''
+    Main function
+    :return: None
+    '''
+    module = AnsibleModule(
+        argument_spec=ClcBlueprintPackage.define_argument_spec(),
+        supports_check_mode=True
+    )
+    clc_blueprint_package = ClcBlueprintPackage(module)
+    clc_blueprint_package.process_request()
     
-    # Predefined token types
-EOR_TOKEN_TYPE = 1
+        def get_job_status(self):
+        try:
+            response = self.server.get_job_info(self.name)
+            if 'color' not in response:
+                return self.EXCL_STATE
+            else:
+                return response['color'].encode('utf-8')
     
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licensc]
+    from __future__ import (absolute_import, division, print_function)
     
-    class EarlyExitException(RecognitionException):
-    '''@brief The recognizer did not match anything for a (..)+ loop.'''
+        if not layman.is_repo(name):
+        if not list_url:
+            raise ModuleError('Overlay '%s' is not on the list of known '
+                              'overlays and URL of the remote list was not provided.' % name)
     
-            @param channel Skip tokens on any channel but this one; this is how we
-            skip whitespace...
-            
-        '''
-        
-        TokenStream.__init__(self)
-        
-        self.tokenSource = tokenSource
+    # -- General configuration ---------------------------------------------
     
-        if 'message' in self._response:
-      return self._HandleMessageResponse()
+    test_locate_faces = '''
+face_locations = face_recognition.face_locations(image)
+'''
+    
+    # Open video file
+video_capture = cv2.VideoCapture('short_hamilton_clip.mp4')
+    
+            face_encoding_a1 = api.face_encodings(img_a1)[0]
+        face_encoding_a2 = api.face_encodings(img_a2)[0]
+        face_encoding_a3 = api.face_encodings(img_a3)[0]
+        face_encoding_b1 = api.face_encodings(img_b1)[0]
+    
+    # Find all facial features in all the faces in the image
+face_landmarks_list = face_recognition.face_landmarks(image)
+    
+            # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    
+    # Loop through each face found in the unknown image
+for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    # See if the face is a match for the known face(s)
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    
+    
+def _FilterToMatchingCompletions( completed_item, completions ):
+  '''Filter to completions matching the item Vim said was completed'''
+  match_keys = [ 'word', 'abbr', 'menu', 'info' ]
+  matched_completions = []
+  for index, completion in enumerate( completions ):
+    item = _ConvertCompletionDataToVimData( index, completion )
+    
+    
+  def ShouldUseCache( self ):
+    return bool( self.user_options[ 'cache_omnifunc' ] )
+    
+            vimsupport.ReplaceChunks(
+          self._response[ 'fixits' ][ fixit_index ][ 'chunks' ],
+          silent = self._command == 'Format' )
+      except RuntimeError as e:
+        vimsupport.PostVimMessage( str( e ) )
+    
+      for filter_type in iterkeys( config ):
+    compiler = FILTER_COMPILERS.get( filter_type )
     
     from __future__ import unicode_literals
 from __future__ import print_function
@@ -126,59 +120,17 @@ from __future__ import absolute_import
 from builtins import *  # noqa
     
     
-GENERIC_RESPONSE = {
-  'clang': {
-    'has_support': True,
-    'version': 'Clang version'
-  },
-  'completer': {
-    'items': [
-      {
-        'key': 'key',
-        'value': 'value'
-      }
-    ],
-    'name': 'Completer name',
-    'servers': [
-      {
-        'address': '127.0.0.1',
-        'executable': '/path/to/executable',
-        'extras': [
-          {
-            'key': 'key',
-            'value': 'value'
-          }
-        ],
-        'is_running': True,
-        'logfiles': [
-          '/path/to/stdout/logfile',
-          '/path/to/stderr/logfile'
-        ],
-        'name': 'Server name',
-        'pid': 12345,
-        'port': 1234
-      }
-    ]
-  },
-  'extra_conf': {
-    'is_loaded': False,
-    'path': '/path/to/extra/conf'
-  },
-  'python': {
-    'executable': '/path/to/python/interpreter',
-    'version': 'Python version'
-  }
-}
+def CommaSeparatedFiletypes_test():
     
-      post_vim_message.assert_has_exact_calls( [
-    call( 'On the first day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'A test file in a Command-T', warning=False, truncate=True ),
-    call( 'On the second day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'Two popup menus, and a test file in a Command-T',
-          warning=False,
-          truncate=True ),
-  ] )
+    
+def KeywordsFromSyntaxListOutput_JunkIgnored_test():
+  assert_that( syntax_parse._KeywordsFromSyntaxListOutput( '''
+--- Syntax items ---
+foogroup xxx foo bar
+             zoo goo
+             links to Statement
+Spell        cluster=NONE
+NoSpell      cluster=NONE''' ),
+               contains_inanyorder( 'foo', 'bar', 'zoo', 'goo' ) )
+    
+    '''Implements ProcessPoolExecutor.
