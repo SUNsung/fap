e666a30163b189f96635e1df3cab82133b8195d1
SUNsung <weatherius@gmail.com>
2019-08-17 10:47:39 +0300

    Rewrite [29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb] File in: 2019-08-17 10:47:38

--- ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
+++ ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
@@ -1,189 +1,164 @@
 
         
-        # Searches and reads files present on each GitLab project repository
-module Gitlab
-  module Template
-    module Finders
-      class RepoTemplateFinder < BaseTemplateFinder
-        # Raised when file is not found
-        FileNotFoundError = Class.new(StandardError)
+                xcpretty = 'xcpretty #{Snapshot.config[:xcpretty_args]}'
+        xcpretty << '--no-color' if Helper.colors_disabled?
     
-        def aes256_gcm_encrypt(value)
-      encrypted_token = Encryptor.encrypt(AES256_GCM_OPTIONS.merge(value: value))
-      Base64.strict_encode64(encrypted_token)
+            options = []
+        options += project_path_array
+        options << '-sdk '#{config[:sdk]}'' if config[:sdk]
+        options << '-derivedDataPath '#{derived_data_path}''
+        options << '-resultBundlePath '#{result_bundle_path}'' if result_bundle_path
+        options << config[:xcargs] if config[:xcargs]
+        return options
+      end
+    
+          def self.available_options
+        user = CredentialsManager::AppfileConfig.try_fetch_value(:apple_dev_portal_id)
+        user ||= CredentialsManager::AppfileConfig.try_fetch_value(:apple_id)
+    
+        it 'should fail to create a nil device UDID' do
+      expect do
+        Spaceship::Device.create!(name: 'Demo Device', udid: nil)
+      end.to raise_error('You cannot create a device without a device_id (UDID) and name')
     end
     
-          context 'and user can update_project_member' do
-        before do
-          allow(presenter).to receive(:can?).with(user, :update_project_member, presenter).and_return(true)
-        end
+            expect_runner_run_with(expected_options)
     
-      context 'when should not request diff stats' do
-    it 'Repository#diff_stats is not called' do
-      collection_default_args[:diff_options][:include_stats] = false
-    
-            def create_project_service_account
-          Clusters::Gcp::Kubernetes::CreateOrUpdateServiceAccountService.namespace_creator(
-            platform.kubeclient,
-            service_account_name: kubernetes_namespace.service_account_name,
-            service_account_namespace: kubernetes_namespace.namespace,
-            rbac: platform.rbac?
-          ).execute
-        end
-    
-          stime = Time.now.to_i
-    
-        # In the case where a plus or slash happened at the end of a chunk,
-    # we'll have two dots next to each other, so fix it up.  Note that this
-    # is searching for literal dots, not a regex matching any two
-    # characters
-    b64.gsub!('..', '.')
-    
-    payload = File.binread('payload.dylib')
-output_data[add_dylib, payload.size] = payload
-    
-          unless opts[:domain_sid]
-        domain_sid = dcsync_result[:sid].split('-')
-        opts[:domain_sid] = domain_sid[0, domain_sid.length - 1].join('-')
-        print_warning('Domain SID missing, using #{opts[:domain_sid]} extracted from SID of #{krbtgt_username}')
-      end
-    end
-    
-        path = datastore['PATH'] || session.sys.config.getenv('USERPROFILE')
-    path.chomp!('\\')
-    
-      # Function for writing executable to target host
-  #-------------------------------------------------------------------------------
-  def write_exe_to_target(rexe, rexename)
-    # check if we have write permission
-    # I made it by myself because the function filestat.writable? was not implemented yet.
-    if not datastore['LocalExePath'].nil?
-    
-    
-    
-      #logfile name
-  logfile = logs + ::File::Separator + Rex::FileUtils.clean_path(host + filenameinfo) + '.rc'
-  return logfile
-end
-    
-      #
-  # The OSX version uses an apple script to do this
-  #
-  def osx_start_video(_id)
-    script = ''
-    script << %Q|osascript -e 'tell application 'Safari' to open location '#{youtube_url}'' |
-    script << %Q|-e 'activate application 'Safari'' |
-    script << %Q|-e 'tell application 'System Events' to key code {59, 55, 3}'|
-    
-        def index
-      pods_json = PodPresenter.as_collection(Pod.all)
-    
-      def user_search
-    if params[:admins_controller_user_search]
-      search_params = params.require(:admins_controller_user_search)
-                            .permit(:username, :email, :guid, :under13)
-      @search = UserSearch.new(search_params)
-      @users = @search.perform
-    end
-    
-          def http_error_page_as_json(e)
-        render json: {error: :invalid_request, error_description: e.message}, status: 400
-      end
-    
-          private
-    
-        if @aspect.save
-      result = {id: @aspect.id, name: @aspect.name}
-      if aspecting_person_id.present?
-        aspect_membership = connect_person_to_aspect(aspecting_person_id)
-        result[:aspect_membership] = AspectMembershipPresenter.new(aspect_membership).base_hash if aspect_membership
-      end
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-            it 'returns false' do
-          expect(node.pure?).to be(false)
-        end
-      end
-    end
-    
-      describe '#parent_class' do
-    context 'when a parent class is specified' do
-      let(:source) do
-        'class Foo < Bar; end'
-      end
-    
-          it 'is not fooled by single line blocks' do
-        expect_no_offenses(<<~RUBY)
-          some_method #{open} do_something #{close}
-          something_else
-        RUBY
+          def self.category
+        :project
       end
     end
   end
 end
 
     
-        context 'when a keyword argument is unused' do
-      let(:source) { <<-RUBY }
-        def some_method(foo, bar: 1)
-          puts foo
-        end
-      RUBY
-    
-          it { expect(if_node.is_a?(described_class)).to be(true) }
+        it 'does not permit path traversal requests' do
+      expect { finder.find('../foo') }.to raise_error(/Invalid path/)
     end
+  end
+end
+
     
-            context 'with MyNamespace' do
-          it 'does not register an offense' do
-            expect_no_offenses('1.is_a?(MyNamespace::#{klass})')
-          end
-        end
+          expect(result).to contain_exactly('files/html/500.html')
+    end
+  end
+end
+
+    
+      private
+    
+        def find(query)
+      query = Gitlab::Search::Query.new(query, encode_binary: true) do
+        filter :filename, matcher: ->(filter, blob) { blob.binary_filename =~ /#{filter[:regex_value]}$/i }
+        filter :path, matcher: ->(filter, blob) { blob.binary_filename =~ /#{filter[:regex_value]}/i }
+        filter :extension, matcher: ->(filter, blob) { blob.binary_filename =~ /\.#{filter[:regex_value]}$/i }
       end
+    
+      def output_chronic_duration_attribute(source_attribute)
+    value = attributes[source_attribute.to_s]
+    ChronicDuration.output(value, format: :short) if value
+  end
+end
+
+    
+      let(:iso_env) do
+    # We have to create a Vagrantfile so there is a root path
+    env = isolated_environment
+    env.vagrantfile('')
+    env.create_vagrant_env
+  end
+    
+      # Automatically install locally defined plugins instead of
+  # waiting for user confirmation.
+  #
+  # @return [Boolean]
+  def self.auto_install_local_plugins?
+    if ENV['VAGRANT_INSTALL_LOCAL_PLUGINS']
+      true
+    else
+      false
     end
   end
     
-    module RuboCop
-  module AST
-    # A node extension for `for` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `for` nodes within RuboCop.
-    class ForNode < Node
-      # Returns the keyword of the `for` statement as a string.
-      #
-      # @return [String] the keyword of the `until` statement
-      def keyword
-        'for'
-      end
-    
-    is_travis = ENV['TRAVIS_OS_NAME'] && !ENV['TRAVIS_OS_NAME'].empty?
-    
-      config.vm.define :smartos do |smartos|
-    smartos.vm.box = 'smartos-base1310-64-virtualbox-20130806.box'
-    smartos.vm.box_url = 'http://dlc-int.openindiana.org/aszeszo/vagrant/smartos-base1310-64-virtualbox-20130806.box'
-  end
-    
-    class FPM::Package::NPM < FPM::Package
-  class << self
-    include FPM::Util
-  end
-  # Flags '--foo' will be accessable  as attributes[:npm_foo]
-  option '--bin', 'NPM_EXECUTABLE',
-    'The path to the npm executable you wish to run.', :default => 'npm'
-    
-      # Default specfile generator just makes one specfile, whatever that is for
-  # this package.
-  def generate_specfile(builddir)
-    paths = []
-    logger.info('PWD: #{File.join(builddir, unpack_data_to)}')
-    fileroot = File.join(builddir, unpack_data_to)
-    Dir.chdir(fileroot) do
-      Find.find('.') do |p|
-        next if p == '.'
-        paths << p
-      end
+      def mon_check_owner
+    if @mon_owner != Thread.current
+      raise ThreadError, 'current thread not owner'
     end
-    logger.info(paths[-1])
-    manifests = %w{package.pp package/remove.pp}
+  end
+    
+        @co.comment = input
+    
+      def test_push__plural
+    assert_equal(Readline::HISTORY, Readline::HISTORY.push('0', '1', '2', '3', '4'))
+    (0..4).each do |i|
+      assert_external_string_equal(i.to_s, Readline::HISTORY[i])
+    end
+    assert_equal(5, Readline::HISTORY.length)
+    
+    class Reline::MacroTest < Reline::TestCase
+  def setup
+    @config = Reline::Config.new
+    @line_editor = Reline::LineEditor.new(@config)
+    @line_editor.instance_variable_set(:@screen_size, [24, 80])
+    @output = @line_editor.output = File.open(IO::NULL, 'w')
+  end
+    
+      def test_macro_commands_for_editing
+    @config.add_default_key_binding('\C-x\C-d'.bytes, :delete_char)
+    @config.add_default_key_binding('\C-x\C-h'.bytes, :backward_delete_char)
+    @config.add_default_key_binding('\C-x\C-v'.bytes, :quoted_insert)
+    #@config.add_default_key_binding('\C-xa'.bytes, :self_insert)
+    @config.add_default_key_binding('\C-x\C-t'.bytes, :transpose_chars)
+    @config.add_default_key_binding('\C-x\M-t'.bytes, :transpose_words)
+    @config.add_default_key_binding('\C-x\M-u'.bytes, :upcase_word)
+    @config.add_default_key_binding('\C-x\M-l'.bytes, :downcase_word)
+    @config.add_default_key_binding('\C-x\M-c'.bytes, :capitalize_word)
+    @writer.write('abcde\C-b\C-b\C-b\C-x\C-d\C-x\C-h\C-x\C-v\C-a\C-f\C-f EF\C-x\C-t gh\C-x\M-t\C-b\C-b\C-b\C-b\C-b\C-b\C-b\C-b\C-x\M-u\C-x\M-l\C-x\M-c\n')
+    assert_equal 'a\C-aDE gh Fe', Reline.readmultiline(&proc{ true })
+  end
+end
+
+    
+    module Bourbon
+  class Generator < Thor
+    map ['-v', '--version'] => :version
+    
+      context 'called with two colors' do
+    it 'applies to alternating sides' do
+      rule = 'border-color: #0f0 #00f'
+    
+      context 'called with three widths' do
+    it 'applies second width to left and right' do
+      rule = 'border-width: 4px 5px 6px'
+    
+          expect('.all-buttons-active').to have_ruleset(ruleset)
+    end
+  end
+    
+          expect('.margin-explicit').to have_rule(rule)
+    end
+  end
+    
+      context 'called with two sizes' do
+    it 'applies to height and width' do
+      rule = 'height: 2em; width: 1em;'
+    
+        @inputs_list = %w(
+      [type='color']
+      [type='date']
+      [type='datetime']
+      [type='datetime-local']
+      [type='email']
+      [type='month']
+      [type='number']
+      [type='password']
+      [type='search']
+      [type='tel']
+      [type='text']
+      [type='time']
+      [type='url']
+      [type='week']
+      input:not([type])
+      textarea
+    )
+  end
