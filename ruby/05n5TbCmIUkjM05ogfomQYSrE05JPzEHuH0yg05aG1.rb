0810a3fea8184d88cc59a10ab5e1953126240436
SUNsung <weatherius@gmail.com>
2019-08-17 16:22:15 +0300

    Rewrite [05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb] File in: 2019-08-17 16:22:15

--- ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
+++ ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
@@ -1,3 +1,207 @@
 
         
-        
+            file package(gem, '.tar.gz') => ['pkg/'] do |f|
+      sh <<-SH
+        git archive \
+          --prefix=#{gem}-#{source_version}/ \
+          --format=tar \
+          HEAD -- #{directory} | gzip > #{f.name}
+      SH
+    end
+  end
+    
+    class Rack::Builder
+  include Sinatra::Delegator
+end
+
+    
+    <style type='text/css' media='screen'>
+  *                   {margin: 0; padding: 0; border: 0; outline: 0;}
+  div.clear           {clear: both;}
+  body                {background: #EEEEEE; margin: 0; padding: 0;
+                       font-family: 'Lucida Grande', 'Lucida Sans Unicode',
+                       'Garuda';}
+  code                {font-family: 'Lucida Console', monospace;
+                       font-size: 12px;}
+  li                  {height: 18px;}
+  ul                  {list-style: none; margin: 0; padding: 0;}
+  ol:hover            {cursor: pointer;}
+  ol li               {white-space: pre;}
+  #explanation        {font-size: 12px; color: #666666;
+                       margin: 20px 0 0 100px;}
+/* WRAP */
+  #wrap               {width: 1000px; background: #FFFFFF; margin: 0 auto;
+                       padding: 30px 50px 20px 50px;
+                       border-left: 1px solid #DDDDDD;
+                       border-right: 1px solid #DDDDDD;}
+/* HEADER */
+  #header             {margin: 0 auto 25px auto;}
+  #header img         {float: left;}
+  #header #summary    {float: left; margin: 12px 0 0 20px; width:660px;
+                       font-family: 'Lucida Grande', 'Lucida Sans Unicode';}
+  h1                  {margin: 0; font-size: 36px; color: #981919;}
+  h2                  {margin: 0; font-size: 22px; color: #333333;}
+  #header ul          {margin: 0; font-size: 12px; color: #666666;}
+  #header ul li strong{color: #444444;}
+  #header ul li       {display: inline; padding: 0 10px;}
+  #header ul li.first {padding-left: 0;}
+  #header ul li.last  {border: 0; padding-right: 0;}
+/* BODY */
+  #backtrace,
+  #get,
+  #post,
+  #cookies,
+  #rack               {width: 980px; margin: 0 auto 10px auto;}
+  p#nav               {float: right; font-size: 14px;}
+/* BACKTRACE */
+  a#expando           {float: left; padding-left: 5px; color: #666666;
+                      font-size: 14px; text-decoration: none; cursor: pointer;}
+  a#expando:hover     {text-decoration: underline;}
+  h3                  {float: left; width: 100px; margin-bottom: 10px;
+                       color: #981919; font-size: 14px; font-weight: bold;}
+  #nav a              {color: #666666; text-decoration: none; padding: 0 5px;}
+  #backtrace li.frame-info {background: #f7f7f7; padding-left: 10px;
+                           font-size: 12px; color: #333333;}
+  #backtrace ul       {list-style-position: outside; border: 1px solid #E9E9E9;
+                       border-bottom: 0;}
+  #backtrace ol       {width: 920px; margin-left: 50px;
+                       font: 10px 'Lucida Console', monospace; color: #666666;}
+  #backtrace ol li    {border: 0; border-left: 1px solid #E9E9E9;
+                       padding: 2px 0;}
+  #backtrace ol code  {font-size: 10px; color: #555555; padding-left: 5px;}
+  #backtrace-ul li    {border-bottom: 1px solid #E9E9E9; height: auto;
+                       padding: 3px 0;}
+  #backtrace-ul .code {padding: 6px 0 4px 0;}
+  #backtrace.condensed .system,
+  #backtrace.condensed .framework {display:none;}
+/* REQUEST DATA */
+  p.no-data           {padding-top: 2px; font-size: 12px; color: #666666;}
+  table.req           {width: 980px; text-align: left; font-size: 12px;
+                       color: #666666; padding: 0; border-spacing: 0;
+                       border: 1px solid #EEEEEE; border-bottom: 0;
+                       border-left: 0;
+                       clear:both}
+  table.req tr th     {padding: 2px 10px; font-weight: bold;
+                       background: #F7F7F7; border-bottom: 1px solid #EEEEEE;
+                       border-left: 1px solid #EEEEEE;}
+  table.req tr td     {padding: 2px 20px 2px 10px;
+                       border-bottom: 1px solid #EEEEEE;
+                       border-left: 1px solid #EEEEEE;}
+/* HIDE PRE/POST CODE AT START */
+  .pre-context,
+  .post-context       {display: none;}
+    
+          def accepts?(env)
+        cookie_header = env['HTTP_COOKIE']
+        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
+        cookies.each do |k, v|
+          if k == session_key && Array(v).size > 1
+            bad_cookies << k
+          elsif k != session_key && Rack::Utils.unescape(k) == session_key
+            bad_cookies << k
+          end
+        end
+        bad_cookies.empty?
+      end
+    
+        it 'sets a session authenticity token if one does not exist' do
+      session = {}
+      allow(Rack::Protection::AuthenticityToken).to receive(:random_token).and_return(token)
+      allow_any_instance_of(Rack::Protection::AuthenticityToken).to receive(:mask_token).and_return(masked_token)
+      Rack::Protection::AuthenticityToken.token(session)
+      expect(session[:csrf]).to eq(token)
+    end
+  end
+    
+    ENV['REDIS_URL'] ||= 'redis://localhost/15'
+    
+          assert_raises ArgumentError do
+        Sidekiq::Client.push('queue' => 'foo', 'class' => MyWorker, 'noargs' => [1, 2])
+      end
+    
+        it 'logs the exception to Sidekiq.logger' do
+      Component.new.invoke_exception(:a => 1)
+      @str_logger.rewind
+      log = @str_logger.readlines
+      assert_match(/'a':1/, log[0], 'didn't include the context')
+      assert_match(/Something didn't work!/, log[1], 'didn't include the exception message')
+      assert_match(/test\/test_exception_handler.rb/, log[2], 'didn't include the backtrace')
+    end
+    
+        final_action = nil
+    chain.invoke { final_action = true }
+    assert_nil final_action
+    assert_equal [], recorder
+  end
+    
+      config.vm.define 'arch' do |arch|
+    arch.vm.box = 'jfredett/arch-puppet'
+  end
+    
+    require 'fpm'
+require 'tmpdir'
+require 'fpm/package/pleaserun'
+    
+        install_args = [
+      attributes[:npm_bin],
+      'install',
+      # use 'package' or 'package@version'
+     (version ? '#{package}@#{version}' : package)
+    ]
+    
+      private
+  def input(command)
+    platforms = [
+      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
+      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
+      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
+      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
+      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
+    ]
+    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
+                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
+                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
+                             'log_directory', 'log_file_stderr', 'log_file_stdout']
+    
+      # This method is used by the puppet manifest template
+  def puppetsort(hash)
+    # TODO(sissel): Implement sorting that follows the puppet style guide
+    # Such as, 'ensure' goes first, etc.
+    return hash.to_a
+  end # def puppetsort
+    
+    # Support for python packages.
+#
+# This supports input, but not output.
+#
+# Example:
+#
+#     # Download the django python package:
+#     pkg = FPM::Package::Python.new
+#     pkg.input('Django')
+#
+class FPM::Package::Python < FPM::Package
+  # Flags '--foo' will be accessable  as attributes[:python_foo]
+  option '--bin', 'PYTHON_EXECUTABLE',
+    'The path to the python executable you wish to run.', :default => 'python'
+  option '--easyinstall', 'EASYINSTALL_EXECUTABLE',
+    'The path to the easy_install executable tool', :default => 'easy_install'
+  option '--pip', 'PIP_EXECUTABLE',
+    'The path to the pip executable tool. If not specified, easy_install ' \
+    'is used instead', :default => nil
+  option '--pypi', 'PYPI_URL',
+    'PyPi Server uri for retrieving packages.',
+    :default => 'https://pypi.python.org/simple'
+  option '--package-prefix', 'NAMEPREFIX',
+    '(DEPRECATED, use --package-name-prefix) Name to prefix the package ' \
+    'name with.' do |value|
+    logger.warn('Using deprecated flag: --package-prefix. Please use ' \
+                 '--package-name-prefix')
+    value
+  end
+  option '--package-name-prefix', 'PREFIX', 'Name to prefix the package ' \
+    'name with.', :default => 'python'
+  option '--fix-name', :flag, 'Should the target package name be prefixed?',
+    :default => true
+  option '--fix-dependencies', :flag, 'Should the package dependencies be ' \
+    'prefixed?', :default => true
