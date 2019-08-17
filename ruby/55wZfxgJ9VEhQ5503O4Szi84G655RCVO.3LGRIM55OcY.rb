a1403e9f435739423c0980e6108b8bd0b75dcbd7
SUNsung <weatherius@gmail.com>
2019-08-17 19:21:55 +0300

    Rewrite [55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb] File in: 2019-08-17 19:21:54

--- ruby/55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb
+++ ruby/55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb
@@ -1,87 +1,67 @@
 
         
-        describe 'CommonMark' do
-  it 'passes spec' do
+        require 'vagrant/util'
+require 'vagrant/util/shell_quote'
+require 'vagrant/util/which'
     
-        Logster.add_to_env(request.env, 'CSP Report', report)
-    Rails.logger.warn('CSP Violation: '#{report['blocked-uri']}'')
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
+    end
     
-        it 'rejects invalid directives and ones that are not allowed to be extended' do
-      builder << {
-        invalid_src: ['invalid'],
-      }
+        describe '#name' do
+      subject { super().name }
+      it     { should eq(:default)  }
+    end
     
-        # if you need to test this and are having ssl issues see:
-    #  http://stackoverflow.com/questions/6756460/openssl-error-using-omniauth-specified-ssl-path-but-didnt-work
-    # OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
-    @omniauth = OmniAuth::Builder.new(app) do
-      Discourse.authenticators.each do |authenticator|
-        authenticator.register_middleware(self)
+          it 'it should exclude the dependency' do
+        subject.attributes[:python_disable_dependency] = 'rtxt-dep1'
+        subject.input(example_dir)
+        insist { subject.dependencies.sort } == ['python-rtxt-dep2 = 0.1', 'python-rtxt-dep4  ']
       end
     end
     
-      def self.languages
-    langs = Dir.glob(HIGHLIGHTJS_DIR + 'languages/*.js').map do |path|
-      File.basename(path)[0..-8]
+          # Value sanity check
+      insist { subject.name } == 'name'
+      insist { subject.version } == '1.23'
+      insist { subject.scripts[:before_install] } == 'example before_install'
+      insist { subject.scripts[:after_install] } == 'example after_install'
+      insist { subject.scripts[:before_remove] } == 'example before_remove'
+      insist { subject.scripts[:after_remove] } == 'example after_remove'
+      insist { subject.scripts[:rpm_verifyscript] } == 'example rpm_verifyscript'
+      insist { subject.scripts[:rpm_posttrans] } == 'example rpm_posttrans'
+      insist { subject.scripts[:rpm_pretrans] } == 'example rpm_pretrans'
     end
     
-        def base_url
-      @base_url || Discourse.base_url
-    end
-    attr_writer :base_url
+      describe '#description' do
+    it_behaves_like :Default, description.gsub(/^#/, '').to_sym, 'no description given'
+    it_behaves_like :Mutator, description.gsub(/^#/, '').to_sym
   end
     
-          Theme.where(id: Theme.transform_ids(theme_ids)).find_each do |theme|
-        theme.cached_settings.each do |setting, value|
-          extensions << build_theme_extension(value) if setting.to_s == THEME_SETTING
-        end
-      end
+        def validate
+      # Make sure the user has passed '-s' and '-t' flags
+      mandatory(@command.input_type,
+                'Missing required -s flag. What package source did you want?')
+      mandatory(@command.output_type,
+                'Missing required -t flag. What package output did you want?')
     
-                  subscriber = Lograge::LogSubscribers::ActionController.new
-              payload = ActiveSupport::HashWithIndifferentAccess.new(
-                controller: self.class.name,
-                action: action_name,
-                params: request.filtered_parameters,
-                headers: request.headers,
-                format: request.format.ref,
-                method: request.request_method,
-                path: request.fullpath,
-                view_runtime: view_runtime * 1000.0,
-                db_runtime: db_runtime * 1000.0,
-                timings: timings,
-                status: response.status
-              )
-    
-          # Returns the keyword of the `case` statement as a string.
-      #
-      # @return [String] the keyword of the `case` statement
-      def keyword
-        'case'
-      end
-    
-          # The receiver of the method definition, if any.
-      #
-      # @return [Node, nil] the receiver of the method definition, or `nil`.
-      def receiver
-        node_parts[3]
-      end
-    
-          # Returns the value of this `hash` element.
-      #
-      # @note For keyword splats, this returns the whole node
-      #
-      # @return [Node] the value of the hash element
-      def value
-        node_parts[1]
-      end
-    
-            def swap_range(corrector, range1, range2)
-          src1 = range1.source
-          src2 = range2.source
-          corrector.replace(range1, src2)
-          corrector.replace(range2, src1)
-        end
+        self.description = info['description']
+    # Supposedly you can upload a package for npm with no author/author email
+    # so I'm being safer with this. Author can also be a hash or a string
+    self.vendor = 'Unknown <unknown@unknown.unknown>'
+    if info.include?('author')
+      author_info = info['author']
+      # If a hash, assemble into a string
+      if author_info.respond_to? :fetch
+        self.vendor = sprintf('%s <%s>', author_info.fetch('name', 'unknown'),
+                              author_info.fetch('email', 'unknown@unknown.unknown'))
+      else
+        # Probably will need a better check for validity here
+        self.vendor = author_info unless author_info == ''
       end
     end
-  end
-end
+    
+      def to_s_extension; 'pkg'; end
+    
+        File.write(build_path('packlist'), files.sort.join('\n'))
