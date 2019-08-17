67b85f0c534e08e31e2215b4adbe2826ce2ae1c6
SUNsung <weatherius@gmail.com>
2019-08-17 11:40:20 +0300

    Rewrite [56CYzaP6hRiH.56BKdED4OtAeE56T7ukFRZZtU256N48.cpp] File in: 2019-08-17 11:40:19

--- cccccc/56CYzaP6hRiH.56BKdED4OtAeE56T7ukFRZZtU256N48.cpp
+++ cccccc/56CYzaP6hRiH.56BKdED4OtAeE56T7ukFRZZtU256N48.cpp
@@ -1,212 +1,44 @@
 
         
-        
-    {  tensorflow::mutex mu_;
-  absl::flat_hash_map<se::Stream*, CusolverContext> contexts_ GUARDED_BY(mu_);
-};
+        #ifndef BITCOIN_QT_NETWORKSTYLE_H
+#define BITCOIN_QT_NETWORKSTYLE_H
     
-      Status ExecuteOnStream(const ExecuteParams& params) override;
+        ~reverse_lock() {
+        templock.lock();
+        templock.swap(lock);
+    }
     
-        http://www.apache.org/licenses/LICENSE-2.0
+    ; shuffle xDxC -> DC00
+_SHUF_DC00:              ddq 0x0b0a090803020100FFFFFFFFFFFFFFFF
+*/
     
-    #ifndef TENSORFLOW_COMPILER_XLA_SERVICE_GPU_OUTFEED_THUNK_H_
-#define TENSORFLOW_COMPILER_XLA_SERVICE_GPU_OUTFEED_THUNK_H_
-    
-    #include 'absl/strings/str_cat.h'
-#include 'absl/strings/str_format.h'
-#include 'tensorflow/compiler/xla/service/gpu/hlo_execution_profiler.h'
-#include 'tensorflow/compiler/xla/types.h'
-#include 'tensorflow/compiler/xla/util.h'
-#include 'tensorflow/compiler/xla/xla_data.pb.h'
-#include 'tensorflow/core/platform/logging.h'
-#include 'tensorflow/core/platform/stream_executor_no_cuda.h'
-#include 'tensorflow/stream_executor/blas.h'
-#include 'tensorflow/stream_executor/device_memory.h'
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #include <grpc/support/port_platform.h>
-    
-    
-    {
-    { private:
-  std::shared_ptr<grpc::Channel> CreateChannelWithInterceptors(
-      const string& target, const grpc::ChannelArguments& args,
-      std::vector<
-          std::unique_ptr<experimental::ClientInterceptorFactoryInterface>>
-          interceptor_creators) override {
-    grpc_channel_args channel_args;
-    args.SetChannelArgs(&channel_args);
-    return CreateChannelInternal(
-        '',
-        grpc_cronet_secure_channel_create(engine_, target.c_str(),
-                                          &channel_args, nullptr),
-        std::move(interceptor_creators));
+      void Compact(const Slice& start, const Slice& limit) {
+    db_->CompactRange(&start, &limit);
   }
-  void* engine_;
-};
-}  // namespace grpc
-namespace grpc_impl {
-std::shared_ptr<ChannelCredentials> CronetChannelCredentials(void* engine) {
-  return std::shared_ptr<ChannelCredentials>(
-      new grpc::CronetChannelCredentialsImpl(engine));
+    
+    #include <stdio.h>
+#include 'db/dbformat.h'
+#include 'port/port.h'
+#include 'util/coding.h'
+    
+    
+    {}  // namespace
+    
+    // Make the CURRENT file point to the descriptor file with the
+// specified number.
+extern Status SetCurrentFile(Env* env, const std::string& dbname,
+                             uint64_t descriptor_number);
+    
+            void addWrite() {
+            if (m_write) return;
+            m_write = new QSocketNotifier(m_ctx->c.fd, QSocketNotifier::Write, 0);
+            connect(m_write, SIGNAL(activated(int)), this, SLOT(write()));
+        }
+    
+        private:
+        void finish() { emit finished(); }
+    
+    
+    {    cache.clear();
+    ASSERT_EQ(cache.get('test'), nullptr);
 }
-}  // namespace grpc_impl
-
-    
-      void SendRpc(int num_rpcs) {
-    for (int i = 0; i < num_rpcs; i++) {
-      EchoRequest send_request;
-      EchoRequest recv_request;
-      EchoResponse send_response;
-      EchoResponse recv_response;
-      Status recv_status;
-    }
-    }
-    
-    template class PlusNode<float>;
-template class PlusNode<double>;
-template class PlusNode<half>;
-    
-    
-    {
-    {                fstream.GetMarker(FileMarker::fileMarkerEndSection, L'EMACKP');
-            }
-        }
-    private:
-       // helper function to save/load map<wstring, shared_ptr<Matrix<ElemType>> structure 
-       void SaveParameters(File& f, const map<wstring, shared_ptr<Matrix<ElemType>>>& parameters) const
-        {
-            // save sizeof(ElemType)
-            unsigned int size = sizeof(ElemType);
-            f << size;
-            // save number of pairs 
-            unsigned int numPairs = parameters.size();
-            f << numPairs;
-            for (auto& x : parameters)
-            {
-                f << x.first;
-                f << *x.second;
-            }
-            f.Flush();
-            return;
-        }
-       void LoadParameters(File& f, map<wstring, shared_ptr<Matrix<ElemType>>>& parameters, DEVICEID_TYPE deviceID)
-       {
-           unsigned int size = 0;
-           unsigned int pair = 0;
-           f >> size;
-           f >> pair;
-           if (size != sizeof(ElemType))
-           {
-               LogicError('Mismatched ElemType in loading BlockMomentumSGD checkpoint. Expecting %s, while loading element size=%d\n',
-                   sizeof(ElemType) == 4 ? 'float' : 'double',
-                   size
-                   );
-           }
-           parameters.clear();
-           for (size_t i = 0; i < pair; i++)
-           {
-               wstring name;
-               f >> name;
-               shared_ptr<Matrix<ElemType>> mat = make_shared<Matrix<ElemType>>(deviceID);
-               f >> *mat;
-               parameters[name] = mat;
-           }
-       }
-    
-    #include 'ComputationNode.h'
-#include 'InputAndParamNodes.h'
-#include 'LinearAlgebraNodes.h'
-#include 'NonlinearityNodes.h'
-#include 'ConvolutionalNodes.h'
-#include 'RecurrentNodes.h'
-#include 'PreComputeNodes.h'
-    
-    struct IPreComputeNode
-{
-    // check whether node has already undergone precomputation
-    virtual bool HasComputed() const = 0;
-    // call this with 'false' at start and with 'true' at end
-    // This is used for resetting and updating from accumulators.
-    virtual void MarkComputed(const bool hasComputed) = 0;
-};
-    
-    
-    {            if (m_useAsyncAggregation)
-            {
-                std::unique_ptr<MatrixComputeStreamEvent> mainStreamSyncEvent(MatrixComputeStreamEvent::Create(deviceId));
-                mainStreamSyncEvent->SynchronizeDataTransferFetchStreamWithEvent<float>();
-            }
-        }
-    
-    BOOST_AUTO_TEST_CASE(EvalScalarTimesDualOutputTest)
-{
-    std::string modelDefinition =
-        'deviceId = -1 \n'
-        'precision = \'float\' \n'
-        'traceLevel = 1 \n'
-        'run=NDLNetworkBuilder \n'
-        'NDLNetworkBuilder=[ \n'
-        'i1 = Input(1) \n'
-        'i2 = Input(1) \n'
-        'o1 = Times(Constant(3), i1, tag=\'output\') \n'
-        'o2 = Times(Constant(5), i1, tag=\'output\') \n'
-        'FeatureNodes = (i1) \n'
-        '] \n';
-    }
-    
-    namespace {
-SequenceNumber ReadRecords(
-    std::unique_ptr<TransactionLogIterator>& iter,
-    int& count) {
-  count = 0;
-  SequenceNumber lastSequence = 0;
-  BatchResult res;
-  while (iter->Valid()) {
-    res = iter->GetBatch();
-    EXPECT_TRUE(res.sequence > lastSequence);
-    ++count;
-    lastSequence = res.sequence;
-    EXPECT_OK(iter->status());
-    iter->Next();
-  }
-  return res.sequence;
-}
-    }
-    
-    #if !defined(ROCKSDB_LITE) && !defined(OS_WIN)
-    
-      // put and get from non-default column family
-  s = db->Put(WriteOptions(), handles[1], Slice('key'), Slice('value'));
-  assert(s.ok());
-  std::string value;
-  s = db->Get(ReadOptions(), handles[1], Slice('key'), &value);
-  assert(s.ok());
-    
-      // Write a key in this transaction
-  txn->Put('abc', 'def');
-    
-      // initialize column families options
-  std::unique_ptr<CompactionFilter> compaction_filter;
-  compaction_filter.reset(new DummyCompactionFilter());
-  cf_descs[0].options.table_factory.reset(NewBlockBasedTableFactory(bbt_opts));
-  cf_descs[0].options.compaction_filter = compaction_filter.get();
-  cf_descs[1].options.table_factory.reset(NewBlockBasedTableFactory(bbt_opts));
-    
-    int main() {
-  // open DB
-  Options options;
-  TransactionDBOptions txn_db_options;
-  options.create_if_missing = true;
-  TransactionDB* txn_db;
-    }
