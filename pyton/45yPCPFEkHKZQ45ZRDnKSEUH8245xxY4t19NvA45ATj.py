e3d5c7880e8b616715a0e5c4eafb91392c503298
SUNsung <weatherius@gmail.com>
2019-08-18 22:28:27 +0300

    Rewrite [45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py] File in: 2019-08-18 22:28:26

--- pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
+++ pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
@@ -1,205 +1,64 @@
 
         
-            def test_how_to_configure(self, shell, config_exists):
-        config_exists.return_value = True
-        assert shell.how_to_configure().can_configure_automatically
+                self.assertFalse(asyncio.iscoroutinefunction(mock.Mock()))
     
-        @pytest.mark.parametrize('side_effect, exception', [
-        ([b'\n'], IndexError), (OSError, OSError)])
-    def test_get_version_error(self, side_effect, exception, shell, Popen):
-        Popen.return_value.stdout.read.side_effect = side_effect
-        with pytest.raises(exception):
-            shell._get_version()
-        assert Popen.call_args[0][0] == ['tcsh', '--version']
-
+        def test_default_data(self):
+        eq = self.assertEqual
+        eq(self.db.guess_type('foo.html'), ('text/html', None))
+        eq(self.db.guess_type('foo.tgz'), ('application/x-tar', 'gzip'))
+        eq(self.db.guess_type('foo.tar.gz'), ('application/x-tar', 'gzip'))
+        eq(self.db.guess_type('foo.tar.Z'), ('application/x-tar', 'compress'))
+        eq(self.db.guess_type('foo.tar.bz2'), ('application/x-tar', 'bzip2'))
+        eq(self.db.guess_type('foo.tar.xz'), ('application/x-tar', 'xz'))
     
-        def _get_version(self):
-        '''Returns the version of the current shell'''
-        proc = Popen(['tcsh', '--version'], stdout=PIPE, stderr=DEVNULL)
-        return proc.stdout.read().decode('utf-8').split()[1]
-
+            The count of unfinished tasks goes up whenever an item is added to the
+        queue. The count goes down whenever a consumer thread calls task_done()
+        to indicate the item was retrieved and all work on it is complete.
     
-    from thefuck.rules.pyenv_no_such_command import get_new_command, match
-from thefuck.types import Command
+            self.assertEqual(results, list(range(N + 1)))
     
+        def subtype_as_add(self, method, subtype, outcome):
+        m, msg_headers, payload = self._make_subtype_test_message(subtype)
+        cm = self._TestSetContentManager()
+        add_method = 'add_attachment' if method=='mixed' else 'add_' + method
+        if outcome == 'raises':
+            self._check_disallowed_subtype_raises(m, method, subtype, add_method)
+            return
+        getattr(m, add_method)('test', content_manager=cm)
+        self.assertEqual(m.get_content_maintype(), 'multipart')
+        self.assertEqual(m.get_content_subtype(), method)
+        if method == subtype or subtype == 'no_content':
+            self.assertEqual(len(m.get_payload()), 1)
+            for name, value in msg_headers:
+                self.assertEqual(m[name], value)
+            part = m.get_payload()[0]
+        else:
+            self.assertEqual(len(m.get_payload()), 2)
+            self._check_make_multipart(m, msg_headers, payload)
+            part = m.get_payload()[1]
+        self.assertEqual(part.get_content_type(), 'text/plain')
+        self.assertEqual(part.get_payload(), 'test')
+        if method=='mixed':
+            self.assertEqual(part['Content-Disposition'], 'attachment')
+        elif method=='related':
+            self.assertEqual(part['Content-Disposition'], 'inline')
+        else:
+            # Otherwise we don't guess.
+            self.assertIsNone(part['Content-Disposition'])
     
-def new_pteredor(probe_nat=True):
-    if os.path.isfile(log_file):
-        try:
-            os.remove(log_file)
-        except Exception as e:
-            xlog.warn('remove %s fail:%r', log_file, e)
+            # asyncio issue #184: Ensure that _StreamProtocol constructor
+        # retrieves the current loop if the loop parameter is not set
+        stream = mock.Mock()
+        protocol = _StreamProtocol(stream, _asyncio_internal=True)
+        self.assertIs(protocol._loop, self.loop)
     
-                    # EOF Transition to accept state?
-                if c == EOF and self.eof[s] >= 0:
-                    #print 'EOF Transition to accept state %d' \
-                    #  % self.accept[self.eof[s]]
-                    return self.accept[self.eof[s]]
+            exp9 = '''PeriodIndex: 3 entries, 2013Q1 to 2013Q3
+Freq: Q-DEC'''
     
+            def random_text(nobs=100):
+            df = []
+            for i in range(nobs):
+                idx = np.random.randint(len(letters), size=2)
+                idx.sort()
     
-def _get_blobs(im, rois, target_scale, target_max_size):
-    '''Convert an image and RoIs within that image into network inputs.'''
-    blobs = {}
-    blobs['data'], im_scale, blobs['im_info'] = \
-        blob_utils.get_image_blob(im, target_scale, target_max_size)
-    if rois is not None:
-        blobs['rois'] = _get_rois_blob(rois, im_scale)
-    return blobs, im_scale
-
-    
-    
-def cityscapes_to_coco_with_rider(cityscapes_id):
-    lookup = {
-        0: 0,  # ... background
-        1: 2,  # bicycle
-        2: 3,  # car
-        3: 1,  # person
-        4: 7,  # train
-        5: 8,  # truck
-        6: 4,  # motorcycle
-        7: 6,  # bus
-        8: 1,  # rider ('person', *rider has human right!*)
-    }
-    return lookup[cityscapes_id]
-    
-    
-def combined_roidb_for_training(dataset_names, proposal_files):
-    '''Load and concatenate roidbs for one or more datasets, along with optional
-    object proposals. The roidb entries are then prepared for use in training,
-    which involves caching certain types of metadata for each roidb entry.
-    '''
-    def get_roidb(dataset_name, proposal_file):
-        ds = JsonDataset(dataset_name)
-        roidb = ds.get_roidb(
-            gt=True,
-            proposal_file=proposal_file,
-            crowd_filter_thresh=cfg.TRAIN.CROWD_FILTER_THRESH
-        )
-        if cfg.TRAIN.USE_FLIPPED:
-            logger.info('Appending horizontally-flipped training examples...')
-            extend_with_flipped_entries(roidb, ds)
-        logger.info('Loaded dataset: {:s}'.format(ds.name))
-        return roidb
-    
-    
-def voc_info(json_dataset):
-    year = json_dataset.name[4:8]
-    image_set = json_dataset.name[9:]
-    devkit_path = get_devkit_dir(json_dataset.name)
-    assert os.path.exists(devkit_path), \
-        'Devkit directory {} not found'.format(devkit_path)
-    anno_path = os.path.join(
-        devkit_path, 'VOC' + year, 'Annotations', '{:s}.xml')
-    image_set_path = os.path.join(
-        devkit_path, 'VOC' + year, 'ImageSets', 'Main', image_set + '.txt')
-    return dict(
-        year=year,
-        image_set=image_set,
-        devkit_path=devkit_path,
-        anno_path=anno_path,
-        image_set_path=image_set_path)
-
-    
-    
-def basic_gn_shortcut(model, prefix, blob_in, dim_in, dim_out, stride):
-    if dim_in == dim_out:
-        return blob_in
-    
-        current = roi_feat
-    for i in range(cfg.FAST_RCNN.NUM_STACKED_CONVS):
-        current = model.ConvGN(
-            current, 'head_conv' + str(i + 1), dim_in, hidden_dim, 3,
-            group_gn=get_group_gn(hidden_dim),
-            stride=1, pad=1,
-            weight_init=('MSRAFill', {}),
-            bias_init=('ConstantFill', {'value': 0.}))
-        current = model.Relu(current, current)
-        dim_in = hidden_dim
-    
-    # array([[ -83.,  -39.,  100.,   56.],
-#        [-175.,  -87.,  192.,  104.],
-#        [-359., -183.,  376.,  200.],
-#        [ -55.,  -55.,   72.,   72.],
-#        [-119., -119.,  136.,  136.],
-#        [-247., -247.,  264.,  264.],
-#        [ -35.,  -79.,   52.,   96.],
-#        [ -79., -167.,   96.,  184.],
-#        [-167., -343.,  184.,  360.]])
-    
-    
-def add_single_scale_rpn_outputs(model, blob_in, dim_in, spatial_scale):
-    '''Add RPN outputs to a single scale model (i.e., no FPN).'''
-    anchors = generate_anchors(
-        stride=1. / spatial_scale,
-        sizes=cfg.RPN.SIZES,
-        aspect_ratios=cfg.RPN.ASPECT_RATIOS
-    )
-    num_anchors = anchors.shape[0]
-    dim_out = dim_in
-    # RPN hidden representation
-    model.Conv(
-        blob_in,
-        'conv_rpn',
-        dim_in,
-        dim_out,
-        kernel=3,
-        pad=1,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
-    model.Relu('conv_rpn', 'conv_rpn')
-    # Proposal classification scores
-    model.Conv(
-        'conv_rpn',
-        'rpn_cls_logits',
-        dim_in,
-        num_anchors,
-        kernel=1,
-        pad=0,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
-    # Proposal bbox regression deltas
-    model.Conv(
-        'conv_rpn',
-        'rpn_bbox_pred',
-        dim_in,
-        4 * num_anchors,
-        kernel=1,
-        pad=0,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
-    
-    
-# octave and aspect fields are only used on RetinaNet. Octave corresponds to the
-# scale of the anchor and aspect denotes which aspect ratio is used in the range
-# of aspect ratios
-FieldOfAnchors = namedtuple(
-    'FieldOfAnchors', [
-        'field_of_anchors', 'num_cell_anchors', 'stride', 'field_size',
-        'octave', 'aspect'
-    ]
-)
-    
-    
-def add_keypoint_rcnn_blobs(
-    blobs, roidb, fg_rois_per_image, fg_inds, im_scale, batch_idx
-):
-    '''Add Mask R-CNN keypoint specific blobs to the given blobs dictionary.'''
-    # Note: gt_inds must match how they're computed in
-    # datasets.json_dataset._merge_proposal_boxes_into_roidb
-    gt_inds = np.where(roidb['gt_classes'] > 0)[0]
-    max_overlaps = roidb['max_overlaps']
-    gt_keypoints = roidb['gt_keypoints']
-    
-            gt_rois = entry['boxes'][gt_inds, :] * scale
-        gt_classes = entry['gt_classes'][gt_inds]
-    
-    
-def test_iloc_nonunique():
-    s = Series([0, 1, 2], index=[0, 1, 0])
-    assert s.iloc[2] == 2
+    jieba.enable_parallel(4)
