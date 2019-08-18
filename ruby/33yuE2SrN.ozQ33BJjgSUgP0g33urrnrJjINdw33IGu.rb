9a9fe646e1a9a63cadd8c295a03521df7c4ad99c
SUNsung <weatherius@gmail.com>
2019-08-18 16:14:31 +0300

    Rewrite [33yuE2SrN.ozQ33BJjgSUgP0g33urrnrJjINdw33IGu.rb] File in: 2019-08-18 16:14:31

--- ruby/33yuE2SrN.ozQ33BJjgSUgP0g33urrnrJjINdw33IGu.rb
+++ ruby/33yuE2SrN.ozQ33BJjgSUgP0g33urrnrJjINdw33IGu.rb
@@ -1,163 +1,139 @@
-require 'benchmark/ips'
-require 'jekyll'
-require 'json'
-    
-          #
-    
-    module Jekyll
-  module Commands
-    class NewTheme < Jekyll::Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:'new-theme') do |c|
-            c.syntax 'new-theme NAME'
-            c.description 'Creates a new Jekyll theme scaffold'
-            c.option 'code_of_conduct', \
-                     '-c', '--code-of-conduct', \
-                     'Include a Code of Conduct. (defaults to false)'
-    
-          command.args.each do |arg|
-        arg_value = arg.value
-        if arg.value_type.to_s.to_sym == :string_closure
-          closure = proc { |string_value| closure_argument_value = string_value }
-          arg_value = closure
-        end
-        parameter_map[arg.name.to_sym] = arg_value
-      end
-    
-              expect(result).to eq('swiftlint lint --path #{path}')
-        end
-    
-    # To avoid 'PR & Runs' for which tests don't pass, we want to make spec errors more visible
-# The code below will run on Circle, parses the results in JSON and posts them to the PR as comment
-containing_dir = ENV['CIRCLE_TEST_REPORTS'] || '.' # for local testing
-file_path = File.join(containing_dir, 'rspec', 'fastlane-junit-results.xml')
-    
-    # The * turns the array into a parameter list
-# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
-# We need to use that, because otherwise invocations like
-# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
-exec(*exec_arr)
 
-    
-      it 'renders the import form' do
-    visit new_scenario_imports_path
-    expect(page).to have_text('Import a Public Scenario')
+        
+          before do
+    # Use a simple client for all data models
+    allow(Spaceship::TestFlight::Base).to receive(:client).and_return(mock_client)
+    allow(mock_client).to receive(:team_id).and_return('')
   end
     
-        it 'understands hl=1,3-4,9' do
-      stub(params).[](:hl) { '1,3-4,9' }
-      expect((1..10).select { |i| highlighted?(i) }).to eq [1, 3, 4, 9]
-    end
+            all_tvos = devices.map do |device|
+          device = device.downcase
+          device.include?('apple tv')
+        end
+        # Return true if all devices are iOS devices
+        return true unless all_tvos.include?(false)
     
-      context '#parse_duration' do
-    it 'works with correct arguments' do
-      expect(Utils.parse_duration('2.days')).to eq(2.days)
-      expect(Utils.parse_duration('2.seconds')).to eq(2)
-      expect(Utils.parse_duration('2')).to eq(2)
-    end
-    
-    describe ConvertWebsiteAgentTemplateForMerge do
-  let :old_extract do
-    {
-      'url' => { 'css' => '#comic img', 'value' => '@src' },
-      'title' => { 'css' => '#comic img', 'value' => '@alt' },
-      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
-    }
-  end
-    
-        it 'updates Agents' last_error_log_at when an error is logged' do
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 3, :outbound_event => events(:jane_website_agent_event))
-      expect(agents(:jane_website_agent).reload.last_error_log_at).to be_nil
-    
-        it 'should generate the correct last checkpoint url' do
-      @checker.options['path'] = 'last_checkpoint/usps/9361289878905919630610'
-      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/last_checkpoint/usps/9361289878905919630610')
-    end
-  end
-    
-        def str_to_byte_pos(pos)
-      @s.string.slice(0, pos).bytesize
-    end
-  end
-end
-    
-      # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is 'scratch space' for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
-    
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
+          def self.find_api_token(params)
+        return if params[:gsp_path]
+        unless params[:api_token].to_s.length > 0
+          Dir['./**/Info.plist'].each do |current|
+            result = Actions::GetInfoPlistValueAction.run(path: current, key: 'Fabric')
+            next unless result
+            next unless result.kind_of?(Hash)
+            params[:api_token] ||= result['APIKey']
+            UI.verbose('found an APIKey in #{current}')
+          end
+        end
       end
-    end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
+    
+          def extract_filters
+        fragments = []
+    
+      describe '#can_update?' do
+    context 'when user can update_group_member' do
+      before do
+        allow(presenter).to receive(:can?).with(user, :update_group_member, presenter).and_return(true)
+      end
+    
+            def clear_cache
+          # No-op
+        end
+    
+      def output_chronic_duration_attribute(source_attribute)
+    value = attributes[source_attribute.to_s]
+    ChronicDuration.output(value, format: :short) if value
   end
 end
 
     
-      gem.licenses      = ['MIT']
+    Gem::Specification.new do |gem|
+  gem.name          = 'capistrano'
+  gem.version       = Capistrano::VERSION
+  gem.authors       = ['Tom Clements', 'Lee Hambley']
+  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
+  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
+  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
+  gem.homepage      = 'http://capistranorb.com/'
     
-    Given(/^(\d+) valid existing releases$/) do |num|
-  a_day = 86_400 # in seconds
-  offset = -(a_day * num.to_i)
-  num.to_i.times do
-    run_vagrant_command('mkdir -p #{TestApp.release_path(TestApp.timestamp(offset))}')
-    offset += a_day
+    World(RemoteCommandHelpers)
+
+    
+      at_exit do
+    if ENV['KEEP_RUNNING']
+      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
+      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
+    else
+      vagrant_cli_command('destroy -f')
+    end
   end
-end
     
-          def echo?
-        (options || {}).fetch(:echo, true)
+        def ask(key, default=nil, options={})
+      question = Question.new(key, default, options)
+      set(key, question)
+    end
+    
+          def response
+        return @response if defined? @response
+    
+          def print_deprecation_warnings_if_applicable
+        if using_default_scm?
+          warn_add_git_to_capfile unless scm_plugin_installed?
+        elsif built_in_scm_name?
+          warn_set_scm_is_deprecated
+        elsif third_party_scm_name?
+          warn_third_party_scm_must_be_upgraded
+        end
+      end
+    
+          attr_reader :validators
+    
+          def untrusted_keys
+        keys - @trusted_keys
+      end
+    
+        it 'returns a flare tag if there are 2 flare tags in the list' do
+      valid_article = create(:article, tags: %w[ama explainlikeimfive])
+      expect(described_class.new(valid_article).tag.name).to eq('explainlikeimfive')
+    end
+  end
+    
+      describe 'POST blocks' do
+    it 'creates block from input data' do
+      post '/blocks', params: {
+        block: {
+          input_css: '.blue { color: blue;}',
+          input_html: 'yo',
+          input_javascript: 'alert('hey')'
+        }
+      }
+      expect(Block.all.size).to eq(1)
+    end
+  end
+    
+        describe 'When it does not find article from id' do
+      it 'does not call the service' do
+        allow(Article).to receive(:find_by)
+        described_class.perform_now('Reaction', 456, milestone_service)
+        expect(milestone_service).not_to have_received(:call)
       end
     end
+    
+        initialize_with { Tmuxinator::Project.load(file) }
   end
 end
 
     
-            def lvalue(key)
-          key.to_s.chomp('=').to_sym
-        end
-      end
-    end
+      subject { instance }
+    
+      describe 'tmux_new_session_command' do
+    let(:command) { '#{executable} new-session -d -s #{session} -n #{window}' }
+    let(:executable) { project.tmux }
+    let(:session) { project.name }
+    let(:window) { project.windows.first.name }
+    
+        def self.create_connection
+      bunny_host = [ 'localhost' ]
+    
+      def new
+    @ip_address = @ip_pool.ip_addresses.build
   end
-end
-
-    
-            if callable_without_parameters?(value_to_evaluate)
-          super(key, assert_valid_later(key, value_to_evaluate), &nil)
-        else
-          assert_valid_now(key, value_to_evaluate)
-          super
-        end
-      end
-    
-    %i(git_strategy hg_strategy svn_strategy).each do |strategy|
-  validate(strategy) do |key, _value|
-    warn(
-      '[Deprecation Warning] #{key} is deprecated and will be removed in '\
-      'Capistrano 3.7.0.\n'\
-      'https://github.com/capistrano/capistrano/blob/master/UPGRADING-3.7.md'
-    )
-  end
-end
-    
-    shared_examples_for 'multiline literal brace layout' do
-  include MultilineLiteralBraceHelper
-    
-          # Calls the given block for each `when` node in the `case` statement.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_when
-        return when_branches.to_enum(__method__) unless block_given?
-    
-          disabled = (switch == 'disable')
