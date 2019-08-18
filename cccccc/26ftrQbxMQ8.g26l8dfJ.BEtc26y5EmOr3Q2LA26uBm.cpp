0e9a2ec182bd1b4385d77ff00c4aae803e8b0fae
SUNsung <weatherius@gmail.com>
2019-08-18 11:35:10 +0300

    Rewrite [26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp] File in: 2019-08-18 11:35:10

--- cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
+++ cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
@@ -1,286 +1,243 @@
 
         
-          const char* tmpdir = getenv('TMPDIR');
-  if (tmpdir == nullptr) {
-    tmpdir = '/tmp';
-  }
+                for (size_t i = 0; i < outputs.size(); i++)
+        {
+            UMat srcBlob = inputs[i];
+            void *src_handle = inputs[i].handle(ACCESS_READ);
+            void *dst_handle = outputs[i].handle(ACCESS_WRITE);
+            if (src_handle != dst_handle)
+            {
+                UMat umat = srcBlob.reshape(1, (int)outShapes[i].size(), &outShapes[i][0]);
+                umat.copyTo(outputs[i]);
+            }
+        }
+        outs.assign(outputs);
     
-    Github Links:
+            // Applies the colormap on a given image.
+        //
+        // This function expects BGR-aligned data of type CV_8UC1 or CV_8UC3.
+        // Throws an error for wrong-aligned lookup table, which must be
+        // of size 256 in the latest OpenCV release (2.3.1).
+        void operator()(InputArray src, OutputArray dst) const;
     
-    OPERATOR_SCHEMA(Glu)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-Applies gated linear unit to the input Tensor X. The output Y is half the size
-of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
-[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
-X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
-)DOC')
-    .Input(0, 'X', '1D input tensor')
-    .Output(0, 'Y', '1D output tensor');
+    #include 'test_precomp.hpp'
     
-    #endif  // TESSERACT_CCSTRUCT_DEBUGPIXA_H_
-
-    
-    
-// Returns the median value of the vector, given that the values are
-// circular, with the given modulus. Values may be signed or unsigned,
-// eg range from -pi to pi (modulus 2pi) or from 0 to 2pi (modulus 2pi).
-// NOTE that the array is shuffled, but the time taken is linear.
-// An assumption is made that most of the values are spread over no more than
-// half the range, but wrap-around is accounted for if the median is near
-// the wrap-around point.
-// Cannot be a member of GenericVector, as it makes heavy used of LLSQ.
-// T must be an integer or float/double type.
-template<typename T> T MedianOfCircularValues(T modulus, GenericVector<T>* v) {
-  LLSQ stats;
-  T halfrange = static_cast<T>(modulus / 2);
-  int num_elements = v->size();
-  for (int i = 0; i < num_elements; ++i) {
-    stats.add((*v)[i], (*v)[i] + halfrange);
-  }
-  bool offset_needed = stats.y_variance() < stats.x_variance();
-  if (offset_needed) {
-    for (int i = 0; i < num_elements; ++i) {
-      (*v)[i] += halfrange;
-    }
-  }
-  int median_index = v->choose_nth_item(num_elements / 2);
-  if (offset_needed) {
-    for (int i = 0; i < num_elements; ++i) {
-      (*v)[i] -= halfrange;
-    }
-  }
-  return (*v)[median_index];
+    static void help()
+{
+    cout << '\n This program demonstrates how to detect compute and match ORB BRISK and AKAZE descriptors \n'
+        'Usage: \n'
+        '  ./matchmethod_orb_akaze_brisk --image1=<image1(basketball1.png as default)> --image2=<image2(basketball2.png as default)>\n'
+        'Press a key when image window is active to change algorithm or descriptor';
 }
     
-      // A hole in the heap exists at hole_index, and we want to fill it with the
-  // given pair. SiftDown sifts the hole downward to the correct position and
-  // returns the destination index without actually putting pair there.
-  int SiftDown(int hole_index, const Pair& pair) {
-    int heap_size = heap_.size();
-    int child;
-    while ((child = LeftChild(hole_index)) < heap_size) {
-      if (child + 1 < heap_size && heap_[child + 1] < heap_[child])
-        ++child;
-      if (heap_[child] < pair) {
-        heap_[hole_index] = heap_[child];
-        hole_index = child;
-      } else {
+    #ifndef OPENCV_LOGGER_DEFINES_HPP
+#define OPENCV_LOGGER_DEFINES_HPP
+    
+        // Changes the log level of the tags with matching name part according
+    // to the specified scope.
+    void setLevelByNamePart(const std::string& namePart, LogLevel level, MatchingScope scope);
+    
+    	return 'Image';
+}
+void ResourceImporterImage::get_recognized_extensions(List<String> *p_extensions) const {
+    
+    
+    {	rest_length = p_rest_length;
+	update();
+	if (get_joint().is_valid())
+		Physics2DServer::get_singleton()->damped_string_joint_set_param(get_joint(), Physics2DServer::DAMPED_STRING_REST_LENGTH, p_rest_length ? p_rest_length : length);
+}
+    
+    	String source_emission_file;
+    
+    	FileAccessNetworkClient *self = (FileAccessNetworkClient *)s;
+    
+    	void _send_rpc(Node *p_from, int p_to, bool p_unreliable, bool p_set, const StringName &p_name, const Variant **p_arg, int p_argcount);
+	bool _send_confirm_path(NodePath p_path, PathSentCache *psc, int p_target);
+    
+    	struct Skeleton : RID_Data {
+		bool use_2d;
+		int size;
+		Vector<float> skel_texture;
+		GLuint texture;
+		SelfList<Skeleton> update_list;
+		Set<RasterizerScene::InstanceBase *> instances; //instances using skeleton
+		Transform2D base_transform_2d;
+		bool use_world_transform;
+		Transform world_transform;
+    }
+    
+    	strings.push_back(fragment_code4.get_data());
+    
+    
+    {	connections->update_tree();
+}
+    
+    void ChannelArguments::SetMaxReceiveMessageSize(int size) {
+  SetInt(GRPC_ARG_MAX_RECEIVE_MESSAGE_LENGTH, size);
+}
+    
+    class ClientChannelStressTest {
+ public:
+  void Run() {
+    Start();
+    // Keep updating resolution for the test duration.
+    gpr_log(GPR_INFO, 'Start updating resolution.');
+    const auto wait_duration =
+        std::chrono::milliseconds(kResolutionUpdateIntervalMs);
+    std::vector<AddressData> addresses;
+    auto start_time = std::chrono::steady_clock::now();
+    while (true) {
+      if (std::chrono::duration_cast<std::chrono::seconds>(
+              std::chrono::steady_clock::now() - start_time)
+              .count() > kTestDurationSec) {
         break;
       }
+      // Generate a random subset of balancers.
+      addresses.clear();
+      for (const auto& balancer_server : balancer_servers_) {
+        // Select each address with probability of 0.8.
+        if (std::rand() % 10 < 8) {
+          addresses.emplace_back(AddressData{balancer_server.port_, true, ''});
+        }
+      }
+      std::shuffle(addresses.begin(), addresses.end(),
+                   std::mt19937(std::random_device()()));
+      SetNextResolution(addresses);
+      std::this_thread::sleep_for(wait_duration);
     }
-    return hole_index;
-  }
-    
-    
-    {  // WARNING! Keep data as the first element! KDPairInc and KDPairDec depend
-  // on the order of these elements so they can downcast pointers appropriately
-  // for use by GenericHeap::Reshuffle.
-  Data data;
-  Key key;
-};
-// Specialization of KDPair to provide operator< for sorting in increasing order
-// and recasting of data pointers for use with DoublePtr.
-template <typename Key, typename Data>
-struct KDPairInc : public KDPair<Key, Data> {
-  KDPairInc() = default;
-  KDPairInc(Key k, Data d) : KDPair<Key, Data>(k, d) {}
-  // Operator< facilitates sorting in increasing order.
-  int operator<(const KDPairInc<Key, Data>& other) const {
-    return this->key < other.key;
-  }
-  // Returns the input Data pointer recast to a KDPairInc pointer.
-  // Just casts a pointer to the first element to a pointer to the whole struct.
-  static KDPairInc* RecastDataPointer(Data* data_ptr) {
-    return reinterpret_cast<KDPairInc*>(data_ptr);
-  }
-};
-// Specialization of KDPair to provide operator< for sorting in decreasing order
-// and recasting of data pointers for use with DoublePtr.
-template <typename Key, typename Data>
-struct KDPairDec : public KDPair<Key, Data> {
-  KDPairDec() = default;
-  KDPairDec(Key k, Data d) : KDPair<Key, Data>(k, d) {}
-  // Operator< facilitates sorting in decreasing order by using operator> on
-  // the key values.
-  int operator<(const KDPairDec<Key, Data>& other) const {
-    return this->key > other.key;
-  }
-  // Returns the input Data pointer recast to a KDPairDec pointer.
-  // Just casts a pointer to the first element to a pointer to the whole struct.
-  static KDPairDec* RecastDataPointer(Data* data_ptr) {
-    return reinterpret_cast<KDPairDec*>(data_ptr);
-  }
-};
-    
-    class QRSequenceGenerator {
- public:
-  // Object is initialized with the size of the output range.
-  explicit QRSequenceGenerator(int N) : N_(N), next_num_(0) {
-    num_bits_ = static_cast<int>(ceil(log(static_cast<double>(N)) / log(2.0)));
+    gpr_log(GPR_INFO, 'Finish updating resolution.');
+    Shutdown();
   }
     }
     
+    static grpc_iomgr_platform_vtable vtable = {
+    iomgr_platform_init,
+    iomgr_platform_flush,
+    iomgr_platform_shutdown,
+    iomgr_platform_shutdown_background_closure,
+    iomgr_platform_is_any_background_poller_thread,
+    iomgr_platform_add_closure_to_background_poller};
     
-    {}  // namespace tesseract
-
+    #define GPR_GLOBAL_CONFIG_DEFINE_INT32(name, default_value, help)         \
+  static char g_env_str_##name[] = #name;                                 \
+  static ::grpc_core::GlobalConfigEnvInt32 g_env_##name(g_env_str_##name, \
+                                                        default_value);   \
+  int32_t gpr_global_config_get_##name() { return g_env_##name.Get(); }   \
+  void gpr_global_config_set_##name(int32_t value) { g_env_##name.Set(value); }
     
     
-    {}  // namespace mongo
-
-    
-    #include 'mongo/db/stats/top.h'
-#include 'mongo/unittest/unittest.h'
-    
-        virtual ~Base() {
-        if (!collection())
-            return;
-        WriteUnitOfWork wunit(&_opCtx);
-        _context.db()->dropCollection(&_opCtx, nss()).transitional_ignore();
-        wunit.commit();
-    }
-    
-            // Set up the count stage
-        auto params = makeCountScanParams(&_opCtx, getIndex(ctx.db(), BSON('a' << 1)));
-        params.startKey = BSON('' << 3);
-        params.startKeyInclusive = false;
-        params.endKey = BSON('' << 7);
-        params.endKeyInclusive = false;
-    
-    #ifdef REGEX_RUN_DEBUG
-    if (fTraceDebug) {
-        printf('MatchAt(startIdx=%ld)\n', startIdx);
-        printf('Original Pattern: \'%s\'\n', CStr(StringFromUText(fPattern->fPattern))());
-        printf('Input String:     \'%s\'\n\n', CStr(StringFromUText(fInputText))());
-    }
-#endif
-    
-    #if !UCONFIG_NO_TRANSLITERATION
-    
-    #include 'unicode/regex.h'
-#include 'unicode/uclean.h'
-#include 'cmemory.h'
-#include 'cstr.h'
-#include 'uassert.h'
-#include 'uhash.h'
-#include 'uvector.h'
-#include 'uvectr32.h'
-#include 'uvectr64.h'
-#include 'regexcmp.h'
-#include 'regeximp.h'
-#include 'regexst.h'
-    
-    U_NAMESPACE_BEGIN
-    
-    U_NAMESPACE_BEGIN
-    
-    #endif
-
-    
-    namespace apollo {
-namespace planning {
-    }
-    }
-    
-    #pragma once
-    
-    namespace apollo {
-namespace planning {
-    }
-    }
-    
-        params.push_back(static_cast<float>(node['world2cam'].size()));
-    
-    TEST_F(StageApproachingParkingSpotTest, Init) {
-  StageApproachingParkingSpot stage_approaching_parking_spot(config_);
-  EXPECT_EQ(stage_approaching_parking_spot.Name(),
-            ScenarioConfig::StageType_Name(config_.stage_type()));
+    {  gpr_setenv(name, value);
+  gpr_unsetenv(name);
+  retrieved_value = gpr_getenv(name);
+  GPR_ASSERT(retrieved_value == nullptr);
 }
     
-    inline void L2Norm(int feat_dim, float *feat_data) {
-  if (feat_dim == 0) {
-    return;
-  }
-  // feature normalization
-  float l2norm = 0.0f;
-  for (int i = 0; i < feat_dim; ++i) {
-    l2norm += feat_data[i] * feat_data[i];
-  }
-  if (l2norm == 0) {
-    float val = 1.f / std::sqrt(static_cast<float>(feat_dim));
-    for (int i = 0; i < feat_dim; ++i) {
-      feat_data[i] = val;
+    
+    {  GPR_ASSERT(nullptr == gpr_memrchr(nullptr, 'a', 0));
+  GPR_ASSERT(nullptr == gpr_memrchr('', 'a', 0));
+  GPR_ASSERT(nullptr == gpr_memrchr('hello', 'b', 5));
+  GPR_ASSERT(0 == strcmp((const char*)gpr_memrchr('hello', 'h', 5), 'hello'));
+  GPR_ASSERT(0 == strcmp((const char*)gpr_memrchr('hello', 'o', 5), 'o'));
+  GPR_ASSERT(0 == strcmp((const char*)gpr_memrchr('hello', 'l', 5), 'lo'));
+}
+    
+    TEST_F(GlobalConfigEnvTest, Int32WithEnvTest) {
+  const char* int32_var_name = 'INT32_VAR';
     }
-  } else {
-    l2norm = std::sqrt(l2norm);
-    for (int i = 0; i < feat_dim; ++i) {
-      feat_data[i] /= l2norm;
+    
+    
+    {}  // namespace grpc
+    
+    
+    {  // The default service has a size limit of the service name.
+  const grpc::string kTooLongServiceName(201, 'x');
+  SendHealthCheckRpc(kTooLongServiceName,
+                     Status(StatusCode::INVALID_ARGUMENT, ''));
+}
+    
+        EXPECT_FALSE(cstream->Read(&response));
+    EXPECT_EQ(request.message(), exp);
+    
+    namespace grpc {
+namespace testing {
+    }
+    }
+    
+     private:
+  void GotTag(void* got_tag, bool ok, bool ignore_ok) {
+    auto it = expectations_.find(got_tag);
+    if (it != expectations_.end()) {
+      if (!ignore_ok) {
+        EXPECT_EQ(it->second, ok);
+      }
+      expectations_.erase(it);
     }
   }
-}
     
-    /// Size of blocks used for POSTing data back to remote endpoints
-CLI_FLAG(uint32,
-         carver_block_size,
-         8192,
-         'Size of blocks used for POSTing data back to remote endpoints');
+    // Force AddServerBuilderPlugin() to be called at static initialization time.
+struct StaticTestPluginInitializer {
+  StaticTestPluginInitializer() { AddTestServerBuilderPlugin(); }
+} static_plugin_initializer_test_;
     
-      /**
-   * @brief Map a function across the set of scheduled queries
-   *
-   * @param predicate is a function which accepts two parameters, the name of
-   * the query and the ScheduledQuery struct of the queries data. predicate
-   * will be called on each currently scheduled query.
-   *
-   * @param blacklisted [optional] return blacklisted queries if true.
-   *
-   * @code{.cpp}
-   *   std::map<std::string, ScheduledQuery> queries;
-   *   Config::get().scheduledQueries(
-   *      ([&queries](std::string name, const ScheduledQuery& query) {
-   *        queries[name] = query;
-   *      }));
-   * @endcode
-   */
-  void scheduledQueries(
-      std::function<void(std::string name, const ScheduledQuery& query)>
-          predicate,
-      bool blacklisted = false) const;
+    TableCache::TableCache(const std::string& dbname, const Options& options,
+                       int entries)
+    : env_(options.env),
+      dbname_(dbname),
+      options_(options),
+      cache_(NewLRUCache(entries)) {}
     
-    FLAG(uint64,
-     pack_refresh_interval,
-     3600,
-     'Cache expiration for a packs discovery queries');
     
-      // Return default value in case of NotFound error
-  Expected<int32_t, DatabaseError> getInt32Or(const std::string& domain,
-                                              const std::string& key,
-                                              const int32_t default_value = 0);
-  Expected<std::string, DatabaseError> getStringOr(
-      const std::string& domain,
-      const std::string& key,
-      const std::string& default_value = '');
+    {}  // namespace leveldb
     
-    template <typename StorageType>
-void InMemoryStorage<StorageType>::put(const std::string& key,
-                                       const StorageType value) {
-  storage_[key] = value;
-}
-    
-    ExpectedSuccess<DatabaseError> RocksdbDatabase::checkDbConnection() {
-  if (db_ == nullptr) {
-    return createError(DatabaseError::DbIsNotOpen, 'Database is closed');
+    Slice FilterBlockBuilder::Finish() {
+  if (!start_.empty()) {
+    GenerateFilter();
   }
-  return Success();
-}
+    }
     
-    #include <boost/noncopyable.hpp>
-    
-      virtual void SetUp() {
-    boost::uuids::uuid uuid = boost::uuids::random_generator()();
-    auto random_name = boost::uuids::to_string(uuid);
-    auto path = boost::filesystem::temp_directory_path().append(random_name);
-    boost::filesystem::create_directory(path);
-    path_ = path.string();
+      // Iterator interface methods
+  bool Valid() const { return valid_; }
+  Slice key() const {
+    assert(Valid());
+    return key_;
   }
+  Slice value() const {
+    assert(Valid());
+    return iter_->value();
+  }
+  // Methods below require iter() != nullptr
+  Status status() const {
+    assert(iter_);
+    return iter_->status();
+  }
+  void Next() {
+    assert(iter_);
+    iter_->Next();
+    Update();
+  }
+  void Prev() {
+    assert(iter_);
+    iter_->Prev();
+    Update();
+  }
+  void Seek(const Slice& k) {
+    assert(iter_);
+    iter_->Seek(k);
+    Update();
+  }
+  void SeekToFirst() {
+    assert(iter_);
+    iter_->SeekToFirst();
+    Update();
+  }
+  void SeekToLast() {
+    assert(iter_);
+    iter_->SeekToLast();
+    Update();
+  }
+    
+        // add values
+    auto res1 = object.emplace('three', 3);
+    null.emplace('A', 'a');
+    null.emplace('B', 'b');
