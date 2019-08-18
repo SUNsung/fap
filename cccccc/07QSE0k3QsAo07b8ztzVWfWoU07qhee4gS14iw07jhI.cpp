26437b0b3d799f9a707e132b825e04e0fc9ff8f7
SUNsung <weatherius@gmail.com>
2019-08-18 09:11:33 +0300

    Rewrite [07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp] File in: 2019-08-18 09:11:32

--- cccccc/07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp
+++ cccccc/07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp
@@ -1,90 +1,212 @@
 
         
-        #include 'src/core/lib/iomgr/iocp_windows.h'
-#include 'src/core/lib/iomgr/iomgr_internal.h'
-#include 'src/core/lib/iomgr/pollset.h'
-#include 'src/core/lib/iomgr/pollset_windows.h'
-#include 'src/core/lib/iomgr/sockaddr_windows.h'
-#include 'src/core/lib/iomgr/socket_windows.h'
-    
-    TEST(GlobalConfigTest, Int32Test) {
-  EXPECT_EQ(0, GPR_GLOBAL_CONFIG_GET(int32_var));
-  GPR_GLOBAL_CONFIG_SET(int32_var, 1024);
-  EXPECT_EQ(1024, GPR_GLOBAL_CONFIG_GET(int32_var));
+        TEST(JavaDocCommentTest, Escaping) {
+  EXPECT_EQ('foo /&#42; bar *&#47; baz', EscapeJavadoc('foo /* bar */ baz'));
+  EXPECT_EQ('foo /&#42;&#47; baz', EscapeJavadoc('foo /*/ baz'));
+  EXPECT_EQ('{&#64;foo}', EscapeJavadoc('{@foo}'));
+  EXPECT_EQ('&lt;i&gt;&amp;&lt;/i&gt;', EscapeJavadoc('<i>&</i>'));
+  EXPECT_EQ('foo&#92;u1234bar', EscapeJavadoc('foo\\u1234bar'));
+  EXPECT_EQ('&#64;deprecated', EscapeJavadoc('@deprecated'));
 }
     
-    #endif  // GRPCPP_CREATE_CHANNEL_H
+    TEST(TemplateUtilTest, TestIdentity) {
+  EXPECT_TRUE(
+      (type_equals_<GOOGLE_NAMESPACE::identity_<int>::type, int>::value));
+  EXPECT_TRUE(
+      (type_equals_<GOOGLE_NAMESPACE::identity_<void>::type, void>::value));
+}
+    
+    using namespace std;
+    
+    // Generate constructors.
+#include 'ipc/struct_constructor_macros.h'
+#include 'content/nw/src/common/common_message_generator.h'
+    
+    namespace nwapi {
+    }
+    
+    Menu::Menu(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+           const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
+  Create(option);
+}
+    
+    class ObjectManager;
+    
+    #include 'base/run_loop.h'
+#include 'base/values.h'
+#include 'base/strings/utf_string_conversions.h'
+#include 'base/message_loop/message_loop_current.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/nw/src/api/menuitem/menuitem.h'
+#include 'content/public/browser/render_frame_host.h'
+#include 'content/public/browser/render_view_host.h'
+#include 'content/public/browser/render_widget_host_view.h'
+#include 'content/public/browser/web_contents.h'
+#include 'extensions/browser/app_window/app_window.h'
+#include 'skia/ext/image_operations.h'
+#include 'ui/aura/client/screen_position_client.h'
+#include 'ui/aura/window.h'
+#include 'ui/aura/window_tree_host.h'
+#include 'ui/events/platform/platform_event_source.h'
+#include 'ui/views/controls/menu/menu_runner.h'
+#include 'ui/views/widget/widget.h'
+#include 'ui/views/focus/focus_manager.h'
+#include 'vector'
+    
+    bool NwClipboardClearSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  clipboard->Clear(ui::CLIPBOARD_TYPE_COPY_PASTE);
+  return true;
+}
+    
+    
+    {
+} // namespace extensions
+#endif
 
     
-    typedef ::grpc_impl::ChannelArguments ChannelArguments;
+      // Return a new numeric id.  May be used by multiple clients who are
+  // sharing the same cache to partition the key space.  Typically the
+  // client will allocate a new id at startup and prepend the id to
+  // its cache keys.
+  virtual uint64_t NewId() = 0;
     
-    CallCredentials::CallCredentials() { g_gli_initializer.summon(); }
+      // Create an object supporting random-access reads from the file with the
+  // specified name.  On success, stores a pointer to the new file in
+  // *result and returns OK.  On failure stores nullptr in *result and
+  // returns non-OK.  If the file does not exist, returns a non-OK
+  // status.  Implementations should return a NotFound status when the file does
+  // not exist.
+  //
+  // The returned file may be concurrently accessed by multiple threads.
+  virtual Status NewRandomAccessFile(const std::string& fname,
+                                     RandomAccessFile** result) = 0;
     
-          response_reader->StartCall();
-      response_reader->Finish(&recv_response, &recv_status, tag(4));
+      WriteOptions write_options;
+  ASSERT_OK(db->Put(write_options, '1', 'b'));
+  ASSERT_OK(db->Put(write_options, '2', 'c'));
+  ASSERT_OK(db->Put(write_options, '3', 'd'));
+  ASSERT_OK(db->Put(write_options, '4', 'e'));
+  ASSERT_OK(db->Put(write_options, '5', 'f'));
     
-    #include <grpc/grpc.h>
-#include <grpc/support/alloc.h>
-#include <grpc/support/log.h>
-#include <grpc/support/time.h>
-#include <grpcpp/channel.h>
-#include <grpcpp/client_context.h>
-#include <grpcpp/create_channel.h>
-#include <grpcpp/server.h>
-#include <grpcpp/server_builder.h>
-#include <grpcpp/server_context.h>
-    
-      str = tree.DumpModel(fmap, true, 'dot');
-  ASSERT_NE(str.find(R'('feat_0')'), std::string::npos);
-  ASSERT_NE(str.find(R'(feat_1<1)'), std::string::npos);
-  ASSERT_NE(str.find(R'(feat_2<2)'), std::string::npos);
-    
-        // ------ From cpu quantile histogram -------.
-    DMLC_DECLARE_FIELD(sparse_threshold).set_range(0, 1.0).set_default(0.2)
-        .describe('percentage threshold for treating a feature as sparse');
-    DMLC_DECLARE_FIELD(enable_feature_grouping).set_lower_bound(0).set_default(0)
-        .describe('if >0, enable feature grouping to ameliorate work imbalance '
-                  'among worker threads');
-    DMLC_DECLARE_FIELD(max_conflict_rate).set_range(0, 1.0).set_default(0)
-        .describe('when grouping features, how many \'conflicts\' to allow.'
-       'conflict is when an instance has nonzero values for two or more features.'
-       'default is 0, meaning features should be strictly complementary.');
-    DMLC_DECLARE_FIELD(max_search_group).set_lower_bound(0).set_default(100)
-        .describe('when grouping features, how much effort to expend to prevent '
-                  'singleton groups. We'll try to insert each feature into existing '
-                  'groups before creating a new group for that feature; to save time, '
-                  'only up to (max_search_group) of existing groups will be '
-                  'considered. If set to zero, ALL existing groups will be examined.');
-    
-    
-    {
-    {void SparsePageWriter::Alloc(std::shared_ptr<SparsePage>* out_page) {
-  CHECK(*out_page == nullptr);
-  if (num_free_buffer_ != 0) {
-    out_page->reset(new SparsePage());
-    --num_free_buffer_;
-  } else {
-    CHECK(qrecycle_.Pop(out_page));
+      // Read the block contents as well as the type/crc footer.
+  // See table_builder.cc for the code that built this structure.
+  size_t n = static_cast<size_t>(handle.size());
+  char* buf = new char[n + kBlockTrailerSize];
+  Slice contents;
+  Status s = file->Read(handle.offset(), n + kBlockTrailerSize, &contents, buf);
+  if (!s.ok()) {
+    delete[] buf;
+    return s;
   }
-}
-}  // namespace data
-}  // namespace xgboost
-    
-      Transform<>::Init(TestTransformRange<bst_float>{},
-	                Range{0, static_cast<Range::DifferenceType>(size)},
-	                TRANSFORM_GPU_RANGE)
-      .Eval(&out_vec, &in_vec);
-  std::vector<bst_float> res = out_vec.HostVector();
-    
-    DMLC_REGISTER_PARAMETER(MyLogisticParam);
+  if (contents.size() != n + kBlockTrailerSize) {
+    delete[] buf;
+    return Status::Corruption('truncated block read');
+  }
     
     
+    {}  // namespace leveldb
+
+    
+    #ifndef STORAGE_LEVELDB_TABLE_ITERATOR_WRAPPER_H_
+#define STORAGE_LEVELDB_TABLE_ITERATOR_WRAPPER_H_
+    
+        task_pack(&buf, data);
+    
+    #if __linux__
+#include <sys/mman.h>
+#endif
+    
+    
+    {    //1
+    ret = p.read(&p, buf, sizeof(buf));
+    if (ret < 0)
     {
-    {// use R's PRNG to replacd
-CustomGlobalRandomEngine::result_type
-CustomGlobalRandomEngine::operator()() {
-  return static_cast<result_type>(
-      std::floor(unif_rand() * CustomGlobalRandomEngine::max()));
+        swSysWarn('read() failed.');
+    }
+    ASSERT_GT(ret, 0);
+    ASSERT_EQ(strcmp('hello world1', buf), 0);
+    //2
+    ret = p.read(&p, buf, sizeof(buf));
+    ASSERT_GT(ret, 0);
+    ASSERT_EQ(strcmp('hello world2', buf), 0);
+    //3
+    ret = p.read(&p, buf, sizeof(buf));
+    ASSERT_GT(ret, 0);
+    ASSERT_EQ(strcmp('hello world3', buf), 0);
 }
-}  // namespace common
-}  // namespace xgboost
+    
+    TEST(coroutine_gethostbyname, resolve_cache_inet4_and_inet6)
+{
+    coro_test([](void *arg)
+    {
+        System::set_dns_cache_capacity(10);
+    }
+    }
+    
+        virtual void onStart() = 0;
+    virtual void onShutdown() = 0;
+    virtual void onWorkerStart(int worker_id) = 0;
+    virtual void onWorkerStop(int worker_id) = 0;
+    virtual void onReceive(int fd, const DataBuffer &data) = 0;
+    virtual void onConnect(int fd) = 0;
+    virtual void onClose(int fd) = 0;
+    virtual void onPacket(const DataBuffer &, ClientInfo &) = 0;
+    virtual void onPipeMessage(int src_worker_id, const DataBuffer &) = 0;
+    virtual void onTask(int, int, const DataBuffer &) = 0;
+    virtual void onFinish(int, const DataBuffer &) = 0;
+    
+            //保存长度值
+        memcpy(ptr, &size, sizeof(size));
+        //在指针末尾保存一个串号
+        memcpy((char*) ptr + size - 4, &(send_pkg.serial_num), sizeof(send_pkg.serial_num));
+    
+        un1.sun_family = AF_UNIX;
+    un2.sun_family = AF_UNIX;
+    
+    static int thread_onTask(swThreadPool *pool, void *task, int task_len)
+{
+    sw_atomic_long_t *n = (sw_atomic_long_t *) task;
+    sw_atomic_fetch_add(n, 1);
+    if (*n == N - 1)
+    {
+        write(_pipe, (void*) n, sizeof(long));
+    }
+    return SW_OK;
+}
+    
+    class RedisQtAdapter : public QObject {
+    }
+    
+    void swoole_mime_type_set(const char *suffix, const char *mime_type)
+{
+    mime_map[string(suffix)] = string(mime_type);
+}
+    
+            /**
+         * Less or equal comparison operator.
+         * @param point Point<T> to be compared.
+         * @result Whether the instance satisfies the condition with respect to point.
+         */
+        inline bool operator<=(const Point<T>& point) const
+        {
+            return area() <= point.area();
+        }
+    
+    /**
+ * Blocking copy operation between iterators and a buffer.
+ * Host to Device.
+ * Uses specified queue.
+ */
+template< typename IteratorType >
+inline cl_int copy( const CommandQueue &queue, IteratorType startIterator, IteratorType endIterator, cl::Buffer &buffer )
+{
+    typedef typename std::iterator_traits<IteratorType>::value_type DataType;
+    cl_int error;
+    
+    size_type length = endIterator-startIterator;
+    size_type byteLength = length*sizeof(DataType);
+    }
