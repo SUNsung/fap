4b17f1820ecbcffc8c0d296d17fa716854c43cf4
SUNsung <weatherius@gmail.com>
2019-08-19 14:58:00 +0300

    Rewrite [25YEf9Xhe3GLc253F.WV34UOsI25fcne6xfIflg254CR.rb] File in: 2019-08-19 14:57:59

--- ruby/25YEf9Xhe3GLc253F.WV34UOsI25fcne6xfIflg254CR.rb
+++ ruby/25YEf9Xhe3GLc253F.WV34UOsI25fcne6xfIflg254CR.rb
@@ -1,118 +1,59 @@
 
         
-                # Initializes the communicator with the machine that we will be
-        # communicating with. This base method does nothing (it doesn't
-        # even store the machine in an instance variable for you), so you're
-        # expected to override this and do something with the machine if
-        # you care about it.
-        #
-        # @param [Machine] machine The machine this instance is expected to
-        #   communicate with.
-        def initialize(machine)
-        end
+                  # Ignore most global objects (found elsewhere)
+          if type == 'Global Objects'
+            entries << [name, node['id']] if name.start_with?('_') || name == 'global'
+            next
+          end
     
-            # Configures the given list of networks on the virtual machine.
-        #
-        # The networks parameter will be an array of hashes where the hashes
-        # represent the configuration of a network interface. The structure
-        # of the hash will be roughly the following:
-        #
-        # {
-        #   type:      :static,
-        #   ip:        '192.168.33.10',
-        #   netmask:   '255.255.255.0',
-        #   interface: 1
-        # }
-        #
-        def configure_networks(networks)
-          raise BaseError, _key: :unsupported_configure_networks
-        end
+        it 'acts like #depends_on' do
+      f = formula 'foo' do
+        url 'foo-1.0'
     
-            # This contains all the provider plugins by name, and returns
-        # the provider class and options.
-        #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :providers
+        it 'allows specifying dependencies before certain version' do
+      spec.uses_from_macos('foo', before: :high_sierra)
     
-            # This returns all the registered provider capabilities.
-        #
-        # @return [Hash]
-        def provider_capabilities
-          results = Hash.new { |h, k| h[k] = Registry.new }
+        it 'allows specifying deprecated options as a Hash from an Array/String to an Array/String' do
+      subject.deprecated_option(['foo1', 'foo2'] => 'bar1', 'foo3' => ['bar2', 'bar3'])
+      expect(subject.deprecated_options).to include(DeprecatedOption.new('foo1', 'bar1'))
+      expect(subject.deprecated_options).to include(DeprecatedOption.new('foo2', 'bar1'))
+      expect(subject.deprecated_options).to include(DeprecatedOption.new('foo3', 'bar2'))
+      expect(subject.deprecated_options).to include(DeprecatedOption.new('foo3', 'bar3'))
+    end
     
-            # This method is called if the underlying machine ID changes. Providers
-        # can use this method to load in new data for the actual backing
-        # machine or to realize that the machine is now gone (the ID can
-        # become `nil`). No parameters are given, since the underlying machine
-        # is simply the machine instance given to this object. And no
-        # return value is necessary.
-        def machine_id_changed
-        end
+        def quarantine
+      return if @quarantine.nil?
+      return unless Quarantine.available?
     
-      # staged_path not available in Installer/Uninstall Stanza, workaround by nesting with preflight/postflight
-  # see https://github.com/Homebrew/homebrew-cask/pull/8887
-  # and https://github.com/Homebrew/homebrew-cask-versions/pull/296
-    
-          def use_identicon
-        @page.wiki.user_icons == 'identicon'
+          unless system curl_executable, '--silent', '--fail', '--output', '/dev/null', package_url
+        package_blob = <<~JSON
+          {'name': '#{bintray_package}',
+           'public_download_numbers': true,
+           'public_stats': true}
+        JSON
+        curl '--silent', '--fail', '--user', '#{bintray_user}:#{bintray_key}',
+             '--header', 'Content-Type: application/json',
+             '--data', package_blob, bintray_repo_url
+        puts
       end
     
-        end
+        ohai 'Patching #{name}'
+    patches.each(&:apply)
   end
-end
-
     
-    # Commit file to wiki, overwriting previous versions of that file
-def commit_test_file(wiki, dir, filename, ext, content)
-  committer = Gollum::Committer.new(wiki, :message => 'Added testfile', :parent  => wiki.repo.head.commit)
-  committer.add_to_index(dir, filename, ext, content, true)
-    committer.after_commit do |committer, sha|
-      wiki.clear_cache
-      committer.update_working_dir(dir, filename, ext)
+        def test_module_const_get_toplevel
+      bug12089 = '[ruby-dev:49498] [Bug #12089]'
+      name = noninterned_name('A')
+      e = assert_not_interned_error(Object, :const_get, name)
+      assert_equal(name, e.name)
+      assert_not_match(/Object::/, e.message, bug12089)
     end
-  committer.commit
-end
-
     
-      test 'clean path with double leading slash' do
-    assert_equal '/Mordor', clean_path('//Mordor')
-  end
-end
-    
-        # Extract the path string that Gollum::Wiki expects
-    def extract_path(file_path)
-      return nil if file_path.nil?
-      last_slash = file_path.rindex('/')
-      if last_slash
-        file_path[0, last_slash]
+        ScratchPad.record []
+    while true
+      begin
+        ScratchPad << enum.next
+      rescue StopIteration
+        break
       end
     end
-    
-      s.require_paths = %w[lib]
-    
-    # Set ruby to UTF-8 mode
-# This is required for Ruby 1.8.7 which gollum still supports.
-$KCODE = 'U' if RUBY_VERSION[0, 3] == '1.8'
-    
-      private
-    
-      def update
-    if @ip_address.update(safe_params)
-      redirect_to_with_json [:edit, @ip_pool]
-    else
-      render_form_errors 'edit', @ip_address
-    end
-  end
-    
-      def index
-    if @server
-      @ip_pool_rules = @server.ip_pool_rules
-    else
-      @ip_pool_rules = organization.ip_pool_rules
-    end
-  end
-    
-      def persist
-    auth_session.persist! if logged_in?
-    render :plain => 'OK'
-  end
