7dd263752ea95b10ca0cedea7a3a65b6244da9cc
SUNsung <weatherius@gmail.com>
2019-08-19 13:08:15 +0300

    Rewrite [30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp] File in: 2019-08-19 13:08:14

--- cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
+++ cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
@@ -1,118 +1,193 @@
 
         
-          // Verify outputs.
-  uint8_t* output_tensor = static_cast<uint8_t*>(stage.GetOutputs()->at(0));
-  for (int i = 0; i < kTotalElements; i++) {
-    EXPECT_EQ(output_tensor[i], static_cast<uint8_t>(6));
+        #endif
+
+    
+    
+    {  // Create an availability attribute for each observed platform and add
+  // to ToDecl.
+  DeclAttributes &Attrs = ToDecl->getAttrs();
+  for (auto &Pair : Inferred) {
+    auto *Attr = createAvailableAttr(Pair.first, Pair.second, Context);
+    Attrs.add(Attr);
   }
-    
-    template <typename T>
-struct MatrixDiagPart<CPUDevice, T> {
-  static void Compute(OpKernelContext* context, const CPUDevice& device,
-                      typename TTypes<T, 3>::ConstTensor& input,
-                      typename TTypes<T>::Tensor& output,
-                      const Eigen::Index lower_diag_index,
-                      const Eigen::Index upper_diag_index,
-                      const Eigen::Index max_diag_len, const T padding_value) {
-    // 10 in cost_per_batch is from existing heuristic.
-    // TODO(penporn): Tune for the best constant in cost_per_batch.
-    const Eigen::Index num_diags = upper_diag_index - lower_diag_index + 1;
-    const Eigen::Index output_elements_in_batch = num_diags * max_diag_len;
-    const Eigen::Index cost_per_batch = 10 * output_elements_in_batch;
-    const Eigen::Index num_batches = input.dimension(0);
-    const Eigen::Index num_rows = input.dimension(1);
-    const Eigen::Index num_cols = input.dimension(2);
-    }
-    }
-    
-    
-    {  return metrics;
 }
     
-    // A list of inputs of a given node of the TensorFlow/Eager graph.
-class OpInputs {
- public:
-  explicit OpInputs(const TfLiteIntArray* indexes) {
-    for (int index : TfLiteIntArrayView(indexes)) {
-      inputs_.push_back(index);
-    }
-    forwardable_.resize(inputs_.size());
-  }
-  ~OpInputs() {}
-    }
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-        OP_REQUIRES(
-        ctx, TensorShapeUtils::IsVector(shape_tensor.shape()),
-        errors::InvalidArgument('Input shape should be a vector, got shape: ',
-                                shape_tensor.shape().DebugString()));
-    int32 num_batches = shape_tensor.flat<int32>()(0);
-    
-    static constexpr int kMaxIterations = 1000;
-    
-    
-    
-    SECP256K1_INLINE static void secp256k1_fe_sqr_inner(uint64_t *r, const uint64_t *a) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            rcx:rbx = d
- *            r10-r14 = a0-a4
- *            r15     = M (0xfffffffffffff)
- *            rdi     = r
- *            rsi     = a / t?
- */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
-    'movq $0xfffffffffffff,%%r15\n'
+    /// Measure the relative offset between two pointers. This measures
+/// (referent - base) using wrapping arithmetic. The result is truncated if
+/// Offset is smaller than a pointer, with an assertion that the
+/// pre-truncation result is a sign extension of the truncated result.
+template<typename Offset, typename A, typename B>
+static inline Offset measureRelativeOffset(A *referent, B *base) {
+  static_assert(std::is_integral<Offset>::value &&
+                std::is_signed<Offset>::value,
+                'offset type should be signed integer');
     }
     
-    /** Encode a Bech32 string. Returns the empty string in case of failure. */
-std::string Encode(const std::string& hrp, const std::vector<uint8_t>& values);
+    /// A value for `SymbolNamespace` which indicates that this type came
+/// from a C `typedef` that was imported as a distinct type instead
+/// of a `typealias`.  This can happen for reasons like:
+///
+/// - the `typedef` was declared with the `swift_wrapper` attribute
+/// - the `typedef` is a CF type
+constexpr static const char CTypedef[] = 't';
     
-        // Erase the data in small chunks
-    always_true_key_tester_t key_tester;
-    const uint64_t max_erased_per_pass = 100;
-    for (continue_bool_t done_erasing = continue_bool_t::CONTINUE;
-         done_erasing == continue_bool_t::CONTINUE;) {
-        scoped_ptr_t<txn_t> txn;
-        scoped_ptr_t<real_superblock_t> superblock;
+    #include 'swift/Basic/LLVM.h'
+#include 'swift/Basic/Range.h'
+#include 'llvm/ADT/Hashing.h'
+#include 'llvm/Support/MD5.h'
+#include 'llvm/Support/MemoryBuffer.h'
+#include 'llvm/Support/YAMLParser.h'
+#include 'llvm/Support/YAMLTraits.h'
+#include 'llvm/Support/raw_ostream.h'
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+    
+    namespace swift {
+struct SupplementaryOutputPaths {
+  /// The path to which we should emit an Objective-C header for the module.
+  ///
+  /// Currently only makes sense when the compiler has whole module knowledge.
+  /// The modes for which it makes sense incuide both WMO and the 'merge
+  /// modules' job that happens after the normal compilation jobs. That's where
+  /// the header is emitted in single-file mode, since it needs whole-module
+  /// information.
+  ///
+  /// \sa swift::printAsObjC
+  std::string ObjCHeaderOutputPath;
+    }
     }
     
-    #if GTEST_HAS_GLOBAL_WSTRING
-  // Converts the given wide string to a narrow string using the UTF-8
-  // encoding, and streams the result to this Message object.
-  Message& operator <<(const ::wstring& wstr);
-#endif  // GTEST_HAS_GLOBAL_WSTRING
+    namespace {
+    }
     
-    #if GTEST_HAS_TYPED_TEST
+    REGISTER_CPU_OPERATOR(
+    MergeMultiMapFeatureTensors,
+    MergeMultiMapFeatureTensorsOp<CPUContext>);
+OPERATOR_SCHEMA(MergeMultiMapFeatureTensors)
+    .SetDoc(
+        'Merge given multi-feature tensors with map features into one.' + doc)
+    .NumInputs([](int n) { return n >= 5 && n % 5 == 0; })
+    .NumOutputs(5)
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_keys', '.keys')
+    .Input(2, 'in1_values_lengths', '.values.lengths')
+    .Input(3, 'in1_values_keys', '.values.keys')
+    .Input(4, 'in1_values_values', '.values.values')
+    .Output(0, 'out_lengths', '.lengths')
+    .Output(1, 'out_keys', '.keys')
+    .Output(2, 'out_values_lengths', '.values_lengths')
+    .Output(3, 'out_values_keys', '.values.keys')
+    .Output(4, 'out_values_values', '.values.values');
     
-    // Macros for testing exceptions.
-//
-//    * {ASSERT|EXPECT}_THROW(statement, expected_exception):
-//         Tests that the statement throws the expected exception.
-//    * {ASSERT|EXPECT}_NO_THROW(statement):
-//         Tests that the statement doesn't throw any exception.
-//    * {ASSERT|EXPECT}_ANY_THROW(statement):
-//         Tests that the statement throws an exception.
+    X before running op:
+[[ 3.813361   -1.319647    5.2089314  -4.931328    0.6218652 ]
+ [ 7.2757645   5.5552588   5.785643   -2.4790506  -0.41400087]
+ [ 1.1541046  -6.933266    3.3754056   1.6569928  -1.7670316 ]
+ [-3.4932013   4.891472    1.5530115  -3.2443287  -4.605099  ]
+ [-4.574543   -7.360948    5.91305    -8.196495   -5.357458  ]]
+X after running op:
+[[ 3. -2.  5. -5.  0.]
+ [ 7.  5.  5. -3. -1.]
+ [ 1. -7.  3.  1. -2.]
+ [-4.  4.  1. -4. -5.]
+ [-5. -8.  5. -9. -6.]]
+    
+    OPERATOR_SCHEMA(Glu)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .SetDoc(R'DOC(
+Applies gated linear unit to the input Tensor X. The output Y is half the size
+of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
+[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
+X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
+)DOC')
+    .Input(0, 'X', '1D input tensor')
+    .Output(0, 'Y', '1D output tensor');
+    
+    bool VideoStreamPlaybackGDNative::is_playing() const {
+	return playing;
+}
+    
+    	static _FORCE_INLINE_ uint32_t hash(const String &p_string) { return p_string.hash(); }
+	static _FORCE_INLINE_ uint32_t hash(const char *p_cstr) { return hash_djb2(p_cstr); }
+	static _FORCE_INLINE_ uint32_t hash(const uint64_t p_int) { return hash_one_uint64(p_int); }
+    
+    	bool exit_thread;
+    
+    	if (port_in != 0) {
+		MIDIPortDispose(port_in);
+		port_in = 0;
+	}
+    
+    public:
+	virtual Error open();
+	virtual void close();
+    
+    #endif // AUDIO_STREAM_EDITOR_PLUGIN_H
+
+    
+    #endif // POWER_ANDROID_H
+
+    
+    void ImGui_ImplOpenGL3_DestroyFontsTexture()
+{
+    if (g_FontTexture)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        glDeleteTextures(1, &g_FontTexture);
+        io.Fonts->TexID = 0;
+        g_FontTexture = 0;
+    }
+}
+    
+            ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+        ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+        ImGui::Checkbox('Another Window', &show_another_window);
+    
+                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+    
+        // Initialize Direct3D
+    if (!CreateDeviceD3D(hwnd))
+    {
+        CleanupDeviceD3D();
+        ::UnregisterClass(wc.lpszClassName, wc.hInstance);
+        return 1;
+    }
+    
+        // Poll and handle events (inputs, window resize, etc.)
+    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+    // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        ImGui_ImplSDL2_ProcessEvent(&event);
+        // Capture events here, based on io.WantCaptureMouse and io.WantCaptureKeyboard
+    }
+    
+    static void ImGui_ImplDX9_SetupRenderState(ImDrawData* draw_data)
+{
+    // Setup viewport
+    D3DVIEWPORT9 vp;
+    vp.X = vp.Y = 0;
+    vp.Width = (DWORD)draw_data->DisplaySize.x;
+    vp.Height = (DWORD)draw_data->DisplaySize.y;
+    vp.MinZ = 0.0f;
+    vp.MaxZ = 1.0f;
+    g_pd3dDevice->SetViewport(&vp);
+    }
+    
+    // Implemented features:
+//  [X] Platform: Clipboard support.
+//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad'.
+//  [x] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'. FIXME: 3 cursors types are missing from GLFW.
+//  [X] Platform: Keyboard arrays indexed using GLFW_KEY_* codes, e.g. ImGui::IsKeyPressed(GLFW_KEY_SPACE).
     
     
-    {  GTEST_DISALLOW_ASSIGN_(NativeArray);
-};
-    
-      // Smart pointer members.
-  void reset(T* ptr = NULL) {
-    depart();
-    capture(ptr);
-  }
-  T* get() const { return value_; }
-  T* operator->() const { return value_; }
-  T& operator*() const { return *value_; }
+    {    return 0;
+}
