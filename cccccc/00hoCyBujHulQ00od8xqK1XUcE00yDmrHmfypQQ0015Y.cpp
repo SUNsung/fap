a03033e3ee0dc66740c35b101367ba7a81197775
SUNsung <weatherius@gmail.com>
2019-08-19 11:39:08 +0300

    Rewrite [00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp] File in: 2019-08-19 11:39:07

--- cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
+++ cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
@@ -1,126 +1,371 @@
 
         
-        #define IPC_MESSAGE_START ShellMsgStart
+          // Creates a CertificateManagerModel. The model will be passed to the callback
+  // when it is ready. The caller must ensure the model does not outlive the
+  // |browser_context|.
+  static void Create(content::BrowserContext* browser_context,
+                     const CreationCallback& callback);
     
+    #ifndef CHROME_BROWSER_UI_VIEWS_FRAME_GLOBAL_MENU_BAR_REGISTRAR_X11_H_
+#define CHROME_BROWSER_UI_VIEWS_FRAME_GLOBAL_MENU_BAR_REGISTRAR_X11_H_
     
-bool MenuDelegate::GetAcceleratorForCommandId(
-      int command_id,
-      ui::Accelerator* accelerator) const {
-  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  if (!item)
-    return false;
-    }
-    
-       bool IsCommandIdChecked(int command_id) const override;
-   bool IsCommandIdEnabled(int command_id) const override;
-    
-    #include 'base/logging.h'
-#include 'base/strings/string_util.h'
-#include 'base/values.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/nw/src/api/menu/menu.h'
-    
-    #include 'base/files/file_path.h'
-#include 'base/strings/string_util.h'
-#include 'base/strings/utf_string_conversions.h'
-#include 'base/threading/thread_restrictions.h'
-#include 'base/values.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/nw/src/api/menu/menu.h'
-#include 'content/nw/src/nw_base.h'
-#include 'content/nw/src/nw_content.h'
-#include 'content/nw/src/nw_package.h'
-#include 'ui/base/accelerators/accelerator.h'
-#include 'ui/gfx/image/image_skia_operations.h'
-#include 'ui/events/event_constants.h'//for modifier key code
-#include 'base/logging.h'
-    
-    namespace extensions {
+    // Class template for extracting and storing single argument for callback
+// at position |index|.
+template <size_t index, typename ArgType>
+struct ArgumentHolder {
+  using ArgLocalType = typename CallbackParamTraits<ArgType>::LocalType;
     }
     
     
-    {} // namespace extensions
-#endif
-
-    
-    
-    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.destroy', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwObjDestroyFunction);
+    { private:
+  scoped_refptr<RefCountedPersistent<v8::Object>> handle_;
 };
     
-        std::unique_ptr<base::ListValue> args = nwapi::nw__screen::OnSourceAdded::Create(
-      src.id.ToString(),
-      base::UTF16ToUTF8(src.name),
-      index,
-      type,
-      src.id.type == content::DesktopMediaID::TYPE_SCREEN && GetPrimaryMonitorIndex() == index);
+      std::string current_dir = tokens[1];
+  // Remove the first two tokens.  The remaining tokens should be the command
+  // line argv array.
+  tokens.erase(tokens.begin());
+  tokens.erase(tokens.begin());
     
-    TEST(MovableMessageTest, SelfMoveAssignment) {
-  // The `self` reference is necessary to defeat -Wself-move.
-  protobuf_unittest::TestAllTypes message, &self = message;
-  TestUtil::SetAllFields(&message);
-  message = std::move(self);
-  TestUtil::ExpectAllFieldsSet(message);
+    bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv);
+    
+    #include 'ImfInt64.h'
+#include 'ImfNamespace.h'
+#include 'ImfExport.h'
+    
+    
+template <>
+const char *
+FloatVectorAttribute::staticTypeName ()
+{
+    return 'floatvector';
 }
     
-    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
     
-    int main(int argc, const char** argv) {
-  FileInputStream fin(STDIN_FILENO);
-  GzipInputStream in(&fin);
+OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_EXIT
+    
+    
+    {    return fps;
+}
+    
+    #ifndef IMFGENERICINPUTFILE_H_
+#define IMFGENERICINPUTFILE_H_
+    
+    
+    {	os.write (s.data(), int(s.length()));
     }
     
-    class Proto3DataStripper : public DataStripper {
- private:
-  virtual bool ShouldBeClear(const FieldDescriptor *field) {
-    return field->type() == FieldDescriptor::TYPE_GROUP ||
-           field->is_extension();
+        IMF_EXPORT
+    Iterator			find (const char name[]);
+    IMF_EXPORT
+    ConstIterator		find (const char name[]) const;
+    
+    
+//
+// DECODING TABLE BUILDING
+//
+    
+    #include 'ImathFun.h'
+#include 'IlmThreadMutex.h'
+#include 'Iex.h'
+#include 'half.h'
+    
+    #include 'ImfMultiPartInputFile.h'
+    
+    #ifndef IMFINPUTPART_H_
+#define IMFINPUTPART_H_
+    
+    
+OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_EXIT
+    
+    OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_ENTER
+    
+    // Computes the Otsu threshold(s) for the given image rectangle, making one
+// for each channel. Each channel is always one byte per pixel.
+// Returns an array of threshold values and an array of hi_values, such
+// that a pixel value >threshold[channel] is considered foreground if
+// hi_values[channel] is 0 or background if 1. A hi_value of -1 indicates
+// that there is no apparent foreground. At least one hi_value will not be -1.
+// Delete thresholds and hi_values with delete [] after use.
+// The return value is the number of channels in the input image, being
+// the size of the output thresholds and hi_values arrays.
+int OtsuThreshold(Pix* src_pix, int left, int top, int width, int height,
+                  int** thresholds, int** hi_values);
+    
+    // A useful base struct to facilitate the common operation of sorting a vector
+// of simple or smart-pointer data using a separate key. Similar to STL pair.
+template <typename Key, typename Data>
+struct KDPair {
+  KDPair() = default;
+  KDPair(Key k, Data d) : data(d), key(k) {}
+    }
+    
+    // Provides access to the ShapeTable that this classifier works with.
+const ShapeTable* TessClassifier::GetShapeTable() const {
+  return classify_->shape_table();
+}
+// Provides access to the UNICHARSET that this classifier works with.
+// Only needs to be overridden if GetShapeTable() can return nullptr.
+const UNICHARSET& TessClassifier::GetUnicharset() const {
+  return classify_->unicharset;
+}
+    
+    template <typename Dtype>
+void hdf5_load_nd_dataset(
+    hid_t file_id, const char* dataset_name_, int min_dim, int max_dim,
+    Blob<Dtype>* blob, bool reshape = false);
+    
+    template <typename Dtype>
+void HDF5OutputLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  CHECK_GE(bottom.size(), 2);
+  CHECK_EQ(bottom[0]->num(), bottom[1]->num());
+  data_blob_.Reshape(bottom[0]->num(), bottom[0]->channels(),
+                     bottom[0]->height(), bottom[0]->width());
+  label_blob_.Reshape(bottom[1]->num(), bottom[1]->channels(),
+                     bottom[1]->height(), bottom[1]->width());
+  const int data_datum_dim = bottom[0]->count() / bottom[0]->num();
+  const int label_datum_dim = bottom[1]->count() / bottom[1]->num();
+    }
+    
+    // Return the current learning rate. The currently implemented learning rate
+// policies are as follows:
+//    - fixed: always return base_lr.
+//    - step: return base_lr * gamma ^ (floor(iter / step))
+//    - exp: return base_lr * gamma ^ iter
+//    - inv: return base_lr * (1 + gamma * iter) ^ (- power)
+//    - multistep: similar to step but it allows non uniform steps defined by
+//      stepvalue
+//    - poly: the effective learning rate follows a polynomial decay, to be
+//      zero by the max_iter. return base_lr (1 - iter/max_iter) ^ (power)
+//    - sigmoid: the effective learning rate follows a sigmod decay
+//      return base_lr ( 1/(1 + exp(-gamma * (iter - stepsize))))
+//
+// where base_lr, max_iter, gamma, step, stepvalue and power are defined
+// in the solver parameter protocol buffer, and iter is the current iteration.
+template <typename Dtype>
+Dtype SGDSolver<Dtype>::GetLearningRate() {
+  Dtype rate;
+  const string& lr_policy = this->param_.lr_policy();
+  if (lr_policy == 'fixed') {
+    rate = this->param_.base_lr();
+  } else if (lr_policy == 'step') {
+    CHECK_GT(this->param_.stepsize(), 0);
+    this->current_step_ = this->iter_ / this->param_.stepsize();
+    CHECK_GE(this->param_.gamma(), 0);
+    rate = this->param_.base_lr() *
+        pow(this->param_.gamma(), this->current_step_);
+  } else if (lr_policy == 'exp') {
+    CHECK_GE(this->param_.gamma(), 0);
+    rate = this->param_.base_lr() * pow(this->param_.gamma(), this->iter_);
+  } else if (lr_policy == 'inv') {
+    CHECK_GE(this->param_.gamma(), 0);
+    rate = this->param_.base_lr() *
+        pow(Dtype(1) + this->param_.gamma() * this->iter_,
+            - this->param_.power());
+  } else if (lr_policy == 'multistep') {
+    if (this->current_step_ < this->param_.stepvalue_size() &&
+          this->iter_ >= this->param_.stepvalue(this->current_step_)) {
+      this->current_step_++;
+      LOG(INFO) << 'MultiStep Status: Iteration ' <<
+      this->iter_ << ', step = ' << this->current_step_;
+    }
+    CHECK_GE(this->param_.gamma(), 0);
+    rate = this->param_.base_lr() *
+        pow(this->param_.gamma(), this->current_step_);
+  } else if (lr_policy == 'poly') {
+    rate = this->param_.base_lr() * pow(Dtype(1.) -
+        (Dtype(this->iter_) / Dtype(this->param_.max_iter())),
+        this->param_.power());
+  } else if (lr_policy == 'sigmoid') {
+    CHECK_GE(this->param_.gamma(), 0);
+    CHECK_GT(this->param_.stepsize(), 0);
+    rate = this->param_.base_lr() * (Dtype(1.) /
+        (Dtype(1.) + exp(-this->param_.gamma() * (Dtype(this->iter_) -
+          Dtype(this->param_.stepsize())))));
+  } else {
+    LOG(FATAL) << 'Unknown learning rate policy: ' << lr_policy;
   }
-};
+  return rate;
+}
     
-    #include 'google/protobuf/message.h'
-#include 'google/protobuf/descriptor.h'
-#include 'google/protobuf/descriptor.pb.h'
+    TYPED_TEST_CASE(HDF5DataLayerTest, TestDtypesAndDevices);
     
-    namespace HPHP {
+    // Verifies format of data stored in HDF5 file and reshapes blob accordingly.
+template <typename Dtype>
+void hdf5_load_nd_dataset_helper(
+    hid_t file_id, const char* dataset_name_, int min_dim, int max_dim,
+    Blob<Dtype>* blob, bool reshape) {
+  // Verify that the dataset exists.
+  CHECK(H5LTfind_dataset(file_id, dataset_name_))
+      << 'Failed to find HDF5 dataset ' << dataset_name_;
+  // Verify that the number of dimensions is in the accepted range.
+  herr_t status;
+  int ndims;
+  status = H5LTget_dataset_ndims(file_id, dataset_name_, &ndims);
+  CHECK_GE(status, 0) << 'Failed to get dataset ndims for ' << dataset_name_;
+  CHECK_GE(ndims, min_dim);
+  CHECK_LE(ndims, max_dim);
     }
     
+    #include 'caffe/layers/neuron_layer.hpp'
     
-    {///////////////////////////////////////////////////////////////////////////////
+      LOG(INFO) << 'Writing Training data';
+  for (int fileid = 0; fileid < kCIFARTrainBatches; ++fileid) {
+    // Open files
+    LOG(INFO) << 'Training Batch ' << fileid + 1;
+    string batchFileName = input_folder + '/data_batch_'
+      + caffe::format_int(fileid+1) + '.bin';
+    std::ifstream data_file(batchFileName.c_str(),
+        std::ios::in | std::ios::binary);
+    CHECK(data_file) << 'Unable to open train file #' << fileid + 1;
+    for (int itemid = 0; itemid < kCIFARBatchSize; ++itemid) {
+      read_image(&data_file, &label, str_buffer);
+      datum.set_label(label);
+      datum.set_data(str_buffer, kCIFARImageNBytes);
+      string out;
+      CHECK(datum.SerializeToString(&out));
+      txn->Put(caffe::format_int(fileid * kCIFARBatchSize + itemid, 5), out);
+    }
+  }
+  txn->Commit();
+  train_db->Close();
+    
+      virtual inline const char* type() const { return 'Pooling'; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int MinTopBlobs() const { return 1; }
+  // MAX POOL layers can output an extra top blob for the mask;
+  // others can only output the pooled inputs.
+  virtual inline int MaxTopBlobs() const {
+    return (this->layer_param_.pooling_param().pool() ==
+            PoolingParameter_PoolMethod_MAX) ? 2 : 1;
+  }
+    
+    /**
+ * @brief Applies common transformations to the input data, such as
+ * scaling, mirroring, substracting the image mean...
+ */
+template <typename Dtype>
+class DataTransformer {
+ public:
+  explicit DataTransformer(const TransformationParameter& param, Phase phase);
+  virtual ~DataTransformer() {}
+    }
+    
+      void FailHijackedRecvMessage() override {
+    GPR_CODEGEN_ASSERT(hooks_[static_cast<size_t>(
+        experimental::InterceptionHookPoints::PRE_RECV_MESSAGE)]);
+    *got_message_ = false;
+  }
+    
+    void ChannelArguments::SetCompressionAlgorithm(
+    grpc_compression_algorithm algorithm) {
+  SetInt(GRPC_COMPRESSION_CHANNEL_DEFAULT_ALGORITHM, algorithm);
 }
+    
+    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
+  for (unsigned int i = 0; i < args_.size(); i++) {
+    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
+      return args_[i].value.string;
+    }
+  }
+  return '';
+}
+    
+    static bool iomgr_platform_is_any_background_poller_thread(void) {
+  return false;
+}
+    
+      socket = grpc_winsocket_create(sock, 'client');
+  info = &socket->write_info;
+  success = ConnectEx(sock, (grpc_sockaddr*)&addr->addr, (int)addr->len, NULL,
+                      0, NULL, &info->overlapped);
+    
+    void LogParsingError(const char* name, const char* value) {
+  char* error_message;
+  gpr_asprintf(&error_message,
+               'Illegal value '%s' specified for environment variable '%s'',
+               value, name);
+  (*g_global_config_env_error_func)(error_message);
+  gpr_free(error_message);
+}
+    
+    #include 'src/core/lib/gprpp/global_config_generic.h'
+#include 'src/core/lib/gprpp/memory.h'
+    
+    namespace {
+    }
+    
+    #ifndef NDEBUG
+#define GRPC_COMBINER_DEBUG_ARGS \
+  , const char *file, int line, const char *reason
+#define GRPC_COMBINER_REF(combiner, reason) \
+  grpc_combiner_ref((combiner), __FILE__, __LINE__, (reason))
+#define GRPC_COMBINER_UNREF(combiner, reason) \
+  grpc_combiner_unref((combiner), __FILE__, __LINE__, (reason))
+#else
+#define GRPC_COMBINER_DEBUG_ARGS
+#define GRPC_COMBINER_REF(combiner, reason) grpc_combiner_ref((combiner))
+#define GRPC_COMBINER_UNREF(combiner, reason) grpc_combiner_unref((combiner))
+#endif
+    
+      std::shared_ptr<grpc::Channel> CreateChannelImpl(
+      const string& target, const grpc::ChannelArguments& args) override {
+    return CreateChannelWithInterceptors(
+        target, args,
+        std::vector<std::unique_ptr<
+            experimental::ClientInterceptorFactoryInterface>>());
+  }
+    
+      // Test all rpc methods.
+  void TestAllMethods() {
+    SendEcho();
+    SendSimpleClientStreaming();
+    SendSimpleServerStreaming();
+    SendBidiStreaming();
+  }
+    
+    #ifdef GRPC_POSIX_SOCKET
+// Thread-local variable to so that only polls from this test assert
+// non-blocking (not polls from resolver, timer thread, etc)
+GPR_TLS_DECL(g_is_nonblocking_test);
+    
+      DHTTaskFactory* taskFactory_;
+    
+    
+    {
+    {    nodes.push_back(node);
+  }
+  localNode_ = localNode;
+  nodes_ = nodes;
+  A2_LOG_INFO('DHT routing table was loaded successfully');
+}
+    
+    #include <vector>
+#include <deque>
+#include <memory>
+    
+      virtual std::shared_ptr<DHTTask>
+  createPeerAnnounceTask(const unsigned char* infoHash) = 0;
+    
+    
+    {} // namespace aria2
 
     
-    #ifndef incl_HPHP_OUTPUT_FILE_H_
-#define incl_HPHP_OUTPUT_FILE_H_
+    #endif // D_DHT_TASK_QUEUE_H
+
     
     
-    {  StructuredLogEntry cols;
-  fillCols(cols);
-  cols.setStr('event_name', event);
-  cols.setInt('priority', priority);
-  StructuredLog::log('hhvm_perf_warning', cols);
+    {  void updateTokenSecret();
+};
+    
+    public:
+  DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
+                        std::chrono::seconds interval);
+    
+    DNSCache::AddrEntry::AddrEntry(const std::string& addr)
+    : addr_(addr), good_(true)
+{
 }
     
-        // change element at index 1 (second element) to 'second'
-    array.at(1) = 'second';
-    
-        // out_of_range.401
-    try
-    {
-        // try to use a an invalid array index
-        json::const_reference ref = j.at('/array/4'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+        void markBad(const std::string& addr);
