7bc61717680edd5de5d2be90a99070dadbe8605e
SUNsung <weatherius@gmail.com>
2019-08-19 14:22:10 +0300

    Rewrite [36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py] File in: 2019-08-19 14:22:09

--- pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
+++ pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
@@ -1,73 +1,83 @@
 
         
-                self._built = False
+            def __init__(self, employee_id, name, rank, call_center):
+        self.employee_id = employee_id
+        self.name = name
+        self.rank = rank
+        self.call = None
+        self.call_center = call_center
     
+        def message_user(self, friend_id, message):
+        pass
     
-def multi_highway_dense(x, n_layer, act_fn=relu, carry_bias=-1.0, name=None):
-    '''多层 highway_dense
-    Input shape:  [batch_size, n_input]
-    Output shape: [batch_size, n_input]
-    '''
-    name = name or 'highway_dense'
-    for i in range(n_layer):
-        x = highway_dense(x, act_fn=act_fn, carry_bias=carry_bias, name='{}-{}'.format(name, i))
+        def __init__(self, seller_category_map, seller_category_overrides_map):
+        self.seller_category_map = seller_category_map
+        self.seller_category_overrides_map = seller_category_overrides_map
     
-        def __init__(self, l1=0., l2=0.):
-        '''
-        Args:
-            l1(float): L1 正则化的系数
-            l2(float): L2 正则化的系数
-        '''
-        self.l1 = np.asarray(l1, dtype=np.float32)
-        self.l2 = np.asarray(l2, dtype=np.float32)
+        def extract_year_month(self, line):
+        '''Return the year and month portions of the timestamp.'''
+        pass
     
-            Creates an attribute for each grammar symbol (nonterminal),
-        whose value is the symbol's type (an int >= 256).
-        '''
-        for name, symbol in grammar.symbol2number.items():
-            setattr(self, name, symbol)
+        def extract_max_priority_page(self):
+        '''Return the highest priority link in `links_to_crawl`.'''
+        pass
     
+        @pytest.fixture(autouse=True)
+    def Popen(self, mocker):
+        mock = mocker.patch('thefuck.shells.fish.Popen')
+        mock.return_value.stdout.read.side_effect = [(
+            b'cd\nfish_config\nfuck\nfunced\nfuncsave\ngrep\nhistory\nll\nls\n'
+            b'man\nmath\npopd\npushd\nruby'),
+            (b'alias fish_key_reader /usr/bin/fish_key_reader\nalias g git\n'
+             b'alias alias_with_equal_sign=echo\ninvalid_alias'), b'func1\nfunc2', b'']
+        return mock
     
-def spaces(a=1, b=(), c=[], d={}, e=True, f=-1, g=1 if False else 2, h='', i=r''):
-    offset = attr.ib(default=attr.Factory(lambda: _r.uniform(10000, 200000)))
-    assert task._cancel_stack[: len(old_stack)] == old_stack
-    
-    # flake8: noqa
-    
-    
-from .config import (
-    Any,
-    Bool,
-    ConfigType,
-    ConfigTypeAttributes,
-    Int,
-    no_comma_here_yet,
-    #  and some comments,
-    #  resolve_to_config_type,
-    #  DEFAULT_TYPE_ATTRIBUTES,
-)
-    
-            assert expectedexpectedexpectedexpectedexpectedexpectedexpectedexpectedexpect == {
-            key1: value1,
-            key2: value2,
-            key3: value3,
-            key4: value4,
-            key5: value5,
-            key6: value6,
-            key7: value7,
-            key8: value8,
-            key9: value9,
-        }
+        def _get_version(self):
+        '''Returns the version of the current shell'''
+        proc = Popen(['bash', '-c', 'echo $BASH_VERSION'],
+                     stdout=PIPE, stderr=DEVNULL)
+        return proc.stdout.read().decode('utf-8').strip()
 
     
-        def mixed_use_of_local_and_run(self):
-        '''
-        Run command truly locally, and over SSH via localhost
-        '''
-        cxn = Connection('localhost')
-        result = cxn.local('echo foo', hide=True)
-        assert result.stdout == 'foo\n'
-        assert not cxn.is_connected  # meh way of proving it didn't use SSH yet
-        result = cxn.run('echo foo', hide=True)
-        assert cxn.is_connected  # NOW it's using SSH
-        assert result.stdout == 'foo\n'
+        def instant_mode_alias(self, alias_name):
+        warn('Instant mode not supported by your shell')
+        return self.app_alias(alias_name)
+    
+        assert not match(Command(script, output))
+    
+    
+@for_app('pyenv')
+def get_new_command(command):
+    broken = re.findall(r'pyenv: no such command `([^']*)'', command.output)[0]
+    matched = [replace_argument(command.script, broken, common_typo)
+               for common_typo in COMMON_TYPOS.get(broken, [])]
+    matched.extend(replace_command(command, broken, get_pyenv_commands()))
+    return matched
+
+    
+        def test_query_string_params_items(self, httpbin):
+        r = http('--print=Hhb', 'GET', httpbin.url + '/get', 'a==1')
+        path = '/get?a=1'
+        url = httpbin.url + path
+        assert HTTP_OK in r
+        assert 'GET %s HTTP/1.1' % path in r
+        assert ''url': '%s'' % url in r
+    
+        def get_converters(self):
+        return [plugin for plugin in self
+                if issubclass(plugin, ConverterPlugin)]
+    
+    
+def test_default_options(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http(httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['form'] == {'foo': 'bar'}
+    
+        def test_GET_with_data_auto_JSON_headers(self, httpbin):
+        # JSON headers should automatically be set also for GET with data.
+        r = http('POST', httpbin.url + '/post', 'a=b')
+        assert HTTP_OK in r
+        assert r.json['headers']['Accept'] == JSON_ACCEPT
+        assert r.json['headers']['Content-Type'] == 'application/json'
