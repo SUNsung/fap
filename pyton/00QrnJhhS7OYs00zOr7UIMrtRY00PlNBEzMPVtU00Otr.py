b5294e1d7dc78db4dc94fc76b849818c1a9f7f70
SUNsung <weatherius@gmail.com>
2019-08-20 10:43:06 +0300

    Rewrite [00QrnJhhS7OYs00zOr7UIMrtRY00PlNBEzMPVtU00Otr.py] File in: 2019-08-20 10:43:06

--- pyton/00QrnJhhS7OYs00zOr7UIMrtRY00PlNBEzMPVtU00Otr.py
+++ pyton/00QrnJhhS7OYs00zOr7UIMrtRY00PlNBEzMPVtU00Otr.py
@@ -1,72 +1,225 @@
 
         
-            with open('README.md', 'w+') as sorted_file:
-        sorted_file.write(final_README)
+        PYTHON2_EXCEPTIONS = (
+    'ArithmeticError',
+    'AssertionError',
+    'AttributeError',
+    'BaseException',
+    'BufferError',
+    'BytesWarning',
+    'DeprecationWarning',
+    'EOFError',
+    'EnvironmentError',
+    'Exception',
+    'FloatingPointError',
+    'FutureWarning',
+    'GeneratorExit',
+    'IOError',
+    'ImportError',
+    'ImportWarning',
+    'IndentationError',
+    'IndexError',
+    'KeyError',
+    'KeyboardInterrupt',
+    'LookupError',
+    'MemoryError',
+    'NameError',
+    'NotImplementedError',
+    'OSError',
+    'OverflowError',
+    'PendingDeprecationWarning',
+    'ReferenceError',
+    'RuntimeError',
+    'RuntimeWarning',
+    # StandardError is gone in Python 3, so we map it to Exception
+    'StopIteration',
+    'SyntaxError',
+    'SyntaxWarning',
+    'SystemError',
+    'SystemExit',
+    'TabError',
+    'TargetScopeError',
+    'TypeError',
+    'UnboundLocalError',
+    'UnicodeDecodeError',
+    'UnicodeEncodeError',
+    'UnicodeError',
+    'UnicodeTranslateError',
+    'UnicodeWarning',
+    'UserWarning',
+    'ValueError',
+    'Warning',
+    'ZeroDivisionError',
+)
+    
+        def __call__(self, event, args):
+        # Avoid recursion when we call id() below
+        if event == 'builtins.id':
+            return
+    
+        @unittest.skipUnless(os.link in os.supports_dir_fd, 'test needs dir_fd support in os.link()')
+    def test_link_dir_fd(self):
+        f = posix.open(posix.getcwd(), posix.O_RDONLY)
+        try:
+            posix.link(support.TESTFN, support.TESTFN + 'link', src_dir_fd=f, dst_dir_fd=f)
+        except PermissionError as e:
+            self.skipTest('posix.link(): %s' % e)
+        else:
+            # should have same inodes
+            self.assertEqual(posix.stat(support.TESTFN)[1],
+                posix.stat(support.TESTFN + 'link')[1])
+        finally:
+            posix.close(f)
+            support.unlink(support.TESTFN + 'link')
+    
+        if resp[:3] != '227':
+        raise error_reply(resp)
+    global _227_re
+    if _227_re is None:
+        import re
+        _227_re = re.compile(r'(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)', re.ASCII)
+    m = _227_re.search(resp)
+    if not m:
+        raise error_proto(resp)
+    numbers = m.groups()
+    host = '.'.join(numbers[:4])
+    port = (int(numbers[4]) << 8) + int(numbers[5])
+    return host, port
+    
+        def date(self):
+        '''Process the DATE command.
+        Returns:
+        - resp: server response if successful
+        - date: datetime object
+        '''
+        resp = self._shortcmd('DATE')
+        if not resp.startswith('111'):
+            raise NNTPReplyError(resp)
+        elem = resp.split()
+        if len(elem) != 2:
+            raise NNTPDataError(resp)
+        date = elem[1]
+        if len(date) != 14:
+            raise NNTPDataError(resp)
+        return resp, _parse_datetime(date, None)
+    
+            n = 0
+        first_time = None
+        while not end_event and BW_END not in s:
+            _sendto(sock, s, remote_addr)
+            s = _recv(sock, packet_size)
+            if first_time is None:
+                first_time = _time()
+            n += 1
+        end_time = _time()
     
     
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
+class ByteArraySubclass(bytearray):
+    pass
+    
+        def test_check_encoding_errors(self):
+        # bpo-37388: open() and TextIOWrapper must check encoding and errors
+        # arguments in dev mode
+        mod = self.io.__name__
+        filename = __file__
+        invalid = 'Boom, Shaka Laka, Boom!'
+        code = textwrap.dedent(f'''
+            import sys
+            from {mod} import open, TextIOWrapper
+    
+        def test_catch_warnings_recording(self):
+        wmod = self.module
+        # Ensure warnings are recorded when requested
+        with wmod.catch_warnings(module=wmod, record=True) as w:
+            self.assertEqual(w, [])
+            self.assertIs(type(w), list)
+            wmod.simplefilter('always')
+            wmod.warn('foo')
+            self.assertEqual(str(w[-1].message), 'foo')
+            wmod.warn('bar')
+            self.assertEqual(str(w[-1].message), 'bar')
+            self.assertEqual(str(w[0].message), 'foo')
+            self.assertEqual(str(w[1].message), 'bar')
+            del w[:]
+            self.assertEqual(w, [])
+        # Ensure warnings are not recorded when not requested
+        orig_showwarning = wmod.showwarning
+        with wmod.catch_warnings(module=wmod, record=False) as w:
+            self.assertIsNone(w)
+            self.assertIs(wmod.showwarning, orig_showwarning)
+    
+        def test_repr_bound(self):
+        async def serve(stream):
+            pass
     
     
-@pytest.mark.parametrize('script, output, help_text, result', [
-    ('apt-get isntall vim', invalid_operation('isntall'),
-     apt_get_help, 'apt-get install vim'),
-    ('apt saerch vim', invalid_operation('saerch'),
-     apt_help, 'apt search vim'),
-])
-def test_get_new_command(set_help, output, script, help_text, result):
-    set_help(help_text)
-    assert get_new_command(Command(script, output))[0] == result
+def test_setitem_raises_incompatible_freq():
+    arr = PeriodArray(np.arange(3), freq='D')
+    with pytest.raises(IncompatibleFrequency, match='freq'):
+        arr[0] = pd.Period('2000', freq='A')
+    
+        def test_select_dtypes_datetime_with_tz(self):
+    
+        def test_interp_bad_method(self):
+        df = DataFrame(
+            {
+                'A': [1, 2, np.nan, 4],
+                'B': [1, 4, 9, np.nan],
+                'C': [1, 2, 3, 5],
+                'D': list('abcd'),
+            }
+        )
+        with pytest.raises(ValueError):
+            df.interpolate(method='not_a_method')
+    
+            d1 = [Timestamp(x) for x in ['2016-01-01', '2015-01-01', np.nan, '2016-01-01']]
+        d2 = [
+            Timestamp(x)
+            for x in ['2017-01-01', '2014-01-01', '2016-01-01', '2015-01-01']
+        ]
+        df = pd.DataFrame({'a': d1, 'b': d2}, index=[0, 1, 2, 3])
+    
+        slob = slice(
+        *idx.slice_locs(
+            df.index[5] + timedelta(seconds=30), df.index[15] - timedelta(seconds=30)
+        )
+    )
+    sliced = stacked[slob]
+    expected = df[6:15].stack()
+    tm.assert_almost_equal(sliced.values, expected.values)
+    
+            pi1 = period_range(freq='D', start='1/1/2001', end='12/1/2009')
+        pi2 = period_range(freq='D', start='12/31/2000', end='11/30/2009')
+        assert len(pi1) == len(pi2)
+        tm.assert_index_equal(pi1.shift(-1), pi2)
+    
+            result = abs(rng)
+        exp = TimedeltaIndex(
+            ['2 days', '1 days', '0 days', '1 days', '2 days'], name='x'
+        )
+        tm.assert_index_equal(result, exp)
+        assert result.freq is None
 
     
-    no_match_output = '''
-Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
-Get:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease [89.2 kB]
-Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
-Get:4 http://security.ubuntu.com/ubuntu zesty-security InRelease [89.2 kB]
-Hit:5 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
-Hit:6 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
-Hit:7 https://download.docker.com/linux/ubuntu zesty InRelease
-Get:8 http://us.archive.ubuntu.com/ubuntu zesty-updates/main i386 Packages [232 kB]
-Get:9 http://us.archive.ubuntu.com/ubuntu zesty-updates/main amd64 Packages [235 kB]
-Get:10 http://us.archive.ubuntu.com/ubuntu zesty-updates/main amd64 DEP-11 Metadata [55.2 kB]
-Get:11 http://us.archive.ubuntu.com/ubuntu zesty-updates/main DEP-11 64x64 Icons [32.3 kB]
-Get:12 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe amd64 Packages [156 kB]
-Get:13 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe i386 Packages [156 kB]
-Get:14 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe amd64 DEP-11 Metadata [175 kB]
-Get:15 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe DEP-11 64x64 Icons [253 kB]
-Get:16 http://us.archive.ubuntu.com/ubuntu zesty-updates/multiverse amd64 DEP-11 Metadata [5,840 B]
-Get:17 http://us.archive.ubuntu.com/ubuntu zesty-backports/universe amd64 DEP-11 Metadata [4,588 B]
-Get:18 http://security.ubuntu.com/ubuntu zesty-security/main amd64 DEP-11 Metadata [12.7 kB]
-Get:19 http://security.ubuntu.com/ubuntu zesty-security/main DEP-11 64x64 Icons [17.6 kB]
-Get:20 http://security.ubuntu.com/ubuntu zesty-security/universe amd64 DEP-11 Metadata [21.6 kB]
-Get:21 http://security.ubuntu.com/ubuntu zesty-security/universe DEP-11 64x64 Icons [47.7 kB]
-Get:22 http://security.ubuntu.com/ubuntu zesty-security/multiverse amd64 DEP-11 Metadata [208 B]
-Fetched 1,673 kB in 0s (1,716 kB/s)
-Reading package lists... Done
-Building dependency tree
-Reading state information... Done
-All packages are up to date.
-'''
+        def _get_obs(self):
+        return np.array([self.wealth]), self.rounds
     
+        def reset(self):
+        self.s = categorical_sample(self.isd, self.np_random)
+        self.lastaction = None
+        return self.s
     
-@pytest.fixture
-def brew_no_available_formula():
-    return '''Error: No available formula for elsticsearch '''
+        def to_jsonable(self, sample_n):
+        return [sample.tolist() for sample in sample_n]
     
+            self.action_space = spaces.Box(low=np.array([-self.bounds]), high=np.array([self.bounds]),
+                                       dtype=np.float32)
+        self.observation_space = spaces.Discrete(4)
     
-@pytest.mark.parametrize('command, new_command', [
-    (Command('cargo buid', no_such_subcommand_old), 'cargo build'),
-    (Command('cargo buils', no_such_subcommand), 'cargo build')])
-def test_get_new_command(command, new_command):
-    assert get_new_command(command) == new_command
-
-    
-    
-@parametrize_extensions
-@parametrize_filename
-@parametrize_script
-def test_match(ext, tar_error, filename, unquoted, quoted, script, fixed):
-    tar_error(unquoted.format(ext))
-    assert match(Command(script.format(filename.format(ext)), ''))
+        def step(self, action):
+        if action==0: pass
+        elif action==1: self.cube_x -= 1
+        elif action==2: self.cube_x += 1
+        else: assert 0, 'Action %i is out of range' % action
+        self.cube_y += 1
+        self.step_n += 1
