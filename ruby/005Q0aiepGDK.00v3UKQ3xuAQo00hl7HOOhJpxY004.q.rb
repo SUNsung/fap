5e73f1fcc91c0ed837d6b6a5163d05f539ab06c7
SUNsung <weatherius@gmail.com>
2019-08-20 04:31:16 +0300

    Rewrite [005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb] File in: 2019-08-20 04:31:16

--- ruby/005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb
+++ ruby/005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb
@@ -1,215 +1,187 @@
 
         
-              def test_invert_remove_foreign_key_with_on_delete_on_update
-        enable = @recorder.inverse_of :remove_foreign_key, [:dogs, :people, on_delete: :nullify, on_update: :cascade]
-        assert_equal [:add_foreign_key, [:dogs, :people, on_delete: :nullify, on_update: :cascade]], enable
-      end
+            @posts_per_day = Post.where('created_at >= ?', Date.today - 21.days).group('DATE(created_at)').order('DATE(created_at) ASC').count
+    @most_posts_within = @posts_per_day.values.max.to_f
     
-        %w(SPATIAL FULLTEXT UNIQUE).each do |type|
-      expected = 'ALTER TABLE `people` ADD #{type} INDEX `index_people_on_last_name`  (`last_name`)'
-      assert_sql(expected) do
-        ActiveRecord::Base.connection.change_table(:people, bulk: true) do |t|
-          t.index :last_name, type: type
+          def fetch_public_key(o_auth_app, jwt)
+        public_key = fetch_public_key_from_json(o_auth_app.jwks, jwt)
+        if public_key.empty? && o_auth_app.jwks_uri
+          response = Faraday.get(o_auth_app.jwks_uri)
+          public_key = fetch_public_key_from_json(response.body, jwt)
         end
-      end
-    end
-    
-          def self.delegate_to(klass)
-        self.type_klass = klass
+        raise Rack::OAuth2::Server::Authorize::BadRequest(:unauthorized_client) if public_key.empty?
+        public_key
       end
     
-          def get_device_type_with_simctl(device_names)
-        return device_names if Helper.test?
-    
-              raise 'Device name must be 50 characters or less. \'#{name}\' has a #{name.length} character length.' if name.length > 50
-    
-            # Find target
-        target = targets.find do |t|
-          t.name == target_name
-        end
-        UI.user_error!('Cannot find target named '#{target_name}'') unless target
-    
-          def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :workspace,
-                                       env_name: 'IPA_WORKSPACE',
-                                       description: 'WORKSPACE Workspace (.xcworkspace) file to use to build app (automatically detected in current directory)',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :project,
-                                       env_name: 'IPA_PROJECT',
-                                       description: 'Project (.xcodeproj) file to use to build app (automatically detected in current directory, overridden by --workspace option, if passed)',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :configuration,
-                                       env_name: 'IPA_CONFIGURATION',
-                                       description: 'Configuration used to build',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :scheme,
-                                       env_name: 'IPA_SCHEME',
-                                       description: 'Scheme used to build app',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :clean,
-                                       env_name: 'IPA_CLEAN',
-                                       description: 'Clean project before building',
-                                       optional: true,
-                                       is_string: false),
-          FastlaneCore::ConfigItem.new(key: :archive,
-                                       env_name: 'IPA_ARCHIVE',
-                                       description: 'Archive project after building',
-                                       optional: true,
-                                       is_string: false),
-          FastlaneCore::ConfigItem.new(key: :destination,
-                                       env_name: 'IPA_DESTINATION',
-                                       description: 'Build destination. Defaults to current directory',
-                                       default_value_dynamic: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :embed,
-                                       env_name: 'IPA_EMBED',
-                                       description: 'Sign .ipa file with .mobileprovision',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :identity,
-                                       env_name: 'IPA_IDENTITY',
-                                       description: 'Identity to be used along with --embed',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :sdk,
-                                       env_name: 'IPA_SDK',
-                                       description: 'Use SDK as the name or path of the base SDK when building the project',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :ipa,
-                                       env_name: 'IPA_IPA',
-                                       description: 'Specify the name of the .ipa file to generate (including file extension)',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :xcconfig,
-                                       env_name: 'IPA_XCCONFIG',
-                                       description: 'Use an extra XCCONFIG file to build the app',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :xcargs,
-                                       env_name: 'IPA_XCARGS',
-                                       description: 'Pass additional arguments to xcodebuild when building the app. Be sure to quote multiple args',
-                                       optional: true,
-                                       type: :shell_string)
-        ]
-      end
-    
-      it 'asks to accept conflicts when the scenario was modified' do
-    DefaultScenarioImporter.seed(user)
-    agent = user.agents.where(name: 'Rain Notifier').first
-    agent.options['expected_receive_period_in_days'] = 9001
-    agent.save!
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This Scenario already exists in your system.')
-    expect(page).to have_text('9001')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
-  end
-    
-          def check
-        create_event :payload => {}
-      end
+        if message.save
+      logger.info 'event=create type=message user=#{current_user.diaspora_handle} status=success ' \
+                  'message=#{message.id} chars=#{params[:message][:text].length}'
+      Diaspora::Federation::Dispatcher.defer_dispatch(current_user, message)
+    else
+      flash[:error] = I18n.t('conversations.new_conversation.fail')
     end
-    
-      describe '.seed' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
-    end
-    
-      describe '#working?' do
-    it 'should not be working until the first event was received' do
-      expect(@checker).not_to be_working
-      @checker.last_receive_at = Time.now
-      expect(@checker).to be_working
-    end
-  end
-    
-      describe '#chef_installed' do
-    let(:version) { '15.0.0' }
-    let(:command) { 'test -x /opt/chef_solo/bin/knife&& /opt/chef_solo/bin/knife --version | grep '15.0.0'' }
-    
-    # Configure VAGRANT_CWD so that the tests never find an actual
-# Vagrantfile anywhere, or at least this minimizes those chances.
-ENV['VAGRANT_CWD'] = VAGRANT_TEST_CWD
-    
-      it 'provides the provider' do
-    expect(subject.provider).to eq(provider)
-  end
-    
-        # Returns whether or not we know how to install the provider with
-    # the given name.
-    #
-    # @return [Boolean]
-    def can_install_provider?(name)
-      host.capability?(provider_install_key(name))
-    end
-    
-      describe '#machine_index' do
-    it 'returns a machine index' do
-      expect(subject.machine_index).to be_kind_of(Vagrant::MachineIndex)
-    end
-    
-    require 'log4r'
-    
-              triggers = filter_triggers(triggers, guest_name, type)
-    
-      let(:triggers) { VagrantPlugins::Kernel_V2::TriggerConfig.new }
-  let(:hash_block) { {info: 'hi', run: {inline: 'echo 'hi''}} }
-  let(:hash_block_two) { {warn: 'WARNING!!', run_remote: {inline: 'echo 'hi''}} }
-    
-        context 'when verify_host_key is true' do
-      let(:ssh_info) {{
-        host: 'localhost',
-        port: 2222,
-        username: 'vagrant',
-        private_key_path: [private_key_path],
-        verify_host_key: true
-      }}
-    
-          it 'ensures recipient is not following sender' do
-        expect(recipient.following?(sender)).to be false
-      end
-    end
-  end
-    
-        map = map.sort { |a, b| a[0].size <=> b[0].size }.to_h
-    
-        it 'removes the following relation between user and target user' do
-      expect(user.account.following?(other_account)).to be false
-    end
-    
-        it 'renders the headers' do
-      expect(mail.subject).to eq('bob favourited your status')
-      expect(mail.to).to eq([receiver.email])
-    end
-    
-      it 'does not notify when sender is muted with hide_notifications' do
-    recipient.mute!(sender, notifications: true)
-    is_expected.to_not change(Notification, :count)
-  end
-    
-        it 'generates the contacts_json fixture', :fixture => true do
-      json = bob.contacts.map { |c|
-               ContactPresenter.new(c, bob).full_hash_with_person
-             }.to_json
-      save_fixture(json, 'contacts_json')
-    end
+    redirect_to conversations_path(:conversation_id => conversation.id)
   end
 end
 
     
-      # fetch data
-  fields = {
-    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
-    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
-    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
-  }
+        it 'handles symbols and strings' do
+      q = Sidekiq::Queue.new('bar')
+      assert_equal 0, q.size
+      assert SetWorker.set('queue' => 'bar', :retry => 11).perform_async(1)
+      job = q.first
+      assert_equal 'bar', job['queue']
+      assert_equal 11, job['retry']
     
-          def report(env)
-        warn env, 'attack reported by #{self.class}'
-        env[options[:report_key]] = true
+        describe 'enqueued' do
+      it 'handles latency for good jobs' do
+        Sidekiq.redis do |conn|
+          conn.rpush 'queue:default', '{\'enqueued_at\': #{Time.now.to_f}}'
+          conn.sadd 'queues', 'default'
+        end
+        s = Sidekiq::Stats.new
+        assert s.default_queue_latency > 0
+        q = Sidekiq::Queue.new
+        assert q.latency > 0
       end
     
-      it 'accepts post requests with masked X-CSRF-Token header' do
-    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => masked_token)
-    expect(last_response).to be_ok
+        assert_equal dead_set.find_job('123123').value, serialized_job
   end
+    
+    desc 'Set up _deploy folder and deploy branch for Github Pages deployment'
+task :setup_github_pages, :repo do |t, args|
+  if args.repo
+    repo_url = args.repo
+  else
+    puts 'Enter the read/write url for your repository'
+    puts '(For example, 'git@github.com:your_username/your_username.github.io.git)'
+    puts '           or 'https://github.com/your_username/your_username.github.io')'
+    repo_url = get_stdin('Repository url: ')
+  end
+  protocol = (repo_url.match(/(^git)@/).nil?) ? 'https' : 'git'
+  if protocol == 'git'
+    user = repo_url.match(/:([^\/]+)/)[1]
+  else
+    user = repo_url.match(/github\.com\/([^\/]+)/)[1]
+  end
+  branch = (repo_url.match(/\/[\w-]+\.github\.(?:io|com)/).nil?) ? 'gh-pages' : 'master'
+  project = (branch == 'gh-pages') ? repo_url.match(/([^\/]+?)(\.git|$)/i)[1] : ''
+  unless (`git remote -v` =~ /origin.+?octopress(?:\.git)?/).nil?
+    # If octopress is still the origin remote (from cloning) rename it to octopress
+    system 'git remote rename origin octopress'
+    if branch == 'master'
+      # If this is a user/organization pages repository, add the correct origin remote
+      # and checkout the source branch for committing changes to the blog source.
+      system 'git remote add origin #{repo_url}'
+      puts 'Added remote #{repo_url} as origin'
+      system 'git config branch.master.remote origin'
+      puts 'Set origin as default remote'
+      system 'git branch -m master source'
+      puts 'Master branch renamed to 'source' for committing your blog source files'
+    else
+      unless !public_dir.match('#{project}').nil?
+        system 'rake set_root_dir[#{project}]'
+      end
+    end
+  end
+  url = blog_url(user, project, source_dir)
+  jekyll_config = IO.read('_config.yml')
+  jekyll_config.sub!(/^url:.*$/, 'url: #{url}')
+  File.open('_config.yml', 'w') do |f|
+    f.write jekyll_config
+  end
+  rm_rf deploy_dir
+  mkdir deploy_dir
+  cd '#{deploy_dir}' do
+    system 'git init'
+    system 'echo 'My Octopress Page is coming soon &hellip;' > index.html'
+    system 'git add .'
+    system 'git commit -m \'Octopress init\''
+    system 'git branch -m gh-pages' unless branch == 'master'
+    system 'git remote add origin #{repo_url}'
+    rakefile = IO.read(__FILE__)
+    rakefile.sub!(/deploy_branch(\s*)=(\s*)([''])[\w-]*['']/, 'deploy_branch\\1=\\2\\3#{branch}\\3')
+    rakefile.sub!(/deploy_default(\s*)=(\s*)([''])[\w-]*['']/, 'deploy_default\\1=\\2\\3push\\3')
+    File.open(__FILE__, 'w') do |f|
+      f.write rakefile
+    end
+  end
+  puts '\n---\n## Now you can deploy to #{repo_url} with `rake deploy` ##'
+end
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
+    end
+    
+        def handle_gist_redirecting(data)
+      redirected_url = data.header['Location']
+      if redirected_url.nil? || redirected_url.empty?
+        raise ArgumentError, 'GitHub replied with a 302 but didn't provide a location in the response headers.'
+      end
+    
+      describe '#vendor' do
+    it_behaves_like :Default, description.gsub(/^#/, '').to_sym, 'none'
+    it_behaves_like :Mutator, description.gsub(/^#/, '').to_sym
+  end
+    
+      private
+  # return the identifier by prepending the reverse-domain prefix
+  # to the package name, else return just the name
+  def identifier
+    identifier = name.dup
+    if self.attributes[:osxpkg_identifier_prefix]
+      identifier.insert(0, '#{self.attributes[:osxpkg_identifier_prefix]}.')
+    end
+    identifier
+  end # def identifier
+    
+        File.write(build_path('description'), self.description + '\n')
+    
+      private
+  def input(command)
+    platforms = [
+      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
+      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
+      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
+      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
+      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
+    ]
+    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
+                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
+                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
+                             'log_directory', 'log_file_stderr', 'log_file_stdout']
+    
+          File.open(File.join(builddir, 'manifests', manifest), 'w') do |f|
+        logger.info('manifest: #{f.path}')
+        template = template(File.join('puppet', '#{manifest}.erb'))
+        ::Dir.chdir(fileroot) do
+          f.puts template.result(binding)
+        end
+      end
+    end
+  end # def generate_specfile
