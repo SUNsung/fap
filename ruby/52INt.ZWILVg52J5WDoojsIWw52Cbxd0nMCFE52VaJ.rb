cd18d903a3e1d75de95a79ddf43eab2d4d611838
SUNsung <weatherius@gmail.com>
2019-08-20 12:40:54 +0300

    Rewrite [52INt.ZWILVg52J5WDoojsIWw52Cbxd0nMCFE52VaJ.rb] File in: 2019-08-20 12:40:54

--- ruby/52INt.ZWILVg52J5WDoojsIWw52Cbxd0nMCFE52VaJ.rb
+++ ruby/52INt.ZWILVg52J5WDoojsIWw52Cbxd0nMCFE52VaJ.rb
@@ -1,204 +1,192 @@
 
         
-          # ==> Configuration for :encryptable
-  # Allow you to use another hashing or encryption algorithm besides bcrypt (default).
-  # You can use :sha1, :sha512 or algorithms from others authentication tools as
-  # :clearance_sha1, :authlogic_sha512 (then you should set stretches above to 20
-  # for default behavior) and :restful_authentication_sha1 (then you should set
-  # stretches to 10, and copy REST_AUTH_SITE_KEY to pepper).
-  #
-  # Require the `devise-encryptable` gem when using anything other than bcrypt
-  # config.encryptor = :sha512
-    
-      test 'Devise.email_regexp should match valid email addresses' do
-    valid_emails = ['test@example.com', 'jo@jo.co', 'f4$_m@you.com', 'testing.example@example.com.ua', 'test@tt', 'test@valid---domain.com']
-    non_valid_emails = ['rex', 'test user@example.com', 'test_user@example server.com']
-    
-      # POST /resource/confirmation
-  def create
-    self.resource = resource_class.send_confirmation_instructions(resource_params)
-    yield resource if block_given?
-    
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
+          it 'imports a scenario that does not exist yet' do
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
+    click_on 'Start Import'
+    expect(page).to have_text('This scenario has a few agents to get you started. Feel free to change them or delete them as you see fit!')
+    expect(page).not_to have_text('This Scenario already exists in your system.')
+    check('I confirm that I want to import these Agents.')
+    click_on 'Finish Import'
+    expect(page).to have_text('Import successful!')
   end
     
-      # Sets the resource creating an instance variable
-  def resource=(new_resource)
-    instance_variable_set(:'@#{resource_name}', new_resource)
+      describe '#style_colors' do
+    it 'returns a css style-formated version of the scenario foreground and background colors' do
+      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
+    end
+    
+        it 'outputs control links to agents within the incoming set, but not outside it' do
+      agents(:jane_rain_notifier_agent).control_targets = [agents(:jane_weather_agent), agents(:jane_basecamp_agent)]
+      agents(:jane_rain_notifier_agent).save!
+    
+          expect(@scheduler.scheduler_agent_jobs.map(&:scheduler_agent)).to eq([@agent2])
+    end
+    
+          @log.level = 4
+      expect(@log).to be_valid
+    
+        context 'to configure' do
+      let(:real_target) {
+        Agents::TriggerAgent.create!(
+          name: 'somename',
+          options: {
+            expected_receive_period_in_days: 2,
+            rules: [
+              {
+                'type' => 'field<value',
+                'value' => '200.0',
+                'path' => 'price',
+              }
+            ],
+            keep_event: 'true'
+          },
+          user: users(:bob)
+        )
+      }
+    
+        context 'serializing' do
+      before(:each) do
+        @checker.options['mode'] = 'serialize'
+        @checker.options['data_path'] = '$.data'
+        @checker.options['data_key'] = 'data'
+      end
+    
+        def initial_page?
+      root_page? || context[:initial_paths].include?(subpath)
+    end
+    
+        private
+    
+        def as_json
+      { name: name, path: path, type: type }
+    end
   end
+end
+
     
-        def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
+        def initialize
+      @pages = {}
     end
     
-    class ApplicationController < ActionController::Base
-end
-    
-          default_failure_app(options)
-      default_controllers(options)
-      default_path_names(options)
-      default_used_route(options)
-      default_used_helpers(options)
+          def stub(path, &block)
+        @stubs[path] = block
+        @stubs
+      end
     end
     
-    def source_version
-  @source_version ||= File.read(File.expand_path('../VERSION', __FILE__)).strip
-end
+            css('pre').each do |node|
+          node.content = node.content.strip
     
-    @@ chat
-<pre id='chat'></pre>
-<form>
-  <input id='msg' placeholder='type message here...' />
-</form>
-    
-      File.open('rack-protection.gemspec', 'w') { |f| f << content }
-end
-    
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
-          end
+            css('.note h3', '.warning h3').each do |node|
+          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
         end
-        bad_cookies.empty?
-      end
     
-        BUNDLE_DIR = ::File.join(LOGSTASH_HOME, 'vendor', 'bundle')
-    GEMFILE_PATH = ::File.join(LOGSTASH_HOME, 'Gemfile')
-    LOCAL_GEM_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'local_gems')
-    CACHE_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'cache')
-    LOCKFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile.lock'))
-    GEMFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile'))
-    
-        def locales_path(path)
-      return ::File.join(LOGSTASH_CORE, 'locales', path)
-    end
-    
-      desc 'Build a tar.gz of all logstash plugins from logstash-plugins github repo'
-  task 'tar-all-plugins' => ['prepare-all', 'generate_build_metadata'] do
-    puts('[artifact:tar] Building tar.gz of all plugins')
-    build_tar('ELASTIC-LICENSE', '-all-plugins')
+      let(:iso_env) do
+    # We have to create a Vagrantfile so there is a root path
+    env = isolated_environment
+    env.vagrantfile('')
+    env.create_vagrant_env
   end
     
-          # Handle string rvalues, they just get turned into regexps
-      # Maybe we really shouldn't handle these anymore...
-      if regexp.class == org.logstash.config.ir.expression.ValueExpression
-        regexp = jdsl.eRegex(source_meta, regexp.get)
+      let(:machine) { iso_env.machine(iso_env.machine_names[0], :dummy) }
+  let(:communicator) { VagrantTests::DummyCommunicator::Communicator.new(machine) }
+  let(:config)  { double('config') }
+    
+        let(:network_settings) { {'NetworkSettings' => {'Bridge'=>'', 'SandboxID'=>'randomid', 'HairpinMode'=>false, 'LinkLocalIPv6Address'=>'', 'LinkLocalIPv6PrefixLen'=>0, 'Ports'=>{'443/tcp'=>nil, '80/tcp'=>nil}, 'SandboxKey'=>'/var/run/docker/netns/158b7024a9e4', 'SecondaryIPAddresses'=>nil, 'SecondaryIPv6Addresses'=>nil, 'EndpointID'=>'randomEndpointID', 'Gateway'=>'172.17.0.1', 'GlobalIPv6Address'=>'', 'GlobalIPv6PrefixLen'=>0, 'IPAddress'=>'127.0.0.1', 'IPPrefixLen'=>16, 'IPv6Gateway'=>'', 'MacAddress'=>'02:42:ac:11:00:02', 'Networks'=>{'bridge'=>{'IPAMConfig'=>nil, 'Links'=>nil, 'Aliases'=>nil, 'NetworkID'=>'networkIDVar', 'EndpointID'=>'endpointIDVar', 'Gateway'=>'127.0.0.1', 'IPAddress'=>'127.0.0.1', 'IPPrefixLen'=>16, 'IPv6Gateway'=>'', 'GlobalIPv6Address'=>'', 'GlobalIPv6PrefixLen'=>0, 'MacAddress'=>'02:42:ac:11:00:02', 'DriverOpts'=>nil}}}} }
+    
+          # This is a process-local mutex that can be used by parallel
+      # providers to lock the host VM access.
+      def host_vm_mutex
+        @@host_vm_mutex
       end
     
-          def pack_uri(plugin_name)
-        url = '#{elastic_pack_base_uri}/#{plugin_name}/#{plugin_name}-#{LOGSTASH_VERSION}.#{PACK_EXTENSION}'
-        URI.parse(url)
-      end
-    
-        not_same_pipeline_id = described_class.new(source, :another_pipeline, unordered_config_parts, settings)
-    expect(subject).not_to eq(not_same_pipeline_id)
-  end
-    
-          def header
-        if @header.nil?
-          if page = @page.header
-            @header = page.text_data
-          else
-            @header = false
-          end
+      def new_machine(options)
+    double('machine').tap do |m|
+      allow(m).to receive(:provider_name).and_return(provider_name)
+      allow(m).to receive(:provider_options).and_return(options)
+      allow(m).to receive(:action) do |action, opts|
+        lock.synchronize do
+          called_actions << [m, action, opts]
         end
-        @header
       end
-    
-          def _identicon_code(blob)
-        string_to_code blob + @request.host
-      end
-    
-          def date
-        first = page.last_version
-        return Time.now.strftime(DATE_FORMAT) unless first
-        first.authored_date.strftime(DATE_FORMAT)
-      end
-    
-      test 'create redirects to page if already exists' do
-    name = 'A'
-    get '/create/#{name}'
-    follow_redirect!
-    assert_equal '/#{name}', last_request.fullpath
-    assert last_response.ok?
-  end
-    
-      test 'clean path with leading slash' do
-    assert_equal '/Mordor', clean_path('/Mordor')
-  end
-    
-        assert body.include?('<span class='username'>Charles Pence</span>'), '/latest_changes should include the Author Charles Pence'
-    assert body.include?('a8ad3c0'), '/latest_changes should include the :latest_changes_count commit'
-    assert !body.include?('60f12f4'), '/latest_changes should not include more than latest_changes_count commits'
-    assert body.include?('<a href='Data-Two.csv/874f597a5659b4c3b153674ea04e406ff393975e'>Data-Two.csv</a>'), '/latest_changes include links to modified files in #{body}'
-    assert body.include?('<a href='Hobbit/874f597a5659b4c3b153674ea04e406ff393975e'>Hobbit.md</a>'), '/latest_changes should include links to modified pages in #{body}'
-  end
-    
-        @view = Precious::Views::Page.new
-    @view.instance_variable_set :@page, page
-    @view.instance_variable_set :@content, page.formatted_data
-    @view.instance_variable_set :@h1_title, false
-    
-    module Gollum
-  VERSION = '4.1.4'
-    
-      if defined?(Gollum::GIT_ADAPTER) && Gollum::GIT_ADAPTER != 'grit'
-    def to_ascii
-      self # Do not transliterate utf-8 url's unless using Grit
     end
   end
     
-    describe Sidekiq::ExceptionHandler do
-  describe 'with mock logger' do
-    before do
-      @old_logger = Sidekiq.logger
-      @str_logger = StringIO.new
-      Sidekiq.logger = Logger.new(@str_logger)
-    end
+        # Fallback to the default
+    path ||= '~/.vagrant.d'
     
-      it 'does not support invalid concurrency' do
-    assert_raises(ArgumentError) { new_manager(concurrency: 0) }
-    assert_raises(ArgumentError) { new_manager(concurrency: -1) }
-  end
+      let(:machine) { iso_env.machine(iso_env.machine_names[0], :dummy) }
     
-        def last?
-      index == tab.panes.length - 1
-    end
-    
-      describe '#hook_on_project_start' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_start' }
-    end
-  end
-  describe '#hook_on_project_first_start' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_first_start' }
-    end
-  end
-  describe '#hook_on_project_restart' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_restart' }
-    end
-  end
-  describe '#hook_on_project_exit' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_exit' }
-    end
-  end
-  describe '#hook_on_project_stop' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_stop' }
+          expect(response).to redirect_to(settings_preferences_notifications_path)
+      user.reload
+      expect(user.settings['notification_emails']['follow']).to be true
+      expect(user.settings['interactions']['must_be_follower']).to be false
     end
   end
 end
+
+    
+            let(:object_json) do
+          {
+            id: 'https://example.com/actor#bar',
+            type: 'Note',
+            content: 'Lorem ipsum',
+            to: 'http://example.com/followers',
+            attributedTo: 'https://example.com/actor',
+          }
+        end
+    
+      def save
+    case action
+    when 'unfollow'
+      unfollow!
+    when 'remove_from_followers'
+      remove_from_followers!
+    when 'block_domains'
+      block_domains!
+    when 'approve'
+      approve!
+    when 'reject'
+      reject!
+    end
+  end
+    
+        @inboxes = [@status.mentions, @status.reblogs, @status.preloadable_poll.votes].flat_map do |relation|
+      relation.includes(:account).map do |record|
+        record.account.preferred_inbox_url if !record.account.local? && record.account.activitypub?
+      end
+    end
+    
+    class ActivityPub::UpdateDistributionWorker
+  include Sidekiq::Worker
+  include Payloadable
+    
+      before do
+    sign_in user, scope: :user
+  end
+    
+      let(:json) do
+    {
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      id: 'foo',
+      type: 'Move',
+      actor: old_account.uri,
+      object: old_account.uri,
+      target: new_account.uri,
+    }.with_indifferent_access
+  end
+    
+        # advance scanner to pos after the next match of pattern and return the match
+    def scan_next(pattern)
+      return unless @s.scan_until(pattern)
+      @s.matched
+    end
+    
+        def puts(*args)
+      STDERR.puts *args unless @silence
+    end
+    
+      # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
