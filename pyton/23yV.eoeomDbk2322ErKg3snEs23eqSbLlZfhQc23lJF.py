e90f5f7ce982d75efe55c47352a16b67edae3875
SUNsung <weatherius@gmail.com>
2019-08-21 01:31:03 +0300

    Rewrite [23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py] File in: 2019-08-21 01:31:03

--- pyton/23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py
+++ pyton/23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py
@@ -1,136 +1,210 @@
 
         
-                status, long_video_id, key = params[2], params[5], params[6]
-        status = remove_start(status, 'PRODUCT_')
-    
-                    for caption in asset.get('caption_metadata', []):
-                    caption_url = caption.get('source_url')
-                    if not caption_url:
-                        continue
-                    subtitles.setdefault(caption.get('language', 'en'), []).append({
-                        'url': caption_url,
-                        'ext': determine_ext(caption_url, 'vtt'),
-                    })
-            elif asset.get('type') == 'image':
-                asset_location = asset.get('location')
-                if not asset_location:
-                    continue
-                thumbnails.append({
-                    'url': asset_location,
-                    'width': int_or_none(asset.get('width')),
-                    'height': int_or_none(asset.get('height')),
-                })
-        self._sort_formats(formats)
-    
-    import sys
-import os
-import textwrap
-    
-        def test_youporn(self):
-        self._assert_restricted(
-            'http://www.youporn.com/watch/505835/sex-ed-is-it-safe-to-masturbate-daily/',
-            '505835.mp4', 2, old_age=25)
-    
-            return [package.split()[0] for package in packages]
-    except subprocess.CalledProcessError as err:
-        if err.returncode == 1 and err.output == '':
-            return []
-        else:
-            raise err
-    
-        @pytest.mark.parametrize('key, value', [
-        ('TF_OVERRIDDEN_ALIASES', 'cut,git,sed'),  # legacy
-        ('THEFUCK_OVERRIDDEN_ALIASES', 'cut,git,sed'),
-        ('THEFUCK_OVERRIDDEN_ALIASES', 'cut, git, sed'),
-        ('THEFUCK_OVERRIDDEN_ALIASES', ' cut,\tgit,sed\n'),
-        ('THEFUCK_OVERRIDDEN_ALIASES', '\ncut,\n\ngit,\tsed\r')])
-    def test_get_overridden_aliases(self, shell, os_environ, key, value):
-        os_environ[key] = value
-        overridden = shell._get_overridden_aliases()
-        assert set(overridden) == {'cd', 'cut', 'git', 'grep',
-                                   'ls', 'man', 'open', 'sed'}
-    
-        @pytest.mark.parametrize('side_effect, expected_version, call_args', [
-        ([b'''Major  Minor  Build  Revision
------  -----  -----  --------
-5      1      17763  316     \n'''], 'PowerShell 5.1.17763.316', ['powershell.exe']),
-        ([IOError, b'PowerShell 6.1.2\n'], 'PowerShell 6.1.2', ['powershell.exe', 'pwsh'])])
-    def test_info(self, side_effect, expected_version, call_args, shell, Popen):
-        Popen.return_value.stdout.read.side_effect = side_effect
-        assert shell.info() == expected_version
-        assert Popen.call_count == len(call_args)
-        assert all([Popen.call_args_list[i][0][0][0] == call_arg for i, call_arg in enumerate(call_args)])
-    
-        assert not match(Command(script, output))
-    
-        # Load image file and find face locations
-    X_img = face_recognition.load_image_file(X_img_path)
-    X_face_locations = face_recognition.face_locations(X_img)
-    
-        face_names = []
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
-    
-    # Load a sample picture and learn how to recognize it.
-print('Loading known face image(s)')
-obama_image = face_recognition.load_image_file('obama_small.jpg')
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-    
-            # empty numpy list
-        faces_to_compare = np.array([])
-    
-    # Load the jpg file into a numpy array
-image = face_recognition.load_image_file('biden.jpg')
-    
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_frame = frame[:, :, ::-1]
-    
-    # Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
-    
-        # 获得所有人脸的位置以及它们的编码
-    face_locations = face_recognition.face_locations(output)
-    print('Found {} faces in image.'.format(len(face_locations)))
-    face_encodings = face_recognition.face_encodings(output, face_locations)
-    
-    Request receiver in simple form keeps a reference to a single successor.
-As a variation some receivers may be capable of sending requests out
-in several directions, forming a `tree of responsibility`.
-    
-    
-def main():
+            Args:
+      base_sizes: list of tuples of anchor base sizes. For example, setting
+        base_sizes=[(1, 2, 3), (4, 5)] means that we want 3 anchors at each
+        grid point on the first layer with the base sizes of 1, 2, and 3, and 2
+        anchors at each grid point on the second layer with the base sizes of
+        4 and 5.
+      aspect_ratios: list or tuple of aspect ratios. For example, setting
+        aspect_ratios=[(1.0, 2.0, 0.5), (1.0, 2.0)] means that we want 3 anchors
+        at each grid point on the first layer with aspect ratios of 1.0, 2.0,
+        and 0.5, and 2 anchors at each grid point on the sercond layer with the
+        base sizes of 1.0 and 2.0.
+      anchor_strides: list of pairs of strides in pixels (in y and x directions
+        respectively). For example, setting anchor_strides=[(25, 25), (50, 50)]
+        means that we want the anchors corresponding to the first layer to be
+        strided by 25 pixels and those in the second layer to be strided by 50
+        pixels in both y and x directions.
+      anchor_offsets: list of pairs of offsets in pixels (in y and x directions
+        respectively). The offset specifies where we want the center of the
+        (0, 0)-th anchor to lie for each layer. For example, setting
+        anchor_offsets=[(10, 10), (20, 20)]) means that we want the
+        (0, 0)-th anchor of the first layer to lie at (10, 10) in pixel space
+        and likewise that we want the (0, 0)-th anchor of the second layer to
+        lie at (25, 25) in pixel space.
+      normalize_coordinates: whether to produce anchors in normalized
+        coordinates. (defaults to True).
     '''
-    >>> molly = User('Molly')
-    >>> mark = User('Mark')
-    >>> ethan = User('Ethan')
+    self._base_sizes = base_sizes
+    self._aspect_ratios = aspect_ratios
+    self._anchor_strides = anchor_strides
+    self._anchor_offsets = anchor_offsets
+    self._normalize_coordinates = normalize_coordinates
     
+      if optimizer_config.use_moving_average:
+    optimizer = tf.contrib.opt.MovingAverageOptimizer(
+        optimizer, average_decay=optimizer_config.moving_average_decay)
     
-def on_sale_discount(order):
-    return order.price * 0.25 + 20
+      with tf.gfile.GFile(FLAGS.pipeline_config_path, 'r') as f:
+    text_format.Merge(f.read(), pipeline_config)
+  text_format.Merge(FLAGS.config_override, pipeline_config)
+  export_tflite_ssd_graph_lib.export_tflite_graph(
+      pipeline_config, FLAGS.trained_checkpoint_prefix, FLAGS.output_directory,
+      FLAGS.add_postprocessing_op, FLAGS.max_detections,
+      FLAGS.max_classes_per_detection, use_regular_nms=FLAGS.use_regular_nms)
     
-    *TL;DR
-Defines the skeleton of a base algorithm, deferring definition of exact
-steps to subclasses.
-    
-    
-if __name__ == '__main__':
-    rm1 = Borg()
-    rm2 = Borg()
-    
-    *What does this example do?
-In this example queue.Queue is used to create the pool (wrapped in a
-custom ObjectPool object to use with the with statement), and it is
-populated with strings.
-As we can see, the first string object put in 'yam' is USED by the
-with statement. But because it is released back into the pool
-afterwards it is reused by the explicit call to sample_queue.get().
-Same thing happens with 'sam', when the ObjectPool created insided the
-function is deleted (by the GC) and the object is returned.
-    
+        Raises:
+      ValueError: If `second_stage_batch_size` > `first_stage_max_proposals`
+      ValueError: If first_stage_anchor_generator is not of type
+        grid_anchor_generator.GridAnchorGenerator.
     '''
-Reference: https://en.wikipedia.org/wiki/Delegation_pattern
-Author: https://github.com/IuryAlves
+    # TODO(rathodv): add_summaries and crop_and_resize_fn is currently
+    # unused. Respect that directive in the future.
+    super(RFCNMetaArch, self).__init__(
+        is_training,
+        num_classes,
+        image_resizer_fn,
+        feature_extractor,
+        number_of_stages,
+        first_stage_anchor_generator,
+        first_stage_target_assigner,
+        first_stage_atrous_rate,
+        first_stage_box_predictor_arg_scope_fn,
+        first_stage_box_predictor_kernel_size,
+        first_stage_box_predictor_depth,
+        first_stage_minibatch_size,
+        first_stage_sampler,
+        first_stage_non_max_suppression_fn,
+        first_stage_max_proposals,
+        first_stage_localization_loss_weight,
+        first_stage_objectness_loss_weight,
+        crop_and_resize_fn,
+        None,  # initial_crop_size is not used in R-FCN
+        None,  # maxpool_kernel_size is not use in R-FCN
+        None,  # maxpool_stride is not use in R-FCN
+        second_stage_target_assigner,
+        None,  # fully_connected_box_predictor is not used in R-FCN.
+        second_stage_batch_size,
+        second_stage_sampler,
+        second_stage_non_max_suppression_fn,
+        second_stage_score_conversion_fn,
+        second_stage_localization_loss_weight,
+        second_stage_classification_loss_weight,
+        second_stage_classification_loss,
+        1.0,  # second stage mask prediction loss weight isn't used in R-FCN.
+        hard_example_miner,
+        parallel_iterations,
+        add_summaries,
+        clip_anchors_to_image,
+        use_static_shapes,
+        resize_masks,
+        freeze_batchnorm=freeze_batchnorm)
     
-        def __init__(self):
-        self.business_logic = BusinessLogic()
+      ece_update_op = _ece_from_bins(
+      update_bin_counts_op,
+      update_bin_true_sum_op,
+      update_bin_preds_sum_op,
+      name='update_op')
+  ece = _ece_from_bins(bin_counts, bin_true_sum, bin_preds_sum, name='value')
+  return ece, ece_update_op
+
+    
+      Please refer to Mask RCNN paper:
+  https://arxiv.org/abs/1703.06870
+  '''
+    
+    
+class RfcnBoxPredictor(box_predictor.BoxPredictor):
+  '''RFCN Box Predictor.
+    
+            if input_type not in ('dict', 'pair', 'string'):
+            raise ValueError('input_type must be 'dict', 'pair' or 'string','
+                             ' got %r.' % input_type)
+    
+    
+def rmse(a, b):
+    return np.sqrt(np.mean((a - b) ** 2))
+    
+                gc.collect()
+            print('- benchmarking Lasso')
+            clf = Lasso(alpha=alpha, fit_intercept=False,
+                        precompute=precompute)
+            tstart = time()
+            clf.fit(X, Y)
+            lasso_results.append(time() - tstart)
+    
+                gc.collect()
+            print('- benchmarking ElasticNet')
+            clf = ElasticNet(alpha=alpha, l1_ratio=0.5, fit_intercept=False)
+            tstart = time()
+            clf.fit(X_train, y_train)
+            elnet_results[i, j, 0] = mean_squared_error(clf.predict(X_test),
+                                                        y_test)
+            elnet_results[i, j, 1] = time() - tstart
+    
+    model = SpectralBiclustering(n_clusters=n_clusters, method='log',
+                             random_state=0)
+model.fit(data)
+score = consensus_score(model.biclusters_,
+                        (rows[:, row_idx], columns[:, col_idx]))
+    
+    from sklearn.datasets import make_biclusters
+from sklearn.datasets import samples_generator as sg
+from sklearn.cluster.bicluster import SpectralCoclustering
+from sklearn.metrics import consensus_score
+    
+    # To apply a classifier on this data, we need to flatten the image, to
+# turn the data in a (samples, feature) matrix:
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+    
+        plt.imshow(avg_dist, interpolation='nearest', cmap=plt.cm.gnuplot2,
+               vmin=0)
+    plt.xticks(range(n_clusters), labels, rotation=45)
+    plt.yticks(range(n_clusters), labels)
+    plt.colorbar()
+    plt.suptitle('Interclass %s distances' % metric, size=18)
+    plt.tight_layout()
+    
+            plt.xlim(-2.5, 2.5)
+        plt.ylim(-2.5, 2.5)
+        plt.xticks(())
+        plt.yticks(())
+        plt.text(.99, .01, ('%.2fs' % (t1 - t0)).lstrip('0'),
+                 transform=plt.gca().transAxes, size=15,
+                 horizontalalignment='right')
+        plot_num += 1
+    
+    # The following bandwidth can be automatically detected using
+bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
+    
+    # #############################################################################
+# Generate data (swiss roll dataset)
+n_samples = 1500
+noise = 0.05
+X, _ = make_swiss_roll(n_samples, noise)
+# Make it thinner
+X[:, 1] *= .5
+    
+        def __init__(self, idf_path=None):
+        self.path = ''
+        self.idf_freq = {}
+        self.median_idf = 0.0
+        if idf_path:
+            self.set_new_path(idf_path)
+    
+        (prob, state) = max((V[len(obs) - 1][y], y) for y in 'ES')
+    
+        last = [(V[-1][y], y) for y in mem_path[-1].keys()]
+    # if len(last)==0:
+    #     print obs
+    prob, state = max(last)
+    
+    print('/'.join(jieba.cut('如果放到post中将出错。', HMM=False)))
+#如果/放到/post/中将/出错/。
+print(jieba.suggest_freq(('中', '将'), True))
+#494
+print('/'.join(jieba.cut('如果放到post中将出错。', HMM=False)))
+#如果/放到/post/中/将/出错/。
+print('/'.join(jieba.cut('「台中」正确应该不会被切开', HMM=False)))
+#「/台/中/」/正确/应该/不会/被/切开
+print(jieba.suggest_freq('台中', True))
+#69
+print('/'.join(jieba.cut('「台中」正确应该不会被切开', HMM=False)))
+#「/台中/」/正确/应该/不会/被/切开
+    
+    default_encoding='utf-8'
+    
+    t2 = time.time()
+tm_cost = t2-t1
