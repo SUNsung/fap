00a48bc1145ebf322829133083bc97309ed2088c
SUNsung <weatherius@gmail.com>
2019-08-20 19:34:02 +0300

    Rewrite [4438ApNC1QfUk447svL6EbHYQk44DizFzEMbT1644egQ.rb] File in: 2019-08-20 19:34:02

--- ruby/4438ApNC1QfUk447svL6EbHYQk44DizFzEMbT1644egQ.rb
+++ ruby/4438ApNC1QfUk447svL6EbHYQk44DizFzEMbT1644egQ.rb
@@ -1,109 +1,122 @@
 
         
-        #############################################################################
-#
-# Helper functions
-#
-#############################################################################
+                machine.communicate.sudo(build_rsync_chown(opts))
+      end
     
-    require 'benchmark/ips'
-require 'jekyll'
-require 'json'
+        let(:cmd) { 'find /vagrant -path /vagrant/.vagrant -prune -o '!' -type l -a '(' ! -user vagrant -or ! -group vagrant ')' -exec chown vagrant:vagrant '{}' +' }
     
-    if pathutil_relative == native_relative
-  Benchmark.ips do |x|
-    x.report('pathutil') { pathutil_relative }
-    x.report('native')   { native_relative }
-    x.compare!
+    # Create a temporary directory where test vagrant will run. The reason we save
+# this to a constant is so we can clean it up later.
+VAGRANT_TEST_CWD = Dir.mktmpdir('vagrant-test-cwd')
+    
+      before do
+    allow(machine).to receive(:communicate).and_return(communicator)
   end
-else
-  print 'PATHUTIL: '
-  puts pathutil_relative
-  print 'NATIVE:   '
-  puts native_relative
+    
+        it 'has the valid configuration' do
+      expect(subject.config.vm.box).to eq('foo')
+    end
+    
+            def build(dir, **opts, &block)
+          name = machine.name.to_s
+          @logger.debug('Applying build for `#{name}` using `#{dir}` directory.')
+          begin
+            update_composition do |composition|
+              services = composition['services'] ||= {}
+              services[name] ||= {}
+              services[name]['build'] = {'context' => dir}
+              # Extract custom dockerfile location if set
+              if opts[:extra_args] && opts[:extra_args].include?('--file')
+                services[name]['build']['dockerfile'] = opts[:extra_args][opts[:extra_args].index('--file') + 1]
+              end
+              # Extract any build args that can be found
+              case opts[:build_args]
+              when Array
+                if opts[:build_args].include?('--build-arg')
+                  idx = 0
+                  build_args = {}
+                  while(idx < opts[:build_args].size)
+                    arg_value = opts[:build_args][idx]
+                    idx += 1
+                    if arg_value.start_with?('--build-arg')
+                      if !arg_value.include?('=')
+                        arg_value = opts[:build_args][idx]
+                        idx += 1
+                      end
+                      key, val = arg_value.to_s.split('=', 2).to_s.split('=')
+                      build_args[key] = val
+                    end
+                  end
+                end
+              when Hash
+                services[name]['build']['args'] = opts[:build_args]
+              end
+            end
+          rescue => error
+            @logger.error('Failed to apply build using `#{dir}` directory: #{error.class} - #{error}')
+            update_composition do |composition|
+              composition['services'].delete(name)
+            end
+            raise
+          end
+        end
+    
+        it 'returns an array of all known containers' do
+      expect(subject).to receive(:execute).with('/sbin/ip', '-4', 'addr', 'show', 'scope', 'global', 'docker0')
+      expect(subject.docker_bridge_ip).to eq('123.456.789.012')
+    end
+  end
 end
 
     
-      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+      before { allow(controller).to receive(:doorkeeper_token) { token } }
     
-    module Jekyll
-  module Drops
-    class UnifiedPayloadDrop < Drop
-      mutable true
+      def inboxes
+    return @inboxes if defined?(@inboxes)
     
-        open_dry_run_modal(agent)
-    click_on('Dry Run')
-    expect(page).to have_text('Dry Run started')
-    expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabLog']')
-  end
-end
-
+      sidekiq_options queue: 'push'
     
-      it 'imports a scenario which requires a service' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'spec/data_fixtures/twitter_scenario.json'))
-    click_on 'Start Import'
-    check('I confirm that I want to import these Agents.')
-    expect { click_on 'Finish Import' }.to change(Scenario, :count).by(1)
-    expect(page).to have_text('Import successful!')
-  end
-end
-
+      sidekiq_options queue: 'push'
     
-        it 'works for queued jobs' do
-      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
-    end
-  end
-    
-      describe 'initialization' do
-    it 'is initialized with an attributes hash' do
-      expect(ScenarioImport.new(:url => 'http://google.com').url).to eq('http://google.com')
-    end
-  end
-    
-      describe 'migrating the 'make_message' format' do
-    it 'should work' do
-      expect(LiquidMigrator.convert_make_message('<message>')).to eq('{{message}}')
-      expect(LiquidMigrator.convert_make_message('<new.message>')).to eq('{{new.message}}')
-      expect(LiquidMigrator.convert_make_message('Hello <world>. How is <nested.life>')).to eq('Hello {{world}}. How is {{nested.life}}')
-    end
-  end
-    
-        it 'cleans up old logs when there are more than log_length' do
-      stub(AgentLog).log_length { 4 }
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 1')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 2')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 3')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 4')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 4')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 1')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 5')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 5')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 2')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 6')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 6')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 3')
+          expect(response).to redirect_to(settings_preferences_other_path)
+      user.reload
+      expect(user.locale).to eq 'en'
+      expect(user.chosen_languages).to eq ['es', 'fr']
     end
     
-      describe '#working?' do
-    it 'should not be working until the first event was received' do
-      expect(@checker).not_to be_working
-      @checker.last_receive_at = Time.now
-      expect(@checker).to be_working
+        # Register a new view key with the view factory
+    #
+    # eg:
+    #
+    #   factory = AbstractViewFactory.new
+    #   factory.register my_view: SomeViewClass
+    #
+    # You can setup many at the same time:
+    #
+    #   factory.register  my_view: SomeClass,
+    #                     another_view: OtherViewClass
+    #
+    def register(view_hash)
+      self.class.register view_hash
     end
-  end
     
-        def self.can_extract?(path)
-      mime_type = 'application/vnd.adobe.air-application-installer-package+zip'
-      path.magic_number.match?(/.{59}#{Regexp.escape(mime_type)}/)
+          # Override the default to_s to include a closing form tag
+      def to_s
+        content + closing_form_tag
+      end
+    
+          def build(label_text = '')
+        label do
+          input type: 'checkbox', id: 'collection_selection_toggle_all', name: 'collection_selection_toggle_all', class: 'toggle_all'
+          text_node label_text if label_text.present?
+        end
+      end
     end
     
-    namespace :bower do
+    # Add a setting to the application to configure the ability
+ActiveAdmin::Application.inheritable_setting :cancan_ability_class, 'Ability'
+    
+      class DynamicSetting
+    def self.build(setting, type)
+      (type ? klass(type) : self).new(setting)
+    end
