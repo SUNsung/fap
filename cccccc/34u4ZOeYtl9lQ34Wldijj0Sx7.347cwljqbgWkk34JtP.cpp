23a27762d7dd41d2f37c269b84c9224a38ed7708
SUNsung <weatherius@gmail.com>
2019-08-20 16:40:05 +0300

    Rewrite [34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp] File in: 2019-08-20 16:40:05

--- cccccc/34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp
+++ cccccc/34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp
@@ -1,149 +1,146 @@
 
         
-        Dictionary::Dictionary(v8::Isolate* isolate, v8::Local<v8::Object> object)
-    : isolate_(isolate), object_(object) {}
+        // Build a Table file from the contents of *iter.  The generated file
+// will be named according to meta->number.  On success, the rest of
+// *meta will be filled with metadata about the generated table.
+// If no data is present in *iter, meta->file_size will be set to
+// zero, and no Table file will be produced.
+Status BuildTable(const std::string& dbname, Env* env, const Options& options,
+                  TableCache* table_cache, Iterator* iter, FileMetaData* meta);
     
-    Promise::Promise(v8::Isolate* isolate) : isolate_(isolate) {
-  resolver_ = v8::Promise::Resolver::New(isolate);
+    
+    {  // Force creation of multiple memtables by reducing the write buffer size.
+  Options opt;
+  opt.reuse_logs = true;
+  opt.write_buffer_size = (kNum * 100) / 2;
+  Open(&opt);
+  ASSERT_LE(2, NumTables());
+  ASSERT_EQ(1, NumLogs());
+  ASSERT_NE(old_log_file, FirstLogFile()) << 'must not reuse log';
+  for (int i = 0; i < kNum; i++) {
+    char buf[100];
+    snprintf(buf, sizeof(buf), '%050d', i);
+    ASSERT_EQ(buf, Get(buf));
+  }
 }
     
-      v8::Local<v8::Object> GetThis() { return info_->This(); }
+      Arena arena_;
     
-    template <typename P1, typename P2, typename P3, typename P4>
-inline WrappableBase* InvokeFactory(
-    Arguments* args,
-    const base::Callback<WrappableBase*(P1, P2, P3, P4)>& callback) {
-  typename CallbackParamTraits<P1>::LocalType a1;
-  typename CallbackParamTraits<P2>::LocalType a2;
-  typename CallbackParamTraits<P3>::LocalType a3;
-  typename CallbackParamTraits<P4>::LocalType a4;
-  if (!GetNextArgument(args, 0, true, &a1) ||
-      !GetNextArgument(args, 0, false, &a2) ||
-      !GetNextArgument(args, 0, false, &a3) ||
-      !GetNextArgument(args, 0, false, &a4))
-    return nullptr;
-  return callback.Run(a1, a2, a3, a4);
-}
-    
-    
-    {}  // namespace mate
-    
-      void reset() { handle_.Reset(); }
-    
-      template <typename Sig>
-  static void SetConstructor(v8::Isolate* isolate,
-                             const base::Callback<Sig>& constructor) {
-    v8::Local<v8::FunctionTemplate> templ = CreateFunctionTemplate(
-        isolate, base::Bind(&internal::InvokeNew<Sig>, constructor));
-    templ->InstanceTemplate()->SetInternalFieldCount(1);
-    T::BuildPrototype(isolate, templ);
-    gin::PerIsolateData::From(isolate)->SetFunctionTemplate(&kWrapperInfo,
-                                                            templ);
+      Cache::Handle* handle = nullptr;
+  Status s = FindTable(file_number, file_size, &handle);
+  if (!s.ok()) {
+    return NewErrorIterator(s);
   }
     
-    #if defined(OS_WIN)
-  HWND remote_window_;               // The HWND_MESSAGE of another browser.
-  base::win::MessageWindow window_;  // The message-only window.
-  bool is_virtualized_;  // Stuck inside Microsoft Softricity VM environment.
-  HANDLE lock_file_;
-  base::FilePath user_data_dir_;
-  ShouldKillRemoteProcessCallback should_kill_remote_process_callback_;
-#elif defined(OS_POSIX) && !defined(OS_ANDROID)
-  // Start listening to the socket.
-  void StartListening(int sock);
+      static Slice Contents(const WriteBatch* batch) { return Slice(batch->rep_); }
     
-      // The symlink lock is pointed to the hostname and process id, so other
-  // processes can find it out.
-  base::FilePath symlink_content(base::StringPrintf(
-      '%s%c%u', net::GetHostName().c_str(), kLockDelimiter, current_pid_));
+    #include 'leveldb/export.h'
+#include 'leveldb/slice.h'
     
-    // A map to cache Python Pools per C++ pointer.
-// Pointers are not owned here, and belong to the PyDescriptorPool.
-static std::unordered_map<const DescriptorPool*, PyDescriptorPool*>*
-    descriptor_pool_map;
+    #ifndef STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
+#define STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
     
-      // Make child side of the pipes inheritable.
-  if (!SetHandleInformation(stdin_pipe_read, HANDLE_FLAG_INHERIT,
-                            HANDLE_FLAG_INHERIT)) {
-    GOOGLE_LOG(FATAL) << 'SetHandleInformation: '
-                      << Win32ErrorMessage(GetLastError());
-  }
-  if (!SetHandleInformation(stdout_pipe_write, HANDLE_FLAG_INHERIT,
-                            HANDLE_FLAG_INHERIT)) {
-    GOOGLE_LOG(FATAL) << 'SetHandleInformation: '
-                      << Win32ErrorMessage(GetLastError());
-  }
+      RandomAccessFile(const RandomAccessFile&) = delete;
+  RandomAccessFile& operator=(const RandomAccessFile&) = delete;
     
-    #endif  // GOOGLE_PROTOBUF_STUBS_MATHUTIL_H_
-
+    public:
+  DHTReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
+                     const std::shared_ptr<DHTNode>& newNode);
     
-      MapTestUtil::SetMapFields(&message1);
-  MapTestUtil::SetMapFields(&message2);
-  MapTestUtil::ModifyMapFields(&message2);
-    
-    TEST_F(JsonObjectWriterTest, TestWebsafeByteEncoding) {
-  ow_ = new JsonObjectWriter('', out_stream_);
-  ow_->set_use_websafe_base64_for_bytes(true);
-  ow_->StartObject('')
-      ->RenderBytes('bytes', '\x03\xef\xc0\x10')
-      ->EndObject();
+    class DHTSetup {
+public:
+  DHTSetup();
     }
     
-      // Duplicate keys
-    
-    
-bool ZeroCopyOutputStream::WriteAliasedRaw(const void* /* data */,
-                                           int /* size */) {
-  GOOGLE_LOG(FATAL) << 'This ZeroCopyOutputStream doesn't support aliasing. '
-                'Reaching here usually means a ZeroCopyOutputStream '
-                'implementation bug.';
-  return false;
-}
-    
-    // Generate destructors.
-#include 'ipc/struct_destructor_macros.h'
-#include 'content/nw/src/common/common_message_generator.h'
-    
-      int id() const { return id_; }
-  std::string extension_id_;
-  ObjectManager* object_manager() const { return object_manager_.get(); }
-    
-    typedef std::map<std::string,std::string> KeyMap;
-    
-    class NwClipboardReadAvailableTypesFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardReadAvailableTypesFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    namespace aria2 {
     }
     
-    class OrientationDetector {
- public:
-  OrientationDetector(const GenericVector<int>* allowed_scripts,
-                      OSResults* results);
-  bool detect_blob(BLOB_CHOICE_LIST* scores);
-  int get_orientation();
- private:
-  OSResults* osr_;
-  const GenericVector<int>* allowed_scripts_;
+    class DownloadContext;
+class PeerStorage;
+class DHTTask;
+class DHTNode;
+class DHTBucket;
+    
+    
+    {} // namespace aria2
+    
+      virtual ~DHTUnknownMessage();
+    
+        const std::string& getGoodAddr() const;
+    
+        try {
+      pt::read_json(stream, tree);
+    } catch (const pt::ptree_error& e) {
+      stream.close();
+      return Status(
+          1, 'Error reading docker API response for ' + uri + ': ' + e.what());
+    }
+    
+    void validate_row(const Row& row, const ValidatatioMap& validation_map) {
+  for (auto const& rec : row) {
+    EXPECT_NE(validation_map.count(rec.first), std::size_t{0})
+        << 'Unexpected column ' << boost::io::quoted(rec.first) << ' in a row';
+  }
+  for (auto iter : validation_map) {
+    std::string key = iter.first;
+    auto row_data_iter = row.find(key);
+    ASSERT_NE(row_data_iter, row.end())
+        << 'Could not find column ' << boost::io::quoted(key)
+        << ' in the generated columns';
+    std::string value = row_data_iter->second;
+    ValidatatioDataType validator = iter.second;
+    if (validator.type() == typeid(int)) {
+      int flags = boost::get<int>(validator);
+      ASSERT_TRUE(validate_value_using_flags(value, flags))
+          << 'Standard validator of the column ' << boost::io::quoted(key)
+          << ' with value ' << boost::io::quoted(value) << ' failed';
+    } else {
+      ASSERT_TRUE(boost::get<CustomCheckerType>(validator)(value))
+          << 'Custom validator of the column ' << boost::io::quoted(key)
+          << ' with value ' << boost::io::quoted(value) << ' failed';
+    }
+  }
+}
+void validate_rows(const std::vector<Row>& rows,
+                   const ValidatatioMap& validation_map) {
+  for (auto row : rows) {
+    validate_row(row, validation_map);
+  }
+}
+    
+      // Initialize registries and plugins
+  registryAndPluginInit();
+    
+    #include <osquery/utils/info/platform_type.h>
+    
+      // Running status of the carver
+  Status status_;
+    
+      /**
+   * @brief When config sources are updated the config will 'purge'.
+   *
+   * The general 'purge' action applies to searching for outdated query results,
+   * timestamps, saved intervals, etc. This event only occurs before a source
+   * is updated. Since updating the configuration may have expected side effects
+   * such as changing watched files or overwriting (modifying) pack content,
+   * this 'purge' action is assumed to be destructive and potentially expensive.
+   */
+  void purge();
+    
+    // Get an example generate config with one static source name to JSON content.
+std::map<std::string, std::string> getTestConfigMap(const std::string& file);
+    
+    
+    {  // storage map is built on open, so no need to protect it with locks
+  std::unordered_map<std::string, InMemoryStorageRef> storage_;
 };
     
-    class TBOX;
-    
-    
-    {  // Stores all the source points in the order they were given and their
-  // halfwidths, if any.
-  GenericVector<PointWidth> pts_;
-  // Stores the computed perpendicular distances of (some of) the pts_ from a
-  // given vector (assuming it goes through the origin, making it a line).
-  // Since the distances may be a subset of the input points, and get
-  // re-ordered by the nth_item function, the original point is stored
-  // along side the distance.
-  GenericVector<DistPointPair> distances_;  // Distances of points.
-  // The squared length of the vector used to compute distances_.
-  double square_length_;
+    enum class RocksdbMigrationError {
+  InvalidArgument = 1,
+  FailToOpen = 2,
+  FailToGetVersion = 3,
+  NoMigrationFromCurrentVersion = 5,
+  MigrationLogicError = 6,
+  FailToOpenSrcDatabase = 7,
+  FailMoveDatabase = 8,
 };
-    
-    #include 'errcode.h'
-#include 'genericvector.h'
-    
-    #endif  // TESSERACT_CCUTIL_QRSEQUENCE_H_
