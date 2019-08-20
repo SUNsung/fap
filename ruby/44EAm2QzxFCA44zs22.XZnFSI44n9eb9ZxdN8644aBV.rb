354ac03c0d1639ad4f591853b096c4f74338c2a1
SUNsung <weatherius@gmail.com>
2019-08-20 05:07:19 +0300

    Rewrite [44EAm2QzxFCA44zs22.XZnFSI44n9eb9ZxdN8644aBV.rb] File in: 2019-08-20 05:07:19

--- ruby/44EAm2QzxFCA44zs22.XZnFSI44n9eb9ZxdN8644aBV.rb
+++ ruby/44EAm2QzxFCA44zs22.XZnFSI44n9eb9ZxdN8644aBV.rb
@@ -1,97 +1,70 @@
 
         
-                def extract_heredoc(node)
-          return node if heredoc_node?(node)
-          return node.receiver if single_line_send_with_heredoc_receiver?(node)
+            # advance scanner to pos after the next match of pattern and return the match
+    def scan_next(pattern)
+      return unless @s.scan_until(pattern)
+      @s.matched
+    end
     
-              check(node, node.arguments, 'parameter of %<article>s method call',
-                node.last_argument.source_range.end_pos,
-                node.source_range.end_pos)
-        end
-        alias on_csend on_send
-    
-              expect_offense(<<~RUBY)
-            def some_method(foo)
-                            ^^^ #{message}
-              binding(:something)
-            end
-          RUBY
-        end
+          def has_vector?(request, headers)
+        return false if request.xhr?
+        return false if options[:allow_if] && options[:allow_if].call(request.env)
+        return false unless headers['Content-Type'].to_s.split(';', 2).first =~ /^\s*application\/json\s*$/
+        origin(request.env).nil? and referrer(request.env) != request.host
       end
+    
+          def remove_page_extentions(page_path)
+        Gollum::Markup.formats.values.each do |format|
+          page_path = page_path.gsub(/\.#{format[:regexp]}$/, '')
+        end
+        return page_path
+      end
+    
+        assert_match /New/, last_response.body, ''New' link is blocked in pages template'
+    
+      test 'extracting paths from URLs' do
+    assert_nil extract_path('Eye-Of-Sauron')
+    assert_equal 'Mordor', extract_path('Mordor/Sauron')
+    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
+  end
+    
+    context 'Precious::Views::Page' do
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
+  end
+    
+    require 'gollum'
+require 'gollum/views/layout'
+require 'gollum/views/editable'
+require 'gollum/views/has_page'
+    
+        it 'accepts chat channel invitation' do
+      membership = ChatChannelMembership.last
+      sign_in second_user
+      put '/chat_channel_memberships/#{membership.id}', params: {
+        chat_channel_membership: {
+          user_action: 'accept'
+        }
+      }
+      expect(ChatChannelMembership.find(membership.id).status).to eq('active')
+    end
+    
+      def satellite_tweet!
+    @article.tags.find_each do |tag|
+      BufferUpdate.buff!(@article.id, twitter_buffer_text, tag.buffer_profile_id_code, 'twitter', tag.id) if tag.buffer_profile_id_code.present?
+    end
+    @article.update(last_buffered: Time.current)
+  end
+    
+      def self.upbuff!(buffer_update_id, admin_id, body_text, status)
+    buffer_update = BufferUpdate.find(buffer_update_id)
+    if status == 'confirmed'
+      buffer_response = send_to_buffer(body_text, buffer_update.buffer_profile_id_code)
+      buffer_update.update!(buffer_response: buffer_response, status: status, approver_user_id: admin_id, body_text: body_text)
+    else
+      buffer_update.update!(status: status, approver_user_id: admin_id)
     end
   end
-    
-      context 'when a variable is assigned and referenced when defining a module' do
-    it 'does not register an offense' do
-      expect_no_offenses(<<~RUBY)
-        x = Object.new
-        module x::Foo
-        end
-      RUBY
-    end
-  end
-    
-            rhs
-      end
-    end
-  end
-end
-
-    
-    RSpec.describe RuboCop::Cop::Lint::UnifiedInteger, :config do
-  subject(:cop) { described_class.new(config) }
-    
-    def with_logging(lvl=Logger::DEBUG)
-  old = Sidekiq.logger.level
-  begin
-    Sidekiq.logger.level = lvl
-    yield
-  ensure
-    Sidekiq.logger.level = old
-  end
-end
-
-    
-      def app
-    Sidekiq::Web
-  end
-    
-          describe 'failed' do
-        it 'retrieves hash of dates' do
-          Sidekiq.redis do |c|
-            c.incrby('stat:failed:2012-12-24', 4)
-            c.incrby('stat:failed:2012-12-25', 1)
-            c.incrby('stat:failed:2012-12-26', 6)
-            c.incrby('stat:failed:2012-12-27', 2)
-          end
-          Time.stub(:now, Time.parse('2012-12-26 1:00:00 -0500')) do
-            s = Sidekiq::Stats::History.new(2)
-            assert_equal ({ '2012-12-26' => 6, '2012-12-25' => 1 }), s.failed
-    
-        assert_nil dead_set.find_job('000101')
-    assert dead_set.find_job('000102')
-    assert dead_set.find_job('000103')
-  end
-end
-
-    
-          e = assert_raises ArgumentError do
-        Sidekiq.on(:startp)
-      end
-      assert_match(/Invalid event name/, e.message)
-      e = assert_raises ArgumentError do
-        Sidekiq.on('startup')
-      end
-      assert_match(/Symbols only/, e.message)
-      Sidekiq.on(:startup) do
-        1 + 1
-      end
-    
-        ## Write the results to a file
-    ## Requires railsexpress patched MRI build
-    # brew install qcachegrind
-    #File.open('callgrind.profile', 'w') do |f|
-      #RubyProf::CallTreePrinter.new(result).print(f, :min_percent => 1)
-    #end
-  end
-end
