b1ff1510017760f2d63bb94281b2e668bddad054
SUNsung <weatherius@gmail.com>
2019-08-20 23:40:19 +0300

    Rewrite [54D.M9ivKaMag54kSo0fhyOf7I54Q5Cz7w77L5g540IN.cpp] File in: 2019-08-20 23:40:19

--- cccccc/54D.M9ivKaMag54kSo0fhyOf7I54Q5Cz7w77L5g540IN.cpp
+++ cccccc/54D.M9ivKaMag54kSo0fhyOf7I54Q5Cz7w77L5g540IN.cpp
@@ -1,161 +1,213 @@
 
         
-        BOOST_FIXTURE_TEST_SUITE(univalue_tests, BasicTestingSetup)
+        #endif  // CHROME_BROWSER_UI_VIEWS_FRAME_GLOBAL_MENU_BAR_REGISTRAR_X11_H_
+
     
-          if ((step % 100) == 0) {
-        ASSERT_TRUE(CompareIterators(step, &model, db_, NULL, NULL));
-        ASSERT_TRUE(CompareIterators(step, &model, db_, model_snap, db_snap));
-        // Save a snapshot from each DB this time that we'll use next
-        // time we compare things, to make sure the current state is
-        // preserved with the snapshot
-        if (model_snap != NULL) model.ReleaseSnapshot(model_snap);
-        if (db_snap != NULL) db_->ReleaseSnapshot(db_snap);
-    }
     
-    template <>
-IMF_EXPORT
-void FloatVectorAttribute::writeValueTo
-    (OPENEXR_IMF_INTERNAL_NAMESPACE::OStream &, int) const;
+    {}  // namespace mate
+
     
-    OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_ENTER
+    template <typename P1, typename P2>
+inline WrappableBase* InvokeFactory(
+    Arguments* args,
+    const base::Callback<WrappableBase*(P1, P2)>& callback) {
+  typename CallbackParamTraits<P1>::LocalType a1;
+  typename CallbackParamTraits<P2>::LocalType a2;
+  if (!GetNextArgument(args, 0, true, &a1) ||
+      !GetNextArgument(args, 0, false, &a2))
+    return nullptr;
+  return callback.Run(a1, a2);
+}
     
-        if (abs (double (fps) - double (fps_59_94())) < e)
-	return fps_59_94();
+    #endif  // NATIVE_MATE_NATIVE_MATE_HANDLE_H_
+
     
-        if (header.hasType() && isDeepData(header.type()))
-    {
-        version |= NON_IMAGE_FLAG;
-    }
+    // This converter handles any subclass of Wrappable.
+template <typename T>
+struct Converter<T*,
+                 typename std::enable_if<
+                     std::is_convertible<T*, WrappableBase*>::value>::type> {
+  static v8::Local<v8::Value> ToV8(v8::Isolate* isolate, T* val) {
+    if (val)
+      return val->GetWrapper();
     else
-    {
-        // (TODO) we may want to check something else in function signature
-        // instead of hasTileDescription()?
-        if (header.hasTileDescription())
-            version |= TILED_FLAG;
-    }
-    
-    
-    
-    
-OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_EXIT
-    
-    #endif /* IMFINPUTPART_H_ */
-
-    
-    
-    {  if (!base::JSONWriter::Write(top_dictionary, &json))
-    return false;
-  int written = base::WriteFile(path, json.data(), (int)json.size());
-  if (static_cast<unsigned>(written) != json.size()) {
-    LOG(ERROR) << 'Error writing ' << path.AsUTF8Unsafe()
-               << ' ; write result:' << written << ' expected:' << json.size();
-    return false;
+      return v8::Null(isolate);
   }
-  return true;
-}
-    
-    
-    
-      bool delay_destruction() { return delay_destruction_; }
-  void set_delay_destruction(bool val) { delay_destruction_ = val; }
-  bool pending_destruction() { return pending_destruction_; }
-  void set_pending_destruction (bool val) { pending_destruction_ = val; }
- protected:
-  int id_;
-  bool delay_destruction_;
-  bool pending_destruction_;
-  base::WeakPtr<ObjectManager> object_manager_;
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
-};
-    
-      private:
-    bool WriteText(ClipboardData& data) {
-      DCHECK(data.type == TYPE_TEXT);
-      scw_->WriteText(base::UTF8ToUTF16(*(data.data)));
-      return true;
     }
     
-    bool ParagraphModel::Comparable(const ParagraphModel &other) const {
-  if (justification_ != other.justification_)
-    return false;
-  if (justification_ == JUSTIFICATION_CENTER ||
-      justification_ == JUSTIFICATION_UNKNOWN)
-    return true;
-  int tolerance = (tolerance_ + other.tolerance_) / 4;
-  return NearlyEqual(margin_ + first_indent_,
-                     other.margin_ + other.first_indent_, tolerance) &&
-         NearlyEqual(margin_ + body_indent_,
-                     other.margin_ + other.body_indent_, tolerance);
-}
+      static void FirstWeakCallback(
+      const v8::WeakCallbackInfo<WrappableBase>& data);
+  static void SecondWeakCallback(
+      const v8::WeakCallbackInfo<WrappableBase>& data);
     
-    RES ResourceFormatLoaderVideoStreamGDNative::load(const String &p_path, const String &p_original_path, Error *r_error) {
-	FileAccess *f = FileAccess::open(p_path, FileAccess::READ);
-	if (!f) {
-		if (r_error) {
-			*r_error = ERR_CANT_OPEN;
-		}
-		return RES();
-	}
-	memdelete(f);
-	VideoStreamGDNative *stream = memnew(VideoStreamGDNative);
-	stream->set_file(p_path);
-	Ref<VideoStreamGDNative> ogv_stream = Ref<VideoStreamGDNative>(stream);
-	if (r_error) {
-		*r_error = OK;
-	}
-	return ogv_stream;
-}
-    
-    	int shadow_atlas_size;
-	ShadowAtlasQuadrantSubdiv shadow_atlas_quadrant_subdiv[4];
-    
-    	void set_agent_height(float p_value);
-	float get_agent_height() const;
-    
-    protected:
-	Error set_error(Error p_error) const;
+    void GlobalMenuBarRegistrarX11::OnWindowMapped(unsigned long xid) {
+  live_xids_.insert(xid);
+    }
     
     
-    {	const char *ptr;
-	static StaticCString create(const char *p_ptr);
+    {}  // namespace
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(UvTaskRunner);
 };
     
+        QColor TextColor() const { return textColor; }
+    QColor SingleColor() const { return singleColor; }
     
-    {
-    {
-    {					if (bytes < 256) {
-						buffer[bytes++] = byte;
-						// If we know the size of the current packet receive it if it reached the expected size
-						if (bytes >= expected_size) {
-							md->receive_input_packet(timestamp, buffer, bytes);
-							bytes = 0;
-						}
-					}
-				}
-			} while (ret > 0);
-		}
+    int secp256k1_ecdsa_recoverable_signature_parse_compact(const secp256k1_context* ctx, secp256k1_ecdsa_recoverable_signature* sig, const unsigned char *input64, int recid) {
+    secp256k1_scalar r, s;
+    int ret = 1;
+    int overflow = 0;
+    }
     
-    	Vector<snd_rawmidi_t *> connected_inputs;
+    #include <stdio.h>
+#include 'db/dbformat.h'
+#include 'db/filename.h'
+#include 'db/log_reader.h'
+#include 'db/version_edit.h'
+#include 'db/write_batch_internal.h'
+#include 'leveldb/env.h'
+#include 'leveldb/iterator.h'
+#include 'leveldb/options.h'
+#include 'leveldb/status.h'
+#include 'leveldb/table.h'
+#include 'leveldb/write_batch.h'
+#include 'util/logging.h'
     
-    MIDIDriverCoreMidi::MIDIDriverCoreMidi() :
-		client(0) {
+    class Env;
+class Iterator;
+class TableCache;
+class VersionEdit;
+    
+      fname = TableFileName('bar', 200);
+  ASSERT_EQ('bar/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(200, number);
+  ASSERT_EQ(kTableFile, type);
+    
+      // REQUIRES: External synchronization
+  void WriteStep(Random* rnd) {
+    const uint32_t k = rnd->Next() % K;
+    const intptr_t g = current_.Get(k) + 1;
+    const Key key = MakeKey(k, g);
+    list_.Insert(key);
+    current_.Set(k, g);
+  }
+    
+    TEST(WriteBatchTest, Multiple) {
+  WriteBatch batch;
+  batch.Put(Slice('foo'), Slice('bar'));
+  batch.Delete(Slice('box'));
+  batch.Put(Slice('baz'), Slice('boo'));
+  WriteBatchInternal::SetSequence(&batch, 100);
+  ASSERT_EQ(100, WriteBatchInternal::Sequence(&batch));
+  ASSERT_EQ(3, WriteBatchInternal::Count(&batch));
+  ASSERT_EQ(
+      'Put(baz, boo)@102'
+      'Delete(box)@101'
+      'Put(foo, bar)@100',
+      PrintContents(&batch));
 }
     
-    #endif // WINMIDI_ENABLED
-
+    inline bool operator!=(const Slice& x, const Slice& y) { return !(x == y); }
     
-    godot_int GDAPI godot_pool_vector2_array_size(const godot_pool_vector2_array *p_self) {
-	const PoolVector<Vector2> *self = (const PoolVector<Vector2> *)p_self;
-	return self->size();
+      // count the keys
+  leveldb::Iterator* iter = db->NewIterator(leveldb::ReadOptions());
+  size_t num_keys = 0;
+  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
+    num_keys++;
+  }
+  delete iter;
+  ASSERT_EQ(kNumKeys, num_keys) << 'Bad number of keys';
+    
+    namespace leveldb {
+    }
+    
+    Status Footer::DecodeFrom(Slice* input) {
+  const char* magic_ptr = input->data() + kEncodedLength - 8;
+  const uint32_t magic_lo = DecodeFixed32(magic_ptr);
+  const uint32_t magic_hi = DecodeFixed32(magic_ptr + 4);
+  const uint64_t magic = ((static_cast<uint64_t>(magic_hi) << 32) |
+                          (static_cast<uint64_t>(magic_lo)));
+  if (magic != kTableMagicNumber) {
+    return Status::Corruption('not an sstable (bad magic number)');
+  }
+    }
+    
+    Status Table::InternalGet(const ReadOptions& options, const Slice& k, void* arg,
+                          void (*handle_result)(void*, const Slice&,
+                                                const Slice&)) {
+  Status s;
+  Iterator* iiter = rep_->index_block->NewIterator(rep_->options.comparator);
+  iiter->Seek(k);
+  if (iiter->Valid()) {
+    Slice handle_value = iiter->value();
+    FilterBlockReader* filter = rep_->filter;
+    BlockHandle handle;
+    if (filter != nullptr && handle.DecodeFrom(&handle_value).ok() &&
+        !filter->KeyMayMatch(handle.offset(), k)) {
+      // Not found
+    } else {
+      Iterator* block_iter = BlockReader(this, options, iiter->value());
+      block_iter->Seek(k);
+      if (block_iter->Valid()) {
+        (*handle_result)(arg, block_iter->key(), block_iter->value());
+      }
+      s = block_iter->status();
+      delete block_iter;
+    }
+  }
+  if (s.ok()) {
+    s = iiter->status();
+  }
+  delete iiter;
+  return s;
 }
     
-    	ClassDB::bind_method(D_METHOD('set_service_type', 'type'), &UPNPDevice::set_service_type);
-	ClassDB::bind_method(D_METHOD('get_service_type'), &UPNPDevice::get_service_type);
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, 'service_type'), 'set_service_type', 'get_service_type');
     
-    	bool is_valid_gateway() const;
-	String query_external_address() const;
-	int add_port_mapping(int port, int port_internal = 0, String desc = '', String proto = 'UDP', int duration = 0) const;
-	int delete_port_mapping(int port, String proto = 'UDP') const;
+    {  expected = {'select pid from processes where name = 'foobar';'};
+  Pack fpack('discovery_pack', getPackWithDiscovery().doc());
+  EXPECT_EQ(fpack.getDiscoveryQueries(), expected);
+}
+    
+    namespace fs = boost::filesystem;
+    
+    boost::filesystem::path const& getTestConfigDirectory();
+    
+    #include <osquery/core/database/database.h>
+    
+      Expected<int32_t, DatabaseError> getInt32(const std::string& domain,
+                                            const std::string& key) override;
+  Expected<std::string, DatabaseError> getString(
+      const std::string& domain, const std::string& key) override;
+    
+      rj::Document doc;
+  if (doc.Parse(request.at('log').c_str()).HasParseError()) {
+    return;
+  }
+    
+      /**
+   * @brief See the usesLogStatus method, log a Glog status.
+   *
+   * @param log A vector of parsed Glog log lines.
+   * @return Status non-op indicating success or failure.
+   */
+  virtual Status logStatus(const std::vector<StatusLogLine>& log) {
+    (void)log;
+    return Status(1, 'Not enabled');
+  }
+    
+    void Plugin::setName(const std::string& name) {
+  if (!name_.empty() && name != name_) {
+    std::string error = 'Cannot rename plugin ' + name_ + ' to ' + name;
+    throw std::runtime_error(error);
+  }
+    }
+    
+    /**
+ * @brief The response part of a plugin (registry item's) call.
+ *
+ * If a Registry::call succeeds it will fill in a PluginResponse.
+ * This response is a vector of key value maps.
+ */
+using PluginResponse = std::vector<PluginRequest>;
