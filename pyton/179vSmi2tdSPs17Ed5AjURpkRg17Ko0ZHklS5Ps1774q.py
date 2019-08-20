f309846363a31ea7532939b322b7aad82ad63ce1
SUNsung <weatherius@gmail.com>
2019-08-20 11:07:25 +0300

    Rewrite [179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py] File in: 2019-08-20 11:07:25

--- pyton/179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py
+++ pyton/179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py
@@ -1,129 +1,114 @@
 
         
-            def is_face_card(self):
-        '''Jack = 11, Queen = 12, King = 13'''
-        return True if 10 < self._value <= 13 else False
+        
+class ChunkedEncodingError(RequestException):
+    '''The server declared chunked encoding but sent an invalid chunk.'''
     
-        def parse_query(self, query):
-        '''Remove markup, break text into terms, deal with typos,
-        normalize capitalization, convert to use boolean operations.
-        '''
-        ...
+        def test_copy(self):
+        copy = self.case_insensitive_dict.copy()
+        assert copy is not self.case_insensitive_dict
+        assert copy == self.case_insensitive_dict
     
-        # Define the loss function and the optimization algorithm
-    self.cross_entropies = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=self.scores, labels=self.labels)
-    self.cost = tf.reduce_sum(self.cross_entropies, name='cost')
-    self.global_step = tf.Variable(0, name='global_step', trainable=False)
-    self.optimizer = tf.train.AdamOptimizer()
-    self.train_op = self.optimizer.minimize(
-        self.cost, global_step=self.global_step)
-    
-        # The output is [num_batch_paths, max_path_len, output_dim].
-    self.lstm_outputs, _ = tf.nn.dynamic_rnn(
-        lstm_cell, self.path_matrix, dtype=tf.float32,
-        sequence_length=self.sequence_lengths)
-    
-      return d
-    
-      # Project all the channels data onto the low-D PCA basis, where
-  # low-d is the npcs parameter.
-  all_data_pca_pxtc = np.dot(evecs_nxn[:, 0:npcs].T, all_data_zm_nxtc)
-    
-          if len(example) > num_steps:
-        final_x = example[:num_steps]
-        final_y = example[1:(num_steps + 1)]
-        w[i] = 1
+                if self.wait_to_close_event:
+                self.wait_to_close_event.wait(self.WAIT_EVENT_TIMEOUT)
+        finally:
+            self.ready_event.set() # just in case of exception
+            self._close_server_sock_ignore_errors()
+            self.stop_event.set()
     
     
-def ptb_raw_data(data_path=None):
-  '''Load PTB raw data from data directory 'data_path'.
-  Reads PTB text files, converts strings to integer ids,
-  and performs mini-batching of the inputs.
-  The PTB dataset comes from Tomas Mikolov's webpage:
-  http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
-  Args:
-    data_path: string path to the directory where simple-examples.tgz has
-      been extracted.
-  Returns:
-    tuple (train_data, valid_data, test_data, vocabulary)
-    where each of the data objects can be passed to PTBIterator.
-  '''
-    
-      Args:
-    predictions:  Discriminator linear predictions Tensor of shape [batch_size,
-      sequence_length]
-    labels: Labels for predictions, Tensor of shape [batch_size,
-      sequence_length]
-    missing_tokens:  Indicator for the missing tokens.  Evaluate the loss only
-      on the tokens that were missing.
-    
-        Args:
-      time: positive integer constant reflecting the current timestep.
-      cell_state: state of RNNCell.
-      cell_input: input provided by `dynamic_rnn_decoder`.
-      cell_output: output of RNNCell.
-      context_state: context state provided by `dynamic_rnn_decoder`.
-    
-    
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
-    
-    
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
-    
-        assert get_new_command(Command('brew install aa',
-                                   brew_no_available_formula))\
-        != 'brew install aha'
+def dispatch_hook(key, hooks, hook_data, **kwargs):
+    '''Dispatches a hook dictionary on a given piece of data.'''
+    hooks = hooks or {}
+    hooks = hooks.get(key)
+    if hooks:
+        if hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        for hook in hooks:
+            _hook_data = hook(hook_data, **kwargs)
+            if _hook_data is not None:
+                hook_data = _hook_data
+    return hook_data
 
     
-                        resp = HTTPResponse.from_httplib(
-                        r,
-                        pool=conn,
-                        connection=low_conn,
-                        preload_content=False,
-                        decode_content=False
-                    )
-                except:
-                    # If we hit any problems here, clean up the connection.
-                    # Then, reraise so that we can handle the actual exception.
-                    low_conn.close()
-                    raise
+    # Syntax sugar.
+_ver = sys.version_info
     
-                # the (n+1)th request fails
-            with pytest.raises(requests.exceptions.ConnectionError):
-                r = requests.get(server_url)
+    # 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
     
-        def prepare(self):
-        '''Constructs a :class:`PreparedRequest <PreparedRequest>` for transmission and returns it.'''
-        p = PreparedRequest()
-        p.prepare(
-            method=self.method,
-            url=self.url,
-            headers=self.headers,
-            files=self.files,
-            data=self.data,
-            json=self.json,
-            params=self.params,
-            auth=self.auth,
-            cookies=self.cookies,
-            hooks=self.hooks,
-        )
-        return p
+        def process_appid_not_exist(self, appid, ip):
+        ret = self.check_api(ip, 'xxnet-1')
+        if ret and ret.ok:
+            self.set_appid_not_exist(appid)
+        else:
+            self.logger.warn('process_appid_not_exist, remove ip:%s', ip)
     
-        @pytest.mark.parametrize(
-        'old_uri, new_uri', (
-            ('https://example.com:443/foo', 'https://example.com/bar'),
-            ('http://example.com:80/foo', 'http://example.com/bar'),
-            ('https://example.com/foo', 'https://example.com:443/bar'),
-            ('http://example.com/foo', 'http://example.com:80/bar')
-        ))
-    def test_should_strip_auth_default_port(self, old_uri, new_uri):
-        s = requests.Session()
-        assert not s.should_strip_auth(old_uri, new_uri)
     
-    # If false, no index is generated.
-# html_use_index = True
+def get_line_value(r, n):
+    rls = r.split('\r\n')
+    if len(rls) < n + 1:
+        return None
+    
+                    # not in range and not EOF/EOT, must be invalid symbol
+                self.noViableAlt(s, input)
+                return 0
+    
+    
+############################################################################
+#
+# token implementations
+#
+# Token
+# +- CommonToken
+# \- ClassicToken
+#
+############################################################################
+    
+    
+def test_iloc_nonunique():
+    s = Series([0, 1, 2], index=[0, 1, 0])
+    assert s.iloc[2] == 2
+
+    
+    
+from .pandas_vb_common import setup  # noqa: F401
+
+    
+    
+def _FormatYcmdDebugInfo( ycmd ):
+  python = ycmd[ 'python' ]
+  clang = ycmd[ 'clang' ]
+  message = ( 'Server Python interpreter: {0}\n'
+              'Server Python version: {1}\n'
+              'Server has Clang support compiled in: {2}\n'
+              'Clang version: {3}\n'.format( python[ 'executable' ],
+                                             python[ 'version' ],
+                                             clang[ 'has_support' ],
+                                             clang[ 'version' ] ) )
+  extra_conf = ycmd[ 'extra_conf' ]
+  extra_conf_path = extra_conf[ 'path' ]
+  if not extra_conf_path:
+    message += 'No extra configuration file found\n'
+  elif not extra_conf[ 'is_loaded' ]:
+    message += ( 'Extra configuration file found but not loaded\n'
+                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
+  else:
+    message += ( 'Extra configuration file found and loaded\n'
+                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
+  return message
+    
+      _assert_rejects( f, 'This is a Taco' )
+  _assert_rejects( f, 'This is a Burrito' )
+    
+      def shutdown( self, wait=True ):
+    with self._shutdown_lock:
+      self._shutdown = True
+      self._work_queue.put( None )
+    if wait:
+      for t in self._threads:
+        t.join()
+  shutdown.__doc__ = _base.Executor.shutdown.__doc__
