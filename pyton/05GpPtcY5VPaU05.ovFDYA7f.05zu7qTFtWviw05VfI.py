887ae380b463d1f9d6340cf77e75ae3e4a532d15
SUNsung <weatherius@gmail.com>
2019-08-21 16:43:50 +0300

    Rewrite [05GpPtcY5VPaU05.ovFDYA7f.05zu7qTFtWviw05VfI.py] File in: 2019-08-21 16:43:50

--- pyton/05GpPtcY5VPaU05.ovFDYA7f.05zu7qTFtWviw05VfI.py
+++ pyton/05GpPtcY5VPaU05.ovFDYA7f.05zu7qTFtWviw05VfI.py
@@ -1,133 +1,255 @@
 
         
-            def __init__(self, value, suit):
-        self.value = value
-        self.suit = suit
-        self.is_available = True
+                paths = []
+        for manifest_url in (info.get('manifest') or {}).values():
+            if not manifest_url:
+                continue
+            manifest_url = update_url_query(manifest_url, {'filter': ''})
+            path = self._search_regex(r'https?://[^/]+/(.+?)\.ism/', manifest_url, 'path')
+            if path in paths:
+                continue
+            paths.append(path)
     
-        def get(self, query):
-        '''Get the stored query result from the cache.
-    
-        def __init__(self, license_plate):
-        super(Motorcycle, self).__init__(VehicleSize.MOTORCYCLE, license_plate, spot_size=1)
-    
-    
-class UnaccentExtension(CreateExtension):
-    
-        def exists(self, session_key=None):
-        '''
-        This method makes sense when you're talking to a shared resource, but
-        it doesn't matter when you're storing the information in the client's
-        cookie.
-        '''
-        return False
-    
-        objects = BaseSessionManager()
-    
-    '''
-    
-        if not HAS_BOTO3:
-        module.fail_json(msg='boto3 required for this module')
-    
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.gcp import check_min_pkg_version, get_google_cloud_credentials
-    
-        fwservice = gateway._getFirewallService()
-    
-            if group is not None:
-            changed = client.modify_if_diff(name, ipa_role.get('member_group', []), group,
-                                            client.role_add_group,
-                                            client.role_remove_group) or changed
-        if host is not None:
-            changed = client.modify_if_diff(name, ipa_role.get('member_host', []), host,
-                                            client.role_add_host,
-                                            client.role_remove_host) or changed
-    
-        if module.params['user']:
-        params['deploy[local_username]'] = module.params['user']
-    
-    RETURN = '''# '''
-    
-    import time
-import re
-    
-    
-def delete(resource):
-    return request(resource, xml=None, method='DELETE')
-    
-    # Documents to append as an appendix to all manuals.
-#texinfo_appendices = []
-    
-    # This is a very simple benchmark to give you an idea of how fast each step of face recognition will run on your system.
-# Notice that face detection gets very slow at large image sizes. So you might consider running face detection on a
-# scaled down version of your image and then running face encodings on the the full size image.
-    
-    For example, if k=3, and the three closest face images to the given image in the training set are one image of Biden
-and two images of Obama, The result would be 'Obama'.
-    
-        # Display the resulting image
-    cv2.imshow('Video', frame)
-    
-        # Loop over each face found in the frame to see if it's someone we know.
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
-        name = '<Unknown Person>'
-    
-    
-# Remove the drawing library from memory as per the Pillow docs
-del draw
-    
-            face_encoding_a1 = api.face_encodings(img_a1)[0]
-        face_encoding_a2 = api.face_encodings(img_a2)[0]
-        face_encoding_a3 = api.face_encodings(img_a3)[0]
-        face_encoding_b1 = api.face_encodings(img_b1)[0]
-    
-    setup(
-    name='face_recognition',
-    version='1.2.3',
-    description='Recognize faces from Python or from the command line',
-    long_description=readme + '\n\n' + history,
-    author='Adam Geitgey',
-    author_email='ageitgey@gmail.com',
-    url='https://github.com/ageitgey/face_recognition',
-    packages=[
-        'face_recognition',
-    ],
-    package_dir={'face_recognition': 'face_recognition'},
-    package_data={
-        'face_recognition': ['models/*.dat']
-    },
-    entry_points={
-        'console_scripts': [
-            'face_recognition=face_recognition.face_recognition_cli:main',
-            'face_detection=face_recognition.face_detection_cli:main'
-        ]
-    },
-    install_requires=requirements,
-    license='MIT license',
-    zip_safe=False,
-    keywords='face_recognition',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
-)
+            return {
+            'id': video_id,
+            'title': title,
+            'formats': formats,
+            'thumbnails': thumbnails,
+            'description': video_data.get('short_description'),
+            'like_count': int_or_none(video_data.get('like_count')),
+            'timestamp': parse_iso8601(video_data.get('released_at')),
+            'series': series,
+            'episode': episode,
+            'season': season,
+            'season_id': season_id,
+            'season_number': season_number,
+            'episode_number': episode_number,
+            'subtitles': subtitles,
+            'age_limit': parse_age_limit(video_data.get('rating')),
+        }
 
     
-        # 载入用户上传的图片
-    img = face_recognition.load_image_file(file_stream)
-    # 为用户上传的图片中的人脸编码
-    unknown_face_encodings = face_recognition.face_encodings(img)
+    
+def hex_str(int_list):
+    return codecs.encode(intlist_to_bytes(int_list), 'hex')
+    
+    print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
+    
+    import youtube_dl
+    
+    
+def main():
+    parser = optparse.OptionParser(usage='%prog OUTFILE.md')
+    options, args = parser.parse_args()
+    if len(args) != 1:
+        parser.error('Expected an output filename')
+    
+        outfile, = args
+    
+    
+class AppidManager(object):
+    lock = threading.Lock()
+    
+    if __name__ == '__main__':
+    python_path = os.path.abspath( os.path.join(current_path, os.pardir, os.pardir, 'python27', '1.0'))
+    
+    
+
+    
+    
+class MismatchedNotSetException(MismatchedSetException):
+    '''@brief Used for remote debugger deserialization'''
+    
+    def __str__(self):
+        return 'MismatchedNotSetException(%r!=%r)' % (
+            self.getUnexpectedType(), self.expecting
+            )
+    __repr__ = __str__
+    
+    PYTHON3_IMPORTERROR_EXCEPTIONS = (
+    'ModuleNotFoundError',
+)
+    
+        def test_path_like_ob(self):
+        filename = 'LICENSE.txt'
+        filepath = pathlib.Path(filename)
+        filepath_with_abs_dir = pathlib.Path('/dir/'+filename)
+        filepath_relative = pathlib.Path('../dir/'+filename)
+        path_dir = pathlib.Path('./')
+    
+            Optional `rest' argument can be a string that is sent as the
+        argument to a REST command.  This is essentially a server
+        marker used to tell the server to skip over any data up to the
+        given marker.
+        '''
+        size = None
+        if self.passiveserver:
+            host, port = self.makepasv()
+            conn = socket.create_connection((host, port), self.timeout,
+                                            source_address=self.source_address)
+            try:
+                if rest is not None:
+                    self.sendcmd('REST %s' % rest)
+                resp = self.sendcmd(cmd)
+                # Some servers apparently send a 200 reply to
+                # a LIST or STOR command, before the 150 reply
+                # (and way before the 226 reply). This seems to
+                # be in violation of the protocol (which only allows
+                # 1xx or error messages for LIST), so we just discard
+                # this response.
+                if resp[0] == '2':
+                    resp = self.getresp()
+                if resp[0] != '1':
+                    raise error_reply(resp)
+            except:
+                conn.close()
+                raise
+        else:
+            with self.makeport() as sock:
+                if rest is not None:
+                    self.sendcmd('REST %s' % rest)
+                resp = self.sendcmd(cmd)
+                # See above.
+                if resp[0] == '2':
+                    resp = self.getresp()
+                if resp[0] != '1':
+                    raise error_reply(resp)
+                conn, sockaddr = sock.accept()
+                if self.timeout is not _GLOBAL_DEFAULT_TIMEOUT:
+                    conn.settimeout(self.timeout)
+        if resp[:3] == '150':
+            # this is conditional in case we received a 125
+            size = parse150(resp)
+        return conn, size
+    
+            Result is tuple of 2 ints (message count, mailbox size)
+        '''
+        retval = self._shortcmd('STAT')
+        rets = retval.split()
+        if self._debugging: print('*stat*', repr(rets))
+        numMessages = int(rets[1])
+        sizeMessages = int(rets[2])
+        return (numMessages, sizeMessages)
+    
+        def consume_timeout(self, q, results, sentinel):
+        while True:
+            while True:
+                try:
+                    val = q.get(timeout=1e-5)
+                except self.queue.Empty:
+                    pass
+                else:
+                    break
+            if val == sentinel:
+                return
+            results.append(val)
+    
+            The payload will either be a list object or a string.  If you mutate
+        the list object, you modify the message's payload in place.  Optional
+        i returns that index into the payload.
+    
+    
+        def _communicate(self, input, endtime, orig_timeout):
+            # Start reader threads feeding into a list hanging off of this
+            # object, unless they've already been started.
+            if self.stdout and not hasattr(self, '_stdout_buff'):
+                self._stdout_buff = []
+                self.stdout_thread = \
+                        threading.Thread(target=self._readerthread,
+                                         args=(self.stdout, self._stdout_buff))
+                self.stdout_thread.daemon = True
+                self.stdout_thread.start()
+            if self.stderr and not hasattr(self, '_stderr_buff'):
+                self._stderr_buff = []
+                self.stderr_thread = \
+                        threading.Thread(target=self._readerthread,
+                                         args=(self.stderr, self._stderr_buff))
+                self.stderr_thread.daemon = True
+                self.stderr_thread.start()
+    
+            async def test():
+            async with asyncio.StreamServer(handle_client, '127.0.0.1', 0) as server:
+                await server.start_serving()
+                task = asyncio.create_task(client(server))
+                await fut1
+                fut2.set_result(None)
+                await server.close()
+                await task
+    
+        #Parsing the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dir_path', help = 'Path to tldr 'pages' directory')
+    parser.add_argument('-c', choices=['solarized-light', 'solarized-dark'], help='Color scheme of the PDF')
+    args = parser.parse_args()
+    
+    loc = args.dir_path
+    if args.c == 'solarized-light' or args.c == 'solarized-dark':
+        colorscheme = args.c
+        
+    main(loc, colorscheme)
+
+    
+            # Use GaussianFill for class-agnostic mask prediction; fills based on
+        # fan-in can be too large in this case and cause divergence
+        fill = (
+            cfg.MRCNN.CONV_INIT
+            if cfg.MRCNN.CLS_SPECIFIC_MASK else 'GaussianFill'
+        )
+        blob_out = model.Conv(
+            blob_in,
+            'mask_fcn_logits',
+            dim,
+            num_cls,
+            kernel=1,
+            pad=0,
+            stride=1,
+            weight_init=(fill, {'std': 0.001}),
+            bias_init=const_fill(0.0)
+        )
+    
+        def test_handles_odd_length_arrays(self):
+        X = np.random.randn(77).astype(np.float32)
+        Y_exp = np.copy(X)
+        Y_exp[0::2] = 0.0
+        Y_act = self._run_zero_even_op(X)
+        np.testing.assert_allclose(Y_act, Y_exp)
+    
+        return all_results
+    
+        cdo_cuda = mutils.get_device_option_cuda()
+    cdo_cpu = mutils.get_device_option_cpu()
+    
+    # mapping coco categories to cityscapes (our converted json) id
+# cityscapes
+# INFO roidb.py: 220: 1       bicycle: 7286
+# INFO roidb.py: 220: 2           car: 53684
+# INFO roidb.py: 220: 3        person: 35704
+# INFO roidb.py: 220: 4         train: 336
+# INFO roidb.py: 220: 5         truck: 964
+# INFO roidb.py: 220: 6    motorcycle: 1468
+# INFO roidb.py: 220: 7           bus: 758
+# INFO roidb.py: 220: 8         rider: 3504
+    
+    
+def get_im_prefix(name):
+    '''Retrieve the image prefix for the dataset.'''
+    return _DATASETS[name][_IM_PREFIX] if _IM_PREFIX in _DATASETS[name] else ''
+    
+    from detectron.core.config import cfg
+from detectron.modeling.generate_anchors import generate_anchors
+from detectron.utils.c2 import const_fill
+from detectron.utils.c2 import gauss_fill
+from detectron.utils.net import get_group_gn
+import detectron.modeling.ResNet as ResNet
+import detectron.utils.blob as blob_utils
+import detectron.utils.boxes as box_utils
