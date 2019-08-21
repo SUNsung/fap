c76bc323b211ead8d0e750f818257ab2359c072b
SUNsung <weatherius@gmail.com>
2019-08-21 10:01:24 +0300

    Rewrite [1558iKO.9UVDg15LQiRrUMwqHo15mx3RpWHQZ6o152NY.py] File in: 2019-08-21 10:01:23

--- pyton/1558iKO.9UVDg15LQiRrUMwqHo15mx3RpWHQZ6o152NY.py
+++ pyton/1558iKO.9UVDg15LQiRrUMwqHo15mx3RpWHQZ6o152NY.py
@@ -1,149 +1,124 @@
 
         
-            def __init__(self, employee_id, name, rank, call_center):
-        self.employee_id = employee_id
-        self.name = name
-        self.rank = rank
-        self.call = None
-        self.call_center = call_center
+        
+def report_ok(ip):
+    if '.' in ip:
+        IPv4.report_ok()
+    else:
+        IPv6.report_ok()
     
-        def reducer(self, key, values):
-        total = sum(values)
-        if total == 1:
-            yield key, total
+            sorted_similarity transforms a distance matrix into a sorted distance matrix according to
+        the order implied by the hierarchical tree (dendrogram)
+        '''
+        logger.info('Sorting face distances. Depending on your dataset this may take some time...')
+        num_predictions = predictions.shape[0]
+        result_linkage = linkage(predictions, method=method, preserve_input=False)
+        result_order = self.seriation(result_linkage,
+                                      num_predictions,
+                                      num_predictions + num_predictions - 2)
     
+        def __init__(self, parent):
+        logger.debug('Initializing %s', self.__class__.__name__)
+        ttk.Notebook.__init__(self, parent, width=780)
+        parent.add(self)
+        tk_vars = get_config().tk_vars
+        self.wrapper_var = tk_vars['display']
+        self.runningtask = tk_vars['runningtask']
     
-class HashTable(object):
+        The following variables should be defined:
+        _HELPTEXT: A string describing what this plugin does
+        _DEFAULTS: A dictionary containing the options, defaults and meta information. The
+                   dictionary should be defined as:
+                       {<option_name>: {<metadata>}}
     
-        def crawl_page(self, page):
-        for url in page.child_urls:
-            self.data_store.add_link_to_crawl(url)
-        self.reverse_index_queue.generate(page)
-        self.doc_index_queue.generate(page)
-        self.data_store.remove_link_to_crawl(page.url)
-        self.data_store.insert_crawled_link(page.url, page.signature)
+        def set_config(self, configfile, config):
+        ''' Set the config to either global config or passed in config '''
+        section = '.'.join(self.__module__.split('.')[-2:])
+        if config is None:
+            logger.debug('Loading base config')
+            retval = get_config(section, configfile=configfile)
+        else:
+            logger.debug('Loading passed in config')
+            config.section = section
+            retval = config.config_dict
+            config.section = None
+        logger.debug('Config: %s', retval)
+        return retval
     
-            def split_url(url):
-            '''
-            Return a list of url parts via urlparse.urlsplit(), or raise
-            ValidationError for some malformed URLs.
-            '''
-            try:
-                return list(urlsplit(url))
-            except ValueError:
-                # urlparse.urlsplit can raise a ValueError with some
-                # misformatted URLs.
-                raise ValidationError(self.error_messages['invalid'], code='invalid')
+        def check_transparency_format(self):
+        ''' Make sure that the output format is correct if draw_transparent is selected '''
+        transparent = self.config['draw_transparent']
+        if not transparent or (transparent and self.config['format'] == 'png'):
+            return
+        logger.warning('Draw Transparent selected, but the requested format does not support '
+                       'transparency. Changing output format to 'png'')
+        self.config['format'] = 'png'
     
+            As an alternative you might even in case of success
+        call the next handler.
+        '''
+        res = self.check_range(request)
+        if not res and self.successor:
+            self.successor.handle(request)
     
-# admin and form for #18263
-class SomeChildModelForm(forms.ModelForm):
+    from __future__ import print_function
+import os
     
-        def test_inline_add_m2m_add_perm(self):
-        permission = Permission.objects.get(codename='add_book', content_type=self.book_ct)
-        self.user.user_permissions.add(permission)
-        response = self.client.get(reverse('admin:admin_inlines_author_add'))
-        # No change permission on Books, so no inline
-        self.assertNotContains(response, '<h2>Author-book relationships</h2>')
-        self.assertNotContains(response, 'Add another Author-Book Relationship')
-        self.assertNotContains(response, 'id='id_Author_books-TOTAL_FORMS'')
-    
-        def as_oracle(self, compiler, connection, **extra_context):
-        if self.output_field.get_internal_type() == 'DurationField':
-            expression = self.get_source_expressions()[0]
-            options = self._get_repr_options()
-            from django.db.backends.oracle.functions import IntervalToSeconds, SecondsToInterval
-            return compiler.compile(
-                SecondsToInterval(self.__class__(IntervalToSeconds(expression), **options))
-            )
-        return super().as_sql(compiler, connection, **extra_context)
-    
-        def distance_expr_for_lookup(self, lhs, rhs, **kwargs):
-        return super().distance_expr_for_lookup(
-            self._normalize_distance_lookup_arg(lhs),
-            self._normalize_distance_lookup_arg(rhs),
-            **kwargs
-        )
-    
-        @cached_property
-    def max_name_length(self):
-        # Allow an index name longer than 30 characters when the suffix is
-        # longer than the usual 3 character limit. The 30 character limit for
-        # cross-database compatibility isn't applicable to PostgreSQL-specific
-        # indexes.
-        return Index.max_name_length - len(Index.suffix) + len(self.suffix)
-    
-        def test_default_options(self):
-        args = ['test', '--settings=test_project.settings']
-        out, err = self.run_django_admin(args)
-        self.assertNoOutput(err)
-        self.assertOutput(out, '1:2:3')
-    
-                # Checking the alias dictionary (case-insensitive) to see if an
-            # alias exists for the given driver.
-            if dr_input.lower() in self._alias:
-                name = self._alias[dr_input.lower()]
-            else:
-                name = dr_input
-    
-        def __len__(self):
-        'Return the count of fields in this feature.'
-        return self.num_fields
-    
-            # If response is not 4xx, do not auth
-        # See https://github.com/requests/requests/issues/3772
-        if not 400 <= r.status_code < 500:
-            self._thread_local.num_401_calls = 1
-            return r
-    
-        # Check chardet for compatibility.
-    major, minor, patch = chardet_version.split('.')[:3]
-    major, minor, patch = int(major), int(minor), int(patch)
-    # chardet >= 3.0.2, < 3.1.0
-    assert major == 3
-    assert minor < 1
-    assert patch >= 2
-    
-        def inner(*suffix):
-        return urljoin(httpbin_url, '/'.join(suffix))
-    
-            self.host = host
-        self.port = port
-        self.requests_to_handle = requests_to_handle
+    '''
+http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
+Implementation of the iterator pattern with a generator
     
     
-def remove_cookie_by_name(cookiejar, name, domain=None, path=None):
-    '''Unsets a cookie by name, by default over all domains and paths.
+class UserSpecification(CompositeSpecification):
+    def is_satisfied_by(self, candidate):
+        return isinstance(candidate, User)
     
-        c = Controller(blackboard)
-    contributions = c.run_loop()
+        def toggle_amfm(self):
+        print(u'Switching to FM')
+        self.radio.state = self.radio.fmstate
     
-    https://en.wikipedia.org/wiki/Blackboard_system
+    The idea is to abstract the creation of objects depending on business
+logic, platform choice, etc.
+    
+    '''
+*What is this pattern about?
+It decouples the creation of a complex object and its representation,
+so that the same process can be reused to build objects from the same
+family.
+This is useful when you must separate the specification of an object
+from its actual representation (generally for abstraction).
+    
+    
+class GreekLocalizer(object):
+    '''A simple localizer a la gettext'''
+    
+        def test_object(queue):
+        pool = ObjectPool(queue, True)
+        print('Inside func: {}'.format(pool.item))
+    
+    
+def gradient_check():
+    '''
+    梯度检查
+    '''
+    # 设计一个误差函数，取所有节点输出项之和
+    error_function = lambda o: o.sum()
+    
+    rnn = RecursiveLayer(2, 2, IdentityActivator(), 1e-3)
+    
+    '''
+Created on 2017-05-18
+Update  on 2017-05-18
+Author: Peter Harrington/1988/片刻
+GitHub: https://github.com/apachecn/AiLearning
 '''
+from __future__ import print_function
     
-    Request receiver in simple form keeps a reference to a single successor.
-As a variation some receivers may be capable of sending requests out
-in several directions, forming a `tree of responsibility`.
-    
+        Args:
+        nodeToTest  满足minSup {所有的元素+(value, treeNode)}
+        targetNode  Tree对象的子节点
     '''
-*TL;DR
-Encapsulates all information needed to perform an action or trigger an event.
-    
-        # Counting to five...
-    >>> for number in count_to_five():
-    ...     print(number)
-    one
-    two
-    three
-    four
-    five
-    '''
-    
-    *TL;DR
-Maintains a list of dependents and notifies them of any state changes.
-    
-    
-def main():
-    '''
-    >>> Order(100)
-    <Price: 100, price after discount: 100>
+    # 建立相同元素之间的关系，例如： 左边的r指向右边的r值
+    while (nodeToTest.nodeLink is not None):
+        nodeToTest = nodeToTest.nodeLink
+    nodeToTest.nodeLink = targetNode
