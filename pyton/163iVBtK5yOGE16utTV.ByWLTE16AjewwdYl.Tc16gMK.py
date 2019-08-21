580d6bf406f0d97a03a558bff9c421f3a057a3b5
SUNsung <weatherius@gmail.com>
2019-08-21 23:28:00 +0300

    Rewrite [163iVBtK5yOGE16utTV.ByWLTE16AjewwdYl.Tc16gMK.py] File in: 2019-08-21 23:27:59

--- pyton/163iVBtK5yOGE16utTV.ByWLTE16AjewwdYl.Tc16gMK.py
+++ pyton/163iVBtK5yOGE16utTV.ByWLTE16AjewwdYl.Tc16gMK.py
@@ -1,67 +1,168 @@
 
         
-                thumbnails = []
-        for k, v in item.items():
-            if k.startswith('photo_') and v:
-                width = k.replace('photo_', '')
-                thumbnails.append({
-                    'id': width,
-                    'url': v,
-                    'width': int_or_none(width),
-                })
+        import numpy as np
+import tensorflow as tf
+    
+        This generator is more flexible than the multiple_grid_anchor_generator
+    and multiscale_grid_anchor_generator, and can generate any of the anchors
+    that they can generate, plus additional anchor configurations. In
+    particular, it allows the explicit specification of scale and aspect ratios
+    at each layer without making any assumptions between the relationship
+    between scales and aspect ratios between layers.
+    
+    Inputs:
+'normalized_input_image_tensor': a float32 tensor of shape
+[1, height, width, 3] containing the normalized input image. Note that the
+height and width must be compatible with the height and width configured in
+the fixed_shape_image resizer options in the pipeline config proto.
+    
+      if eval_config.use_moving_averages:
+    variable_averages = tf.train.ExponentialMovingAverage(0.0)
+    variables_to_restore = variable_averages.variables_to_restore()
+  saver = tf.train.Saver(variables_to_restore)
+    
+      def _match(self, similarity_matrix, valid_rows):
+    '''Bipartite matches a collection rows and columns. A greedy bi-partite.
+    
+      for _, groundtruth in enumerate(all_annotations.groupby('ImageID')):
+    image_id, image_groundtruth = groundtruth
+    groundtruth_dictionary = utils.build_groundtruth_vrd_dictionary(
+        image_groundtruth, class_label_map, relationship_label_map)
+    
+    
+# TODO(rathodv): Merge the implementation with ConvolutionalBoxPredictor above
+# since they are very similar.
+class WeightSharedConvolutionalBoxPredictor(box_predictor.BoxPredictor):
+  '''Convolutional Box Predictor with weight sharing.
+    
+        num_masks = 1 if self._masks_are_class_agnostic else self._num_classes
+    self._mask_predictor_layers.append(
+        tf.keras.layers.Conv2D(
+            num_masks,
+            [3, 3],
+            padding='SAME',
+            name='MaskPredictor_last_conv2d',
+            **self._conv_hyperparams.params(use_bias=True)))
+    
+            return {
+            'id': video_id,
+            'title': title,
+            'description': video.get('long_description') or video.get(
+                'short_description'),
+            'duration': float_or_none(video.get('duration'), scale=1000),
+            'formats': formats,
+            'subtitles': subtitles,
+        }
+    
+                for video in videos:
+                video_id = video.get('videoSeq')
+                if not video_id:
+                    continue
+                video_id = compat_str(video_id)
+                entries.append(
+                    self.url_result(
+                        'http://www.vlive.tv/video/%s' % video_id,
+                        ie=VLiveIE.ie_key(), video_id=video_id))
+    
+    
+class SouthParkDkIE(SouthParkIE):
+    IE_NAME = 'southparkstudios.dk'
+    _VALID_URL = r'https?://(?:www\.)?(?P<url>southparkstudios\.(?:dk|nu)/(?:clips|full-episodes|collections)/(?P<id>.+?)(\?|#|$))'
+    _FEED_URL = 'http://www.southparkstudios.dk/feeds/video-player/mrss/'
     
         _TESTS = [{
-        'url': 'http://www.southparkstudios.dk/full-episodes/s18e07-grounded-vindaloop',
+        'url': 'http://www.cbs.com/shows/garth-brooks/video/_u7W953k6la293J7EPTd9oHkSPs6Xn6_/connect-chat-feat-garth-brooks/',
         'info_dict': {
-            'title': 'Grounded Vindaloop',
-            'description': 'Butters is convinced he\'s living in a virtual reality.',
+            'id': '_u7W953k6la293J7EPTd9oHkSPs6Xn6_',
+            'ext': 'mp4',
+            'title': 'Connect Chat feat. Garth Brooks',
+            'description': 'Connect with country music singer Garth Brooks, as he chats with fans on Wednesday November 27, 2013. Be sure to tune in to Garth Brooks: Live from Las Vegas, Friday November 29, at 9/8c on CBS!',
+            'duration': 1495,
+            'timestamp': 1385585425,
+            'upload_date': '20131127',
+            'uploader': 'CBSI-NEW',
         },
-        'playlist_mincount': 3,
+        'params': {
+            # m3u8 download
+            'skip_download': True,
+        },
+        '_skip': 'Blocked outside the US',
     }, {
-        'url': 'http://www.southparkstudios.dk/collections/2476/superhero-showdown/1',
+        'url': 'http://colbertlateshow.com/video/8GmB0oY0McANFvp2aEffk9jZZZ2YyXxy/the-colbeard/',
         'only_matching': True,
     }, {
-        'url': 'http://www.southparkstudios.nu/collections/2476/superhero-showdown/1',
+        'url': 'http://www.colbertlateshow.com/podcasts/dYSwjqPs_X1tvbV_P2FcPWRa_qT6akTC/in-the-bad-room-with-stephen/',
         'only_matching': True,
     }]
-
+    
+    import json
+import sys
+import hashlib
+import os.path
+    
+    import io
+import optparse
+    
+    with io.open(README_FILE, encoding='utf-8') as f:
+    oldreadme = f.read()
+    
+        diropts = []
+    for opt in opts_dir:
+        if opt._short_opts:
+            diropts.extend(opt._short_opts)
+        if opt._long_opts:
+            diropts.extend(opt._long_opts)
+    
+        params = {
+        'age_limit': age,
+        'skip_download': True,
+        'writeinfojson': True,
+        'outtmpl': '%(id)s.%(ext)s',
+    }
+    ydl = YoutubeDL(params)
+    ydl.add_default_info_extractors()
+    json_filename = os.path.splitext(filename)[0] + '.info.json'
+    try_rm(json_filename)
+    ydl.download([url])
+    res = os.path.exists(json_filename)
+    try_rm(json_filename)
+    return res
+    
+        def reset(self):
+        ''' Reset all config options to default '''
+        logger.debug('Resetting config')
+        for section, items in self.config.defaults.items():
+            logger.debug('Resetting section: '%s'', section)
+            lookup = [section.split('.')[0], section] if '.' in section else [section, section]
+            for item, def_opt in items.items():
+                if item == 'helptext':
+                    continue
+                default = def_opt['default']
+                tk_var = self.config_dict_gui[lookup[0]][lookup[1]][item]['selected']
+                logger.debug('Resetting: '%s' to '%s'', item, default)
+                tk_var.set(default)
+    
+        Defaults files should be named <plugin_name>_defaults.py
+    Any items placed into this file will automatically get added to the relevant config .ini files
+    within the faceswap/config folder.
     
     
-    {    # Options that need a file parameter
-    'download-archive': ['--require-parameter'],
-    'cookies': ['--require-parameter'],
-    'load-info': ['--require-parameter'],
-    'batch-file': ['--require-parameter'],
-}
+_HELPTEXT = 'Options for outputting converted frames to an animated gif.'
     
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
     
-def openssl_encode(algo, key, iv):
-    cmd = ['openssl', 'enc', '-e', '-' + algo, '-K', hex_str(key), '-iv', hex_str(iv)]
-    prog = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, _ = prog.communicate(secret_msg)
-    return out
-    
-    new_version = {}
-    
-    
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
-    
-    lazy_extractors_filename = sys.argv[1]
-if os.path.exists(lazy_extractors_filename):
-    os.remove(lazy_extractors_filename)
-    
-    AUTOHSTS_PERMANENT = 31536000
-'''Value for the last max-age of HSTS'''
-    
-        @certbot_util.patch_get_utility()
-    def test_select_cancel(self, mock_util):
-        mock_util().checklist.return_value = (display_util.CANCEL, 'whatever')
-        vhs = select_vhost_multiple([self.vhosts[2], self.vhosts[3]])
-        self.assertFalse(vhs)
-    
-            self.assertFalse(self.vhost2.conflicts([self.addr1,
-                                                self.addr_default]))
+                    var_x = K.reshape(inputs, (batch_size,
+                                           height,
+                                           width,
+                                           self.group,
+                                           channels // self.group))
+                mean = K.mean(var_x, axis=[1, 2, 4], keepdims=True)
+                std = K.sqrt(K.var(var_x, axis=[1, 2, 4], keepdims=True) + self.epsilon)
+                var_x = (var_x - mean) / std
