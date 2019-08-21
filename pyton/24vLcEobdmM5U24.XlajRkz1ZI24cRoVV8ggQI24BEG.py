cb8908a87b45486119bf03e16e6b24b7bb6ab723
SUNsung <weatherius@gmail.com>
2019-08-21 06:22:26 +0300

    Rewrite [24vLcEobdmM5U24.XlajRkz1ZI24cRoVV8ggQI24BEG.py] File in: 2019-08-21 06:22:26

--- pyton/24vLcEobdmM5U24.XlajRkz1ZI24cRoVV8ggQI24BEG.py
+++ pyton/24vLcEobdmM5U24.XlajRkz1ZI24cRoVV8ggQI24BEG.py
@@ -1,267 +1,119 @@
 
         
-            with gzip.open(paths[1], 'rb') as imgpath:
-        x_train = np.frombuffer(imgpath.read(), np.uint8,
-                                offset=16).reshape(len(y_train), 28, 28)
+        print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
     
-        model = create_multi_input_model_from(*models)
-    model.compile(loss='categorical_crossentropy', optimizer='sgd')
-    assert len(model.losses) == 8
+        # Arguments
+        x: Numpy array.
     
-    import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop
+        def on_epoch_begin(self, epoch, logs=None):
+        self.seen = 0
+        self.totals = {}
     
+            self.kernel_initializer = initializers.get(kernel_initializer)
+        self.recurrent_initializer = initializers.get(recurrent_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
     
-def global_function():
-    def inner_function():
-        class LocalClass:
-            pass
-        global inner_global_function
-        def inner_global_function():
-            def inner_function2():
-                pass
-            return inner_function2
-        return LocalClass
-    return lambda: inner_function
+            rows = conv_utils.conv_output_length(rows, self.kernel_size[0],
+                                             self.padding, self.strides[0])
+        cols = conv_utils.conv_output_length(cols, self.kernel_size[1],
+                                             self.padding, self.strides[1])
     
-            The default implementation looks the file's extension
-        up in the table self.extensions_map, using application/octet-stream
-        as a default; however it would be permissible (if
-        slow) to look inside the data to make a better guess.
+        # Output shape
+        Same shape as input.
     
-            os.chdir(self.parent_dir)
+    # Apply a dense layer to the every temporal slice of an input. For each of step
+# of the output sequence, decide which character should be chosen.
+model.add(layers.TimeDistributed(layers.Dense(len(chars), activation='softmax')))
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+model.summary()
     
-        counter = 1
-    for part in msg.walk():
-        # multipart/* are just containers
-        if part.get_content_maintype() == 'multipart':
-            continue
-        # Applications should really sanitize the given filename so that an
-        # email message can't be used to overwrite important files
-        filename = part.get_filename()
-        if not filename:
-            ext = mimetypes.guess_extension(part.get_content_type())
-            if not ext:
-                # Use a generic bag-of-bits extension
-                ext = '.bin'
-            filename = 'part-%03d%s' % (counter, ext)
-        counter += 1
-        with open(os.path.join(args.directory, filename), 'wb') as fp:
-            fp.write(part.get_payload(decode=True))
+        def test_session_by_path(self, httpbin):
+        self.start_session(httpbin)
+        session_path = os.path.join(self.config_dir, 'session-by-path.json')
+        r1 = http('--session=' + session_path, 'GET', httpbin.url + '/get',
+                  'Foo:Bar', env=self.env())
+        assert HTTP_OK in r1
     
-    # Proxy type for generator objects
-class GeneratorProxy(BaseProxy):
-    _exposed_ = ['__next__']
-    def __iter__(self):
-        return self
-    def __next__(self):
-        return self._callmethod('__next__')
+        def items(self):
+        for key, values in super(RequestItemsDict, self).items():
+            if not isinstance(values, list):
+                values = [values]
+            for value in values:
+                yield key, value
     
-    con = sqlite3.connect(DB_FILE)
-cur = con.cursor()
-cur.execute('''
-        create table people
-        (
-          name_last      varchar(20),
-          age            integer
-        )
-        ''')
-    
-    def shorten_title(title):
-    m1 = re.search('[[0-9]*]', title)
-    m2 = re.search(''.*'', title)
-    if m1:
-        title = m1.group(0)
-    if m2:
-        title = ' '.join((title, m2.group(0)))   
-    return title[:50] + ' [...]'    
+        See also `test_auth_plugins.py`
     
     
-class JSONSerializer(Serializer):
-    ''' JSON Serializer '''
-    ext = 'json'
-    woptions = 'w'
-    roptions = 'r'
+def test_credentials_in_url(httpbin_both):
+    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
+                   auth='user:password')
+    r = http('GET', url)
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
     
-        def __init__(self, image, alignments, original_roi=None):
-        logger.debug('Initializing %s: (alignments: %s, original_roi: %s)',
-                     self.__class__.__name__, alignments, original_roi)
-        self.image = image
-        self.alignments = alignments
-        self.roi = original_roi
-        self.colors = {1: (255, 0, 0),
-                       2: (0, 255, 0),
-                       3: (0, 0, 255),
-                       4: (255, 255, 0),
-                       5: (255, 0, 255),
-                       6: (0, 255, 255)}
-        logger.debug('Initialized %s', self.__class__.__name__)
+        class Plugin(AuthPlugin):
+        auth_type = 'test-require-false'
+        auth_require = False
     
-        def on_enter(self, event=None):
-        ''' Schedule on an enter event '''
-        self.schedule()
+        def test_GET_explicit_JSON_explicit_headers(self, httpbin):
+        r = http('--json', 'GET', httpbin.url + '/headers',
+                 'Accept:application/xml',
+                 'Content-Type:application/xml')
+        assert HTTP_OK in r
+        assert ''Accept': 'application/xml'' in r
+        assert ''Content-Type': 'application/xml'' in r
     
-        def decoder(self):
-        ''' Decoder Network '''
-        input_ = Input(shape=(8, 8, 512))
-        var_x = input_
+            res = [spam(a := eggs(a := h)) for h in range(2)]
     
-        def initialize_globals(self, pathscript):
-        ''' Initialize config and images global constants '''
-        cliopts = CliOptions()
-        scaling_factor = self.get_scaling()
-        pathcache = os.path.join(pathscript, 'lib', 'gui', '.cache')
-        statusbar = StatusBar(self)
-        session = Session()
-        initialize_config(cliopts, scaling_factor, pathcache, statusbar, session)
-        initialize_images()
+            class Circular(object):
+            def __init__(self):
+                self.circular = self
     
-            self.status_message.set('Ready')
-    
-        def get_one_option_variable(self, command, title):
-        ''' Return a single tk_var for the specified
-            command and control_title '''
-        for option in self.gen_command_options(command):
-            if option['control_title'] == title:
-                return option['value']
-        return None
-    
-        def build(self):
-        ''' Update the plot area with loss values '''
-        logger.debug('Building training graph')
-        self.plotcanvas.draw()
-        logger.debug('Built training graph')
-    
-    
-  def _SendRequest( self ):
-    self._response_future = self.PostDataToHandlerAsync(
-      self._request_data,
-      'receive_messages',
-      timeout = TIMEOUT_SECONDS )
-    return
+                if timeout is not None:
+                endtime = _time() + timeout
+                # Enter a busy loop if we have a timeout.  This busy loop was
+                # cribbed from Lib/threading.py in Thread.wait() at r71065.
+                delay = 0.0005 # 500 us -> initial delay of 1 ms
+                while True:
+                    if self._waitpid_lock.acquire(False):
+                        try:
+                            if self.returncode is not None:
+                                break  # Another thread waited.
+                            (pid, sts) = self._try_wait(os.WNOHANG)
+                            assert pid == self.pid or pid == 0
+                            if pid == self.pid:
+                                self._handle_exitstatus(sts)
+                                break
+                        finally:
+                            self._waitpid_lock.release()
+                    remaining = self._remaining_time(endtime)
+                    if remaining <= 0:
+                        raise TimeoutExpired(self.args, timeout)
+                    delay = min(delay * 2, remaining, .05)
+                    time.sleep(delay)
+            else:
+                while self.returncode is None:
+                    with self._waitpid_lock:
+                        if self.returncode is not None:
+                            break  # Another thread waited.
+                        (pid, sts) = self._try_wait(0)
+                        # Check the pid and loop as waitpid has been known to
+                        # return 0 even without WNOHANG in odd situations.
+                        # http://bugs.python.org/issue14396.
+                        if pid == self.pid:
+                            self._handle_exitstatus(sts)
+            return self.returncode
     
     
-def SendShutdownRequest():
-  request = ShutdownRequest()
-  # This is a blocking call.
-  request.Start()
-
+class Migration(SchemaMigration):
     
-    
-def _assert_rejects( filter, text ):
-  _assert_accept_equals( filter, text, False )
-    
-    
-def KeywordsFromSyntaxListOutput_Function_test():
-  assert_that( syntax_parse._KeywordsFromSyntaxListOutput( '''
-foogroup xxx foo bar
-             zoo goo
-             links to Function''' ),
-               contains_inanyorder( 'foo', 'bar', 'zoo', 'goo' ) )
-    
-        # Create and fill-in the class template
-    numfields = len(field_names)
-    argtxt = repr(field_names).replace(''', '')[1:-1]   # tuple repr without parens or quotes
-    reprtxt = ', '.join('%s=%%r' % name for name in field_names)
-    dicttxt = ', '.join('%r: t[%d]' % (name, pos) for pos, name in enumerate(field_names))
-    template = '''class %(typename)s(tuple):
-        '%(typename)s(%(argtxt)s)' \n
-        __slots__ = () \n
-        _fields = %(field_names)r \n
-        def __new__(_cls, %(argtxt)s):
-            return _tuple.__new__(_cls, (%(argtxt)s)) \n
-        @classmethod
-        def _make(cls, iterable, new=tuple.__new__, len=len):
-            'Make a new %(typename)s object from a sequence or iterable'
-            result = new(cls, iterable)
-            if len(result) != %(numfields)d:
-                raise TypeError('Expected %(numfields)d arguments, got %%d' %% len(result))
-            return result \n
-        def __repr__(self):
-            return '%(typename)s(%(reprtxt)s)' %% self \n
-        def _asdict(t):
-            'Return a new dict which maps field names to their values'
-            return {%(dicttxt)s} \n
-        def _replace(_self, **kwds):
-            'Return a new %(typename)s object replacing specified fields with new values'
-            result = _self._make(map(kwds.pop, %(field_names)r, _self))
-            if kwds:
-                raise ValueError('Got unexpected field names: %%r' %% kwds.keys())
-            return result \n
-        def __getnewargs__(self):
-            return tuple(self) \n\n''' % locals()
-    for i, name in enumerate(field_names):
-        template += '        %s = _property(_itemgetter(%d))\n' % (name, i)
-    
-    # If false, no module index is generated.
-#latex_use_modindex = True
-
-    
-        def textrank(self, sentence, topK=20, withWeight=False, allowPOS=('ns', 'n', 'vn', 'v'), withFlag=False):
-        '''
-        Extract keywords from sentence using TextRank algorithm.
-        Parameter:
-            - topK: return how many top keywords. `None` for all possible words.
-            - withWeight: if True, return a list of (word, weight);
-                          if False, return a list of words.
-            - allowPOS: the allowed POS list eg. ['ns', 'n', 'vn', 'v'].
-                        if the POS of w is not in this list, it will be filtered.
-            - withFlag: if True, return a list of pair(word, weight) like posseg.cut
-                        if False, return a list of words
-        '''
-        self.pos_filt = frozenset(allowPOS)
-        g = UndirectWeightedGraph()
-        cm = defaultdict(int)
-        words = tuple(self.tokenizer.cut(sentence))
-        for i, wp in enumerate(words):
-            if self.pairfilter(wp):
-                for j in xrange(i + 1, i + self.span):
-                    if j >= len(words):
-                        break
-                    if not self.pairfilter(words[j]):
-                        continue
-                    if allowPOS and withFlag:
-                        cm[(wp, words[j])] += 1
-                    else:
-                        cm[(wp.word, words[j].word)] += 1
-    
-        def __repr__(self):
-        return 'pair(%r, %r)' % (self.word, self.flag)
-    
-    parser = OptionParser(USAGE)
-parser.add_option('-k', dest='topK')
-opt, args = parser.parse_args()
-    
-        def testDefaultCut(self):
-        for content in test_contents:
-            result = jieba.cut(content)
-            assert isinstance(result, types.GeneratorType), 'Test DefaultCut Generator error'
-            result = list(result)
-            assert isinstance(result, list), 'Test DefaultCut error on content: %s' % content
-            print(' , '.join(result), file=sys.stderr)
-        print('testDefaultCut', file=sys.stderr)
-    
-    log_f = open('1.log','w')
-log_f.write(' / '.join(map(str, words)))
-    
-        for (disposition, _) in _iter_multipart_parts(data_bytes, boundary):
-        if disposition.get('name') == 'file' and 'filename' in disposition:
-            filename = disposition['filename']
-            break
-    
-        cmd = '{ test `which aws` || . .venv/bin/activate; }; aws'
-    endpoint_url = None
-    env = aws_stack.get_environment(env)
-    if env.region == REGION_LOCAL:
-        endpoint_url = aws_stack.get_local_service_url(service)
-    if endpoint_url:
-        if endpoint_url.startswith('https://'):
-            cmd += ' --no-verify-ssl'
-        cmd = '%s --endpoint-url='%s'' % (cmd, endpoint_url)
-        if not is_port_open(endpoint_url):
-            raise socket.error()
-    cmd = '%s %s' % (cmd, service)
-    return cmd
+        def get_dist_paths(self):
+        return [
+            # Also see sentry.utils.integrationdocs.DOC_FOLDER
+            os.path.join(self.get_root_path(), 'src', 'sentry', 'integration-docs'),
+        ]
