6a8cc4fee7ee5bc0fc8a70a41587a0e17fcb8c0e
SUNsung <weatherius@gmail.com>
2019-08-21 05:13:41 +0300

    Rewrite [18VrFQNa2wojs18.YzJXWMmnh218IYEDPqdbAIo18dx3.py] File in: 2019-08-21 05:13:41

--- pyton/18VrFQNa2wojs18.YzJXWMmnh218IYEDPqdbAIo18dx3.py
+++ pyton/18VrFQNa2wojs18.YzJXWMmnh218IYEDPqdbAIo18dx3.py
@@ -1,114 +1,154 @@
 
         
-          Args:
-    length: Sequence length.
-    hidden_size: Size of the
-    min_timescale: Minimum scale that will be applied at each position
-    max_timescale: Maximum scale that will be applied at each position
+            def test_expand_localhost_shorthand_with_port_and_path(self):
+        args = parser.parse_args(args=[':3000/path'], env=MockEnvironment())
+        assert args.url == 'http://localhost:3000/path'
     
-      def __init__(self, output_dir=None, root_data_dir=None, **kwargs):
-    def_flags = {}
-    def_flags['param_set'] = 'big'
-    def_flags['use_synthetic_data'] = True
-    def_flags['train_steps'] = 200
-    def_flags['steps_between_evals'] = 200
-    def_flags['hooks'] = ['ExamplesPerSecondHook']
+            if self.args.output_options_history is None:
+            self.args.output_options_history = self.args.output_options
     
-      def _ComputeVlad(self,
-                   features,
-                   codebook,
-                   use_l2_normalization=True,
-                   num_assignments=1):
-    '''Compute VLAD representation.
     
-      Args:
-    image_names: List of image names. These are used to compose output file
-      names for boxes and features.
-    image_paths: List of image paths. image_paths[i] is the path for the image
-      named by image_names[i]. `image_names` and `image_paths` must have the
-      same number of elements.
-    delf_config_path: Path to DelfConfig proto text file.
-    detector_model_dir: Directory where detector SavedModel is located.
-    detector_thresh: Threshold used to decide if an image's detected box
-      undergoes feature extraction.
-    output_features_dir: Directory where DELF features will be written to.
-    output_boxes_dir: Directory where detected boxes will be written to.
-    output_mapping: CSV file which maps each .delf file name to the image ID and
-      detected box ID.
+def get_package_meta(package_name):
+    api_url = 'https://pypi.python.org/pypi/{}/json'.format(package_name)
+    resp = requests.get(api_url).json()
+    hasher = hashlib.sha256()
+    for release in resp['urls']:
+        download_url = release['url']
+        if download_url.endswith('.tar.gz'):
+            hasher.update(requests.get(download_url).content)
+            return {
+                'name': package_name,
+                'url': download_url,
+                'sha256': hasher.hexdigest(),
+            }
+    else:
+        raise RuntimeError(
+            '{}: download not found: {}'.format(package_name, resp))
     
-      aggregation_extraction.ExtractAggregatedRepresentationsToFiles(
-      image_names=image_list,
-      features_dir=cmd_args.features_dir,
-      aggregation_config_path=cmd_args.aggregation_config_path,
-      mapping_path=cmd_args.index_mapping_path,
-      output_aggregation_dir=cmd_args.output_aggregation_dir)
     
-    from delf import feature_extractor
+class Conversion(object):
     
-    args = parser.parse_args()
+        # noinspection PyUnboundLocalVariable
+    return '%.*f %s' % (precision, n / factor, suffix)
+
     
-        def __cut_detail(self, sentence):
-        blocks = re_han_detail.split(sentence)
-        for blk in blocks:
-            if re_han_detail.match(blk):
-                for word in self.__cut(blk):
-                    yield word
+    
+def test_default_options(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http(httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['form'] == {'foo': 'bar'}
+    
+    
+def issue_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    ref = 'https://github.com/scrapy/scrapy/issues/' + text
+    set_classes(options)
+    node = nodes.reference(rawtext, 'issue ' + text, refuri=ref, **options)
+    return [node], []
+    
+    '''
+    
+        def process_options(self, args, opts):
+        ScrapyCommand.process_options(self, args, opts)
+        try:
+            opts.spargs = arglist_to_dict(opts.spargs)
+        except ValueError:
+            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
+        if opts.output:
+            if opts.output == '-':
+                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
             else:
-                tmp = re_skip_detail.split(blk)
-                for x in tmp:
-                    if x:
-                        if re_num.match(x):
-                            yield pair(x, 'm')
-                        elif re_eng.match(x):
-                            yield pair(x, 'eng')
-                        else:
-                            yield pair(x, 'x')
+                self.settings.set('FEED_URI', opts.output, priority='cmdline')
+            feed_exporters = without_none_values(self.settings.getwithbase('FEED_EXPORTERS'))
+            valid_output_formats = feed_exporters.keys()
+            if not opts.output_format:
+                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
+            if opts.output_format not in valid_output_formats:
+                raise UsageError('Unrecognized output format '%s', set one'
+                                 ' using the '-t' switch or as a file extension'
+                                 ' from the supported list %s' % (opts.output_format,
+                                                                  tuple(valid_output_formats)))
+            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
+    
+        def syntax(self):
+        return '[options]'
     
     
-    {        self.assertTrue(is_valid('[]'))
-        self.assertTrue(is_valid('[]()[]'))
-        self.assertFalse(is_valid('[[[]]'))
-        self.assertTrue(is_valid('{([])}'))
-        self.assertFalse(is_valid('(}'))
-    
-    
-class TestHuffmanCoding(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.file_in_name = 'huffman_coding_in.txt'
-        cls.file_out_bin_name = 'huffman_coding_out.bin'
-        cls.file_out_name = 'huffman_coding_out.txt'
-    
-            assert renderer, '.accepted_renderer not set on Response'
-        assert accepted_media_type, '.accepted_media_type not set on Response'
-        assert context is not None, '.renderer_context not set on Response'
-        context['response'] = self
-    
-        def handle(self, *args, **options):
-        username = options['username']
-        reset_token = options['reset_token']
-    
-                # if a form media type, copy data & files refs to the underlying
-            # http request so that closable objects are handled appropriately.
-            if is_form_media_type(self.content_type):
-                self._request._post = self.POST
-                self._request._files = self.FILES
-    
-            available_requests = self.num_requests - len(self.history) + 1
-        if available_requests <= 0:
-            return None
-    
-    
-def get_breadcrumbs(url, request=None):
+# XXX: move it to w3lib?
+_ajax_crawlable_re = re.compile(six.u(r'<meta\s+name=['\']fragment['\']\s+content=['\']!['\']/?>'))
+def _has_ajaxcrawlable_meta(text):
     '''
-    Given a url returns a list of breadcrumbs, which are each a
-    tuple of (name, url).
+    >>> _has_ajaxcrawlable_meta('<html><head><meta name='fragment'  content='!'/></head><body></body></html>')
+    True
+    >>> _has_ajaxcrawlable_meta('<html><head><meta name='fragment' content='!'></head></html>')
+    True
+    >>> _has_ajaxcrawlable_meta('<html><head><!--<meta name='fragment'  content='!'/>--></head><body></body></html>')
+    False
+    >>> _has_ajaxcrawlable_meta('<html></html>')
+    False
     '''
-    from rest_framework.reverse import preserve_builtin_query_params
-    from rest_framework.views import APIView
     
-        def test_generic_fk(self):
+            if not any(os.path.isfile('%s/certutil' % x) for x in os.environ['PATH'].split(os.pathsep)):
+            xlog.warn('please install *libnss3-tools* package to import GoAgent root ca')
+            return False
+    
+            if self.continue_fail_count > 10:
+            # don't set network_stat to 'unknown', wait for check
+            # network_stat = 'unknown'
+            xlog.debug('report_connect_fail %s continue_fail_count:%d',
+                       self.type, self.continue_fail_count)
+            self.triger_check_network(True)
+    
+            if self.token is not None:
+            return 'MissingTokenException(at %r)' % self.token.text
+    
+    
+# 全连接层实现类
+class FullConnectedLayer(object):
+    def __init__(self, input_size, output_size, 
+                 activator):
         '''
-        Test a relationship that spans a GenericForeignKey field.
-        IE. A forward generic relationship.
+        构造函数
+        input_size: 本层输入向量的维度
+        output_size: 本层输出向量的维度
+        activator: 激活函数
         '''
+        self.input_size = input_size
+        self.output_size = output_size
+        self.activator = activator
+        # 权重数组W
+        self.W = np.random.uniform(-0.1, 0.1,
+            (output_size, input_size))
+        # 偏置项b
+        self.b = np.zeros((output_size, 1))
+        # 输出向量
+        self.output = np.zeros((output_size, 1))
+    
+    # 定义激活函数 f
+f = lambda x: x
+    
+        # 抽取条件模式基
+    # 查询树节点的，频繁子项
+    print('x --->', findPrefixPath('x', myHeaderTab['x'][1]))
+    print('z --->', findPrefixPath('z', myHeaderTab['z'][1]))
+    print('r --->', findPrefixPath('r', myHeaderTab['r'][1]))
+    
+    '''
+Created on Jun 1, 2011
+Update  on 2017-05-18
+Author: Peter Harrington/片刻
+GitHub：https://github.com/apachecn/AiLearning
+'''
+from __future__ import print_function
+from numpy import *
+import matplotlib.pyplot as plt
+print(__doc__)
+    
+    # 累计样本总和，总和 和 平分和的总和
+cumN, cumVal, cumSumSq = 0.0, 0.0, 0.0
+for instance in mapperOut:
+    nj = float(instance[0])
+    cumN += nj
+    cumVal += nj*float(instance[1])
+    cumSumSq += nj*float(instance[2])
