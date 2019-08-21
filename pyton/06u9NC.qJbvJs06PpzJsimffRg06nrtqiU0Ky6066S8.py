63b703a3b8f76ca276948785cefd6189708dd836
SUNsung <weatherius@gmail.com>
2019-08-21 21:15:46 +0300

    Rewrite [06u9NC.qJbvJs06PpzJsimffRg06nrtqiU0Ky6066S8.py] File in: 2019-08-21 21:15:46

--- pyton/06u9NC.qJbvJs06PpzJsimffRg06nrtqiU0Ky6066S8.py
+++ pyton/06u9NC.qJbvJs06PpzJsimffRg06nrtqiU0Ky6066S8.py
@@ -1,202 +1,140 @@
 
         
-        index_title = 0
-index_desc = 1
-index_auth = 2
-index_https = 3
-index_cors = 4
-index_link = 5
-num_segments = 5
+            This could be extended by having nested blocks, sorting them recursively
+    and flattening the end structure into a list of lines. Revision 2 maybe ^.^.
+'''
     
-        def test_timeout(self):
-        self.assertTrue(self.timeout.timeout)
-        self.assertFalse(self.invalid.timeout)
-    
-        def test_save_failure(self):
-        self.client.installer = mock.MagicMock()
-        self.client.installer.save.side_effect = errors.PluginError
-        self._test_error()
-        self.client.installer.recovery_routine.assert_called_once_with()
-        self.client.installer.save.assert_called_once_with(mock.ANY)
-    
-       dnssec-keygen -a HMAC-SHA512 -b 512 -n HOST keyname.
-    
-        def _cleanup(self, _domain, validation_name, validation):
-        self._get_rfc2136_client().del_txt_record(validation_name, validation)
-    
-            # Same story for the scores:
-        #   - scores are (A, H, W) format from conv output
-        #   - transpose to (H, W, A)
-        #   - reshape to (H * W * A, 1) where rows are ordered by (H, W, A)
-        #     to match the order of anchors and bbox_deltas
-        scores = scores.transpose((1, 2, 0)).reshape((-1, 1))
-    
-        def test_gpu_handles_even_length_arrays(self):
-        X = np.random.rand(64).astype(np.float32)
-        Y_exp = np.copy(X)
-        Y_exp[0::2] = 0.0
-        Y_act = self._run_zero_even_op_gpu(X)
-        np.testing.assert_allclose(Y_act, Y_exp)
-    
-    
-def set_up_matplotlib():
-    '''Set matplotlib up.'''
-    import matplotlib
-    # Use a non-interactive backend
-    matplotlib.use('Agg')
-    
-    import cv2
-import logging
-import os
-import uuid
-    
-    parser = ArgumentParser(usage='%s -m jieba [options] filename' % sys.executable, description='Jieba command line interface.', epilog='If no filename specified, use STDIN instead.')
-parser.add_argument('-d', '--delimiter', metavar='DELIM', default=' / ',
-                    nargs='?', const=' ',
-                    help='use DELIM instead of ' / ' for word delimiter; or a space if it is used without DELIM')
-parser.add_argument('-p', '--pos', metavar='DELIM', nargs='?', const='_',
-                    help='enable POS tagging; if DELIM is specified, use DELIM instead of '_' for POS delimiter')
-parser.add_argument('-D', '--dict', help='use DICT as dictionary')
-parser.add_argument('-u', '--user-dict',
-                    help='use USER_DICT together with the default dictionary or DICT (if specified)')
-parser.add_argument('-a', '--cut-all',
-                    action='store_true', dest='cutall', default=False,
-                    help='full pattern cutting (ignored with POS tagging)')
-parser.add_argument('-n', '--no-hmm', dest='hmm', action='store_false',
-                    default=True, help='don't use the Hidden Markov Model')
-parser.add_argument('-q', '--quiet', action='store_true', default=False,
-                    help='don't print loading messages to stderr')
-parser.add_argument('-V', '--version', action='version',
-                    version='Jieba ' + jieba.__version__)
-parser.add_argument('filename', nargs='?', help='input file')
-    
-        def lcut(self, *args, **kwargs):
-        return list(self.cut(*args, **kwargs))
-    
-    print('-'*40)
-print(' 搜索模式')
-print('-'*40)
-    
-    if opt.topK is None:
-    topK = 10
-else:
-    topK = int(opt.topK)
-    
-    tags = jieba.analyse.extract_tags(content, topK=topK, withWeight=withWeight)
-    
-    
-  def JustDocString_test( self ):
-    self._Check( 'not_an_int', {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'extra_data': {
-        'doc_string':    'DOC STRING',
-      },
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'info'     : 'DOC STRING',
-      'equal'    : 1,
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': 'not_an_int',
-    } )
-    
-    
-@YouCompleteMeInstance( { 'g:ycm_extra_conf_vim_data': [ 'undefined_value' ] } )
-def SendCommandRequest_ExtraConfData_UndefinedValue_test( ycm ):
-  current_buffer = VimBuffer( 'buffer' )
-  with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
-    with patch( 'ycm.youcompleteme.SendCommandRequest' ) as send_request:
-      ycm.SendCommandRequest( [ 'GoTo' ], 'belowright', False, 1, 1 )
-      assert_that(
-        # Positional arguments passed to SendCommandRequest.
-        send_request.call_args[ 0 ],
-        contains(
-          contains( 'GoTo' ),
-          'belowright',
-          'same-buffer',
-          has_entries( {
-            'options': has_entries( {
-              'tab_size': 2,
-              'insert_spaces': True,
-            } )
-          } )
+            self.assertFalse(sns_listener.get_topic_by_arn(topic_arn))
+        sns_listener.do_create_topic(topic_arn)
+        self.assertTrue(sns_listener.get_topic_by_arn(topic_arn) is not None)
+        sns_listener.do_subscribe(
+            topic_arn,
+            'http://localhost:1234/listen',
+            'http',
+            sub_arn,
+            {}
         )
-      )
+        self.assertTrue(sns_listener.get_subscription_by_arn(sub_arn))
+        sns_listener.do_unsubscribe(sub_arn)
+        self.assertFalse(sns_listener.get_subscription_by_arn(sub_arn))
     
-    from ycm.client.base_request import BaseRequest
+            part_head, _ = part.split(b'\r\n\r\n', 1)
+        head_parsed = parse_data(part_head.lstrip(b'\r\n'))
     
-            Returns:
-            False if the Future was cancelled, True otherwise.
+        # make sure domain deletion works
+    es_client.delete_elasticsearch_domain(DomainName=TEST_DOMAIN_NAME)
+    assert_false(TEST_DOMAIN_NAME in
+        [d['DomainName'] for d in es_client.list_domain_names()['DomainNames']])
+    
+            # Create a large amount of items
+        num_items = 20
+        for i in range(0, num_items):
+            item = {PARTITION_KEY: 'id%s' % i, 'data1': 'foobar123 ' * 1000}
+            table.put_item(Item=item)
     
     
-def test_references_remain_unresolved(validator):
-    rules_set_registry.extend(
-        (('boolean', {'type': 'boolean'}), ('booleans', {'valuesrules': 'boolean'}))
-    )
-    validator.schema = {'foo': 'booleans'}
-    assert 'booleans' == validator.schema['foo']
-    assert 'boolean' == rules_set_registry._storage['booleans']['valuesrules']
+class SigmoidActivator(object):
+    def forward(self, weighted_input):
+        return 1.0 / (1.0 + np.exp(-weighted_input))
     
     
-def test_empty_schema():
-    validator = Validator()
-    with pytest.raises(SchemaError, match=errors.SCHEMA_ERROR_MISSING):
-        validator({}, schema=None)
-    
-        for i in range(min_length):
-        a, b = x[i], y[i]
-    
-        Parameters
-    ----------
-    complete_options : bool
-        always complete the options, even when the user hasn't typed a first dash (Default value = False)
-    match_incomplete : func
-        a function with two parameters choice and incomplete. Must return True
-        if incomplete is a correct match for choice, False otherwise.
+# 将图像画出来
+def plot(linear_unit):
     '''
-    global _initialized
-    if not _initialized:
-        _patch()
-        completion_configuration.complete_options = complete_options
-        if match_incomplete is not None:
-            completion_configuration.match_incomplete = match_incomplete
-        _initialized = True
-    
-        Parameters
-    ----------
-    shell : Shell
-        The shell type targeted. It will be guessed with get_auto_shell() if the value is None (Default value = None)
-    prog_name : str
-        The program name on the command line. It will be automatically computed if the value is None
-        (Default value = None)
-    env_name : str
-        The environment variable name used to control the completion. It will be automatically computed if the value is
-        None (Default value = None)
-    path : str
-        The installation path of the code to be evaluated by the shell. The standard installation path is used if the
-        value is None (Default value = None)
-    append : bool
-        Whether to append the content to the file or to override it. The default behavior depends on the shell type
-        (Default value = None)
-    extra_env : dict
-        A set of environment variables and their values to be added to the generated code (Default value = None)
+    Desc:
+        将我们训练好的线性单元对数据的分类情况作图画出来
+    Args:
+        linear_unit —— 训练好的线性单元
+    Returns:
+        None
     '''
-    prog_name = prog_name or click.get_current_context().find_root().info_name
-    shell = shell or get_auto_shell()
-    if append is None and path is not None:
-        append = True
-    if append is not None:
-        mode = 'a' if append else 'w'
-    else:
-        mode = None
+    # 引入绘图的库
+    import matplotlib.pyplot as plt
+    # 获取训练数据：特征 input_vecs 与 对应的标签 labels
+    input_vecs, labels = get_training_dataset()
+    # figure() 创建一个 Figure 对象，与用户交互的整个窗口，这个 figure 中容纳着 subplots
+    fig = plt.figure()
+    # 在 figure 对象中创建 1行1列中的第一个图
+    ax = fig.add_subplot(111)
+    # scatter(x, y) 绘制散点图，其中的 x,y 是相同长度的数组序列
+    ax.scatter(map(lambda x: x[0], input_vecs), labels)
+    # 设置权重
+    weights = linear_unit.weights
+    # 设置偏置项
+    bias = linear_unit.bias
+    # range(start, stop, step) 从 start 开始，到 stop 结束，步长为 step
+    x = range(0, 12, 1)
+    # 计算感知器对输入计算得到的值
+    y = map(lambda x: weights[0] * x + bias, x)
+    # 将图画出来
+    ax.plot(x, y)
+    # 将最终的图展示出来
+    plt.show()
     
-    from . import DistlibException
-from .compat import (HTTPBasicAuthHandler, Request, HTTPPasswordMgr,
-                     urlparse, build_opener, string_types)
-from .util import cached_property, zip_dir, ServerProxy
+        def init_state_vec(self):
+        '''
+        初始化保存状态的向量
+        '''
+        state_vec_list = []
+        state_vec_list.append(np.zeros(
+            (self.state_width, 1)))
+        return state_vec_list
+    
+        def calc_delta(self, parent_delta, parent):
+        '''
+        计算每个节点的delta
+        '''
+        parent.delta = parent_delta
+        if parent.children:
+            # 根据式2计算每个子节点的delta
+            children_delta = np.dot(self.W.T, parent_delta) * (
+                self.activator.backward(parent.children_data)
+            )
+            # slices = [(子节点编号，子节点delta起始位置，子节点delta结束位置)]
+            slices = [(i, i * self.node_width, 
+                        (i + 1) * self.node_width)
+                        for i in range(self.child_count)]
+            # 针对每个子节点，递归调用calc_delta函数
+            for s in slices:
+                self.calc_delta(children_delta[s[1]:s[2]], 
+                                parent.children[s[0]])
+    
+    # 转化关系， 数组转化为矩阵
+randMat = mat(randArray)
+'''
+.I 表示对矩阵求逆(可以利用矩阵的初等变换)
+   意义：逆矩阵是一个判断相似性的工具。逆矩阵A与列向量p相乘后，将得到列向量q，q的第i个分量表示p与A的第i个列向量的相似度。
+   参考案例链接：
+   https://www.zhihu.com/question/33258489
+   http://blog.csdn.net/vernice/article/details/48506027
+.T 表示对矩阵转置(行列颠倒)
+    * 等同于: .transpose()
+.A 返回矩阵基于的数组
+    参考案例链接：
+    http://blog.csdn.net/qq403977698/article/details/47254539
+'''
+invRandMat = randMat.I
+TraRandMat = randMat.T
+ArrRandMat = randMat.A
+# 输出结果
+print('randArray=(%s) \n' % type(randArray), randArray)
+print('randMat=(%s) \n' % type(randMat), randMat)
+print('invRandMat=(%s) \n' % type(invRandMat), invRandMat)
+print('TraRandMat=(%s) \n' % type(TraRandMat), TraRandMat)
+print('ArrRandMat=(%s) \n' % type(ArrRandMat), ArrRandMat)
+# 矩阵和逆矩阵 进行求积 (单位矩阵，对角线都为1嘛，理论上4*4的矩阵其他的都为0)
+myEye = randMat*invRandMat
+# 误差
+print(myEye - eye(4))
+    
+    
+class MRmean(MRJob):
+    def __init__(self, *args, **kwargs):  # 对数据初始化
+        super(MRmean, self).__init__(*args, **kwargs)
+        self.inCount = 0
+        self.inSum = 0
+        self.inSqSum = 0
+    
+    def reduce(key, values, mu=0.1):
+  sumETE = None
+  sumETDe = None
