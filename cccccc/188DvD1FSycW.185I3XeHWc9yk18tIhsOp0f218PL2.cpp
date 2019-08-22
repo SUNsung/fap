2b91c3722cf2b8d56e30246a6a2e10f0eb23ba2d
SUNsung <weatherius@gmail.com>
2019-08-22 23:41:52 +0300

    Rewrite [188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp] File in: 2019-08-22 23:41:46

--- cccccc/188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp
+++ cccccc/188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp
@@ -1,24 +1,422 @@
 
         
-            // change element at index 1 (second element) to 'second'
-    array.at(1) = 'second';
+        #include <unordered_map>
     
-        // output element with JSON pointer '/number'
-    std::cout << j.at('/number'_json_pointer) << '\n';
-    // output element with JSON pointer '/string'
-    std::cout << j.at('/string'_json_pointer) << '\n';
-    // output element with JSON pointer '/array'
-    std::cout << j.at('/array'_json_pointer) << '\n';
-    // output element with JSON pointer '/array/1'
-    std::cout << j.at('/array/1'_json_pointer) << '\n';
+    #include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
     
-        // out_of_range.403
-    try
-    {
-        // try to use a JSON pointer to an nonexistent object key
-        json::const_reference ref = j.at('/foo'_json_pointer);
+    int NPyBfloat16_SetItem(PyObject* item, void* data, void* arr) {
+  bfloat16 x;
+  if (!AsBfloat16(item, &x)) return -1;
+  memcpy(data, &x, sizeof(bfloat16));
+  return 0;
+}
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    #include 'tensorflow/stream_executor/cuda/cuda_platform_id.h'
+    
+      // Computes real-to-complex FFT in forward direction.
+  virtual bool DoFft(Stream *stream, Plan *plan,
+                     const DeviceMemory<float> &input,
+                     DeviceMemory<std::complex<float>> *output) = 0;
+  virtual bool DoFft(Stream *stream, Plan *plan,
+                     const DeviceMemory<double> &input,
+                     DeviceMemory<std::complex<double>> *output) = 0;
+    
+    namespace stream_executor {
+namespace host {
     }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
     }
+    
+    // This file contains declarations relating to kernel cache configuration
+// parameters recognized by the StreamExecutor.
+#ifndef TENSORFLOW_STREAM_EXECUTOR_KERNEL_CACHE_CONFIG_H_
+#define TENSORFLOW_STREAM_EXECUTOR_KERNEL_CACHE_CONFIG_H_
+    
+      std::atomic<int> processed_tasks_num_;
+    
+    
+    {  bool isChild() {
+    return pid == 0;
+  }
+};
+    
+    
+    {} // namespace
+    
+    namespace caffe2 {
+    }
+    
+      // Unimplemented is left unimplemented to test the returned error.
+    
+    class ClientChannelStressTest {
+ public:
+  void Run() {
+    Start();
+    // Keep updating resolution for the test duration.
+    gpr_log(GPR_INFO, 'Start updating resolution.');
+    const auto wait_duration =
+        std::chrono::milliseconds(kResolutionUpdateIntervalMs);
+    std::vector<AddressData> addresses;
+    auto start_time = std::chrono::steady_clock::now();
+    while (true) {
+      if (std::chrono::duration_cast<std::chrono::seconds>(
+              std::chrono::steady_clock::now() - start_time)
+              .count() > kTestDurationSec) {
+        break;
+      }
+      // Generate a random subset of balancers.
+      addresses.clear();
+      for (const auto& balancer_server : balancer_servers_) {
+        // Select each address with probability of 0.8.
+        if (std::rand() % 10 < 8) {
+          addresses.emplace_back(AddressData{balancer_server.port_, true, ''});
+        }
+      }
+      std::shuffle(addresses.begin(), addresses.end(),
+                   std::mt19937(std::random_device()()));
+      SetNextResolution(addresses);
+      std::this_thread::sleep_for(wait_duration);
+    }
+    gpr_log(GPR_INFO, 'Finish updating resolution.');
+    Shutdown();
+  }
+    }
+    
+    #endif /* GRPC_WINSOCK_SOCKET */
+
+    
+    #include <limits.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+    
+    static inline std::shared_ptr<::grpc::Channel>
+CreateCustomChannelWithInterceptors(
+    const grpc::string& target,
+    const std::shared_ptr<ChannelCredentials>& creds,
+    const ChannelArguments& args,
+    std::vector<
+        std::unique_ptr<experimental::ClientInterceptorFactoryInterface>>
+        interceptor_creators) {
+  return ::grpc_impl::experimental::CreateCustomChannelWithInterceptors(
+      target, creds, args, std::move(interceptor_creators));
+}
+    
+    TEST_F(HybridEnd2endTest,
+       AsyncRequestStreamResponseStream_SyncStreamedUnaryDupService) {
+  typedef EchoTestService::WithAsyncMethod_RequestStream<
+      EchoTestService::WithAsyncMethod_ResponseStream<TestServiceImpl>>
+      SType;
+  SType service;
+  StreamedUnaryDupPkg dup_service;
+  SetUpServer(&service, &dup_service, nullptr, nullptr, 8192);
+  ResetStub();
+  std::thread response_stream_handler_thread(HandleServerStreaming<SType>,
+                                             &service, cqs_[0].get());
+  std::thread request_stream_handler_thread(HandleClientStreaming<SType>,
+                                            &service, cqs_[1].get());
+  TestAllMethods();
+  SendEchoToDupService();
+  response_stream_handler_thread.join();
+  request_stream_handler_thread.join();
+}
+    
+    #include 'pch.h'
+#include 'AppResourceProvider.h'
+    
+    // Declaration of the LiveRegionHost class.
+// This class announces NarratorAnnouncements using the LiveRegionChanged event.
+// This event is unreliable and should be deprecated in favor of the new
+// RaiseNotification API in RS3.
+    
+    Platform::String ^ BindableBase::GetStringRepresentation()
+{
+    return this->ToString();
+}
+
+    
+    
+/*!\brief
+ * cpu function of deformable_im2col algorithm
+ * \param s device stream
+ * \param data_im pointer of an image (C, H, W, ...) in the image batch
+ * \param data_offset pointer of offset (C, H, W, ...) in the offset batch
+ * \param im_shape input image shape in dimensions (N, C, H, W,)
+ * \param col_shape column buffer shape (#channels, output_im_height, output_im_width, ...)
+ * \param kernel_shape kernel filter shape
+ * \param pad pad shape
+ * \param stride stride shape
+ * \param dilation dilation shape
+ * \param deformable_group #offset group that deformable convolution use
+ * \param data_col column buffer pointer
+ */
+template <typename DType>
+inline void deformable_im2col(mshadow::Stream<cpu>* s,
+                              const DType* data_im, const DType* data_offset,
+                              const mxnet::TShape& im_shape,
+                              const mxnet::TShape& col_shape,
+                              const mxnet::TShape& kernel_shape,
+                              const mxnet::TShape& pad,
+                              const mxnet::TShape& stride,
+                              const mxnet::TShape& dilation,
+                              const index_t deformable_group,
+                              DType* data_col) {
+  if (2 == kernel_shape.ndim()) {
+    deformable_im2col_cpu(data_im, data_offset,
+                          im_shape[1], im_shape[2], im_shape[3],
+                          kernel_shape[0], kernel_shape[1],
+                          pad[0], pad[1],
+                          stride[0], stride[1],
+                          dilation[0], dilation[1],
+                          deformable_group,
+                          col_shape[1], col_shape[2], data_col);
+  } else {
+    LOG(FATAL) << 'not implemented';
+  }
+}
+    
+      if (rnn_forward_prim->size() <= (size_t)layer_index) {
+    primitive rnn_prim = rnn_forward(prim_desc, (*x_memory)[layer_index],
+          (*hcx_memory)[layer_index], (*wx_memory)[layer_index],
+          (*wh_memory)[layer_index], (*bias_memory)[layer_index],
+          (*y_memory)[layer_index],
+         (*hcy_memory)[layer_index], null_memory_);
+    rnn_forward_prim->push_back(rnn_prim);
+  }
+  MKLDNNStream::Get()->RegisterPrim((*rnn_forward_prim)[layer_index]);
+  MKLDNNStream::Get()->Submit();
+    
+    inline void IndexArrayBuildSelectedAxesWorkspace(const mxnet::Tuple<int> &axes,
+                                                 const std::vector<int64_t> &index_products,
+                                                 int64_t* workspace,
+                                                 const int ndim) {
+  for (int i = 0; i < axes.ndim(); i++) {
+    // Make sure that the axis is between 0 and ndim.
+    const int axis = ((axes[i] % ndim) + ndim) % ndim;
+    }
+    }
+    
+    #include <mxnet/op_attr_types.h>
+#include <vector>
+    
+    #ifndef MXNET_RTC_H_
+#define MXNET_RTC_H_
+#include './base.h'
+#if MXNET_USE_CUDA && MXNET_ENABLE_CUDA_RTC
+#include <nvrtc.h>
+#include <cuda.h>
+    
+    // Initialization funciton called by caffeOp & caffeLoss
+template<typename Dtype>
+void InitCaffeBlobs(std::vector< ::caffe::Blob<Dtype>*>* v, int n_num) {
+  for (index_t i=0; i < n_num; ++i)
+    v->push_back(new ::caffe::Blob<Dtype>());
+}
+    
+      for (uint32_t nid = 0; nid < idx.num_nodes(); ++nid) {
+    const auto& inode = idx[nid];
+    if (inode.source->op() != ewise_plus_op) continue;
+    int sid = storage_id[idx.entry_id(inode.inputs[0])];
+    if (sid != storage_id[idx.entry_id(nid, 0)]) continue;
+    if (idx[inode.inputs[0].node_id].source->is_variable()) continue;
+    if (idx[inode.inputs[1].node_id].source->is_variable()) continue;
+    uint32_t eid_rhs  = idx.entry_id(inode.inputs[1]);
+    if (ref_count[eid_rhs] != 1) continue;
+    if (inode.inputs[0].node_id >= inode.inputs[1].node_id) continue;
+    // TODO(haibin) support inplace addto for Dynamic Storage
+    if (storage_id[eid_rhs] == kDynamicStorageID) continue;
+    CHECK_NE(storage_id[eid_rhs], sid);
+    storage_id[eid_rhs] = sid;
+    addto_entry[eid_rhs] = 1;
+    storage_inplace_index[eid_rhs] = -1;
+    skip_plus_node[nid] = 1;
+  }
+    
+    struct Monitor {
+ private:
+  struct Statistics {
+    Timer timer;
+    size_t count{0};
+    uint64_t nvtx_id;
+  };
+  std::string label = '';
+  std::map<std::string, Statistics> statistics_map;
+  Timer self_timer;
+    }
+    
+    void SparsePageWriter::PushWrite(std::shared_ptr<SparsePage>&& page) {
+  qworkers_[clock_ptr_].Push(std::move(page));
+  clock_ptr_ = (clock_ptr_ + 1) % workers_.size();
+}
+    
+    #if _MSC_VER >= 1800
+  return lgamma(v);
+#else
+#pragma message('Warning: lgamma function was not available until VS2013'\
+                ', poisson regression will be disabled')
+  utils::Error('lgamma function was not available until VS2013');
+  return static_cast<T>(1.0);
+#endif  // _MSC_VER >= 1800
+    
+    template <typename T>
+T* HostDeviceVector<T>::DevicePointer(int device) { return nullptr; }
+    
+      /*!
+   * \brief dense feature vector that can be taken by RegTree
+   * and can be construct from sparse feature vector.
+   */
+  struct FVec {
+    /*!
+     * \brief initialize the vector with size vector
+     * \param size The size of the feature vector.
+     */
+    void Init(size_t size);
+    /*!
+     * \brief fill the vector with sparse vector
+     * \param inst The sparse instance to fill.
+     */
+    void Fill(const SparsePage::Inst& inst);
+    /*!
+     * \brief drop the trace after fill, must be called after fill.
+     * \param inst The sparse instance to drop.
+     */
+    void Drop(const SparsePage::Inst& inst);
+    /*!
+     * \brief returns the size of the feature vector
+     * \return the size of the feature vector
+     */
+    size_t Size() const;
+    /*!
+     * \brief get ith value
+     * \param i feature index.
+     * \return the i-th feature value
+     */
+    bst_float Fvalue(size_t i) const;
+    /*!
+     * \brief check whether i-th entry is missing
+     * \param i feature index.
+     * \return whether i-th value is missing.
+     */
+    bool IsMissing(size_t i) const;
+    }
+    
+        // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    
+        // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    
+        // Will project scissor/clipping rectangles into framebuffer space
+    ImVec2 clip_off = draw_data->DisplayPos;         // (0,0) unless using multi-viewports
+    ImVec2 clip_scale = draw_data->FramebufferScale; // (1,1) unless using retina display which are often (2,2)
+    
+    #ifndef incl_HPHP_JSON_H_
+#define incl_HPHP_JSON_H_
+    
+    #endif
+
+    
+    
+    {
+    {      tmp = get_node(fault->children,'Detail');
+      if (tmp != nullptr) {
+        details = master_to_zval(encodePtr(), tmp);
+      }
+    }
+    obj->m_soap_fault =
+        SystemLib::AllocSoapFaultObject(String(faultcode, CopyString),
+                                        faultstring,
+                                        faultactor,
+                                        details);
+    xmlFreeDoc(response);
+    return false;
+  }
+    
+      proxygen::HTTPTransaction *getOrCreateTransaction(
+    proxygen::HTTPTransaction *clientTxn, HTTPMessage** msg, bool newPushOk) {
+    if (!m_pushTxn && newPushOk) {
+      if (!clientTxn) {
+        *msg = nullptr;
+        return nullptr;
+      }
+      m_pushTxn = clientTxn->newPushedTransaction(this);
+      *msg = &m_pushPromise;
+    } else {
+      if (m_egressError) {
+        *msg = nullptr;
+        return nullptr;
+      }
+      *msg = &m_response;
+    }
+    return m_pushTxn;
+  }
+    
+    TEST_F(ProxygenTransportBasicTest, valid_expect_overlarge_length) {
+  auto req = getRequest(HTTPMethod::POST);
+  auto length = folly::to<std::string>(RuntimeOption::MaxPostSize + 1);
+  req->getHeaders().add(HTTP_HEADER_CONTENT_LENGTH, length);
+  req->getHeaders().add(HTTP_HEADER_EXPECT, '100-continue');
+  EXPECT_CALL(m_server, onRequestError(_));
+  EXPECT_CALL(m_txn, sendHeaders(IsResponseStatusCode(417)));
+  EXPECT_CALL(m_txn, sendEOM());
+  m_transport->onHeadersComplete(std::move(req));
+}
+    
+      if (rightTy <= TCls) {
+    if (op == Op::Eq || op == Op::Same) {
+      push(env, gen(env, EqCls, left, right));
+      return;
+    }
+    if (op == Op::Neq || op == Op::NSame) {
+      push(
+        env, gen(env, XorBool, gen(env, EqCls, left, right), cns(env, true)));
+      return;
+    }
+  }
+    
+      static ReflectionTypeAliasHandle* Get(ObjectData* obj) {
+    return Native::data<ReflectionTypeAliasHandle>(obj);
+  }
+    
+    ArrayData* ArrayCommon::Pop(ArrayData* a, Variant &value) {
+  if (!a->empty()) {
+    auto const pos = a->iter_last();
+    value = a->getValue(pos);
+    return a->remove(a->getKey(pos));
+  }
+  value = uninit_null();
+  return a;
+}
+    
+    
+    {  for (unsigned int i = 0; i < TEST_SIZE; i++) {
+    funcs[i]->start();
+  }
+  for (unsigned int i = 0; i < TEST_SIZE; i++) {
+    funcs[i]->waitForEnd();
+  }
+  for (unsigned int i = 0; i < TEST_SIZE; i++) {
+    VS(transports[i]->m_code, 200);
+    VS(String(transports[i]->m_response), 'Hello, world!');
+  }
+  return Count(true);
+}
+    
+    /*
+ * Replace %{schema} with the repo schema in path.
+ */
+std::string insertSchema(const char* path);
