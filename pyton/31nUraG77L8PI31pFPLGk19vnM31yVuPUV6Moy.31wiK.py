3b04169926872f963895d8e6588e161b1454db65
SUNsung <weatherius@gmail.com>
2019-08-22 23:09:41 +0300

    Rewrite [31nUraG77L8PI31pFPLGk19vnM31yVuPUV6Moy.31wiK.py] File in: 2019-08-22 23:09:40

--- pyton/31nUraG77L8PI31pFPLGk19vnM31yVuPUV6Moy.31wiK.py
+++ pyton/31nUraG77L8PI31pFPLGk19vnM31yVuPUV6Moy.31wiK.py
@@ -1,149 +1,65 @@
 
         
-            # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+        import os, json, imp
+here = os.path.abspath(os.path.dirname(__file__))
+proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
+try:
+    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
+except:
+    README = ''
+CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
+VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
+    
+        stream_types = [  #this is just a sample. Will make it in prepare()
+        # {'id': '1080'},
+        # {'id': '720'},
+        # {'id': '360'},
+        # {'id': '288'},
+        # {'id': '190'},
+        # {'id': '180'},
+        
+    ]
+    
+    def rebuilt_url(url):
+    path = urllib.parse.urlparse(url).path
+    aid = path.split('/')[-1].split('_')[0]
+    return 'http://www.dailymotion.com/embed/video/{}?autoplay=1'.format(aid)
     
     
-class AuthActions(object):
-    def __init__(self, client):
-        self._client = client
+def huomaotv_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    room_id_pattern = r'huomao.com/(\d+)'
+    room_id = match1(url, room_id_pattern)
+    html = get_content(get_mobile_room_url(room_id))
     
-        auth.login()
-    # current user can't modify other user's post
-    assert client.post('/1/update').status_code == 403
-    assert client.post('/1/delete').status_code == 403
-    # current user doesn't see edit link
-    assert b'href='/1/update'' not in client.get('/').data
+    def ifeng_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+# old pattern /uuid.shtml
+# now it could be #uuid
+    id = r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', url)
+    if id:
+        return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
     
-        if current_app.config['JSONIFY_PRETTYPRINT_REGULAR'] or current_app.debug:
-        indent = 2
-        separators = (', ', ': ')
+    import codecs
+import errno
+import os
+import sys
+import warnings
+from tempfile import mkdtemp
     
+        def write(self, text=None):
+        if not self.write_to_stdout:
+            return self.write_err(text)
+        if text is None or isinstance(text, six.string_types) and text == 'None':
+            pass
+        if not self.stdout.closed:
+            stdout = self.stdout
+        else:
+            stdout = sys.stdout
+        stdout.write(decode_output(u'\r', target_stream=stdout))
+        text = to_text(text)
+        line = decode_output(u'{0}\n'.format(text), target_stream=stdout)
+        stdout.write(line)
+        stdout.write(CLEAR_LINE)
     
-fig = plt.figure(figsize=(6, 1.25))
-    
-    
-@pytest.mark.parametrize('grps', [
-    ['qux'], ['qux', 'quux']])
-@pytest.mark.parametrize('vals', [
-    [2, 2, 8, 2, 6],
-    [pd.Timestamp('2018-01-02'), pd.Timestamp('2018-01-02'),
-     pd.Timestamp('2018-01-08'), pd.Timestamp('2018-01-02'),
-     pd.Timestamp('2018-01-06')]])
-@pytest.mark.parametrize('ties_method,ascending,pct,exp', [
-    ('average', True, False, [2., 2., 5., 2., 4.]),
-    ('average', True, True, [0.4, 0.4, 1.0, 0.4, 0.8]),
-    ('average', False, False, [4., 4., 1., 4., 2.]),
-    ('average', False, True, [.8, .8, .2, .8, .4]),
-    ('min', True, False, [1., 1., 5., 1., 4.]),
-    ('min', True, True, [0.2, 0.2, 1.0, 0.2, 0.8]),
-    ('min', False, False, [3., 3., 1., 3., 2.]),
-    ('min', False, True, [.6, .6, .2, .6, .4]),
-    ('max', True, False, [3., 3., 5., 3., 4.]),
-    ('max', True, True, [0.6, 0.6, 1.0, 0.6, 0.8]),
-    ('max', False, False, [5., 5., 1., 5., 2.]),
-    ('max', False, True, [1., 1., .2, 1., .4]),
-    ('first', True, False, [1., 2., 5., 3., 4.]),
-    ('first', True, True, [0.2, 0.4, 1.0, 0.6, 0.8]),
-    ('first', False, False, [3., 4., 1., 5., 2.]),
-    ('first', False, True, [.6, .8, .2, 1., .4]),
-    ('dense', True, False, [1., 1., 3., 1., 2.]),
-    ('dense', True, True, [1. / 3., 1. / 3., 3. / 3., 1. / 3., 2. / 3.]),
-    ('dense', False, False, [3., 3., 1., 3., 2.]),
-    ('dense', False, True, [3. / 3., 3. / 3., 1. / 3., 3. / 3., 2. / 3.]),
-])
-def test_rank_args(grps, vals, ties_method, ascending, pct, exp):
-    key = np.repeat(grps, len(vals))
-    vals = vals * len(grps)
-    df = DataFrame({'key': key, 'val': vals})
-    result = df.groupby('key').rank(method=ties_method,
-                                    ascending=ascending, pct=pct)
-    
-        with tm.ensure_clean() as path:
-        df.to_json(path, compression=compression)
-        with open(path, 'rb') as f:
-            s3_resource.Bucket('pandas-test').put_object(Key='test-1', Body=f)
-    
-    import io
-    
-        def test_insertion_sort(self):
-        insertion_sort = InsertionSort()
-    
-    
-if __name__ == '__main__':
-  testutils.main()
-
-    
-    if __name__ == '__main__':
-  main()
-
-    
-    SetParseFn and SetParseFns allow you to set the functions Fire uses for parsing
-command line arguments to client code.
-'''
-    
-      def testNonComparable(self):
-    with self.assertRaises(ValueError):
-      tc.NonComparable() != 2  # pylint: disable=expression-not-assigned
-    with self.assertRaises(ValueError):
-      tc.NonComparable() == 2  # pylint: disable=expression-not-assigned
-    
-        Args:
-      arg1: arg1_description
-      arg2: arg2_description
-    '''
-    docstring_info = docstrings.parse(docstring)
-    expected_docstring_info = DocstringInfo(
-        summary='One line description.',
-        args=[
-            ArgInfo(name='arg1', description='arg1_description'),
-            ArgInfo(name='arg2', description='arg2_description'),
-        ]
-    )
-    self.assertEqual(docstring_info, expected_docstring_info)
-    
-      def testFloatForExpectedInt(self):
-    self.assertEqual(
-        fire.Fire(tc.MixedDefaults,
-                  command=['sum', '--alpha', '2.2', '--beta', '3.0']), 8.2)
-    self.assertEqual(
-        fire.Fire(
-            tc.NumberDefaults,
-            command=['integer_reciprocal', '--divisor', '5.0']), 0.2)
-    self.assertEqual(
-        fire.Fire(tc.NumberDefaults, command=['integer_reciprocal', '4.0']),
-        0.25)
-    
-      def SplitLine(self, line, width):
-    '''Splits line into width length chunks.
-    
-    '''A module for dealing with unknown string and environment encodings.'''
-    
-    # Usage
-# curl -H 'Host: example.com' localhost:8000
-# curl -H 'Host: sub.example.com' localhost:8000
-# curl -H 'Host: bp.example.com' localhost:8000/question
-# curl -H 'Host: bp.example.com' localhost:8000/answer
-    
-    
-def test_http_web_a_is_forwarded(docker_compose, nginxproxy):
-    r = nginxproxy.get('http://webA.nginx-proxy/port', allow_redirects=False)
-    assert r.status_code == 200
-    assert 'answer from port 81\n' == r.text
-    
-    
-raw_version = docker.from_env().version()['Version']
-pytestmark = pytest.mark.skipif(
-    versiontuple(raw_version) < (1, 13),
-    reason='Docker compose syntax v3 requires docker engine v1.13 or later (got %s)' % raw_version)
-    
-    
-##### Testing the handling of X-Forwarded-Ssl #####
-    
-    def test_X_Forwarded_Ssl_is_generated(docker_compose, nginxproxy):
-    r = nginxproxy.get('https://web.nginx-proxy.tld/headers')
-    assert r.status_code == 200
-    assert 'X-Forwarded-Ssl: on\n' in r.text
+        @property
+    def language(self):
+        raise NotImplementedError
