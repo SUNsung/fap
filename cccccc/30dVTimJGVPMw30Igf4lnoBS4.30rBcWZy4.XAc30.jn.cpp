9cf03f838552545df0b530603fcb71914217bf94
SUNsung <weatherius@gmail.com>
2019-08-22 07:46:26 +0300

    Rewrite [30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp] File in: 2019-08-22 07:46:26

--- cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
+++ cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
@@ -1,193 +1,158 @@
 
         
-        #endif
+            // Create the pipes to which the ConPTY will connect
+    if (CreatePipe(&hPipePTYIn, phPipeOut, NULL, 0) &&
+        CreatePipe(phPipeIn, &hPipePTYOut, NULL, 0))
+    {
+        // Determine required size of Pseudo Console
+        COORD consoleSize{};
+        CONSOLE_SCREEN_BUFFER_INFO csbi{};
+        HANDLE hConsole{ GetStdHandle(STD_OUTPUT_HANDLE) };
+        if (GetConsoleScreenBufferInfo(hConsole, &csbi))
+        {
+            consoleSize.X = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+            consoleSize.Y = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+        }
+    }
+    
+    	for (Map<ConstraintSW *, int>::Element *E = constraint_map.front(); E; E = E->next()) {
+    }
+    
+    #endif // CPU_PARTICLES_2D_EDITOR_PLUGIN_H
+
+    
+    protected:
+	static void _bind_methods();
+    
+    #include 'core/math/face3.h'
+#include 'core/reference.h'
+    
+    InputEventMouseMotion::InputEventMouseMotion() {
+}
+    
+    #endif // AUDIO_STREAM_PREVIEW_H
 
     
     
-    {  // Create an availability attribute for each observed platform and add
-  // to ToDecl.
-  DeclAttributes &Attrs = ToDecl->getAttrs();
-  for (auto &Pair : Inferred) {
-    auto *Attr = createAvailableAttr(Pair.first, Pair.second, Context);
-    Attrs.add(Attr);
-  }
-}
-    
-    /// Measure the relative offset between two pointers. This measures
-/// (referent - base) using wrapping arithmetic. The result is truncated if
-/// Offset is smaller than a pointer, with an assertion that the
-/// pre-truncation result is a sign extension of the truncated result.
-template<typename Offset, typename A, typename B>
-static inline Offset measureRelativeOffset(A *referent, B *base) {
-  static_assert(std::is_integral<Offset>::value &&
-                std::is_signed<Offset>::value,
-                'offset type should be signed integer');
-    }
-    
-    /// A value for `SymbolNamespace` which indicates that this type came
-/// from a C `typedef` that was imported as a distinct type instead
-/// of a `typealias`.  This can happen for reasons like:
-///
-/// - the `typedef` was declared with the `swift_wrapper` attribute
-/// - the `typedef` is a CF type
-constexpr static const char CTypedef[] = 't';
-    
-    #include 'swift/Basic/LLVM.h'
-#include 'swift/Basic/Range.h'
-#include 'llvm/ADT/Hashing.h'
-#include 'llvm/Support/MD5.h'
-#include 'llvm/Support/MemoryBuffer.h'
-#include 'llvm/Support/YAMLParser.h'
-#include 'llvm/Support/YAMLTraits.h'
-#include 'llvm/Support/raw_ostream.h'
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-    
-    namespace swift {
-struct SupplementaryOutputPaths {
-  /// The path to which we should emit an Objective-C header for the module.
-  ///
-  /// Currently only makes sense when the compiler has whole module knowledge.
-  /// The modes for which it makes sense incuide both WMO and the 'merge
-  /// modules' job that happens after the normal compilation jobs. That's where
-  /// the header is emitted in single-file mode, since it needs whole-module
-  /// information.
-  ///
-  /// \sa swift::printAsObjC
-  std::string ObjCHeaderOutputPath;
-    }
-    }
-    
-    namespace {
-    }
-    
-    REGISTER_CPU_OPERATOR(
-    MergeMultiMapFeatureTensors,
-    MergeMultiMapFeatureTensorsOp<CPUContext>);
-OPERATOR_SCHEMA(MergeMultiMapFeatureTensors)
-    .SetDoc(
-        'Merge given multi-feature tensors with map features into one.' + doc)
-    .NumInputs([](int n) { return n >= 5 && n % 5 == 0; })
-    .NumOutputs(5)
-    .Input(0, 'in1_lengths', '.lengths')
-    .Input(1, 'in1_keys', '.keys')
-    .Input(2, 'in1_values_lengths', '.values.lengths')
-    .Input(3, 'in1_values_keys', '.values.keys')
-    .Input(4, 'in1_values_values', '.values.values')
-    .Output(0, 'out_lengths', '.lengths')
-    .Output(1, 'out_keys', '.keys')
-    .Output(2, 'out_values_lengths', '.values_lengths')
-    .Output(3, 'out_values_keys', '.values.keys')
-    .Output(4, 'out_values_values', '.values.values');
-    
-    X before running op:
-[[ 3.813361   -1.319647    5.2089314  -4.931328    0.6218652 ]
- [ 7.2757645   5.5552588   5.785643   -2.4790506  -0.41400087]
- [ 1.1541046  -6.933266    3.3754056   1.6569928  -1.7670316 ]
- [-3.4932013   4.891472    1.5530115  -3.2443287  -4.605099  ]
- [-4.574543   -7.360948    5.91305    -8.196495   -5.357458  ]]
-X after running op:
-[[ 3. -2.  5. -5.  0.]
- [ 7.  5.  5. -3. -1.]
- [ 1. -7.  3.  1. -2.]
- [-4.  4.  1. -4. -5.]
- [-5. -8.  5. -9. -6.]]
-    
-    OPERATOR_SCHEMA(Glu)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-Applies gated linear unit to the input Tensor X. The output Y is half the size
-of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
-[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
-X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
-)DOC')
-    .Input(0, 'X', '1D input tensor')
-    .Output(0, 'Y', '1D output tensor');
-    
-    bool VideoStreamPlaybackGDNative::is_playing() const {
-	return playing;
-}
-    
-    	static _FORCE_INLINE_ uint32_t hash(const String &p_string) { return p_string.hash(); }
-	static _FORCE_INLINE_ uint32_t hash(const char *p_cstr) { return hash_djb2(p_cstr); }
-	static _FORCE_INLINE_ uint32_t hash(const uint64_t p_int) { return hash_one_uint64(p_int); }
-    
-    	bool exit_thread;
-    
-    	if (port_in != 0) {
-		MIDIPortDispose(port_in);
-		port_in = 0;
+    {	if (err != OK) {
+		mkdirerr->popup_centered_minsize(Size2(250, 80) * EDSCALE);
+	} else {
+		opened_paths.insert(dir);
+		//reload(dir.plus_file(makedirname->get_text()));
+		EditorFileSystem::get_singleton()->scan_changes(); //we created a dir, so rescan changes
 	}
-    
-    public:
-	virtual Error open();
-	virtual void close();
-    
-    #endif // AUDIO_STREAM_EDITOR_PLUGIN_H
-
-    
-    #endif // POWER_ANDROID_H
-
-    
-    void ImGui_ImplOpenGL3_DestroyFontsTexture()
-{
-    if (g_FontTexture)
-    {
-        ImGuiIO& io = ImGui::GetIO();
-        glDeleteTextures(1, &g_FontTexture);
-        io.Fonts->TexID = 0;
-        g_FontTexture = 0;
-    }
+	makedirname->set_text(''); // reset label
 }
     
-            ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-        ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-        ImGui::Checkbox('Another Window', &show_another_window);
+    #include 'scene/gui/box_container.h'
+#include 'scene/gui/menu_button.h'
     
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-        // Initialize Direct3D
-    if (!CreateDeviceD3D(hwnd))
-    {
-        CleanupDeviceD3D();
-        ::UnregisterClass(wc.lpszClassName, wc.hInstance);
-        return 1;
+      void RunServerInterceptors() {
+    auto* rpc_info = call_->server_rpc_info();
+    if (!reverse_) {
+      current_interceptor_index_ = 0;
+    } else {
+      current_interceptor_index_ = rpc_info->interceptors_.size() - 1;
     }
+    rpc_info->RunInterceptor(this, current_interceptor_index_);
+  }
     
-        // Poll and handle events (inputs, window resize, etc.)
-    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-    // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-    SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-        ImGui_ImplSDL2_ProcessEvent(&event);
-        // Capture events here, based on io.WantCaptureMouse and io.WantCaptureKeyboard
-    }
-    
-    static void ImGui_ImplDX9_SetupRenderState(ImDrawData* draw_data)
-{
-    // Setup viewport
-    D3DVIEWPORT9 vp;
-    vp.X = vp.Y = 0;
-    vp.Width = (DWORD)draw_data->DisplaySize.x;
-    vp.Height = (DWORD)draw_data->DisplaySize.y;
-    vp.MinZ = 0.0f;
-    vp.MaxZ = 1.0f;
-    g_pd3dDevice->SetViewport(&vp);
-    }
-    
-    // Implemented features:
-//  [X] Platform: Clipboard support.
-//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad'.
-//  [x] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'. FIXME: 3 cursors types are missing from GLFW.
-//  [X] Platform: Keyboard arrays indexed using GLFW_KEY_* codes, e.g. ImGui::IsKeyPressed(GLFW_KEY_SPACE).
-    
-    
-    {    return 0;
+    void ChannelArguments::SetLoadBalancingPolicyName(
+    const grpc::string& lb_policy_name) {
+  SetString(GRPC_ARG_LB_POLICY_NAME, lb_policy_name);
 }
+    
+    #endif
+
+    
+    static void iomgr_platform_shutdown_background_closure(void) {}
+    
+    
+    {  padded = gpr_leftpad('foo', '0', 5);
+  GPR_ASSERT(0 == strcmp('00foo', padded));
+  gpr_free(padded);
+}
+    
+    static grpc::internal::GrpcLibraryInitializer g_gli_initializer;
+ChannelCredentials::ChannelCredentials() { g_gli_initializer.summon(); }
+    
+    #include <grpcpp/security/credentials.h>
+    
+      request.set_message('Hello');
+  EXPECT_TRUE(stream->Write(request));
+  EXPECT_TRUE(stream->Read(&response));
+  EXPECT_EQ(response.message(), request.message());
+    
+    #include <stdio.h>
+#include <stdlib.h>
+#include 'PSTokenizer.h'
+    
+    #ifdef USE_GCC_PRAGMAS
+#pragma interface
+#endif
+    
+      // get alignment
+  if (dict->lookup('Dm', &obj)->isName()) {
+    const char *dm = obj.getName();
+    
+    if (strcmp('H', dm) == 0)
+      alignment = transitionHorizontal;
+    else if (strcmp('V', dm) == 0)
+      alignment = transitionVertical;
+  }
+  obj.free();
+    
+    void Parser::shift(int objNum) {
+  if (inlineImg > 0) {
+    if (inlineImg < 2) {
+      ++inlineImg;
+    } else {
+      // in a damaged content stream, if 'ID' shows up in the middle
+      // of a dictionary, we need to reset
+      inlineImg = 0;
+    }
+  } else if (buf2.isCmd('ID')) {
+    lexer->skipChar();		// skip char after 'ID' command
+    inlineImg = 1;
+  }
+  buf1.free();
+  buf2.shallowCopy(&buf1);
+  if (inlineImg > 0)		// don't buffer inline image data
+    buf2.initNull();
+  else
+    lexer->getObj(&buf2, objNum);
+}
+
+    
+    #include <stdlib.h>
+#include <stddef.h>
+#include 'ProfileData.h'
+    
+      MediaParameters* getMHParameters() { return &MH; }
+  MediaParameters* getBEParameters() { return &BE; }
+    
+      newsound->kind = kind;
+  if (fileName) {
+    newsound->fileName = fileName->copy();
+  }
+  newsound->samplingRate = samplingRate;
+  newsound->channels = channels;
+  newsound->bitsPerSample = bitsPerSample;
+  newsound->encoding = encoding;
+    
+      //----- update graphics state
+  virtual void updateAll(GfxState *state);
+  virtual void updateCTM(GfxState *state, double m11, double m12,
+			 double m21, double m22, double m31, double m32);
+  virtual void updateLineDash(GfxState *state);
+  virtual void updateFlatness(GfxState *state);
+  virtual void updateLineJoin(GfxState *state);
+  virtual void updateLineCap(GfxState *state);
+  virtual void updateMiterLimit(GfxState *state);
+  virtual void updateLineWidth(GfxState *state);
+  virtual void updateStrokeAdjust(GfxState *state);
+  virtual void updateFillColor(GfxState *state);
+  virtual void updateStrokeColor(GfxState *state);
+  virtual void updateBlendMode(GfxState *state);
+  virtual void updateFillOpacity(GfxState *state);
+  virtual void updateStrokeOpacity(GfxState *state);
