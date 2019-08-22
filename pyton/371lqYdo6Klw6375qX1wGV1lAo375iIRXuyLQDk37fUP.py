bc22aebea2d169d23a8353d632a6adaab54caa70
SUNsung <weatherius@gmail.com>
2019-08-22 20:37:25 +0300

    Rewrite [371lqYdo6Klw6375qX1wGV1lAo375iIRXuyLQDk37fUP.py] File in: 2019-08-22 20:37:24

--- pyton/371lqYdo6Klw6375qX1wGV1lAo375iIRXuyLQDk37fUP.py
+++ pyton/371lqYdo6Klw6375qX1wGV1lAo375iIRXuyLQDk37fUP.py
@@ -1,51 +1,287 @@
 
         
-            def notify_call_completed(self, call):
+            def possible_scores(self):
+        '''Return a list of possible scores, taking Aces into account.'''
         pass
     
-        def _park_starting_at_spot(self, spot, vehicle):
-        '''Occupy starting at spot.spot_number to vehicle.spot_size.'''
-        pass
-    
-            Emit key value pairs of the form:
-    
-        def reducer(self, key, values):
-        total = sum(values)
-        if total == 1:
-            yield key, total
+            Accessing a node updates its position to the front of the LRU list.
+        '''
+        node = self.lookup.get(query)
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
     
     
-class Person(object):
-    
-        decoded_masks = [
-        np.array(mask_util.decode(rle), dtype=np.float32) for rle in masks
-    ]
-    
-        def test_throws_on_non_1D_arrays(self):
-        X = np.zeros((2, 2), dtype=np.float32)
-        with self.assertRaisesRegexp(RuntimeError, 'X\.ndim\(\) == 1'):
-            self._run_zero_even_op(X)
+class ParkingSpot(object):
     
     
-def combined_roidb_for_training(dataset_names, proposal_files):
-    '''Load and concatenate roidbs for one or more datasets, along with optional
-    object proposals. The roidb entries are then prepared for use in training,
-    which involves caching certain types of metadata for each roidb entry.
+if __name__ == '__main__':
+    RemoveDuplicateUrls.run()
+
+    
+        ax.plot(x_test, func(x_test), color='blue', label='sin($2\\pi x$)')
+    ax.scatter(x_train, y_train, s=50, alpha=0.5, label='observation')
+    ax.plot(x_test, ymean, color='red', label='predict mean')
+    ax.fill_between(x_test, ymean-ystd, ymean+ystd,
+                    color='pink', alpha=0.5, label='predict std')
+    ax.set_ylim(-1.3, 1.3)
+    ax.legend()
+    title = '$\\alpha$_init$={:.2f},\\ \\lambda$_init$={}$'.format(
+            init[0], init[1])
+    if i == 0:
+        title += ' (Default)'
+    ax.set_title(title, fontsize=12)
+    text = '$\\alpha={:.1f}$\n$\\lambda={:.3f}$\n$L={:.1f}$'.format(
+           reg.alpha_, reg.lambda_, reg.scores_[-1])
+    ax.text(0.05, -1.0, text, fontsize=12)
+    
+    # Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+h = .02  # step size in the mesh
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+    
+    
+def _get_git_revision():
+    try:
+        revision = subprocess.check_output(REVISION_CMD.split()).strip()
+    except (subprocess.CalledProcessError, OSError):
+        print('Failed to execute git to get revision')
+        return None
+    return revision.decode('utf-8')
+    
     '''
-    def get_roidb(dataset_name, proposal_file):
-        ds = JsonDataset(dataset_name)
-        roidb = ds.get_roidb(
-            gt=True,
-            proposal_file=proposal_file,
-            crowd_filter_thresh=cfg.TRAIN.CROWD_FILTER_THRESH
-        )
-        if cfg.TRAIN.USE_FLIPPED:
-            logger.info('Appending horizontally-flipped training examples...')
-            extend_with_flipped_entries(roidb, ds)
-        logger.info('Loaded dataset: {:s}'.format(ds.name))
-        return roidb
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: Simplified BSD
+    
+    
+if __name__ == '__main__':
+    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
+    # block to be able to use a multi-core grid search that also works under
+    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
+    # The multiprocessing module is used as the backend of joblib.Parallel
+    # that is used when n_jobs != 1 in GridSearchCV
+    
+    The example is engineered to show the effect of the choice of different
+metrics. It is applied to waveforms, which can be seen as
+high-dimensional vector. Indeed, the difference between metrics is
+usually more pronounced in high dimension (in particular for euclidean
+and cityblock).
+    
+    for name, label in [('Setosa', 0),
+                    ('Versicolour', 1),
+                    ('Virginica', 2)]:
+    ax.text3D(X[y == label, 3].mean(),
+              X[y == label, 0].mean(),
+              X[y == label, 2].mean() + 2, name,
+              horizontalalignment='center',
+              bbox=dict(alpha=.2, edgecolor='w', facecolor='w'))
+# Reorder the labels to have colors matching the cluster results
+y = np.choose(y, [1, 2, 0]).astype(np.float)
+ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y, edgecolor='k')
+    
+    MOCK_MODULES = ['face_recognition_models', 'Click', 'dlib', 'numpy', 'PIL']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+    
+    setup_face_landmarks = '''
+import face_recognition
+    
+        :param X_img_path: path to image to be recognized
+    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
+    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
+    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
+           of mis-classifying an unknown person as a known one.
+    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
+        For faces of unrecognized persons, the name 'unknown' will be returned.
+    '''
+    if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
+        raise Exception('Invalid image path: {}'.format(X_img_path))
+    
+    # Load a sample picture and learn how to recognize it.
+print('Loading known face image(s)')
+obama_image = face_recognition.load_image_file('obama_small.jpg')
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    
+    import face_recognition
+import picamera
+import numpy as np
+    
+    
+def face_distance(face_encodings, face_to_compare):
+    '''
+    Given a list of face encodings, compare them to a known face encoding and get a euclidean distance
+    for each comparison face. The distance tells you how similar the faces are.
+    
+        return ret
+    
+    
+def get_params_dict():
+    '''以字典形式获取所有 trainable 参数'''
+    param_dict = dict()
+    for var in tf.trainable_variables():
+        param_dict[var.name] = {'shape': list(map(int, var.shape)),
+                                'number': int(reduce(mul, var.shape, 1))}
+    return param_dict
+    
+    from detectron.core.config import cfg
+from detectron.datasets.dataset_catalog import get_raw_dir
+    
+    # Required dataset entry keys
+_IM_DIR = 'image_directory'
+_ANN_FN = 'annotation_file'
+    
+        _compute_and_log_stats(roidb)
     
     ... -> RoI ----\                               /-> box cls output -> cls loss
                 -> RoIFeatureXform -> box head
 ... -> Feature /                               \-> box reg output -> reg loss
        Map
+    
+    
+def _whctrs(anchor):
+    '''Return width, height, x center, and y center for an anchor (window).'''
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[1] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
+    
+    
+def build_data_parallel_model(model, single_gpu_build_func):
+    '''Build a data parallel model given a function that builds the model on a
+    single GPU.
+    '''
+    if model.only_build_forward_pass:
+        single_gpu_build_func(model)
+    elif model.train:
+        all_loss_gradients = _build_forward_graph(model, single_gpu_build_func)
+        # Add backward pass on all GPUs
+        model.AddGradientOperators(all_loss_gradients)
+        if cfg.NUM_GPUS > 1:
+            _add_allreduce_graph(model)
+        for gpu_id in range(cfg.NUM_GPUS):
+            # After allreduce, all GPUs perform SGD updates on their identical
+            # params and gradients in parallel
+            with c2_utils.NamedCudaScope(gpu_id):
+                add_single_gpu_param_update_ops(model, gpu_id)
+    else:
+        # Test-time network operates on single GPU
+        # Test-time parallelism is implemented through multiprocessing
+        with c2_utils.NamedCudaScope(model.target_gpu_id):
+            single_gpu_build_func(model)
+    
+        # ==========================================================================
+    # classification tower with logits and prob prediction
+    # ==========================================================================
+    for lvl in range(k_min, k_max + 1):
+        bl_in = blobs_in[k_max - lvl]  # blobs_in is in reversed order
+        # classification tower stack convolution starts
+        for nconv in range(cfg.RETINANET.NUM_CONVS):
+            suffix = 'n{}_fpn{}'.format(nconv, lvl)
+            dim_in, dim_out = dim_in, dim_in
+            if lvl == k_min:
+                bl_out = model.Conv(
+                    bl_in,
+                    'retnet_cls_conv_' + suffix,
+                    dim_in,
+                    dim_out,
+                    3,
+                    stride=1,
+                    pad=1,
+                    weight_init=('GaussianFill', {
+                        'std': 0.01
+                    }),
+                    bias_init=('ConstantFill', {
+                        'value': 0.
+                    })
+                )
+            else:
+                bl_out = model.ConvShared(
+                    bl_in,
+                    'retnet_cls_conv_' + suffix,
+                    dim_in,
+                    dim_out,
+                    3,
+                    stride=1,
+                    pad=1,
+                    weight='retnet_cls_conv_n{}_fpn{}_w'.format(nconv, k_min),
+                    bias='retnet_cls_conv_n{}_fpn{}_b'.format(nconv, k_min)
+                )
+            bl_in = model.Relu(bl_out, bl_out)
+            bl_feat = bl_in
+        # cls tower stack convolution ends. Add the logits layer now
+        if lvl == k_min:
+            retnet_cls_pred = model.Conv(
+                bl_feat,
+                'retnet_cls_pred_fpn{}'.format(lvl),
+                dim_in,
+                cls_pred_dim * A,
+                3,
+                pad=1,
+                stride=1,
+                weight_init=('GaussianFill', {
+                    'std': 0.01
+                }),
+                bias_init=bias_init
+            )
+        else:
+            retnet_cls_pred = model.ConvShared(
+                bl_feat,
+                'retnet_cls_pred_fpn{}'.format(lvl),
+                dim_in,
+                cls_pred_dim * A,
+                3,
+                pad=1,
+                stride=1,
+                weight='retnet_cls_pred_fpn{}_w'.format(k_min),
+                bias='retnet_cls_pred_fpn{}_b'.format(k_min)
+            )
+        if not model.train:
+            if cfg.RETINANET.SOFTMAX:
+                model.net.GroupSpatialSoftmax(
+                    retnet_cls_pred,
+                    'retnet_cls_prob_fpn{}'.format(lvl),
+                    num_classes=cls_pred_dim
+                )
+            else:
+                model.net.Sigmoid(
+                    retnet_cls_pred, 'retnet_cls_prob_fpn{}'.format(lvl)
+                )
+        if cfg.RETINANET.SHARE_CLS_BBOX_TOWER:
+            bbox_feat_list.append(bl_feat)
+    
+    '''Common utility functions for RPN and RetinaNet minibtach blobs preparation.
+'''
+    
+        # Select background RoIs as those within [BG_THRESH_LO, BG_THRESH_HI)
+    bg_inds = np.where(
+        (max_overlaps < cfg.TRAIN.BG_THRESH_HI) &
+        (max_overlaps >= cfg.TRAIN.BG_THRESH_LO)
+    )[0]
+    # Compute number of background RoIs to take from this image (guarding
+    # against there being fewer than desired)
+    bg_rois_per_this_image = rois_per_image - fg_rois_per_this_image
+    bg_rois_per_this_image = np.minimum(bg_rois_per_this_image, bg_inds.size)
+    # Sample foreground regions without replacement
+    if bg_inds.size > 0:
+        bg_inds = npr.choice(
+            bg_inds, size=bg_rois_per_this_image, replace=False
+        )
+    
+        net = core.Net('dequeue_net')
+    net.type = 'dag'
+    all_blobs = []
+    for gpu_id in range(cfg.NUM_GPUS):
+        with core.NameScope('gpu_{}'.format(gpu_id)):
+            with core.DeviceScope(muji.OnGPU(gpu_id)):
+                for blob_name in blob_names:
+                    blob = core.ScopedName(blob_name)
+                    all_blobs.append(blob)
+                    workspace.CreateBlob(blob)
+                    logger.info('Creating blob: {}'.format(blob))
+                net.DequeueBlobs(
+                    roi_data_loader._blobs_queue_name, blob_names)
+    logger.info('Protobuf:\n' + str(net.Proto()))
