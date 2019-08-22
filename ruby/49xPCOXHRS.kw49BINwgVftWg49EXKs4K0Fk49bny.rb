2ad05581e1f79e95e3902f2cbb7963ef9cc8bee0
SUNsung <weatherius@gmail.com>
2019-08-22 03:25:02 +0300

    Rewrite [49xPCOXHRS.kw49BINwgVftWg49EXKs4K0Fk49bny.rb] File in: 2019-08-22 03:25:02

--- ruby/49xPCOXHRS.kw49BINwgVftWg49EXKs4K0Fk49bny.rb
+++ ruby/49xPCOXHRS.kw49BINwgVftWg49EXKs4K0Fk49bny.rb
@@ -1,53 +1,78 @@
 
         
-          # POST /resource/sign_in
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  end
+              def setup_large_app_icon
+        large_app_icon = raw_data['largeAppIcon']['value']
+        @large_app_icon = nil
+        @large_app_icon = Tunes::AppImage.factory(large_app_icon) if large_app_icon
+      end
     
-        def reset_password_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :reset_password_instructions, opts)
+            value = arguments.first
+        value = yield if value.nil? && block_given?
+    
+            # Then check for iTMSTransporter in the Xcode path
+        [
+          '../Applications/Application Loader.app/Contents/MacOS/itms',
+          '../Applications/Application Loader.app/Contents/itms',
+          '../SharedFrameworks/ContentDeliveryServices.framework/Versions/A/itms' # For Xcode 11
+        ].each do |path|
+          result = File.expand_path(File.join(self.xcode_path, path))
+          return result if File.exist?(result)
+        end
+        UI.user_error!('Could not find transporter at #{self.xcode_path}. Please make sure you set the correct path to your Xcode installation.')
+      elsif self.windows?
+        [
+          'C:/Program Files (x86)/itms'
+        ].each do |path|
+          return path if File.exist?(path)
+        end
+        UI.user_error!('Could not find transporter at usual locations. Please use environment variable `FASTLANE_ITUNES_TRANSPORTER_PATH` to specify your installation path.')
+      else
+        # not Mac or Windows
+        return ''
+      end
     end
     
-    module Devise
-  module Controllers
-    # Create url helpers to be used with resource/scope configuration. Acts as
-    # proxies to the generated routes created by devise.
-    # Resource param can be a string or symbol, a class, or an instance object.
-    # Example using a :user resource:
-    #
-    #   new_session_path(:user)      => new_user_session_path
-    #   session_path(:user)          => user_session_path
-    #   destroy_session_path(:user)  => destroy_user_session_path
-    #
-    #   new_password_path(:user)     => new_user_password_path
-    #   password_path(:user)         => user_password_path
-    #   edit_password_path(:user)    => edit_user_password_path
-    #
-    #   new_confirmation_path(:user) => new_user_confirmation_path
-    #   confirmation_path(:user)     => user_confirmation_path
-    #
-    # Those helpers are included by default to ActionController::Base.
-    #
-    # In case you want to add such helpers to another class, you can do
-    # that as long as this new class includes both url_helpers and
-    # mounted_helpers. Example:
-    #
-    #     include Rails.application.routes.url_helpers
-    #     include Rails.application.routes.mounted_helpers
-    #
-    module UrlHelpers
-      def self.remove_helpers!
-        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
-          remove_method method
+            # @return (Array) Returns all Watches registered for this account
+        def all_watches
+          client.devices_by_class('watch').map { |device| self.factory(device) }
         end
+    
+          def self.details
+        'This is useful when deploying automated builds. The changelog from Jenkins lists all the commit messages since the last build.'
       end
     
-          def extend_remember_period
-        self.class.extend_remember_period
-      end
+      private
+    
+        ActivityPub::DeliveryWorker.push_bulk(Relay.enabled.pluck(:inbox_url)) do |inbox_url|
+      [signed_payload, @account.id, inbox_url]
+    end
+  rescue ActiveRecord::RecordNotFound
+    true
+  end
+    
+      let(:json) do
+    {
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      id: 'foo',
+      type: 'Announce',
+      actor: 'https://example.com/actor',
+      object: object_json,
+      to: 'http://example.com/followers',
+    }.with_indifferent_access
+  end
+    
+      it 'should respect scopes' do
+    @session.within '//p[@id='first']' do
+      expect { @session.assert_no_selector('.//a[@id='foo']') }.to raise_error(Capybara::ElementNotFound)
+      @session.assert_no_selector('.//a[@id='red']')
+    end
+  end
+    
+      it 'should allow regexp matches' do
+    expect do
+      @session.assert_title(/w[a-z]{3}_js/)
+    end.not_to raise_error
+    
+      it 'should be false if the given selector does not match the element' do
+    expect { @element.assert_matches_selector(:css, '.not_number') }.to raise_error(Capybara::ElementNotFound)
+  end
