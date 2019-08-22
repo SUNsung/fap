2f8ca58a9604434eba4d63efba24a087e88e7a6b
SUNsung <weatherius@gmail.com>
2019-08-22 09:21:25 +0300

    Rewrite [32on2Gk1fYKr232ocDQQhqEGpk32cBnTCxGrNWw32Lu..cpp] File in: 2019-08-22 09:21:24

--- cccccc/32on2Gk1fYKr232ocDQQhqEGpk32cBnTCxGrNWw32Lu..cpp
+++ cccccc/32on2Gk1fYKr232ocDQQhqEGpk32cBnTCxGrNWw32Lu..cpp
@@ -1,111 +1,38 @@
 
         
-        #include 'base/callback.h'
-#include 'base/location.h'
-#include 'base/single_thread_task_runner.h'
-#include 'uv.h'  // NOLINT(build/include)
-    
-    
-    {    manager->GetCookieList(url, options, std::move(callback));
-  }
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(Cookies);
-};
-    
-      base::DictionaryValue request;
-  int request_id = ++previous_request_id_;
-  pending_requests_.emplace(request_id, std::move(promise));
-  request.SetInteger('id', request_id);
-  request.SetString('method', method);
-  if (!command_params.empty())
-    request.Set('params',
-                base::Value::ToUniquePtrValue(command_params.Clone()));
-    
-    #ifndef SHELL_BROWSER_API_ATOM_API_DOWNLOAD_ITEM_H_
-#define SHELL_BROWSER_API_ATOM_API_DOWNLOAD_ITEM_H_
-    
-    #include <map>
-#include <string>
-#include <vector>
-    
-    
-    {}  // namespace
-    
-    
-    {}  // namespace api
-    
-    namespace api {
+        void SILGenModule::emitLazyConformancesForFunction(SILFunction *F) {
+  LazyConformanceEmitter emitter(*this);
     }
     
-    /** @brief The function renders on two NV12 planes passed drawing primitivies
+    /// This describes a list of parameters.  Each parameter descriptor is tail
+/// allocated onto this list.
+class alignas(ParamDecl *) ParameterList final :
+    private llvm::TrailingObjects<ParameterList, ParamDecl *> {
+  friend TrailingObjects;
+    }
     
-        VideoWriter writer;
-    EXPECT_NO_THROW(writer.open(pipeline, apiPref, 0/*fourcc*/, 30/*fps*/, frame_size, true));
-    EXPECT_FALSE(writer.isOpened());
-    // no frames
-    EXPECT_NO_THROW(writer.release());
+    KNOWN_STDLIB_TYPE_DECL(AnyKeyPath, NominalTypeDecl, 0)
+KNOWN_STDLIB_TYPE_DECL(PartialKeyPath, NominalTypeDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(KeyPath, NominalTypeDecl, 2)
+KNOWN_STDLIB_TYPE_DECL(WritableKeyPath, NominalTypeDecl, 2)
+KNOWN_STDLIB_TYPE_DECL(ReferenceWritableKeyPath, NominalTypeDecl, 2)
     
-            __host__ __device__ __forceinline__ thresh_to_zero_func() {}
-       __host__  __device__ __forceinline__ thresh_to_zero_func(const thresh_to_zero_func& other)
-            : thresh(other.thresh) {}
-    
-            TwilightShifted(int n) : ColorMap() {
-            init(n);
+            // FIXME: This code needs to be cleaned up and updated
+        // to make it work for 32 bit platforms.
+        if (SectionName != '.sw5cptr' && SectionName != '.sw5bltn') {
+          Begin += 8;
+          End -= 8;
         }
     
-    #endif // OPENCV_GAPI_GTYPE_TRAITS_HPP
-
-    
-        Mat pts0_copy = pts0.clone();
-    Mat pts1_copy = pts1.clone();
-    
-    TEST(Imgcodecs_Tiff, decode_tile_remainder)
-{
-    /* see issue #3472 - dealing with tiled images where the tile size is
-     * not a multiple of image size.
-     * The tiled images were created with 'convert' from ImageMagick,
-     * using the command 'convert <input> -define tiff:tile-geometry=128x128 -depth [8|16] <output>
-     * Note that the conversion to 16 bits expands the range from 0-255 to 0-255*255,
-     * so the test converts back but rounding errors cause small differences.
-     */
-    const string root = cvtest::TS::ptr()->get_data_path();
-    cv::Mat img = imread(root + 'readwrite/non_tiled.tif',-1);
-    ASSERT_FALSE(img.empty());
-    ASSERT_TRUE(img.channels() == 3);
-    cv::Mat tiled8 = imread(root + 'readwrite/tiled_8.tif', -1);
-    ASSERT_FALSE(tiled8.empty());
-    ASSERT_PRED_FORMAT2(cvtest::MatComparator(0, 0), img, tiled8);
-    cv::Mat tiled16 = imread(root + 'readwrite/tiled_16.tif', -1);
-    ASSERT_FALSE(tiled16.empty());
-    ASSERT_TRUE(tiled16.elemSize() == 6);
-    tiled16.convertTo(tiled8, CV_8UC3, 1./256.);
-    ASSERT_PRED_FORMAT2(cvtest::MatComparator(2, 0), img, tiled8);
-    // What about 32, 64 bit?
+    void TypeRefBuilder::dumpCaptureSection(std::ostream &OS) {
+  for (const auto &sections : ReflectionInfos) {
+    uint64_t TypeRefOffset = sections.Capture.SectionOffset
+                           - sections.TypeReference.SectionOffset;
+    for (const auto &descriptor : sections.Capture.Metadata) {
+      auto info = getClosureContextInfo(descriptor, TypeRefOffset);
+      info.dump(OS);
+    }
+  }
 }
     
-    
-    {    // exception out_of_range.401
-    try
-    {
-        // try to write at a nonexisting key
-        object.at('the fast') = 'il rapido';
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-}
-
-    
-        // change an array element
-    j.at('/array/1'_json_pointer) = 21;
-    // output the changed array
-    std::cout << j['array'] << '\n';
-    
-    int main()
-{
-    // create JSON values
-    json object = {{'one', 1}, {'two', 2}};
-    json null;
-    }
+    #endif // SWIFT_ABI_CLASS_H
