998b8bce6e533964cb4c00b5068811c6ff7c0538
SUNsung <weatherius@gmail.com>
2019-08-22 14:25:26 +0300

    Rewrite [34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp] File in: 2019-08-22 14:25:26

--- cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
+++ cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
@@ -1,204 +1,121 @@
 
         
-         protected:
-  explicit AutoUpdater(v8::Isolate* isolate);
-  ~AutoUpdater() override;
+          void visitInjectEnumAddrInst(InjectEnumAddrInst *IEAI) {
+    SGM.useConformancesFromType(IEAI->getOperand()->getType().getASTType());
+  }
     
-    #endif  // ATOM_BROWSER_API_ATOM_API_NET_H_
+    KNOWN_STDLIB_TYPE_DECL(AnyKeyPath, NominalTypeDecl, 0)
+KNOWN_STDLIB_TYPE_DECL(PartialKeyPath, NominalTypeDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(KeyPath, NominalTypeDecl, 2)
+KNOWN_STDLIB_TYPE_DECL(WritableKeyPath, NominalTypeDecl, 2)
+KNOWN_STDLIB_TYPE_DECL(ReferenceWritableKeyPath, NominalTypeDecl, 2)
+    
+    #endif
 
     
-    // All instances of TrackableObject will be kept in a weak map and can be got
-// from its ID.
-template <typename T>
-class TrackableObject : public TrackableObjectBase,
-                        public mate::EventEmitter<T> {
- public:
-  // Mark the JS object as destroyed.
-  void MarkDestroyed() {
-    v8::Local<v8::Object> wrapper = Wrappable<T>::GetWrapper();
-    if (!wrapper.IsEmpty()) {
-      wrapper->SetAlignedPointerInInternalField(0, nullptr);
+      bool operator==(FieldDescriptorIterator const &other) const {
+    return Cur == other.Cur && End == other.End;
+  }
+    
+    using ReadBytesResult = swift::remote::MemoryReader::ReadBytesResult;
+    
+    #endif
+#endif
+
+    
+    
+    {  void moduleLoad(const IniSetting::Map& ini, Hdf globalConfig) override {
+    Config::Bind(Enabled, ini, globalConfig,
+                 'HealthMonitor.EnableHealthMonitor', true);
+    Config::Bind(UpdateFreq, ini, globalConfig,
+                 'HealthMonitor.UpdateFreq', 1000 /* miliseconds */);
+  }
+} s_host_health_monitor_extension;
+    
+    void ProxygenTransport::onBody(std::unique_ptr<folly::IOBuf> chain) noexcept {
+  // If we've already sent a response, allow up to 128k of data to be received
+  // before just sending an abort. This gives the client an opportunity to
+  // process the response before receiving some form of a reset caused by the
+  // abort.
+  if (m_sendEnded && m_clientTxn->isEgressComplete()) {
+    m_bodyLengthPastLimit -= chain->computeChainDataLength();
+    if (m_bodyLengthPastLimit <= 0) {
+      Logger::Warning('Received body after a response has completed, aborting');
+      abort();
     }
+    return;
   }
     }
     
-    #endif  // ATOM_BROWSER_NET_ASAR_ASAR_PROTOCOL_HANDLER_H_
-
-    
-    #include 'atom/browser/net/url_request_about_job.h'
-    
-    namespace relauncher {
-    }
-    
-    void DragFileItems(const std::vector<base::FilePath>& files,
-                   const gfx::Image& icon,
-                   gfx::NativeView view);
+    #include 'hphp/runtime/base/array-data.h'
+#include 'hphp/runtime/base/array-data-defs.h'
+#include 'hphp/runtime/base/array-init.h'
+#include 'hphp/runtime/base/array-iterator.h'
+#include 'hphp/runtime/base/mixed-array-defs.h'
+#include 'hphp/runtime/base/object-data.h'
+#include 'hphp/runtime/base/set-array.h'
+#include 'hphp/runtime/base/packed-array.h'
+#include 'hphp/runtime/base/type-variant.h'
     
     
-    {
-    {
-    {
-    {
-    {}  // namespace
-}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
-    
-      uint8 expected_data[] = {
-      0x5,
-      // All as is (00 op)
-      0x1,  0x0A, 0x0,
-      // Underscore, upper + 9 (10 op)
-      0x3,  0xCA, 0x0,
-      //  Upper + 3 (10 op), underscore, upper + 5 (10 op)
-      0x2,  0x44, 0xC6, 0x0,
-      // All Upper for 4 (11 op), underscore, underscore, upper + 5 (10 op),
-      // underscore, lower + 0 (01 op)
-      0x4,  0x64, 0x80, 0xC5, 0xA1, 0x0,
-      // 2 byte key: as is + 3 (00 op), underscore, lower + 4 (01 op),
-      //   underscore, lower + 3 (01 op), underscore, lower + 1 (01 op),
-      //   underscore, lower + 30 (01 op), as is + 30 (00 op), as is + 13 (00
-      //   op),
-      //   underscore, as is + 3 (00 op)
-      0xE8, 0x07, 0x04, 0xA5, 0xA4, 0xA2, 0xBF, 0x1F, 0x0E, 0x84, 0x0,
+    {  enum class RefCheckResult {
+    Pass, // No refs
+    Fail, // Referenced ref
+    Collapse // Non-referenced ref
   };
-  string expected((const char*)expected_data, sizeof(expected_data));
-    
-    
-bool ZeroCopyOutputStream::WriteAliasedRaw(const void* /* data */,
-                                           int /* size */) {
-  GOOGLE_LOG(FATAL) << 'This ZeroCopyOutputStream doesn't support aliasing. '
-                'Reaching here usually means a ZeroCopyOutputStream '
-                'implementation bug.';
-  return false;
-}
-    
-    
-    {
-    {
-    {
-}  // namespace internal
-}  // namespace protobuf
-}  // namespace google
-    
-     public:
-  static void StripFile(const FileDescriptor* old_file,
-                        FileDescriptorProto *file) {
-    for (int i = file->mutable_message_type()->size() - 1; i >= 0; i--) {
-      if (IsMessageSet(old_file->message_type(i))) {
-        file->mutable_message_type()->DeleteSubrange(i, 1);
-        continue;
-      }
-      StripMessage(old_file->message_type(i), file->mutable_message_type(i));
-    }
-    for (int i = file->mutable_extension()->size() - 1; i >= 0; i--) {
-      auto field = old_file->extension(i);
-      if (field->type() == FieldDescriptor::TYPE_GROUP ||
-          IsMessageSet(field->message_type()) ||
-          IsMessageSet(field->containing_type())) {
-        file->mutable_extension()->DeleteSubrange(i, 1);
-      }
-    }
-  }
-    
-    namespace CalcEngine
-{
-    class Number
-    {
-    public:
-        Number() noexcept;
-        Number(int32_t sign, int32_t exp, std::vector<uint32_t> const& mantissa) noexcept;
-    }
-    }
-    
-    
-    {    Rational Sinh(Rational const& rat);
-    Rational Cosh(Rational const& rat);
-    Rational Tanh(Rational const& rat);
-    Rational ASinh(Rational const& rat);
-    Rational ACosh(Rational const& rat);
-    Rational ATanh(Rational const& rat);
-}
-
-    
-                property bool IsCurrentAfterLast
-            {
-                virtual bool get() = Windows::UI::Xaml::Data::ICollectionView::IsCurrentAfterLast::get
-                {
-                    return m_currentPosition >= static_cast<int>(m_source->Size);
-                }
-            }
-    
-    void NarratorNotifier::OnAnnouncementChanged(_In_ DependencyObject ^ dependencyObject, _In_ DependencyPropertyChangedEventArgs ^ e)
-{
-    auto instance = safe_cast<NarratorNotifier ^>(dependencyObject);
-    if (instance != nullptr)
-    {
-        instance->Announce(safe_cast<NarratorAnnouncement ^>(e->NewValue));
-    }
-}
-
-    
-    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
-    
-    namespace apollo {
-namespace drivers {
-namespace conti_radar {
-    }
-    }
-    }
-    
-    #include 'modules/localization/msf/local_map/base_map/base_map_matrix.h'
-    
-    
-    { protected:
-  SpeedLimit speed_limit_;
+  static RefCheckResult CheckForRefs(const ArrayData*);
 };
     
-    void SplineSegKernel::CalculateSecondOrderDerivative(
-    const uint32_t num_params) {
-  kernel_second_order_derivative_ =
-      Eigen::MatrixXd::Zero(num_params, num_params);
-  for (int r = 2; r < kernel_second_order_derivative_.rows(); ++r) {
-    for (int c = 2; c < kernel_second_order_derivative_.cols(); ++c) {
-      kernel_second_order_derivative_(r, c) =
-          (r * r - r) * (c * c - c) / (r + c - 3.0);
+        inline void consumer_remove(Coroutine *co)
+    {
+        consumer_queue.remove(co);
     }
-  }
+    
+        DataBuffer(const char *str, size_t length)
+    {
+        copy((void *) str, length);
+    }
+    
+    
+    {    for (i = 0; i < 10; i++)
+    {
+        free(lists[i]);
+    }
 }
-    
-    // config detail: {'name': 'manual_input', 'offset': 0.0, 'precision': 0.001,
-// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 7,
-// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
-double Accelrpt68::manual_input(const std::uint8_t* bytes,
-                                int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-    
-    {
-    {
-    {}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
 
     
     
-    {  auto &brakerpt = chassis_detail.gem().brake_rpt_6c();
-  EXPECT_DOUBLE_EQ(brakerpt.manual_input(), 0.258);
-  EXPECT_DOUBLE_EQ(brakerpt.commanded_value(), 0.772);
-  EXPECT_DOUBLE_EQ(brakerpt.output_value(), 4.37);
-  EXPECT_EQ(brakerpt.brake_on_off(), Brake_rpt_6c::BRAKE_ON_OFF_ON);
+    {    ASSERT_GT(cid, 0);
+    Coroutine::get_by_cid(cid)->resume();
+    ASSERT_EQ(cid, _cid);
 }
+
     
-    // config detail: {'name': 'longitude_seconds', 'offset': 0.0, 'precision': 1.0,
-// 'len': 8, 'is_signed_var': True, 'physical_range': '[-128|127]', 'bit': 47,
-// 'type': 'int', 'order': 'motorola', 'physical_unit': 'sec'}
-int Latlonheadingrpt82::longitude_seconds(const std::uint8_t* bytes,
-                                          int32_t length) const {
-  Byte t0(bytes + 5);
-  int32_t x = t0.get_byte(0, 8);
+    TEST(os_wait, wait_before_child_exit)
+{
+    coro_test([](void *arg)
+    {
+        swoole_coroutine_signal_init();
     }
+    }
+    
+        private:
+        void addRead() {
+            if (m_read) return;
+            m_read = new QSocketNotifier(m_ctx->c.fd, QSocketNotifier::Read, 0);
+            connect(m_read, SIGNAL(activated(int)), this, SLOT(read()));
+        }
+    
+            /**
+         * It returns a string with the whole Rectangle<T> data. Useful for debugging.
+         * The format is: `[x, y, width, height]`
+         * @return A string with the Rectangle<T> values in the above format.
+         */
+        std::string toString() const;
+    
+            virtual void initializationOnThread();
+    
+    #endif // OPENPOSE_HAND_HAND_PARAMETERS_HPP
+
+    
+    #endif // OPENPOSE_TRACKING_PERSON_ID_EXTRACTOR_HPP
