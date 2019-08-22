f2144cc57a183faab97a6bf261b7dd6817acfb7c
SUNsung <weatherius@gmail.com>
2019-08-23 02:55:37 +0300

    Rewrite [27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp] File in: 2019-08-23 02:55:37

--- cccccc/27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp
+++ cccccc/27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp
@@ -1,49 +1,243 @@
 
         
-        template <typename T>
-struct HashMapComparatorDefault {
-	static bool compare(const T &p_lhs, const T &p_rhs) {
-		return p_lhs == p_rhs;
-	}
+          // Array holding scores for each orientation id [0,3].
+  // Orientation ids [0..3] map to [0, 270, 180, 90] degree orientations of the
+  // page respectively, where the values refer to the amount of clockwise
+  // rotation to be applied to the page for the text to be upright and readable.
+  float orientations[4];
+  // Script confidence scores for each of 4 possible orientations.
+  float scripts_na[4][kMaxNumberOfScripts];
+    
+      // Adds an element with a weight of 1.
+  void add(double x, double y);
+  // Adds an element with a specified weight.
+  void add(double x, double y, double weight);
+  // Adds a whole LLSQ.
+  void add(const LLSQ& other);
+  // Deletes an element with a weight of 1.
+  void remove(double x, double y);
+  int32_t count() const {  // no of elements
+    return static_cast<int>(total_weight + 0.5);
+  }
+    
+     private:
+  // Free allocated memory and clear pointers.
+  void Clear();
+  // Setup default values.
+  void Init();
+    
+      // Free up any currently unused dawgs.
+  void DeleteUnusedDawgs() {
+    dawgs_.DeleteUnusedObjects();
+  }
+    
+    
+    {    float posX = 0;
+    auto lastChild = Children->GetAt(Children->Size - 1);
+    float lastChildWidth = 0;
+    if (Children->Size > 2 && ShouldPrioritizeLastItem())
+    {
+        lastChildWidth = lastChild->DesiredSize.Width;
     }
-    
-    size_t MemoryPool::total_memory = 0;
-size_t MemoryPool::max_memory = 0;
-    
-    StringName _scs_create(const char *p_chr);
-    
-    #endif // MIDI_DRIVER_ALSAMIDI_H
-#endif // ALSAMIDI_ENABLED
+    for (auto item : Children)
+    {
+        auto widthAvailable = finalSize.Width - posX;
+        if (item != lastChild)
+        {
+            widthAvailable -= lastChildWidth;
+        }
+        float itemWidth = item->DesiredSize.Width;
+        if (widthAvailable > 0 && itemWidth <= widthAvailable)
+        {
+            // stack the items horizontally (left to right)
+            item->Arrange(Rect(posX, 0, itemWidth, finalSize.Height));
+            AutomationProperties::SetAccessibilityView(item, ::AccessibilityView::Content);
+            posX += item->RenderSize.Width;
+        }
+        else
+        {
+            // Not display the item
+            item->Arrange(Rect(0, 0, 0, 0));
+            AutomationProperties::SetAccessibilityView(item, ::AccessibilityView::Raw);
+        }
+    }
+    return finalSize;
+}
 
     
-    void MIDIDriverWinMidi::close() {
-    }
-    
-    public:
-	virtual Error open();
-	virtual void close();
-    
-    
-    {		Vector<Vector<int> > holes;
-	};
-    
-    godot_string GDAPI godot_string_path_to_file(const godot_string *p_self, const godot_string *p_path) {
-	const String *self = (const String *)p_self;
-	String *path = (String *)p_path;
-	godot_string result;
-	String return_value = self->path_to_file(*path);
-	memnew_placement(&result, String(return_value));
-    }
-    
-    Size HorizontalNoOverflowStackPanel::ArrangeOverride(Size finalSize)
+    class CCalcEngine
 {
-    if (Children->Size == 0)
+public:
+    CCalcEngine(
+        bool fPrecedence,
+        bool fIntegerMode,
+        CalculationManager::IResourceProvider* const pResourceProvider,
+        __in_opt ICalcDisplay* pCalcDisplay,
+        __in_opt std::shared_ptr<IHistoryDisplay> pHistoryDisplay);
+    void ProcessCommand(OpCode wID);
+    void DisplayError(uint32_t nError);
+    std::unique_ptr<CalcEngine::Rational> PersistedMemObject();
+    void PersistedMemObject(CalcEngine::Rational const& memObject);
+    bool FInErrorState()
     {
-        return finalSize;
+        return m_bError;
     }
+    bool FInRecordingState()
+    {
+        return m_bRecord;
+    }
+    void SettingsChanged();
+    bool IsCurrentTooBigForTrig();
+    int GetCurrentRadix();
+    std::wstring GetCurrentResultForRadix(uint32_t radix, int32_t precision);
+    void ChangePrecision(int32_t precision)
+    {
+        m_precision = precision;
+        ChangeConstants(m_radix, precision);
+    }
+    std::wstring GroupDigitsPerRadix(std::wstring_view numberString, uint32_t radix);
+    std::wstring GetStringForDisplay(CalcEngine::Rational const& rat, uint32_t radix);
+    void UpdateMaxIntDigits();
+    wchar_t DecimalSeparator() const;
     }
     
-        auto operandCommand = std::make_shared<COpndCommand>(commands, fNegative, fDecimal, fSciFmt);
-    operandCommand->Initialize(rat);
-    int iCommandEnd = AddCommand(operandCommand);
-    m_lastOpStartIndex = IchAddSzToEquationSz(numStr, iCommandEnd);
+    NarratorAnnouncement ^ CalculatorAnnouncement::GetMaxDigitsReachedAnnouncement(String ^ announcement)
+{
+    return ref new NarratorAnnouncement(
+        announcement, CalculatorActivityIds::MaxDigitsReached, AutomationNotificationKind::Other, AutomationNotificationProcessing::ImportantMostRecent);
+}
+    
+    /*!
+ * \brief Get string attribute from Booster.
+ * \param handle handle
+ * \param key The key of the attribute.
+ * \param out The result attribute, can be NULL if the attribute do not exist.
+ * \param success Whether the result is contained in out.
+ * \return 0 when success, -1 when failure happens
+ */
+XGB_DLL int XGBoosterGetAttr(BoosterHandle handle,
+                             const char* key,
+                             const char** out,
+                             int *success);
+/*!
+ * \brief Set or delete string attribute.
+ *
+ * \param handle handle
+ * \param key The key of the attribute.
+ * \param value The value to be saved.
+ *              If nullptr, the attribute would be deleted.
+ * \return 0 when success, -1 when failure happens
+ */
+XGB_DLL int XGBoosterSetAttr(BoosterHandle handle,
+                             const char* key,
+                             const char* value);
+/*!
+ * \brief Get the names of all attribute from Booster.
+ * \param handle handle
+ * \param out_len the argument to hold the output length
+ * \param out pointer to hold the output attribute stings
+ * \return 0 when success, -1 when failure happens
+ */
+XGB_DLL int XGBoosterGetAttrNames(BoosterHandle handle,
+                                  bst_ulong* out_len,
+                                  const char*** out);
+    
+    // Finally register the objective function.
+// After it succeeds you can try use xgboost with objective=mylogistic
+XGBOOST_REGISTER_OBJECTIVE(MyLogistic, 'mylogistic')
+.describe('User defined logistic regression plugin')
+.set_body([]() { return new MyLogistic(); });
+    
+    XGBOOST_REGISTER_SPARSE_PAGE_FORMAT(lz4hc)
+.describe('Apply LZ4 binary data compression(high compression ratio) for ext memory.')
+.set_body([]() {
+    return new SparsePageLZ4Format<bst_uint>(true);
+  });
+    
+      std::shared_ptr<HostDeviceVector<int>> ColSample(
+      std::shared_ptr<HostDeviceVector<int>> p_features, float colsample) {
+    if (colsample == 1.0f) return p_features;
+    const auto& features = p_features->HostVector();
+    CHECK_GT(features.size(), 0);
+    int n = std::max(1, static_cast<int>(colsample * features.size()));
+    auto p_new_features = std::make_shared<HostDeviceVector<int>>();
+    auto& new_features = *p_new_features;
+    new_features.Resize(features.size());
+    std::copy(features.begin(), features.end(),
+              new_features.HostVector().begin());
+    std::shuffle(new_features.HostVector().begin(),
+                 new_features.HostVector().end(), rng_);
+    new_features.Resize(n);
+    std::sort(new_features.HostVector().begin(),
+              new_features.HostVector().end());
+    }
+    
+      vals_in.clear(); ss.flush(); ss.clear(); ss.str('');
+  ss << '(3L,2L,1L)';
+  ss >> vals_in;
+  EXPECT_EQ(vals_in, vals);
+    
+        void UserPostProcessing::initializationOnThread()
+    {
+        try
+        {
+        }
+        catch (const std::exception& e)
+        {
+            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
+        }
+    }
+    
+        template<typename T>
+    void Rectangle<T>::recenter(const T newWidth, const T newHeight)
+    {
+        try
+        {
+            const auto centerPoint = center();
+            x = centerPoint.x - T(newWidth / 2.f);
+            y = centerPoint.y - T(newHeight / 2.f);
+            width = newWidth;
+            height = newHeight;
+        }
+        catch (const std::exception& e)
+        {
+            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
+        }
+    }
+    
+      if (m_affectedKeys.size() == 0) {
+    return returnResults();
+  }
+    
+      for (QString k : m_affectedKeys) {
+    auto future = m_connection->cmd(
+        {'EXPIRE', k.toUtf8(), ttl}, this, m_dbIndex,
+        [this](const RedisClient::Response&) {
+          QMutexLocker l(&m_processedKeysMutex);
+          m_progress++;
+          emit progress(m_progress);
+        },
+        [this, k](const QString& err) {
+          QMutexLocker l(&m_errorsMutex);
+          m_errors.append(
+              QCoreApplication::translate('RDM', 'Cannot set TTL for key ') +
+              QString('%1: %2').arg(k).arg(err));
+        });
+    }
+    
+    class TtlOperation : public AbstractOperation {
+  Q_OBJECT
+ public:
+  TtlOperation(QSharedPointer<RedisClient::Connection> connection, int dbIndex,
+               OperationCallback callback,
+               QRegExp keyPattern = QRegExp('*', Qt::CaseSensitive,
+                                            QRegExp::Wildcard));
+    }
+    
+    
+    {    return callback(filter, processed, e);
+  };
+    
+      virtual void run(QSharedPointer<RedisClient::Connection> targetConnection =
+                       QSharedPointer<RedisClient::Connection>(),
+                   int targetDbIndex = 0);
