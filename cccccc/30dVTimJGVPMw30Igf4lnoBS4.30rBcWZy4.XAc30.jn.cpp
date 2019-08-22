4ce38bafc4e3d68f3ff723aa21151fbfd00fffbc
SUNsung <weatherius@gmail.com>
2019-08-22 19:29:24 +0300

    Rewrite [30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp] File in: 2019-08-22 19:29:24

--- cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
+++ cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
@@ -1,158 +1,267 @@
 
         
-            // Create the pipes to which the ConPTY will connect
-    if (CreatePipe(&hPipePTYIn, phPipeOut, NULL, 0) &&
-        CreatePipe(phPipeIn, &hPipePTYOut, NULL, 0))
-    {
-        // Determine required size of Pseudo Console
-        COORD consoleSize{};
-        CONSOLE_SCREEN_BUFFER_INFO csbi{};
-        HANDLE hConsole{ GetStdHandle(STD_OUTPUT_HANDLE) };
-        if (GetConsoleScreenBufferInfo(hConsole, &csbi))
-        {
-            consoleSize.X = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-            consoleSize.Y = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-        }
+        namespace mate {
     }
     
-    	for (Map<ConstraintSW *, int>::Element *E = constraint_map.front(); E; E = E->next()) {
-    }
-    
-    #endif // CPU_PARTICLES_2D_EDITOR_PLUGIN_H
+    #endif  // NATIVE_MATE_NATIVE_MATE_PROMISE_H_
 
+    
+    napi_value Init(napi_env env, napi_value exports) {
+  napi_status status;
+  napi_property_descriptor descriptors[] = {
+      {'Print', NULL, Print, NULL, NULL, NULL, napi_default, NULL}};
+    }
+    
+    
+    {  live_xids_.erase(xid);
+}
+    
+    
+    {}  // namespace electron
+    
+    #endif  // SHELL_APP_UV_TASK_RUNNER_H_
+
+    
+    	// EPA
+	struct	EPA
+	{
+		/* Types		*/
+		typedef	GJK::sSV	sSV;
+		struct	sFace
+		{
+			Vector3	n;
+			real_t	d;
+			real_t	p;
+			sSV*		c[3];
+			sFace*		f[3];
+			sFace*		l[2];
+			U1			e[3];
+			U1			pass;
+		};
+		struct	sList
+		{
+			sFace*		root;
+			U			count;
+			sList() : root(0),count(0)	{}
+		};
+		struct	sHorizon
+		{
+			sFace*		cf;
+			sFace*		ff;
+			U			nf;
+			sHorizon() : cf(0),ff(0),nf(0)	{}
+		};
+		struct	eStatus { enum _ {
+			Valid,
+			Touching,
+			Degenerated,
+			NonConvex,
+			InvalidHull,
+			OutOfFaces,
+			OutOfVertices,
+			AccuraryReached,
+			FallBack,
+			Failed		};};
+			/* Fields		*/
+			eStatus::_		m_status;
+			GJK::sSimplex	m_result;
+			Vector3		m_normal;
+			real_t		m_depth;
+			sSV				m_sv_store[EPA_MAX_VERTICES];
+			sFace			m_fc_store[EPA_MAX_FACES];
+			U				m_nextsv;
+			sList			m_hull;
+			sList			m_stock;
+			/* Methods		*/
+			EPA()
+			{
+				Initialize();
+			}
+    }
+    
+    	enum {
+		MENU_LOAD_EMISSION_MASK,
+		MENU_CLEAR_EMISSION_MASK,
+		MENU_RESTART
+	};
+    
+    	PoolVector<Triangle> get_triangles() const { return triangles; }
+	PoolVector<Vector3> get_vertices() const { return vertices; }
+	void get_indices(PoolVector<int> *r_triangles_indices) const;
+    
+    void InputEventScreenDrag::set_index(int p_index) {
+    }
+    
+    	virtual RID material_create();
+    
+    		GLint loc = glGetUniformLocation(v.id, texunit_pairs[i].name);
+		if (loc >= 0) {
+			if (texunit_pairs[i].index < 0) {
+				glUniform1i(loc, max_image_units + texunit_pairs[i].index); //negative, goes down
+			} else {
+    }
+    }
+    
+    	struct Preview {
+		Ref<AudioStreamPreview> preview;
+		Ref<AudioStream> base_stream;
+		Ref<AudioStreamPlayback> playback;
+		volatile bool generating;
+		ObjectID id;
+		Thread *thread;
+	};
+    
+    		const Variant &key = keys[i];
+		PropertyInfo pi(key.get_type(), index + ': key');
+		p_list->push_back(pi);
+    
+    EditorDirDialog::EditorDirDialog() {
+    }
+    
+    public:
+	enum {
+		NOTIFICATION_EDITOR_SETTINGS_CHANGED = 10000
+	};
+    
+    class TestServiceImpl : public ::grpc::testing::EchoTestService::Service {
+ public:
+  TestServiceImpl() : signal_client_(false), host_() {}
+  explicit TestServiceImpl(const grpc::string& host)
+      : signal_client_(false), host_(new grpc::string(host)) {}
+    }
+    
+      ac = (async_connect*)gpr_malloc(sizeof(async_connect));
+  ac->on_done = on_done;
+  ac->socket = socket;
+  gpr_mu_init(&ac->mu);
+  ac->refs = 2;
+  ac->addr_name = grpc_sockaddr_to_uri(addr);
+  ac->endpoint = endpoint;
+  ac->channel_args = grpc_channel_args_copy(channel_args);
+  GRPC_CLOSURE_INIT(&ac->on_connect, on_connect, ac, grpc_schedule_on_exec_ctx);
+    
+    static void expect_dump(const char* buf, size_t len, uint32_t flags,
+                        const char* result) {
+  char* got = gpr_dump(buf, len, flags);
+  GPR_ASSERT(0 == strcmp(got, result));
+  gpr_free(got);
+}
+    
+    namespace experimental {
+    }
+    
+    
+    {}  // namespace
+    
+      srv_stream.Write(send_response_buffer_, tag(5));
+  cli_stream->Read(&recv_response_, tag(6));
+  Verifier().Expect(5, true).Expect(6, true).Verify(cq_.get());
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+    
+      void TearDown() override {
+    auto plugin = CheckPresent();
+    EXPECT_TRUE(plugin);
+    EXPECT_TRUE(plugin->init_server_is_called());
+    EXPECT_TRUE(plugin->finish_is_called());
+    server_->Shutdown();
+    cq_->Shutdown();
+    cq_thread_->join();
+    delete cq_thread_;
+  }
+    
+        int m_nTempCom;                          /* Holding place for the last command.          */
+    size_t m_openParenCount;                 // Number of open parentheses.
+    std::array<int, MAXPRECDEPTH> m_nOp;     /* Holding array for parenthesis operations.    */
+    std::array<int, MAXPRECDEPTH> m_nPrecOp; /* Holding array for precedence  operations.    */
+    size_t m_precedenceOpCount;              /* Current number of precedence ops in holding. */
+    int m_nLastCom;                          // Last command entered.
+    ANGLE_TYPE m_angletype;                  // Current Angle type when in dec mode. one of deg, rad or grad
+    NUM_WIDTH m_numwidth;                    // one of qword, dword, word or byte mode.
+    int32_t m_dwWordBitWidth;                // # of bits in currently selected word size
+    
+    
+    {    m_historyItems.push_back(spHistoryItem);
+    unsigned int lastIndex = static_cast<unsigned>(m_historyItems.size() - 1);
+    return lastIndex;
+}
+    
+    
+    {    return false;
+}
+    
+        ComputationNodePtr Crop(const ComputationNodePtr input1, const ComputationNodePtr input2, const std::wstring nodeName = L'');
+    ComputationNodePtr Crop(const ComputationNodePtr input1, const ComputationNodePtr input2, size_t offsetX, size_t offsetY, const std::wstring nodeName = L'');
+    ComputationNodePtr Crop(const ComputationNodePtr input1, const ComputationNodePtr input2, const ComputationNodePtr eqNode1, const ComputationNodePtr eqNode2, const std::wstring nodeName = L'');
+    
+            bool wasDataRead = trainSetDataReader.GetMinibatch(inputMatrices); // fill in the minibatch data into the Input nodes' buffers directly
+        // If this returns false, the matrices may contain garbage or not sized to 0 columns.
+        // On the other hand, if it returns a 0-column matrix, that would be a perfectly cromulent minibatch (in case of data parallelism with distributed reading).
+        // If a passed matrix does not match a reader section, that is an error.
+    
+    
+    {            return retval;
+        }
+        // borrow DownCast function from ComputationNetwork
+        ComputationNodePtr DownCast(ComputationNodeBasePtr inode)
+        {
+            ComputationNodePtr node = dynamic_pointer_cast<ComputationNode<ElemType>>(inode);
+            if (!node)
+                InvalidArgument('an ComputationNodeBasePtr of mismatching precision was passed');
+            return node;
+        }
     
     protected:
-	static void _bind_methods();
     
-    #include 'core/math/face3.h'
-#include 'core/reference.h'
-    
-    InputEventMouseMotion::InputEventMouseMotion() {
-}
-    
-    #endif // AUDIO_STREAM_PREVIEW_H
-
-    
-    
-    {	if (err != OK) {
-		mkdirerr->popup_centered_minsize(Size2(250, 80) * EDSCALE);
-	} else {
-		opened_paths.insert(dir);
-		//reload(dir.plus_file(makedirname->get_text()));
-		EditorFileSystem::get_singleton()->scan_changes(); //we created a dir, so rescan changes
-	}
-	makedirname->set_text(''); // reset label
-}
-    
-    #include 'scene/gui/box_container.h'
-#include 'scene/gui/menu_button.h'
-    
-      void RunServerInterceptors() {
-    auto* rpc_info = call_->server_rpc_info();
-    if (!reverse_) {
-      current_interceptor_index_ = 0;
-    } else {
-      current_interceptor_index_ = rpc_info->interceptors_.size() - 1;
+            // Initiate aggregation only if any samples were processed in previous iteration
+        if (resetState || (headerCPU->numSamples != 0))
+        {
+            int deviceId = gradients[0]->GetDeviceId();
+            DistGradHeader* newGradHeader = m_bufferedGradHeader;
+            MatrixComputeStreamEvent* mainStreamSyncEvent = MatrixComputeStreamEvent::Create(deviceId);
     }
-    rpc_info->RunInterceptor(this, current_interceptor_index_);
-  }
     
-    void ChannelArguments::SetLoadBalancingPolicyName(
-    const grpc::string& lb_policy_name) {
-  SetString(GRPC_ARG_LB_POLICY_NAME, lb_policy_name);
+    
+    {  gps_stamp = gps_stamp * 1000;
+  return gps_stamp;
 }
     
-    #endif
-
-    
-    static void iomgr_platform_shutdown_background_closure(void) {}
-    
-    
-    {  padded = gpr_leftpad('foo', '0', 5);
-  GPR_ASSERT(0 == strcmp('00foo', padded));
-  gpr_free(padded);
+    TEST_F(GraphSegmentorTest, test_segment_graph) {
+  {
+    GraphSegmentor segmentor;
+    segmentor.Init(5.0);
+    segmentor.SegmentGraph(num_vertices_, num_edges_, nullptr, false);
+    EXPECT_EQ(0, segmentor.universe().GetSetsNum());
+    segmentor.SegmentGraph(num_vertices_, num_edges_, edges_, true);
+    EXPECT_EQ(3, segmentor.universe().GetSetsNum());
+  }
+  {
+    GraphSegmentor segmentor;
+    segmentor.Init(6.0);
+    segmentor.SegmentGraph(num_vertices_, num_edges_, edges_);
+    EXPECT_EQ(1, segmentor.universe().GetSetsNum());
+  }
+  {
+    GraphSegmentor segmentor;
+    segmentor.Init(2.0);
+    segmentor.SegmentGraph(num_vertices_, num_edges_, edges_);
+    EXPECT_EQ(4, segmentor.universe().GetSetsNum());
+  }
 }
     
-    static grpc::internal::GrpcLibraryInitializer g_gli_initializer;
-ChannelCredentials::ChannelCredentials() { g_gli_initializer.summon(); }
+    /**
+ * @file curve_math.cc
+ **/
     
-    #include <grpcpp/security/credentials.h>
+    #include 'modules/planning/traffic_rules/traffic_rule.h'
     
-      request.set_message('Hello');
-  EXPECT_TRUE(stream->Write(request));
-  EXPECT_TRUE(stream->Read(&response));
-  EXPECT_EQ(response.message(), request.message());
-    
-    #include <stdio.h>
-#include <stdlib.h>
-#include 'PSTokenizer.h'
-    
-    #ifdef USE_GCC_PRAGMAS
-#pragma interface
-#endif
-    
-      // get alignment
-  if (dict->lookup('Dm', &obj)->isName()) {
-    const char *dm = obj.getName();
-    
-    if (strcmp('H', dm) == 0)
-      alignment = transitionHorizontal;
-    else if (strcmp('V', dm) == 0)
-      alignment = transitionVertical;
+      // 2. If vehicle is not on current reference line yet, no rerouting
+  if (!segments.IsOnSegment()) {
+    return true;
   }
-  obj.free();
     
-    void Parser::shift(int objNum) {
-  if (inlineImg > 0) {
-    if (inlineImg < 2) {
-      ++inlineImg;
-    } else {
-      // in a damaged content stream, if 'ID' shows up in the middle
-      // of a dictionary, we need to reset
-      inlineImg = 0;
-    }
-  } else if (buf2.isCmd('ID')) {
-    lexer->skipChar();		// skip char after 'ID' command
-    inlineImg = 1;
-  }
-  buf1.free();
-  buf2.shallowCopy(&buf1);
-  if (inlineImg > 0)		// don't buffer inline image data
-    buf2.initNull();
-  else
-    lexer->getObj(&buf2, objNum);
+    
+    {  EXPECT_EQ(expect, real);
 }
-
     
-    #include <stdlib.h>
-#include <stddef.h>
-#include 'ProfileData.h'
-    
-      MediaParameters* getMHParameters() { return &MH; }
-  MediaParameters* getBEParameters() { return &BE; }
-    
-      newsound->kind = kind;
-  if (fileName) {
-    newsound->fileName = fileName->copy();
-  }
-  newsound->samplingRate = samplingRate;
-  newsound->channels = channels;
-  newsound->bitsPerSample = bitsPerSample;
-  newsound->encoding = encoding;
-    
-      //----- update graphics state
-  virtual void updateAll(GfxState *state);
-  virtual void updateCTM(GfxState *state, double m11, double m12,
-			 double m21, double m22, double m31, double m32);
-  virtual void updateLineDash(GfxState *state);
-  virtual void updateFlatness(GfxState *state);
-  virtual void updateLineJoin(GfxState *state);
-  virtual void updateLineCap(GfxState *state);
-  virtual void updateMiterLimit(GfxState *state);
-  virtual void updateLineWidth(GfxState *state);
-  virtual void updateStrokeAdjust(GfxState *state);
-  virtual void updateFillColor(GfxState *state);
-  virtual void updateStrokeColor(GfxState *state);
-  virtual void updateBlendMode(GfxState *state);
-  virtual void updateFillOpacity(GfxState *state);
-  virtual void updateStrokeOpacity(GfxState *state);
+      void CreateProcessor();
+  bool NotifyProcessor(uint64_t crid) override;
