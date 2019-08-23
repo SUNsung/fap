906d83ad717e7a84f9deedd49e8bf66b5873ffc1
SUNsung <weatherius@gmail.com>
2019-08-23 18:31:43 +0300

    Rewrite [13cpAayw8vE613UvzFPhZ9tpM13TXo3C0GRxao13bYI.rb] File in: 2019-08-23 18:31:43

--- ruby/13cpAayw8vE613UvzFPhZ9tpM13TXo3C0GRxao13bYI.rb
+++ ruby/13cpAayw8vE613UvzFPhZ9tpM13TXo3C0GRxao13bYI.rb
@@ -1,156 +1,205 @@
 
         
-        WITH_LIQUID = <<-LIQUID.freeze
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
-pharetra tempus. {{ author }} et metus fermentum, eu cursus lorem
-mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
+                @version_value = '#{major}.#{minor}.#{patch}.#{new_appendix}'
+      end
     
-    def graceful_require
-  Jekyll::External.require_with_graceful_fail('json')
-  JSON.pretty_generate(DATA)
+        def run(path, color = nil)
+      unless color
+        color = Frameit::Color::BLACK
+        color = Frameit::Color::SILVER if Frameit.config[:white] || Frameit.config[:silver]
+        color = Frameit::Color::GOLD if Frameit.config[:gold]
+        color = Frameit::Color::ROSE_GOLD if Frameit.config[:rose_gold]
+      end
+    
+              disabled_services_object = self.service_object
+          disabled_services.each do |k, v|
+            disabled_services_object.__hash__[k] = true
+            disabled_services_object.send('#{k}=', v)
+          end
+          Produce::Service.disable(disabled_services_object, nil) unless disabled_services.empty?
+        end
+      end
+    
+          it 'inspect works' do
+        TunesStubbing.itc_stub_app_uninclude_future_territories
+    
+      context 'authenticated' do
+    before { subject.login(username, password) }
+    describe '#teams' do
+      let(:teams) { subject.teams }
+      it 'returns the list of available teams' do
+        expect(teams).to be_instance_of(Array)
+        expect(teams.first.keys).to eq(['status', 'teamId', 'type', 'extendedTeamAttributes', 'teamAgent', 'memberships', 'currentTeamMember', 'name'])
+      end
+    end
+    
+          def pipe(device_type, language, locale)
+        not_implemented(__method__)
+      end
+    
+          def self.details
+        [
+          'This action will return the current build number set on your project.',
+          'You first have to set up your Xcode project, if you haven't done it already: [https://developer.apple.com/library/ios/qa/qa1827/_index.html](https://developer.apple.com/library/ios/qa/qa1827/_index.html).'
+        ].join('\n')
+      end
+    
+          def self.deprecated_notes
+        [
+          'You are using legacy `shenzhen` to build your app, which will be removed soon!',
+          'It is recommended to upgrade to _gym_.',
+          'To do so, just replace `ipa(...)` with `gym(...)` in your Fastfile.',
+          'To make code signing work, follow [https://docs.fastlane.tools/codesigning/xcode-project/](https://docs.fastlane.tools/codesigning/xcode-project/).'
+        ].join('\n')
+      end
+    end
+  end
 end
+# rubocop:enable Lint/AssignmentInCondition
+
     
-      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
-    cmd = g.split('-').last
-    p.command(cmd.to_sym) do |c|
-      c.syntax cmd
-      c.action do
-        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
-          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
+          def_node_matcher :extract_shared_values_key?, <<-PATTERN
+        (const (const nil? :SharedValues) $_)
+      PATTERN
+    
+            it 'passes when `apple_id` is correct' do
+          options = {
+            username: 'username@example.com',
+            apple_id: '123456789'
+          }
+          pilot_config = FastlaneCore::Configuration.create(Pilot::Options.available_options, options)
+          expect(pilot_config[:apple_id]).to eq('123456789')
+        end
       end
-    end
-  end
-    
-          def initialize(runtime, path_or_io, options)
-        @runtime = runtime
-        @snippets_input = []
-        @io = ensure_io(path_or_io)
-        @prefixes = options[:prefixes] || {}
-        @delayed_messages = []
-        @options = options
-        @exceptions = []
-        @indent = 0
-        @timings = {}
-      end
-    
-                  EM.start_server(
-                opts['host'],
-                opts['livereload_port'],
-                HttpAwareConnection,
-                opts
-              ) do |ws|
-                handle_websockets_event(ws)
-              end
-    
-        def process(args)
-      arg_is_present? args, '--server', 'The --server command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
-                          use the 'build' subcommand.'
-      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
-                          '--watch'.'
-      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
-                          the '--watch' switch.'
-      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
-                          favour of 'highlighter'.'
-      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
-                          your config files.'
-      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
-                          config files.'
-      no_subcommand(args)
-    end
-    
-        def filter_const(name)
-      if name.is_a? Array
-        name.map &method(:filter_const)
-      else
-        Docs.const_get '#{name}_filter'.camelize
-      end
-    end
-    
-    module Docs
-  class PageDb
-    attr_reader :pages
-    
-    require 'active_support/subscriber'
-    
-      def verify_payload?
-    payload.present? && VerifySalmonService.new.call(payload)
-  end
-    
-      def encoded_challenge
-    HTMLEntities.new.encode(params['hub.challenge'])
-  end
-    
-    module Localized
-  extend ActiveSupport::Concern
-    
-      def rate_limit_limit
-    api_throttle_data[:limit].to_s
-  end
-    
-      # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-    
-    module Workers
-  class PublishToHub < Base
-    def perform(*_args)
-      # don't publish to pubsubhubbub in cucumber
-    end
-  end
-    
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
     end
   end
 end
 
     
-          it 'should not create the participation' do
-        post :create, params: {post_id: @post.id}
-        expect(alice.participations.where(:target_id => @post.id)).not_to exist
-        expect(response.code).to eq('403')
+        it 'When dep ensure is used without `-vendor-only`' do
+      expect_offense(<<~RUBY)
+        class Foo < Formula
+          url 'https://brew.sh/foo-1.0.tgz'
+          homepage 'https://brew.sh'
+    
+          expect(f.class.stable.deps.first.name).to eq('foo')
+      expect(f.class.devel.deps.first.name).to eq('foo')
+      expect(f.class.head.deps.first.name).to eq('foo')
+    end
+    
+        it 'works with tags' do
+      spec.uses_from_macos('foo' => :head, :after => :el_capitan)
+    
+          def separate_header_package?
+        version >= '10'
       end
-    end
-  end
     
-        it 'redirects to / for a mobile user' do
-      request.headers['X_MOBILE_DEVICE'] = true
-      delete :destroy
-      expect(response).to redirect_to root_path
-    end
-  end
+        context 'with referer set' do
+      let(:specs) { { referer: 'https://somehost/also' } }
     
-              expect(gon['preloads'][:mentioned_person][:name]).to eq(alice.person.name)
-          expect(gon['preloads'][:mentioned_person][:handle]).to eq(alice.person.diaspora_handle)
-        end
+      apply_simple_captcha :message => I18n.t('simple_captcha.message.failed'), :add_to_base => true
+    
+    if rails_env == 'production'
+  config('without test:development')
+elsif rails_env == 'test'
+  config('without development')
+end
+    
+          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
+      def create
+        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
+        client = Api::OpenidConnect::OAuthApplication.register! registrar
+        render json: client.as_json(root: false)
       end
+    
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
+    
+      def create
+    # Contacts autocomplete does not work the same way on mobile and desktop
+    # Mobile returns contact ids array while desktop returns person id
+    # This will have to be removed when mobile autocomplete is ported to Typeahead
+    recipients_param, column = [%i(contact_ids id), %i(person_ids person_id)].find {|param, _| params[param].present? }
+    if recipients_param
+      person_ids = current_user.contacts.mutual.where(column => params[recipients_param].split(',')).pluck(:person_id)
     end
     
-          Dir.chdir(file_path) do
-        contents = file.read
-        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
-          contents = $1.lstrip
-        end
-        contents = pre_filter(contents)
-        if @raw
-          contents
-        else
-          partial = Liquid::Template.parse(contents)
-          context.stack do
-            partial.render(context)
+            if deprecated_pods.any?
+          UI.section 'The following pods are deprecated:' do
+            deprecated_pods.each do |spec|
+              if spec.deprecated_in_favor_of
+                UI.puts '- #{spec.name}' \
+                  ' (in favor of #{spec.deprecated_in_favor_of})'
+              else
+                UI.puts '- #{spec.name}'
+              end
+            end
           end
         end
       end
+    
+        export LANG=en_US.UTF-8
+    DOC
+  else
+    STDERR.puts <<-DOC
+    \e[33mWARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
+    Consider adding the following to ~/.profile:
+    
+        # Sets up a lighweight master repo in `tmp/cocoapods/master` with the
+    # contents of `spec/fixtures/spec-repos/test_repo`.
+    #
+    def set_up_old_test_repo
+      require 'fileutils'
+      test_old_repo_path.mkpath
+      origin = ROOT + 'spec/fixtures/spec-repos/test_repo/.'
+      destination = tmp_repos_path + '../master'
+      FileUtils.cp_r(origin, destination)
+      FileUtils.rm_r(destination + './.git')
+      repo_make('../master')
     end
-  end
-end
+    
+              let(:expected_plugin_args) do
+            {
+                'key' => '%{host}',
+                'filter_options' => {
+                    'string'  => 'string',
+                    'integer' => 3,
+                    'nested'  => { # <-- This is nested hash!
+                        'string'  => 'nested-string',
+                        'integer' => 7,
+                        'quoted-key-string' => 'nested-quoted-key-string',
+                        'quoted-key-integer' => 31,
+                        'deep'    => { # <-- This is deeper nested hash!
+                            'string'  => 'deeply-nested-string',
+                            'integer' => 127,
+                            'quoted-key-string' => 'deeply-nested-quoted-key-string',
+                            'quoted-key-integer' => 8191
+                        }
+                    }
+                },
+                'ttl' => 5
+            }
+          end
+    
+    require 'clamp'
+require 'pluginmanager/util'
+require 'pluginmanager/gemfile'
+require 'pluginmanager/install'
+require 'pluginmanager/remove'
+require 'pluginmanager/list'
+require 'pluginmanager/update'
+require 'pluginmanager/pack'
+require 'pluginmanager/unpack'
+require 'pluginmanager/generate'
+require 'pluginmanager/prepare_offline_pack'
+require 'pluginmanager/proxy_support'
+configure_proxy
+    
+            PluginManager.ui.debug('Looking if package named: #{plugin_name} exists at #{uri}')
