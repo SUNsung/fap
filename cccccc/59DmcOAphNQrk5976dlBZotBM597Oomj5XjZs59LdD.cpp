3dcb722710cff92433058498eae9d7d8a1cbce90
SUNsung <weatherius@gmail.com>
2019-08-23 19:40:40 +0300

    Rewrite [59DmcOAphNQrk5976dlBZotBM597Oomj5XjZs59LdD.cpp] File in: 2019-08-23 19:40:39

--- cccccc/59DmcOAphNQrk5976dlBZotBM597Oomj5XjZs59LdD.cpp
+++ cccccc/59DmcOAphNQrk5976dlBZotBM597Oomj5XjZs59LdD.cpp
@@ -1,3 +1,343 @@
 
         
-        
+        template <typename Dtype>
+void SGDSolver<Dtype>::SnapshotSolverState(const string& model_filename) {
+  switch (this->param_.snapshot_format()) {
+    case caffe::SolverParameter_SnapshotFormat_BINARYPROTO:
+      SnapshotSolverStateToBinaryProto(model_filename);
+      break;
+    case caffe::SolverParameter_SnapshotFormat_HDF5:
+      SnapshotSolverStateToHDF5(model_filename);
+      break;
+    default:
+      LOG(FATAL) << 'Unsupported snapshot format.';
+  }
+}
+    
+      virtual ~HDF5OutputLayerTest() {
+    delete blob_data_;
+    delete blob_label_;
+  }
+    
+    template <typename Dtype>
+void ClipLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down,
+    const vector<Blob<Dtype>*>& bottom) {
+  if (propagate_down[0]) {
+    const Dtype* bottom_data = bottom[0]->cpu_data();
+    const Dtype* top_diff = top[0]->cpu_diff();
+    Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+    const int count = bottom[0]->count();
+    }
+    }
+    
+    TYPED_TEST(NeuronLayerTest, TestTanHGradient) {
+  typedef typename TypeParam::Dtype Dtype;
+  LayerParameter layer_param;
+  TanHLayer<Dtype> layer(layer_param);
+  GradientChecker<Dtype> checker(1e-2, 1e-3);
+  checker.CheckGradientEltwise(&layer, this->blob_bottom_vec_,
+      this->blob_top_vec_);
+}
+    
+      /**
+   * @brief Reshape all layers from bottom to top.
+   *
+   * This is useful to propagate changes to layer sizes without running
+   * a forward pass, e.g. to compute output feature size.
+   */
+  void Reshape();
+    
+    template <typename Dtype>
+void Net<Dtype>::CopyTrainedLayersFrom(const NetParameter& param) {
+  int num_source_layers = param.layer_size();
+  for (int i = 0; i < num_source_layers; ++i) {
+    const LayerParameter& source_layer = param.layer(i);
+    const string& source_layer_name = source_layer.name();
+    int target_layer_id = 0;
+    while (target_layer_id != layer_names_.size() &&
+        layer_names_[target_layer_id] != source_layer_name) {
+      ++target_layer_id;
+    }
+    if (target_layer_id == layer_names_.size()) {
+      LOG(INFO) << 'Ignoring source layer ' << source_layer_name;
+      continue;
+    }
+    DLOG(INFO) << 'Copying source layer ' << source_layer_name;
+    vector<shared_ptr<Blob<Dtype> > >& target_blobs =
+        layers_[target_layer_id]->blobs();
+    CHECK_EQ(target_blobs.size(), source_layer.blobs_size())
+        << 'Incompatible number of blobs for layer ' << source_layer_name;
+    for (int j = 0; j < target_blobs.size(); ++j) {
+      if (!target_blobs[j]->ShapeEquals(source_layer.blobs(j))) {
+        Blob<Dtype> source_blob;
+        const bool kReshape = true;
+        source_blob.FromProto(source_layer.blobs(j), kReshape);
+        LOG(FATAL) << 'Cannot copy param ' << j << ' weights from layer ''
+            << source_layer_name << ''; shape mismatch.  Source param shape is '
+            << source_blob.shape_string() << '; target param shape is '
+            << target_blobs[j]->shape_string() << '. '
+            << 'To learn this layer's parameters from scratch rather than '
+            << 'copying from a saved net, rename the layer.';
+      }
+      const bool kReshape = false;
+      target_blobs[j]->FromProto(source_layer.blobs(j), kReshape);
+    }
+  }
+}
+    
+      CHECK_EQ(net_->num_inputs(), 1) << 'Network should have exactly one input.';
+  CHECK_EQ(net_->num_outputs(), 1) << 'Network should have exactly one output.';
+    
+    protected:
+	float cell_size;
+	float cell_height;
+	float agent_height;
+	float agent_radius;
+	float agent_max_climb;
+	float agent_max_slope;
+	float region_min_size;
+	float region_merge_size;
+	float edge_max_length;
+	float edge_max_error;
+	float verts_per_poly;
+	float detail_sample_distance;
+	float detail_sample_max_error;
+    
+    	virtual bool eof_reached() const;
+    
+    void MIDIDriverALSAMidi::unlock() const {
+    }
+    
+    	int sources = MIDIGetNumberOfSources();
+	for (int i = 0; i < sources; i++) {
+    }
+    
+    godot_pool_color_array_write_access GDAPI *godot_pool_color_array_write(godot_pool_color_array *p_self) {
+	PoolVector<Color> *self = (PoolVector<Color> *)p_self;
+	return (godot_pool_color_array_write_access *)memnew(PoolVector<Color>::Write(self->write()));
+}
+    
+    #include 'upnp.h'
+#include 'upnp_device.h'
+    
+    
+    {	OS::PowerState get_power_state();
+	int get_power_seconds_left();
+	int get_power_percent_left();
+};
+    
+    	// Check for machine-specific then user config source file.
+	PathCombine(cpuCfgPath, userConfigDirPath, L'ConEmu-%COMPUTERNAME%.xml');
+	ExpandEnvironmentStrings(cpuCfgPath, cpuCfgPath, sizeof(cpuCfgPath) / sizeof(cpuCfgPath[0]));
+    
+    void Page::display(OutputDev *out, double hDPI, double vDPI,
+		   int rotate, GBool useMediaBox, GBool crop,
+		   GBool printing, Catalog *catalog,
+		   GBool (*abortCheckCbk)(void *data),
+		   void *abortCheckCbkData,
+                   GBool (*annotDisplayDecideCbk)(Annot *annot, void *user_data),
+                   void *annotDisplayDecideCbkData) {
+  displaySlice(out, hDPI, vDPI, rotate, useMediaBox, crop, -1, -1, -1, -1, printing, catalog,
+	       abortCheckCbk, abortCheckCbkData,
+               annotDisplayDecideCbk, annotDisplayDecideCbkData);
+}
+    
+    
+    {    switch (interval->style) {
+    case Interval::Arabic:
+      number = strtol(str + prefixLength, &end, 10);
+      if (*end == '\0' && number - interval->first < interval->length) {
+	*index = base + number - interval->first;
+	return gTrue;
+      }
+      break;
+    case Interval::LowercaseRoman:
+    case Interval::UppercaseRoman:
+      number = fromRoman(str + prefixLength);
+      if (number >= 0 && number - interval->first < interval->length) {
+	*index = base + number - interval->first;
+	return gTrue;
+      }
+      break;
+    case Interval::UppercaseLatin:
+    case Interval::LowercaseLatin:
+      number = fromLatin(str + prefixLength);
+      if (number >= 0 && number - interval->first < interval->length) {
+	*index = base + number - interval->first;
+	return gTrue;
+      }
+      break;
+    case Interval::None:
+      break;
+    }
+  }
+    
+    //------------------------------------------------------------------------
+// PageTransition
+//------------------------------------------------------------------------
+    
+    void PreScanOutputDev::stroke(GfxState *state) {
+  double *dash;
+  int dashLen;
+  double dashStart;
+    }
+    
+    #endif
+
+    
+    
+    {  if (readAttrs)
+  {
+    Object tmp;
+    Dict *dict = streamObj->getStream()->getDict();
+    dict->lookup('F', &tmp);
+    if (!tmp.isNull()) {
+      Object obj1;
+      // valid 'F' key -> external file
+      kind = soundExternal;
+      if (getFileSpecNameForPlatform (&tmp, &obj1)) {
+        fileName = obj1.getString()->copy();
+        obj1.free();
+      }
+    } else {
+      // no file specification, then the sound data have to be
+      // extracted from the stream
+      kind = soundEmbedded;
+    }
+    tmp.free();
+    // sampling rate
+    dict->lookup('R', &tmp);
+    if (tmp.isNum()) {
+      samplingRate = tmp.getNum();
+    }
+    tmp.free();
+    // sound channels
+    dict->lookup('C', &tmp);
+    if (tmp.isInt()) {
+      channels = tmp.getInt();
+    }
+    tmp.free();
+    // bits per sample
+    dict->lookup('B', &tmp);
+    if (tmp.isInt()) {
+      bitsPerSample = tmp.getInt();
+    }
+    tmp.free();
+    // encoding format
+    dict->lookup('E', &tmp);
+    if (tmp.isName())
+    {
+      const char *enc = tmp.getName();
+      if (strcmp('Raw', enc) == 0) {
+        encoding = soundRaw;
+      } else if (strcmp('Signed', enc) == 0) {
+        encoding = soundSigned;
+      } else if (strcmp('muLaw', enc) == 0) {
+        encoding = soundMuLaw;
+      } else if (strcmp('ALaw', enc) == 0) {
+        encoding = soundALaw;
+      }
+    }
+    tmp.free();
+  }
+}
+    
+    //------------------------------------------------------------------------
+    
+        /**
+     * If the last oplog timestamp is being tracked for this PlanExecutor, return it.
+     * Otherwise return a null timestamp.
+     */
+    virtual Timestamp getLatestOplogTimestamp() const = 0;
+    
+        Top() = default;
+    
+    TEST_F(ExtensionsCallbackRealTest, TextBasic) {
+    ASSERT_OK(dbtests::createIndex(&_opCtx,
+                                   _nss.ns(),
+                                   BSON('a'
+                                        << 'text'),
+                                   false));  // isUnique
+    }
+    
+    //
+// Check that cursor returns no results if there are no docs within interval
+// and the first key located during initialization is above upper bound
+//
+class QueryStageCountScanNothingInIntervalFirstMatchTooHigh : public CountBase {
+public:
+    void run() {
+        dbtests::WriteContextForTests ctx(&_opCtx, ns());
+    }
+    }
+    
+    void RemoveTransliterator::handleTransliterate(Replaceable& text, UTransPosition& index,
+                                               UBool /*isIncremental*/) const {
+    // Our caller (filteredTransliterate) has already narrowed us
+    // to an unfiltered run.  Delete it.
+    UnicodeString empty;
+    text.handleReplaceBetween(index.start, index.limit, empty);
+    int32_t len = index.limit - index.start;
+    index.contextLimit -= len;
+    index.limit -= len;
+}
+U_NAMESPACE_END
+    
+    SearchIterator::SearchIterator(CharacterIterator &text, 
+                               BreakIterator     *breakiter) :
+                               m_breakiterator_(breakiter)
+{
+    m_search_                     = (USearch *)uprv_malloc(sizeof(USearch));
+    m_search_->breakIter          = NULL;
+    m_search_->isOverlap          = FALSE;
+    m_search_->isCanonicalMatch   = FALSE;
+    m_search_->elementComparisonType = 0;
+    m_search_->isForwardSearching = TRUE;
+    m_search_->reset              = TRUE;
+    m_search_->matchedIndex       = USEARCH_DONE;
+    m_search_->matchedLength      = 0;
+    text.getText(m_text_);
+    m_search_->text               = m_text_.getBuffer();
+    m_search_->textLength         = m_text_.length();
+    m_breakiterator_             = breakiter;
+}
+    
+      // Load the options file.
+  DBOptions loaded_db_opt;
+  std::vector<ColumnFamilyDescriptor> loaded_cf_descs;
+  s = LoadLatestOptions(kDBPath, Env::Default(), &loaded_db_opt,
+                        &loaded_cf_descs);
+  assert(s.ok());
+  assert(loaded_db_opt.create_if_missing == db_opt.create_if_missing);
+    
+      txn->Rollback();
+    
+    // Move all L0 files to target_level skipping compaction.
+// This operation succeeds only if the files in L0 have disjoint ranges; this
+// is guaranteed to happen, for instance, if keys are inserted in sorted
+// order. Furthermore, all levels between 1 and target_level must be empty.
+// If any of the above condition is violated, InvalidArgument will be
+// returned.
+Status PromoteL0(DB* db, ColumnFamilyHandle* column_family,
+                 int target_level = 1);
+    
+    namespace rocksdb {
+namespace lua {
+class LuaStateWrapper {
+ public:
+  explicit LuaStateWrapper(const std::string& lua_script) {
+    lua_state_ = luaL_newstate();
+    Init(lua_script, {});
+  }
+  LuaStateWrapper(
+      const std::string& lua_script,
+      const std::vector<std::shared_ptr<RocksLuaCustomLibrary>>& libraries) {
+    lua_state_ = luaL_newstate();
+    Init(lua_script, libraries);
+  }
+  lua_State* GetLuaState() const { return lua_state_; }
+  ~LuaStateWrapper() { lua_close(lua_state_); }
+    }
+    }
+    }
