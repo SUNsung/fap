bf55151be2fc6b65ae351251870dc548730bc893
SUNsung <weatherius@gmail.com>
2019-08-23 08:37:32 +0300

    Rewrite [038JQgPJY28pY03CyJa4cE3XNU03iOJbs43xopQ038XY.py] File in: 2019-08-23 08:37:32

--- pyton/038JQgPJY28pY03CyJa4cE3XNU03iOJbs43xopQ038XY.py
+++ pyton/038JQgPJY28pY03CyJa4cE3XNU03iOJbs43xopQ038XY.py
@@ -1,228 +1,73 @@
 
         
-        This typically means that you attempted to use functionality that needed
-an active HTTP request.  Consult the documentation on testing for
-information about how to avoid this problem.\
-'''
-_app_ctx_err_msg = '''\
-Working outside of application context.
+            return render_template('blog/create.html')
     
-        @app.teardown_appcontext
-    def cleanup(exception):
-        cleanup_stuff.append(exception)
+    from flaskr import create_app
+from flaskr.db import get_db
+from flaskr.db import init_db
     
-        def test_tabular_model_form_meta_readonly_field(self):
-        '''
-        Tabular inlines use ModelForm.Meta.help_texts and labels for read-only
-        fields.
-        '''
-        response = self.client.get(reverse('admin:admin_inlines_someparentmodel_add'))
-        self.assertContains(
-            response,
-            '<img src='/static/admin/img/icon-unknown.svg' '
-            'class='help help-tooltip' width='10' height='10' '
-            'alt='(Help text from ModelForm.Meta)' '
-            'title='Help text from ModelForm.Meta'>'
+    
+def test_login(client, auth):
+    # test that viewing the page renders without template errors
+    assert client.get('/auth/login').status_code == 200
+    
+            rv = client.get('/', headers={'Range': 'bytes=4-'})
+        assert rv.status_code == 206
+        with app.open_resource('static/index.html') as f:
+            assert rv.data == f.read()[4:]
+        rv.close()
+    
+    plt.tight_layout()
+plt.show()
+
+    
+    run_time = time.time() - t0
+print('Example run in %.3f s' % run_time)
+plt.show()
+
+    
+    # The digits dataset
+digits = datasets.load_digits()
+    
+        def reset_model(self):
+        self.set_state(
+            self.init_qpos + self.np_random.uniform(low=-.005, high=.005, size=self.model.nq),
+            self.init_qvel + self.np_random.uniform(low=-.005, high=.005, size=self.model.nv)
         )
-        self.assertContains(response, 'Label from ModelForm.Meta')
+        return self._get_obs()
     
-            This is the most complex implementation of the spatial backends due to
-        what is supported on geodetic geometry columns vs. what's available on
-        projected geometry columns.  In addition, it has to take into account
-        the geography column type.
-        '''
-        # Getting the distance parameter
-        value = dist_val[0]
+        def random_color(self):
+        return np.array([
+            self.np_random.randint(low=0, high=255),
+            self.np_random.randint(low=0, high=255),
+            self.np_random.randint(low=0, high=255),
+            ]).astype('uint8')
     
-    
-def _multi_decorate(decorators, method):
+    class Connector(object):
     '''
-    Decorate `method` with one or more function decorators. `decorators` can be
-    a single decorator or an iterable of decorators.
-    '''
-    if hasattr(decorators, '__iter__'):
-        # Apply a list/tuple of decorators if 'decorators' is one. Decorator
-        # functions are applied so that the call order is the same as the
-        # order in which they appear in the iterable.
-        decorators = decorators[::-1]
-    else:
-        decorators = [decorators]
-    
-              # Getting the 'description' field for the feature.
-          desc = feature['description']
-    
-        if dat == 'forestcover':
-        dataset = fetch_covtype(shuffle=True, random_state=random_state)
-        X = dataset.data
-        y = dataset.target
-        # normal data are those with attribute 2
-        # abnormal those with attribute 4
-        s = (y == 2) + (y == 4)
-        X = X[s, :]
-        y = y[s]
-        y = (y != 2).astype(int)
-        print_outlier_ratio(y)
-    
-    from sklearn.svm.bounds import l1_min_c
-from sklearn.svm import LinearSVC
-from sklearn.linear_model.logistic import LogisticRegression
-    
-    In both cases, only 10% of the features are informative.
-'''
-import gc
-from time import time
-import numpy as np
-    
-        for n_components in [i.astype(int) for i in
-                         np.linspace(data.shape[1] // 10,
-                                     data.shape[1], num=4)]:
-        all_times = defaultdict(list)
-        all_errors = defaultdict(list)
-        pca = PCA(n_components=n_components)
-        rpca = PCA(n_components=n_components, svd_solver='randomized',
-                   random_state=1999)
-        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
-                                                               ('rpca', rpca)]}
-    
-    from sklearn.utils import check_random_state
-from sklearn.metrics.pairwise import pairwise_distances
-from sklearn.metrics.pairwise import pairwise_kernels
-    
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
-    
-        # TASK: Predict the outcome on the testing set and store it in a variable
-    # named y_predicted
-    
-    plt.show()
-
-    
-    for connectivity in (None, knn_graph):
-    for n_clusters in (30, 3):
-        plt.figure(figsize=(10, 4))
-        for index, linkage in enumerate(('average',
-                                         'complete',
-                                         'ward',
-                                         'single')):
-            plt.subplot(1, 4, index + 1)
-            model = AgglomerativeClustering(linkage=linkage,
-                                            connectivity=connectivity,
-                                            n_clusters=n_clusters)
-            t0 = time.time()
-            model.fit(X)
-            elapsed_time = time.time() - t0
-            plt.scatter(X[:, 0], X[:, 1], c=model.labels_,
-                        cmap=plt.cm.nipy_spectral)
-            plt.title('linkage=%s\n(time %.2fs)' % (linkage, elapsed_time),
-                      fontdict=dict(verticalalignment='top'))
-            plt.axis('equal')
-            plt.axis('off')
-    
-        classifiers = proj_info['classifiers'],
-    
-    
-def cntv_download(url, **kwargs):
-    if re.match(r'http://tv\.cntv\.cn/video/(\w+)/(\w+)', url):
-        rid = match1(url, r'http://tv\.cntv\.cn/video/\w+/(\w+)')
-    elif re.match(r'http://tv\.cctv\.com/\d+/\d+/\d+/\w+.shtml', url):
-        rid = r1(r'var guid = '(\w+)'', get_content(url))
-    elif re.match(r'http://\w+\.cntv\.cn/(\w+/\w+/(classpage/video/)?)?\d+/\d+\.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/(\w+/)*VIDE\d+.shtml', url) or \
-         re.match(r'http://(\w+).cntv.cn/(\w+)/classpage/video/(\d+)/(\d+).shtml', url) or \
-         re.match(r'http://\w+.cctv.com/\d+/\d+/\d+/\w+.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/\d+/\d+/\d+/\w+.shtml', url): 
-        page = get_content(url)
-        rid = r1(r'videoCenterId','(\w+)'', page)
-        if rid is None:
-            guid = re.search(r'guid\s*=\s*'([0-9a-z]+)'', page).group(1)
-            rid = guid
-    elif re.match(r'http://xiyou.cntv.cn/v-[\w-]+\.html', url):
-        rid = r1(r'http://xiyou.cntv.cn/v-([\w-]+)\.html', url)
-    else:
-        raise NotImplementedError(url)
-    
-        @classmethod
-    def get_cdninfo(cls, hashid):
-        url = 'http://jobsfe.funshion.com/query/v1/mp4/{}.json'.format(hashid)
-        meta = json.loads(get_content(url, decoded=False).decode('utf8'))
-        return meta['playlist'][0]['urls']
-    
-            mp3 = match1(content, r'name='filename'\s*value='([^']+\.mp3)'')
-        if mp3: mp3 = 'http://res.infoq.com/downloads/mp3downloads/%s' % mp3
-    
-        # Counting to five...
-    >>> for number in count_to_five():
-    ...     print(number)
-    one
-    two
-    three
-    four
-    five
+    This class defines generic dbms protocol functionalities for plugins.
     '''
     
-    '''
-https://www.djangospin.com/design-patterns-python/mediator/
+        def search(self):
+        warnMsg = 'on Informix search option is not available'
+        logger.warn(warnMsg)
     
+    class InjectionDict(AttribDict):
+    def __init__(self):
+        AttribDict.__init__(self)
     
-# Test our radio out
-def main():
-    radio = Radio()
-    actions = [radio.scan] * 2 + [radio.toggle_amfm] + [radio.scan] * 2
-    actions *= 2
+            def run(self):
+            try:
+                self.result = func(*(args or ()), **(kwargs or {}))
+                self.timeout_state = TIMEOUT_STATE.NORMAL
+            except Exception as ex:
+                logger.log(CUSTOM_LOGGING.TRAFFIC_IN, ex)
+                self.result = default
+                self.timeout_state = TIMEOUT_STATE.EXCEPTION
     
-        print('`{}` was processed'.format(data))
+                            continue
     
+    def dependencies():
+    singleTimeWarnMessage('tamper script '%s' is only meant to be run against %s < 5.1' % (os.path.basename(__file__).split('.')[0], DBMS.MYSQL))
     
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod(optionflags=doctest.ELLIPSIS)
-
-    
-        sample_queue = queue.Queue()
-    
-    '''
-*What is this pattern about?
-This patterns aims to reduce the number of classes required by an
-application. Instead of relying on subclasses it creates objects by
-copying a prototypical instance at run-time.
-    
-        def __init__(self, wrapped):
-        self._wrapped = wrapped
-    
-    
-  def Start( self ):
-    request_data = BuildRequestData()
-    request_data.update( { 'filetypes': self.filetypes } )
-    self._response = self.PostDataToHandler( request_data,
-                                             'semantic_completion_available' )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-        self._cached_response = self.HandleFuture( self._response_future,
-                                               truncate_message = True )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+            If a is not None or an int or long, hash(a) is used instead.
