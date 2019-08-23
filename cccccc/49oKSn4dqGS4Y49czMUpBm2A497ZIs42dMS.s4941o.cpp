a01d84ae919b808212eb73bc9033d8b04a734ff9
SUNsung <weatherius@gmail.com>
2019-08-24 01:25:20 +0300

    Rewrite [49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp] File in: 2019-08-24 01:25:19

--- cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
+++ cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
@@ -1,258 +1,114 @@
 
         
-        namespace Ui {
-    class OpenURIDialog;
-}
-    
-    private:
-    Ui::SignVerifyMessageDialog *ui;
-    WalletModel *model;
-    const PlatformStyle *platformStyle;
-    
-    
-    {    Lock& lock;
-    Lock templock;
-};
-    
-        /* Check NULLs for conversion */
-    CHECK(secp256k1_ecdsa_sign(both, &normal_sig, message, privkey, NULL, NULL) == 1);
-    ecount = 0;
-    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, NULL, &recsig) == 0);
-    CHECK(ecount == 1);
-    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, &normal_sig, NULL) == 0);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, &normal_sig, &recsig) == 1);
-    
-        add	rsp, STACK_SIZE
-    
-    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
-  result->append(key.user_key.data(), key.user_key.size());
-  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
-}
-    
-      // When user keys are different, but correctly ordered
-  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('hello', 200, kTypeValue)));
-    
-    namespace opencv_test { namespace {
-    }
-    }
-    
-    
-    {    int            _tableSymbol[1 << TABLE_LOOKUP_BITS];
-    unsigned char  _tableCodeLen[1 << TABLE_LOOKUP_BITS];
-    Int64          _tableMin;
-};
-    
-    
-    {    return fps;
-}
-    
-    
-void
-Header::erase (const char name[])
+        namespace winrt::Microsoft::Terminal::Settings::implementation
 {
-    if (name[0] == 0)
-        THROW (IEX_NAMESPACE::ArgExc, 'Image attribute name cannot be an empty string.');
-    
-    
-    AttributeMap::iterator i = _map.find (name);
-    if (i != _map.end())
-        _map.erase (i);
+    KeyChord::KeyChord(bool ctrl, bool alt, bool shift, int32_t vkey) :
+        _modifiers{ (ctrl ? Settings::KeyModifiers::Ctrl : Settings::KeyModifiers::None) |
+                    (alt ? Settings::KeyModifiers::Alt : Settings::KeyModifiers::None) |
+                    (shift ? Settings::KeyModifiers::Shift : Settings::KeyModifiers::None) },
+        _vkey{ vkey }
+    {
+    }
     }
     
-    
-#ifndef INCLUDED_IMF_IO_H
-#define INCLUDED_IMF_IO_H
-    
-    bool
-InputPart::isComplete () const
-{
-    return file->isComplete();
-}
-    
-    //
-// Used to wrap OPENEXR_IMF_INTERNAL_NAMESPACE::IStream as a Mutex.
-//
-struct InputStreamMutex : public Mutex
-{
-    OPENEXR_IMF_INTERNAL_NAMESPACE::IStream* is;
-    Int64 currentPosition;
+      if (isa<SuperMethodInst>(AI.getCallee())) {
+    auto Instance = AI.getArguments().back();
+    auto ClassType = getSelfInstanceType(Instance->getType().getASTType());
+    auto *CD = ClassType.getClassOrBoundGenericClass();
     }
     
-      // Returns true if no interceptors are run. Returns false otherwise if there
-  // are interceptors registered. After the interceptors are done running \a f
-  // will be invoked. This is to be used only by BaseAsyncRequest and
-  // SyncRequest.
-  bool RunInterceptors(std::function<void(void)> f) {
-    // This is used only by the server for initial call request
-    GPR_CODEGEN_ASSERT(reverse_ == true);
-    GPR_CODEGEN_ASSERT(call_->client_rpc_info() == nullptr);
-    auto* server_rpc_info = call_->server_rpc_info();
-    if (server_rpc_info == nullptr ||
-        server_rpc_info->interceptors_.size() == 0) {
+        // ObjC functions are called through the runtime and are therefore alive
+    // even if not referenced inside SIL.
+    if (F->getRepresentation() == SILFunctionTypeRepresentation::ObjCMethod)
       return true;
-    }
-    callback_ = std::move(f);
-    RunServerInterceptors();
-    return false;
+    
+    #ifndef KNOWN_STDLIB_TYPE_DECL
+/// KNOWN_STDLIB_TYPE_DECL(NAME, DECL_CLASS, NUM_GENERIC_PARAMS)
+///
+/// The macro is expanded for each known standard library type. NAME is
+/// bound to the unqualified name of the type. DECL_CLASS is bound to the
+/// Decl subclass it is expected to be an instance of. NUM_GENERIC_PARAMS is
+/// bound to the number of generic parameters the type is expected to have.
+#define KNOWN_STDLIB_TYPE_DECL(NAME, DECL_CLASS, NUM_GENERIC_PARAMS)
+#endif
+    
+    
+    {  size_t getOpaqueValue() const { return Data; }
+  static IntegerLiteralFlags getFromOpaqueValue(size_t value) {
+    return IntegerLiteralFlags(value);
   }
+};
     
-    ChannelArguments::ChannelArguments(const ChannelArguments& other)
-    : strings_(other.strings_) {
-  args_.reserve(other.args_.size());
-  auto list_it_dst = strings_.begin();
-  auto list_it_src = other.strings_.begin();
-  for (auto a = other.args_.begin(); a != other.args_.end(); ++a) {
-    grpc_arg ap;
-    ap.type = a->type;
-    GPR_ASSERT(list_it_src->c_str() == a->key);
-    ap.key = const_cast<char*>(list_it_dst->c_str());
-    ++list_it_src;
-    ++list_it_dst;
-    switch (a->type) {
-      case GRPC_ARG_INTEGER:
-        ap.value.integer = a->value.integer;
-        break;
-      case GRPC_ARG_STRING:
-        GPR_ASSERT(list_it_src->c_str() == a->value.string);
-        ap.value.string = const_cast<char*>(list_it_dst->c_str());
-        ++list_it_src;
-        ++list_it_dst;
-        break;
-      case GRPC_ARG_POINTER:
-        ap.value.pointer = a->value.pointer;
-        ap.value.pointer.p = a->value.pointer.vtable->copy(ap.value.pointer.p);
-        break;
-    }
-    args_.push_back(ap);
-  }
-}
+    #endif // BITCOIN_QT_OPENURIDIALOG_H
+
     
-    #include <grpc/grpc_security.h>
-#include 'src/core/lib/channel/channel_args.h'
-    
-      memset(&addr, 0, sizeof(addr));
-  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-  addr.sin_family = AF_INET;
-  GPR_ASSERT(bind(lst_sock, (grpc_sockaddr*)&addr, sizeof(addr)) !=
-             SOCKET_ERROR);
-  GPR_ASSERT(listen(lst_sock, SOMAXCONN) != SOCKET_ERROR);
-  GPR_ASSERT(getsockname(lst_sock, (grpc_sockaddr*)&addr, &addr_len) !=
-             SOCKET_ERROR);
-    
-      /* Use dualstack sockets where available. */
-  if (grpc_sockaddr_to_v4mapped(addr, &addr6_v4mapped)) {
-    addr = &addr6_v4mapped;
-  }
-    
-    // Base class for all classes to access environment variables.
-class GlobalConfigEnv {
- protected:
-  // `name` should be writable and alive after constructor is called.
-  constexpr explicit GlobalConfigEnv(char* name) : name_(name) {}
+    static int recovery_test_nonce_function(unsigned char *nonce32, const unsigned char *msg32, const unsigned char *key32, const unsigned char *algo16, void *data, unsigned int counter) {
+    (void) msg32;
+    (void) key32;
+    (void) algo16;
+    (void) data;
     }
     
+    #include <stdint.h>
+#include <string>
+#include <vector>
     
-    {  gpr_setenv(name, value);
-  retrieved_value = gpr_getenv(name);
-  GPR_ASSERT(retrieved_value != nullptr);
-  GPR_ASSERT(strcmp(value, retrieved_value) == 0);
-  gpr_free(retrieved_value);
+    void InternalKeyComparator::FindShortSuccessor(std::string* key) const {
+  Slice user_key = ExtractUserKey(*key);
+  std::string tmp(user_key.data(), user_key.size());
+  user_comparator_->FindShortSuccessor(&tmp);
+  if (tmp.size() < user_key.size() &&
+      user_comparator_->Compare(user_key, tmp) < 0) {
+    // User key has become shorter physically, but larger logically.
+    // Tack on the earliest possible number to the shortened user key.
+    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
+    assert(this->Compare(*key, tmp) < 0);
+    key->swap(tmp);
+  }
 }
     
     
     {
-    { private:
-  std::shared_ptr<grpc::Channel> CreateChannelWithInterceptors(
-      const string& target, const grpc::ChannelArguments& args,
-      std::vector<
-          std::unique_ptr<experimental::ClientInterceptorFactoryInterface>>
-          interceptor_creators) override {
-    grpc_channel_args channel_args;
-    args.SetChannelArgs(&channel_args);
-    return CreateChannelInternal(
-        '',
-        grpc_cronet_secure_channel_create(engine_, target.c_str(),
-                                          &channel_args, nullptr),
-        std::move(interceptor_creators));
+}  // namespace leveldb
+    
+    
+    {  // Errors
+  static const char* errors[] = {
+    '',
+    'foo',
+    'foo-dx-100.log',
+    '.log',
+    '',
+    'manifest',
+    'CURREN',
+    'CURRENTX',
+    'MANIFES',
+    'MANIFEST',
+    'MANIFEST-',
+    'XMANIFEST-3',
+    'MANIFEST-3x',
+    'LOC',
+    'LOCKx',
+    'LO',
+    'LOGx',
+    '18446744073709551616.log',
+    '184467440737095516150.log',
+    '100',
+    '100.',
+    '100.lop'
+  };
+  for (int i = 0; i < sizeof(errors) / sizeof(errors[0]); i++) {
+    std::string f = errors[i];
+    ASSERT_TRUE(!ParseFileName(f, &number, &type)) << f;
   }
-  void* engine_;
-};
-}  // namespace grpc
-namespace grpc_impl {
-std::shared_ptr<ChannelCredentials> CronetChannelCredentials(void* engine) {
-  return std::shared_ptr<ChannelCredentials>(
-      new grpc::CronetChannelCredentialsImpl(engine));
-}
-}  // namespace grpc_impl
-
-    
-    #include <grpc/grpc.h>
-#include <grpc/support/alloc.h>
-#include <grpc/support/log.h>
-#include <grpc/support/time.h>
-#include <grpcpp/channel.h>
-#include <grpcpp/client_context.h>
-#include <grpcpp/create_channel.h>
-#include <grpcpp/server.h>
-#include <grpcpp/server_builder.h>
-#include <grpcpp/server_context.h>
-    
-    
-    {    buf_lock_t sindex_block(superblock->get(), alt_create_t::create);
-    initialize_secondary_indexes(&sindex_block);
-    sb->sindex_block = sindex_block.block_id();
 }
     
-    /* `real_superblock_t` represents the superblock for the primary B-tree of a table. */
-class real_superblock_t : public superblock_t {
-public:
-    explicit real_superblock_t(buf_lock_t &&sb_buf);
-    real_superblock_t(buf_lock_t &&sb_buf, new_semaphore_in_line_t &&write_semaphore_acq);
-    }
+    using json = nlohmann::json;
     
-        response->n_shards = 1;
-    if (trace.has()) {
-        response->event_log = std::move(*trace).extract_event_log();
-    }
-    // This is a tad hacky, this just adds a stop event to signal the end of the
-    // parallel task.
+        // create a JSON number from an anonymous enum
+    enum { enum_value = 17 };
+    json j_enum(enum_value);
     
-        // Returns the UUID of the created index, or r_nullopt if an index by `name`
-    // already existed.
-    MUST_USE optional<uuid_u> add_sindex_internal(
-        const sindex_name_t &name,
-        const std::vector<char> &opaque_definition,
-        buf_lock_t *sindex_block);
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7>
-internal::ValueArray7<T1, T2, T3, T4, T5, T6, T7> Values(T1 v1, T2 v2, T3 v3,
-    T4 v4, T5 v5, T6 v6, T7 v7) {
-  return internal::ValueArray7<T1, T2, T3, T4, T5, T6, T7>(v1, v2, v3, v4, v5,
-      v6, v7);
-}
-    
-      // Runs SetUpTestCase() for this TestCase.  This wrapper is needed
-  // for catching exceptions thrown from SetUpTestCase().
-  void RunSetUpTestCase() { (*set_up_tc_)(); }
-    
-    
-    {  return AssertionFailure() << pred_text << '('
-                            << e1 << ') evaluates to false, where'
-                            << '\n' << e1 << ' evaluates to ' << v1;
-}
-    
-    #include <stdlib.h>
-#include <assert.h>
-    
-    template <int k>
-struct SameSizeTuplePrefixComparator<k, k> {
-  template <class Tuple1, class Tuple2>
-  static bool Eq(const Tuple1& t1, const Tuple2& t2) {
-    return SameSizeTuplePrefixComparator<k - 1, k - 1>::Eq(t1, t2) &&
-        ::std::tr1::get<k - 1>(t1) == ::std::tr1::get<k - 1>(t2);
-  }
-};
+        // print values
+    std::cout << object << '\n';
+    std::cout << *res1.first << ' ' << std::boolalpha << res1.second << '\n';
