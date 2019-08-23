26bbf305d6d21f59b2fd44587aa7a1fb1bcc59d0
SUNsung <weatherius@gmail.com>
2019-08-23 13:01:10 +0300

    Rewrite [57QNI.r0YnPtA574Nq7nlA4Ng57C5M7UEq3LE57tVP.rb] File in: 2019-08-23 13:01:10

--- ruby/57QNI.r0YnPtA574Nq7nlA4Ng57C5M7UEq3LE57tVP.rb
+++ ruby/57QNI.r0YnPtA574Nq7nlA4Ng57C5M7UEq3LE57tVP.rb
@@ -1,140 +1,129 @@
 
         
-              # Check for mixed tab/spaces. Upgrade this to an error() soon.
-      if (ln.length > 1) and (ln =~ /^([\t ]*)/) and ($1.match(/\x20\x09|\x09\x20/))
-        warn('Space-Tab mixed indent: #{ln.inspect}', idx)
+                # set log level
+        log_level = ssh_info[:log_level] || 'FATAL'
+    
+          subject.rsync_single(machine, ssh_info, opts)
+    end
+    
+        let(:empty_opts) {{:type=>:rsync,
+                 :guestpath=>'/vagrant',
+                 :hostpath=>'/home/user/syncfolder',
+                 :disabled=>false,
+                 :__vagrantfile=>true,
+                 :exclude=>[],
+                 :owner=>'vagrant',
+                 :group=>'vagrant'}}
+    
+          expect(env[:box_added]).to equal(box)
+    end
+    
+              config_file = TemplateRenderer.render(template, {
+            custom_configuration: remote_custom_config_path,
+            encrypted_data_bag_secret: guest_encrypted_data_bag_secret_key_path,
+            environment:      @config.environment,
+            file_cache_path:  guest_file_cache_path,
+            file_backup_path: guest_file_backup_path,
+            log_level:        @config.log_level.to_sym,
+            node_name:        @config.node_name,
+            verbose_logging:  @config.verbose_logging,
+            enable_reporting: @config.enable_reporting,
+            http_proxy:       @config.http_proxy,
+            http_proxy_user:  @config.http_proxy_user,
+            http_proxy_pass:  @config.http_proxy_pass,
+            https_proxy:      @config.https_proxy,
+            https_proxy_user: @config.https_proxy_user,
+            https_proxy_pass: @config.https_proxy_pass,
+            no_proxy:         @config.no_proxy,
+            formatter:        @config.formatter
+          }.merge(template_vars))
+    
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
+    end
+    
+          it 'should expand the relative host directory' do
+        expect(docker_yml).to receive(:write).with(%r{/compose/cwd/path})
       end
+    end
     
-        xml_header = %Q|<?xml version='1.0' encoding='UTF-8'?>
-\t<c:contact c:Version='1' xmlns:c='http://schemas.microsoft.com/Contact' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:MSP2P='http://schemas.microsoft.com/Contact/Extended/MSP2P'>
-\t<c:CreationDate>2019-04-10T20:19:26Z</c:CreationDate><c:Extended xsi:nil='true'/>
-\t|
+      def find_user
+    return User.find_for_authentication(username: params[:user][:username]) if params[:user][:username]
     
-      #
-  # Flushes all header pairs.
-  #
-  def reset
-    self.cmd_string = ''
-    self.clear
-    self.dcase_hash.clear
+      def add_invites
+    InvitationCode.find_by_token(params[:invite_code_id]).add_invites!
+    redirect_to user_search_path
   end
     
-    
-      # If we found the resource handler for this resource, call its
-      # procedure.
-      if (p[1] == true)
-        Rex::ThreadFactory.spawn('HTTPServerRequestHandler', false) {
-          handler.on_request(cli, request)
-        }
-      else
-        handler.on_request(cli, request)
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
       end
+    
+      respond_to :json
+    
+        begin
+      if current_user.auto_follow_back && @aspect.id == current_user.auto_follow_back_aspect.id
+        current_user.update(auto_follow_back: false, auto_follow_back_aspect: nil)
+        flash[:notice] = I18n.t 'aspects.destroy.success_auto_follow_back', name: @aspect.name
+      else
+        flash[:notice] = I18n.t 'aspects.destroy.success', name: @aspect.name
+      end
+      @aspect.destroy
+    rescue ActiveRecord::StatementInvalid => e
+      flash[:error] = I18n.t 'aspects.destroy.failure', name: @aspect.name
+    end
+    
+      def destroy
+    if like_service.destroy(params[:id])
+      head :no_content
     else
-      elog('Failed to find handler for resource: #{request.resource}',
-        LogSource)
-    
-      def ring_time
-    (self.ring_finish || Time.now).to_i - self.ring_start.to_i
-  end
-    
-              # Encodes the msg_type field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_msg_type
-            bn = OpenSSL::BN.new(msg_type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-    Then(/^the specified stage files are created$/) do
-  qa = TestApp.test_app_path.join('config/deploy/qa.rb')
-  production = TestApp.test_app_path.join('config/deploy/production.rb')
-  expect(File.exist?(qa)).to be true
-  expect(File.exist?(production)).to be true
-end
-    
-          def warn_set_scm_is_deprecated
-        $stderr.puts(<<-MESSAGE)
-[Deprecation Notice] `set :scm, #{scm_name.inspect}` is deprecated.
-To ensure your project is compatible with future versions of Capistrano,
-remove the :scm setting and instead add these lines to your Capfile after
-`require 'capistrano/deploy'`:
-    
-          def escaped_name
-        CGI.escape(@name)
-      end
-    
-      test 'clean path without leading slash' do
-    assert_equal '/Mordor', clean_path('Mordor')
-  end
-    
-    # internal
-require File.expand_path('../gollum/uri_encode_component', __FILE__)
-    
-    require 'gollum'
-require 'gollum/views/layout'
-require 'gollum/views/editable'
-require 'gollum/views/has_page'
-    
-        it 'returns nil if an except is provided' do
-      valid_article = create(:article, tags: 'explainlikeimfive')
-      expect(described_class.new(valid_article, 'explainlikeimfive').tag).to eq(nil)
-    end
-    
-      describe 'POST /users/api_secrets' do
-    context 'when create succeeds' do
-      let(:valid_params) { { description: 'My Test 3rd Party App' } }
-    
-    RSpec.describe 'all routes', type: :routing do
-  let(:podcast)     { create(:podcast) }
-  let(:user)        { create(:user) }
-    
-        private
-    
-                if response.cvv_result
-              self.cvv_response_code = response.cvv_result['code']
-              self.cvv_response_message = response.cvv_result['message']
-            end
-          end
-          send('#{success_state}!')
-        else
-          send(failure_state)
-          gateway_error(response)
-        end
-      end
-    
-        def method_missing(method, *args)
-      name = method.to_s.delete('=')
-      if has_preference? name
-        if method.to_s =~ /=$/
-          set_preference(name, args.first)
-        else
-          get_preference name
-        end
-      else
-        super
-      end
+      render plain: I18n.t('likes.destroy.error'), status: 404
     end
   end
-end
-
     
-          return_items.group_by(&:exchange_variant).map do |variant, variant_return_items|
-        variant_inventory_units = variant_return_items.map(&:exchange_inventory_units).flatten
-        line_item = Spree::LineItem.create!(variant: variant, quantity: variant_return_items.count, order: order)
-        variant_inventory_units.each { |i| i.update!(line_item_id: line_item.id, order_id: order.id) }
-      end
+      def render_as_json(unread_count, unread_count_by_type, notification_list)
+    {
+      unread_count:         unread_count,
+      unread_count_by_type: unread_count_by_type,
+      notification_list:    notification_list.map {|note|
+        NotificationSerializer.new(note, default_serializer_options).as_json
+      }
+    }.as_json
+  end
     
-            def show
-          authorize! :show, @order, order_token
-          @address = find_address
-          respond_with(@address)
-        end
+    module RailsCommandHelpers
+  def framework_version?(version_string)
+    framework_version =~ /^#{version_string}/
+  end
     
-            def inventory_unit
-          @inventory_unit ||= InventoryUnit.accessible_by(current_ability, :show).find(params[:id])
-        end
-    
-            def destroy
-          @option_value = scope.accessible_by(current_ability, :destroy).find(params[:id])
-          @option_value.destroy
-          render plain: nil, status: 204
-        end
+      # Provides configurability to Paperclip. The options available are:
+  # * whiny: Will raise an error if Paperclip cannot process thumbnails of
+  #   an uploaded image. Defaults to true.
+  # * log: Logs progress to the Rails log. Uses ActiveRecord's logger, so honors
+  #   log levels, etc. Defaults to true.
+  # * command_path: Defines the path at which to find the command line
+  #   programs if they are not visible to Rails the system's search path. Defaults to
+  #   nil, which uses the first executable found in the user's search path.
+  # * use_exif_orientation: Whether to inspect EXIF data to determine an
+  #   image's orientation. Defaults to true.
+  def self.options
+    @options ||= {
+      command_path: nil,
+      content_type_mappings: {},
+      log: true,
+      log_command: true,
+      read_timeout: nil,
+      swallow_stderr: true,
+      use_exif_orientation: true,
+      whiny: true,
+      is_windows: Gem.win_platform?
+    }
+  end
