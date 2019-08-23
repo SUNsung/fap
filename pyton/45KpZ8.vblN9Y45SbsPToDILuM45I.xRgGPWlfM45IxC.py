5e380ceb35c49ea5f83e281050fd2898429d9910
SUNsung <weatherius@gmail.com>
2019-08-23 16:34:21 +0300

    Rewrite [45KpZ8.vblN9Y45SbsPToDILuM45I.xRgGPWlfM45IxC.py] File in: 2019-08-23 16:34:21

--- pyton/45KpZ8.vblN9Y45SbsPToDILuM45I.xRgGPWlfM45IxC.py
+++ pyton/45KpZ8.vblN9Y45SbsPToDILuM45I.xRgGPWlfM45IxC.py
@@ -1,77 +1,215 @@
 
         
-        from mrjob.job import MRJob
+            def env(self):
+        '''
+        Return an environment.
+    
+            `stdout` is represented by the instance itself (print r)
+        `stderr`: text written to stderr
+        `exit_status`: the exit status
+        `json`: decoded JSON (if possible) or `None`
     
     
-class PollErrorTest(unittest.TestCase):
-    '''Tests for acme.errors.PollError.'''
+setup(
+    name='httpie',
+    version=httpie.__version__,
+    description=httpie.__doc__.strip(),
+    long_description=long_description(),
+    url='http://httpie.org/',
+    download_url='https://github.com/jakubroztocil/httpie',
+    author=httpie.__author__,
+    author_email='jakub@roztocil.co',
+    license=httpie.__licence__,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'http = httpie.__main__:main',
+        ],
+    },
+    extras_require=extras_require,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        'Topic :: System :: Networking',
+        'Topic :: Terminals',
+        'Topic :: Text Processing',
+        'Topic :: Utilities'
+    ],
+)
+
     
-    # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-]
-    
-            self.assertTrue(self.addr_default.conflicts(self.addr))
-        self.assertTrue(self.addr_default.conflicts(self.addr1))
-        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
-    
-                        self.assertTrue(mock_eab_from_data.called)
-    
-        def cleanup(self):
-        self._stop_nginx()
-        super(IntegrationTestsContext, self).cleanup()
+        def __init__(self, **kwargs):
+        '''
+        Use keyword arguments to overwrite
+        any of the class attributes for this instance.
     
     
-@pytest.mark.parametrize('certname_pattern, params, context', [
-    ('nginx.{0}.wtf', ['run'], {'default_server': True}),
-    ('nginx2.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': True}),
-    # Overlapping location block and server-block-level return 301
-    ('nginx3.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': True}),
-    # No matching server block; default_server exists
-    ('nginx4.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': True}),
-    # No matching server block; default_server does not exist
-    ('nginx5.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': False}),
-    # Multiple domains, mix of matching and not
-    ('nginx6.{0}.wtf,nginx7.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': False}),
-], indirect=['context'])
-def test_certificate_deployment(certname_pattern, params, context):
-    # type: (str, list, nginx_context.IntegrationTestsContext) -> None
+class Conversion(object):
+    
+        # Set to `False` to make it possible to invoke this auth
+    # plugin without requiring the user to specify credentials
+    # through `--auth, -a`.
+    auth_require = True
+    
+    
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
+    
+    
+class ContentDecodingError(RequestException, BaseHTTPError):
+    '''Failed to decode response content'''
+    
+        styles = {
+        # No corresponding class for the following:
+        #Text:                     '', # class:  ''
+        Whitespace:                'underline #f8f8f8',      # class: 'w'
+        Error:                     '#a40000 border:#ef2929', # class: 'err'
+        Other:                     '#000000',                # class 'x'
+    }
+    
+    
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
     '''
-    Test various scenarios to deploy a certificate to nginx using certbot.
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
+    
+        def get(self, key, default=None):
+        return self.__dict__.get(key, default)
+
+    
+        def test_cookielib_cookiejar_on_redirect(self, httpbin):
+        '''Tests resolve_redirect doesn't fail when merging cookies
+        with non-RequestsCookieJar cookiejar.
+    
+    First, we fix a training set and increase the number of
+samples. Then we plot the computation time as function of
+the number of samples.
+    
+        An example with a long-untouched module that everyone has
+    >>> _linkcode_resolve('py', {'module': 'tty',
+    ...                          'fullname': 'setraw'},
+    ...                   package='tty',
+    ...                   url_fmt='http://hg.python.org/cpython/file/'
+    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
+    ...                   revision='xxxx')
+    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
     '''
-    domains = certname_pattern.format(context.worker_id)
-    command = ['--domains', domains]
-    command.extend(params)
-    context.certbot_test_nginx(command)
     
-        command = [
-        'certbot',
-        '--server', directory_url,
-        '--no-verify-ssl',
-        '--http-01-port', str(http_01_port),
-        '--https-port', str(tls_alpn_01_port),
-        '--manual-public-ip-logging-ok',
-        '--config-dir', config_dir,
-        '--work-dir', os.path.join(workspace, 'work'),
-        '--logs-dir', os.path.join(workspace, 'logs'),
-        '--non-interactive',
-        '--no-redirect',
-        '--agree-tos',
-        '--register-unsafely-without-email',
-        '--debug',
-        '-vv'
-    ]
+    '''
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: Simplified BSD
     
-            if threads != 'default':
-            expr.set_numexpr_threads(threads)
-        if not use_numexpr:
-            expr.set_use_numexpr(False)
+    The data is generated with the ``make_checkerboard`` function, then
+shuffled and passed to the Spectral Biclustering algorithm. The rows
+and columns of the shuffled matrix are rearranged to show the
+biclusters found by the algorithm.
     
-        def time_pandas_dtype(self, dtype):
-        pandas_dtype(dtype)
+    This example is commented in the
+:ref:`tutorial section of the user manual <introduction>`.
+    
+    import argparse
+import h5py
+import json
+import os
+import scipy.misc
+import sys
+    
+    
+def evaluate_proposal_file(dataset, proposal_file, output_dir):
+    '''Evaluate box proposal average recall.'''
+    roidb = dataset.get_roidb(gt=True, proposal_file=proposal_file)
+    results = task_evaluation.evaluate_box_proposals(dataset, roidb)
+    task_evaluation.log_box_proposal_results(results)
+    recall_file = os.path.join(output_dir, 'rpn_proposal_recall.pkl')
+    save_object(results, recall_file)
+    return results
+
+    
+    
+def get_rpn_blob_names(is_training=True):
+    '''Blob names used by RPN.'''
+    # im_info: (height, width, image scale)
+    blob_names = ['im_info']
+    if is_training:
+        # gt boxes: (batch_idx, x1, y1, x2, y2, cls)
+        blob_names += ['roidb']
+        if cfg.FPN.FPN_ON and cfg.FPN.MULTILEVEL_RPN:
+            # Same format as RPN blobs, but one per FPN level
+            for lvl in range(cfg.FPN.RPN_MIN_LEVEL, cfg.FPN.RPN_MAX_LEVEL + 1):
+                blob_names += [
+                    'rpn_labels_int32_wide_fpn' + str(lvl),
+                    'rpn_bbox_targets_wide_fpn' + str(lvl),
+                    'rpn_bbox_inside_weights_wide_fpn' + str(lvl),
+                    'rpn_bbox_outside_weights_wide_fpn' + str(lvl)
+                ]
+        else:
+            # Single level RPN blobs
+            blob_names += [
+                'rpn_labels_int32_wide',
+                'rpn_bbox_targets_wide',
+                'rpn_bbox_inside_weights_wide',
+                'rpn_bbox_outside_weights_wide'
+            ]
+    return blob_names
+    
+        def enqueue_blobs(self, gpu_id, blob_names, blobs):
+        '''Put a mini-batch on a BlobsQueue.'''
+        assert len(blob_names) == len(blobs)
+        t = time.time()
+        dev = c2_utils.CudaDevice(gpu_id)
+        queue_name = 'gpu_{}/{}'.format(gpu_id, self._blobs_queue_name)
+        blob_names = ['gpu_{}/{}'.format(gpu_id, b) for b in blob_names]
+        for (blob_name, blob) in zip(blob_names, blobs):
+            workspace.FeedBlob(blob_name, blob, device_option=dev)
+        logger.debug(
+            'enqueue_blobs {}: workspace.FeedBlob: {}'.
+            format(gpu_id, time.time() - t)
+        )
+        t = time.time()
+        op = core.CreateOperator(
+            'SafeEnqueueBlobs', [queue_name] + blob_names,
+            blob_names + [queue_name + '_enqueue_status'],
+            device_option=dev
+        )
+        workspace.RunOperatorOnce(op)
+        logger.debug(
+            'enqueue_blobs {}: workspace.RunOperatorOnce: {}'.
+            format(gpu_id, time.time() - t)
+        )
+    
+        # Override config with the one saved in the detections file
+    if args.cfg_file is not None:
+        core_config.merge_cfg_from_cfg(core_config.load_cfg(dets['cfg']))
+    else:
+        core_config._merge_a_into_b(core_config.load_cfg(dets['cfg']), cfg)
+    results = task_evaluation.evaluate_all(
+        dataset,
+        dets['all_boxes'],
+        dets['all_segms'],
+        dets['all_keyps'],
+        output_dir,
+        use_matlab=args.matlab_eval
+    )
+    task_evaluation.log_copy_paste_friendly_results(results)
