1a0bacc98fa24182fd7f3126aa955dc075575da9
SUNsung <weatherius@gmail.com>
2019-08-24 18:22:55 +0300

    Rewrite [15jxZlql5li1w15Y1cfj0x1eF615d2GlFTdWGXw15KqN.cpp] File in: 2019-08-24 18:22:54

--- cccccc/15jxZlql5li1w15Y1cfj0x1eF615d2GlFTdWGXw15KqN.cpp
+++ cccccc/15jxZlql5li1w15Y1cfj0x1eF615d2GlFTdWGXw15KqN.cpp
@@ -1,268 +1,185 @@
 
         
-        void accumulate(const Size2D &size,
-                const u8 *srcBase, ptrdiff_t srcStride,
-                s16 *dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    size_t roiw16 = size.width >= 15 ? size.width - 15 : 0;
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+          bool has_leaders;   // does the line contain leader dots (.....)?
+  bool has_drop_cap;  // does the line have a drop cap?
+  int pix_ldistance;  // distance to the left pblock boundary in pixels
+  int pix_rdistance;  // distance to the right pblock boundary in pixels
+  float pix_xheight;  // guessed xheight for the line
+  int average_interword_space; // average space between words in pixels.
+    
+    // Solve the dynamic programming problem for the given array of points, with
+// the given size and cost function.
+// Steps backwards are limited to being between min_step and max_step
+// inclusive.
+// The return value is the tail of the best path.
+DPPoint* DPPoint::Solve(int min_step, int max_step, bool debug,
+                        CostFunc cost_func, int size, DPPoint* points) {
+  if (size <= 0 || max_step < min_step || min_step >= size)
+    return nullptr;  // Degenerate, but not necessarily an error.
+  ASSERT_HOST(min_step > 0);  // Infinite loop possible if this is not true.
+  if (debug)
+    tprintf('min = %d, max=%d\n',
+            min_step, max_step);
+  // Evaluate the total cost at each point.
+  for (int i = 0; i < size; ++i) {
+    for (int offset = min_step; offset <= max_step; ++offset) {
+      DPPoint* prev = offset <= i ? points + i - offset : nullptr;
+      int64_t new_cost = (points[i].*cost_func)(prev);
+      if (points[i].best_prev_ != nullptr && offset > min_step * 2 &&
+          new_cost > points[i].total_cost_)
+        break;  // Find only the first minimum if going over twice the min.
     }
-    
-    
-    {
-    {
-    {            for (; j < size.width; j++)
-                dst[j] = (s16)((s32)src0[j] + (s32)src1[j]);
-        }
-    }
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-    (void)policy;
-#endif
-}
-    
-    struct BitwiseAnd
-{
-    typedef u8 type;
-    }
-    
-            int16x8_t dya = vabsq_s16(dy);
-        int16x8_t dxa = vabsq_s16(dx);
-        int16x8_t norm = vaddq_s16(dya, dxa);
-    
-    #ifndef CAROTENE_SRC_COMMON_HPP
-#define CAROTENE_SRC_COMMON_HPP
-    
-    bool isGaussianBlur5x5Supported(const Size2D &size, s32 cn, BORDER_MODE border)
-{
-    return isSupportedConfiguration() &&
-           cn > 0 && cn <= 4 &&
-           size.width >= 8 && size.height >= 2 &&
-           (border == BORDER_MODE_CONSTANT ||
-            border == BORDER_MODE_REFLECT101 ||
-            border == BORDER_MODE_REFLECT ||
-            border == BORDER_MODE_REPLICATE ||
-            border == BORDER_MODE_WRAP);
-}
-    
-      scoped_ptr<base::Value> value_option(
-      converter->FromV8Value(options, isolate->GetCurrentContext()));
-  if (!value_option.get() ||
-      !value_option->IsType(base::Value::TYPE_DICTIONARY))
-    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-        'Unable to convert 'option' passed to AllocateObject')));
-    
-    void Clipboard::SetText(std::string& text) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  ui::Clipboard::ObjectMap map;
-  map[ui::Clipboard::CBF_TEXT].push_back(
-      std::vector<char>(text.begin(), text.end()));
-  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
-}
-    
-    Menu::Menu(int id,
-           const base::WeakPtr<ObjectManager>& object_manager,
-           const base::DictionaryValue& option,
-           const std::string& extension_id)
-  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
-  Create(option);
-}
-    
-    void MenuDelegate::ExecuteCommand(int command_id, int event_flags) {
-  if (command_id < 0)
-    return;
-    }
-    
-    class MenuDelegate : public ui::SimpleMenuModel::Delegate {
- public:
-  MenuDelegate(ObjectManager* object_manager);
-  ~MenuDelegate() override;
-    }
-    
-    void PointMenuPositionFunc(GtkMenu* menu,
-                           int* x,
-                           int* y,
-                           gboolean* push_in,
-                           gpointer userdata) {
-  *push_in = TRUE;
-    }
-    
-    void Menu::UpdateKeys(views::FocusManager *focus_manager){
-  if (focus_manager == NULL){
-    return ;
-  } else {
-    focus_manager_ = focus_manager;
-    for(auto* item : menu_items_) {
-      item->UpdateKeys(focus_manager);
+    points[i].total_cost_ += points[i].local_cost_;
+    if (debug) {
+      tprintf('At point %d, local cost=%d, total_cost=%d, steps=%d\n',
+              i, points[i].local_cost_, points[i].total_cost_,
+              points[i].total_steps_);
     }
   }
+  // Now find the end of the best path and return it.
+  int best_cost = points[size - 1].total_cost_;
+  int best_end = size - 1;
+  for (int end = best_end - 1; end >= size - min_step; --end) {
+    int cost = points[end].total_cost_;
+    if (cost < best_cost) {
+      best_cost = cost;
+      best_end = end;
+    }
+  }
+  return points + best_end;
 }
     
-    MenuItem::MenuItem(int id,
-                   const base::WeakPtr<ObjectManager>& object_manager,
-                   const base::DictionaryValue& option,
-                   const std::string& extension_id)
-  : Base(id, object_manager, option, extension_id) {
-  Create(option);
-}
-    
-    
-    {}  // namespace nwapi
-    
-    void MenuItem::SetTooltip(const std::string& tooltip) {
-  is_modified_ = true;
-  tooltip_ = base::UTF8ToUTF16(tooltip);
-  if (menu_)
-    menu_->UpdateStates();
-}
-    
-    NwAppClearAppCacheFunction::~NwAppClearAppCacheFunction() {
-}
-    
-    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
- public:
-  NwMenuGetNSStringWithFixupFunction(){}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    
- protected:
-  ~NwMenuGetNSStringWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
-};
-    
-      //----- update text state
-  virtual void updateFont(GfxState *state);
-  virtual void updateTextMat(GfxState *state);
-  virtual void updateCharSpace(GfxState *state);
-  virtual void updateRender(GfxState *state);
-  virtual void updateRise(GfxState *state);
-  virtual void updateWordSpace(GfxState *state);
-  virtual void updateHorizScaling(GfxState *state);
-  virtual void updateTextPos(GfxState *state);
-  virtual void updateTextShift(GfxState *state, double shift);
-    
-    
-    {  links = new Links(getAnnots(&obj), catalog->getBaseURI());
-  obj.free();
-  return links;
-}
-    
-    #include <limits.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <assert.h>
-    
-      // Constructor.
-  Parser(XRef *xrefA, Lexer *lexerA, GBool allowStreamsA);
-    
-        Object *put(const Ref &ref);
-    Object *lookup(const Ref &ref, Object *obj);
-    
-    // TODO: SelectorRendition
-
-    
-      GBool getIsEmbedded() { return isEmbedded; }
-  Stream* getEmbbededStream() { return embeddedStream; }
-  // write embedded stream to file
-  void outputToFile(FILE*);
-    
-    #endif // ENABLE_PLUGINS
-
-    
-      // Attempt to authorize the document, using the supplied
-  // authorization data (which may be NULL).  Returns true if
-  // successful (i.e., if at least the right to open the document was
-  // granted).
-  virtual GBool authorize(void *authData) = 0;
-    
-    private:
-  // Create a sound. The Object obj is ensured to be a Stream with a Dict
-  Sound(Object *obj, bool readAttrs = true);
-    
-    
-    {    return g_ClipboardText;
-}
-    
-    #include 'imgui.h'      // IMGUI_API, ImFontAtlas
-    
-    
-    {        // Rendering
-        ImGui::Render();
-        al_clear_to_color(al_map_rgba_f(clear_color.x, clear_color.y, clear_color.z, clear_color.w));
-        ImGui_ImplAllegro5_RenderDrawData(ImGui::GetDrawData());
-        al_flip_display();
+    namespace ImGuiFreeType
+{
+    // Hinting greatly impacts visuals (and glyph sizes).
+    // When disabled, FreeType generates blurrier glyphs, more or less matches the stb's output.
+    // The Default hinting mode usually looks good, but may distort glyphs in an unusual way.
+    // The Light hinting mode generates fuzzier glyphs but better matches Microsoft's rasterizer.
     }
     
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
     
-    {        // see if our best match qualifies
-        if (best < 3) { // fast path literals
-            ++q;
-        } else if (best > 2  &&  best <= 0x80    &&  dist <= 0x100) {
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            stb_out(0x80 + best-1);
-            stb_out(dist-1);
-        } else if (best > 5  &&  best <= 0x100   &&  dist <= 0x4000) {
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            stb_out2(0x4000 + dist-1);
-            stb_out(best-1);
-        } else if (best > 7  &&  best <= 0x100   &&  dist <= 0x80000) {
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            stb_out3(0x180000 + dist-1);
-            stb_out(best-1);
-        } else if (best > 8  &&  best <= 0x10000 &&  dist <= 0x80000) {
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            stb_out3(0x100000 + dist-1);
-            stb_out2(best-1);
-        } else if (best > 9                      &&  dist <= 0x1000000) {
-            if (best > 65536) best = 65536;
-            outliterals(lit_start, q-lit_start); lit_start = (q += best);
-            if (best <= 0x100) {
-                stb_out(0x06);
-                stb_out3(dist-1);
-                stb_out(best-1);
-            } else {
-                stb_out(0x04);
-                stb_out3(dist-1);
-                stb_out2(best-1);
-            }
-        } else {  // fallback literals if no match was a balanced tradeoff
-            ++q;
+    // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, 'legacy_stdio_definitions')
+#endif
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+        void UserPostProcessing::doSomething(cv::Mat& output, const cv::Mat& input)
+    {
+        try
+        {
+            // Random operation on data
+            cv::bitwise_not(input, output);
+        }
+        catch (const std::exception& e)
+        {
+            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
         }
     }
     
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'ID3D10ShaderResourceView*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-//  [X] Renderer: Support for large meshes (64k+ vertices) with 16-bits indices.
+        private:
+        const bool mMergeResults;
+        const int mLevels;
+        const int mPatchSize;
+        const bool mTrackVelocity;
+        const float mConfidenceThreshold;
+        const bool mScaleVarying;
+        const float mRescale;
     
-    // Forward declarations of helper functions
-bool CreateDeviceD3D(HWND hWnd);
-void CleanupDeviceD3D();
-void ResetDevice();
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-    
-    // Render function
-// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
-void ImGui_ImplDX10_RenderDrawData(ImDrawData* draw_data)
-{
-    // Avoid rendering when minimized
-    if (draw_data->DisplaySize.x <= 0.0f || draw_data->DisplaySize.y <= 0.0f)
-        return;
+    void BulkOperations::AbstractOperation::run(
+    QSharedPointer<RedisClient::Connection> targetConnection,
+    int targetDbIndex) {
+  if (!isMetadataValid()) {
+    qWarning() << QString('Invalid metadata for %1').arg(getTypeName());
+    return;
+  }
     }
     
-        // Backup GL state
-    GLint last_texture; glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
-    GLint last_polygon_mode[2]; glGetIntegerv(GL_POLYGON_MODE, last_polygon_mode);
-    GLint last_viewport[4]; glGetIntegerv(GL_VIEWPORT, last_viewport);
-    GLint last_scissor_box[4]; glGetIntegerv(GL_SCISSOR_BOX, last_scissor_box);
-    glPushAttrib(GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT | GL_TRANSFORM_BIT);
+    
+    {          m_combinator->combine(future);
+        }
+    
+      void loadNamespaceItems(
+      QSharedPointer<ConnectionsTree::AbstractNamespaceItem> parent,
+      const QString& filter, std::function<void(const QString& err)> callback,
+      QSet<QByteArray> expandedNs) override;
+    
+    QVariant BulkOperations::Manager::getTargetConnections() {
+  return QVariant(m_model->getConnections());
+}
+    
+      int operationProgress() const;
+    
+     protected:
+  virtual bool isMetadataValid() const { return true; }
+    
+              auto future = m_connection->cmd(
+              rawCmd, this, m_dbIndex,
+              [this](const RedisClient::Response&) {
+                QMutexLocker l(&m_processedKeysMutex);
+                m_progress++;
+                emit progress(m_progress);
+              },
+              [this, rawCmd](const QString& err) {
+                QMutexLocker l(&m_errorsMutex);
+                m_errors.append(
+                    QCoreApplication::translate('RDM',
+                                                'Cannot execute command ') +
+                    QString('%1: %2')
+                        .arg(QString::fromUtf8(rawCmd.join(QByteArray(' '))))
+                        .arg(err));
+              });
+    
+      /**
+   * Similar to split, but will return the entire queue instead of throwing
+   * if n exceeds the number of bytes in the queue.
+   */
+  std::unique_ptr<folly::IOBuf> splitAtMost(size_t n) {
+    return split(n, false);
+  }
+    
+    class UniqueInstance {
+ public:
+  template <typename... Key, typename... Mapped>
+  FOLLY_EXPORT explicit UniqueInstance(
+      char const* tmpl,
+      tag_t<Key...>,
+      tag_t<Mapped...>) noexcept {
+    static Ptr const ptrs[] = {&typeid(Key)..., &typeid(Mapped)...};
+    auto& global = createGlobal<Value, tag_t<Tag, Key...>>();
+    enforce(tmpl, ptrs, sizeof...(Key), sizeof...(Mapped), global);
+  }
+    }
+    
+    #include <folly/executors/InlineExecutor.h>
+#include <folly/futures/detail/Core.h>
+#include <folly/lang/Pretty.h>
+    
+      /// Move ctor
+  ///
+  /// Postconditions:
+  ///
+  /// - `this` will have whatever shared-state was previously held by `other`
+  ///   (if any)
+  /// - `other.valid()` will be false (`other` will not have any shared state)
+  Promise(Promise<T>&& other) noexcept;
+    
+      bool writeWithPriority(T&& item, size_t priority) {
+    size_t queue = std::min(getNumPriorities() - 1, priority);
+    CHECK_LT(queue, queues_.size());
+    return queues_.at(queue).write(std::move(item));
+  }
+    
+      ScopedEventBaseThread sebt{kThreadName};
+  sebt.getEventBase()->runInEventBaseThread([&] {
+    createdThreadName = folly::getCurrentThreadName();
+    done.post();
+  });
