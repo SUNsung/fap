4dbb6f0329c53e302740cab13fa8f8ddfb870bcb
SUNsung <weatherius@gmail.com>
2019-08-24 23:10:18 +0300

    Rewrite [57bVX3eG4wGjs571yf6ldkl8WM57Rdq9Nbtsdms57H1g.rb] File in: 2019-08-24 23:10:18

--- ruby/57bVX3eG4wGjs571yf6ldkl8WM57Rdq9Nbtsdms57H1g.rb
+++ ruby/57bVX3eG4wGjs571yf6ldkl8WM57Rdq9Nbtsdms57H1g.rb
@@ -1,194 +1,114 @@
 
         
-        module Jekyll
-  class StaticFile
-    extend Forwardable
-    
-          Jekyll.logger.info 'Source:', site.source
-      Jekyll.logger.info 'Destination:', site.dest
-      Jekyll.logger.info 'Plugins and Cache:', site.safe ? 'disabled' : 'enabled'
-      Jekyll.logger.info 'Profiling phases:', build_phases.join(', ').cyan
-      Jekyll.logger.info 'Profiling...'
-    
-    def linkify(markdown)
-  linkify_prs(markdown)
-end
-    
-    if pathutil_relative == native_relative
-  Benchmark.ips do |x|
-    x.report('pathutil') { pathutil_relative }
-    x.report('native')   { native_relative }
-    x.compare!
-  end
-else
-  print 'PATHUTIL: '
-  puts pathutil_relative
-  print 'NATIVE:   '
-  puts native_relative
-end
-
-    
-    AfterConfiguration do |config|
-  f = Jekyll::Cucumber::Formatter.new(nil, $stdout, {})
-    
-          def self.example_code
-        [
-          'modify_services(
-            username: 'test.account@gmail.com',
-            app_identifier: 'com.someorg.app',
-            services: {
-              push_notification: 'on',
-              associated_domains: 'off',
-              wallet: :on,
-              apple_pay: :off,
-              data_protection: true,
-              multipath: false
-            })'
-        ]
-      end
-    
-              UI.message('Latest upload for live-version #{app.live_version.version} is build: #{build_nr}')
-    
-          def self.get(bundle_id_id: nil, includes: nil)
-        return Spaceship::ConnectAPI.get_bundle_id(bundle_id_id: bundle_id_id, includes: includes).first
+            # Override configuration for a specific platform. If received platform name
+    # does not match the platform name available as environment variable, no
+    # changes will be applied.
+    #
+    # @param platform_name Symbol representing a platform name.
+    # @yield Block to run for overriding configuration values.
+    #
+    def for_platform(platform_name)
+      if ENV['FASTLANE_PLATFORM_NAME'] == platform_name.to_s
+        with_a_clean_config_merged_when_complete do
+          yield
+        end
       end
     end
-  end
-end
-
     
-          def self.all(filter: {}, includes: nil, limit: nil, sort: nil)
-        resps = Spaceship::ConnectAPI.get_devices(filter: filter, includes: includes).all_pages
-        return resps.map(&:to_models).flatten
+          verify_config!
+    
+          it 'is active' do
+        mock_client_response(:get_build) do
+          {
+            'externalState' => 'testflight.build.state.testing.active'
+          }
+        end
+        expect(build).to be_active
       end
+    
+          it 'inspect works' do
+        allow_any_instance_of(Time).to receive(:to_i).and_return(effective_date)
+        TunesStubbing.itc_stub_update_price_tier
+    
+                  # Creates the mapping for a bundle identifier and profile specifier/uuid
+              if has_profile_specifier
+                provisioning_profile_mapping[bundle_identifier] = provisioning_profile_specifier
+              elsif has_profile_uuid
+                provisioning_profile_mapping[bundle_identifier] = provisioning_profile_uuid
+              end
+            end
+    
+        class GetBuildNumberAction < Action
+      require 'shellwords'
+    
+        class MakeChangelogFromJenkinsAction < Action
+      def self.run(params)
+        require 'json'
+        require 'net/http'
+    
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
+    end
+    
+        it 'returns false if not installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(false)
+      expect(subject.chef_installed(machine, 'chef_solo', version)).to be_falsey
     end
   end
 end
 
     
-        def at_xpath(*args)
-      doc.at_xpath(*args)
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
     end
     
-              node['data-language'] = 'typescript' if node['path'].try(:ends_with?, '.ts')
-          node['data-language'] = 'html' if node['path'].try(:ends_with?, '.html')
-          node['data-language'] = 'css' if node['path'].try(:ends_with?, '.css')
-          node['data-language'] = 'js' if node['path'].try(:ends_with?, '.js')
-          node['data-language'] = 'json' if node['path'].try(:ends_with?, '.json')
-          node['data-language'] = node['language'].sub(/\Ats/, 'typescript').strip if node['language']
-          node['data-language'] ||= 'typescript' if node.content.start_with?('@')
+            case version
+        when CURRENT_SETUP_VERSION
+          # We're already good, at the latest version.
+        when '1.1'
+          # We need to update our directory structure
+          upgrade_home_path_v1_1
     
-          # Valid client connection?
-      if (client)
-        # Increment the has connection counter
-        self.pending_connections += 1
+      describe '#rm' do
+    context 'when container has been created' do
+      before { allow(subject).to receive(:created?).and_return(true) }
     
-    require 'msf/core/handler/reverse_tcp'
-require 'msf/base/sessions/command_shell'
-require 'msf/base/sessions/command_shell_options'
-    
-        arg = datastore['ARGV0']
-    if arg
-      if arg.length >= 16
-        raise ArgumentError, 'The specified argv[0] must be less than 16 bytes.'
+          it 'does not render html variant when user logged in' do
+        html_variant = create(:html_variant, published: true, approved: true)
+        sign_in user
+        get article.path
+        expect(response.body).not_to include html_variant.html
       end
-      p[192, arg.length] = arg
-    end
     
-      # Run Method for when run command is issued
-  def run
-    unless is_root?
-      fail_with(Failure::BadConfig, 'Insufficient Privileges: must be running as root to dump the hashes')
+      def article_social_image_url(article)
+    image = user_defined_image(article)
+    if image.present?
+      return cl_image_path(image,
+                           type: 'fetch',
+                           width: '1000',
+                           height: '500',
+                           crop: 'imagga_scale',
+                           quality: 'auto',
+                           flags: 'progressive',
+                           fetch_format: 'auto',
+                           sign_url: true)
     end
+    return legacy_article_social_image(article) unless use_new_social_url?(article)
     
-    for segment in macho.segments
-  #next if segment.segname == MachO::LoadCommands::SEGMENT_NAMES[:SEG_PAGEZERO]
-  puts 'segment: #{segment.segname} off: #{segment.offset.to_s(16)} vmaddr: #{segment.vmaddr.to_s(16)} fileoff: #{segment.fileoff.to_s(16)}'
-  for section in segment.sections
-    puts 'section: #{section.sectname} off: #{section.offset.to_s(16)} addr: #{section.addr.to_s(16)} size: #{section.size.to_s(16)}'
-    flat_addr = section.addr - min
-    section_data = data[section.offset, section.size]
-    #file_section = section.offset
-    #puts 'info: #{segment.fileoff.to_s(16)} #{segment.offset.to_s(16)} #{section.size.to_s(16)} #{file_section.to_s(16)}'
-    #puts '?: #{data.size.to_s(16)} #{file_section.to_s(16)}'
-    if section_data
-      puts 'flat_addr: #{flat_addr.to_s(16)} (#{section_data.size.to_s(16)})'
-      if start == -1 or start > flat_addr
-        start = flat_addr
-      end
-      output_data[flat_addr, section_data.size] = section_data
-    end
+      def self.buff!(article_id, text, buffer_profile_id_code, social_service_name = 'twitter', tag_id = nil)
+    buffer_response = send_to_buffer(text, buffer_profile_id_code)
+    create(
+      article_id: article_id,
+      tag_id: tag_id,
+      body_text: text,
+      buffer_profile_id_code: buffer_profile_id_code,
+      social_service_name: social_service_name,
+      buffer_response: buffer_response,
+      status: 'sent_direct',
+    )
   end
-end
-    
-      #
-  # Use a locally stored Kerberos ticket in the current session.
-  #
-  def cmd_kerberos_ticket_use(*args)
-    if args.length != 1
-      print_line('Usage: kerberos_ticket_use ticketpath')
-      return
-    end
-    
-      def initialize(info = {})
-    super(update_info(info,
-      'Name' => 'Windows Persistent Service Installer',
-      'Description'   => %q{
-        This Module will generate and upload an executable to a remote host, next will make it a persistent service.
-        It will create a new service which will start the payload whenever the service is running. Admin or system
-        privilege is required.
-      },
-      'License'       => MSF_LICENSE,
-      'Author'        => [ 'Green-m <greenm.xxoo[at]gmail.com>' ],
-      'Platform'      => [ 'windows' ],
-      'Targets'       => [['Windows', {}]],
-      'SessionTypes'  => [ 'meterpreter', 'shell'],
-      'DefaultTarget'        => 0,
-      'References'           => [
-        [ 'URL', 'https://github.com/rapid7/metasploit-framework/blob/master/external/source/metsvc/src/metsvc.cpp' ]
-      ],
-      'DisclosureDate'=> 'Oct 20 2018'
-    ))
-    
-      gem.add_development_dependency 'danger'
-  gem.add_development_dependency 'mocha'
-  gem.add_development_dependency 'rspec'
-  gem.add_development_dependency 'rubocop', '0.48.1'
-end
-
-    
-      run_vagrant_command(symlinked?(TestApp.current_path, specific_release_path))
-end
-
-    
-          private
-    
-        def tmux_split_command
-      path = if tab.root?
-               '#{Tmuxinator::Config.default_path_option} #{tab.root}'
-             end
-      '#{project.tmux} splitw #{path} -t #{tab.tmux_window_target}'
-    end
-    
-        def _yaml_root
-      File.expand_path(yaml['root']).shellescape if yaml['root']
-    end
-    
-      factory :nameless_window_project, class: Tmuxinator::Project do
-    transient do
-      file { yaml_load('spec/fixtures/nameless_window.yml') }
-    end
-    
-    describe Tmuxinator::Pane do
-  let(:klass) { described_class }
-  let(:instance) { klass.new(index, project, window, *commands) }
-  # let(:index) { 'vim' }
-  # let(:project) { 0 }
-  # let(:tab) { nil }
-  # let(:commands) { nil }
-  let(:index) { 0 }
-  let(:project) { double }
-  let(:window) { double }
-  let(:commands) { ['vim', 'bash'] }
-    
-        desc 'completions [arg1 arg2]', COMMANDS[:completions]
