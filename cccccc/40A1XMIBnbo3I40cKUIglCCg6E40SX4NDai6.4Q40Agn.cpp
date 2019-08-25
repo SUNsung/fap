cece979870d6704f9bd4e0b7483df2b911697e19
SUNsung <weatherius@gmail.com>
2019-08-25 23:40:12 +0300

    Rewrite [40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp] File in: 2019-08-25 23:40:11

--- cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
+++ cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
@@ -1,187 +1,155 @@
 
         
-        void DHTRoutingTable::showBuckets() const
-{
-  /*
-    for(std::deque<std::shared_ptr<DHTBucket> >::const_iterator itr =
-    buckets_.begin(); itr != buckets_.end(); ++itr) {
-    cerr << 'prefix = ' << (*itr)->getPrefixLength() << ', '
-    << 'nodes = ' << (*itr)->countNode() << endl;
-    }
-  */
+        void first(void) {
+  printf('first\n');  // prints
+  emscripten_sleep(1);
+  longjmp(buf, 1);  // jumps back to where setjmp was called - making setjmp now
+                    // return 1
 }
     
-    std::pair<std::vector<std::unique_ptr<Command>>,
-          std::vector<std::unique_ptr<Command>>>
-DHTSetup::setup(DownloadEngine* e, int family)
-{
-  std::vector<std::unique_ptr<Command>> tempCommands;
-  std::vector<std::unique_ptr<Command>> tempRoutineCommands;
-  if ((family != AF_INET && family != AF_INET6) ||
-      (family == AF_INET && DHTRegistry::isInitialized()) ||
-      (family == AF_INET6 && DHTRegistry::isInitialized6())) {
-    return {};
-  }
-  try {
-    // load routing table and localnode id here
-    std::shared_ptr<DHTNode> localNode;
-    }
-    }
+      // Does this device use functionShadedFill(), axialShadedFill(), and
+  // radialShadedFill()?  If this returns false, these shaded fills
+  // will be reduced to a series of other drawing operations.
+  virtual GBool useShadedFills(int type)
+    { return type < 4 && level >= psLevel2; }
     
-    class DHTSetup {
+    //------------------------------------------------------------------------
+    
+      parse(tree);
+    
+    
+    {private:
+  
+  PageTransitionType type;           // transition style
+  int duration;                      // duration of the effect in seconds
+  PageTransitionAlignment alignment; // dimension of the effect
+  PageTransitionDirection direction; // direction of motion
+  int angle;                         // direction in degrees
+  double scale;                      // scale
+  GBool rectangular;                 // is the area to be flown in rectangular?
+  GBool ok;                          // set if created successfully
+};
+    
+      // get stream start position
+  lexer->skipToNextLine();
+  pos = lexer->getPos();
+    
+    class ProfileData {
 public:
-  DHTSetup();
     }
     
-    DHTTokenTracker::~DHTTokenTracker() = default;
+        bool bFound = false;
     
-    DHTUnknownMessage::DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
-                                     const unsigned char* data, size_t length,
-                                     const std::string& ipaddr, uint16_t port)
-    : DHTMessage(localNode, std::shared_ptr<DHTNode>()),
-      length_(length),
-      ipaddr_(ipaddr),
-      port_(port)
-{
-  if (length_ == 0) {
-    data_ = nullptr;
-  }
-  else {
-    data_ = new unsigned char[length];
-    memcpy(data_, data, length);
-  }
+    
+    {    static AAssetManager* assetmanager;
+    static ZipFile* obbfile;
+};
+    
+    #include <string>
+    
+    
+    {    JS::RootedObject textureData(cx, jsb_cocostudio_TextureData_prototype);
+    JS_DefineProperty(cx, textureData, 'contourDataList', JS::UndefinedHandleValue, JSPROP_ENUMERATE | JSPROP_PERMANENT | JSPROP_SHARED, js_get_TextureData_contourDataList, js_set_TextureData_contourDataList);
+    JS_DefineProperty(cx, textureData, 'name', JS::UndefinedHandleValue, JSPROP_ENUMERATE | JSPROP_PERMANENT | JSPROP_SHARED, js_get_TextureData_name, js_set_TextureData_name);
+    JS_DefineProperty(cx, textureData, 'width', JS::UndefinedHandleValue, JSPROP_ENUMERATE | JSPROP_PERMANENT | JSPROP_SHARED, js_get_TextureData_width, js_set_TextureData_width);
+    JS_DefineProperty(cx, textureData, 'height', JS::UndefinedHandleValue, JSPROP_ENUMERATE | JSPROP_PERMANENT | JSPROP_SHARED, js_get_TextureData_height, js_set_TextureData_height);
+    JS_DefineProperty(cx, textureData, 'pivotX', JS::UndefinedHandleValue, JSPROP_ENUMERATE | JSPROP_PERMANENT | JSPROP_SHARED, js_get_TextureData_pivotX, js_set_TextureData_pivotX);
+    JS_DefineProperty(cx, textureData, 'pivotY', JS::UndefinedHandleValue, JSPROP_ENUMERATE | JSPROP_PERMANENT | JSPROP_SHARED, js_get_TextureData_pivotY, js_set_TextureData_pivotY);
+}
+
+    
+    
+    {    if (argc == 0) {
+        JS::RootedObject tmp(cx, JS_NewObject(cx, NULL, JS::NullPtr(), JS::NullPtr()));
+        if (!tmp) return false;
+        ui::Margin margin = cobj->getMargin();
+        bool ok = JS_DefineProperty(cx, tmp, 'left', margin.left, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
+            JS_DefineProperty(cx, tmp, 'top', margin.top, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
+            JS_DefineProperty(cx, tmp, 'right', margin.right, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
+            JS_DefineProperty(cx, tmp, 'bottom', margin.bottom, JSPROP_ENUMERATE | JSPROP_PERMANENT);
+        if (ok) 
+        {
+            args.rval().set(OBJECT_TO_JSVAL(tmp));
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+    }
+    JS_ReportError(cx, 'Invalid number of arguments');
+    return false;
 }
     
-    #include <gtest/gtest.h>
     
-    /*
- * Visitor for rewriting Vreg uses, replacing them with the expressions for
- * their defs.
- */
-struct OptVisit {
-  const Env& env;
-  const RegState& state;
-    }
-    
-    int DecodedInstruction::decodeOpcode(uint8_t* ip) {
-  int sz = 1;
-  if (*ip == 0x0f) {
-    ++ip;
-    ++sz;
-    m_map_select = 1;
-    if (*ip == 0x38) {
-      ++ip;
-      ++sz;
-      m_map_select = 2;
-    } else if (*ip == 0x3a) {
-      ++ip;
-      ++sz;
-      m_map_select = 3;
-    }
-  }
-    }
-    
-    bool OutputFile::flush() {
-  if (!isClosed()) {
-    g_context->flush();
-    return true;
-  }
-  return false;
+    {
 }
     
-    bool Pipe::closeImpl() {
-  bool ret = true;
-  *s_pcloseRet = 0;
-  if (valid() && !isClosed()) {
-    assertx(m_stream);
-#ifdef _MSC_VER
-    int pcloseRet = _pclose(m_stream);
-#else
-    int pcloseRet = LightProcess::pclose(m_stream);
-    if (WIFEXITED(pcloseRet)) pcloseRet = WEXITSTATUS(pcloseRet);
-#endif
-    *s_pcloseRet = pcloseRet;
-    ret = (pcloseRet == 0);
-    setIsClosed(true);
-    m_stream = nullptr;
-  }
-  File::closeImpl();
-  return ret;
-}
-    
-    bool HHVM_FUNCTION(stream_context_set_option,
-                   const Variant& stream_or_context,
-                   const Variant& wrapper,
-                   const Variant& option = uninit_variant,
-                   const Variant& value = uninit_variant);
+        /** Render the scene.
+     * @param renderer The renderer use to render the scene.
+     * @param eyeTransforms The AdditionalTransform List of camera of multiView.
+     * @param eyeProjections The projection matrix List of camera of multiView.
+     * @param multiViewCount The number of multiView.
+     * @js NA
+     */
+    virtual void render(Renderer* renderer, const Mat4* eyeTransforms, const Mat4* eyeProjections, unsigned int multiViewCount);
     
     
-    {  {
-    Array arr = make_map_array(0, 'a', 1, 'b');
-    EXPECT_TRUE(arr->isVectorData());
-  }
-  {
-    Array arr = make_map_array(1, 'a', 0, 'b');
-    EXPECT_TRUE(!arr->isVectorData());
-  }
-  {
-    Array arr = make_map_array(1, 'a', 2, 'b');
-    EXPECT_TRUE(!arr->isVectorData());
-  }
-  {
-    Array arr = make_map_array(1, 'a');
-    arr.set(0, 'b');
-    EXPECT_TRUE(!arr->isVectorData());
-  }
-}
+
     
-    void processInit() {
-  TRACE(1, 'mcgen startup\n');
-    }
+        // Overrides
+    virtual void startWithTarget(Node *target) override;
+    virtual ActionCamera * reverse() const override;
+    virtual ActionCamera *clone() const override;
     
-    using apollo::common::Status;
-using apollo::dreamview::Chart;
-using apollo::planning_internal::SLFrameDebug;
-using apollo::planning_internal::SpeedPlan;
-using apollo::planning_internal::STGraphDebug;
-    
-    #include 'cyber/binary.h'
-#include 'glog/logging.h'
-#include 'glog/raw_logging.h'
-    
-    namespace apollo {
-namespace planning {
-namespace scenario {
-namespace valet_parking {
-    }
-    }
+    namespace v8 {
+namespace internal {
     }
     }
     
     
-    {  for (const auto& stage_config : config_.stage_config()) {
-    stage_config_map_[stage_config.stage_type()] = &stage_config;
-  }
-  for (int i = 0; i < config_.stage_type_size(); ++i) {
-    auto stage_type = config_.stage_type(i);
-    CHECK(common::util::ContainsKey(stage_config_map_, stage_type))
-        << 'stage type : ' << ScenarioConfig::StageType_Name(stage_type)
-        << ' has no config';
-  }
-  ADEBUG << 'init stage '
-         << ScenarioConfig::StageType_Name(config_.stage_type(0));
-  current_stage_ = CreateStage(*stage_config_map_[config_.stage_type(0)]);
-}
+    {  // Pages that are only made iterable but have their free lists ignored.
+  IterabilityList iterability_list_;
+  CancelableTaskManager::Id iterability_task_id_;
+  base::Semaphore iterability_task_semaphore_;
+  bool iterability_in_progress_;
+  bool iterability_task_started_;
+  bool should_reduce_memory_;
+};
     
-    #pragma once
+    class EntryFrameConstants : public AllStatic {
+ public:
+  // This is the offset to where JSEntry pushes the current value of
+  // Isolate::c_entry_fp onto the stack.
+  static constexpr int kCallerFPOffset = -6 * kSystemPointerSize;
+    }
     
-    #ifndef CYBER_BASE_ATOMIC_HASH_MAP_H_
-#define CYBER_BASE_ATOMIC_HASH_MAP_H_
-    
-    Status ControllerAgent::Reset() {
-  for (auto &controller : controller_list_) {
-    ADEBUG << 'controller:' << controller->Name() << ' reset...';
-    controller->Reset();
+      if (RootsTable::IsImmortalImmovable(index)) {
+    Handle<Object> object = isolate()->root_handle(index);
+    if (object->IsSmi()) {
+      mov(destination, Immediate(Smi::cast(*object)));
+      return;
+    } else {
+      DCHECK(object->IsHeapObject());
+      mov(destination, Handle<HeapObject>::cast(object));
+      return;
+    }
   }
-  return Status::OK();
-}
+    
+        {
+      Comment('perform smi operation');
+      // If rhs is known to be an Smi we want to fast path Smi operation. This
+      // is for AddSmi operation. For the normal Add operation, we want to fast
+      // path both Smi and Number operations, so this path should not be marked
+      // as Deferred.
+      Label if_overflow(this,
+                        rhs_is_smi ? Label::kDeferred : Label::kNonDeferred);
+      TNode<Smi> smi_result = TrySmiAdd(CAST(lhs), CAST(rhs), &if_overflow);
+      // Not overflowed.
+      {
+        var_type_feedback.Bind(
+            SmiConstant(BinaryOperationFeedback::kSignedSmall));
+        var_result.Bind(smi_result);
+        Goto(&end);
+      }
+    }
