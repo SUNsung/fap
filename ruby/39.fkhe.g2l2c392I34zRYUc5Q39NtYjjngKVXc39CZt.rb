2af51c4a58a8b64a6d23414dc37111ae3355c830
SUNsung <weatherius@gmail.com>
2019-08-25 15:58:12 +0300

    Rewrite [39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb] File in: 2019-08-25 15:58:12

--- ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
+++ ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
@@ -1,64 +1,106 @@
 
         
-        Dir.glob('rake/**.rake').each { |f| import f }
+            SPLIT_INTS = /(?<=\d)\.(?=[\s\d])/.freeze
     
-    DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
-    
-    STDOUT.sync = true
-    
-        def process(args)
-      arg_is_present? args, '--server', 'The --server command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
-                          use the 'build' subcommand.'
-      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
-                          '--watch'.'
-      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
-                          the '--watch' switch.'
-      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
-                          favour of 'highlighter'.'
-      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
-                          your config files.'
-      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
-                          config files.'
-      no_subcommand(args)
+        def at_css(*args)
+      doc.at_css(*args)
     end
     
-      # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
-    
-      # set version
-  content.sub! /(s\.version.*=\s+).*/, '\\1\'#{Rack::Protection::VERSION}\''
-    
-          def react(env)
-        result = send(options[:reaction], env)
-        result if Array === result and result.size == 3
+        def filter_const(name)
+      if name.is_a? Array
+        name.map &method(:filter_const)
+      else
+        Docs.const_get '#{name}_filter'.camelize
       end
-    
-        def URIAddEncodedOctetToBuffer(octet, result, index)
-      result[index] = 37; # Char code of '%'.
-      index         += 1
-      result[index] = @@hexCharCodeArray[octet >> 4];
-      index         += 1
-      result[index] = @@hexCharCodeArray[octet & 0x0F];
-      index += 1
-      return index;
     end
     
-          def has_header
-        @header = (@page.header || false) if @header.nil? && @page
-        !!@header
-      end
+        def initialize
+      @pages = {}
+    end
     
-          def noindex
-        @version ? true : false
-      end
+        def mime_type
+      headers['Content-Type'] || 'text/plain'
+    end
     
-    module Precious
-  module Helpers
+    module Docs
+  class Scraper < Doc
+    class << self
+      attr_accessor :base_url, :root_path, :initial_paths, :options, :html_filters, :text_filters, :stubs
     
-      def new
-    @broadcast = Broadcast.new
+      def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
+    error.to_s.humanize if error
   end
+    
+      # Helper for use after calling send_*_instructions methods on a resource.
+  # If we are in paranoid mode, we always act as if the resource was valid
+  # and instructions were sent.
+  def successfully_sent?(resource)
+    notice = if Devise.paranoid
+      resource.errors.clear
+      :send_paranoid_instructions
+    elsif resource.errors.empty?
+      :send_instructions
+    end
+    
+            routes.each do |module_name, actions|
+          [:path, :url].each do |path_or_url|
+            actions.each do |action|
+              action = action ? '#{action}_' : ''
+              method = :'#{action}#{module_name}_#{path_or_url}'
+    
+          attr_reader :scope_name, :resource
+    
+          File.open('bower.json', 'w') do |f|
+        f.puts JSON.pretty_generate(spec)
+      end
+    end
+  end
+end
+
+    
+      def test_font_helper_without_suffix
+    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
+  end
+    
+        context 'when iteration is nil' do
+      before do
+        subject.iteration = nil
+      end
+    
+      describe '#maintainer' do
+    require 'socket'
+    default_maintainer = '<#{ENV['USER']}@#{Socket.gethostname}>'
+    it_behaves_like :Default, description.gsub(/^#/, '').to_sym, default_maintainer
+    it_behaves_like :Mutator, description.gsub(/^#/, '').to_sym
+  end
+    
+      private
+    
+      # Take a flat package as input
+  def input(input_path)
+    # TODO: Fail if it's a Distribution pkg or old-fashioned
+    expand_dir = File.join(build_path, 'expand')
+    # expand_dir must not already exist for pkgutil --expand
+    safesystem('pkgutil --expand #{input_path} #{expand_dir}')
+    
+      option '--user', 'USER',
+    'Set the user to USER in the prototype files.',
+    :default => 'root'
+    
+          File.open(File.join(builddir, 'manifests', manifest), 'w') do |f|
+        logger.info('manifest: #{f.path}')
+        template = template(File.join('puppet', '#{manifest}.erb'))
+        ::Dir.chdir(fileroot) do
+          f.puts template.result(binding)
+        end
+      end
+    end
+  end # def generate_specfile
+    
+    # TODO(sissel): Add dependency checking support.
+# IIRC this has to be done as a 'checkinstall' step.
+class FPM::Package::Solaris < FPM::Package
