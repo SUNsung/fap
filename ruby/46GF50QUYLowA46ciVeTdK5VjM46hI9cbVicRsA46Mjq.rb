19a0543a3f480af2ece86777d191cda5f4c46a80
SUNsung <weatherius@gmail.com>
2019-08-25 23:09:43 +0300

    Rewrite [46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb] File in: 2019-08-25 23:09:43

--- ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
+++ ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
@@ -1,89 +1,44 @@
 
         
-              if @email_domain_block.save
-        log_action :create, @email_domain_block
-        redirect_to admin_email_domain_blocks_path, notice: I18n.t('admin.email_domain_blocks.created_msg')
-      else
-        render :new
-      end
-    end
-    
-    module Admin
-  class ReportNotesController < BaseController
-    before_action :set_report_note, only: [:destroy]
-    
-      def after_sign_in_path_for(resource)
-    if resource.email_verified?
-      root_path
-    else
-      finish_signup_path
+              UpOnlyMigration.new.migrate(:down) # should be no error
+      connection = ActiveRecord::Base.connection
+      assert_not connection.column_exists?(:horses, :oldie)
+      Horse.reset_column_information
     end
   end
 end
 
     
-      included do
-    before_action :set_session_activity
+      def test_invalid_using_multiple_contexts
+    r = WrongReply.new(title: 'Wrong Create')
+    assert r.invalid?([:special_case, :create])
+    assert_equal 'Invalid', r.errors[:author_name].join
+    assert_equal 'is Wrong Create', r.errors[:title].join
   end
     
-          def title
-        'Create a new page'
-      end
-    
+        class AbstractHelpers < ControllerWithHelpers
+      helper(HelperyTest) do
+        def helpery_test
+          'World'
         end
-  end
-end
-
-    
-    module Gollum
-end
-Gollum::GIT_ADAPTER = ENV['GIT_ADAPTER'] if ENV['GIT_ADAPTER']
-    
-      test 'create sets the correct path for a relative path subdirectory with the page file directory set' do
-    Precious::App.set(:wiki_options, { :page_file_dir => 'foo' })
-    dir  = 'bardir'
-    name = '#{dir}/baz'
-    get '/create/foo/#{name}'
-    assert_match(/\/#{dir}/, last_response.body)
-    assert_no_match(/[^\/]#{dir}/, last_response.body)
-    # reset page_file_dir
-    Precious::App.set(:wiki_options, { :page_file_dir => nil })
-  end
-    
-      test 'extracting paths from URLs' do
-    assert_nil extract_path('Eye-Of-Sauron')
-    assert_equal 'Mordor', extract_path('Mordor/Sauron')
-    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
-  end
-    
-    def version
-  line = File.read('lib/#{name}.rb')[/^\s*VERSION\s*=\s*.*/]
-  line.match(/.*VERSION\s*=\s*[''](.*)['']/)[1]
-end
-    
-    exec         = {}
-options      = {
-  :port => 4567,
-  :bind => '0.0.0.0',
-}
-wiki_options = {
-  :live_preview  => false,
-  :allow_uploads => false,
-  :allow_editing => true,
-}
-    
-        # Ensure path begins with a single leading slash
-    def clean_path(path)
-      if path
-        (path[0] != '/' ? path.insert(0, '/') : path).gsub(/\/{2,}/, '/')
       end
+    
+        # any errors will be logged to $stderr by invoke!
+    # Bundler cannot update and clean gems in one operation so we have to call the CLI twice.
+    options = {:update => plugins, :rubygems_source => gemfile.gemset.sources}
+    options[:local] = true if local?
+    output = LogStash::Bundler.invoke!(options)
+    # We currently dont removed unused gems from the logstash installation
+    # see: https://github.com/elastic/logstash/issues/6339
+    # output = LogStash::Bundler.invoke!(:clean => true)
+    display_updated_plugins(previous_gem_specs_map)
+  rescue => exception
+    gemfile.restore!
+    report_exception('Updated Aborted', exception)
+  ensure
+    display_bundler_output(output)
+  end
+    
+        after :each do
+      logstash.uninstall
     end
-    
-      puts '\n== Restarting application server =='
-  system! 'bin/rails restart'
-end
-
-    
-      # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
