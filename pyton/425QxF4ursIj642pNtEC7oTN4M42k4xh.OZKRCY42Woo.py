8dd016c0358252fecc36398fd550a13bc801976a
SUNsung <weatherius@gmail.com>
2019-08-25 03:25:17 +0300

    Rewrite [425QxF4ursIj642pNtEC7oTN4M42k4xh.OZKRCY42Woo.py] File in: 2019-08-25 03:25:17

--- pyton/425QxF4ursIj642pNtEC7oTN4M42k4xh.OZKRCY42Woo.py
+++ pyton/425QxF4ursIj642pNtEC7oTN4M42k4xh.OZKRCY42Woo.py
@@ -1,98 +1,199 @@
 
         
-                vid = int(video_id)
-        video_data = list(filter(
-            lambda episode: episode.get('video_id') == vid, response['data']))[0]
-        formats = []
+            scikit_regressor_results.append(
+        delta.seconds + delta.microseconds / mu_second)
     
-            formats = [
-            {'url': format_url}
-            for format_url in orderedSet(format_urls)]
+        for line in input_file:
+        linestrip = line.strip()
+        if len(linestrip) == 0:
+            in_exercise_region = False
+        elif linestrip.startswith('# TASK:'):
+            in_exercise_region = True
     
+    In both examples below, the main result is that the empirical covariance
+estimate, as a non-robust one, is highly influenced by the heterogeneous
+structure of the observations. Although the robust covariance estimate is
+able to focus on the main mode of the data distribution, it sticks to the
+assumption that the data should be Gaussian distributed, yielding some biased
+estimation of the data structure, but yet accurate to some extent.
+The One-Class SVM does not assume any parametric form of the data distribution
+and can therefore model the complex shape of the data much better.
     
-class SouthParkIE(MTVServicesInfoExtractor):
-    IE_NAME = 'southpark.cc.com'
-    _VALID_URL = r'https?://(?:www\.)?(?P<url>southpark\.cc\.com/(?:clips|(?:full-)?episodes|collections)/(?P<id>.+?)(\?|#|$))'
+    plt.matshow(fit_data, cmap=plt.cm.Blues)
+plt.title('After biclustering; rearranged to show biclusters')
     
-    filenames = {
-    'bin': 'youtube-dl',
-    'exe': 'youtube-dl.exe',
-    'tar': 'youtube-dl-%s.tar.gz' % version}
-build_dir = os.path.join('..', '..', 'build', version)
-for key, filename in filenames.items():
-    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
-    fn = os.path.join(build_dir, filename)
-    with open(fn, 'rb') as f:
-        data = f.read()
-    if not data:
-        raise ValueError('File %s is empty!' % fn)
-    sha256sum = hashlib.sha256(data).hexdigest()
-    new_version[key] = (url, sha256sum)
+    X_restored = agglo.inverse_transform(X_reduced)
+images_restored = np.reshape(X_restored, images.shape)
+plt.figure(1, figsize=(4, 3.5))
+plt.clf()
+plt.subplots_adjust(left=.01, right=.99, bottom=.01, top=.91)
+for i in range(4):
+    plt.subplot(3, 4, i + 1)
+    plt.imshow(images[i], cmap=plt.cm.gray, vmax=16, interpolation='nearest')
+    plt.xticks(())
+    plt.yticks(())
+    if i == 1:
+        plt.title('Original data')
+    plt.subplot(3, 4, 4 + i + 1)
+    plt.imshow(images_restored[i], cmap=plt.cm.gray, vmax=16,
+               interpolation='nearest')
+    if i == 1:
+        plt.title('Agglomerated data')
+    plt.xticks(())
+    plt.yticks(())
     
-    import io
-import sys
-import re
+    - single linkage is fast, and can perform well on
+  non-globular data, but it performs poorly in the
+  presence of noise.
+- average and complete linkage perform well on
+  cleanly separated globular clusters, but have mixed
+  results otherwise.
+- Ward is the most effective method for noisy data.
     
-            # Display results overlaid on an image
-        show_prediction_labels_on_image(os.path.join('knn_examples/test', image_file), predictions)
-
+    Some of the clusters learned without connectivity constraints do not
+respect the structure of the swiss roll and extend across different folds of
+the manifolds. On the opposite, when opposing connectivity constraints,
+the clusters form a nice parcellation of the swiss roll.
+'''
     
-    
-def print_result(filename, location):
-    top, right, bottom, left = location
-    print('{},{},{},{},{}'.format(filename, top, right, bottom, left))
-    
-    print('I found {} face(s) in this photograph.'.format(len(face_landmarks_list)))
-    
-        # 将每一个人脸与已知样本图片比对
-    for face_encoding in face_encodings:
-        # 看是否属于奥巴马或者拜登
-        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
-        name = '<Unknown Person>'
-    
-        # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-    
-    # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
-# See http://pillow.readthedocs.io/ for more about PIL/Pillow
-pil_image = Image.fromarray(unknown_image)
-# Create a Pillow ImageDraw Draw instance to draw with
-draw = ImageDraw.Draw(pil_image)
-    
-    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-    
-def softsign(x):
-    '''
-    o = x / (1 + abs(x))
-    '''
-    return tf.nn.softsign(x)
+    plt.subplot(223)
+plt.scatter(Y_train_r[:, 0], Y_train_r[:, 1], label='train',
+            marker='*', c='b', s=50)
+plt.scatter(Y_test_r[:, 0], Y_test_r[:, 1], label='test',
+            marker='*', c='r', s=50)
+plt.xlabel('Y comp. 1')
+plt.ylabel('Y comp. 2')
+plt.title('Y comp. 1 vs Y comp. 2 , (test corr = %.2f)'
+          % np.corrcoef(Y_test_r[:, 0], Y_test_r[:, 1])[0, 1])
+plt.legend(loc='best')
+plt.xticks(())
+plt.yticks(())
+plt.show()
     
     
-def relu6(x):
-    '''
-    `o = min(max(x, 0), 6)`
-    '''
-    return tf.nn.relu6(x)
+class RC4FileObject(object):
+    '''fileobj for rc4'''
+    def __init__(self, stream, key):
+        self.__stream = stream
+        self.__cipher = _Crypto_Cipher_ARC4_new(key) if key else lambda x:x
+    def __getattr__(self, attr):
+        if attr not in ('__stream', '__cipher'):
+            return getattr(self.__stream, attr)
+    def read(self, size=-1):
+        return self.__cipher.encrypt(self.__stream.read(size))
     
-        Args:
-        x(tf.Tensor):
-        kernel_size(int or list of int):
-        out_channels(int):
-        act_fn(function):
-        strides(int or list of int):
-        padding(str):
-        name(str):
-        reuse(bool):
+    # begin[licence]
+#
+# [The 'BSD licence']
+# Copyright (c) 2005-2008 Terence Parr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# end[licence]
     
-            # h_tilde(h~): question to context attended query vectors
-        b = tf.reduce_max(S, axis=2)  # [N, T]
-        b = softmax(b, axis=-1)  # [N, T]
-        b = tf.expand_dims(b, axis=1)  # [N, 1, T]
-        h_tilde = tf.matmul(b, h)  # [N, 1, d]
-        h_tilde = tf.tile(h_tilde, [1, T, 1])  # [N, T, d]
+    
+def main():
+    test = TestSelectionSort()
+    selection_sort = SelectionSort()
+    test.test_selection_sort(selection_sort.sort)
+    try:
+        test.test_selection_sort(selection_sort.sort_recursive)
+        test.test_selection_sort(selection_sort.sor_iterative_alt)
+    except NameError:
+        # Alternate solutions are only defined
+        # in the solutions file
+        pass
+    
+            print('Success: test_end_to_end')
+    
+        def extract_min(self):
+        if not self.array:
+            return None
+        minimum = sys.maxsize
+        for index, node in enumerate(self.array):
+            if node.key < minimum:
+                minimum = node.key
+                minimum_index = index
+        return self.array.pop(minimum_index)
+    
+            return self.__folder_list
+    
+            # Optionals configuration keys
+        # N/A
+    
+            # If the duration is < 0 (update + export time > refresh_time)
+        # Then display the interface and log a message
+        if duration <= 0:
+            logger.warning('Update and export time higher than refresh_time.')
+            duration = 0.1
+    
+    AMP (Application Monitoring Process)
+A Glances AMP is a Python script called (every *refresh* seconds) if:
+- the AMP is *enabled* in the Glances configuration file
+- a process is running (match the *regex* define in the configuration file)
+The script should define a Amp (GlancesAmp) class with, at least, an update method.
+The update method should call the set_result method to set the AMP return string.
+The return string is a string with one or more line (\n between lines).
+If the *one_line* var is true then the AMP will be displayed in one line.
+'''
+    
+            for p in plist.json():
+            print('HTTP RESTful request: %s/%s/views' % (URL, p))
+            req = self.http_get('%s/%s/views' % (URL, p))
+            self.assertTrue(req.ok)
+            self.assertIsInstance(req.json(), dict)
+    
+        def __init__(self, args, config):
+        '''Init the Outdated class'''
+        self.args = args
+        self.config = config
+        self.cache_dir = user_cache_dir()
+        self.cache_file = os.path.join(self.cache_dir, 'glances-version.db')
+    
+        def history_value(self, pos=1):
+        '''Return the value in position pos in the history.
+        Default is to return the latest value added to the history.
+        '''
+        return self._history[-pos]
+    
+    try:
+    from zeroconf import (
+        __version__ as __zeroconf_version,
+        ServiceBrowser,
+        ServiceInfo,
+        Zeroconf
+    )
+    zeroconf_tag = True
+except ImportError:
+    zeroconf_tag = False
+    
+        def __init__(self):
+        '''Init the events class.'''
+        # Maximum size of the events list
+        self.events_max = 10
+    
+    import sys
+    
+            # Mandatories configuration keys (additional to host and port)
+        # N/A
