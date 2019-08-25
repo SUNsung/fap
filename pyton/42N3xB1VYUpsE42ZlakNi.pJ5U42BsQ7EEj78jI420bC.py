d80d17ca51611bf41559e67283f91827e0d4a652
SUNsung <weatherius@gmail.com>
2019-08-25 08:01:02 +0300

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2019-08-25 08:01:01

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,90 +1,118 @@
 
         
         
-class LinkedList(object):
+def get_db():
+    '''Connect to the application's configured database. The connection
+    is unique for each request and will be reused if this is called
+    again.
+    '''
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
     
-    from enum import Enum
+        monkeypatch.setattr(werkzeug.serving, 'run_simple', run_simple_mock)
+    app.run()
+    assert rv['result'] == 'running...'
     
-    
-class HitCounts(MRJob):
-    
-        def __init__(self):
-        self.lookup = {}  # key: person_id, value: person_server
-    
-        def adjust_request_args(self, args):
-        args['url'] = self.args[0]
-        return args
-    
-        class _v20_S3Connection(S3Connection):
-        '''A dummy S3Connection wrapper that doesn't do any synchronous download'''
-        def _mexe(self, http_request, *args, **kwargs):
-            http_request.authorize(connection=self)
-            return http_request.headers
-    
-        def __init__(self, headers):
-        self._headers = headers
-    
-        # The font size ('10pt', '11pt' or '12pt').
-    #'pointsize': '10pt',
-    
-    Algorithm Description:
-The knn classifier is first trained on a set of labeled (known) faces and can then predict the person
-in an unknown image by finding the k most similar faces (images with closet face-features under eucledian distance)
-in its training set, and performing a majority vote (possibly weighted) on their label.
+            @app.route('/dict')
+        def return_dict():
+            return flask.jsonify(d)
     
     
-def print_result(filename, location):
-    top, right, bottom, left = location
-    print('{},{},{},{},{}'.format(filename, top, right, bottom, left))
-    
-    # This is a super simple (but slow) example of running face recognition on live video from your webcam.
-# There's a second example that's a little more complicated but runs faster.
-    
-    # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file('biden.jpg')
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+  def ShouldUseNowInner( self, request_data ):
+    if request_data[ 'force_semantic' ]:
+      return True
+    disabled_filetypes = self.user_options[
+      'filetype_specific_completion_to_disable' ]
+    if not vimsupport.CurrentFiletypesEnabled( disabled_filetypes ):
+      return False
+    return super( OmniCompleter, self ).ShouldUseNowInner( request_data )
     
     
-  def ShouldUseCache( self ):
-    return bool( self.user_options[ 'cache_omnifunc' ] )
-    
-      current_buffer = VimBuffer( 'buffer',
-                              contents = [ '†åsty_π.ππ' ],
-                              filetype = FILETYPE,
-                              omnifunc = Omnifunc )
-    
-            # When there are multiple fixit suggestions, present them as a list to
-        # the user hand have her choose which one to apply.
-        if len( self._response[ 'fixits' ] ) > 1:
-          fixit_index = vimsupport.SelectFromList(
-            'Multiple FixIt suggestions are available at this location. '
-            'Which one would you like to apply?',
-            [ fixit[ 'text' ] for fixit in self._response[ 'fixits' ] ] )
+  def _HandleGotoResponse( self, modifiers ):
+    if isinstance( self._response, list ):
+      vimsupport.SetQuickFixList(
+        [ _BuildQfListItem( x ) for x in self._response ] )
+      vimsupport.OpenQuickFixList( focus = True, autoclose = True )
+    else:
+      vimsupport.JumpToLocation( self._response[ 'filepath' ],
+                                 self._response[ 'line_num' ],
+                                 self._response[ 'column_num' ],
+                                 modifiers,
+                                 self._buffer_command )
     
     
-@contextlib.contextmanager
-def UserOptions( options ):
-  old_vim_options = test_utils.VIM_OPTIONS.copy()
-  test_utils.VIM_OPTIONS.update( DEFAULT_CLIENT_OPTIONS )
-  test_utils.VIM_OPTIONS.update( options )
-  try:
-    yield
-  finally:
-    test_utils.VIM_OPTIONS = old_vim_options
+class DiagnosticInterface( object ):
+  def __init__( self, bufnr, user_options ):
+    self._bufnr = bufnr
+    self._user_options = user_options
+    self._diagnostics = []
+    self._diag_filter = DiagnosticFilter.CreateFromOptions( user_options )
+    # Line and column numbers are 1-based
+    self._line_to_diags = defaultdict( list )
+    self._previous_diag_line_number = -1
+    self._diag_message_needs_clearing = False
     
     
-def SendShutdownRequest():
-  request = ShutdownRequest()
-  # This is a blocking call.
-  request.Start()
+def SendEventNotificationAsync( event_name,
+                                buffer_number = None,
+                                extra_data = None ):
+  event = EventNotification( event_name, buffer_number, extra_data )
+  event.Start()
 
     
     
-def _assert_accepts( filter, text ):
-  _assert_accept_equals( filter, text, True )
+class MessagesPoll( BaseRequest ):
+  def __init__( self ):
+    super( MessagesPoll, self ).__init__()
+    self._request_data = BuildRequestData()
+    self._response_future = None
     
-        # For pickling to work, the __module__ variable needs to be set to the frame
-    # where the named tuple is created.  Bypass this step in enviroments where
-    # sys._getframe is not defined (Jython for example).
-    if hasattr(_sys, '_getframe'):
-        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+      # Ignore 'syntax match' lines (see ':h syn-match').
+  if line.startswith( 'match ' ):
+    return []
+    
+    
+def FormatDebugInfoResponse_NoExtraConf_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'extra_conf' ].update( {
+    'is_loaded': False,
+    'path': None
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'No extra configuration file found\n'
+    )
+  )
+    
+    
+def HandlePollResponse_MultipleDiagnostics_test():
+  diagnostics_handler = ExtendedMock()
+  messages = [
+    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER1' ] },
+    { 'filepath': 'bar', 'diagnostics': [ 'PLACEHOLDER2' ] },
+    { 'filepath': 'baz', 'diagnostics': [ 'PLACEHOLDER3' ] },
+    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER4' ] },
+  ]
+  assert_that( _HandlePollResponse( messages, diagnostics_handler ),
+               equal_to( True ) )
+  diagnostics_handler.UpdateWithNewDiagnosticsForFile.assert_has_exact_calls( [
+    call( 'foo', [ 'PLACEHOLDER1' ] ),
+    call( 'bar', [ 'PLACEHOLDER2' ] ),
+    call( 'baz', [ 'PLACEHOLDER3' ] ),
+    call( 'foo', [ 'PLACEHOLDER4' ] )
+  ] )
+    
+      _assert_rejects( f, 'This is a Taco' )
+  _assert_rejects( f, 'This is a Burrito' )
+    
+    
+def KeywordsFromSyntaxListOutput_ContainedArgAllowed_test():
+  assert_that( syntax_parse._KeywordsFromSyntaxListOutput( '''
+phpFunctions   xxx contained gzclose yaz_syntax html_entity_decode fbsql_read_blob png2wbmp mssql_init cpdf_set_title gztell fbsql_insert_id empty cpdf_restore mysql_field_type closelog swftext ldap_search curl_errno gmp_div_r mssql_data_seek getmyinode printer_draw_pie mcve_initconn ncurses_getmaxyx defined
+                   contained replace_child has_attributes specified insertdocument assign node_name hwstat addshape get_attribute_node html_dump_mem userlist
+                   links to Function''' ), # noqa
+              has_items( 'gzclose', 'userlist', 'ldap_search' ) )
