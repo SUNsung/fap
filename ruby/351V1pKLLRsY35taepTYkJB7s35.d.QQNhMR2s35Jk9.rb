02658791c1c4dec3a5075a81218196e3dc6882db
SUNsung <weatherius@gmail.com>
2019-08-26 02:52:13 +0300

    Rewrite [351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb] File in: 2019-08-26 02:52:12

--- ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
+++ ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
@@ -1,122 +1,133 @@
 
         
-          # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-    
-      # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
-    
-          # @return [Xcodeproj::Project] The Pods Xcode project.
-      #
-      attr_reader :pods_project
-    
-              # Generates the contents of the xcconfig file and saves it to disk.
-          #
-          # @param  [PBXNativeTarget] native_target
-          #         the native target to link the module map file into.
-          #
-          # @return [void]
-          #
-          def create_xcconfig_file(native_target)
-            native_target.build_configurations.each do |configuration|
-              next unless target.user_build_configurations.key?(configuration.name)
-              path = target.xcconfig_path(configuration.name)
-              build_settings = target.build_settings(configuration.name)
-              update_changed_file(build_settings, path)
-              target.xcconfigs[configuration.name] = build_settings.xcconfig
-              xcconfig_file_ref = add_file_to_support_group(path)
-              configuration.base_configuration_reference = xcconfig_file_ref
-            end
-          end
-    
-            it 'allows specifying linkage' do
-          BuildType.new(:linkage => :dynamic).should == BuildType.dynamic_library
-        end
-    
-          # Creates a default launchscreen storyboard.
-      #
-      # @param  [Project] project
-      #         the Xcodeproj to generate the launchscreen storyboard into.
-      #
-      # @param  [PBXNativeTarget] target
-      #         the native target to link the generated launchscreen storyboard into.
-      #
-      # @param  [Symbol] platform
-      #         the platform of the target. Can be `:ios` or `:osx`, etc.
-      #
-      # @param  [String] deployment_target
-      #         the deployment target for the platform.
-      #
-      # @param  [String] name
-      #         The name to use for the target, defaults to 'App'.
-      #
-      # @return [PBXFileReference] the created file reference of the launchscreen storyboard.
-      #
-      def self.add_launchscreen_storyboard(project, target, group, deployment_target, name = 'App')
-        launch_storyboard_file = AppTargetHelper.create_launchscreen_storyboard_file(project, deployment_target, name)
-        launch_storyboard_ref = group.new_file(launch_storyboard_file)
-        target.resources_build_phase.add_file_reference(launch_storyboard_ref)
-      end
-    
-          private
-    
-            it 'cleans up linked libraries and frameworks from the frameworks build phase' do
-          @pod_bundle.stubs(:build_type => Target::BuildType.dynamic_framework)
-          @target_integrator.integrate!
-          target = @target_integrator.send(:native_targets).first
-          phase = target.frameworks_build_phase
-          phase.files.find { |f| f.file_ref.path == 'Pods.framework' }.should.not.be.nil
-          phase.files.find { |f| f.file_ref.path == 'Pods-Something.framework' }.should.be.nil
-          @pod_bundle.stubs(:product_name => 'Pods-Something.framework')
-          @pod_bundle.stubs(:product_basename => 'Pods-Something')
-          @target_integrator.integrate!
-          phase.files.find { |f| f.file_ref.path == 'Pods.framework' }.should.be.nil
-          phase.files.find { |f| f.file_ref.path == 'Pods-Something.framework' }.should.not.be.nil
-        end
-    
-          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
-      def left_shift int, shift
-        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
-        # 1>>31, 2**32
-        (r & 2147483648) == 0 ? r : r - 4294967296
-      end
-    
-          def toc_content
-        @toc_content
-      end
-    
-      test 'extract destination file name in case of path renaming' do
-    view = Precious::Views::LatestChanges.new
-    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
-    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
-  end
-    
-      s.name              = 'gollum'
-  s.version           = '4.1.4'
-  s.date              = '2018-10-01'
-  s.rubyforge_project = 'gollum'
-  s.license           = 'MIT'
-    
-        it 'rejects chat channel invitation' do
-      membership = ChatChannelMembership.last
-      sign_in second_user
-      put '/chat_channel_memberships/#{membership.id}', params: {
-        chat_channel_membership: { user_action: 'reject' }
-      }
-      expect(ChatChannelMembership.find(membership.id).status).to eq('rejected')
+          # Get the title for the page.
+  #
+  # @param [Middleman::Page] page
+  #
+  # @return [String]
+  def title_for(page)
+    if page && page.data.page_title
+      return '#{page.data.page_title} - Vagrant by HashiCorp'
     end
     
-          it 'renders second and third users if present' do
-        user2 = create(:user)
-        user3 = create(:user)
-        article.update(second_user_id: user2.id, third_user_id: user3.id)
-        get article.path
-        expect(response.body).to include '<em>with <b><a href=\'#{user2.path}\'>'
+            def verify_binary(binary)
+          # Checks for the existence of chef binary and error if it
+          # doesn't exist.
+          if windows?
+            command = 'if ((&'#{binary}' -v) -Match 'Chef*'){ exit 0 } else { exit 1 }'
+          else
+            command = 'sh -c 'command -v #{binary}''
+          end
+    
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
+    end
+    
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
+    end
+    
+    # Unset all host plugins so that we aren't executing subprocess things
+# to detect a host for every test.
+Vagrant.plugin('2').manager.registered.dup.each do |plugin|
+  if plugin.components.hosts.to_hash.length > 0
+    Vagrant.plugin('2').manager.unregister(plugin)
+  end
+end
+    
+        it 'raises an error if the download failed' do
+      dl = double('downloader')
+      allow(Vagrant::Util::Downloader).to receive(:new).and_return(dl)
+      expect(dl).to receive(:download!).and_raise(
+        Vagrant::Errors::DownloaderError.new(message: 'foo'))
+    
+            {
+          host: port_info['HostIp'],
+          port: port_info['HostPort']
+        }
       end
     
-      context 'when redirected routes' do
-    include RSpec::Rails::RequestExampleGroup
+          it 'should exit the process if exit_code has been set' do
+        subject.custom(machine) { |m| Thread.current[:exit_code] = 1}
+        subject.custom(machine) { |*_| }
+        expect(Process).to receive(:exit!).with(1)
+        subject.run
+      end
+    end
+  end
+end
+
     
-      describe '#render' do
-    it 'renders the template' do
-      expect(File).to receive(:read).at_least(:once) { 'wemux ls 2>/dev/null' }
+        context 'when disabling compression or dsa_authentication flags' do
+      let(:ssh_info) {{
+        host: 'localhost',
+        port: 2222,
+        username: 'vagrant',
+        private_key_path: [private_key_path],
+        compression: false,
+        dsa_authentication: false
+      }}
+    
+          it 'Does not render below article variant when no variants published' do
+        html_variant = create(:html_variant, published: false, approved: true, group: 'article_show_below_article_cta')
+        get article.path + '?variant_version=0'
+        expect(response.body).not_to include html_variant.html
+      end
+    
+      def index
+    @pages = Page.all
+  end
+    
+        # Creates an instance of CategoryIndex for each category page, renders it, and
+    # writes the output to a file.
+    #
+    #  +category_dir+ is the String path to the category folder.
+    #  +category+     is the category currently being processed.
+    def write_category_index(category_dir, category)
+      index = CategoryIndex.new(self, self.source, category_dir, category)
+      index.render(self.layouts, site_payload)
+      index.write(self.dest)
+      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
+      self.pages << index
+    
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
+      end
+    end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
+    
+        context 'when I18n enforces available locales' do
+      before { I18n.enforce_available_locales = true }
+    
+                    memo[memo_key] = handle_passed_param(declared_parent_param, passed_children_params) do
+                  declared(passed_children_params, options, declared_children_params)
+                end
+              end
+            else
+              # If it is not a Hash then it does not have children.
+              # Find its value or set it to nil.
+              has_renaming = route_setting(:renamed_params) && route_setting(:renamed_params).find { |current| current[declared_param] }
+              param_renaming = has_renaming[declared_param] if has_renaming
+    
+        class LazyValueArray < LazyValueEnumerable
+      def initialize(array)
+        super
+        @value_hash = []
+        array.each_with_index do |value, index|
+          self[index] = value
+        end
+      end
