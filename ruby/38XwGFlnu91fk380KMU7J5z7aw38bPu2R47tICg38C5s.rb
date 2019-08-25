b128793b3484196f0dfc04b5305842995b77a797
SUNsung <weatherius@gmail.com>
2019-08-25 17:01:02 +0300

    Rewrite [38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb] File in: 2019-08-25 17:01:02

--- ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
+++ ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
@@ -1,84 +1,147 @@
 
         
-            it 'does not output links to other agents outside of the incoming set' do
-      Link.create!(:source_id => agents(:jane_weather_agent).id, :receiver_id => agents(:jane_website_agent).id)
-      Link.create!(:source_id => agents(:jane_website_agent).id, :receiver_id => agents(:jane_rain_notifier_agent).id)
+            it 'should return the file when s3 cdn enabled' do
+      SiteSetting.s3_cdn_url = 'https://cdn.s3.amazonaws.com'
+      stub_request(:get, Discourse.store.cdn_url(upload_s3.url)).to_return(status: 200, body: 'Hello world')
     
-          def remember_me_is_active?(resource)
-        return false unless resource.respond_to?(:remember_me)
-        scope = Devise::Mapping.find_scope!(resource)
-        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
-        resource.remember_me?(token, generated_at)
-      end
-    
-    module Devise
-  module Controllers
-    # Create url helpers to be used with resource/scope configuration. Acts as
-    # proxies to the generated routes created by devise.
-    # Resource param can be a string or symbol, a class, or an instance object.
-    # Example using a :user resource:
-    #
-    #   new_session_path(:user)      => new_user_session_path
-    #   session_path(:user)          => user_session_path
-    #   destroy_session_path(:user)  => destroy_user_session_path
-    #
-    #   new_password_path(:user)     => new_user_password_path
-    #   password_path(:user)         => user_password_path
-    #   edit_password_path(:user)    => edit_user_password_path
-    #
-    #   new_confirmation_path(:user) => new_user_confirmation_path
-    #   confirmation_path(:user)     => user_confirmation_path
-    #
-    # Those helpers are included by default to ActionController::Base.
-    #
-    # In case you want to add such helpers to another class, you can do
-    # that as long as this new class includes both url_helpers and
-    # mounted_helpers. Example:
-    #
-    #     include Rails.application.routes.url_helpers
-    #     include Rails.application.routes.mounted_helpers
-    #
-    module UrlHelpers
-      def self.remove_helpers!
-        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
-          remove_method method
-        end
-      end
-    
-            @email = headers[:to]
-        headers
-      end
-    
-          private
-    
-        Dir.chdir bundled_app2 do
-      install_gemfile bundled_app2('Gemfile'), <<-G
-        source 'file://#{gem_repo2}'
-        gem 'rack_two', '1.0.0'
-      G
+        def handle_bounce(e)
+      # never reply to bounced emails
+      log_email_process_failure(@mail, e)
+      set_incoming_email_rejection_message(@receiver.incoming_email, I18n.t('emails.incoming.errors.bounced_email_error'))
     end
     
-    def r; return; end; a,b,*c = r(); test_ok([a,b,c] == [nil,nil,[]])
-def r; return nil; end; a,b,*c = r(); test_ok([a,b,c] == [nil,nil,[]])
-def r; return 1; end; a,b,*c = r(); test_ok([a,b,c] == [1,nil,[]])
-def r; return []; end; a,b,*c = r(); test_ok([a,b,c] == [nil,nil,[]])
-def r; return [1]; end; a,b,*c = r(); test_ok([a,b,c] == [1,nil,[]])
-def r; return [nil]; end; a,b,*c = r(); test_ok([a,b,c] == [nil,nil,[]])
-def r; return [[]]; end; a,b,*c = r(); test_ok([a,b,c] == [[],nil,[]])
-def r; return [1,2]; end; a,b,*c = r(); test_ok([a,b,c] == [1,2,[]])
-def r; return [*[]]; end; a,b,*c = r(); test_ok([a,b,c] == [nil,nil,[]])
-def r; return [*[1]]; end; a,b,*c = r(); test_ok([a,b,c] == [1,nil,[]])
-def r; return [*[1,2]]; end; a,b,*c = r(); test_ok([a,b,c] == [1,2,[]])
+    require 'rails_helper'
+require 'theme_store/git_importer'
     
-                UI.puts 'Analyzed #{report.analyzed_paths.count} podspecs files.\n\n'
-            if report.pods_by_error.count.zero?
-              UI.puts 'All the specs passed validation.'.green << '\n\n'
-            else
-              raise Informative, '#{report.pods_by_error.count} podspecs failed validation.'
-            end
-          end
+      def personal_messages
+    guardian.ensure_can_tag_pms!
+    allowed_user = fetch_user_from_params
+    raise Discourse::NotFound if allowed_user.blank?
+    raise Discourse::NotFound if current_user.id != allowed_user.id && !@guardian.is_admin?
+    pm_tags = Tag.pm_tags(guardian: guardian, allowed_user: allowed_user)
+    
+            test_multisite_connection('second') do
+          upload = build_upload
+          expect(store.store_upload(uploaded_file, upload)).to eq(
+            '//#{SiteSetting.s3_upload_bucket}.s3.dualstack.us-east-1.amazonaws.com/uploads/second/original/1X/c530c06cf89c410c0355d7852644a73fc3ec8c04.png'
+          )
+          expect(upload.etag).to eq('ETag')
         end
       end
     end
   end
-end
+    
+      system('yarn install --dev')
+    
+      # Get the title for the page.
+  #
+  # @param [Middleman::Page] page
+  #
+  # @return [String]
+  def title_for(page)
+    if page && page.data.page_title
+      return '#{page.data.page_title} - Vagrant by HashiCorp'
+    end
+    
+        it 'returns true if installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(true)
+      subject.chef_installed(machine, 'chef_solo', version)
+    end
+    
+      describe '#chef_installed' do
+    let(:version) { '15.0.0' }
+    let(:command) { 'test -x /opt/chef_solo/bin/knife&& /opt/chef_solo/bin/knife --version | grep '15.0.0'' }
+    
+      let(:machine) { iso_env.machine(iso_env.machine_names[0], :dummy) }
+  let(:communicator) { VagrantTests::DummyCommunicator::Communicator.new(machine) }
+  let(:config)  { double('config') }
+    
+        # Initializes by loading a Vagrantfile.
+    #
+    # @param [Config::Loader] loader Configuration loader that should
+    #   already be configured with the proper Vagrantfile locations.
+    #   This usually comes from {Vagrant::Environment}
+    # @param [Array<Symbol>] keys The Vagrantfiles to load and the
+    #   order to load them in (keys within the loader).
+    def initialize(loader, keys)
+      @keys   = keys
+      @loader = loader
+      @config, _ = loader.load(keys)
+      @logger = Log4r::Logger.new('vagrant::vagrantfile')
+    end
+    
+          expect { subject.usable?(true) }.
+        to raise_error(Vagrant::Errors::VirtualBoxInvalidVersion)
+    end
+  end
+    
+        context 'when extra_args is provided as a string' do
+      let(:ssh_info) {{
+        host: 'localhost',
+        port: 2222,
+        username: 'vagrant',
+        private_key_path: [private_key_path],
+        extra_args: '-6'
+      }}
+    
+        # Enable Vagrant environment specific plugins at given data path
+    #
+    # @param [Pathname] Path to Vagrant::Environment data directory
+    # @return [Pathname] Path to environment specific gem directory
+    def environment_path=(env_data_path)
+      @env_plugin_gem_path = env_data_path.join('plugins', 'gems', RUBY_VERSION).freeze
+    end
+    
+            entries = []
+        prune   = []
+        @env.machine_index.each do |entry|
+          # If we're pruning and this entry is invalid, skip it
+          # and prune it later.
+          if options[:prune] && !entry.valid?(@env.home_path)
+            prune << entry
+            next
+          end
+    
+        before do
+      iso_env.machine_index.set(new_entry('foo'))
+      iso_env.machine_index.set(new_entry('bar'))
+      allow($stdout).to receive(:puts)
+    end
+    
+      describe '#flare_tag_hash' do
+    let(:tag) { create(:tag, name: 'ama', bg_color_hex: '#f3f3f3', text_color_hex: '#cccccc') }
+    let(:valid_article) { create(:article, tags: tag.name) }
+    
+        it 'denies chat channel invitation to non-authorized user' do
+      expect do
+        post '/chat_channel_memberships', params: {
+          chat_channel_membership: {
+            user_id: second_user.id, chat_channel_id: chat_channel.id
+          }
+        }
+      end.to raise_error(Pundit::NotAuthorizedError)
+    end
+  end
+    
+      def facebook_post!
+    BufferUpdate.buff!(@article.id, fb_buffer_text, ApplicationConfig['BUFFER_FACEBOOK_ID'], 'facebook')
+    BufferUpdate.buff!(@article.id, fb_buffer_text, ApplicationConfig['BUFFER_LINKEDIN_ID'], 'linkedin')
+    @article.update(facebook_last_buffered: Time.current)
+  end
+    
+          now = Time.current
+      @user.onboarding_package_requested_again = true if @user.onboarding_package_requested
+      @user.onboarding_package_requested = true
+      @user.onboarding_package_form_submmitted_at = now
+      @user.personal_data_updated_at = now
+      @user.shipping_validated_at = now if user_params[:shipping_validated] == '1'
+      if @user.save!
+        format.html { redirect_to '/freestickers/edit' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
