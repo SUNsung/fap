08e033370dc6a7cc2c9f631a2197e2934d0d6b60
SUNsung <weatherius@gmail.com>
2019-08-25 03:01:53 +0300

    Rewrite [19Hfue33ZMVfg19y.94QpzaLo19zRazly0jJGY19hf.rb] File in: 2019-08-25 03:01:52

--- ruby/19Hfue33ZMVfg19y.94QpzaLo19zRazly0jJGY19hf.rb
+++ ruby/19Hfue33ZMVfg19y.94QpzaLo19zRazly0jJGY19hf.rb
@@ -1,257 +1,191 @@
 
         
-            if cors_origins
-      if origin = env['HTTP_ORIGIN']
-        origin = nil unless cors_origins.include?(origin)
-      end
+        class RenderersMetalTest < ActionController::TestCase
+  tests MetalRenderingJsonController
     
-      def self.cleanup!
-    DB.exec(<<~SQL)
-      DELETE FROM drafts
-       WHERE sequence < (
-        SELECT MAX(s.sequence)
-          FROM draft_sequences s
-         WHERE s.draft_key = drafts.draft_key
-           AND s.user_id = drafts.user_id
-      )
-    SQL
-    
-        SCRIPT_ASSET_DIRECTORIES = [
-      # [dir, can_use_s3_cdn, can_use_cdn]
-      ['/assets/',             true, true],
-      ['/brotli_asset/',       true, true],
-      ['/extra-locales/',      false, false],
-      ['/highlight-js/',       false, true],
-      ['/javascripts/',        false, true],
-      ['/plugins/',            false, true],
-      ['/theme-javascripts/',  false, true],
-      ['/svg-sprite/',         false, true],
-    ]
-    
-        plugin.activate!
-    Discourse.plugins << plugin
-    
-          if SvgSprite.version(theme_ids) != params[:version]
-        return redirect_to path(SvgSprite.path(theme_ids))
-      end
-    
-        # Not using this during integration tests makes the tests 4x times faster
-    # without changing the coverage.
-    track_files '#{SimpleCov.root}/{#{subdirs},*.rb}'
-  end
-    
-        def self.can_extract?(path)
-      mime_type = 'application/vnd.adobe.air-application-installer-package+zip'
-      path.magic_number.match?(/.{59}#{Regexp.escape(mime_type)}/)
-    end
-    
-    def main
-  @ruby = File.expand_path('miniruby')
-  @verbose = false
-  $VERBOSE = false
-  $stress = false
-  @color = nil
-  @tty = nil
-  @quiet = false
-  dir = nil
-  quiet = false
-  tests = nil
-  ARGV.delete_if {|arg|
-    case arg
-    when /\A--ruby=(.*)/
-      @ruby = $1
-      @ruby.gsub!(/^([^ ]*)/){File.expand_path($1)}
-      @ruby.gsub!(/(\s+-I\s*)((?!(?:\.\/)*-(?:\s|\z))\S+)/){$1+File.expand_path($2)}
-      @ruby.gsub!(/(\s+-r\s*)(\.\.?\/\S+)/){$1+File.expand_path($2)}
-      true
-    when /\A--sets=(.*)/
-      tests = Dir.glob('#{File.dirname($0)}/test_{#{$1}}*.rb').sort
-      puts tests.map {|path| File.basename(path) }.inspect
-      true
-    when /\A--dir=(.*)/
-      dir = $1
-      true
-    when /\A(--stress|-s)/
-      $stress = true
-    when /\A--color(?:=(?:always|(auto)|(never)|(.*)))?\z/
-      warn 'unknown --color argument: #$3' if $3
-      @color = $1 ? nil : !$2
-      true
-    when /\A--tty(=(?:yes|(no)|(.*)))?\z/
-      warn 'unknown --tty argument: #$3' if $3
-      @tty = !$1 || !$2
-      true
-    when /\A(-q|--q(uiet))\z/
-      quiet = true
-      @quiet = true
-      true
-    when /\A(-v|--v(erbose))\z/
-      @verbose = true
-    when /\A(-h|--h(elp)?)\z/
-      puts(<<-End)
-Usage: #{File.basename($0, '.*')} --ruby=PATH [--sets=NAME,NAME,...]
-        --sets=NAME,NAME,...        Name of test sets.
-        --dir=DIRECTORY             Working directory.
-                                    default: /tmp/bootstraptestXXXXX.tmpwd
-        --color[=WHEN]              Colorize the output.  WHEN defaults to 'always'
-                                    or can be 'never' or 'auto'.
-    -s, --stress                    stress test.
-    -v, --verbose                   Output test name before exec.
-    -q, --quiet                     Don\'t print header message.
-    -h, --help                      Print this message and quit.
-End
-      exit true
-    when /\A-j/
-      true
+        if options[:packaged]
+      slugs = Dir[File.join(Docs.store_path, '*.tar.gz')].map { |f| File.basename(f, '.tar.gz') }
+      docs = find_docs_by_slugs(slugs)
     else
-      false
+      docs = find_docs(names)
     end
-  }
-  if tests and not ARGV.empty?
-    $stderr.puts '--tests and arguments are exclusive'
-    exit false
-  end
-  tests ||= ARGV
-  tests = Dir.glob('#{File.dirname($0)}/test_*.rb').sort if tests.empty?
-  pathes = tests.map {|path| File.expand_path(path) }
     
-      def test_keep_if
-    h1 = {}
-    ENV.each_pair {|k, v| h1[k] = v }
-    ENV['test'] = 'foo'
-    ENV.keep_if {|k, v| IGNORE_CASE ? k.upcase != 'TEST' : k != 'test' }
-    h2 = {}
-    ENV.each_pair {|k, v| h2[k] = v }
-    assert_equal(h1, h2)
+            css('section', 'code b', '.accordion', '.accordion-overflow', '.accordion-content').each do |node|
+          node.before(node.children).remove
+        end
     
-    src_testdir = File.dirname(File.realpath(__FILE__))
-$LOAD_PATH << src_testdir
-tool_dir = File.join src_testdir, '..', 'tool'
-$LOAD_PATH.unshift '#{tool_dir}/lib'
     
-    class Object # :nodoc:
-    
-          ##
-      # Is this running on mri?
-    
-        output.gsub!(/ = \d+.\d\d s = /, ' = 0.00 s = ')
-    output.gsub!(/0x[A-Fa-f0-9]+/, '0xXXX')
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
-  end
-end
-    
-      class FetchWebfinger < Base
-    def perform(*_args)
-      # don't do real discovery in cucumber
-    end
-  end
-end
-
-    
-        it 'should return a dummy value in the json version' do
-      FactoryGirl.create(:notification, recipient: alice, target: post)
-      get :read_all, format: :json
-      expect(response).not_to be_redirect
-    end
-  end
-end
-
-    
-          it 'should not create the participation' do
-        post :create, params: {post_id: @post.id}
-        expect(alice.participations.where(:target_id => @post.id)).not_to exist
-        expect(response.code).to eq('403')
-      end
-    end
-  end
-    
-        context 'for a logged out user' do
-      before do
-        sign_out :user
+      def get_name
+        name ||= at_css('#inner h1').content
+        name.remove! '» '
+        name.remove! 'Data Source: '
+        name
       end
     
-          it 'succeeds on mobile' do
-        get :public, format: :mobile
-        expect(response).to be_success
-      end
-    end
+        attr_reader :request_options
     
-        it 'rejects invalid jsfiddle link' do
-      expect do
-        generate_new_liquid('invalid_jsfiddle_link')
-      end.to raise_error(StandardError)
-    end
+    module Docs
+  class URL < URI::Generic
+    PARSER = URI::Parser.new
     
-    # enable logging
-FPM::Util.send :module_function, :logger
-FPM::Util.logger.level = :info
-FPM::Util.logger.subscribe STDERR
-    
-      # KNOWN ISSUE:
-  # If an un-matched bracket is used in valid bash, as in
-  # `echo '{'`, this function will choke.
-  # However, to cover this case basically
-  # requires writing almost half a bash parser,
-  # and it is a very small corner case.
-  # Otherwise, this approach is very robust.
-  def gobble_function(cons,prod)
-    level = 1
-    while level > 0
-      line = cons.next
-      # Not the best, but pretty good
-      # short of writing an *actual* sh
-      # parser
-      level += line.count '{'
-      level -= line.count '}'
-      if level > 0
-        prod.push(line.rstrip())
-      else
-        fine = line.sub(/\s*[}]\s*$/, '')
-        if !(fine =~ /^\s*$/)
-          prod.push(fine.rstrip())
+            # Remove code highlighting
+        css('pre').each do |node|
+          node.content = node.content
+          node['data-language'] = 'c' if node['class'] == 'c'
+          node['data-language'] = 'ruby' if node['class'] && node['class'].include?('ruby')
         end
       end
     end
-  end # def gobble_function
+  end
+end
+
     
-        # Add the tar compression flag if necessary
-    tar_compression_flag(input_path).tap do |flag|
-      args << flag unless flag.nil?
-    end
+            css('hr', '.content-header', 'button', '.docs-alert').remove
     
-              if tmux_version == 'master'
-            TMUX_MASTER_VERSION
-          else
-            tmux_version.to_f
+            css('#synopsis > details > ul > li').each_with_object [] do |node, entries|
+          link = node.at_css('a')
+          name = node.content.strip
+          name.remove! %r{\A(?:module|data|newtype|class|type family m|type)\s+}
+          name.sub! %r{\A\((.+?)\)}, '\1'
+          name.sub!(/ (?:\:\: (\w+))?.*\z/) { |_| $1 ? ' (#{$1})' : '' }
+    
+              # Classes
+          if name.gsub! 'Class: ', ''
+            name.remove! 'events.' # EventEmitter
+            klass = name
+            entries << [name, node['id']]
+            next
           end
+    
+        xml.add_element(
+      'soap:Envelope',
+      {'xmlns:soap'  => 'http://www.w3.org/2003/05/soap-envelope'}
+    )
+    
+    
+    {      if in_super and line =~ /['']References[''][[:space:]]*=>/
+        in_refs = true
+      elsif in_super and in_refs and line =~ /^[[:space:]]+\],*/m
+        in_refs = false
+      elsif in_super and line =~ /['']Notes[''][[:space:]]*=>/
+        in_notes = true
+      elsif in_super and in_notes and line =~ /^[[:space:]]+\},*/m
+        break
+      elsif in_super and in_refs and line =~ /[^#]+\[[[:space:]]*[''](.+)[''][[:space:]]*,[[:space:]]*[''](.+)[''][[:space:]]*\]/
+        identifier = $1.strip.upcase
+        value      = $2.strip
+    
+      #
+  # Constructs the payload
+  #
+  def generate
+    return super + command_string
+  end
+    
+      # Provides the uri of the redirection location.
+  #
+  # @return [URI] the uri of the redirection location.
+  # @return [nil] if the response hasn't a Location header or it isn't a valid uri.
+  def redirection
+    begin
+      URI(headers['Location'])
+    rescue ::URI::InvalidURIError
+      nil
+    end
+  end
+    
+      def decode_audio_frame(buff)
+    case self.codec
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module CredentialCache
+        # This class provides a representation of a Principal stored in the Kerberos Credential Cache.
+        class Principal < Element
+          # @!attribute name_type
+          #   @return [Integer]
+          attr_accessor :name_type
+          # @!attribute realm
+          #   @return [String]
+          attr_accessor :realm
+          # @!attribute components
+          #   @return [Array<String>]
+          attr_accessor :components
+    
+            end
+      end
+    end
+  end
+end
+    
+              # Encodes the Rex::Proto::Kerberos::Model::ApReq into an ASN.1 String
+          #
+          # @return [String]
+          def encode
+            elems = []
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_pvno], 0, :CONTEXT_SPECIFIC)
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_msg_type], 1, :CONTEXT_SPECIFIC)
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_options], 2, :CONTEXT_SPECIFIC)
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_ticket], 3, :CONTEXT_SPECIFIC)
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_authenticator], 4, :CONTEXT_SPECIFIC)
+            seq = OpenSSL::ASN1::Sequence.new(elems)
+    
+          @func = Fiddle::Function.new(handle[func], import, export, calltype)
+    rescue Fiddle::DLError => e
+      raise LoadError, e.message, e.backtrace
+    end
+    
+        assert_in_delta (3.165778513216168+1.959601041421606i),     CMath.sin(1+2i)
+    assert_in_delta (2.0327230070196656-3.0518977991517997i),   CMath.cos(1+2i)
+    assert_in_delta (0.033812826079896774+1.0147936161466338i), CMath.tan(1+2i)
+    assert_in_delta (-0.4890562590412937+1.4031192506220405i),  CMath.sinh(1+2i)
+    assert_in_delta (-0.64214812471552+1.0686074213827783i),    CMath.cosh(1+2i)
+    assert_in_delta (1.16673625724092-0.2434582011857252i),     CMath.tanh(1+2i)
+    assert_in_delta (0.4270785863924755+1.5285709194809978i),   CMath.asin(1+2i)
+    assert_in_delta (1.1437177404024204-1.528570919480998i),    CMath.acos(1+2i)
+    assert_in_delta (1.3389725222944935+0.4023594781085251i),   CMath.atan(1+2i)
+    assert_in_delta (1.4693517443681852+1.0634400235777521i),   CMath.asinh(1+2i)
+    assert_in_delta (1.528570919480998+1.1437177404024204i),    CMath.acosh(1+2i)
+    assert_in_delta (0.17328679513998635+1.1780972450961724i),  CMath.atanh(1+2i)
+    
+    describe 'GzipReader#eof?' do
+    
+        it 'returns a hash with the flare tag's text_color_hex' do
+      expect(described_class.new(valid_article).tag_hash.value?('#cccccc')).to be true
+    end
+  end
+end
+
+    
+      it 'renders a user's story successfully' do
+    expect(get: '/ben/this-is-a-slug').to route_to(
+      controller: 'stories',
+      action: 'show',
+      slug: 'this-is-a-slug',
+      username: 'ben',
+    )
+  end
+    
+          expect(instance.render).to match %r{wemux.ls.2>\/dev\/null}
+    end
+  end
+    
+            it 'deletes the project' do
+          expect(FileUtils).to receive(:rm)
+          capture_io { cli.start }
         end
       end
     
-        COMMANDS = {
-      commands: 'Lists commands available in tmuxinator',
-      completions: 'Used for shell completion',
-      new: 'Create a new project file and open it in your editor',
-      edit: 'Alias of new',
-      open: 'Alias of new',
-      start: %w{
-        Start a tmux session using a project's name (with an optional [ALIAS]
-        for project reuse) or a path to a project config file (via the -p flag)
-      }.join(' '),
-      stop: 'Stop a tmux session using a project's tmuxinator config',
-      local: 'Start a tmux session using ./.tmuxinator.yml',
-      debug: 'Output the shell commands that are generated by tmuxinator',
-      copy: %w{
-        Copy an existing project to a new project and
-        open it in your editor
-      }.join(' '),
-      delete: 'Deletes given project',
-      implode: 'Deletes all tmuxinator projects',
-      version: 'Display installed tmuxinator version',
-      doctor: 'Look for problems in your configuration',
-      list: 'Lists all tmuxinator projects'
-    }.freeze
+        context 'when a project config file is provided' do
+      it 'should raise if the project config file can't be found' do
+        project_config = 'dont-exist.yml'
+        regex = /Project config \(#{project_config}\) doesn't exist\./
+        expect do
+          described_class.validate(project_config: project_config)
+        end.to raise_error RuntimeError, regex
+      end
+    
+        context 'root not set' do
+      let(:root?) { false }
+      let(:root) { nil }
