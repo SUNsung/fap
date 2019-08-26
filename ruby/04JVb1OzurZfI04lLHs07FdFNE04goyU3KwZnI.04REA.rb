5adbe981aeab12b1cef52b7a99f913b9fb0e941f
SUNsung <weatherius@gmail.com>
2019-08-26 18:30:47 +0300

    Rewrite [04JVb1OzurZfI04lLHs07FdFNE04goyU3KwZnI.04REA.rb] File in: 2019-08-26 18:30:47

--- ruby/04JVb1OzurZfI04lLHs07FdFNE04goyU3KwZnI.04REA.rb
+++ ruby/04JVb1OzurZfI04lLHs07FdFNE04goyU3KwZnI.04REA.rb
@@ -1,253 +1,137 @@
 
         
-              Jekyll.logger.info '', 'and done. Generating results..'
-      Jekyll.logger.info ''
-    end
+          def upload
+    guardian.ensure_can_admin_tags!
     
-    def slugify(header)
-  header.delete('#').strip.downcase.gsub(%r!\s+!, '-')
-end
-    
-          desc 'Get the list of the available template' do
-        detail 'This feature was introduced in GitLab #{gitlab_version}.'
-        success Entities::TemplatesList
-      end
-      params do
-        use :pagination
-      end
-      get 'templates/#{template_type}' do
-        templates = ::Kaminari.paginate_array(TemplateFinder.build(template_type, nil).execute)
-        present paginate(templates), with: Entities::TemplatesList
-      end
-    
-    # Searches and reads files present on each GitLab project repository
-module Gitlab
-  module Template
-    module Finders
-      class RepoTemplateFinder < BaseTemplateFinder
-        # Raised when file is not found
-        FileNotFoundError = Class.new(StandardError)
-    
-        it 'finds a template in the Bar category' do
-      create_template!('bar/test-template')
-    
-        def aes256_gcm_decrypt(value)
-      return unless value
-    
-            def diff_file_with_new_path(new_path)
-          diff_files.find { |diff_file| diff_file.new_path == new_path }
-        end
-    
-              find_every_method_call_by_name(body_node, :system).each do |method|
-            next unless match = regex_match_group(parameters(method).first, /^(env|export)(\s+)?/)
-    
-            # Ask Spotlight where Xcode is. If the user didn't install the
-        # helper tools and installed Xcode in a non-conventional place, this
-        # is our only option. See: https://superuser.com/questions/390757
-        MacOS.app_with_bundle_id(BUNDLE_ID, OLD_BUNDLE_ID)
-      end
-    
-      # Does the {Bottle} this BottleSpecification belongs to need to be relocated?
-  def skip_relocation?
-    cellar == :any_skip_relocation
-  end
-    
-      it 'properly handles Casks that are not present' do
-    expect {
-      described_class.run('notacask')
-    }.to raise_error(Cask::CaskUnavailableError)
-  end
-end
-
-    
-          it 'adds the appropriate curl args' do
-        expect(subject).to receive(:system_command!) { |*, args:, **|
-          expect(args.each_cons(2)).to include(['-d', 'form=data'])
-          expect(args.each_cons(2)).to include(['-d', 'is=good'])
-        }
-    
-            # @return [Array<String>] The list of the UUIDs of the
-        #         user targets integrated by this umbrella
-        #         target.  They can be used to find the
-        #         targets opening the project They can be used
-        #         to find the targets opening the project with
-        #         Xcodeproj.
-        #
-        def user_target_uuids
-          user_targets.map(&:uuid)
-        end
-      end
-    end
-  end
-end
-
-    
-        # Checks that the podfile exists.
-    #
-    # @raise  If the podfile does not exists.
-    #
-    # @return [void]
-    #
-    def verify_podfile_exists!
-      unless config.podfile
-        raise Informative, 'No `Podfile' found in the project directory.'
-      end
-    end
-    
-          it 'allows to differentiate between an exclusive variant with a specific subspec and ' \
-        'an inclusive variant with the default subspecs plus a non-default subspec' do
-        variants = PodVariantSet.new([
-          PodVariant.new([@foo_subspec], [], [], Platform.ios),
-          PodVariant.new([@root_spec, @default_subspec, @foo_subspec], [], [], Platform.ios),
-        ])
-        variants.scope_suffixes.values.should == %w(Foo .default-Foo)
-      end
-    
-              it 'requires a host target for XPC service targets' do
-            @target.user_targets.first.stubs(:symbol_type).returns(:xpc_service)
-            @target.requires_host_target?.should == true
-          end
-    
-            it 'allows specifying packaging' do
-          BuildType.new(:packaging => :framework).should == BuildType.static_framework
-        end
-    
-    @property (nonatomic, strong) UIWindow *window;
-    
-      describe 'PUT blocks' do
-    it 'updates block from input data' do
-      block = create(:block, user_id: user.id, input_css: '.blue { color: blue;}')
-      put '/blocks/#{block.id}', params: {
-        block: { input_css: '.blue { color: red;}',
-                 input_html: 'yo',
-                 input_javascript: 'alert('hey')' }
+        def ensure_cors!
+      rule = {
+        allowed_headers: ['*'],
+        allowed_methods: ['PUT'],
+        allowed_origins: [Discourse.base_url_no_prefix],
+        max_age_seconds: 3000
       }
-      expect(Block.last.processed_css).to include('color: red')
-    end
+    
+        if text = I18n.t('reviewables.reasons.#{object.reason}', base_url: Discourse.base_url, default: nil)
+      # Create a convenient link to any site settings if the user is staff
+      settings_url = '#{Discourse.base_url}/admin/site_settings/category/all_results?filter='
+    
+    desc 'run chrome headless smoke tests on current build'
+task 'smoke:test' do
+  if RbConfig::CONFIG['host_os'][/darwin|mac os/]
+    google_chrome_cli = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+  else
+    google_chrome_cli = 'google-chrome'
   end
     
-      it 'renders a user index if there is a user with the username successfully' do
-    expect(get: '/#{user.username}').to route_to(
-      controller: 'stories',
-      action: 'index',
-      username: user.username,
-    )
-  end
+          category.set_permissions(everyone: :full)
+      category.save!
     
-      it 'allows delayed scheduling of AM mails' do
-    ss = Sidekiq::ScheduledSet.new
-    assert_equal 0, ss.size
-    UserMailer.delay_for(5.days).greetings(1, 2)
-    assert_equal 1, ss.size
-  end
+        context 'when overridden' do
+      after do
+        settings.remove_override!(:test_hello?)
+      end
     
-        def with_sidekiq_option(name, value)
-      _original, Sidekiq.options[name] = Sidekiq.options[name], value
-      begin
-        yield
-      ensure
-        Sidekiq.options[name] = _original
+        # The path used after confirmation.
+    def after_confirmation_path_for(resource_name, resource)
+      if signed_in?(resource_name)
+        signed_in_root_path(resource)
+      else
+        new_session_path(resource_name)
       end
     end
     
-      describe 'default_worker_options' do
-    it 'stringifies keys' do
-      @old_options = Sidekiq.default_worker_options
-      begin
-        Sidekiq.default_worker_options = { queue: 'cat'}
-        assert_equal 'cat', Sidekiq.default_worker_options['queue']
-      ensure
-        Sidekiq.default_worker_options = @old_options
-      end
-    end
-  end
-    
-        context 'and :python_fix_dependencies? is true' do
-      before :each do
-        subject.attributes[:python_fix_dependencies?] = true
-      end
-    
-          context 'the getter' do
-        before do
-          subject.send('#{item}=', value)
-        end
-        it 'returns the value set previously' do
-          expect(subject.send(item)).to(be == value)
-        end
-      end
-    end
-  end
-    
-              # clear off ownership info
-          header = replace_ownership_headers(header, true)
-    
-        wordsize = case @architecture
-    when nil, 'native'
-      %x{getconf LONG_BIT}.chomp # 'native' is current arch
-    when 'amd64'
-      '64'
-    when 'i386'
-      '32'
-    else
-      %x{getconf LONG_BIT}.chomp # default to native, the current arch
+        def unlock_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :unlock_instructions, opts)
     end
     
-      option '--identifier-prefix', 'IDENTIFIER_PREFIX',
-    'Reverse domain prefix prepended to package identifier, ' \
-    'ie. 'org.great.my'. If this is omitted, the identifer ' \
-    'will be the package name.'
-  option '--payload-free', :flag, 'Define no payload, assumes use of script options.',
-    :default => false
-  option '--ownership', 'OWNERSHIP',
-    '--ownership option passed to pkgbuild. Defaults to 'recommended'. ' \
-    'See pkgbuild(1).', :default => 'recommended' do |value|
-    if !OWNERSHIP_OPTIONS.include?(value)
-      raise ArgumentError, 'osxpkg-ownership value of '#{value}' is invalid. ' \
-        'Must be one of #{OWNERSHIP_OPTIONS.join(', ')}'
-    end
-    value
-  end
+    module Devise
+  module Controllers
+    # Provide the ability to store a location.
+    # Used to redirect back to a desired path after sign in.
+    # Included by default in all controllers.
+    module StoreLocation
+      # Returns and delete (if it's navigational format) the url stored in the session for
+      # the given scope. Useful for giving redirect backs after sign up:
+      #
+      # Example:
+      #
+      #   redirect_to stored_location_for(:user) || root_path
+      #
+      def stored_location_for(resource_or_scope)
+        session_key = stored_location_key_for(resource_or_scope)
     
-        # Remove the stuff we don't want
-    delete_these = ['.depdb', '.depdblock', '.filemap', '.lock', '.channel', 'cache', 'temp', 'download', '.channels', '.registry']
-    Find.find(staging_path) do |path|
-      if File.file?(path)
-        logger.info('replacing staging_path in file', :replace_in => path, :staging_path => staging_path)
-        begin
-          content = File.read(path).gsub(/#{Regexp.escape(staging_path)}/, '')
-          File.write(path, content)
-        rescue ArgumentError => e
-          logger.warn('error replacing staging_path in file', :replace_in => path, :error => e)
-        end
-      end
-      FileUtils.rm_r(path) if delete_these.include?(File.basename(path))
+        proxy = Devise::Hooks::Proxy.new(warden)
+    
+        def default_path_names(options)
+      @path_names = Hash.new { |h,k| h[k] = k.to_s }
+      @path_names[:registration] = ''
+      @path_names.merge!(options[:path_names]) if options[:path_names]
     end
     
-          base = staging_path(File.join(attributes[:prefix], '#{platform.platform}/#{platform.target_version || 'default'}'))
-      target = File.join(base, 'files')
-      actions_script = File.join(base, 'install_actions.sh')
-      ::PleaseRun::Installer.install_files(platform, target, false)
-      ::PleaseRun::Installer.write_actions(platform, actions_script)
-    end
+    module Devise
+  module Models
+    # Rememberable manages generating and clearing token for remembering the user
+    # from a saved cookie. Rememberable also has utility methods for dealing
+    # with serializing the user into the cookie and back from the cookie, trying
+    # to lookup the record based on the saved information.
+    # You probably wouldn't use rememberable methods directly, they are used
+    # mostly internally for handling the remember token.
+    #
+    # == Options
+    #
+    # Rememberable adds the following options in devise_for:
+    #
+    #   * +remember_for+: the time you want the user will be remembered without
+    #     asking for credentials. After this time the user will be blocked and
+    #     will have to enter their credentials again. This configuration is also
+    #     used to calculate the expires time for the cookie created to remember
+    #     the user. By default remember_for is 2.weeks.
+    #
+    #   * +extend_remember_period+: if true, extends the user's remember period
+    #     when remembered via cookie. False by default.
+    #
+    #   * +rememberable_options+: configuration options passed to the created cookie.
+    #
+    # == Examples
+    #
+    #   User.find(1).remember_me!  # regenerating the token
+    #   User.find(1).forget_me!    # clearing the token
+    #
+    #   # generating info to put into cookies
+    #   User.serialize_into_cookie(user)
+    #
+    #   # lookup the user based on the incoming cookie information
+    #   User.serialize_from_cookie(cookie_string)
+    module Rememberable
+      extend ActiveSupport::Concern
     
-          success = safesystem(setup_cmd)
-      #%x{#{setup_cmd}}
-      if !success
-        logger.error('setup.py get_metadata failed', :command => setup_cmd,
-                      :exitcode => $?.exitstatus)
-        raise 'An unexpected error occurred while processing the setup.py file'
-      end
-      File.read(tmp)
-    end
-    logger.debug('result from `setup.py get_metadata`', :data => output)
-    metadata = JSON.parse(output)
-    logger.info('object output of get_metadata', :json => metadata)
-    
-    describe 'margin' do
-  before(:all) do
-    ParserSupport.parse_file('library/margin')
-  end
-    
-          expect('.all-text-inputs-hover').to have_ruleset(ruleset)
-    end
-  end
+    # skip over blogs that aren't found
+unavailable = []
+fast_forwards = [
+  'Baidu Research',
+  'Booking.com',
+  'Fynd',
+  'Graphcool',
+  'LinkedIn',
+  'Medallia',
+  'OmniTI',
+  'Paperless Post',
+  'Pluralsight',
+  'Prolific Interactive',
+  'Quora',
+  'Robert Elder Software',
+  'Simple',
+  'SlideShare',
+  'SourceClear',
+  'Viget',
+  'Zalando',
+  'Zapier',
+  'Zynga',
+  'Dave Beazley',
+  'Edan Kwan',
+  'Grzegorz Gajos',
+  'Joe Armstrong',
+  'Kai Hendry',
+  'LiveOverflow'
+]
