6e2acbb43ba8f3aefd444eb49b62f52aafdf1099
SUNsung <weatherius@gmail.com>
2019-08-26 23:42:24 +0300

    Rewrite [56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb] File in: 2019-08-26 23:42:23

--- ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
+++ ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
@@ -1,113 +1,21 @@
 
         
-              it 'adds no-skip-current flag to command if no_skip_current is set to true' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              command: 'build',
-              no_skip_current: true
-            )
-          end').runner.execute(:test)
-    
-          it 'handles the extensions parameter with a single element correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', extensions: ['m'])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --include=\\*.m')
-      end
-    
-        it 'shell-escapes environment variable values' do
-      message = 'A message'
-      path = '/usr/my local/bin'
-      command = command_from_args({ 'PATH' => path }, 'git', 'commit', '-m', message)
-      expect(command).to eq('PATH=#{path.shellescape} git commit -m #{message.shellescape}')
+            def fragment_url_string?(str)
+      str[0] == '#'
     end
     
-        os = 'windows'
-    shellescape_testcases.each do |testcase|
-      it testcase['it'] + ': ' + testcase['it_result'][os] do
-        str = testcase['str'].to_s
-        expect_correct_implementation_to_be_called(str, :shellescape, os)
-        escaped = str.shellescape
-        expect(escaped).to eq(testcase['expect'][os])
-      end
-    end
-  end
+    module Docs
+  class PageDb
+    attr_reader :pages
     
-    # The * turns the array into a parameter list
-# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
-# We need to use that, because otherwise invocations like
-# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
-exec(*exec_arr)
-
-    
-          it 'splits correctly' do
-        expected = ['One', 'Two', 'Three', 'Four Token']
-        expect(generator.split_keywords(keywords)).to eq(expected)
-      end
+        def document?
+      @content =~ DOCUMENT_RGX
     end
     
-    module Vagrant
-  # This class handles guest-OS specific interactions with a machine.
-  # It is primarily responsible for detecting the proper guest OS
-  # implementation and then delegating capabilities.
-  #
-  # Vagrant has many tasks which require specific guest OS knowledge.
-  # These are implemented using a guest/capability system. Various plugins
-  # register as 'guests' which determine the underlying OS of the system.
-  # Then, 'guest capabilities' register themselves for a specific OS (one
-  # or more), and these capabilities are called.
-  #
-  # Example capabilities might be 'mount_virtualbox_shared_folder' or
-  # 'configure_networks'.
-  #
-  # This system allows for maximum flexibility and pluginability for doing
-  # guest OS specific operations.
-  class Guest
-    include CapabilityHost
+        def root_path
+      self.class.root_path
+    end
     
-            # Execute a command on the remote machine. The exact semantics
-        # of this method are up to the implementor, but in general the
-        # users of this class will expect this to be a shell.
-        #
-        # This method gives you no way to write data back to the remote
-        # machine, so only execute commands that don't expect input.
-        #
-        # @param [String] command Command to execute.
-        # @yield [type, data] Realtime output of the command being executed.
-        # @yieldparam [String] type Type of the output. This can be
-        #   `:stdout`, `:stderr`, etc. The exact types are up to the
-        #   implementor.
-        # @yieldparam [String] data Data for the given output.
-        # @return [Integer] Exit code of the command.
-        def execute(command, opts=nil)
-        end
-    
-            # Initializes the system. Any subclasses MUST make sure this
-        # method is called on the parent. Therefore, if a subclass overrides
-        # `initialize`, then you must call `super`.
-        def initialize(vm)
-          @vm = vm
-        end
-    
-            # This returns all the config classes for the various pushes.
-        #
-        # @return [Registry]
-        def push_configs
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.configs[:push])
-            end
-          end
-        end
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have path #{expected.inspect} but was #{actual.current_path.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have path #{expected.inspect} but it had'
-  end
-end
-    
-          delete :destroy, params: {post_id: @message.id, id: @like.id}, format: :json
-      expect(response.status).to eq(204)
+        def format_url(url)
+      url.to_s.remove %r{\Ahttps?://}
     end
