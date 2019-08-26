b1a3f2382bf68b13594142fd2e41aae2e9568023
SUNsung <weatherius@gmail.com>
2019-08-26 06:13:54 +0300

    Rewrite [36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py] File in: 2019-08-26 06:13:53

--- pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
+++ pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
@@ -1,191 +1,362 @@
 
         
-        print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
+        
+class SplitDateTimeField(MultiValueField):
+    widget = SplitDateTimeWidget
+    hidden_widget = SplitHiddenDateTimeWidget
+    default_error_messages = {
+        'invalid_date': _('Enter a valid date.'),
+        'invalid_time': _('Enter a valid time.'),
+    }
+    
+        class Media:
+        js = ('my_awesome_inline_scripts.js',)
+    
+        def test_inlines_show_change_link_unregistered(self):
+        'Inlines `show_change_link` disabled for unregistered models.'
+        parent = ParentModelWithCustomPk.objects.create(my_own_pk='foo', name='Foo')
+        ChildModel1.objects.create(my_own_pk='bar', name='Bar', parent=parent)
+        ChildModel2.objects.create(my_own_pk='baz', name='Baz', parent=parent)
+        response = self.client.get(reverse('admin:admin_inlines_parentmodelwithcustompk_change', args=('foo',)))
+        self.assertFalse(response.context['inline_admin_formset'].opts.has_registered_model)
+        self.assertNotContains(response, INLINE_CHANGELINK_HTML)
+    
+        def as_sql(self, connection, lookup, template_params, *args):
+        if lookup.lhs.output_field.geography and not self.geography:
+            raise ValueError('PostGIS geography does not support the '%s' '
+                             'function/operator.' % (self.func or self.op,))
+    
+        def test_version_loose_tuple(self):
+        expect = ('1.2.3b1.dev0', 1, 2, 3)
+        ops = FakePostGISOperations(expect[0])
+        actual = ops.postgis_version_tuple()
+        self.assertEqual(expect, actual)
+    
+        Test classes using this mixin need to define a `work_subdir` attribute
+    which designates the subdir under `tests/i18n/` that will be copied to the
+    temporary tree from which its test cases will run.
+    
+        def test_no_wrap_enabled(self):
+        management.call_command('makemessages', locale=[LOCALE], verbosity=0, no_wrap=True)
+        self.assertTrue(os.path.exists(self.PO_FILE))
+        with open(self.PO_FILE) as fp:
+            po_contents = fp.read()
+            self.assertMsgId(
+                'This literal should also be included wrapped or not wrapped '
+                'depending on the use of the --no-wrap option.',
+                po_contents
+            )
+    
+        def __getitem__(self, index):
+        '''
+        Get the Field object at the specified index, which may be either
+        an integer or the Field's string label.  Note that the Field object
+        is not the field's _value_ -- use the `get` method instead to
+        retrieve the value (e.g. an integer) instead of a Field instance.
+        '''
+        if isinstance(index, str):
+            i = self.index(index)
+        elif 0 <= index < self.num_fields:
+            i = index
+        else:
+            raise IndexError('Index out of range when accessing field in a feature: %s.' % index)
+        return Field(self, i)
+    
+        # #### Field Properties ####
+    @property
+    def is_set(self):
+        'Return True if the value of this field isn't null, False otherwise.'
+        return capi.is_field_set(self._feat.ptr, self._index)
+    
+        @property
+    def nodata_value(self):
+        '''
+        Return the nodata value for this band, or None if it isn't set.
+        '''
+        # Get value and nodata exists flag
+        nodata_exists = c_int()
+        value = capi.get_band_nodata_value(self._ptr, nodata_exists)
+        if not nodata_exists:
+            value = None
+        # If the pixeltype is an integer, convert to int
+        elif self.datatype() in GDAL_INTEGER_TYPES:
+            value = int(value)
+        return value
+    
+        # #### Other Routines ####
+    @property
+    def area(self):
+        'Return the area of the Geometry.'
+        return capi.geos_area(self.ptr, byref(c_double()))
+    
+        return project
+    
+    REQUESTS_IMP_ERR = None
+try:
+    import requests
+except ImportError:
+    REQUESTS_IMP_ERR = traceback.format_exc()
+    REQUESTS_FOUND = False
+else:
+    REQUESTS_FOUND = True
+    
+    PASSLIB_IMP_ERR = None
+try:
+    from passlib.apache import HtpasswdFile, htpasswd_context
+    from passlib.context import CryptContext
+    import passlib
+except ImportError:
+    PASSLIB_IMP_ERR = traceback.format_exc()
+    passlib_installed = False
+else:
+    passlib_installed = True
+    
+    # Deletes the previously created issue
+- taiga_issue:
+    taiga_host: https://mytaigahost.example.com
+    project: myproject
+    subject: An error has been found
+    issue_type: Bug
+    state: absent
+'''
+    
+        return list(merged_items.values())
+    
+            # Change the password (or throw an exception)
+        try:
+            self.connection.passwd_s(self.dn, None, self.passwd)
+        except ldap.LDAPError as e:
+            self.fail('Unable to set password', e)
+    
+        # Search for given device
+    if device is not None:
+        devices_by_nickname = {}
+        for d in pb.devices:
+            devices_by_nickname[d.nickname] = d
     
     
-def test_invalid_data_format():
-    with pytest.raises(ValueError):
-        K.normalize_data_format('channels_middle')
+DOCUMENTATION = '''
+---
+module: ipmi_power
+short_description: Power management for machine
+description:
+  - Use this module for power management
+version_added: '2.2'
+options:
+  name:
+    description:
+      - Hostname or ip address of the BMC.
+    required: true
+  port:
+    description:
+      - Remote RMCP port.
+    default: 623
+  user:
+    description:
+      - Username to use to connect to the BMC.
+    required: true
+  password:
+    description:
+      - Password to connect to the BMC.
+    required: true
+  state:
+    description:
+      - Whether to ensure that the machine in desired state.
+    required: true
+    choices:
+        - on -- Request system turn on
+        - off -- Request system turn off without waiting for OS to shutdown
+        - shutdown -- Have system request OS proper shutdown
+        - reset -- Request system reset without waiting for OS
+        - boot -- If system is off, then 'on', else 'reset'
+  timeout:
+    description:
+      - Maximum number of seconds before interrupt request.
+    default: 300
+requirements:
+  - 'python >= 2.6'
+  - pyghmi
+author: 'Bulat Gaifullin (@bgaifullin) <gaifullinbf@gmail.com>'
+'''
     
-        # - Produce data on 1 worker thread, consume on main thread:
-    #   - Worker thread is the only thread running the generator
-    model.evaluate_generator(custom_generator(),
-                             steps=STEPS,
-                             max_queue_size=10,
-                             workers=1,
-                             use_multiprocessing=False)
+        # 读取文件
+    for f in files_list:
+        txt = open(f).read()
+        # words = word_tokenize(txt)
+        words = tokenizer.tokenize(txt)
+        # creating inverted index data structure
+        for word in words:
+            word = word_clean(word)  # 单词清洗
+            if word not in index:
+                index[word] = {f}
+            else:
+                index[word].add(f)
     
-    if os.path.isfile(log_file):
-    os.remove(log_file)
+            if n_dim == 2:
+            return tf.nn.softmax(x)
+        else:
+            e = tf.exp(x - tf.reduce_max(x, axis=axis, keepdims=True))
+            s = tf.reduce_sum(e, axis=axis, keepdims=True)
+            return e / s
+    '''
+    return tf.nn.softmax(x, axis=axis)
     
-        major = int(m.group(1))
-    minor = int(m.group(2))
-    patch = int(m.group(4) or 0)
-    beta = int(m.group(6) or sys.maxint)
     
-    MIN_TOKEN_TYPE = UP+1
-	
-INVALID_TOKEN_TYPE = 0
-    
-            return False
-    
-        with tf.variable_scope(name, reuse=reuse):
-        alpha = get_w(alpha_shape, w_initializer=alpha_init, name='alpha')
-        # o = relu(x) + 0.5 * tf.multiply(alpha, x - tf.abs(x))  # TFLearn
-        o = leaky_relu(x, alpha)  # TensorLayer / <Deep Learning>
-    
-        def _build(self, n_input):
-        ''''''
-        with tf.variable_scope(self.name or 'dense'):
-            self.W, self.b = get_wb([n_input, self.n_unit])
-    
-    import tensorflow as tf
+def multi_highway_conv2d(x, kernel_size, n_layer,
+                         act_fn=relu,
+                         strides=1,
+                         padding='SAME',
+                         carry_bias=-1.0,
+                         name=None):
+    '''多层 highway_conv2d'''
+    if isinstance(kernel_size, int):
+        kernel_size = [kernel_size] * n_layer
     
     l1_l2_regularizer = L1L2Regularizer(l1=0.01, l2=0.01)
 
     
-    SPLIT_LINE = '-----------'
-    
-    References:
-    https://www.tensorflow.org/api_guides/python/array_ops
-    keras.backend
+    sum_ngrams = 0
+for s in sentences:
+    for w in s:
+        w = w.lower()
+        # from gensim.models._utils_any2vec import compute_ngrams
+        ret = compute_ngrams(w, min_ngrams, max_ngrams)
+        print(ret)
+        sum_ngrams += len(ret)
 '''
-    
-    # 因为 'a', 'aa', 'aaa' 中都只含有 '<a' ，所以它们直积上都只是 '<a'
-print(model.wv['a'])
-print(model.wv['aa'])
-print(model.wv['aaa'])
-print(model.wv['<a'])
+['<h', 'he', 'el', 'll', 'lo', 'o>', '<he', 'hel', 'ell', 'llo', 'lo>', '<hel', 'hell', 'ello', 'llo>']
+['<w', 'wo', 'or', 'rl', 'ld', 'd>', '<wo', 'wor', 'orl', 'rld', 'ld>', '<wor', 'worl', 'orld', 'rld>']
+['<!', '!>', '<!>']
+['<i', 'i>', '<i>']
+['<a', 'am', 'm>', '<am', 'am>', '<am>']
+['<h', 'hu', 'ua', 'ay', 'y>', '<hu', 'hua', 'uay', 'ay>', '<hua', 'huay', 'uay>']
+['<.', '.>', '<.>']
 '''
-[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
-[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
-[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
-[ 0.00226487 -0.19139008  0.17918809  0.13084619 -0.1939924 ]
-'''
+assert sum_ngrams == len(model.wv.vectors_ngrams)
+print(sum_ngrams)  # 57
 print()
     
-        def test_default_encoder_naive_fails(self):
-        from acme.fields import RFC3339Field
-        self.assertRaises(
-            ValueError, RFC3339Field.default_encoder, datetime.datetime.now())
+        def test_str(self):
+        self.assertEqual('Invalid nonce ('xxx'): error', str(self.error))
     
-        good_nonce = jose.encode_b64jose(b'foo')
-    wrong_nonce = u'F'
-    # Following just makes sure wrong_nonce is wrong
+            # Self test
+        self.assertTrue(self.addr.conflicts(self.addr))
+        self.assertTrue(self.addr1.conflicts(self.addr1))
+        # This is a tricky one...
+        self.assertTrue(self.addr1.conflicts(self.addr2))
+    
+    Certbot will emit a warning if it detects that the credentials file can be
+accessed by other users on your system. The warning reads 'Unsafe permissions
+on credentials configuration file', followed by the path to the credentials
+file. This warning will be emitted each time Certbot uses the credentials file,
+including for renewal, and cannot be silenced except by addressing the issue
+(e.g., by using a command like ``chmod 600`` to restrict access to the file).
+    
+    
+@pytest.fixture()
+def context(request):
+    # Fixture request is a built-in pytest fixture describing current test request.
+    integration_test_context = nginx_context.IntegrationTestsContext(request)
     try:
-        jose.b64decode(wrong_nonce)
-    except (ValueError, TypeError):
-        assert True
-    else:
-        assert False  # pragma: no cover
+        yield integration_test_context
+    finally:
+        integration_test_context.cleanup()
     
-            :param str domain_name: The domain name to query for an SOA record.
-        :returns: True if found, False otherwise.
-        :rtype: bool
-        :raises certbot.errors.PluginError: if no response is received.
+    def get_extension(link):
+    extension = os.path.splitext(link)[1][1:]
+    if extension in ['pdf', 'html']:
+        return extension
+    if 'pdf' in extension:
+        return 'pdf'    
+    return 'pdf'    
+    
+    
+from __future__ import print_function
+import numpy as np
+from activators import ReluActivator, IdentityActivator
+    
+    # 引入 Perceptron 类
+from __future__ import print_function
+from perceptron import Perceptron
+    
+    
+# 图像数据加载器
+class ImageLoader(Loader):
+    def get_picture(self, content, index):
         '''
-    
-    
-@pytest.mark.parametrize('certname_pattern, params, context', [
-    ('nginx.{0}.wtf', ['run'], {'default_server': True}),
-    ('nginx2.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': True}),
-    # Overlapping location block and server-block-level return 301
-    ('nginx3.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': True}),
-    # No matching server block; default_server exists
-    ('nginx4.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': True}),
-    # No matching server block; default_server does not exist
-    ('nginx5.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': False}),
-    # Multiple domains, mix of matching and not
-    ('nginx6.{0}.wtf,nginx7.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': False}),
-], indirect=['context'])
-def test_certificate_deployment(certname_pattern, params, context):
-    # type: (str, list, nginx_context.IntegrationTestsContext) -> None
-    '''
-    Test various scenarios to deploy a certificate to nginx using certbot.
-    '''
-    domains = certname_pattern.format(context.worker_id)
-    command = ['--domains', domains]
-    command.extend(params)
-    context.certbot_test_nginx(command)
-    
-        # Execution of certbot in a self-contained workspace
-    workspace = os.environ.get('WORKSPACE', os.path.join(os.getcwd(), '.certbot_test_workspace'))
-    if not os.path.exists(workspace):
-        print('--> Creating a workspace for certbot_test: {0}'.format(workspace))
-        os.mkdir(workspace)
-    else:
-        print('--> Using an existing workspace for certbot_test: {0}'.format(workspace))
-    config_dir = os.path.join(workspace, 'conf')
-    
-        def get_dist_paths(self):
-        return [
-            # Also see sentry.utils.integrationdocs.DOC_FOLDER
-            os.path.join(self.get_root_path(), 'src', 'sentry', 'integration-docs'),
-        ]
-    
-            try:
-            # GitHub doesn't separate first and last names. Let's try.
-            first_name, last_name = name.split(' ', 1)
-        except ValueError:
-            details['first_name'] = name
-        else:
-            details['first_name'] = first_name
-            details['last_name'] = last_name
-        return details
-    
-        def backward(self, delta_array):
+        内部函数，从文件中获取图像
         '''
-        反向计算W和b的梯度
-        delta_array: 从上一层传递过来的误差项
-        '''
-        # 式8
-        self.delta = self.activator.backward(self.input) * np.dot(
-            self.W.T, delta_array)
-        self.W_grad = np.dot(delta_array, self.input.T)
-        self.b_grad = delta_array
+        start = index * 28 * 28 + 16
+        picture = []
+        for i in range(28):
+            picture.append([])
+            for j in range(28):
+                picture[i].append(
+                    self.to_int(content[start + i * 28 + j]))
+        return picture
     
-        def _update_weights(self, input_vec, output, label, rate):
-        '''
-        Desc:
-            按照感知器规则更新权重
-        Args:
-            input_vec —— 输入向量
-            output —— 经过感知器规则计算得到的输出
-            label —— 输入向量对应的标签
-            rate —— 学习率
-        Returns:
-            None
-        '''
-        # 利用感知器规则更新权重
-        delta = label - output
-        # map() 接收一个函数 f 和一个 list ，并通过把函数 f 依次作用在 list 的每个元素上，得到一个新的 list 返回。比如我们的 f 函数是计算平方， map(f, [1,2,3,4,5]) ===> 返回 [1,4,9,16,25]
-        # zip() 接收任意多个（包括 0 个和 1个）序列作为参数，返回一个 tuple 列表。例：x = [1,2,3] y = [4,5,6] z = [7,8,9] xyz = zip(x, y, z) ===> [(1,4,7), (2,5,8), (3,6,9)]
-        self.weights = map(lambda (x, w): w + rate * delta * x, zip(input_vec, self.weights))
-        # 更新 bias
-        self.bias += rate * delta
-    
-        def forward(self, input_array):
-        '''
-        Desc:
-            实现循环层的前向计算
-        '''
-        self.times += 1
-        state = (np.dot(self.U, input_array) +
-                 np.dot(self.W, self.state_list[-1]))
-        element_wise_op(state, self.activator.forward)
-        self.state_list.append(state)
-    
-    '''
-如果上面的代码运行没有问题，说明numpy安装没有问题
+    # 转化关系， 数组转化为矩阵
+randMat = mat(randArray)
 '''
-
+.I 表示对矩阵求逆(可以利用矩阵的初等变换)
+   意义：逆矩阵是一个判断相似性的工具。逆矩阵A与列向量p相乘后，将得到列向量q，q的第i个分量表示p与A的第i个列向量的相似度。
+   参考案例链接：
+   https://www.zhihu.com/question/33258489
+   http://blog.csdn.net/vernice/article/details/48506027
+.T 表示对矩阵转置(行列颠倒)
+    * 等同于: .transpose()
+.A 返回矩阵基于的数组
+    参考案例链接：
+    http://blog.csdn.net/qq403977698/article/details/47254539
+'''
+invRandMat = randMat.I
+TraRandMat = randMat.T
+ArrRandMat = randMat.A
+# 输出结果
+print('randArray=(%s) \n' % type(randArray), randArray)
+print('randMat=(%s) \n' % type(randMat), randMat)
+print('invRandMat=(%s) \n' % type(invRandMat), invRandMat)
+print('TraRandMat=(%s) \n' % type(TraRandMat), TraRandMat)
+print('ArrRandMat=(%s) \n' % type(ArrRandMat), ArrRandMat)
+# 矩阵和逆矩阵 进行求积 (单位矩阵，对角线都为1嘛，理论上4*4的矩阵其他的都为0)
+myEye = randMat*invRandMat
+# 误差
+print(myEye - eye(4))
     
     
-# 基于SVD的评分估计
-# 在recommend() 中，这个函数用于替换对standEst()的调用，该函数对给定用户给定物品构建了一个评分估计值
-def svdEst(dataMat, user, simMeas, item):
-    '''svdEst( )
+def loadDataSet(fileName, delim='\t'):
+    fr = open(fileName)
+    stringArr = [line.strip().split(delim) for line in fr.readlines()]
+    datArr = [map(float, line) for line in stringArr]
+    return mat(datArr)
+    
+        def __init__(self, *args, **kwargs):
+        super(MRWordCountUtility, self).__init__(*args, **kwargs)
+        self.chars = 0
+        self.words = 0
+        self.lines = 0
+    
+        def new_future_import(self, old):
+        new = FromImport('__future__',
+                         [Name('absolute_import', prefix=' '), Comma(),
+                          Name('division', prefix=' '), Comma(),
+                          Name('print_function', prefix=' ')])
+        if old is not None:
+            new.prefix = old.prefix
+        return new
+    
+        from tornado.platform.auto import set_close_exec
+'''
+    
+        c-ares fails to resolve some names when ``family`` is ``AF_UNSPEC``,
+    so it is only recommended for use in ``AF_INET`` (i.e. IPv4).  This is
+    the default for ``tornado.simple_httpclient``, but other libraries
+    may default to ``AF_UNSPEC``.
+    
+    
+class MiscFutureTest(AsyncTestCase):
+    def test_future_set_result_unless_cancelled(self):
+        fut = Future()  # type: Future[int]
+        future_set_result_unless_cancelled(fut, 42)
+        self.assertEqual(fut.result(), 42)
+        self.assertFalse(fut.cancelled())
