a9e4b79778a6739848959191847aef33a6971084
SUNsung <weatherius@gmail.com>
2019-08-27 23:46:04 +0300

    Rewrite [07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py] File in: 2019-08-27 23:46:03

--- pyton/07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py
+++ pyton/07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py
@@ -1,149 +1,160 @@
 
         
-        sys.path.insert(0, dirn(dirn((os.path.abspath(__file__)))))
-    
-        def test_youtube(self):
-        self._assert_restricted('07FYdnEawAQ', '07FYdnEawAQ.mp4', 10)
-    
-    VERSION = '3.29'
-    
-        @memoize
-    def get_aliases(self):
-        proc = Popen(['tcsh', '-ic', 'alias'], stdout=PIPE, stderr=DEVNULL)
-        return dict(
-            self._parse_alias(alias)
-            for alias in proc.stdout.read().decode('utf-8').split('\n')
-            if alias and '\t' in alias)
+            def test_guess_when_method_not_set(self):
+        self.parser.args = argparse.Namespace()
+        self.parser.args.method = None
+        self.parser.args.url = 'http://example.com/'
+        self.parser.args.items = []
+        self.parser.args.ignore_stdin = False
+        self.parser.env = MockEnvironment()
     
     
-@pytest.mark.usefixtures('no_memoize')
-@pytest.mark.parametrize('script, result', [
-    ('vom file.py', ['vim file.py']),
-    ('fucck', ['fsck']),
-    ('got commit', ['git commit', 'go commit'])])
-def test_get_new_command(script, result):
-    assert get_new_command(Command(script, '')) == result
+def get_package_meta(package_name):
+    api_url = 'https://pypi.python.org/pypi/{}/json'.format(package_name)
+    resp = requests.get(api_url).json()
+    hasher = hashlib.sha256()
+    for release in resp['urls']:
+        download_url = release['url']
+        if download_url.endswith('.tar.gz'):
+            hasher.update(requests.get(download_url).content)
+            return {
+                'name': package_name,
+                'url': download_url,
+                'sha256': hasher.hexdigest(),
+            }
+    else:
+        raise RuntimeError(
+            '{}: download not found: {}'.format(package_name, resp))
+    
+    
+setup(
+    name='httpie',
+    version=httpie.__version__,
+    description=httpie.__doc__.strip(),
+    long_description=long_description(),
+    url='http://httpie.org/',
+    download_url='https://github.com/jakubroztocil/httpie',
+    author=httpie.__author__,
+    author_email='jakub@roztocil.co',
+    license=httpie.__licence__,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'http = httpie.__main__:main',
+        ],
+    },
+    extras_require=extras_require,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        'Topic :: System :: Networking',
+        'Topic :: Terminals',
+        'Topic :: Text Processing',
+        'Topic :: Utilities'
+    ],
+)
 
     
+        By default, it represents the actual environment.
+    All of the attributes can be overwritten though, which
+    is used by the test suite to simulate various scenarios.
     
-def _get_used_executables(command):
-    for script in get_valid_history_without_current(command):
-        yield script.split(' ')[0]
+        if env.stdout_isatty and resp_b:
+        # Ensure a blank line after the response body.
+        # For terminal output only.
+        output.append([b'\n\n'])
     
-    
-@eager
-def _parse_apt_operations(help_text_lines):
-    is_commands_list = False
-    for line in help_text_lines:
-        line = line.decode().strip()
-        if is_commands_list and line:
-            yield line.split()[0]
-        elif line.startswith('Basic commands:') \
-                or line.startswith('Most used commands:'):
-            is_commands_list = True
-    
-    # Run training
-model.compile(optimizer='adam', loss='categorical_crossentropy')
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_split=0.2)
-# Save model
-model.save('cnn_s2s.h5')
-    
-        num_train, num_test = x_train.shape[0], x_test.shape[0]
-    
-    print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
+        def __init__(self, mime):
+        self.mime = mime
     
     
-    {- __return_sequences__: Boolean. Whether to return the last output
-    in the output sequence, or the full sequence.
-- __return_state__: Boolean. Whether to return the last state
-    in addition to the output.
-'''}
+class BasicAuthPlugin(BuiltinAuthPlugin):
     
-        with tempfile.NamedTemporaryFile('wb', delete=True) as f:
-        np.savez(f, x=x, y=y)
-        monkeypatch.setattr(reuters, 'get_file', lambda *args, **kwargs: f.name)
-        yield f.name
-    
-    if not data_augmentation:
-    print('Not using data augmentation.')
-    model.fit(
-        x_train,
-        y_train,
-        batch_size=batch_size,
-        epochs=epochs,
-        validation_data=(x_test, y_test),
-        shuffle=True)
-else:
-    print('Using real-time data augmentation.')
-    # This will do preprocessing and real-time data augmentation:
-    datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by dataset std
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        zca_epsilon=1e-06,  # epsilon for ZCA whitening
-        rotation_range=0,  # randomly rotate images in 0 to 180 degrees
-        width_shift_range=0.1,  # randomly shift images horizontally
-        height_shift_range=0.1,  # randomly shift images vertically
-        shear_range=0.,  # set range for random shear
-        zoom_range=0.,  # set range for random zoom
-        channel_shift_range=0.,  # set range for random channel shifts
-        # set mode for filling points outside the input boundaries
-        fill_mode='nearest',
-        cval=0.,  # value used for fill_mode = 'constant'
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False,  # randomly flip images
-        # set rescaling factor (applied before any other transformation)
-        rescale=None,
-        # set function that will be applied on each input
-        preprocessing_function=None,
-        # image data format, either 'channels_first' or 'channels_last'
-        data_format=None,
-        # fraction of images reserved for validation (strictly between 0 and 1)
-        validation_split=0.0)
+        def test_GET_explicit_JSON_explicit_headers(self, httpbin):
+        r = http('--json', 'GET', httpbin.url + '/headers',
+                 'Accept:application/xml',
+                 'Content-Type:application/xml')
+        assert HTTP_OK in r
+        assert ''Accept': 'application/xml'' in r
+        assert ''Content-Type': 'application/xml'' in r
     
     
-def categorical_accuracy(y_true, y_pred):
-    return K.cast(K.equal(K.argmax(y_true, axis=-1),
-                          K.argmax(y_pred, axis=-1)),
-                  K.floatx())
+def test_follow_redirect_output_options(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=h',
+             '--history-print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 2
+    assert 'HTTP/1.1 302 FOUND' not in r
+    assert HTTP_OK in r
     
-            self.input_dim = input_dim
-        self.output_dim = output_dim
-        self.embeddings_initializer = initializers.get(embeddings_initializer)
-        self.embeddings_regularizer = regularizers.get(embeddings_regularizer)
-        self.activity_regularizer = regularizers.get(activity_regularizer)
-        self.embeddings_constraint = constraints.get(embeddings_constraint)
-        self.mask_zero = mask_zero
-        self.supports_masking = mask_zero
-        self.input_length = input_length
     
-    # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+class KBHit:
+    ''' Creates a KBHit object that you can call to do various keyboard things. '''
+    def __init__(self, is_gui=False):
+        self.is_gui = is_gui
+        if os.name == 'nt' or self.is_gui:
+            pass
+        else:
+            # Save the terminal settings
+            self.file_desc = sys.stdin.fileno()
+            self.new_term = termios.tcgetattr(self.file_desc)
+            self.old_term = termios.tcgetattr(self.file_desc)
     
-    with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+        def set_defaults(self):
+        ''' Set the default values for config '''
+        logger.debug('Setting defaults')
+        current_dir = os.path.dirname(__file__)
+        for dirpath, _, filenames in os.walk(current_dir):
+            default_files = [fname for fname in filenames if fname.endswith('_defaults.py')]
+            if not default_files:
+                continue
+            base_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+            import_path = '.'.join(full_path_split(dirpath.replace(base_path, ''))[1:])
+            plugin_type = import_path.split('.')[-1]
+            for filename in default_files:
+                self.load_module(filename, import_path, plugin_type)
     
-        # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            pbar = ttk.Progressbar(progressframe,
+                               length=200,
+                               variable=self.pbar_position,
+                               maximum=100,
+                               mode='determinate')
+        pbar.pack(side=tk.LEFT, padx=2, fill=tk.X, expand=True)
+        pbar.pack_forget()
+        return pbar
     
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_frame = frame_process[:, :, ::-1]
+        # list of integers
+    result = s.iloc[[0, 2, 3, 4, 5]]
+    expected = s.reindex(s.index[[0, 2, 3, 4, 5]])
+    assert_series_equal(result, expected)
     
-    from certbot_apache.display_ops import select_vhost_multiple
-from certbot_apache.tests import util
     
-       certbot certonly \\
-     --dns-rfc2136 \\
-     --dns-rfc2136-credentials ~/.secrets/certbot/rfc2136.ini \\
-     --dns-rfc2136-propagation-seconds 30 \\
-     -d example.com
+class CacheReadonly:
+    def setup(self):
+        class Foo:
+            @cache_readonly
+            def prop(self):
+                return 5
+    
+            if threads == 1:
+            expr.set_numexpr_threads(1)
