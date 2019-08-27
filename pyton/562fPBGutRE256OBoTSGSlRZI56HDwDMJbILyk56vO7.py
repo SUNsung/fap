271faa6e7ec4d802bb8bece18093e78c15709ba0
SUNsung <weatherius@gmail.com>
2019-08-28 02:07:24 +0300

    Rewrite [562fPBGutRE256OBoTSGSlRZI56HDwDMJbILyk56vO7.py] File in: 2019-08-28 02:07:24

--- pyton/562fPBGutRE256OBoTSGSlRZI56HDwDMJbILyk56vO7.py
+++ pyton/562fPBGutRE256OBoTSGSlRZI56HDwDMJbILyk56vO7.py
@@ -1,288 +1,195 @@
 
         
-                series = source.get('format')
-        season_number = int_or_none(self._search_regex(
-            r'staffel-(\d+)', url, 'season number', default=None))
-        episode_number = int_or_none(self._search_regex(
-            r'episode-(\d+)', url, 'episode number', default=None))
+        
+def make_setting_element(setting_data, app, fromdocname):
+    refnode = make_refnode(app.builder, fromdocname,
+                           todocname=setting_data['docname'],
+                           targetid=setting_data['refid'],
+                           child=nodes.Text(setting_data['setting_name']))
+    p = nodes.paragraph()
+    p += refnode
     
-            subheading = video.get('subheading')
-        if subheading:
-            title += ' - %s' % subheading
+        def parse(self, response):
+        pass
+
     
-                    for caption in asset.get('caption_metadata', []):
-                    caption_url = caption.get('source_url')
-                    if not caption_url:
-                        continue
-                    subtitles.setdefault(caption.get('language', 'en'), []).append({
-                        'url': caption_url,
-                        'ext': determine_ext(caption_url, 'vtt'),
-                    })
-            elif asset.get('type') == 'image':
-                asset_location = asset.get('location')
-                if not asset_location:
-                    continue
-                thumbnails.append({
-                    'url': asset_location,
-                    'width': int_or_none(asset.get('width')),
-                    'height': int_or_none(asset.get('height')),
-                })
-        self._sort_formats(formats)
+    # Scrapy version
+import pkgutil
+__version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
+version_info = tuple(int(v) if v.isdigit() else v
+                     for v in __version__.split('.'))
+del pkgutil
     
-    secret_msg = b'Secret message goes here'
-    
-        # Get the version from youtube_dl/version.py without importing the package
-    exec(compile(open('youtube_dl/version.py').read(),
-                 'youtube_dl/version.py', 'exec'))
-    
-        def test_cache(self):
-        ydl = FakeYDL({
-            'cachedir': self.test_dir,
-        })
-        c = Cache(ydl)
-        obj = {'x': 1, 'y': ['ä', '\\a', True]}
-        self.assertEqual(c.load('test_cache', 'k.'), None)
-        c.store('test_cache', 'k.', obj)
-        self.assertEqual(c.load('test_cache', 'k2'), None)
-        self.assertFalse(_is_empty(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), obj)
-        self.assertEqual(c.load('test_cache', 'y'), None)
-        self.assertEqual(c.load('test_cache2', 'k.'), None)
-        c.remove()
-        self.assertFalse(os.path.exists(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), None)
-    
-        def test_guess_when_method_set_but_invalid_and_item_exists(self):
-        self.parser.args = argparse.Namespace()
-        self.parser.args.method = 'http://example.com/'
-        self.parser.args.url = 'new_item=a'
-        self.parser.args.items = [
-            KeyValue(
-                key='old_item', value='b', sep='=', orig='old_item=b')
-        ]
-        self.parser.args.ignore_stdin = False
-    
-        Exceptions are propagated.
-    
-    
-PACKAGES = [
-    'httpie',
-    'Pygments',
-    'requests',
-    'certifi',
-    'urllib3',
-    'idna',
-    'chardet',
-    'PySocks',
-]
-    
+        def long_desc(self):
+        '''A long description of the command. Return short description when not
+        available. It cannot contain newlines, since contents will be formatted
+        by optparser which removes newlines and wraps text.
         '''
-    is_windows = is_windows
-    config_dir = DEFAULT_CONFIG_DIR
-    stdin = sys.stdin
-    stdin_isatty = stdin.isatty()
-    stdin_encoding = None
-    stdout = sys.stdout
-    stdout_isatty = stdout.isatty()
-    stdout_encoding = None
-    stderr = sys.stderr
-    stderr_isatty = stderr.isatty()
-    colors = 256
-    if not is_windows:
-        if curses:
-            try:
-                curses.setupterm()
-                colors = curses.tigetnum('colors')
-            except curses.error:
-                pass
+        return self.short_desc()
+    
+    
+class _BenchSpider(scrapy.Spider):
+    '''A spider that follows all links'''
+    name = 'follow'
+    total = 10000
+    show = 20
+    baseurl = 'http://localhost:8998'
+    link_extractor = LinkExtractor()
+    
+        def run(self, args, opts):
+        if len(args) < 1:
+            raise UsageError()
+        elif len(args) > 1:
+            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
+        spname = args[0]
+    
+        def short_desc(self):
+        return 'Fetch a URL using the Scrapy downloader'
+    
+        def run(self, args, opts):
+        if len(args) != 1:
+            raise UsageError()
+        filename = args[0]
+        if not os.path.exists(filename):
+            raise UsageError('File not found: %s\n' % filename)
+        try:
+            module = _import_file(filename)
+        except (ImportError, ValueError) as e:
+            raise UsageError('Unable to load %r: %s\n' % (filename, e))
+        spclasses = list(iter_spider_classes(module))
+        if not spclasses:
+            raise UsageError('No spider found in file: %s\n' % filename)
+        spidercls = spclasses.pop()
+    
+        def run(self, args, opts):
+        settings = self.crawler_process.settings
+        if opts.get:
+            s = settings.get(opts.get)
+            if isinstance(s, BaseSettings):
+                print(json.dumps(s.copy_to_dict()))
+            else:
+                print(s)
+        elif opts.getbool:
+            print(settings.getbool(opts.getbool))
+        elif opts.getint:
+            print(settings.getint(opts.getint))
+        elif opts.getfloat:
+            print(settings.getfloat(opts.getfloat))
+        elif opts.getlist:
+            print(settings.getlist(opts.getlist))
+
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('--verbose', '-v', dest='verbose', action='store_true',
+            help='also display twisted/python/platform info (useful for bug reports)')
+    
+        class _v20_S3Connection(S3Connection):
+        '''A dummy S3Connection wrapper that doesn't do any synchronous download'''
+        def _mexe(self, http_request, *args, **kwargs):
+            http_request.authorize(connection=self)
+            return http_request.headers
+    
+    from scrapy import signals
+    
+        def __init__(self):
+        ca_certs = os.path.join(current_path, 'cacert.pem')
+        openssl_context = SSLContext(
+            logger, ca_certs=ca_certs,
+            cipher_suites=['ALL', '!RC4-SHA', '!ECDHE-RSA-RC4-SHA', '!ECDHE-RSA-AES128-GCM-SHA256',
+                           '!AES128-GCM-SHA256', '!ECDHE-RSA-AES128-SHA', '!AES128-SHA']
+        )
+        host_manager = HostManagerBase()
+        connect_creator = ConnectCreator(logger, config, openssl_context, host_manager,
+                                         debug=True)
+        self.check_ip = CheckIp(logger, config, connect_creator)
+    
+    
+    def getDescription(self):
+        return 'n/a'
+    
+     	# The index of the character relative to the beginning of the
+        # line 0..n-1
+        self.charPositionInLine = 0
+    
+        def test_local_visibility(self):
+        v = 'Local variable'
+        expected = {0: 'Local variable', 1: 'Local variable',
+                    2: 'Local variable', 3: 'Local variable',
+                    4: 'Local variable', 5: 'Local variable',
+                    6: 'Local variable', 7: 'Local variable',
+                    8: 'Local variable', 9: 'Local variable'}
+        actual = {k: v for k in range(10)}
+        self.assertEqual(actual, expected)
+        self.assertEqual(v, 'Local variable')
+    
+    def run_bandwidth_client(**kwargs):
+    cmd_line = [sys.executable, '-E', os.path.abspath(__file__)]
+    cmd_line.extend(['--bwclient', repr(kwargs)])
+    return subprocess.Popen(cmd_line) #, stdin=subprocess.PIPE,
+                            #stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    
+    class BytesSubclass(bytes):
+    pass
+    
+    class ExtensionTests(unittest.TestCase):
+    def CheckScriptStringSql(self):
+        con = sqlite.connect(':memory:')
+        cur = con.cursor()
+        cur.executescript('''
+            -- bla bla
+            /* a stupid comment */
+            create table a(i);
+            insert into a(i) values (5);
+            ''')
+        cur.execute('select i from a')
+        res = cur.fetchone()[0]
+        self.assertEqual(res, 5)
+    
+    __author__ = 'Brian Quinlan (brian@sweetapp.com)'
+    
+        The pattern may contain simple shell-style wildcards a la
+    fnmatch. However, unlike fnmatch, filenames starting with a
+    dot are special cases that are not matched by '*' and '?'
+    patterns.
+    
+    This module provides generic, low- and high-level interfaces for
+creating temporary files and directories.  All of the interfaces
+provided by this module can be used without fear of race conditions
+except for 'mktemp'.  'mktemp' is subject to race conditions and
+should not be used; it is provided for backward compatibility only.
+    
+        good_nonce = jose.encode_b64jose(b'foo')
+    wrong_nonce = u'F'
+    # Following just makes sure wrong_nonce is wrong
+    try:
+        jose.b64decode(wrong_nonce)
+    except (ValueError, TypeError):
+        assert True
     else:
-        # noinspection PyUnresolvedReferences
-        import colorama.initialise
-        stdout = colorama.initialise.wrap_stream(
-            stdout, convert=None, strip=None,
-            autoreset=True, wrap=True
-        )
-        stderr = colorama.initialise.wrap_stream(
-            stderr, convert=None, strip=None,
-            autoreset=True, wrap=True
-        )
-        del colorama
+        assert False  # pragma: no cover
     
-        @property
-    def content_type(self):
-        '''Return the message content type.'''
-        ct = self._orig.headers.get('Content-Type', '')
-        if not isinstance(ct, str):
-            ct = ct.decode('utf8')
-        return ct
+        def test_without_eab_arguments(self):
+        with mock.patch('certbot.client.acme_client.BackwardsCompatibleClientV2') as mock_client:
+            mock_client().external_account_required.side_effect = self._false_mock
+            with mock.patch('certbot.eff.handle_subscription'):
+                target = 'certbot.client.messages.ExternalAccountBinding.from_data'
+                with mock.patch(target) as mock_eab_from_data:
+                    self.config.eab_kid = None
+                    self.config.eab_hmac_key = None
+                    self._call()
     
-        Assumes `from __future__ import division`.
+    .. caution::
+   You should protect this TSIG key material as it can be used to potentially
+   add, update, or delete any record in the target DNS server. Users who can
+   read this file can use these credentials to issue arbitrary API calls on
+   your behalf. Users who can cause Certbot to run using these credentials can
+   complete a ``dns-01`` challenge to acquire new certificates or revoke
+   existing certificates for associated domains, even if those domains aren't
+   being managed by this server.
     
-    
-with codecs.open(JSON_FILE_PATH, encoding='utf8') as f:
-    JSON_FILE_CONTENT = f.read()
-    
-    
-@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
-def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
-    r = http(follow_flag, httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 1
-    assert HTTP_OK in r
-    
-                ret[i, :] = i_label, i_score
-    
-    
-def selu(x):
-    '''缩放型指数线性单元'''
-    alpha = 1.6732632423543772848170429916717
-    scale = 1.0507009873554804934193349852946
-    o = tf.nn.elu(x)
-    return scale * tf.where(x > 0, o, alpha * o)
-
-    
-    References:
-    https://github.com/philipperemy/keras-attention-mechanism
-'''
-    
-        return o
-    
-            # u_tilde(u~): context to question attended query vectors
-        u_tilde = tf.matmul(softmax(S), u)  # [N, T, d]
-    
-    SPLIT_LINE = '-----------'
-    
-    
-class RFC3339Field(jose.Field):
-    '''RFC3339 field encoder/decoder.
-    
-        def test_get_sni_addr(self):
-        from certbot_apache.obj import Addr
-        self.assertEqual(
-            self.addr.get_sni_addr('443'), Addr.fromstring('*:443'))
-        self.assertEqual(
-            self.addr.get_sni_addr('225'), Addr.fromstring('*:225'))
-        self.assertEqual(
-            self.addr1.get_sni_addr('443'), Addr.fromstring('127.0.0.1'))
-    
-    
-class IntegrationTestsContext(certbot_context.IntegrationTestsContext):
-    '''General fixture describing a certbot-nginx integration tests context'''
-    def __init__(self, request):
-        super(IntegrationTestsContext, self).__init__(request)
-    
-        command = [
-        'certbot',
-        '--server', directory_url,
-        '--no-verify-ssl',
-        '--http-01-port', str(http_01_port),
-        '--https-port', str(tls_alpn_01_port),
-        '--manual-public-ip-logging-ok',
-        '--config-dir', config_dir,
-        '--work-dir', os.path.join(workspace, 'work'),
-        '--logs-dir', os.path.join(workspace, 'logs'),
-        '--non-interactive',
-        '--no-redirect',
-        '--agree-tos',
-        '--register-unsafely-without-email',
-        '--debug',
-        '-vv'
-    ]
-    
-    # Windows
-if os.name == 'nt':
-    import msvcrt  # pylint: disable=import-error
-    
-        # incorporate derivatives flowing through the checkpointed nodes
-    checkpoints_sorted_lists = tf_toposort(checkpoints, within_ops=fwd_ops)
-    for ts in checkpoints_sorted_lists[::-1]:
-        debug_print('Processing list {}'.format(ts))
-        checkpoints_other = [r for r in checkpoints if r not in ts]
-        checkpoints_disconnected_other = [checkpoints_disconnected[r] for r in checkpoints_other]
-    
-            r_jaw = (self.landmarks[0:9], self.landmarks[17:18])
-        l_jaw = (self.landmarks[8:17], self.landmarks[26:27])
-        r_cheek = (self.landmarks[17:20], self.landmarks[8:9])
-        l_cheek = (self.landmarks[24:27], self.landmarks[8:9])
-        nose_ridge = (self.landmarks[19:25], self.landmarks[8:9],)
-        r_eye = (self.landmarks[17:22],
-                 self.landmarks[27:28],
-                 self.landmarks[31:36],
-                 self.landmarks[8:9])
-        l_eye = (self.landmarks[22:27],
-                 self.landmarks[27:28],
-                 self.landmarks[31:36],
-                 self.landmarks[8:9])
-        nose = (self.landmarks[27:31], self.landmarks[31:36])
-        parts = [r_jaw, l_jaw, r_cheek, l_cheek, nose_ridge, r_eye, l_eye, nose]
-    
-        def load_module(self, filename, module_path, plugin_type):
-        ''' Load the defaults module and add defaults '''
-        logger.debug('Adding defaults: (filename: %s, module_path: %s, plugin_type: %s',
-                     filename, module_path, plugin_type)
-        module = os.path.splitext(filename)[0]
-        section = '.'.join((plugin_type, module.replace('_defaults', '')))
-        logger.debug('Importing defaults module: %s.%s', module_path, module)
-        mod = import_module('{}.{}'.format(module_path, module))
-        self.add_section(title=section, info=mod._HELPTEXT)  # pylint:disable=protected-access
-        for key, val in mod._DEFAULTS.items():  # pylint:disable=protected-access
-            self.add_item(section=section, title=key, **val)
-        logger.debug('Added defaults: %s', section)
-
-    
-        def set_config(self, configfile, config):
-        ''' Set the config to either global config or passed in config '''
-        section = '.'.join(self.__module__.split('.')[-2:])
-        if config is None:
-            retval = get_config(section, configfile)
-        else:
-            config.section = section
-            retval = config.config_dict
-            config.section = None
-        logger.debug('Config: %s', retval)
-        return retval
-    
-            mask = BlurMask(self.config['type'],
-                        mask,
-                        self.config['radius'],
-                        self.config['passes']).blurred
-        logger.debug('Built box mask. Shape: %s', mask.shape)
-        return mask
-    
-        def set_config(self, configfile, config):
-        ''' Set the config to either global config or passed in config '''
-        section = '.'.join(self.__module__.split('.')[-2:])
-        if config is None:
-            logger.debug('Loading base config')
-            retval = get_config(section, configfile=configfile)
-        else:
-            logger.debug('Loading passed in config')
-            config.section = section
-            retval = config.config_dict
-            config.section = None
-        logger.debug('Config: %s', retval)
-        return retval
-    
-        Defaults files should be named <plugin_name>_defaults.py
-    Any items placed into this file will automatically get added to the relevant config .ini files
-    within the faceswap/config folder.
-    
-        The following variables should be defined:
-        _HELPTEXT: A string describing what this plugin does
-        _DEFAULTS: A dictionary containing the options, defaults and meta information. The
-                   dictionary should be defined as:
-                       {<option_name>: {<metadata>}}
-    
-        def get_save_kwargs(self):
-        ''' Return the save parameters for the file format '''
-        filetype = self.config['format']
-        kwargs = dict()
-        if filetype in ('gif', 'jpg', 'png'):
-            kwargs['optimize'] = self.config['optimize']
-        if filetype == 'gif':
-            kwargs['interlace'] = self.config['gif_interlace']
-        if filetype == 'png':
-            kwargs['compress_level'] = self.config['png_compress_level']
-        if filetype == 'tif':
-            kwargs['compression'] = self.config['tif_compression']
-        logger.debug(kwargs)
-        return kwargs
+                # Authoritative Answer bit should be set
+            if (rcode == dns.rcode.NOERROR and response.get_rrset(response.answer,
+                domain, dns.rdataclass.IN, dns.rdatatype.SOA) and response.flags & dns.flags.AA):
+                logger.debug('Received authoritative SOA response for %s', domain_name)
+                return True
