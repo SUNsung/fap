7ff06958c5bfefd79f0357f6e03f36234f69aba3
SUNsung <weatherius@gmail.com>
2019-08-27 04:51:42 +0300

    Rewrite [13cpAayw8vE613UvzFPhZ9tpM13TXo3C0GRxao13bYI.rb] File in: 2019-08-27 04:51:42

--- ruby/13cpAayw8vE613UvzFPhZ9tpM13TXo3C0GRxao13bYI.rb
+++ ruby/13cpAayw8vE613UvzFPhZ9tpM13TXo3C0GRxao13bYI.rb
@@ -1,192 +1,18 @@
 
         
-                @version_value = '#{major}.#{minor}.#{patch}.#{new_appendix}'
-      end
-    
-        def run(path, color = nil)
-      unless color
-        color = Frameit::Color::BLACK
-        color = Frameit::Color::SILVER if Frameit.config[:white] || Frameit.config[:silver]
-        color = Frameit::Color::GOLD if Frameit.config[:gold]
-        color = Frameit::Color::ROSE_GOLD if Frameit.config[:rose_gold]
-      end
-    
-              disabled_services_object = self.service_object
-          disabled_services.each do |k, v|
-            disabled_services_object.__hash__[k] = true
-            disabled_services_object.send('#{k}=', v)
-          end
-          Produce::Service.disable(disabled_services_object, nil) unless disabled_services.empty?
-        end
-      end
-    
-          it 'inspect works' do
-        TunesStubbing.itc_stub_app_uninclude_future_territories
-    
-      context 'authenticated' do
-    before { subject.login(username, password) }
-    describe '#teams' do
-      let(:teams) { subject.teams }
-      it 'returns the list of available teams' do
-        expect(teams).to be_instance_of(Array)
-        expect(teams.first.keys).to eq(['status', 'teamId', 'type', 'extendedTeamAttributes', 'teamAgent', 'memberships', 'currentTeamMember', 'name'])
-      end
-    end
-    
-          def pipe(device_type, language, locale)
-        not_implemented(__method__)
-      end
-    
-          def self.details
-        [
-          'This action will return the current build number set on your project.',
-          'You first have to set up your Xcode project, if you haven't done it already: [https://developer.apple.com/library/ios/qa/qa1827/_index.html](https://developer.apple.com/library/ios/qa/qa1827/_index.html).'
-        ].join('\n')
-      end
-    
-          def self.deprecated_notes
-        [
-          'You are using legacy `shenzhen` to build your app, which will be removed soon!',
-          'It is recommended to upgrade to _gym_.',
-          'To do so, just replace `ipa(...)` with `gym(...)` in your Fastfile.',
-          'To make code signing work, follow [https://docs.fastlane.tools/codesigning/xcode-project/](https://docs.fastlane.tools/codesigning/xcode-project/).'
-        ].join('\n')
-      end
+            def to_json(options = {})
+      super except: [:c, :e]
     end
   end
-end
-# rubocop:enable Lint/AssignmentInCondition
-
-    
-          def_node_matcher :extract_shared_values_key?, <<-PATTERN
-        (const (const nil? :SharedValues) $_)
-      PATTERN
-    
-            it 'passes when `apple_id` is correct' do
-          options = {
-            username: 'username@example.com',
-            apple_id: '123456789'
-          }
-          pilot_config = FastlaneCore::Configuration.create(Pilot::Options.available_options, options)
-          expect(pilot_config[:apple_id]).to eq('123456789')
-        end
-      end
+  class CsvRenderable
+    def to_csv
+      'c,s,v'
     end
   end
-end
-
-    
-        it 'When dep ensure is used without `-vendor-only`' do
-      expect_offense(<<~RUBY)
-        class Foo < Formula
-          url 'https://brew.sh/foo-1.0.tgz'
-          homepage 'https://brew.sh'
-    
-          expect(f.class.stable.deps.first.name).to eq('foo')
-      expect(f.class.devel.deps.first.name).to eq('foo')
-      expect(f.class.head.deps.first.name).to eq('foo')
+  class TestController < ActionController::Base
+    def render_simon_says
+      render simon: 'foo'
     end
-    
-        it 'works with tags' do
-      spec.uses_from_macos('foo' => :head, :after => :el_capitan)
-    
-          def separate_header_package?
-        version >= '10'
-      end
-    
-        context 'with referer set' do
-      let(:specs) { { referer: 'https://somehost/also' } }
-    
-      apply_simple_captcha :message => I18n.t('simple_captcha.message.failed'), :add_to_base => true
-    
-    if rails_env == 'production'
-  config('without test:development')
-elsif rails_env == 'test'
-  config('without development')
-end
-    
-          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
-      def create
-        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
-        client = Api::OpenidConnect::OAuthApplication.register! registrar
-        render json: client.as_json(root: false)
-      end
-    
-          def create
-        req = Rack::Request.new(request.env)
-        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
-          handle_jwt_bearer(req)
-        end
-        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
-        headers.each {|name, value| response.headers[name] = value }
-        nil
-      end
-    
-      def create
-    # Contacts autocomplete does not work the same way on mobile and desktop
-    # Mobile returns contact ids array while desktop returns person id
-    # This will have to be removed when mobile autocomplete is ported to Typeahead
-    recipients_param, column = [%i(contact_ids id), %i(person_ids person_id)].find {|param, _| params[param].present? }
-    if recipients_param
-      person_ids = current_user.contacts.mutual.where(column => params[recipients_param].split(',')).pluck(:person_id)
-    end
-    
-            if deprecated_pods.any?
-          UI.section 'The following pods are deprecated:' do
-            deprecated_pods.each do |spec|
-              if spec.deprecated_in_favor_of
-                UI.puts '- #{spec.name}' \
-                  ' (in favor of #{spec.deprecated_in_favor_of})'
-              else
-                UI.puts '- #{spec.name}'
-              end
-            end
-          end
-        end
-      end
-    
-        export LANG=en_US.UTF-8
-    DOC
-  else
-    STDERR.puts <<-DOC
-    \e[33mWARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
-    Consider adding the following to ~/.profile:
-    
-        # Sets up a lighweight master repo in `tmp/cocoapods/master` with the
-    # contents of `spec/fixtures/spec-repos/test_repo`.
-    #
-    def set_up_old_test_repo
-      require 'fileutils'
-      test_old_repo_path.mkpath
-      origin = ROOT + 'spec/fixtures/spec-repos/test_repo/.'
-      destination = tmp_repos_path + '../master'
-      FileUtils.cp_r(origin, destination)
-      FileUtils.rm_r(destination + './.git')
-      repo_make('../master')
-    end
-    
-              let(:expected_plugin_args) do
-            {
-                'key' => '%{host}',
-                'filter_options' => {
-                    'string'  => 'string',
-                    'integer' => 3,
-                    'nested'  => { # <-- This is nested hash!
-                        'string'  => 'nested-string',
-                        'integer' => 7,
-                        'quoted-key-string' => 'nested-quoted-key-string',
-                        'quoted-key-integer' => 31,
-                        'deep'    => { # <-- This is deeper nested hash!
-                            'string'  => 'deeply-nested-string',
-                            'integer' => 127,
-                            'quoted-key-string' => 'deeply-nested-quoted-key-string',
-                            'quoted-key-integer' => 8191
-                        }
-                    }
-                },
-                'ttl' => 5
-            }
-          end
     
     require 'clamp'
 require 'pluginmanager/util'
@@ -202,4 +28,24 @@ require 'pluginmanager/prepare_offline_pack'
 require 'pluginmanager/proxy_support'
 configure_proxy
     
-            PluginManager.ui.debug('Looking if package named: #{plugin_name} exists at #{uri}')
+              it 'fails when installing a non logstash plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install  bundler')
+            expect(command).not_to install_successfully
+          end
+    
+          it 'list the plugin with his version' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name} \(\d+\.\d+.\d+\)/)
+      end
+    end
+  end
+end
+
+    
+        context 'update a specific plugin' do
+      it 'has executed successfully' do
+        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
+        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
+        expect(logstash).not_to have_installed?(plugin_name, previous_version)
+      end
+    end
