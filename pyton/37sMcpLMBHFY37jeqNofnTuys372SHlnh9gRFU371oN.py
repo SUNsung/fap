5aa41018029183fd1f5b8228f20e9c73c812953c
SUNsung <weatherius@gmail.com>
2019-08-27 08:52:02 +0300

    Rewrite [37sMcpLMBHFY37jeqNofnTuys372SHlnh9gRFU371oN.py] File in: 2019-08-27 08:52:02

--- pyton/37sMcpLMBHFY37jeqNofnTuys372SHlnh9gRFU371oN.py
+++ pyton/37sMcpLMBHFY37jeqNofnTuys372SHlnh9gRFU371oN.py
@@ -1,116 +1,107 @@
 
         
         
-def to_native_string(string, encoding='ascii'):
-    '''Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
+def check_format(filename):
     '''
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        if is_py2:
-            out = string.encode(encoding)
-        else:
-            out = string.decode(encoding)
-    
-        def test_repr(self):
-        assert repr(self.case_insensitive_dict) == '{'Accept': 'application/json'}'
-    
-            #: File-like object representation of response (for advanced usage).
-        #: Use of ``raw`` requires that ``stream=True`` be set on the request.
-        # This requirement does not apply for use internally to Requests.
-        self.raw = None
-    
-    - cloudwatchevent_rule:
-    name: MyDisabledCronTask
-    schedule_expression: 'rate(5 minutes)'
-    description: Run my disabled scheduled task
-    state: disabled
-    targets:
-      - id: MyOtherTargetId
-        arn: arn:aws:lambda:us-east-1:123456789012:function:MyFunction
-        input: '{'foo': 'bar'}'
-    
-            elif desired_state == 'disabled':
-            if current_state == HOST_ABSENT:
-                self.fail(msg='absent host cannot be put in disabled state')
-            elif current_state in [HOST_STATES.MONITORED, HOST_STATES.OFFLINE]:
-                if one.host.status(host.ID, HOST_STATUS.DISABLED):
-                    self.wait_for_host_state(host, [HOST_STATES.DISABLED])
-                    result['changed'] = True
-                else:
-                    self.fail(msg='could not disable host')
-            elif current_state in [HOST_STATES.DISABLED]:
-                pass
+    validates that each line is formatted correctly,
+    appending to error list as needed
+    '''
+    with open(filename) as fp:
+        lines = list(line.rstrip() for line in fp)
+    check_alphabetical(lines)
+    # START Check Entries
+    num_in_category = min_entries_per_section + 1
+    category = ''
+    category_line = 0
+    for line_num, line in enumerate(lines):
+        if section_title_re.match(line):
+            title_links.append(section_title_re.match(line).group(1))
+        # check each section for the minimum number of entries
+        if line.startswith(anchor):
+            match = anchor_re.match(line)
+            if match:
+                if match.group(1) not in title_links:
+                    add_error(line_num, 'section header ({}) not added as a title link'.format(match.group(1)))
             else:
-                self.fail(msg='unknown host state %s, cowardly refusing to change state to disable' % current_state_name)
-    
-        vca = vca_login(module)
-    
-    
-def rule_to_string(rule):
-    strings = list()
-    for key, value in rule.items():
-        strings.append('%s=%s' % (key, value))
-    return ', '.join(strings)
-    
-    # Ensure role is absent
-- ipa_role:
-    name: dba
-    state: absent
-    ipa_host: ipa.example.com
-    ipa_user: admin
-    ipa_pass: topsecret
-'''
-    
-        # Insert state-specific attributes to body
-    if state == 'started':
-        for k in ('source_system', 'env', 'owner', 'description'):
-            v = module.params[k]
-            if v is not None:
-                body[k] = v
-    
-        def manage(self):
-        rc, out = self._exec(['list'])
-        if rc != 0:
-            self.module.fail_json(msg='Unable to list icinga2 features. '
-                                      'Ensure icinga2 is installed and present in binary path.')
-    
-            running_status = get_status()
-        while running_status == '' or 'pending' in running_status or 'initializing' in running_status:
-            if time.time() >= timeout_time:
-                module.fail_json(
-                    msg='waited too long for 'pending', or 'initiating' status to go away ({0})'.format(
-                        running_status
-                    ),
-                    state=state
-                )
+                add_error(line_num, 'section header is not formatted correctly')
+            if num_in_category < min_entries_per_section:
+                add_error(category_line, '{} section does not have the minimum {} entries (only has {})'.format(
+                    category, min_entries_per_section, num_in_category))
+            category = line.split(' ')[1]
+            category_line = line_num
+            num_in_category = 0
+            continue
+        # skips lines that we do not care about
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        num_in_category += 1
+        segments = line.split('|')[1:-1]
+        if len(segments) < num_segments:
+            add_error(line_num, 'entry does not have all the required sections (have {}, need {})'.format(
+                len(segments), num_segments))
+            continue
+        # START Global
+        for segment in segments:
+            # every line segment should start and end with exactly 1 space
+            if len(segment) - len(segment.lstrip()) != 1 or len(segment) - len(segment.rstrip()) != 1:
+                add_error(line_num, 'each segment must start and end with exactly 1 space')
+        # END Global
+        segments = [seg.strip() for seg in segments]
+        check_entry(line_num, segments)
+    # END Check Entries
     
     
-def main():
-    global module
-    module = AnsibleModule(
-        argument_spec=dict(
-            device=dict(required=True, aliases=['host', 'name']),
-            landscape=dict(required=True),
-            state=dict(choices=['present', 'absent'], default='present'),
-            community=dict(required=True, no_log=True),
-            agentport=dict(type='int', default=161),
-            url=dict(required=True, aliases=['oneclick_url']),
-            url_username=dict(required=True, aliases=['oneclick_user']),
-            url_password=dict(required=True, no_log=True, aliases=['oneclick_password']),
-            use_proxy=dict(type='bool', default='yes'),
-            validate_certs=dict(type='bool', default='yes'),
-        ),
-        required_if=[('state', 'present', ['community'])],
-        supports_check_mode=True
-    )
+@pytest.mark.parametrize('override, old, new', [
+    ({'key': 'val'}, {}, {'key': 'val'}),
+    ({'key': 'new-val'}, {'key': 'val'}, {'key': 'val'}),
+    ({'key': 'new-val', 'unset': 'unset'}, {'key': 'val'}, {'key': 'val', 'unset': 'unset'})])
+def test_default_settings(settings, override, old, new):
+    settings.clear()
+    settings.update(old)
+    default_settings(override)(lambda _: _)(None)
+    assert settings == new
     
-            self.assertEqual(jws, JWS.from_json(jws.to_json()))
     
-            self.vhost1b = VirtualHost(
-            'filep', 'vh_path', set([self.addr1]), False, False, 'localhost')
+class TestGeneric(object):
+    @pytest.fixture
+    def shell(self):
+        return Generic()
     
-    setup(
-    name='itchat',
+        import sys
+    from mymodule import entry_function
+    sys.exit(entry_function())
+    
+    from thefuck.rules.az_cli import match, get_new_command
+from thefuck.types import Command
+    
+        def parse(self, response):
+        pass
+
+    
+        def process_options(self, args, opts):
+        try:
+            self.settings.setdict(arglist_to_dict(opts.set),
+                                  priority='cmdline')
+        except ValueError:
+            raise UsageError('Invalid -s value, use -s NAME=VALUE', print_help=False)
+    
+    logger = logging.getLogger(__name__)
+    
+    
+IPv4 = CheckNetwork('IPv4')
+IPv4.urls = [
+            'https://www.microsoft.com',
+            'https://www.apple.com',
+            'https://code.jquery.com',
+            'https://cdn.bootcss.com',
+            'https://cdnjs.cloudflare.com']
+IPv4.triger_check_network()
+    
+    	# Track the line at which the error occurred in case this is
+	# generated from a lexer.  We need to track this since the
+        # unexpected char doesn't carry the line info.
+        self.line = None
+    
+                # The char position into the input buffer where this token stops
+            # This is the index of the last char, *not* the index after it!
+            self.stop = stop
