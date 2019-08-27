c9c3ae37139b2ae4de6622672f3fb26780f65218
SUNsung <weatherius@gmail.com>
2019-08-27 14:24:52 +0300

    Rewrite [06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py] File in: 2019-08-27 14:24:52

--- pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
+++ pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
@@ -1,278 +1,70 @@
 
         
-            # Then we cluster the lines together as blocks
-    # Each block represents a collection of lines that should be sorted
-    # This was done by assuming only links ([...](...)) are meant to be sorted
-    # Clustering is done by indentation
-    blocks = []
-    last_indent = None
-    for line in read_me:
-        s_line = line.lstrip()
-        indent = len(line) - len(s_line)
+                (2016-01, shopping), 25
+        (2016-01, shopping), 100
+        (2016-01, gas), 50
+        '''
+        timestamp, category, amount = line.split('\t')
+        period = self. extract_year_month(timestamp)
+        if period == self.current_year_month():
+            yield (period, category), amount
     
-        fpath = os.path.join(path, 'test_batch')
-    x_test, y_test = load_batch(fpath)
+        def extract_url(self, line):
+        '''Extract the generated url from the log line.'''
+        pass
     
-    
-def deserialize(config, custom_objects=None):
-    return deserialize_keras_object(config,
-                                    module_objects=globals(),
-                                    custom_objects=custom_objects,
-                                    printable_module_name='regularizer')
-    
-        def recurrent_conv(self, x, w):
-        conv_out = K.conv2d(x, w, strides=(1, 1),
-                            padding='same',
-                            data_format=self.data_format)
-        return conv_out
-    
-    # input image dimensions
-img_rows, img_cols = 28, 28
-# number of convolutional filters to use
-filters = 32
-# size of pooling area for max pooling
-pool_size = 2
-# convolution kernel size
-kernel_size = 3
-    
-    print('Loading data...')
-(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
-                                                         test_split=0.2)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
-    
-    for i, backend in enumerate(backends):
-    for prefix, path in prefix_map.items():
-        if backend.startswith(prefix):
-            models = __import__(path, globals(), locals(), ['models'], level=0).models
-            if i == 0:
-                # If this is the first iteration of the loop, we need to
-                # emulate ``from x import *`` by copying the module contents
-                # into the local (module) scope. This follows the same rules as
-                # the import statement itself, as defined in the refrence docs:
-                # https://docs.python.org/2.7/reference/simple_stmts.html#import
-                if getattr(models, '__all__', None) is not None:
-                    predicate = lambda name: name in models.__all__
-                else:
-                    predicate = lambda name: not name.startswith('_')
-                locals().update({k: v for k, v in vars(models).items() if predicate(k)})
-            break
-    else:
-        raise ImproperlyConfigured('Found unknown tagstore backend '%s'' % backend)
+        def remove(self, key):
+        hash_index = self._hash_function(key)
+        for index, item in enumerate(self.table[hash_index]):
+            if item.key == key:
+                del self.table[hash_index][index]
+                return
+        raise KeyError('Key not found')
 
     
-            # Adding unique constraint on 'TagValue', fields ['project_id',
-        # 'environment_id', '_key', 'value']
-        db.create_unique(u'tagstore_tagvalue', ['project_id', 'environment_id', 'key_id', 'value'])
-    
-    
-@instrumented_task(
-    name='sentry.tasks.process_buffer.process_pending',
-    queue='buffers.process_pending',
-)
-def process_pending(partition=None):
-    '''
-    Process pending buffers.
-    '''
-    from sentry import buffer
-    from sentry.app import locks
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-    
-        # Find which gt ROI each ex ROI has max overlap with:
-    # this will be the ex ROI's gt target
-    gt_assignment = ex_gt_overlaps.argmax(axis=1)
-    gt_rois = rois[gt_inds[gt_assignment], :]
-    ex_rois = rois[ex_inds, :]
-    # Use class '1' for all boxes if using class_agnostic_bbox_reg
-    targets[ex_inds, 0] = (
-        1 if cfg.MODEL.CLS_AGNOSTIC_BBOX_REG else labels[ex_inds])
-    targets[ex_inds, 1:] = box_utils.bbox_transform_inv(
-        ex_rois, gt_rois, cfg.MODEL.BBOX_REG_WEIGHTS)
-    return targets
-    
-    
-def basic_bn_shortcut(model, prefix, blob_in, dim_in, dim_out, stride):
-    ''' For a pre-trained network that used BN. An AffineChannel op replaces BN
-    during fine-tuning.
-    '''
-    
-            if model.train:
-            loss_gradients = {}
-            for lg in head_loss_gradients.values():
-                if lg is not None:
-                    loss_gradients.update(lg)
-            return loss_gradients
         else:
-            return None
+        try:
+            return importlib.import_module(name)
+        except ImportError as e:
+            if 'No module named' in str(e) and name in str(e):
+                pytest.skip('skipping as {} not available'.format(name))
+            raise
     
     
-def add_single_gpu_param_update_ops(model, gpu_id):
-    # Learning rate of 0 is a dummy value to be set properly at the
-    # start of training
-    lr = model.param_init_net.ConstantFill(
-        [], 'lr', shape=[1], value=0.0
-    )
-    one = model.param_init_net.ConstantFill(
-        [], 'one', shape=[1], value=1.0
-    )
-    wd = model.param_init_net.ConstantFill(
-        [], 'wd', shape=[1], value=cfg.SOLVER.WEIGHT_DECAY
-    )
-    # weight decay of GroupNorm's parameters
-    wd_gn = model.param_init_net.ConstantFill(
-        [], 'wd_gn', shape=[1], value=cfg.SOLVER.WEIGHT_DECAY_GN
-    )
-    for param in model.TrainableParams(gpu_id=gpu_id):
-        logger.debug('param ' + str(param) + ' will be updated')
-        param_grad = model.param_to_grad[param]
-        # Initialize momentum vector
-        param_momentum = model.param_init_net.ConstantFill(
-            [param], param + '_momentum', value=0.0
-        )
-        if param in model.biases:
-            # Special treatment for biases (mainly to match historical impl.
-            # details):
-            # (1) Do not apply weight decay
-            # (2) Use a 2x higher learning rate
-            model.Scale(param_grad, param_grad, scale=2.0)
-        elif param in model.gn_params:
-            # Special treatment for GroupNorm's parameters
-            model.WeightedSum([param_grad, one, param, wd_gn], param_grad)
-        elif cfg.SOLVER.WEIGHT_DECAY > 0:
-            # Apply weight decay to non-bias weights
-            model.WeightedSum([param_grad, one, param, wd], param_grad)
-        # Update param_grad and param_momentum in place
-        model.net.MomentumSGDUpdate(
-            [param_grad, param_momentum, lr, param],
-            [param_grad, param_momentum, param],
-            momentum=cfg.SOLVER.MOMENTUM
-        )
+def test_frame_mixed_depth_get():
+    arrays = [
+        ['a', 'top', 'top', 'routine1', 'routine1', 'routine2'],
+        ['', 'OD', 'OD', 'result1', 'result2', 'result1'],
+        ['', 'wx', 'wy', '', '', ''],
+    ]
+    
+    
+def test_hash_keys():
+    # Using different hash keys, should have
+    # different hashes for the same data.
+    #
+    # This only matters for object dtypes.
+    obj = Series(list('abc'))
+    
+    
+def ChineseAnalyzer(stoplist=STOP_WORDS, minsize=1, stemfn=stem, cachesize=50000):
+    return (ChineseTokenizer() | LowercaseFilter() |
+            StopFilter(stoplist=stoplist, minsize=minsize) |
+            StemFilter(stemfn=stemfn, ignore=None, cachesize=cachesize))
 
     
+    import jieba
+import jieba.analyse
+from optparse import OptionParser
     
-def add_single_scale_rpn_outputs(model, blob_in, dim_in, spatial_scale):
-    '''Add RPN outputs to a single scale model (i.e., no FPN).'''
-    anchors = generate_anchors(
-        stride=1. / spatial_scale,
-        sizes=cfg.RPN.SIZES,
-        aspect_ratios=cfg.RPN.ASPECT_RATIOS
-    )
-    num_anchors = anchors.shape[0]
-    dim_out = dim_in
-    # RPN hidden representation
-    model.Conv(
-        blob_in,
-        'conv_rpn',
-        dim_in,
-        dim_out,
-        kernel=3,
-        pad=1,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
-    model.Relu('conv_rpn', 'conv_rpn')
-    # Proposal classification scores
-    model.Conv(
-        'conv_rpn',
-        'rpn_cls_logits',
-        dim_in,
-        num_anchors,
-        kernel=1,
-        pad=0,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
-    # Proposal bbox regression deltas
-    model.Conv(
-        'conv_rpn',
-        'rpn_bbox_pred',
-        dim_in,
-        4 * num_anchors,
-        kernel=1,
-        pad=0,
-        stride=1,
-        weight_init=gauss_fill(0.01),
-        bias_init=const_fill(0.0)
-    )
+        def testPosseg_NOHMM(self):
+        import jieba.posseg as pseg
+        for content in test_contents:
+            result = pseg.cut(content,HMM=False)
+            assert isinstance(result, types.GeneratorType), 'Test Posseg Generator error'
+            result = list(result)
+            assert isinstance(result, list), 'Test Posseg error on content: %s' % content
+            print(' , '.join([w.word + ' / ' + w.flag for w in result]), file=sys.stderr)
+        print('testPosseg_NOHMM', file=sys.stderr)
     
-    
-class CompleterAvailableRequest( BaseRequest ):
-  def __init__( self, filetypes ):
-    super( CompleterAvailableRequest, self ).__init__()
-    self.filetypes = filetypes
-    self._response = None
-    
-    
-def _FormatYcmdDebugInfo( ycmd ):
-  python = ycmd[ 'python' ]
-  clang = ycmd[ 'clang' ]
-  message = ( 'Server Python interpreter: {0}\n'
-              'Server Python version: {1}\n'
-              'Server has Clang support compiled in: {2}\n'
-              'Clang version: {3}\n'.format( python[ 'executable' ],
-                                             python[ 'version' ],
-                                             clang[ 'has_support' ],
-                                             clang[ 'version' ] ) )
-  extra_conf = ycmd[ 'extra_conf' ]
-  extra_conf_path = extra_conf[ 'path' ]
-  if not extra_conf_path:
-    message += 'No extra configuration file found\n'
-  elif not extra_conf[ 'is_loaded' ]:
-    message += ( 'Extra configuration file found but not loaded\n'
-                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
-  else:
-    message += ( 'Extra configuration file found and loaded\n'
-                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
-  return message
-    
-        return result
-
-    
-    if args.quiet:
-    jieba.setLogLevel(60)
-if args.pos:
-    import jieba.posseg
-    posdelim = args.pos
-    def cutfunc(sentence, _, HMM=True):
-        for w, f in jieba.posseg.cut(sentence, HMM):
-            yield w + posdelim + f
-else:
-    cutfunc = jieba.cut
-    
-            for terms, w in cm.items():
-            g.addEdge(terms[0], terms[1], w)
-        nodes_rank = g.rank()
-        if withWeight:
-            tags = sorted(nodes_rank.items(), key=itemgetter(1), reverse=True)
-        else:
-            tags = sorted(nodes_rank, key=nodes_rank.__getitem__, reverse=True)
-    
-        def set_stop_words(self, stop_words_path):
-        abs_path = _get_abs_path(stop_words_path)
-        if not os.path.isfile(abs_path):
-            raise Exception('jieba: file does not exist: ' + abs_path)
-        content = open(abs_path, 'rb').read().decode('utf-8')
-        for line in content.splitlines():
-            self.stop_words.add(line)
-    
-    initialize = dt.initialize
-    
-    file_name = args[0]
-    
-    jieba.analyse.set_idf_path('../extra_dict/idf.txt.big');
-    
-    print(','.join(tags))
-    
-    log_f = open('1.log','w')
-log_f.write(' / '.join(map(str, words)))
-    
-    table_header = '''
-| Contributor | Github | Issues |
-|-------------|--------|--------|
-'''
+    USAGE ='usage:    python extract_tags.py [file name] -k [top k]'
