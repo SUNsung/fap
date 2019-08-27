042ed02545be4d7cd68e280f6f7ba20943e169f3
SUNsung <weatherius@gmail.com>
2019-08-27 20:37:24 +0300

    Rewrite [36w5FTaZbjfPA36CTyyIkye9ts365TQNORAEzyw36OP9.rb] File in: 2019-08-27 20:37:24

--- ruby/36w5FTaZbjfPA36CTyyIkye9ts365TQNORAEzyw36OP9.rb
+++ ruby/36w5FTaZbjfPA36CTyyIkye9ts365TQNORAEzyw36OP9.rb
@@ -1,153 +1,104 @@
 
         
-            it 'syncs added folders to the proper path' do
-      paths['/foo'] = [
-        { machine: machine_stub('m1'), opts: double('opts_m1') },
-        { machine: machine_stub('m2'), opts: double('opts_m2') },
-      ]
-      paths['/bar'] = [
-        { machine: machine_stub('m3'), opts: double('opts_m3') },
-      ]
+            def can_send_rejection_email?(email, type)
+      return false if @receiver.sent_to_mailinglist_mirror?
+      return true if type == :email_reject_unrecognized_error
     
-      let(:box_collection) { Vagrant::BoxCollection.new(environment.boxes_dir) }
+      def perform
+    args = { version: params[:version].to_i }
     
-      subject { described_class }
+        describe 'when image is not authorized' do
+      describe 'when image is for site setting' do
+        let(:filename) { 'logo.png' }
+        let(:file) { file_from_fixtures(filename) }
     
-          pushes = self.vagrantfile.config.push.__compiled_pushes
-      if !pushes.key?(name)
-        raise Vagrant::Errors::PushStrategyNotDefined,
-          name: name,
-          pushes: pushes.keys
+            it 'removes the file from s3 on multisite' do
+          test_multisite_connection('default') do
+            upload = build_upload
+            store.expects(:get_depth_for).with(upload.id).returns(0)
+            s3_helper.expects(:s3_bucket).returns(s3_bucket).at_least_once
+            upload.update!(url: '//s3-upload-bucket.s3.dualstack.us-west-1.amazonaws.com/discourse-uploads/uploads/default/original/1X/#{upload.sha1}.png')
+            s3_object = stub
+    
+        if text = I18n.t('reviewables.reasons.#{object.reason}', base_url: Discourse.base_url, default: nil)
+      # Create a convenient link to any site settings if the user is staff
+      settings_url = '#{Discourse.base_url}/admin/site_settings/category/all_results?filter='
+    
+          templates = TemplateFinder.build(:licenses, nil, popular: popular).execute
+    
+              entries = @repository.tree(:head, dir).entries
+    
+            raise ClaimInvalid, 'Missing required 'iat' claim' if options.valid_within && !@decoded['iat']
+    
+              access_requester = source.requesters.find_by!(user_id: params[:user_id])
+          member = ::Members::ApproveAccessRequestService
+            .new(current_user, declared_params)
+            .execute(access_requester)
+    
+          def request_authorization_consent_form
+        add_claims_to_scopes
+        endpoint = Api::OpenidConnect::AuthorizationPoint::EndpointStartPoint.new(current_user)
+        handle_start_point_response(endpoint)
       end
     
-            # Save the composition
-        #
-        # @param [Hash] composition New composition
-        def write_composition(composition)
-          @logger.debug('Saving composition to `#{composition_path}`: #{composition}')
-          tmp_file = Tempfile.new('vagrant-docker-compose')
-          tmp_file.write(composition.to_yaml)
-          tmp_file.close
-          synchronized do
-            FileUtils.mv(tmp_file.path, composition_path.to_s)
-          end
-        end
-    
-              ui.output(I18n.t(
-            'vagrant.box_updating',
-            name: update[0].name,
-            provider: update[2].name,
-            old: box.version,
-            new: update[1].version))
-          @env.action_runner.run(Vagrant::Action.action_box_add, {
-            box_url: box.metadata_url,
-            box_provider: update[2].name,
-            box_version: update[1].version,
-            ui: ui,
-            box_force: force,
-            box_download_client_cert: download_options[:client_cert],
-            box_download_ca_cert: download_options[:ca_cert],
-            box_download_ca_path: download_options[:ca_path],
-            box_download_insecure: download_options[:insecure]
-          })
-        end
+          def handle_jwt_bearer(req)
+        jwt_string = req['client_assertion']
+        jwt = JSON::JWT.decode jwt_string, :skip_verification
+        o_auth_app = Api::OpenidConnect::OAuthApplication.find_by(client_id: jwt['iss'])
+        raise Rack::OAuth2::Server::Authorize::BadRequest(:invalid_request) unless o_auth_app
+        public_key = fetch_public_key(o_auth_app, jwt)
+        JSON::JWT.decode(jwt_string, JSON::JWK.new(public_key).to_key)
+        req.update_param('client_id', o_auth_app.client_id)
+        req.update_param('client_secret', o_auth_app.client_secret)
       end
+    
+      def set_up_contacts_mobile
+    @contacts = case params[:set]
+      when 'only_sharing'
+        current_user.contacts.only_sharing
+      when 'all'
+        current_user.contacts
+      else
+        if params[:a_id]
+          @aspect = current_user.aspects.find(params[:a_id])
+          @aspect.contacts
+        else
+          current_user.contacts.receiving
+        end
     end
+    @contacts = @contacts.for_a_stream.paginate(:page => params[:page], :per_page => 25)
+    @contacts_size = @contacts.length
   end
 end
 
     
-      let(:argv)     { [] }
-  let(:iso_env) do
-    # We have to create a Vagrantfile so there is a root path
-    test_iso_env.vagrantfile('')
-    test_iso_env.create_vagrant_env
-  end
-  let(:test_iso_env) { isolated_environment }
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
     
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
-          end
-        end
-        bad_cookies.empty?
-      end
+    require 'minitest/autorun'
     
-    RSpec.describe RuboCop::AST::ClassNode do
-  let(:class_node) { parse_source(source).ast }
-    
-              outermost_send = outermost_send_on_same_line(heredoc_arg)
-          return unless outermost_send
-          return unless outermost_send.loc.end
-          return unless heredoc_arg.first_line != outermost_send.loc.end.line
-    
-      context 'when an access modifier has no effect' do
-    it 'registers an offense' do
-      expect_offense(<<~RUBY)
-        class SomeClass
-          def some_method
-            puts 10
-          end
-          private
-          ^^^^^^^ Useless `private` access modifier.
-          def self.some_method
-            puts 10
-          end
-        end
-      RUBY
-    end
-  end
-    
-            return unless rhs
-    
-      include_examples 'registers an offense', 'Fixnum'
-  include_examples 'registers an offense', 'Bignum'
-    
-            def offense_location(node)
-          if node.loc.respond_to?(:end) && node.loc.end
-            :end
-          else
-            :expression
-          end
-        end
-      end
-    end
-  end
-end
-
-    
-            foobar
-      end
-    RUBY
-  end
-    
-    When /^I append gems from Appraisal Gemfile$/ do
-  File.read(ENV['BUNDLE_GEMFILE']).split(/\n/).each do |line|
-    if line =~ /^gem '(?!rails|appraisal)/
-      append_to_gemfile line.strip
-    end
-  end
-end
-    
-    When /^I reset Bundler environment variable$/ do
-  BUNDLE_ENV_VARS.each do |key|
-    ENV[key] = nil
-  end
-end
-    
-      def migration_class_name
-    migration_name.camelize
-  end
-    
-        def clear
-      @attachments = Hash.new { |h,k| h[k] = {} }
+          q = Sidekiq::Queue.new('foo')
+      job = q.first
+      assert_equal 'foo', job['queue']
+      assert_equal 12, job['retry']
     end
     
-        def path
-      @file.respond_to?(:path) ? @file.path : @file
+        it 'can clear retries' do
+      add_retry
+      add_retry('test')
+      r = Sidekiq::RetrySet.new
+      assert_equal 2, r.size
+      r.clear
+      assert_equal 0, r.size
     end
+    
+      describe 'poller' do
+    before do
+      Sidekiq.redis{|c| c.flushdb}
+      @error_1  = { 'class' => ScheduledWorker.name, 'args' => [0], 'queue' => 'queue_1' }
+      @error_2  = { 'class' => ScheduledWorker.name, 'args' => [1], 'queue' => 'queue_2' }
+      @error_3  = { 'class' => ScheduledWorker.name, 'args' => [2], 'queue' => 'queue_3' }
+      @future_1 = { 'class' => ScheduledWorker.name, 'args' => [3], 'queue' => 'queue_4' }
+      @future_2 = { 'class' => ScheduledWorker.name, 'args' => [4], 'queue' => 'queue_5' }
+      @future_3 = { 'class' => ScheduledWorker.name, 'args' => [5], 'queue' => 'queue_6' }
