3715bad68cfe33e2b824bb62b8f1419af6e13d91
SUNsung <weatherius@gmail.com>
2019-08-28 07:06:42 +0300

    Rewrite [04lBi4pN.Tpp604X6JrFzb8uuw04H9ArEYFXtQo04ptx.py] File in: 2019-08-28 07:06:41

--- pyton/04lBi4pN.Tpp604X6JrFzb8uuw04H9ArEYFXtQo04ptx.py
+++ pyton/04lBi4pN.Tpp604X6JrFzb8uuw04H9ArEYFXtQo04ptx.py
@@ -1,180 +1,73 @@
 
         
-            with app.test_request_context():
-        assert url_for('index', args=[4, 5, 6]) == '/4,5,6'
+        
+class InlineFieldset(Fieldset):
+    def __init__(self, formset, *args, **kwargs):
+        self.formset = formset
+        super().__init__(*args, **kwargs)
     
-        def before_app_request(self, f):
-        '''Like :meth:`Flask.before_request`.  Such a function is executed
-        before each request, even if outside of a blueprint.
-        '''
-        self.record_once(
-            lambda s: s.app.before_request_funcs.setdefault(None, []).append(f)
+        def has_change_permission(self, request, obj=None):
+        return False
+    
+        def test_plain_annotate(self):
+        agg = Sum('book__pages', filter=Q(book__rating__gt=3))
+        qs = Author.objects.annotate(pages=agg).order_by('pk')
+        self.assertSequenceEqual([a.pages for a in qs], [447, None, 1047])
+    
+    
+class UUIDConverter:
+    regex = '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+    
+        def _assertPoKeyword(self, keyword, expected_value, haystack, use_quotes=True):
+        q = '''
+        if use_quotes:
+            expected_value = ''%s'' % expected_value
+            q = '''
+        needle = '%s %s' % (keyword, expected_value)
+        expected_value = re.escape(expected_value)
+        return self.assertTrue(
+            re.search('^%s %s' % (keyword, expected_value), haystack, re.MULTILINE),
+            'Could not find %(q)s%(n)s%(q)s in generated PO file' % {'n': needle, 'q': q}
         )
-        return f
     
-        def __get__(self, obj, type=None):
-        if obj is None:
-            return self
-        rv = obj.config[self.__name__]
-        if self.get_converter is not None:
-            rv = self.get_converter(rv)
-        return rv
+        @property
+    def fields(self):
+        'Return a list of fields in the Feature.'
+        return [
+            force_str(
+                capi.get_field_name(capi.get_field_defn(self._layer._ldefn, i)),
+                self.encoding,
+                strings_only=True
+            ) for i in range(self.num_fields)
+        ]
+    
+        @property
+    def boundary(self):
+        'Return the boundary as a newly allocated Geometry object.'
+        return self._topology(capi.geos_boundary(self.ptr))
+    
+        def contribute(self):
+        self.blackboard.common_state['problems'] += random.randint(10, 20)
+        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
+        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
+        self.blackboard.common_state['progress'] += random.randint(10, 30)
     
     
-# context locals
-_request_ctx_stack = LocalStack()
-_app_ctx_stack = LocalStack()
-current_app = LocalProxy(_find_app)
-request = LocalProxy(partial(_lookup_req_object, 'request'))
-session = LocalProxy(partial(_lookup_req_object, 'session'))
-g = LocalProxy(partial(_lookup_app_object, 'g'))
-
+class UserSpecification(CompositeSpecification):
+    def is_satisfied_by(self, candidate):
+        return isinstance(candidate, User)
     
-        #: The canonical way to decorate class-based views is to decorate the
-    #: return value of as_view().  However since this moves parts of the
-    #: logic from the class declaration to the place where it's hooked
-    #: into the routing system.
-    #:
-    #: You can place one or more decorators in this list and whenever the
-    #: view function is created the result is automatically decorated.
-    #:
-    #: .. versionadded:: 0.8
-    decorators = ()
-    
-        Snippets without code (only comments) or containing lines starting with ??? should not yeld files,
-    but the counter for naming snippets should still increment.
     '''
-    parser = argparse.ArgumentParser(description='Split md file into plain text and code blocks')
-    parser.add_argument('sourcefile',
-                        help='which file to read')
-    parser.add_argument('targetfile',
-                        help='where to put plain text')
-    parser.add_argument('codedir',
-                        help='where to put codeblocks')
-    args = parser.parse_args()
+*What is this pattern about?
+    
+        def build_size(self):
+        self.size = 'Small'
     
     
-def generate_rpn_on_dataset(
-    weights_file,
-    dataset_name,
-    _proposal_file_ignored,
-    output_dir,
-    multi_gpu=False,
-    gpu_id=0
-):
-    '''Run inference on a dataset.'''
-    dataset = JsonDataset(dataset_name)
-    test_timer = Timer()
-    test_timer.tic()
-    if multi_gpu:
-        num_images = len(dataset.get_roidb())
-        _boxes, _scores, _ids, rpn_file = multi_gpu_generate_rpn_on_dataset(
-            weights_file, dataset_name, _proposal_file_ignored, num_images,
-            output_dir
-        )
-    else:
-        # Processes entire dataset range by default
-        _boxes, _scores, _ids, rpn_file = generate_rpn_on_range(
-            weights_file,
-            dataset_name,
-            _proposal_file_ignored,
-            output_dir,
-            gpu_id=gpu_id
-        )
-    test_timer.toc()
-    logger.info('Total inference time: {:.3f}s'.format(test_timer.average_time))
-    return evaluate_proposal_file(dataset, rpn_file, output_dir)
-    
-    
-def test_net_on_dataset(
-    weights_file,
-    dataset_name,
-    proposal_file,
-    output_dir,
-    multi_gpu=False,
-    gpu_id=0
-):
-    '''Run inference on a dataset.'''
-    dataset = JsonDataset(dataset_name)
-    test_timer = Timer()
-    test_timer.tic()
-    if multi_gpu:
-        num_images = len(dataset.get_roidb())
-        all_boxes, all_segms, all_keyps = multi_gpu_test_net_on_dataset(
-            weights_file, dataset_name, proposal_file, num_images, output_dir
-        )
-    else:
-        all_boxes, all_segms, all_keyps = test_net(
-            weights_file, dataset_name, proposal_file, output_dir, gpu_id=gpu_id
-        )
-    test_timer.toc()
-    logger.info('Total inference time: {:.3f}s'.format(test_timer.average_time))
-    results = task_evaluation.evaluate_all(
-        dataset, all_boxes, all_segms, all_keyps, output_dir
-    )
-    return results
-    
-        def test_deprecated_key_from_file(self):
-        # You should see logger messages like:
-        #   'Deprecated config key (ignoring): MODEL.DILATION'
-        with tempfile.NamedTemporaryFile() as f:
-            cfg2 = copy.deepcopy(cfg)
-            cfg2.MODEL.DILATION = 2
-            envu.yaml_dump(cfg2, f)
-            with self.assertRaises(AttributeError):
-                _ = cfg.MODEL.DILATION  # noqa
-            core_config.merge_cfg_from_file(f.name)
-            with self.assertRaises(AttributeError):
-                _ = cfg.MODEL.DILATION  # noqa
-    
-    '''Perform inference on a single image or all images with a certain extension
-(e.g., .jpg) in a folder. Allows for using a combination of multiple models.
-For example, one model may be used for RPN, another model for Fast R-CNN style
-box detection, yet another model to predict masks, and yet another model to
-predict keypoints.
-'''
-    
-    
-def _do_keypoint_eval(json_dataset, res_file, output_dir):
-    ann_type = 'keypoints'
-    imgIds = json_dataset.COCO.getImgIds()
-    imgIds.sort()
-    coco_dt = json_dataset.COCO.loadRes(res_file)
-    coco_eval = COCOeval(json_dataset.COCO, coco_dt, ann_type)
-    coco_eval.params.imgIds = imgIds
-    coco_eval.evaluate()
-    coco_eval.accumulate()
-    eval_file = os.path.join(output_dir, 'keypoint_results.pkl')
-    save_object(coco_eval, eval_file)
-    logger.info('Wrote json eval results to: {}'.format(eval_file))
-    coco_eval.summarize()
-    return coco_eval
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
     
-    
-def log_json_stats(stats, sort_keys=True):
-    # hack to control precision of top-level floats
-    stats = {
-        k: '{:.6f}'.format(v) if isinstance(v, float) else v
-        for k, v in stats.items()
-    }
-    print('json_stats: {:s}'.format(json.dumps(stats, sort_keys=sort_keys)))
-    
-    from caffe2.python import utils as c2_py_utils
-    
-    import numpy as np
-import scipy.io as sio
-import sys
-    
-            'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    
-        def step(self, a):
-        self.do_simulation(a, self.frame_skip)
-        pos_after = self.sim.data.qpos[2]
-        data = self.sim.data
-        uph_cost = (pos_after - 0) / self.model.opt.timestep
-    
-        Adapted from Example 6.6 (page 106) from Reinforcement Learning: An Introduction
-    by Sutton and Barto:
-    http://incompleteideas.net/book/bookdraft2018jan1.pdf
+        def add(self, graphic):
+        self.graphics.append(graphic)
