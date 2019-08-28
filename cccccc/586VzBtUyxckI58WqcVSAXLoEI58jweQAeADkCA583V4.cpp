f6c2dcb2ed18aaf68d82e39843767639c9e89ebb
SUNsung <weatherius@gmail.com>
2019-08-28 07:31:49 +0300

    Rewrite [586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp] File in: 2019-08-28 07:31:48

--- cccccc/586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp
+++ cccccc/586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp
@@ -1,62 +1,164 @@
 
         
-        TEST(Converter, UnknownType) {
-  using namespace caffe2::testing;
-  caffe2::NetDef net;
-  NetMutator(&net)
-      .newOp('NeverSeen', {'X'}, {'X'})
-      .setDeviceOptionName('device_' + c10::to_string(rand() % 2));
-  auto nn = caffe2::convertToNNModule(net);
-  auto new_netdef = caffe2::convertToCaffe2Proto(nn);
-}
+          // Returns true if the death test passed; that is, the test process
+  // exited during the test, its exit status matches a user-supplied
+  // predicate, and its stderr output matches a user-supplied regular
+  // expression.
+  // The user-supplied predicate may be a macro expression rather
+  // than a function pointer or functor, or else Wait and Passed could
+  // be combined.
+  virtual bool Passed(bool exit_status_ok) = 0;
     
-    <summary> <b>Example</b> </summary>
-    
-    OPERATOR_SCHEMA(Glu)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-Applies gated linear unit to the input Tensor X. The output Y is half the size
-of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
-[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
-X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
-)DOC')
-    .Input(0, 'X', '1D input tensor')
-    .Output(0, 'Y', '1D output tensor');
+      // Returns true iff this is NAN (not a number).
+  bool is_nan() const {
+    // It's a NAN if the exponent bits are all ones and the fraction
+    // bits are not entirely zeros.
+    return (exponent_bits() == kExponentBitMask) && (fraction_bits() != 0);
+  }
     
     
-    {	return sky_energy;
-}
+    { private:
+  String();  // Not meant to be instantiated.
+};  // class String
     
-    
-    {		bool open;
-		int size;
-		int offset;
-		String name;
-		int access_flags;
-	} file;
-    
-    #ifdef ALSAMIDI_ENABLED
-    
-    #ifdef __cplusplus
-extern 'C' {
-#endif
-    }
-    
-    
-    {	UPNP();
-	~UPNP();
+    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
+    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
+    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
+    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
+    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
+    GTEST_TEMPLATE_ T16, GTEST_TEMPLATE_ T17, GTEST_TEMPLATE_ T18,
+    GTEST_TEMPLATE_ T19, GTEST_TEMPLATE_ T20, GTEST_TEMPLATE_ T21,
+    GTEST_TEMPLATE_ T22, GTEST_TEMPLATE_ T23, GTEST_TEMPLATE_ T24,
+    GTEST_TEMPLATE_ T25, GTEST_TEMPLATE_ T26, GTEST_TEMPLATE_ T27,
+    GTEST_TEMPLATE_ T28, GTEST_TEMPLATE_ T29, GTEST_TEMPLATE_ T30,
+    GTEST_TEMPLATE_ T31, GTEST_TEMPLATE_ T32, GTEST_TEMPLATE_ T33,
+    GTEST_TEMPLATE_ T34, GTEST_TEMPLATE_ T35, GTEST_TEMPLATE_ T36,
+    GTEST_TEMPLATE_ T37, GTEST_TEMPLATE_ T38, GTEST_TEMPLATE_ T39,
+    GTEST_TEMPLATE_ T40, GTEST_TEMPLATE_ T41, GTEST_TEMPLATE_ T42,
+    GTEST_TEMPLATE_ T43, GTEST_TEMPLATE_ T44>
+struct Templates44 {
+  typedef TemplateSel<T1> Head;
+  typedef Templates43<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
+      T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
+      T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42,
+      T43, T44> Tail;
 };
     
-    	ClassDB::bind_method(D_METHOD('set_igd_service_type', 'type'), &UPNPDevice::set_igd_service_type);
-	ClassDB::bind_method(D_METHOD('get_igd_service_type'), &UPNPDevice::get_igd_service_type);
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, 'igd_service_type'), 'set_igd_service_type', 'get_igd_service_type');
+      common::Status PlanOnReferenceLine(
+      const common::TrajectoryPoint& planning_start_point, Frame* frame,
+      ReferenceLineInfo* reference_line_info);
     
-    #endif // SEMAPHORE_IPHONE_H
+    #define protected public
+#define private public
+#include 'modules/planning/scenarios/park/valet_parking/valet_parking_scenario.h'
+    
+    #include 'modules/prediction/common/prediction_thread_pool.h'
+    
+    class TrafficLightUnprotectedRightTurnStageStop : public Stage {
+ public:
+  explicit TrafficLightUnprotectedRightTurnStageStop(
+      const ScenarioConfig::StageConfig& config)
+      : Stage(config) {}
+    }
+    
+      RoleAttrVec writers;
+  GetWritersOfNode(node_name, &writers);
+  if (writers.empty()) {
+    return;
+  }
+  std::unordered_set<std::string> channels;
+  for (auto& writer : writers) {
+    channels.emplace(writer.channel_name());
+  }
+    
+    void ControllerAgent::RegisterControllers(const ControlConf *control_conf) {
+  AINFO << 'Only support MPC controller or Lat + Lon controllers as of now';
+  for (auto active_controller : control_conf->active_controllers()) {
+    switch (active_controller) {
+      case ControlConf::MPC_CONTROLLER:
+        controller_factory_.Register(
+            ControlConf::MPC_CONTROLLER,
+            []() -> Controller * { return new MPCController(); });
+        break;
+      case ControlConf::LAT_CONTROLLER:
+        controller_factory_.Register(
+            ControlConf::LAT_CONTROLLER,
+            []() -> Controller * { return new LatController(); });
+        break;
+      case ControlConf::LON_CONTROLLER:
+        controller_factory_.Register(
+            ControlConf::LON_CONTROLLER,
+            []() -> Controller * { return new LonController(); });
+        break;
+      default:
+        AERROR << 'Unknown active controller type:' << active_controller;
+    }
+  }
+}
+    
+    namespace apollo {
+namespace perception {
+namespace camera {
+    }
+    }
+    }
+    
+    #if defined(__GNUC__) && ((__GNUC__ >= 4) || ((__GNUC__ == 3) && (__GNUC_MINOR__ >= 1)))
+#pragma GCC diagnostic warning '-Wdeprecated-declarations'
+#elif _MSC_VER >= 1400 //vs 2005 or higher
+#pragma warning (pop)
+#endif
 
     
-    PowerOSX::PowerOSX() :
-		nsecs_left(-1),
-		percent_left(-1),
-		power_state(OS::POWERSTATE_UNKNOWN) {
+    /*
+ * Update each tick
+ * Time is the percentage of the way through the duration
+ */
+void PageTurn3D::update(float time)
+{
+    float tt = MAX(0, time - 0.25f);
+    float deltaAy = (tt * tt * 500);
+    float ay = -100 - deltaAy;
+    
+    float deltaTheta = sqrtf(time);
+    float theta = deltaTheta > 0.5f ? (float)M_PI_2*deltaTheta : (float)M_PI_2*(1-deltaTheta);
+    
+    float rotateByYAxis = (2-time)* M_PI;
+    
+    float sinTheta = sinf(theta);
+    float cosTheta = cosf(theta);
+    
+    for (int i = 0; i <= _gridSize.width; ++i)
+    {
+        for (int j = 0; j <= _gridSize.height; ++j)
+        {
+            // Get original vertex
+            Vec3 p = getOriginalVertex(Vec2(i ,j));
+            
+            p.x -= getGridRect().origin.x;
+            float R = sqrtf((p.x * p.x) + ((p.y - ay) * (p.y - ay)));
+            float r = R * sinTheta;
+            float alpha = asinf( p.x / R );
+            float beta = alpha / sinTheta;
+            float cosBeta = cosf( beta );
+            
+            // If beta > PI then we've wrapped around the cone
+            // Reduce the radius to stop these points interfering with others
+            if (beta <= M_PI)
+            {
+                p.x = ( r * sinf(beta));
+            }
+            else
+            {
+                // Force X = 0 to stop wrapped
+                // points
+                p.x = 0;
+            }
+    }
+    }
+    }
+    
+    void AnimationCache::addAnimation(Animation *animation, const std::string& name)
+{
+    _animations.insert(name, animation);
 }
