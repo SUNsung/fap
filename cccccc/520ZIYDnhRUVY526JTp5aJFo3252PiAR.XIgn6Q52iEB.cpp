24e3a9575fc1426f1e248efde503a3c279808719
SUNsung <weatherius@gmail.com>
2019-08-28 22:46:46 +0300

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2019-08-28 22:46:46

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,293 +1,242 @@
 
         
-        void AutoUpdater::SetFeedURL(mate::Arguments* args) {
-  auto_updater::AutoUpdater::SetFeedURL(args);
-}
-    
-    NODE_LINKED_MODULE_CONTEXT_AWARE(atom_browser_debugger, Initialize)
-
-    
-      AcceleratorCallbackMap accelerator_callback_map_;
-    
-    #include 'base/compiler_specific.h'
-#include 'native_mate/handle.h'
-#include 'shell/browser/api/trackable_object.h'
-#include 'shell/browser/lib/power_observer.h'
-#include 'ui/base/idle/idle.h'
+        	size_t len = f->get_len();
     
     
-    {}  // namespace electron
+    {		ERR_FAIL_COND_V(!cc, NULL);
+		v.code_version = cc->version;
+	}
     
-      // Array holding scores for each orientation id [0,3].
-  // Orientation ids [0..3] map to [0, 270, 180, 90] degree orientations of the
-  // page respectively, where the values refer to the amount of clockwise
-  // rotation to be applied to the page for the text to be upright and readable.
-  float orientations[4];
-  // Script confidence scores for each of 4 possible orientations.
-  float scripts_na[4][kMaxNumberOfScripts];
+    	if (p_what == NOTIFICATION_EXIT_TREE) {
+		EditorFileSystem::get_singleton()->disconnect('filesystem_changed', this, 'reload');
+	}
     
-    #endif  // TESSERACT_CCSTRUCT_OCRPARA_H_
-
+    	int tex_flags = 0;
+	if (repeat > 0)
+		tex_flags |= Texture::FLAG_REPEAT;
+	if (repeat == 2)
+		tex_flags |= Texture::FLAG_MIRRORED_REPEAT;
+	if (filter)
+		tex_flags |= Texture::FLAG_FILTER;
+	if (mipmaps || compress_mode == COMPRESS_VIDEO_RAM)
+		tex_flags |= Texture::FLAG_MIPMAPS;
+	if (anisotropic)
+		tex_flags |= Texture::FLAG_ANISOTROPIC_FILTER;
+	if (srgb == 1)
+		tex_flags |= Texture::FLAG_CONVERT_TO_LINEAR;
     
-      /* Initial reject modes (pre NN_ACCEPT) */
-  R_POOR_MATCH,         // TEMP Ray's original heuristic (Not used)
-  R_NOT_TESS_ACCEPTED,  // TEMP Tess didn't accept WERD
-  R_CONTAINS_BLANKS,    // TEMP Tess failed on other chs in WERD
-  R_BAD_PERMUTER,       // POTENTIAL Bad permuter for WERD
+    	int x[3];
+	int y[3];
     
-    // A useful base class to facilitate the common operation of sorting a vector
-// of owned pointer data using a separate key. This class owns its data pointer,
-// deleting it when it has finished with it, and providing copy constructor and
-// operator= that have move semantics so that the data does not get copied and
-// only a single instance of KDPtrPair holds a specific data pointer.
-template <typename Key, typename Data>
-class KDPtrPair {
- public:
-  KDPtrPair() : data_(nullptr) {}
-  KDPtrPair(Key k, Data* d) : data_(d), key_(k) {}
-  // Copy constructor steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  KDPtrPair(KDPtrPair& src) : data_(src.data_), key_(src.key_) {
-    src.data_ = nullptr;
-  }
-  // Destructor deletes data, assuming it is the sole owner.
-  ~KDPtrPair() {
-    delete this->data_;
-    this->data_ = nullptr;
-  }
-  // Operator= steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  void operator=(KDPtrPair& src) {
-    delete this->data_;
-    this->data_ = src.data_;
-    src.data_ = nullptr;
-    this->key_ = src.key_;
-  }
+    	set_custom_minimum_size(Size2(0, 300 * EDSCALE));
+    
+        try
+    {
+        tananglerat(&prat, angletype, RATIONAL_BASE, RATIONAL_PRECISION);
+    }
+    catch (uint32_t error)
+    {
+        destroyrat(prat);
+        throw(error);
     }
     
-    
-    { protected:
-  // Outputs the integer formed by reversing the bits of the input integer. Only
-  // the lowest num_bits_ bits of the input integer are reversed.
-  int GetBinaryReversedInteger(int in_val) const {
-    int bit_pos = num_bits_;
-    int out_val = 0;
-    while(bit_pos--) {
-      // Set the value of the last bit.
-      out_val |= (in_val & 0x1);
-      if (bit_pos > 0) {
-        // Left-shift output value to prepare for storing the next bit.
-        out_val <<= 1;
-      }
-      // Right-shift input value to prepare for retrieving the next bit.
-      in_val >>= 1;
+    IAsyncOperationWithProgress<String ^, HttpProgress> ^ CurrencyHttpClient::GetCurrencyRatios()
+{
+    wstring uri = wstring{ sc_RatiosUriRelativeTo } + m_sourceCurrencyCode->Data();
+    auto ratiosUri = ref new Uri(StringReference(uri.c_str()));
     }
-    return out_val;
-  }
-  int N_;
-  // Next number to be considered for reversal and output.
-  int next_num_;
-  // number of bits required to represent the numbers of the sequence
-  int num_bits_;
-};
+    
+            // Now update our result value based on the state we are in
+        switch (state)
+        {
+        case MANTS:
+            pnumret->sign = (curChar == L'-') ? -1 : 1;
+            break;
+        case EXPSZ:
+        case EXPS:
+            expSign = (curChar == L'-') ? -1 : 1;
+            break;
+        case EXPDZ:
+        case EXPD:
+        {
+            curChar = NormalizeCharDigit(curChar, radix);
+    }
+    }
+    
+    StatusWith<BSONObj> countCommandAsAggregationCommand(const CountCommand& cmd,
+                                                     const NamespaceString& nss) {
+    BSONObjBuilder aggregationBuilder;
+    aggregationBuilder.append('aggregate', nss.coll());
+    }
+    
+            WorkingSet ws;
+        CountScan count(&_opCtx, params, &ws);
     
     
-    {
-    {
-    {      case AlignContext::Dead:
-        if (bytes > 2) {
-          a.ud2();
-          bytes -= 2;
-        }
-        if (bytes > 0) {
-          a.emitInt3s(bytes);
-        }
+RegexMatcher &RegexMatcher::reset(const UnicodeString &input) {
+    fInputText = utext_openConstUnicodeString(fInputText, &input, &fDeferredStatus);
+    if (fPattern->fNeedsAltInput) {
+        fAltInputText = utext_clone(fAltInputText, fInputText, FALSE, TRUE, &fDeferredStatus);
+    }
+    if (U_FAILURE(fDeferredStatus)) {
+        return *this;
+    }
+    fInputLength = utext_nativeLength(fInputText);
+    }
+    
+    ScientificNumberFormatter::ScientificNumberFormatter(
+        DecimalFormat *fmtToAdopt, Style *styleToAdopt, UErrorCode &status)
+        : fPreExponent(),
+          fDecimalFormat(fmtToAdopt),
+          fStyle(styleToAdopt),
+          fStaticSets(NULL) {
+    if (U_FAILURE(status)) {
         return;
     }
-    not_reached();
+    if (fDecimalFormat == NULL || fStyle == NULL) {
+        status = U_ILLEGAL_ARGUMENT_ERROR;
+        return;
+    }
+    const DecimalFormatSymbols *sym = fDecimalFormat->getDecimalFormatSymbols();
+    if (sym == NULL) {
+        status = U_ILLEGAL_ARGUMENT_ERROR;
+        return;
+    }
+    getPreExponent(*sym, fPreExponent);
+    fStaticSets = DecimalFormatStaticSets::getStaticSets(status);
+}
+    
+    U_NAMESPACE_BEGIN
+    
+    void CmdInfo::PrintInfo(DebuggerClient* client, StringBuffer &sb,
+                        const Array& info, const std::string &subsymbol) {
+  if (info.exists(s_params)) {
+    PrintHeader(client, sb, info);
+    sb.printf('function %s%s(%s);\n',
+              info[s_ref].toBoolean() ? '&' : '',
+              info[s_name].toString().data(),
+              GetParams(info[s_params].toArray(),
+                        info[s_varg].toBoolean()).data());
+    if (info[s_type_profiling].toArray().size() != 0) {
+      sb.printf('Type profile:\n%s',
+                GetTypeProfilingInfo(info[s_type_profiling].toArray(),
+                                     info[s_params].toArray()).data());
+    }
+    return;
   }
-};
-    
-    
-    {
-    {      decref_local(v);
-      next_local(v);
-      v << cmpq{ local, last, sf };
-      return sf;
-    });
-  }, true);
-    
-    struct X64Assembler final : public X64AssemblerBase {
-public:
-  explicit X64Assembler(CodeBlock& cb) : X64AssemblerBase(cb) {}
     }
     
-      DispReg operator+(intptr_t x) const {
-    return DispReg(base, disp + x);
-  }
+    #define VS_COMMAND_COMMON_IMPL(ClassName, Target, RequiresBreak)  \
+  VS_COMMAND_COMMON_TARGET(ClassName, Target, RequiresBreak)
     
+      forEachInstanceProp(
+    var,
+    [&](
+      const std::string& objectClassName,
+      const std::string& propName,
+      const std::string& propClassName,
+      const char* visibilityDescription,
+      folly::dynamic& presentationHint,
+      const Variant& propertyVariant
+    ) {
+    }
     
-    {
-    {
-    {
-    {              line_end = limit = (char *)lookup_trailing_spaces(buf, buf_len);
-              line_end_len = buf_len - (size_t)(limit - buf);
-              state = 0;
-            }
-            break;
-          }
-          break;
-        case 1:
-          /* we need to determine if the enclosure is
-           * 'real' or is it escaped */
-          switch (state) {
-          case 1: /* escaped */
-            bptr++;
-            state = 0;
-            break;
-          case 2: /* embedded enclosure ? let's check it */
-            if (*bptr != enclosure_char) {
-              /* real enclosure */
-              memcpy(tptr, hunk_begin, bptr - hunk_begin - 1);
-              tptr += (bptr - hunk_begin - 1);
-              hunk_begin = bptr;
-              goto quit_loop_2;
-            }
-            memcpy(tptr, hunk_begin, bptr - hunk_begin);
-            tptr += (bptr - hunk_begin);
-            bptr++;
-            hunk_begin = bptr;
-            state = 0;
-            break;
-          default:
-            if (*bptr == enclosure_char) {
-              state = 2;
-            } else if (*bptr == escape_char) {
-              state = 1;
-            }
-            bptr++;
-            break;
-          }
-          break;
-        }
-        inc_len = (bptr < limit ? 1 : 0);
+      if (data.isObject() &&
+      data.toObject().instanceof(SystemLib::s_IteratorClass)) {
+    array_copy = Array::Create();
+    for (ArrayIter iter(data.toObject().get()); iter; ++iter) {
+      if (!iter.first().isNull() && iter.first().isString()) {
+        array_copy.toArrRef().set(iter.first(), iter.second());
+      } else {
+        array_copy.toArrRef().append(iter.second());
       }
-    
-    
-///////////////////////////////////////////////////////////////////////////////
-    
-    req::ptr<File>
-PhpStreamWrapper::open(const String& filename, const String& mode,
-                       int options, const req::ptr<StreamContext>& context) {
-  if (strncasecmp(filename.c_str(), 'php://', 6)) {
-    return nullptr;
-  }
     }
+    data = array_copy;
+  }
     
-    Pipe::Pipe() {
+    template<class T, class V, bool case_sensitive, class E>
+inline
+const T& FixedStringMapBuilder<T,V,case_sensitive,E>::operator[](V idx) const {
+  return (*const_cast<FixedStringMapBuilder*>(this))[idx];
 }
     
-    #endif
-
     
-    Scanner::Scanner(std::istream &stream, int type,
-                 const char *fileName /* = '' */,
-                 bool sha1 /* = false */)
-    : m_filename(fileName), m_source(nullptr), m_len(0), m_pos(0),
-      m_state(Start), m_type(type), m_yyscanner(nullptr), m_token(nullptr),
-      m_loc(nullptr), m_lastToken(-1), m_isHHFile(0), m_lookaheadLtDepth(0) {
-  m_stream = &stream;
-  m_streamOwner = false;
-  if (sha1) computeSha1();
-  init();
+    {
+    {}}
+    
+    
+    {}
+    
+    TEST(CyberNodeTest, create_reader) {
+  EXPECT_TRUE(apollo::cyber::OK());
+  apollo::cyber::proto::Chatter chat;
+  apollo::cyber::PyNode node('listener');
+  pr = node.create_reader('channel/chatter', chat.GetTypeName());
+  EXPECT_EQ('apollo.cyber.proto.Chatter', chat.GetTypeName());
+  EXPECT_NE(pr, nullptr);
+  pr->register_func(cbfun);
+  delete pr;
+  pr = nullptr;
 }
     
-    /*
- * Used to maintain a mapping from the bytecode to its corresponding x86.
- */
-struct TransBCMapping {
-  SHA1   sha1;
-  Offset bcStart;
-  TCA    aStart;
-  TCA    acoldStart;
-  TCA    afrozenStart;
-};
-    
-    #include <cstddef>
-    
-      if (trec->kind == TransKind::LivePrologue ||
-      trec->kind == TransKind::ProfPrologue ||
-      trec->kind == TransKind::OptPrologue) {
-    return {true, ExtOpFuncPrologue};
-  }
-  if (isTraceletGuard(addr, trec)) {
-    return {true, ExtOpTraceletGuard};
-  }
-    
-    #include <boost/operators.hpp>
-    
-      Reopen(options);
-    
-    TEST_F(DBTestXactLogIterator, TransactionLogIteratorBlobs) {
-  Options options = OptionsForLogIterTest();
-  DestroyAndReopen(options);
-  CreateAndReopenWithCF({'pikachu'}, options);
-  {
-    WriteBatch batch;
-    batch.Put(handles_[1], 'key1', DummyString(1024));
-    batch.Put(handles_[0], 'key2', DummyString(1024));
-    batch.PutLogData(Slice('blob1'));
-    batch.Put(handles_[1], 'key3', DummyString(1024));
-    batch.PutLogData(Slice('blob2'));
-    batch.Delete(handles_[0], 'key2');
-    dbfull()->Write(WriteOptions(), &batch);
-    ReopenWithColumnFamilies({'default', 'pikachu'}, options);
-  }
-    }
-    
-    #ifndef ROCKSDB_LITE
-    
-      bool PartialMerge(const Slice& /*key*/, const Slice& left_operand,
-                    const Slice& right_operand, std::string* new_value,
-                    Logger* /*logger*/) const override {
-    if (left_operand.compare(right_operand) >= 0) {
-      new_value->assign(left_operand.data(), left_operand.size());
+        // 2. vertical control check
+    if ((mode == Chassis::COMPLETE_AUTO_DRIVE ||
+         mode == Chassis::AUTO_SPEED_ONLY) &&
+        !CheckResponse(CHECK_RESPONSE_SPEED_UNIT_FLAG, false)) {
+      ++vertical_ctrl_fail;
+      if (vertical_ctrl_fail >= kMaxFailAttempt) {
+        emergency_mode = true;
+        set_chassis_error_code(Chassis::MANUAL_INTERVENTION);
+      }
     } else {
-      new_value->assign(right_operand.data(), right_operand.size());
+      vertical_ctrl_fail = 0;
     }
-    return true;
+    if (CheckChassisError()) {
+      set_chassis_error_code(Chassis::CHASSIS_ERROR);
+      emergency_mode = true;
+    }
+    
+      // set scenario_type in PlanningContext
+  auto* scenario = PlanningContext::Instance()
+                       ->mutable_planning_status()
+                       ->mutable_scenario();
+  scenario->Clear();
+  scenario->set_scenario_type(scenario_type());
+    
+    void ControllerAgent::RegisterControllers(const ControlConf *control_conf) {
+  AINFO << 'Only support MPC controller or Lat + Lon controllers as of now';
+  for (auto active_controller : control_conf->active_controllers()) {
+    switch (active_controller) {
+      case ControlConf::MPC_CONTROLLER:
+        controller_factory_.Register(
+            ControlConf::MPC_CONTROLLER,
+            []() -> Controller * { return new MPCController(); });
+        break;
+      case ControlConf::LAT_CONTROLLER:
+        controller_factory_.Register(
+            ControlConf::LAT_CONTROLLER,
+            []() -> Controller * { return new LatController(); });
+        break;
+      case ControlConf::LON_CONTROLLER:
+        controller_factory_.Register(
+            ControlConf::LON_CONTROLLER,
+            []() -> Controller * { return new LonController(); });
+        break;
+      default:
+        AERROR << 'Unknown active controller type:' << active_controller;
+    }
   }
-    
-    Status WriteBatchBase::Merge(const SliceParts& key, const SliceParts& value) {
-  std::string key_buf, value_buf;
-  Slice key_slice(key, &key_buf);
-  Slice value_slice(value, &value_buf);
-    }
-    
-    #include 'rocksdb/status.h'
-    
-    
-    {  return 0;
 }
+    
+      dp_init_options.image_height = FLAGS_height;
+  dp_init_options.image_width = FLAGS_width;
+  dp_init_options.device_id = 0;
+    
+    #endif  // CYBER_SCHEDULER_POLICY_SCHEDULER_CHOREOGRAPHY_H_
 
     
-      ////////////////////////////////////////////////////////
-  //
-  // 'Repeatable Read' (Snapshot Isolation) Example
-  //   -- Using a single Snapshot
-  //
-  ////////////////////////////////////////////////////////
-    
-     private:
-  void Init(
-      const std::string& lua_script,
-      const std::vector<std::shared_ptr<RocksLuaCustomLibrary>>& libraries) {
-    if (lua_state_) {
-      luaL_openlibs(lua_state_);
-      for (const auto& library : libraries) {
-        luaL_openlib(lua_state_, library->Name(), library->Lib(), 0);
-        library->CustomSetup(lua_state_);
-      }
-      luaL_dostring(lua_state_, lua_script.c_str());
-    }
+    #define RETURN_IF_NULL(ptr)          \
+  if (ptr == nullptr) {              \
+    AWARN << #ptr << ' is nullptr.'; \
+    return;                          \
   }
