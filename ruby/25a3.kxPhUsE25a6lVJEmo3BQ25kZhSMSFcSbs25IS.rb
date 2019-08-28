9b571bff9909d7291ceeae7a0cf993ab9b515cff
SUNsung <weatherius@gmail.com>
2019-08-28 17:19:43 +0300

    Rewrite [25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb] File in: 2019-08-28 17:19:43

--- ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
+++ ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
@@ -1,207 +1,106 @@
 
         
-                  # Parse the options and return if we don't have any target.
-          argv = parse_options(opts)
-          return if !argv
-    
-      describe '.rsync_chown_support?' do
-    let(:local_version) { '3.1.1' }
-    let(:remote_version) { '3.1.1' }
-    
-              command = []
-          command << 'sudo' if !File.writable?('/etc/exports')
-          command += [
-            'sed', '-E', '-e',
-            '/^# VAGRANT-BEGIN:( #{user})? #{id}/,' +
-            '/^# VAGRANT-END:( #{user})? #{id}/ d',
-            '-ibak',
-            '/etc/exports'
-          ]
-    
-          def build_rsync_chown(opts)
-        guest_path = Shellwords.escape(opts[:guestpath])
-        if(opts[:exclude] && !Array(opts[:exclude]).empty?)
-          exclude_base = Pathname.new(opts[:guestpath])
-          exclusions = Array(opts[:exclude]).map do |ex_path|
-            ex_path = ex_path.slice(1, ex_path.size) if ex_path.start_with?(File::SEPARATOR)
-            '-path #{Shellwords.escape(exclude_base.join(ex_path))} -prune'
-          end.join(' -o ') + ' -o '
-        end
-        'find #{guest_path} #{exclusions}' \
-          ''!' -type l -a ' \
-          ''(' ! -user #{opts[:owner]} -or ! -group #{opts[:group]} ')' -exec ' \
-          'chown #{opts[:owner]}:#{opts[:group]} '{}' +'
+            def find_by_content(query)
+      repository.search_files_by_content(query, ref).map do |result|
+        Gitlab::Search::FoundBlob.new(content_match: result, project: project, ref: ref, repository: repository)
       end
     end
-  end
-end
-
     
-    describe VagrantPlugins::Chef::Cap::Linux::ChefInstalled do
-  include_context 'unit'
+            @decoded ||= ::JWT.decode(request.params['jwt'], secret, true, { algorithm: options.algorithm }).first
     
-          # Track the original box so we know if we changed
-      box = nil
-      initial_box = original_box = config.vm.box
-      initial_version = original_version = config.vm.box_version
+            desc 'Requests access for the authenticated user to a #{source_type}.' do
+          detail 'This feature was introduced in GitLab 8.11.'
+          success Entities::AccessRequester
+        end
+        post ':id/access_requests' do
+          source = find_source(source_type, params[:id])
+          access_requester = source.request_access(current_user)
     
-            env.box3('base', '1.0', :foo)
-      end
+    module API
+  class Badges < Grape::API
+    include PaginationParams
     
-            # Lookup the ID for the container with the given name
-        #
-        # @param [String] name Name of container
-        # @return [String] Container ID
-        def get_container_id(name)
-          compose_execute('ps', '-q', name).chomp
+        def at_css(*args)
+      doc.at_css(*args)
+    end
+    
+        def insert(index, *names)
+      @filters.insert assert_index(index), *filter_const(names)
+    end
+    
+    module Docs
+  class Scraper < Doc
+    class << self
+      attr_accessor :base_url, :root_path, :initial_paths, :options, :html_filters, :text_filters, :stubs
+    
+            css('.filetree').each do |node|
+          node.content = node.css('.file').map(&:inner_html).join('\n')
+          node.name = 'pre'
+          node.remove_attribute('class')
         end
     
-          def find_matching_tag(tag)
-        # Used primarily by developers testing beta macOS releases.
-        if OS::Mac.prerelease? && ARGV.skip_or_later_bottles?
-          generic_find_matching_tag(tag)
+          def get_type
+        if slug.start_with?('guide/')
+          'Guide'
+        elsif slug.start_with?('cookbook/')
+          'Cookbook'
+        elsif slug == 'glossary'
+          'Guide'
         else
-          generic_find_matching_tag(tag) ||
-            find_older_compatible_tag(tag)
+          type = at_css('.nav-title.is-selected').content.strip
+          type.remove! ' Reference'
+          type << ': #{mod}' if mod
+          type
         end
       end
     
-              find_instance_method_call(body_node, 'Formula', :factory) do
-            problem '\'Formula.factory(name)\' is deprecated in favor of \'Formula[name]\''
-          end
+      def origin_account
+    @account
+  end
     
-        it 'dependencies with invalid options in array value which lead to force rebuild' do
-      expect_offense(<<~RUBY)
-        class Foo < Formula
-          desc 'foo'
-          url 'https://brew.sh/foo-1.0.tgz'
-          depends_on 'httpd' => [:build, :test]
-          depends_on 'foo' => [:optional, 'with-bar']
-                                           ^^^^^^^^ Dependency foo should not use option with-bar
-          depends_on 'icu4c' => [:optional, 'c++11']
-                                             ^^^^^ Dependency icu4c should not use option c++11
-        end
-      RUBY
+    namespace :emojis do
+  desc 'Generate a unicode to filename mapping'
+  task :generate do
+    source = 'http://www.unicode.org/Public/emoji/11.0/emoji-test.txt'
+    codes  = []
+    dest   = Rails.root.join('app', 'javascript', 'mastodon', 'features', 'emoji', 'emoji_map.json')
+    
+        it 'returns http success' do
+      expect(response).to have_http_status(200)
     end
     
-    require 'cask/cmd/options'
+        context '!(follow.account.nil? || !follow.account.local?)' do
+      let(:_nil)    { false }
+      let(:local)   { true }
     
-        def metadata_timestamped_path(version: self.version, timestamp: :latest, create: false)
-      raise CaskError, 'Cannot create metadata path when timestamp is :latest.' if create && timestamp == :latest
-    
-      it 'does still notify when sender\'s domain is blocked but sender is followed' do
-    recipient.block_domain!(sender.domain)
-    recipient.follow!(sender)
-    is_expected.to change(Notification, :count)
+      def test_font_helper_with_suffix_question
+    assert_match %r(url\(['']?/assets/.*eot\?.*['']?\)), @css
   end
     
-      def refresh_poll
-    ActivityPub::FetchRemotePollService.new.call(@poll, current_account) if user_signed_in? && @poll.possibly_stale?
-  end
-end
-
-    
-      attr_accessor :account_ids, :action, :current_account
-    
-      def create_report!
-    @report = @source_account.reports.create!(
-      target_account: @target_account,
-      status_ids: @status_ids,
-      comment: @comment,
-      uri: @options[:uri]
-    )
+      describe '#name' do
+    it { expect(instance.name).to eq 'wemux' }
   end
     
-    class ActivityPub::ReplyDistributionWorker
-  include Sidekiq::Worker
-  include Payloadable
-    
-    describe Settings::Preferences::OtherController do
-  render_views
-    
-      before do
-    sign_in user, scope: :user
-  end
-    
-        # Do not perform synchronous validation for remote accounts
-    return if @proof.provider_username.blank? || !@proof.account.local?
-    
-        def self.authenticate_with_pam(attributes = {})
-      super if Devise.pam_authentication
+        def tmux_select_first_pane
+      '#{project.tmux} select-pane -t #{tmux_window_target}.#{panes.first.index + project.pane_base_index}'
     end
-  end
-end
-
     
-      before do
-    follower.follow!(old_account)
-    
-      subject { described_class.new }
-    
-      gem.required_ruby_version = '>= 2.0'
-  gem.add_dependency 'airbrussh', '>= 1.0.0'
-  gem.add_dependency 'i18n'
-  gem.add_dependency 'rake', '>= 10.0.0'
-  gem.add_dependency 'sshkit', '>= 1.9.0'
-    
-    Then(/^the invalid (.+) release is ignored$/) do |filename|
-  test = 'ls -g #{TestApp.releases_path} | grep #{filename}'
-  _, _, status = vagrant_cli_command('ssh -c #{test.shellescape}')
-  expect(status).to be_success
-end
-    
-    MESSAGE
+          it 'returns the string' do
+        expect(pre).to eq('mysql.server start')
       end
     end
+    
+      def commands_match
+    @actual.commands == commands
   end
-end
-
     
-          # rubocop:disable Security/MarshalLoad
-      def add_role(role, hosts, options={})
-        options_deepcopy = Marshal.dump(options.merge(roles: role))
-        Array(hosts).each { |host| add_host(host, Marshal.load(options_deepcopy)) }
-      end
-      # rubocop:enable Security/MarshalLoad
-    
-    Vagrant.configure('2') do |config|
-  # All Vagrant configuration is done here. The most common configuration
-  # options are documented and commented below. For a complete reference,
-  # please see the online documentation at vagrantup.com.
-    
-        # Reference
-    # http://www.debian.org/doc/manuals/maint-guide/first.en.html
-    # http://wiki.debian.org/DeveloperConfiguration
-    # https://github.com/jordansissel/fpm/issues/37
-    if ENV.include?('DEBEMAIL') and ENV.include?('DEBFULLNAME')
-      # Use DEBEMAIL and DEBFULLNAME as the default maintainer if available.
-      @maintainer = '#{ENV['DEBFULLNAME']} <#{ENV['DEBEMAIL']}>'
-    else
-      # TODO(sissel): Maybe support using 'git config' for a default as well?
-      # git config --get user.name, etc can be useful.
-      #
-      # Otherwise default to user@currenthost
-      @maintainer = '<#{ENV['USER']}@#{Socket.gethostname}>'
+          say out
     end
     
-        temp_info = pkginfo_template_path
-    
-      def output(output_path)
-    self.scripts.each do |name, path|
-      case name
-        when 'pre-install'
-          safesystem('cp', path, './preinstall')
-          File.chmod(0755, './preinstall')
-        when 'post-install'
-          safesystem('cp', path, './postinstall')
-          File.chmod(0755, './postinstall')
-        when 'pre-uninstall'
-          raise FPM::InvalidPackageConfiguration.new(
-            'pre-uninstall is not supported by Solaris packages'
-          )
-        when 'post-uninstall'
-          raise FPM::InvalidPackageConfiguration.new(
-            'post-uninstall is not supported by Solaris packages'
-          )
-      end # case name
-    end # self.scripts.each
+          it 'should take precedence over a named project' do
+        allow(described_class).to receive_messages(directory: fixtures_dir)
+        project_config = File.join(fixtures_dir, 'sample_number_as_name.yml')
+        project = described_class.validate(name: 'sample',
+                                           project_config: project_config)
+        expect(project.name).to eq('222')
+      end
