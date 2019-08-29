24e45669b746b414a34f5b0aaf598a242a586345
SUNsung <weatherius@gmail.com>
2019-08-29 12:15:48 +0300

    Rewrite [40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp] File in: 2019-08-29 12:15:47

--- cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
+++ cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
@@ -1,207 +1,42 @@
 
         
-        NODE_LINKED_MODULE_CONTEXT_AWARE(atom_browser_content_tracing, Initialize)
-
-    
-    #include 'base/callback_list.h'
-#include 'gin/handle.h'
-#include 'net/cookies/canonical_cookie.h'
-#include 'shell/browser/api/trackable_object.h'
-#include 'shell/browser/net/cookie_details.h'
-#include 'shell/common/promise_util.h'
-    
-    class DownloadItem : public mate::TrackableObject<DownloadItem>,
-                     public download::DownloadItem::Observer {
- public:
-  static mate::Handle<DownloadItem> Create(v8::Isolate* isolate,
-                                           download::DownloadItem* item);
+        class EditorRunNative : public HBoxContainer {
     }
     
-    
-    {}  // namespace mate
-    
-    void MenuViews::PopupAt(TopLevelWindow* window,
-                        int x,
-                        int y,
-                        int positioning_item,
-                        const base::Closure& callback) {
-  auto* native_window = static_cast<NativeWindowViews*>(window->window());
-  if (!native_window)
-    return;
-    }
-    
-    #include <map>
-#include <memory>
-    
-    static PyObject* AddFileDescriptor(PyObject* self, PyObject* descriptor) {
-  const FileDescriptor* file_descriptor =
-      PyFileDescriptor_AsDescriptor(descriptor);
-  if (!file_descriptor) {
-    return NULL;
-  }
-  if (file_descriptor !=
-      reinterpret_cast<PyDescriptorPool*>(self)->pool->FindFileByName(
-          file_descriptor->name())) {
-    PyErr_Format(PyExc_ValueError,
-                 'The file descriptor %s does not belong to this pool',
-                 file_descriptor->name().c_str());
-    return NULL;
-  }
-  Py_RETURN_NONE;
-}
-    
-    TEST(StatusOr, TestAssignmentStatusOk) {
-  const int kI = 4;
-  StatusOr<int> source(kI);
-  StatusOr<int> target;
-  target = source;
-  EXPECT_EQ(source.status(), target.status());
-  EXPECT_EQ(source.ValueOrDie(), target.ValueOrDie());
-}
-    
-    	//compute a FAKE linear velocity - this is easy
-    
-    void CPUParticlesEditor::_generate_emission_points() {
-    }
-    
-    #endif // CPU_PARTICLES_EDITOR_PLUGIN_H
-
-    
-    	EditorFileDialog *file;
-	EditorNode *editor;
-    
-    	void set_allow_object_decoding(bool p_enable);
-	bool is_object_decoding_allowed() const;
-    
-    	PoolVector<Triangle> triangles;
-	PoolVector<Vector3> vertices;
-    
-    class EditorImportPlugin : public ResourceImporter {
-	GDCLASS(EditorImportPlugin, ResourceImporter);
-    }
-    
-    	void _create_clips(Node *scene, const Array &p_clips, bool p_bake_all);
-	void _filter_anim_tracks(Ref<Animation> anim, Set<String> &keep);
-	void _filter_tracks(Node *scene, const String &p_text);
-	void _optimize_animations(Node *scene, float p_max_lin_error, float p_max_ang_error, float p_max_angle);
-    
-    ResourceImporterTexture::ResourceImporterTexture() {
-    }
+    #include 'core/io/resource_importer.h'
     
     			Ref<BitMap> bit_map;
 			bit_map.instance();
 			bit_map->create_from_image_alpha(image);
 			Vector<Vector<Vector2> > polygons = bit_map->clip_opaque_to_polygons(Rect2(0, 0, image->get_width(), image->get_height()));
     
-    #ifndef PLUGIN_CONFIG_DIALOG_H
-#define PLUGIN_CONFIG_DIALOG_H
-    
-                // evaluate only scalar parameters
-            vector<void*> params = EvaluateParameters(node, baseName, id, parameter.size() - id, pass);
-            id = 0; // reset counter because the params array starts at zero
-            size_t windowWidth = ((NDLNode<ElemType>*) params[id++])->GetScalar();
-            size_t windowHeight = ((NDLNode<ElemType>*) params[id++])->GetScalar();
-            size_t horizontalSubsample = ((NDLNode<ElemType>*) params[id++])->GetScalar();
-            size_t verticalSubsample = ((NDLNode<ElemType>*) params[id++])->GetScalar();
-            assert(id == 4);
-    
-    
-    {                // Consider the following example:
-                // NdlBLob=[
-                //      P=MacroCall1(...)
-                //      C=MacroCall2(P)
-                // ]
-                // # MacroDefinition
-                // MacroCall2(X)
-                // {
-                //      A=MacroCall3(...)
-                //      D=Times(A.B,X.B)}
-                // }
-                //
-    
-        ComputationNetworkPtr net;
-    
-        // Insert - insert a new name and value into the dictionary
-    void Insert(const std::wstring& name, const std::string& val)
-    {
-        Insert(Microsoft::MSR::CNTK::ToLegacyString(Microsoft::MSR::CNTK::ToUTF8(name)), val);
+    /**
+ * This structure contains per-service-context state related to the oplog.
+ */
+class LocalOplogInfo {
+public:
+    static LocalOplogInfo* get(ServiceContext& service);
+    static LocalOplogInfo* get(ServiceContext* service);
+    static LocalOplogInfo* get(OperationContext* opCtx);
     }
     
-    public:
+        // The number of documents that we still need to skip.
+    long long _leftToSkip;
     
-    #include <stdio.h>
-#include <math.h>
-#define EVAL_EXPORTS // creating the exports here
-#include 'Eval.h'
-#include 'Actions.h'
-#include 'CNTKEval.h'
-#include 'CPUMatrix.h' // for SetNumThreads()
-#include 'SimpleOutputWriter.h'
-#include 'NDLNetworkBuilder.h'
-#ifdef LEAKDETECT
-#include <vld.h> // leak detection
-#endif
-#include 'BestGpu.h'
-#include 'InputAndParamNodes.h'
-#include 'latticearchive.h'
-#include <limits>
-#include 'RecurrentNodes.h'
+            // Insert some docs
+        for (int i = 0; i < 10; ++i) {
+            insert(BSON('a' << i));
+        }
     
-    template<> ASGDHelper<half>* NewASGDHelper<half>(
-    const std::list<ComputationNodeBasePtr> & learnableNodes,
-    size_t nodeNumRanks,
-    bool useAsyncBuffer,
-    bool isSimulatedModelAveragingSGD,
-    AdjustLearningRateAtBeginning adjusttype,
-    double adjustCoef,
-    size_t adjustPerMinibatches,
-    int traceLevel,
-    int syncPerfStats)
-{
-    RuntimeError('NewASGDHelper - half not supported!');
+    //----------------------------------------------------------------------------
+//
+//  ScriptSet implementation
+//
+//----------------------------------------------------------------------------
+ScriptSet::ScriptSet() {
+    for (uint32_t i=0; i<UPRV_LENGTHOF(bits); i++) {
+        bits[i] = 0;
+    }
 }
     
-    
-    {            string prefix = '\t\t(model aggregation stats) %d-th sync: %8.2f seconds since last report (%.2f seconds on comm.); %d samples processed by %d workers (%d by me);\n'
-                            '\t\t(model aggregation stats) %d-th sync: totalThroughput = %.2fk samplesPerSecond , throughputPerWorker = %.2fk samplesPerSecond\n';
-            fprintf(stderr, prefix.c_str(), (int)m_numSyncPerformedInCurrentEpoch, secondsSinceLastReport, secondOnCommunication, (int)totalSamplesProcessedSinceLastReport, (int)m_numWorkers, (int)localSamplesProcessedSinceLastReport,
-                                            (int)m_numSyncPerformedInCurrentEpoch, totalThroughput, throughputPerWorker); 
-        }
-    };
-    // base class for MA-SGD algorithm family 
-    template<typename ElemType>
-    class IMASGD
-    {
-        typedef shared_ptr<ComputationNode<ElemType>> ComputationNodePtr;
-     public:
-         IMASGD(const MPIWrapperPtr& pMPI, size_t perfReportFreq, DEVICEID_TYPE devId)
-             : m_MAworkerStatus(pMPI->NumNodesInUse(), MAWorkerStatus::NOTSTARTED), 
-             m_numSyncPerformed(0), 
-             m_numWorkers(pMPI->NumNodesInUse()), 
-             m_myRank(pMPI->CurrentNodeRank()),
-             m_pMPI(pMPI), 
-             m_nccl(devId, pMPI),
-             m_deviceId(devId),
-             m_perfReporter(pMPI->CurrentNodeRank(), pMPI->NumNodesInUse())
-         {
-             m_perfReporter.SetReportFrequency(perfReportFreq);
-         }
-         virtual ~IMASGD()
-         {
-         }
-         
-         virtual void OnEpochStart(const std::list<ComputationNodeBasePtr>& /*LearnableNodes*/)
-         {
-             m_MAworkerStatus.resize(m_numWorkers);
-             std::fill(m_MAworkerStatus.begin(), m_MAworkerStatus.end(), MAWorkerStatus::DataProcessing);
-             m_pMPI->WaitAll(); 
-             m_perfReporter.OnEpochStart();
-         }
-    
-    
-    {
-    {
-    {}}}
-
-    
-    #pragma once
+    #if !UCONFIG_NO_BREAK_ITERATION
