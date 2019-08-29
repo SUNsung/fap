cd9d7e281cdb10ebfbbf886c1dea1540977733de
SUNsung <weatherius@gmail.com>
2019-08-29 23:18:26 +0300

    Rewrite [54.IZRkjZE3e654C1km6nFXJ6o54dyx4Pao4IKk54Skz.rb] File in: 2019-08-29 23:18:26

--- ruby/54.IZRkjZE3e654C1km6nFXJ6o54dyx4Pao4IKk54Skz.rb
+++ ruby/54.IZRkjZE3e654C1km6nFXJ6o54dyx4Pao4IKk54Skz.rb
@@ -1,237 +1,41 @@
 
         
-              def_delegator  :@obj, :site_data, :data
-      def_delegators :@obj, :time, :pages, :static_files, :tags, :categories
+              it { expect(class_node.parent_class.const_type?).to be(true) }
+    end
     
-    def remove_head_from_history(markdown)
-  index = markdown =~ %r!^##\s+\d+\.\d+\.\d+!
-  markdown[index..-1]
-end
-    
-    def pr(url)
-  if url.end_with?(FORWARD_SLASH)
-    url
-  else
-    url_dir = File.dirname(url)
-    url_dir.end_with?(FORWARD_SLASH) ? url_dir : '#{url_dir}/'
-  end
-end
-    
-    # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
-    
-      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
-    
-      def failure
-    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-    redirect_to after_omniauth_failure_path_for(resource_name)
+        it { expect(module_node.identifier.const_type?).to be(true) }
   end
     
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
-    
-    class User < ActiveRecord::Base
-  devise :database_authenticatable
-end
-    
-    module Devise
-  module Controllers
-    # Create url helpers to be used with resource/scope configuration. Acts as
-    # proxies to the generated routes created by devise.
-    # Resource param can be a string or symbol, a class, or an instance object.
-    # Example using a :user resource:
-    #
-    #   new_session_path(:user)      => new_user_session_path
-    #   session_path(:user)          => user_session_path
-    #   destroy_session_path(:user)  => destroy_user_session_path
-    #
-    #   new_password_path(:user)     => new_user_password_path
-    #   password_path(:user)         => user_password_path
-    #   edit_password_path(:user)    => edit_user_password_path
-    #
-    #   new_confirmation_path(:user) => new_user_confirmation_path
-    #   confirmation_path(:user)     => user_confirmation_path
-    #
-    # Those helpers are included by default to ActionController::Base.
-    #
-    # In case you want to add such helpers to another class, you can do
-    # that as long as this new class includes both url_helpers and
-    # mounted_helpers. Example:
-    #
-    #     include Rails.application.routes.url_helpers
-    #     include Rails.application.routes.mounted_helpers
-    #
-    module UrlHelpers
-      def self.remove_helpers!
-        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
-          remove_method method
-        end
-      end
-    
-          def mailer_from(mapping)
-        mailer_sender(mapping, :from)
-      end
-    
-        def authenticatable?
-      @authenticatable ||= self.modules.any? { |m| m.to_s =~ /authenticatable/ }
-    end
-    
-              if formula_tap == 'homebrew-core' && (depends_on?('veclibfort') || depends_on?('lapack'))
-            problem 'Formulae should use OpenBLAS as the default serial linear algebra library.'
-          end
-    
-        it 'formula path shortcut : man' do
-      expect_offense(<<~'RUBY')
-        class Foo < Formula
-          desc 'foo'
-          url 'https://brew.sh/foo-1.0.tgz'
-          def install
-            mv '#{share}/man', share
-                         ^^^^ '#{share}/man' should be '#{man}'
-          end
-        end
-      RUBY
-    end
-    
-          # Version string (a pretty long one) of the CLT package.
-      # Note, that different ways to install the CLTs lead to different
-      # version numbers.
-      def version
-        if @version ||= detect_version
-          ::Version.new @version
-        else
-          ::Version::NULL
-        end
-      end
-    
-        return false if args[:after] && OS::Mac.version < args[:after]
-    
-        it 'ignores OS version specifications' do
-      f = formula 'foo' do
-        url 'foo-1.0'
-    
-        private
-    
-      describe '#fetch' do
-    before do
-      subject.temporary_path.dirname.mkpath
-      FileUtils.touch subject.temporary_path
-    end
-    
-      def execute
-    signal_deprecation_warning_for_pack
-    
-        class Main < Clamp::Command
-      subcommand 'list', 'List all installed Logstash plugins', LogStash::PluginManager::List
-      subcommand 'install', 'Install a Logstash plugin', LogStash::PluginManager::Install
-      subcommand 'remove', 'Remove a Logstash plugin', LogStash::PluginManager::Remove
-      subcommand 'update', 'Update a plugin', LogStash::PluginManager::Update
-      subcommand 'pack', 'Package currently installed plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Pack
-      subcommand 'unpack', 'Unpack packaged plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Unpack
-      subcommand 'generate', 'Create the foundation for a new plugin', LogStash::PluginManager::Generate
-      subcommand 'uninstall', 'Uninstall a plugin. Deprecated: Please use remove instead', LogStash::PluginManager::Remove
-      subcommand 'prepare-offline-pack', 'Create an archive of specified plugins to use for offline installation', LogStash::PluginManager::PrepareOfflinePack
-    end
-  end
-end
-    
-            def correct_for_blockarg_type(node)
-          lambda do |corrector|
-            range = range_with_surrounding_space(range: node.source_range,
-                                                 side: :left)
-            range = range_with_surrounding_comma(range, :left)
-            corrector.remove(range)
-          end
-        end
+          it 'is not fooled by single line blocks' do
+        expect_no_offenses(<<~RUBY)
+          some_method #{open} do_something #{close}
+          something_else
+        RUBY
       end
     end
   end
 end
 
     
-            elements[i - 1]
-      end
-    
-          # Set the correct feed URL.
-      self.data['feed_url'] = '#{category_dir}/#{name}'
-    end
-    
-        def cache(gist, file, data)
-      cache_file = get_cache_file_for gist, file
-      File.open(cache_file, 'w') do |io|
-        io.write data
-      end
-    end
-    
-    module Jekyll
-    
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
-      end
-    end
-  end
-    
-      # Escapes CDATA sections in post content
-  def cdata_escape(input)
-    input.gsub(/<!\[CDATA\[/, '&lt;![CDATA[').gsub(/\]\]>/, ']]&gt;')
-  end
-    
-          Dir.chdir(file_path) do
-        contents = file.read
-        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
-          contents = $1.lstrip
-        end
-        contents = pre_filter(contents)
-        if @raw
-          contents
-        else
-          partial = Liquid::Template.parse(contents)
-          context.stack do
-            partial.render(context)
+      describe 'similar name suggestion' do
+    context 'when there's a similar variable-like method invocation' do
+      it 'suggests the method name' do
+        expect_offense(<<~RUBY)
+          def some_method
+            enviromnent = {}
+            ^^^^^^^^^^^ Useless assignment to variable - `enviromnent`. Did you mean `environment`?
+            another_symbol
+            puts environment
           end
-        end
+        RUBY
       end
     end
-  end
-end
     
+          module_function
     
-    
-        def tmux_split_command
-      path = if tab.root?
-               '#{Tmuxinator::Config.default_path_option} #{tab.root}'
-             end
-      '#{project.tmux} splitw #{path} -t #{tab.tmux_window_target}'
-    end
-    
-          describe 'attach' do
-        it 'sets force_attach to false when no attach argument is provided' do
-          project = described_class.new.create_project(name: name)
-          expect(project.force_attach).to eq(false)
-        end
-    
-      describe '#directories' do
-    context 'without TMUXINATOR_CONFIG environment' do
-      before do
-        allow(described_class).to receive(:environment?).and_return false
+          # The closing delimiter for this `block` literal.
+      #
+      # @return [String] the closing delimiter for the `block` literal
+      def closing_delimiter
+        delimiters.last
       end
