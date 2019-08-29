1866eca74de7870c1cc78d9fde6294383220b552
SUNsung <weatherius@gmail.com>
2019-08-29 18:13:48 +0300

    Rewrite [26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp] File in: 2019-08-29 18:13:47

--- cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
+++ cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
@@ -1,175 +1,172 @@
 
         
-        INSTANTIATE_TEST_CASE_P(InstantiationName,
-                        FooTest,
-                        Values('meeny', 'miny', 'moe'));
+            const QString &getAppName() const { return appName; }
+    const QIcon &getAppIcon() const { return appIcon; }
+    const QIcon &getTrayAndWindowIcon() const { return trayAndWindowIcon; }
+    const QString &getTitleAddText() const { return titleAddText; }
     
-    #if GTEST_HAS_STD_WSTRING
-template <>
-class UniversalTersePrinter<const wchar_t*> {
- public:
-  static void Print(const wchar_t* str, ::std::ostream* os) {
-    if (str == NULL) {
-      *os << 'NULL';
-    } else {
-      UniversalPrint(::std::wstring(str), os);
-    }
+    bool ParseInt32(const std::string& str, int32_t *out)
+{
+    if (!ParsePrechecks(str))
+        return false;
+    char *endp = NULL;
+    errno = 0; // strtol will not set errno if valid
+    long int n = strtol(str.c_str(), &endp, 10);
+    if(out) *out = (int32_t)n;
+    // Note that strtol returns a *long int*, so even if strtol doesn't report a over/underflow
+    // we still have to check that the returned value is within the range of an *int32_t*. On 64-bit
+    // platforms the size of these types may be different.
+    return endp && *endp == 0 && !errno &&
+        n >= std::numeric_limits<int32_t>::min() &&
+        n <= std::numeric_limits<int32_t>::max();
+}
+    
+    void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
+                                        std::string* dst) const {
+  // We rely on the fact that the code in table.cc does not mind us
+  // adjusting keys[].
+  Slice* mkey = const_cast<Slice*>(keys);
+  for (int i = 0; i < n; i++) {
+    mkey[i] = ExtractUserKey(keys[i]);
+    // TODO(sanjay): Suppress dups?
   }
-};
-#endif
+  user_policy_->CreateFilter(keys, n, dst);
+}
     
-    // The 'Types' template argument below must have spaces around it
-// since some compilers may choke on '>>' when passing a template
-// instance (e.g. Types<int>)
-# define TYPED_TEST_CASE(CaseName, Types) \
-  typedef ::testing::internal::TypeList< Types >::type \
-      GTEST_TYPE_PARAMS_(CaseName)
+    #include <stdio.h>
+#include 'leveldb/comparator.h'
+#include 'leveldb/db.h'
+#include 'leveldb/filter_policy.h'
+#include 'leveldb/slice.h'
+#include 'leveldb/table_builder.h'
+#include 'util/coding.h'
+#include 'util/logging.h'
     
-    typedef internal::TimeInMillis TimeInMillis;
+    Status DumpFile(Env* env, const std::string& fname, WritableFile* dst) {
+  FileType ftype;
+  if (!GuessType(fname, &ftype)) {
+    return Status::InvalidArgument(fname + ': unknown file type');
+  }
+  switch (ftype) {
+    case kLogFile:         return DumpLog(env, fname, dst);
+    case kDescriptorFile:  return DumpDescriptor(env, fname, dst);
+    case kTableFile:       return DumpTable(env, fname, dst);
+    default:
+      break;
+  }
+  return Status::InvalidArgument(fname + ': not a dump-able file type');
+}
+    
+    std::string LockFileName(const std::string& dbname) {
+  return dbname + '/LOCK';
+}
     
     
-    {  // 7. Ill-formed data should throw exception
-  for (const char* str : {'data = C:\\My Documents\\cat.csv', 'cow=',
-                          'C# = 100%', '= woof ',
-                          'interaction_constraints = [[0, 2], [1]]',
-                          'data = \'train.txt#cache',
-                          'data = \'train.txt#cache', 'foo = \'bar\''}) {
-    ASSERT_THROW(parser.ParseKeyValuePair(str, &key, &value), dmlc::Error);
+    {
+    {}  // namespace testing
+}  // namespace grpc
+    
+    ChannelArguments::ChannelArguments(const ChannelArguments& other)
+    : strings_(other.strings_) {
+  args_.reserve(other.args_.size());
+  auto list_it_dst = strings_.begin();
+  auto list_it_src = other.strings_.begin();
+  for (auto a = other.args_.begin(); a != other.args_.end(); ++a) {
+    grpc_arg ap;
+    ap.type = a->type;
+    GPR_ASSERT(list_it_src->c_str() == a->key);
+    ap.key = const_cast<char*>(list_it_dst->c_str());
+    ++list_it_src;
+    ++list_it_dst;
+    switch (a->type) {
+      case GRPC_ARG_INTEGER:
+        ap.value.integer = a->value.integer;
+        break;
+      case GRPC_ARG_STRING:
+        GPR_ASSERT(list_it_src->c_str() == a->value.string);
+        ap.value.string = const_cast<char*>(list_it_dst->c_str());
+        ++list_it_src;
+        ++list_it_dst;
+        break;
+      case GRPC_ARG_POINTER:
+        ap.value.pointer = a->value.pointer;
+        ap.value.pointer.p = a->value.pointer.vtable->copy(ap.value.pointer.p);
+        break;
+    }
+    args_.push_back(ap);
   }
 }
     
-    // explicit instantiations are required, as HostDeviceVector isn't header-only
-template class HostDeviceVector<bst_float>;
-template class HostDeviceVector<GradientPair>;
-template class HostDeviceVector<int>;
-template class HostDeviceVector<Entry>;
-template class HostDeviceVector<size_t>;
+    
+    {  gpr_setenv(name, value);
+  gpr_unsetenv(name);
+  retrieved_value = gpr_getenv(name);
+  GPR_ASSERT(retrieved_value == nullptr);
+}
     
     
-    {
-    {
-    {                    // Create crop node with offsets but without inputs (will be attached later in resolve phase).
-                    nodePtr = builder.Crop(nullptr, nullptr, offsetX, offsetY, name);
-                }
-                else
-                {
-                    // We have 4 node inputs (2 crop inputs and 2 equivalence node inputs).
-                    nodePtr = builder.Crop(nullptr, nullptr, nullptr, nullptr, name);
-                }
-            }
-            else
-            {
-                // Just two inputs, must be node inputs which will be attached in the resolve phase below.
-                nodePtr = builder.Crop(nullptr, nullptr, name);
-            }
-            // Done processing in this phase.
-            nodeParamStart = 0;
-            nodeParamCount = 0;
-        }
-        else
-        {
-            // In non-initial phase we just process node inputs below, here we just set inputs of interest.
-            nodeParamStart = 0;
-            nodeParamCount = nodePtr->GetNumInputs();
-        }
-    }
-    else if (cnNodeType == OperationName2Of(CastNode))
-    {
-        if (parameter.size() < 1)
-            RuntimeError('%ls should have 1 or more parameters (node and cast precision).', cnNodeType.c_str());
+    {  bool ret;
+  GPR_ASSERT(true == gpr_parse_bool_value('truE', &ret) && true == ret);
+  GPR_ASSERT(true == gpr_parse_bool_value('falsE', &ret) && false == ret);
+  GPR_ASSERT(true == gpr_parse_bool_value('1', &ret) && true == ret);
+  GPR_ASSERT(true == gpr_parse_bool_value('0', &ret) && false == ret);
+  GPR_ASSERT(true == gpr_parse_bool_value('Yes', &ret) && true == ret);
+  GPR_ASSERT(true == gpr_parse_bool_value('No', &ret) && false == ret);
+  GPR_ASSERT(true == gpr_parse_bool_value('Y', &ret) && true == ret);
+  GPR_ASSERT(true == gpr_parse_bool_value('N', &ret) && false == ret);
+  GPR_ASSERT(false == gpr_parse_bool_value(nullptr, &ret));
+  GPR_ASSERT(false == gpr_parse_bool_value('', &ret));
+}
     
+    TEST_F(HybridEnd2endTest, GenericEchoAsyncRequestStream) {
+  typedef EchoTestService::WithAsyncMethod_RequestStream<
+      EchoTestService::WithGenericMethod_Echo<TestServiceImpl>>
+      SType;
+  SType service;
+  AsyncGenericService generic_service;
+  SetUpServer(&service, nullptr, &generic_service, nullptr);
+  ResetStub();
+  std::thread generic_handler_thread(HandleGenericCall, &generic_service,
+                                     cqs_[0].get());
+  std::thread request_stream_handler_thread(HandleClientStreaming<SType>,
+                                            &service, cqs_[1].get());
+  TestAllMethods();
+  generic_handler_thread.join();
+  request_stream_handler_thread.join();
+}
     
-    {
-    {
-    {}}}
-
+    #include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/security/credentials.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
     
-        // ParseParameters - parse the parameters of a macro, or an array
-    // ndlNode - node we should add the parameters to
-    // value - parameters as config value
-    // createNew - create a new parameter node if one does not exist
-    void ParseParameters(NDLNode<ElemType>* ndlNode, const ConfigValue& value, bool createNew = false)
-    {
-        ConfigArray parameters = value;
-        for (auto iter = parameters.begin(); iter != parameters.end(); ++iter)
-        {
-            ConfigValue param = *iter;
-            NDLNode<ElemType>* paramNode = NULL;
-            auto foundBrace = param.find_first_of(FUNCTIONOPEN);
-            if (foundBrace != npos) // a nested call as a parameter
-                paramNode = ParseCall(param);
-            else // must be predefined variable or constant
-            {
-                paramNode = ParseVariable(param, createNew);
-    }
-    }
-    }
+    template <class ElemType>
+shared_ptr<ComputationNode<ElemType>> ComputationNetworkBuilder<ElemType>::Sqrt(const ComputationNodePtr a, const std::wstring nodeName)
+{
+    return net.AddNodeToNetAndAttachInputs(New<SqrtNode<ElemType>>(net.GetDeviceId(), nodeName), { a });
+}
     
-                for (int i = 1; i < numHiddenLayers; i++)
-            {
-                u = builder.CreateLearnableParameter(msra::strfun::wstrprintf(L'U%d', i), m_layerSizes[i + 1], m_layerSizes[i]);
-                m_net->RandomInitLearnableParameters(u, m_uniformInit, randomSeed++, m_initValueScale);
-                if (m_recurrentLayers.size() > 0 && m_recurrentLayers[recur_idx] == i + 1)
-                {
-                    w = builder.CreateLearnableParameter(msra::strfun::wstrprintf(L'W%d', i), m_layerSizes[i + 1], m_layerSizes[i + 1]);
-                    m_net->RandomInitLearnableParameters(w, m_uniformInit, randomSeed++, m_initValueScale);
-                    std::list<ComputationNodeBasePtr> recurrent_loop;
-                    pastValue = builder.PastValue(NULL, m_defaultHiddenActivity, m_layerSizes[i + 1], 1);
-                    output = SimpleNetworkBuilder<ElemType>::ApplyNonlinearFunction(builder.Plus(builder.Times(u, input), builder.Times(w, pastValue)), i);
-                    pastValue->AttachInputs({ output });
-                    recur_idx++;
-                }
-                else
-                {
-                    output = SimpleNetworkBuilder<ElemType>::ApplyNonlinearFunction(builder.Plus(builder.Times(u, input), b), i);
-                }
-    }
+                    fstream.GetMarker(FileMarker::fileMarkerBeginSection, L'BMomentumAsTimeConstant');
+                fstream >> m_blockMomentumAsTimeConstantPerWorker;
+                fstream.GetMarker(FileMarker::fileMarkerEndSection, L'EMomentumAsTimeConstant');
     
-            // Get option used by multiverso sparse update
-        m_getOptions.reserve(m_localBufferNum);
-        m_addOptions.reserve(m_localBufferNum);
+            clsweight = builder.CreateLearnableParameter(L'WeightForClassPostProb', m_nbrCls, m_layerSizes[numHiddenLayers]);
+        m_net->RandomInitLearnableParameters(clsweight, m_uniformInit, randomSeed++, m_initValueScale);
+        clslogpostprob = builder.Times(clsweight, input, 1, L'ClassPostProb');
     
-            // decide start column and end column
-        size_t st = numParallelSequences *  rank      / numProcs;
-        size_t en = numParallelSequences * (rank + 1) / numProcs;
-        assert(rank < numProcs);
-        en = en > numParallelSequences ? numParallelSequences : en; // TODO: why are these two tests necessary? We should rather test rank
-        en = (rank + 1 == numProcs) ? numParallelSequences : en;
-        size_t numNewParallelSequence = en - st;
+            m_workerArray->Add(m_deltaArray, m_totalModelSize);
+        m_workerArray->Get(m_deltaArray, m_totalModelSize);
+        WaitAll();
+        m_workerArray->Get(m_deltaArray, m_totalModelSize);
     
-    #include 'Basics.h'
-#include 'MPIWrapper.h'
-#include 'Matrix.h'
-#include 'SimpleDistGradAggregatorHelper.h'
-#include 'DistGradHeader.h'
-#include 'IDistGradAggregator.h'
-#include 'SimpleDistGradAggregator.h'
-#include 'V2SimpleDistGradAggregator.h'
-    
-    // The config may be accessed and updated asynchronously; use mutexes.
-Mutex config_hash_mutex_;
-Mutex config_refresh_mutex_;
-Mutex config_backup_mutex_;
-    
-      // Initialize a discovery cache at time 0.
-  discovery_cache_ = std::make_pair<size_t, bool>(0, false);
-  valid_ = true;
-    
-    class InMemoryDatabase final : public Database {
- public:
-  explicit InMemoryDatabase(std::string name) : Database(std::move(name)){};
-  ~InMemoryDatabase() override {}
-    }
-    
-    ExpectedSuccess<DatabaseError> RocksdbDatabase::putStringsUnsafe(
-    const std::string& domain,
-    const std::vector<std::pair<std::string, std::string>>& data) {
-  auto handle = getHandle(domain);
-  if (handle) {
-    std::shared_ptr<Handle> handle_ptr = handle.take();
-    rocksdb::WriteBatch batch;
-    }
-    }
-    
-    #include <osquery/core/database/in_memory_database.h>
-#include <osquery/database.h>
+    public:
+    V2SimpleDistGradAggregator(const MPIWrapperPtr& mpi, bool useAsyncAggregation, int deviceId, int syncStatsTrace, ::CNTK::DistributedCommunicatorPtr communicator)
+        : IDistGradAggregator<ElemType>(mpi), m_useAsyncAggregation(useAsyncAggregation), m_initialized(false), m_bufferedGradHeader(nullptr), m_syncStatsTrace(syncStatsTrace), m_iterationCount(0),
+        m_communicator(communicator), m_nccl(deviceId, mpi)
+    {}
