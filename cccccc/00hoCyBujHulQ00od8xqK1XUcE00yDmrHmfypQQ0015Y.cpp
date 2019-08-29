fb3df87cd283a5cb24452e7a6b32de36d889e7e1
SUNsung <weatherius@gmail.com>
2019-08-29 14:22:04 +0300

    Rewrite [00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp] File in: 2019-08-29 14:22:03

--- cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
+++ cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
@@ -1,371 +1,186 @@
 
         
-          // Creates a CertificateManagerModel. The model will be passed to the callback
-  // when it is ready. The caller must ensure the model does not outlive the
-  // |browser_context|.
-  static void Create(content::BrowserContext* browser_context,
-                     const CreationCallback& callback);
-    
-    #ifndef CHROME_BROWSER_UI_VIEWS_FRAME_GLOBAL_MENU_BAR_REGISTRAR_X11_H_
-#define CHROME_BROWSER_UI_VIEWS_FRAME_GLOBAL_MENU_BAR_REGISTRAR_X11_H_
-    
-    // Class template for extracting and storing single argument for callback
-// at position |index|.
-template <size_t index, typename ArgType>
-struct ArgumentHolder {
-  using ArgLocalType = typename CallbackParamTraits<ArgType>::LocalType;
+        void ProcessSingleton::LinuxWatcher::SocketReader::FinishWithACK(
+    const char* message,
+    size_t length) {
+  if (message && length) {
+    // Not necessary to care about the return value.
+    WriteToSocket(fd_, message, length);
+  }
     }
     
+     protected:
+  explicit AutoUpdater(v8::Isolate* isolate);
+  ~AutoUpdater() override;
     
-    { private:
-  scoped_refptr<RefCountedPersistent<v8::Object>> handle_;
+    
+    {}  // namespace electron
+
+    
+    namespace net {
+class URLRequestContextGetter;
+}
+    
+    template <>
+struct Converter<electron::api::DesktopCapturer::Source> {
+  static v8::Local<v8::Value> ToV8(
+      v8::Isolate* isolate,
+      const electron::api::DesktopCapturer::Source& source) {
+    mate::Dictionary dict(isolate, v8::Object::New(isolate));
+    content::DesktopMediaID id = source.media_list_source.id;
+    dict.Set('name', base::UTF16ToUTF8(source.media_list_source.name));
+    dict.Set('id', id.ToString());
+    dict.Set('thumbnail',
+             electron::api::NativeImage::Create(
+                 isolate, gfx::Image(source.media_list_source.thumbnail)));
+    dict.Set('display_id', source.display_id);
+    if (source.fetch_icon) {
+      dict.Set(
+          'appIcon',
+          electron::api::NativeImage::Create(
+              isolate, gfx::Image(GetWindowIcon(source.media_list_source.id))));
+    }
+    return ConvertToV8(isolate, dict);
+  }
 };
     
-      std::string current_dir = tokens[1];
-  // Remove the first two tokens.  The remaining tokens should be the command
-  // line argv array.
-  tokens.erase(tokens.begin());
-  tokens.erase(tokens.begin());
     
-    bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv);
+    {}  // namespace electron
     
-    #include 'ImfInt64.h'
-#include 'ImfNamespace.h'
-#include 'ImfExport.h'
+      mate::Dictionary dict(isolate, exports);
+  dict.Set('Notification', Notification::GetConstructor(isolate)
+                               ->GetFunction(context)
+                               .ToLocalChecked());
     
-    
-template <>
-const char *
-FloatVectorAttribute::staticTypeName ()
-{
-    return 'floatvector';
+    TEST_F(DBTestXactLogIterator, TransactionLogIteratorBatchOperations) {
+  do {
+    Options options = OptionsForLogIterTest();
+    DestroyAndReopen(options);
+    CreateAndReopenWithCF({'pikachu'}, options);
+    WriteBatch batch;
+    batch.Put(handles_[1], 'key1', DummyString(1024));
+    batch.Put(handles_[0], 'key2', DummyString(1024));
+    batch.Put(handles_[1], 'key3', DummyString(1024));
+    batch.Delete(handles_[0], 'key2');
+    dbfull()->Write(WriteOptions(), &batch);
+    Flush(1);
+    Flush(0);
+    ReopenWithColumnFamilies({'default', 'pikachu'}, options);
+    Put(1, 'key4', DummyString(1024));
+    auto iter = OpenTransactionLogIter(3);
+    ExpectRecords(2, iter);
+  } while (ChangeCompactOptions());
 }
     
-    
-OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_EXIT
-    
-    
-    {    return fps;
-}
-    
-    #ifndef IMFGENERICINPUTFILE_H_
-#define IMFGENERICINPUTFILE_H_
-    
-    
-    {	os.write (s.data(), int(s.length()));
+      Decision FilterV2(int /*level*/, const Slice& key, ValueType value_type,
+                    const Slice& value, std::string* /*new_value*/,
+                    std::string* /*skip_until*/) const override {
+    if (value_type != kBlobIndex) {
+      return Decision::kKeep;
     }
-    
-        IMF_EXPORT
-    Iterator			find (const char name[]);
-    IMF_EXPORT
-    ConstIterator		find (const char name[]) const;
-    
-    
-//
-// DECODING TABLE BUILDING
-//
-    
-    #include 'ImathFun.h'
-#include 'IlmThreadMutex.h'
-#include 'Iex.h'
-#include 'half.h'
-    
-    #include 'ImfMultiPartInputFile.h'
-    
-    #ifndef IMFINPUTPART_H_
-#define IMFINPUTPART_H_
-    
-    
-OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_EXIT
-    
-    OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_ENTER
-    
-    // Computes the Otsu threshold(s) for the given image rectangle, making one
-// for each channel. Each channel is always one byte per pixel.
-// Returns an array of threshold values and an array of hi_values, such
-// that a pixel value >threshold[channel] is considered foreground if
-// hi_values[channel] is 0 or background if 1. A hi_value of -1 indicates
-// that there is no apparent foreground. At least one hi_value will not be -1.
-// Delete thresholds and hi_values with delete [] after use.
-// The return value is the number of channels in the input image, being
-// the size of the output thresholds and hi_values arrays.
-int OtsuThreshold(Pix* src_pix, int left, int top, int width, int height,
-                  int** thresholds, int** hi_values);
-    
-    // A useful base struct to facilitate the common operation of sorting a vector
-// of simple or smart-pointer data using a separate key. Similar to STL pair.
-template <typename Key, typename Data>
-struct KDPair {
-  KDPair() = default;
-  KDPair(Key k, Data d) : data(d), key(k) {}
+    BlobIndex blob_index;
+    Status s = blob_index.DecodeFrom(value);
+    if (!s.ok()) {
+      // Unable to decode blob index. Keeping the value.
+      return Decision::kKeep;
     }
-    
-    // Provides access to the ShapeTable that this classifier works with.
-const ShapeTable* TessClassifier::GetShapeTable() const {
-  return classify_->shape_table();
-}
-// Provides access to the UNICHARSET that this classifier works with.
-// Only needs to be overridden if GetShapeTable() can return nullptr.
-const UNICHARSET& TessClassifier::GetUnicharset() const {
-  return classify_->unicharset;
-}
-    
-    template <typename Dtype>
-void hdf5_load_nd_dataset(
-    hid_t file_id, const char* dataset_name_, int min_dim, int max_dim,
-    Blob<Dtype>* blob, bool reshape = false);
-    
-    template <typename Dtype>
-void HDF5OutputLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-  CHECK_GE(bottom.size(), 2);
-  CHECK_EQ(bottom[0]->num(), bottom[1]->num());
-  data_blob_.Reshape(bottom[0]->num(), bottom[0]->channels(),
-                     bottom[0]->height(), bottom[0]->width());
-  label_blob_.Reshape(bottom[1]->num(), bottom[1]->channels(),
-                     bottom[1]->height(), bottom[1]->width());
-  const int data_datum_dim = bottom[0]->count() / bottom[0]->num();
-  const int label_datum_dim = bottom[1]->count() / bottom[1]->num();
+    if (blob_index.HasTTL() && blob_index.expiration() <= current_time_) {
+      // Expired
+      expired_count_++;
+      expired_size_ += key.size() + value.size();
+      return Decision::kRemove;
     }
-    
-    // Return the current learning rate. The currently implemented learning rate
-// policies are as follows:
-//    - fixed: always return base_lr.
-//    - step: return base_lr * gamma ^ (floor(iter / step))
-//    - exp: return base_lr * gamma ^ iter
-//    - inv: return base_lr * (1 + gamma * iter) ^ (- power)
-//    - multistep: similar to step but it allows non uniform steps defined by
-//      stepvalue
-//    - poly: the effective learning rate follows a polynomial decay, to be
-//      zero by the max_iter. return base_lr (1 - iter/max_iter) ^ (power)
-//    - sigmoid: the effective learning rate follows a sigmod decay
-//      return base_lr ( 1/(1 + exp(-gamma * (iter - stepsize))))
-//
-// where base_lr, max_iter, gamma, step, stepvalue and power are defined
-// in the solver parameter protocol buffer, and iter is the current iteration.
-template <typename Dtype>
-Dtype SGDSolver<Dtype>::GetLearningRate() {
-  Dtype rate;
-  const string& lr_policy = this->param_.lr_policy();
-  if (lr_policy == 'fixed') {
-    rate = this->param_.base_lr();
-  } else if (lr_policy == 'step') {
-    CHECK_GT(this->param_.stepsize(), 0);
-    this->current_step_ = this->iter_ / this->param_.stepsize();
-    CHECK_GE(this->param_.gamma(), 0);
-    rate = this->param_.base_lr() *
-        pow(this->param_.gamma(), this->current_step_);
-  } else if (lr_policy == 'exp') {
-    CHECK_GE(this->param_.gamma(), 0);
-    rate = this->param_.base_lr() * pow(this->param_.gamma(), this->iter_);
-  } else if (lr_policy == 'inv') {
-    CHECK_GE(this->param_.gamma(), 0);
-    rate = this->param_.base_lr() *
-        pow(Dtype(1) + this->param_.gamma() * this->iter_,
-            - this->param_.power());
-  } else if (lr_policy == 'multistep') {
-    if (this->current_step_ < this->param_.stepvalue_size() &&
-          this->iter_ >= this->param_.stepvalue(this->current_step_)) {
-      this->current_step_++;
-      LOG(INFO) << 'MultiStep Status: Iteration ' <<
-      this->iter_ << ', step = ' << this->current_step_;
+    if (!blob_index.IsInlined() &&
+        blob_index.file_number() < context_.next_file_number &&
+        context_.current_blob_files.count(blob_index.file_number()) == 0) {
+      // Corresponding blob file gone. Could have been garbage collected or
+      // evicted by FIFO eviction.
+      evicted_count_++;
+      evicted_size_ += key.size() + value.size();
+      return Decision::kRemove;
     }
-    CHECK_GE(this->param_.gamma(), 0);
-    rate = this->param_.base_lr() *
-        pow(this->param_.gamma(), this->current_step_);
-  } else if (lr_policy == 'poly') {
-    rate = this->param_.base_lr() * pow(Dtype(1.) -
-        (Dtype(this->iter_) / Dtype(this->param_.max_iter())),
-        this->param_.power());
-  } else if (lr_policy == 'sigmoid') {
-    CHECK_GE(this->param_.gamma(), 0);
-    CHECK_GT(this->param_.stepsize(), 0);
-    rate = this->param_.base_lr() * (Dtype(1.) /
-        (Dtype(1.) + exp(-this->param_.gamma() * (Dtype(this->iter_) -
-          Dtype(this->param_.stepsize())))));
-  } else {
-    LOG(FATAL) << 'Unknown learning rate policy: ' << lr_policy;
-  }
-  return rate;
-}
-    
-    TYPED_TEST_CASE(HDF5DataLayerTest, TestDtypesAndDevices);
-    
-    // Verifies format of data stored in HDF5 file and reshapes blob accordingly.
-template <typename Dtype>
-void hdf5_load_nd_dataset_helper(
-    hid_t file_id, const char* dataset_name_, int min_dim, int max_dim,
-    Blob<Dtype>* blob, bool reshape) {
-  // Verify that the dataset exists.
-  CHECK(H5LTfind_dataset(file_id, dataset_name_))
-      << 'Failed to find HDF5 dataset ' << dataset_name_;
-  // Verify that the number of dimensions is in the accepted range.
-  herr_t status;
-  int ndims;
-  status = H5LTget_dataset_ndims(file_id, dataset_name_, &ndims);
-  CHECK_GE(status, 0) << 'Failed to get dataset ndims for ' << dataset_name_;
-  CHECK_GE(ndims, min_dim);
-  CHECK_LE(ndims, max_dim);
+    if (context_.fifo_eviction_seq > 0 && blob_index.HasTTL() &&
+        blob_index.expiration() < context_.evict_expiration_up_to) {
+      // Hack: Internal key is passed to BlobIndexCompactionFilter for it to
+      // get sequence number.
+      ParsedInternalKey ikey;
+      bool ok = ParseInternalKey(key, &ikey);
+      // Remove keys that could have been remove by last FIFO eviction.
+      // If get error while parsing key, ignore and continue.
+      if (ok && ikey.sequence < context_.fifo_eviction_seq) {
+        evicted_count_++;
+        evicted_size_ += key.size() + value.size();
+        return Decision::kRemove;
+      }
     }
-    
-    #include 'caffe/layers/neuron_layer.hpp'
-    
-      LOG(INFO) << 'Writing Training data';
-  for (int fileid = 0; fileid < kCIFARTrainBatches; ++fileid) {
-    // Open files
-    LOG(INFO) << 'Training Batch ' << fileid + 1;
-    string batchFileName = input_folder + '/data_batch_'
-      + caffe::format_int(fileid+1) + '.bin';
-    std::ifstream data_file(batchFileName.c_str(),
-        std::ios::in | std::ios::binary);
-    CHECK(data_file) << 'Unable to open train file #' << fileid + 1;
-    for (int itemid = 0; itemid < kCIFARBatchSize; ++itemid) {
-      read_image(&data_file, &label, str_buffer);
-      datum.set_label(label);
-      datum.set_data(str_buffer, kCIFARImageNBytes);
-      string out;
-      CHECK(datum.SerializeToString(&out));
-      txn->Put(caffe::format_int(fileid * kCIFARBatchSize + itemid, 5), out);
-    }
-  }
-  txn->Commit();
-  train_db->Close();
-    
-      virtual inline const char* type() const { return 'Pooling'; }
-  virtual inline int ExactNumBottomBlobs() const { return 1; }
-  virtual inline int MinTopBlobs() const { return 1; }
-  // MAX POOL layers can output an extra top blob for the mask;
-  // others can only output the pooled inputs.
-  virtual inline int MaxTopBlobs() const {
-    return (this->layer_param_.pooling_param().pool() ==
-            PoolingParameter_PoolMethod_MAX) ? 2 : 1;
+    return Decision::kKeep;
   }
     
-    /**
- * @brief Applies common transformations to the input data, such as
- * scaling, mirroring, substracting the image mean...
- */
-template <typename Dtype>
-class DataTransformer {
+    // A merge operator that mimics Put semantics
+// Since this merge-operator will not be used in production,
+// it is implemented as a non-associative merge operator to illustrate the
+// new interface and for testing purposes. (That is, we inherit from
+// the MergeOperator class rather than the AssociativeMergeOperator
+// which would be simpler in this case).
+//
+// From the client-perspective, semantics are the same.
+class PutOperator : public MergeOperator {
  public:
-  explicit DataTransformer(const TransformationParameter& param, Phase phase);
-  virtual ~DataTransformer() {}
+  bool FullMerge(const Slice& /*key*/, const Slice* /*existing_value*/,
+                 const std::deque<std::string>& operand_sequence,
+                 std::string* new_value, Logger* /*logger*/) const override {
+    // Put basically only looks at the current/latest value
+    assert(!operand_sequence.empty());
+    assert(new_value != nullptr);
+    new_value->assign(operand_sequence.back());
+    return true;
+  }
     }
     
-      void FailHijackedRecvMessage() override {
-    GPR_CODEGEN_ASSERT(hooks_[static_cast<size_t>(
-        experimental::InterceptionHookPoints::PRE_RECV_MESSAGE)]);
-    *got_message_ = false;
-  }
-    
-    void ChannelArguments::SetCompressionAlgorithm(
-    grpc_compression_algorithm algorithm) {
-  SetInt(GRPC_COMPRESSION_CHANNEL_DEFAULT_ALGORITHM, algorithm);
-}
-    
-    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
-  for (unsigned int i = 0; i < args_.size(); i++) {
-    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
-      return args_[i].value.string;
-    }
-  }
-  return '';
-}
-    
-    static bool iomgr_platform_is_any_background_poller_thread(void) {
-  return false;
-}
-    
-      socket = grpc_winsocket_create(sock, 'client');
-  info = &socket->write_info;
-  success = ConnectEx(sock, (grpc_sockaddr*)&addr->addr, (int)addr->len, NULL,
-                      0, NULL, &info->overlapped);
-    
-    void LogParsingError(const char* name, const char* value) {
-  char* error_message;
-  gpr_asprintf(&error_message,
-               'Illegal value '%s' specified for environment variable '%s'',
-               value, name);
-  (*g_global_config_env_error_func)(error_message);
-  gpr_free(error_message);
-}
-    
-    #include 'src/core/lib/gprpp/global_config_generic.h'
-#include 'src/core/lib/gprpp/memory.h'
-    
-    namespace {
+      if (timeout_time < 0) {
+    // If timeout is negative, do not use a timeout
+    cv_.wait(lock);
+  } else {
+    auto duration = std::chrono::microseconds(timeout_time);
+    auto cv_status = cv_.wait_for(lock, duration);
     }
     
-    #ifndef NDEBUG
-#define GRPC_COMBINER_DEBUG_ARGS \
-  , const char *file, int line, const char *reason
-#define GRPC_COMBINER_REF(combiner, reason) \
-  grpc_combiner_ref((combiner), __FILE__, __LINE__, (reason))
-#define GRPC_COMBINER_UNREF(combiner, reason) \
-  grpc_combiner_unref((combiner), __FILE__, __LINE__, (reason))
-#else
-#define GRPC_COMBINER_DEBUG_ARGS
-#define GRPC_COMBINER_REF(combiner, reason) grpc_combiner_ref((combiner))
-#define GRPC_COMBINER_UNREF(combiner, reason) grpc_combiner_unref((combiner))
-#endif
-    
-      std::shared_ptr<grpc::Channel> CreateChannelImpl(
-      const string& target, const grpc::ChannelArguments& args) override {
-    return CreateChannelWithInterceptors(
-        target, args,
-        std::vector<std::unique_ptr<
-            experimental::ClientInterceptorFactoryInterface>>());
-  }
-    
-      // Test all rpc methods.
-  void TestAllMethods() {
-    SendEcho();
-    SendSimpleClientStreaming();
-    SendSimpleServerStreaming();
-    SendBidiStreaming();
-  }
-    
-    #ifdef GRPC_POSIX_SOCKET
-// Thread-local variable to so that only polls from this test assert
-// non-blocking (not polls from resolver, timer thread, etc)
-GPR_TLS_DECL(g_is_nonblocking_test);
-    
-      DHTTaskFactory* taskFactory_;
-    
-    
-    {
-    {    nodes.push_back(node);
-  }
-  localNode_ = localNode;
-  nodes_ = nodes;
-  A2_LOG_INFO('DHT routing table was loaded successfully');
+    StopWriteToken::~StopWriteToken() {
+  assert(controller_->total_stopped_ >= 1);
+  --controller_->total_stopped_;
 }
     
-    #include <vector>
-#include <deque>
-#include <memory>
     
-      virtual std::shared_ptr<DHTTask>
-  createPeerAnnounceTask(const unsigned char* infoHash) = 0;
-    
-    
-    {} // namespace aria2
-
-    
-    #endif // D_DHT_TASK_QUEUE_H
+    {}  // namespace rocksdb
 
     
     
-    {  void updateTokenSecret();
-};
+    {// Factor method to create a new persistent cache
+Status NewPersistentCache(Env* const env, const std::string& path,
+                          const uint64_t size,
+                          const std::shared_ptr<Logger>& log,
+                          const bool optimized_for_nvm,
+                          std::shared_ptr<PersistentCache>* cache);
+}  // namespace rocksdb
+
     
-    public:
-  DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
-                        std::chrono::seconds interval);
+    namespace rocksdb {
+    }
     
-    DNSCache::AddrEntry::AddrEntry(const std::string& addr)
-    : addr_(addr), good_(true)
-{
-}
+            Point(const T x = 0, const T y = 0);
     
-        void markBad(const std::string& addr);
+    
+    {        Rectangle<T> operator/(const T value) const;
+    };
+    
+        const auto FACE_NUMBER_PARTS = 70u;
+    #define FACE_PAIRS_RENDER_GPU \
+        0,1,  1,2,  2,3,  3,4,  4,5,  5,6,  6,7,  7,8,  8,9,  9,10,  10,11,  11,12,  12,13,  13,14,  14,15,  15,16,  17,18,  18,19,  19,20, \
+        20,21,  22,23,  23,24,  24,25,  25,26,  27,28,  28,29,  29,30,  31,32,  32,33,  33,34,  34,35,  36,37,  37,38,  38,39,  39,40,  40,41, \
+        41,36,  42,43,  43,44,  44,45,  45,46,  46,47,  47,42,  48,49,  49,50,  50,51,  51,52,  52,53,  53,54,  54,55,  55,56,  56,57,  57,58, \
+        58,59,  59,48,  60,61,  61,62,  62,63,  63,64,  64,65,  65,66,  66,67,  67,60
+    #define FACE_SCALES_RENDER_GPU 1
+    const std::vector<unsigned int> FACE_PAIRS_RENDER {FACE_PAIRS_RENDER_GPU};
+    #define FACE_COLORS_RENDER_GPU 255.f,    255.f,    255.f
+    const std::vector<float> FACE_COLORS_RENDER{FACE_COLORS_RENDER_GPU};
+    const std::vector<float> FACE_SCALES_RENDER{FACE_SCALES_RENDER_GPU};
