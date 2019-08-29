35a81c5e1f839f6e3a977c9556aef0d566ebf253
SUNsung <weatherius@gmail.com>
2019-08-29 04:31:29 +0300

    Rewrite [16d8t4NGZs5Jk16Xw1rGe1HZU16bK5G.eUoQWk16KYa.cpp] File in: 2019-08-29 04:31:28

--- cccccc/16d8t4NGZs5Jk16Xw1rGe1HZU16bK5G.eUoQWk16KYa.cpp
+++ cccccc/16d8t4NGZs5Jk16Xw1rGe1HZU16bK5G.eUoQWk16KYa.cpp
@@ -1,174 +1,165 @@
 
         
-        
-    { private:
-  const HloCustomCallInstruction* cudnn_call_;
-  std::vector<BufferAllocation::Slice> operand_buffers_;
-  BufferAllocation::Slice result_buffer_;
-  BufferAllocation::Slice scratch_buffer_;
-  BufferAllocation::Slice tuple_result_buffer_;
-};
-    
-    #include 'tensorflow/compiler/xla/service/gpu/infeed_thunk.h'
-#include 'tensorflow/compiler/xla/service/gpu/hlo_execution_profiler.h'
-#include 'tensorflow/compiler/xla/service/gpu/infeed_manager.h'
-#include 'tensorflow/compiler/xla/util.h'
-#include 'tensorflow/core/platform/stream_executor_no_cuda.h'
-    
-    
-    {
-    {}  // namespace gpu
-}  // namespace xla
+        // static
+void CertificateManagerModel::GetCertDBOnIOThread(
+    content::ResourceContext* context,
+    const CreationCallback& callback) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  net::NSSCertDatabase* cert_db = GetNSSCertDatabaseForResourceContext(
+      context, base::BindOnce(&CertificateManagerModel::DidGetCertDBOnIOThread,
+                              callback));
+  if (cert_db)
+    DidGetCertDBOnIOThread(callback, cert_db);
+}
 
     
-      Status ExecuteOnStream(const ExecuteParams& params) override;
-    
-    SequentialThunk::SequentialThunk(std::vector<std::unique_ptr<Thunk>> thunks,
-                                 const HloInstruction* hlo)
-    : Thunk(Kind::kSequential, hlo), thunks_(std::move(thunks)) {}
-    
-    #include 'tensorflow/compiler/xla/service/gpu/buffer_allocations.h'
-#include 'tensorflow/compiler/xla/service/gpu/hlo_execution_profiler.h'
-#include 'tensorflow/compiler/xla/service/gpu/thunk.h'
-#include 'tensorflow/compiler/xla/service/hlo_instruction.h'
-#include 'tensorflow/core/platform/stream_executor_no_cuda.h'
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-        OP_REQUIRES(
-        context, TensorShapeUtils::IsVectorOrHigher(buffer_shape),
-        errors::InvalidArgument('Output must be at least 1-D, ',
-                                'got shape: ', buffer_shape.DebugString()));
-    
-    #include 'hdf5.h'
-    
-    // Get softmax layer according to engine.
-template <typename Dtype>
-shared_ptr<Layer<Dtype> > GetSoftmaxLayer(const LayerParameter& param) {
-  SoftmaxParameter_Engine engine = param.softmax_param().engine();
-  if (engine == SoftmaxParameter_Engine_DEFAULT) {
-    engine = SoftmaxParameter_Engine_CAFFE;
-#ifdef USE_CUDNN
-    engine = SoftmaxParameter_Engine_CUDNN;
-#endif
-  }
-  if (engine == SoftmaxParameter_Engine_CAFFE) {
-    return shared_ptr<Layer<Dtype> >(new SoftmaxLayer<Dtype>(param));
-#ifdef USE_CUDNN
-  } else if (engine == SoftmaxParameter_Engine_CUDNN) {
-    return shared_ptr<Layer<Dtype> >(new CuDNNSoftmaxLayer<Dtype>(param));
-#endif
-  } else {
-    LOG(FATAL) << 'Layer ' << param.name() << ' has unknown engine.';
-    throw;  // Avoids missing return warning
-  }
+    void InspectableWebContentsImpl::SaveToFile(const std::string& url,
+                                            const std::string& content,
+                                            bool save_as) {
+  if (delegate_)
+    delegate_->DevToolsSaveToFile(url, content, save_as);
 }
     
-    TYPED_TEST(NeuronLayerTest, TestReLUGradient) {
-  typedef typename TypeParam::Dtype Dtype;
-  LayerParameter layer_param;
-  ReLULayer<Dtype> layer(layer_param);
-  GradientChecker<Dtype> checker(1e-2, 1e-3, 1701, 0., 0.01);
-  checker.CheckGradientEltwise(&layer, this->blob_bottom_vec_,
-      this->blob_top_vec_);
-}
+    #include 'base/bind.h'
+#include 'base/debug/leak_annotations.h'
+#include 'base/logging.h'
+#include 'content/public/browser/browser_thread.h'
+#include 'shell/browser/ui/views/global_menu_bar_x11.h'
     
-    Classifier::Classifier(const string& model_file,
-                       const string& trained_file,
-                       const string& mean_file,
-                       const string& label_file) {
-#ifdef CPU_ONLY
-  Caffe::set_mode(Caffe::CPU);
-#else
-  Caffe::set_mode(Caffe::GPU);
-#endif
+    
+    {}  // namespace electron
+    
+    using views::MenuRunner;
+    
+      void ModifySendStatus(const Status& status) override {
+    GPR_CODEGEN_ASSERT(false &&
+                       'It is illegal to call ModifySendStatus on a method '
+                       'which has a Cancel notification');
+    return;
+  }
+    
+    
+    {  Alarm alarm_;
+  bool signal_client_;
+  std::mutex mu_;
+  std::unique_ptr<grpc::string> host_;
+};
+    
+    /* Tries to issue one async connection, then schedules both an IOCP
+   notification request for the connection, and one timeout alert. */
+static void tcp_connect(grpc_closure* on_done, grpc_endpoint** endpoint,
+                        grpc_pollset_set* interested_parties,
+                        const grpc_channel_args* channel_args,
+                        const grpc_resolved_address* addr,
+                        grpc_millis deadline) {
+  SOCKET sock = INVALID_SOCKET;
+  BOOL success;
+  int status;
+  grpc_resolved_address addr6_v4mapped;
+  grpc_resolved_address local_address;
+  async_connect* ac;
+  grpc_winsocket* socket = NULL;
+  LPFN_CONNECTEX ConnectEx;
+  GUID guid = WSAID_CONNECTEX;
+  DWORD ioctl_num_bytes;
+  grpc_winsocket_callback_info* info;
+  grpc_error* error = GRPC_ERROR_NONE;
     }
     
-      virtual inline const char* type() const { return 'Pooling'; }
-  virtual inline int ExactNumBottomBlobs() const { return 1; }
-  virtual inline int MinTopBlobs() const { return 1; }
-  // MAX POOL layers can output an extra top blob for the mask;
-  // others can only output the pooled inputs.
-  virtual inline int MaxTopBlobs() const {
-    return (this->layer_param_.pooling_param().pool() ==
-            PoolingParameter_PoolMethod_MAX) ? 2 : 1;
-  }
+    namespace grpc_core {
+    }
     
+      EXPECT_FALSE(IsConfigErrorCalled());
+    
+    static inline std::shared_ptr<::grpc::Channel> CreateCustomChannel(
+    const grpc::string& target,
+    const std::shared_ptr<ChannelCredentials>& creds,
+    const ChannelArguments& args) {
+  return ::grpc_impl::CreateCustomChannelImpl(target, creds, args);
+}
+    
+    #ifndef GRPCPP_SUPPORT_CHANNEL_ARGUMENTS_H
+#define GRPCPP_SUPPORT_CHANNEL_ARGUMENTS_H
+    
+        grpc::string exp = '';
+    EXPECT_TRUE(cstream->Read(&response));
+    exp.append(response.message() + ' ');
+    
+    
+// Metainfo functions
+void get_superblock_metainfo(
+    real_superblock_t *superblock,
+    std::vector< std::pair<std::vector<char>, std::vector<char> > > *kv_pairs_out,
+    cluster_version_t *version_out);
+    
+    void store_t::protocol_read(const read_t &_read,
+                            read_response_t *response,
+                            real_superblock_t *superblock,
+                            signal_t *interruptor) {
+    scoped_ptr_t<profile::trace_t> trace = ql::maybe_make_profile_trace(_read.profile);
+    }
+    
+        {
+        txn_t txn(&cache_conn, write_durability_t::HARD, 1);
+        {
+            buf_lock_t sb_lock(&txn, SUPERBLOCK_ID, alt_create_t::create);
+            real_superblock_t superblock(std::move(sb_lock));
+            btree_slice_t::init_real_superblock(
+                &superblock, std::vector<char>(), binary_blob_t());
+        }
+        txn.commit();
+    }
+    
+    // INTERNAL IMPLEMENTATION - DO NOT USE IN USER CODE.
+//
+// Expands to the namespace name that the type-parameterized tests for
+// the given type-parameterized test case are defined in.  The exact
+// name of the namespace is subject to change without notice.
+# define GTEST_CASE_NAMESPACE_(TestCaseName) \
+  gtest_case_##TestCaseName##_
+    
+      // Clones a 0-terminated C string, allocating memory using new.  The
+  // caller is responsible for deleting the return value using
+  // delete[].  Returns the cloned string, or NULL if the input is
+  // NULL.
+  //
+  // This is different from strdup() in string.h, which allocates
+  // memory using malloc().
+  static const char* CloneCString(const char* c_str);
+    
+    template <typename T0 = void, typename T1 = void, typename T2 = void,
+    typename T3 = void, typename T4 = void, typename T5 = void,
+    typename T6 = void, typename T7 = void, typename T8 = void,
+    typename T9 = void>
+class tuple;
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21>
+struct Types21 {
+  typedef T1 Head;
+  typedef Types20<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
+      T16, T17, T18, T19, T20, T21> Tail;
+};
+    
+    namespace op
+{
     /**
- * @brief A wrapper around SyncedMemory holders serving as the basic
- *        computational unit through which Layer%s, Net%s, and Solver%s
- *        interact.
- *
- * TODO(dox): more thorough description.
- */
-template <typename Dtype>
-class Blob {
- public:
-  Blob()
-       : data_(), diff_(), count_(0), capacity_(0) {}
+     * Add your class description here.
+     */
+    class UserPostProcessing
+    {
+    public:
+        /**
+         * Add your constructor description here.
+         */
+        UserPostProcessing();
+    }
     }
     
-      /**
-   * @brief Applies the transformation defined in the data layer's
-   * transform_param block to a cv::Mat
-   *
-   * @param cv_img
-   *    cv::Mat containing the data to be transformed.
-   * @param transformed_blob
-   *    This is destination blob. It can be part of top blob's data if
-   *    set_cpu_data() is used. See image_data_layer.cpp for an example.
-   */
-  void Transform(const cv::Mat& cv_img, Blob<Dtype>* transformed_blob);
-#endif  // USE_OPENCV
+            virtual void initializationOnThread();
     
-    
-    {    // Create SwapChain, RenderPass, Framebuffer, etc.
-    IM_ASSERT(g_MinImageCount >= 2);
-    ImGui_ImplVulkanH_CreateWindow(g_Instance, g_PhysicalDevice, g_Device, wd, g_QueueFamily, g_Allocator, width, height, g_MinImageCount);
-}
-    
-    DHTResponseMessage::DHTResponseMessage(
-    const std::shared_ptr<DHTNode>& localNode,
-    const std::shared_ptr<DHTNode>& remoteNode,
-    const std::string& transactionID)
-    : DHTAbstractMessage(localNode, remoteNode, transactionID)
-{
-}
-    
-    std::shared_ptr<DHTBucket>
-DHTRoutingTable::getBucketFor(const std::shared_ptr<DHTNode>& node) const
-{
-  return getBucketFor(node->getID());
-}
-    
-    void DHTRoutingTableDeserializer::deserialize(const std::string& filename)
-{
-  A2_LOG_INFO(fmt('Loading DHT routing table from %s.', filename.c_str()));
-  BufferedFile fp(filename.c_str(), BufferedFile::READ);
-  if (!fp) {
-    throw DL_ABORT_EX(
-        fmt('Failed to load DHT routing table from %s', filename.c_str()));
-  }
-  char header[8];
-  memset(header, 0, sizeof(header));
-  // magic
-  header[0] = 0xa1u;
-  header[1] = 0xa2u;
-  // format ID
-  header[2] = 0x02u;
-  // version
-  header[6] = 0;
-  header[7] = 0x03u;
-    }
-    
-    namespace aria2 {
-    }
-    
-    #include <vector>
-#include <memory>
-    
-    namespace {
-const size_t NUM_CONCURRENT_TASK = 15;
-} // namespace
+    #include <openpose/pose/poseParameters.hpp>
+#include <openpose/pose/poseParametersRender.hpp>
