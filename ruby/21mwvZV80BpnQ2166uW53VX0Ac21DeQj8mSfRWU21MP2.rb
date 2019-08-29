af9348cae94ec3088b2b3690d834c21e084bf2a4
SUNsung <weatherius@gmail.com>
2019-08-29 22:16:43 +0300

    Rewrite [21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb] File in: 2019-08-29 22:16:39

--- ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
+++ ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
@@ -1,137 +1,142 @@
 
         
-          class BaseStore
+            def respond_to_mime
+      respond_to do |type|
+        type.json do
+          render json: JsonRenderable.new
+        end
+        type.js   { render json: 'JS', callback: 'alert' }
+        type.csv  { render csv: CsvRenderable.new    }
+        type.xml  { render xml: XmlRenderable.new     }
+        type.html { render body: 'HTML'    }
+        type.rss  { render body: 'RSS'     }
+        type.all  { render body: 'Nothing' }
+        type.any(:js, :xml) { render body: 'Either JS or XML' }
+      end
+    end
+  end
     
-          SiteSetting.enable_s3_uploads = true
-      SiteSetting.s3_upload_bucket = 's3-upload-bucket'
-      SiteSetting.s3_access_key_id = 'some key'
-      SiteSetting.s3_secret_access_key = 'some secret key'
+          cattr_accessor :type_klass
     
-          if message_template
-        # inform the user about the rejection
-        message = Mail::Message.new(mail_string)
-        template_args[:former_title] = message.subject
-        template_args[:destination] = message.to
-        template_args[:site_name] = SiteSetting.title
+    ActionController::Base.helpers_path = File.expand_path('../../fixtures/helpers', __dir__)
     
-          type_supervisor.load_setting(
-        name,
-        opts.extract!(*SiteSettings::TypeSupervisor::CONSUMED_OPTS)
-      )
+            # how to operate
+        FastlaneCore::ConfigItem.new(key: :force,
+                                     short_option: '-f',
+                                     description: 'Skip the HTML report file verification',
+                                     is_string: false,
+                                     default_value: false),
+        FastlaneCore::ConfigItem.new(key: :overwrite_screenshots,
+                                     env_name: 'DELIVER_OVERWRITE_SCREENSHOTS',
+                                     description: 'Clear all previously uploaded screenshots before uploading the new ones',
+                                     is_string: false,
+                                     default_value: false),
+        FastlaneCore::ConfigItem.new(key: :submit_for_review,
+                                     env_name: 'DELIVER_SUBMIT_FOR_REVIEW',
+                                     description: 'Submit the new version for Review after uploading everything',
+                                     is_string: false,
+                                     default_value: false),
+        FastlaneCore::ConfigItem.new(key: :reject_if_possible,
+                                     env_name: 'DELIVER_REJECT_IF_POSSIBLE',
+                                     description: 'Rejects the previously submitted build if it's in a state where it's possible',
+                                     is_string: false,
+                                     default_value: false),
     
-        cookie = horde_login(username, password)
-    fail_with(Failure::NoAccess, 'Unable to login. Verify USERNAME/PASSWORD or TARGETURI.') if cookie.nil? || cookie.empty?
-    vprint_good('Authenticated to Horde.')
+          it 'deletes the watch app data' do
+        version.upload_watch_icon!(nil)
+        expect(version.watch_app_icon.url).to eq(nil)
+        expect(version.watch_app_icon.original_file_name).to eq(nil)
+        expect(version.watch_app_icon.asset_token).to eq(nil)
+      end
+    end
     
-              # Encodes the data
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_data(data)
-            OpenSSL::ASN1::OctetString.new(data)
-          end
+          def self.is_supported?(platform)
+        platform == :android
+      end
+    
+        class MakeChangelogFromJenkinsAction < Action
+      def self.run(params)
+        require 'json'
+        require 'net/http'
+    
+            version_podspec_file = Helper::PodspecHelper.new(podspec_path, params[:require_variable_prefix])
+    
+            # Login
+        credentials = JSON.parse(json_key_data)
+        callback_uri = 'https://fastlane.github.io/managed_google_play-callback/callback.html'
+        uri = 'https://play.google.com/apps/publish/delegatePrivateApp?service_account=#{credentials['client_email']}&continueUrl=#{URI.escape(callback_uri)}'
+    
+      # GET /resource/unlock/new
+  def new
+    self.resource = resource_class.new
+  end
+    
+        def confirmation_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :confirmation_instructions, opts)
+    end
+    
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
+    
+          attr_reader :scope_name, :resource
+    
+        # Include the chosen devise modules in your model:
+    #
+    #   devise :database_authenticatable, :confirmable, :recoverable
+    #
+    # You can also give any of the devise configuration values in form of a hash,
+    # with specific values for this model. Please check your Devise initializer
+    # for a complete description on those values.
+    #
+    def devise(*modules)
+      options = modules.extract_options!.dup
+    
+            # Recreate the user based on the stored cookie
+        def serialize_from_cookie(*args)
+          id, token, generated_at = *args
+    
+      private
+  def update_all?
+    plugins_arg.size == 0
+  end
+    
+          it 'list the plugins with their versions' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose')
+        result.stdout.split('\n').each do |plugin|
+          expect(plugin).to match(/^logstash-\w+-\w+\s\(\d+\.\d+.\d+(.\w+)?\)/)
         end
       end
     end
-  end
-end
     
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   XSS
-    # Supported browsers:: all
-    # More infos::         http://en.wikipedia.org/wiki/Cross-site_scripting
-    #
-    # Automatically escapes Rack::Request#params so they can be embedded in HTML
-    # or JavaScript without any further issues. Calls +html_safe+ on the escaped
-    # strings if defined, to avoid double-escaping in Rails.
-    #
-    # Options:
-    # escape:: What escaping modes to use, should be Symbol or Array of Symbols.
-    #          Available: :html (default), :javascript, :url
-    class EscapedParams < Base
-      extend Rack::Utils
-    
-      it 'should allow changing the protection settings' do
-    mock_app do
-      use Rack::Protection::ContentSecurityPolicy, :default_src => 'none', :script_src => 'https://cdn.mybank.net', :style_src => 'https://cdn.mybank.net', :img_src => 'https://cdn.mybank.net', :connect_src => 'https://api.mybank.com', :frame_src => 'self', :font_src => 'https://cdn.mybank.net', :object_src => 'https://cdn.mybank.net', :media_src => 'https://cdn.mybank.net', :report_uri => '/my_amazing_csp_report_parser', :sandbox => 'allow-scripts'
-    
-        it 'accepts chat channel invitation' do
-      membership = ChatChannelMembership.last
-      sign_in second_user
-      put '/chat_channel_memberships/#{membership.id}', params: {
-        chat_channel_membership: {
-          user_action: 'accept'
-        }
-      }
-      expect(ChatChannelMembership.find(membership.id).status).to eq('active')
+        def attach?
+      yaml_attach = if yaml['attach'].nil?
+                      true
+                    else
+                      yaml['attach']
+                    end
+      attach = force_attach || !force_detach && yaml_attach
+      attach
     end
     
-          it 'renders to appropriate page if user changes username twice' do
-        old_username = user.username
-        user.update(username: 'new_hotness_#{rand(10_000)}')
-        user.update(username: 'new_new_username_#{rand(10_000)}')
-        get '/#{old_username}/#{article.slug}'
-        expect(response.body).to redirect_to('/#{user.username}/#{article.slug}')
+      describe '#enviroment' do
+    context 'environment variable $TMUXINATOR_CONFIG is not empty' do
+      it 'is $TMUXINATOR_CONFIG' do
+        allow(ENV).to receive(:[]).with('TMUXINATOR_CONFIG').
+          and_return 'expected'
+        # allow(XDG).to receive(:[]).with('CONFIG').and_return 'expected'
+        allow(File).to receive(:directory?).and_return true
+        expect(described_class.environment).to eq 'expected'
       end
-    
-      context 'when redirected routes' do
-    include RSpec::Rails::RequestExampleGroup
-    
-          def header
-        if @header.nil?
-          if page = @page.header
-            @header = page.text_data
-          else
-            @header = false
-          end
-        end
-        @header
-      end
-    
-    # Make sure we're in the test dir, the tests expect that to be the current
-# directory.
-TEST_DIR  = File.join(File.dirname(__FILE__), *%w[.])
-    
-      def app
-    Precious::App
-  end
-end
-    
-      base_path = wiki_options[:base_path]
-    
-        def render
-      self.class.render_template(Tmuxinator::Config.template, binding)
     end
     
-        def initialize(index, project, tab, *commands)
-      @commands = commands
-      @index = index
-      @project = project
-      @tab = tab
-    end
-    
-      factory :project_with_force_detach, class: Tmuxinator::Project do
-    transient do
-      file { yaml_load('spec/fixtures/detach.yml') }
-    end
-    initialize_with { Tmuxinator::Project.new(file, force_detach: true) }
-  end
-    
-      context 'hook value is string' do
-    before { project.yaml[hook_name] = 'echo 'on hook'' }
-    
-        context 'pre in yaml is Array' do
+        context '$EDITOR is not set' do
       before do
-        project.yaml['pre'] = [
-          'mysql.server start',
-          'memcached -d'
-        ]
+        allow(ENV).to receive(:[]).with('EDITOR') { nil }
       end
-    
-      captured_stdout = StringIO.new
-  captured_stderr = StringIO.new
-    
-        def doctor
-      say 'Checking if tmux is installed ==> '
-      yes_no Tmuxinator::Doctor.installed?
