0d1f81324c272641b7c57451622026ceefb02469
SUNsung <weatherius@gmail.com>
2019-08-29 15:55:07 +0300

    Rewrite [52rCo4HTfrJfQ52hMbM0wYcpU52GSVtW.AbE252qKq.cpp] File in: 2019-08-29 15:55:06

--- cccccc/52rCo4HTfrJfQ52hMbM0wYcpU52GSVtW.AbE252qKq.cpp
+++ cccccc/52rCo4HTfrJfQ52hMbM0wYcpU52GSVtW.AbE252qKq.cpp
@@ -1,311 +1,92 @@
 
         
-        // Generate param traits log methods.
-#include 'ipc/param_traits_log_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
-
+            OPENEXR_IMF_INTERNAL_NAMESPACE::Xdr::read <OPENEXR_IMF_INTERNAL_NAMESPACE::StreamIO> (is, magic);
+    OPENEXR_IMF_INTERNAL_NAMESPACE::Xdr::read <OPENEXR_IMF_INTERNAL_NAMESPACE::StreamIO> (is, version);
+    
+        if (im < 0 || im >= HUF_ENCSIZE || iM < 0 || iM >= HUF_ENCSIZE)
+	invalidTableSize();
     
     
-    {  NOTREACHED() << 'Calling unknown sync method ' << method << ' of App';
-}
-    
-    void Base::CallSync(const std::string& method,
-                    const base::ListValue& arguments,
-                    base::ListValue* result) {
-  NOTREACHED() << 'Uncatched callAsync in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
-}
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(Base);
-};
-    
-    #ifndef CONTENT_NW_SRC_API_BINDINGS_COMMON_H_
-#define CONTENT_NW_SRC_API_BINDINGS_COMMON_H_
-    
-    void Clipboard::SetText(std::string& text) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  ui::Clipboard::ObjectMap map;
-  map[ui::Clipboard::CBF_TEXT].push_back(
-      std::vector<char>(text.begin(), text.end()));
-  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
-}
-    
-    namespace nwapi {
-    }
-    
-    namespace nw {
-    }
-    
-    // Popup menus may get squished if they open up too close to the bottom of the
-// screen. This function takes the size of the screen, the size of the menu,
-// an optional widget, the Y position of the mouse click, and adjusts the popup
-// menu's Y position to make it fit if it's possible to do so.
-// Returns the new Y position of the popup menu.
-int CalculateMenuYPosition(const GdkRectangle* screen_rect,
-                           const GtkRequisition* menu_req,
-                           GtkWidget* widget, const int y) {
-  CHECK(screen_rect);
-  CHECK(menu_req);
-  // If the menu would run off the bottom of the screen, and there is enough
-  // screen space upwards to accommodate the menu, then pop upwards. If there
-  // is a widget, then also move the anchor point to the top of the widget
-  // rather than the bottom.
-  const int screen_top = screen_rect->y;
-  const int screen_bottom = screen_rect->y + screen_rect->height;
-  const int menu_bottom = y + menu_req->height;
-  int alternate_y = y - menu_req->height;
-  if (widget) {
-    GtkAllocation allocation;
-    gtk_widget_get_allocation(widget, &allocation);
-    alternate_y -= allocation.height;
-  }
-  if (menu_bottom >= screen_bottom && alternate_y >= screen_top)
-    return alternate_y;
-  return y;
-}
-    
-    MenuItem::MenuItem(int id,
-                   const base::WeakPtr<ObjectManager>& object_manager,
-                   const base::DictionaryValue& option,
-                   const std::string& extension_id)
-  : Base(id, object_manager, option, extension_id) {
-  Create(option);
-}
-    
-    void MenuItem::SetChecked(bool checked) {
-  // Set active will cause 'activate' to be emitted, so block here
-  block_active_ = true;
-  gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(menu_item_), checked);
-  block_active_ = false;
-}
-    
-    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
- public:
-  NwMenuGetNSStringWithFixupFunction(){}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    
- protected:
-  ~NwMenuGetNSStringWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
-};
-    
-    
-    {    scoped_ptr_t<txn_t> txn;
-    {
-        scoped_ptr_t<real_superblock_t> superblock;
-        acquire_superblock_for_write(
-            1,
-            durability,
-            token,
-            &txn,
-            &superblock,
-            interruptor);
-        metainfo->update(superblock.get(), new_metainfo);
-    }
-    txn->commit();
-}
-    
-        store_t(const region_t &region,
-            serializer_t *serializer,
-            cache_balancer_t *balancer,
-            const std::string &perfmon_name,
-            bool create,
-            perfmon_collection_t *parent_perfmon_collection,
-            rdb_context_t *_ctx,
-            io_backender_t *io_backender,
-            const base_path_t &base_path,
-            namespace_id_t table_id,
-            update_sindexes_t update_sindexes,
-            which_cpu_shard_t which_cpu_shard);
-    ~store_t();
-    
-    
-    {
-    {}  // namespace internal
-}  // namespace testing
-    
-    #if GTEST_HAS_PARAM_TEST
-    
-      // These fields are immutable properties of the test.
-  const std::string test_case_name_;     // Test case name
-  const std::string name_;               // Test name
-  // Name of the parameter type, or NULL if this is not a typed or a
-  // type-parameterized test.
-  const internal::scoped_ptr<const ::std::string> type_param_;
-  // Text representation of the value parameter, or NULL if this is not a
-  // value-parameterized test.
-  const internal::scoped_ptr<const ::std::string> value_param_;
-  const internal::TypeId fixture_class_id_;   // ID of the test fixture class
-  bool should_run_;                 // True iff this test should run
-  bool is_disabled_;                // True iff this test is disabled
-  bool matches_filter_;             // True if this test matches the
-                                    // user-specified filter.
-  internal::TestFactoryBase* const factory_;  // The factory that creates
-                                              // the test object
-    
-    // 4-ary predicate assertion macros.
-#define EXPECT_PRED_FORMAT4(pred_format, v1, v2, v3, v4) \
-  GTEST_PRED_FORMAT4_(pred_format, v1, v2, v3, v4, GTEST_NONFATAL_FAILURE_)
-#define EXPECT_PRED4(pred, v1, v2, v3, v4) \
-  GTEST_PRED4_(pred, v1, v2, v3, v4, GTEST_NONFATAL_FAILURE_)
-#define ASSERT_PRED_FORMAT4(pred_format, v1, v2, v3, v4) \
-  GTEST_PRED_FORMAT4_(pred_format, v1, v2, v3, v4, GTEST_FATAL_FAILURE_)
-#define ASSERT_PRED4(pred, v1, v2, v3, v4) \
-  GTEST_PRED4_(pred, v1, v2, v3, v4, GTEST_FATAL_FAILURE_)
-    
-    # else
-#  define GTEST_EXECUTE_DEATH_TEST_STATEMENT_(statement, death_test) \
-  GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement)
-    
-    namespace testing {
-namespace internal {
-    }
-    }
-    
-    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
-    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
-    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
-    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
-    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
-    GTEST_TEMPLATE_ T16, GTEST_TEMPLATE_ T17, GTEST_TEMPLATE_ T18,
-    GTEST_TEMPLATE_ T19, GTEST_TEMPLATE_ T20, GTEST_TEMPLATE_ T21,
-    GTEST_TEMPLATE_ T22, GTEST_TEMPLATE_ T23, GTEST_TEMPLATE_ T24,
-    GTEST_TEMPLATE_ T25, GTEST_TEMPLATE_ T26, GTEST_TEMPLATE_ T27,
-    GTEST_TEMPLATE_ T28, GTEST_TEMPLATE_ T29, GTEST_TEMPLATE_ T30,
-    GTEST_TEMPLATE_ T31, GTEST_TEMPLATE_ T32, GTEST_TEMPLATE_ T33,
-    GTEST_TEMPLATE_ T34, GTEST_TEMPLATE_ T35, GTEST_TEMPLATE_ T36,
-    GTEST_TEMPLATE_ T37, GTEST_TEMPLATE_ T38, GTEST_TEMPLATE_ T39,
-    GTEST_TEMPLATE_ T40, GTEST_TEMPLATE_ T41, GTEST_TEMPLATE_ T42,
-    GTEST_TEMPLATE_ T43, GTEST_TEMPLATE_ T44>
-struct Templates44 {
-  typedef TemplateSel<T1> Head;
-  typedef Templates43<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
-      T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-      T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42,
-      T43, T44> Tail;
-};
-    
-    
-    {  // give up - can't do anything with this font
-  } else {
-    error(-1, 'Couldn't find a font to substitute for '%s' ('%s' character collection)',
-	  font->getName() ? font->getName()->getCString() : '(unnamed)',
-	  ((GfxCIDFont *)font)->getCollection()
-	    ? ((GfxCIDFont *)font)->getCollection()->getCString()
-	    : '(unknown)');
-    return;
-  }
-    
-    MediaRendition::~MediaRendition() {
-  if (fileName)
-    delete fileName;
-  if (contentType)
-    delete contentType;
-    }
-    
-    
-    {} // namespace aria2
-    
-      char zero[18];
-  memset(zero, 0, sizeof(zero));
-    
-    
-    {} // namespace aria2
-    
-    std::shared_ptr<DHTTask>
-DHTTaskFactoryImpl::createPeerAnnounceTask(const unsigned char* infoHash)
+char *
+IStream::readMemoryMapped (int n)
 {
-  // TODO
-  return nullptr;
+    throw IEX_NAMESPACE::InputExc ('Attempt to perform a memory-mapped read '
+			 'on a file that is not memory mapped.');
+    return 0;
 }
     
-    void DHTTaskQueueImpl::addPeriodicTask2(const std::shared_ptr<DHTTask>& task)
-{
-  periodicTaskQueue2_.addTask(task);
+        void		initialize ();
+    void                multiPartInitialize(InputPartData* part);
+    void                compatibilityInitialize(OPENEXR_IMF_INTERNAL_NAMESPACE::IStream& is);
+    TiledInputFile *	tFile ();
+    
+    friend void TiledOutputFile::copyPixels (InputFile &);
+    
+    Data *		_data;
+    
+    
+    {				set_value(get_value() - (custom_step >= 0 ? custom_step : get_step()));
+				return;
+			}
+    
+    
+    {	particles_editor->edit(Object::cast_to<CPUParticles>(p_object));
 }
     
-    void DHTTokenUpdateCommand::setTokenTracker(DHTTokenTracker* tokenTracker)
-{
-  tokenTracker_ = tokenTracker;
-}
+    	void _process_packet(int p_from, const uint8_t *p_packet, int p_packet_len);
+	void _process_simplify_path(int p_from, const uint8_t *p_packet, int p_packet_len);
+	void _process_confirm_path(int p_from, const uint8_t *p_packet, int p_packet_len);
+	Node *_process_get_node(int p_from, const uint8_t *p_packet, int p_packet_len);
+	void _process_rpc(Node *p_node, const StringName &p_name, int p_from, const uint8_t *p_packet, int p_packet_len, int p_offset);
+	void _process_rset(Node *p_node, const StringName &p_name, int p_from, const uint8_t *p_packet, int p_packet_len, int p_offset);
+	void _process_raw(int p_from, const uint8_t *p_packet, int p_packet_len);
     
-    #include <cstring>
-#include <cstdlib>
-    
-      Status NewDirectory(const std::string& name,
-                      std::unique_ptr<Directory>* result) override {
-    PERF_TIMER_GUARD(env_new_directory_nanos);
-    return EnvWrapper::NewDirectory(name, result);
-  }
-    
-    Status WriteBatchBase::DeleteRange(const SliceParts& begin_key,
-                                   const SliceParts& end_key) {
-  std::string begin_key_buf, end_key_buf;
-  Slice begin_key_slice(begin_key, &begin_key_buf);
-  Slice end_key_slice(end_key, &end_key_buf);
-  return DeleteRange(begin_key_slice, end_key_slice);
-}
+    	int line = 1;
+	String total_code;
     
     
-    { private:
-  // no copying allowed
-  WriteControllerToken(const WriteControllerToken&) = delete;
-  void operator=(const WriteControllerToken&) = delete;
+    {	AudioStreamPreview();
 };
     
-      // atomic write
-  WriteBatch batch;
-  batch.Put(handles[0], Slice('key2'), Slice('value2'));
-  batch.Put(handles[1], Slice('key3'), Slice('value3'));
-  batch.Delete(handles[0], Slice('key'));
-  s = db->Write(WriteOptions(), &batch);
-  assert(s.ok());
     
-    class MyFilter : public rocksdb::CompactionFilter {
- public:
-  bool Filter(int level, const rocksdb::Slice& key,
-              const rocksdb::Slice& existing_value, std::string* new_value,
-              bool* value_changed) const override {
-    fprintf(stderr, 'Filter(%s)\n', key.ToString().c_str());
-    ++count_;
-    assert(*value_changed == false);
-    return false;
-  }
+    {	ClassDB::bind_method(D_METHOD('_set_key'), &DictionaryPropertyEdit::_set_key);
+	ClassDB::bind_method(D_METHOD('_set_value'), &DictionaryPropertyEdit::_set_value);
+	ClassDB::bind_method(D_METHOD('_notif_change'), &DictionaryPropertyEdit::_notif_change);
+	ClassDB::bind_method(D_METHOD('_notif_changev'), &DictionaryPropertyEdit::_notif_changev);
+	ClassDB::bind_method(D_METHOD('_dont_undo_redo'), &DictionaryPropertyEdit::_dont_undo_redo);
+}
+    
+    		undo_redo->create_action(TTR('Remove BlendSpace1D Point'));
+		undo_redo->add_do_method(blend_space.ptr(), 'remove_blend_point', selected_point);
+		undo_redo->add_undo_method(blend_space.ptr(), 'add_blend_point', blend_space->get_blend_point_node(selected_point), blend_space->get_blend_point_position(selected_point), selected_point);
+		undo_redo->add_do_method(this, '_update_space');
+		undo_redo->add_undo_method(this, '_update_space');
+		undo_redo->commit_action();
+    
+    	Ref<StyleBox> style = get_stylebox('panel', 'PopupMenu');
+	Ref<Font> font = get_font('font', 'PopupMenu');
+	Ref<Texture> slot_icon = get_icon('VisualShaderPort', 'EditorIcons');
+    
+    /*!
+ * \brief load a data matrix
+ * \param fname the name of the file
+ * \param silent whether print messages during loading
+ * \param out a loaded data matrix
+ * \return 0 when success, -1 when failure happens
+ */
+XGB_DLL int XGDMatrixCreateFromFile(const char *fname,
+                                    int silent,
+                                    DMatrixHandle *out);
+    
+        for (size_t i = 0; i < symbol_bits_; i++) {
+      size_t byte_idx = ((offset + 1) * symbol_bits_ - (i + 1)) / bits_per_byte;
+      byte_idx += detail::kPadding;
+      size_t bit_idx =
+          ((bits_per_byte + i) - ((offset + 1) * symbol_bits_)) % bits_per_byte;
     }
     
-    #include 'rocksdb/cache.h'
-#include 'rocksdb/compaction_filter.h'
-#include 'rocksdb/db.h'
-#include 'rocksdb/options.h'
-#include 'rocksdb/slice.h'
-#include 'rocksdb/table.h'
-#include 'rocksdb/utilities/options_util.h'
-    
-    #pragma once
-    
-    namespace rocksdb {
-namespace lua {
-class LuaStateWrapper {
- public:
-  explicit LuaStateWrapper(const std::string& lua_script) {
-    lua_state_ = luaL_newstate();
-    Init(lua_script, {});
-  }
-  LuaStateWrapper(
-      const std::string& lua_script,
-      const std::vector<std::shared_ptr<RocksLuaCustomLibrary>>& libraries) {
-    lua_state_ = luaL_newstate();
-    Init(lua_script, libraries);
-  }
-  lua_State* GetLuaState() const { return lua_state_; }
-  ~LuaStateWrapper() { lua_close(lua_state_); }
-    }
-    }
+    TEST(Param, VectorStreamRead) {
+  std::vector<int> vals = {3, 2, 1};
+  std::stringstream ss;
+  std::vector<int> vals_in;
     }
