95fa95d16e0a98cf5a4a1e4c3c9d8773a9f2395f
SUNsung <weatherius@gmail.com>
2019-08-29 17:26:17 +0300

    Rewrite [11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py] File in: 2019-08-29 17:26:17

--- pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
+++ pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
@@ -1,111 +1,270 @@
 
         
-            # test that successful registration redirects to the login page
-    response = client.post('/auth/register', data={'username': 'a', 'password': 'a'})
-    assert 'http://localhost/auth/login' == response.headers['Location']
-    
-    import pytest
-    
-        try:
-        try:
-            rv = url_adapter.build(
-                endpoint, values, method=method, force_external=external
+                    warn(
+                Warning(
+                    'The blueprint was already registered once '
+                    'but is getting modified now.  These changes '
+                    'will not show up.'
+                )
             )
-        finally:
-            if old_scheme is not None:
-                url_adapter.url_scheme = old_scheme
-    except BuildError as error:
-        # We need to inject the values again so that the app callback can
-        # deal with that sort of stuff.
-        values['_external'] = external
-        values['_anchor'] = anchor
-        values['_method'] = method
-        values['_scheme'] = scheme
-        return appctx.app.handle_url_build_error(error, endpoint, values)
+        self.deferred_functions.append(func)
     
-        return kwargs
+            :param namespace: a configuration namespace
+        :param lowercase: a flag indicating if the keys of the resulting
+                          dictionary should be lowercase
+        :param trim_namespace: a flag indicating if the keys of the resulting
+                          dictionary should not include the namespace
+    
+        if args and kwargs:
+        raise TypeError('jsonify() behavior undefined when passed both args and kwargs')
+    elif len(args) == 1:  # single args are passed directly to dumps()
+        data = args[0]
+    else:
+        data = args or kwargs
+    
+        s.register(Tag2, index=None)
+    assert isinstance(s.order[-1], Tag2)
 
     
-        def __init__(self, orig):
-        self._orig = orig
+        @app.route('/')
+    def index():
+        return flask.render_template('simple_template.html', whiskey=42)
     
-        def __init__(self, msg, with_headers=True, with_body=True,
-                 on_body_chunk_downloaded=None):
-        '''
-        :param msg: a :class:`models.HTTPMessage` subclass
-        :param with_headers: if `True`, headers will be included
-        :param with_body: if `True`, body will be included
+        @possible_keys
+    def test_delitem(self, key):
+        del self.case_insensitive_dict[key]
+        assert key not in self.case_insensitive_dict
     
-        def get_formatters_grouped(self):
-        groups = {}
-        for group_name, group in groupby(
-                self.get_formatters(),
-                key=lambda p: getattr(p, 'group_name', 'format')):
-            groups[group_name] = list(group)
-        return groups
+        def _handle_requests(self):
+        for _ in range(self.requests_to_handle):
+            sock = self._accept_connection()
+            if not sock:
+                break
     
-        '''
-    abbrevs = [
-        (1 << 50, 'PB'),
-        (1 << 40, 'TB'),
-        (1 << 30, 'GB'),
-        (1 << 20, 'MB'),
-        (1 << 10, 'kB'),
-        (1, 'B')
-    ]
+        def test_autoset_header_values_are_native(self, httpbin):
+        data = 'this is a string'
+        length = '16'
+        req = requests.Request('POST', httpbin('post'), data=data)
+        p = req.prepare()
     
+            if isinstance(timeout, tuple):
+            try:
+                connect, read = timeout
+                timeout = TimeoutSauce(connect=connect, read=read)
+            except ValueError as e:
+                # this may raise a string formatting error.
+                err = ('Invalid timeout {}. Pass a (connect, read) '
+                       'timeout tuple, or a single float to set '
+                       'both timeouts to the same value'.format(timeout))
+                raise ValueError(err)
+        elif isinstance(timeout, TimeoutSauce):
+            pass
+        else:
+            timeout = TimeoutSauce(connect=timeout, read=timeout)
     
-@pytest.mark.parametrize('argument_name', ['--auth-type', '-A'])
-def test_digest_auth(httpbin_both, argument_name):
-    r = http(argument_name + '=digest', '--auth=user:password',
-             'GET', httpbin_both.url + '/digest-auth/auth/user/password')
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
+            for field, val in fields:
+            if isinstance(val, basestring) or not hasattr(val, '__iter__'):
+                val = [val]
+            for v in val:
+                if v is not None:
+                    # Don't call str() on bytestrings: in Py3 it all goes wrong.
+                    if not isinstance(v, bytes):
+                        v = str(v)
     
-    
-def test_auth_plugin_parse_auth_false(httpbin):
-    
-        if not info_only and not dry_run:
-        if not kwargs['caption']:
-            print('Skipping danmaku.')
-            return
-        try:
-            title = get_filename(title)
-            print('Downloading %s ...\n' % (title + '.cmt.json'))
-            cmt = get_srt_json(vid)
-            with open(os.path.join(output_dir, title + '.cmt.json'), 'w', encoding='utf-8') as x:
-                x.write(cmt)
-        except:
+        def test_server_finishes_when_no_connections(self):
+        '''the server thread exits even if there are no connections'''
+        server = Server.basic_response_server()
+        with server:
             pass
     
-            for stream in info['data']['vidl']:
-            try:
-                stream_id = self.vd_2_id[stream['vd']]
-                if stream_id in self.stream_types:
-                    continue
-                stream_profile = self.id_2_profile[stream_id]
-                self.streams[stream_id] = {'video_profile': stream_profile, 'container': 'm3u8', 'src': [stream['m3u']], 'size' : 0, 'm3u8_url': stream['m3u']}
-            except Exception as e:
-                log.i('vd: {} is not handled'.format(stream['vd']))
-                log.i('info is {}'.format(stream))
+        # Check urllib3 for compatibility.
+    major, minor, patch = urllib3_version  # noqa: F811
+    major, minor, patch = int(major), int(minor), int(patch)
+    # urllib3 >= 1.21.1, <= 1.25
+    assert major == 1
+    assert minor >= 21
+    assert minor <= 25
     
+    scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
     
-    import urllib
+        Notes
+    -----
+    NaNs are ignored in the algorithm.
     
-    from ..common import *
+    First, we fix a training set and increase the number of
+samples. Then we plot the computation time as function of
+the number of samples.
     
-    #----------------------------------------------------------------------
-def ckplayer_download(url, output_dir = '.', merge = False, info_only = False, is_xml = True, **kwargs):
-    if is_xml:  #URL is XML URL
+    from sklearn.utils import check_random_state
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.metrics.pairwise import pairwise_kernels
+    
+                gc.collect()
+            print('max_iter', max_iter)
+            print('- benchmarking A-SGD')
+            clf = SGDRegressor(alpha=alpha / n_train, fit_intercept=False,
+                               max_iter=max_iter, learning_rate='invscaling',
+                               eta0=.002, power_t=0.05, tol=1e-3,
+                               average=(max_iter * n_train // 2))
+    
+    Does two benchmarks
+    
         try:
-            title = kwargs['title']
-        except:
-            title = ''
+        fn = inspect.getsourcefile(obj)
+    except Exception:
+        fn = None
+    if not fn:
         try:
-            headers = kwargs['headers']  #headers provided
-            ckinfo = get_content(url, headers = headers)
-        except NameError:
-            ckinfo = get_content(url)
-        
-        ckplayer_download_by_xml(ckinfo, output_dir, merge, 
-                                info_only, title = title)
+            fn = inspect.getsourcefile(sys.modules[obj.__module__])
+        except Exception:
+            fn = None
+    if not fn:
+        return
+    
+    
+if __name__ == '__main__':
+    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
+    # block to be able to use a multi-core grid search that also works under
+    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
+    # The multiprocessing module is used as the backend of joblib.Parallel
+    # that is used when n_jobs != 1 in GridSearchCV
+    
+    plt.figure(2)  # 'banana' shape
+plt.title('Outlier detection on a real data set (boston housing)')
+plt.scatter(X2[:, 0], X2[:, 1], color='black')
+plt.xlim((xx2.min(), xx2.max()))
+plt.ylim((yy2.min(), yy2.max()))
+plt.legend((legend2_values_list[0].collections[0],
+            legend2_values_list[1].collections[0],
+            legend2_values_list[2].collections[0]),
+           (legend2_keys_list[0], legend2_keys_list[1], legend2_keys_list[2]),
+           loc='upper center',
+           prop=matplotlib.font_manager.FontProperties(size=12))
+plt.ylabel('% lower status of the population')
+plt.xlabel('average number of rooms per dwelling')
+    
+        def close(self):
+        self.closed = True
+    
+        @support.cpython_only
+    @unittest.skipUnless(hasattr(os, 'pipe2'), 'test needs os.pipe2()')
+    @support.requires_linux_version(2, 6, 27)
+    def test_pipe2_c_limits(self):
+        # Issue 15989
+        import _testcapi
+        self.assertRaises(OverflowError, os.pipe2, _testcapi.INT_MAX + 1)
+        self.assertRaises(OverflowError, os.pipe2, _testcapi.UINT_MAX + 1)
+    
+    import sys
+import socket
+from socket import _GLOBAL_DEFAULT_TIMEOUT
+    
+    >>> from nntplib import NNTP
+>>> s = NNTP('news')
+>>> resp, count, first, last, name = s.group('comp.lang.python')
+>>> print('Group', name, 'has', count, 'articles, range', first, 'to', last)
+Group comp.lang.python has 51 articles, range 5770 to 5821
+>>> resp, subs = s.xhdr('subject', '{0}-{1}'.format(first, last))
+>>> resp = s.quit()
+>>>
+    
+    def run_latency_tests(max_threads):
+    for task in latency_tasks:
+        print('Background CPU task:', task.__doc__)
+        print()
+        func, args = task()
+        nthreads = 0
+        while nthreads <= max_threads:
+            results = run_latency_test(func, args, nthreads)
+            n = len(results)
+            # We print out milliseconds
+            lats = [1000 * (t2 - t1) for (t1, t2) in results]
+            #print(list(map(int, lats)))
+            avg = sum(lats) / n
+            dev = (sum((x - avg) ** 2 for x in lats) / n) ** 0.5
+            print('CPU threads=%d: %d ms. (std dev: %d ms.)' % (nthreads, avg, dev), end='')
+            print()
+            #print('    [... from %d samples]' % n)
+            nthreads += 1
+        print()
+    
+                return STARTUPINFO(dwFlags=self.dwFlags,
+                               hStdInput=self.hStdInput,
+                               hStdOutput=self.hStdOutput,
+                               hStdError=self.hStdError,
+                               wShowWindow=self.wShowWindow,
+                               lpAttributeList=attr_list)
+    
+            # Adding model 'EventTag'
+        db.create_table(u'tagstore_eventtag', (
+            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()),
+            ('environment_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()),
+            ('key', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['tagstore.TagKey'], db_column='key')),
+            ('event_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(
+                db_index=True)),
+            ('group_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()),
+            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
+            ('value', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['tagstore.TagValue'], db_column='value')),
+        ))
+        db.send_create_signal('tagstore', ['EventTag'])
+    
+            # Adding unique constraint on 'EventTag', fields ['event_id', 'key', 'value']
+        db.create_unique(u'tagstore_eventtag', ['event_id', 'key_id', 'value_id'])
+    
+        >>> list(shingle(2, ('foo', 'bar', 'baz')))
+    [('foo', 'bar'), ('bar', 'baz')]
+    '''
+    return itertools.izip(
+        *map(
+            lambda i__iterator: advance(i__iterator[0], i__iterator[1]),
+            enumerate(itertools.tee(iterator, n)),
+        )
+    )
+    
+    *TL;DR
+Encapsulates how a set of objects interact.
+'''
+    
+    
+class HexViewer:
+    def update(self, subject):
+        print(u'HexViewer: Subject %s has data 0x%x' % (subject.name, subject.data))
+    
+    
+class SuperUserSpecification(CompositeSpecification):
+    def is_satisfied_by(self, candidate):
+        return getattr(candidate, 'super_user', False)
+    
+        >>> Order(1000, discount_strategy=on_sale_discount)
+    <Price: 1000, price after discount: 730.0>
+    '''
+    
+    *What does this example do?
+This particular implementation abstracts the creation of a pet and
+does so depending on the factory we chose (Dog or Cat, or random_animal)
+This works because both Dog/Cat and random_animal respect a common
+interface (callable for creation and .speak()).
+Now my application can create pets abstractly and decide later,
+based on my own criteria, dogs over cats.
+    
+        def localize(self, msg):
+        '''We'll punt if we don't have a translation'''
+        return self.translations.get(msg, msg)
+    
+    
+def main():
+    shapes = (CircleShape(1, 2, 3, DrawingAPI1()), CircleShape(5, 7, 11, DrawingAPI2()))
+    
+    
+# Complex computer parts
+class CPU(object):
+    '''
+    Simple CPU representation.
+    '''
+    def freeze(self):
+        print('Freezing processor.')
