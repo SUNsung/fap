91060fa9a833e73e9230c7a748fc027bd24c1e9b
SUNsung <weatherius@gmail.com>
2019-08-29 10:19:50 +0300

    Rewrite [289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py] File in: 2019-08-29 10:19:49

--- pyton/289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py
+++ pyton/289UZcqoGTEGQ28jSO3Luwdlwg28M40aY20HeJc28qE.py
@@ -1,306 +1,174 @@
 
         
-        
-@pytest.mark.parametrize('args, result', [
-    (('apt-get instol vim', 'instol', 'install'), 'apt-get install vim'),
-    (('git brnch', 'brnch', 'branch'), 'git branch')])
-def test_replace_argument(args, result):
-    assert replace_argument(*args) == result
-    
-        @wraps(fn)
-    def wrapper(*args, **kwargs):
-        if not memoize.disabled:
-            key = pickle.dumps((args, kwargs))
-            if key not in memo:
-                memo[key] = fn(*args, **kwargs)
-            value = memo[key]
-        else:
-            # Memoize is disabled, call the function
-            value = fn(*args, **kwargs)
-    
-    For example the `llc` program is in package `llvm` so this:
-    yay -S llc
-should be:
-    yay -S llvm
+        Provides utility functions that are consumed internally by Requests
+which depend on extremely few external helpers (such as compat)
 '''
     
-        def _expand_aliases(self, command_script):
-        aliases = self.get_aliases()
-        binary = command_script.split(' ')[0]
-        if binary in aliases and aliases[binary] != binary:
-            return command_script.replace(binary, aliases[binary], 1)
-        elif binary in aliases:
-            return u'fish -ic '{}''.format(command_script.replace(''', r'\''))
+    
+class StreamConsumedError(RequestException, TypeError):
+    '''The content for this response was already consumed'''
+    
+    
+@pytest.fixture
+def httpbin_secure(httpbin_secure):
+    return prepare_url(httpbin_secure)
+
+    
+    import pytest
+    
+        def __init__(self, handler=None, host='localhost', port=0, requests_to_handle=1, wait_to_close_event=None):
+        super(Server, self).__init__()
+    
+    elif is_py3:
+    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
+    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
+    from http import cookiejar as cookielib
+    from http.cookies import Morsel
+    from io import StringIO
+    # Keep OrderedDict for backwards compatibility.
+    from collections import OrderedDict
+    from collections.abc import Callable, Mapping, MutableMapping
+    
+    
+class LookupDict(dict):
+    '''Dictionary lookup object.'''
+    
+    ]
+test_requirements = [
+    'pytest-httpbin==0.0.7',
+    'pytest-cov',
+    'pytest-mock',
+    'pytest-xdist',
+    'PySocks>=1.5.6, !=1.5.7',
+    'pytest>=3'
+]
+    
+        :param url: URL for the new :class:`Request` object.
+    :param params: (optional) Dictionary, list of tuples or bytes to send
+        in the query string for the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
+    
+        def set_config(self, configfile, config):
+        ''' Set the config to either global config or passed in config '''
+        section = '.'.join(self.__module__.split('.')[-2:])
+        if config is None:
+            logger.debug('Loading base config')
+            retval = get_config(section, configfile=configfile)
         else:
-            return command_script
+            logger.debug('Loading passed in config')
+            config.section = section
+            retval = config.config_dict
+            config.section = None
+        logger.debug('Config: %s', retval)
+        return retval
     
+            self.status()
+        self.pbar = self.progress_bar()
     
-@pytest.mark.parametrize('script, pyenv_cmd, result', [
-    ('pyenv globe', 'globe', 'pyenv global'),
-    ('pyenv intall 3.8.0', 'intall', 'pyenv install 3.8.0'),
-    ('pyenv list', 'list', 'pyenv install --list'),
-    ('pyenv remove 3.8.0', 'remove', 'pyenv uninstall 3.8.0'),
-])
-def test_get_new_command(script, pyenv_cmd, output, result):
-    assert result in get_new_command(Command(script, output))
+            return blended.astype('float32') / 255.0
 
     
     
-@for_app('pyenv')
-def get_new_command(command):
-    broken = re.findall(r'pyenv: no such command `([^']*)'', command.output)[0]
-    matched = [replace_argument(command.script, broken, common_typo)
-               for common_typo in COMMON_TYPOS.get(broken, [])]
-    matched.extend(replace_command(command, broken, get_pyenv_commands()))
-    return matched
-
+class EnglishLocalizer(object):
+    '''Simply echoes the message'''
     
     
-easy_install.ScriptWriter.get_args = get_args
+class Delegator(object):
+    '''
+    >>> delegator = Delegator(Delegate())
+    >>> delegator.p1
+    123
+    >>> delegator.p2
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Delegate' object has no attribute 'p2'
+    >>> delegator.do_something('nothing')
+    'Doing nothing'
+    >>> delegator.do_anything()
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Delegate' object has no attribute 'do_anything'
+    '''
     
-            # Keyword arguments > stream.encoding > default utf8
-        if self.stdin_encoding is None:
-            self.stdin_encoding = getattr(
-                self.stdin, 'encoding', None) or 'utf8'
-        if self.stdout_encoding is None:
-            actual_stdout = self.stdout
-            if is_windows:
-                # noinspection PyUnresolvedReferences
-                from colorama import AnsiToWin32
-                if isinstance(self.stdout, AnsiToWin32):
-                    actual_stdout = self.stdout.wrapped
-            self.stdout_encoding = getattr(
-                actual_stdout, 'encoding', None) or 'utf8'
+        >>> cat = Cat()
+    >>> objects.append(Adapter(cat, make_noise=cat.meow))
+    >>> human = Human()
+    >>> objects.append(Adapter(human, make_noise=human.speak))
+    >>> car = Car()
+    >>> objects.append(Adapter(car, make_noise=lambda: car.make_noise(3)))
     
+    
+def main():
+    shapes = (CircleShape(1, 2, 3, DrawingAPI1()), CircleShape(5, 7, 11, DrawingAPI2()))
+    
+    '''
+*What is this pattern about?
+The Decorator pattern is used to dynamically add a new feature to an
+object without changing its implementation. It differs from
+inheritance because the new feature is added only to that particular
+object, not to the entire subclass.
+    
+        def test_series_unnamed(self):
+        result = build_table_schema(pd.Series([1, 2, 3]), version=False)
+        expected = {
+            'fields': [
+                {'name': 'index', 'type': 'integer'},
+                {'name': 'values', 'type': 'integer'},
+            ],
+            'primaryKey': ['index'],
+        }
+        assert result == expected
+    
+        def bp_sensitivity_map(self, sensitivity_array,
+                           activator):
         '''
+        计算传递到上一层的sensitivity map
+        sensitivity_array: 本层的sensitivity map
+        activator: 上一层的激活函数
+        '''
+        # 处理卷积步长，对原始sensitivity map进行扩展
+        expanded_array = self.expand_sensitivity_map(
+            sensitivity_array)
+        # full卷积，对sensitivitiy map进行zero padding
+        # 虽然原始输入的zero padding单元也会获得残差
+        # 但这个残差不需要继续向上传递，因此就不计算了
+        expanded_width = expanded_array.shape[2]
+        zp = (self.input_width +  
+              self.filter_width - 1 - expanded_width) / 2
+        padded_array = padding(expanded_array, zp)
+        # 初始化delta_array，用于保存传递到上一层的
+        # sensitivity map
+        self.delta_array = self.create_delta_array()
+        # 对于具有多个filter的卷积层来说，最终传递到上一层的
+        # sensitivity map相当于所有的filter的
+        # sensitivity map之和
+        for f in range(self.filter_number):
+            filter = self.filters[f]
+            # 将filter权重翻转180度
+            flipped_weights = np.array(map(
+                lambda i: np.rot90(i, 2), 
+                filter.get_weights()))
+            # 计算与一个filter对应的delta_array
+            delta_array = self.create_delta_array()
+            for d in range(delta_array.shape[0]):
+                conv(padded_array[f], flipped_weights[d],
+                    delta_array[d], 1, 0)
+            self.delta_array += delta_array
+        # 将计算结果与激活函数的偏导数做element-wise乘法操作
+        derivative_array = np.array(self.input_array)
+        element_wise_op(derivative_array, 
+                        activator.backward)
+        self.delta_array *= derivative_array
     
+        def calc_gradient_t(self, t):
+        '''
+        计算每个时刻t权重的梯度
+        '''
+        gradient = np.dot(self.delta_list[t],
+            self.state_list[t-1].T)
+        self.gradient_list[t] = gradient
     
-class TestAutoContentTypeAndAcceptHeaders:
-    '''
-    Test that Accept and Content-Type correctly defaults to JSON,
-    but can still be overridden. The same with Content-Type when --form
-    -f is used.
-    
-    
-@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
-def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
-    r = http(follow_flag, httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 1
-    assert HTTP_OK in r
-    
-            admin_command = '''osascript -e 'do shell script '%s' with administrator privileges' ''' % exec_command
-        cmd = admin_command.encode('utf-8')
-        xlog.info('try auto import CA command:%s', cmd)
-        os.system(cmd)
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licence]
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-    
-def detect_faces_in_image(file_stream):
-    # Pre-calculated face encoding of Obama generated with face_recognition.face_encodings(img)
-    known_face_encoding = [-0.09634063,  0.12095481, -0.00436332, -0.07643753,  0.0080383,
-                            0.01902981, -0.07184699, -0.09383309,  0.18518871, -0.09588896,
-                            0.23951106,  0.0986533 , -0.22114635, -0.1363683 ,  0.04405268,
-                            0.11574756, -0.19899382, -0.09597053, -0.11969153, -0.12277931,
-                            0.03416885, -0.00267565,  0.09203379,  0.04713435, -0.12731361,
-                           -0.35371891, -0.0503444 , -0.17841317, -0.00310897, -0.09844551,
-                           -0.06910533, -0.00503746, -0.18466514, -0.09851682,  0.02903969,
-                           -0.02174894,  0.02261871,  0.0032102 ,  0.20312519,  0.02999607,
-                           -0.11646006,  0.09432904,  0.02774341,  0.22102901,  0.26725179,
-                            0.06896867, -0.00490024, -0.09441824,  0.11115381, -0.22592428,
-                            0.06230862,  0.16559327,  0.06232892,  0.03458837,  0.09459756,
-                           -0.18777156,  0.00654241,  0.08582542, -0.13578284,  0.0150229 ,
-                            0.00670836, -0.08195844, -0.04346499,  0.03347827,  0.20310158,
-                            0.09987706, -0.12370517, -0.06683611,  0.12704916, -0.02160804,
-                            0.00984683,  0.00766284, -0.18980607, -0.19641446, -0.22800779,
-                            0.09010898,  0.39178532,  0.18818057, -0.20875394,  0.03097027,
-                           -0.21300618,  0.02532415,  0.07938635,  0.01000703, -0.07719778,
-                           -0.12651891, -0.04318593,  0.06219772,  0.09163868,  0.05039065,
-                           -0.04922386,  0.21839413, -0.02394437,  0.06173781,  0.0292527 ,
-                            0.06160797, -0.15553983, -0.02440624, -0.17509389, -0.0630486 ,
-                            0.01428208, -0.03637431,  0.03971229,  0.13983178, -0.23006812,
-                            0.04999552,  0.0108454 , -0.03970895,  0.02501768,  0.08157793,
-                           -0.03224047, -0.04502571,  0.0556995 , -0.24374914,  0.25514284,
-                            0.24795187,  0.04060191,  0.17597422,  0.07966681,  0.01920104,
-                           -0.01194376, -0.02300822, -0.17204897, -0.0596558 ,  0.05307484,
-                            0.07417042,  0.07126575,  0.00209804]
-    
-        for file in image_files_in_folder(known_people_folder):
-        basename = os.path.splitext(os.path.basename(file))[0]
-        img = face_recognition.load_image_file(file)
-        encodings = face_recognition.face_encodings(img)
-    
-            return link_list
-    
-        for quality in ['1080','720','480','380','240','144','auto']:
-        try:
-            real_url = info[quality][1]['url']
-            if real_url:
-                break
-        except KeyError:
-            pass
-    
-        else:
-        titles = re.findall(r''name':'([^']*)'', html)
-        real_urls = [re.sub('\\\\/', '/', i) for i in re.findall(r''rawUrl':'([^']*)'', html)]
-    
-        def load_module(self, filename, module_path, plugin_type):
-        ''' Load the defaults module and add defaults '''
-        logger.debug('Adding defaults: (filename: %s, module_path: %s, plugin_type: %s',
-                     filename, module_path, plugin_type)
-        module = os.path.splitext(filename)[0]
-        section = '.'.join((plugin_type, module.replace('_defaults', '')))
-        logger.debug('Importing defaults module: %s.%s', module_path, module)
-        mod = import_module('{}.{}'.format(module_path, module))
-        self.add_section(title=section, info=mod._HELPTEXT)  # pylint:disable=protected-access
-        for key, val in mod._DEFAULTS.items():  # pylint:disable=protected-access
-            self.add_item(section=section, title=key, **val)
-        logger.debug('Added defaults: %s', section)
-
-    
-    
-_DEFAULTS = {
-    'threshold': {
-        'default': 99.0,
-        'info': 'Adjust the threshold for histogram matching. Can reduce extreme colors '
-                'leaking in by filtering out colors at the extreme ends of the histogram '
-                'spectrum.',
-        'datatype': float,
-        'rounding': 1,
-        'min_max': (90.0, 100.0),
-        'choices': [],
-        'gui_radio': False,
-        'fixed': True,
-    }
-}
-
-    
-    
-_DEFAULTS = {
-    'fps': {
-        'default': 25,
-        'info': 'Frames per Second.',
-        'datatype': int,
-        'rounding': 1,
-        'min_max': (1, 60),
-        'choices': [],
-        'gui_radio': False,
-        'fixed': True,
-    },
-    'loop': {
-        'default': 0,
-        'info': 'The number of iterations. Set to 0 to loop indefinitely.',
-        'datatype': int,
-        'rounding': 1,
-        'min_max': (0, 100),
-        'choices': [],
-        'gui_radio': False,
-        'fixed': True,
-    },
-    'palettesize': {
-        'default': '256',
-        'info': 'The number of colors to quantize the image to. Is rounded to the nearest '
-                'power of two.',
-        'datatype': str,
-        'rounding': None,
-        'min_max': None,
-        'choices': ['2', '4', '8', '16', '32', '64', '128', '256'],
-        'gui_radio': False,
-        'fixed': True,
-    },
-    'subrectangles': {
-        'default': False,
-        'info': 'If True, will try and optimize the GIF by storing only the rectangular parts '
-                'of each frame that change with respect to the previous.',
-        'datatype': bool,
-        'rounding': None,
-        'min_max': None,
-        'choices': [],
-        'gui_radio': False,
-        'fixed': True,
-    },
-}
-
-    
-    
-class User(object):
-    '''A class whose instances want to interact with each other'''
-    
-    '''
-@author: Gordeev Andrey <gordeev.and.and@gmail.com>
-    
-    
-OUTPUT = '''
-Scanning... Station is 1380 AM
-Scanning... Station is 1510 AM
-Switching to FM
-Scanning... Station is 89.1 FM
-Scanning... Station is 103.9 FM
-Scanning... Station is 81.3 FM
-Scanning... Station is 89.1 FM
-Switching to AM
-Scanning... Station is 1250 AM
-Scanning... Station is 1380 AM
-'''
-
-    
-        # Localize some text
-    >>> for msg in 'dog parrot cat bear'.split():
-    ...     print(e.localize(msg), g.localize(msg))
-    dog σκύλος
-    parrot parrot
-    cat γάτα
-    bear bear
-    '''
-    
-        def __getattr__(self, name):
-        attr = getattr(self.delegate, name)
-    
-        def set_stop_words(self, stop_words_path):
-        abs_path = _get_abs_path(stop_words_path)
-        if not os.path.isfile(abs_path):
-            raise Exception('jieba: file does not exist: ' + abs_path)
-        content = open(abs_path, 'rb').read().decode('utf-8')
-        for line in content.splitlines():
-            self.stop_words.add(line)
-    
-    for f_name in glob.glob(pattern):
-    with open(f_name) as f:
-        print('read file:', f_name)
-        for line in f: #one line as a document
-            words = ' '.join(jieba.cut(line))
-            docs.append(words)
-    
-    print('speed' , len(content)/tm_cost, ' bytes/second')
+    from numpy import random, mat, eye
