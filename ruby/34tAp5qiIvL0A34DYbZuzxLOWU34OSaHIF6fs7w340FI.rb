142db27768b93d478f72ea2d44d2aa9f91077cf8
SUNsung <weatherius@gmail.com>
2019-08-30 05:46:00 +0300

    Rewrite [34tAp5qiIvL0A34DYbZuzxLOWU34OSaHIF6fs7w340FI.rb] File in: 2019-08-30 05:46:00

--- ruby/34tAp5qiIvL0A34DYbZuzxLOWU34OSaHIF6fs7w340FI.rb
+++ ruby/34tAp5qiIvL0A34DYbZuzxLOWU34OSaHIF6fs7w340FI.rb
@@ -1,138 +1,97 @@
-  protected
+
+        
+          end
     
-    class Devise::OmniauthCallbacksController < DeviseController
-  prepend_before_action { request.env['devise.skip_timeout'] = true }
-    
-      protected
-    
-      # Returns a signed in resource from session (if one exists)
-  def signed_in_resource
-    warden.authenticate(scope: resource_name)
+        expect(cooked).to include('class='poll'')
+    expect(cooked).to include('data-poll-status='open'')
+    expect(cooked).to include('data-poll-name='poll'')
+    expect(cooked).to include('data-poll-type='multiple'')
+    expect(cooked).to include('data-poll-min='1'')
+    expect(cooked).to include('data-poll-max='3'')
+    expect(cooked).to include('data-poll-public='true'')
   end
     
-          # Sign in a user bypassing the warden callbacks and stores the user
-      # straight in session. This option is useful in cases the user is already
-      # signed in, but we want to refresh the credentials in session.
-      #
-      # Examples:
-      #
-      #   bypass_sign_in @user, scope: :user
-      #   bypass_sign_in @user
-      def bypass_sign_in(resource, scope: nil)
-        scope ||= Devise::Mapping.find_scope!(resource)
-        expire_data_after_sign_in!
-        warden.session_serializer.store(resource, scope)
-      end
+          topic.update_status(
+        :closed, true, Discourse.system_user,
+        message: I18n.t(
+          'topic_statuses.autoclosed_topic_max_posts',
+          count: SiteSetting.auto_close_topics_post_count
+        )
+      )
+    end
+  end
     
-      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
-     options[:store] != false && !env['devise.skip_timeoutable']
-    last_request_at = warden.session(scope)['last_request_at']
+          let(:filename) { 'utf-8.txt' }
+      let(:file) { file_from_fixtures(filename, 'encodings') }
     
-            # Initializes the communicator with the machine that we will be
-        # communicating with. This base method does nothing (it doesn't
-        # even store the machine in an instance variable for you), so you're
-        # expected to override this and do something with the machine if
-        # you care about it.
-        #
-        # @param [Machine] machine The machine this instance is expected to
-        #   communicate with.
-        def initialize(machine)
-        end
+        before(:each) do
+      SiteSetting.s3_upload_bucket = 'some-really-cool-bucket'
+      SiteSetting.s3_access_key_id = 's3-access-key-id'
+      SiteSetting.s3_secret_access_key = 's3-secret-access-key'
+      SiteSetting.enable_s3_uploads = true
+      SiteSetting.prevent_anons_from_downloading_files = true
+    end
     
-            # This returns all the registered guests.
-        #
-        # @return [Hash]
-        def guests
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.guests)
-            end
+          <% id = 1 %>
+      <% frames.each do |frame| %>
+          <% if frame.context_line && frame.context_line != '#' %>
+    
+            # Set these key values to boolean 'true' to include in policy
+        NO_ARG_DIRECTIVES.each do |d|
+          if options.key?(d) && options[d].is_a?(TrueClass)
+            directives << d.to_s.sub(/_/, '-')
           end
         end
     
-      # Under Phusion Passenger smart spawning, we need to reopen all IO streams
-  # after workers have forked.
-  #
-  # The rolling file appender uses shared file locks to ensure that only one
-  # process will roll the log file. Each process writing to the file must have
-  # its own open file descriptor for `flock` to function properly. Reopening
-  # the file descriptors after forking ensures that each worker has a unique
-  # file descriptor.
-  if defined? PhusionPassenger
-    PhusionPassenger.on_event(:starting_worker_process) do |forked|
-      Logging.reopen if forked
-    end
-  end
-end
+      it 'should allow changing the protection settings' do
+    mock_app do
+      use Rack::Protection::ContentSecurityPolicy, :default_src => 'none', :script_src => 'https://cdn.mybank.net', :style_src => 'https://cdn.mybank.net', :img_src => 'https://cdn.mybank.net', :connect_src => 'https://api.mybank.com', :frame_src => 'self', :font_src => 'https://cdn.mybank.net', :object_src => 'https://cdn.mybank.net', :media_src => 'https://cdn.mybank.net', :report_uri => '/my_amazing_csp_report_parser', :sandbox => 'allow-scripts'
     
-      def up_down(change)
-    change.up do
-      Mention.update_all(mentions_container_type: 'Post')
-      change_column :mentions, :mentions_container_type, :string, null: false
-      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
-    end
+        scope :checkout, -> { with_state('checkout') }
+    scope :completed, -> { with_state('completed') }
+    scope :pending, -> { with_state('pending') }
+    scope :processing, -> { with_state('processing') }
+    scope :failed, -> { with_state('failed') }
     
-    When /^I (?:sign|log) in manually as '([^']*)' with password '([^']*)'( on the mobile website)?$/ \
-do |username, password, mobile|
-  @me = User.find_by_username(username)
-  @me.password ||= password
-  manual_login
-  confirm_login mobile
-end
-    
-    Then /^I should see an image in the publisher$/ do
-  photo_in_publisher.should be_present
-end
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
-  end
-end
-    
-        it 'generates the contacts_json fixture', :fixture => true do
-      json = bob.contacts.map { |c|
-               ContactPresenter.new(c, bob).full_hash_with_person
-             }.to_json
-      save_fixture(json, 'contacts_json')
-    end
-  end
-end
-
-    
-        it 'generates a jasmine fixture', :fixture => true do
-      get :bookmarklet
-      save_fixture(html_for('body'), 'bookmarklet')
-    end
-    
-        it 'returns an array of likes for a post' do
-      bob.like!(@message)
-      get :index, params: {post_id: @message.id}
-      expect(JSON.parse(response.body).map {|h| h['id'] }).to match_array(@message.likes.map(&:id))
-    end
-    
-          it 'creates a reshare' do
-        expect{
-          post_request!
-        }.to change(Reshare, :count).by(1)
+          def protect_from_connection_error
+        yield
+      rescue ActiveMerchant::ConnectionError => e
+        gateway_error(e)
       end
     
-            self.summary = 'The repl listens to commands on standard input'
-        self.description = <<-DESC
-          The repl listens to commands on standard input and prints their
-          result to standard output.
-          It accepts all the other ipc subcommands. The repl will signal the
-          end of output with the the ASCII CR+LF `\\n\\r`.
-        DESC
+    module Spree
+  class ReturnItem::EligibilityValidator::Default < Spree::ReturnItem::EligibilityValidator::BaseValidator
+    class_attribute :permitted_eligibility_validators
+    self.permitted_eligibility_validators = [
+      ReturnItem::EligibilityValidator::OrderCompleted,
+      ReturnItem::EligibilityValidator::TimeSincePurchase,
+      ReturnItem::EligibilityValidator::RMARequired,
+      ReturnItem::EligibilityValidator::InventoryShipped,
+      ReturnItem::EligibilityValidator::NoReimbursements
+    ]
     
-    When /^(?:|I )attach the file '([^']*)' to '([^']*)'$/ do |path, field|
-  attach_file(field, File.expand_path(path))
-end
-    
-      def generate_migration
-    migration_template('paperclip_migration.rb.erb',
-                       'db/migrate/#{migration_file_name}',
-                       migration_version: migration_version)
+      def self.load_translations(hash, root = [])
+    hash.each do |k, v|
+      if v.is_a?(Hash)
+        load_translations(v, root.dup << k)
+      else
+        key = (root + [k]).join('.')
+        unused_translations << key
+      end
+    end
   end
+    
+          let!(:shipping_method) { create(:shipping_method) }
+      let(:package)          { build(:stock_package, contents: inventory_units.map { |_i| ContentItem.new(inventory_unit) }) }
+      let(:ship_address)     { create(:ship_address) }
+      let(:order)            { build(:order_with_line_items, ship_address: ship_address) }
+      let(:inventory_units)  { order.inventory_units }
+    
+            scope
+      end
+    
+            def show
+          authorize! :admin, ReturnAuthorization
+          @return_authorization = order.return_authorizations.accessible_by(current_ability, :show).find(params[:id])
+          respond_with(@return_authorization)
+        end
