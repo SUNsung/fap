33612ea1411f37067b5024a499fcd82a44c2bd2c
SUNsung <weatherius@gmail.com>
2019-08-30 14:45:45 +0300

    Rewrite [47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp] File in: 2019-08-30 14:45:45

--- cccccc/47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp
+++ cccccc/47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp
@@ -1,308 +1,91 @@
 
         
-        // Type-opaque holder for an argument to format(), with associated actions on
-// the type held as explicit function pointers.  This allows FormatArg's for
-// each argument to be allocated as a homogenous array inside FormatList
-// whereas a naive implementation based on inheritance does not.
-class FormatArg
-{
+        TEST(ClusteredBitVector, AddAdd) {
+  ClusteredBitVector vec;
+  vec.add(8, 206);
+  vec.add(17, 56854);
+  EXPECT_EQ(25u, vec.size());
+  EXPECT_EQ(true, vec[9]);
+}
+    
+    KNOWN_STDLIB_TYPE_DECL(Encoder, ProtocolDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(Decoder, ProtocolDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(KeyedEncodingContainer, NominalTypeDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(KeyedDecodingContainer, NominalTypeDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(RangeReplaceableCollection, ProtocolDecl, 1)
+    
+      uselocale(OldLocale);
+    
+    #ifndef SWIFT_REFLECTION_METADATASOURCEBUILDER_H
+#define SWIFT_REFLECTION_METADATASOURCEBUILDER_H
+    
+      /// The required alignment of the first byte of an object of this
+  /// type, expressed as a mask of the low bits that must not be set
+  /// in the pointer.
+  ///
+  /// This representation can be easily converted to the 'alignof'
+  /// result by merely adding 1, but it is more directly useful for
+  /// performing dynamic structure layouts, and it grants an
+  /// additional bit of precision in a compact field without needing
+  /// to switch to an exponent representation.
+  ///
+  /// For example, if the type needs to be 8-byte aligned, the
+  /// appropriate alignment mask should be 0x7.
+  size_t getAlignmentMask() const {
+    return (Data & AlignmentMask);
+  }
+  constexpr TargetValueWitnessFlags withAlignmentMask(size_t alignMask) const {
+    return TargetValueWitnessFlags((Data & ~AlignmentMask) | alignMask);
+  }
+    
+      /// Attempt to collect information from the given info chunk.
+  ///
+  /// \return true if collection was successful.
+  template <bool Asserting>
+  bool collect(StringRef value) {
+#define check(CONDITION, COMMENT)            \
+    do {                                     \
+      if (!Asserting) {                      \
+        if (!(CONDITION)) return false;      \
+      } else {                               \
+        assert((CONDITION) && COMMENT);      \
+      }                                      \
+    } while (false)
+    }
+    
     public:
-        FormatArg()
-             : m_value(nullptr),
-             m_formatImpl(nullptr),
-             m_toIntImpl(nullptr)
-         { }
-    }
-    
-    
-    
-    #ifndef BITCOIN_BECH32_H
-#define BITCOIN_BECH32_H
-    
-    // Generate constructors.
-#include 'ipc/struct_constructor_macros.h'
-#include 'content/nw/src/common/common_message_generator.h'
-    
-    class App {
- public:
-  static void Call(const std::string& method,
-                   const base::ListValue& arguments);
-    }
-    
-      virtual void Call(const std::string& method,
-                    const base::ListValue& arguments,
-                    content::RenderFrameHost* rvh = nullptr);
-  virtual void CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result);
-    
-    Clipboard::Clipboard(int id,
-           const base::WeakPtr<DispatcherHost>& dispatcher_host,
-           const base::DictionaryValue& option)
-    : Base(id, dispatcher_host, option) {
-}
-    
-    class Clipboard : public Base {
- public:
-  Clipboard(int id,
-            const base::WeakPtr<DispatcherHost>& dispatcher_host,
-            const base::DictionaryValue& option);
-  ~Clipboard() override;
-    }
-    
-    
-    {}
-    
-    
-    {}  // namespace nwapi
-
-    
-    
-    {}
-    
-    #include 'base/values.h'
-#include 'content/nw/src/api/dispatcher_host.h'
-#include 'content/nw/src/api/menu/menu.h'
-#include 'gdk/gdkkeysyms.h'//to get keyval from name
-    
-    void MenuItem::SetEnabled(bool enabled) {
-  is_enabled_ = enabled;
-  if (menu_)
-    menu_->UpdateStates();
-}
-    
-          std::unique_ptr<SkBitmap> bitmap(new SkBitmap());
-      if (data.type == TYPE_PNG &&
-        !gfx::PNGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size(), bitmap.get())) {
-        error_ = 'Failed to decode as PNG';
-        return false;
-      } else if (data.type == TYPE_JPEG) {
-        std::unique_ptr<SkBitmap> tmp_bitmap = gfx::JPEGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size());
-        if (tmp_bitmap == NULL) {
-          error_ = 'Failed to decode as JPEG';
-          return false;
-        }
-        bitmap = std::move(tmp_bitmap);
-      }
-    
-    #include 'extensions/browser/extension_function.h'
-    
-    		Image image(a_pafSourceRGBA, a_uiSourceWidth,
-					a_uiSourceHeight,
-					a_eErrMetric);
-		image.m_bVerboseOutput = a_bVerboseOutput;
-		image.Encode(a_format, a_eErrMetric, a_fEffort, a_uiJobs, a_uiMaxJobs);
-    
-    
-    {			return fDX*fDX + fDY*fDY + fDZ*fDZ + fDW*fDW;
-		}
-    
-    	class Block4x4EncodingBits_RG11
-	{
-	public:
-    }
-    
-      The output buffer must be at least 5% larger than the input buffer
-  and can not be smaller than 66 bytes.
-    
-       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-    
-    /** 16x32 multiply, followed by a 16-bit shift right and 32-bit add.
-    Result fits in 32 bits. */
-#undef MAC16_32_Q16
-static OPUS_INLINE opus_val32 MAC16_32_Q16_armv5e(opus_val32 c, opus_val16 a,
- opus_val32 b)
-{
-  int res;
-  __asm__(
-      '#MAC16_32_Q16\n\t'
-      'smlawb %0, %1, %2, %3;\n'
-      : '=r'(res)
-      : 'r'(b), 'r'(a), 'r'(c)
-  );
-  return res;
-}
-#define MAC16_32_Q16(c, a, b) (MAC16_32_Q16_armv5e(c, a, b))
-    
-       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-    
-      // check if font is already embedded
-  fileName = font->getExtFontFile();
-  for (i = 0; i < fontFileNameLen; ++i) {
-    if (!fontFileNames[i]->cmp(fileName)) {
-      return psFileNames[i]->copy();
-    }
+  /// Retrieve the argument labels provided at the call site.
+  ArrayRef<Identifier> getArgumentLabels() const {
+    return { this->template getTrailingObjects<Identifier>(),
+             asDerived().getNumArguments() };
   }
     
-      // Get the next PostScript token.  Returns false at end-of-stream.
-  GBool getToken(char *buf, int size, int *length);
-    
-    class PopplerCache
+    /// <summary>
+/// Invoked when Navigation to a certain page fails
+/// </summary>
+/// <param name='sender'>The Frame which failed navigation</param>
+/// <param name='e'>Details about the navigation failure</param>
+void App::OnNavigationFailed(Platform::Object ^ sender, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs ^ e)
 {
-  public:
-    PopplerCache(int cacheSizeA);
-    ~PopplerCache();
-    
-    /* The item returned is owned by the cache */
-    PopplerCacheItem *lookup(const PopplerCacheKey &key);
-    
-    /* The key and item pointers ownership is taken by the cache */
-    void put(PopplerCacheKey *key, PopplerCacheItem *item);
-    
-    /* The max size of the cache */
-    int size();
-    
-    /* The number of items in the cache */
-    int numberOfItems();
-    
-    /* The n-th item in the cache */
-    PopplerCacheItem *item(int index);
-    
-    /* The n-th key in the cache */
-    PopplerCacheKey *key(int index);
-  
-  private:
-    PopplerCache(const PopplerCache &cache); // not allowed
-  
-    PopplerCacheKey **keys;
-    PopplerCacheItem **items;
-    int lastValidCacheIndex;
-    int cacheSize;
-};
-    
-    void PreScanOutputDev::fill(GfxState *state) {
-  check(state->getFillColorSpace(), state->getFillColor(),
-	state->getFillOpacity(), state->getBlendMode());
-}
-    
-    ProfileData::ProfileData() {
-	count = 0;
-	total = 0.0;
-	min = 0.0;
-	max = 0.0;
-}
-    
-      int width;                             // -1
-  int height;                            // -1
-  
-  // floating window position
-  MediaWindowRelativeTo relativeTo;      // windowRelativeToDocument (or to desktop)
-  double XPosition;                      // 0.5
-  double YPosition;                      // 0.5
-    
-    
-    {  return newsound;
+    throw ref new FailureException('Failed to load Page ' + e->SourcePageType.Name);
 }
 
     
-      virtual ~SplashGouraudPattern();
-    
-    PDFDoc *
-StdinPDFDocBuilder::buildPDFDoc(const GooString &uri, GooString *ownerPassword,
-                                    GooString *userPassword, void *guiDataA)
+    bool HorizontalNoOverflowStackPanel::ShouldPrioritizeLastItem()
 {
-  Object obj;
-    }
-    
-    void RemoveTransliterator::handleTransliterate(Replaceable& text, UTransPosition& index,
-                                               UBool /*isIncremental*/) const {
-    // Our caller (filteredTransliterate) has already narrowed us
-    // to an unfiltered run.  Delete it.
-    UnicodeString empty;
-    text.handleReplaceBetween(index.start, index.limit, empty);
-    int32_t len = index.limit - index.start;
-    index.contextLimit -= len;
-    index.limit -= len;
-}
-U_NAMESPACE_END
-    
-        case URX_ONECHAR:
-    case URX_ONECHAR_I:
-        if (val < 0x20) {
-            printf('%#x', val);
-        } else {
-            printf(''%s'', CStr(UnicodeString(val))());
-        }
-        break;
-    
-    ScientificNumberFormatter::ScientificNumberFormatter(
-        const ScientificNumberFormatter &other)
-        : UObject(other),
-          fPreExponent(other.fPreExponent),
-          fDecimalFormat(NULL),
-          fStyle(NULL),
-          fStaticSets(other.fStaticSets) {
-    fDecimalFormat = static_cast<DecimalFormat *>(
-            other.fDecimalFormat->clone());
-    fStyle = other.fStyle->clone();
+    return false;
 }
     
-    #if !UCONFIG_NO_FORMATTING
+    inline constexpr auto IDS_DIVBYZERO = IDS_ERRORS_FIRST;
+inline constexpr auto IDS_DOMAIN = IDS_ERRORS_FIRST + 1;
+inline constexpr auto IDS_UNDEFINED = IDS_ERRORS_FIRST + 2;
+inline constexpr auto IDS_POS_INFINITY = IDS_ERRORS_FIRST + 3;
+inline constexpr auto IDS_NEG_INFINITY = IDS_ERRORS_FIRST + 4;
+inline constexpr auto IDS_NOMEM = IDS_ERRORS_FIRST + 6;
+inline constexpr auto IDS_TOOMANY = IDS_ERRORS_FIRST + 7;
+inline constexpr auto IDS_OVERFLOW = IDS_ERRORS_FIRST + 8;
+inline constexpr auto IDS_NORESULT = IDS_ERRORS_FIRST + 9;
+inline constexpr auto IDS_INSUFFICIENT_DATA = IDS_ERRORS_FIRST + 10;
     
-    SharedBreakIterator::SharedBreakIterator(
-        BreakIterator *biToAdopt) : ptr(biToAdopt) { }
-    
-    U_NAMESPACE_END
-    
-    int32_t StandardPlural::indexFromString(const UnicodeString &keyword, UErrorCode &errorCode) {
-    if (U_FAILURE(errorCode)) { return OTHER; }
-    int32_t i = indexOrNegativeFromString(keyword);
-    if (i >= 0) {
-        return i;
-    } else {
-        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
-        return OTHER;
-    }
-}
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    /*
- * If Trace::hhbbc_time >= 1, print some stats about the program to a
- * temporary file.  If it's greater than or equal to 2, also dump it
- * to stdout.
- */
-void print_stats(const Index&, const php::Program&);
-    
-    void VMTOC::setTOCDataBlock(HPHP::DataBlock *db) {
-  if(m_tocvector == nullptr) {
-    m_tocvector = db;
-    HPHP::Address addr = m_tocvector->frontier();
-    forceAlignment(addr);
-  }
-  return;
-}
-    
-       void EmitSCForm(const uint8_t op,
-                   const uint16_t lev) {
-      SC_form_t sc_formater {{
-                              1,
-                              lev,
-                              op
-                            }};
-    }
-    
-    #ifndef HPHP_DATA_STREAM_WRAPPER_H
-#define HPHP_DATA_STREAM_WRAPPER_H
+    // Used to strip trailing zeros, and prevent combinatorial explosions
+bool stripzeroesnum(_Inout_ PNUMBER pnum, int32_t starting);
