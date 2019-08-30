34daec35123fa1c3ba6499551e2936bff8a156fe
SUNsung <weatherius@gmail.com>
2019-08-31 02:07:04 +0300

    Rewrite [37eEHXK1fE6VI37wWGC4gYiOs37i0eryjIsQAU37CJe.cpp] File in: 2019-08-31 02:07:04

--- cccccc/37eEHXK1fE6VI37wWGC4gYiOs37i0eryjIsQAU37CJe.cpp
+++ cccccc/37eEHXK1fE6VI37wWGC4gYiOs37i0eryjIsQAU37CJe.cpp
@@ -1,206 +1,87 @@
 
         
-            void acquire_superblock_for_write(
-            int expected_change_count,
-            write_durability_t durability,
-            write_token_t *token,
-            scoped_ptr_t<txn_t> *txn_out,
-            scoped_ptr_t<real_superblock_t> *sb_out,
-            signal_t *interruptor)
-            THROWS_ONLY(interrupted_exc_t);
+        #include <QIcon>
+#include <QPixmap>
+#include <QString>
     
-      // Returns true iff the test part passed.
-  bool passed() const { return type_ == kSuccess; }
+        const char *s = 'pippy';
+    BOOST_CHECK(arr.push_back(s));
     
-    // Next, declare that you will define a type-parameterized test case
-// (the _P suffix is for 'parameterized' or 'pattern', whichever you
-// prefer):
-TYPED_TEST_CASE_P(FooTest);
-    
-      template <typename T>
-  operator ParamGenerator<T>() const {
-    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
-        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
-        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
-        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
-        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
-        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
-        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
-        static_cast<T>(v21_)};
-    return ValuesIn(array);
+    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
+  size_t usize = user_key.size();
+  size_t needed = usize + 13;  // A conservative estimate
+  char* dst;
+  if (needed <= sizeof(space_)) {
+    dst = space_;
+  } else {
+    dst = new char[needed];
   }
-    
-      // Compares two C strings.  Returns true iff they have the same content.
-  //
-  // Unlike strcmp(), this function can handle NULL argument(s).  A
-  // NULL C string is considered different to any non-NULL C string,
-  // including the empty string.
-  static bool CStringEquals(const char* lhs, const char* rhs);
-    
-    template <GTEST_7_TYPENAMES_(T)>
-class GTEST_7_TUPLE_(T) {
- public:
-  template <int k> friend class gtest_internal::Get;
-    }
-    
-            try
-        {
-            andrat(&lhsRat, rhsRat, RATIONAL_BASE, RATIONAL_PRECISION);
-            destroyrat(rhsRat);
-        }
-        catch (uint32_t error)
-        {
-            destroyrat(lhsRat);
-            destroyrat(rhsRat);
-            throw(error);
-        }
-    
-    
-    {        pnumret->exp += expSign * expValue;
-    }
-    
-    typedef enum eNUMOBJ_FMT NUMOBJ_FMT;
-typedef enum eANGLE_TYPE ANGLE_TYPE;
-    
-                Windows::Globalization::NumberFormatting::DecimalFormatter ^ m_ratioFormatter;
-            std::wstring m_ratioFormat;
-            Windows::Foundation::DateTime m_cacheTimestamp;
-            std::wstring m_timestampFormat;
-    
-        vector<uint32_t> const& Number::Mantissa() const
-    {
-        return m_mantissa;
-    }
-    
-    vector<shared_ptr<HISTORYITEM>> const& CalculatorHistory::GetHistory()
-{
-    return m_historyItems;
+  start_ = dst;
+  dst = EncodeVarint32(dst, usize + 8);
+  kstart_ = dst;
+  memcpy(dst, user_key.data(), usize);
+  dst += usize;
+  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
+  dst += 8;
+  end_ = dst;
 }
     
-            CalcInput(wchar_t decSymbol)
-            : m_hasExponent(false)
-            , m_hasDecimal(false)
-            , m_decPtIndex(0)
-            , m_decSymbol(decSymbol)
-            , m_base()
-            , m_exponent()
-        {
-        }
+    static void TestKey(const std::string& key,
+                    uint64_t seq,
+                    ValueType vt) {
+  std::string encoded = IKey(key, seq, vt);
+    }
     
     
-    {    return (labelArray.arrSize() > 0) ? BSON('errorLabels' << labelArray.arr()) : BSONObj();
+    {		char      *lPr = (char *)malloc(MAX_PATH);
+		char      *pR = (char *)malloc(MAX_PATH);
+		size_t i;
+		wcstombs_s(&i, lPr, (size_t)MAX_PATH,
+			legacyUserProfilePath, (size_t)MAX_PATH);
+		wcstombs_s(&i, pR, (size_t)MAX_PATH,
+			userProfilePath, (size_t)MAX_PATH);
+		rename(lPr, pR);
+	}
+    
+    #define TEST(i, j) \
+  X(i, j, false, CC_O, CC_NO, CC_B, CC_NAE, CC_AE, CC_NB, CC_NC, CC_E, CC_Z, \
+    CC_NE, CC_NZ, CC_BE, CC_NA, CC_A, CC_NBE, CC_S, CC_NS, CC_P, CC_NP, CC_L, \
+    CC_NGE, CC_GE, CC_NL, CC_LE, CC_NG, CC_G, CC_NLE)
+#define AND(i, j, k) \
+  X(i, j, k, CC_O, CC_NO, CC_B, CC_NAE, CC_AE, CC_NB, CC_NC, CC_E, CC_Z, \
+    CC_NE, CC_NZ, CC_BE, CC_NA, CC_A, CC_NBE, CC_S, CC_NS, CC_P, CC_NP, CC_L, \
+    CC_NGE, CC_GE, CC_NL, CC_LE, CC_NG, CC_G, CC_NLE)
+#define CMP(i, j) \
+  X(i, j, false, CC_B, CC_NAE, CC_AE, CC_NB, CC_NC, CC_E, CC_Z, CC_NE, CC_NZ, \
+    CC_BE, CC_NA, CC_A, CC_NBE, CC_L, CC_NGE, CC_GE, CC_NL, CC_LE, CC_NG, \
+    CC_G, CC_NLE)
+#define ADD(i, j, k) \
+  X(i, j, k, CC_B, CC_NAE, CC_AE, CC_NB, CC_NC, CC_E, CC_Z, CC_NE, CC_NZ, \
+    CC_BE, CC_NA, CC_A, CC_NBE, CC_L, CC_NGE, CC_GE, CC_NL, CC_LE, CC_NG, \
+    CC_G, CC_NLE)
+#define SUB(i, j, k) \
+  X(i, j, k, CC_B, CC_NAE, CC_AE, CC_NB, CC_NC, CC_E, CC_Z, CC_NE, CC_NZ, \
+    CC_BE, CC_NA, CC_A, CC_NBE, CC_L, CC_NGE, CC_GE, CC_NL, CC_LE, CC_NG, \
+    CC_G, CC_NLE)
+#define INC(i, j) \
+  X(i, j, true, CC_E, CC_Z, CC_NE, CC_NZ, CC_L, CC_NGE, CC_GE, CC_NL, CC_LE, \
+    CC_NG, CC_G, CC_NLE)
+#define DEC(i, j) \
+  X(i, j, true, CC_E, CC_Z, CC_NE, CC_NZ, CC_L, CC_NGE, CC_GE, CC_NL, CC_LE, \
+    CC_NG, CC_G, CC_NLE)
+    
+    inline MemoryRef Reg64::operator[](Reg64 idx) const {
+  return *(*this + idx * 1);
 }
     
-        const HostAndPort& getHostAndPort() const override;
-    transport::ConnectSSLMode getSslMode() const override;
-    bool isHealthy() override;
-    AsyncDBClient* client();
-    Date_t now() override;
-    
-    #include 'mongo/base/status.h'
-#include 'mongo/util/functional.h'
-    
-        ASSERT_EQ(obj['towardsNegativeInfinity'].safeNumberLongForHash(),
-              std::numeric_limits<long long>::lowest());
-    ASSERT_EQ(obj['towardsNegativeInfinity'].safeNumberLong(),
-              std::numeric_limits<long long>::lowest());
-    
-        // Complete the command by appending the other options to the aggregate command.
-    if (auto collation = cmd.getCollation()) {
-        aggregationBuilder.append(kCollationField, collation.get());
-    }
-    
-        /**
-     * Save any state required to recover from changes to the underlying collection's data.
-     *
-     * While in the 'saved' state, it is only legal to call restoreState,
-     * detachFromOperationContext, or the destructor.
-     */
-    virtual void saveState() = 0;
-    
-        for (size_t i = 0; i < names.size(); i++) {
-        BSONObjBuilder bb(b.subobjStart(names[i]));
-    }
-    
-                    utext_extract(input, nextOutputStringStart, fActiveLimit, remainingChars, remaining16Length+1, &status);
-                if (dest[i]) {
-                    utext_replace(dest[i], 0, utext_nativeLength(dest[i]), remainingChars, remaining16Length, &status);
-                } else {
-                    UText remainingText = UTEXT_INITIALIZER;
-                    utext_openUChars(&remainingText, remainingChars, remaining16Length, &status);
-                    dest[i] = utext_clone(NULL, &remainingText, TRUE, FALSE, &status);
-                    utext_close(&remainingText);
-                }
-    
-      if (static_cast<int>(current_percent) >= limit_percentage_) {
-    if (FLAG_trace_stress_scavenge) {
-      heap_.isolate()->PrintWithTimestamp('[Scavenge] GC requested\n');
-    }
-    }
-    
-        template <typename Callback>
-    void Update(Callback callback) {
-      size_t new_index = 0;
-      for (size_t i = 0; i < index_; i++) {
-        if (callback(entries_[i], &entries_[new_index])) {
-          new_index++;
-        }
-      }
-      index_ = new_index;
-    }
-    
-    void InterpreterDispatchDescriptor::InitializePlatformSpecific(
-    CallInterfaceDescriptorData* data) {
-  Register registers[] = {
-      kInterpreterAccumulatorRegister, kInterpreterBytecodeOffsetRegister,
-      kInterpreterBytecodeArrayRegister, kInterpreterDispatchTableRegister};
-  data->InitializePlatformSpecific(arraysize(registers), registers);
+    Scanner::Scanner(std::istream &stream, int type,
+                 const char *fileName /* = '' */,
+                 bool sha1 /* = false */)
+    : m_filename(fileName), m_source(nullptr), m_len(0), m_pos(0),
+      m_state(Start), m_type(type), m_yyscanner(nullptr), m_token(nullptr),
+      m_loc(nullptr), m_lastToken(-1), m_isHHFile(0), m_lookaheadLtDepth(0) {
+  m_stream = &stream;
+  m_streamOwner = false;
+  if (sha1) computeSha1();
+  init();
 }
-    
-    void TurboAssembler::Cvttss2ui(Register dst, Operand src, XMMRegister tmp) {
-  Label done;
-  cvttss2si(dst, src);
-  test(dst, dst);
-  j(positive, &done);
-  Move(tmp, static_cast<float>(INT32_MIN));
-  addss(tmp, src);
-  cvttss2si(dst, tmp);
-  or_(dst, Immediate(0x80000000));
-  bind(&done);
-}
-    
-          TNode<HeapObject> handler = CAST(var_handler.value());
-      Label if_transitioning_element_store(this);
-      GotoIfNot(IsCode(handler), &if_transitioning_element_store);
-      TailCallStub(StoreWithVectorDescriptor{}, CAST(handler), CAST(p->context),
-                   p->receiver, p->name, p->value, p->slot, p->vector);
-    
-    namespace v8 {
-namespace internal {
-    }
-    }
-    
-    Config::Config(void) : m_config(YGConfigNew()) {}
-    
-      const YGNodeRef root_child0 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetPositionType(root_child0, YGPositionTypeAbsolute);
-  YGNodeStyleSetPosition(root_child0, YGEdgeBottom, 10);
-  YGNodeStyleSetWidth(root_child0, 60);
-  YGNodeStyleSetHeight(root_child0, 40);
-  YGNodeInsertChild(root, root_child0, 0);
-  YGNodeCalculateLayout(root, YGUndefined, YGUndefined, YGDirectionLTR);
-    
-      ASSERT_FLOAT_EQ(0, YGNodeLayoutGetLeft(root_child0_child0_child0));
-  ASSERT_FLOAT_EQ(0, YGNodeLayoutGetTop(root_child0_child0_child0));
-  ASSERT_FLOAT_EQ(1080, YGNodeLayoutGetWidth(root_child0_child0_child0));
-  ASSERT_FLOAT_EQ(144, YGNodeLayoutGetHeight(root_child0_child0_child0));
-    
-      ASSERT_EQ(0, YGNodeLayoutGetLeft(root_child0));
-  ASSERT_EQ(0, YGNodeLayoutGetTop(root_child0));
-  ASSERT_EQ(100, YGNodeLayoutGetWidth(root_child0));
-  ASSERT_EQ(100, YGNodeLayoutGetHeight(root_child0));
-    
-      ASSERT_FLOAT_EQ(0, YGNodeLayoutGetLeft(root_child0));
-  ASSERT_FLOAT_EQ(0, YGNodeLayoutGetTop(root_child0));
-  ASSERT_FLOAT_EQ(100, YGNodeLayoutGetWidth(root_child0));
-  ASSERT_FLOAT_EQ(100, YGNodeLayoutGetHeight(root_child0));
