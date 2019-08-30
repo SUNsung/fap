beb31f797e9873d1855063b89a02a164c13ffeef
SUNsung <weatherius@gmail.com>
2019-08-30 03:21:48 +0300

    Rewrite [22pDzrFb9r0E22hwWYMhbJCkw22bv1o2M2DmA.22NXs.py] File in: 2019-08-30 03:21:48

--- pyton/22pDzrFb9r0E22hwWYMhbJCkw22bv1o2M2DmA.22NXs.py
+++ pyton/22pDzrFb9r0E22hwWYMhbJCkw22bv1o2M2DmA.22NXs.py
@@ -1,273 +1,97 @@
 
         
-        import os
-import sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        
+class CallableChoiceIterator:
+    def __init__(self, choices_func):
+        self.choices_func = choices_func
     
-    import youtube_dl
+        def as_postgresql(self, compiler, connection, **extra_context):
+        # Cast FloatField to DecimalField as PostgreSQL doesn't support the
+        # following function signatures:
+        # - LOG(double, double)
+        # - MOD(double, double)
+        output_field = DecimalField(decimal_places=sys.float_info.dig, max_digits=1000)
+        clone = self.copy()
+        clone.set_source_expressions([
+            Cast(expression, output_field) if isinstance(expression.output_field, FloatField)
+            else expression for expression in self.get_source_expressions()
+        ])
+        return clone.as_sql(compiler, connection, **extra_context)
     
-    module_contents = [
-    module_template + '\n' + getsource(InfoExtractor.suitable) + '\n',
-    'class LazyLoadSearchExtractor(LazyLoadExtractor):\n    pass\n']
-    
-    ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-README_FILE = os.path.join(ROOT_DIR, 'README.md')
-    
-    from youtube_dl.utils import encodeArgument
-    
-        def test_allsubtitles(self):
-        self.DL.expect_warning('Automatic Captions not supported by this server')
-        self.DL.params['writesubtitles'] = True
-        self.DL.params['allsubtitles'] = True
-        subtitles = self.getSubtitles()
-        self.assertEqual(set(subtitles.keys()), set(['heb']))
-        self.assertEqual(md5(subtitles['heb']), 'e758c5d7cb982f6bef14f377ec7a3920')
-    
-    batch_size = 128
-num_classes = 10
-epochs = 12
-log_dir = './logs'
-    
-        for i in range(1, 6):
-        fpath = os.path.join(path, 'data_batch_' + str(i))
-        (x_train[(i - 1) * 10000: i * 10000, :, :, :],
-         y_train[(i - 1) * 10000: i * 10000]) = load_batch(fpath)
-    
-    history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-    
-    1 - Train a simple convnet on the MNIST dataset the first 5 digits [0..4].
-2 - Freeze convolutional layers and fine-tune dense layers
-   for the classification of digits [5..9].
-    
-    
-def test_deconv_length():
-    assert conv_utils.deconv_length(None, 1, 7, 'same', None) is None
-    assert conv_utils.deconv_length(224, 1, 7, 'same', None) == 224
-    assert conv_utils.deconv_length(224, 2, 7, 'same', None) == 448
-    assert conv_utils.deconv_length(32, 1, 5, 'valid', None) == 36
-    assert conv_utils.deconv_length(32, 2, 5, 'valid', None) == 67
-    assert conv_utils.deconv_length(32, 1, 5, 'full', None) == 28
-    assert conv_utils.deconv_length(32, 2, 5, 'full', None) == 59
-    assert conv_utils.deconv_length(224, 1, 7, 'same', 0) == 224
-    assert conv_utils.deconv_length(224, 2, 7, 'same', 0) == 447
-    assert conv_utils.deconv_length(224, 2, 7, 'same', 1) == 448
-    assert conv_utils.deconv_length(32, 1, 5, 'valid', 0) == 36
-    assert conv_utils.deconv_length(32, 2, 5, 'valid', 0) == 67
-    assert conv_utils.deconv_length(32, 2, 5, 'valid', 1) == 68
-    assert conv_utils.deconv_length(6, 1, 3, 'full', 0) == 4
-    assert conv_utils.deconv_length(6, 2, 3, 'full', 1) == 10
-    assert conv_utils.deconv_length(6, 2, 3, 'full', 2) == 11
-    
-        def handle(self):
-        '''Handle multiple requests if necessary.'''
-        self.close_connection = True
-    
-        # 'The specifier name is a ``dotted name'' that may resolve either to
-    # a module, a test case class, a TestSuite instance, a test method
-    # within a test case class, or a callable object which returns a
-    # TestCase or TestSuite instance.'
-    #
-    # What happens when the module is found, but the attribute isn't?
-    def test_loadTestsFromName__unknown_attr_name_on_package(self):
-        loader = unittest.TestLoader()
-    
-        def test_cookie_second_line_noncommented_first_line(self):
-        lines = (
-            b'print('\xc2\xa3')\n',
-            b'# vim: set fileencoding=iso8859-15 :\n',
-            b'print('\xe2\x82\xac')\n'
+        def test_filtered_aggregate_ref_annotation(self):
+        aggs = Author.objects.annotate(
+            double_age=F('age') * 2,
+        ).aggregate(
+            cnt=Count('pk', filter=Q(double_age__gt=100)),
         )
-        encoding, consumed_lines = detect_encoding(self.get_readline(lines))
-        self.assertEqual(encoding, 'utf-8')
-        expected = [b'print('\xc2\xa3')\n']
-        self.assertEqual(consumed_lines, expected)
+        self.assertEqual(aggs['cnt'], 2)
     
-    from . import abc as resources_abc
-from contextlib import contextmanager, suppress
-from importlib import import_module
-from importlib.abc import ResourceLoader
-from io import BytesIO, TextIOWrapper
-from pathlib import Path
-from types import ModuleType
-from typing import Iterable, Iterator, Optional, Set, Union   # noqa: F401
-from typing import cast
-from typing.io import BinaryIO, TextIO
-from zipimport import ZipImportError
+        def test_version_dev_tuple(self):
+        expect = ('1.2.3dev', 1, 2, 3)
+        ops = FakePostGISOperations(expect[0])
+        actual = ops.postgis_version_tuple()
+        self.assertEqual(expect, actual)
     
-    # Make a local copy of what we are going to send.
-with open('outgoing.msg', 'wb') as f:
-    f.write(bytes(msg))
+        # Copy any attributes that a decorator adds to the function it decorates.
+    for dec in decorators:
+        _update_method_wrapper(_wrapper, dec)
+    # Preserve any existing attributes of 'method', including the name.
+    update_wrapper(_wrapper, method)
+    return _wrapper
     
-        for filename in os.listdir(directory):
-        path = os.path.join(directory, filename)
-        if not os.path.isfile(path):
-            continue
-        # Guess the content type based on the file's extension.  Encoding
-        # will be ignored, although we should check for simple things like
-        # gzip'd or compressed files.
-        ctype, encoding = mimetypes.guess_type(path)
-        if ctype is None or encoding is not None:
-            # No guess could be made, or the file is encoded (compressed), so
-            # use a generic bag-of-bits type.
-            ctype = 'application/octet-stream'
-        maintype, subtype = ctype.split('/', 1)
-        with open(path, 'rb') as fp:
-            msg.add_attachment(fp.read(),
-                               maintype=maintype,
-                               subtype=subtype,
-                               filename=filename)
-    # Now send or store the message
-    if args.output:
-        with open(args.output, 'wb') as fp:
-            fp.write(msg.as_bytes(policy=SMTP))
-    else:
-        with smtplib.SMTP('localhost') as s:
-            s.send_message(msg)
+            # Making sure we get a valid pointer to the OGR Driver
+        if not driver:
+            raise GDALException('Could not initialize GDAL/OGR Driver on input: %s' % dr_input)
+        self.ptr = driver
     
-                if buffer.lstrip().upper().startswith('SELECT'):
-                print(cur.fetchall())
-        except sqlite3.Error as e:
-            print('An error occurred:', e.args[0])
-        buffer = ''
+            if size[0] > self.width or size[1] > self.height:
+            raise ValueError('Size is larger than raster.')
     
-    # Register the adapter
-sqlite3.register_adapter(Point, adapt_point)
-    
-    import sqlite3
-import os
-    
-    
-def get_github_url(app, view, path):
-    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
-    return (
-        github_fmt.format(app.config.edit_on_github_project, view,
-                          app.config.edit_on_github_branch,
-                          app.config.edit_on_github_src_path, path))
-    
-    
-def display_temp(hass: HomeAssistant, temperature: float, unit: str,
-                 precision: float) -> float:
-    '''Convert temperature into preferred units/precision for display.'''
-    temperature_unit = unit
-    ha_unit = hass.config.units.temperature_unit
-    
-    
-def mock_real_ip(app):
-    '''Inject middleware to mock real IP.
-    
-        def test_extended_properties(self):
-        print(u'John's relatives: {0}'.format(self.John.relatives))
-        self.assertDictEqual(
-            {'name': 'John', 'occupation': 'Coder', 'relatives': 'Many relatives.', 'call_count2': 0},
-            self.John.__dict__,
-        )
-    
-        def test_extended_property_values_cloning(self):
-        sample_object_1 = self.prototype.clone()
-        sample_object_1.some_value = 'test string'
-        sample_object_2 = self.prototype.clone()
-        self.assertRaises(AttributeError, lambda: sample_object_2.some_value)
-    
-        def test_sales_manager_shall_not_talk_through_proxy_with_delay(cls):
-        cls.ntp.busy = 'No'
-        start_time = time()
-        cls.ntp.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-This Sales Manager will not talk to you whether he/she is busy or not\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time * 10), expected_execution_time)
-    
-    
-class Provider:
-    def __init__(self):
-        self.msg_queue = []
-        self.subscribers = {}
-    
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-    
-    
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod(optionflags=doctest.ELLIPSIS)
-
-    
-        >>> class ClassRegistree(BaseRegisteredClass):
-    ...    def __init__(self, *args, **kwargs):
-    ...        pass
-    
-        def handle(self, request):
+        @property
+    def extent(self):
         '''
-        Handle request and stop.
-        If can't - call next handler in chain.
+        Return the extent as a 4-tuple (xmin, ymin, xmax, ymax).
+        '''
+        # Calculate boundary values based on scale and size
+        xval = self.origin.x + self.scale.x * self.width
+        yval = self.origin.y + self.scale.y * self.height
+        # Calculate min and max values
+        xmin = min(xval, self.origin.x)
+        xmax = max(xval, self.origin.x)
+        ymin = min(yval, self.origin.y)
+        ymax = max(yval, self.origin.y)
     
+        def contains(self, x):
+        if isinstance(x, list):
+            x = np.array(x)  # Promote list to array for contains check
+        return ((x==0) | (x==1)).all()
     
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
+            # Flag to payout 1.5 on a 'natural' blackjack win, like casino rules
+        # Ref: http://www.bicyclecards.com/how-to-play/blackjack/
+        self.natural = natural
+        # Start the first game
+        self.reset()
     
+            '''
+        nvec: vector of counts of each categorical variable
+        '''
+        assert (np.array(nvec) > 0).all(), 'nvec (counts) have to be positive'
+        self.nvec = np.asarray(nvec, dtype=np.int64)
     
-@patch( 'ycm.client.messages_request.PostVimMessage',
-        new_callable = ExtendedMock )
-def HandlePollResponse_MultipleMessagesAndDiagnostics_test( post_vim_message ):
-  diagnostics_handler = ExtendedMock()
-  messages = [
-    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER1' ] },
-    { 'message': 'On the first day of Christmas, my VimScript gave to me' },
-    { 'filepath': 'bar', 'diagnostics': [ 'PLACEHOLDER2' ] },
-    { 'message': 'A test file in a Command-T' },
-    { 'filepath': 'baz', 'diagnostics': [ 'PLACEHOLDER3' ] },
-    { 'message': 'On the second day of Christmas, my VimScript gave to me' },
-    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER4' ] },
-    { 'message': 'Two popup menus, and a test file in a Command-T' },
-  ]
-  assert_that( _HandlePollResponse( messages, diagnostics_handler ),
-               equal_to( True ) )
-  diagnostics_handler.UpdateWithNewDiagnosticsForFile.assert_has_exact_calls( [
-    call( 'foo', [ 'PLACEHOLDER1' ] ),
-    call( 'bar', [ 'PLACEHOLDER2' ] ),
-    call( 'baz', [ 'PLACEHOLDER3' ] ),
-    call( 'foo', [ 'PLACEHOLDER4' ] )
-  ] )
+        def _get_obs(self, pos=None):
+        if pos is None:
+            pos = self.read_head_position
+        if pos < 0:
+            return self.base
+        if isinstance(pos, np.ndarray):
+            pos = pos.item()
+        try:
+            return self.input_data[pos]
+        except IndexError:
+            return self.base
     
-      def shutdown( self, wait=True ):
-    with self._shutdown_lock:
-      self._shutdown = True
-      self._work_queue.put( None )
-    if wait:
-      for t in self._threads:
-        t.join()
-  shutdown.__doc__ = _base.Executor.shutdown.__doc__
-
-    
-            if return_when == FIRST_EXCEPTION:
-            waiter = _AllCompletedWaiter(pending_count, stop_on_exception=True)
-        elif return_when == ALL_COMPLETED:
-            waiter = _AllCompletedWaiter(pending_count, stop_on_exception=False)
+            logger.debug('Starting ffmpeg with '%s'', ' '.join(self.cmdline))
+        if hasattr(os,'setsid'): #setsid not present on Windows
+            self.proc = subprocess.Popen(self.cmdline, stdin=subprocess.PIPE, preexec_fn=os.setsid)
         else:
-            raise ValueError('Invalid return condition: %r' % return_when)
-    
-            # Shutdown is a two-step process.
-        self._shutdown_thread = False
-        self._shutdown_lock = threading.Lock()
-        self._queue_count = 0
-        self._pending_work_items = {}
+            self.proc = subprocess.Popen(self.cmdline, stdin=subprocess.PIPE)
