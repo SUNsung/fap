33286c8df969cb8eac02bf6234676e7e434afa35
SUNsung <weatherius@gmail.com>
2019-08-30 07:06:42 +0300

    Rewrite [12E2l4KyiCh6k125VvhPg23fc12HNGr1sNKLl612izw.py] File in: 2019-08-30 07:06:42

--- pyton/12E2l4KyiCh6k125VvhPg23fc12HNGr1sNKLl612izw.py
+++ pyton/12E2l4KyiCh6k125VvhPg23fc12HNGr1sNKLl612izw.py
@@ -1,140 +1,85 @@
 
         
-          Args:
-    length: Sequence length.
-    hidden_size: Size of the
-    min_timescale: Minimum scale that will be applied at each position
-    max_timescale: Maximum scale that will be applied at each position
-    
-            while not sess.should_stop():
-          tf.logging.info('Visualizing batch %d', batch + 1)
-          _process_batch(sess=sess,
-                         original_images=samples[common.ORIGINAL_IMAGE],
-                         semantic_predictions=predictions,
-                         image_names=samples[common.IMAGE_NAME],
-                         image_heights=samples[common.HEIGHT],
-                         image_widths=samples[common.WIDTH],
-                         image_id_offset=image_id_offset,
-                         save_dir=save_dir,
-                         raw_save_dir=raw_save_dir,
-                         train_id_to_eval_id=train_id_to_eval_id)
-          image_id_offset += FLAGS.vis_batch_size
-          batch += 1
-    
-        Returns:
-      rvlad: [K*D] float tensor.
-    '''
-    
-      Args:
-    subtoken_counts: defaultdict mapping subtokens to their counts
-    min_count: int count used to filter subtokens
-    
-      Raises:
-    ValueError: If AggregationConfig is malformed, or `mapping_path` is
-      missing.
-  '''
-  num_images = len(image_names)
+                if self.url and not self.vid:
+            html = get_html(self.url)
+            tvid = r1(r'#curid=(.+)_', self.url) or \
+                   r1(r'tvid=([^&]+)', self.url) or \
+                   r1(r'data-player-tvid='([^']+)'', html) or r1(r'tv(?:i|I)d=(.+?)\&', html) or r1(r'param\[\'tvid\'\]\s*=\s*'(.+?)'', html)
+            videoid = r1(r'#curid=.+_(.*)$', self.url) or \
+                      r1(r'vid=([^&]+)', self.url) or \
+                      r1(r'data-player-videoid='([^']+)'', html) or r1(r'vid=(.+?)\&', html) or r1(r'param\[\'vid\'\]\s*=\s*'(.+?)'', html)
+            self.vid = (tvid, videoid)
+            info_u = 'http://pcw-api.iqiyi.com/video/video/playervideoinfo?tvid=' + tvid
+            json_res = get_content(info_u)
+            self.title = json.loads(json_res)['data']['vn']
+        tvid, videoid = self.vid
+        info = getVMS(tvid, videoid)
+        assert info['code'] == 'A00000', 'can't play this video'
     
     
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.register('type', 'bool', lambda v: v.lower() == 'true')
-  parser.add_argument(
-      '--aggregation_config_path',
-      type=str,
-      default='/tmp/aggregation_config.pbtxt',
-      help='''
-      Path to AggregationConfig proto text file with configuration to be used
-      for extraction.
-      ''')
-  parser.add_argument(
-      '--dataset_file_path',
-      type=str,
-      default='/tmp/gnd_roxford5k.mat',
-      help='''
-      Dataset file for Revisited Oxford or Paris dataset, in .mat format.
-      ''')
-  parser.add_argument(
-      '--use_query_images',
-      type=lambda x: (str(x).lower() == 'true'),
-      default=False,
-      help='''
-      If True, processes the query images of the dataset. If False, processes
-      the database (ie, index) images.
-      ''')
-  parser.add_argument(
-      '--features_dir',
-      type=str,
-      default='/tmp/features',
-      help='''
-      Directory where image features are located, all in .delf format.
-      ''')
-  parser.add_argument(
-      '--index_mapping_path',
-      type=str,
-      default='',
-      help='''
-      Optional CSV file which maps each .delf file name to the index image ID
-      and detected box ID. If regional aggregation is performed, this should be
-      set. Otherwise, this is ignored.
-      Usually this file is obtained as an output from the
-      `extract_index_boxes_and_features.py` script.
-      ''')
-  parser.add_argument(
-      '--output_aggregation_dir',
-      type=str,
-      default='/tmp/aggregation',
-      help='''
-      Directory where aggregation output will be written to. Each image's
-      features will be written to a file with same name, and extension replaced
-      by one of
-      ['.vlad', '.asmk', '.asmk_star', '.rvlad', '.rasmk', '.rasmk_star'].
-      ''')
-  cmd_args, unparsed = parser.parse_known_args()
-  app.run(main=main, argv=[sys.argv[0]] + unparsed)
+def kugou_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    if url.lower().find('5sing') != -1:
+        # for 5sing.kugou.com
+        html = get_html(url)
+        ticket = r1(r''ticket':\s*'(.*)'', html)
+        j = loads(str(b64decode(ticket), encoding='utf-8'))
+        url = j['file']
+        title = j['songName']
+        songtype, ext, size = url_info(url)
+        print_info(site_info, title, songtype, size)
+        if not info_only:
+            download_urls([url], title, ext, size, output_dir, merge=merge)
+    elif url.lower().find('hash') != -1:
+        return kugou_download_by_hash(url, output_dir, merge, info_only)
+    else:
+        # for the www.kugou.com/
+        return kugou_download_playlist(url, output_dir=output_dir, merge=merge, info_only=info_only)
+        # raise NotImplementedError(url)       
+    
+    
+def letv_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    url = url_locations([url])[0]
+    if re.match(r'http://yuntv.letv.com/', url):
+        letvcloud_download(url, output_dir=output_dir, merge=merge, info_only=info_only)
+    elif 'sports.le.com' in url:
+        html = get_content(url)
+        vid = match1(url, r'video/(\d+)\.html')
+        title = match1(html, r'<h2 class='title'>([^<]+)</h2>')
+        letv_download_by_vid(vid, title=title, output_dir=output_dir, merge=merge, info_only=info_only, **kwargs)
+    else:
+        html = get_content(url)
+        vid = match1(url, r'http://www.letv.com/ptv/vplay/(\d+).html') or \
+              match1(url, r'http://www.le.com/ptv/vplay/(\d+).html') or \
+              match1(html, r'vid='(\d+)'')
+        title = match1(html, r'name='irTitle' content='(.*?)'')
+        letv_download_by_vid(vid, title=title, output_dir=output_dir, merge=merge, info_only=info_only, **kwargs)
+    
+            account_number = match1(html, r'data-account='(\d+)'')
+    
+    from xml.etree import cElementTree as ET
+from copy import copy
+from ..common import *
+#----------------------------------------------------------------------
+def ckplayer_get_info_by_xml(ckinfo):
+    '''str->dict
+    Information for CKPlayer API content.'''
+    e = ET.XML(ckinfo)
+    video_dict = {'title': '',
+                  #'duration': 0,
+                  'links': [],
+                  'size': 0,
+                  'flashvars': '',}
+    dictified = dictify(e)['ckplayer']
+    if 'info' in dictified:
+        if '_text' in dictified['info'][0]['title'][0]:  #title
+            video_dict['title'] = dictified['info'][0]['title'][0]['_text'].strip()
+    
+    site_info = 'FC2Video'
+download = fc2video_download
+download_playlist = playlist_not_supported('fc2video')
 
     
-            # Crop query image according to bounding box.
-        bbox = [int(round(b)) for b in ground_truth[i]['bbx']]
-        im = np.array(_PilLoader(input_image_filename).crop(bbox))
+    from ..common import *
     
-    import tensorflow as tf
     
-    # Minimum dimensions below which DELF features are not extracted (empty
-# features are returned). This applies after any resizing is performed.
-_MIN_HEIGHT = 10
-_MIN_WIDTH = 10
-    
-      # Read solution.
-  print('Reading solution...')
-  public_solution, private_solution, ignored_ids = dataset_file_io.ReadSolution(
-      cmd_args.solution_path, dataset_file_io.RECOGNITION_TASK_ID)
-  print('done!')
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-        face_found = False
-    is_obama = False
-    
-    def get_download_fn(core, url, msgId):
-    def download_fn(downloadDir=None):
-        params = {
-            'msgid': msgId,
-            'skey': core.loginInfo['skey'],}
-        headers = { 'User-Agent' : config.USER_AGENT }
-        r = core.s.get(url, params=params, stream=True, headers = headers)
-        tempStorage = io.BytesIO()
-        for block in r.iter_content(1024):
-            tempStorage.write(block)
-        if downloadDir is None:
-            return tempStorage.getvalue()
-        with open(downloadDir, 'wb') as f:
-            f.write(tempStorage.getvalue())
-        tempStorage.seek(0)
-        return ReturnValue({'BaseResponse': {
-            'ErrMsg': 'Successfully downloaded',
-            'Ret': 0, },
-            'PostFix': utils.get_image_postfix(tempStorage.read(20)), })
-    return download_fn
+from .pandas_vb_common import setup  # noqa: F401
