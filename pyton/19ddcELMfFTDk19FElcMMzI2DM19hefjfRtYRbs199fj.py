13107cd51445dbb85679691f459cef73ebb63f3a
SUNsung <weatherius@gmail.com>
2019-08-30 11:43:53 +0300

    Rewrite [19ddcELMfFTDk19FElcMMzI2DM19hefjfRtYRbs199fj.py] File in: 2019-08-30 11:43:53

--- pyton/19ddcELMfFTDk19FElcMMzI2DM19hefjfRtYRbs199fj.py
+++ pyton/19ddcELMfFTDk19FElcMMzI2DM19hefjfRtYRbs199fj.py
@@ -1,93 +1,150 @@
 
         
-        
-class CallState(Enum):
+            def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
     
-        def reducer(self, key, values):
-        '''Sum values for each key.
+        def syntax(self):
+        return '<spider>'
     
-        def extract_url(self, line):
-        '''Extract the generated url from the log line.'''
-        pass
+        def _genspider(self, module, name, domain, template_name, template_file):
+        '''Generate the spider module, based on the given template'''
+        tvars = {
+            'project_name': self.settings.get('BOT_NAME'),
+            'ProjectName': string_camelcase(self.settings.get('BOT_NAME')),
+            'module': module,
+            'name': name,
+            'domain': domain,
+            'classname': '%sSpider' % ''.join(s.capitalize() \
+                for s in module.split('_'))
+        }
+        if self.settings.get('NEWSPIDER_MODULE'):
+            spiders_module = import_module(self.settings['NEWSPIDER_MODULE'])
+            spiders_dir = abspath(dirname(spiders_module.__file__))
+        else:
+            spiders_module = None
+            spiders_dir = '.'
+        spider_file = '%s.py' % join(spiders_dir, module)
+        shutil.copyfile(template_file, spider_file)
+        render_templatefile(spider_file, **tvars)
+        print('Created spider %r using template %r ' % (name, \
+            template_name), end=('' if spiders_module else '\n'))
+        if spiders_module:
+            print('in module:\n  %s.%s' % (spiders_module.__name__, module))
     
-        # Test single image
-    x = np.random.uniform(0, 255, (10, 10, 3))
-    xint = x.astype('int32')
-    assert utils.preprocess_input(x).shape == x.shape
-    assert utils.preprocess_input(xint).shape == xint.shape
-    
-    data_dim = 5
-num_classes = 2
-batch_size = 10
-    
-        s = score_euclidean(data[0], data[1])
-    print(s)
-    
-        Args:
-        x(tf.Tensor):
-        n_unit_ls(list of int):
-        act_fn:
-        name(str):
-    '''
-    # n_layer = len(n_unit_list)
-    name = name or 'dense'
-    for i, n_unit in enumerate(n_unit_ls):
-        x = dense(x, n_unit, act_fn=act_fn, name='{}-{}'.format(name, i))
-    
-        这里实际上没有用到 J 和 d 这个参数，保留是为了与 `attention_flow()` 的参数兼容
-    
-        References:
-        K.repeat()
-        tf.tile()
-    '''
-    assert x.get_shape().ndims == 2
-    x = tf.expand_dims(x, axis=1)  # -> [batch_size, 1, n_input]
-    return tf.tile(x, [1, n, 1])  # -> [batch_size, n, n_input]
-    
-        mat = np.empty(euler.shape[:-1] + (3, 3), dtype=np.float64)
-    mat[..., 2, 2] = cj * ck
-    mat[..., 2, 1] = sj * sc - cs
-    mat[..., 2, 0] = sj * cc + ss
-    mat[..., 1, 2] = cj * sk
-    mat[..., 1, 1] = sj * ss + cc
-    mat[..., 1, 0] = sj * cs - sc
-    mat[..., 0, 2] = -sj
-    mat[..., 0, 1] = cj * si
-    mat[..., 0, 0] = cj * ci
-    return mat
-    
-    
-def mocap_set_action(sim, action):
-    '''The action controls the robot using mocaps. Specifically, bodies
-    on the robot (for example the gripper wrist) is controlled with
-    mocap bodies. In this case the action is the desired difference
-    in position and orientation (quaternion), in world coordinates,
-    of the of the target body. The mocap is positioned relative to
-    the target body according to the delta, and the MuJoCo equality
-    constraint optimizer tries to center the welded body on the mocap.
-    '''
-    if sim.model.nmocap > 0:
-        action, _ = np.split(action, (sim.model.nmocap * 7, ))
-        action = action.reshape(sim.model.nmocap, 7)
-    
-        def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
-    
-    def update_rollout_dict(spec, rollout_dict):
-    '''
-    Takes as input the environment spec for which the rollout is to be generated,
-    and the existing dictionary of rollouts. Returns True iff the dictionary was
-    modified.
-    '''
-    # Skip platform-dependent
-    if should_skip_env_spec_for_tests(spec):
-        logger.info('Skipping tests for {}'.format(spec.id))
+        # Stripping scripts and comments is slow (about 20x slower than
+    # just checking if a string is in text); this is a quick fail-fast
+    # path that should work for most pages.
+    if 'fragment' not in text:
+        return False
+    if 'content' not in text:
         return False
     
-        def step(self, dt):
-        for w in self.wheels:
-            # Steer each wheel
-            dir = np.sign(w.steer - w.joint.angle)
-            val = abs(w.steer - w.joint.angle)
-            w.joint.motorSpeed = dir*min(50.0*val, 3.0)
+        def process_request(self, request, spider):
+        if self._timeout:
+            request.meta.setdefault('download_timeout', self._timeout)
+
+    
+        def report_not_exist(self, appid, ip):
+        self.logger.debug('report_not_exist:%s %s', appid, ip)
+        th = threading.Thread(target=self.process_appid_not_exist, args=(appid, ip))
+        th.start()
+    
+    
+    def insertBefore(self, *args):
+        if len(args) == 2:
+            programName = self.DEFAULT_PROGRAM_NAME
+            index = args[0]
+            text = args[1]
+            
+        elif len(args) == 3:
+            programName = args[0]
+            index = args[1]
+            text = args[2]
+    
+        API_ENDPOINT = 'http://p.bokecc.com/'
+    
+        if '_text' in dictified['flashvars'][0]:
+        video_dict['flashvars'] = dictified['flashvars'][0]['_text'].strip()
+    
+    
+@contextlib.contextmanager
+def capture_ops():
+    '''Decorator to capture ops created in the block.
+    with capture_ops() as ops:
+        # create some ops
+    print(ops) # => prints ops created.
+    '''
+    
+    
+class facehull(Mask):  # pylint: disable=invalid-name
+    ''' Basic face hull mask '''
+    def build_mask(self):
+        mask = np.zeros(self.face.shape[0:2] + (1, ), dtype=np.float32)
+        hull = cv2.convexHull(  # pylint: disable=no-member
+            np.array(self.landmarks).reshape((-1, 2)))
+        cv2.fillConvexPoly(mask, hull, 255.0, lineType=cv2.LINE_AA)  # pylint: disable=no-member
+        return mask
+
+    
+        def load_module(self, filename, module_path, plugin_type):
+        ''' Load the defaults module and add defaults '''
+        logger.debug('Adding defaults: (filename: %s, module_path: %s, plugin_type: %s',
+                     filename, module_path, plugin_type)
+        module = os.path.splitext(filename)[0]
+        section = '.'.join((plugin_type, module.replace('_defaults', '')))
+        logger.debug('Importing defaults module: %s.%s', module_path, module)
+        mod = import_module('{}.{}'.format(module_path, module))
+        self.add_section(title=section, info=mod._HELPTEXT)  # pylint:disable=protected-access
+        for key, val in mod._DEFAULTS.items():  # pylint:disable=protected-access
+            self.add_item(section=section, title=key, **val)
+        logger.debug('Added defaults: %s', section)
+
+    
+            For actions that occur identically for each frame (e.g. blend_box), constants can
+        be placed into self.func_constants to be compiled at launch, then referenced for
+        each face. '''
+    def __init__(self, mask_type, output_size, predicted_available=False, **kwargs):
+        super().__init__(mask_type, output_size, predicted_available, **kwargs)
+        self.mask = self.get_mask() if not self.skip else None
+    
+    import cv2
+from ._base import Output, logger
+    
+        The following variables should be defined:
+        _HELPTEXT: A string describing what this plugin does
+        _DEFAULTS: A dictionary containing the options, defaults and meta information. The
+                   dictionary should be defined as:
+                       {<option_name>: {<metadata>}}
+    
+    import cv2
+import numpy as np
+from ._base import Adjustment
+    
+        def forwards(self, orm):
+        # Removing unique constraint on 'EventTag', fields ['event_id', 'key', 'value']
+        db.delete_unique(u'tagstore_eventtag', ['event_id', 'key_id', 'value_id'])
+    
+            # Changing field 'GroupTagValue.group_id'
+        db.alter_column(u'tagstore_grouptagvalue', 'group_id', self.gf(
+            'sentry.db.models.fields.bounded.BoundedBigIntegerField')())
+    
+    
+class Migration(SchemaMigration):
+    
+        def add(self, ident, func, args=None, kwargs=None):
+        if args is None:
+            args = ()
+        if kwargs is None:
+            kwargs = {}
+        task = (ident, func, args, kwargs)
+        self.tasks.append(ident)
+        self.queue.put_nowait(task)
+    
+    from django.conf import settings
+from social_auth.utils import custom_user_frozen_models
