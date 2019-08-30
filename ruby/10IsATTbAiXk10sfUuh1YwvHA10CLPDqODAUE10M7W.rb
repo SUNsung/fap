68773b8f7823863a638eb26028393efea8cb8c21
SUNsung <weatherius@gmail.com>
2019-08-30 21:47:18 +0300

    Rewrite [10IsATTbAiXk10sfUuh1YwvHA10CLPDqODAUE10M7W.rb] File in: 2019-08-30 21:47:18

--- ruby/10IsATTbAiXk10sfUuh1YwvHA10CLPDqODAUE10M7W.rb
+++ ruby/10IsATTbAiXk10sfUuh1YwvHA10CLPDqODAUE10M7W.rb
@@ -1,191 +1,209 @@
 
         
-          task :rollback_release_path do
-    on release_roles(:all) do
-      releases = capture(:ls, '-xt', releases_path).split
-      if releases.count < 2
-        error t(:cannot_rollback)
-        exit 1
+          has_many :citations, foreign_key: 'book1_id'
+  has_many :references, -> { distinct }, through: :citations, source: :reference_of
+    
+          def self.services_mapping
+        {
+            access_wifi: 'access_wifi',
+            app_group: 'app_group',
+            apple_pay: 'apple_pay',
+            associated_domains: 'associated_domains',
+            auto_fill_credential: 'auto_fill_credential',
+            data_protection: 'data_protection',
+            game_center: 'game_center',
+            health_kit: 'healthkit',
+            home_kit: 'homekit',
+            hotspot: 'hotspot',
+            icloud: 'icloud',
+            in_app_purchase: 'in_app_purchase',
+            inter_app_audio: 'inter_app_audio',
+            multipath: 'multipath',
+            network_extension: 'network_extension',
+            nfc_tag_reading: 'nfc_tag_reading',
+            personal_vpn: 'personal_vpn',
+            passbook: 'passbook',
+            push_notification: 'push_notification',
+            siri_kit: 'sirikit',
+            vpn_configuration: 'vpn_conf',
+            wallet: 'wallet',
+            wireless_accessory: 'wireless_conf'
+        }
       end
     
-    Gem::Specification.new do |gem|
-  gem.name          = 'capistrano'
-  gem.version       = Capistrano::VERSION
-  gem.authors       = ['Tom Clements', 'Lee Hambley']
-  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
-  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
-  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
-  gem.homepage      = 'http://capistranorb.com/'
-    
-    Given(/^the configuration is in a custom location$/) do
-  TestApp.move_configuration_to_custom_location('app')
-end
-    
-      def safely_remove_file(_path)
-    run_vagrant_command('rm #{test_file}')
-  rescue
-    VagrantHelpers::VagrantSSHCommandError
-  end
-end
-    
-      def run_vagrant_command(command)
-    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
-    return [stdout, stderr] if status.success?
-    raise VagrantSSHCommandError, status
-  end
-end
-    
-        def add_filter(filter=nil, &block)
-      if block
-        raise ArgumentError, 'Both a block and an object were given' if filter
-    
-          def netssh_options
-        @netssh_options ||= super.merge(fetch(:ssh_options) || {})
-      end
-    
-          def fetch_primary(role)
-        hosts = roles_for([role])
-        hosts.find(&:primary) || hosts.first
-      end
-    
-          it 'Does not render below article variant when no variants published' do
-        html_variant = create(:html_variant, published: false, approved: true, group: 'article_show_below_article_cta')
-        get article.path + '?variant_version=0'
-        expect(response.body).not_to include html_variant.html
-      end
-    
-      def satellite_tweet!
-    @article.tags.find_each do |tag|
-      BufferUpdate.buff!(@article.id, twitter_buffer_text, tag.buffer_profile_id_code, 'twitter', tag.id) if tag.buffer_profile_id_code.present?
-    end
-    @article.update(last_buffered: Time.current)
-  end
-    
-      def update
-    prevent_request_if_requested_twice
-    @user = current_user
-    @user.assign_attributes(user_params)
-    @errors = []
-    confirm_presence
-    respond_to do |format|
-      if @invalid_form
-        render :edit
-        return
-      end
-    
-      it 'can display a single scheduled job' do
-    params = add_scheduled
-    get '/scheduled/0-shouldntexist'
-    assert_equal 302, last_response.status
-    get '/scheduled/#{job_params(*params)}'
-    assert_equal 200, last_response.status
-    assert_match(/HardWorker/, last_response.body)
-  end
-    
-          q.clear
-      assert SetWorker.perform_async(1)
-      assert_equal 0, q.size
-    
-        assert_nil dead_set.find_job('000103')
-    assert dead_set.find_job('000102')
-    assert dead_set.find_job('000101')
-  end
-    
-    end
-
-    
-      it 'logs large payloads' do
-    output = capture_logging(Logger::WARN) do
-      SomeClass.delay.doit('a' * 8192)
-    end
-    assert_match(/#{SomeClass}.doit job argument is/, output)
-  end
-    
-          msg['locale'] = 'jp'
-      I18n.locale = I18n.default_locale
-      assert_equal :en, I18n.locale
-      mw = Sidekiq::Middleware::I18n::Server.new
-      mw.call(nil, msg, nil) do
-        assert_equal :jp, I18n.locale
-      end
-      assert_equal :en, I18n.locale
-    end
-    
-          e = assert_raises ArgumentError do
-        Sidekiq.on(:startp)
-      end
-      assert_match(/Invalid event name/, e.message)
-      e = assert_raises ArgumentError do
-        Sidekiq.on('startup')
-      end
-      assert_match(/Symbols only/, e.message)
-      Sidekiq.on(:startup) do
-        1 + 1
-      end
-    
-        if input_type == 'pleaserun'
-      # Special case for pleaserun that all parameters are considered the 'command'
-      # to run through pleaserun.
-      input.input(args)
-    else
-      # Each remaining command line parameter is used as an 'input' argument.
-      # For directories, this means paths. For things like gem and python, this
-      # means package name or paths to the packages (rails, foo-1.0.gem, django,
-      # bar/setup.py, etc)
-      args.each do |arg|
-        input.input(arg)
+      describe 'keys api' do
+    let(:api_root) { 'https://developer.apple.com/services-account/QH65B2/account/auth/key' }
+    before do
+      MockAPI::DeveloperPortalServer.post('/services-account/QH65B2/account/auth/key/:action') do
+        {
+          keys: []
+        }
       end
     end
     
-      # Clean up any temporary storage used by this class.
-  def cleanup
-    cleanup_staging
-    cleanup_build
-  end # def cleanup
+          def pipe(device_type, language, locale)
+        log_path = xcodebuild_log_path(device_type: device_type, language: language, locale: locale)
+        return ['| tee #{log_path.shellescape} | xcpretty #{Snapshot.config[:xcpretty_args]}']
+      end
     
-      option '--origin', 'ABI',
-         'Sets the FreeBSD 'origin' pkg field',
-         :default => 'fpm/<name>'
+          def self.output
+        [
+          ['MATCH_PROVISIONING_PROFILE_MAPPING', 'The match provisioning profile mapping'],
+          ['SIGH_PROFILE_TYPE', 'The profile type, can be appstore, adhoc, development, enterprise']
+        ]
+      end
     
-      public(:input)
-end # class FPM::Package::PleaseRun
-
-    
-        # easy_install will put stuff in @tmpdir/packagename/, so find that:
-    #  @tmpdir/somepackage/setup.py
-    dirs = ::Dir.glob(File.join(target, '*'))
-    if dirs.length != 1
-      raise 'Unexpected directory layout after easy_install. Maybe file a bug? The directory is #{build_path}'
-    end
-    return dirs.first
-  end # def download
-    
-      def migration_name
-    'add_attachment_#{attachment_names.join('_')}_to_#{name.underscore.pluralize}'
-  end
-    
-        def add_active_record_callbacks
-      name = @name
-      @klass.send(:after_save) { send(name).send(:save) }
-      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
-      if @klass.respond_to?(:after_commit)
-        @klass.send(:after_commit, on: :destroy) do
-          send(name).send(:flush_deletes)
+            it 'raises an error if `apple_id` is set to bundle identifier' do
+          expect do
+            options = {
+              username: 'username@example.com',
+              apple_id: 'com.bundle.identifier'
+            }
+            pilot_config = FastlaneCore::Configuration.create(Pilot::Options.available_options, options)
+          end.to raise_error('`apple_id` value is incorrect. The correct value should be taken from Apple ID property in the App Information section in App Store Connect.')
         end
-      else
-        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
+    
+        let(:custom_tester_group) do
+      Spaceship::ConnectAPI::BetaGroup.new('1', {
+        name: 'Test Group'
+      })
+    end
+    
+            # Build up the actual command to execute
+        command = [
+          'rsync',
+          args,
+          '-e', rsh.flatten.join(' '),
+          excludes.map { |e| ['--exclude', e] },
+          hostpath,
+          '#{username}@#{host}:#{guestpath}',
+        ].flatten
+    
+      before do
+    allow(machine).to receive(:communicate).and_return(communicator)
+  end
+    
+          if config_class
+        p.config(name, :provider) { config_class }
       end
     end
     
-        desc 'version', 'Show Bourbon version'
-    def version
-      say 'Bourbon #{Bourbon::VERSION}'
+      describe '#initialize' do
+    let(:gemrc_location) { 'C:\\My\\Config\\File' }
+    
+    
+  subject{ described_class.new(machine) }
+    
+        def prepare_output
+      @output_encoding = nil
+      return unless @output.is_a?(StringIO)
+    
+        def test_struct_aset
+      s = Struct.new(:foo).new
+      name = noninterned_name
+      assert_raise(NameError) {s[name] = true}
+      assert_not_interned(name)
     end
     
-          expect('.border-width-implied-left').to have_rule(rule)
+      it 'calls the given block for each byte in the stream, passing the byte as an argument' do
+    gz = Zlib::GzipReader.new @io
+    
+      it 'increments position' do
+    gz = Zlib::GzipReader.new @io
+    (0..@data.size).each do |i|
+      gz.pos.should == i
+      gz.getc
     end
   end
     
-      context 'called with four sizes' do
-    it 'applies different widths to all sides' do
-      rule = 'padding: 7px 8px 9px 10px'
+        quarantine! do # https://bugs.ruby-lang.org/issues/13675
+      describe 'with nil' do
+        it 'does not insert anything into the stream' do
+          @gz.ungetbyte nil
+          @gz.read.should == 'abcde'
+        end
+    
+      it 'inflates chunked data' do
+    @chunks.map { |chunk| chunk.length }.should == [16384, 16384, 16384, 16384, 16384, 16384, 1696]
+  end
+    
+    module LogStash
+  module Api
+    module Modules
+      class Node < ::LogStash::Api::Modules::Base
+        def node
+          factory.build(:node)
+        end
+    
+            get '/pipelines/:id?' do
+          payload = pipeline_payload(params['id'])
+          halt(404) if payload.empty?
+          respond_with(:pipelines => payload)
+        end
+    
+          {
+          :type => queue_type,
+          :events_count => events,
+          :queue_size_in_bytes => queue_size_in_bytes,
+          :max_queue_size_in_bytes => max_queue_size_in_bytes,
+      }
+    end
+  end
+end; end;
+
+    
+          @event.remove('@timestamp')
+      @event.remove('@version')
+    
+          case op
+      when :and
+        return jdsl.eAnd(source_meta, left, right);
+      when :nand
+        return jdsl.eNand(source_meta, left, right);
+      when :or
+        return jdsl.eOr(source_meta, left, right);
+      when :xor
+        return jdsl.eXor(source_meta, left, right);
+      else
+        raise 'Unknown op #{jop}'
+      end
+    end
+    
+    task :spec    => 'spec:all'
+task :default => :spec
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
+    end
+    
+      end
+    
+    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
+
+    
+    Liquid::Template.register_tag('video', Jekyll::VideoTag)
