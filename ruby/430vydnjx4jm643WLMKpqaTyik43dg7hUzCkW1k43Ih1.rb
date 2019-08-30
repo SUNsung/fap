6f9cac39039f02d0fd6d3498f969c3dbe616545b
SUNsung <weatherius@gmail.com>
2019-08-31 01:31:58 +0300

    Rewrite [430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb] File in: 2019-08-31 01:31:58

--- ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
+++ ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
@@ -1,231 +1,173 @@
 
         
-          context 'generating limited posts' do
-    setup do
-      clear_dest
-      @site = fixture_site('limit_posts' => 5)
-      @site.process
-      @index = File.read(dest_dir('index.html'))
-    end
-    
-        context 'when a custom highlighter is chosen' do
-      should 'use the chosen highlighter if it's available' do
-        override = {
-          'highlighter' => nil,
-          'markdown'    => 'kramdown',
-          'kramdown'    => {
-            'syntax_highlighter' => :coderay,
-          },
-        }
-    
-    def converted_history(markdown)
-  remove_head_from_history(
-    custom_release_header_anchors(
-      liquid_escape(
-        linkify(
-          normalize_bullets(markdown)
-        )
-      )
-    )
-  )
-end
-    
-    WITH_LIQUID = <<-LIQUID.freeze
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
-pharetra tempus. {{ author }} et metus fermentum, eu cursus lorem
-mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
-    
-    # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
-    
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-      def create_template!(name_with_category)
-    full_path = File.join(base_dir, name_with_category)
-    FileUtils.mkdir_p(File.dirname(full_path))
-    FileUtils.touch(full_path)
+          def serialize_options(resource)
+    methods = resource_class.authentication_keys.dup
+    methods = methods.keys if methods.is_a?(Hash)
+    methods << :password if resource.respond_to?(:password)
+    { methods: methods, only: [:password] }
   end
     
-              # We need to build up mapping of directories that are enclosed
-          # within each other because the exports file has to have subdirectories
-          # of an exported directory on the same line. e.g.:
-          #
-          #   '/foo' '/foo/bar' ...
-          #   '/bar'
-          #
-          # We build up this mapping within the following hash.
-          logger.debug('Compiling map of sub-directories for NFS exports...')
-          dirmap = {}
-          folders.sort_by { |_, opts| opts[:hostpath] }.each do |_, opts|
-            hostpath = opts[:hostpath].dup
-            hostpath.gsub!(''', '\'')
+      # Gets the actual resource stored in the instance variable
+  def resource
+    instance_variable_get(:'@#{resource_name}')
+  end
     
-        it 'returns false if not installed' do
-      expect(machine.communicate).to receive(:test).
-        with(command, sudo: true).and_return(false)
-      expect(subject.chef_installed(machine, 'chef_solo', version)).to be_falsey
+        def password_change(record, opts={})
+      devise_mail(record, :password_change, opts)
     end
   end
 end
 
     
-        # Returns a {Machine} for the given name and provider that
-    # is represented by this Vagrantfile.
-    #
-    # @param [Symbol] name Name of the machine.
-    # @param [Symbol] provider The provider the machine should
-    #   be backed by (required for provider overrides).
-    # @param [BoxCollection] boxes BoxCollection to look up the
-    #   box Vagrantfile.
-    # @param [Pathname] data_path Path where local machine data
-    #   can be stored.
-    # @param [Environment] env The environment running this machine
-    # @return [Machine]
-    def machine(name, provider, boxes, data_path, env)
-      # Load the configuration for the machine
-      results = machine_config(name, provider, boxes, data_path)
-      box             = results[:box]
-      config          = results[:config]
-      config_errors   = results[:config_errors]
-      config_warnings = results[:config_warnings]
-      provider_cls    = results[:provider_cls]
-      provider_options = results[:provider_options]
+    class TestApp < Rails::Application
+  config.root = File.dirname(__FILE__)
+  config.session_store :cookie_store, key: 'cookie_store_key'
+  secrets.secret_token    = 'secret_token'
+  secrets.secret_key_base = 'secret_key_base'
+  config.eager_load = false
     
-        context 'with a volumes key in use for mounting' do
-      let(:compose_config) { {'volumes'=>{'my_volume_key'=>'data'}} }
+        unless env['devise.skip_trackable']
+      warden.session(scope)['last_request_at'] = Time.now.utc.to_i
+    end
+  end
+end
+
     
-              if !boxes[name]
-            raise Vagrant::Errors::BoxNotFound, name: name.to_s
+          # Resets reset password token and send reset password instructions by email.
+      # Returns the token sent in the e-mail.
+      def send_reset_password_instructions
+        token = set_reset_password_token
+        send_reset_password_instructions_notification(token)
+    
+          private
+    
+          # Determines what how a Trigger should behave if it runs into an error.
+      # Defaults to :halt, otherwise can only be set to :continue.
+      #
+      # @return [Symbol]
+      attr_accessor :on_error
+    
+    # Unset all host plugins so that we aren't executing subprocess things
+# to detect a host for every test.
+Vagrant.plugin('2').manager.registered.dup.each do |plugin|
+  if plugin.components.hosts.to_hash.length > 0
+    Vagrant.plugin('2').manager.unregister(plugin)
+  end
+end
+    
+        it 'locks the environment' do
+      another = env.create_vagrant_env
+      raised  = false
+    
+              if providers.include? provider.downcase
+            raise Errors::ProviderNotFoundSuggestion,
+              machine: name, provider: provider,
+              suggestion: provider.downcase, providers: providers_str
           end
     
-    describe Formula do
-  describe '#uses_from_macos' do
-    before do
-      allow(OS).to receive(:mac?).and_return(false)
+            vf_path           = @machine.provider_config.vagrant_vagrantfile
+        host_machine_name = @machine.provider_config.vagrant_machine || :default
+        if !vf_path
+          # We don't have a Vagrantfile path set, so we're going to use
+          # the default but we need to copy it into the data dir so that
+          # we don't write into our installation dir (we can't).
+          default_path = File.expand_path('../hostmachine/Vagrantfile', __FILE__)
+          vf_path      = @machine.env.data_dir.join('docker-host', 'Vagrantfile')
+          begin
+            @machine.env.lock('docker-provider-hostvm') do
+              vf_path.dirname.mkpath
+              FileUtils.cp(default_path, vf_path)
+            end
+          rescue Vagrant::Errors::EnvironmentLockedError
+            # Lock contention, just retry
+            retry
+          end
+    
+                @ui.detail(I18n.t('vagrant.trigger.run.script', path: config.path))
+          end
+    
+      describe '#run' do
+    let(:machine) { new_machine(provider_options) }
+    let(:machine2) { new_machine(provider_options) }
+    
+        it 'continues on error' do
+      allow(Vagrant::Util::Subprocess).to receive(:execute).
+        and_raise('Fail!')
+      allow(env).to receive(:root_path).and_return('/vagrant/home')
+      allow(FileUtils).to receive(:chmod).and_return(true)
+    
+        it 'searches current PATH if original PATH did not result in valid executable' do
+      expect(Vagrant::Util::Which).to receive(:which).with('ssh', original_path: true).and_return(nil)
+      expect(Vagrant::Util::Which).to receive(:which).with('ssh').and_return('valid-return')
+      allow(Vagrant::Util::SafeExec).to receive(:exec).and_return(nil)
+      described_class.exec(ssh_info)
     end
     
-        it 'allows specifying deprecated options as a Hash from an Array/String to an Array/String' do
-      subject.deprecated_option(['foo1', 'foo2'] => 'bar1', 'foo3' => ['bar2', 'bar3'])
-      expect(subject.deprecated_options).to include(DeprecatedOption.new('foo1', 'bar1'))
-      expect(subject.deprecated_options).to include(DeprecatedOption.new('foo2', 'bar1'))
-      expect(subject.deprecated_options).to include(DeprecatedOption.new('foo3', 'bar2'))
-      expect(subject.deprecated_options).to include(DeprecatedOption.new('foo3', 'bar3'))
-    end
+            # Machine-readable (non-formatted) output
+        @env.ui.machine('metadata', 'machine-count', entries.length.to_s);
+        entries.each do |entry|
+          opts = { 'target' => entry.name.to_s }
+          @env.ui.machine('machine-id', entry.id.to_s[0...7], opts)
+          @env.ui.machine('provider-name', entry.provider.to_s, opts)
+          @env.ui.machine('machine-home', entry.vagrantfile_path.to_s, opts)
+          @env.ui.machine('state', entry.state.to_s, opts)
+        end
     
-        def run
-      command_name, *args = detect_command_and_arguments(*@args)
-      command = if help?
-        args.unshift(command_name) unless command_name.nil?
-        'help'
-      else
-        self.class.lookup_command(command_name)
+      if rss_url && rss_url.length > 0
+    blogs.push(Struct::Blog.new(name, web_url, rss_url))
+  else
+    unavailable.push(Struct::Blog.new(name, web_url, rss_url))
+  end
+end
+    
+          private
+    
+          def initialize(values={})
+        @trusted_keys = []
+        @fetched_keys = []
+        @locations = {}
+        @values = values
+        @trusted = true
       end
     
-        def fetch
-      downloader.fetch
-      @downloaded_path = downloader.cached_location
-    rescue => e
-      error = CaskError.new('Download failed on Cask '#{cask}' with message: #{e}')
-      error.set_backtrace e.backtrace
-      raise error
-    end
-    
-        return unless follow.account.activitypub?
-    
-        @inboxes = [@status.mentions, @status.reblogs, @status.preloadable_poll.votes].flat_map do |relation|
-      relation.includes(:account).map do |record|
-        record.account.preferred_inbox_url if !record.account.local? && record.account.activitypub?
+          def after
+        @versions[1][0..6]
       end
-    end
     
-    end
-end
-end
+    def normal(text)
+  text.gsub!(' ', '')
+  text.gsub!('\n', '')
+  text
 end
     
-      # @return [String] containing blob for ShadowHashData in user's plist
-  # @return [nil] if shadow is invalid
-  def grab_shadow_blob(user)
-    shadow_bytes = cmd_exec('dscl . read /Users/#{user} dsAttrTypeNative:ShadowHashData').gsub(/\s+/, '')
-    return nil unless shadow_bytes.start_with? 'dsAttrTypeNative:ShadowHashData:'
-    # strip the other bytes
-    shadow_bytes.sub!(/^dsAttrTypeNative:ShadowHashData:/, '')
+      if wiki_options[:plantuml_url]
+    Gollum::Filter::PlantUML.configure do |config|
+      puts 'Using #{wiki_options[:plantuml_url]} as PlantUML endpoint'
+      config.url = wiki_options[:plantuml_url]
+    end
   end
     
-    payload = File.binread('payload.dylib')
-output_data[add_dylib, payload.size] = payload
-    
-    end
-
-    
-      def webcam_stop
-    client.send_request(Packet.create_request('webcam_stop'))
-    true
-  end
-    
-        vprint_good 'Writing to #{net_com_opts[:target]}'
-    
-          explicit_plugins_specs.each do |spec|
-        packet_gem.add(spec.name)
+        context 'when iteration is nil' do
+      before do
+        subject.iteration = nil
       end
     
-        after :each do
-      logstash.uninstall
+      # Handle architecture naming conversion:
+  # <osname>:<osversion>:<arch>:<wordsize>[.other]
+  def architecture
+    osname    = %x{uname -s}.chomp
+    osversion = %x{uname -r}.chomp.split('.').first
+    
+      end # def input
+    
+        if File.exists?(params[:output])
+      # TODO(sissel): Allow folks to choose output?
+      logger.error('Puppet module directory '#{params[:output]}' already ' \
+                    'exists. Delete it or choose another output (-p flag)')
     end
     
-    desc 'Deploy website via rsync'
-task :rsync do
-  exclude = ''
-  if File.exists?('./rsync-exclude')
-    exclude = '--exclude-from '#{File.expand_path('./rsync-exclude')}''
+      def create_scripts
+    if script?(:after_install)
+      File.write(File.join(fpm_meta_path, 'after_install'), script(:after_install))
+    end
   end
-  puts '## Deploying website via Rsync'
-  ok_failed system('rsync -avze 'ssh -p #{ssh_port}' #{exclude} #{rsync_args} #{'--delete' unless rsync_delete == false} #{public_dir}/ #{ssh_user}:#{document_root}')
-end
-    
-    run SinatraStaticServer
-
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-        # Creates an instance of CategoryIndex for each category page, renders it, and
-    # writes the output to a file.
-    #
-    #  +category_dir+ is the String path to the category folder.
-    #  +category+     is the category currently being processed.
-    def write_category_index(category_dir, category)
-      index = CategoryIndex.new(self, self.source, category_dir, category)
-      index.render(self.layouts, site_payload)
-      index.write(self.dest)
-      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
-      self.pages << index
