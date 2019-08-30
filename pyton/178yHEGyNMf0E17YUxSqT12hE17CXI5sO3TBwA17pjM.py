e52d6bd7ccb7e08975a4e837b17daede03a603ff
SUNsung <weatherius@gmail.com>
2019-08-30 08:55:54 +0300

    Rewrite [178yHEGyNMf0E17YUxSqT12hE17CXI5sO3TBwA17pjM.py] File in: 2019-08-30 08:55:53

--- pyton/178yHEGyNMf0E17YUxSqT12hE17CXI5sO3TBwA17pjM.py
+++ pyton/178yHEGyNMf0E17YUxSqT12hE17CXI5sO3TBwA17pjM.py
@@ -1,241 +1,156 @@
 
-def register_type_handlers(connection, **kwargs):
-    if connection.vendor != 'postgresql' or connection.alias == NO_DB_ALIAS:
-        return
+        
+                '''
+        self.enabled = True
+        self.kwargs = kwargs
     
-        def get_decoded(self):
-        session_store_class = self.get_session_store_class()
-        return session_store_class().decode(self.session_data)
+        def test_GET_explicit_JSON_explicit_headers(self, httpbin):
+        r = http('--json', 'GET', httpbin.url + '/headers',
+                 'Accept:application/xml',
+                 'Content-Type:application/xml')
+        assert HTTP_OK in r
+        assert ''Accept': 'application/xml'' in r
+        assert ''Content-Type': 'application/xml'' in r
+    
+    
+def rst_filenames():
+    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
+        if '.tox' not in root:
+            for filename in fnmatch.filter(filenames, '*.rst'):
+                yield os.path.join(root, filename)
+    
+    
+def test_unicode_digest_auth(httpbin):
+    # it doesn't really authenticate us because httpbin
+    # doesn't interpret the utf8-encoded auth
+    http('--auth-type=digest',
+         '--auth', u'test:%s' % UNICODE,
+         httpbin.url + u'/digest-auth/auth/test/' + UNICODE)
 
     
     
-@functools.lru_cache()
-def get_default_renderer():
-    renderer_class = import_string(settings.FORM_RENDERER)
-    return renderer_class()
+class ExitStatus:
+    '''Program exit code constants.'''
+    SUCCESS = 0
+    ERROR = 1
+    PLUGIN_ERROR = 7
     
-        # Display the results
-    for top, right, bottom, left in face_locations:
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-    
-        # Load the uploaded image file
-    img = face_recognition.load_image_file(file_stream)
-    # Get face encodings for any faces in the uploaded image
-    unknown_face_encodings = face_recognition.face_encodings(img)
-    
-        # 获得所有人脸的位置以及它们的编码
-    face_locations = face_recognition.face_locations(output)
-    print('Found {} faces in image.'.format(len(face_locations)))
-    face_encodings = face_recognition.face_encodings(output, face_locations)
-    
-        :param file: image file name or file object to load
-    :param mode: format to convert the image to. Only 'RGB' (8-bit RGB, 3 channels) and 'L' (black and white) are supported.
-    :return: image contents as numpy array
-    '''
-    im = PIL.Image.open(file)
-    if mode:
-        im = im.convert(mode)
-    return np.array(im)
-    
-                # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-    
-    if settings.SENTRY_TAGSTORE == 'sentry.utils.services.ServiceDelegator':
-    backends = [
-        backend['path'] for backend in
-        settings.SENTRY_TAGSTORE_OPTIONS.get('backends', {}).values()
-    ]
-elif settings.SENTRY_TAGSTORE.startswith('sentry.tagstore.multi'):
-    backends = [
-        backend[0] for backend in
-        settings.SENTRY_TAGSTORE_OPTIONS.get('backends', [])
-    ]
-else:
-    backends = [settings.SENTRY_TAGSTORE]
-    
-        def forwards(self, orm):
-        # Adding model 'TagValue'
-        db.create_table(u'tagstore_tagvalue', (
-            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
-            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
-            ('_key', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                to=orm['tagstore.TagKey'], db_column='key_id')),
-            ('value', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('data', self.gf('sentry.db.models.fields.gzippeddict.GzippedDictField')(null=True, blank=True)),
-            ('times_seen', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
-            ('last_seen', self.gf('django.db.models.fields.DateTimeField')(
-                null=True, db_index=True)),
-            ('first_seen', self.gf('django.db.models.fields.DateTimeField')(
-                null=True, db_index=True)),
-        ))
-        db.send_create_signal('tagstore', ['TagValue'])
-    
-        models = {
-        'tagstore.eventtag': {
-            'Meta': {'unique_together': '(('project_id', 'event_id', 'key', 'value'),)', 'object_name': 'EventTag', 'index_together': '(('project_id', 'key', 'value'), ('group_id', 'key', 'value'))'},
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
-            'event_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
-            'group_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
-            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
-            'key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
-            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {}),
-            'value': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagValue']', 'db_column': ''value_id''})
-        },
-        'tagstore.grouptagkey': {
-            'Meta': {'unique_together': '(('project_id', 'group_id', '_key'),)', 'object_name': 'GroupTagKey'},
-            '_key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
-            'group_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
-            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
-            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
-            'values_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'})
-        },
-        'tagstore.grouptagvalue': {
-            'Meta': {'unique_together': '(('project_id', 'group_id', '_key', '_value'),)', 'object_name': 'GroupTagValue', 'index_together': '(('project_id', '_key', '_value', 'last_seen'),)'},
-            '_key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
-            '_value': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagValue']', 'db_column': ''value_id''}),
-            'first_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
-            'group_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
-            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
-            'last_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
-            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
-            'times_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'})
-        },
-        'tagstore.tagkey': {
-            'Meta': {'unique_together': '(('project_id', 'environment_id', 'key'),)', 'object_name': 'TagKey'},
-            'environment_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'null': 'True'}),
-            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
-            'key': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
-            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
-            'status': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'}),
-            'values_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'})
-        },
-        'tagstore.tagvalue': {
-            'Meta': {'unique_together': '(('project_id', '_key', 'value'),)', 'object_name': 'TagValue', 'index_together': '(('project_id', '_key', 'last_seen'),)'},
-            '_key': ('sentry.db.models.fields.foreignkey.FlexibleForeignKey', [], {'to': 'orm['tagstore.TagKey']', 'db_column': ''key_id''}),
-            'data': ('sentry.db.models.fields.gzippeddict.GzippedDictField', [], {'null': 'True', 'blank': 'True'}),
-            'first_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
-            'id': ('sentry.db.models.fields.bounded.BoundedBigAutoField', [], {'primary_key': 'True'}),
-            'last_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
-            'project_id': ('sentry.db.models.fields.bounded.BoundedBigIntegerField', [], {'db_index': 'True'}),
-            'times_seen': ('sentry.db.models.fields.bounded.BoundedPositiveIntegerField', [], {'default': '0'}),
-            'value': ('django.db.models.fields.CharField', [], {'max_length': '200'})
-        }
-    }
-    
-    from sentry.db.models import (Model, BoundedBigIntegerField, FlexibleForeignKey, sane_repr)
-    
-            for platform, project_id in queryset:
-            platform = platform.lower()
-            if platform not in VALID_PLATFORMS:
-                continue
-            ProjectPlatform.objects.create_or_update(
-                project_id=project_id,
-                platform=platform,
-                values={
-                    'last_seen': now,
-                },
-            )
-        min_project_id += step
-    
-    :copyright: (c) 2010-2015 by the Sentry Team, see AUTHORS for more details.
-:license: BSD, see LICENSE for more details.
-'''
-    
-            for job in job_list:
-            logger.debug('Sending scheduled job %s with payload %r', job.name, job.payload)
-            app.send_task(job.name, kwargs=job.payload)
-            job.delete()
+    del sys
 
     
+        def process_options(self, args, opts):
+        ScrapyCommand.process_options(self, args, opts)
+        try:
+            opts.spargs = arglist_to_dict(opts.spargs)
+        except ValueError:
+            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
+        if opts.output:
+            if opts.output == '-':
+                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
+            else:
+                self.settings.set('FEED_URI', opts.output, priority='cmdline')
+            feed_exporters = without_none_values(
+                self.settings.getwithbase('FEED_EXPORTERS'))
+            valid_output_formats = feed_exporters.keys()
+            if not opts.output_format:
+                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
+            if opts.output_format not in valid_output_formats:
+                raise UsageError('Unrecognized output format '%s', set one'
+                                 ' using the '-t' switch or as a file extension'
+                                 ' from the supported list %s' % (opts.output_format,
+                                                                  tuple(valid_output_formats)))
+            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
     
-  def Start( self ):
-    self.PostDataToHandler( {},
-                            'shutdown',
-                            TIMEOUT_SECONDS,
-                            display_message = False )
+        return _S3Connection
     
-      # Ignore 'syntax match' lines (see ':h syn-match').
-  if line.startswith( 'match ' ):
-    return []
+            respcls = responsetypes.from_args(body=body)
+        return response.replace(body=body, cls=respcls)
     
     
-@patch( 'ycm.client.messages_request.PostVimMessage',
-        new_callable = ExtendedMock )
-def HandlePollResponse_SingleMessage_test( post_vim_message ):
-  assert_that( _HandlePollResponse( [ { 'message': 'this is a message' } ] ,
-                                    None ),
-               equal_to( True ) )
+class DefaultHeadersMiddleware(object):
     
-        # For pickling to work, the __module__ variable needs to be set to the frame
-    # where the named tuple is created.  Bypass this step in enviroments where
-    # sys._getframe is not defined (Jython for example).
-    if hasattr(_sys, '_getframe'):
-        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+        def __init__(self, timeout=180):
+        self._timeout = timeout
     
-        @staticmethod
-    def gaussian(new_face, kernel_center, amount):
-        ''' Sharpen using gaussian filter '''
-        kernel_size = kernel_center[0]
-        blur = cv2.GaussianBlur(new_face, kernel_size, 0)  # pylint: disable=no-member
-        new_face = cv2.addWeighted(new_face,  # pylint: disable=no-member
-                                   1.0 + (0.5 * amount),
-                                   blur,
-                                   -(0.5 * amount),
-                                   0)
-        return new_face
+        @nonce.decoder
+    def nonce(value):  # pylint: disable=missing-docstring,no-self-argument
+        try:
+            return jose.decode_b64jose(value)
+        except jose.DeserializationError as error:
+            # TODO: custom error
+            raise jose.DeserializationError('Invalid nonce: {0}'.format(error))
     
-        def output_filename(self, filename):
-        ''' Return the output filename with the correct folder and extension
-            NB: The plugin must have a config item 'format' that contains the
-                file extension to use this method '''
-        filename = os.path.splitext(os.path.basename(filename))[0]
-        out_filename = '{}.{}'.format(filename, self.config['format'])
-        out_filename = os.path.join(self.output_folder, out_filename)
-        logger.trace('in filename: '%s', out filename: '%s'', filename, out_filename)
-        return out_filename
+            cert_pem = test_util.load_vector(certs[0])
+        chain_pem = (test_util.load_vector(certs[0]) + test_util.load_vector(certs[1]))
+        candidate_cert_path = os.path.join(tmp_path, 'certs', 'cert_512.pem')
+        candidate_chain_path = os.path.join(tmp_path, 'chains', 'chain.pem')
+        candidate_fullchain_path = os.path.join(tmp_path, 'chains', 'fullchain.pem')
+        mock_parser.verb = 'certonly'
+        mock_parser.args = ['--cert-path', candidate_cert_path,
+                            '--chain-path', candidate_chain_path,
+                            '--fullchain-path', candidate_fullchain_path]
     
-            output_args.extend(['-c:v', codec])
-        output_args.extend(['-crf', str(self.config['crf'])])
-        output_args.extend(['-preset', self.config['preset']])
+        def _get_rfc2136_client(self):
+        return _RFC2136Client(self.credentials.conf('server'),
+                              int(self.credentials.conf('port') or self.PORT),
+                              self.credentials.conf('name'),
+                              self.credentials.conf('secret'),
+                              self.ALGORITHMS.get(self.credentials.conf('algorithm'),
+                                                  dns.tsig.HMAC_MD5))
     
-        def get_save_args(self):
-        ''' Return the save parameters for the file format '''
-        filetype = self.config['format']
-        args = list()
-        if filetype == 'jpg' and self.config['jpg_quality'] > 0:
-            args = (cv2.IMWRITE_JPEG_QUALITY,  # pylint: disable=no-member
-                    self.config['jpg_quality'])
-        if filetype == 'png' and self.config['png_compress_level'] > -1:
-            args = (cv2.IMWRITE_PNG_COMPRESSION,  # pylint: disable=no-member
-                    self.config['png_compress_level'])
-        logger.debug(args)
-        return args
+            self.webroot = os.path.join(self.nginx_root, 'webroot')
+        os.mkdir(self.webroot)
+        with open(os.path.join(self.webroot, 'index.html'), 'w') as file_handler:
+            file_handler.write('Hello World!')
     
-        http://stackoverflow.com/questions/3221956/
-           what-is-the-simplest-way-to-make-tooltips-
-           in-tkinter/36221216#36221216
+        with open(context.nginx_config_path, 'r') as file_h:
+        current_nginx_config = file_h.read()
     
-            if self.scale:
-            self.gamma = self.add_weight(shape=shape,
-                                         name='gamma',
-                                         initializer=self.gamma_initializer,
-                                         regularizer=self.gamma_regularizer,
-                                         constraint=self.gamma_constraint)
-        else:
-            self.gamma = None
-        if self.center:
-            self.beta = self.add_weight(shape=shape,
-                                        name='beta',
-                                        initializer=self.beta_initializer,
-                                        regularizer=self.beta_regularizer,
-                                        constraint=self.beta_constraint)
-        else:
-            self.beta = None
-        self.built = True
+        @property
+    def problem(self):
+        '''Return the Exception raised during plugin setup, or None if all is well'''
+        if isinstance(self._prepared, Exception):
+            return self._prepared
+        return None
+    
+    
+def rand_(left, right):
+    return operator.and_(right, left)
+    
+        def time_pandas_dtype(self, dtype):
+        pandas_dtype(dtype)
+    
+        for fixit in fixit_completion:
+      vimsupport.ReplaceChunks( fixit[ 'chunks' ], silent=True )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+        with patch( 'ycm.youcompleteme.SendCommandRequest' ) as send_request:
+      ycm.SendCommandRequest( [ 'GoTo', 'ft=python' ], '', False, 1, 1 )
+      send_request.assert_called_once_with( *expected_args )
+
+    
+      post_vim_message.assert_has_exact_calls( [
+    call( 'On the first day of Christmas, my VimScript gave to me',
+          warning=False,
+          truncate=True ),
+    call( 'A test file in a Command-T', warning=False, truncate=True ),
+    call( 'On the second day of Christmas, my VimScript gave to me',
+          warning=False,
+          truncate=True ),
+    call( 'Two popup menus, and a test file in a Command-T',
+          warning=False,
+          truncate=True ),
+  ] )
