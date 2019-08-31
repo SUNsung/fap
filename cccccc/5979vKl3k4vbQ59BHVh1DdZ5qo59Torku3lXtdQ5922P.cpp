a49f1ec439ee311d2d5af18426007949b56ff69b
SUNsung <weatherius@gmail.com>
2019-08-31 04:31:39 +0300

    Rewrite [5979vKl3k4vbQ59BHVh1DdZ5qo59Torku3lXtdQ5922P.cpp] File in: 2019-08-31 04:31:39

--- cccccc/5979vKl3k4vbQ59BHVh1DdZ5qo59Torku3lXtdQ5922P.cpp
+++ cccccc/5979vKl3k4vbQ59BHVh1DdZ5qo59Torku3lXtdQ5922P.cpp
@@ -1,237 +1,145 @@
 
         
-        
-    {	BIND_ENUM_CONSTANT(TEXTURE_SIZE_256);
-	BIND_ENUM_CONSTANT(TEXTURE_SIZE_512);
-	BIND_ENUM_CONSTANT(TEXTURE_SIZE_1024);
-	BIND_ENUM_CONSTANT(TEXTURE_SIZE_2048);
-	BIND_ENUM_CONSTANT(TEXTURE_SIZE_4096);
-	BIND_ENUM_CONSTANT(TEXTURE_SIZE_MAX);
-}
+        public:
+    explicit OpenURIDialog(QWidget *parent);
+    ~OpenURIDialog();
     
-    	static void _convert_detail_mesh_to_native_navigation_mesh(const rcPolyMeshDetail *p_detail_mesh, Ref<NavigationMesh> p_nav_mesh);
-	static void _build_recast_navigation_mesh(Ref<NavigationMesh> p_nav_mesh, EditorProgress *ep,
-			rcHeightfield *hf, rcCompactHeightfield *chf, rcContourSet *cset, rcPolyMesh *poly_mesh,
-			rcPolyMeshDetail *detail_mesh, Vector<float> &vertices, Vector<int> &indices);
-    
-    public:
-	enum SamplePartitionType {
-		SAMPLE_PARTITION_WATERSHED = 0,
-		SAMPLE_PARTITION_MONOTONE,
-		SAMPLE_PARTITION_LAYERS,
-		SAMPLE_PARTITION_MAX
-	};
-    
-    
-    {	UPNP();
-	~UPNP();
-};
-    
-    	ClassDB::bind_method(D_METHOD('set_igd_status', 'status'), &UPNPDevice::set_igd_status);
-	ClassDB::bind_method(D_METHOD('get_igd_status'), &UPNPDevice::get_igd_status);
-	ADD_PROPERTY(PropertyInfo(Variant::INT, 'igd_status', PROPERTY_HINT_ENUM), 'set_igd_status', 'get_igd_status');
-    
-    	UPNPDevice();
-	~UPNPDevice();
-    
-    	bool UpdatePowerInfo();
-    
-    
-    {	return memnew(SemaphoreIphone);
-}
-    
-    struct cgsem {
-	int pipefd[2];
-};
-    
-    	bool disabled;
-    
-    struct SquaredLogError {
-  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return x; }
-  XGBOOST_DEVICE static bool CheckLabel(bst_float label) {
-    return label > -1;
-  }
-  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
-    predt = fmaxf(predt, -1 + 1e-6);  // ensure correct value for log1p
-    return (std::log1p(predt) - std::log1p(label)) / (predt + 1);
-  }
-  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
-    predt = fmaxf(predt, -1 + 1e-6);
-    float res = (-std::log1p(predt) + std::log1p(label) + 1) /
-                std::pow(predt + 1, 2);
-    res = fmaxf(res, 1e-6f);
-    return res;
-  }
-  static bst_float ProbToMargin(bst_float base_score) { return base_score; }
-  static const char* LabelErrorMsg() {
-    return 'label must be greater than -1 for rmsle so that log(label + 1) can be valid.';
-  }
-  static const char* DefaultEvalMetric() { return 'rmsle'; }
-};
-    
-    
-    {    data_vec.resize(offset_vec.back());
-    CHECK_EQ(index_.data.size(), value_.data.size());
-    CHECK_EQ(index_.data.size(), data_vec.size());
-    for (size_t i = 0; i < data_vec.size(); ++i) {
-      data_vec[i] = Entry(index_.data[i] + min_index_, value_.data[i]);
+    /**
+ * An RAII-style reverse lock. Unlocks on construction and locks on destruction.
+ */
+template<typename Lock>
+class reverse_lock
+{
+public:
     }
-    return true;
-  }
     
-    #include <xgboost/base.h>
-#include <xgboost/data.h>
-#include <cstdint>
-#include <utility>
-#include './host_device_vector.h'
+        BOOST_CHECK(obj.exists('age'));
+    BOOST_CHECK(obj.exists('first'));
+    BOOST_CHECK(obj.exists('last'));
+    BOOST_CHECK(obj.exists('distance'));
+    BOOST_CHECK(obj.exists('time'));
+    BOOST_CHECK(obj.exists('calories'));
+    BOOST_CHECK(obj.exists('temperature'));
+    BOOST_CHECK(obj.exists('moon'));
+    BOOST_CHECK(obj.exists('spoon'));
+    BOOST_CHECK(obj.exists('cat1'));
+    BOOST_CHECK(obj.exists('cat2'));
     
-      vals_in.clear(); ss.flush(); ss.clear(); ss.str('');
-  ss << ' ( 3, 2,1 ) ';
-  ss >> vals_in;
-  EXPECT_EQ(vals_in, vals);
-    
-    namespace xgboost {
-ConsoleLogger::~ConsoleLogger() {
-  if (cur_verbosity_ == LogVerbosity::kIgnore ||
-      cur_verbosity_ <= global_verbosity_) {
-    dmlc::CustomLogMessage::Log(log_stream_.str());
-  }
-}
-TrackerLogger::~TrackerLogger() {
-  dmlc::CustomLogMessage::Log(log_stream_.str());
-}
-}  // namespace xgboost
-    
-    
-    {  return AlignImpl::s_table[idx];
-}
-    
-      auto const release = emitDecRefHelper(cb, data, local, type, local | last);
-  // Now we know where release is, we can patch the calls to
-  // releaseFake and point them to the correct address.
-  for (auto addr = start; addr < release; ) {
-    x64::DecodedInstruction di(addr, addr);
-    if (di.hasPicOffset() && di.picAddress() == releaseFake) {
-      always_assert(di.isCall());
-      di.setPicAddress(release);
-    }
-    addr += di.size();
-  }
-    
-    
-    {    for (auto const s : succs(code.back())) {
-      FTRACE(4, '  -> {}\n', s);
-      auto& succ_state = env.block_states[s];
-      if (merge_into(succ_state, state)) workQ.push(env.block_to_rpo[s]);
-    }
-  } while (!workQ.empty());
-    
-    inline Vptr operator+(Vptr lhs, int32_t d) {
-  auto copy = lhs;
-  copy.disp += d;
-  return copy;
-}
-    
-      void xedInstrRelBr(xed_iclass_enum_t instr,
-                     CodeAddress destination, int size) {
-    static XedLenCache lenCache;
-    auto instrLen = xedCacheLen(
-                      &lenCache,
-                      [&]() {
-                        return xedEmit(instr, toXedOperand((CodeAddress)0,
-                                       size), 0, dest());
-                      }, xedLenCacheKey(instr, size));
-    auto target = destination - (frontier() + instrLen);
-    xedEmit(instr, toXedOperand((CodeAddress)target, size));
-  }
-    
-      int sz = 0;
-  switch (*ip) {
-    case 0xc4:
-    case 0x8f:
-      if (*ip == 0xc4) {
-        m_flags.vex = 1;
-      } else {
-        // 0x8f is both a valid one-byte opcode and the first byte of the
-        // 3-byte XOP prefix. Figure out which one we have here by inspecting
-        // the next byte.
-        if (ip[1] & 0x18) {
-          m_flags.xop = 1;
+      std::string AllEntriesFor(const Slice& user_key) {
+    Iterator* iter = dbfull()->TEST_NewInternalIterator();
+    InternalKey target(user_key, kMaxSequenceNumber, kTypeValue);
+    iter->Seek(target.Encode());
+    std::string result;
+    if (!iter->status().ok()) {
+      result = iter->status().ToString();
+    } else {
+      result = '[ ';
+      bool first = true;
+      while (iter->Valid()) {
+        ParsedInternalKey ikey;
+        if (!ParseInternalKey(iter->key(), &ikey)) {
+          result += 'CORRUPTED';
         } else {
-          return 0;
+          if (last_options_.comparator->Compare(ikey.user_key, user_key) != 0) {
+            break;
+          }
+          if (!first) {
+            result += ', ';
+          }
+          first = false;
+          switch (ikey.type) {
+            case kTypeValue:
+              result += iter->value().ToString();
+              break;
+            case kTypeDeletion:
+              result += 'DEL';
+              break;
+          }
         }
+        iter->Next();
       }
+      if (!first) {
+        result += ' ';
+      }
+      result += ']';
     }
+    delete iter;
+    return result;
+  }
     
-      if (!strcasecmp(req, 'stdin')) {
-    return make_from(BuiltinFiles::GetSTDIN());
-  }
-  if (!strcasecmp(req, 'stdout')) {
-    return make_from(BuiltinFiles::GetSTDOUT());
-  }
-  if (!strcasecmp(req, 'stderr')) {
-    return make_from(BuiltinFiles::GetSTDERR());
-  }
-  if (!strncasecmp(req, 'fd/', sizeof('fd/') - 1)) {
-    return openFD(req + sizeof('fd/') - 1);
-  }
-  if (!strncasecmp(req, 'temp', sizeof('temp') - 1)) {
-    auto file = req::make<TempFile>(true, s_php, s_temp);
-    if (!file->valid()) {
-      raise_warning('Unable to create temporary file');
-      return nullptr;
-    }
-    return file;
-  }
-  if (!strcasecmp(req, 'memory')) {
-    auto file = req::make<TempFile>(true, s_php, s_memory);
-    if (!file->valid()) {
-      raise_warning('Unable to create temporary file');
-      return nullptr;
-    }
-    }
-    
-    namespace HPHP {
-    }
-    
-    int64_t TempFile::getLength() {
-  struct stat sb;
-  if (StatCache::lstat(File::TranslatePathWithFileCache(m_rawName).c_str(), &sb)) {
-    Logger::Verbose('%s/%d: %s', __FUNCTION__, __LINE__,
-                    folly::errnoStr(errno).c_str());
-    // use fstat directly
-    if (fstat(getFd(), &sb) != 0) return -1;
-    return sb.st_size;
-  }
-  return sb.st_size;
+    void SetProxyConfigCallback(
+    base::WaitableEvent* done,
+    net::URLRequestContextGetter* url_request_context_getter,
+    const net::ProxyConfig& proxy_config) {
+  net::ProxyService* proxy_service =
+      url_request_context_getter->GetURLRequestContext()->proxy_service();
+  proxy_service->ResetConfigService(
+      new net::ProxyConfigServiceFixed(proxy_config));
+  done->Signal();
 }
     
-    const StaticString
-  s_errno('errno'),
-  s_errstr('errstr');
+    void Base::CallSync(const std::string& method,
+                    const base::ListValue& arguments,
+                    base::ListValue* result) {
+  NOTREACHED() << 'Uncatched callAsync in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
+}
     
-      void initStandard();
-  void initErrorFunc();
-  void initClassobj();
-  void initClosure();
-  void initNetwork();
-  void initOptions();
-  void initGc();
-  void initOutput();
-  void initString();
-  void initVariable();
-  void initFunction();
-  void initMisc();
-  void initFile();
-  void initIntrinsics();
-  void initMath();
-  void initProcess();
+      scoped_ptr<base::Value> value_option(
+      converter->FromV8Value(options, isolate->GetCurrentContext()));
+  if (!value_option.get() ||
+      !value_option->IsType(base::Value::TYPE_DICTIONARY))
+    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
+        'Unable to convert 'option' passed to AllocateObject')));
     
-    Variant HHVM_FUNCTION(stream_socket_recvfrom,
-                      const Resource& socket,
-                      int length,
-                      int flags = 0,
-                      VRefParam address = uninit_null());
     
-    #include 'hphp/runtime/vm/jit/mcgen-prologue.h'
-#include 'hphp/runtime/vm/jit/mcgen-translate.h'
+    {} // namespace nwapi
+
+    
+    void Menu::RemoveKeys() {
+  if (!focus_manager_) return;
+    }
+    
+    class NwClipboardGetListSyncFunction : public NWSyncExtensionFunction {
+ public:
+  NwClipboardGetListSyncFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+        // out_of_range.401
+    try
+    {
+        // try to use a an invalid array index
+        json::const_reference ref = j.at('/array/4'_json_pointer);
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+        // create a JSON number from number_integer_t
+    json::number_integer_t value_integer_t = -42;
+    json j_integer_t(value_integer_t);
+    
+        // print values
+    std::cout << object << '\n';
+    std::cout << *res1.first << ' ' << std::boolalpha << res1.second << '\n';
+    
+    
+    {    // COMPILE_TEMPLATE_DATUM(UserPostProcessing);
+}
+    
+            inline T area() const
+        {
+            return x * y;
+        }
+    
+        /*! \brief Copy assignment to forward copy to the superclass correctly.
+     * Required for MSVC.
+     */
+    BufferD3D10& operator = (const BufferD3D10 &buf)
+    {
+        Buffer::operator=(buf);
+        return *this;
+    }
