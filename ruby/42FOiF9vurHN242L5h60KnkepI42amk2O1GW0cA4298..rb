33804531d438bf1d3b9449707b43e67991d9dec9
SUNsung <weatherius@gmail.com>
2019-09-01 02:52:51 +0300

    Rewrite [42FOiF9vurHN242L5h60KnkepI42amk2O1GW0cA4298..rb] File in: 2019-09-01 02:52:51

--- ruby/42FOiF9vurHN242L5h60KnkepI42amk2O1GW0cA4298..rb
+++ ruby/42FOiF9vurHN242L5h60KnkepI42amk2O1GW0cA4298..rb
@@ -1,193 +1,127 @@
 
         
-                result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            tag: '#{tag}',
-            message: '#{message}',
-            force: true
-          })
-        end').runner.execute(:test)
+        RSpec.describe FileStore::BaseStore do
+  fab!(:upload) { Fabricate(:upload, id: 9999, sha1: Digest::SHA1.hexdigest('9999')) }
     
-          it 'adds no_create_docset param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            no_create_docset: true
-          )
-        end').runner.execute(:test)
+      context '#set_traps' do
+    it 'sets traps for INT TERM and QUIT' do
+      agent_runner = AgentRunner.new
+      mock(Signal).trap('INT')
+      mock(Signal).trap('TERM')
+      mock(Signal).trap('QUIT')
+      agent_runner.set_traps
     
-    class Array
-  def shelljoin
-    CrossplatformShellwords.shelljoin(self)
+        it 'strips punctuation' do
+      expect(AgentsExporter.new(:name => 'foo,bar').filename).to eq('foo-bar.json')
+    end
+    
+      describe 'up' do
+    it 'should update extract and template options for an existing WebsiteAgent' do
+      expect(agent.options).to include('extract' => old_extract,
+                                       'template' => old_template)
+      ConvertWebsiteAgentTemplateForMerge.new.up
+      agent.reload
+      expect(agent.options).to include('extract' => new_extract,
+                                       'template' => new_template)
+    end
   end
-end
     
-          it 'keeps the specified metadata folder' do
-        expect(options[:metadata_path]).to eq('./metadata')
+        it 'should generate the correct last checkpoint url' do
+      @checker.options['path'] = 'last_checkpoint/usps/9361289878905919630610'
+      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/last_checkpoint/usps/9361289878905919630610')
+    end
+  end
+    
+      describe 'check' do
+    it 'should command targets' do
+      stub(Agent).async_check(target.id).once { nil }
+      agent.check
+    end
+  end
+    
+      describe '#build_rsync_chown' do
+    let(:opts) {{:type=>:rsync,
+                 :guestpath=>'/vagrant',
+                 :hostpath=>'/home/user/syncfolder',
+                 :disabled=>false,
+                 :__vagrantfile=>true,
+                 :exclude=>['.vagrant'],
+                 :owner=>'vagrant',
+                 :group=>'vagrant'}}
+    
+        def byte_to_str_pos(pos)
+      @s.string.byteslice(0, pos).length
+    end
+    
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
       end
     end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
   end
 end
 
     
-        def resource_params
-      params.require(:user).permit(
-        :unconfirmed_email
-      )
-    end
-  end
-end
-
+          # Checks the client's masked token to see if it matches the
+      # session token.
+      def valid_token?(session, token)
+        return false if token.nil? || token.empty?
     
-              redirect_to admin_reports_path, notice: I18n.t('admin.reports.resolved_msg')
-          return
-        end
-    
-      def maxwidth_or_default
-    (params[:maxwidth].presence || 400).to_i
-  end
-    
-      def verified_domain
-    return signed_request_account.domain if signed_request_account
-  end
-    
-      def update
-    if verify_payload?
-      process_salmon
-      head 202
-    elsif payload.present?
-      render plain: signature_verification_failure_reason, status: 401
-    else
-      head 400
-    end
-  end
-    
-        @web_subscription = ::Web::PushSubscription.create!(
-      endpoint: subscription_params[:endpoint],
-      key_p256dh: subscription_params[:keys][:p256dh],
-      key_auth: subscription_params[:keys][:auth],
-      data: data_params,
-      user_id: current_user.id,
-      access_token_id: doorkeeper_token.id
-    )
-    
-      def after_sign_in_path_for(resource)
-    if resource.email_verified?
-      root_path
-    else
-      finish_signup_path
-    end
-  end
-end
-
-    
-      private
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
-      end
-    end
-  end
-end
-    
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
-      end
-    end
-  end
-    
-        initialize_with { Tmuxinator::Project.new(file) }
-  end
-    
-      describe '#hook_on_project_start' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_start' }
-    end
-  end
-  describe '#hook_on_project_first_start' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_first_start' }
-    end
-  end
-  describe '#hook_on_project_restart' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_restart' }
-    end
-  end
-  describe '#hook_on_project_exit' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_exit' }
-    end
-  end
-  describe '#hook_on_project_stop' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_stop' }
-    end
-  end
-end
-
-    
-    require 'tmuxinator'
-require 'factory_bot'
-    
-      describe '#default?' do
-    let(:directory) { described_class.directory }
-    let(:local_default) { described_class::LOCAL_DEFAULT }
-    let(:proj_default) { described_class.default }
-    
-          it 'force detach renders the project without attach code' do
-        ARGV.replace(['debug', '--attach=false', 'sample'])
-        capture_io { cli.start }
-        # Currently no project is rendered at all
-        # expect(out).to_not include 'attach-session'
+          def instrument(env)
+        return unless i = options[:instrumenter]
+        env['rack.protection.attack'] = self.class.name.split('::').last.downcase
+        i.instrument('rack.protection', env)
       end
     
-    describe Tmuxinator::WemuxSupport do
-  let(:klass) { Class.new }
-  let(:instance) { klass.new }
-    
-      s.add_development_dependency 'activesupport', '< 5.0.0' # Please see issue #432
-  s.add_development_dependency 'awesome_print', '~> 1.2'
-  s.add_development_dependency 'bundler', '>= 1.3'
-  s.add_development_dependency 'coveralls', '~> 0.8'
-  s.add_development_dependency 'factory_bot', '~> 4.8'
-  s.add_development_dependency 'pry', '~> 0.10'
-  s.add_development_dependency 'rake', '~> 10.4'
-  s.add_development_dependency 'rspec', '~> 3.3'
-  s.add_development_dependency 'rubocop', '~> 0.48.1'
-  s.add_development_dependency 'simplecov', '~> 0.16'
-    
-    Then 'I should see a group {string} with the scopes {string} and {string}' do |group, name1, name2|
-  group = group.tr(' ', '').underscore.downcase
-  name1 = name1.tr(' ', '').underscore.downcase
-  name2 = name2.tr(' ', '').underscore.downcase
-  expect(page).to     have_css '.scopes .scope-group-#{group} .#{name1}'
-  expect(page).to     have_css '.scopes .scope-group-#{group} .#{name2}'
-end
-    
+          def call(env)
+        status, headers, body = super
+        response = Rack::Response.new(body, status, headers)
+        request = Rack::Request.new(env)
+        remove_bad_cookies(request, response)
+        response.finish
       end
     
-                default_title = render_or_call_method_or_proc_on(self, batch_action.title)
-            title = I18n.t('active_admin.batch_actions.labels.#{batch_action.sym}', default: default_title)
-            label = I18n.t('active_admin.batch_actions.action_label', title: title)
+      it 'allows for a custom authenticity token param' do
+    mock_app do
+      use Rack::Protection::AuthenticityToken, :authenticity_param => 'csrf_param'
+      run proc { |e| [200, {'Content-Type' => 'text/plain'}, ['hi']] }
+    end
     
-        def self.controllers
-      {
-        sessions: 'active_admin/devise/sessions',
-        passwords: 'active_admin/devise/passwords',
-        unlocks: 'active_admin/devise/unlocks',
-        registrations: 'active_admin/devise/registrations',
-        confirmations: 'active_admin/devise/confirmations'
-      }
+      s.add_dependency 'erubis', '~> 2.6'
+  s.add_dependency 'thor', '~> 0.19', '>= 0.15.0'
+  s.add_dependency 'xdg', '~> 2.2', '>= 2.2.5'
+    
+        def tmux_split_command
+      path = if tab.root?
+               '#{Tmuxinator::Config.default_path_option} #{tab.root}'
+             end
+      '#{project.tmux} splitw #{path} -t #{tab.tmux_window_target}'
+    end
+    
+          def validate(options = {})
+        name = options[:name]
+        options[:force_attach] ||= false
+        options[:force_detach] ||= false
+        project_config = options.fetch(:project_config) { false }
+        project_file = if valid_project_config?(project_config)
+                         project_config
+                       elsif valid_local_project?(name)
+                         local_project
+                       elsif valid_standard_project?(name)
+                         project(name)
+                       else
+                         # This branch should never be reached,
+                         # but just in case ...
+                         raise NO_PROJECT_FOUND_MSG
+                       end
+    
+          it 'returns true' do
+        expect(described_class.shell?).to be_truthy
+      end
     end
