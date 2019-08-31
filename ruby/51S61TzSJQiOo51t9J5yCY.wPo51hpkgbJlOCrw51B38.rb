23a63c88b10aeda9bf91d3fc705ea2e4ff261a03
SUNsung <weatherius@gmail.com>
2019-08-31 17:28:20 +0300

    Rewrite [51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb] File in: 2019-08-31 17:28:20

--- ruby/51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb
+++ ruby/51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb
@@ -1,31 +1,67 @@
 
         
-                def id_for_already_imported_cache(issue)
-          issue.number
+            let(:validator) { described_class.new }
+    let(:account)   { double(username: username, errors: errors) }
+    let(:errors )   { double(add: nil) }
+    
+          it 'creates a follow request relation between user and target user' do
+        expect(user.account.requested?(other_account)).to be true
+      end
+    
+      def eligible_users
+    User.emailable
+        .where('current_sign_in_at < ?', (FREQUENCY + SIGN_IN_OFFSET).ago)
+        .where('last_emailed_at IS NULL OR last_emailed_at < ?', FREQUENCY.ago)
+  end
+end
+
+    
+      # Loaded immediately after dependencies to ensure proper override of their
+  # UI methods.
+  #
+  require 'cocoapods/user_interface'
+    
+            def create_project(path, object_version, pod_target_subproject)
+          object_version ||= Xcodeproj::Constants::DEFAULT_OBJECT_VERSION
+          Pod::Project.new(path, false, object_version, :pod_target_subproject => pod_target_subproject)
         end
     
-              issue.label_names.each do |label_name|
-            # Although unlikely it's technically possible for an issue to be
-            # given a label that was created and assigned after we imported all
-            # the project's labels.
-            next unless (label_id = label_finder.id_for(label_name))
+        it 'includes additional entries if requested' do
+      generator = Generator::InfoPlistFile.new('1.0.0', Platform.new(:ios, '10.8'), :appl, 'UILaunchStoryboardName' => 'LaunchScreen')
+      file = temporary_directory + 'Info.plist'
+      generator.save_as(file)
+      Xcodeproj::Plist.read_from_path(file).should == {
+        'CFBundleDevelopmentRegion' => 'en',
+        'CFBundleExecutable' => '${EXECUTABLE_NAME}',
+        'CFBundleIdentifier' => '${PRODUCT_BUNDLE_IDENTIFIER}',
+        'CFBundleInfoDictionaryVersion' => '6.0',
+        'CFBundleName' => '${PRODUCT_NAME}',
+        'CFBundlePackageType' => 'APPL',
+        'CFBundleShortVersionString' => '1.0.0',
+        'CFBundleSignature' => '????',
+        'CFBundleVersion' => '${CURRENT_PROJECT_VERSION}',
+        'NSPrincipalClass' => '',
+        'UILaunchStoryboardName' => 'LaunchScreen',
+      }
+    end
     
-          # The method that will be called for traversing through all the objects to
-      # import, yielding them to the supplied block.
-      def each_object_to_import
-        repo = project.import_source
-    
-          private
-    
-            def execute_repl_command(repl_command)
-          unless repl_command == '\n'
-            repl_commands = repl_command.split
-            subcommand = repl_commands.shift.capitalize
-            arguments = repl_commands
-            subcommand_class = Pod::Command::IPC.const_get(subcommand)
-            subcommand_class.new(CLAide::ARGV.new(arguments)).run
-            signal_end_of_output
-          end
+            # Initialize a new instance
+        #
+        # @param [TargetDefinition] target_definition @see #target_definition
+        # @param [Xcodeproj::Project] project @see #project
+        # @param [Array<String>] project_target_uuids @see #project_target_uuids
+        # @param [Hash{String=>Symbol}] build_configurations @see #build_configurations
+        # @param [Platform] platform @see #platform
+        # @param [Array<String>] archs @see #archs
+        #
+        def initialize(target_definition, project, project_target_uuids, build_configurations, platform, archs)
+          @target_definition = target_definition
+          @project = project
+          @project_target_uuids = project_target_uuids
+          @build_configurations = build_configurations
+          @platform = platform
+          @archs = archs
+          @client_root = project.project_dir.realpath
         end
       end
     end
@@ -33,150 +69,21 @@
 end
 
     
-          #-----------------------------------------------------------------------#
-    
-    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
-  file_shared_path = TestApp.shared_path.join(file)
-  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
-  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
-end
-    
-      at_exit do
-    if ENV['KEEP_RUNNING']
-      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
-      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
-    else
-      vagrant_cli_command('destroy -f')
-    end
+      describe '#name' do
+    it { expect(instance.name).to eq 'wemux' }
   end
     
-        def set_if_empty(key, value=nil, &block)
-      set(key, value, &block) unless keys.include?(key)
-    end
-    
-          def call
-        ask_question
-        value_or_default
-      end
-    
-          private
-    
-            def swap_range(corrector, range1, range2)
-          src1 = range1.source
-          src2 = range2.source
-          corrector.replace(range1, src2)
-          corrector.replace(range2, src1)
-        end
+        def build_commands(_prefix, command_yml)
+      if command_yml.is_a?(Array)
+        command_yml.map do |command|
+          '#{tmux_window_command_prefix} #{command.shellescape} C-m' if command
+        end.compact
+      elsif command_yml.is_a?(String) && !command_yml.empty?
+        ['#{tmux_window_command_prefix} #{command_yml.shellescape} C-m']
+      else
+        []
       end
     end
+    
+        include_examples :unsupported_version_message, :local
   end
-end
-
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-        # Outputs a single category as an <a> link.
-    #
-    #  +category+ is a category string to format as an <a> link
-    #
-    # Returns string
-    #
-    def category_link(category)
-      dir = @context.registers[:site].config['category_dir']
-      '<a class='category' href='/#{dir}/#{category.to_url}/'>#{category}</a>'
-    end
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-      option '--validate', :flag, 'Validate with pkg install',
-    :default => true
-    
-        pear_cmd = 'pear -c #{config} remote-info #{input_package}'
-    logger.info('Fetching package information', :package => input_package, :command => pear_cmd)
-    name = %x{#{pear_cmd} | sed -ne '/^Package\s*/s/^Package\s*//p'}.chomp
-    self.name = '#{attributes[:pear_package_name_prefix]}-#{name}'
-    self.version = %x{#{pear_cmd} | sed -ne '/^Installed\s*/s/^Installed\s*//p'}.chomp
-    self.description  = %x{#{pear_cmd} | sed -ne '/^Summary\s*/s/^Summary\s*//p'}.chomp
-    logger.debug('Package info', :name => self.name, :version => self.version,
-                  :description => self.description)
-    
-          success = safesystem(setup_cmd)
-      #%x{#{setup_cmd}}
-      if !success
-        logger.error('setup.py get_metadata failed', :command => setup_cmd,
-                      :exitcode => $?.exitstatus)
-        raise 'An unexpected error occurred while processing the setup.py file'
-      end
-      File.read(tmp)
-    end
-    logger.debug('result from `setup.py get_metadata`', :data => output)
-    metadata = JSON.parse(output)
-    logger.info('object output of get_metadata', :json => metadata)
-    
-        args = [ tar_cmd,
-             '-C',
-             staging_path,
-             '-cf',
-             payload_tar,
-             '--owner=0',
-             '--group=0',
-             '--numeric-owner',
-             '.' ]
-    
-    class FPM::RakeTask < Rake::TaskLib
-  attr_reader :options
-    
-        if !program.include?('/') and !program_in_path?(program)
-      raise ExecutableNotFound.new(program)
-    end
-    
-        def _send_keys(t, e)
-      '#{project.tmux} send-keys -t #{t} #{e} C-m'
-    end
-  end
-end
-
-    
-        initialize_with { Tmuxinator::Project.new(file, custom_name: 'custom') }
-  end
-    
-      it { expect(subject.tmux_window_and_pane_target).to eql 'foo:0.1' }
-end
-
-    
-              it 'creates a project file' do
-            capture_io { cli.start }
-            expect(file.string).to_not be_empty
-            # make sure the output is valid YAML
-            expect { YAML.parse file.string }.to_not raise_error
-          end
-        end
