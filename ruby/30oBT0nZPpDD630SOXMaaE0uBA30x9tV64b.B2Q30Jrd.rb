a54418fd8670f06717c9ce607658cbe7e8bc5137
SUNsung <weatherius@gmail.com>
2019-08-31 07:46:18 +0300

    Rewrite [30oBT0nZPpDD630SOXMaaE0uBA30x9tV64b.B2Q30Jrd.rb] File in: 2019-08-31 07:46:18

--- ruby/30oBT0nZPpDD630SOXMaaE0uBA30x9tV64b.B2Q30Jrd.rb
+++ ruby/30oBT0nZPpDD630SOXMaaE0uBA30x9tV64b.B2Q30Jrd.rb
@@ -1,92 +1,101 @@
 
         
-                  # The key is untrusted input, so ensure we can't be directed outside
-          # of base_dir inside the repository
-          Gitlab::Utils.check_path_traversal!(file_name)
-    
-      def notification_service
-    @notification_service ||= NotificationService.new
-  end
-end
-
-    
-        def find_by_filename(query)
-      search_filenames(query).map do |filename|
-        Gitlab::Search::FoundBlob.new(blob_filename: filename, project: project, ref: ref, repository: repository)
-      end
+            def error?
+      code == 0 || code != 404 && code != 403 && code >= 400 && code <= 599
     end
     
-        subject.unfold_diff_files([position])
+            css('pre[name]').each do |node|
+          node.before(%(<div class='pre-title'>#{node['name']}</div>))
+        end
+    
+      def serialize_options(resource)
+    methods = resource_class.authentication_keys.dup
+    methods = methods.keys if methods.is_a?(Hash)
+    methods << :password if resource.respond_to?(:password)
+    { methods: methods, only: [:password] }
   end
-end
-
     
-            desc 'Denies an access request for the given user.' do
-          detail 'This feature was introduced in GitLab 8.11.'
-        end
-        params do
-          requires :user_id, type: Integer, desc: 'The user ID of the access requester'
-        end
-        # rubocop: disable CodeReuse/ActiveRecord
-        delete ':id/access_requests/:user_id' do
-          source = find_source(source_type, params[:id])
-          member = source.requesters.find_by!(user_id: params[:user_id])
+          protected
     
-        alias log puts
-    
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
+          def mailer_from(mapping)
+        mailer_sender(mapping, :from)
       end
+    
+    module Devise
+  module Models
+    # Timeoutable takes care of verifying whether a user session has already
+    # expired or not. When a session expires after the configured time, the user
+    # will be asked for credentials again, it means, they will be redirected
+    # to the sign in page.
+    #
+    # == Options
+    #
+    # Timeoutable adds the following options to devise_for:
+    #
+    #   * +timeout_in+: the interval to timeout the user session without activity.
+    #
+    # == Examples
+    #
+    #   user.timedout?(30.minutes.ago)
+    #
+    module Timeoutable
+      extend ActiveSupport::Concern
+    
+          converted_row = row.collect do |field|
+        quote(field)
+      end
+      line = converted_row.join(@column_separator) + @row_separator
+      if @output_encoding
+        line = line.encode(@output_encoding)
+      end
+      @output << line
+    
+      def test_marshal_dump_ivar
+    s = 'data with ivar'
+    s.instance_variable_set(:@t, 42)
+    t = Bug8276.new(s)
+    s = Marshal.dump(t)
+    assert_raise(FrozenError) {Marshal.load(s)}
+  end
+    
+      before :each do
+    @data = '{'a':1234}'
+    @zip = [31, 139, 8, 0, 0, 0, 0, 0, 0, 3, 171, 86, 74, 84, 178, 50,
+            52, 50, 54, 169, 5, 0, 196, 20, 118, 213, 10, 0, 0, 0].pack('C*')
+    @io = StringIO.new @zip
+  end
+    
     end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
-  end
-end
 
     
-    @@ layout
-<html>
-  <head>
-    <title>Super Simple Chat with Sinatra</title>
-    <meta charset='utf-8' />
-    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
-  </head>
-  <body><%= yield %></body>
-</html>
+        gz.read 5
+    gz.pos.should == 5
     
-          def formats
-        super(:markdown)
-      end
-    
-          def title
-        'Latest Changes (Globally)'
-      end
-    
-        # ascii only file names prevent UTF8 issues
-    # when using git repos across operating systems
-    # as this test demonstrates, translation is not
-    # great
-    assert_equal 'm-plus-F', 'μ†ℱ'.to_url
+      def dashboard
+    gon.push(pod_version: pod_version)
   end
     
-    #required to upload bigger binary files
-Gollum::set_git_timeout(120)
-Gollum::set_git_max_filesize(190 * 10**6)
-    
-          # @param type [Symbol]
-      # @param key [Symbol]
-      # @param value [Object] will be stored if the value is currently empty
-      # @return either the old value, if it wasn't nil, or the given value
-      def get_or_set(type, key, value)
-        setting = inheritable_setting.send(type)
-        if value.nil?
-          setting[key]
+          def render_error(error_description, detailed_error=nil)
+        @error_description = error_description
+        @detailed_error = detailed_error
+        if request.format == :mobile
+          render 'api/openid_connect/error/error.mobile', layout: 'application.mobile'
         else
-          setting[key] = value
+          render 'api/openid_connect/error/error', layout: 'with_header_with_footer'
         end
       end
+    end
+  end
+end
+
+    
+        raise ActiveRecord::RecordNotFound unless membership.present?
+    
+        respond_to do |format|
+      format.html
+      format.xml { render :xml => @notifications.to_xml }
+      format.json {
+        render json: render_as_json(@unread_notification_count, @grouped_unread_notification_counts, @notifications)
+      }
+    end
+  end
