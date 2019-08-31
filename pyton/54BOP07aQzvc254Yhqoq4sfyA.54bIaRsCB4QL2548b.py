2ffcade82fd0cf8995d6e2ce77972363db08b98f
SUNsung <weatherius@gmail.com>
2019-08-31 23:30:40 +0300

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2019-08-31 23:30:40

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,133 +1,52 @@
 
         
-            # Sorting the libraries
-    inner_blocks = sorted(blocks[0].split('##'))
-    for i in range(1 , len(inner_blocks)):
-        if inner_blocks[i][0] != '#':
-            inner_blocks[i] = '##' + inner_blocks[i]
-    inner_blocks=''.join(inner_blocks)
+                    elif opt in ('-V', '--version'):
+                # Display version.
+                log.println('you-get:', log.BOLD)
+                log.println('    version:  {}'.format(__version__))
+                if head is not None:
+                    log.println('    branch:   {}\n    commit:   {}'.format(*head))
+                else:
+                    log.println('    branch:   {}\n    commit:   {}'.format('(stable)', '(tag v{})'.format(__version__)))
     
-        if args.invalidation_mode:
-        ivl_mode = args.invalidation_mode.replace('-', '_').upper()
-        invalidation_mode = py_compile.PycInvalidationMode[ivl_mode]
-    else:
-        invalidation_mode = None
+    site = Bigthink()
+download = site.download_by_url
+
     
-        pl[nsstr('anEmptyList')] = NSMutableArray.alloc().init()
+        ep = 'http://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={}'
     
-        def test_imul(self):
-        u = self.type2test([0, 1])
-        u *= 3
-        self.assertEqual(u, self.type2test([0, 1, 0, 1, 0, 1]))
-        u *= 0
-        self.assertEqual(u, self.type2test([]))
+        html = get_content(rebuilt_url(url))
+    info = json.loads(match1(html, r'qualities':({.+?}),''))
+    title = match1(html, r''video_title'\s*:\s*'([^']+)'') or \
+            match1(html, r''title'\s*:\s*'([^']+)'')
+    title = unicodize(title)
     
-            # Test that sequences of unhashable objects can be tested for sameness:
-        self.assertCountEqual([[1, 2], [3, 4], 0], [False, [3, 4], [1, 2]])
-        # Test that iterator of unhashable objects can be tested for sameness:
-        self.assertCountEqual(iter([1, 2, [], 3, 4]),
-                              iter([1, 2, [], 3, 4]))
+        if re.match(r'https?://movie', url):
+        title = match1(html, 'name='description' content='([^']+)')
+        tid = match1(url, 'trailer/(\d+)')
+        real_url = 'https://movie.douban.com/trailer/video_url?tid=%s' % tid
+        type, ext, size = url_info(real_url)
     
-            t = threading.Thread(target=f)
-        t.start()
-        t.join()
-    
-            firstId = events[0][2]
-        self.assertSequenceEqual(
-            [
-                ('Created', ' ', firstId),
-                ('cpython._PySys_ClearAuditHooks', ' ', firstId),
-            ],
-            events,
-        )
-    
-        def run_ignoring_vars(self, predicate, **env_vars):
-        # Runs a subprocess with -E set, even though we're passing
-        # specific environment variables
-        # Logical inversion to match predicate check to a zero return
-        # code indicating success
-        code = 'import sys; sys.stderr.write(str(sys.flags)); sys.exit(not ({}))'.format(predicate)
-        return assert_python_ok('-E', '-c', code, **env_vars)
-    
-          # We want to ensure that headers appear in the right order:
-      # 1) for foo.cc, foo.h  (preferred location)
-      # 2) c system files
-      # 3) cpp system files
-      # 4) for foo.cc, foo.h  (deprecated location)
-      # 5) other google headers
-      #
-      # We classify each include statement as one of those 5 types
-      # using a number of techniques. The include_state object keeps
-      # track of the highest type seen, and complains if we see a
-      # lower type after that.
-      error_message = include_state.CheckNextIncludeOrder(
-          _ClassifyInclude(fileinfo, include, is_system))
-      if error_message:
-        error(filename, linenum, 'build/include_order', 4,
-              '%s. Should be: %s.h, c system, c++ system, other.' %
-              (error_message, fileinfo.BaseName()))
-      canonical_include = include_state.CanonicalizeAlphabeticalOrder(include)
-      if not include_state.IsInAlphabeticalOrder(
-          clean_lines, linenum, canonical_include):
-        error(filename, linenum, 'build/include_alpha', 4,
-              'Include '%s' not in alphabetical order' % include)
-      include_state.SetLastHeader(canonical_include)
-    
-    import os
-import sys
-from itertools import chain, groupby
-from collections import OrderedDict
-    
-        def _freeze(self, final_text, err=False):
-        '''Stop spinner, compose last frame and 'freeze' it.'''
-        if not final_text:
-            final_text = ''
-        target = self.stderr if err else self.stdout
-        if target.closed:
-            target = sys.stderr if err else sys.stdout
-        text = to_text(final_text)
-        last_frame = self._compose_out(text, mode='last')
-        self._last_frame = decode_output(last_frame, target_stream=target)
+    site_info = 'ifeng.com'
+download = ifeng_download
+download_playlist = playlist_not_supported('ifeng')
+
     
     
-def test__error_2():
-    v = Validator(schema={'foo': {'keysrules': {'type': 'integer'}}})
-    v.document = {'foo': {'0': 'bar'}}
-    v._error('foo', errors.KEYSRULES, ())
-    error = v._errors[0]
-    assert error.document_path == ('foo',)
-    assert error.schema_path == ('foo', 'keysrules')
-    assert error.code == 0x83
-    assert error.rule == 'keysrules'
-    assert error.constraint == {'type': 'integer'}
-    assert error.value == {'0': 'bar'}
-    assert error.info == ((),)
-    assert error.is_group_error
-    assert not error.is_logic_error
+@gen.engine
+def e1():
+    for i in range(10):
+        yield gen.Task(e2)
     
-        class MyValidator(Validator):
-        def _validate_root_doc(self, root_doc, field, value):
-            ''' {'type': 'boolean'} '''
-            if 'sub' not in self.root_document or len(self.root_document['sub']) != 2:
-                self._error(field, 'self.context is not the root doc!')
     
-            :param fields: The fields to send as a list of (fieldname, value)
-                       tuples.
-        :param files: The files to send as a list of (fieldname, filename,
-                      file_bytes) tuple.
-        '''
-        # Adapted from packaging, which in turn was adapted from
-        # http://code.activestate.com/recipes/146306
+if __name__ == '__main__':
+    main()
+
     
-        def _env_setup(self, initial_qpos):
-        for name, value in initial_qpos.items():
-            self.sim.data.set_joint_qpos(name, value)
-        utils.reset_mocap_welds(self.sim)
-        self.sim.forward()
-    
-        With inspiration from:
-    https://github.com/dennybritz/reinforcement-learning/blob/master/lib/envs/cliff_walking.py
-    
-            self.action_space = spaces.Box(low=np.array([-self.bounds]), high=np.array([self.bounds]),
-                                       dtype=np.float32)
-        self.observation_space = spaces.Discrete(4)
+        @gen_test
+    def test_websocket_close_buffered_data(self):
+        ws = yield websocket_connect('ws://127.0.0.1:%d/echo' % self.get_http_port())
+        ws.write_message('hello')
+        ws.write_message('world')
+        # Close the underlying stream.
+        ws.stream.close()
