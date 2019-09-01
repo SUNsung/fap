64cf787d1047d154793743dec43f9402468d29c4
SUNsung <weatherius@gmail.com>
2019-09-01 05:40:00 +0300

    Rewrite [369Jxba1ZJyMk36Ru2YzTzYTc236.s8QdbzPoFY36rMH.cpp] File in: 2019-09-01 05:40:00

--- cccccc/369Jxba1ZJyMk36Ru2YzTzYTc236.s8QdbzPoFY36rMH.cpp
+++ cccccc/369Jxba1ZJyMk36Ru2YzTzYTc236.s8QdbzPoFY36rMH.cpp
@@ -1,311 +1,246 @@
 
         
         
-    {  for (auto &BB : *F)
-    for (auto &I : BB)
-      emitter.visit(&I);
-}
-    
-      auto getGenericArgsTypeListForContext =
-    [&](const ContextDescriptor *context) -> NodePointer {
-      if (demangledGenerics.empty())
-        return nullptr;
-    }
-    
-      /// Get the execution count corresponding to \p Node from a profile, if one
-  /// is available.
-  ProfileCounter getExecutionCount(ASTNode Node);
-    
-    KNOWN_STDLIB_TYPE_DECL(Bool, NominalTypeDecl, 0)
-    
-    static llvm::Function *
-getAccessorForComputedComponent(IRGenModule &IGM,
-                                const KeyPathPatternComponent &component,
-                                KeyPathAccessor whichAccessor,
-                                GenericEnvironment *genericEnv,
-                                ArrayRef<GenericRequirement> requirements,
-                                bool hasSubscriptIndices) {
-  SILFunction *accessor;
-  switch (whichAccessor) {
-  case Getter:
-    accessor = component.getComputedPropertyGetter();
-    break;
-  case Setter:
-    accessor = component.getComputedPropertySetter();
-    break;
-  case Equals:
-    accessor = component.getSubscriptIndexEquals();
-    break;
-  case Hash:
-    accessor = component.getSubscriptIndexHash();
-    break;
-  }
-  
-  // If the accessor is not generic, and locally available, we can use it as is.
-  // If it's only externally available, we need a local thunk to relative-
-  // reference.
-  if (requirements.empty() &&
-      !isAvailableExternally(accessor->getLinkage()) &&
-      &IGM == IGM.IRGen.getGenModule(accessor)) {
-    return IGM.getAddrOfSILFunction(accessor, NotForDefinition);
-  }
-  auto accessorFn = IGM.getAddrOfSILFunction(accessor, NotForDefinition);
-  
-  auto accessorFnTy = cast<llvm::FunctionType>(
-    accessorFn->getType()->getPointerElementType());;
-  
-  // Otherwise, we need a thunk to unmarshal the generic environment from the
-  // argument area. It'd be nice to have a good way to represent this
-  // directly in SIL, of course...
-  const char *thunkName;
-  unsigned numArgsToForward;
-    }
-    
-    
-    {    // Forbid the parameter to be an autoclosure.
-    if (param->isAutoClosure()) {
-      ctx.Diags.diagnose(attr->getLocation(),
-                         diag::function_builder_parameter_autoclosure,
-                         nominal->getFullName());
-      mutableAttr->setInvalid();
-      return Type();
-    }
-  }
-    
-    
-    {  ReadBytesResult readBytes(RemoteAddress Addr, uint64_t Size) {
-    if (!isAddressValid(Addr, Size))
-      return ReadBytesResult(nullptr, [](const void *) {});
-    return ReadBytesResult((const void *)(Addr.getAddressData()),
-                           [](const void *) {});
-  }
+    {    QString appName;
+    QIcon appIcon;
+    QIcon trayAndWindowIcon;
+    QString titleAddText;
 };
     
-    	EditorFileDialog *emission_file_dialog;
-	SceneTreeDialog *emission_tree_dialog;
-    
-    	void invert();
-	void contrast();
-	Color inverted() const;
-	Color contrasted() const;
-    
-    #define ERR_BREAK(m_cond)                                                                                              \
-	{                                                                                                                  \
-		if (unlikely(m_cond)) {                                                                                        \
-			_err_print_error(FUNCTION_STR, __FILE__, __LINE__, 'Condition ' ' _STR(m_cond) ' ' is true. Breaking..:'); \
-			break;                                                                                                     \
-		}                                                                                                              \
-		_err_error_exists = false;                                                                                     \
-	}
-    
-    
-    {		int face_index;
-	};
-    
-    			if (path[i] == ':' || path[i] == 0) {
-    }
-    
-    				case ShaderLanguage::TYPE_SAMPLER2D: {
-    }
-    
-      // check if form is already defined
-  for (i = 0; i < formIDLen; ++i) {
-    if (formIDs[i].num == id.num && formIDs[i].gen == id.gen) {
-      return;
-    }
-  }
-    
-      for (i = 0; i < intervals.getLength(); i++) {
-    interval = (Interval *) intervals.get(i);
-    const int base = interval->base;
-    prefixLength = interval->prefix->getLength();
-    if (label->cmpN(interval->prefix, prefixLength) != 0)
-      continue;
-    }
-    
-    private:
-    
-    
-    {  Object encryptDict;
-  XpdfSecurityHandler *xsh;
-  void *docData;
-  int permFlags;
-  Guchar fileKey[16];
-  int fileKeyLength;
-  int encVersion;
-  CryptAlgorithm encAlgorithm;
-  GBool ok;
-};
-#endif // ENABLE_PLUGINS
-    
-      newsound->kind = kind;
-  if (fileName) {
-    newsound->fileName = fileName->copy();
-  }
-  newsound->samplingRate = samplingRate;
-  newsound->channels = channels;
-  newsound->bitsPerSample = bitsPerSample;
-  newsound->encoding = encoding;
-    
-      SoundKind getSoundKind() { return kind; }
-  GooString *getFileName() { return fileName; }
-  double getSamplingRate() { return samplingRate; }
-  int getChannels() { return channels; }
-  int getBitsPerSample() { return bitsPerSample; }
-  SoundEncoding getEncoding() { return encoding; }
-    
-      /*
-   * Create a FixedStringMap from the builder.
-   */
-  void create(FSMap& map);
-    
-    #include <boost/ptr_container/ptr_vector.hpp>
-    
-    template<class T>
-Variant nativePropHandlerSet(const Object& obj,
-                             const String& name,
-                             const Variant& value) {
-  CHECK_NATIVE_PROP_SUPPORTED(name, 'set')
-  return T::setProp(obj, name, value);
-}
-    
-      // If the RDS entry isn't initialized, mark it as initialized and store a null
-  // pointer in it. The setter will allocate the cache and update the pointer.
-  auto const sf = checkRDSHandleInitialized(v, handle);
-  ifThen(
-    v, CC_NE, sf,
-    [&](Vout& v) {
-      v << storeqi{0, getHandleAddr(handle)};
-      markRDSHandleInitialized(v, handle);
-    }
-  );
-    
-    TEST_F(ProxygenTransportBasicTest, unsupported_method) {
-  auto req = getRequest(HTTPMethod::UNSUB);
-  EXPECT_CALL(m_server, onRequestError(_));
-  EXPECT_CALL(m_txn, sendHeaders(IsResponseStatusCode(400)));
-  EXPECT_CALL(m_txn, sendEOM());
-  m_transport->onHeadersComplete(std::move(req));
-  m_transport->onEOM();
-}
-    
-      const Class* getClass() const { return m_cls; }
-  void setClass(const Class* cls) {
-    assertx(cls != nullptr);
-    assertx(m_cls == nullptr);
-    m_cls = cls;
-  }
-    
-    
-    {  for (auto numProcessed = 0; numProcessed < numPaths; ++numProcessed) {
-    size_t i;
-    result_q.blockingRead(i);
-    HackCFactsExtractor::merge_result(
-      workerResults[i],
-      outResArr,
-      StrNR(pathList.at(i)));
-  }
-  dispatcher.waitEmpty();
-}
-    
-    
-    {}
-    
-    TEST_F(DBTestXactLogIterator, TransactionLogIteratorCheckAfterRestart) {
-  do {
-    Options options = OptionsForLogIterTest();
-    DestroyAndReopen(options);
-    Put('key1', DummyString(1024));
-    Put('key2', DummyString(1023));
-    dbfull()->Flush(FlushOptions());
-    Reopen(options);
-    auto iter = OpenTransactionLogIter(0);
-    ExpectRecords(2, iter);
-  } while (ChangeCompactOptions());
-}
-    
-      ~BlobIndexCompactionFilter() override {
-    RecordTick(statistics_, BLOB_DB_BLOB_INDEX_EXPIRED_COUNT, expired_count_);
-    RecordTick(statistics_, BLOB_DB_BLOB_INDEX_EXPIRED_SIZE, expired_size_);
-    RecordTick(statistics_, BLOB_DB_BLOB_INDEX_EVICTED_COUNT, evicted_count_);
-    RecordTick(statistics_, BLOB_DB_BLOB_INDEX_EVICTED_SIZE, evicted_size_);
-  }
-    
-    uint64_t WriteController::NowMicrosMonotonic(Env* env) {
-  return env->NowNanos() / std::milli::den;
-}
-    
-      // atomic write
-  WriteBatch batch;
-  batch.Put(handles[0], Slice('key2'), Slice('value2'));
-  batch.Put(handles[1], Slice('key3'), Slice('value3'));
-  batch.Delete(handles[0], Slice('key'));
-  s = db->Write(WriteOptions(), &batch);
-  assert(s.ok());
-    
-      bool FilterMergeOperand(int level, const rocksdb::Slice& key,
-                          const rocksdb::Slice& existing_value) const override {
-    fprintf(stderr, 'FilterMerge(%s)\n', key.ToString().c_str());
-    ++merge_count_;
-    return existing_value == 'bad';
-  }
-    
-    #ifndef ROCKSDB_LITE
-    
-    
-    {}  // namespace
-    
-    #pragma once
-    
-    
-    {
-    {
-    {  lua_State* lua_state_;
-};
-}  // namespace lua
-}  // namespace rocksdb
-#endif  // LUA
+    #endif // BITCOIN_QT_OPENURIDIALOG_H
 
     
     
-    {  uint64_t fp_[detail::poly_size(BITS)];
-};
-    
-    
-    {  char* mapStart = static_cast<char*>(mapStart_) + offset;
-  PLOG_IF(WARNING, ::madvise(mapStart, length, advice)) << 'madvise';
+    {    secp256k1_scalar_set_b32(&sec, seckey, &overflow);
+    /* Fail if the secret key is invalid. */
+    if (!overflow && !secp256k1_scalar_is_zero(&sec)) {
+        unsigned char nonce32[32];
+        unsigned int count = 0;
+        secp256k1_scalar_set_b32(&msg, msg32, NULL);
+        while (1) {
+            ret = noncefp(nonce32, msg32, seckey, NULL, (void*)noncedata, count);
+            if (!ret) {
+                break;
+            }
+            secp256k1_scalar_set_b32(&non, nonce32, &overflow);
+            if (!secp256k1_scalar_is_zero(&non) && !overflow) {
+                if (secp256k1_ecdsa_sig_sign(&ctx->ecmult_gen_ctx, &r, &s, &sec, &msg, &non, &recid)) {
+                    break;
+                }
+            }
+            count++;
+        }
+        memset(nonce32, 0, 32);
+        secp256k1_scalar_clear(&msg);
+        secp256k1_scalar_clear(&non);
+        secp256k1_scalar_clear(&sec);
+    }
+    if (ret) {
+        secp256k1_ecdsa_recoverable_signature_save(signature, &r, &s, recid);
+    } else {
+        memset(signature, 0, sizeof(*signature));
+    }
+    return ret;
 }
     
-    template <
-    typename Key,
-    typename Hasher = f14::DefaultHasher<Key>,
-    typename KeyEqual = f14::DefaultKeyEqual<Key>,
-    typename Alloc = f14::DefaultAlloc<Key>>
-class F14VectorSet;
+    const std::vector<std::string>& UniValue::getKeys() const
+{
+    if (typ != VOBJ)
+        throw std::runtime_error('JSON value is not an object as expected');
+    return keys;
+}
     
-    #if __linux__
-    if (noTransparentTls_) {
-      // Ignore return value, errors are ok
-      netops::setsockopt(fd, SOL_SOCKET, SO_NO_TRANSPARENT_TLS, nullptr, 0);
+    
+    {    BOOST_CHECK_EQUAL(vals[1].get_int(), 10);
+    BOOST_CHECK_THROW(vals[1].get_bool(), std::runtime_error);
+}
+    
+    namespace sha256_sse4
+{
+void Transform(uint32_t* s, const unsigned char* chunk, size_t blocks)
+{
+    static const uint32_t K256 alignas(16) [] = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+        0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+        0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+    };
+    static const uint32_t FLIP_MASK alignas(16) [] = {0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f};
+    static const uint32_t SHUF_00BA alignas(16) [] = {0x03020100, 0x0b0a0908, 0xffffffff, 0xffffffff};
+    static const uint32_t SHUF_DC00 alignas(16) [] = {0xffffffff, 0xffffffff, 0x03020100, 0x0b0a0908};
+    uint32_t a, b, c, d, f, g, h, y0, y1, y2;
+    uint64_t tbl;
+    uint64_t inp_end, inp;
+    uint32_t xfer alignas(16) [4];
     }
-#endif
+    }
     
-    /**
- * Arena that uses the system allocator (malloc / free)
- */
-class SysArena : public Arena<SysAllocator<void>> {
+    void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
+                                        std::string* dst) const {
+  // We rely on the fact that the code in table.cc does not mind us
+  // adjusting keys[].
+  Slice* mkey = const_cast<Slice*>(keys);
+  for (int i = 0; i < n; i++) {
+    mkey[i] = ExtractUserKey(keys[i]);
+    // TODO(sanjay): Suppress dups?
+  }
+  user_policy_->CreateFilter(keys, n, dst);
+}
+    
+    // Return the name of the log file with the specified number
+// in the db named by 'dbname'.  The result will be prefixed with
+// 'dbname'.
+extern std::string LogFileName(const std::string& dbname, uint64_t number);
+    
+    class StdoutPrinter : public WritableFile {
  public:
-  explicit SysArena(
-      size_t minBlockSize = kDefaultMinBlockSize,
-      size_t sizeLimit = kNoSizeLimit,
-      size_t maxAlign = kDefaultMaxAlign)
-      : Arena<SysAllocator<void>>({}, minBlockSize, sizeLimit, maxAlign) {}
+  virtual Status Append(const Slice& data) {
+    fwrite(data.data(), 1, data.size(), stdout);
+    return Status::OK();
+  }
+  virtual Status Close() { return Status::OK(); }
+  virtual Status Flush() { return Status::OK(); }
+  virtual Status Sync() { return Status::OK(); }
+  virtual std::string GetName() const { return '[stdout]'; }
 };
     
-    struct UniformSubset {
-  UniformSubset(uint64_t seed, size_t subsetSize, size_t stepsBetweenSelect)
-      : uniform_(DeterministicSchedule::uniform(seed)),
-        subsetSize_(subsetSize),
-        stepsBetweenSelect_(stepsBetweenSelect),
-        stepsLeft_(0) {}
+    #include 'rect.h'    // for TBOX
+#include 'strngs.h'  // for STRING
+    
+    
+// Returns the median value of the vector, given that the values are
+// circular, with the given modulus. Values may be signed or unsigned,
+// eg range from -pi to pi (modulus 2pi) or from 0 to 2pi (modulus 2pi).
+// NOTE that the array is shuffled, but the time taken is linear.
+// An assumption is made that most of the values are spread over no more than
+// half the range, but wrap-around is accounted for if the median is near
+// the wrap-around point.
+// Cannot be a member of GenericVector, as it makes heavy used of LLSQ.
+// T must be an integer or float/double type.
+template<typename T> T MedianOfCircularValues(T modulus, GenericVector<T>* v) {
+  LLSQ stats;
+  T halfrange = static_cast<T>(modulus / 2);
+  int num_elements = v->size();
+  for (int i = 0; i < num_elements; ++i) {
+    stats.add((*v)[i], (*v)[i] + halfrange);
+  }
+  bool offset_needed = stats.y_variance() < stats.x_variance();
+  if (offset_needed) {
+    for (int i = 0; i < num_elements; ++i) {
+      (*v)[i] += halfrange;
+    }
+  }
+  int median_index = v->choose_nth_item(num_elements / 2);
+  if (offset_needed) {
+    for (int i = 0; i < num_elements; ++i) {
+      (*v)[i] -= halfrange;
+    }
+  }
+  return (*v)[median_index];
+}
+    
+    // Computes the histogram for the given image rectangle, and the given
+// single channel. Each channel is always one byte per pixel.
+// Histogram is always a kHistogramSize(256) element array to count
+// occurrences of each pixel value.
+void HistogramRect(Pix* src_pix, int channel,
+                   int left, int top, int width, int height,
+                   int* histogram);
+    
+      /* Reject modes generated after QUALITY_ACCEPT but before MINIMAL_REJ accep*/
+  R_DOC_REJ,    // TEMP Document rejection
+  R_BLOCK_REJ,  // TEMP Block rejection
+  R_ROW_REJ,    // TEMP Row rejection
+  R_UNLV_REJ,   // TEMP ~ turned to - or ^ turned to space
+    
+    namespace tesseract {
+    }
+    
+      std::string fname = TableFileName(dbname, meta->number);
+  if (iter->Valid()) {
+    WritableFile* file;
+    s = env->NewWritableFile(fname, &file);
+    if (!s.ok()) {
+      return s;
+    }
+    }
+    
+      fname = LogFileName('foo', 192);
+  ASSERT_EQ('foo/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(192, number);
+  ASSERT_EQ(kLogFile, type);
+    
+    struct TableAndFile {
+  RandomAccessFile* file;
+  Table* table;
+};
+    
+      // Set the count for the number of entries in the batch.
+  static void SetCount(WriteBatch* batch, int n);
+    
+    namespace leveldb {
+    }
+    
+      // Delete the specified directory.
+  virtual Status DeleteDir(const std::string& dirname) = 0;
+    
+    #if LEVELDB_HAS_PORT_CONFIG_H
+#include 'port/port_config.h'
+#endif  // LEVELDB_HAS_PORT_CONFIG_H
+    
+      // Iterator interface methods
+  bool Valid() const { return valid_; }
+  Slice key() const {
+    assert(Valid());
+    return key_;
+  }
+  Slice value() const {
+    assert(Valid());
+    return iter_->value();
+  }
+  // Methods below require iter() != nullptr
+  Status status() const {
+    assert(iter_);
+    return iter_->status();
+  }
+  void Next() {
+    assert(iter_);
+    iter_->Next();
+    Update();
+  }
+  void Prev() {
+    assert(iter_);
+    iter_->Prev();
+    Update();
+  }
+  void Seek(const Slice& k) {
+    assert(iter_);
+    iter_->Seek(k);
+    Update();
+  }
+  void SeekToFirst() {
+    assert(iter_);
+    iter_->SeekToFirst();
+    Update();
+  }
+  void SeekToLast() {
+    assert(iter_);
+    iter_->SeekToLast();
+    Update();
+  }
+    
+    namespace leveldb {
     }
