2686656a95b60458b991a978511c3525aa81c29a
SUNsung <weatherius@gmail.com>
2019-09-01 14:31:20 +0300

    Rewrite [588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py] File in: 2019-09-01 14:31:19

--- pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
+++ pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
@@ -1,223 +1,213 @@
 
         
-            def process_options(self, args, opts):
-        ScrapyCommand.process_options(self, args, opts)
-        try:
-            opts.spargs = arglist_to_dict(opts.spargs)
-        except ValueError:
-            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
-        if opts.output:
-            if opts.output == '-':
-                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
-            else:
-                self.settings.set('FEED_URI', opts.output, priority='cmdline')
-            feed_exporters = without_none_values(self.settings.getwithbase('FEED_EXPORTERS'))
-            valid_output_formats = feed_exporters.keys()
-            if not opts.output_format:
-                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
-            if opts.output_format not in valid_output_formats:
-                raise UsageError('Unrecognized output format '%s', set one'
-                                 ' using the '-t' switch or as a file extension'
-                                 ' from the supported list %s' % (opts.output_format,
-                                                                  tuple(valid_output_formats)))
-            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
-    
-        def syntax(self):
-        return '[options]'
-    
-        if len(sys.argv) > 1:
-        ip = sys.argv[1]
-    else:
-        ip = '46.134.208.94'
-        ip = '2001:ee0:3203:a::12'
-        print('Usage: check_ip.py [ip] [top_domain] [wait_time=0]')
-    print('test ip:%s' % ip)
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licence]
+                '''
+        assert not self.status.time_started
     
     
-def get_params_dict():
-    '''以字典形式获取所有 trainable 参数'''
-    param_dict = dict()
-    for var in tf.trainable_variables():
-        param_dict[var.name] = {'shape': list(map(int, var.shape)),
-                                'number': int(reduce(mul, var.shape, 1))}
-    return param_dict
-    
-        @certbot_util.patch_get_utility()
-    def test_select_cancel(self, mock_util):
-        mock_util().checklist.return_value = (display_util.CANCEL, 'whatever')
-        vhs = select_vhost_multiple([self.vhosts[2], self.vhosts[3]])
-        self.assertFalse(vhs)
-    
-            mz = self.dns.managedZones()  # managedZones | pylint: disable=no-member
-        for zone_name in zone_dns_name_guesses:
-            try:
-                request = mz.list(project=self.project_id, dnsName=zone_name + '.')
-                response = request.execute()
-                zones = response['managedZones']
-            except googleapiclient_errors.Error as e:
-                raise errors.PluginError('Encountered error finding managed zone: {0}'
-                                         .format(e))
-    
-        def test_find_domain(self):
-        # _query_soa | pylint: disable=protected-access
-        self.rfc2136_client._query_soa = mock.MagicMock(side_effect=[False, False, True])
-    
-        def _start_nginx(self, default_server):
-        self.nginx_config = config.construct_nginx_config(
-            self.nginx_root, self.webroot, self.http_01_port, self.tls_alpn_01_port,
-            self.other_port, default_server, wtf_prefix=self.worker_id)
-        with open(self.nginx_config_path, 'w') as file:
-            file.write(self.nginx_config)
-    
-            responses.add(
-            method=responses.POST,
-            url='http://example.slack.com/response_url',
-            body='{'ok': true}',
-            status=200,
-            content_type='application/json',
-        )
-    
-    
-@implements_to_string
-class Problem(object):
-    
-            return [
-            Problem(
-                message,
-                url=absolute_uri('/manage/queue/'),
-            ),
+class PyTest(TestCommand):
+    # `$ python setup.py test' simply installs minimal requirements
+    # and runs the tests with no fancy stuff like parallel execution.
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            '--doctest-modules', '--verbose',
+            './httpie', './tests'
         ]
-
-    
-            for job in job_list:
-            logger.debug('Sending scheduled job %s with payload %r', job.name, job.payload)
-            app.send_task(job.name, kwargs=job.payload)
-            job.delete()
-
+        self.test_suite = True
     
     
-def _GetRequiredNamespaceImport( completion ):
-  if ( 'extra_data' not in completion
-       or 'required_namespace_import' not in completion[ 'extra_data' ] ):
-    return None
-  return completion[ 'extra_data' ][ 'required_namespace_import' ]
-    
-    
-  def NullCharactersInExtraInfoAndDocString_test( self ):
-    self._Check( '0', {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'detailed_info':   'DETAILED\x00INFO',
-      'extra_data': {
-        'doc_string': 'DOC\x00STRING'
-      },
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'info'     : 'DETAILEDINFO\nDOCSTRING',
-      'equal'    : 1,
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '0',
-    } )
-    
-      def ServerResponse( *args ):
-    return {
-      'completions': [ {
-        'insertion_text': 'insertion_text',
-        'menu_text': 'menu_text',
-        'extra_menu_info': 'extra_menu_info',
-        'detailed_info': 'detailed_info',
-        'kind': 'kind',
-        'extra_data': {
-           'doc_string': 'doc_string'
-        }
-      } ],
-      'completion_start_column': 3,
-      'errors': [ {
-        'exception': {
-           'TYPE': 'Exception'
-        },
-        'message': 'message',
-        'traceback': 'traceback'
-      } ]
+def main():
+    package_meta_map = {
+        package_name: get_package_meta(package_name)
+        for package_name in PACKAGES
     }
+    httpie_meta = package_meta_map.pop('httpie')
+    print()
+    print('  url '{url}''.format(url=httpie_meta['url']))
+    print('  sha256 '{sha256}''.format(sha256=httpie_meta['sha256']))
+    print()
+    for dep_meta in package_meta_map.values():
+        print('  resource '{name}' do'.format(name=dep_meta['name']))
+        print('    url '{url}''.format(url=dep_meta['url']))
+        print('    sha256 '{sha256}''.format(sha256=dep_meta['sha256']))
+        print('  end')
+        print('')
     
-      _assert_accepts( f, { 'text' : 'This is an IMPORTANT taco',
-                        'kind' : 'WARNING' } )
-  _assert_rejects( f, { 'text' : 'This taco will NOT be shown',
-                        'kind' : 'ERROR' } )
+    import json
+import platform
+import sys
+import ssl
     
-        # Parse and validate the field names.  Validation serves two purposes,
-    # generating informative error messages and preventing template injection attacks.
-    if isinstance(field_names, basestring):
-        field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
-    field_names = tuple(map(str, field_names))
-    for name in (typename,) + field_names:
-        if not all(c.isalnum() or c=='_' for c in name):
-            raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
-        if _iskeyword(name):
-            raise ValueError('Type names and field names cannot be a keyword: %r' % name)
-        if name[0].isdigit():
-            raise ValueError('Type names and field names cannot start with a number: %r' % name)
-    seen_names = set()
-    for name in field_names:
-        if name.startswith('_'):
-            raise ValueError('Field names cannot start with an underscore: %r' % name)
-        if name in seen_names:
-            raise ValueError('Encountered duplicate field name: %r' % name)
-        seen_names.add(name)
     
-        Args:
-        pending_work_items: A dict mapping work ids to _WorkItems e.g.
-            {5: <_WorkItem...>, 6: <_WorkItem...>, ...}
-        work_ids: A queue.Queue of work ids e.g. Queue([5, 6, ...]). Work ids
-            are consumed and the corresponding _WorkItems from
-            pending_work_items are transformed into _CallItems and put in
-            call_queue.
-        call_queue: A multiprocessing.Queue that will be filled with _CallItems
-            derived from _WorkItems.
-    '''
-    while True:
-        if call_queue.full():
-            return
-        try:
-            work_id = work_ids.get(block=False)
-        except queue.Empty:
-            return
-        else:
-            work_item = pending_work_items[work_id]
+def default_hooks():
+    return {event: [] for event in HOOKS}
+    
+    elif is_py3:
+    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
+    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
+    from http import cookiejar as cookielib
+    from http.cookies import Morsel
+    from io import StringIO
+    # Keep OrderedDict for backwards compatibility.
+    from collections import OrderedDict
+    from collections.abc import Callable, Mapping, MutableMapping
+    
+        def __delitem__(self, key):
+        del self._store[key.lower()]
+    
+    try:
+    from urllib3.contrib.socks import SOCKSProxyManager
+except ImportError:
+    def SOCKSProxyManager(*args, **kwargs):
+        raise InvalidSchema('Missing dependencies for SOCKS support.')
+    
+    import urllib3
+import chardet
+import warnings
+from .exceptions import RequestsDependencyWarning
+    
+        author = proj_info['author'],
+    author_email = proj_info['author_email'],
+    url = proj_info['url'],
+    license = proj_info['license'],
+    
+        if not opts and not args:
+        # Display help.
+        print(_help)
+        # Enter GUI mode.
+        #from .gui import gui_main
+        #gui_main()
+    else:
+        conf = {}
+        for opt, arg in opts:
+            if opt in ('-h', '--help'):
+                # Display help.
+                print(_help)
+    
+    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
+    host = r1(r'host=([^&]*)', html)
+    assert host
+    type = r1(r'videofiletype=([^&]*)', html)
+    assert type
+    vid = r1(r'&stream_name=([^&]*)', html)
+    assert vid
+    dir_str = r1(r'&dir=([^&]*)', html).strip()
+    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
+    _, ext, size = url_info(url)
+    print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+            link_list = []
+    
+    __all__ = ['cbs_download']
+    
+            url = ep_url.format(self.vid)
+        meta = json.loads(get_content(url))
+        streams = meta['playlist']
+        for stream in streams:
+            definition = stream['code']
+            for s in stream['playinfo']:
+                codec = 'h' + s['codec'][2:]
+                # h.264 -> h264
+                for st in self.__class__.stream_types:
+                    s_id = '{}_{}'.format(definition, codec)
+                    if codec == 'h264':
+                        s_id = definition
+                    if s_id == st['id']:
+                        clear_info = self.__class__.dec_playinfo(s, self.__class__.coeff)
+                        cdn_list = self.__class__.get_cdninfo(clear_info['hashid'])
+                        base_url = cdn_list[0]
+                        vf = urllib.parse.quote(s['vf'])
+                        video_size = int(s['filesize'])
+                        token = urllib.parse.quote(base64.b64encode(clear_info['token'].encode('utf8')))
+                        video_url = '{}?token={}&vf={}'.format(base_url, token, vf)
+                        self.streams[s_id] = dict(size=video_size, src=[video_url], container='mp4')
+    
+    
+site_info = 'huomao.com'
+download = huomaotv_download
+download_playlist = playlist_not_supported('huomao')
+
+    
+        scores = dets[:, 4]
+    order = np.argsort(-scores)
+    
+            # 3. remove predicted boxes with either height or width < min_size
+        keep = _filter_boxes(proposals, min_size, im_info)
+        proposals = proposals[keep, :]
+        scores = scores[keep]
+    
+        def test_gpu_preserves_vals_at_odd_inds(self):
+        X = np.array([0, 1, 2, 3, 4], dtype=np.float32)
+        Y_exp = np.array([0, 1, 0, 3, 0], dtype=np.float32)
+        Y_act = self._run_zero_even_op_gpu(X)
+        np.testing.assert_allclose(Y_act[1::2], Y_exp[1::2])
+    
+    
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Convert a COCO pre-trained model for use with Cityscapes')
+    parser.add_argument(
+        '--coco_model', dest='coco_model_file_name',
+        help='Pretrained network weights file path',
+        default=None, type=str)
+    parser.add_argument(
+        '--convert_func', dest='convert_func',
+        help='Blob conversion function',
+        default='cityscapes_to_coco', type=str)
+    parser.add_argument(
+        '--output', dest='out_file_name',
+        help='Output file path',
+        default=None, type=str)
+    
+        def test_renamed_key_from_list(self):
+        # You should see logger messages like:
+        #  'Key EXAMPLE.RENAMED.KEY was renamed to EXAMPLE.KEY;
+        #  please update your config'
+        opts = ['EXAMPLE.RENAMED.KEY', 'foobar']
+        with self.assertRaises(AttributeError):
+            _ = cfg.EXAMPLE.RENAMED.KEY  # noqa
+        with self.assertRaises(KeyError):
+            core_config.merge_cfg_from_list(opts)
+    
+    '''Primitives for running multiple single-GPU jobs in parallel over subranges of
+data. These are used for running multi-GPU inference. Subprocesses are used to
+avoid the GIL since inference may involve non-trivial amounts of Python code.
+'''
+    
+        # Load the Cityscapes eval script *after* setting the required env vars,
+    # since the script reads their values into global variables (at load time).
+    import cityscapesscripts.evaluation.evalInstanceLevelSemanticLabeling \
+        as cityscapes_eval
+    
+    # coco (val5k)
+# INFO roidb.py: 220: 1        person: 21296
+# INFO roidb.py: 220: 2       bicycle: 628
+# INFO roidb.py: 220: 3           car: 3818
+# INFO roidb.py: 220: 4    motorcycle: 732
+# INFO roidb.py: 220: 5      airplane: 286 <------ irrelevant
+# INFO roidb.py: 220: 6           bus: 564
+# INFO roidb.py: 220: 7         train: 380
+# INFO roidb.py: 220: 8         truck: 828
+    
+    
+def datasets():
+    '''Retrieve the list of available dataset names.'''
+    return _DATASETS.keys()
+    
+    
+def _scale_enum(anchor, scales):
+    '''Enumerate a set of anchors for each scale wrt an anchor.'''
+    w, h, x_ctr, y_ctr = _whctrs(anchor)
+    ws = w * scales
+    hs = h * scales
+    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+    return anchors
+
+    
+    import os
+import sys
+import codecs
+import re
