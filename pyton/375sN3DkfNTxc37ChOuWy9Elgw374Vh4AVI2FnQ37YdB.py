5758683634747931a7953290ffff2b200789b46f
SUNsung <weatherius@gmail.com>
2019-09-01 04:00:41 +0300

    Rewrite [375sN3DkfNTxc37ChOuWy9Elgw374Vh4AVI2FnQ37YdB.py] File in: 2019-09-01 04:00:40

--- pyton/375sN3DkfNTxc37ChOuWy9Elgw374Vh4AVI2FnQ37YdB.py
+++ pyton/375sN3DkfNTxc37ChOuWy9Elgw374Vh4AVI2FnQ37YdB.py
@@ -1,78 +1,39 @@
 
         
-        # Plot the results (= shape of the data points cloud)
-plt.figure(1)  # two clusters
-plt.title('Outlier detection on a real data set (boston housing)')
-plt.scatter(X1[:, 0], X1[:, 1], color='black')
-bbox_args = dict(boxstyle='round', fc='0.8')
-arrow_args = dict(arrowstyle='->')
-plt.annotate('several confounded points', xy=(24, 19),
-             xycoords='data', textcoords='data',
-             xytext=(13, 10), bbox=bbox_args, arrowprops=arrow_args)
-plt.xlim((xx1.min(), xx1.max()))
-plt.ylim((yy1.min(), yy1.max()))
-plt.legend((legend1_values_list[0].collections[0],
-            legend1_values_list[1].collections[0],
-            legend1_values_list[2].collections[0]),
-           (legend1_keys_list[0], legend1_keys_list[1], legend1_keys_list[2]),
-           loc='upper center',
-           prop=matplotlib.font_manager.FontProperties(size=12))
-plt.ylabel('accessibility to radial highways')
-plt.xlabel('pupil-teacher ratio by town')
-    
-    from sklearn import datasets, cluster
-from sklearn.feature_extraction.image import grid_to_graph
-    
-    The second plot demonstrate one single run of the ``MiniBatchKMeans``
-estimator using a ``init='random'`` and ``n_init=1``. This run leads to
-a bad convergence (local optimum) with estimated centers stuck
-between ground truth clusters.
-    
-    # Anisotropicly distributed data
-random_state = 170
-X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
-transformation = [[0.6, -0.6], [-0.4, 0.8]]
-X_aniso = np.dot(X, transformation)
-aniso = (X_aniso, y)
-    
-    print(__doc__)
-    
-    # plot shrinkage coefficient
-plt.subplot(2, 1, 2)
-plt.errorbar(n_samples_range, lw_shrinkage.mean(1), yerr=lw_shrinkage.std(1),
-             label='Ledoit-Wolf', color='navy', lw=2)
-plt.errorbar(n_samples_range, oa_shrinkage.mean(1), yerr=oa_shrinkage.std(1),
-             label='OAS', color='darkorange', lw=2)
-plt.xlabel('n_samples')
-plt.ylabel('Shrinkage')
-plt.legend(loc='lower right')
-plt.ylim(plt.ylim()[0], 1. + (plt.ylim()[1] - plt.ylim()[0]) / 10.)
-plt.xlim(5, 31)
-    
-        def test_decode_good(self):
-        self.assertEqual('x', self.field.decode('x'))
-    
-        Assumes:
-     * That subclasses also subclass unittest.TestCase
-     * That the authenticator is stored as self.auth
+        
+class TestAutoContentTypeAndAcceptHeaders:
     '''
+    Test that Accept and Content-Type correctly defaults to JSON,
+    but can still be overridden. The same with Content-Type when --form
+    -f is used.
     
-    ===================================== =====================================
-``--dns-rfc2136-credentials``         RFC 2136 credentials_ INI file.
-                                      (Required)
-``--dns-rfc2136-propagation-seconds`` The number of seconds to wait for DNS
-                                      to propagate before asking the ACME
-                                      server to verify the DNS record.
-                                      (Default: 60)
-===================================== =====================================
     
-                logger.debug('No authoritative SOA record found for %s', domain_name)
-            return False
-        except Exception as e:
-            raise errors.PluginError('Encountered error when making query: {0}'
-                                     .format(e))
+@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
+def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
+    r = http(follow_flag, httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 1
+    assert HTTP_OK in r
+    
+        if explicit_json and body and (not lexer or isinstance(lexer, TextLexer)):
+        # JSON response with an incorrect Content-Type?
+        try:
+            json.loads(body)  # FIXME: the body also gets parsed in json.py
+        except ValueError:
+            pass  # Nope
+        else:
+            lexer = pygments.lexers.get_lexer_by_name('json')
+    
+    
+class Environment:
+    '''
+    Information about the execution context
+    (standard streams, config directory, etc).
+    
+        def format_body(self, content, mime):
+        if is_valid_mime(mime):
+            for p in self.enabled_plugins:
+                content = p.format_body(content, mime)
+        return content
 
     
-    
-if __name__ == '__main__':
-    main()
+        keywords='wechat itchat api robot weixin personal extend',
