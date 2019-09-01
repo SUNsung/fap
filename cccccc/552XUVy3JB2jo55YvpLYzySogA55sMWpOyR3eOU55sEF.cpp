62adf96eee1adec30b5b808ee62c5ab24568dca5
SUNsung <weatherius@gmail.com>
2019-09-01 08:59:17 +0300

    Rewrite [552XUVy3JB2jo55YvpLYzySogA55sMWpOyR3eOU55sEF.cpp] File in: 2019-09-01 08:59:16

--- cccccc/552XUVy3JB2jo55YvpLYzySogA55sMWpOyR3eOU55sEF.cpp
+++ cccccc/552XUVy3JB2jo55YvpLYzySogA55sMWpOyR3eOU55sEF.cpp
@@ -1,196 +1,243 @@
 
         
-        #include <cstdio>    // for FILE
-#include 'strngs.h'  // for STRING
+          // base::SingleThreadTaskRunner:
+  bool PostDelayedTask(const base::Location& from_here,
+                       base::OnceClosure task,
+                       base::TimeDelta delay) override;
+  bool RunsTasksInCurrentSequence() const override;
+  bool PostNonNestableDelayedTask(const base::Location& from_here,
+                                  base::OnceClosure task,
+                                  base::TimeDelta delay) override;
     
     
-    {}  // namespace tesseract
-    
-      // Returns the x,y means as an FCOORD.
-  FCOORD mean_point() const;
-    
-      // Connects this and other, discarding any existing connections.
-  void Connect(DoublePtr* other) {
-    other->Disconnect();
-    Disconnect();
-    other->other_end_ = this;
-    other_end_ = other;
-  }
-  // Disconnects this and other, making OtherEnd() return nullptr for both.
-  void Disconnect() {
-    if (other_end_ != nullptr) {
-      other_end_->other_end_ = nullptr;
-      other_end_ = nullptr;
-    }
-  }
-  // Returns the pointer to the other end of the double pointer.
-  DoublePtr* OtherEnd() const {
-    return other_end_;
-  }
-    
-      // Get the value of the top (smallest, defined by operator< ) element.
-  const Pair& PeekTop() const {
-    return heap_[0];
-  }
-  // Get the value of the worst (largest, defined by operator< ) element.
-  const Pair& PeekWorst() const { return heap_[IndexOfWorst()]; }
-    
-    // A useful base class to facilitate the common operation of sorting a vector
-// of owned pointer data using a separate key. This class owns its data pointer,
-// deleting it when it has finished with it, and providing copy constructor and
-// operator= that have move semantics so that the data does not get copied and
-// only a single instance of KDPtrPair holds a specific data pointer.
-template <typename Key, typename Data>
-class KDPtrPair {
- public:
-  KDPtrPair() : data_(nullptr) {}
-  KDPtrPair(Key k, Data* d) : data_(d), key_(k) {}
-  // Copy constructor steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  KDPtrPair(KDPtrPair& src) : data_(src.data_), key_(src.key_) {
-    src.data_ = nullptr;
-  }
-  // Destructor deletes data, assuming it is the sole owner.
-  ~KDPtrPair() {
-    delete this->data_;
-    this->data_ = nullptr;
-  }
-  // Operator= steals the pointer from src and nulls it in src, thereby
-  // moving the (single) ownership of the data.
-  void operator=(KDPtrPair& src) {
-    delete this->data_;
-    this->data_ = src.data_;
-    src.data_ = nullptr;
-    this->key_ = src.key_;
-  }
-    }
-    
-    
-    { protected:
-  // Outputs the integer formed by reversing the bits of the input integer. Only
-  // the lowest num_bits_ bits of the input integer are reversed.
-  int GetBinaryReversedInteger(int in_val) const {
-    int bit_pos = num_bits_;
-    int out_val = 0;
-    while(bit_pos--) {
-      // Set the value of the last bit.
-      out_val |= (in_val & 0x1);
-      if (bit_pos > 0) {
-        // Left-shift output value to prepare for storing the next bit.
-        out_val <<= 1;
+    {
+    {      int device_name_index = 0;
+      for (auto& source : screen_sources) {
+        const auto& device_name = device_names[device_name_index++];
+        std::wstring wide_device_name;
+        base::UTF8ToWide(device_name.c_str(), device_name.size(),
+                         &wide_device_name);
+        const int64_t device_id =
+            display::win::DisplayInfo::DeviceIdFromDeviceName(
+                wide_device_name.c_str());
+        source.display_id = base::NumberToString(device_id);
       }
-      // Right-shift input value to prepare for retrieving the next bit.
-      in_val >>= 1;
     }
-    return out_val;
+#elif defined(OS_MACOSX)
+    // On Mac, the IDs across the APIs match.
+    for (auto& source : screen_sources) {
+      source.display_id = base::NumberToString(source.media_list_source.id.id);
+    }
+#endif  // defined(OS_WIN)
+    // TODO(ajmacd): Add Linux support. The IDs across APIs differ but Chrome
+    // only supports capturing the entire desktop on Linux. Revisit this if
+    // individual screen support is added.
+    std::move(screen_sources.begin(), screen_sources.end(),
+              std::back_inserter(captured_sources_));
   }
-  int N_;
-  // Next number to be considered for reversal and output.
-  int next_num_;
-  // number of bits required to represent the numbers of the sequence
-  int num_bits_;
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(DownloadItem);
 };
     
-    // Feature distance calculator designed to provide a fast distance calculation
-// based on set difference between a given feature set and many other feature
-// sets in turn.
-// Representation of a feature set as an array of bools that are sparsely
-// true, and companion arrays that allow fast feature set distance
-// calculations with allowance of offsets in position.
-// Init is expensive, so for greatest efficiency, to re-initialize for a new
-// feature set, use Set(..., false) on the SAME feature set as was used to
-// setup with Set(..., true), to return to its initialized state before
-// reuse with Set(..., true) on a new feature set.
-class IntFeatureDist {
- public:
-  IntFeatureDist();
-  ~IntFeatureDist();
-    }
     
-     private:
-  grpc::string Ip4ToPackedString(const char* ip_str) {
-    struct in_addr ip4;
-    GPR_ASSERT(inet_pton(AF_INET, ip_str, &ip4) == 1);
-    return grpc::string(reinterpret_cast<const char*>(&ip4), sizeof(ip4));
-  }
+    {}  // namespace api
     
-    #include <grpc/support/log.h>
-    
-    void ClearConfigErrorCalled() { g_config_error_function_called = false; }
-    
-    #ifndef NDEBUG
-#define GRPC_COMBINER_DEBUG_ARGS \
-  , const char *file, int line, const char *reason
-#define GRPC_COMBINER_REF(combiner, reason) \
-  grpc_combiner_ref((combiner), __FILE__, __LINE__, (reason))
-#define GRPC_COMBINER_UNREF(combiner, reason) \
-  grpc_combiner_unref((combiner), __FILE__, __LINE__, (reason))
-#else
-#define GRPC_COMBINER_DEBUG_ARGS
-#define GRPC_COMBINER_REF(combiner, reason) grpc_combiner_ref((combiner))
-#define GRPC_COMBINER_UNREF(combiner, reason) grpc_combiner_unref((combiner))
-#endif
-    
-    static inline std::shared_ptr<::grpc::Channel> CreateChannel(
-    const grpc::string& target,
-    const std::shared_ptr<ChannelCredentials>& creds) {
-  return ::grpc_impl::CreateChannelImpl(target, creds);
-}
-    
-    #endif  // GRPCPP_SUPPORT_CHANNEL_ARGUMENTS_H
+    #endif  // SHELL_BROWSER_API_ATOM_API_MENU_VIEWS_H_
 
     
-    #include 'src/core/lib/iomgr/iomgr.h'
-#include 'src/proto/grpc/testing/duplicate/echo_duplicate.grpc.pb.h'
-#include 'src/proto/grpc/testing/echo.grpc.pb.h'
-#include 'test/core/util/port.h'
-#include 'test/core/util/test_config.h'
-#include 'test/cpp/end2end/test_service_impl.h'
-#include 'test/cpp/util/byte_buffer_proto_helper.h'
+    
+typedef TypedAttribute<OPENEXR_IMF_INTERNAL_NAMESPACE::Envmap> EnvmapAttribute;
     
     
-    {  EXPECT_EQ(send_response_.message(), recv_response_.message());
-  EXPECT_TRUE(recv_status_.ok());
+    
+    #include <ImfFloatVectorAttribute.h>
+    
+    
+FrameBuffer::Iterator
+FrameBuffer::find (const string &name)
+{
+    return find (name.c_str());
 }
     
-      void RecvThreadFunc() {
-    using common::ErrorCode;
-    using common::time::AsInt64;
-    using common::time::Clock;
-    using common::time::micros;
-    AINFO << 'Receive thread starting...';
-    TestCanParam *param = param_ptr();
-    CanClient *client = param->can_client;
-    int64_t start = 0;
-    std::vector<CanFrame> buf;
+    
+    //----------------------------------------------------------------------
+    // Preview image:
+    //
+    // The preview image is a PreviewImageAttribute whose name is 'preview'.
+    // This attribute is special -- while an image file is being written,
+    // the pixels of the preview image can be changed repeatedly by calling
+    // OutputFile::updatePreviewImage().
+    //
+    // Convenience functions:
+    //
+    // setPreviewImage(p)
+    //     calls insert ('preview', PreviewImageAttribute (p))
+    //
+    // previewImage()
+    //     returns typedAttribute<PreviewImageAttribute>('preview').value()
+    //
+    // hasPreviewImage()
+    //     return findTypedAttribute<PreviewImageAttribute>('preview') != 0
+    //
+    //----------------------------------------------------------------------
+    
+    
+void
+hufBuildEncTable
+    (Int64*	frq,	// io: input frequencies [HUF_ENCSIZE], output table
+     int*	im,	//  o: min frq index
+     int*	iM)	//  o: max frq index
+{
+    //
+    // This function assumes that when it is called, array frq
+    // indicates the frequency of all possible symbols in the data
+    // that are to be Huffman-encoded.  (frq[i] contains the number
+    // of occurrences of symbol i in the data.)
+    //
+    // The loop below does three things:
+    //
+    // 1) Finds the minimum and maximum indices that point
+    //    to non-zero entries in frq:
+    //
+    //     frq[im] != 0, and frq[i] == 0 for all i < im
+    //     frq[iM] != 0, and frq[i] == 0 for all i > iM
+    //
+    // 2) Fills array fHeap with pointers to all non-zero
+    //    entries in frq.
+    //
+    // 3) Initializes array hlink such that hlink[i] == i
+    //    for all array entries.
+    //
     }
     
-    class Crosswalk : public TrafficRule {
- public:
-  explicit Crosswalk(const TrafficRuleConfig& config);
-  virtual ~Crosswalk() = default;
+     
+        IMF_EXPORT
+        void                rawPixelDataToBuffer (int scanLine,
+                                                  char *pixelData,
+                                                  int &pixelDataSize) const;
+    
+    //-----------------------------------------------------------------------------
+//
+//	class IntAttribute
+//
+//-----------------------------------------------------------------------------
+    
+      // Is page valid?
+  GBool isOk() { return ok; }
+    
+    #ifndef PAGE_TRANSITION_H
+#define PAGE_TRANSITION_H
+    
+    class ExternalSecurityHandler: public SecurityHandler {
+public:
     }
     
-    #include 'modules/common/time/time.h'
-#include 'modules/map/hdmap/hdmap_util.h'
-#include 'modules/planning/common/planning_context.h'
-#include 'modules/planning/common/planning_gflags.h'
-#include 'modules/planning/proto/planning_internal.pb.h'
-#include 'modules/planning/tasks/task_factory.h'
+      SplashFont *getCurrentFont() { return font; }
     
-        // fill back results
-    FillResults(object_center, dimension_hwl, rotation_y, camera2world_pose,
-                theta_ray, obj);
     
-    #include 'modules/planning/scenarios/scenario.h'
+    {
+    {}  // namespace data
+}  // namespace xgboost
+#endif  // XGBOOST_DATA_SPARSE_PAGE_WRITER_H_
+
     
-    #define protected public
-#define private public
-#include 'modules/planning/scenarios/park/valet_parking/valet_parking_scenario.h'
+      /**
+  * \brief Column sampler constructor.
+  * \note This constructor synchronizes the RNG seed across processes.
+  */
+  ColumnSampler() {
+    uint32_t seed = common::GlobalRandom()();
+    rabit::Broadcast(&seed, sizeof(seed), 0);
+    rng_.seed(seed);
+  }
     
-    #include <string>
-#include <vector>
     
-      ObstacleDetectorInitOptions init_options;
-  init_options.root_dir = FLAGS_detector_root;
-  init_options.conf_file = FLAGS_detector_conf;
+    {  void setTimeout(std::chrono::seconds timeout)
+  {
+    timeout_ = std::move(timeout);
+  }
+};
+    
+    DHTResponseMessage::DHTResponseMessage(
+    const std::shared_ptr<DHTNode>& localNode,
+    const std::shared_ptr<DHTNode>& remoteNode,
+    const std::string& transactionID)
+    : DHTAbstractMessage(localNode, remoteNode, transactionID)
+{
+}
+    
+    #include 'DHTAbstractMessage.h'
+#include 'A2STR.h'
+#include 'ValueBase.h'
+    
+    class DHTTask {
+public:
+  virtual ~DHTTask() = default;
+    }
+    
+      ~DHTTaskExecutor();
+    
+    void DHTTaskFactoryImpl::setCommonProperty(
+    const std::shared_ptr<DHTAbstractTask>& task)
+{
+  task->setRoutingTable(routingTable_);
+  task->setMessageDispatcher(dispatcher_);
+  task->setMessageFactory(factory_);
+  task->setTaskQueue(taskQueue_);
+  task->setLocalNode(localNode_);
+}
+    
+      virtual void addPeriodicTask1(const std::shared_ptr<DHTTask>& task) = 0;
+    
+    #endif // D_DHT_TOKEN_TRACKER_H
+
+    
+    #include 'DHTMessage.h'
+    
+        const auto FACE_NUMBER_PARTS = 70u;
+    #define FACE_PAIRS_RENDER_GPU \
+        0,1,  1,2,  2,3,  3,4,  4,5,  5,6,  6,7,  7,8,  8,9,  9,10,  10,11,  11,12,  12,13,  13,14,  14,15,  15,16,  17,18,  18,19,  19,20, \
+        20,21,  22,23,  23,24,  24,25,  25,26,  27,28,  28,29,  29,30,  31,32,  32,33,  33,34,  34,35,  36,37,  37,38,  38,39,  39,40,  40,41, \
+        41,36,  42,43,  43,44,  44,45,  45,46,  46,47,  47,42,  48,49,  49,50,  50,51,  51,52,  52,53,  53,54,  54,55,  55,56,  56,57,  57,58, \
+        58,59,  59,48,  60,61,  61,62,  62,63,  63,64,  64,65,  65,66,  66,67,  67,60
+    #define FACE_SCALES_RENDER_GPU 1
+    const std::vector<unsigned int> FACE_PAIRS_RENDER {FACE_PAIRS_RENDER_GPU};
+    #define FACE_COLORS_RENDER_GPU 255.f,    255.f,    255.f
+    const std::vector<float> FACE_COLORS_RENDER{FACE_COLORS_RENDER_GPU};
+    const std::vector<float> FACE_SCALES_RENDER{FACE_SCALES_RENDER_GPU};
+    
+    
+    {        detail::errHandler(error, __CREATE_PROGRAM_WITH_BINARY_ERR);
+        if (err != NULL) {
+            *err = error;
+        }
+    }
+    
+    #ifdef USE_3D_ADAM_MODEL
+    #include <adam/totalmodel.h>
+#endif
+#include <openpose/core/common.hpp>
+#include <openpose/gui/enumClasses.hpp>
+#include <openpose/gui/gui.hpp>
+    
+    namespace op
+{
+    struct PersonEntry
+    {
+        long long counterLastDetection;
+        std::vector<cv::Point2f> keypoints;
+        std::vector<char> status;
+        /*
+        PersonEntry(long long _last_frame, 
+                    std::vector<cv::Point2f> _keypoints,
+                    std::vector<char> _active):
+                    last_frame(_last_frame), keypoints(_keypoints),
+                    active(_active)
+                    {}
+        */
+    };
+    class OP_API PersonIdExtractor
+    {
+    }
+    }
