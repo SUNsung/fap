95229ee241ebbba0860cc73a1d70f315908685ee
SUNsung <weatherius@gmail.com>
2019-09-01 12:42:59 +0300

    Rewrite [27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py] File in: 2019-09-01 12:42:59

--- pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
+++ pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
@@ -1,45 +1,134 @@
 
         
-        tokenizer = RegexpTokenizer(r'\w+')
-''' 分词器：匹配所有连续的字母串
-比如 'abc!@#def$%^gh, ijk' -> 'abc' 'def' 'gh' 'ijk'
-'''
-stemmer = PorterStemmer()
-''' 词干提取器
-'''
+        
+    {    # Options that need a file parameter
+    'download-archive': ['--require-parameter'],
+    'cookies': ['--require-parameter'],
+    'load-info': ['--require-parameter'],
+    'batch-file': ['--require-parameter'],
+}
     
     
-def identity(x):
-    ''''''
-    return tf.identity(x)
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
     
-        with tf.variable_scope(name, reuse=reuse):
-        alpha = get_w(alpha_shape, w_initializer=alpha_init, name='alpha')
-        # o = relu(x) + 0.5 * tf.multiply(alpha, x - tf.abs(x))  # TFLearn
-        o = leaky_relu(x, alpha)  # TensorLayer / <Deep Learning>
-    
-        with tf.variable_scope(name, reuse=reuse):
-        char_embed_mat = get_w([char_vocab_size, c_embed_size], name='char_embed_matrix')
+    header = oldreadme[:oldreadme.index('# OPTIONS')]
+footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
     
     
-class L1L2Regularizer(object):
-    '''L1 L2 正则化
+def main():
+    parser = optparse.OptionParser(usage='%prog OUTFILE.md')
+    options, args = parser.parse_args()
+    if len(args) != 1:
+        parser.error('Expected an output filename')
+    
+            if in_options:
+            if line.lstrip().startswith('-'):
+                split = re.split(r'\s{2,}', line.lstrip())
+                # Description string may start with `-` as well. If there is
+                # only one piece then it's a description bit not an option.
+                if len(split) > 1:
+                    option, description = split
+                    split_option = option.split(' ')
+    
+        def test_youtube(self):
+        self._assert_restricted('07FYdnEawAQ', '07FYdnEawAQ.mp4', 10)
     
     
-def get_params_dict():
-    '''以字典形式获取所有 trainable 参数'''
-    param_dict = dict()
-    for var in tf.trainable_variables():
-        param_dict[var.name] = {'shape': list(map(int, var.shape)),
-                                'number': int(reduce(mul, var.shape, 1))}
-    return param_dict
+def _mkdir(d):
+    if not os.path.exists(d):
+        os.mkdir(d)
     
-    References:
-    https://www.tensorflow.org/api_guides/python/array_ops
-    keras.backend
-'''
+    from utils import TESTS_ROOT
     
-    if word_vec.any():  #
-    word_vec = word_vec / max(1, ngrams_found)
-else:  # 如果一个 ngram 都没找到，gensim 会报错；个人认为把 0 向量传出来也可以
-    raise KeyError('all ngrams for word %s absent from model' % word_unk)
+    
+def main():
+    package_meta_map = {
+        package_name: get_package_meta(package_name)
+        for package_name in PACKAGES
+    }
+    httpie_meta = package_meta_map.pop('httpie')
+    print()
+    print('  url '{url}''.format(url=httpie_meta['url']))
+    print('  sha256 '{sha256}''.format(sha256=httpie_meta['sha256']))
+    print()
+    for dep_meta in package_meta_map.values():
+        print('  resource '{name}' do'.format(name=dep_meta['name']))
+        print('    url '{url}''.format(url=dep_meta['url']))
+        print('    sha256 '{sha256}''.format(sha256=dep_meta['sha256']))
+        print('  end')
+        print('')
+    
+        def __call__(self, value):
+        # Session name can be a path or just a name.
+        if (os.path.sep not in value
+                and not VALID_SESSION_NAME_PATTERN.search(value)):
+            raise argparse.ArgumentError(None, self.error_message)
+        return value
+    
+    sessions = parser.add_argument_group(title='Sessions') \
+    .add_mutually_exclusive_group(required=False)
+    
+    from sklearn.ensemble import IsolationForest
+from sklearn.metrics import roc_curve, auc
+from sklearn.datasets import fetch_kddcup99, fetch_covtype, fetch_openml
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.utils import shuffle as sh
+    
+        Scale each row of the data matrix by multiplying with specific scale
+    provided by the caller assuming a (n_samples, n_features) shape.
+    
+        for ns in n_samples:
+        for nf in n_features:
+            it += 1
+            print('==================')
+            print('Iteration %s of %s' % (it, max(len(n_samples),
+                                          len(n_features))))
+            print('==================')
+            n_informative = nf // 10
+            X, Y, coef_ = make_regression(n_samples=ns, n_features=nf,
+                                          n_informative=n_informative,
+                                          noise=0.1, coef=True)
+    
+        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
+                                   'blob/{revision}/{package}/'
+                                   '{path}#L{lineno}')
+    '''
+    revision = _get_git_revision()
+    return partial(_linkcode_resolve, revision=revision, package=package,
+                   url_fmt=url_fmt)
+
+    
+    import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+    
+    # Create a graph capturing local connectivity. Larger number of neighbors
+# will give more homogeneous clusters to the cost of computation
+# time. A very large number of neighbors gives more evenly distributed
+# cluster sizes, but may not impose the local manifold structure of
+# the data
+knn_graph = kneighbors_graph(X, 30, include_self=False)
+    
+    X = list()
+y = list()
+for i, (phi, a) in enumerate([(.5, .15), (.5, .6), (.3, .2)]):
+    for _ in range(30):
+        phase_noise = .01 * np.random.normal()
+        amplitude_noise = .04 * np.random.normal()
+        additional_noise = 1 - 2 * np.random.rand(n_features)
+        # Make the noise sparse
+        additional_noise[np.abs(additional_noise) < .997] = 0
+    
+    # Anisotropicly distributed data
+transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
+X_aniso = np.dot(X, transformation)
+y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_aniso)
