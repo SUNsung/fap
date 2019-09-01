cd24045823860c02dfaa446c80d67db085c1b0ce
SUNsung <weatherius@gmail.com>
2019-09-01 20:54:39 +0300

    Rewrite [162DdPZugselA16dYVt6buLtiA16hf7orJSY8Ak16AQ5.py] File in: 2019-09-01 20:54:39

--- pyton/162DdPZugselA16dYVt6buLtiA16hf7orJSY8Ak16AQ5.py
+++ pyton/162DdPZugselA16dYVt6buLtiA16hf7orJSY8Ak16AQ5.py
@@ -1,69 +1,27 @@
 
         
-            def report_not_exist(self, appid, ip):
-        self.logger.debug('report_not_exist:%s %s', appid, ip)
-        th = threading.Thread(target=self.process_appid_not_exist, args=(appid, ip))
-        th.start()
+            description = proj_info['description'],
+    keywords = proj_info['keywords'],
     
-            self._checking_lock.acquire()
-        self._checking_num += 1
-        self._checking_lock.release()
+    site_info = 'baomihua.com'
+download = baomihua_download
+download_playlist = playlist_not_supported('baomihua')
+
     
-    And tree.TreeParser finally fetches its input from a tree.TreeNodeStream:
+    from ..common import *
     
-        def test_get_subscribe_attributes(self):
-        req_data = {
-            'Attribute.entry.1.key': ['RawMessageDelivery'],
-            'Attribute.entry.1.value': ['true'],
-            'Attribute.entry.2.key': ['FilterPolicy'],
-            'Attribute.entry.2.value': ['{'type': ['foo', 'bar']}']
-        }
-        attributes = sns_listener.get_subscribe_attributes(req_data)
-        self.assertDictEqual(
-            attributes,
-            {
-                'RawMessageDelivery': 'true',
-                'FilterPolicy': '{'type': ['foo', 'bar']}'
-            }
-        )
+    from ..common import *
+from ..extractor import VideoExtractor
     
-    TEST_BUCKET_NAME_WITH_NOTIFICATIONS = 'test_bucket_notif_1'
-TEST_QUEUE_NAME_FOR_S3 = 'test_queue'
-TEST_TOPIC_NAME = 'test_topic_name_for_sqs'
-TEST_S3_TOPIC_NAME = 'test_topic_name_for_s3_to_sns_to_sqs'
-TEST_QUEUE_NAME_FOR_SNS = 'test_queue_for_sns'
+            :raises ServerNotFoundError: Not running on Google Compute or DNS not available
+        :raises ValueError: Server is found, but response code is not 200
+        :returns: project id
+        '''
+        url = '{0}project/project-id'.format(METADATA_URL)
     
+            self.webroot = os.path.join(self.nginx_root, 'webroot')
+        os.mkdir(self.webroot)
+        with open(os.path.join(self.webroot, 'index.html'), 'w') as file_handler:
+            file_handler.write('Hello World!')
     
-def record(api, method, path, data, headers):
-    ''' Record a given API call to a persistent file on disk '''
-    file_path = get_file_path(api, create=True)
-    if CURRENTLY_REPLAYING or not file_path or not should_record(api, method, path, data, headers):
-        return
-    entry = None
-    try:
-        if isinstance(data, dict):
-            data = json.dumps(data)
-        if data or data in [u'', b'']:
-            try:
-                data = to_bytes(data)
-            except Exception as e:
-                LOGGER.warning('Unable to call to_bytes: %s' % e)
-            data = to_str(base64.b64encode(data))
-        entry = {
-            'a': api,
-            'm': method,
-            'p': path,
-            'd': data,
-            'h': dict(headers)
-        }
-        with open(file_path, 'a') as dumpfile:
-            dumpfile.write('%s\n' % json.dumps(entry))
-    except Exception as e:
-        print('Error recording API call to persistent file: %s %s' % (e, traceback.format_exc()))
-    
-        # create ES domain
-    es_client.create_elasticsearch_domain(DomainName=TEST_DOMAIN_NAME)
-    assert_true(TEST_DOMAIN_NAME in
-        [d['DomainName'] for d in es_client.list_domain_names()['DomainNames']])
-    
-            iam_client = aws_stack.connect_to_service('iam')
+        assert server_cert == certbot_cert
