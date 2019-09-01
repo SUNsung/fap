e4f59d428cd3403d4b85ae1787eb2988650274f3
SUNsung <weatherius@gmail.com>
2019-09-01 17:46:56 +0300

    Rewrite [56NkswV74Yxc.56H7nQ8xqafmg563JOApHelNZQ56yq0.cpp] File in: 2019-09-01 17:46:56

--- cccccc/56NkswV74Yxc.56H7nQ8xqafmg563JOApHelNZQ56yq0.cpp
+++ cccccc/56NkswV74Yxc.56H7nQ8xqafmg563JOApHelNZQ56yq0.cpp
@@ -1,303 +1,289 @@
 
         
-        #include <grpc/grpc_security.h>
-#include 'src/core/lib/channel/channel_args.h'
+        #undef REGISTER_POPULATION_COUNT
     
-    
-    {
-    {
-    {}  // namespace
-}  // namespace testing
-}  // namespace grpc
-    
-    void grpc_set_default_iomgr_platform() {
-  grpc_set_tcp_client_impl(&grpc_windows_tcp_client_vtable);
-  grpc_set_tcp_server_impl(&grpc_windows_tcp_server_vtable);
-  grpc_set_timer_impl(&grpc_generic_timer_vtable);
-  grpc_set_pollset_vtable(&grpc_windows_pollset_vtable);
-  grpc_set_pollset_set_vtable(&grpc_windows_pollset_set_vtable);
-  grpc_set_resolver_impl(&grpc_windows_resolver_vtable);
-  grpc_set_iomgr_platform_vtable(&vtable);
-}
-    
-      ac = (async_connect*)gpr_malloc(sizeof(async_connect));
-  ac->on_done = on_done;
-  ac->socket = socket;
-  gpr_mu_init(&ac->mu);
-  ac->refs = 2;
-  ac->addr_name = grpc_sockaddr_to_uri(addr);
-  ac->endpoint = endpoint;
-  ac->channel_args = grpc_channel_args_copy(channel_args);
-  GRPC_CLOSURE_INIT(&ac->on_connect, on_connect, ac, grpc_schedule_on_exec_ctx);
-    
-    
-    {  value = GPR_GLOBAL_CONFIG_GET(string_var);
-  EXPECT_EQ(0, strcmp(value.get(), 'Test'));
-}
-    
-    
-    {    CompareType(method_desc->input_type()->full_name());
-    CompareType(method_desc->output_type()->full_name());
-  }
-    
-    
-    {} // namespace aria2
-    
-    std::shared_ptr<DHTNode> DHTRoutingTable::getNode(const unsigned char* nodeID,
-                                                  const std::string& ipaddr,
-                                                  uint16_t port) const
-{
-  std::shared_ptr<DHTBucket> bucket = getBucketFor(nodeID);
-  return bucket->getNode(nodeID, ipaddr, port);
-}
-    
-    #endif // D_DHT_TASK_H
-
-    
-    
-    {  size_t getQueueSize() const { return queue_.size(); }
-};
-    
-    void DHTTaskFactoryImpl::setCommonProperty(
-    const std::shared_ptr<DHTAbstractTask>& task)
-{
-  task->setRoutingTable(routingTable_);
-  task->setMessageDispatcher(dispatcher_);
-  task->setMessageFactory(factory_);
-  task->setTaskQueue(taskQueue_);
-  task->setLocalNode(localNode_);
-}
-    
-    
-    {  virtual void addImmediateTask(const std::shared_ptr<DHTTask>& task) = 0;
-};
-    
-    #include 'util.h'
-#include 'bittorrent_helper.h'
-#include 'DlAbortEx.h'
-#include 'DHTConstants.h'
-#include 'MessageDigest.h'
-#include 'message_digest_helper.h'
-#include 'fmt.h'
-    
-    namespace aria2 {
-    }
-    
-        // out_of_range.109
-    try
-    {
-        // try to use an array index that is not a number
-        json::reference ref = j.at('/array/one'_json_pointer);
-    }
-    catch (json::parse_error& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-    
-    int main()
-{
-    // ============
-    // object types
-    // ============
-    }
-    
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-    }
-    }
-    }
-    
-    TEST_F(DistanceApproachIPOPTInterfaceTest, eval_f) {
-  int n = 1274;
-  double obj_value = 0.0;
-  double x[1274];
-  std::fill_n(x, n, 1.2);
-  bool res = ptop_->eval_f(n, x, true, obj_value);
-  EXPECT_DOUBLE_EQ(obj_value, 1443.3600000000008) << 'eval_f: ' << obj_value;
-  EXPECT_TRUE(res);
-}
-    
-    class StageApproachingParkingSpot : public Stage {
- public:
-  explicit StageApproachingParkingSpot(
-      const ScenarioConfig::StageConfig& config)
-      : Stage(config) {}
-    }
-    
-    class PullOverScenarioTest : public ::testing::Test {
- public:
-  virtual void SetUp() {}
-    }
-    
-    #include 'gtest/gtest.h'
-#include 'modules/planning/proto/planning_config.pb.h'
-    
-     private:
-  void WriteOutputJpgFile(const std::vector<uint8_t>& jpeg_buffer,
-                          const std::string& output_jpg_file) const;
-  std::string GetOutputFile(const int frame_num) const;
-    
-    
-    {  EXPECT_EQ(expect, real);
-}
-    
-    #include 'cyber/croutine/croutine.h'
-#include 'cyber/proto/choreography_conf.pb.h'
-#include 'cyber/scheduler/scheduler.h'
-    
-        bool Has(K key) {
-      Entry *m_target = head_->next.load(std::memory_order_acquire);
-      while (Entry *target = m_target) {
-        if (target->key < key) {
-          m_target = target->next.load(std::memory_order_acquire);
-          continue;
-        } else {
-          return target->key == key;
+        // MatrixDiagPartV2-specific.
+    if (context->num_inputs() > 1) {
+      auto& diag_index = context->input(1);
+      OP_REQUIRES(context,
+                  TensorShapeUtils::IsScalar(diag_index.shape()) ||
+                      TensorShapeUtils::IsVector(diag_index.shape()),
+                  errors::InvalidArgument(
+                      'diag_index must be a scalar or vector, received shape: ',
+                      diag_index.shape().DebugString()));
+      lower_diag_index = diag_index.flat<int32>()(0);
+      upper_diag_index = lower_diag_index;
+      if (TensorShapeUtils::IsVector(diag_index.shape())) {
+        auto diag_index_size = diag_index.dim_size(0);
+        OP_REQUIRES(
+            context, 0 < diag_index_size && diag_index_size <= 2,
+            errors::InvalidArgument(
+                'diag_index must have only one or two elements, received ',
+                diag_index_size, ' elements.'));
+        if (diag_index_size > 1) {
+          upper_diag_index = diag_index.flat<int32>()(1);
         }
       }
-      return false;
+      padding_value = context->input(2).flat<T>()(0);
     }
+    const TensorShape& input_shape = input.shape();
     
-        int ret = 0;
-    ret = fscanf(fp, '%254s %f %f %lf %f %f %f %f %f %f %f %lf %lf %lf %f %f',
-                 type, &trash, &trash, &obj->camera_supplement.alpha, &x1, &y1,
-                 &x2, &y2, &obj->size[2], &obj->size[1], &obj->size[0],
-                 &obj->center[0], &obj->center[1], &obj->center[2], &obj->theta,
-                 &score);
-    AINFO << 'fscanf return: ' << ret;
-    if (FLAGS_dist_type == 'H-from-h') {
-      obj->size[0] = static_cast<float>(obj->center[2]);
-    } else if (FLAGS_dist_type == 'H-on-h') {
-      obj->size[0] = static_cast<float>(obj->center[2]) * (y2 - y1);
-    }
-    obj->camera_supplement.box.xmin = std::max<float>(x1, 0);
-    obj->camera_supplement.box.ymin = std::max<float>(y1, 0);
-    obj->camera_supplement.box.xmax =
-        std::min<float>(x2, static_cast<float>(FLAGS_width));
-    obj->camera_supplement.box.ymax =
-        std::min<float>(y2, static_cast<float>(FLAGS_height));
-    obj->camera_supplement.area_id = 5;
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-    namespace osquery {
-    }
+        http://www.apache.org/licenses/LICENSE-2.0
     
-    /**
- * @brief Returns a pointer of where the requested symbol exists
- */
-void* platformModuleGetSymbol(ModuleHandle module, const std::string& symbol);
+        http://www.apache.org/licenses/LICENSE-2.0
     
-    // clang-format off
-#define WIN32_NO_STATUS
-#include <Windows.h>
-#undef WIN32_NO_STATUS
-#include <ntstatus.h>
-// clang-format on
-#include <iomanip>
-#include <psapi.h>
-#include <stdlib.h>
-#include <tlhelp32.h>
-#include <winternl.h>
+    #include 'tensorflow/cc/ops/array_ops.h'
+#include 'tensorflow/cc/ops/no_op.h'
+#include 'tensorflow/cc/ops/standard_ops.h'
+#include 'tensorflow/core/framework/node_def.pb.h'
+#include 'tensorflow/core/framework/tensor_testutil.h'
+#include 'tensorflow/core/grappler/devices.h'
+#include 'tensorflow/core/grappler/grappler_item.h'
+#include 'tensorflow/core/grappler/inputs/trivial_test_graph_input_yielder.h'
+#include 'tensorflow/core/grappler/utils.h'
+#include 'tensorflow/core/grappler/utils/grappler_test.h'
+#include 'tensorflow/core/lib/core/status_test_util.h'
+#include 'tensorflow/core/platform/test.h'
     
-      /*
-   * @brief a variable tracking all of the paths we attempt to carve
-   *
-   * This is a globbed set of file paths that we're expecting will be
-   * carved.
-   */
-  std::set<boost::filesystem::path> carvePaths_;
-    
-    
-    {  rocksdb::Options options = getOptions();
-  auto db_path = boost::filesystem::path(path_).make_preferred();
-  auto destroy_status = rocksdb::DestroyDB(db_path.string(), options);
-  if (!destroy_status.ok()) {
-    return createError(DatabaseError::FailToDestroyDB, 'Fail to destroy db: ')
-           << destroy_status.ToString();
-  }
-  return Success();
-}
-    
-     private:
-  rocksdb::Options getOptions();
-  std::vector<rocksdb::ColumnFamilyDescriptor> createDefaultColumnFamilies(
-      const rocksdb::Options& options);
-  ExpectedSuccess<DatabaseError> openInternal(
-      const rocksdb::Options& options, const boost::filesystem::path& path);
-  Expected<std::string, DatabaseError> getRawBytesInternal(
-      Handle* handle, const std::string& key);
-  ExpectedSuccess<DatabaseError> putRawBytesInternal(Handle* handle,
-                                                     const std::string& key,
-                                                     const std::string& value);
-  ExpectedSuccess<DatabaseError> checkDbConnection();
-  Expected<std::shared_ptr<Handle>, DatabaseError> getHandle(
-      const std::string& domain);
-    
-    
-    {} // namespace osquery
-
-    
-    int Flag::createAlias(const std::string& alias, const FlagDetail& flag) {
-  instance().aliases_.insert(std::make_pair(alias, flag));
-  return 0;
-}
-    
-    
-    {    bzero(data, 256);
-    ret = p.read(&p, data, 255);
-    ASSERT_GT(ret, 0);
-    ASSERT_EQ(strcmp('hello world\n你好中国。\n', data), 0);
-}
-
-    
-        coro_test({
-        make_pair([](void *arg)
-        {
-            auto chan = (Channel *) arg;
-    }
-    }
-    
-    
-    {
-    {
-    {    inline Coroutine* pop_coroutine(enum opcode type)
-    {
-        Coroutine* co;
-        if (type == PRODUCER)
-        {
-            co = producer_queue.front();
-            producer_queue.pop_front();
-            swTraceLog(SW_TRACE_CHANNEL, 'resume producer cid=%ld', co->get_cid());
-        }
-        else // if (type == CONSUMER)
-        {
-            co = consumer_queue.front();
-            consumer_queue.pop_front();
-            swTraceLog(SW_TRACE_CHANNEL, 'resume consumer cid=%ld', co->get_cid());
-        }
-        return co;
-    }
+    class OrientationDetector {
+ public:
+  OrientationDetector(const GenericVector<int>* allowed_scripts,
+                      OSResults* results);
+  bool detect_blob(BLOB_CHOICE_LIST* scores);
+  int get_orientation();
+ private:
+  OSResults* osr_;
+  const GenericVector<int>* allowed_scripts_;
 };
-//-------------------------------------------------------------------------------
-}}
-
     
-    
-    {    ASSERT_GT(cid, 0);
-    ASSERT_EQ(cid, _cid);
+    void Tesseract::PrerecAllWordsPar(const GenericVector<WordData>& words) {
+  // Prepare all the blobs.
+  GenericVector<BlobData> blobs;
+  for (int w = 0; w < words.size(); ++w) {
+    if (words[w].word->ratings != nullptr &&
+        words[w].word->ratings->get(0, 0) == nullptr) {
+      for (int s = 0; s < words[w].lang_words.size(); ++s) {
+        Tesseract* sub = s < sub_langs_.size() ? sub_langs_[s] : this;
+        const WERD_RES& word = *words[w].lang_words[s];
+        for (int b = 0; b < word.chopped_word->NumBlobs(); ++b) {
+          blobs.push_back(BlobData(b, sub, word));
+        }
+      }
+    }
+  }
+  // Pre-classify all the blobs.
+  if (tessedit_parallelize > 1) {
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(10)
+#endif  // _OPENMP
+    for (int b = 0; b < blobs.size(); ++b) {
+      *blobs[b].choices =
+          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    }
+  } else {
+    // TODO(AMD) parallelize this.
+    for (int b = 0; b < blobs.size(); ++b) {
+      *blobs[b].choices =
+          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    }
+  }
 }
     
-    namespace swoole
-{
-/**
- * This cache isn't thread safe
- */
-class LRUCache
-{
-private:
-    typedef std::pair<time_t, std::shared_ptr<void>> cache_node_t;
-    typedef std::list<std::pair<std::string, cache_node_t>> cache_list_t;
-    }
+    // Class to hold a Pixa collection of debug images with captions and save them
+// to a PDF file.
+class DebugPixa {
+ public:
+  // TODO(rays) add another constructor with size control.
+  DebugPixa() {
+    pixa_ = pixaCreate(0);
+    fonts_ = bmfCreate(nullptr, 14);
+  }
+  // If the filename_ has been set and there are any debug images, they are
+  // written to the set filename_.
+  ~DebugPixa() {
+    pixaDestroy(&pixa_);
+    bmfDestroy(&fonts_);
+  }
     }
     
-            execute_data = prev = l;
+    
+    {  int delta = this - prev;
+  int32_t n = prev->n_ + 1;
+  int32_t sig_x = prev->sig_x_ + delta;
+  int64_t sig_xsq = prev->sig_xsq_ + delta * delta;
+  int64_t cost = (sig_xsq - sig_x * sig_x / n) / n;
+  cost += prev->total_cost_;
+  UpdateIfBetter(cost, prev->total_steps_ + 1, prev, n, sig_x, sig_xsq);
+  return cost;
+}
+    
+    #ifndef NORMALIS_H
+#define NORMALIS_H
+    
+      // The pointed-to Pair has changed its key value, so the location of pair
+  // is reshuffled to maintain the heap invariant.
+  // Must be a valid pointer to an element of the heap_!
+  // Caution! Since GenericHeap is based on GenericVector, reallocs may occur
+  // whenever the vector is extended and elements may get shuffled by any
+  // Push or Pop operation. Therefore use this function only if Data in Pair is
+  // of type DoublePtr, derived (first) from DoublePtr, or has a DoublePtr as
+  // its first element. Reshuffles the heap to maintain the invariant.
+  // Time = O(log n).
+  void Reshuffle(Pair* pair) {
+    int index = pair - &heap_[0];
+    Pair hole_pair = heap_[index];
+    index = SiftDown(index, hole_pair);
+    index = SiftUp(index, hole_pair);
+    heap_[index] = hole_pair;
+  }
+    
+    class CPUParticlesEditor : public ParticlesEditorBase {
+    }
+    
+    	ConfirmationDialog *generate_visibility_rect;
+	SpinBox *generate_seconds;
+    
+    
+    {	return String();
+}
+    
+    	virtual void gi_probe_set_normal_bias(RID p_probe, float p_range);
+	virtual float gi_probe_get_normal_bias(RID p_probe) const;
+    
+    #endif // AUDIO_STREAM_PREVIEW_H
+
+    
+    protected:
+	static void _bind_methods();
+    
+    	points.clear();
+    
+      bool signal_client() {
+    std::unique_lock<std::mutex> lock(mu_);
+    return signal_client_;
+  }
+    
+    void ChannelArguments::SetResourceQuota(
+    const grpc_impl::ResourceQuota& resource_quota) {
+  SetPointerWithVtable(GRPC_ARG_RESOURCE_QUOTA,
+                       resource_quota.c_resource_quota(),
+                       grpc_resource_quota_arg_vtable());
+}
+    
+      Status Echo(ServerContext* context, const EchoRequest* request,
+              EchoResponse* response) override {
+    gpr_event_set(ev_, (void*)1);
+    while (!context->IsCancelled()) {
+    }
+    return Status::OK;
+  }
+    
+    grpc_endpoint_pair grpc_iomgr_create_endpoint_pair(
+    const char* name, grpc_channel_args* channel_args) {
+  SOCKET sv[2];
+  grpc_endpoint_pair p;
+  create_sockets(sv);
+  grpc_core::ExecCtx exec_ctx;
+  p.client = grpc_tcp_create(grpc_winsocket_create(sv[1], 'endpoint:client'),
+                             channel_args, 'endpoint:server');
+  p.server = grpc_tcp_create(grpc_winsocket_create(sv[0], 'endpoint:server'),
+                             channel_args, 'endpoint:client');
+    }
+    
+    void grpc_socket_become_ready(grpc_winsocket* socket,
+                              grpc_winsocket_callback_info* info) {
+  GPR_ASSERT(!info->has_pending_iocp);
+  gpr_mu_lock(&socket->state_mu);
+  if (info->closure) {
+    GRPC_CLOSURE_SCHED(info->closure, GRPC_ERROR_NONE);
+    info->closure = NULL;
+  } else {
+    info->has_pending_iocp = 1;
+  }
+  bool should_destroy = check_destroyable(socket);
+  gpr_mu_unlock(&socket->state_mu);
+  if (should_destroy) destroy(socket);
+}
+    
+    
+    { private:
+  int32_t default_value_;
+};
+    
+      LOG_TEST_NAME('test_int64toa');
+    
+      request.set_message('You should be dead');
+  // This may succeed or fail depending on the state of the TCP connection
+  stream->Write(request);
+  // But the read will definitely fail
+  EXPECT_FALSE(stream->Read(&response));
+    
+    class TestServiceImpl : public EchoTestService::Service {
+ public:
+  Status Echo(ServerContext* context, const EchoRequest* request,
+              EchoResponse* response) override {
+    response->set_message(request->message());
+    return Status::OK;
+  }
+    }
+    
+    // Regular Async, both peers use proto
+TEST_F(RawEnd2EndTest, PureAsyncService) {
+  typedef grpc::testing::EchoTestService::AsyncService SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+  grpc::ServerAsyncResponseWriter<EchoResponse> response_writer(&srv_ctx_);
+    }
+    
+      void UpdatePlugins(
+      std::vector<std::unique_ptr<ServerBuilderPlugin>>* plugins) override {
+    plugins->clear();
+    }
+    
+    #pragma once
+    
+    namespace CalculatorUnitTests
+{
+    static UCM::Unit UNIT1 = { 1, L'UNIT1', L'U1', true, false, false };
+    static UCM::Unit UNIT2 = { 2, L'UNIT2', L'U2', false, true, false };
+    static UCM::Unit UNIT3 = { 3, L'UNIT3', L'U3', false, false, false };
+    static UCM::Unit UNIT4 = { 4, L'UNIT4', L'U4', true, false, false };
+    static UCM::Unit UNIT5 = { 5, L'UNIT5', L'U5', false, false, false };
+    static UCM::Unit UNIT6 = { 6, L'UNIT6', L'U6', false, true, false };
+    static UCM::Unit UNIT7 = { 7, L'UNIT7', L'U7', false, true, false };
+    static UCM::Unit UNIT8 = { 8, L'UNIT8', L'U8', false, false, false };
+    static UCM::Unit UNIT9 = { 9, L'UNIT9', L'U9', true, false, false };
+    static UCM::Unit UNITWHIMSY = { 10, L'Whimsy', L'UW', true, false, true };
+    }
+    
+        vector<uint32_t> const& Number::Mantissa() const
+    {
+        return m_mantissa;
+    }
+    
+    using namespace std;
+using namespace CalculationManager;
+    
+    
+    {    private:
+        bool m_isNegative;
+    };
+    
+            static bool IsValid(NarratorAnnouncement ^ announcement);
+    
+        // output element with JSON pointer '/number'
+    std::cout << j.at('/number'_json_pointer) << '\n';
+    // output element with JSON pointer '/string'
+    std::cout << j.at('/string'_json_pointer) << '\n';
+    // output element with JSON pointer '/array'
+    std::cout << j.at('/array'_json_pointer) << '\n';
+    // output element with JSON pointer '/array/1'
+    std::cout << j.at('/array/1'_json_pointer) << '\n';
+    
+        // create a JSON number from number_unsigned_t
+    json::number_integer_t value_unsigned_t = 17;
+    json j_unsigned_t(value_unsigned_t);
