3efb1c60cae0a5faf0d98e5f30fbb52af8006e6c
SUNsung <weatherius@gmail.com>
2019-09-01 16:01:49 +0300

    Rewrite [05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb] File in: 2019-09-01 16:01:48

--- ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
+++ ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
@@ -1,207 +1,151 @@
 
         
-            file package(gem, '.tar.gz') => ['pkg/'] do |f|
-      sh <<-SH
-        git archive \
-          --prefix=#{gem}-#{source_version}/ \
-          --format=tar \
-          HEAD -- #{directory} | gzip > #{f.name}
-      SH
+            def not_implemented
+      raise 'Not implemented.'
+    end
+    
+      def user_ids
+    [ topic.user_id, topic.last_post_user_id, *topic.featured_user_ids ]
+  end
+    
+      def handle_spam
+    if @spam
+      GroupMessage.create(Group[:moderators].name,
+                           :spam_post_blocked,
+                           user: @user,
+                           limit_once_per: 24.hours,
+                           message_params: { domains: @post.linked_hosts.keys.join(', ') })
+    elsif @post && errors.blank? && !skip_validations?
+      SpamRule::FlagSockpuppets.new(@post).perform
     end
   end
     
-    class Rack::Builder
-  include Sinatra::Delegator
+          it 'removes the file from s3 on multisite' do
+        test_multisite_connection('default') do
+          upload = build_upload
+          store.expects(:get_depth_for).with(upload.id).returns(0)
+          s3_helper.expects(:s3_bucket).returns(s3_bucket).at_least_once
+          upload.update!(url: '//s3-upload-bucket.s3.dualstack.us-west-1.amazonaws.com/uploads/default/original/1X/#{upload.sha1}.png')
+          s3_object = stub
+    
+      context 'editing category' do
+    
+          if !shadowed_val.nil?
+        setup_shadowed_methods(name, shadowed_val)
+      else
+        setup_methods(name)
+      end
+    end
+  end
+    
+      def test_font_helper_without_suffix
+    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
+  end
+    
+          super(argv)
+    ensure
+      UI.print_warnings
+    end
+    
+            def attrs
+          attrs = {
+            'size' => size,
+            'mtime' => mtime,
+          }.reject { |_k, v| v.nil? }
+          return nil if attrs.empty?
+          attrs.to_s
+        end
+      end
+    
+          LAUNCHSCREEN_STORYBOARD_CONTENTS_IOS_8 = <<-XML.strip_heredoc.freeze
+              <?xml version='1.0' encoding='UTF-8' standalone='no'?>
+              <document type='com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB' version='3.0' toolsVersion='13122.16' systemVersion='17A277' targetRuntime='iOS.CocoaTouch' propertyAccessControl='none' useAutolayout='YES' launchScreen='YES' useTraitCollections='YES' colorMatched='YES' initialViewController='01J-lp-oVM'>
+                <dependencies>
+                  <plugIn identifier='com.apple.InterfaceBuilder.IBCocoaTouchPlugin' version='13104.12'/>
+                  <capability name='documents saved in the Xcode 8 format' minToolsVersion='8.0'/>
+                </dependencies>
+                <scenes>
+                  <!--View Controller-->
+                  <scene sceneID='EHf-IW-A2E'>
+                    <objects>
+                      <viewController id='01J-lp-oVM' sceneMemberID='viewController'>
+                        <layoutGuides>
+                          <viewControllerLayoutGuide type='top' id='rUq-ht-380'/>
+                          <viewControllerLayoutGuide type='bottom' id='a9l-8d-mfx'/>
+                        </layoutGuides>
+                        <view key='view' contentMode='scaleToFill' id='Ze5-6b-2t3'>
+                          <rect key='frame' x='0.0' y='0.0' width='375' height='667'/>
+                          <autoresizingMask key='autoresizingMask' widthSizable='YES' heightSizable='YES'/>
+                          <color key='backgroundColor' red='1' green='1' blue='1' alpha='1' colorSpace='custom' customColorSpace='sRGB'/>
+                        </view>
+                      </viewController>
+                      <placeholder placeholderIdentifier='IBFirstResponder' id='iYj-Kq-Ea1' userLabel='First Responder' sceneMemberID='firstResponder'/>
+                    </objects>
+                    <point key='canvasLocation' x='53' y='375'/>
+                  </scene>
+                </scenes>
+              </document>
+      XML
+    
+                  <% if frame.post_context %>
+              <ol start='<%=h frame.lineno + 1 %>' class='post-context'
+                  id='post-<%= id %>' onclick='toggle(<%= id %>);'>
+                <% frame.post_context.each do |line| %>
+                <li class='post-context-line'><code><%=h line %></code></li>
+                <% end %>
+              </ol>
+              <% end %>
+              <div class='clear'></div>
+            </li>
+    
+      task :index do
+    doc = File.read('README.md')
+    file = 'doc/rack-protection-readme.md'
+    Dir.mkdir 'doc' unless File.directory? 'doc'
+    puts 'writing #{file}'
+    File.open(file, 'w') { |f| f << doc }
+  end
+    
+    module Rack
+  module Protection
+    class Base
+      DEFAULT_OPTIONS = {
+        :reaction    => :default_reaction, :logging   => true,
+        :message     => 'Forbidden',       :encryptor => Digest::SHA1,
+        :session_key => 'rack.session',    :status    => 403,
+        :allow_empty_referrer => true,
+        :report_key           => 'protection.failed',
+        :html_types           => %w[text/html application/xhtml text/xml application/xml]
+      }
+    
+          def call(env)
+        status, headers, body = @app.call(env)
+        header = options[:report_only] ? 'Content-Security-Policy-Report-Only' : 'Content-Security-Policy'
+        headers[header] ||= csp_policy if html? headers
+        [status, headers, body]
+      end
+    end
+  end
 end
 
     
-    <style type='text/css' media='screen'>
-  *                   {margin: 0; padding: 0; border: 0; outline: 0;}
-  div.clear           {clear: both;}
-  body                {background: #EEEEEE; margin: 0; padding: 0;
-                       font-family: 'Lucida Grande', 'Lucida Sans Unicode',
-                       'Garuda';}
-  code                {font-family: 'Lucida Console', monospace;
-                       font-size: 12px;}
-  li                  {height: 18px;}
-  ul                  {list-style: none; margin: 0; padding: 0;}
-  ol:hover            {cursor: pointer;}
-  ol li               {white-space: pre;}
-  #explanation        {font-size: 12px; color: #666666;
-                       margin: 20px 0 0 100px;}
-/* WRAP */
-  #wrap               {width: 1000px; background: #FFFFFF; margin: 0 auto;
-                       padding: 30px 50px 20px 50px;
-                       border-left: 1px solid #DDDDDD;
-                       border-right: 1px solid #DDDDDD;}
-/* HEADER */
-  #header             {margin: 0 auto 25px auto;}
-  #header img         {float: left;}
-  #header #summary    {float: left; margin: 12px 0 0 20px; width:660px;
-                       font-family: 'Lucida Grande', 'Lucida Sans Unicode';}
-  h1                  {margin: 0; font-size: 36px; color: #981919;}
-  h2                  {margin: 0; font-size: 22px; color: #333333;}
-  #header ul          {margin: 0; font-size: 12px; color: #666666;}
-  #header ul li strong{color: #444444;}
-  #header ul li       {display: inline; padding: 0 10px;}
-  #header ul li.first {padding-left: 0;}
-  #header ul li.last  {border: 0; padding-right: 0;}
-/* BODY */
-  #backtrace,
-  #get,
-  #post,
-  #cookies,
-  #rack               {width: 980px; margin: 0 auto 10px auto;}
-  p#nav               {float: right; font-size: 14px;}
-/* BACKTRACE */
-  a#expando           {float: left; padding-left: 5px; color: #666666;
-                      font-size: 14px; text-decoration: none; cursor: pointer;}
-  a#expando:hover     {text-decoration: underline;}
-  h3                  {float: left; width: 100px; margin-bottom: 10px;
-                       color: #981919; font-size: 14px; font-weight: bold;}
-  #nav a              {color: #666666; text-decoration: none; padding: 0 5px;}
-  #backtrace li.frame-info {background: #f7f7f7; padding-left: 10px;
-                           font-size: 12px; color: #333333;}
-  #backtrace ul       {list-style-position: outside; border: 1px solid #E9E9E9;
-                       border-bottom: 0;}
-  #backtrace ol       {width: 920px; margin-left: 50px;
-                       font: 10px 'Lucida Console', monospace; color: #666666;}
-  #backtrace ol li    {border: 0; border-left: 1px solid #E9E9E9;
-                       padding: 2px 0;}
-  #backtrace ol code  {font-size: 10px; color: #555555; padding-left: 5px;}
-  #backtrace-ul li    {border-bottom: 1px solid #E9E9E9; height: auto;
-                       padding: 3px 0;}
-  #backtrace-ul .code {padding: 6px 0 4px 0;}
-  #backtrace.condensed .system,
-  #backtrace.condensed .framework {display:none;}
-/* REQUEST DATA */
-  p.no-data           {padding-top: 2px; font-size: 12px; color: #666666;}
-  table.req           {width: 980px; text-align: left; font-size: 12px;
-                       color: #666666; padding: 0; border-spacing: 0;
-                       border: 1px solid #EEEEEE; border-bottom: 0;
-                       border-left: 0;
-                       clear:both}
-  table.req tr th     {padding: 2px 10px; font-weight: bold;
-                       background: #F7F7F7; border-bottom: 1px solid #EEEEEE;
-                       border-left: 1px solid #EEEEEE;}
-  table.req tr td     {padding: 2px 20px 2px 10px;
-                       border-bottom: 1px solid #EEEEEE;
-                       border-left: 1px solid #EEEEEE;}
-/* HIDE PRE/POST CODE AT START */
-  .pre-context,
-  .post-context       {display: none;}
-    
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
-          end
-        end
-        bad_cookies.empty?
+          def redirect(env)
+        request = Request.new(env)
+        warn env, 'attack prevented by #{self.class}'
+        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
       end
     
-        it 'sets a session authenticity token if one does not exist' do
-      session = {}
-      allow(Rack::Protection::AuthenticityToken).to receive(:random_token).and_return(token)
-      allow_any_instance_of(Rack::Protection::AuthenticityToken).to receive(:mask_token).and_return(masked_token)
-      Rack::Protection::AuthenticityToken.token(session)
-      expect(session[:csrf]).to eq(token)
-    end
+        headers = get('/', {}, 'wants' => 'text/html').headers
+    expect(headers['Content-Security-Policy']).to be_nil
+    expect(headers['Content-Security-Policy-Report-Only']).to eq('connect-src 'self'; default-src none; img-src 'self'; report-uri /my_amazing_csp_report_parser; script-src 'self'; style-src 'self'')
   end
     
-    ENV['REDIS_URL'] ||= 'redis://localhost/15'
-    
-          assert_raises ArgumentError do
-        Sidekiq::Client.push('queue' => 'foo', 'class' => MyWorker, 'noargs' => [1, 2])
+      context 'escaping' do
+    it 'escapes html entities' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']]]
       end
-    
-        it 'logs the exception to Sidekiq.logger' do
-      Component.new.invoke_exception(:a => 1)
-      @str_logger.rewind
-      log = @str_logger.readlines
-      assert_match(/'a':1/, log[0], 'didn't include the context')
-      assert_match(/Something didn't work!/, log[1], 'didn't include the exception message')
-      assert_match(/test\/test_exception_handler.rb/, log[2], 'didn't include the backtrace')
+      get '/', :foo => '<bar>'
+      expect(body).to eq('&lt;bar&gt;')
     end
-    
-        final_action = nil
-    chain.invoke { final_action = true }
-    assert_nil final_action
-    assert_equal [], recorder
-  end
-    
-      config.vm.define 'arch' do |arch|
-    arch.vm.box = 'jfredett/arch-puppet'
-  end
-    
-    require 'fpm'
-require 'tmpdir'
-require 'fpm/package/pleaserun'
-    
-        install_args = [
-      attributes[:npm_bin],
-      'install',
-      # use 'package' or 'package@version'
-     (version ? '#{package}@#{version}' : package)
-    ]
-    
-      private
-  def input(command)
-    platforms = [
-      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
-      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
-      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
-      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
-      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
-    ]
-    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
-                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
-                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
-                             'log_directory', 'log_file_stderr', 'log_file_stdout']
-    
-      # This method is used by the puppet manifest template
-  def puppetsort(hash)
-    # TODO(sissel): Implement sorting that follows the puppet style guide
-    # Such as, 'ensure' goes first, etc.
-    return hash.to_a
-  end # def puppetsort
-    
-    # Support for python packages.
-#
-# This supports input, but not output.
-#
-# Example:
-#
-#     # Download the django python package:
-#     pkg = FPM::Package::Python.new
-#     pkg.input('Django')
-#
-class FPM::Package::Python < FPM::Package
-  # Flags '--foo' will be accessable  as attributes[:python_foo]
-  option '--bin', 'PYTHON_EXECUTABLE',
-    'The path to the python executable you wish to run.', :default => 'python'
-  option '--easyinstall', 'EASYINSTALL_EXECUTABLE',
-    'The path to the easy_install executable tool', :default => 'easy_install'
-  option '--pip', 'PIP_EXECUTABLE',
-    'The path to the pip executable tool. If not specified, easy_install ' \
-    'is used instead', :default => nil
-  option '--pypi', 'PYPI_URL',
-    'PyPi Server uri for retrieving packages.',
-    :default => 'https://pypi.python.org/simple'
-  option '--package-prefix', 'NAMEPREFIX',
-    '(DEPRECATED, use --package-name-prefix) Name to prefix the package ' \
-    'name with.' do |value|
-    logger.warn('Using deprecated flag: --package-prefix. Please use ' \
-                 '--package-name-prefix')
-    value
-  end
-  option '--package-name-prefix', 'PREFIX', 'Name to prefix the package ' \
-    'name with.', :default => 'python'
-  option '--fix-name', :flag, 'Should the target package name be prefixed?',
-    :default => true
-  option '--fix-dependencies', :flag, 'Should the package dependencies be ' \
-    'prefixed?', :default => true
