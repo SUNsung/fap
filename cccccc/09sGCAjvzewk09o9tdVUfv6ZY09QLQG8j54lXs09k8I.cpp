19a66c9593760875d568075bb2d2348a5e806010
SUNsung <weatherius@gmail.com>
2019-09-01 17:08:49 +0300

    Rewrite [09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp] File in: 2019-09-01 17:08:48

--- cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
+++ cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
@@ -1,104 +1,425 @@
 
         
-        /// @brief Fills a Blob with Gaussian-distributed values @f$ x = a @f$.
-template <typename Dtype>
-class GaussianFiller : public Filler<Dtype> {
+            // Create the pipes to which the ConPTY will connect
+    if (CreatePipe(&hPipePTYIn, phPipeOut, NULL, 0) &&
+        CreatePipe(phPipeIn, &hPipePTYOut, NULL, 0))
+    {
+        // Determine required size of Pseudo Console
+        COORD consoleSize{};
+        CONSOLE_SCREEN_BUFFER_INFO csbi{};
+        HANDLE hConsole{ GetStdHandle(STD_OUTPUT_HANDLE) };
+        if (GetConsoleScreenBufferInfo(hConsole, &csbi))
+        {
+            consoleSize.X = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+            consoleSize.Y = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+        }
+    }
+    
+    #include <ImfEnvmapAttribute.h>
+    
+    
+    {    buildTables(base, offset);
+}
+    
+    
+    //--------------------------------------------
+    // Subsampling: pixel (x, y) is present in the
+    // slice only if 
+    //
+    //  x % xSampling == 0 && y % ySampling == 0
+    //
+    //--------------------------------------------
+    
+        if (getVersion (version) != EXR_VERSION)
+    {
+        THROW (IEX_NAMESPACE::InputExc, 'Cannot read '
+                              'version ' << getVersion (version) << ' '
+                              'image files.  Current file format version '
+                              'is ' << EXR_VERSION << '.');
+    }
+    
+    	    if ((dataWindow.max.x - dataWindow.min.x + 1) %
+		    i.channel().xSampling)
+	    {
+		THROW (IEX_NAMESPACE::ArgExc, 'Number of pixels per row in the '
+				    'image's data window is not a multiple '
+				    'of the x subsampling factor of '
+				    'the \'' << i.name() << '\' channel.');
+	    }
+    
+    	    if (im + zerun > iM + 1)
+		tableTooLong();
+    
+    #ifndef IMFINPUTPART_H_
+#define IMFINPUTPART_H_
+    
+    bool os_detect_blob(BLOBNBOX* bbox, OrientationDetector* o,
+                    ScriptDetector* s, OSResults*,
+                    tesseract::Tesseract* tess);
+    
+    // Reads all boxes from the given filename.
+// Reads a specific target_page number if >= 0, or all pages otherwise.
+// Skips blanks if skip_blanks is true.
+// The UTF-8 label of the box is put in texts, and the full box definition as
+// a string is put in box_texts, with the corresponding page number in pages.
+// Each of the output vectors is optional (may be nullptr).
+// Returns false if no boxes are found.
+bool ReadAllBoxes(int target_page, bool skip_blanks, const STRING& filename,
+                  GenericVector<TBOX>* boxes,
+                  GenericVector<STRING>* texts,
+                  GenericVector<STRING>* box_texts,
+                  GenericVector<int>* pages);
+    
+    #endif  // TESSERACT_CCSTRUCT_OCRPARA_H_
+
+    
+    #ifndef TESSERACT_CCUTIL_QRSEQUENCE_H_
+#define TESSERACT_CCUTIL_QRSEQUENCE_H_
+    
+    // A UNICHARMAP stores unique unichars. Each of them is associated with one
+// UNICHAR_ID.
+class UNICHARMAP {
  public:
-  explicit GaussianFiller(const FillerParameter& param)
-      : Filler<Dtype>(param) {}
-  virtual void Fill(Blob<Dtype>* blob) {
-    Dtype* data = blob->mutable_cpu_data();
-    CHECK(blob->count());
-    caffe_rng_gaussian<Dtype>(blob->count(), Dtype(this->filler_param_.mean()),
-        Dtype(this->filler_param_.std()), blob->mutable_cpu_data());
-    int sparse = this->filler_param_.sparse();
-    CHECK_GE(sparse, -1);
-    if (sparse >= 0) {
-      // Sparse initialization is implemented for 'weight' blobs; i.e. matrices.
-      // These have num == channels == 1; width is number of inputs; height is
-      // number of outputs.  The 'sparse' variable specifies the mean number
-      // of non-zero input weights for a given output.
-      CHECK_GE(blob->num_axes(), 1);
-      const int num_outputs = blob->shape(0);
-      Dtype non_zero_probability = Dtype(sparse) / Dtype(num_outputs);
-      rand_vec_.reset(new SyncedMemory(blob->count() * sizeof(int)));
-      int* mask = reinterpret_cast<int*>(rand_vec_->mutable_cpu_data());
-      caffe_rng_bernoulli(blob->count(), non_zero_probability, mask);
-      for (int i = 0; i < blob->count(); ++i) {
-        data[i] *= mask[i];
-      }
-    }
-  }
     }
     
-    template <typename Dtype>
-class LayerRegistry {
- public:
-  typedef shared_ptr<Layer<Dtype> > (*Creator)(const LayerParameter&);
-  typedef std::map<string, Creator> CreatorRegistry;
+    #include 'tessclassifier.h'
+    
+    void ProgressDialog::end_task(const String &p_task) {
     }
     
-      virtual inline int ExactNumBottomBlobs() const { return 3; }
-  virtual inline const char* type() const { return 'ContrastiveLoss'; }
-  /**
-   * Unlike most loss layers, in the ContrastiveLossLayer we can backpropagate
-   * to the first two inputs.
-   */
-  virtual inline bool AllowForceBackward(const int bottom_index) const {
-    return bottom_index != 2;
-  }
+    	PoolVector<Vector3> vertices;
+	struct Polygon {
+		Vector<int> indices;
+	};
+	Vector<Polygon> polygons;
+	Ref<ArrayMesh> debug_mesh;
+    
+    	virtual bool is_open() const;
+    
+    	static void CALLBACK read(HMIDIIN hMidiIn, UINT wMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2);
+    
+    void GDAPI godot_pool_vector2_array_resize(godot_pool_vector2_array *p_self, const godot_int p_size) {
+	PoolVector<Vector2> *self = (PoolVector<Vector2> *)p_self;
+	self->resize(p_size);
+}
+    
+    void unregister_upnp_types() {
+}
+
     
     
-    {  size_t *workspace_fwd_sizes_;
-  size_t *workspace_bwd_data_sizes_;
-  size_t *workspace_bwd_filter_sizes_;
-  size_t workspaceSizeInBytes;  // size of underlying storage
-  void *workspaceData;  // underlying storage
-  void **workspace;  // aliases into workspaceData
+    {	UPNP();
+	~UPNP();
 };
-#endif
     
-      bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnLRNDescriptor_t norm_desc_;
-  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
-    
-    #ifdef USE_CUDNN
-/**
- * @brief cuDNN implementation of SoftmaxLayer.
- *        Fallback to SoftmaxLayer for CPU mode.
- */
-template <typename Dtype>
-class CuDNNSoftmaxLayer : public SoftmaxLayer<Dtype> {
- public:
-  explicit CuDNNSoftmaxLayer(const LayerParameter& param)
-      : SoftmaxLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNSoftmaxLayer();
+    class PowerAndroid {
     }
+    
+    #ifndef POWER_IPHONE_H
+#define POWER_IPHONE_H
+    
+    	bool disabled;
+    
+        void release();
+    buf_lock_t *get() { return &sb_buf_; }
+    
+                rdb_live_deletion_context_t deletion_context;
+            std::vector<rdb_modification_report_t> mod_reports;
+            key_range_t deleted_range;
+            rdb_erase_small_range(
+                store.btree.get(),
+                &tester,
+                key_range_t::universe(),
+                super_block.get(),
+                &deletion_context,
+                &dummy_interruptor,
+                0,
+                &mod_reports,
+                &deleted_range);
+    
+    // Overload for wchar_t type.
+// Prints a wchar_t as a symbol if it is printable or as its internal
+// code otherwise and also as its decimal code (except for L'\0').
+// The L'\0' char is printed as 'L'\\0''. The decimal code is printed
+// as signed integer when wchar_t is implemented by the compiler
+// as a signed type and is printed as an unsigned integer when wchar_t
+// is implemented as an unsigned type.
+GTEST_API_ void PrintTo(wchar_t wc, ::std::ostream* os);
+    
+    // Next, associate a list of types with the test case, which will be
+// repeated for each type in the list.  The typedef is necessary for
+// the macro to parse correctly.
+typedef testing::Types<char, int, unsigned int> MyTypes;
+TYPED_TEST_CASE(FooTest, MyTypes);
+    
+    template <typename T>
+class linked_ptr {
+ public:
+  typedef T element_type;
+    }
+    
+        TableBuilder* builder = new TableBuilder(options, file);
+    meta->smallest.DecodeFrom(iter->key());
+    for (; iter->Valid(); iter->Next()) {
+      Slice key = iter->key();
+      meta->largest.DecodeFrom(key);
+      builder->Add(key, iter->value());
+    }
+    
+    struct Options;
+struct FileMetaData;
     
     
     { private:
-  /*! \brief the underlying stream */
-  dmlc::Stream *stream_;
-  /*! \brief buffer to hold data */
-  std::string buffer_;
-  /*! \brief length of valid data in buffer */
-  size_t read_len_;
-  /*! \brief pointer in the buffer */
-  size_t read_ptr_;
+  std::string dbname_;
+  Env* env_;
+  DB* db_;
 };
     
-    template<typename DType>
-inline void CompressArray<DType>::InitCompressChunks(
-    const std::vector<bst_uint>& chunk_ptr) {
-  raw_chunks_ = chunk_ptr;
-  CHECK_GE(raw_chunks_.size(), 2);
-  out_buffer_.resize(raw_chunks_.size() - 1);
-  for (size_t i = 0; i < out_buffer_.size(); ++i) {
-    out_buffer_[i].resize(raw_chunks_[i + 1] - raw_chunks_[i]);
+      static Key RandomTarget(Random* rnd) {
+    switch (rnd->Next() % 10) {
+      case 0:
+        // Seek to beginning
+        return MakeKey(0, 0);
+      case 1:
+        // Seek to end
+        return MakeKey(K, 0);
+      default:
+        // Seek to middle
+        return MakeKey(rnd->Next() % K, 0);
+    }
+  }
+    
+      // If the cache contains entry for key, erase it.  Note that the
+  // underlying entry will be kept around until all existing handles
+  // to it have been released.
+  virtual void Erase(const Slice& key) = 0;
+    
+      // create first key range
+  leveldb::WriteBatch batch;
+  for (size_t i = 0; i < kNumKeys; i++) {
+    batch.Put(Key1(i), 'value for range 1 key');
+  }
+  ASSERT_OK(db->Write(leveldb::WriteOptions(), &batch));
+    
+    inline uint32_t AcceleratedCRC32C(uint32_t crc, const char* buf, size_t size) {
+#if HAVE_CRC32C
+  return ::crc32c::Extend(crc, reinterpret_cast<const uint8_t*>(buf), size);
+#else
+  // Silence compiler warnings about unused arguments.
+  (void)crc;
+  (void)buf;
+  (void)size;
+  return 0;
+#endif  // HAVE_CRC32C
+}
+    
+    void FilterBlockBuilder::GenerateFilter() {
+  const size_t num_keys = start_.size();
+  if (num_keys == 0) {
+    // Fast path if there are no keys for this filter
+    filter_offsets_.push_back(result_.size());
+    return;
+  }
+    }
+    
+    class Comparator;
+class Iterator;
+    
+      std::string s;
+  for (size_t i = 0; i < values.size(); i++) {
+    PutVarint64(&s, values[i]);
+  }
+    
+    /****************************************************************************\
+* void DisplayNum(void)
+*
+* Convert m_currentVal to a string in the current radix.
+*
+* Updates the following variables:
+*   m_currentVal, m_numberString
+\****************************************************************************/
+//
+// State of calc last time DisplayNum was called
+//
+typedef struct
+{
+    Rational value;
+    int32_t precision;
+    uint32_t radix;
+    int nFE;
+    NUM_WIDTH numwidth;
+    bool fIntMath;
+    bool bRecord;
+    bool bUseSep;
+} LASTDISP;
+    
+            MANTTYPE* ptrRet = ret->mant;
+        for (auto const& digit : this->Mantissa())
+        {
+            *ptrRet++ = digit;
+        }
+    
+            CalcInput(wchar_t decSymbol)
+            : m_hasExponent(false)
+            , m_hasDecimal(false)
+            , m_decPtIndex(0)
+            , m_decSymbol(decSymbol)
+            , m_base()
+            , m_exponent()
+        {
+        }
+    
+        /**
+     * Sets the global Timestamp to be 'newTime'.
+     */
+    void setNewTimestamp(ServiceContext* opCtx, const Timestamp& newTime);
+    
+    
+    {    // Conservatvely estimated size of the batch, for ensuring it doesn't grow past the maximum BSON
+    // size
+    int _estimatedSizeBytes{0};
+};
+    
+        // The working set used to pass intermediate results between stages. Not owned
+    // by us.
+    WorkingSet* _ws;
+    
+    /**
+ * Factory method
+ */
+static Transliterator* RemoveTransliterator_create(const UnicodeString& /*ID*/,
+                                                   Transliterator::Token /*context*/) {
+    /* We don't need the ID or context. We just remove data */
+    return new RemoveTransliterator();
+}
+    
+    ScriptSet &ScriptSet::Union(const ScriptSet &other) {
+    for (uint32_t i=0; i<UPRV_LENGTHOF(bits); i++) {
+        bits[i] |= other.bits[i];
+    }
+    return *this;
+}
+    
+        UnicodeString &displayScripts(UnicodeString &dest) const; // append script names to dest string.
+    ScriptSet & parseScripts(const UnicodeString &scriptsString, UErrorCode &status);  // Replaces ScriptSet contents.
+    
+    UnicodeString&
+SelectFormat::format(const UnicodeString& keyword,
+                     UnicodeString& appendTo,
+                     FieldPosition& /*pos */,
+                     UErrorCode& status) const {
+    if (U_FAILURE(status)) {
+        return appendTo;
+    }
+    // Check for the validity of the keyword
+    if (!PatternProps::isIdentifier(keyword.getBuffer(), keyword.length())) {
+        status = U_ILLEGAL_ARGUMENT_ERROR;  // Invalid formatting argument.
+    }
+    if (msgPattern.countParts() == 0) {
+        status = U_INVALID_STATE_ERROR;
+        return appendTo;
+    }
+    int32_t msgStart = findSubMessage(msgPattern, 0, keyword, status);
+    if (!MessageImpl::jdkAposMode(msgPattern)) {
+        int32_t patternStart = msgPattern.getPart(msgStart).getLimit();
+        int32_t msgLimit = msgPattern.getLimitPartIndex(msgStart);
+        appendTo.append(msgPattern.getPatternString(),
+                        patternStart,
+                        msgPattern.getPatternIndex(msgLimit) - patternStart);
+        return appendTo;
+    }
+    // JDK compatibility mode: Remove SKIP_SYNTAX.
+    return MessageImpl::appendSubMessageWithoutSkipSyntax(msgPattern, msgStart, appendTo);
+}
+    
+    #define DOT               ((UChar)0x002E)
+#define SINGLE_QUOTE      ((UChar)0x0027)
+#define SLASH             ((UChar)0x002F)
+#define BACKSLASH         ((UChar)0x005C)
+#define SPACE             ((UChar)0x0020)
+#define TAB               ((UChar)0x0009)
+#define QUOTATION_MARK    ((UChar)0x0022)
+#define ASTERISK          ((UChar)0x002A)
+#define COMMA             ((UChar)0x002C)
+#define HYPHEN            ((UChar)0x002D)
+#define U_ZERO            ((UChar)0x0030)
+#define U_ONE             ((UChar)0x0031)
+#define U_TWO             ((UChar)0x0032)
+#define U_THREE           ((UChar)0x0033)
+#define U_FOUR            ((UChar)0x0034)
+#define U_FIVE            ((UChar)0x0035)
+#define U_SIX             ((UChar)0x0036)
+#define U_SEVEN           ((UChar)0x0037)
+#define U_EIGHT           ((UChar)0x0038)
+#define U_NINE            ((UChar)0x0039)
+#define COLON             ((UChar)0x003A)
+#define SEMI_COLON        ((UChar)0x003B)
+#define CAP_A             ((UChar)0x0041)
+#define CAP_B             ((UChar)0x0042)
+#define CAP_R             ((UChar)0x0052)
+#define CAP_Z             ((UChar)0x005A)
+#define LOWLINE           ((UChar)0x005F)
+#define LEFTBRACE         ((UChar)0x007B)
+#define RIGHTBRACE        ((UChar)0x007D)
+    
+    
+class U_I18N_API SharedDateFormatSymbols : public SharedObject {
+public:
+    SharedDateFormatSymbols(
+            const Locale &loc, const char *type, UErrorCode &status)
+            : dfs(loc, type, status) { }
+    virtual ~SharedDateFormatSymbols();
+    const DateFormatSymbols &get() const { return dfs; }
+private:
+    DateFormatSymbols dfs;
+    SharedDateFormatSymbols(const SharedDateFormatSymbols &);
+    SharedDateFormatSymbols &operator=(const SharedDateFormatSymbols &);
+};
+    
+    #endif
+#endif
+
+    
+    void HostHealthMonitor::notifyObservers(HealthLevel newStatus) {
+  if (newStatus != m_status) {
+    Logger::Warning('Health level (lower is better) changes from %d to %d.',
+                    static_cast<int>(m_status), static_cast<int>(newStatus));
+    }
+    }
+    
+    
+    {///////////////////////////////////////////////////////////////////////////////
+}
+    
+    void ProxygenTransport::messageAvailable(ResponseMessage&& message) noexcept {
+  if (!m_clientTxn) {
+    return;
+  }
+    }
+    
+    
+    {  if (setOpNeedsTypeCheck(prop.typeConstraint, Op, lhs)) {
+    /*
+     * If this property has a type-hint, we can't do the setop truely in
+     * place. We need to verify that the new value satisfies the type-hint
+     * before assigning back to the property (if we raise a warning and throw,
+     * we don't want to have already put the value into the prop).
+     */
+    Cell temp;
+    cellDup(*lhs, temp);
+    SCOPE_FAIL { tvDecRefGen(&temp); };
+    setOpOpToHelper(Op)(&temp, rhs);
+    prop.typeConstraint.verifyProperty(&temp, cls, prop.cls, prop.name);
+    cellMove(temp, lhs);
+  } else {
+    setOpOpToHelper(Op)(lhs, rhs);
   }
 }
+    
+    /*
+ * Version identifier for the hhbc repo schema.  Normally this is determined at
+ * build-time, but it can be overridden at run-time via the
+ * HHVM_RUNTIME_REPO_SCHEMA environment variable.
+ */
+folly::StringPiece repoSchemaId();
