a8c5b54f420b86f8fccfdad1dde5d7904c69bd6b
SUNsung <weatherius@gmail.com>
2019-09-02 10:01:39 +0300

    Rewrite [42qjH3Gl5znAc42ArAVKm2GgPQ429OwSbEhkR.642CVB.rb] File in: 2019-09-02 10:01:39

--- ruby/42qjH3Gl5znAc42ArAVKm2GgPQ429OwSbEhkR.642CVB.rb
+++ ruby/42qjH3Gl5znAc42ArAVKm2GgPQ429OwSbEhkR.642CVB.rb
@@ -1,41 +1,179 @@
 
         
-                  # Check for long inreplace block vars
-          find_all_blocks(body_node, :inreplace) do |node|
-            block_arg = node.arguments.children.first
-            next unless block_arg.source.size > 1
-    
-      describe '#satisfied?' do
-    subject { described_class.new(%w[1.8]) }
-    
-          expect(spec.deps).to be_empty
-    end
-    
-        it 'special cases the cxx11 option' do
-      subject.option(:cxx11)
-      expect(subject).to have_defined_option('c++11')
-      expect(subject).not_to have_defined_option('cxx11')
-    end
-    
-          # Look in the standard locations, because even if port or fink are
-      # not in the path they can still break builds if the build scripts
-      # have these paths baked in.
-      %w[/sw/bin/fink /opt/local/bin/port].each do |ponk|
-        path = Pathname.new(ponk)
-        paths << path if path.exist?
+              it 'returns an active nav link with menu' do
+        stub(self).current_page?('/things') { true }
+        stub(self).current_page?('/things/stuff') { false }
+        nav = nav_link('Things', '/things') { nav_link('Stuff', '/things/stuff') }
+        expect(nav).to be_html_safe
+        a0 = Nokogiri(nav).at('li.dropdown.dropdown-hover.active > a[href='/things']')
+        expect(a0).to be_a Nokogiri::XML::Element
+        expect(a0.text.strip).to eq('Things')
+        a1 = Nokogiri(nav).at('li.dropdown.dropdown-hover.active > li:not(.active) > a[href='/things/stuff']')
+        expect(a1).to be_a Nokogiri::XML::Element
+        expect(a1.text.strip).to eq('Stuff')
       end
     
-          if external_command = which('brewcask-#{command}', path)
-        exec external_command, *ARGV[1..-1]
+        it 'does not output links to other agents outside of the incoming set' do
+      Link.create!(:source_id => agents(:jane_weather_agent).id, :receiver_id => agents(:jane_website_agent).id)
+      Link.create!(:source_id => agents(:jane_website_agent).id, :receiver_id => agents(:jane_rain_notifier_agent).id)
+    
+    describe ConvertWebsiteAgentTemplateForMerge do
+  let :old_extract do
+    {
+      'url' => { 'css' => '#comic img', 'value' => '@src' },
+      'title' => { 'css' => '#comic img', 'value' => '@alt' },
+      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
+    }
+  end
+    
+      describe '#check' do
+    it 'should check that initial run creates an event' do
+      @checker.memory[:last_updated_at] = '2016-03-15T14:01:05+00:00'
+      expect { @checker.check }.to change { Event.count }.by(1)
+    end
+  end
+end
+
+    
+      def authenticate_with_2fa
+    self.resource = find_user
+    
+    if rails_env != 'development'
+  config('path vendor/bundle')
+  config('frozen true')
+  config('disable_shared_gems true')
+end
+    
+      def create_hash(model, opts={})
+    opts[:range] ||= 1.day
+    plural = model.to_s.underscore.pluralize
+    eval(<<DATA
+      @#{plural} = {
+        :day_before => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]*2})..Time.now.midnight - #{opts[:range]})).count,
+        :yesterday => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]})..Time.now.midnight)).count
+      }
+      @#{plural}[:change] = percent_change(@#{plural}[:yesterday], @#{plural}[:day_before])
+DATA
+    )
+  end
+    
+          def auth_user_unless_prompt_none!
+        prompt = params[:prompt]
+        if prompt && prompt.include?('none')
+          handle_prompt_none
+        elsif prompt && prompt.include?('login')
+          new_params = params.except('controller', 'action').permit!.to_h.merge(prompt: prompt.remove('login'))
+          reauthenticate(new_params)
+        else
+          authenticate_user!
+        end
       end
     
-        bintray_user = ENV['HOMEBREW_BINTRAY_USER']
-    bintray_key = ENV['HOMEBREW_BINTRAY_KEY']
-    raise 'Missing HOMEBREW_BINTRAY_USER or HOMEBREW_BINTRAY_KEY variables!' if !bintray_user || !bintray_key
+          def get_key_from_kid(keys, kid)
+        keys.each do |key|
+          return key if key.has_value?(kid)
+        end
+      end
     
-      def install_bottle_for?(dep, build)
-    return pour_bottle? if dep == formula
-    return false if ARGV.build_formula_from_source?(dep)
-    return false unless dep.bottle && dep.pour_bottle?
-    return false unless build.used_options.empty?
-    return false unless dep.bottle.compatible_cellar?
+        @contact = current_user.share_with(@person, @aspect)
+    
+      def set_up_contacts
+    if params[:a_id].present?
+      @aspect = current_user.aspects.find(params[:a_id])
+      gon.preloads[:aspect] = AspectPresenter.new(@aspect).as_json
+    end
+    @contacts_size = current_user.contacts.size
+  end
+    
+      private
+    
+        respond_to do |format|
+      format.html
+      format.xml { render :xml => @notifications.to_xml }
+      format.json {
+        render json: render_as_json(@unread_notification_count, @grouped_unread_notification_counts, @notifications)
+      }
+    end
+  end
+    
+            if Utils::HttpClient.remote_file_exist?(uri)
+          PluginManager.ui.debug('Found package at: #{uri}')
+          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
+        else
+          PluginManager.ui.debug('Package not found at: #{uri}')
+          return nil
+        end
+      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
+        # This probably means there is a firewall in place of the proxy is not correctly configured.
+        # So lets skip this strategy but log a meaningful errors.
+        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
+    
+        desc 'Generate a valid ssh-config'
+    task :ssh_config do
+      require 'json'
+      # Loop until the Vagrant box finishes SSH bootstrap
+      raw_ssh_config = Stud.try(50.times, LogStash::CommandExecutor::CommandError) do
+          LogStash::VagrantHelpers.fetch_config.stdout.split('\n');
+      end
+      parsed_ssh_config = LogStash::VagrantHelpers.parse(raw_ssh_config)
+      File.write('.vm_ssh_config', parsed_ssh_config.to_json)
+    end
+    
+        context 'with a specific plugin' do
+      let(:plugin_name) { 'logstash-input-stdin' }
+      it 'list the plugin and display the plugin name' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
+      end
+    
+        context 'update all the plugins' do
+      it 'has executed successfully' do
+        logstash.run_command_in_path('bin/logstash-plugin update --no-verify')
+        expect(logstash).to have_installed?(plugin_name, '0.1.1')
+      end
+    end
+  end
+end
+
+    
+    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
+
+    
+        # Loops through the list of category pages and processes each one.
+    def write_category_indexes
+      if self.layouts.key? 'category_index'
+        dir = self.config['category_dir'] || 'categories'
+        self.categories.keys.each do |category|
+          self.write_category_index(File.join(dir, category.to_url), category)
+        end
+    
+          locations = Array.new
+      while (data.code.to_i == 301 || data.code.to_i == 302)
+        data = handle_gist_redirecting(data)
+        break if locations.include? data.header['Location']
+        locations << data.header['Location']
+      end
+    
+          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
+        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
+        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
+          @img['title']  = title
+          @img['alt']    = alt
+        else
+          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
+        end
+        @img['class'].gsub!(/'/, '') if @img['class']
+      end
+      super
+    end
+    
+    Liquid::Template.register_tag('include_code', Jekyll::IncludeCodeTag)
+
+    
+          unless file.file?
+        return 'File #{file} could not be found'
+      end
+    
+        def poster
+      'poster='#{@poster}'' if @poster
+    end
