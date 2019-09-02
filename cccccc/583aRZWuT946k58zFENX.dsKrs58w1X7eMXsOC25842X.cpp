19befbcf43259a78e034f484fc87241c697cf568
SUNsung <weatherius@gmail.com>
2019-09-03 01:51:45 +0300

    Rewrite [583aRZWuT946k58zFENX.dsKrs58w1X7eMXsOC25842X.cpp] File in: 2019-09-03 01:51:44

--- cccccc/583aRZWuT946k58zFENX.dsKrs58w1X7eMXsOC25842X.cpp
+++ cccccc/583aRZWuT946k58zFENX.dsKrs58w1X7eMXsOC25842X.cpp
@@ -1,177 +1,80 @@
 
         
-        #include <QIcon>
-#include <QPixmap>
-#include <QString>
-    
-        /* Check bad contexts and NULLs for signing */
-    ecount = 0;
-    CHECK(secp256k1_ecdsa_sign_recoverable(none, &recsig, message, privkey, NULL, NULL) == 0);
-    CHECK(ecount == 1);
-    CHECK(secp256k1_ecdsa_sign_recoverable(sign, &recsig, message, privkey, NULL, NULL) == 1);
-    CHECK(ecount == 1);
-    CHECK(secp256k1_ecdsa_sign_recoverable(vrfy, &recsig, message, privkey, NULL, NULL) == 0);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_sign_recoverable(both, &recsig, message, privkey, NULL, NULL) == 1);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_sign_recoverable(both, NULL, message, privkey, NULL, NULL) == 0);
-    CHECK(ecount == 3);
-    CHECK(secp256k1_ecdsa_sign_recoverable(both, &recsig, NULL, privkey, NULL, NULL) == 0);
-    CHECK(ecount == 4);
-    CHECK(secp256k1_ecdsa_sign_recoverable(both, &recsig, message, NULL, NULL, NULL) == 0);
-    CHECK(ecount == 5);
-    /* This will fail or succeed randomly, and in either case will not ARG_CHECK failure */
-    secp256k1_ecdsa_sign_recoverable(both, &recsig, message, privkey, recovery_test_nonce_function, NULL);
-    CHECK(ecount == 5);
-    /* These will all fail, but not in ARG_CHECK way */
-    CHECK(secp256k1_ecdsa_sign_recoverable(both, &recsig, message, zero_privkey, NULL, NULL) == 0);
-    CHECK(secp256k1_ecdsa_sign_recoverable(both, &recsig, message, over_privkey, NULL, NULL) == 0);
-    /* This one will succeed. */
-    CHECK(secp256k1_ecdsa_sign_recoverable(both, &recsig, message, privkey, NULL, NULL) == 1);
-    CHECK(ecount == 5);
-    
-        BOOST_CHECK(v.setArray());
-    BOOST_CHECK(v.isArray());
-    BOOST_CHECK_EQUAL(v.size(), 0);
-    
-    ROTATE_ARGS
-	movdqa	XTMP2, XTMP3	; XTMP2 = W[-15]
-    mov	y0, e		; y0 = e
-    mov	y1, a		; y1 = a
-	movdqa	XTMP4, XTMP3	; XTMP4 = W[-15]
-    ror	y0, (25-11)	; y0 = e >> (25-11)
-    xor	y0, e		; y0 = e ^ (e >> (25-11))
-    mov	y2, f		; y2 = f
-    ror	y1, (22-13)	; y1 = a >> (22-13)
-	pslld	XTMP3, (32-18)
-    xor	y1, a		; y1 = a ^ (a >> (22-13)
-    ror	y0, (11-6)	; y0 = (e >> (11-6)) ^ (e >> (25-6))
-    xor	y2, g		; y2 = f^g
-	psrld	XTMP2, 18
-    ror	y1, (13-2)	; y1 = (a >> (13-2)) ^ (a >> (22-2))
-    xor	y0, e		; y0 = e ^ (e >> (11-6)) ^ (e >> (25-6))
-    and	y2, e		; y2 = (f^g)&e
-    ror	y0, 6		; y0 = S1 = (e>>6) & (e>>11) ^ (e>>25)
-	pxor	XTMP1, XTMP3
-    xor	y1, a		; y1 = a ^ (a >> (13-2)) ^ (a >> (22-2))
-    xor	y2, g		; y2 = CH = ((f^g)&e)^g
-	psrld	XTMP4, 3	; XTMP4 = W[-15] >> 3
-    add	y2, y0		; y2 = S1 + CH
-    add	y2, [rsp + _XFER + 1*4]	; y2 = k + w + S1 + CH
-    ror	y1, 2		; y1 = S0 = (a>>2) ^ (a>>13) ^ (a>>22)
-	pxor	XTMP1, XTMP2	; XTMP1 = W[-15] ror 7 ^ W[-15] ror 18
-    mov	y0, a		; y0 = a
-    add	h, y2		; h = h + S1 + CH + k + w
-    mov	y2, a		; y2 = a
-	pxor	XTMP1, XTMP4	; XTMP1 = s0
-    or	y0, c		; y0 = a|c
-    add	d, h		; d = d + h + S1 + CH + k + w
-    and	y2, c		; y2 = a&c
-	;; compute low s1
-	pshufd	XTMP2, X3, 11111010b	; XTMP2 = W[-2] {BBAA}
-    and	y0, b		; y0 = (a|c)&b
-    add	h, y1		; h = h + S1 + CH + k + w + S0
-	paddd	XTMP0, XTMP1	; XTMP0 = W[-16] + W[-7] + s0
-    or	y0, y2		; y0 = MAJ = (a|c)&b)|(a&c)
-    add	h, y0		; h = h + S1 + CH + k + w + S0 + MAJ
-    
-    static std::string IKey(const std::string& user_key,
-                        uint64_t seq,
-                        ValueType vt) {
-  std::string encoded;
-  AppendInternalKey(&encoded, ParsedInternalKey(user_key, seq, vt));
-  return encoded;
-}
-    
-    // Return the legacy file name for an sstable with the specified number
-// in the db named by 'dbname'. The result will be prefixed with
-// 'dbname'.
-extern std::string SSTTableFileName(const std::string& dbname, uint64_t number);
-    
-      fname = LockFileName('foo');
-  ASSERT_EQ('foo/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(0, number);
-  ASSERT_EQ(kDBLockFile, type);
-    
-    #define guarantee_xwinerr(cond, err, msg, ...) do {                     \
-        if (UNLIKELY(!(cond))) {                                        \
-            DWORD guarantee_winerr_err = (err);                         \
-            crash_or_trap(format_assert_message('Guarantee', (cond)) '(error 0x%x - %s) ' msg, guarantee_winerr_err, winerr_string(guarantee_winerr_err).c_str(), ##__VA_ARGS__); \
-        }                                                               \
-    } while (0)
-    
-        // Used by `delayed_clear_and_drop_sindex` and during index post-construction.
-    // Clears out a slice of a secondary index.
-    void clear_sindex_data(
-            uuid_u sindex_id,
-            value_sizer_t *sizer,
-            const deletion_context_t *deletion_context,
-            const key_range_t &pkey_range_to_clear,
-            signal_t *interruptor)
-            THROWS_ONLY(interrupted_exc_t);
-    
-    class DerivedTest : public BaseTest, public ::testing::WithParamInterface<int> {
-  // The usual test fixture members go here too.
-};
-    
-    // This helper class can be used to mock out Google Test failure reporting
-// so that we can test Google Test or code that builds on Google Test.
-//
-// An object of this class appends a TestPartResult object to the
-// TestPartResultArray object given in the constructor whenever a Google Test
-// failure is reported. It can either intercept only failures that are
-// generated in the same thread that created this object or it can intercept
-// all generated failures. The scope of this mock object can be controlled with
-// the second argument to the two arguments constructor.
-class GTEST_API_ ScopedFakeTestPartResultReporter
-    : public TestPartResultReporterInterface {
- public:
-  // The two possible mocking modes of this object.
-  enum InterceptMode {
-    INTERCEPT_ONLY_CURRENT_THREAD,  // Intercepts only thread local failures.
-    INTERCEPT_ALL_THREADS           // Intercepts all failures.
-  };
+          // Take the ClassHierarchyAnalysis into account.
+  // If a given class has no subclasses and
+  // - private
+  // - or internal and it is a WMO compilation
+  // then this class can be considered final for the purpose
+  // of devirtualization.
+  if (CHA) {
+    if (!CHA->hasKnownDirectSubclasses(CD)) {
+      switch (CD->getEffectiveAccess()) {
+      case AccessLevel::Open:
+        return false;
+      case AccessLevel::Public:
+      case AccessLevel::Internal:
+        if (!M.isWholeModule())
+          return false;
+        break;
+      case AccessLevel::FilePrivate:
+      case AccessLevel::Private:
+        break;
+      }
+    }
     }
     
-    namespace internal {
-    }
+    class ClassDecl;
+enum IsSerialized_t : unsigned char;
+class SILFunction;
+class SILModule;
     
-    // 5-ary predicate assertion macros.
-#define EXPECT_PRED_FORMAT5(pred_format, v1, v2, v3, v4, v5) \
-  GTEST_PRED_FORMAT5_(pred_format, v1, v2, v3, v4, v5, GTEST_NONFATAL_FAILURE_)
-#define EXPECT_PRED5(pred, v1, v2, v3, v4, v5) \
-  GTEST_PRED5_(pred, v1, v2, v3, v4, v5, GTEST_NONFATAL_FAILURE_)
-#define ASSERT_PRED_FORMAT5(pred_format, v1, v2, v3, v4, v5) \
-  GTEST_PRED_FORMAT5_(pred_format, v1, v2, v3, v4, v5, GTEST_FATAL_FAILURE_)
-#define ASSERT_PRED5(pred, v1, v2, v3, v4, v5) \
-  GTEST_PRED5_(pred, v1, v2, v3, v4, v5, GTEST_FATAL_FAILURE_)
     
-      // Returns the floating-point number that represent positive infinity.
-  static RawType Infinity() {
-    return ReinterpretBits(kExponentBitMask);
+    {    return Impl1 == Impl2;
   }
     
-      virtual void fillMessage(Dict* msgDict) CXX11_OVERRIDE;
+    KNOWN_STDLIB_TYPE_DECL(AnyKeyPath, NominalTypeDecl, 0)
+KNOWN_STDLIB_TYPE_DECL(PartialKeyPath, NominalTypeDecl, 1)
+KNOWN_STDLIB_TYPE_DECL(KeyPath, NominalTypeDecl, 2)
+KNOWN_STDLIB_TYPE_DECL(WritableKeyPath, NominalTypeDecl, 2)
+KNOWN_STDLIB_TYPE_DECL(ReferenceWritableKeyPath, NominalTypeDecl, 2)
+    
+    #ifndef SWIFT_REFLECTION_METADATASOURCEBUILDER_H
+#define SWIFT_REFLECTION_METADATASOURCEBUILDER_H
+    
+      const Derived &asDerived() const {
+    return *static_cast<const Derived *>(this);
+  }
     
     
-    {} // namespace aria2
+    {} // end namespace swift
     
-      std::shared_ptr<DHTNode> localNode_;
+    #endif // SWIFT_BASIC_PRIMARYSPECIFICPATHS_H
+
     
-      virtual void startup() = 0;
-    
-    class DHTTaskExecutor {
-private:
-  int numConcurrent_;
-  std::vector<std::shared_ptr<DHTTask>> execTasks_;
-  std::deque<std::shared_ptr<DHTTask>> queue_;
+    namespace swift {
+struct SupplementaryOutputPaths {
+  /// The path to which we should emit an Objective-C header for the module.
+  ///
+  /// Currently only makes sense when the compiler has whole module knowledge.
+  /// The modes for which it makes sense incuide both WMO and the 'merge
+  /// modules' job that happens after the normal compilation jobs. That's where
+  /// the header is emitted in single-file mode, since it needs whole-module
+  /// information.
+  ///
+  /// \sa swift::printAsObjC
+  std::string ObjCHeaderOutputPath;
+    }
     }
     
-        // change element with key 'the bad'
-    object.at('the bad') = 'il cattivo';
-    
-    
-    {    std::cout << null << '\n';
-    std::cout << *res2.first << ' ' << std::boolalpha << res2.second << '\n';
+    void Config::files(std::function<void(const std::string& category,
+                                      const std::vector<std::string>& files)>
+                       predicate) const {
+  RecursiveLock lock(config_files_mutex_);
+  for (const auto& it : files_) {
+    for (const auto& category : it.second) {
+      predicate(category.first, category.second);
+    }
+  }
 }
+    
+    boost::filesystem::path const& getTestConfigDirectory();
