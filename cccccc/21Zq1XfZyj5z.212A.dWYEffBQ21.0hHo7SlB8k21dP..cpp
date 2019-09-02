59c89c81c7e8811d2dbc0413c54c64dd92db2b22
SUNsung <weatherius@gmail.com>
2019-09-02 19:21:59 +0300

    Rewrite [21Zq1XfZyj5z.212A.dWYEffBQ21.0hHo7SlB8k21dP..cpp] File in: 2019-09-02 19:21:59

--- cccccc/21Zq1XfZyj5z.212A.dWYEffBQ21.0hHo7SlB8k21dP..cpp
+++ cccccc/21Zq1XfZyj5z.212A.dWYEffBQ21.0hHo7SlB8k21dP..cpp
@@ -1,127 +1,166 @@
 
         
-                        for (; i < length; ++i)
-                {
-                    wchar_t ch = localizedString[i];
-                    if (!IsEnUsDigit(ch))
-                    {
-                        for (int j = 0; j < 10; ++j)
-                        {
-                            if (ch == m_digitSymbols[j])
-                            {
-                                ch = j.ToString()->Data()[0];
-                                break;
-                                // ch = val - L'0';
-                            }
-                        }
-                    }
-                    if (ch == m_decimalSeparator)
-                    {
-                        ch = L'.';
-                    }
-                    englishString[i] = ch;
-                }
-                englishString[i] = '\0';
-    
-    /// <summary>
-/// Invoked when application execution is being suspended. Application state is saved
-/// without knowing whether the application will be terminated or resumed with the contents
-/// of memory still intact.
-/// </summary>
-/// <param name='sender'>The source of the suspend request.</param>
-/// <param name='e'>Details about the suspend request.</param>
-void App::OnSuspending(Object ^ sender, SuspendingEventArgs ^ e)
-{
-    (void)sender; // Unused parameter
-    (void)e;      // Unused parameter
-    }
-    
-    
-    {    float posX = 0;
-    auto lastChild = Children->GetAt(Children->Size - 1);
-    float lastChildWidth = 0;
-    if (Children->Size > 2 && ShouldPrioritizeLastItem())
-    {
-        lastChildWidth = lastChild->DesiredSize.Width;
-    }
-    for (auto item : Children)
-    {
-        auto widthAvailable = finalSize.Width - posX;
-        if (item != lastChild)
-        {
-            widthAvailable -= lastChildWidth;
-        }
-        float itemWidth = item->DesiredSize.Width;
-        if (widthAvailable > 0 && itemWidth <= widthAvailable)
-        {
-            // stack the items horizontally (left to right)
-            item->Arrange(Rect(posX, 0, itemWidth, finalSize.Height));
-            AutomationProperties::SetAccessibilityView(item, ::AccessibilityView::Content);
-            posX += item->RenderSize.Width;
-        }
-        else
-        {
-            // Not display the item
-            item->Arrange(Rect(0, 0, 0, 0));
-            AutomationProperties::SetAccessibilityView(item, ::AccessibilityView::Raw);
-        }
-    }
-    return finalSize;
-}
+        #endif  // TESSERACT_CCMAIN_PARAGRAPHS_H_
 
     
-    #include 'CCommand.h'
-#include 'EngineStrings.h'
-#include '../Command.h'
-#include '../CalculatorVector.h'
-#include '../ExpressionCommand.h'
-#include 'RadixType.h'
-#include 'History.h' // for History Collector
-#include 'CalcInput.h'
-#include 'CalcUtils.h'
-#include 'ICalcDisplay.h'
-#include 'Rational.h'
-#include 'RationalMath.h'
     
-            class CurrencyDataLoader : public UCM::IConverterDataLoader, public UCM::ICurrencyConverterDataLoader
-        {
-        public:
-            CurrencyDataLoader(_In_ std::unique_ptr<CalculatorApp::DataLoaders::ICurrencyHttpClient> client, const wchar_t* overrideLanguage = nullptr);
-            ~CurrencyDataLoader();
-    }
-    
-        // to be changed when pszexp is back
-    tokens->GetString(&generatedExpression);
-    // Prefixing and suffixing the special Unicode markers to ensure that the expression
-    // in the history doesn't get broken for RTL languages
-    spHistoryItem->historyItemVector.expression = L'\u202d' + generatedExpression + L'\u202c';
-    spHistoryItem->historyItemVector.result = wstring(result);
-    addedIndex = AddItem(spHistoryItem);
-    
-    class WriteCallback {
- public:
-  virtual ~WriteCallback() {}
-    }
-    
-    #include <rocksdb/compaction_filter.h>
-#include <rocksdb/db.h>
-#include <rocksdb/merge_operator.h>
-#include <rocksdb/options.h>
-    
-    struct UndumpOptions {
-  // Database that we will load the dumped file into
-  std::string db_path;
-  // File location of the dumped file that will be loaded
-  std::string dump_location;
-  // Compact the db after loading the dumped file
-  bool compact_db = false;
+    { private:
+  double total_weight;         // no of elements or sum of weights.
+  double sigx;                 // sum of x
+  double sigy;                 // sum of y
+  double sigxx;                // sum x squared
+  double sigxy;                // sum of xy
+  double sigyy;                // sum y squared
 };
     
-    // PersistentCache
-//
-// Persistent cache interface for caching IO pages on a persistent medium. The
-// cache interface is specifically designed for persistent read cache.
-class PersistentCache {
+      // ValidFirstLine() and ValidBodyLine() take arguments describing a text line
+  // in a block of text which we are trying to model:
+  //   lmargin, lindent:  these add up to the distance from the leftmost ink
+  //                      in the text line to the surrounding text block's left
+  //                      edge.
+  //   rmargin, rindent:  these add up to the distance from the rightmost ink
+  //                      in the text line to the surrounding text block's right
+  //                      edge.
+  // The caller determines the division between 'margin' and 'indent', which
+  // only actually affect whether we think the line may be centered.
+  //
+  // If the amount of whitespace matches the amount of whitespace expected on
+  // the relevant side of the line (within tolerance_) we say it matches.
+    
+    #define           MAP_ACCEPT '1'
+#define           MAP_REJECT_PERM '0'
+#define           MAP_REJECT_TEMP '2'
+#define           MAP_REJECT_POTENTIAL '3'
+    
+    
+    {}  // namespace tesseract.
+    
+    
+    {  // WARNING! Keep data as the first element! KDPairInc and KDPairDec depend
+  // on the order of these elements so they can downcast pointers appropriately
+  // for use by GenericHeap::Reshuffle.
+  Data data;
+  Key key;
+};
+// Specialization of KDPair to provide operator< for sorting in increasing order
+// and recasting of data pointers for use with DoublePtr.
+template <typename Key, typename Data>
+struct KDPairInc : public KDPair<Key, Data> {
+  KDPairInc() = default;
+  KDPairInc(Key k, Data d) : KDPair<Key, Data>(k, d) {}
+  // Operator< facilitates sorting in increasing order.
+  int operator<(const KDPairInc<Key, Data>& other) const {
+    return this->key < other.key;
+  }
+  // Returns the input Data pointer recast to a KDPairInc pointer.
+  // Just casts a pointer to the first element to a pointer to the whole struct.
+  static KDPairInc* RecastDataPointer(Data* data_ptr) {
+    return reinterpret_cast<KDPairInc*>(data_ptr);
+  }
+};
+// Specialization of KDPair to provide operator< for sorting in decreasing order
+// and recasting of data pointers for use with DoublePtr.
+template <typename Key, typename Data>
+struct KDPairDec : public KDPair<Key, Data> {
+  KDPairDec() = default;
+  KDPairDec(Key k, Data d) : KDPair<Key, Data>(k, d) {}
+  // Operator< facilitates sorting in decreasing order by using operator> on
+  // the key values.
+  int operator<(const KDPairDec<Key, Data>& other) const {
+    return this->key > other.key;
+  }
+  // Returns the input Data pointer recast to a KDPairDec pointer.
+  // Just casts a pointer to the first element to a pointer to the whole struct.
+  static KDPairDec* RecastDataPointer(Data* data_ptr) {
+    return reinterpret_cast<KDPairDec*>(data_ptr);
+  }
+};
+    
+    
+    {  UNICHARMAP_NODE* nodes;
+};
+    
+      // If we manage the given dawg, decrement its count,
+  // and possibly delete it if the count reaches zero.
+  // If dawg is unknown to us, return false.
+  bool FreeDawg(Dawg *dawg) {
+    return dawgs_.Free(dawg);
+  }
+    
+    TEST_F(DistanceApproachIPOPTInterfaceTest, eval_f) {
+  int n = 1274;
+  double obj_value = 0.0;
+  double x[1274];
+  std::fill_n(x, n, 1.2);
+  bool res = ptop_->eval_f(n, x, true, obj_value);
+  EXPECT_DOUBLE_EQ(obj_value, 1443.3600000000008) << 'eval_f: ' << obj_value;
+  EXPECT_TRUE(res);
+}
+    
+      ValetParkingContext* GetContext() {
+    return GetContextAs<ValetParkingContext>();
+  }
+    
+    GemController::~GemController() {}
+    
+    namespace apollo {
+namespace planning {
+namespace scenario {
+namespace valet_parking {
+class StageApproachingParkingSpotTest : public ::testing::Test {
  public:
-  typedef std::vector<std::map<std::string, double>> StatsType;
+  virtual void SetUp() {
+    config_.set_stage_type(
+        ScenarioConfig::VALET_PARKING_APPROACHING_PARKING_SPOT);
+  }
+    }
+    }
+    }
+    }
+    }
+    
+    
+    {    K key = 0;
+    std::atomic<V *> value_ptr = {nullptr};
+    std::atomic<Entry *> next = {nullptr};
+  };
+    
+    bool ChannelManager::IsMessageTypeMatching(const std::string& lhs,
+                                           const std::string& rhs) {
+  if (lhs == rhs) {
+    return true;
+  }
+  if (exempted_msg_types_.count(lhs) > 0) {
+    return true;
+  }
+  if (exempted_msg_types_.count(rhs) > 0) {
+    return true;
+  }
+  return false;
+}
+    
+    
+    {
+    {    for (const auto& pair : data) {
+      auto status = batch.Put(handle_ptr.get(), pair.first, pair.second);
+      if (!status.ok()) {
+        auto batch_write_error = createError(RocksdbError::BatchWriteFail)
+                                 << status.ToString();
+        return createError(DatabaseError::FailToWriteData,
+                           std::move(batch_write_error))
+               << 'Failed to write data';
+      }
+    }
+    auto status = db_->Write(batch_write_options_, &batch);
+    if (!status.ok()) {
+      return createError(DatabaseError::FailToWriteData) << status.ToString();
+    }
+  }
+  return handle.takeError();
+}
+    
+    namespace osquery {
+    }
+    
+    namespace osquery {
     }
