9800f997dcf649ac6711b5b12f54d0a6afecc851
SUNsung <weatherius@gmail.com>
2019-09-02 14:01:59 +0300

    Rewrite [00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py] File in: 2019-09-02 14:01:59

--- pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
+++ pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
@@ -1,164 +1,122 @@
 
         
-        if S < N:
-  # Note that this isn't necessary for this synthetic example, but
-  # it's useful to see how the input factor matrices were initialized
-  # for actual neurophysiology data.
-  datasets = add_alignment_projections(datasets, npcs=FLAGS.npcs)
-    
-        states_t_bxn, outputs_t_bxn = sess.run([states_t, outputs_t],
-                                           feed_dict=feed_dict)
-    states_nxt = np.transpose(np.squeeze(np.asarray(states_t_bxn)))
-    outputs_t_bxn = np.squeeze(np.asarray(outputs_t_bxn))
-    r_sxt = np.dot(P_nxn, states_nxt)
+                discriminator_train_loss = np.mean(np.array(epoch_disc_loss), axis=0)
     
     
-def create_discriminator(hparams,
-                         sequence,
-                         is_training,
-                         reuse=None,
-                         initial_state=None,
-                         inputs=None,
-                         present=None):
-  '''Create the Discriminator model specified by the FLAGS and hparams.
+def test_multithreading_evaluating():
+    arr_data = np.random.randint(0, 256, (50, 2))
+    arr_labels = np.random.randint(0, 2, 50)
     
-      # Cumulative Discounted Returns.  The true value function V*(s).
-  cumulative_rewards = []
-  for t in xrange(FLAGS.sequence_length):
-    cum_value = tf.zeros(shape=[FLAGS.batch_size])
-    for s in xrange(t, FLAGS.sequence_length):
-      cum_value += missing_list[s] * np.power(gamma, (s - t)) * rewards_list[s]
-    cumulative_rewards.append(cum_value)
-  cumulative_rewards = tf.stack(cumulative_rewards, axis=1)
+    from __future__ import absolute_import
+import json
     
-      Returns:
-    variable_mapping:  Dictionary with Key: ckpt_name, Value: model_varself.
-  '''
-  assert (FLAGS.discriminator_model == 'bidirectional_zaremba' or
-          FLAGS.discriminator_model == 'bidirectional_vd')
-  assert hparams.dis_num_layers == 2
-    
-        with tf.variable_scope('rnn') as vs:
-      outputs, _, _ = tf.contrib.rnn.static_bidirectional_rnn(
-          cell_fwd, cell_bwd, rnn_inputs, state_fwd, state_bwd, scope=vs)
-    
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-    
-            if module.params.get('next_marker'):
-            params['Marker'] = module.params.get('next_marker')
+        def _delete_group(self, group_name):
+        '''
+        Delete the provided server group
+        :param group_name: string - the server group to delete
+        :return: none
+        '''
+        response = None
+        group, parent = self.group_dict.get(group_name)
         try:
-            lambda_facts.update(aliases=client.list_aliases(FunctionName=function_name, **params)['Aliases'])
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                lambda_facts.update(aliases=[])
-            else:
-                module.fail_json_aws(e, msg='Trying to get aliases')
-    else:
-        module.fail_json(msg='Parameter function_name required for query=aliases.')
+            response = group.Delete()
+        except CLCException as ex:
+            self.module.fail_json(msg='Failed to delete group :{0}. {1}'.format(
+                group_name, ex.response_text
+            ))
+        return response
     
-        # build list of params
-    params = {}
+    - name: Delete Public IP from Server
+  hosts: localhost
+  gather_facts: False
+  connection: local
+  tasks:
+    - name: Create Public IP For Servers
+      clc_publicip:
+        server_ids:
+          - UC1TEST-SVR01
+          - UC1TEST-SVR02
+        state: absent
+      register: clc
     
-        # Build the deployment object we return
-    deployment = dict(token=token, url=url)
-    deployment.update(body)
-    if 'errorMessage' in deployment:
-        message = deployment.pop('errorMessage')
-        deployment['message'] = message
+        def add(self):
+        ''' If self.dn does not exist, returns a callable that will add it. '''
+        def _add():
+            self.connection.add_s(self.dn, modlist)
     
-    # Copyright 2014 Benjamin Curtis <benjamin.curtis@gmail.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+            # Change the password (or throw an exception)
+        try:
+            self.connection.passwd_s(self.dn, None, self.passwd)
+        except ldap.LDAPError as e:
+            self.fail('Unable to set password', e)
     
-        def manage(self):
-        rc, out = self._exec(['list'])
-        if rc != 0:
-            self.module.fail_json(msg='Unable to list icinga2 features. '
-                                      'Ensure icinga2 is installed and present in binary path.')
+        # Send push notification
+    try:
+        if push_type == 'link':
+            target.push_link(title, url, body)
+        else:
+            target.push_note(title, body)
+        module.exit_json(changed=False, msg='OK')
+    except PushError as e:
+        module.fail_json(msg='An error occurred, Pushbullet's response: %s' % str(e))
     
-        if running and state == 'stopped':
-        if module.check_mode:
-            module.exit_json(changed=True)
-        status = run_command('stop')
-        if status in ['not monitored'] or 'stop pending' in status:
-            module.exit_json(changed=True, name=name, state=state)
-        module.fail_json(msg='%s process not stopped' % name, status=status)
+        return True
     
-        if root.get('total-models') == '0':
-        return None
+    - name: Reconfigure IMC to boot from storage
+  imc_rest:
+    hostname: '{{ imc_host }}'
+    username: '{{ imc_username }}'
+    password: '{{ imc_password }}'
+    validate_certs: no
+    content: |
+      <configConfMo><inConfig>
+        <lsbootStorage dn='sys/rack-unit-1/boot-policy/storage-read-write' access='read-write' order='1' type='storage'/>
+      </inConfig></configConfMo>
+  delegate_to: localhost
     
-        def syntax(self):
-        return '[options] <url>'
+    latest_release:
+    description: Version of the latest release
+    type: str
+    returned: success
+    sample: 1.1.0
+'''
     
-                # trustRoot set to platformTrust() will use the platform's root CAs.
-            #
-            # This means that a website like https://www.cacert.org will be rejected
-            # by default, since CAcert.org CA certificate is seldom shipped.
-            return optionsForClientTLS(hostname.decode('ascii'),
-                                       trustRoot=platformTrust(),
-                                       extraCertificateOptions={
-                                            'method': self._ssl_method,
-                                       })
+        # ensure folder exists
+    if not os.path.exists(args.codedir):
+        os.makedirs(args.codedir)
     
-            # If no credentials could be found anywhere,
-        # consider this an anonymous connection request by default;
-        # unless 'anon' was set explicitly (True/False).
-        anon = kw.get('anon')
-        if anon is None and not aws_access_key_id and not aws_secret_access_key:
-            kw['anon'] = True
-        self.anon = kw.get('anon')
+        for i in range(len(s)):
+        result = s.iloc[i]
+        exp = s[s.index[i]]
+        assert_almost_equal(result, exp)
     
-        def _debug_cookie(self, request, spider):
-        if self.debug:
-            cl = [to_native_str(c, errors='replace')
-                  for c in request.headers.getlist('Cookie')]
-            if cl:
-                cookies = '\n'.join('Cookie: {}\n'.format(c) for c in cl)
-                msg = 'Sending cookies to: {}\n{}'.format(request, cookies)
-                logger.debug(msg, extra={'spider': spider})
+        @Substitution(name='ewm')
+    @Appender(_doc_template)
+    @Appender(_pairwise_template)
+    def cov(self, other=None, pairwise=None, bias=False, **kwargs):
+        '''
+        Exponential weighted sample covariance.
+        '''
+        if other is None:
+            other = self._selected_obj
+            # only default unset
+            pairwise = True if pairwise is None else pairwise
+        other = self._shallow_copy(other)
     
+        >>> @deprecate_kwarg(old_arg_name='cols', new_arg_name=None)
+    ... def f(cols='', another_param=''):
+    ...     print(cols)
+    ...
+    >>> f(cols='should raise warning')
+    FutureWarning: the 'cols' keyword is deprecated and will be removed in a
+    future version please takes steps to stop use of 'cols'
+    should raise warning
+    >>> f(another_param='should not raise warning')
+    should not raise warning
     
-if __name__ == '__main__':
-    unittest.main()  # pragma: no cover
-
+            print('None input')
+        assert_raises(TypeError, merge_sort.sort, None)
     
-    
-KEY = jose.JWKRSA.load(test_util.load_vector('rsa512_key.pem'))
-    
-            self.vhosts.append(
-            obj.VirtualHost(
-                'path', 'aug_path', set([obj.Addr.fromstring('*:80')]),
-                False, False,
-                'wildcard.com', set(['*.wildcard.com'])))
-    
-       # Cloudflare API credentials used by Certbot
-   dns_cloudflare_email = cloudflare@example.com
-   dns_cloudflare_api_key = 0123456789abcdef0123456789abcdef01234567
-    
-        def reset(self):
-        return 0
-
-    
-        Args:
-        a (Optional[int, str]): None seeds from an operating system specific randomness source.
-        max_bytes: Maximum number of bytes to use in the seed.
-    '''
-    # Adapted from https://svn.python.org/projects/python/tags/r32/Lib/random.py
-    if a is None:
-        a = _bigint_from_bytes(os.urandom(max_bytes))
-    elif isinstance(a, str):
-        a = a.encode('utf8')
-        a += hashlib.sha512(a).digest()
-        a = _bigint_from_bytes(a[:max_bytes])
-    elif isinstance(a, integer_types):
-        a = a % 2**(8 * max_bytes)
-    else:
-        raise error.Error('Invalid type for seed: {} ({})'.format(type(a), a))
-    
-    class StatsRecorder(object):
-    def __init__(self, directory, file_prefix, autoreset=False, env_id=None):
-        self.autoreset = autoreset
-        self.env_id = env_id
-    
-        def render(self, mode=None):
-        pass
+            print('Test: Enqueue after a dequeue')
+        queue.enqueue(5)
+        assert_equal(queue.dequeue(), 5)
