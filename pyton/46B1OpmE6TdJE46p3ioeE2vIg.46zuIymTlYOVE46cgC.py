0a523e3a33f9135341cb6d590c19e4acd2b531a4
SUNsung <weatherius@gmail.com>
2019-09-02 15:30:53 +0300

    Rewrite [46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py] File in: 2019-09-02 15:30:53

--- pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
+++ pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
@@ -1,158 +1,128 @@
 
         
-        from django.core.exceptions import ImproperlyConfigured
-from django.http import HttpResponse
-from django.test import RequestFactory, SimpleTestCase, override_settings
-from django.test.utils import require_jinja2
-from django.urls import resolve
-from django.views.generic import RedirectView, TemplateView, View
+            def test_encode_override(self):
+        self.assertEqual('y', self.field.encode('y'))
+    
+        @certbot_util.patch_get_utility()
+    def test_select_correct(self, mock_util):
+        mock_util().checklist.return_value = (
+            display_util.OK, [self.vhosts[3].display_repr(),
+                              self.vhosts[2].display_repr()])
+        vhs = select_vhost_multiple([self.vhosts[3],
+                                     self.vhosts[2],
+                                     self.vhosts[1]])
+        self.assertTrue(self.vhosts[2] in vhs)
+        self.assertTrue(self.vhosts[3] in vhs)
+        self.assertFalse(self.vhosts[1] in vhs)
+    
+        @mock.patch('dns.query.tcp')
+    def test_add_txt_record(self, query_mock):
+        query_mock.return_value.rcode.return_value = dns.rcode.NOERROR
+        # _find_domain | pylint: disable=protected-access
+        self.rfc2136_client._find_domain = mock.MagicMock(return_value='example.com')
+    
+        def certbot_test_nginx(self, args):
+        '''
+        Main command to execute certbot using the nginx plugin.
+        :param list args: list of arguments to pass to nginx
+        :param bool force_renew: set to False to not renew by default
+        '''
+        command = ['--authenticator', 'nginx', '--installer', 'nginx',
+                   '--nginx-server-root', self.nginx_root]
+        command.extend(args)
+        return certbot_call.certbot_test(
+            command, self.directory_url, self.http_01_port, self.tls_alpn_01_port,
+            self.config_dir, self.workspace, force_renew=True)
     
     
-@unittest.skipUnless(HAS_POSTGRES, 'The psycopg2 driver is needed for these tests')
-class TestPostGISVersionCheck(unittest.TestCase):
+@pytest.mark.parametrize('certname_pattern, params, context', [
+    ('nginx.{0}.wtf', ['run'], {'default_server': True}),
+    ('nginx2.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': True}),
+    # Overlapping location block and server-block-level return 301
+    ('nginx3.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': True}),
+    # No matching server block; default_server exists
+    ('nginx4.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': True}),
+    # No matching server block; default_server does not exist
+    ('nginx5.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': False}),
+    # Multiple domains, mix of matching and not
+    ('nginx6.{0}.wtf,nginx7.{0}.wtf', ['--preferred-challenges', 'http'], {'default_server': False}),
+], indirect=['context'])
+def test_certificate_deployment(certname_pattern, params, context):
+    # type: (str, list, nginx_context.IntegrationTestsContext) -> None
     '''
-    The PostGIS version check parses correctly the version numbers
+    Test various scenarios to deploy a certificate to nginx using certbot.
     '''
+    domains = certname_pattern.format(context.worker_id)
+    command = ['--domains', domains]
+    command.extend(params)
+    context.certbot_test_nginx(command)
     
-        def test_gist_index(self):
-        # Ensure the table is there and doesn't have an index.
-        self.assertNotIn('field', self.get_constraints(CharFieldModel._meta.db_table))
-        # Add the index.
-        index_name = 'char_field_model_field_gist'
-        index = GistIndex(fields=['field'], name=index_name)
-        with connection.schema_editor() as editor:
-            editor.add_index(CharFieldModel, index)
-        constraints = self.get_constraints(CharFieldModel._meta.db_table)
-        # The index was added.
-        self.assertEqual(constraints[index_name]['type'], GistIndex.suffix)
-        # Drop the index.
-        with connection.schema_editor() as editor:
-            editor.remove_index(CharFieldModel, index)
-        self.assertNotIn(index_name, self.get_constraints(CharFieldModel._meta.db_table))
+            # Pylint checks for super init, but also claims the super
+        # has no __init__member
+        # pylint: disable=super-init-not-called
+        self._plugins = collections.OrderedDict(sorted(six.iteritems(plugins)))
     
-    
-def decorator_from_middleware(middleware_class):
-    '''
-    Given a middleware class (not an instance), return a view decorator. This
-    lets you use middleware functionality on a per-view basis. The middleware
-    is created with no params passed.
-    '''
-    return make_middleware_decorator(middleware_class)()
-    
-        @property
-    def name(self):
-        'Return the name of this layer in the Data Source.'
-        name = capi.get_fd_name(self._ldefn)
-        return force_str(name, self._ds.encoding, strings_only=True)
-    
-    '''
-=========================================================
-Logistic Regression 3-class Classifier
-=========================================================
-    
-    
-def fixed_batch_size_comparison(data):
-    all_features = [i.astype(int) for i in np.linspace(data.shape[1] // 10,
-                                                       data.shape[1], num=5)]
-    batch_size = 1000
-    # Compare runtimes and error for fixed batch size
-    all_times = defaultdict(list)
-    all_errors = defaultdict(list)
-    for n_components in all_features:
-        pca = PCA(n_components=n_components)
-        ipca = IncrementalPCA(n_components=n_components, batch_size=batch_size)
-        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
-                                                               ('ipca', ipca)]}
-    
-        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
-    plt.plot(sample_sizes, one_core, label='one core')
-    plt.plot(sample_sizes, multi_core, label='multi core')
-    plt.xlabel('n_samples')
-    plt.ylabel('Time (s)')
-    plt.title('Parallel %s' % func.__name__)
-    plt.legend()
-    
-    import gc
-    
-        xx = np.arange(start_dim, start_dim + n * step, step)
-    plt.subplot(212)
-    plt.title('Learning in high dimensional spaces')
-    plt.plot(xx, scikit_classifier_results, 'g-', label='classification')
-    plt.plot(xx, scikit_regressor_results, 'r-', label='regression')
-    plt.legend(loc='upper left')
-    plt.xlabel('number of dimensions')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    plt.show()
+            gold_tuples (iterable): Gold-standard training data.
+        pipeline (list): The pipeline the model is part of.
+        '''
+        if self.model is True:
+            self.model = self.Model(pipeline[0].model.nO)
+            link_vectors_to_models(self.vocab)
+        if sgd is None:
+            sgd = self.create_optimizer()
+        return sgd
 
     
-    import sys
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import load_files
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
     
-    We selected two sets of two variables from the Boston housing data set
-as an illustration of what kind of analysis can be done with several
-outlier detection tools. For the purpose of visualization, we are working
-with two-dimensional examples, but one should be aware that things are
-not so trivial in high-dimension, as it will be pointed out.
+@pytest.mark.parametrize('package', ['thinc'])
+def test_util_get_package_path(package):
+    '''Test that a Path object is returned for a package name.'''
+    path = util.get_package_path(package)
+    assert isinstance(path, Path)
     
-    The dataset is generated using the ``make_biclusters`` function, which
-creates a matrix of small values and implants bicluster with large
-values. The rows and columns are then shuffled and passed to the
-Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
-make biclusters contiguous shows how accurately the algorithm found
-the biclusters.
+        for base, _, files in os.walk(root):
+        for filename in files:
+            if filename.endswith('.pxd'):
+                hash_add(base, filename, new_db)
+                if hash_changed(base, filename, db):
+                    res = True
     
-        parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
-    parser.add_argument('-d', action='store', dest='directory')
-    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
-    parser.add_argument('--overwrite', action='store_true', default = False)    
-    results = parser.parse_args()
     
-        for url in urls:
-        if not url.startswith('http'):
-            print('markdown file name: ' + url)
-            continue
-        if check_live_url(url):
-            print(url)
-        else:
-            print(url, file=sys.stderr)
+def get_labelled_sentences(docs, doc_labels):
+    labels = []
+    sentences = []
+    for doc, y in zip(docs, doc_labels):
+        for sent in doc.sents:
+            sentences.append(sent)
+            labels.append(y)
+    return sentences, numpy.asarray(labels, dtype='int32')
     
-    def upload_file(self, fileDir, isPicture=False, isVideo=False,
-        toUserName='filehelper', file_=None, preparedFile=None):
-    logger.debug('Request to upload a %s: %s' % (
-        'picture' if isPicture else 'video' if isVideo else 'file', fileDir))
-    if not preparedFile:
-        preparedFile = _prepare_file(fileDir, file_)
-        if not preparedFile:
-            return preparedFile
-    fileSize, fileMd5, file_ = \
-        preparedFile['fileSize'], preparedFile['fileMd5'], preparedFile['file_']
-    fileSymbol = 'pic' if isPicture else 'video' if isVideo else'doc'
-    chunks = int((fileSize - 1) / 524288) + 1
-    clientMediaId = int(time.time() * 1e4)
-    uploadMediaRequest = json.dumps(OrderedDict([
-        ('UploadType', 2),
-        ('BaseRequest', self.loginInfo['BaseRequest']),
-        ('ClientMediaId', clientMediaId),
-        ('TotalLen', fileSize),
-        ('StartPos', 0),
-        ('DataLen', fileSize),
-        ('MediaType', 4),
-        ('FromUserName', self.storageClass.userName),
-        ('ToUserName', toUserName),
-        ('FileMd5', fileMd5)]
-        ), separators = (',', ':'))
-    r = {'BaseResponse': {'Ret': -1005, 'ErrMsg': 'Empty file detected'}}
-    for chunk in range(chunks):
-        r = upload_chunk_file(self, fileDir, fileSymbol, fileSize,
-            file_, chunk, chunks, uploadMediaRequest)
-    file_.close()
-    if isinstance(r, dict):
-        return ReturnValue(r)
-    return ReturnValue(rawResponse=r)
+    
+def train(train_loc, dev_loc, shape, settings):
+    train_texts1, train_texts2, train_labels = read_snli(train_loc)
+    dev_texts1, dev_texts2, dev_labels = read_snli(dev_loc)
+    
+        vectors = np.ndarray((100, 8), dtype='float32')
+    shape = (10, 16, 3)
+    settings = {'lr': 0.001, 'dropout': 0.2, 'gru_encode': True, 'entail_dir': 'both'}
+    model = build_model(vectors, shape, settings)
+    
+            # Masks
+        if masks is not None:
+            mask = masks[:, :, i]
+            masked_image = apply_mask(masked_image, mask, color)
+            # Mask Polygon
+            # Pad to ensure proper polygons for masks that touch image edges.
+            padded_mask = np.zeros(
+                (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+            padded_mask[1:-1, 1:-1] = mask
+            contours = find_contours(padded_mask, 0.5)
+            for verts in contours:
+                # Subtract the padding and flip (y, x) to (x, y)
+                verts = np.fliplr(verts) - 1
+                p = Polygon(verts, facecolor='none', edgecolor=color)
+                ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
+    
+        # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + nucleus
