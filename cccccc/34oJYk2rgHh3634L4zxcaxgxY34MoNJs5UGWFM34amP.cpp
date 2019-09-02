2e4e8d25933e89b2afdb61464a43224e9ffffc45
SUNsung <weatherius@gmail.com>
2019-09-02 17:45:26 +0300

    Rewrite [34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp] File in: 2019-09-02 17:45:26

--- cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
+++ cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
@@ -1,121 +1,57 @@
 
         
-          void visitInjectEnumAddrInst(InjectEnumAddrInst *IEAI) {
-    SGM.useConformancesFromType(IEAI->getOperand()->getType().getASTType());
+          // 4. If the end of current passage region not appeared, no rerouting
+  const auto& route_end_waypoint = segments.RouteEndWaypoint();
+  if (!route_end_waypoint.lane) {
+    return true;
+  }
+  auto point = route_end_waypoint.lane->GetSmoothPoint(route_end_waypoint.s);
+  const auto& reference_line = reference_line_info_->reference_line();
+  common::SLPoint sl_point;
+  if (!reference_line.XYToSL({point.x(), point.y()}, &sl_point)) {
+    AERROR << 'Failed to project point: ' << point.ShortDebugString();
+    return false;
+  }
+  if (!reference_line.IsOnLane(sl_point)) {
+    return true;
+  }
+  // 5. If the end of current passage region is further than kPrepareRoutingTime
+  // * speed, no rerouting
+  double adc_s = reference_line_info_->AdcSlBoundary().end_s();
+  const auto vehicle_state = common::VehicleStateProvider::Instance();
+  double speed = vehicle_state->linear_velocity();
+  const double prepare_rerouting_time =
+      config_.rerouting().prepare_rerouting_time();
+  const double prepare_distance = speed * prepare_rerouting_time;
+  if (sl_point.s() > adc_s + prepare_distance) {
+    ADEBUG << 'No need rerouting now because still can drive for time: '
+           << prepare_rerouting_time << ' seconds';
+    return true;
+  }
+  // 6. Check if we have done rerouting before
+  auto* rerouting = PlanningContext::Instance()
+                        ->mutable_planning_status()
+                        ->mutable_rerouting();
+  if (rerouting == nullptr) {
+    AERROR << 'rerouting is nullptr.';
+    return false;
+  }
+  double current_time = Clock::NowInSeconds();
+  if (rerouting->has_last_rerouting_time() &&
+      (current_time - rerouting->last_rerouting_time() <
+       config_.rerouting().cooldown_time())) {
+    ADEBUG << 'Skip rerouting and wait for previous rerouting result';
+    return true;
+  }
+  if (!frame_->Rerouting()) {
+    AERROR << 'Failed to send rerouting request';
+    return false;
   }
     
-    KNOWN_STDLIB_TYPE_DECL(AnyKeyPath, NominalTypeDecl, 0)
-KNOWN_STDLIB_TYPE_DECL(PartialKeyPath, NominalTypeDecl, 1)
-KNOWN_STDLIB_TYPE_DECL(KeyPath, NominalTypeDecl, 2)
-KNOWN_STDLIB_TYPE_DECL(WritableKeyPath, NominalTypeDecl, 2)
-KNOWN_STDLIB_TYPE_DECL(ReferenceWritableKeyPath, NominalTypeDecl, 2)
-    
-    #endif
-
-    
-      bool operator==(FieldDescriptorIterator const &other) const {
-    return Cur == other.Cur && End == other.End;
+    #define RETURN_VAL_IF_NULL(ptr, val) \
+  if (ptr == nullptr) {              \
+    AWARN << #ptr << ' is nullptr.'; \
+    return val;                      \
   }
     
-    using ReadBytesResult = swift::remote::MemoryReader::ReadBytesResult;
-    
-    #endif
-#endif
-
-    
-    
-    {  void moduleLoad(const IniSetting::Map& ini, Hdf globalConfig) override {
-    Config::Bind(Enabled, ini, globalConfig,
-                 'HealthMonitor.EnableHealthMonitor', true);
-    Config::Bind(UpdateFreq, ini, globalConfig,
-                 'HealthMonitor.UpdateFreq', 1000 /* miliseconds */);
-  }
-} s_host_health_monitor_extension;
-    
-    void ProxygenTransport::onBody(std::unique_ptr<folly::IOBuf> chain) noexcept {
-  // If we've already sent a response, allow up to 128k of data to be received
-  // before just sending an abort. This gives the client an opportunity to
-  // process the response before receiving some form of a reset caused by the
-  // abort.
-  if (m_sendEnded && m_clientTxn->isEgressComplete()) {
-    m_bodyLengthPastLimit -= chain->computeChainDataLength();
-    if (m_bodyLengthPastLimit <= 0) {
-      Logger::Warning('Received body after a response has completed, aborting');
-      abort();
-    }
-    return;
-  }
-    }
-    
-    #include 'hphp/runtime/base/array-data.h'
-#include 'hphp/runtime/base/array-data-defs.h'
-#include 'hphp/runtime/base/array-init.h'
-#include 'hphp/runtime/base/array-iterator.h'
-#include 'hphp/runtime/base/mixed-array-defs.h'
-#include 'hphp/runtime/base/object-data.h'
-#include 'hphp/runtime/base/set-array.h'
-#include 'hphp/runtime/base/packed-array.h'
-#include 'hphp/runtime/base/type-variant.h'
-    
-    
-    {  enum class RefCheckResult {
-    Pass, // No refs
-    Fail, // Referenced ref
-    Collapse // Non-referenced ref
-  };
-  static RefCheckResult CheckForRefs(const ArrayData*);
-};
-    
-        inline void consumer_remove(Coroutine *co)
-    {
-        consumer_queue.remove(co);
-    }
-    
-        DataBuffer(const char *str, size_t length)
-    {
-        copy((void *) str, length);
-    }
-    
-    
-    {    for (i = 0; i < 10; i++)
-    {
-        free(lists[i]);
-    }
-}
-
-    
-    
-    {    ASSERT_GT(cid, 0);
-    Coroutine::get_by_cid(cid)->resume();
-    ASSERT_EQ(cid, _cid);
-}
-
-    
-    TEST(os_wait, wait_before_child_exit)
-{
-    coro_test([](void *arg)
-    {
-        swoole_coroutine_signal_init();
-    }
-    }
-    
-        private:
-        void addRead() {
-            if (m_read) return;
-            m_read = new QSocketNotifier(m_ctx->c.fd, QSocketNotifier::Read, 0);
-            connect(m_read, SIGNAL(activated(int)), this, SLOT(read()));
-        }
-    
-            /**
-         * It returns a string with the whole Rectangle<T> data. Useful for debugging.
-         * The format is: `[x, y, width, height]`
-         * @return A string with the Rectangle<T> values in the above format.
-         */
-        std::string toString() const;
-    
-            virtual void initializationOnThread();
-    
-    #endif // OPENPOSE_HAND_HAND_PARAMETERS_HPP
-
-    
-    #endif // OPENPOSE_TRACKING_PERSON_ID_EXTRACTOR_HPP
+      bool CheckADCStop(const Frame& frame);
