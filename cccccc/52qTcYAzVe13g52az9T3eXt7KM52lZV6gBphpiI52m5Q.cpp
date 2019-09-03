8ef059baa4f6a768a1d0b779ea93585661be0edc
SUNsung <weatherius@gmail.com>
2019-09-03 13:53:15 +0300

    Rewrite [52qTcYAzVe13g52az9T3eXt7KM52lZV6gBphpiI52m5Q.cpp] File in: 2019-09-03 13:53:14

--- cccccc/52qTcYAzVe13g52az9T3eXt7KM52lZV6gBphpiI52m5Q.cpp
+++ cccccc/52qTcYAzVe13g52az9T3eXt7KM52lZV6gBphpiI52m5Q.cpp
@@ -1,357 +1,188 @@
 
         
-                    // Initialize the necessary startup info struct        
-            STARTUPINFOEX startupInfo{};
-            if (S_OK == InitializeStartupInfoAttachedToPseudoConsole(&startupInfo, hPC))
-            {
-                // Launch ping to emit some text back via the pipe
-                PROCESS_INFORMATION piClient{};
-                hr = CreateProcess(
-                    NULL,                           // No module name - use Command Line
-                    szCommand,                      // Command Line
-                    NULL,                           // Process handle not inheritable
-                    NULL,                           // Thread handle not inheritable
-                    FALSE,                          // Inherit handles
-                    EXTENDED_STARTUPINFO_PRESENT,   // Creation flags
-                    NULL,                           // Use parent's environment block
-                    NULL,                           // Use parent's starting directory 
-                    &startupInfo.StartupInfo,       // Pointer to STARTUPINFO
-                    &piClient)                      // Pointer to PROCESS_INFORMATION
-                    ? S_OK
-                    : GetLastError();
-    }
-    
-    class GetMergeSingleMapFeatureTensorsGradient : public GradientMakerBase {
-  using GradientMakerBase::GradientMakerBase;
-  vector<OperatorDef> GetGradientDefs() override {
-    vector<string> input_blob_names{};
-    vector<string> output_blob_names{};
-    }
-    }
-    
-    Github Link:
-- https://github.com/pytorch/pytorch/blob/master/caffe2/operators/floor_op.cc
-    
-              vector<TensorShape> out(1);
-          switch (order) {
-            case StorageOrder::NCHW:
-              out[0] = CreateTensorShape(
-                  vector<int>{N, C * kernel_h * kernel_w, out_h, out_w},
-                  TensorProto::FLOAT);
-              break;
-            case StorageOrder::NHWC:
-              out[0] = CreateTensorShape(
-                  vector<int>{N, out_h, out_w, kernel_h * kernel_w * C},
-                  TensorProto::FLOAT);
-              break;
-            default:
-              CAFFE_THROW('Unknown storage order: ', order);
-          }
-    
-    
-    {} // namespace caffe2
-    
-    #include 'hdf5.h'
-#include 'hdf5_hl.h'
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs @f$
-   *        y = \max(min, \min(max, x))
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    
-    {}  // namespace caffe
-
-    
-    TYPED_TEST(NeuronLayerTest, TestDropoutGradientTest) {
-  typedef typename TypeParam::Dtype Dtype;
-  LayerParameter layer_param;
-  layer_param.set_phase(TEST);
-  DropoutLayer<Dtype> layer(layer_param);
-  GradientChecker<Dtype> checker(1e-2, 1e-3);
-  checker.CheckGradientEltwise(&layer, this->blob_bottom_vec_,
-      this->blob_top_vec_);
-}
-    
-    template <typename Dtype>
-void InnerProductLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-    const vector<Blob<Dtype>*>& top) {
-  const Dtype* bottom_data = bottom[0]->cpu_data();
-  Dtype* top_data = top[0]->mutable_cpu_data();
-  const Dtype* weight = this->blobs_[0]->cpu_data();
-  caffe_cpu_gemm<Dtype>(CblasNoTrans, transpose_ ? CblasNoTrans : CblasTrans,
-      M_, N_, K_, (Dtype)1.,
-      bottom_data, weight, (Dtype)0., top_data);
-  if (bias_term_) {
-    caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, N_, 1, (Dtype)1.,
-        bias_multiplier_.cpu_data(),
-        this->blobs_[1]->cpu_data(), (Dtype)1., top_data);
-  }
-}
-    
-    /**
-  * @brief Enumeration of actions that a client of the Solver may request by
-  * implementing the Solver's action request function, which a
-  * client may optionally provide in order to request early termination
-  * or saving a snapshot without exiting. In the executable caffe, this
-  * mechanism is used to allow the snapshot to be saved when stopping
-  * execution with a SIGINT (Ctrl-C).
-  */
-  namespace SolverAction {
-    enum Enum {
-      NONE = 0,  // Take no special action.
-      STOP = 1,  // Stop training. snapshot_after_train controls whether a
-                 // snapshot is created.
-      SNAPSHOT = 2  // Take a snapshot, and keep training.
-    };
-  }
-    
-      bp::class_<Net<Dtype>, shared_ptr<Net<Dtype> >, boost::noncopyable >('Net',
-    bp::no_init)
-    // Constructor
-    .def('__init__', bp::make_constructor(&Net_Init,
-          bp::default_call_policies(), (bp::arg('network_file'), 'phase',
-            bp::arg('level')=0, bp::arg('stages')=bp::object(),
-            bp::arg('weights')=bp::object())))
-    // Legacy constructor
-    .def('__init__', bp::make_constructor(&Net_Init_Load))
-    .def('_forward', &Net<Dtype>::ForwardFromTo)
-    .def('_backward', &Net<Dtype>::BackwardFromTo)
-    .def('reshape', &Net<Dtype>::Reshape)
-    .def('clear_param_diffs', &Net<Dtype>::ClearParamDiffs)
-    // The cast is to select a particular overload.
-    .def('copy_from', static_cast<void (Net<Dtype>::*)(const string&)>(
-        &Net<Dtype>::CopyTrainedLayersFrom))
-    .def('share_with', &Net<Dtype>::ShareTrainedLayersWith)
-    .add_property('_blob_loss_weights', bp::make_function(
-        &Net<Dtype>::blob_loss_weights, bp::return_internal_reference<>()))
-    .def('_bottom_ids', bp::make_function(&Net<Dtype>::bottom_ids,
-        bp::return_value_policy<bp::copy_const_reference>()))
-    .def('_top_ids', bp::make_function(&Net<Dtype>::top_ids,
-        bp::return_value_policy<bp::copy_const_reference>()))
-    .add_property('_blobs', bp::make_function(&Net<Dtype>::blobs,
-        bp::return_internal_reference<>()))
-    .add_property('layers', bp::make_function(&Net<Dtype>::layers,
-        bp::return_internal_reference<>()))
-    .add_property('_blob_names', bp::make_function(&Net<Dtype>::blob_names,
-        bp::return_value_policy<bp::copy_const_reference>()))
-    .add_property('_layer_names', bp::make_function(&Net<Dtype>::layer_names,
-        bp::return_value_policy<bp::copy_const_reference>()))
-    .add_property('_inputs', bp::make_function(&Net<Dtype>::input_blob_indices,
-        bp::return_value_policy<bp::copy_const_reference>()))
-    .add_property('_outputs',
-        bp::make_function(&Net<Dtype>::output_blob_indices,
-        bp::return_value_policy<bp::copy_const_reference>()))
-    .def('_set_input_arrays', &Net_SetInputArrays,
-        bp::with_custodian_and_ward<1, 2, bp::with_custodian_and_ward<1, 3> >())
-    .def('save', &Net_Save)
-    .def('save_hdf5', &Net_SaveHDF5)
-    .def('load_hdf5', &Net_LoadHDF5)
-    .def('before_forward', &Net_before_forward)
-    .def('after_forward', &Net_after_forward)
-    .def('before_backward', &Net_before_backward)
-    .def('after_backward', &Net_after_backward)
-    .def('after_backward', &Net_add_nccl);
-  BP_REGISTER_SHARED_PTR_TO_PYTHON(Net<Dtype>);
-    
-    using std::min;
-using std::max;
-    
-    void convert_dataset(const string& input_folder, const string& output_folder,
-    const string& db_type) {
-  scoped_ptr<db::DB> train_db(db::GetDB(db_type));
-  train_db->Open(output_folder + '/cifar10_train_' + db_type, db::NEW);
-  scoped_ptr<db::Transaction> txn(train_db->NewTransaction());
-  // Data buffer
-  int label;
-  char str_buffer[kCIFARImageNBytes];
-  Datum datum;
-  datum.set_channels(3);
-  datum.set_height(kCIFARSize);
-  datum.set_width(kCIFARSize);
-    }
-    
-      string file = argv[5];
-    
-    #include 'caffe/proto/caffe.pb.h'
-#include 'caffe/util/format.hpp'
-#include 'caffe/util/math_functions.hpp'
-    
-      cli_sock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0,
-                       grpc_get_default_wsa_socket_flags());
-  GPR_ASSERT(cli_sock != INVALID_SOCKET);
-    
-    static void iomgr_platform_flush(void) { grpc_iocp_flush(); }
-    
-    #ifndef GRPCPP_CREATE_CHANNEL_H
-#define GRPCPP_CREATE_CHANNEL_H
-    
-    #ifndef GRPCPP_SUPPORT_CHANNEL_ARGUMENTS_H
-#define GRPCPP_SUPPORT_CHANNEL_ARGUMENTS_H
-    
-    using grpc::testing::EchoRequest;
-using grpc::testing::EchoResponse;
-using std::chrono::system_clock;
-    
-      void TearDown() override {
-    if (server_) {
-      server_->Shutdown();
-    }
-    void* ignored_tag;
-    bool ignored_ok;
-    for (auto it = cqs_.begin(); it != cqs_.end(); ++it) {
-      (*it)->Shutdown();
-      while ((*it)->Next(&ignored_tag, &ignored_ok))
-        ;
-    }
-  }
-    
-      void SetUp() override {
-    port_ = grpc_pick_unused_port_or_die();
-    ref_desc_pool_ = protobuf::DescriptorPool::generated_pool();
-    }
-    
-      send_request_.set_message('hello bidi streaming');
-  std::unique_ptr<ClientAsyncReaderWriter<EchoRequest, EchoResponse>>
-      cli_stream(stub_->AsyncBidiStream(&cli_ctx_, cq_.get(), tag(1)));
-    
-    `btree/secondary_operations.*` and `btree/reql_specific.*` are the only files in the
-`btree/` directory that know about ReQL-specific concepts such as metainfo and sindexes.
-They should probably be moved out of the `btree/` directory. */
-    
-    void store_t::write(
-        DEBUG_ONLY(const metainfo_checker_t& metainfo_checker, )
-        const region_map_t<binary_blob_t>& new_metainfo,
-        const write_t &_write,
-        write_response_t *response,
-        const write_durability_t durability,
-        state_timestamp_t timestamp,
-        UNUSED order_token_t order_token,  // TODO
-        write_token_t *token,
-        signal_t *interruptor)
-        THROWS_ONLY(interrupted_exc_t) {
-    assert_thread();
-    }
-    
-        std::map<sindex_name_t, secondary_index_t> get_sindexes() const;
-    
-                for (;;) {
-                if (it == sindexes.end()) {
-                    ASSERT_TRUE(jt == mirror.end());
-                    break;
-                }
-                ASSERT_TRUE(jt != mirror.end());
-    }
-    
-    template <GTEST_6_TYPENAMES_(T)>
-struct tuple_size<GTEST_6_TUPLE_(T) > {
-  static const int value = 6;
-};
-    
-      static void setInitialized(bool f) { data_.initialized = f; }
-    
-    void DHTReplaceNodeTask::onReceived(const DHTPingReplyMessage* message)
+        class FastHufDecoder
 {
-  A2_LOG_INFO(fmt('ReplaceNode: Ping reply received from %s.',
-                  message->getRemoteNode()->toString().c_str()));
-  setFinished(true);
-}
+  public:
+    }
     
-    void DHTTaskExecutor::update()
+    
+OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_EXIT
+    
+    typedef std::vector<float>
+    FloatVector;
+    
+    
+FrameBuffer::ConstIterator
+FrameBuffer::find (const char name[]) const
 {
-  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
-                                  std::mem_fn(&DHTTask::finished)),
-                   execTasks_.end());
-  int r;
-  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
-    r = numConcurrent_ - execTasks_.size();
-  }
-  else {
-    r = 0;
-  }
-  while (r && !queue_.empty()) {
-    std::shared_ptr<DHTTask> task = queue_.front();
-    queue_.pop_front();
-    task->startup();
-    if (!task->finished()) {
-      execTasks_.push_back(task);
-      --r;
-    }
-  }
-  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
-                   static_cast<unsigned int>(getExecutingTaskSize()),
-                   static_cast<unsigned int>(getQueueSize())));
+    return _map.find (name);
 }
     
-    #include <memory>
+    OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_ENTER
     
-    void DHTTaskQueueImpl::executeTask()
+    
+class GenericOutputFile
 {
-  A2_LOG_DEBUG('Updating periodicTaskQueue1');
-  periodicTaskQueue1_.update();
-  A2_LOG_DEBUG('Updating periodicTaskQueue2');
-  periodicTaskQueue2_.update();
-  A2_LOG_DEBUG('Updating immediateTaskQueue');
-  immediateTaskQueue_.update();
+    public:
+        IMF_EXPORT
+        virtual ~GenericOutputFile() {}
+    }
+    
+    
+//---------------------
+// Template definitions
+//---------------------
+    
+    
+//
+// DECODING TABLE BUILDING
+//
+    
+    TEST_F(JsonObjectWriterTest, ObjectInList) {
+  ow_ = new JsonObjectWriter('', out_stream_);
+  ow_->StartList('')
+      ->StartObject('')
+      ->RenderString('field', 'value')
+      ->EndObject()
+      ->EndList();
+  EXPECT_EQ('[{\'field\':\'value\'}]', CloseStreamAndGetString());
 }
     
-    class DHTTaskQueueImpl : public DHTTaskQueue {
-private:
-  DHTTaskExecutor periodicTaskQueue1_;
+    TEST(StatusOr, TestPointerAssignmentStatusOk) {
+  const int kI = 0;
+  StatusOr<const int*> source(&kI);
+  StatusOr<const int*> target;
+  target = source;
+  EXPECT_EQ(source.status(), target.status());
+  EXPECT_EQ(source.ValueOrDie(), target.ValueOrDie());
+}
+    
+    namespace GOOGLE_NAMESPACE = google::protobuf::internal;
+    
+        if (dataset.message_name() == 'benchmarks.proto3.GoogleMessage1') {
+      message = new benchmarks::proto3::GoogleMessage1;
+    } else if (dataset.message_name() == 'benchmarks.proto2.GoogleMessage1') {
+      message = new benchmarks::proto2::GoogleMessage1;
+    } else if (dataset.message_name() == 'benchmarks.proto2.GoogleMessage2') {
+      message = new benchmarks::proto2::GoogleMessage2;
+    } else if (dataset.message_name() ==
+        'benchmarks.google_message3.GoogleMessage3') {
+      message = new benchmarks::google_message3::GoogleMessage3;
+    } else if (dataset.message_name() ==
+        'benchmarks.google_message4.GoogleMessage4') {
+      message = new benchmarks::google_message4::GoogleMessage4;
+    } else {
+      std::cerr << 'Unknown message type: ' << dataset.message_name();
+      exit(1);
     }
     
-    class DHTTokenTracker {
-private:
-  static const size_t SECRET_SIZE = 4;
+        for (int i = 0; i < dataset.payload_size(); i++) {
+      message->ParseFromString(dataset.payload(i));
+      Proto3DataStripper stripper;
+      stripper.StripMessage(message);
+      dataset.set_payload(i, message->SerializeAsString());
     }
     
-    namespace apollo {
-namespace drivers {
-namespace velodyne {
-    }
-    }
-    }
+        std::vector<std::pair<string,string>> option_pairs;
+    ParseGeneratorParameter(parameter, &option_pairs);
     
-    void Poller::HandleChanges() {
-  CtrlParamMap local_params;
-  {
-    ReadLockGuard<AtomicRWLock> lck(poll_data_lock_);
-    if (ctrl_params_.empty()) {
-      return;
-    }
-    local_params.swap(ctrl_params_);
+    // ReadNextBox factors out the code to interpret a line of a box
+// file so that applybox and unicharset_extractor interpret the same way.
+// This function returns the next valid box file utf8 string and coords
+// and returns true, or false on eof (and closes the file).
+// It ignores the utf8 file signature ByteOrderMark (U+FEFF=EF BB BF), checks
+// for valid utf-8 and allows space or tab between fields.
+// utf8_str is set with the unichar string, and bounding box with the box.
+// If there are page numbers in the file, it reads them all.
+bool ReadNextBox(int *line_number, FILE* box_file,
+                 STRING* utf8_str, TBOX* bounding_box);
+// As ReadNextBox above, but get a specific page number. (0-based)
+// Use -1 to read any page number. Files without page number all
+// read as if they are page 0.
+bool ReadNextBox(int target_page, int *line_number, FILE* box_file,
+                 STRING* utf8_str, TBOX* bounding_box);
+    
+      // Returns the x,y means as an FCOORD.
+  FCOORD mean_point() const;
+    
+    static STRING ParagraphJustificationToString(
+    tesseract::ParagraphJustification justification) {
+  switch (justification) {
+    case JUSTIFICATION_LEFT:
+      return 'LEFT';
+    case JUSTIFICATION_RIGHT:
+      return 'RIGHT';
+    case JUSTIFICATION_CENTER:
+      return 'CENTER';
+    default:
+      return 'UNKNOWN';
   }
-    }
-    
-    
-    {  return (b * d - 3.0 * a * c) / (d * d * d);
 }
     
-    #pragma once
+      /* Reject modes generated after NN_ACCEPT but before MM_ACCEPT */
+  R_HYPHEN,        // TEMP Post NN dodgy hyphen or full stop
+  R_DUBIOUS,       // TEMP Post NN dodgy chars
+  R_NO_ALPHANUMS,  // TEMP No alphanumerics in word after NN
+  R_MOSTLY_REJ,    // TEMP Most of word rejected so rej the rest
+  R_XHT_FIXUP,     // TEMP Xht tests unsure
     
-    #define AINFO_EVERY(freq) \
-  LOG_EVERY_N(INFO, freq) << LEFT_BRACKET << MODULE_NAME << RIGHT_BRACKET
-#define AWARN_EVERY(freq) \
-  LOG_EVERY_N(WARNING, freq) << LEFT_BRACKET << MODULE_NAME << RIGHT_BRACKET
-#define AERROR_EVERY(freq) \
-  LOG_EVERY_N(ERROR, freq) << LEFT_BRACKET << MODULE_NAME << RIGHT_BRACKET
+    #include 'errcode.h'
     
+      // Removes the MAXIMUM element of the heap. (MIN from a MAX heap.) If entry is
+  // not nullptr, the element is copied into *entry, otherwise it is discarded.
+  // Time = O(n). Returns false if the heap was already empty.
+  bool PopWorst(Pair* entry) {
+    int worst_index = IndexOfWorst();
+    if (worst_index < 0) return false;  // It cannot be empty!
+    // Extract the worst element from the heap, leaving a hole at worst_index.
+    if (entry != nullptr)
+      *entry = heap_[worst_index];
+    int heap_size = heap_.size() - 1;
+    if (heap_size > 0) {
+      // Sift the hole upwards to match the last element of the heap_
+      Pair hole_pair = heap_[heap_size];
+      int hole_index = SiftUp(worst_index, hole_pair);
+      heap_[hole_index] = hole_pair;
+    }
+    heap_.truncate(heap_size);
+    return true;
+  }
     
-    {
-    {
-    {
-    {}  // namespace valet_parking
-}  // namespace scenario
-}  // namespace planning
-}  // namespace apollo
+    // Displays classification as the given shape_id. Creates as many windows
+// as it feels fit, using index as a guide for placement. Adds any created
+// windows to the windows output and returns a new index that may be used
+// by any subsequent classifiers. Caller waits for the user to view and
+// then destroys the windows by clearing the vector.
+int TessClassifier::DisplayClassifyAs(
+    const TrainingSample& sample, Pix* page_pix, int unichar_id, int index,
+    PointerVector<ScrollView>* windows) {
+  int shape_id = unichar_id;
+  // TODO(rays) Fix this so it works with both flat and real shapetables.
+  //  if (GetShapeTable() != nullptr)
+  //  shape_id = BestShapeForUnichar(sample, page_pix, unichar_id, nullptr);
+  if (shape_id < 0) return index;
+  if (UnusedClassIdIn(classify_->PreTrainedTemplates, shape_id)) {
+    tprintf('No built-in templates for class/shape %d\n', shape_id);
+    return index;
+  }
+  classify_->ShowBestMatchFor(shape_id, sample.features(),
+                              sample.num_features());
+  return index;
+}
+    
+      if (obj->dictLookup('D', &tmp)->isArray()) {
+    Array * dim = tmp.getArray();
+    
+    if (dim->getLength() >= 2) {
+      Object dd;
+      if (dim->get(0, &dd)->isInt()) {
+	width = dd.getInt();
+      }
+      dd.free();
+      if (dim->get(1, &dd)->isInt()) {
+	height = dd.getInt();
+      }
+      dd.free();
+    }
+  }
+  tmp.free();
