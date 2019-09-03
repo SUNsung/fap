615f3f3356cf474b78d528b507cfb4439b703944
SUNsung <weatherius@gmail.com>
2019-09-03 21:31:59 +0300

    Rewrite [405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb] File in: 2019-09-03 21:31:59

--- ruby/405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb
+++ ruby/405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb
@@ -1,145 +1,182 @@
 
         
-        class BugTest < Minitest::Test
-  def test_stuff
-    assert 'zomg'.present?
-    refute ''.present?
+        namespace :bower do
+    
+      def process_bootstrap
+    log_status 'Convert Bootstrap LESS to Sass'
+    puts ' repo   : #@repo_url'
+    puts ' branch : #@branch_sha #@repo_url/tree/#@branch'
+    puts ' save to: #{@save_to.to_json}'
+    puts ' twbs cache: #{@cache_path}'
+    puts '-' * 60
+    
+        alias log puts
+    
+    if rails_env == 'production'
+  config('without test:development')
+elsif rails_env == 'test'
+  config('without development')
+end
+    
+      def aspect_params
+    params.require(:aspect).permit(:name, :chat_enabled, :order_id)
   end
 end
 
     
-          def test_invert_drop_join_table
-        block = Proc.new { }
-        create_join_table = @recorder.inverse_of :drop_join_table, [:musics, :artists, table_name: :catalog], &block
-        assert_equal [:create_join_table, [:musics, :artists, table_name: :catalog], block], create_join_table
-      end
+    class ContactsController < ApplicationController
+  before_action :authenticate_user!
     
-          # 2FA: Request security code to trusted phone
-      stub_request(:put, 'https://idmsa.apple.com/appleauth/auth/verify/phone').
-        with(body: '{\'phoneNumber\':{\'id\':1},\'mode\':\'sms\'}').
-        to_return(status: 200, body: '', headers: {})
+      def create
+    # Contacts autocomplete does not work the same way on mobile and desktop
+    # Mobile returns contact ids array while desktop returns person id
+    # This will have to be removed when mobile autocomplete is ported to Typeahead
+    recipients_param, column = [%i(contact_ids id), %i(person_ids person_id)].find {|param, _| params[param].present? }
+    if recipients_param
+      person_ids = current_user.contacts.mutual.where(column => params[recipients_param].split(',')).pluck(:person_id)
+    end
     
-            all_tvos = devices.map do |device|
-          device = device.downcase
-          device.include?('apple tv')
-        end
-        # Return true if all devices are iOS devices
-        return true unless all_tvos.include?(false)
+        @invalid_emails = html_safe_string_from_session_array(:invalid_email_invites)
+    @valid_emails   = html_safe_string_from_session_array(:valid_email_invites)
     
-          def xcodebuild_log_path(device_type: nil, language: nil, locale: nil)
-        name_components = [Snapshot.project.app_name, Snapshot.config[:scheme]]
+        if message.save
+      logger.info 'event=create type=message user=#{current_user.diaspora_handle} status=success ' \
+                  'message=#{message.id} chars=#{params[:message][:text].length}'
+      Diaspora::Federation::Dispatcher.defer_dispatch(current_user, message)
+    else
+      flash[:error] = I18n.t('conversations.new_conversation.fail')
+    end
+    redirect_to conversations_path(:conversation_id => conversation.id)
+  end
+end
+
     
-              # It is valid to have the same name for multiple devices
-          device = client.create_device!(name, udid, mac: mac)
-    
-              # Raise a custom exception, as the the normal one is useless for the user
-          UI.user_error!('A build error occurred, this is usually related to code signing. Take a look at the error above')
-        end
-      end
-    
-          # Some providers have a lot (> 100) entries, which makes browsing them unwieldy.
-      # Any present in the list below will have an extra set of types added, breaking the pages out into the different
-      # products they offer.
-      LARGE_PROVIDERS = {
-        'aws'     => true,
-        'azurerm' => true,
-        'google'  => true,
+        respond_to do |format|
+      format.html
+      format.xml { render :xml => @notifications.to_xml }
+      format.json {
+        render json: render_as_json(@unread_notification_count, @grouped_unread_notification_counts, @notifications)
       }
+    end
+  end
     
-            at_css('h1').content = 'Apache Cordova' if root_page?
-    
-          def get_name
-        name = context[:html_title]
-        name.remove! 'SQLite Query Language: '
-        name.remove! %r{\.\z}
-        name = at_css('#document_title').content if name == 'No Title'
-        name
-      end
-    
-            def initialize(machine, config)
-          super
-    
-    describe VagrantPlugins::Chef::Cap::FreeBSD::ChefInstalled do
-  include_context 'unit'
-    
-            expect(box_collection).to receive(:add).with(any_args) { |path, name, version, **opts|
-          expect(checksum(path)).to eq(checksum(box_path))
-          expect(name).to eq('foo')
-          expect(version).to eq('0')
-          expect(opts[:metadata_url]).to be_nil
-          true
-        }.and_return(box)
-    
-    module VagrantPlugins
-  module DockerProvider
-    class Provider < Vagrant.plugin('2', :provider)
-      @@host_vm_mutex = Mutex.new
-    
-          trigger = trigger_run.after_triggers.first
-      shell_config = trigger.run_remote
-      on_error = trigger.on_error
-      exit_codes = trigger.exit_codes
-    
-        it 'searches current PATH if original PATH did not result in valid executable' do
-      expect(Vagrant::Util::Which).to receive(:which).with('ssh', original_path: true).and_return(nil)
-      expect(Vagrant::Util::Which).to receive(:which).with('ssh').and_return('valid-return')
-      allow(Vagrant::Util::SafeExec).to receive(:exec).and_return(nil)
-      described_class.exec(ssh_info)
+        def roles
+      ['--roles ROLES', '-r',
+       'Run SSH commands only on hosts matching these roles',
+       lambda do |value|
+         Configuration.env.add_cmdline_filter(:role, value)
+       end]
     end
     
-      if rss_url && rss_url.length > 0
-    blogs.push(Struct::Blog.new(name, web_url, rss_url))
-  else
-    unavailable.push(Struct::Blog.new(name, web_url, rss_url))
-  end
-end
-    
-    module LogStash
-  module PluginManager
-  end
-end
-    
-          explicit_plugins_specs = explicitly_declared_plugins_specs
-    
-              it 'successfully install the plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-dns')
-          end
+            if echo?
+          $stdin.gets
+        else
+          $stdin.noecho(&:gets).tap { $stdout.print '\n' }
         end
-    
-        context 'without a specific plugin' do
-      it 'display a list of plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list')
-        expect(result.stdout.split('\n').size).to be > 1
+      rescue Errno::EIO
+        # when stdio gets closed
+        return
       end
     
-        after :each do
-      logstash.uninstall
+          def warn_third_party_scm_must_be_upgraded
+        $stderr.puts(<<-MESSAGE)
+[Deprecation Notice] `set :scm, #{scm_name.inspect}` is deprecated.
+To ensure this custom SCM will work with future versions of Capistrano,
+please upgrade it to a version that uses the new SCM plugin mechanism
+documented here:
+    
+          it 'flashes an error message' do
+        post '/users/api_secrets', params: { api_secret: invalid_params }
+        expect(flash[:error]).to be_truthy
+        expect(flash[:notice]).to be_nil
+      end
     end
-    
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
   end
-    
-    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
+end
 
     
-      # Condenses multiple spaces and tabs into a single space
-  def condense_spaces(input)
-    input.gsub(/\s{2,}/, ' ')
+        it 'denies chat channel invitation to non-authorized user' do
+      expect do
+        post '/chat_channel_memberships', params: {
+          chat_channel_membership: {
+            user_id: second_user.id, chat_channel_id: chat_channel.id
+          }
+        }
+      end.to raise_error(Pundit::NotAuthorizedError)
+    end
   end
     
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
+      def user_defined_image(article)
+    return article.social_image if article.social_image.present?
+    return article.main_image if article.main_image.present?
+    return article.video_thumbnail_url if article.video_thumbnail_url.present?
+  end
+end
+
+    
+      def new
+    @page = Page.new
+  end
+    
+        it 'allows Resque helpers to point to different Redi' do
+      conn = MiniTest::Mock.new
+      conn.expect(:multi, []) { |*args, &block| block.call }
+      conn.expect(:zadd, 1, [String, Array])
+      DWorker.sidekiq_options('pool' => ConnectionPool.new(size: 1) { conn })
+      Sidekiq::Client.enqueue_in(10, DWorker, 3)
+      conn.verify
+    end
+  end
+    
+        it 'can schedule' do
+      ss = Sidekiq::ScheduledSet.new
+      q = Sidekiq::Queue.new
+    
+      class YetAnotherCustomMiddleware
+    def initialize(name, recorder)
+      @name = name
+      @recorder = recorder
+    end
+    
+        it 'removes the enqueued_at field when scheduling' do
+      ss = Sidekiq::ScheduledSet.new
+      ss.clear
+    
+      describe 'redis connection' do
+  	it 'returns error without creating a connection if block is not given' do
+  		assert_raises(ArgumentError) do
+  			Sidekiq.redis
       end
-      super
+  	end
+  end
+    
+        ## Write the results to a file
+    ## Requires railsexpress patched MRI build
+    # brew install qcachegrind
+    #File.open('callgrind.profile', 'w') do |f|
+      #RubyProf::CallTreePrinter.new(result).print(f, :min_percent => 1)
+    #end
+  end
+end
+    
+        def tmux_layout_command
+      '#{project.tmux} select-layout -t #{tmux_window_target} #{layout}'
+    end
+    
+        it 'returns the string' do
+      expect(project.send('hook_#{hook_name}')).to eq('echo 'on hook'')
+    end
+  end
+    
+      it { expect(subject.tmux_window_and_pane_target).to eql 'foo:0.1' }
+end
+
+    
+        msg = 'Actual pane does not match expected'
+    msg << '\n  Expected #{@commands} but has #{actual.commands}' if @commands
+    msg << '\n  Expected pane to have #{@expected_attrs}' if @expected_attrs
+  end
+    
+            expect(described_class.validate).to be_a Tmuxinator::Project
+      end
     end
