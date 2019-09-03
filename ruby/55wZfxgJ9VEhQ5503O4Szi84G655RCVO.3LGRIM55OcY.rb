35356130355d1c9db254d441c57561a2b5f3a7b1
SUNsung <weatherius@gmail.com>
2019-09-04 01:32:17 +0300

    Rewrite [55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb] File in: 2019-09-04 01:32:17

--- ruby/55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb
+++ ruby/55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb
@@ -1,218 +1,247 @@
 
         
-          def setup
-    # enable a logger so that (e.g.) the benchmarking stuff runs, so we can get
-    # a more accurate simulation of what happens in 'real life'.
-    super
-    @controller.logger = ActiveSupport::Logger.new(nil)
+          def test_inspect_limited_select_instance
+    assert_equal %(#<Topic id: 1>), Topic.all.merge!(select: 'id', where: 'id = 1').first.inspect
+    assert_equal %(#<Topic id: 1, title: 'The First Topic'>), Topic.all.merge!(select: 'id, title', where: 'id = 1').first.inspect
   end
     
-          def publish(name, *args)
-        notifier.publish(name, *args)
+    require 'active_support'
+require 'active_support/core_ext/object/blank'
+require 'minitest/autorun'
+    
+      def test_throw_away_typing
+    d = Developer.new('name' => 'David', 'salary' => '100,000')
+    assert_not_predicate d, :valid?
+    assert_equal 100, d.salary
+    assert_equal '100,000', d.salary_before_type_cast
+  end
+    
+        before_action :ensure_configured
+    
+        config.action_mailbox.queues = ActiveSupport::InheritableOptions.new \
+      incineration: :action_mailbox_incineration, routing: :action_mailbox_routing
+    
+      spec.files         = %w[
+    LICENSE.txt
+    README.md
+    ext/etc/constdefs.h
+    ext/etc/etc.c
+    ext/etc/extconf.rb
+    ext/etc/mkconstants.rb
+    stub/etc.rb
+    test/etc/test_etc.rb
+  ]
+  spec.bindir        = 'exe'
+  spec.require_paths = ['lib']
+  spec.extensions    = %w{ext/etc/extconf.rb}
+    
+        def prepare_format
+      @column_separator = @options[:column_separator].to_s.encode(@encoding)
+      row_separator = @options[:row_separator]
+      if row_separator == :auto
+        @row_separator = $INPUT_RECORD_SEPARATOR.encode(@encoding)
+      else
+        @row_separator = row_separator.to_s.encode(@encoding)
+      end
+      @quote_character = @options[:quote_character]
+      @force_quotes = @options[:force_quotes]
+      unless @force_quotes
+        @quotable_pattern =
+          Regexp.new('[\r\n'.encode(@encoding) +
+                     Regexp.escape(@column_separator) +
+                     Regexp.escape(@quote_character.encode(@encoding)) +
+                     ']'.encode(@encoding))
+      end
+      @quote_empty = @options.fetch(:quote_empty, true)
+    end
+    
+    require 'racc/raccs'
+    
+          it 'does not decrement pos' do
+        @gz.ungetc ''
+        @gz.pos.should == 10
       end
     
-            attr_reader :symbol
+      before do
+    @zeros    = Zlib::Deflate.deflate('0' * 100_000)
+    @inflator = Zlib::Inflate.new
+    @chunks   = []
     
-            def select_directory(file_name)
-          @categories.keys.find do |category|
-            File.exist?(File.join(category_directory(category), file_name))
-          end
+    class SessionsController < Devise::SessionsController
+  # rubocop:disable Rails/LexicallyScopedActionFilter
+  before_action :authenticate_with_2fa, only: :create
+  after_action :reset_authentication_token, only: :create
+  before_action :reset_authentication_token, only: :destroy
+  # rubocop:enable Rails/LexicallyScopedActionFilter
+    
+        @posts_per_day = Post.where('created_at >= ?', Date.today - 21.days).group('DATE(created_at)').order('DATE(created_at) ASC').count
+    @most_posts_within = @posts_per_day.values.max.to_f
+    
+        if @aspect.update_attributes!(aspect_params)
+      flash[:notice] = I18n.t 'aspects.update.success', :name => @aspect.name
+    else
+      flash[:error] = I18n.t 'aspects.update.failure', :name => @aspect.name
+    end
+    render :json => { :id => @aspect.id, :name => @aspect.name }
+  end
+    
+        redirect_to stream_path
+  end
+end
+
+    
+      def create
+    conversation = Conversation.find(params[:conversation_id])
+    
+      def default_serializer_options
+    {
+      context: self,
+      root:    false
+    }
+  end
+    
+    namespace :doc do
+  task :readmes do
+    Dir.glob 'lib/rack/protection/*.rb' do |file|
+      excluded_files = %w[lib/rack/protection/base.rb lib/rack/protection/version.rb]
+      next if excluded_files.include?(file)
+      doc  = File.read(file)[/^  module Protection(\n)+(    #[^\n]*\n)*/m].scan(/^ *#(?!#) ?(.*)\n/).join('\n')
+      file = 'doc/#{file[4..-4].tr('/_', '-')}.rdoc'
+      Dir.mkdir 'doc' unless File.directory? 'doc'
+      puts 'writing #{file}'
+      File.open(file, 'w') { |f| f << doc }
+    end
+  end
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
+    
+        it 'leaves TempFiles untouched' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['file'][:filename] + '\n' + \
+                                                 request.params['file'][:tempfile].read + '\n' + \
+                                                 request.params['other']]]
+      end
+    
+        context 'for a class definition' do
+      let(:src) { 'class C < Super; def method; end end' }
+    
+          class << self
+        attr_reader :processed_source
+    
+          def within_column_limit?(element, max, line)
+        element && element.loc.column < max && element.loc.line == line
+      end
+    
+              check(node, node.arguments, 'parameter of %<article>s method call',
+                node.last_argument.source_range.end_pos,
+                node.source_range.end_pos)
+        end
+        alias on_csend on_send
+    
+      it 'registers offense when guard clause is before `begin`' do
+    expect_offense(<<~RUBY)
+      def foo
+        return another_object if something_different?
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Add empty line after guard clause.
+        begin
+          bar
+        rescue SomeException
+          baz
         end
       end
-    end
+    RUBY
   end
-end
-
     
-            def find(key)
-          file_name = '#{key}#{@extension}'
-    
-      def notification_service
-    @notification_service ||= NotificationService.new
-  end
-end
-
-    
-      def output_chronic_duration_attribute(source_attribute)
-    value = attributes[source_attribute.to_s]
-    ChronicDuration.output(value, format: :short) if value
-  end
-end
-
-    
-              find_every_method_call_by_name(body_node, :xcodebuild).each do |m|
-            next if parameters_passed?(m, /SYMROOT=/)
-    
-        it 'deprecated ENV.universal_binary usage' do
-      expect_offense(<<~RUBY)
-        class Foo < Formula
-          desc 'foo'
-          url 'https://brew.sh/foo-1.0.tgz'
-          if build?
-             ENV.universal_binary
-             ^^^^^^^^^^^^^^^^^^^^ macOS has been 64-bit only since 10.6 so ENV.universal_binary is deprecated.
-          end
-        end
-      RUBY
-    end
-    
-      describe '#display_s' do
-    context 'without specific version' do
-      its(:display_s) { is_expected.to eq('java') }
-    end
-    
-        def languages
-      @languages ||= [
-        *ARGV.value('language')&.split(','),
-        *ENV['HOMEBREW_LANGUAGES']&.split(','),
-        *Open3.capture2('defaults', 'read', '-g', 'AppleLanguages')[0].scan(/[^ \n'(),]+/),
-      ].uniq
-    end
-    
-        def downloader
-      @downloader ||= begin
-        strategy = DownloadStrategyDetector.detect(cask.url.to_s, cask.url.using)
-        strategy.new(cask.url.to_s, cask.token, cask.version, cache: Cache.path, **cask.url.specs)
-      end
-    end
-    
-          return unless @cask.sourcefile_path
-    
-          it 'adds the appropriate curl args' do
-        expect(subject).to receive(:system_command!) { |*, args:, **|
-          expect(args.each_cons(2)).to include(['-d', 'form=data'])
-          expect(args.each_cons(2)).to include(['-d', 'is=good'])
-        }
-    
-    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
-  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
-  require 'rubygems'
-  require 'bundler/setup'
-  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-elsif ENV['COCOAPODS_NO_BUNDLER']
-  require 'rubygems'
-  gem 'cocoapods'
-end
-    
-          def ==(other)
-        self.class == other.class &&
-            spec == other.spec &&
-            used_by_non_library_targets_only? == other.used_by_non_library_targets_only?
-      end
-    end
-  end
-end
-
-    
-              # Prints a warning informing the user that a build configuration of
-          # the integrated target is overriding the CocoaPods build settings.
-          #
-          # @param  [Target::AggregateTarget] pod_bundle
-          #         The Pods bundle.
-          #
-          # @param  [XcodeProj::PBXNativeTarget] target
-          #         The native target.
-          #
-          # @param  [Xcodeproj::XCBuildConfiguration] config
-          #         The build configuration.
-          #
-          # @param  [String] key
-          #         The key of the overridden build setting.
-          #
-          def self.print_override_warning(pod_bundle, target, config, key)
-            actions = [
-              'Use the `$(inherited)` flag, or',
-              'Remove the build settings from the target.',
-            ]
-            message = 'The `#{target.name} [#{config.name}]` ' \
-              'target overrides the `#{key}` build setting defined in ' \
-              '`#{pod_bundle.pod_bundle.xcconfig_relative_path(config.name)}'. ' \
-              'This can lead to problems with the CocoaPods installation'
-            UI.warn(message, actions)
-          end
-    
-            it 'adds the developer frameworks search paths to the xcconfig if XCTest has been detected' do
-          xcconfig = BuildSettings.new(stub('Target'))
-          xcconfig.stubs(:frameworks => %w(XCTest))
-          frameworks_search_paths = xcconfig.framework_search_paths
-          frameworks_search_paths.should == %w($(PLATFORM_DIR)/Developer/Library/Frameworks)
-        end
-    
-            # @return [PodfileDependencyCache] the cache of all dependencies in the podfile.
-        #
-        attr_reader :podfile_dependency_cache
-    
-          def title
-        @page.title
+          # The name of the dispatched method as a symbol.
+      #
+      # @return [Symbol] the name of the dispatched method
+      def method_name
+        send_node.method_name
       end
     
-          def title
-        'Latest Changes (Globally)'
+        context 'when create fails' do
+      let(:invalid_params) { { description: nil } } # Force model validation error
+    
+      it 'renders a user index if there is a user with the username successfully' do
+    expect(get: '/#{user.username}').to route_to(
+      controller: 'stories',
+      action: 'index',
+      username: user.username,
+    )
+  end
+    
+        def set(*args)
+      options = args.extract_options!
+      options.each do |name, value|
+        set_preference name, value
       end
     
-    def commit_details
-  { :message => 'Did something at #{Time.now}',
-    :name    => 'Tom Preston-Werner',
-    :email   => 'tom@github.com' }
-end
+                shipment.save!
     
-      test 'clean path with double leading slash' do
-    assert_equal '/Mordor', clean_path('//Mordor')
-  end
-end
-    
-      test 'remove page extentions' do
-    view = Precious::Views::LatestChanges.new
-    assert_equal 'page', view.remove_page_extentions('page.wiki')
-    assert_equal 'page-wiki', view.remove_page_extentions('page-wiki.md')
-    assert_equal 'file.any_extention', view.remove_page_extentions('file.any_extention')
-  end
-    
-          def preference_field_for(form, field, options)
-        case options[:type]
-        when :integer
-          form.text_field(field, preference_field_options(options))
-        when :boolean
-          form.check_box(field, preference_field_options(options))
-        when :string
-          form.text_field(field, preference_field_options(options))
-        when :password
-          form.password_field(field, preference_field_options(options))
-        when :text
-          form.text_area(field, preference_field_options(options))
+              scope = scope.not_deleted unless params[:show_deleted]
+          scope = scope.not_discontinued unless params[:show_discontinued]
         else
-          form.text_field(field, preference_field_options(options))
-        end
-      end
-    
-            def address_params
-          params.require(:address).permit(permitted_address_attributes)
+          scope = Product.accessible_by(current_ability, :show).active.includes(*product_includes)
         end
     
-            def destroy
-          @option_type = Spree::OptionType.accessible_by(current_ability, :destroy).find(params[:id])
-          @option_type.destroy
-          render plain: nil, status: 204
+            def find_address
+          if @order.bill_address_id == params[:id].to_i
+            @order.bill_address
+          elsif @order.ship_address_id == params[:id].to_i
+            @order.ship_address
+          else
+            raise CanCan::AccessDenied
+          end
         end
-    
-            def taxon
-          @taxon ||= taxonomy.taxons.accessible_by(current_ability, :show).find(params[:id])
-        end
-    
-      if options.respond_to? 'keys'
-    options.each do |k,v|
-      unless v.nil?
-        v = v.join ',' if v.respond_to? 'join'
-        v = v.to_json if v.respond_to? 'keys'
-        output += ' data-#{k.sub'_','-'}='#{v}''
       end
     end
-  elsif options.respond_to? 'join'
-    output += ' data-value='#{config[key].join(',')}''
-  else
-    output += ' data-value='#{config[key]}''
   end
-  output += '></#{tag}>'
 end
+
     
-    end
+            def create
+          authorize! :create, Image
+          @image = scope.images.new(image_params)
+          if @image.save
+            respond_with(@image, status: 201, default_template: :show)
+          else
+            invalid_resource!(@image)
+          end
+        end
+    
+            def fire
+          inventory_unit.send('#{@event}!') if @event
+        end
+    
+            def update
+          @option_type = Spree::OptionType.accessible_by(current_ability, :update).find(params[:id])
+          if @option_type.update(option_type_params)
+            render :show
+          else
+            invalid_resource!(@option_type)
+          end
+        end
+    
+            def create
+          authorize! :create, Spree::OptionValue
+          @option_value = scope.new(option_value_params)
+          if @option_value.save
+            render :show, status: 201
+          else
+            invalid_resource!(@option_value)
+          end
+        end
+    
+            def update
+          if @property
+            authorize! :update, @property
+            @property.update(property_params)
+            respond_with(@property, status: 200, default_template: :show)
+          else
+            invalid_resource!(@property)
+          end
+        end
