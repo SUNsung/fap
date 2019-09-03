5b4962a08e276d312e2754a9a6e60e46fcdc5e78
SUNsung <weatherius@gmail.com>
2019-09-03 12:23:23 +0300

    Rewrite [313vaQKhusjM231rlUtkRcB2qM31VJcjN3hM5oU318Nb.cpp] File in: 2019-09-03 12:23:22

--- cccccc/313vaQKhusjM231rlUtkRcB2qM31VJcjN3hM5oU318Nb.cpp
+++ cccccc/313vaQKhusjM231rlUtkRcB2qM31VJcjN3hM5oU318Nb.cpp
@@ -1,174 +1,341 @@
 
         
-        #include 'db/dbformat.h'
-#include 'db/filename.h'
-#include 'db/table_cache.h'
-#include 'db/version_edit.h'
-#include 'leveldb/db.h'
-#include 'leveldb/env.h'
-#include 'leveldb/iterator.h'
+         private:
+  se::blas::UpperLower uplo_;
     
-    enum FileType {
-  kLogFile,
-  kDBLockFile,
-  kTableFile,
-  kDescriptorFile,
-  kCurrentFile,
-  kTempFile,
-  kInfoLogFile  // Either the current one, or an old one
-};
-    
-    class Slice;
-    
-      // Return a pointer to the beginning of the referenced data
-  const char* data() const { return data_; }
-    
-    #include 'leveldb/db.h'
-#include 'util/testharness.h'
-    
-    
-    {  dst.clear();
-  PutFixed64(&dst, 0x0807060504030201ull);
-  ASSERT_EQ(8, dst.size());
-  ASSERT_EQ(0x01, static_cast<int>(dst[0]));
-  ASSERT_EQ(0x02, static_cast<int>(dst[1]));
-  ASSERT_EQ(0x03, static_cast<int>(dst[2]));
-  ASSERT_EQ(0x04, static_cast<int>(dst[3]));
-  ASSERT_EQ(0x05, static_cast<int>(dst[4]));
-  ASSERT_EQ(0x06, static_cast<int>(dst[5]));
-  ASSERT_EQ(0x07, static_cast<int>(dst[6]));
-  ASSERT_EQ(0x08, static_cast<int>(dst[7]));
-}
-    
-        // if we are in integer mode, within the base, and we're on the last digit then
-    // there are special cases where we can actually add one more digit.
-    if (isIntegerMode && pNumSec->value.size() == maxCount && !m_hasExponent)
-    {
-        bool allowExtraDigit = false;
+    // ConditionalThunk implements the conditional instruction on GPU by reading the
+// predicate of the conditional and executing the true or the false computation
+// depending on the value of the predicate.
+//
+// ConditionalThunk assumes that the buffers of the conditional result and the
+// result of the true and false computations share the same allocation. Also,
+// the buffers of the true operand of the conditional and that of the parameter
+// instruction of the true computation share the same allocation. Similarly, the
+// buffers of the false operand and that of the parameter instruction of the
+// false computation share the same allocation.
+class ConditionalThunk : public Thunk {
+ public:
+  ConditionalThunk(
+      const BufferAllocation::Slice& branch_index_buffer_index,
+      absl::Span<const BufferAllocation::Slice> branch_operand_buffer_indexes,
+      std::vector<ThunkSequence> branch_thunk_sequences,
+      const HloInstruction* hlo);
     }
     
-    using namespace CalcEngine;
-using namespace CalcEngine::RationalMath;
-using namespace std;
-    
-                void OnModeChanged();
-    
-            static int Initialize();
-        static void RegisterHosts();
-        static INarratorAnnouncementHost ^ GetHostProducer();
-    
-    NcclComm::~NcclComm()
-{
-    if (m_stream != nullptr)
-        cudaStreamDestroy(m_stream);
-    if (m_ncclComm != nullptr)
-        ncclCommDestroy(m_ncclComm);
-}
-    
-    
-    {            return tokenEnd; // done with the macro now
+            BufferAllocation::Slice slice = outfeed_slices_.element(index);
+        se::DeviceMemoryBase data_address;
+        if (slice.allocation()) {
+          // If we have a static allocation, read it from there. This avoids
+          // synchronizing the host and device just to read a pointer.
+          data_address = buffer_allocations.GetDeviceAddress(slice);
+        } else {
+          // Otherwise we have to read the tuple pointer first.
+          CHECK(!index.empty());
+          // Copy the parent buffer to the host.
+          BufferAllocation::Slice tuple_slice =
+              outfeed_slices_.element(ShapeIndexView(index).ConsumeFront());
+          if (!tuple_slice.allocation()) {
+            return Unimplemented(
+                'Nested dynamic tuples are not supported on GPU');
+          }
+          se::DeviceMemoryBase tuple_address =
+              buffer_allocations.GetDeviceAddress(tuple_slice);
+          CHECK(tuple_slice.size() % sizeof(void*) == 0)
+              << 'Tuple size must be a multiple of pointer size';
+          std::vector<void*> tuple_element_buffer_addresses(tuple_slice.size() /
+                                                            sizeof(void*));
+          stream.ThenMemcpy(tuple_element_buffer_addresses.data(),
+                            tuple_address, tuple_slice.size());
+          TF_RETURN_IF_ERROR(stream.BlockHostUntilDone());
+          // The data address is specified by the element of the tuple pointer
+          // buffer.
+          data_address =
+              se::DeviceMemoryBase(tuple_element_buffer_addresses[index.back()],
+                                   (*buffer)->length());
         }
     
+      const std::vector<std::unique_ptr<Thunk>>& thunks() const { return thunks_; }
     
-    {        // We don't use CreateFromFile() here since the user might specify OutputNodeNames in the config.
-        // By not compiling the network before patching, we avoid double log output for validation.
-        net = make_shared<ComputationNetwork>(deviceId);
-        net->SetTraceLevel(config(L'traceLevel', 0));
-        net->Read<ElemType>(modelPath);
-        if (outputNodeNames.size() > 0)
-            PatchOutputNodes(net, outputNodeNames, outputNodeNamesVector);
-        net->CompileNetwork();
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    // Creates a scalar add computation and returns it.
+XlaComputation CreateScalarAddComputation(PrimitiveType type,
+                                          XlaBuilder* builder);
+    
+        OP_REQUIRES(
+        context, TensorShapeUtils::IsVectorOrHigher(buffer_shape),
+        errors::InvalidArgument('Output must be at least 1-D, ',
+                                'got shape: ', buffer_shape.DebugString()));
+    
+    struct PARA;
+    
+      double m() const;  // get gradient
+  double c(double m) const;            // get constant
+  double rms(double m, double c) const;            // get error
+  double pearson() const;  // get correlation coefficient.
+    
+    const int kBlnCellHeight = 256;     // Full-height for baseline normalization.
+const int kBlnXHeight = 128;        // x-height for baseline normalization.
+const int kBlnBaselineOffset = 64;  // offset for baseline normalization.
+    
+        REJ(  //classwise copy
+        const REJ &source) {
+      flags1 = source.flags1;
+      flags2 = source.flags2;
     }
+    
+    namespace tesseract {
+    }
+    
+      // Functions to navigate the tree. Unlike the original implementation, we
+  // store the root at index 0.
+  int ParentNode(int index) const {
+    return (index + 1) / 2 - 1;
+  }
+  int LeftChild(int index) const {
+    return index * 2 + 1;
+  }
+    
+    
+    { private:
+  // Data members are private to keep deletion of data_ encapsulated.
+  Data* data_;
+  Key key_;
+};
+// Specialization of KDPtrPair to provide operator< for sorting in increasing
+// order.
+template <typename Key, typename Data>
+struct KDPtrPairInc : public KDPtrPair<Key, Data> {
+  // Since we are doing non-standard stuff we have to duplicate *all* the
+  // constructors and operator=.
+  KDPtrPairInc() : KDPtrPair<Key, Data>() {}
+  KDPtrPairInc(Key k, Data* d) : KDPtrPair<Key, Data>(k, d) {}
+  KDPtrPairInc(KDPtrPairInc& src) : KDPtrPair<Key, Data>(src) {}
+  void operator=(KDPtrPairInc& src) {
+    KDPtrPair<Key, Data>::operator=(src);
+  }
+  // Operator< facilitates sorting in increasing order.
+  int operator<(const KDPtrPairInc<Key, Data>& other) const {
+    return this->key() < other.key();
+  }
+};
+// Specialization of KDPtrPair to provide operator< for sorting in decreasing
+// order.
+template <typename Key, typename Data>
+struct KDPtrPairDec : public KDPtrPair<Key, Data> {
+  // Since we are doing non-standard stuff we have to duplicate *all* the
+  // constructors and operator=.
+  KDPtrPairDec() : KDPtrPair<Key, Data>() {}
+  KDPtrPairDec(Key k, Data* d) : KDPtrPair<Key, Data>(k, d) {}
+  KDPtrPairDec(KDPtrPairDec& src) : KDPtrPair<Key, Data>(src) {}
+  void operator=(KDPtrPairDec& src) {
+    KDPtrPair<Key, Data>::operator=(src);
+  }
+  // Operator< facilitates sorting in decreasing order by using operator> on
+  // the key values.
+  int operator<(const KDPtrPairDec<Key, Data>& other) const {
+    return this->key() > other.key();
+  }
+};
+    
+    
+    {}  // namespace tesseract.
+    
+    // Provides access to the ShapeTable that this classifier works with.
+const ShapeTable* TessClassifier::GetShapeTable() const {
+  return classify_->shape_table();
+}
+// Provides access to the UNICHARSET that this classifier works with.
+// Only needs to be overridden if GetShapeTable() can return nullptr.
+const UNICHARSET& TessClassifier::GetUnicharset() const {
+  return classify_->unicharset;
+}
+    
+    	return 'OGGVorbis';
+}
+void ResourceImporterOGGVorbis::get_recognized_extensions(List<String> *p_extensions) const {
+    
+    
+    {	Particles2DEditorPlugin(EditorNode *p_node);
+	~Particles2DEditorPlugin();
+};
+    
+    	void _process_packet(int p_from, const uint8_t *p_packet, int p_packet_len);
+	void _process_simplify_path(int p_from, const uint8_t *p_packet, int p_packet_len);
+	void _process_confirm_path(int p_from, const uint8_t *p_packet, int p_packet_len);
+	Node *_process_get_node(int p_from, const uint8_t *p_packet, int p_packet_len);
+	void _process_rpc(Node *p_node, const StringName &p_name, int p_from, const uint8_t *p_packet, int p_packet_len, int p_offset);
+	void _process_rset(Node *p_node, const StringName &p_name, int p_from, const uint8_t *p_packet, int p_packet_len, int p_offset);
+	void _process_raw(int p_from, const uint8_t *p_packet, int p_packet_len);
+    
+    public:
+	bool is_valid() const;
+	bool intersect_segment(const Vector3 &p_begin, const Vector3 &p_end, Vector3 &r_point, Vector3 &r_normal) const;
+	bool intersect_ray(const Vector3 &p_begin, const Vector3 &p_dir, Vector3 &r_point, Vector3 &r_normal) const;
+	bool intersect_convex_shape(const Plane *p_planes, int p_plane_count) const;
+	bool inside_convex_shape(const Plane *p_planes, int p_plane_count, Vector3 p_scale = Vector3(1, 1, 1)) const;
+	Vector3 get_area_normal(const AABB &p_aabb) const;
+	PoolVector<Face3> get_faces() const;
+    
+    
+    {	ADD_PROPERTY(PropertyInfo(Variant::INT, 'device'), 'set_device', 'get_device');
+}
+    
+    		uint32_t texture_count;
+    
+    	if (feedback_count) {
+		Vector<const char *> feedback;
+		for (int i = 0; i < feedback_count; i++) {
+    }
+    }
+    
+    
+    {	Object *o = ObjectDB::get_instance(obj);
+	if (!o)
+		return Dictionary();
+	Variant dict = o->get(property);
+	if (dict.get_type() != Variant::DICTIONARY)
+		return Dictionary();
+	return dict;
+}
+    
+    	tree = memnew(Tree);
+	add_child(tree);
+    
+    
+    {					for (int j = 0; j < fc; j++) {
+						iw[j * 3 + 0] = chart.faces[j].vertex[0];
+						iw[j * 3 + 1] = chart.faces[j].vertex[1];
+						iw[j * 3 + 2] = chart.faces[j].vertex[2];
+					}
+				}
+    
+    		updating = false;
     
     
     {
-    {                    if (tokenStart >= totalLength)
-                    {
-                        // if nothing left, we are done
-                        break;
-                    }
-                }
-            }
-            // token has been determined to range from tokenStart to tokenEnd
+    {				prev_idx = idx;
+			}
+		}
     
-                // 4. third, allocate space for accumulated gradient
-            for (auto& n : m_LearnableNodePtr)
-            {
-                auto node = n.second;
-                if (node->IsParameterUpdateRequired())
-                {
-                    wstring nodeName = node->GetName();
-                    shared_ptr<ComputationNode<ElemType>> pLearnableNode = node; // TODO: what's this for?
-                    const auto& funvalue = pLearnableNode->Value(); // gradient may not be allocated when this function is first called
-                    size_t nrow = funvalue.GetNumRows();
-                    size_t ncol = funvalue.GetNumCols();
-                    if (m_cachedGradient.find(nodeName) == m_cachedGradient.end())
-                    {
-                        // not allocated yet
-                        auto matrixp = make_shared<Matrix<ElemType>>(nrow, ncol, funvalue.GetDeviceId());
-                        matrixp->SetValue(0);
-                        m_cachedGradient.AddInput(nodeName, matrixp, pLearnableNode->GetMBLayout()/*null*/, pLearnableNode->GetSampleLayout());
-                    }
-                }
-            }
-            // 5. for stateful node
-            for (auto x : m_netStatefulNodes)
-            {
-                wstring name = x.first;
-                if (m_netStates[name].empty())
-                {
-                    // this only happens in the first minibatch in an epoch
-                    m_netStates[name].resize(actualnumSubminibatches);
-                }
-            }
+    /* QuadricOrientation */
+#define GLU_OUTSIDE                        100020
+#define GLU_INSIDE                         100021
     
-    // struct _Null {};
-// _Null Null;
-enum class Null {};
+      //----- initialization and control
     
-    char *string_html_decode(const char *input, int &len,
-                         bool decode_double_quote, bool decode_single_quote,
-                         const char *charset_hint, bool all,
-                         bool xhp /* = false */) {
-  assert(input);
+    private:
+  void parse(Object *tree);
+    
+    //========================================================================
+//
+// Modified under the Poppler project - http://poppler.freedesktop.org
+//
+// All changes made under the Poppler project to this file are licensed
+// under GPL version 2 or later
+//
+// Copyright (C) 2006, 2009, 201, 2010 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2006 Krzysztof Kowalczyk <kkowalczyk@gmail.com>
+// Copyright (C) 2009 Ilya Gorenbein <igorenbein@finjan.com>
+//
+// To see a description of the changes please see the Changelog file that
+// came with your tarball or type make ChangeLog if you are building from git
+//
+//========================================================================
+    
+      //----- text drawing
+  virtual void beginStringOp(GfxState *state);
+  virtual void endStringOp(GfxState *state);
+  virtual GBool beginType3Char(GfxState *state, double x, double y,
+			       double dx, double dy,
+			       CharCode code, Unicode *u, int uLen);
+  virtual void endType3Char(GfxState *state);
+    
+      // parse from a floating window parameters dictionary
+  void parseFWParams(Object* obj);
+    
+    GBool ExternalSecurityHandler::authorize(void *authData) {
+  char *key;
+  int length;
     }
     
-    #endif
-#endif
+    Sound::Sound(Object *obj, bool readAttrs)
+{
+  streamObj = new Object();
+  streamObj->initNull();
+  obj->copy(streamObj);
+    }
+    
+    void SplashOutputDev::clipToStrokePath(GfxState *state) {
+  SplashPath *path, *path2;
+    }
+    
+    #endif //__CCINSTANT_ACTION_H__
 
     
-    #include <folly/Singleton.h>
-#include <folly/system/ThreadName.h>
+    #endif // __ACTION_CCPAGETURN3D_ACTION_H__
+
     
-      static ReflectionTypeAliasHandle* Get(ObjectData* obj) {
-    return Native::data<ReflectionTypeAliasHandle>(obj);
-  }
+        /** 
+     * @brief Initializes with a duration and destination percentage. 
+     * @param duration Specify the duration of the ProgressTo action. It's a value in seconds.
+     * @param percent Specify the destination percentage.
+     * @return If the creation success, return true; otherwise, return false.
+     */
+    bool initWithDuration(float duration, float percent);
     
-    
-    {  struct stat st;
-  auto w = Stream::getWrapperFromURI(StrNR(cleanPath));
-  if (w && !dynamic_cast<FileStreamWrapper*>(w)) {
-    if (w->stat(cleanPath.c_str(), &st)) {
-      return;
-    }
-    if (S_ISDIR(st.st_mode)) {
-      return;
-    }
-    const auto f = w->open(StrNR(cleanPath), 'r', 0, nullptr);
-    if (!f) return;
-    auto str = f->read();
-    HackCFactsExtractor::parse_file_impl(cleanPath, allowHipHopSyntax,
-                                         str.slice(), res, state);
-  } else {
-    // It would be nice to have an atomic stat + open operation here but this
-    // doesn't seem to be possible with STL in a portable way.
-    if (stat(cleanPath.c_str(), &st)) {
-      return;
-    }
-    if (S_ISDIR(st.st_mode)) {
-      return;
-    }
-    HackCFactsExtractor::parse_file_impl(cleanPath, allowHipHopSyntax,
-                                         folly::StringPiece{''}, res, state);
-  }
+    ActionTween *ActionTween::clone() const
+{
+    return ActionTween::create(_duration, _key, _from, _to);
 }
+    
+    /** Read an NSDictionary from a plist file and parse it automatically for animations */
+void AnimationCache::addAnimationsWithFile(const std::string& plist)
+{
+    CCASSERT(!plist.empty(), 'Invalid texture file name');
+    if (plist.empty()) {
+        log('%s error:file name is empty!', __FUNCTION__);
+        return;
+    }
+    
+    ValueMap dict = FileUtils::getInstance()->getValueMapFromFile(plist);
+    }
+    
+    /**
+ * PolygonInfo is an object holding the required data to display Sprites.
+ * It can be a simple as a triangle, or as complex as a whole 3D mesh
+ */
+class CC_DLL PolygonInfo
+{
+public:
+    /// @name Creators
+    /// @{
+    /**
+     * Creates an empty Polygon info
+     * @memberof PolygonInfo
+     * @return PolygonInfo object
+     */
+    PolygonInfo();
+    }
+    }
+    
+        /**
+    @brief Enable/Disable the clipping.
+    @param enabled Pass true to enable clipping. Pass false to disable clipping.
+    */
+    void setClippingEnabled(bool enabled) {
+        _clippingEnabled = enabled;
+    }
+    
+    bool ComponentContainer::add(Component *com)
+{
+    bool ret = false;
+    CCASSERT(com != nullptr, 'Component must be non-nil');
+    CCASSERT(com->getOwner() == nullptr, 'Component already added. It can't be added again');
+    do
+    {
+        auto componentName = com->getName();
+    }
+    }
+    
+    http://www.cocos2d-x.org
