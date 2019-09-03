1352ba6aced9fe4d543c67a4482afe9ce646918c
SUNsung <weatherius@gmail.com>
2019-09-03 11:15:58 +0300

    Rewrite [14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py] File in: 2019-09-03 11:15:57

--- pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
+++ pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
@@ -1,145 +1,72 @@
 
         
-            def get(self):
-        if len(self.config.GAE_APPIDS):
-            if len(self.working_appid_list) == 0:
-                time_to_reset = 600 - (time.time() - self.last_reset_time)
-                if time_to_reset > 0:
-                    self.logger.warn('all appid out of quota, wait %d seconds to reset', time_to_reset)
-                    time.sleep(time_to_reset)
-                    return None
-                else:
-                    self.logger.warn('reset appid')
-                    self.reset_appid()
+        site_info = 'CNTV.com'
+download = cntv_download
+download_playlist = playlist_not_supported('cntv')
+
+    
+    	xml = get_html('http://www.ehow.com/services/video/series.xml?demand_ehow_videoid=%s' % vid)
+    
+	from xml.dom.minidom import parseString
+	doc = parseString(xml)
+	tab = doc.getElementsByTagName('related')[0].firstChild
+    
+        Args:
+        trees           决策树的集合
+        row             测试数据集的每一行数据
+    Returns:
+        返回随机森林中，决策树结果出现次数做大的
+    '''
+    
+        def bp_sensitivity_map(self, sensitivity_array,
+                           activator):
+        '''
+        计算传递到上一层的sensitivity map
+        sensitivity_array: 本层的sensitivity map
+        activator: 上一层的激活函数
+        '''
+        # 处理卷积步长，对原始sensitivity map进行扩展
+        expanded_array = self.expand_sensitivity_map(
+            sensitivity_array)
+        # full卷积，对sensitivitiy map进行zero padding
+        # 虽然原始输入的zero padding单元也会获得残差
+        # 但这个残差不需要继续向上传递，因此就不计算了
+        expanded_width = expanded_array.shape[2]
+        zp = (self.input_width +  
+              self.filter_width - 1 - expanded_width) / 2
+        padded_array = padding(expanded_array, zp)
+        # 初始化delta_array，用于保存传递到上一层的
+        # sensitivity map
+        self.delta_array = self.create_delta_array()
+        # 对于具有多个filter的卷积层来说，最终传递到上一层的
+        # sensitivity map相当于所有的filter的
+        # sensitivity map之和
+        for f in range(self.filter_number):
+            filter = self.filters[f]
+            # 将filter权重翻转180度
+            flipped_weights = np.array(map(
+                lambda i: np.rot90(i, 2), 
+                filter.get_weights()))
+            # 计算与一个filter对应的delta_array
+            delta_array = self.create_delta_array()
+            for d in range(delta_array.shape[0]):
+                conv(padded_array[f], flipped_weights[d],
+                    delta_array[d], 1, 0)
+            self.delta_array += delta_array
+        # 将计算结果与激活函数的偏导数做element-wise乘法操作
+        derivative_array = np.array(self.input_array)
+        element_wise_op(derivative_array, 
+                        activator.backward)
+        self.delta_array *= derivative_array
     
     
-def get_cmd_out(cmd):
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out = proc.stdout
-    lines = out.readlines()
-    return lines
-    
-        def triger_check_network(self, fail=False, force=False):
-        time_now = time.time()
-        if not force:
-            if self._checking_num > 0:
-                return
-    
-            # mmmhhh,... perhaps look at the first argument
-        # (f_locals[co_varnames[0]]?) and test if it's a (sub)class of
-        # requested recognizer...
-        
-        rules = []
-        for frame in reversed(inspect.stack()):
-            code = frame[0].f_code
-            codeMod = inspect.getmodule(code)
-            if codeMod is None:
-                continue
-    
-    
-    
-    
-    def __str__(self):
-        if self.type == EOF:
-            return '<EOF>'
-    
-    
-def test_astype_categorical():
-    arr = period_array(['2000', '2001', '2001', None], freq='D')
-    result = arr.astype('category')
-    categories = pd.PeriodIndex(['2000', '2001'], freq='D')
-    expected = pd.Categorical.from_codes([0, 1, 1, -1], categories=categories)
-    tm.assert_categorical_equal(result, expected)
-    
-        def __init__(self, values, dtype=None, copy=False):
-        for val in values:
-            if not isinstance(val, self.dtype.type):
-                raise TypeError('All values must be of type ' + str(self.dtype.type))
-        self.data = values
-    
-            result = float_frame.cov(min_periods=len(float_frame) + 1)
-        assert isna(result.values).all()
-    
-        def test_oauth10_request_parameters(self):
-        response = self.fetch('/oauth10/client/request_params')
-        response.rethrow()
-        parsed = json_decode(response.body)
-        self.assertEqual(parsed['oauth_consumer_key'], 'asdf')
-        self.assertEqual(parsed['oauth_token'], 'uiop')
-        self.assertTrue('oauth_nonce' in parsed)
-        self.assertTrue('oauth_signature' in parsed)
-    
-    
-def main():
-    test = TestSelectionSort()
-    selection_sort = SelectionSort()
-    test.test_selection_sort(selection_sort.sort)
-    try:
-        test.test_selection_sort(selection_sort.sort_recursive)
-        test.test_selection_sort(selection_sort.sor_iterative_alt)
-    except NameError:
-        # Alternate solutions are only defined
-        # in the solutions file
-        pass
-    
-            print('Test: Enqueue on empty stack')
-        print('Test: Enqueue on non-empty stack')
-        print('Test: Multiple enqueue in a row')
-        num_items = 3
-        for i in range(0, num_items):
-            queue.enqueue(i)
-    
-    		def PreOrder (node):
-			if node is not None:
-				preOrder.append(node.data)
-				PreOrder(node.leftChild)
-				PreOrder(node.rightChild)
-    
-            print('Test: remove on a key that already exists')
-        hash_table.remove(10)
-        assert_equal(hash_table.get(0), 'foo')
-        assert_raises(KeyError, hash_table.get, 10)
-    
-            if 'start_period' in service_dict['healthcheck']:
-            service_dict['healthcheck']['start_period'] = serialize_ns_time_value(
-                service_dict['healthcheck']['start_period']
-            )
-    
-            return reduce(get_value, key.split('.'), self.dictionary)
-    
-    import codecs
-import hashlib
-import json.decoder
-import logging
-import ntpath
-import random
-    
-    
-def tls_config_from_options(options, environment=None):
-    environment = environment or Environment()
-    cert_path = environment.get('DOCKER_CERT_PATH') or None
-    
-            if config_path == self.config_file.filename:
-            try:
-                service_config = self.config_file.get_service(service_name)
-            except KeyError:
-                raise ConfigurationError(
-                    'Cannot extend service '{}' in {}: Service not found'.format(
-                        service_name, config_path)
-                )
-        else:
-            extends_file = ConfigFile.from_filename(config_path)
-            validate_config_version([self.config_file, extends_file])
-            extended_file = process_config_file(
-                extends_file, self.environment, service_name=service_name
-            )
-            service_config = extended_file.get_service(service_name)
-    
-        opts = parse_opts(args)
-    
-        def test_create_container_with_cpu_quota(self):
-        service = self.create_service('db', cpu_quota=40000, cpu_period=150000)
-        container = service.create_container()
-        container.start()
-        assert container.get('HostConfig.CpuQuota') == 40000
-        assert container.get('HostConfig.CpuPeriod') == 150000
+# 数据加载器基类
+class Loader(object):
+    def __init__(self, path, count):
+        '''
+        初始化加载器
+        path: 数据文件路径
+        count: 文件中的样本个数
+        '''
+        self.path = path
+        self.count = count
