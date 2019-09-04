fe5e922747b22d09e1befa39f7ea5ce00f78e532
SUNsung <weatherius@gmail.com>
2019-09-04 16:26:42 +0300

    Rewrite [302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb] File in: 2019-09-04 16:26:42

--- ruby/302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb
+++ ruby/302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb
@@ -1,142 +1,273 @@
 
         
-            def translation_scope
-      'devise.confirmations'
+          class Processor
+    attr_reader :receiver
+    
+    # This is used in topic lists
+require_dependency 'topic_poster'
+    
+          trigger_after_events unless opts[:skip_events]
+    
+          ensure_cors!
+      presigned_url(obj, :put, UPLOAD_URL_EXPIRES_AFTER_SECONDS)
+    rescue Aws::Errors::ServiceError => e
+      Rails.logger.warn('Failed to generate upload URL for S3: #{e.message.presence || e.class.name}')
+      raise StorageError
     end
+    
+          it 'should return signed URL for private uploads in S3' do
+        SiteSetting.prevent_anons_from_downloading_files = true
+        SiteSetting.authorized_extensions = 'pdf'
+    
+          # it works with arrays of values
+      tc.record_change('colors', nil, ['red', 'blue'])
+      expect(tc.diff['colors']).to eq([nil, ['red', 'blue']])
+    
+    class HeatSettingsUpdater
+  def self.update
+    return unless SiteSetting.automatic_topic_heat_values
+    
+          it 'doesn't change settings when automatic_topic_heat_values is false' do
+        SiteSetting.automatic_topic_heat_values = false
+        expect {
+          update_settings
+        }.to_not change { UserHistory.count }
+        expect_default_values
+      end
+    end
+  end
 end
 
     
-      # DELETE /resource/sign_out
-  def destroy
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message! :notice, :signed_out if signed_out
-    yield if block_given?
-    respond_to_on_destroy
+        before do
+      stub(Agents::DotFoo).valid_type?('Agents::DotFoo') { true }
+      stub(Agents::DotBar).valid_type?('Agents::DotBar') { true }
+    end
+    
+    describe AgentsExporter do
+  describe '#as_json' do
+    let(:name) { 'My set of Agents' }
+    let(:description) { 'These Agents work together nicely!' }
+    let(:guid) { 'some-guid' }
+    let(:tag_fg_color) { '#ffffff' }
+    let(:tag_bg_color) { '#000000' }
+    let(:icon) { 'Camera' }
+    let(:source_url) { 'http://yourhuginn.com/scenarios/2/export.json' }
+    let(:agent_list) { [agents(:jane_weather_agent), agents(:jane_rain_notifier_agent)] }
+    let(:exporter) { AgentsExporter.new(
+      agents: agent_list, name: name, description: description,
+      source_url: source_url, guid: guid, tag_fg_color: tag_fg_color,
+      tag_bg_color: tag_bg_color, icon: icon) }
+    
+          @scheduler.schedule_scheduler_agents
+    
+      let :new_extract do
+    {
+      'url' => { 'css' => '#comic img', 'value' => '@src' },
+      'title' => { 'css' => '#comic img', 'value' => '@alt' },
+      'hovertext' => { 'css' => '#comic img', 'value' => '@title', 'hidden' => true }
+    }
   end
     
-      # Get message for given
-  def find_message(kind, options = {})
-    options[:scope] ||= translation_scope
-    options[:default] = Array(options[:default]).unshift(kind.to_sym)
-    options[:resource_name] = resource_name
-    options = devise_i18n_options(options)
-    I18n.t('#{options[:resource_name]}.#{kind}', options)
+        it 'accepts objects as well as strings' do
+      log = AgentLog.log_for_agent(agents(:jane_website_agent), events(:bob_website_agent_event).payload)
+      expect(log.message).to include(''title'=>'foo'')
+    end
   end
     
-          def remember_me_is_active?(resource)
-        return false unless resource.respond_to?(:remember_me)
-        scope = Devise::Mapping.find_scope!(resource)
-        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
-        resource.remember_me?(token, generated_at)
+      it 'reports an offense when using depends_on :tex' do
+    expect_offense(<<~RUBY)
+      class Foo < Formula
+        url 'https://brew.sh/foo-1.0.tgz'
+        depends_on :tex
+        ^^^^^^^^^^^^^^^ :tex is deprecated.
+      end
+    RUBY
+  end
+end
+    
+        return false if args[:before] && OS::Mac.version >= args[:before]
+    
+        def sdk_path_if_needed(v = nil)
+      # Prefer Xcode SDK when both Xcode and the CLT are installed.
+      # Expected results:
+      # 1. On Xcode-only systems, return the Xcode SDK.
+      # 2. On Xcode-and-CLT systems where headers are provided by the system, return nil.
+      # 3. On CLT-only systems with no CLT SDK, return nil.
+      # 4. On CLT-only systems with a CLT SDK, where headers are provided by the system, return nil.
+      # 5. On CLT-only systems with a CLT SDK, where headers are not provided by the system, return the CLT SDK.
+    
+            # This logic provides a fake Xcode version based on the
+        # installed CLT version. This is useful as they are packaged
+        # simultaneously so workarounds need to apply to both based on their
+        # comparable version.
+        case (DevelopmentTools.clang_version.to_f * 10).to_i
+        when 0       then 'dunno'
+        when 60      then '6.0'
+        when 61      then '6.1'
+        when 70      then '7.0'
+        when 73      then '7.3'
+        when 80      then '8.0'
+        when 81      then '8.3'
+        when 90      then '9.2'
+        when 91      then '9.4'
+        when 100     then '10.2.1'
+        when 110     then '11.0'
+        else              '11.0'
+        end
       end
     
-                authentication_keys_changed || encrypted_password_changed
-          end
-        else
-          def clear_reset_password_token?
-            encrypted_password_changed = respond_to?(:encrypted_password_changed?) && encrypted_password_changed?
-            authentication_keys_changed = self.class.authentication_keys.any? do |attribute|
-              respond_to?('#{attribute}_changed?') && send('#{attribute}_changed?')
-            end
-    
-    end
-end
-end
-
-    
-      #
-  # Returns the hardcore alias for the HTTP service
-  #
-  def self.hardcore_alias(*args)
-    '#{(args[0] || '')}#{(args[1] || '')}'
+      before do
+    allow(machine).to receive(:communicate).and_return(communicator)
   end
     
+            expect { instance }.to_not raise_error
     
-  #
-  # Payload types were copied from xCAT-server source code (IPMI.pm)
-  #
-  RMCP_ERRORS = {
-    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
-    2 => 'Invalid Session ID', #this shouldn't occur...
-    3 => 'Invalid payload type',#shouldn't occur..
-    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
-    5 => 'Invalid integrity algorithm', #same as above
-    6 => 'No matching authentication payload',
-    7 => 'No matching integrity payload',
-    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
-    9 => 'Invalid role',
-    0xa => 'Unauthorised role or privilege level requested',
-    0xb => 'Insufficient resources to create a session at the requested role',
-    0xc => 'Invalid username length',
-    0xd => 'Unauthorized name',
-    0xe => 'Unauthorized GUID',
-    0xf => 'Invalid integrity check value',
-    0x10 => 'Invalid confidentiality algorithm',
-    0x11 => 'No cipher suite match with proposed security algorithms',
-    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
-  }
+          results = subject.machine_config(:default, :foo, boxes)
+      box     = results[:box]
+      config  = results[:config]
+      expect(config.vm.box).to eq('foo')
+      expect(box).to be_nil
+      expect(results[:provider_cls]).to equal(provider_cls)
+    end
     
+                errors << I18n.t('vagrant.general.batch_unexpected_error',
+                             machine: thread[:machine].name,
+                             message: message)
+          else
+            errors << I18n.t('vagrant.general.batch_vagrant_error',
+                             machine: thread[:machine].name,
+                             message: thread[:error].message)
+          end
+        end
+      end
     
-  # open rmcpplus_request with cipherzero
-  def self.create_ipmi_session_open_cipher_zero_request(console_session_id)
-    head = [
-      0x06, 0x00, 0xff, 0x07,   # RMCP Header
-      0x06,                     # RMCP+ Authentication Type
-      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
-      0x00, 0x00, 0x00, 0x00,   # Session ID
-      0x00, 0x00, 0x00, 0x00    # Sequence Number
-    ].pack('C*')
+            expect(described_class.exec(ssh_info)).to eq(nil)
+        expect(Vagrant::Util::SafeExec).to have_received(:exec)
+          .with(ssh_path, 'vagrant@localhost', '-p', '2222', '-o', 'LogLevel=FATAL', '-i', ssh_info[:private_key_path][0])
+      end
+    end
     
-              # Encodes the type field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_type
-            bn = OpenSSL::BN.new(type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
+      def_delegators :@logger, :log, :log_status, :log_processing, :log_transform, :log_file_info, :log_processed, :log_http_get_file, :log_http_get_files, :silence_log
     
-                int
+        def pos=(i)
+      @s.pos = str_to_byte_pos i
+      i
+    end
+    
+      # Generate digests for assets URLs.
+  config.assets.digest = true
+    
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+    
+    module LogStash
+  module PluginManager
+    class Error < StandardError; end
+    
+          PluginManager.ui.info('Installing file: #{local_file}')
+      uncompressed_path = uncompress(local_file)
+      PluginManager.ui.debug('Pack uncompressed to #{uncompressed_path}')
+      pack = LogStash::PluginManager::PackInstaller::Pack.new(uncompressed_path)
+      raise PluginManager::InvalidPackError, 'The pack must contains at least one plugin' unless pack.valid?
+    
+        # remove 'system' local gems used by LS
+    local_gems = gemfile.locally_installed_gems.map(&:name) - NON_PLUGIN_LOCAL_GEMS
+    
+              it 'successfully install the plugin when verification is disabled' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest')
           end
     
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a Kerberos KDC-REQ (response) data
-        # definition
-        class KdcResponse < Element
-          # @!attribute pvno
-          #   @return [Integer] The protocol version number
-          attr_accessor :pvno
-          # @!attribute msg_type
-          #   @return [Integer] The type of a protocol message
-          attr_accessor :msg_type
-          # @!attribute crealm
-          #   @return [String] The realm part of the client's principal identifier
-          attr_accessor :crealm
-          # @!attribute cname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the client's principal identifier
-          attr_accessor :cname
-          # @!attribute ticket
-          #   @return [Rex::Proto::Kerberos::Model::Ticket] The issued ticket
-          attr_accessor :ticket
-          # @!attribute enc_part
-          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] The encrypted part of the response
-          attr_accessor :enc_part
+          it 'list the plugins with their versions' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose')
+        result.stdout.split('\n').each do |plugin|
+          expect(plugin).to match(/^logstash-\w+-\w+\s\(\d+\.\d+.\d+(.\w+)?\)/)
+        end
+      end
+    end
     
-      url 'http://swupdl.adobe.com/updates/oobe/aam20/mac/AdobeLightroom-#{version.major}.0/#{version}/setup.dmg'
-  name 'Adobe Photoshop Lightroom'
-  homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
+          set_preference args[0], args[1] if args.size == 2
+    end
     
-    # Add our files (should be in the current directory):
-package.input('my-executable=/usr/bin/')
-package.input('my-library.so=/usr/lib/')
+        def eligible_for_return?
+      validators.all?(&:eligible_for_return?)
+    end
     
-              if(file.eof?())
-            raise StandardError.new('Invalid tar stream, eof before end-of-tar record')
-          end
+            private
     
-      public(:input, :output, :identifier, :to_s)
+            def find_payment
+          @payment = @order.payments.find_by!(number: params[:id])
+        end
     
-        # Add necessary metadata to the generated manifest.
-    metadata_template = template('p5p_metadata.erb').result(binding)
-    File.write(build_path('#{name}.mog'), metadata_template)
+            def product_property
+          if @product
+            @product_property ||= @product.product_properties.find_by(id: params[:id])
+            @product_property ||= @product.product_properties.includes(:property).where(spree_properties: { name: params[:id] }).first
+            raise ActiveRecord::RecordNotFound unless @product_property
+    
+            def update
+          @shipment = Spree::Shipment.accessible_by(current_ability, :update).readonly(false).find_by!(number: params[:id])
+          @shipment.update_attributes_and_order(shipment_params)
+    
+      def create
+    @address_endpoint = @server.address_endpoints.build(safe_params)
+    if @address_endpoint.save
+      flash[:notice] = params[:return_notice] if params[:return_notice].present?
+      redirect_to_with_json [:return_to, [organization, @server, :address_endpoints]]
+    else
+      render_form_errors 'new', @address_endpoint
+    end
+  end
+    
+      extend ActiveSupport::Concern
+    
+      def new
+    @http_endpoint = @server.http_endpoints.build
+  end
+    
+      before_action :admin_required, :only => [:new, :create]
+  before_action :require_organization_admin, :only => [:edit, :update, :delete, :destroy]
+    
+      include WithinOrganization
+    
+      def show
+    if @server.created_at < 48.hours.ago
+      @graph_type = :daily
+      graph_data = @server.message_db.statistics.get(:daily, [:incoming, :outgoing, :bounces], Time.now, 30)
+    elsif @server.created_at < 24.hours.ago
+      @graph_type = :hourly
+      graph_data = @server.message_db.statistics.get(:hourly, [:incoming, :outgoing, :bounces], Time.now, 48)
+    else
+      @graph_type = :hourly
+      graph_data = @server.message_db.statistics.get(:hourly, [:incoming, :outgoing, :bounces], Time.now, 24)
+    end
+    @first_date = graph_data.first.first
+    @last_date = graph_data.last.first
+    @graph_data = graph_data.map(&:last)
+    @messages = @server.message_db.messages(:order => 'id', :direction => 'desc', :limit => 6)
+  end
+    
+      def persist
+    auth_session.persist! if logged_in?
+    render :plain => 'OK'
+  end
+    
+      private
+    
+      def create
+    @track_domain = @server.track_domains.build(params.require(:track_domain).permit(:name, :domain_id, :track_loads, :track_clicks, :excluded_click_domains, :ssl_enabled))
+    if @track_domain.save
+      redirect_to_with_json [:return_to, [organization, @server, :track_domains]]
+    else
+      render_form_errors 'new', @track_domain
+    end
+  end
+    
+      def new
+    @organization_user = organization.organization_users.build
+  end
