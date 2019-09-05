f138dc7bc2f5e7a4f539389ffc2a13a3385598d1
SUNsung <weatherius@gmail.com>
2019-09-05 18:07:04 +0300

    Rewrite [55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb] File in: 2019-09-05 18:07:03

--- ruby/55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb
+++ ruby/55EDfGgPAsjrw5598NLXIyattE55DYEHz5VKRzw55DR..rb
@@ -1,53 +1,75 @@
 
         
-          describe '#resource' do
-    it 'defines a resource' do
-      subject.resource('foo') { url 'foo-1.0' }
-      expect(subject).to have_defined_resource('foo')
+            it 'creates an agent with a control target' do
+      visit '/'
+      page.find('a', text: 'Agents').trigger(:mouseover)
+      click_on('New Agent')
+    
+      it 'requires a URL or file uplaod' do
+    visit new_scenario_imports_path
+    click_on 'Start Import'
+    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
+  end
+    
+        describe 'with block' do
+      it 'returns a nav link with menu' do
+        stub(self).current_page?('/things') { false }
+        stub(self).current_page?('/things/stuff') { false }
+        nav = nav_link('Things', '/things') { nav_link('Stuff', '/things/stuff') }
+        expect(nav).to be_html_safe
+        a0 = Nokogiri(nav).at('li.dropdown.dropdown-hover:not(.active) > a[href='/things']')
+        expect(a0).to be_a Nokogiri::XML::Element
+        expect(a0.text.strip).to eq('Things')
+        a1 = Nokogiri(nav).at('li.dropdown.dropdown-hover:not(.active) > li:not(.active) > a[href='/things/stuff']')
+        expect(a1).to be_a Nokogiri::XML::Element
+        expect(a1.text.strip).to eq('Stuff')
+      end
+    
+        it 'works for running jobs' do
+      job.locked_at = Time.now
+      job.locked_by = 'test'
+      expect(status(job)).to eq('<span class='label label-info'>running</span>')
     end
     
-        def downloader
-      @downloader ||= begin
-        strategy = DownloadStrategyDetector.detect(cask.url.to_s, cask.url.using)
-        strategy.new(cask.url.to_s, cask.token, cask.version, cache: Cache.path, **cask.url.specs)
-      end
+          it 'does not load workers specified in the :except option' do
+        agent_runner = AgentRunner.new(except: HuginnScheduler)
+        workers = agent_runner.send(:load_workers)
+        expect(workers.keys).to eq(['DelayedJobWorker'])
+    
+          expect(exporter.as_json[:links]).to eq([{ :source => guid_order(agent_list, :jane_weather_agent), :receiver => guid_order(agent_list, :jane_rain_notifier_agent) }])
     end
     
-        def run
-      command_name, *args = detect_command_and_arguments(*@args)
-      command = if help?
-        args.unshift(command_name) unless command_name.nil?
-        'help'
-      else
-        self.class.lookup_command(command_name)
-      end
+      describe '#helpers' do
+    it 'should return the correct request header' do
+      expect(@checker.send(:request_options)).to eq({:headers => {'aftership-api-key' => '800deeaf-e285-9d62-bc90-j999c1973cc9', 'Content-Type'=>'application/json'}})
+    end
     
-          def upload_dest
-        @upload_dest
-      end
+      describe 'receive_events' do
+    it 'should command targets' do
+      stub(Agent).async_check(target.id).once { nil }
     
-          def use_identicon
-        @wiki.user_icons == 'identicon'
-      end
-    
-      test 'clean path with double leading slash' do
-    assert_equal '/Mordor', clean_path('//Mordor')
+    desc 'Test the paperclip plugin under all supported Rails versions.'
+task :all do |t|
+  if ENV['BUNDLE_GEMFILE']
+    exec('rake spec && cucumber')
+  else
+    exec('rm -f gemfiles/*.lock')
+    Rake::Task['appraisal:gemfiles'].execute
+    Rake::Task['appraisal:install'].execute
+    exec('rake appraisal')
   end
 end
     
-      test 'extract destination file name in case of path renaming' do
-    view = Precious::Views::LatestChanges.new
-    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
-    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
+      def framework_major_version
+    framework_version.split('.').first.to_i
   end
+end
+World(RailsCommandHelpers)
+
     
-      test 'transliteration' do
-    # we transliterate only when adapter is grit
-    return if defined?(Gollum::GIT_ADAPTER) && Gollum::GIT_ADAPTER != 'grit'
-    
-          expect(instance.render).to match %r{wemux.ls.2>\/dev\/null}
-    end
-  end
-    
-      orig_stdout = $stdout
-  orig_stderr = $stderr
+    # The base module that gets included in ActiveRecord::Base. See the
+# documentation for Paperclip::ClassMethods for more useful information.
+module Paperclip
+  extend Helpers
+  extend Logger
+  extend ProcessorHelpers
