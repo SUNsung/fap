bdc6b0a139aa844df40b96b9c9e1be7fb9e30b98
SUNsung <weatherius@gmail.com>
2019-09-05 04:31:42 +0300

    Rewrite [28q87iyREjPRs28970czot68yY288wmPn8boB6.28VJ0.cpp] File in: 2019-09-05 04:31:41

--- cccccc/28q87iyREjPRs28970czot68yY288wmPn8boB6.28VJ0.cpp
+++ cccccc/28q87iyREjPRs28970czot68yY288wmPn8boB6.28VJ0.cpp
@@ -1,168 +1,296 @@
 
         
-          net::NSSCertDatabase* cert_db_;
-  // Whether the certificate database has a public slot associated with the
-  // profile. If not set, importing certificates is not allowed with this model.
-  bool is_user_db_available_;
+        template <typename T>
+void DynamicStitchGPUImpl(const Eigen::GpuDevice& gpu_device,
+                          const int32 slice_size, const int32 first_dim_size,
+                          const GpuDeviceArrayStruct<int>& input_indices,
+                          const GpuDeviceArrayStruct<const T*>& input_ptrs,
+                          T* output);
+#define REGISTER_GPU(T)                                           \
+  extern template void DynamicStitchGPUImpl(                      \
+      const Eigen::GpuDevice& gpu_device, const int32 slice_size, \
+      const int32 first_dim_size,                                 \
+      const GpuDeviceArrayStruct<int32>& input_indices,           \
+      const GpuDeviceArrayStruct<const T*>& input_ptrs, T* output);
+TF_CALL_GPU_NUMBER_TYPES(REGISTER_GPU);
+TF_CALL_complex64(REGISTER_GPU);
+TF_CALL_complex128(REGISTER_GPU);
+TF_CALL_int64(REGISTER_GPU);
+TF_CALL_int32(REGISTER_GPU);
+#undef REGISTER_GPU
     
-      v8::Local<v8::Value> PeekNext() const;
-    
-    // Like Dictionary, but stores object in persistent handle so you can keep it
-// safely on heap.
-class PersistentDictionary : public Dictionary {
- public:
-  PersistentDictionary();
-  PersistentDictionary(v8::Isolate* isolate, v8::Local<v8::Object> object);
-  PersistentDictionary(const PersistentDictionary& other);
-  ~PersistentDictionary() override;
-    }
-    
-    template <>
-struct Converter<Promise> {
-  static v8::Local<v8::Value> ToV8(v8::Isolate* isolate, Promise val);
-  // TODO(MarshallOfSound): Implement FromV8 to allow promise chaining
-  //                        in native land
-  // static bool FromV8(v8::Isolate* isolate,
-  //                    v8::Local<v8::Value> val,
-  //                    Promise* out);
+    struct TfLiteModelInfo {
+  std::vector<const TfLiteTensor*> inputs;
+  std::vector<const TfLiteTensor*> outputs;
 };
     
-    namespace mate {
-    }
-    
-    #if defined(OS_WIN)
-#include <windows.h>
-#endif  // defined(OS_WIN)
-    
-    ProcessSingleton::~ProcessSingleton() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  // Manually free resources with IO explicitly allowed.
-  base::ThreadRestrictions::ScopedAllowIO allow_io;
-  watcher_ = nullptr;
-  ignore_result(socket_dir_.Delete());
-}
-    
-      // 3. Use of forward and backward slashes in value
-  ASSERT_TRUE(parser.ParseKeyValuePair('test:data = test/data.libsvm',
-                                       &key, &value));
-  ASSERT_EQ(key, 'test:data');
-  ASSERT_EQ(value, 'test/data.libsvm');
-  ASSERT_TRUE(parser.ParseKeyValuePair('data = C:\\data.libsvm', &key, &value));
-  ASSERT_EQ(key, 'data');
-  ASSERT_EQ(value, 'C:\\data.libsvm');
-    
-    
-    {
-    {#ifndef XGBOOST_USE_CUDA
-  EXPECT_EQ(GPUSet::AllVisible(), GPUSet::Empty());
-#endif
-}
-}  // namespace xgboost
+    #endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 
     
-    template<typename StorageIndex>
-class SparsePageLZ4Format : public SparsePageFormat {
- public:
-  explicit SparsePageLZ4Format(bool use_lz4_hc)
-      : use_lz4_hc_(use_lz4_hc) {
-    raw_bytes_ = raw_bytes_value_ = raw_bytes_index_ = 0;
-    encoded_bytes_value_ = encoded_bytes_index_ = 0;
-    nthread_ = dmlc::GetEnv('XGBOOST_LZ4_DECODE_NTHREAD', 4);
-    nthread_write_ = dmlc::GetEnv('XGBOOST_LZ4_COMPRESS_NTHREAD', 12);
-  }
-  virtual ~SparsePageLZ4Format() {
-    size_t encoded_bytes = raw_bytes_ +  encoded_bytes_value_ + encoded_bytes_index_;
-    raw_bytes_ += raw_bytes_value_ + raw_bytes_index_;
-    if (raw_bytes_ != 0) {
-      LOG(CONSOLE) << 'raw_bytes=' << raw_bytes_
-                   << ', encoded_bytes=' << encoded_bytes
-                   << ', ratio=' << double(encoded_bytes) / raw_bytes_
-                   << ', ratio-index=' << double(encoded_bytes_index_) /raw_bytes_index_
-                   << ', ratio-value=' << double(encoded_bytes_value_) /raw_bytes_value_;
+    template <typename T>
+__global__ void MatrixDiagKernel(const int num_threads, const int num_rows,
+                                 const int num_cols, const int num_diags,
+                                 const int max_diag_len,
+                                 const int lower_diag_index,
+                                 const int upper_diag_index, const T padding,
+                                 const T* diag_ptr, T* output_ptr) {
+  GPU_1D_KERNEL_LOOP(index, num_threads) {
+    const int batch_and_row_index = index / num_cols;
+    const int col = index - batch_and_row_index * num_cols;
+    const int batch = batch_and_row_index / num_rows;
+    const int row = batch_and_row_index - batch * num_rows;
+    const int diag_index = col - row;
+    const int diag_index_in_input = upper_diag_index - diag_index;
+    const int index_in_the_diagonal = col - max(diag_index, 0);
+    if (lower_diag_index <= diag_index && diag_index <= upper_diag_index) {
+      output_ptr[index] =
+          diag_ptr[batch * num_diags * max_diag_len +
+                   diag_index_in_input * max_diag_len + index_in_the_diagonal];
+    } else {
+      output_ptr[index] = padding;
     }
   }
+}
+    
+    namespace tensorflow {
+namespace grappler {
+    }
     }
     
-        // create (floating point) JSON numbers
-    json j_ok(v_ok);
-    json j_nan(v_nan);
-    json j_infinity(v_infinity);
-    json j_float(n_float);
-    json j_float_nan(n_float_nan);
-    json j_double(n_double);
+      // Null char in the string.
     
-    void  wakeupLock_Unlock(void* _object) {
-    xverbose_function();
-    xassert2(_object);
-    xdebug2(TSF'_object= %0', _object);
+    void MapLiteTestUtil::ExpectMapFieldsSetInitialized(
+    const unittest::TestMapLite& message) {
+  MapTestUtilImpl::ExpectMapFieldsSetInitialized<unittest::MapEnumLite,
+                                                 unittest::MAP_ENUM_FOO_LITE>(
+      message);
+}
+    
+    TEST(StructurallyValidTest, ValidUTF8String) {
+  // On GCC, this string can be written as:
+  //   'abcd 1234 - \u2014\u2013\u2212'
+  // MSVC seems to interpret \u differently.
+  string valid_str('abcd 1234 - \342\200\224\342\200\223\342\210\222 - xyz789');
+  EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data(),
+                                      valid_str.size()));
+  // Additional check for pointer alignment
+  for (int i = 1; i < 8; ++i) {
+    EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data() + i,
+                                        valid_str.size() - i));
+  }
+}
+    
+    using google::protobuf::FileDescriptorProto;
+using google::protobuf::FileDescriptor;
+using google::protobuf::DescriptorPool;
+using google::protobuf::io::Printer;
+using google::protobuf::util::SchemaGroupStripper;
+using google::protobuf::util::EnumScrubber;
+    
+    
+    {  }
+    
+      // True when a ScopedPyObjectPtr and a raw pointer refer to the same object.
+  // Comparison operators are non reflexive.
+  bool operator==(const PyObjectStruct* p) const { return ptr_ == p; }
+  bool operator!=(const PyObjectStruct* p) const { return ptr_ != p; }
+    
+    /**
+ * @brief Parses nano-seconds out of specified date strings and returns
+ *        difference.
+ *
+ * @param iso1 Date string in format: 2017-05-01T16:08:43.661631023Z
+ * @param iso2 Date string in format: 2017-05-01T16:08:43.661631023Z
+ * @return Nano-seconds difference.
+ */
+long diffNanos(const std::string& iso1, const std::string& iso2) {
+  if (iso1.empty() || iso2.empty()) {
+    return 0L;
+  }
     }
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+    namespace osquery {
+    }
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+    
+    {} // namespace osquery
+
     
     
     {
-    {        if (_size != fwrite(zero_data, sizeof(char), _size, file)) {
-            _mmmap_file.close();
-            fclose(file);
-            boost::filesystem::remove(_filepath);
-            delete[] zero_data;
-            return false;
-        }
-        fclose(file);
-        delete[] zero_data;
-    }
-#endif
-    return is_open;
+    {  EXPECT_EQ(
+      hashFromFile(HashType::HASH_TYPE_SHA256,
+                   (getWorkingDir() / fs::path('test.data.extract')).string()),
+      hashFromFile(HashType::HASH_TYPE_SHA256, test_data_file.string()));
+}
+} // namespace osquery
+
+    
+      /// Update the refresh rate.
+  void setRefresh(size_t refresh_sec);
+    
+      // Updating with a new source will reconfigure.
+  get().update({{'data', '{\'options\':{}}'}, {'data1', '{}'}});
+  EXPECT_EQ(placebo->configures, 3U);
+  // Updating and not including a source is handled by the config plugin.
+  // The config will expect the other source to update asynchronously and does
+  // not consider the missing key as a delete request.
+  get().update({{'data', '{\'options\':{}}'}});
+  EXPECT_EQ(placebo->configures, 3U);
+    
+    std::map<std::string, std::string> getTestConfigMap(const std::string& file) {
+  std::string content;
+  auto const filepath = getTestConfigDirectory() / file;
+  auto status = readFile(filepath, content);
+  EXPECT_TRUE(status.ok())
+      << 'Could not read file: ' << boost::io::quoted(filepath.string())
+      << ', because: ' << status.what();
+  std::map<std::string, std::string> config;
+  config['awesome'] = content;
+  return config;
 }
     
+    #include <map>
+#include <string>
     
-    {	return s_version;
+      virtual ExpectedSuccess<DatabaseError> putInt32(const std::string& domain,
+                                                  const std::string& key,
+                                                  const int32_t value);
+  virtual ExpectedSuccess<DatabaseError> putString(
+      const std::string& domain,
+      const std::string& key,
+      const std::string& value) = 0;
+    
+      // Performance and optimization settings.
+  options.compression = rocksdb::kNoCompression;
+  options.compaction_style = rocksdb::kCompactionStyleLevel;
+  options.arena_block_size = (4 * 1024);
+  options.write_buffer_size = (4 * 1024) * FLAGS_rocksdb_buffer_blocks;
+  options.max_write_buffer_number =
+      static_cast<int>(FLAGS_rocksdb_write_buffer);
+  options.min_write_buffer_number_to_merge =
+      static_cast<int>(FLAGS_rocksdb_merge_number);
+    
+    std::string Flag::getValue(const std::string& name) {
+  const auto& custom = instance().custom_;
+  auto custom_flag = custom.find(name);
+  if (custom_flag != custom.end()) {
+    return custom_flag->second;
+  }
+    }
+    
+    #include 'env/env_chroot.h'
+    
+    /*
+ * Class:     org_rocksdb_CompactionJobStats
+ * Method:    fileRangeSyncNanos
+ * Signature: (J)J
+ */
+jlong Java_org_rocksdb_CompactionJobStats_fileRangeSyncNanos(
+    JNIEnv*, jclass, jlong jhandle) {
+  auto* compact_job_stats =
+      reinterpret_cast<rocksdb::CompactionJobStats*>(jhandle);
+  return static_cast<jlong>(
+      compact_job_stats->file_range_sync_nanos);
 }
     
-    enum {
-    kRetCodeInternalStateError =  -888,
-    kRetCodeParamNotMatch,
-    kRetCodeDnsItemNotFound,
-    kRetCodeGetADDRTimeout
-};
-struct DnsItem {
-    thread_tid      threadid;
-    //parameter
-    const char *node;
-    const char *service;
-    const struct addrinfo *hints;
-    struct addrinfo **res;
-    //~parameter
+    /*
+ * Class:     org_rocksdb_CompressionOptions
+ * Method:    maxDictBytes
+ * Signature: (J)I
+ */
+jint Java_org_rocksdb_CompressionOptions_maxDictBytes(
+    JNIEnv*, jobject, jlong jhandle) {
+  auto* opt = reinterpret_cast<rocksdb::CompressionOptions*>(jhandle);
+  return static_cast<jint>(opt->max_dict_bytes);
+}
     
-    int error_code;
-    int status;
+    /*
+ * Class:     org_rocksdb_HdfsEnv
+ * Method:    disposeInternal
+ * Signature: (J)V
+ */
+void Java_org_rocksdb_HdfsEnv_disposeInternal(
+    JNIEnv*, jobject, jlong jhandle) {
+  auto* e = reinterpret_cast<rocksdb::Env*>(jhandle);
+  assert(e != nullptr);
+  delete e;
+}
     
-    DnsItem(): threadid(0), node(NULL), service(NULL), hints(NULL), res(NULL), error_code(0), status(kGetADDRNotBegin) {}
+    /*
+ * Class:     org_rocksdb_EnvOptions
+ * Method:    setFallocateWithKeepSize
+ * Signature: (JZ)V
+ */
+void Java_org_rocksdb_EnvOptions_setFallocateWithKeepSize(
+    JNIEnv*, jobject, jlong jhandle, jboolean fallocate_with_keep_size) {
+  ENV_OPTIONS_SET_BOOL(jhandle, fallocate_with_keep_size);
+}
     
-    bool EqualParameter(const DnsItem& _item) {
-        return _item.node == node
-            && _item.service == service
-            && _item.hints == hints
-            && _item.res == res;
+    /*
+ * Class:     org_rocksdb_IngestExternalFileOptions
+ * Method:    setAllowBlockingFlush
+ * Signature: (JZ)V
+ */
+void Java_org_rocksdb_IngestExternalFileOptions_setAllowBlockingFlush(
+    JNIEnv*, jobject, jlong jhandle, jboolean jallow_blocking_flush) {
+  auto* options =
+      reinterpret_cast<rocksdb::IngestExternalFileOptions*>(jhandle);
+  options->allow_blocking_flush = static_cast<bool>(jallow_blocking_flush);
+}
+    
+    /*
+ * Class:     org_rocksdb_SstFileManager
+ * Method:    isMaxAllowedSpaceReached
+ * Signature: (J)Z
+ */
+jboolean Java_org_rocksdb_SstFileManager_isMaxAllowedSpaceReached(
+    JNIEnv* /*env*/, jobject /*jobj*/, jlong jhandle) {
+  auto* sptr_sst_file_manager =
+      reinterpret_cast<std::shared_ptr<rocksdb::SstFileManager>*>(jhandle);
+  return sptr_sst_file_manager->get()->IsMaxAllowedSpaceReached();
+}
+    
+    namespace rocksdb {
     }
-    std::string ToString() const {
-        XMessage xmsg;
-        xmsg(TSF'node:%_, service:%_, hints:%_, res:%_, tid:%_, error_code:%_, status:%_',
-             NULL==node?'NULL':node
-            ,NULL==service?'NULL':service
-            ,NULL==hints?(void*)0:hints
-            ,NULL==res?(void*)0:res
-            ,threadid
-            ,error_code
-            ,status);
-        return xmsg.String();
+    
+    void Ripple3D::update(float time)
+{
+    int i, j;
     }
-};
+    
+                    _currentTarget->currentAction->step(dt);
+    
+    PageTurn3D *PageTurn3D::clone() const
+{
+    // no copy constructor
+    return PageTurn3D::create(_duration, _gridSize);
+}
+    
+    /**
+@brief FadeOutDownTiles action.
+@details Fades out the target node with many tiles from top to bottom.
+ */
+class CC_DLL FadeOutDownTiles : public FadeOutUpTiles
+{
+public:
+    /** 
+    * @brief Create the action with the grid size and the duration.
+    * @param duration Specify the duration of the FadeOutDownTiles action. It's a value in seconds.
+    * @param gridSize Specify the size of the grid.
+    * @return If the creation success, return a pointer of FadeOutDownTiles action; otherwise, return nil.
+    */
+    static FadeOutDownTiles* create(float duration, const Size& gridSize);
+    }
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+    
+        std::vector<cocos2d::Vec2> rdp(const std::vector<cocos2d::Vec2>& v, float optimization);
+    float perpendicularDistance(const cocos2d::Vec2& i, const cocos2d::Vec2& start, const cocos2d::Vec2& end);
