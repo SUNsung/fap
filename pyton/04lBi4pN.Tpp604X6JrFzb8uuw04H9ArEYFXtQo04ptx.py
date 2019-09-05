e24197ae07a6493dc34268ace9359e95aacae812
SUNsung <weatherius@gmail.com>
2019-09-05 10:48:24 +0300

    Rewrite [04lBi4pN.Tpp604X6JrFzb8uuw04H9ArEYFXtQo04ptx.py] File in: 2019-09-05 10:48:24

--- pyton/04lBi4pN.Tpp604X6JrFzb8uuw04H9ArEYFXtQo04ptx.py
+++ pyton/04lBi4pN.Tpp604X6JrFzb8uuw04H9ArEYFXtQo04ptx.py
@@ -1,73 +1,38 @@
 
         
-        
-class InlineFieldset(Fieldset):
-    def __init__(self, formset, *args, **kwargs):
-        self.formset = formset
-        super().__init__(*args, **kwargs)
+        if not IS_PYPY:
+    from ._hashing import transform as _hashing_transform
+else:
+    def _hashing_transform(*args, **kwargs):
+        raise NotImplementedError(
+                'FeatureHasher is not compatible with PyPy (see '
+                'https://github.com/scikit-learn/scikit-learn/issues/11540 '
+                'for the status updates).')
     
-        def has_change_permission(self, request, obj=None):
-        return False
-    
-        def test_plain_annotate(self):
-        agg = Sum('book__pages', filter=Q(book__rating__gt=3))
-        qs = Author.objects.annotate(pages=agg).order_by('pk')
-        self.assertSequenceEqual([a.pages for a in qs], [447, None, 1047])
+    from sklearn.cluster import AgglomerativeClustering
     
     
-class UUIDConverter:
-    regex = '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+def bench_scikit_tree_classifier(X, Y):
+    '''Benchmark with scikit-learn decision tree classifier'''
     
-        def _assertPoKeyword(self, keyword, expected_value, haystack, use_quotes=True):
-        q = '''
-        if use_quotes:
-            expected_value = ''%s'' % expected_value
-            q = '''
-        needle = '%s %s' % (keyword, expected_value)
-        expected_value = re.escape(expected_value)
-        return self.assertTrue(
-            re.search('^%s %s' % (keyword, expected_value), haystack, re.MULTILINE),
-            'Could not find %(q)s%(n)s%(q)s in generated PO file' % {'n': needle, 'q': q}
-        )
-    
-        @property
-    def fields(self):
-        'Return a list of fields in the Feature.'
-        return [
-            force_str(
-                capi.get_field_name(capi.get_field_defn(self._layer._ldefn, i)),
-                self.encoding,
-                strings_only=True
-            ) for i in range(self.num_fields)
-        ]
-    
-        @property
-    def boundary(self):
-        'Return the boundary as a newly allocated Geometry object.'
-        return self._topology(capi.geos_boundary(self.ptr))
-    
-        def contribute(self):
-        self.blackboard.common_state['problems'] += random.randint(10, 20)
-        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
-        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
-        self.blackboard.common_state['progress'] += random.randint(10, 30)
+        fn = os.path.relpath(fn,
+                         start=os.path.dirname(__import__(package).__file__))
+    try:
+        lineno = inspect.getsourcelines(obj)[1]
+    except Exception:
+        lineno = ''
+    return url_fmt.format(revision=revision, package=package,
+                          path=fn, lineno=lineno)
     
     
-class UserSpecification(CompositeSpecification):
-    def is_satisfied_by(self, candidate):
-        return isinstance(candidate, User)
+# Code source: Gaël Varoquaux
+# Modified for documentation by Jaques Grobler
+# License: BSD 3 clause
     
-    '''
-*What is this pattern about?
-    
-        def build_size(self):
-        self.size = 'Small'
-    
-    
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
-
-    
-        def add(self, graphic):
-        self.graphics.append(graphic)
+        #Removing unnecessary intermediate files
+    try:
+        os.remove('htmlout.html')
+        os.remove('title.html')
+        os.remove('dir_title.html')
+    except OSError:
+        print('Error removing temporary file(s)')
