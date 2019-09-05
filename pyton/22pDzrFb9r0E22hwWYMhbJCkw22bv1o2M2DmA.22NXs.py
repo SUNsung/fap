4e3f5b48d63c57ddd2ee047be618c4894fb97973
SUNsung <weatherius@gmail.com>
2019-09-05 17:27:57 +0300

    Rewrite [22pDzrFb9r0E22hwWYMhbJCkw22bv1o2M2DmA.22NXs.py] File in: 2019-09-05 17:27:57

--- pyton/22pDzrFb9r0E22hwWYMhbJCkw22bv1o2M2DmA.22NXs.py
+++ pyton/22pDzrFb9r0E22hwWYMhbJCkw22bv1o2M2DmA.22NXs.py
@@ -1,73 +1,122 @@
 
         
-            Requests that produced this error are safe to retry.
+            # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
+    
+    print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+    
+    
+def create_pairs(x, digit_indices):
+    '''Positive and negative pair creation.
+    Alternates between positive and negative pairs.
     '''
+    pairs = []
+    labels = []
+    n = min([len(digit_indices[d]) for d in range(num_classes)]) - 1
+    for d in range(num_classes):
+        for i in range(n):
+            z1, z2 = digit_indices[d][i], digit_indices[d][i + 1]
+            pairs += [[x[z1], x[z2]]]
+            inc = random.randrange(1, num_classes)
+            dn = (d + inc) % num_classes
+            z1, z2 = digit_indices[d][i], digit_indices[dn][i]
+            pairs += [[x[z1], x[z2]]]
+            labels += [1, 0]
+    return np.array(pairs), np.array(labels)
     
-        @classmethod
-    def basic_response_server(cls, **kwargs):
-        return cls.text_response_server(
-            'HTTP/1.1 200 OK\r\n' +
-            'Content-Length: 0\r\n\r\n',
-            **kwargs
-        )
+    from keras.datasets import boston_housing
+from keras.datasets import imdb
+from keras.datasets import reuters
     
-        :param cookie_dict: Dict of key/values to insert into CookieJar.
-    :param cookiejar: (optional) A cookiejar to add the cookies to.
-    :param overwrite: (optional) If False, will not replace cookies
-        already in the jar with new ones.
-    :rtype: CookieJar
+    
+@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
+def test_orthogonal(tensor_shape):
+    _runner(initializers.orthogonal(), tensor_shape,
+            target_mean=0.)
+    
+        def build(self, input_shape):
+        input_dim_capsule = input_shape[-1]
+        if self.share_weights:
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(1, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='glorot_uniform',
+                trainable=True)
+        else:
+            input_num_capsule = input_shape[-2]
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(input_num_capsule, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='glorot_uniform',
+                trainable=True)
+    
+        metric_fn = BinaryTruePositives()
+    config = metrics.serialize(metric_fn)
+    metric_fn = metrics.deserialize(
+        config, custom_objects={'BinaryTruePositives': BinaryTruePositives})
+    
+        # Arguments
+        count_mode: One of 'steps' or 'samples'.
+            Whether the progress bar should
+            count samples seen or steps (batches) seen.
+        stateful_metrics: Iterable of string names of metrics that
+            should *not* be averaged over an epoch.
+            Metrics in this list will be logged as-is.
+            All others will be averaged over time (e.g. loss, etc).
+    
+            self.kernel_constraint = constraints.get(kernel_constraint)
+        self.recurrent_constraint = constraints.get(recurrent_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+    
+        :param str u_string: unicode string to check. Must be unicode
+        and not Python 2 `str`.
+    :rtype: bool
     '''
-    if cookiejar is None:
-        cookiejar = RequestsCookieJar()
-    
-    # Output file base name for HTML help builder.
-htmlhelp_basename = 'Requestsdoc'
-    
-        def redirect_resp_handler(sock):
-        consume_socket_content(sock, timeout=0.5)
-        location = u'//{}:{}/{}'.format(host, port, path)
-        sock.send(
-            b'HTTP/1.1 301 Moved Permanently\r\n'
-            b'Content-Length: 0\r\n'
-            b'Location: ' + location.encode('utf8') + b'\r\n'
-            b'\r\n'
-        )
-        redirect_request.append(consume_socket_content(sock, timeout=0.5))
-        sock.send(b'HTTP/1.1 200 OK\r\n\r\n')
-    
-    
-def _check_cryptography(cryptography_version):
-    # cryptography < 1.3.4
+    assert isinstance(u_string, str)
     try:
-        cryptography_version = list(map(int, cryptography_version.split('.')))
-    except ValueError:
-        return
-    
-    
-def head(url, **kwargs):
-    r'''Sends a HEAD request.
-    
-        def test_delivery_stream_tags(self):
-        result = firehose_api.get_delivery_stream_tags(TEST_STREAM_NAME)
-        self.assertEquals(TEST_TAGS, result['Tags'])
-        result = firehose_api.get_delivery_stream_tags(TEST_STREAM_NAME, exclusive_start_tag_key='MyTag')
-        self.assertEquals([TEST_TAG_2], result['Tags'])
-        result = firehose_api.get_delivery_stream_tags(TEST_STREAM_NAME, limit=1)
-        self.assertEquals([TEST_TAG_1], result['Tags'])
-        self.assertEquals(True, result['HasMore'])
+        u_string.encode('ascii')
+        return True
+    except UnicodeEncodeError:
+        return False
 
     
-        def _list_stack_names(self):
-        client = aws_stack.connect_to_service('cloudformation')
-        stack_names = [s['StackName'] for s in client.list_stacks()['StackSummaries']]
-        return stack_names
+        The code in `cookielib.CookieJar` expects this interface in order to correctly
+    manage cookie policies, i.e., determine whether a cookie can be set, given the
+    domains of the request and the cookie.
     
-        def test_attribute_raw_subscribe(self):
-        # create SNS topic and connect it to an SQS queue
-        queue_arn = aws_stack.sqs_queue_arn(TEST_QUEUE_NAME)
-        self.sns_client.subscribe(
-            TopicArn=self.topic_arn,
-            Protocol='sqs',
-            Endpoint=queue_arn,
-            Attributes={'RawMessageDelivery': 'true'}
-        )
+    # If true, keep warnings as 'system message' paragraphs in the built documents.
+# keep_warnings = False
+    
+    '''
+requests.auth
+~~~~~~~~~~~~~
+    
+        with server as (host, port):
+        url = u'http://{}:{}'.format(host, port)
+        r = requests.get(url=url, allow_redirects=True)
+        assert r.status_code == 200
+        assert len(r.history) == 1
+        assert r.history[0].status_code == 301
+        assert redirect_request[0].startswith(b'GET /' + expected_path + b' HTTP/1.1')
+        assert r.url == u'{}/{}'.format(url, expected_path.decode('ascii'))
+    
+    '''
+requests.compat
+~~~~~~~~~~~~~~~
+    
+        tf.logging.info('*** Features ***')
+    for name in sorted(features.keys()):
+      tf.logging.info('  name = %s, shape = %s' % (name, features[name].shape))
+    
+      return example
