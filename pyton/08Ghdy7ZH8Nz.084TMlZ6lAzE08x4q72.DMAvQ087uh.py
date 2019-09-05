d52a6fde79fe8e5278f4ffcd32231994564359f9
SUNsung <weatherius@gmail.com>
2019-09-05 12:54:40 +0300

    Rewrite [08Ghdy7ZH8Nz.084TMlZ6lAzE08x4q72.DMAvQ087uh.py] File in: 2019-09-05 12:54:39

--- pyton/08Ghdy7ZH8Nz.084TMlZ6lAzE08x4q72.DMAvQ087uh.py
+++ pyton/08Ghdy7ZH8Nz.084TMlZ6lAzE08x4q72.DMAvQ087uh.py
@@ -1,178 +1,80 @@
 
         
-        
-class User(object):
-    
-        def current_year_month(self):
-        '''Return the current year and month.'''
-        ...
-    
-        def mapper_sort(self, key, value):
-        '''Construct key to ensure proper sorting.
-    
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
-    
-        def process_query(self, query):
-        query = self.parse_query(query)
-        results = self.memory_cache.get(query)
-        if results is None:
-            results = self.reverse_index_cluster.process_search(query)
-            self.memory_cache.set(query, results)
-        return results
-    
-            if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif (
-            db.execute('SELECT id FROM user WHERE username = ?', (username,)).fetchone()
-            is not None
-        ):
-            error = 'User {0} is already registered.'.format(username)
-    
-    
-def test_init_db_command(runner, monkeypatch):
-    class Recorder(object):
-        called = False
-    
-    from flask import has_request_context
-from flask import url_for
-    
-    # Read lines from the linkcheck output file
-try:
-    with open('build/linkcheck/output.txt') as out:
-        output_lines = out.readlines()
-except IOError:
-    print('linkcheck output not found; please run linkcheck first.')
-    exit(1)
-    
-        def process_options(self, args, opts):
-        ScrapyCommand.process_options(self, args, opts)
-        try:
-            opts.spargs = arglist_to_dict(opts.spargs)
-        except ValueError:
-            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
-        if opts.output:
-            if opts.output == '-':
-                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
-            else:
-                self.settings.set('FEED_URI', opts.output, priority='cmdline')
-            feed_exporters = without_none_values(
-                self.settings.getwithbase('FEED_EXPORTERS'))
-            valid_output_formats = feed_exporters.keys()
-            if not opts.output_format:
-                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
-            if opts.output_format not in valid_output_formats:
-                raise UsageError('Unrecognized output format '%s', set one'
-                                 ' using the '-t' switch or as a file extension'
-                                 ' from the supported list %s' % (opts.output_format,
-                                                                  tuple(valid_output_formats)))
-            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
-    
-        def long_desc(self):
-        return ('Edit a spider using the editor defined in the EDITOR environment'
-                ' variable or else the EDITOR setting')
-    
-        def _find_template(self, template):
-        template_file = join(self.templates_dir, '%s.tmpl' % template)
-        if exists(template_file):
-            return template_file
-        print('Unable to find template: %s\n' % template)
-        print('Use 'scrapy genspider --list' to see all available templates.')
-    
-    
-class Command(ScrapyCommand):
-    
-        DEFAULT_CIPHERS = AcceptableCiphers.fromOpenSSLCipherString('DEFAULT')
+            entry_points = {'console_scripts': proj_info['console_scripts']}
+)
 
     
-        def report_out_of_quota(self, appid):
-        self.logger.warn('report_out_of_quota:%s', appid)
-        with self.lock:
-            if appid not in self.out_of_quota_appids:
-                self.out_of_quota_appids.append(appid)
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+    
+        #if dictify(e)['ckplayer']['info'][0]['title'][0]['_text'].strip():  #duration
+        #video_dict['title'] = dictify(e)['ckplayer']['info'][0]['title'][0]['_text'].strip()
+    
+    site_info = 'CNTV.com'
+download = cntv_download
+download_playlist = playlist_not_supported('cntv')
+
+    
+            for i in range(len(titles)):
+            title = titles[i]
+            datas = {
+                'sid': song_id[i],
+                'ssid': song_ssid[i]
+            }
+            post_params = urllib.parse.urlencode(datas).encode('utf-8')
             try:
-                self.working_appid_list.remove(appid)
+                resp = urllib.request.urlopen(get_song_url, post_params)
+                resp_data = json.loads(resp.read().decode('utf-8'))
+                real_url = resp_data['r']
+                type, ext, size = url_info(real_url)
+                print_info(site_info, title, type, size)
             except:
                 pass
     
-        @staticmethod
-    def import_linux_firefox_ca(common_name, ca_file):
-        xlog.debug('Begin importing CA to Firefox')
-        firefox_config_path = CertUtil.get_linux_firefox_path()
-        if not firefox_config_path:
-            #xlog.debug('Not found Firefox path')
-            return False
+    site_info = 'Giphy.com'
+download = giphy_download
+download_playlist = playlist_not_supported('giphy')
+
+    
+    def ifeng_download_by_id(id, title = None, output_dir = '.', merge = True, info_only = False):
+    assert r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', id), id
+    url = 'http://vxml.ifengimg.com/video_info_new/%s/%s/%s.xml' % (id[-2], id[-2:], id)
+    xml = get_html(url, 'utf-8')
+    title = r1(r'Name='([^']+)'', xml)
+    title = unescape_html(title)
+    url = r1(r'VideoPlayUrl='([^']+)'', xml)
+    from random import randint
+    r = randint(10, 19)
+    url = url.replace('http://wideo.ifeng.com/', 'http://ips.ifeng.com/wideo.ifeng.com/')
+    type, ext, size = url_info(url)
+    
+    '''
+http://code.activestate.com/recipes/131499-observer-pattern/
+    
+            if not meth:
+            meth = self.generic_visit
+        return meth(node, *args, **kwargs)
+    
+    ### OUTPUT ###
+# rm1: Running
+# rm2: Running
+# rm1: Zombie
+# rm2: Zombie
+# rm1 id: 140732837899224
+# rm2 id: 140732837899296
+# rm1: Init
+# rm2: Init
+# rm3: Init
+
     
     
-    def rewind(self, marker=None):
-        '''
-        Reset the stream so that next call to index would return marker.
-        The marker will usually be index() but it doesn't have to be.  It's
-        just a marker to indicate what state the stream was in.  This is
-        essentially calling release() and seek().  If there are markers
-        created after this marker argument, this routine must unroll them
-        like a stack.  Assume the state the stream was in when this marker
-        was created.
+if __name__ == '__main__':
+    main()
     
-        def test_untag_resource(self):
-        response = testutil.send_dynamodb_request('', action='UntagResource', request_body=json.dumps({
-            'ResourceArn': testutil.get_sample_arn('dynamodb', 'table'),
-            'TagKeys': ['tagkey1', 'tagkey2']  # Keys to untag
-        }))
-        assert response.status_code == 200
-        assert not response._content  # Empty string if untagging succeeded (mocked for now)
-    
-    
-def get_kcl_dir():
-    return get_dir_of_file(kcl.__file__)
-    
-        # create ES domain
-    es_client.create_elasticsearch_domain(DomainName=TEST_DOMAIN_NAME)
-    assert_true(TEST_DOMAIN_NAME in
-        [d['DomainName'] for d in es_client.list_domain_names()['DomainNames']])
-    
-        cmd = 'list-event-source-mappings'
-    if func_name:
-        cmd = '%s --function-name %s' % (cmd, func_name)
-    out = cmd_lambda(cmd, env=env)
-    out = json.loads(out)
-    result = out['EventSourceMappings']
-    return result
-    
-    
-def get_rest_api_paths(rest_api_id):
-    apigateway = aws_stack.connect_to_service(service_name='apigateway')
-    resources = apigateway.get_resources(restApiId=rest_api_id, limit=100)
-    resource_map = {}
-    for resource in resources['items']:
-        path = aws_stack.get_apigateway_path_for_resource(rest_api_id, resource['id'])
-        resource_map[path] = resource
-    return resource_map
-    
-    
-class BigfilePiecefieldPacked(object):
-    __slots__ = ['data']
-    
-            # Load all include files data into a merged set
-        for include_path in self.file_content['includes']:
-            address, inner_path = include_path.split('/', 1)
-            try:
-                content = self.site_manager.get(address).storage.loadJson(inner_path)
-            except Exception as err:
-                self.log.warning(
-                    'Error loading include %s: %s' %
-                    (include_path, Debug.formatException(err))
-                )
-                continue
-    
-    
-def open_database(database, mode=MODE_AUTO):
-    '''Open a Maxmind DB database
-    
-        int_from_byte = lambda x: x
+    *What does this example do?
+This example shows a way to add formatting options (boldface and
+italic) to a text by appending the corresponding tags (<b> and
+<i>). Also, we can see that decorators can be applied one after the other,
+since the original text is passed to the bold wrapper, which in turn
+is passed to the italic wrapper.
