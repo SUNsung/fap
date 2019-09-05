529a133f5563f6b11ce04f75a81902e071620fee
SUNsung <weatherius@gmail.com>
2019-09-05 05:46:18 +0300

    Rewrite [266ZzVGtn4Q8s26sCIvAA18xvo26L0DvfHVzVbQ26hNn.rb] File in: 2019-09-05 05:46:18

--- ruby/266ZzVGtn4Q8s26sCIvAA18xvo26L0DvfHVzVbQ26hNn.rb
+++ ruby/266ZzVGtn4Q8s26sCIvAA18xvo26L0DvfHVzVbQ26hNn.rb
@@ -1,177 +1,154 @@
 
         
-        version = ['', 'ext/etc/'].find do |dir|
-  begin
-    break File.open(File.expand_path('../#{dir}/etc.c', __FILE__)) do |f|
-      f.gets '\n#define RUBY_ETC_VERSION '
-      f.gets[/'(.+)'/, 1]
-    end
-  rescue
-    next
+          def test_inspect_instance
+    topic = topics(:first)
+    assert_equal %(#<Topic id: 1, title: 'The First Topic', author_name: 'David', author_email_address: 'david@loudthinking.com', written_on: '#{topic.written_on.to_s(:db)}', bonus_time: '#{topic.bonus_time.to_s(:db)}', last_read: '#{topic.last_read.to_s(:db)}', content: 'Have a nice day', important: nil, approved: false, replies_count: 1, unique_replies_count: 0, parent_id: nil, parent_title: nil, type: nil, group: nil, created_at: '#{topic.created_at.to_s(:db)}', updated_at: '#{topic.updated_at.to_s(:db)}'>), topic.inspect
   end
-end
     
-          def test_on_event_hash_return4
-        ev = WIN32OLE_EVENT.new(@db)
-        ev.on_event('WillConnect'){|*args|
-          {'return' => 1, 'ConnectionString' => CONNSTR}
-        }
-        @db.connectionString = 'XXX'
-        @db.open
-        assert(true)
+            assert_equal <<~MESSAGE, run_rake_routes
+                                         Prefix Verb   URI Pattern                                                                              Controller#Action
+                                           cart GET    /cart(.:format)                                                                          cart#show
+                  rails_mandrill_inbound_emails POST   /rails/action_mailbox/mandrill/inbound_emails(.:format)                                  action_mailbox/ingresses/mandrill/inbound_emails#create
+                  rails_postmark_inbound_emails POST   /rails/action_mailbox/postmark/inbound_emails(.:format)                                  action_mailbox/ingresses/postmark/inbound_emails#create
+                     rails_relay_inbound_emails POST   /rails/action_mailbox/relay/inbound_emails(.:format)                                     action_mailbox/ingresses/relay/inbound_emails#create
+                  rails_sendgrid_inbound_emails POST   /rails/action_mailbox/sendgrid/inbound_emails(.:format)                                  action_mailbox/ingresses/sendgrid/inbound_emails#create
+                   rails_mailgun_inbound_emails POST   /rails/action_mailbox/mailgun/inbound_emails/mime(.:format)                              action_mailbox/ingresses/mailgun/inbound_emails#create
+                 rails_conductor_inbound_emails GET    /rails/conductor/action_mailbox/inbound_emails(.:format)                                 rails/conductor/action_mailbox/inbound_emails#index
+                                                POST   /rails/conductor/action_mailbox/inbound_emails(.:format)                                 rails/conductor/action_mailbox/inbound_emails#create
+              new_rails_conductor_inbound_email GET    /rails/conductor/action_mailbox/inbound_emails/new(.:format)                             rails/conductor/action_mailbox/inbound_emails#new
+             edit_rails_conductor_inbound_email GET    /rails/conductor/action_mailbox/inbound_emails/:id/edit(.:format)                        rails/conductor/action_mailbox/inbound_emails#edit
+                  rails_conductor_inbound_email GET    /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#show
+                                                PATCH  /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#update
+                                                PUT    /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#update
+                                                DELETE /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#destroy
+          rails_conductor_inbound_email_reroute POST   /rails/conductor/action_mailbox/:inbound_email_id/reroute(.:format)                      rails/conductor/action_mailbox/reroutes#create
+                             rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+                      rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+                             rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+                      update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+                           rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+        MESSAGE
       end
     
-        ScratchPad.record []
-    while true
-      begin
-        ScratchPad << enum.next
-      rescue StopIteration
-        break
-      end
-    end
-    
-      it 'returns true when at EOF' do
-    gz = Zlib::GzipReader.new @io
-    gz.eof?.should be_false
-    gz.read
-    gz.eof?.should be_true
-  end
-    
-        describe 'with an integer' do
-      it 'prepends the byte to the stream' do
-        @gz.ungetbyte 0x21
-        @gz.read.should == '!12345abcde'
-      end
-    
-          respond_with do |format|
-        format.html { redirect_to admin_pods_path }
-        format.json { render json: PodPresenter.new(pod).as_json }
-      end
-    end
-  end
-end
-
-    
-      def show
-    if @aspect = current_user.aspects.where(:id => params[:id]).first
-      redirect_to aspects_path('a_ids[]' => @aspect.id)
-    else
-      redirect_to aspects_path
-    end
-  end
-    
-      respond_to :html, :mobile, :json
-    
-    World(RemoteCommandHelpers)
-
-    
-        [stdout, stderr, status]
-  end
-    
-          # rubocop:disable Style/GuardClause
-      def register_legacy_scm_hooks
-        if Rake::Task.task_defined?('deploy:new_release_path')
-          after 'deploy:new_release_path', '#{scm_name}:create_release'
-        end
-    
-    module Capistrano
-  class Configuration
-    class Servers
-      include Enumerable
-    
-          # Given a callable that provides a value, wrap the callable with another
-      # object that responds to `call`. This new object will perform validation
-      # and then return the original callable's value.
+          # Pure data class which describes an umbrella target.
       #
-      # If the callable is a `Question`, the object returned by this method will
-      # also be a `Question` (a `ValidatedQuestion`, to be precise). This
-      # ensures that `is_a?(Question)` remains true even after the validation
-      # wrapper is applied. This is needed so that `Configuration#is_question?`
-      # works as expected.
-      #
-      def assert_valid_later(key, callable)
-        validation_callback = lambda do
-          value = callable.call
-          assert_valid_now(key, value)
-          value
-        end
+      class UmbrellaTargetDescription
+        # @return [Xcodeproj::Project] The user project into which this target
+        #         is integrated.
+        #
+        attr_reader :user_project
     
-    set_if_empty :local_user, -> { ENV['USER'] || ENV['LOGNAME'] || ENV['USERNAME'] }
-
-    
-          it 'does not create the ApiSecret' do
-        expect { post '/users/api_secrets', params: { api_secret: invalid_params } }.
-          not_to(change { user.api_secrets.count })
-      end
-    
-      describe 'DELETE blocks' do
-    it 'updates block from input data' do
-      block = create(:block, user_id: user.id, input_css: '.blue { color: blue;}')
-      delete '/blocks/#{block.id}'
-      expect(Block.all.size).to eq(0)
-    end
-  end
-end
-
-    
-      it 'renders a podcast index if there is a podcast with the slug successfully' do
-    expect(get: '/#{podcast.slug}').to route_to(
-      controller: 'stories',
-      action: 'index',
-      username: podcast.slug,
-    )
-  end
-    
-      def permitted_attributes
-    %i[title body_html body_markdown main_image published canonical_url
-       description allow_small_edits allow_big_edits tag_list publish_under_org
-       video video_code video_source_url video_thumbnail_url receive_notifications
-       archived]
-  end
-    
-        def tmux_pre_command
-      _send_target(tab.pre.shellescape) if tab.pre
+        it 'prints the correct output to the console' do
+      io = ''
+      UI.indentation_level = 1
+      config.verbose = true
+      Executable::Indenter.any_instance.stubs(:io).returns(io)
+      cmd = ['-e', <<-RB]
+        3.times { |i| puts i }
+      RB
+      Executable.execute_command('ruby', cmd, true)
+      io.should == ' 0\n 1\n 2\n'
     end
     
-    describe Tmuxinator::Pane do
-  let(:klass) { described_class }
-  let(:instance) { klass.new(index, project, window, *commands) }
-  # let(:index) { 'vim' }
-  # let(:project) { 0 }
-  # let(:tab) { nil }
-  # let(:commands) { nil }
-  let(:index) { 0 }
-  let(:project) { double }
-  let(:window) { double }
-  let(:commands) { ['vim', 'bash'] }
+        it 'writes the module map to the disk for a library' do
+      path = temporary_directory + 'BananaLib.modulemap'
+      @pod_target.stubs(:build_type => Target::BuildType.static_library)
+      @gen.save_as(path)
+      path.read.should == <<-EOS.strip_heredoc
+        module BananaLib {
+          umbrella header 'BananaLib-umbrella.h'
+    }
     
-        context 'with deprecations' do
-      context 'rbenv option is present' do
-        before do
-          allow(project).to receive_messages(rbenv?: true)
-          allow(project).to \
-            receive_message_chain(:yaml, :[]).and_return('2.0.0-p247')
+            it 'does not use testspecs for equality' do
+          k0 = PodVariant.new(@specs, @testspecs, [], @platform, false)
+          k1 = PodVariant.new(@specs, [], [], @platform, false)
+          k0.should == k1
         end
     
-        msg = 'Actual pane does not match expected'
-    msg << '\n  Expected #{@commands} but has #{actual.commands}' if @commands
-    msg << '\n  Expected pane to have #{@expected_attrs}' if @expected_attrs
-  end
+                  FileUtils.expects(:ln_sf).with(relative_path, target_module_path)
+              native_target = mock(:build_configurations => [])
+              @installer.send(:create_module_map, native_target)
+            end
+          end
     
-            it 'creates a new tmuxinator project file' do
-          capture_io { cli.start }
-          expect(file.string).to_not be_empty
+              BuildType.infer_from_spec(stub('spec', :root => stub('root_spec', :static_framework => true)), :host_requires_frameworks => false).
+            should == BuildType.static_library
+          BuildType.infer_from_spec(stub('spec', :root => stub('root_spec', :static_framework => false)), :host_requires_frameworks => false).
+            should == BuildType.static_library
+          BuildType.infer_from_spec(stub('spec', :root => stub('root_spec', :static_framework => true)), :host_requires_frameworks => true).
+            should == BuildType.static_framework
+          BuildType.infer_from_spec(stub('spec', :root => stub('root_spec', :static_framework => false)), :host_requires_frameworks => true).
+            should == BuildType.dynamic_framework
         end
       end
     
-          def version
-        if Tmuxinator::Doctor.installed?
-          tmux_version = `tmux -V`.split(' ')[1]
+          alias eql? ==
     
-          it 'is empty if no configuration directories exist' do
-        allow(File).to receive(:directory?).and_return false
-        expect(described_class.directories).to eq []
-      end
+    require 'clamp'
+require 'pluginmanager/util'
+require 'pluginmanager/gemfile'
+require 'pluginmanager/install'
+require 'pluginmanager/remove'
+require 'pluginmanager/list'
+require 'pluginmanager/update'
+require 'pluginmanager/pack'
+require 'pluginmanager/unpack'
+require 'pluginmanager/generate'
+require 'pluginmanager/prepare_offline_pack'
+require 'pluginmanager/proxy_support'
+configure_proxy
     
-          it { is_expected.to be false }
+          options = {:debug => ENV['LS_QA_DEBUG']}
+      puts 'Destroying #{machines}'
+      LogStash::VagrantHelpers.destroy(machines, options)
+      puts 'Bootstrapping #{machines}'
+      LogStash::VagrantHelpers.bootstrap(machines, options)
     end
+    
+              it 'fails when installing a non logstash plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install  bundler')
+            expect(command).not_to install_successfully
+          end
+    
+        def default_attributes(&block)
+      return if @options.nil?
+      @options.each do |flag, param, help, options, _block|
+        attr = flag.first.gsub(/^-+/, '').gsub(/-/, '_').gsub('[no_]', '')
+        attr += '?' if param == :flag
+        yield attr.to_sym, options[:default]
+      end
+    end # def default_attributes
+    
+              # directories have a magic string inserted into their name
+          full_record_path = extension_header[TAR_NAME_OFFSET_START..TAR_NAME_OFFSET_END].delete('\0')
+          full_record_path = add_paxstring(full_record_path)
+    
+      option '--php-bin', 'PHP_BIN',
+    'Specify php executable path if differs from the os used for packaging'
+    
+      option '--name', 'SERVICE_NAME', 'The name of the service you are creating'
+  option '--chdir', 'CHDIR', 'The working directory used by the service'
+    
+      def build!(params)
+    # TODO(sissel): Support these somehow, perhaps with execs and files.
+    self.scripts.each do |name, path|
+      case name
+        when 'pre-install'
+        when 'post-install'
+        when 'pre-uninstall'
+        when 'post-uninstall'
+      end # case name
+    end # self.scripts.each
+    
+    # Support for self extracting sh files (.sh files)
+#
+# This class only supports output of packages.
+#
+# The sh package is a single sh file with a tar payload concatenated to the end.
+# The script can unpack the tarball to install it and call optional post install scripts.
+class FPM::Package::Sh < FPM::Package
+    
+          # TODO(sissel): preinstall/postinstall
+      # strip @prefix, since BASEDIR will set prefix via the pkginfo file
+      IO.popen('pkgproto #{staging_path}/#{@prefix}=').each_line do |line|
+        type, klass, path, mode, user, group = line.split
