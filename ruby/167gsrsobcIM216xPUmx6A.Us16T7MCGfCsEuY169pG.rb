88281445c6428fe3a6e4907490493db4688faeb0
SUNsung <weatherius@gmail.com>
2019-09-05 19:49:18 +0300

    Rewrite [167gsrsobcIM216xPUmx6A.Us16T7MCGfCsEuY169pG.rb] File in: 2019-09-05 19:49:17

--- ruby/167gsrsobcIM216xPUmx6A.Us16T7MCGfCsEuY169pG.rb
+++ ruby/167gsrsobcIM216xPUmx6A.Us16T7MCGfCsEuY169pG.rb
@@ -1,289 +1,126 @@
 
         
-            it 'should return the right path' do
-      optimized = Fabricate.build(:optimized_image, upload: upload, version: 1)
-      expect(FileStore::BaseStore.new.get_path_for_optimized_image(optimized)).to eq(optimized_path)
+            def subpath
+      @subpath ||= subpath_to(current_url)
     end
     
-            # strip out users in groups you already belong to
-        target_users = target_users
-          .joins('LEFT JOIN group_users gu ON gu.user_id = topic_allowed_users.user_id AND gu.group_id IN (#{sanitize_sql_array(my_group_ids)})')
-          .where('gu.group_id IS NULL')
-      end
-    
-        def free_bytes
-      nil
+        def effective_path
+      @effective_path ||= effective_url.path
     end
   end
 end
 
     
-          before do
-        SiteSetting.png_to_jpg_quality = 1
-      end
-    
-          app = find_app(apple_id: config[:apple_id], app_identifier: config[:app_identifier])
-    
-          def self.get(app_id: nil, includes: nil)
-        return Spaceship::ConnectAPI.get_app(app_id: app_id, includes: includes).first
-      end
-    
-          def self.type
-        return 'betaTesters'
-      end
-    
-              super(base_dir)
-        end
-    
-        def aes256_gcm_decrypt(value)
-      return unless value
-    
-          expect(diffable.project.repository)
-        .to receive(:diff_stats)
-        .with(diffable.diff_refs.base_sha, diffable.diff_refs.head_sha)
-        .and_call_original
-    
-        def origin
-      if scheme && host
-        origin = '#{scheme}://#{host}'
-        origin.downcase!
-        origin << ':#{port}' if port
-        origin
-      else
-        nil
-      end
+        def build_page(path)
+      response = request_one url_for(path)
+      result = handle_response(response)
+      yield result if block_given?
+      result
     end
     
-            css('pre code').each do |node|
+            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
           node.before(node.children).remove
         end
     
-          REPLACE_TYPES = {
-        'C++ Addons'                => 'Miscellaneous',
-        'C/C++ Addons'              => 'Miscellaneous',
-        'Debugger'                  => 'Miscellaneous',
-        'Deprecated APIs'           => 'Miscellaneous',
-        'Tracing'                   => 'Miscellaneous',
-        'os'                        => 'OS',
-        'StringDecoder'             => 'String Decoder',
-        'TLS (SSL)'                 => 'TLS/SSL',
-        'UDP / Datagram Sockets'    => 'UDP/Datagram',
-        'VM (Executing JavaScript)' => 'VM',
-        'Executing JavaScript'      => 'VM' }
+        it 'creates a muting relation' do
+      expect(user.account.muting?(other_account)).to be true
+    end
     
-            css('.class > dt[id]', '.exception > dt[id]', '.attribute > dt[id]').each do |node|
-          entries << [node['id'], node['id']]
+      describe 'follow_request' do
+    let(:follow_request) { Fabricate(:follow_request, account: sender, target_account: receiver.account) }
+    let(:mail) { NotificationMailer.follow_request(receiver.account, Notification.create!(account: receiver.account, activity: follow_request)) }
+    
+      describe 'GET #show' do
+    let(:poll) { Fabricate(:poll, status: Fabricate(:status, visibility: visibility)) }
+    
+      # The default wrapper to be used by the FormBuilder.
+  config.default_wrapper = :default
+    
+          def remove_page_extentions(page_path)
+        Gollum::Markup.formats.values.each do |format|
+          page_path = page_path.gsub(/\.#{format[:regexp]}$/, '')
         end
-    
-      #
-  # Return the content length header
-  #
-  def set_content_len_header(clen)
-    if opts['method'] == 'GET' && (clen == 0 || opts['chunked_size'] > 0)
-      # This condition only applies to GET because of the specs.
-      # RFC-7230:
-      # A Content-Length header field is normally sent in a POST
-      # request even when the value is 0 (indicating an empty payload body)
-      return ''
-    elsif opts['headers'] && opts['headers']['Content-Length']
-      # If the module has a modified content-length header, respect that by
-      # not setting another one.
-      return ''
-    end
-    set_formatted_header('Content-Length', clen)
-  end
-    
-            # Decodes a Kerberos response
-        #
-        # @param data [String] the raw response message
-        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] the kerberos message response
-        # @raise [RuntimeError] if the response can't be processed
-        def decode_kerb_response(data)
-          asn1 = OpenSSL::ASN1.decode(data)
-          msg_type = asn1.value[0].value[1].value[0].value
-    
-      find_files = ->(path) {
-    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
-      path if File.file?(path)
-    end.compact
-  }
-    
-        def str_to_byte_pos(pos)
-      @s.string.slice(0, pos).bytesize
-    end
-  end
-end
-    
-      # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
-    
-          respond_with do |format|
-        format.html { redirect_to admin_pods_path }
-        format.json { render json: PodPresenter.new(pod).as_json }
-      end
-    end
-  end
-end
-
-    
-          def save_params_and_render_consent_form(endpoint)
-        @o_auth_application = endpoint.o_auth_application
-        @response_type = endpoint.response_type
-        @redirect_uri = endpoint.redirect_uri
-        @scopes = endpoint.scopes
-        save_request_parameters
-        @app = UserApplicationPresenter.new @o_auth_application, @scopes
-        render :new
+        return page_path
       end
     
-    module Api
-  module OpenidConnect
-    class ClientsController < ApplicationController
-      skip_before_action :verify_authenticity_token
+      test 'transliteration' do
+    # we transliterate only when adapter is grit
+    return if defined?(Gollum::GIT_ADAPTER) && Gollum::GIT_ADAPTER != 'grit'
     
-        # set the flash message
-    respond_to do |format|
-      format.json do
-        if success
-          render json: AspectMembershipPresenter.new(membership).base_hash
-        else
-          render plain: membership.errors.full_messages, status: 403
-        end
-      end
+    # Run the frontend, based on Sinatra
+#
+# There are a number of wiki options that can be set for the frontend
+#
+# Example
+# require 'gollum/app'
+# Precious::App.set(:wiki_options, {
+#     :universal_toc => false,
+# }
+#
+# See the wiki.rb file for more details on wiki options
+module Precious
+  class App < Sinatra::Base
+    register Mustache::Sinatra
+    include Precious::Helpers
+    
+    is_travis = ENV['TRAVIS_OS_NAME'] && !ENV['TRAVIS_OS_NAME'].empty?
+    
+        it 'should default to native' do
+      expected = %x{uname -m}.chomp
+      insist { subject.instance_eval { @architecture } } == 'native'
+      insist { subject.architecture } == expected
     end
   end
     
-        opts = params.require(:conversation).permit(:subject)
-    opts[:participant_ids] = person_ids
-    opts[:message] = { text: params[:conversation][:text] }
-    @conversation = current_user.build_conversation(opts)
+      describe '#exclude (internal method)' do
+    it 'should obey attributes[:excludes]' do
+      File.write(subject.staging_path('hello'), 'hello')
+      File.write(subject.staging_path('world'), 'world')
+      subject.attributes[:excludes] = ['*world*']
+      subject.instance_eval { exclude }
+      insist { subject.files } == ['hello']
+    end
     
-      def toggle_mobile
-    session[:mobile_view] = session[:mobile_view].nil? ? true : !session[:mobile_view]
-    
-    CLEAN.include '**/*.rbc'
-    
-    begin
-  require 'bundler'
-  Bundler::GemHelper.install_tasks
-rescue LoadError => e
-  $stderr.puts e
-end
-    
-            else
-          false # Token is malformed
-        end
-      end
-    
-          def is_edit_page
-        false
-      end
-    
-    module Gollum
-end
-Gollum::GIT_ADAPTER = ENV['GIT_ADAPTER'] if ENV['GIT_ADAPTER']
-    
-      if cfg = options[:config]
-    # If the path begins with a '/' it will be considered an absolute path,
-    # otherwise it will be relative to the CWD
-    cfg = File.join(Dir.getwd, cfg) unless cfg.slice(0) == File::SEPARATOR
-    require cfg
+      config.vm.define 'arch' do |arch|
+    arch.vm.box = 'jfredett/arch-puppet'
   end
     
-      config.fail_fast = ENV['FAIL_FAST'] || false
-  config.order = 'random'
-end
-
+      option '--config-files', 'CONFIG_FILES',
+    'Mark a file in the package as being a config file. This uses 'conffiles'' \
+    ' in debs and %config in rpm. If you have multiple files to mark as ' \
+    'configuration files, specify this flag multiple times.  If argument is ' \
+    'directory all files inside it will be recursively marked as config files.',
+    :multivalued => true, :attribute_name => :config_files
+  option '--directories', 'DIRECTORIES', 'Recursively mark a directory as being owned ' \
+    'by the package. Use this flag multiple times if you have multiple directories ' \
+    'and they are not under the same parent directory ', :multivalued => true,
+    :attribute_name => :directories
+  option ['-a', '--architecture'], 'ARCHITECTURE',
+    'The architecture name. Usually matches 'uname -m'. For automatic values,' \
+    ' you can use '-a all' or '-a native'. These two strings will be ' \
+    'translated into the correct value for your platform and target package type.'
+  option ['-m', '--maintainer'], 'MAINTAINER',
+    'The maintainer of this package.',
+    :default => '<#{ENV['USER']}@#{Socket.gethostname}>'
+  option ['-S', '--package-name-suffix'], 'PACKAGE_NAME_SUFFIX',
+    'a name suffix to append to package and dependencies.'
+  option ['-e', '--edit'], :flag,
+    'Edit the package spec before building.', :default => false
     
-            def scope
-          if params[:product_id]
-            Spree::Product.friendly.find(params[:product_id])
-          elsif params[:variant_id]
-            Spree::Variant.find(params[:variant_id])
-          end
-        end
-      end
-    end
-  end
-end
-
+        # Get a list of all known package subclasses
+    def types
+      return @subclasses
+    end # def self.types
     
-            def find_order
-          @order = Spree::Order.find_by!(number: order_id)
-          authorize! :show, @order, order_token
-        end
+        # Publish the package.
+    repo_path = build_path('#{name}_repo')
+    safesystem('pkgrepo', 'create', repo_path)
+    safesystem('pkgrepo', 'set', '-s', repo_path, 'publisher/prefix=#{attributes[:p5p_publisher]}')
+    safesystem('pkgsend', '-s', repo_path,
+      'publish', '-d', '#{staging_path}', '#{build_path}/#{name}.p5m')
+    safesystem('pkgrecv', '-s', repo_path, '-a',
+      '-d', build_path('#{name}.p5p'), name)
     
-              @properties = if params[:ids]
-                          @properties.where(id: params[:ids].split(',').flatten)
-                        else
-                          @properties.ransack(params[:q]).result
-                        end
+        self.description = control['pkgdesc'][0]
     
-            def show
-          authorize! :admin, ReturnAuthorization
-          @return_authorization = order.return_authorizations.accessible_by(current_ability, :show).find(params[:id])
-          respond_with(@return_authorization)
-        end
     
-            def show
-          @state = scope.find(params[:id])
-          respond_with(@state)
-        end
-    
-            def stock_location_params
-          params.require(:stock_location).permit(permitted_stock_location_attributes)
-        end
-      end
-    end
-  end
-end
-
-    
-            def stock_location
-          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
-          @stock_location ||= StockLocation.accessible_by(current_ability, :show).find(params[:stock_location_id])
-        end
-    
-            def update
-          authorize! :update, @store
-          if @store.update(store_params)
-            respond_with(@store, status: 200, default_template: :show)
-          else
-            invalid_resource!(@store)
-          end
-        end
-    
-            def show
-          @variant = scope.includes(*variant_includes).find(params[:id])
-          respond_with(@variant)
-        end
-    
-        it 'allows sidekiq_options to point to different Redi' do
-      conn = MiniTest::Mock.new
-      conn.expect(:multi, [0, 1])
-      DWorker.sidekiq_options('pool' => ConnectionPool.new(size: 1) { conn })
-      DWorker.perform_async(1,2,3)
-      conn.verify
-    end
-    
-      describe 'scheduler' do
-    it 'can start and stop' do
-      f = Sidekiq::Scheduled::Poller.new
-      f.start
-      f.terminate
-    end
-    
-        it 'has no enqueued_at time for jobs enqueued in the future' do
-      job_id = ApiWorker.perform_in(100, 1, 'foo')
-      job = Sidekiq::ScheduledSet.new.find_job(job_id)
-      assert_nil job.enqueued_at
-    end
-    
-        it 'supports I18n.enforce_available_locales = true' do
-      I18n.enforce_available_locales = true
-      I18n.available_locales = [:en, :jp]
-    
-          Sidekiq::Testing.inline! do
-        assert Sidekiq::Testing.enabled?
-        assert Sidekiq::Testing.inline?
-      end
