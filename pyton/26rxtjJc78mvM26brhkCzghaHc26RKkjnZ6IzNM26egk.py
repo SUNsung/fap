0138b57c1adbb73818a839e41b7fc1703c544e29
SUNsung <weatherius@gmail.com>
2019-09-05 14:19:22 +0300

    Rewrite [26rxtjJc78mvM26brhkCzghaHc26RKkjnZ6IzNM26egk.py] File in: 2019-09-05 14:19:21

--- pyton/26rxtjJc78mvM26brhkCzghaHc26RKkjnZ6IzNM26egk.py
+++ pyton/26rxtjJc78mvM26brhkCzghaHc26RKkjnZ6IzNM26egk.py
@@ -1,234 +1,224 @@
 
         
-        from mrjob.job import MRJob
+            def test_when_etag_changed(self, shelve, fn, key):
+        shelve.update({key: {'etag': '-1', 'value': 'old-value'}})
+        assert fn() == 'test'
+        assert shelve == {key: {'etag': '0', 'value': 'test'}}
     
-            Emit key value pairs of the form:
     
-        def get_person(self, person_id):
-        person_server = self.lookup[person_id]
-        return person_server.people[person_id]
+def match(command):
+    return (command.script_parts
+            and (command.script_parts[0] in ('pacman', 'yay', 'yaourt')
+                 or command.script_parts[0:2] == ['sudo', 'pacman'])
+            and 'error: target not found:' in command.output)
     
-        def crawl(self):
-        while True:
-            page = self.data_store.extract_max_priority_page()
-            if page is None:
-                break
-            if self.data_store.crawled_similar(page.signature):
-                self.data_store.reduce_priority_link_to_crawl(page.url)
-            else:
-                self.crawl_page(page)
-            page = self.data_store.extract_max_priority_page()
+        def test_how_to_configure_when_config_not_found(self, shell,
+                                                    config_exists):
+        config_exists.return_value = False
+        assert not shell.how_to_configure().can_configure_automatically
+    
+    
+@pytest.mark.parametrize('script, pyenv_cmd, result', [
+    ('pyenv globe', 'globe', 'pyenv global'),
+    ('pyenv intall 3.8.0', 'intall', 'pyenv install 3.8.0'),
+    ('pyenv list', 'list', 'pyenv install --list'),
+    ('pyenv remove 3.8.0', 'remove', 'pyenv uninstall 3.8.0'),
+])
+def test_get_new_command(script, pyenv_cmd, output, result):
+    assert result in get_new_command(Command(script, output))
 
     
-      Args:
-    records: a record list with TensorFlow examples.
+    The most similar choice to 'lis' is:
+    list
+'''
+    
+    # Run training
+model.compile(optimizer='adam', loss='categorical_crossentropy')
+model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_split=0.2)
+# Save model
+model.save('cnn_s2s.h5')
+    
+        # build the discriminator
+    print('Discriminator model:')
+    discriminator = build_discriminator()
+    discriminator.compile(
+        optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1),
+        loss=['binary_crossentropy', 'sparse_categorical_crossentropy']
+    )
+    discriminator.summary()
+    
+    - __return_sequences__: Boolean. Whether to return the last output
+    in the output sequence, or the full sequence.
+- __return_state__: Boolean. Whether to return the last state
+    in addition to the output.
+- __go_backwards__: Boolean (default False).
+    If True, process the input sequence backwards and return the
+    reversed sequence.
+- __stateful__: Boolean (default False). If True, the last state
+    for each sample at index i in a batch will be used as initial
+    state for the sample of index i in the following batch.
+- __unroll__: Boolean (default False).
+    If True, the network will be unrolled,
+    else a symbolic loop will be used.
+    Unrolling can speed-up a RNN,
+    although it tends to be more memory-intensive.
+    Unrolling is only suitable for short sequences.
+- __input_dim__: dimensionality of the input (integer).
+    This argument (or alternatively,
+    the keyword argument `input_shape`)
+    is required when using this layer as the first layer in a model.
+- __input_length__: Length of input sequences, to be specified
+    when it is constant.
+    This argument is required if you are going to connect
+    `Flatten` then `Dense` layers upstream
+    (without it, the shape of the dense outputs cannot be computed).
+    Note that if the recurrent layer is not the first layer
+    in your model, you would need to specify the input length
+    at the level of the first layer
+    (e.g. via the `input_shape` argument)
+    
+            with np.load('data_threads.npz') as arr:
+            while True:
+                batch_index = np.random.randint(0, n_samples - batch_size)
+                start = batch_index
+                end = start + batch_size
+                X = arr['data'][start: end]
+                y = arr['labels'][start: end]
+                yield X, y
+    
+            if self.stateful:
+            updates = []
+            for i in range(len(states)):
+                updates.append((self.states[i], states[i]))
+            self.add_update(updates, inputs)
+    
+            if (
+            response_dest_account_alias and str(response_dest_account_alias) != str(request_dest_account_alias)) or (
+            response_enabled != request_enabled) or (
+            response_source and response_source != request_source) or (
+                response_dest and response_dest != request_dest) or (
+                    response_ports and response_ports != request_ports):
+            changed = True
+        return changed
+    
+    RETURN = '''
+server_ids:
+    description: The list of server ids that are changed
+    returned: success
+    type: list
+    sample:
+        [
+            'UC1TEST-SVR01',
+            'UC1TEST-SVR02'
+        ]
+'''
     
     
-  if truth_bxtxn is not None:
-    plot_time_series(truth_bxtxn, bidx, n_to_plot=n_to_plot, color='k',
-                     scale=scale)
+if __name__ == '__main__':
+    main()
+
     
-      Args:
-    d: hyperparameter dictionary
+            # Checks all keys in first dict against the second dict
+        for key in resource1:
+            if key not in resource2:
+                if resource1[key] is not None:
+                    # Inexistent key is equivalent to exist with value None
+                    self.module.log(self.MSG_DIFF_AT_KEY.format(key) + debug_resources)
+                    return False
+            # If both values are null, empty or False it will be considered equal.
+            elif not resource1[key] and not resource2[key]:
+                continue
+            elif isinstance(resource1[key], Mapping):
+                # recursive call
+                if not self.compare(resource1[key], resource2[key]):
+                    self.module.log(self.MSG_DIFF_AT_KEY.format(key) + debug_resources)
+                    return False
+            elif isinstance(resource1[key], list):
+                # change comparison function to compare_list
+                if not self.compare_list(resource1[key], resource2[key]):
+                    self.module.log(self.MSG_DIFF_AT_KEY.format(key) + debug_resources)
+                    return False
+            elif _standardize_value(resource1[key]) != _standardize_value(resource2[key]):
+                self.module.log(self.MSG_DIFF_AT_KEY.format(key) + debug_resources)
+                return False
     
-      if prefix_words.find('<S>') != 0:
-    prefix_words = '<S> ' + prefix_words
+    RETURN = '''
+modlist:
+  description: list of modified parameters
+  returned: success
+  type: list
+  sample: '[[2, 'olcRootDN', ['cn=root,dc=example,dc=com']]]'
+'''
     
-      for batch in range(num_batches):
-    x = np.zeros([batch_size, num_steps], dtype=np.int32)
-    y = np.zeros([batch_size, num_steps], dtype=np.int32)
-    w = np.zeros([batch_size, num_steps], dtype=np.float)
+        @staticmethod
+    def get_streams_by_id(account_number, video_id):
+        '''
+        int, int->list
+        
+        Get the height of the videos.
+        
+        Since brightcove is using 3 kinds of links: rtmp, http and https,
+        we will be using the HTTPS one to make it secure.
+        
+        If somehow akamaihd.net is blocked by the Great Fucking Wall,
+        change the 'startswith https' to http.
+        '''
+        endpoint = 'https://edge.api.brightcove.com/playback/v1/accounts/{account_number}/videos/{video_id}'.format(account_number = account_number, video_id = video_id)
+        fake_header_id = fake_headers
+        #is this somehow related to the time? Magic....
+        fake_header_id['Accept'] ='application/json;pk=BCpkADawqM1cc6wmJQC2tvoXZt4mrB7bFfi6zGt9QnOzprPZcGLE9OMGJwspQwKfuFYuCjAAJ53JdjI8zGFx1ll4rxhYJ255AXH1BQ10rnm34weknpfG-sippyQ'
     
-      if epoch_size == 0:
-    raise ValueError('epoch_size == 0, decrease batch_size or num_steps')
+    #----------------------------------------------------------------------
+def ckplayer_download_by_xml(ckinfo, output_dir = '.', merge = False, info_only = False, **kwargs):
+    #Info XML
+    video_info = ckplayer_get_info_by_xml(ckinfo)
     
-    import train_mask_gan
-from data import imdb_loader
-from data import ptb_loader
+    try:
+        title = kwargs['title']
+    except:
+        title = ''
+    type_ = ''
+    size = 0
     
+    if len(video_info['links']) > 0:  #has link
+        type_, _ext, size = url_info(video_info['links'][0])  #use 1st to determine type, ext
     
-def generator_GAN_loss(predictions):
-  '''Generator GAN loss based on Discriminator predictions.'''
-  return -tf.log(tf.reduce_mean(predictions))
-    
-    
-def create_dis_pretrain_op(hparams, dis_loss, global_step):
-  '''Create a train op for pretraining.'''
-  with tf.name_scope('pretrain_generator'):
-    optimizer = tf.train.AdamOptimizer(hparams.dis_pretrain_learning_rate)
-    dis_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('dis')
-    ]
-    if FLAGS.dis_update_share_embedding and FLAGS.dis_share_embedding:
-      shared_embedding = [
-          v for v in tf.trainable_variables()
-          if v.op.name == 'gen/decoder/rnn/embedding'
-      ][0]
-      dis_vars.append(shared_embedding)
-    dis_grads = tf.gradients(dis_loss, dis_vars)
-    dis_grads_clipped, _ = tf.clip_by_global_norm(dis_grads,
-                                                  FLAGS.grad_clipping)
-    dis_pretrain_op = optimizer.apply_gradients(
-        zip(dis_grads_clipped, dis_vars), global_step=global_step)
-    return dis_pretrain_op
-    
-          elif FLAGS.generator_model == 'seq2seq_nas':
-        # Encoder.
-        gen_encoder_variable_maps = variable_mapping.gen_encoder_seq2seq_nas(
-            hparams)
-        gen_encoder_init_saver = tf.train.Saver(
-            var_list=gen_encoder_variable_maps)
-        # Decoder.
-        gen_decoder_variable_maps = variable_mapping.gen_decoder_seq2seq_nas(
-            hparams)
-        gen_decoder_init_saver = tf.train.Saver(
-            var_list=gen_decoder_variable_maps)
-        init_savers['gen_encoder_init_saver'] = gen_encoder_init_saver
-        init_savers['gen_decoder_init_saver'] = gen_decoder_init_saver
-    
-    
-def evaluate_masks(
-    json_dataset,
-    all_boxes,
-    all_segms,
-    output_dir,
-    use_salt=True,
-    cleanup=False
-):
-    if cfg.CLUSTER.ON_CLUSTER:
-        # On the cluster avoid saving these files in the job directory
-        output_dir = '/tmp'
-    res_file = os.path.join(
-        output_dir, 'segmentations_' + json_dataset.name + '_results')
-    if use_salt:
-        res_file += '_{}'.format(str(uuid.uuid4()))
-    res_file += '.json'
-    
-    Flexible network configuration is achieved by specifying the function name that
-builds a network module (e.g., the name of the conv backbone or the mask roi
-head). However we may wish to change names over time without breaking previous
-config files. This module provides backwards naming compatibility by providing
-a mapping from the old name to the new name.
-    
-    def add_generic_rpn_outputs(model, blob_in, dim_in, spatial_scale_in):
-    '''Add RPN outputs (objectness classification and bounding box regression)
-    to an RPN model. Abstracts away the use of FPN.
-    '''
-    loss_gradients = None
-    if cfg.FPN.FPN_ON:
-        # Delegate to the FPN module
-        FPN.add_fpn_rpn_outputs(model, blob_in, dim_in, spatial_scale_in)
-        if cfg.MODEL.FASTER_RCNN:
-            # CollectAndDistributeFpnRpnProposals also labels proposals when in
-            # training mode
-            model.CollectAndDistributeFpnRpnProposals()
-        if model.train:
-            loss_gradients = FPN.add_fpn_rpn_losses(model)
+    if 'size' in video_info:
+        size = int(video_info['size'])
     else:
-        # Not using FPN, add RPN to a single scale
-        add_single_scale_rpn_outputs(model, blob_in, dim_in, spatial_scale_in)
-        if model.train:
-            loss_gradients = add_single_scale_rpn_losses(model)
-    return loss_gradients
+        for i in video_info['links'][1:]:  #save 1st one
+            size += url_info(i)[2]
     
+    print_info(site_info, title, type_, size)
+    if not info_only:
+        download_urls(video_info['links'], title, _ext, size, output_dir=output_dir, merge=merge)
     
-def _FormatYcmdDebugInfo( ycmd ):
-  python = ycmd[ 'python' ]
-  clang = ycmd[ 'clang' ]
-  message = ( 'Server Python interpreter: {0}\n'
-              'Server Python version: {1}\n'
-              'Server has Clang support compiled in: {2}\n'
-              'Clang version: {3}\n'.format( python[ 'executable' ],
-                                             python[ 'version' ],
-                                             clang[ 'has_support' ],
-                                             clang[ 'version' ] ) )
-  extra_conf = ycmd[ 'extra_conf' ]
-  extra_conf_path = extra_conf[ 'path' ]
-  if not extra_conf_path:
-    message += 'No extra configuration file found\n'
-  elif not extra_conf[ 'is_loaded' ]:
-    message += ( 'Extra configuration file found but not loaded\n'
-                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
-  else:
-    message += ( 'Extra configuration file found and loaded\n'
-                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
-  return message
+    __all__ = ['dailymotion_download']
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+            for i in range(len(titles)):
+            title = titles[i]
+            datas = {
+                'sid': song_id[i],
+                'ssid': song_ssid[i]
+            }
+            post_params = urllib.parse.urlencode(datas).encode('utf-8')
+            try:
+                resp = urllib.request.urlopen(get_song_url, post_params)
+                resp_data = json.loads(resp.read().decode('utf-8'))
+                real_url = resp_data['r']
+                type, ext, size = url_info(real_url)
+                print_info(site_info, title, type, size)
+            except:
+                pass
     
-            if return_when == FIRST_EXCEPTION:
-            waiter = _AllCompletedWaiter(pending_count, stop_on_exception=True)
-        elif return_when == ALL_COMPLETED:
-            waiter = _AllCompletedWaiter(pending_count, stop_on_exception=False)
-        else:
-            raise ValueError('Invalid return condition: %r' % return_when)
-    
-        def __init__(self, obj_name, obj, log_name=None, max_lines=10):
-        self.obj_name = obj_name
-        self.obj = obj
-        self.max_lines = max_lines
-        self.log = logging.getLogger(log_name or __name__)
-    
-        def __init__(self, destination, *args, **kwargs):
-        super(BinaryDownloader, self).__init__(*args, **kwargs)
-        self.destination = destination
-        os.makedirs(self.destination, exist_ok=True)
-    
-        @pytest.mark.skipif(six.PY3, reason='Env values in Python 3 are already Unicode')
-    def test_unicode_path_from_env(self):
-        with mock.patch.dict(os.environ):
-            os.environ['COMPOSE_FILE'] = b'\xe5\xb0\xb1\xe5\x90\x83\xe9\xa5\xad/docker-compose.yml'
-            environment = Environment.from_env_file('.')
-            assert get_config_path_from_options(
-                '.', {}, environment
-            ) == ['就吃饭/docker-compose.yml']
-
-    
-    from compose.cli import colors
-from compose.cli.formatter import ConsoleWarningFormatter
-from tests import unittest
-    
-    import unittest
-    
-    
-class TestServicePort(object):
-    def test_parse_dict(self):
-        data = {
-            'target': 8000,
-            'published': 8000,
-            'protocol': 'udp',
-            'mode': 'global',
-        }
-        ports = ServicePort.parse(data)
-        assert len(ports) == 1
-        assert ports[0].repr() == data
-    
-        def test_parallel_execute(self):
-        results, errors = parallel_execute(
-            objects=[1, 2, 3, 4, 5],
-            func=lambda x: x * 2,
-            get_name=six.text_type,
-            msg='Doubling',
-        )
-    
-            self.assert_produces(reader, ['abc\n', 'def\n', 'ghi'])
-    
-    
-def test_second():
-    assert timeparse.timeparse('200s') == 200
-    
-    
-def input_fn_builder(input_file, seq_length, is_training, drop_remainder):
-  '''Creates an `input_fn` closure to be passed to TPUEstimator.'''
-    
-            # Empty lines are used as document delimiters
-        if not line:
-          all_documents.append([])
-        tokens = tokenizer.tokenize(line)
-        if tokens:
-          all_documents[-1].append(tokens)
+        html = get_content(url)
+    uuid_pattern = r''([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})''
+    id = r1(r'var vid='([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})'', html)
+    if id is None:
+        video_pattern = r''vid'\s*:\s*' + uuid_pattern
+        id = match1(html, video_pattern)
+    assert id, 'can't find video info'
+    return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
