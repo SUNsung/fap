6c4bf1c72d418d4ee81d2bfac969c23723d3013d
SUNsung <weatherius@gmail.com>
2019-09-06 21:57:58 +0300

    Rewrite [29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb] File in: 2019-09-06 21:57:57

--- ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
+++ ruby/29O1vJufamD6M29GTfErr0DnaY29jxZfieSkycA297w5.rb
@@ -1,110 +1,84 @@
 
         
-          # Returns the FQDN of the image URL.
-  #
-  # @param [String] path
-  #
-  # @return [String]
-  def image_url(path)
-    File.join(base_url, image_path(path))
-  end
+                  find_every_method_call_by_name(body_node, :system).each do |method_node|
+            # Skip Kibana: npm cache edge (see formula for more details)
+            next if @formula_name =~ /^kibana(@\d[\d.]*)?$/
     
-              # Create a temporary file to store the data so we can upload it.
-          remote_file = File.join(guest_provisioning_path, 'dna.json')
-          @machine.communicate.sudo(remove_command(remote_file), error_check: false)
-          Tempfile.open('vagrant-chef-provisioner-config') do |f|
-            f.binmode
-            f.write(json)
-            f.fsync
-            f.close
-            @machine.communicate.upload(f.path, remote_file)
-          end
+      it 'unless build.without? conditional' do
+    expect_offense(<<~RUBY)
+      class Foo < Formula
+        desc 'foo'
+        url 'https://brew.sh/foo-1.0.tgz'
+        def post_install
+          return unless build.without? 'bar'
+                        ^^^^^^^^^^^^^^^^^^^^ Use if build.with? 'bar' instead of unless build.without? 'bar'
         end
-    
-        it 'returns true if installed' do
-      expect(machine.communicate).to receive(:test).
-        with(command, sudo: true).and_return(true)
-      subject.chef_installed(machine, 'chef_solo', version)
-    end
-    
-    # Load in helpers
-require 'unit/support/dummy_communicator'
-require 'unit/support/dummy_provider'
-require 'unit/support/shared/base_context'
-require 'unit/support/shared/action_synced_folders_context'
-require 'unit/support/shared/capability_helpers_context'
-require 'unit/support/shared/plugin_command_context'
-require 'unit/support/shared/virtualbox_context'
-    
-            expect(box_collection).to receive(:add).with(any_args) { |path, name, version, **opts|
-          expect(checksum(path)).to eq(checksum(box_path))
-          expect(name).to eq('foo')
-          expect(version).to eq('0')
-          expect(opts[:metadata_url]).to be_nil
-          true
-        }.and_return(box)
-    
-            if !par
-          thread.join(THREAD_MAX_JOIN_TIMEOUT) while thread.alive?
-        end
-        threads << thread
       end
-    
-        context 'when extra_args is provided as a string' do
-      let(:ssh_info) {{
-        host: 'localhost',
-        port: 2222,
-        username: 'vagrant',
-        private_key_path: [private_key_path],
-        extra_args: '-6'
-      }}
-    
-        # Fallback to the default
-    path ||= '~/.vagrant.d'
-    
-          spec['version'] = Bootstrap::VERSION
-    
-    get '/stream', :provides => 'text/event-stream' do
-  stream :keep_open do |out|
-    connections << out
-    out.callback { connections.delete(out) }
+    RUBY
   end
+    
+    describe SoftwareSpec do
+  subject(:spec) { described_class.new }
+    
+    get '/' do
+  halt erb(:login) unless params[:user]
+  erb :chat, :locals => { :user => params[:user].gsub(/\W/, '') }
 end
     
-                <li class='code <%= frame_class(frame) %>'>
-              <% if frame.pre_context %>
-              <ol start='<%=h frame.pre_context_lineno + 1 %>'
-                  class='pre-context' id='pre-<%= id %>'
-                  onclick='toggle(<%= id %>);'>
-                <% frame.pre_context.each do |line| %>
-                <li class='pre-context-line'><code><%=h line %></code></li>
-                <% end %>
-              </ol>
-              <% end %>
+        <div id='backtrace' class='condensed'>
+      <h3>BACKTRACE</h3>
+      <p><a href='#' id='expando'
+            onclick='toggleBacktrace(); return false'>(expand)</a></p>
+      <p id='nav'><strong>JUMP TO:</strong>
+         <% unless bad_request?(exception) %>
+            <a href='#get-info'>GET</a>
+            <a href='#post-info'>POST</a>
+         <% end %>
+         <a href='#cookie-info'>COOKIES</a>
+         <a href='#env-info'>ENV</a>
+      </p>
+      <div class='clear'></div>
     
-          def self.default_reaction(reaction)
-        alias_method(:default_reaction, reaction)
+          # Essentially the inverse of +mask_token+.
+      def unmask_token(masked_token)
+        # Split the token into the one-time pad and the encrypted
+        # value and decrypt it
+        token_length = masked_token.length / 2
+        one_time_pad = masked_token[0...token_length]
+        encrypted_token = masked_token[token_length..-1]
+        xor_byte_strings(one_time_pad, encrypted_token)
       end
     
-        headers = get('/', {}, 'wants' => 'text/html').headers
-    expect(headers['Content-Security-Policy']).to eq('connect-src 'self'; default-src none; img-src 'self'; script-src 'self'; style-src 'self'')
-  end
-    
-    require 'rspec/core/rake_task'
-RSpec::Core::RakeTask.new(:spec) do |spec|
-  spec.pattern = 'spec/**/*_spec.rb'
-  spec.exclude_pattern = 'spec/integration/**/*_spec.rb'
-end
-    
-        def write(header)
-      @cookies.select { |key, _value| @send_cookies[key] == true }.each do |name, value|
-        cookie_value = value.is_a?(Hash) ? value : { value: value }
-        Rack::Utils.set_cookie_header! header, name, cookie_value
-      end
+        it 'Reads referrer from Host header when Referer header is missing' do
+      env = {'HTTP_HOST' => 'foo.com'}
+      expect(subject.referrer(env)).to eq('foo.com')
     end
     
-            if presenter
-          embeds = { env: env }
-          embeds[:version] = env[Grape::Env::API_VERSION] if env[Grape::Env::API_VERSION]
-          presented_message = presenter.represent(presented_message, embeds).serializable_hash
-        end
+          get '/?95df8d9bf5237ad08df3115ee74dcb10'
+      expect(body).to eq('hi')
+    end
+    
+          insist { input.attributes[:snap_grade] } == 'test-grade'
+    end
+  end
+end
+
+    
+      def set_default_prefix
+    attributes[:prefix] = self.class.default_prefix
+    attributes[:prefix_given?] = true
+  end
+    
+        # Combine template and filelist; allow user to edit before proceeding
+    File.open(manifest_fn, 'w') do |manifest|
+      manifest.write metadata_template
+      manifest.write pkg_generate
+    end
+    edit_file(manifest_fn) if attributes[:edit?]
+    
+    # This provides PHP PEAR package support.
+#
+# This provides input support, but not output support.
+class FPM::Package::PEAR < FPM::Package
+  option '--package-name-prefix', 'PREFIX',
+    'Name prefix for pear package', :default => 'php-pear'
