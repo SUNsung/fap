5f7255b517879cde9394c48a7541b63d42ecc95d
SUNsung <weatherius@gmail.com>
2019-09-06 17:32:03 +0300

    Rewrite [26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py] File in: 2019-09-06 17:32:02

--- pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
+++ pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
@@ -1,175 +1,204 @@
 
         
-                try:
-            self._db = shelve.open(cache_path)
-        except shelve_open_error + (ImportError,):
-            # Caused when switching between Python versions
-            warn('Removing possibly out-dated cache')
-            os.remove(cache_path)
-            self._db = shelve.open(cache_path)
+        
+def waveform_to_examples(data, sample_rate):
+  '''Converts audio waveform into an array of examples for VGGish.
     
-            packages = subprocess.check_output(
-            ['pkgfile', '-b', '-v', command],
-            universal_newlines=True, stderr=utils.DEVNULL
-        ).splitlines()
+    from __future__ import print_function
     
-        @pytest.fixture(autouse=True)
-    def Popen(self, mocker):
-        mock = mocker.patch('thefuck.shells.fish.Popen')
-        mock.return_value.stdout.read.side_effect = [(
-            b'cd\nfish_config\nfuck\nfunced\nfuncsave\ngrep\nhistory\nll\nls\n'
-            b'man\nmath\npopd\npushd\nruby'),
-            (b'alias fish_key_reader /usr/bin/fish_key_reader\nalias g git\n'
-             b'alias alias_with_equal_sign=echo\ninvalid_alias'), b'func1\nfunc2', b'']
-        return mock
+      # Read list of query images from dataset file.
+  tf.logging.info('Reading list of query images and boxes from dataset file...')
+  query_list, _, ground_truth = dataset.ReadDatasetFile(
+      cmd_args.dataset_file_path)
+  num_images = len(query_list)
+  tf.logging.info('done! Found %d images', num_images)
     
-        @pytest.mark.parametrize('side_effect, exception', [
-        ([b'\n'], IndexError), (OSError, OSError)])
-    def test_get_version_error(self, side_effect, exception, shell, Popen):
-        Popen.return_value.stdout.read.side_effect = side_effect
-        with pytest.raises(exception):
-            shell._get_version()
-        assert Popen.call_args[0][0] == ['tcsh', '--version']
+    The :class:`sklearn.svm.OneClassSVM` is known to be sensitive to outliers and
+thus does not perform very well for outlier detection. This estimator is best
+suited for novelty detection when the training set is not contaminated by
+outliers. That said, outlier detection in high-dimension, or without any
+assumptions on the distribution of the inlying data is very challenging, and a
+One-class SVM might give useful results in these situations depending on the
+value of its hyperparameters.
+    
+    import numpy as np
+from sklearn.covariance import EllipticEnvelope
+from sklearn.svm import OneClassSVM
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+from sklearn.datasets import load_boston
+    
+    plt.matshow(fit_data, cmap=plt.cm.Blues)
+plt.title('After biclustering; rearranged to show biclusters')
+    
+    Two consequences of imposing a connectivity can be seen. First clustering
+with a connectivity matrix is much faster.
+    
+    # If not '', a 'Last updated on:' timestamp is inserted at every page
+# bottom, using the given strftime format.
+#html_last_updated_fmt = '%b %d, %Y'
+    
+        # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
+    
+            if True in result:
+            [print_result(image_to_check, name, distance, show_distance) for is_match, name, distance in zip(result, known_names, distances) if is_match]
+        else:
+            print_result(image_to_check, 'unknown_person', None, show_distance)
+    
+        def test_face_locations(self):
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
+        detected_faces = api.face_locations(img)
+    
+    
+def batch_face_locations(images, number_of_times_to_upsample=1, batch_size=128):
+    '''
+    Returns an 2d array of bounding boxes of human faces in a image using the cnn face detector
+    If you are using a GPU, this can give you much faster results since the GPU
+    can process batches of images at once. If you aren't using a GPU, you don't need this function.
+    
+        pil_image.show()
 
     
-        def from_shell(self, command_script):
-        '''Prepares command before running in app.'''
-        return self._expand_aliases(command_script)
+        # 获得所有人脸的位置以及它们的编码
+    face_locations = face_recognition.face_locations(output)
+    print('Found {} faces in image.'.format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
     
-        def app_alias(self, alias_name):
-        return ('alias {0} 'setenv TF_SHELL tcsh && setenv TF_ALIAS {0} && '
-                'set fucked_cmd=`history -h 2 | head -n 1` && '
-                'eval `thefuck ${{fucked_cmd}}`'').format(alias_name)
-    
-    
-@pytest.mark.usefixtures('no_memoize')
-@pytest.mark.parametrize('script, output, which', [
-    ('qweqwe', 'qweqwe: not found', None),
-    ('vom file.py', 'some text', None),
-    ('vim file.py', 'vim: not found', 'vim')])
-def test_not_match(mocker, script, output, which):
-    mocker.patch('thefuck.rules.no_command.which', return_value=which)
-    
-    
-@pytest.fixture(autouse=True)
-def Popen(mocker):
-    mock = mocker.patch('thefuck.rules.pyenv_no_such_command.Popen')
-    mock.return_value.stdout.readlines.return_value = (
-        b'--version\nactivate\ncommands\ncompletions\ndeactivate\nexec_\n'
-        b'global\nhelp\nhooks\ninit\ninstall\nlocal\nprefix_\n'
-        b'realpath.dylib\nrehash\nroot\nshell\nshims\nuninstall\nversion_\n'
-        b'version-file\nversion-file-read\nversion-file-write\nversion-name_\n'
-        b'version-origin\nversions\nvirtualenv\nvirtualenv-delete_\n'
-        b'virtualenv-init\nvirtualenv-prefix\nvirtualenvs_\n'
-        b'virtualenvwrapper\nvirtualenvwrapper_lazy\nwhence\nwhich_\n'
-    ).split()
-    return mock
-    
-        # One from history:
-    already_used = get_closest(
-        old_command, _get_used_executables(command),
-        fallback_to_first=False)
-    if already_used:
-        new_cmds = [already_used]
-    else:
-        new_cmds = []
-    
-            assert certdata, 'cert file %r is broken' % certfile
-        import ctypes
-        import ctypes.wintypes
-        class CERT_CONTEXT(ctypes.Structure):
-            _fields_ = [
-                ('dwCertEncodingType', ctypes.wintypes.DWORD),
-                ('pbCertEncoded', ctypes.POINTER(ctypes.wintypes.BYTE)),
-                ('cbCertEncoded', ctypes.wintypes.DWORD),
-                ('pCertInfo', ctypes.c_void_p),
-                ('hCertStore', ctypes.c_void_p),]
-        X509_ASN_ENCODING = 0x1
-        CERT_STORE_ADD_ALWAYS = 4
-        CERT_STORE_PROV_SYSTEM = 10
-        CERT_STORE_OPEN_EXISTING_FLAG = 0x4000
-        CERT_SYSTEM_STORE_CURRENT_USER = 1 << 16
-        CERT_SYSTEM_STORE_LOCAL_MACHINE = 2 << 16
-        CERT_FIND_SUBJECT_STR = 8 << 16 | 7
-        crypt32 = ctypes.windll.crypt32
-        ca_exists = False
-        store_handle = None
-        pCertCtx = None
-        ret = 0
-        for store in (CERT_SYSTEM_STORE_LOCAL_MACHINE, CERT_SYSTEM_STORE_CURRENT_USER):
-            try:
-                store_handle = crypt32.CertOpenStore(CERT_STORE_PROV_SYSTEM, 0, None, CERT_STORE_OPEN_EXISTING_FLAG | store, u'root')
-                if not store_handle:
-                    if store == CERT_SYSTEM_STORE_CURRENT_USER and not ca_exists:
-                        xlog.warning('CertUtil.import_windows_ca failed: could not open system cert store')
-                        return False
-                    else:
-                        continue
-    
-        '''
-    
-    def __init__(self, type=None, channel=DEFAULT_CHANNEL, text=None,
-                 input=None, start=None, stop=None, oldToken=None):
-        Token.__init__(self)
-        
-        if oldToken is not None:
-            self.type = oldToken.type
-            self.line = oldToken.line
-            self.charPositionInLine = oldToken.charPositionInLine
-            self.channel = oldToken.channel
-            self.index = oldToken.index
-            self._text = oldToken._text
-            if isinstance(oldToken, CommonToken):
-                self.input = oldToken.input
-                self.start = oldToken.start
-                self.stop = oldToken.stop
-            
+            #If training image contains none or more than faces, print an error message and exit
+        if len(face_bounding_boxes) != 1:
+            print(person + '/' + person_img + ' contains none or more than one faces and can't be used for training.')
+            exit()
         else:
-            self.type = type
-            self.input = input
-            self.charPositionInLine = -1 # set to invalid position
-            self.line = 0
-            self.channel = channel
-            
-	    #What token number is this from 0..n-1 tokens; < 0 implies invalid index
-            self.index = -1
-            
-            # We need to be able to change the text once in a while.  If
-            # this is non-null, then getText should return this.  Note that
-            # start/stop are not affected by changing this.
-            self._text = text
+            face_enc = face_recognition.face_encodings(face)[0]
+            # Add face encoding for current image with corresponding label (name) to the training data
+            encodings.append(face_enc)
+            names.append(person)
     
-        # Replace random parameters with COCO parameters if class mapping exists
-    for i in range(NUM_CS_CLS):
-        coco_cls_id = getattr(cs, convert_func)(i)
-        if coco_cls_id >= 0:  # otherwise ignore (rand init)
-            cs_blob[i] = coco_blob[coco_cls_id]
+                # If a match was found in known_face_encodings, just use the first one.
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
     
-            # Test: merge with converted type
-        cfg2 = AttrDict()
-        cfg2.TRAIN = AttrDict()
-        cfg2.TRAIN.SCALES = [1]
-        core_config.merge_cfg_from_cfg(cfg2)
-        assert type(cfg.TRAIN.SCALES) is tuple
-        assert cfg.TRAIN.SCALES[0] == 1
+        description = proj_info['description'],
+    keywords = proj_info['keywords'],
     
-    from caffe2.proto import caffe2_pb2
+    _help = '''Usage: {} [OPTION]... [URL]...
+TODO
+'''.format(script_name)
     
-        boxes = []
-    scores = []
-    ids = []
-    for i in range(raw_data.shape[0]):
-        if i % 1000 == 0:
-            print('{}/{}'.format(i + 1, len(roidb)))
-        # selective search boxes are 1-indexed and (y1, x1, y2, x2)
-        i_boxes = raw_data[i][:, (1, 0, 3, 2)] - 1
-        boxes.append(i_boxes.astype(np.float32))
-        scores.append(np.zeros((i_boxes.shape[0]), dtype=np.float32))
-        ids.append(roidb[i]['id'])
+            for i in self.tree.iterfind('video/quality'):
+            quality = i.attrib ['value']
+            url = i[0].attrib['playurl']
+            self.stream_types.append({'id': quality,
+                                      'video_profile': i.attrib ['desp']})
+            self.streams[quality] = {'url': url,
+                                     'video_profile': i.attrib ['desp']}
+            self.streams_sorted = [dict([('id', stream_type['id'])] + list(self.streams[stream_type['id']].items())) for stream_type in self.__class__.stream_types if stream_type['id'] in self.streams]
     
-    from detectron.core.config import cfg
-from detectron.datasets import task_evaluation
-from detectron.datasets.json_dataset import JsonDataset
-from detectron.utils.io import load_object
-from detectron.utils.logging import setup_logging
-import detectron.core.config as core_config
+    __all__ = ['cbs_download']
+    
+    def rebuilt_url(url):
+    path = urllib.parse.urlparse(url).path
+    aid = path.split('/')[-1].split('_')[0]
+    return 'http://www.dailymotion.com/embed/video/{}?autoplay=1'.format(aid)
+    
+            print_info(site_info, title, type, size)
+        if not info_only:
+            download_urls([real_url], title, ext, size, output_dir, merge = merge)
+    
+    #----------------------------------------------------------------------
+def makeMimi(upid):
+    '''From http://cdn37.atwikiimg.com/sitescript/pub/dksitescript/FC2.site.js
+    Also com.hps.util.fc2.FC2EncrptUtil.makeMimiLocal
+    L110'''
+    strSeed = 'gGddgPfeaf_gzyr'
+    prehash = upid + '_' + strSeed
+    return md5(prehash.encode('utf-8')).hexdigest()
+    
+        stream_id_pattern = r'id='html_stream' value='(\w+)''
+    stream_id = match1(html, stream_id_pattern)
+    
+        The following keys are expected for the _DEFAULTS <metadata> dict:
+        datatype:  [required] A python type class. This limits the type of data that can be
+                   provided in the .ini file and ensures that the value is returned in the
+                   correct type to faceswap. Valid datatypes are: <class 'int'>, <class 'float'>,
+                   <class 'str'>, <class 'bool'>.
+        default:   [required] The default value for this option.
+        info:      [required] A string describing what this option does.
+        choices:   [optional] If this option's datatype is of <class 'str'> then valid
+                   selections can be defined here. This validates the option and also enables
+                   a combobox / radio option in the GUI.
+        gui_radio: [optional] If <choices> are defined, this indicates that the GUI should use
+                   radio buttons rather than a combobox to display this option.
+        min_max:   [partial] For <class 'int'> and <class 'float'> datatypes this is required
+                   otherwise it is ignored. Should be a tuple of min and max accepted values.
+                   This is used for controlling the GUI slider range. Values are not enforced.
+        rounding:  [partial] For <class 'int'> and <class 'float'> datatypes this is
+                   required otherwise it is ignored. Used for the GUI slider. For floats, this
+                   is the number of decimal places to display. For ints this is the step size.
+        fixed:     [optional] [train only]. Training configurations are fixed when the model is
+                   created, and then reloaded from the state file. Marking an item as fixed=False
+                   indicates that this value can be changed for existing models, and will override
+                   the value saved in the state file with the updated value in config. If not
+                   provided this will default to True.
+'''
+    
+            return blended.astype('float32') / 255.0
+
+    
+        def rename(self, src, dest):
+        print(u'renaming %s to %s' % (src, dest))
+        os.rename(src, dest)
+    
+    
+def main():
+    '''
+    # Counting to two...
+    >>> for number in count_to_two():
+    ...     print(number)
+    one
+    two
+    
+        >>> data1.data = 3
+    DecimalViewer: Subject Data 1 has data 3
+    HexViewer: Subject Data 1 has data 0x3
+    
+    
+def on_sale_discount(order):
+    return order.price * 0.25 + 20
+    
+    
+def get_localizer(language='English'):
+    '''Factory'''
+    localizers = {
+        'English': EnglishLocalizer,
+        'Greek': GreekLocalizer,
+    }
+    return localizers[language]()
+    
+    
+class Delegator(object):
+    '''
+    >>> delegator = Delegator(Delegate())
+    >>> delegator.p1
+    123
+    >>> delegator.p2
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Delegate' object has no attribute 'p2'
+    >>> delegator.do_something('nothing')
+    'Doing nothing'
+    >>> delegator.do_anything()
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Delegate' object has no attribute 'do_anything'
+    '''
+    
+        graphic.add(graphic1)
+    graphic.add(graphic2)
+    
+    *TL;DR
+Adds behaviour to object without affecting its class.
+'''
