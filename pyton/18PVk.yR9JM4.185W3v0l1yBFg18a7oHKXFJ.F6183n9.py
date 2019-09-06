bd3e21f8ff32f4e089f6accde0b0a6a44081bd6e
SUNsung <weatherius@gmail.com>
2019-09-06 17:52:28 +0300

    Rewrite [18PVk.yR9JM4.185W3v0l1yBFg18a7oHKXFJ.F6183n9.py] File in: 2019-09-06 17:52:28

--- pyton/18PVk.yR9JM4.185W3v0l1yBFg18a7oHKXFJ.F6183n9.py
+++ pyton/18PVk.yR9JM4.185W3v0l1yBFg18a7oHKXFJ.F6183n9.py
@@ -1,50 +1,192 @@
 
         
-            def _escalate_call(self):
-        self.call.state = CallState.READY
-        call = self.call
-        self.call = None
-        self.call_center.notify_call_escalated(call)
-    
-        def __init__(self, cards):
-        super(BlackJackHand, self).__init__(cards)
-    
-        def reject_friend_request(self, friend_id):
-        pass
+            with open('README.md', 'w+') as sorted_file:
+        sorted_file.write(final_README)
     
     
-class RemoveDuplicateUrls(MRJob):
+class ContentDecodingError(RequestException, BaseHTTPError):
+    '''Failed to decode response content'''
     
-        def set(self, key, value):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                item.value = value
-                return
-        self.table[hash_index].append(Item(key, value))
+        def get(self, name, default=None, domain=None, path=None):
+        '''Dict-like get() that also supports optional domain and path args in
+        order to resolve naming collisions from using one cookie jar over
+        multiple domains.
     
-        def __init__(self, pages, data_store, reverse_index_queue, doc_index_queue):
-        self.pages = pages
-        self.data_store = data_store
-        self.reverse_index_queue = reverse_index_queue
-        self.doc_index_queue = doc_index_queue
+        # Copy is required
+    def copy(self):
+        return CaseInsensitiveDict(self._store.values())
     
-        :param filepath: Optional filepath the the blns.txt file
-    :returns: The list of naughty strings
-    '''
+            :param request: The :class:`PreparedRequest <PreparedRequest>` being sent.
+        :param stream: (optional) Whether to stream the request content.
+        :param timeout: (optional) How long to wait for the server to send
+            data before giving up, as a float, or a :ref:`(connect timeout,
+            read timeout) <timeouts>` tuple.
+        :type timeout: float or tuple
+        :param verify: (optional) Either a boolean, in which case it controls whether we verify
+            the server's TLS certificate, or a string, in which case it must be a path
+            to a CA bundle to use
+        :param cert: (optional) Any user-provided SSL certificate to be trusted.
+        :param proxies: (optional) The proxies dictionary to apply to the request.
+        '''
+        raise NotImplementedError
     
-    fp.close()
+    
+class RequestHooksMixin(object):
+    def register_hook(self, event, hook):
+        '''Properly register a hook.'''
+    
+        def test_json_bom_response(self):
+        '''the text_response_server sends the given JSON with UTF-8 BOM'''
+        server = Server.text_response_server(
+            'HTTP/1.1 200 OK\r\n' +
+            'Content-Type: application/json; charset=utf-8\r\n' +
+            u'\r\n\ufeff{'success': true}'
+        )
+    
+            return spec
+    
+        def _get_policies_for_datacenter(self, p):
+        '''
+        Get the Policies for a datacenter by calling the CLC API.
+        :param p: datacenter to get policies from
+        :return: policies in the datacenter
+        '''
+        response = {}
+    
+    ---
+- name: Create Alert Policy Example
+  hosts: localhost
+  gather_facts: False
+  connection: local
+  tasks:
+    - name: Create an Alert Policy for disk above 80% for 5 minutes
+      clc_alert_policy:
+        alias: wfad
+        name: 'alert for disk > 80%'
+        alert_recipients:
+            - test1@centurylink.com
+            - test2@centurylink.com
+        metric: 'disk'
+        duration: '00:05:00'
+        threshold: 80
+        state: present
+      register: policy
+    
+            if not CLC_FOUND:
+            self.module.fail_json(msg=missing_required_lib('clc-sdk'), exception=CLC_IMP_ERR)
+        if not REQUESTS_FOUND:
+            self.module.fail_json(msg=missing_required_lib('requests'), exception=REQUESTS_IMP_ERR)
+        if requests.__version__ and LooseVersion(
+                requests.__version__) < LooseVersion('2.5.0'):
+            self.module.fail_json(
+                msg='requests library  version should be >= 2.5.0')
+    
+        def _ensure_alert_policy_absent(
+            self, server, server_params):
+        '''
+        ensures the alert policy is removed from the server
+        :param server: the CLC server object
+        :param server_params: the dictionary of server parameters
+        :return: (changed, group) -
+            changed: Boolean whether a change was made
+            result: The result from the CLC API call
+        '''
+        changed = False
+    
+    
+from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+from ansible.module_utils.urls import fetch_url
+if __name__ == '__main__':
+    main()
 
     
-        iterkeys = lambda d: d.iterkeys()
-    itervalues = lambda d: d.itervalues()
-    iteritems = lambda d: d.iteritems()
+            self.result['changed'] = True
+        try:
+            config_file = self.get_config()
+            self.result['diff']['after'] = config_file
+            if not self.module.check_mode:
+                self.server.create_job(self.name, config_file)
+        except Exception as e:
+            self.module.fail_json(msg='Unable to create job, %s for %s' % (to_native(e), self.jenkins_url),
+                                  exception=traceback.format_exc())
     
     
-def ChineseAnalyzer(stoplist=STOP_WORDS, minsize=1, stemfn=stem, cachesize=50000):
-    return (ChineseTokenizer() | LowercaseFilter() |
-            StopFilter(stoplist=stoplist, minsize=minsize) |
-            StemFilter(stemfn=stemfn, ignore=None, cachesize=cachesize))
-
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
     
-    tags = jieba.analyse.extract_tags(content, topK=topK)
+            release = repository.create_release(
+            tag, target, name, body, draft, prerelease)
+        if release:
+            module.exit_json(changed=True, tag=release.tag_name)
+        else:
+            module.exit_json(changed=False, tag=None)
+    
+        By default this will get the strings from the blns.txt file
+    
+            The optional 'block' and 'timeout' arguments are ignored, as this method
+        never blocks.  They are provided for compatibility with the Queue class.
+        '''
+        self._queue.append(item)
+        self._count.release()
+    
+            The first argument is a list of regular expressions, either
+        compiled (re.Pattern instances) or uncompiled (strings).
+        The optional second argument is a timeout, in seconds; default
+        is no timeout.
+    
+        # bpo-31217: Integer pool to get a single integer object for the same
+    # value. The pool is used to prevent false alarm when checking for memory
+    # block leaks. Fill the pool with values in -1000..1000 which are the most
+    # common (reference, memory block, file descriptor) differences.
+    int_pool = {value: value for value in range(-1000, 1000)}
+    def get_pooled_int(value):
+        return int_pool.setdefault(value, value)
+    
+    
+    # -- Predicates for the sub-command list ---------------------------
+    
+        def setUp(self):
+        from acme.errors import BadNonce
+        self.error = BadNonce(nonce='xxx', error='error')
+    
+        @classmethod
+    def default_decoder(cls, value):
+        try:
+            return pyrfc3339.parse(value)
+        except ValueError as error:
+            raise jose.DeserializationError(error)
+    
+    The JWS implementation in josepy only implements the base JOSE standard. In
+order to support the new header fields defined in ACME, this module defines some
+ACME-specific classes that layer on top of josepy.
+'''
+import josepy as jose
+    
+        def setUp(self):
+        super(EnhanceConfigTest, self).setUp()
+    
+            if record_contents:
+            # We need to remove old records in the same request
+            data['deletions'] = [
+                {
+                    'kind': 'dns#resourceRecordSet',
+                    'type': 'TXT',
+                    'name': record_name + '.',
+                    'rrdatas': record_contents,
+                    'ttl': record_ttl,
+                },
+            ]
+    
+            update = dns.update.Update(
+            domain,
+            keyring=self.keyring,
+            keyalgorithm=self.algorithm)
+        update.add(rel, record_ttl, dns.rdatatype.TXT, record_content)
+    
+        def setUp(self):
+        from certbot_dns_rfc2136.dns_rfc2136 import _RFC2136Client
+    
+              # plugin is an instance providing IPlugin, initialized
+          # somewhere else in the code
+          plugin_registry.find_init(plugin).name
