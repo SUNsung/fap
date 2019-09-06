e6a355db1719b09ff884077026bb4309bd72fb7b
SUNsung <weatherius@gmail.com>
2019-09-06 21:01:45 +0300

    Rewrite [12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb] File in: 2019-09-06 21:01:44

--- ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
+++ ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
@@ -1,129 +1,113 @@
 
         
-            Rex::ThreadFactory.spawn('AESEncryption', false) {
-      c1 = OpenSSL::Cipher.new('aes-128-cfb8')
-      c1.encrypt
-      c1.key=key
-      sock.put([0].pack('N'))
-      sock.put(c1.iv=c1.random_iv)
-      buf1 = socks[0].read(4096)
-      while buf1 and buf1 != ''
-        sock.put(c1.update(buf1))
-        buf1 = socks[0].read(4096)
-      end
-      sock.close()
-    }
-    
-        # Only try the same host/port combination once
-    phash = rhost + ':' + lport.to_s
-    return if self.listener_pairs[phash]
-    self.listener_pairs[phash] = true
-    
-        # Similarly, when we seperate large payloads into chunks to avoid the
-    # 998-byte problem mentioned above, we have to make sure that the first
-    # character of each chunk is an alpha character.  This simple algorithm
-    # will create a broken string in the case of 99 consecutive digits,
-    # slashes, and plusses in the base64 encoding, but the likelihood of
-    # that is low enough that I don't care.
-    i = 900;
-    while i < b64.length
-      while (b64[i].chr =~ %r{[0-9/+]})
-        i += 1
-      end
-      b64.insert(i,'.')
-      i += 900
+            def to_json
+      JSON.generate(as_json)
     end
     
+        def filter_const(name)
+      if name.is_a? Array
+        name.map &method(:filter_const)
+      else
+        Docs.const_get '#{name}_filter'.camelize
+      end
+    end
     
-end
-end
-end
-
+        def document?
+      @content =~ DOCUMENT_RGX
+    end
     
+        def request_options
+      options = { params: self.class.params, headers: self.class.headers }
+      options[:accept_encoding] = 'gzip' if self.class.force_gzip
+      options
+    end
     
-  #
-  # Payload types were copied from xCAT-server source code (IPMI.pm)
-  #
-  RMCP_ERRORS = {
-    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
-    2 => 'Invalid Session ID', #this shouldn't occur...
-    3 => 'Invalid payload type',#shouldn't occur..
-    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
-    5 => 'Invalid integrity algorithm', #same as above
-    6 => 'No matching authentication payload',
-    7 => 'No matching integrity payload',
-    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
-    9 => 'Invalid role',
-    0xa => 'Unauthorised role or privilege level requested',
-    0xb => 'Insufficient resources to create a session at the requested role',
-    0xc => 'Invalid username length',
-    0xd => 'Unauthorized name',
-    0xe => 'Unauthorized GUID',
-    0xf => 'Invalid integrity check value',
-    0x10 => 'Invalid confidentiality algorithm',
-    0x11 => 'No cipher suite match with proposed security algorithms',
-    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
-  }
+            css('*[layout]').remove_attr('layout')
+        css('*[layout-xs]').remove_attr('layout-xs')
+        css('*[flex]').remove_attr('flex')
+        css('*[flex-xs]').remove_attr('flex-xs')
+        css('*[ng-class]').remove_attr('ng-class')
+        css('*[align]').remove_attr('align')
+        css('h1, h2, h3').remove_attr('class')
     
-    namespace :bower do
+          private
     
-      # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-    
-              begin
-            lineno = frame.lineno-1
-            lines = ::File.readlines(frame.filename)
-            frame.pre_context_lineno = [lineno-CONTEXT, 0].max
-            frame.pre_context = lines[frame.pre_context_lineno...lineno]
-            frame.context_line = lines[lineno].chomp
-            frame.post_context_lineno = [lineno+CONTEXT, lines.size].min
-            frame.post_context = lines[lineno+1..frame.post_context_lineno]
-          rescue
+              if formula_tap == 'homebrew-core' && (depends_on?('veclibfort') || depends_on?('lapack'))
+            problem 'Formulae should use OpenBLAS as the default serial linear algebra library.'
           end
     
-            safe?(env) ||
-          valid_token?(session, env['HTTP_X_CSRF_TOKEN']) ||
-          valid_token?(session, Request.new(env).params[options[:authenticity_param]]) ||
-          ( options[:allow_if] && options[:allow_if].call(env) )
-      end
-    
-            modes       = Array options[:escape]
-        @escaper    = options[:escaper]
-        @html       = modes.include? :html
-        @javascript = modes.include? :javascript
-        @url        = modes.include? :url
-    
-              react_and_close(env, body) or [status, headers, body]
-        else
-          [status, headers, body]
+              def install
+            system 'dep', 'ensure'
+            ^^^^^^^^^^^^^^^^^^^^^^ use \'dep\', \'ensure\', \'-vendor-only\'
+          end
         end
+      RUBY
+    end
+    
+      before do
+    ENV['JAVA_HOME'] = nil
+  end
+    
+        def mdfind_query(*ids)
+      ids.map! { |id| 'kMDItemCFBundleIdentifier == #{id}' }.join(' || ')
+    end
+    
+          let(:specs) { { user_agent: :fake } }
+    
+        private
+    
+      def import_aspects
+    contact_groups.each do |group|
+      begin
+        user.aspects.create!(group.slice('name', 'chat_enabled'))
+      rescue ActiveRecord::RecordInvalid => e
+        logger.warn '#{self}: #{e}'
+      end
+    end
+    set_auto_follow_back_aspect
+  end
+    
+        context 'with duplicates' do
+      let(:archive_hash) {
+        {
+          'user' => {
+            'auto_follow_back_aspect' => 'Friends',
+            'profile'                 => {
+              'entity_data' => {
+                'author' => 'old_id@old_pod.nowhere'
+              }
+            },
+            'contact_groups'          => [{
+              'chat_enabled' => true,
+              'name'         => 'Friends'
+            }],
+            'followed_tags'           => [target.tag_followings.first.tag.name],
+            'post_subscriptions'      => [target.participations.first.target.guid]
+          }
+        }
+      }
+    
+        def recheck
+      pod = Pod.find(params[:pod_id])
+      pod.test_connection!
+    
+          rescue_from OpenSSL::SSL::SSLError do |e|
+        validation_fail_as_json(e)
       end
     
-      it 'accepts post form requests with masked authenticity_token field' do
-    post('/', {'authenticity_token' => masked_token}, 'rack.session' => session)
-    expect(last_response).to be_ok
-  end
+        # do it!
+    success = membership.destroy
     
-        headers = get('/', {}, 'wants' => 'text/html').headers
-    expect(headers['Content-Security-Policy']).to eq('connect-src https://api.mybank.com; default-src none; font-src https://cdn.mybank.net; frame-src self; img-src https://cdn.mybank.net; media-src https://cdn.mybank.net; object-src https://cdn.mybank.net; report-uri /my_amazing_csp_report_parser; sandbox allow-scripts; script-src https://cdn.mybank.net; style-src https://cdn.mybank.net')
-    expect(headers['Content-Security-Policy-Report-Only']).to be_nil
-  end
+      respond_to :html, :mobile, :json
     
-          moo = MiniTest::Mock.new
-      moo.expect(:multi, [0, 1])
-      beef = ConnectionPool.new(size: 1) { moo }
+    def commit_details
+  { :message => 'Did something at #{Time.now}',
+    :name    => 'Tom Preston-Werner',
+    :email   => 'tom@github.com' }
+end
     
-    describe 'DeadSet' do
-  def dead_set
-    Sidekiq::DeadSet.new
-  end
-    
-      it 'allows until delayed scheduling of AR class methods' do
-    ss = Sidekiq::ScheduledSet.new
-    assert_equal 0, ss.size
-    MyModel.delay_until(1.day.from_now).long_class_method
-    assert_equal 1, ss.size
-  end
-    
-        DirectWorker.perform_one
-    assert_equal 1, DirectWorker.jobs.size
+        assert_no_match /Delete this Page/, last_response.body, ''Delete this Page' link not blocked in page template'
+    assert_no_match /New/,              last_response.body, ''New' button not blocked in page template'
+    assert_no_match /Upload/,           last_response.body, ''Upload' link not blocked in page template'
+    assert_no_match /Rename/,           last_response.body, ''Rename' link not blocked in page template'
+    assert_no_match /Edit/,             last_response.body, ''Edit' link not blocked in page template'
