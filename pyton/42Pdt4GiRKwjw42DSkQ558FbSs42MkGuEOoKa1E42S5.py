e7f00bc430da69560ac231994cc4913b91088537
SUNsung <weatherius@gmail.com>
2019-09-06 15:58:28 +0300

    Rewrite [42Pdt4GiRKwjw42DSkQ558FbSs42MkGuEOoKa1E42S5.py] File in: 2019-09-06 15:58:27

--- pyton/42Pdt4GiRKwjw42DSkQ558FbSs42MkGuEOoKa1E42S5.py
+++ pyton/42Pdt4GiRKwjw42DSkQ558FbSs42MkGuEOoKa1E42S5.py
@@ -1,169 +1,252 @@
 
         
-        password = key + 16 * [0]
-new_key = aes_encrypt(password, key_expansion(password)) * (32 // 16)
-r = openssl_encode('aes-256-ctr', new_key, iv)
-print('aes_decrypt_text 32')
-print(repr(r))
+            def extract_url(self, line):
+        '''Extract the generated url from the log line.'''
+        pass
+    
+    
+class LookupService(object):
+    
+            # Returns
+            A tensor (or list of tensors if the layer has multiple inputs).
+        '''
+        return self._get_node_attribute_at_index(node_index,
+                                                 'input_tensors',
+                                                 'input')
+    
+    
+# Playing with these hyperparameters will also allow you to achieve new effects
+step = 0.01  # Gradient ascent step size
+num_octave = 3  # Number of scales at which to run gradient ascent
+octave_scale = 1.4  # Size ratio between scales
+iterations = 20  # Number of ascent steps per scale
+max_loss = 10.
+    
+    
+def compute_accuracy(y_true, y_pred):
+    '''Compute classification accuracy with a fixed threshold on distances.
+    '''
+    pred = y_pred.ravel() < 0.5
+    return np.mean(pred == y_true)
+    
+    
+def test_conv_input_length():
+    assert conv_utils.conv_input_length(None, 7, 'same', 1) is None
+    assert conv_utils.conv_input_length(112, 7, 'same', 1) == 112
+    assert conv_utils.conv_input_length(112, 7, 'same', 2) == 223
+    assert conv_utils.conv_input_length(28, 5, 'valid', 1) == 32
+    assert conv_utils.conv_input_length(14, 5, 'valid', 2) == 31
+    assert conv_utils.conv_input_length(36, 5, 'full', 1) == 32
+    assert conv_utils.conv_input_length(18, 5, 'full', 2) == 31
+    
+            if self.go_backwards:
+            # Reverse time axis.
+            inputs = K.reverse(inputs, 1)
+        output, states = self._process_batch(inputs, initial_state)
+    
+        @interfaces.legacy_embedding_support
+    def __init__(self, input_dim, output_dim,
+                 embeddings_initializer='uniform',
+                 embeddings_regularizer=None,
+                 activity_regularizer=None,
+                 embeddings_constraint=None,
+                 mask_zero=False,
+                 input_length=None,
+                 **kwargs):
+        if 'input_shape' not in kwargs:
+            if input_length:
+                kwargs['input_shape'] = (input_length,)
+            else:
+                kwargs['input_shape'] = (None,)
+        super(Embedding, self).__init__(**kwargs)
+    
+    # Author: Arthur Mensch <arthur.mensch@m4x.org>
+# License: BSD 3 clause
+    
+    
+def inplace_csr_row_scale(X, scale):
+    ''' Inplace row scaling of a CSR matrix.
+    
+    for i, n in enumerate(n_samples):
+    for j, p in enumerate(n_features):
+        X = np.random.normal(size=(n, p))
+        t0 = time.time()
+        ward.fit(X)
+        scikits_time[j, i] = time.time() - t0
+        t0 = time.time()
+        hierarchy.ward(X)
+        scipy_time[j, i] = time.time() - t0
+    
+        dim = start_dim
+    for i in range(0, n):
+        print('============================================')
+        print('Entering iteration %s of %s' % (i, n))
+        print('============================================')
+        dim += step
+        X = np.random.randn(100, dim)
+        Y = np.random.randint(0, n_classes, (100,))
+        bench_scikit_tree_classifier(X, Y)
+        Y = np.random.randn(100)
+        bench_scikit_tree_regressor(X, Y)
+    
+    plt.matshow(np.outer(np.sort(model.row_labels_) + 1,
+                     np.sort(model.column_labels_) + 1),
+            cmap=plt.cm.Blues)
+plt.title('Checkerboard structure of rearranged data')
+    
+    # Author: Kemal Eren <kemal@kemaleren.com>
+# License: BSD 3 clause
+    
+    plt.subplot(3, 4, 10)
+plt.imshow(np.reshape(agglo.labels_, images[0].shape),
+           interpolation='nearest', cmap=plt.cm.nipy_spectral)
+plt.xticks(())
+plt.yticks(())
+plt.title('Labels')
+plt.show()
 
     
     
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
+# Create child splits for a node or make terminal  # 创建子分割器，递归分类，直到分类结束
+def split(node, max_depth, min_size, n_features, depth):  # max_depth = 10, min_size = 1, n_features=int(sqrt((len(dataset[0])-1)
+    left, right = node['groups']
+    del(node['groups'])
+# check for a no split
+    if not left or not right:
+        node['left'] = node['right'] = to_terminal(left + right)
+        return
+# check for max depth
+    if depth >= max_depth:   # max_depth=10 表示递归十次，若分类还未结束，则选取数据中分类标签较多的作为结果，使分类提前结束，防止过拟合
+        node['left'], node['right'] = to_terminal(left), to_terminal(right)
+        return
+# process left child
+    if len(left) <= min_size:
+        node['left'] = to_terminal(left)
+    else:
+        node['left'] = get_split(left, n_features)  # node['left']是一个字典，形式为{'index':b_index, 'value':b_value, 'groups':b_groups}，所以node是一个多层字典
+        split(node['left'], max_depth, min_size, n_features, depth+1)  # 递归，depth+1计算递归层数
+# process right child
+    if len(right) <= min_size:
+        node['right'] = to_terminal(right)
+    else:
+        node['right'] = get_split(right, n_features)
+        split(node['right'], max_depth, min_size, n_features, depth+1)
     
     
-def build_completion(opt_parser):
-    opts = [opt for group in opt_parser.option_groups
-            for opt in group.option_list]
-    opts_file = [opt for opt in opts if opt.metavar == 'FILE']
-    opts_dir = [opt for opt in opts if opt.metavar == 'DIR']
-    
-            if 'skip' in test_case:
-            print_skipping(test_case['skip'])
-            return
-        for other_ie in other_ies:
-            if not other_ie.working():
-                print_skipping('test depends on %sIE, marked as not WORKING' % other_ie.ie_key())
-                return
-    
-        def test_secondary_proxy_http(self):
-        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL()
-        req = compat_urllib_request.Request('http://yt-dl.org/ip')
-        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
-        self.assertEqual(
-            ydl.urlopen(req).read().decode('utf-8'),
-            params['secondary_server_ip'])
-    
-                    fn = os.path.join(dirpath, basename)
-                with io.open(fn, encoding='utf-8') as inf:
-                    code = inf.read()
-    
-    
-@pytest.fixture(params=[(python_3, False),
-                        (python_3, True),
-                        (python_2, False)])
-def proc(request, spawnu, TIMEOUT):
-    container, instant_mode = request.param
-    proc = spawnu(*container)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(init_bashrc.format(
-        u'--enable-experimental-instant-mode' if instant_mode else ''))
-    proc.sendline(u'bash')
-    if instant_mode:
-        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
-    return proc
+class ConvLayer(object):
+    '''
+    Desc:
+        用ConvLayer类来实现一个卷积层。下面的代码是初始化一个卷积层，可以在构造函数中设置卷积层的超参数。
+    '''
+    def __init__(self, input_width, input_height, 
+                 channel_number, filter_width, 
+                 filter_height, filter_number, 
+                 zero_padding, stride, activator,
+                 learning_rate):
+        self.input_width = input_width
+        self.input_height = input_height
+        self.channel_number = channel_number
+        self.filter_width = filter_width
+        self.filter_height = filter_height
+        self.filter_number = filter_number
+        self.zero_padding = zero_padding
+        self.stride = stride
+        self.output_width = \
+            ConvLayer.calculate_output_size(
+            self.input_width, filter_width, zero_padding,
+            stride)
+        self.output_height = \
+            ConvLayer.calculate_output_size(
+            self.input_height, filter_height, zero_padding,
+            stride)
+        self.output_array = np.zeros((self.filter_number, 
+            self.output_height, self.output_width))
+        self.filters = []
+        for i in range(filter_number):
+            self.filters.append(Filter(filter_width, 
+                filter_height, self.channel_number))
+        self.activator = activator
+        self.learning_rate = learning_rate
     
     
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
+# 将图像画出来
+def plot(linear_unit):
+    '''
+    Desc:
+        将我们训练好的线性单元对数据的分类情况作图画出来
+    Args:
+        linear_unit —— 训练好的线性单元
+    Returns:
+        None
+    '''
+    # 引入绘图的库
+    import matplotlib.pyplot as plt
+    # 获取训练数据：特征 input_vecs 与 对应的标签 labels
+    input_vecs, labels = get_training_dataset()
+    # figure() 创建一个 Figure 对象，与用户交互的整个窗口，这个 figure 中容纳着 subplots
+    fig = plt.figure()
+    # 在 figure 对象中创建 1行1列中的第一个图
+    ax = fig.add_subplot(111)
+    # scatter(x, y) 绘制散点图，其中的 x,y 是相同长度的数组序列
+    ax.scatter(map(lambda x: x[0], input_vecs), labels)
+    # 设置权重
+    weights = linear_unit.weights
+    # 设置偏置项
+    bias = linear_unit.bias
+    # range(start, stop, step) 从 start 开始，到 stop 结束，步长为 step
+    x = range(0, 12, 1)
+    # 计算感知器对输入计算得到的值
+    y = map(lambda x: weights[0] * x + bias, x)
+    # 将图画出来
+    ax.plot(x, y)
+    # 将最终的图展示出来
+    plt.show()
     
-        assert match(command)
-    
-    
-@pytest.mark.parametrize('script, output, help_text, result', [
-    ('apt-get isntall vim', invalid_operation('isntall'),
-     apt_get_help, 'apt-get install vim'),
-    ('apt saerch vim', invalid_operation('saerch'),
-     apt_help, 'apt search vim'),
-])
-def test_get_new_command(set_help, output, script, help_text, result):
-    set_help(help_text)
-    assert get_new_command(Command(script, output))[0] == result
-
-    
-    match_output = '''
-Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
-Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
-Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
-Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
-Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
-Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
-Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
-Fetched 89.2 kB in 0s (122 kB/s)
-Reading package lists... Done
-Building dependency tree
-Reading state information... Done
-8 packages can be upgraded. Run 'apt list --upgradable' to see them.
-'''
-    
-    
-misspelled_subcommand_with_multiple_options = '''\
-usage: aws [options] <command> <subcommand> [<subcommand> ...] [parameters]
-To see help text, you can run:
-    
-        out1 = utils.preprocess_input(x, 'channels_last')
-    out1int = utils.preprocess_input(xint, 'channels_last')
-    out2 = utils.preprocess_input(np.transpose(x, (2, 0, 1)),
-                                  'channels_first')
-    out2int = utils.preprocess_input(np.transpose(xint, (2, 0, 1)),
-                                     'channels_first')
-    assert_allclose(out1, out2.transpose(1, 2, 0))
-    assert_allclose(out1int, out2int.transpose(1, 2, 0))
-    
-    model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-    
-    Linkfix - a companion to sphinx's linkcheck builder.
-    
-        def __enter__(self):
-        from scrapy.utils.test import get_testenv
-        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
-        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                     env=get_testenv())
-        self.proc.stdout.readline()
-    
-    
-    
-            self.crawler_process.crawl(spname, **opts.spargs)
-        self.crawler_process.start()
-    
-            assertion = (self.min_bound <= occurrences <= self.max_bound)
-    
-        def handleResponse(self, response):
-        if self.factory.method.upper() == b'HEAD':
-            self.factory.page(b'')
-        elif self.length is not None and self.length > 0:
-            self.factory.noPage(self._connection_lost_reason)
+        # 针对每一行的数据
+    # 最大的key,  添加
+    Args:
+        items       满足minSup 排序后的元素key的数组（大到小的排序）
+        inTree      空的Tree对象
+        headerTable 满足minSup {所有的元素+(value, treeNode)}
+        count       原数据集中每一组Kay出现的次数
+    '''
+    # 取出 元素 出现次数最高的
+    # 如果该元素在 inTree.children 这个字典中，就进行累加
+    # 如果该元素不存在 就 inTree.children 字典中新增key，value为初始化的 treeNode 对象
+    if items[0] in inTree.children:
+        # 更新 最大元素，对应的 treeNode 对象的count进行叠加
+        inTree.children[items[0]].inc(count)
+    else:
+        # 如果不存在子节点，我们为该inTree添加子节点
+        inTree.children[items[0]] = treeNode(items[0], count, inTree)
+        # 如果满足minSup的dist字典的value值第二位为null， 我们就设置该元素为 本节点对应的tree节点
+        # 如果元素第二位不为null，我们就更新header节点
+        if headerTable[items[0]][1] is None:
+            # headerTable只记录第一次节点出现的位置
+            headerTable[items[0]][1] = inTree.children[items[0]]
         else:
-            self.factory.page(response)
-        self.transport.loseConnection()
+            # 本质上是修改headerTable的key对应的Tree，的nodeLink值
+            updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
+    if len(items) > 1:
+        # 递归的调用，在items[0]的基础上，添加item0[1]做子节点， count只要循环的进行累计加和而已，统计出节点的最后的统计值。
+        updateTree(items[1:], inTree.children[items[0]], headerTable, count)
     
-        def encode(self, value):
-        if value != self.value:
-            logger.warning(
-                'Overriding fixed field (%s) with %r', self.json_name, value)
-        return value
+            最后，我们可能会注意到有一些小的负值，他们主要源自数值误差应该四舍五入成0.
+        '''
+        print('主成分：%s, 方差占比：%s%%, 累积方差占比：%s%%' % (format(i+1, '2.0f'), format(line_cov_score/cov_all_score*100, '4.2f'), format(sum_cov_score/cov_all_score*100, '4.1f')))
     
-            self.assertFalse(self.addr_defined.conflicts(self.addr1))
-        self.assertFalse(self.addr_defined.conflicts(self.addr2))
-        self.assertFalse(self.addr_defined.conflicts(self.addr))
-        self.assertFalse(self.addr_defined.conflicts(self.addr_default))
+        def __init__(self, *args, **kwargs):
+        super(MRsvm, self).__init__(*args, **kwargs)
+        self.data = pickle.load(open('/opt/git/MachineLearnidata/15.BigData_MapReduce/svmDat27'))
+        self.w = 0
+        self.eta = 0.69
+        self.dataList = []
+        self.k = self.options.batchsize
+        self.numMappers = 1
+        self.t = 1  # iteration number
     
-    Certbot will emit a warning if it detects that the credentials file can be
-accessed by other users on your system. The warning reads 'Unsafe permissions
-on credentials configuration file', followed by the path to the credentials
-file. This warning will be emitted each time Certbot uses the credentials file,
-including for renewal, and cannot be silenced except by addressing the issue
-(e.g., by using a command like ``chmod 600`` to restrict access to the file).
     
-    Certbot will emit a warning if it detects that the credentials file can be
-accessed by other users on your system. The warning reads 'Unsafe permissions
-on credentials configuration file', followed by the path to the credentials
-file. This warning will be emitted each time Certbot uses the credentials file,
-including for renewal, and cannot be silenced except by addressing the issue
-(e.g., by using a command like ``chmod 600`` to restrict access to the file).
+def map(key, value):
+   # input key= class for one training example, e.g. '-1.0'
+   classes = [float(item) for item in key.split(',')]   # e.g. [-1.0]
+   D = numpy.diag(classes)
