c6d4f7f80a2c6859bc8878e9f738ff33d9b3d8d4
SUNsung <weatherius@gmail.com>
2019-09-07 03:19:49 +0300

    Rewrite [22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb] File in: 2019-09-07 03:19:48

--- ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
+++ ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
@@ -1,158 +1,218 @@
 
         
-            let!(:bob_formatting_agent) {
-      agents(:bob_formatting_agent).tap { |agent|
-        # Make this valid
-        agent.options['instructions']['foo'] = 'bar'
-        agent.save!
-      }
-    }
-    
-        describe '#agents_dot' do
-      before do
-        @agents = [
-          @foo = Agents::DotFoo.new(name: 'foo').tap { |agent|
-            agent.user = users(:bob)
-            agent.save!
-          },
-    
-    end
+          def primary_group_lookup
+    @primary_group_lookup ||= options[:primary_group_lookup] || PrimaryGroupLookup.new(user_ids)
+  end
+end
 
     
-      describe '.seed' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+      context 'when plugin registered fallback locale' do
+    before do
+      DiscoursePluginRegistry.register_locale('es_MX', fallbackLocale: 'es')
+      DiscoursePluginRegistry.register_locale('de_AT', fallbackLocale: 'de')
     end
     
-        it 'does not output links to other agents outside of the incoming set' do
-      Link.create!(:source_id => agents(:jane_weather_agent).id, :receiver_id => agents(:jane_website_agent).id)
-      Link.create!(:source_id => agents(:jane_website_agent).id, :receiver_id => agents(:jane_rain_notifier_agent).id)
+            upload = Fabricate(:upload)
+        settings.logo = upload
+        settings.refresh!
+        setting = settings.all_settings.last
     
-      let :new_extract do
-    {
-      'url' => { 'css' => '#comic img', 'value' => '@src' },
-      'title' => { 'css' => '#comic img', 'value' => '@alt' },
-      'hovertext' => { 'css' => '#comic img', 'value' => '@title', 'hidden' => true }
-    }
-  end
-    
-        it 'cleans up old logs when there are more than log_length' do
-      stub(AgentLog).log_length { 4 }
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 1')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 2')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 3')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 4')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 4')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 1')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 5')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 5')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 2')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 6')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 6')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 3')
-    end
-    
-        it 'should generate the correct last checkpoint url' do
-      @checker.options['path'] = 'last_checkpoint/usps/9361289878905919630610'
-      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/last_checkpoint/usps/9361289878905919630610')
-    end
-  end
-    
-        @a.rw = 'W'
-    
-        refute @co.suppressed?
-  end
-    
-      def header
-    <<-'HEADER'
-# SOME DESCRIPTIVE TITLE.
-# Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER
-# This file is distributed under the same license as the PACKAGE package.
-# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
-#, fuzzy
-msgid ''
-msgstr ''
-'Project-Id-Version: PACKAGE VERSEION\n'
-'Report-Msgid-Bugs-To:\n'
-'PO-Revision-Date: YEAR-MO_DA HO:MI+ZONE\n'
-'Last-Translator: FULL NAME <EMAIL@ADDRESS>\n'
-'Language-Team: LANGUAGE <LL@li.org>\n'
-'Language:\n'
-'MIME-Version: 1.0\n'
-'Content-Type: text/plain; charset=CHARSET\n'
-'Content-Transfer-Encoding: 8bit\n'
-'Plural-Forms: nplurals=INTEGER; plural=EXPRESSION;\n'
-    HEADER
-  end
-    
-      def test_references_multiple
-    options = {:references => [['lib/rdoc.rb', 29], ['lib/rdoc/i18n.rb', 9]]}
-    assert_equal <<-'ENTRY', entry('Hello', options).to_s
-#: lib/rdoc.rb:29
-#: lib/rdoc/i18n.rb:9
-msgid 'Hello'
-msgstr ''
-    ENTRY
-  end
-    
-      def test_module_extended
-    m1 = @xref_data.add_module RDoc::NormalModule, 'Mod1'
-    m1_m3 = m1.add_module RDoc::NormalModule, 'Mod3'
-    m1_m2 = m1.add_module RDoc::NormalModule, 'Mod2'
-    m1_m2_m3 = m1_m2.add_module RDoc::NormalModule, 'Mod3'
-    m1_m2_m3_m4 = m1_m2_m3.add_module RDoc::NormalModule, 'Mod4'
-    m1_m2_m4 = m1_m2.add_module RDoc::NormalModule, 'Mod4'
-    m1_m2_k0 = m1_m2.add_class RDoc::NormalClass, 'Klass0'
-    m1_m2_k0_m4 = m1_m2_k0.add_module RDoc::NormalModule, 'Mod4'
-    #m1_m2_k0_m4_m5 = m1_m2_k0_m4.add_module RDoc::NormalModule, 'Mod5'
-    m1_m2_k0_m4_m6 = m1_m2_k0_m4.add_module RDoc::NormalModule, 'Mod6'
-    m1_m2_k0_m5 = m1_m2_k0.add_module RDoc::NormalModule, 'Mod5'
-    
-    Then(/^the current symlink points to that specific release$/) do
-  specific_release_path = TestApp.releases_path.join(@rollback_release)
-    
-          def load_built_in_scm
-        require 'capistrano/scm/#{scm_name}'
-        scm_class = Object.const_get(built_in_scm_plugin_class_name)
-        # We use :load_immediately because we are initializing the SCM plugin
-        # late in the load process and therefore can't use the standard
-        # load:defaults technique.
-        install_plugin(scm_class, load_immediately: true)
+          rounded_new_value = if new_value.is_a?(Integer)
+        if new_value > 9
+          digits = new_value.digits.reverse
+          (digits[0] * 10 + digits[1]) * 10.pow(digits[2..-1].size)
+        else
+          new_value
+        end
+      else
+        new_value.round(2)
       end
     
-          def primary
-        self if fetch(:primary)
+          it 'changes settings when difference is significant' do
+        expect {
+          update_settings
+        }.to change { UserHistory.count }.by(6)
+        expect(SiteSetting.topic_views_heat_high).to eq(10_000)
+        expect(SiteSetting.topic_views_heat_medium).to eq(4000)
+        expect(SiteSetting.topic_views_heat_low).to eq(2000)
+        expect(SiteSetting.topic_post_like_heat_high).to eq(2.2)
+        expect(SiteSetting.topic_post_like_heat_medium).to eq(1.07)
+        expect(SiteSetting.topic_post_like_heat_low).to eq(0.55)
       end
     
-          def roles_for(names)
-        options = extract_options(names)
-        s = Filter.new(:role, names).filter(servers_by_key.values)
-        s.select { |server| server.select?(options) }
+        converted_text = replace_duplicate_links(text)
+    converted_text = escape_special_characters(converted_text)
+    converted_text = indent_with_non_breaking_spaces(converted_text)
+    converted_text
+  end
+    
+        describe '#provisioning_profiles_via_xcode_api' do
+      it 'makes a call to the developer portal API' do
+        profiles = subject.provisioning_profiles_via_xcode_api
+        expect(profiles).to be_instance_of(Array)
+        expect(profiles.sample.keys).to include('provisioningProfileId',
+                                                'name',
+                                                'status',
+                                                'type',
+                                                'distributionMethod',
+                                                'proProPlatform',
+                                                'version',
+                                                'dateExpire',
+                                                # 'managingApp', not all profiles have it
+                                                'deviceIds',
+                                                'appId',
+                                                'certificateIds')
+        expect(a_request(:post, /developerservices2.apple.com/)).to have_been_made
       end
-    
-          # Decorate Variables#set to add validation behavior.
-      def set(key, value=nil, &block)
-        assert_value_or_block_not_both(value, block)
-    
-          extend Tmuxinator::WemuxSupport if wemux?
     end
     
-          @name = first_key.to_s.shellescape unless first_key.nil?
-      @yaml = window_yaml.values.first
-      @project = project
-      @index = index
-      @commands = build_commands(tmux_window_command_prefix, @yaml)
-    end
-    
-    require 'tmuxinator'
-require 'factory_bot'
-    
-            it 'returns two panes in an Array' do
-          expect(window.panes).to match [
-            a_pane.with(index: 0).and_commands('vim'),
-            a_pane.with(index: 1).and_commands(command1, command2)
-          ]
+            # Stub Time.now to return current time on first call and 6 minutes later on second
+        before { allow(Time).to receive(:now).and_return(time_now, (time_now + 60 * 6)) }
+        it 'throws a UI error' do
+          allow(fake_app).to receive(:latest_version).and_return(fake_version)
+          allow(fake_version).to receive(:candidate_builds).and_return([])
+          expect do
+            review_submitter.wait_for_build(fake_app, '1.2.3')
+          end.to raise_error(FastlaneCore::Interface::FastlaneError, 'Could not find any available candidate builds on App Store Connect to submit')
         end
       end
+    
+          def resolve_result_bundle_path(language, locale)
+        Snapshot.cache[:result_bundle_path] = {}
+        language_key = locale || language
+    
+            return parts
+      end
+    
+        ARGS_MAP = {
+      workspace: '-w',
+      project: '-p',
+      configuration: '-c',
+      scheme: '-s',
+      clean: '--clean',
+      archive: '--archive',
+      destination: '-d',
+      embed: '-m',
+      identity: '-i',
+      sdk: '--sdk',
+      ipa: '--ipa',
+      xcconfig: '--xcconfig',
+      xcargs: '--xcargs'
+    }
+    
+      def auth_options
+    { scope: resource_name, recall: '#{controller_path}#new' }
+  end
+    
+      # Gets the actual resource stored in the instance variable
+  def resource
+    instance_variable_get(:'@#{resource_name}')
+  end
+    
+          generate_helpers!(Devise::URL_HELPERS)
+    
+          klass.devise_modules.each do |mod|
+        constant = const_get(mod.to_s.classify)
+    
+          module ClassMethods
+        Devise::Models.config(self, :timeout_in)
+      end
     end
   end
+end
+
+    
+      def wrap_aes_socket(sock)
+    if datastore['PAYLOAD'] !~ /java\// or (datastore['AESPassword'] || '') == ''
+      return sock
+    end
+    
+    
+    {        This will not work on circa 2009 and older Debian-based Linux
+        distributions (including Ubuntu) because they compile bash
+        without the /dev/tcp feature.
+      },
+      'Author'        => 'hdm',
+      'License'       => MSF_LICENSE,
+      'Platform'      => 'unix',
+      'Arch'          => ARCH_CMD,
+      'Handler'       => Msf::Handler::ReverseTcp,
+      'Session'       => Msf::Sessions::CommandShell,
+      'PayloadType'   => 'cmd_bash',
+      'RequiredCmd'   => 'bash-tcp',
+      'Payload'       =>
+        {
+          'Offsets' => { },
+          'Payload' => ''
+        }
+      ))
+  end
+    
+    # Wrappers
+require 'msf/core/encoded_payload'
+    
+      def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name'            => 'LNK Code Execution Vulnerability',
+        'Description'     => %q{
+          This module exploits a vulnerability in the handling of Windows Shortcut files (.LNK)
+          that contain a dynamic icon, loaded from a malicious DLL.
+    }
+    
+        com_opts = {}
+    com_opts[:net_clr] = 4.0 # Min .NET runtime to load into a PS session
+    com_opts[:target] = datastore['OUTPUT_TARGET'] || session.sys.config.getenv('TEMP') + '\\#{ Rex::Text.rand_text_alpha(rand(8)+8) }.exe'
+    com_opts[:payload] = payload_script #payload.encoded
+    vprint_good com_opts[:payload].length.to_s
+    
+    desc 'copy dot files for deployment'
+task :copydot, :source, :dest do |t, args|
+  FileList['#{args.source}/**/.*'].exclude('**/.', '**/..', '**/.DS_Store', '**/._*').each do |file|
+    cp_r file, file.gsub(/#{args.source}/, '#{args.dest}') unless File.directory?(file)
+  end
+end
+    
+        # Outputs a single category as an <a> link.
+    #
+    #  +category+ is a category string to format as an <a> link
+    #
+    # Returns string
+    #
+    def category_link(category)
+      dir = @context.registers[:site].config['category_dir']
+      '<a class='category' href='/#{dir}/#{category.to_url}/'>#{category}</a>'
+    end
+    
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+    
+      class IncludeArrayTag < Liquid::Tag
+    Syntax = /(#{Liquid::QuotedFragment}+)/
+    def initialize(tag_name, markup, tokens)
+      if markup =~ Syntax
+        @array_name = $1
+      else
+        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
+      end
+    
+    
+    
+      class VideoTag < Liquid::Tag
+    @video = nil
+    @poster = ''
+    @height = ''
+    @width = ''
+    
+      it 'calls Hooks.commands_from' do
+    expect(Tmuxinator::Hooks).to receive(:commands_from).
+      with(kind_of(Tmuxinator::Project), hook_name).once
+    project.send('hook_#{hook_name}')
+  end
+    
+        context 'when the file doesn't exist' do
+      before do
+        allow(File).to receive(:exist?).with(local_default) { false }
+        allow(File).to receive(:exist?).with(proj_default) { false }
+      end
