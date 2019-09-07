8cfb956f2fcbf5e522d5af68daa0d0362edab910
SUNsung <weatherius@gmail.com>
2019-09-07 17:06:57 +0300

    Rewrite [352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp] File in: 2019-09-07 17:06:57

--- cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
+++ cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
@@ -1,219 +1,89 @@
 
         
-        #include <grpc/support/log.h>
-    
-    
-    {}  // namespace grpc
-    
-    #endif  // STORAGE_LEVELDB_UTIL_ENV_POSIX_TEST_HELPER_H_
-
-    
-    class WritableFile;
-    
-    #ifndef STORAGE_LEVELDB_DB_VERSION_SET_H_
-#define STORAGE_LEVELDB_DB_VERSION_SET_H_
-    
-      // The size of the database changes caused by this batch.
-  //
-  // This number is tied to implementation details, and may change across
-  // releases. It is intended for LevelDB usage metrics.
-  size_t ApproximateSize() const;
-    
-    Rational RationalMath::ASin(Rational const& rat, ANGLE_TYPE angletype)
-{
-    PRAT prat = rat.ToPRAT();
+        template <class T, bool Parallel>
+class DynamicStitchOpImplCPU : public DynamicStitchOpImplBase<T> {
+ public:
+  explicit DynamicStitchOpImplCPU(OpKernelConstruction* c)
+      : DynamicStitchOpImplBase<T>(
+            c, (Parallel ? 'ParallelDynamicStitchOp' : 'DynamicStitchOp')) {}
     }
     
-    static constexpr wchar_t DEFAULT_DEC_SEPARATOR = L'.';
-static constexpr wchar_t DEFAULT_GRP_SEPARATOR = L',';
-static constexpr wstring_view DEFAULT_GRP_STR = L'3;0';
-static constexpr wstring_view DEFAULT_NUMBER_STR = L'0';
+    #include 'tensorflow/lite/c/c_api_internal.h'
+#include 'tensorflow/lite/tools/evaluation/stages/image_classification_stage.h'
     
-    StandardPeers::AutomationNotificationKind NotificationHost::GetWindowsNotificationKind(CustomPeers::AutomationNotificationKind customKindType)
-{
-    switch (customKindType)
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    
+    {}  // namespace
+    
+      // For convolution and its grad ops.
+  static ConvolutionDimensions ConvolutionDimensionsFromInputs(
+      const TensorShapeProto& original_image_shape,
+      const TensorShapeProto& original_filter_shape, const OpInfo& op_info,
+      bool* found_unknown_shapes);
+    
+      void Compute(OpKernelContext* c) override {
+    const Tensor& params = c->input(0);
+    const Tensor& indices = c->input(1);
+    }
+    
+    #include 'KeyChord.g.cpp'
+    
+    
     {
-    case CustomPeers::AutomationNotificationKind::ItemAdded:
-        return StandardPeers::AutomationNotificationKind::ItemAdded;
-    }
-    }
-    
-    #pragma once
-    
-    
-    {        auto maskMatrix = GetMatrix();
-        size_t rowOffset = offset[0];
-        size_t colOffset = offset[1];
-        size_t sliceRowLength = (shape[0] != NDShape::InferredDimension) ? shape[0] : (maskMatrix->GetNumRows() - rowOffset);
-        size_t sliceColLength = (shape[1] != NDShape::InferredDimension) ? shape[1] : (maskMatrix->GetNumCols() - colOffset);
-        if ((rowOffset == 0) && (sliceRowLength == maskMatrix->GetNumRows()))
-            maskMatrix->ColumnSlice(colOffset, sliceColLength).SetValue((char)maskKind);
-        else
-        {
-            // Since Matrix does not support strides in the row dimension, we will need to create separate slices for each column
-            for (size_t i = colOffset; i < (colOffset + sliceColLength); ++i)
-            {
-                auto column = maskMatrix->ColumnSlice(i, 1);
-                column.Reshape(1, maskMatrix->GetNumRows());
-                column.ColumnSlice(rowOffset, sliceRowLength).SetValue((char)maskKind);
-            }
+    {            codeCount[codeLen]++;
         }
     }
     
-        template <typename T> 
-    inline std::string GetVersionsString(size_t currentVersion, size_t dictVersion)
-    {
-        std::stringstream info;
-        info << 'Current ' << Typename<T>() << ' version = ' << currentVersion 
-             << ', Dictionary version = ' << dictVersion;
-        return info.str();
-    }
     
-        private:
-        bool m_isReadOnly;
-        NDShape m_sampleShape;
-        std::vector<Axis> m_sampleDynamicAxes;
-        NDShape m_unpackedShape;
-    
-    
-    {
-    {    private:
-        // Disallow copy and move construction and assignment
-        VariableFields(const VariableFields&) = delete; VariableFields& operator=(const VariableFields& other) = delete; VariableFields(VariableFields&&) = delete; VariableFields& operator=(VariableFields&&) = delete;
-    };
-}
+OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_EXIT 
 
     
-    // GetHmmData - Get the HMM definition for SE training
-// hmm - HMM definition
-// returns - true if succeed
-bool DataReader::GetHmmData(msra::asr::simplesenonehmm* hmm)
-{
-    bool bRet = true;
-    for (size_t i = 0; i < m_ioNames.size(); i++)
-        bRet &= m_dataReaders[m_ioNames[i]]->GetHmmData(hmm);
-    return bRet;
-}
+    #include 'ImfAttribute.h'
     
-            if (cols0 * wordsInEachSample != rows1)
-            LogicError('LookupTableNode: rows of input 1 is not a multiple of cols of input 0. This usually happens when the feature dimension is not specified as that in the network definition of look-up-table dimension size.');
-    
-    using json = nlohmann::json;
-    
-      if (client_options_.timeout_) {
-    timer_.async_wait(
-        [=](boost_system::error_code const& ec) { timeoutHandler(ec); });
-  }
-    
-    enum class RocksdbMigrationError {
-  InvalidArgument = 1,
-  FailToOpen = 2,
-  FailToGetVersion = 3,
-  NoMigrationFromCurrentVersion = 5,
-  MigrationLogicError = 6,
-  FailToOpenSrcDatabase = 7,
-  FailMoveDatabase = 8,
-};
-    
-    Status LoggerPlugin::call(const PluginRequest& request,
-                          PluginResponse& response) {
-  std::vector<StatusLogLine> intermediate_logs;
-  if (request.count('string') > 0) {
-    return this->logString(request.at('string'));
-  } else if (request.count('snapshot') > 0) {
-    return this->logSnapshot(request.at('snapshot'));
-  } else if (request.count('init') > 0) {
-    deserializeIntermediateLog(request, intermediate_logs);
-    this->setProcessName(request.at('init'));
-    this->init(this->name(), intermediate_logs);
-    return Status(0);
-  } else if (request.count('status') > 0) {
-    deserializeIntermediateLog(request, intermediate_logs);
-    return this->logStatus(intermediate_logs);
-  } else if (request.count('event') > 0) {
-    return this->logEvent(request.at('event'));
-  } else if (request.count('action') && request.at('action') == 'features') {
-    size_t features = 0;
-    features |= (usesLogStatus()) ? LOGGER_FEATURE_LOGSTATUS : 0;
-    features |= (usesLogEvent()) ? LOGGER_FEATURE_LOGEVENT : 0;
-    return Status(static_cast<int>(features));
-  } else {
-    return Status(1, 'Unsupported call to logger plugin');
-  }
-}
+    #include <vector>
     
     
-    {  /**
-   * @brief The host identifier at the time when logs are flushed.
-   *
-   * There is occasionally a delay between logging a status and decorating
-   * with the host identifier. In most cases the identifier is static so this
-   * does not matter. In some cases the host identifier causes database lookups.
-   */
-  std::string identifier;
-};
+    //---------------------------------------------------------------------
+    // Memory layout:  The address of pixel (x, y) is
+    //
+    //  base + (xp / xSampling) * xStride + (yp / ySampling) * yStride
+    //
+    // where xp and yp are computed as follows:
+    //
+    //  * If we are reading or writing a scanline-based file:
+    //
+    //      xp = x
+    //      yp = y
+    //
+    //  * If we are reading a tile whose upper left coorner is at (xt, yt):
+    //
+    //      if xTileCoords is true then xp = x - xt, else xp = x
+    //      if yTileCoords is true then yp = y - yt, else yp = y
+    //
+    //---------------------------------------------------------------------
+    
+        IMF_EXPORT
+    float &			screenWindowWidth ();
+    IMF_EXPORT
+    const float &		screenWindowWidth () const;
+    
+        IMF_EXPORT
+    IStream (const char fileName[]);
     
     
-    {} // namespace osquery
-
-    
-    #include 'modules/drivers/canbus/can_client/esd/esd_can_client.h'
-    
-    
-    {  SocketCanClientRaw socket_can_client;
-  EXPECT_TRUE(socket_can_client.Init(param));
-  EXPECT_EQ(socket_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
-  std::vector<CanFrame> frames;
-  int32_t num = 0;
-  EXPECT_EQ(socket_can_client.Send(frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  EXPECT_EQ(socket_can_client.Receive(&frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
-  CanFrame can_frame;
-  frames.push_back(can_frame);
-  EXPECT_EQ(socket_can_client.SendSingleFrame(frames),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  socket_can_client.Stop();
-}
-    
-    #include 'gtest/gtest.h'
-    
-    class SpeedLimitTest : public ::testing::Test {
- public:
-  virtual void SetUp() {
-    speed_limit_.Clear();
-    for (int i = 0; i < 100; ++i) {
-      std::pair<double, double> sp;
-      sp.first = i * 1.0;
-      sp.second = (i % 2 == 0) ? 5.0 : 10.0;
-      speed_limit_.AppendSpeedLimit(sp.first, sp.second);
+    {
+    {
+    {
+    {
+    {		    fromPtr += fromSlice.xStride * toSlice.xSampling;
+		    toPtr += toSlice.xStride;
+                }
+            }
+        }
     }
-  }
-    }
-    
-      MatrixXd offset_golden = MatrixXd::Zero(10, 1);
-  offset_golden(0, 0) = -2.0 * 1.0 *
-                            (init_derivative + init_second_derivative * 0.1) /
-                            std::pow(0.1, 5) -
-                        6.0 * 1.0 * init_derivative / std::pow(0.1, 5);
-  offset_golden(1, 0) = 2.0 * 1.0 * init_derivative / std::pow(0.1, 5);
-    
-    TEST_F(Accelrpt68Test, reset) {
-  Accelrpt68 acc;
-  int32_t length = 8;
-  ChassisDetail chassis_detail;
-  uint8_t bytes[8] = {0x01, 0x02, 0x03, 0x04, 0x11, 0x12, 0x13, 0x14};
-    }
-    
-      Byte t1(bytes + 5);
-  int32_t t = t1.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-    
-    {  auto &brakerpt = chassis_detail.gem().brake_rpt_6c();
-  EXPECT_DOUBLE_EQ(brakerpt.manual_input(), 0.258);
-  EXPECT_DOUBLE_EQ(brakerpt.commanded_value(), 0.772);
-  EXPECT_DOUBLE_EQ(brakerpt.output_value(), 4.37);
-  EXPECT_EQ(brakerpt.brake_on_off(), Brake_rpt_6c::BRAKE_ON_OFF_ON);
 }
+    
+    OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_ENTER
