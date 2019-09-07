7c602630b61634f3b2938e3d07152e7b04f2cba8
SUNsung <weatherius@gmail.com>
2019-09-07 04:06:25 +0300

    Rewrite [13eOGSK2VxSGo13pB3NX2c.Gw13CA4DAedOQfA13Zyt.cpp] File in: 2019-09-07 04:06:25

--- cccccc/13eOGSK2VxSGo13pB3NX2c.Gw13CA4DAedOQfA13Zyt.cpp
+++ cccccc/13eOGSK2VxSGo13pB3NX2c.Gw13CA4DAedOQfA13Zyt.cpp
@@ -1,149 +1,39 @@
 
         
-        Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#ifndef TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
-#define TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
-    
-    
-    {  if (debug) {
-    const OpRegistrationData* op_reg_data;
-    Status status = OpRegistry::Global()->LookUp(node->op(), &op_reg_data);
-    if (!status.ok()) {
-      os << '\tCouldn't find op registration for ' << node->op() << std::endl;
-    } else if (!op_reg_data->shape_inference_fn) {
-      os << '\tCouldn't find shape function for op ' << node->op() << std::endl;
-    } else if (properties.HasInputProperties(node->name())) {
-      const std::vector<OpInfo::TensorProperties>& props =
-          properties.GetInputProperties(node->name());
-      for (int i = 0; i < props.size(); ++i) {
-        const OpInfo::TensorProperties& prop = props[i];
-        if (prop.has_value()) {
-          os << '\t'
-             << 'input ' << i << ' (' << DataTypeString(prop.dtype())
-             << ') has known value' << std::endl;
-        }
-      }
-    }
+          void visitTupleElementAddrInst(TupleElementAddrInst *TEAI) {
+    SGM.useConformancesFromType(TEAI->getOperand()->getType().getASTType());
   }
+    
+    #endif // SWIFT_SIL_PROFILER_H
+
+    
+    #if defined(__APPLE__) || defined(__FreeBSD__) || defined(__ANDROID__)
+static inline locale_t getCLocale() {
+  // On these platforms convenience functions from xlocale.h interpret nullptr
+  // as C locale.
+  return nullptr;
 }
-    
-    struct Bfloat16EqFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a == b; }
-};
-struct Bfloat16NeFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a != b; }
-};
-struct Bfloat16LtFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a < b; }
-};
-struct Bfloat16GtFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a > b; }
-};
-struct Bfloat16LeFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a <= b; }
-};
-struct Bfloat16GeFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a >= b; }
-};
-    
-    
-    {}  // namespace tensorflow
-    
-    namespace tensorflow {
-    }
-    
-    #include 'tensorflow/stream_executor/platform/port.h'
-#include 'tensorflow/stream_executor/platform/logging.h'
-    
-    namespace stream_executor {
-namespace host {
-    }
-    }
-    
-    void HostTimer::StopNow() { duration_ = clock::now() - start_time_; }
-    
-    #ifndef TENSORFLOW_STREAM_EXECUTOR_HOST_HOST_TIMER_H_
-#define TENSORFLOW_STREAM_EXECUTOR_HOST_HOST_TIMER_H_
-    
-    IMGUI_IMPL_API bool     ImGui_ImplDX9_Init(IDirect3DDevice9* device);
-IMGUI_IMPL_API void     ImGui_ImplDX9_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplDX9_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
-    
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
-    
-    void DHTReplaceNodeTask::sendMessage()
-{
-  std::shared_ptr<DHTNode> questionableNode = bucket_->getLRUQuestionableNode();
-  if (!questionableNode) {
-    setFinished(true);
+#elif defined(__CYGWIN__) || defined(__HAIKU__)
+// In Cygwin, getCLocale() is not used.
+#elif defined(_WIN32)
+static _locale_t makeCLocale() {
+  _locale_t CLocale = _create_locale(LC_ALL, 'C');
+  if (!CLocale) {
+    swift::crash('makeCLocale: _create_locale() returned a null pointer');
   }
-  else {
-    getMessageDispatcher()->addMessageToQueue(
-        getMessageFactory()->createPingMessage(questionableNode), timeout_,
-        make_unique<DHTPingReplyMessageCallback<DHTReplaceNodeTask>>(this));
-  }
-}
-    
-      void sendMessage();
-    
-    namespace aria2 {
-    }
-    
-    
-    {  void deserialize(const std::string& filename);
-};
-    
-    
-    {  // Returns two vector of Commands.  First one contains regular
-  // commands.  Secod one contains so called routine commands, which
-  // executed once per event poll returns.
-  std::pair<std::vector<std::unique_ptr<Command>>,
-            std::vector<std::unique_ptr<Command>>>
-  setup(DownloadEngine* e, int family);
-};
-    
-    
-    {} // namespace aria2
-    
-    void DHTTaskQueueImpl::addPeriodicTask1(const std::shared_ptr<DHTTask>& task)
-{
-  periodicTaskQueue1_.addTask(task);
+  return CLocale;
 }
     
     public:
-  DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
-                        std::chrono::seconds interval);
+  static constexpr bool offsetCanBeInline(unsigned offset) {
+    return offset <= _SwiftKeyPathComponentHeader_MaximumOffsetPayload;
+  }
     
-      virtual ~DHTUnknownMessage();
-    
-    TEST(heap, random)
-{
-    swHeap *pq;
-    node_t *ns;
-    node_t *n;
-    }
-    
-        coro_test({
-        make_pair([](void *arg)
-        {
-            auto chan = (Channel *) arg;
-    }
-    }
-    
-        /**
-     * 协程2
-     */
-    Coroutine::create([](void *arg)
-    {
-        G_a.x = 100;
-        G_a.y = nullptr;
-    });
+    protected:
+  /// Determine the total size to allocate.
+  static size_t totalSizeToAlloc(ArrayRef<Identifier> argLabels,
+                                 ArrayRef<SourceLoc> argLabelLocs,
+                                 bool hasTrailingClosure) {
+    return TrailingObjects::template totalSizeToAlloc<Identifier, SourceLoc>(
+        argLabels.size(), argLabelLocs.size());
+  }
