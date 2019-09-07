70ed4be0a3a676adb4bfbb50df21cee64cec46cb
SUNsung <weatherius@gmail.com>
2019-09-08 01:15:45 +0300

    Rewrite [43dn4Rkl1wlso43i9ohoJ8.cw43hJMHOd45CY.43Nyl.py] File in: 2019-09-08 01:15:45

--- pyton/43dn4Rkl1wlso43i9ohoJ8.cw43hJMHOd45CY.43Nyl.py
+++ pyton/43dn4Rkl1wlso43i9ohoJ8.cw43hJMHOd45CY.43Nyl.py
@@ -1,147 +1,75 @@
 
         
-            @staticmethod
-    def format_seconds(seconds):
-        (mins, secs) = divmod(seconds, 60)
-        (hours, mins) = divmod(mins, 60)
-        if hours > 99:
-            return '--:--:--'
-        if hours == 0:
-            return '%02d:%02d' % (mins, secs)
-        else:
-            return '%02d:%02d:%02d' % (hours, mins, secs)
-    
-        _TESTS = [{
-        # episode with annual navigation
-        'url': 'https://www.tvnow.de/shows/grip-das-motormagazin-1669/2017-05/episode-405-der-neue-porsche-911-gt-3-331082',
-        'info_dict': {
-            'id': '331082',
-            'display_id': 'grip-das-motormagazin/der-neue-porsche-911-gt-3',
-            'ext': 'mp4',
-            'title': 'Der neue Porsche 911 GT 3',
-            'description': 'md5:6143220c661f9b0aae73b245e5d898bb',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'timestamp': 1495994400,
-            'upload_date': '20170528',
-            'duration': 5283,
-            'series': 'GRIP - Das Motormagazin',
-            'season_number': 14,
-            'episode_number': 405,
-            'episode': 'Der neue Porsche 911 GT 3',
-        },
-    }, {
-        # rtl2, episode with season navigation
-        'url': 'https://www.tvnow.de/shows/armes-deutschland-11471/staffel-3/episode-14-bernd-steht-seit-der-trennung-von-seiner-frau-allein-da-526124',
-        'only_matching': True,
-    }, {
-        # rtlnitro
-        'url': 'https://www.tvnow.de/serien/alarm-fuer-cobra-11-die-autobahnpolizei-1815/staffel-13/episode-5-auf-eigene-faust-pilot-366822',
-        'only_matching': True,
-    }, {
-        # superrtl
-        'url': 'https://www.tvnow.de/shows/die-lustigsten-schlamassel-der-welt-1221/staffel-2/episode-14-u-a-ketchup-effekt-364120',
-        'only_matching': True,
-    }, {
-        # ntv
-        'url': 'https://www.tvnow.de/shows/startup-news-10674/staffel-2/episode-39-goetter-in-weiss-387630',
-        'only_matching': True,
-    }, {
-        # vox
-        'url': 'https://www.tvnow.de/shows/auto-mobil-174/2017-11/episode-46-neues-vom-automobilmarkt-2017-11-19-17-00-00-380072',
-        'only_matching': True,
-    }, {
-        'url': 'https://www.tvnow.de/shows/grip-das-motormagazin-1669/2017-05/episode-405-der-neue-porsche-911-gt-3-331082',
-        'only_matching': True,
-    }]
-    
-    from .common import InfoExtractor
-from ..compat import (
-    compat_urllib_parse_urlencode,
-    compat_str,
-)
-from ..utils import (
-    dict_get,
-    ExtractorError,
-    float_or_none,
-    int_or_none,
-    remove_start,
-    try_get,
-    urlencode_postdata,
-)
-    
-            asset_types = []
-        subtitles = {}
-        formats = []
-        last_e = None
-        for item in items_data.findall('.//item'):
-            asset_type = xpath_text(item, 'assetType')
-            if not asset_type or asset_type in asset_types or 'HLS_FPS' in asset_type or 'DASH_CENC' in asset_type:
-                continue
-            asset_types.append(asset_type)
-            query = {
-                'mbr': 'true',
-                'assetTypes': asset_type,
-            }
-            if asset_type.startswith('HLS') or asset_type in ('OnceURL', 'StreamPack'):
-                query['formats'] = 'MPEG4,M3U'
-            elif asset_type in ('RTMP', 'WIFI', '3G'):
-                query['formats'] = 'MPEG4,FLV'
-            try:
-                tp_formats, tp_subtitles = self._extract_theplatform_smil(
-                    update_url_query(tp_release_url, query), content_id,
-                    'Downloading %s SMIL data' % asset_type)
-            except ExtractorError as e:
-                last_e = e
-                continue
-            formats.extend(tp_formats)
-            subtitles = self._merge_subtitles(subtitles, tp_subtitles)
-        if last_e and not formats:
-            raise last_e
-        self._sort_formats(formats)
-    
-        for group in opt_parser.option_groups:
-        for option in group.option_list:
-            long_option = option.get_opt_string().strip('-')
-            complete_cmd = ['complete', '--command', 'youtube-dl', '--long-option', long_option]
-            if option._short_opts:
-                complete_cmd += ['--short-option', option._short_opts[0].strip('-')]
-            if option.help != optparse.SUPPRESS_HELP:
-                complete_cmd += ['--description', option.help]
-            complete_cmd.extend(EXTRA_ARGS.get(long_option, []))
-            commands.append(shell_quote(complete_cmd))
-    
-    import rsa
-import json
-from binascii import hexlify
-    
-        template = template.replace('@SITES@', textwrap.indent('\n'.join(ie_htmls), '\t'))
+        EXAMPLES = '''
+- name: Create a new Linode.
+  linode_v4:
+    label: new-linode
+    type: g6-nanode-1
+    region: eu-west
+    image: linode/debian9
+    root_pass: passw0rd
+    authorized_keys:
+      - 'ssh-rsa ...'
+    state: present
     
     
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
+def create_missing_directories(dest):
+    destpath = os.path.dirname(dest)
+    if not os.path.exists(destpath):
+        os.makedirs(destpath)
     
-    ie_template = '''
-class {name}({bases}):
-    _VALID_URL = {valid_url!r}
-    _module = '{module}'
+            # The first resource is True / Not Null and the second resource is False / Null
+        if resource1 and not resource2:
+            self.module.log('resource1 and not resource2. ' + debug_resources)
+            return False
+    
+                if isinstance(value, list):
+                attrs[name] = list(map(to_bytes, value))
+            else:
+                attrs[name].append(to_bytes(value))
+    
+            # Change the password (or throw an exception)
+        try:
+            self.connection.passwd_s(self.dn, None, self.passwd)
+        except ldap.LDAPError as e:
+            self.fail('Unable to set password', e)
+    
+    # send an email to more than one recipient that the build failed
+- sendgrid:
+      username: '{{ sendgrid_username }}'
+      password: '{{ sendgrid_password }}'
+      from_address: 'build@mycompany.com'
+      to_addresses:
+        - 'ops@mycompany.com'
+        - 'devteam@mycompany.com'
+      subject: 'Build failure!.'
+      body: 'Unable to pull source repository from Git server.'
+  delegate_to: localhost
 '''
     
-        outfile, = args
+    from ansible.module_utils.basic import AnsibleModule, missing_required_lib
     
-        params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
-    }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
+    Request receiver in simple form keeps a reference to a single successor.
+As a variation some receivers may be capable of sending requests out
+in several directions, forming a `tree of responsibility`.
+    
+    '''
+*What is this pattern about?
+Define a family of algorithms, encapsulate each one, and make them interchangeable.
+Strategy lets the algorithm vary independently from clients that use it.
+    
+    
+def convert_to_text(data):
+    print('[CONVERT]')
+    return '{} as text'.format(data)
+    
+            pet = self.pet_factory()
+        print('We have a lovely {}'.format(pet))
+        print('It says {}'.format(pet.speak()))
+    
+    
+if __name__ == '__main__':
+    main()
+    
+        def render(self):
+        return self._text
