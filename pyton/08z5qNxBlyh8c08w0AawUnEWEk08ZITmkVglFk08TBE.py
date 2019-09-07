457db21f7ad6e50ca1e11f557793345a1fd4a0da
SUNsung <weatherius@gmail.com>
2019-09-08 01:58:19 +0300

    Rewrite [08z5qNxBlyh8c08w0AawUnEWEk08ZITmkVglFk08TBE.py] File in: 2019-09-08 01:58:18

--- pyton/08z5qNxBlyh8c08w0AawUnEWEk08ZITmkVglFk08TBE.py
+++ pyton/08z5qNxBlyh8c08w0AawUnEWEk08ZITmkVglFk08TBE.py
@@ -1,18 +1,81 @@
 
         
-            def __init__(self, module):
+        
+def formset_view(request):
+    'A view that tests a simple formset'
+    if request.method == 'POST':
+        formset = TestFormSet(request.POST)
+        if formset.is_valid():
+            t = Template('Valid POST data.', name='Valid POST Template')
+            c = Context()
+        else:
+            t = Template('Invalid POST data. {{ my_formset.errors }}',
+                         name='Invalid POST Template')
+            c = Context({'my_formset': formset})
+    else:
+        formset = TestForm(request.GET)
+        t = Template('Viewing base formset. {{ my_formset }}.',
+                     name='Formset GET Template')
+        c = Context({'my_formset': formset})
+    return HttpResponse(t.render(c))
+    
+        def test_location_comments_for_templatized_files(self):
         '''
-        Construct module
+        Ensure no leaky paths in comments, e.g. #: path\to\file.html.py:123
+        Refs #21209/#26341.
         '''
-        self.module = module
-        if not CLC_FOUND:
-            self.module.fail_json(msg=missing_required_lib('clc-sdk'), exception=CLC_IMP_ERR)
-        if not REQUESTS_FOUND:
-            self.module.fail_json(msg=missing_required_lib('requests'), exception=REQUESTS_IMP_ERR)
-        if requests.__version__ and LooseVersion(
-                requests.__version__) < LooseVersion('2.5.0'):
-            self.module.fail_json(
-                msg='requests library  version should be >= 2.5.0')
+        management.call_command('makemessages', locale=[LOCALE], verbosity=0)
+        self.assertTrue(os.path.exists(self.PO_FILE))
+        with open(self.PO_FILE) as fp:
+            po_contents = fp.read()
+        self.assertMsgId('#: templates/test.html.py', po_contents)
+        self.assertLocationCommentNotPresent(self.PO_FILE, None, '.html.py')
+        self.assertLocationCommentPresent(self.PO_FILE, 5, 'templates', 'test.html')
+    
+        @property
+    def layer_name(self):
+        'Return the name of the layer for the feature.'
+        name = capi.get_feat_name(self._layer._ldefn)
+        return force_str(name, self.encoding, strings_only=True)
+    
+    
+# Class mapping dictionary for OFT Types and reverse mapping.
+OGRFieldTypes = {
+    0: OFTInteger,
+    1: OFTIntegerList,
+    2: OFTReal,
+    3: OFTRealList,
+    4: OFTString,
+    5: OFTStringList,
+    6: OFTWideString,
+    7: OFTWideStringList,
+    8: OFTBinary,
+    9: OFTDate,
+    10: OFTTime,
+    11: OFTDateTime,
+    # New 64-bit integer types in GDAL 2
+    12: OFTInteger64,
+    13: OFTInteger64List,
+}
+ROGRFieldTypes = {cls: num for num, cls in OGRFieldTypes.items()}
+
+    
+        @x.setter
+    def x(self, value):
+        gtf = self._raster.geotransform
+        gtf[self.indices[self._prop][0]] = value
+        self._raster.geotransform = gtf
+    
+        def test_mark_safe_lazy_result_implements_dunder_html(self):
+        self.assertEqual(mark_safe(lazystr('a&b')).__html__(), 'a&b')
+    
+        def _get_alert_policies(self, alias):
+        '''
+        Get the alert policies for account alias by calling the CLC API.
+        :param alias: the account alias
+        :return: the alert policies for the account alias
+        '''
+        response = {}
     
     #
 #  Requires the clc-python-sdk.
@@ -21,7 +84,7 @@
 CLC_IMP_ERR = None
 try:
     import clc as clc_sdk
-    from clc import APIFailedResponse
+    from clc import CLCException
 except ImportError:
     CLC_IMP_ERR = traceback.format_exc()
     CLC_FOUND = False
@@ -29,146 +92,84 @@ except ImportError:
 else:
     CLC_FOUND = True
     
+        if module.params['state'] == 'present' and instance is not None:
+        module.exit_json(changed=False, instance=instance._raw_json)
     
-DOCUMENTATION = '''
-module: clc_modify_server
-short_description: modify servers in CenturyLink Cloud.
-description:
-  - An Ansible module to modify servers in CenturyLink Cloud.
-version_added: '2.0'
-options:
-  server_ids:
-    description:
-      - A list of server Ids to modify.
-    required: True
-  cpu:
-    description:
-      - How many CPUs to update on the server
-  memory:
-    description:
-      - Memory (in GB) to set to the server.
-  anti_affinity_policy_id:
-    description:
-      - The anti affinity policy id to be set for a hyper scale server.
-        This is mutually exclusive with 'anti_affinity_policy_name'
-  anti_affinity_policy_name:
-    description:
-      - The anti affinity policy name to be set for a hyper scale server.
-        This is mutually exclusive with 'anti_affinity_policy_id'
-  alert_policy_id:
-    description:
-      - The alert policy id to be associated to the server.
-        This is mutually exclusive with 'alert_policy_name'
-  alert_policy_name:
-    description:
-      - The alert policy name to be associated to the server.
-        This is mutually exclusive with 'alert_policy_id'
-  state:
-    description:
-      - The state to insure that the provided resources are in.
-    default: 'present'
-    choices: ['present', 'absent']
-  wait:
-    description:
-      - Whether to wait for the provisioning tasks to finish before returning.
-    type: bool
-    default: 'yes'
-requirements:
-    - python = 2.7
-    - requests >= 2.5.0
-    - clc-sdk
-author: 'CLC Runner (@clc-runner)'
-notes:
-    - To use this module, it is required to set the below environment variables which enables access to the
-      Centurylink Cloud
-          - CLC_V2_API_USERNAME, the account login id for the centurylink cloud
-          - CLC_V2_API_PASSWORD, the account password for the centurylink cloud
-    - Alternatively, the module accepts the API token and account alias. The API token can be generated using the
-      CLC account login and password via the HTTP api call @ https://api.ctl.io/v2/authentication/login
-          - CLC_V2_API_TOKEN, the API token generated from https://api.ctl.io/v2/authentication/login
-          - CLC_ACCT_ALIAS, the account alias associated with the centurylink cloud
-    - Users can set CLC_V2_API_URL to specify an endpoint for pointing to a different CLC environment.
-'''
-    
-    RETURN = '''
-'''
-import traceback
-    
-    - name: Disable HTTP and increase session timeout to max value 10800 secs
-  imc_rest:
-    hostname: '{{ imc_host }}'
-    username: '{{ imc_username }}'
-    password: '{{ imc_password }}'
-    validate_certs: no
-    timeout: 120
-    content: |
-        <configConfMo><inConfig>
-          <commHttp dn='sys/svc-ext/http-svc' adminState='disabled'/>
-        </inConfig></configConfMo>
-    
-    import traceback
+            else:
+            # More than 1 matching issue
+            return (False, changed, 'More than one issue with subject %s in project %s' % (issue_subject, project_name), {})
     
     
 def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            name=dict(required=True),
-            port=dict(default=623, type='int'),
-            state=dict(required=True, choices=['on', 'off', 'shutdown', 'reset', 'boot']),
-            user=dict(required=True, no_log=True),
-            password=dict(required=True, no_log=True),
-            timeout=dict(default=300, type='int'),
-        ),
-        supports_check_mode=True,
-    )
+    run_module()
     
     
-def main():
-    object_dict = dict(
-        module=dict(type='str', required=True),
-        class_name=dict(type='str', aliases=['class'], required=True),
-        properties=dict(type='dict', required=True),
-        children=dict(type='list'),
-    )
-    argument_spec = ucs_argument_spec
-    argument_spec.update(
-        objects=dict(type='list', elements='dict', options=object_dict, required=True),
-        state=dict(type='str', choices=['present', 'absent'], default='present'),
-    )
+def post_sendgrid_api(module, username, password, from_address, to_addresses,
+                      subject, body, api_key=None, cc=None, bcc=None, attachments=None,
+                      html_body=False, from_name=None, headers=None):
     
-        def decode(self, value):
-        if value != self.value:
-            raise jose.DeserializationError('Expected {0!r}'.format(self.value))
-        return self.value
+    from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+    
+    EXAMPLES = '''
+- name: Get latest release of a public repository
+  github_release:
+    user: ansible
+    repo: ansible
+    action: latest_release
     
     
-class Header(jose.Header):
-    '''ACME-specific JOSE Header. Implements nonce, kid, and url.
-    '''
-    nonce = jose.Field('nonce', omitempty=True, encoder=jose.encode_b64jose)
-    kid = jose.Field('kid', omitempty=True)
-    url = jose.Field('url', omitempty=True)
+@YouCompleteMeInstance( { 'g:ycm_cache_omnifunc': 1,
+                          'g:ycm_semantic_triggers': TRIGGERS } )
+def OmniCompleter_GetCompletions_Cache_List_Filter_Unicode_test( ycm ):
+  def Omnifunc( findstart, base ):
+    if findstart:
+      return 12
+    return [ '†est', 'å_unicode_identifier', 'πππππππ yummy πie' ]
     
-            :param str domain: The domain to use to look up the managed zone.
-        :param str record_name: The record name (typically beginning with '_acme-challenge.').
-        :param str record_content: The record content (typically the challenge validation).
-        :param int record_ttl: The record TTL (number of seconds that the record may be cached).
-        :raises certbot.errors.PluginError: if an error occurs communicating with the Google API
-        '''
     
-        additional_args = []
-    if misc.get_certbot_version() >= LooseVersion('0.30.0'):
-        additional_args.append('--no-random-sleep-on-renew')
+def CompileLevel( level ):
+  # valid kinds are WARNING and ERROR;
+  #  expected input levels are `warning` and `error`
+  # NOTE: we don't validate the input...
+  expected_kind = level.upper()
     
-        result = False
-    try:
-        ret = urlopen(url, timeout=2)
-        result = (ret.code == 200)
-    except HTTPError as e:
-        print(e, file=sys.stderr)
-    except URLError as e:
-        print(e, file=sys.stderr)
-    except timeout as e:
-        print(e, file=sys.stderr)
-    except Exception as e:
-        print(e, file=sys.stderr)
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+      post_vim_message.assert_has_exact_calls( [
+    call( 'this is a message', warning=False, truncate=True )
+  ] )
+    
+      with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
+    with MockCompletionRequest( ServerResponse ):
+      ycm.SendCompletionRequest()
+      ok_( ycm.CompletionRequestReady() )
+      response = ycm.GetCompletionResponse()
+      post_vim_message.assert_has_exact_calls( [
+        call( 'Exception: message', truncate = True )
+      ] )
+      assert_that(
+        response,
+        has_entries( {
+          'completions': contains( has_entries( {
+            'word': 'insertion_text',
+            'abbr': 'menu_text',
+            'menu': 'extra_menu_info',
+            'info': 'detailed_info\ndoc_string',
+            'kind': 'k',
+            'dup': 1,
+            'empty': 1
+          } ) ),
+          'completion_start_column': 3
+        } )
+      )
+    
+    
+def KeywordsFromSyntaxListOutput_PhpSyntax_ContainsPreProc_test():
+  assert_that( syntax_parse._KeywordsFromSyntaxListOutput(
+                   ContentsOfTestFile( 'php_syntax' ) ),
+               has_items( 'skip', 'function' ) )
