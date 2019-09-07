71834c672aa285d55d2b50425835b470c92a5f36
SUNsung <weatherius@gmail.com>
2019-09-07 11:24:40 +0300

    Rewrite [26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py] File in: 2019-09-07 11:24:39

--- pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
+++ pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
@@ -1,204 +1,25 @@
 
         
-        
-def waveform_to_examples(data, sample_rate):
-  '''Converts audio waveform into an array of examples for VGGish.
+                        for line in text:
+                    if re.match(r'^>', line):
+                        line = line[:0] + '####' + line[1:]
+                    html = markdown.markdown(line)
+                    out.write(html)
+                out.write(footer)
     
-    from __future__ import print_function
+        def backward(self, output):
+        return 1 if output > 0 else 0
     
-      # Read list of query images from dataset file.
-  tf.logging.info('Reading list of query images and boxes from dataset file...')
-  query_list, _, ground_truth = dataset.ReadDatasetFile(
-      cmd_args.dataset_file_path)
-  num_images = len(query_list)
-  tf.logging.info('done! Found %d images', num_images)
-    
-    The :class:`sklearn.svm.OneClassSVM` is known to be sensitive to outliers and
-thus does not perform very well for outlier detection. This estimator is best
-suited for novelty detection when the training set is not contaminated by
-outliers. That said, outlier detection in high-dimension, or without any
-assumptions on the distribution of the inlying data is very challenging, and a
-One-class SVM might give useful results in these situations depending on the
-value of its hyperparameters.
-    
-    import numpy as np
-from sklearn.covariance import EllipticEnvelope
-from sklearn.svm import OneClassSVM
-import matplotlib.pyplot as plt
-import matplotlib.font_manager
-from sklearn.datasets import load_boston
-    
-    plt.matshow(fit_data, cmap=plt.cm.Blues)
-plt.title('After biclustering; rearranged to show biclusters')
-    
-    Two consequences of imposing a connectivity can be seen. First clustering
-with a connectivity matrix is much faster.
-    
-    # If not '', a 'Last updated on:' timestamp is inserted at every page
-# bottom, using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-            if True in result:
-            [print_result(image_to_check, name, distance, show_distance) for is_match, name, distance in zip(result, known_names, distances) if is_match]
-        else:
-            print_result(image_to_check, 'unknown_person', None, show_distance)
-    
-        def test_face_locations(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
-        detected_faces = api.face_locations(img)
-    
-    
-def batch_face_locations(images, number_of_times_to_upsample=1, batch_size=128):
-    '''
-    Returns an 2d array of bounding boxes of human faces in a image using the cnn face detector
-    If you are using a GPU, this can give you much faster results since the GPU
-    can process batches of images at once. If you aren't using a GPU, you don't need this function.
-    
-        pil_image.show()
-
-    
-        # 获得所有人脸的位置以及它们的编码
-    face_locations = face_recognition.face_locations(output)
-    print('Found {} faces in image.'.format(len(face_locations)))
-    face_encodings = face_recognition.face_encodings(output, face_locations)
-    
-            #If training image contains none or more than faces, print an error message and exit
-        if len(face_bounding_boxes) != 1:
-            print(person + '/' + person_img + ' contains none or more than one faces and can't be used for training.')
-            exit()
-        else:
-            face_enc = face_recognition.face_encodings(face)[0]
-            # Add face encoding for current image with corresponding label (name) to the training data
-            encodings.append(face_enc)
-            names.append(person)
-    
-                # If a match was found in known_face_encodings, just use the first one.
-            if True in matches:
-                first_match_index = matches.index(True)
-                name = known_face_names[first_match_index]
-    
-        description = proj_info['description'],
-    keywords = proj_info['keywords'],
-    
-    _help = '''Usage: {} [OPTION]... [URL]...
-TODO
-'''.format(script_name)
-    
-            for i in self.tree.iterfind('video/quality'):
-            quality = i.attrib ['value']
-            url = i[0].attrib['playurl']
-            self.stream_types.append({'id': quality,
-                                      'video_profile': i.attrib ['desp']})
-            self.streams[quality] = {'url': url,
-                                     'video_profile': i.attrib ['desp']}
-            self.streams_sorted = [dict([('id', stream_type['id'])] + list(self.streams[stream_type['id']].items())) for stream_type in self.__class__.stream_types if stream_type['id'] in self.streams]
-    
-    __all__ = ['cbs_download']
-    
-    def rebuilt_url(url):
-    path = urllib.parse.urlparse(url).path
-    aid = path.split('/')[-1].split('_')[0]
-    return 'http://www.dailymotion.com/embed/video/{}?autoplay=1'.format(aid)
-    
-            print_info(site_info, title, type, size)
-        if not info_only:
-            download_urls([real_url], title, ext, size, output_dir, merge = merge)
-    
-    #----------------------------------------------------------------------
-def makeMimi(upid):
-    '''From http://cdn37.atwikiimg.com/sitescript/pub/dksitescript/FC2.site.js
-    Also com.hps.util.fc2.FC2EncrptUtil.makeMimiLocal
-    L110'''
-    strSeed = 'gGddgPfeaf_gzyr'
-    prehash = upid + '_' + strSeed
-    return md5(prehash.encode('utf-8')).hexdigest()
-    
-        stream_id_pattern = r'id='html_stream' value='(\w+)''
-    stream_id = match1(html, stream_id_pattern)
-    
-        The following keys are expected for the _DEFAULTS <metadata> dict:
-        datatype:  [required] A python type class. This limits the type of data that can be
-                   provided in the .ini file and ensures that the value is returned in the
-                   correct type to faceswap. Valid datatypes are: <class 'int'>, <class 'float'>,
-                   <class 'str'>, <class 'bool'>.
-        default:   [required] The default value for this option.
-        info:      [required] A string describing what this option does.
-        choices:   [optional] If this option's datatype is of <class 'str'> then valid
-                   selections can be defined here. This validates the option and also enables
-                   a combobox / radio option in the GUI.
-        gui_radio: [optional] If <choices> are defined, this indicates that the GUI should use
-                   radio buttons rather than a combobox to display this option.
-        min_max:   [partial] For <class 'int'> and <class 'float'> datatypes this is required
-                   otherwise it is ignored. Should be a tuple of min and max accepted values.
-                   This is used for controlling the GUI slider range. Values are not enforced.
-        rounding:  [partial] For <class 'int'> and <class 'float'> datatypes this is
-                   required otherwise it is ignored. Used for the GUI slider. For floats, this
-                   is the number of decimal places to display. For ints this is the step size.
-        fixed:     [optional] [train only]. Training configurations are fixed when the model is
-                   created, and then reloaded from the state file. Marking an item as fixed=False
-                   indicates that this value can be changed for existing models, and will override
-                   the value saved in the state file with the updated value in config. If not
-                   provided this will default to True.
-'''
-    
-            return blended.astype('float32') / 255.0
-
-    
-        def rename(self, src, dest):
-        print(u'renaming %s to %s' % (src, dest))
-        os.rename(src, dest)
-    
-    
-def main():
-    '''
-    # Counting to two...
-    >>> for number in count_to_two():
-    ...     print(number)
-    one
-    two
-    
-        >>> data1.data = 3
-    DecimalViewer: Subject Data 1 has data 3
-    HexViewer: Subject Data 1 has data 0x3
-    
-    
-def on_sale_discount(order):
-    return order.price * 0.25 + 20
-    
-    
-def get_localizer(language='English'):
-    '''Factory'''
-    localizers = {
-        'English': EnglishLocalizer,
-        'Greek': GreekLocalizer,
-    }
-    return localizers[language]()
-    
-    
-class Delegator(object):
-    '''
-    >>> delegator = Delegator(Delegate())
-    >>> delegator.p1
-    123
-    >>> delegator.p2
-    Traceback (most recent call last):
-    ...
-    AttributeError: 'Delegate' object has no attribute 'p2'
-    >>> delegator.do_something('nothing')
-    'Doing nothing'
-    >>> delegator.do_anything()
-    Traceback (most recent call last):
-    ...
-    AttributeError: 'Delegate' object has no attribute 'do_anything'
-    '''
-    
-        graphic.add(graphic1)
-    graphic.add(graphic2)
-    
-    *TL;DR
-Adds behaviour to object without affecting its class.
-'''
+        # -1表示倒序，返回topN的特征值[-1 到 -(topNfeat+1) 但是不包括-(topNfeat+1)本身的倒叙]
+    eigValInd = eigValInd[:-(topNfeat+1):-1]
+    # print 'eigValInd2=', eigValInd
+    # 重组 eigVects 最大到最小
+    redEigVects = eigVects[:, eigValInd]
+    # print 'redEigVects=', redEigVects.T
+    # 将数据转换到新空间
+    # print '---', shape(meanRemoved), shape(redEigVects)
+    lowDDataMat = meanRemoved * redEigVects
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+    # print 'lowDDataMat=', lowDDataMat
+    # print 'reconMat=', reconMat
+    return lowDDataMat, reconMat
