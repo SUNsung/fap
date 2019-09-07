b4a15c5c9ee09fda5e0ad7aa9947cceacb7e6447
SUNsung <weatherius@gmail.com>
2019-09-08 01:37:35 +0300

    Rewrite [23ZaU4lXi82cY23eFADJd4tKuc23lUj65IDwVUo23kYp.cpp] File in: 2019-09-08 01:37:34

--- cccccc/23ZaU4lXi82cY23eFADJd4tKuc23lUj65IDwVUo23kYp.cpp
+++ cccccc/23ZaU4lXi82cY23eFADJd4tKuc23lUj65IDwVUo23kYp.cpp
@@ -1,43 +1,241 @@
 
         
-          void AddDocStringArgs();
-  void AddDocStringInputs();
-  void AddDocStringAttrs();
-  void AddDocStringNameArg();
-  void AddOutputGlobals();
-  void AddDocStringOutputs();
-  void AddBody(const string& prefix);
-  void AddBodyNoReturn(const string& apply_prefix);
-  void AddExport();
+          std::unique_ptr<FlatBufferModel> model_;
+  std::unique_ptr<ops::builtin::BuiltinOpResolver> resolver_;
+  std::unique_ptr<Interpreter> interpreter_;
+  std::vector<Interpreter::TfLiteDelegatePtr> delegates_;
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    // Returns an OpInfo for a BatchMatMul
+OpContext DescribeBatchMatMul(const std::vector<int>& dims_a,
+                              const std::vector<int>& dims_b) {
+  OpContext op_context;
+  SetCpuDevice(&op_context.op_info);
+  op_context.op_info.set_op('BatchMatMul');
+    }
     
-    REGISTER_OP('Invalid')
-    .Attr('invalid attr: int32')  // invalid since the name has a space.
-    .Doc(R'doc(
-An op to test that invalid ops do not successfully generate invalid python code.
-)doc');
+    #define DECLARE_GPU_SPECS(T)         \
+  DECLARE_GPU_SPECS_INDEX(T, int32); \
+  DECLARE_GPU_SPECS_INDEX(T, int64)
     
-    #ifndef TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
+      void Compute(OpKernelContext* c) override {
+    const Tensor& params = c->input(0);
+    const Tensor& indices = c->input(1);
+    }
     
-      // Computes complex-to-complex FFT in the transform direction as specified
-  // by direction parameter.
-  virtual bool DoFft(Stream *stream, Plan *plan,
-                     const DeviceMemory<std::complex<float>> &input,
-                     DeviceMemory<std::complex<float>> *output) = 0;
-  virtual bool DoFft(Stream *stream, Plan *plan,
-                     const DeviceMemory<std::complex<double>> &input,
-                     DeviceMemory<std::complex<double>> *output) = 0;
+    #undef REGISTER
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+      Shape shuffled_shape;
+  ShuffleDims(input_shape, input_axes_order, output_axes_order,
+              &shuffled_shape);
+  std::vector<float> shuffled_data(input_flat_size);
+  ShuffleArray(input_shape, input_axes_order, output_axes_order, shuffled_shape,
+               input_data, shuffled_data.data());
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Status ValidateHostPortPair(const string& host_port);
+    
+      // Checks if two graphs are equal. Both graphs must have the same set of nodes
+  // with the same inputs and attributes. Nodes can be in different order.
+  //
+  // NOTE: This function uses EXPECT/ASSERT macros to check node properties
+  // equality, and adds all failuires to the current test.
+  void CompareGraphs(GraphDef want, GraphDef got) const;
+    
+    #endif  // TENSORFLOW_CORE_GRAPPLER_OPTIMIZERS_GENERIC_LAYOUT_OPTIMIZER_TRANSPOSER_FACTORY_H_
+
+    
+      void pollAndSchedule(int task_id);
+  void schedule(int task_id, bool run_inline = false) noexcept;
+  void reset() override;
+  virtual void finishRun();
+  void parentCallback(int parent_id);
+  bool isInlineTask(int parent_id, int child_id) const;
+    
+    class GetMergeSingleScalarFeatureTensorsGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    vector<string> input_blob_names{};
+    vector<string> output_blob_names{};
+    }
+    }
+    
+    **Result**
+    
+    // Build a Table file from the contents of *iter.  The generated file
+// will be named according to meta->number.  On success, the rest of
+// *meta will be filled with metadata about the generated table.
+// If no data is present in *iter, meta->file_size will be set to
+// zero, and no Table file will be produced.
+Status BuildTable(const std::string& dbname, Env* env, const Options& options,
+                  TableCache* table_cache, Iterator* iter, FileMetaData* meta);
+    
+    // Return the legacy file name for an sstable with the specified number
+// in the db named by 'dbname'. The result will be prefixed with
+// 'dbname'.
+std::string SSTTableFileName(const std::string& dbname, uint64_t number);
+    
+      fname = CurrentFileName('foo');
+  ASSERT_EQ('foo/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(0, number);
+  ASSERT_EQ(kCurrentFile, type);
+    
+    
+    {  Table* table = reinterpret_cast<TableAndFile*>(cache_->Value(handle))->table;
+  Iterator* result = table->NewIterator(options);
+  result->RegisterCleanup(&UnrefEntry, cache_, handle);
+  if (tableptr != nullptr) {
+    *tableptr = table;
+  }
+  return result;
+}
+    
+    int main(int argc, char** argv) { return leveldb::test::RunAllTests(); }
+
+    
+    #include <string>
+    
+      // Return true iff 'x' is a prefix of '*this'
+  bool starts_with(const Slice& x) const {
+    return ((size_ >= x.size_) && (memcmp(data_, x.data_, x.size_) == 0));
+  }
+    
+      // Open database.  Disable compression since it affects the creation
+  // of layers and the code below is trying to test against a very
+  // specific scenario.
+  leveldb::DB* db;
+  leveldb::Options db_options;
+  db_options.create_if_missing = true;
+  db_options.compression = leveldb::kNoCompression;
+  ASSERT_OK(leveldb::DB::Open(db_options, dbpath, &db));
+    
+    int main(int argc, char** argv) { return leveldb::test::RunAllTests(); }
+
+    
+    Iterator::~Iterator() {
+  if (!cleanup_head_.IsEmpty()) {
+    cleanup_head_.Run();
+    for (CleanupNode* node = cleanup_head_.next; node != nullptr;) {
+      node->Run();
+      CleanupNode* next_node = node->next;
+      delete node;
+      node = next_node;
+    }
+  }
+}
+    
+    
+    {  const char* p = s.data();
+  const char* limit = p + s.size();
+  for (size_t i = 0; i < values.size(); i++) {
+    ASSERT_TRUE(p < limit);
+    uint64_t actual;
+    const char* start = p;
+    p = GetVarint64Ptr(p, limit, &actual);
+    ASSERT_TRUE(p != nullptr);
+    ASSERT_EQ(values[i], actual);
+    ASSERT_EQ(VarintLength(actual), p - start);
+  }
+  ASSERT_EQ(p, limit);
+}
+    
+            // Now update our result value based on the state we are in
+        switch (state)
+        {
+        case MANTS:
+            pnumret->sign = (curChar == L'-') ? -1 : 1;
+            break;
+        case EXPSZ:
+        case EXPS:
+            expSign = (curChar == L'-') ? -1 : 1;
+            break;
+        case EXPDZ:
+        case EXPD:
+        {
+            curChar = NormalizeCharDigit(curChar, radix);
+    }
+    }
+    
+            UINT m_initCallCount;
+        UINT m_getCategoriesCallCount;
+        UINT m_setCurrentCategoryCallCount;
+        UINT m_setCurUnitTypesCallCount;
+        UINT m_switchActiveCallCount;
+        UINT m_sendCommandCallCount;
+        UINT m_setVMCallbackCallCount;
+    
+    NarratorAnnouncement ^ CalculatorAnnouncement::GetNoRightParenthesisAddedAnnouncement(String ^ announcement)
+{
+    return ref new NarratorAnnouncement(
+        announcement,
+        CalculatorActivityIds::NoParenthesisAdded,
+        AutomationNotificationKind::ActionCompleted,
+        AutomationNotificationProcessing::ImportantMostRecent);
+}
+
+    
+    using apollo::common::Status;
+using apollo::dreamview::Chart;
+using apollo::planning_internal::SLFrameDebug;
+using apollo::planning_internal::SpeedPlan;
+using apollo::planning_internal::STGraphDebug;
+    
+    #define AINFO_IF(cond) ALOG_IF(INFO, cond, MODULE_NAME)
+#define AWARN_IF(cond) ALOG_IF(WARN, cond, MODULE_NAME)
+#define AERROR_IF(cond) ALOG_IF(ERROR, cond, MODULE_NAME)
+#define AFATAL_IF(cond) ALOG_IF(FATAL, cond, MODULE_NAME)
+#define ALOG_IF(severity, cond, module) \
+  !(cond) ? (void)0                     \
+          : google::LogMessageVoidify() & ALOG_MODULE(module, severity)
+    
+      // 11
+  // TODO(QiL) : verify the unit here.
+  if (chassis_detail.gem().has_steering_rpt_1_6e() &&
+      chassis_detail.gem().steering_rpt_1_6e().has_output_value()) {
+    chassis_.set_steering_percentage(static_cast<float>(
+        chassis_detail.gem().steering_rpt_1_6e().output_value() * 100.0 /
+        vehicle_params_.max_steer_angle()));
+  } else {
+    chassis_.set_steering_percentage(0);
+  }
+    
+    
+    {
+    {
+    {
+    {}  // namespace valet_parking
+}  // namespace scenario
+}  // namespace planning
+}  // namespace apollo
+
+    
+    
+    {  EXPECT_EQ(expect, real);
+}
+*/
+    
+    using apollo::cyber::croutine::CRoutine;
+using apollo::cyber::proto::ChoreographyTask;
+    
+    ChannelManager::ChannelManager() {
+  allowed_role_ |= 1 << RoleType::ROLE_WRITER;
+  allowed_role_ |= 1 << RoleType::ROLE_READER;
+  change_type_ = ChangeType::CHANGE_CHANNEL;
+  channel_name_ = 'channel_change_broadcast';
+  exempted_msg_types_.emplace(message::MessageType<message::RawMessage>());
+  exempted_msg_types_.emplace(message::MessageType<message::PyMessageWrap>());
+}
+    
+        obj->sub_type = GetObjectSubType(type);
+    obj->type = base::kSubType2TypeMap.at(obj->sub_type);
+    obj->type_probs.assign(static_cast<int>(base::ObjectType::MAX_OBJECT_TYPE),
+                           0);
+    obj->sub_type_probs.assign(
+        static_cast<int>(base::ObjectSubType::MAX_OBJECT_TYPE), 0);
+    obj->sub_type_probs[static_cast<int>(obj->sub_type)] = score;
+    obj->type_probs[static_cast<int>(obj->type)] = score;
