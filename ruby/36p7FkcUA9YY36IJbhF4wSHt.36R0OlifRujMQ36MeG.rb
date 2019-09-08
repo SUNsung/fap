f0a270baa05fce6df65c60875f3146b1f3c3cb4e
SUNsung <weatherius@gmail.com>
2019-09-08 12:24:42 +0300

    Rewrite [36p7FkcUA9YY36IJbhF4wSHt.36R0OlifRujMQ36MeG.rb] File in: 2019-09-08 12:24:42

--- ruby/36p7FkcUA9YY36IJbhF4wSHt.36R0OlifRujMQ36MeG.rb
+++ ruby/36p7FkcUA9YY36IJbhF4wSHt.36R0OlifRujMQ36MeG.rb
@@ -1,105 +1,71 @@
 
         
-              def html_pages
-        @site_html_pages ||= @obj.pages.select do |page|
-          page.html? || page.url.end_with?('/')
+            def editor(screenshot)
+      if screenshot.mac?
+        return MacEditor.new(screenshot)
+      else
+        return Editor.new(screenshot, Frameit.config[:debug_mode])
+      end
+    end
+  end
+end
+
+    
+              disabled_services_object = self.service_object
+          disabled_services.each do |k, v|
+            disabled_services_object.__hash__[k] = true
+            disabled_services_object.send('#{k}=', v)
+          end
+          Produce::Service.disable(disabled_services_object, nil) unless disabled_services.empty?
         end
       end
     
-    WITH_LIQUID = <<-LIQUID.freeze
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
-pharetra tempus. {{ author }} et metus fermentum, eu cursus lorem
-mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
+            new_appendix = new_appendix.sub('.', '') if new_appendix.start_with?('.')
+        major = version_match[:major].to_i
+        minor = version_match[:minor].to_i || 0
+        patch = version_match[:patch].to_i || 0
     
-        def defaults_deprecate_type(old, current)
-      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
-      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
-                        'type: #{current}'.'
-    end
-  end
-end
-
-    
-        def promote_track
-      version_codes = client.track_version_codes(Supply.config[:track])
-      # the actual value passed for the rollout argument does not matter because it will be ignored by the Google Play API
-      # but it has to be between 0.0 and 1.0 to pass the validity check. So we are passing the default value 0.1
-      client.update_track(Supply.config[:track], 0.1, nil) if Supply.config[:deactivate_on_promote]
-      client.update_track(Supply.config[:track_promote_to], Supply.config[:rollout] || 0.1, version_codes)
+          @app ||= Spaceship::ConnectAPI::App.get(app_id: @app_id)
+      unless @app
+        UI.user_error!('Could not find app with #{(config[:apple_id] || config[:app_identifier])}')
+      end
+      return @app
     end
     
-            Actions.lane_context[SharedValues::FL_CHANGELOG] = changelog.strip.length > 0 ? changelog : params[:fallback_changelog]
-      end
+              metric = (tester.beta_tester_metrics || []).first
+          if metric.installed?
+            install_version = '#{metric.installed_cf_bundle_short_version_string} (#{metric.installed_cf_bundle_version})'
+            pretty_date = metric.installed_cf_bundle_version
+          end
     
-          def self.description
-        'Receive the version number from a podspec file'
-      end
+        private
     
-            contains_key = self.shared_values_constants.include?(key.to_s)
-        add_offense(node, :expression, MISSING_CONST_DEFINITION_MSG) unless contains_key
-      end
-    
-          [
-        # app upload info
-        FastlaneCore::ConfigItem.new(key: :username,
-                                     short_option: '-u',
-                                     env_name: 'PILOT_USERNAME',
-                                     description: 'Your Apple ID Username',
-                                     default_value: user,
-                                     default_value_dynamic: true),
-        FastlaneCore::ConfigItem.new(key: :app_identifier,
-                                     short_option: '-a',
-                                     env_name: 'PILOT_APP_IDENTIFIER',
-                                     description: 'The bundle identifier of the app to upload or manage testers (optional)',
-                                     optional: true,
-                                     code_gen_sensitive: true,
-                                     # This incorrect env name is here for backwards compatibility
-                                     default_value: ENV['TESTFLIGHT_APP_IDENTITIFER'] || CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier),
-                                     default_value_dynamic: true),
-        FastlaneCore::ConfigItem.new(key: :app_platform,
-                                     short_option: '-m',
-                                     env_name: 'PILOT_PLATFORM',
-                                     description: 'The platform to use (optional)',
-                                     optional: true,
-                                     default_value: 'ios',
-                                     verify_block: proc do |value|
-                                       UI.user_error!('The platform can only be ios, appletvos, or osx') unless ['ios', 'appletvos', 'osx'].include?(value)
-                                     end),
-        FastlaneCore::ConfigItem.new(key: :apple_id,
-                                     short_option: '-p',
-                                     env_name: 'PILOT_APPLE_ID',
-                                     description: 'Apple ID property in the App Information section in App Store Connect',
-                                     optional: true,
-                                     code_gen_sensitive: true,
-                                     default_value: ENV['TESTFLIGHT_APPLE_ID'],
-                                     default_value_dynamic: true,
-                                     type: String,
-                                     verify_block: proc do |value|
-                                       error_message = '`apple_id` value is incorrect. The correct value should be taken from Apple ID property in the App Information section in App Store Connect.'
-    
-            groups = [custom_tester_group]
-        group_names = groups.map(&:name).join(';')
-        expect(FastlaneCore::UI).to receive(:success).with('Successfully added tester #{fake_tester.email} to app #{fake_app_name} in group(s) #{group_names}')
-    
-          def encode_token(token)
-        Base64.strict_encode64(token)
-      end
-    
-          def call(env)
-        request               = Request.new(env)
-        status, headers, body = app.call(env)
-    
-            post '/', :file => Rack::Test::UploadedFile.new(temp_file.path), :other => '<bar>'
-        expect(body).to eq('_escaped_params_tmp_file\nhello world\n&lt;bar&gt;')
-      ensure
-        File.unlink(temp_file.path)
+          def self.get(bundle_id_id: nil, includes: nil)
+        return Spaceship::ConnectAPI.get_bundle_id(bundle_id_id: bundle_id_id, includes: includes).first
       end
     end
   end
 end
 
     
-      it 'accepts post form requests with correct authenticity_token field' do
-    post('/', {'authenticity_token' => token}, 'rack.session' => session)
-    expect(last_response).to be_ok
-  end
+        def check_sha256_actually_256(sha256: cask.sha256, stanza: 'sha256')
+      odebug 'Verifying #{stanza} string is a legal SHA-256 digest'
+      return unless sha256.is_a?(String)
+      return if sha256.length == 64 && sha256[/^[0-9a-f]+$/i]
+    
+    module RuboCop
+  module Cop
+    module FormulaAudit
+      # This cop checks for various miscellaneous Homebrew coding styles.
+      class Lines < FormulaCop
+        def audit_formula(_node, _class_node, _parent_class_node, _body_node)
+          [:automake, :ant, :autoconf, :emacs, :expat, :libtool, :mysql, :perl,
+           :postgresql, :python, :python3, :rbenv, :ruby].each do |dependency|
+            next unless depends_on?(dependency)
+    
+        it 'works with tags' do
+      spec.uses_from_macos('foo' => :head, :after => :el_capitan)
+    
+    module Cask
+  class Download
+    attr_reader :cask
