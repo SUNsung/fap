0890563d7e8076bb780e4d0b9d3f42d62bc016a5
SUNsung <weatherius@gmail.com>
2019-09-08 16:36:57 +0300

    Rewrite [05KlaQIp7ZVsU05NhJKHYgYKdg055PBW6uNizik053uY.rb] File in: 2019-09-08 16:36:57

--- ruby/05KlaQIp7ZVsU05NhJKHYgYKdg055PBW6uNizik053uY.rb
+++ ruby/05KlaQIp7ZVsU05NhJKHYgYKdg055PBW6uNizik053uY.rb
@@ -1,201 +1,94 @@
 
         
-              # `{{ site.related_posts }}` is how posts can get posts related to
-      # them, either through LSI if it's enabled, or through the most
-      # recent posts.
-      # We should remove this in 4.0 and switch to `{{ post.related_posts }}`.
-      def related_posts
-        return nil unless @current_document.is_a?(Jekyll::Document)
-    
-                problem 'Dir([\'#{string_content(path)}\']) is unnecessary; just use \'#{match[0]}\''
-          end
-    
-              find_method_with_args(body_node, :system, 'xcodebuild') do
-            problem %q(use 'xcodebuild *args' instead of 'system 'xcodebuild', *args')
-          end
-    
-        it 'When plist_options are not defined when using a formula-defined plist', :ruby23 do
-      expect_offense(<<~RUBY)
-        class Foo < Formula
-          url 'https://brew.sh/foo-1.0.tgz'
-          homepage 'https://brew.sh'
-    
-        checksum, tag = spec.checksum_for(Utils::Bottles.tag)
-    
-        it 'acts like #depends_on' do
-      f = formula 'foo' do
-        url 'foo-1.0'
-    
-          expect(spec.deps).to be_empty
+            def reset_password_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :reset_password_instructions, opts)
     end
     
-      it 'properly handles Casks that are not present' do
-    expect {
-      described_class.run('notacask')
-    }.to raise_error(Cask::CaskUnavailableError)
-  end
-end
-
+      private
     
-        case val
-    when nil     then Version.detect(url, specs)
-    when String  then Version.create(val)
-    when Version then val
-    else
-      raise TypeError, 'version '#{val.inspect}' should be a string'
-    end
-  end
+          # Sign out a given user or scope. This helper is useful for signing out a user
+      # after deleting accounts. Returns true if there was a logout and false if there
+      # is no user logged in on the referred scope
+      #
+      # Examples:
+      #
+      #   sign_out :user     # sign_out(scope)
+      #   sign_out @user     # sign_out(resource)
+      #
+      def sign_out(resource_or_scope=nil)
+        return sign_out_all_scopes unless resource_or_scope
+        scope = Devise::Mapping.find_scope!(resource_or_scope)
+        user = warden.user(scope: scope, run_callbacks: false) # If there is no user
     
-      def show
-    render json: @poll, serializer: REST::PollSerializer, include_results: true
-  end
-    
-        return if skip_distribution?
-    
-        return unless @account.present? && @status.distributable?
-    
-      # ID to add for error notification helper.
-  # config.error_notification_id = nil
-    
-        def pam_conflict(_attributes)
-      # Block pam login tries on traditional account
-    end
-    
-          it 'processes payload with actor if valid signature exists' do
-        payload['signature'] = { 'type' => 'RsaSignature2017' }
-    
-    # create and write to opml file
-xml = Builder::XmlMarkup.new(indent: 2)
-xml.instruct! :xml, version: '1.0', encoding: 'UTF-8'
-xml.tag!('opml', version: '1.0') do
-  # head
-  xml.tag!('head') do
-    xml.title TITLE
-  end
-    
-      task :publishing do
-    invoke 'deploy:symlink:release'
-  end
-    
-        def hostfilter
-      ['--hosts HOSTS', '-z',
-       'Run SSH commands only on matching hosts',
-       lambda do |value|
-         Configuration.env.add_cmdline_filter(:host, value)
-       end]
-    end
-    
-        private
-    
-          def call
-        ask_question
-        value_or_default
+          # Stores the provided location to redirect the user after signing in.
+      # Useful in combination with the `stored_location_for` helper.
+      #
+      # Example:
+      #
+      #   store_location_for(:user, dashboard_path)
+      #   redirect_to user_facebook_omniauth_authorize_path
+      #
+      def store_location_for(resource_or_scope, location)
+        session_key = stored_location_key_for(resource_or_scope)
+        
+        path = extract_path_from_location(location)
+        session[session_key] = path if path
       end
     
-    set_if_empty :local_user, -> { ENV['USER'] || ENV['LOGNAME'] || ENV['USERNAME'] }
-
-    
-      def autocorrect_source_file(source)
-    Tempfile.open('tmp') { |f| autocorrect_source(source, f) }
-  end
-    
-            rhs
+          def initialize_from_record(record)
+        @scope_name = Devise::Mapping.find_scope!(record)
+        @resource   = instance_variable_set('@#{devise_mapping.name}', record)
       end
+    
+        def fullpath
+      '/#{@path_prefix}/#{@path}'.squeeze('/')
     end
-  end
-end
-
     
-            def outermost_send_on_same_line(heredoc)
-          previous = heredoc
-          current = previous.parent
-          until send_missing_closing_parens?(current, previous, heredoc)
-            previous = current
-            current = current.parent
-            return unless previous && current
-          end
-          current
-        end
-    
-        context 'correct + opposite' do
-      it 'registers an offense' do
-        inspect_source(source)
-        expect(cop.offenses.size).to eq(1)
-        expect(cop.messages.first)
-          .to eq('`end` at 7, 4 is not aligned with `foo def` at 5, 0.')
-        expect(cop.highlights.first).to eq('end')
-        expect(cop.config_to_allow_offenses).to eq('Enabled' => false)
-      end
-    
-            expect(new_source).to eq(<<~RUBY)
-          def func
-            {a: 1, b: 2, c: 3}.each do |(a, b)|
-              puts a, b
+          accessors.each do |accessor|
+        mod.class_eval <<-METHOD, __FILE__, __LINE__ + 1
+          def #{accessor}
+            if defined?(@#{accessor})
+              @#{accessor}
+            elsif superclass.respond_to?(:#{accessor})
+              superclass.#{accessor}
+            else
+              Devise.#{accessor}
             end
           end
-        RUBY
-      end
     
-      before { inspect_source(source) }
+        it 'renders the body' do
+      expect(mail.body.encoded).to match('You were mentioned by bob')
+      expect(mail.body.encoded).to include 'The body of the foreign status'
+    end
+  end
     
-          # Checks whether this node body is a void context.
-      # Always `true` for `for`.
-      #
-      # @return [true] whether the `for` node body is a void context
-      def void_context?
-        true
-      end
+        # Calculates min and max from length validations for numeric inputs
+    b.optional :min_max
     
-          # Returns the inverse delimiter of the `pair` as a string.
-      #
-      # @param [Boolean] with_spacing whether to include spacing
-      # @return [String] the inverse delimiter of the `pair`
-      def inverse_delimiter(with_spacing = false)
-        if with_spacing
-          hash_rocket? ? SPACED_COLON : SPACED_HASH_ROCKET
-        else
-          hash_rocket? ? COLON : HASH_ROCKET
+          def gateway_action(source, action, success_state)
+        protect_from_connection_error do
+          response = payment_method.send(action, money.amount_in_cents,
+                                         source,
+                                         gateway_options)
+          handle_response(response, success_state, :failure)
         end
       end
     
-          params = add_dead('jid-with-hyphen')
-      post '/morgue/#{job_params(*params)}', 'retry' => 'Retry'
-      assert_equal 302, last_response.status
-      assert_equal 0, Sidekiq::DeadSet.new.size
+            def update
+          authorize! :update, @order, order_token
+          @address = find_address
     
-        it 'handles symbols and strings' do
-      q = Sidekiq::Queue.new('bar')
-      assert_equal 0, q.size
-      assert SetWorker.set('queue' => 'bar', :retry => 11).perform_async(1)
-      job = q.first
-      assert_equal 'bar', job['queue']
-      assert_equal 11, job['retry']
+            def update
+          @image = scope.images.accessible_by(current_ability, :update).find(params[:id])
+          if @image.update(image_params)
+            respond_with(@image, default_template: :show)
+          else
+            invalid_resource!(@image)
+          end
+        end
     
-        it 'shows empty retries' do
-      r = Sidekiq::RetrySet.new
-      assert_equal 0, r.size
-    end
-    
-    describe Sidekiq::ExceptionHandler do
-  describe 'with mock logger' do
-    before do
-      @old_logger = Sidekiq.logger
-      @str_logger = StringIO.new
-      Sidekiq.logger = Logger.new(@str_logger)
-    end
-    
-      describe 'I18n' do
-    before do
-      require 'i18n'
-      I18n.enforce_available_locales = false
-      require 'sidekiq/middleware/i18n'
-    end
-    
-          Time.stub(:now, created_time) do
-        @retry.schedule (enqueued_time - 60).to_f, @error_1.merge!('created_at' => created_time.to_f)
-        @retry.schedule (enqueued_time - 50).to_f, @error_2.merge!('created_at' => created_time.to_f)
-        @retry.schedule (enqueued_time + 60).to_f, @error_3.merge!('created_at' => created_time.to_f)
-        @scheduled.schedule (enqueued_time - 60).to_f, @future_1.merge!('created_at' => created_time.to_f)
-        @scheduled.schedule (enqueued_time - 50).to_f, @future_2.merge!('created_at' => created_time.to_f)
-        @scheduled.schedule (enqueued_time + 60).to_f, @future_3.merge!('created_at' => created_time.to_f)
-      end
+            def show
+          @option_type = Spree::OptionType.accessible_by(current_ability, :show).find(params[:id])
+          respond_with(@option_type)
+        end
