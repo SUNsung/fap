167a78026c7b22a59e6cd8ce2c9590dbec9b5681
SUNsung <weatherius@gmail.com>
2019-09-08 07:46:42 +0300

    Rewrite [34gPNBNGAg.fw34IIQxPQzSXa.34k2xfGtFXOiw34eai.cpp] File in: 2019-09-08 07:46:41

--- cccccc/34gPNBNGAg.fw34IIQxPQzSXa.34k2xfGtFXOiw34eai.cpp
+++ cccccc/34gPNBNGAg.fw34IIQxPQzSXa.34k2xfGtFXOiw34eai.cpp
@@ -1,81 +1,144 @@
 
         
-        void CacheImpl::releaseValue(void *Value) {
-  cache_release_value(static_cast<cache_t*>(Impl), Value);
+        //
+// Alternative Canonical Huffman decoder:
+//
+// Canonical Huffman decoder based on 'On the Implementation of Minimum
+// Redundancy Prefix Codes' by Moffat and Turpin - highly recommended
+// reading as a good description of the problem space, as well as 
+// a fast decoding algorithm.
+//
+// The premise is that instead of working directly with the coded 
+// symbols, we create a new ordering based on the frequency of symbols.
+// Less frequent symbols (and thus longer codes) are ordered earler.
+// We're calling the values in this ordering 'Ids', as oppsed to 
+// 'Symbols' - which are the short values we eventually want decoded.
+//
+// With this new ordering, a few small tables can be derived ('base' 
+// and 'offset') which drive the decoding. To cut down on the 
+// linear scanning of these tables, you can add a small table
+// to directly look up short codes (as you might in a traditional
+// lookup-table driven decoder). 
+//
+// The decoder is meant to be compatible with the encoder (and decoder)
+// in ImfHuf.cpp, just faster. For ease of implementation, this decoder
+// should only be used on compressed bitstreams >= 128 bits long.
+//
+    
+    
+    
+    
+FrameBuffer::Iterator
+FrameBuffer::find (const char name[])
+{
+    return _map.find (name);
 }
     
-        if (node->Further || node->Right) {
-      IndentScope ms(this, (childKind == ChildKind::Right ||
-                            childKind == ChildKind::Further ||
-                            childKind == ChildKind::Root) ? '  ' : '| ');
+    #include 'ImfIO.h'
+#include 'ImfHeader.h'
+#include 'ImfNamespace.h'
+#include 'ImfExport.h'
+    
+        IMF_EXPORT
+    TileDescription &		tileDescription ();
+    IMF_EXPORT
+    const TileDescription &	tileDescription () const;
+    
+    OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_EXIT
+    
+    
+OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_ENTER
+    
+    #include <stdio.h>
+#include <stdlib.h>
+#include 'PSTokenizer.h'
+    
+    //========================================================================
+//
+// Modified under the Poppler project - http://poppler.freedesktop.org
+//
+// All changes made under the Poppler project to this file are licensed
+// under GPL version 2 or later
+//
+// Copyright (C) 2006 Scott Turner <scotty1024@mac.com>
+//
+// To see a description of the changes please see the Changelog file that
+// came with your tarball or type make ChangeLog if you are building from git
+//
+//========================================================================
+    
+    #ifndef PAGE_TRANSITION_H
+#define PAGE_TRANSITION_H
+    
+    class ProfileData {
+public:
     }
     
-        void emitDiagnosticMessage(clang::FullSourceLoc Loc,
-                               clang::PresumedLoc PLoc,
-                               clang::DiagnosticsEngine::Level Level,
-                               StringRef Message,
-                               ArrayRef<clang::CharSourceRange> Ranges,
-                               clang::DiagOrStoredDiag Info) override {
-      if (isInSwiftBuffers(Loc)) {
-        // FIXME: Ideally, we'd report non-suppressed diagnostics on synthetic
-        // buffers, printing their names (eg. <swift-imported-modules>:...) but
-        // this risks printing _excerpts_ of those buffers to stderr too; at
-        // present the synthetic buffers are 'large blocks of null bytes' which
-        // we definitely don't want to print out. So until we have some clever
-        // way to print the name but suppress printing excerpts, we just replace
-        // the Loc with an invalid one here, which suppresses both.
-        Loc = clang::FullSourceLoc();
-        if (shouldSuppressDiagInSwiftBuffers(Info))
-          return;
-      }
-      callback(Loc, Level, Message);
-    }
     
-    const char *Demangle::getNodeKindString(swift::Demangle::Node::Kind k) {
-  switch (k) {
-#define NODE(ID)                                                               \
-  case Node::Kind::ID:                                                         \
-    return #ID;
-#include 'swift/Demangling/DemangleNodes.def'
+    {  if (!ok) {
+    return gFalse;
   }
-  return 'Demangle::Node::Kind::???';
+  permFlags = (*xsh->authorize)(xsh->handlerData, docData, authData);
+  if (!(permFlags & xpdfPermissionOpen)) {
+    return gFalse;
+  }
+  if (!(*xsh->getKey)(xsh->handlerData, docData, &key, &length, &encVersion)) {
+    return gFalse;
+  }
+  if ((fileKeyLength = length) > 16) {
+    fileKeyLength = 16;
+  }
+  memcpy(fileKey, key, fileKeyLength);
+  (*xsh->freeKey)(xsh->handlerData, docData, key, length);
+  return gTrue;
 }
     
-    class LLVM_LIBRARY_VISIBILITY Windows : public ToolChain {
-protected:
-  InvocationInfo constructInvocation(const LinkJobAction &job,
-                                     const JobContext &context) const override;
+      tx = transpGroupStack->tx;
+  ty = transpGroupStack->ty;
+  tBitmap = transpGroupStack->tBitmap;
+    
+      //----- Type 3 font operators
+  virtual void type3D0(GfxState *state, double wx, double wy);
+  virtual void type3D1(GfxState *state, double wx, double wy,
+		       double llx, double lly, double urx, double ury);
+    
+        // static helper to instantiate a network from a file
+    template <class ElemType>
+    static ComputationNetworkPtr CreateFromFile(DEVICEID_TYPE deviceId, const std::wstring& fileName)
+    {
+        auto net = make_shared<ComputationNetwork>(deviceId);
+        net->Load<ElemType>(fileName);
+        return net;
     }
     
-      /// Emits an error for each file in \p unusedPrimaryFiles.
-  ///
-  /// \returns true if \p unusedPrimaryFiles is non-empty.
-  bool diagnoseUnusedPrimaryFiles(std::set<StringRef> unusedPrimaryFiles);
-    
-            unordered_map<int,int> record;
-        for(int i = 0 ; i < nums.size() ; i ++)
-            record[nums[i]] = i;
-    
-        ListNode* head = createLinkedList(arr, n);
-    printLinkedList(head);
-    
-    
-    {    return 0;
-}
-    
-        head = Solution().swapPairs(head);
-    printLinkedList(head);
+        public:
+        MASGDPerfStats(size_t myRank, size_t numWorkers):
+            m_numWorkers(numWorkers), m_myRank(myRank), m_numSyncPerformedInCurrentEpoch(0), m_reportFrequency(1), 
+            m_totalSamplesProcessedSinceLastReport(0), m_localSamplesProcessedSinceLastReport(0)
+        {
+            m_Timer.Start();
+        }
     
     
     {
-    {        delete dummyHead1;
-        delete dummyHead2;
-        return ret;
-    }
-};
+    {
+    {}}}
+
     
-    #include <iostream>
-#include <vector>
-#include <stack>
+      virtual void deleteDbKeys(ConnectionsTree::DatabaseItem& db) override;
     
-            stack<TreeNode*> stack, output;
+    
+    {  return qcompress::decompress(value.toByteArray());
+}
+    
+      if (role == name) {
+    return data['name'];
+  } else if (role == version) {
+    return data['version'];
+  } else if (role == cmd) {
+    return data['cmd'];
+  } else if (role == description) {
+    return data['description'];
+  }
+    
+      void openServerStats(QSharedPointer<RedisClient::Connection> connection);
