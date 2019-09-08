ea184e8df88fc86e1b4cb7ae94d1525459cb2aef
SUNsung <weatherius@gmail.com>
2019-09-08 22:19:56 +0300

    Rewrite [564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb] File in: 2019-09-08 22:19:55

--- ruby/564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb
+++ ruby/564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb
@@ -1,64 +1,133 @@
 
         
-        __END__
+              # `{{ site.related_posts }}` is how posts can get posts related to
+      # them, either through LSI if it's enabled, or through the most
+      # recent posts.
+      # We should remove this in 4.0 and switch to `{{ post.related_posts }}`.
+      def related_posts
+        return nil unless @current_document.is_a?(Jekyll::Document)
     
-          def referrer(env)
-        ref = env['HTTP_REFERER'].to_s
-        return if !options[:allow_empty_referrer] and ref.empty?
-        URI.parse(ref).host || Request.new(env).host
-      rescue URI::InvalidURIError
-      end
+    # Mimic how the proposed change would first execute a couple of checks and
+# proceed to process with Liquid if necessary
+def conditional_liquid(content)
+  return content if content.nil? || content.empty?
+  return content unless content.include?('{%') || content.include?('{{')
+  always_liquid(content)
+end
+    }
+    }
+    }
     
-      it 'accepts post requests with correct X-CSRF-Token header' do
-    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => token)
-    expect(last_response).to be_ok
+    # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+
+    
+    if pathutil_relative == native_relative
+  Benchmark.ips do |x|
+    x.report('pathutil') { pathutil_relative }
+    x.report('native')   { native_relative }
+    x.compare!
   end
+else
+  print 'PATHUTIL: '
+  puts pathutil_relative
+  print 'NATIVE:   '
+  puts native_relative
+end
+
     
-        it 'Reads referrer from Host header when Referer header is relative' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
-      expect(subject.referrer(env)).to eq('foo.com')
-    end
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-        it 'redirects requests with duplicate session cookies' do
-      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
-      expect(last_response).to be_redirect
-      expect(last_response.location).to eq('/')
-    end
+            # rubocop:disable Metrics/AbcSize
+        def process(args, opts)
+          if !args || args.empty?
+            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
+          end
     
-          def format
-        @format = (@page.format || false) if @format.nil?
-        @format.to_s.downcase
-      end
+      it 'parses the URL and sets the corresponding instance variables' do
+    expect(subject.instance_variable_get(:@user)).to eq('homebrew')
+    expect(subject.instance_variable_get(:@repo)).to eq('brew')
+  end
+end
+    
+    describe Cask::Cmd::Cache, :cask do
+  let(:local_transmission) {
+    Cask::CaskLoader.load(cask_path('local-transmission'))
+  }
+    
+            def guest_encrypted_data_bag_secret_key_path
+          if @config.encrypted_data_bag_secret_key_path
+            File.join(guest_provisioning_path, 'encrypted_data_bag_secret_key')
+          end
+        end
+    
+      let(:machine) { iso_env.machine(iso_env.machine_names[0], :dummy) }
+  let(:communicator) { VagrantTests::DummyCommunicator::Communicator.new(machine) }
+  let(:config)  { double('config') }
+    
+        it 'returns false if not installed' do
+      expect(machine.communicate).to receive(:test).
+        with(command, sudo: true).and_return(false)
+      expect(subject.chef_installed(machine, 'chef_solo', version)).to be_falsey
     end
   end
 end
 
     
-          def remove_page_extentions(page_path)
-        Gollum::Markup.formats.values.each do |format|
-          page_path = page_path.gsub(/\.#{format[:regexp]}$/, '')
+    # Configure RSpec
+RSpec.configure do |c|
+  c.formatter = :documentation
+    
+              # Test if the provider is usable or not
+          begin
+            provider_cls.usable?(true)
+          rescue Errors::VagrantError => e
+            raise Errors::ProviderNotUsable,
+              machine: name.to_s,
+              provider: provider.to_s,
+              message: e.to_s
+          end
+        else
+          box_formats = provider
         end
-        return page_path
       end
     
-          attr_reader :name, :path
-    
-      test 'frontend links for editing blocked' do
-    Precious::App.set(:wiki_options, { allow_editing: false })
-    get '/A'
-    
-      # determine file list from git ls-files
-  files = `git ls-files`.
-    split('\n').
-    sort.
-    reject { |file| file =~ /^\./ }.
-    reject { |file| file =~ /^(rdoc|pkg|test|Home\.md|\.gitattributes)/ }.
-    map { |file| '    #{file}' }.
-    join('\n')
-    
-        # Remove all slashes from the start of string.
-    # Remove all double slashes
-    def clean_url url
-      return url if url.nil?
-      url.gsub('%2F', '/').gsub(/^\/+/, '').gsub('//', '/')
+        it 'should include the machine state' do
+      expect($stdout).to receive(:puts).with(/,state,/).twice
     end
+    
+          it 'sets the description' do
+        post '/users/api_secrets', params: { api_secret: valid_params }
+        expect(user.api_secrets.last.description).to eq valid_params[:description]
+      end
+    
+        body = last_response.body
+    
+      test 'extracting paths from URLs' do
+    assert_nil extract_path('Eye-Of-Sauron')
+    assert_equal 'Mordor', extract_path('Mordor/Sauron')
+    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
+  end
+    
+      new_version
+end
+    
+          ws  = WorkSpace.new(binding)
+      irb = Irb.new(ws)
