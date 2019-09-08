96f6766f8d69775984e34cf08884ed785bc74d03
SUNsung <weatherius@gmail.com>
2019-09-08 09:43:37 +0300

    Rewrite [12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp] File in: 2019-09-08 09:43:36

--- cccccc/12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp
+++ cccccc/12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp
@@ -1,53 +1,219 @@
 
         
-        
-    {	mix_udata = p_userdata;
-	mix_callback = p_callback;
-}
+        KNOWN_STDLIB_TYPE_DECL(_MaxBuiltinFloatType, TypeAliasDecl, 0)
     
     #endif
 
     
-    		md->unlock();
-    
-    #endif // MIDI_DRIVER_ALSAMIDI_H
-#endif // ALSAMIDI_ENABLED
-
-    
-    #endif // MIDI_DRIVER_WINMIDI_H
-#endif // WINMIDI_ENABLED
-
-    
-    void GDAPI godot_pool_color_array_new_with_array(godot_pool_color_array *r_dest, const godot_array *p_a) {
-	PoolVector<Color> *dest = (PoolVector<Color> *)r_dest;
-	Array *a = (Array *)p_a;
-	memnew_placement(dest, PoolVector<Color>);
+      for (auto Info : ReflectionInfos) {
+    uint64_t TypeRefOffset = Info.Builtin.SectionOffset
+                           - Info.TypeReference.SectionOffset;
+    for (auto &BuiltinTypeDescriptor : Info.Builtin.Metadata) {
+      assert(BuiltinTypeDescriptor.Size > 0);
+      assert(BuiltinTypeDescriptor.getAlignment() > 0);
+      assert(BuiltinTypeDescriptor.Stride > 0);
+      if (!BuiltinTypeDescriptor.hasMangledTypeName())
+        continue;
+      auto CandidateMangledName =
+          BuiltinTypeDescriptor.getMangledTypeName(TypeRefOffset);
+      if (!reflectionNameMatches(Dem, CandidateMangledName, MangledName))
+        continue;
+      return &BuiltinTypeDescriptor;
     }
+  }
     
-    #include 'core/error_macros.h'
+    public:
+  static constexpr bool offsetCanBeInline(unsigned offset) {
+    return offset <= _SwiftKeyPathComponentHeader_MaximumOffsetPayload;
+  }
+    
+      constexpr bool hasExtraArgument() const {
+    return (Value & 0x40u) != 0;
+  }
+    
+        UniValue obj2(UniValue::VOBJ);
+    BOOST_CHECK(obj2.pushKV('cat1', 9000));
+    BOOST_CHECK(obj2.pushKV('cat2', 12345));
+    
+    #ifndef BITCOIN_CRYPTO_RIPEMD160_H
+#define BITCOIN_CRYPTO_RIPEMD160_H
     
     
-    {	return 0;
+    {    Status s = target()->NewWritableFile(f, r);
+    if (s.ok()) {
+      if (strstr(f.c_str(), '.ldb') != NULL ||
+          strstr(f.c_str(), '.log') != NULL) {
+        *r = new DataFile(this, *r);
+      } else if (strstr(f.c_str(), 'MANIFEST') != NULL) {
+        *r = new ManifestFile(this, *r);
+      }
+    }
+    return s;
+  }
+    
+    Status DumpFile(Env* env, const std::string& fname, WritableFile* dst) {
+  FileType ftype;
+  if (!GuessType(fname, &ftype)) {
+    return Status::InvalidArgument(fname + ': unknown file type');
+  }
+  switch (ftype) {
+    case kLogFile:         return DumpLog(env, fname, dst);
+    case kDescriptorFile:  return DumpDescriptor(env, fname, dst);
+    case kTableFile:       return DumpTable(env, fname, dst);
+    default:
+      break;
+  }
+  return Status::InvalidArgument(fname + ': not a dump-able file type');
+}
+    
+    int main(int argc, char** argv) {
+  leveldb::Env* env = leveldb::Env::Default();
+  bool ok = true;
+  if (argc < 2) {
+    Usage();
+    ok = false;
+  } else {
+    std::string command = argv[1];
+    if (command == 'dump') {
+      ok = leveldb::HandleDumpCommand(env, argv+2, argc-2);
+    } else {
+      Usage();
+      ok = false;
+    }
+  }
+  return (ok ? 0 : 1);
 }
 
     
-        // Setup Platform/Renderer bindings
-    ImGui_ImplWin32_Init(hwnd);
-    ImGui_ImplDX10_Init(g_pd3dDevice);
+    SEXP XGBoosterSetAttr_R(SEXP handle, SEXP name, SEXP val) {
+  R_API_BEGIN();
+  const char *v = isNull(val) ? nullptr : CHAR(asChar(val));
+  CHECK_CALL(XGBoosterSetAttr(R_ExternalPtrAddr(handle),
+                              CHAR(asChar(name)), v));
+  R_API_END();
+  return R_NilValue;
+}
     
-        // Decide GL+GLSL versions
-#if __APPLE__
-    // GL 3.2 Core + GLSL 150
-    const char* glsl_version = '#version 150';
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-#else
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = '#version 130';
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-#endif
+    /*!
+ * \brief The data reading callback function.
+ *  The iterator will be able to give subset of batch in the data.
+ *
+ *  If there is data, the function will call set_function to set the data.
+ *
+ * \param data_handle The handle to the callback.
+ * \param set_function The batch returned by the iterator
+ * \param set_function_handle The handle to be passed to set function.
+ * \return 0 if we are reaching the end and batch is not returned.
+ */
+XGB_EXTERN_C typedef int XGBCallbackDataIterNext(  // NOLINT(*)
+    DataIterHandle data_handle, XGBCallbackSetData *set_function,
+    DataHolderHandle set_function_handle);
+    
+        for (size_t i = 0; i < data_vec.size(); ++i) {
+      bst_uint idx = data_vec[i].index - min_index_;
+      CHECK_LE(idx, static_cast<bst_uint>(std::numeric_limits<StorageIndex>::max()))
+          << 'The storage index is chosen to limited to smaller equal than '
+          << std::numeric_limits<StorageIndex>::max()
+          << 'min_index=' << min_index_;
+      index_.data[i] = static_cast<StorageIndex>(idx);
+      value_.data[i] = data_vec[i].fvalue;
+    }
+    
+      bool Next() override {
+    if (!parser_->Next()) return false;
+    const RowBlock<IndexType>& batch = parser_->Value();
+    LOG(INFO) << batch.size;
+    dense_index_.resize(num_col_ * batch.size);
+    dense_value_.resize(num_col_ * batch.size);
+    std::fill(dense_value_.begin(), dense_value_.end(), 0.0);
+    offset_.resize(batch.size + 1);
+    offset_[0] = 0;
+    }
+    
+    
+    {  delete options.compaction_filter;
+}
+    
+    TEST_F(DBTestXactLogIterator, TransactionLogIteratorStallAtLastRecord) {
+  do {
+    Options options = OptionsForLogIterTest();
+    DestroyAndReopen(options);
+    Put('key1', DummyString(1024));
+    auto iter = OpenTransactionLogIter(0);
+    ASSERT_OK(iter->status());
+    ASSERT_TRUE(iter->Valid());
+    iter->Next();
+    ASSERT_TRUE(!iter->Valid());
+    ASSERT_OK(iter->status());
+    Put('key2', DummyString(1024));
+    iter->Next();
+    ASSERT_OK(iter->status());
+    ASSERT_TRUE(iter->Valid());
+  } while (ChangeCompactOptions());
+}
+    
+      Status GetFileSize(const std::string& fname, uint64_t* file_size) override {
+    PERF_TIMER_GUARD(env_get_file_size_nanos);
+    return EnvWrapper::GetFileSize(fname, file_size);
+  }
+    
+      bool PartialMergeMulti(const Slice& /*key*/,
+                         const std::deque<Slice>& operand_list,
+                         std::string* new_value,
+                         Logger* /*logger*/) const override {
+    Slice max;
+    for (const auto& operand : operand_list) {
+      if (max.compare(operand) < 0) {
+        max = operand;
+      }
+    }
+    }
+    
+    // A merge operator that mimics Put semantics
+// Since this merge-operator will not be used in production,
+// it is implemented as a non-associative merge operator to illustrate the
+// new interface and for testing purposes. (That is, we inherit from
+// the MergeOperator class rather than the AssociativeMergeOperator
+// which would be simpler in this case).
+//
+// From the client-perspective, semantics are the same.
+class PutOperator : public MergeOperator {
+ public:
+  bool FullMerge(const Slice& /*key*/, const Slice* /*existing_value*/,
+                 const std::deque<std::string>& operand_sequence,
+                 std::string* new_value, Logger* /*logger*/) const override {
+    // Put basically only looks at the current/latest value
+    assert(!operand_sequence.empty());
+    assert(new_value != nullptr);
+    new_value->assign(operand_sequence.back());
+    return true;
+  }
+    }
+    
+      // Need to refill more than one interval. Need to sleep longer. Check
+  // whether expiration will hit
+    
+    // This is an example interface of external-compaction algorithm.
+// Compaction algorithm can be implemented outside the core-RocksDB
+// code by using the pluggable compaction APIs that RocksDb provides.
+class Compactor : public EventListener {
+ public:
+  // Picks and returns a compaction task given the specified DB
+  // and column family.  It is the caller's responsibility to
+  // destroy the returned CompactionTask.  Returns 'nullptr'
+  // if it cannot find a proper compaction task.
+  virtual CompactionTask* PickCompaction(
+      DB* db, const std::string& cf_name) = 0;
+    }
+    
+      // Transaction could not commit since the write outside of the txn conflicted
+  // with the read!
+  assert(s.IsBusy());
+    
+      s = db->Get(ReadOptions(), 'key1', &value);
+  assert(s.IsNotFound());
+    
+      // Do some reads and writes to key 'x'
+  read_options.snapshot = txn_db->GetSnapshot();
+  s = txn->Get(read_options, 'x', &value);
+  txn->Put('x', 'x');
