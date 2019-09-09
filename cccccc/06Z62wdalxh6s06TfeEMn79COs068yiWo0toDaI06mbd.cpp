e1cfddcade17649b37fa084120c83cd44ef1979e
SUNsung <weatherius@gmail.com>
2019-09-09 08:54:56 +0300

    Rewrite [06Z62wdalxh6s06TfeEMn79COs068yiWo0toDaI06mbd.cpp] File in: 2019-09-09 08:54:56

--- cccccc/06Z62wdalxh6s06TfeEMn79COs068yiWo0toDaI06mbd.cpp
+++ cccccc/06Z62wdalxh6s06TfeEMn79COs068yiWo0toDaI06mbd.cpp
@@ -1,197 +1,116 @@
 
         
-        
-    {
-    {    private:
-        Number m_p;
-        Number m_q;
-    };
-}
-
+        int os_detect(TO_BLOCK_LIST* port_blocks,
+              OSResults* osr,
+              tesseract::Tesseract* tess);
     
-            if (e->PreviousExecutionState == ApplicationExecutionState::Terminated)
-        {
-            // TODO: Restore the saved session state only when appropriate, scheduling the
-            // final launch steps after the restore is complete
-        }
-    
-        static std::vector<uint32_t> DigitGroupingStringToGroupingVector(std::wstring_view groupingString);
-    std::wstring GroupDigits(std::wstring_view delimiter, std::vector<uint32_t> const& grouping, std::wstring_view displayString, bool isNumNegative = false);
-    
-        PNUMBER Number::ToPNUMBER() const
-    {
-        PNUMBER ret = _createnum(static_cast<uint32_t>(this->Mantissa().size()) + 1);
-        ret->sign = this->Sign();
-        ret->exp = this->Exp();
-        ret->cdigit = static_cast<int32_t>(this->Mantissa().size());
+    struct BlobData {
+  BlobData() : blob(nullptr), choices(nullptr) {}
+  BlobData(int index, Tesseract* tess, const WERD_RES& word)
+    : blob(word.chopped_word->blobs[index]),
+      tesseract(tess),
+      choices(&(*word.ratings)(index, index)) {}
     }
     
-    //////////////////////////////////////////////////
-//
-// InitialOneTimeOnlyNumberSetup
-//
-//////////////////////////////////////////////////
-void CCalcEngine::InitialOneTimeOnlySetup(CalculationManager::IResourceProvider& resourceProvider)
+    
+    { private:
+  // The collection of images to put in the PDF.
+  Pixa* pixa_;
+  // The fonts used to draw text captions.
+  L_Bmf* fonts_;
+};
+    
+      // Backwards compatible fit returning a gradient and constant.
+  // Deprecated. Prefer Fit(ICOORD*, ICOORD*) where possible, but use this
+  // function in preference to the LMS class.
+  double Fit(float* m, float* c);
+    
+    
+    {    UNICHARMAP_NODE* children;
+    UNICHAR_ID id;
+  };
+    
+    DHTResponseMessage::~DHTResponseMessage() = default;
+    
+      virtual void fillMessage(Dict* msgDict) CXX11_OVERRIDE;
+    
+    std::shared_ptr<DHTNode> DHTRoutingTable::getNode(const unsigned char* nodeID,
+                                                  const std::string& ipaddr,
+                                                  uint16_t port) const
 {
-    LoadEngineStrings(resourceProvider);
-    }
-    
-            std::wstring value;
-    
-    
-    {    AutomationProperties::SetName(m_host, announcement->Announcement);
-    AutomationPeer ^ peer = FrameworkElementAutomationPeer::FromElement(m_host);
-    if (peer != nullptr)
-    {
-        peer->RaiseAutomationEvent(AutomationEvents::LiveRegionChanged);
-    }
+  std::shared_ptr<DHTBucket> bucket = getBucketFor(nodeID);
+  return bucket->getNode(nodeID, ipaddr, port);
 }
-
     
-      void Unref() {
-    bool do_delete = false;
-    {
-      MutexLock lock(&mutex_);
-      --refs_;
-      assert(refs_ >= 0);
-      if (refs_ <= 0) {
-        do_delete = true;
+      void showBuckets() const;
+    
+    void DHTRoutingTableSerializer::setNodes(
+    const std::vector<std::shared_ptr<DHTNode>>& nodes)
+{
+  nodes_ = nodes;
+}
+    
+      std::shared_ptr<DHTNode> localNode_;
+    
+    namespace aria2 {
+    }
+    
+      virtual std::shared_ptr<DHTTask>
+  createPingTask(const std::shared_ptr<DHTNode>& remoteNode,
+                 int numRetry = 0) CXX11_OVERRIDE;
+    
+      virtual void executeTask() = 0;
+    
+    void DHTTaskQueueImpl::addPeriodicTask2(const std::shared_ptr<DHTTask>& task)
+{
+  periodicTaskQueue2_.addTask(task);
+}
+    
+    namespace aria2 {
+    }
+    
+          std::vector<std::string> entry_pts;
+      for (const auto& ent_pt :
+           container_details.get_child('Config.Entrypoint')) {
+        entry_pts.push_back(ent_pt.second.data());
       }
-    }
-    }
+      r['config_entrypoint'] = osquery::join(entry_pts, ', ');
     
-    /*
- * Class:     org_rocksdb_CompactionOptionsFIFO
- * Method:    maxTableFilesSize
- * Signature: (J)J
- */
-jlong Java_org_rocksdb_CompactionOptionsFIFO_maxTableFilesSize(
-    JNIEnv*, jobject, jlong jhandle) {
-  auto* opt = reinterpret_cast<rocksdb::CompactionOptionsFIFO*>(jhandle);
-  return static_cast<jlong>(opt->max_table_files_size);
-}
-    
-    /*
- * Class:     org_rocksdb_CompactionOptionsUniversal
- * Method:    setCompressionSizePercent
- * Signature: (JI)V
- */
-void Java_org_rocksdb_CompactionOptionsUniversal_setCompressionSizePercent(
-    JNIEnv*, jobject, jlong jhandle,
-    jint jcompression_size_percent) {
-  auto* opt = reinterpret_cast<rocksdb::CompactionOptionsUniversal*>(jhandle);
-  opt->compression_size_percent =
-      static_cast<unsigned int>(jcompression_size_percent);
-}
-    
-    /*
- * Class:     org_rocksdb_Env
- * Method:    getThreadList
- * Signature: (J)[Lorg/rocksdb/ThreadStatus;
- */
-jobjectArray Java_org_rocksdb_Env_getThreadList(
-    JNIEnv* env, jobject, jlong jhandle) {
-  auto* rocks_env = reinterpret_cast<rocksdb::Env*>(jhandle);
-  std::vector<rocksdb::ThreadStatus> thread_status;
-  rocksdb::Status s = rocks_env->GetThreadList(&thread_status);
-  if (!s.ok()) {
-    // error, throw exception
-    rocksdb::RocksDBExceptionJni::ThrowNew(env, s);
-    return nullptr;
+    bool verifyMacAddress(std::string const& value) {
+  if (value == 'incomplete') {
+    return true;
+  } else {
+    boost::smatch match;
+    // IEEE 802: six groups of two hexadecimal digits, separated by '-' or ':'
+    boost::regex rxMacAddress('^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$');
+    return boost::regex_match(value, match, rxMacAddress);
   }
-    }
-    
-    /*
- * Class:     org_rocksdb_IngestExternalFileOptions
- * Method:    setAllowBlockingFlush
- * Signature: (JZ)V
- */
-void Java_org_rocksdb_IngestExternalFileOptions_setAllowBlockingFlush(
-    JNIEnv*, jobject, jlong jhandle, jboolean jallow_blocking_flush) {
-  auto* options =
-      reinterpret_cast<rocksdb::IngestExternalFileOptions*>(jhandle);
-  options->allow_blocking_flush = static_cast<bool>(jallow_blocking_flush);
 }
     
-      jbyte* end_key = new jbyte[jend_key_len];
-  env->GetByteArrayRegion(jend_key, jend_key_off, jend_key_len, end_key);
-  if (env->ExceptionCheck()) {
-    // exception thrown: ArrayIndexOutOfBoundsException
-    delete[] begin_key;
-    delete[] end_key;
-    return false;
+    
+    {} // namespace osquery
+
+    
+      /**
+   * @brief the distributed work ID of a carve
+   *
+   * This value should be used by the TLS endpoints where carve data is
+   * aggregated, to tie together a distributed query with the carve data
+   */
+  std::string requestId_;
+    
+      virtual ExpectedSuccess<DatabaseError> open() = 0;
+  virtual void close() = 0;
+    
+    
+    {  rocksdb::Options options = getOptions();
+  auto db_path = boost::filesystem::path(path_).make_preferred();
+  auto destroy_status = rocksdb::DestroyDB(db_path.string(), options);
+  if (!destroy_status.ok()) {
+    return createError(DatabaseError::FailToDestroyDB, 'Fail to destroy db: ')
+           << destroy_status.ToString();
   }
-  rocksdb::Slice end_key_slice(reinterpret_cast<char*>(end_key), jend_key_len);
-    
-    
-    {    Vec3 _center;
-    Vec3 _eye;
-    Vec3 _up;
-};
-    
-    void ActionManager::removeAllActions()
-{
-    for (tHashElement *element = _targets; element != nullptr; )
-    {
-        auto target = element->target;
-        element = (tHashElement*)element->hh.next;
-        removeAllActionsFromTarget(target);
-    }
+  return Success();
 }
     
-    #include '2d/CCAction.h'
-#include 'base/CCVector.h'
-#include 'base/CCRef.h'
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __ACTION_CCTILEDGRID_ACTION_H__
-#define __ACTION_CCTILEDGRID_ACTION_H__
-    
-        /** Return the TMXObjectGroup for the specific group. 
-     * 
-     * @return Return the TMXObjectGroup for the specific group.
-     */
-    TMXObjectGroup* getObjectGroup(const std::string& groupName) const;
-    
-    
-    {private:
-    static std::unordered_map<std::string, FontAtlas *> _atlasMap;
-};
-    
-    class Texture2D;
-class FontCharMap : public Font
-{  
-public:
-    static FontCharMap * create(const std::string& charMapFile, int itemWidth, int itemHeight, int startCharMap);
-    static FontCharMap * create(Texture2D* texture, int itemWidth, int itemHeight, int startCharMap);
-    static FontCharMap * create(const std::string& plistFile);
-    
-    virtual int* getHorizontalKerningForTextUTF32(const std::u32string& text, int &outNumLetters) const override;
-    virtual FontAtlas *createFontAtlas() override;
-    
-protected:    
-    FontCharMap(Texture2D* texture,int itemWidth, int itemHeight, int startCharMap)
-        :_texture(texture)
-        ,_mapStartChar(startCharMap)
-        ,_itemWidth(itemWidth)
-        ,_itemHeight(itemHeight)
-    {}
-    /**
-     * @js NA
-     * @lua NA
-     */
-    virtual ~FontCharMap();
-    
-private:
-    Texture2D* _texture;
-    int _mapStartChar;
-    int _itemWidth;
-    int _itemHeight;
-    }
+    #pragma once
