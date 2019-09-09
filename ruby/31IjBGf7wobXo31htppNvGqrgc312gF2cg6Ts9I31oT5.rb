7d89693ca16c3b5f7a5b65b4faa93fb1a23b6633
SUNsung <weatherius@gmail.com>
2019-09-09 16:52:45 +0300

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2019-09-09 16:52:44

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,185 +1,181 @@
 
         
-              run_test_command('test/models/post_test.rb:4:9').tap do |output|
-        assert_match 'PostTest:FirstFilter', output
-        assert_match 'PostTest:SecondFilter', output
-        assert_match '2 runs, 2 assertions', output
+            it 'shows the dry run pop up with previous events and allows use previously received event' do
+      emitter.events << Event.new(payload: {url: 'http://xkcd.com/'})
+      agent.sources << emitter
+      agent.options.merge!('url' => '', 'url_from_event' => '{{url}}')
+      agent.save!
+    
+        it 'respects an environment variable that specifies a path or URL to a different scenario' do
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
+    end
+    
+          it 'loads all workers' do
+        workers = @agent_runner.send(:load_workers)
+        expect(workers).to be_a(Hash)
+        expect(workers.keys).to eq(['HuginnScheduler', 'DelayedJobWorker'])
       end
+    
+        it 'for the forenoon' do
+      expect(@scheduler.send(:hour_to_schedule_name, 6)).to eq('6am')
     end
     
-        def to_json(options = {})
-      super except: [:c, :e]
+      describe 'migrating a hash' do
+    it 'should convert every attribute' do
+      expect(LiquidMigrator.convert_hash({'a' => '$.data', 'b' => 'This is a <$.test>'})).to eq(
+                                  {'a' => '$.data', 'b' => 'This is a {{test}}'}
+      )
+    end
+    it 'should work with leading_dollarsign_is_jsonpath' do
+      expect(LiquidMigrator.convert_hash({'a' => '$.data', 'b' => 'This is a <$.test>'}, leading_dollarsign_is_jsonpath: true)).to eq(
+                                  {'a' => '{{data}}', 'b' => 'This is a {{test}}'}
+      )
+    end
+    it 'should use the corresponding *_path attributes when using merge_path_attributes'do
+      expect(LiquidMigrator.convert_hash({'a' => 'default', 'a_path' => '$.data'}, {leading_dollarsign_is_jsonpath: true, merge_path_attributes: true})).to eq(
+                                  {'a' => '{{data}}'}
+      )
+    end
+    it 'should raise an exception when encountering complex JSONPaths' do
+      expect { LiquidMigrator.convert_hash({'b' => 'This is <$.complex[2]>'}) }.
+        to raise_error('JSONPath '$.complex[2]' is too complex, please check your migration.')
     end
   end
-  class CsvRenderable
-    def to_csv
-      'c,s,v'
-    end
-  end
-  class TestController < ActionController::Base
-    def render_simon_says
-      render simon: 'foo'
-    end
     
-        it 'handles a large amount of output' do
-      cmd = ['-e', <<-RB]
-        puts File.read(#{__FILE__.inspect})
-      RB
-      Executable.execute_command('ruby', cmd, true).should == File.read(__FILE__)
-    end
+            new(sandbox, sandbox.root.to_s, pods_project, umbrella_targets_descriptions)
+      end
     
-            # @private
-        # @return [Hash<PodVariant, String>]
+            # Displays the state of each pod.
         #
-        def scope_by_platform
-          grouped_variants = group_by { |v| v.platform.name }
-          if grouped_variants.all? { |set| set.variants.count == 1 }
-            # => Platform name
-            platform_name_proc = proc { |v| Platform.string_name(v.platform.symbolic_name).tr(' ', '') }
-          else
-            grouped_variants = group_by(&:platform)
-            # => Platform name + SDK version
-            platform_name_proc = proc { |v| v.platform.to_s.tr(' ', '') }
-          end
-          scope_if_necessary(grouped_variants.map(&:scope_without_suffix), &platform_name_proc)
-        end
-    
-    module Pod
-  class Installer
-    class Analyzer
-      describe PodVariant do
-        before do
-          @specs = [stub('Spec'), stub('Spec/Foo')]
-          @testspecs = [stub('Spec/Tests')]
-          @appspecs = [stub('Spec/App')]
-          @platform = Platform.ios
-          @type = Target::BuildType.dynamic_framework
-        end
-    
-            it 'returns :framework as product type' do
-          @target.product_type.should == :framework
-        end
-    
-          LAUNCHSCREEN_STORYBOARD_CONTENTS_IOS_8 = <<-XML.strip_heredoc.freeze
-              <?xml version='1.0' encoding='UTF-8' standalone='no'?>
-              <document type='com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB' version='3.0' toolsVersion='13122.16' systemVersion='17A277' targetRuntime='iOS.CocoaTouch' propertyAccessControl='none' useAutolayout='YES' launchScreen='YES' useTraitCollections='YES' colorMatched='YES' initialViewController='01J-lp-oVM'>
-                <dependencies>
-                  <plugIn identifier='com.apple.InterfaceBuilder.IBCocoaTouchPlugin' version='13104.12'/>
-                  <capability name='documents saved in the Xcode 8 format' minToolsVersion='8.0'/>
-                </dependencies>
-                <scenes>
-                  <!--View Controller-->
-                  <scene sceneID='EHf-IW-A2E'>
-                    <objects>
-                      <viewController id='01J-lp-oVM' sceneMemberID='viewController'>
-                        <layoutGuides>
-                          <viewControllerLayoutGuide type='top' id='rUq-ht-380'/>
-                          <viewControllerLayoutGuide type='bottom' id='a9l-8d-mfx'/>
-                        </layoutGuides>
-                        <view key='view' contentMode='scaleToFill' id='Ze5-6b-2t3'>
-                          <rect key='frame' x='0.0' y='0.0' width='375' height='667'/>
-                          <autoresizingMask key='autoresizingMask' widthSizable='YES' heightSizable='YES'/>
-                          <color key='backgroundColor' red='1' green='1' blue='1' alpha='1' colorSpace='custom' customColorSpace='sRGB'/>
-                        </view>
-                      </viewController>
-                      <placeholder placeholderIdentifier='IBFirstResponder' id='iYj-Kq-Ea1' userLabel='First Responder' sceneMemberID='firstResponder'/>
-                    </objects>
-                    <point key='canvasLocation' x='53' y='375'/>
-                  </scene>
-                </scenes>
-              </document>
-      XML
-    
-          alias eql? ==
-    
-              if pods_by_state
-            {
-              :added => :added,
-              :changed => :changed,
-              :removed => :deleted,
-              :unchanged => :unchanged,
-            }.each do |state, spec_state|
-              Array(pods_by_state[state]).each do |name|
-                add_name(name, spec_state)
-              end
-            end
+        # @return [void]
+        #
+        def print
+          states = %i(added deleted changed unchanged)
+          lines(states).each do |line|
+            UI.message(line, '', 2)
           end
         end
     
-          it 'Does not render variant when no variants published' do
-        html_variant = create(:html_variant, published: false, approved: true)
-        get article.path + '?variant_version=1'
-        expect(response.body).not_to include html_variant.html
+            # Initialize a new instance
+        #
+        # @param [Sandbox] sandbox @see #sandbox
+        # @param [String] path @see #path
+        # @param [Array<PodTarget>] pod_targets @see #pod_targets
+        # @param [Hash{String=>Symbol}] build_configurations @see #build_configurations
+        # @param [Array<Platform>] platforms @see #platforms
+        # @param [String] object_version @see #object_version
+        # @param [String] podfile_path @see #podfile_path
+        #
+        def initialize(sandbox, path, pod_targets, build_configurations, platforms,
+                       object_version, podfile_path = nil, pod_target_subproject: false)
+          @sandbox = sandbox
+          @path = path
+          @pod_targets = pod_targets
+          @build_configurations = build_configurations
+          @platforms = platforms
+          @object_version = object_version
+          @podfile_path = podfile_path
+          @pod_target_subproject = pod_target_subproject
+        end
+    
+          #-----------------------------------------------------------------------#
+    
+        it 'complains if wrong parameters' do
+      lambda { run_command('lib', 'create') }.should.raise CLAide::Help
+    end
+    
+      def exists?(type, path)
+    %Q{[ -#{type} '#{path}' ]}
+  end
+    
+            if callable_without_parameters?(value_to_evaluate)
+          super(key, assert_valid_later(key, value_to_evaluate), &nil)
+        else
+          assert_valid_now(key, value_to_evaluate)
+          super
+        end
       end
     
-      def user_is_author?
-    if record.instance_of?(Article)
-      record.user_id == user.id
+    
+    {    if (pre.style.display == 'block') {
+      pre.style.display = 'none';
+      post.style.display = 'none';
+      context.style.background = 'none';
+    } else {
+      pre.style.display = 'block';
+      post.style.display = 'block';
+      context.style.background = '#fffed9';
+    }
+  }
+    
+            if has_vector?(request, headers)
+          warn env, 'attack prevented by #{self.class}'
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
+    
+        it 'copes with nested arrays' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']['bar']]]
+      end
+      get '/', :foo => {:bar => '<bar>'}
+      expect(body).to eq('&lt;bar&gt;')
+    end
+    
+          # Parse through mail to get the from/sender headers
+      mail = Mail.new(raw_message.split('\r\n\r\n', 2).first)
+      from_headers = {'from' => mail.from, 'sender' => mail.sender}
+      authenticated_domain = identity.server.find_authenticated_domain_from_headers(from_headers)
+    
+      rescue Acme::Client::Error => e
+    @retries = 0
+    if e.is_a?(Acme::Client::Error::BadNonce) && @retries < 5
+      @retries += 1
+      logger.info 'Bad nounce encountered. Retrying (#{@retries} of 5 attempts)'
+      sleep 1
+      verify
     else
-      record.pluck(:user_id).uniq == [user.id]
+      logger.info 'Error: #{e.class} (#{e.message})'
+      return false
     end
   end
     
-        respond_to do |format|
-      format.html do
-        render 'user', layout: false
-      end
-      format.png do
-        html = render_to_string('user', formats: :html, layout: false)
-        redirect_to HtmlCssToImage.fetch_url(html: html, css: PNG_CSS, google_fonts: 'Roboto'), status: :found
-      end
-    end
+                    begin
+                  if @fixed_result
+                    result = @fixed_result
+                  else
+                    case queued_message.message.endpoint
+                    when SMTPEndpoint
+                      sender = cached_sender(Postal::SMTPSender, queued_message.message.recipient_domain, nil, :servers => [queued_message.message.endpoint])
+                    when HTTPEndpoint
+                      sender = cached_sender(Postal::HTTPSender, queued_message.message.endpoint)
+                    when AddressEndpoint
+                      sender = cached_sender(Postal::SMTPSender, queued_message.message.endpoint.domain, nil, :force_rcpt_to => queued_message.message.endpoint.address)
+                    else
+                      log '#{log_prefix} Invalid endpoint for route (#{queued_message.message.endpoint_type})'
+                      queued_message.message.create_delivery('HardFail', :details => 'Invalid endpoint for route.')
+                      queued_message.destroy
+                      next
+                    end
+                    result = sender.send_message(queued_message.message)
+                    if result.connect_error
+                      @fixed_result = result
+                    end
+                  end
+                end
+    
+      def new
+    @ip_pool_rule = @server ? @server.ip_pool_rules.build : organization.ip_pool_rules.build
   end
     
-      def self.send_to_buffer(text, buffer_profile_id_code)
-    client = Buffer::Client.new(ApplicationConfig['BUFFER_ACCESS_TOKEN'])
-    client.create_update(
-      body: {
-        text:
-        text,
-        profile_ids: [
-          buffer_profile_id_code,
-        ]
-      },
-    )
-  end
-    
-          now = Time.current
-      @user.onboarding_package_requested_again = true if @user.onboarding_package_requested
-      @user.onboarding_package_requested = true
-      @user.onboarding_package_form_submmitted_at = now
-      @user.personal_data_updated_at = now
-      @user.shipping_validated_at = now if user_params[:shipping_validated] == '1'
-      if @user.save!
-        format.html { redirect_to '/freestickers/edit' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+        if time.nil?
+      raise TimeUndetermined, 'Couldn't determine a suitable time from '#{string}''
+    else
+      time
     end
   end
-    
-        context 'converter' do
-      it 'does not build Virtus::Attribute multiple times' do
-        subject.params do
-          requires :something, type: Array[String]
-        end
-        subject.get do
-        end
-    
-        def write(header)
-      @cookies.select { |key, _value| @send_cookies[key] == true }.each do |name, value|
-        cookie_value = value.is_a?(Hash) ? value : { value: value }
-        Rack::Utils.set_cookie_header! header, name, cookie_value
-      end
-    end
-    
-          # Fetch our current inheritable settings, which are inherited by
-      # nested scopes but not shared across siblings.
-      def inheritable_setting
-        @inheritable_setting ||= Grape::Util::InheritableSetting.new.tap { |new_settings| new_settings.inherit_from top_level_setting }
-      end
-    
-              presenter = found_code[2] if found_code
-        end
