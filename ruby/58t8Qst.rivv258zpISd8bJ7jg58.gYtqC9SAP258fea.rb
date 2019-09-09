d2fcc3c2877bf602c038e59b3929ad7687529f3c
SUNsung <weatherius@gmail.com>
2019-09-09 05:21:35 +0300

    Rewrite [58t8Qst.rivv258zpISd8bJ7jg58.gYtqC9SAP258fea.rb] File in: 2019-09-09 05:21:35

--- ruby/58t8Qst.rivv258zpISd8bJ7jg58.gYtqC9SAP258fea.rb
+++ ruby/58t8Qst.rivv258zpISd8bJ7jg58.gYtqC9SAP258fea.rb
@@ -1,139 +1,27 @@
 
         
-                path = @site.in_source_dir(File.join(dir, magic_dir, entry))
-        Document.new(path,
-                     :site       => @site,
-                     :collection => @site.posts)
-      end.reject(&:nil?)
-    end
+          def worker_class
+    ProofProvider::Keybase::Worker
+  end
     
-        def placeholders
-      {
-        :collection => @collection.label,
-        :path       => cleaned_relative_path,
-        :output_ext => '',
-        :name       => '',
-        :title      => '',
-      }
-    end
+        # In case for some reason we didn't have a redirect for the profile already, set it
+    origin_account.update(moved_to_account: target_account) if origin_account.moved_to_account_id.nil?
     
-            should 'create index URL based on filename' do
-          @page = setup_page('/contacts', 'index.html')
-          assert_equal '/contacts/', @page.url
-        end
-      end
-    
-        should 'not break kramdown' do
-      kramdown_doc = Kramdown::Document.new('# Some Header #', @config['kramdown'])
-      assert_equal :class, kramdown_doc.options[:syntax_highlighter_opts][:css]
-      assert_equal 'lipsum', kramdown_doc.options[:syntax_highlighter_opts][:foobar]
-    end
-    
-        require 'memory_profiler'
-    
-          def before_step(step)
-        @current_step = step
-      end
-    
-              theme.create!
-          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
-                             ' is ready for you in #{theme.path.to_s.cyan}!'
-          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
-        end
-        # rubocop:enable Metrics/AbcSize
+          it 'ensures recipient is not following sender' do
+        expect(recipient.following?(sender)).to be false
       end
     end
   end
-end
+    
+    run SinatraStaticServer
 
     
-          def use_identicon
-        @page.wiki.user_icons == 'identicon'
-      end
-    
-          def base_url
-        @base_url
-      end
-    
-    context 'Precious::Views::Editing' do
-  include Rack::Test::Methods
-  setup do
-    examples = testpath 'examples'
-    @path    = File.join(examples, 'test.git')
-    Precious::App.set(:gollum_path, @path)
-    FileUtils.cp_r File.join(examples, 'revert.git'), @path, :remove_destination => true
-    @wiki = Gollum::Wiki.new(@path)
-  end
-    
-        class Mgr
-      attr_reader :latest_error
-      attr_reader :mutex
-      attr_reader :cond
-      def initialize
-        @mutex = ::Mutex.new
-        @cond = ::ConditionVariable.new
-      end
-      def processor_died(inst, err)
-        @latest_error = err
-        @mutex.synchronize do
-          @cond.signal
-        end
-      end
-      def processor_stopped(inst)
-        @mutex.synchronize do
-          @cond.signal
-        end
-      end
-      def options
-        { :concurrency => 3, :queues => ['default'] }
-      end
-    end
-    
-        describe 'failed' do
-      it 'returns number of failed jobs' do
-        Sidekiq.redis { |conn| conn.set('stat:failed', 5) }
-        s = Sidekiq::Stats.new
-        assert_equal 5, s.failed
-      end
-    end
-    
-        it 'calculates an average poll interval based on the number of known Sidekiq processes' do
-      with_sidekiq_option(:average_scheduled_poll_interval, 10) do
-        3.times do |i|
-          Sidekiq.redis do |conn|
-            conn.sadd('processes', 'process-#{i}')
-            conn.hset('process-#{i}', 'info', nil)
-          end
-        end
-    
-    describe Sidekiq do
-  describe 'json processing' do
-    it 'handles json' do
-      assert_equal({'foo' => 'bar'}, Sidekiq.load_json('{\'foo\':\'bar\'}'))
-      assert_equal '{\'foo\':\'bar\'}', Sidekiq.dump_json({ 'foo' => 'bar' })
-    end
-  end
-    
-    self_read, self_write = IO.pipe
-%w(INT TERM TSTP TTIN).each do |sig|
-  begin
-    trap sig do
-      self_write.puts(sig)
-    end
-  rescue ArgumentError
-    puts 'Signal #{sig} not supported'
-  end
-end
-    
-    Sidekiq::Extensions.enable_delay!
+    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
 
     
-        initialize_with { Tmuxinator::Project.new(file, force_attach: true) }
-  end
+    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
+
     
-        it 'lists only projects in $TMUXINATOR_CONFIG when set' do
-      allow(ENV).to receive(:[]).with('TMUXINATOR_CONFIG').
-        and_return '#{fixtures_dir}/TMUXINATOR_CONFIG'
-      expect(described_class.configs).to eq ['TMUXINATOR_CONFIG']
-    end
-  end
+    module Jekyll
+    
+    
