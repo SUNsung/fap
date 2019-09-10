19a7db145abee653f316168a2ff4e5af7324d4dd
SUNsung <weatherius@gmail.com>
2019-09-10 18:20:05 +0300

    Rewrite [32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp] File in: 2019-09-10 18:20:05

--- cccccc/32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp
+++ cccccc/32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp
@@ -1,319 +1,266 @@
 
         
         
-    {
-  return PyEnumDescriptor_FromDescriptor(enum_descriptor);
-}
-    
-      if (child_stdin_ != NULL) {
-    // Child did not finish reading input before it closed the output.
-    // Presumably it exited with an error.
-    CloseHandleOrDie(child_stdin_);
-    child_stdin_ = NULL;
-  }
-    
-      // Absolute value of the difference between two numbers.
-  // Works correctly for signed types and special floating point values.
-  template<typename T>
-  static typename MathLimits<T>::UnsignedType AbsDiff(const T x, const T y) {
-    // Carries out arithmetic as unsigned to avoid overflow.
-    typedef typename MathLimits<T>::UnsignedType R;
-    return x > y ? R(x) - R(y) : R(y) - R(x);
-  }
-    
-    TEST_F(JsonObjectWriterTest, TestWebsafeByteEncoding) {
-  ow_ = new JsonObjectWriter('', out_stream_);
-  ow_->set_use_websafe_base64_for_bytes(true);
-  ow_->StartObject('')
-      ->RenderBytes('bytes', '\x03\xef\xc0\x10')
-      ->EndObject();
-    }
-    
-      auto* message1_on_arena =
-      Arena::CreateMessage<protobuf_unittest::TestAllTypes>(&arena);
-  TestUtil::SetAllFields(message1_on_arena);
-  const auto* nested = &message1_on_arena->optional_nested_message();
-    
-      desired_output_for_decode = 'abcdezghIJ';
-  expected = string('\0abcdezghIJ\0', 12);
-  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
-                                                     desired_output_for_decode);
-  EXPECT_EQ(expected, result);
-    
-    std::string ReadFile(const std::string& name) {
-  std::ifstream file(name.c_str());
-  GOOGLE_CHECK(file.is_open()) << 'Couldn't find file ''
-      << name
-      << '', please make sure you are running this command from the benchmarks'
-      << ' directory.\n';
-  return std::string((std::istreambuf_iterator<char>(file)),
-                     std::istreambuf_iterator<char>());
-}
-    
-      //----- text drawing
-  virtual void drawString(GfxState *state, GooString *s);
-  virtual void beginTextObject(GfxState *state);
-  virtual GBool deviceHasTextClip(GfxState *state) { return haveTextClip && haveCSPattern; }
-  virtual void endTextObject(GfxState *state);
-    
-      // Get alignment
-  PageTransitionAlignment getAlignment() { return alignment; }
-    
-    #ifdef USE_GCC_PRAGMAS
-#pragma implementation
-#endif
-    
-    
     { private:
-  // vector: node_id -> elements
-  std::vector<Elem> elem_of_each_node_;
+  const ShapeTree<BufferAllocation::Slice> infeed_slices_;
 };
     
-    #ifdef __CUDACC__
-  __device__ void AtomicWriteSymbol
-    (CompressedByteT* buffer, uint64_t symbol, size_t offset) {
-    size_t ibit_start = offset * symbol_bits_;
-    size_t ibit_end = (offset + 1) * symbol_bits_ - 1;
-    size_t ibyte_start = ibit_start / 8, ibyte_end = ibit_end / 8;
-    }
+    #include 'tensorflow/compiler/xla/service/gpu/hlo_execution_profiler.h'
+#include 'tensorflow/core/lib/core/errors.h'
     
-    namespace aria2 {
-    }
+    #endif 
+
     
+    #include 'ImfAttribute.h'
+#include 'ImfNamespace.h'
     
+        if (getVersion (version) != EXR_VERSION)
     {
-    {    PrefPtr prefEntryPointHost = family == AF_INET ? PREF_DHT_ENTRY_POINT_HOST
-                                                   : PREF_DHT_ENTRY_POINT_HOST6;
-    if (!e->getOption()->get(prefEntryPointHost).empty()) {
-      {
-        PrefPtr prefEntryPointPort = family == AF_INET
-                                         ? PREF_DHT_ENTRY_POINT_PORT
-                                         : PREF_DHT_ENTRY_POINT_PORT6;
-        std::pair<std::string, uint16_t> addr(
-            e->getOption()->get(prefEntryPointHost),
-            e->getOption()->getAsInt(prefEntryPointPort));
-        std::vector<std::pair<std::string, uint16_t>> entryPoints;
-        entryPoints.push_back(addr);
-        auto command = make_unique<DHTEntryPointNameResolveCommand>(
-            e->newCUID(), e, family, entryPoints);
-        command->setBootstrapEnabled(true);
-        command->setTaskQueue(taskQueue.get());
-        command->setTaskFactory(taskFactory.get());
-        command->setRoutingTable(routingTable.get());
-        command->setLocalNode(localNode);
-        tempCommands.push_back(std::move(command));
-      }
-    }
-    else {
-      A2_LOG_INFO('No DHT entry point specified.');
-    }
-    {
-      auto command = make_unique<DHTInteractionCommand>(e->newCUID(), e);
-      command->setMessageDispatcher(dispatcher.get());
-      command->setMessageReceiver(receiver.get());
-      command->setTaskQueue(taskQueue.get());
-      command->setReadCheckSocket(connection->getSocket());
-      command->setConnection(std::move(connection));
-      command->setUDPTrackerClient(udpTrackerClient);
-      tempRoutineCommands.push_back(std::move(command));
-    }
-    {
-      auto command = make_unique<DHTTokenUpdateCommand>(
-          e->newCUID(), e, DHT_TOKEN_UPDATE_INTERVAL);
-      command->setTokenTracker(tokenTracker.get());
-      tempCommands.push_back(std::move(command));
-    }
-    {
-      auto command = make_unique<DHTBucketRefreshCommand>(
-          e->newCUID(), e, DHT_BUCKET_REFRESH_CHECK_INTERVAL);
-      command->setTaskQueue(taskQueue.get());
-      command->setRoutingTable(routingTable.get());
-      command->setTaskFactory(taskFactory.get());
-      tempCommands.push_back(std::move(command));
-    }
-    {
-      auto command = make_unique<DHTPeerAnnounceCommand>(
-          e->newCUID(), e, DHT_PEER_ANNOUNCE_CHECK_INTERVAL);
-      command->setPeerAnnounceStorage(peerAnnounceStorage.get());
-      tempCommands.push_back(std::move(command));
-    }
-    {
-      auto command =
-          make_unique<DHTAutoSaveCommand>(e->newCUID(), e, family, 30_min);
-      command->setLocalNode(localNode);
-      command->setRoutingTable(routingTable.get());
-      tempCommands.push_back(std::move(command));
-    }
-    // add deserialized nodes to routing table
-    auto& desnodes = deserializer.getNodes();
-    for (auto& node : desnodes) {
-      routingTable->addNode(node);
-    }
-    if (!desnodes.empty()) {
-      auto task = std::static_pointer_cast<DHTBucketRefreshTask>(
-          taskFactory->createBucketRefreshTask());
-      task->setForceRefresh(true);
-      taskQueue->addPeriodicTask1(task);
-    }
-    // assign them into DHTRegistry
-    if (family == AF_INET) {
-      DHTRegistry::getMutableData().localNode = localNode;
-      DHTRegistry::getMutableData().routingTable = std::move(routingTable);
-      DHTRegistry::getMutableData().taskQueue = std::move(taskQueue);
-      DHTRegistry::getMutableData().taskFactory = std::move(taskFactory);
-      DHTRegistry::getMutableData().peerAnnounceStorage =
-          std::move(peerAnnounceStorage);
-      DHTRegistry::getMutableData().tokenTracker = std::move(tokenTracker);
-      DHTRegistry::getMutableData().messageDispatcher = std::move(dispatcher);
-      DHTRegistry::getMutableData().messageReceiver = std::move(receiver);
-      DHTRegistry::getMutableData().messageFactory = std::move(factory);
-      e->getBtRegistry()->setUDPTrackerClient(udpTrackerClient);
-      DHTRegistry::setInitialized(true);
-    }
-    else {
-      DHTRegistry::getMutableData6().localNode = localNode;
-      DHTRegistry::getMutableData6().routingTable = std::move(routingTable);
-      DHTRegistry::getMutableData6().taskQueue = std::move(taskQueue);
-      DHTRegistry::getMutableData6().taskFactory = std::move(taskFactory);
-      DHTRegistry::getMutableData6().peerAnnounceStorage =
-          std::move(peerAnnounceStorage);
-      DHTRegistry::getMutableData6().tokenTracker = std::move(tokenTracker);
-      DHTRegistry::getMutableData6().messageDispatcher = std::move(dispatcher);
-      DHTRegistry::getMutableData6().messageReceiver = std::move(receiver);
-      DHTRegistry::getMutableData6().messageFactory = std::move(factory);
-      DHTRegistry::setInitialized6(true);
-    }
-    if (e->getBtRegistry()->getUdpPort() == 0) {
-      // We assign port last so that no exception gets in the way
-      e->getBtRegistry()->setUdpPort(port);
-    }
-  }
-  catch (RecoverableException& ex) {
-    A2_LOG_ERROR_EX(fmt('Exception caught while initializing DHT functionality.'
-                        ' DHT is disabled.'),
-                    ex);
-    tempCommands.clear();
-    tempRoutineCommands.clear();
-    if (family == AF_INET) {
-      DHTRegistry::clearData();
-      e->getBtRegistry()->setUDPTrackerClient(
-          std::shared_ptr<UDPTrackerClient>{});
-    }
-    else {
-      DHTRegistry::clearData6();
-    }
-  }
-  return std::make_pair(std::move(tempCommands),
-                        std::move(tempRoutineCommands));
-}
-    
-    class DHTTask {
-public:
-  virtual ~DHTTask() = default;
-    }
-    
-    #include <algorithm>
-    
-    
-    {} // namespace aria2
-    
-    namespace aria2 {
-    }
-    
-    #endif  // !defined(ROCKSDB_LITE)
-    
-    namespace rocksdb {
-    }
-    
-    #include 'utilities/transactions/transaction_db_mutex_impl.h'
-    
-    namespace rocksdb {
-    }
-    
-      // put and get from non-default column family
-  s = db->Put(WriteOptions(), handles[1], Slice('key'), Slice('value'));
-  assert(s.ok());
-  std::string value;
-  s = db->Get(ReadOptions(), handles[1], Slice('key'), &value);
-  assert(s.ok());
-    
-    
-    {  mutable int count_ = 0;
-  mutable int merge_count_ = 0;
-};
-    
-    using namespace rocksdb;
-    
-      // Any internal progress/error information generated by the db will
-  // be written to info_log if it is non-NULL, or to a file stored
-  // in the same directory as the DB contents if info_log is NULL.
-  // Default: NULL
-  Logger* info_log;
-    
-    namespace rocksdb {
-namespace lua {
-class LuaStateWrapper {
- public:
-  explicit LuaStateWrapper(const std::string& lua_script) {
-    lua_state_ = luaL_newstate();
-    Init(lua_script, {});
-  }
-  LuaStateWrapper(
-      const std::string& lua_script,
-      const std::vector<std::shared_ptr<RocksLuaCustomLibrary>>& libraries) {
-    lua_state_ = luaL_newstate();
-    Init(lua_script, libraries);
-  }
-  lua_State* GetLuaState() const { return lua_state_; }
-  ~LuaStateWrapper() { lua_close(lua_state_); }
-    }
-    }
+        THROW (IEX_NAMESPACE::InputExc, 'Cannot read '
+                              'version ' << getVersion (version) << ' '
+                              'image files.  Current file format version '
+                              'is ' << EXR_VERSION << '.');
     }
     
     
-    {    //1
-    ret = p.read(&p, buf, sizeof(buf));
-    if (ret < 0)
-    {
-        swSysWarn('read() failed.');
-    }
-    ASSERT_GT(ret, 0);
-    ASSERT_EQ(strcmp('hello world1', buf), 0);
-    //2
-    ret = p.read(&p, buf, sizeof(buf));
-    ASSERT_GT(ret, 0);
-    ASSERT_EQ(strcmp('hello world2', buf), 0);
-    //3
-    ret = p.read(&p, buf, sizeof(buf));
-    ASSERT_GT(ret, 0);
-    ASSERT_EQ(strcmp('hello world3', buf), 0);
-}
-    
-    bool zend::eval(std::string code, std::string filename)
+IMATH_NAMESPACE::V2f &	
+Header::screenWindowCenter ()
 {
-    return zend_eval_stringl((char*) code.c_str(), code.length(), nullptr, (char *) filename.c_str()) == SUCCESS;
+    return static_cast <V2fAttribute &>
+	((*this)['screenWindowCenter']).value();
+}
+    
+    
+int
+hufCompress (const unsigned short raw[],
+	     int nRaw,
+	     char compressed[])
+{
+    if (nRaw == 0)
+	return 0;
+    }
+    
+    
+    {            ifd->tFile->readTiles (0, ifd->tFile->numXTiles (0) - 1, j, j);
+            ifd->cachedTileY = j;
+        }
+    
+    
+OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_EXIT 
+
+    
+    // The Blank Application template is documented at https://go.microsoft.com/fwlink/?LinkId=402347&clcid=0x409
+    
+    
+    {        return result;
+    }
+    
+    
+    {
+    {        std::shared_ptr<UCM::IUnitConverterVMCallback> m_vmCallback;
+        std::vector<std::tuple<std::wstring, UCM::Unit>> m_suggestedList;
+        std::wstring m_curValue;
+    };
 }
 
     
-            for (int i = 0; i < 100; ++i)
+        spHistoryItem->historyItemVector.spTokens = tokens;
+    spHistoryItem->historyItemVector.spCommands = commands;
+    
+            // IVector<Object^>
+          // Not implemented methods
+        virtual void Append(Platform::Object^ /*item*/) = Windows::Foundation::Collections::IVector<Platform::Object^>::Append
+            {
+                throw ref new Platform::NotImplementedException();
+            }
+            virtual void Clear() = Windows::Foundation::Collections::IVector<Platform::Object ^>::Clear
+            {
+                throw ref new Platform::NotImplementedException();
+            }
+            virtual unsigned int GetMany(
+                unsigned int /*startIndex*/,
+                Platform::WriteOnlyArray<Platform::Object ^> ^ /*items*/) = Windows::Foundation::Collections::IVector<Platform::Object ^>::GetMany
+            {
+                throw ref new Platform::NotImplementedException();
+            }
+            virtual Windows::Foundation::Collections::IVectorView<Platform::Object ^> ^ GetView() = Windows::Foundation::Collections::IVector<
+                Platform::Object ^>::GetView
+            {
+                throw ref new Platform::NotImplementedException();
+            }
+            virtual void InsertAt(unsigned int /*index*/, Platform::Object ^ /*item*/) = Windows::Foundation::Collections::IVector<Platform::Object ^>::InsertAt
+            {
+                throw ref new Platform::NotImplementedException();
+            }
+            virtual void RemoveAt(unsigned int /*index*/) = Windows::Foundation::Collections::IVector<Platform::Object ^>::RemoveAt
+            {
+                throw ref new Platform::NotImplementedException();
+            }
+            virtual void RemoveAtEnd() = Windows::Foundation::Collections::IVector<Platform::Object ^>::RemoveAtEnd
+            {
+                throw ref new Platform::NotImplementedException();
+            }
+            virtual void
+            ReplaceAll(const Platform::Array<Platform::Object ^> ^ /*items*/) = Windows::Foundation::Collections::IVector<Platform::Object ^>::ReplaceAll
+            {
+                throw ref new Platform::NotImplementedException();
+            }
+            virtual void SetAt(unsigned int /*index*/, Platform::Object ^ /*item*/) = Windows::Foundation::Collections::IVector<Platform::Object ^>::SetAt
+            {
+                throw ref new Platform::NotImplementedException();
+            }
+    
+    NarratorAnnouncement ^ CalculatorAnnouncement::GetCategoryNameChangedAnnouncement(String ^ announcement)
+{
+    return ref new NarratorAnnouncement(
+        announcement,
+        CalculatorActivityIds::CategoryNameChanged,
+        AutomationNotificationKind::ActionCompleted,
+        AutomationNotificationProcessing::ImportantMostRecent);
+}
+    
+    #include <adapters/qt.h>
+    
+                    /* force into an fd */
+                if (stream == NULL || FAILURE == php_stream_cast(stream, PHP_STREAM_CAST_RELEASE|PHP_STREAM_AS_FD, (void ** )&fd, REPORT_ERRORS))
+                {
+                    goto exit_fail;
+                }
+    
+        string relativePath = string();
+    size_t position = fullPath.find(apkprefix);
+    if (0 == position) {
+        // 'assets/' is at the beginning of the path and we don't want it
+        relativePath += fullPath.substr(apkprefix.size());
+    } else {
+        relativePath = fullPath;
+    }
+    
+    if (obbfile)
+    {
+        if (obbfile->getFileData(relativePath, buffer))
+            return FileUtils::Status::OK;
+    }
+    
+    extern const char * getApkPath();
+extern std::string getPackageNameJNI();
+extern int getObbAssetFileDescriptorJNI(const char* path, long* startOffset, long* size);
+extern void conversionEncodingJNI(const char* src, int byteSize, const char* fromCharset, char* dst, const char* newCharset);
+extern bool removeDirectoryJNI(const char* path);
+    
+    void RelativeLayoutManager::doLayout(LayoutProtocol *layout)
+{
+    
+    _widgetChildren = this->getAllWidgets(layout);
+    
+    while (_unlayoutChildCount > 0)
+    {
+        for (auto& subWidget : _widgetChildren)
         {
-            std::string addr2 = System::gethostbyname('www.baidu.com', AF_INET);
-            ASSERT_EQ(addr1, addr2);
+            _widget = static_cast<Widget*>(subWidget);
+            
+            RelativeLayoutParameter* layoutParameter = dynamic_cast<RelativeLayoutParameter*>(_widget->getLayoutParameter());
+            
+            if (layoutParameter)
+            {
+                if (layoutParameter->_put)
+                {
+                    continue;
+                }
+                
+               
+                bool ret = this->calculateFinalPositionWithRelativeWidget(layout);
+                if (!ret) {
+                    continue;
+                }
+                
+                this->calculateFinalPositionWithRelativeAlign();
+            
+            
+                _widget->setPosition(Vec2(_finalPositionX, _finalPositionY));
+                
+                layoutParameter->_put = true;
+            }
         }
-    
-    TEST(hashmap, integer)
-{
-    swHashMap *ht = swHashMap_new(16, free);
-    swFdInfo *pkt, *tmp;
-    int i;
-    swFdInfo *lists[MAP_SIZE];
+        _unlayoutChildCount--;
+    }
     }
     
-    TEST(lru_cache, lru_kick)
+    void SpritePolygonTest2::initSprites()
 {
-    dtor_num = 0;
-    shared_ptr<lru_cache_test_class> val = make_shared<lru_cache_test_class>();
-    shared_ptr<lru_cache_test_class> val1 = make_shared<lru_cache_test_class>();
-    shared_ptr<lru_cache_test_class> val2 = make_shared<lru_cache_test_class>();
-    shared_ptr<lru_cache_test_class> val3 = make_shared<lru_cache_test_class>();
+    auto s = Director::getInstance()->getWinSize();
+    auto offset = Vec2(0.15*s.width,0);
+    auto filename = s_pathGrossini;
+    Rect head = Rect(30,25,25,25);
+    
+    //Sprite
+    auto pinfo = AutoPolygon::generatePolygon(filename, head);
+    _polygonSprite = Sprite::create(pinfo);
+    _polygonSprite->setTag(101);
+    addChild(_polygonSprite);
+    _polygonSprite->setPosition(Vec2(s)/2 + offset);
+    
+    _normalSprite = Sprite::create(filename,head);
+    _normalSprite->setTag(100);
+    addChild(_normalSprite);
+    _normalSprite->setPosition(Vec2(s)/2 - offset);
     }
+    
+    It is a good practice to use a Scene as the parent of all your nodes.
+ 
+Scene will create a default camera for you.
+*/
+class CC_DLL Scene : public Node
+{
+public:
+    /** Creates a new Scene object. 
+     *
+     * @return An autoreleased Scene object.
+     */
+    static Scene *create();
+    }
+    
+    std::string SpriteRenderTextureBug::title() const
+{
+    return 'SpriteRenderTextureBug';
+}
+    
+    
+    {
+    {        if (JNI_OK == status_) {
+            we_attach_ = true;
+            pthread_setspecific(g_env_key, env_);
+        } else {
+            ASSERT2(false, 'vm:%p, env:%p, status:%d', vm_, env_, status_);
+            env_ = NULL;
+            return;
+        }
+    } while (false);
+    
+    jint ret = env_->PushLocalFrame(_capacity);
+    ASSERT2(0 == ret, 'ret:%d', ret);
+}
+    
+        TrafficStatistics(const TrafficStatistics&);
+    TrafficStatistics& operator=(const TrafficStatistics&);
+    
+    #include 'mars/baseevent/active_logic.h'
+#include 'mars/baseevent/baseprjevent.h'
+#include 'mars/comm/singleton.h'
+#include 'mars/comm/xlogger/xlogger.h'
+#include 'mars/comm/thread/lock.h'
+#include 'mars/comm/time_utils.h'
+#include 'mars/comm/bootrun.h'
+#include 'mars/comm/messagequeue/message_queue.h'
+    
+    #include <exception>
+#include 'comm/xlogger/xlogger.h'
+    
+    
+enum {
+    kGetADDRNotBegin,
+    kGetADDRDoing,
+    kGetADDRTimeout,
+    kGetADDRSuc,
+    kGetADDRFail,
+};
