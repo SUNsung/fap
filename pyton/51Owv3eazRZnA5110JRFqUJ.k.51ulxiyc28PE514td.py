493d93fb793ca4058b626807ec3fe1472ed5e0b2
SUNsung <weatherius@gmail.com>
2019-09-11 01:22:07 +0300

    Rewrite [51Owv3eazRZnA5110JRFqUJ.k.51ulxiyc28PE514td.py] File in: 2019-09-11 01:22:06

--- pyton/51Owv3eazRZnA5110JRFqUJ.k.51ulxiyc28PE514td.py
+++ pyton/51Owv3eazRZnA5110JRFqUJ.k.51ulxiyc28PE514td.py
@@ -1,144 +1,80 @@
 
         
-            # Loading and vectorizing the data:
-    print('====== %s ======' % dat)
-    print('--- Fetching data...')
-    if dat in ['http', 'smtp', 'SF', 'SA']:
-        dataset = fetch_kddcup99(subset=dat, shuffle=True,
-                                 percent10=True, random_state=random_state)
-        X = dataset.data
-        y = dataset.target
+            # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule('/', endpoint='index')
     
-    :class:`sklearn.covariance.EllipticEnvelope` assumes the data is Gaussian and
-learns an ellipse. It thus degrades when the data is not unimodal. Notice
-however that this estimator is robust to outliers.
+            if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ? WHERE id = ?', (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
     
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
-    
-        for line in input_file:
-        linestrip = line.strip()
-        if len(linestrip) == 0:
-            in_exercise_region = False
-        elif linestrip.startswith('# TASK:'):
-            in_exercise_region = True
-    
+        Validates that the username is not already taken. Hashes the
+    password for security.
     '''
-print(__doc__)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
     
-    plt.show()
+        return g.db
+    
+    
+# noinspection PyAbstractClass
+class BuiltinAuthPlugin(AuthPlugin):
+    package_name = '(builtin)'
+    
+            '''
+        available_plugins = plugin_manager.get_formatters_grouped()
+        self.enabled_plugins = []
+        for group in groups:
+            for cls in available_plugins[group]:
+                p = cls(env=env, **kwargs)
+                if p.enabled:
+                    self.enabled_plugins.append(p)
+    
+            '''
+    
+        plugin_manager.register(Plugin)
+    
+        for line, diags in iteritems( self._line_to_diags ):
+      if not diags:
+        continue
+    
+    
+def SendEventNotificationAsync( event_name,
+                                buffer_number = None,
+                                extra_data = None ):
+  event = EventNotification( event_name, buffer_number, extra_data )
+  event.Start()
 
     
+      opts = { 'filter_diagnostics' : {
+    'java,c,cs' : { 'regex' : '.*taco.*' } } }
     
-def is_setting_index(node):
-    if node.tagname == 'index':
-        # index entries for setting directives look like:
-        # [(u'pair', u'SETTING_NAME; setting', u'std:setting-SETTING_NAME', '')]
-        entry_type, info, refid = node['entries'][0][:3]
-        return entry_type == 'pair' and info.endswith('; setting')
-    return False
-    
-        def run(self, args, opts):
-        '''
-        Entry point for running commands
-        '''
-        raise NotImplementedError
-
-    
-            # scrapy already handles #! links properly
-        ajax_crawl_request = request.replace(url=request.url+'#!')
-        logger.debug('Downloading AJAX crawlable %(ajax_crawl_request)s instead of %(request)s',
-                     {'ajax_crawl_request': ajax_crawl_request, 'request': request},
-                     extra={'spider': spider})
-    
-    # If true, show URL addresses after external links.
-#man_show_urls = False
-    
-    # Load a test image and get encondings for it
-image_to_test = face_recognition.load_image_file('obama2.jpg')
-image_to_test_encoding = face_recognition.face_encodings(image_to_test)[0]
-    
-    
-def show_prediction_labels_on_image(img_path, predictions):
-    '''
-    Shows the face recognition results visually.
-    
-    # Load the jpg file into a numpy array
-image = face_recognition.load_image_file('biden.jpg')
-    
-            if file and allowed_file(file.filename):
-            # The image file seems valid! Detect faces and return the result.
-            return detect_faces_in_image(file)
-    
-            self.assertEqual(len(face_landmarks), 1)
-        self.assertEqual(face_landmarks[0].num_parts, 68)
-        self.assertEqual((example_landmark.x, example_landmark.y), (552, 399))
-    
-    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-            # Wait to read
-        while Global.read_num != worker_id or Global.read_num != prev_id(Global.buff_num, worker_num):
-            time.sleep(0.01)
-    
-    with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-    
-            #Creating directory title page for current directory
-        with open('dir_title.html', 'w') as os_html:
-            os_html.write(header + dir_title)
-        
-        group.append(HTML('dir_title.html').render())
-    
-    # 引入 Perceptron 类
-from __future__ import print_function
-from perceptron import Perceptron
-    
-    
-# def mineTweets(tweetArr, minSup=5):
-#     '''
-#     获取频繁项集
-#     '''
-#     parsedList = []
-#     for i in range(14):
-#         for j in range(100):
-#             parsedList.append(textParse(tweetArr[i][j].text))
-#     initSet = createInitSet(parsedList)
-#     myFPtree, myHeaderTab = createTree(initSet, minSup)
-#     myFreqList = []
-#     mineTree(myFPtree, myHeaderTab, minSup, set([]), myFreqList)
-#     return myFreqList
-    
-    
-def analyse_data(dataMat):
-    meanVals = mean(dataMat, axis=0)
-    meanRemoved = dataMat-meanVals
-    covMat = cov(meanRemoved, rowvar=0)
-    eigvals, eigVects = linalg.eig(mat(covMat))
-    eigValInd = argsort(eigvals)
-    
-        # 分析插入的 Sigma 长度
-    analyse_data(Sigma, 20)
-    
-            for index in self.dataList:
-            wDelta += float(labels[index]) * X[index, :]  # wDelta += label*dataSet
-        eta = 1.0/(2.0*self.t)       # calc new: eta
-        # calc new: w = (1.0 - 1/t)*w + (eta/k)*wDelta
-        wMat = (1.0 - 1.0/self.t)*wMat + (eta/self.k)*wDelta
-        for mapperNum in range(1, self.numMappers+1):
-            yield (mapperNum, ['w', wMat.tolist()[0]])    # 发出 w
-            if self.t < self.options.iterations:
-                yield (mapperNum, ['t', self.t+1])        # 增量 T
-                for j in range(self.k/self.numMappers):   # emit random ints for mappers iid
-                    yield (mapperNum, ['x', random.randint(shape(self.data)[0])])
-    
-    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+        >>> Point = namedtuple('Point', 'x y')
+    >>> Point.__doc__                   # docstring for the new class
+    'Point(x, y)'
+    >>> p = Point(11, y=22)             # instantiate with positional args or keywords
+    >>> p[0] + p[1]                     # indexable like a plain tuple
+    33
+    >>> x, y = p                        # unpack like a regular tuple
+    >>> x, y
+    (11, 22)
+    >>> p.x + p.y                       # fields also accessable by name
+    33
+    >>> d = p._asdict()                 # convert to a dictionary
+    >>> d['x']
+    11
+    >>> Point(**d)                      # convert from a dictionary
+    Point(x=11, y=22)
+    >>> p._replace(x=100)               # _replace() is like str.replace() but targets named fields
+    Point(x=100, y=22)
