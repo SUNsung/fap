9e19fc12a52676a2e00734d6084e27b44aeaef74
SUNsung <weatherius@gmail.com>
2019-09-10 21:19:33 +0300

    Rewrite [57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py] File in: 2019-09-10 21:19:32

--- pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
+++ pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
@@ -1,77 +1,106 @@
 
         
-        
-class TimeField(BaseTemporalField):
-    widget = TimeInput
-    input_formats = formats.get_format_lazy('TIME_INPUT_FORMATS')
-    default_error_messages = {
-        'invalid': _('Enter a valid time.')
-    }
+        # Set parameters:
+# ngram_range = 2 will add bi-grams features
+ngram_range = 1
+max_features = 20000
+maxlen = 400
+batch_size = 32
+embedding_dims = 50
+epochs = 5
     
-        def test_inline_editable_pk(self):
-        response = self.client.get(reverse('admin:admin_inlines_author_add'))
-        self.assertContains(
-            response,
-            '<input class='vIntegerField' id='id_editablepkbook_set-0-manual_pk' '
-            'name='editablepkbook_set-0-manual_pk' type='number'>',
-            html=True, count=1
-        )
-        self.assertContains(
-            response,
-            '<input class='vIntegerField' id='id_editablepkbook_set-2-0-manual_pk' '
-            'name='editablepkbook_set-2-0-manual_pk' type='number'>',
-            html=True, count=1
-        )
     
-        def test_object_at_custom_name_in_context_data(self):
-        # Checks 'pony' key presence in dict returned by get_context_date
-        test_view = views.CustomSingleObjectView()
-        test_view.context_object_name = 'pony'
-        context = test_view.get_context_data()
-        self.assertEqual(context['pony'], test_view.object)
-    
-        def as_mysql(self, compiler, connection, **extra_context):
-        sql, params = super().as_sql(compiler, connection, **extra_context)
-        if self.output_field.get_internal_type() == 'DurationField':
-            sql = 'CAST(%s AS SIGNED)' % sql
-        return sql, params
-    
-                try:
-                vtup = self.postgis_version_tuple()
-            except ProgrammingError:
-                raise ImproperlyConfigured(
-                    'Cannot determine PostGIS version for database '%s' '
-                    'using command 'SELECT postgis_lib_version()'. '
-                    'GeoDjango requires at least PostGIS version 2.2. '
-                    'Was the database created from a spatial database '
-                    'template?' % self.connection.settings_dict['NAME']
-                )
-            version = vtup[1:]
-        return version
-    
-        def __iter__(self):
-        'Iterate over each Feature in the Layer.'
-        # ResetReading() must be called before iteration is to begin.
-        capi.reset_reading(self._ptr)
-        for i in range(self.num_feat):
-            yield Feature(capi.get_next_feature(self._ptr), self)
-    
-        :param filepath: Optional filepath the the blns.txt file
-    :returns: The list of naughty strings
+def create_pairs(x, digit_indices):
+    '''Positive and negative pair creation.
+    Alternates between positive and negative pairs.
     '''
+    pairs = []
+    labels = []
+    n = min([len(digit_indices[d]) for d in range(num_classes)]) - 1
+    for d in range(num_classes):
+        for i in range(n):
+            z1, z2 = digit_indices[d][i], digit_indices[d][i + 1]
+            pairs += [[x[z1], x[z2]]]
+            inc = random.randrange(1, num_classes)
+            dn = (d + inc) % num_classes
+            z1, z2 = digit_indices[d][i], digit_indices[dn][i]
+            pairs += [[x[z1], x[z2]]]
+            labels += [1, 0]
+    return np.array(pairs), np.array(labels)
     
-    This pattern aims to decouple the senders of a request from its
-receivers by allowing request to move through chained
-receivers until it is handled.
     
-    *Examples in Python ecosystem:
-- Python's ast.NodeVisitor: https://github.com/python/cpython/blob/master/Lib/ast.py#L250
-which is then being used e.g. in tools like `pyflakes`.
-- `Black` formatter tool implements it's own: https://github.com/ambv/black/blob/master/black.py#L718
+def test_reuters_load_does_not_affect_global_rng(fake_downloaded_reuters_path):
+    np.random.seed(1337)
+    before = np.random.randint(0, 100, size=10)
+    
+    from .merge import Add
+from .merge import Subtract
+from .merge import Multiply
+from .merge import Average
+from .merge import Maximum
+from .merge import Minimum
+from .merge import Concatenate
+from .merge import Dot
+from .merge import add
+from .merge import subtract
+from .merge import multiply
+from .merge import average
+from .merge import maximum
+from .merge import minimum
+from .merge import concatenate
+from .merge import dot
+    
+    Output after 4 epochs on CPU: ~0.8146
+Time per epoch on CPU (Core i7): ~150s.
 '''
     
-        graphic.render()
+    In this examples we will use a movie review dataset.
+    
+    This example demonstrates how to generate a checkerboard dataset and
+bicluster it using the Spectral Biclustering algorithm.
     
     
-class ItalicWrapper(TextTag):
-    '''Wraps a tag in <i>'''
+X = np.concatenate((x, y))
+X += .7 * np.random.randn(2, n_samples)
+X = X.T
+    
+        def process(self, detected_face, predicted_mask=None):
+        ''' Return mask and perform processing '''
+        mask = self.get_mask(detected_face, predicted_mask)
+        raw_mask = mask.copy()
+        if not self.skip and self.do_erode:
+            mask = self.erode(mask)
+        if not self.skip and self.do_blend:
+            mask = self.blend(mask)
+        raw_mask = np.expand_dims(raw_mask, axis=-1) if raw_mask.ndim != 3 else raw_mask
+        mask = np.expand_dims(mask, axis=-1) if mask.ndim != 3 else mask
+        logger.trace('mask shape: %s, raw_mask shape: %s', mask.shape, raw_mask.shape)
+        return mask, raw_mask
+    
+    
+class Adjustment():
+    ''' Parent class for scaling adjustments '''
+    def __init__(self, configfile=None, config=None):
+        logger.debug('Initializing %s: (configfile: %s, config: %s)',
+                     self.__class__.__name__, configfile, config)
+        self.config = self.set_config(configfile, config)
+        logger.debug('config: %s', self.config)
+        logger.debug('Initialized %s', self.__class__.__name__)
+    
+            background = self.background
+        pad = self.pad
+        widget = self.widget
+    
+    
+def rmod(left, right):
+    # check if right is a string as % is the string
+    # formatting operation; this is a TypeError
+    # otherwise perform the op
+    if isinstance(right, str):
+        raise TypeError(
+            '{typ} cannot perform the operation mod'.format(typ=type(left).__name__)
+        )
+    
+        path_types.append(Path)
+except ImportError:
+    pass
