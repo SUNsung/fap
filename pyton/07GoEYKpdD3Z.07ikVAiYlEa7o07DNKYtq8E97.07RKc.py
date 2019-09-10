94b50c482a543d41dde83a932a9ed0bfceddb4fe
SUNsung <weatherius@gmail.com>
2019-09-10 09:39:49 +0300

    Rewrite [07GoEYKpdD3Z.07ikVAiYlEa7o07DNKYtq8E97.07RKc.py] File in: 2019-09-10 09:39:49

--- pyton/07GoEYKpdD3Z.07ikVAiYlEa7o07DNKYtq8E97.07RKc.py
+++ pyton/07GoEYKpdD3Z.07ikVAiYlEa7o07DNKYtq8E97.07RKc.py
@@ -1,101 +1,90 @@
 
         
-                # Load GoAgent CA
-        with open(CertUtil.ca_keyfile, 'rb') as fp:
-            content = fp.read()
-        ca = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, content)
-        CertUtil.ca_privatekey = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, content)
-        CertUtil.ca_thumbprint = ca.digest('sha1')
-        CertUtil.ca_subject = ca.get_subject()
-        ca_cert_error = True
-        if os.path.exists(CertUtil.ca_certfile):
-            with open(CertUtil.ca_certfile, 'rb') as fp:
-                ca_cert_error = fp.read() not in content
-        if ca_cert_error:
-            with open(CertUtil.ca_certfile, 'wb') as fp:
-                fp.write(OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, ca))
+        
+def check_entry(line_num, segments):
+    # START Title
+    raw_title = segments[index_title]
+    title_re_match = link_re.match(raw_title)
+    # url should be wrapped in '[TITLE](LINK)' Markdown syntax
+    if not title_re_match:
+        add_error(line_num, 'Title syntax should be '[TITLE](LINK)'')
+    else:
+        # do not allow '... API' in the entry title
+        title = title_re_match.group(1)
+        if title.upper().endswith(' API'):
+            add_error(line_num, 'Title should not end with '... API'. Every entry is an API here!')
+        # do not allow duplicate links
+        link = title_re_match.group(2)
+        if link in previous_links:
+            add_error(line_num, 'Duplicate link - entries should only be included in one section')
+        else:
+            previous_links.append(link)
+    # END Title
+    # START Description
+    # first character should be capitalized
+    char = segments[index_desc][0]
+    if char.upper() != char:
+        add_error(line_num, 'first character of description is not capitalized')
+    # last character should not punctuation
+    char = segments[index_desc][-1]
+    if char in punctuation:
+        add_error(line_num, 'description should not end with {}'.format(char))
+    desc_length = len(segments[index_desc])
+    if desc_length > 100:
+        add_error(line_num, 'description should not exceed 100 characters (currently {})'.format(desc_length))
+    # END Description
+    # START Auth
+    # values should conform to valid options only
+    auth = segments[index_auth]
+    if auth != 'No' and (not auth.startswith('`') or not auth.endswith('`')):
+        add_error(line_num, 'auth value is not enclosed with `backticks`')
+    if auth.replace('`', '') not in auth_keys:
+        add_error(line_num, '{} is not a valid Auth option'.format(auth))
+    # END Auth
+    # START HTTPS
+    # values should conform to valid options only
+    https = segments[index_https]
+    if https not in https_keys:
+        add_error(line_num, '{} is not a valid HTTPS option'.format(https))
+    # END HTTPS
+    # START CORS
+    # values should conform to valid options only
+    cors = segments[index_cors]
+    if cors not in cors_keys:
+        add_error(line_num, '{} is not a valid CORS option'.format(cors))
+    # END CORS
     
-                    # not in range and not EOF/EOT, must be invalid symbol
-                self.noViableAlt(s, input)
-                return 0
+    for image in TEST_IMAGES:
+    size = image.split('-')[1].split('.')[0]
+    print('Timings at {}:'.format(size))
     
+    # Load some images to compare against
+known_obama_image = face_recognition.load_image_file('obama.jpg')
+known_biden_image = face_recognition.load_image_file('biden.jpg')
     
-    def rewind(self, marker=None):
-        '''
-        Reset the stream so that next call to index would return marker.
-        The marker will usually be index() but it doesn't have to be.  It's
-        just a marker to indicate what state the stream was in.  This is
-        essentially calling release() and seek().  If there are markers
-        created after this marker argument, this routine must unroll them
-        like a stack.  Assume the state the stream was in when this marker
-        was created.
+        # Apply some eyeliner
+    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
+    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
     
-            return '[@%d,%d:%d=%r,<%d>%s,%d:%d]' % (
-            self.index,
-            self.start, self.stop,
-            txt,
-            self.type, channelStr,
-            self.line, self.charPositionInLine
-            )
+            # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
     
-    
-    # Same, but for 3.x to 2.x
-REVERSE_IMPORT_MAPPING = dict((v, k) for (k, v) in IMPORT_MAPPING.items())
-assert len(REVERSE_IMPORT_MAPPING) == len(IMPORT_MAPPING)
-REVERSE_NAME_MAPPING = dict((v, k) for (k, v) in NAME_MAPPING.items())
-assert len(REVERSE_NAME_MAPPING) == len(NAME_MAPPING)
-    
-    ConfigDialog_spec = {
-    'file': 'configdialog',
-    'kwds': {'title': 'ConfigDialogTest',
-             '_htest': True,},
-    'msg': 'IDLE preferences dialog.\n'
-           'In the 'Fonts/Tabs' tab, changing font face, should update the '
-           'font face of the text in the area below it.\nIn the '
-           ''Highlighting' tab, try different color schemes. Clicking '
-           'items in the sample program should update the choices above it.'
-           '\nIn the 'Keys', 'General' and 'Extensions' tabs, test settings '
-           'of interest.'
-           '\n[Ok] to close the dialog.[Apply] to apply the settings and '
-           'and [Cancel] to revert all changes.\nRe-run the test to ensure '
-           'changes made have persisted.'
-    }
-    
-        def verify_code(self, code_object, *, bytecode_written=False):
-        super().verify_code(code_object)
-        if bytecode_written:
-            self.assertIn(self.cached, self.loader.written)
-            data = bytearray(self.util.MAGIC_NUMBER)
-            data.extend(self.init._pack_uint32(0))
-            data.extend(self.init._pack_uint32(self.loader.source_mtime))
-            data.extend(self.init._pack_uint32(self.loader.source_size))
-            data.extend(marshal.dumps(code_object))
-            self.assertEqual(self.loader.written[self.cached], bytes(data))
-    
-            '''
-        endtime = None
-        waittime = timeout
-        result = predicate()
-        while not result:
-            if waittime is not None:
-                if endtime is None:
-                    endtime = _time() + waittime
-                else:
-                    waittime = endtime - _time()
-                    if waittime <= 0:
-                        break
-            self.wait(waittime)
-            result = predicate()
-        return result
-    
-    def python_implementation():
-    
-    def _unparse_datetime(dt, legacy=False):
-    '''Format a date or datetime object as a pair of (date, time) strings
-    in the format required by the NEWNEWS and NEWGROUPS commands.  If a
-    date object is passed, the time is assumed to be midnight (00h00).
+        packages = find_packages('src'),
+    package_dir = {'' : 'src'},
     
     
-@need_c_queue
-class CPriorityQueueTest(PriorityQueueTest, unittest.TestCase):
-    queue = c_queue
+def huomaotv_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    room_id_pattern = r'huomao.com/(\d+)'
+    room_id = match1(url, room_id_pattern)
+    html = get_content(get_mobile_room_url(room_id))
+    
+        html = get_content(url)
+    uuid_pattern = r''([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})''
+    id = r1(r'var vid='([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})'', html)
+    if id is None:
+        video_pattern = r''vid'\s*:\s*' + uuid_pattern
+        id = match1(html, video_pattern)
+    assert id, 'can't find video info'
+    return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
