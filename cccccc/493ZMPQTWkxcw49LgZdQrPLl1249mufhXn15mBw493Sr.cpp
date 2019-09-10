f9ed3fd62704ca1e0f243f82dfdb2a34f61ac2e4
SUNsung <weatherius@gmail.com>
2019-09-10 20:07:03 +0300

    Rewrite [493ZMPQTWkxcw49LgZdQrPLl1249mufhXn15mBw493Sr.cpp] File in: 2019-09-10 20:07:03

--- cccccc/493ZMPQTWkxcw49LgZdQrPLl1249mufhXn15mBw493Sr.cpp
+++ cccccc/493ZMPQTWkxcw49LgZdQrPLl1249mufhXn15mBw493Sr.cpp
@@ -1,338 +1,260 @@
 
         
-        
-    {    QString appName;
-    QIcon appIcon;
-    QIcon trayAndWindowIcon;
-    QString titleAddText;
+          auto conformance = conformanceRef.getConcrete();
+    
+    const char *swift::_swift_stdlib_strtod_clocale(
+    const char * nptr, double *outResult) {
+  return _swift_stdlib_strtoX_clocale_impl(
+    nptr, outResult, HUGE_VAL, strtod_l);
+}
+    
+      /// Find the conformance of the given Swift type to the
+  /// _BridgedStoredNSError protocol.
+  Optional<ProtocolConformanceRef>
+  getConformanceToBridgedStoredNSError(SILLocation loc, Type type);
+    
+      // On failure, fill out the cache with everything we know about.
+  std::vector<std::pair<std::string, const TypeRef *>> Fields;
+  for (auto &Info : ReflectionInfos) {
+    uint64_t TypeRefOffset = Info.Field.SectionOffset
+                           - Info.TypeReference.SectionOffset;
+    for (auto &FD : Info.Field.Metadata) {
+      if (!FD.hasMangledTypeName())
+        continue;
+      auto CandidateMangledName = FD.getMangledTypeName(TypeRefOffset);
+      auto NormalizedName = normalizeReflectionName(Dem, CandidateMangledName);
+      FieldTypeInfoCache[NormalizedName] = {&FD, &Info};
+      Dem.clear();
+    }
+  }
+    
+      /// Append any retroactive conformances.
+  void appendRetroactiveConformances(Type type);
+  void appendRetroactiveConformances(SubstitutionMap subMap,
+                                     ModuleDecl *fromModule);
+  void appendImplFunctionType(SILFunctionType *fn);
+    
+    
+    {  /// Produces a contextual type involving archetypes within the context of
+  /// the type.
+  Contextual,
 };
     
-    
-    {    /* cleanup */
-    secp256k1_context_destroy(none);
-    secp256k1_context_destroy(sign);
-    secp256k1_context_destroy(vrfy);
-    secp256k1_context_destroy(both);
-}
-    
-        // Other valid inputs
-    CheckSplitTorReplyLine('COMMAND', 'COMMAND', '');
-    CheckSplitTorReplyLine('COMMAND SOME  ARGS', 'COMMAND', 'SOME  ARGS');
-    
-    
-    {    CRIPEMD160();
-    CRIPEMD160& Write(const unsigned char* data, size_t len);
-    void Finalize(unsigned char hash[OUTPUT_SIZE]);
-    CRIPEMD160& Reset();
-};
-    
-    ;; input is [rsp + _XFER + %1 * 4]
-%macro DO_ROUND 1
-    mov	y0, e		; y0 = e
-    ror	y0, (25-11)	; y0 = e >> (25-11)
-    mov	y1, a		; y1 = a
-    xor	y0, e		; y0 = e ^ (e >> (25-11))
-    ror	y1, (22-13)	; y1 = a >> (22-13)
-    mov	y2, f		; y2 = f
-    xor	y1, a		; y1 = a ^ (a >> (22-13)
-    ror	y0, (11-6)	; y0 = (e >> (11-6)) ^ (e >> (25-6))
-    xor	y2, g		; y2 = f^g
-    xor	y0, e		; y0 = e ^ (e >> (11-6)) ^ (e >> (25-6))
-    ror	y1, (13-2)	; y1 = (a >> (13-2)) ^ (a >> (22-2))
-    and	y2, e		; y2 = (f^g)&e
-    xor	y1, a		; y1 = a ^ (a >> (13-2)) ^ (a >> (22-2))
-    ror	y0, 6		; y0 = S1 = (e>>6) & (e>>11) ^ (e>>25)
-    xor	y2, g		; y2 = CH = ((f^g)&e)^g
-    add	y2, y0		; y2 = S1 + CH
-    ror	y1, 2		; y1 = S0 = (a>>2) ^ (a>>13) ^ (a>>22)
-    add	y2, [rsp + _XFER + %1 * 4]	; y2 = k + w + S1 + CH
-    mov	y0, a		; y0 = a
-    add	h, y2		; h = h + S1 + CH + k + w
-    mov	y2, a		; y2 = a
-    or	y0, c		; y0 = a|c
-    add	d, h		; d = d + h + S1 + CH + k + w
-    and	y2, c		; y2 = a&c
-    and	y0, b		; y0 = (a|c)&b
-    add	h, y1		; h = h + S1 + CH + k + w + S0
-    or	y0, y2		; y0 = MAJ = (a|c)&b)|(a&c)
-    add	h, y0		; h = h + S1 + CH + k + w + S0 + MAJ
-    ROTATE_ARGS
-%endm
-    
-    #include 'absl/strings/str_cat.h'
-#include 'absl/strings/string_view.h'
-#include 'opencensus/stats/stats.h'
-#include 'src/core/lib/surface/call.h'
-#include 'src/cpp/ext/filters/census/grpc_plugin.h'
-#include 'src/cpp/ext/filters/census/measures.h'
-    
-      CensusClientCallData()
-      : recv_trailing_metadata_(nullptr),
-        initial_on_done_recv_trailing_metadata_(nullptr),
-        initial_on_done_recv_message_(nullptr),
-        elapsed_time_(0),
-        recv_message_(nullptr),
-        recv_message_count_(0),
-        sent_message_count_(0) {
-    memset(&stats_bin_, 0, sizeof(grpc_linked_mdelem));
-    memset(&tracing_bin_, 0, sizeof(grpc_linked_mdelem));
-    memset(&path_, 0, sizeof(grpc_slice));
-    memset(&on_done_recv_trailing_metadata_, 0, sizeof(grpc_closure));
-    memset(&on_done_recv_message_, 0, sizeof(grpc_closure));
-  }
-    
-    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
-::opencensus::stats::MeasureDouble RpcClientServerLatency();
-::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
-    
-    
-    {}  // namespace grpc
-
-    
-    // TraceContextEncoding encapsulates the logic for encoding and decoding of
-// trace contexts.
-class TraceContextEncoding {
- public:
-  // Size of encoded GrpcTraceContext. (16 + 8 + 1 + 4)
-  static constexpr size_t kGrpcTraceContextSize = 29;
-  // Error value.
-  static constexpr size_t kEncodeDecodeFailure = 0;
-    }
-    
-    
-    {  return Status::OK;
-}
-    
-    void DynamicThreadPool::Add(const std::function<void()>& callback) {
-  std::lock_guard<std::mutex> lock(mu_);
-  // Add works to the callbacks list
-  callbacks_.push(callback);
-  // Increase pool size or notify as needed
-  if (threads_waiting_ == 0) {
-    // Kick off a new thread
-    nthreads_++;
-    new DynamicThread(this);
-  } else {
-    cv_.notify_one();
-  }
-  // Also use this chance to harvest dead threads
-  if (!dead_threads_.empty()) {
-    ReapThreads(&dead_threads_);
-  }
-}
-    
-    #include <grpcpp/support/config.h>
-    
-    
-    {      out_.data.clear();
-      out_.data.push_back(batch_data_);
-      out_.data.push_back(batch_label_);
-      loc_ += batch_size_;
-      return true;
-    }
-    
-    namespace mxnet {
-namespace op {
-template<>
-Operator *CreateOp<cpu>(CaffeLossParam param, int dtype) {
-  Operator *op = NULL;
-  switch (dtype) {
-  case mshadow::kFloat32:
-    op = new CaffeLoss<cpu, float>(param);
-    break;
-  case mshadow::kFloat64:
-    op = new CaffeLoss<cpu, double>(param);
-    break;
-  case mshadow::kFloat16:
-    LOG(FATAL) << 'float16 layer is not supported by caffe';
-    break;
-  default:
-    LOG(FATAL) << 'Unsupported type ' << dtype;
-  }
-  return op;
-}
-    }
-    }
-    
-      for (auto& e : idx.outputs()) {
-    ++ref_count[idx.entry_id(e)];
-  }
-  for (uint32_t nid = 0; nid < idx.num_nodes(); ++nid) {
-    for (auto &e : idx[nid].inputs) {
-      ++ref_count[idx.entry_id(e)];
-    }
-  }
-    
-    /*!
- *  Copyright (c) 2015 by Contributors
- * \file image_augmenter.h
- * \brief Interface of opencv based image augmenter
+    /**
+ * Provides the minimal api for a simple out of line executor that can run non-cancellable
+ * callbacks.
+ *
+ * The contract for scheduling work on an executor is that it never blocks the caller.  It doesn't
+ * necessarily need to offer forward progress guarantees, but actual calls to schedule() should not
+ * deadlock.
+ *
+ * If you manage the lifetime of your executor using a shared_ptr, you can begin a chain of
+ * execution like this:
+ *      ExecutorFuture(myExec)
+ *          .then([] { return doThing1(); })
+ *          .then([] { return doThing2(); })
+ *          ...
  */
-#ifndef MXNET_IO_IMAGE_AUGMENTER_H_
-#define MXNET_IO_IMAGE_AUGMENTER_H_
-    
-      virtual void accept(DHTMessageCallback* callback) = 0;
-    
-      char zero[18];
-  memset(zero, 0, sizeof(zero));
-    
-        dispatcher->setTimeout(std::chrono::seconds(messageTimeout));
+class OutOfLineExecutor {
+public:
+    using Task = unique_function<void(Status)>;
+    }
     
     
-    {  // Returns two vector of Commands.  First one contains regular
-  // commands.  Secod one contains so called routine commands, which
-  // executed once per event poll returns.
-  std::pair<std::vector<std::unique_ptr<Command>>,
-            std::vector<std::unique_ptr<Command>>>
-  setup(DownloadEngine* e, int family);
+    {class ValidId : public Base {
+public:
+    void run() {
+        ASSERT(fixDocumentForInsert(_opCtx.getServiceContext(), BSON('_id' << 5)).isOK());
+        ASSERT(
+            fixDocumentForInsert(_opCtx.getServiceContext(), BSON('_id' << BSON('x' << 5))).isOK());
+        ASSERT(!fixDocumentForInsert(_opCtx.getServiceContext(), BSON('_id' << BSON('$x' << 5)))
+                    .isOK());
+        ASSERT(!fixDocumentForInsert(_opCtx.getServiceContext(), BSON('_id' << BSON('$oid' << 5)))
+                    .isOK());
+    }
 };
+}  // namespace Insert
     
-    #include 'common.h'
-    
-    namespace aria2 {
+    //
+// Check that cursor relocates its end location properly if end location
+// changes during a yield
+//
+class QueryStageCountScanInsertNewDocsDuringYield : public CountBase {
+public:
+    void run() {
+        dbtests::WriteContextForTests ctx(&_opCtx, ns());
+    }
     }
     
-    #include <cstring>
+    public:
     
-    ///Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
+    //--------------------------------------------------------------------------
+//
+//    RegexPattern    Default Constructor
+//
+//--------------------------------------------------------------------------
+RegexPattern::RegexPattern() {
+    // Init all of this instances data.
+    init();
+}
     
-    int main() {
+    
+    {    // Compare identical level.
+    const Normalizer2Impl &nfcImpl = data->nfcImpl;
+    left += equalPrefixLength;
+    right += equalPrefixLength;
+    if(settings->dontCheckFCD()) {
+        UTF16NFDIterator leftIter(left, leftLimit);
+        UTF16NFDIterator rightIter(right, rightLimit);
+        return compareNFDIter(nfcImpl, leftIter, rightIter);
+    } else {
+        FCDUTF16NFDIterator leftIter(nfcImpl, left, leftLimit);
+        FCDUTF16NFDIterator rightIter(nfcImpl, right, rightLimit);
+        return compareNFDIter(nfcImpl, leftIter, rightIter);
+    }
+}
+    
+    U_NAMESPACE_END
+    
+    namespace apollo {
+namespace drivers {
+namespace velodyne {
+    }
+    }
+    }
+    
+    namespace apollo {
+namespace planning {
+    }
     }
     
     
-// Classic Non-Recursive algorithm for inorder traversal
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
+    {    // wait for other agent receiving is ok.
+    while (!other_agent()->is_receiving()) {
+      std::this_thread::yield();
     }
-    
-    #include <iostream>
-#include <vector>
-#include <stack>
-    
-    /// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-    
-    int main() {
-    }
-    
-            while(!output.empty()){
-            res.push_back((output.top())->val);
-            output.pop();
+    int64_t start = AsInt64<micros>(Clock::Now());
+    while (true) {
+      // param->print();
+      if (count >= FLAGS_agent_mutual_send_frames) {
+        break;
+      }
+      for (int32_t i = 0; i < MAX_CAN_SEND_FRAME_LEN; ++i) {
+        // frames[i].id = id_count & 0x3FF;
+        send_id = id;
+        frames[i].id = id;
+        frames[i].len = 8;
+        frames[i].data[7] = static_cast<uint8_t>(count % 256);
+        for (uint8_t j = 0; j < 7; ++j) {
+          frames[i].data[j] = j;
         }
-    
-    #pragma once
-    
-      // Will be called while on the write thread before the write executes.  If
-  // this function returns a non-OK status, the write will be aborted and this
-  // status will be returned to the caller of DB::Write().
-  virtual Status Callback(DB* db) = 0;
-    
-    
-    {    std::vector<std::string> input_file_names;
-    for (auto level : cf_meta.levels) {
-      for (auto file : level.files) {
-        if (file.being_compacted) {
-          return nullptr;
+        ++count;
+        ++id;
+        if (id > end_id) {
+          id = start_id;
         }
-        input_file_names.push_back(file.name);
+      }
+      int32_t frame_num = MAX_CAN_SEND_FRAME_LEN;
+      if (client->Send(frames, &frame_num) != ErrorCode::OK) {
+        param->send_err_cnt += MAX_CAN_SEND_FRAME_LEN;
+        AERROR << 'send_thread send msg failed!, id:' << send_id
+               << ', conf:' << param->conf.ShortDebugString();
+      } else {
+        param->send_cnt += frame_num;
+        param->send_lost_cnt += MAX_CAN_SEND_FRAME_LEN - frame_num;
+        AINFO << 'send_frames: ' << frame_num << 'send_frame#'
+              << frames[0].CanFrameString()
+              << ' send lost:' << MAX_CAN_SEND_FRAME_LEN - frame_num
+              << ', conf:' << param->conf.ShortDebugString();
       }
     }
-    return new CompactionTask(
-        db, this, cf_name, input_file_names,
-        options_.num_levels - 1, compact_options_, false);
+    int64_t end = AsInt64<micros>(Clock::Now());
+    param->send_time = static_cast<int32_t>(end - start);
+    // In case for finish too quick to receiver miss some msg
+    sleep(2);
+    AINFO << 'Send thread stopping...' << param->conf.ShortDebugString();
+    is_sending_finish(true);
+    return;
   }
     
-    #include <stdint.h>
-#include <memory>
-#include <string>
-    
-    // Simple RAII wrapper class for Snapshot.
-// Constructing this object will create a snapshot.  Destructing will
-// release the snapshot.
-class ManagedSnapshot {
- public:
-  explicit ManagedSnapshot(DB* db);
+    Status ControllerAgent::Init(const ControlConf *control_conf) {
+  RegisterControllers(control_conf);
+  CHECK(InitializeConf(control_conf).ok()) << 'Failed to initialize config.';
+  for (auto &controller : controller_list_) {
+    if (controller == nullptr) {
+      return Status(ErrorCode::CONTROL_INIT_ERROR, 'Controller is null.');
     }
-    
-    Speed* Speed::create(ActionInterval* action, float speed)
-{
-    Speed *ret = new (std::nothrow) Speed();
-    if (ret && ret->initWithAction(action, speed))
-    {
-        ret->autorelease();
-        return ret;
+    if (!controller->Init(control_conf_).ok()) {
+      AERROR << 'Controller <' << controller->Name() << '> init failed!';
+      return Status(ErrorCode::CONTROL_INIT_ERROR,
+                    'Failed to init Controller:' + controller->Name());
     }
-    CC_SAFE_DELETE(ret);
-    return nullptr;
+    AINFO << 'Controller <' << controller->Name() << '> init done!';
+  }
+  return Status::OK();
 }
     
-    /** @class FiniteTimeAction
- * @brief
- * Base class actions that do have a finite time duration.
- * Possible actions:
- * - An action with a duration of 0 seconds.
- * - An action with a duration of 35.5 seconds.
- * Infinite time actions are valid.
- */
-class CC_DLL FiniteTimeAction : public Action
-{
-public:
-    /** Get duration in seconds of the action. 
-     *
-     * @return The duration in seconds of the action.
-     */
-    float getDuration() const { return _duration; }
-    /** Set duration in seconds of the action. 
-     *
-     * @param duration In seconds of the action.
-     */
-    void setDuration(float duration) { _duration = duration; }
+    namespace apollo {
+namespace planning {
+namespace scenario {
+namespace traffic_light {
     }
+    }
+    }
+    }
+    
     
-    protected:
-    Vec2 _position;
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-    std::vector<cocos2d::Vec2> AutoPolygon::marchSquare(const Rect& rect, const Vec2& start, float threshold)
-{
-    int stepx = 0;
-    int stepy = 0;
-    int prevx = 0;
-    int prevy = 0;
-    int startx = start.x;
-    int starty = start.y;
-    int curx = startx;
-    int cury = starty;
-    unsigned int count = 0;
-    std::vector<int> case9s;
-    std::vector<int> case6s;
-    int i;
-    std::vector<int>::iterator it;
-    std::vector<cocos2d::Vec2> _points;
-    do{
-        int sv = getSquareValue(curx, cury, rect, threshold);
-        switch(sv){
+    {  switch (sub_type) {
+    case base::ObjectSubType::TRAFFICCONE: {
+      const float *min_tmplt_cur_type = kMinTemplateHWL.at(sub_type).data();
+      const float *max_tmplt_cur_type = kMaxTemplateHWL.at(sub_type).data();
+      dimension_hwl[0] = std::min(dimension_hwl[0], max_tmplt_cur_type[0]);
+      dimension_hwl[0] = std::max(dimension_hwl[0], min_tmplt_cur_type[0]);
+      break;
     }
+    case base::ObjectSubType::PEDESTRIAN:
+    case base::ObjectSubType::CYCLIST:
+    case base::ObjectSubType::MOTORCYCLIST: {
+      const float *min_tmplt_cur_type = kMinTemplateHWL.at(sub_type).data();
+      const float *mid_tmplt_cur_type = kMidTemplateHWL.at(sub_type).data();
+      const float *max_tmplt_cur_type = kMaxTemplateHWL.at(sub_type).data();
+      float dh_min = fabsf(dimension_hwl[0] - min_tmplt_cur_type[0]);
+      float dh_mid = fabsf(dimension_hwl[0] - mid_tmplt_cur_type[0]);
+      float dh_max = fabsf(dimension_hwl[0] - max_tmplt_cur_type[0]);
+      std::vector<std::pair<float, float>> diff_hs;
+      diff_hs.push_back(std::make_pair(dh_min, min_tmplt_cur_type[0]));
+      diff_hs.push_back(std::make_pair(dh_mid, mid_tmplt_cur_type[0]));
+      diff_hs.push_back(std::make_pair(dh_max, max_tmplt_cur_type[0]));
+      sort(diff_hs.begin(), diff_hs.end(),
+           [](const std::pair<float, float> &a,
+              const std::pair<float, float> &b) -> bool {
+             return a.first < b.first;
+           });
+      dimension_hwl[0] = diff_hs[0].second;
+      break;
     }
-    }
+    case base::ObjectSubType::CAR:
+      type_min_vol_index =
+          kLookUpTableMinVolumeIndex.at(TemplateIndex::CAR_MIN_VOLUME_INDEX);
+      break;
+    case base::ObjectSubType::VAN:
+      type_min_vol_index =
+          kLookUpTableMinVolumeIndex.at(TemplateIndex::VAN_MIN_VOLUME_INDEX);
+      break;
+    case base::ObjectSubType::TRUCK:
+      type_min_vol_index =
+          kLookUpTableMinVolumeIndex.at(TemplateIndex::TRUCK_MIN_VOLUME_INDEX);
+      break;
+    case base::ObjectSubType::BUS:
+      type_min_vol_index =
+          kLookUpTableMinVolumeIndex.at(TemplateIndex::BUS_MIN_VOLUME_INDEX);
+      break;
+    default:
+      if (min_dimension_val < multicue_param_.min_dimension_val()) {
+        common::IScale3(dimension_hwl, multicue_param_.min_dimension_val() *
+                                           common::IRec(min_dimension_val));
+      }
+      break;
+  }
+  return type_min_vol_index;
+}
