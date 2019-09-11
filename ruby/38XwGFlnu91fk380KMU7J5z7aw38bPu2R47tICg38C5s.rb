4b3f5f79d2ed5ed656acca69e8e63d440c3f916e
SUNsung <weatherius@gmail.com>
2019-09-12 00:10:02 +0300

    Rewrite [38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb] File in: 2019-09-12 00:10:02

--- ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
+++ ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
@@ -1,147 +1,118 @@
 
         
-            it 'should return the file when s3 cdn enabled' do
-      SiteSetting.s3_cdn_url = 'https://cdn.s3.amazonaws.com'
-      stub_request(:get, Discourse.store.cdn_url(upload_s3.url)).to_return(status: 200, body: 'Hello world')
+            expected = <<~MD
+      <div class='poll' data-poll-status='open' data-poll-type='multiple' data-poll-name='poll'>
+      <div>
+      <div class='poll-container'>
+      <ol>
+      <li data-poll-option-id='b6475cbf6acb8676b20c60582cfc487a'>test 1 <img src='/images/emoji/twitter/slight_smile.png?v=#{Emoji::EMOJI_VERSION}' title=':slight_smile:' class='emoji' alt=':slight_smile:'> <b>test</b>
+      </li>
+      <li data-poll-option-id='7158af352698eb1443d709818df097d4'>test 2</li>
+      </ol>
+      </div>
+      <div class='poll-info'>
+      <p>
+      <span class='info-number'>0</span>
+      <span class='info-label'>voters</span>
+      </p>
+      </div>
+      </div>
+      </div>
+    MD
     
-        def handle_bounce(e)
-      # never reply to bounced emails
-      log_email_process_failure(@mail, e)
-      set_incoming_email_rejection_message(@receiver.incoming_email, I18n.t('emails.incoming.errors.bounced_email_error'))
+          # there might be more information available in the exception
+      if message_template == :email_reject_invalid_post && e.message.size > 6
+        message_template = :email_reject_invalid_post_specified
+        template_args[:post_error] = e.message
+      end
+    
+          settings2.refresh!
+      expect(settings2.hello).to eq(99)
     end
     
-    require 'rails_helper'
-require 'theme_store/git_importer'
-    
-      def personal_messages
-    guardian.ensure_can_tag_pms!
-    allowed_user = fetch_user_from_params
-    raise Discourse::NotFound if allowed_user.blank?
-    raise Discourse::NotFound if current_user.id != allowed_user.id && !@guardian.is_admin?
-    pm_tags = Tag.pm_tags(guardian: guardian, allowed_user: allowed_user)
-    
-            test_multisite_connection('second') do
-          upload = build_upload
-          expect(store.store_upload(uploaded_file, upload)).to eq(
-            '//#{SiteSetting.s3_upload_bucket}.s3.dualstack.us-east-1.amazonaws.com/uploads/second/original/1X/c530c06cf89c410c0355d7852644a73fc3ec8c04.png'
-          )
-          expect(upload.etag).to eq('ETag')
+            if @run_remote && @run_remote.is_a?(Hash)
+          new_run = VagrantPlugins::Shell::Config.new
+          new_run.set_options(@run_remote)
+          new_run.finalize!
+          @run_remote = new_run
         end
+    
+    # Load in helpers
+require 'unit/support/dummy_communicator'
+require 'unit/support/dummy_provider'
+require 'unit/support/shared/base_context'
+require 'unit/support/shared/action_synced_folders_context'
+require 'unit/support/shared/capability_helpers_context'
+require 'unit/support/shared/plugin_command_context'
+require 'unit/support/shared/virtualbox_context'
+    
+      describe '#machine' do
+    # A helper to register a provider for use in tests.
+    def register_provider(name, config_class=nil, options=nil)
+      provider_cls = Class.new(VagrantTests::DummyProvider)
+    
+          # If it is a multi-machine environment, then return the primary
+      @config.vm.defined_vms.each do |name, subvm|
+        return name if subvm.options[:primary]
       end
-    end
+    
+          iso_env.box3('base', '1.0', :foo, vagrantfile: <<-VF)
+      Vagrant.configure('2') do |config|
+        config.ssh.port = 123
+      end
+      VF
+    
+    require File.expand_path('../../base', __FILE__)
+    
+      # This returns a true/false if we are running within a bundler environment
+  #
+  # @return [Boolean]
+  def self.in_bundler?
+    !!ENV['BUNDLE_GEMFILE'] &&
+      !defined?(::Bundler).nil?
   end
     
-      system('yarn install --dev')
-    
-      # Get the title for the page.
-  #
-  # @param [Middleman::Page] page
-  #
-  # @return [String]
-  def title_for(page)
-    if page && page.data.page_title
-      return '#{page.data.page_title} - Vagrant by HashiCorp'
-    end
-    
-        it 'returns true if installed' do
-      expect(machine.communicate).to receive(:test).
-        with(command, sudo: true).and_return(true)
-      subject.chef_installed(machine, 'chef_solo', version)
-    end
-    
-      describe '#chef_installed' do
-    let(:version) { '15.0.0' }
-    let(:command) { 'test -x /opt/chef_solo/bin/knife&& /opt/chef_solo/bin/knife --version | grep '15.0.0'' }
-    
-      let(:machine) { iso_env.machine(iso_env.machine_names[0], :dummy) }
-  let(:communicator) { VagrantTests::DummyCommunicator::Communicator.new(machine) }
-  let(:config)  { double('config') }
-    
-        # Initializes by loading a Vagrantfile.
-    #
-    # @param [Config::Loader] loader Configuration loader that should
-    #   already be configured with the proper Vagrantfile locations.
-    #   This usually comes from {Vagrant::Environment}
-    # @param [Array<Symbol>] keys The Vagrantfiles to load and the
-    #   order to load them in (keys within the loader).
-    def initialize(loader, keys)
-      @keys   = keys
-      @loader = loader
-      @config, _ = loader.load(keys)
-      @logger = Log4r::Logger.new('vagrant::vagrantfile')
-    end
-    
-          expect { subject.usable?(true) }.
-        to raise_error(Vagrant::Errors::VirtualBoxInvalidVersion)
-    end
+      def send_sinatra_file(path, &missing_file_block)
+    file_path = File.join(File.dirname(__FILE__), 'public',  path)
+    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
+    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
   end
     
-        context 'when extra_args is provided as a string' do
-      let(:ssh_info) {{
-        host: 'localhost',
-        port: 2222,
-        username: 'vagrant',
-        private_key_path: [private_key_path],
-        extra_args: '-6'
-      }}
-    
-        # Enable Vagrant environment specific plugins at given data path
-    #
-    # @param [Pathname] Path to Vagrant::Environment data directory
-    # @return [Pathname] Path to environment specific gem directory
-    def environment_path=(env_data_path)
-      @env_plugin_gem_path = env_data_path.join('plugins', 'gems', RUBY_VERSION).freeze
-    end
-    
-            entries = []
-        prune   = []
-        @env.machine_index.each do |entry|
-          # If we're pruning and this entry is invalid, skip it
-          # and prune it later.
-          if options[:prune] && !entry.valid?(@env.home_path)
-            prune << entry
-            next
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
           end
-    
-        before do
-      iso_env.machine_index.set(new_entry('foo'))
-      iso_env.machine_index.set(new_entry('bar'))
-      allow($stdout).to receive(:puts)
-    end
-    
-      describe '#flare_tag_hash' do
-    let(:tag) { create(:tag, name: 'ama', bg_color_hex: '#f3f3f3', text_color_hex: '#cccccc') }
-    let(:valid_article) { create(:article, tags: tag.name) }
-    
-        it 'denies chat channel invitation to non-authorized user' do
-      expect do
-        post '/chat_channel_memberships', params: {
-          chat_channel_membership: {
-            user_id: second_user.id, chat_channel_id: chat_channel.id
-          }
-        }
-      end.to raise_error(Pundit::NotAuthorizedError)
-    end
-  end
-    
-      def facebook_post!
-    BufferUpdate.buff!(@article.id, fb_buffer_text, ApplicationConfig['BUFFER_FACEBOOK_ID'], 'facebook')
-    BufferUpdate.buff!(@article.id, fb_buffer_text, ApplicationConfig['BUFFER_LINKEDIN_ID'], 'linkedin')
-    @article.update(facebook_last_buffered: Time.current)
-  end
-    
-          now = Time.current
-      @user.onboarding_package_requested_again = true if @user.onboarding_package_requested
-      @user.onboarding_package_requested = true
-      @user.onboarding_package_form_submmitted_at = now
-      @user.personal_data_updated_at = now
-      @user.shipping_validated_at = now if user_params[:shipping_validated] == '1'
-      if @user.save!
-        format.html { redirect_to '/freestickers/edit' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
       end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
     end
-  end
+    
+        def initialize(tag_name, markup, tokens)
+      attributes = ['class', 'src', 'width', 'height', 'title']
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
+    end
