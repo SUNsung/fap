d10d35542c78e3da4f7433b9a48a083a5e47c5c0
SUNsung <weatherius@gmail.com>
2019-09-11 14:53:13 +0300

    Rewrite [12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb] File in: 2019-09-11 14:53:13

--- ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
+++ ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
@@ -1,88 +1,205 @@
 
         
-            def mime_type
-      headers['Content-Type'] || 'text/plain'
-    end
-    
-      def down
-    remove_index :share_visibilities, name: :shareable_and_user_id
-    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id
-  end
-    
-      class PostToService < Base
-    def perform(*_args)
-      # don't post to services in cucumber
-    end
-  end
-    
-        it 'generates the contacts_json fixture', :fixture => true do
-      json = bob.contacts.map { |c|
-               ContactPresenter.new(c, bob).full_hash_with_person
-             }.to_json
-      save_fixture(json, 'contacts_json')
-    end
-  end
+          expect(status).to be_success
 end
+    
+    World(RemoteCommandHelpers)
 
     
-          it 'should not be catched when it is unexpected' do
-        @target = alice.post :status_message, text: 'AWESOME', to: @alices_aspect.id
-        allow(alice).to receive(:like!).and_raise('something')
-        allow(@controller).to receive(:current_user).and_return(alice)
-        expect { post :create, params: like_hash, format: :json }.to raise_error('something')
+          # Given a callable that provides a value, wrap the callable with another
+      # object that responds to `call`. This new object will perform validation
+      # and then return the original callable's value.
+      #
+      # If the callable is a `Question`, the object returned by this method will
+      # also be a `Question` (a `ValidatedQuestion`, to be precise). This
+      # ensures that `is_a?(Question)` remains true even after the validation
+      # wrapper is applied. This is needed so that `Configuration#is_question?`
+      # works as expected.
+      #
+      def assert_valid_later(key, callable)
+        validation_callback = lambda do
+          value = callable.call
+          assert_valid_now(key, value)
+          value
+        end
+    
+          def delete(key)
+        values.delete(key)
       end
-    end
+    
+          def page_name
+        @name.gsub('-', ' ')
+      end
+    
+          def versions
+        i = @versions.size + 1
+        @versions.map do |v|
+          i -= 1
+          { :id        => v.id,
+            :id7       => v.id[0..6],
+            :num       => i,
+            :author    => v.author.name.respond_to?(:force_encoding) ? v.author.name.force_encoding('UTF-8') : v.author.name,
+            :message   => v.message.respond_to?(:force_encoding) ? v.message.force_encoding('UTF-8') : v.message,
+            :date      => v.authored_date.strftime('%B %d, %Y'),
+            :gravatar  => Digest::MD5.hexdigest(v.author.email.strip.downcase),
+            :identicon => self._identicon_code(v.author.email),
+            :date_full => v.authored_date,
+            :files     => v.stats.files.map { |f,*rest|
+              page_path = extract_renamed_path_destination(f)
+              page_path = remove_page_extentions(page_path)
+              { :file => f,
+                :link => '#{page_path}/#{v.id}'
+              }
+            }
+          }
+        end
+      end
+    
+          attr_reader :name, :path
+    
+        # Test page_header_from_content(@content)
+    actual = @view.title
+    assert_equal '1 & 2', actual
   end
     
-        it 'redirects to /stream for a mobile user' do
-      request.headers['X_MOBILE_DEVICE'] = true
-      post :create, params: {user: {remember_me: '0', username: @user.username, password: 'evankorth'}}
-      expect(response).to be_redirect
-      expect(response.location).to match /^#{stream_url}\??$/
-    end
-  end
+    # Configure Rails Environment
+ENV['RAILS_ENV'] = 'test'
     
-          it 'it calls toggle_hidden_shareable' do
-        expect(@controller.current_user).to receive(:toggle_hidden_shareable).with(an_instance_of(StatusMessage))
-        put :update, params: {id: 42, post_id: @status.id}, format: :js
+          def content_type
+        case params[:format]
+        when 'json'
+          'application/json; charset=utf-8'
+        when 'xml'
+          'text/xml; charset=utf-8'
+        end
       end
-    end
     
-        context 'with other cancelled return items exist for the same inventory unit' do
-      let(:old_reception_status) { 'cancelled' }
-    
-            def scope
-          if params[:product_id]
-            Spree::Product.friendly.find(params[:product_id])
-          elsif params[:variant_id]
-            Spree::Variant.find(params[:variant_id])
-          end
+            def inventory_unit_params
+          params.require(:inventory_unit).permit(permitted_inventory_unit_attributes)
         end
       end
     end
   end
 end
 
+    
+            def show
+          respond_with(stock_location)
+        end
+    
+            def taxonomy
+          if params[:taxonomy_id].present?
+            @taxonomy ||= Spree::Taxonomy.accessible_by(current_ability, :show).find(params[:taxonomy_id])
+          end
+        end
     
             def update
-          authorize! :update, inventory_unit.order
-    
-            def create
-          authorize! :create, Spree::OptionType
-          @option_type = Spree::OptionType.new(option_type_params)
-          if @option_type.save
-            render :show, status: 201
+          @variant = scope.accessible_by(current_ability, :update).find(params[:id])
+          if @variant.update(variant_params)
+            respond_with(@variant, status: 200, default_template: :show)
           else
-            invalid_resource!(@option_type)
+            invalid_resource!(@product)
           end
         end
     
-            def destroy
-          if @property
-            authorize! :destroy, @property
-            @property.destroy
-            respond_with(@property, status: 204)
-          else
-            invalid_resource!(@property)
-          end
+      rescue Acme::Client::Error => e
+    @retries = 0
+    if e.is_a?(Acme::Client::Error::BadNonce) && @retries < 5
+      @retries += 1
+      logger.info 'Bad nounce encountered. Retrying (#{@retries} of 5 attempts)'
+      sleep 1
+      verify
+    else
+      logger.info 'Error: #{e.class} (#{e.message})'
+      return false
+    end
+  end
+    
+      def update
+    if @address_endpoint.update(safe_params)
+      redirect_to_with_json [organization, @server, :address_endpoints]
+    else
+      render_form_errors 'edit', @address_endpoint
+    end
+  end
+    
+      def create
+    @credential = @server.credentials.build(params.require(:credential).permit(:type, :name, :hold))
+    if @credential.save
+      redirect_to_with_json [organization, @server, :credentials]
+    else
+      render_form_errors 'new', @credential
+    end
+  end
+    
+      before_action :admin_required
+  before_action { @ip_pool = IPPool.find_by_uuid!(params[:ip_pool_id]) }
+  before_action { params[:id] && @ip_address = @ip_pool.ip_addresses.find(params[:id]) }
+    
+      def create
+    if params[:direction] == 'incoming'
+      session[:test_in_from] = params[:message][:from] if params[:message]
+      @message = IncomingMessagePrototype.new(@server, request.ip, 'web-ui', params[:message])
+      @message.attachments = [{:name => 'test.txt', :content_type => 'text/plain', :data => 'Hello world!'}]
+    else
+      session[:test_out_to] = params[:message][:to] if params[:message]
+      @message = OutgoingMessagePrototype.new(@server, request.ip, 'web-ui', params[:message])
+    end
+    if result = @message.create_messages
+      if result.size == 1
+        redirect_to_with_json organization_server_message_path(organization, @server, result.first.last[:id]), :notice => 'Message was queued successfully'
+      else
+        redirect_to_with_json [:queue, organization, @server], :notice => 'Messages queued successfully '
+      end
+    else
+      respond_to do |wants|
+        wants.html do
+          flash.now[:alert] = 'Your message could not be sent. Ensure that all fields are completed fully. #{result.errors.inspect}'
+          render 'new'
         end
+        wants.json do
+          render :json => {:flash => {:alert => 'Your message could not be sent. Please check all field are completed fully.'}}
+        end
+      end
+    
+      def verify
+    if request.post?
+      if params[:code].to_s.strip == current_user.email_verification_token.to_s || (Rails.env.development? && params[:code].to_s.strip == '123456')
+        current_user.verify!
+        redirect_to_with_json [:return_to, root_path], :notice => 'Thanks - your e-mail address has been verified successfully.'
+      else
+        flash_now :alert, 'The code you've entered isn't correct. Please check and try again.'
+      end
+    end
+  end
+    
+      argument :attachment_names, :required => true, :type => :array, :desc => 'The names of the attachment(s) to add.',
+           :banner => 'attachment_one attachment_two attachment_three ...'
+    
+      # Provides configurability to Paperclip. The options available are:
+  # * whiny: Will raise an error if Paperclip cannot process thumbnails of
+  #   an uploaded image. Defaults to true.
+  # * log: Logs progress to the Rails log. Uses ActiveRecord's logger, so honors
+  #   log levels, etc. Defaults to true.
+  # * command_path: Defines the path at which to find the command line
+  #   programs if they are not visible to Rails the system's search path. Defaults to
+  #   nil, which uses the first executable found in the user's search path.
+  # * use_exif_orientation: Whether to inspect EXIF data to determine an
+  #   image's orientation. Defaults to true.
+  def self.options
+    @options ||= {
+      command_path: nil,
+      content_type_mappings: {},
+      log: true,
+      log_command: true,
+      read_timeout: nil,
+      swallow_stderr: true,
+      use_exif_orientation: true,
+      whiny: true,
+      is_windows: Gem.win_platform?
+    }
+  end
+    
+        def clear
+      @attachments = Hash.new { |h,k| h[k] = {} }
+    end
