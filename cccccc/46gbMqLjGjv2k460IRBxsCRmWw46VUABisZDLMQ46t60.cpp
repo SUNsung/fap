79c8ed14f963e6c26e6661037f42fcadc5196199
SUNsung <weatherius@gmail.com>
2019-09-11 20:37:51 +0300

    Rewrite [46gbMqLjGjv2k460IRBxsCRmWw46VUABisZDLMQ46t60.cpp] File in: 2019-09-11 20:37:51

--- cccccc/46gbMqLjGjv2k460IRBxsCRmWw46VUABisZDLMQ46t60.cpp
+++ cccccc/46gbMqLjGjv2k460IRBxsCRmWw46VUABisZDLMQ46t60.cpp
@@ -1,161 +1,116 @@
 
         
-        private Q_SLOTS:
-    void on_selectFileButton_clicked();
+        #ifndef NDEBUG
+#define DEBUG_VAR
+#else
+#define DEBUG_VAR UNUSED
+#endif
+    
+     private:
+  // Replaces multiple consecutive separators with a single separator.
+  // For example, 'bar///foo' becomes 'bar/foo'. Does not eliminate other
+  // redundancies that might be in a pathname involving '.' or '..'.
+  //
+  // A pathname with multiple consecutive separators may occur either through
+  // user error or as a result of some scripts or APIs that generate a pathname
+  // with a trailing separator. On other platforms the same API or script
+  // may NOT generate a pathname with a trailing '/'. Then elsewhere that
+  // pathname may have another '/' and pathname components added to it,
+  // without checking for the separator already being there.
+  // The script language and operating system may allow paths like 'foo//bar'
+  // but some of the functions in FilePath will not handle that correctly. In
+  // particular, RemoveTrailingPathSeparator() only removes one separator, and
+  // it is called in CreateDirectoriesRecursively() assuming that it will change
+  // a pathname from directory syntax (trailing separator) to filename syntax.
+  //
+  // On Windows this method also replaces the alternate path separator '/' with
+  // the primary path separator '\\', so that for example 'bar\\/\\foo' becomes
+  // 'bar\\foo'.
+    
+      explicit tuple(GTEST_BY_REF_(T0) f0, GTEST_BY_REF_(T1) f1,
+      GTEST_BY_REF_(T2) f2, GTEST_BY_REF_(T3) f3, GTEST_BY_REF_(T4) f4,
+      GTEST_BY_REF_(T5) f5) : f0_(f0), f1_(f1), f2_(f2), f3_(f3), f4_(f4),
+      f5_(f5) {}
+    
+    // Build a Table file from the contents of *iter.  The generated file
+// will be named according to meta->number.  On success, the rest of
+// *meta will be filled with metadata about the generated table.
+// If no data is present in *iter, meta->file_size will be set to
+// zero, and no Table file will be produced.
+Status BuildTable(const std::string& dbname, Env* env, const Options& options,
+                  TableCache* table_cache, Iterator* iter, FileMetaData* meta);
+    
+    // Return the name of the descriptor file for the db named by
+// 'dbname' and the specified incarnation number.  The result will be
+// prefixed with 'dbname'.
+std::string DescriptorFileName(const std::string& dbname, uint64_t number);
+    
+      // If the cache has no mapping for 'key', returns nullptr.
+  //
+  // Else return a handle that corresponds to the mapping.  The caller
+  // must call this->Release(handle) when the returned mapping is no
+  // longer needed.
+  virtual Handle* Lookup(const Slice& key) = 0;
+    
+    // Return a builtin comparator that uses lexicographic byte-wise
+// ordering.  The result remains the property of this module and
+// must not be deleted.
+LEVELDB_EXPORT const Comparator* BytewiseComparator();
+    
+    #ifndef STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
+#define STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
+    
+    #include 'leveldb/export.h'
     
     
-    {    Lock& lock;
-    Lock templock;
-};
-    
-    SECP256K1_INLINE static void secp256k1_fe_sqr_inner(uint64_t *r, const uint64_t *a) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            rcx:rbx = d
- *            r10-r14 = a0-a4
- *            r15     = M (0xfffffffffffff)
- *            rdi     = r
- *            rsi     = a / t?
- */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
-    'movq $0xfffffffffffff,%%r15\n'
-    }
-    
-    #include <stdint.h>
-#include <stdlib.h>
-    
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
+    {  // close database
+  delete db;
+  DestroyDB(dbpath, leveldb::Options());
 }
+    
+    // port/port_config.h availability is automatically detected via __has_include
+// in newer compilers. If LEVELDB_HAS_PORT_CONFIG_H is defined, it overrides the
+// configuration detection.
+#if defined(LEVELDB_HAS_PORT_CONFIG_H)
+    
+    #endif  // STORAGE_LEVELDB_TABLE_TWO_LEVEL_ITERATOR_H_
 
     
-    template <typename Dtype>
-void HDF5DataLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-  // Refuse transformation parameters since HDF5 is totally generic.
-  CHECK(!this->layer_param_.has_transform_param()) <<
-      this->type() << ' does not transform data.';
-  // Read the source to parse the filenames.
-  const string& source = this->layer_param_.hdf5_data_param().source();
-  LOG(INFO) << 'Loading list of HDF5 filenames from: ' << source;
-  hdf_filenames_.clear();
-  std::ifstream source_file(source.c_str());
-  if (source_file.is_open()) {
-    std::string line;
-    while (source_file >> line) {
-      hdf_filenames_.push_back(line);
-    }
-  } else {
-    LOG(FATAL) << 'Failed to open source file: ' << source;
-  }
-  source_file.close();
-  num_files_ = hdf_filenames_.size();
-  current_file_ = 0;
-  LOG(INFO) << 'Number of HDF5 files: ' << num_files_;
-  CHECK_GE(num_files_, 1) << 'Must have at least 1 HDF5 filename listed in '
-    << source;
-    }
-    
-    template <typename Dtype>
-void SGDSolver<Dtype>::SnapshotSolverStateToBinaryProto(
-    const string& model_filename) {
-  SolverState state;
-  state.set_iter(this->iter_);
-  state.set_learned_net(model_filename);
-  state.set_current_step(this->current_step_);
-  state.clear_history();
-  for (int i = 0; i < history_.size(); ++i) {
-    // Add history
-    BlobProto* history_blob = state.add_history();
-    history_[i]->ToProto(history_blob);
-  }
-  string snapshot_filename = Solver<Dtype>::SnapshotFilename('.solverstate');
-  LOG(INFO)
-    << 'Snapshotting solver state to binary proto file ' << snapshot_filename;
-  WriteProtoToBinaryFile(state, snapshot_filename.c_str());
-}
+        public:
+        BlockMomentumSGD(const MPIWrapperPtr& pMPI, size_t reportFreq, DEVICEID_TYPE devID, 
+                        bool useNestrovMomentum, bool resetSGDM, 
+                        double blockLearningRate, 
+                        double blockMomentumAsTimeConstant, size_t syncPeriod)
+            :IMASGD<ElemType>(pMPI, reportFreq, devID)
+        {
+            m_syncPeriodPerWorker = syncPeriod / pMPI->NumNodesInUse();
+            m_blockMomentumAsTimeConstantPerWorker = blockMomentumAsTimeConstant / pMPI->NumNodesInUse(); 
+            m_useNesterovMomentum = useNestrovMomentum;
+            m_resetSGDMomentumAfterAggregation = resetSGDM; 
+            m_blockLearningRate = blockLearningRate;
+        }
     
     
-    {  string* filename;
-  Blob<Dtype>* const blob_top_data_;
-  Blob<Dtype>* const blob_top_label_;
-  Blob<Dtype>* const blob_top_label2_;
-  vector<Blob<Dtype>*> blob_bottom_vec_;
-  vector<Blob<Dtype>*> blob_top_vec_;
-};
+    {            // Let's update our weights no matter what.
+            float secondsOnCommunication = 0.0f;
+            size_t totalSamples = 0;
+            ModelAggregationProcessing(samplesSinceLastSync, learnableNodes, smoothedGradients, totalSamples, secondsOnCommunication);
+        }
     
-    int hdf5_load_int(hid_t loc_id, const string& dataset_name) {
-  int val;
-  herr_t status = H5LTread_dataset_int(loc_id, dataset_name.c_str(), &val);
-  CHECK_GE(status, 0)
-    << 'Failed to load int dataset with name ' << dataset_name;
-  return val;
-}
-    
-    TYPED_TEST(NeuronLayerTest, TestLogGradient) {
-  typedef typename TypeParam::Dtype Dtype;
-  // Test default base of '-1' -- should actually set base := e.
-  const Dtype kBase = -1;
-  const Dtype kScale = 1;
-  const Dtype kShift = 0;
-  this->TestLogGradient(kBase, kScale, kShift);
-}
-    
-      // rotate
-  dict->lookup('Rotate', &obj1);
-  if (obj1.isInt()) {
-    rotate = obj1.getInt();
-  }
-  obj1.free();
-  while (rotate < 0) {
-    rotate += 360;
-  }
-  while (rotate >= 360) {
-    rotate -= 360;
-  }
-    
-    
-    {  XRef *xref;			// the xref table for this PDF file
-  Object pageObj;               // page dictionary
-  Ref pageRef;                  // page reference
-  int num;			// page number
-  PageAttrs *attrs;		// page attributes
-  Object annots;		// annotations array
-  Object contents;		// page contents
-  FormPageWidgets *pageWidgets; 			// the form for that page
-  Object thumb;			// page thumbnail
-  Object trans;			// page transition
-  Object actions;		// page addiction actions
-  double duration;              // page duration
-  GBool ok;			// true if page is valid
-};
-    
-    
-    {  GooList intervals;
-};
-
-    
-      // Get angle
-  int getAngle() { return angle; }
-    
-    private:
-    
-    
-    {    int num, gen;
-};
-    
-    
-    
-    GBool ExternalSecurityHandler::authorize(void *authData) {
-  char *key;
-  int length;
+        // generate a generic symbol name for a node
+    void GenerateName()
+    {
+        char buffer[10];
+        sprintf(buffer, '%d', ++s_nameCounter);
+        m_name = std::string('unnamed') + buffer;
     }
     
-    //------------------------------------------------------------------------
+            m_labelEmbeddingSize   = config('labelEmbeddingSize',   '10');
+        m_constForgetGateValue = config('constForgetGateValue', 'false');
+        m_constInputGateValue  = config('constInputGateValue',  'false');
+        m_constOutputGateValue = config('constOutputGateValue', 'false');
+    
+        // The following functions create nodes and add them to the net, but don't attach inputs (some don't have inputs).
+    // There are special versions for nodes with custom constructors, and a catch-all, CreateComputationNode(), for all others.
+    // TODO: Do we really need these? Folks who want to use C++ can instead say net->AddNodeToNet(New<>(...)), which is not that different.
+    // TODO: separate into nodes that have inputs and those that duplicate functions with input adding except just not adding inputs. Clear?
