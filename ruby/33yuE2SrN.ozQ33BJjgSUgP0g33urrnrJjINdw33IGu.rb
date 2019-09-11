4293ac57c4afd4d68094386c793e1e1d92becc33
SUNsung <weatherius@gmail.com>
2019-09-12 00:35:00 +0300

    Rewrite [33yuE2SrN.ozQ33BJjgSUgP0g33urrnrJjINdw33IGu.rb] File in: 2019-09-12 00:34:59

--- ruby/33yuE2SrN.ozQ33BJjgSUgP0g33urrnrJjINdw33IGu.rb
+++ ruby/33yuE2SrN.ozQ33BJjgSUgP0g33urrnrJjINdw33IGu.rb
@@ -1,139 +1,120 @@
 
         
-          before do
-    # Use a simple client for all data models
-    allow(Spaceship::TestFlight::Base).to receive(:client).and_return(mock_client)
-    allow(mock_client).to receive(:team_id).and_return('')
+            # The path used after unlocking the resource
+    def after_unlock_path_for(resource)
+      new_session_path(resource)  if is_navigational_format?
+    end
+    
+      # Returns real navigational formats which are supported by Rails
+  def navigational_formats
+    @navigational_formats ||= Devise.navigational_formats.select { |format| Mime::EXTENSION_LOOKUP[format.to_s] }
   end
     
-            all_tvos = devices.map do |device|
-          device = device.downcase
-          device.include?('apple tv')
+              path
         end
-        # Return true if all devices are iOS devices
-        return true unless all_tvos.include?(false)
+      end
     
-          def self.find_api_token(params)
-        return if params[:gsp_path]
-        unless params[:api_token].to_s.length > 0
-          Dir['./**/Info.plist'].each do |current|
-            result = Actions::GetInfoPlistValueAction.run(path: current, key: 'Fabric')
-            next unless result
-            next unless result.kind_of?(Hash)
-            params[:api_token] ||= result['APIKey']
-            UI.verbose('found an APIKey in #{current}')
+        if last_request_at.is_a? Integer
+      last_request_at = Time.at(last_request_at).utc
+    elsif last_request_at.is_a? String
+      last_request_at = Time.parse(last_request_at)
+    end
+    
+          @sign_out_via = options[:sign_out_via] || Devise.sign_out_via
+      @format = options[:format]
+    
+          klass.devise_modules.each do |mod|
+        constant = const_get(mod.to_s.classify)
+    
+              begin
+            lineno = frame.lineno-1
+            lines = ::File.readlines(frame.filename)
+            frame.pre_context_lineno = [lineno-CONTEXT, 0].max
+            frame.pre_context = lines[frame.pre_context_lineno...lineno]
+            frame.context_line = lines[lineno].chomp
+            frame.post_context_lineno = [lineno+CONTEXT, lines.size].min
+            frame.post_context = lines[lineno+1..frame.post_context_lineno]
+          rescue
           end
-        end
-      end
     
-          def extract_filters
-        fragments = []
+      # escape unicode
+  content.gsub!(/./) { |c| c.bytesize > 1 ? '\\u{#{c.codepoints.first.to_s(16)}}' : c }
     
-      describe '#can_update?' do
-    context 'when user can update_group_member' do
-      before do
-        allow(presenter).to receive(:can?).with(user, :update_group_member, presenter).and_return(true)
-      end
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   XSS and others
+    # Supported browsers:: Firefox 23+, Safari 7+, Chrome 25+, Opera 15+
+    #
+    # Description:: Content Security Policy, a mechanism web applications
+    #               can use to mitigate a broad class of content injection
+    #               vulnerabilities, such as cross-site scripting (XSS).
+    #               Content Security Policy is a declarative policy that lets
+    #               the authors (or server administrators) of a web application
+    #               inform the client about the sources from which the
+    #               application expects to load resources.
+    #
+    # More info::   W3C CSP Level 1 : https://www.w3.org/TR/CSP1/ (deprecated)
+    #               W3C CSP Level 2 : https://www.w3.org/TR/CSP2/ (current)
+    #               W3C CSP Level 3 : https://www.w3.org/TR/CSP3/ (draft)
+    #               https://developer.mozilla.org/en-US/docs/Web/Security/CSP
+    #               http://caniuse.com/#search=ContentSecurityPolicy
+    #               http://content-security-policy.com/
+    #               https://securityheaders.io
+    #               https://scotthelme.co.uk/csp-cheat-sheet/
+    #               http://www.html5rocks.com/en/tutorials/security/content-security-policy/
+    #
+    # Sets the 'Content-Security-Policy[-Report-Only]' header.
+    #
+    # Options: ContentSecurityPolicy configuration is a complex topic with
+    #          several levels of support that has evolved over time.
+    #          See the W3C documentation and the links in the more info
+    #          section for CSP usage examples and best practices. The
+    #          CSP3 directives in the 'NO_ARG_DIRECTIVES' constant need to be
+    #          presented in the options hash with a boolean 'true' in order
+    #          to be used in a policy.
+    #
+    class ContentSecurityPolicy < Base
+      default_options default_src: :none, script_src: ''self'',
+                      img_src: ''self'', style_src: ''self'',
+                      connect_src: ''self'', report_only: false
     
-            def clear_cache
-          # No-op
-        end
+            modes       = Array options[:escape]
+        @escaper    = options[:escaper]
+        @html       = modes.include? :html
+        @javascript = modes.include? :javascript
+        @url        = modes.include? :url
     
-      def output_chronic_duration_attribute(source_attribute)
-    value = attributes[source_attribute.to_s]
-    ChronicDuration.output(value, format: :short) if value
+    RunnerTool.configure
+    
+        def _send_keys(t, e)
+      '#{project.tmux} send-keys -t #{t} #{e} C-m'
+    end
   end
 end
 
     
-    Gem::Specification.new do |gem|
-  gem.name          = 'capistrano'
-  gem.version       = Capistrano::VERSION
-  gem.authors       = ['Tom Clements', 'Lee Hambley']
-  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
-  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
-  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
-  gem.homepage      = 'http://capistranorb.com/'
-    
-    World(RemoteCommandHelpers)
-
-    
-      at_exit do
-    if ENV['KEEP_RUNNING']
-      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
-      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
-    else
-      vagrant_cli_command('destroy -f')
+        def _project_root
+      project.root if project.root?
     end
+    
+      it 'creates an instance' do
+    expect(subject).to be_a(Tmuxinator::Pane)
   end
     
-        def ask(key, default=nil, options={})
-      question = Question.new(key, default, options)
-      set(key, question)
-    end
-    
-          def response
-        return @response if defined? @response
-    
-          def print_deprecation_warnings_if_applicable
-        if using_default_scm?
-          warn_add_git_to_capfile unless scm_plugin_installed?
-        elsif built_in_scm_name?
-          warn_set_scm_is_deprecated
-        elsif third_party_scm_name?
-          warn_third_party_scm_must_be_upgraded
+          # Existant directories which may contain project files
+      # Listed in search order
+      # Used by `implode` and `list` commands
+      def directories
+        if environment?
+          [environment]
+        else
+          [xdg, home].select { |d| File.directory? d }
         end
       end
     
-          attr_reader :validators
-    
-          def untrusted_keys
-        keys - @trusted_keys
-      end
-    
-        it 'returns a flare tag if there are 2 flare tags in the list' do
-      valid_article = create(:article, tags: %w[ama explainlikeimfive])
-      expect(described_class.new(valid_article).tag.name).to eq('explainlikeimfive')
-    end
-  end
-    
-      describe 'POST blocks' do
-    it 'creates block from input data' do
-      post '/blocks', params: {
-        block: {
-          input_css: '.blue { color: blue;}',
-          input_html: 'yo',
-          input_javascript: 'alert('hey')'
-        }
-      }
-      expect(Block.all.size).to eq(1)
-    end
-  end
-    
-        describe 'When it does not find article from id' do
-      it 'does not call the service' do
-        allow(Article).to receive(:find_by)
-        described_class.perform_now('Reaction', 456, milestone_service)
-        expect(milestone_service).not_to have_received(:call)
-      end
-    end
-    
-        initialize_with { Tmuxinator::Project.load(file) }
-  end
-end
-
-    
-      subject { instance }
-    
-      describe 'tmux_new_session_command' do
-    let(:command) { '#{executable} new-session -d -s #{session} -n #{window}' }
-    let(:executable) { project.tmux }
-    let(:session) { project.name }
-    let(:window) { project.windows.first.name }
-    
-        def self.create_connection
-      bunny_host = [ 'localhost' ]
-    
-      def new
-    @ip_address = @ip_pool.ip_addresses.build
-  end
+        context 'only ~/.tmuxinator exists' do
+      it 'is ~/.tmuxinator' do
+        allow(described_class).to receive(:environment?).and_return false
+        allow(described_class).to receive(:xdg?).and_return false
+        allow(described_class).to receive(:home?).and_return true
