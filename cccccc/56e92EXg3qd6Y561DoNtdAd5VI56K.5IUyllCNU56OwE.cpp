0ad198aa94efe3f209cb514896950cf7d8582ca1
SUNsung <weatherius@gmail.com>
2019-09-11 03:10:20 +0300

    Rewrite [56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp] File in: 2019-09-11 03:10:19

--- cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
+++ cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
@@ -1,87 +1,118 @@
 
         
-          std::multimap<grpc::string_ref, grpc::string_ref>* GetRecvTrailingMetadata()
-      override {
-    return recv_trailing_metadata_->map();
-  }
+          // Post 'reopen' event.
+  // (This event is received when the user clicked the icon in the Dock).
+  static void EmitReopenEvent();
     
-    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
-  for (unsigned int i = 0; i < args_.size(); i++) {
-    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
-      return args_[i].value.string;
+      int id() const { return id_; }
+  std::string extension_id_;
+  ObjectManager* object_manager() const { return object_manager_.get(); }
+    
+    v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
+                                           int object_id,
+                                           const std::string& type,
+                                           const std::string& method,
+                                           v8::Handle<v8::Value> args) {
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  scoped_ptr<V8ValueConverter> converter(V8ValueConverter::create());
     }
-  }
-  return '';
+    
+    void MenuDelegate::ExecuteCommand(int command_id, int event_flags) {
+  if (command_id < 0)
+    return;
+    }
+    
+    void Menu::Remove(MenuItem* menu_item, int pos) {
+  std::vector<MenuItem*>::iterator begin = menu_items.begin();
+  menu_items.erase(begin+pos);
+  gtk_container_remove(GTK_CONTAINER(menu_), menu_item->menu_item_);
 }
     
-    class ClientChannelStressTest {
- public:
-  void Run() {
-    Start();
-    // Keep updating resolution for the test duration.
-    gpr_log(GPR_INFO, 'Start updating resolution.');
-    const auto wait_duration =
-        std::chrono::milliseconds(kResolutionUpdateIntervalMs);
-    std::vector<AddressData> addresses;
-    auto start_time = std::chrono::steady_clock::now();
-    while (true) {
-      if (std::chrono::duration_cast<std::chrono::seconds>(
-              std::chrono::steady_clock::now() - start_time)
-              .count() > kTestDurationSec) {
-        break;
-      }
-      // Generate a random subset of balancers.
-      addresses.clear();
-      for (const auto& balancer_server : balancer_servers_) {
-        // Select each address with probability of 0.8.
-        if (std::rand() % 10 < 8) {
-          addresses.emplace_back(AddressData{balancer_server.port_, true, ''});
-        }
-      }
-      std::shuffle(addresses.begin(), addresses.end(),
-                   std::mt19937(std::random_device()()));
-      SetNextResolution(addresses);
-      std::this_thread::sleep_for(wait_duration);
-    }
-    gpr_log(GPR_INFO, 'Finish updating resolution.');
-    Shutdown();
-  }
-    }
+    static KeyMap keymap = {
+  {'`'    , 'Backquote'},
+  {'\\'   , 'Backslash'},
+  {'['    , 'BracketLeft'},
+  {']'    , 'BracketRight'},
+  {','    , 'Comma'},
+  {'='    , 'Equal'},
+  {'-'    , 'Minus'},
+  {'.'    , 'Period'},
+  {'''    , 'Quote'},
+  {';'    , 'Semicolon'},
+  {'/'    , 'Slash'},
+  {'\n'   , 'Enter'},
+  {'\t'   , 'Tab'},
+  {'UP'   , 'ArrowUp'},
+  {'DOWN' , 'ArrowDown'},
+  {'LEFT' , 'ArrowLeft'},
+  {'RIGHT', 'ArrowRight'},
+  {'ESC'  , 'Escape'},
+  {'MEDIANEXTTRACK', 'MediaTrackNext'},
+  {'MEDIAPREVTRACK', 'MediaTrackPrevious'}
+};
     
-    /* Schedule a shutdown of the socket operations. Will call the pending
-   operations to abort them. We need to do that this way because of the
-   various callsites of that function, which happens to be in various
-   mutex hold states, and that'd be unsafe to call them directly. */
-void grpc_winsocket_shutdown(grpc_winsocket* winsocket) {
-  /* Grab the function pointer for DisconnectEx for that specific socket.
-     It may change depending on the interface. */
-  int status;
-  GUID guid = WSAID_DISCONNECTEX;
-  LPFN_DISCONNECTEX DisconnectEx;
-  DWORD ioctl_num_bytes;
-    }
     
-    UniquePtr<char> GlobalConfigEnvString::Get() {
-  UniquePtr<char> str = GetValue();
-  if (str == nullptr) {
-    return UniquePtr<char>(gpr_strdup(default_value_));
-  }
-  return str;
+    {  return RespondNow(NoArguments());
 }
     
-    #include <grpc/grpc_cronet.h>
+      // implement nw.Screen.stopMonitor()
+  class NwScreenStopMonitorFunction : public NWSyncExtensionFunction {
+  public:
+    NwScreenStopMonitorFunction();
+    bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    #include <stdio.h>
+#include <string.h>
+    
+    int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
+}
+
+    
+    // Ref/unref the lock, for when we're sharing the lock ownership
+// Prefer to use the macros above
+grpc_combiner* grpc_combiner_ref(grpc_combiner* lock GRPC_COMBINER_DEBUG_ARGS);
+void grpc_combiner_unref(grpc_combiner* lock GRPC_COMBINER_DEBUG_ARGS);
+// Fetch a scheduler to schedule closures against
+grpc_closure_scheduler* grpc_combiner_scheduler(grpc_combiner* lock);
+// Scheduler to execute \a action within the lock just prior to unlocking.
+grpc_closure_scheduler* grpc_combiner_finally_scheduler(grpc_combiner* lock);
+    
+    #ifndef GRPCPP_CREATE_CHANNEL_H
+#define GRPCPP_CREATE_CHANNEL_H
+    
+    #include <grpc/grpc.h>
 #include <grpcpp/channel.h>
-#include <grpcpp/support/channel_arguments.h>
-#include 'src/cpp/client/create_channel_internal.h'
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/security/credentials.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
     
-        auto stream = stub->ResponseStream(&context, request);
-    EXPECT_TRUE(stream->Read(&response));
-    EXPECT_EQ(response.message(), request.message() + '0_dup');
-    EXPECT_TRUE(stream->Read(&response));
-    EXPECT_EQ(response.message(), request.message() + '1_dup');
-    EXPECT_TRUE(stream->Read(&response));
-    EXPECT_EQ(response.message(), request.message() + '2_dup');
-    EXPECT_FALSE(stream->Read(&response));
     
-      service->RequestRequestStream(&srv_ctx_, &srv_stream, cq_.get(), cq_.get(),
-                                tag(2));
+    {
+    {
+    {				if (PathFileExists(szArgList[i]))
+				{
+					cmderOptions.cmderStart = szArgList[i];
+					i++;
+				}
+				else
+				{
+					MessageBox(NULL, L'Unrecognized parameter.\n\nValid options:\n\n    /c [CMDER User Root Path]\n\n    /task [ConEmu Task Name]\n\n    [/start [Start in Path] | [Start in Path]]\n\n    /single\n\n    /m\n\n    /x [ConEmu extra arguments]\n\nor\n\n    /register [USER | ALL]\n\nor\n\n    /unregister [USER | ALL]\n', MB_TITLE, MB_OK);
+					cmderOptions.error = true;
+				}
+			}
+			else
+			{
+				MessageBox(NULL, L'Unrecognized parameter.\n\nValid options:\n\n    /c [CMDER User Root Path]\n\n    /task [ConEmu Task Name]\n\n    [/start [Start in Path] | [Start in Path]]\n\n    /single\n\n    /m\n\n    /x [ConEmu extra arguments]\n\nor\n\n    /register [USER | ALL]\n\nor\n\n    /unregister [USER | ALL]\n', MB_TITLE, MB_OK);
+				cmderOptions.error = true;
+			}
+		}
+	}
