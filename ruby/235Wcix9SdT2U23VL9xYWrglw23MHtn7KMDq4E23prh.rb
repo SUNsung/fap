291366e0ff30819497557a37d4727d17ec996f28
SUNsung <weatherius@gmail.com>
2019-09-12 06:47:09 +0300

    Rewrite [235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb] File in: 2019-09-12 06:47:09

--- ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
+++ ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
@@ -1,128 +1,163 @@
 
         
-              topics.each do |t|
-        t.posters = t.posters_summary(
-          avatar_lookup: avatar_lookup,
-          primary_group_lookup: primary_group_lookup,
-          translations: translations
-        )
-      end
+            # Generate 'basename without extension' and strip away any trailing periods.
+    # NOTE: `String#gsub` removes all trailing periods (in comparison to `String#chomp`)
+    def basename
+      @basename ||= File.basename(name, extname).gsub(%r!\.*\z!, '')
     end
     
-            if path.present?
-          path = '#{path}/' unless path.end_with?('/')
-          path = Regexp.quote(path)
-        end
+    module Jekyll
+  module Drops
+    class SiteDrop < Drop
+      extend Forwardable
     
-        @s3_client.stub_responses(:put_object, -> (context) do
-      check_context(context)
+    def graceful_require
+  Jekyll::External.require_with_graceful_fail('json')
+  JSON.pretty_generate(DATA)
+end
     
-        it 'works when default_locale is not English' do
-      SiteSetting.default_locale = :de
-    
-        h3('Configurable attributes') + to_table(header, content)
-  end
-    
-          it_behaves_like 'accepts', 'bar > 42'
-      it_behaves_like 'accepts', 'bar <= nil'
-      it_behaves_like 'accepts', 'a > 3 && 5 > a'
-      it_behaves_like 'offense', 'answer != 42'
-      it_behaves_like 'offense', 'foo == false'
-    end
-  end
+    # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
 end
 
     
-          # Checks whether the method is a predicate method.
-      #
-      # @return [Boolean] whether the method is a predicate method
-      def predicate_method?
-        method_name.to_s.end_with?('?')
+    Then(/^the default stage files are created$/) do
+  staging = TestApp.test_app_path.join('config/deploy/staging.rb')
+  production = TestApp.test_app_path.join('config/deploy/production.rb')
+  expect(File.exist?(staging)).to be true
+  expect(File.exist?(production)).to be true
+end
+    
+        (stdout + stderr).each_line { |line| puts '[vagrant] #{line}' }
+    
+        def add_filter(filter=nil, &block)
+      if block
+        raise ArgumentError, 'Both a block and an object were given' if filter
+    
+          def print_deprecation_warnings_if_applicable
+        if using_default_scm?
+          warn_add_git_to_capfile unless scm_plugin_installed?
+        elsif built_in_scm_name?
+          warn_set_scm_is_deprecated
+        elsif third_party_scm_name?
+          warn_third_party_scm_must_be_upgraded
+        end
       end
     
-    describe FPM::Package::CPAN, :if => have_cpanm do
-  subject { FPM::Package::CPAN.new }
+    # We use a special :_default_git value so that SCMResolver can tell whether the
+# default has been replaced by the user via `set`.
+set_if_empty :scm, Capistrano::Configuration::SCMResolver::DEFAULT_GIT
+set_if_empty :branch, 'master'
+set_if_empty :deploy_to, -> { '/var/www/#{fetch(:application)}' }
+set_if_empty :tmp_dir, '/tmp'
     
-          it 'it should prefix requirements.txt' do
-        subject.input(example_dir)
-        insist { subject.dependencies.sort } == ['python-rtxt-dep1 > 0.1', 'python-rtxt-dep2 = 0.1', 'python-rtxt-dep4  ']
-      end
+      before { sign_in user }
     
-          it 'should have the custom apps' do
-        pending('Ruby 1.x and 2.0.x are unsupported for Snap because it lacks Psych::safe_load') if is_old_ruby
-        insist { input.attributes[:snap_apps] } == []
-      end
+      describe 'PUT /chat_channel_memberships/:id' do
+    before do
+      user.add_role(:super_admin)
+      post '/chat_channel_memberships', params: {
+        chat_channel_membership: { user_id: second_user.id, chat_channel_id: chat_channel.id }
+      }
+    end
     
-      shared_examples_for :Mutator do |item|
-    context 'when set' do
-      let(:value) { 'whatever' }
-      it 'should return the set value' do
-        expect(subject.send('#{item}=', value)).to(be == value)
-      end
-    
-      config.vm.define 'debian7' do |debian7|
-    debian7.vm.box = 'puppetlabs/centos-7.0-64-puppet'
+      def delete_confirm?
+    update?
   end
     
-    require 'fpm'
-require 'tmpdir'
-require 'fpm/package/pleaserun'
-    
-          mandatory(@command.args.any? || @command.inputs || @command.input_type == 'empty',
-                'No parameters given. You need to pass additional command ' \
-                'arguments so that I know what you want to build packages ' \
-                'from. For example, for '-s dir' you would pass a list of ' \
-                'files and directories. For '-s gem' you would pass a one' \
-                ' or more gems to package from. As a full example, this ' \
-                'will make an rpm of the 'json' rubygem: ' \
-                '`fpm -s gem -t rpm json`')
-    end # def validate
-    
-      def edit_file(path)
-    editor = ENV['FPM_EDITOR'] || ENV['EDITOR'] || 'vi'
-    logger.info('Launching editor', :file => path)
-    command = '#{editor} #{Shellwords.escape(path)}'
-    system('#{editor} #{Shellwords.escape(path)}')
-    if !$?.success?
-      raise ProcessFailed.new(''#{editor}' failed (exit code ' \
-                              '#{$?.exitstatus}) Full command was: ' \
-                              '#{command}');
+      def self.upbuff!(buffer_update_id, admin_id, body_text, status)
+    buffer_update = BufferUpdate.find(buffer_update_id)
+    if status == 'confirmed'
+      buffer_response = send_to_buffer(body_text, buffer_update.buffer_profile_id_code)
+      buffer_update.update!(buffer_response: buffer_response, status: status, approver_user_id: admin_id, body_text: body_text)
+    else
+      buffer_update.update!(status: status, approver_user_id: admin_id)
     end
-    
-        # control tar.
-    begin
-      write_pkginfo(control_path)
-      write_control_scripts(control_path)
-      tar_path(control_path, controltar_path)
-    ensure
-      FileUtils.rm_r(control_path)
-    end
-    
-        # Query details about our now-installed package.
-    # We do this by using 'npm ls' with json + long enabled to query details
-    # about the installed package.
-    npm_ls_out = safesystemout(attributes[:npm_bin], 'ls', '--json', '--long', *npm_flags)
-    npm_ls = JSON.parse(npm_ls_out)
-    name, info = npm_ls['dependencies'].first
-    
-      # Map of what scripts are named.
-  SCRIPT_MAP = {
-    :before_install     => 'preinstall',
-    :after_install      => 'postinstall',
-  } unless defined?(SCRIPT_MAP)
-    
-        # Cleanup
-    safesystem('mv', build_path('#{name}.p5p'), output_path)
-    
-        # Make one file. The installscript can unpack itself.
-    `cat #{install_script} #{payload} > #{output_path}`
-    FileUtils.chmod('+x', output_path)
   end
     
-      def architecture
-    case @architecture
-    when nil, 'native'
-      @architecture = %x{uname -p}.chomp
+      def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to '/internal/pages'
+  end
+    
+        def URIEncodePair(cc1, cc2, result, index)
+      u = ((cc1 >> 6) & 0xF) + 1;
+      w = (cc1 >> 2) & 0xF;
+      x = cc1 & 3;
+      y = (cc2 >> 6) & 0xF;
+      z = cc2 & 63;
+      octets = Array.new(4);
+      octets[0] = (u >> 2) + 240;
+      octets[1] = (((u & 3) << 4) | w) + 128;
+      octets[2] = ((x << 4) | y) + 128;
+      octets[3] = z + 128;
+      return URIEncodeOctets(octets, result, index);
     end
-    # 'all' is a valid arch according to
-    # http://www.bolthole.com/solaris/makeapackage.html
+    
+          def previous_link
+        label = '&laquo; Previous'
+        if @page_num == 1
+          %(<span class='disabled'>#{label}</span>)
+        else
+          link = url('/history/#{@page.name}?page=#{@page_num-1}')
+          %(<a href='#{link}' hotkey='h'>#{label}</a>)
+        end
+      end
+    
+          def partial(name)
+        if name == :author_template
+          self.class.partial('history_authors/#{@wiki.user_icons}')
+        else
+          super
+        end
+      end
+    
+          def css # custom css
+        @css
+      end
+    
+      test 'create sets the correct path for a relative path subdirectory' do
+    dir  = 'foodir'
+    name = '#{dir}/bar'
+    get '/create/#{name}'
+    assert_match(/\/#{dir}/, last_response.body)
+    assert_no_match(/[^\/]#{dir}/, last_response.body)
+  end
+    
+        def wiki_new
+      Gollum::Wiki.new(settings.gollum_path, settings.wiki_options)
+    end
+    
+        it 'logs the exception to Sidekiq.logger' do
+      Component.new.invoke_exception(:a => 1)
+      @str_logger.rewind
+      log = @str_logger.readlines
+      assert_match(/'a':1/, log[0], 'didn't include the context')
+      assert_match(/Something didn't work!/, log[1], 'didn't include the exception message')
+      assert_match(/test\/test_exception_handler.rb/, log[2], 'didn't include the backtrace')
+    end
+    
+          assert SomeScheduledWorker.perform_in(5.days.from_now, 'mike')
+      assert_equal 3, ss.size
+    
+      describe 'error handling' do
+    it 'deals with user-specified error handlers which raise errors' do
+      output = capture_logging do
+        begin
+          Sidekiq.error_handlers << proc {|x, hash|
+            raise 'boom'
+          }
+          cli = Sidekiq::CLI.new
+          cli.handle_exception(RuntimeError.new('hello'))
+        ensure
+          Sidekiq.error_handlers.pop
+        end
+      end
+      assert_includes output, 'boom'
+      assert_includes output, 'ERROR'
+    end
+  end
