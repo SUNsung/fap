b144861c5ac950cb18c1dfa41d9064c6c48c7948
SUNsung <weatherius@gmail.com>
2019-09-12 03:43:25 +0300

    Rewrite [03xyzO7yPLcTM03JmKV0wFjN7U03sCr95JyGgh203rSs.py] File in: 2019-09-12 03:43:25

--- pyton/03xyzO7yPLcTM03JmKV0wFjN7U03sCr95JyGgh203rSs.py
+++ pyton/03xyzO7yPLcTM03JmKV0wFjN7U03sCr95JyGgh203rSs.py
@@ -1,54 +1,254 @@
 
         
-        
-logger = logging.getLogger(__name__)
+                channel_info = self._download_json(
+            'http://api.vfan.vlive.tv/vproxy/channelplus/decodeChannelCode',
+            channel_code, note='Downloading decode channel code',
+            query={
+                'app_id': app_id,
+                'channelCode': channel_code,
+                '_': int(time.time())
+            })
+    
+            return {
+            'id': video_id,
+            'title': title,
+            'formats': formats,
+            'thumbnails': thumbnails,
+            'description': video_data.get('short_description'),
+            'like_count': int_or_none(video_data.get('like_count')),
+            'timestamp': parse_iso8601(video_data.get('released_at')),
+            'series': series,
+            'episode': episode,
+            'season': season,
+            'season_id': season_id,
+            'season_number': season_number,
+            'episode_number': episode_number,
+            'subtitles': subtitles,
+            'age_limit': parse_age_limit(video_data.get('rating')),
+        }
+
+    
+    secret_msg = b'Secret message goes here'
+    
+    import rsa
+import json
+from binascii import hexlify
+    
+        template = template.replace('@SITES@', textwrap.indent('\n'.join(ie_htmls), '\t'))
+    
+    
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
+    
+    
+if __name__ == '__main__':
+    main()
+
+    
+                        if not split_option[-1].startswith('-'):  # metavar
+                        option = ' '.join(split_option[:-1] + ['*%s*' % split_option[-1]])
+    
+    
+class TestCache(unittest.TestCase):
+    def setUp(self):
+        TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+        TESTDATA_DIR = os.path.join(TEST_DIR, 'testdata')
+        _mkdir(TESTDATA_DIR)
+        self.test_dir = os.path.join(TESTDATA_DIR, 'cache_test')
+        self.tearDown()
+    
+        :param filepath: Optional filepath the the blns.txt file
+    :returns: The list of naughty strings
+    '''
+    
+    if __name__ == '__main__':
+    unittest.main()
+
+    
+            results = []
+        threads = []
+        end_event = []
+        start_cond = threading.Condition()
+        started = False
+        if nthreads > 0:
+            # Warm up
+            func(*args)
+    
+    # Recursively yields relative pathnames inside a literal directory.
+def _rlistdir(dirname, dironly):
+    names = list(_iterdir(dirname, dironly))
+    for x in names:
+        if not _ishidden(x):
+            yield x
+            path = os.path.join(dirname, x) if dirname else x
+            for y in _rlistdir(path, dironly):
+                yield os.path.join(x, y)
+    
+        # bpo-31217: Integer pool to get a single integer object for the same
+    # value. The pool is used to prevent false alarm when checking for memory
+    # block leaks. Fill the pool with values in -1000..1000 which are the most
+    # common (reference, memory block, file descriptor) differences.
+    int_pool = {value: value for value in range(-1000, 1000)}
+    def get_pooled_int(value):
+        return int_pool.setdefault(value, value)
+    
+        def has_ext_modules(self):
+        return self.distribution.has_ext_modules()
+    
+        For Python-coded functions and methods, the first line is introspected.
+    Delete 'self' parameter for classes (.__init__) and bound methods.
+    The next lines are the first lines of the doc string up to the first
+    empty line or _MAX_LINES.    For builtins, this typically includes
+    the arguments in addition to the return value.
+    '''
+    argspec = default = ''
+    try:
+        ob_call = ob.__call__
+    except BaseException:
+        return default
+    
+            if List.__doc__ is not None:
+            tiptest(List,
+                    f'(iterable=(), /){calltip._argument_positional}'
+                    f'\n{List.__doc__}')
+        tiptest(list.__new__,
+              '(*args, **kwargs)\n'
+              'Create and return a new object.  '
+              'See help(type) for accurate signature.')
+        tiptest(list.__init__,
+              '(self, /, *args, **kwargs)'
+              + calltip._argument_positional + '\n' +
+              'Initialize self.  See help(type(self)) for accurate signature.')
+        append_doc = (calltip._argument_positional
+                      + '\nAppend object to the end of the list.')
+        tiptest(list.append, '(self, object, /)' + append_doc)
+        tiptest(List.append, '(self, object, /)' + append_doc)
+        tiptest([].append, '(object, /)' + append_doc)
     
         def setUp(self):
-        self.base_dir = '/example_path'
-        self.vhosts = util.get_vh_truth(
-            self.base_dir, 'debian_apache_2_4/multiple_vhosts')
+        from acme.errors import MissingNonce
+        self.response = mock.MagicMock(headers={})
+        self.response.request.method = 'FOO'
+        self.error = MissingNonce(self.response)
     
-            # pylint: disable=no-member
-        self.auth.add_parser_arguments(m)
-    
-    import httplib2
-import zope.interface
-from googleapiclient import discovery
-from googleapiclient import errors as googleapiclient_errors
-from oauth2client.service_account import ServiceAccountCredentials
-    
-        lineage = domains.split(',')[0]
-    server_cert = ssl.get_server_certificate(('localhost', context.tls_alpn_01_port))
-    with open(os.path.join(context.workspace, 'conf/live/{0}/cert.pem'.format(lineage)), 'r') as file:
-        certbot_cert = file.read()
-    
-        n_bins: int
-        Number of judgement categories to prefrom calculation over.
-        Prediction are binned based on probability, since 'descrete' 
-        probabilities aren't required. 
+        def test_kid_serialize(self):
+        from acme.jws import JWS
+        jws = JWS.sign(payload=b'foo', key=self.privkey,
+                       alg=jose.RS256, nonce=self.nonce,
+                       url=self.url, kid=self.kid)
+        self.assertEqual(jws.signature.combined.nonce, self.nonce)
+        self.assertEqual(jws.signature.combined.url, self.url)
+        self.assertEqual(jws.signature.combined.kid, self.kid)
+        self.assertEqual(jws.signature.combined.jwk, None)
+        # TODO: check that nonce is in protected header
     
     
-def sample_from_pvect(pvect):
-    ''' Provided utility function: given a symbolic vector of
-    probabilities (which MUST sum to 1), sample one element
-    and return its index.
-    '''
-    onehot_sample = rng.multinomial(n=1, pvals=pvect)
-    sample = onehot_sample.argmax()
-    return sample
+Named Arguments
+---------------
     
-        LOGPATS  = r'(\S+) (\S+) \[(.*?)\] (\S+) (\S+) ' \
-               r'(\S+) (\S+) (\S+) ('([^']+)'|-) ' \
-               r'(\S+) (\S+) (\S+) (\S+) (\S+) (\S+) ' \
-               r'('([^']+)'|-) ('([^']+)'|-)'
-    NUM_ENTRIES_PER_LINE = 17
-    logpat = re.compile(LOGPATS)
+        @classmethod
+    def entry_point_to_plugin_name(cls, entry_point):
+        '''Unique plugin name for an ``entry_point``'''
+        if entry_point.dist.key in cls.PREFIX_FREE_DISTRIBUTIONS:
+            return entry_point.name
+        return entry_point.dist.key + ':' + entry_point.name
     
-        ax.plot([0.3, 0.5, 0.7], [0.6, 0.9, 0.6], '-k')
-    ax.plot([0.12, 0.3, 0.38], [0.3, 0.6, 0.3], '-k')
-    ax.plot([0.62, 0.7, 0.88], [0.3, 0.6, 0.3], '-k')
-    ax.plot([0.0, 0.12, 0.20], [0.0, 0.3, 0.0], '--k')
-    ax.plot([0.28, 0.38, 0.48], [0.0, 0.3, 0.0], '--k')
-    ax.plot([0.52, 0.62, 0.72], [0.0, 0.3, 0.0], '--k')
-    ax.plot([0.8, 0.88, 1.0], [0.0, 0.3, 0.0], '--k')
-    ax.axis([0, 1, 0, 1])
+    
+def goal_distance(goal_a, goal_b):
+    assert goal_a.shape == goal_b.shape
+    return np.linalg.norm(goal_a - goal_b, axis=-1)
+    
+        def _limit_coordinates(self, coord):
+        '''
+        Prevent the agent from falling out of the grid world
+        :param coord:
+        :return:
+        '''
+        coord[0] = min(coord[0], self.shape[0] - 1)
+        coord[0] = max(coord[0], 0)
+        coord[1] = min(coord[1], self.shape[1] - 1)
+        coord[1] = max(coord[1], 0)
+        return coord
+    
+        def reset(self):
+        self._check_levelup()
+        self.last_action = None
+        self.last_reward = 0
+        self.read_head_position = self.READ_HEAD_START
+        self.write_head_position = 0
+        self.episode_total_reward = 0.0
+        self.time = 0
+        length = self.np_random.randint(3) + AlgorithmicEnv.min_length
+        self.input_data = self.generate_input_data(length)
+        self.target = self.target_from_input_data(self.input_data)
+        return self._get_obs()
+    
+            self.moon = self.world.CreateStaticBody( shapes=edgeShape(vertices=[(0, 0), (W, 0)]) )
+        self.sky_polys = []
+        for i in range(CHUNKS-1):
+            p1 = (chunk_x[i],   smooth_y[i])
+            p2 = (chunk_x[i+1], smooth_y[i+1])
+            self.moon.CreateEdgeFixture(
+                vertices=[p1,p2],
+                density=0,
+                friction=0.1)
+            self.sky_polys.append( [p1, p2, (p2[0],H), (p1[0],H)] )
+    
+            # Go from one checkpoint to another to create track
+        x, y, beta = 1.5*TRACK_RAD, 0, 0
+        dest_i = 0
+        laps = 0
+        track = []
+        no_freeze = 2500
+        visited_other_side = False
+        while True:
+            alpha = math.atan2(y, x)
+            if visited_other_side and alpha > 0:
+                laps += 1
+                visited_other_side = False
+            if alpha < 0:
+                visited_other_side = True
+                alpha += 2*math.pi
+            while True: # Find destination from checkpoints
+                failed = True
+                while True:
+                    dest_alpha, dest_x, dest_y = checkpoints[dest_i % len(checkpoints)]
+                    if alpha <= dest_alpha:
+                        failed = False
+                        break
+                    dest_i += 1
+                    if dest_i % len(checkpoints) == 0:
+                        break
+                if not failed:
+                    break
+                alpha -= 2*math.pi
+                continue
+            r1x = math.cos(beta)
+            r1y = math.sin(beta)
+            p1x = -r1y
+            p1y = r1x
+            dest_dx = dest_x - x  # vector towards destination
+            dest_dy = dest_y - y
+            proj = r1x*dest_dx + r1y*dest_dy  # destination vector projected on rad
+            while beta - alpha >  1.5*math.pi:
+                 beta -= 2*math.pi
+            while beta - alpha < -1.5*math.pi:
+                 beta += 2*math.pi
+            prev_beta = beta
+            proj *= SCALE
+            if proj >  0.3:
+                 beta -= min(TRACK_TURN_RATE, abs(0.001*proj))
+            if proj < -0.3:
+                 beta += min(TRACK_TURN_RATE, abs(0.001*proj))
+            x += p1x*TRACK_DETAIL_STEP
+            y += p1y*TRACK_DETAIL_STEP
+            track.append( (alpha,prev_beta*0.5 + beta*0.5,x,y) )
+            if laps > 4:
+                 break
+            no_freeze -= 1
+            if no_freeze==0:
+                 break
+        # print '\n'.join([str(t) for t in enumerate(track)])
