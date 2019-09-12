439b0ab2f7e378b1c4d67afaf0d975eb4a78424b
SUNsung <weatherius@gmail.com>
2019-09-12 03:22:45 +0300

    Rewrite [51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb] File in: 2019-09-12 03:22:45

--- ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
+++ ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
@@ -1,90 +1,171 @@
 
         
-                expect(FileStore::BaseStore.new.get_path_for_upload(upload))
-          .to eq('original/2X/4/4170ac2a2782a1516fe9e13d7322ae482c1bd594.png')
+              attr_accessor :type_of_file
+    
+          it 'returns the current version once parsed with prerelease' do
+        test_content = 'spec.version = '1.3.2-SNAPSHOT''
+        result = @version_podspec_file.parse(test_content)
+        expect(result).to eq('1.3.2-SNAPSHOT')
+        expect(@version_podspec_file.version_value).to eq('1.3.2-SNAPSHOT')
+        expect(@version_podspec_file.version_match[:major]).to eq('1')
+        expect(@version_podspec_file.version_match[:minor]).to eq('3')
+        expect(@version_podspec_file.version_match[:patch]).to eq('2')
+        expect(@version_podspec_file.version_match[:prerelease]).to eq('SNAPSHOT')
       end
+    
+            device = find_device(device_name, os_version)
+        if device.nil?
+          UI.user_error!('No device found named '#{device_name}' for version '#{os_version}'')
+        elsif device.os_version != os_version
+          UI.important('Using device named '#{device_name}' with version '#{device.os_version}' because no match was found for version '#{os_version}'')
+        end
+        value = 'platform=#{os} Simulator,id=#{device.udid},OS=#{device.os_version}'
+    
+        it 'should fail to create a device name longer than 50 characters' do
+      expect do
+        Spaceship::Device.create!(name: 'Demo Device (Apple Watch Series 3 - 42mm GPS Black)', udid: '7f6c8dc83d77134b5a3a1c53f1202b395b04482b')
+      end.to raise_error('Device name must be 50 characters or less. \'Demo Device (Apple Watch Series 3 - 42mm GPS Black)\' has a 51 character length.')
     end
     
-        result = result.where('topics.deleted_at IS NULL') if require_deleted_clause
-    result = result.where('topics.posts_count <= ?', options[:max_posts]) if options[:max_posts].present?
-    result = result.where('topics.posts_count >= ?', options[:min_posts]) if options[:min_posts].present?
+    module Spaceship
+  module Portal
+    # Represents a device from the Apple Developer Portal
+    class Device < PortalBase
+      # @return (String) The ID given from the developer portal. You'll probably not need it.
+      # @example
+      #   'XJXGVS46MW'
+      attr_accessor :id
     
-            # no optimisation possible without losing details
-        expect(upload.filesize).to eq(9558)
+          def self.return_type
+        :string
+      end
     
-      def staff_available_actions
-    UserHistory.staff_actions.sort.map do |name|
-      {
-        id: name,
-        action_id: UserHistory.actions[name] || UserHistory.actions[:custom_staff],
-      }
+          def self.category
+        :misc
+      end
     end
   end
 end
 
     
+            _definee, _method_name, _args, body = *node
+        return add_offense(node, :expression, format(MISSING_KEYS_MSG, key: self.shared_values_constants.join(', '), list: [])) if body.nil?
+        return add_offense(node, :expression, format(MISSING_KEYS_MSG, key: self.shared_values_constants.join(', '), list: [])) unless body.array_type?
     
-# Documentation comments:
-#  - All documentation comes from Nutshell.
-#  - MonitorMixin.new_cond appears in the example, but is not documented in
-#    Nutshell.
-#  - All the internals (internal modules Accessible and Initializable, class
-#    ConditionVariable) appear in RDoc.  It might be good to hide them, by
-#    making them private, or marking them :nodoc:, etc.
-#  - RDoc doesn't recognise aliases, so we have mon_synchronize documented, but
-#    not synchronize.
-#  - mon_owner is in Nutshell, but appears as an accessor in a separate module
-#    here, so is hard/impossible to RDoc.  Some other useful accessors
-#    (mon_count and some queue stuff) are also in this module, and don't appear
-#    directly in the RDoc output.
-#  - in short, it may be worth changing the code layout in this file to make the
-#    documentation easier
-
+              Fastlane::FastFile.new.parse('lane :test do
+            get_managed_play_store_publishing_rights(
+              json_key: '#{json_key_path}'
+            )
+          end').runner.execute(:test)
+        end
     
-        assert @co.document_children
     
-        comment = doc(para('this is a comment'))
     
+        # In the case where a plus or slash happened at the end of a chunk,
+    # we'll have two dots next to each other, so fix it up.  Note that this
+    # is searching for literal dots, not a regex matching any two
+    # characters
+    b64.gsub!('..', '.')
+    
+      def initialize(info = {})
+    super(merge_info(info,
+      'Name'          => 'Unix Command Shell, Reverse TCP (/dev/tcp)',
+      'Description'   => %q{
+        Creates an interactive shell via bash's builtin /dev/tcp.
+    }
+    
+        if opts[:exitfunk]
+      asm << asm_exitfunk(opts)
     end
-
     
-        assert_equal expected, doc
+      CachedSize = 616
+    
+    
+    
+      #
+  # Displays information about the various creds commands
+  #
+  def cmd_creds_usage(provider)
+    print_line('Usage: creds_#{provider} [options]')
+    print_line
+    print_line('Dump #{provider} credentials.')
+    print_line(@@creds_opts.usage)
   end
     
-        out = m.convert doc, tt
+        # Write script to %TEMP% on target
+    script_on_target = write_exe_to_target(raw, rexename)
     
-      def test_file_equals
-    @d.file = 'file.rb'
+      # Use an array of `from -> to` associations so that things
+  # such as metsrv can be copied from the appropriate location
+  # but named correctly on the target.
+  bins = {
+    'metsrv.x86.dll'    => 'metsrv.dll',
+    'metsvc-server.exe' => nil,
+    'metsvc.exe'        => nil
+  }
     
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
+          def react_and_close(env, body)
+        reaction = react(env)
     
-      # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
-    
-        it 'returns a flare tag if there is a flare tag in the list' do
-      valid_article = create(:article, tags: 'ama')
-      expect(described_class.new(valid_article).tag.name).to eq('ama')
+      it 'should allow changing report only' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::ContentSecurityPolicy, :report_uri => '/my_amazing_csp_report_parser', :report_only => true
+      run DummyApp
     end
     
-          it 'renders articles of long length without breaking' do
-        # This is a pretty weak test, just to exercise different lengths with no breakage
-        article.update(title: (0...75).map { rand(65..90).chr }.join)
-        get article.path
-        article.update(title: (0...100).map { rand(65..90).chr }.join)
-        get article.path
-        article.update(title: (0...118).map { rand(65..90).chr }.join)
-        get article.path
-        expect(response.body).to include 'title'
+      it_behaves_like 'any rack application'
+    
+    have_cpanm = program_exists?('cpanm')
+if !have_cpanm
+  Cabin::Channel.get('rspec') \
+    .warn('Skipping CPAN#input tests because 'cpanm' isn't in your PATH')
+end
+    
+        return @architecture
+  end # def architecture
+    
+    end
+    
+                  #
+              # If the message has been hard failed, check to see how many other recent hard fails we've had for the address
+              # and if there are more than 2, suppress the address for 30 days.
+              #
+              if result.type == 'HardFail'
+                recent_hard_fails = queued_message.server.message_db.select(:messages, :where => {:rcpt_to => queued_message.message.rcpt_to, :status => 'HardFail', :timestamp => {:greater_than => 24.hours.ago.to_f}}, :count => true)
+                if recent_hard_fails >= 1
+                  if queued_message.server.message_db.suppression_list.add(:recipient, queued_message.message.rcpt_to, :reason => 'too many hard fails')
+                    log '#{log_prefix} Added #{queued_message.message.rcpt_to} to suppression list because #{recent_hard_fails} hard fails in 24 hours'
+                    result.details += '.' if result.details =~ /\.\z/
+                    result.details += ' Recipient added to suppression list (too many hard fails).'
+                  end
+                end
+              end
+    
+      expansion(:status) {
+    {
+      :status => o.status,
+      :last_delivery_attempt => o.last_delivery_attempt ? o.last_delivery_attempt.to_f : nil,
+      :held => o.held == 1 ? true : false,
+      :hold_expiry => o.hold_expiry ? o.hold_expiry.to_f : nil
+    }
+  }
+    
+      def flash_now(type, message, options = {})
+    respond_to do |wants|
+      wants.html do
+        flash.now[type] = message
+        if options[:render_action]
+          render options[:render_action]
+        end
       end
+      wants.json { render :json => {:flash => {type => message}} }
     end
-    
-      it 'renders a user index if there is a user with the username successfully' do
-    expect(get: '/#{user.username}').to route_to(
-      controller: 'stories',
-      action: 'index',
-      username: user.username,
-    )
   end
+    
+      included do
+    helper_method :organization
+    before_action :add_organization_to_page_title
+  end
+    
+    end
