cb06f10b182f19a7324712a9dbf4be0a4d2d3b3f
SUNsung <weatherius@gmail.com>
2019-09-12 15:01:54 +0300

    Rewrite [43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py] File in: 2019-09-12 15:01:54

--- pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
+++ pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
@@ -1,232 +1,148 @@
 
         
-        
-class CBSNewsLiveVideoIE(InfoExtractor):
-    IE_NAME = 'cbsnews:livevideo'
-    IE_DESC = 'CBS News Live Videos'
-    _VALID_URL = r'https?://(?:www\.)?cbsnews\.com/live/video/(?P<id>[^/?#]+)'
+                url = ep_url.format(self.vid)
+        meta = json.loads(get_content(url))
+        streams = meta['playlist']
+        for stream in streams:
+            definition = stream['code']
+            for s in stream['playinfo']:
+                codec = 'h' + s['codec'][2:]
+                # h.264 -> h264
+                for st in self.__class__.stream_types:
+                    s_id = '{}_{}'.format(definition, codec)
+                    if codec == 'h264':
+                        s_id = definition
+                    if s_id == st['id']:
+                        clear_info = self.__class__.dec_playinfo(s, self.__class__.coeff)
+                        cdn_list = self.__class__.get_cdninfo(clear_info['hashid'])
+                        base_url = cdn_list[0]
+                        vf = urllib.parse.quote(s['vf'])
+                        video_size = int(s['filesize'])
+                        token = urllib.parse.quote(base64.b64encode(clear_info['token'].encode('utf8')))
+                        video_url = '{}?token={}&vf={}'.format(base_url, token, vf)
+                        self.streams[s_id] = dict(size=video_size, src=[video_url], container='mp4')
+    
+        if title is None:
+      title = url[0]
+    
+    class Imgur(VideoExtractor):
+    name = 'Imgur'
+    
+            self.assertEqual(result['Message'], {'message': 'abc'})
+    
+        # with a probability of 1, always throw errors
+    config.KINESIS_ERROR_PROBABILITY = 1.0
+    test_all_errors = kinesis.put_records(StreamName=TEST_STREAM_NAME, Records=records)
+    assert_equal(test_all_errors['FailedRecordCount'], 1)
+    
+        for (disposition, _) in _iter_multipart_parts(data_bytes, boundary):
+        if disposition.get('name') == 'file' and 'filename' in disposition:
+            filename = disposition['filename']
+            break
     
     
-def openssl_encode(algo, key, iv):
-    cmd = ['openssl', 'enc', '-e', '-' + algo, '-K', hex_str(key), '-iv', hex_str(iv)]
-    prog = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, _ = prog.communicate(secret_msg)
-    return out
+class TestIAMIntegrations(unittest.TestCase):
     
-    with open('update/LATEST_VERSION', 'w') as f:
-    f.write(version)
+            # publish message to SNS, receive it from SQS, assert that messages are equal
+        message = u'ö§a1'_!?,. £$-'
+        self.sns_client.publish(TopicArn=self.topic_arn, Message=message)
+        msgs = self.sqs_client.receive_message(QueueUrl=self.queue_url)
+        msg_received = msgs['Messages'][0]
+        msg_received = json.loads(to_str(msg_received['Body']))
+        msg_received = msg_received['Message']
+        self.assertEqual(message, msg_received)
     
-    print('Enter the PKCS1 private key, followed by a blank line:')
-privkey = b''
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    if line == '':
-        break
-    privkey += line.encode('ascii') + b'\n'
-privkey = rsa.PrivateKey.load_pkcs1(privkey)
+        def test_put_rule(self):
+        self.events_client.put_rule(Name='test_rule', EventPattern=json.dumps(TEST_EVENT_PATTERN))
+        rules = self.events_client.list_rules(NamePrefix='test_rule')['Rules']
     
-    youtube\-dl \- download videos from youtube.com or other video platforms
+    def config_cython():
+    '''Try to configure cython and return cython configuration'''
+    if not with_cython:
+        return []
+    # pylint: disable=unreachable
+    if os.name == 'nt':
+        print('WARNING: Cython is not supported on Windows, will compile without cython module')
+        return []
     
-        flags = [opt.get_opt_string() for opt in opts]
-    
-    
-class ActionForm(forms.Form):
-    action = forms.ChoiceField(label=_('Action:'))
-    select_across = forms.BooleanField(
-        label='',
-        required=False,
-        initial=0,
-        widget=forms.HiddenInput({'class': 'select-across'}),
-    )
+    # Horovod: pin context to local rank
+context = mx.cpu(hvd.local_rank()) if args.no_cuda else mx.gpu(hvd.local_rank())
+num_workers = hvd.size()
     
     
-class NonAutoPKBookChildTabularInline(admin.TabularInline):
-    model = NonAutoPKBookChild
-    classes = ('collapse',)
-    
-    from django.core.exceptions import ImproperlyConfigured
-from django.db import ProgrammingError
-    
-              # We can also increment through all of the fields
-          #  attached to this feature.
-          for field in feature:
-              # Get the name of the field (e.g. 'description')
-              nm = field.name
+if num_gpus > 8 :
+    print('The machine has {} gpus. We will run the test on 8 gpus.'.format(num_gpus))
+    print('There is a limit for all PCI-E hardware on creating number of P2P peers. The limit is 8.')
+    num_gpus = 8;
     
     
-class OFTInteger64List(Field):
-    pass
+def main():
+    '''Entrypoint for test_converter'''
+    parser = argparse.ArgumentParser(description='Test Caffe converter')
+    parser.add_argument('--cpu', action='store_true', help='use cpu?')
+    parser.add_argument('--image_url', type=str,
+                        default='https://github.com/dmlc/web-data/raw/master/mxnet/doc/'\
+                                'tutorials/python/predict_image/cat.jpg',
+                        help='input image to test inference, can be either file path or url')
+    args = parser.parse_args()
+    if args.cpu:
+        gpus = [-1]
+        default_batch_size = 32
+    else:
+        num_gpus = mx.context.num_gpus()
+        assert num_gpus, 'At least one GPU is needed to run test_converter in GPU mode'
+        default_batch_size = 32 * num_gpus
     
-        @property
-    def convex_hull(self):
-        '''
-        Return the smallest convex Polygon that contains all the points
-        in the Geometry.
-        '''
-        return self._topology(capi.geos_convexhull(self.ptr))
     
-        def test_mark_safe_decorator_does_not_affect_dunder_html(self):
-        '''
-        mark_safe doesn't affect a callable that has an __html__() method.
-        '''
-        class SafeStringContainer:
-            def __html__(self):
-                return '<html></html>'
+def check_layer_bidirectional_varseqlen(size, in_size):
+    weights = {}
+    for d in ['l', 'r']:
+        weights['lstm_{}0_i2h_weight'.format(d)] = mx.random.uniform(shape=(size*4, in_size))
+        weights['lstm_{}0_h2h_weight'.format(d)] = mx.random.uniform(shape=(size*4, size))
+        weights['lstm_{}0_i2h_bias'.format(d)] = mx.random.uniform(shape=(size*4,))
+        weights['lstm_{}0_h2h_bias'.format(d)] = mx.random.uniform(shape=(size*4,))
     
-        # Loading and vectorizing the data:
-    print('====== %s ======' % dat)
-    print('--- Fetching data...')
-    if dat in ['http', 'smtp', 'SF', 'SA']:
-        dataset = fetch_kddcup99(subset=dat, shuffle=True,
-                                 percent10=True, random_state=random_state)
-        X = dataset.data
-        y = dataset.target
+        def test_crout_matrix_decomposition(self):
+        self.assertEqual(([[9.0, 0.0], [7.0, 0.0]],
+                          [[1.0, 1.0], [0.0, 1.0]]),
+                         crout_matrix_decomposition.crout_matrix_decomposition(
+                             [[9, 9], [7, 7]]))
     
-    Show below is a logistic-regression classifiers decision boundaries on the
-first two dimensions (sepal length and width) of the `iris
-<https://en.wikipedia.org/wiki/Iris_flower_data_set>`_ dataset. The datapoints
-are colored according to their labels.
+            nested_list = [2, 1, [3, [4, 5], 6], 7, [8]]
+        flattened = flatten_iter(nested_list)
+        self.assertEqual(next(flattened), 2)
+        self.assertEqual(next(flattened), 1)
+        self.assertEqual(next(flattened), 3)
+        self.assertEqual(next(flattened), 4)
+        self.assertEqual(next(flattened), 5)
+        self.assertEqual(next(flattened), 6)
+        self.assertEqual(next(flattened), 7)
+        self.assertEqual(next(flattened), 8)
+        self.assertRaises(StopIteration, next, flattened)
     
-    # Turn up tolerance for faster convergence
-clf = LogisticRegression(
-    C=50. / train_samples, penalty='l1', solver='saga', tol=0.1
-)
-clf.fit(X_train, y_train)
-sparsity = np.mean(clf.coef_ == 0) * 100
-score = clf.score(X_test, y_test)
-# print('Best C % .4f' % clf.C_)
-print('Sparsity with L1 penalty: %.2f%%' % sparsity)
-print('Test score with L1 penalty: %.4f' % score)
+                i = int.from_bytes(byte, 'big')
+            self.buffer.extend(list('{0:08b}'.format(i)))
     
-        Parameters
-    ----------
-    X : CSR or CSC sparse matrix, shape=(n_samples, n_features)
-        Matrix whose two columns are to be swapped.
+                    # case 1: node's red, can not get black node
+                # set brother is black and parent is red 
+                if node_brother.color == 1:
+                    node_brother.color = 0
+                    node.parent.color = 1
+                    self.left_rotate(node.parent)
+                    node_brother = node.parent.right
     
-        n = 10
-    step = 10000
-    n_samples = 10000
-    dim = 10
-    n_classes = 10
-    for i in range(n):
-        print('============================================')
-        print('Entering iteration %s of %s' % (i, n))
-        print('============================================')
-        n_samples += step
-        X = np.random.randn(n_samples, dim)
-        Y = np.random.randint(0, n_classes, (n_samples,))
-        bench_scikit_tree_classifier(X, Y)
-        Y = np.random.randn(n_samples)
-        bench_scikit_tree_regressor(X, Y)
+        def testDiscoverRequest(self, announcer, announcer_remote):
+        assert len(announcer_remote.known_peers) == 0
+        with Spy.Spy(announcer_remote, 'handleMessage') as responses:
+            announcer_remote.broadcast({'cmd': 'discoverRequest', 'params': {}}, port=announcer.listen_port)
+            time.sleep(0.1)
     
-        # TASK: print the mean and std for each candidate along with the parameter
-    # settings for all the candidates explored by grid search.
-    n_candidates = len(grid_search.cv_results_['params'])
-    for i in range(n_candidates):
-        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
-                 % (grid_search.cv_results_['params'][i],
-                    grid_search.cv_results_['mean_test_score'][i],
-                    grid_search.cv_results_['std_test_score'][i]))
+            # Remove include
+        filter_storage.includeRemove(site.address, 'filters.json')
     
-        if f == os.path.basename(__file__):
-        continue
-    
-    Second, when using a connectivity matrix, single, average and complete
-linkage are unstable and tend to create a few clusters that grow very
-quickly. Indeed, average and complete linkage fight this percolation behavior
-by considering all the distances between two clusters when merging them (
-while single linkage exaggerates the behaviour by considering only the
-shortest distance between clusters). The connectivity graph breaks this
-mechanism for average and complete linkage, making them resemble the more
-brittle single linkage. This effect is more pronounced for very sparse graphs
-(try decreasing the number of neighbors in kneighbors_graph) and with
-complete linkage. In particular, having a very small number of neighbors in
-the graph, imposes a geometry that is close to that of single linkage,
-which is well known to have this percolation instability. '''
-# Authors: Gael Varoquaux, Nelle Varoquaux
-# License: BSD 3 clause
-    
-    digits = datasets.load_digits()
-images = digits.images
-X = np.reshape(images, (len(images), -1))
-connectivity = grid_to_graph(*images[0].shape)
-    
-        verifyThirdPartyFile(url, recipe['checksum'], sourceArchive)
-    print('Extracting archive for %s'%(name,))
-    buildDir=os.path.join(WORKDIR, '_bld')
-    if not os.path.exists(buildDir):
-        os.mkdir(buildDir)
-    
-            expected = {9: 1, 18: 2, 19: 2, 27: 3, 28: 3, 29: 3, 36: 4, 37: 4,
-                    38: 4, 39: 4, 45: 5, 46: 5, 47: 5, 48: 5, 49: 5, 54: 6,
-                    55: 6, 56: 6, 57: 6, 58: 6, 59: 6, 63: 7, 64: 7, 65: 7,
-                    66: 7, 67: 7, 68: 7, 69: 7, 72: 8, 73: 8, 74: 8, 75: 8,
-                    76: 8, 77: 8, 78: 8, 79: 8, 81: 9, 82: 9, 83: 9, 84: 9,
-                    85: 9, 86: 9, 87: 9, 88: 9, 89: 9}
-        actual = {k: v for v in range(10) for k in range(v * 9, v * 10)}
-        self.assertEqual(k, 'Local Variable')
-        self.assertEqual(actual, expected)
-    
-    class NNTPPermanentError(NNTPError):
-    '''5xx errors'''
-    pass
-    
-    __all__ = ['Empty', 'Full', 'Queue', 'PriorityQueue', 'LifoQueue', 'SimpleQueue']
-    
-        def test_unreadable(self):
-        class UnReadable(self.BytesIO):
-            def readable(self):
-                return False
-        txt = self.TextIOWrapper(UnReadable())
-        self.assertRaises(OSError, txt.read)
-    
-        def CheckInTransaction(self):
-        # Can't use db from setUp because we want to test initial state.
-        cx = sqlite.connect(':memory:')
-        cu = cx.cursor()
-        self.assertEqual(cx.in_transaction, False)
-        cu.execute('create table transactiontest(id integer primary key, name text)')
-        self.assertEqual(cx.in_transaction, False)
-        cu.execute('insert into transactiontest(name) values (?)', ('foo',))
-        self.assertEqual(cx.in_transaction, True)
-        cu.execute('select name from transactiontest where name=?', ['foo'])
-        row = cu.fetchone()
-        self.assertEqual(cx.in_transaction, True)
-        cx.commit()
-        self.assertEqual(cx.in_transaction, False)
-        cu.execute('select name from transactiontest where name=?', ['foo'])
-        row = cu.fetchone()
-        self.assertEqual(cx.in_transaction, False)
-    
-    if sys.platform[:3] == 'win':
-    class WindowsDefault(BaseBrowser):
-        def open(self, url, new=0, autoraise=True):
-            sys.audit('webbrowser.open', url)
-            try:
-                os.startfile(url)
-            except OSError:
-                # [Error 22] No application is associated with the specified
-                # file for this operation: '<URL>'
-                return False
-            else:
-                return True
-    
-        # Clear ABC registries, restoring previously saved ABC registries.
-    abs_classes = [getattr(collections.abc, a) for a in collections.abc.__all__]
-    abs_classes = filter(isabstract, abs_classes)
-    for abc in abs_classes:
-        for obj in abc.__subclasses__() + [abc]:
-            for ref in abcs.get(obj, set()):
-                if ref() is not None:
-                    obj.register(ref())
-            obj._abc_caches_clear()
-    
-        def try_open_calltip_event(self, event):
-        '''Happens when it would be nice to open a calltip, but not really
-        necessary, for example after an opening bracket, so function calls
-        won't be made.
-        '''
-        self.open_calltip(False)
+        def list(self, inner_path, *args, **kwags):
+        if '.zip' in inner_path or '.tar.gz' in inner_path:
+            match = re.match('^(.*\.(?:tar.gz|tar.bz2|zip))(.*)', inner_path)
+            archive_inner_path, path_within = match.groups()
+            archive = self.openArchive(archive_inner_path)
+            path_within = path_within.lstrip('/')
