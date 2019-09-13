09ea9fc9ab56d0a0d3cb82a3431d80c1d01e6c6e
SUNsung <weatherius@gmail.com>
2019-09-13 21:10:01 +0300

    Rewrite [29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb] File in: 2019-09-13 21:10:01

--- ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
+++ ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
@@ -1,164 +1,111 @@
 
         
-                xcpretty = 'xcpretty #{Snapshot.config[:xcpretty_args]}'
-        xcpretty << '--no-color' if Helper.colors_disabled?
-    
-            options = []
-        options += project_path_array
-        options << '-sdk '#{config[:sdk]}'' if config[:sdk]
-        options << '-derivedDataPath '#{derived_data_path}''
-        options << '-resultBundlePath '#{result_bundle_path}'' if result_bundle_path
-        options << config[:xcargs] if config[:xcargs]
-        return options
+              def find_matching_tag(tag)
+        # Used primarily by developers testing beta macOS releases.
+        if OS::Mac.prerelease? && ARGV.skip_or_later_bottles?
+          generic_find_matching_tag(tag)
+        else
+          generic_find_matching_tag(tag) ||
+            find_older_compatible_tag(tag)
+        end
       end
     
-          def self.available_options
-        user = CredentialsManager::AppfileConfig.try_fetch_value(:apple_dev_portal_id)
-        user ||= CredentialsManager::AppfileConfig.try_fetch_value(:apple_id)
+                problem '\'inreplace <filenames> do |s|\' is preferred over \'|#{block_arg.source}|\'.'
+          end
     
-        it 'should fail to create a nil device UDID' do
-      expect do
-        Spaceship::Device.create!(name: 'Demo Device', udid: nil)
-      end.to raise_error('You cannot create a device without a device_id (UDID) and name')
-    end
+          def detect_version_from_clang_version
+        return 'dunno' if DevelopmentTools.clang_version.null?
     
-            expect_runner_run_with(expected_options)
-    
-          def self.category
-        :project
+            subject.fetch
       end
     end
   end
+    
+        def metadata_subdir(leaf, version: self.version, timestamp: :latest, create: false)
+      raise CaskError, 'Cannot create metadata subdir when timestamp is :latest.' if create && timestamp == :latest
+    
+        # TODO stop escaping - HTML5 allows anything but space
+    assert_ref @c2_c3_h1_meh, 'C2::C3::H1#m?'
+    
+      def test_definition
+    m = RDoc::NormalModule.new 'M'
+    
+    class C2
+  def b
+  end
+    
+      def test_vi_paste_prev_for_mbchar_by_plural_code_points
+    input_keys('か\u3099き\u3099く\u3099け\u3099こ\u3099\C-[3h')
+    assert_line('か\u3099き\u3099く\u3099け\u3099こ\u3099')
+    assert_byte_pointer_size('か\u3099')
+    assert_cursor(2)
+    assert_cursor_max(10)
+    input_keys('P')
+    assert_line('か\u3099き\u3099く\u3099け\u3099こ\u3099')
+    assert_byte_pointer_size('か\u3099')
+    assert_cursor(2)
+    assert_cursor_max(10)
+    input_keys('d$')
+    assert_line('か\u3099')
+    assert_byte_pointer_size('')
+    assert_cursor(0)
+    assert_cursor_max(2)
+    input_keys('P')
+    assert_line('き\u3099く\u3099け\u3099こ\u3099か\u3099')
+    assert_byte_pointer_size('き\u3099く\u3099け\u3099')
+    assert_cursor(6)
+    assert_cursor_max(10)
+    input_keys('2P')
+    assert_line('き\u3099く\u3099け\u3099き\u3099く\u3099け\u3099き\u3099く\u3099け\u3099こ\u3099こ\u3099こ\u3099か\u3099')
+    assert_byte_pointer_size('き\u3099く\u3099け\u3099き\u3099く\u3099け\u3099き\u3099く\u3099け\u3099')
+    assert_cursor(18)
+    assert_cursor_max(26)
+  end
+    
+      def test_append_three_with_max_two
+    @kill_ring = Reline::KillRing.new(2)
+    assert_equal(Reline::KillRing::State::FRESH, @kill_ring.instance_variable_get(:@state))
+    @kill_ring.append('a')
+    assert_equal(Reline::KillRing::State::CONTINUED, @kill_ring.instance_variable_get(:@state))
+    @kill_ring.process
+    assert_equal(Reline::KillRing::State::PROCESSED, @kill_ring.instance_variable_get(:@state))
+    @kill_ring.process
+    assert_equal(Reline::KillRing::State::FRESH, @kill_ring.instance_variable_get(:@state))
+    @kill_ring.append('b')
+    assert_equal(Reline::KillRing::State::CONTINUED, @kill_ring.instance_variable_get(:@state))
+    @kill_ring.process
+    assert_equal(Reline::KillRing::State::PROCESSED, @kill_ring.instance_variable_get(:@state))
+    @kill_ring.process
+    assert_equal(Reline::KillRing::State::FRESH, @kill_ring.instance_variable_get(:@state))
+    @kill_ring.append('c')
+    assert_equal(Reline::KillRing::State::CONTINUED, @kill_ring.instance_variable_get(:@state))
+    @kill_ring.process
+    assert_equal(Reline::KillRing::State::PROCESSED, @kill_ring.instance_variable_get(:@state))
+    assert_equal('c', @kill_ring.yank)
+    assert_equal(Reline::KillRing::State::YANK, @kill_ring.instance_variable_get(:@state))
+    assert_equal('c', @kill_ring.yank)
+    assert_equal(Reline::KillRing::State::YANK, @kill_ring.instance_variable_get(:@state))
+    assert_equal(['b', 'c'], @kill_ring.yank_pop)
+    assert_equal(Reline::KillRing::State::YANK, @kill_ring.instance_variable_get(:@state))
+    assert_equal(['c', 'b'], @kill_ring.yank_pop)
+    assert_equal(Reline::KillRing::State::YANK, @kill_ring.instance_variable_get(:@state))
+    assert_equal(['b', 'c'], @kill_ring.yank_pop)
+    assert_equal(Reline::KillRing::State::YANK, @kill_ring.instance_variable_get(:@state))
+  end
+    
+    ::Bundler.with_friendly_errors do
+  ::Bundler::CLI.start(ARGV, :debug => true)
 end
 
     
-        it 'does not permit path traversal requests' do
-      expect { finder.find('../foo') }.to raise_error(/Invalid path/)
-    end
-  end
+    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
+Gem.use_paths(LogStash::Environment.logstash_gem_home)
+    
+    class LogStash::PluginManager::Update < LogStash::PluginManager::Command
+  REJECTED_OPTIONS = [:path, :git, :github]
+  # These are local gems used by LS and needs to be filtered out of other plugin gems
+  NON_PLUGIN_LOCAL_GEMS = ['logstash-core', 'logstash-core-plugin-api']
+    
+    RSpec.configure do |c|
+  c.include ServiceTester
 end
-
-    
-          expect(result).to contain_exactly('files/html/500.html')
-    end
-  end
-end
-
-    
-      private
-    
-        def find(query)
-      query = Gitlab::Search::Query.new(query, encode_binary: true) do
-        filter :filename, matcher: ->(filter, blob) { blob.binary_filename =~ /#{filter[:regex_value]}$/i }
-        filter :path, matcher: ->(filter, blob) { blob.binary_filename =~ /#{filter[:regex_value]}/i }
-        filter :extension, matcher: ->(filter, blob) { blob.binary_filename =~ /\.#{filter[:regex_value]}$/i }
-      end
-    
-      def output_chronic_duration_attribute(source_attribute)
-    value = attributes[source_attribute.to_s]
-    ChronicDuration.output(value, format: :short) if value
-  end
-end
-
-    
-      let(:iso_env) do
-    # We have to create a Vagrantfile so there is a root path
-    env = isolated_environment
-    env.vagrantfile('')
-    env.create_vagrant_env
-  end
-    
-      # Automatically install locally defined plugins instead of
-  # waiting for user confirmation.
-  #
-  # @return [Boolean]
-  def self.auto_install_local_plugins?
-    if ENV['VAGRANT_INSTALL_LOCAL_PLUGINS']
-      true
-    else
-      false
-    end
-  end
-    
-      def mon_check_owner
-    if @mon_owner != Thread.current
-      raise ThreadError, 'current thread not owner'
-    end
-  end
-    
-        @co.comment = input
-    
-      def test_push__plural
-    assert_equal(Readline::HISTORY, Readline::HISTORY.push('0', '1', '2', '3', '4'))
-    (0..4).each do |i|
-      assert_external_string_equal(i.to_s, Readline::HISTORY[i])
-    end
-    assert_equal(5, Readline::HISTORY.length)
-    
-    class Reline::MacroTest < Reline::TestCase
-  def setup
-    @config = Reline::Config.new
-    @line_editor = Reline::LineEditor.new(@config)
-    @line_editor.instance_variable_set(:@screen_size, [24, 80])
-    @output = @line_editor.output = File.open(IO::NULL, 'w')
-  end
-    
-      def test_macro_commands_for_editing
-    @config.add_default_key_binding('\C-x\C-d'.bytes, :delete_char)
-    @config.add_default_key_binding('\C-x\C-h'.bytes, :backward_delete_char)
-    @config.add_default_key_binding('\C-x\C-v'.bytes, :quoted_insert)
-    #@config.add_default_key_binding('\C-xa'.bytes, :self_insert)
-    @config.add_default_key_binding('\C-x\C-t'.bytes, :transpose_chars)
-    @config.add_default_key_binding('\C-x\M-t'.bytes, :transpose_words)
-    @config.add_default_key_binding('\C-x\M-u'.bytes, :upcase_word)
-    @config.add_default_key_binding('\C-x\M-l'.bytes, :downcase_word)
-    @config.add_default_key_binding('\C-x\M-c'.bytes, :capitalize_word)
-    @writer.write('abcde\C-b\C-b\C-b\C-x\C-d\C-x\C-h\C-x\C-v\C-a\C-f\C-f EF\C-x\C-t gh\C-x\M-t\C-b\C-b\C-b\C-b\C-b\C-b\C-b\C-b\C-x\M-u\C-x\M-l\C-x\M-c\n')
-    assert_equal 'a\C-aDE gh Fe', Reline.readmultiline(&proc{ true })
-  end
-end
-
-    
-    module Bourbon
-  class Generator < Thor
-    map ['-v', '--version'] => :version
-    
-      context 'called with two colors' do
-    it 'applies to alternating sides' do
-      rule = 'border-color: #0f0 #00f'
-    
-      context 'called with three widths' do
-    it 'applies second width to left and right' do
-      rule = 'border-width: 4px 5px 6px'
-    
-          expect('.all-buttons-active').to have_ruleset(ruleset)
-    end
-  end
-    
-          expect('.margin-explicit').to have_rule(rule)
-    end
-  end
-    
-      context 'called with two sizes' do
-    it 'applies to height and width' do
-      rule = 'height: 2em; width: 1em;'
-    
-        @inputs_list = %w(
-      [type='color']
-      [type='date']
-      [type='datetime']
-      [type='datetime-local']
-      [type='email']
-      [type='month']
-      [type='number']
-      [type='password']
-      [type='search']
-      [type='tel']
-      [type='text']
-      [type='time']
-      [type='url']
-      [type='week']
-      input:not([type])
-      textarea
-    )
-  end
