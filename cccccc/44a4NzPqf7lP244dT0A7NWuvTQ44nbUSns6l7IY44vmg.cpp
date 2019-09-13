22c9f6f9909fbd790bc2202b7966c334cd137a9d
SUNsung <weatherius@gmail.com>
2019-09-13 17:49:43 +0300

    Rewrite [44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp] File in: 2019-09-13 17:49:43

--- cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
+++ cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
@@ -1,210 +1,173 @@
 
         
-        // Generate destructors.
-#include 'ipc/struct_destructor_macros.h'
-#include 'content/nw/src/common/common_message_generator.h'
+            // Check if the method implementation is the same in a super class, i.e.
+    // it is not overridden in the derived class.
+    auto *Impl1 = MethodCl->findImplementingMethod(FD);
+    assert(Impl1);
+    auto *Impl2 = ImplCl->findImplementingMethod(FD);
+    assert(Impl2);
     
-    
-    {  DISALLOW_COPY_AND_ASSIGN(MenuDelegate);
-};
-    
-    void MenuItem::SetSubmenu(Menu* sub_menu) {
-  submenu_ = sub_menu;
-  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
-    sub_menu->UpdateKeys(gtk_accel_group);
+      // Next we get the symbol name that we are going to use in our backtrace.
+  std::string symbolName;
+  // We initialize symbolAddr to framePC so that if we succeed in finding the
+  // symbol, we get the offset in the function and if we fail to find the symbol
+  // we just get HexAddr + 0.
+  uintptr_t symbolAddr = uintptr_t(framePC);
+  bool foundSymbol =
+      getSymbolNameAddr(libraryName, syminfo, symbolName, symbolAddr);
+  ptrdiff_t offset = 0;
+  if (foundSymbol) {
+    offset = ptrdiff_t(uintptr_t(framePC) - symbolAddr);
+  } else {
+    offset = ptrdiff_t(uintptr_t(framePC) - uintptr_t(syminfo.baseAddress));
+    symbolAddr = uintptr_t(framePC);
+    symbolName = '<unavailable>';
   }
-  if (sub_menu == NULL)
-    gtk_menu_item_remove_submenu(GTK_MENU_ITEM(menu_item_));
-  else
-    gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item_), sub_menu->menu_);
-}
     
-    #if defined(OS_WIN) || defined(OS_LINUX)
-bool MenuItem::AcceleratorPressed(const ui::Accelerator& accelerator) {
-#if defined(OS_WIN)
-  if (meta_down_flag_) {
-    if ((::GetKeyState(VK_APPS) & 0x8000) != 0x8000) {
-      return true;
-    }
-  }
-#endif
-  OnClick();
-  return true;
-}
-    
-        bool Write(ClipboardData& data) {
-      switch(data.type) {
-        case TYPE_TEXT:
-        return WriteText(data);
-        break;
-        case TYPE_HTML:
-        return WriteHTML(data);
-        break;
-        case TYPE_RTF:
-        return WriteRTF(data);
-        break;
-        case TYPE_PNG:
-        case TYPE_JPEG:
-        return WriteImage(data);
-        break;
-        case TYPE_NONE:
-        NOTREACHED();
-        return false;
-      }
-      NOTREACHED();
-      return false;
+    template <typename T>
+static uint64_t swift_floatingPointToString(char *Buffer, size_t BufferLength,
+                                            T Value, const char *Format, 
+                                            bool Debug) {
+  if (BufferLength < 32)
+    swift::crash('swift_floatingPointToString: insufficient buffer size');
     }
     
-    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
- public:
-  NwMenuGetNSStringFWithFixupFunction() {}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
+      /// Queue of delayed SILFunctions that need to be forced.
+  std::deque<std::pair<SILDeclRef, DelayedFunction>> forcedFunctions;
     
- protected:
-  ~NwMenuGetNSStringFWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
-};
-    
-     protected:
-  ~NwObjDestroyFunction() override;
-    
-      // implement nw.Screen.stopMonitor()
-  class NwScreenStopMonitorFunction : public NWSyncExtensionFunction {
-  public:
-    NwScreenStopMonitorFunction();
-    bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-      // Array holding scores for each orientation id [0,3].
-  // Orientation ids [0..3] map to [0, 270, 180, 90] degree orientations of the
-  // page respectively, where the values refer to the amount of clockwise
-  // rotation to be applied to the page for the text to be upright and readable.
-  float orientations[4];
-  // Script confidence scores for each of 4 possible orientations.
-  float scripts_na[4][kMaxNumberOfScripts];
-    
-      // ValidFirstLine() and ValidBodyLine() take arguments describing a text line
-  // in a block of text which we are trying to model:
-  //   lmargin, lindent:  these add up to the distance from the leftmost ink
-  //                      in the text line to the surrounding text block's left
-  //                      edge.
-  //   rmargin, rindent:  these add up to the distance from the rightmost ink
-  //                      in the text line to the surrounding text block's right
-  //                      edge.
-  // The caller determines the division between 'margin' and 'indent', which
-  // only actually affect whether we think the line may be centered.
+      // Fixed-size multi-payload enums have a special descriptor format that
+  // encodes spare bits.
   //
-  // If the amount of whitespace matches the amount of whitespace expected on
-  // the relevant side of the line (within tolerance_) we say it matches.
+  // FIXME: Actually implement this. For now, a descriptor with this kind
+  // just means we also have a builtin descriptor from which we get the
+  // size and alignment.
+  MultiPayloadEnum,
     
-     private:
-  // A hole in the heap exists at hole_index, and we want to fill it with the
-  // given pair. SiftUp sifts the hole upward to the correct position and
-  // returns the destination index without actually putting pair there.
-  int SiftUp(int hole_index, const Pair& pair) {
-    int parent;
-    while (hole_index > 0 && pair < heap_[parent = ParentNode(hole_index)]) {
-      heap_[hole_index] = heap_[parent];
-      hole_index = parent;
-    }
-    return hole_index;
+    
+    {    return { this->template getTrailingObjects<SourceLoc>(),
+             asDerived().getNumArguments() };
   }
     
-    // Class to encapsulate all the functionality and sub-structures required
-// to count errors for an isolated character classifier (ShapeClassifier).
-class ErrorCounter {
- public:
-  // Computes and returns the unweighted boosting_mode error rate of the given
-  // classifier. Can be used for testing, or inside an iterative training
-  // system, including one that uses boosting.
-  // report_levels:
-  // 0 = no output.
-  // 1 = bottom-line error rate.
-  // 2 = bottom-line error rate + time.
-  // 3 = font-level error rate + time.
-  // 4 = list of all errors + short classifier debug output on 16 errors.
-  // 5 = list of all errors + short classifier debug output on 25 errors.
-  // * The boosting_mode determines which error type is used for computing the
-  //   scaled_error output, and setting the is_error flag in the samples.
-  // * The fontinfo_table is used to get string font names for the debug
-  //   output, and also to count font attributes errors.
-  // * The page_images vector may contain a Pix* (which may be nullptr) for each
-  //   page index assigned to the samples.
-  // * The it provides encapsulated iteration over some sample set.
-  // * The outputs unichar_error, scaled_error and totals_report are all
-  //   optional.
-  // * If not nullptr, unichar error gets the top1 unichar error rate.
-  // * Scaled_error gets the error chosen by boosting_mode weighted by the
-  //   weights on the samples.
-  // * Fonts_report gets a string summarizing the error rates for each font in
-  //   both human-readable form and as a tab-separated list of error counts.
-  //   The human-readable form is all before the first tab.
-  // * The return value is the un-weighted version of the scaled_error.
-  static double ComputeErrorRate(ShapeClassifier* classifier,
-                                 int report_level, CountTypes boosting_mode,
-                                 const FontInfoTable& fontinfo_table,
-                                 const GenericVector<Pix*>& page_images,
-                                 SampleIterator* it,
-                                 double* unichar_error,
-                                 double* scaled_error,
-                                 STRING* fonts_report);
-  // Tests a pair of classifiers, debugging errors of the new against the old.
-  // See errorcounter.h for description of arguments.
-  // Iterates over the samples, calling the classifiers in normal/silent mode.
-  // If the new_classifier makes a boosting_mode error that the old_classifier
-  // does not, and the appropriate, it will then call the new_classifier again
-  // with a debug flag and a keep_this argument to find out what is going on.
-  static void DebugNewErrors(ShapeClassifier* new_classifier,
-                             ShapeClassifier* old_classifier,
-                             CountTypes boosting_mode,
-                             const FontInfoTable& fontinfo_table,
-                             const GenericVector<Pix*>& page_images,
-                             SampleIterator* it);
+    
+template <>
+const char *
+FloatAttribute::staticTypeName ()
+{
+    return 'float';
+}
+    
+    class Header;
+    
+    
+IStream::IStream (const char fileName[]): _fileName (fileName)
+{
+    // empty
+}
+    
+    		    _data->cachedBuffer->insert
+			(k.name(),
+			 Slice (HALF,
+				(char *)(new half[tileRowSize] - 
+					_data->offset),
+				sizeof (half),
+				sizeof (half) *
+				    _data->tFile->levelWidth(0),
+				1, 1,
+				s.fillValue,
+				false, true));
+		    break;
+    
+    
+    {} // namespace nwapi
+
+    
+    class BaseEvent {
+  friend class EventListener;
+  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
     }
     
-    // Setup the map for the given indexed_features that have been indexed by
-// feature_map.
-void IntFeatureDist::Set(const GenericVector<int>& indexed_features,
-                          int canonical_count, bool value) {
-  total_feature_weight_ = canonical_count;
-  for (int i = 0; i < indexed_features.size(); ++i) {
-    const int f = indexed_features[i];
-    features_[f] = value;
-    for (int dir = -kNumOffsetMaps; dir <= kNumOffsetMaps; ++dir) {
-      if (dir == 0) continue;
-      const int mapped_f = feature_map_->OffsetFeature(f, dir);
-      if (mapped_f >= 0) {
-        features_delta_one_[mapped_f] = value;
-        for (int dir2 = -kNumOffsetMaps; dir2 <= kNumOffsetMaps; ++dir2) {
-          if (dir2 == 0) continue;
-          const int mapped_f2 = feature_map_->OffsetFeature(mapped_f, dir2);
-          if (mapped_f2 >= 0)
-            features_delta_two_[mapped_f2] = value;
-        }
-      }
+    
+    {}  // namespace nw
+    
+    void MenuItem::UpdateKeys(views::FocusManager *focus_manager){
+  if (focus_manager == NULL){
+    return ;
+  } else {
+    focus_manager_ = focus_manager;
+    if (enable_shortcut_){
+      focus_manager->RegisterAccelerator(
+        accelerator_,
+        ui::AcceleratorManager::kHighPriority,
+        this);
+    }
+    if (submenu_ != NULL){
+      submenu_->UpdateKeys(focus_manager);
     }
   }
 }
     
-    VideoStreamPlaybackGDNative::~VideoStreamPlaybackGDNative() {
-	cleanup();
+      protected:
+    ~NwScreenRegisterStreamFunction() override {}
+    DECLARE_EXTENSION_FUNCTION('nw.Screen.registerStream', UNKNOWN)
+    
+      std::shared_ptr<DHTNode> localNode_;
+    
+    
+    {  // Returns two vector of Commands.  First one contains regular
+  // commands.  Secod one contains so called routine commands, which
+  // executed once per event poll returns.
+  std::pair<std::vector<std::unique_ptr<Command>>,
+            std::vector<std::unique_ptr<Command>>>
+  setup(DownloadEngine* e, int family);
+};
+    
+    std::shared_ptr<DHTTask>
+DHTTaskFactoryImpl::createNodeLookupTask(const unsigned char* targetID)
+{
+  auto task = std::make_shared<DHTNodeLookupTask>(targetID);
+  setCommonProperty(task);
+  return task;
 }
     
-    	ClassDB::bind_method(D_METHOD('set_sun_angle_min', 'degrees'), &ProceduralSky::set_sun_angle_min);
-	ClassDB::bind_method(D_METHOD('get_sun_angle_min'), &ProceduralSky::get_sun_angle_min);
     
-    	EditorNavigationMeshGenerator();
-	~EditorNavigationMeshGenerator();
+    {  void updateTokenSecret();
+};
     
-    StringName _scs_create(const char *p_chr);
+    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
+                                             std::chrono::seconds interval)
+    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
+{
+}
     
-    #include <alsa/asoundlib.h>
-#include <stdio.h>
+      virtual ~DHTUnknownMessage();
     
-    		Vector<Edge> edges;
-		Ref<Material> material;
-		bool smooth;
-		bool invert;
+    ConnectionsTree::Model &ConnectionsTree::TreeItem::model() { return m_model; }
+    
+      auto errCallback = [this](const QString& k) {
+    return [this, k](const QString& err) {
+      QMutexLocker l(&m_errorsMutex);
+      m_errors.append(QCoreApplication::translate('RDM', 'Cannot copy key ') +
+                      QString('%1: %2').arg(k).arg(err));
+    };
+  };
+    
+      for (QString k : m_affectedKeys) {
+    auto future = m_connection->cmd(
+        {'EXPIRE', k.toUtf8(), ttl}, this, m_dbIndex,
+        [this](const RedisClient::Response&) {
+          QMutexLocker l(&m_processedKeysMutex);
+          m_progress++;
+          emit progress(m_progress);
+        },
+        [this, k](const QString& err) {
+          QMutexLocker l(&m_errorsMutex);
+          m_errors.append(
+              QCoreApplication::translate('RDM', 'Cannot set TTL for key ') +
+              QString('%1: %2').arg(k).arg(err));
+        });
+    }
+    
+    #include 'operations/copyoperation.h'
+#include 'operations/deleteoperation.h'
+#include 'operations/rdbimport.h'
+#include 'operations/ttloperation.h'
