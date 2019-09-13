6cb07c6aa0cddc165dacb6a71443b61576c10b47
SUNsung <weatherius@gmail.com>
2019-09-13 22:04:12 +0300

    Rewrite [56t4HHA9l6kQc56rczb2UoTZrY56zYDfnTJPaOw568Hb.rb] File in: 2019-09-13 22:04:11

--- ruby/56t4HHA9l6kQc56rczb2UoTZrY56zYDfnTJPaOw568Hb.rb
+++ ruby/56t4HHA9l6kQc56rczb2UoTZrY56zYDfnTJPaOw568Hb.rb
@@ -1,141 +1,121 @@
 
         
-            def download_url(upload)
-      not_implemented
+            def as_json
+      { name: name, path: path, type: type }
     end
-    
-        it 'should work with trailing slash url' do
-      Discourse::Utils.expects(:execute_command).with('git', 'clone', url, @temp_folder)
-    
-          result
-    end
-  end
-    
-        def file_regex
-      @file_regex ||= begin
-        path = @s3_helper.s3_bucket_folder_path || ''
-    
-      context 'S3 specific behavior' do
-    before { create_backups }
-    after(:all) { remove_backups }
-    
-    require_dependency 'reviewable_score_type_serializer'
-    
-      def process_message(message)
-    begin
-      @last_message_processed = message.global_id
-      MessageBus.on_connect.call(message.site_id)
-      refresh!
-    ensure
-      MessageBus.on_disconnect.call(message.site_id)
-    end
-  end
-    
-        context 'similar activity' do
-      let!(:hottest_topic1) { Fabricate(:topic, views: 3530, posts_count: 100, like_count: 201) }
-      let!(:hottest_topic2) { Fabricate(:topic, views: 3530, posts_count: 100, like_count: 201) }
-      let!(:warm_topic1) { Fabricate(:topic, views: 2020, posts_count: 100, like_count: 99) }
-      let!(:warm_topic2) { Fabricate(:topic, views: 2020, posts_count: 100, like_count: 99) }
-      let!(:warm_topic3) { Fabricate(:topic, views: 2020, posts_count: 100, like_count: 99) }
-      let!(:lukewarm_topic1) { Fabricate(:topic, views: 1010, posts_count: 100, like_count: 51) }
-      let!(:lukewarm_topic2) { Fabricate(:topic, views: 1010, posts_count: 100, like_count: 51) }
-      let!(:lukewarm_topic3) { Fabricate(:topic, views: 1010, posts_count: 100, like_count: 51) }
-      let!(:lukewarm_topic4) { Fabricate(:topic, views: 1010, posts_count: 100, like_count: 51) }
-      let!(:cold_topic) { Fabricate(:topic, views: 100, posts_count: 100, like_count: 1) }
-    
-        it 'an invalid rebuild statement' do
-      expect_offense(<<~RUBY)
-        class Foo < Formula
-          desc 'foo'
-          url 'https://brew.sh/foo-1.0.tgz'
-          bottle do
-            rebuild 0
-            ^^^^^^^^^ 'rebuild 0' should be removed
-            sha256 'fe0679b932dd43a87fd415b609a7fbac7a069d117642ae8ebaac46ae1fb9f0b3' => :sierra
-          end
-        end
-      RUBY
-    end
-    
-    class Bottle
-  class Filename
-    attr_reader :name, :version, :tag, :rebuild
-    
-            uses_from_macos('foo', after: :mojave)
-      end
-    
-          def detect_version
-        version = nil
-        [EXECUTABLE_PKG_ID, MAVERICKS_NEW_PKG_ID].each do |id|
-          next unless File.exist?('#{PKG_PATH}/usr/bin/clang')
-    
-      it 'properly handles Casks that are not present' do
-    expect {
-      described_class.run('notacask')
-    }.to raise_error(Cask::CaskUnavailableError)
   end
 end
 
     
-      def test_symlink_exists(path)
-    exists?('L', path)
+        def initialize
+      @pages = {}
+    end
+    
+        private
+    
+        def build_page(path)
+      response = request_one url_for(path)
+      result = handle_response(response)
+      yield result if block_given?
+      result
+    end
+    
+          private
+    
+            css('header').each do |node|
+          node.before(node.children).remove
+        end
+    
+          def expire_data_after_sign_in!
+        # session.keys will return an empty array if the session is not yet loaded.
+        # This is a bug in both Rack and Rails.
+        # A call to #empty? forces the session to be loaded.
+        session.empty?
+        session.keys.grep(/^devise\./).each { |k| session.delete(k) }
+      end
+    
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
+    end
+    
+            selected_modules.each do |m|
+          mod = Devise::Models.const_get(m.to_s.classify)
+    
+          def upload_dest
+        @upload_dest
+      end
+    
+          def title
+        @page.title
+      end
+    
+    # Silence locale validation warning
+require 'i18n'
+I18n.enforce_available_locales = false
+    
+        follow_redirect!
+    assert_equal '/G/K/C', last_request.fullpath
+    assert last_response.ok?
+    
+      test 'extract destination file name in case of path renaming' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
+    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
   end
     
-        def dry_run
-      ['--dry-run', '-n',
-       'Do a dry run without executing actions',
-       lambda do |_value|
-         Configuration.env.set(:sshkit_backend, SSHKit::Backend::Printer)
-       end]
-    end
-    
-        def setup_filters
-      @filters = cmdline_filters
-      @filters += @custom_filters if @custom_filters
-      @filters << Filter.new(:role, ENV['ROLES']) if ENV['ROLES']
-      @filters << Filter.new(:host, ENV['HOSTS']) if ENV['HOSTS']
-      fh = fetch_for(:filter, {}) || {}
-      @filters << Filter.new(:host, fh[:hosts]) if fh[:hosts]
-      @filters << Filter.new(:role, fh[:roles]) if fh[:roles]
-      @filters << Filter.new(:host, fh[:host]) if fh[:host]
-      @filters << Filter.new(:role, fh[:role]) if fh[:role]
-    end
-    
-          def self.[](host)
-        host.is_a?(Server) ? host : new(host)
-      end
-    
-        def URIEncodeOctets(octets, result, index)
-      if (@@hexCharCodeArray == 0)
-        @@hexCharCodeArray = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
-                              65, 66, 67, 68, 69, 70];
-      end
-      index = URIAddEncodedOctetToBuffer(octets[0], result, index);
-      if (octets[1]);
-        index = URIAddEncodedOctetToBuffer(octets[1], result, index)
-      end
-      if (octets[2]);
-        index = URIAddEncodedOctetToBuffer(octets[2], result, index)
-      end
-      if (octets[3]);
-        index = URIAddEncodedOctetToBuffer(octets[3], result, index)
-      end
-      return index;
-    end
-    
-          def editable
-        @editable
-      end
-    
-        # Test page_header_from_content(@content)
-    actual = @view.title
-    assert_equal '1 & 2', actual
+      teardown do
+    FileUtils.rm_rf(@path)
   end
     
-        @wiki.update_page(@wiki.page('PG'), nil, nil, '다른 text', {})
-    page = @wiki.page('PG')
-    assert_equal '다른 text', utf8(page.raw_data)
+        # Extract the 'page' name from the file_path
+    def extract_name(file_path)
+      if file_path[-1, 1] == '/'
+        return nil
+      end
     
-    # Set ruby to UTF-8 mode
-# This is required for Ruby 1.8.7 which gollum still supports.
-$KCODE = 'U' if RUBY_VERSION[0, 3] == '1.8'
+      class Error < StandardError;
+  end
+    
+      end
+    
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
+    end
+  end
+end
+    
+      # Escapes CDATA sections in post content
+  def cdata_escape(input)
+    input.gsub(/<!\[CDATA\[/, '&lt;![CDATA[').gsub(/\]\]>/, ']]&gt;')
+  end
+    
+        def render(context)
+      output = super
+      types = {
+        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
+        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
+        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
+      }
+      if @videos.size > 0
+        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
+        @videos.each do |v|
+          video << '<source src='#{v}' #{types[File.extname(v)]}>'
+        end
+        video += '</video>'
+      else
+        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+      end
+    end
