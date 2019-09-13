6d5640ee248989082dec3651dfcceefeafeb9bce
SUNsung <weatherius@gmail.com>
2019-09-13 09:09:43 +0300

    Rewrite [37tZYcEv4bETg37PS3o5UaEsJk374mhWOQx62Uc37akG.py] File in: 2019-09-13 09:09:42

--- pyton/37tZYcEv4bETg37PS3o5UaEsJk374mhWOQx62Uc37akG.py
+++ pyton/37tZYcEv4bETg37PS3o5UaEsJk374mhWOQx62Uc37akG.py
@@ -1,160 +1,57 @@
 
         
-        
-class TestLocalhostShorthand:
-    def test_expand_localhost_shorthand(self):
-        args = parser.parse_args(args=[':'], env=MockEnvironment())
-        assert args.url == 'http://localhost'
+                webpage = self._download_webpage(
+            'https://www.bitchute.com/video/%s' % video_id, video_id, headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.57 Safari/537.36',
+            })
     
-        def test_filename_from_url(self):
-        assert 'foo.txt' == filename_from_url(
-            url='http://example.org/foo',
-            content_type='text/plain'
-        )
-        assert 'foo.html' == filename_from_url(
-            url='http://example.org/foo',
-            content_type='text/html; charset=utf8'
-        )
-        assert 'foo' == filename_from_url(
-            url='http://example.org/foo',
-            content_type=None
-        )
-        assert 'foo' == filename_from_url(
-            url='http://example.org/foo',
-            content_type='x-foo/bar'
-        )
+            subtitles = {}
+        for resource in video.get('resources', []):
+            if resource.startswith('closed_caption_'):
+                splitted_resource = resource.split('_')
+                if splitted_resource[2]:
+                    subtitles.setdefault('en', []).append({
+                        'url': 'https://resources.redbull.tv/%s/%s' % (video_id, resource),
+                        'ext': splitted_resource[2],
+                    })
     
-        '''
-    headers = []
-    data = []
-    files = []
-    params = []
-    for item in items:
-        value = item.value
-        if item.sep == SEP_HEADERS:
-            if value == '':
-                # No value => unset the header
-                value = None
-            target = headers
-        elif item.sep == SEP_HEADERS_EMPTY:
-            if item.value:
-                raise ParseError(
-                    'Invalid item '%s' '
-                    '(to specify an empty header use `Header;`)'
-                    % item.orig
-                )
-            target = headers
-        elif item.sep == SEP_QUERY:
-            target = params
-        elif item.sep == SEP_FILES:
-            try:
-                with open(os.path.expanduser(value), 'rb') as f:
-                    value = (os.path.basename(value),
-                             BytesIO(f.read()),
-                             get_content_type(value))
-            except IOError as e:
-                raise ParseError(''%s': %s' % (item.orig, e))
-            target = files
+    r = openssl_encode('aes-128-cbc', key, iv)
+print('aes_cbc_decrypt')
+print(repr(r))
     
-                # >leading or trailing LWS MAY be removed without
-            # >changing the semantics of the field value'
-            # -https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
-            # Also, requests raises `InvalidHeader` for leading spaces.
-            value = value.strip()
-    
-            status_line = 'HTTP/{version} {status} {reason}'.format(
-            version=version,
-            status=original.status,
-            reason=original.reason
-        )
-        headers = [status_line]
-        try:
-            # `original.msg` is a `http.client.HTTPMessage` on Python 3
-            # `_headers` is a 2-tuple
-            headers.extend(
-                '%s: %s' % header for header in original.msg._headers)
-        except AttributeError:
-            # and a `httplib.HTTPMessage` on Python 2.x
-            # `headers` is a list of `name: val<CRLF>`.
-            headers.extend(h.strip() for h in original.msg.headers)
-    
-        >>> humanize_bytes(1)
-    '1 B'
-    >>> humanize_bytes(1024, precision=1)
-    '1.0 kB'
-    >>> humanize_bytes(1024 * 123, precision=1)
-    '123.0 kB'
-    >>> humanize_bytes(1024 * 12342, precision=1)
-    '12.1 MB'
-    >>> humanize_bytes(1024 * 12342, precision=2)
-    '12.05 MB'
-    >>> humanize_bytes(1024 * 1234, precision=2)
-    '1.21 MB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
-    '1.31 GB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
-    '1.3 GB'
-    
-        plugin_manager.register(Plugin)
-    try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            BASIC_AUTH_HEADER_VALUE,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
+        with io.open(outfile, 'w', encoding='utf-8') as outf:
+        outf.write(out)
     
     
-def test_default_options(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http(httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['form'] == {'foo': 'bar'}
+class TestAgeRestriction(unittest.TestCase):
+    def _assert_restricted(self, url, filename, age, old_age=None):
+        self.assertTrue(_download_restricted(url, filename, old_age))
+        self.assertFalse(_download_restricted(url, filename, age))
     
-    This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-'''
+        if probe_nat:
+        pteredor_is_running = False
+        log = Log()
+        log.write('qualified: %s\nNAT type: %s' % (prober.qualified, prober.nat_type))
+        log.close()
+    return prober
     
+    @section recognizers Recognizers
     
-_DEFAULTS = {
-    'threshold': {
-        'default': 99.0,
-        'info': 'Adjust the threshold for histogram matching. Can reduce extreme colors '
-                'leaking in by filtering out colors at the extreme ends of the histogram '
-                'spectrum.',
-        'datatype': float,
-        'rounding': 1,
-        'min_max': (90.0, 100.0),
-        'choices': [],
-        'gui_radio': False,
-        'fixed': True,
-    }
-}
-
+        Any state that has a semantic predicate edge is special; those states
+    are generated with if-then-else structures in a specialStateTransition()
+    which is generated by cyclicDFA template.
     
-    logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+    '''
     
-        Defaults files should be named <plugin_name>_defaults.py
-    Any items placed into this file will automatically get added to the relevant config .ini files
-    within the faceswap/config folder.
+    def __init__(
+        self,
+        recognizer, decisionNumber,
+        eot, eof, min, max, accept, special, transition
+        ):
+        ## Which recognizer encloses this DFA?  Needed to check backtracking
+        self.recognizer = recognizer
     
-    import tkinter as tk
-from tkinter import ttk
-    
-            if not obs_states:
-            obs_states = prev_states_expect_next if prev_states_expect_next else all_states
-    
-    parser = OptionParser(USAGE)
-parser.add_option('-k', dest='topK')
-opt, args = parser.parse_args()
-    
-    
-'''
+            ## What character index in the stream did the current token start at?
+        # Needed, for example, to get the text for current token.  Set at
+        # the start of nextToken.
+        self.tokenStartCharIndex = -1
