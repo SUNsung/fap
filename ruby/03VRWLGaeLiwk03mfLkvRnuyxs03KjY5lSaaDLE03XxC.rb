b829dd7e21298ccb7df1a2f74e757631335f75b5
SUNsung <weatherius@gmail.com>
2019-09-15 01:25:14 +0300

    Rewrite [03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb] File in: 2019-09-15 01:25:14

--- ruby/03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb
+++ ruby/03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb
@@ -1,151 +1,139 @@
 
         
-        class Devise::ConfirmationsController < DeviseController
-  # GET /resource/confirmation/new
-  def new
-    self.resource = resource_class.new
-  end
-    
-      def failure_message
-    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
-    error   = exception.error_reason if exception.respond_to?(:error_reason)
-    error ||= exception.error        if exception.respond_to?(:error)
-    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
-    error.to_s.humanize if error
-  end
-    
-          def headers_for(action, opts)
-        headers = {
-          subject: subject_for(action),
-          to: resource.email,
-          from: mailer_sender(devise_mapping),
-          reply_to: mailer_reply_to(devise_mapping),
-          template_path: template_paths,
-          template_name: action
-        }.merge(opts)
-    
-    module Devise
-  # Responsible for handling devise mappings and routes configuration. Each
-  # resource configured by devise_for in routes is actually creating a mapping
-  # object. You can refer to devise_for in routes for usage options.
-  #
-  # The required value in devise_for is actually not used internally, but it's
-  # inflected to find all other values.
-  #
-  #   map.devise_for :users
-  #   mapping = Devise.mappings[:user]
-  #
-  #   mapping.name #=> :user
-  #   # is the scope used in controllers and warden, given in the route as :singular.
-  #
-  #   mapping.as   #=> 'users'
-  #   # how the mapping should be search in the path, given in the route as :as.
-  #
-  #   mapping.to   #=> User
-  #   # is the class to be loaded from routes, given in the route as :class_name.
-  #
-  #   mapping.modules  #=> [:authenticatable]
-  #   # is the modules included in the class
-  #
-  class Mapping #:nodoc:
-    attr_reader :singular, :scoped_path, :path, :controllers, :path_names,
-                :class_name, :sign_out_via, :format, :used_routes, :used_helpers,
-                :failure_app, :router_name
-    
-        # The hook which is called inside devise.
-    # So your ORM can include devise compatibility stuff.
-    def devise_modules_hook!
-      yield
-    end
-  end
-end
-    
-        def clear_cache
-      downloader.clear_cache if force
+              open_dry_run_modal(agent)
+      find('.dry-run-event-sample').click
+      within(:css, '.modal .builder') do
+        expect(page).to have_text('http://xkcd.com/')
+      end
+      click_on('Dry Run')
+      expect(page).to have_text('Biologists play reverse')
+      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
     end
     
-        it 'returns true if installed' do
-      expect(machine.communicate).to receive(:test).
-        with(command, sudo: true).and_return(true)
-      subject.chef_installed(machine, 'chef_solo', version)
+        it 'respects an environment variable that specifies a path or URL to a different scenario' do
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
     end
     
-        it 'returns false if not installed' do
-      expect(machine.communicate).to receive(:test).
-        with(command, sudo: true).and_return(false)
-      expect(subject.chef_installed(machine, 'chef_solo', version)).to be_falsey
+      describe 'down' do
+    let :valid_options do
+      super().merge('extract' => new_extract,
+                    'template' => new_template)
+    end
+    
+        it 'should raise error when response has an error' do
+      stub(HTTParty).post { {'error' => {'message' => 'Sample error'}} }
+      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Sample error/)
     end
   end
 end
 
     
-      before do
-    allow(machine).to receive(:communicate).and_return(communicator)
+    # Use this hook to configure devise mailer, warden hooks and so forth.
+# Many of these configuration options can be set straight in your model.
+Devise.setup do |config|
+  # The secret key used by Devise. Devise uses this key to generate
+  # random tokens. Changing this key will render invalid all existing
+  # confirmation, reset password and unlock tokens in the database.
+  config.secret_key = AppConfig.secret_token
+    
+    puts 'Configuring Bundler for #{rails_env} environment and #{database} database.'
+    
+    class HomeController < ApplicationController
+  def show
+    partial_dir = Rails.root.join('app', 'views', 'home')
+    if user_signed_in?
+      redirect_to stream_path
+    elsif request.format == :mobile
+      if partial_dir.join('_show.mobile.haml').exist? ||
+         partial_dir.join('_show.mobile.erb').exist? ||
+         partial_dir.join('_show.haml').exist?
+        render :show
+      else
+        redirect_to user_session_path
+      end
+    elsif partial_dir.join('_show.html.haml').exist? ||
+          partial_dir.join('_show.html.erb').exist? ||
+          partial_dir.join('_show.haml').exist?
+      render :show
+    elsif Role.admins.any?
+      render :default
+    else
+      redirect_to podmin_path
+    end
   end
     
-        it 'should return the name and provider of active machines' do
-      machines = instance.local_data_path.join('machines')
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
     
-          # Create the machine and cache it for future calls. This will also
-      # return the machine from this method.
-      return Machine.new(name, provider, provider_cls, provider_config,
-        provider_options, config, data_path, box, env, self)
-    end
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
     
-          if @allow_parallel
-        par = true
-        @logger.info('Enabling parallelization by default.')
-      end
-    
-          def execute
-        options = {}
-    
-            def scope
-          @scope ||= if params[:option_type_id]
-                       Spree::OptionType.find(params[:option_type_id]).option_values.accessible_by(current_ability, :show)
-                     else
-                       Spree::OptionValue.accessible_by(current_ability, :show).load
-                     end
+            # Describes what makes each {PodVariant} distinct among the others.
+        #
+        # @return [Hash<PodVariant, String>]
+        #
+        def scope_suffixes
+          return { variants.first => nil } if variants.count == 1
+          Hash[scope_by_specs.map do |variant, scope|
+            require 'digest'
+            scope = Digest::MD5.hexdigest(scope)[0..7] if !scope.nil? && scope.length >= 50
+            [variant, scope]
+          end]
         end
     
-            def new; end
-    
-            def cancel
-          @return_authorization = order.return_authorizations.accessible_by(current_ability, :update).find(params[:id])
-          if @return_authorization.cancel
-            respond_with @return_authorization, default_template: :show
-          else
-            invalid_resource!(@return_authorization)
-          end
+            case a
+        when 0 then 'just now'
+        when 1 then 'a second ago'
+        when 2..59 then a.to_s + ' seconds ago'
+        when 60..119 then 'a minute ago' # 120 = 2 minutes
+        when 120..3540 then (a / 60).to_i.to_s + ' minutes ago'
+        when 3541..7100 then 'an hour ago' # 3600 = 1 hour
+        when 7101..82_800 then ((a + 99) / 3600).to_i.to_s + ' hours ago'
+        when 82_801..172_000 then 'a day ago' # 86400 = 1 day
+        when 172_001..518_400 then ((a + 800) / (60 * 60 * 24)).to_i.to_s + ' days ago'
+        when 518_400..1_036_800 then 'a week ago'
+        when 1_036_801..4_147_204 then ((a + 180_000) / (60 * 60 * 24 * 7)).to_i.to_s + ' weeks ago'
+        else date.strftime('%d %b %Y')
         end
-    
-            def zone_params
-          attrs = params.require(:zone).permit!
-          if attrs[:zone_members]
-            attrs[:zone_members_attributes] = attrs.delete(:zone_members)
-          end
-          attrs
-        end
-    
-    def get_stdin(message)
-  print message
-  STDIN.gets.chomp
-end
-    
-      if options.respond_to? 'keys'
-    options.each do |k,v|
-      unless v.nil?
-        v = v.join ',' if v.respond_to? 'join'
-        v = v.to_json if v.respond_to? 'keys'
-        output += ' data-#{k.sub'_','-'}='#{v}''
       end
     end
-  elsif options.respond_to? 'join'
-    output += ' data-value='#{config[key].join(',')}''
-  else
-    output += ' data-value='#{config[key]}''
   end
-  output += '></#{tag}>'
 end
+
     
-    module Jekyll
+          it 'sets the description' do
+        post '/users/api_secrets', params: { api_secret: valid_params }
+        expect(user.api_secrets.last.description).to eq valid_params[:description]
+      end
+    
+      describe 'DELETE blocks' do
+    it 'updates block from input data' do
+      block = create(:block, user_id: user.id, input_css: '.blue { color: blue;}')
+      delete '/blocks/#{block.id}'
+      expect(Block.all.size).to eq(0)
+    end
+  end
+end
+
+    
+      it 'renders a user index if there is a user with the username successfully' do
+    expect(get: '/#{user.username}').to route_to(
+      controller: 'stories',
+      action: 'index',
+      username: user.username,
+    )
+  end
+    
+      def self.upbuff!(buffer_update_id, admin_id, body_text, status)
+    buffer_update = BufferUpdate.find(buffer_update_id)
+    if status == 'confirmed'
+      buffer_response = send_to_buffer(body_text, buffer_update.buffer_profile_id_code)
+      buffer_update.update!(buffer_response: buffer_response, status: status, approver_user_id: admin_id, body_text: body_text)
+    else
+      buffer_update.update!(status: status, approver_user_id: admin_id)
+    end
+  end
