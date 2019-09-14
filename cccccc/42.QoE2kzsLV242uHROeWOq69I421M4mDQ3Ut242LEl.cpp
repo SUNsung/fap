a25a6f3dc0a4f30967732db49bdca68819240df9
SUNsung <weatherius@gmail.com>
2019-09-15 01:58:54 +0300

    Rewrite [42.QoE2kzsLV242uHROeWOq69I421M4mDQ3Ut242LEl.cpp] File in: 2019-09-15 01:58:54

--- cccccc/42.QoE2kzsLV242uHROeWOq69I421M4mDQ3Ut242LEl.cpp
+++ cccccc/42.QoE2kzsLV242uHROeWOq69I421M4mDQ3Ut242LEl.cpp
@@ -1,48 +1,249 @@
 
         
-            NDLBuilderImpl(ComputationNetworkPtr computationNetwork)
+        /// SILProfiler - Maps AST nodes to profile counters.
+class SILProfiler : public SILAllocated<SILProfiler> {
+private:
+  SILModule &M;
+    }
+    
+    TEST(ClusteredBitVector, AppendSetBits) {
+  ClusteredBitVector vec;
+  vec.add(23, 7988315);
+  vec.appendSetBits(77);
+  EXPECT_EQ(100u, vec.size());
+  EXPECT_EQ(true, vec[0]);
+  EXPECT_EQ(true, vec[30]);
+  EXPECT_EQ(true, vec[70]);
+}
+    
+    
+    {} // end llvm namespace
+    
+    
+    {  char *end;
+  _set_errno(0);
+  *result = _strtof_l(str, &end, getCLocale());
+  if (*result == HUGE_VALF || *result == -HUGE_VALF || *result == 0.0 || *result == -0.0) {
+    if (errno == ERANGE)
+        end = nullptr;
+  }
+  return end;
+}
+    
+    /// Knows how to make a deep copy of a debug scope.
+class ScopeCloner {
+  llvm::SmallDenseMap<const SILDebugScope *,
+                      const SILDebugScope *> ClonedScopeCache;
+  SILFunction &NewFn;
+public:
+  /// ScopeCloner expects NewFn to be a clone of the original
+  /// function, with all debug scopes and locations still pointing to
+  /// the original function.
+  ScopeCloner(SILFunction &NewFn);
+  /// Return a (cached) deep copy of a scope.
+  const SILDebugScope *getOrCreateClonedScope(const SILDebugScope *OrigScope);
+};
+    
+    
+    {  const SelfWitnessTableMetadataSource *
+  createSelfWitnessTable() {
+    return SelfWitnessTableMetadataSource::create(*this);
+  }
+};
+    
+    
+    {    return { this->template getTrailingObjects<SourceLoc>(),
+             asDerived().getNumArguments() };
+  }
+    
+      /// Given a 'cascading' job, that is a job whose dependents must be recompiled
+  /// when this job is recompiled, Compute two sets of jobs:
+  /// 1. Return value (via visited) is the set of jobs needing recompilation
+  /// after this one, and
+  /// 2. Jobs not previously known to need dependencies reexamined after they
+  /// are recompiled. Such jobs are added to the \ref cascadingJobs set, and
+  /// accessed via \ref isMarked.
+  void markTransitive(
+      SmallVectorImpl<const driver::Job *> &consequentJobsToRecompile,
+      const driver::Job *jobToBeRecompiled, const void *ignored = nullptr);
+    
+    #endif  // TESSERACT_CCSTRUCT_DEBUGPIXA_H_
+
+    
+    
+    {}  // namespace tesseract.
+    
+    // Update the other members if the cost is lower.
+void DPPoint::UpdateIfBetter(int64_t cost, int32_t steps, const DPPoint* prev,
+                             int32_t n, int32_t sig_x, int64_t sig_xsq) {
+  if (cost < total_cost_) {
+    total_cost_ = cost;
+    total_steps_ = steps;
+    best_prev_ = prev;
+    n_ = n;
+    sig_x_ = sig_x;
+    sig_xsq_ = sig_xsq;
+  }
+}
+    
+      /* Reject modes generated after QUALITY_ACCEPT but before MINIMAL_REJ accep*/
+  R_DOC_REJ,    // TEMP Document rejection
+  R_BLOCK_REJ,  // TEMP Block rejection
+  R_ROW_REJ,    // TEMP Row rejection
+  R_UNLV_REJ,   // TEMP ~ turned to - or ^ turned to space
+    
+    #include 'unichar.h'
+    
+    /**----------------------------------------------------------------------------
+          Include Files and Type Defines
+----------------------------------------------------------------------------**/
+#include 'blobs.h'
+#include 'intproto.h'
+#include 'normalis.h'
+#include <cmath>
+    
+      // Visual debugger classifies the given sample, displays the results and
+  // solicits user input to display other classifications. Returns when
+  // the user has finished with debugging the sample.
+  // Probably doesn't need to be overridden if the subclass provides
+  // DisplayClassifyAs.
+  virtual void DebugDisplay(const TrainingSample& sample, Pix* page_pix,
+                            UNICHAR_ID unichar_id);
+    
+    typedef enum {
+  DO_NOT_CANCEL = 0,
+  CANCEL_BEFORE_PROCESSING,
+  CANCEL_DURING_PROCESSING,
+  CANCEL_AFTER_PROCESSING
+} ServerTryCancelRequestPhase;
+    
+    void ChannelArguments::SetResourceQuota(
+    const grpc_impl::ResourceQuota& resource_quota) {
+  SetPointerWithVtable(GRPC_ARG_RESOURCE_QUOTA,
+                       resource_quota.c_resource_quota(),
+                       grpc_resource_quota_arg_vtable());
+}
+    
+    static void create_sockets(SOCKET sv[2]) {
+  SOCKET svr_sock = INVALID_SOCKET;
+  SOCKET lst_sock = INVALID_SOCKET;
+  SOCKET cli_sock = INVALID_SOCKET;
+  SOCKADDR_IN addr;
+  int addr_len = sizeof(addr);
+    }
+    
+    static void on_alarm(void* acp, grpc_error* error) {
+  async_connect* ac = (async_connect*)acp;
+  gpr_mu_lock(&ac->mu);
+  grpc_winsocket* socket = ac->socket;
+  ac->socket = NULL;
+  if (socket != NULL) {
+    grpc_winsocket_shutdown(socket);
+  }
+  async_connect_unlock_and_cleanup(ac, socket);
+}
+    
+    #endif  // GRPCPP_SUPPORT_CHANNEL_ARGUMENTS_H
+
+    
+    
+    {}  // namespace grpc_impl
+
+    
+    class CronetChannelCredentialsImpl final : public ChannelCredentials {
+ public:
+  CronetChannelCredentialsImpl(void* engine) : engine_(engine) {}
+    }
+    
+      void InsertPluginWithTestService() {
+    if (GetParam()) {
+      // Add ServerBuilder plugin in static initialization
+      auto plugin = CheckPresent();
+      EXPECT_TRUE(plugin);
+      plugin->SetRegisterService();
+    } else {
+      // Add ServerBuilder plugin using ServerBuilder::SetOption()
+      std::unique_ptr<InsertPluginServerBuilderOption> option(
+          new InsertPluginServerBuilderOption());
+      option->SetRegisterService();
+      builder_->SetOption(std::move(option));
+    }
+  }
+    
+      // Client sends 20 requests and the server returns after reading 10 requests.
+  // If return_cancel is true, server returns CANCELLED status. Otherwise it
+  // returns OK.
+  void DoBidiStream(bool return_cancelled) {
+    EchoRequest request;
+    EchoResponse response;
+    ClientContext context;
+    }
+    
+    #include <grpc/support/port_platform.h>
+    
+            /**
+         * Not equal comparison operator.
+         * @param point Point<T> to be compared.
+         * @result Whether the instance satisfies the condition with respect to point.
+         */
+        inline bool operator!=(const Point<T>& point) const
+        {
+            return area() != point.area();
+        }
+    
+        template<typename T>
+    std::string Point<T>::toString() const
     {
-        m_computationNetwork = computationNetwork;
-        m_nodeEvaluator = new NDLNodeEvaluatorImpl<ElemType>(m_computationNetwork);
+        try
+        {
+            return '[' + std::to_string(x) + ', ' + std::to_string(y) + ']';
+        }
+        catch (const std::exception& e)
+        {
+            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
+            return '';
+        }
     }
     
-    // CheckFunction - check to see if we match a function name
-// string1 - [in,out] string to compare, if comparision is equal and at least half the full node name will replace with full node name
-// allowUndeterminedVariable - [out] set to true if undetermined variables (symbols yet to be defined) are allowed here
-// return - true if function name found
-bool CheckFunction(std::string& p_nodeType, bool* allowUndeterminedVariable)
-{
-    if (allowUndeterminedVariable)
-        *allowUndeterminedVariable = true; // be default we allow undetermined variables
+    
+    {  try {
+    eventHandlers()[event]();
+  } catch (...) {
+    qWarning() << 'Error on event processing: ' << event;
+  }
+}
+    
+    void BulkOperations::CopyOperation::performOperation(
+    QSharedPointer<RedisClient::Connection> targetConnection,
+    int targetDbIndex) {
+  m_combinator = QSharedPointer<AsyncFuture::Combinator>(
+      new AsyncFuture::Combinator(AsyncFuture::FailFast));
     }
     
-            if (des->OperationName() == OperationNameOf(PastValueNode) || des->OperationName() == L'Delay')
-        {
-            // special treament for arc with PastValue node as the children
-            // create a dummy node
-            ComputationNodeBasePtr pastValueNode = des;
-            wstring dummyName = des->GetName() + L'.dummy';
-            wstring out = msra::strfun::wstrprintf(L'node [ shape = box3d  , color = lightgray, style = \'filled\' , label = \'%ls\' ] ; \'%ls\'\n',
-                                                   (pastValueNode->GetName() + L'\\n(PastValue)').c_str(),
-                                                   dummyName.c_str());
-            line = out;
-            line += msra::strfun::wstrprintf(L'\'%ls\' -> \'%ls\' ; \n', dummyName.c_str(), srcname.c_str());
-        }
-        else if (des->OperationName() == OperationNameOf(FutureValueNode))
-        {
-            // special treament for arc with FutureValue node as the children
-            // create a dummy node
-            ComputationNodeBasePtr futureValueNode = des;
-            wstring dummyName = des->GetName() + L'.dummy';
-            wstring out = msra::strfun::wstrprintf(L'node [ shape = box3d  , color = red, style = \'filled\' , label = \'%ls\' ] ; \'%ls\'\n',
-                                                   (futureValueNode->GetName() + L'\\n(FutureValue)').c_str(),
-                                                   dummyName.c_str());
-            line = out;
-            line += msra::strfun::wstrprintf(L'\'%ls\' -> \'%ls\' ; \n', dummyName.c_str(), srcname.c_str());
-        }
-        else
-        {
-            line = msra::strfun::wstrprintf(L'\'%ls\' -> \'%ls\' ; \n', desname.c_str(), srcname.c_str());
-        }
+    class CopyOperation : public AbstractOperation {
+  Q_OBJECT
+ public:
+  CopyOperation(QSharedPointer<RedisClient::Connection> connection, int dbIndex,
+                OperationCallback callback,
+                QRegExp keyPattern = QRegExp('*', Qt::CaseSensitive,
+                                             QRegExp::Wildcard));
+    }
     
-            ValueContainer<ElemType>& vec = outputs[i2].m_buffer;
+      m_progress = 0;
+  m_errors.clear();
+    
+      if (series->count() == 0 && ax) {
+    ax->setMin(QDateTime::currentDateTime());
+  }
+    
+    BulkOperations::RDBImportOperation::RDBImportOperation(
+    QSharedPointer<RedisClient::Connection> connection, int dbIndex,
+    OperationCallback callback, QSharedPointer<QPython> p, QRegExp keyPattern)
+    : BulkOperations::AbstractOperation(connection, dbIndex, callback,
+                                        keyPattern),
+      m_python(p) {
+  m_python->importModule_sync('rdb');
+}
+    
+    namespace BulkOperations {
+    }
