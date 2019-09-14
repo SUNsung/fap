4cab380ad10fbd489c424c9119e91b26c7e437df
SUNsung <weatherius@gmail.com>
2019-09-14 19:34:15 +0300

    Rewrite [36DZoYhGct1yw36XqCk1xbuiGE36ncWlvlqkDRI36lGH.cpp] File in: 2019-09-14 19:34:15

--- cccccc/36DZoYhGct1yw36XqCk1xbuiGE36ncWlvlqkDRI36lGH.cpp
+++ cccccc/36DZoYhGct1yw36XqCk1xbuiGE36ncWlvlqkDRI36lGH.cpp
@@ -1,200 +1,152 @@
 
         
-        bool CertificateManagerModel::ImportCACerts(
-    const net::ScopedCERTCertificateList& certificates,
-    net::NSSCertDatabase::TrustBits trust_bits,
-    net::NSSCertDatabase::ImportCertFailureList* not_imported) {
-  return cert_db_->ImportCACerts(certificates, trust_bits, not_imported);
-}
+        using namespace OPENEXR_IMF_INTERNAL_NAMESPACE;
     
-    napi_value Init(napi_env env, napi_value exports) {
-  napi_status status;
-  napi_property_descriptor descriptors[] = {
-      {'Print', NULL, Print, NULL, NULL, NULL, napi_default, NULL}};
+    #include <ImfFramesPerSecond.h>
+#include 'ImathFun.h'
+    
+        OPENEXR_IMF_INTERNAL_NAMESPACE::Xdr::read <OPENEXR_IMF_INTERNAL_NAMESPACE::StreamIO> (is, magic);
+    OPENEXR_IMF_INTERNAL_NAMESPACE::Xdr::read <OPENEXR_IMF_INTERNAL_NAMESPACE::StreamIO> (is, version);
+    
+    OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_EXIT
+
+    
+    
+void
+Header::setType(const string& type)
+{
+    if (isSupportedType(type) == false)
+    {
+        throw IEX_NAMESPACE::ArgExc (type + 'is not a supported image type.' +
+                           'The following are supported: ' +
+                           SCANLINEIMAGE + ', ' +
+                           TILEDIMAGE + ', ' +
+                           DEEPSCANLINE + ' or ' +
+                           DEEPTILE + '.');
+    }
     }
     
-    namespace electron {
+    
+void	
+InputFile::Data::deleteCachedBuffer()
+{
+    //
+    // Delete the cached frame buffer, and all memory
+    // allocated for the slices in the cached frameBuffer.
+    //
     }
-    
-    #ifndef SHELL_APP_COMMAND_LINE_ARGS_H_
-#define SHELL_APP_COMMAND_LINE_ARGS_H_
-    
-      // NativeWindowObserver:
-  void RequestPreferredWidth(int* width) override;
-  void OnCloseButtonClicked(bool* prevent_default) override;
-    
-    
-    {}  // namespace api
-    
-    class GlobalShortcut : public extensions::GlobalShortcutListener::Observer,
-                       public mate::TrackableObject<GlobalShortcut> {
- public:
-  static mate::Handle<GlobalShortcut> Create(v8::Isolate* isolate);
-    }
-    
-            _tableSymbol[i]  = 0xffff;
-        _tableCodeLen[i] = 0; 
-    
-        IMF_EXPORT
-    void decode (const unsigned char *src,
-                 int numSrcBits,
-                 unsigned short *dst,
-                 int numDstElems);
-    
-    #include 'ImfGenericInputFile.h'
-    
-    OPENEXR_IMF_INTERNAL_NAMESPACE_HEADER_ENTER
     
     
 OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_ENTER
     
-      // Apply the shard, platform, and version checking.
-  // It is important to set each value such that the packs meta-table can report
-  // each of the restrictions.
-  if ((shard_ > 0 && shard_ < getMachineShard()) || !checkPlatform() ||
-      !checkVersion()) {
-    return;
-  }
+    // Get basic type definitions.
+#define IPC_MESSAGE_IMPL
+#include 'content/nw/src/common/common_message_generator.h'
     
-     private:
-  template <typename T>
-  Expected<T, DatabaseError> getValue(const std::string& domain,
-                                      const std::string& key);
-  template <typename T>
-  ExpectedSuccess<DatabaseError> putValue(const std::string& domain,
-                                          const std::string& key,
-                                          const T& value);
+      DVLOG(1) << 'remote::AllocateObject(routing_id=' << routing_id << ', object_id=' << object_id << ')';
     
-    ExpectedSuccess<RocksdbMigrationError> RocksdbMigration::moveDb(
-    const std::string& src_path, const std::string& dst_path) {
-  boost::system::error_code ec;
-  if (fs::exists(fs::path(dst_path), ec)) {
-    return createError(RocksdbMigrationError::FailMoveDatabase)
-           << 'Database at dst path already exists or path is not accessible. '
-              'Path: '
-           << dst_path << 'Error: ' << ec.value() << ' ' << ec.message();
-  }
-    }
-    
-      std::map<std::string, FlagInfo> flags;
-  for (const auto& flag : info) {
-    if (instance().flags_.count(flag.name) == 0) {
-      // This flag info was not defined within osquery.
-      continue;
-    }
-    }
+       void Call(const std::string& method,
+                    const base::ListValue& arguments) override;
+   void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) override;
     
     
-    {  name_ = name;
+    {  int delta = this - prev;
+  int32_t n = prev->n_ + 1;
+  int32_t sig_x = prev->sig_x_ + delta;
+  int64_t sig_xsq = prev->sig_xsq_ + delta * delta;
+  int64_t cost = (sig_xsq - sig_x * sig_x / n) / n;
+  cost += prev->total_cost_;
+  UpdateIfBetter(cost, prev->total_steps_ + 1, prev, n, sig_x, sig_xsq);
+  return cost;
 }
     
+    #endif
+
     
-    { public:
-  Status call(const PluginRequest& request, PluginResponse& response) override;
-};
+    ELISTIZE(PARA)
     
-      virtual void SetUp() {
-    boost::uuids::uuid uuid = boost::uuids::random_generator()();
-    auto random_name = boost::uuids::to_string(uuid);
-    auto path = boost::filesystem::temp_directory_path().append(random_name);
-    boost::filesystem::create_directory(path);
-    path_ = path.string();
+      // Simple accessors.
+  bool empty() const {
+    return heap_.empty();
+  }
+  int size() const {
+    return heap_.size();
+  }
+  int size_reserved() const {
+    return heap_.size_reserved();
+  }
+  void clear() {
+    // Clear truncates to 0 to keep the number reserved in tact.
+    heap_.truncate(0);
+  }
+  // Provides access to the underlying vector.
+  // Caution! any changes that modify the keys will invalidate the heap!
+  GenericVector<Pair>* heap() {
+    return &heap_;
+  }
+  // Provides read-only access to an element of the underlying vector.
+  const Pair& get(int index) const {
+    return heap_[index];
   }
     
-    
-    {  for (size_t i = 0; i < len;) {
-    int h1 = fromHex(data_[i++]);
-    if (h1 < 0) {
-      return false;
-    }
-    int h2 = fromHex(data_[i++]);
-    if (h2 < 0) {
-      return false;
-    }
-    result->push_back(static_cast<char>((h1 << 4) | h2));
+      // Free up any currently unused dawgs.
+  void DeleteUnusedDawgs() {
+    dawgs_.DeleteUnusedObjects();
   }
-  return true;
+    
+    bool OrbitCamera::initWithDuration(float t, float radius, float deltaRadius, float angleZ, float deltaAngleZ, float angleX, float deltaAngleX)
+{
+    if ( ActionInterval::initWithDuration(t) )
+    {
+        _radius = radius;
+        _deltaRadius = deltaRadius;
+        _angleZ = angleZ;
+        _deltaAngleZ = deltaAngleZ;
+        _angleX = angleX;
+        _deltaAngleX = deltaAngleX;
+    }
+    }
+    
+    #ifndef __CCCAMERA_ACTION_H__
+#define __CCCAMERA_ACTION_H__
+    
+        if (action)
+    {
+        if (action->initWithDuration(duration, gridSize, waves, amplitude, horizontal, vertical))
+        {
+            action->autorelease();
+        }
+        else
+        {
+            CC_SAFE_RELEASE_NULL(action);
+        }
+    }
+    
+    NS_CC_BEGIN
+    
+    static bool sendComponentEventToJS(Component* node, int action)
+{
+    auto scriptEngine = ScriptEngineManager::getInstance()->getScriptEngine();
+    
+    if (scriptEngine->isCalledFromScript())
+    {
+        scriptEngine->setCalledFromScript(false);
+    }
+    else
+    {
+        BasicScriptData data(node,(void*)&action);
+        ScriptEvent scriptEvent(kComponentEvent,(void*)&data);
+        if (scriptEngine->sendEvent(&scriptEvent))
+            return true;
+    }
+    
+    return false;
 }
     
-    #include 'utilities/blob_db/blob_compaction_filter.h'
-#include 'db/dbformat.h'
+     * It adds support for the TMX tiled map format used by http://www.mapeditor.org.
+ * It supports isometric, hexagonal and orthogonal tiles.
+ * It also supports object groups, objects, and properties.
     
-    #include 'rocksdb/merge_operator.h'
-#include 'rocksdb/slice.h'
-#include 'utilities/merge_operators.h'
-    
-      // Will be called while on the write thread before the write executes.  If
-  // this function returns a non-OK status, the write will be aborted and this
-  // status will be returned to the caller of DB::Write().
-  virtual Status Callback(DB* db) = 0;
-    
-        coro_test({
-        make_pair([](void *arg)
-        {
-            auto chan = (Channel *) arg;
-            int i = 1;
-            bool ret;
-    }
-    }
-    
-        inline size_t producer_num()
-    {
-        return producer_queue.size();
-    }
-    
-        swHashMap_del(hm, (char *) SW_STRL('willdel'));
-    swHashMap_update(hm, (char *) SW_STRL('willupadte'), (void *) (9999 * 5555));
-    
-            send_pkg.ptr = ptr;
-        send_pkg.size = size;
-        send_pkg.serial_num = rand();
-    
-        fd1 = socket(AF_UNIX,SOCK_DGRAM,0);
-    strncpy(un1.sun_path, sock1_path, sizeof(un1.sun_path) - 1); 
-    bind(fd1,(struct sockaddr *)&un1,sizeof(un1));
-    
-        private:
-        void addRead() {
-            if (m_read) return;
-            m_read = new QSocketNotifier(m_ctx->c.fd, QSocketNotifier::Read, 0);
-            connect(m_read, SIGNAL(activated(int)), this, SLOT(read()));
-        }
-    
-    
-    {    i = l % SIZEOF_LONG;
-    l -= i;
-    for (addr = l;; addr += SIZEOF_LONG)
-    {
-        if (0 > trace_get_long(traced_pid, addr, &l))
-        {
-            return -1;
-        }
-        for (; i < SIZEOF_LONG; i++)
-        {
-            --sz;
-            if (sz && lc[i])
-            {
-                *buf++ = lc[i];
-                continue;
-            }
-            *buf = '\0';
-            return 0;
-        }
-        i = 0;
-    }
-    return 0;
-}
-    
-        template<typename T>
-    Rectangle<T> Rectangle<T>::operator*(const T value) const
-    {
-        try
-        {
-            return Rectangle<T>{T(x * value), T(y * value), T(width * value), T(height * value)};
-        }
-        catch (const std::exception& e)
-        {
-            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
-            return Rectangle<T>{};
-        }
-    }
+    /// @cond DO_NOT_SHOW
