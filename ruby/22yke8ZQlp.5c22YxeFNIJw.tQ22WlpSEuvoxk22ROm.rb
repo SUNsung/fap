61540302b1b7769dfe4ada48c82a404aa32ac3e2
SUNsung <weatherius@gmail.com>
2019-09-14 11:18:55 +0300

    Rewrite [22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb] File in: 2019-09-14 11:18:55

--- ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
+++ ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
@@ -1,98 +1,63 @@
 
         
-            def aes256_gcm_encrypt(value)
-      encrypted_token = Encryptor.encrypt(AES256_GCM_OPTIONS.merge(value: value))
-      Base64.strict_encode64(encrypted_token)
+              template.resolve!(
+        project_name: params[:project].presence,
+        fullname: params[:fullname].presence || current_user&.name
+      )
+    
+          args [:secret]
+    
+    describe GroupMemberPresenter do
+  let(:user) { double(:user) }
+  let(:group) { double(:group) }
+  let(:group_member) { double(:group_member, source: group) }
+  let(:presenter) { described_class.new(group_member, current_user: user) }
+    
+              if badge.persisted?
+            present_badges(source, badge)
+          else
+            render_validation_error!(badge)
+          end
+        end
+    
+          unless File.exists?(File.join(Docs.store_path, '#{doc.path}.tar.gz'))
+        puts 'ERROR: package for '#{doc.slug}' documentation not found. Run 'thor docs:package #{doc.slug}' to create it.'
+        return
+      end
     end
     
-        context 'when user cannot update_group_member' do
-      before do
-        allow(presenter).to receive(:can?).with(user, :update_group_member, presenter).and_return(false)
-        allow(presenter).to receive(:can?).with(user, :override_group_member, presenter).and_return(false)
+          base_dir = Pathname.new(normalized_path)
+      base_dir = base_dir.parent unless path.end_with? '/'
+    
+            # Move id attributes to headings
+        css('.method-detail').each do |node|
+          next unless heading = node.at_css('.method-heading')
+          heading['id'] = node['id']
+          node.remove_attribute 'id'
+        end
+    
+              # Differentiate server classes (http, https, net, etc.)
+          name.sub!('server.') { '#{(klass || 'https').sub('.', '_').downcase}.' }
+          # Differentiate socket classes (net, dgram, etc.)
+          name.sub!('socket.') { '#{klass.sub('.', '_').downcase}.' }
+    
+            begin
+          Component.new.handle_exception exception
+          pass
+        rescue StandardError
+          flunk 'failed handling a nil backtrace'
+        end
       end
-    
-      def output_chronic_duration_attribute(source_attribute)
-    value = attributes[source_attribute.to_s]
-    ChronicDuration.output(value, format: :short) if value
-  end
-end
-
-    
-          it 'deactivates an existing user' do
-        visit admin_users_path
-        expect(page).to have_no_text('inactive')
-        find(:css, 'a[href='/admin/users/#{users(:bob).id}/deactivate']').click
-        expect(page).to have_text('inactive')
-        users(:bob).reload
-        expect(users(:bob)).not_to be_active
-      end
-    
-          select2('Example Scheduler', from: 'Controllers')
-    
-      context 'successful dry runs' do
-    before do
-      stub_request(:get, 'http://xkcd.com/').
-        with(:headers => {'Accept-Encoding'=>'gzip,deflate', 'User-Agent'=>'Huginn - https://github.com/huginn/huginn'}).
-        to_return(:status => 200, :body => File.read(Rails.root.join('spec/data_fixtures/xkcd.html')), :headers => {})
-    end
-    
-      it 'imports a scenario that does not exist yet' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This scenario has a few agents to get you started. Feel free to change them or delete them as you see fit!')
-    expect(page).not_to have_text('This Scenario already exists in your system.')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
-  end
-    
-          it 'loads only the workers specified in the :only option' do
-        agent_runner = AgentRunner.new(only: HuginnScheduler)
-        workers = agent_runner.send(:load_workers)
-        expect(workers.keys).to eq(['HuginnScheduler'])
-        agent_runner.stop
-      end
-    
-          expect(data[:agents][guid_order(agent_list, :jane_weather_agent)]).not_to have_key(:propagate_immediately) # can't receive events
-      expect(data[:agents][guid_order(agent_list, :jane_rain_notifier_agent)]).not_to have_key(:schedule) # can't be scheduled
-    end
-    
-      describe 'cleanup_failed_jobs!' do
-    before do
-      3.times do |i|
-        Delayed::Job.create(failed_at: Time.now - i.minutes)
-      end
-      @keep = Delayed::Job.order(:failed_at)[1]
-    end
-    
-        it 'html_safes the output unless :skip_safe is passed in' do
-      expect(Utils.jsonify({:foo => 'bar'})).to be_html_safe
-      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => false)).to be_html_safe
-      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => true)).not_to be_html_safe
     end
   end
     
-      describe '#log_for_agent' do
-    it 'creates AgentLogs' do
-      log = AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 4, :outbound_event => events(:jane_website_agent_event))
-      expect(log).not_to be_new_record
-      expect(log.agent).to eq(agents(:jane_website_agent))
-      expect(log.outbound_event).to eq(events(:jane_website_agent_event))
-      expect(log.message).to eq('some message')
-      expect(log.level).to eq(4)
-    end
+      it 'allows delayed scheduling of AR class methods' do
+    ss = Sidekiq::ScheduledSet.new
+    assert_equal 0, ss.size
+    MyModel.delay_for(5.days).long_class_method
+    assert_equal 1, ss.size
+  end
     
-      class Blockquote < Liquid::Block
-    FullCiteWithTitle = /(\S.*)\s+(https?:\/\/)(\S+)\s+(.+)/i
-    FullCite = /(\S.*)\s+(https?:\/\/)(\S+)/i
-    AuthorTitle = /([^,]+),([^,]+)/
-    Author =  /(.+)/
-    
-        def html_output_for(script_url, code)
-      code = CGI.escapeHTML code
-      <<-HTML
-<div><script src='#{script_url}'></script>
-<noscript><pre><code>#{code}</code></pre></noscript></div>
-      HTML
+        after do
+      Sidekiq::Testing.disable!
     end
