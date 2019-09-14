7b7e6a990e8f6991cabb18ac4307276e8c008cb1
SUNsung <weatherius@gmail.com>
2019-09-14 07:19:57 +0300

    Rewrite [141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp] File in: 2019-09-14 07:19:56

--- cccccc/141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp
+++ cccccc/141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp
@@ -1,248 +1,229 @@
 
         
-        
-    {  const char* Name() const override { return 'MaxOperator'; }
-};
+        Example 2 (with KEY):
+DATA  = [1, 2, 3, 4, 5, 6, 7, 8]
+KEY   = [0, 1, 3, 2, 1, 0, 1, 0]
+RANGES = [
+  [
+    [2, 4],
+    [0, 2],
+  ],
+  [
+    [0, 0],
+    [6, 2],
+  ]
+]
+lengths = [4, 2]
+OUTPUT[0] = [[6, 5, 4, 3], [0, 0, 0, 0]]
+OUTPUT[1] = [[1, 2], [8, 7]]
     
-    Status WriteBatchBase::SingleDelete(ColumnFamilyHandle* column_family,
-                                    const SliceParts& key) {
-  std::string key_buf;
-  Slice key_slice(key, &key_buf);
-  return SingleDelete(column_family, key_slice);
+    #ifdef _WIN32
+#include 'windows.hpp'
+#endif
+    
+    template <typename T1, typename T2, typename T3>
+void PrintTo(const ::std::tr1::tuple<T1, T2, T3>& t, ::std::ostream* os) {
+  PrintTupleTo(t, os);
 }
     
-    #include 'rocksdb/status.h'
+      // Returns true iff the current test has a (either fatal or
+  // non-fatal) failure.
+  static bool HasFailure() { return HasFatalFailure() || HasNonfatalFailure(); }
     
-      // atomic write
-  WriteBatch batch;
-  batch.Put(handles[0], Slice('key2'), Slice('value2'));
-  batch.Put(handles[1], Slice('key3'), Slice('value3'));
-  batch.Delete(handles[0], Slice('key'));
-  s = db->Write(WriteOptions(), &batch);
-  assert(s.ok());
+    // Makes sure this header is not included before gtest.h.
+#ifndef GTEST_INCLUDE_GTEST_GTEST_H_
+# error Do not include gtest_pred_impl.h directly.  Include gtest.h instead.
+#endif  // GTEST_INCLUDE_GTEST_GTEST_H_
     
-      Status s = OptimisticTransactionDB::Open(options, kDBPath, &txn_db);
-  assert(s.ok());
-  db = txn_db->GetBaseDB();
+      // Compares two wide C strings.  Returns true iff they have the same
+  // content.
+  //
+  // Unlike wcscmp(), this function can handle NULL argument(s).  A
+  // NULL C string is considered different to any non-NULL C string,
+  // including the empty string.
+  static bool WideCStringEquals(const wchar_t* lhs, const wchar_t* rhs);
     
+        Collection* getCollection() const;
+    void setCollection(Collection* oplog);
+    void resetCollection();
     
-    {  // close DB
-  for (auto* handle : handles) {
-    delete handle;
-  }
-  delete db;
-}
-
-    
-    // Returns the current memory usage of the specified DB instances.
-class MemoryUtil {
- public:
-  enum UsageType : int {
-    // Memory usage of all the mem-tables.
-    kMemTableTotal = 0,
-    // Memory usage of those un-flushed mem-tables.
-    kMemTableUnFlushed = 1,
-    // Memory usage of all the table readers.
-    kTableReadersTotal = 2,
-    // Memory usage by Cache.
-    kCacheTotal = 3,
-    kNumUsageTypes = 4
-  };
+        // Note that we only apply the TransientTxnError label if the 'autocommit' field is present in
+    // the session options. When present, 'autocommit' will always be false, so we don't check its
+    // value.
+    if (sessionOptions.getAutocommit() &&
+        isTransientTransactionError(code,
+                                    hasWriteConcernError,
+                                    commandName == 'commitTransaction' ||
+                                        commandName == 'coordinateCommitTransaction')) {
+        // An error code for which isTransientTransactionError() is true indicates a transaction
+        // failure with no persistent side effects.
+        labelArray << txn::TransientTxnErrorFieldName;
     }
     
-    // Options to use when starting an Optimistic Transaction
-struct OptimisticTransactionOptions {
-  // Setting set_snapshot=true is the same as calling SetSnapshot().
-  bool set_snapshot = false;
-    }
+        // Build an aggregation pipeline that performs the counting. We add stages that satisfy the
+    // query, skip and limit before finishing with the actual $count stage.
+    BSONArrayBuilder pipelineBuilder(aggregationBuilder.subarrayStart('pipeline'));
     
-    const Rect Rect::ZERO = Rect(0, 0, 0, 0);
+        //
+    // Factory methods.
+    //
+    // On success, return a new PlanExecutor, owned by the caller.
+    //
+    // Passing YIELD_AUTO to any of these factories will construct a yielding executor which
+    // may yield in the following circumstances:
+    //   - During plan selection inside the call to make().
+    //   - On any call to getNext().
+    //   - On any call to restoreState().
+    //   - While executing the plan inside executePlan().
+    //
+    // If auto-yielding is enabled, a yield during make() may result in the PlanExecutor being
+    // killed, in which case this method will return a non-OK status.
+    //
     
-    Waves3D* Waves3D::create(float duration, const Size& gridSize, unsigned int waves, float amplitude)
-{
-    Waves3D *action = new (std::nothrow) Waves3D();
-    }
-    
-    
-    {private:
-    CC_DISALLOW_COPY_AND_ASSIGN(Waves3D);
-};
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-        /**
-     * get vertex count
-     * @return number of vertices
-     */
-    unsigned int getVertCount() const;
-    
-    /**
-     * get triangles count
-     * @return number of triangles
-     */
-    unsigned int getTrianglesCount() const;
-    
-    class StencilStateManager;
-/**
- *  @addtogroup _2d
- *  @{
- */
-/** ClippingNode is a subclass of Node.
- * It draws its content (children) clipped using a stencil.
- * The stencil is an other Node that will not be drawn.
- * The clipping is done using the alpha part of the stencil (adjusted with an alphaThreshold).
- */
-class CC_DLL ClippingNode : public Node
-{
-public:
-    /** Creates and initializes a clipping node without a stencil.
-     *
-     * @return An autorelease ClippingNode.
-     */
-    static ClippingNode* create();
-    
-    /** Creates and initializes a clipping node with an other node as its stencil.
-     * The stencil node will be retained.
-     * @param stencil The stencil node.
-     */
-    static ClippingNode* create(Node *stencil);
-    }
-    }
+        BSONObj query = fromjson('{$text:{$search:\'awesome\', $diacriticSensitive: 0}}');
+    StatusWithMatchExpression result =
+        ExtensionsCallbackReal(&_opCtx, &_nss).parseText(query.firstElement());
     
     
     {
-    {        float scaleX = _scaleX;
-        float scaleY = _scaleY;
-        Node *parent = this->getParent();
-        while (parent) {
-            scaleX *= parent->getScaleX();
-            scaleY *= parent->getScaleY();
-            parent = parent->getParent();
-        }
-        
-        const Point pos = convertToWorldSpace(Point(_clippingRegion.origin.x, _clippingRegion.origin.y));
-        GLView* glView = Director::getInstance()->getOpenGLView();
-        glView->setScissorInPoints(pos.x,
-                                   pos.y,
-                                   _clippingRegion.size.width * scaleX,
-                                   _clippingRegion.size.height * scaleY);
+    {        BSONElement a = fixed['a'];
+        ASSERT(o['a'].type() == bsonTimestamp);
+        ASSERT(o['a'].timestampValue() == 0);
+        ASSERT(a.type() == bsonTimestamp);
+        ASSERT(a.timestampValue() > 0);
     }
+};
+    
+            PlanStage::StageState countState = count->work(&wsid);
+    
+    
+    {                if (minCount == 0) {
+                    fp = StateSave(fp, loopLoc+1, status);
+                }
+                if (maxCount == -1) {
+                    fp->fExtra[opValue+1] = fp->fInputIdx;   //  For loop breaking.
+                } else if (maxCount == 0) {
+                    fp = (REStackFrame *)fStack->popFrame(fFrameSize);
+                }
+            }
+            break;
+    
+    void RemoveTransliterator::handleTransliterate(Replaceable& text, UTransPosition& index,
+                                               UBool /*isIncremental*/) const {
+    // Our caller (filteredTransliterate) has already narrowed us
+    // to an unfiltered run.  Delete it.
+    UnicodeString empty;
+    text.handleReplaceBetween(index.start, index.limit, empty);
+    int32_t len = index.limit - index.start;
+    index.contextLimit -= len;
+    index.limit -= len;
+}
+U_NAMESPACE_END
+    
+    int32_t RegexPattern::groupNumberFromName(const char *groupName, int32_t nameLength, UErrorCode &status) const {
+    if (U_FAILURE(status)) {
+        return 0;
+    }
+    UnicodeString name(groupName, nameLength, US_INV);
+    return groupNumberFromName(name, status);
 }
     
-    Component::~Component()
+    UBool
+SelectFormat::operator==(const Format& other) const {
+    if (this == &other) {
+        return TRUE;
+    }
+    if (!Format::operator==(other)) {
+        return FALSE;
+    }
+    const SelectFormat& o = (const SelectFormat&)other;
+    return msgPattern == o.msgPattern;
+}
+    
+    class U_I18N_API SharedCalendar : public SharedObject {
+public:
+    SharedCalendar(Calendar *calToAdopt) : ptr(calToAdopt) { }
+    virtual ~SharedCalendar();
+    const Calendar *get() const { return ptr; }
+    const Calendar *operator->() const { return ptr; }
+    const Calendar &operator*() const { return *ptr; }
+private:
+    Calendar *ptr;
+    SharedCalendar(const SharedCalendar &);
+    SharedCalendar &operator=(const SharedCalendar &);
+};
+    
+    namespace aria2 {
+    }
+    
+    void DHTRoutingTable::setTaskQueue(DHTTaskQueue* taskQueue)
 {
+  taskQueue_ = taskQueue;
 }
     
-    #include <glog/logging.h>
-#include <algorithm>
-#include <vector>
-    
-    template <>
-inline uint32_t Fingerprint<128>::shlor32(uint32_t v) {
-  uint32_t out = (uint32_t)(fp_[0] >> 32);
-  fp_[0] = (fp_[0] << 32) | (fp_[1] >> 32);
-  fp_[1] = (fp_[1] << 32) | ((uint64_t)v);
-  return out;
-}
-    
-      if (!(data->flags & FLAG_IO_THREAD_STARTED)) {
-    // Do not start the I/O thread if the constructor has not finished yet
-    return;
-  }
-  if (data->flags & FLAG_DESTROYING) {
-    // Do not restart the I/O thread if we were being destroyed.
-    // If there are more pending messages that need to be flushed the
-    // destructor's stopIoThread() call will handle flushing the messages in
-    // this case.
-    return;
-  }
-    
-      void writeMessage(folly::StringPiece buffer, uint32_t flags = 0) override;
-    
-    template <
-    class KeyT,
-    class ValueT,
-    class Allocator = std::allocator<char>,
-    class ProbeFcn = AtomicHashArrayLinearProbeFcn>
-void testMap() {
-  typedef AtomicHashArray<
-      KeyT,
-      ValueT,
-      std::hash<KeyT>,
-      std::equal_to<KeyT>,
-      Allocator,
-      ProbeFcn>
-      MyArr;
-  auto arr = MyArr::create(150);
-  map<KeyT, ValueT> ref;
-  for (int i = 0; i < 100; ++i) {
-    auto e = createEntry<KeyT, ValueT>(i);
-    auto ret = arr->insert(e);
-    EXPECT_EQ(!ref.count(e.first), ret.second); // succeed iff not in ref
-    ref.insert(e);
-    EXPECT_EQ(ref.size(), arr->size());
-    if (ret.first == arr->end()) {
-      EXPECT_FALSE('AHA should not have run out of space.');
-      continue;
+        DHTRoutingTableDeserializer deserializer(family);
+    const std::string& dhtFile = e->getOption()->get(
+        family == AF_INET ? PREF_DHT_FILE_PATH : PREF_DHT_FILE_PATH6);
+    try {
+      deserializer.deserialize(dhtFile);
+      localNode = deserializer.getLocalNode();
     }
-    EXPECT_EQ(e.first, ret.first->first);
-    EXPECT_EQ(ref.find(e.first)->second, ret.first->second);
-  }
+    catch (RecoverableException& e) {
+      A2_LOG_ERROR_EX(
+          fmt('Exception caught while loading DHT routing table from %s',
+              dhtFile.c_str()),
+          e);
+    }
+    if (!localNode) {
+      localNode = std::make_shared<DHTNode>();
     }
     
-    class FifoSemaphoreTest : public testing::Test {};
+      void setMessageFactory(DHTMessageFactory* factory);
     
-      /** Calls a user-defined auxiliary function if any, and releases
-   *  permission for the current thread to perform inter-thread
-   *  communication. The bool parameter indicates the success of the
-   *  shared access (if conditional, true otherwise). */
-  static void afterSharedAccess(bool success);
-    
-      /**
-   * Try to combine a task as a combined critical section untill the given time
-   *
-   * Like the other try_lock() mehtods, this is allowed to fail spuriously,
-   * and is not guaranteed to return true even when the mutex is currently
-   * unlocked.
-   *
-   * Note that this does not necessarily have the same performance
-   * characteristics as the non-timed version of the combine method.  If
-   * performance is critical, use that one instead
-   */
-  template <
-      typename Rep,
-      typename Period,
-      typename Task,
-      typename ReturnType = decltype(std::declval<Task&>()())>
-  folly::Optional<ReturnType> try_lock_combine_for(
-      const std::chrono::duration<Rep, Period>& duration,
-      Task task);
-    
-    // this test makes sure that the coroutine is destroyed properly
-TEST(Expected, CoroutineCleanedUp) {
-  int count_dest = 0;
-  auto r = [&]() -> Expected<int, Err> {
-    SCOPE_EXIT {
-      ++count_dest;
-    };
-    auto x = co_await Expected<int, Err>(makeUnexpected(Err::badder()));
-    ADD_FAILURE() << 'Should not be resuming';
-    co_return x;
-  }();
-  EXPECT_FALSE(r.hasValue());
-  EXPECT_EQ(1, count_dest);
+    bool DHTTokenTracker::validateToken(const std::string& token,
+                                    const unsigned char* infoHash,
+                                    const std::string& ipaddr,
+                                    uint16_t port) const
+{
+  for (auto& elem : secret_) {
+    if (generateToken(infoHash, ipaddr, port, elem) == token) {
+      return true;
+    }
+  }
+  return false;
 }
+    
+    namespace aria2 {
+    }
+    
+    
+    {  bool FullMergeV2(const MergeOperationInput& merge_in,
+                   MergeOperationOutput* merge_out) const override {
+    // Put basically only looks at the current/latest value
+    assert(!merge_in.operand_list.empty());
+    merge_out->existing_operand = merge_in.operand_list.back();
+    return true;
+  }
+};
+    
+      // drop column family
+  s = db->DropColumnFamily(handles[1]);
+  assert(s.ok());
+    
+      // Commit.  Since the snapshot was advanced, the write done outside of the
+  // transaction does not prevent this transaction from Committing.
+  s = txn->Commit();
+  assert(s.ok());
+  delete txn;
+  // Clear snapshot from read options since it is no longer valid
+  read_options.snapshot = nullptr;
+    
+    #pragma once
+    
+    #include 'rocksdb/env.h'
+#include 'rocksdb/slice.h'
+#include 'rocksdb/statistics.h'
+#include 'rocksdb/status.h'
+    
+    #include 'rocksdb/cache.h'
+#include 'rocksdb/db.h'
