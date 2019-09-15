8bf8efcbc06eaf4ca6c3156dc5bdbc6852089490
SUNsung <weatherius@gmail.com>
2019-09-15 08:07:27 +0300

    Rewrite [02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb] File in: 2019-09-15 08:07:26

--- ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
+++ ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
@@ -1,194 +1,105 @@
 
         
-            it 'returns a Glyphicon icon element with an addidional class' do
-      icon = icon_tag('glyphicon-help', class: 'text-info')
-      expect(icon).to be_html_safe
-      expect(Nokogiri(icon).at('span.glyphicon.glyphicon-help.text-info')).to be_a Nokogiri::XML::Element
+          describe '#working' do
+    before do
+      @agent = agents(:jane_website_agent)
     end
     
-    describe JobsHelper do
-  let(:job) { Delayed::Job.new }
-    
-    describe ScenarioHelper do
-  let(:scenario) { users(:bob).scenarios.build(name: 'Scene', tag_fg_color: '#AAAAAA', tag_bg_color: '#000000') }
-    
-        it 'can be turned off' do
-      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+        it 'respects an environment variable that specifies a path or URL to a different scenario' do
       stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'false' }
-      DefaultScenarioImporter.import(user)
+      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
     end
     
-      it_behaves_like AgentControllerConcern
-    
-        SPLIT_INTS = /(?<=\d)\.(?=[\s\d])/.freeze
-    
-        def as_json
-      @pages
+    describe LiquidMigrator do
+  describe 'converting JSONPath strings' do
+    it 'should work' do
+      expect(LiquidMigrator.convert_string('$.data', true)).to eq('{{data}}')
+      expect(LiquidMigrator.convert_string('$.data.test', true)).to eq('{{data.test}}')
+      expect(LiquidMigrator.convert_string('$first_title', true)).to eq('{{first_title}}')
     end
     
-            css('code code').each do |node|
-          node.before(node.children).remove
+            it 'creates one event per file' do
+          @checker.options['output'] = 'event_per_file'
+          expect { @checker.receive([with_headers]) }.to change(Event, :count).by(1)
+          expect(Event.last.payload).to eq(@checker.options['data_key'] => [{'one'=>'1', 'two'=>'2'}, {'one'=>'2', 'two'=>'3'}])
+        end
+      end
+    
+      def initialize(info = {})
+    super(merge_info(info,
+      'Name'          => 'Bind TCP Stager (RC4 Stage Encryption, Metasm)',
+      'Description'   => 'Connect back to the attacker',
+      'Author'        => ['hdm', 'skape', 'sf', 'mihi', 'max3raza', 'RageLtMan'],
+      'License'       => MSF_LICENSE,
+      'Platform'      => 'win',
+      'Arch'          => ARCH_X64,
+      'Handler'       => Msf::Handler::BindTcp,
+      'Convention'    => 'sockrdi',
+      'Stager'        => { 'RequiresMidstager' => false }
+      ))
+  end
+end
+
+    
+      def read(data)
+    require 'pdf-reader'
+    
+    class MetasploitModule < Msf::Exploit::Remote
+  Rank = ManualRanking
+    
+        found_browser_path
+  end
+    
+        report_note(host: host,
+        type: 'host.persistance.cleanup',
+        data: {
+          local_id: session.sid,
+          stype: session.type,
+          desc: session.info,
+          platform: session.platform,
+          via_payload: session.via_payload,
+          via_exploit: session.via_exploit,
+          created_at: Time.now.utc,
+          commands: @clean_up_rc
+        })
+  end
+    
+          def test_on_event_hash_return4
+        ev = WIN32OLE_EVENT.new(@db)
+        ev.on_event('WillConnect'){|*args|
+          {'return' => 1, 'ConnectionString' => CONNSTR}
+        }
+        @db.connectionString = 'XXX'
+        @db.open
+        assert(true)
+      end
+    
+    require 'racc/raccs'
+    
+        ScratchPad.recorded.should == [49, 50, 51, 52, 53, 97, 98, 99, 100, 101]
+  end
+    
+        # Loops through the list of category pages and processes each one.
+    def write_category_indexes
+      if self.layouts.key? 'category_index'
+        dir = self.config['category_dir'] || 'categories'
+        self.categories.keys.each do |category|
+          self.write_category_index(File.join(dir, category.to_url), category)
         end
     
-            location_badge = at_css('.location-badge')
-        if location_badge && doc.last_element_child != location_badge
-          doc.last_element_child.after(location_badge)
-        end
+    module Jekyll
     
-            subtitle = at_css('.hero-subtitle').try(:content)
-        breadcrumbs = css('.breadcrumbs li').map(&:content)[2..-2]
+      def safe_params
+    params.require(:ip_pool).permit(:name, :default)
+  end
     
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
+      def create
+    @smtp_endpoint = @server.smtp_endpoints.build(safe_params)
+    if @smtp_endpoint.save
+      flash[:notice] = params[:return_notice] if params[:return_notice].present?
+      redirect_to_with_json [:return_to, [organization, @server, :smtp_endpoints]]
     else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+      render_form_errors 'new', @smtp_endpoint
     end
   end
-    
-        def password_change(record, opts={})
-      devise_mail(record, :password_change, opts)
-    end
-  end
-end
-
-    
-          def headers_for(action, opts)
-        headers = {
-          subject: subject_for(action),
-          to: resource.email,
-          from: mailer_sender(devise_mapping),
-          reply_to: mailer_reply_to(devise_mapping),
-          template_path: template_paths,
-          template_name: action
-        }.merge(opts)
-    
-    require 'devise/hooks/timeoutable'
-    
-    require 'utils/bottles'
-    
-              def install
-            xcodebuild '-project', 'meow.xcodeproject'
-            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ xcodebuild should be passed an explicit \'SYMROOT\'
-          end
-        end
-      RUBY
-    end
-    
-          def toolchain_path
-        Pathname.new('#{prefix}/Toolchains/XcodeDefault.xctoolchain')
-      end
-    
-        let(:follow)  { double(account: account, errors: errors) }
-    let(:errors)  { double(add: nil) }
-    let(:account) { double(nil?: _nil, local?: local, following_count: 0, followers_count: 0) }
-    let(:_nil)    { true }
-    let(:local)   { false }
-    
-        def quote(field)
-      if @force_quotes
-        quote_field(field)
-      else
-        if field.nil?  # represent +nil+ fields as empty unquoted fields
-          ''
-        else
-          field = String(field)  # Stringify fields
-          # represent empty fields as empty quoted fields
-          if (@quote_empty and field.empty?) or @quotable_pattern.match?(field)
-            quote_field(field)
-          else
-            field  # unquoted field
-          end
-        end
-      end
-    end
-  end
-end
-
-    
-      def test_sqrt
-    assert_equal 1, CMath.sqrt(1)
-    assert_equal CMath.sqrt(1i), CMath.sqrt(1.0i), '[ruby-core:31672]'
-    assert_equal Complex(0,2), CMath.sqrt(-4.0)
-    assert_equal Complex(0,2), CMath.sqrt(-4)
-    assert_equal Complex(0,2), CMath.sqrt(Rational(-4))
-    assert_equal Complex(0,3), CMath.sqrt(-9.0)
-    assert_equal Complex(0,3), CMath.sqrt(-9)
-    assert_equal Complex(0,3), CMath.sqrt(Rational(-9))
-    assert_in_delta (1.272019649514069+0.7861513777574233i), CMath.sqrt(1+2i)
-    assert_in_delta 3.0i, CMath.sqrt(-9)
-    assert_raise_with_message(TypeError, 'Numeric Number required') { CMath.sqrt('1') }
-  end
-    
-      it 'increments position before calling the block' do
-    gz = Zlib::GzipReader.new @io
-    
-    end
-
-    
-    describe 'GzipReader#ungetbyte' do
-  before :each do
-    @data = '12345abcde'
-    @zip = [31, 139, 8, 0, 44, 220, 209, 71, 0, 3, 51, 52, 50, 54, 49, 77,
-            76, 74, 78, 73, 5, 0, 157, 5, 0, 36, 10, 0, 0, 0].pack('C*')
-    @io = StringIO.new @zip
-  end
-    
-        private
-    
-      # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-    
-    get '/stream', :provides => 'text/event-stream' do
-  stream :keep_open do |out|
-    connections << out
-    out.callback { connections.delete(out) }
-  end
-end
-    
-          # Creates a masked version of the authenticity token that varies
-      # on each request. The masking is used to mitigate SSL attacks
-      # like BREACH.
-      def mask_token(token)
-        token = decode_token(token)
-        one_time_pad = SecureRandom.random_bytes(token.length)
-        encrypted_token = xor_byte_strings(one_time_pad, token)
-        masked_token = one_time_pad + encrypted_token
-        encode_token(masked_token)
-      end
-    
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   Cookie Tossing
-    # Supported browsers:: all
-    # More infos::         https://github.com/blog/1466-yummy-cookies-across-domains
-    #
-    # Does not accept HTTP requests if the HTTP_COOKIE header contains more than one
-    # session cookie. This does not protect against a cookie overflow attack.
-    #
-    # Options:
-    #
-    # session_key:: The name of the session cookie (default: 'rack.session')
-    class CookieTossing < Base
-      default_reaction :deny
-    
-            modes       = Array options[:escape]
-        @escaper    = options[:escaper]
-        @html       = modes.include? :html
-        @javascript = modes.include? :javascript
-        @url        = modes.include? :url
-    
-          def react_and_close(env, body)
-        reaction = react(env)
-    
-          get '/', {}, 'HTTP_COOKIE' => '_session=EVIL_SESSION_TOKEN; _session=SESSION_TOKEN'
-      expect(last_response).not_to be_ok
-    end
-  end
-end
-
-    
-      it_behaves_like 'any rack application'
