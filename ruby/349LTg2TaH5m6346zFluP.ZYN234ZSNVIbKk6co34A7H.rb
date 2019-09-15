49c65b91d453bb2ac08900166680c7eb9e33ad84
SUNsung <weatherius@gmail.com>
2019-09-15 07:10:15 +0300

    Rewrite [349LTg2TaH5m6346zFluP.ZYN234ZSNVIbKk6co34A7H.rb] File in: 2019-09-15 07:10:15

--- ruby/349LTg2TaH5m6346zFluP.ZYN234ZSNVIbKk6co34A7H.rb
+++ ruby/349LTg2TaH5m6346zFluP.ZYN234ZSNVIbKk6co34A7H.rb
@@ -1,77 +1,101 @@
 
         
-                @current_document.related_posts
+        require 'active_support'
+require 'active_support/core_ext/object/blank'
+require 'minitest/autorun'
+    
+        class InvertibleRevertMigration < SilentMigration
+      def change
+        revert do
+          create_table('horses') do |t|
+            t.column :content, :text
+            t.column :remind_at, :datetime
+          end
+        end
       end
-      attr_writer :current_document
-    
-    def graceful_require
-  Jekyll::External.require_with_graceful_fail('json')
-  JSON.pretty_generate(DATA)
-end
-    
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-        it 'shows reblogs by default' do
-      recipient.follow!(sender)
-      is_expected.to change(Notification, :count)
     end
     
-    class ActivityPub::DistributionWorker
-  include Sidekiq::Worker
-  include Payloadable
+      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
+  p.option 'destination', '-d', '--destination [DIR]',
+    'Destination directory (defaults to ./_site)'
+  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
+  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
+    'Plugins directory (defaults to ./_plugins)'
+  p.option 'layouts_dir', '--layouts DIR', String,
+    'Layouts directory (defaults to ./_layouts)'
+  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
     
-      def signed_payload
-    @signed_payload ||= Oj.dump(serialize_payload(@account, ActivityPub::UpdateSerializer, signer: @account, sign_with: @options[:sign_with]))
+              theme.create!
+          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
+                             ' is ready for you in #{theme.path.to_s.cyan}!'
+          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
+        end
+        # rubocop:enable Metrics/AbcSize
+      end
+    end
   end
 end
 
     
-    describe Settings::Preferences::NotificationsController do
-  render_views
+      let(:unknown_object_json) do
+    {
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      id: 'https://example.com/actor/hello-world',
+      type: 'Note',
+      attributedTo: 'https://example.com/actor',
+      content: 'Hello world',
+      to: 'http://example.com/followers',
+    }
+  end
     
-      class_methods do
-    def ldap_get_user(attributes = {})
-      resource = joins(:account).find_by(accounts: { username: attributes[Devise.ldap_uid.to_sym].first })
+      # How the label text should be generated altogether with the required text.
+  config.label_text = lambda { |label, required, explicit_label| '#{label} #{required}' }
     
-        it 'makes followers unfollow old account' do
-      expect(follower.following?(old_account)).to be false
-    end
+          it 'should have the correct 'before_uninstall' trigger script' do
+        insist { @rpm.tags[:triggername][2] } == 'test'
+        insist { @rpm.tags[:triggerversion][2] } == ''
+        # This specific check is broken in newer versions of rpm/rpmbuild? -Jordan
+        #insist { @rpm.tags[:triggerflags][2] & (1 << 17)} == ( 1 << 17) # See FPM::Package::RPM#rpm_get_trigger_type
+        #insist { @rpm.tags[:triggerindex][2] } == 2
+        insist { @rpm.tags[:triggerscriptprog][2] } == '/bin/sh'
+        insist { @rpm.tags[:triggerscripts][2] } == '#!/bin/sh\necho before_uninstall trigger executed'
+      end
     
-      context 'when a matching undo has been received first' do
-    let(:undo_json) do
-      {
-        '@context': 'https://www.w3.org/ns/activitystreams',
-        id: 'bar',
-        type: 'Undo',
-        actor: ActivityPub::TagManager.instance.uri_for(sender),
-        object: json,
-      }.with_indifferent_access
-    end
+        before do
+      # output a package, use it as the input, set the subject to that input
+      # package. This helps ensure that we can write and read packages
+      # properly.
+      # The target file must not exist.
     
-          explicit_path = ::File.join(temp_path, LOGSTASH_DIR)
-      dependencies_path = ::File.join(temp_path, DEPENDENCIES_DIR)
+        input.conflicts += conflicts
+    input.dependencies += dependencies
+    input.provides += provides
+    input.replaces += replaces
+    input.config_files += config_files
+    input.directories += directories
     
-          PluginManager.ui.info('Install successful')
-    rescue ::Bundler::BundlerError => e
-      raise PluginManager::InstallError.new(e), 'An error occurred went installing plugins'
+      # hash of scripts for maintainer/package scripts (postinstall, etc)
+  #
+  # The keys are :before_install, etc
+  # The values are the text to use in the script.
+  attr_accessor :scripts
+    
+        # control tar.
+    begin
+      write_pkginfo(control_path)
+      write_control_scripts(control_path)
+      tar_path(control_path, controltar_path)
     ensure
-      FileUtils.rm_rf(uncompressed_path) if uncompressed_path && Dir.exist?(uncompressed_path)
+      FileUtils.rm_r(control_path)
     end
     
-              it 'successfully install the plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-dns')
-          end
-        end
+      def self.default_prefix
+    npm_prefix = safesystemout('npm', 'prefix', '-g').chomp
+    if npm_prefix.count('\n') > 0
+      raise FPM::InvalidPackageConfiguration, '`npm prefix -g` returned unexpected output.'
+    elsif !File.directory?(npm_prefix)
+      raise FPM::InvalidPackageConfiguration, '`npm prefix -g` returned a non-existent directory'
+    end
+    logger.info('Setting default npm install prefix', :prefix => npm_prefix)
+    npm_prefix
+  end
