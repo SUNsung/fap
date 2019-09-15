fe6bc89294d8cdec4c4913c4f6f4f094394862ec
SUNsung <weatherius@gmail.com>
2019-09-15 03:31:03 +0300

    Rewrite [32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py] File in: 2019-09-15 03:31:02

--- pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
+++ pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
@@ -1,144 +1,112 @@
 
         
-            @classmethod
-    def clear_expired(cls):
-        pass
+            def test_binary_suppresses_when_not_terminal_but_pretty(self, httpbin):
+        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
+        r = http('--pretty=all', 'GET', httpbin + '/bytes/1024?seed=1', env=env)
+        assert BINARY_SUPPRESSED_NOTICE.decode() in r
+    
+    <https://github.com/Homebrew/homebrew-core/blob/master/Formula/httpie.rb>
+    
+        def iter_lines(self, chunk_size: int) -> Iterable[bytes]:
+        '''Return an iterator over the body yielding (`line`, `line_feed`).'''
+        raise NotImplementedError()
+    
+        '''
+    mime, encoding = mimetypes.guess_type(filename, strict=False)
+    if mime:
+        content_type = mime
+        if encoding:
+            content_type = '%s; charset=%s' % (mime, encoding)
+        return content_type
 
     
-        @classmethod
-    def get_session_store_class(cls):
-        raise NotImplementedError
+    # MCP 2.x version patten for location (datacenter) names.
+#
+# Note that this is not a totally reliable way of determining MCP version.
+# Unfortunately, libcloud's NodeLocation currently makes no provision for extended properties.
+# At some point we may therefore want to either enhance libcloud or enable overriding mcp_version
+# by specifying it in the module parameters.
+MCP_2_LOCATION_NAME_PATTERN = re.compile(r'.*MCP\s?2.*')
     
-    
-@html_safe
-class BoundWidget:
-    '''
-    A container class used for iterating over widgets. This is useful for
-    widgets that have choices. For example, the following can be used in a
-    template:
-    
-    
-def to_native_string(string, encoding='ascii'):
-    '''Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
-    '''
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        if is_py2:
-            out = string.encode(encoding)
+            if v2_api_token and clc_alias:
+            self.clc._LOGIN_TOKEN_V2 = v2_api_token
+            self.clc._V2_ENABLED = True
+            self.clc.ALIAS = clc_alias
+        elif v2_api_username and v2_api_passwd:
+            self.clc.v2.SetCredentials(
+                api_username=v2_api_username,
+                api_passwd=v2_api_passwd)
         else:
-            out = string.decode(encoding)
+            return self.module.fail_json(
+                msg='You must set the CLC_V2_API_USERNAME and CLC_V2_API_PASSWD '
+                    'environment variables')
     
-        @get_item_parameters
-    def test_get(self, key, value):
-        assert self.lookup_dict.get(key) == value
+    
+def absent(dest, username, check_mode):
+    ''' Ensures user is absent
+    
+    
+class LdapPasswd(LdapGeneric):
+    def __init__(self, module):
+        LdapGeneric.__init__(self, module)
+    
+        except Exception as ex:
+        module.fail_json(msg=ex.message)
+    
+    
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+    
+            module.exit_json(changed=changed, **response)
+    except Exception as e:
+        module.fail_json(msg=str(e))
+    
+        if not has_boolean_value(module, name):
+        module.fail_json(msg='SELinux boolean %s does not exist.' % name)
+    
+    print(__doc__)
+    
+    plt.matshow(fit_data, cmap=plt.cm.Blues)
+plt.title('After biclustering; rearranged to show biclusters')
+    
+    # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+    
+    # #############################################################################
+# Compute clustering
+print('Compute unstructured hierarchical clustering...')
+st = time.time()
+ward = AgglomerativeClustering(n_clusters=6, linkage='ward').fit(X)
+elapsed_time = time.time() - st
+label = ward.labels_
+print('Elapsed time: %.2fs' % elapsed_time)
+print('Number of points: %i' % label.size)
+    
+    plt.subplot(224)
+plt.scatter(X_train_r[:, 1], Y_train_r[:, 1], label='train',
+            marker='o', c='b', s=25)
+plt.scatter(X_test_r[:, 1], Y_test_r[:, 1], label='test',
+            marker='o', c='r', s=25)
+plt.xlabel('x scores')
+plt.ylabel('y scores')
+plt.title('Comp. 2: X vs Y (test corr = %.2f)' %
+          np.corrcoef(X_test_r[:, 1], Y_test_r[:, 1])[0, 1])
+plt.xticks(())
+plt.yticks(())
+plt.legend(loc='best')
+    
+    plt.show()
 
     
-    
-def info():
-    '''Generate information for a bug report.'''
-    try:
-        platform_info = {
-            'system': platform.system(),
-            'release': platform.release(),
-        }
-    except IOError:
-        platform_info = {
-            'system': 'Unknown',
-            'release': 'Unknown',
-        }
-    
-        @staticmethod
-    def _encode_params(data):
-        '''Encode parameters in a piece of data.
-    
-    Some codes have multiple names, and both upper- and lower-case versions of
-the names are allowed. For example, ``codes.ok``, ``codes.OK``, and
-``codes.okay`` all correspond to the HTTP status code 200.
-'''
-    
-            #: Dictionary mapping protocol or protocol and host to the URL of the proxy
-        #: (e.g. {'http': 'foo.bar:3128', 'http://host.name': 'foo.bar:4012'}) to
-        #: be used on each :class:`Request <Request>`.
-        self.proxies = {}
-    
-        def test_HTTP_307_ALLOW_REDIRECT_POST(self, httpbin):
-        r = requests.post(httpbin('redirect-to'), data='test', params={'url': 'post', 'status_code': 307})
-        assert r.status_code == 200
-        assert r.history[0].status_code == 307
-        assert r.history[0].is_redirect
-        assert r.json()['data'] == 'test'
-    
-    # For 'manual' documents, if this is true, then toplevel headings are parts,
-# not chapters.
-# latex_use_parts = False
-    
-        async def real_ip_startup(app):
-        '''Startup of real ip.'''
-        app.middlewares.insert(0, mock_real_ip)
-    
-    
-def gradient_loss(y_true, y_pred):
-    '''
-    Calculates the first and second order gradient difference between pixels of an image in the
-    x and y dimensions. These gradients are then compared between the ground truth and the
-    predicted image and the difference is taken. The difference used is a smooth L1 norm
-    (approximate to MAE but differable at zero) When used as a loss, its minimization will result
-    in predicted images approaching the samelevel of sharpness / blurriness as the ground truth.
-    
-        def start(self):
-        ''' Run the processing pool '''
-        logging.debug('Pooling Processes: (target: '%s', args: %s, kwargs: %s)',
-                      self._name, self._args, self._kwargs)
-        for idx in range(self.procs):
-            logger.debug('Adding process %s of %s to mp.Pool '%s'',
-                         idx + 1, self.procs, self._name)
-            self.pool.apply_async(self._method, args=self._args, kwds=self._kwargs)
-            _launched_processes.add(self.pool)
-        logging.debug('Pooled Processes: '%s'', self._name)
-    
-        def update_hashes(self):
-        ''' Update hashes to alignments '''
-        logger.info('Updating hashes to alignments...')
-        updated = 0
-        for frame, hashes in self.face_hashes.items():
-            if not self.alignments.frame_exists(frame):
-                logger.warning('Frame not found in alignments file. Skipping: '%s'', frame)
-                continue
-            if not self.alignments.frame_has_faces(frame):
-                logger.warning('Frame does not have faces. Skipping: '%s'', frame)
-                continue
-            existing = [face.get('hash', None)
-                        for face in self.alignments.get_faces_in_frame(frame)]
-            if any(hsh not in existing for hsh in list(hashes.values())):
-                self.alignments.add_face_hashes(frame, hashes)
-                updated += 1
-        return updated
-
-    
-                if offscreen:
-    
-            if self.axis is None:
-            shape = (1,)
-        else:
-            shape = (input_shape[self.axis],)
-    
-        # partial derivatives to the checkpointed tensors and xs
-    ops_to_copy = fast_backward_ops(seed_ops=[y.op for y in ys],
-                                    stop_at_ts=checkpoints, within_ops=fwd_ops)
-    debug_print('Found {} ops to copy within fwd_ops {}, seed {}, stop_at {}'.format(
-        len(ops_to_copy), fwd_ops, [r.op for r in ys], checkpoints))
-    debug_print('ops_to_copy = {}'.format(ops_to_copy))
-    debug_print('Processing list {}'.format(ys))
-    _, info = ge.copy_with_input_replacements(ge.sgv(ops_to_copy), {})
-    for origin_op, op in info._transformed_ops.items():
-        op._set_device(origin_op.node_def.device)
-    copied_ops = info._transformed_ops.values()
-    debug_print('Copied {} to {}'.format(ops_to_copy, copied_ops))
-    ge.reroute_ts(checkpoints_disconnected.values(),
-                  checkpoints_disconnected.keys(),
-                  can_modify=copied_ops)
-    debug_print('Rewired {} in place of {} restricted to {}'.format(
-        checkpoints_disconnected.values(), checkpoints_disconnected.keys(), copied_ops))
+            # put all lines in the file into a Python list
+        strings = f.readlines()
+        
+        # above line leaves trailing newline characters; strip them out
+        strings = [x.strip(u'\n') for x in strings]
+        
+        # remove empty-lines and comments
+        strings = [x for x in strings if x and not x.startswith(u'#')]
+        
+        # insert empty string since all are being removed
+        strings.insert(0, u'')
