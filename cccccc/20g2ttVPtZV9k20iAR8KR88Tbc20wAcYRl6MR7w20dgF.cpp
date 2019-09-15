8836cb9e168e8d005103f9f249d384d9578a91ba
SUNsung <weatherius@gmail.com>
2019-09-15 05:43:35 +0300

    Rewrite [20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp] File in: 2019-09-15 05:43:34

--- cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
+++ cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
@@ -1,266 +1,212 @@
 
         
-        STATISTIC(NumClassDevirt, 'Number of class_method applies devirtualized');
-STATISTIC(NumWitnessDevirt, 'Number of witness_method applies devirtualized');
+        
+    {}  // namespace caffe
     
-        findAnchorsInTables();
-    
-    namespace swift {
-    }
-    
-      /// This class provides a non-trivial .cxx_construct or .cxx_destruct
-  /// implementation.
-  HasCXXStructors      = 0x00004,
-    
-      /// Read a swiftdeps file from \p buffer and return a SourceFileDepGraph if
-  /// successful.
-  Optional<SourceFileDepGraph> static loadFromBuffer(llvm::MemoryBuffer &);
-    
-        UMat tmp(Size(dst_frame1.cols / (1 << src_scale), dst_frame1.rows / (1 << src_scale)), CV_8U);
-    randu(tmp, 0, 255);
-    resize(tmp, dst_frame1, dst_frame1.size(), 0.0, 0.0, INTER_LINEAR_EXACT);
-    resize(tmp, dst_frame2, dst_frame2.size(), 0.0, 0.0, INTER_LINEAR_EXACT);
-    
-    
-    {    bufferNumBits -= numBits;
-    return (buffer >> bufferNumBits) & ((1 << numBits) - 1);
+    template <typename Dtype>
+void Solver<Dtype>::InitTrainNet() {
+  const int num_train_nets = param_.has_net() + param_.has_net_param() +
+      param_.has_train_net() + param_.has_train_net_param();
+  const string field_names = 'net, net_param, train_net, train_net_param';
+  CHECK_GE(num_train_nets, 1) << 'SolverParameter must specify a train net '
+      << 'using one of these fields: ' << field_names;
+  CHECK_LE(num_train_nets, 1) << 'SolverParameter must not contain more than '
+      << 'one of these fields specifying a train_net: ' << field_names;
+  NetParameter net_param;
+  if (param_.has_train_net_param()) {
+    LOG_IF(INFO, Caffe::root_solver())
+        << 'Creating training net specified in train_net_param.';
+    net_param.CopyFrom(param_.train_net_param());
+  } else if (param_.has_train_net()) {
+    LOG_IF(INFO, Caffe::root_solver())
+        << 'Creating training net from train_net file: ' << param_.train_net();
+    ReadNetParamsFromTextFileOrDie(param_.train_net(), &net_param);
+  }
+  if (param_.has_net_param()) {
+    LOG_IF(INFO, Caffe::root_solver())
+        << 'Creating training net specified in net_param.';
+    net_param.CopyFrom(param_.net_param());
+  }
+  if (param_.has_net()) {
+    LOG_IF(INFO, Caffe::root_solver())
+        << 'Creating training net from net file: ' << param_.net();
+    ReadNetParamsFromTextFileOrDie(param_.net(), &net_param);
+  }
+  // Set the correct NetState.  We start with the solver defaults (lowest
+  // precedence); then, merge in any NetState specified by the net_param itself;
+  // finally, merge in any NetState specified by the train_state (highest
+  // precedence).
+  NetState net_state;
+  net_state.set_phase(TRAIN);
+  net_state.MergeFrom(net_param.state());
+  net_state.MergeFrom(param_.train_state());
+  net_param.mutable_state()->CopyFrom(net_state);
+  net_.reset(new Net<Dtype>(net_param));
+  for (int w_idx = 0; w_idx < param_.weights_size(); ++w_idx) {
+    LoadNetWeights(net_, param_.weights(w_idx));
+  }
 }
     
-        //
-    // Number of bits in our acceleration table. Should match all
-    // codes up to TABLE_LOOKUP_BITS in length.
-    //
-    
-    template <>
-IMF_EXPORT
-void FloatVectorAttribute::readValueFrom
-    (OPENEXR_IMF_INTERNAL_NAMESPACE::IStream &, int, int);
-    
-    // file handling classes
-class OutputFile;
-class TiledInputFile;
-class ScanLineInputFile;
-class InputFile;
-class TiledOutputFile;
-class DeepScanLineInputFile;
-class DeepScanLineOutputFile;
-class DeepTiledInputFile;
-class DeepTiledOutputFile;
-class AcesInputFile;
-class AcesOutputFile;
-class TiledInputPart;
-class TiledInputFile;
-class TileOffsets;
-    
-    OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_ENTER
-    
-    void GenericInputFile::readMagicNumberAndVersionField(OPENEXR_IMF_INTERNAL_NAMESPACE::IStream& is, int& version)
-{
-    //
-    // Read the magic number and the file format version number.
-    // Then check if we can read the rest of this file.
-    //
+    int main(int argc, char** argv) {
+#ifndef GFLAGS_GFLAGS_H_
+  namespace gflags = google;
+#endif
     }
     
-        if (parts == 1)
+    int VPXDecoder::Image::getWidth(int plane) const
+{
+	if (!plane)
+		return w;
+	return ceilRshift(w, chromaShiftW);
+}
+int VPXDecoder::Image::getHeight(int plane) const
+{
+	if (!plane)
+		return h;
+	return ceilRshift(h, chromaShiftH);
+}
+    
+    #define todB_nn(x) todB(x)
+    
+    
+#else
+/* No-op version that performs no synchronization. *_rtcd() is idempotent,
+ * so as long as your platform provides atomic loads/stores of pointers
+ * no synchronization is strictly necessary.
+ */
+    
+    
+    {    // out_of_range.404
+    try
     {
-        if (headers[0].type() == TILEDIMAGE)
-            version |= TILED_FLAG;
+        // try to use a JSON pointer that cannot be resolved
+        json::const_reference ref = j.at('/number/foo'_json_pointer);
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+}
+
+    
+    int main()
+{
+    // create JSON values
+    json object = {{'one', 1}, {'two', 2}};
+    json null;
+    }
+    
+    TEST_F(CarverTests, test_compression_decompression) {
+  auto const test_data_file = getWorkingDir() / 'test.data';
+  writeTextFile(test_data_file, R'raw_text(
+2TItVMSvAY8OFlbYnx1O1NSsuehfNhNiV4Qw4IPP6exA47HVzAlEXZI3blanlAd2
+JSxCUr+3boxWMwsgW2jJPzypSKvfXB9EDbFKiDjVueniBfiAepwta57pZ9tQDnJA
+uRioApcqYSWL14OJrnPQFHel5FpXylmVdIkiz()cT82JsOPZmh56vDn62Kk/mU7V
+RltGAYEpKmi8e71fuB8d/S6Lau{}AmL1153X7E+4d1G1UfiQa7Q02uVjxLLE5FEj
+JTDjVqIQNhi50Pt4J4RVopYzy1AZGwPHLhwFVIPH0s/LmzVW+xbT8/V2UMSzK4XB
+oqADd9Ckcdtplx3k7bcLU[U04j8WWUtUccmB+4e2KS]i3x7WDKviPY/sWy9xFapv
+)raw_text');
+  {
+    auto s = osquery::compress(test_data_file,
+                               getWorkingDir() / fs::path('test.zst'));
+    ASSERT_TRUE(s.ok()) << s.what();
+  }
+  {
+    auto s =
+        osquery::decompress(getWorkingDir() / fs::path('test.zst'),
+                            getWorkingDir() / fs::path('test.data.extract'));
+    ASSERT_TRUE(s.ok()) << s.what();
+  }
+    }
+    
+    TEST_F(PacksTests, test_schedule) {
+  Pack fpack('discovery_pack', getPackWithDiscovery().doc());
+  // Expect a single query in the schedule since one query has an explicit
+  // invalid/fake platform requirement.
+  EXPECT_EQ(fpack.getSchedule().size(), 1U);
+}
+    
+    void Plugin::setName(const std::string& name) {
+  if (!name_.empty() && name != name_) {
+    std::string error = 'Cannot rename plugin ' + name_ + ' to ' + name;
+    throw std::runtime_error(error);
+  }
+    }
+    
+      /// Use the SQL implementation to parse a query string and return details
+  /// (name, type) about the columns.
+  virtual Status getQueryColumns(const std::string& query,
+                                 TableColumns& columns) const = 0;
+    
+        ret = swSocket_unix_sendto(fd1,sock2_path,test_data,strlen(test_data));
+    ASSERT_GT(ret, 0);
+    
+    
+    {    redisAsyncCommand(m_ctx, NULL, NULL, 'SET key %s', m_value);
+    redisAsyncCommand(m_ctx, getCallback, this, 'GET key');
+}
+    
+    bool swoole_mime_type_delete(const char *suffix, const char *mime_type)
+{
+    if (mime_map.find(suffix) == mime_map.end())
+    {
+        return false;
     }
     else
     {
-        version |= MULTI_PART_FILE_FLAG;
+        mime_map.erase(string(suffix));
+        return true;
     }
+}
     
-    for (int i = 0; i < parts; i++)
+    
+    {    if (p < data + header.length)
     {
-        if (usesLongNames (headers[i]))
-            version |= LONG_NAMES_FLAG;
+        // 1              character set
+        charset = *p;
+        p += 1;
+        // 2              status flags
+        memcpy(&status_flags, p, 2);
+        p += 2;
+        // 2              capability flags (upper 2 bytes)
+        memcpy(((char *) (&capability_flags) + 2), p, 2);
+        p += 2;
+        // 1              auth plugin data length
+        auth_plugin_data_length = (uint8_t) *p;
+        p += 1;
+        // x              reserved
+        memcpy(&reserved, p, sizeof(reserved));
+        p += sizeof(reserved);
+        if (capability_flags & SW_MYSQL_CLIENT_SECURE_CONNECTION)
+        {
+            uint8_t len = SW_MAX(13, auth_plugin_data_length - 8);
+            memcpy(auth_plugin_data + 8, p, len);
+            p += len;
+        }
+        if (capability_flags & SW_MYSQL_CLIENT_PLUGIN_AUTH)
+        {
+            auth_plugin_name = std::string(p, strlen(p));
+            swTraceLog(SW_TRACE_MYSQL_CLIENT, 'use %s auth plugin', auth_plugin_name.c_str());
+        }
     }
-    
-    
-//
-// DECODING
-//
-    
-    
-    //--------------------------
-    // Access to the file header
-    //--------------------------
-    
-    #endif /* IMFINPUTPARTDATA_H_ */
-
-    
-    #endif /* IMFINPUTSTREAMMUTEX_H_ */
-
-    
-    // Author: anuraag@google.com (Anuraag Agrawal)
-// Author: tibell@google.com (Johan Tibell)
-    
-    PyObject* MergeFrom(RepeatedCompositeContainer* self, PyObject* other) {
-  return Extend(self, other);
-}
-    
-    // Builds a RepeatedScalarContainer object, from a parent message and a
-// field descriptor.
-extern RepeatedScalarContainer* NewContainer(
-    CMessage* parent, const FieldDescriptor* parent_field_descriptor);
-    
-    #include <google/protobuf/stubs/logging.h>
-#include <google/protobuf/stubs/common.h>
-#include <google/protobuf/io/coded_stream.h>
-#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
-#include <google/protobuf/arena.h>
-    
-      // Creates a ZeroCopyOutputStream which will insert code into the given file
-  // at the given insertion point.  See plugin.proto (plugin.pb.h) for more
-  // information on insertion points.  The default implementation
-  // assert-fails -- it exists only for backwards-compatibility.
-  //
-  // WARNING:  This feature is currently EXPERIMENTAL and is subject to change.
-  virtual io::ZeroCopyOutputStream* OpenForInsert(
-      const std::string& filename, const std::string& insertion_point);
-    
-      Context* context_;
-  ClassNameResolver* name_resolver_;
-    
-    #endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_FILE_H__
-
-    
-    U_NAMESPACE_BEGIN
-    
-    #include 'unicode/scientificnumberformatter.h'
-#include 'unicode/dcfmtsym.h'
-#include 'unicode/fpositer.h'
-#include 'unicode/utf16.h'
-#include 'unicode/uniset.h'
-#include 'decfmtst.h'
-#include 'unicode/decimfmt.h'
-    
-    int32_t SearchIterator::first(UErrorCode &status)
-{
-    if (U_FAILURE(status)) {
-        return USEARCH_DONE;
-    }
-    setOffset(0, status);
-    return handleNext(0, status);
-}
-    
-    #if !UCONFIG_NO_FORMATTING
-    
-    class U_I18N_API SharedCalendar : public SharedObject {
-public:
-    SharedCalendar(Calendar *calToAdopt) : ptr(calToAdopt) { }
-    virtual ~SharedCalendar();
-    const Calendar *get() const { return ptr; }
-    const Calendar *operator->() const { return ptr; }
-    const Calendar &operator*() const { return *ptr; }
-private:
-    Calendar *ptr;
-    SharedCalendar(const SharedCalendar &);
-    SharedCalendar &operator=(const SharedCalendar &);
+    swTraceLog(
+        SW_TRACE_MYSQL_CLIENT, 'Server protocol=%d, version=%s, connection_id=%d, capabilites=0x%08x, status=%u, auth_plugin_name=%s, auth_plugin_data=L%u[%s]',
+        protocol_version, server_version.c_str(), connection_id, capability_flags, status_flags, auth_plugin_name.c_str(), auth_plugin_data_length, auth_plugin_data
+    );
 };
     
-    #endif
-
+    	err->has_error = 1;
+	err->level = E_WARNING;
+	spprintf(&err->msg, 0, 'error converting %s data (path: %s): %.*s',
+			what_conv,
+			path.s && *ZSTR_VAL(path.s) != '\0' ? ZSTR_VAL(path.s) : 'unavailable',
+			user_msg_size, user_msg);
+	err->should_free = 1;
     
-    // Main code
-int main(int, char**)
-{
-    // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
-    {
-        printf('Error: %s\n', SDL_GetError());
-        return -1;
-    }
-    }
+                type = (zend_uchar *)&l;
+            if (0 > trace_get_long(traced_pid, function + offsetof(zend_function, type), &l))
+            {
+                return -1;
+            }
     
-        // Setup window
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow('Dear ImGui SDL2+OpenGL example', SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-    SDL_GL_MakeCurrent(window, gl_context);
-    SDL_GL_SetSwapInterval(1); // Enable vsync
-    
-    
-    {        g_pSwapChain->Present(1, 0); // Present with vsync
-        //g_pSwapChain->Present(0, 0); // Present without vsync
-    }
-    
-    // Implemented features:
-//  [X] Platform: Clipboard support.
-//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad'.
-//  [x] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'. FIXME: 3 cursors types are missing from GLFW.
-//  [X] Platform: Keyboard arrays indexed using GLFW_KEY_* codes, e.g. ImGui::IsKeyPressed(GLFW_KEY_SPACE).
-    
-        // For the browser using Emscripten, we are going to use WebGL1 with GL ES2. See the Makefile. for requirement details.
-    // It is very likely the generated file won't work in many browsers. Firefox is the only sure bet, but I have successfully
-    // run this code on Chrome for Android for example.
-    const char* glsl_version = '#version 100';
-    //const char* glsl_version = '#version 300 es';
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-        // Build atlas
-    unsigned char* tex_pixels = NULL;
-    int tex_w, tex_h;
-    io.Fonts->GetTexDataAsRGBA32(&tex_pixels, &tex_w, &tex_h);
-    
-        zend_string *opened_path;
-    if (!file_handle.opened_path)
-    {
-        file_handle.opened_path = zend_string_init(file.c_str(), file.length(), 0);
-    }
-    opened_path = zend_string_copy(file_handle.opened_path);
-    zval dummy;
-    
-    namespace swoole { namespace coroutine {
-//-------------------------------------------------------------------------------
-class Channel
-{
-public:
-    enum opcode
-    {
-        PRODUCER = 1,
-        CONSUMER = 2,
-    };
-    }
-    }
-    }
-    
-        for (i = 0; i < MAP_SIZE; i++)
-    {
-        pkt = (swFdInfo *) malloc(sizeof(swFdInfo));
-        pkt->key = i;
-        pkt->fd = i * 37;
-        swHashMap_add_int(ht, pkt->fd, pkt);
-        lists[i] = pkt;
-    }
-    
-            if (pid == 0)
-        {
-            exit(0);
-        }
-    
-    
-int php_do_setsockopt_ipv6_rfc3542(Socket *php_sock, int level, int optname, zval *arg4)
-{
-#ifdef IPV6_PKTINFO
-	struct err_s	err = {0};
-#endif
-	zend_llist		*allocations = NULL;
-	void			*opt_ptr;
-	socklen_t		optlen;
-	int				retval;
-    }
+            goto exit_fail;
