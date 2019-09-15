318f6923560fe759aa2f5923524c58e04be85687
SUNsung <weatherius@gmail.com>
2019-09-15 21:36:22 +0300

    Rewrite [20L.T6cD9vF7.20bLB.C1xX8bo20KD4OnTBoUAk20w6j.cpp] File in: 2019-09-15 21:36:22

--- cccccc/20L.T6cD9vF7.20bLB.C1xX8bo20KD4OnTBoUAk20w6j.cpp
+++ cccccc/20L.T6cD9vF7.20bLB.C1xX8bo20KD4OnTBoUAk20w6j.cpp
@@ -1,154 +1,144 @@
 
         
-        
-    {
-    {
-    {      CHECK(blob_dims == blob->shape()) << 'Cannot load blob from hdf5; shape '
-            << 'mismatch. Source shape is ' << source_shape_string
-            << ' target shape is ' << blob->shape_string();
-    }
+          inline std::vector<Elem>::const_iterator begin() const {  // NOLINT
+    return elem_of_each_node_.begin();
   }
-}
     
-        Dtype min = this->layer_param_.clip_param().min();
-    Dtype max = this->layer_param_.clip_param().max();
+      iter = 0;
+  size_t n_conditions = 0;
+  while ((iter = str.find('gain', iter + 1)) != std::string::npos) {
+    n_conditions++;
+  }
+  ASSERT_EQ(n_conditions, 3);
     
-    namespace caffe {
-    }
-    
-      bp::class_<Solver<Dtype>, shared_ptr<Solver<Dtype> >, boost::noncopyable>(
-    'Solver', bp::no_init)
-    .add_property('net', &Solver<Dtype>::net)
-    .add_property('test_nets', bp::make_function(&Solver<Dtype>::test_nets,
-          bp::return_internal_reference<>()))
-    .add_property('iter', &Solver<Dtype>::iter)
-    .def('add_callback', &Solver_add_callback<Dtype>)
-    .def('add_callback', &Solver_add_nccl)
-    .def('solve', static_cast<void (Solver<Dtype>::*)(const char*)>(
-          &Solver<Dtype>::Solve), SolveOverloads())
-    .def('step', &Solver<Dtype>::Step)
-    .def('restore', &Solver<Dtype>::Restore)
-    .def('snapshot', &Solver<Dtype>::Snapshot)
-    .def('share_weights', &share_weights)
-    .def('apply_update', &Solver<Dtype>::ApplyUpdate)
-    .add_property('param', bp::make_function(&Solver<Dtype>::param,
-              bp::return_internal_reference<>()));
-  BP_REGISTER_SHARED_PTR_TO_PYTHON(Solver<Dtype>);
-    
-      // This random number generator facade hides boost and CUDA rng
-  // implementation from one another (for cross-platform compatibility).
-  class RNG {
-   public:
-    RNG();
-    explicit RNG(unsigned int seed);
-    explicit RNG(const RNG&);
-    RNG& operator=(const RNG&);
-    void* generator();
-   private:
-    class Generator;
-    shared_ptr<Generator> generator_;
-  };
-    
-    /** @brief Fills a Blob with values @f$ x \in [0, 1] @f$
- *         such that @f$ \forall i \sum_j x_{ij} = 1 @f$.
+    /*!
+ * \brief define regression tree to be the most common tree model.
+ *  This is the data structure used in xgboost's major tree models.
  */
-template <typename Dtype>
-class PositiveUnitballFiller : public Filler<Dtype> {
+class RegTree {
  public:
-  explicit PositiveUnitballFiller(const FillerParameter& param)
-      : Filler<Dtype>(param) {}
-  virtual void Fill(Blob<Dtype>* blob) {
-    Dtype* data = blob->mutable_cpu_data();
-    DCHECK(blob->count());
-    caffe_rng_uniform<Dtype>(blob->count(), 0, 1, blob->mutable_cpu_data());
-    // We expect the filler to not be called very frequently, so we will
-    // just use a simple implementation
-    int dim = blob->count() / blob->shape(0);
-    CHECK(dim);
-    for (int i = 0; i < blob->shape(0); ++i) {
-      Dtype sum = 0;
-      for (int j = 0; j < dim; ++j) {
-        sum += data[i * dim + j];
-      }
-      for (int j = 0; j < dim; ++j) {
-        data[i * dim + j] /= sum;
-      }
+  /*! \brief auxiliary statistics of node to help tree building */
+  using SplitCondT = bst_float;
+  /*! \brief tree node */
+  class Node {
+   public:
+    Node()  {
+      // assert compact alignment
+      static_assert(sizeof(Node) == 4 * sizeof(int) + sizeof(Info),
+                    'Node: 64 bit align');
     }
-    CHECK_EQ(this->filler_param_.sparse(), -1)
-         << 'Sparsity not supported by this Filler.';
+    /*! \brief index of left child */
+    XGBOOST_DEVICE int LeftChild() const {
+      return this->cleft_;
+    }
+    /*! \brief index of right child */
+    XGBOOST_DEVICE int RightChild() const {
+      return this->cright_;
+    }
+    /*! \brief index of default child when feature is missing */
+    XGBOOST_DEVICE int DefaultChild() const {
+      return this->DefaultLeft() ? this->LeftChild() : this->RightChild();
+    }
+    /*! \brief feature index of split condition */
+    XGBOOST_DEVICE unsigned SplitIndex() const {
+      return sindex_ & ((1U << 31) - 1U);
+    }
+    /*! \brief when feature is unknown, whether goes to left child */
+    XGBOOST_DEVICE bool DefaultLeft() const {
+      return (sindex_ >> 31) != 0;
+    }
+    /*! \brief whether current node is leaf node */
+    XGBOOST_DEVICE bool IsLeaf() const {
+      return cleft_ == -1;
+    }
+    /*! \return get leaf value of leaf node */
+    XGBOOST_DEVICE bst_float LeafValue() const {
+      return (this->info_).leaf_value;
+    }
+    /*! \return get split condition of the node */
+    XGBOOST_DEVICE SplitCondT SplitCond() const {
+      return (this->info_).split_cond;
+    }
+    /*! \brief get parent of the node */
+    XGBOOST_DEVICE int Parent() const {
+      return parent_ & ((1U << 31) - 1);
+    }
+    /*! \brief whether current node is left child */
+    XGBOOST_DEVICE bool IsLeftChild() const {
+      return (parent_ & (1U << 31)) != 0;
+    }
+    /*! \brief whether this node is deleted */
+    XGBOOST_DEVICE bool IsDeleted() const {
+      return sindex_ == std::numeric_limits<unsigned>::max();
+    }
+    /*! \brief whether current node is root */
+    XGBOOST_DEVICE bool IsRoot() const { return parent_ == -1; }
+    /*!
+     * \brief set the left child
+     * \param nid node id to right child
+     */
+    XGBOOST_DEVICE void SetLeftChild(int nid) {
+      this->cleft_ = nid;
+    }
+    /*!
+     * \brief set the right child
+     * \param nid node id to right child
+     */
+    XGBOOST_DEVICE void SetRightChild(int nid) {
+      this->cright_ = nid;
+    }
+    /*!
+     * \brief set split condition of current node
+     * \param split_index feature index to split
+     * \param split_cond  split condition
+     * \param default_left the default direction when feature is unknown
+     */
+    XGBOOST_DEVICE void SetSplit(unsigned split_index, SplitCondT split_cond,
+                          bool default_left = false) {
+      if (default_left) split_index |= (1U << 31);
+      this->sindex_ = split_index;
+      (this->info_).split_cond = split_cond;
+    }
+    /*!
+     * \brief set the leaf value of the node
+     * \param value leaf value
+     * \param right right index, could be used to store
+     *        additional information
+     */
+    XGBOOST_DEVICE void SetLeaf(bst_float value, int right = -1) {
+      (this->info_).leaf_value = value;
+      this->cleft_ = -1;
+      this->cright_ = right;
+    }
+    /*! \brief mark that this node is deleted */
+    XGBOOST_DEVICE void MarkDelete() {
+      this->sindex_ = std::numeric_limits<unsigned>::max();
+    }
+    /*! \brief Reuse this deleted node. */
+    XGBOOST_DEVICE void Reuse() {
+      this->sindex_ = 0;
+    }
+    // set parent
+    XGBOOST_DEVICE void SetParent(int pidx, bool is_left_child = true) {
+      if (is_left_child) pidx |= (1U << 31);
+      this->parent_ = pidx;
+    }
+    bool operator==(const Node& b) const {
+      return parent_ == b.parent_ && cleft_ == b.cleft_ &&
+             cright_ == b.cright_ && sindex_ == b.sindex_ &&
+             info_.leaf_value == b.info_.leaf_value;
+    }
+    }
+    }
+    
+    namespace xgboost {
+ConsoleLogger::~ConsoleLogger() {
+  if (cur_verbosity_ == LogVerbosity::kIgnore ||
+      cur_verbosity_ <= global_verbosity_) {
+    dmlc::CustomLogMessage::Log(log_stream_.str());
   }
-};
-    
-    
-    {	return mirroring;
 }
-    
-    	Usage usage;
-    
-    #endif
-
-    
-    public:
-	_FORCE_INLINE_ StringName get_sname() const {
-    }
-    
-    
-    {		String get_name() const { return cname ? String(cname) : name; }
-		int idx;
-		uint32_t hash;
-		_Data *prev;
-		_Data *next;
-		_Data() {
-			cname = NULL;
-			next = prev = NULL;
-			idx = 0;
-			hash = 0;
-		}
-	};
-    
-    void GDAPI godot_pool_vector2_array_resize(godot_pool_vector2_array *p_self, const godot_int p_size) {
-	PoolVector<Vector2> *self = (PoolVector<Vector2> *)p_self;
-	self->resize(p_size);
+TrackerLogger::~TrackerLogger() {
+  dmlc::CustomLogMessage::Log(log_stream_.str());
 }
-    
-    const void GDAPI *godot_string_name_get_data_unique_pointer(const godot_string_name *p_self) {
-	const StringName *self = (const StringName *)p_self;
-	return self->data_unique_pointer();
-}
-    
-    String UPNPDevice::get_igd_service_type() const {
-	return igd_service_type;
-}
-    
-    
-    {  Alarm alarm_;
-  bool signal_client_;
-  std::mutex mu_;
-  std::unique_ptr<grpc::string> host_;
-};
-    
-      gpr_mu_lock(&winsocket->state_mu);
-  if (winsocket->shutdown_called) {
-    gpr_mu_unlock(&winsocket->state_mu);
-    return;
-  }
-  winsocket->shutdown_called = true;
-  gpr_mu_unlock(&winsocket->state_mu);
-    
-    int main(int argc, char** argv) {
-  std::string me = argv[0];
-  auto lslash = me.rfind('/');
-  if (lslash != std::string::npos) {
-    g_root = me.substr(0, lslash);
-  } else {
-    g_root = '.';
-  }
-    }
-    
-        const protobuf::FileDescriptor* file_desc = service_desc->file();
-    if (known_files_.find(file_desc->package() + '/' + file_desc->name()) !=
-        known_files_.end()) {
-      EXPECT_EQ(file_desc->DebugString(),
-                ref_service_desc->file()->DebugString());
-      known_files_.insert(file_desc->package() + '/' + file_desc->name());
-    }
+}  // namespace xgboost
