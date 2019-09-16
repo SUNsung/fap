6b495adf84dc85cde0dfd0c0fa6ed54879e1bb55
SUNsung <weatherius@gmail.com>
2019-09-16 17:25:54 +0300

    Rewrite [36A6WJCDcXGdI36kaPJ4qDvkKA36.gW9qMyLCx.36f5p.py] File in: 2019-09-16 17:25:53

--- pyton/36A6WJCDcXGdI36kaPJ4qDvkKA36.gW9qMyLCx.36f5p.py
+++ pyton/36A6WJCDcXGdI36kaPJ4qDvkKA36.gW9qMyLCx.36f5p.py
@@ -1,68 +1,189 @@
 
         
-            HEART = 0
-    DIAMOND = 1
-    CLUBS = 2
-    SPADE = 3
+            def from_pyfile(self, filename, silent=False):
+        '''Updates the values in the config from a Python file.  This function
+        behaves as if the file was imported as module with the
+        :meth:`from_object` function.
     
     
-class State(Enum):
-    unvisited = 0
-    visited = 1
+def _lookup_req_object(name):
+    top = _request_ctx_stack.top
+    if top is None:
+        raise RuntimeError(_request_ctx_err_msg)
+    return getattr(top, name)
     
-        @property
-    def origin_req_host(self):
-        return self.get_origin_req_host()
+                def post(self):
+                session['counter'] = session.get('counter', 0) + 1
+                return 'OK'
     
-        return {
-        'platform': platform_info,
-        'implementation': implementation_info,
-        'system_ssl': system_ssl_info,
-        'using_pyopenssl': pyopenssl is not None,
-        'pyOpenSSL': pyopenssl_info,
-        'urllib3': urllib3_info,
-        'chardet': chardet_info,
-        'cryptography': cryptography_info,
-        'idna': idna_info,
-        'requests': {
-            'version': requests_version,
-        },
-    }
+            subprocess.check_call(
+            [sys.executable, 'setup.py', 'bdist_egg'], cwd=str(modules_tmpdir)
+        )
+        egg_path, = modules_tmpdir.join('dist/').listdir()
+        monkeypatch.syspath_prepend(str(egg_path))
+        return egg_path
     
-    
-def test_idna_without_version_attribute(mocker):
-    '''Older versions of IDNA don't provide a __version__ attribute, verify
-    that if we have such a package, we don't blow up.
-    '''
-    mocker.patch('requests.help.idna', new=None)
-    assert info()['idna'] == {'version': ''}
-    
-        close_server = threading.Event()
-    server = Server(response_handler, wait_to_close_event=close_server)
-    
-    
-class TestSuperLen:
-    
-                        new_fields.append(
-                        (field.decode('utf-8') if isinstance(field, bytes) else field,
-                         v.encode('utf-8') if isinstance(v, str) else v))
-    
-    import josepy as jose
-    
-    # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-    
-    
-def im_conv_body_only(model, im, target_scale, target_max_size):
-    '''Runs `model.conv_body_net` on the given image `im`.'''
-    im_blob, im_scale, _im_info = blob_utils.get_image_blob(
-        im, target_scale, target_max_size
+        pytest.raises(
+        AssertionError,
+        lambda: bp.add_url_rule('/bar/123', endpoint='bar.123', view_func=foo_foo_foo),
     )
-    workspace.FeedBlob(core.ScopedName('data'), im_blob)
-    workspace.RunNet(model.conv_body_net.Proto().name)
-    return im_scale
+    
+        def __enter__(self, *a):
+        sys.addaudithook(self)
+        return self
+    
+    import errno
+import sys
+import signal
+import time
+import os
+import platform
+import pwd
+import stat
+import tempfile
+import unittest
+import warnings
+import textwrap
+    
+    
+# Exceptions raised when an error or invalid response is received
+class NNTPError(Exception):
+    '''Base class for all nntplib exceptions'''
+    def __init__(self, *args):
+        Exception.__init__(self, *args)
+        try:
+            self.response = args[0]
+        except IndexError:
+            self.response = 'No response given'
+    
+            If optional args 'block' is true and 'timeout' is None (the default),
+        block if necessary until a free slot is available. If 'timeout' is
+        a non-negative number, it blocks at most 'timeout' seconds and raises
+        the Full exception if no free slot was available within that time.
+        Otherwise ('block' is false), put an item on the queue if a free slot
+        is immediately available, else raise the Full exception ('timeout'
+        is ignored in that case).
+        '''
+        with self.not_full:
+            if self.maxsize > 0:
+                if not block:
+                    if self._qsize() >= self.maxsize:
+                        raise Full
+                elif timeout is None:
+                    while self._qsize() >= self.maxsize:
+                        self.not_full.wait()
+                elif timeout < 0:
+                    raise ValueError(''timeout' must be a non-negative number')
+                else:
+                    endtime = time() + timeout
+                    while self._qsize() >= self.maxsize:
+                        remaining = endtime - time()
+                        if remaining <= 0.0:
+                            raise Full
+                        self.not_full.wait(remaining)
+            self._put(item)
+            self.unfinished_tasks += 1
+            self.not_empty.notify()
+    
+    
+# Execute a function that blocks, and in a separate thread, a function that
+# triggers the release.  Returns the result of the blocking function.  Caution:
+# block_func must guarantee to block until trigger_func is called, and
+# trigger_func must guarantee to change queue state so that block_func can make
+# enough progress to return.  In particular, a block_func that just raises an
+# exception regardless of whether trigger_func is called will lead to
+# timing-dependent sporadic failures, and one of those went rarely seen but
+# undiagnosed for years.  Now block_func must be unexceptional.  If block_func
+# is supposed to raise an exception, call do_exceptional_blocking_test()
+# instead.
+    
+            Most messages have a default content type of text/plain, except for
+        messages that are subparts of multipart/digest containers.  Such
+        subparts have a default content type of message/rfc822.
+        '''
+        return self._default_type
+    
+        def test_BufferedIOBase_readinto(self):
+        # Exercise the default BufferedIOBase.readinto() and readinto1()
+        # implementations (which call read() or read1() internally).
+        class Reader(self.BufferedIOBase):
+            def __init__(self, avail):
+                self.avail = avail
+            def read(self, size):
+                result = self.avail[:size]
+                self.avail = self.avail[size:]
+                return result
+            def read1(self, size):
+                '''Returns no more than 5 bytes at once'''
+                return self.read(min(size, 5))
+        tests = (
+            # (test method, total data available, read buffer size, expected
+            #     read size)
+            ('readinto', 10, 5, 5),
+            ('readinto', 10, 6, 6),  # More than read1() can return
+            ('readinto', 5, 6, 5),  # Buffer larger than total available
+            ('readinto', 6, 7, 6),
+            ('readinto', 10, 0, 0),  # Empty buffer
+            ('readinto1', 10, 5, 5),  # Result limited to single read1() call
+            ('readinto1', 10, 6, 5),  # Buffer larger than read1() can return
+            ('readinto1', 5, 6, 5),  # Buffer larger than total available
+            ('readinto1', 6, 7, 5),
+            ('readinto1', 10, 0, 0),  # Empty buffer
+        )
+        UNUSED_BYTE = 0x81
+        for test in tests:
+            with self.subTest(test):
+                method, avail, request, result = test
+                reader = Reader(bytes(range(avail)))
+                buffer = bytearray((UNUSED_BYTE,) * request)
+                method = getattr(reader, method)
+                self.assertEqual(method(buffer), result)
+                self.assertEqual(len(buffer), request)
+                self.assertSequenceEqual(buffer[:result], range(result))
+                unused = (UNUSED_BYTE,) * (request - result)
+                self.assertSequenceEqual(buffer[result:], unused)
+                self.assertEqual(len(reader.avail), avail - result)
+    
+    # Recursively yields relative pathnames inside a literal directory.
+def _rlistdir(dirname, dironly):
+    names = list(_iterdir(dirname, dironly))
+    for x in names:
+        if not _ishidden(x):
+            yield x
+            path = os.path.join(dirname, x) if dirname else x
+            for y in _rlistdir(path, dironly):
+                yield os.path.join(x, y)
+    
+    Provides the 'spawn()' function, a front-end to various platform-
+specific functions for launching another program in a sub-process.
+Also provides the 'find_executable()' to search the path for a given
+executable name.
+'''
+    
+        For Python-coded functions and methods, the first line is introspected.
+    Delete 'self' parameter for classes (.__init__) and bound methods.
+    The next lines are the first lines of the doc string up to the first
+    empty line or _MAX_LINES.    For builtins, this typically includes
+    the arguments in addition to the return value.
+    '''
+    argspec = default = ''
+    try:
+        ob_call = ob.__call__
+    except BaseException:
+        return default
+    
+    if args.quiet:
+    jieba.setLogLevel(60)
+if args.pos:
+    import jieba.posseg
+    posdelim = args.pos
+    def cutfunc(sentence, _, HMM=True):
+        for w, f in jieba.posseg.cut(sentence, HMM):
+            yield w + posdelim + f
+else:
+    cutfunc = jieba.cut
+    
+    
+content = open(file_name,'rb').read()
+    
+    
