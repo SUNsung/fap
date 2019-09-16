f89abda23093690dcc55e422851497c3fba7321b
SUNsung <weatherius@gmail.com>
2019-09-16 05:43:36 +0300

    Rewrite [41NyPO2ExNv8M41Ti8.xPhAO3w41CzbywEIL8N641.jo.rb] File in: 2019-09-16 05:43:36

--- ruby/41NyPO2ExNv8M41Ti8.xPhAO3w41CzbywEIL8N641.jo.rb
+++ ruby/41NyPO2ExNv8M41Ti8.xPhAO3w41CzbywEIL8N641.jo.rb
@@ -1,239 +1,154 @@
 
         
-              app_file 'config/environments/test.rb', <<-RUBY
-        Rails.application.configure do
-          config.action_controller.allow_forgery_protection = true
-          config.action_dispatch.show_exceptions = false
+            def set_app_rating(v, options)
+      return unless options[:app_rating_config_path]
+    
+          def setup
+        # Since September 2015 we don't get the url any more, so we have to manually build it
+        self.url = '#{HOST_URL}/#{self.asset_token}'
+      end
+    
+          # This method will generate the required keys/values
+      # for App Store Connect to validate the review attachment file
+      def generate_review_attachment_file(review_attachment_data, review_attachment_file)
+        {
+          assetToken: review_attachment_data['token'],
+          name: File.basename(review_attachment_file),
+          fileType: Utilities.content_type(review_attachment_file),
+          url: nil
+        }
+      end
+    
+        describe 'Upload screenshots', :screenshots do
+      before do
+        allow(Spaceship::UploadFile).to receive(:from_path) do |path|
+          du_uploadimage_correct_screenshot if path == 'path_to_screenshot'
         end
-      RUBY
+      end
+      let(:screenshot_path) { 'path_to_screenshot' }
     
-    module ActiveSupport
-  # = Notifications
-  #
-  # <tt>ActiveSupport::Notifications</tt> provides an instrumentation API for
-  # Ruby.
-  #
-  # == Instrumenters
-  #
-  # To instrument an event you just need to do:
-  #
-  #   ActiveSupport::Notifications.instrument('render', extra: :information) do
-  #     render plain: 'Foo'
-  #   end
-  #
-  # That first executes the block and then notifies all subscribers once done.
-  #
-  # In the example above +render+ is the name of the event, and the rest is called
-  # the _payload_. The payload is a mechanism that allows instrumenters to pass
-  # extra information to subscribers. Payloads consist of a hash whose contents
-  # are arbitrary and generally depend on the event.
-  #
-  # == Subscribers
-  #
-  # You can consume those events and the information they provide by registering
-  # a subscriber.
-  #
-  #   ActiveSupport::Notifications.subscribe('render') do |name, start, finish, id, payload|
-  #     name    # => String, name of the event (such as 'render' from above)
-  #     start   # => Time, when the instrumented block started execution
-  #     finish  # => Time, when the instrumented block ended execution
-  #     id      # => String, unique ID for the instrumenter that fired the event
-  #     payload # => Hash, the payload
-  #   end
-  #
-  # Here, the +start+ and +finish+ values represent wall-clock time. If you are
-  # concerned about accuracy, you can register a monotonic subscriber.
-  #
-  #   ActiveSupport::Notifications.monotonic_subscribe('render') do |name, start, finish, id, payload|
-  #     name    # => String, name of the event (such as 'render' from above)
-  #     start   # => Monotonic time, when the instrumented block started execution
-  #     finish  # => Monotonic time, when the instrumented block ended execution
-  #     id      # => String, unique ID for the instrumenter that fired the event
-  #     payload # => Hash, the payload
-  #   end
-  #
-  # The +start+ and +finish+ values above represent monotonic time.
-  #
-  # For instance, let's store all 'render' events in an array:
-  #
-  #   events = []
-  #
-  #   ActiveSupport::Notifications.subscribe('render') do |*args|
-  #     events << ActiveSupport::Notifications::Event.new(*args)
-  #   end
-  #
-  # That code returns right away, you are just subscribing to 'render' events.
-  # The block is saved and will be called whenever someone instruments 'render':
-  #
-  #   ActiveSupport::Notifications.instrument('render', extra: :information) do
-  #     render plain: 'Foo'
-  #   end
-  #
-  #   event = events.first
-  #   event.name      # => 'render'
-  #   event.duration  # => 10 (in milliseconds)
-  #   event.payload   # => { extra: :information }
-  #
-  # The block in the <tt>subscribe</tt> call gets the name of the event, start
-  # timestamp, end timestamp, a string with a unique identifier for that event's instrumenter
-  # (something like '535801666f04d0298cd6'), and a hash with the payload, in
-  # that order.
-  #
-  # If an exception happens during that particular instrumentation the payload will
-  # have a key <tt>:exception</tt> with an array of two elements as value: a string with
-  # the name of the exception class, and the exception message.
-  # The <tt>:exception_object</tt> key of the payload will have the exception
-  # itself as the value:
-  #
-  #   event.payload[:exception]         # => ['ArgumentError', 'Invalid value']
-  #   event.payload[:exception_object]  # => #<ArgumentError: Invalid value>
-  #
-  # As the earlier example depicts, the class <tt>ActiveSupport::Notifications::Event</tt>
-  # is able to take the arguments as they come and provide an object-oriented
-  # interface to that data.
-  #
-  # It is also possible to pass an object which responds to <tt>call</tt> method
-  # as the second parameter to the <tt>subscribe</tt> method instead of a block:
-  #
-  #   module ActionController
-  #     class PageRequest
-  #       def call(name, started, finished, unique_id, payload)
-  #         Rails.logger.debug ['notification:', name, started, finished, unique_id, payload].join(' ')
-  #       end
-  #     end
-  #   end
-  #
-  #   ActiveSupport::Notifications.subscribe('process_action.action_controller', ActionController::PageRequest.new)
-  #
-  # resulting in the following output within the logs including a hash with the payload:
-  #
-  #   notification: process_action.action_controller 2012-04-13 01:08:35 +0300 2012-04-13 01:08:35 +0300 af358ed7fab884532ec7 {
-  #      controller: 'Devise::SessionsController',
-  #      action: 'new',
-  #      params: {'action'=>'new', 'controller'=>'devise/sessions'},
-  #      format: :html,
-  #      method: 'GET',
-  #      path: '/login/sign_in',
-  #      status: 200,
-  #      view_runtime: 279.3080806732178,
-  #      db_runtime: 40.053
-  #    }
-  #
-  # You can also subscribe to all events whose name matches a certain regexp:
-  #
-  #   ActiveSupport::Notifications.subscribe(/render/) do |*args|
-  #     ...
-  #   end
-  #
-  # and even pass no argument to <tt>subscribe</tt>, in which case you are subscribing
-  # to all events.
-  #
-  # == Temporary Subscriptions
-  #
-  # Sometimes you do not want to subscribe to an event for the entire life of
-  # the application. There are two ways to unsubscribe.
-  #
-  # WARNING: The instrumentation framework is designed for long-running subscribers,
-  # use this feature sparingly because it wipes some internal caches and that has
-  # a negative impact on performance.
-  #
-  # === Subscribe While a Block Runs
-  #
-  # You can subscribe to some event temporarily while some block runs. For
-  # example, in
-  #
-  #   callback = lambda {|*args| ... }
-  #   ActiveSupport::Notifications.subscribed(callback, 'sql.active_record') do
-  #     ...
-  #   end
-  #
-  # the callback will be called for all 'sql.active_record' events instrumented
-  # during the execution of the block. The callback is unsubscribed automatically
-  # after that.
-  #
-  # To record +started+ and +finished+ values with monotonic time,
-  # specify the optional <tt>:monotonic</tt> option to the
-  # <tt>subscribed</tt> method. The <tt>:monotonic</tt> option is set
-  # to +false+ by default.
-  #
-  #   callback = lambda {|name, started, finished, unique_id, payload| ... }
-  #   ActiveSupport::Notifications.subscribed(callback, 'sql.active_record', monotonic: true) do
-  #     ...
-  #   end
-  #
-  # === Manual Unsubscription
-  #
-  # The +subscribe+ method returns a subscriber object:
-  #
-  #   subscriber = ActiveSupport::Notifications.subscribe('render') do |*args|
-  #     ...
-  #   end
-  #
-  # To prevent that block from being called anymore, just unsubscribe passing
-  # that reference:
-  #
-  #   ActiveSupport::Notifications.unsubscribe(subscriber)
-  #
-  # You can also unsubscribe by passing the name of the subscriber object. Note
-  # that this will unsubscribe all subscriptions with the given name:
-  #
-  #   ActiveSupport::Notifications.unsubscribe('render')
-  #
-  # Subscribers using a regexp or other pattern-matching object will remain subscribed
-  # to all events that match their original pattern, unless those events match a string
-  # passed to `unsubscribe`:
-  #
-  #   subscriber = ActiveSupport::Notifications.subscribe(/render/) { }
-  #   ActiveSupport::Notifications.unsubscribe('render_template.action_view')
-  #   subscriber.matches?('render_template.action_view') # => false
-  #   subscriber.matches?('render_partial.action_view') # => true
-  #
-  # == Default Queue
-  #
-  # Notifications ships with a queue implementation that consumes and publishes events
-  # to all log subscribers. You can use any queue implementation you want.
-  #
-  module Notifications
-    class << self
-      attr_accessor :notifier
+      ['development', 'distribution'].each do |type|
+    describe 'nuke #{type} option handling' do
+      it 'can use the git_url short flag from tool options' do
+        stub_commander_runner_args(['nuke', type, '-r', 'git@github.com:you/your_repo.git'])
     
-            attr_reader :symbol
+            changelog = ''
     
-          p = Sidekiq::Processor.new(mgr)
-      JoeWorker.perform_async(1)
-      q = Sidekiq::Queue.new
-      assert_equal 1, q.size
+          def self.available_options
+        require 'match'
+        Match::Options.available_options
+      end
     
-      def new_manager(opts)
-    Sidekiq::Manager.new(opts)
+          def_node_matcher :extract_shared_values_key?, <<-PATTERN
+        (const (const nil? :SharedValues) $_)
+      PATTERN
+    
+          def self.is_supported?(platform)
+        [:android].include?(platform)
+      end
+    
+          def initialize(query, filter_opts = {}, &block)
+        @raw_query = query.dup
+        @filters = []
+        @filter_options = { default_parser: :downcase.to_proc }.merge(filter_opts)
+    
+          context 'and user can admin_project_member' do
+        before do
+          allow(presenter).to receive(:can?).with(user, :admin_project_member, project).and_return(true)
+        end
+    
+            def clear_cache
+          # No-op
+        end
+    
+            desc 'Denies an access request for the given user.' do
+          detail 'This feature was introduced in GitLab 8.11.'
+        end
+        params do
+          requires :user_id, type: Integer, desc: 'The user ID of the access requester'
+        end
+        # rubocop: disable CodeReuse/ActiveRecord
+        delete ':id/access_requests/:user_id' do
+          source = find_source(source_type, params[:id])
+          member = source.requesters.find_by!(user_id: params[:user_id])
+    
+            # Create a new deploy key
+        deploy_key_attributes = declared_params.except(:can_push).merge(user: current_user)
+        deploy_key_project = add_deploy_keys_project(user_project, deploy_key_attributes: deploy_key_attributes, can_push: !!params[:can_push])
+    
+      # Get the title for the page.
+  #
+  # @param [Middleman::Page] page
+  #
+  # @return [String]
+  def title_for(page)
+    if page && page.data.page_title
+      return '#{page.data.page_title} - Vagrant by HashiCorp'
+    end
+    
+          env[:box_url] = tf.path
+    
+            # Skip providers that can't be defaulted, unless they're in our
+        # config, in which case someone made our decision for us.
+        if !config.include?(key)
+          next if popts.key?(:defaultable) && !popts[:defaultable]
+        end
+    
+          # Load once so that we can get the proper box value
+      config, config_warnings, config_errors = @loader.load(keys)
+    
+            before do
+          allow(meta_file).to receive(:file?).and_return(true)
+          allow(meta_file).to receive(:read).and_return(meta_file_content)
+        end
+    
+      let(:platform)   { double('platform') }
+    
+      # This returns the path to the ~/.vagrant.d folder where Vagrant's
+  # per-user state is stored.
+  #
+  # @return [Pathname]
+  def self.user_data_path
+    # Use user specified env var if available
+    path = ENV['VAGRANT_HOME']
+    
+        private
+    
+        def tmux_window_name_option
+      name ? '-n #{name}' : ''
+    end
+    
+        initialize_with { Tmuxinator::Project.new(file) }
   end
     
-        it 'calculates an average poll interval based on the number of known Sidekiq processes' do
-      with_sidekiq_option(:average_scheduled_poll_interval, 10) do
-        3.times do |i|
-          Sidekiq.redis do |conn|
-            conn.sadd('processes', 'process-#{i}')
-            conn.hset('process-#{i}', 'info', nil)
-          end
-        end
-    
-          assert SomeScheduledWorker.perform_in(1.month, 'mike')
-      job = ss.first
-      assert job['created_at']
-      refute job['enqueued_at']
+          it 'defaults to 0' do
+        expect(project.base_index).to eq 0
+      end
     end
   end
     
-          e = assert_raises ArgumentError do
-        Sidekiq.on(:startp)
-      end
-      assert_match(/Invalid event name/, e.message)
-      e = assert_raises ArgumentError do
-        Sidekiq.on('startup')
-      end
-      assert_match(/Symbols only/, e.message)
-      Sidekiq.on(:startup) do
-        1 + 1
-      end
+      def attributes_match
+    expect(@actual).to have_attributes(@expected_attrs)
+  end
     
+        it 'should generate a project file using the correct project file path' do
+      file = StringIO.new
+      allow(File).to receive(:open) { |&block| block.yield file }
+      Dir.mktmpdir do |dir|
+        path = '#{dir}/#{name}.yml'
+        _ = described_class.new.generate_project_file(name, path)
+        expect(file.string).to match %r{\A# #{path}$}
+      end
+    end
+  end
+    
+        context 'both $XDG_CONFIG_HOME/tmuxinator and ~/.tmuxinator exist' do
+      it 'is #xdg' do
+        allow(described_class).to receive(:environment?).and_return false
+        allow(described_class).to receive(:xdg?).and_return true
+        allow(described_class).to receive(:home?).and_return true
+    
+          it 'returns an empty Array' do
+        expect(window.panes).to be_empty
+      end
     end
