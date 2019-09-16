6596691d4142947f659e6658a8161af3ef54603e
SUNsung <weatherius@gmail.com>
2019-09-17 02:22:35 +0300

    Rewrite [33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb] File in: 2019-09-17 02:22:35

--- ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
+++ ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
@@ -1,195 +1,147 @@
 
         
-            included do
-      ##
-      # :singleton-method:
-      # Indicates the format used to generate the timestamp in the cache key, if
-      # versioning is off. Accepts any of the symbols in <tt>Time::DATE_FORMATS</tt>.
-      #
-      # This is +:usec+, by default.
-      class_attribute :cache_timestamp_format, instance_writer: false, default: :usec
+              @actual_array = @tutorials_collection.docs.map(&:relative_path)
+    end
     
-          def instrumenter_for(notifier)
-        @registry[notifier] ||= Instrumenter.new(notifier)
+          Jekyll.logger.info 'Total allocated: #{total_allocated_output} (#{report.total_allocated} objects)'.cyan
+      Jekyll.logger.info 'Total retained:  #{total_retained_output} (#{report.total_retained} objects)'.cyan
+    
+    DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
+    
+    STDOUT.sync = true
+    
+              new_theme_name = args.join('_')
+          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
+    
+      # Get the title for the page.
+  #
+  # @param [Middleman::Page] page
+  #
+  # @return [String]
+  def title_for(page)
+    if page && page.data.page_title
+      return '#{page.data.page_title} - Vagrant by HashiCorp'
+    end
+    
+      let(:iso_env) do
+    # We have to create a Vagrantfile so there is a root path
+    env = isolated_environment
+    env.vagrantfile('')
+    env.create_vagrant_env
+  end
+    
+          env[:box_url] = tf.path
+      expect(box_collection).to receive(:find).
+        with('foo/bar', 'virtualbox', '0.7').and_return(box)
+      expect(box_collection).to receive(:add).never
+      expect(app).to receive(:call).never
+    
+        # Initializes Bundler and the various gem paths so that we can begin
+    # loading gems.
+    def init!(plugins, repair=false)
+      if !@initial_specifications
+        @initial_specifications = Gem::Specification.find_all{true}
+      else
+        Gem::Specification.all = @initial_specifications
+        Gem::Specification.reset
       end
-    end
     
-            attr_reader :symbol
-    
-    module AbstractController
-  module Testing
-    class ControllerWithHelpers < AbstractController::Base
-      include AbstractController::Helpers
-      include AbstractController::Rendering
-      include ActionView::Rendering
-    
-        loose_cooked = PrettyText.cook(md)
-    
-          ensure_cors!
-      presigned_url(obj, :put, UPLOAD_URL_EXPIRES_AFTER_SECONDS)
-    rescue Aws::Errors::ServiceError => e
-      Rails.logger.warn('Failed to generate upload URL for S3: #{e.message.presence || e.class.name}')
-      raise StorageError
-    end
-    
-            # no optimisation possible without losing details
-        expect(upload.filesize).to eq(9558)
-    
-          expect(rules.length).to eq(2)
-      expect(rules[1]['Expiration']['Days']).to eq('100')
-      # fixes the bad filter
-      expect(rules[0]['Filter']['Prefix']).to eq('projectdocs/')
-    end.to_return(status: 200, body: '', headers: {})
-    
-      results = +''
-    
-              it 'creates missing tags if some exist' do
-            Fabricate(:tag, name: 'totally')
-            expect {
-              @result = subject.revise!(user, raw: 'lets totally update the body', tags: ['totally', 'update'])
-            }.to change { Tag.count }.by(1)
-            expect(@result).to eq(true)
-            post.reload
-            expect(post.topic.tags.map(&:name).sort).to eq(['totally', 'update'])
+            entries = []
+        prune   = []
+        @env.machine_index.each do |entry|
+          # If we're pruning and this entry is invalid, skip it
+          # and prune it later.
+          if options[:prune] && !entry.valid?(@env.home_path)
+            prune << entry
+            next
           end
     
-        it 'returns false if not installed' do
-      expect(machine.communicate).to receive(:test).
-        with(command, sudo: true).and_return(false)
-      expect(subject.chef_installed(machine, 'chef_solo', version)).to be_falsey
-    end
+          # Valid entry because the machine does exist
+      entryC_env = isolated_environment
+      entryC_env.vagrantfile('')
+      entryC_venv = entryC_env.create_vagrant_env
+      entryC_machine = entryC_venv.machine(entryC_venv.machine_names[0], :dummy)
+      entryC_machine.id = 'foo'
+      entryC = new_entry(entryC_machine.name)
+      entryC.provider = 'dummy'
+      entryC.vagrantfile_path = entryC_env.workdir
+      locked = iso_env.machine_index.set(entryC)
+      iso_env.machine_index.release(locked)
+    
+        $stderr.puts 'ok'
+  rescue => err
+    $stderr.puts 'fail (' + err.type.to_s + ')'
+    $stderr.puts err.message
+    $stderr.puts err.backtrace
+    $stderr.puts
   end
 end
 
     
-          allow(driver_obj).to receive(:inspect_container).and_return(empty_network_settings)
-    
-    require 'log4r'
-    
-      def new_machine(options)
-    double('machine').tap do |m|
-      allow(m).to receive(:provider_name).and_return(provider_name)
-      allow(m).to receive(:provider_options).and_return(options)
-      allow(m).to receive(:action) do |action, opts|
-        lock.synchronize do
-          called_actions << [m, action, opts]
-        end
-      end
+      # This is especially important for JRuby, since eof? there
+  # is more than just a simple accessor.
+  it 'does not affect the reading data' do
+    gz = Zlib::GzipReader.new @io
+    0.upto(9) do |i|
+      gz.eof?.should be_false
+      gz.read(1).should == @data[i, 1]
     end
+    gz.eof?.should be_true
+    gz.read().should == ''
+    gz.eof?.should be_true
   end
     
-        # This is a custom Gem::Resolver::Set for use with Vagrant plugins. It is
-    # a modified Gem::Resolver::VendorSet that supports multiple versions of
-    # a specific gem
-    class PluginSet < Gem::Resolver::VendorSet
-      ##
-      # Adds a specification to the set with the given +name+ which has been
-      # unpacked into the given +directory+.
-      def add_vendor_gem(name, directory)
-        gemspec = File.join(directory, '#{name}.gemspec')
-        spec = Gem::Specification.load(gemspec)
-        if !spec
-          raise Gem::GemNotFoundException,
-            'unable to find #{gemspec} for gem #{name}'
+        quarantine! do # https://bugs.ruby-lang.org/issues/13675
+      describe 'with nil' do
+        it 'does not insert anything into the stream' do
+          @gz.ungetbyte nil
+          @gz.read.should == 'abcde'
         end
     
-    module VagrantPlugins
-  module CommandGlobalStatus
-    class Command < Vagrant.plugin('2', :command)
-      def self.synopsis
-        'outputs status Vagrant environments for this user'
-      end
-    
-      def symlinked?(symlink_path, target_path)
-    '[ #{symlink_path} -ef #{target_path} ]'
+    describe 'GzipWriter#write' do
+  before :each do
+    @data = '12345abcde'
+    @zip = [31, 139, 8, 0, 44, 220, 209, 71, 0, 3, 51, 52, 50, 54, 49, 77,
+            76, 74, 78, 73, 5, 0, 157, 5, 0, 36, 10, 0, 0, 0].pack('C*')
+    @io = StringIO.new ''.b
   end
     
-          def roles_array
-        roles.to_a
-      end
-    
-        def URIEncodeOctets(octets, result, index)
-      if (@@hexCharCodeArray == 0)
-        @@hexCharCodeArray = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
-                              65, 66, 67, 68, 69, 70];
-      end
-      index = URIAddEncodedOctetToBuffer(octets[0], result, index);
-      if (octets[1]);
-        index = URIAddEncodedOctetToBuffer(octets[1], result, index)
-      end
-      if (octets[2]);
-        index = URIAddEncodedOctetToBuffer(octets[2], result, index)
-      end
-      if (octets[3]);
-        index = URIAddEncodedOctetToBuffer(octets[3], result, index)
-      end
-      return index;
+        @inflator.finish do |chunk|
+      @chunks << chunk
     end
-    
-      test 'missing emoji' do
-    get '/emoji/oggy_was_here'
-    assert_equal 404, last_response.status
   end
     
-        def not_found(msg = nil)
-      @message = msg || 'The requested page does not exist.'
-      status 404
-      return mustache :error
-    end
+    Then(/^the default stage files are created$/) do
+  staging = TestApp.test_app_path.join('config/deploy/staging.rb')
+  production = TestApp.test_app_path.join('config/deploy/production.rb')
+  expect(File.exist?(staging)).to be true
+  expect(File.exist?(production)).to be true
+end
     
-        def _send_keys(t, e)
-      '#{project.tmux} send-keys -t #{t} #{e} C-m'
-    end
+          ServerKey = Struct.new(:hostname, :port)
+    
+    %i(git_strategy hg_strategy svn_strategy).each do |strategy|
+  validate(strategy) do |key, _value|
+    warn(
+      '[Deprecation Warning] #{key} is deprecated and will be removed in '\
+      'Capistrano 3.7.0.\n'\
+      'https://github.com/capistrano/capistrano/blob/master/UPGRADING-3.7.md'
+    )
   end
 end
-
     
-      describe '::load' do
-    let(:path) { File.expand_path('../../../fixtures/sample.yml', __FILE__) }
-    let(:options) { {} }
-    
-        msg = 'Actual pane does not match expected'
-    msg << '\n  Expected #{@commands} but has #{actual.commands}' if @commands
-    msg << '\n  Expected pane to have #{@expected_attrs}' if @expected_attrs
+      def self.send_to_buffer(text, buffer_profile_id_code)
+    client = Buffer::Client.new(ApplicationConfig['BUFFER_ACCESS_TOKEN'])
+    client.create_update(
+      body: {
+        text:
+        text,
+        profile_ids: [
+          buffer_profile_id_code,
+        ]
+      },
+    )
   end
     
-        COMMANDS = {
-      commands: 'Lists commands available in tmuxinator',
-      completions: 'Used for shell completion',
-      new: 'Create a new project file and open it in your editor',
-      edit: 'Alias of new',
-      open: 'Alias of new',
-      start: %w{
-        Start a tmux session using a project's name (with an optional [ALIAS]
-        for project reuse) or a path to a project config file (via the -p flag)
-      }.join(' '),
-      stop: 'Stop a tmux session using a project's tmuxinator config',
-      local: 'Start a tmux session using ./.tmuxinator.yml',
-      debug: 'Output the shell commands that are generated by tmuxinator',
-      copy: %w{
-        Copy an existing project to a new project and
-        open it in your editor
-      }.join(' '),
-      delete: 'Deletes given project',
-      implode: 'Deletes all tmuxinator projects',
-      version: 'Display installed tmuxinator version',
-      doctor: 'Look for problems in your configuration',
-      list: 'Lists all tmuxinator projects'
-    }.freeze
-    
-        context 'environment variable $TMUXINATOR_CONFIG is nil' do
-      it 'is an empty string' do
-        allow(ENV).to receive(:[]).with('TMUXINATOR_CONFIG').
-          and_return nil
-        # allow(XDG).to receive(:[]).with('CONFIG').and_return nil
-        allow(File).to receive(:directory?).and_return true
-        expect(described_class.environment).to eq ''
-      end
-    end
-    
-    describe Tmuxinator::Doctor do
-  describe '.installed?' do
-    context 'tmux is installed' do
-      before do
-        allow(Kernel).to receive(:system) { true }
-      end
+      private
