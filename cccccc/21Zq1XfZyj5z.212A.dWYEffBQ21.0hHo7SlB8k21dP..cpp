ef6d760de4495eea8480370b59a094c334d82866
SUNsung <weatherius@gmail.com>
2019-09-16 21:19:21 +0300

    Rewrite [21Zq1XfZyj5z.212A.dWYEffBQ21.0hHo7SlB8k21dP..cpp] File in: 2019-09-16 21:19:21

--- cccccc/21Zq1XfZyj5z.212A.dWYEffBQ21.0hHo7SlB8k21dP..cpp
+++ cccccc/21Zq1XfZyj5z.212A.dWYEffBQ21.0hHo7SlB8k21dP..cpp
@@ -1,166 +1,178 @@
 
         
-        #endif  // TESSERACT_CCMAIN_PARAGRAPHS_H_
+        template <>
+IMF_EXPORT
+const char *FloatVectorAttribute::staticTypeName ();
+    
+    
+    
+    
+class GenericOutputFile
+{
+    public:
+        IMF_EXPORT
+        virtual ~GenericOutputFile() {}
+    }
+    
+    
+void
+hufPackEncTable
+    (const Int64*	hcode,		// i : encoding table [HUF_ENCSIZE]
+     int		im,		// i : min hcode index
+     int		iM,		// i : max hcode index
+     char**		pcode)		//  o: ptr to packed table (updated)
+{
+    char *p = *pcode;
+    Int64 c = 0;
+    int lc = 0;
+    }
+    
+    
+OStream::~OStream ()
+{
+    // empty
+}
+    
+    OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_ENTER
+    
+    
+    {        // We don't use CreateFromFile() here since the user might specify OutputNodeNames in the config.
+        // By not compiling the network before patching, we avoid double log output for validation.
+        net = make_shared<ComputationNetwork>(deviceId);
+        net->SetTraceLevel(config(L'traceLevel', 0));
+        net->Read<ElemType>(modelPath);
+        if (outputNodeNames.size() > 0)
+            PatchOutputNodes(net, outputNodeNames, outputNodeNamesVector);
+        net->CompileNetwork();
+    }
+    
+    // dictionary of parameters
+// care should be used when using this class it has parent links to stack variables
+// which are assumed to exist and have lifetimes that are allocated and freed in a FIFO manner.
+// If this is not the case for a particular variable (stored in a class or something), you must
+// call ClearParent() to disconnect it from it's parents before they are freed.
+// usage: This class is intended to be used as local variables where the 'parent'
+// parameters have lifetimes longer than the 'child' parameters
+// for example:
+// int wmain(int argc, wchar_t* argv[]) {
+//    ConfigParameters config = ConfigParameters::ParseCommandLine(argc, argv);
+//    A(config);
+// }
+// void A(const ConfigParameters& config) {ConfigParameters subkey1 = config('a'); /* use the config params */ B(subkey);}
+// void B(const ConfigParameters& config) {ConfigParameters subkey2 = config('b'); /* use the config params */}
+class ConfigParameters : public ConfigParser, public ConfigDictionary
+{
+    // WARNING: the parent pointer use requires parent lifetimes be longer than or equal to children.
+    const ConfigParameters* m_parent;
+    }
+    
+    
+template <typename ElemType>
+void CNTKEvalBase<ElemType>::Init(const std::string& config)
+{
+    m_config.Parse(config);
+    size_t nThreads = m_config('numCPUThreads', '1');
+    CPUMatrix<ElemType>::SetNumThreads(nThreads);
+    }
+    
+    template std::shared_ptr<IDistGradAggregator<double>> GetSimpleDistGradAggregator<double>(
+    const MPIWrapperPtr& mpi,
+    bool useAsyncAggregation,
+    int deviceId,
+    int syncStatsTrace,
+    size_t packThresholdSizeInBytes,
+    bool useFP16AllReduce);
+    
+    TEST(pipe, unixsock)
+{
+    swPipe p;
+    char buf[1024];
+    bzero(&p, sizeof(p));
+    int ret = swPipeUnsock_create(&p, 1, SOCK_DGRAM);
+    ASSERT_EQ(ret, 0);
+    }
+    
+            int setContext(redisAsyncContext * ac) {
+            if (ac->ev.data != NULL) {
+                return REDIS_ERR;
+            }
+            m_ctx = ac;
+            m_ctx->ev.data = this;
+            m_ctx->ev.addRead = RedisQtAddRead;
+            m_ctx->ev.delRead = RedisQtDelRead;
+            m_ctx->ev.addWrite = RedisQtAddWrite;
+            m_ctx->ev.delWrite = RedisQtDelWrite;
+            m_ctx->ev.cleanup = RedisQtCleanup;
+            return REDIS_OK;
+        }
+    
+    TEST(lru_cache, lru_kick)
+{
+    dtor_num = 0;
+    shared_ptr<lru_cache_test_class> val = make_shared<lru_cache_test_class>();
+    shared_ptr<lru_cache_test_class> val1 = make_shared<lru_cache_test_class>();
+    shared_ptr<lru_cache_test_class> val2 = make_shared<lru_cache_test_class>();
+    shared_ptr<lru_cache_test_class> val3 = make_shared<lru_cache_test_class>();
+    }
+    
+    /* ERRORS */
+static void do_from_to_zval_err(struct err_s *err,
+								zend_llist *keys,
+								const char *what_conv,
+								const char *fmt,
+								va_list ap)
+{
+	smart_str			path = {0};
+	const char			**node;
+	char				*user_msg;
+	int					user_msg_size;
+	zend_llist_position	pos;
+    }
+    
+    
+    {	return res == 0 ? SUCCESS : FAILURE;
+}
 
     
+    PHP_METHOD(swoole_atomic, set)
+{
+    sw_atomic_t *atomic = (sw_atomic_t *) swoole_get_object(ZEND_THIS);
+    zend_long set_value;
+    }
     
-    { private:
-  double total_weight;         // no of elements or sum of weights.
-  double sigx;                 // sum of x
-  double sigy;                 // sum of y
-  double sigxx;                // sum x squared
-  double sigxy;                // sum of xy
-  double sigyy;                // sum y squared
-};
+    namespace mars {
+    }
     
-      // ValidFirstLine() and ValidBodyLine() take arguments describing a text line
-  // in a block of text which we are trying to model:
-  //   lmargin, lindent:  these add up to the distance from the leftmost ink
-  //                      in the text line to the surrounding text block's left
-  //                      edge.
-  //   rmargin, rindent:  these add up to the distance from the rightmost ink
-  //                      in the text line to the surrounding text block's right
-  //                      edge.
-  // The caller determines the division between 'margin' and 'indent', which
-  // only actually affect whether we think the line may be centered.
-  //
-  // If the amount of whitespace matches the amount of whitespace expected on
-  // the relevant side of the line (within tolerance_) we say it matches.
+            char* zero_data = new char[_size];
+        memset(zero_data, 0, _size);
     
-    #define           MAP_ACCEPT '1'
-#define           MAP_REJECT_PERM '0'
-#define           MAP_REJECT_TEMP '2'
-#define           MAP_REJECT_POTENTIAL '3'
+    /*
+ * app_logic.h
+ *
+ *  Created on: 2016/3/3
+ *      Author: caoshaokun
+ */
     
+      private:
+    void __ReportData();
+    bool __IsShouldReport() const;
     
-    {}  // namespace tesseract.
-    
-    
-    {  // WARNING! Keep data as the first element! KDPairInc and KDPairDec depend
-  // on the order of these elements so they can downcast pointers appropriately
-  // for use by GenericHeap::Reshuffle.
-  Data data;
-  Key key;
-};
-// Specialization of KDPair to provide operator< for sorting in increasing order
-// and recasting of data pointers for use with DoublePtr.
-template <typename Key, typename Data>
-struct KDPairInc : public KDPair<Key, Data> {
-  KDPairInc() = default;
-  KDPairInc(Key k, Data d) : KDPair<Key, Data>(k, d) {}
-  // Operator< facilitates sorting in increasing order.
-  int operator<(const KDPairInc<Key, Data>& other) const {
-    return this->key < other.key;
-  }
-  // Returns the input Data pointer recast to a KDPairInc pointer.
-  // Just casts a pointer to the first element to a pointer to the whole struct.
-  static KDPairInc* RecastDataPointer(Data* data_ptr) {
-    return reinterpret_cast<KDPairInc*>(data_ptr);
-  }
-};
-// Specialization of KDPair to provide operator< for sorting in decreasing order
-// and recasting of data pointers for use with DoublePtr.
-template <typename Key, typename Data>
-struct KDPairDec : public KDPair<Key, Data> {
-  KDPairDec() = default;
-  KDPairDec(Key k, Data d) : KDPair<Key, Data>(k, d) {}
-  // Operator< facilitates sorting in decreasing order by using operator> on
-  // the key values.
-  int operator<(const KDPairDec<Key, Data>& other) const {
-    return this->key > other.key;
-  }
-  // Returns the input Data pointer recast to a KDPairDec pointer.
-  // Just casts a pointer to the first element to a pointer to the whole struct.
-  static KDPairDec* RecastDataPointer(Data* data_ptr) {
-    return reinterpret_cast<KDPairDec*>(data_ptr);
-  }
-};
-    
-    
-    {  UNICHARMAP_NODE* nodes;
-};
-    
-      // If we manage the given dawg, decrement its count,
-  // and possibly delete it if the count reaches zero.
-  // If dawg is unknown to us, return false.
-  bool FreeDawg(Dawg *dawg) {
-    return dawgs_.Free(dawg);
-  }
-    
-    TEST_F(DistanceApproachIPOPTInterfaceTest, eval_f) {
-  int n = 1274;
-  double obj_value = 0.0;
-  double x[1274];
-  std::fill_n(x, n, 1.2);
-  bool res = ptop_->eval_f(n, x, true, obj_value);
-  EXPECT_DOUBLE_EQ(obj_value, 1443.3600000000008) << 'eval_f: ' << obj_value;
-  EXPECT_TRUE(res);
+    inline wchar_t to_wchar(size_t i) {
+    if (i <= 9) {
+        return static_cast<wchar_t>(L'0' + i);
+    } else {
+        return static_cast<wchar_t>(L'a' + (i-10));
+    }
 }
     
-      ValetParkingContext* GetContext() {
-    return GetContextAs<ValetParkingContext>();
-  }
-    
-    GemController::~GemController() {}
-    
-    namespace apollo {
-namespace planning {
-namespace scenario {
-namespace valet_parking {
-class StageApproachingParkingSpotTest : public ::testing::Test {
- public:
-  virtual void SetUp() {
-    config_.set_stage_type(
-        ScenarioConfig::VALET_PARKING_APPROACHING_PARKING_SPOT);
-  }
-    }
-    }
-    }
-    }
-    }
-    
-    
-    {    K key = 0;
-    std::atomic<V *> value_ptr = {nullptr};
-    std::atomic<Entry *> next = {nullptr};
-  };
-    
-    bool ChannelManager::IsMessageTypeMatching(const std::string& lhs,
-                                           const std::string& rhs) {
-  if (lhs == rhs) {
-    return true;
-  }
-  if (exempted_msg_types_.count(lhs) > 0) {
-    return true;
-  }
-  if (exempted_msg_types_.count(rhs) > 0) {
-    return true;
-  }
-  return false;
+    int SocketSelect::Select() {
+    return Select(WSA_INFINITE);
 }
     
+            parray_ = (unsigned char*) p;
     
-    {
-    {    for (const auto& pair : data) {
-      auto status = batch.Put(handle_ptr.get(), pair.first, pair.second);
-      if (!status.ok()) {
-        auto batch_write_error = createError(RocksdbError::BatchWriteFail)
-                                 << status.ToString();
-        return createError(DatabaseError::FailToWriteData,
-                           std::move(batch_write_error))
-               << 'Failed to write data';
-      }
-    }
-    auto status = db_->Write(batch_write_options_, &batch);
-    if (!status.ok()) {
-      return createError(DatabaseError::FailToWriteData) << status.ToString();
-    }
-  }
-  return handle.takeError();
-}
     
-    namespace osquery {
+namespace mars_boost {} namespace boost = mars_boost; namespace mars_boost {
     }
     
-    namespace osquery {
-    }
+        ~ScopedJstring();
