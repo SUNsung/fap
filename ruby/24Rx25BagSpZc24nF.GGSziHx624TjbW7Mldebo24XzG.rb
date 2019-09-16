e3df8381ecd8adf9500cfc08f583bbaa17d934a3
SUNsung <weatherius@gmail.com>
2019-09-16 13:40:38 +0300

    Rewrite [24Rx25BagSpZc24nF.GGSziHx624TjbW7Mldebo24XzG.rb] File in: 2019-09-16 13:40:38

--- ruby/24Rx25BagSpZc24nF.GGSziHx624TjbW7Mldebo24XzG.rb
+++ ruby/24Rx25BagSpZc24nF.GGSziHx624TjbW7Mldebo24XzG.rb
@@ -1,145 +1,101 @@
 
         
-          def create_user(attr)
-    allowed_keys = %w[
-      email strip_exif show_community_spotlight_in_stream language disable_mail auto_follow_back
-    ]
-    data = convert_keys(archive_hash['user'], allowed_keys)
-    # setting getting_started to false as the user doesn't need to see the getting started wizard
-    data.merge!(
-      username:              attr[:username],
-      password:              attr[:password],
-      password_confirmation: attr[:password],
-      getting_started:       false,
-      person:                {
-        profile_attributes: profile_attributes
-      }
-    )
-    self.user = User.build(data)
-    user.save!
+          if current_adapter?(:PostgreSQLAdapter)
+    def test_numeric_fields_with_nan
+      m = NumericData.new(
+        bank_balance: BigDecimal('NaN'),
+        big_bank_balance: BigDecimal('NaN'),
+        world_population: 2**62,
+        my_house_population: 3
+      )
+      assert_predicate m.bank_balance, :nan?
+      assert_predicate m.big_bank_balance, :nan?
+      assert m.save
+    
+      # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+    
+      def target_uri
+    value_or_id(@json['target'])
   end
     
-      def return_photo_error(message)
-    respond_to do |format|
-      format.json { render(layout: false, json: {'success' => false, 'error' => message}.to_json) }
-      format.html { render(layout: false, json: {'success' => false, 'error' => message}.to_json) }
+        it 'returns http success' do
+      expect(response).to have_http_status(200)
     end
-  end
-end
-
     
-        context 'Conversation' do
-      let(:participant) { FactoryGirl.create(:person) }
-      let(:diaspora_entity) { FactoryGirl.create(:conversation_with_message, participants: [participant]) }
-      let(:federation_entity) { described_class.build(diaspora_entity) }
+      spec.summary       = %q{Provides access to information typically stored in UNIX /etc directory.}
+  spec.description   = spec.summary
+  spec.homepage      = 'https://github.com/ruby/etc'
+  spec.license       = 'BSD-2-Clause'
     
-      private
+        private
+    def prepare
+      @encoding = @options[:encoding]
     
-      def respond_destroy_success
-    respond_to do |format|
-      format.mobile { redirect_back fallback_location: stream_path }
-      format.js { head :no_content }
-      format.json { head :no_content }
-    end
-  end
-    
-      def contacts_by_type(type)
-    order = ['profiles.first_name ASC', 'profiles.last_name ASC', 'profiles.diaspora_handle ASC']
-    contacts = case type
-      when 'all'
-        order.unshift 'receiving DESC'
-        current_user.contacts
-      when 'only_sharing'
-        current_user.contacts.only_sharing
-      when 'receiving'
-        current_user.contacts.receiving
-      when 'by_aspect'
-        order.unshift 'contact_id IS NOT NULL DESC'
-        contacts_by_aspect(@aspect.id)
-      else
-        raise ArgumentError, 'unknown type #{type}'
+          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
+      def create
+        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
+        client = Api::OpenidConnect::OAuthApplication.register! registrar
+        render json: client.as_json(root: false)
       end
-    contacts.includes(person: :profile)
-            .order(order)
-  end
     
-      task :print_config_variables do
-    puts
-    puts '------- Printing current config variables -------'
-    env.keys.each do |config_variable_key|
-      if is_question?(config_variable_key)
-        puts '#{config_variable_key.inspect} => Question (awaits user input on next fetch(#{config_variable_key.inspect}))'
+    class HomeController < ApplicationController
+  def show
+    partial_dir = Rails.root.join('app', 'views', 'home')
+    if user_signed_in?
+      redirect_to stream_path
+    elsif request.format == :mobile
+      if partial_dir.join('_show.mobile.haml').exist? ||
+         partial_dir.join('_show.mobile.erb').exist? ||
+         partial_dir.join('_show.haml').exist?
+        render :show
       else
-        puts '#{config_variable_key.inspect} => #{fetch(config_variable_key).inspect}'
+        redirect_to user_session_path
       end
-    end
-    
-    World(RemoteCommandHelpers)
-
-    
-      at_exit do
-    if ENV['KEEP_RUNNING']
-      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
-      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
+    elsif partial_dir.join('_show.html.haml').exist? ||
+          partial_dir.join('_show.html.erb').exist? ||
+          partial_dir.join('_show.haml').exist?
+      render :show
+    elsif Role.admins.any?
+      render :default
     else
-      vagrant_cli_command('destroy -f')
+      redirect_to podmin_path
     end
   end
     
-        def install_plugin(plugin, load_hooks: true, load_immediately: false)
-      installer.install(plugin,
-                        load_hooks: load_hooks,
-                        load_immediately: load_immediately)
-    end
-    
-        specify 'fetch article id only if :article_id key in hash ' do
-      allow(Article).to receive(:find_by)
-      described_class.perform_now('Reaction', 456, milestone_service)
-      expect(Article).to have_received(:find_by).with(id: 456)
-    end
-    
-    def capture_logging(lvl=Logger::INFO)
-  old = Sidekiq.logger
-  begin
-    out = StringIO.new
-    logger = Logger.new(out)
-    logger.level = lvl
-    Sidekiq.logger = logger
-    yield
-    out.string
-  ensure
-    Sidekiq.logger = old
-  end
-end
-    
-        def setup
-      Sidekiq.redis {|c| c.flushdb }
-    end
-    
-          JoeWorker.perform_in(0.01, 0)
-    
-        describe 'when the exception does not have a backtrace' do
-      it 'does not fail' do
-        exception = ExceptionHandlerTestException.new
-        assert_nil exception.backtrace
-    
-        Thank you for installing tmuxinator.
-    
-        it 'returns the string' do
-      expect(project.send('hook_#{hook_name}')).to eq('echo 'on hook'')
-    end
+      def index
+    render json: like_service.find_for_post(params[:post_id])
+      .includes(author: :profile)
+      .as_api_response(:backbone)
   end
     
-          # $TMUXINATOR_CONFIG (and create directory) or ''.
-      def environment
-        environment = ENV['TMUXINATOR_CONFIG']
-        return '' if environment.to_s.empty? # variable is unset (nil) or blank
-        FileUtils::mkdir_p(environment) unless File.directory?(environment)
-        environment
+        it 'handles symbols and strings' do
+      q = Sidekiq::Queue.new('bar')
+      assert_equal 0, q.size
+      assert SetWorker.set('queue' => 'bar', :retry => 11).perform_async(1)
+      job = q.first
+      assert_equal 'bar', job['queue']
+      assert_equal 11, job['retry']
+    
+          a = $count
+      mgr.mutex.synchronize do
+        p.start
+        mgr.cond.wait(mgr.mutex)
       end
+      b = $count
+      assert_equal a, b
     
-      describe '.editor?' do
-    context '$EDITOR is set' do
-      before do
-        allow(ENV).to receive(:[]).with('EDITOR') { 'vim' }
-      end
+        after do
+      Sidekiq::Testing.disable!
+    end
+    
+      class InlineWorker
+    include Sidekiq::Worker
+    def perform(pass)
+      raise ArgumentError, 'no jid' unless jid
+      raise InlineError unless pass
+    end
+  end
