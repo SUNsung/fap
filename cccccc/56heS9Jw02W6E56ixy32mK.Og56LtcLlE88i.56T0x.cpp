5ced2dcf06539067014bc0383ba1dd40fe9148a5
SUNsung <weatherius@gmail.com>
2019-09-16 16:30:50 +0300

    Rewrite [56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp] File in: 2019-09-16 16:30:50

--- cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
+++ cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
@@ -1,172 +1,45 @@
 
         
-          CHROMEG_CALLBACK_1(GlobalMenuBarRegistrarX11,
-                     void,
-                     OnProxyCreated,
-                     GObject*,
-                     GAsyncResult*);
-  CHROMEG_CALLBACK_1(GlobalMenuBarRegistrarX11,
-                     void,
-                     OnNameOwnerChanged,
-                     GObject*,
-                     GParamSpec*);
-    
-    #include 'native_mate/promise.h'
-    
-    
-    {  AfterInit(isolate);
+        static float unitnorm(float x){
+  if(x<0)return(-1.f);
+  return(1.f);
 }
     
-    namespace mate {
-    }
+    #define MULADDC_CORE                    \
+        'ldq    $6,  0($1)      \n\t'   \
+        'addq   $1,  8, $1      \n\t'   \
+        'mulq   $6, $4, $7      \n\t'   \
+        'umulh  $6, $4, $6      \n\t'   \
+        'addq   $7, $3, $7      \n\t'   \
+        'cmpult $7, $3, $3      \n\t'   \
+        'ldq    $5,  0($2)      \n\t'   \
+        'addq   $7, $5, $7      \n\t'   \
+        'cmpult $7, $5, $5      \n\t'   \
+        'stq    $7,  0($2)      \n\t'   \
+        'addq   $2,  8, $2      \n\t'   \
+        'addq   $6, $3, $3      \n\t'   \
+        'addq   $5, $3, $3      \n\t'
     
-      // Get the V8 wrapper of |web_content|, return empty handle if not wrapped.
-  static mate::Handle<WebContents> From(v8::Isolate* isolate,
-                                        content::WebContents* web_content);
-    
-    
-    {  // Check if the optional_nested_message was actually moved (and not just
-  // copied).
-  EXPECT_EQ(nested, &message2.optional_nested_message());
-  EXPECT_NE(nested, &message1.optional_nested_message());
-}
-    
-    #include <google/protobuf/compiler/command_line_interface.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/io/printer.h>
-    
-    Status& Status::operator=(const Status& other) {
-  error_code_ = other.error_code_;
-  error_message_ = other.error_message_;
-  return *this;
-}
-    
-    TEST(StatusOr, TestPointerValue) {
-  const int kI = 0;
-  StatusOr<const int*> thing(&kI);
-  EXPECT_EQ(&kI, thing.ValueOrDie());
-}
-    
-    
-    {
-    {
-    {}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-    
-      void ScrubFile(FileDescriptorProto *file) {
-    for (int i = 0; i < file->enum_type_size(); i++) {
-      ScrubEnum(file->mutable_enum_type(i));
-    }
-    for (int i = 0; i < file->mutable_message_type()->size(); i++) {
-      ScrubMessage(file->mutable_message_type(i));
-    }
-  }
-    
-      {
-    // Read the existing address book.
-    fstream input(argv[1], ios::in | ios::binary);
-    if (!input) {
-      cout << argv[1] << ': File not found.  Creating a new file.' << endl;
-    } else if (!address_book.ParseFromIstream(&input)) {
-      cerr << 'Failed to parse address book.' << endl;
-      return -1;
-    }
-  }
-    
-    // Iterates though all people in the AddressBook and prints info about them.
-void ListPeople(const tutorial::AddressBook& address_book) {
-  for (int i = 0; i < address_book.people_size(); i++) {
-    const tutorial::Person& person = address_book.people(i);
-    }
-    }
-    
-    ]]
-    
-    #define EXPECT_NONFATAL_FAILURE_ON_ALL_THREADS(statement, substr) \
-  do {\
-    ::testing::TestPartResultArray gtest_failures;\
-    ::testing::internal::SingleFailureChecker gtest_checker(\
-        &gtest_failures, ::testing::TestPartResult::kNonFatalFailure, \
-        (substr));\
-    {\
-      ::testing::ScopedFakeTestPartResultReporter gtest_reporter(\
-          ::testing::ScopedFakeTestPartResultReporter::INTERCEPT_ALL_THREADS, \
-          &gtest_failures);\
-      if (::testing::internal::AlwaysTrue()) { statement; }\
-    }\
-  } while (::testing::internal::AlwaysFalse())
-    
-    
-    {  return AssertionFailure() << pred_text << '('
-                            << e1 << ', '
-                            << e2 << ', '
-                            << e3 << ', '
-                            << e4 << ') evaluates to false, where'
-                            << '\n' << e1 << ' evaluates to ' << v1
-                            << '\n' << e2 << ' evaluates to ' << v2
-                            << '\n' << e3 << ' evaluates to ' << v3
-                            << '\n' << e4 << ' evaluates to ' << v4;
-}
-    
-    #include 'gtest/internal/gtest-internal.h'
-    
-      // Returns a copy of the FilePath with the case-insensitive extension removed.
-  // Example: FilePath('dir/file.exe').RemoveExtension('EXE') returns
-  // FilePath('dir/file'). If a case-insensitive extension is not
-  // found, returns a copy of the original FilePath.
-  FilePath RemoveExtension(const char* extension) const;
-    
-    // Gets the content of the stringstream's buffer as an std::string.  Each '\0'
-// character in the buffer is replaced with '\\0'.
-GTEST_API_ std::string StringStreamToString(::std::stringstream* stream);
-    
-      // Now, we have that n is odd and n >= 3.
-    
-    #include <string.h>
-    
-      // Adds an element to the end of the queue.  A copy of the element is
-  // created using the copy constructor, and then stored in the queue.
-  // Changes made to the element in the queue doesn't affect the source
-  // object, and vice versa.
-  void Enqueue(const E& element) {
-    QueueNode<E>* new_node = new QueueNode<E>(element);
-    }
-    
-    bool AuthPropertyIterator::operator==(const AuthPropertyIterator& rhs) const {
-  if (property_ == nullptr || rhs.property_ == nullptr) {
-    return property_ == rhs.property_;
-  } else {
-    return index_ == rhs.index_;
-  }
-}
-    
-    // A CallData class will be created for every grpc call within a channel. It is
-// used to store data and methods specific to that call. CensusClientCallData is
-// thread-compatible, however typically only 1 thread should be interacting with
-// a call at a time.
-class CensusClientCallData : public CallData {
- public:
-  // Maximum size of trace context is sent on the wire.
-  static constexpr uint32_t kMaxTraceContextLen = 64;
-  // Maximum size of tags that are sent on the wire.
-  static constexpr uint32_t kMaxTagsLen = 2048;
-    }
-    
-      void FillErrorResponse(const Status& status,
-                         reflection::v1alpha::ErrorResponse* error_response);
-    
-    #include <grpcpp/impl/channel_argument_option.h>
-    
-    #endif  // !defined(GPR_LINUX) && !defined(GPR_WINDOWS) && !defined(GPR_APPLE)
-
-    
-    namespace {
-    }
-    
-    LoadRecordValue::LoadRecordValue(grpc::string metric_name, uint64_t num_calls,
-                                 double total_metric_value) {
-  call_metrics_.emplace(std::move(metric_name),
-                        CallMetricValue(num_calls, total_metric_value));
-}
+    		char      *lPr = (char *)malloc(MAX_PATH);
+		char      *pR = (char *)malloc(MAX_PATH);
+		size_t i;
+		wcstombs_s(&i, lPr, (size_t)MAX_PATH,
+			legacyUserAliasesPath, (size_t)MAX_PATH);
+		wcstombs_s(&i, pR, (size_t)MAX_PATH,
+			userAliasesPath, (size_t)MAX_PATH);
+		rename(lPr, pR);
+	}
+	
+	/*
+	Was /c [path] specified?
+	*/
+	if (wcscmp(userConfigDirPath, L'') == 0)
+	{
+		// No - It wasn't. 
+		PathCombine(userConfigDirPath, exeDir, L'config');
+	}
+	else
+	{
+		// Yes - It was.
+		PathCombine(userBinDirPath, userConfigDirPath, L'bin');
+		SHCreateDirectoryEx(0, userBinDirPath, 0);
