9f4ca7b33b9e80ae489b5ab31e209aad998d9d8f
SUNsung <weatherius@gmail.com>
2019-09-16 12:28:07 +0300

    Rewrite [19ddcELMfFTDk19FElcMMzI2DM19hefjfRtYRbs199fj.py] File in: 2019-09-16 12:28:06

--- pyton/19ddcELMfFTDk19FElcMMzI2DM19hefjfRtYRbs199fj.py
+++ pyton/19ddcELMfFTDk19FElcMMzI2DM19hefjfRtYRbs199fj.py
@@ -1,96 +1,58 @@
 
         
-            def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
-                          help='set spider argument (may be repeated)')
-        parser.add_option('-o', '--output', metavar='FILE',
-                          help='dump scraped items into FILE (use - for stdout)')
-        parser.add_option('-t', '--output-format', metavar='FORMAT',
-                          help='format to use for dumping items with -o')
+            def test_inline_media_only_inline(self):
+        holder = Holder3(dummy=13)
+        holder.save()
+        Inner3(dummy=42, holder=holder).save()
+        change_url = reverse('admin:admin_inlines_holder3_change', args=(holder.id,))
+        response = self.client.get(change_url)
+        self.assertEqual(
+            response.context['inline_admin_formsets'][0].media._js,
+            [
+                'admin/js/vendor/jquery/jquery.min.js',
+                'my_awesome_inline_scripts.js',
+                'custom_number.js',
+                'admin/js/jquery.init.js',
+                'admin/js/inlines.min.js',
+            ]
+        )
+        self.assertContains(response, 'my_awesome_inline_scripts.js')
     
-        def syntax(self):
-        return '<spider>'
-    
-        def _genspider(self, module, name, domain, template_name, template_file):
-        '''Generate the spider module, based on the given template'''
-        tvars = {
-            'project_name': self.settings.get('BOT_NAME'),
-            'ProjectName': string_camelcase(self.settings.get('BOT_NAME')),
-            'module': module,
-            'name': name,
-            'domain': domain,
-            'classname': '%sSpider' % ''.join(s.capitalize() \
-                for s in module.split('_'))
-        }
-        if self.settings.get('NEWSPIDER_MODULE'):
-            spiders_module = import_module(self.settings['NEWSPIDER_MODULE'])
-            spiders_dir = abspath(dirname(spiders_module.__file__))
+                if 'skew' in ds_input:
+                self.skew.x, self.skew.y = ds_input['skew']
+        elif isinstance(ds_input, c_void_p):
+            # Instantiate the object using an existing pointer to a gdal raster.
+            self._ptr = ds_input
         else:
-            spiders_module = None
-            spiders_dir = '.'
-        spider_file = '%s.py' % join(spiders_dir, module)
-        shutil.copyfile(template_file, spider_file)
-        render_templatefile(spider_file, **tvars)
-        print('Created spider %r using template %r ' % (name, \
-            template_name), end=('' if spiders_module else '\n'))
-        if spiders_module:
-            print('in module:\n  %s.%s' % (spiders_module.__name__, module))
+            raise GDALException('Invalid data source input type: '{}'.'.format(type(ds_input)))
     
-        # Stripping scripts and comments is slow (about 20x slower than
-    # just checking if a string is in text); this is a quick fail-fast
-    # path that should work for most pages.
-    if 'fragment' not in text:
-        return False
-    if 'content' not in text:
-        return False
+        def set_defaults(self):
+        ''' Set the default values for config '''
+        logger.debug('Setting defaults')
+        current_dir = os.path.dirname(__file__)
+        for dirpath, _, filenames in os.walk(current_dir):
+            default_files = [fname for fname in filenames if fname.endswith('_defaults.py')]
+            if not default_files:
+                continue
+            base_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+            import_path = '.'.join(full_path_split(dirpath.replace(base_path, ''))[1:])
+            plugin_type = import_path.split('.')[-1]
+            for filename in default_files:
+                self.load_module(filename, import_path, plugin_type)
     
-        def process_request(self, request, spider):
-        if self._timeout:
-            request.meta.setdefault('download_timeout', self._timeout)
-
-    
-        def report_not_exist(self, appid, ip):
-        self.logger.debug('report_not_exist:%s %s', appid, ip)
-        th = threading.Thread(target=self.process_appid_not_exist, args=(appid, ip))
-        th.start()
-    
-    
-    def insertBefore(self, *args):
-        if len(args) == 2:
-            programName = self.DEFAULT_PROGRAM_NAME
-            index = args[0]
-            text = args[1]
-            
-        elif len(args) == 3:
-            programName = args[0]
-            index = args[1]
-            text = args[2]
-    
-        API_ENDPOINT = 'http://p.bokecc.com/'
-    
-        if '_text' in dictified['flashvars'][0]:
-        video_dict['flashvars'] = dictified['flashvars'][0]['_text'].strip()
-    
-    
-@contextlib.contextmanager
-def capture_ops():
-    '''Decorator to capture ops created in the block.
-    with capture_ops() as ops:
-        # create some ops
-    print(ops) # => prints ops created.
-    '''
-    
-    
-class facehull(Mask):  # pylint: disable=invalid-name
-    ''' Basic face hull mask '''
-    def build_mask(self):
-        mask = np.zeros(self.face.shape[0:2] + (1, ), dtype=np.float32)
-        hull = cv2.convexHull(  # pylint: disable=no-member
-            np.array(self.landmarks).reshape((-1, 2)))
-        cv2.fillConvexPoly(mask, hull, 255.0, lineType=cv2.LINE_AA)  # pylint: disable=no-member
-        return mask
-
+        def set_config(self, configfile, config):
+        ''' Set the config to either global config or passed in config '''
+        section = '.'.join(self.__module__.split('.')[-2:])
+        if config is None:
+            logger.debug('Loading base config')
+            retval = get_config(section, configfile=configfile)
+        else:
+            logger.debug('Loading passed in config')
+            config.section = section
+            retval = config.config_dict
+            config.section = None
+        logger.debug('Config: %s', retval)
+        return retval
     
         def load_module(self, filename, module_path, plugin_type):
         ''' Load the defaults module and add defaults '''
@@ -106,45 +68,124 @@ class facehull(Mask):  # pylint: disable=invalid-name
         logger.debug('Added defaults: %s', section)
 
     
-            For actions that occur identically for each frame (e.g. blend_box), constants can
-        be placed into self.func_constants to be compiled at launch, then referenced for
-        each face. '''
-    def __init__(self, mask_type, output_size, predicted_available=False, **kwargs):
-        super().__init__(mask_type, output_size, predicted_available, **kwargs)
-        self.mask = self.get_mask() if not self.skip else None
     
-    import cv2
-from ._base import Output, logger
+def icnr_keras(shape, dtype=None):
+    '''
+    Custom initializer for subpix upscaling
+    From https://github.com/kostyaev/ICNR
+    Note: upscale factor is fixed to 2, and the base initializer is fixed to random normal.
+    '''
+    # TODO Roll this into ICNR_init when porting GAN 2.2
+    shape = list(shape)
+    scale = 2
+    initializer = tf.keras.initializers.RandomNormal(0, 0.02)
     
-        The following variables should be defined:
-        _HELPTEXT: A string describing what this plugin does
-        _DEFAULTS: A dictionary containing the options, defaults and meta information. The
-                   dictionary should be defined as:
-                       {<option_name>: {<metadata>}}
+        def __init__(self, landmarks, face, channels=4):
+        logger.trace('Initializing %s: (face_shape: %s, channels: %s, landmarks: %s)',
+                     self.__class__.__name__, face.shape, channels, landmarks)
+        self.landmarks = landmarks
+        self.face = face
+        self.channels = channels
     
-    import cv2
-import numpy as np
-from ._base import Adjustment
+        Args:
+        dataset     原始数据集
+        n_folds     数据集dataset分成n_flods份
+    Returns:
+        dataset_split    list集合，存放的是：将数据集进行抽重抽样 n_folds 份，数据可以重复重复抽取，每一次list的元素是无重复的
+    '''
+    dataset_split = list()
+    dataset_copy = list(dataset)       # 复制一份 dataset,防止 dataset 的内容改变
+    fold_size = len(dataset) / n_folds
+    for i in range(n_folds):
+        fold = list()                  # 每次循环 fold 清零，防止重复导入 dataset_split
+        while len(fold) < fold_size:   # 这里不能用 if，if 只是在第一次判断时起作用，while 执行循环，直到条件不成立
+            # 有放回的随机采样，有一些样本被重复采样，从而在训练集中多次出现，有的则从未在训练集中出现，此则自助采样法。从而保证每棵决策树训练集的差异性            
+            index = randrange(len(dataset_copy))
+            # 将对应索引 index 的内容从 dataset_copy 中导出，并将该内容从 dataset_copy 中删除。
+            # pop() 函数用于移除列表中的一个元素（默认最后一个元素），并且返回该元素的值。
+            # fold.append(dataset_copy.pop(index))  # 无放回的方式
+            fold.append(dataset_copy[index])  # 有放回的方式
+        dataset_split.append(fold)
+    # 由dataset分割出的n_folds个数据构成的列表，为了用于交叉验证
+    return dataset_split
     
-        def forwards(self, orm):
-        # Removing unique constraint on 'EventTag', fields ['event_id', 'key', 'value']
-        db.delete_unique(u'tagstore_eventtag', ['event_id', 'key_id', 'value_id'])
     
-            # Changing field 'GroupTagValue.group_id'
-        db.alter_column(u'tagstore_grouptagvalue', 'group_id', self.gf(
-            'sentry.db.models.fields.bounded.BoundedBigIntegerField')())
+def classify0(inX, dataSet, labels, k):
+    '''
+    inx[1,2,3]
+    DS=[[1,2,3],[1,2,0]]
+    inX: 用于分类的输入向量
+    dataSet: 输入的训练样本集
+    labels: 标签向量
+    k: 选择最近邻居的数目
+    注意：labels元素数目和dataSet行数相同；程序使用欧式距离公式.
+    
+        def __str__(self):
+        '''
+        Desc:
+            将连接信息打印出来
+        Args:
+            None
+        Returns:
+            连接信息进行返回
+        '''
+        # 格式为：上游节点的层的索引+上游节点的节点索引 ---> 下游节点的层的索引+下游节点的节点索引，最后一个数是权重
+        return '(%u-%u) -> (%u-%u) = %f' % (
+            self.upstream_node.layer_index, 
+            self.upstream_node.node_index,
+            self.downstream_node.layer_index, 
+            self.downstream_node.node_index, 
+            self.weight)
+    
+        def reset_state(self):
+        # 当前时刻初始化为t0
+        self.times = 0       
+        # 各个时刻的单元状态向量c
+        self.c_list = self.init_state_vec()
+        # 各个时刻的输出向量h
+        self.h_list = self.init_state_vec()
+        # 各个时刻的遗忘门f
+        self.f_list = self.init_state_vec()
+        # 各个时刻的输入门i
+        self.i_list = self.init_state_vec()
+        # 各个时刻的输出门o
+        self.o_list = self.init_state_vec()
+        # 各个时刻的即时状态c~
+        self.ct_list = self.init_state_vec()
+    
+        # 计算forward值
+    x, d = data_set()
+    rl.forward(x[0])
+    rl.forward(x[1])
+    
+    # 求取sensitivity map
+    sensitivity_array = np.ones(rl.state_list[-1].shape,
+                                dtype=np.float64)
+    # 计算梯度
+    rl.backward(sensitivity_array, IdentityActivator())
+    
+    # 检查梯度
+    epsilon = 10e-4
+    for i in range(rl.W.shape[0]):
+        for j in range(rl.W.shape[1]):
+            rl.W[i,j] += epsilon
+            rl.reset_state()
+            rl.forward(x[0])
+            rl.forward(x[1])
+            err1 = error_function(rl.state_list[-1])
+            rl.W[i,j] -= 2*epsilon
+            rl.reset_state()
+            rl.forward(x[0])
+            rl.forward(x[1])
+            err2 = error_function(rl.state_list[-1])
+            expect_grad = (err1 - err2) / (2 * epsilon)
+            rl.W[i,j] += epsilon
+            print('weights(%d,%d): expected - actural %f - %f' % (
+                i, j, expect_grad, rl.gradient[i,j]))
     
     
-class Migration(SchemaMigration):
-    
-        def add(self, ident, func, args=None, kwargs=None):
-        if args is None:
-            args = ()
-        if kwargs is None:
-            kwargs = {}
-        task = (ident, func, args, kwargs)
-        self.tasks.append(ident)
-        self.queue.put_nowait(task)
-    
-    from django.conf import settings
-from social_auth.utils import custom_user_frozen_models
+datArr, labelList = loadDataSet('data/15.BigData_MapReduce/testSet.txt')
+datMat = mat(datArr)
+# finalWs = seqPegasos(datMat, labelList, 2, 5000)
+finalWs = batchPegasos(datMat, labelList, 2, 50, 100)
+print(finalWs)
