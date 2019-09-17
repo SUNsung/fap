689efbce42e46d91686f0ff55bf4ee63cb31ead9
SUNsung <weatherius@gmail.com>
2019-09-17 16:16:44 +0300

    Rewrite [53hgA9sgsINss53zFLSPuER9vI533SS7VePWLrw53f6c.cpp] File in: 2019-09-17 16:16:43

--- cccccc/53hgA9sgsINss53zFLSPuER9vI533SS7VePWLrw53f6c.cpp
+++ cccccc/53hgA9sgsINss53zFLSPuER9vI533SS7VePWLrw53f6c.cpp
@@ -1,50 +1,316 @@
 
         
-          /* Parse XML packet */
-  xmlDocPtr response = soap_xmlParseMemory(buffer, buffer_size);
-  if (!response) {
-    add_soap_fault(obj, 'Client', 'looks like we got no XML document');
-    return false;
-  }
-  if (xmlGetIntSubset(response) != nullptr) {
-    add_soap_fault(obj, 'Client', 'DTD are not supported by SOAP');
-    xmlFreeDoc(response);
-    return false;
-  }
-    
-      /*
-   * Add an object that occupies an index but can't be located by name.
-   */
-  void addUnnamed(const T& t);
-    
-    #include 'hphp/util/assertions.h'
-#include <atomic>
-#include <folly/portability/SysMman.h>
-    
-    // Specify the mount point of hugetlbfs with 1G page size.  Returns whether the
-// operation succeeded, i.e., the specified path is accessible, and is on a
-// hugetlbfs with 1G page size.
-bool set_hugetlbfs_path(const char* path);
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    
-    {      for (int i = 0; i < indent; ++i) {
-        os << '  ';
-      }
-      os << folly::sformat('   **** {} ({}) ==> {} [{}:{}]\n',
-                           attr_name, type, attr_value,
-                           attr_form, form);
-      return true;
+          CusolverContext* context;
+  {
+    tensorflow::mutex_lock lock(mu_);
+    auto result = contexts_.emplace(params.stream, CusolverContext());
+    if (result.second) {
+      TF_ASSIGN_OR_RETURN(result.first->second,
+                          CusolverContext::Create(params.stream));
     }
-  );
+    context = &result.first->second;
+  }
     
-    #include 'hphp/util/embedded-data.h'
+      {
+    // The infeed buffer has an extra outer tuple with a token. Adjust the index
+    // accordingly.
+    ShapeIndex index = {0};
+    std::function<void(std::vector<void*>*)> copy_tuple_contents =
+        [&](std::vector<void*>* tuple_element_addresses) {
+          const Shape& shape = ShapeUtil::GetSubshape(infeed_buffers.shape(),
+                                                      ShapeIndexView(index, 1));
+          // For the leaf buffers of the tuple copy the elements directly.
+          if (shape.IsArray()) {
+            const BufferAllocation::Slice& tuple_element_buffer =
+                infeed_slices_.element(index);
+            se::DeviceMemoryBase tuple_element_address =
+                buffer_allocations.GetDeviceAddress(tuple_element_buffer);
+    }
+    }
+    }
     
-    /*
- * Unique identifier for this hhvm binary, determined at build-time. Unlike
- * compilerId(), this is computed based on the contents of the executable and
- * thus varies depending on the type of build. It cannot be overridden and
- * serves as an id for anything relying on an exact binary.
- */
-folly::StringPiece buildId();
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    #include 'tensorflow/compiler/xla/service/gpu/outfeed_thunk.h'
+#include 'tensorflow/compiler/xla/literal.h'
+#include 'tensorflow/compiler/xla/service/gpu/hlo_execution_profiler.h'
+#include 'tensorflow/compiler/xla/service/gpu/outfeed_manager.h'
+#include 'tensorflow/compiler/xla/util.h'
+#include 'tensorflow/core/platform/stream_executor_no_cuda.h'
+    
+    SequentialThunk::SequentialThunk(std::vector<std::unique_ptr<Thunk>> thunks,
+                                 const HloInstruction* hlo)
+    : Thunk(Kind::kSequential, hlo), thunks_(std::move(thunks)) {}
+    
+    #ifndef TENSORFLOW_COMPILER_XLA_SERVICE_GPU_TRIANGULAR_SOLVE_THUNK_H_
+#define TENSORFLOW_COMPILER_XLA_SERVICE_GPU_TRIANGULAR_SOLVE_THUNK_H_
+    
+      auto op_profiler = profiler.MakeScopedInstructionProfiler(hlo_instruction());
+  while (true) {
+    // Invoke thunk sequence for while 'condition' computation.
+    profiler.StartHloComputation();
+    VLOG(3) << 'Executing condition computation';
+    TF_RETURN_IF_ERROR(condition_thunk_sequence_->ExecuteOnStream(params));
+    profiler.FinishHloComputation(hlo_instruction()->while_condition());
+    }
+    
+    Status AbsGrad(const AttrSlice& attrs, FunctionDef* g) {
+  // clang-format off
+  return GradForUnaryCwise(g, {
+      {{'sign'}, 'Sign', {'x'}, {}, {'dy'}},
+      {{'dx'}, 'Mul', {'dy', 'sign'}},
+  });
+  // clang-format on
+}
+REGISTER_OP_GRADIENT('Abs', AbsGrad);
+    
+    
+    {	return true;
+}
+bool OpusVorbisDecoder::openOpus(const WebMDemuxer &demuxer)
+{
+	int opusErr = 0;
+	m_opus = opus_decoder_create(demuxer.getSampleRate(), m_channels, &opusErr);
+	if (!opusErr)
+	{
+		m_numSamples = demuxer.getSampleRate() * 0.06 + 0.5; //Maximum frame size (for 60 ms frame)
+		return true;
+	}
+	return false;
+}
+    
+    /*Accesses one of four (signed) bytes given an index.
+  This can be used to avoid small lookup tables.*/
+#define OC_BYTE_TABLE32(_a,_b,_c,_d,_i) \
+  ((signed char) \
+   (((_a)&0xFF|((_b)&0xFF)<<8|((_c)&0xFF)<<16|((_d)&0xFF)<<24)>>(_i)*8))
+/*Accesses one of eight (unsigned) nibbles given an index.
+  This can be used to avoid small lookup tables.*/
+#define OC_UNIBBLE_TABLE32(_a,_b,_c,_d,_e,_f,_g,_h,_i) \
+  ((((_a)&0xF|((_b)&0xF)<<4|((_c)&0xF)<<8|((_d)&0xF)<<12| \
+   ((_e)&0xF)<<16|((_f)&0xF)<<20|((_g)&0xF)<<24|((_h)&0xF)<<28)>>(_i)*4)&0xF)
+    
+    static const static_bookblock _resbook_8s_0={
+  {
+    {0},
+    {0,0,&_8c0_s_p1_0},
+    {0},
+    {0,0,&_8c0_s_p3_0},
+    {0,0,&_8c0_s_p4_0},
+    {0,0,&_8c0_s_p5_0},
+    {0,0,&_8c0_s_p6_0},
+    {&_8c0_s_p7_0,&_8c0_s_p7_1},
+    {&_8c0_s_p8_0,&_8c0_s_p8_1},
+    {&_8c0_s_p9_0,&_8c0_s_p9_1,&_8c0_s_p9_2}
+   }
+};
+static const static_bookblock _resbook_8s_1={
+  {
+    {0},
+    {0,0,&_8c1_s_p1_0},
+    {0},
+    {0,0,&_8c1_s_p3_0},
+    {0,0,&_8c1_s_p4_0},
+    {0,0,&_8c1_s_p5_0},
+    {0,0,&_8c1_s_p6_0},
+    {&_8c1_s_p7_0,&_8c1_s_p7_1},
+    {&_8c1_s_p8_0,&_8c1_s_p8_1},
+    {&_8c1_s_p9_0,&_8c1_s_p9_1,&_8c1_s_p9_2}
+   }
+};
+    
+    // Constants:
+//  for (int i = 1; i< 32; ++i)
+//    printf('static const int cospi_%d_64 = %.0f;\n', i,
+//           round(16384 * cos(i*M_PI/64)));
+// Note: sin(k*Pi/64) = cos((32-k)*Pi/64)
+static const tran_high_t cospi_1_64  = 16364;
+static const tran_high_t cospi_2_64  = 16305;
+static const tran_high_t cospi_3_64  = 16207;
+static const tran_high_t cospi_4_64  = 16069;
+static const tran_high_t cospi_5_64  = 15893;
+static const tran_high_t cospi_6_64  = 15679;
+static const tran_high_t cospi_7_64  = 15426;
+static const tran_high_t cospi_8_64  = 15137;
+static const tran_high_t cospi_9_64  = 14811;
+static const tran_high_t cospi_10_64 = 14449;
+static const tran_high_t cospi_11_64 = 14053;
+static const tran_high_t cospi_12_64 = 13623;
+static const tran_high_t cospi_13_64 = 13160;
+static const tran_high_t cospi_14_64 = 12665;
+static const tran_high_t cospi_15_64 = 12140;
+static const tran_high_t cospi_16_64 = 11585;
+static const tran_high_t cospi_17_64 = 11003;
+static const tran_high_t cospi_18_64 = 10394;
+static const tran_high_t cospi_19_64 = 9760;
+static const tran_high_t cospi_20_64 = 9102;
+static const tran_high_t cospi_21_64 = 8423;
+static const tran_high_t cospi_22_64 = 7723;
+static const tran_high_t cospi_23_64 = 7005;
+static const tran_high_t cospi_24_64 = 6270;
+static const tran_high_t cospi_25_64 = 5520;
+static const tran_high_t cospi_26_64 = 4756;
+static const tran_high_t cospi_27_64 = 3981;
+static const tran_high_t cospi_28_64 = 3196;
+static const tran_high_t cospi_29_64 = 2404;
+static const tran_high_t cospi_30_64 = 1606;
+static const tran_high_t cospi_31_64 = 804;
+    
+                std::wstring GetNumberGroupingSeparatorStr() const
+            {
+                std::wstring result;
+                result.push_back(m_numberGroupSeparator);
+                return result;
+            }
+    
+    
+    {    float posX = 0;
+    auto lastChild = Children->GetAt(Children->Size - 1);
+    float lastChildWidth = 0;
+    if (Children->Size > 2 && ShouldPrioritizeLastItem())
+    {
+        lastChildWidth = lastChild->DesiredSize.Width;
+    }
+    for (auto item : Children)
+    {
+        auto widthAvailable = finalSize.Width - posX;
+        if (item != lastChild)
+        {
+            widthAvailable -= lastChildWidth;
+        }
+        float itemWidth = item->DesiredSize.Width;
+        if (widthAvailable > 0 && itemWidth <= widthAvailable)
+        {
+            // stack the items horizontally (left to right)
+            item->Arrange(Rect(posX, 0, itemWidth, finalSize.Height));
+            AutomationProperties::SetAccessibilityView(item, ::AccessibilityView::Content);
+            posX += item->RenderSize.Width;
+        }
+        else
+        {
+            // Not display the item
+            item->Arrange(Rect(0, 0, 0, 0));
+            AutomationProperties::SetAccessibilityView(item, ::AccessibilityView::Raw);
+        }
+    }
+    return finalSize;
+}
+
+    
+    //-----------------------------------------------------------------------------
+//
+// List of useful constants for evaluation, note this list needs to be
+// initialized.
+//
+//-----------------------------------------------------------------------------
+    
+    namespace CalculatorUnitTests
+{
+    static UCM::Unit UNIT1 = { 1, L'UNIT1', L'U1', true, false, false };
+    static UCM::Unit UNIT2 = { 2, L'UNIT2', L'U2', false, true, false };
+    static UCM::Unit UNIT3 = { 3, L'UNIT3', L'U3', false, false, false };
+    static UCM::Unit UNIT4 = { 4, L'UNIT4', L'U4', true, false, false };
+    static UCM::Unit UNIT5 = { 5, L'UNIT5', L'U5', false, false, false };
+    static UCM::Unit UNIT6 = { 6, L'UNIT6', L'U6', false, true, false };
+    static UCM::Unit UNIT7 = { 7, L'UNIT7', L'U7', false, true, false };
+    static UCM::Unit UNIT8 = { 8, L'UNIT8', L'U8', false, false, false };
+    static UCM::Unit UNIT9 = { 9, L'UNIT9', L'U9', true, false, false };
+    static UCM::Unit UNITWHIMSY = { 10, L'Whimsy', L'UW', true, false, true };
+    }
+    
+    void CHistoryCollector::RemoveLastOpndFromHistory()
+{
+    TruncateEquationSzFromIch(m_lastOpStartIndex);
+    SetExpressionDisplay();
+    m_lastOpStartIndex = -1;
+    // This will not restore the m_lastBinOpStartIndex, as it isn't possible to remove that also later
+}
+    
+        class CalculatorHistory : public IHistoryDisplay
+    {
+    public:
+        CalculatorHistory(const size_t maxSize);
+        unsigned int AddToHistory(
+            _In_ std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> const& spTokens,
+            _In_ std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> const& spCommands,
+            std::wstring_view result);
+        std::vector<std::shared_ptr<HISTORYITEM>> const& GetHistory();
+        std::shared_ptr<HISTORYITEM> const& GetHistoryItem(unsigned int uIdx);
+        void ClearHistory();
+        unsigned int AddItem(_In_ std::shared_ptr<HISTORYITEM> const& spHistoryItem);
+        bool RemoveItem(unsigned int uIdx);
+        size_t MaxHistorySize() const
+        {
+            return m_maxHistorySize;
+        }
+        ~CalculatorHistory(void);
+    }
+    
+    namespace CalculatorApp
+{
+    namespace Common
+    {
+        ref class AlwaysSelectedCollectionView sealed : public Windows::UI::Xaml::DependencyObject, public Windows::UI::Xaml::Data::ICollectionView
+        {
+            internal : AlwaysSelectedCollectionView(Windows::UI::Xaml::Interop::IBindableVector ^ source)
+                : m_currentPosition(-1)
+            {
+                m_source = source;
+    }
+    }
+    }
+    }
+    
+    
+    {
+    {protected:
+    coroutine_context_t ctx_;
+    coroutine_context_t swap_ctx_;
+    coroutine_func_t fn_;
+    char* stack_;
+    uint32_t stack_size_;
+#ifdef SW_CONTEXT_PROTECT_STACK_PAGE
+    uint32_t protect_page_;
+#endif
+#ifdef USE_VALGRIND
+    uint32_t valgrind_stack_id;
+#endif
+    void *private_data_;
+    bool end_;
+};
+//namespace end
+}
+
+    
+    
+    
+    TEST(coroutine_base, yield_resume)
+{
+    long _cid;
+    long cid = Coroutine::create([](void *arg)
+    {
+        long cid = Coroutine::get_current_cid();
+        Coroutine *co = Coroutine::get_by_cid(cid);
+        co->yield();
+        *(long *) arg = Coroutine::get_current_cid();
+    }, &_cid);
+    }
+    
+    
+    {    swHeap_free(pq);
+}
+
+    
+        proc = (proc_co_t*) emalloc(sizeof(proc_co_t));
+    proc->wstatus = nullptr;
+    proc->command = command;
+    proc->pipes = (zend_resource **) emalloc(sizeof(zend_resource *) * ndesc);
+    proc->npipes = ndesc;
+    proc->child = child;
+    proc->env = env;
+    proc->running = true;
