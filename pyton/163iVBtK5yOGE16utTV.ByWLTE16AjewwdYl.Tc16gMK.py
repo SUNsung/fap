917dd80cf2ca2a7582d694eefa186bece2ab652c
SUNsung <weatherius@gmail.com>
2019-09-17 23:32:09 +0300

    Rewrite [163iVBtK5yOGE16utTV.ByWLTE16AjewwdYl.Tc16gMK.py] File in: 2019-09-17 23:32:08

--- pyton/163iVBtK5yOGE16utTV.ByWLTE16AjewwdYl.Tc16gMK.py
+++ pyton/163iVBtK5yOGE16utTV.ByWLTE16AjewwdYl.Tc16gMK.py
@@ -1,162 +1,208 @@
 
         
-        import numpy as np
-import tensorflow as tf
-    
-        This generator is more flexible than the multiple_grid_anchor_generator
-    and multiscale_grid_anchor_generator, and can generate any of the anchors
-    that they can generate, plus additional anchor configurations. In
-    particular, it allows the explicit specification of scale and aspect ratios
-    at each layer without making any assumptions between the relationship
-    between scales and aspect ratios between layers.
-    
-    Inputs:
-'normalized_input_image_tensor': a float32 tensor of shape
-[1, height, width, 3] containing the normalized input image. Note that the
-height and width must be compatible with the height and width configured in
-the fixed_shape_image resizer options in the pipeline config proto.
-    
-      if eval_config.use_moving_averages:
-    variable_averages = tf.train.ExponentialMovingAverage(0.0)
-    variables_to_restore = variable_averages.variables_to_restore()
-  saver = tf.train.Saver(variables_to_restore)
-    
-      def _match(self, similarity_matrix, valid_rows):
-    '''Bipartite matches a collection rows and columns. A greedy bi-partite.
-    
-      for _, groundtruth in enumerate(all_annotations.groupby('ImageID')):
-    image_id, image_groundtruth = groundtruth
-    groundtruth_dictionary = utils.build_groundtruth_vrd_dictionary(
-        image_groundtruth, class_label_map, relationship_label_map)
-    
-    
-# TODO(rathodv): Merge the implementation with ConvolutionalBoxPredictor above
-# since they are very similar.
-class WeightSharedConvolutionalBoxPredictor(box_predictor.BoxPredictor):
-  '''Convolutional Box Predictor with weight sharing.
-    
-        num_masks = 1 if self._masks_are_class_agnostic else self._num_classes
-    self._mask_predictor_layers.append(
-        tf.keras.layers.Conv2D(
-            num_masks,
-            [3, 3],
-            padding='SAME',
-            name='MaskPredictor_last_conv2d',
-            **self._conv_hyperparams.params(use_bias=True)))
-    
-            return {
-            'id': video_id,
-            'title': title,
-            'description': video.get('long_description') or video.get(
-                'short_description'),
-            'duration': float_or_none(video.get('duration'), scale=1000),
-            'formats': formats,
-            'subtitles': subtitles,
-        }
-    
-                for video in videos:
-                video_id = video.get('videoSeq')
-                if not video_id:
-                    continue
-                video_id = compat_str(video_id)
-                entries.append(
-                    self.url_result(
-                        'http://www.vlive.tv/video/%s' % video_id,
-                        ie=VLiveIE.ie_key(), video_id=video_id))
-    
-    
-class SouthParkDkIE(SouthParkIE):
-    IE_NAME = 'southparkstudios.dk'
-    _VALID_URL = r'https?://(?:www\.)?(?P<url>southparkstudios\.(?:dk|nu)/(?:clips|full-episodes|collections)/(?P<id>.+?)(\?|#|$))'
-    _FEED_URL = 'http://www.southparkstudios.dk/feeds/video-player/mrss/'
-    
-        _TESTS = [{
-        'url': 'http://www.cbs.com/shows/garth-brooks/video/_u7W953k6la293J7EPTd9oHkSPs6Xn6_/connect-chat-feat-garth-brooks/',
+        
+class BitChuteIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?bitchute\.com/(?:video|embed|torrent/[^/]+)/(?P<id>[^/?#&]+)'
+    _TESTS = [{
+        'url': 'https://www.bitchute.com/video/szoMrox2JEI/',
+        'md5': '66c4a70e6bfc40dcb6be3eb1d74939eb',
         'info_dict': {
-            'id': '_u7W953k6la293J7EPTd9oHkSPs6Xn6_',
+            'id': 'szoMrox2JEI',
             'ext': 'mp4',
-            'title': 'Connect Chat feat. Garth Brooks',
-            'description': 'Connect with country music singer Garth Brooks, as he chats with fans on Wednesday November 27, 2013. Be sure to tune in to Garth Brooks: Live from Las Vegas, Friday November 29, at 9/8c on CBS!',
-            'duration': 1495,
-            'timestamp': 1385585425,
-            'upload_date': '20131127',
-            'uploader': 'CBSI-NEW',
+            'title': 'Fuck bitches get money',
+            'description': 'md5:3f21f6fb5b1d17c3dee9cf6b5fe60b3a',
+            'thumbnail': r're:^https?://.*\.jpg$',
+            'uploader': 'Victoria X Rave',
         },
-        'params': {
-            # m3u8 download
-            'skip_download': True,
-        },
-        '_skip': 'Blocked outside the US',
     }, {
-        'url': 'http://colbertlateshow.com/video/8GmB0oY0McANFvp2aEffk9jZZZ2YyXxy/the-colbeard/',
+        'url': 'https://www.bitchute.com/embed/lbb5G1hjPhw/',
         'only_matching': True,
     }, {
-        'url': 'http://www.colbertlateshow.com/podcasts/dYSwjqPs_X1tvbV_P2FcPWRa_qT6akTC/in-the-bad-room-with-stephen/',
+        'url': 'https://www.bitchute.com/torrent/Zee5BE49045h/szoMrox2JEI.webtorrent',
         'only_matching': True,
     }]
     
-    import json
-import sys
-import hashlib
-import os.path
-    
-    import io
-import optparse
-    
-    with io.open(README_FILE, encoding='utf-8') as f:
-    oldreadme = f.read()
-    
-        diropts = []
-    for opt in opts_dir:
-        if opt._short_opts:
-            diropts.extend(opt._short_opts)
-        if opt._long_opts:
-            diropts.extend(opt._long_opts)
-    
-        params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
-    }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
-    
-        def reset(self):
-        ''' Reset all config options to default '''
-        logger.debug('Resetting config')
-        for section, items in self.config.defaults.items():
-            logger.debug('Resetting section: '%s'', section)
-            lookup = [section.split('.')[0], section] if '.' in section else [section, section]
-            for item, def_opt in items.items():
-                if item == 'helptext':
-                    continue
-                default = def_opt['default']
-                tk_var = self.config_dict_gui[lookup[0]][lookup[1]][item]['selected']
-                logger.debug('Resetting: '%s' to '%s'', item, default)
-                tk_var.set(default)
-    
-        Defaults files should be named <plugin_name>_defaults.py
-    Any items placed into this file will automatically get added to the relevant config .ini files
-    within the faceswap/config folder.
+            description = source.get('description')
+        thumbnail = url_or_none(source.get('poster'))
+        timestamp = unified_timestamp(source.get('previewStart'))
+        duration = parse_duration(source.get('length'))
     
     
-_HELPTEXT = 'Options for outputting converted frames to an animated gif.'
+class SouthParkIE(MTVServicesInfoExtractor):
+    IE_NAME = 'southpark.cc.com'
+    _VALID_URL = r'https?://(?:www\.)?(?P<url>southpark\.cc\.com/(?:clips|(?:full-)?episodes|collections)/(?P<id>.+?)(\?|#|$))'
     
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+        def _real_extract(self, url):
+        content_id = self._match_id(url)
+        return self._extract_video_info(content_id)
+
+    
+        with open('supportedsites.html', 'w', encoding='utf-8') as sitesf:
+        sitesf.write(template)
+    
+        with io.open(outfile, 'w', encoding='utf-8') as outf:
+        outf.write(out)
+    
+    
+def build_completion(opt_parser):
+    opts = [opt for group in opt_parser.option_groups
+            for opt in group.option_list]
+    opts_file = [opt for opt in opts if opt.metavar == 'FILE']
+    opts_dir = [opt for opt in opts if opt.metavar == 'DIR']
+    
+    
+def get_new_command(command):
+    pgr = command.script_parts[-1]
+    
+    
+def archlinux_env():
+    if utils.which('yay'):
+        pacman = 'yay'
+    elif utils.which('yaourt'):
+        pacman = 'yaourt'
+    elif utils.which('pacman'):
+        pacman = 'sudo pacman'
+    else:
+        return False, None
+    
+        def test_get_version_error(self, shell, Popen):
+        Popen.return_value.stdout.read.side_effect = RuntimeError
+        with pytest.raises(RuntimeError):
+            shell._get_version()
+        assert Popen.call_args[0][0] == ['powershell.exe', '$PSVersionTable.PSVersion']
+
+    
+        def test_get_version_error(self, shell, Popen):
+        Popen.return_value.stdout.read.side_effect = OSError
+        with pytest.raises(OSError):
+            shell._get_version()
+        assert Popen.call_args[0][0] == ['zsh', '-c', 'echo $ZSH_VERSION']
+
+    
+    
+no_suggestions = '''\
+az provider: error: the following arguments are required: _subcommand
+usage: az provider [-h] {list,show,register,unregister,operation} ...
 '''
+    
+        return app
+
+    
+        def check(self, value):
+        return (
+            isinstance(value, dict)
+            and len(value) == 1
+            and next(iter(value)) in self.serializer.tags
+        )
+    
+        class _FakeSignal(object):
+        '''If blinker is unavailable, create a fake class with the same
+        interface that allows sending of signals but will fail with an
+        error on anything else.  Instead of doing anything on send, it
+        will just ignore the arguments and do nothing instead.
+        '''
+    
+                # Since we have to open a new request context for the session
+            # handling we want to make sure that we hide out own context
+            # from the caller.  By pushing the original request context
+            # (or None) on top of this and popping it we get exactly that
+            # behavior.  It's important to not use the push and pop
+            # methods of the actual request context object since that would
+            # mean that cleanup handlers are called
+            _request_ctx_stack.push(outer_reqctx)
+            try:
+                yield sess
+            finally:
+                _request_ctx_stack.pop()
+    
+            class CounterAPI(MethodView):
+            def get(self):
+                return session.get('counter', 0)
+    
+        @property
+    def blueprint(self):
+        '''The name of the current blueprint'''
+        if self.url_rule and '.' in self.url_rule.endpoint:
+            return self.url_rule.endpoint.rsplit('.', 1)[0]
+    
+        @blue.route('/function')
+    def blue_func_test():
+        raise MyFunctionException()
+    
+        # SSLContext
+    if sys.version_info < (2, 7, 9):
+        ssl_context = object()
+    else:
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    
+        requires_project = False
+    default_settings = {'LOG_ENABLED': False,
+                        'SPIDER_LOADER_WARN_ONLY': True}
+    
+    
+# XXX: move it to w3lib?
+_ajax_crawlable_re = re.compile(six.u(r'<meta\s+name=['\']fragment['\']\s+content=['\']!['\']/?>'))
+def _has_ajaxcrawlable_meta(text):
+    '''
+    >>> _has_ajaxcrawlable_meta('<html><head><meta name='fragment'  content='!'/></head><body></body></html>')
+    True
+    >>> _has_ajaxcrawlable_meta('<html><head><meta name='fragment' content='!'></head></html>')
+    True
+    >>> _has_ajaxcrawlable_meta('<html><head><!--<meta name='fragment'  content='!'/>--></head><body></body></html>')
+    False
+    >>> _has_ajaxcrawlable_meta('<html></html>')
+    False
+    '''
+    
+        def __init__(self):
+        self._formats = {
+            'tar': self._is_tar,
+            'zip': self._is_zip,
+            'gz': self._is_gzip,
+            'bz2': self._is_bzip2
+        }
+    
+        @classmethod
+    def from_crawler(cls, crawler):
+        o = cls(crawler.settings.getfloat('DOWNLOAD_TIMEOUT'))
+        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
+        return o
+    
+            return response
+    
+        Args:
+        x(tf.Tensor):
+        n_unit_ls(list of int):
+        act_fn:
+        name(str):
+    '''
+    # n_layer = len(n_unit_list)
+    name = name or 'dense'
+    for i, n_unit in enumerate(n_unit_ls):
+        x = dense(x, n_unit, act_fn=act_fn, name='{}-{}'.format(name, i))
+    
+        def __call__(self, x):
+        '''
+        Args:
+            x: 注意 x.dtype == float32
+        '''
+        # x = tf.cast(x, dtype=tf.float32)  # 交给外部处理
+        loss_regularization = 0.
+        if self.l1:
+            loss_regularization += tf.reduce_sum(self.l1 * tf.abs(x))
+        if self.l2:
+            loss_regularization += tf.reduce_sum(self.l2 * tf.square(x))
+        return loss_regularization
+    
+    This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
+    
+        # check that we have some nodes to checkpoint
+    if not checkpoints:
+        raise Exception('no checkpoints nodes found or given as input! ')
     
                     var_x = K.reshape(inputs, (batch_size,
                                            height,
@@ -166,3 +212,32 @@ THE SOFTWARE.
                 mean = K.mean(var_x, axis=[1, 2, 4], keepdims=True)
                 std = K.sqrt(K.var(var_x, axis=[1, 2, 4], keepdims=True) + self.epsilon)
                 var_x = (var_x - mean) / std
+    
+            the output mask will be <mask_type>.mask
+        channels: 1, 3 or 4:
+                    1 - Returns a single channel mask
+                    3 - Returns a 3 channel mask
+                    4 - Returns the original image with the mask in the alpha channel '''
+    
+        def set_defaults(self):
+        ''' Set the default values for config '''
+        logger.debug('Setting defaults')
+        current_dir = os.path.dirname(__file__)
+        for dirpath, _, filenames in os.walk(current_dir):
+            default_files = [fname for fname in filenames if fname.endswith('_defaults.py')]
+            if not default_files:
+                continue
+            base_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+            import_path = '.'.join(full_path_split(dirpath.replace(base_path, ''))[1:])
+            plugin_type = import_path.split('.')[-1]
+            for filename in default_files:
+                self.load_module(filename, import_path, plugin_type)
+    
+    logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+    
+            insertion = np.rint(new_face[y_crop, x_crop] * 255.0).astype('uint8')
+        insertion_mask = np.rint(raw_mask[y_crop, x_crop] * 255.0).astype('uint8')
+        insertion_mask[insertion_mask != 0] = 255
+        prior = np.rint(np.pad(old_face * 255.0,
+                               ((height, height), (width, width), (0, 0)),
+                               'constant')).astype('uint8')
