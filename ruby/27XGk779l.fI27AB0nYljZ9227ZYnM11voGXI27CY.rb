385d2ddca73a05a39ac2240637157d2452e578ea
SUNsung <weatherius@gmail.com>
2019-09-18 02:29:23 +0300

    Rewrite [27XGk779l.fI27AB0nYljZ9227ZYnM11voGXI27CY.rb] File in: 2019-09-18 02:29:22

--- ruby/27XGk779l.fI27AB0nYljZ9227ZYnM11voGXI27CY.rb
+++ ruby/27XGk779l.fI27AB0nYljZ9227ZYnM11voGXI27CY.rb
@@ -1,150 +1,223 @@
 
         
-              [poll type=regular]
-      * Hello
-      * World
-      [/poll]
-    MD
+              @site.exclude = excludes + ['exclude*']
+      assert_equal files, @site.reader.filter_entries(excludes + files + ['excludeA'])
+    end
     
-        it 'should import branch from ssh url' do
-      Discourse::Utils.expects(:execute_command).with({
-        'GIT_SSH_COMMAND' => 'ssh -i #{@ssh_folder}/id_rsa -o StrictHostKeyChecking=no'
-      }, 'git', 'clone', '--single-branch', '-b', branch, ssh_url, @temp_folder)
+    Mercenary.program(:jekyll) do |p|
+  p.version Jekyll::VERSION
+  p.description 'Jekyll is a blog-aware, static site generator in Ruby'
+  p.syntax 'jekyll <subcommand> [options]'
     
-        def fetch_group(group_name)
-      group_name = group_name.downcase
-      group = @groups[group_name]
+          def before_feature(_feature)
+        @exceptions = []
+        @indent = 0
+      end
     
-        post_action_types = PostActionType.where(id: PostActionType.flag_types.values).order('id')
+    module Jekyll
+  module Commands
+    class NewTheme < Jekyll::Command
+      class << self
+        def init_with_program(prog)
+          prog.command(:'new-theme') do |c|
+            c.syntax 'new-theme NAME'
+            c.description 'Creates a new Jekyll theme scaffold'
+            c.option 'code_of_conduct', \
+                     '-c', '--code-of-conduct', \
+                     'Include a Code of Conduct. (defaults to false)'
     
-    class Admin::GroupsController < Admin::AdminController
-  def bulk
+          it 'requires the passwords to match when changing them' do
+        visit edit_admin_user_path(users(:bob))
+        fill_in 'Password', with: '12345678'
+        fill_in 'Password confirmation', with: 'no_match'
+        click_on 'Update User'
+        expect(page).to have_text('Password confirmation doesn't match')
+      end
+    end
+    
+        it 'returns a correct icon tag for GitHub' do
+      icon = omniauth_provider_icon(:github)
+      expect(icon).to be_html_safe
+      elem = Nokogiri(icon).at('i.fa.fa-github')
+      expect(elem).to be_a Nokogiri::XML::Element
+    end
+    
+        it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
+    end
   end
     
-            /^#{path}[^\/]*\.t?gz$/i
+          expect(exporter.as_json[:control_links]).to eq([{ :controller => guid_order(agent_list, :jane_rain_notifier_agent), :control_target => guid_order(agent_list, :jane_weather_agent) }])
+    end
+  end
+    
+        it 'accepts objects as well as strings' do
+      log = AgentLog.log_for_agent(agents(:jane_website_agent), events(:bob_website_agent_event).payload)
+      expect(log.message).to include(''title'=>'foo'')
+    end
+  end
+    
+        it 'makes followers unfollow old account' do
+      expect(follower.following?(old_account)).to be false
+    end
+    
+          it 'not calls errors.add' do
+        expect(errors).not_to have_received(:add).with(attribute, any_args)
+      end
+    end
+  end
+end
+
+    
+      sidekiq_options unique: :until_executed, retry: 0
+    
+    namespace :bower do
+    
+        def log_http_get_file(url, cached = false)
+      s = '  #{'CACHED ' if cached}GET #{url}...'
+      if cached
+        puts dark green s
+      else
+        puts dark cyan s
       end
     end
     
-              s3_helper.expects(:s3_bucket).returns(s3_bucket).at_least_once
-          s3_bucket.expects(:object).with('uploads/second/original/1X/#{upload.sha1}.png').returns(s3_object)
-          s3_object.expects(:acl).returns(s3_object)
-          s3_object.expects(:put).with(acl: 'private').returns(s3_object)
-    
-        desc 'Get the list of the available license template' do
-      detail 'This feature was introduced in GitLab 8.7.'
-      success ::API::Entities::License
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
+      end
     end
-    params do
-      optional :popular, type: Boolean, desc: 'If passed, returns only popular licenses'
-      use :pagination
-    end
-    get 'templates/licenses' do
-      popular = declared(params)[:popular]
-      popular = to_boolean(popular) if popular.present?
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
+  end
+end
+
     
-          encrypted_token = settings.first.runners_registration_token_encrypted
-      decrypted_token = ::Gitlab::CryptoHelper.aes256_gcm_decrypt(encrypted_token)
+          def response_type_as_space_seperated_values
+        [*session[:response_type]].join(' ')
+      end
     
-          context 'and user cannot admin_group_member' do
-        before do
-          allow(presenter).to receive(:can?).with(user, :admin_group_member, group).and_return(false)
+      def contacts_by_aspect(aspect_id)
+    contacts = current_user.contacts.arel_table
+    aspect_memberships = AspectMembership.arel_table
+    current_user.contacts.joins(
+      contacts.outer_join(aspect_memberships).on(
+        aspect_memberships[:aspect_id].eq(aspect_id).and(
+          aspect_memberships[:contact_id].eq(contacts[:id])
+        )
+      ).join_sources
+    )
+  end
+    
+            # Initialize a new instance
+        #
+        # @param  [Hash{Symbol=>String}] pods_by_state
+        #         The name of the pods grouped by their state
+        #         (`:added`, `:removed`, `:changed` or `:unchanged`).
+        #
+        def initialize(pods_by_state = nil)
+          @added     = Set.new
+          @deleted   = Set.new
+          @changed   = Set.new
+          @unchanged = Set.new
+    
+      # Loaded immediately after dependencies to ensure proper override of their
+  # UI methods.
+  #
+  require 'cocoapods/user_interface'
+    
+            def create_project(path, object_version, pod_target_subproject)
+          object_version ||= Xcodeproj::Constants::DEFAULT_OBJECT_VERSION
+          Pod::Project.new(path, false, object_version, :pod_target_subproject => pod_target_subproject)
         end
     
-            it { expect(presenter.can_resend_invite?).to eq(true) }
-      end
-    
-            new(sandbox, sandbox.root.to_s, pods_project, umbrella_targets_descriptions)
-      end
-    
-          # @return [Boolean] whether the target is built as a framework
+          # @return [String] Wraps a string taking into account the width of the
+      # terminal and an option indent. Adapted from
+      # https://macromates.com/blog/2006/wrapping-text-with-regular-expressions/
       #
-      def framework?
-        packaging == :framework
-      end
-    
-    ENV['SKIP_SETUP'] = 'true'
-if ENV['SKIP_XCODEBUILD'].nil? && Pod::Executable.which('xcodebuild').nil?
-  ENV['SKIP_XCODEBUILD'] = 'true'
-end
-    
-            it 'can be initialized with specs, platform and whether it requires frameworks' do
-          variant = PodVariant.new(@specs, [], [], @platform, @type)
-          variant.specs.should == @specs
-          variant.platform.should == @platform
-          variant.build_type.should == @type
-        end
-    
-              it 'does not require a host target, if there is no user project (manual integration)' do
-            @target.stubs(:user_project).returns(nil)
-            @target.stubs(:user_target_uuids).returns([])
-            @target.requires_host_target?.should == false
-          end
-    
-              it 'saves the xcconfig' do
-            path = temporary_directory + 'sample.xcconfig'
-            @generator.save_as(path)
-            generated = Xcodeproj::Config.new(path)
-            generated.class.should == Xcodeproj::Config
-          end
-    
-          def updates
-        @updates ||= begin
-          ensure_external_podspecs_present!
-          spec_sets.map do |set|
-            spec = set.specification
-            source_version = set.versions.first
-            pod_name = spec.root.name
-            lockfile_version = lockfile.version(pod_name)
-            if source_version > lockfile_version
-              matching_spec = unlocked_pods.find { |s| s.name == pod_name }
-              matching_version =
-                matching_spec ? matching_spec.version : '(unused)'
-              [pod_name, lockfile_version, matching_version, source_version]
-            end
-          end.compact.uniq
-        end
-      end
-    
-    Gem::Specification.new do |gem|
-  gem.name          = 'capistrano'
-  gem.version       = Capistrano::VERSION
-  gem.authors       = ['Tom Clements', 'Lee Hambley']
-  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
-  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
-  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
-  gem.homepage      = 'http://capistranorb.com/'
-    
-    Given(/^a custom task to generate a file$/) do
-  TestApp.copy_task_to_test_app('spec/support/tasks/database.rake')
-end
-    
-        (stdout + stderr).each_line { |line| puts '[vagrant] #{line}' }
-    
-        private
-    
-        def configure_backend
-      backend.configure do |sshkit|
-        configure_sshkit_output(sshkit)
-        sshkit.output_verbosity = fetch(:log_level)
-        sshkit.default_env      = fetch(:default_env)
-        sshkit.backend          = fetch(:sshkit_backend, SSHKit::Backend::Netssh)
-        sshkit.backend.configure do |backend|
-          backend.pty                = fetch(:pty)
-          backend.connection_timeout = fetch(:connection_timeout)
-          backend.ssh_options        = (backend.ssh_options || {}).merge(fetch(:ssh_options, {}))
+      # @param [String] string  The string to wrap
+      #
+      # @param [String] indent  The string to use to indent the result.
+      #
+      # @return [String]        The formatted string.
+      #
+      # @note If CocoaPods is not being run in a terminal or the width of the
+      # terminal is too small a width of 80 is assumed.
+      #
+      def wrap_string(string, indent = 0)
+        if disable_wrap
+          (' ' * indent) + string
+        else
+          first_space = ' ' * indent
+          indented = CLAide::Command::Banner::TextWrapper.wrap_with_indent(string, indent, 9999)
+          first_space + indented
         end
       end
     end
+  end
+  UI = UserInterface
     
-          ServerKey = Struct.new(:hostname, :port)
+          it 'shows the git URL (when an upstream is not configured)' do
+        output = run_command('repo', 'list')
+        output.should.include? '- URL:  https://github.com/apiaryio/Specs'
+      end
+    end
+  end
+end
+
     
-          def keys
-        values.keys
+              file = AppTargetHelper.create_app_host_main_file(project, :osx)
+          file.basename.to_s.should == 'main.m'
+          file.read.should == AppTargetHelper::MACOS_APP_HOST_MAIN_CONTENTS
+        end
       end
     
-    # Exit cleanly from an early interrupt
-Signal.trap('INT') { exit 1 }
-    
-      it 'records when the config was read' do
-    expect(subject.read_at).to be <= Time.now
+      def symlinked?(symlink_path, target_path)
+    '[ #{symlink_path} -ef #{target_path} ]'
   end
+    
+      at_exit do
+    if ENV['KEEP_RUNNING']
+      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
+      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
+    else
+      vagrant_cli_command('destroy -f')
+    end
+  end
+    
+            def call
+          @validator.call
+        end
+      end
+    end
+  end
+end
+
+    
+          return_items.group_by(&:exchange_variant).map do |variant, variant_return_items|
+        variant_inventory_units = variant_return_items.map(&:exchange_inventory_units).flatten
+        line_item = Spree::LineItem.create!(variant: variant, quantity: variant_return_items.count, order: order)
+        variant_inventory_units.each { |i| i.update!(line_item_id: line_item.id, order_id: order.id) }
+      end
+    
+                allow(subject).to receive(:shipping_methods).and_return(shipping_methods)
+    
+            def new; end
+    
+            def destroy
+          if @property
+            authorize! :destroy, @property
+            @property.destroy
+            respond_with(@property, status: 204)
+          else
+            invalid_resource!(@property)
+          end
+        end
+    
+            private
