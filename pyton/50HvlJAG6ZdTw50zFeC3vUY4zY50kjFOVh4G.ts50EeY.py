c29e34c7089203a76d33821c9c6259a5a4652071
SUNsung <weatherius@gmail.com>
2019-09-18 02:46:07 +0300

    Rewrite [50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py] File in: 2019-09-18 02:46:06

--- pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
+++ pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
@@ -1,105 +1,41 @@
 
         
-            def _real_extract(self, url):
-        video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
-        embed_url = self._proto_relative_url(self._search_regex(
-            r'<iframe.+?src='((?:https?:)?//(?:daxab\.com|dxb\.to|[^/]+/player)/[^']+)'.*?></iframe>',
-            webpage, 'embed url'))
-        if VKIE.suitable(embed_url):
-            return self.url_result(embed_url, VKIE.ie_key(), video_id)
+        *TL;DR
+Encapsulates how a set of objects interact.
+'''
     
-        infile, outfile = args
+    *References:
+https://fkromer.github.io/python-pattern-references/design/#singleton
     
-        def save(self, fail_silently=False):
-        self['__meta__'] = {
-            'httpie': __version__
-        }
-        if self.helpurl:
-            self['__meta__']['help'] = self.helpurl
+    '''
+*What is this pattern about?
+The Adapter pattern provides a different interface for a class. We can
+think about it as a cable adapter that allows you to charge a phone
+somewhere that has outlets in a different shape. Following this idea,
+the Adapter pattern is useful to integrate classes that couldn't be
+integrated due to their incompatible interfaces.
     
-    import pytest
+        Note that for open file and socket handles it is generally
+    preferable to set the ``FD_CLOEXEC`` flag (using `fcntl` or
+    ``tornado.platform.auto.set_close_exec``) instead
+    of using a reload hook to close them.
+    '''
+    _reload_hooks.append(fn)
     
-    from fixtures import FILE_PATH, FILE_CONTENT
-from utils import http, HTTP_OK, MockEnvironment
+        .. deprecated:: 5.0
     
-    
-def main():
-    package_meta_map = {
-        package_name: get_package_meta(package_name)
-        for package_name in PACKAGES
-    }
-    httpie_meta = package_meta_map.pop('httpie')
-    print()
-    print('  url '{url}''.format(url=httpie_meta['url']))
-    print('  sha256 '{sha256}''.format(sha256=httpie_meta['sha256']))
-    print()
-    for dep_meta in package_meta_map.values():
-        print('  resource '{name}' do'.format(name=dep_meta['name']))
-        print('    url '{url}''.format(url=dep_meta['url']))
-        print('    sha256 '{sha256}''.format(sha256=dep_meta['sha256']))
-        print('  end')
-        print('')
-    
-        # noinspection PyProtectedMember
-    @property
-    def headers(self):
-        original = self._orig.raw._original_response
-    
-        try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            USERNAME,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
-
-    
-    import logging
-import numpy as np
+        def _sock_state_cb(self, fd: int, readable: bool, writable: bool) -> None:
+        state = (IOLoop.READ if readable else 0) | (IOLoop.WRITE if writable else 0)
+        if not state:
+            self.io_loop.remove_handler(fd)
+            del self.fds[fd]
+        elif fd in self.fds:
+            self.io_loop.update_handler(fd, state)
+            self.fds[fd] = state
+        else:
+            self.io_loop.add_handler(fd, self._handle_events, state)
+            self.fds[fd] = state
     
     
-_DEFAULTS = {
-    'confidence': {
-        'default': 50,
-        'info': 'The confidence level at which the detector has succesfully found a face.\n'
-                'Higher levels will be more discriminating, lower levels will have more false '
-                'positives.',
-        'datatype': int,
-        'rounding': 5,
-        'min_max': (25, 100),
-        'choices': [],
-        'gui_radio': False,
-        'fixed': True,
-    }
-}
-
-    
-        The following variables should be defined:
-        _HELPTEXT: A string describing what this plugin does
-        _DEFAULTS: A dictionary containing the options, defaults and meta information. The
-                   dictionary should be defined as:
-                       {<option_name>: {<metadata>}}
-    
-            Returns:
-        -------
-        transfer: NumPy array
-            OpenCV image (w, h, 3) NumPy array (uint8)
-        '''
-        clip = self.config.get('clip', True)
-        preserve_paper = self.config.get('preserve_paper', True)
-    
-        @staticmethod
-    def get_kernel_size(new_face, radius_percent):
-        ''' Return the kernel size and central point for the given radius
-            relative to frame width '''
-        radius = max(1, round(new_face.shape[1] * radius_percent / 100))
-        kernel_size = int((radius * 2) + 1)
-        kernel_size = (kernel_size, kernel_size)
-        logger.trace(kernel_size)
-        return kernel_size, radius
+class GeneratorClientTest(ClientTestMixin, AsyncTestCase):
+    client_class = GeneratorCapClient
