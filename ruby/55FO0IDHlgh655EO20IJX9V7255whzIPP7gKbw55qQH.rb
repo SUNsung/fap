8cfc3ed2ebc1e3f88454b2fd12ca096a50f88bcb
SUNsung <weatherius@gmail.com>
2019-09-17 09:37:27 +0300

    Rewrite [55FO0IDHlgh655EO20IJX9V7255whzIPP7gKbw55qQH.rb] File in: 2019-09-17 09:37:27

--- ruby/55FO0IDHlgh655EO20IJX9V7255whzIPP7gKbw55qQH.rb
+++ ruby/55FO0IDHlgh655EO20IJX9V7255whzIPP7gKbw55qQH.rb
@@ -1,22 +1,6 @@
 
         
-        #############################################################################
-#
-# Standard tasks
-#
-#############################################################################
-    
-    require 'benchmark/ips'
-require 'pathutil'
-    
-                  Jekyll.logger.info 'LiveReload address:',
-                                 'http://#{opts['host']}:#{opts['livereload_port']}'
-            end
-          end
-          @thread.abort_on_exception = true
-        end
-    
-      it 'imports a scenario that does not exist yet' do
+          it 'imports a scenario that does not exist yet' do
     visit new_scenario_imports_path
     attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
     click_on 'Start Import'
@@ -27,84 +11,72 @@ require 'pathutil'
     expect(page).to have_text('Import successful!')
   end
     
-      describe '#scenario_label' do
-    it 'creates a scenario label with the scenario name' do
-      expect(scenario_label(scenario)).to eq(
-        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Scene</span>'
+        it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
       )
     end
+  end
     
-        it 'requires a message' do
-      @log.message = ''
-      expect(@log).not_to be_valid
-      @log.message = nil
-      expect(@log).not_to be_valid
-      expect(@log).to have(1).error_on(:message)
+    describe ConvertWebsiteAgentTemplateForMerge do
+  let :old_extract do
+    {
+      'url' => { 'css' => '#comic img', 'value' => '@src' },
+      'title' => { 'css' => '#comic img', 'value' => '@alt' },
+      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
+    }
+  end
+    
+        it 'should generate the correct last checkpoint url' do
+      @checker.options['path'] = 'last_checkpoint/usps/9361289878905919630610'
+      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/last_checkpoint/usps/9361289878905919630610')
     end
+  end
     
-      describe '#receive' do
-    it 'sends a message' do
-      stub(HTTParty).post { {'id' => 1, 'message' => 'blah', 'title' => 'blah','source_name' => 'Custom Notification'} }
-      @checker.receive([@event])
-    end
+        @checker = Agents::BasecampAgent.new(:name => 'somename', :options => @valid_params)
+    @checker.service = services(:generic)
+    @checker.user = users(:jane)
+    @checker.save!
     
-        def base_url
-      context[:base_url]
-    end
-    
-        def as_json
-      @pages
-    end
-    
-        private
-    
-            css('.l-sub-section', '.alert', '.banner').each do |node|
-          node.name = 'blockquote'
-        end
-    
-            name.prepend '#{breadcrumbs.join('.')}#' if breadcrumbs.present? && breadcrumbs[0] != name
-        name << '()' if %w(Function Method Constructor).include?(subtitle)
-        name
+          def redirect(env)
+        request = Request.new(env)
+        warn env, 'attack prevented by #{self.class}'
+        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
       end
     
-            # Remove ng-* attributes
-        css('*').each do |node|
-          node.attributes.each_key do |attribute|
-            node.remove_attribute(attribute) if attribute.start_with? 'ng-'
-          end
-        end
+        it 'leaves normal params untouched' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']]]
+      end
+      get '/', :foo => 'bar'
+      expect(body).to eq('bar')
+    end
     
-        doc = rand_text_alpha_lower(4..8)
-    entity = rand_text_alpha_lower(4..8)
-    local_file = '/etc/passwd'
+      it 'accepts post requests with correct X-CSRF-Token header' do
+    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => token)
+    expect(last_response).to be_ok
+  end
     
+        it 'saves and restores locale' do
+      I18n.locale = 'fr'
+      msg = {}
+      mw = Sidekiq::Middleware::I18n::Client.new
+      mw.call(nil, msg, nil, nil) { }
+      assert_equal :fr, msg['locale']
     
-  def initialize(client, src_id)
-    self.client = client
-    self.scall  = src_id
-    self.dcall  = 0
-    self.iseq   = 0
-    self.oseq   = 0
-    self.state  = nil
+          q = Sidekiq::Queue.new('custom_queue')
+      qs = q.size
+      assert SomeScheduledWorker.perform_in(-300, 'mike')
+      assert_equal 3, ss.size
+      assert_equal qs+1, q.size
     
-      #
-  # Move these into an IPMI stack or mixin at some point
-  #
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
     
-              private
-    
-              # Encrypts the cipher using RC4-HMAC schema
-          #
-          # @param data [String] the data to encrypt
-          # @param key [String] the key to encrypt
-          # @param msg_type [Integer] the message type
-          # @return [String] the encrypted data
-          def encrypt_rc4_hmac(data, key, msg_type)
-            k1 = OpenSSL::HMAC.digest('MD5', key, [msg_type].pack('V'))
-    
-              # Rex::Proto::Kerberos::Model::ApReq decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'AP-REQ decoding not supported'
-          end
+      # Extracts raw content DIV from template, used for page description as {{ content }}
+  # contains complete sub-template code on main page level
+  def raw_content(input)
+    /<div class='entry-content'>(?<content>[\s\S]*?)<\/div>\s*<(footer|\/article)>/ =~ input
+    return (content.nil?) ? input : content
+  end
