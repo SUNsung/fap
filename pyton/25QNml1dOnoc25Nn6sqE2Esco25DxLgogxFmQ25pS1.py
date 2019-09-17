d3284dada92ba4a5958e2fe709639267e82c9c78
SUNsung <weatherius@gmail.com>
2019-09-17 18:01:36 +0300

    Rewrite [25QNml1dOnoc25Nn6sqE2Esco25DxLgogxFmQ25pS1.py] File in: 2019-09-17 18:01:36

--- pyton/25QNml1dOnoc25Nn6sqE2Esco25DxLgogxFmQ25pS1.py
+++ pyton/25QNml1dOnoc25Nn6sqE2Esco25DxLgogxFmQ25pS1.py
@@ -1,121 +1,91 @@
 
         
-                if triple is None:
-            detail = 'no match'
+        
+def findGroup(gitlab_instance, identifier):
+    try:
+        project = gitlab_instance.groups.get(identifier)
+    except Exception as e:
+        return None
+    
+            self.module = module
+    
+            # An issue is identified by the project_name, the issue_subject and the issue_type
+        matching_issue_list = filter(lambda x: x.subject == issue_subject and x.type == type_id, project.list_issues())
+        matching_issue_list_len = len(matching_issue_list)
+    
+    # Sends a push notification with title and body to a channel
+- pushbullet:
+    api_key: ABC123abc123ABC123abc123ABC123ab
+    channel: my-awesome-channel
+    title: ALERT! Signup service is down
+    body: Error rate on signup service is over 90% for more than 2 minutes
+'''
+    
+        username = module.params['username']
+    password = module.params['password']
+    api_key = module.params['api_key']
+    bcc = module.params['bcc']
+    cc = module.params['cc']
+    headers = module.params['headers']
+    from_name = module.params['from_name']
+    from_address = module.params['from_address']
+    to_addresses = module.params['to_addresses']
+    subject = module.params['subject']
+    body = module.params['body']
+    html_body = module.params['html_body']
+    attachments = module.params['attachments']
+    
+        # --- run command ---
+    try:
+        ipmi_cmd = command.Command(
+            bmc=name, userid=user, password=password, port=port
+        )
+        module.debug('ipmi instantiated - name: '%s'' % name)
+        current = ipmi_cmd.get_bootdev()
+        # uefimode may not supported by BMC, so use desired value as default
+        current.setdefault('uefimode', uefiboot)
+        if state == 'present' and current != dict(bootdev=bootdev, persistent=persistent, uefimode=uefiboot):
+            request = dict(bootdev=bootdev, uefiboot=uefiboot, persist=persistent)
+        elif state == 'absent' and current['bootdev'] == bootdev:
+            request = dict(bootdev='default')
         else:
-            detail = 'found (%r)' % (triple[1] or '<string>')
-            total_found += 1
-        info.append('       -> %s' % detail)
+            module.exit_json(changed=False, **current)
     
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
     
-class TagDict(JSONTag):
-    '''Tag for 1-item dicts whose only key matches a registered tag.
+            # Value was changed.
+        return True
     
-        num_train_samples = 50000
+            # Deleting model 'Nonce'
+        db.delete_table(u'social_auth_nonce')
     
-    import six
-from . import backend as K
-from .utils.generic_utils import serialize_keras_object
-from .utils.generic_utils import deserialize_keras_object
+        models = {
+        'sentry.user': {
+            'Meta': {'object_name': 'User', 'db_table': ''auth_user''},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'id': ('sentry.db.models.fields.bounded.BoundedAutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_managed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_password_expired': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_password_change': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'db_column': ''first_name'', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'})
+        },
+        'social_auth.usersocialauth': {
+            'Meta': {'unique_together': '(('provider', 'uid', 'user'),)', 'object_name': 'UserSocialAuth'},
+            'extra_data': ('social_auth.fields.JSONField', [], {'default': ''{}''}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'provider': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'uid': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': ''social_auth'', 'to': 'orm['sentry.User']'})
+        }
+    }
     
-    score_model2 = model2.evaluate(x_test,
-                               y_test,
-                               batch_size=batch_size,
-                               verbose=1)
-    
-    
-def test_invalid_convert_kernel():
-    with pytest.raises(ValueError):
-        conv_utils.convert_kernel(np.zeros((10, 20)))
-    
-    
-def plot_batch_times(all_times, n_features, all_batch_sizes, data):
-    plt.figure()
-    plot_results(all_batch_sizes, all_times['pca'], label='PCA')
-    plot_results(all_batch_sizes, all_times['ipca'], label='IncrementalPCA')
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm runtime vs. batch_size for n_components %i\n \
-                 LFW, size %i x %i' % (
-                 n_features, data.shape[0], data.shape[1]))
-    plt.xlabel('Batch size')
-    plt.ylabel('Time (seconds)')
-    
-    from sklearn.cluster import AgglomerativeClustering
-    
-        xx = range(0, n * step, step)
-    plt.figure('scikit-learn tree benchmark results')
-    plt.subplot(211)
-    plt.title('Learning with varying number of samples')
-    plt.plot(xx, scikit_classifier_results, 'g-', label='classification')
-    plt.plot(xx, scikit_regressor_results, 'r-', label='regression')
-    plt.legend(loc='upper left')
-    plt.xlabel('number of samples')
-    plt.ylabel('Time (s)')
-    
-    In both examples below, the main result is that the empirical covariance
-estimate, as a non-robust one, is highly influenced by the heterogeneous
-structure of the observations. Although the robust covariance estimate is
-able to focus on the main mode of the data distribution, it sticks to the
-assumption that the data should be Gaussian distributed, yielding some biased
-estimation of the data structure, but yet accurate to some extent.
-The One-Class SVM does not assume any parametric form of the data distribution
-and can therefore model the complex shape of the data much better.
-    
-    # One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'face_recognition',
-     u'Face Recognition Documentation',
-     [u'Adam Geitgey'], 1)
-]
-    
-        # Save the trained KNN classifier
-    if model_save_path is not None:
-        with open(model_save_path, 'wb') as f:
-            pickle.dump(knn_clf, f)
-    
-    
-def detect_faces_in_image(file_stream):
-    # Pre-calculated face encoding of Obama generated with face_recognition.face_encodings(img)
-    known_face_encoding = [-0.09634063,  0.12095481, -0.00436332, -0.07643753,  0.0080383,
-                            0.01902981, -0.07184699, -0.09383309,  0.18518871, -0.09588896,
-                            0.23951106,  0.0986533 , -0.22114635, -0.1363683 ,  0.04405268,
-                            0.11574756, -0.19899382, -0.09597053, -0.11969153, -0.12277931,
-                            0.03416885, -0.00267565,  0.09203379,  0.04713435, -0.12731361,
-                           -0.35371891, -0.0503444 , -0.17841317, -0.00310897, -0.09844551,
-                           -0.06910533, -0.00503746, -0.18466514, -0.09851682,  0.02903969,
-                           -0.02174894,  0.02261871,  0.0032102 ,  0.20312519,  0.02999607,
-                           -0.11646006,  0.09432904,  0.02774341,  0.22102901,  0.26725179,
-                            0.06896867, -0.00490024, -0.09441824,  0.11115381, -0.22592428,
-                            0.06230862,  0.16559327,  0.06232892,  0.03458837,  0.09459756,
-                           -0.18777156,  0.00654241,  0.08582542, -0.13578284,  0.0150229 ,
-                            0.00670836, -0.08195844, -0.04346499,  0.03347827,  0.20310158,
-                            0.09987706, -0.12370517, -0.06683611,  0.12704916, -0.02160804,
-                            0.00984683,  0.00766284, -0.18980607, -0.19641446, -0.22800779,
-                            0.09010898,  0.39178532,  0.18818057, -0.20875394,  0.03097027,
-                           -0.21300618,  0.02532415,  0.07938635,  0.01000703, -0.07719778,
-                           -0.12651891, -0.04318593,  0.06219772,  0.09163868,  0.05039065,
-                           -0.04922386,  0.21839413, -0.02394437,  0.06173781,  0.0292527 ,
-                            0.06160797, -0.15553983, -0.02440624, -0.17509389, -0.0630486 ,
-                            0.01428208, -0.03637431,  0.03971229,  0.13983178, -0.23006812,
-                            0.04999552,  0.0108454 , -0.03970895,  0.02501768,  0.08157793,
-                           -0.03224047, -0.04502571,  0.0556995 , -0.24374914,  0.25514284,
-                            0.24795187,  0.04060191,  0.17597422,  0.07966681,  0.01920104,
-                           -0.01194376, -0.02300822, -0.17204897, -0.0596558 ,  0.05307484,
-                            0.07417042,  0.07126575,  0.00209804]
-    
-    
-def scan_known_people(known_people_folder):
-    known_names = []
-    known_face_encodings = []
-    
-        # Print the location of each facial feature in this image
-    for facial_feature in face_landmarks.keys():
-        print('The {} in this face has the following points: {}'.format(facial_feature, face_landmarks[facial_feature]))
-    
-    
-# Many subprocess use to process frames.
-def process(worker_id, read_frame_list, write_frame_list):
-    known_face_encodings = Global.known_face_encodings
-    known_face_names = Global.known_face_names
-    while not Global.is_exit:
+        author='LittleCoder',
+    author_email='i7meavnktqegm1b@qq.com',
