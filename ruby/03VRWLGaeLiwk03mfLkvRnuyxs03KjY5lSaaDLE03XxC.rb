d49a7e423bcc083e87d29e7eaf5c630eacd03c0c
SUNsung <weatherius@gmail.com>
2019-09-17 04:17:15 +0300

    Rewrite [03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb] File in: 2019-09-17 04:17:14

--- ruby/03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb
+++ ruby/03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb
@@ -1,42 +1,145 @@
 
         
-          describe 'GET blocks index' do
-    it 'renders proper blocks index' do
-      create(:block, user_id: user.id, input_css: '.blue { color: blue;}')
-      get '/blocks'
-      expect(response.body).to include('color: blue')
+            def unlock_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :unlock_instructions, opts)
+    end
+    
+        # Receives an object and find a scope for it. If a scope cannot be found,
+    # raises an error. If a symbol is given, it's considered to be the scope.
+    def self.find_scope!(obj)
+      obj = obj.devise_scope if obj.respond_to?(:devise_scope)
+      case obj
+      when String, Symbol
+        return obj.to_sym
+      when Class
+        Devise.mappings.each_value { |m| return m.name if obj <= m.to }
+      else
+        Devise.mappings.each_value { |m| return m.name if obj.is_a?(m.to) }
+      end
+    
+          it 'does not create a block from sender to recipient' do
+        expect(sender.blocking?(recipient)).to be false
+      end
+    
+        it 'renders the headers' do
+      expect(mail.subject).to eq('You were mentioned by bob')
+      expect(mail.to).to eq([receiver.email])
+    end
+    
+        context 'when parent status is public' do
+      let(:visibility) { 'public' }
+    
+            it 'does not create anything' do
+          expect(sender.statuses.count).to eq 0
+        end
+      end
+    end
+    
+        @save_to.each { |_, v| FileUtils.mkdir_p(v) }
+    
+      # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # config.action_dispatch.rack_cache = true
+    
+          # This double assignment is to prevent an 'unused variable' warning on
+      # Ruby 1.9.3.  Yes, it is dumb, but I don't like Ruby yelling at me.
+      path = path = (req.script_name + req.path_info).squeeze('/')
+    
+    namespace :doc do
+  task :readmes do
+    Dir.glob 'lib/rack/protection/*.rb' do |file|
+      excluded_files = %w[lib/rack/protection/base.rb lib/rack/protection/version.rb]
+      next if excluded_files.include?(file)
+      doc  = File.read(file)[/^  module Protection(\n)+(    #[^\n]*\n)*/m].scan(/^ *#(?!#) ?(.*)\n/).join('\n')
+      file = 'doc/#{file[4..-4].tr('/_', '-')}.rdoc'
+      Dir.mkdir 'doc' unless File.directory? 'doc'
+      puts 'writing #{file}'
+      File.open(file, 'w') { |f| f << doc }
     end
   end
     
-      it 'renders a podcast index if there is a podcast with the slug successfully' do
-    expect(get: '/#{podcast.slug}').to route_to(
-      controller: 'stories',
-      action: 'index',
-      username: podcast.slug,
-    )
+          def redirect(env)
+        request = Request.new(env)
+        warn env, 'attack prevented by #{self.class}'
+        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
+      end
+    
+          expected_header = <<-END.chomp
+rack.%2573ession=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.%2573ession=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.%2573ession=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
+END
+      expect(last_response.headers['Set-Cookie']).to eq(expected_header)
+    end
   end
     
-      def user_org_admin?
-    user.org_admin?(record.organization_id)
+      # retrieve only the latest spec for all locally installed plugins
+  # @return [Hash] result hash {plugin_name.downcase => plugin_spec}
+  def find_latest_gem_specs
+    LogStash::PluginManager.all_installed_plugins_gem_specs(gemfile).inject({}) do |result, spec|
+      previous = result[spec.name.downcase]
+      result[spec.name.downcase] = previous ? [previous, spec].max_by{|s| s.version} : spec
+      result
+    end
   end
 end
 
     
-      def self.buff!(article_id, text, buffer_profile_id_code, social_service_name = 'twitter', tag_id = nil)
-    buffer_response = send_to_buffer(text, buffer_profile_id_code)
-    create(
-      article_id: article_id,
-      tag_id: tag_id,
-      body_text: text,
-      buffer_profile_id_code: buffer_profile_id_code,
-      social_service_name: social_service_name,
-      buffer_response: buffer_response,
-      status: 'sent_direct',
-    )
-  end
+    selected_boxes = if ENV.include?('LS_VAGRANT_HOST') then
+                   config.platforms.select { |p| p.name  == ENV['LS_VAGRANT_HOST'] }
+                 else
+                   default_vagrant_boxes
+                 end
     
-      def page_params
-    allowed_params = %i[title slug body_markdown body_html description template is_top_level_path]
-    params.require(:page).permit(allowed_params)
-  end
+          context 'at the top level' do
+        let(:src) { 'if a then b else c end' }
+    
+            it_behaves_like 'nonmatching'
+      end
+    
+                do_something
+          #{close}
+        RUBY
+    
+    # This file provides the following methods:
+#   encodeURIComponent(componentString)
+#   string.charCodeAt(k)
+    
+          def title
+        'Latest Changes (Globally)'
+      end
+    
+          def author
+        first = page.last_version
+        return DEFAULT_AUTHOR unless first
+        first.author.name.respond_to?(:force_encoding) ? first.author.name.force_encoding('UTF-8') : first.author.name
+      end
+    
+    def replace_header(head, header_name)
+  head.sub!(/(\.#{header_name}\s*= ').*'/) { '#{$1}#{send(header_name)}''}
 end
+    
+        get %r{/(.+?)/([0-9a-f]{40})} do
+      file_path = params[:captures][0]
+      version   = params[:captures][1]
+      wikip     = wiki_page(file_path, file_path, version)
+      name      = wikip.name
+      path      = wikip.path
+      if page = wikip.page
+        @page    = page
+        @name    = name
+        @content = page.formatted_data
+        @version = version
+        @bar_side = wikip.wiki.bar_side
+        mustache :page
+      elsif file = wikip.wiki.file('#{file_path}', version, true)
+        show_file(file)
+      else
+        halt 404
+      end
+    end
