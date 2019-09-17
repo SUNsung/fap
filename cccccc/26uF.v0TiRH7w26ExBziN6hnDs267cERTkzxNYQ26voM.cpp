091b34a80fbb89f0fb1afa5522c58745eb45b23f
SUNsung <weatherius@gmail.com>
2019-09-18 01:35:40 +0300

    Rewrite [26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp] File in: 2019-09-18 01:35:40

--- cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
+++ cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
@@ -1,255 +1,277 @@
 
         
-        
-    {
-    {        return result;
+        #ifndef NATIVE_MATE_NATIVE_MATE_CONSTRUCTOR_H_
+#define NATIVE_MATE_NATIVE_MATE_CONSTRUCTOR_H_
+    
+      v8::Local<v8::Object> GetHandle() const override;
+    
+      // Delegate implementations.
+  void OnError(const std::string& error) override;
+  void OnError(const std::string& message,
+               const int code,
+               const std::string& domain) override;
+  void OnCheckingForUpdate() override;
+  void OnUpdateAvailable() override;
+  void OnUpdateNotAvailable() override;
+  void OnUpdateDownloaded(const std::string& release_notes,
+                          const std::string& release_name,
+                          const base::Time& release_date,
+                          const std::string& update_url) override;
+    
+    #include 'base/callback.h'
+#include 'chrome/browser/extensions/global_shortcut_listener.h'
+#include 'native_mate/handle.h'
+#include 'shell/browser/api/trackable_object.h'
+#include 'ui/base/accelerators/accelerator.h'
+    
+    
+    {}  // namespace
+    
+      /// Bookkeeping to ensure that useConformancesFrom{ObjectiveC,}Type() is
+  /// only called once for each unique type, as an optimization.
+  llvm::DenseSet<TypeBase *> usedConformancesFromTypes;
+  llvm::DenseSet<TypeBase *> usedConformancesFromObjectiveCTypes;
+    
+    #include 'swift/Reflection/MetadataSource.h'
+    
+      StringRef getFieldName(uintptr_t Offset, uintptr_t Low,
+                         uintptr_t High) const {
+    uintptr_t nameAddr = (uintptr_t)FieldName.get() + Offset;
+    if (nameAddr < Low || nameAddr > High)
+      return '';
+    return (const char *)nameAddr;
+  }
+    
+          Demangle::Demangler Dem;
+      auto Demangled = Dem.demangleType(Line);
+      auto *TypeRef =
+          swift::Demangle::decodeMangledType(Context.getBuilder(), Demangled);
+      if (TypeRef == nullptr) {
+        OS << 'Invalid typeref: ' << Line << '\n';
+        continue;
+      }
+    
+      /// This class has hidden visibility.
+  Hidden               = 0x00010,
+    
+      static constexpr uint32_t validateInlineOffset(uint32_t offset) {
+    return assert(offsetCanBeInline(offset)
+                  && 'offset too big!'),
+           offset;
+  }
+    
+        case_setIfNonEmpty(ABIName)
+    case_setIfNonEmpty(SymbolNamespace)
+    case_setIfNonEmpty(RelatedEntityName)
+    
+    SECP256K1_INLINE static void secp256k1_fe_sqr_inner(uint64_t *r, const uint64_t *a) {
+/**
+ * Registers: rdx:rax = multiplication accumulator
+ *            r9:r8   = c
+ *            rcx:rbx = d
+ *            r10-r14 = a0-a4
+ *            r15     = M (0xfffffffffffff)
+ *            rdi     = r
+ *            rsi     = a / t?
+ */
+  uint64_t tmp1, tmp2, tmp3;
+__asm__ __volatile__(
+    'movq 0(%%rsi),%%r10\n'
+    'movq 8(%%rsi),%%r11\n'
+    'movq 16(%%rsi),%%r12\n'
+    'movq 24(%%rsi),%%r13\n'
+    'movq 32(%%rsi),%%r14\n'
+    'movq $0xfffffffffffff,%%r15\n'
     }
-}
+    
+        UniValue obj2(UniValue::VOBJ);
+    BOOST_CHECK(obj2.pushKV('cat1', 9000));
+    BOOST_CHECK(obj2.pushKV('cat2', 12345));
+    
+    #endif // BITCOIN_CRYPTO_RIPEMD160_H
 
+    
+      Status NewRandomAccessFile(const std::string& f, RandomAccessFile** r) {
+    class CountingFile : public RandomAccessFile {
+     private:
+      RandomAccessFile* target_;
+      AtomicCounter* counter_;
+     public:
+      CountingFile(RandomAccessFile* target, AtomicCounter* counter)
+          : target_(target), counter_(counter) {
+      }
+      virtual ~CountingFile() { delete target_; }
+      virtual Status Read(uint64_t offset, size_t n, Slice* result,
+                          char* scratch) const {
+        counter_->Increment();
+        return target_->Read(offset, n, result, scratch);
+      }
+    };
+    }
+    
+      fname = DescriptorFileName('bar', 100);
+  ASSERT_EQ('bar/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(100, number);
+  ASSERT_EQ(kDescriptorFile, type);
+    
+    // Header is checksum (4 bytes), length (2 bytes), type (1 byte).
+static const int kHeaderSize = 4 + 2 + 1;
+    
+      // Generate goog.provides() calls.
+  void FindProvides(const GeneratorOptions& options, io::Printer* printer,
+                    const std::vector<const FileDescriptor*>& file,
+                    std::set<std::string>* provided) const;
+  void FindProvidesForFile(const GeneratorOptions& options,
+                           io::Printer* printer, const FileDescriptor* file,
+                           std::set<std::string>* provided) const;
+  void FindProvidesForMessage(const GeneratorOptions& options,
+                              io::Printer* printer, const Descriptor* desc,
+                              std::set<std::string>* provided) const;
+  void FindProvidesForEnum(const GeneratorOptions& options,
+                           io::Printer* printer, const EnumDescriptor* enumdesc,
+                           std::set<std::string>* provided) const;
+  // For extension fields at file scope.
+  void FindProvidesForFields(const GeneratorOptions& options,
+                             io::Printer* printer,
+                             const std::vector<const FieldDescriptor*>& fields,
+                             std::set<std::string>* provided) const;
+  // Print the goog.provides() found by the methods above.
+  void GenerateProvides(const GeneratorOptions& options, io::Printer* printer,
+                        std::set<std::string>* provided) const;
+    
+    namespace google {
+namespace protobuf {
+namespace io {
+    }
+    }
+    }
+    
+    #include <google/protobuf/stubs/bytestream.h>
+    
+    TEST(StatusOr, TestPointerAssignmentStatusOk) {
+  const int kI = 0;
+  StatusOr<const int*> source(&kI);
+  StatusOr<const int*> target;
+  target = source;
+  EXPECT_EQ(source.status(), target.status());
+  EXPECT_EQ(source.ValueOrDie(), target.ValueOrDie());
+}
+    
+    TEST(StructurallyValidTest, ValidUTF8String) {
+  // On GCC, this string can be written as:
+  //   'abcd 1234 - \u2014\u2013\u2212'
+  // MSVC seems to interpret \u differently.
+  string valid_str('abcd 1234 - \342\200\224\342\200\223\342\210\222 - xyz789');
+  EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data(),
+                                      valid_str.size()));
+  // Additional check for pointer alignment
+  for (int i = 1; i < 8; ++i) {
+    EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data() + i,
+                                        valid_str.size() - i));
+  }
+}
+    
+    
+    {  typedef integral_constant<int, 1> one_type;
+  EXPECT_EQ(1, one_type::value);
+}
+    
+    // Generate param traits read methods.
+#include 'ipc/param_traits_read_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+    
+    Base::Base(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+	   const std::string& extension_id)
+    : extension_id_(extension_id),
+      id_(id),
+      delay_destruction_(false),
+      pending_destruction_(false),
+      object_manager_(object_manager) {
+}
+    
+    base::StringPiece GetStringResource(int resource_id) {
+  return ResourceBundle::GetSharedInstance().GetRawDataResource(resource_id);
+}
+    
+    #include 'content/nw/src/api/menuitem/menuitem.h'
+    
+     protected:
+  ~NwObjCreateFunction() override;
+    
+    
+    {    for (auto& media_list : media_list_) {
+      media_list->StartUpdating(this);
+    }
+  }
+    
+      // implement nw.Screen.initEventListeners()
+  class NwScreenInitEventListenersFunction: public NWSyncExtensionFunction {
+    public:
+      NwScreenInitEventListenersFunction();
+      bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
     
     #include <array>
 #include <string>
 #include <string_view>
 #include <unordered_map>
     
-        // A large penalty is paid for conversion of digits no one will see anyway.
-    // limit the digits to the minimum of the existing precision or the
-    // requested precision.
-    cdigits = precision + 1;
-    if (cdigits > (uint32_t)a->cdigit)
+    
+    {    bool Number::IsZero() const
     {
-        cdigits = (uint32_t)a->cdigit;
-    }
-    
-        class UnitConverterMock : public UnitConversionManager::IUnitConverter
-    {
-    public:
-        UnitConverterMock();
-        void Initialize() override;
-        std::vector<UCM::Category> GetCategories() override;
-        UCM::CategorySelectionInitializer SetCurrentCategory(const UCM::Category& input) override;
-        UCM::Category GetCurrentCategory();
-        void SetCurrentUnitTypes(const UCM::Unit& fromType, const UCM::Unit& toType) override;
-        void SwitchActive(const std::wstring& newValue);
-        std::wstring SaveUserPreferences() override;
-        void RestoreUserPreferences(_In_ const std::wstring& userPreferences) override;
-        void SendCommand(UCM::Command command) override;
-        void SetViewModelCallback(const std::shared_ptr<UCM::IUnitConverterVMCallback>& newCallback) override;
-        void SetViewModelCurrencyCallback(_In_ const std::shared_ptr<UCM::IViewModelCurrencyCallback>& /*newCallback*/) override
-        {
-        }
-        void Calculate() override
-        {
-        }
-        void ResetCategoriesAndRatios() override
-        {
-        }
-        std::future<std::pair<bool, std::wstring>> RefreshCurrencyRatios() override
-        {
-            co_return std::make_pair(true, L'');
-        }
-    }
-    
-    unsigned int CalculatorHistory::AddItem(_In_ shared_ptr<HISTORYITEM> const& spHistoryItem)
-{
-    if (m_historyItems.size() >= m_maxHistorySize)
-    {
-        m_historyItems.erase(m_historyItems.begin());
-    }
-    }
-    
-        struct HISTORYITEMVECTOR
-    {
-        std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> spTokens;
-        std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> spCommands;
-        std::wstring expression;
-        std::wstring result;
-    };
-    
-            void Clear();
-        bool TryToggleSign(bool isIntegerMode, std::wstring_view maxNumStr);
-        bool TryAddDigit(unsigned int value, uint32_t radix, bool isIntegerMode, std::wstring_view maxNumStr, int32_t wordBitWidth, int maxDigits);
-        bool TryAddDecimalPt();
-        bool HasDecimalPt();
-        bool TryBeginExponent();
-        void Backspace();
-        void SetDecimalSymbol(wchar_t decSymbol);
-        std::wstring ToString(uint32_t radix);
-        Rational ToRational(uint32_t radix, int32_t precision);
-    
-    
-    {    AutomationProperties::SetName(m_host, announcement->Announcement);
-    AutomationPeer ^ peer = FrameworkElementAutomationPeer::FromElement(m_host);
-    if (peer != nullptr)
-    {
-        peer->RaiseAutomationEvent(AutomationEvents::LiveRegionChanged);
+        return all_of(m_mantissa.begin(), m_mantissa.end(), [](auto&& i) { return i == 0; });
     }
 }
 
     
-    SparsePageWriter::SparsePageWriter(
-    const std::vector<std::string>& name_shards,
-    const std::vector<std::string>& format_shards,
-    size_t extra_buffer_capacity)
-    : num_free_buffer_(extra_buffer_capacity + name_shards.size()),
-      clock_ptr_(0),
-      workers_(name_shards.size()),
-      qworkers_(name_shards.size()) {
-  CHECK_EQ(name_shards.size(), format_shards.size());
-  // start writer threads
-  for (size_t i = 0; i < name_shards.size(); ++i) {
-    std::string name_shard = name_shards[i];
-    std::string format_shard = format_shards[i];
-    auto* wqueue = &qworkers_[i];
-    workers_[i].reset(new std::thread(
-        [this, name_shard, format_shard, wqueue] () {
-          std::unique_ptr<dmlc::Stream> fo(
-              dmlc::Stream::Create(name_shard.c_str(), 'w'));
-          std::unique_ptr<SparsePageFormat> fmt(
-              SparsePageFormat::Create(format_shard));
-          fo->Write(format_shard);
-          std::shared_ptr<SparsePage> page;
-          while (wqueue->Pop(&page)) {
-            if (page == nullptr) break;
-            fmt->Write(*page, fo.get());
-            qrecycle_.Push(std::move(page));
-          }
-          fo.reset(nullptr);
-          LOG(INFO) << 'SparsePage::Writer Finished writing to ' << name_shard;
-        }));
+        // if the grouping pattern or thousands symbol changed we need to refresh the display
+    if (m_decGrouping != lastDecGrouping || m_groupSeparator != lastSep)
+    {
+        numChanged = true;
+    }
+    
+    namespace CalculationManager
+{
+    enum CALCULATOR_MODE
+    {
+        CM_STD = 0,
+        CM_SCI,
+    };
+    }
+    
+    NarratorAnnouncement ^ CalculatorAnnouncement::GetDisplayCopiedAnnouncement(String ^ announcement)
+{
+    return ref new NarratorAnnouncement(
+        announcement, CalculatorActivityIds::DisplayCopied, AutomationNotificationKind::ActionCompleted, AutomationNotificationProcessing::ImportantMostRecent);
+}
+    
+      if (op == Operation::DELETE_KEYS) {
+    m_operation = QSharedPointer<BulkOperations::AbstractOperation>(
+        new BulkOperations::DeleteOperation(connection, dbIndex,
+                                            callbackWrapper, keyPattern));
+  } else if (op == Operation::TTL) {
+    m_operation = QSharedPointer<BulkOperations::AbstractOperation>(
+        new BulkOperations::TtlOperation(connection, dbIndex, callbackWrapper,
+                                         keyPattern));
+  } else if (op == Operation::COPY_KEYS) {
+    m_operation = QSharedPointer<BulkOperations::AbstractOperation>(
+        new BulkOperations::CopyOperation(connection, dbIndex, callbackWrapper,
+                                          keyPattern));
+  } else if (op == Operation::IMPORT_RDB_KEYS) {
+    m_operation = QSharedPointer<BulkOperations::AbstractOperation>(
+        new BulkOperations::RDBImportOperation(
+            connection, dbIndex, callbackWrapper, m_python, keyPattern));
   }
-}
     
-      loss_chg = 50.0;
-  sum_hess = 10.0;
-  base_weight = 30.0;
-  leaf_child_cnt = 0;
-  fo->Write(&loss_chg, sizeof(float));
-  fo->Write(&sum_hess, sizeof(float));
-  fo->Write(&base_weight, sizeof(float));
-  fo->Write(&leaf_child_cnt, sizeof(int));
-  fo.reset();
-  std::unique_ptr<dmlc::Stream> fi(dmlc::Stream::Create(tmp_file.c_str(), 'r'));
-    
-    /*! \brief Mini batch used in XGBoost Data Iteration */
-typedef struct {  // NOLINT(*)
-  /*! \brief number of rows in the minibatch */
-  size_t size;
-  /*! \brief row pointer to the rows in the data */
-#ifdef __APPLE__
-  /* Necessary as Java on MacOS defines jlong as long int
-   * and gcc defines int64_t as long long int. */
-  long* offset; // NOLINT(*)
-#else
-  int64_t* offset;  // NOLINT(*)
-#endif  // __APPLE__
-  /*! \brief labels of each instance */
-  float* label;
-  /*! \brief weight of each instance, can be NULL */
-  float* weight;
-  /*! \brief feature index */
-  int* index;
-  /*! \brief feature values */
-  float* value;
-} XGBoostBatchCSR;
-    
-      bool Read(SparsePage* page,
-            dmlc::SeekStream* fi,
-            const std::vector<bst_uint>& sorted_index_set) override {
-    if (!fi->Read(&disk_offset_)) return false;
-    auto& offset_vec = page->offset.HostVector();
-    auto& data_vec = page->data.HostVector();
-    // setup the offset
-    offset_vec.clear();
-    offset_vec.push_back(0);
-    for (unsigned int fid : sorted_index_set) {
-      CHECK_LT(fid + 1, disk_offset_.size());
-      size_t size = disk_offset_[fid + 1] - disk_offset_[fid];
-      offset_vec.push_back(offset_vec.back() + size);
-    }
-    data_vec.resize(offset_vec.back());
-    // read in the data
-    size_t begin = fi->Tell();
-    size_t curr_offset = 0;
-    for (size_t i = 0; i < sorted_index_set.size();) {
-      bst_uint fid = sorted_index_set[i];
-      if (disk_offset_[fid] != curr_offset) {
-        CHECK_GT(disk_offset_[fid], curr_offset);
-        fi->Seek(begin + disk_offset_[fid] * sizeof(Entry));
-        curr_offset = disk_offset_[fid];
-      }
-      size_t j, size_to_read = 0;
-      for (j = i; j < sorted_index_set.size(); ++j) {
-        if (disk_offset_[sorted_index_set[j]] == disk_offset_[fid] + size_to_read) {
-          size_to_read += offset_vec[j + 1] - offset_vec[j];
-        } else {
-          break;
-        }
-      }
-    }
+    void BulkOperations::RDBImportOperation::performOperation(
+    QSharedPointer<RedisClient::Connection>, int) {
+  m_combinator = QSharedPointer<AsyncFuture::Combinator>(
+      new AsyncFuture::Combinator(AsyncFuture::FailFast));
     }
     
-      vals_in.clear(); ss.flush(); ss.clear(); ss.str('');
-  ss << ' (3,2,1,)';
-  ss >> vals_in;
-  EXPECT_EQ(vals_in, vals);
-    
-    
-    { private:
-  RowBlock<IndexType> out_;
-  std::unique_ptr<Parser<IndexType> > parser_;
-  uint32_t num_col_;
-  std::vector<size_t> offset_;
-  std::vector<IndexType> dense_index_;
-  std::vector<xgboost::bst_float> dense_value_;
-};
-    
-    const std::string& DHTResponseMessage::getType() const { return R; }
-    
-    void DHTRoutingTable::showBuckets() const
-{
-  /*
-    for(std::deque<std::shared_ptr<DHTBucket> >::const_iterator itr =
-    buckets_.begin(); itr != buckets_.end(); ++itr) {
-    cerr << 'prefix = ' << (*itr)->getPrefixLength() << ', '
-    << 'nodes = ' << (*itr)->countNode() << endl;
-    }
-  */
-}
-    
-    #include <cstring>
-#include <cassert>
-#include <cstdio>
-#include <utility>
-    
-    class DHTRoutingTableDeserializer {
-private:
-  int family_;
-    }
-    
-    DHTTaskExecutor::DHTTaskExecutor(int numConcurrent)
-    : numConcurrent_(numConcurrent)
-{
-}
-    
-      virtual std::shared_ptr<DHTTask>
-  createPeerAnnounceTask(const unsigned char* infoHash) CXX11_OVERRIDE;
-    
-    void DHTTaskQueueImpl::executeTask()
-{
-  A2_LOG_DEBUG('Updating periodicTaskQueue1');
-  periodicTaskQueue1_.update();
-  A2_LOG_DEBUG('Updating periodicTaskQueue2');
-  periodicTaskQueue2_.update();
-  A2_LOG_DEBUG('Updating immediateTaskQueue');
-  immediateTaskQueue_.update();
-}
-    
-    namespace aria2 {
-    }
-    
-    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
-                                             std::chrono::seconds interval)
-    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
-{
-}
+      bool isMetadataValid() const override;
