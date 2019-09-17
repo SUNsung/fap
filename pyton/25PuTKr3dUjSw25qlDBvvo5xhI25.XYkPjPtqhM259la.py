d3a2848c22971715ddee8996a3e00065f76e41e8
SUNsung <weatherius@gmail.com>
2019-09-18 00:43:11 +0300

    Rewrite [25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py] File in: 2019-09-18 00:43:11

--- pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
+++ pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
@@ -1,129 +1,66 @@
 
         
-        import jieba
-import re
+                self._wait_for_requests(self.module, request_list)
+        self._refresh_servers(self.module, changed_servers)
     
-    seg_list = jieba.cut('我来到北京清华大学', cut_all=True)
-print('Full Mode: ' + '/ '.join(seg_list))  # 全模式
+        items_map = collections.OrderedDict([(i[key], i.copy()) for i in original_list])
     
-    import jieba
-jieba.enable_parallel(4)
-import jieba.analyse
-from optparse import OptionParser
+    # Sends a push notification with title and body to a channel
+- pushbullet:
+    api_key: ABC123abc123ABC123abc123ABC123ab
+    channel: my-awesome-channel
+    title: ALERT! Signup service is down
+    body: Error rate on signup service is over 90% for more than 2 minutes
+'''
     
-    log_f = open('1.log','wb')
-log_f.write(words.encode('utf-8'))
     
-        # For training, we want a lot of parallel reading and shuffling.
-    # For eval, we want no shuffling and parallel reading doesn't matter.
-    if is_training:
-      d = tf.data.Dataset.from_tensor_slices(tf.constant(input_files))
-      d = d.repeat()
-      d = d.shuffle(buffer_size=len(input_files))
+if __name__ == '__main__':
+    main()
+
     
-          feature = InputFeatures(
-          unique_id=unique_id,
-          example_index=example_index,
-          doc_span_index=doc_span_index,
-          tokens=tokens,
-          token_to_orig_map=token_to_orig_map,
-          token_is_max_context=token_is_max_context,
-          input_ids=input_ids,
-          input_mask=input_mask,
-          segment_ids=segment_ids,
-          start_position=start_position,
-          end_position=end_position,
-          is_impossible=example.is_impossible)
+    extends_documentation_fragment:
+    - utm
+'''
     
-        predict_file = os.path.join(FLAGS.output_dir, 'predict.tf_record')
-    run_classifier.file_based_convert_examples_to_features(
-        predict_examples, label_list, FLAGS.max_seq_length, tokenizer,
-        predict_file)
     
-    flags.DEFINE_integer('max_predictions_per_seq', 20,
-                     'Maximum number of masked LM predictions per sequence.')
+def rmul(left, right):
+    return right * left
     
-        def feed(self, aBuf):
-        for c in aBuf:
-            # PY3K: aBuf is a byte array, so c is an int, not a byte
-            for codingSM in self._mCodingSM:
-                if not codingSM:
-                    continue
-                if not codingSM.active:
-                    continue
-                codingState = codingSM.next_state(wrap_ord(c))
-                if codingState == constants.eError:
-                    codingSM.active = False
-                    self._mActiveSM -= 1
-                    if self._mActiveSM <= 0:
-                        self._mState = constants.eNotMe
-                        return self.get_state()
-                elif codingState == constants.eItsMe:
-                    self._mState = constants.eFoundIt
-                    self._mDetectedCharset = codingSM.get_coding_state_machine()  # nopep8
-                    return self.get_state()
+        def test_to_json_period_index(self):
+        idx = pd.period_range('2016', freq='Q-JAN', periods=2)
+        data = pd.Series(1, idx)
+        result = data.to_json(orient='table', date_format='iso')
+        result = json.loads(result, object_pairs_hook=OrderedDict)
+        result['schema'].pop('pandas_version')
     
-    ISO2022KR_cls = (
-2,0,0,0,0,0,0,0,  # 00 - 07
-0,0,0,0,0,0,0,0,  # 08 - 0f
-0,0,0,0,0,0,0,0,  # 10 - 17
-0,0,0,1,0,0,0,0,  # 18 - 1f
-0,0,0,0,3,0,0,0,  # 20 - 27
-0,4,0,0,0,0,0,0,  # 28 - 2f
-0,0,0,0,0,0,0,0,  # 30 - 37
-0,0,0,0,0,0,0,0,  # 38 - 3f
-0,0,0,5,0,0,0,0,  # 40 - 47
-0,0,0,0,0,0,0,0,  # 48 - 4f
-0,0,0,0,0,0,0,0,  # 50 - 57
-0,0,0,0,0,0,0,0,  # 58 - 5f
-0,0,0,0,0,0,0,0,  # 60 - 67
-0,0,0,0,0,0,0,0,  # 68 - 6f
-0,0,0,0,0,0,0,0,  # 70 - 77
-0,0,0,0,0,0,0,0,  # 78 - 7f
-2,2,2,2,2,2,2,2,  # 80 - 87
-2,2,2,2,2,2,2,2,  # 88 - 8f
-2,2,2,2,2,2,2,2,  # 90 - 97
-2,2,2,2,2,2,2,2,  # 98 - 9f
-2,2,2,2,2,2,2,2,  # a0 - a7
-2,2,2,2,2,2,2,2,  # a8 - af
-2,2,2,2,2,2,2,2,  # b0 - b7
-2,2,2,2,2,2,2,2,  # b8 - bf
-2,2,2,2,2,2,2,2,  # c0 - c7
-2,2,2,2,2,2,2,2,  # c8 - cf
-2,2,2,2,2,2,2,2,  # d0 - d7
-2,2,2,2,2,2,2,2,  # d8 - df
-2,2,2,2,2,2,2,2,  # e0 - e7
-2,2,2,2,2,2,2,2,  # e8 - ef
-2,2,2,2,2,2,2,2,  # f0 - f7
-2,2,2,2,2,2,2,2,  # f8 - ff
-)
+            msg = 'Invalid length for values or for binner'
+        with pytest.raises(ValueError, match=msg):
+            generate_bins_generic(values, [], 'right')
+        with pytest.raises(ValueError, match=msg):
+            generate_bins_generic(values[:0], binner, 'right')
     
-    # 128  --> 0.79
-# 256  --> 0.92
-# 512  --> 0.986
-# 1024 --> 0.99944
-# 2048 --> 0.99999
-#
-# Idea Distribution Ratio = 0.98653 / (1-0.98653) = 73.24
-# Random Distribution Ration = 512 / (2350-512) = 0.279.
-# 
-# Typical Distribution Ratio  
+        def get_table(self, table_name, schema=None):
+        schema = schema or self.meta.schema
+        if schema:
+            tbl = self.meta.tables.get('.'.join([schema, table_name]))
+        else:
+            tbl = self.meta.tables.get(table_name)
     
-    # 128  --> 0.42261
-# 256  --> 0.57851
-# 512  --> 0.74851
-# 1024 --> 0.89384
-# 2048 --> 0.97583
-#
-# Idea Distribution Ratio = 0.74851/(1-0.74851) =2.98
-# Random Distribution Ration = 512/(5401-512)=0.105
-#
-# Typical Distribution Ratio about 25% of Ideal one, still much higher than RDR
+            url2 = s3_listener.expand_redirect_url('http://example.org/?id=I', 'K', 'B')
+        self.assertEqual(url2, 'http://example.org/?id=I&key=K&bucket=B')
     
-    Latin5CyrillicModel = {
-  'charToOrderMap': latin5_CharToOrderMap,
-  'precedenceMatrix': RussianLangModel,
-  'mTypicalPositiveRatio': 0.976601,
-  'keepEnglishLetter': False,
-  'charsetName': 'ISO-8859-5'
-}
+            if response.content:
+            # fix hardcoded account ID in ARNs returned from this API
+            self._fix_account_id(response)
+            # fix dates returned from this API (fixes an issue with Terraform)
+            self._fix_date_format(response)
+            # fix content-length header
+            response.headers['content-length'] = len(response._content)
+    
+    
+def publish_lambda_duration(time_before, kwargs):
+    time_after = now_utc()
+    publish_lambda_metric('Duration', time_after - time_before, kwargs)
+    
+            self.assertEqual(1, len(rules))
+        self.assertEqual(TEST_EVENT_PATTERN, json.loads(rules[0]['EventPattern']))
