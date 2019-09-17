c2f09350e5e376602ca508d57e3b2458063de5f3
SUNsung <weatherius@gmail.com>
2019-09-17 05:22:30 +0300

    Rewrite [46gbMqLjGjv2k460IRBxsCRmWw46VUABisZDLMQ46t60.cpp] File in: 2019-09-17 05:22:30

--- cccccc/46gbMqLjGjv2k460IRBxsCRmWw46VUABisZDLMQ46t60.cpp
+++ cccccc/46gbMqLjGjv2k460IRBxsCRmWw46VUABisZDLMQ46t60.cpp
@@ -1,116 +1,233 @@
 
         
-        #ifndef NDEBUG
-#define DEBUG_VAR
-#else
-#define DEBUG_VAR UNUSED
-#endif
+        IPC_SYNC_MESSAGE_ROUTED3_1(ShellViewHostMsg_Call_Static_Method_Sync,
+                           std::string /* type name */,
+                           std::string /* method name */,
+                           base::ListValue /* arguments */,
+                           base::ListValue /* result */)
     
-     private:
-  // Replaces multiple consecutive separators with a single separator.
-  // For example, 'bar///foo' becomes 'bar/foo'. Does not eliminate other
-  // redundancies that might be in a pathname involving '.' or '..'.
-  //
-  // A pathname with multiple consecutive separators may occur either through
-  // user error or as a result of some scripts or APIs that generate a pathname
-  // with a trailing separator. On other platforms the same API or script
-  // may NOT generate a pathname with a trailing '/'. Then elsewhere that
-  // pathname may have another '/' and pathname components added to it,
-  // without checking for the separator already being there.
-  // The script language and operating system may allow paths like 'foo//bar'
-  // but some of the functions in FilePath will not handle that correctly. In
-  // particular, RemoveTrailingPathSeparator() only removes one separator, and
-  // it is called in CreateDirectoriesRecursively() assuming that it will change
-  // a pathname from directory syntax (trailing separator) to filename syntax.
-  //
-  // On Windows this method also replaces the alternate path separator '/' with
-  // the primary path separator '\\', so that for example 'bar\\/\\foo' becomes
-  // 'bar\\foo'.
+      static void Call(content::Shell* shell,
+                   const std::string& method,
+                   const base::ListValue& arguments,
+                   base::ListValue* result,
+                   DispatcherHost* dispatcher_host);
     
-      explicit tuple(GTEST_BY_REF_(T0) f0, GTEST_BY_REF_(T1) f1,
-      GTEST_BY_REF_(T2) f2, GTEST_BY_REF_(T3) f3, GTEST_BY_REF_(T4) f4,
-      GTEST_BY_REF_(T5) f5) : f0_(f0), f1_(f1), f2_(f2), f3_(f3), f4_(f4),
-      f5_(f5) {}
-    
-    // Build a Table file from the contents of *iter.  The generated file
-// will be named according to meta->number.  On success, the rest of
-// *meta will be filled with metadata about the generated table.
-// If no data is present in *iter, meta->file_size will be set to
-// zero, and no Table file will be produced.
-Status BuildTable(const std::string& dbname, Env* env, const Options& options,
-                  TableCache* table_cache, Iterator* iter, FileMetaData* meta);
-    
-    // Return the name of the descriptor file for the db named by
-// 'dbname' and the specified incarnation number.  The result will be
-// prefixed with 'dbname'.
-std::string DescriptorFileName(const std::string& dbname, uint64_t number);
-    
-      // If the cache has no mapping for 'key', returns nullptr.
-  //
-  // Else return a handle that corresponds to the mapping.  The caller
-  // must call this->Release(handle) when the returned mapping is no
-  // longer needed.
-  virtual Handle* Lookup(const Slice& key) = 0;
-    
-    // Return a builtin comparator that uses lexicographic byte-wise
-// ordering.  The result remains the property of this module and
-// must not be deleted.
-LEVELDB_EXPORT const Comparator* BytewiseComparator();
-    
-    #ifndef STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
-#define STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
-    
-    #include 'leveldb/export.h'
-    
-    
-    {  // close database
-  delete db;
-  DestroyDB(dbpath, leveldb::Options());
+    Base::Base(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+	   const std::string& extension_id)
+    : extension_id_(extension_id),
+      id_(id),
+      delay_destruction_(false),
+      pending_destruction_(false),
+      object_manager_(object_manager) {
 }
     
-    // port/port_config.h availability is automatically detected via __has_include
-// in newer compilers. If LEVELDB_HAS_PORT_CONFIG_H is defined, it overrides the
-// configuration detection.
-#if defined(LEVELDB_HAS_PORT_CONFIG_H)
-    
-    #endif  // STORAGE_LEVELDB_TABLE_TWO_LEVEL_ITERATOR_H_
-
-    
-        public:
-        BlockMomentumSGD(const MPIWrapperPtr& pMPI, size_t reportFreq, DEVICEID_TYPE devID, 
-                        bool useNestrovMomentum, bool resetSGDM, 
-                        double blockLearningRate, 
-                        double blockMomentumAsTimeConstant, size_t syncPeriod)
-            :IMASGD<ElemType>(pMPI, reportFreq, devID)
-        {
-            m_syncPeriodPerWorker = syncPeriod / pMPI->NumNodesInUse();
-            m_blockMomentumAsTimeConstantPerWorker = blockMomentumAsTimeConstant / pMPI->NumNodesInUse(); 
-            m_useNesterovMomentum = useNestrovMomentum;
-            m_resetSGDMomentumAfterAggregation = resetSGDM; 
-            m_blockLearningRate = blockLearningRate;
-        }
+      ~EventListener() override;
     
     
-    {            // Let's update our weights no matter what.
-            float secondsOnCommunication = 0.0f;
-            size_t totalSamples = 0;
-            ModelAggregationProcessing(samplesSinceLastSync, learnableNodes, smoothedGradients, totalSamples, secondsOnCommunication);
-        }
-    
-        // generate a generic symbol name for a node
-    void GenerateName()
     {
-        char buffer[10];
-        sprintf(buffer, '%d', ++s_nameCounter);
-        m_name = std::string('unnamed') + buffer;
+    {    if (zoom_controller) {
+      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
+      if (zoom_factor > content::kMaximumZoomFactor) {
+        zoom_factor = content::kMaximumZoomFactor;
+      }
+      if (zoom_factor < content::kMinimumZoomFactor) {
+        zoom_factor = content::kMinimumZoomFactor;
+      }
+      x *= zoom_factor;
+      y *= zoom_factor;
     }
     
-            m_labelEmbeddingSize   = config('labelEmbeddingSize',   '10');
-        m_constForgetGateValue = config('constForgetGateValue', 'false');
-        m_constInputGateValue  = config('constInputGateValue',  'false');
-        m_constOutputGateValue = config('constOutputGateValue', 'false');
+    Popup(x, y, rvh);
+  } else if (method == 'EnableShowEvent') {
+    arguments.GetBoolean(0, &enable_show_event_);
+  } else {
+    NOTREACHED() << 'Invalid call to Menu method:' << method
+                 << ' arguments:' << arguments;
+  }
+}
     
-        // The following functions create nodes and add them to the net, but don't attach inputs (some don't have inputs).
-    // There are special versions for nodes with custom constructors, and a catch-all, CreateComputationNode(), for all others.
-    // TODO: Do we really need these? Folks who want to use C++ can instead say net->AddNodeToNet(New<>(...)), which is not that different.
-    // TODO: separate into nodes that have inputs and those that duplicate functions with input adding except just not adding inputs. Clear?
+    #include 'base/logging.h'
+#include 'base/strings/string16.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/nw/src/api/menuitem/menuitem.h'
+    
+      focus_manager_ = NULL;
+  window_ = NULL;
+    
+      GURL manifest_url(manifest);
+  scoped_refptr<CannedBrowsingDataAppCacheHelper> helper(
+                                                         new CannedBrowsingDataAppCacheHelper(content::BrowserContext::GetDefaultStoragePartition(context_)
+                                                                                              ->GetAppCacheService()));
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.setListSync', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwClipboardSetListSyncFunction);
+};
+    
+    bool NwObjCreateFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::DictionaryValue* options = nullptr;
+  int id = 0;
+  std::string type;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetDictionary(2, &options));
+    }
+    
+    
+    {      if (ns > time_ns)
+        return pResult;
+    }
+    
+    static const static_codebook*const _floor_Xx0_books[]={
+  0
+};
+    
+    static const static_bookblock _resbook_16s_0={
+  {
+    {0},
+    {0,0,&_16c0_s_p1_0},
+    {0},
+    {0,0,&_16c0_s_p3_0},
+    {0,0,&_16c0_s_p4_0},
+    {0,0,&_16c0_s_p5_0},
+    {0,0,&_16c0_s_p6_0},
+    {&_16c0_s_p7_0,&_16c0_s_p7_1},
+    {&_16c0_s_p8_0,&_16c0_s_p8_1},
+    {&_16c0_s_p9_0,&_16c0_s_p9_1,&_16c0_s_p9_2}
+   }
+};
+static const static_bookblock _resbook_16s_1={
+  {
+    {0},
+    {0,0,&_16c1_s_p1_0},
+    {0},
+    {0,0,&_16c1_s_p3_0},
+    {0,0,&_16c1_s_p4_0},
+    {0,0,&_16c1_s_p5_0},
+    {0,0,&_16c1_s_p6_0},
+    {&_16c1_s_p7_0,&_16c1_s_p7_1},
+    {&_16c1_s_p8_0,&_16c1_s_p8_1},
+    {&_16c1_s_p9_0,&_16c1_s_p9_1,&_16c1_s_p9_2}
+   }
+};
+static const static_bookblock _resbook_16s_2={
+  {
+    {0},
+    {0,0,&_16c2_s_p1_0},
+    {0,0,&_16c2_s_p2_0},
+    {0,0,&_16c2_s_p3_0},
+    {0,0,&_16c2_s_p4_0},
+    {&_16c2_s_p5_0,&_16c2_s_p5_1},
+    {&_16c2_s_p6_0,&_16c2_s_p6_1},
+    {&_16c2_s_p7_0,&_16c2_s_p7_1},
+    {&_16c2_s_p8_0,&_16c2_s_p8_1},
+    {&_16c2_s_p9_0,&_16c2_s_p9_1,&_16c2_s_p9_2}
+   }
+};
+    
+    #endif /* default implementation */
+    
+    #define HAS_MMX     0x01
+#define HAS_SSE     0x02
+#define HAS_SSE2    0x04
+#define HAS_SSE3    0x08
+#define HAS_SSSE3   0x10
+#define HAS_SSE4_1  0x20
+#define HAS_AVX     0x40
+#define HAS_AVX2    0x80
+#ifndef BIT
+#define BIT(n) (1<<n)
+#endif
+    
+      CodeBlock main;
+  main.init(code, codeSize, 'test');
+  DataBlock data;
+  data.init(data_buffer, dataSize, 'data');
+    
+      CodeAddress frontier() const {
+    return codeBlock.frontier();
+  }
+    
+    
+    {  // open a temporary file
+  snprintf(path, sizeof(path), '/tmp/XXXXXX');
+  int fd = mkstemp(path);
+  if (fd == -1) {
+    raise_warning('Unable to open temporary file');
+    return;
+  }
+  setFd(fd);
+  m_stream = fdopen(fd, 'r+');
+  setName(path);
+  m_rawName = std::string(path);
+}
+    
+    ZipFile::~ZipFile() {
+  ZipFile::closeImpl();
+}
+    
+        arr = make_varray('s0').merge(make_varray('s1'));
+    EXPECT_TRUE(equal(arr, make_varray('s0', 's1')));
+    
+      bool isValid() const { return id != kInvalidTransID; }
+  bool isConsistent() const;
+  std::string print() const;
+  Offset bcPast() const;
+  void optimizeForMemory();
+    
+    // If set, releaseUnit will contain a pointer to any extraneous unit created due
+// to race-conditions while compiling
+Unit* compile_file(const char* s, size_t sz, const SHA1& sha1,
+                   const char* fname, const Native::FuncTable& nativeFuncs,
+                   const RepoOptions&, Unit** releaseUnit = nullptr);
+    
+      SHA1() : q{} {}
+    
+    TransEnv::~TransEnv() {}
+    
+    
+    {    new_value->assign(max.data(), max.size());
+    return true;
+  }
+    
+    
+    {  bool FullMergeV2(const MergeOperationInput& merge_in,
+                   MergeOperationOutput* merge_out) const override {
+    // Put basically only looks at the current/latest value
+    assert(!merge_in.operand_list.empty());
+    merge_out->existing_operand = merge_in.operand_list.back();
+    return true;
+  }
+};
+    
+    
+    { private:
+  std::condition_variable cv_;
+};
+    
+      // drop column family
+  s = db->DropColumnFamily(handles[1]);
+  assert(s.ok());
+    
+    #include 'rocksdb/cache.h'
+#include 'rocksdb/compaction_filter.h'
+#include 'rocksdb/db.h'
+#include 'rocksdb/options.h'
+#include 'rocksdb/slice.h'
+#include 'rocksdb/table.h'
+#include 'rocksdb/utilities/options_util.h'
+    
+    
+    {  virtual std::string GetPrintableOptions() const = 0;
+};
+    
+      // Approximate size of user data packed per block.  Note that the
+  // block size specified here corresponds to uncompressed data.  The
+  // actual size of the unit read from disk may be smaller if
+  // compression is enabled.  This parameter can be changed dynamically.
+  //
+  // Default: 4K
+  size_t block_size;
