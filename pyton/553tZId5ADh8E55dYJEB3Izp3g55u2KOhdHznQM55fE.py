7cf24c7d03c65a466d4e1f3e7d55c66f42f69b93
SUNsung <weatherius@gmail.com>
2019-09-18 12:34:04 +0300

    Rewrite [553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py] File in: 2019-09-18 12:34:04

--- pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
+++ pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
@@ -1,173 +1,65 @@
 
         
-                if self.args.output_options is None:
-            if self.args.verbose:
-                self.args.output_options = ''.join(OUTPUT_OPTIONS)
-            else:
-                self.args.output_options = (
-                    OUTPUT_OPTIONS_DEFAULT
-                    if self.env.stdout_isatty
-                    else OUTPUT_OPTIONS_DEFAULT_STDOUT_REDIRECTED
-                )
+            def _assert_allows(self, response, *expected_methods):
+        'Assert allowed HTTP methods reported in the Allow response header'
+        response_allows = set(response['Allow'].split(', '))
+        self.assertEqual(set(expected_methods + ('OPTIONS',)), response_allows)
     
-    from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
+        def test_plain_annotate(self):
+        agg = Sum('book__pages', filter=Q(book__rating__gt=3))
+        qs = Author.objects.annotate(pages=agg).order_by('pk')
+        self.assertSequenceEqual([a.pages for a in qs], [447, None, 1047])
     
-    
-def test_default_options_overwrite(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['json'] == {'foo': 'bar'}
-    
-    
-@mock.patch('httpie.core.get_response')
-def test_timeout(get_response):
-    def error(msg, *args, **kwargs):
-        global error_msg
-        error_msg = msg % args
-    
-    # If true, show URL addresses after external links.
-#man_show_urls = False
-    
-    known_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
-]
-    
-        # Load the uploaded image file
-    img = face_recognition.load_image_file(file_stream)
-    # Get face encodings for any faces in the uploaded image
-    unknown_face_encodings = face_recognition.face_encodings(img)
-    
-            # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
-    
-        # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(output)
-    print('Found {} faces in image.'.format(len(face_locations)))
-    face_encodings = face_recognition.face_encodings(output, face_locations)
-    
-    print('I found {} face(s) in this photograph.'.format(len(face_landmarks_list)))
-    
-    setup(
-    name='face_recognition',
-    version='1.2.3',
-    description='Recognize faces from Python or from the command line',
-    long_description=readme + '\n\n' + history,
-    author='Adam Geitgey',
-    author_email='ageitgey@gmail.com',
-    url='https://github.com/ageitgey/face_recognition',
-    packages=[
-        'face_recognition',
-    ],
-    package_dir={'face_recognition': 'face_recognition'},
-    package_data={
-        'face_recognition': ['models/*.dat']
-    },
-    entry_points={
-        'console_scripts': [
-            'face_recognition=face_recognition.face_recognition_cli:main',
-            'face_detection=face_recognition.face_detection_cli:main'
-        ]
-    },
-    install_requires=requirements,
-    license='MIT license',
-    zip_safe=False,
-    keywords='face_recognition',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
-)
+        def test_no_version_number(self):
+        ops = FakePostGISOperations()
+        with self.assertRaises(ImproperlyConfigured):
+            ops.spatial_version
 
     
-            # Or instead, use the known face with the smallest distance to the new face
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
+        def __init__(self, feat, layer):
+        '''
+        Initialize Feature from a pointer and its Layer object.
+        '''
+        if not feat:
+            raise GDALException('Cannot create OGR Feature, invalid pointer given.')
+        self.ptr = feat
+        self._layer = layer
+    
+        @property
+    def value(self):
+        'Return an integer contained in this field.'
+        return self.as_int(self._bit64)
     
     
-def load_data(file_path):
-    '''加载数据
-        源数据格式为多行，每行为两个浮点数，分别表示 (x,y)
-    '''
-    data = []
-    with open(file_path, 'r', encoding='utf-8') as fr:
-        for line in fr.read().splitlines():
-            line_float = list(map(float, line.split('\t')))
-            data.append(line_float)
-    data = np.array(data)
-    return data
+class GEOSGeometryBase(GEOSBase):
     
-    import cv2
-import numpy as np
-from fastcluster import linkage
+            set_available_apps() must be balanced with unset_available_apps().
     
-        def process(self, new_face):
-        ''' The blend box function. Adds the created mask to the alpha channel '''
-        if self.skip:
-            logger.trace('Skipping blend box')
-            return new_face
+            video_id = match1(html, r'data-brightcove-id='(\d+)'')
+        
+        assert account_number, video_id
     
-        def get_erosion_kernel(self, mask):
-        ''' Get the erosion kernel '''
-        erosion_ratio = self.config['erosion'] / 100
-        mask_radius = np.sqrt(np.sum(mask)) / 2
-        kernel_size = max(1, int(abs(erosion_ratio * mask_radius)))
-        erosion_kernel = cv2.getStructuringElement(  # pylint: disable=no-member
-            cv2.MORPH_ELLIPSE,  # pylint: disable=no-member
-            (kernel_size, kernel_size))
-        logger.trace('erosion_kernel shape: %s', erosion_kernel.shape)
-        return erosion_kernel
     
-    logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+def cntv_download_by_id(rid, **kwargs):
+    CNTV().download_by_vid(rid, **kwargs)
     
-        The following keys are expected for the _DEFAULTS <metadata> dict:
-        datatype:  [required] A python type class. This limits the type of data that can be
-                   provided in the .ini file and ensures that the value is returned in the
-                   correct type to faceswap. Valid datatypes are: <class 'int'>, <class 'float'>,
-                   <class 'str'>, <class 'bool'>.
-        default:   [required] The default value for this option.
-        info:      [required] A string describing what this option does.
-        choices:   [optional] If this option's datatype is of <class 'str'> then valid
-                   selections can be defined here. This validates the option and also enables
-                   a combobox / radio option in the GUI.
-        gui_radio: [optional] If <choices> are defined, this indicates that the GUI should use
-                   radio buttons rather than a combobox to display this option.
-        min_max:   [partial] For <class 'int'> and <class 'float'> datatypes this is required
-                   otherwise it is ignored. Should be a tuple of min and max accepted values.
-                   This is used for controlling the GUI slider range. Values are not enforced.
-        rounding:  [partial] For <class 'int'> and <class 'float'> datatypes this is
-                   required otherwise it is ignored. Used for the GUI slider. For floats, this
-                   is the number of decimal places to display. For ints this is the step size.
-        fixed:     [optional] [train only]. Training configurations are fixed when the model is
-                   created, and then reloaded from the state file. Marking an item as fixed=False
-                   indicates that this value can be changed for existing models, and will override
-                   the value saved in the state file with the updated value in config. If not
-                   provided this will default to True.
-'''
+    from ..common import *
+from ..extractor import VideoExtractor
+from .universal import *
     
-        new_shape = shape[:3] + [int(shape[3] / (scale ** 2))]
-    var_x = initializer(new_shape, dtype)
-    var_x = tf.transpose(var_x, perm=[2, 0, 1, 3])
-    var_x = tf.image.resize_nearest_neighbor(var_x, size=(shape[0] * scale, shape[1] * scale))
-    var_x = tf.space_to_depth(var_x, block_size=scale)
-    var_x = tf.transpose(var_x, perm=[1, 2, 0, 3])
-    return var_x
+    import mock
+    
+    # Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
+    
+    Certbot will emit a warning if it detects that the credentials file can be
+accessed by other users on your system. The warning reads 'Unsafe permissions
+on credentials configuration file', followed by the path to the credentials
+file. This warning will be emitted each time Certbot uses the credentials file,
+including for renewal, and cannot be silenced except by addressing the issue
+(e.g., by using a command like ``chmod 600`` to restrict access to the file).
+    
+            path = os.path.join(self.tempdir, 'file.ini')
+        dns_test_common.write(VALID_CONFIG, path)
