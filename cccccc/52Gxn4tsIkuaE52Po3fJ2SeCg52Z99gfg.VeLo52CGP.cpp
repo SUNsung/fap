858816a95ad5b4fc2b8155056cc2f8bc9f555106
SUNsung <weatherius@gmail.com>
2019-09-18 17:08:08 +0300

    Rewrite [52Gxn4tsIkuaE52Po3fJ2SeCg52Z99gfg.VeLo52CGP.cpp] File in: 2019-09-18 17:08:08

--- cccccc/52Gxn4tsIkuaE52Po3fJ2SeCg52Z99gfg.VeLo52CGP.cpp
+++ cccccc/52Gxn4tsIkuaE52Po3fJ2SeCg52Z99gfg.VeLo52CGP.cpp
@@ -1,238 +1,158 @@
 
         
-        Status CholeskyThunk::ExecuteOnStream(const ExecuteParams& params) {
-  VLOG(3) << 'type=' << PrimitiveType_Name(type_)
-          << ' uplo=' << se::blas::UpperLowerString(uplo_)
-          << ' batch_size=' << batch_size_ << ' n=' << n_
-       << ' a=' << a_buffer_.ToString()
-       << ' workspace=' << workspace_buffer_.ToString()
-       << ' info=' << info_buffer_.ToString();
-    }
+        #ifndef TESSERACT_CCUTIL_BOXREAD_H_
+#define TESSERACT_CCUTIL_BOXREAD_H_
     
     
-    { private:
-  const BufferAllocation::Slice source_buffer_;
-  const BufferAllocation::Slice destination_buffer_;
-  const uint64 mem_size_;
-};
+    {    ICOORD pt;
+    int halfwidth;
+  };
+  // Type holds the distance of each point from the fitted line and the point
+  // itself. Use of double allows integer distances from ICOORDs to be stored
+  // exactly, and also the floating point results from ConstrainedFit.
+  using DistPointPair = KDPairInc<double, ICOORD>;
     
-    namespace xla {
-namespace gpu {
-    }
-    }
+      // Return true if the given unichar representation is already present in the
+  // UNICHARMAP. The first length characters (maximum) from unichar_repr are
+  // used. The length MUST be non-zero.
+  bool contains(const char* const unichar_repr, int length) const;
     
-    #include 'tensorflow/compiler/xla/service/buffer_assignment.h'
-#include 'tensorflow/compiler/xla/service/gpu/buffer_allocations.h'
-#include 'tensorflow/compiler/xla/service/gpu/hlo_execution_profiler.h'
-#include 'tensorflow/compiler/xla/service/gpu/thunk.h'
-#include 'tensorflow/compiler/xla/service/hlo_instruction.h'
-#include 'tensorflow/core/platform/stream_executor_no_cuda.h'
-#include 'tensorflow/core/platform/types.h'
+    class DENORM;
     
-    
-    { private:
-  // The list of sub-thunks.
-  std::vector<std::unique_ptr<Thunk>> thunks_;
-};
-    
-        insert_rows(0, (TOTAL_KEYS_TO_INSERT * 9) / 10, store.get());
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35,
-    typename T36, typename T37, typename T38, typename T39, typename T40,
-    typename T41, typename T42>
-internal::ValueArray42<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-    T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41,
-    T42> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
-    T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
-    T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
-    T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31, T32 v32, T33 v33,
-    T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39, T40 v40, T41 v41,
-    T42 v42) {
-  return internal::ValueArray42<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39,
-      T40, T41, T42>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13,
-      v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27,
-      v28, v29, v30, v31, v32, v33, v34, v35, v36, v37, v38, v39, v40, v41,
-      v42);
-}
-    
-      linked_ptr& operator=(linked_ptr const& ptr) {
-    if (&ptr != this) {
-      depart();
-      copy(&ptr);
-    }
-    return *this;
+      // Free up any currently unused dawgs.
+  void DeleteUnusedDawgs() {
+    dawgs_.DeleteUnusedObjects();
   }
     
-    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
-    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
-    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
-    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
-    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
-    GTEST_TEMPLATE_ T16, GTEST_TEMPLATE_ T17, GTEST_TEMPLATE_ T18,
-    GTEST_TEMPLATE_ T19, GTEST_TEMPLATE_ T20, GTEST_TEMPLATE_ T21,
-    GTEST_TEMPLATE_ T22, GTEST_TEMPLATE_ T23, GTEST_TEMPLATE_ T24,
-    GTEST_TEMPLATE_ T25>
-struct Templates25 {
-  typedef TemplateSel<T1> Head;
-  typedef Templates24<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
-      T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25> Tail;
+    struct DANGERR_INFO {
+  DANGERR_INFO() :
+    begin(-1), end(-1), dangerous(false), correct_is_ngram(false),
+    leftmost(INVALID_UNICHAR_ID) {}
+  DANGERR_INFO(int b, int e, bool d, bool n, UNICHAR_ID l) :
+    begin(b), end(e), dangerous(d), correct_is_ngram(n), leftmost(l) {}
+  int begin;
+  int end;
+  bool dangerous;
+  bool correct_is_ngram;
+  UNICHAR_ID leftmost;   // in the replacement, what's the leftmost character?
 };
     
-        auto groupItr = grouping.begin();
-    auto currGrouping = *groupItr;
-    // Mark the 'end' of the string as either rend() or rend()-1 if there is a negative sign
-    // We exclude the sign here because we don't want to end up with e.g. '-,123,456'
-    // Then, iterate from back to front, adding group delimiters as needed.
-    auto reverse_end = displayString.rend() - (isNumNegative ? 1 : 0);
-    while (ritr != reverse_end)
-    {
-        groupedStream << *ritr++;
-        groupingSize++;
+    namespace caffe {
     }
     
-    using namespace std;
-using namespace CalculatorApp::Controls;
-using namespace Windows::Foundation;
-using namespace Windows::UI::Xaml::Automation;
-using namespace Windows::UI::Xaml::Automation::Peers;
+    TYPED_TEST(NeuronLayerTest, TestLogGradientBase2Scale3) {
+  typedef typename TypeParam::Dtype Dtype;
+  const Dtype kBase = 2;
+  const Dtype kScale = 3;
+  const Dtype kShift = 0;
+  this->TestLogGradient(kBase, kScale, kShift);
+}
     
-        destroyrat(pint);
+      // Helpers for Init.
+  /**
+   * @brief Remove layers that the user specified should be excluded given the current
+   *        phase, level, and stage.
+   */
+  static void FilterNet(const NetParameter& param,
+      NetParameter* param_filtered);
+  /// @brief return whether NetState state meets NetStateRule rule
+  static bool StateMeetsRule(const NetState& state, const NetStateRule& rule,
+      const string& layer_name);
     
-        struct HISTORYITEMVECTOR
-    {
-        std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> spTokens;
-        std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> spCommands;
-        std::wstring expression;
-        std::wstring result;
-    };
+      // True iff a request to stop early was received.
+  bool requested_early_exit_;
     
-        CleanupDeviceD3D();
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    // Return the function that the solver can use to find out if a snapshot or
+// early exit is being requested.
+ActionCallback SignalHandler::GetActionFunction() {
+  return boost::bind(&SignalHandler::CheckForSignals, this);
+}
     
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+      const Dtype* cpu_data() const;
+  void set_cpu_data(Dtype* data);
+  const int* gpu_shape() const;
+  const Dtype* gpu_data() const;
+  void set_gpu_data(Dtype* data);
+  const Dtype* cpu_diff() const;
+  const Dtype* gpu_diff() const;
+  Dtype* mutable_cpu_data();
+  Dtype* mutable_gpu_data();
+  Dtype* mutable_cpu_diff();
+  Dtype* mutable_gpu_diff();
+  void Update();
+  void FromProto(const BlobProto& proto, bool reshape = true);
+  void ToProto(BlobProto* proto, bool write_diff = false) const;
     
-    // About OpenGL function loaders: modern OpenGL doesn't have a standard header file and requires individual function pointers to be loaded manually.
-// Helper libraries are often used for this purpose! Here we are supporting a few common ones: gl3w, glew, glad.
-// You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
-#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-#include <GL/gl3w.h>    // Initialize with gl3wInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-#include <GL/glew.h>    // Initialize with glewInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-#include <glad/glad.h>  // Initialize with gladLoadGL()
-#else
-#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+    
+    {  thr.join();
+}
+    
+      // Unsets `name` variable.
+  void Unset();
+    
+    TEST(GlobalConfigTest, StringTest) {
+  grpc_core::UniquePtr<char> value;
+    }
+    
+      SecureChannelCredentials* AsSecureCredentials() override { return nullptr; }
+    
+      request.set_message('You should be dead');
+  // This may succeed or fail depending on the state of the TCP connection
+  stream->Write(request);
+  // But the read will definitely fail
+  EXPECT_FALSE(stream->Read(&response));
+    
+    Status BuildTable(const std::string& dbname, Env* env, const Options& options,
+                  TableCache* table_cache, Iterator* iter, FileMetaData* meta) {
+  Status s;
+  meta->file_size = 0;
+  iter->SeekToFirst();
+    }
+    
+    namespace leveldb {
+    }
+    
+    namespace leveldb {
+    }
+    
+      // If *start < limit, changes *start to a short string in [start,limit).
+  // Simple comparator implementations may return with *start unchanged,
+  // i.e., an implementation of this method that does nothing is correct.
+  virtual void FindShortestSeparator(std::string* start,
+                                     const Slice& limit) const = 0;
+    
+    // Test for issue 178: a manual compaction causes deleted data to reappear.
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+    
+    #if defined(HAVE_VALGRIND) && !defined(HAVE_KQUEUE)
+#define USE_VALGRIND 1
 #endif
     
-        // Main loop
-    MSG msg;
-    ZeroMemory(&msg, sizeof(msg));
-    while (msg.message != WM_QUIT)
-    {
-        // Poll and handle messages (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        if (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-        {
-            ::TranslateMessage(&msg);
-            ::DispatchMessage(&msg);
-            continue;
-        }
-    }
+    #if defined(USE_VALGRIND)
+    valgrind_stack_id = VALGRIND_STACK_REGISTER(static_cast<char *>(stack_) + stack_size, stack_);
+#endif
     
-    IMGUI_IMPL_API bool     ImGui_Marmalade_Init(bool install_callbacks);
-IMGUI_IMPL_API void     ImGui_Marmalade_Shutdown();
-IMGUI_IMPL_API void     ImGui_Marmalade_NewFrame();
-IMGUI_IMPL_API void     ImGui_Marmalade_RenderDrawData(ImDrawData* draw_data);
-    
-    // Implemented features:
-//  [X] Platform: Clipboard support.
-//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad'.
-//  [x] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'. FIXME: 3 cursors types are missing from GLFW.
-//  [X] Platform: Keyboard arrays indexed using GLFW_KEY_* codes, e.g. ImGui::IsKeyPressed(GLFW_KEY_SPACE).
-    
-    // Main code
-int main(int, char**)
+    typedef struct
 {
-    // Create application window
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T('ImGui Example'), NULL };
-    ::RegisterClassEx(&wc);
-    HWND hwnd = ::CreateWindow(wc.lpszClassName, _T('Dear ImGui DirectX10 Example'), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
-    }
+    int fd;
+    int key;
+} swFdInfo;
     
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-        // Render command lists
-    for (int n = 0; n < draw_data->CmdListsCount; n++)
-    {
-        const ImDrawList* cmd_list = draw_data->CmdLists[n];
-        const ImDrawVert* vtx_buffer = cmd_list->VtxBuffer.Data;
-        const ImDrawIdx* idx_buffer = cmd_list->IdxBuffer.Data;
-        glVertexPointer(2, GL_FLOAT, sizeof(ImDrawVert), (const GLvoid*)((const char*)vtx_buffer + IM_OFFSETOF(ImDrawVert, pos)));
-        glTexCoordPointer(2, GL_FLOAT, sizeof(ImDrawVert), (const GLvoid*)((const char*)vtx_buffer + IM_OFFSETOF(ImDrawVert, uv)));
-        glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(ImDrawVert), (const GLvoid*)((const char*)vtx_buffer + IM_OFFSETOF(ImDrawVert, col)));
-    }
-    
-    TEST_F(DistanceApproachIPOPTInterfaceTest, get_bounds_info) {
-  int n = 1274;
-  int m = 2194;
-  double x_l[1274];
-  double x_u[1274];
-  double g_l[2194];
-  double g_u[2194];
-  bool res = ptop_->get_bounds_info(n, x_l, x_u, m, g_l, g_u);
-  EXPECT_TRUE(res);
+    void swoole_throw_error(int code)
+{
+    throw swoole::Exception(code);
 }
-    
-      ValetParkingContext* GetContext() {
-    return GetContextAs<ValetParkingContext>();
-  }
-    
-    
-    {
-    {}  // namespace planning
-}  // namespace apollo
 
     
-    Scenario::Scenario(const ScenarioConfig& config, const ScenarioContext* context)
-    : config_(config), scenario_context_(context) {
-  name_ = ScenarioConfig::ScenarioType_Name(config.scenario_type());
-}
+            if (pid == 0)
+        {
+            exit(0);
+        }
     
-    #include 'modules/planning/traffic_rules/rerouting.h'
-    
-      bool IsBlockingObstacleWithinDestination(
-      const ReferenceLineInfo& reference_line_info);
-    
-     private:
-  void WriteOutputJpgFile(const std::vector<uint8_t>& jpeg_buffer,
-                          const std::string& output_jpg_file) const;
-  std::string GetOutputFile(const int frame_num) const;
-    
-      EXPECT_EQ(0, PredictionThreadPool::s_thread_pool_level);
-    
-    Expected<std::string, DatabaseError> Database::getStringOr(
-    const std::string& domain,
-    const std::string& key,
-    const std::string& default_value) {
-  auto result = getString(domain, key);
-  if (!result && result.getError() == DatabaseError::KeyNotFound) {
-    return default_value;
-  }
-  return result;
-}
+        cache.set('test3', val3);
+    ASSERT_EQ(cache.get('test3').get(), val3.get());
+    val3.reset();
+    ASSERT_EQ(dtor_num, 3);
+    ASSERT_EQ(cache.get('test2'), nullptr);
