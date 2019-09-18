323f3624490fd9504a81735b500c1f7d3c0ea4d9
SUNsung <weatherius@gmail.com>
2019-09-18 05:24:54 +0300

    Rewrite [30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb] File in: 2019-09-18 05:24:54

--- ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
+++ ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
@@ -1,53 +1,82 @@
 
         
-            should 'not filter entries within include' do
-      includes = %w(_index.html .htaccess include*)
-      files = %w(index.html _index.html .htaccess includeA)
+          # DELETE /resource/sign_out
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
+  end
     
-    # Sample contents
-WITHOUT_LIQUID = <<-TEXT.freeze
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
-pharetra tempus. Etiam bibendum magna et metus fermentum, eu cursus lorem
-mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
+          # Forgets the given resource by deleting a cookie
+      def forget_me(resource)
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.forget_me!
+        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
+      end
     
-            # rubocop:disable Metrics/AbcSize
-        def process(args, opts)
-          if !args || args.empty?
-            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
-          end
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
+        end
     
-    require 'em-websocket'
+          def remember_me!
+        self.remember_token ||= self.class.remember_token if respond_to?(:remember_token)
+        self.remember_created_at ||= Time.now.utc
+        save(validate: false) if self.changed?
+      end
     
-    describe BottleSpecification do
-  specify '#sha256' do
-    checksums = {
-      snow_leopard_32: 'deadbeef' * 8,
-      snow_leopard:    'faceb00c' * 8,
-      lion:            'baadf00d' * 8,
-      mountain_lion:   '8badf00d' * 8,
-    }
+        # Creates an instance of CategoryIndex for each category page, renders it, and
+    # writes the output to a file.
+    #
+    #  +category_dir+ is the String path to the category folder.
+    #  +category+     is the category currently being processed.
+    def write_category_index(category_dir, category)
+      index = CategoryIndex.new(self, self.source, category_dir, category)
+      index.render(self.layouts, site_payload)
+      index.write(self.dest)
+      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
+      self.pages << index
     
-      it 'prints the file used to cache the Cask' do
-    transmission_location = CurlDownloadStrategy.new(
-      local_transmission.url.to_s, local_transmission.token, local_transmission.version,
-      cache: Cask::Cache.path, **local_transmission.url.specs
-    ).cached_location
-    caffeine_location = CurlDownloadStrategy.new(
-      local_caffeine.url.to_s, local_caffeine.token, local_caffeine.version,
-      cache: Cask::Cache.path, **local_caffeine.url.specs
-    ).cached_location
+    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
+
     
-        context 'with cookies set' do
-      let(:specs) {
-        {
-          cookies: {
-            coo: 'k/e',
-            mon: 'ster',
-          },
-        }
-      }
-    
-        def []=(name, value)
-      @cookies[name.to_s] = value
-      @send_cookies[name.to_s] = true
+      class RenderPartialTag < Liquid::Tag
+    include OctopressFilters
+    def initialize(tag_name, markup, tokens)
+      @file = nil
+      @raw = false
+      if markup =~ /^(\S+)\s?(\w+)?/
+        @file = $1.strip
+        @raw = $2 == 'raw'
+      end
+      super
     end
+    
+        def pane_index
+      index + tab.project.pane_base_index
+    end
+    
+            expect(project.startup_pane).to eq('sample:0.4')
+      end
+    end
+  end
+    
+        def list
+      say 'tmuxinator projects:'
+      if options[:newline]
+        say Tmuxinator::Config.configs.join('\n')
+      else
+        print_in_columns Tmuxinator::Config.configs
+      end
+    end
+    
+        context 'when a project config file is provided' do
+      it 'should raise if the project config file can't be found' do
+        project_config = 'dont-exist.yml'
+        regex = /Project config \(#{project_config}\) doesn't exist\./
+        expect do
+          described_class.validate(project_config: project_config)
+        end.to raise_error RuntimeError, regex
+      end
