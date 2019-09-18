37cce710ef71809bc4e4aa9e0f4c62c8d7fbdb0e
SUNsung <weatherius@gmail.com>
2019-09-18 10:52:20 +0300

    Rewrite [08TqtTl.4I0WI08WatVnzbKKFg08.peDLVj438Q08ECz.py] File in: 2019-09-18 10:52:20

--- pyton/08TqtTl.4I0WI08WatVnzbKKFg08.peDLVj438Q08ECz.py
+++ pyton/08TqtTl.4I0WI08WatVnzbKKFg08.peDLVj438Q08ECz.py
@@ -1,138 +1,110 @@
 
         
-                atexit.register(self._db.close)
+        import numpy as np
+import tensorflow as tf
     
-    import pytest
-from thefuck.shells import Generic
+      Args:
+    ground_truth: Usually the output from ReadDatasetFile(). List containing
+      ground-truth information for dataset. Each entry is a dict corresponding
+      to the ground-truth information for a query. The dict must have keys
+      'easy', 'hard', and 'junk', mapping to a NumPy array of integers.
     
+    from object_detection.core import matcher
+from object_detection.utils import shape_utils
     
-@for_app('pyenv')
-def get_new_command(command):
-    broken = re.findall(r'pyenv: no such command `([^']*)'', command.output)[0]
-    matched = [replace_argument(command.script, broken, common_typo)
-               for common_typo in COMMON_TYPOS.get(broken, [])]
-    matched.extend(replace_command(command, broken, get_pyenv_commands()))
-    return matched
-
-    
-    (c) 2016, Aaron Christianson
-http://github.com/ninjaaron/fast-entry_points
-'''
-from setuptools.command import easy_install
-import re
-TEMPLATE = r'''\
-# -*- coding: utf-8 -*-
-# EASY-INSTALL-ENTRY-SCRIPT: '{3}','{4}','{5}'
-__requires__ = '{3}'
-import re
-import sys
+      img_names = tf.gfile.Glob(os.path.join(dataset_dir, '*.jpg'))
+  random.shuffle(img_names)
+  seg_names = []
+  for f in img_names:
+    # get the filename without the extension
+    basename = os.path.basename(f).split('.')[0]
+    # cover its corresponding *_seg.png
+    seg = os.path.join(dataset_label_dir, basename+'.png')
+    seg_names.append(seg)
     
     
-class FilePathField(ChoiceField):
-    def __init__(self, path, *, match=None, recursive=False, allow_files=True,
-                 allow_folders=False, **kwargs):
-        self.path, self.match, self.recursive = path, match, recursive
-        self.allow_files, self.allow_folders = allow_files, allow_folders
-        super().__init__(choices=(), **kwargs)
+class RedBullTVRrnContentIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?redbull(?:\.tv|\.com(?:/[^/]+)?(?:/tv)?)/(?:video|live)/rrn:content:[^:]+:(?P<id>[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12})'
+    _TESTS = [{
+        'url': 'https://www.redbull.com/int-en/tv/video/rrn:content:live-videos:e3e6feb4-e95f-50b7-962a-c70f8fd13c73/mens-dh-finals-fort-william',
+        'only_matching': True,
+    }, {
+        'url': 'https://www.redbull.com/int-en/tv/video/rrn:content:videos:a36a0f36-ff1b-5db8-a69d-ee11a14bf48b/tn-ts-style?playlist=rrn:content:event-profiles:83f05926-5de8-5389-b5e4-9bb312d715e8:extras',
+        'only_matching': True,
+    }]
+    
+            episode_number = int_or_none(video_data.get('episode_number'))
+        series = None
+        season = None
+        season_id = None
+        season_number = None
+        for metadata in video_data.get('__children', {}).get('Item', {}).values():
+            if metadata.get('type') == 'show':
+                series = metadata.get('title')
+            elif metadata.get('type') == 'season':
+                season = metadata.get('title')
+                season_id = metadata.get('id')
+                season_number = int_or_none(metadata.get('season_number'))
+    
+    FISH_COMPLETION_FILE = 'youtube-dl.fish'
+FISH_COMPLETION_TEMPLATE = 'devscripts/fish-completion.in'
     
     
-site.register(TitleCollection, inlines=[TitleInline])
-# Test bug #12561 and #12778
-# only ModelAdmin media
-site.register(Holder, HolderAdmin, inlines=[InnerInline])
-# ModelAdmin and Inline media
-site.register(Holder2, HolderAdmin, inlines=[InnerInline2, InnerInline2Tabular])
-# only Inline media
-site.register(Holder3, inlines=[InnerInline3])
+def openssl_encode(algo, key, iv):
+    cmd = ['openssl', 'enc', '-e', '-' + algo, '-K', hex_str(key), '-iv', hex_str(iv)]
+    prog = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, _ = prog.communicate(secret_msg)
+    return out
     
-        class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-        abstract = True
+        def test_youtube(self):
+        self._assert_restricted('07FYdnEawAQ', '07FYdnEawAQ.mp4', 10)
     
-        def _resolve_output_field(self):
-        source_fields = self.get_source_fields()
-        if any(isinstance(s, DecimalField) for s in source_fields):
-            return DecimalField()
-        if any(isinstance(s, IntegerField) for s in source_fields):
-            return FloatField()
-        return super()._resolve_output_field() if source_fields else FloatField()
-
+            self.check_tokenize('class async:pass', '''\
+    NAME       'class'       (1, 0) (1, 5)
+    NAME       'async'       (1, 6) (1, 11)
+    OP         ':'           (1, 11) (1, 12)
+    NAME       'pass'        (1, 12) (1, 16)
+    ''')
     
-    try:
-    from django.contrib.gis.db.backends.postgis.operations import PostGISOperations
-    HAS_POSTGRES = True
-except ImportError:
-    HAS_POSTGRES = False
+            def log_message(self, msg, *args):
+            log.info(msg, *args)
     
-        def test_gist_parameters(self):
-        index_name = 'integer_array_gist_buffering'
-        index = GistIndex(fields=['field'], name=index_name, buffering=True, fillfactor=80)
-        with connection.schema_editor() as editor:
-            editor.add_index(CharFieldModel, index)
-        constraints = self.get_constraints(CharFieldModel._meta.db_table)
-        self.assertEqual(constraints[index_name]['type'], GistIndex.suffix)
-        self.assertEqual(constraints[index_name]['options'], ['buffering=on', 'fillfactor=80'])
-        with connection.schema_editor() as editor:
-            editor.remove_index(CharFieldModel, index)
-        self.assertNotIn(index_name, self.get_constraints(CharFieldModel._meta.db_table))
+    _WIN32_CLIENT_RELEASES = {
+    (5, 0): '2000',
+    (5, 1): 'XP',
+    # Strictly, 5.2 client is XP 64-bit, but platform.py historically
+    # has always called it 2003 Server
+    (5, 2): '2003Server',
+    (5, None): 'post2003',
+    }
     
-        def as_datetime(self):
-        'Retrieve the Field's value as a tuple of date & time components.'
-        if not self.is_set:
-            return None
-        yy, mm, dd, hh, mn, ss, tz = [c_int() for i in range(7)]
-        status = capi.get_field_as_datetime(
-            self._feat.ptr, self._index, byref(yy), byref(mm), byref(dd),
-            byref(hh), byref(mn), byref(ss), byref(tz))
-        if status:
-            return (yy, mm, dd, hh, mn, ss, tz)
-        else:
-            raise GDALException('Unable to retrieve date & time information from the field.')
-    
-        if isinstance(password, str):
-        password = password.encode('latin1')
+        # Pre-allocate to ensure that the loop doesn't allocate anything new
+    rep_range = list(range(repcount))
+    rc_deltas = [0] * repcount
+    alloc_deltas = [0] * repcount
+    fd_deltas = [0] * repcount
+    getallocatedblocks = sys.getallocatedblocks
+    gettotalrefcount = sys.gettotalrefcount
+    fd_count = support.fd_count
     
         def initialize_options(self):
-        TestCommand.initialize_options(self)
-        try:
-            from multiprocessing import cpu_count
-            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
-        except (ImportError, NotImplementedError):
-            self.pytest_args = ['-n', '1', '--boxed']
+        self.build_base = 'build'
+        # these are decided only after 'build_base' has its final value
+        # (unless overridden by the user or client)
+        self.build_purelib = None
+        self.build_platlib = None
+        self.build_lib = None
+        self.build_temp = None
+        self.build_scripts = None
+        self.compiler = None
+        self.plat_name = None
+        self.debug = None
+        self.force = 0
+        self.executable = None
+        self.parallel = None
     
-        def test_nonhttp_schemes_dont_check_URLs(self):
-        test_urls = (
-            'data:image/gif;base64,R0lGODlhAQABAHAAACH5BAUAAAAALAAAAAABAAEAAAICRAEAOw==',
-            'file:///etc/passwd',
-            'magnet:?xt=urn:btih:be08f00302bc2d1d3cfa3af02024fa647a271431',
-        )
-        for test_url in test_urls:
-            req = requests.Request('GET', test_url)
-            preq = req.prepare()
-            assert test_url == preq.url
     
-    if is_py2:
-    from urllib import (
-        quote, unquote, quote_plus, unquote_plus, urlencode, getproxies,
-        proxy_bypass, proxy_bypass_environment, getproxies_environment)
-    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urldefrag
-    from urllib2 import parse_http_list
-    import cookielib
-    from Cookie import Morsel
-    from StringIO import StringIO
-    from collections import Callable, Mapping, MutableMapping, OrderedDict
+def get_top_states(t_state_v, K=4):
+    return sorted(t_state_v, key=t_state_v.__getitem__, reverse=True)[:K]
     
-            # it had actual content (not checking for SOCKS protocol for now)
-        assert len(fake_proxy.handler_results[0]) > 0
-    
-        def test_valid(self):
-        assert address_in_network('192.168.1.1', '192.168.1.0/24')
-    
-            #: The amount of time elapsed between sending the request
-        #: and the arrival of the response (as a timedelta).
-        #: This property specifically measures the time taken between sending
-        #: the first byte of the request and finishing parsing the headers. It
-        #: is therefore unaffected by consuming the response content or the
-        #: value of the ``stream`` keyword argument.
-        self.elapsed = datetime.timedelta(0)
+    file_name = args[0]
