f54103ff5fb8e679481575775953072f48d58954
SUNsung <weatherius@gmail.com>
2019-09-19 02:16:59 +0300

    Rewrite [057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp] File in: 2019-09-19 02:16:59

--- cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
+++ cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
@@ -1,333 +1,140 @@
 
         
-            void operator() (const typename internal::VecTraits<T>::vec128 & v_src0,
-                     const typename internal::VecTraits<T>::vec128 & v_src1,
-                     typename internal::VecTraits<T>::vec128 & v_dst) const
-    {
-        typename internal::VecTraits<T>::vec128 v_min = internal::vminq(v_src0, v_src1);
-        typename internal::VecTraits<T>::vec128 v_max = internal::vmaxq(v_src0, v_src1);
-        v_dst = internal::vqsubq(v_max, v_min);
-    }
+        #endif // BITCOIN_REVERSELOCK_H
+
     
-    #ifdef CAROTENE_NEON
-    // this ugly contruction is needed to avoid:
-    // /usr/lib/gcc/arm-linux-gnueabihf/4.8/include/arm_neon.h:3581:59: error: argument must be a constant
-    // return (int16x8_t)__builtin_neon_vshr_nv8hi (__a, __b, 1);
+    #endif /* SECP256K1_MODULE_RECOVERY_MAIN_H */
+
     
-        void operator() (const typename VecTraits<s32>::vec128 & v_src0,
-                     const typename VecTraits<s32>::vec128 & v_src1,
-                     typename VecTraits<s32>::vec128 & v_dst) const
-    {
-        float32x4_t vs1 = vcvtq_f32_s32(v_src0);
-        float32x4_t vs2 = vcvtq_f32_s32(v_src1);
-    }
-    
-    #define VROW_LINE(type, n) type * dst##n = internal::getRowPtr(dst##n##Base, dst##n##Stride, i);
-#define VST1Q_LINE(type, n) vst1q_##type(dst##n + dj, v_src.val[n]);
-#define VST1_LINE(type, n) vst1_##type(dst##n + dj, v_src.val[n]);
-#define SST_LINE(type, n) dst##n[dj] = src[sj + n];
-    
-    #define MERGE_QUAD(sgn, bits, n) { \
-                                     FILL_LINES##n(PREF, sgn##bits) \
-                                     MERGE_ASM##n(sgn, bits) \
-                                 }
-    
-    namespace CAROTENE_NS {
-    }
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(s8, f32, 16,
-,
-{
-     for (size_t i = 0; i < w; i += 16)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.8 {d0-d1}, [%[src]]                              \n\t'
-             'vmovl.s8 q1, d0                                       \n\t'
-             'vmovl.s8 q2, d1                                       \n\t'
-             'vmovl.s16 q3, d2                                      \n\t'
-             'vmovl.s16 q4, d3                                      \n\t'
-             'vmovl.s16 q5, d4                                      \n\t'
-             'vmovl.s16 q6, d5                                      \n\t'
-             'vcvt.f32.s32 q7, q3                                   \n\t'
-             'vcvt.f32.s32 q8, q4                                   \n\t'
-             'vcvt.f32.s32 q9, q5                                   \n\t'
-             'vcvt.f32.s32 q10, q6                                  \n\t'
-             'vst1.32 {d14-d15}, [%[dst1]]                          \n\t'
-             'vst1.32 {d16-d17}, [%[dst2]]                          \n\t'
-             'vst1.32 {d18-d19}, [%[dst3]]                          \n\t'
-             'vst1.32 {d20-d21}, [%[dst4]]                          \n\t'
-             : /*no output*/
-             : [src] 'r' (_src + i),
-               [dst1] 'r' (_dst + i + 0),
-               [dst2] 'r' (_dst + i + 4),
-               [dst3] 'r' (_dst + i + 8),
-               [dst4] 'r' (_dst + i + 12)
-             : 'd0','d1','d2','d3','d4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21'
-         );
-     }
-})
-#else
-CVT_FUNC(s8, f32, 16,
-,
-{
-     for (size_t i = 0; i < w; i += 16)
-     {
-         internal::prefetch(_src + i);
-         int8x16_t vline_s8 = vld1q_s8(_src + i);
+    // static
+void App::Quit(RenderProcessHost* render_process_host) {
+  // Send the quit message.
+  int no_use;
+  if (render_process_host) {
+    render_process_host->Send(new ViewMsg_WillQuit(&no_use));
+  }else{
+    std::set<RenderProcessHost*> rphs;
+    std::set<RenderProcessHost*>::iterator it;
     }
     }
     
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(u16, s8, 16,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.8 {d4-d5}, [%[src1]]                             \n\t'
-            'vmovl.u16 q3, d4                                      \n\t'
-            'vmovl.u16 q4, d5                                      \n\t'
-            'vcvt.f32.u32 q5, q3                                   \n\t'
-            'vcvt.f32.u32 q6, q4                                   \n\t'
-            'vmul.f32 q7, q5, q0                                   \n\t'
-            'vmul.f32 q8, q6, q0                                   \n\t'
-            'vadd.f32 q9, q7, q1                                   \n\t'
-            'vadd.f32 q10, q8, q1                                  \n\t'
-            'vcvt.s32.f32 q11, q9                                  \n\t'
-            'vcvt.s32.f32 q12, q10                                 \n\t'
-            'vqmovn.s32 d26, q11                                   \n\t'
-            'vqmovn.s32 d27, q12                                   \n\t'
-            'vqmovn.s16 d28, q13                                   \n\t'
-            'vst1.8 {d28}, [%[dst]]                                \n\t'
-            : /*no output*/
-            : [src1] 'r' (_src + i),
-              [dst] 'r' (_dst + i + 0),
-               'w'  (vscale), 'w' (vshift)
-            : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25','d26','d27','d28'
-        );
-    }
-})
-#else
-CVTS_FUNC(u16, s8, 16,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        uint16x8_t vline = vld1q_u16(_src + i);
-        uint32x4_t vline1_u32 = vmovl_u16(vget_low_u16 (vline));
-        uint32x4_t vline2_u32 = vmovl_u16(vget_high_u16(vline));
-        float32x4_t vline1_f32 = vcvtq_f32_u32(vline1_u32);
-        float32x4_t vline2_f32 = vcvtq_f32_u32(vline2_u32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        int16x4_t vRes1 = vqmovn_s32(vline1_s32);
-        int16x4_t vRes2 = vqmovn_s32(vline2_s32);
-        int8x8_t vRes = vqmovn_s16(vcombine_s16(vRes1, vRes2));
-        vst1_s8(_dst + i, vRes);
-    }
-})
-#endif
+    Base::Base(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+	   const std::string& extension_id)
+    : extension_id_(extension_id),
+      id_(id),
+      delay_destruction_(false),
+      pending_destruction_(false),
+      object_manager_(object_manager) {
+}
     
-                // make extrapolation for the first elements
-            if (!x)
-            {
-                // make border
-                if (border == BORDER_MODE_CONSTANT)
-                    tcurr = v_border_x4;
-                else if (border == BORDER_MODE_REPLICATE)
-                    tcurr = vdupq_n_u16(vgetq_lane_u16(tnext, 0));
+      bool delay_destruction() { return delay_destruction_; }
+  void set_delay_destruction(bool val) { delay_destruction_ = val; }
+  bool pending_destruction() { return pending_destruction_; }
+  void set_pending_destruction (bool val) { pending_destruction_ = val; }
+ protected:
+  int id_;
+  bool delay_destruction_;
+  bool pending_destruction_;
+  base::WeakPtr<ObjectManager> object_manager_;
+    
+    v8::Handle<v8::Value> AllocateId(int routing_id) {
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::EscapableHandleScope scope(isolate);
     }
     
+       void Call(const std::string& method,
+                    const base::ListValue& arguments) override;
+   void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) override;
     
-    {                // make border
-                    if (border == BORDER_MODE_REPLICATE || border == BORDER_MODE_REFLECT)
-                    {
-                        tcurr = vsetq_lane_s16(vgetq_lane_s16(tcurr, 0),tcurr, 7);
-                    }
-                    else if (border == BORDER_MODE_CONSTANT)
-                    {
-                        tcurr = vsetq_lane_s16(borderValue, tcurr, 7);
-                    }
-                    else if (border == BORDER_MODE_REFLECT101)
-                    {
-                        tcurr = vsetq_lane_s16(vgetq_lane_s16(tcurr, 1),tcurr, 7);
-                    }
-                continue;
-            }
+    Menu::~Menu() {
+  Destroy();
+}
     
-    
-    {
-    {    return SingleGradientDef(
-        'MergeSingleListFeatureTensorsGradient',
-        '',
-        input_blob_names,
-        output_blob_names);
-  }
+    static KeyMap keymap = {
+  {'`'    , 'Backquote'},
+  {'\\'   , 'Backslash'},
+  {'['    , 'BracketLeft'},
+  {']'    , 'BracketRight'},
+  {','    , 'Comma'},
+  {'='    , 'Equal'},
+  {'-'    , 'Minus'},
+  {'.'    , 'Period'},
+  {'''    , 'Quote'},
+  {';'    , 'Semicolon'},
+  {'/'    , 'Slash'},
+  {'\n'   , 'Enter'},
+  {'\t'   , 'Tab'},
+  {'UP'   , 'ArrowUp'},
+  {'DOWN' , 'ArrowDown'},
+  {'LEFT' , 'ArrowLeft'},
+  {'RIGHT', 'ArrowRight'},
+  {'ESC'  , 'Escape'},
+  {'MEDIANEXTTRACK', 'MediaTrackNext'},
+  {'MEDIAPREVTRACK', 'MediaTrackPrevious'}
 };
     
-              const int dkernel_h = dilation_h * (kernel_h - 1) + 1;
-          const int dkernel_w = dilation_w * (kernel_w - 1) + 1;
-          CAFFE_ENFORCE(H >= dkernel_h);
-          CAFFE_ENFORCE(W >= dkernel_w);
-          const int out_h = (H + 2 * pad - dkernel_h) / stride_h + 1;
-          const int out_w = (W + 2 * pad - dkernel_w) / stride_w + 1;
-    
-    namespace caffe2 {
+        void Reset() {
+      scw_->Reset();
     }
     
-    #include <caffe/layer.hpp>
-#include <caffe/blob.hpp>
-#include <caffe/layer_factory.hpp>
     
-    namespace mxnet {
-namespace io {
-/*!
- * \brief OpenCV based Image augmenter,
- *  The augmenter can contain internal temp state.
- */
-class ImageAugmenter {
- public:
-  /*!
-   *  \brief Initialize the Operator by setting the parameters
-   *  This function need to be called before all other functions.
-   *  \param kwargs the keyword arguments parameters
-   */
-  virtual void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) = 0;
-  /*!
-   * \brief augment src image.
-   *   this function is not thread safe, and will only be called by one thread
-   *   however, it will tries to re-use memory space as much as possible
-   * \param src the source image
-   * \param prnd pointer to random number generator.
-   * \return The processed image.
-   */
-  virtual cv::Mat Process(const cv::Mat &src, std::vector<float> *label,
-                          common::RANDOM_ENGINE *prnd) = 0;
-  // virtual destructor
-  virtual ~ImageAugmenter() {}
-  /*!
-   * \brief factory function
-   * \param name Name of the augmenter
-   * \return The created augmenter.
-   */
-  static ImageAugmenter* Create(const std::string& name);
+    {  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
+  manager->OnCallObjectMethod(render_frame_host(), id, type, method, *arguments);
+  return RespondNow(NoArguments());
+}
+    
+    
+    {}  // namespace caffe
+
+    
+    namespace caffe {
+    }
+    
+    namespace caffe {
+    }
+    
+    
+    {  CHECK(reinterpret_cast<float*>(input_channels->at(0).data)
+        == net_->input_blobs()[0]->cpu_data())
+    << 'Input channels are not wrapping the input layer of the network.';
+}
+    
+    
+    {  int kernel_h_, kernel_w_;
+  int stride_h_, stride_w_;
+  int pad_h_, pad_w_;
+  int channels_;
+  int height_, width_;
+  int pooled_height_, pooled_width_;
+  bool global_pooling_;
+  PoolingParameter_RoundMode round_mode_;
+  Blob<Dtype> rand_idx_;
+  Blob<int> max_idx_;
 };
-    }
-    }
     
+    #include 'glog/logging.h'
+#include 'google/protobuf/text_format.h'
+#include 'stdint.h'
     
+    #endif // OPENPOSE_TRACKING_PERSON_ID_EXTRACTOR_HPP
+
+    
+        template<typename T>
+    Rectangle<T>::Rectangle(const Rectangle<T>& rectangle)
     {
-    {inline void Dequantize2BitImpl(mshadow::Stream<mshadow::cpu> *s,
-                               const std::vector<mxnet::TBlob> &inputs,
-                               const float threshold) {
-  Dequantize2BitKernelLaunch(s, inputs, threshold);
-}
-}  // namespace kvstore
-}  // namespace mxnet
-    
-    template <>
-void EvalBroadcast<DEVICE>(TBlob const& src, TBlob* ret, int size, RunContext ctx) {
-  typedef DEVICE xpu;
-  mshadow::Stream<xpu>* s = ctx.get_stream<xpu>();
-  mshadow::Tensor<xpu, 3> out = ret->get<xpu, 3, real_t>(s);
-  mshadow::Tensor<xpu, 2> in = src.get<xpu, 2, real_t>(s);
-  out = mshadow::expr::broadcast_with_axis(in, 0, size);
-}
-    
-     private:
-  inline void Init(mshadow::Stream<gpu> *s,
-                   const std::vector<TBlob> &in_data,
-                   const std::vector<TBlob> &out_data) {
-    using namespace mshadow;
-    #if CUDNN_MAJOR >= 5
-    format_ = CUDNN_TENSOR_NCHW;
-    #endif
-    CHECK_EQ(in_data.size(), 2U);
-    CHECK_EQ(out_data.size(), 3U);
-    if (!init_cudnn_) {
-      init_cudnn_ = true;
-      Tensor<gpu, 4, DType> data = in_data[st::kData].get<gpu, 4, DType>(s);
-      Tensor<gpu, 4, DType> out = out_data[st::kOut].get<gpu, 4, DType>(s);
-      CUDNN_CALL(cudnnCreateSpatialTransformerDescriptor(&st_desc_));
-      CUDNN_CALL(cudnnCreateTensorDescriptor(&in_desc_));
-      CUDNN_CALL(cudnnCreateTensorDescriptor(&out_desc_));
-      CUDNN_CALL(cudnnSetTensor4dDescriptor(in_desc_,
-                                            format_,
-                                            dtype_,
-                                            data.size(0),
-                                            data.size(1),
-                                            data.size(2),
-                                            data.size(3)));
-      CUDNN_CALL(cudnnSetTensor4dDescriptor(out_desc_,
-                                            format_,
-                                            dtype_,
-                                            out.size(0),
-                                            out.size(1),
-                                            out.size(2),
-                                            out.size(3)));
-      if (param_.sampler_type == st::kBilinear) {
-        int dim[] = {static_cast<int>(out.size(0)), static_cast<int>(out.size(1)),
-                     static_cast<int>(out.size(2)), static_cast<int>(out.size(3))};
-        CUDNN_CALL(cudnnSetSpatialTransformerNdDescriptor(st_desc_,
-                                                          sampler_,
-                                                          dtype_,
-                                                          4,
-                                                          dim));
-      }
+        try
+        {
+            x = rectangle.x;
+            y = rectangle.y;
+            width = rectangle.width;
+            height = rectangle.height;
+        }
+        catch (const std::exception& e)
+        {
+            error(e.what(), __LINE__, __FUNCTION__, __FILE__);
+        }
     }
-  }
-    
-      void onTimeout(const std::shared_ptr<DHTNode>& node);
-    
-    #include 'DHTNode.h'
-#include 'DlAbortEx.h'
-#include 'DHTConstants.h'
-#include 'bittorrent_helper.h'
-#include 'Logger.h'
-#include 'a2netcompat.h'
-#include 'util.h'
-#include 'TimeA2.h'
-#include 'fmt.h'
-#include 'File.h'
-#include 'LogFactory.h'
-#include 'BufferedFile.h'
-    
-    namespace aria2 {
-    }
-    
-    namespace aria2 {
-    }
-    
-      virtual ~DHTTaskQueueImpl();
-    
-    void DHTTokenUpdateCommand::process()
-{
-  try {
-    tokenTracker_->updateTokenSecret();
-  }
-  catch (RecoverableException& e) {
-    A2_LOG_ERROR_EX(EX_EXCEPTION_CAUGHT, e);
-  }
-}
-    
-    #include <memory>
-    
-      // always return false
-  virtual bool isReply() const CXX11_OVERRIDE;
-    
-        // create values of different integer types
-    short n_short = 42;
-    int n_int = -23;
-    long n_long = 1024;
-    int_least32_t n_int_least32_t = -17;
-    uint8_t n_uint8_t = 8;
