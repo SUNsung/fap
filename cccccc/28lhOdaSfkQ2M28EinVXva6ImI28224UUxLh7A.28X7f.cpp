4e1d029c0857354b5e1fd6f544d5c16b64932eef
SUNsung <weatherius@gmail.com>
2019-09-18 23:23:19 +0300

    Rewrite [28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp] File in: 2019-09-18 23:23:19

--- cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
+++ cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
@@ -1,239 +1,185 @@
 
         
-        #include 'KeyChord.g.cpp'
+        #include 'pch.h'
+#include 'KeyChord.h'
     
-            // Write received text to the Console
-        // Note: Write to the Console using WriteFile(hConsole...), not printf()/puts() to
-        // prevent partially-read VT sequences from corrupting output
-        WriteFile(hConsole, szBuffer, dwBytesRead, &dwBytesWritten, NULL);
+        /// True if this is a witness method, false if it's a vtable method.
+    bool isWitnessMethod;
     
-        QString getDataDirectory();
-    void setDataDirectory(const QString &dataDir);
+    SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_API
+uint64_t swift_float64ToString(char *Buffer, size_t BufferLength,
+                               double Value, bool Debug) {
+  return swift_format_double(Value, Buffer, BufferLength);
+}
+    
+    #ifndef SWIFT_AST_ASTMANGLER_H
+#define SWIFT_AST_ASTMANGLER_H
+    
+      bool isPrimary() const { return IsPrimary; }
+  llvm::MemoryBuffer *buffer() const { return Buffer; }
+  const std::string &file() const {
+    assert(!Filename.empty());
+    return Filename;
+  }
+    
+    void MapLiteTestUtil::SetArenaMapFields(unittest::TestArenaMapLite* message) {
+  MapTestUtilImpl::SetArenaMapFields<unittest::MapEnumLite,
+                                     unittest::MAP_ENUM_BAR_LITE,
+                                     unittest::MAP_ENUM_BAZ_LITE>(message);
+}
+    
+      // Deletes the current owned object, if any.
+  // Then takes ownership of a new object without incrementing the reference
+  // count.
+  // This function must be called with a reference that you own.
+  //   this->reset(this->get()) is wrong!
+  //   this->reset(this->release()) is OK.
+  PyObjectStruct* reset(PyObjectStruct* p = NULL) {
+    Py_XDECREF(ptr_);
+    ptr_ = p;
+    return ptr_;
+  }
     
     
+    {  RenderThread::Get()->Send(new ShellViewHostMsg_Allocate_Object(
+      routing_id,
+      object_id,
+      type,
+      *static_cast<base::DictionaryValue*>(value_option.get())));
+  return v8::Undefined(isolate);
+}
     
-    #endif // BITCOIN_CRYPTO_RIPEMD160_H
+     private:
+  void SetText(std::string& text);
+  std::string GetText();
+  void Clear();
+    
+    EventListener::EventListener(int id,
+  const base::WeakPtr<DispatcherHost>& dispatcher_host,
+  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    }
+    
+    
+    {  int menu_id;
+  if (option.GetInteger('submenu', &menu_id))
+    SetSubmenu(object_manager()->GetApiObject<Menu>(menu_id));
+}
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.setListSync', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwClipboardSetListSyncFunction);
+};
+    
+    
+    {    Data() : initialized(false) {}
+  };
+    
+    DHTReplaceNodeTask::DHTReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
+                                       const std::shared_ptr<DHTNode>& newNode)
+    : bucket_(bucket),
+      newNode_(newNode),
+      numRetry_(0),
+      timeout_(DHT_MESSAGE_TIMEOUT)
+{
+}
+    
+    class DHTNode;
+class DHTBucket;
+class DHTTaskQueue;
+class DHTTaskFactory;
+class DHTBucketTreeNode;
+    
+      uint32_t temp32;
+  uint64_t temp64;
+  // time
+  if (version == 2) {
+    READ_CHECK(fp, &temp32, sizeof(temp32));
+    serializedTime_.setTimeFromEpoch(ntohl(temp32));
+    // 4bytes reserved
+    readBytes(fp, buf, buf.size(), 4);
+  }
+  else {
+    READ_CHECK(fp, &temp64, sizeof(temp64));
+    serializedTime_.setTimeFromEpoch(ntoh64(temp64));
+  }
+    
+    #endif // D_DHT_ROUTING_TABLE_SERIALIZER_H
 
     
     
-    {}  // namespace config
+    {  virtual bool finished() = 0;
+};
     
-    #include <stdio.h>
-#include 'db/dbformat.h'
-#include 'db/filename.h'
-#include 'db/log_reader.h'
-#include 'db/version_edit.h'
-#include 'db/write_batch_internal.h'
-#include 'leveldb/env.h'
-#include 'leveldb/iterator.h'
-#include 'leveldb/options.h'
-#include 'leveldb/status.h'
-#include 'leveldb/table.h'
-#include 'leveldb/write_batch.h'
-#include 'util/logging.h'
+      void update();
     
-        void operator()(const changefeed_stamp_t &s) {
-        response->response = do_stamp(s, s.region, s.region.inner.left);
+    #include 'common.h'
+    
+    namespace aria2 {
     }
     
-        void protocol_write(const write_t &write,
-                        write_response_t *response,
-                        state_timestamp_t timestamp,
-                        scoped_ptr_t<real_superblock_t> *superblock,
-                        signal_t *interruptor);
+    DNSCache::CacheEntry::~CacheEntry() = default;
     
+    const std::map<std::int32_t, std::string> kDriveLetters{
+    {0, 'A:\\'},  {1, 'B:\\'},  {2, 'C:\\'},  {3, 'D:\\'},  {4, 'E:\\'},
+    {5, 'F:\\'},  {6, 'G:\\'},  {7, 'H:\\'},  {8, 'I:\\'},  {9, 'J:\\'},
+    {10, 'K:\\'}, {11, 'L:\\'}, {12, 'M:\\'}, {13, 'N:\\'}, {14, 'O:\\'},
+    {15, 'P:\\'}, {16, 'Q:\\'}, {17, 'R:\\'}, {18, 'S:\\'}, {19, 'T:\\'},
+    {20, 'U:\\'}, {21, 'V:\\'}, {22, 'W:\\'}, {23, 'X:\\'}, {24, 'Y:\\'},
+    {25, 'Z:\\'},
+};
     
-    {
-    {                rdb_live_deletion_context_t deletion_context;
-                rdb_set(key, data, true, store.get_sindex_slice(sindex_uuid),
-                    repli_timestamp_t::distant_past,
-                    sindex_super_block.get(), &deletion_context, &response,
-                    &mod_info, static_cast<profile::trace_t *>(nullptr));
-            }
-            txn->commit();
+        try {
+      for (const auto& processes : container.get_child('Processes')) {
+        std::vector<std::string> vector;
+        for (const auto& v : processes.second) {
+          vector.push_back(v.second.data());
         }
-    
-    #if GTEST_OS_SYMBIAN
-  // Streams a value (either a pointer or not) to this object.
-  template <typename T>
-  inline Message& operator <<(const T& value) {
-    StreamHelper(typename internal::is_pointer<T>::type(), value);
-    return *this;
-  }
-#else
-  // Streams a non-pointer value to this object.
-  template <typename T>
-  inline Message& operator <<(const T& val) {
-    // Some libraries overload << for STL containers.  These
-    // overloads are defined in the global namespace instead of ::std.
-    //
-    // C++'s symbol lookup rule (i.e. Koenig lookup) says that these
-    // overloads are visible in either the std namespace or the global
-    // namespace, but not other namespaces, including the testing
-    // namespace which Google Test's Message class is in.
-    //
-    // To allow STL containers (and other types that has a << operator
-    // defined in the global namespace) to be used in Google Test
-    // assertions, testing::Message must access the custom << operator
-    // from the global namespace.  With this using declaration,
-    // overloads of << defined in the global namespace and those
-    // visible via Koenig lookup are both exposed in this function.
-    using ::operator <<;
-    *ss_ << val;
-    return *this;
-  }
-    
-    // With this overloaded version, we allow anonymous enums to be used
-// in {ASSERT|EXPECT}_EQ when compiled with gcc 4, as anonymous enums
-// can be implicitly cast to BiggestInt.
-GTEST_API_ AssertionResult CmpHelperEQ(const char* expected_expression,
-                                       const char* actual_expression,
-                                       BiggestInt expected,
-                                       BiggestInt actual);
-    
-    // In describing the results of death tests, these terms are used with
-// the corresponding definitions:
-//
-// exit status:  The integer exit information in the format specified
-//               by wait(2)
-// exit code:    The integer code passed to exit(3), _exit(2), or
-//               returned from main()
-class GTEST_API_ DeathTest {
- public:
-  // Create returns false if there was an error determining the
-  // appropriate action to take for the current death test; for example,
-  // if the gtest_death_test_style flag is set to an invalid value.
-  // The LastMessage method will return a more detailed message in that
-  // case.  Otherwise, the DeathTest pointer pointed to by the 'test'
-  // argument is set.  If the death test should be skipped, the pointer
-  // is set to NULL; otherwise, it is set to the address of a new concrete
-  // DeathTest object that controls the execution of the current test.
-  static bool Create(const char* statement, const RE* regex,
-                     const char* file, int line, DeathTest** test);
-  DeathTest();
-  virtual ~DeathTest() { }
+    }
     }
     
-    // A function to convert T* into linked_ptr<T>
-// Doing e.g. make_linked_ptr(new FooBarBaz<type>(arg)) is a shorter notation
-// for linked_ptr<FooBarBaz<type> >(new FooBarBaz<type>(arg))
-template <typename T>
-linked_ptr<T> make_linked_ptr(T* ptr) {
-  return linked_ptr<T>(ptr);
+    bool SpecificValuesCheck::operator()(const std::string& string) const {
+  return set_.find(string) != set_.end();
 }
     
-      template <typename T>
-  operator ParamGenerator<T>() const {
-    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
-        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
-        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
-        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
-        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
-        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
-        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
-        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_),
-        static_cast<T>(v24_), static_cast<T>(v25_), static_cast<T>(v26_),
-        static_cast<T>(v27_), static_cast<T>(v28_), static_cast<T>(v29_),
-        static_cast<T>(v30_), static_cast<T>(v31_), static_cast<T>(v32_),
-        static_cast<T>(v33_), static_cast<T>(v34_), static_cast<T>(v35_),
-        static_cast<T>(v36_), static_cast<T>(v37_), static_cast<T>(v38_),
-        static_cast<T>(v39_), static_cast<T>(v40_), static_cast<T>(v41_),
-        static_cast<T>(v42_)};
-    return ValuesIn(array);
+    void Initializer::initWorker(const std::string& name) const {
+  // Clear worker's arguments.
+  auto original_name = std::string((*argv_)[0]);
+  for (int i = 1; i < *argc_; i++) {
+    if ((*argv_)[i] != nullptr) {
+      memset((*argv_)[i], '\0', strlen((*argv_)[i]));
+    }
   }
-    
-      // Creates a UTF-16 wide string from the given ANSI string, allocating
-  // memory using new. The caller is responsible for deleting the return
-  // value using delete[]. Returns the wide string, or NULL if the
-  // input is NULL.
-  //
-  // The wide string is created using the ANSI codepage (CP_ACP) to
-  // match the behaviour of the ANSI versions of Win32 calls and the
-  // C runtime.
-  static LPCWSTR AnsiToUtf16(const char* c_str);
-    
-        ImGui_ImplDX9_Shutdown();
-    ImGui_ImplWin32_Shutdown();
-    ImGui::DestroyContext();
-    
-        // Upload texture to graphics system
-    g_FontTexture = NULL;
-    if (g_pd3dDevice->CreateTexture(width, height, 1, D3DUSAGE_DYNAMIC, D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &g_FontTexture, NULL) < 0)
-        return false;
-    D3DLOCKED_RECT tex_locked_rect;
-    if (g_FontTexture->LockRect(0, &tex_locked_rect, NULL, 0) != D3D_OK)
-        return false;
-    for (int y = 0; y < height; y++)
-        memcpy((unsigned char *)tex_locked_rect.pBits + tex_locked_rect.Pitch * y, pixels + (width * bytes_per_pixel) * y, (width * bytes_per_pixel));
-    g_FontTexture->UnlockRect(0);
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    IMGUI_IMPL_API bool     ImGui_Marmalade_Init(bool install_callbacks);
-IMGUI_IMPL_API void     ImGui_Marmalade_Shutdown();
-IMGUI_IMPL_API void     ImGui_Marmalade_NewFrame();
-IMGUI_IMPL_API void     ImGui_Marmalade_RenderDrawData(ImDrawData* draw_data);
-    
-        // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    #include <osquery/flags.h>
-#include <osquery/process/process.h>
-    
-    #ifdef WIN32
-/**
- * @brief Handles the resource lifetime of a PSECURITY_DESCRIPTOR
- *
- * Class to handle the scope of a PSECURITY_DESCRIPTOR from
- * GetSecurityInfo/GetNamedSecurityInfo class of functions (or any
- * PSECURITY_DESCRIPTOR pointer where the buffer is allocated via LocalAlloc)
- */
-class SecurityDescriptor {
- public:
-  explicit SecurityDescriptor(PSECURITY_DESCRIPTOR sd) : sd_(sd) {}
     }
     
-    // clang-format off
-#include <LM.h>
-// clang-format on
-    
-    #include <osquery/logger.h>
-    
-      /*
-   * @brief Helper function to POST a carve to the graph endpoint.
-   *
-   * Once all of the files have been carved and the tgz has been
-   * created, we POST the carved file to an endpoint specified by the
-   * carver_start_endpoint and carver_continue_endpoint
-   */
-  Status postCarve(const boost::filesystem::path& path);
-    
-      /*
-   * If the initial configuration includes a non-0 refresh, start an
-   * additional service that sleeps and periodically regenerates the
-   * configuration.
-   */
-  if (!FLAGS_config_check && !started_thread_ && getRefresh() > 0) {
-    Dispatcher::addService(refresh_runner_);
-    started_thread_ = true;
-  }
-    
-    TEST_F(ConfigTests, test_config_backup_integrate) {
-  const auto config_enable_backup_saved = FLAGS_config_enable_backup;
-  FLAGS_config_enable_backup = true;
+      do {
+    ret = NetLocalGroupEnum(nullptr,
+                            groupInfoLevel,
+                            (LPBYTE*)&lginfo,
+                            MAX_PREFERRED_LENGTH,
+                            &numGroupsRead,
+                            &totalGroups,
+                            nullptr);
     }
+    
+    typedef struct _PROCESS_BASIC_INFORMATION {
+  PVOID Reserved1;
+  PPEB PebBaseAddress;
+  PVOID Reserved2[2];
+  ULONG_PTR UniqueProcessId;
+  PVOID Reserved3;
+} PROCESS_BASIC_INFORMATION;
+    
+    #include <osquery/core.h>
+#include <osquery/tables.h>
+#include <osquery/logger.h>
+#include <osquery/sql.h>
     
     
     {} // namespace osquery
 
     
-    #include <osquery/core/database/database.h>
+    #pragma once
+    
+      using DataType = boost::variant<std::string, int32_t>;
+  using InMemoryStorageRef = std::unique_ptr<InMemoryStorage<DataType>>;
