ae1d27f9dfa616a28b54aa1774cf8fb148ecc17d
SUNsung <weatherius@gmail.com>
2019-09-18 04:42:39 +0300

    Rewrite [03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb] File in: 2019-09-18 04:42:38

--- ruby/03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb
+++ ruby/03VRWLGaeLiwk03mfLkvRnuyxs03KjY5lSaaDLE03XxC.rb
@@ -1,145 +1,68 @@
 
         
-            def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
-    end
+              screenshots = Dir.glob('#{path}/**/*.{png,PNG}').uniq # uniq because thanks to {png,PNG} there are duplicates
     
-        # Receives an object and find a scope for it. If a scope cannot be found,
-    # raises an error. If a symbol is given, it's considered to be the scope.
-    def self.find_scope!(obj)
-      obj = obj.devise_scope if obj.respond_to?(:devise_scope)
-      case obj
-      when String, Symbol
-        return obj.to_sym
-      when Class
-        Devise.mappings.each_value { |m| return m.name if obj <= m.to }
-      else
-        Devise.mappings.each_value { |m| return m.name if obj.is_a?(m.to) }
+    describe Spaceship::TestFlight::Build do
+  let(:mock_client) { double('MockClient') }
+    
+          it 'raises an error if something is wrong' do
+        TunesStubbing.itc_stub_broken_create
+        expect do
+          Spaceship::Tunes::Application.create!(name: 'My Name',
+                                                sku: 'SKU123',
+                                                bundle_id: 'net.sunapps.123')
+        end.to raise_error('You must choose a primary language. You must choose a primary language.')
       end
     
-          it 'does not create a block from sender to recipient' do
-        expect(sender.blocking?(recipient)).to be false
-      end
-    
-        it 'renders the headers' do
-      expect(mail.subject).to eq('You were mentioned by bob')
-      expect(mail.to).to eq([receiver.email])
-    end
-    
-        context 'when parent status is public' do
-      let(:visibility) { 'public' }
-    
-            it 'does not create anything' do
-          expect(sender.statuses.count).to eq 0
+            return return_val
+      rescue => ex
+        Dir.chdir(path_to_use) do
+          # Provide error block exception without color code
+          begin
+            error_blocks[current_platform].call(current_lane, ex, parameters) if current_platform && error_blocks[current_platform]
+            error_blocks[nil].call(current_lane, ex, parameters) if error_blocks[nil]
+          rescue => error_block_exception
+            UI.error('An error occurred while executing the `error` block:')
+            UI.error(error_block_exception.to_s)
+            raise ex # raise the original error message
+          end
         end
-      end
-    end
     
-        @save_to.each { |_, v| FileUtils.mkdir_p(v) }
+          self.project.project_paths.each do |project_path|
+        UI.verbose('Parsing project file '#{project_path}' to find selected provisioning profiles')
+        UI.verbose('Finding provision profiles for '#{specified_configuration}'') if specified_configuration
     
-      # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+    describe Deliver::SubmitForReview do
+  let(:review_submitter) { Deliver::SubmitForReview.new }
     
-          # This double assignment is to prevent an 'unused variable' warning on
-      # Ruby 1.9.3.  Yes, it is dumb, but I don't like Ruby yelling at me.
-      path = path = (req.script_name + req.path_info).squeeze('/')
-    
-    namespace :doc do
-  task :readmes do
-    Dir.glob 'lib/rack/protection/*.rb' do |file|
-      excluded_files = %w[lib/rack/protection/base.rb lib/rack/protection/version.rb]
-      next if excluded_files.include?(file)
-      doc  = File.read(file)[/^  module Protection(\n)+(    #[^\n]*\n)*/m].scan(/^ *#(?!#) ?(.*)\n/).join('\n')
-      file = 'doc/#{file[4..-4].tr('/_', '-')}.rdoc'
-      Dir.mkdir 'doc' unless File.directory? 'doc'
-      puts 'writing #{file}'
-      File.open(file, 'w') { |f| f << doc }
-    end
-  end
-    
-          def redirect(env)
-        request = Request.new(env)
-        warn env, 'attack prevented by #{self.class}'
-        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
+          def self.is_supported?(platform)
+        [:ios, :mac].include?(platform)
       end
     
-          expected_header = <<-END.chomp
-rack.%2573ession=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.%2573ession=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
-rack.session=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
-END
-      expect(last_response.headers['Set-Cookie']).to eq(expected_header)
-    end
+          it 'prefers an explicitly specified changelog value' do
+        values = Fastlane::FastFile.new.parse('lane :test do
+          # changelog_from_git_commits sets this lane context variable
+          Actions.lane_context[SharedValues::FL_CHANGELOG] = 'autogenerated changelog'
+          pilot(changelog: 'custom changelog')
+        end').runner.execute(:test)
+    
+          def is_create_page
+        false
+      end
+    
+          def has_path
+        !@path.nil?
+      end
+    
+      test 'extracting paths from URLs' do
+    assert_nil extract_path('Eye-Of-Sauron')
+    assert_equal 'Mordor', extract_path('Mordor/Sauron')
+    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
   end
     
-      # retrieve only the latest spec for all locally installed plugins
-  # @return [Hash] result hash {plugin_name.downcase => plugin_spec}
-  def find_latest_gem_specs
-    LogStash::PluginManager.all_installed_plugins_gem_specs(gemfile).inject({}) do |result, spec|
-      previous = result[spec.name.downcase]
-      result[spec.name.downcase] = previous ? [previous, spec].max_by{|s| s.version} : spec
-      result
-    end
-  end
+      # piece file back together and write
+  manifest = '  s.files = %w[\n#{files}\n  ]\n'
+  spec = [head, manifest, tail].join('  # = MANIFEST =\n')
+  File.open(gemspec_file, 'w') { |io| io.write(spec) }
+  puts 'Updated #{gemspec_file}'
 end
-
-    
-    selected_boxes = if ENV.include?('LS_VAGRANT_HOST') then
-                   config.platforms.select { |p| p.name  == ENV['LS_VAGRANT_HOST'] }
-                 else
-                   default_vagrant_boxes
-                 end
-    
-          context 'at the top level' do
-        let(:src) { 'if a then b else c end' }
-    
-            it_behaves_like 'nonmatching'
-      end
-    
-                do_something
-          #{close}
-        RUBY
-    
-    # This file provides the following methods:
-#   encodeURIComponent(componentString)
-#   string.charCodeAt(k)
-    
-          def title
-        'Latest Changes (Globally)'
-      end
-    
-          def author
-        first = page.last_version
-        return DEFAULT_AUTHOR unless first
-        first.author.name.respond_to?(:force_encoding) ? first.author.name.force_encoding('UTF-8') : first.author.name
-      end
-    
-    def replace_header(head, header_name)
-  head.sub!(/(\.#{header_name}\s*= ').*'/) { '#{$1}#{send(header_name)}''}
-end
-    
-        get %r{/(.+?)/([0-9a-f]{40})} do
-      file_path = params[:captures][0]
-      version   = params[:captures][1]
-      wikip     = wiki_page(file_path, file_path, version)
-      name      = wikip.name
-      path      = wikip.path
-      if page = wikip.page
-        @page    = page
-        @name    = name
-        @content = page.formatted_data
-        @version = version
-        @bar_side = wikip.wiki.bar_side
-        mustache :page
-      elsif file = wikip.wiki.file('#{file_path}', version, true)
-        show_file(file)
-      else
-        halt 404
-      end
-    end
