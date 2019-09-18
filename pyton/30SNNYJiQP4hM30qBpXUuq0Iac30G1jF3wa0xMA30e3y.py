713c6606756294e4ede1be2c07b40018eb50bc48
SUNsung <weatherius@gmail.com>
2019-09-18 08:46:18 +0300

    Rewrite [30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py] File in: 2019-09-18 08:46:18

--- pyton/30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py
+++ pyton/30SNNYJiQP4hM30qBpXUuq0Iac30G1jF3wa0xMA30e3y.py
@@ -1,126 +1,160 @@
 
         
-        for shape in successive_shapes:
-    print('Processing image shape', shape)
-    img = resize_img(img, shape)
-    img = gradient_ascent(img,
-                          iterations=iterations,
-                          step=step,
-                          max_loss=max_loss)
-    upscaled_shrunk_original_img = resize_img(shrunk_original_img, shape)
-    same_size_original = resize_img(original_img, shape)
-    lost_detail = same_size_original - upscaled_shrunk_original_img
+                # vvvvid embed_info decryption algorithm is reverse engineered from function $ds(h) at vvvvid.js
+        def ds(h):
+            g = 'MNOPIJKL89+/4567UVWXQRSTEFGHABCDcdefYZabstuvopqr0123wxyzklmnghij'
     
-    print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
+            title = self._html_search_regex(
+            (r'<[^>]+\bid=['\']video-title[^>]+>([^<]+)', r'<title>([^<]+)'),
+            webpage, 'title', default=None) or self._html_search_meta(
+            'description', webpage, 'title',
+            default=None) or self._og_search_description(webpage)
     
-        assert np.array_equal(before, after)
-    
-        def compute_output_shape(self, input_shape):
-        return None, self.num_capsule, self.dim_capsule
-    
-        with pytest.raises(ValueError):
-        conv_utils.normalize_tuple(None, 2, 'kernel_size')
-    with pytest.raises(ValueError):
-        conv_utils.normalize_tuple([2, 3, 4], 2, 'kernel_size')
-    with pytest.raises(ValueError):
-        conv_utils.normalize_tuple(['str', 'impossible'], 2, 'kernel_size')
-    
-    __version__ = '${version}'
-    
-            if matching_issue_list_len == 0:
-            # The issue does not exist in the project
-            if state == 'present':
-                # This implies a change
-                changed = True
-                if not check_mode:
-                    # Create the issue
-                    new_issue = project.add_issue(issue_subject, priority_id, status_id, type_id, severity_id, tags=issue_tags, description=issue_description)
-                    if issue_attachment:
-                        new_issue.attach(issue_attachment, description=issue_attachment_description)
-                        issue['attachment'] = issue_attachment
-                        issue['attachment_description'] = issue_attachment_description
-                return (True, changed, 'Issue created', issue)
-    
-        def __init__(self, additional_arg_spec=None, validate_etag_support=False):
-        '''
-        OneViewModuleBase constructor.
+        def _extract_video_info(self, content_id, site='cbs', mpx_acc=2198311517):
+        items_data = self._download_xml(
+            'http://can.cbs.com/thunder/player/videoPlayerService.php',
+            content_id, query={'partner': site, 'contentId': content_id})
+        video_data = xpath_element(items_data, './/item')
+        title = xpath_text(video_data, 'videoTitle', 'title', True)
+        tp_path = 'dJ5BDC/media/guid/%d/%s' % (mpx_acc, content_id)
+        tp_release_url = 'http://link.theplatform.com/s/' + tp_path
     
     
-def main():
-    module = AnsibleModule(
-        argument_spec=gen_specs(passwd=dict(no_log=True)),
-        supports_check_mode=True,
-    )
+class Go90IE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?go90\.com/(?:videos|embed)/(?P<id>[0-9a-zA-Z]+)'
+    _TESTS = [{
+        'url': 'https://www.go90.com/videos/84BUqjLpf9D',
+        'md5': 'efa7670dbbbf21a7b07b360652b24a32',
+        'info_dict': {
+            'id': '84BUqjLpf9D',
+            'ext': 'mp4',
+            'title': 'Daily VICE - Inside The Utah Coalition Against Pornography Convention',
+            'description': 'VICE\'s Karley Sciortino meets with activists who discuss the state\'s strong anti-porn stance. Then, VICE Sports explains NFL contracts.',
+            'timestamp': 1491868800,
+            'upload_date': '20170411',
+            'age_limit': 14,
+        }
+    }, {
+        'url': 'https://www.go90.com/embed/261MflWkD3N',
+        'only_matching': True,
+    }]
+    _GEO_BYPASS = False
     
-        def appid_exist(self, appids):
-        for appid in appids.split('|'):
-            if appid == '':
-                continue
-            if appid in self.config.GAE_APPIDS:
-                return True
-        return False
+    filenames = {
+    'bin': 'youtube-dl',
+    'exe': 'youtube-dl.exe',
+    'tar': 'youtube-dl-%s.tar.gz' % version}
+build_dir = os.path.join('..', '..', 'build', version)
+for key, filename in filenames.items():
+    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
+    fn = os.path.join(build_dir, filename)
+    with open(fn, 'rb') as f:
+        data = f.read()
+    if not data:
+        raise ValueError('File %s is empty!' % fn)
+    sha256sum = hashlib.sha256(data).hexdigest()
+    new_version[key] = (url, sha256sum)
     
     
-class CheckIp(front_base.check_ip.CheckIp):
-    def check_response(self, response):
-        server_type = response.headers.get('Server', '')
-        self.logger.debug('status:%d', response.status)
-        self.logger.debug('Server type:%s', server_type)
+parser = youtube_dl.parseOpts()[0]
+build_completion(parser)
+
     
-        def getUnexpectedToken(self):
-        return self.token
     
-            stat => ID '=' expr ';'
-             => ID '=' atom ('+' atom)* ';'
-             => ID '=' '(' expr ')' ('+' atom)* ';'
-             => ID '=' '(' atom ')' ('+' atom)* ';'
-             => ID '=' '(' INT ')' ('+' atom)* ';'
-             => ID '=' '(' INT ')' ';'
+def _mkdir(d):
+    if not os.path.exists(d):
+        os.mkdir(d)
     
-                    # throw exception unless disjoint or identical
-                disjoint = (prevRop.lastIndex < rop.index
-                            or prevRop.index > rop.lastIndex)
-                same = (prevRop.index == rop.index
-                        and prevRop.lastIndex == rop.lastIndex)
-                if not disjoint and not same:
-                    raise ValueError(
-                        'replace op boundaries of %s overlap with previous %s'
-                        % (rop, prevRop))
+    # Add any paths that contain custom static files (such as style sheets)
+# here, relative to this directory. They are copied after the builtin
+# static files, so a file named 'default.css' will overwrite the builtin
+# 'default.css'.
+html_static_path = ['_static']
     
+        :param X_img_path: path to image to be recognized
+    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
+    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
+    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
+           of mis-classifying an unknown person as a known one.
+    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
+        For faces of unrecognized persons, the name 'unknown' will be returned.
     '''
-http://code.activestate.com/recipes/131499-observer-pattern/
+    if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
+        raise Exception('Invalid image path: {}'.format(X_img_path))
     
-        root_specification = UserSpecification().and_specification(SuperUserSpecification())
+        # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
     
-        def scan(self):
-        '''Scan the dial to the next station'''
-        self.pos += 1
-        if self.pos == len(self.stations):
-            self.pos = 0
-        print(u'Scanning... Station is %s %s' % (self.stations[self.pos], self.name))
+    # You can change this to any folder on your system
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
     
-        print('rm1: {0}'.format(rm1))
-    print('rm2: {0}'.format(rm2))
-    print('rm3: {0}'.format(rm3))
+        pool.starmap(test_image, function_parameters)
     
-            def wrapper(*args, **kwargs):
-            return attr(*args, **kwargs)
-        return wrapper
-    
-        products = {
-        'milk': {'price': 1.50, 'quantity': 10},
-        'eggs': {'price': 0.20, 'quantity': 100},
-        'cheese': {'price': 2.00, 'quantity': 10},
-    }
+        # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
     
     
-# Refined Abstraction
-class CircleShape(object):
-    def __init__(self, x, y, radius, drawing_api):
-        self._x = x
-        self._y = y
-        self._radius = radius
-        self._drawing_api = drawing_api
+def detect_faces_in_image(file_stream):
+    # 用face_recognition.face_encodings(img)接口提前把奥巴马人脸的编码录入
+    known_face_encoding = [-0.09634063,  0.12095481, -0.00436332, -0.07643753,  0.0080383,
+                            0.01902981, -0.07184699, -0.09383309,  0.18518871, -0.09588896,
+                            0.23951106,  0.0986533 , -0.22114635, -0.1363683 ,  0.04405268,
+                            0.11574756, -0.19899382, -0.09597053, -0.11969153, -0.12277931,
+                            0.03416885, -0.00267565,  0.09203379,  0.04713435, -0.12731361,
+                           -0.35371891, -0.0503444 , -0.17841317, -0.00310897, -0.09844551,
+                           -0.06910533, -0.00503746, -0.18466514, -0.09851682,  0.02903969,
+                           -0.02174894,  0.02261871,  0.0032102 ,  0.20312519,  0.02999607,
+                           -0.11646006,  0.09432904,  0.02774341,  0.22102901,  0.26725179,
+                            0.06896867, -0.00490024, -0.09441824,  0.11115381, -0.22592428,
+                            0.06230862,  0.16559327,  0.06232892,  0.03458837,  0.09459756,
+                           -0.18777156,  0.00654241,  0.08582542, -0.13578284,  0.0150229 ,
+                            0.00670836, -0.08195844, -0.04346499,  0.03347827,  0.20310158,
+                            0.09987706, -0.12370517, -0.06683611,  0.12704916, -0.02160804,
+                            0.00984683,  0.00766284, -0.18980607, -0.19641446, -0.22800779,
+                            0.09010898,  0.39178532,  0.18818057, -0.20875394,  0.03097027,
+                           -0.21300618,  0.02532415,  0.07938635,  0.01000703, -0.07719778,
+                           -0.12651891, -0.04318593,  0.06219772,  0.09163868,  0.05039065,
+                           -0.04922386,  0.21839413, -0.02394437,  0.06173781,  0.0292527 ,
+                            0.06160797, -0.15553983, -0.02440624, -0.17509389, -0.0630486 ,
+                            0.01428208, -0.03637431,  0.03971229,  0.13983178, -0.23006812,
+                            0.04999552,  0.0108454 , -0.03970895,  0.02501768,  0.08157793,
+                           -0.03224047, -0.04502571,  0.0556995 , -0.24374914,  0.25514284,
+                            0.24795187,  0.04060191,  0.17597422,  0.07966681,  0.01920104,
+                           -0.01194376, -0.02300822, -0.17204897, -0.0596558 ,  0.05307484,
+                            0.07417042,  0.07126575,  0.00209804]
+    
+    import getopt
+import os
+import platform
+import sys
+from .version import script_name, __version__
+from .util import git, log
+    
+        if '_text' in dictified['video'][0]['file'][0]:  #link exist
+        video_dict['links'] = [i['file'][0]['_text'].strip() for i in dictified['video']]
+    
+        def extract(self, **kwargs):
+        if 'stream_id' in kwargs and kwargs['stream_id']:
+            i = kwargs['stream_id']
+            if 'size' not in self.streams[i]:
+                self.streams[i]['size'] = urls_size(self.streams[i]['src'])
+    
+        Args:
+      clean_lines: A CleansedLines instance containing the file.
+      linenum: The number of the line to check.
+      pos: position just after the suspected template argument.
+    Returns:
+      True if (linenum, pos) is inside template arguments.
+    '''
+    while linenum < clean_lines.NumLines():
+      # Find the earliest character that might indicate a template argument
+      line = clean_lines.elided[linenum]
+      match = Match(r'^[^{};=\[\]\.<>]*(.)', line[pos:])
+      if not match:
+        linenum += 1
+        pos = 0
+        continue
+      token = match.group(1)
+      pos += len(match.group(0))
