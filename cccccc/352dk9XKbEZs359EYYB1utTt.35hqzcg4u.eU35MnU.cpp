d36af0eeb432ac099aba5c8d425e13f580be3695
SUNsung <weatherius@gmail.com>
2019-09-18 20:26:31 +0300

    Rewrite [352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp] File in: 2019-09-18 20:26:31

--- cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
+++ cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
@@ -1,156 +1,222 @@
 
         
-        TF_LITE_MICRO_TESTS_BEGIN
+          // Methods used during initialization, see the comment at the top of the .cc
+  // file for details.
+  static void DidGetCertDBOnUIThread(net::NSSCertDatabase* cert_db,
+                                     bool is_user_db_available,
+                                     const CreationCallback& callback);
+  static void DidGetCertDBOnIOThread(const CreationCallback& callback,
+                                     net::NSSCertDatabase* cert_db);
+  static void GetCertDBOnIOThread(content::ResourceContext* context,
+                                  const CreationCallback& callback);
     
-    #include 'tensorflow/lite/context.h'
+    #include 'native_mate/dictionary.h'
+#include 'native_mate/scoped_persistent.h'
     
-      void CheckArgsAndAllocateResult(OpKernelContext* c,
-                                  OpInputList* indices_inputs,
-                                  OpInputList* data_inputs, int* first_dim_size,
-                                  int* data_elements_size,
-                                  Tensor** result_ptr) {
-    // Find maximum index in the indices vectors
-    OP_REQUIRES_OK(c, c->input_list('indices', indices_inputs));
+      // Notify another process, if available. Otherwise sets ourselves as the
+  // singleton instance. Returns PROCESS_NONE if we became the singleton
+  // instance. Callers are guaranteed to either have notified an existing
+  // process or have grabbed the singleton (unless the profile is locked by an
+  // unreachable process).
+  // TODO(brettw): Make the implementation of this method non-platform-specific
+  // by making Linux re-use the Windows implementation.
+  NotifyResult NotifyOtherProcessOrCreate();
+  void StartListeningOnSocket();
+  void OnBrowserReady();
+    
+    namespace api {
     }
     
-      TfLiteModelInfo model_info_;
-  const std::vector<void*>* inputs_ = nullptr;
-  std::vector<void*> outputs_;
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    #define REGISTER_GATHER_ND_FULL(dev, type, index_type)                 \
-  REGISTER_KERNEL_BUILDER(Name('GatherNd')                             \
-                              .Device(DEVICE_##dev)                    \
-                              .TypeConstraint<type>('Tparams')         \
-                              .TypeConstraint<index_type>('Tindices'), \
-                          GatherNdOp<dev##Device, type, index_type>)
-    
-    void ConvertTileOperator(const Model& model,
-                         const TensorFlowTileOperator& src_op,
-                         GraphDef* tensorflow_graph) {
-  tensorflow::NodeDef* tile_op = tensorflow_graph->add_node();
-  tile_op->set_op('Tile');
-  tile_op->set_name(src_op.outputs[0]);
-  CHECK_EQ(src_op.inputs.size(), 2);
-  *tile_op->add_input() = src_op.inputs[0];
-  *tile_op->add_input() = src_op.inputs[1];
-  const tensorflow::DataType data_type =
-      GetTensorFlowDataType(model, src_op.inputs[0]);
-  (*tile_op->mutable_attr())['T'].set_type(data_type);
-  const tensorflow::DataType multiples_data_type =
-      GetTensorFlowDataType(model, src_op.inputs[1]);
-  (*tile_op->mutable_attr())['Tmultiples'].set_type(multiples_data_type);
+    View::~View() {
+  if (delete_view_)
+    delete view_;
 }
     
-    namespace tensorflow {
-namespace functor {
+            friend Rational operator<<(Rational lhs, Rational const& rhs);
+        friend Rational operator>>(Rational lhs, Rational const& rhs);
+    
+    /// <summary>
+/// Invoked when Navigation to a certain page fails
+/// </summary>
+/// <param name='sender'>The Frame which failed navigation</param>
+/// <param name='e'>Details about the navigation failure</param>
+void App::OnNavigationFailed(Platform::Object ^ sender, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs ^ e)
+{
+    throw ref new FailureException('Failed to load Page ' + e->SourcePageType.Name);
+}
+
+    
+    
+    {
+    {        if ((m_radix == 10) && IsNumberInvalid(m_numberString, MAX_EXPONENT, m_precision, m_radix))
+        {
+            DisplayError(CALC_E_OVERFLOW);
+        }
+        else
+        {
+            // Display the string and return.
+            SetPrimaryDisplay(GroupDigitsPerRadix(m_numberString, m_radix));
+        }
     }
+}
+    
+        int32_t const& Number::Exp() const
+    {
+        return m_exp;
     }
     
-    // Collects one sample of monitoring profile and shows user-friendly metrics.
-// If timestamp flag is true, timestamp will be displayed in '%H:%M:%S' format.
-Status Monitor(const tensorflow::string& service_addr, int duration_ms,
-               int monitoring_level, bool display_timestamp, string* result);
+    CalculatorHistory::CalculatorHistory(size_t maxSize)
+    : m_maxHistorySize(maxSize)
+{
+}
     
-      // Checks if two graphs are equal. Both graphs must have the same set of nodes
-  // with the same inputs and attributes. Nodes can be in different order.
-  //
-  // NOTE: This function uses EXPECT/ASSERT macros to check node properties
-  // equality, and adds all failuires to the current test.
-  void CompareGraphs(GraphDef want, GraphDef got) const;
+        struct HISTORYITEMVECTOR
+    {
+        std::shared_ptr<CalculatorVector<std::pair<std::wstring, int>>> spTokens;
+        std::shared_ptr<CalculatorVector<std::shared_ptr<IExpressionCommand>>> spCommands;
+        std::wstring expression;
+        std::wstring result;
+    };
+    
+    // Space to hold enough digits for a quadword binary number (64) plus digit separator strings for that number (20)
+constexpr int MAX_STRLEN = 84;
+    
+      dict->lookup('Size', &obj1);
+  if (obj1.isArray() && obj1.arrayGetLength() == 2) {
+    obj1.arrayGet(0, &obj2);
+    width = obj2.getInt();
+    obj2.free();
+    obj1.arrayGet(1, &obj2);
+    height = obj2.getInt();
+    obj2.free();
+    writePSFmt('%ALDImageDimensions: {0:d} {1:d}\n', width, height);
+  }
+  obj1.free();
+    
+    //------------------------------------------------------------------------
+    
+    #include 'goo/gtypes.h'
+    
+    #include 'Object.h'
     
     
-    {  absl::flat_hash_map<string, std::shared_ptr<Transposer>> transposer_map_;
+    {  private:
+    XRef *xref;
+    PopplerCache *cache;
 };
     
     
-    {  ANNOTATE_SCOPED_MEMORY_LEAK;  // http://crbug.com/314087
-  // TODO(erg): The mozilla implementation goes to a lot of callback trouble
-  // just to make sure that they react to make sure there's some sort of
-  // cancelable object; including making a whole callback just to handle the
-  // cancelable.
-  //
-  // I don't see any reason why we should care if 'RegisterWindow' completes or
-  // not.
-  g_dbus_proxy_call(registrar_proxy_, 'RegisterWindow',
-                    g_variant_new('(uo)', xid, path.c_str()),
-                    G_DBUS_CALL_FLAGS_NONE, -1, nullptr, nullptr, nullptr);
+    {  if (inlineImg) {
+    str->reset();
+    j = height * ((width * colorMap->getNumPixelComps() *
+		   colorMap->getBits() + 7) / 8);
+    for (i = 0; i < j; ++i)
+      str->getChar();
+    str->close();
+  }
 }
     
-      // mate::TrackableObject:
-  static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
-    
-    #ifndef INCLUDED_IMF_FLOAT_ATTRIBUTE_H
-#define INCLUDED_IMF_FLOAT_ATTRIBUTE_H
-    
-    
-FrameBuffer::ConstIterator
-FrameBuffer::find (const char name[]) const
-{
-    return _map.find (name);
-}
-    
-        if (abs (double (fps) - double (fps_23_976())) < e)
-	return fps_23_976();
-    
-    OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_ENTER
-    
-    #ifndef IMFINPUTSTREAMMUTEX_H_
-#define IMFINPUTSTREAMMUTEX_H_
-    
-      static Mutex s_mutex;
-  static StatsMap s_stats;
-    
-      static constexpr ptrdiff_t tableOff() {
-    return offsetof(FixedStringMap, m_table);
+      /*! \brief return corresponding element set given the node_id */
+  inline Elem operator[](unsigned node_id) const {
+    const Elem e = elem_of_each_node_[node_id];
+    return e;
   }
-  static constexpr ptrdiff_t sizeOff() {
-    return offsetof(FixedStringMap, m_extra);
-  }
-  static constexpr size_t sizeSize() {
-    return sizeof(m_extra);
-  }
-private:
-  struct Elm {
-    LowStringPtr sd;
-    V data;
-  };
     
-    extern 'C' int get_watchdog_count_() noexcept {
-  std::lock_guard<std::mutex> guard(watchdog_mut);
-  return watchdog_count;
-}
-
-    
-    namespace {
+        for (size_t i = 0; i < data_vec.size(); ++i) {
+      bst_uint idx = data_vec[i].index - min_index_;
+      CHECK_LE(idx, static_cast<bst_uint>(std::numeric_limits<StorageIndex>::max()))
+          << 'The storage index is chosen to limited to smaller equal than '
+          << std::numeric_limits<StorageIndex>::max()
+          << 'min_index=' << min_index_;
+      index_.data[i] = static_cast<StorageIndex>(idx);
+      value_.data[i] = data_vec[i].fvalue;
     }
     
-    #endif
-
+    class CompressedBufferWriter {
+ private:
+  size_t symbol_bits_;
+  size_t offset_;
+    }
     
-    bool HHVM_FUNCTION(is_bool, const Variant& v);
-bool HHVM_FUNCTION(is_int, const Variant& v);
-bool HHVM_FUNCTION(is_float, const Variant& v);
-bool HHVM_FUNCTION(is_numeric, const Variant& v);
-bool HHVM_FUNCTION(is_string, const Variant& v);
-bool HHVM_FUNCTION(is_scalar, const Variant& v);
-bool HHVM_FUNCTION(is_array, const Variant& v);
-// HH\\is_vec
-bool HHVM_FUNCTION(HH_is_vec, const Variant& v);
-bool HHVM_FUNCTION(is_object, const Variant& v);
-bool HHVM_FUNCTION(is_resource, const Variant& v);
-bool HHVM_FUNCTION(HH_is_meth_caller, TypedValue v);
+        SPAN_ASSERT_TRUE(left == right, status_);
     
-      // No instructions in the middle of a block should have taken edges,
-  // or be an unconditional Jmp.
-  for (auto it = begin(b.hhbcs); it != end(b.hhbcs); ++it) {
-    assert(it->op != Op::Jmp && 'unconditional Jmp mid-block');
-    if (std::next(it) == end(b.hhbcs)) break;
-    forEachTakenEdge(*it, [&](BlockId /*blk*/) {
-      assert(!'Instruction in middle of block had a jump target');
-    });
+                if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text('counter = %d', counter);
+    
+    #pragma once
+    
+        // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+    
+            // because we use a shared hash table, can only update it
+        // _after_ we've probed all of them
+        chash[h1] = chash[h2] = chash[h3] = chash[h4] = q;
+    
+    
+    // out_of_range.106
+    try
+    {
+        // try to use an array index with leading '0'
+        json::reference ref = j.at('/array/01'_json_pointer);
+    }
+    catch (json::parse_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+      // Will be called while on the write thread before the write executes.  If
+  // this function returns a non-OK status, the write will be aborted and this
+  // status will be returned to the caller of DB::Write().
+  virtual Status Callback(DB* db) = 0;
+    
+      {
+    std::string string_val;
+    // If it cannot pin the value, it copies the value to its internal buffer.
+    // The intenral buffer could be set during construction.
+    PinnableSlice pinnable_val(&string_val);
+    db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key2', &pinnable_val);
+    assert(pinnable_val == 'value');
+    // If the value is not pinned, the internal buffer must have the value.
+    assert(pinnable_val.IsPinned() || string_val == 'value');
   }
+    
+      // Lookup page cache by page identifier
+  //
+  // page_key   Page identifier
+  // buf        Buffer where the data should be copied
+  // size       Size of the page
+  virtual Status Lookup(const Slice& key, std::unique_ptr<char[]>* data,
+                        size_t* size) = 0;
+    
+    
+    { private:
+  DB* db_;
+  const Snapshot* snapshot_;
+};
+    
+    #include 'rocksdb/utilities/lua/rocks_lua_custom_library.h'
+    
+    #include <atomic>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <openpose/core/common.hpp>
+    
+        public:
+        PersonTracker(const bool mergeResults, const int levels = 3, const int patchSize = 31,
+                      const float confidenceThreshold = 0.05f, const bool trackVelocity = false,
+                      const bool scaleVarying = false, const float rescale = 640);
