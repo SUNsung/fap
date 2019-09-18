a58ca24687132670e7a8334f4307f78fb843bd0d
SUNsung <weatherius@gmail.com>
2019-09-18 23:04:22 +0300

    Rewrite [03mqZc2vstXes03Gg4EUzFklSU03e91hHMg3jYc032sw.py] File in: 2019-09-18 23:04:22

--- pyton/03mqZc2vstXes03Gg4EUzFklSU03e91hHMg3jYc032sw.py
+++ pyton/03mqZc2vstXes03Gg4EUzFklSU03e91hHMg3jYc032sw.py
@@ -1,91 +1,160 @@
 
         
-        
-with codecs.open(JSON_FILE_PATH, encoding='utf8') as f:
-    JSON_FILE_CONTENT = f.read()
+        def run_latency_tests(max_threads):
+    for task in latency_tasks:
+        print('Background CPU task:', task.__doc__)
+        print()
+        func, args = task()
+        nthreads = 0
+        while nthreads <= max_threads:
+            results = run_latency_test(func, args, nthreads)
+            n = len(results)
+            # We print out milliseconds
+            lats = [1000 * (t2 - t1) for (t1, t2) in results]
+            #print(list(map(int, lats)))
+            avg = sum(lats) / n
+            dev = (sum((x - avg) ** 2 for x in lats) / n) ** 0.5
+            print('CPU threads=%d: %d ms. (std dev: %d ms.)' % (nthreads, avg, dev), end='')
+            print()
+            #print('    [... from %d samples]' % n)
+            nthreads += 1
+        print()
     
-        def test_binary_file_path(self, httpbin):
-        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
-        r = http('--print=B', 'POST', httpbin.url + '/post',
-                 '@' + BIN_FILE_PATH_ARG, env=env, )
-        assert r == BIN_FILE_CONTENT
-    
-        def __init__(self, error_message):
-        self.error_message = error_message
-    
-        @property
-    def encoding(self) -> Optional[str]:
-        '''Return a `str` with the message's encoding, if known.'''
-        raise NotImplementedError()
-    
-        # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
-    ERROR_CTRL_C = 130
-    
-        platforms = 'any',
-    zip_safe = True,
-    include_package_data = True,
-    
-        if re.match(r'https?://movie', url):
-        title = match1(html, 'name='description' content='([^']+)')
-        tid = match1(url, 'trailer/(\d+)')
-        real_url = 'https://movie.douban.com/trailer/video_url?tid=%s' % tid
-        type, ext, size = url_info(real_url)
-    
-    #----------------------------------------------------------------------
-def makeMimi(upid):
-    '''From http://cdn37.atwikiimg.com/sitescript/pub/dksitescript/FC2.site.js
-    Also com.hps.util.fc2.FC2EncrptUtil.makeMimiLocal
-    L110'''
-    strSeed = 'gGddgPfeaf_gzyr'
-    prehash = upid + '_' + strSeed
-    return md5(prehash.encode('utf-8')).hexdigest()
+        def test_warn_on_dealloc(self):
+        self._check_warn_on_dealloc(support.TESTFN, 'wb', buffering=0)
+        self._check_warn_on_dealloc(support.TESTFN, 'wb')
+        self._check_warn_on_dealloc(support.TESTFN, 'w')
     
     
-class KBaseMapping:
-    def __init__(self, base=62):
-        self.base = base
-        mapping_table = [str(num) for num in range(10)]
-        for i in range(26):
-            mapping_table.append(chr(i + ord('a')))
-        for i in range(26):
-            mapping_table.append(chr(i + ord('A')))
+def suite():
+    module_suite = unittest.makeSuite(ModuleTests, 'Check')
+    connection_suite = unittest.makeSuite(ConnectionTests, 'Check')
+    cursor_suite = unittest.makeSuite(CursorTests, 'Check')
+    thread_suite = unittest.makeSuite(ThreadTests, 'Check')
+    constructor_suite = unittest.makeSuite(ConstructorTests, 'Check')
+    ext_suite = unittest.makeSuite(ExtensionTests, 'Check')
+    closed_con_suite = unittest.makeSuite(ClosedConTests, 'Check')
+    closed_cur_suite = unittest.makeSuite(ClosedCurTests, 'Check')
+    on_conflict_suite = unittest.makeSuite(SqliteOnConflictTests, 'Check')
+    return unittest.TestSuite((
+        module_suite, connection_suite, cursor_suite, thread_suite,
+        constructor_suite, ext_suite, closed_con_suite, closed_cur_suite,
+        on_conflict_suite,
+    ))
     
-        html = get_content(url)
-    uuid_pattern = r''([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})''
-    id = r1(r'var vid='([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})'', html)
-    if id is None:
-        video_pattern = r''vid'\s*:\s*' + uuid_pattern
-        id = match1(html, video_pattern)
-    assert id, 'can't find video info'
-    return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
+    def _ishidden(path):
+    return path[0] in ('.', b'.'[0])
     
-    site = Infoq()
-download = site.download_by_url
-download_playlist = site.download_by_url
-
+    Implements the Distutils 'build' command.'''
     
+        A string listing directories separated by 'os.pathsep'; defaults to
+    os.environ['PATH'].  Returns the complete filename or None if not found.
     '''
-Created on Jun 14, 2011
-Update  on 2017-05-18
-FP-Growth FP means frequent pattern
-the FP-Growth algorithm needs:
-1. FP-tree (class treeNode)
-2. header table (use dict)
-This finds frequent itemsets similar to apriori but does not find association rules.
-Author: Peter/片刻
-GitHub: https://github.com/apachecn/AiLearning
-'''
-from __future__ import print_function
-print(__doc__)
+    _, ext = os.path.splitext(executable)
+    if (sys.platform == 'win32') and (ext != '.exe'):
+        executable = executable + '.exe'
+    
+    class TestUntestedModules(unittest.TestCase):
+    def test_untested_modules_can_be_imported(self):
+        untested = ('encodings', 'formatter')
+        with support.check_warnings(quiet=True):
+            for name in untested:
+                try:
+                    support.import_module('test.test_{}'.format(name))
+                except unittest.SkipTest:
+                    importlib.import_module(name)
+                else:
+                    self.fail('{} has tests even though test_sundry claims '
+                              'otherwise'.format(name))
+    
+        @staticmethod
+    def _get_char_frequency(file) -> dict:
+        is_end_of_file = False
+        signs_frequency = defaultdict(lambda: 0)
+        while not is_end_of_file:
+            prev_pos = file.tell()
+            sign = file.read(1)
+            curr_pos = file.tell()
+            if prev_pos == curr_pos:
+                is_end_of_file = True
+            else:
+                signs_frequency[int.from_bytes(sign, 'big')] += 1
+    
+    # A A B C D C F G
+    
+        def insert(self, val):
+        if val not in self.idxs:
+            self.nums.append(val)
+            self.idxs[val] = len(self.nums)-1
+            return True
+        return False
+    
+            # No candidates when expecting some, fail early.
+        if not candidates and expected_num > 0:
+            raise AssertionError(
+                'No %r events found' % expected_data['event_topic']
+            )
+    
+        def test_remember_old_session(self):
+        self._setSessionCookie(days_old=60)
+        upgrade_cookie_security()
+        self.assertTrue(c.cookies[g.login_cookie].dirty)
+        self.assertEqual(c.cookies[g.login_cookie].expires, NEVER)
+    
+    class TestGetScrapeUrl(unittest.TestCase):
+    @patch('r2.lib.media.Link')
+    def test_link_post(self, Link):
+        post = Link()
+        post.url = 'https://example.com'
+        post.is_self = False
+        url = _get_scrape_url(post)
+        self.assertEqual(url, 'https://example.com')
+    
+        def test_cropping(self):
+        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
+                      height=800)
+        max_ratio = 0.5
+        url = self.provider.resize_image(image, max_ratio=max_ratio)
+        crop = URLENCODED_COMMA.join(('faces', 'entropy'))
+        self.assertEqual(url,
+                ('https://example.com/a.jpg?fit=crop&crop=%s&arh=%s'
+                    % (crop, max_ratio)))
     
     
-datArr, labelList = loadDataSet('data/15.BigData_MapReduce/testSet.txt')
-datMat = mat(datArr)
-# finalWs = seqPegasos(datMat, labelList, 2, 5000)
-finalWs = batchPegasos(datMat, labelList, 2, 50, 100)
-print(finalWs)
+class TestLocalResizer(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.provider = UnsplashitImageResizingProvider()
     
-       # create a tuple with the values to be used by reducer
-   # and encode it with base64 to avoid potential trouble with '\t' and '\n' used
-   # as default separators in Hadoop Streaming
-   producedvalue = base64.b64encode(pickle.dumps((E.T*E, E.T*D*e)))
+    from pylons import app_globals as g
+    
+            for i in xrange(1, 4):
+            for j in xrange(i):
+                csb.record(str(i), j + 1)
+        self.assertEquals(
+            set([('1', '1|c'),
+                 ('2', '3|c'),
+                 ('3', '6|c')]),
+            set(csb.flush()))
+    
+    	interval = 0.5
+    
+    		def __str__(self):
+			return PowerlineRenderBytesResult(self)
+    
+    
+class Prompt(object):
+	__slots__ = ('powerline', 'side', 'savedpsvar', 'savedps', 'args', 'theme', 'above', '__weakref__')
+    
+    
+def int_or_sig(s):
+	if s.startswith('sig'):
+		return u(s)
+	else:
+		return int(s)
+    
+    	def load_theme_config(self, name):
+		r = super(IPythonPowerline, self).load_theme_config(name)
+		if name in self.theme_overrides:
+			mergedicts(r, self.theme_overrides[name])
+		return r
