f9be9c8f7ebadee9b8ceb78f056fa658074c48bc
SUNsung <weatherius@gmail.com>
2019-09-18 22:37:47 +0300

    Rewrite [235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb] File in: 2019-09-18 22:37:47

--- ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
+++ ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
@@ -1,163 +1,159 @@
 
         
-            # Generate 'basename without extension' and strip away any trailing periods.
-    # NOTE: `String#gsub` removes all trailing periods (in comparison to `String#chomp`)
-    def basename
-      @basename ||= File.basename(name, extname).gsub(%r!\.*\z!, '')
+                  resource 'setuptools' do
+          ^^^^^^^^^^^^^^^^^^^^^ Formulae using virtualenvs do not need a `setuptools` resource.
+            url 'https://foo.com/foo.tar.gz'
+            sha256 'db0904a28253cfe53e7dedc765c71596f3c53bb8a866ae50123320ec1a7b73fd'
+          end
+    
+          def provides_sdk?
+        version >= '8'
+      end
+    
+        def initialize(cask, force: false, quarantine: nil)
+      @cask = cask
+      @force = force
+      @quarantine = quarantine
     end
     
-    module Jekyll
-  module Drops
-    class SiteDrop < Drop
-      extend Forwardable
-    
-    def graceful_require
-  Jekyll::External.require_with_graceful_fail('json')
-  JSON.pretty_generate(DATA)
-end
-    
-    # No trailing slash
-Benchmark.ips do |x|
-  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
-  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
-  x.compare!
+          def test_s_new_exc_tainted
+        th = Thread.new {
+          $SAFE=1
+          str = 'ConnectionEvents'
+          str.taint
+          WIN32OLE_EVENT.new(@db, str)
+        }
+        exc = assert_raise(SecurityError) {
+          th.join
+        }
+        assert_match(/insecure event creation - `ConnectionEvents'/, exc.message)
+      ensure
+        $SAFE = 0
+      end
+    end
+  end
 end
 
     
-    Then(/^the default stage files are created$/) do
-  staging = TestApp.test_app_path.join('config/deploy/staging.rb')
-  production = TestApp.test_app_path.join('config/deploy/production.rb')
-  expect(File.exist?(staging)).to be true
-  expect(File.exist?(production)).to be true
+      before :each do
+    @data = 'firstline\nsecondline\n\nforthline'
+    @zip = [31, 139, 8, 0, 244, 125, 128, 88, 2, 255, 75, 203, 44, 42, 46, 201,
+            201, 204, 75, 229, 42, 78, 77, 206, 207, 75, 1, 51, 185, 210,242,
+            139, 74, 50, 64, 76, 0, 180, 54, 61, 111, 31, 0, 0, 0].pack('C*')
+    
+    end
+
+    
+        # Returns a new {Gem::Version} based on the systems `git` version.
+    #
+    # @return [Gem::Version]
+    #
+    def self.git_version
+      raw_version = Executable.capture_command('git', ['--version']).first
+      unless match = raw_version.scan(/\d+\.\d+\.\d+/).first
+        raise 'Failed to extract git version from `git --version` (#{raw_version.inspect})'
+      end
+      Gem::Version.new(match)
+    end
+    
+    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
+  require 'rubygems'
+  require 'bundler/setup'
+  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+elsif ENV['COCOAPODS_NO_BUNDLER']
+  require 'rubygems'
+  gem 'cocoapods'
 end
     
-        (stdout + stderr).each_line { |line| puts '[vagrant] #{line}' }
-    
-        def add_filter(filter=nil, &block)
-      if block
-        raise ArgumentError, 'Both a block and an object were given' if filter
-    
-          def print_deprecation_warnings_if_applicable
-        if using_default_scm?
-          warn_add_git_to_capfile unless scm_plugin_installed?
-        elsif built_in_scm_name?
-          warn_set_scm_is_deprecated
-        elsif third_party_scm_name?
-          warn_third_party_scm_must_be_upgraded
+            def prepare(sandbox, project, pod_targets, build_configurations, platforms, podfile_path)
+          UI.message '- Creating #{project.project_name} project' do
+            build_configurations.each do |name, type|
+              project.add_build_configuration(name, type)
+            end
+            # Reset symroot just in case the user has added a new build configuration other than 'Debug' or 'Release'.
+            project.symroot = Pod::Project::LEGACY_BUILD_ROOT
+            pod_names = pod_targets.map(&:pod_name).uniq
+            pod_names.each do |pod_name|
+              local = sandbox.local?(pod_name)
+              path = sandbox.pod_dir(pod_name)
+              was_absolute = sandbox.local_path_was_absolute?(pod_name)
+              project.add_pod_group(pod_name, path, local, was_absolute)
+            end
+            if podfile_path
+              project.add_podfile(podfile_path)
+            end
+            osx_deployment_target = platforms.select { |p| p.name == :osx }.map(&:deployment_target).min
+            ios_deployment_target = platforms.select { |p| p.name == :ios }.map(&:deployment_target).min
+            watchos_deployment_target = platforms.select { |p| p.name == :watchos }.map(&:deployment_target).min
+            tvos_deployment_target = platforms.select { |p| p.name == :tvos }.map(&:deployment_target).min
+            project.build_configurations.each do |build_configuration|
+              build_configuration.build_settings['MACOSX_DEPLOYMENT_TARGET'] = osx_deployment_target.to_s if osx_deployment_target
+              build_configuration.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = ios_deployment_target.to_s if ios_deployment_target
+              build_configuration.build_settings['WATCHOS_DEPLOYMENT_TARGET'] = watchos_deployment_target.to_s if watchos_deployment_target
+              build_configuration.build_settings['TVOS_DEPLOYMENT_TARGET'] = tvos_deployment_target.to_s if tvos_deployment_target
+              build_configuration.build_settings['STRIP_INSTALLED_PRODUCT'] = 'NO'
+              build_configuration.build_settings['CLANG_ENABLE_OBJC_ARC'] = 'YES'
+            end
+          end
         end
       end
-    
-    # We use a special :_default_git value so that SCMResolver can tell whether the
-# default has been replaced by the user via `set`.
-set_if_empty :scm, Capistrano::Configuration::SCMResolver::DEFAULT_GIT
-set_if_empty :branch, 'master'
-set_if_empty :deploy_to, -> { '/var/www/#{fetch(:application)}' }
-set_if_empty :tmp_dir, '/tmp'
-    
-      before { sign_in user }
-    
-      describe 'PUT /chat_channel_memberships/:id' do
-    before do
-      user.add_role(:super_admin)
-      post '/chat_channel_memberships', params: {
-        chat_channel_membership: { user_id: second_user.id, chat_channel_id: chat_channel.id }
-      }
-    end
-    
-      def delete_confirm?
-    update?
-  end
-    
-      def self.upbuff!(buffer_update_id, admin_id, body_text, status)
-    buffer_update = BufferUpdate.find(buffer_update_id)
-    if status == 'confirmed'
-      buffer_response = send_to_buffer(body_text, buffer_update.buffer_profile_id_code)
-      buffer_update.update!(buffer_response: buffer_response, status: status, approver_user_id: admin_id, body_text: body_text)
-    else
-      buffer_update.update!(status: status, approver_user_id: admin_id)
     end
   end
+end
+
     
-      def destroy
-    @page = Page.find(params[:id])
-    @page.destroy
-    redirect_to '/internal/pages'
-  end
-    
-        def URIEncodePair(cc1, cc2, result, index)
-      u = ((cc1 >> 6) & 0xF) + 1;
-      w = (cc1 >> 2) & 0xF;
-      x = cc1 & 3;
-      y = (cc2 >> 6) & 0xF;
-      z = cc2 & 63;
-      octets = Array.new(4);
-      octets[0] = (u >> 2) + 240;
-      octets[1] = (((u & 3) << 4) | w) + 128;
-      octets[2] = ((x << 4) | y) + 128;
-      octets[3] = z + 128;
-      return URIEncodeOctets(octets, result, index);
-    end
-    
-          def previous_link
-        label = '&laquo; Previous'
-        if @page_num == 1
-          %(<span class='disabled'>#{label}</span>)
+            if podspecs.size <= 1
+          result += [license, readme, podspecs, docs]
         else
-          link = url('/history/#{@page.name}?page=#{@page_num-1}')
-          %(<a href='#{link}' hotkey='h'>#{label}</a>)
+          # Manually add non-globbing files since there are multiple podspecs in the same folder
+          result << podspec_file
+          if license_file = spec_license
+            absolute_path = root + license_file
+            result << absolute_path if File.exist?(absolute_path)
+          end
         end
+        result.compact.flatten.sort
       end
     
-          def partial(name)
-        if name == :author_template
-          self.class.partial('history_authors/#{@wiki.user_icons}')
-        else
-          super
-        end
-      end
+    module Pod
+  # Provides support for UI output. It provides support for nested sections of
+  # information and for a verbose mode.
+  #
+  module UserInterface
+    require 'colored2'
     
-          def css # custom css
-        @css
-      end
+        it 'returns nil if an article doesn't have a flare tag' do
+      expect(described_class.new(article).tag_hash).to be nil
+    end
     
-      test 'create sets the correct path for a relative path subdirectory' do
-    dir  = 'foodir'
-    name = '#{dir}/bar'
-    get '/create/#{name}'
-    assert_match(/\/#{dir}/, last_response.body)
-    assert_no_match(/[^\/]#{dir}/, last_response.body)
+      it 'renders a user index if there is a user with the username successfully' do
+    expect(get: '/#{user.username}').to route_to(
+      controller: 'stories',
+      action: 'index',
+      username: user.username,
+    )
   end
     
-        def wiki_new
-      Gollum::Wiki.new(settings.gollum_path, settings.wiki_options)
-    end
-    
-        it 'logs the exception to Sidekiq.logger' do
-      Component.new.invoke_exception(:a => 1)
-      @str_logger.rewind
-      log = @str_logger.readlines
-      assert_match(/'a':1/, log[0], 'didn't include the context')
-      assert_match(/Something didn't work!/, log[1], 'didn't include the exception message')
-      assert_match(/test\/test_exception_handler.rb/, log[2], 'didn't include the backtrace')
-    end
-    
-          assert SomeScheduledWorker.perform_in(5.days.from_now, 'mike')
-      assert_equal 3, ss.size
-    
-      describe 'error handling' do
-    it 'deals with user-specified error handlers which raise errors' do
-      output = capture_logging do
-        begin
-          Sidekiq.error_handlers << proc {|x, hash|
-            raise 'boom'
-          }
-          cli = Sidekiq::CLI.new
-          cli.handle_exception(RuntimeError.new('hello'))
-        ensure
-          Sidekiq.error_handlers.pop
-        end
-      end
-      assert_includes output, 'boom'
-      assert_includes output, 'ERROR'
+        it 'embeds the published timestamp' do
+      visit '/#{user.username}/#{article.slug}'
+      selector = 'article time[datetime='#{timestamp}']'
+      expect(page).to have_selector(selector)
     end
   end
+end
+
+    
+        context 'exchange has not been requested' do
+      before { allow(subject).to receive(:exchange_requested?).and_return(false) }
+    
+            context 'if the currency is different than the order's currency' do
+          let(:currency) { 'GBP' }
+    
+            def image_params
+          params.require(:image).permit(permitted_image_attributes)
+        end
+    
+            def fire
+          inventory_unit.send('#{@event}!') if @event
+        end
