2dd3ff951edd597c66d50a679b686769a7dab942
SUNsung <weatherius@gmail.com>
2019-09-19 05:54:56 +0300

    Rewrite [183H1.VhI8hFQ18GOQW0aduSug18Qu4E3sY2l8Y18I5t.rb] File in: 2019-09-19 05:54:56

--- ruby/183H1.VhI8hFQ18GOQW0aduSug18Qu4E3sY2l8Y18I5t.rb
+++ ruby/183H1.VhI8hFQ18GOQW0aduSug18Qu4E3sY2l8Y18I5t.rb
@@ -1,295 +1,122 @@
 
         
-        # Sample contents
-WITHOUT_LIQUID = <<-TEXT.freeze
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
-pharetra tempus. Etiam bibendum magna et metus fermentum, eu cursus lorem
-mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
+            attr_reader :filters
     
-            def start(opts)
-          @thread = Thread.new do
-            # Use epoll if the kernel supports it
-            EM.epoll
-            EM.run do
-              EM.error_handler { |e| log_error(e) }
-    
-        def sha256(value)
-      salt = Settings.attr_encrypted_db_key_base_truncated
-      ::Digest::SHA256.base64digest('#{value}#{salt}')
-    end
-    
-            raise ClaimInvalid, 'Missing required 'iat' claim' if options.valid_within && !@decoded['iat']
-    
-              if formula_tap == 'homebrew-core' && (depends_on?('veclibfort') || depends_on?('lapack'))
-            problem 'Formulae should use OpenBLAS as the default serial linear algebra library.'
-          end
-    
-    module Utils
-  class Bottles
-    class << self
-      undef tag
-    
-        depends_on(deps) if add_mac_dependency?(args)
-  end
-    
-          expect(f.class.stable.deps.first.name).to eq('foo')
-      expect(f.class.devel.deps.first.name).to eq('foo')
-      expect(f.class.head.deps.first.name).to eq('foo')
+          @terminal_width = if !tty?
+        nil
+      elsif ENV['COLUMNS']
+        ENV['COLUMNS'].to_i
+      else
+        `stty size`.scan(/\d+/).last.to_i
+      end
+    rescue
+      @terminal_width = nil
     end
   end
 end
 
     
-          allow(OS).to receive(:mac?).and_return(true)
-      allow(OS::Mac).to receive(:version).and_return(OS::Mac::Version.new(sierra_os_version))
-    end
-    
-        it 'raises an error when accessing missing resources' do
-      subject.owner = owner
-      expect {
-        subject.resource('foo')
-      }.to raise_error(ResourceMissingError)
-    end
-  end
-    
-      it 'prints the file used to cache the Cask' do
-    transmission_location = CurlDownloadStrategy.new(
-      local_transmission.url.to_s, local_transmission.token, local_transmission.version,
-      cache: Cask::Cache.path, **local_transmission.url.specs
-    ).cached_location
-    caffeine_location = CurlDownloadStrategy.new(
-      local_caffeine.url.to_s, local_caffeine.token, local_caffeine.version,
-      cache: Cask::Cache.path, **local_caffeine.url.specs
-    ).cached_location
-    
-          # Homebrew Cask metadata
-      if @cask.metadata_versioned_path.directory?
-        @cask.metadata_versioned_path.children.each do |subdir|
-          gain_permissions_remove(subdir)
+            css('#angular-2-glossary ~ .l-sub-section').each do |node|
+          node.before(node.children).remove
         end
     
-    
-    
-    protected
-    
-        case res.code
-    when 201
-      print_good('Playing https://www.youtube.com/watch?v=#{vid}')
-    when 200
-      print_status('Stopping video')
-    when 404
-      print_error('Target no longer supports casting via the DIAL protocol. ' \
-                  'CASTV2 is not supported by this module at this time.')
-    end
-  end
-    
-      def parse(reader)
-    # PDF
-    # print_status 'PDF Version: #{reader.pdf_version}'
-    # print_status 'PDF Title: #{reader.info['title']}'
-    # print_status 'PDF Info: #{reader.info}'
-    # print_status 'PDF Metadata: #{reader.metadata}'
-    # print_status 'PDF Pages: #{reader.page_count}'
-    
-      def initialize(info = {})
-    super(update_info(
-              info,
-              'Name' => 'Allegro Software RomPager 'Misfortune Cookie' (CVE-2014-9222) Authentication Bypass',
-              'Description' => %q(
-        This module exploits HTTP servers that appear to be vulnerable to the
-        'Misfortune Cookie' vulnerability which affects Allegro Software
-        Rompager versions before 4.34 and can allow attackers to authenticate
-        to the HTTP service as an administrator without providing valid
-        credentials.
-      ),
-              'Author' => [
-                  'Jon Hart <jon_hart[at]rapid7.com>', # metasploit scanner module
-                  'Jan Trencansky <jan.trencansky[at]gmail.com>', # metasploit auxiliary admin module
-                  'Lior Oppenheim' # CVE-2014-9222
-              ],
-              'References' => [
-                  ['CVE', '2014-9222'],
-                  ['URL', 'http://mis.fortunecook.ie'],
-                  ['URL', 'http://mis.fortunecook.ie/misfortune-cookie-suspected-vulnerable.pdf'], # list of likely vulnerable devices
-                  ['URL', 'http://mis.fortunecook.ie/too-many-cooks-exploiting-tr069_tal-oppenheim_31c3.pdf'] # 31C3 presentation with POC
-              ],
-              'DisclosureDate' => 'Dec 17 2014',
-              'License' => MSF_LICENSE
-          ))
-    
-        print_status('Sending Jenkins and Groovy go-go-gadgets')
-    send_request_cgi(
-      'method'   => 'GET',
-      'uri'      => go_go_gadget1,
-      'vars_get' => vars_get
-    )
-  end
-    
-          ])
-    
-        for line in config_lines
-      line.chomp
-      line_num += 1
-      if line_num == 8
-        enc_password = Rex::Text.decode_base64(line)
-        password = decrypt_password(enc_password)
-      elsif line_num == 12
-        if line.match(/<(.*)>.(.*)/)
-          # Parse for output
-          url = $1
-          realm = $2
-          realm.gsub! '\r', ''   #Remove \r (not common)
-          if line.match(/<(.*):\/\/(.*):(.*)>/)
-            # Parse for reporting
-            sname = $1
-            host = $2
-            portnum = $3
-            portnum.gsub! '\r', ''   #Remove \r (not common)
+            if mod
+          if name == 'Index'
+            return slug.split('/')[1..-2].join('/')
+          elsif name == 'Angular'
+            return slug.split('/').last.split('-').first
           end
-        else
-          url = '<Unknown/Error>'
         end
-      elsif line_num == 16
-        user_name = line
-        user_name.gsub! '\r', ''  #Remove \r (not common)
-      end
-    end
-    config.close
     
-        # Check for result
-    begin
-      size = session.fs.file.stat(net_com_opts[:target].gsub('\\', '\\\\')).size
-      print_good 'File #{net_com_opts[:target].gsub('\\', '\\\\')} found, #{size}kb'
-    rescue
-      print_error 'File #{net_com_opts[:target].gsub('\\', '\\\\')} not found,' \
-                  ' NET CLR version #{datastore['NET_CLR_VER']} possibly not available'
-      return
-    end
+      # Configure which authentication keys should have whitespace stripped.
+  # These keys will have whitespace before and after removed upon creating or
+  # modifying a user and when used to authenticate or find a user. Default is :email.
+  config.strip_whitespace_keys = %i(email unconfirmed_email username)
     
-    task :gemspec => 'rack-protection.gemspec'
-task :default => :spec
-task :test    => :spec
-
-    
-            if @javascript and not @escaper.respond_to? :escape_javascript
-          fail('Use EscapeUtils for JavaScript escaping.')
+          def fetch_public_key(o_auth_app, jwt)
+        public_key = fetch_public_key_from_json(o_auth_app.jwks, jwt)
+        if public_key.empty? && o_auth_app.jwks_uri
+          response = Faraday.get(o_auth_app.jwks_uri)
+          public_key = fetch_public_key_from_json(response.body, jwt)
         end
+        raise Rack::OAuth2::Server::Authorize::BadRequest(:unauthorized_client) if public_key.empty?
+        public_key
       end
     
-            reaction
-      end
+        membership = contact.aspect_memberships.where(aspect_id: aspect.id).first
     
-      context 'escaping' do
-    it 'escapes html entities' do
-      mock_app do |env|
-        request = Rack::Request.new(env)
-        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']]]
-      end
-      get '/', :foo => '<bar>'
-      expect(body).to eq('&lt;bar&gt;')
-    end
+    class ContactsController < ApplicationController
+  before_action :authenticate_user!
     
-          # Try to add the gems to the current gemfile and lock file, if successful
-      # both of them will be updated. This injector is similar to Bundler's own injector class
-      # minus the support for additionals source and doing local resolution only.
-      ::Bundler::LogstashInjector.inject!(pack)
-    
-        puts('Updating #{filtered_plugins.collect(&:name).join(', ')}') unless filtered_plugins.empty?
-    
-      describe 'on #{logstash.hostname}' do
-    context 'with a direct internet connection' do
-      context 'when the plugin exist' do
-        context 'from a local `.GEM` file' do
-          let(:gem_name) { 'logstash-filter-qatest-0.1.1.gem' }
-          let(:gem_path_on_vagrant) { '/tmp/#{gem_name}' }
-          before(:each) do
-            logstash.download('https://rubygems.org/gems/#{gem_name}', gem_path_on_vagrant)
-          end
-    
-      describe 'bulk' do
-    after do
-      Sidekiq::Queue.new.clear
-    end
-    it 'can push a large set of jobs at once' do
-      jids = Sidekiq::Client.push_bulk('class' => QueuedWorker, 'args' => (1..1_000).to_a.map { |x| Array(x) })
-      assert_equal 1_000, jids.size
-    end
-    it 'can push a large set of jobs at once using a String class' do
-      jids = Sidekiq::Client.push_bulk('class' => 'QueuedWorker', 'args' => (1..1_000).to_a.map { |x| Array(x) })
-      assert_equal 1_000, jids.size
-    end
-    it 'returns the jids for the jobs' do
-      Sidekiq::Client.push_bulk('class' => 'QueuedWorker', 'args' => (1..2).to_a.map { |x| Array(x) }).each do |jid|
-        assert_match(/[0-9a-f]{12}/, jid)
-      end
-    end
-    it 'handles no jobs' do
-      result = Sidekiq::Client.push_bulk('class' => 'QueuedWorker', 'args' => [])
-      assert_equal 0, result.size
-    end
-  end
-    
-        it 'can start and stop' do
-      f = Sidekiq::Processor.new(Mgr.new)
-      f.terminate
-    end
-    
-      def options
-    { :concurrency => 3, :queues => ['default'] }
-  end
-    
-          refute Sidekiq::Testing.enabled?
-      refute Sidekiq::Testing.fake?
-    end
-    
-        def self.register_private_key(email_address)
-      registration = client.new_account(:contact => 'mailto:#{email_address}', :terms_of_service_agreed => true)
-      logger.info 'Successfully registered private key with address #{email_address}'
-      true
-    end
-    
-      def check_mx_records
-    result = resolver.getresources(self.name, Resolv::DNS::Resource::IN::MX)
-    records = result.map(&:exchange)
-    if records.empty?
-      self.mx_status = 'Missing'
-      self.mx_error = 'There are no MX records for #{self.name}'
+      def raw
+    @conversation = current_user.conversations.where(id: params[:conversation_id]).first
+    if @conversation
+      @first_unread_message_id = @conversation.first_unread_message(current_user).try(:id)
+      @conversation.set_read(current_user)
+      render partial: 'conversations/show', locals: {conversation: @conversation}
     else
-      missing_records = Postal.config.dns.mx_records.dup - records.map { |r| r.to_s.downcase }
-      if missing_records.empty?
-        self.mx_status = 'OK'
-        self.mx_error = nil
-      elsif missing_records.size == Postal.config.dns.mx_records.size
-        self.mx_status = 'Missing'
-        self.mx_error = 'You have MX records but none of them point to us.'
-      else
-        self.mx_status = 'Invalid'
-        self.mx_error = 'MX #{missing_records.size == 1 ? 'record' : 'records'} for #{missing_records.to_sentence} are missing and are required.'
-      end
+      head :not_found
     end
   end
     
-    end
-
+      def toggle_mobile
+    session[:mobile_view] = session[:mobile_view].nil? ? true : !session[:mobile_view]
     
-      def suspend
-    @server.suspend(params[:reason])
-    redirect_to_with_json [organization, @server], :notice => 'Server has been suspended'
-  end
-    
-      def update
-    if @smtp_endpoint.update(safe_params)
-      redirect_to_with_json [organization, @server, :smtp_endpoints]
-    else
-      render_form_errors 'edit', @smtp_endpoint
-    end
-  end
+    class InvitationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_invitations_available!, only: :create
     
       def create
-    @webhook = @server.webhooks.build(safe_params)
-    if @webhook.save
-      redirect_to_with_json [organization, @server, :webhooks]
-    else
-      render_form_errors 'new', @webhook
+    conversation = Conversation.find(params[:conversation_id])
+    
+        def method_missing(method, *args)
+      name = method.to_s.delete('=')
+      if has_preference? name
+        if method.to_s =~ /=$/
+          set_preference(name, args.first)
+        else
+          get_preference name
+        end
+      else
+        super
+      end
     end
   end
+end
+
+    
+    RSpec.configure do |config|
+  # Need to check here again because this is used in i18n_spec too.
+  if ENV['CHECK_TRANSLATIONS']
+    config.after :suite do
+      Spree.check_missing_translations
+      if Spree.missing_translation_messages.any?
+        puts '\nThere are missing translations within Spree:'
+        puts Spree.missing_translation_messages.sort
+        exit(1)
+      end
+    
+      describe '.exchange_variant_engine' do
+    it 'defaults to the same product calculator' do
+      expect(Spree::ReturnItem.exchange_variant_engine).to eq Spree::ReturnItem::ExchangeVariantEligibility::SameProduct
+    end
+  end
+    
+          attr_accessor :current_api_user
+    
+            def update
+          @image = scope.images.accessible_by(current_ability, :update).find(params[:id])
+          if @image.update(image_params)
+            respond_with(@image, default_template: :show)
+          else
+            invalid_resource!(@image)
+          end
+        end
+    
+            def update
+          authorize! :update, inventory_unit.order
+    
+              Spree::Dependencies.cart_remove_item_service.constantize.call(order: @shipment.order,
+                                                                        variant: variant,
+                                                                        quantity: quantity,
+                                                                        options: { shipment: @shipment })
