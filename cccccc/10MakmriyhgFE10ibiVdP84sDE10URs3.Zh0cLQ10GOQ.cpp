4825b1d0f08928d62afab0eb05efc4d9b8a3dfa1
SUNsung <weatherius@gmail.com>
2019-09-19 11:04:52 +0300

    Rewrite [10MakmriyhgFE10ibiVdP84sDE10URs3.Zh0cLQ10GOQ.cpp] File in: 2019-09-19 11:04:51

--- cccccc/10MakmriyhgFE10ibiVdP84sDE10URs3.Zh0cLQ10GOQ.cpp
+++ cccccc/10MakmriyhgFE10ibiVdP84sDE10URs3.Zh0cLQ10GOQ.cpp
@@ -1,249 +1,321 @@
 
         
-        const int32 MAX_CONNECTED = 32;
-const float32 COLLAPSE_DIST_SQR = CMP_EPSILON*CMP_EPSILON;//0.1f;//1000*CMP_EPSILON*1000*CMP_EPSILON;
-	
-class b2PolyNode{
-public:
-	b2Vec2 position;
-	b2PolyNode* connected[MAX_CONNECTED];
-	int32 nConnected;
-	bool visited;
+        // Wraps operations to the global DescriptorPool which contains information
+// about all messages and fields.
+//
+// There is normally one pool per process. We make it a Python object only
+// because it contains many Python references.
+//
+// 'Methods' that interacts with this DescriptorPool are in the cdescriptor_pool
+// namespace.
+typedef struct PyDescriptorPool {
+  PyObject_HEAD
     }
     
-    #ifndef B2_TRIANGLE_H
-#define B2_TRIANGLE_H
+     private:
+  void GenerateHeader(const GeneratorOptions& options,
+                      const FileDescriptor* file, io::Printer* printer) const;
     
-    namespace Etc
-{
-	// ----------------------------------------------------------------------------------------------------
-	// C-style inteface to the encoder
-	//
-	void Encode(float *a_pafSourceRGBA,
-				unsigned int a_uiSourceWidth, 
-				unsigned int a_uiSourceHeight,
-				Image::Format a_format,
-				ErrorMetric a_eErrMetric,
-				float a_fEffort,
-				unsigned int a_uiJobs,
-				unsigned int a_uiMaxJobs,
-				unsigned char **a_ppaucEncodingBits,
-				unsigned int *a_puiEncodingBitsBytes,
-				unsigned int *a_puiExtendedWidth,
-				unsigned int *a_puiExtendedHeight, 
-				int *a_piEncodingTime_ms, bool a_bVerboseOutput)
-	{
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace java {
+namespace {
+    }
+    }
+    }
     }
     }
     
-    	// C-style inteface to the encoder
-	void Encode(float *a_pafSourceRGBA,
-				unsigned int a_uiSourceWidth,
-				unsigned int a_uiSourceHeight,
-				Image::Format a_format,
-				ErrorMetric a_eErrMetric,
-				float a_fEffort,
-				unsigned int a_uiJobs,
-				unsigned int a_uimaxJobs,
-				unsigned char **a_ppaucEncodingBits,
-				unsigned int *a_puiEncodingBitsBytes,
-				unsigned int *a_puiExtendedWidth,
-				unsigned int *a_puiExtendedHeight,
-				int *a_piEncodingTime_ms, bool a_bVerboseOutput = false);
     
-    		m_errormetric = a_errormetric;
+    { private:
+  StringPiece data_;
+  int block_size_;
+};
     
-    		static unsigned int GetBytesPerBlock(Format a_format)
-		{
-			switch (a_format)
-			{
-			case Format::RGB8:
-			case Format::R11:
-			case Format::RGB8A1:
-				return 8;
-				break;
-    }
-    }
-    
-    #include 'vpx_dsp/vpx_dsp_common.h'
-    
-    /**
-  Decompress a block of compressed data and returns the size of the
-  decompressed block. If error occurs, e.g. the compressed data is
-  corrupted or the output buffer is not large enough, then 0 (zero)
-  will be returned instead.
-    
-       C_MUL(m,a,b)         : m = a*b
-   C_FIXDIV( c , div )  : if a fixed point impl., c /= div. noop otherwise
-   C_SUB( res, a,b)     : res = a - b
-   C_SUBFROM( res , a)  : res -= a
-   C_ADDTO( res , a)    : res += a
- * */
-#ifdef FIXED_POINT
-#include 'arch.h'
-    
-    #define ABS16(x) ((x) < 0 ? (-(x)) : (x))
-#define ABS32(x) ((x) < 0 ? (-(x)) : (x))
-    
-    #endif /* OPUS_HAVE_RTCD */
-    
-    #endif
-
-    
-    /*The number of bits to output at a time.*/
-# define EC_SYM_BITS   (8)
-/*The total number of bits in each of the state registers.*/
-# define EC_CODE_BITS  (32)
-/*The maximum symbol value.*/
-# define EC_SYM_MAX    ((1U<<EC_SYM_BITS)-1)
-/*Bits to shift by to move a symbol into the high-order position.*/
-# define EC_CODE_SHIFT (EC_CODE_BITS-EC_SYM_BITS-1)
-/*Carry bit of the high-order range symbol.*/
-# define EC_CODE_TOP   (((opus_uint32)1U)<<(EC_CODE_BITS-1))
-/*Low-order bit of the high-order range symbol.*/
-# define EC_CODE_BOT   (EC_CODE_TOP>>EC_SYM_BITS)
-/*The number of bits available for the last, partial symbol in the code field.*/
-# define EC_CODE_EXTRA ((EC_CODE_BITS-2)%EC_SYM_BITS+1)
-#endif
-
-    
-      if (!manualCtrl) {
-    Page *page;
-    // this check is needed in case the document has zero pages
-    if ((page = doc->getPage(firstPage))) {
-      writeHeader(firstPage, lastPage,
-		  page->getMediaBox(),
-		  page->getCropBox(),
-		  page->getRotate(),
-		  pstitle);
-    } else {
-      error(-1, 'Invalid page %d', firstPage);
-      box = new PDFRectangle(0, 0, 1, 1);
-      writeHeader(firstPage, lastPage, box, box, 0, pstitle);
-      delete box;
-    }
-    if (mode != psModeForm) {
-      writePS('%%BeginProlog\n');
-    }
-    writeXpdfProcset();
-    if (mode != psModeForm) {
-      writePS('%%EndProlog\n');
-      writePS('%%BeginSetup\n');
-    }
-    writeDocSetup(doc, catalog, firstPage, lastPage, duplexA);
-    if (mode != psModeForm) {
-      writePS('%%EndSetup\n');
-    }
+    class Proto3DataStripper : public DataStripper {
+ private:
+  virtual bool ShouldBeClear(const FieldDescriptor *field) {
+    return field->type() == FieldDescriptor::TYPE_GROUP ||
+           field->is_extension();
   }
+};
     
-      count = p - buffer;
-  if (buffer[0] >= 'a' && buffer[0] <= 'z')
-    return 26 * (count - 1) + buffer[0] - 'a' + 1;
-  if (buffer[0] >= 'A' && buffer[0] <= 'Z')
-    return 26 * (count - 1) + buffer[0] - 'A' + 1;
-    
-      // get alignment
-  if (dict->lookup('Dm', &obj)->isName()) {
-    const char *dm = obj.getName();
-    
-    if (strcmp('H', dm) == 0)
-      alignment = transitionHorizontal;
-    else if (strcmp('V', dm) == 0)
-      alignment = transitionVertical;
-  }
-  obj.free();
-    
-        Object *put(const Ref &ref);
-    Object *lookup(const Ref &ref, Object *obj);
-    
-      MediaParameters* getMHParameters() { return &MH; }
-  MediaParameters* getBEParameters() { return &BE; }
-    
-    
-    {#ifdef SPLASH_CMYK
-  if (cm == splashModeCMYK8) {
-    SplashColor rgbSrc;
-    SplashColor rgbDest;
-    SplashColor rgbBlend;
-    cmykToRGB(src, rgbSrc);
-    cmykToRGB(dest, rgbDest);
-    for (i = 0; i < 3; ++i) {
-      rgbBlend[i] = rgbDest[i] > rgbSrc[i] ? rgbDest[i] : rgbSrc[i];
+        for (int i = 0; i < dataset.payload_size(); i++) {
+      message->ParseFromString(dataset.payload(i));
+      GogoDataStripper stripper;
+      stripper.StripMessage(message);
+      dataset.set_payload(i, message->SerializeAsString());
     }
-    rgbToCMYK(rgbBlend, blend);
-  } else
-#endif
-  {
-    for (i = 0; i < splashColorModeNComps[cm]; ++i) {
-      blend[i] = dest[i] > src[i] ? dest[i] : src[i];
+    
+    template <typename Dtype>
+void HDF5OutputLayer<Dtype>::SaveBlobs() {
+  // TODO: no limit on the number of blobs
+  LOG(INFO) << 'Saving HDF5 file ' << file_name_;
+  CHECK_EQ(data_blob_.num(), label_blob_.num()) <<
+      'data blob and label blob must have the same batch size';
+  hdf5_save_nd_dataset(file_id_, HDF5_DATA_DATASET_NAME, data_blob_);
+  hdf5_save_nd_dataset(file_id_, HDF5_DATA_LABEL_NAME, label_blob_);
+  LOG(INFO) << 'Successfully saved ' << data_blob_.num() << ' rows';
+}
+    
+    // Verifies format of data stored in HDF5 file and reshapes blob accordingly.
+template <typename Dtype>
+void hdf5_load_nd_dataset_helper(
+    hid_t file_id, const char* dataset_name_, int min_dim, int max_dim,
+    Blob<Dtype>* blob, bool reshape) {
+  // Verify that the dataset exists.
+  CHECK(H5LTfind_dataset(file_id, dataset_name_))
+      << 'Failed to find HDF5 dataset ' << dataset_name_;
+  // Verify that the number of dimensions is in the accepted range.
+  herr_t status;
+  int ndims;
+  status = H5LTget_dataset_ndims(file_id, dataset_name_, &ndims);
+  CHECK_GE(status, 0) << 'Failed to get dataset ndims for ' << dataset_name_;
+  CHECK_GE(ndims, min_dim);
+  CHECK_LE(ndims, max_dim);
     }
+    
+     protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the computed outputs @f$
+   *        y = \max(min, \min(max, x))
+   *      @f$
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+    // Load weights from the caffemodel(s) specified in 'weights' solver parameter
+// into the train and test nets.
+template <typename Dtype>
+void LoadNetWeights(shared_ptr<Net<Dtype> > net,
+    const std::string& model_list) {
+  std::vector<std::string> model_names;
+  boost::split(model_names, model_list, boost::is_any_of(','));
+  for (int i = 0; i < model_names.size(); ++i) {
+    boost::trim(model_names[i]);
+    LOG(INFO) << 'Finetuning from ' << model_names[i];
+    net->CopyTrainedLayersFrom(model_names[i]);
   }
 }
     
-    #include 'StdinPDFDocBuilder.h'
-#include 'CachedFile.h'
-#include 'StdinCachedFile.h'
     
-      // if background compaction is not working, write will stall
-  // because of options.level0_stop_writes_trigger
-  for (int i = 1000; i < 99999; ++i) {
-    db->Put(WriteOptions(), std::to_string(i),
-                            std::string(500, 'a' + (i % 26)));
-  }
+    {}  // namespace caffe
+
     
-      // Initialize pointer options for each column family
-  for (size_t i = 0; i < loaded_cf_descs.size(); ++i) {
-    auto* loaded_bbt_opt = reinterpret_cast<BlockBasedTableOptions*>(
-        loaded_cf_descs[0].options.table_factory->GetOptions());
-    // Expect the same as BlockBasedTableOptions will be loaded form file.
-    assert(loaded_bbt_opt->block_size == bbt_opts.block_size);
-    // However, block_cache needs to be manually initialized as documented
-    // in rocksdb/utilities/options_util.h.
-    loaded_bbt_opt->block_cache = cache;
-  }
-  // In addition, as pointer options are initialized with default value,
-  // we need to properly initialized all the pointer options if non-defalut
-  // values are used before calling DB::Open().
-  assert(loaded_cf_descs[0].options.compaction_filter == nullptr);
-  loaded_cf_descs[0].options.compaction_filter = compaction_filter.get();
+      /* This operation will write the separate BGR planes directly to the
+   * input layer of the network because it is wrapped by the cv::Mat
+   * objects in input_channels. */
+  cv::split(sample_normalized, *input_channels);
     
-      PinnableSlice pinnable_val;
-  db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key1', &pinnable_val);
-  assert(s.IsNotFound());
-  // Reset PinnableSlice after each use and before each reuse
-  pinnable_val.Reset();
-  db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key2', &pinnable_val);
-  assert(pinnable_val == 'value');
-  pinnable_val.Reset();
-  // The Slice pointed by pinnable_val is not valid after this point
+    #endif  // CAFFE_POOLING_LAYER_HPP_
+
     
-    class DbDumpTool {
+      const Dtype* cpu_data() const;
+  void set_cpu_data(Dtype* data);
+  const int* gpu_shape() const;
+  const Dtype* gpu_data() const;
+  void set_gpu_data(Dtype* data);
+  const Dtype* cpu_diff() const;
+  const Dtype* gpu_diff() const;
+  Dtype* mutable_cpu_data();
+  Dtype* mutable_gpu_data();
+  Dtype* mutable_cpu_diff();
+  Dtype* mutable_gpu_diff();
+  void Update();
+  void FromProto(const BlobProto& proto, bool reshape = true);
+  void ToProto(BlobProto* proto, bool write_diff = false) const;
+    
+        glfwDestroyWindow(window);
+    glfwTerminate();
+    
+            // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin('Another Window', &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text('Hello from another window!');
+            if (ImGui::Button('Close Me'))
+                show_another_window = false;
+            ImGui::End();
+        }
+    
+    // Callbacks (installed by default if you enable 'install_callbacks' during initialization)
+// You can also handle inputs yourself and use those as a reference.
+IMGUI_IMPL_API int32    ImGui_Marmalade_PointerButtonEventCallback(void* system_data, void* user_data);
+IMGUI_IMPL_API int32    ImGui_Marmalade_KeyCallback(void* system_data, void* user_data);
+IMGUI_IMPL_API int32    ImGui_Marmalade_CharCallback(void* system_data, void* user_data);
+
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+        CleanupDeviceD3D();
+    ::DestroyWindow(hwnd);
+    ::UnregisterClass(wc.lpszClassName, wc.hInstance);
+    
+        {
+        D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+        desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+        desc.NumDescriptors = NUM_BACK_BUFFERS;
+        desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        desc.NodeMask = 1;
+        if (g_pd3dDevice->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&g_pd3dRtvDescHeap)) != S_OK)
+            return false;
+    }
+    
+    
+    // exception type_error.304
+    try
+    {
+        // use at() on a non-object type
+        json str = 'I am a string';
+        str.at('the good') = 'Another string';
+    }
+    catch (json::type_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+        // change element at index 1 (second element) to 'second'
+    array.at(1) = 'second';
+    
+        // the following call will not add an object, because there is already
+    // a value stored at key 'B'
+    auto res2 = null.emplace('B', 'c');
+    
+    class InteractionPredictor : public SequencePredictor {
  public:
-  bool Run(const DumpOptions& dump_options,
-           rocksdb::Options options = rocksdb::Options());
-};
-    
-    namespace rocksdb {
-namespace experimental {
-    }
+  /**
+   * @brief Constructor
+   */
+  InteractionPredictor();
     }
     
-      // Return stats as map of {string, double} per-tier
-  //
-  // Persistent cache can be initialized as a tier of caches. The stats are per
-  // tire top-down
-  virtual StatsType Stats() = 0;
+    class GraphSegmentorTest : public testing::Test {
+ protected:
+  void SetUp() {
+    edges_ = new Edge[10];
+    edges_[0].w = 6.f;
+    edges_[0].a = 1;
+    edges_[0].b = 2;
+    edges_[1].w = 1.f;
+    edges_[1].a = 1;
+    edges_[1].b = 3;
+    edges_[2].w = 5.f;
+    edges_[2].a = 1;
+    edges_[2].b = 4;
+    edges_[3].w = 5.f;
+    edges_[3].a = 3;
+    edges_[3].b = 2;
+    edges_[4].w = 5.f;
+    edges_[4].a = 3;
+    edges_[4].b = 4;
+    edges_[5].w = 3.f;
+    edges_[5].a = 5;
+    edges_[5].b = 2;
+    edges_[6].w = 6.f;
+    edges_[6].a = 3;
+    edges_[6].b = 5;
+    edges_[7].w = 4.f;
+    edges_[7].a = 3;
+    edges_[7].b = 0;
+    edges_[8].w = 2.f;
+    edges_[8].a = 4;
+    edges_[8].b = 0;
+    edges_[9].w = 6.f;
+    edges_[9].a = 5;
+    edges_[9].b = 0;
+  }
+    }
     
-     private:
-  void Init(
-      const std::string& lua_script,
-      const std::vector<std::shared_ptr<RocksLuaCustomLibrary>>& libraries) {
-    if (lua_state_) {
-      luaL_openlibs(lua_state_);
-      for (const auto& library : libraries) {
-        luaL_openlib(lua_state_, library->Name(), library->Lib(), 0);
-        library->CustomSetup(lua_state_);
-      }
-      luaL_dostring(lua_state_, lua_script.c_str());
+      std::string config_module_path =
+      GetAbsolutePath(work_root_, FLAGS_config_manager_path);
+  AINFO << 'WORK_ROOT: ' << work_root_
+        << ' config_root_path: ' << config_module_path;
+    
+      EXPECT_TRUE(apollo::cyber::OK());
+  uint64_t seq = 5;
+  msgChat->set_timestamp(Time::Now().ToNanosecond());
+  msgChat->set_lidar_timestamp(Time::Now().ToNanosecond());
+  msgChat->set_seq(seq++);
+  msgChat->set_content('Hello, apollo!');
+    
+      void WaitForFinish() {
+    if (thread_send_ != nullptr && thread_send_->joinable()) {
+      thread_send_->join();
+      thread_send_.reset();
+      AINFO << 'Send thread stopped. conf:'
+            << param_ptr_->conf.ShortDebugString();
+    }
+    if (thread_recv_ != nullptr && thread_recv_->joinable()) {
+      thread_recv_->join();
+      thread_recv_.reset();
+      AINFO << 'Recv thread stopped. conf:'
+            << param_ptr_->conf.ShortDebugString();
     }
   }
+    
+    /**
+ * @brief Normalize angle to [-PI, PI).
+ * @param angle the original value of the angle.
+ * @return The normalized value of the angle.
+ */
+double NormalizeAngle(const double angle);
+    
+      // Init object template
+  object_template_manager_ = ObjectTemplateManager::Instance();
+    
+    #include 'modules/planning/scenarios/park/valet_parking/valet_parking_scenario.h'
+#include 'modules/planning/scenarios/stage.h'
+    
+    TEST_F(DistanceApproachIPOPTInterfaceTest, eval_f) {
+  int n = 1274;
+  double obj_value = 0.0;
+  double x[1274];
+  std::fill_n(x, n, 1.2);
+  bool res = ptop_->eval_f(n, x, true, obj_value);
+  EXPECT_DOUBLE_EQ(obj_value, 1443.3600000000008) << 'eval_f: ' << obj_value;
+  EXPECT_TRUE(res);
+}
+    
+      Status UnlockFile(FileLock* lock) override {
+    PERF_TIMER_GUARD(env_unlock_file_nanos);
+    return EnvWrapper::UnlockFile(lock);
+  }
+    
+    // A merge operator that mimics Put semantics
+// Since this merge-operator will not be used in production,
+// it is implemented as a non-associative merge operator to illustrate the
+// new interface and for testing purposes. (That is, we inherit from
+// the MergeOperator class rather than the AssociativeMergeOperator
+// which would be simpler in this case).
+//
+// From the client-perspective, semantics are the same.
+class PutOperator : public MergeOperator {
+ public:
+  bool FullMerge(const Slice& /*key*/, const Slice* /*existing_value*/,
+                 const std::deque<std::string>& operand_sequence,
+                 std::string* new_value, Logger* /*logger*/) const override {
+    // Put basically only looks at the current/latest value
+    assert(!operand_sequence.empty());
+    assert(new_value != nullptr);
+    new_value->assign(operand_sequence.back());
+    return true;
+  }
+    }
+    
+      MyFilter filter;
+    
+    #include 'rocksdb/cache.h'
+#include 'rocksdb/db.h'
+    
+    #pragma once
+#ifndef ROCKSDB_LITE
