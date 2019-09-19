7d9d0d45c0c294d0a202cbd18573a9a3dd70e3ba
SUNsung <weatherius@gmail.com>
2019-09-19 07:46:15 +0300

    Rewrite [430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb] File in: 2019-09-19 07:46:14

--- ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
+++ ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
@@ -1,24 +1,76 @@
 
         
-          it 'calls the given block for each byte in the stream, passing the byte as an argument' do
-    gz = Zlib::GzipReader.new @io
+              it 'calls errors.add' do
+        expect(errors).to have_received(:add).with(attribute, I18n.t('applications.invalid_url'))
+      end
+    end
     
-            it 'does not make eof? false' do
-          @gz.ungetbyte nil
-          @gz.eof?.should be_true
+          context 'limit_reached?' do
+        let(:limit_reached) { true }
+    
+            it 'does notify' do
+          is_expected.to change(Notification, :count)
         end
       end
     end
+    
+      def refresh_poll
+    ActivityPub::FetchRemotePollService.new.call(@poll, current_account) if user_signed_in? && @poll.possibly_stale?
   end
 end
 
     
-    Then(/^the release is created$/) do
-  run_vagrant_command('ls -g #{TestApp.releases_path}')
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+    
+      def test_image_helper
+    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
+  end
+    
+      describe 'PUT blocks' do
+    it 'updates block from input data' do
+      block = create(:block, user_id: user.id, input_css: '.blue { color: blue;}')
+      put '/blocks/#{block.id}', params: {
+        block: { input_css: '.blue { color: red;}',
+                 input_html: 'yo',
+                 input_javascript: 'alert('hey')' }
+      }
+      expect(Block.last.processed_css).to include('color: red')
+    end
+  end
+    
+      it 'renders a user index if there is a user with the username successfully' do
+    expect(get: '/#{user.username}').to route_to(
+      controller: 'stories',
+      action: 'index',
+      username: user.username,
+    )
+  end
+    
+    When /^I configure the application to use '([^\']+)'$/ do |gem_name|
+  append_to_gemfile 'gem '#{gem_name}''
 end
     
-        def cmdline_filters
-      @cmdline_filters ||= []
+    After do
+  ORIGINAL_BUNDLE_VARS.each_pair do |key, value|
+    ENV[key] = value
+  end
+end
+    
+        def self.names_for(klass)
+      instance.names_for(klass)
     end
     
-          include Capistrano::ProcHelpers
+        def define
+      define_flush_errors
+      define_getters
+      define_setter
+      define_query
+      register_new_attachment
+      add_active_record_callbacks
+      add_paperclip_callbacks
+      add_required_validations
+    end
