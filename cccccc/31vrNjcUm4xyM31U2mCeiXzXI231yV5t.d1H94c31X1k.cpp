9528259e511e7dde31a11c1800fd1190baa89f2a
SUNsung <weatherius@gmail.com>
2019-09-19 18:15:46 +0300

    Rewrite [31vrNjcUm4xyM31U2mCeiXzXI231yV5t.d1H94c31X1k.cpp] File in: 2019-09-19 18:15:45

--- cccccc/31vrNjcUm4xyM31U2mCeiXzXI231yV5t.d1H94c31X1k.cpp
+++ cccccc/31vrNjcUm4xyM31U2mCeiXzXI231yV5t.d1H94c31X1k.cpp
@@ -1,123 +1,57 @@
 
         
-        
-    {
-    {    switch (Bytes) {
-    case 1:
-      OutUTF8.push_back(S);
-      break;
-    case 2: {
-      uint8_t Byte2 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte1 = S | 0xC0;
-      OutUTF8.push_back(Byte1);
-      OutUTF8.push_back(Byte2);
-      break;
-    }
-    case 3: {
-      uint8_t Byte3 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte2 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte1 = S | 0xE0;
-      OutUTF8.push_back(Byte1);
-      OutUTF8.push_back(Byte2);
-      OutUTF8.push_back(Byte3);
-      break;
-    }
-    case 4: {
-      uint8_t Byte4 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte3 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte2 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte1 = S | 0xF0;
-      OutUTF8.push_back(Byte1);
-      OutUTF8.push_back(Byte2);
-      OutUTF8.push_back(Byte3);
-      OutUTF8.push_back(Byte4);
-      break;
-    }
-    }
-  }
-  return true;
-}
+            if (error != CL_SUCCESS) {
+        return error;
+    } 
     
-    void GenerateDSYMJobAction::anchor() {}
-    
-    class LLVM_LIBRARY_VISIBILITY GenericUnix : public ToolChain {
-protected:
-  InvocationInfo constructInvocation(const InterpretJobAction &job,
-                                     const JobContext &context) const override;
-  InvocationInfo constructInvocation(const AutolinkExtractJobAction &job,
-                                     const JobContext &context) const override;
-    }
-    
-    #include <stdint.h>
-    
-      // Lookup page cache by page identifier
-  //
-  // page_key   Page identifier
-  // buf        Buffer where the data should be copied
-  // size       Size of the page
-  virtual Status Lookup(const Slice& key, std::unique_ptr<char[]>* data,
-                        size_t* size) = 0;
-    
-    TEST(ByteTest, ByteToString) {
-  unsigned char value = 0x34;
-  EXPECT_EQ('34', Byte::byte_to_hex(value));
-  EXPECT_EQ('00110100', Byte::byte_to_binary(value));
-  uint32_t int_value = 0xE13A;
-  EXPECT_EQ('E13A', Byte::byte_to_hex(int_value));
-}
-    
-    
-    {  double ret = x * OBJECT_LENGTH_RES;
-  return ret;
-}
-    
-    
-    {  int ret = x;
-  return ret;
-}
-    
-        for (i = 1; i < 1024; i++)
-    {
-        uint32_t key = (rand() % (20000 * 37));
-        if (key % 37 == 0)
-        {
-            continue;
-        }
-        int ret = (int) (long) swRbtree_find(tree, key);
-        ASSERT_EQ(ret, 0);
-    }
-    
-    
-    {
-    {    inline Coroutine* pop_coroutine(enum opcode type)
-    {
-        Coroutine* co;
-        if (type == PRODUCER)
-        {
-            co = producer_queue.front();
-            producer_queue.pop_front();
-            swTraceLog(SW_TRACE_CHANNEL, 'resume producer cid=%ld', co->get_cid());
-        }
-        else // if (type == CONSUMER)
-        {
-            co = consumer_queue.front();
-            consumer_queue.pop_front();
-            swTraceLog(SW_TRACE_CHANNEL, 'resume consumer cid=%ld', co->get_cid());
-        }
-        return co;
-    }
-};
-};
+    #endif // OPENPOSE_HAND_HAND_PARAMETERS_HPP
 
     
+    namespace op
+{
+    struct PersonEntry
+    {
+        long long counterLastDetection;
+        std::vector<cv::Point2f> keypoints;
+        std::vector<char> status;
+        /*
+        PersonEntry(long long _last_frame, 
+                    std::vector<cv::Point2f> _keypoints,
+                    std::vector<char> _active):
+                    last_frame(_last_frame), keypoints(_keypoints),
+                    active(_active)
+                    {}
+        */
+    };
+    class OP_API PersonIdExtractor
+    {
+    }
+    }
     
-    {    ASSERT_GT(cid, 0);
-    Coroutine::get_by_cid(cid)->resume();
-    ASSERT_EQ(cid, _cid);
-}
+    BulkOperations::AbstractOperation::AbstractOperation(
+    QSharedPointer<RedisClient::Connection> connection, int dbIndex,
+    OperationCallback callback, QRegExp keyPattern)
+    : m_connection(connection),
+      m_dbIndex(dbIndex),
+      m_keyPattern(keyPattern),
+      m_currentState(State::READY),
+      m_progress(0),
+      m_callback(callback) {}
+    
+    BulkOperations::CopyOperation::CopyOperation(
+    QSharedPointer<RedisClient::Connection> connection, int dbIndex,
+    OperationCallback callback, QRegExp keyPattern)
+    : BulkOperations::AbstractOperation(connection, dbIndex, callback,
+                                        keyPattern) {}
+    
+    BulkOperations::TtlOperation::TtlOperation(
+    QSharedPointer<RedisClient::Connection> connection, int dbIndex,
+    OperationCallback callback, QRegExp keyPattern)
+    : BulkOperations::AbstractOperation(connection, dbIndex, callback,
+                                        keyPattern) {}
+    
+      void openServerStats() override;
+    
+    
+    {    return callback(filter, processed, e);
+  };
