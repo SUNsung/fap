76dfabec5055a930cfce7fccb9cb85f86c2da720
SUNsung <weatherius@gmail.com>
2019-09-19 05:36:41 +0300

    Rewrite [26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp] File in: 2019-09-19 05:36:40

--- cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
+++ cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
@@ -1,243 +1,80 @@
 
         
-                for (size_t i = 0; i < outputs.size(); i++)
-        {
-            UMat srcBlob = inputs[i];
-            void *src_handle = inputs[i].handle(ACCESS_READ);
-            void *dst_handle = outputs[i].handle(ACCESS_WRITE);
-            if (src_handle != dst_handle)
-            {
-                UMat umat = srcBlob.reshape(1, (int)outShapes[i].size(), &outShapes[i][0]);
-                umat.copyTo(outputs[i]);
-            }
-        }
-        outs.assign(outputs);
-    
-            // Applies the colormap on a given image.
-        //
-        // This function expects BGR-aligned data of type CV_8UC1 or CV_8UC3.
-        // Throws an error for wrong-aligned lookup table, which must be
-        // of size 256 in the latest OpenCV release (2.3.1).
-        void operator()(InputArray src, OutputArray dst) const;
-    
-    #include 'test_precomp.hpp'
-    
-    static void help()
-{
-    cout << '\n This program demonstrates how to detect compute and match ORB BRISK and AKAZE descriptors \n'
-        'Usage: \n'
-        '  ./matchmethod_orb_akaze_brisk --image1=<image1(basketball1.png as default)> --image2=<image2(basketball2.png as default)>\n'
-        'Press a key when image window is active to change algorithm or descriptor';
-}
-    
-    #ifndef OPENCV_LOGGER_DEFINES_HPP
-#define OPENCV_LOGGER_DEFINES_HPP
-    
-        // Changes the log level of the tags with matching name part according
-    // to the specified scope.
-    void setLevelByNamePart(const std::string& namePart, LogLevel level, MatchingScope scope);
-    
-    	return 'Image';
-}
-void ResourceImporterImage::get_recognized_extensions(List<String> *p_extensions) const {
-    
-    
-    {	rest_length = p_rest_length;
-	update();
-	if (get_joint().is_valid())
-		Physics2DServer::get_singleton()->damped_string_joint_set_param(get_joint(), Physics2DServer::DAMPED_STRING_REST_LENGTH, p_rest_length ? p_rest_length : length);
-}
-    
-    	String source_emission_file;
-    
-    	FileAccessNetworkClient *self = (FileAccessNetworkClient *)s;
-    
-    	void _send_rpc(Node *p_from, int p_to, bool p_unreliable, bool p_set, const StringName &p_name, const Variant **p_arg, int p_argcount);
-	bool _send_confirm_path(NodePath p_path, PathSentCache *psc, int p_target);
-    
-    	struct Skeleton : RID_Data {
-		bool use_2d;
-		int size;
-		Vector<float> skel_texture;
-		GLuint texture;
-		SelfList<Skeleton> update_list;
-		Set<RasterizerScene::InstanceBase *> instances; //instances using skeleton
-		Transform2D base_transform_2d;
-		bool use_world_transform;
-		Transform world_transform;
+            // out_of_range.109
+    try
+    {
+        // try to use an array index that is not a number
+        json::reference ref = j.at('/array/one'_json_pointer);
+    }
+    catch (json::parse_error& e)
+    {
+        std::cout << e.what() << '\n';
     }
     
-    	strings.push_back(fragment_code4.get_data());
-    
-    
-    {	connections->update_tree();
-}
-    
-    void ChannelArguments::SetMaxReceiveMessageSize(int size) {
-  SetInt(GRPC_ARG_MAX_RECEIVE_MESSAGE_LENGTH, size);
-}
-    
-    class ClientChannelStressTest {
- public:
-  void Run() {
-    Start();
-    // Keep updating resolution for the test duration.
-    gpr_log(GPR_INFO, 'Start updating resolution.');
-    const auto wait_duration =
-        std::chrono::milliseconds(kResolutionUpdateIntervalMs);
-    std::vector<AddressData> addresses;
-    auto start_time = std::chrono::steady_clock::now();
-    while (true) {
-      if (std::chrono::duration_cast<std::chrono::seconds>(
-              std::chrono::steady_clock::now() - start_time)
-              .count() > kTestDurationSec) {
-        break;
-      }
-      // Generate a random subset of balancers.
-      addresses.clear();
-      for (const auto& balancer_server : balancer_servers_) {
-        // Select each address with probability of 0.8.
-        if (std::rand() % 10 < 8) {
-          addresses.emplace_back(AddressData{balancer_server.port_, true, ''});
-        }
-      }
-      std::shuffle(addresses.begin(), addresses.end(),
-                   std::mt19937(std::random_device()()));
-      SetNextResolution(addresses);
-      std::this_thread::sleep_for(wait_duration);
-    }
-    gpr_log(GPR_INFO, 'Finish updating resolution.');
-    Shutdown();
-  }
-    }
-    
-    static grpc_iomgr_platform_vtable vtable = {
-    iomgr_platform_init,
-    iomgr_platform_flush,
-    iomgr_platform_shutdown,
-    iomgr_platform_shutdown_background_closure,
-    iomgr_platform_is_any_background_poller_thread,
-    iomgr_platform_add_closure_to_background_poller};
-    
-    #define GPR_GLOBAL_CONFIG_DEFINE_INT32(name, default_value, help)         \
-  static char g_env_str_##name[] = #name;                                 \
-  static ::grpc_core::GlobalConfigEnvInt32 g_env_##name(g_env_str_##name, \
-                                                        default_value);   \
-  int32_t gpr_global_config_get_##name() { return g_env_##name.Get(); }   \
-  void gpr_global_config_set_##name(int32_t value) { g_env_##name.Set(value); }
-    
-    
-    {  gpr_setenv(name, value);
-  gpr_unsetenv(name);
-  retrieved_value = gpr_getenv(name);
-  GPR_ASSERT(retrieved_value == nullptr);
-}
-    
-    
-    {  GPR_ASSERT(nullptr == gpr_memrchr(nullptr, 'a', 0));
-  GPR_ASSERT(nullptr == gpr_memrchr('', 'a', 0));
-  GPR_ASSERT(nullptr == gpr_memrchr('hello', 'b', 5));
-  GPR_ASSERT(0 == strcmp((const char*)gpr_memrchr('hello', 'h', 5), 'hello'));
-  GPR_ASSERT(0 == strcmp((const char*)gpr_memrchr('hello', 'o', 5), 'o'));
-  GPR_ASSERT(0 == strcmp((const char*)gpr_memrchr('hello', 'l', 5), 'lo'));
-}
-    
-    TEST_F(GlobalConfigEnvTest, Int32WithEnvTest) {
-  const char* int32_var_name = 'INT32_VAR';
-    }
-    
-    
-    {}  // namespace grpc
-    
-    
-    {  // The default service has a size limit of the service name.
-  const grpc::string kTooLongServiceName(201, 'x');
-  SendHealthCheckRpc(kTooLongServiceName,
-                     Status(StatusCode::INVALID_ARGUMENT, ''));
-}
-    
-        EXPECT_FALSE(cstream->Read(&response));
-    EXPECT_EQ(request.message(), exp);
-    
-    namespace grpc {
-namespace testing {
+    #ifndef NDEBUG  // sync point is not included with DNDEBUG build
+TEST_F(DBTestXactLogIterator, TransactionLogIteratorRace) {
+  static const int LOG_ITERATOR_RACE_TEST_COUNT = 2;
+  static const char* sync_points[LOG_ITERATOR_RACE_TEST_COUNT][4] = {
+      {'WalManager::GetSortedWalFiles:1',  'WalManager::PurgeObsoleteFiles:1',
+       'WalManager::PurgeObsoleteFiles:2', 'WalManager::GetSortedWalFiles:2'},
+      {'WalManager::GetSortedWalsOfType:1',
+       'WalManager::PurgeObsoleteFiles:1',
+       'WalManager::PurgeObsoleteFiles:2',
+       'WalManager::GetSortedWalsOfType:2'}};
+  for (int test = 0; test < LOG_ITERATOR_RACE_TEST_COUNT; ++test) {
+    // Setup sync point dependency to reproduce the race condition of
+    // a log file moved to archived dir, in the middle of GetSortedWalFiles
+    rocksdb::SyncPoint::GetInstance()->LoadDependency(
+      { { sync_points[test][0], sync_points[test][1] },
+        { sync_points[test][2], sync_points[test][3] },
+      });
     }
     }
     
-     private:
-  void GotTag(void* got_tag, bool ok, bool ignore_ok) {
-    auto it = expectations_.find(got_tag);
-    if (it != expectations_.end()) {
-      if (!ignore_ok) {
-        EXPECT_EQ(it->second, ok);
-      }
-      expectations_.erase(it);
-    }
-  }
+    const SliceTransform* NewNoopTransform() {
+  return new NoopTransform;
+}
     
-    // Force AddServerBuilderPlugin() to be called at static initialization time.
-struct StaticTestPluginInitializer {
-  StaticTestPluginInitializer() { AddTestServerBuilderPlugin(); }
-} static_plugin_initializer_test_;
-    
-    TableCache::TableCache(const std::string& dbname, const Options& options,
-                       int entries)
-    : env_(options.env),
-      dbname_(dbname),
-      options_(options),
-      cache_(NewLRUCache(entries)) {}
-    
-    
-    {}  // namespace leveldb
-    
-    Slice FilterBlockBuilder::Finish() {
-  if (!start_.empty()) {
-    GenerateFilter();
-  }
+    namespace {
     }
     
-      // Iterator interface methods
-  bool Valid() const { return valid_; }
-  Slice key() const {
-    assert(Valid());
-    return key_;
-  }
-  Slice value() const {
-    assert(Valid());
-    return iter_->value();
-  }
-  // Methods below require iter() != nullptr
-  Status status() const {
-    assert(iter_);
-    return iter_->status();
-  }
-  void Next() {
-    assert(iter_);
-    iter_->Next();
-    Update();
-  }
-  void Prev() {
-    assert(iter_);
-    iter_->Prev();
-    Update();
-  }
-  void Seek(const Slice& k) {
-    assert(iter_);
-    iter_->Seek(k);
-    Update();
-  }
-  void SeekToFirst() {
-    assert(iter_);
-    iter_->SeekToFirst();
-    Update();
-  }
-  void SeekToLast() {
-    assert(iter_);
-    iter_->SeekToLast();
-    Update();
-  }
+      if (timeout_time < 0) {
+    // If timeout is negative, do not use a timeout
+    cv_.wait(lock);
+  } else {
+    auto duration = std::chrono::microseconds(timeout_time);
+    auto cv_status = cv_.wait_for(lock, duration);
+    }
     
-        // add values
-    auto res1 = object.emplace('three', 3);
-    null.emplace('A', 'a');
-    null.emplace('B', 'b');
+      const Snapshot* snapshot = txn->GetSnapshot();
+    
+    // Move all L0 files to target_level skipping compaction.
+// This operation succeeds only if the files in L0 have disjoint ranges; this
+// is guaranteed to happen, for instance, if keys are inserted in sorted
+// order. Furthermore, all levels between 1 and target_level must be empty.
+// If any of the above condition is violated, InvalidArgument will be
+// returned.
+Status PromoteL0(DB* db, ColumnFamilyHandle* column_family,
+                 int target_level = 1);
+    
+    namespace rocksdb {
+    }
+    
+    
+    {
+    {  // Returns the approximate memory usage of different types in the input
+  // list of DBs and Cache set.  For instance, in the output map
+  // usage_by_type, usage_by_type[kMemTableTotal] will store the memory
+  // usage of all the mem-tables from all the input rocksdb instances.
+  //
+  // Note that for memory usage inside Cache class, we will
+  // only report the usage of the input 'cache_set' without
+  // including those Cache usage inside the input list 'dbs'
+  // of DBs.
+  static Status GetApproximateMemoryUsageByType(
+      const std::vector<DB*>& dbs,
+      const std::unordered_set<const Cache*> cache_set,
+      std::map<MemoryUtil::UsageType, uint64_t>* usage_by_type);
+};
+}  // namespace rocksdb
+#endif  // !ROCKSDB_LITE
