9fa70a90f186898cf0fe2b010b9759a250a05278
SUNsung <weatherius@gmail.com>
2019-09-19 15:30:48 +0300

    Rewrite [33joLooaStRnI33mKCQen0tIek33ZxmhPCSbM.o330bG.cpp] File in: 2019-09-19 15:30:47

--- cccccc/33joLooaStRnI33mKCQen0tIek33ZxmhPCSbM.o330bG.cpp
+++ cccccc/33joLooaStRnI33mKCQen0tIek33ZxmhPCSbM.o330bG.cpp
@@ -1,435 +1,81 @@
 
         
-            QString getURI();
+          // TODO(erg): Mozilla's implementation has a workaround for GDBus
+  // cancellation here. However, it's marked as fixed. If there's weird
+  // problems with cancelation, look at how they fixed their issues.
     
-    #ifndef BITCOIN_REVERSELOCK_H
-#define BITCOIN_REVERSELOCK_H
-    
-    SECP256K1_INLINE static void secp256k1_fe_mul_inner(uint64_t *r, const uint64_t *a, const uint64_t * SECP256K1_RESTRICT b) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            r15:rcx = d
- *            r10-r14 = a0-a4
- *            rbx     = b
- *            rdi     = r
- *            rsi     = a / t?
- */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
+    LRESULT CALLBACK PowerMonitor::WndProc(HWND hwnd,
+                                       UINT message,
+                                       WPARAM wparam,
+                                       LPARAM lparam) {
+  if (message == WM_WTSSESSION_CHANGE) {
+    if (wparam == WTS_SESSION_LOCK) {
+      Emit('lock-screen');
+    } else if (wparam == WTS_SESSION_UNLOCK) {
+      Emit('unlock-screen');
     }
-    
-        /* Check bad contexts and NULLs for recovery */
-    ecount = 0;
-    CHECK(secp256k1_ecdsa_recover(none, &recpubkey, &recsig, message) == 0);
-    CHECK(ecount == 1);
-    CHECK(secp256k1_ecdsa_recover(sign, &recpubkey, &recsig, message) == 0);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(vrfy, &recpubkey, &recsig, message) == 1);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, message) == 1);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(both, NULL, &recsig, message) == 0);
-    CHECK(ecount == 3);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, NULL, message) == 0);
-    CHECK(ecount == 4);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, NULL) == 0);
-    CHECK(ecount == 5);
-    
-    #ifndef BITCOIN_CRYPTO_RIPEMD160_H
-#define BITCOIN_CRYPTO_RIPEMD160_H
-    
-    ROTATE_ARGS
-	movdqa	XTMP3, XTMP2	; XTMP3 = W[-2] {BBAA}
-    mov	y0, e		; y0 = e
-    mov	y1, a		; y1 = a
-    ror	y0, (25-11)	; y0 = e >> (25-11)
-	movdqa	XTMP4, XTMP2	; XTMP4 = W[-2] {BBAA}
-    xor	y0, e		; y0 = e ^ (e >> (25-11))
-    ror	y1, (22-13)	; y1 = a >> (22-13)
-    mov	y2, f		; y2 = f
-    xor	y1, a		; y1 = a ^ (a >> (22-13)
-    ror	y0, (11-6)	; y0 = (e >> (11-6)) ^ (e >> (25-6))
-	psrlq	XTMP2, 17	; XTMP2 = W[-2] ror 17 {xBxA}
-    xor	y2, g		; y2 = f^g
-	psrlq	XTMP3, 19	; XTMP3 = W[-2] ror 19 {xBxA}
-    xor	y0, e		; y0 = e ^ (e >> (11-6)) ^ (e >> (25-6))
-    and	y2, e		; y2 = (f^g)&e
-	psrld	XTMP4, 10	; XTMP4 = W[-2] >> 10 {BBAA}
-    ror	y1, (13-2)	; y1 = (a >> (13-2)) ^ (a >> (22-2))
-    xor	y1, a		; y1 = a ^ (a >> (13-2)) ^ (a >> (22-2))
-    xor	y2, g		; y2 = CH = ((f^g)&e)^g
-    ror	y0, 6		; y0 = S1 = (e>>6) & (e>>11) ^ (e>>25)
-	pxor	XTMP2, XTMP3
-    add	y2, y0		; y2 = S1 + CH
-    ror	y1, 2		; y1 = S0 = (a>>2) ^ (a>>13) ^ (a>>22)
-    add	y2, [rsp + _XFER + 2*4]	; y2 = k + w + S1 + CH
-	pxor	XTMP4, XTMP2	; XTMP4 = s1 {xBxA}
-    mov	y0, a		; y0 = a
-    add	h, y2		; h = h + S1 + CH + k + w
-    mov	y2, a		; y2 = a
-	pshufb	XTMP4, SHUF_00BA	; XTMP4 = s1 {00BA}
-    or	y0, c		; y0 = a|c
-    add	d, h		; d = d + h + S1 + CH + k + w
-    and	y2, c		; y2 = a&c
-	paddd	XTMP0, XTMP4	; XTMP0 = {..., ..., W[1], W[0]}
-    and	y0, b		; y0 = (a|c)&b
-    add	h, y1		; h = h + S1 + CH + k + w + S0
-	;; compute high s1
-	pshufd	XTMP2, XTMP0, 01010000b	; XTMP2 = W[-2] {DDCC}
-    or	y0, y2		; y0 = MAJ = (a|c)&b)|(a&c)
-    add	h, y0		; h = h + S1 + CH + k + w + S0 + MAJ
-    
-    
-    {}  // namespace
-    
-    std::string DescriptorFileName(const std::string& dbname, uint64_t number) {
-  assert(number > 0);
-  char buf[100];
-  snprintf(buf, sizeof(buf), '/MANIFEST-%06llu',
-           static_cast<unsigned long long>(number));
-  return dbname + buf;
-}
-    
-      fname = LogFileName('foo', 192);
-  ASSERT_EQ('foo/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(192, number);
-  ASSERT_EQ(kLogFile, type);
-    
-    #include 'content/nw/src/api/api_messages.h'
-#include 'content/nw/src/api/dispatcher_host.h'
-#include 'content/nw/src/api/shortcut/global_shortcut_listener.h'
-#include 'content/nw/src/api/shortcut/shortcut.h'
-#include 'content/nw/src/breakpad_linux.h'
-#include 'content/nw/src/browser/native_window.h'
-#include 'content/nw/src/browser/net_disk_cache_remover.h'
-#include 'content/nw/src/nw_package.h'
-#include 'content/nw/src/nw_shell.h'
-#include 'content/nw/src/shell_browser_context.h'
-#include 'content/common/view_messages.h'
-#include 'content/public/browser/browser_thread.h'
-#include 'content/public/browser/web_contents.h'
-#include 'content/public/browser/render_process_host.h'
-#include 'net/proxy/proxy_config.h'
-#include 'net/proxy/proxy_config_service_fixed.h'
-#include 'net/proxy/proxy_service.h'
-#include 'net/url_request/url_request_context.h'
-#include 'net/url_request/url_request_context_getter.h'
-    
-    bool MenuDelegate::IsCommandIdEnabled(int command_id) const {
-  if (command_id < 0)
-    return false;
-    }
-    
-    
-    {}  // namespace nw
-
-    
-    
-    {  gfx::Image originImage;
-  nw::Package* package = nw::InitNWPackage();
-  if (nw::GetImage(package, base::FilePath::FromUTF8Unsafe(icon), &originImage)) {
-    const gfx::ImageSkia* originImageSkia = originImage.ToImageSkia();
-    gfx::ImageSkia resizedImageSkia = gfx::ImageSkiaOperations::CreateResizedImage(*originImageSkia,
-                                                                                   skia::ImageOperations::RESIZE_GOOD,
-                                                                                   gfx::Size(kIconWidth, kIconHeight));
-    icon_ = gfx::Image(resizedImageSkia);
   }
+  return ::DefWindowProc(hwnd, message, wparam, lparam);
 }
     
-    /****************************************************************************\
-*
-* DigitGroupingStringToGroupingVector
-*
-* Description:
-*   This will take the digit grouping string found in the regional applet and
-*   represent this string as a vector.
-*
-*   groupingString
-*   0;0      - no grouping
-*   3;0      - group every 3 digits
-*   3        - group 1st 3, then no grouping after
-*   3;0;0    - group 1st 3, then no grouping after
-*   3;2;0    - group 1st 3 and then every 2 digits
-*   4;0      - group every 4 digits
-*   5;3;2;0  - group 5, then 3, then every 2
-*   5;3;2    - group 5, then 3, then 2, then no grouping after
-*
-* Returns: the groupings as a vector
-*
-\****************************************************************************/
-vector<uint32_t> CCalcEngine::DigitGroupingStringToGroupingVector(wstring_view groupingString)
-{
-    vector<uint32_t> grouping{};
-    uint32_t currentGroup = 0;
-    wchar_t* next = nullptr;
-    for (const wchar_t* itr = groupingString.data(); *itr != L'\0'; ++itr)
-    {
-        // Try to parse a grouping number from the string
-        currentGroup = wcstoul(itr, &next, 10);
-    }
-    }
-    
-    // Arithmetic expression evaluator error strings
-inline constexpr auto IDS_ERR_UNK_CH = CSTRINGSENGMAX + 1;
-inline constexpr auto IDS_ERR_UNK_FN = CSTRINGSENGMAX + 2;
-inline constexpr auto IDS_ERR_UNEX_NUM = CSTRINGSENGMAX + 3;
-inline constexpr auto IDS_ERR_UNEX_CH = CSTRINGSENGMAX + 4;
-inline constexpr auto IDS_ERR_UNEX_SZ = CSTRINGSENGMAX + 5;
-inline constexpr auto IDS_ERR_MISMATCH_CLOSE = CSTRINGSENGMAX + 6;
-inline constexpr auto IDS_ERR_UNEX_END = CSTRINGSENGMAX + 7;
-inline constexpr auto IDS_ERR_SG_INV_ERROR = CSTRINGSENGMAX + 8;
-inline constexpr auto IDS_ERR_INPUT_OVERFLOW = CSTRINGSENGMAX + 9;
-inline constexpr auto IDS_ERR_OUTPUT_OVERFLOW = CSTRINGSENGMAX + 10;
-    
-    
-    {
-    {        return Calc_ULongLongToULong(ull64Result, pulResult);
-    }
+    void View::AddChildViewAt(mate::Handle<View> child, size_t index) {
+  if (index > child_views_.size())
+    return;
+  child_views_.emplace(child_views_.begin() + index,     // index
+                       isolate(), child->GetWrapper());  // v8::Global(args...)
+  view()->AddChildViewAt(child->view(), index);
 }
-    
-    extern PNUMBER i32factnum(int32_t ini32, uint32_t radix);
-extern PNUMBER i32prodnum(int32_t start, int32_t stop, uint32_t radix);
-extern PNUMBER i32tonum(int32_t ini32, uint32_t radix);
-extern PNUMBER Ui32tonum(uint32_t ini32, uint32_t radix);
-extern PNUMBER numtonRadixx(PNUMBER a, uint32_t radix);
-    
-    #pragma once
-    
-    //////////////////////////////////////////////////
-//
-// CCalcEngine::CCalcEngine
-//
-//////////////////////////////////////////////////
-CCalcEngine::CCalcEngine(
-    bool fPrecedence,
-    bool fIntegerMode,
-    CalculationManager::IResourceProvider* const pResourceProvider,
-    __in_opt ICalcDisplay* pCalcDisplay,
-    __in_opt shared_ptr<IHistoryDisplay> pHistoryDisplay)
-    : m_fPrecedence(fPrecedence)
-    , m_fIntegerMode(fIntegerMode)
-    , m_pCalcDisplay(pCalcDisplay)
-    , m_resourceProvider(pResourceProvider)
-    , m_nOpCode(0)
-    , m_nPrevOpCode(0)
-    , m_bChangeOp(false)
-    , m_bRecord(false)
-    , m_bSetCalcState(false)
-    , m_input(DEFAULT_DEC_SEPARATOR)
-    , m_nFE(FMT_FLOAT)
-    , m_memoryValue{ make_unique<Rational>() }
-    , m_holdVal{}
-    , m_currentVal{}
-    , m_lastVal{}
-    , m_parenVals{}
-    , m_precedenceVals{}
-    , m_bError(false)
-    , m_bInv(false)
-    , m_bNoPrevEqu(true)
-    , m_radix(DEFAULT_RADIX)
-    , m_precision(DEFAULT_PRECISION)
-    , m_cIntDigitsSav(DEFAULT_MAX_DIGITS)
-    , m_decGrouping()
-    , m_numberString(DEFAULT_NUMBER_STR)
-    , m_nTempCom(0)
-    , m_openParenCount(0)
-    , m_nOp()
-    , m_nPrecOp()
-    , m_precedenceOpCount(0)
-    , m_nLastCom(0)
-    , m_angletype(ANGLE_DEG)
-    , m_numwidth(QWORD_WIDTH)
-    , m_HistoryCollector(pCalcDisplay, pHistoryDisplay, DEFAULT_DEC_SEPARATOR)
-    , m_groupSeparator(DEFAULT_GRP_SEPARATOR)
-{
-    InitChopNumbers();
-    }
-    
-    #endif /* __glu_h__ */
-
-    
-      // End a page.
-  virtual void endPage();
-    
-      // Get duration, the maximum length of time, in seconds,
-  // that the page is displayed before the presentation automatically
-  // advances to the next page
-  double getDuration() { return duration; }
-    
-    // if changed remember to keep in sync with frontend enums
-enum PageTransitionDirection {
-  transitionInward = 0,
-  transitionOutward
-};
-    
-    Object *Parser::getObj(Object *obj, Guchar *fileKey,
-           CryptAlgorithm encAlgorithm, int keyLength,
-           int objNum, int objGen) {
-  std::set<int> fetchOriginatorNums;
-  return getObj(obj, fileKey, encAlgorithm, keyLength, objNum, objGen, &fetchOriginatorNums);
-}
-    
-    #ifndef PARSER_H
-#define PARSER_H
-    
-    int PopplerCache::size()
-{
-  return cacheSize;
-}
-    
-      // Returns true if the operations performed since the last call to
-  // clearStats() included any transparency.
-  GBool usesTransparency() { return transparency; }
-    
-    
-    
-    
-    {  if (readAttrs)
-  {
-    Object tmp;
-    Dict *dict = streamObj->getStream()->getDict();
-    dict->lookup('F', &tmp);
-    if (!tmp.isNull()) {
-      Object obj1;
-      // valid 'F' key -> external file
-      kind = soundExternal;
-      if (getFileSpecNameForPlatform (&tmp, &obj1)) {
-        fileName = obj1.getString()->copy();
-        obj1.free();
-      }
-    } else {
-      // no file specification, then the sound data have to be
-      // extracted from the stream
-      kind = soundEmbedded;
-    }
-    tmp.free();
-    // sampling rate
-    dict->lookup('R', &tmp);
-    if (tmp.isNum()) {
-      samplingRate = tmp.getNum();
-    }
-    tmp.free();
-    // sound channels
-    dict->lookup('C', &tmp);
-    if (tmp.isInt()) {
-      channels = tmp.getInt();
-    }
-    tmp.free();
-    // bits per sample
-    dict->lookup('B', &tmp);
-    if (tmp.isInt()) {
-      bitsPerSample = tmp.getInt();
-    }
-    tmp.free();
-    // encoding format
-    dict->lookup('E', &tmp);
-    if (tmp.isName())
-    {
-      const char *enc = tmp.getName();
-      if (strcmp('Raw', enc) == 0) {
-        encoding = soundRaw;
-      } else if (strcmp('Signed', enc) == 0) {
-        encoding = soundSigned;
-      } else if (strcmp('muLaw', enc) == 0) {
-        encoding = soundMuLaw;
-      } else if (strcmp('ALaw', enc) == 0) {
-        encoding = soundALaw;
-      }
-    }
-    tmp.free();
-  }
-}
-    
-      // push a new stack entry
-  transpGroup = new SplashTransparencyGroup();
-  transpGroup->tx = tx;
-  transpGroup->ty = ty;
-  transpGroup->blendingColorSpace = blendingColorSpace;
-  transpGroup->isolated = isolated;
-  transpGroup->next = transpGroupStack;
-  transpGroupStack = transpGroup;
-    
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-        // Select Present Mode
-#ifdef IMGUI_UNLIMITED_FRAME_RATE
-    VkPresentModeKHR present_modes[] = { VK_PRESENT_MODE_MAILBOX_KHR, VK_PRESENT_MODE_IMMEDIATE_KHR, VK_PRESENT_MODE_FIFO_KHR };
-#else
-    VkPresentModeKHR present_modes[] = { VK_PRESENT_MODE_FIFO_KHR };
 #endif
-    wd->PresentMode = ImGui_ImplVulkanH_SelectPresentMode(g_PhysicalDevice, wd->Surface, &present_modes[0], IM_ARRAYSIZE(present_modes));
-    //printf('[vulkan] Selected PresentMode = %d\n', wd->PresentMode);
     
-    // Implemented features:
-//  [X] Platform: Clipboard support.
-//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad'.
-//  [x] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'. FIXME: 3 cursors types are missing from GLFW.
-//  [X] Platform: Keyboard arrays indexed using GLFW_KEY_* codes, e.g. ImGui::IsKeyPressed(GLFW_KEY_SPACE).
-    
-            // Process all the config files in order, and then add on the inital config,
-        // which will override run and load if they exist
-        std::string fileContents = m_script.ReadConfigFiles(filePaths);
-        fileContents += m_initialConfig;
-    
-        // CheckName - check for a name in our symbols, see if it exists
-    // name - name we are looking for
-    // localOnly - only look in the current scope, and not the global scope
-    // if it does exist return the node that represents the name
-    NDLNode<ElemType>* CheckName(const std::string& name, bool localOnly = false)
-    {
-        // first try local script
-        auto found = FindSymbol(name);
-        if (found != NULL)
-        {
-            return found;
-        }
+    namespace api {
     }
     
-            //                input = builder.CreateSparseInputNode(L'features', m_layerSizes[0]);
-        input = builder.CreateInputNode(L'features', m_layerSizes[0]);
-        featin = input;
-        m_net->AddToNodeGroup(L'feature', input);
+      void visitInitExistentialAddrInst(InitExistentialAddrInst *IEAI) {
+    SGM.useConformancesFromType(IEAI->getFormalConcreteType());
+    SGM.useConformancesFromObjectiveCType(IEAI->getFormalConcreteType());
+    for (auto conformance : IEAI->getConformances())
+      SGM.useConformance(conformance);
+  }
     
-    extern 'C' EVAL_API void  GetEvalExtendedF(IEvaluateModelExtended<float>** peval)
-{
-    GetEvalExtended(peval);
-}
-extern 'C' EVAL_API void GetEvalExtendedD(IEvaluateModelExtended<double>** peval)
-{
-    GetEvalExtended(peval);
-}
-    
-        wstring GetModelNameForEpoch(const int epoch, bool bLastModel = false) const;
-    
-    
-    {    // After values of accumulators have been aggregated across nodes, we have to update evaluation results for
-    // evaluation nodes that accumulate results.
-    UpdateEpochEvaluationForAccumulatedResult<ElemType>(epochEvalErrors, evaluationNodes, localEpochEvalErrors,
-                                                        containsAccumulatedResult);
-}
-    
-    
-    {        return err;
+    /// SILProfiler - Maps AST nodes to profile counters.
+class SILProfiler : public SILAllocated<SILProfiler> {
+private:
+  SILModule &M;
     }
     
-            virtual ~GuiAdam();
+    TEST(ClusteredBitVector, AppendEmptyAfterAdd) {
+  ClusteredBitVector vec;
+  vec.add(31, 2133572605);
+    }
     
-        private:
-        const float mConfidenceThreshold;
-        const float mInlierRatioThreshold;
-        const float mDistanceThreshold;
-        const int mNumberFramesToDeletePerson;
-        long long mNextPersonId;
-        cv::Mat mImagePrevious;
-        std::vector<cv::Mat> mPyramidImagesPrevious;
-        std::unordered_map<int, PersonEntry> mPersonEntries;
-        // Thread-safe variables
-        std::atomic<long long> mLastFrameId;
+    SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_API
+uint64_t swift_float80ToString(char *Buffer, size_t BufferLength,
+                               long double Value, bool Debug) {
+#if SWIFT_DTOA_FLOAT80_SUPPORT
+  return swift_format_float80(Value, Buffer, BufferLength);
+#else
+  // Use this when 'long double' is not true Float80
+  return swift_floatingPointToString<long double>(Buffer, BufferLength, Value,
+                                                  '%0.*Lg', Debug);
+#endif
+    }
+    
+    class GenericRequirementFlags {
+  uint32_t Value;
+  
+  explicit constexpr GenericRequirementFlags(uint32_t Value)
+    : Value(Value) {}
+public:
+  constexpr GenericRequirementFlags(GenericRequirementKind kind,
+                                    bool hasKeyArgument,
+                                    bool hasExtraArgument)
+    : GenericRequirementFlags(GenericRequirementFlags(0)
+                         .withKind(kind)
+                         .withKeyArgument(hasKeyArgument)
+                         .withExtraArgument(hasExtraArgument))
+  {}
+  
+  constexpr bool hasKeyArgument() const {
+    return (Value & 0x80u) != 0;
+  }
+    }
