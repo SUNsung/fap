84aaf5d5a9d729702051c051f25349f2db63e89d
SUNsung <weatherius@gmail.com>
2019-09-20 00:53:25 +0300

    Rewrite [56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp] File in: 2019-09-20 00:53:25

--- cccccc/56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp
+++ cccccc/56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp
@@ -1,136 +1,356 @@
 
         
-        Base::~Base() {
-}
+        #ifndef BITCOIN_QT_OPENURIDIALOG_H
+#define BITCOIN_QT_OPENURIDIALOG_H
     
-    void Clipboard::Call(const std::string& method,
-                     const base::ListValue& arguments) {
-  if (method == 'Set') {
-    std::string text, type;
-    arguments.GetString(0, &text);
-    arguments.GetString(1, &type);
-    SetText(text);
-  } else if (method == 'Clear') {
-    Clear();
-  } else {
-    NOTREACHED() << 'Invalid call to Clipboard method:' << method
-                 << ' arguments:' << arguments;
+    #ifndef BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
+#define BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
+    
+    #endif // BITCOIN_CRYPTO_RIPEMD160_H
+
+    
+    namespace sha256_sse4
+{
+void Transform(uint32_t* s, const unsigned char* chunk, size_t blocks)
+{
+    static const uint32_t K256 alignas(16) [] = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+        0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+        0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+    };
+    static const uint32_t FLIP_MASK alignas(16) [] = {0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f};
+    static const uint32_t SHUF_00BA alignas(16) [] = {0x03020100, 0x0b0a0908, 0xffffffff, 0xffffffff};
+    static const uint32_t SHUF_DC00 alignas(16) [] = {0xffffffff, 0xffffffff, 0x03020100, 0x0b0a0908};
+    uint32_t a, b, c, d, f, g, h, y0, y1, y2;
+    uint64_t tbl;
+    uint64_t inp_end, inp;
+    uint32_t xfer alignas(16) [4];
+    }
+    }
+    
+        // Make files spanning the following ranges in level-0:
+    //  files[0]  200 .. 900
+    //  files[1]  300 .. 500
+    // Note that files are sorted by smallest key.
+    ASSERT_OK(Put('300', 'v300'));
+    ASSERT_OK(Put('500', 'v500'));
+    dbfull()->TEST_CompactMemTable();
+    ASSERT_OK(Put('200', 'v200'));
+    ASSERT_OK(Put('600', 'v600'));
+    ASSERT_OK(Put('900', 'v900'));
+    dbfull()->TEST_CompactMemTable();
+    ASSERT_EQ('2,1,1', FilesPerLevel());
+    
+    // Return the name of the sstable with the specified number
+// in the db named by 'dbname'.  The result will be prefixed with
+// 'dbname'.
+extern std::string TableFileName(const std::string& dbname, uint64_t number);
+    
+      // Successful parses
+  static struct {
+    const char* fname;
+    uint64_t number;
+    FileType type;
+  } cases[] = {
+    { '100.log',            100,   kLogFile },
+    { '0.log',              0,     kLogFile },
+    { '0.sst',              0,     kTableFile },
+    { '0.ldb',              0,     kTableFile },
+    { 'CURRENT',            0,     kCurrentFile },
+    { 'LOCK',               0,     kDBLockFile },
+    { 'MANIFEST-2',         2,     kDescriptorFile },
+    { 'MANIFEST-7',         7,     kDescriptorFile },
+    { 'LOG',                0,     kInfoLogFile },
+    { 'LOG.old',            0,     kInfoLogFile },
+    { '18446744073709551615.log', 18446744073709551615ull, kLogFile },
+  };
+  for (int i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
+    std::string f = cases[i].fname;
+    ASSERT_TRUE(ParseFileName(f, &number, &type)) << f;
+    ASSERT_EQ(cases[i].type, type) << f;
+    ASSERT_EQ(cases[i].number, number) << f;
   }
-}
     
-    EventListener::EventListener(int id,
-  const base::WeakPtr<DispatcherHost>& dispatcher_host,
-  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    bool Reader::ReadRecord(Slice* record, std::string* scratch) {
+  if (last_record_offset_ < initial_offset_) {
+    if (!SkipToInitialBlock()) {
+      return false;
+    }
+  }
     }
     
-    class BaseEvent {
-  friend class EventListener;
-  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
-    }
     
-    void Menu::Call(const std::string& method,
-                const base::ListValue& arguments,
-                content::RenderFrameHost* rvh) {
-  if (method == 'Append') {
-    int object_id = 0;
-    arguments.GetInteger(0, &object_id);
-    Append(object_manager()->GetApiObject<MenuItem>(object_id));
-  } else if (method == 'Insert') {
-    int object_id = 0;
-    arguments.GetInteger(0, &object_id);
-    int pos = 0;
-    arguments.GetInteger(1, &pos);
-    Insert(object_manager()->GetApiObject<MenuItem>(object_id), pos);
-  } else if (method == 'Remove') {
-    int object_id = 0;
-    arguments.GetInteger(0, &object_id);
-    int pos = 0;
-    arguments.GetInteger(1, &pos);
-    Remove(object_manager()->GetApiObject<MenuItem>(object_id), pos);
-  } else if (method == 'Popup') {
-    int x = 0;
-    arguments.GetInteger(0, &x);
-    int y = 0;
-    arguments.GetInteger(1, &y);
-    content::WebContents* web_contents = content::WebContents::FromRenderFrameHost(rvh);
-    DCHECK(web_contents);
-    zoom::ZoomController* zoom_controller = zoom::ZoomController::FromWebContents(web_contents);
-    }
-    }
-    
-    namespace extensions {
-    }
-    
-      // implement nw.Screen.isMonitorStarted()
-  class NwScreenIsMonitorStartedFunction : public NWSyncExtensionFunction {
-  public:
-    NwScreenIsMonitorStartedFunction();
-    bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-        void operator()(const changefeed_point_stamp_t &s) {
-        // Need to wait for the superblock to make sure we get the right changefeed
-        // stamp.
-        superblock->get()->read_acq_signal()->wait_lazily_unordered();
-    }
-    
-    #if GTEST_HAS_DEATH_TEST
-    
-    // The Message class works like an ostream repeater.
-//
-// Typical usage:
-//
-//   1. You stream a bunch of values to a Message object.
-//      It will remember the text in a stringstream.
-//   2. Then you stream the Message object to an ostream.
-//      This causes the text in the Message to be streamed
-//      to the ostream.
-//
-// For example;
-//
-//   testing::Message foo;
-//   foo << 1 << ' != ' << 2;
-//   std::cout << foo;
-//
-// will print '1 != 2'.
-//
-// Message is not intended to be inherited from.  In particular, its
-// destructor is not virtual.
-//
-// Note that stringstream behaves differently in gcc and in MSVC.  You
-// can stream a NULL char pointer to it in the former, but not in the
-// latter (it causes an access violation if you do).  The Message
-// class hides this difference by treating a NULL char pointer as
-// '(null)'.
-class GTEST_API_ Message {
- private:
-  // The type of basic IO manipulators (endl, ends, and flush) for
-  // narrow streams.
-  typedef std::ostream& (*BasicNarrowIoManip)(std::ostream&);
-    }
-    
-      template <typename Tuple>
-  static void TersePrintPrefixToStrings(const Tuple&, Strings*) {}
+    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
 };
-// We have to specialize the entire TuplePrefixPrinter<> class
-// template here, even though the definition of
-// TersePrintPrefixToStrings() is the same as the generic version, as
-// Embarcadero (formerly CodeGear, formerly Borland) C++ doesn't
-// support specializing a method template of a class template.
-template <>
-struct TuplePrefixPrinter<1> {
-  template <typename Tuple>
-  static void PrintPrefixTo(const Tuple& t, ::std::ostream* os) {
-    UniversalPrinter<typename ::std::tr1::tuple_element<0, Tuple>::type>::
-        Print(::std::tr1::get<0>(t), os);
+    
+    void Menu::RemoveKeys() {
+  if (!focus_manager_) return;
+    }
+    
+    MenuItem::MenuItem(int id,
+                   const base::WeakPtr<ObjectManager>& object_manager,
+                   const base::DictionaryValue& option,
+                   const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id) {
+  Create(option);
+}
+    
+        perfmon_collection_t perfmon_collection;
+    // Mind the constructor ordering. We must destruct the cache and btree
+    // before we destruct perfmon_collection
+    scoped_ptr_t<cache_t> cache;
+    scoped_ptr_t<cache_conn_t> general_cache_conn;
+    scoped_ptr_t<btree_slice_t> btree;
+    io_backender_t *io_backender_;
+    base_path_t base_path_;
+    perfmon_membership_t perfmon_collection_membership;
+    scoped_ptr_t<store_metainfo_manager_t> metainfo;
+    
+    // Then, use TYPED_TEST() instead of TEST_F() to define as many typed
+// tests for this test case as you want.
+TYPED_TEST(FooTest, DoesBlah) {
+  // Inside a test, refer to TypeParam to get the type parameter.
+  // Since we are inside a derived class template, C++ requires use to
+  // visit the members of FooTest via 'this'.
+  TypeParam n = this->value_;
+    }
+    
+    
+    {  return EqFailure(expected_expression,
+                   actual_expression,
+                   FormatForComparisonFailureMessage(expected, actual),
+                   FormatForComparisonFailureMessage(actual, expected),
+                   false);
+}
+    
+    
+    {  const T1 v1_;
+  const T2 v2_;
+  const T3 v3_;
+  const T4 v4_;
+  const T5 v5_;
+  const T6 v6_;
+  const T7 v7_;
+  const T8 v8_;
+  const T9 v9_;
+  const T10 v10_;
+  const T11 v11_;
+  const T12 v12_;
+  const T13 v13_;
+  const T14 v14_;
+  const T15 v15_;
+  const T16 v16_;
+  const T17 v17_;
+  const T18 v18_;
+  const T19 v19_;
+  const T20 v20_;
+  const T21 v21_;
+  const T22 v22_;
+  const T23 v23_;
+  const T24 v24_;
+  const T25 v25_;
+  const T26 v26_;
+  const T27 v27_;
+  const T28 v28_;
+};
+    
+      tuple& operator=(const tuple& t) { return CopyFrom(t); }
+    
+            friend Rational operator&(Rational lhs, Rational const& rhs);
+        friend Rational operator|(Rational lhs, Rational const& rhs);
+        friend Rational operator^(Rational lhs, Rational const& rhs);
+    
+    
+    {    Microsoft::VisualStudio::TestPlatform::TestExecutor::WinRTCore::UnitTestClient::Run(e->Arguments);
+}
+    
+        bool operator>=(Rational const& lhs, Rational const& rhs)
+    {
+        return !(lhs < rhs);
+    }
+    
+            iCommandEnd = AddCommand(spExpressionCommand);
+    
+    //////////////////////////////////////////////////
+//
+// CCalcEngine::CCalcEngine
+//
+//////////////////////////////////////////////////
+CCalcEngine::CCalcEngine(
+    bool fPrecedence,
+    bool fIntegerMode,
+    CalculationManager::IResourceProvider* const pResourceProvider,
+    __in_opt ICalcDisplay* pCalcDisplay,
+    __in_opt shared_ptr<IHistoryDisplay> pHistoryDisplay)
+    : m_fPrecedence(fPrecedence)
+    , m_fIntegerMode(fIntegerMode)
+    , m_pCalcDisplay(pCalcDisplay)
+    , m_resourceProvider(pResourceProvider)
+    , m_nOpCode(0)
+    , m_nPrevOpCode(0)
+    , m_bChangeOp(false)
+    , m_bRecord(false)
+    , m_bSetCalcState(false)
+    , m_input(DEFAULT_DEC_SEPARATOR)
+    , m_nFE(FMT_FLOAT)
+    , m_memoryValue{ make_unique<Rational>() }
+    , m_holdVal{}
+    , m_currentVal{}
+    , m_lastVal{}
+    , m_parenVals{}
+    , m_precedenceVals{}
+    , m_bError(false)
+    , m_bInv(false)
+    , m_bNoPrevEqu(true)
+    , m_radix(DEFAULT_RADIX)
+    , m_precision(DEFAULT_PRECISION)
+    , m_cIntDigitsSav(DEFAULT_MAX_DIGITS)
+    , m_decGrouping()
+    , m_numberString(DEFAULT_NUMBER_STR)
+    , m_nTempCom(0)
+    , m_openParenCount(0)
+    , m_nOp()
+    , m_nPrecOp()
+    , m_precedenceOpCount(0)
+    , m_nLastCom(0)
+    , m_angletype(ANGLE_DEG)
+    , m_numwidth(QWORD_WIDTH)
+    , m_HistoryCollector(pCalcDisplay, pHistoryDisplay, DEFAULT_DEC_SEPARATOR)
+    , m_groupSeparator(DEFAULT_GRP_SEPARATOR)
+{
+    InitChopNumbers();
+    }
+    
+    #include 'common.h'
+    
+    #include 'common.h'
+    
+    namespace aria2 {
+    }
+    
+      /* Parse content of <Body> element */
+  return_value = Array::Create();
+  resp = body->children;
+  while (resp != nullptr && resp->type != XML_ELEMENT_NODE) {
+    resp = resp->next;
+  }
+  if (resp != nullptr) {
+    if (fn && fn->binding && fn->binding->bindingType == BINDING_SOAP) {
+      /* Function has WSDL description */
+      sdlParamPtr h_param, param;
+      xmlNodePtr val = nullptr;
+      const char *name, *ns = nullptr;
+      Variant tmp(Variant::NullInit{});
+      sdlSoapBindingFunctionPtr fnb =
+        (sdlSoapBindingFunctionPtr)fn->bindingAttributes;
+      int res_count;
+    }
+    }
+    
+    //////////////////////////////////////////////////////////////////////
+    
+      bool operator==(double v) const { return compare(v) == 0;}
+  bool operator!=(double v) const { return compare(v) != 0;}
+  bool operator>=(double v) const { return compare(v) >= 0;}
+  bool operator<=(double v) const { return compare(v) <= 0;}
+  bool operator> (double v) const { return compare(v) >  0;}
+  bool operator< (double v) const { return compare(v) <  0;}
+    
+    void printStr(int fd, folly::StringPiece s) {
+  write(fd, s.begin(), s.size());
+}
+    
+      // Form a linked list of mappers for the same range.
+  static void append(RangeMapper**& ptail, RangeMapper* next) {
+    assert((*ptail) == nullptr);        // always keep track of the tail.
+    *ptail = next;
+    ptail = &(next->m_fallback);
+    assert((*ptail) == nullptr);        // next already has fallback?
   }
     
-      // The mask for the fraction bits.
-  static const Bits kFractionBitMask =
-    ~static_cast<Bits>(0) >> (kExponentBitCount + 1);
     
-    // A unique type used as the default value for the arguments of class
-// template Types.  This allows us to simulate variadic templates
-// (e.g. Types<int>, Type<int, double>, and etc), which C++ doesn't
-// support directly.
-struct None {};
+    {}
+    
+      const Mutex& getMutex() const { return m_mutex; }
+  Mutex& getMutex() { return m_mutex; }
+    
+    
+    {  v << divsd{divisor, dividend, d};
+}
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    #include <folly/Format.h>
+    
+    class Context
+{
+public:
+    Context(size_t stack_size, coroutine_func_t fn, void* private_data);
+    ~Context();
+    bool swap_in();
+    bool swap_out();
+#if !defined(SW_NO_USE_ASM_CONTEXT) && defined(SW_LOG_TRACE_OPEN)
+    ssize_t get_stack_usage();
+#endif
+    inline bool is_end()
+    {
+        return end_;
+    }
+    static void context_func(void* arg);
+    }
+    
+    
+    {
+    {
+    {            ret = chan->push(&i);
+            ASSERT_TRUE(ret);
+            ret = chan->push(&i);
+            ASSERT_TRUE(ret);
+        }, &chan)
+    });
+}
+    
+        inline size_t producer_num()
+    {
+        return producer_queue.size();
+    }
+    
+    
+    {    for (i = 0; i < 10; i++)
+    {
+        free(lists[i]);
+    }
+}
+
+    
+        for (long i = 0; i < N; i++)
+    {
+        ASSERT_EQ(swThreadPool_dispatch(&pool, (void*) &result, sizeof(result)), SW_OK);
+    }
+    
+    
+    {
+    {        ASSERT_EQ(WEXITSTATUS(status), 0);
+    });
+}
+    
+        for (i = 1; i < 1024; i++)
+    {
+        uint32_t key = ((rand() % 19999) + 1) * 37;
+        int ret = (int) (long) swRbtree_find(tree, key);
+        ASSERT_GT(ret, 0);
+        lists.insert(key);
+    }
