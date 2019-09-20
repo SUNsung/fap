beedc2fc8f1b9d47bd646ae6d89d3620db107fca
SUNsung <weatherius@gmail.com>
2019-09-20 17:19:22 +0300

    Rewrite [31vrNjcUm4xyM31U2mCeiXzXI231yV5t.d1H94c31X1k.cpp] File in: 2019-09-20 17:19:21

--- cccccc/31vrNjcUm4xyM31U2mCeiXzXI231yV5t.d1H94c31X1k.cpp
+++ cccccc/31vrNjcUm4xyM31U2mCeiXzXI231yV5t.d1H94c31X1k.cpp
@@ -1,57 +1,270 @@
 
         
-            if (error != CL_SUCCESS) {
-        return error;
-    } 
+        #include 'absl/strings/str_cat.h'
+#include 'absl/strings/str_format.h'
+#include 'tensorflow/compiler/xla/service/gpu/hlo_execution_profiler.h'
+#include 'tensorflow/compiler/xla/types.h'
+#include 'tensorflow/compiler/xla/util.h'
+#include 'tensorflow/compiler/xla/xla_data.pb.h'
+#include 'tensorflow/core/platform/logging.h'
+#include 'tensorflow/core/platform/stream_executor_no_cuda.h'
+#include 'tensorflow/stream_executor/blas.h'
+#include 'tensorflow/stream_executor/device_memory.h'
     
-    #endif // OPENPOSE_HAND_HAND_PARAMETERS_HPP
+    // ConditionalThunk implements the conditional instruction on GPU by reading the
+// predicate of the conditional and executing the true or the false computation
+// depending on the value of the predicate.
+//
+// ConditionalThunk assumes that the buffers of the conditional result and the
+// result of the true and false computations share the same allocation. Also,
+// the buffers of the true operand of the conditional and that of the parameter
+// instruction of the true computation share the same allocation. Similarly, the
+// buffers of the false operand and that of the parameter instruction of the
+// false computation share the same allocation.
+class ConditionalThunk : public Thunk {
+ public:
+  ConditionalThunk(
+      const BufferAllocation::Slice& branch_index_buffer_index,
+      absl::Span<const BufferAllocation::Slice> branch_operand_buffer_indexes,
+      std::vector<ThunkSequence> branch_thunk_sequences,
+      const HloInstruction* hlo);
+    }
+    
+    namespace xla {
+namespace gpu {
+    }
+    }
+    
+    // Thunk that sets a given chunk of memory to a particular 32-bit value.  The
+// destination chunk must have size divisible by 32 bits.
+class Memset32BitValueThunk : public Thunk {
+ public:
+  explicit Memset32BitValueThunk(uint32 value,
+                                 const BufferAllocation::Slice& dest,
+                                 const HloInstruction* hlo)
+      : Thunk(Kind::kMemset32BitValue, hlo), value_(value), dest_(dest) {}
+    }
+    
+    #include 'absl/container/flat_hash_set.h'
+#include 'tensorflow/compiler/xla/service/buffer_assignment.h'
+#include 'tensorflow/compiler/xla/service/gpu/buffer_allocations.h'
+#include 'tensorflow/compiler/xla/service/gpu/hlo_execution_profiler.h'
+#include 'tensorflow/compiler/xla/service/gpu/thunk.h'
+#include 'tensorflow/compiler/xla/service/hlo_instruction.h'
+#include 'tensorflow/core/platform/mutex.h'
+#include 'tensorflow/core/platform/stream_executor_no_cuda.h'
+#include 'tensorflow/core/platform/types.h'
+    
+      xla::XlaBuilder* b = ctx->builder();
+  // Find the max in each batch, resulting in a tensor of shape [batch]
+  auto logits_max =
+      xla::Reduce(logits, xla::MinValue(b, xla_type), max_func, {kClassDim});
+    
+    // Helper method to convert an orientation index to its value in degrees.
+// The value represents the amount of clockwise rotation in degrees that must be
+// applied for the text to be upright (readable).
+TESS_API int OrientationIdToValue(const int& id);
+    
+      // Transforms the given coords one step forward to normalized space, without
+  // using any block rotation or predecessor.
+  void LocalNormTransform(const TPOINT& pt, TPOINT* transformed) const;
+  void LocalNormTransform(const FCOORD& pt, FCOORD* transformed) const;
+  // Transforms the given coords forward to normalized space using the
+  // full transformation sequence defined by the block rotation, the
+  // predecessors, deepest first, and finally this. If first_norm is not nullptr,
+  // then the first and deepest transformation used is first_norm, ending
+  // with this, and the block rotation will not be applied.
+  void NormTransform(const DENORM* first_norm, const TPOINT& pt,
+                     TPOINT* transformed) const;
+  void NormTransform(const DENORM* first_norm, const FCOORD& pt,
+                     FCOORD* transformed) const;
+  // Transforms the given coords one step back to source space, without
+  // using to any block rotation or predecessor.
+  void LocalDenormTransform(const TPOINT& pt, TPOINT* original) const;
+  void LocalDenormTransform(const FCOORD& pt, FCOORD* original) const;
+  // Transforms the given coords all the way back to source image space using
+  // the full transformation sequence defined by this and its predecessors
+  // recursively, shallowest first, and finally any block re_rotation.
+  // If last_denorm is not nullptr, then the last transformation used will
+  // be last_denorm, and the block re_rotation will never be executed.
+  void DenormTransform(const DENORM* last_denorm, const TPOINT& pt,
+                       TPOINT* original) const;
+  void DenormTransform(const DENORM* last_denorm, const FCOORD& pt,
+                       FCOORD* original) const;
+    
+    STRING ParagraphModel::ToString() const {
+  char buffer[200];
+  const STRING &alignment = ParagraphJustificationToString(justification_);
+  snprintf(buffer, sizeof(buffer),
+           'margin: %d, first_indent: %d, body_indent: %d, alignment: %s',
+           margin_, first_indent_, body_indent_, alignment.string());
+  return STRING(buffer);
+}
 
     
-    namespace op
+    struct PARA : public ELIST_LINK {
+ public:
+  PARA() : model(nullptr), is_list_item(false),
+           is_very_first_or_continuation(false), has_drop_cap(false) {}
+    }
+    
+      // Functions to navigate the tree. Unlike the original implementation, we
+  // store the root at index 0.
+  int ParentNode(int index) const {
+    return (index + 1) / 2 - 1;
+  }
+  int LeftChild(int index) const {
+    return index * 2 + 1;
+  }
+    
+    namespace tesseract {
+    }
+    
+      // Return the minimum number of characters that must be used from this string
+  // to obtain a match in the UNICHARMAP.
+  int minmatch(const char* const unichar_repr) const;
+    
+    /**
+ * @brief Clip: @f$ y = \max(min, \min(max, x)) @f$.
+ */
+template <typename Dtype>
+class ClipLayer : public NeuronLayer<Dtype> {
+ public:
+  /**
+   * @param param provides ClipParameter clip_param,
+   *     with ClipLayer options:
+   *   - min
+   *   - max
+   */
+  explicit ClipLayer(const LayerParameter& param)
+      : NeuronLayer<Dtype>(param) {}
+    }
+    
+    #include 'caffe/layers/clip_layer.hpp'
+    
+    #endif  // CAFFE_SOLVER_HPP_
+
+    
+    
+    {  // Since boost 1.53 boost.python will convert str and bytes
+  // to std::string but will convert std::string to str. Here we
+  // force a bytes object to be returned. When this object
+  // is passed back to the NCCL constructor boost.python will
+  // correctly convert the bytes to std::string automatically
+  PyObject* py_uid = PyBytes_FromString(uid.c_str());
+  return bp::object(bp::handle<>(py_uid));
+#else
+  // automatic conversion is correct for python 2.
+  return bp::object(uid);
+#endif
+}
+#endif
+    
+    namespace caffe {
+    }
+    
+      // Set the signal handlers to the default.
+  void UnhookHandler() {
+    if (already_hooked_up) {
+      struct sigaction sa;
+      // Setup the sighup handler
+      sa.sa_handler = SIG_DFL;
+      // Restart the system call, if at all possible
+      sa.sa_flags = SA_RESTART;
+      // Block every signal during the handler
+      sigfillset(&sa.sa_mask);
+      // Intercept SIGHUP and SIGINT
+      if (sigaction(SIGHUP, &sa, NULL) == -1) {
+        LOG(FATAL) << 'Cannot uninstall SIGHUP handler.';
+      }
+      if (sigaction(SIGINT, &sa, NULL) == -1) {
+        LOG(FATAL) << 'Cannot uninstall SIGINT handler.';
+      }
+    }
+    }
+    
+    /* Return the indices of the top N values of vector v. */
+static std::vector<int> Argmax(const std::vector<float>& v, int N) {
+  std::vector<std::pair<float, int> > pairs;
+  for (size_t i = 0; i < v.size(); ++i)
+    pairs.push_back(std::make_pair(v[i], i));
+  std::partial_sort(pairs.begin(), pairs.begin() + N, pairs.end(), PairCompare);
+    }
+    
+      char label_i;
+  char label_j;
+  char* pixels = new char[2 * rows * cols];
+  std::string value;
+    
+    #endif  // CAFFE_DATA_TRANSFORMER_HPP_
+
+    
+                    fstream.PutMarker(FileMarker::fileMarkerBeginSection, L'BMomentumAsTimeConstant');
+                fstream << m_blockMomentumAsTimeConstantPerWorker; 
+                fstream.PutMarker(FileMarker::fileMarkerEndSection, L'EMomentumAsTimeConstant');
+    
+        NDLScript<ElemType> m_script;
+    const ConfigParameters* m_baseConfig; // NOTE: the lifetime of the parent MUST exist from the call to Init to the BuildNetworkFromDescription() call for stringize
+    
+    
+    {            outputFromEachLayer[1] = input;
+        }
+    
+        // determine which epoch to start with, including recovering a checkpoint if any and 'makeMode' enabled
+    int startEpoch = optimizer->DetermineStartEpoch(makeMode);
+    if (startEpoch == optimizer->GetMaxEpochs())
+    {
+        LOGPRINTF(stderr, 'No further training is necessary.\n');
+        return;
+    }
+    
+    
+template <typename ElemType>
+void CNTKEvalBase<ElemType>::Init(const std::string& config)
 {
-    struct PersonEntry
-    {
-        long long counterLastDetection;
-        std::vector<cv::Point2f> keypoints;
-        std::vector<char> status;
-        /*
-        PersonEntry(long long _last_frame, 
-                    std::vector<cv::Point2f> _keypoints,
-                    std::vector<char> _active):
-                    last_frame(_last_frame), keypoints(_keypoints),
-                    active(_active)
-                    {}
-        */
-    };
-    class OP_API PersonIdExtractor
-    {
-    }
+    m_config.Parse(config);
+    size_t nThreads = m_config('numCPUThreads', '1');
+    CPUMatrix<ElemType>::SetNumThreads(nThreads);
     }
     
-    BulkOperations::AbstractOperation::AbstractOperation(
-    QSharedPointer<RedisClient::Connection> connection, int dbIndex,
-    OperationCallback callback, QRegExp keyPattern)
-    : m_connection(connection),
-      m_dbIndex(dbIndex),
-      m_keyPattern(keyPattern),
-      m_currentState(State::READY),
-      m_progress(0),
-      m_callback(callback) {}
     
-    BulkOperations::CopyOperation::CopyOperation(
-    QSharedPointer<RedisClient::Connection> connection, int dbIndex,
-    OperationCallback callback, QRegExp keyPattern)
-    : BulkOperations::AbstractOperation(connection, dbIndex, callback,
-                                        keyPattern) {}
-    
-    BulkOperations::TtlOperation::TtlOperation(
-    QSharedPointer<RedisClient::Connection> connection, int dbIndex,
-    OperationCallback callback, QRegExp keyPattern)
-    : BulkOperations::AbstractOperation(connection, dbIndex, callback,
-                                        keyPattern) {}
-    
-      void openServerStats() override;
+    {                if (m_LearnableNodePtr.find(name) == m_LearnableNodePtr.end())
+                    LogicError('DoneWithCurrentSubMinibatch: Node '%ls' not found in LearnableNode set.', name.c_str());
+                m_LearnableNodePtr[name]->Gradient().SetValue(accumulategrad);
+                accumulategrad.SetValue(0);
+            }
+            // also revert net.m_MBLayoutPtr
+            m_netMBLayoutPtr->CopyFrom(m_MBLayoutCache);
     
     
-    {    return callback(filter, processed, e);
-  };
+    {
+    {                m_totalSamplesProcessedSinceLastReport = 0; 
+                m_localSamplesProcessedSinceLastReport = 0; 
+            }
+        }
+        void OnArriveAtSyncPoint(double secondOnSyncPoint, bool printMessage)
+        {
+            if (printMessage)
+            {
+                m_accumulatedSecondsOnSyncPointInOneEpoch += secondOnSyncPoint;
+                m_syncPointHitCounterInOneEpoch++;
+                fprintf(stderr, '\t\t(model aggregation stats): %d-th sync point was hit, introducing a %.2f-seconds latency this time; accumulated time on sync point = %.2f seconds , average latency = %.2f seconds\n',
+                        (int)m_syncPointHitCounterInOneEpoch, 
+                        secondOnSyncPoint, 
+                        m_accumulatedSecondsOnSyncPointInOneEpoch, 
+                        m_accumulatedSecondsOnSyncPointInOneEpoch / m_syncPointHitCounterInOneEpoch);
+            }
+        }
+    
+        BOOST_CHECK_EQUAL_COLLECTIONS(result.begin(), result.end(), expected.begin(), expected.end());
+    
+    // We accumulate criteria in this struct.
+// Criteria are accumulated together with their counts (counts depend on sequence lengths, and different criteria may have different sequence lengths).
+struct CriterionAccumulatorBase
+{
+    CriterionAccumulatorBase() {};
+    virtual ~CriterionAccumulatorBase() {};
+    virtual const CriterionAccumulatorBase& Add(size_t i, size_t numSamplesInMinibatch) = 0;
+    virtual const CriterionAccumulatorBase& Assign(size_t i, size_t numSamplesInMinibatch) = 0;
+    virtual EpochCriterion GetCriterion(size_t i) const = 0;
+};
