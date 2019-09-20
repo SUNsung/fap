4ddbabd0596889f9df7e7aca3db93fdbca6deb08
SUNsung <weatherius@gmail.com>
2019-09-20 23:22:18 +0300

    Rewrite [12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb] File in: 2019-09-20 23:22:18

--- ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
+++ ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
@@ -1,161 +1,141 @@
 
         
-            should 'hide unpublished page' do
-      refute_exist dest_dir('/unpublished.html')
-    end
-    
-    def gem_file
-  '#{name}-#{Gem::Version.new(version)}.gem'
-end
-    
-    Nullam luctus fermentum est id blandit. Phasellus consectetur ullamcorper
-ligula, at finibus eros laoreet id. Etiam sit amet est in libero efficitur
-tristique. Ut nec magna augue. Quisque ut fringilla lacus, ac dictum enim.
-Aliquam vel ornare mauris. Suspendisse ornare diam tempor nulla facilisis
-aliquet. Sed ultrices placerat ultricies.
-TEXT
-    
-    def graceful_require
-  Jekyll::External.require_with_graceful_fail('json')
-  JSON.pretty_generate(DATA)
-end
-    
-    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
-COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
-    
-        if successfully_sent?(resource)
-      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
-    end
-  end
-    
-        # The path used after sending unlock password instructions
-    def after_sending_unlock_instructions_path_for(resource)
-      new_session_path(resource) if is_navigational_format?
-    end
-    
-      def respond_with_navigational(*args, &block)
-    respond_with(*args) do |format|
-      format.any(*navigational_formats, &block)
-    end
-  end
-    
-        def reset_password_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :reset_password_instructions, opts)
-    end
-    
-    DeviseCreateUsers.migrate(:up)
-    
-          generate_helpers!(Devise::URL_HELPERS)
-    
-        def initialize(name, options) #:nodoc:
-      @scoped_path = options[:as] ? '#{options[:as]}/#{name}' : name.to_s
-      @singular = (options[:singular] || @scoped_path.tr('/', '_').singularize).to_sym
-    
-    describe SoftwareSpec do
-  subject(:spec) { described_class.new }
-    
-          def pretty_name
-        to_sym.to_s.split('_').map(&:capitalize).join(' ')
+              def self.is_supported?(platform)
+        platform == :ios
       end
     
-          ohai 'Installing Formula dependencies: #{not_installed.map(&:to_s).join(', ')}'
-      not_installed.each do |formula|
-        FormulaInstaller.new(formula).tap do |fi|
-          fi.installed_as_dependency = true
-          fi.installed_on_request = false
-          fi.show_header = true
-          fi.verbose = verbose?
-          fi.prelude
-          fi.install
-          fi.finish
-        end
+            build.update_build_information!(description: 'a newer description')
+      end
+    
+            v = history[8]
+        expect(v.version_string).to eq('1.4.1')
+        expect(v.version_id).to eq(815_259_390)
+        expect(v.items.count).to eq(7)
+        expect(v.items[3].state_key).to eq('pendingDeveloperRelease')
+        expect(v.items[3].user_name).to eq('Apple')
+        expect(v.items[3].user_email).to eq(nil)
+        expect(v.items[3].date).to eq(1_450_461_891_000)
       end
     end
     
-          it 'adds the appropriate curl args' do
-        expect(subject).to receive(:system_command!) { |*, args:, **|
-          expect(args.each_cons(2)).to include(['--user-agent', 'Mozilla/25.0.1'])
-        }
-    
-            # Initialize a new instance from its attributes.
-        #
-        # @param [Array<Specification>] specs      @see #specs
-        # @param [Array<Specification>] test_specs @see #test_specs
-        # @param [Array<Specification>] app_specs  @see #app_specs
-        # @param [Platform] platform               @see #platform
-        # @param [Target::BuildType] build_type    @see #build_type
-        #
-        def initialize(specs, test_specs, app_specs, platform, build_type = Target::BuildType.static_library)
-          @specs = specs
-          @test_specs = test_specs
-          @app_specs = app_specs
-          @platform = platform
-          @build_type = build_type
-          @hash = [specs, platform, build_type].hash
-        end
-    
-    Then(/^the release is created$/) do
-  run_vagrant_command('ls -g #{TestApp.releases_path}')
-end
-    
-        def servers
-      @servers ||= Servers.new
+        describe '#create_app_group' do
+      it 'should make a request create an app group' do
+        response = subject.create_app_group!('Production App Group', 'group.tools.fastlane.spaceship')
+        expect(response['name']).to eq('Production App Group')
+        expect(response['identifier']).to eq('group.tools.fastlane')
+      end
     end
     
-          def question
-        if default.nil?
-          I18n.t(:question, key: key, scope: :capistrano)
-        else
-          I18n.t(:question_default, key: key, default_value: default, scope: :capistrano)
-        end
+        # @param lane [Lane] A lane object
+    def add_lane(lane, override = false)
+      lanes[lane.platform] ||= {}
+    
+        describe 'handle conflicts' do
+      it 'Both primary and secondary are available, and both match the export method, it should prefer the primary mapping' do
+        result = csm.merge_profile_mapping(primary_mapping: { 'identifier.1' => 'Ap-pStoreValue2' },
+                                       secondary_mapping: { 'identifier.1' => 'Ap-pStoreValue1' },
+                                           export_method: 'app-store')
+    
+    module Fastlane
+  module Actions
+    class UploadSymbolsToCrashlyticsAction < Action
+      def self.run(params)
+        require 'tmpdir'
+    
+        def add_entry(entry)
+      if @index.add?(entry.as_json.to_s)
+        @entries << entry.dup
+        @types[entry.type].count += 1 if entry.type
       end
+    end
     
-            def fetch(key)
-          @properties[key]
+        def root_page?
+      subpath.blank? || subpath == '/' || subpath == root_path
+    end
+    
+        attr_reader :filters
+    
+        def blank?
+      body.blank?
+    end
+    
+        def root_path
+      self.class.root_path
+    end
+    
+            css('.l-sub-section', '.alert', '.banner').each do |node|
+          node.name = 'blockquote'
         end
     
-          def each
-        servers_by_key.values.each { |server| yield server }
-      end
-    
-          # TODO: translate attribute first
-      # if BASE_ATTRIBUTES_KEY.key respond to a string message, then short_message is returned
-      # if BASE_ATTRIBUTES_KEY.key respond to a Hash, means it may have problem , summary and resolution
-      def compose_message(key, **attributes)
-        short_message = translate_message(key, **attributes)
-        if short_message.is_a? Hash
-          @problem = problem(key, **attributes)
-          @summary = summary(key, **attributes)
-          @resolution = resolution(key, **attributes)
-          [['Problem', @problem], ['Summary', @summary], ['Resolution', @resolution]].reduce('') do |message, detail_array|
-            message << '\n#{detail_array[0]}:\n  #{detail_array[1]}' unless detail_array[1].blank?
-            message
+            css('ul.methods', 'ul.properties', 'ul.events').add_class('defs').each do |node|
+          node.css('> li > h3').each do |h3|
+            next if h3.content.present?
+            h3.content = h3.next_element.content
+            h3.next_element.remove
           end
-        else
-          short_message
         end
       end
+    end
+  end
+end
+
     
-          context 'when the fallback locale is not available' do
-        before do
-          I18n.available_locales = %i[de jp]
-          I18n.default_locale = :de
+        if resource.errors.empty?
+      set_flash_message! :notice, :unlocked
+      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
+    
+        if notice
+      set_flash_message! :notice, notice
+      true
+    end
+  end
+    
+    if defined?(ActionMailer)
+  class Devise::Mailer < Devise.parent_mailer.constantize
+    include Devise::Mailers::Helpers
+    
+    class ApplicationController < ActionController::Base
+end
+    
+          def stored_location_key_for(resource_or_scope)
+        scope = Devise::Mapping.find_scope!(resource_or_scope)
+        '#{scope}_return_to'
+      end
+    
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
         end
     
-              get '/', a: %w[the other]
-          expect(last_response.status).to eq(200)
-          expect(last_response.body).to eq('['the', 'other']')
+          def devise_mapping
+        @devise_mapping ||= Devise.mappings[scope_name]
+      end
     
-    task :spec
+          def unauthorized
+        render 'spree/api/errors/unauthorized', status: 401 and return
+      end
     
-          # This module extends user defined helpers
-      # to provide some API-specific functionality.
-      module BaseHelper
-        attr_accessor :api
-        def params(name, &block)
-          @named_params ||= {}
-          @named_params[name] = block
+            def create
+          authorize! :create, Image
+          @image = scope.images.new(image_params)
+          if @image.save
+            respond_with(@image, status: 201, default_template: :show)
+          else
+            invalid_resource!(@image)
+          end
+        end
+    
+              inventory_unit.transaction do
+            if inventory_unit.update(inventory_unit_params)
+              fire
+              render :show, status: 200
+            else
+              invalid_resource!(inventory_unit)
+            end
+          end
         end
