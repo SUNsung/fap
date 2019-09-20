72ec26183364a33c904737bee03e95f3f0cafeb7
SUNsung <weatherius@gmail.com>
2019-09-20 08:36:39 +0300

    Rewrite [50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py] File in: 2019-09-20 08:36:39

--- pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
+++ pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
@@ -1,117 +1,57 @@
 
         
-            OPERATOR = 0
-    SUPERVISOR = 1
-    DIRECTOR = 2
-    
-            The shuffle/sort step of MapReduce will then do a
-        distributed sort on the keys, resulting in:
-    
-    
-class HashTable(object):
-    
-            # Adding unique constraint on 'TagValue', fields ['project_id',
-        # 'environment_id', '_key', 'value']
-        db.create_unique(u'tagstore_tagvalue', ['project_id', 'environment_id', 'key', 'value'])
-    
-        def backwards(self, orm):
-        # Adding model 'TagKey'
-        db.create_table(u'tagstore_tagkey', (
-            ('status', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
-            ('environment_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)),
-            ('values_seen', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(default=0)),
-            ('key', self.gf('django.db.models.fields.CharField')(max_length=32)),
-            ('project_id', self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(db_index=True)),
-            ('id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(primary_key=True)),
-        ))
-        db.send_create_signal('tagstore', ['TagKey'])
-    
-        for resolution in resolution_list:
+            @nonce.decoder
+    def nonce(value):  # pylint: disable=missing-docstring,no-self-argument
         try:
-            activity = Activity.objects.filter(
-                group=resolution.group_id,
-                type=Activity.SET_RESOLVED_IN_RELEASE,
-                ident=resolution.id,
-            ).order_by('-datetime')[0]
-        except IndexError:
-            continue
+            return jose.decode_b64jose(value)
+        except jose.DeserializationError as error:
+            # TODO: custom error
+            raise jose.DeserializationError('Invalid nonce: {0}'.format(error))
     
-    from sentry.models import Option
-from sentry.options import default_manager
-from sentry.options.manager import UnknownOption
-from sentry.tasks.base import instrumented_task
+    from certbot import errors
     
-    from __future__ import absolute_import
+        def test_get_sni_addr(self):
+        from certbot_apache.obj import Addr
+        self.assertEqual(
+            self.addr.get_sni_addr('443'), Addr.fromstring('*:443'))
+        self.assertEqual(
+            self.addr.get_sni_addr('225'), Addr.fromstring('*:225'))
+        self.assertEqual(
+            self.addr1.get_sni_addr('443'), Addr.fromstring('127.0.0.1'))
     
-            for job in job_list:
-            logger.debug('Sending scheduled job %s with payload %r', job.name, job.payload)
-            app.send_task(job.name, kwargs=job.payload)
-            job.delete()
-
+    DOMAIN = 'example.com'
+KEY = jose.JWKRSA.load(test_util.load_vector('rsa512_key.pem'))
     
-        To extract all the way down to the ndarray, pass ``extract_numpy=True``.
+            if record_contents:
+            # We need to remove old records in the same request
+            data['deletions'] = [
+                {
+                    'kind': 'dns#resourceRecordSet',
+                    'type': 'TXT',
+                    'name': record_name + '.',
+                    'rrdatas': record_contents,
+                    'ttl': record_ttl,
+                },
+            ]
     
-        def time_add_overflow_arr_mask_nan(self):
-        checked_add_with_arr(self.arr, self.arr_mixed, arr_mask=self.arr_nan_1)
+            # Adding index on 'EventTag', fields ['environment_id', 'key', 'value']
+        db.create_index(u'tagstore_eventtag', ['environment_id', 'key', 'value'])
     
-        def time_series_constructor(self, data_fmt, with_index, dtype):
-        Series(self.data, index=self.index)
+            # Changing field 'GroupTagKey.group_id'
+        db.alter_column(u'tagstore_grouptagkey', 'group_id', self.gf(
+            'sentry.db.models.fields.bounded.BoundedPositiveIntegerField')())
     
-    def _prepare_file(fileDir, file_=None):
-    fileDict = {}
-    if file_:
-        if hasattr(file_, 'read'):
-            file_ = file_.read()
+        def run(self):
+        while True:
+            try:
+                ident, func, args, kwargs = self.queue.get_nowait()
+            except Empty:
+                break
+    
+        if debug_on:
+        if logging_file:
+            if len(_logger.handlers) < 2:
+                _logger.addHandler(logging.FileHandler(logging_file))
+                _logger.setLevel(logging.DEBUG)
         else:
-            return ReturnValue({'BaseResponse': {
-                'ErrMsg': 'file_ param should be opened file',
-                'Ret': -1005, }})
-    else:
-        if not utils.check_file(fileDir):
-            return ReturnValue({'BaseResponse': {
-                'ErrMsg': 'No file found in specific dir',
-                'Ret': -1002, }})
-        with open(fileDir, 'rb') as f:
-            file_ = f.read()
-    fileDict['fileSize'] = len(file_)
-    fileDict['fileMd5'] = hashlib.md5(file_).hexdigest()
-    fileDict['file_'] = io.BytesIO(file_)
-    return fileDict
-    
-    from mitmproxy import log
-from mitmproxy import ctx
-    
-        def send(self, message):
-        if isinstance(message, list):
-            message = b''.join(message)
-        self.wfile.write(message)
-        self.wfile.flush()
-    
-        class ImageData(KaitaiStruct):
-        def __init__(self, _io, _parent=None, _root=None):
-            self._io = _io
-            self._parent = _parent
-            self._root = _root if _root else self
-            self.lzw_min_code_size = self._io.read_u1()
-            self.subblocks = self._root.Subblocks(self._io, self, self._root)
-    
-        '''
-    Return true if the continuation bit is set in the byte
-    '''
-    def checkContinuationBit(self, byteval):
-        continuationBitmask = 0x80
-        return (continuationBitmask & byteval) != 0
-    
-        def take(self):
-        '''
-        Scripts or other parties make 'take' a reply out of a normal flow.
-        For example, intercepted flows are taken out so that the connection thread does not proceed.
-        '''
-        if self.state != 'start':
-            raise exceptions.ControlException(
-                'Reply is {}, but expected it to be start.'.format(self.state)
-            )
-        self._state = 'taken'
-    
-    '''
-import unittest
+            raise IOError('SOUTH_LOGGING_ON is True. You also need a SOUTH_LOGGING_FILE setting.')
